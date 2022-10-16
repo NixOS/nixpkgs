315147549c587daff4f51b8b829c389648f02960
David Costa <david@zarel.net>
2022-10-16 19:16:10 +0200

    nixosTests.syncthing: test integrity of shared file

--- nixos/tests/syncthing/syncthing-declarative.nix
+++ nixos/tests/syncthing/syncthing-declarative.nix
@@ -7,6 +7,7 @@ import ../make-test-python.nix ({ lib, pkgs, ... }:
     '';
     idA = genNodeId "a";
     idB = genNodeId "b";
+    idC = genNodeId "c";
   in
   {
     name = "syncthing";
@@ -19,12 +20,13 @@ import ../make-test-python.nix ({ lib, pkgs, ... }:
           openDefaultPorts = true;
           cert = "${idA}/cert.pem";
           key = "${idA}/key.pem";
-          devices.b = {
-            id = lib.fileContents "${idB}/id";
-          };
-          folders.foo = {
-            path = "/var/lib/syncthing/foo";
-            devices = [ "b" ];
+          devices.b.id = lib.fileContents "${idB}/id";
+          devices.c.id = lib.fileContents "${idC}/id";
+          folders = {
+            foo = {
+              path = "/var/lib/syncthing/foo";
+              devices = [ "b" "c" ];
+            };
           };
         };
       };
@@ -34,12 +36,29 @@ import ../make-test-python.nix ({ lib, pkgs, ... }:
           openDefaultPorts = true;
           cert = "${idB}/cert.pem";
           key = "${idB}/key.pem";
-          devices.a = {
-            id = lib.fileContents "${idA}/id";
+          devices.a.id = lib.fileContents "${idA}/id";
+          devices.c.id = lib.fileContents "${idC}/id";
+          folders = {
+            foo = {
+              path = "/var/lib/syncthing/foo";
+              devices = [ "a" "c" ];
+            };
           };
-          folders.foo = {
-            path = "/var/lib/syncthing/foo";
-            devices = [ "a" ];
+        };
+      };
+      c = {
+        services.syncthing = {
+          enable = true;
+          openDefaultPorts = true;
+          cert = "${idC}/cert.pem";
+          key = "${idC}/key.pem";
+          devices.a.id = lib.fileContents "${idA}/id";
+          devices.b.id = lib.fileContents "${idB}/id";
+          folders = {
+            foo = {
+              path = "/var/lib/syncthing/foo";
+              devices = [ "a" "b" ];
+            };
           };
         };
       };
@@ -50,16 +69,25 @@ import ../make-test-python.nix ({ lib, pkgs, ... }:
 
       a.wait_for_unit("syncthing.service")
       b.wait_for_unit("syncthing.service")
+      c.wait_for_unit("syncthing.service")
       a.wait_for_open_port(22000)
       b.wait_for_open_port(22000)
+      c.wait_for_open_port(22000)
 
       a.wait_for_file("/var/lib/syncthing/foo")
       b.wait_for_file("/var/lib/syncthing/foo")
+      c.wait_for_file("/var/lib/syncthing/foo")
 
-      a.succeed("echo a2b > /var/lib/syncthing/foo/a2b")
-      b.succeed("echo b2a > /var/lib/syncthing/foo/b2a")
+      a.succeed("echo a2bc > /var/lib/syncthing/foo/a2bc")
+      b.succeed("echo b2ac > /var/lib/syncthing/foo/b2ac")
 
-      a.wait_for_file("/var/lib/syncthing/foo/b2a")
-      b.wait_for_file("/var/lib/syncthing/foo/a2b")
+      a.wait_for_file("/var/lib/syncthing/foo/b2ac")
+      b.wait_for_file("/var/lib/syncthing/foo/a2bc")
+      c.wait_for_file("/var/lib/syncthing/foo/b2ac")
+      c.wait_for_file("/var/lib/syncthing/foo/a2bc")
+
+      # Foo on C is trusted, check content of file matches
+      c.succeed("grep a2bc /var/lib/syncthing/foo/a2bc")
+      c.succeed("grep b2ac /var/lib/syncthing/foo/b2ac")
     '';
   })
