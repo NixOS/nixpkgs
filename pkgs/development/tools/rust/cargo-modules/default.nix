a2474ce493d33b9439f6b0170ce28a50b2ac518c
figsoda <figsoda@pm.me>
2022-10-18 10:42:33 -0400

    cargo-modules: 0.5.11 -> 0.5.12

--- pkgs/development/tools/rust/cargo-modules/default.nix
+++ pkgs/development/tools/rust/cargo-modules/default.nix
@@ -1,30 +1,28 @@
-{ lib, rustPlatform, fetchFromGitHub, stdenv, CoreFoundation, CoreServices }:
+{ lib, rustPlatform, fetchCrate, stdenv, CoreFoundation, CoreServices }:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-modules";
-  version = "0.5.11";
+  version = "0.5.12";
 
-  src = fetchFromGitHub {
-    owner = "regexident";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-dxy46ls0n7j2uax+djqB9Zy/uGgV37w5K1Zc8Wzd1Vc=";
+  src = fetchCrate {
+    inherit pname version;
+    sha256 = "sha256-Fm3D1VnudZhXpH7ymQNpViwwODvXYeO/e2xT3XXfclk=";
   };
 
-  cargoSha256 = "sha256-2Q4pGnMo4FiPPGz2XXOv6+zB5DxHA8oEqztidO2Vvyw=";
+  cargoSha256 = "sha256-PiYonf+aD+Q3mWtP+7NDu9wu3vKrMRAlYh94fXLMWD8=";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     CoreFoundation
     CoreServices
   ];
 
+  # the crate version doesn't include all the files required to run tests
+  doCheck = false;
+
   meta = with lib; {
     description = "A cargo plugin for showing a tree-like overview of a crate's modules";
     homepage = "https://github.com/regexident/cargo-modules";
     license = with licenses; [ mpl20 ];
-    # all tests fail with:
-    # thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: "tests run with disabled concurrency, automatic snapshot name generation is not supported.  Consider using the \"backtrace\" feature of insta which tries to recover test names from the call stack."', /private/tmp/nix-build-cargo-modules-0.5.9.drv-0/cargo-modules-0.5.9-vendor.tar.gz/insta/src/runtime.rs:908:22
-    broken = (stdenv.isDarwin && stdenv.isx86_64);
     maintainers = with maintainers; [ figsoda rvarago ];
   };
 }
