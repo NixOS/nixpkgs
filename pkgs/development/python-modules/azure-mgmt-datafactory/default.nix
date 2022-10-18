954a90c7d2bcdf8c539321eda3f8853f854fc296
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-18 23:01:12 +0200

    python310Packages.azure-mgmt-datafactory: update disabled

--- pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
+++ pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   version = "2.8.1";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
