88eab1e431cabd0ed621428d8b40d425a07af39f
Jacob Birkett <jacob@birkett.dev>
2022-10-17 18:52:01 -0700

    lxqt.lxqt-policykit: fix missing meta.mainProgram

--- pkgs/desktops/lxqt/lxqt-policykit/default.nix
+++ pkgs/desktops/lxqt/lxqt-policykit/default.nix
@@ -52,6 +52,7 @@ mkDerivation rec {
   meta = with lib; {
     homepage = "https://github.com/lxqt/lxqt-policykit";
     description = "The LXQt PolicyKit agent";
+    mainProgram = "lxqt-policykit-agent";
     license = licenses.lgpl21Plus;
     platforms = platforms.linux;
     maintainers = teams.lxqt.members;
