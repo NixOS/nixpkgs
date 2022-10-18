36fd12ad76a195a7a21d760984610bc0f106093e
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-18 23:12:36 +0200

    python310Packages.libpyfoscam: disable on older Python releases
    - update meta

--- pkgs/development/python-modules/libpyfoscam/default.nix
+++ pkgs/development/python-modules/libpyfoscam/default.nix
@@ -1,25 +1,31 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "libpyfoscam";
   version = "1.2.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-2E6zQT86UEOKoFnAfXfeWt352HIdOQZBGy5vR0WQO6Y=";
+    hash = "sha256-2E6zQT86UEOKoFnAfXfeWt352HIdOQZBGy5vR0WQO6Y=";
   };
 
   # tests need access to a camera
   doCheck = false;
 
-  pythonImportsCheck = [ "libpyfoscam" ];
+  pythonImportsCheck = [
+    "libpyfoscam"
+  ];
 
   meta = with lib; {
     description = "Python Library for Foscam IP Cameras";
-    homepage = "https://github.com/viswa-swami/python-foscam";
+    homepage = "https://github.com/krmarien/python-foscam";
     license = licenses.lgpl3Plus;
     maintainers = with maintainers; [ dotlambda ];
   };
