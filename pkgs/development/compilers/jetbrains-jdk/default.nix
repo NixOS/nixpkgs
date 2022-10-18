6e4bc89f44b51a8c0b8f9ac3ea9d9a161e512fd1
GenericNerdyUsername <genericnerdyusername@proton.me>
2022-10-18 23:17:38 +0100

    jetbrains-jdk: add JCEF support
    (cherry picked from commit b1efcf7886de07fb8d91d61bfee7431a1fc07904)

--- pkgs/development/compilers/jetbrains-jdk/default.nix
+++ pkgs/development/compilers/jetbrains-jdk/default.nix
@@ -3,11 +3,41 @@
 , fetchFromGitHub
 , jetbrains
 , openjdk17
+, openjdk17-bootstrap
+, git
+, autoconf
+, unzip
+, rsync
+, debugBuild ? false
+
+, libXdamage
+, libXxf86vm
+, libXrandr
+, libXi
+, libXcursor
+, libXrender
+, libX11
+, libXext
+, libxcb
+, nss
+, nspr
+, libdrm
+, mesa
+, wayland
+, udev
 }:
 
 openjdk17.overrideAttrs (oldAttrs: rec {
-  pname = "jetbrains-jdk";
-  version = "17.0.3-b469.37";
+  pname = "jetbrains-jdk-jcef";
+  javaVersion = "17.0.3";
+  build = "653";
+  # To get the new tag:
+  # git clone https://github.com/jetbrains/jetbrainsruntime
+  # cd jetbrainsruntime
+  # git reset --hard [revision]
+  # git log --simplify-by-decoration --decorate=short --pretty=short | grep "jdk-" | cut -d "(" -f2 | cut -d ")" -f1 | awk '{print $2}' | sort -t "-" -k 2 -g | tail -n 1
+  openjdkTag = "jdk-18+0";
+  version = "${javaVersion}-b${build}";
 
   src = fetchFromGitHub {
     owner = "JetBrains";
@@ -19,6 +49,78 @@ openjdk17.overrideAttrs (oldAttrs: rec {
      else "sha256-O+OIDRJcIsb/vhO2+SYuYdUYWYTGkBcQ9cHTExLIFDE=";
   };
 
+  BOOT_JDK = openjdk17-bootstrap.home;
+  SOURCE_DATE_EPOCH = 1666098567;
+
+  patches = [];
+
+  # Configure is done in build phase
+  configurePhase = "true";
+
+  buildPhase = ''
+    runHook preBuild
+
+    mkdir -p jcef_linux_x64/jmods
+    cp ${jetbrains.jcef}/* jcef_linux_x64/jmods
+
+    sed \
+        -e "s/OPENJDK_TAG=.*/OPENJDK_TAG=${openjdkTag}/" \
+        -e "s/SOURCE_DATE_EPOCH=.*//" \
+        -e "s/export SOURCE_DATE_EPOCH//" \
+        -i jb/project/tools/common/scripts/common.sh
+    sed -i "s/STATIC_CONF_ARGS/STATIC_CONF_ARGS \$configureFlags/" jb/project/tools/linux/scripts/mkimages_x64.sh
+    sed \
+        -e "s/create_image_bundle \"jb/#/" \
+        -e "s/echo Creating /exit 0 #/" \
+        -i jb/project/tools/linux/scripts/mkimages_x64.sh
+
+    patchShebangs .
+    ./jb/project/tools/linux/scripts/mkimages_x64.sh ${build} ${if debugBuild then "fd" else "jcef"}
+
+    runHook postBuild
+  '';
+
+  installPhase = let
+    buildType = if debugBuild then "fastdebug" else "release";
+    debugSuffix = if debugBuild then "-fastdebug" else "";
+    jcefSuffix = if debugBuild then "" else "_jcef";
+  in ''
+    runHook preInstall
+
+    rm -rf build/linux-x86_64-server-${buildType}/images/jdk
+    mv build/linux-x86_64-server-${buildType}/images/jbrsdk${jcefSuffix}-${javaVersion}-x64${debugSuffix}-b${build} build/linux-x86_64-server-${buildType}/images/jdk
+  '' + oldAttrs.installPhase + "runHook postInstall";
+
+  postInstall = ''
+    chmod +x $out/lib/openjdk/lib/chrome-sandbox
+  '';
+
+  dontStrip = debugBuild;
+
+  postFixup = ''
+      # Build the set of output library directories to rpath against
+      LIBDIRS="${lib.makeLibraryPath [
+        libXdamage libXxf86vm libXrandr libXi libXcursor libXrender libX11 libXext libxcb
+        nss nspr libdrm mesa wayland udev
+      ]}"
+      for output in $outputs; do
+        if [ "$output" = debug ]; then continue; fi
+        LIBDIRS="$(find $(eval echo \$$output) -name \*.so\* -exec dirname {} \+ | sort -u | tr '\n' ':'):$LIBDIRS"
+      done
+      # Add the local library paths to remove dependencies on the bootstrap
+      for output in $outputs; do
+        if [ "$output" = debug ]; then continue; fi
+        OUTPUTDIR=$(eval echo \$$output)
+        BINLIBS=$(find $OUTPUTDIR/bin/ -type f; find $OUTPUTDIR -name \*.so\*)
+        echo "$BINLIBS" | while read i; do
+          patchelf --set-rpath "$LIBDIRS:$(patchelf --print-rpath "$i")" "$i" || true
+          patchelf --shrink-rpath "$i" || true
+        done
+      done
+    '';
+
+  nativeBuildInputs = [ git autoconf unzip rsync ] ++ oldAttrs.nativeBuildInputs;
+
   meta = with lib; {
     description = "An OpenJDK fork to better support Jetbrains's products.";
     longDescription = ''
@@ -28,7 +130,6 @@ openjdk17.overrideAttrs (oldAttrs: rec {
      include: Subpixel Anti-Aliasing, enhanced font rendering on Linux, HiDPI
      support, ligatures, some fixes for native crashes not presented in
      official build, and other small enhancements.
-
      JetBrains Runtime is not a certified build of OpenJDK. Please, use at
      your own risk.
     '';
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28962,6 +28962,7 @@ with pkgs;
     jdk = jetbrains.jdk;
   }) // {
     jdk = callPackage ../development/compilers/jetbrains-jdk {  };
+    jcef = callPackage ../development/compilers/jetbrains-jdk/jcef.nix { };
   });
 
   jmusicbot = callPackage ../applications/audio/jmusicbot { };
