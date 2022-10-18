54e896edba273f406c39701b9e464863b1389262
Vincent Haupert <mail@vincent-haupert.de>
2022-10-18 17:53:38 +0200

    pulumiPackages.pulumi-language-python: init at 3.43.1

--- pkgs/tools/admin/pulumi-packages/default.nix
+++ pkgs/tools/admin/pulumi-packages/default.nix
@@ -4,5 +4,6 @@ let
   callPackage' = p: args: callPackage p (args // { inherit mkPulumiPackage; });
 in
 {
+  pulumi-language-python = callPackage ./pulumi-language-python.nix { };
   pulumi-random = callPackage' ./pulumi-random.nix { };
 }
