6c0324eea849e0f29895788168c1efffe71e4953
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-18 13:16:11 +0000

    imagemagick: 7.1.0-48 -> 7.1.0-51
    (cherry picked from commit 4d9e748454dbe09367def8a5ae02636d5059e063)

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -45,13 +45,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "imagemagick";
-  version = "7.1.0-48";
+  version = "7.1.0-51";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     rev = version;
-    hash = "sha256-ExGeZVqRajuYcck0JrIFGDEam4PfUCXF/jI2dudHh+E=";
+    hash = "sha256-u2QUtCQ4LzS60iVOOKWx/itmC9uMxPhNvsNANPHrvpE=";
   };
 
   outputs = [ "out" "dev" "doc" ]; # bin/ isn't really big
