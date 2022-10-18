e2dfb801b60ce06c898dc983063603e8779e3f4d
Aaron Jheng <wentworth@outlook.com>
2022-10-18 03:28:24 +0000

    pdfdiff: 0.92 -> 0.93

--- pkgs/applications/misc/pdfdiff/default.nix
+++ pkgs/applications/misc/pdfdiff/default.nix
@@ -1,34 +1,40 @@
-{ lib, python2Packages, fetchurl, xpdf }:
+{ lib, stdenv, python3Packages, fetchFromGitHub, makeWrapper, xpdf, testers, pdfdiff }:
 
-python2Packages.buildPythonApplication rec {
+stdenv.mkDerivation rec {
   pname = "pdfdiff";
-  version = "0.92";
+  version = "0.93";
 
-  src = fetchurl {
-    url = "https://www.cs.ox.ac.uk/people/cas.cremers/downloads/software/pdfdiff.py";
-    sha256 = "0zxwjjbklz87wkbhkmsvhc7xmv5php7m2a9vm6ydhmhlxsybf836";
+  src = fetchFromGitHub {
+    owner = "cascremers";
+    repo = "pdfdiff";
+    rev = version;
+    sha256 = "sha256-NPki/PFm0b71Ksak1mimR4w6J2a0jBCbQDTMQR4uZFI=";
   };
 
-  buildInputs = [  python2Packages.wrapPython ];
+  postPatch = ''
+    sed -i -r 's|progName = "pdfdiff.py"|progName = "pdfdiff"|' pdfdiff.py
+  '';
+
+  nativeBuildInputs = [ makeWrapper ];
 
   dontConfigure = true;
   dontBuild = true;
   doCheck = false;
 
-  unpackPhase = "cp $src pdfdiff.py";
-
-  postPatch = ''
-    sed -i -r 's|pdftotextProgram = "pdftotext"|pdftotextProgram = "${xpdf}/bin/pdftotext"|' pdfdiff.py
-    sed -i -r 's|progName = "pdfdiff.py"|progName = "pdfdiff"|' pdfdiff.py
-    '';
-
   installPhase = ''
-    mkdir -p $out/bin
-    cp pdfdiff.py $out/bin/pdfdiff
-    chmod +x $out/bin/pdfdiff
+    install -D -m 0755 pdfdiff.py $out/bin/pdfdiff
+  '';
 
-    substituteInPlace $out/bin/pdfdiff --replace "#!/usr/bin/python" "#!${python2Packages.python.interpreter}"
-    '';
+  postFixup = ''
+    substituteInPlace $out/bin/pdfdiff --replace "#!/usr/bin/env python3" "#!${python3Packages.python.interpreter}"
+
+    wrapProgram $out/bin/pdfdiff --prefix PATH : ${lib.makeBinPath [ xpdf ]}
+  '';
+
+  passthru.tests.version = testers.testVersion {
+    package = pdfdiff;
+    command = "pdfdiff --help";
+  };
 
   meta = with lib; {
     homepage = "http://www.cs.ox.ac.uk/people/cas.cremers/misc/pdfdiff.html";
