2c9c52fb83d211e1d7ff19066820934df57815ec
Stefan Frijters <sfrijters@gmail.com>
2022-10-18 23:35:13 +0200

    wine{Unstable,Staging}: 7.17 -> 7.18

--- pkgs/applications/emulators/wine/sources.nix
+++ pkgs/applications/emulators/wine/sources.nix
@@ -46,9 +46,9 @@ in rec {
 
   unstable = fetchurl rec {
     # NOTE: Don't forget to change the SHA256 for staging as well.
-    version = "7.17";
+    version = "7.18";
     url = "https://dl.winehq.org/wine/source/7.x/wine-${version}.tar.xz";
-    sha256 = "sha256-JDa4rFDWKPTKOsUwDBgmY9/PpIuhulVIp3KOtmH7T0E=";
+    sha256 = "sha256-Pt9ieT8lmbC7FxQtLliGcAVUl1ReGUP+gGc7kmfP6fY=";
     inherit (stable) gecko32 gecko64 patches;
 
     mono = fetchurl rec {
@@ -61,7 +61,7 @@ in rec {
   staging = fetchFromGitHub rec {
     # https://github.com/wine-staging/wine-staging/releases
     inherit (unstable) version;
-    sha256 = "sha256-eC5nYX6Cjutd30rrAn6SavWlQtF8swMHDzsESN4SUmo=";
+    sha256 = "sha256-WkDuBiUDESrxvPFHcS91KWF2m7M4C5yDx0fSPmVXsp8=";
     owner = "wine-staging";
     repo = "wine-staging";
     rev = "v${version}";
