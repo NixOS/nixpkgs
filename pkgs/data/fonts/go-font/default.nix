a589016c5fcb21869fa1f7ce5c76f66c350b7f0b
Berk D. Demir <bdd@mindcast.org>
2022-10-18 22:43:26 -0700

    go-font: 2017-03-30 -> 2.010

--- pkgs/data/fonts/go-font/default.nix
+++ pkgs/data/fonts/go-font/default.nix
@@ -1,11 +1,11 @@
 { lib, fetchgit }:
 
 let
-  version = "2017-03-30";
+  version = "2.010";
 in (fetchgit {
   name = "go-font-${version}";
   url = "https://go.googlesource.com/image";
-  rev = "f03a046406d4d7fbfd4ed29f554da8f6114049fc";
+  rev = "41969df76e82aeec85fa3821b1e24955ea993001";
 
   postFetch = ''
     mv $out/* .
@@ -15,11 +15,12 @@ in (fetchgit {
     mv $out/share/fonts/truetype/README $out/share/doc/go-font/LICENSE
   '';
 
-  sha256 = "1488426ya2nzmwjas947fx9h5wzxrp9wasn8nkjqf0y0mpd4f1xz";
+  sha256 = "dteUL/4ZUq3ybL6HaLYqu2Tslx3q8VvELIY3tVC+ODo=";
 }) // {
   meta = with lib; {
     homepage = "https://blog.golang.org/go-fonts";
     description = "The Go font family";
+    changelog = "https://go.googlesource.com/image/+log/refs/heads/master/font/gofont";
     license = licenses.bsd3;
     maintainers = with maintainers; [ sternenseemann ];
     platforms = lib.platforms.all;
