23511ff5b64143dfc1f60313f3eb6842fe3d9b37
Robert Scott <code@humanleg.org.uk>
2022-10-19 22:51:23 +0100

    mysql80: 8.0.30 -> 8.0.31

--- pkgs/servers/sql/mysql/8.0.x.nix
+++ pkgs/servers/sql/mysql/8.0.x.nix
@@ -6,11 +6,11 @@
 let
 self = stdenv.mkDerivation rec {
   pname = "mysql";
-  version = "8.0.30";
+  version = "8.0.31";
 
   src = fetchurl {
     url = "https://dev.mysql.com/get/Downloads/MySQL-${self.mysqlVersion}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-yYjVxrqaVmkqbNbpgTRltfyTaO1LRh35cFmi/BYMi4Q=";
+    sha256 = "sha256-Z7uMunWyjpXH95SFY/AfuEUo/LsaNduoOdTORP4Bm6o=";
   };
 
   nativeBuildInputs = [ bison cmake pkg-config ]
