0bbca8b919aebde45594cd5d3c1d0c35ae04af85
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-20 15:18:10 +0000

    netbird-ui: 0.10.0 -> 0.10.1

--- pkgs/tools/networking/netbird/default.nix
+++ pkgs/tools/networking/netbird/default.nix
@@ -14,13 +14,13 @@ let
 in
 buildGoModule rec {
   pname = "netbird";
-  version = "0.10.0";
+  version = "0.10.1";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-irOrYQIMy1GS/EsN4YtLP4/lHuQ+r7ZGzvNA4dkHi8s=";
+    sha256 = "sha256-ZP8MYvGotBVvuLZHFJgTz1SGAZLwuZwk9ohNtfcCSgs=";
   };
 
   vendorSha256 = "sha256-+jJUKp2EvtDxGsv+9B+s7FiIopvO8yH9jldeRZkrd20=";
