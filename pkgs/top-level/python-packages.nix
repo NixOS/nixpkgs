d2fa65f6e941ba263d7f0ba22ee24b1cae0d5fec
Ben Darwin <bcdarwin@gmail.com>
2022-10-20 13:06:50 -0400

    python3Packages.skrl: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10325,6 +10325,8 @@ in {
 
   skorch = callPackage ../development/python-modules/skorch { };
 
+  skrl = callPackage ../development/python-modules/skrl { };
+
   skybellpy = callPackage ../development/python-modules/skybellpy { };
 
   skyfield = callPackage ../development/python-modules/skyfield { };
