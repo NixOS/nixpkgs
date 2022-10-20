edc4feb452380ac8d0b6d4a7913e949d2e03294a
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-20 12:10:47 +0200

    python310Packages.httpagentparser: disable on older Python releases

--- pkgs/development/python-modules/httpagentparser/default.nix
+++ pkgs/development/python-modules/httpagentparser/default.nix
@@ -1,26 +1,31 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "httpagentparser";
   version = "1.9.5";
+  format = "setuptools";
 
-  # Github version does not have any release tags
+  disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-U879nWWZD2/lnAN4ytjqG53493DS6L2dh2LtrgM76Ao=";
+    hash = "sha256-U879nWWZD2/lnAN4ytjqG53493DS6L2dh2LtrgM76Ao=";
   };
 
   # PyPi version does not include test directory
   doCheck = false;
 
-  pythonImportsCheck = [ "httpagentparser" ];
+  pythonImportsCheck = [
+    "httpagentparser"
+  ];
 
   meta = with lib; {
+    description = "Module to extract OS, Browser, etc. information from http user agent string";
     homepage = "https://github.com/shon/httpagentparser";
-    description = "Extracts OS Browser etc information from http user agent string";
     license = licenses.mit;
     maintainers = with maintainers; [ gador ];
   };
