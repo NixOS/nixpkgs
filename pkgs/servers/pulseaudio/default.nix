8f652e28c460579660697c5d2c6ee5a4a7d5b57c
Giulio De Pasquale <depasquale+git@giugl.io>
2022-10-20 15:43:22 +0200

    Revert "pulseaudio: Avoid mass rebuild"
    This reverts commit 795e450547741d4b5fd057e3b6fae67150fddd85.

--- pkgs/servers/pulseaudio/default.nix
+++ pkgs/servers/pulseaudio/default.nix
@@ -9,7 +9,7 @@
 
 , x11Support ? false
 
-, useSystemd ? (stdenv.isLinux && (!stdenv.hostPlatform == stdenv.buildPlatform || libOnly))
+, useSystemd ? (stdenv.isLinux && !libOnly)
 
 , # Whether to support the JACK sound system as a backend.
   jackaudioSupport ? false
