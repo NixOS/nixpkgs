19643a1dd676b4275aabe46bf7ca419dadc60f0d
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2022-10-21 18:53:22 -0500

    mssql-odbc unixODBCDrivers.mssqlodbc-18

--- pkgs/development/libraries/unixODBCDrivers/default.nix
+++ pkgs/development/libraries/unixODBCDrivers/default.nix
@@ -180,6 +180,51 @@
     };
   };
 
+  msodbcsql18 = stdenv.mkDerivation rec {
+    pname = "msodbcsql18";
+    version = "${versionMajor}.${versionMinor}.${versionAdditional}-1";
+
+    versionMajor = "18";
+    versionMinor = "1";
+    versionAdditional = "1.1";
+
+    src = fetchurl {
+      url = "https://packages.microsoft.com/debian/10/prod/pool/main/m/msodbcsql${versionMajor}/msodbcsql${versionMajor}_${version}_amd64.deb";
+      sha256 = "otC4+e23RnN/OJ/nHh3wS5bPBfR6USYr1sFZrwKsGbg=";
+    };
+
+    nativeBuildInputs = [ dpkg patchelf ];
+
+    unpackPhase = "dpkg -x $src ./";
+    buildPhase = "";
+
+    installPhase = ''
+      mkdir -p $out
+      mkdir -p $out/lib
+      cp -r opt/microsoft/msodbcsql${versionMajor}/lib64 opt/microsoft/msodbcsql${versionMajor}/share $out/
+    '';
+
+    postFixup = ''
+      patchelf --set-rpath ${lib.makeLibraryPath [ unixODBC openssl libkrb5 libuuid stdenv.cc.cc ]} \
+        $out/lib/libmsodbcsql-${versionMajor}.${versionMinor}.so.${versionAdditional}
+    '';
+
+    passthru = {
+      fancyName = "ODBC Driver 18 for SQL Server";
+      driver = "lib/libmsodbcsql-${versionMajor}.${versionMinor}.so.${versionAdditional}";
+    };
+
+    meta = with lib; {
+      broken = stdenv.isDarwin;
+      description = "ODBC Driver 18 for SQL Server";
+      homepage = "https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-2018";
+      sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+      license = licenses.unfree;
+      platforms = platforms.linux;
+      maintainers = with maintainers; [ spencerjanssen ];
+    };
+  };
+
   redshift = stdenv.mkDerivation rec {
     pname = "redshift-odbc";
     version = "1.4.49.1000";
