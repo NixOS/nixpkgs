4d268b7b39df9304299000e1affa95b76594dff3
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-21 10:17:28 +0200

    python310Packages.rns: 0.3.14 -> 0.3.16

--- pkgs/development/python-modules/rns/default.nix
+++ pkgs/development/python-modules/rns/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "rns";
-  version = "0.3.14";
+  version = "0.3.16";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "markqvist";
     repo = "Reticulum";
     rev = "refs/tags/${version}";
-    hash = "sha256-W5zQiGzuBHzEebVZjnVUO7Htx2/hnpeBHWoBfeXuaOQ=";
+    hash = "sha256-+TXIxyLLIWK0lLUyh4irTYHXZLAv8zFYUGKAbA7D9qA=";
   };
 
   propagatedBuildInputs = [
