eb511dca4d343d8e017be0043c594a028b96e349
Jonas Heinrich <onny@project-insanity.org>
2022-10-22 12:01:05 +0200

    nixos/manticore: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1172,6 +1172,7 @@
   ./services/search/elasticsearch-curator.nix
   ./services/search/elasticsearch.nix
   ./services/search/hound.nix
+  ./services/search/manticore.nix
   ./services/search/meilisearch.nix
   ./services/search/opensearch.nix
   ./services/search/qdrant.nix
---
+++ nixos/modules/services/search/manticore.nix
@@ -0,0 +1,63 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.manticore;
+
+in {
+
+  options = {
+    services.manticore = {
+
+      enable = mkEnableOption (mdDoc "Manticore Search full-text search solution");
+
+      configFile = mkOption {
+        type = types.path;
+        default = "${pkgs.manticoresearch}/etc/manticoresearch/manticore.conf";
+        defaultText = literalExpression "''${pkgs.manticoresearch}/etc/manticoresearch/manticore.conf";
+        description = lib.mdDoc "Path to Manticore configuration file.";
+      };
+
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    systemd = {
+      packages = [ pkgs.manticoresearch ];
+      services.manticore.serviceConfig = {
+        ExecStart = "${pkgs.manticoresearch}/bin/searchd --config ${cfg.configFile}";
+        ExecStop = "${pkgs.manticoresearch}/bin/searchd --config ${cfg.configFile} --stopwait";
+        DynamicUser = true;
+        LogsDirectory = "manticore";
+        RuntimeDirectory = "manticore";
+        StateDirectory = "manticore";
+        ReadWritePaths = "";
+        CapabilityBoundingSet = "";
+        RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" ];
+        RestrictNamespaces = true;
+        PrivateDevices = true;
+        PrivateUsers = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [ "@system-service" "~@privileged" ];
+        RestrictRealtime = true;
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        UMask = "0066";
+        ProtectHostname = true;
+      };
+    };
+
+  };
+
+  meta.maintainers = with lib.maintainers; [ onny ];
+
+}
