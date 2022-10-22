ac2a5267b63580fcc047eee10e50b5429806ddb7
Erik Arvstedt <erik.arvstedt@gmail.com>
2022-10-22 19:36:02 +0200

    btcpayserver: 1.6.10 -> 1.6.12

--- pkgs/applications/blockchains/btcpayserver/default.nix
+++ pkgs/applications/blockchains/btcpayserver/default.nix
@@ -6,13 +6,13 @@
 
 buildDotnetModule rec {
   pname = "btcpayserver";
-  version = "1.6.10";
+  version = "1.6.12";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-fy8mIGVij6rjaGEWE6700gbiFnH741hIuvg26W1dBlw=";
+    sha256 = "sha256-izjKrzHbyRsUhlUAf0PELVGBt5JmzvBzgBytB9z80cI=";
   };
 
   projectFile = "BTCPayServer/BTCPayServer.csproj";
