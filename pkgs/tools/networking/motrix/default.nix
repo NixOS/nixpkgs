0b73dd854d34e822fff3fb9e006ffaf59db3302f
Mostly Void <dit7ya@users.noreply.github.com>
2022-10-22 05:43:33 +0530

    Apply suggestions from code review
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/networking/motrix/default.nix
+++ pkgs/tools/networking/motrix/default.nix
@@ -6,23 +6,21 @@
 let
   pname = "motrix";
   version = "1.6.11";
-  sha256 = "sha256-tE2Q7NM+cQOg+vyqyfRwg05EOMQWhhggTA6S+VT+SkM=";
-  name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/agalwood/Motrix/releases/download/v${version}/Motrix-${version}.AppImage";
-    inherit sha256;
+    sha256 = "sha256-tE2Q7NM+cQOg+vyqyfRwg05EOMQWhhggTA6S+VT+SkM=";
   };
 
   appimageContents = appimageTools.extractType2 {
-    inherit name src;
+    inherit pname src;
   };
 in
 appimageTools.wrapType2 rec {
-  inherit name src;
+  inherit pname version src;
 
   extraInstallCommands = ''
-    mv $out/bin/${name} $out/bin/${pname}
+    mv $out/bin/${pname}-${version} $out/bin/${pname}
     install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
     cp -r ${appimageContents}/usr/share/icons $out/share
   '';
