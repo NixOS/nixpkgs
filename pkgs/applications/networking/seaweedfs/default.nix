a863ccbf669525ae0386ed889c7d996ac1247cd2
Azat Bahawi <azat@bahawi.net>
2022-10-22 14:22:15 +0300

    seaweedfs: 3.27 -> 3.32

--- pkgs/applications/networking/seaweedfs/default.nix
+++ pkgs/applications/networking/seaweedfs/default.nix
@@ -7,26 +7,49 @@
 
 buildGoModule rec {
   pname = "seaweedfs";
-  version = "3.27";
+  version = "3.32";
 
   src = fetchFromGitHub {
     owner = "chrislusf";
     repo = "seaweedfs";
     rev = version;
-    sha256 = "sha256-kvKUgw6A4UHOuDmKuOv+XS/0XiOf2ENWxl2WmJ4cVTE=";
+    hash = "sha256-GMOLlkBfY3ShVojdRrmpMYgoea52kq4aXr/oZj5bJWo=";
   };
 
-  vendorSha256 = "sha256-sgLHRDdi9gkcSzeBaDCxtbvWSzjTshb2WbmMyRepUKA=";
+  vendorHash = "sha256-cEzPKx54rssyAytYenIcud3K0f7xuO8WzE8wdMqZipE=";
 
   subPackages = [ "weed" ];
 
-  passthru.tests.version =
-    testers.testVersion { package = seaweedfs; command = "weed version"; };
+  ldflags = [
+    "-w"
+    "-s"
+    "-X github.com/seaweedfs/seaweedfs/weed/util.COMMIT=N/A"
+  ];
+
+  tags = [
+    "elastic"
+    "gocdk"
+    "sqlite"
+    "ydb"
+    "tikv"
+  ];
+
+  preBuild = ''
+    export GODEBUG=http2client=0
+  '';
+
+  # There are no tests.
+  doCheck = false;
+
+  passthru.tests.version = testers.testVersion {
+    package = seaweedfs;
+    command = "weed version";
+  };
 
   meta = with lib; {
     description = "Simple and highly scalable distributed file system";
     homepage = "https://github.com/chrislusf/seaweedfs";
-    maintainers = with maintainers; [ cmacrae ];
+    maintainers = with maintainers; [ azahi cmacrae ];
     mainProgram = "weed";
     license = licenses.asl20;
   };
