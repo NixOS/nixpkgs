e5b085809ef9afaa80f1d8914410a7b1c4e223dd
Mario Rodas <marsam@users.noreply.github.com>
2022-10-22 04:20:00 +0000

    python310Packages.grpcio-tools: 1.49.1 -> 1.50.0

--- pkgs/development/python-modules/grpcio-tools/default.nix
+++ pkgs/development/python-modules/grpcio-tools/default.nix
@@ -2,16 +2,16 @@
 
 buildPythonPackage rec {
   pname = "grpcio-tools";
-  version = "1.49.1";
+  version = "1.50.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-hMxk5bRrrUPV170v13K2VuugNmlhGHqEfpCOLLc125E=";
+    sha256 = "88b75f2afd889c7c6939f58d76b58ab84de4723c7de882a1f8448af6632e256f";
   };
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace 'protobuf>=4.21.3,<5.0dev' 'protobuf'
+      --replace 'protobuf>=4.21.6,<5.0dev' 'protobuf'
   '';
 
   outputs = [ "out" "dev" ];
