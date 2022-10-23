5375f831940da9f32eeee19082d3c3176e1debf8
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-10-22 21:52:40 -0400

    dawncut: init at 1.54a

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35431,6 +35431,8 @@ with pkgs;
 
   dawn = callPackage ../applications/science/physics/dawn {};
 
+  dawncut = callPackage ../applications/science/physics/dawncut {};
+
   elmerfem = callPackage ../applications/science/physics/elmerfem {};
 
   mcfm = callPackage ../applications/science/physics/MCFM {
