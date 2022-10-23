c486b14861116a214c0d72eb7fba17e95dd3f9b4
José Romildo <malaquias@gmail.com>
2022-10-23 11:23:25 -0300

    ocamlPackages.ppx_deriving_cmdliner: compatibility with ppxlib-0.26

--- pkgs/development/ocaml-modules/ppx_deriving_cmdliner/default.nix
+++ pkgs/development/ocaml-modules/ppx_deriving_cmdliner/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildDunePackage
 , fetchFromGitHub
+, fetchpatch
 , alcotest
 , cmdliner
 , ppx_deriving
@@ -19,6 +20,15 @@ buildDunePackage rec {
     rev = "v${version}";
     sha256 = "sha256-/22KLQnxu3e2ZSca6ZLxTJDfv/rsmgCUkJnZC0RwRi8";
   };
+
+  patches = [
+    # Ppxlib.0.26.0 compatibility
+    # remove when a new version is released
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/hammerlab/ppx_deriving_cmdliner/pull/50.patch";
+      sha256 = "sha256-FfUfEAsyobwZ99+s5sFAaCE6Xgx7jLr/q79OxDbGcvQ=";
+    })
+  ];
 
   propagatedBuildInputs = [
     cmdliner
