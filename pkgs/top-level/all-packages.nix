6fdfa339456e2581484c8fac79f1b9037bb91aae
Madoura <madouura@gmail.com>
2022-10-23 18:22:35 -0500

    rocsparse: init at 2.3.2-5.3.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14859,6 +14859,8 @@ with pkgs;
 
   rocminfo = callPackage ../development/tools/rocminfo { };
 
+  rocsparse = callPackage ../development/libraries/rocsparse { };
+
   rtags = callPackage ../development/tools/rtags {
     inherit (darwin) apple_sdk;
   };
