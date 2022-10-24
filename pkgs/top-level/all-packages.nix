81d6f1c7dc78ea07a6516efec4dafbc32fee9738
thyol <thyol@pm.me>
2022-10-24 16:51:52 +0200

    sratoolkit: init at 2.11.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16218,6 +16218,8 @@ with pkgs;
 
   srandrd = callPackage ../tools/X11/srandrd { };
 
+  sratoolkit = callPackage ../applications/science/biology/sratoolkit { };
+
   srecord = callPackage ../development/tools/misc/srecord { };
 
   srelay = callPackage ../tools/networking/srelay { };
