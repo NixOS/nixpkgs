24a9b1154e78fc2854c6959584a54ddcd08cd520
Florian Brandes <florian.brandes@posteo.de>
2022-10-24 16:52:46 +0200

    python3Packages.reorder-python-imports: 3.8.1 -> 3.8.5
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/development/python-modules/reorder-python-imports/default.nix
+++ pkgs/development/python-modules/reorder-python-imports/default.nix
@@ -2,21 +2,19 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pytestCheckHook
-, pythonOlder
 , aspy-refactor-imports
 , classify-imports
 }:
 
 buildPythonPackage rec {
   pname = "reorder-python-imports";
-  version = "3.8.1";
-  disabled = pythonOlder "3.7";
+  version = "3.8.5";
 
   src = fetchFromGitHub {
     owner = "asottile";
     repo = "reorder_python_imports";
     rev = "v${version}";
-    hash = "sha256-CLC9dfNSYqEBZB2fP34jpA/4cxm0HZzjo/e7Yn8XPFc=";
+    hash = "sha256-p/yhvPd2U5QSGBJFUjdy1Nmt83p8JGm2f0qKiXSjN30=";
   };
 
   propagatedBuildInputs = [
