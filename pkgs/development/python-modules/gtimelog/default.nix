09a27f5647e44ad18d94dd82c2e93af2511e7d43
Krzysztof Nazarewski <3494992+nazarewk@users.noreply.github.com>
2022-10-24 15:15:09 +0200

    gtimelog: fix and update to latest master
    it was not working before, exiting with:
    ```
    Typelib files for Gtk-3.0 are not available.
    
    If you're on Ubuntu or another Debian-like distribution, please install
    them with
    
        sudo apt install gir1.2-gtk-3.0
    ```

--- pkgs/development/python-modules/gtimelog/default.nix
+++ pkgs/development/python-modules/gtimelog/default.nix
@@ -1,27 +1,43 @@
-{ lib, fetchFromGitHub, makeWrapper
-, glibcLocales, gobject-introspection, gtk3, libsoup, libsecret
-, buildPythonPackage, python
-, pygobject3, freezegun, mock
+{ lib
+, fetchFromGitHub
+, makeWrapper
+, python
+, buildPythonPackage
+, atk
+, gdk-pixbuf
+, glibcLocales
+, glib-networking
+, gobject-introspection
+, gtk3
+, harfbuzz
+, libsecret
+, libsoup
+, pango
 }:
-
 buildPythonPackage rec {
   pname = "gtimelog";
-  version = "unstable-2020-05-16";
+  version = "unstable-2022-10-28";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
-    rev = "80682ddbf9e0d68b8c67257289784f3b49b543d8";
-    sha256 = "0qv2kv7vc3qqlzxsisgg31cmrkkqgnmxspbj10c5fhdmwzzwi0i9";
+    rev = "73d2228cea7e500858075a139fd321b1ccbae0d1";
+    sha256 = "sha256-g6gU95uvBhBbB4b1McE2LuVPDCjdmmhBX3lVyCY96zA=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [
-    glibcLocales gobject-introspection gtk3 libsoup libsecret
+    glibcLocales
+    gobject-introspection
+    gtk3
+    libsoup
+    libsecret
   ];
 
-  propagatedBuildInputs = [
-    pygobject3 freezegun mock
+  propagatedBuildInputs = with python.pkgs; [
+    pygobject3
+    freezegun
+    mock
   ];
 
   checkPhase = ''
@@ -30,6 +46,26 @@ buildPythonPackage rec {
   '';
 
   pythonImportsCheck = [ "gtimelog" ];
+
+  # found this fix at Matrix
+  # see https://matrix.to/#/!KqkRjyTEzAGRiZFBYT:nixos.org/$reRwjXiP3c4q9-z3wnnnBF9Q5KYGi9w3DxeQLhEJhnI?via=nixos.org&via=matrix.org&via=tchncs.de
+  # resolves runtime error: Typelib files for Gtk-3.0 are not available.
+  makeWrapperArgs = [
+    "--set GI_TYPELIB_PATH ${lib.makeSearchPathOutput "lib"
+      "lib/girepository-1.0" ([
+        gtk3
+        libsoup
+        libsecret
+        pango
+        harfbuzz
+        gdk-pixbuf
+        atk
+      ])}"
+    "--set GIO_MODULE_DIR ${lib.makeSearchPathOutput "out"
+      "lib/gio/modules" ([
+        glib-networking
+      ])}"
+  ];
 
   preFixup = ''
     wrapProgram $out/bin/gtimelog \
@@ -42,18 +78,16 @@ buildPythonPackage rec {
     longDescription = ''
       GTimeLog is a small time tracking application for GNOME.
       It's main goal is to be as unintrusive as possible.
-
       To run gtimelog successfully on a system that does not have full GNOME 3
       installed, the following NixOS options should be set:
       - programs.dconf.enable = true;
       - services.gnome.gnome-keyring.enable = true;
-
       In addition, the following packages should be added to the environment:
       - gnome.adwaita-icon-theme
       - gnome.dconf
     '';
     homepage = "https://gtimelog.org/";
     license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ oxzi ];
+    maintainers = with maintainers; [ oxzi nazarewk ];
   };
 }
