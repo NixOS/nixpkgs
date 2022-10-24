a658543aae4043ab58101af4368d130a0989035e
Madoura <madouura@gmail.com>
2022-10-24 00:21:06 -0500

    hipsparse: init at 2.3.1-5.3.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14833,6 +14833,8 @@ with pkgs;
     inherit (llvmPackages_rocm) clang llvm;
   };
 
+  hipsparse = callPackage ../development/libraries/hipsparse { };
+
   rocm-cmake = callPackage ../development/tools/build-managers/rocm-cmake { };
 
   rocm-comgr = callPackage ../development/libraries/rocm-comgr {
