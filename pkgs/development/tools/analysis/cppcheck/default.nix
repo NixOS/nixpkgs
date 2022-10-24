cb7eb7ee381e85db97ee31081b3ab62c94a7a664
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-24 09:34:05 +0000

    cppcheck: 2.9 -> 2.9.1

--- pkgs/development/tools/analysis/cppcheck/default.nix
+++ pkgs/development/tools/analysis/cppcheck/default.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cppcheck";
-  version = "2.9";
+  version = "2.9.1";
 
   src = fetchFromGitHub {
     owner = "danmar";
     repo = "cppcheck";
     rev = version;
-    sha256 = "sha256-UkmtW/3CLU9tFNjVLhQPhYkYflFLOBc/7Qc8lSBOo3I=";
+    sha256 = "sha256-8vwuNK7qQg+pkUnWpEe8272BuHJUaidm8MoGsXVt1X8=";
   };
 
   buildInputs = [ pcre
