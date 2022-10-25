41d6d47146622554fc13f0b423cfbcccf90aa4b4
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-25 21:29:45 +0000

    python310Packages.regenmaschine: 2022.09.2 -> 2022.10.0

--- pkgs/development/python-modules/regenmaschine/default.nix
+++ pkgs/development/python-modules/regenmaschine/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "regenmaschine";
-  version = "2022.09.2";
+  version = "2022.10.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "bachya";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-lpmytE2WkZuLcONm/kvtmXnHaimzaKOre9S5hIqDNVE=";
+    sha256 = "sha256-HJgAQnBhoGo7PDwDl0XNXpSVRM8L9SFHM90kycCK0rE=";
   };
 
   nativeBuildInputs = [
