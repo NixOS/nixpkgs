927e9d1976b13d6d19e1c69ae30343ff64b92b95
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-25 03:45:31 +0000

    pyinfra: 2.4 -> 2.5.1

--- pkgs/development/python-modules/pyinfra/default.nix
+++ pkgs/development/python-modules/pyinfra/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pyinfra";
-  version = "2.4";
+  version = "2.5.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "Fizzadar";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-nWH4o6NqyqkZg/HxF6NesnA6ijWo+B94BeohSsP60TY=";
+    hash = "sha256-cN7nyjdnU/ls3HNhC2m1sunRrmZFT5pFZr+kEBcwHkU=";
   };
 
   propagatedBuildInputs = [
