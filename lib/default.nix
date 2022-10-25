d7d57f9f15131bbd8657c7dd2b3c93d1d6d89bf7
squalus <squalus@squalus.net>
2022-10-25 00:20:21 -0700

    lib/strings: add toIntBase function
    Add function to parse integer strings with different bases.

--- lib/default.nix
+++ lib/default.nix
@@ -103,7 +103,7 @@ let
       getName getVersion
       nameFromURL enableFeature enableFeatureAs withFeature
       withFeatureAs fixedWidthString fixedWidthNumber isStorePath
-      toInt toIntBase10 readPathsFromFile fileContents;
+      toInt toIntBase toIntBase10 readPathsFromFile fileContents;
     inherit (self.stringsWithDeps) textClosureList textClosureMap
       noDepEntry fullDepEntry packEntry stringAfter;
     inherit (self.customisation) overrideDerivation makeOverridable
