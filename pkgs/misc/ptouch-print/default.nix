85d4cb16d4e4339b0dc878dd93cba21f54e18903
Justinas Stankevicius <justinas@justinas.org>
2022-10-27 00:51:18 +0300

    ptouch-print: 1.4.3 -> 1.5

--- pkgs/misc/ptouch-print/default.nix
+++ pkgs/misc/ptouch-print/default.nix
@@ -1,33 +1,47 @@
-{ lib, stdenv
+{ lib
+, stdenv
 , fetchgit
-, autoreconfHook
+, cmake
+, gettext
 , gd
 , libusb1
+, pkg-config
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "ptouch-print";
-  version = "1.4.3";
+  version = "1.5";
 
   src = fetchgit {
-    url = "https://mockmoon-cybernetics.ch/cgi/cgit/linux/ptouch-print.git";
-    rev = "v${version}";
-    sha256 = "0i57asg2hj1nfwy5lcb0vhrpvb9dqfhf81vh4i929h1kiqhlw2hx";
+    url = "https://git.familie-radermacher.ch/linux/ptouch-print.git";
+    rev = "e3c0073466ed99dbde9bbbcceea1c54f64967fc8";
+    sha256 = "sha256-Tn7sODj0NqLUAIHYwcR8BK63/EHMwwR5M9EkFHur0gY=";
   };
 
   nativeBuildInputs = [
-    autoreconfHook
+    cmake
+    pkg-config
   ];
 
   buildInputs = [
     gd
+    gettext
     libusb1
   ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mv ptouch-print $out/bin
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "Command line tool to print labels on Brother P-Touch printers on Linux";
     license = licenses.gpl3Plus;
-    homepage = "https://mockmoon-cybernetics.ch/computer/p-touch2430pc/";
+    homepage = "https://dominic.familie-radermacher.ch/projekte/ptouch-print/";
     maintainers = with maintainers; [ shamilton ];
     platforms = platforms.linux;
   };
