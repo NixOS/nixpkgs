b7f7d75c74bb90963c36d87784651a2ddb2a2699
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-26 09:07:20 +0200

    python310Packages.pytest-qt: disable on older Python releases
    - add pythonImportsCheck

--- pkgs/development/python-modules/pytest-qt/default.nix
+++ pkgs/development/python-modules/pytest-qt/default.nix
@@ -4,15 +4,19 @@
 , setuptools-scm
 , pytest
 , pyqt5
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pytest-qt";
   version = "4.2.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-AKF7WG3VMLbXqTmZI6QEicpKmjCXGQERdfVdxrXcj0E=";
+    hash = "sha256-AKF7WG3VMLbXqTmZI6QEicpKmjCXGQERdfVdxrXcj0E=";
   };
 
   nativeBuildInputs = [
@@ -27,7 +31,11 @@ buildPythonPackage rec {
     pyqt5
   ];
 
-  # tests require X server
+  pythonImportsCheck = [
+    "pytestqt"
+  ];
+
+  # Tests require X server
   doCheck = false;
 
   meta = with lib; {
