d6401ccea77389f45fbc2982f9d3a6863c15b14d
Bernardo Meurer <bernardo@meurer.org>
2022-10-26 18:21:26 +0100

    linux: 5.4.219 -> 5.4.220

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.219";
+  version = "5.4.220";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0qd2a0cx6bq11qq2513xmm5jxzfrq6axvsc0pjbvdpv9fa9av4sj";
+    sha256 = "0mh2p0hxb971mv3jjld5c85cbs85b5nmcq5j9akvq8y2jbai6b4d";
   };
 } // (args.argsOverride or {}))
