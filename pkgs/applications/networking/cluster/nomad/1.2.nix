ee5d39648ed5ea58997dcb70436f355a047dc906
techknowlogick <techknowlogick@gitea.io>
2022-10-27 11:21:26 -0400

    nomad_1_2: 1.2.13 -> 1.2.14

--- pkgs/applications/networking/cluster/nomad/1.2.nix
+++ pkgs/applications/networking/cluster/nomad/1.2.nix
@@ -4,7 +4,7 @@
 
 callPackage ./generic.nix {
   inherit buildGoModule;
-  version = "1.2.13";
-  sha256 = "sha256-yDcvN6cKG1BlBq1ygYB58bS1YRHWqJgLXRlqI7lrW1A=";
-  vendorSha256 = "sha256-dPErDlJ4oNpER3Ij4yrN77V8sZvDUuXY7dM39u9xT4I=";
+  version = "1.2.14";
+  sha256 = "sha256-BEbRXakMbgE44z1NOGThUuT1FukFUc1cnPkV5PXAY+4=";
+  vendorSha256 = "sha256-bOJ/qlvY3NHlR9C08vwfVn4Z/bSH15EPs3vvq78JoKs=";
 }
