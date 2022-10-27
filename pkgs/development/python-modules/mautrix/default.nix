94345383b899a66eae979e4938398f11c57b61b2
Frederik Rietdijk <fridh@fridh.nl>
2022-10-27 10:01:56 +0200

    python3Packages.mautrix: 0.18.4 -> 0.18.6

--- pkgs/development/python-modules/mautrix/default.nix
+++ pkgs/development/python-modules/mautrix/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "mautrix";
-  version = "0.18.4";
+  version = "0.18.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-ymSnGgcy1sW7HlWsSbSRty+60MtChzpF56eH7wrdfh8=";
+    sha256 = "sha256-IpFtdDCGSM6A6l0/IYkfTgV+fLlco340Tos4msx9XEs=";
   };
 
   propagatedBuildInputs = [
