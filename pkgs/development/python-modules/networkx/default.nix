3ac7163fd04b7cb1599dfb0328367376d0d4b9d0
Frederik Rietdijk <fridh@fridh.nl>
2022-10-27 10:01:57 +0200

    python3Packages.networkx: 2.8.6 -> 2.8.7

--- pkgs/development/python-modules/networkx/default.nix
+++ pkgs/development/python-modules/networkx/default.nix
@@ -11,12 +11,12 @@
 buildPythonPackage rec {
   pname = "networkx";
   # upgrade may break sage, please test the sage build or ping @timokau on upgrade
-  version = "2.8.6";
+  version = "2.8.7";
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-vSt3MDAIYMvS2v6OWvif9cmmXDl1s1J5nYemI4tDAaY=";
+    sha256 = "sha256-gVOD/VLs4KcCS1/YQIzBOjieo1DNkSF4uC7ti5b4LNM=";
   };
 
   propagatedBuildInputs = [ decorator setuptools ];
