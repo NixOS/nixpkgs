961ee1a7412b8105527ef0e078cd1d24967adcc5
Jörg Thalheim <joerg@thalheim.io>
2022-10-27 21:16:23 +0200

    snappymail: 2.19.2 -> 2.19.3

--- pkgs/servers/snappymail/default.nix
+++ pkgs/servers/snappymail/default.nix
@@ -2,11 +2,11 @@
 , dataPath ? "/var/lib/snappymail" }:
 stdenv.mkDerivation rec {
   pname = "snappymail";
-  version = "2.19.2";
+  version = "2.19.3";
 
   src = fetchurl {
     url = "https://github.com/the-djmaze/snappymail/releases/download/v${version}/snappymail-${version}.tar.gz";
-    sha256 = "sha256-nIv70cS+7euRvtarI6DeXNst8a0rLVClYQ7CJubQUBU=";
+    sha256 = "sha256-m0qVus26JprMm1v4D3Uw6RtEWKcoMl0hHpJjvfqudtk=";
   };
 
   sourceRoot = "snappymail";
