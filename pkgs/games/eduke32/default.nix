c3e1a9697d0e65808027a4910b3797fe1c0bd1a5
Sergei Trofimovich <slyich@gmail.com>
2022-10-27 07:58:23 +0100

    eduke32: fix build by removing explicit header paths
    In https://github.com/NixOS/nixpkgs/pull/197798#issuecomment-1291196023
    we noticed that `eduke32` fails to build as:
    
        source/build/include/sdl_inc.h:17:12: fatal error: SDL2/SDL.h: No such file or directory
           17 | #  include <SDL2/SDL.h>
              |            ^~~~~~~~~~~~
    
    It statred after https://github.com/NixOS/nixpkgs/pull/191724 where
    `NIX_CFLAGS_COMPILE` entries were cleaned up slightly.
    
    This change works build failure around. But I think it's a good change:
    there should be no reason to set SDL2 offset via forced include paths.

--- pkgs/games/eduke32/default.nix
+++ pkgs/games/eduke32/default.nix
@@ -58,8 +58,6 @@ in stdenv.mkDerivation rec {
     done
   '';
 
-  NIX_CFLAGS_COMPILE = "-I${lib.getDev SDL2}/include/SDL2 -I${lib.getDev SDL2_mixer}/include/SDL2";
-
   makeFlags = [
     "SDLCONFIG=${SDL2}/bin/sdl2-config"
   ] ++ lib.optionals stdenv.isDarwin [
