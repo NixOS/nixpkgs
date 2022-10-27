0a234d45151897659e073cbdd505cbc535f5d418
Adam Stephens <adam@valkor.net>
2022-10-27 17:07:12 -0400

    squeezelite: 1.9.9.1403 -> 1.9.9.1411

--- pkgs/applications/audio/squeezelite/default.nix
+++ pkgs/applications/audio/squeezelite/default.nix
@@ -34,13 +34,13 @@ stdenv.mkDerivation {
   pname = binName;
   # versions are specified in `squeezelite.h`
   # see https://github.com/ralph-irving/squeezelite/issues/29
-  version = "1.9.9.1403";
+  version = "1.9.9.1411";
 
   src = fetchFromGitHub {
     owner = "ralph-irving";
     repo = "squeezelite";
-    rev = "bc72c0de3fff771540a2a45aaafafed539387b3c";
-    hash = "sha256-205i61mbeQG2MzSE9NtPHSuNeyMbjZzbZVCFFzjqKqQ=";
+    rev = "ca44fc6e258bb413d6281d927063b25940f42e5c";
+    hash = "sha256-aZ+2nyy6tK3VwgTCWGoNaU4//kkHUzd6DZSfTEIgbvY=";
   };
 
   buildInputs = [ flac libmad libvorbis mpg123 ]
