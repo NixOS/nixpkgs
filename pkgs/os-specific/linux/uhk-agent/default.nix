1af10e8351b2c8de4e7f3f45aa6b3d9940460252
joachimschmidt557 <joachim.schmidt557@outlook.com>
2022-10-27 09:28:16 +0200

    uhk-agent: 1.5.17 -> 2.0.2

--- pkgs/os-specific/linux/uhk-agent/default.nix
+++ pkgs/os-specific/linux/uhk-agent/default.nix
@@ -1,11 +1,11 @@
 { appimageTools, lib, fetchurl, polkit, udev }:
 let
   pname = "uhk-agent";
-  version = "1.5.17";
+  version = "2.0.2";
   src = fetchurl {
     url = "https://github.com/UltimateHackingKeyboard/agent/releases/download/v${version}/UHK.Agent-${version}-linux-x86_64.AppImage";
     name = "${pname}-${version}.AppImage";
-    sha256 = "sha256-auOoTTRmkXVDDvcmRFzQIStNlbai8bTBLb/KUjk6EAc=";
+    sha256 = "sha256-MlxC7sAP1ehFKHFCRg7vhjMY0VODkEqKyJxrDtTZCHQ=";
   };
 
   appimageContents = appimageTools.extract {
