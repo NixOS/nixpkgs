6a65150dc6d63c01cad00dadddfcfe3cec781d53
Frederik Rietdijk <fridh@fridh.nl>
2022-10-27 10:01:51 +0200

    python3Packages.aiohttp: 3.8.1 -> 3.8.3

--- pkgs/development/python-modules/aiohttp/default.nix
+++ pkgs/development/python-modules/aiohttp/default.nix
@@ -29,12 +29,12 @@
 
 buildPythonPackage rec {
   pname = "aiohttp";
-  version = "3.8.1";
+  version = "3.8.3";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "fc5471e1a54de15ef71c1bc6ebe80d4dc681ea600e68bfd1cbce40427f0b7578";
+    sha256 = "sha256-OCj7QbcgMXa4L+XWmeDYRUNfI3R1CkS0gOprkw9r4mk=";
   };
 
   postPatch = ''
