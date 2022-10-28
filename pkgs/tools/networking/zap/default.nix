e497acaac7ddcb8db1f7d3feb3d4dabea680cc3a
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-28 01:15:29 +0000

    zap: 2.11.1 -> 2.12.0

--- pkgs/tools/networking/zap/default.nix
+++ pkgs/tools/networking/zap/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "zap";
-  version = "2.11.1";
+  version = "2.12.0";
   src = fetchurl {
     url = "https://github.com/zaproxy/zaproxy/releases/download/v${version}/ZAP_${version}_Linux.tar.gz";
-    sha256 = "0b1qqrjm4m76djy0az9hnz3rqpz1qkql4faqmi7gkx33b1p6d0sz";
+    sha256 = "sha256-nESTyZHLk0cGOGTSQ2o3lc87aXYGJeez20Ac00LT/FU=";
   };
 
   buildInputs = [ jre ];
