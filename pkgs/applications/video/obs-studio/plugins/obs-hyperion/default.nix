a34fb665ea4d61bee2596c7e34490b0f4d011ce4
Artturin <Artturin@artturin.com>
2022-10-28 06:41:44 +0300

    obs-studio-plugins.obs-hyperion: fix src and switch to cmake

--- pkgs/applications/video/obs-studio/plugins/obs-hyperion/default.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-hyperion/default.nix
@@ -1,22 +1,37 @@
-{ stdenv, lib, fetchFromGitLab, meson, ninja, pkg-config, obs-studio, libGL
-, qtbase }:
+{ stdenv, lib, fetchFromGitHub, cmake, pkg-config, obs-studio, libGL
+, qtbase, flatbuffers }:
 
 stdenv.mkDerivation rec {
   pname = "obs-hyperion";
   version = "1.0.1";
 
-  src = fetchFromGitLab {
+  src = fetchFromGitHub {
     owner = "hyperion-project";
     repo = "hyperion-obs-plugin";
-    rev = "v${version}";
-    sha256 = "sha256-Si+TGYWpNPtUUFT+M571lCYslPyeYX92MdYV2EGgcyQ=";
+    rev = version;
+    sha256 = "sha256-pfWfJWuIoa+74u5J76/GE+OuHkksbwOAPfsR9OGX3L4=";
   };
 
-  nativeBuildInputs = [ meson pkg-config ninja ];
+  nativeBuildInputs = [ cmake pkg-config ];
   buildInputs = [ obs-studio libGL qtbase ];
+
+  dontWrapQtApps = true;
+
+  cmakeFlags = [
+    "-DOBS_SOURCE=${obs-studio.src}"
+    "-DGLOBAL_INSTALLATION=ON"
+  ];
+
+  preConfigure = ''
+    # https://github.com/hyperion-project/hyperion-obs-plugin/issues/7
+    rm -rf external/flatbuffers
+    cp -r ${flatbuffers.src} external/flatbuffers
+    chmod -R a+w external
+  '';
 
   meta = with lib; {
     description = "OBS Studio plugin to connect to a Hyperion.ng server";
+    homepage = "https://github.com/hyperion-project/hyperion-obs-plugin";
     license = licenses.mit;
     maintainers = with maintainers; [ algram ];
     platforms = [ "x86_64-linux" ];
