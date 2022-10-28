8717a0abcb461d41625c53bd8deb3413944a526c
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-28 09:44:10 +0200

    python310Packages.pyswitchbot: 0.20.0 -> 0.20.2

--- pkgs/development/python-modules/pyswitchbot/default.nix
+++ pkgs/development/python-modules/pyswitchbot/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "pyswitchbot";
-  version = "0.20.0";
+  version = "0.20.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "Danielhiversen";
     repo = "pySwitchbot";
     rev = "refs/tags/${version}";
-    hash = "sha256-vPeXNxYuuzi74459JnMpb9v/wv9W4puG6agy00/Fil4=";
+    hash = "sha256-GNjyDkqhOP9sq2qoAZCdSrUNte3FynVxhy9RSUn9j/c=";
   };
 
   propagatedBuildInputs = [
