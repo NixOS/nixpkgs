a3320262186201db10df1bd42ebb9a99f7fb44d3
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-30 01:42:03 +0200

    python310Packages.pydeconz: 104 -> 105

--- pkgs/development/python-modules/pydeconz/default.nix
+++ pkgs/development/python-modules/pydeconz/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "pydeconz";
-  version = "104";
+  version = "105";
   format = "setuptools";
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Kane610";
     repo = "deconz";
     rev = "refs/tags/v${version}";
-    hash = "sha256-bG2dhWa+KnSsigmOHE4wShaS56M9z6vqrFNek3PLbic=";
+    hash = "sha256-8GQJ4PCl8te3sovC5FwWoaQnXIRR27b7gPwfmyPRVBE=";
   };
 
   propagatedBuildInputs = [
