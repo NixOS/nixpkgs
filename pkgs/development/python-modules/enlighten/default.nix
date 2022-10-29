32034da16cc2050c5c8703c5c4a13d62507bd79f
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-10-30 00:56:43 +0200

    python3Packages.enlighten: fix build

--- pkgs/development/python-modules/enlighten/default.nix
+++ pkgs/development/python-modules/enlighten/default.nix
@@ -36,6 +36,9 @@ buildPythonPackage rec {
   disabledTests = [
     # AssertionError: <_io.TextIOWrapper name='<stdout>' mode='w' encoding='utf-8'> is not...
     "test_init"
+    # AssertionError: Invalid format specifier (deprecated since prefixed 0.4.0)
+    "test_floats_prefixed"
+    "test_subcounter_prefixed"
   ] ++ lib.optionals stdenv.isDarwin [
     # https://github.com/Rockhopper-Technologies/enlighten/issues/44
     "test_autorefresh"
