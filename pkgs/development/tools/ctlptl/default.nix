78f009c3bdb972c256e1e368c66f9786a416f130
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-29 14:07:23 +0000

    ctlptl: 0.8.10 -> 0.8.11

--- pkgs/development/tools/ctlptl/default.nix
+++ pkgs/development/tools/ctlptl/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "ctlptl";
-  version = "0.8.10";
+  version = "0.8.11";
 
   src = fetchFromGitHub {
     owner = "tilt-dev";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-OeK49cQ22yjTfmN7E/VIRD/u8f5x7TmMrEupi9CKg+8=";
+    sha256 = "sha256-69xFfDNitLu8KUQP9ij0ndV6JCzp9wrLsHKwEjuhYdY=";
   };
 
   vendorSha256 = "sha256-M9B/rfMBjYJb9szmYPVZqURlcv62qHOLJ3ka0v++z0s=";
