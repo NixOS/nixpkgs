6406679d4c992a6b0e2cabccb965f2a082a368bc
Aaron Jheng <wentworth@outlook.com>
2022-10-29 06:18:34 +0000

    yeetgif: 1.23.6 -> unstable-2021-11-20

--- pkgs/applications/graphics/yeetgif/default.nix
+++ pkgs/applications/graphics/yeetgif/default.nix
@@ -1,17 +1,19 @@
-{ buildGoPackage, fetchFromGitHub, lib }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "yeetgif";
-  version = "1.23.6";
-
-  goPackagePath = "github.com/sgreben/yeetgif";
+  version = "unstable-2021-11-20";
 
   src = fetchFromGitHub {
     owner = "sgreben";
     repo = pname;
-    rev = version;
-    sha256 = "05z1ylsra60bb4cvr383g9im94zsph1dgicqbv5p73qgs634ckk7";
+    rev = "5d2067b9832898c2b1ac51bf6a5f107619038270";
+    sha256 = "sha256-Z22Cs5xFYglr6yqam1DXdQVE50DBjgb+v9MHJOI7uFQ=";
   };
+
+  vendorSha256 = null;
+
+  ldflags = [ "-s" "-w" ];
 
   meta = with lib; {
     description = "gif effects CLI. single binary, no dependencies. linux, osx, windows. #1 workplace productivity booster. #yeetgif #eggplant #golang";
