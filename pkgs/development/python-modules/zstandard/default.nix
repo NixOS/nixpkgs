b844200a4bccd39deb32a6079fc7e2362e52ee27
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-10-30 22:08:19 +0100

    python310Packages.zstandard: 0.18.0 -> 0.19.0

--- pkgs/development/python-modules/zstandard/default.nix
+++ pkgs/development/python-modules/zstandard/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "zstandard";
-  version = "0.18.0";
+  version = "0.19.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-CsA1eg2YW0/zGoVHRAQNe1dUOF0fmPcUXDDgLGhly28=";
+    sha256 = "sha256-MdEvzZQt2Nv1LKX2sbvih/ROXVUaCBqYP/PqIIKGeGM=";
   };
 
   propagatedNativeBuildInputs = [
