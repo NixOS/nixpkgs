54cb67ac972aebf45cf8bd6330d72fe6ba30fa38
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-30 09:17:08 +0100

    python310Packages.motor: update disabled

--- pkgs/development/python-modules/motor/default.nix
+++ pkgs/development/python-modules/motor/default.nix
@@ -9,23 +9,31 @@
 buildPythonPackage rec {
   pname = "motor";
   version = "3.1.1";
-  disabled = pythonOlder "3.6";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "mongodb";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-PD41ZyUFY4fYaO4Fo7kiTWcn1a6LOd81K3PWAAagmr4=";
+    hash = "sha256-PD41ZyUFY4fYaO4Fo7kiTWcn1a6LOd81K3PWAAagmr4=";
   };
 
-  propagatedBuildInputs = [ pymongo ];
+  propagatedBuildInputs = [
+    pymongo
+  ];
 
-  checkInputs = [ mockupdb ];
+  checkInputs = [
+    mockupdb
+  ];
 
   # network connections
   doCheck = false;
 
-  pythonImportsCheck = [ "motor" ];
+  pythonImportsCheck = [
+    "motor"
+  ];
 
   meta = with lib; {
     description = "Non-blocking MongoDB driver for Tornado or asyncio";
