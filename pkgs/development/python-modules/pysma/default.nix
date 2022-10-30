a03e49715a8d3f36122f879e9dd21874809e63e1
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-30 10:55:23 +0000

    python310Packages.pysma: 0.6.12 -> 0.7.2

--- pkgs/development/python-modules/pysma/default.nix
+++ pkgs/development/python-modules/pysma/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pysma";
-  version = "0.6.12";
+  version = "0.7.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-uxMxqx5qbahMvTm3akiOTODhKLNVhHzBAUsOcZo/35I=";
+    sha256 = "sha256-hIrdT0b9XKw1UoPZtQQ7IaW+HV8wuA9Rwoo8XYdGyw8=";
   };
 
   propagatedBuildInputs = [
