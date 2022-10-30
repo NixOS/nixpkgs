a4b30281b8fa156934fc4ee5a6f1fd8611b4dd3b
Jonas Nick <jonasd.nick@gmail.com>
2022-10-30 22:00:43 +0000

    wasabiwallet: 2.0.1.3 -> 2.0.2

--- pkgs/applications/blockchains/wasabiwallet/default.nix
+++ pkgs/applications/blockchains/wasabiwallet/default.nix
@@ -8,6 +8,8 @@
 , openssl
 , xorg
 , zlib
+, libICE
+, libSM
 }:
 
 let
@@ -21,15 +23,17 @@ let
     stdenv.cc.cc.lib
     xorg.libX11
     zlib
+    libICE
+    libSM
   ];
 in
 stdenv.mkDerivation rec {
   pname = "wasabiwallet";
-  version = "2.0.1.3";
+  version = "2.0.2";
 
   src = fetchurl {
     url = "https://github.com/zkSNACKs/WalletWasabi/releases/download/v${version}/Wasabi-${version}.tar.gz";
-    sha256 = "sha256-cATqg/n4/BDQtuCVjHAx3EfMLmlX5EjeQ01gavy/L8o=";
+    sha256 = "sha256-0DFl+UFxQckRM2qXFqDpKaRQ5sIMUbNj7l3zKPKAOnQ=";
   };
 
   dontBuild = true;
@@ -48,11 +52,15 @@ stdenv.mkDerivation rec {
     mkdir -p $out/opt/${pname} $out/bin $out/share/applications
     cp -Rv . $out/opt/${pname}
     cd $out/opt/${pname}
-    for i in $(find . -type f -name '*.so') wassabee
+    BINARIES="wassabee \
+              Microservices/Binaries/lin64/Tor/tor \
+              Microservices/Binaries/lin64/bitcoind \
+              Microservices/Binaries/lin64/hwi"
+    for i in $(find . -type f -name '*.so') $BINARIES
       do
         patchelf --set-rpath ${libPath} $i
       done
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" wassabee
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $BINARIES
     ln -s $out/opt/${pname}/wassabee $out/bin/${pname}
     cp -v $desktopItem/share/applications/* $out/share/applications
   '';
