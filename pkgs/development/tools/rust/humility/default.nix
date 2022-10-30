NixOS/nixpkgs

commit 63d54a85c19b55df8a05c76210bd862b338af62b

humility: init at unstable-2022-09-15
A debugger for the Hubris RTOS

pkgs/development/tools/rust/humility/default.nix
@@ -1,0 +1,32 @@
+{ lib
+, rustPlatform
+, fetchFromGitHub
+, libusb
+, libftdi
+, cargo-readme
+, pkg-config
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "humility";
+  version = "unstable-2022-09-15";
+
+  nativeBuildInputs = [ pkg-config cargo-readme ];
+  buildInputs = [ libusb libftdi ];
+
+  src = fetchFromGitHub {
+    owner = "oxidecomputer";
+    repo = pname;
+    rev = "d336c21c7b6da7f8203a9331c7657581de2bc6ad";
+    sha256 = "sha256-yW7QcxTWbL2YsV2bvfhbqQ2nawlPQbYxBfIGCWo28GY=";
+  };
+
+  cargoSha256 = "sha256-UhO8VO3OCfYc8Xq/P+l9f5ZrhOD/TBzSClAeAXLJLlc=";
+
+  meta = with lib; {
+    description = "Debugger for Hubris";
+    homepage = "https://github.com/oxidecomputer/humility";
+    license = with licenses; [ mpl20 ];
+    maintainers = with maintainers; [ therishidesai ];
+  };
+}

pkgs/top-level/all-packages.nix
@@ -24438,6 +24438,8 @@ with pkgs;
 
   htop-vim = callPackage ../tools/system/htop/htop-vim.nix { };
 
+  humility = callPackage ../development/tools/rust/humility {};
+
   btop = callPackage ../tools/system/btop {
     stdenv = gcc11Stdenv;
   };

commit 68dddfca5e4c62bdbb4cdca5aca5df2f81d5e6ef

humility: fix eval with aliases disabled
Fixes: 63d54a85c19 ("humility: init at unstable-2022-09-15")

pkgs/development/tools/rust/humility/default.nix
@@ -1,7 +1,7 @@
 { lib
 , rustPlatform
 , fetchFromGitHub
-, libusb
+, libusb1
 , libftdi
 , cargo-readme
 , pkg-config
@@ -12,7 +12,7 @@ rustPlatform.buildRustPackage rec {
   version = "unstable-2022-09-15";
 
   nativeBuildInputs = [ pkg-config cargo-readme ];
-  buildInputs = [ libusb libftdi ];
+  buildInputs = [ libusb1 libftdi ];
 
   src = fetchFromGitHub {
     owner = "oxidecomputer";