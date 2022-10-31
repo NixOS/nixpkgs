041135e417346082d0eef82709626109b161f7c6
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-31 04:52:37 +0000

    hyfetch: 1.4.2 -> 1.4.3

--- pkgs/tools/misc/hyfetch/default.nix
+++ pkgs/tools/misc/hyfetch/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonPackage rec {
   pname = "HyFetch";
-  version = "1.4.2";
+  version = "1.4.3";
 
   src = fetchFromGitHub {
     repo = "hyfetch";
     owner = "hykilpikonna";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-5TzIhbyrhQmuxR/Vs3XpOj/8FnykmBiDj6sXfFZK0uM=";
+    sha256 = "sha256-ScdcV1ojtVz/OdR7k+y7Wj4VAne++8LY9Vz+wO3CJGI=";
   };
 
   propagatedBuildInputs = [
