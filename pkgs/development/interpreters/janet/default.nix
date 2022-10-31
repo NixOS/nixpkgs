1b5563461579a396e27a6cbf5e927179a790fc6f
R. Ryantm <ryantm-bot@ryantm.com>
2022-10-31 00:16:59 +0000

    janet: 1.24.1 -> 1.25.1

--- pkgs/development/interpreters/janet/default.nix
+++ pkgs/development/interpreters/janet/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "janet";
-  version = "1.24.1";
+  version = "1.25.1";
 
   src = fetchFromGitHub {
     owner = "janet-lang";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-uGbaoWJAWbSQ7QkocU7gFZUiWb0GD8mtuO7V0sUXTv0=";
+    sha256 = "sha256-TWKiSE8mqXz5i4vd32WylTsyxMXXKGmVl3U2q2sMwDQ=";
   };
 
   postPatch = ''
