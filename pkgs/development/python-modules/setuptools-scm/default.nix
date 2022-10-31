d6875837436c399b7b496c490502c2d8871a9492
Theodore Ni <3806110+tjni@users.noreply.github.com>
2022-10-31 00:19:33 -0700

    python37Packages.setuptools-scm: fix in python 3.7
    We need to remove the dependency on importlib-metadata specified within
    setup.cfg, since importlib-metadata depends on setuptools-scm.

--- pkgs/development/python-modules/setuptools-scm/default.nix
+++ pkgs/development/python-modules/setuptools-scm/default.nix
@@ -1,6 +1,7 @@
 { buildPythonPackage
 , callPackage
 , fetchPypi
+, pythonOlder
 , packaging
 , typing-extensions
 , tomli
@@ -15,12 +16,25 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "setuptools_scm";
     inherit version;
-    sha256 = "sha256-Ax4Tr3cdb4krlBrbbqBFRbv5Hrxc5ox4qvP/9uH7SEQ=";
+    hash = "sha256-Ax4Tr3cdb4krlBrbbqBFRbv5Hrxc5ox4qvP/9uH7SEQ=";
   };
+
+  # We need to remove importlib-metadata as a dependency when using Python 3.7
+  # because it depends on setuptools_scm, and we build both from source.
+  #
+  # This still bootstraps because of:
+  #
+  #   https://github.com/pypa/setuptools_scm/pull/724
+  #
+  postPatch = lib.optionalString (pythonOlder "3.8") ''
+    substituteInPlace setup.cfg --replace "importlib-metadata;python_version < '3.8'" ""
+  '';
 
   propagatedBuildInputs = [
     packaging
     typing-extensions
+    # Once 7.0.6+ of setuptools_scm is released, make tomli only a dependency
+    # when Python < 3.11. See https://github.com/pypa/setuptools_scm/pull/748.
     tomli
     setuptools
   ];
