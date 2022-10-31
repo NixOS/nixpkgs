4846e88b918c7512ea02f76ee8b998b63431eccf
Vincent Demeester <vincent@sbr.pm>
2022-10-31 16:28:50 +0100

    tektoncd-cli: 0.24.1 -> 0.27.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- pkgs/applications/networking/cluster/tektoncd-cli/default.nix
+++ pkgs/applications/networking/cluster/tektoncd-cli/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "tektoncd-cli";
-  version = "0.24.1";
+  version = "0.27.0";
 
   src = fetchFromGitHub {
     owner = "tektoncd";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-8dCmORfTMFHSyc9FOpL01ywxGcH3uolzD2aOtyy191Q=";
+    sha256 = "sha256-zLPZueKdUNauIzqXOV773SZ/RWg7UuMNeJHr7z6ZJ+E=";
   };
 
   vendorSha256 = null;
