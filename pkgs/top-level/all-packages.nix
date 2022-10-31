18cdb24cd3e9dc62be7801175c46f45ddb28a0b2
Fabian Affolter <mail@fabian-affolter.ch>
2022-10-31 10:47:09 +0100

    legitify: init at 0.1.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29838,6 +29838,8 @@ with pkgs;
 
   legit = callPackage ../applications/version-management/git-and-tools/legit { };
 
+  legitify = callPackage ../development/tools/legitify { };
+
   lens = callPackage ../applications/networking/cluster/lens { };
 
   leo-editor = libsForQt5.callPackage ../applications/editors/leo-editor { };
