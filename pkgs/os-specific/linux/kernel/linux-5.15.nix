59557c5dc414eabe47e603eabea53aecc9560c0f
Bernardo Meurer <bernardo@meurer.org>
2022-10-31 11:39:22 +0000

    linux: 5.15.75 -> 5.15.76
    (cherry picked from commit ac745085d8286d0fe73e8c651e3fe3ec26a8ee4f)

--- pkgs/os-specific/linux/kernel/linux-5.15.nix
+++ pkgs/os-specific/linux/kernel/linux-5.15.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.15.75";
+  version = "5.15.76";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -15,6 +15,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1ijzqkp9mbfvm8ii5rash401b6wqywkql9j2rxdgbk2r6vfmp9nr";
+    sha256 = "0zymcp88654qk896djvc2ngdksvhkzh1ndhfk1dn5qqrqhha01wh";
   };
 } // (args.argsOverride or { }))
