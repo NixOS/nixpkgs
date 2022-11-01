8a837a95ed519d667bda873eb3dfa9b5ec55858b
Naïm Favier <n@monade.li>
2022-11-01 14:29:24 +0100

    crimson-pro: init at unstable-2022-08-30
    A professionally produced redesign of Crimson by Jacques Le Bailly.
    https://github.com/Fonthausen/CrimsonPro

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26064,6 +26064,8 @@ with pkgs;
 
   crimson = callPackage ../data/fonts/crimson {};
 
+  crimson-pro = callPackage ../data/fonts/crimson-pro {};
+
   dejavu_fonts = lowPrio (callPackage ../data/fonts/dejavu-fonts {});
 
   # solve collision for nix-env before https://github.com/NixOS/nix/pull/815
