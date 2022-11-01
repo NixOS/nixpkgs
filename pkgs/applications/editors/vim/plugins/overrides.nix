54b03e5f4a8708f7625454563415ae36d4619d02
Skyler Grey <skyler3665@gmail.com>
2022-11-01 23:44:18 +0000

    vimPlugins.coc-spell-checker: use a node package
    - coc-spell-checker needs to be from nodePackages rather than generated
      as normal vim plugins are
    - this commit adds coc-spell-checker to nodePackages and adds an
      override to the vim plugin

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -1306,6 +1306,7 @@ self: super: {
       "coc-smartf"
       "coc-snippets"
       "coc-solargraph"
+      "coc-spell-checker"
       "coc-sqlfluff"
       "coc-stylelint"
       "coc-sumneko-lua"
