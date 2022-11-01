09e02af555cfcfc218f9127e572ea0029d9ff096
kilianar <mail@kilianar.de>
2022-11-01 14:49:31 +0100

    logseq: 0.8.9 -> 0.8.10
    https://github.com/logseq/logseq/releases/tag/0.8.10

--- pkgs/applications/misc/logseq/default.nix
+++ pkgs/applications/misc/logseq/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "logseq";
-  version = "0.8.9";
+  version = "0.8.10";
 
   src = fetchurl {
     url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
-    sha256 = "sha256-s9xG2SLkuaz8wRK47ywSw9JjsJlRjaMNeRmQY0ZnrK8=";
+    sha256 = "sha256-n6uRMkV/+U2M0/FjQJx0LIDGgeIX6TY58j2sweSmlbc=";
     name = "${pname}-${version}.AppImage";
   };
 
