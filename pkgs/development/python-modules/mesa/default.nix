89867128a7b5127567d94436d718a476db189fe5
Louis Bettens <louis@bettens.info>
2022-11-01 10:02:44 +0100

    python3Packages.mesa: 0.8.7 -> 1.1.1

--- pkgs/development/python-modules/mesa/default.nix
+++ pkgs/development/python-modules/mesa/default.nix
@@ -1,10 +1,20 @@
-{ lib, buildPythonPackage, fetchFromGitHub, isPy27
-, cookiecutter, networkx , pandas, tornado, tqdm
-, pytest }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, fetchurl
+, fetchzip
+, isPy27
+, cookiecutter
+, networkx
+, pandas
+, tornado
+, tqdm
+, pytest
+}:
 
 buildPythonPackage rec {
   pname = "mesa";
-  version = "0.8.7";
+  version = "1.1.1";
 
   # According to their docs, this library is for Python 3+.
   disabled = isPy27;
@@ -13,8 +23,39 @@ buildPythonPackage rec {
     owner = "projectmesa";
     repo = "mesa";
     rev = "v${version}";
-    sha256 = "0i1bpdqjrx4avgrzyqxpwxx86j11yhrq1j4kca854xahvhmwis19";
+    hash = "sha256-6EVxhRRR0qmwsKlFeD/U89uolfNxgXd66doDDTxsvS4=";
   };
+
+  postPatch =
+    let
+      bootstrap_version = "5.1.3";
+      bootstrap = fetchzip {
+        url = "https://github.com/twbs/bootstrap/releases/download/v${bootstrap_version}/bootstrap-${bootstrap_version}-dist.zip";
+        hash = "sha256-eADXbCTyaVaFylZS86Ou/FwyhoqwHfJicpkyOfKfIG8=";
+      };
+      bootstrap_slider_version = "11.0.2";
+      bootstrap_slider = fetchzip {
+        url = "https://github.com/seiyria/bootstrap-slider/archive/refs/tags/v${bootstrap_slider_version}.zip";
+        hash = "sha256-8ekuRfxbEABVch3FvpYKzH8EYS9sENs7kfApxyi2V2Q=";
+      };
+      d3_version = "7.4.3";
+      d3 = fetchurl {
+        url = "https://cdnjs.cloudflare.com/ajax/libs/d3/${d3_version}/d3.min.js";
+        hash = "sha256-+MpQAOeqA18DtHk7JETZadE17KoMl0jCS89tlo6OWGM=";
+      };
+      chartjs_version = "3.6.1";
+      chartjs = fetchurl {
+        url = "https://cdn.jsdelivr.net/npm/chart.js@${chartjs_version}/dist/chart.min.js";
+        hash = "sha256-bj8JMRF/ShthPyTNKaHVG4LwnGL/KNnJ5CAASgZE99I=";
+      };
+    in
+    ''
+      mkdir -p mesa/visualization/templates/{,js/}external
+      cp -R ${bootstrap} mesa/visualization/templates/external/bootstrap-${bootstrap_version}-dist
+      cp -R ${bootstrap_slider} mesa/visualization/templates/external/bootstrap-slider-${bootstrap_slider_version}
+      cp ${d3} mesa/visualization/templates/js/external/d3-${d3_version}.min.js
+      cp ${chartjs} mesa/visualization/templates/js/external/chart-${chartjs_version}.min.js
+    '';
 
   checkInputs = [ pytest ];
 
