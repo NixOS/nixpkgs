0f37df52bc1e83d7f32543296717bed4732cb60c
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-01 12:30:45 +0000

    python310Packages.pydmd: 0.4.0.post2210 -> 0.4.0.post2211

--- pkgs/development/python-modules/pydmd/default.nix
+++ pkgs/development/python-modules/pydmd/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "pydmd";
-  version = "0.4.0.post2210";
+  version = "0.4.0.post2211";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "mathLab";
     repo = "PyDMD";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-2GSmd+OxdGW9XdLGL08Bnrz0WCtTAYQtYHf2gM5CFIE=";
+    sha256 = "sha256-DTKw7Gy2w+zas9u62SN6QDhoeMEjSk/Ej09do38yfNY=";
   };
 
   propagatedBuildInputs = [
