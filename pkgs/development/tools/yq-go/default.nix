3f4650ef903b965ac7b71d33aa93959863cd57f4
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-11-01 14:23:09 +0100

    yq-go: 4.29.1 -> 4.29.2

--- pkgs/development/tools/yq-go/default.nix
+++ pkgs/development/tools/yq-go/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "yq-go";
-  version = "4.29.1";
+  version = "4.29.2";
 
   src = fetchFromGitHub {
     owner = "mikefarah";
     repo = "yq";
     rev = "v${version}";
-    sha256 = "sha256-aZMlWC6qtN2TvND18QO8Q3UicSNa9cZT/xPmia3V6FM=";
+    sha256 = "sha256-ZHl0FTxWdoxHrbWx6mp5qR/l/FtC3bBkzGuR0T2NouA=";
   };
 
   vendorSha256 = "sha256-L3l6wH4bR1/R6MtQTHYsyRE5E/EPnpNwa310zUONo+s=";
