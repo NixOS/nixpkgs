8cc5d8e32a64f0b93c5a5c502f498ff128565ff3
Bernardo Meurer <bernardo@meurer.org>
2022-11-01 15:15:08 +0000

    linuxKernel.kernels.linux_5_19: drop
    The 5.19.x series has reached EOL, and is no longer supported upstream.

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -16,7 +16,7 @@
 , enablePython ? true
 
 # for determining the latest compatible linuxPackages
-, linuxPackages_5_19 ? pkgs.linuxKernel.packages.linux_5_19
+, linuxPackages_5_15 ? pkgs.linuxKernel.packages.linux_5_15
 , linuxPackages_6_0 ? pkgs.linuxKernel.packages.linux_6_0
 }:
 
@@ -218,7 +218,7 @@ in {
   zfsStable = common {
     # check the release notes for compatible kernels
     kernelCompatible = kernel.kernelOlder "5.20";
-    latestCompatibleLinuxPackages = linuxPackages_5_19;
+    latestCompatibleLinuxPackages = linuxPackages_5_15;
 
     # this package should point to the latest release.
     version = "2.1.6";
