9f2fe862966918f9e8c1b2d032cafe196235a34c
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-11-01 15:34:27 +0100

    python310Packages.types-typed-ast: 1.5.8 -> 1.5.8.1

--- pkgs/development/python-modules/types-typed-ast/default.nix
+++ pkgs/development/python-modules/types-typed-ast/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "types-typed-ast";
-  version = "1.5.8";
+  version = "1.5.8.1";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-FW+6ypA+hXH+cLsLVi/7qSp8vbe6yacTNRyifYgYO6c=";
+    hash = "sha256-9kwV7ATAkRgZrEwP0AUnlRMgAfv16iPR1+T+rX3km/w=";
   };
 
   # Module doesn't have tests
