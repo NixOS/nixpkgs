fddf4b1926edee5cb4338b6092310415302ff6f6
Vladimír Čunát <v@cunat.cz>
2022-11-01 17:32:27 +0100

    openssl_1_1: drop a long unused patch
    (cherry picked from commit 6aa0c5e9186dc9022fbab2290516aaa862add8b7)

--- pkgs/development/libraries/openssl/default.nix
+++ pkgs/development/libraries/openssl/default.nix
@@ -185,7 +185,7 @@ let
 in {
 
 
-  openssl_1_1 = common rec {
+  openssl_1_1 = common {
     version = "1.1.1s";
     sha256 = "sha256-xawB52Dub/Dath1rK70wFGck0GPrMiGAxvGKb3Tktqo=";
     patches = [
@@ -194,8 +194,6 @@ in {
       (if stdenv.hostPlatform.isDarwin
        then ./use-etc-ssl-certs-darwin.patch
        else ./use-etc-ssl-certs.patch)
-    ] ++ lib.optionals (stdenv.isDarwin && (builtins.substring 5 5 version) < "m") [
-      ./1.1/macos-yosemite-compat.patch
     ];
     withDocs = true;
   };
