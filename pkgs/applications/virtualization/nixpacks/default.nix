b57c654b9a8c85a8f6fb76940825fdadf865e3a0
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-02 08:50:52 +0000

    nixpacks: 0.11.5 -> 0.11.6

--- pkgs/applications/virtualization/nixpacks/default.nix
+++ pkgs/applications/virtualization/nixpacks/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "nixpacks";
-  version = "0.11.5";
+  version = "0.11.6";
 
   src = fetchFromGitHub {
     owner = "railwayapp";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-QLF49k8f8YYwPl26pyz9/bbO4IO0KqSlgTFNjbvPf7k=";
+    sha256 = "sha256-9b1j3hY4ChlLIXmNsIjt6SjqHOYSqdY98amV36WPU74=";
   };
 
-  cargoSha256 = "sha256-u0L3NJ4ku5ETBx6PKgVStrcSCX4I7E6GNtW+iv3yy2g=";
+  cargoSha256 = "sha256-s34mYMaHElBTD7I6QZtGsVTZUbVkcSmavvLs2k4mJDw=";
 
   # skip test due FHS dependency
   doCheck = false;
