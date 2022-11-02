4572cfd2b86662d9ea654048f068391dab24b4ac
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-02 00:53:43 +0000

    python310Packages.twilio: 7.15.0 -> 7.15.1

--- pkgs/development/python-modules/twilio/default.nix
+++ pkgs/development/python-modules/twilio/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "twilio";
-  version = "7.15.0";
+  version = "7.15.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "twilio";
     repo = "twilio-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-raRrEK1Ub5yV4T2onZj+Y+pmtm09z3KxGmtqK6TnlSk=";
+    hash = "sha256-Ud4NbiTO7ki+l8hPzGP7DlReRA0XK0OJhDZRPNLWm7A=";
   };
 
   propagatedBuildInputs = [
