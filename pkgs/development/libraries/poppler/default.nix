a06429504b67debf3f50f55d1a6d6d3721031be1
Jan Tojnar <jtojnar@gmail.com>
2022-11-02 12:56:43 +0100

    poppler: 22.09.0 → 22.11.0
    https://lists.freedesktop.org/archives/poppler/2022-October/015332.html
    https://gitlab.freedesktop.org/poppler/poppler/-/compare/poppler-22.09.0...poppler-22.10.0
    
    https://lists.freedesktop.org/archives/poppler/2022-November/015346.html
    https://gitlab.freedesktop.org/poppler/poppler/-/compare/poppler-22.10.0...poppler-22.11.0

--- pkgs/development/libraries/poppler/default.nix
+++ pkgs/development/libraries/poppler/default.nix
@@ -47,13 +47,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: rec {
   pname = "poppler-${suffix}";
-  version = "22.09.0"; # beware: updates often break cups-filters build, check texlive and scribus too!
+  version = "22.11.0"; # beware: updates often break cups-filters build, check texlive and scribus too!
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "https://poppler.freedesktop.org/poppler-${version}.tar.xz";
-    hash = "sha256-16j3SCETWcrbd0uj4Y7NpkZLNAJwRcBkjrMNWFKkHi4=";
+    hash = "sha256-CTuphE7XdChVFzYcFeIaMbpN8nikmSY9RAPMp08tqCg=";
   };
 
   nativeBuildInputs = [
