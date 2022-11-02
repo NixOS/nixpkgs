428f22c3596f5bf0b3f1a0e3308816baf99a3fc8
Sandro <sandro.jaeckel@gmail.com>
2022-11-02 09:13:10 +0100

    gopls: 0.10.0 -> 0.10.1 (#199050)

--- pkgs/development/tools/gopls/default.nix
+++ pkgs/development/tools/gopls/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "gopls";
-  version = "0.10.0";
+  version = "0.10.1";
 
   src = fetchFromGitHub {
     owner = "golang";
     repo = "tools";
     rev = "gopls/v${version}";
-    sha256 = "sha256-bSy5aoIrYjMG6hlkDf4vyR6r2XpjKAOX0C6MitYeg8k=";
+    sha256 = "sha256-9WDqd8Xgiov/OFAFl5yZmon4o3grbOxzZs3wnNu7pbg=";
   };
 
   modRoot = "gopls";
-  vendorSha256 = "sha256-UwHZRSH2amS1um9hi/MRs3nQiCXCl52+S7+hqc/Orqc=";
+  vendorSha256 = "sha256-EZ/XPta2vQfemywoC2kbTamJ43K4tr4I7mwVzrTbRkA=";
 
   doCheck = false;
 
