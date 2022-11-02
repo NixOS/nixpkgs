1f89f9b6ff4b12df362900b69a517ba7bb68437c
Nick Cao <nickcao@nichi.co>
2022-11-02 13:41:58 +0800

    libsForQt5_openssl_1_1: drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21862,21 +21862,6 @@ with pkgs;
   qt5 =        qt515;
   libsForQt5 = libsForQt515;
 
-  # TODO: remove once no package needs this anymore or together with OpenSSL 1.1
-  qt5_openssl_1_1 = qt5.overrideScope' (_: super: {
-    qtbase = super.qtbase.override {
-      openssl = openssl_1_1;
-      libmysqlclient = libmysqlclient.override {
-        openssl = openssl_1_1;
-        curl = curl.override { openssl = openssl_1_1; };
-      };
-    };
-  });
-  libsForQt5_openssl_1_1 = import ./qt5-packages.nix {
-    inherit lib pkgs;
-    qt5 = qt5_openssl_1_1;
-  };
-
   # plasma5Packages maps to the Qt5 packages set that is used to build the plasma5 desktop
   plasma5Packages = libsForQt515;
 
