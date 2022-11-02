862277ac9d34273cd953f42061e23d488d6b7e8b
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-02 00:10:26 +0000

    kstars: 3.6.0 -> 3.6.1

--- pkgs/applications/science/astronomy/kstars/default.nix
+++ pkgs/applications/science/astronomy/kstars/default.nix
@@ -14,11 +14,11 @@
 
 mkDerivation rec {
   pname = "kstars";
-  version = "3.6.0";
+  version = "3.6.1";
 
   src = fetchurl {
     url = "mirror://kde/stable/kstars/kstars-${version}.tar.xz";
-    sha256 = "sha256-wdv2TOOPIB3VRG3XMs58i/HvMDZ9wvERAhBQNslJ25E=";
+    sha256 = "sha256-WWbtfqvGd13+a41z8/MjlchllpuhLX08nu15OlYUeuI=";
   };
 
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
