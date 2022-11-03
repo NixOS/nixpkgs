b0bd0b41ae15fcdb4e31d2f790ebb82cc16dbd67
Madoura <madouura@gmail.com>
2022-11-03 06:20:02 -0500

    rocmlir: init at 5.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14859,6 +14859,8 @@ with pkgs;
 
   rocminfo = callPackage ../development/tools/rocminfo { };
 
+  rocmlir = callPackage ../development/libraries/rocmlir { };
+
   rtags = callPackage ../development/tools/rtags {
     inherit (darwin) apple_sdk;
   };
