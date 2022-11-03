7450166dec60b3862b4e38974993de914c38a188
Berk D. Demir <bdd@mindcast.org>
2022-11-03 00:25:58 -0700

    go-font: Fetch only the installables instead of entire blobs repo
    Switch to using `fetchzip` to download a subdirectory tarball including
    TrueType fonts and their license instead of cloning Go's 'images'
    repository.
    
    - Simplifies the derivation
    - Downloads 900K instead of 28M
    - Requires 1/8 CPU cycles
    - ...shortening the build time.

--- pkgs/data/fonts/go-font/default.nix
+++ pkgs/data/fonts/go-font/default.nix
@@ -1,20 +1,18 @@
-{ lib, fetchgit }:
+{ lib, fetchzip }:
 
 let
   version = "2.010";
-in (fetchgit {
-  name = "go-font-${version}";
-  url = "https://go.googlesource.com/image";
   rev = "41969df76e82aeec85fa3821b1e24955ea993001";
+in (fetchzip {
+  name = "go-font-${version}";
+  url = "https://go.googlesource.com/image/+archive/${rev}/font/gofont/ttfs.tar.gz";
+  stripRoot = false;
 
   postFetch = ''
-    mv $out source
-    cd source
-
     mkdir -p $out/share/fonts/truetype
     mkdir -p $out/share/doc/go-font
-    cp font/gofont/ttfs/* $out/share/fonts/truetype
-    mv $out/share/fonts/truetype/README $out/share/doc/go-font/LICENSE
+    mv $out/*.ttf $out/share/fonts/truetype
+    mv $out/README $out/share/doc/go-font/LICENSE
   '';
 
   sha256 = "175jwq16qjnd2k923n9gcbjizchy7yv4n41dm691sjwrhbl0b13x";
