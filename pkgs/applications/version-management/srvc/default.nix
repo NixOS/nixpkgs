3a00c537fa350581ee41692fd51fb1ff7c203407
John Shaffer <jdsha@proton.me>
2022-11-03 13:25:56 -0500

    srvc: 0.6.0 -> 0.7.0

--- pkgs/applications/version-management/srvc/default.nix
+++ pkgs/applications/version-management/srvc/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "srvc";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "insilica";
     repo = "rs-srvc";
     rev = "v${version}";
-    sha256 = "sha256-PBs86cvEacvCt/2JnURL4qKvXGXRZHWaGYrPUSsnt0I=";
+    sha256 = "sha256-XGZ6Jr/RVDOLDa0sANZIsKtNjY3pEBlOtei+xNGPBBY=";
   };
 
-  cargoSha256 = "sha256-5CUbfI67gsINdHcxN8KbIN10Mu90rAU53DbmQ5QotWg=";
+  cargoSha256 = "sha256-sjMg5XMnAQZjp6z9prtRq+0pyNQ1oTm0/KWOZPxDW4w=";
 
   meta = with lib; {
     description = "Sysrev version control";
