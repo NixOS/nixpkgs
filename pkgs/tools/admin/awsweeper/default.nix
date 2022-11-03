ac84ebbd20ca5cf059ad3759c6dc6d266bb4d58d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-11-03 18:20:19 +0100

    awsweeper: fix build on aarch64-darwin

--- pkgs/tools/admin/awsweeper/default.nix
+++ pkgs/tools/admin/awsweeper/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub, updateGolangSysHook }:
 
 buildGoModule rec {
   pname = "awsweeper";
@@ -11,7 +11,9 @@ buildGoModule rec {
     sha256 = "sha256-5D/4Z8ADlA+4+2EINmP5OfX5exzhfbq2TydPRlJDA6Y=";
   };
 
-  vendorSha256 = "sha256-jzK56x5mzQkD3tSs6X0Z2Zn1OLXFHgWHz0YLZ3m3NS4=";
+  nativeBuildInputs = [ updateGolangSysHook ];
+
+  vendorSha256 = "sha256-zgn+n/1N+JVqaKrtSc4T659LG+Yj5w/qTpouTzBl6xg=";
 
   ldflags = [ "-s" "-w" "-X github.com/jckuester/awsweeper/internal.version=${version}" "-X github.com/jckuester/awsweeper/internal.commit=${src.rev}" "-X github.com/jckuester/awsweeper/internal.date=unknown" ];
 
