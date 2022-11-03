a408e4c5d34f68e8ae9c33a82e5cfb5cc04ae755
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-03 21:16:44 +0100

    python310Packages.fastrlock: disable on older Python releases
    - add pythonImportsCheck

--- pkgs/development/python-modules/fastrlock/default.nix
+++ pkgs/development/python-modules/fastrlock/default.nix
@@ -1,17 +1,43 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib
+, buildPythonPackage
+, cython
+, fetchFromGitHub
+, pytestCheckHook
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "fastrlock";
   version = "0.8.1";
+  format = "setuptools";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-il8vAAIcSscuTauRDcGGPA4Aii5/tchDkzrpvPw9CAI=";
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "scoder";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "sha256-KYJd1wGJo+z34cY0YfsRbpC9IsQY/VJqycGpMmLmaVk=";
   };
 
+  nativeBuildInputs = [
+    cython
+  ];
+
+  # Todo: Check why the tests have an import error
+  doCheck = false;
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "fastrlock"
+  ];
+
   meta = with lib; {
+    description = "RLock implementation for CPython";
     homepage = "https://github.com/scoder/fastrlock";
-    description = "A fast RLock implementation for CPython";
     license = licenses.mit;
     maintainers = with maintainers; [ hyphon81 ];
   };
