e9eb56ab08cf8d438dfe4bdbe713f6f57b8fbc13
Louis Bettens <louis@bettens.info>
2022-11-03 23:34:30 +0100

    python3Packages.clvm-tools-rs: 0.1.19 -> 0.1.24

--- pkgs/development/python-modules/clvm-tools-rs/default.nix
+++ pkgs/development/python-modules/clvm-tools-rs/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "clvm-tools-rs";
-  version = "0.1.19";
+  version = "0.1.24";
   disabled = pythonOlder "3.7";
   format = "pyproject";
 
@@ -15,13 +15,13 @@ buildPythonPackage rec {
     owner = "Chia-Network";
     repo = "clvm_tools_rs";
     rev = version;
-    sha256 = "sha256-LQbFBZBLUAjyqIAWIn+N8tUrBMskRoKvMMg5gfTyVU8=";
+    sha256 = "sha256-1pXbmurR94JUPEUMCw0oFc4D8zL+TMbDLilbHEVqpzU=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    sha256 = "sha256-LcDWpMM+PUElsXO82H6QVOp338+NduC/j3pXQKSni3I=";
+    sha256 = "sha256-q7v4NY4EcKJ/LHoKM9BsitLnTfBpGc5p53FQ3CCL8Ls=";
   };
 
   nativeBuildInputs = with rustPlatform; [
