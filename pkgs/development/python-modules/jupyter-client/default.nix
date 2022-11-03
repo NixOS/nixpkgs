0b845f6aad295292f6b2494d430f2d817c1c02b2
Frederik Rietdijk <fridh@fridh.nl>
2022-11-03 11:04:16 +0100

    python3Packages.jupyter_client: 7.3.5 -> 7.4.4

--- pkgs/development/python-modules/jupyter-client/default.nix
+++ pkgs/development/python-modules/jupyter-client/default.nix
@@ -15,12 +15,12 @@
 
 buildPythonPackage rec {
   pname = "jupyter_client";
-  version = "7.3.5";
+  version = "7.4.4";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-PFhGahuNVdugvzzgg05PW3dguvmNHXPbCt1vGd6ezR0=";
+    sha256 = "sha256-VhbbYJrHIEIuakuJPWVyuNZV/0HgWDZ/RFmg0sByaDI=";
   };
 
   nativeBuildInputs = [
