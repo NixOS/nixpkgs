a7e37e6ae09d751987bc88c1e1823ff230454175
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-11-03 23:57:51 +0100

    vt-cli: fix build on aarch64-darwin

--- pkgs/tools/security/vt-cli/default.nix
+++ pkgs/tools/security/vt-cli/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, updateGolangSysHook
 }:
 
 buildGoModule rec {
@@ -14,7 +15,9 @@ buildGoModule rec {
     sha256 = "0jqr2xf6f9ywavkx5hzcfnky8ax23ahdj24hjsnq6zlpdqvfn1xb";
   };
 
-  vendorSha256 = "sha256-XN6dJpoJe9nJn+Tr9SYD64LE0XFiO2vlpdyI9SrZZjQ=";
+  nativeBuildInputs = [ updateGolangSysHook ];
+
+  vendorSha256 = "sha256-wM5VPcNGEgnUvJEFrbcLVFAlzCA6a+a97xLeK5x0IEI=";
 
   ldflags = [
     "-X github.com/VirusTotal/vt-cli/cmd.Version=${version}"
