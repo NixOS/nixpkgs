9a7cdc74a3d4c001f887ad732353002f3fbb71ec
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-03 04:02:43 +0000

    adguardhome: 0.107.16 -> 0.107.17

--- pkgs/servers/adguardhome/bins.nix
+++ pkgs/servers/adguardhome/bins.nix
@@ -1,23 +1,23 @@
 { fetchurl, fetchzip }:
 {
 x86_64-darwin = fetchzip {
-  sha256 = "sha256-rVc1ad8qwXfEuqZUrTsdzSQHUit3j8PQp3B0dD6FrcQ=";
-  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.16/AdGuardHome_darwin_amd64.zip";
+  sha256 = "sha256-afHd6evLu478mnprzD3U1b/7pXYu1zBWc+9WlWaBlDs=";
+  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.17/AdGuardHome_darwin_amd64.zip";
 };
 aarch64-darwin = fetchzip {
-  sha256 = "sha256-GaeTdv467dVfSk2YuILmi2KphMfFYfLdpNI+dVx/UoU=";
-  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.16/AdGuardHome_darwin_arm64.zip";
+  sha256 = "sha256-2tE7chJP+3PjedyvyU9iLWq52OuBG2XGkyWuQzDizXw=";
+  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.17/AdGuardHome_darwin_arm64.zip";
 };
 i686-linux = fetchurl {
-  sha256 = "sha256-6fXU2vU3MgveWZhgrphOeczDc1GUBv7ZUehPF8+GUdY=";
-  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.16/AdGuardHome_linux_386.tar.gz";
+  sha256 = "sha256-iJXzQ9ELOy9OglEqYHvd9JwM12XTCbX1PFzAp31aMLA=";
+  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.17/AdGuardHome_linux_386.tar.gz";
 };
 x86_64-linux = fetchurl {
-  sha256 = "sha256-6rUC0R0bGS4omX9bybShk78mFaNRjUUkQqczZE2Wz5M=";
-  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.16/AdGuardHome_linux_amd64.tar.gz";
+  sha256 = "sha256-WMeDwSJrfmm4ZI84m3iNPzkg7kZE9oDwtngyXzh+rRw=";
+  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.17/AdGuardHome_linux_amd64.tar.gz";
 };
 aarch64-linux = fetchurl {
-  sha256 = "sha256-3cowMi081TIx65dgzBP+YkIj4IBwyK1Yd/gn2zL9kOI=";
-  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.16/AdGuardHome_linux_arm64.tar.gz";
+  sha256 = "sha256-9MGGzzPwX85iF228ibQ5NnTIxQ8dErD0+AOQGQpEKTs=";
+  url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.17/AdGuardHome_linux_arm64.tar.gz";
 };
 }
