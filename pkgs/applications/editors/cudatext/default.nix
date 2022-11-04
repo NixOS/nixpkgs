0d051fcf0cb7c043164685c4a28c0c66ae5c693b
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-04 04:15:40 +0000

    cudatext-qt: 1.174.0 -> 1.175.0

--- pkgs/applications/editors/cudatext/default.nix
+++ pkgs/applications/editors/cudatext/default.nix
@@ -38,13 +38,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "cudatext";
-  version = "1.174.0";
+  version = "1.175.0";
 
   src = fetchFromGitHub {
     owner = "Alexey-T";
     repo = "CudaText";
     rev = version;
-    hash = "sha256-3jj04ndyuJpfXu9z8j/Pdp8yNybmzLbrPr/S+uV7Rfg=";
+    hash = "sha256-Q4T4CmMK+sxOst18pW4L4uMYzc/heMetntM0L+HrSlo=";
   };
 
   postPatch = ''
