5b78fd6b704382fa67f8b425634102c3ed362a93
Colin Arnott <colin@urandom.co.uk>
2022-11-04 23:12:45 +0000

    signal-desktop-beta: reinit at 6.1.0-beta.1
    signal-desktop-beta was removed due to maintainability concerns in
    256e347dfe2e8ab0070d85320fc1cc7da163eb92. Unfortunately, there are a
    number of features, like stories, that are only enabled on the beta
    builds. As such, this change adds that package back, but based upon the
    current signal-desktop derivation.
    
    We have restructured the derivation into the generic+versions structure
    seen in libmicrohttpd, however since there are a fixed number of
    releases, stable and beta, they both live default.nix and are inherited
    at the top level.
    
    I have also added myself to the maintainers list, however we could also
    split that up, so that I alone bear the maintenance responsibilities.
    
    (cherry picked from commit bd35bc1f873a09f0b8c7fbf1a3860a9debe45a05)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11407,7 +11407,7 @@ with pkgs;
 
   signal-cli = callPackage ../applications/networking/instant-messengers/signal-cli { };
 
-  signal-desktop = callPackage ../applications/networking/instant-messengers/signal-desktop { };
+  inherit (callPackage ../applications/networking/instant-messengers/signal-desktop {}) signal-desktop signal-desktop-beta;
 
   slither-analyzer = with python3Packages; toPythonApplication slither-analyzer;
 
