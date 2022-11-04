8dc7d564aa91fc6efec474320840480dc92adadd
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2022-11-04 23:07:35 +0100

    gammu: fix script dependencies on bash and dialog

--- pkgs/applications/misc/gammu/default.nix
+++ pkgs/applications/misc/gammu/default.nix
@@ -1,5 +1,5 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, cmake, bluez, libusb1, curl
-, libiconv, gettext, sqlite
+{ lib, stdenv, fetchFromGitHub, substituteAll, pkg-config, cmake, bluez, libusb1, curl
+, libiconv, gettext, sqlite, bash, dialog
 , dbiSupport ? false, libdbi ? null, libdbiDrivers ? null
 , postgresSupport ? false, postgresql ? null
 }:
@@ -17,13 +17,20 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-aeaGHVxOMiXRU6RHws+oAnzdO9RY1jw/X/xuGfSt76I=";
   };
 
-  patches = [ ./bashcomp-dir.patch ./systemd.patch ];
+  patches = [
+    ./bashcomp-dir.patch
+    ./systemd.patch
+    (substituteAll {
+      src = ./gammu-config-dialog.patch;
+      dialog = "${dialog}/bin/dialog";
+    })
+  ];
 
   nativeBuildInputs = [ pkg-config cmake ];
 
   strictDeps = true;
 
-  buildInputs = [ bluez libusb1 curl gettext sqlite libiconv ]
+  buildInputs = [ bash bluez libusb1 curl gettext sqlite libiconv ]
   ++ optionals dbiSupport [ libdbi libdbiDrivers ]
   ++ optionals postgresSupport [ postgresql ];
 
