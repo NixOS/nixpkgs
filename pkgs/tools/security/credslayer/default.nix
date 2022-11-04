ac60345207ca0fc65599742b3ab9bdf97aab2668
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-04 03:25:36 +0000

    credslayer: 0.1.2 -> 0.1.3

--- pkgs/tools/security/credslayer/default.nix
+++ pkgs/tools/security/credslayer/default.nix
@@ -6,13 +6,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "credslayer";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchFromGitHub {
     owner = "ShellCode33";
     repo = "CredSLayer";
-    rev = "v${version}";
-    sha256 = "1rbfy0h9c2gra1r2b39kngj3m7g177nmzzs5xy9np8lxixrh17pc";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-gryV9MHULY6ZHy6YDFQDIkZsfIX8La0tHT0vrrQJNDQ=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
