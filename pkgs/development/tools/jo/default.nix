0be100eafcff7fd89667e177dfd488c75d6ebffb
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-04 09:57:01 +0000

    jo: 1.7 -> 1.9

--- pkgs/development/tools/jo/default.nix
+++ pkgs/development/tools/jo/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "jo";
-  version = "1.7";
+  version = "1.9";
 
   src = fetchFromGitHub {
     owner  = "jpmens";
     repo = "jo";
     rev = version;
-    sha256 ="sha256-uJUbe593k7ENfbKCFhmm4Io0CPB109LF9EH8Qw0BFiY=";
+    sha256 ="sha256-1q4/RpxfoAdtY3m8bBuj7bhD17V+4dYo3Vb8zMbI1YU=";
   };
 
   enableParallelBuilding = true;
