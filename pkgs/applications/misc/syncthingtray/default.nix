822ccef174c410e889f444747edc6f75ce4f5a34
Doron Behar <doron.behar@gmail.com>
2022-11-05 12:44:12 +0200

    syncthingtray: 1.2.3 -> 1.3.0

--- pkgs/applications/misc/syncthingtray/default.nix
+++ pkgs/applications/misc/syncthingtray/default.nix
@@ -21,14 +21,14 @@
 }:
 
 mkDerivation rec {
-  version = "1.2.3";
+  version = "1.3.0";
   pname = "syncthingtray";
 
   src = fetchFromGitHub {
     owner = "Martchus";
     repo = "syncthingtray";
     rev = "v${version}";
-    sha256 = "sha256-jMl2kXpHVXH/TfdPbq6bzdpNec6f1AUWsMNZzaAvK/I=";
+    sha256 = "sha256-uhVRO9aiYJbUmwDp1+LIYF3wNBbVduVpTtVzaS0oUMU=";
   };
 
   buildInputs = [
