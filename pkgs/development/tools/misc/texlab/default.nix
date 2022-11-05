0912afd6efd0f7acc0855dbefe94b0eac3c016f9
Kira Bruneau <kira.bruneau@pm.me>
2022-11-05 02:48:19 +0000

    texlab: 4.3.0 → 4.3.1

--- pkgs/development/tools/misc/texlab/default.nix
+++ pkgs/development/tools/misc/texlab/default.nix
@@ -14,16 +14,16 @@ let
   isCross = stdenv.hostPlatform != stdenv.buildPlatform;
 in rustPlatform.buildRustPackage rec {
   pname = "texlab";
-  version = "4.3.0";
+  version = "4.3.1";
 
   src = fetchFromGitHub {
     owner = "latex-lsp";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-PhzxWLkqsHU32vIZwHzdy86bwIzDDmnxp12PS774P3k=";
+    sha256 = "sha256-gtPnuKmKfUBZDM6DATJB5NxndOwvG5JpBRO4cEU6lIU=";
   };
 
-  cargoSha256 = "sha256-tH566L0oHnI9mNp1KtJYaBaWCkiAOrtBYkfVSCA2N6w=";
+  cargoSha256 = "sha256-nu2KltPgexBTxG13kUgHgMrxefPD+Gaj5qBIWWFPdFs=";
 
   outputs = [ "out" ] ++ lib.optional (!isCross) "man";
 
@@ -36,19 +36,11 @@ in rustPlatform.buildRustPackage rec {
     CoreServices
   ];
 
-  postInstall = ''
-    # Remove generated dylib of human_name dependency. TexLab statically
-    # links to the generated rlib and doesn't reference the dylib. I
-    # couldn't find any way to prevent building this by passing cargo flags.
-    # See https://github.com/djudd/human-name/blob/master/Cargo.toml#L43
-    rm "$out/lib/libhuman_name${stdenv.hostPlatform.extensions.sharedLibrary}"
-    rmdir "$out/lib"
-  ''
   # When we cross compile we cannot run the output executable to
   # generate the man page
-  + lib.optionalString (!isCross) ''
+  postInstall = lib.optionalString (!isCross) ''
     # TexLab builds man page separately in CI:
-    # https://github.com/latex-lsp/texlab/blob/v4.3.0/.github/workflows/publish.yml#L131-L135
+    # https://github.com/latex-lsp/texlab/blob/v4.3.1/.github/workflows/publish.yml#L126-L130
     help2man --no-info "$out/bin/texlab" > texlab.1
     installManPage texlab.1
   '';
