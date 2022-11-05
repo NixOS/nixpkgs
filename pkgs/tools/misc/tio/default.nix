a18ecf7b73476e9e8f2f484d476b44ca790f676e
Bobby Rong <rjl931189261@126.com>
2022-11-05 21:40:45 +0800

    tio: remove unused cmake from nativeBuildInputs
    https://github.com/jtojnar/nixpkgs-hammering/blob/main/explanations/meson-cmake.md

--- pkgs/tools/misc/tio/default.nix
+++ pkgs/tools/misc/tio/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, cmake, pkg-config, inih, bash-completion }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, inih, bash-completion }:
 
 stdenv.mkDerivation rec {
   pname = "tio";
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     hash = "sha256-BjA9Zl6JcgDxTj4KPiWItSq9XuX9FJkpZnhdMBGZQpQ=";
   };
 
-  nativeBuildInputs = [ meson ninja cmake pkg-config inih bash-completion ];
+  nativeBuildInputs = [ meson ninja pkg-config inih bash-completion ];
 
   meta = with lib; {
     description = "Serial console TTY";
