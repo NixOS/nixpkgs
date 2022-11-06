d2552d4b8f272af9ecc5a7b3663c229211b127b4
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-06 17:39:37 +0100

    python310Packages.plugwise: 0.25.7 -> 0.26.0

--- pkgs/development/python-modules/plugwise/default.nix
+++ pkgs/development/python-modules/plugwise/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "plugwise";
-  version = "0.25.7";
+  version = "0.26.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = "python-plugwise";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-DLRTyGoH6yTdEOY2qQcP63K1l2tNTz0EO4DBAGYYoO8=";
+    sha256 = "sha256-WDjZZFl64tYZ7cy7xcLEX2/87TJSOw71QSro6cgE98s=";
   };
 
   propagatedBuildInputs = [
