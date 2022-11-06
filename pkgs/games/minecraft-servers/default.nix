fe066b8694100201d4d9cda278d35f8fe446349c
K1aymore <klaymorer@protonmail.com>
2022-11-06 11:31:51 -0800

    Only Vanilla and Fabric (Forge broken), fabric package

--- pkgs/games/minecraft-servers/default.nix
+++ pkgs/games/minecraft-servers/default.nix
@@ -2,8 +2,10 @@
 let
   servers = lib.importJSON ./servers.json;
   vanillaServers = lib.filterAttrs (n: v: lib.hasPrefix "vanilla-" n) servers;
+  fabricServers = lib.filterAttrs (n: v: lib.hasPrefix "fabric-" n) servers;
 
   latestVersion = lib.last (builtins.sort lib.versionOlder (builtins.catAttrs "version" (lib.collect (x: x ? version) vanillaServers)));
+  latestFabric = lib.last (builtins.sort lib.versionOlder (builtins.catAttrs "version" (lib.collect (x: x ? version) fabricServers)));
   escapeVersion = builtins.replaceStrings [ "." ] [ "-" ];
 
   getJavaVersion = v: (builtins.getAttr "openjdk${toString v}" javaPackages.compiler).headless;
@@ -16,10 +18,11 @@ let
         jre_headless = getJavaVersion (if value.javaVersion == null then 8 else value.javaVersion); # versions <= 1.6 will default to 8
       };
     })
-    servers;
+    (lib.mergeAttrs vanillaServers fabricServers); # Forge servers broken
 in
 lib.recurseIntoAttrs (
   packages // {
     vanilla = builtins.getAttr "vanilla-${lib.concatStringsSep "-" (lib.take 2 (lib.splitString "." latestVersion))}" packages;
+    fabric = builtins.getAttr "fabric-${lib.concatStringsSep "-" (lib.take 2 (lib.splitString "." latestFabric))}" packages;
   }
 )
