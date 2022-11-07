8ec252784ed0a348c2a752bee96f23a7a81c5755
Matt Layher <mdlayher@gmail.com>
2022-11-07 16:34:09 -0500

    zrepl: 0.5.0 -> 0.6.0
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37848,9 +37848,7 @@ with pkgs;
 
   zfs-replicate = python3Packages.callPackage ../tools/backup/zfs-replicate { };
 
-  zrepl = callPackage ../tools/backup/zrepl {
-    buildGoModule = buildGo118Module; # nixosTests.zrepl fails with 1.19
-  };
+  zrepl = callPackage ../tools/backup/zrepl { };
 
   uhubctl = callPackage ../tools/misc/uhubctl {};
 
