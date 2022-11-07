0b9d587d923e984f7f4cf5a83c3520eba56d3a06
Colin Arnott <colin@urandom.co.uk>
2022-11-07 17:28:22 +0000

    devbox: init at 0.1.0
    Fixes #196717

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16729,6 +16729,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  devbox = callPackage ../development/tools/devbox { };
+
   libcxx = llvmPackages.libcxx;
   libcxxabi = llvmPackages.libcxxabi;
 
