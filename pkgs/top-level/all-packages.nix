05a5700ae2856f1e6ef827d671a5dc1395acb525
Zane van Iperen <zane@zanevaniperen.com>
2022-11-07 16:47:58 +1000

    gst_all_1: use ffmpeg-headless
    gst-libav only uses ffmpeg for its (de)coders and (de)muxers. SDL,
    pipewire, and whatever is handled separately.
    
    Depend on ffmpeg-headless to reduce closure size.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19193,7 +19193,7 @@ with pkgs;
   gsettings-qt = libsForQt5.callPackage ../development/libraries/gsettings-qt { };
 
   gst_all_1 = recurseIntoAttrs(callPackage ../development/libraries/gstreamer {
-    callPackage = newScope (gst_all_1 // { libav = pkgs.ffmpeg; });
+    callPackage = newScope (gst_all_1 // { libav = pkgs.ffmpeg-headless; });
     inherit (darwin.apple_sdk.frameworks) AudioToolbox AVFoundation Cocoa CoreFoundation CoreMedia CoreServices CoreVideo DiskArbitration Foundation IOKit MediaToolbox OpenGL VideoToolbox;
   });
 
