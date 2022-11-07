c98fe77373995b8cc18ae14f50d03fc0ee13d8d6
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-07 17:22:02 +0100

    python310Packages.bleak-retry-connector: 2.8.2 -> 2.8.3

--- pkgs/development/python-modules/bleak-retry-connector/default.nix
+++ pkgs/development/python-modules/bleak-retry-connector/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "bleak-retry-connector";
-  version = "2.8.2";
+  version = "2.8.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-u/7gEY1HsOc2JqGmq/kS46wcA0p8B7D08vrOHWIuFyY=";
+    hash = "sha256-XBIBI/39l0JOJOX5GChTJ+Snwb9jjfofmM4cAdsV8lg=";
   };
 
   postPatch = ''
