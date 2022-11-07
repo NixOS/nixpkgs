dd909bc774a6a2466ccfabeb140f37c6d3f0ca45
Francesco Gazzetta <fgaz@fgaz.me>
2022-11-07 17:13:20 +0100

    dillong: init at unstable-2021-12-13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18551,6 +18551,8 @@ with pkgs;
     fltk = fltk13;
   };
 
+  dillong = callPackage ../applications/networking/browsers/dillong { };
+
   directfb = callPackage ../development/libraries/directfb { };
 
   discordchatexporter-cli = callPackage ../tools/backup/discordchatexporter-cli { };
