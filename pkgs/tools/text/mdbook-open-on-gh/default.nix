48043e4cb1bd78e3ddbb2660b9812bb3600948bc
Matthias Beyer <mail@beyermatthias.de>
2022-11-08 08:57:50 +0100

    mdbook-open-on-gh: 2.2.0 -> 2.3.0
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/tools/text/mdbook-open-on-gh/default.nix
+++ pkgs/tools/text/mdbook-open-on-gh/default.nix
@@ -1,26 +1,17 @@
-{ lib, rustPlatform, fetchFromGitHub, fetchpatch }:
+{ lib, rustPlatform, fetchFromGitHub }:
 
 rustPlatform.buildRustPackage rec {
   pname = "mdbook-open-on-gh";
-  version = "2.2.0";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "badboy";
     repo = pname;
-    rev = "2.2.0";
-    hash = "sha256-x7ESuXoF5dYnJZpgDyYliVixCG4w/VX/Vhm3VqxsiEI=";
+    rev = version;
+    hash = "sha256-omQTyJ7XKRBjX8jyWLONajAYnwr93nElrwDLdvs2MxM=";
   };
 
-  cargoPatches = [
-    # https://github.com/badboy/mdbook-open-on-gh/pull/7
-    (fetchpatch {
-      name = "update-mdbook-for-rust-1.64.patch";
-      url = "https://github.com/badboy/mdbook-open-on-gh/commit/bd20601bfcec144c9302b1ba1a1aff4b95b334d9.patch";
-      hash = "sha256-3Df9Q3sqCpZzqCN9fi+wdeWjLUW4XdywIS3QUjsDE9g=";
-    })
-  ];
-
-  cargoHash = "sha256-N0RwengTWk4luPIecIxzbFReGi+PtE77FJalPq1CdbA=";
+  cargoHash = "sha256-57KcqALWbiGtp6HWSN42gZ0St38oHu3inZ0TT77j7go=";
 
   meta = with lib; {
     description = "mdbook preprocessor to add a open-on-github link on every page";
