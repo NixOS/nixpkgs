c561e1d2b6205ff9b398794444b0e6111e598068
Sergei Trofimovich <slyich@gmail.com>
2022-11-08 08:02:33 +0000

    fwbuilder: disable blanket `-Werror`
    `-Werror` flag usually causes build failures due to minor changes in
    compiler versions. They might be useful for developers themselves but
    are rarely useful for distributions.
    
    This time the change fixes `fwbuilder` build against newer `libxml2`:
    
        libfwbuilder/src/fwbuilder/XMLTools.cpp: In static member function 'static void libfwbuilder::XMLTools::initXMLTools()':
        libfwbuilder/src/fwbuilder/XMLTools.cpp:222:18: error: 'int xmlInitMemory()' is deprecated [-Werror=deprecated-declarations]
          222 |     xmlInitMemory();
              |     ~~~~~~~~~~~~~^~
    
    ZFH: https://github.com/NixOS/nixpkgs/issues/199919

--- pkgs/tools/security/fwbuilder/default.nix
+++ pkgs/tools/security/fwbuilder/default.nix
@@ -20,6 +20,13 @@ stdenv.mkDerivation rec {
     hash = "sha256-j5HjGcIqq93Ca9OBqEgSotoSXyw+q6Fqxa3hKk1ctwQ=";
   };
 
+  postPatch = ''
+    # Avoid blanket -Werror as it triggers on any minor compiler
+    # warnings like deprecated functions or invalid indentat8ion.
+    # Leave fixing these problems to upstream.
+    substituteInPlace CMakeLists.txt --replace ';-Werror;' ';'
+  '';
+
   nativeBuildInputs = [
     cmake
     wrapQtAppsHook
@@ -29,10 +36,6 @@ stdenv.mkDerivation rec {
     wayland
     wayland-protocols
     qtwayland
-  ];
-
-  NIX_CFLAGS_COMPILE = [
-    "-Wno-error=misleading-indentation"
   ];
 
   meta = with lib; {
