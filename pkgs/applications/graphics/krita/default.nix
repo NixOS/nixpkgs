e71f343311e08d49d40811c59191cad1cc4d28ab
mingchuan <ming@culpring.com>
2022-11-07 21:03:38 -0800

    krita: 5.1.1 -> 5.1.3

--- pkgs/applications/graphics/krita/default.nix
+++ pkgs/applications/graphics/krita/default.nix
@@ -1,7 +1,7 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "5.1.1";
+  version = "5.1.3";
   kde-channel = "stable";
-  sha256 = "sha256-Tdv4l6+nsYcTFpfRKiO6OYlGOAaLLq4Ss7Q0/kKtjiQ=";
+  sha256 = "sha256-69+P0wMIciGxuc6tmWG1OospmvvwcZl6zHNQygEngo0=";
 })
