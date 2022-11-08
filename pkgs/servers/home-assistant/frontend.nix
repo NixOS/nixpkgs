16eda690a3474657d4c7bfb26c43c3cc3f066933
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-11-09 00:05:31 +0100

    home-assistant: 2022.11.1 -> 2022.11.2
    https://github.com/home-assistant/core/releases/tag/2022.11.2

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -4,7 +4,7 @@ buildPythonPackage rec {
   # the frontend version corresponding to a specific home-assistant version can be found here
   # https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/frontend/manifest.json
   pname = "home-assistant-frontend";
-  version = "20221102.1";
+  version = "20221108.0";
   format = "wheel";
 
   src = fetchPypi {
@@ -12,7 +12,7 @@ buildPythonPackage rec {
     pname = "home_assistant_frontend";
     dist = "py3";
     python = "py3";
-    sha256 = "sha256-HAFJxrBI6wB1XMe+71tF/bijuQAoyAw8LBQbfv+Vqco=";
+    sha256 = "sha256-jwDKWDI5fhrQNtEqMMWQZJFjjJYSqjNZFOVRsQvI9pY=";
   };
 
   # there is nothing to strip in this package
