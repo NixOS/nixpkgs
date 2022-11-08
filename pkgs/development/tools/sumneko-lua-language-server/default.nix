7c7be123192fad2073508ffe40d8de2138231ecd
figsoda <figsoda@pm.me>
2022-11-08 16:09:11 -0500

    sumneko-lua-language-server: 3.5.6 -> 3.6.1

--- pkgs/development/tools/sumneko-lua-language-server/default.nix
+++ pkgs/development/tools/sumneko-lua-language-server/default.nix
@@ -4,13 +4,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "sumneko-lua-language-server";
-  version = "3.5.6";
+  version = "3.6.1";
 
   src = fetchFromGitHub {
     owner = "sumneko";
     repo = "lua-language-server";
     rev = version;
-    sha256 = "sha256-S07/N6Cq/YG0kS2riPI8wy/fOxPHkMrGqpmUd+ymwJ0=";
+    sha256 = "sha256-jlx2Tn5NuHq7OFHPHOknZfLAbQanWLMTsw7wO1SJOkk=";
     fetchSubmodules = true;
   };
 
