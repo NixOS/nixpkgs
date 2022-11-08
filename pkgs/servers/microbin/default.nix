11e941860c42e3c900227f0a3da484a334574baa
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-08 01:12:31 +0000

    microbin: 1.1.0 -> 1.2.0

--- pkgs/servers/microbin/default.nix
+++ pkgs/servers/microbin/default.nix
@@ -5,15 +5,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "microbin";
-  version = "1.1.0";
+  version = "1.2.0";
 
   # GitHub sources do not have Cargo.lock
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-gfEO7Rrzc4KSnSXFrMmGLrTXuZIUCdumt2N429nHPi8=";
+    sha256 = "sha256-dZClslUTUchx+sOJzFG8wiAgyW/0RcCKfKYklKfVrzM=";
   };
 
-  cargoSha256 = "sha256-k/5CG8bf5RuO6K9mEj6seqV6AuWMqatBRDaSS0guhi0=";
+  cargoSha256 = "sha256-fBbChu5iy/2H/8IYCwd1OwxplGPZAmkd8z8xD7Uc0vo=";
 
   meta = with lib; {
     description = "A tiny, self-contained, configurable paste bin and URL shortener written in Rust";
