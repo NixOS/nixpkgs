eeaa0526a32383470a2160fa598237b3623a5d68
kyren <kyren@kyju.org>
2022-11-08 08:18:32 -0500

    aseprite-unfree: 1.2.16.3 -> 1.2.40
    Also updates aseprite skia fork and skia deps, includes the following
    patches from AUR:
    
    - shared-libwebp.patch
    - shared-skia-deps.patch

--- pkgs/applications/editors/aseprite/default.nix
+++ pkgs/applications/editors/aseprite/default.nix
@@ -1,7 +1,7 @@
-{ stdenv, lib, callPackage, fetchFromGitHub, fetchpatch, cmake, ninja, pkg-config
+{ stdenv, lib, callPackage, fetchFromGitHub, cmake, ninja, pkg-config
 , curl, freetype, giflib, libjpeg, libpng, libwebp, pixman, tinyxml, zlib
 , harfbuzzFull, glib, fontconfig, pcre
-, libX11, libXext, libXcursor, libXxf86vm, libGL
+, libX11, libXext, libXcursor, libXxf86vm, libGL, libXi
 , unfree ? false
 , cmark
 }:
@@ -15,16 +15,16 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "aseprite";
-  version = if unfree then "1.2.16.3" else "1.1.7";
+  version = if unfree then "1.2.40" else "1.1.7";
 
   src = fetchFromGitHub {
     owner = "aseprite";
     repo = "aseprite";
     rev = "v${version}";
     fetchSubmodules = true;
-    sha256 = if unfree
-      then "16yn7y9xdc5jd50cq7bmsm320gv23pp71lr8hg2nmynzc8ibyda8"
-      else "0gd49lns2bpzbkwax5jf9x1xmg1j8ij997kcxr2596cwiswnw4di";
+    hash = if unfree
+      then "sha256-KUdJA6HTAKrLT8xrwFikVDbc5RODysclcsEyQekMRZo="
+      else "sha256-sRFuuY6cmVRE7myelGREMrzaQ09Olq74XP8uoS1NpD0=";
   };
 
   nativeBuildInputs = [
@@ -37,24 +37,20 @@ stdenv.mkDerivation rec {
   ] ++ lib.optionals unfree [
     cmark
     harfbuzzFull glib fontconfig pcre
-    skia libGL
+    skia libGL libXi
   ];
 
-  patches = if !unfree then [
+  patches = lib.optionals (!unfree) [
     ./allegro-glibc-2.30.patch
-  ] else [
-    (fetchpatch {
-      url = "https://github.com/lfont/aseprite/commit/f1ebc47012d3fed52306ed5922787b4b98cc0a7b.patch";
-      sha256 = "03xg7x6b9iv7z18vzlqxhcfphmx4v3qhs9f5rgf38ppyklca5jyw";
-    })
-    (fetchpatch {
-      url = "https://github.com/orivej/aseprite/commit/ea87e65b357ad0bd65467af5529183b5a48a8c17.patch";
-      sha256 = "1vwn8ivap1pzdh444sdvvkndp55iz146nhmd80xbm8cyzn3qmg91";
-    })
+  ] ++ lib.optionals unfree [
+    ./shared-libwebp.patch
+    ./shared-skia-deps.patch
   ];
 
-  postPatch = ''
+  postPatch = if (!unfree) then ''
     sed -i src/config.h -e "s-\\(#define VERSION\\) .*-\\1 \"$version\"-"
+  '' else ''
+    sed -i src/ver/CMakeLists.txt -e "s-set(VERSION \".*\")-set(VERSION \"$version\")-"
   '';
 
   cmakeFlags = [
@@ -73,15 +69,17 @@ stdenv.mkDerivation rec {
   ] ++ lib.optionals unfree [
     "-DUSE_SHARED_CMARK=ON"
     "-DUSE_SHARED_HARFBUZZ=ON"
-    # Aseprite needs internal freetype headers.
-    "-DUSE_SHARED_FREETYPE=OFF"
+    "-DUSE_SHARED_WEBP=ON"
     # Disable libarchive programs.
     "-DENABLE_CAT=OFF"
     "-DENABLE_CPIO=OFF"
     "-DENABLE_TAR=OFF"
     # UI backend.
+    "-DLAF_WITH_EXAMPLES=OFF"
     "-DLAF_OS_BACKEND=skia"
+    "-DENABLE_DESKTOP_INTEGRATION=ON"
     "-DSKIA_DIR=${skia}"
+    "-DSKIA_LIBRARY_DIR=${skia}/out/Release"
   ];
 
   postInstall = ''
