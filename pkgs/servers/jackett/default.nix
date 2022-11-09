071f1f9db58a1b1030ff64e175d14f7e1901d21f
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-09 18:53:24 +0000

    jackett: 0.20.2175 -> 0.20.2225

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -9,13 +9,13 @@
 
 buildDotnetModule rec {
   pname = "jackett";
-  version = "0.20.2175";
+  version = "0.20.2225";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "MkMEsD8hU23RPvLJvEN2IT5BiuE1ySuVRLmPK2Yqsa0=";
+    sha256 = "HrI0B5gvN/UC9TmErwsmRc85aqCXfYYEQfPkRchHY5Q=";
   };
 
   projectFile = "src/Jackett.Server/Jackett.Server.csproj";
