e2507e1176f1daf8e9b46b268ee58f1cbef31246
Tom Houlé <tom@tomhoule.com>
2022-11-09 12:05:04 +0100

    nodePackages.prisma: 4.5.0 -> 4.6.0

--- pkgs/development/node-packages/overrides.nix
+++ pkgs/development/node-packages/overrides.nix
@@ -414,7 +414,7 @@ final: prev: {
 
     src = fetchurl {
       url = "https://registry.npmjs.org/prisma/-/prisma-${version}.tgz";
-      sha512 = "sha512-9Aeg4qiKlv9Wsjz4NO8k2CzRzlvS3A4FYVJ5+28sBBZ0eEwbiVOE/Jj7v6rZC1tFW2s4GSICQOAyuOjc6WsNew==";
+      sha512 = "sha512-TAnObUMGCM9NLt9nsRs1WWYQGPKsJOK8bN/7gSAnBcYIxMCFFDe+XtFYJbyTzsJZ/i+0rH4zg8au3m7HX354LA==";
     };
     postInstall = with pkgs; ''
       wrapProgram "$out/bin/prisma" \
