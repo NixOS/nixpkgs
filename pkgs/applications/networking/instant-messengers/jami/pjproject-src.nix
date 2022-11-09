eb8acbbcf92cfb111abf1db6f4ff2f0abe3ac015
linsui <linsui555@gmail.com>
2022-11-09 18:41:31 +0800

    [22.05] Backport pjsip CVE fix for jami

--- pkgs/applications/networking/instant-messengers/jami/pjproject-src.nix
+++ pkgs/applications/networking/instant-messengers/jami/pjproject-src.nix
@@ -1,6 +1,6 @@
 {
   owner = "savoirfairelinux";
   repo = "pjproject";
-  rev = "e1f389d0b905011e0cb62cbdf7a8b37fc1bcde1a";
-  sha256 = "0inpmyb6mhrzr0g309d6clkc99lddqdvyf9xajz0igvgp9pvgpza";
+  rev = "513a3f14c44b2c2652f9219ec20dea64b236b713";
+  hash = "sha256-93AlJGMnlzJMrJquelpHQQKjhEgfpTFXTMqkBnm87u8=";
 }
