ecdeb57705582057a950373790aa7e3e5e49d6ad
José Romildo <malaquias@gmail.com>
2022-11-09 10:11:09 -0300

    lxqt.libqtxdg: 3.9.1 -> 3.10.0

--- pkgs/desktops/lxqt/libqtxdg/default.nix
+++ pkgs/desktops/lxqt/libqtxdg/default.nix
@@ -10,13 +10,13 @@
 
 mkDerivation rec {
   pname = "libqtxdg";
-  version = "3.9.1";
+  version = "3.10.0";
 
   src = fetchFromGitHub {
     owner = "lxqt";
     repo = pname;
     rev = version;
-    sha256 = "zrlaOiIsfbwjHFjqhYZ9lCo+oTsddICxl2UAum9Xoi4=";
+    sha256 = "Lynm6Qxy02Os69YQ1cb2W0hV7sq9kBhbACqjHTGj7Tw=";
   };
 
   nativeBuildInputs = [
