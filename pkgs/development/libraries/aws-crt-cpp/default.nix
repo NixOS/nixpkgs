34c647c0519a4b16441eb44ef1d6918069e8b9e6
Tobias Mayer <tobim@fastmail.fm>
2022-11-09 22:11:20 +0100

    aws-crt-cpp: 0.18.9 -> 0.18.13

--- pkgs/development/libraries/aws-crt-cpp/default.nix
+++ pkgs/development/libraries/aws-crt-cpp/default.nix
@@ -17,7 +17,7 @@
 
 stdenv.mkDerivation rec {
   pname = "aws-crt-cpp";
-  version = "0.18.9";
+  version = "0.18.13";
 
   outputs = [ "out" "dev" ];
 
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
     owner = "awslabs";
     repo = "aws-crt-cpp";
     rev = "v${version}";
-    sha256 = "sha256-NEsEKUKmADevb8SSc8EFuXLc12fuOf6fXI76yVeDQno=";
+    hash = "sha256-tvPywzpDwJJ9q6KcKicGkIHEBLGMtdrXvbPcNPC6snk=";
   };
 
   patches = [
