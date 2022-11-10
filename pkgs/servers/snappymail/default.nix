2921158cb25a59ed7570e2029aa82f744c4af46c
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-10 11:37:06 +0000

    snappymail: 2.19.4 -> 2.20.6

--- pkgs/servers/snappymail/default.nix
+++ pkgs/servers/snappymail/default.nix
@@ -2,11 +2,11 @@
 , dataPath ? "/var/lib/snappymail" }:
 stdenv.mkDerivation rec {
   pname = "snappymail";
-  version = "2.19.4";
+  version = "2.20.6";
 
   src = fetchurl {
     url = "https://github.com/the-djmaze/snappymail/releases/download/v${version}/snappymail-${version}.tar.gz";
-    sha256 = "sha256-XjnNBK/w9aPfG11fVcESIMy1UFAI1j/iAKz1GvZzfq4=";
+    sha256 = "sha256-xALHgE+N/X2YqGd3qZxAiZF1onzO/FVfxSbwjAaep/A=";
   };
 
   sourceRoot = "snappymail";
