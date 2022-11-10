9d14bfb7d5f4a58c763cf811c3ba51508f636f4b
Gabriel Fontes <hi@m7.rs>
2022-11-10 20:50:16 -0300

    apgdiff: init at 2.7.0
    Co-authored by: AndersonTorres <torres.anderson.85@protonmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16203,6 +16203,8 @@ with pkgs;
 
   anybadge = with python3Packages; toPythonApplication anybadge;
 
+  apgdiff = callPackage ../development/tools/database/apgdiff { };
+
   apkg = callPackage ../tools/package-management/apkg { };
 
   augeas = callPackage ../tools/system/augeas { };
