11c77697bfc2cac9fdcfe9e8f7ff37fbc1df9abd
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-10 09:10:30 +0100

    python310Packages.meshtastic: 2.0.2 -> 2.0.3

--- pkgs/development/python-modules/meshtastic/default.nix
+++ pkgs/development/python-modules/meshtastic/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "meshtastic";
-  version = "2.0.2";
+  version = "2.0.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "meshtastic";
     repo = "Meshtastic-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-Z2/nzsHRpaykqLRkJoPdNGSLNbJvd4GOUpTYl8MG8mw=";
+    hash = "sha256-h8OuDmm9I8lElhAGSpPx8sPUTY+EnFp2VXOYrYjiYNk=";
   };
 
   propagatedBuildInputs = [
