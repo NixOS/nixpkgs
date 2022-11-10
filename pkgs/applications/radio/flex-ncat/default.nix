69d4f8a39d717726cfb4ded076114499c963e523
Mikael Voss <mvs@nyantec.com>
2022-11-10 21:42:02 +0100

    flex-ncat: 0.1-20221007.1 -> 0.1-20221109.1
    https://github.com/kc2g-flex-tools/nCAT/releases/tag/v0.1-20221109.1

--- pkgs/applications/radio/flex-ncat/default.nix
+++ pkgs/applications/radio/flex-ncat/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "flex-ncat";
-  version = "0.1-20221007.1";
+  version = "0.1-20221109.1";
 
   src = fetchFromGitHub {
     owner = "kc2g-flex-tools";
     repo = "nCAT";
     rev = "v${version}";
-    hash = "sha256-9rxI3wsqjhaH7DD1Go/8s0r6jXaE15Z9PPtbsnsfrM0=";
+    hash = "sha256-MlbzPZuEOhb3wJMXWkrt6DK8z0MPgznSm0N9Y6vJVWY=";
   };
 
   vendorSha256 = "sha256-lnJtFixgv4ke4Knavb+XKFPzHCiAPhNtfZS3SRVvY2g=";
