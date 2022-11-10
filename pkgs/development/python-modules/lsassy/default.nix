210f2c858c25fe62956710c016b267b3173e8bf3
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-10 09:17:31 +0100

    python310Packages.lsassy: 3.1.3 -> 3.1.4

--- pkgs/development/python-modules/lsassy/default.nix
+++ pkgs/development/python-modules/lsassy/default.nix
@@ -3,19 +3,23 @@
 , fetchFromGitHub
 , impacket
 , netaddr
+, pythonOlder
 , pypykatz
 , rich
 }:
 
 buildPythonPackage rec {
   pname = "lsassy";
-  version = "3.1.3";
+  version = "3.1.4";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "Hackndo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-h99wqgRNy2GkiertDddFJQN4hv2R26bo72zNwLPR3mk=";
+    hash = "sha256-/YDBzH8Mji6PWBEIbsgZcTvoAWAsBbJV3cHfySY/gGA=";
   };
 
   propagatedBuildInputs = [
