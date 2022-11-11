4051827e9ee8c6cc72399f2637b68ce1f5e5cb06
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-11 08:38:51 +0100

    python310Packages.types-requests: 2.28.11.3 -> 2.28.11.4

--- pkgs/development/python-modules/types-requests/default.nix
+++ pkgs/development/python-modules/types-requests/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "types-requests";
-  version = "2.28.11.3";
+  version = "2.28.11.4";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-vpAHr/sX8KCbtI532xi7bgvp+CYJx48drm2zAlnXIdY=";
+    sha256 = "sha256-1PNCsN9DImLp4ybRdjjurpaliB5456aq5G0zhw1zlS4=";
   };
 
   propagatedBuildInputs = [
