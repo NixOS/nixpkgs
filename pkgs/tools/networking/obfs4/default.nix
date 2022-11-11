b4622ed855b2168a6526a52eb1d94f4e98bc7078
Adam Joseph <adam@westernsemico.com>
2022-11-11 12:01:44 -0800

    obfs4: add meta fields
    This commit adds these meta fields:
    
    - license
    - changelog
    - downloadPage
    - longDescription from openwrt https://openwrt.org/packages/pkgdata/obfs4proxy
    
    It also improves the `meta.description` field to be a bit more descriptive.

--- pkgs/tools/networking/obfs4/default.nix
+++ pkgs/tools/networking/obfs4/default.nix
@@ -13,9 +13,23 @@ buildGoModule rec {
   vendorSha256 = "0yjanv5piygffpdfysviijl7cql2k0r05bsxnlj4hbamsriz9xqy";
 
   meta = with lib; {
-    description = "A pluggable transport proxy";
+    description = "Circumvents censorship by transforming Tor traffic between clients and bridges";
+    longDescription = ''
+      Obfs4proxy is a tool that attempts to circumvent censorship by
+      transforming the Tor traffic between the client and the bridge.
+      This way censors, who usually monitor traffic between the client
+      and the bridge, will see innocent-looking transformed traffic
+      instead of the actual Tor traffic.  obfs4proxy implements the
+      obfsucation protocols obfs2, obfs3, and obfs4.  It is written in
+      Go and is compliant with the Tor pluggable transports
+      specification, and its modular architecture allows it to support
+      multiple pluggable transports.
+    '';
     homepage = "https://www.torproject.org/projects/obfsproxy";
     maintainers = with maintainers; [ thoughtpolice ];
     mainProgram = "obfs4proxy";
+    changelog = "https://gitweb.torproject.org/pluggable-transports/obfs4.git/plain/ChangeLog";
+    downloadPage = "https://gitweb.torproject.org/pluggable-transports/obfs4.git/";
+    license = with lib.licenses; [ bsd2 bsd3 gpl3 ];
   };
 }
