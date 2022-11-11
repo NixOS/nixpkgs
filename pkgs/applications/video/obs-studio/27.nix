58e5e0a39b5788c3947c6ddd4d8f11fc95906fe2
Franz Pletz <fpletz@fnordicwalking.de>
2022-11-11 15:28:56 +0100

    obs-studio27: remove

--- pkgs/applications/video/obs-studio/27.nix
+++
@@ -1,7 +0,0 @@
-{ callPackage, qtx11extras, ... } @ args:
-
-callPackage ./generic.nix (args // {
-  version = "27.2.4";
-  sha256 = "sha256-OiSejQovSmhItrnrQlcVp9PCDRgAhuxTinSpXbH8bo0=";
-  extraBuildInputs = [ qtx11extras ];
-})
