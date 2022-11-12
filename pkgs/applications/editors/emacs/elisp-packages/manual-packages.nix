c47cbf3acaff10179b418c216bb5e6e1cd63c2c4
AndersonTorres <torres.anderson.85@protonmail.com>
2022-11-12 15:21:12 -0300

    emacs.pkgs.prisma-mode: move to a dedicated directory
    There is no tangible reason to pollute a file like manual-packages with full
    descriptions of expressions. `callPackage` exists for a reason :)

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages.nix
@@ -10,34 +10,7 @@ self: with self; {
 
   matrix-client = callPackage ./matrix-client { _map = self.map; };
 
-  prisma-mode = let
-    rev = "5283ca7403bcb21ca0cac8ecb063600752dfd9d4";
-  in melpaBuild {
-    pname = "prisma-mode";
-    version = "20211207.0";
-
-    commit = rev;
-
-    packageRequires = [ js2-mode ];
-
-    src = pkgs.fetchFromGitHub {
-      owner = "pimeys";
-      repo = "emacs-prisma-mode";
-      inherit rev;
-      sha256 = "sha256-DJJfjbu27Gi7Nzsa1cdi8nIQowKH8ZxgQBwfXLB0Q/I=";
-    };
-
-    recipe = pkgs.writeText "recipe" ''
-      (prisma-mode
-      :repo "pimeys/emacs-prisma-mode"
-      :fetcher github)
-    '';
-
-    meta = {
-      description = "Major mode for Prisma Schema Language";
-      license = lib.licenses.gpl2Only;
-    };
-  };
+  prisma-mode = callPackage ./prisma-mode { };
 
   agda-input = callPackage ./agda-input { };
 
