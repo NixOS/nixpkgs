41d312d2d4245a2792ecf3f290800d2e7cde0801
Jonas Heinrich <onny@project-insanity.org>
2022-11-12 17:34:26 +0100

    nc4nix: unstable-2022-08-06 -> unstable-2022-11-12

--- pkgs/development/tools/nc4nix/default.nix
+++ pkgs/development/tools/nc4nix/default.nix
@@ -5,21 +5,21 @@
 
 buildGoModule rec {
   pname = "nc4nix";
-  version = "unstable-2022-08-06";
+  version = "unstable-2022-11-12";
 
   src = fetchFromGitLab {
-    domain = "git.helsinki.tools";
-    owner = "helsinki-systems";
+    domain = "git.project-insanity.org";
+    owner = "onny";
     repo = "nc4nix";
-    rev = "91d92e8c339862fe81fb066fd370da7757042367";
-    sha256 = "sha256-8ggYOc+w3oiY2B0Ned5B26/ZNco2vCdvScC+Ms+gOWo=";
+    rev = "d7f55a42b5ca0d02382349c6ec776eefe6079703";
+    sha256 = "sha256-MfMW2B+udXV/lQPGUBFuAE7jwmy4D1M+CYkCuJ088aM=";
   };
 
   vendorSha256 = "sha256-uhINWxFny/OY7M2vV3ehFzP90J6Z8cn5IZHWOuEg91M=";
 
   meta = with lib; {
     description = "Packaging helper for Nextcloud apps";
-    homepage = "https://git.helsinki.tools/helsinki-systems/nc4nix";
+    homepage = "https://git.project-insanity.org/onny/nc4nix";
     license = licenses.unfree;
     maintainers = with maintainers; [ onny ];
     platforms = platforms.linux;
