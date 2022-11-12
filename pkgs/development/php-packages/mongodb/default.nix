7a8708d57f7cb1dff788c1bfa5265eae1bd94828
Elis Hirwing <elis@hirwing.se>
2022-11-12 12:39:35 +0100

    php.extensions.mongodb: 1.14.1 -> 1.14.2

--- pkgs/development/php-packages/mongodb/default.nix
+++ pkgs/development/php-packages/mongodb/default.nix
@@ -14,8 +14,8 @@
 buildPecl {
   pname = "mongodb";
 
-  version = "1.14.1";
-  sha256 = "sha256-uuKodv3ME9bNATn7Wyrv1RydXISmpPxTvr2+PxYtg14=";
+  version = "1.14.2";
+  sha256 = "1amayawrkyl1f44b8qwvm9567nxxfpv12pkn65adgjbwk5ds6z3g";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [
