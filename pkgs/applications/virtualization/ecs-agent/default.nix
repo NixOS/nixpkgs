80771ac66020b3ec304e5843731ca216488b201c
Mario Rodas <marsam@users.noreply.github.com>
2022-11-12 05:10:00 +0000

    ecs-agent: add ldflags

--- pkgs/applications/virtualization/ecs-agent/default.nix
+++ pkgs/applications/virtualization/ecs-agent/default.nix
@@ -14,9 +14,12 @@ buildGoPackage rec {
     sha256 = "sha256-mZzDvD+40YiC8cBpLlYd7K1p5UBYpso9OMCDijopuik=";
   };
 
+  ldflags = [ "-s" "-w" ];
+
   meta = with lib; {
     description = "The agent that runs on AWS EC2 container instances and starts containers on behalf of Amazon ECS";
     homepage    = "https://github.com/aws/amazon-ecs-agent";
+    changelog   = "https://github.com/aws/amazon-ecs-agent/raw/v${version}/CHANGELOG.md";
     license     = licenses.asl20;
     platforms   = platforms.unix;
     maintainers = with maintainers; [ copumpkin ];
