4e858129bdd55d36479d8b27e5be4edff763f357
Mario Rodas <marsam@users.noreply.github.com>
2022-11-12 08:04:00 +0000

    python310Packages.colored: add format and pythonImportsCheck

--- pkgs/development/python-modules/colored/default.nix
+++ pkgs/development/python-modules/colored/default.nix
@@ -1,19 +1,28 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, nose
 }:
 
 buildPythonPackage rec {
   pname = "colored";
   version = "1.4.4";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-BP9NTdUUJ0/juZohu1L7lvJojAHpP7p77zciHny1bOA=";
   };
 
-  # No proper test suite
-  doCheck = false;
+  checkInputs = [ nose ];
+
+  checkPhase = ''
+    nosetests
+  '';
+
+  pythonImportsCheck = [
+    "colored"
+  ];
 
   meta = with lib; {
     homepage = "https://gitlab.com/dslackw/colored";
