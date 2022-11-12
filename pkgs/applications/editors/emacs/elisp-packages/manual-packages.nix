457efcd3520907e5d1a304632fd308558359958e
AndersonTorres <torres.anderson.85@protonmail.com>
2022-11-12 11:25:08 -0300

    emacs.pkgs.elisp-ffi: move to a dedicated directory
    There is no tangible reason to pollute a file like manual-packages with full
    descriptions of expressions. `callPackage` exists for a reason :)

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages.nix
@@ -2,47 +2,7 @@
 
 self: with self; {
 
-  elisp-ffi = let
-    rev = "da37c516a0e59bdce63fb2dc006a231dee62a1d9";
-  in melpaBuild {
-    pname = "elisp-ffi";
-    version = "20170518.0";
-
-    commit = rev;
-
-    src = pkgs.fetchFromGitHub {
-      owner = "skeeto";
-      repo = "elisp-ffi";
-      inherit rev;
-      sha256 = "sha256-StOezQEnNTjRmjY02ub5FRh59aL6gWfw+qgboz0wF94=";
-    };
-
-    nativeBuildInputs = [ pkgs.pkg-config ];
-
-    buildInputs = [ pkgs.libffi ];
-
-    preBuild = ''
-      mv ffi.el elisp-ffi.el
-      make
-    '';
-
-    recipe = pkgs.writeText "recipe" ''
-      (elisp-ffi
-      :repo "skeeto/elisp-ffi"
-      :fetcher github)
-    '';
-
-    meta = {
-      description = "Emacs Lisp Foreign Function Interface";
-      longDescription = ''
-        This library provides an FFI for Emacs Lisp so that Emacs
-        programs can invoke functions in native libraries. It works by
-        driving a subprocess to do the heavy lifting, passing result
-        values on to Emacs.
-      '';
-      license = lib.licenses.publicDomain;
-    };
-  };
+  elisp-ffi = callPackage ./elisp-ffi { };
 
   ghc-mod = melpaBuild {
     pname = "ghc";
