10a33afb0cfa9dac53347400bd7c61738e2d2017
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-13 03:37:09 +0000

    python310Packages.google-cloud-pubsub: 2.13.10 -> 2.13.11

--- pkgs/development/python-modules/google-cloud-pubsub/default.nix
+++ pkgs/development/python-modules/google-cloud-pubsub/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-pubsub";
-  version = "2.13.10";
+  version = "2.13.11";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-PnRqGpfJa9xj/FSZUp7XJi8UZPbejIuuYjjBIgCRmsA=";
+    hash = "sha256-CFh47PuzmR/qavOgEaot2fVBXRnHYNDemlhSTSqhut4=";
   };
 
   propagatedBuildInputs = [
