90333b74e2662f3fe32cbd47398a2b140d7a8573
AndersonTorres <torres.anderson.85@protonmail.com>
2022-11-13 20:03:19 -0300

    harfbuzz: rewrite mesonFeatureFlag
    This is merely cosmetical.

--- pkgs/development/libraries/harfbuzz/default.nix
+++ pkgs/development/libraries/harfbuzz/default.nix
@@ -32,8 +32,8 @@
 let
   version = "5.2.0";
   inherit (lib) optional optionals optionalString;
-  mesonFeatureFlag = opt: b:
-    "-D${opt}=${if b then "enabled" else "disabled"}";
+  mesonFeatureFlag = feature: flag:
+    "-D${feature}=${if flag then "enabled" else "disabled"}";
 in
 
 stdenv.mkDerivation {
