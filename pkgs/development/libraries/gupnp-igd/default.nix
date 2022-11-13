6f30ca3f9b2590b69883e3c80a530f61ace97795
Yureka <yuka@yuka.dev>
2022-11-13 15:24:14 +0100

    gupnp-idg: patch for gupnp 1.6, libsoup 3.x compat

--- pkgs/development/libraries/gupnp-igd/default.nix
+++ pkgs/development/libraries/gupnp-igd/default.nix
@@ -1,5 +1,6 @@
 { lib, stdenv
 , fetchurl
+, fetchpatch
 , pkg-config
 , meson
 , ninja
@@ -24,6 +25,18 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "sha256-S1EgCYqhPt0ngYup7k1/6WG/VAv1DQVv9wPGFUXgK+E=";
   };
+
+  patches = [
+    # both patches are unreleased commits for gupnp 1.6.x / libsoup 3.x compat
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gupnp-igd/-/commit/649b7100339c57531a8e31f69220f8e17f0860e0.diff";
+      sha256 = "sha256-5YxpClasT5ZUb4uKkN0nHxTColwcsSSKX6zdxjJZmn4=";
+    })
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gupnp-igd/-/commit/79a1e4cf8c256132978a1d8ab718c8ad132386de.diff";
+      sha256 = "sha256-1TFq+s5T7Ymq8g7gcbpMSHsBuncbl1JU4waOgk4mq/0=";
+    })
+  ];
 
   depsBuildBuild = [
     pkg-config
