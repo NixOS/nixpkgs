65309a9622979b00477451c6d77cd3823579559a
sternenseemann <sternenseemann@systemli.org>
2022-11-13 14:14:31 +0100

    gnatcoll-{lzma,gmp,iconv,omp,python3,readline,syslog,zlib}: 22.0.0 -> 23.0.0

--- pkgs/development/libraries/ada/gnatcoll/bindings.nix
+++ pkgs/development/libraries/ada/gnatcoll/bindings.nix
@@ -33,18 +33,14 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gnatcoll-${component}";
-  version = "22.0.0";
+  version = "23.0.0";
 
   src = fetchFromGitHub {
     owner = "AdaCore";
     repo = "gnatcoll-bindings";
     rev = "v${version}";
-    sha256 = "0wbwnd6jccwfd4jdxbnzhc0jhm8ad4phz6y9b1gk8adykkk6jcz4";
+    sha256 = "1jnnfsvll4jh6ip0fww4mh2cm61h7dzpxz3zaa2psrc1w54x34nn";
   };
-
-  patches = [
-    ./omp-setup-text-mode.patch
-  ];
 
   nativeBuildInputs = [
     gprbuild
