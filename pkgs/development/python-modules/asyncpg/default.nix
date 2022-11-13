2f34c88e130945e319a1fad5baaf36639d5159ee
Aneesh Agrawal <aneeshusa@gmail.com>
2022-11-13 17:24:58 -0500

    python310Packages.asyncpg: 0.26.0 -> 0.27.0 (#199918)
    This is the first release to support PostgreSQL 15 (and Python 3.11).
    It also drops support for Python 3.6, so bump the minimum Python to 3.7.

--- pkgs/development/python-modules/asyncpg/default.nix
+++ pkgs/development/python-modules/asyncpg/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "asyncpg";
-  version = "0.26.0";
+  version = "0.27.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-d+aEok/uF7o+SHypgtAlntF7rhr2gAb0zyhLI7og6iw=";
+    hash = "sha256-cgmG2aRwXdikD98XIDb1rnhyJQNqfrRucExFqo9iwFQ=";
   };
 
   # sandboxing issues on aarch64-darwin, see https://github.com/NixOS/nixpkgs/issues/198495
