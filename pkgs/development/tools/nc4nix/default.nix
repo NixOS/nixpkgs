88e0973f46ad4f000049b42f42104e11b5d01afb
Jonas Heinrich <onny@project-insanity.org>
2022-11-13 17:08:43 +0100

    nc4nix: unstable-2022-11-12 -> unstable-2022-11-13

--- pkgs/development/tools/nc4nix/default.nix
+++ pkgs/development/tools/nc4nix/default.nix
@@ -1,21 +1,33 @@
 { lib
 , buildGoModule
 , fetchFromGitLab
+, nix
+, makeWrapper
 }:
 
 buildGoModule rec {
   pname = "nc4nix";
-  version = "unstable-2022-11-12";
+  version = "unstable-2022-11-13";
 
   src = fetchFromGitLab {
     domain = "git.project-insanity.org";
     owner = "onny";
     repo = "nc4nix";
-    rev = "d7f55a42b5ca0d02382349c6ec776eefe6079703";
-    sha256 = "sha256-MfMW2B+udXV/lQPGUBFuAE7jwmy4D1M+CYkCuJ088aM=";
+    rev = "857e789287692e42f3fcaae039d6f323b383543b";
+    sha256 = "sha256-ekuvqTyoaYiNju4yiQLPmxaXaGD4T3Wv9A8CHY1MZOI=";
   };
 
   vendorSha256 = "sha256-uhINWxFny/OY7M2vV3ehFzP90J6Z8cn5IZHWOuEg91M=";
+
+ nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  postInstall = ''
+    # Depends on nix-prefetch-url
+    wrapProgram $out/bin/nc4nix \
+      --prefix PATH : ${lib.makeBinPath [ nix ]}
+  '';
 
   meta = with lib; {
     description = "Packaging helper for Nextcloud apps";
