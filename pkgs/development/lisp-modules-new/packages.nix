2f103f2b1f4a9cd20fe00e331f1177cf31086f26
Kasper Gałkowski <k@galkowski.xyz>
2022-11-13 13:34:39 +0100

    lispPackages_new: Fix patching without build-with-compile-into-pwd
    Add patch for cffi-libffi on Darwin

--- pkgs/development/lisp-modules-new/packages.nix
+++ pkgs/development/lisp-modules-new/packages.nix
@@ -102,6 +102,7 @@ let
       url = "https://github.com/cffi/cffi/archive/3f842b92ef808900bf20dae92c2d74232c2f6d3a.tar.gz";
       sha256 = "1jilvmbbfrmb23j07lwmkbffc6r35wnvas5s4zjc84i856ccclm2";
     };
+    patches = [ ./patches/cffi-libffi-darwin-ffi-h.patch ];
   });
 
   cl-unicode = build-with-compile-into-pwd {
