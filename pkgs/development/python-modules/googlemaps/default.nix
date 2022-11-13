900abacf7d2726040eb268840405d8e6b55beac3
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-13 03:53:14 +0000

    python310Packages.googlemaps: 4.6.0 -> 4.7.0

--- pkgs/development/python-modules/googlemaps/default.nix
+++ pkgs/development/python-modules/googlemaps/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "googlemaps";
-  version = "4.6.0";
+  version = "4.7.0";
   disabled = pythonOlder "3.5";
 
   src = fetchFromGitHub {
     owner = "googlemaps";
     repo = "google-maps-services-python";
-    rev = "v${version}";
-    sha256 = "sha256-pzCM1uZupqJgoogwacuuy1P8I9LF65w7ZS6vY10VgeU=";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-qn98b7oTU9/u0+EJ4OTOksLquJiWl/od9m498UuFiwo=";
   };
 
   propagatedBuildInputs = [ requests ];
