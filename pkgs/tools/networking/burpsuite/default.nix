6821c5f47d2ae81eea008404ede97c22191bf2d8
Thomas Boerger <thomas@webhippie.de>
2022-11-13 21:45:18 +0100

    burpsuite: add desktop item and icon

--- pkgs/tools/networking/burpsuite/default.nix
+++ pkgs/tools/networking/burpsuite/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, jdk11, runtimeShell, unzip, chromium }:
+{ lib, stdenv, fetchurl, makeDesktopItem, jdk11, runtimeShell, unzip, chromium }:
 
 stdenv.mkDerivation rec {
   pname = "burpsuite";
@@ -13,6 +13,15 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-BLX/SgHctXciOZoA6Eh4zuDJoxNSZgvoj2Teg1fV80g=";
   };
 
+  desktopItem = makeDesktopItem rec {
+    name = pname;
+    exec = pname;
+    icon = pname;
+    desktopName = "Burp Suite Community Edition";
+    comment = meta.description;
+    categories = [ "Development" "Security" "System" ];
+  };
+
   dontUnpack = true;
   dontBuild = true;
   installPhase = ''
@@ -25,6 +34,9 @@ stdenv.mkDerivation rec {
     ln -sf "${chromium}/bin/chromium" "$HOME/.BurpSuite/burpbrowser/$linux64/chrome"
     exec ${jdk11}/bin/java -jar ${src} "$@"' > $out/bin/burpsuite
     chmod +x $out/bin/burpsuite
+
+    install -Dm644 ${./icon.png} $out/share/pixmaps/${pname}.png
+    cp -r ${desktopItem}/share/applications $out/share
 
     runHook postInstall
   '';
