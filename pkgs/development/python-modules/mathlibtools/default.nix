85505200705f5a974969be5e6f8f0da1de30d4a8
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-13 09:59:29 +0000

    python310Packages.mathlibtools: 1.1.2 -> 1.3.0

--- pkgs/development/python-modules/mathlibtools/default.nix
+++ pkgs/development/python-modules/mathlibtools/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "mathlibtools";
-  version = "1.1.2";
+  version = "1.3.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Jbnb3FKyB1NAehB8tZxBV6d7JJCOgWZPMWMaFEAOzkM=";
+    hash = "sha256-iBYSh5Y8MYqzjeNt70eURr40SSKh0x41plemeaaOfy8=";
   };
 
   propagatedBuildInputs = [
