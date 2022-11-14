792716b1ae279dccd4730fa474caf4ca4a2437c9
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-14 07:53:53 +0000

    cargo-auditable: 0.5.2 -> 0.5.3

--- pkgs/development/tools/rust/cargo-auditable/default.nix
+++ pkgs/development/tools/rust/cargo-auditable/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-auditable";
-  version = "0.5.2";
+  version = "0.5.3";
 
   src = fetchFromGitHub {
     owner = "rust-secure-code";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-4CHuthi7GXZKHenOE2Bk+Ps1AJlPkhvMIGHmV9Z00hA=";
+    sha256 = "sha256-bRx+a6vzjiS2dglVkTyUm8OASM2Qq05S7LuVkHaIYMU=";
   };
 
-  cargoSha256 = "sha256-puq8BgYuynFZCepYZdQ9ggDYJlFDks7s/l3UxM9F7ag=";
+  cargoSha256 = "sha256-QZi45U+MV8h4AMcN3QLIfAn/gHzoBWuOsC7gDSBY2jI=";
 
   meta = with lib; {
     description = "A tool to make production Rust binaries auditable";
