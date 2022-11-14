52ea73876b4e705657f549a081d2ff073ebbe94c
Logan Glasson <logan@glasson.nz>
2022-11-14 18:29:46 +1300

    quisk: 4.1.73 -> 4.2.12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31573,7 +31573,7 @@ with pkgs;
 
   quirc = callPackage ../tools/graphics/quirc {};
 
-  quisk = python38Packages.callPackage ../applications/radio/quisk { };
+  quisk = python39Packages.callPackage ../applications/radio/quisk { };
 
   quiterss = libsForQt514.callPackage ../applications/networking/newsreaders/quiterss {};
 
