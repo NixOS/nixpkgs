2df0ca53e9c0be5721f81068bac085724fa3cb4b
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-14 05:33:16 +0000

    awscli2: 2.8.11 -> 2.8.12

--- pkgs/tools/admin/awscli2/default.nix
+++ pkgs/tools/admin/awscli2/default.nix
@@ -32,14 +32,14 @@ let
 in
 with py.pkgs; buildPythonApplication rec {
   pname = "awscli2";
-  version = "2.8.11"; # N.B: if you change this, check if overrides are still up-to-date
+  version = "2.8.12"; # N.B: if you change this, check if overrides are still up-to-date
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = "aws-cli";
     rev = version;
-    sha256 = "sha256-E4O4DEdPwfIt+XOiCF2gLZILYTfZ4BhXMUgKggtsoCc=";
+    sha256 = "sha256-OeOSSB0WgVJEszmkXmMkmJNq37sPID7HFaTbXkBUwlI=";
   };
 
   nativeBuildInputs = [
