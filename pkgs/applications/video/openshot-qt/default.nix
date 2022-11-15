b974455136da5be2189b000438257cb64e1d4210
Austin Butler <austinabutler@gmail.com>
2022-11-15 00:04:27 -0800

    openshot-qt: fix Python 3.10 incompatibility

--- pkgs/applications/video/openshot-qt/default.nix
+++ pkgs/applications/video/openshot-qt/default.nix
@@ -2,6 +2,7 @@
 , stdenv
 , mkDerivationWith
 , fetchFromGitHub
+, fetchpatch
 , doxygen
 , gtk3
 , libopenshot
@@ -46,6 +47,22 @@ mkDerivationWith python3Packages.buildPythonApplication rec {
     # tries to create caching directories during install
     export HOME=$(mktemp -d)
   '';
+
+  # Following Arch PKGBUILD for Python 3.10 compatibility
+  # https://github.com/OpenShot/openshot-qt/issues/4608
+  # https://github.com/archlinux/svntogit-community/commits/packages/openshot/trunk
+  patches = [
+    (fetchpatch {
+      name = "video-widget.patch";
+      url = "https://github.com/OpenShot/openshot-qt/commit/9748a13268d66a5949aebc970637b5903756d018.patch";
+      hash = "sha256-QwLtcooDJeYWDp80a1dUFo2so/zEWqqsq5IgkXWX324=";
+    })
+    (fetchpatch {
+      name = "python-3.10-int.patch";
+      url = "https://github.com/OpenShot/openshot-qt/commit/fff785eb1e3e0c30ed6ca57e2d1933aaa807ae22.patch";
+      hash = "sha256-ee/s7rhF71ot5oPkGK+j59mL1B3e72xoH27KFoDL8s8=";
+    })
+  ];
 
   postFixup = ''
     wrapProgram $out/bin/openshot-qt \
