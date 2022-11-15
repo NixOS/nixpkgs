80bba36edb21a30ac49c46dfff19c6dcf6f7707d
K1aymore <klaymorer@protonmail.com>
2022-11-15 06:08:01 -0800

    Remove Forge servers

--- pkgs/games/minecraft-servers/default.nix
+++ pkgs/games/minecraft-servers/default.nix
@@ -2,7 +2,7 @@
 let
   servers = lib.importJSON ./servers.json;
   vanillaServers = lib.filterAttrs (n: v: lib.hasPrefix "vanilla-" n) servers;
-  fabricServers = lib.filterAttrs (n: v: lib.hasPrefix "fabric-" n) servers;
+  vanillaServers = lib.filterAttrs (n: v: lib.hasPrefix "fabric-" n) servers;
 
   latestVersion = lib.last (builtins.sort lib.versionOlder (builtins.catAttrs "version" (lib.collect (x: x ? version) vanillaServers)));
   latestFabric = lib.last (builtins.sort lib.versionOlder (builtins.catAttrs "version" (lib.collect (x: x ? version) fabricServers)));
@@ -18,7 +18,7 @@ let
         jre_headless = getJavaVersion (if value.javaVersion == null then 8 else value.javaVersion); # versions <= 1.6 will default to 8
       };
     })
-    (lib.mergeAttrs vanillaServers fabricServers); # Forge servers broken
+    servers; # Forge servers broken
 in
 lib.recurseIntoAttrs (
   packages // {
