28f36728602763cced4feb006ab707ec41e5ae31
Sandro Jäckel <sandro.jaeckel@gmail.com>
2022-11-16 00:28:45 +0100

    Revert "gplates: fix failing build"
    This reverts commit 8a236e246ab1aceea6e5b181f0d566be6bb3a614.

--- pkgs/applications/science/misc/gplates/default.nix
+++ pkgs/applications/science/misc/gplates/default.nix
@@ -14,15 +14,13 @@
 , libGLU
 , mpfr
 , proj
-# build with Python 3.10 fails, because boost <= 1.78 can't find
-# pythons with double digits in minor versions, like X.YZ
-, python39
+, python3
 , qtxmlpatterns
 , qwt
 }:
 
 let
-  python = python39.withPackages (ps: with ps; [
+  python = python3.withPackages (ps: with ps; [
     numpy
   ]);
   boost' = boost.override {
