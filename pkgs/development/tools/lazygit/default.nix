b6239d9b019b76da41d5965e8d90ba18ec05adb5
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-15 06:57:06 +0000

    lazygit: 0.35 -> 0.36.0

--- pkgs/development/tools/lazygit/default.nix
+++ pkgs/development/tools/lazygit/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "lazygit";
-  version = "0.35";
+  version = "0.36.0";
 
   src = fetchFromGitHub {
     owner = "jesseduffield";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-TYtjwEQTXjkp1o95cXJw0VDVv5us07K+6NrJF9bsJfg=";
+    sha256 = "sha256-0emAo/K27lmQ+409maTR/hnonQVkbijF0RAwvIErBFo=";
   };
 
   vendorSha256 = null;
