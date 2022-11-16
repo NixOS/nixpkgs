81db0fc59c8b4c200e75a82474c67568f4820336
Artturin <Artturin@artturin.com>
2022-11-16 21:55:46 +0200

    fsfs: remove package which has never worked and project is dead
    fsfs was added in https://github.com/NixOS/nixpkgs/commit/f00bdb6f150e2786bcb0186e95bf05b0d70ed44d
    with a throw that states "it still does not build"

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6846,8 +6846,6 @@ with pkgs;
 
   fsarchiver = callPackage ../tools/archivers/fsarchiver { };
 
-  fsfs = callPackage ../tools/filesystems/fsfs { };
-
   fstl = qt5.callPackage ../applications/graphics/fstl { };
 
   fswebcam = callPackage ../os-specific/linux/fswebcam { };
