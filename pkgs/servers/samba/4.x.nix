5f6a4c84f6bd9be83490466c431076eb4ae944e1
Janne Heß <janne@hess.ooo>
2022-11-16 22:06:50 +0100

    samba4: 4.17.2 -> 4.17.3

--- pkgs/servers/samba/4.x.nix
+++ pkgs/servers/samba/4.x.nix
@@ -47,11 +47,11 @@ with lib;
 
 stdenv.mkDerivation rec {
   pname = "samba";
-  version = "4.17.2";
+  version = "4.17.3";
 
   src = fetchurl {
     url = "mirror://samba/pub/samba/stable/${pname}-${version}.tar.gz";
-    hash = "sha256-5V3fTVF4+MhDFqv1PF7dezU5njt9hry4G3UmHIJ7s7g=";
+    hash = "sha256-XRxCDLMexhPHhvmFN/lZZZCB7ca+g3PmjocUCGiTjiY=";
   };
 
   outputs = [ "out" "dev" "man" ];
