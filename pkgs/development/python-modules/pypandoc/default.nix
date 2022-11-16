10e247864ef0c4aedc3e94de9635ed516c7b9a3a
sternenseemann <sternenseemann@systemli.org>
2022-11-16 17:30:41 +0100

    python3Packages.pypandoc: 1.8.1 -> 1.10
    https://github.com/JessicaTegner/pypandoc/releases/tag/v1.9
    https://github.com/JessicaTegner/pypandoc/releases/tag/v1.10

--- pkgs/development/python-modules/pypandoc/default.nix
+++ pkgs/development/python-modules/pypandoc/default.nix
@@ -1,16 +1,16 @@
 { lib, substituteAll, buildPythonPackage, fetchFromGitHub
-, pandoc, texlive
+, pandoc, pandocfilters, texlive
 }:
 
 buildPythonPackage rec {
   pname = "pypandoc";
-  version = "1.8.1";
+  version = "1.10";
 
   src = fetchFromGitHub {
-    owner = "NicklasTegner";
+    owner = "JessicaTegner";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-1vQmONQFJrjptwVVjw25Wyt59loatjScsdnSax+q/f8=";
+    hash = "sha256:05m585l4sipjzpkrv4yj5s7w45yxhxlym55lkhnavsshlvisinkz";
   };
 
   patches = [
@@ -24,11 +24,12 @@ buildPythonPackage rec {
 
   checkInputs = [
     texlive.combined.scheme-small
+    pandocfilters
   ];
 
   meta = with lib; {
     description = "Thin wrapper for pandoc";
-    homepage = "https://github.com/NicklasTegner/pypandoc";
+    homepage = "https://github.com/JessicaTegner/pypandoc";
     license = licenses.mit;
     maintainers = with maintainers; [ sternenseemann bennofs ];
   };
