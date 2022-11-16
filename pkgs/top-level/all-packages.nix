1462c3cc487e282988fa0a2747b021fb934f6cf6
Arthur Gautier <arthur.gautier@arista.com>
2022-11-16 10:56:45 -0800

    zlint: init at 3.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13297,6 +13297,8 @@ with pkgs;
 
   zkar = callPackage ../tools/security/zkar { };
 
+  zlint = callPackage ../tools/security/zlint { };
+
   zmap = callPackage ../tools/security/zmap { };
 
   zpool-iostat-viz = callPackage ../tools/filesystems/zpool-iostat-viz { };
