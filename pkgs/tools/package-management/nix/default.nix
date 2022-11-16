da14dfafd9a740a01464ab2f42d04d0bd7825459
zowoq <59103226+zowoq@users.noreply.github.com>
2022-11-17 07:15:58 +1000

    nixVersions.nix_2_9: add non-existing output patch

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -81,6 +81,14 @@ in lib.makeExtensible (self: {
   nix_2_9 = common {
     version = "2.9.2";
     sha256 = "sha256-uZCaBo9rdWRO/AlQMvVVjpAwzYijB2H5KKQqde6eHkg=";
+    patches = [
+      (fetchpatch {
+        # https://github.com/NixOS/nix/pull/7283
+        name = "fix-requires-non-existing-output.patch";
+        url = "https://github.com/NixOS/nix/commit/3ade5f5d6026b825a80bdcc221058c4f14e10a27.patch";
+        sha256 = "sha256-s1ybRFCjQaSGj7LKu0Z5g7UiHqdJGeD+iPoQL0vaiS0=";
+      })
+    ];
   };
 
   nix_2_10 = common {
