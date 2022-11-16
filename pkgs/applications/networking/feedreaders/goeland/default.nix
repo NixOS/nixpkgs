79cbd32c181d506b4f76565d0642a3cf4b3f7b57
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-16 02:58:01 +0000

    goeland: 0.11.0 -> 0.12.1

--- pkgs/applications/networking/feedreaders/goeland/default.nix
+++ pkgs/applications/networking/feedreaders/goeland/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "goeland";
-  version = "0.11.0";
+  version = "0.12.1";
 
   src = fetchFromGitHub {
     owner = "slurdge";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-9SxlxRco+eLyyIrMjNCdqAyttDnoc8nuMh+ecCpk3bg=";
+    sha256 = "sha256-7ZfZ5sf8vK51geWZ5iKFPwd/v26f7MTDmtKkxamKwRQ=";
   };
 
-  vendorSha256 = "sha256-a29TtT6xSapIiHvC9KHQm4gd1QCK3l0RpKe1ieaKUKA=";
+  vendorSha256 = "sha256-iljGBe8c6dqEHRpMN5cz7wmminejoiXXDKuQDazDztA=";
 
   ldflags = [
     "-s"
