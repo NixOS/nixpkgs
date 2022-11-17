c66fe95accd1942d16ad505db32a2b97c7d64934
atha <74566464+Pandademic@users.noreply.github.com>
2022-11-17 18:31:37 -0500

    add wlroots 0.16 - it builds!

---
+++ pkgs/development/libraries/wlroots/0.16.nix
@@ -0,0 +1,75 @@
+{ lib, stdenv, fetchFromGitLab, meson, ninja, hwdata, cmake, pkg-config, wayland-scanner
+, libGL, wayland, wayland-protocols, libinput, libxkbcommon, pixman
+,libcap, mesa, xorg
+, libpng, ffmpeg_4, seatd, vulkan-loader, glslang
+, nixosTests
+
+, enableXWayland ? true, xwayland ? null
+}:
+
+stdenv.mkDerivation rec {
+  pname = "wlroots";
+  version = "0.16.0";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    owner = "wlroots";
+    repo = "wlroots";
+    rev = version;
+    sha256 = "k7BFx1xvvsdCXNWX0XeZYwv8H/myk4p42i2Y6vjILqM=";
+  };
+
+  # $out for the library and $examples for the example programs (in examples):
+  outputs = [ "out" "examples" ];
+
+  strictDeps = true;
+  depsBuildBuild = [ pkg-config ];
+
+  nativeBuildInputs = [ meson ninja pkg-config wayland-scanner glslang cmake hwdata ];
+
+  buildInputs = [
+    libGL wayland wayland-protocols libinput libxkbcommon pixman
+    xorg.xcbutilwm xorg.libX11 libcap xorg.xcbutilimage xorg.xcbutilerrors mesa
+    libpng ffmpeg_4 xorg.xcbutilrenderutil seatd vulkan-loader
+  ]
+    ++ lib.optional enableXWayland xwayland
+  ;
+
+  mesonFlags =
+    lib.optional (!enableXWayland) "-Dxwayland=disabled"
+  ;
+
+  patchPhase = ''
+    a=$(ls /nix/store | grep hwdata | tail -n 1 )
+    
+    substituteInPlace backend/drm/meson.build --replace  "/usr/share/hwdata/pnp.ids" "/nix/store/$a/share/hwdata/pnp.ids"
+  '';
+
+  postFixup = ''
+    # Install ALL example programs to $examples:
+    # screencopy dmabuf-capture input-inhibitor layer-shell idle-inhibit idle
+    # screenshot output-layout multi-pointer rotation tablet touch pointer
+    # simple
+    mkdir -p $examples/bin
+    cd ./examples
+    for binary in $(find . -executable -type f -printf '%P\n' | grep -vE '\.so'); do
+      cp "$binary" "$examples/bin/wlroots-$binary"
+    done  
+  '';
+
+  # Test via TinyWL (the "minimum viable product" Wayland compositor based on wlroots):
+  passthru.tests.tinywl = nixosTests.tinywl;
+
+  meta = with lib; {
+    description = "A modular Wayland compositor library";
+    longDescription = ''
+      Pluggable, composable, unopinionated modules for building a Wayland
+      compositor; or about 50,000 lines of code you were going to write anyway.
+    '';
+    inherit (src.meta) homepage;
+    changelog = "https://gitlab.freedesktop.org/wlroots/wlroots/-/tags/${version}";
+    license     = licenses.mit;
+    platforms   = platforms.linux;
+    maintainers = with maintainers; [ primeos synthetica ];
+  };
+}
