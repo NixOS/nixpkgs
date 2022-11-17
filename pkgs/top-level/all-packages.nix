1f892796749b84d2c3ea87fd717a2dfe2b46c26b
Aaron Jheng <wentworth@outlook.com>
2022-11-17 14:48:03 +0000

    cue: 0.4.3 -> 0.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3182,9 +3182,7 @@ with pkgs;
 
   cudd = callPackage ../development/libraries/cudd { };
 
-  cue = callPackage ../development/tools/cue {
-    buildGoModule = buildGo118Module; # tests fail with 1.19
-  };
+  cue = callPackage ../development/tools/cue { };
 
   writeCueValidator = callPackage ../development/tools/cue/validator.nix { };
 
