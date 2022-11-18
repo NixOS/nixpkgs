34e0667b74c5055f81986c759344e3a7de6414c1
Naïm Favier <n@monade.li>
2022-11-18 10:29:41 +0100

    dwarf-fortress.dfhack: use CXXFLAGS instead of NIX_CFLAGS_COMPILE
    See https://github.com/NixOS/nixpkgs/issues/79303.
    
    Avoids warnings about -fpermissive not being a valid C flag.

--- pkgs/games/dwarf-fortress/dfhack/default.nix
+++ pkgs/games/dwarf-fortress/dfhack/default.nix
@@ -125,7 +125,7 @@ in
     });
 
     # gcc 11 fix
-    NIX_CFLAGS_COMPILE = lib.optionalString (lib.versionOlder version "0.47.05-r3") "-fpermissive";
+    CXXFLAGS = lib.optionalString (lib.versionOlder version "0.47.05-r3") "-fpermissive";
 
     # As of
     # https://github.com/DFHack/dfhack/commit/56e43a0dde023c5a4595a22b29d800153b31e3c4,
