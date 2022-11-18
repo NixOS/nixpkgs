fc7c518d2c1a86931224c29d617f67f51af47156
sternenseemann <sternenseemann@systemli.org>
2022-11-18 16:04:28 +0100

    haskellPackages.mkDerivation: add support for --enable-relocatable

--- pkgs/test/haskell/default.nix
+++ pkgs/test/haskell/default.nix
@@ -4,6 +4,7 @@ lib.recurseIntoAttrs {
   shellFor = callPackage ./shellFor { };
   cabalSdist = callPackage ./cabalSdist { };
   documentationTarball = callPackage ./documentationTarball { };
+  pathsModule = lib.recurseIntoAttrs (callPackage ./pathsModule { });
   setBuildTarget = callPackage ./setBuildTarget { };
   writers = callPackage ./writers { };
 }
