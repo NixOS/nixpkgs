208ee8b2e23d1a658a43e67aef38694de2219069
Tom Hubrecht <tom@hubrecht.ovh>
2022-11-19 13:13:56 +0100

    nixos/fail2ban: use attrsets for settings instead of strings

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -256,6 +256,7 @@ in {
   etebase-server = handleTest ./etebase-server.nix {};
   etesync-dav = handleTest ./etesync-dav.nix {};
   evcc = handleTest ./evcc.nix {};
+  fail2ban = handleTest ./fail2ban.nix { };
   fakeroute = handleTest ./fakeroute.nix {};
   fancontrol = handleTest ./fancontrol.nix {};
   fcitx5 = handleTest ./fcitx5 {};
