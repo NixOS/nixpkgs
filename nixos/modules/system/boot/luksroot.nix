60c22cd52e8c03874dbca78883e537955f4bde4f
vawvaw <valentin@vaw-valentin.de>
2022-11-19 15:13:30 +0100

    Fix yubikey password retries

--- nixos/modules/system/boot/luksroot.nix
+++ nixos/modules/system/boot/luksroot.nix
@@ -176,12 +176,6 @@ let
                     # and try reading it from /dev/console with a timeout
                     IFS= read -t 1 -r passphrase
                     if [ -n "$passphrase" ]; then
-                       ${if luks.reusePassphrases then ''
-                         # remember it for the next device
-                         echo -n "$passphrase" > /crypt-ramfs/passphrase
-                       '' else ''
-                         # Don't save it to ramfs. We are very paranoid
-                       ''}
                        echo
                        break
                     fi
@@ -192,9 +186,10 @@ let
             if [ $? == 0 ]; then
                 echo " - success"
                 ${if luks.reusePassphrases then ''
-                  # we don't rm here because we might reuse it for the next device
+                  # remember it for the next device
+                  echo -n "$passphrase" > /crypt-ramfs/passphrase
                 '' else ''
-                  rm -f /crypt-ramfs/passphrase
+                  # Don't save it to ramfs. We are very paranoid
                 ''}
                 break
             else
@@ -269,12 +264,6 @@ let
                     # Try reading it from /dev/console with a timeout
                     IFS= read -t 1 -r k_user
                     if [ -n "$k_user" ]; then
-                       ${if luks.reusePassphrases then ''
-                         # Remember it for the next device
-                         echo -n "$k_user" > /crypt-ramfs/passphrase
-                       '' else ''
-                         # Don't save it to ramfs. We are very paranoid
-                       ''}
                        echo
                        break
                     fi
@@ -293,9 +282,10 @@ let
             if [ $? == 0 ]; then
                 opened=true
                 ${if luks.reusePassphrases then ''
-                  # We don't rm here because we might reuse it for the next device
+                  # Remember it for the next device
+                  echo -n "$k_user" > /crypt-ramfs/passphrase
                 '' else ''
-                  rm -f /crypt-ramfs/passphrase
+                  # Don't save it to ramfs. We are very paranoid
                 ''}
                 break
             else
