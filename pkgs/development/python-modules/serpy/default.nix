4bd1aa5006bef573fccde0e1f34e48a02f95d81e
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-19 10:43:35 +0100

    python310Packages.serpy: add pythonImportsCheck

--- pkgs/development/python-modules/serpy/default.nix
+++ pkgs/development/python-modules/serpy/default.nix
@@ -1,18 +1,32 @@
-{ lib, buildPythonPackage, fetchPypi, six }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, six
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "serpy";
   version = "0.3.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "3772b2a9923fbf674000ff51abebf6ea8f0fca0a2cfcbfa0d63ff118193d1ec5";
   };
 
-  propagatedBuildInputs = [ six ];
+  propagatedBuildInputs = [
+    six
+  ];
 
   # ImportError: No module named 'tests
   doCheck = false;
+
+  pythonImportsCheck = [
+    "serpy"
+  ];
 
   meta = with lib; {
     description = "Ridiculously fast object serialization";
