f525342399d1650a54eba42a807d86d3d9d4e083
Jordan Isaacs <mail@jdisaacs.com>
2022-11-20 18:03:15 -0500

    waylock: init at 0.4.2
    When using, ensure to install a waylock pam.d file.
    On NixOS use `security.pam.services.waylock = {}`.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29432,6 +29432,8 @@ with pkgs;
 
   waybar = callPackage ../applications/misc/waybar {};
 
+  waylock = callPackage ../applications/misc/waylock {};
+
   wayshot = callPackage ../tools/misc/wayshot { };
 
   waylevel = callPackage ../tools/misc/waylevel { };
