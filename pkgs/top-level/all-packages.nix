77b90b4bacba08ff5c35f4f38df6d3d581646c8f
Luna Nova <git@lunnova.dev>
2022-11-20 10:30:02 -0800

    notepadqq: 1.4.8 -> 2.0.0-beta
    Using a beta release as qtwebkit is no longer supported and the beta release supports qtwebengine

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30980,7 +30980,7 @@ with pkgs;
 
   notepad-next = libsForQt5.callPackage ../applications/editors/notepad-next { };
 
-  notepadqq = libsForQt514.callPackage ../applications/editors/notepadqq { };
+  notepadqq = libsForQt5.callPackage ../applications/editors/notepadqq { };
 
   notmuch = callPackage ../applications/networking/mailreaders/notmuch {
     gmime = gmime3;
