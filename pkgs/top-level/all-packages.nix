b872cd95b090305019eeacba76837d1b3d1c8489
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2022-11-20 14:15:47 -0600

    zeal: unstable-2021-12-25 → unstable-2022-10-02

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27174,7 +27174,9 @@ with pkgs;
     inherit (plasma5Packages) breeze-icons;
   };
 
-  zeal = libsForQt5.callPackage ../data/documentation/zeal { };
+  zeal-qt5 = libsForQt5.callPackage ../data/documentation/zeal { };
+  zeal-qt6 = qt6Packages.callPackage ../data/documentation/zeal { };
+  zeal = zeal-qt5;
 
   zilla-slab = callPackage ../data/fonts/zilla-slab { };
 
