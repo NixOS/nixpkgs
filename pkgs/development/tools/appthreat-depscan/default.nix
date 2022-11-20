2073ee4b6e962656bc3475e9fa3de71a3afc2729
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-20 19:42:05 +0100

    appthreat-depscan: add changelog to meta

--- pkgs/development/tools/appthreat-depscan/default.nix
+++ pkgs/development/tools/appthreat-depscan/default.nix
@@ -46,6 +46,7 @@ python3.pkgs.buildPythonApplication rec {
   meta = with lib; {
     description = "Tool to audit dependencies based on known vulnerabilities and advisories";
     homepage = "https://github.com/AppThreat/dep-scan";
+    changelog = "https://github.com/AppThreat/dep-scan/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
