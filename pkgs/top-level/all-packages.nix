1996190b651a63f08d9942805658aa153440c1c3
ajs124 <git@ajs124.de>
2022-11-21 00:41:11 +0100

    openssl_legacy: init
    openssl_3, but with a openssl.cnf that enables legacy ciphers
    this way we can migrate away from openssl_1_1, while not breaking
    applications relying on deprecated stuff

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21745,6 +21745,10 @@ with pkgs;
 
   openssl = openssl_3;
 
+  openssl_legacy = openssl.override {
+    conf = ../development/libraries/openssl/3.0/legacy.cnf;
+  };
+
   inherit (callPackages ../development/libraries/openssl { })
     openssl_1_1
     openssl_3;
