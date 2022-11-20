dee33b1edd02b4cd81b10d681aa97e2430a94708
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-20 01:00:42 +0100

    python310Packages.shtab: add changelog to meta

--- pkgs/development/python-modules/shtab/default.nix
+++ pkgs/development/python-modules/shtab/default.nix
@@ -46,6 +46,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Module for shell tab completion of Python CLI applications";
     homepage = "https://docs.iterative.ai/shtab/";
+    changelog = "https://github.com/iterative/shtab/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
