ad063875d421ca3524e781f73ff4bc72f4739b7b
apfelkuchen06 <apfelkuchen@hrnz.li>
2022-11-21 19:26:38 +0100

    sourcehut.listssrht: 0.51.11 -> 0.55.1

--- pkgs/applications/version-management/sourcehut/lists.nix
+++ pkgs/applications/version-management/sourcehut/lists.nix
@@ -14,20 +14,20 @@
 
 buildPythonPackage rec {
   pname = "listssrht";
-  version = "0.51.11";
+  version = "0.55.1";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = "lists.sr.ht";
     rev = version;
-    sha256 = "sha256-Qb70oOazZfmHpC5r0oMYCFdvfAeKbq3mQA8+M56YYnY=";
+    sha256 = "sha256-gvx5oWsXZitJM4SFjvffF/9mUuylvYrjvsaHsIMQaKM=";
   };
 
   listssrht-api = buildGoModule ({
     inherit src version;
     pname = "listssrht-api";
     modRoot = "api";
-    vendorSha256 = "sha256-xnmMkRSokbhWD+kz0XQ9AinYdm6/50FRBISURPvlzD0=";
+    vendorSha256 = "sha256-gs4JfDOwCnTc54P8L2KGKwhq0HB0v5BOh4UavEN2kh4=";
   } // import ./fix-gqlgen-trimpath.nix { inherit unzip; });
 
   postPatch = ''
@@ -50,6 +50,8 @@ buildPythonPackage rec {
   '';
 
   postInstall = ''
+    mkdir -p $out/bin $out/share/sql
+    cp schema.sql $out/share/sql/lists-schema.sql
     ln -s ${listssrht-api}/bin/api $out/bin/listssrht-api
   '';
 
