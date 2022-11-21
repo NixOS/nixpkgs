607fe05ebb2c40d53f2456c26ee593b4dfaefcec
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-21 12:54:38 +0000

    ell: 0.53 -> 0.54

--- pkgs/os-specific/linux/ell/default.nix
+++ pkgs/os-specific/linux/ell/default.nix
@@ -7,14 +7,14 @@
 
 stdenv.mkDerivation rec {
   pname = "ell";
-  version = "0.53";
+  version = "0.54";
 
   outputs = [ "out" "dev" ];
 
   src = fetchgit {
     url = "https://git.kernel.org/pub/scm/libs/ell/ell.git";
     rev = version;
-    sha256 = "sha256-txtXPWmLfZLBsK3s94W5LyA81mTHStpP3nkq6KW33Mk=";
+    sha256 = "sha256-Oi+S4DWXuTUL36Xh3iWIZj9rdN2qUDHmZiFSH1csW+8=";
   };
 
   nativeBuildInputs = [
