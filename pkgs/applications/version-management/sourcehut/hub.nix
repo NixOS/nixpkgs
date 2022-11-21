1961926a540f165b8a67371cefca5bc2eede46c2
apfelkuchen06 <apfelkuchen@hrnz.li>
2022-11-21 19:25:57 +0100

    sourcehut.hubsrht: 0.14.14 -> 0.16.1

--- pkgs/applications/version-management/sourcehut/hub.nix
+++ pkgs/applications/version-management/sourcehut/hub.nix
@@ -3,26 +3,46 @@
 , buildPythonPackage
 , srht
 , pyyaml
+, buildGoModule
+, unzip
 }:
 
 buildPythonPackage rec {
   pname = "hubsrht";
-  version = "0.14.14";
+  version = "0.16.1";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = "hub.sr.ht";
     rev = version;
-    sha256 = "sha256-4n6oQ+AAvdJY/5KflxAp62chjyrlSUkmt319DKZk33w=";
+    sha256 = "sha256-ivQ7bJhZyBl6aoMUCoLNkDQ/WcSlpGaTuP8TbjSn8ek=";
   };
+
+  hubsrht-api = buildGoModule ({
+    inherit src version;
+    pname = "hubsrht-api";
+    modRoot = "api";
+    vendorSha256 = "sha256-K5EmZ4U+xItTR85+SCwhwg5KUGLkKHo9Nr2pkvmJpfo=";
+  } // import ./fix-gqlgen-trimpath.nix { inherit unzip; });
 
   propagatedBuildInputs = [
     srht
     pyyaml
   ];
 
+  postPatch = ''
+    substituteInPlace Makefile \
+      --replace "all: api" ""
+  '';
+
   preBuild = ''
     export PKGVER=${version}
+  '';
+
+  postInstall = ''
+    mkdir -p $out/bin $out/share/sql
+    cp schema.sql $out/share/sql/hub-schema.sql
+    ln -s ${hubsrht-api}/bin/api $out/bin/hubsrht-api
   '';
 
   dontUseSetuptoolsCheck = true;
