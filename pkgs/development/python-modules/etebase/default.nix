eb260f82d8814961e9acf711625dc80402c3ac5f
thyol <thyol@pm.me>
2022-11-21 21:32:58 +0100

    etebase: update dependencies

--- pkgs/development/python-modules/etebase/default.nix
+++ pkgs/development/python-modules/etebase/default.nix
@@ -1,3 +1,4 @@
+
 { lib
 , stdenv
 , fetchFromGitHub
@@ -9,6 +10,7 @@
 , openssl
 , Security
 , msgpack
+, fetchpatch
 }:
 
 buildPythonPackage rec {
@@ -17,15 +19,24 @@ buildPythonPackage rec {
 
   src = fetchFromGitHub {
     owner = "etesync";
+    rev = "ac3e5138a2e18dc11f77d9a75f07d4b8dd8c4445";
+    hash = "sha256-OSzOoL7o4rWYOK1OyxfpslOnNfnK5CKhdhiFEXz/WvY=";
     repo = "etebase-py";
-    rev = "v${version}";
-    hash = "sha256-87t3toMaApnOSPBKfTGGLo2VRLqU8irFac9lg3kA1eE=";
   };
 
-  cargoDeps = rustPlatform.fetchCargoTarball {
-    inherit src;
-    name = "${pname}-${version}";
-    hash = "sha256-Qg0aJ6CZaPxGneIs4o402A+fhI/nlJ9X/XdMFqkD/YY=";
+  patches = [
+    # Update dependencies
+    (fetchpatch {
+      url = "https://github.com/thyol/etebase-py/commit/db5533f4c895b6e7fc287e68e1fdd1cc9ca02bba.patch";
+      hash = "sha256-glKxAEk+19R3lANwplw54V0BDZxpgvgrF+POnJscv9k=";
+    })
+  ];
+
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "flapigen-0.6.0-pre8" = "sha256-8KBhVgHn+UtrEtBQJEVTrx+h4IqhKTn9Csx6yBAV0RQ=";
+    };
   };
 
   format = "pyproject";
