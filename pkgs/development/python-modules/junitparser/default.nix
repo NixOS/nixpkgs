9d533b4cbb84ad91e9dc9561aff31ca9afa02361
Ricardo Steijn <ricardo.steijn97@gmail.com>
2022-11-21 23:49:43 +0100

    python310Packages,junitparser: use github source and add unittesthook

--- pkgs/development/python-modules/junitparser/default.nix
+++ pkgs/development/python-modules/junitparser/default.nix
@@ -1,22 +1,28 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , future
+, glibcLocales
+, lxml
+, unittestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "junitparser";
   version = "2.8.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-qNQpCrn7k/IBXg2+9h75O36hEOmQmQh6RT9VyOFIGt8=";
+  src = fetchFromGitHub {
+    owner = "weiwei";
+    repo = pname;
+    rev = version;
+    hash = "sha256-rhDP05GSWT4K6Z2ip8C9+e3WbvBJOwP0vctvANBs7cw=";
   };
 
   propagatedBuildInputs = [ future ];
 
-  # Pypi release does not include the tests folder
-  doCheck = false;
+  checkInputs = [ unittestCheckHook lxml glibcLocales ];
+
+  unittestFlagsArray = [ "-v" ];
 
   meta = with lib; {
     description = "Manipulates JUnit/xUnit Result XML files";
