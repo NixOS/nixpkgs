bb029673bface2fc9fb807f209f63ca06478a72d
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-11-21 21:08:55 +0100

    22.11 beta release

--- nixos/modules/misc/version.nix
+++ nixos/modules/misc/version.nix
@@ -114,7 +114,7 @@ in
     defaultChannel = mkOption {
       internal = true;
       type = types.str;
-      default = "https://nixos.org/channels/nixos-unstable";
+      default = "https://nixos.org/channels/nixos-22.11";
       description = lib.mdDoc "Default NixOS channel to which the root user is subscribed.";
     };
 
