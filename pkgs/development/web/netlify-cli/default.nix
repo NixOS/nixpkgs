ae83b9c7c656f4556ff2af3115d13aa90742d527
panda2134 <ljypanda@live.com>
2022-11-22 12:18:51 +0800

    netlify-cli: 6.13.2 -> 12.2.4
    Updates netlify-cli to 12.2.4, while switching to NodeJS 16.
    According to its issue page, there seems to be problems running
    this tool with NodeJS 18, so we'd better stick to NodeJS 16
    instead.

--- pkgs/development/web/netlify-cli/default.nix
+++ pkgs/development/web/netlify-cli/default.nix
@@ -1,12 +1,18 @@
 { callPackage, fetchFromGitHub, lib, pkgs }:
 let
   nodePackages = import ./composition.nix { inherit pkgs; };
+  meta = (lib.importJSON ./netlify-cli.json);
 in
   nodePackages.package.override {
     preRebuild = ''
       export ESBUILD_BINARY_PATH="${pkgs.esbuild_netlify}/bin/esbuild"
     '';
-    src = fetchFromGitHub (lib.importJSON ./netlify-cli.json);
+    src = fetchFromGitHub {
+      owner = meta.owner;
+      repo = meta.repo;
+      rev = meta.rev;
+      sha256 = meta.sha256;
+    };
     bypassCache = true;
     reconstructLock = true;
     passthru.tests.test = callPackage ./test.nix { };
