62f4c61a1fbb73bd06b1dbe83f51f791ead70a93
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-22 18:39:40 +0100

    python310Packages.django-maintenance-mode: add changelog to meta
    - disabled on older Python releases
    - add pythonImportsCheck

--- pkgs/development/python-modules/django-maintenance-mode/default.nix
+++ pkgs/development/python-modules/django-maintenance-mode/default.nix
@@ -9,6 +9,9 @@
 buildPythonPackage rec {
   pname = "django-maintenance-mode";
   version = "0.17.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "fabiocaccamo";
@@ -17,13 +20,23 @@ buildPythonPackage rec {
     hash = "sha256-/JMdElJsl7f6THUIvp28XcsoP/5Sa31XzGl3PZFPAH8=";
   };
 
-  checkInputs = [ pytest ];
+  propagatedBuildInputs = [
+    django
+    python-fsutil
+  ];
 
-  propagatedBuildInputs = [ django python-fsutil ];
+  checkInputs = [
+    pytest
+  ];
+
+  pythonImportsCheck = [
+    "maintenance_mode"
+  ];
 
   meta = with lib; {
     description = "Shows a 503 error page when maintenance-mode is on";
     homepage = "https://github.com/fabiocaccamo/django-maintenance-mode";
+    changelog = "https://github.com/fabiocaccamo/django-maintenance-mode/releases/tag/${version}";
     maintainers = with maintainers; [ mrmebelman ];
     license = licenses.bsd3;
   };
