dbde15a513603821875b40e1f848d88c6f5f92d4
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-22 18:31:29 +0100

    python310Packages.broadlink: add changelog to meta

--- pkgs/development/python-modules/broadlink/default.nix
+++ pkgs/development/python-modules/broadlink/default.nix
@@ -31,6 +31,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python API for controlling Broadlink IR controllers";
     homepage =  "https://github.com/mjg59/python-broadlink";
+    changelog = "https://github.com/mjg59/python-broadlink/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
