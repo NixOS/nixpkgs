a7e786b1e5360263843b21a88a58c690afaa642e
zowoq <59103226+zowoq@users.noreply.github.com>
2022-11-23 07:25:06 +1000

    nomad_1_4: 1.4.2 -> 1.4.3
    https://github.com/hashicorp/nomad/releases/tag/v1.4.3
    (cherry picked from commit 40c825f840531d0e601f27b61313f6e0f7ea9567)

--- pkgs/applications/networking/cluster/nomad/1.4.nix
+++ pkgs/applications/networking/cluster/nomad/1.4.nix
@@ -4,7 +4,7 @@
 
 callPackage ./generic.nix {
   inherit buildGoModule;
-  version = "1.4.2";
-  sha256 = "sha256-GGLy/6FgMTSZ701F0QGwcw1EFZSUMyPOlokThOTtdJg=";
-  vendorSha256 = "sha256-dd8rTGcO4GVMRuABwT4HeucZqYKxrgRUkua/bSPLNH0=";
+  version = "1.4.3";
+  sha256 = "sha256-GQVfrn9VlzfdIj73W3hBpHcevsXZcb6Uj808HUCZUUg=";
+  vendorSha256 = "sha256-JQRpsQhq5r/QcgFwtnptmvnjBEhdCFrXFrTKkJioL3A=";
 }
