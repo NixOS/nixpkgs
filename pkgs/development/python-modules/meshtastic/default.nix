a22a960374e13063c69e88b3a37e19847350afaf
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-23 18:57:45 +0100

    python310Packages.meshtastic: 2.0.3 -> 2.0.4
    Changelog: https://github.com/meshtastic/python/releases/tag/2.0.4

--- pkgs/development/python-modules/meshtastic/default.nix
+++ pkgs/development/python-modules/meshtastic/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "meshtastic";
-  version = "2.0.3";
+  version = "2.0.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "meshtastic";
     repo = "Meshtastic-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-h8OuDmm9I8lElhAGSpPx8sPUTY+EnFp2VXOYrYjiYNk=";
+    hash = "sha256-WPmoK/5pTVv9ueRnR6Gxtj86LM8ChB0dMfEvo+lLmy0=";
   };
 
   propagatedBuildInputs = [
@@ -98,6 +98,8 @@ buildPythonPackage rec {
     "test_watchGPIOs"
     "test_writeConfig_with_no_radioConfig"
     "test_writeGPIOs"
+    "test_reboot"
+    "test_shutdown"
   ];
 
   meta = with lib; {
