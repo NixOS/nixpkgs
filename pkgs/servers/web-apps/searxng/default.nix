fa13d2f932ce27e19ef7b49db7e5e99b710803f6
Daniel Nagy <danielnagy@posteo.de>
2022-11-23 12:00:00 +0100

    searxng: unstable-2022-09-01 -> unstable-2023-01-15

--- pkgs/servers/web-apps/searxng/default.nix
+++ pkgs/servers/web-apps/searxng/default.nix
@@ -1,21 +1,23 @@
 { lib
 , python3
 , fetchFromGitHub
+, unstableGitUpdater
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "searxng";
-  version = "unstable-2022-09-01";
+  version = "unstable-2023-01-15";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
-    rev = "174e5242569812618af4ebd9a646ba2a6ded5459";
-    sha256 = "sha256-Q1+4HkgOoTRtW5XYWpC5dpukkrjG5fP0585soo/srmQ=";
+    rev = "13b0c251c45c3d14700723b25b601be56178e8df";
+    sha256 = "sha256-f6RKVarW3iSkDP++MdkRt1QAdnHX/fxcD2GMvQt3hnA=";
   };
 
   postPatch = ''
     sed -i 's/==.*$//' requirements.txt
+    sed -i 's/fasttext-predict/fasttext/' requirements.txt
   '';
 
   preBuild = ''
@@ -40,8 +42,12 @@ python3.pkgs.buildPythonApplication rec {
     httpx
     httpx-socks
     markdown-it-py
+    fasttext
+    pybind11
   ] ++ httpx.optional-dependencies.http2
   ++ httpx-socks.optional-dependencies.asyncio;
+
+  passthru.updateScript = unstableGitUpdater { url = src.meta.homepage; };
 
   # tests try to connect to network
   doCheck = false;
@@ -53,7 +59,7 @@ python3.pkgs.buildPythonApplication rec {
   '';
 
   meta = with lib; {
-    homepage = "https://github.com/searxng/searxng";
+    homepage = "https://docs.searxng.org/";
     description = "A fork of Searx, a privacy-respecting, hackable metasearch engine";
     license = licenses.agpl3Plus;
     maintainers = with maintainers; [ kranzes ];
