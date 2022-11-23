cfa112c8355b754c9eb39fd2f0fa046504ba98ad
figsoda <figsoda@pm.me>
2022-11-23 15:48:47 -0500

    cargo-nextest: 0.9.43 -> 0.9.44
    Changelog: https://nexte.st/CHANGELOG.html

--- pkgs/development/tools/rust/cargo-nextest/default.nix
+++ pkgs/development/tools/rust/cargo-nextest/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-nextest";
-  version = "0.9.43";
+  version = "0.9.44";
 
   src = fetchFromGitHub {
     owner = "nextest-rs";
     repo = "nextest";
     rev = "cargo-nextest-${version}";
-    sha256 = "sha256-7Ujm5xqgyw4/P/XBZKh9yE9sWz9n+WmZbGdmif9oK+w=";
+    sha256 = "sha256-hEBRwezymduUlPZTAHAmK5Xkc/6nGr/JDL5TDWUiZO0=";
   };
 
-  cargoSha256 = "sha256-S46W+6+FyjI8BrdedDCzHHK+j3EyofQ9u8Ut1yr1/TI=";
+  cargoSha256 = "sha256-i7XYDy9NVOszanCGDaoPkc6z5DV8vm6TsekqBkV5eRA=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ Security ];
 
