0f96e0860c7d0f290f7b09d9b8425855d04d3be5
Monson Shao <holymonson@gmail.com>
2022-11-23 16:15:00 +0800

    samba: add notes for darwin

--- pkgs/servers/samba/4.x.nix
+++ pkgs/servers/samba/4.x.nix
@@ -30,6 +30,7 @@
 , bash
 , python3Packages
 , nixosTests
+, libiconv
 
 , enableLDAP ? false, openldap
 , enablePrinting ? false, cups
@@ -103,6 +104,7 @@ stdenv.mkDerivation rec {
     tdb
     libxcrypt
   ] ++ optionals stdenv.isLinux [ liburing systemd ]
+    ++ optionals stdenv.isDarwin [ libiconv ]
     ++ optionals enableLDAP [ openldap.dev python3Packages.markdown ]
     ++ optional (enablePrinting && stdenv.isLinux) cups
     ++ optional enableMDNS avahi
@@ -208,6 +210,8 @@ stdenv.mkDerivation rec {
     license = licenses.gpl3;
     platforms = platforms.unix;
     # N.B. enableGlusterFS does not build
+    # TODO: darwin support needs newer SDK for "_futimens" and "_utimensat"
+    # see https://github.com/NixOS/nixpkgs/issues/101229
     broken = stdenv.isDarwin || enableGlusterFS;
     maintainers = with maintainers; [ aneeshusa ];
   };
