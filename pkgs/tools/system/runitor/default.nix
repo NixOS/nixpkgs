c508b094aa4901232c72537d369f271b1b35a436
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-23 03:18:03 +0000

    runitor: 1.0.0 -> 1.1.0

--- pkgs/tools/system/runitor/default.nix
+++ pkgs/tools/system/runitor/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   pname = "runitor";
-  version = "1.0.0";
+  version = "1.1.0";
   vendorSha256 = null;
 
   src = fetchFromGitHub {
     owner = "bdd";
     repo = "runitor";
     rev = "v${version}";
-    sha256 = "sha256-6+U6Mh1XMdGaAmOrqUlVBX9d/GfOgXE22GwcLl0EfIU=";
+    sha256 = "sha256-DeoFm5CidUSAppnTLx11HsAIDWfVIX5RZeckWOaC39E=";
   };
 
   ldflags = [
