d669354552227bf9567ff0d40bff8129bfe18063
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-23 16:52:37 +0100

    python310Packages.sensor-state-data: 2.12.0 -> 2.12.1
    Changelog: https://github.com/Bluetooth-Devices/sensor-state-data/releases/tag/v2.12.1

--- pkgs/development/python-modules/sensor-state-data/default.nix
+++ pkgs/development/python-modules/sensor-state-data/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "sensor-state-data";
-  version = "2.12.0";
+  version = "2.12.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-u17vtw3yu8ibi/omTriy6s33/243WjxM03Nss3pFAYk=";
+    hash = "sha256-Ycn62qQ+IMqtuuE/wJPUDlyTiklu2WYrGD+wVXssRFg=";
   };
 
   nativeBuildInputs = [
