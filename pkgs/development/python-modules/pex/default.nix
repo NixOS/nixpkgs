54939cdeda11de9ad8d9376f50fb198677261b11
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-24 05:35:14 +0000

    python310Packages.pex: 2.1.113 -> 2.1.114

--- pkgs/development/python-modules/pex/default.nix
+++ pkgs/development/python-modules/pex/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "pex";
-  version = "2.1.113";
+  version = "2.1.114";
   format = "flit";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-FykzeHQjBMBZ+NqOMzjwHiOCMLk1rvomjUaoHco2ZQg=";
+    hash = "sha256-EfeoSik6vYQS4MAulfM2xfRUGYG308QzWAj4buCXX4U=";
   };
 
   nativeBuildInputs = [
