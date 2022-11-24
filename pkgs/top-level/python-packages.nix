1d7887dea70a21a2cb34a9c4a09471ada2ce1981
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-24 16:11:43 +0100

    python310Packages.google-reauth: init at 0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4052,6 +4052,8 @@ self: super: with self; {
     inherit (pkgs.buildPackages) meson;
   };
 
+##  gsutil = callPackage ../development/python-modules/gsutil { };
+
   gtfs-realtime-bindings = callPackage ../development/python-modules/gtfs-realtime-bindings { };
 
   gtimelog = callPackage ../development/python-modules/gtimelog { };
