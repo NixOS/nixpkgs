71f6d1f56b640e5bb4a68e7a87265675cc9072a5
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-25 08:57:22 +0100

    python310Packages.rns: add changelog to meta

--- pkgs/development/python-modules/rns/default.nix
+++ pkgs/development/python-modules/rns/default.nix
@@ -37,6 +37,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Cryptography-based networking stack for wide-area networks";
     homepage = "https://github.com/markqvist/Reticulum";
+    changelog = "https://github.com/markqvist/Reticulum/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
