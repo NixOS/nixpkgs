d00fc5bec63d152a7897f06208ca0ea6f78eb589
Robert Schütz <nix@dotlambda.de>
2022-11-25 00:02:18 -0800

    toluapp: build using cmake
    SCons depends on python2.
    
    (cherry picked from commit 527fe355d00f2d327099b5afaaed0b1a66369e7d)

--- pkgs/development/tools/toluapp/default.nix
+++ pkgs/development/tools/toluapp/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, sconsPackages, lua }:
+{ lib, stdenv, fetchFromGitHub, cmake, lua }:
 
 stdenv.mkDerivation rec {
   version = "1.0.93";
@@ -11,15 +11,10 @@ stdenv.mkDerivation rec {
     sha256 = "0zd55bc8smmgk9j4cf0jpibb03lgsvl0knpwhplxbv93mcdnw7s0";
   };
 
-  nativeBuildInputs = [ sconsPackages.scons_3_0_1 ];
+  nativeBuildInputs = [ cmake ];
   buildInputs = [ lua ];
 
   patches = [ ./environ-and-linux-is-kinda-posix.patch ./headers.patch ];
-
-  preConfigure = ''
-    substituteInPlace config_posix.py \
-      --replace /usr/local $out
-  '';
 
   meta = with lib; {
     description = "A tool to integrate C/Cpp code with Lua";
