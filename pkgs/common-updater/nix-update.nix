313fec5cc21480d2e676c388c3bde9eb54cf507d
Francesco Gazzetta <fgaz@fgaz.me>
2022-11-25 11:28:19 +0100

    nix-update-script: add version regex support

--- pkgs/common-updater/nix-update.nix
+++ pkgs/common-updater/nix-update.nix
@@ -1,5 +1,12 @@
-{ nix-update }:
+{ lib
+, nix-update
+}:
 
-{ attrPath }:
+{ attrPath
+# Regex to extract version with, i.e. "jq-(.*)"
+, versionRegex ? null
+}:
 
-[ "${nix-update}/bin/nix-update" attrPath ]
+[ "${nix-update}/bin/nix-update" ] ++
+lib.optionals (versionRegex != null) [ "-vr" versionRegex ] ++
+[ attrPath ]
