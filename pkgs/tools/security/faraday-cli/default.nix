5eb20083cb9f1adc351ae67abf8b04379cda94da
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-25 11:29:35 +0100

    faraday-cli: add changelog to meta

--- pkgs/tools/security/faraday-cli/default.nix
+++ pkgs/tools/security/faraday-cli/default.nix
@@ -44,6 +44,7 @@ python3.pkgs.buildPythonApplication rec {
   meta = with lib; {
     description = "Command Line Interface for Faraday";
     homepage = "https://github.com/infobyte/faraday-cli";
+    changelog = "https://github.com/infobyte/faraday-cli/releases/tag/${version}";
     license = with licenses; [ gpl3Only ];
     maintainers = with maintainers; [ fab ];
   };
