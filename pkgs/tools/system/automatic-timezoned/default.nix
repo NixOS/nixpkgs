a2720aebdd1acf557166d843017282892538ddd6
Maxime Brunet <max@brnt.mx>
2022-11-25 11:11:50 -0800

    automatic-timezoned: add meta.changelog

--- pkgs/tools/system/automatic-timezoned/default.nix
+++ pkgs/tools/system/automatic-timezoned/default.nix
@@ -19,6 +19,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "Automatically update system timezone based on location";
     homepage = "https://github.com/maxbrunet/automatic-timezoned";
+    changelog = "https://github.com/maxbrunet/automatic-timezoned/blob/v${version}/CHANGELOG.md";
     license = licenses.gpl3;
     maintainers = with maintainers; [ maxbrunet ];
     platforms = platforms.linux;
