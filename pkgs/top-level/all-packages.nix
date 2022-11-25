9454b54ed235e2ac457de1d3d1a7cc51b7a2b325
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2022-11-25 23:24:44 +0100

    wlprop: init at unstable-2022-08-18

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3597,6 +3597,8 @@ with pkgs;
 
   wlopm = callPackage ../tools/wayland/wlopm { };
 
+  wlprop = callPackage ../tools/wayland/wlprop {};
+
   wlr-randr = callPackage ../tools/wayland/wlr-randr { };
 
   wlrctl = callPackage ../tools/wayland/wlrctl { };
