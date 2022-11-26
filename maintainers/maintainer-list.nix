a5efec122e610a4f06d8e760befcf4af5be90ed3
Nicholas Sielicki <git@opensource.nslick.com>
2022-11-25 22:58:36 -0600

    maintainers: add sielicki

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12719,6 +12719,13 @@
     githubId = 4294323;
     name = "Langston Barrett";
   };
+  sielicki = {
+    name = "Nicholas Sielicki";
+    email = "nix@opensource.nslick.com";
+    github = "sielicki";
+    githubId = 4522995;
+    matrix = "@sielicki:matrix.org";
+  };
   siers = {
     email = "veinbahs+nixpkgs@gmail.com";
     github = "siers";
