7f5cfb351123cd5a85b1ca4adac9278197097b9d
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-26 15:42:42 +0100

    python310Packages.zigpy: 0.51.5 -> 0.51.6
    Changelog: https://github.com/zigpy/zigpy/releases/tag/0.51.6

--- pkgs/development/python-modules/zigpy/default.nix
+++ pkgs/development/python-modules/zigpy/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "zigpy";
-  version = "0.51.5";
+  version = "0.51.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zigpy";
     rev = "refs/tags/${version}";
-    hash = "sha256-6OSP23lEdl15IjSqGYLCW5+F6rki+rzmXm82QRzabwU=";
+    hash = "sha256-keQFFWPl2SCy1cyvbqDrA9/Yude8bf0qNiyEbFjFv/o=";
   };
 
   propagatedBuildInputs = [
