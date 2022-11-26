d8cf963200b7afe73b4a2736ec45ee123cd8c904
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-26 23:57:21 +0000

    opentelemetry-collector-contrib: 0.65.0 -> 0.66.0

--- pkgs/tools/misc/opentelemetry-collector/contrib.nix
+++ pkgs/tools/misc/opentelemetry-collector/contrib.nix
@@ -6,17 +6,17 @@
 
 buildGoModule rec {
   pname = "opentelemetry-collector-contrib";
-  version = "0.65.0";
+  version = "0.66.0";
 
   src = fetchFromGitHub {
     owner = "open-telemetry";
     repo = "opentelemetry-collector-contrib";
     rev = "v${version}";
-    sha256 = "sha256-c5N/3HAkragpR/vgh909VYu4vpUFe1An+B20SY0C1Rs=";
+    sha256 = "sha256-FT5AoqCHNf2sdKyejALOsL/zHrrxP7vdntagR9vA00I=";
   };
   # proxy vendor to avoid hash missmatches between linux and macOS
   proxyVendor = true;
-  vendorSha256 = "sha256-aXbrm3iKmc+03BZWkRDt47LJP6JT92uH3EyEVQHByj8=";
+  vendorSha256 = "sha256-65bfTCMRJ8iL5ABGPqvkayw4zSn4KkCriEkWYa0Pe68=";
 
   subPackages = [ "cmd/otelcontribcol" ];
 
