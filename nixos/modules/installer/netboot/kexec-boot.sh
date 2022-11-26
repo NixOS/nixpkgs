ed692120e18345ca82fec72c320f2cccb9f30cc5
Emily <vcs@emily.moe>
2022-11-26 11:23:27 +0000

    nixos/kexec-boot: reformat
    This matches the four spaces and camel case variable naming convention
    that seems the most common in separate NixOS shell script files.

--- nixos/modules/installer/netboot/kexec-boot.sh
+++ nixos/modules/installer/netboot/kexec-boot.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-if ! kexec -v >/dev/null 2>&1; then
-  echo "kexec not found: please install kexec-tools" 2>&1
-  exit 1
+
+if ! kexec -v &>/dev/null; then
+    echo 'kexec not found: please install kexec-tools' 2>&1
+    exit 1
 fi
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-kexec --load ${SCRIPT_DIR}/bzImage \
-  --initrd=${SCRIPT_DIR}/initrd.gz \
-  --command-line="$(< "${SCRIPT_DIR}/kernel-params")"
+
+kexecTree=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+kexec --load \
+    --initrd="$kexecTree/initrd.gz" \
+    --command-line="$(<"$kexecTree/kernel-params")" \
+    -- "$kexecTree/bzImage"
+
 kexec -e
