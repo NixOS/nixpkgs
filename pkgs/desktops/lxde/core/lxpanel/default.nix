447888e9f81f30a1baf23a9e1e3109909b1b7158
Robert Schütz <nix@dotlambda.de>
2022-11-25 17:02:08 -0800

    lxpanel: use gtk3 by default
    (cherry picked from commit e61cc2ab0265dbc52f4a8ec7b019710545e6d577)

--- pkgs/desktops/lxde/core/lxpanel/default.nix
+++ pkgs/desktops/lxde/core/lxpanel/default.nix
@@ -7,10 +7,13 @@
 , intltool
 , libxmlxx
 , keybinder
+, keybinder3
 , gtk2
+, gtk3
 , libX11
 , libfm
 , libwnck2
+, libwnck3
 , libXmu
 , libXpm
 , cairo
@@ -21,6 +24,7 @@
 , wirelesstools
 , curl
 , supportAlsa ? false, alsa-lib
+, withGtk3 ? true
 }:
 
 stdenv.mkDerivation rec {
@@ -34,11 +38,11 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ pkg-config gettext m4 intltool libxmlxx ];
   buildInputs = [
-    keybinder
-    gtk2
+    (if withGtk3 then keybinder3 else keybinder)
+    (if withGtk3 then gtk3 else gtk2)
     libX11
-    libfm
-    libwnck2
+    (libfm.override { inherit withGtk3; })
+    (if withGtk3 then libwnck3 else libwnck2)
     libXmu
     libXpm
     cairo
@@ -57,6 +61,8 @@ stdenv.mkDerivation rec {
     substituteInPlace plugins/Makefile.in \
       --replace "@PACKAGE_CFLAGS@" "@PACKAGE_CFLAGS@ -I${gdk-pixbuf-xlib.dev}/include/gdk-pixbuf-2.0"
   '';
+
+  configureFlags = lib.optional withGtk3 "--enable-gtk3";
 
   meta = with lib; {
     description = "Lightweight X11 desktop panel for LXDE";
