fcb17f332dd1dad6b19d66dbbeab7f61da34116c
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-27 08:18:56 +0100

    python310Packages.geomet: add changelog to meta

--- pkgs/development/python-modules/geomet/default.nix
+++ pkgs/development/python-modules/geomet/default.nix
@@ -32,6 +32,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Convert GeoJSON to WKT/WKB (Well-Known Text/Binary) and vice versa";
     homepage = "https://github.com/geomet/geomet";
+    changelog = "https://github.com/geomet/geomet/releases/tag/${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ turion ris ];
   };
