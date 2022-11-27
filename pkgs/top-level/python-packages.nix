7f6c18115bdbf1abc973b75b559e0ec7e24f7cd7
Robert Schütz <nix@dotlambda.de>
2022-11-27 13:03:21 -0800

    python310Packages.x-wr-timezone: init at 0.0.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12037,6 +12037,7 @@ self: super: with self; {
     };
   };
 
+  x-wr-timezone = callPackage ../development/python-modules/x-wr-timezone { };
 
   x11_hash = callPackage ../development/python-modules/x11_hash { };
 
