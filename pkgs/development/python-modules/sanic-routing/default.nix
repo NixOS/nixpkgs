22c724cd5a3465c84f2b8a07df5d04e3f85d215d
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-27 09:03:10 +0100

    python310Packages.sanic-routing: add changelog to meta

--- pkgs/development/python-modules/sanic-routing/default.nix
+++ pkgs/development/python-modules/sanic-routing/default.nix
@@ -3,25 +3,36 @@
 , fetchFromGitHub
 , pytestCheckHook
 , pytest-asyncio
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "sanic-routing";
   version = "22.3.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "sanic-org";
     repo = "sanic-routing";
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     hash = "sha256-dX+uxrVjtPxX0ba3WUE/JKgj0PZzvFdKr/lXQgASN6Y=";
   };
 
-  checkInputs = [ pytestCheckHook pytest-asyncio ];
-  pythonImportsCheck = [ "sanic_routing" ];
+  checkInputs = [
+    pytest-asyncio
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "sanic_routing"
+  ];
 
   meta = with lib; {
     description = "Core routing component for the Sanic web framework";
     homepage = "https://github.com/sanic-org/sanic-routing";
+    changelog = "https://github.com/sanic-org/sanic-routing/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ AluisioASG ];
   };
