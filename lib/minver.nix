e075e71485acb1f1f61e487b3ad545a57c122c6f
Artturin <Artturin@artturin.com>
2022-11-28 00:08:54 +0200

    Bump minver.nix to 2.8
    source for the quote is at https://github.com/NixOS/nixpkgs/pull/89308#issuecomment-638795785
    edolstra> ... I think in the past our rule was it should be possible to evaluate Nixpkgs using the Nix version from the previous NixOS release...

--- lib/minver.nix
+++ lib/minver.nix
@@ -1,2 +1,3 @@
-# Expose the minimum required version for evaluating Nixpkgs
-"2.3"
+# Expose the minimum required version for evaluating Nixpkgs.
+# It should be possible to evaluate Nixpkgs using the default Nix version from the previous NixOS release.
+"2.8"
