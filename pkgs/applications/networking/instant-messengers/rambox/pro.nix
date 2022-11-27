e2b828dcedabb4b96a92f6aa484dbef0d265fef9
Mikilio <kilian.mio@tum.de>
2022-11-27 16:29:06 +0100

    rambox-pro: 1.5.0 -> 2.0.9

--- pkgs/applications/networking/instant-messengers/rambox/pro.nix
+++ pkgs/applications/networking/instant-messengers/rambox/pro.nix
@@ -4,14 +4,14 @@ let
   mkRambox = opts: callPackage (import ./rambox.nix opts) { };
 in mkRambox rec {
   pname = "rambox-pro";
-  version = "1.5.0";
+  version = "2.0.9";
 
   desktopName = "Rambox Pro";
 
   src = {
     x86_64-linux = fetchurl {
-      url = "https://github.com/ramboxapp/download/releases/download/v${version}/RamboxPro-${version}-linux-x64.AppImage";
-      sha256 = "1g7lrjm8yxklqpc2mp8gy0g61wfilr15dl80r3sh6pa5b4k5spir";
+      url = "https://github.com/ramboxapp/download/releases/download/v${version}/Rambox-${version}-linux-x64.AppImage";
+      hash = "sha256-o2ydZodmMAYeU0IiczKNlzY2hgTJbzyJWO/cZSTfAuM=";
     };
   }.${stdenv.system} or (throw "Unsupported system: ${stdenv.system}");
 
