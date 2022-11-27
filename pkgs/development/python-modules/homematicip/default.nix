d6b142e587033d77acc26fa2950a3255fa152d85
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-27 08:05:39 +0100

    python310Packages.homematicip: update disabled

--- pkgs/development/python-modules/homematicip/default.nix
+++ pkgs/development/python-modules/homematicip/default.nix
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   version = "1.0.10";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "hahn-th";
