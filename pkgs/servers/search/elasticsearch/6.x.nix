18271606a27dbd337515e74387e994f6079c3462
Dmitry Ivankov <divanorama@gmail.com>
2022-11-28 21:55:47 +0100

    elasticsearch6: add libcrypt dependency to fix build
    glibc's libcrypt is deprecated and since
    ff30c899d8bd92d1a1c9f4d4e81455b04cb0868e
    is built by default without libcrypt, that's probably the point when `elasticsearch6`
    started failing with
    ```
    auto-patchelf: 3 dependencies could not be satisfied
    error: auto-patchelf could not satisfy dependency libcrypt.so.1 wanted by /nix/store/nd0gn95yfnnmnnw8zk2jnafc9gj2qy91-elasticsearch-6.8.21/modules/x-pack-ml/platform/linux-x86_64/lib/liblog4cxx.so.10
    error: auto-patchelf could not satisfy dependency libcrypt.so.1 wanted by /nix/store/nd0gn95yfnnmnnw8zk2jnafc9gj2qy91-elasticsearch-6.8.21/modules/x-pack-ml/platform/linux-x86_64/lib/libaprutil-1.so.0
    error: auto-patchelf could not satisfy dependency libcrypt.so.1 wanted by /nix/store/nd0gn95yfnnmnnw8zk2jnafc9gj2qy91-elasticsearch-6.8.21/modules/x-pack-ml/platform/linux-x86_64/lib/libapr-1.so.0
    ```
    
    Let's add libxcrypt dependency, also note that `elasticsearch6-oss` doesn't
    seem to need it.
    
    Should resolve https://github.com/NixOS/nixpkgs/issues/203467
    
    Extra note is elk6 may get removed from nixpkgs soon in favor of elk7
    https://github.com/NixOS/nixpkgs/pull/194420

--- pkgs/servers/search/elasticsearch/6.x.nix
+++ pkgs/servers/search/elasticsearch/6.x.nix
@@ -5,6 +5,7 @@
 , makeWrapper
 , jre_headless
 , util-linux, gnugrep, coreutils
+, libxcrypt
 , autoPatchelfHook
 , zlib
 }:
@@ -37,7 +38,7 @@ stdenv.mkDerivation (rec {
 
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ jre_headless util-linux ]
-             ++ optional enableUnfree zlib;
+             ++ optional enableUnfree [ zlib libxcrypt ];
 
   installPhase = ''
     mkdir -p $out
