1b23119f2186d594f2adb1543bcc1bf293c42ca6
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-11-28 23:51:18 +0100

    qt6.qtconnectivity: fix build on aarch64-darwin

--- pkgs/development/libraries/qt-6/default.nix
+++ pkgs/development/libraries/qt-6/default.nix
@@ -90,7 +90,9 @@ let
       qt3d = callPackage ./modules/qt3d.nix { };
       qt5compat = callPackage ./modules/qt5compat.nix { };
       qtcharts = callPackage ./modules/qtcharts.nix { };
-      qtconnectivity = callPackage ./modules/qtconnectivity.nix { };
+      qtconnectivity = callPackage ./modules/qtconnectivity.nix {
+        inherit (darwin.apple_sdk_11_0.frameworks) PCSC;
+      };
       qtdatavis3d = callPackage ./modules/qtdatavis3d.nix { };
       qtdeclarative = callPackage ./modules/qtdeclarative.nix { };
       qtdoc = callPackage ./modules/qtdoc.nix { };
