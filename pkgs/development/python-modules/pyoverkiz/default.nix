ae2d2e89d787e6cd7e4bc6d8f80aa3362cb6b8d5
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-28 20:10:48 +0100

    python310Packages.pyoverkiz: add changelog to meta

--- pkgs/development/python-modules/pyoverkiz/default.nix
+++ pkgs/development/python-modules/pyoverkiz/default.nix
@@ -57,6 +57,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Module to interact with the Somfy TaHoma API or other OverKiz APIs";
     homepage = "https://github.com/iMicknl/python-overkiz-api";
+    changelog = "https://github.com/iMicknl/python-overkiz-api/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
