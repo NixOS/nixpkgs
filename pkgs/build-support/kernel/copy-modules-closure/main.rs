987bf5e0a4d82e9ba00cbd5ada3cdcb2ef318cb6
Dominik Xaver Hörl <hoe.dom@gmx.de>
2022-11-29 13:34:46 +0100

    copy-modules-closure: use deterministic hasher

--- pkgs/build-support/kernel/copy-modules-closure/main.rs
+++ pkgs/build-support/kernel/copy-modules-closure/main.rs
@@ -12,6 +12,9 @@ use std::io;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::hash::Hash;
+use std::hash::BuildHasherDefault;
+use hashers::fx_hash::FxHasher;
+
 
 fn main() {
     let args : Vec<OsString> = env::args_os().collect();
@@ -240,7 +243,7 @@ fn parse_soft_deps(line : Vec<u8>) -> Vec<OsString> {
 
 fn mod_resolved_deps(kernel : &KernelData,
                      allow_missing : bool,
-                     module : &OsString) -> HashSet<OsString> {
+                     module : &OsString) -> HashSet<OsString,BuildHasherDefault<FxHasher>> {
     let deps = mod_deps(&kernel,allow_missing,&module);
     let soft_deps = mod_soft_deps(&kernel,allow_missing,&module);
     deps.into_iter()
@@ -265,16 +268,16 @@ fn nuke_refs(path : &Path){
 
 
 // Traverse dependency tree in DFS fashion, returns the closure in form of a HashSet.
-fn closure<F : Fn(&T) -> HashSet<T>,T : Hash + Eq + Clone>(
+fn closure<F : Fn(&T) -> HashSet<T,BuildHasherDefault<FxHasher>>,T : Hash + Eq + Clone>(
     f : F,
-    start : &HashSet<T>
+    start : &VecDeque<T>
     )
-    -> HashSet<T> {
+    -> HashSet<T,BuildHasherDefault<FxHasher>> {
 
-    let mut visited = HashSet::new();
+    let mut visited = new_hash_set();
     let mut todo : VecDeque::<T> = start.iter().cloned().collect();
     while let Some(p) = todo.pop_front() {
-        let deps : HashSet<T> = f(&p);
+        let deps : HashSet<T,BuildHasherDefault<FxHasher>> = f(&p);
         visited.insert(p);
         for dep in deps {
             if !visited.contains(&dep) {
@@ -283,6 +286,11 @@ fn closure<F : Fn(&T) -> HashSet<T>,T : Hash + Eq + Clone>(
         }
     }
     return visited;
+}
+
+fn new_hash_set<T>() -> HashSet<T,BuildHasherDefault<FxHasher>> {
+  let hasher = BuildHasherDefault::<FxHasher>::default();
+  HashSet::with_hasher(hasher)
 }
 
 fn to_os(bs : &[u8]) -> OsString {
