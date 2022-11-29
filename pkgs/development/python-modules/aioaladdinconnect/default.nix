c60603e6e1678e339fd6f692107dda3fe29cc8d6
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-29 03:19:34 +0000

    python310Packages.aioaladdinconnect: 0.1.47 -> 0.1.48

--- pkgs/development/python-modules/aioaladdinconnect/default.nix
+++ pkgs/development/python-modules/aioaladdinconnect/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "aioaladdinconnect";
-  version = "0.1.47";
+  version = "0.1.48";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "AIOAladdinConnect";
     inherit version;
-    hash = "sha256-1q5CX5vtdK+rNDE6GtIobuacv6dkHhRJ+kirbX62us8=";
+    hash = "sha256-FrefWV42+DqvuAVScavwqtCfiCRXacuCruqGP0Gks6Y=";
   };
 
   propagatedBuildInputs = [
