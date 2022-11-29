5273b911213a7f042d0be30a0b11ac521686d3b9
Philippe Schaaf <philippe.schaaf@secunet.com>
2022-11-29 11:18:21 +0100

    python3Packages.hatch-requirements-txt: init at 0.2.0
    Signed-off-by: Philippe Schaaf <philippe.schaaf@secunet.com>

---
+++ pkgs/development/python-modules/hatch-requirements-txt/default.nix
@@ -0,0 +1,34 @@
+{ lib
+, fetchFromGitHub
+, buildPythonPackage
+, hatchling
+}:
+
+let 
+  pname = "hatch-requirements-txt";
+  version = "0.2.0";
+in 
+buildPythonPackage {
+  inherit pname version;
+  format = "pyproject";
+
+  src = fetchFromGitHub {
+    owner = "repo-helper";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-YaLWWSZDAdCj3zoAJ6Fc2meH8l6bKd/+1NNlliKgzFo=";
+  };
+
+  nativeBuildInputs = [
+    hatchling
+  ];
+
+  pythonImportsCheck = [ "hatch_requirements_txt" ];
+
+  meta = with lib; {
+    description = "Hatchling plugin to read project dependencies from requirements.txt";
+    homepage = "https://github.com/repo-helper/hatch-requirements-txt";
+    license = licenses.mit;
+    maintainers = with maintainers; [ snpschaaf ];
+  };
+}
