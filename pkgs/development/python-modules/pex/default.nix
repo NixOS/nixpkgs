cb07a145d46c61070e4bc9929da0497ea1597e33
Mario Rodas <marsam@users.noreply.github.com>
2022-11-29 04:20:00 +0000

    python310Packages.pex: 2.1.116 -> 2.1.117
    https://github.com/pantsbuild/pex/releases/tag/v2.1.117

--- pkgs/development/python-modules/pex/default.nix
+++ pkgs/development/python-modules/pex/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "pex";
-  version = "2.1.116";
+  version = "2.1.117";
   format = "flit";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-JWQhU928BJcsDp70OeRnUzU4nC/AxuWCIPzz2jAMPoM=";
+    hash = "sha256-Q1dz/xEQuJOrelDAB3YhISK6HPsqBQXGAx7Qgpqq+6w=";
   };
 
   nativeBuildInputs = [
