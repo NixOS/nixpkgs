45a57d83455a382d759b8182a1a5726079b713c7
R. Ryantm <ryantm-bot@ryantm.com>
2022-11-29 22:36:02 +0000

    python310Packages.fakeredis: 2.0.0 -> 2.1.0

--- pkgs/development/python-modules/fakeredis/default.nix
+++ pkgs/development/python-modules/fakeredis/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "fakeredis";
-  version = "2.0.0";
+  version = "2.1.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "dsoftwareinc";
     repo = "fakeredis-py";
     rev = "refs/tags/v${version}";
-    hash = "sha256-y9fuVg5Mu0ZT8hoja9V5mEfOz/hPH66Zbk5Rr/luPSc=";
+    hash = "sha256-d+colAAESTt2YME8URX3e/l6GsC1l0vzg3wY/NQPkDk=";
   };
 
   nativeBuildInputs = [
