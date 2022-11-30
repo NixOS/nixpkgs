2f075e8a197166216d69ee4318e39976c31848a2
Fabian Affolter <mail@fabian-affolter.ch>
2022-11-30 21:29:31 +0100

    checkSSLCert: add changelog to meta

--- pkgs/servers/monitoring/nagios/plugins/check_ssl_cert.nix
+++ pkgs/servers/monitoring/nagios/plugins/check_ssl_cert.nix
@@ -36,6 +36,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Nagios plugin to check the CA and validity of an X.509 certificate";
     homepage = "https://github.com/matteocorti/check_ssl_cert";
+    changelog = "https://github.com/matteocorti/check_ssl_cert/releases/tag/v${version}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ fab ];
     platforms = platforms.all;
