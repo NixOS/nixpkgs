fd03298cd3d4cac0030f27d554005f8e521b7908
Robert Scott <code@humanleg.org.uk>
2022-11-30 22:32:45 +0000

    mujs: add patch for CVE-2022-44789

--- pkgs/development/interpreters/mujs/default.nix
+++ pkgs/development/interpreters/mujs/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, readline }:
+{ lib, stdenv, fetchurl, readline, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "mujs";
@@ -8,6 +8,14 @@ stdenv.mkDerivation rec {
     url = "https://mujs.com/downloads/mujs-${version}.tar.xz";
     sha256 = "sha256-ZpdtHgajUnVKI0Kvc9Guy7U8x82uK2jNoBO33c+SMjM=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2022-44789.patch";
+      url = "https://git.ghostscript.com/?p=mujs.git;a=patch;h=edb50ad66f7601ca9a3544a0e9045e8a8c60561f";
+      sha256 = "sha256-KHBJlDchrXTmN5vEuyQ+saX12F5hyUlJFguiDsZkGqc=";
+    })
+  ];
 
   buildInputs = [ readline ];
 
