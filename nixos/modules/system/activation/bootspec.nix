ee27291b34cf97ca195797733045da1a1ad49ddb
Raito Bezarius <masterancpp@gmail.com>
2022-11-30 15:08:37 +0100

    nixos/activation/bootspec: fix slurping specialisation bootspecs

--- nixos/modules/system/activation/bootspec.nix
+++ nixos/modules/system/activation/bootspec.nix
@@ -24,7 +24,7 @@ let
       generator =
         let
           specialisationLoader = (lib.mapAttrsToList
-            (childName: childToplevel: lib.escapeShellArgs [ "--slurpfile" childName "${childToplevel}/${filename}" ])
+            (childName: childToplevel: lib.escapeShellArgs [ "--slurpfile" childName "${childToplevel}/bootspec/${filename}" ])
             children);
         in
         ''
