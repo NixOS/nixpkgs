6cacef8c88641fd63b5e8fb3401d6aff3c1ca762
Mario Rodas <marsam@users.noreply.github.com>
2022-11-30 04:20:00 +0000

    python310Packages.posix_ipc: add format attribute

--- pkgs/development/python-modules/posix_ipc/default.nix
+++ pkgs/development/python-modules/posix_ipc/default.nix
@@ -1,21 +1,26 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, isPy3k
 }:
 
 buildPythonPackage rec {
   pname = "posix_ipc";
   version = "1.1.0";
+  format = "setuptools";
+
+  disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-+GoVsys4Vzx44wXr2RANgZij2frMA/+v457cNYM3OOM=";
   };
 
+  pythonImportsCheckHook = [ "posix_ipc" ];
+
   meta = with lib; {
     description = "POSIX IPC primitives (semaphores, shared memory and message queues)";
     license = licenses.bsd3;
     homepage = "http://semanchuk.com/philip/posix_ipc/";
   };
-
 }
