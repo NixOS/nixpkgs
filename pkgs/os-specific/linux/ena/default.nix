c5f9c60d580231e31984664e737e1e4bba740c81
Nicholas Sielicki <nix@opensource.nslick.com>
2022-11-30 01:25:02 +0000

    ena: 2.7.1 -> 2.8.0
    * set ENA_PHC_INCLUDE=1 during build to enable ptp hardware stamping
    * add sielicki to maintainers
    * remove kernel 5.17 broken marker
    
    Signed-off-by: Nicholas Sielicki <nix@opensource.nslick.com>

--- pkgs/os-specific/linux/ena/default.nix
+++ pkgs/os-specific/linux/ena/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, fetchFromGitHub, kernel }:
 
 stdenv.mkDerivation rec {
-  version = "2.7.1";
+  version = "2.8.0";
   name = "ena-${version}-${kernel.version}";
 
   src = fetchFromGitHub {
     owner = "amzn";
     repo = "amzn-drivers";
     rev = "ena_linux_${version}";
-    sha256 = "sha256-JkGzmmsAmLvL9e+bg58H79GNHgsqydK/79VoWEq5/Mc=";
+    sha256 = "sha256-Nw86s93v8ahc/yt5sNsGz4LRwzDPtBnIZNno8vpFdsY=";
   };
 
   hardeningDisable = [ "pic" ];
@@ -22,6 +22,7 @@ stdenv.mkDerivation rec {
   configurePhase = ''
     runHook preConfigure
     cd kernel/linux/ena
+    export ENA_PHC_INCLUDE=1
     substituteInPlace Makefile --replace '/lib/modules/$(BUILD_KERNEL)' ${kernel.dev}/lib/modules/${kernel.modDirVersion}
     runHook postConfigure
   '';
@@ -40,8 +41,7 @@ stdenv.mkDerivation rec {
     description = "Amazon Elastic Network Adapter (ENA) driver for Linux";
     homepage = "https://github.com/amzn/amzn-drivers";
     license = licenses.gpl2Only;
-    maintainers = [ maintainers.eelco ];
+    maintainers = with maintainers; [ eelco sielicki ];
     platforms = platforms.linux;
-    broken = kernel.kernelAtLeast "5.17";
   };
 }
