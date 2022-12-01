64d0d965d3eef58d451ab5bd25605098b91e73ed
figsoda <figsoda@pm.me>
2022-11-30 19:45:53 -0500

    cargo-lambda: avoid impurity

--- pkgs/development/tools/rust/cargo-lambda/default.nix
+++ pkgs/development/tools/rust/cargo-lambda/default.nix
@@ -1,4 +1,12 @@
-{ stdenv, lib, rustPlatform, fetchFromGitHub, makeWrapper, cargo-watch, zig, Security }:
+{ lib
+, rustPlatform
+, fetchFromGitHub
+, makeWrapper
+, stdenv
+, Security
+, cargo-watch
+, zig
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-lambda";
@@ -13,20 +21,25 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-p3q5S6IFQQgNp/MHGSUE1DVLFyMLWDTv/dxrUACKSWo=";
 
+  nativeBuildInputs = [ makeWrapper ];
+
   buildInputs = lib.optionals stdenv.isDarwin [ Security ];
 
-  nativeBuildInputs = [ makeWrapper ];
+  checkFlags = [
+    # Disabled because it accesses the network.
+    "--skip=test_download_example"
+  ];
+
+  # remove date from version output to make reproducible
+  postPatch = ''
+    rm crates/cargo-lambda-cli/build.rs
+  '';
 
   postInstall = ''
     wrapProgram $out/bin/cargo-lambda --prefix PATH : ${lib.makeBinPath [ cargo-watch zig ]}
   '';
 
-  checkFlags = [
-    # Disabled because it accesses the network.
-    "--skip test_download_example"
-    # Disabled because it makes assumptions about the file system.
-    "--skip test_target_dir_from_env"
-  ];
+  CARGO_LAMBDA_BUILD_INFO = "(nixpkgs)";
 
   meta = with lib; {
     description = "A Cargo subcommand to help you work with AWS Lambda";
