f898cc1f6b6d376d002706edbc90f147c28c3c09
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-01 08:44:09 +0000

    python310Packages.pip-tools: 6.8.0 -> 6.11.0

--- pkgs/development/python-modules/pip-tools/default.nix
+++ pkgs/development/python-modules/pip-tools/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "pip-tools";
-  version = "6.8.0";
+  version = "6.11.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Oeiu5GVEbgInjYDb69QyXR3YYzJI9DITxzol9Y59ilU=";
+    hash = "sha256-kMXcFQ44VuRGO4HMyZMHzPlVTl24OT6yc3BcsLj3HGA=";
   };
 
   patches = [ ./fix-setup-py-bad-syntax-detection.patch ];
