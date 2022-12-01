faf60907d8d1acb1864622a66ec305d794a57312
Robert Scott <code@humanleg.org.uk>
2022-12-01 21:30:03 +0000

    mujs: add patch for unannounced stack overflow

--- pkgs/development/interpreters/mujs/default.nix
+++ pkgs/development/interpreters/mujs/default.nix
@@ -15,6 +15,11 @@ stdenv.mkDerivation rec {
       url = "https://git.ghostscript.com/?p=mujs.git;a=patch;h=edb50ad66f7601ca9a3544a0e9045e8a8c60561f";
       sha256 = "sha256-KHBJlDchrXTmN5vEuyQ+saX12F5hyUlJFguiDsZkGqc=";
     })
+    (fetchpatch {
+      name = "regex-compilation-stack-overflow.patch";
+      url = "https://git.ghostscript.com/?p=mujs.git;a=patch;h=160ae29578054dc09fd91e5401ef040d52797e61";
+      sha256 = "sha256-R0gTPj10fKtHbVe5RM6HNL772hVl9rXylkPw5qQWt0w=";
+    })
   ];
 
   buildInputs = [ readline ];
