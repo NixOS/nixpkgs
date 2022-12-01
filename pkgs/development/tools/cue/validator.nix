f25d48180af90638001104a61d6d10b22e4ecb28
Raito Bezarius <masterancpp@gmail.com>
2022-12-01 17:59:53 +0100

    cue: add documentation on writeCueValidator

--- pkgs/development/tools/cue/validator.nix
+++ pkgs/development/tools/cue/validator.nix
@@ -1,4 +1,9 @@
 { cue, writeShellScript, lib }:
+# `document` must be a fragment of definition or structure that the input data will be matched against.
+# `document` must exist in the Cue schema file provided (`cueSchemaFile`).
+# The result is a script that can be used to validate the input data (JSON/YAML and more can be supported depending on Cue)
+# against the fragment described by `document` or the whole definition.
+# The script will be strict and enforce concrete values, i.e. partial documents are not supported.
 cueSchemaFile: { document ? null }:
   writeShellScript "validate-using-cue"
   ''${cue}/bin/cue \
