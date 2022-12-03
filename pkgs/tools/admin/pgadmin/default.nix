e5341b067de05360d7488905a185ee2cc62b0e7a
Thomas Gerbet <thomas@gerbet.me>
2022-12-03 12:19:05 +0100

    pgamin4: 6.16 -> 6.17
    Fixes CVE-2022-4223.
    
    https://www.pgadmin.org/docs/pgadmin4/development/release_notes_6_17.html

--- pkgs/tools/admin/pgadmin/default.nix
+++ pkgs/tools/admin/pgadmin/default.nix
@@ -10,11 +10,11 @@
 
 let
   pname = "pgadmin";
-  version = "6.16";
+  version = "6.17";
 
   src = fetchurl {
     url = "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v${version}/source/pgadmin4-${version}.tar.gz";
-    sha256 = "sha256-v2CfoV7QAzLcPXKY5nkUi9+HbHujiulUJS4GVBKxTY4=";
+    sha256 = "sha256-fcMNqki0namB5mRntlJUE9cN+axlw+7b8EA9aactlIw=";
   };
 
   yarnDeps = mkYarnModules {
