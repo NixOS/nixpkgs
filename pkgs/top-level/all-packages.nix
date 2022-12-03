b3752fe3bb7a4b521ed71137278ea2fa06ff6728
Mustafa Çalışkan <muscaln@protonmail.com>
2022-12-03 19:28:46 +0300

    vkmark: init at unstable-2022-09-09

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12505,6 +12505,8 @@ with pkgs;
     vkBasalt32 = pkgsi686Linux.vkBasalt;
   };
 
+  vkmark = callPackage ../tools/graphics/vkmark { };
+
   vncrec = callPackage ../tools/video/vncrec { };
 
   vo-amrwbenc = callPackage ../development/libraries/vo-amrwbenc { };
