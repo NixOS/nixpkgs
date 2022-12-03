8c97a145fc3c883a9fb009152dc864f265f4bb50
Matt Huszagh <huszaghmatt@gmail.com>
2022-12-02 22:23:40 -0800

    vivado: replace 2017.2 with $version

--- pkgs/applications/science/electronics/vivado/default.nix
+++ pkgs/applications/science/electronics/vivado/default.nix
@@ -82,36 +82,36 @@ stdenv.mkDerivation rec {
         echo ''${line}
     done
     # Patch installed files
-    patchShebangs $out/opt/Vivado/2017.2/bin
-    patchShebangs $out/opt/SDK/2017.2/bin
+    patchShebangs $out/opt/Vivado/$version/bin
+    patchShebangs $out/opt/SDK/$version/bin
     echo "Shebangs patched"
     # Hack around lack of libtinfo in NixOS
-    ln -s $ncurses/lib/libncursesw.so.6 $out/opt/Vivado/2017.2/lib/lnx64.o/libtinfo.so.5
-    ln -s $ncurses/lib/libncursesw.so.6 $out/opt/SDK/2017.2/lib/lnx64.o/libtinfo.so.5
+    ln -s $ncurses/lib/libncursesw.so.6 $out/opt/Vivado/$version/lib/lnx64.o/libtinfo.so.5
+    ln -s $ncurses/lib/libncursesw.so.6 $out/opt/SDK/$version/lib/lnx64.o/libtinfo.so.5
     # Patch ELFs
-    for f in $out/opt/Vivado/2017.2/bin/unwrapped/lnx64.o/*
+    for f in $out/opt/Vivado/$version/bin/unwrapped/lnx64.o/*
     do
         patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $f || true
     done
-    for f in $out/opt/SDK/2017.2/bin/unwrapped/lnx64.o/*
+    for f in $out/opt/SDK/$version/bin/unwrapped/lnx64.o/*
     do
         patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $f || true
     done
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/SDK/2017.2/eclipse/lnx64.o/eclipse
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/SDK/2017.2/tps/lnx64/jre/bin/java
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/SDK/$version/eclipse/lnx64.o/eclipse
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/SDK/$version/tps/lnx64/jre/bin/java
     echo "ELFs patched"
-    wrapProgram $out/opt/Vivado/2017.2/bin/vivado --prefix LD_LIBRARY_PATH : "$libPath"
-    wrapProgram $out/opt/SDK/2017.2/bin/xsdk --prefix LD_LIBRARY_PATH : "$libPath"
-    wrapProgram $out/opt/SDK/2017.2/eclipse/lnx64.o/eclipse --prefix LD_LIBRARY_PATH : "$libPath"
-    wrapProgram $out/opt/SDK/2017.2/tps/lnx64/jre/bin/java --prefix LD_LIBRARY_PATH : "$libPath"
+    wrapProgram $out/opt/Vivado/$version/bin/vivado --prefix LD_LIBRARY_PATH : "$libPath"
+    wrapProgram $out/opt/SDK/$version/bin/xsdk --prefix LD_LIBRARY_PATH : "$libPath"
+    wrapProgram $out/opt/SDK/$version/eclipse/lnx64.o/eclipse --prefix LD_LIBRARY_PATH : "$libPath"
+    wrapProgram $out/opt/SDK/$version/tps/lnx64/jre/bin/java --prefix LD_LIBRARY_PATH : "$libPath"
     # wrapProgram on its own will not work because of the way the Vivado script runs ./launch
     # Therefore, we need Even More Patches...
-    sed -i -- 's|`basename "\$0"`|vivado|g' $out/opt/Vivado/2017.2/bin/.vivado-wrapped
-    sed -i -- 's|`basename "\$0"`|xsdk|g' $out/opt/SDK/2017.2/bin/.xsdk-wrapped
+    sed -i -- 's|`basename "\$0"`|vivado|g' $out/opt/Vivado/$version/bin/.vivado-wrapped
+    sed -i -- 's|`basename "\$0"`|xsdk|g' $out/opt/SDK/$version/bin/.xsdk-wrapped
     # Add vivado and xsdk to bin folder
     mkdir $out/bin
-    ln -s $out/opt/Vivado/2017.2/bin/vivado $out/bin/vivado
-    ln -s $out/opt/SDK/2017.2/bin/xsdk $out/bin/xsdk
+    ln -s $out/opt/Vivado/$version/bin/vivado $out/bin/vivado
+    ln -s $out/opt/SDK/$version/bin/xsdk $out/bin/xsdk
   '';
 
   dontInstall = true;
