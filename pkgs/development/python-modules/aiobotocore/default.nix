b51dcff820c053d5c47b003c817a8ded61bc3266
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-03 12:51:51 +0100

    python310Packages.aiobotocore: enable tests

--- pkgs/development/python-modules/aiobotocore/default.nix
+++ pkgs/development/python-modules/aiobotocore/default.nix
@@ -1,11 +1,15 @@
 { lib
+, aiohttp
+, aioitertools
+, botocore
 , buildPythonPackage
-, fetchPypi
+, dill
+, fetchFromGitHub
+, moto
+, pytest-asyncio
+, pytestCheckHook
 , pythonOlder
 , wrapt
-, aioitertools
-, aiohttp
-, botocore
 }:
 
 buildPythonPackage rec {
@@ -15,9 +19,11 @@ buildPythonPackage rec {
 
   disabled = pythonOlder "3.7";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-XI7nnR8UJz4/jzr1yJPbiJ7xYyJ7F47Ky1rqRUfTuac=";
+  src = fetchFromGitHub {
+    owner = "aio-libs";
+    repo = pname;
+    rev = "refs/tags/${version}";
+    hash = "sha256-jJ1Yc5vs33vXdSjDFUXhdquz1s7NxzJELQsM3hthhzg=";
   };
 
   # Relax version constraints: aiobotocore works with newer botocore versions
@@ -27,17 +33,49 @@ buildPythonPackage rec {
   '';
 
   propagatedBuildInputs = [
-    wrapt
     aiohttp
     aioitertools
     botocore
+    wrapt
   ];
 
-  # Tests not distributed on PyPI
-  doCheck = false;
+  checkInputs = [
+    dill
+    moto
+    pytest-asyncio
+    pytestCheckHook
+  ];
 
   pythonImportsCheck = [
     "aiobotocore"
+  ];
+
+  disabledTestPaths = [
+    # Tests require network access
+    "tests/boto_tests/test_signers.py"
+    "tests/python3.8/"
+    "tests/test_basic_s3.py"
+    "tests/test_batch.py"
+    "tests/test_dynamodb.py"
+    "tests/test_ec2.py"
+    "tests/test_eventstreams.py"
+    "tests/test_lambda.py"
+    "tests/test_monitor.py"
+    "tests/test_mturk.py"
+    "tests/test_patches.py"
+    "tests/test_sns.py"
+    "tests/test_sqs.py"
+    "tests/test_version.py"
+    "tests/test_waiter.py"
+  ];
+
+  disabledTests = [
+    "test_get_credential"
+    "test_load_sso_credentials_without_cache"
+    "test_load_sso_credentials"
+    "test_required_config_not_set"
+    "test_sso_cred_fetcher_raises_helpful_message_on_unauthorized_exception"
+    "test_sso_credential_fetcher_can_fetch_credentials"
   ];
 
   meta = with lib; {
