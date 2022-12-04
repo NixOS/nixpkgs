8fd54278951c839f7d437d716e1886d0c5fd7c98
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-04 18:12:00 +0100

    python310Packages.weconnect-mqtt: add changelog to meta

--- pkgs/development/python-modules/weconnect-mqtt/default.nix
+++ pkgs/development/python-modules/weconnect-mqtt/default.nix
@@ -47,6 +47,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python client that publishes data from Volkswagen WeConnect";
     homepage = "https://github.com/tillsteinbach/WeConnect-mqtt";
+    changelog = "https://github.com/tillsteinbach/WeConnect-mqtt/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
