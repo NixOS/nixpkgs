a3d4659349693b7f406e3441f6e77814a145d838
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-04 18:23:07 +0100

    python310Packages.weconnect-mqtt: 0.40.3 -> 0.41.1
    Diff: https://github.com/tillsteinbach/WeConnect-mqtt/compare/refs/tags/v0.40.3...v0.41.1
    
    Changelog: https://github.com/tillsteinbach/WeConnect-mqtt/releases/tag/v0.41.1

--- pkgs/development/python-modules/weconnect-mqtt/default.nix
+++ pkgs/development/python-modules/weconnect-mqtt/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "weconnect-mqtt";
-  version = "0.40.3";
+  version = "0.41.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "tillsteinbach";
     repo = "WeConnect-mqtt";
     rev = "refs/tags/v${version}";
-    hash = "sha256-kV4BWQ4XfB2QjXY5b46+pxt3rhyo1glKRYO2mMJNhJM=";
+    hash = "sha256-RYxfz5uBWV1HLbcIK1N/glJv2w0nfPLBPyM2b7HuDIY=";
   };
 
   propagatedBuildInputs = [
