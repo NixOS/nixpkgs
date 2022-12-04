1eb4e747322e8941c40329b1194f2174c2027b7f
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-04 23:12:16 +0100

    pony-corral: add changelog to meta

--- pkgs/development/compilers/ponyc/pony-corral.nix
+++ pkgs/development/compilers/ponyc/pony-corral.nix
@@ -1,4 +1,8 @@
-{ lib, stdenv, fetchFromGitHub, ponyc }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, ponyc
+}:
 
 stdenv.mkDerivation ( rec {
   pname = "corral";
@@ -7,8 +11,8 @@ stdenv.mkDerivation ( rec {
   src = fetchFromGitHub {
     owner = "ponylang";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-Rv1K6kFRylWodm1uACBs8KqqEqQZh86NqAG50heNteE=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-Rv1K6kFRylWodm1uACBs8KqqEqQZh86NqAG50heNteE=";
   };
 
   buildInputs = [ ponyc ];
@@ -18,6 +22,7 @@ stdenv.mkDerivation ( rec {
   meta = with lib; {
     description = "Corral is a dependency management tool for ponylang (ponyc)";
     homepage = "https://www.ponylang.io";
+    changelog = "https://github.com/ponylang/corral/blob/${version}/CHANGELOG.md";
     license = licenses.bsd2;
     maintainers = with maintainers; [ redvers ];
     platforms = [ "x86_64-linux" "x86_64-darwin" ];
