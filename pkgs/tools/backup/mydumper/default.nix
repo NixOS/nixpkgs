fdfa7cffd824235dd1affe8ca8c9beaed8ceeb80
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-04 13:33:04 +0100

    mydumper: add changelog to meta

--- pkgs/tools/backup/mydumper/default.nix
+++ pkgs/tools/backup/mydumper/default.nix
@@ -13,25 +13,37 @@
 let inherit (lib) getDev; in
 
 stdenv.mkDerivation rec {
-  version = "0.13.1-1";
   pname = "mydumper";
+  version = "0.13.1-1";
 
   src = fetchFromGitHub {
     owner  = "maxbube";
-    repo   = "mydumper";
-    rev    = "v${version}";
-    sha256 = "sha256-Oknivkyr3wOfjnDccEeFVt7D2l1CkeWgXahsQCtAc0I=";
+    repo = "mydumper";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-Oknivkyr3wOfjnDccEeFVt7D2l1CkeWgXahsQCtAc0I=";
   };
 
-  nativeBuildInputs = [ cmake pkg-config ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
 
-  buildInputs = [ glib zlib pcre libmysqlclient libressl ];
+  buildInputs = [
+    glib
+    zlib
+    pcre
+    libmysqlclient
+    libressl
+  ];
 
-  cmakeFlags = [ "-DMYSQL_INCLUDE_DIR=${getDev libmysqlclient}/include/mysql" ];
+  cmakeFlags = [
+    "-DMYSQL_INCLUDE_DIR=${getDev libmysqlclient}/include/mysql"
+  ];
 
   meta = with lib; {
     description = "High-perfomance MySQL backup tool";
     homepage = "https://github.com/maxbube/mydumper";
+    changelog = "https://github.com/mydumper/mydumper/releases/tag/v${version}";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ izorkin ];
