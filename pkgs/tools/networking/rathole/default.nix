e0e9a50e36be2ad34d96962eed496c9f621592bf
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-04 22:59:41 +0100

    rathole: add changelog to meta

--- pkgs/tools/networking/rathole/default.nix
+++ pkgs/tools/networking/rathole/default.nix
@@ -6,6 +6,7 @@
 , openssl
 , CoreServices
 }:
+
 rustPlatform.buildRustPackage rec {
   pname = "rathole";
   version = "0.4.7";
@@ -13,21 +14,28 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "rapiz1";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-YauQg+P4Y8oO8Kn6FB3NxBI7PHoo/bjS38bM1lFeCH0=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-YauQg+P4Y8oO8Kn6FB3NxBI7PHoo/bjS38bM1lFeCH0=";
   };
 
-  cargoSha256 = "sha256-OcPmHqjW79SKMET6J5HIwmR5vESh+PJcQjSMsqmnIb8=";
+  cargoHash = "sha256-OcPmHqjW79SKMET6J5HIwmR5vESh+PJcQjSMsqmnIb8=";
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+  ];
 
-  buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ CoreServices ];
+  buildInputs = [
+    openssl
+  ] ++ lib.optionals stdenv.isDarwin [
+    CoreServices
+  ];
 
   __darwinAllowLocalNetworking = true;
 
   meta = with lib; {
-    description = "A lightweight and high-performance reverse proxy for NAT traversal, written in Rust";
+    description = "Reverse proxy for NAT traversal";
     homepage = "https://github.com/rapiz1/rathole";
+    changelog = "https://github.com/rapiz1/rathole/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ dit7ya ];
   };
