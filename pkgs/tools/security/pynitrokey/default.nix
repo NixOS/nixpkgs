73d013b64e390b0b87629d6fb2d960ad795d0910
Johannes Schleifenbaum <johannes@js-webcoding.de>
2022-12-04 09:56:07 +0100

    pynitrokey: 0.4.30 -> 0.4.31

--- pkgs/tools/security/pynitrokey/default.nix
+++ pkgs/tools/security/pynitrokey/default.nix
@@ -4,12 +4,12 @@ with python3Packages;
 
 buildPythonApplication rec {
   pname = "pynitrokey";
-  version = "0.4.30";
+  version = "0.4.31";
   format = "flit";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-JmvA8Z1oFOP62TpJ0g7VO5hq3YS3/DFB/BKt2o79AOE=";
+    sha256 = "sha256-nqw5wUzQxKCBzYBRhqB6v7WWrF1Ojf8z6Kf1YUA9+wU=";
   };
 
   propagatedBuildInputs = [
@@ -29,6 +29,8 @@ buildPythonApplication rec {
     cffi
     cbor
     nkdfu
+    fido2
+    tlv8
   ];
 
   # spsdk is patched to allow for newer cryptography
