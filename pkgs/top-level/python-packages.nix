54d3bd62ab41f2bad9abb02bb7ff62a1a0b47780
Doron Behar <doron.behar@gmail.com>
2022-12-04 13:13:42 +0200

    python310Packages.lsprotocol: init at 2022.0.0a9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5538,6 +5538,8 @@ self: super: with self; {
 
   lsassy = callPackage ../development/python-modules/lsassy { };
 
+  lsprotocol = callPackage ../development/python-modules/lsprotocol { };
+
   luddite = callPackage ../development/python-modules/luddite { };
 
   ludios_wpull = callPackage ../development/python-modules/ludios_wpull { };
