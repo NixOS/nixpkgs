40ed5fe391fa8a0a1f7e0c234b067ded1f0eaf6c
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-04 10:50:01 +0100

    python310Packages.thriftpy2: asdd changelog to meta

--- pkgs/development/python-modules/thriftpy2/default.nix
+++ pkgs/development/python-modules/thriftpy2/default.nix
@@ -2,8 +2,8 @@
 , buildPythonPackage
 , cython
 , fetchFromGitHub
-, pythonOlder
 , ply
+, pythonOlder
 , six
 , tornado
 }:
@@ -11,6 +11,7 @@
 buildPythonPackage rec {
   pname = "thriftpy2";
   version = "0.4.15";
+  format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
@@ -18,7 +19,7 @@ buildPythonPackage rec {
     owner = "Thriftpy";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-1X1roRFQgLJ+rrRuRlHMVtUdj2D68WGdxvid/0chsvI=";
+    hash = "sha256-1X1roRFQgLJ+rrRuRlHMVtUdj2D68WGdxvid/0chsvI=";
   };
 
   nativeBuildInputs = [
@@ -34,11 +35,14 @@ buildPythonPackage rec {
   # Not all needed files seems to be present
   doCheck = false;
 
-  pythonImportsCheck = [ "thriftpy2" ];
+  pythonImportsCheck = [
+    "thriftpy2"
+  ];
 
   meta = with lib; {
     description = "Python module for Apache Thrift";
     homepage = "https://github.com/Thriftpy/thriftpy2";
+    changelog = "https://github.com/Thriftpy/thriftpy2/blob/v${version}/CHANGES.rst";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
