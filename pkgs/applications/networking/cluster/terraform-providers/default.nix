NixOS/nixpkgs

commit e09752334cba82b1cf7f29c9080ada0538e0b076

terraform-providers: remove providers with unclear licensing
https://api.github.com/repos/logicmonitor/terraform-provider-logicmonitor/license
https://api.github.com/repos/vmware/terraform-provider-nsxt/license
https://api.github.com/repos/ondrejsika/terraform-provider-vercel/license

pkgs/applications/networking/cluster/terraform-providers/default.nix
@@ -90,17 +90,21 @@ let
   removed-providers =
     let
       archived = name: date: throw "the ${name} terraform provider has been archived by upstream on ${date}";
+      license = name: date: throw "the ${name} terraform provider removed from nixpkgs on ${date} because of unclear licensing";
       removed = name: date: throw "the ${name} terraform provider removed from nixpkgs on ${date}";
     in
     lib.optionalAttrs config.allowAliases {
       b2 = removed "b2" "2022/06";
       checkpoint = removed "checkpoint" "2022/11";
       dome9 = removed "dome9" "2022/08";
+      logicmonitor = license "logicmonitor" "2022/11";
       ncloud = removed "ncloud" "2022/08";
+      nsxt = license "nsxt" "2022/11";
       opc = archived "opc" "2022/05";
       oraclepaas = archived "oraclepaas" "2022/05";
       panos = removed "panos" "2022/05";
       template = archived "template" "2022/05";
+      vercel = license "vercel" "2022/11";
     };
 
   # excluding aliases, used by terraform-full

commit d145d9f34fcef9e5f6cec42af73a413e87c5aeaa

terraform-providers: passthru.provider-source-address
Fixes a regression caused by #203000: the attr was turned into an
optional argument, so was no longer being included in `passthru`.

pkgs/applications/networking/cluster/terraform-providers/default.nix
@@ -61,6 +61,7 @@ let
 
         # Keep the attributes around for later consumption
         passthru = attrs // {
+          inherit provider-source-address;
           updateScript = writeShellScript "update" ''
             provider="$(basename ${provider-source-address})"
             ./pkgs/applications/networking/cluster/terraform-providers/update-provider "$provider"