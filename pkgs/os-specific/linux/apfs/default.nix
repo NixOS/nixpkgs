732b59e37016f247a62819163b85f120e7c79b11
Luflosi <luflosi@luflosi.de>
2022-12-05 14:57:30 +0100

    linuxPackages.apfs: unstable-2022-08-15 -> unstable-2022-10-20
    This fixes a compatibility issue with Linux 6.0: https://github.com/linux-apfs/linux-apfs-rw/commit/e2ca36a76d35aea025e4b76f3ffb18401fa554bc.

--- pkgs/os-specific/linux/apfs/default.nix
+++ pkgs/os-specific/linux/apfs/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation {
   pname = "apfs";
-  version = "unstable-2022-08-15-${kernel.version}";
+  version = "unstable-2022-10-20-${kernel.version}";
 
   src = fetchFromGitHub {
     owner = "linux-apfs";
     repo = "linux-apfs-rw";
-    rev = "e4bf2d51d3fe8485ad2b28a89c157ada32ee3d77";
-    sha256 = "sha256-zvl1H9AIExgt6t2A2w7zDwXmRsmLY8y3P6EfbBuFdh8=";
+    rev = "e6eb67c92d425d395eac1c4403629391bdd5064d";
+    sha256 = "sha256-6rv5qZCjOqt0FaNFhA3tYg6/SdssvoT8kPVhalajgOo=";
   };
 
   hardeningDisable = [ "pic" ];
