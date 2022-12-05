dcbb3c0b48aefbeaa2881815a63711bd2d08f29c
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-12-05 15:05:58 +0100

    jemalloc: move comment to right place, update website

--- pkgs/development/libraries/jemalloc/default.nix
+++ pkgs/development/libraries/jemalloc/default.nix
@@ -28,9 +28,9 @@ stdenv.mkDerivation rec {
     })
   ];
 
-  # see the comment on stripPrefix
-  configureFlags = []
-    ++ lib.optional stripPrefix "--with-jemalloc-prefix="
+  configureFlags =
+    # see the comment on stripPrefix
+    lib.optional stripPrefix "--with-jemalloc-prefix="
     ++ lib.optional disableInitExecTls "--disable-initial-exec-tls"
     # jemalloc is unable to correctly detect transparent hugepage support on
     # ARM (https://github.com/jemalloc/jemalloc/issues/526), and the default
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   meta = with lib; {
-    homepage = "http://jemalloc.net";
+    homepage = "https://jemalloc.net/";
     description = "General purpose malloc(3) implementation";
     longDescription = ''
       malloc(3)-compatible memory allocator that emphasizes fragmentation
