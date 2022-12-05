4636b0f75121b01d0d5d9df772398f82211f914f
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-05 15:48:41 +0000

    checkSSLCert: 2.56.0 -> 2.57.0

--- pkgs/servers/monitoring/nagios/plugins/check_ssl_cert.nix
+++ pkgs/servers/monitoring/nagios/plugins/check_ssl_cert.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation rec {
   pname = "check_ssl_cert";
-  version = "2.56.0";
+  version = "2.57.0";
 
   src = fetchFromGitHub {
     owner = "matteocorti";
     repo = "check_ssl_cert";
     rev = "v${version}";
-    hash = "sha256-2Ch5XnHs2PIsmI6JKYQPsDqcUSgWE5+u3vHqAW/SB8E=";
+    hash = "sha256-N+VkdVeJ6UdRPFUFmIpZoL/Mc8MkTd+hAPjha5pimt8=";
   };
 
   nativeBuildInputs = [
