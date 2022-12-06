1ec1413384a84541f853fb48571f55596fa350d6
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-06 16:51:20 +0000

    pocketbase: 0.8.0 -> 0.9.0

--- pkgs/servers/pocketbase/default.nix
+++ pkgs/servers/pocketbase/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "pocketbase";
-  version = "0.8.0";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "pocketbase";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-z7Vs+Z34r5g62X9DnEVkqTrr+V2bWwkfMXitNz+pVN8=";
+    sha256 = "sha256-eWYx45k18HDcXkgfKl9G4Q1h/BcCy+NDVhh5ndoW+Fk=";
   };
 
-  vendorSha256 = "sha256-Ya+D15eAJ7qgEQoM2LcN2VEmmyp4cuS6/wCOEGdrgA8=";
+  vendorSha256 = "sha256-+wPcCF5AZ1HzNH1egCiK4eUQ0UmPPJzKB45lk7SOuCI=";
 
   # This is the released subpackage from upstream repo
   subPackages = [ "examples/base" ];
