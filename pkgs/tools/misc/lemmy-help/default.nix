b2d0c7ba65ea14d5433a093a94b74cf31ddaa991
figsoda <figsoda@pm.me>
2022-12-06 10:43:51 -0500

    lemmy-help: 0.9.0 -> 0.10.0, add figsoda as a maintainer
    Diff: https://github.com/numToStr/lemmy-help/compare/v0.9.0...v0.10.0
    
    Changelog: https://github.com/numToStr/lemmy-help/releases/tag/v0.10.0

--- pkgs/tools/misc/lemmy-help/default.nix
+++ pkgs/tools/misc/lemmy-help/default.nix
@@ -2,18 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "lemmy-help";
-  version = "0.9.0";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "numToStr";
     repo = "lemmy-help";
     rev = "v${version}";
-    sha256 = "sha256-VY8sGxS8wwrezTe4ht9xdr4iE2n9fNSNhfCeGDJL5Lo=";
+    sha256 = "sha256-gsYVrqPcabLCMYN3Gmr6CXTCKKFAy2rDCxmcRwR1Iic=";
   };
 
   buildFeatures = [ "cli" ];
 
-  cargoSha256 = "sha256-yj14kg41EqOco0gx79n8xhf8cyotZ1Mxj2AbNV9TImU=";
+  cargoSha256 = "sha256-iyMEzxCTxJ/CP3UEnLc4SN5zhIjCLGUl4OOk0u0bCJc=";
 
   meta = with lib; {
     description = "A CLI for generating vim help docs from emmylua comments";
@@ -21,6 +21,8 @@ rustPlatform.buildRustPackage rec {
       `lemmy-help` is an emmylua parser as well as a CLI which takes that parsed tree and converts it into vim help docs.
     '';
     homepage = "https://github.com/numToStr/lemmy-help";
+    changelog = "https://github.com/numToStr/lemmy-help/releases/tag/v${version}";
     license = with licenses; [ mit ];
+    maintainers = with maintainers; [ figsoda ];
   };
 }
