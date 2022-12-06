d334c899ccb243bceee8ff488db3e7f829777383
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-06 19:11:53 +0100

    python310Packages.psrpcore: 0.1.2 -> 0.2.0
    Diff: https://github.com/jborean93/psrpcore/compare/refs/tags/v0.1.2...v0.2.0
    
    Changelog: https://github.com/jborean93/psrpcore/blob/v0.2.0/CHANGELOG.md

--- pkgs/development/python-modules/psrpcore/default.nix
+++ pkgs/development/python-modules/psrpcore/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "psrpcore";
-  version = "0.1.2";
+  version = "0.2.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "jborean93";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-f1NGE+wSgi8yqBicZZRfUqzinsqazuIaoAje2y+dK1w=";
+    hash = "sha256-uX99BsQn1Ckl+2Lt4I0EMZLTKeDrX0mtSc9w5aFpvxQ=";
   };
 
   propagatedBuildInputs = [
