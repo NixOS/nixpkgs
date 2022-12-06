9176309a19ff3042798aad359087085e7b63fca7
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-06 07:55:16 +0000

    lefthook: 1.2.3 -> 1.2.4

--- pkgs/applications/version-management/git-and-tools/lefthook/default.nix
+++ pkgs/applications/version-management/git-and-tools/lefthook/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "lefthook";
-  version = "1.2.3";
+  version = "1.2.4";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "evilmartians";
     repo = "lefthook";
-    sha256 = "sha256-tns/JUOYlgG+oiFACnXlCwHBF4enctV4GPlpEZCM/nM=";
+    sha256 = "sha256-Z6j/Y8b9lq2nYS5Ki8iJoDsG3l5M6RylfDqQL7WrwNg=";
   };
 
   vendorSha256 = "sha256-sBcgt2YsV9RQhSjPN6N54tRk7nNvcOVhPEsEP+0Dtco=";
