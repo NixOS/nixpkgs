1751edf6b98a7fa84ab042fbae67382077908626
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-06 19:11:14 +0100

    python310Packages.psrpcore: add changelog to meta

--- pkgs/development/python-modules/psrpcore/default.nix
+++ pkgs/development/python-modules/psrpcore/default.nix
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "jborean93";
     repo = pname;
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     hash = "sha256-f1NGE+wSgi8yqBicZZRfUqzinsqazuIaoAje2y+dK1w=";
   };
 
@@ -38,6 +38,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library for the PowerShell Remoting Protocol (PSRP)";
     homepage = "https://github.com/jborean93/psrpcore";
+    changelog = "https://github.com/jborean93/psrpcore/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
