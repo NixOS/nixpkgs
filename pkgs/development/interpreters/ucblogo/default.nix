67814e545ae9b365668adbc3031047867156d02e
David Lahm <david@klinger-lahm.de>
2022-12-06 10:06:08 +0100

    ucblogo: use fetchFromGitHub, should work in restricted mode now.

--- pkgs/development/interpreters/ucblogo/default.nix
+++ pkgs/development/interpreters/ucblogo/default.nix
@@ -1,39 +1,39 @@
-{
-        wxGTK32
-,       wrapGAppsHook
-,       autoreconfHook
-,       autoconf-archive
-,       texinfo
-,       tetex
-,       stdenv
-,       fetchFromGitHub
-,       gtk3
-,       lib
+{ wxGTK32
+, wrapGAppsHook
+, autoreconfHook
+, autoconf-archive
+, texinfo
+, tetex
+, stdenv
+, fetchFromGitHub
+, gtk3
+, lib
 }:
 stdenv.mkDerivation {
   pname = "ucblogo";
   version = "6.2.2";
-  src = builtins.fetchgit {
+  src = fetchFromGitHub {
     rev = "0ba6ad1629f94a89f98e2bf884c1388c6ee78b6d";
-    ref = "master";
-    url = "https://github.com/jrincayc/ucblogo-code.git";
+    sha256 = "FyyNUUQ0cc6ZVcIzZPctMBws1uWSnWKvYJTN0BOL/28=";
+    owner = "jrincayc";
+    repo = "ucblogo-code";
   };
   nativeBuildInputs = [
-        autoreconfHook
-        texinfo
-        tetex
-        autoconf-archive
+    autoreconfHook
+    texinfo
+    tetex
+    autoconf-archive
   ];
   buildInputs = [
-        gtk3
-        wxGTK32
-        wrapGAppsHook
-      ];
+    gtk3
+    wxGTK32
+    wrapGAppsHook
+  ];
   meta = with lib; {
-      description = "Berkeley Logo interpreter";
-      maintainers = with maintainers; [hllizi];
-      license = lib.licenses.gpl3 ;
-      platforms = lib.platforms.linux;
-      homepage = "https://people.eecs.berkeley.edu/~bh/logo.html";
-    };
+    description = "Berkeley Logo interpreter";
+    maintainers = with maintainers; [ hllizi ];
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.linux;
+    homepage = "https://people.eecs.berkeley.edu/~bh/logo.html";
+  };
 }
