fdca9d18daf15c8c46176479ba0ff8c42966ebfa
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-07 10:49:56 +0100

    python310Packages.google-cloud-spanner: add changelog to meta

--- pkgs/development/python-modules/google-cloud-spanner/default.nix
+++ pkgs/development/python-modules/google-cloud-spanner/default.nix
@@ -69,6 +69,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Cloud Spanner API client library";
     homepage = "https://github.com/googleapis/python-spanner";
+    changelog = "https://github.com/googleapis/python-spanner/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
