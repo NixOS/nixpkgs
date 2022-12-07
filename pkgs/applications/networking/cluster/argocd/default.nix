04632bb5c3480ee5ad30d9ca078cf926be098704
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-07 16:50:44 +0000

    argocd: 2.5.3 -> 2.5.4
    (cherry picked from commit 26cee9b6b331897c987addc76e20bfa20451cf2a)

--- pkgs/applications/networking/cluster/argocd/default.nix
+++ pkgs/applications/networking/cluster/argocd/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "argocd";
-  version = "2.5.3";
+  version = "2.5.4";
 
   src = fetchFromGitHub {
     owner = "argoproj";
     repo = "argo-cd";
     rev = "v${version}";
-    sha256 = "sha256-cL1QV0D8m8rqSDuQgsYBPY7n5K2dy9s9c8VRx65+SV0=";
+    sha256 = "sha256-Wwm9YN/gPsb4AU+JFUDrJQbuK5AEqzX5/D64/6tOtkw=";
   };
 
   proxyVendor = true; # darwin/linux hash mismatch
