e498a1cdf2addd1d49369ecaa6468809b7f4a025
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-07 10:01:28 +0000

    python310Packages.gspread: 5.7.1 -> 5.7.2

--- pkgs/development/python-modules/gspread/default.nix
+++ pkgs/development/python-modules/gspread/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "gspread";
-  version = "5.7.1";
+  version = "5.7.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-XznXohg0O2UU8G4iUODEE+tOgU3eaqv0hdrLaqMcqrA=";
+    hash = "sha256-znb5wWuIzLeSNQFCIkpZr6jmn3Rj89NBcUjL6JLvx8s=";
   };
 
   propagatedBuildInputs = [
