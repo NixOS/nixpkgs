0843740e4e69d42b5ae731bb92fed096e66f9d27
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-08 00:05:52 +0100

    python3Packages.pyvesync: 2.0.4 -> 2.1.1

--- pkgs/development/python-modules/pyvesync/default.nix
+++ pkgs/development/python-modules/pyvesync/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "pyvesync";
-  version = "2.0.4";
+  version = "2.1.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-1Svz/9ZS5ynr88/We1fa+H1IGdC5ljUa4M5O8X+muX4=";
+    sha256 = "sha256-ulYOzCIpHkL/2nQExomoV4mLkU6e0ns4MHxEbjCPGrQ=";
   };
 
   propagatedBuildInputs = [
