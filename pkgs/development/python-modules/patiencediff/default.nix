05a08089944195f93c41cf4e3aa4d89356fe7b9c
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-08 13:55:04 +0000

    python310Packages.patiencediff: 0.2.9 -> 0.2.10

--- pkgs/development/python-modules/patiencediff/default.nix
+++ pkgs/development/python-modules/patiencediff/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "patiencediff";
-  version = "0.2.9";
+  version = "0.2.10";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "breezy-team";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-DvbOA/NXHTuE84zWicOUtAKgGHUmKrAWgeFW1+uA8JY=";
+    hash = "sha256-AlJ61Sp6HSy6nJ6trVF2OD9ziSIW241peRXcda3xWnQ=";
   };
 
   nativeBuildInputs = [
