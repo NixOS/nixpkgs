1001c044978dda87d10c59a72f8dd26d18413208
sternenseemann <sternenseemann@systemli.org>
2022-12-08 01:25:24 +0100

    haskellPackages.rate-limit: allow time 0.11

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -70,6 +70,10 @@ self: super: {
   # 2022-12-07: emailed maintainer about this
   jack = doJailbreak super.jack;
 
+  # Too stricut upper bound on time
+  # https://github.com/acw/rate-limit/issues/9
+  rate-limit = doJailbreak super.rate-limit;
+
   # This test keeps being aborted because it runs too quietly for too long
   Lazy-Pbkdf2 = if pkgs.stdenv.isi686 then dontCheck super.Lazy-Pbkdf2 else super.Lazy-Pbkdf2;
 
