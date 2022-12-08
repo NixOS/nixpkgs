3603f34efdd2aecdfefa2a4c95d9708e34da54dc
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-08 14:31:58 +0000

    python310Packages.phonopy: 2.16.3 -> 2.17.0

--- pkgs/development/python-modules/phonopy/default.nix
+++ pkgs/development/python-modules/phonopy/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "phonopy";
-  version = "2.16.3";
+  version = "2.17.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-xTbTVRVmJvJuXV/RWLA+EMShPEYdagKiawXjtjEbnXk=";
+    hash = "sha256-bBlNCfq+b73Z/yxIhmP9s2kgjlGz+EKTpI1CnpuqkuU=";
   };
 
   propagatedBuildInputs = [
