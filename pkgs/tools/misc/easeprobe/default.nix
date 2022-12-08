f7d7b00db455e1c6caa437e7ac14cd4fad307a35
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-08 07:29:04 +0000

    easeprobe: 1.9.0 -> 2.0.0

--- pkgs/tools/misc/easeprobe/default.nix
+++ pkgs/tools/misc/easeprobe/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "easeprobe";
-  version = "1.9.0";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "megaease";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-iw24TuK5nbHabzRdaJ8X/MzRPNP35M8RjhKnZZXjVfA=";
+    sha256 = "sha256-y9R2OgK+slQUvUMS3E6aX8WVCQ1fSMAruGKggxYRniA=";
   };
 
-  vendorSha256 = "sha256-bfqP57YS2KD6CuFytJyWYCFl0Cx/JJgc1CeW13yBUoo=";
+  vendorSha256 = "sha256-ZfqBSPnIm2GHPREowHmEEPnOovYjoarxrkPeYmZBkIc=";
 
   subPackages = [ "cmd/easeprobe" ];
 
