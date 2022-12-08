6ea6e3ca291627f0df808f397000422c964ca58c
ercao <vip@ercao.cn>
2022-12-08 10:56:59 +0800

    fcitx5-with-addons: make fcitx5-lua addon optional
    Signed-off-by: ercao <vip@ercao.cn>

--- pkgs/tools/inputmethods/fcitx5/with-addons.nix
+++ pkgs/tools/inputmethods/fcitx5/with-addons.nix
@@ -1,11 +1,11 @@
-{ lib, symlinkJoin, makeWrapper, fcitx5, fcitx5-lua, fcitx5-configtool, fcitx5-qt, fcitx5-gtk, addons ? [ ] }:
+{ lib, symlinkJoin, makeWrapper, fcitx5, fcitx5-configtool, fcitx5-qt, fcitx5-gtk, addons ? [ ] }:
 
 with lib;
 
 symlinkJoin {
   name = "fcitx5-with-addons-${fcitx5.version}";
 
-  paths = [ fcitx5 fcitx5-configtool fcitx5-qt fcitx5-lua fcitx5-gtk ] ++ addons;
+  paths = [ fcitx5 fcitx5-configtool fcitx5-qt fcitx5-gtk ] ++ addons;
 
   nativeBuildInputs = [ makeWrapper ];
 
@@ -14,7 +14,7 @@ symlinkJoin {
       --prefix FCITX_ADDON_DIRS : "$out/lib/fcitx5" \
       --suffix XDG_DATA_DIRS : "$out/share" \
       --suffix PATH : "$out/bin" \
-      --suffix LD_LIBRARY_PATH : ${makeLibraryPath fcitx5-lua.extraLdLibraries};
+      --suffix LD_LIBRARY_PATH : ${makeLibraryPath (flatten (map (x: x.extraLdLibraries or []) addons))}
 
     desktop=share/applications/org.fcitx.Fcitx5.desktop
     autostart=etc/xdg/autostart/org.fcitx.Fcitx5.desktop
