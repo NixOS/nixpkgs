8fd94ca63237cf3b140cce8fb4b84488a1e3d31f
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-09 09:47:22 +0100

    python310Packages.mkdocstrings-python: 0.8.0 -> 0.8.2
    Diff: https://github.com/mkdocstrings/python/compare/0.8.0...0.8.2
    
    Changelog: https://github.com/mkdocstrings/python/blob/0.8.2/CHANGELOG.md

--- pkgs/development/python-modules/mkdocstrings-python/default.nix
+++ pkgs/development/python-modules/mkdocstrings-python/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "mkdocstrings-python";
-  version = "0.8.0";
+  version = "0.8.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "mkdocstrings";
     repo = "python";
     rev = version;
-    hash = "sha256-KAVBK0ZR1R27cWH99DVOYNFWKa4ubBXzgM0hVpGRIpE=";
+    hash = "sha256-TwvXH/n2do4GnkxStW8fk9MRm59t/eP0sOjGnl3fYkw=";
   };
 
   nativeBuildInputs = [
