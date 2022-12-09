64421f1a3b0d3922be4c29fd8df84f580b809721
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-09 09:06:55 +0100

    python3Packages.dependency-injector: add changelog to meta
    - disable on unsupported Python releases

--- pkgs/development/python-modules/dependency-injector/default.nix
+++ pkgs/development/python-modules/dependency-injector/default.nix
@@ -12,17 +12,21 @@
 , pytestCheckHook
 , pyyaml
 , six
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "dependency-injector";
   version = "4.40.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "ets-labs";
     repo = "python-dependency-injector";
     rev = version;
-    sha256 = "sha256-lcgPFdAgLmv7ILL2VVfqtGSw96aUfPv9oiOhksRtF3k=";
+    hash = "sha256-lcgPFdAgLmv7ILL2VVfqtGSw96aUfPv9oiOhksRtF3k=";
   };
 
   propagatedBuildInputs = [
@@ -42,11 +46,14 @@ buildPythonPackage rec {
     pyyaml
   ];
 
-  pythonImportsCheck = [ "dependency_injector" ];
+  pythonImportsCheck = [
+    "dependency_injector"
+  ];
 
   meta = with lib; {
     description = "Dependency injection microframework for Python";
     homepage = "https://github.com/ets-labs/python-dependency-injector";
+    changelog = "https://github.com/ets-labs/python-dependency-injector/blob/${version}/docs/main/changelog.rst";
     license = licenses.bsd3;
     maintainers = with maintainers; [ gerschtli ];
   };
