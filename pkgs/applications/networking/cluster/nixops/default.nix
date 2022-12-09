e03ac0b5f198110365f40c3f118d963982cd5eea
Robert Schütz <nix@dotlambda.de>
2022-12-09 10:04:57 -0800

    nixopsUnstable: mark certifi insecure
    (cherry picked from commit 218122fa5c158ca244e842184dc894c6aba198ee)

--- pkgs/applications/networking/cluster/nixops/default.nix
+++ pkgs/applications/networking/cluster/nixops/default.nix
@@ -58,6 +58,14 @@ let
           };
         })
 
+        (self: super: {
+          certifi = super.certifi.overridePythonAttrs (old: {
+            meta = old.meta // {
+              knownVulnerabilities = [ "CVE-2022-23491" ];
+            };
+          });
+        })
+
       ];
     }
   ).python;
