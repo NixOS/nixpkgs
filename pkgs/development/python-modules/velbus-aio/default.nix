55c4146f390bc4b7d0000791cfb495e4033d5ce6
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-10 23:21:35 +0100

    python310Packages.velbus-aio: add changelog to meta

--- pkgs/development/python-modules/velbus-aio/default.nix
+++ pkgs/development/python-modules/velbus-aio/default.nix
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "Cereal2nd";
     repo = pname;
     rev = version;
-    sha256 = "sha256-6Sg2t8UmIyVCfjTW3GDldOk09gVg2ngV9sXhyQI4Kz4=";
+    hash = "sha256-6Sg2t8UmIyVCfjTW3GDldOk09gVg2ngV9sXhyQI4Kz4=";
     fetchSubmodules = true;
   };
 
@@ -40,6 +40,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library to support the Velbus home automation system";
     homepage = "https://github.com/Cereal2nd/velbus-aio";
+    changelog = "https://github.com/Cereal2nd/velbus-aio/releases/tag/${version}";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ fab ];
   };
