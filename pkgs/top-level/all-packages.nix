2cfb9ab1268806616dbf07ae2fc8d72e66837237
Robert James Hernandez <rob@sarcasticadmin.com>
2022-12-10 21:38:56 +0000

    ax25-tools: init at 0.0.10-rc5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27739,6 +27739,8 @@ with pkgs;
 
   avocode = callPackage ../applications/graphics/avocode {};
 
+  ax25-tools = callPackage ../applications/radio/ax25-tools {};
+
   azpainter = callPackage ../applications/graphics/azpainter { };
 
   bambootracker = libsForQt5.callPackage ../applications/audio/bambootracker { };
