7674933d95d04b08c26db7eae180f0f1d9bf9259
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-10 07:05:15 +0000

    pyradio: 0.8.9.31 -> 0.8.9.32

--- pkgs/applications/audio/pyradio/default.nix
+++ pkgs/applications/audio/pyradio/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "pyradio";
-  version = "0.8.9.31";
+  version = "0.8.9.32";
 
   src = fetchFromGitHub {
     owner = "coderholic";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-9Fc42f0plduihXDDLXWBdt62maxDJ0cwumIvbiMcrGc=";
+    sha256 = "sha256-1lczH8xTZV1XVVByLAGXJbjXd20JUJALIJJ3s9+orxM=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
