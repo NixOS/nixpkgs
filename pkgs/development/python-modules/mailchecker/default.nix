14f2ef9fb9f9a42c41a5fd77b2c71550b3d64b25
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-10 16:58:33 +0100

    python310Packages.mailchecker: add changelog to meta

--- pkgs/development/python-modules/mailchecker/default.nix
+++ pkgs/development/python-modules/mailchecker/default.nix
@@ -26,6 +26,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Module for temporary (disposable/throwaway) email detection";
     homepage = "https://github.com/FGRibreau/mailchecker";
+    changelog = "https://github.com/FGRibreau/mailchecker/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
