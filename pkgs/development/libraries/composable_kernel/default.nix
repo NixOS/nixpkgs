90e3db28758406b25ea941bc971a3e1ae5a294ab
Madoura <madouura@gmail.com>
2022-12-10 19:53:48 +0000

    composable_kernel: unstable-2022-11-19 → unstable-2022-12-08

--- pkgs/development/libraries/composable_kernel/default.nix
+++ pkgs/development/libraries/composable_kernel/default.nix
@@ -14,7 +14,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "composable_kernel";
-  version = "unstable-2022-11-19";
+  version = "unstable-2022-12-08";
 
   outputs = [
     "out"
@@ -29,8 +29,8 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "ROCmSoftwarePlatform";
     repo = "composable_kernel";
-    rev = "43a889b72e3faabf04c16ff410d387ce28486c3e";
-    hash = "sha256-DDRrWKec/RcOhW3CrN0gl9NZsp0Bjnja7HAiTcEh7qg=";
+    rev = "d58b7f5155b44c8b608f3edc6a6eab314493ec1a";
+    hash = "sha256-4nzyaWhPnY/0TygcoJAqVzdgfXOkf+o/BE2V9N+Bm7Q=";
   };
 
   nativeBuildInputs = [
