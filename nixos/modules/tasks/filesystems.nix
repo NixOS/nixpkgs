61d888f5ad41e355ffb8a48d03c0be7ba3c17c7a
Lucas Savva <lucas@m1cr0man.com>
2022-12-10 00:43:26 +0000

    nixos/systemd-nspawn: Skip more filesystems for containers
    Within systemd nspawn, even less default filesystems are
    required than what is currently assumed in filesystem.nix.
    This PR wraps more mounts in conditional toggles to respect this.

--- nixos/modules/tasks/filesystems.nix
+++ nixos/modules/tasks/filesystems.nix
@@ -555,6 +555,17 @@ in
 
     # Sync mount options with systemd's src/core/mount-setup.c: mount_table.
     boot.specialFileSystems = {
+      # To hold secrets that shouldn't be written to disk
+      "/run/keys" = {
+        fsType = "ramfs";
+        options = [
+          "nosuid"
+          "nodev"
+          "mode=750"
+        ];
+      };
+    }
+    // optionalAttrs (!config.boot.isContainer) {
       "/proc" = {
         fsType = "proc";
         options = [
@@ -602,18 +613,6 @@ in
           "gid=${toString config.ids.gids.tty}"
         ];
       };
-
-      # To hold secrets that shouldn't be written to disk
-      "/run/keys" = {
-        fsType = "ramfs";
-        options = [
-          "nosuid"
-          "nodev"
-          "mode=750"
-        ];
-      };
-    }
-    // optionalAttrs (!config.boot.isContainer) {
       # systemd-nspawn populates /sys by itself, and remounting it causes all
       # kinds of weird issues (most noticeably, waiting for host disk device
       # nodes).
