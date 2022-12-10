8002aaef02e8009b683f4e760a23460fddfea539
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-10 10:53:56 +0000

    jftui: 0.6.0 -> 0.6.1

--- pkgs/applications/video/jftui/default.nix
+++ pkgs/applications/video/jftui/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "jftui";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "Aanok";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-UwR4IboLSjD/XXvSw1AhJubSxettvL/URhZ/Je6TWPQ=";
+    sha256 = "sha256-TkSnNZBnmDSMsxTFQx1OcFDLQBUqxIrFXQeeslXKqA0=";
   };
 
   nativeBuildInputs = [
