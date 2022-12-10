4400bb23f367eda7bd5e8ff5be18ea05553e3325
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-10 22:29:06 +0100

    python310Packages.hap-python: 4.5.0 -> 4.6.0
    Diff: https://github.com/ikalchev/HAP-python/compare/refs/tags/v4.5.0...v4.6.0
    
    Changelog: https://github.com/ikalchev/HAP-python/blob/v4.6.0/CHANGELOG.md

--- pkgs/development/python-modules/hap-python/default.nix
+++ pkgs/development/python-modules/hap-python/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "hap-python";
-  version = "4.5.0";
+  version = "4.6.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "ikalchev";
     repo = "HAP-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-/XJvCL9hMIrOUwGPcrvSrJ6SZ/E6BQy+isFFlAniIM4=";
+    hash = "sha256-fAJB1gk8zTS/mW5KzWr3z26qctZc/EQlk//WM1Xwpl0=";
   };
 
   propagatedBuildInputs = [
