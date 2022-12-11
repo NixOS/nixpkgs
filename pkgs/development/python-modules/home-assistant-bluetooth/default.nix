3379e44a0be93db64da811e88debd7c0261881ed
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-11 09:45:20 +0100

    python310Packages.home-assistant-bluetooth: 1.8.1 -> 1.9.0
    Diff: https://github.com/home-assistant-libs/home-assistant-bluetooth/compare/refs/tags/v1.8.1...v1.9.0
    
    Changelog: https://github.com/home-assistant-libs/home-assistant-bluetooth/blob/v1.9.0/CHANGELOG.md

--- pkgs/development/python-modules/home-assistant-bluetooth/default.nix
+++ pkgs/development/python-modules/home-assistant-bluetooth/default.nix
@@ -11,15 +11,16 @@
 
 buildPythonPackage rec {
   pname = "home-assistant-bluetooth";
-  version = "1.8.1";
+  version = "1.9.0";
   format = "pyproject";
+
   disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "home-assistant-libs";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-oOrsZmAXLYsJ19DrQ70O0RpeSz8Jn8oD41Tsc5DVUng=";
+    hash = "sha256-/ne2pnSmeHuwlGCrMC0x/yNDCvB/P4SqmK/wwcmHlJQ=";
   };
 
   postPatch = ''
@@ -47,7 +48,7 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Basic bluetooth models used by Home Assistant";
-    changelog = "https://github.com/home-assistant-libs/home-assistant-bluetooth/blob/main/CHANGELOG.md";
+    changelog = "https://github.com/home-assistant-libs/home-assistant-bluetooth/blob/v${version}/CHANGELOG.md";
     homepage = "https://github.com/home-assistant-libs/home-assistant-bluetooth";
     license = licenses.asl20;
     maintainers = teams.home-assistant.members;
