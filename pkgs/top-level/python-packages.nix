a3e65db44aef97a691014b085721624a67c69a00
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-11 11:01:30 +0100

    python310Packages.dnfile: init at 0.12.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2678,6 +2678,8 @@ self: super: with self; {
 
   dnachisel = callPackage ../development/python-modules/dnachisel { };
 
+  dnfile = callPackage ../development/python-modules/dnfile { };
+
   dnslib = callPackage ../development/python-modules/dnslib { };
 
   dnspython = callPackage ../development/python-modules/dnspython { };
