7706127402d57850ca99b8b70c00404724379c0d
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-11 10:00:37 +0100

    python310Packages.pyeconet: 0.1.16 -> 0.1.17
    Diff: https://github.com/w1ll1am23/pyeconet/compare/refs/tags/v0.1.16...0.1.17
    
    Changelog: https://github.com/w1ll1am23/pyeconet/releases/tag/v0.1.17

--- pkgs/development/python-modules/pyeconet/default.nix
+++ pkgs/development/python-modules/pyeconet/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "pyeconet";
-  version = "0.1.16";
+  version = "0.1.17";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "w1ll1am23";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-nwbihYxDs7v3g0fy9W3qOC7Sa/Y+BF+qNgqnNF7puHI=";
+    hash = "sha256-ntxITedoJOt5d7V9TSFQHg0oqBEw8jNGeDLM00RRWHI=";
   };
 
   nativeBuildInputs = [
