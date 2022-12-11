b920b9da2826124d69773942746d01110a82e6d8
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-11 11:19:45 +0100

    python310Packages.python-decouple: init at 3.6
    (cherry picked from commit 8fe67e0c20e6428ca6e834b302068979adb4271a)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8997,6 +8997,8 @@ self: super: with self; {
 
   python-dbusmock = callPackage ../development/python-modules/python-dbusmock { };
 
+  python-decouple = callPackage ../development/python-modules/python-decouple { };
+
   pythondialog = callPackage ../development/python-modules/pythondialog { };
 
   python-didl-lite = callPackage ../development/python-modules/python-didl-lite { };
