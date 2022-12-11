aebf6f67c269dc383a4ceececf7e36458c348b53
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-11 10:04:23 +0100

    python310Packages.pycares: add changelog to meta

--- pkgs/development/python-modules/pycares/default.nix
+++ pkgs/development/python-modules/pycares/default.nix
@@ -4,15 +4,19 @@
 , cffi
 , fetchPypi
 , idna
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pycares";
   version = "4.2.2";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-4fV6gAQ3AIBpS9b7lpof/JFxpZxoJNVPeRwbLk0pg4U=";
+    hash = "sha256-4fV6gAQ3AIBpS9b7lpof/JFxpZxoJNVPeRwbLk0pg4U=";
   };
 
   buildInputs = [
@@ -31,11 +35,14 @@ buildPythonPackage rec {
   # Requires network access
   doCheck = false;
 
-  pythonImportsCheck = [ "pycares" ];
+  pythonImportsCheck = [
+    "pycares"
+  ];
 
   meta = with lib; {
     description = "Python interface for c-ares";
     homepage = "https://github.com/saghul/pycares";
+    changelog = "https://github.com/saghul/pycares/releases/tag/pycares-${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
