c1fa81b7e70f24c34acfbf0d2554b4d2fad8b652
Pavol Rusnak <pavol@rusnak.io>
2022-12-13 00:30:27 +0100

    secp256k1: unstable-2022-02-06 -> 0.2.0

--- pkgs/tools/security/secp256k1/default.nix
+++ pkgs/tools/security/secp256k1/default.nix
@@ -4,16 +4,15 @@
 , autoreconfHook
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "secp256k1";
-
-  version = "unstable-2022-02-06";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "bitcoin-core";
     repo = "secp256k1";
-    rev = "5dcc6f8dbdb1850570919fc9942d22f728dbc0af";
-    sha256 = "x9qG2S6tBSRseWaFIN9N2fRpY1vkv8idT3d3rfJnmaU=";
+    rev = "v${version}";
+    sha256 = "sha256-wYJIMCoo6ryeQN4ZnvEkJ5/332+AkaOwgplDuQQC5MU=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
@@ -30,7 +29,13 @@ stdenv.mkDerivation {
 
   doCheck = true;
 
-  checkPhase = "./tests";
+  checkPhase = ''
+    runHook preCheck
+
+    ./tests
+
+    runHook postCheck
+  '';
 
   meta = with lib; {
     description = "Optimized C library for EC operations on curve secp256k1";
