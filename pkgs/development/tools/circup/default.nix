c3db1f27518009a750ff1abbdee8f4065ffc5cf7
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-12 12:44:38 +0000

    circup: 1.1.3 -> 1.1.4

--- pkgs/development/tools/circup/default.nix
+++ pkgs/development/tools/circup/default.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "circup";
-  version = "1.1.3";
+  version = "1.1.4";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "adafruit";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-BCAsCwQCKMtmjISMVKDblRdev87K4EfX5D2Ot0L5PoQ=";
+    hash = "sha256-nXDje+MJR6olG3G7RO3esf6UAKynMvCP8YetIhnqoeE=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
