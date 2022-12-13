7b50756d8297f16b4aa27e518f578cde9e718c0d
figsoda <figsoda@pm.me>
2022-12-12 22:17:31 -0500

    ruff: 0.0.177 -> 0.0.179
    Diff: https://github.com/charliermarsh/ruff/compare/v0.0.177...v0.0.179
    
    Changelog: https://github.com/charliermarsh/ruff/releases/tag/v0.0.179

--- pkgs/development/tools/ruff/default.nix
+++ pkgs/development/tools/ruff/default.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ruff";
-  version = "0.0.177";
+  version = "0.0.179";
 
   src = fetchFromGitHub {
     owner = "charliermarsh";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-4X4dEVcKjcgww0JnqXkuSbK+VEF4C4/0QLfFrfAoRJ8=";
+    sha256 = "sha256-vc4gYTsA4bbrbn1NMf4ixIrDRS1Abak0FyXkGKUX8Eo=";
   };
 
-  cargoSha256 = "sha256-JnIvSIiJC3eR7vDrkIfElxM79dZAHrdcvtNw26zKoqw=";
+  cargoSha256 = "sha256-etuiSRAXoWPZwsZxFVTE0PG+TK6HlRTQG4WkLWdCg0M=";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     CoreServices
