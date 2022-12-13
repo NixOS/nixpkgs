423b8fc89337df4fa4b6af928330c17ef51930e0
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-13 16:01:26 +0100

    python310Packages.zadnegoale: add changelog to meta

--- pkgs/development/python-modules/zadnegoale/default.nix
+++ pkgs/development/python-modules/zadnegoale/default.nix
@@ -45,6 +45,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python wrapper for getting allergen concentration data from Żadnego Ale servers";
     homepage = "https://github.com/bieniu/zadnegoale";
+    changelog = "https://github.com/bieniu/zadnegoale/releases/tag/${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
