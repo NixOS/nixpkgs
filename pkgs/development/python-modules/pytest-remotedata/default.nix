fbcb841415d8004de3ce6ee07b5df4371172bd4b
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-14 07:38:34 +0100

    python310Packages.pytest-remotedata: add changelog to meta
    - update disabled

--- pkgs/development/python-modules/pytest-remotedata/default.nix
+++ pkgs/development/python-modules/pytest-remotedata/default.nix
@@ -13,11 +13,11 @@ buildPythonPackage rec {
   version = "0.4.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-viHFWONNfBGw9q61CVbAlSC//NArf86cb46FMaQBocg=";
+    hash = "sha256-viHFWONNfBGw9q61CVbAlSC//NArf86cb46FMaQBocg=";
   };
 
   nativeBuildInputs = [
@@ -48,6 +48,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Pytest plugin for controlling remote data access";
     homepage = "https://github.com/astropy/pytest-remotedata";
+    changelog = "https://github.com/astropy/pytest-remotedata/blob/v${version}/CHANGES.rst";
     license = licenses.bsd3;
     maintainers = with maintainers; [ costrouc ];
   };
