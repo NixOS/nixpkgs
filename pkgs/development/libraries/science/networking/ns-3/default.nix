1ff084d6e5bf3e8d41f9a6808f0432b538ecafaa
Rémy Grünblatt <remy@grunblatt.org>
2022-12-14 15:11:20 +0100

    ns-3: 3.35 -> 3.37

--- pkgs/development/libraries/science/networking/ns-3/default.nix
+++ pkgs/development/libraries/science/networking/ns-3/default.nix
@@ -1,104 +1,108 @@
 { stdenv
 , fetchFromGitLab
-, python
-, wafHook
+, python3
+, cmake
 
-# for binding generation
+, sqlite
+, cmake-format
+, harfbuzz
+, gtk3
+, glib
+, libxml2
+, sphinx
+, cppyy ? null
+, gsl ? null
+
+  # for binding generation
 , castxml ? null
 
-# can take a long time, generates > 30000 images/graphs
+  # can take a long time, generates > 30000 images/graphs
 , enableDoxygen ? false
 
-# e.g. "optimized" or "debug". If not set, use default one
+  # e.g. "optimized" or "debug". If not set, use default one
 , build_profile ? null
 
-# --enable-examples
-, withExamples ? false
+  # --enable-examples
+, withExamples ? true
 
-# very long
-, withManual ? false, doxygen ? null, graphviz ? null, imagemagick ? null
-# for manual, tetex is used to get the eps2pdf binary
-# texlive to get latexmk. building manual still fails though
-, dia, tetex ? null, ghostscript ? null, texlive ? null
+  # very long
+, withManual ? true
+, doxygen ? null
+, graphviz ? null
+, imagemagick ? null
+  # for manual, tetex is used to get the eps2pdf binary
+  # texlive to get latexmk. building manual still fails though
+, dia ? null
+, tetex ? null
+, ghostscript ? null
+, texlive ? null
 
-# generates python bindings
-, pythonSupport ? false, ncurses ? null
+  # generates python bindings
+, pythonSupport ? false
+, ncurses ? null
 
-# All modules can be enabled by choosing 'all_modules'.
-# we include here the DCE mandatory ones
+  # All modules can be enabled by choosing 'all_modules'.
+  # we include here the DCE mandatory ones
 , modules ? [ "core" "network" "internet" "point-to-point" "point-to-point-layout" "fd-net-device" "netanim" ]
 , lib
 }:
 
 let
-  pythonEnv = python.withPackages(ps:
+  pythonEnv = python3.withPackages (ps:
     lib.optional withManual ps.sphinx
-    ++ lib.optionals pythonSupport (with ps;[ pybindgen pygccxml ])
+    ++ lib.optionals pythonSupport (with ps;[ pybindgen pygccxml cppyy ])
   );
 in
 stdenv.mkDerivation rec {
   pname = "ns-3";
-  version = "35";
+  version = "37";
 
   src = fetchFromGitLab {
     owner = "nsnam";
-    repo   = "ns-3-dev";
-    rev    = "ns-3.${version}";
-    sha256 = "sha256-3w+lCWWra9sndL8+vkGfH5plrDYYCMFi1PzwIVRku6I=";
+    repo = "ns-3-dev";
+    rev = "ns-3.${version}";
+    sha256 = "sha256-Oj3u2P98dl2Lvyg51F/6XBdLr+8PcmT00TCVgGb30Xc=";
   };
 
-  nativeBuildInputs = [ wafHook python ];
+  nativeBuildInputs = [ python3 cmake ];
 
-  outputs = [ "out" ] ++ lib.optional pythonSupport "py";
-
-  # ncurses is a hidden dependency of waf when checking python
-  buildInputs = lib.optionals pythonSupport [ castxml ncurses ]
+  buildInputs = [ gtk3 sqlite cmake-format harfbuzz libxml2 sphinx gsl ]
+    ++ lib.optionals pythonSupport [ castxml ncurses ]
     ++ lib.optionals enableDoxygen [ doxygen graphviz imagemagick ]
     ++ lib.optionals withManual [ dia tetex ghostscript texlive.combined.scheme-medium ];
 
-  propagatedBuildInputs = [ pythonEnv ];
+  outputs = [ "out" ] ++ lib.optional pythonSupport "py";
 
-  postPatch = ''
-    patchShebangs doc/ns3_html_theme/get_version.sh
+  cmakeFlags = [
+    "-DGTK3_GLIBCONFIG_INCLUDE_DIR=${glib.out}/lib/glib-2.0/include"
+  ];
+
+  configurePhase = ''
+    ${pythonEnv.interpreter} ns3 configure \
+    ${lib.optionalString withExamples " --enable-examples "} \
+    --out=$out \
+    --enable-modules=${lib.concatStringsSep "," modules} \
+    ${lib.optionalString (!isNull build_profile) " --build-profile=${build_profile} "} \
+    ${lib.optionalString pythonSupport " --enable-python-bindings "} \
+    --enable-tests \
+    --enable-gtk -- -DGTK3_GLIBCONFIG_INCLUDE_DIR=${glib.out}/lib/glib-2.0/include
   '';
 
-  wafConfigureFlags = with lib; [
-      "--enable-modules=${concatStringsSep "," modules}"
-      "--with-python=${pythonEnv.interpreter}"
-  ]
-  ++ optional (build_profile != null) "--build-profile=${build_profile}"
-  ++ optional withExamples " --enable-examples "
-  ++ optional doCheck " --enable-tests "
-  ;
-
-  doCheck = true;
-
-  buildTargets = "build"
-    + lib.optionalString enableDoxygen " doxygen"
-    + lib.optionalString withManual "sphinx";
-
-  # to prevent fatal error: 'backward_warning.h' file not found
-  CXXFLAGS = "-D_GLIBCXX_PERMIT_BACKWARD_HASH";
-
-  postBuild = with lib; let flags = concatStringsSep ";" (
-      optional enableDoxygen "./waf doxygen"
-      ++ optional withManual "./waf sphinx"
-    );
-    in "${flags}"
-  ;
-
-  postInstall = ''
-    moveToOutput "${pythonEnv.libPrefix}" "$py"
+  buildPhase = ''
+    ${pythonEnv.interpreter} ns3 build
+    ${lib.optionalString enableDoxygen "${pythonEnv.interpreter} ns3 docs doxygen"}
   '';
 
   # we need to specify the proper interpreter else ns3 can check against a
   # different version
-  checkPhase =  ''
-    ${pythonEnv.interpreter} ./test.py --nowaf
+  checkPhase = ''
+    ${pythonEnv.interpreter} ./test.py
   '';
 
+  # TODO : find out why the fixupPhase fails
+
   # strictoverflow prevents clang from discovering pyembed when bindings
-  hardeningDisable = [ "fortify" "strictoverflow"];
+  hardeningDisable = [ "fortify" "strictoverflow" ];
 
   meta = with lib; {
     broken = (stdenv.isLinux && stdenv.isAarch64);
