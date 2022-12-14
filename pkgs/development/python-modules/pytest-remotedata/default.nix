07def67e0ad13a969937b88d4d0ebf5409d6422d
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-14 05:05:19 +0000

    python310Packages.pytest-remotedata: 0.3.3 -> 0.4.0

--- pkgs/development/python-modules/pytest-remotedata/default.nix
+++ pkgs/development/python-modules/pytest-remotedata/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "pytest-remotedata";
-  version = "0.3.3";
+  version = "0.4.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "66920bf1c62928b079d0e611379111a0d49f10a9509ced54c8269514ccce6ee3";
+    sha256 = "sha256-viHFWONNfBGw9q61CVbAlSC//NArf86cb46FMaQBocg=";
   };
 
   nativeBuildInputs = [
