8ec62e6387ee868ec851cc5877c58f485f93f4ac
Robert Schütz <nix@dotlambda.de>
2022-12-14 12:58:19 -0800

    python27: mark insecure

--- pkgs/development/interpreters/python/cpython/2.7/default.nix
+++ pkgs/development/interpreters/python/cpython/2.7/default.nix
@@ -356,5 +356,8 @@ in with passthru; stdenv.mkDerivation ({
       # Higher priority than Python 3.x so that `/bin/python` points to `/bin/python2`
       # in case both 2 and 3 are installed.
       priority = -100;
+      knownVulnerabilities = [
+        "CVE-2022-45061"
+      ];
     };
   } // crossCompileEnv)
