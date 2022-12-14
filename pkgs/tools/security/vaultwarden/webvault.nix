f5b7e66e96bb56fa75711cdf497d437f78fc954d
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-14 17:26:52 +0100

    vaultwarden.webvault: 2022.6.2 -> 2022.10.0
    Bring the webvault in sync with the backend release.

--- pkgs/tools/security/vaultwarden/webvault.nix
+++ pkgs/tools/security/vaultwarden/webvault.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "vaultwarden-webvault";
-  version = "2022.6.2";
+  version = "2022.10.0";
 
   src = fetchurl {
     url = "https://github.com/dani-garcia/bw_web_builds/releases/download/v${version}/bw_web_v${version}.tar.gz";
-    sha256 = "sha256-IG/eCBTUa7eKeaelqxCWO+rrXJUuBanhsYwklftxdOE=";
+    hash = "sha256-Sf1YnOikjZmloTQvdrFH/UAevQqKQEkNNrCRUhvNZfA=";
   };
 
   buildCommand = ''
