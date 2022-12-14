4bdf1d6982a54c6eb93197841c6fd6c5243b37ef
Colin Arnott <colin@urandom.co.uk>
2022-12-14 10:51:14 +0000

    imaginary: init at 1.2.4
    Fixes #204482

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23928,6 +23928,8 @@ with pkgs;
 
   inspircdMinimal = inspircd.override { extraModules = []; };
 
+  imaginary = callPackage ../servers/imaginary {};
+
   imgproxy = callPackage ../servers/imgproxy { };
 
   irccat = callPackage ../servers/irc/irccat { };
