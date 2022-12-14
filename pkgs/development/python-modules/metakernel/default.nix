1a1a65f88224d79960a02bce5dd32ace97922918
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-14 08:42:28 +0100

    python310Packages.metakernel: add changelog to meta
    - adjust inputs
    - add pythonImportsCheck

--- pkgs/development/python-modules/metakernel/default.nix
+++ pkgs/development/python-modules/metakernel/default.nix
@@ -3,6 +3,9 @@
 , fetchPypi
 , hatchling
 , ipykernel
+, jedi
+, pexpect
+, jupyter_core
 }:
 
 buildPythonPackage rec {
@@ -10,23 +13,35 @@ buildPythonPackage rec {
   version = "0.29.4";
   format = "pyproject";
 
+  disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-kxrF/Msxjht7zGs0aEcL/Sf0qwcLiSoDPDUlE7Lrcmg=";
+    hash = "sha256-kxrF/Msxjht7zGs0aEcL/Sf0qwcLiSoDPDUlE7Lrcmg=";
   };
 
   nativeBuildInputs = [
     hatchling
   ];
 
-  propagatedBuildInputs = [ ipykernel ];
+  propagatedBuildInputs = [
+    ipykernel
+    jedi
+    jupyter_core
+    pexpect
+  ];
 
   # Tests hang, so disable
   doCheck = false;
 
+  pythonImportsCheck = [
+    "metakernel"
+  ];
+
   meta = with lib; {
     description = "Jupyter/IPython Kernel Tools";
     homepage = "https://github.com/Calysto/metakernel";
+    changelog = "https://github.com/Calysto/metakernel/blob/v${version}/CHANGELOG.md";
     license = licenses.bsd3;
     maintainers = with maintainers; [ thomasjm ];
   };
