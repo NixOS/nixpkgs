a2f85e0fa8d22a8ab42173344ce8a96b028e018a
Artturin <Artturin@artturin.com>
2022-12-15 05:52:41 +0200

    androidenv: use callPackage instead of import & fix infinite recursion
    infinite recursion was due to autoPatchelfHook being in buildInputs of
    platform-tools, i will add a lint for it in nix-community/nixpkgs-lint.
    
    ```
    $ nix build ".#pkgsCross.aarch64-android-prebuilt.hello" --show-trace 2>&1 | rg 'while evaluating the attr.+deriv'
        … while evaluating the attribute 'stdenv' of the derivation 'zlib-aarch64-unknown-linux-android-1.2.13'
        … while evaluating the attribute 'CPPFLAGS' of the derivation 'python3-aarch64-unknown-linux-android-3.10.8'
        … while evaluating the attribute 'setuptools' of the derivation 'python-catch-conflicts-hook'
        … while evaluating the attribute 'nativeBuildInputs' of the derivation 'python3.10-pyelftools-0.28'
        … while evaluating the attribute 'passAsFile' of the derivation 'python3-3.10.8-env'
        … while evaluating the attribute 'pythonInterpreter' of the derivation 'auto-patchelf-hook'
        … while evaluating the attribute 'buildInputs' of the derivation 'platform-tools-33.0.2'
        … while evaluating the attribute 'installPhase' of the derivation 'ndk-24.0.8215888'
        … while evaluating the attribute 'installPhase' of the derivation 'aarch64-unknown-linux-android-ndk-toolchain-24.0.8215888'
        … while evaluating the attribute 'bintools_bin' of the derivation 'aarch64-unknown-linux-android-ndk-toolchain-wrapper-24.0.8215888'
        … while evaluating the attribute 'bintools' of the derivation 'aarch64-unknown-linux-android-ndk-toolchain-wrapper-24.0.8215888'
        … while evaluating the attribute 'defaultNativeBuildInputs' of the derivation 'stdenv-linux'
        … while evaluating the attribute 'stdenv' of the derivation 'hello-aarch64-unknown-linux-android-2.12.1'
    ```
    
    stdenv -> stdenv.cc -> bintools -> android-ndk-toolchain -> ndk -> platform-tools -> auto-patchelf-hook -> python3 -> zlib -> stdenv -> stdenv.cc -> ...
    
    autoPatchelfHook was in buildInputs of platform-tools so we needed the host tools to build
    it but platform-tools was a required tool

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3359,9 +3359,7 @@ with pkgs;
 
   anbox = callPackage ../os-specific/linux/anbox { };
 
-  androidenv = callPackage ../development/mobile/androidenv {
-    pkgs_i686 = pkgsi686Linux;
-  };
+  androidenv = callPackage ../development/mobile/androidenv { };
 
   androidndkPkgs = androidndkPkgs_21;
   androidndkPkgs_21 = (callPackage ../development/androidndk-pkgs {})."21";
