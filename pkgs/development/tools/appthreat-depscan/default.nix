bfed02034d2b17c75f31e01d77f7c0bc3ad2d45e
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-15 11:01:18 +0000

    appthreat-depscan: 3.3.0 -> 3.4.0

--- pkgs/development/tools/appthreat-depscan/default.nix
+++ pkgs/development/tools/appthreat-depscan/default.nix
@@ -5,13 +5,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "appthreat-depscan";
-  version = "3.3.0";
+  version = "3.4.0";
 
   src = fetchFromGitHub {
     owner = "AppThreat";
     repo = "dep-scan";
     rev = "refs/tags/v${version}";
-    hash = "sha256-PHyg52I8I9TeSoWKLx2aqMF7Csym4Hnq83fO3hcVEOc=";
+    hash = "sha256-Gp0JXjtU8TZl/k6HHWvMvdggIgyn4zqLyyBqiPtlkA8=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
