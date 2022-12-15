69b683ad17c8f0860a67790b9accf30af4951de7
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-15 16:02:54 +0000

    cglm: 0.8.5 -> 0.8.8

--- pkgs/development/libraries/cglm/default.nix
+++ pkgs/development/libraries/cglm/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cglm";
-  version = "0.8.5";
+  version = "0.8.8";
 
   src = fetchFromGitHub {
     owner = "recp";
     repo = "cglm";
     rev = "v${version}";
-    sha256 = "sha256-PJHDZXc0DD/d+K/4uouv5F8gAf1sE5e3jLkGILPMpnI=";
+    sha256 = "sha256-BzZb8NDgf1NnkZaaxs+0YlVuYod/uiWJxA3geaYN7e0=";
   };
 
   nativeBuildInputs = [ cmake ];
