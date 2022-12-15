afeeaabd5238321598ffa16c409c3a0e7edf36d5
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-15 00:01:12 +0000

    python310Packages.pymyq: 3.1.5 -> 3.1.6

--- pkgs/development/python-modules/pymyq/default.nix
+++ pkgs/development/python-modules/pymyq/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pymyq";
-  version = "3.1.5";
+  version = "3.1.6";
   disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "arraylabs";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-/2eWB4rtHPptfc8Tm0CGk0UB+Hq1EmNhWmdrpPiUJcw=";
+    sha256 = "sha256-zhGCoZ7mkHlfDjEbQihtM23u+N6nfYsQhKmrloevzp8=";
   };
 
   propagatedBuildInputs = [
