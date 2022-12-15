897891c73dbfbf4b32fde94f37b64d6bbb85753c
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-15 13:39:51 +0000

    python310Packages.r2pipe: 1.7.3 -> 1.7.4

--- pkgs/development/python-modules/r2pipe/default.nix
+++ pkgs/development/python-modules/r2pipe/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "r2pipe";
-  version = "1.7.3";
+  version = "1.7.4";
 
   postPatch = let
     r2lib = "${lib.getOutput "lib" radare2}/lib";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-zhKV0+467xFpzmSDswIWPEGpks0X/F+ecBWPWpvakik=";
+    sha256 = "sha256-bmr9/iqlp5GghY6DOpFhBH3k69ErqR3DHx7iAu3m6f0=";
   };
 
   # Tiny sanity check to make sure r2pipe finds radare2 (since r2pipe doesn't
