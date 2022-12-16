fdbbaf7ac306abd068efd678c17a4474d4f66382
David Morgan <djm_uk@protonmail.com>
2022-12-16 16:38:25 +0000

    fzf-git-sh: init at unstable-2022-09-30

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13540,6 +13540,8 @@ with pkgs;
 
   fishPlugins = recurseIntoAttrs (callPackage ../shells/fish/plugins { });
 
+  fzf-git-sh = callPackage ../shells/fzf-git-sh {};
+
   ion = callPackage ../shells/ion {
     inherit (darwin) Security;
   };
