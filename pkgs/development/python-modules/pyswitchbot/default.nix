3155c2fb0e126896eedc40c33d892cf9d268cba6
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-17 10:49:15 +0100

    python310Packages.pyswitchbot: 0.27.0 -> 0.27.1
    Changelog: https://github.com/Danielhiversen/pySwitchbot/releases/tag/0.27.1

--- pkgs/development/python-modules/pyswitchbot/default.nix
+++ pkgs/development/python-modules/pyswitchbot/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "pyswitchbot";
-  version = "0.27.0";
+  version = "0.27.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "Danielhiversen";
     repo = "pySwitchbot";
     rev = "refs/tags/${version}";
-    hash = "sha256-wvwBhTEgIBc/un5vV2LxKRjamotLjs6P+ITp1aNviXg=";
+    hash = "sha256-2ef2a8Ru3fl0mdVBy4CDObd22z0u4AF2V9FvhSfR9B4=";
   };
 
   propagatedBuildInputs = [
