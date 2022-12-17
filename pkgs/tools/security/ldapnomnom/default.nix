9ecbb0786cece01a2cced450a266a4fe2f6b89c8
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-17 16:12:35 +0100

    ldapnomnom: add changelog to meta

--- pkgs/tools/security/ldapnomnom/default.nix
+++ pkgs/tools/security/ldapnomnom/default.nix
@@ -14,11 +14,12 @@ buildGoModule rec {
     hash = "sha256-o29vcPKRX8TWRCpa20DVsh/4K7d3IbaLS3B+jJGBEmo=";
   };
 
-  vendorSha256 = "sha256-3ucnLD+qhBSWY2wLtBcsOcuEf1woqHP17qQg7LlERA8=";
+  vendorHash = "sha256-3ucnLD+qhBSWY2wLtBcsOcuEf1woqHP17qQg7LlERA8=";
 
   meta = with lib; {
     description = "Tool to anonymously bruteforce usernames from Domain controllers";
     homepage = "https://github.com/lkarlslund/ldapnomnom";
+    changelog = "https://github.com/lkarlslund/ldapnomnom/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
