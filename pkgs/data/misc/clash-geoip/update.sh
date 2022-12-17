c8c93b4d62064668f05291f129d8f09e020d578a
Candy Cloud <candyc1oud@outlook.com>
2022-12-17 17:24:35 +0800

    clash-geoip: remove old update script

--- pkgs/data/misc/clash-geoip/update.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p common-updater-scripts curl jq
-set -euo pipefail
-
-RELEASE=$(curl "https://api.github.com/repos/Dreamacro/maxmind-geoip/releases/latest")
-update-source-version clash-geoip "$(echo $RELEASE | jq -r .tag_name)"
