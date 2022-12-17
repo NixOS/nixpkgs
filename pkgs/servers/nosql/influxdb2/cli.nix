3dd90b26310f10eb8a000a9277bb05e7b29d14d1
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-17 12:27:30 +0000

    influxdb2-cli: 2.5.0 -> 2.6.0

--- pkgs/servers/nosql/influxdb2/cli.nix
+++ pkgs/servers/nosql/influxdb2/cli.nix
@@ -5,13 +5,13 @@
 }:
 
 let
-  version = "2.5.0";
+  version = "2.6.0";
 
   src = fetchFromGitHub {
     owner = "influxdata";
     repo = "influx-cli";
     rev = "v${version}";
-    sha256 = "sha256-gztLANO42VbgA6LxiuVh8ESF20JqjC+7znYhmWJKxVA=";
+    sha256 = "sha256-uOlkqp0/X8EiuiPWe58WADXicrBhDJTX4Jj0APQ3j5U=";
   };
 
 in buildGoModule {
@@ -19,7 +19,7 @@ in buildGoModule {
   version = version;
   inherit src;
 
-  vendorSha256 = "sha256-GnVLr9mWehgw8vs4RiOrFHVlPpPT/LP6XvCq94aJxJQ=";
+  vendorSha256 = "sha256-TnPvozwp7bU4BRu3gYce1jyuMClo5YiMGskXZvZqstA=";
   subPackages = [ "cmd/influx" ];
 
   ldflags = [ "-X main.commit=v${version}" "-X main.version=${version}" ];
