66c485bf16033f77030f244fd4f8e0f7320d0464
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-12-17 15:37:37 -0800

    glob-cpp: init at 0.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17305,6 +17305,8 @@ with pkgs;
 
   github-release = callPackage ../development/tools/github/github-release { };
 
+  glob-cpp = callPackage ../development/libraries/glob-cpp { };
+
   global = callPackage ../development/tools/misc/global { };
 
   gnatcoll-db2ada = callPackage ../development/libraries/ada/gnatcoll/db.nix {
