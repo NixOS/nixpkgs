7af8780d026dbea7fe60ef8c76564e8e60020512
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-17 11:52:04 +0000

    python310Packages.dockerfile-parse: 1.2.0 -> 2.0.0

--- pkgs/development/python-modules/dockerfile-parse/default.nix
+++ pkgs/development/python-modules/dockerfile-parse/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "dockerfile-parse";
-  version = "1.2.0";
+  version = "2.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-B+Ze7DE5eOh32oGYVYcLOuR/P6yUpAqWW57eEEhNrMU=";
+    hash = "sha256-If59UQZC8rYamZ1Fw9l0X5UOEf5rokl1Vbj2N4K3jkU=";
   };
 
   propagatedBuildInputs = [
