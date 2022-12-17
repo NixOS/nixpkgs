50d1c2ce19ddd551244d2d295016ec7552b444f2
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-17 16:28:54 +0000

    libretro-core-info: 1.13.0 -> 1.14.0

--- pkgs/applications/emulators/retroarch/libretro-core-info.nix
+++ pkgs/applications/emulators/retroarch/libretro-core-info.nix
@@ -5,12 +5,12 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "libretro-core-info";
-  version = "1.13.0";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "libretro-core-info";
-    hash = "sha256-rTq2h+IGJduBkP4qCACmm3T2PvbZ0mOmwD1jLkJ2j/Q=";
+    hash = "sha256-3nw8jUxBQJxiKlWS6OjTjwUYWKx3r2E7eHmbj4naWrk=";
     rev = "v${version}";
   };
 
