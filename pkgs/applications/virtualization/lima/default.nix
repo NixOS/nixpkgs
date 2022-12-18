788a5c31da627258a538b1d7feee7ba61b666c3b
JB Gosselin <1536838+dennajort@users.noreply.github.com>
2022-12-18 01:13:08 +0100

    Combine `substituteInPlace` on `Makefile`
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/applications/virtualization/lima/default.nix
+++ pkgs/applications/virtualization/lima/default.nix
@@ -27,8 +27,9 @@ buildGoModule rec {
 
   # clean fails with read only vendor dir
   postPatch = ''
-    substituteInPlace Makefile --replace 'binaries: clean' 'binaries:'
-    substituteInPlace Makefile --replace 'codesign --entitlements vz.entitlements -s -' 'codesign --force --entitlements vz.entitlements -s -'
+    substituteInPlace Makefile \
+      --replace 'binaries: clean' 'binaries:' \
+      --replace 'codesign --entitlements vz.entitlements -s -' 'codesign --force --entitlements vz.entitlements -s -'
   '';
 
   # It attaches entitlements with codesign and strip removes those,
