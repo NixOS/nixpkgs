199924df251bb167b37d5cefce061704944e80e6
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-18 14:24:00 +0100

    python310Packages.starlette: 0.21.0 -> 0.22.0
    Diff: https://github.com/encode/starlette/compare/refs/tags/0.21.0...0.22.0
    
    Changelog: https://github.com/encode/starlette/releases/tag/0.22.0

--- pkgs/development/python-modules/starlette/default.nix
+++ pkgs/development/python-modules/starlette/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "starlette";
-  version = "0.21.0";
+  version = "0.22.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "encode";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-wZtlKCD7eE4sCKfhFgWXywACAes8uo4HXkicJAMcgqk=";
+    hash = "sha256-vyGLNQMCWu3zGFF7jRuozVLCqwR1zWBWuYTIDRBncHk=";
   };
 
   nativeBuildInputs = [
