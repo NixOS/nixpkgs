841ea664c286d5269afaa610ae682a3cbf51af81
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-18 21:14:54 +0100

    python310Packages.immutabledict: add changelog to meta

--- pkgs/development/python-modules/immutabledict/default.nix
+++ pkgs/development/python-modules/immutabledict/default.nix
@@ -35,6 +35,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "A fork of frozendict, an immutable wrapper around dictionaries";
     homepage = "https://github.com/corenting/immutabledict";
+    changelog = "https://github.com/corenting/immutabledict/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ hexa ];
   };
