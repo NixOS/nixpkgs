0d10cc917530f69f636c8848c3b1127d0b75aa8a
Francesco Gazzetta <fgaz@fgaz.me>
2022-12-18 14:07:28 +0100

    vengi-tools: 0.0.22 -> 0.0.23

--- pkgs/applications/graphics/vengi-tools/default.nix
+++ pkgs/applications/graphics/vengi-tools/default.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation rec {
   pname = "vengi-tools";
-  version = "0.0.22";
+  version = "0.0.23";
 
   src = fetchFromGitHub {
     owner = "mgerhardy";
     repo = "vengi";
     rev = "v${version}";
-    sha256 = "sha256-OlOnr1Spy8kdie9CyLVOQkY1+ib6Uwcd/xP5TSaZkYg=";
+    sha256 = "sha256-wRqQ7x6eQTrzFU++Qgq/7NXXo5F1onlZBdQ9ttNvvEw=";
   };
 
   nativeBuildInputs = [
