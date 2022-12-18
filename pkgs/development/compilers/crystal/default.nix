3efc4e5f0365462048da25bed62d1b237f733a55
GeopJr <evan@geopjr.dev>
2022-12-18 17:22:59 +0200

    fix: versionOlder => lib.versionOlder

--- pkgs/development/compilers/crystal/default.nix
+++ pkgs/development/compilers/crystal/default.nix
@@ -150,7 +150,7 @@ let
         substituteInPlace spec/std/socket/udp_socket_spec.cr \
           --replace 'it "joins and transmits to multicast groups"' 'pending "joins and transmits to multicast groups"'
 
-      '' + lib.optionalString (stdenv.isDarwin && versionOlder version "1.3.0") ''
+      '' + lib.optionalString (stdenv.isDarwin && lib.versionOlder version "1.3.0") ''
         # See https://github.com/NixOS/nixpkgs/pull/195606#issuecomment-1356491277
         substituteInPlace spec/compiler/loader/unix_spec.cr \
           --replace 'it "parses file paths"' 'pending "parses file paths"'
