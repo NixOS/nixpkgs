1336dec317f0a010df6162b415f0d1b2cce8912f
kilianar <mail@kilianar.de>
2022-12-18 22:09:32 +0100

    portfolio: 0.59.5 -> 0.60.0
    https://github.com/buchen/portfolio/releases/tag/0.60.0

--- pkgs/applications/office/portfolio/default.nix
+++ pkgs/applications/office/portfolio/default.nix
@@ -27,11 +27,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "PortfolioPerformance";
-  version = "0.59.5";
+  version = "0.60.0";
 
   src = fetchurl {
     url = "https://github.com/buchen/portfolio/releases/download/${version}/PortfolioPerformance-${version}-linux.gtk.x86_64.tar.gz";
-    sha256 = "sha256-6AEc9wc3/8AubPvqq8s9R1RnO+P8+92UsNEt6AdQv+w=";
+    sha256 = "sha256-L4ZLlxHsnJrvrrrX56Z+agjnaMl472Izm4Un1uaNqZA=";
   };
 
   nativeBuildInputs = [
