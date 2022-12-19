19ae60662f76e44299514483c1247937e55cd910
Matheus Vieira <matheus.v4200@gmail.com>
2022-12-19 19:40:52 -0300

    gruvbox-gtk-theme: init at unstable-2022-12-09

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26627,6 +26627,8 @@ with pkgs;
     inherit (plasma5Packages) breeze-icons;
   };
 
+  gruvbox-gtk-theme = callPackage ../data/themes/gruvbox-gtk-theme {};
+
   gubbi-font = callPackage ../data/fonts/gubbi { };
 
   gyre-fonts = callPackage ../data/fonts/gyre {};
