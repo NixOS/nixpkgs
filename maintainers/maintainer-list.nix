c79f6e7f949f05d6086f06060ca145bbd7f8861f
Bastien Riviere <me@babariviere.com>
2022-12-19 11:29:13 +0100

    maintainers: update babariviere

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1480,12 +1480,12 @@
     }];
   };
   babariviere = {
-    email = "babathriviere@gmail.com";
+    email = "me@babariviere.com";
     github = "babariviere";
     githubId = 12128029;
     name = "Bastien Rivière";
     keys = [{
-      fingerprint = "2F85 B362 B274 0012 37E2  81EE F202 AD3B 6EDF 4BD1";
+      fingerprint = "74AA 9AB4 E6FF 872B 3C5A  CB3E 3903 5CC0 B75D 1142";
     }];
   };
   babbaj = {
