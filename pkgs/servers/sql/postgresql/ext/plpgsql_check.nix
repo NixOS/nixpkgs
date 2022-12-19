c8663bb893b9a3abf57a9e9cf3080277992f641c
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-19 01:19:02 +0000

    postgresql11Packages.plpgsql_check: 2.2.4 -> 2.2.5

--- pkgs/servers/sql/postgresql/ext/plpgsql_check.nix
+++ pkgs/servers/sql/postgresql/ext/plpgsql_check.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "plpgsql_check";
-  version = "2.2.4";
+  version = "2.2.5";
 
   src = fetchFromGitHub {
     owner = "okbob";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-YUJLh1IgOOnNxPrH8NaY3jGEV+4mTjRffooIANkbbFo=";
+    sha256 = "sha256-47fuc0dnTWeu93P8zntsjT9L8SstN5p0WvnNtT4cHzM=";
   };
 
   buildInputs = [ postgresql ];
