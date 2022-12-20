a861573094edeb287e985569b801b4db9395fc9d
pennae <github@quasiparticle.net>
2022-12-20 17:12:33 +0100

    syncstorage-rs: make linux-only
    doesn't build on darwin, and apparently never has.

--- pkgs/servers/syncstorage-rs/default.nix
+++ pkgs/servers/syncstorage-rs/default.nix
@@ -59,5 +59,6 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/mozilla-services/syncstorage-rs";
     license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [ pennae ];
+    platforms = lib.platforms.linux;
   };
 }
