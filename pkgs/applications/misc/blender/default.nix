d926a7c1c1956bac86aecf971dc9fb1fd5ee30d8
Dominik Ritter <dritter03@googlemail.com>
2022-12-20 21:56:21 +0100

    blender: 3.3.1 -> 3.4.1

--- pkgs/applications/misc/blender/default.nix
+++ pkgs/applications/misc/blender/default.nix
@@ -28,11 +28,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "blender";
-  version = "3.3.1";
+  version = "3.4.1";
 
   src = fetchurl {
     url = "https://download.blender.org/source/${pname}-${version}.tar.xz";
-    hash = "sha256-KtpI8L+KDKgCuYfXV0UgEuH48krPTSNFOwnC1ZURjMo=";
+    hash = "sha256-JHxMEignDJAQ9HIcmFy1tiirUKvPnyZ4Ywc3FC7rkcM=";
   };
 
   patches = lib.optional stdenv.isDarwin ./darwin.patch;
