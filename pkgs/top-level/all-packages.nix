8909db923f2b826bb4482fba46f7a6d798c6240c
Vincent Haupert <mail@vincent-haupert.de>
2022-12-20 13:06:45 +0100

    azure-static-sites-client: init at 1.0.021731

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2667,6 +2667,8 @@ with pkgs;
 
   azure-functions-core-tools = callPackage ../development/tools/azure-functions-core-tools { };
 
+  azure-static-sites-client = callPackage ../development/tools/azure-static-sites-client { };
+
   azure-storage-azcopy = callPackage ../development/tools/azcopy { };
 
   bashblog = callPackage ../tools/text/bashblog { };
