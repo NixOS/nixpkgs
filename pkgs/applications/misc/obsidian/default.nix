36ee125efe686918f7753de341e440c3358040b3
Pawel Peregud <paulperegud@protonmail.com>
2022-12-20 17:22:28 +0100

    obsidian: 1.0.3 -> 1.1.8

--- pkgs/applications/misc/obsidian/default.nix
+++ pkgs/applications/misc/obsidian/default.nix
@@ -12,7 +12,7 @@
 let
   inherit (stdenv.hostPlatform) system;
   pname = "obsidian";
-  version = "1.0.3";
+  version = "1.1.8";
   appname = "Obsidian";
   meta = with lib; {
     description = "A powerful knowledge base that works on top of a local folder of plain text Markdown files";
@@ -25,7 +25,7 @@ let
   filename = if stdenv.isDarwin then "Obsidian-${version}-universal.dmg" else "obsidian-${version}.tar.gz";
   src = fetchurl {
     url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v${version}/${filename}";
-    sha256 = if stdenv.isDarwin then "sha256-DYF9fEpZaP4tD/eeZAegDahR7UZyroqNB9bn2U7sgXs=" else "sha256-MpQk5g4184ZkCAjLU5Ug0ReWgVADskS1QuMcnPdNofs=";
+    sha256 = if stdenv.isDarwin then "sha256-eq8xJCYAmKPM4AZYSUbPFCXklXHvqF3ccX3owzzF7BA=" else "sha256-EGSftd1APwLnCu05sYd2LSe7mGi2nEv8PI9mxS5MFN0=";
   };
 
   icon = fetchurl {
