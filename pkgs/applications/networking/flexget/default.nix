bd24f46f04b1f1fdc18293d1a6176a77f2df959c
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-21 22:09:05 +0000

    flexget: 3.5.12 -> 3.5.13

--- pkgs/applications/networking/flexget/default.nix
+++ pkgs/applications/networking/flexget/default.nix
@@ -5,7 +5,7 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "flexget";
-  version = "3.5.12";
+  version = "3.5.13";
   format = "pyproject";
 
   # Fetch from GitHub in order to use `requirements.in`
@@ -13,7 +13,7 @@ python3Packages.buildPythonApplication rec {
     owner = "flexget";
     repo = "flexget";
     rev = "refs/tags/v${version}";
-    hash = "sha256-nrW6+BhcDvFt7bw0XdiDk2eNnM+ut24xTdPulNDw9X4=";
+    hash = "sha256-0yO4prnYJkD7eiyrEOPHlDTsgGgRhQujsp8k2FsLYKI=";
   };
 
   postPatch = ''
