8b9e529f6e6301db04e13647a753f8514cb43230
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-22 20:55:51 +0100

    python310Packages.hahomematic: 2022.12.6 -> 2022.12.7
    Changelog: https://github.com/danielperna84/hahomematic/releases/tag/2022.12.7

--- pkgs/development/python-modules/hahomematic/default.nix
+++ pkgs/development/python-modules/hahomematic/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "hahomematic";
-  version = "2022.12.6";
+  version = "2022.12.7";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "danielperna84";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-bTplDaEFvDEGWPkhVl23iy4FSniu+HhMrbs3AUTgYM0=";
+    sha256 = "sha256-Noxamy9tPqBDdEExt8yUDi3Blo5aMdTFS7nCYn5B7NI=";
   };
 
   nativeBuildInputs = [
