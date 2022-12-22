ed9a30150fbde85186c31a89c2257ee31f6488c8
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-22 12:24:28 +0000

    python39Packages.sphinxcontrib-spelling: 7.6.2 -> 7.7.0

--- pkgs/development/python-modules/sphinxcontrib-spelling/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-spelling/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "sphinxcontrib-spelling";
-  version = "7.6.2";
+  version = "7.7.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-oSnV3QwAydQUvN9Zmv3bVbrgHx1UNjbn67CUkborq9Q=";
+    hash = "sha256-VlYcP2oVWwlGkU5N6YhymFkxVyncGBteTcimj+eN41o=";
   };
 
   nativeBuildInputs = [
