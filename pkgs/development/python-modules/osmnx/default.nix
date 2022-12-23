8964ec99c4a900384132f4c93d71b2626d054a44
Robert Schütz <nix@dotlambda.de>
2022-12-23 12:04:52 -0800

    python310Packages.osmnx: 1.2.2 -> 1.2.3
    https://github.com/gboeing/osmnx/blob/v1.2.3/CHANGELOG.md

--- pkgs/development/python-modules/osmnx/default.nix
+++ pkgs/development/python-modules/osmnx/default.nix
@@ -1,26 +1,57 @@
-{ lib, buildPythonPackage, fetchFromGitHub, pythonOlder, geopandas, matplotlib, networkx, numpy
-, pandas, requests, Rtree, shapely, folium, scikit-learn, scipy, gdal, rasterio}:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pythonOlder
+, geopandas
+, matplotlib
+, networkx
+, numpy
+, pandas
+, pyproj
+, requests
+, Rtree
+, shapely
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "osmnx";
-  version = "1.2.2";
-  disabled = pythonOlder "3.8";
+  version = "1.2.3";
+  disabled = pythonOlder "3.9";
+
+  format = "setuptools";
 
   src = fetchFromGitHub {
-    owner  = "gboeing";
-    repo   = pname;
-    rev    = "v${version}";
-    sha256 = "sha256-+dUv1QrUmCIOCyUyjYX1kJtZrPuSp3t9xz/sRV7ppgA=";
+    owner = "gboeing";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-W/L5FBo1PYE8zkaUO2a5d+f0laCklrIRVKaaMQyB+tM=";
   };
 
-  propagatedBuildInputs = [ geopandas matplotlib networkx numpy pandas requests Rtree shapely folium scikit-learn scipy gdal rasterio ];
+  propagatedBuildInputs = [
+    geopandas
+    matplotlib
+    networkx
+    numpy
+    pandas
+    pyproj
+    requests
+    Rtree
+    shapely
+  ];
 
   # requires network
-  doCheck = false;
+  #doCheck = false;
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
   pythonImportsCheck = [ "osmnx" ];
 
   meta = with lib; {
-    description = "A package to easily download, construct, project, visualize, and analyze complex street networks from OpenStreetMap with NetworkX.";
+    changelog = "https://github.com/gboeing/osmnx/blob/${src.rev}/CHANGELOG.md";
+    description = "Retrieve, model, analyze, and visualize OpenStreetMap street networks and other spatial data";
     homepage = "https://github.com/gboeing/osmnx";
     license = licenses.mit;
     maintainers = with maintainers; [ psyanticy ];
