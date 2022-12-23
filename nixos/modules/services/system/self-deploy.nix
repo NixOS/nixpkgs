154a802958947dbeeb91c514b6f262e3f3e34854
Gabriella Gonzalez <Gabriel439@gmail.com>
2022-12-23 13:53:41 -0600

    Undo change to argument escaping
    … as caught by @roberth
    
    I don't remember why I originally made the change

--- nixos/modules/services/system/self-deploy.nix
+++ nixos/modules/services/system/self-deploy.nix
@@ -150,7 +150,7 @@ in
           git init ${repositoryDirectory}
         fi
 
-        ${gitWithRepo} fetch ${cfg.repository} ${lib.escapeShellArg cfg.branch}
+        ${gitWithRepo} fetch ${lib.escapeShellArg cfg.repository} ${lib.escapeShellArg cfg.branch}
 
         ${gitWithRepo} checkout FETCH_HEAD
 
