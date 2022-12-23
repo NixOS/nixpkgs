4af515bdb964638f4ee614499b27b0d0e2754458
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-23 07:35:12 +0000

    dolphin-emu-beta: 5.0-17269 -> 5.0-17995

--- pkgs/applications/emulators/dolphin-emu/master.nix
+++ pkgs/applications/emulators/dolphin-emu/master.nix
@@ -52,13 +52,13 @@
 
 stdenv.mkDerivation rec {
   pname = "dolphin-emu";
-  version = "5.0-17269";
+  version = "5.0-17995";
 
   src = fetchFromGitHub {
     owner = "dolphin-emu";
     repo = "dolphin";
-    rev = "48c9c224cf9f82f0f9f2690b7cc6283d7448480c";
-    sha256 = "sha256-WC3jukRygZigLx987CzRmOmJ7DeS1atXrMzU98sRzEg=";
+    rev = "8bad821019721b9b72701b495da95656ace5fea5";
+    sha256 = "sha256-uxHzn+tXRBr11OPpZ4ELBw7DTJH4mnqUBOeyPlXNAh8=";
     fetchSubmodules = true;
   };
 
