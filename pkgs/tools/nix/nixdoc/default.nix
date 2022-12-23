b3792b44c55ce58147055e553cd45f1f04ca3e51
Silvan Mosberger <silvan.mosberger@tweag.io>
2022-12-23 20:57:19 +0100

    lib: correctly render docs for nested identifiers
    Applies https://github.com/nix-community/nixdoc/pull/27 and fixes up the
    location information rendering, allowing lib functions to be nested in
    further scopes
    
    Needed for later commits

--- pkgs/tools/nix/nixdoc/default.nix
+++ pkgs/tools/nix/nixdoc/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, rustPlatform, darwin }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, rustPlatform, darwin }:
 
 rustPlatform.buildRustPackage rec {
   pname = "nixdoc";
@@ -10,6 +10,14 @@ rustPlatform.buildRustPackage rec {
     rev = "v${version}";
     sha256 = "14d4dq06jdqazxvv7fq5872zy0capxyb0fdkp8qg06gxl1iw201s";
   };
+
+  patches = [
+    # Support nested identifiers https://github.com/nix-community/nixdoc/pull/27
+    (fetchpatch {
+      url = "https://github.com/nix-community/nixdoc/pull/27/commits/ea542735bf675fe2ccd37edaffb9138d1a8c1b7e.patch";
+      sha256 = "1fmz44jv2r9qsnjxvkkjfb0safy69l4x4vx1g5gisrp8nwdn94rj";
+    })
+  ];
 
   buildInputs =  lib.optionals stdenv.isDarwin [ darwin.Security ];
 
