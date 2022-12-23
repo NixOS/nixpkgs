59742a5e0446a204c27ea9ab3458521674ea75f9
Karl Hallsby <karl@hallsby.com>
2022-12-23 10:39:25 -0600

    octavePackages.octproj: unmark as broken
    This package broke when Octave went to version 7.1.
    nixpkgs commit 8399907be3c008b6e12863364d6f7848f52034b1 fixed this.
    
    Octave's 7.2 release built the fix into Octave itself, so this is also
    a non-issue now.

--- pkgs/development/octave-modules/octproj/default.nix
+++ pkgs/development/octave-modules/octproj/default.nix
@@ -30,6 +30,5 @@ buildOctavePackage rec {
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ KarlJoad ];
     description = "GNU Octave bindings to PROJ library for cartographic projections and CRS transformations";
-    broken = true; # error: unlink: operation failed: No such file or directory
   };
 }
