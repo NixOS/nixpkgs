0667ef5dd5cc7aa00e7d5ebf4391b7ee1d414a0c
Silvan Mosberger <silvan.mosberger@tweag.io>
2022-12-23 21:08:09 +0100

    lib.path.subpath.normalise: add property tests

--- lib/path/tests/default.nix
+++ lib/path/tests/default.nix
@@ -7,10 +7,14 @@
     inherit system;
   },
   libpath ? ../..,
+  # Random seed
+  seed ? null,
 }:
 pkgs.runCommand "lib-path-tests" {
   nativeBuildInputs = with pkgs; [
     nix
+    jq
+    bc
   ];
 } ''
   # Needed to make Nix evaluation work
@@ -22,6 +26,9 @@ pkgs.runCommand "lib-path-tests" {
   echo "Running unit tests lib/path/tests/unit.nix"
   nix-instantiate --eval lib/path/tests/unit.nix \
     --argstr libpath "$TEST_LIB"
+
+  echo "Running property tests lib/path/tests/prop.sh"
+  bash lib/path/tests/prop.sh ${toString seed}
 
   touch $out
 ''
