f5e81d80c9387a8244d6a20825e8dc140a418693
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-24 13:16:09 +0100

    python310Packages.striprtf: add changelog to meta

--- pkgs/development/python-modules/striprtf/default.nix
+++ pkgs/development/python-modules/striprtf/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 }:
 
 buildPythonPackage rec {
@@ -8,9 +9,11 @@ buildPythonPackage rec {
   version = "0.0.22";
   format = "setuptools";
 
+  disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-T7rb6EJPQStbJjv3L7PA2rTdtXTWCd1NCE9uK4sEQCs=";
+    hash = "sha256-T7rb6EJPQStbJjv3L7PA2rTdtXTWCd1NCE9uK4sEQCs=";
   };
 
   pythonImportsCheck = [
@@ -18,6 +21,7 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
+    changelog = "https://github.com/joshy/striprtf/blob/v${version}/CHANGELOG.md";
     homepage = "https://github.com/joshy/striprtf";
     description = "A simple library to convert rtf to text";
     maintainers = with maintainers; [ aanderse ];
