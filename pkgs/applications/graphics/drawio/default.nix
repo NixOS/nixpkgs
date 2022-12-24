f7667eed3d54f15b79e54cfe116c55d29b07c5f8
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-24 09:44:13 +0000

    drawio: 20.6.2 -> 20.7.4

--- pkgs/applications/graphics/drawio/default.nix
+++ pkgs/applications/graphics/drawio/default.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation rec {
   pname = "drawio";
-  version = "20.6.2";
+  version = "20.7.4";
 
   src = fetchurl {
     url = "https://github.com/jgraph/drawio-desktop/releases/download/v${version}/drawio-x86_64-${version}.rpm";
-    sha256 = "cf408c19622d7812b93f0f778b7f091a4387992f9d9a767f4bc1c417b61b7058";
+    sha256 = "sha256-gmC8s+0eef09wcLE9fBiG82rRPXijRyR1WTlp4gYPq4=";
   };
 
   nativeBuildInputs = [
