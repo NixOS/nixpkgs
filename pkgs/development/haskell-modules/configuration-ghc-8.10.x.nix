bb6e7f184d107ed840f3e2a1946bd571a24b37b8
Naïm Favier <n@monade.li>
2022-12-24 11:36:06 +0100

    haskell.packages.ghc810.haskell-language-server: fix eval
    Mark some packages as not broken on this GHC version to fix eval errors.

--- pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
@@ -118,15 +118,18 @@ self: super: {
     ]) (super.hls-brittany-plugin.overrideScope (lself: lsuper: {
     brittany = doJailbreak lself.brittany_0_13_1_2;
     aeson = lself.aeson_1_5_6_0;
-    multistate = dontCheck lsuper.multistate;
+    multistate = doDistribute (markUnbroken (dontCheck lsuper.multistate));
     lsp-types = doJailbreak lsuper.lsp-types; # Checks require aeson >= 2.0
   }))));
 
   # This package is marked as unbuildable on GHC 9.2, so hackage2nix doesn't include any dependencies.
   # See https://github.com/NixOS/nixpkgs/pull/205902 for why we use `self.<package>.scope`
-  hls-haddock-comments-plugin = addBuildDepends (with self.hls-haddock-comments-plugin.scope; [
+  hls-haddock-comments-plugin = doDistribute (markUnbroken (addBuildDepends (with self.hls-haddock-comments-plugin.scope; [
     ghc-exactprint ghcide hls-plugin-api hls-refactor-plugin lsp-types unordered-containers
-  ]) super.hls-haddock-comments-plugin;
+  ]) super.hls-haddock-comments-plugin));
+
+  hls-splice-plugin = doDistribute (markUnbroken super.hls-splice-plugin);
+  hls-tactics-plugin = doDistribute (markUnbroken super.hls-tactics-plugin);
 
   mime-string = disableOptimization super.mime-string;
 
