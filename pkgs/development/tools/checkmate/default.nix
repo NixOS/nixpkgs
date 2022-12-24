1be983909e343c8cf6b7ba730569272674681de8
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-24 12:29:34 +0000

    checkmate: 0.8.2 -> 0.8.4

--- pkgs/development/tools/checkmate/default.nix
+++ pkgs/development/tools/checkmate/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "checkmate";
-  version = "0.8.2";
+  version = "0.8.4";
 
   src = fetchFromGitHub {
     owner = "adedayo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-v9xVJFX3YJQU9z9L7dhy0S1FvpWoDad36Lq3w4VW0xA=";
+    sha256 = "sha256-tgiZDTPIAYirPX6nGPEAt6BoYEC8uUJwT6zuHJqPF1w=";
   };
 
-  vendorSha256 = "sha256-8/EGoY3+th34gAACDoEHgwhUFmyyKecnQP/WTe56iCQ=";
+  vendorSha256 = "sha256-eL1fLJwzVpU9NqaAl5R/fbaqI3AnEkl6EuPkMTuY86w=";
 
   subPackages = [ "." ];
 
