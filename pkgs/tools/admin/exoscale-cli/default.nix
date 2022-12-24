7d30e602151e5496b4b09483b801a5f192571079
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-24 01:41:55 +0000

    exoscale-cli: 1.61.0 -> 1.63.0

--- pkgs/tools/admin/exoscale-cli/default.nix
+++ pkgs/tools/admin/exoscale-cli/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "exoscale-cli";
-  version = "1.61.0";
+  version = "1.63.0";
 
   src = fetchFromGitHub {
     owner = "exoscale";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-3ak/z6Ww+/VY1/TErQ+CBxq5xte3VeGbCz2P5QlHxLg=";
+    sha256 = "sha256-YRpaPUKExHewuDqIlXjS32Bu6eMHam153Cfbv+crs9M=";
   };
 
   vendorSha256 = null;
