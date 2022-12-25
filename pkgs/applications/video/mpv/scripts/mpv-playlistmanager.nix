e9a1b94d7a9dde57ff67b8e180825192c0746f1a
lunik1 <ch.gpg@themaw.xyz>
2022-12-25 23:16:42 +0000

    mpv-playlistmanager: unstable-2022-08-26 → unstable-2022-12-14

--- pkgs/applications/video/mpv/scripts/mpv-playlistmanager.nix
+++ pkgs/applications/video/mpv/scripts/mpv-playlistmanager.nix
@@ -2,18 +2,19 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "mpv-playlistmanager";
-  version = "unstable-2022-08-26";
+  version = "unstable-2022-12-14";
 
   src = fetchFromGitHub {
     owner = "jonniek";
     repo = "mpv-playlistmanager";
-    rev = "07393162f7f78f8188e976f616f1b89813cec741";
-    sha256 = "sha256-Vgh5F6c90ijp5LVrP2cdAOXo+QtJ9aXI9G/3C2HGqd4=";
+    rev = "0c562fd104b5447082813b482733a3c307466568";
+    sha256 = "sha256-KhJcZQzM4JzIyIiWfnLbP+DsNoVr8UNbL3EyJ6rxDkA=";
   };
 
   postPatch = ''
     substituteInPlace playlistmanager.lua \
-      --replace "youtube-dl" "${lib.getBin yt-dlp}/bin/yt-dlp"
+      --replace 'youtube_dl_executable = "youtube-dl",' \
+      'youtube_dl_executable = "${lib.getBin yt-dlp}/bin/yt-dlp"',
   '';
 
   dontBuild = true;
