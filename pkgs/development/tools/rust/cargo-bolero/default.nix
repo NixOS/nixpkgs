03b71034639e9d965c61028e32eaa432e7878e7a
figsoda <figsoda@pm.me>
2022-12-25 14:19:05 -0500

    cargo-bolero: clean up

--- pkgs/development/tools/rust/cargo-bolero/default.nix
+++ pkgs/development/tools/rust/cargo-bolero/default.nix
@@ -1,25 +1,22 @@
-{ lib, fetchFromGitHub, rustPlatform, stdenv, libbfd, libopcodes, libunwind }:
+{ lib, rustPlatform, fetchCrate, libbfd, libopcodes, libunwind }:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-bolero";
   version = "0.6.2";
 
-  src = fetchFromGitHub {
-    owner = "camshaft";
-    repo = "bolero";
-    rev = "${pname}-v${version}";
-    sha256 = "1p8g8av0l1qsmq09m0nwyyryk1v5bbah5izl4hf80ivi41mywkyi";
+  src = fetchCrate {
+    inherit pname version;
+    sha256 = "sha256-fyXXYV5X9t5biw1byWd1B2eQaPxdQPFWe3kVrLth2Ns=";
   };
 
-  cargoLock.lockFile = ./Cargo.lock;
-  postPatch = "cp ${./Cargo.lock} Cargo.lock";
+  cargoSha256 = "sha256-g8WeDqv5o4GKAd32+SJGLHl/cozWTVrrj1vWwvxX8Bw=";
 
   buildInputs = [ libbfd libopcodes libunwind ];
 
   meta = with lib; {
     description = "Fuzzing and property testing front-end framework for Rust";
-    homepage = "https://github.com/camshaft/cargo-bolero";
+    homepage = "https://github.com/camshaft/bolero";
     license = with licenses; [ mit ];
-    maintainers = [ maintainers.ekleog ];
+    maintainers = with maintainers; [ ekleog ];
   };
 }
