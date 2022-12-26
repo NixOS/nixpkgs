c6442ec5c20888c570d5321ef8d47530d9b5c35e
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-26 02:23:33 +0000

    tesseract: 3.05.00 -> 5.3.0

--- pkgs/applications/graphics/tesseract/tesseract3.nix
+++ pkgs/applications/graphics/tesseract/tesseract3.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "tesseract";
-  version = "3.05.00";
+  version = "5.3.0";
 
   src = fetchFromGitHub {
     owner = "tesseract-ocr";
     repo = "tesseract";
     rev = version;
-    hash = "sha256-YHj00gG/3SW0ILTiQwphiCxuP9OCDya27hyFQB27mYc=";
+    hash = "sha256-Y+RZOnBCjS8XrWeFA4ExUxwsuWA0DndNtpIWjtRi1G8=";
   };
 
   enableParallelBuilding = true;
