14d25dc0ef0c418b7163ff9884861065e30c1c3c
Sandro <sandro.jaeckel@gmail.com>
2022-12-26 22:05:36 +0100

    Apply suggestions from code review

--- pkgs/applications/version-management/forgejo/default.nix
+++ pkgs/applications/version-management/forgejo/default.nix
@@ -6,18 +6,19 @@ gitea.overrideAttrs (old: rec {
 
   src = fetchurl {
     name = "${pname}-src-${version}.tar.gz";
+    # see https://codeberg.org/forgejo/forgejo/releases
     url = "https://codeberg.org/attachments/976c426a-3e04-49ff-9762-47fab50624a3";
     hash = "sha256-kreBMHlMVB1UeG67zMbszGrgjaROateCRswH7GrKnEw=";
   };
 
-  postInstall = old.postInstall + ''
+  postInstall = old.postInstall or "" + ''
     mv $out/bin/{${old.pname},${pname}}
   '';
 
   meta = with lib; {
     description = "A self-hosted lightweight software forge";
     homepage = "https://forgejo.org";
-    changelog = "https://codeberg.org/${pname}/${pname}/releases/tag/v${version}";
+    changelog = "https://codeberg.org/forgejo/forgejo/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ urandom ];
   };
