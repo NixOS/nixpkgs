48adc1627bc7efc33f0f964cc2eafea0b1902f71
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-27 02:55:55 +0000

    python39Packages.nvidia-ml-py: 11.515.48 -> 11.515.75

--- pkgs/development/python-modules/nvidia-ml-py/default.nix
+++ pkgs/development/python-modules/nvidia-ml-py/default.nix
@@ -5,13 +5,13 @@
 
 buildPythonPackage rec {
   pname = "nvidia-ml-py";
-  version = "11.515.48";
+  version = "11.515.75";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
     extension = "tar.gz";
-    hash = "sha256-iNLQu9c8Q3B+FXMObRTtxqE3B/siJIlIlCH6T0rX+sY=";
+    hash = "sha256-48dfBtWjIB3FETbgDljFwTKzvl1gTYbBQ0Jq205BxJA=";
   };
 
   patches = [
