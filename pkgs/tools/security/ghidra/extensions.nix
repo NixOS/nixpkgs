85dc0632cd7b05b6e5f7ff84623dc7c1781d13f4
Emily Trau <me@angus.ws>
2022-12-27 22:03:14 +1100

    ghidra-extensions.sleighdevtools: init at 11.0.2

--- pkgs/tools/security/ghidra/extensions.nix
+++ pkgs/tools/security/ghidra/extensions.nix
@@ -6,4 +6,7 @@ lib.makeScope newScope (self: {
   ghidraninja-ghidra-scripts = self.callPackage ./extensions/ghidraninja-ghidra-scripts { };
 
   gnudisassembler = self.callPackage ./extensions/gnudisassembler { inherit ghidra; };
+
+  sleighdevtools = self.callPackage ./extensions/sleighdevtools { inherit ghidra; };
+
 })
