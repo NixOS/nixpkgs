5c4145698069cbabfe85509f7f6eec4dbc23d19f
Mike <62124625+michaelBelsanti@users.noreply.github.com>
2022-12-27 18:50:22 +0000

    maintainers: quasigod-io -> michaelBelsanti
    I have updated by GitHub account name, so I am updating myself in maintainer-list.nix to match.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9158,6 +9158,12 @@
     githubId = 1575834;
     name = "Michael Adler";
   };
+  michaelBelsanti = {
+    email = "mbels03@protonmail.com";
+    name = "Mike Belsanti";
+    github = "michaelBelsanti";
+    githubId = 62124625;
+  };
   michaelpj = {
     email = "michaelpj@gmail.com";
     github = "michaelpj";
@@ -16115,12 +16121,6 @@
     name = "Andrei Boar";
     github = "zuzuleinen";
     githubId = 944919;
-  };
-  quasigod-io = {
-    email = "quasigod-io@protonmail.com";
-    name = "Michael Belsanti";
-    github = "quasigod-io";
-    githubId = 62124625;
   };
   waelwindows = {
     email = "waelwindows9922@gmail.com";
