17b0cfb2ea76021921d5f5e5266dfcace5eefeca
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-27 22:41:34 +0100

    python310Packages.asana: fix typo

--- pkgs/development/python-modules/asana/default.nix
+++ pkgs/development/python-modules/asana/default.nix
@@ -39,7 +39,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python client library for Asana";
     homepage = "https://github.com/asana/python-asana";
-    chnagelog = "https://github.com/Asana/python-asana/releases/tag/v${version}";
+    changelog = "https://github.com/Asana/python-asana/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ ];
   };
