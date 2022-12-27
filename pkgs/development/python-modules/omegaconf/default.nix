47967562fad687ba6d6ce0fc2a8d075455da6162
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-27 19:42:39 +0100

    python3Packages.omegaconf: relax antlr4 version

--- pkgs/development/python-modules/omegaconf/default.nix
+++ pkgs/development/python-modules/omegaconf/default.nix
@@ -1,4 +1,5 @@
 { lib
+, antlr4
 , antlr4-python3-runtime
 , buildPythonPackage
 , fetchFromGitHub
@@ -8,6 +9,7 @@
 , pytestCheckHook
 , pythonOlder
 , pyyaml
+, substituteAll
 }:
 
 buildPythonPackage rec {
@@ -23,6 +25,21 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-Qxa4uIiX5TAyQ5rFkizdev60S4iVAJ08ES6FpNqf8zI=";
   };
+
+  patches = [
+    (substituteAll {
+      src = ./antlr4.patch;
+      antlr_jar = "${antlr4.out}/share/java/antlr-${antlr4.version}-complete.jar";
+    })
+  ];
+
+  postPatch = ''
+    # We substitute the path to the jar with the one from our antlr4
+    # package, so this file becomes unused
+    rm -v build_helpers/bin/antlr*-complete.jar
+
+    sed -i 's/antlr4-python3-runtime==.*/antlr4-python3-runtime/' requirements/base.txt
+  '';
 
   nativeBuildInputs = [
     jre_minimal
