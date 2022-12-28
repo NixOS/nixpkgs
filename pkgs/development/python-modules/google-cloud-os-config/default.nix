ba0f01af456711a1e8b2e2652a2bfd8287b738f2
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 16:49:08 +0100

    python310Packages.google-cloud-os-config: add changelog to meta

--- pkgs/development/python-modules/google-cloud-os-config/default.nix
+++ pkgs/development/python-modules/google-cloud-os-config/default.nix
@@ -46,6 +46,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Google Cloud OS Config API client library";
     homepage = "https://github.com/googleapis/python-os-config";
+    changelog = "https://github.com/googleapis/python-os-config/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
