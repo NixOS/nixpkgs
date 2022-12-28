dc3b1ab179dc19b516ece4611575200bf395d444
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 09:15:29 +0100

    python310Packages.google-nest-sdm: add changelog to meta

--- pkgs/development/python-modules/google-nest-sdm/default.nix
+++ pkgs/development/python-modules/google-nest-sdm/default.nix
@@ -56,6 +56,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Module for Google Nest Device Access using the Smart Device Management API";
     homepage = "https://github.com/allenporter/python-google-nest-sdm";
+    changelog = "https://github.com/allenporter/python-google-nest-sdm/releases/tag/${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
