9a911023c1a49f98775965b24b3df4d42df0a677
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 13:05:08 +0100

    python310Packages.google-cloud-iam-logging: 1.0.6 -> 1.1.0
    Changelog: https://github.com/googleapis/python-iam-logging/blob/v1.1.0/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-iam-logging/default.nix
+++ pkgs/development/python-modules/google-cloud-iam-logging/default.nix
@@ -5,6 +5,7 @@
 , grpc-google-iam-v1
 , mock
 , proto-plus
+, protobuf
 , pytest-asyncio
 , pytestCheckHook
 , pythonOlder
@@ -12,21 +13,22 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-iam-logging";
-  version = "1.0.6";
+  version = "1.1.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-XiuEki6bNKZ8CzfcBBjnDbAh2yEADZohpP991OTf2DI=";
+    hash = "sha256-q+R8l14wD0PNxP1xKwZcXlbyln3uwoscAsOvletuetg=";
   };
 
   propagatedBuildInputs = [
     google-api-core
     grpc-google-iam-v1
     proto-plus
-  ];
+    protobuf
+  ] ++ google-api-core.optional-dependencies.grpc;
 
   checkInputs = [
     mock
