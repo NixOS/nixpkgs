cacebfa9eb929b5c6d803194297c61b1d8a05b2d
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 16:17:42 +0100

    python310Packages.google-cloud-tasks: add changelog to meta

--- pkgs/development/python-modules/google-cloud-tasks/default.nix
+++ pkgs/development/python-modules/google-cloud-tasks/default.nix
@@ -51,6 +51,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Cloud Tasks API API client library";
     homepage = "https://github.com/googleapis/python-tasks";
+    changelog = "https://github.com/googleapis/python-tasks/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
