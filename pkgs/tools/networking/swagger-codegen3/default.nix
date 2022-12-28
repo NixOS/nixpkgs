9344a238f7e0dbbfa5d6e5eb911d273c9343bcce
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-28 05:45:51 +0000

    swagger-codegen3: 3.0.35 -> 3.0.36

--- pkgs/tools/networking/swagger-codegen3/default.nix
+++ pkgs/tools/networking/swagger-codegen3/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchurl, jre, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  version = "3.0.35";
+  version = "3.0.36";
   pname = "swagger-codegen";
 
   jarfilename = "${pname}-cli-${version}.jar";
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://maven/io/swagger/codegen/v3/${pname}-cli/${version}/${jarfilename}";
-    sha256 = "sha256-GTqB2wyDguzxrVgnkGiQGgkDVt+caaoyRvUdpeItPcA=";
+    sha256 = "sha256-nJsjGTtTM9ZjsqZCKKZFrLPZ4jIp1F0Spi6RzeuCzak=";
   };
 
   dontUnpack = true;
