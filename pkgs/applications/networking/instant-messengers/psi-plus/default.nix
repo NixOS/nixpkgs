9bea102076ab715ba7766263298c4d9753a4240b
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-28 17:51:41 +0000

    psi-plus: 1.5.1642 -> 1.5.1644

--- pkgs/applications/networking/instant-messengers/psi-plus/default.nix
+++ pkgs/applications/networking/instant-messengers/psi-plus/default.nix
@@ -43,13 +43,13 @@ assert enablePsiMedia -> enablePlugins;
 
 mkDerivation rec {
   pname = "psi-plus";
-  version = "1.5.1642";
+  version = "1.5.1644";
 
   src = fetchFromGitHub {
     owner = "psi-plus";
     repo = "psi-plus-snapshots";
     rev = version;
-    sha256 = "sha256-voaGYYt1CZTxiumKvIVupMxdrLKACnfxvtnYfGuya1I=";
+    sha256 = "sha256-wSfgV0moBar27xa91ywky7Apr5QHISZ27lQlp02J1Mg=";
   };
 
   cmakeFlags = [
