a7cf0bbfa665b09a76db1022b9ce9828e6118764
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 15:02:12 +0100

    python310Packages.google-cloud-core: add changelog to meta

--- pkgs/development/python-modules/google-cloud-core/default.nix
+++ pkgs/development/python-modules/google-cloud-core/default.nix
@@ -4,31 +4,43 @@
 , pytestCheckHook
 , google-api-core
 , mock
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "google-cloud-core";
   version = "2.3.2";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-uVKe5wR/2NS/SiGC3mGRVCQN8X++YOrTmQeMGuFSr5o=";
   };
 
-  propagatedBuildInputs = [ google-api-core ];
+  propagatedBuildInputs = [
+    google-api-core
+  ];
 
-  checkInputs = [ mock pytestCheckHook ];
+  checkInputs = [
+    mock
+    pytestCheckHook
+  ];
 
   # prevent google directory from shadowing google imports
   preCheck = ''
     rm -r google
   '';
 
-  pythonImportsCheck = [ "google.cloud" ];
+  pythonImportsCheck = [
+    "google.cloud"
+  ];
 
   meta = with lib; {
     description = "API Client library for Google Cloud: Core Helpers";
     homepage = "https://github.com/googleapis/python-cloud-core";
+    changelog = "https://github.com/googleapis/python-cloud-core/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
