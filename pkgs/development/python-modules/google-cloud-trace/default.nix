50bdd1283b5b509e8354d34848131339b35e653e
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 16:11:13 +0100

    python310Packages.google-cloud-trace: add changelog to meta

--- pkgs/development/python-modules/google-cloud-trace/default.nix
+++ pkgs/development/python-modules/google-cloud-trace/default.nix
@@ -51,6 +51,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Cloud Trace API client library";
     homepage = "https://github.com/googleapis/python-trace";
+    changelog = "https://github.com/googleapis/python-trace/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
