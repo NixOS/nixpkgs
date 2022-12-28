dbeea53715caf8730d6acf7d8b6a38d589daf12f
Stefan Frijters <sfrijters@gmail.com>
2022-12-28 17:23:09 +0100

    maestral-gui: 1.6.4 -> 1.6.5

--- pkgs/applications/networking/maestral-qt/default.nix
+++ pkgs/applications/networking/maestral-qt/default.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "maestral-qt";
-  version = "1.6.4";
+  version = "1.6.5";
   disabled = python3.pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "SamSchott";
     repo = "maestral-qt";
     rev = "refs/tags/v${version}";
-    hash = "sha256-VoqZF2dJkKhaB2xSAtKLGbZJZjcfR1g8/aPcKnRuq8Y=";
+    hash = "sha256-yKsCM8LZ/GR/bc2WW+Ml1vSroB4iaxh09Az/B+aIVBU=";
   };
 
   format = "pyproject";
