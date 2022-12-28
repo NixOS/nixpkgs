c9b08a57cd6ba74374b14e174e468940bf52af66
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 18:28:15 +0100

    python310Packages.fireflyalgorithm: add changelog to meta

--- pkgs/development/python-modules/fireflyalgorithm/default.nix
+++ pkgs/development/python-modules/fireflyalgorithm/default.nix
@@ -12,13 +12,13 @@ buildPythonPackage rec {
   version = "0.3.4";
   format = "pyproject";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "firefly-cpp";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-rJOcPQU/oz/qP787OpZsfbjSsT2dWvhJLTs4N5TriWc=";
+    hash = "sha256-rJOcPQU/oz/qP787OpZsfbjSsT2dWvhJLTs4N5TriWc=";
   };
 
   nativeBuildInputs = [
@@ -40,6 +40,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "An implementation of the stochastic nature-inspired algorithm for optimization";
     homepage = "https://github.com/firefly-cpp/FireflyAlgorithm";
+    changelog = "https://github.com/firefly-cpp/FireflyAlgorithm/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ firefly-cpp ];
   };
