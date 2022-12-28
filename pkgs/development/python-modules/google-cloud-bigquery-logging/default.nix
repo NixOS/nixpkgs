da853fc1fd072b1930238d82a9eac4faaa7fb91c
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 13:07:27 +0100

    python310Packages.google-cloud-bigquery-logging: add changelog to meta

--- pkgs/development/python-modules/google-cloud-bigquery-logging/default.nix
+++ pkgs/development/python-modules/google-cloud-bigquery-logging/default.nix
@@ -42,6 +42,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Bigquery logging client library";
     homepage = "https://github.com/googleapis/python-bigquery-logging";
+    changelog = "https://github.com/googleapis/python-bigquery-logging/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
