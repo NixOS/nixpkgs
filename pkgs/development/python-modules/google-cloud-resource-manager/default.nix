e31c72e3558f017ebaddc2b8d802ccb3281e5046
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 16:38:04 +0100

    python310Packages.google-cloud-resource-manager: add changelog to meta

--- pkgs/development/python-modules/google-cloud-resource-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-resource-manager/default.nix
@@ -47,6 +47,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Google Cloud Resource Manager API client library";
     homepage = "https://github.com/googleapis/python-resource-manager";
+    changelog = "https://github.com/googleapis/python-resource-manager/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
