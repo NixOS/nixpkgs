defc63589b924d3d6caa9be70b0e33dbdbd5baf9
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 14:51:33 +0100

    python310Packages.google-cloud-compute: add changelog to meta

--- pkgs/development/python-modules/google-cloud-compute/default.nix
+++ pkgs/development/python-modules/google-cloud-compute/default.nix
@@ -13,11 +13,13 @@
 buildPythonPackage rec {
   pname = "google-cloud-compute";
   version = "1.5.2";
+  format = "setuptools";
+
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-D0pIR1vQEt/7aIxMo0uDlxvt+fwS2DxCurU/lxMHAjo=";
+    hash = "sha256-D0pIR1vQEt/7aIxMo0uDlxvt+fwS2DxCurU/lxMHAjo=";
   };
 
   propagatedBuildInputs = [
@@ -48,6 +50,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "API Client library for Google Cloud Compute";
     homepage = "https://github.com/googleapis/python-compute";
+    changelog = "https://github.com/googleapis/python-compute/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ jpetrucciani ];
   };
