e1f5ee21381dec50f082b9860d2a209b4f53cb25
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-28 13:22:48 +0100

    python310Packages.google-cloud-bigquery-datatransfer: add changelog to meta

--- pkgs/development/python-modules/google-cloud-bigquery-datatransfer/default.nix
+++ pkgs/development/python-modules/google-cloud-bigquery-datatransfer/default.nix
@@ -44,6 +44,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "BigQuery Data Transfer API client library";
     homepage = "https://github.com/googleapis/python-bigquery-datatransfer";
+    changelog = "https://github.com/googleapis/python-bigquery-datatransfer/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
