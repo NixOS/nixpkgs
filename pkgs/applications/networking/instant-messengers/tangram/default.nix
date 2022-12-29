5e58806ad0babe8e91c137509d3791d53e8b7dc0
Austin Butler <austinabutler@gmail.com>
2022-12-29 14:15:01 -0800

    tangram: 1.3.2 -> 1.4.2

--- pkgs/applications/networking/instant-messengers/tangram/default.nix
+++ pkgs/applications/networking/instant-messengers/tangram/default.nix
@@ -1,21 +1,36 @@
-{ stdenv, lib, fetchFromGitHub, appstream-glib, desktop-file-utils, gdk-pixbuf
-, gettext, gjs, glib, gobject-introspection, gsettings-desktop-schemas, gtk3
-, hicolor-icon-theme, meson, ninja, pkg-config, python3, webkitgtk, wrapGAppsHook
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  appstream-glib,
+  desktop-file-utils,
+  gdk-pixbuf,
+  gettext,
+  gjs,
+  glib,
+  glib-networking,
+  gobject-introspection,
+  gsettings-desktop-schemas,
+  gtk4,
+  hicolor-icon-theme,
+  meson,
+  ninja,
+  pkg-config,
+  python3,
+  webkitgtk,
+  wrapGAppsHook,
 }:
-
 stdenv.mkDerivation rec {
   pname = "tangram";
-  version = "1.3.2";
+  version = "1.4.2";
 
   src = fetchFromGitHub {
     owner = "sonnyp";
     repo = "Tangram";
     rev = "v${version}";
-    sha256 = "sha256-WI0H3bforQ6Jc/+TWFT1zUs4KRtWwvXY2/va+Fnd+iU=";
+    sha256 = "sha256-0KSmqDLp7+mmrzTlil8iXUdHmHUz91AoCcbc6YcHKbI=";
     fetchSubmodules = true;
   };
-
-  buildInputs = [ gdk-pixbuf gjs glib gsettings-desktop-schemas gtk3 webkitgtk ];
 
   nativeBuildInputs = [
     appstream-glib
@@ -30,12 +45,24 @@ stdenv.mkDerivation rec {
     wrapGAppsHook
   ];
 
+  buildInputs = [
+    gdk-pixbuf
+    gjs
+    glib
+    glib-networking
+    gsettings-desktop-schemas
+    gtk4
+    webkitgtk
+  ];
+
+  dontPatchShebangs = true;
   dontWrapGApps = true;
 
   # Fixes https://github.com/NixOS/nixpkgs/issues/31168
   postPatch = ''
     chmod +x build-aux/meson/postinstall.py
     patchShebangs build-aux/meson/postinstall.py
+    substituteInPlace src/re.sonny.Tangram.in --replace "#!/usr/bin/env -S gjs -m" "#!/usr/bin/env -S ${gjs}/bin/gjs -m"
   '';
 
   postFixup = ''
@@ -51,6 +78,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/sonnyp/Tangram";
     license = licenses.gpl3Only;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ austinbutler ];
+    maintainers = with maintainers; [austinbutler];
   };
 }
