ff734d226cbd09e08fda1cecc9d73c82ef55486b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-29 12:40:34 +0100

    python3Packages.flit-scm: Trim dependencies and refactor

--- pkgs/development/python-modules/flit-scm/default.nix
+++ pkgs/development/python-modules/flit-scm/default.nix
@@ -2,7 +2,6 @@
 , buildPythonPackage
 , fetchFromGitLab
 , pythonOlder
-, git
 , flit-core
 , setuptools-scm
 , tomli
@@ -11,19 +10,37 @@
 buildPythonPackage rec {
   pname = "flit-scm";
   version = "1.7.0";
-
   format = "pyproject";
 
   src = fetchFromGitLab {
     owner = "WillDaSilva";
     repo = "flit_scm";
-    rev = version;
-    sha256 = "sha256-K5sH+oHgX/ftvhkY+vIg6wUokAP96YxrTWds3tnEtyg=";
-    leaveDotGit = true;
+    rev = "refs/tags/${version}";
+    hash = "sha256-K5sH+oHgX/ftvhkY+vIg6wUokAP96YxrTWds3tnEtyg=";
   };
 
-  nativeBuildInputs = [ flit-core setuptools-scm tomli git ];
-  propagatedBuildInputs = [ flit-core setuptools-scm ] ++ lib.optionals (pythonOlder "3.11") [ tomli ];
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+  nativeBuildInputs = [
+    flit-core
+    setuptools-scm
+  ] ++ lib.optionals (pythonOlder "3.11") [
+    tomli
+  ];
+
+  propagatedBuildInputs = [
+    flit-core
+    setuptools-scm
+  ] ++ lib.optionals (pythonOlder "3.11") [
+    tomli
+  ];
+
+  pythonImportsCheck = [
+    "flit_scm"
+  ];
+
+
+  doCheck = false; # no tests
 
   meta = with lib; {
     description = "A PEP 518 build backend that uses setuptools_scm to generate a version file from your version control system, then flit to build the package.";
