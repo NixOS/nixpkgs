1d881e37b27e166603f2094585dcdbfa4a6914a1
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-29 21:34:52 +0100

    python3Packages.pytest-forked: fix tests

--- pkgs/development/python-modules/pytest-forked/default.nix
+++ pkgs/development/python-modules/pytest-forked/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
+, fetchpatch
 , setuptools-scm
 , py
 , pytest
@@ -19,6 +20,18 @@ buildPythonPackage rec {
     sha256 = "sha256-i2dYfI+Yy7rf3YBFOe1UVbbtA4AiA0hd0vU8FCLXRA4=";
   };
 
+  patches = [
+    # pytest 7.2.0 compat, remove after 1.4.0
+    (fetchpatch {
+      url = "https://github.com/pytest-dev/pytest-forked/commit/c3c753e96916a4bc5a8a37699e75c6cbbd653fa2.patch";
+      hash = "sha256-QPgxBeMQ0eKJyHXYZyBicVbE+JyKPV/Kbjsb8gNJBGA=";
+    })
+    (fetchpatch {
+      url = "https://github.com/pytest-dev/pytest-forked/commit/de584eda15df6db7912ab6197cfb9ff23024ef23.patch";
+      hash = "sha256-VLE32xZRwFK0nEgCWuSoMW/yyFHEURtNFU9Aa9haLhk=";
+    })
+  ];
+
   nativeBuildInputs = [ setuptools-scm ];
 
   buildInputs = [
@@ -29,7 +42,10 @@ buildPythonPackage rec {
     py
   ];
 
-  checkInputs = [ pytestCheckHook ];
+  checkInputs = [
+    py
+    pytestCheckHook
+  ];
 
   setupHook = ./setup-hook.sh;
 
