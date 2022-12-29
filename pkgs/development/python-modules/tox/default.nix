67eb2ef7c908d7abea63c0a9ce018be9b89ec4bb
Luiz Irber <contact+github@luizirber.org>
2022-12-29 11:29:20 -0800

    python3Packages.tox: 3.27.1 -> 4.0.16

--- pkgs/development/python-modules/tox/default.nix
+++ pkgs/development/python-modules/tox/default.nix
@@ -10,26 +10,53 @@
 , toml
 , tomli
 , filelock
+, hatchling
+, hatch-vcs
+, platformdirs
+, pyproject-api
+, colorama
+, chardet
+, cachetools
 , pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "tox";
-  version = "3.27.1";
+  version = "4.0.16";
+  format = "pyproject";
 
-  buildInputs = [ setuptools-scm ];
-  propagatedBuildInputs = [ packaging pluggy py six virtualenv filelock ]
-                          ++ lib.optional (pythonOlder "3.11") tomli;
+  buildInputs = [
+    hatchling
+    hatch-vcs
+  ];
+  propagatedBuildInputs = [
+    cachetools
+    chardet
+    colorama
+    filelock
+    packaging
+    platformdirs
+    pluggy
+    py
+    pyproject-api
+    six
+    toml
+    virtualenv
+  ]
+  ++ lib.optional
+    (pythonOlder "3.11")
+    tomli;
 
   doCheck = false;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-sqkg41pmjMBpQv/RzzpPsiGk2QnKchkftthLCxinvgQ=";
-  };
+  src = fetchPypi
+    {
+      inherit pname version;
+      hash = "sha256-lo/E4nEQ3v3xWXKJPLFf4WafM4yECNiDUHdGL7B+B/4=";
+    };
 
   meta = with lib; {
-    description = "Virtualenv-based automation of test activities";
+    description = "A generic virtualenv management and test command line tool";
     homepage = "https://tox.readthedocs.io/";
     license = licenses.mit;
   };
