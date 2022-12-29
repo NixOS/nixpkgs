2ce4af2f1cd721bfd8cfbb2c89b2df560dd6bf65
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-29 16:54:53 +0100

    python310Packages.tidyexc:  disable on unsupported Python releases

--- pkgs/development/python-modules/tidyexc/default.nix
+++ pkgs/development/python-modules/tidyexc/default.nix
@@ -1,19 +1,24 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "tidyexc";
   version = "0.10.0";
+  format = "flit";
+
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "1gl1jmihafawg7hvnn4xb20vd2x5qpvca0m1wr2gk0m2jj42yiq6";
   };
 
-  format = "flit";
-  pythonImportsCheck = [ "tidyexc" ];
+  pythonImportsCheck = [
+    "tidyexc"
+  ];
 
   meta = with lib; {
     description = "Raise rich, helpful exceptions";
