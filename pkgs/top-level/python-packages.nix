274d8b73b920f94b6a517ac779b215bcc46e0c41
Luiz Irber <contact+github@luizirber.org>
2022-12-29 11:27:40 -0800

    python3Packages.pyproject-api: init at 1.2.1
    Co-Authored-By: Martin Weinelt <hexa@darmstadt.ccc.de>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6943,6 +6943,8 @@ self: super: with self; {
 
   pyprecice = callPackage ../development/python-modules/pyprecice { };
 
+  pyproject-api = callPackage ../development/python-modules/pyproject-api { };
+
   pypsrp = callPackage ../development/python-modules/pypsrp { };
 
   phpserialize = callPackage ../development/python-modules/phpserialize { };
