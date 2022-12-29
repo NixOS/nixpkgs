c870b0d0435883adb6d45e15289134dc1e75beef
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-29 15:16:09 +0000

    python310Packages.plexapi: 4.13.1 -> 4.13.2

--- pkgs/development/python-modules/plexapi/default.nix
+++ pkgs/development/python-modules/plexapi/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "plexapi";
-  version = "4.13.1";
+  version = "4.13.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "pkkid";
     repo = "python-plexapi";
     rev = "refs/tags/${version}";
-    hash = "sha256-py3UmKie96FR8klf97zsYxUkrQisO7/cnWUgKxgB+tQ=";
+    hash = "sha256-5YwINPgQ4efZBvu5McsLYicW/7keKSi011lthJUR9zw=";
   };
 
   propagatedBuildInputs = [
