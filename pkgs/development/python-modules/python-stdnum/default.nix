662a8afa86dfed6225bf0f3b45168351db869bbc
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:42 +0100

    python3Packages.python-stdnum: 1.17 -> 1.18

--- pkgs/development/python-modules/python-stdnum/default.nix
+++ pkgs/development/python-modules/python-stdnum/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "python-stdnum";
-  version = "1.17";
+  version = "1.18";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-N04rXhORLM2/ULCyP8osPgUxF0gFwy104UXzd1Yyg0A=";
+    hash = "sha256-vMdj2cSa4j2l0remhtX9He7J2QUTQRYKENGscjomvsA=";
   };
 
   checkInputs = [
