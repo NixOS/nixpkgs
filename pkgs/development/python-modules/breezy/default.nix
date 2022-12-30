b0f0d34c1fd115c53bb88339dff4cff8213f5685
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:12:59 +0100

    python3Packages.breezy: 3.2.2 -> 3.3.1

--- pkgs/development/python-modules/breezy/default.nix
+++ pkgs/development/python-modules/breezy/default.nix
@@ -14,13 +14,13 @@
 
 buildPythonPackage rec {
   pname = "breezy";
-  version = "3.2.2";
+  version = "3.3.1";
 
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-GHpuRSCN0F2BdQc2cgyDcQz0gJT1R+xAgcVxJZVZpNU=";
+    sha256 = "sha256-8AwtF3ZvKUf23GkuEkflwXcakYb06UGql+zJ0qwjqKY=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
