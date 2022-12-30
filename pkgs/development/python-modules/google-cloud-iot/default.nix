ec61eae5fc5a9bca8b33b6de8427564bd040a86b
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:14 +0100

    python3Packages.google-cloud-iot: 2.6.4 -> 2.7.0

--- pkgs/development/python-modules/google-cloud-iot/default.nix
+++ pkgs/development/python-modules/google-cloud-iot/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-iot";
-  version = "2.6.4";
+  version = "2.7.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-d3RRVqgetOS3sAT6UkV5DB/NZMjeZd5tBtd9ME8Wvs4=";
+    hash = "sha256-vV5emuGcFwUctnPNRW1NSFej599Lc6rFSKAlaaCatZw=";
   };
 
   propagatedBuildInputs = [
