84d5b4dbf47722bfadcba5dfd966d7c5c82720de
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2022-12-29 22:14:30 -0500

    python310Packages.catboost: 1.0.5 -> 1.1.1, use ya.make
    Switching to ya allows to do darwin build and fixes the cython issue (which,
    despite what the comment says, is not limited to the withCuda=true).
    
    The resulting package doesn't build on darwin with sandbox, as ya-bin tool
    contains frozen python2 executable, which requires /usr/sbin/sysctl to detect
    number of CPUs in the multiprocessing module.

--- pkgs/development/python-modules/catboost/default.nix
+++ pkgs/development/python-modules/catboost/default.nix
@@ -1,44 +1,77 @@
-{ buildPythonPackage, fetchFromGitHub, lib, pythonOlder
-, clang_12, python
+{ lib
+, stdenv
+, buildPythonPackage
+, fetchFromGitHub
+, runCommandNoCC
+, clang_12
+, python
 , graphviz, matplotlib, numpy, pandas, plotly, scipy, six
-, withCuda ? false, cudatoolkit }:
+, withCuda ? false
+, cudatoolkit
+}:
 
 buildPythonPackage rec {
   pname = "catboost";
-  version = "1.0.5";
-
-  disabled = pythonOlder "3.4";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "catboost";
     repo = "catboost";
     rev = "v${version}";
-    sha256 = "ILemeZUBI9jPb9G6F7QX/T1HaVhQ+g6y7YmsT6DFCJk=";
+    hash = "sha256-bqnUHTTRan/spA5y4LRt/sIUYpP3pxzdN/4wHjzgZVY=";
   };
 
-  nativeBuildInputs = [ clang_12 ];
+  ya = runCommandNoCC "ya-bin" {
+    inherit src;
+    YA_CACHE_DIR="./";
+    outputHashMode = "flat";
+    outputHash = {
+      aarch64-darwin = "sha256-FbwQ0fLAxsfX/lyupvCj5myQN8oiEE4R+CJMR1GRkFU=";
+      x86_64-darwin = "sha256-2t3JNm+X+CHuD3ZDQUqgPgP54FzSS+TQ3iiiapjFctE=";
+      x86_64-linux = "sha256-7Nw6tyCTGf/Kvc+NtG4qFvvIEgg5BYbINrIXVUQnrB4=";
+    }.${stdenv.hostPlatform.system} or (throw "unsupported system ${stdenv.hostPlatform.system}");
+    nativeBuildInputs = [ python ];
+  } ''
+    python $src/ya
+    install -Dm444 tools/*/ya-bin $out
+  '';
+
+  nativeBuildInputs = [
+    clang_12
+  ];
 
   propagatedBuildInputs = [ graphviz matplotlib numpy pandas scipy plotly six ]
     ++ lib.optionals withCuda [ cudatoolkit ];
 
-  patches = [
-    ./nix-support.patch
-  ];
-
-  postPatch = ''
-    # substituteInPlace is too slow for these large files, and the target has lots of numbers in it that change often.
-    sed -e 's|\$(YMAKE_PYTHON3-.*)/python3|${python.interpreter}|' -i make/*.makefile
-  '';
-
   preBuild = ''
+    export YA_SOURCE_ROOT="$PWD"
+
     cd catboost/python-package
+
+    cp "$ya" ./ya-bin
+    chmod +x ./ya-bin
+    wrapProgram ./ya-bin \
+      --unset PYTHONPATH \
+      --set YA_CACHE_DIR "./"
+
+    substituteInPlace setup.py \
+      --replace "python, ya, " "'./ya-bin', '-v', " \
+      --replace "_catboost.dylib" "_catboost.so"
+
+    # ImportError: cannot import name '_catboost' from partially initialized module 'catboost'
+    export PYTHONPATH="$PYTHONPATH":
   '';
-  setupPyBuildFlags = [ "--with-ymake=no" ];
+
+  # recover PYTHONPATH to avoid failing in pythonCatchConflictsPhase
+  postInstall = ''
+    export PYTHONPATH="''${PYTHONPATH%%:}"
+  '';
+
   CUDA_ROOT = lib.optional withCuda cudatoolkit;
   enableParallelBuilding = true;
 
-  # Tests use custom "ya" tool, not yet supported.
-  dontUseSetuptoolsCheck = true;
+  # No documented way to run tests
+  doCheck = false;
   pythonImportsCheck = [ "catboost" ];
 
   meta = with lib; {
@@ -49,10 +82,8 @@ buildPythonPackage rec {
       learning tasks for Python, R, Java, C++. Supports computation on CPU and GPU.
     '';
     license = licenses.asl20;
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
     homepage = "https://catboost.ai";
-    maintainers = with maintainers; [ PlushBeaver ];
-    # _catboost.pyx.cpp:226822:19: error: use of undeclared identifier '_PyGen_Send'
-    broken = withCuda;
+    maintainers = with maintainers; [ PlushBeaver veprbl ];
   };
 }
