8beb641f46c348fb49b56a89a8f9b93c62739242
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:17 +0100

    python3Packages.hacking: 4.1.0 -> 5.0.0

--- pkgs/development/python-modules/hacking/default.nix
+++ pkgs/development/python-modules/hacking/default.nix
@@ -10,16 +10,15 @@
 
 buildPythonPackage rec {
   pname = "hacking";
-  version = "4.1.0";
+  version = "5.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0fg19rlcky3n1y1ri61xyjp7534yzf8r102z9dw3zqg93f4kj20m";
+    sha256 = "sha256-qzWyCK8/FHpvlZUnMxw4gK5BrCHMzra/1oqE9OtW4CY=";
   };
 
   postPatch = ''
-    substituteInPlace requirements.txt \
-      --replace "flake8<3.9.0,>=3.8.0" "flake8"
+    sed -i 's/flake8.*/flake8/' requirements.txt
     substituteInPlace hacking/checks/python23.py \
       --replace 'H236: class Foo(object):\n    __metaclass__ = \' 'Okay: class Foo(object):\n    __metaclass__ = \'
     substituteInPlace hacking/checks/except_checks.py \
