942ebe87b7b1816972a456137a8178d6d8fe1245
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:39 +0100

    python3Packages.pyswitchbee: 1.6.2 -> 1.7.16

--- pkgs/development/python-modules/pyswitchbee/default.nix
+++ pkgs/development/python-modules/pyswitchbee/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "pyswitchbee";
-  version = "1.6.2";
+  version = "1.7.16";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "jafar-atili";
     repo = "pySwitchbee";
     rev = "refs/tags/${version}";
-    hash = "sha256-MDutpeHI6MW1VwCRTPJK/y08ee4eidOHluMf6lPQfEk=";
+    hash = "sha256-n04N7wfCBJIbwEEHJ6I1Lt3rpZg46umGPawShlXDuZ8=";
   };
 
   postPatch = ''
