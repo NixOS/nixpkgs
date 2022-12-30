8e3eaa0954205a0b1aa463725d89f7aff2de9585
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:06 +0100

    python3Packages.django-anymail: 8.6 -> 9.0

--- pkgs/development/python-modules/django-anymail/default.nix
+++ pkgs/development/python-modules/django-anymail/default.nix
@@ -13,13 +13,13 @@
 
 buildPythonPackage rec {
   pname = "django-anymail";
-  version = "8.6";
+  version = "9.0";
 
   src = fetchFromGitHub {
     owner = "anymail";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-hLNILUV7qzqHfh7l3SJAoFveUIRSCHTjEQ3ZC3PhZUY=";
+    sha256 = "sha256-qEYBHsaHo1gmrsa6q7DQiUJurC7cXhv5e/SQ7R3Tkzc=";
   };
 
   propagatedBuildInputs = [
