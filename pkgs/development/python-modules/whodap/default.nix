59479a3b04d205f62c1ab74d5e69cfba287674a9
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-30 17:58:09 +0100

    python310Packages.whodap: add changelog to meta

--- pkgs/development/python-modules/whodap/default.nix
+++ pkgs/development/python-modules/whodap/default.nix
@@ -42,6 +42,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python RDAP utility for querying and parsing information about domain names";
     homepage = "https://github.com/pogzyb/whodap";
+    changelog = "https://github.com/pogzyb/whodap/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
