628e0654667d6047e3957f565a82c95f35d24c39
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:19 +0100

    python3Packages.ipykernel: 6.15.3 -> 6.20.0

--- pkgs/development/python-modules/ipykernel/default.nix
+++ pkgs/development/python-modules/ipykernel/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "ipykernel";
-  version = "6.15.3";
+  version = "6.20.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-uB1XsOFxZwhEvynNwRVisQENPahxFcRRPg7mYKg2h2U=";
+    sha256 = "sha256-ffr67GRTAHEMQUecCwvcE6nqZjm+5uOtBCdXZ06amik=";
   };
 
   # debugpy is optional, see https://github.com/ipython/ipykernel/pull/767
