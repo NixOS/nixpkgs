2f03339bf5682bd91c7a7a4a7d96814e05d24c5e
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 21:58:41 +0100

    fixup! python3Packages.automat: 20.2.0 -> 22.10.0

--- pkgs/development/python-modules/automat/default.nix
+++ pkgs/development/python-modules/automat/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, fetchpatch
 , attrs
 , pytest-benchmark
 , pytestCheckHook
@@ -16,18 +15,8 @@ let automat = buildPythonPackage rec {
   src = fetchPypi {
     pname = "Automat";
     inherit version;
-    sha256 = "sha256-5WvrhO2tGdzBHTDo2biV913ute9elrhKRnBms7hLsE4=";
+    hash = "sha256-5WvrhO2tGdzBHTDo2biV913ute9elrhKRnBms7hLsE4=";
   };
-
-  patches = [
-    # don't depend on m2r
-    (fetchpatch {
-      name = "dont-depend-on-m2r.patch";
-      url = "https://github.com/glyph/automat/compare/v20.2.0..2562fa4ddeba5b5945d9482baa4c26a414f5e831.patch";
-      includes = [ "setup.py" ];
-      hash = "sha256-jlPLJMu1QbBpiVYHDiqPydrXjEoZgYZTVVGNxSA0NxY=";
-    })
-  ];
 
   nativeBuildInputs = [
     setuptools-scm
