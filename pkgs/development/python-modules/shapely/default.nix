6426df7cc26bc3329c089185e53d28c9d30bc804
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:47 +0100

    python3Packages.Shapely: 1.8.4 -> 2.0.0

--- pkgs/development/python-modules/shapely/default.nix
+++ pkgs/development/python-modules/shapely/default.nix
@@ -11,13 +11,13 @@
 }:
 
 buildPythonPackage rec {
-  pname = "Shapely";
-  version = "1.8.4";
+  pname = "shapely";
+  version = "2.0.0";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-oZXlHKr6IYKR8suqP+9p/TNTyT7EtlsqRyLEz0DDGYw=";
+    sha256 = "sha256-EfGxIxpsBCE/sSJsaWjRsbOzaexC0ellUGavh2MYYOo=";
   };
 
   nativeBuildInputs = [
@@ -33,17 +33,8 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  # Environment variable used in shapely/_buildcfg.py
+  # Environment variable used in setup.py
   GEOS_LIBRARY_PATH = "${geos}/lib/libgeos_c${stdenv.hostPlatform.extensions.sharedLibrary}";
-
-  patches = [
-    # Patch to search form GOES .so/.dylib files in a Nix-aware way
-    (substituteAll {
-      src = ./library-paths.patch;
-      libgeos_c = GEOS_LIBRARY_PATH;
-      libc = lib.optionalString (!stdenv.isDarwin) "${stdenv.cc.libc}/lib/libc${stdenv.hostPlatform.extensions.sharedLibrary}.6";
-    })
- ];
 
   preCheck = ''
     rm -r shapely # prevent import of local shapely
