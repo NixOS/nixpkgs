28829fb8ae3685a3771e93ab79b6147452780a96
helmi.nour <helmiinour@gmail.com>
2022-12-30 23:24:27 +0100

    addressing PR comments

--- pkgs/servers/xteve/default.nix
+++ pkgs/servers/xteve/default.nix
@@ -1,28 +1,22 @@
 { lib, buildGoModule, fetchFromGitHub }:
 
-let
-  owner = "xteve-project";
-  repo  = "xTeVe";
+buildGoModule rec {
+  pname = "xteve";
   version = "2.2.0.200";
 
-in buildGoModule rec {
-  pname = "xteve";
-  inherit version;
-
   src = fetchFromGitHub {
-    inherit owner repo;
+    owner = "xteve-project";
+    repo = "xTeVe";
     rev = version;
-    sha256 = "sha256-hD4GudSkGZO41nR/CgcMg/SqKjpAO1yJDkfwa8AUges=";
+    hash = "sha256-hD4GudSkGZO41nR/CgcMg/SqKjpAO1yJDkfwa8AUges=";
   };
 
   vendorSha256 = "sha256-oPkSWpqNozfSFLIFsJ+e2pOL6CcR91YHbqibEVF2aSk=";
 
   meta = with lib; {
-    description = ''
-      M3U Proxy for Plex DVR and Emby Live TV.
-    '';
+    description = "M3U Proxy for Plex DVR and Emby Live TV";
     homepage = "https://github.com/xteve-project/xTeVe";
     license = licenses.mit;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ nrhelmi AndersonTorres ];
   };
 }
