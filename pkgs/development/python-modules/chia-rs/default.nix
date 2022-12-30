a1f7dcda9833dc36ca060ddf0658a0fddc53c69e
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:01 +0100

    python3Packages.chia-rs: 0.1.5 -> 0.1.16

--- pkgs/development/python-modules/chia-rs/default.nix
+++ pkgs/development/python-modules/chia-rs/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "chia-rs";
-  version = "0.1.5";
+  version = "0.1.16";
 
   src = fetchFromGitHub {
     owner = "chia-network";
     repo = "chia_rs";
-    rev = version;
-    sha256 = "sha256-4TIRj7FMIArI/EvDARReC4MqDG44zjn/MKoUHAVqq5s=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-WIt7yGceILzVhegluiSb7w3F9qQvI5DjulheGsJrcf8=";
   };
 
   cargoDeps = rustPlatform.importCargoLock {
