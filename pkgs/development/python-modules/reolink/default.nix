ad63942e46dfbf0f702d0425c2aab54ddeab0d12
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:44 +0100

    python3Packages.reolink: 0.61 -> 0.63
    https://github.com/fwestenberg/reolink/releases/tag/v0.62
    https://github.com/fwestenberg/reolink/releases/tag/v0.63

--- pkgs/development/python-modules/reolink/default.nix
+++ pkgs/development/python-modules/reolink/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "reolink";
-  version = "0.61";
+  version = "0.63";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "fwestenberg";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-XUYTDHh0oTro6BT+h4LjRdMukOZTlWP+giFpjLciZNQ=";
+    sha256 = "sha256-qwS7frXRk4gCBoAyBFLlbpYsyQZYqMLGB/pdznwyCoA=";
   };
 
   propagatedBuildInputs = [
