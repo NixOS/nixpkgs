5d3f022ef110fc5bc8ad74aedbab6cdaf2e2276f
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:33 +0100

    python3Packages.ppft: 1.7.6.5 -> 1.7.6.6

--- pkgs/development/python-modules/ppft/default.nix
+++ pkgs/development/python-modules/ppft/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "ppft";
-  version = "1.7.6.5";
+  version = "1.7.6.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-R+DauHpRbAuZks1bDJCDSOTH2WQwTRBrIn+tKK4DIZ4=";
+    sha256 = "sha256-+TPwQE8+gIvIYHRayzt5zU/jHqGaIIiaZF+QBBW+YPE=";
   };
 
   propagatedBuildInputs = [
