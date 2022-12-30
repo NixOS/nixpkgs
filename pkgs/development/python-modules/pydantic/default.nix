2e3314743b46b0851f85f4806718a9eedb22ac27
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:35 +0100

    python3Packages.pydantic: 1.9.2 -> 1.10.4

--- pkgs/development/python-modules/pydantic/default.nix
+++ pkgs/development/python-modules/pydantic/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , buildPythonPackage
+, autoflake
 , cython
 , devtools
 , email-validator
@@ -9,6 +10,7 @@
 , pytestCheckHook
 , python-dotenv
 , pythonOlder
+, pyupgrade
 , typing-extensions
 # dependencies for building documentation.
 # docs fail to build in Darwin sandbox: https://github.com/samuelcolvin/pydantic/issues/4245
@@ -27,7 +29,7 @@
 
 buildPythonPackage rec {
   pname = "pydantic";
-  version = "1.9.2";
+  version = "1.10.4";
 
   outputs = [
     "out"
@@ -41,7 +43,7 @@ buildPythonPackage rec {
     owner = "samuelcolvin";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-ZGFxyQ1qD3zZWTdfTeoGj3UcUwAzO8K0DySdVAsMHyI=";
+    sha256 = "sha256-BFyv1uVq2pLcJeS5955G/pDA3ce9YTqZ6F7kAkwnuvY=";
   };
 
   postPatch = ''
@@ -65,8 +67,10 @@ buildPythonPackage rec {
   ];
 
   propagatedBuildInputs = [
+    autoflake
     devtools
     email-validator
+    pyupgrade
     python-dotenv
     typing-extensions
   ];
@@ -74,6 +78,11 @@ buildPythonPackage rec {
   checkInputs = [
     pytest-mock
     pytestCheckHook
+  ];
+
+  pytestFlagsArray = [
+    # https://github.com/pydantic/pydantic/issues/4817
+    "-W" "ignore::pytest.PytestReturnNotNoneWarning"
   ];
 
   preCheck = ''
