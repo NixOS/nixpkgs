a1d49e998e2829886934de284bbe11d0fadffe0d
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:41 +0100

    python3Packages.python-levenshtein: 0.12.2 -> 0.20.9

--- pkgs/development/python-modules/python-levenshtein/default.nix
+++ pkgs/development/python-modules/python-levenshtein/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "python-levenshtein";
-  version = "0.12.2";
+  version = "0.20.9";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "dc2395fbd148a1ab31090dd113c366695934b9e85fe5a4b2a032745efd0346f6";
+    sha256 = "sha256-TFB7HibeKTdBU5gvpHfOp0Ht8JXYknczQ7SWG+rKyDQ=";
   };
 
   # No tests included in archive
