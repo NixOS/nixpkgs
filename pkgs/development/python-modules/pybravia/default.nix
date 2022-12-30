22257ed5bb5d9d9e8d553cda581fdef04da0041e
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-30 11:25:14 +0100

    python310Packages.pybravia: 0.2.3 -> 0.2.4
    Diff: https://github.com/Drafteed/pybravia/compare/v0.2.3...0.2.4
    
    Changelog: https://github.com/Drafteed/pybravia/releases/tag/v0.2.4

--- pkgs/development/python-modules/pybravia/default.nix
+++ pkgs/development/python-modules/pybravia/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "pybravia";
-  version = "0.2.3";
+  version = "0.2.4";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "Drafteed";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-ZM1XJnEYMNcbVOkpcI1ml7Cck2CXA2QXEpJMx3RwtSQ=";
+    hash = "sha256-dR9UfvgBnCTL+/rn3j9TvfCqD8exp2WziS/+zRyde70=";
   };
 
   nativeBuildInputs = [
