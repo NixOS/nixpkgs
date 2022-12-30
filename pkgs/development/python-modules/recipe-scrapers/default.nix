2a29b6c26c70cb0a70d5f95f7c5148e9f66ce9b0
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:44 +0100

    python3Packages.recipe-scrapers: 14.14.0 -> 14.26.0

--- pkgs/development/python-modules/recipe-scrapers/default.nix
+++ pkgs/development/python-modules/recipe-scrapers/default.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "recipe-scrapers";
-  version = "14.14.0";
+  version = "14.26.0";
 
   src = fetchFromGitHub {
     owner = "hhursev";
     repo = "recipe-scrapers";
-    rev = version;
-    sha256 = "sha256-3qrjNd1jX4JP3qG9YX8MQqwPh8cvfkZa1tEk0uCwego=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-U7A9HmkXPcuYEsY/uGUVh3LYHDYhV/uizELh1/vXG+U=";
   };
 
   propagatedBuildInputs = [
