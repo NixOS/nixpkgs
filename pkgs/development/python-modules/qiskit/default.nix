9dc3de45f3fa9281405462c4ba76c8191c9a1483
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:43 +0100

    python3Packages.qiskit: 0.37.0 -> 0.39.4

--- pkgs/development/python-modules/qiskit/default.nix
+++ pkgs/development/python-modules/qiskit/default.nix
@@ -28,7 +28,7 @@ in
 buildPythonPackage rec {
   pname = "qiskit";
   # NOTE: This version denotes a specific set of subpackages. See https://qiskit.org/documentation/release_notes.html#version-history
-  version = "0.37.0";
+  version = "0.39.4";
 
   disabled = pythonOlder "3.6";
 
@@ -36,7 +36,7 @@ buildPythonPackage rec {
     owner = "Qiskit";
     repo = "qiskit";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-TsDDiSWSjk2iXaxFjGXQxPFEPCR242dR26H0cpA6ZxY=";
+    sha256 = "sha256-4Lf+0Dq64J53qTEzr9gtuYwe1a+zIgHmgoKMTBEef8M=";
   };
 
   propagatedBuildInputs = [
