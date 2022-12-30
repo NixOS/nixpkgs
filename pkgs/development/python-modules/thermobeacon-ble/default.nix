c39d47b119dede3a7d38d67a549d9206f86a1eda
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:52 +0100

    python3Packages.thermobeacon-ble: 0.4.0 -> 0.6.0

--- pkgs/development/python-modules/thermobeacon-ble/default.nix
+++ pkgs/development/python-modules/thermobeacon-ble/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "thermobeacon-ble";
-  version = "0.4.0";
+  version = "0.6.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "bluetooth-devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-t4l5K52Shaw/J4thcP0ken/u77ZX7BsMEEA6ZUpcRKY=";
+    hash = "sha256-WjABxtZ5td25K9QCbLHisT+DMd2Cv/nljwYwxY2br3A=";
   };
 
   nativeBuildInputs = [
