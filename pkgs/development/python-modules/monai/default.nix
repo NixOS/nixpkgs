5f3e1fedee9b086acee7c70a9a0576a0777162dd
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:26 +0100

    python3Packages.monai: 1.0.0 -> 1.1.0

--- pkgs/development/python-modules/monai/default.nix
+++ pkgs/development/python-modules/monai/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "monai";
-  version = "1.0.0";
+  version = "1.1.0";
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "Project-MONAI";
     repo = "MONAI";
     rev = "refs/tags/${version}";
-    hash = "sha256-BnsgZpkXsY2l6inuzOkuvK4zJqYs2dj7jF5gKKiLnZM=";
+    hash = "sha256-XTjZhynIiFtFjJSW6rRAnpErZvf6QHkuK4e2L6l3naM=";
   };
 
   # Ninja is not detected by setuptools for some reason even though it's present:
