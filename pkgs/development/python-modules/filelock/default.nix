b95b8928083140eb4f7fd42076fe33fae17cf650
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 02:27:12 +0100

    python3Packages.filelock: 3.8.0 -> 3.9.0
    https://github.com/tox-dev/py-filelock/releases/tag/3.9.0

--- pkgs/development/python-modules/filelock/default.nix
+++ pkgs/development/python-modules/filelock/default.nix
@@ -1,25 +1,27 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, hatch-vcs
+, hatchling
 , pytestCheckHook
 , pythonOlder
-, setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "filelock";
-  version = "3.8.0";
+  version = "3.9.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-VUR8qmZvIZjFtrE6JtIITSb6WxFcANBlZkshJGgMTtw=";
+    hash = "sha256-ezGfJDQLUfVaK/ehKsB1WpsD5xgxHaxWeg9Pf6vS9d4=";
   };
 
   nativeBuildInputs = [
-    setuptools-scm
+    hatch-vcs
+    hatchling
   ];
 
   checkInputs = [
@@ -27,6 +29,7 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
+    changelog = "https://github.com/tox-dev/py-filelock/releases/tag/${version}";
     description = "A platform independent file lock for Python";
     homepage = "https://github.com/benediktschmitt/py-filelock";
     license = licenses.unlicense;
