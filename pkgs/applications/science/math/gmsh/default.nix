f338b00b5eb09551b643ff00e6a988968c6b38c7
Ben Gamari <ben@smart-cactus.org>
2022-12-30 09:18:27 -0500

    gmsh: 4.11 -> 4.11.1

--- pkgs/applications/science/math/gmsh/default.nix
+++ pkgs/applications/science/math/gmsh/default.nix
@@ -5,11 +5,11 @@ assert (!blas.isILP64) && (!lapack.isILP64);
 
 stdenv.mkDerivation rec {
   pname = "gmsh";
-  version = "4.11.0";
+  version = "4.11.1";
 
   src = fetchurl {
     url = "https://gmsh.info/src/gmsh-${version}-source.tgz";
-    sha256 = "sha256-PPLyRFXuCSUsmeZNTmRilW5o8P8fN7rKC3jICdbMVXo=";
+    sha256 = "sha256-xf4bfL1AOIioFJKfL9D11p4nYAIioYx4bbW3boAFs2U=";
   };
 
   buildInputs = [
