58f3ebe4bd708ba433df39cf01ac025c8f2794c8
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:12:58 +0100

    python3Packages.biliass: 1.3.5 -> 1.3.7

--- pkgs/development/python-modules/biliass/default.nix
+++ pkgs/development/python-modules/biliass/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "biliass";
-  version = "1.3.5";
+  version = "1.3.7";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-kgoQUX2l5YENEozcnfluwvcAO1ZSxlfHPVIa9ABW6IU=";
+    sha256 = "sha256-P7K3bt8MXD6HoJ7duQ9lG99yj0lVwn9SqEEC/TUudK4=";
   };
 
   propagatedBuildInputs = [ protobuf ];
