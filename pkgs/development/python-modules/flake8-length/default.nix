8baf8ac37b6a4def99c761fa91ed32e5c881af60
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:10 +0100

    python3Packages.flake8-length: 0.3.0 -> 0.3.1

--- pkgs/development/python-modules/flake8-length/default.nix
+++ pkgs/development/python-modules/flake8-length/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "flake8-length";
-  version = "0.3.0";
+  version = "0.3.1";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "6e3c068005b0b3b5c8345923fe3e9a107c980baa1354dd19d820018f87409427";
+    sha256 = "sha256-Dr1hTCU2G1STczXJsUPMGFYs1NpIAk1I95vxXsRTtRA=";
   };
 
   propagatedBuildInputs = [
