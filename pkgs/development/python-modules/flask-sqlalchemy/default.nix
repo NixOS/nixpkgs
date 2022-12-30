05e13b86bcd51a87ee793684156146dea48039e8
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:10 +0100

    python3Packages.Flask-SQLAlchemy: 2.5.1 -> 3.0.2

--- pkgs/development/python-modules/flask-sqlalchemy/default.nix
+++ pkgs/development/python-modules/flask-sqlalchemy/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "Flask-SQLAlchemy";
-  version = "2.5.1";
+  version = "3.0.2";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "2bda44b43e7cacb15d4e05ff3cc1f8bc97936cc464623424102bfc2c35e95912";
+    sha256 = "sha256-FhmfWz3ftp4N8vUq5Mdq7b/sgjRiNJ2rshobLgorZek=";
   };
 
   propagatedBuildInputs = [
