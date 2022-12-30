84a33115067c10842ba58075c9384abb3d3efa59
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:47 +0100

    python3Packages.spacy: 3.4.1 -> 3.4.4

--- pkgs/development/python-modules/spacy/default.nix
+++ pkgs/development/python-modules/spacy/default.nix
@@ -31,14 +31,14 @@
 
 buildPythonPackage rec {
   pname = "spacy";
-  version = "3.4.1";
+  version = "3.4.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-WcXPXTSKbA5kpZrFz+bNhdCOhmM3hwyV0exhYdUx5GM=";
+    hash = "sha256-5QDPLLXxhJRhp5KPomlwN1YGm9+3FVkGUkCvbQIIsIw=";
   };
 
   propagatedBuildInputs = [
