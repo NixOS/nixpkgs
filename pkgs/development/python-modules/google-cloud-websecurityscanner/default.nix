e418725873fb2968e93193d30054e5719a112455
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:16 +0100

    python3Packages.google-cloud-websecurityscanner: 1.9.2 -> 1.10.0

--- pkgs/development/python-modules/google-cloud-websecurityscanner/default.nix
+++ pkgs/development/python-modules/google-cloud-websecurityscanner/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-websecurityscanner";
-  version = "1.9.2";
+  version = "1.10.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-fUnoV5I9kMwC9zUdsHit6MbIYFV+3E+GT76vwEmzNNs=";
+    hash = "sha256-93ijizxgVFxXzj8IzxrgzzXTQKl7UH3egvL41HVk5is=";
   };
 
   propagatedBuildInputs = [
