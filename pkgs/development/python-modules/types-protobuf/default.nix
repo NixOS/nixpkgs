f90bbc0e19728ff916fba22c6b926cbf8cd5b6af
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:13:54 +0100

    python3Packages.types-protobuf: 3.20.4.2 -> 4.21.0.2

--- pkgs/development/python-modules/types-protobuf/default.nix
+++ pkgs/development/python-modules/types-protobuf/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "types-protobuf";
-  version = "3.20.4.2";
+  version = "4.21.0.2";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-/WWrhQL5oI4InyytJtUvqwTNVzIs+JbFcKszkcp2C64=";
+    sha256 = "sha256-ffSD00rT/LH6f/8Qc1YNWWyawfQZz6hRsiDJqTOGyZg=";
   };
 
   propagatedBuildInputs = [
