175ee9cae2a544c3bc9eeb3069b000fd3d146430
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-30 20:12:52 +0100

    python3Packages.aiolifx-themes: 0.4.0 -> 0.4.1

--- pkgs/development/python-modules/aiolifx-themes/default.nix
+++ pkgs/development/python-modules/aiolifx-themes/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "aiolifx-themes";
-  version = "0.4.0";
+  version = "0.4.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Djelibeybi";
     repo = "aiolifx-themes";
     rev = "refs/tags/v${version}";
-    hash = "sha256-U3hIKmgnafkuVLi1r42BBXDKVcSSvLaRnmOEANasEyY=";
+    hash = "sha256-ND+0jukCU3jB34Sf4qAZg/+pyXVCoIoMqOoBW7srCSQ=";
   };
 
   prePatch = ''
