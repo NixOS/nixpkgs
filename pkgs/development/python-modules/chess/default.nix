1c1197c2f759a058038b505c52b63e2041253384
Fabian Affolter <mail@fabian-affolter.ch>
2022-12-31 12:59:37 +0100

    python310Packages.chess: add changelog to meta

--- pkgs/development/python-modules/chess/default.nix
+++ pkgs/development/python-modules/chess/default.nix
@@ -8,6 +8,7 @@
 buildPythonPackage rec {
   pname = "chess";
   version = "1.9.4";
+  format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
@@ -15,19 +16,22 @@ buildPythonPackage rec {
     owner = "niklasf";
     repo = "python-${pname}";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-YBABB//53gwJIwrmKJh8W+05hTBhl+49vCYv9//4E+0=";
+    hash = "sha256-YBABB//53gwJIwrmKJh8W+05hTBhl+49vCYv9//4E+0=";
   };
 
-  pythonImportsCheck = [ "chess" ];
+  pythonImportsCheck = [
+    "chess"
+  ];
 
   checkPhase = ''
     ${python.interpreter} ./test.py -v
   '';
 
   meta = with lib; {
-    description = "A chess library for Python, with move generation, move validation, and support for common formats";
+    description = "A chess library with move generation, move validation, and support for common formats";
     homepage = "https://github.com/niklasf/python-chess";
-    maintainers = with maintainers; [ smancill ];
+    changelog = "https://github.com/niklasf/python-chess/blob/v${version}/CHANGELOG.rst";
     license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ smancill ];
   };
 }
