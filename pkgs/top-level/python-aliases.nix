1b1a48f4b2aff57aec3f8c69b2af4e473f1a6bbc
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-12-31 04:57:59 +0100

    python3Packages.python-levenshtein: drop
    Duplicate package, the correct name is just `levenshtein`.

--- pkgs/top-level/python-aliases.nix
+++ pkgs/top-level/python-aliases.nix
@@ -178,6 +178,7 @@ mapAliases ({
   python_magic = python-magic; # added 2022-05-07
   python_mimeparse = python-mimeparse; # added 2021-10-31
   python-language-server = throw "python-language-server is no longer maintained, use the python-lsp-server community fork instead."; # Added 2022-08-03
+  python-Levenshtein = levenshtein;
   python-subunit = subunit; # added 2021-09-10
   pytest_xdist = pytest-xdist; # added 2021-01-04
   python_simple_hipchat = python-simple-hipchat; # added 2021-07-21
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9166,8 +9166,6 @@ self: super: with self; {
 
   python-ldap-test = callPackage ../development/python-modules/python-ldap-test { };
 
-  python-Levenshtein = callPackage ../development/python-modules/python-levenshtein { };
-
   python-linux-procfs = callPackage ../development/python-modules/python-linux-procfs { };
 
   python-logstash = callPackage ../development/python-modules/python-logstash { };
