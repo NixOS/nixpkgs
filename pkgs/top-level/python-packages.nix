06ca784b25937242822aeacb3451d842f5f1c0fc
Ben Gamari <ben@smart-cactus.org>
2022-12-31 11:07:23 -0500

    vtk: Generate egg-info
    VTK's cmake build system fails to generate an `.egg-info` file, which
    various libraries in the Python package system (e.g. `pkg_resources`)
    would use to identify the package. Work around this by generating an
    `egg-info` file ourselves.
    
    This will allow us to drop a workaround in `mayavi`.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11905,7 +11905,7 @@ self: super: with self; {
   vt-py = callPackage ../development/python-modules/vt-py { };
 
   vtk = toPythonModule (pkgs.vtk_9.override {
-    pythonInterpreter = python;
+    inherit python;
     enablePython = true;
   });
 
