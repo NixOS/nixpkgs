983ae504c9bfd3a7a12cc63991c15e2825522ad8
R. Ryantm <ryantm-bot@ryantm.com>
2022-12-31 00:34:10 +0000

    dprint: 0.33.0 -> 0.34.1

--- pkgs/development/tools/dprint/default.nix
+++ pkgs/development/tools/dprint/default.nix
@@ -2,14 +2,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "dprint";
-  version = "0.33.0";
+  version = "0.34.1";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-7UgE8/FPEG3VUtQLdlvfaFx9N6iIci5eu+Z2HBo+7c0=";
+    sha256 = "sha256-sdRmBzP5H/engoa68w1hYlzMgIrHhRltKYsMg/TGTv0=";
   };
 
-  cargoSha256 = "sha256-/7YUbbuoN/qUIHk95ndaKjHZdJOPNTqZqOBvM2dHvoc=";
+  cargoSha256 = "sha256-nkRD1Qx+OgqTwc/mfVa08d790yj/K7BJO4dqu5qig8o=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ Security ];
 
