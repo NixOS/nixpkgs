00903be01097db9473da6cb23c27623291e2f961
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-01-01 04:04:55 +0100

    python3Packages.hassil: expose as top-level attribute

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7953,6 +7953,8 @@ with pkgs;
 
   haskell-language-server = callPackage ../development/tools/haskell/haskell-language-server/withWrapper.nix { };
 
+  hassil = with python3Packages; toPythonApplication hassil;
+
   hasmail = callPackage ../applications/networking/mailreaders/hasmail { };
 
   haste-client = callPackage ../tools/misc/haste-client { };
