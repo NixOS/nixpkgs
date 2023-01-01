6967daac4d817d7001ea5044bbf5383ab94c858a
figsoda <figsoda@pm.me>
2023-01-01 16:45:28 -0500

    ruff: 0.0.205 -> 0.0.206
    Diff: https://github.com/charliermarsh/ruff/compare/v0.0.205...v0.0.206
    
    Changelog: https://github.com/charliermarsh/ruff/releases/tag/v0.0.206

--- pkgs/development/tools/ruff/default.nix
+++ pkgs/development/tools/ruff/default.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ruff";
-  version = "0.0.205";
+  version = "0.0.206";
 
   src = fetchFromGitHub {
     owner = "charliermarsh";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-fs9AjYnsOV3tbmoXoIfK5OJbErfImQl01TvorS/R4zc=";
+    sha256 = "sha256-LX7T7psMaM8j4EZyd/cBmYIQ1Tcvh0BTJE3mvuY8CS8=";
   };
 
-  cargoSha256 = "sha256-YirtNOamGgjM84rZcZhOXfWj1WD4vvAJvi6nKx0lmTI=";
+  cargoSha256 = "sha256-Hw3VSmbRzanov+LxypGMek7a4GKu5pBfCj5xZSPh5es=";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreServices
