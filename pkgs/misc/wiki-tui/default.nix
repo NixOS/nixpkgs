3a68bc8b9e92f303691df830b371e3af2b2641b2
lom <legendofmiracles@protonmail.com>
2023-01-01 11:36:38 -0500

    wiki-tui: 0.6.0 -> 0.6.1

--- pkgs/misc/wiki-tui/default.nix
+++ pkgs/misc/wiki-tui/default.nix
@@ -2,23 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wiki-tui";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "Builditluc";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-sqkVi8w4QoktC1ZLUPHzfMUxIzCadcIj3dEYigz854U=";
+    sha256 = "sha256-WiyRBF3rWLpOZ8mxT89ImRL++Oq9+b88oSKjr4tzCGs=";
   };
 
   buildInputs = [ ncurses openssl ] ++ lib.optional stdenv.isDarwin Security;
 
   nativeBuildInputs = [ pkg-config ];
 
-  cargoSha256 = "sha256-xRj0bF5VymvFVB0tSBndWA+OHBIEY2/ovRIBdDoOHA4=";
-
-  # Tests fail with this error: `found argument --test-threads which was not expected`
-  doCheck = false;
+  cargoSha256 = "sha256-R9xxIDqkU7FeulpD7PUM6aHgA67PVgqxHKYtdrjdaUo=";
 
   meta = with lib; {
     description = "A simple and easy to use Wikipedia Text User Interface";
