1e45ad1ef8823e92392a79a9fe24b04fefbd235c
lom <legendofmiracles@protonmail.com>
2023-01-01 12:44:43 -0500

    hilbish: 1.2.0 -> 2.0.1

--- pkgs/shells/hilbish/default.nix
+++ pkgs/shells/hilbish/default.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "hilbish";
-  version = "1.2.0";
+  version = "2.0.1";
 
   src = fetchFromGitHub {
     owner = "Rosettea";
     repo = "Hilbish";
     rev = "v${version}";
-    sha256 = "sha256-fUGeQM+7FRGTRL3J4c2+c+NTwrM6d99F2ucKsou2kRk=";
+    hash = "sha256-5GPJVusF3/WbEE5VH45Wyxq40wbNxgjO8QI2cLMpdN0=";
     fetchSubmodules = true;
   };
 
   subPackages = [ "." ];
 
-  vendorSha256 = "sha256-j7YAt7+kUJXdd/9LaRZny3MxYdd+0n5G3AffeDMo5DY=";
+  vendorHash = "sha256-Kiy1JR3X++naY2XNLpnGujrNQt7qlL0zxv8E96cHmHo=";
 
   ldflags = [
     "-s"
@@ -28,7 +28,8 @@ buildGoModule rec {
     cp .hilbishrc.lua $out/share/hilbish/
     cp -r docs -t $out/share/hilbish/
     cp -r libs -t $out/share/hilbish/
-    cp -r prelude/ $out/share/hilbish/
+    cp -r emmyLuaDocs -t $out/share/hilbish/
+    cp -r nature $out/share/hilbish/
   '';
 
   meta = with lib; {
