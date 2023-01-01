8f171925b3a0edbdc6611eef50eb07ef991fc7a6
Artturin <Artturin@artturin.com>
2023-01-01 22:27:28 +0200

    makeSetupHook: deprecate deps argument

--- pkgs/build-support/trivial-builders.nix
+++ pkgs/build-support/trivial-builders.nix
@@ -639,8 +639,9 @@ rec {
         inherit depsTargetTargetPropagated;
         propagatedBuildInputs =
           # remove list conditionals before 23.11
-          lib.warnIf (!lib.isList deps) "deps argument to makeSetupHook must be a list. content: ${toString deps}"
-            propagatedBuildInputs ++ (if lib.isList deps then deps else [ deps ] );
+          lib.warnIf (!lib.isList deps) "'deps' argument to makeSetupHook must be a list. content of deps: ${toString deps}"
+            (lib.warnIf (deps != [ ]) "'deps' argument to makeSetupHook is deprecated and will be removed in release 23.11., Please use propagatedBuildInputs instead. content of deps: ${toString deps}"
+              propagatedBuildInputs ++ (if lib.isList deps then deps else [ deps ]));
         strictDeps = true;
         # TODO 2023-01, no backport: simplify to inherit passthru;
         passthru = passthru
