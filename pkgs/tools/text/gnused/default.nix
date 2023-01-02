0ce0096e8f6c01c554b072144aa30af1e74bfbb3
Adam Joseph <adam@westernsemico.com>
2023-01-02 01:06:49 -0800

    drop gnused gnulib.passthru.longdouble-redirect-patch
    This reverts commit d455973ece7f51907e86fa4e69947ae0eca6bfa1.

--- pkgs/tools/text/gnused/default.nix
+++ pkgs/tools/text/gnused/default.nix
@@ -1,5 +1,4 @@
-{ lib, stdenv, fetchurl, perl
-, gnulib }:
+{ lib, stdenv, fetchurl, perl }:
 
 stdenv.mkDerivation rec {
   pname = "gnused";
@@ -13,13 +12,6 @@ stdenv.mkDerivation rec {
   outputs = [ "out" "info" ];
 
   nativeBuildInputs = [ perl ];
-
-  patches = [
-    # this change to gnused's vendored copy of gnulib is already
-    # merged upstream; we can drop this patch on the next version bump
-    gnulib.passthru.longdouble-redirect-patch
-  ];
-
   preConfigure = "patchShebangs ./build-aux/help2man";
 
   # Prevents attempts of running 'help2man' on cross-built binaries.
