f6d5846ee01e99aa32d70f64e085599049f9b1c8
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-01-02 22:27:44 +0100

    protobuf3_21: 3.21.8 -> 3.21.12
    https://github.com/protocolbuffers/protobuf/blob/v21.12/CHANGES.txt

--- pkgs/development/libraries/protobuf/3.21.nix
+++ pkgs/development/libraries/protobuf/3.21.nix
@@ -1,6 +1,6 @@
 { callPackage, abseil-cpp, ... }:
 
 callPackage ./generic-v3-cmake.nix {
-  version = "3.21.8";
-  sha256 = "sha256-cSNHX18CvMmydpYWqfe6WWk9rGxIlFfY/85rfSyznU4=";
+  version = "3.21.12";
+  sha256 = "sha256-VZQEFHq17UsTH5CZZOcJBKiScGV2xPJ/e6gkkVliRCU=";
 }
