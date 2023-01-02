c27941ae27379bb71c407446071d42ec178a2356
hqurve <hqurve@outlook.com>
2023-01-01 20:19:03 -0400

    qgis, qgis-ltr: disable qtwebkit by default
    qtwebkit has been marked has having known vulnerabilities.
    
    Since qtlocation was propagated by qtwebkit, it now needs to be
    explicitly added.
    
    See also: https://github.com/archlinux/svntogit-community/commit/783d52696588264257eed9d32c9fd212d0fdeace

--- pkgs/applications/gis/qgis/unwrapped.nix
+++ pkgs/applications/gis/qgis/unwrapped.nix
@@ -29,11 +29,12 @@
 , qtkeychain
 , qt3d
 , qscintilla
+, qtlocation
 , qtserialport
 , qtxmlpatterns
 , withGrass ? true
 , grass
-, withWebKit ? true
+, withWebKit ? false
 , qtwebkit
 , pdal
 , zstd
@@ -111,6 +112,7 @@ in mkDerivation rec {
     qca-qt5
     qtkeychain
     qscintilla
+    qtlocation
     qtserialport
     qtxmlpatterns
     qt3d
