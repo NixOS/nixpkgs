a8d4cf149cda4f82145291cc9a6819a9baa3951e
Naïm Favier <n@monade.li>
2023-01-02 14:00:58 +0100

    doc: separate manpage URLs from the Pandoc filter
    Move the manpage-to-URL mapping to `doc/manpage-urls.json` so that we can
    reuse that file elsewhere, and generate the `link-manpages.lua` filter from
    that file.
    
    Also modify the Pandoc filter so that it doesn't wrap manpages that are
    already inside a link.
    
    Keeping a Lua filter is essential for speed: a Python filter would
    increase the runtime `md-to-db.sh` from ~20s to ~30s (but Python is not
    to blame; marshalling Pandoc types to and from JSON is a costly operation).
    Parsing in Lua seems tedious, so I went with the Nix way.

--- doc/default.nix
+++ doc/default.nix
@@ -36,4 +36,5 @@ in pkgs.stdenv.mkDerivation {
 
   # Environment variables
   PANDOC_LUA_FILTERS_DIR = "${pkgs.pandoc-lua-filters}/share/pandoc/filters";
+  PANDOC_LINK_MANPAGES_FILTER = import build-aux/pandoc-filters/link-manpages.nix { inherit pkgs; };
 }
