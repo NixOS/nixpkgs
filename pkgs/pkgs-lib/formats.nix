28a27cd874a18b5b5cc4a6c0feb00efad1733680
ckie <git-525ff67@ckie.dev>
2023-01-02 14:32:36 +0200

    pkgs-lib: add libconfig output validation

--- pkgs/pkgs-lib/formats.nix
+++ pkgs/pkgs-lib/formats.nix
@@ -433,6 +433,36 @@ rec {
           };
         in valueType;
       generate = name: value:
-        pkgs.writeText name (lib.generators.toLibconfig { } value);
+        let
+          output = pkgs.writeText name (lib.generators.toLibconfig { } value);
+          validator = pkgs.runCommandCC "libconfig-validator" { buildInputs = [ pkgs.libconfig ]; } ''
+          $CC -lconfig -x c - -o "$out" <<EOF
+//    Copyright (C) 2005-2023  Mark A Lindner, ckie
+//    SPDX-License-Identifier: LGPL-2.1-or-later
+#include <stdio.h>
+#include <libconfig.h>
+int main(int argc, char **argv)
+{
+  config_t cfg;
+  config_init(&cfg);
+  if (argc != 1)
+  {
+     fprintf(stderr, "USAGE: validator <path-to-validate>");
+  }
+  if(! config_read_file(&cfg, argv[1]))
+  {
+    fprintf(stderr, "[libconfig] %s:%d - %s\n", config_error_file(&cfg),
+            config_error_line(&cfg), config_error_text(&cfg));
+    config_destroy(&cfg);
+    return 1;
+  }
+  printf("[libconfig] validation ok\n");
+}
+EOF
+        '';
+        in pkgs.runCommandLocal "validated-${name}" {} ''
+          ${validator} ${output}
+          cp ${output} "$out"
+        '';
     };
 }
