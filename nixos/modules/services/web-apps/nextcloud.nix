4de95055b7956c26880fcbdceaf0eabeffed701c
pennae <github@quasiparticle.net>
2023-01-03 08:03:20 +0100

    nixos/nextcloud: convert manual chapter to MD

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -1146,5 +1146,7 @@ in {
     }
   ]);
 
+  # Don't edit the docbook xml directly, edit the md and generate it:
+  # `pandoc nextcloud.md -t docbook --top-level-division=chapter --extract-media=media -f markdown-smart --lua-filter ../../../../doc/build-aux/pandoc-filters/myst-reader/roles.lua --lua-filter ../../../../doc/build-aux/pandoc-filters/docbook-writer/rst-roles.lua > nextcloud.xml`
   meta.doc = ./nextcloud.xml;
 }
