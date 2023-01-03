078707524efb010b01f491bfdd672a3a53567bee
pennae <github@quasiparticle.net>
2023-01-03 03:27:03 +0100

    nixos/mjolnir: convert manual chapter to MD

--- nixos/modules/services/matrix/mjolnir.nix
+++ nixos/modules/services/matrix/mjolnir.nix
@@ -236,6 +236,8 @@ in
   };
 
   meta = {
+    # Don't edit the docbook xml directly, edit the md and generate it:
+    # `pandoc mjolnir.md -t docbook --top-level-division=chapter --extract-media=media -f markdown-smart --lua-filter ../../../../doc/build-aux/pandoc-filters/myst-reader/roles.lua --lua-filter ../../../../doc/build-aux/pandoc-filters/docbook-writer/rst-roles.lua > mjolnir.xml`
     doc = ./mjolnir.xml;
     maintainers = with maintainers; [ jojosch ];
   };
