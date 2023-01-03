1be48def9638ced0d6e4e44c9f805bcff1f7bd0d
pennae <github@quasiparticle.net>
2023-01-03 07:08:43 +0100

    nixos/grocy: convert manual chapter to MD

--- nixos/modules/services/web-apps/grocy.nix
+++ nixos/modules/services/web-apps/grocy.nix
@@ -167,6 +167,8 @@ in {
 
   meta = {
     maintainers = with maintainers; [ ma27 ];
+    # Don't edit the docbook xml directly, edit the md and generate it:
+    # `pandoc grocy.md -t docbook --top-level-division=chapter --extract-media=media -f markdown-smart --lua-filter ../../../../doc/build-aux/pandoc-filters/myst-reader/roles.lua --lua-filter ../../../../doc/build-aux/pandoc-filters/docbook-writer/rst-roles.lua > grocy.xml`
     doc = ./grocy.xml;
   };
 }
