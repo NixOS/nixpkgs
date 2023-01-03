5320b4cff8a7f454ac0c086804fd4316696be0ae
pennae <github@quasiparticle.net>
2023-01-03 03:11:49 +0100

    nixos/mailman: convert manual chapter to MD

--- nixos/modules/services/mail/mailman.nix
+++ nixos/modules/services/mail/mailman.nix
@@ -642,6 +642,8 @@ in {
 
   meta = {
     maintainers = with lib.maintainers; [ lheckemann qyliss ma27 ];
+    # Don't edit the docbook xml directly, edit the md and generate it:
+    # `pandoc mailman.md -t docbook --top-level-division=chapter --extract-media=media -f markdown-smart --lua-filter ../../../../doc/build-aux/pandoc-filters/myst-reader/roles.lua --lua-filter ../../../../doc/build-aux/pandoc-filters/docbook-writer/rst-roles.lua > mailman.xml`
     doc = ./mailman.xml;
   };
 
