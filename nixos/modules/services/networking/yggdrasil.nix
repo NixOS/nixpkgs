e4897cdf1605e8c30e79402b5bb054085d5f8a06
pennae <github@quasiparticle.net>
2023-01-03 06:32:09 +0100

    nixos/yggdrasil: convert manual chapter to MD

--- nixos/modules/services/networking/yggdrasil.nix
+++ nixos/modules/services/networking/yggdrasil.nix
@@ -193,6 +193,8 @@ in {
     environment.systemPackages = [ cfg.package ];
   });
   meta = {
+    # Don't edit the docbook xml directly, edit the md and generate it:
+    # `pandoc yggdrasil.md -t docbook --top-level-division=chapter --extract-media=media -f markdown-smart --lua-filter ../../../../doc/build-aux/pandoc-filters/myst-reader/roles.lua --lua-filter ../../../../doc/build-aux/pandoc-filters/docbook-writer/rst-roles.lua > yggdrasil.xml`
     doc = ./yggdrasil.xml;
     maintainers = with lib.maintainers; [ gazally ehmry ];
   };
