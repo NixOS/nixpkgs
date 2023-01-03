391ad246a8dae85a30274e5f20ab4dfe1a761ae2
Michael Lohmann <michael.lohmann@bevuta.com>
2023-01-03 20:16:50 +0100

    nixos-version: print error for missing revision to stderr

--- nixos/modules/installer/tools/nixos-version.sh
+++ nixos/modules/installer/tools/nixos-version.sh
@@ -8,7 +8,7 @@ case "$1" in
     ;;
   --hash|--revision)
     if ! [[ @revision@ =~ ^[0-9a-f]+$ ]]; then
-      echo "$0: Nixpkgs commit hash is unknown"
+      echo "$0: Nixpkgs commit hash is unknown" >&2
       exit 1
     fi
     echo "@revision@"
