d07fb5c5d1d113b4a8f1ab8adf78426254ffb5d9
pennae <github@quasiparticle.net>
2023-01-03 05:51:49 +0100

    nixos/prometheus/exporters: convert manual chapter to MD

--- nixos/modules/services/monitoring/prometheus/exporters.nix
+++ nixos/modules/services/monitoring/prometheus/exporters.nix
@@ -323,6 +323,8 @@ in
   );
 
   meta = {
+    # Don't edit the docbook xml directly, edit the md and generate it:
+    # `pandoc exporters.md -t docbook --top-level-division=chapter --extract-media=media -f markdown-smart --lua-filter ../../../../../doc/build-aux/pandoc-filters/myst-reader/roles.lua --lua-filter ../../../../../doc/build-aux/pandoc-filters/docbook-writer/rst-roles.lua > exporters.xml`
     doc = ./exporters.xml;
     maintainers = [ maintainers.willibutz ];
   };
