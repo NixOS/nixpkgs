9ef45285da36251ba252a5e859e7092774f834ed
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-04 08:50:11 +0100

    python310Packages.django-stubs: add changelog to meta

--- pkgs/development/python-modules/django-stubs/default.nix
+++ pkgs/development/python-modules/django-stubs/default.nix
@@ -8,15 +8,19 @@
 , types-pytz
 , types-pyyaml
 , typing-extensions
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "django-stubs";
   version = "1.13.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-vMYYujU9q8VA2YK52sHVoZIWUvj8KhNlPVRaV9XjzA8=";
+    hash = "sha256-vMYYujU9q8VA2YK52sHVoZIWUvj8KhNlPVRaV9XjzA8=";
   };
 
   propagatedBuildInputs = [
@@ -32,6 +36,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "PEP-484 stubs for Django";
     homepage = "https://github.com/typeddjango/django-stubs";
+    changelog = "https://github.com/typeddjango/django-stubs/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ elohmeier ];
   };
