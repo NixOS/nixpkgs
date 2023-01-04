66fb0339383622b7af10f9a02e23be3943fd8004
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-01-04 09:53:07 +0100

    htmlcxx: unbreak on aarch64-darwin

--- pkgs/development/libraries/htmlcxx/default.nix
+++ pkgs/development/libraries/htmlcxx/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, libiconv }:
+{ lib, stdenv, fetchurl, autoreconfHook, libiconv }:
 
 stdenv.mkDerivation rec {
   pname = "htmlcxx";
@@ -9,6 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-XTj5OM9N+aKYpTRq8nGV//q/759GD8KgIjPLz6j8dcg=";
   };
 
+  nativeBuildInputs = [ autoreconfHook ];
   buildInputs = [ libiconv ];
   patches = [
     ./ptrdiff.patch
@@ -20,7 +21,5 @@ stdenv.mkDerivation rec {
     description = "A simple non-validating css1 and html parser for C++";
     license = licenses.lgpl2;
     platforms = platforms.all;
-    # never built on aarch64-darwin since first introduction in nixpkgs
-    broken = stdenv.isDarwin && stdenv.isAarch64;
   };
 }
