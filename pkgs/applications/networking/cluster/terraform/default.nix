7c5d0aa410ddd36f58e36ef033c26ebf3f902a61
techknowlogick <techknowlogick@gitea.io>
2023-01-04 13:01:19 -0500

    terraform: 1.3.6 -> 1.3.7
    (cherry picked from commit 39e738746b9b13b61dcc7237f524efad63ee075e)

--- pkgs/applications/networking/cluster/terraform/default.nix
+++ pkgs/applications/networking/cluster/terraform/default.nix
@@ -168,8 +168,8 @@ rec {
   mkTerraform = attrs: pluggable (generic attrs);
 
   terraform_1 = mkTerraform {
-    version = "1.3.6";
-    sha256 = "sha256-aETsvcHoHSwqWCAdn9JPJLcX1Wi1umUghSjkq37OYDU=";
+    version = "1.3.7";
+    sha256 = "sha256-z49DXJ9oYObJQWHPeuKvQ6jJtAheYuy0+QmvZ74ZbTQ";
     vendorSha256 = "sha256-fviukVGBkbxFs2fJpEp/tFMymXex7NRQdcGIIA9W88k=";
     patches = [ ./provider-path-0_15.patch ];
     passthru = {
