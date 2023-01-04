79aaeb68cab1a55b638296338e4fb8e1ba040e2c
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-01-04 18:11:50 +0100

    bupstash: unbreak on x86_64-darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5731,7 +5731,7 @@ with pkgs;
 
   bup = callPackage ../tools/backup/bup { };
 
-  bupstash = callPackage ../tools/backup/bupstash { };
+  bupstash = darwin.apple_sdk_11_0.callPackage ../tools/backup/bupstash { };
 
   burp = callPackage ../tools/backup/burp { };
 
