3a838e232a7414bbbccbd4b1019841386d26bc12
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-05 21:25:18 +0100

    python310Packages.devolo-plc-api: 0.9.0 -> 1.0.0
    Changelog: https://github.com/2Fake/devolo_plc_api/releases/tag/v1.0.0

--- pkgs/development/python-modules/devolo-plc-api/default.nix
+++ pkgs/development/python-modules/devolo-plc-api/default.nix
@@ -15,8 +15,8 @@
 
 buildPythonPackage rec {
   pname = "devolo-plc-api";
-  version = "0.9.0";
-  format = "setuptools";
+  version = "1.0.0";
+  format = "pyproject";
 
   disabled = pythonOlder "3.8";
 
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "2Fake";
     repo = "devolo_plc_api";
     rev = "refs/tags/v${version}";
-    hash = "sha256-FBcDEEWgfV+OgHriSOZKWZPt0O89nDe2CsY3oqX/6zo=";
+    hash = "sha256-W5wOedxl3ES5xUt98zwRTk0X23MPuJ7J3QjVCjeDM+o=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
@@ -46,14 +46,12 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-
-
   pythonImportsCheck = [
     "devolo_plc_api"
   ];
 
   meta = with lib; {
-    description = "Python module to interact with Devolo PLC devices";
+    description = "Module to interact with Devolo PLC devices";
     homepage = "https://github.com/2Fake/devolo_plc_api";
     changelog = "https://github.com/2Fake/devolo_plc_api/releases/tag/v${version}";
     license = licenses.gpl3Only;
