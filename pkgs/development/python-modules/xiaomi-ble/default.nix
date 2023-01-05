796ab0acbb053e3f68050466334e5fb0a3a9f95d
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-05 22:20:16 +0100

    python310Packages.xiaomi-ble: 0.14.0 -> 0.15.0
    Changelog: https://github.com/Bluetooth-Devices/xiaomi-ble/releases/tag/v0.15.0

--- pkgs/development/python-modules/xiaomi-ble/default.nix
+++ pkgs/development/python-modules/xiaomi-ble/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "xiaomi-ble";
-  version = "0.14.0";
+  version = "0.15.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-erxeLxfrQah85W6USUpDxRu02ChFLjbFcLG/M4S5ADA=";
+    hash = "sha256-6j5hZ9NGmAFpbgYTyC4UuI4XkQwotZND1UMHnmJHCdo=";
   };
 
   nativeBuildInputs = [
