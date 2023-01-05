42e24f80076c3255604724071c47c6422cb07558
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-05 22:21:46 +0100

    python310Packages.skodaconnect: 1.3.0 -> 1.3.1
    Changelog: https://github.com/lendy007/skodaconnect/releases/tag/1.3.1

--- pkgs/development/python-modules/skodaconnect/default.nix
+++ pkgs/development/python-modules/skodaconnect/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "skodaconnect";
-  version = "1.3.0";
+  version = "1.3.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "lendy007";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-1x1TQNhKL3RgoeYSB8l607mHt0VrHwOU1CFemoaTCt8=";
+    hash = "sha256-iaA4mvt9R/OH3RS62b6tfKWEYITfYrGY8lP45mGlN/w=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
