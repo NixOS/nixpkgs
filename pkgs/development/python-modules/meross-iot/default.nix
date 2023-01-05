fd445f66786c8f7c99efe1e6b36d66fc6fb3921b
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-05 21:18:56 +0100

    python310Packages.meross-iot: 0.4.5.4 -> 0.4.5.6
    Changelog: https://github.com/albertogeniola/MerossIot/releases/tag/0.4.5.6

--- pkgs/development/python-modules/meross-iot/default.nix
+++ pkgs/development/python-modules/meross-iot/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "meross-iot";
-  version = "0.4.5.4";
+  version = "0.4.5.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "albertogeniola";
     repo = "MerossIot";
     rev = "refs/tags/${version}";
-    hash = "sha256-HPA3oeanFHx+g0nt/nsTzG3RTCdsjNSvDCXILR271P4=";
+    hash = "sha256-rCV2C+kseCU0AGqpjbF3bs+H2T8Xr6F3v1edQdHqZSY=";
   };
 
   propagatedBuildInputs = [
