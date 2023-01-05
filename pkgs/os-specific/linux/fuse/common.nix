f60686b6af231c21ba94451354577c924ab631ce
oxalica <oxalicc@pm.me>
2023-01-05 09:15:20 +0800

    fuse: split outputs and clean up
    `fuse` consists of the library and SUID binaries. They serve different
    scenarios. Packages depending on libfuse don't want to pull in binaries
    which shadow SUID ones like `fusermount` and cause troubles.
    
    With outputs splitted, we can use `fuse.dev` in development environment
    without risking shadowing SUID binaries.
    
    The `common` output for `/etc` is unnecessary for NixOS and should be
    excluded from installation by default.

--- pkgs/os-specific/linux/fuse/common.nix
+++ pkgs/os-specific/linux/fuse/common.nix
@@ -4,6 +4,7 @@
 , fusePackages, util-linux, gettext, shadow
 , meson, ninja, pkg-config
 , autoreconfHook
+, runtimeShell
 , python3Packages, which
 }:
 
@@ -43,34 +44,57 @@ in stdenv.mkDerivation rec {
     then [ meson ninja pkg-config ]
     else [ autoreconfHook gettext ];
 
-  outputs = [ "out" ] ++ lib.optional isFuse3 "common";
+  # The `common` output (`fuse-common` from top-level) is for `/etc` which shares
+  # between fuse2 and fuse3. From the documentation, `fuse3`'s common files are preferred,
+  # even for `fuse2`.
+  # https://github.com/libfuse/libfuse/blob/d372d3f80b5437e652ea501d8a4a917f7001b9d7/ChangeLog.rst#libfuse-300-2016-12-08
+  #
+  # It contains,
+  # - An init script `/etc/init.d/fuse3`.
+  #   This is unused for systemd-managed system.
+  #
+  # - A udev rules `/etc/udev/rules.d/99-fuse3.rules` to configure `/dev/fuse`.
+  #   This is included in systemd-udevd default rules since very early time, thus unneeded.
+  #   https://github.com/systemd/systemd/blob/0cf2dcf15402c60498165dbd3f14536766c05051/rules.d/50-udev-default.rules.in#L94
+  #
+  # - An example commented `/etc/fuse.conf`.
+  #   In NixOS, we control and generate it in `nixos/modules/programs/fuse.nix`.
+  #   Also the example actually enables nothing.
+  #
+  # Thus we really not need this output currently.
+  outputs = [ "bin" "out" "dev" "man" ] ++ lib.optional isFuse3 "common";
 
   mesonFlags = lib.optionals isFuse3 [
-    "-Dudevrulesdir=/udev/rules.d"
     "-Duseroot=false"
+    "-Dudevrulesdir=/udev/rules.d"
   ];
 
+  # Ensure that FUSE calls the setuid wrapper, not
+  # $out/bin/fusermount. It falls back to calling fusermount in
+  # $PATH, so it should also work on non-NixOS systems.
+  NIX_CFLAGS_COMPILE = ''-DFUSERMOUNT_DIR="/run/wrappers/bin"'';
+
   preConfigure = ''
-    export MOUNT_FUSE_PATH=$out/sbin
+    substituteInPlace lib/mount_util.c \
+      --replace "/bin/mount" "${lib.getBin util-linux}/bin/mount" \
+      --replace "/bin/umount" "${lib.getBin util-linux}/bin/umount"
+    substituteInPlace util/mount.fuse.c \
+      --replace "/bin/sh" "${runtimeShell}"
+  '' + lib.optionalString (!isFuse3) ''
+    export MOUNT_FUSE_PATH=$bin/bin
+
+    # Do not install these files for fuse2, see comment of `outputs`.
     export INIT_D_PATH=$TMPDIR/etc/init.d
-    export UDEV_RULES_PATH=$out/etc/udev/rules.d
+    export UDEV_RULES_PATH=$TMPDIR/etc/udev/rules.d
 
-    # Ensure that FUSE calls the setuid wrapper, not
-    # $out/bin/fusermount. It falls back to calling fusermount in
-    # $PATH, so it should also work on non-NixOS systems.
-    export NIX_CFLAGS_COMPILE="-DFUSERMOUNT_DIR=\"/run/wrappers/bin\""
-
-    substituteInPlace lib/mount_util.c --replace "/bin/" "${util-linux}/bin/"
-    '' + (if isFuse3 then ''
-      # The configure phase will delete these files (temporary workaround for
-      # ./fuse3-install_man.patch)
-      install -D -m444 doc/fusermount3.1 $out/share/man/man1/fusermount3.1
-      install -D -m444 doc/mount.fuse3.8 $out/share/man/man8/mount.fuse3.8
-    '' else ''
-      substituteInPlace util/mount.fuse.c --replace '"su"' '"${shadow.su}/bin/su"'
-      sed -e 's@CONFIG_RPATH=/usr/share/gettext/config.rpath@CONFIG_RPATH=${gettext}/share/gettext/config.rpath@' -i makeconf.sh
-      ./makeconf.sh
-    '');
+    # This is for `setuid=`, and needs root permission anyway.
+    # No need to use the SUID wrapper.
+    substituteInPlace util/mount.fuse.c \
+      --replace '"su"' '"${lib.getBin shadow.su}/bin/su"'
+    substituteInPlace makeconf.sh \
+      --replace 'CONFIG_RPATH=/usr/share/gettext/config.rpath' 'CONFIG_RPATH=${lib.getLib gettext}/share/gettext/config.rpath'
+    ./makeconf.sh
+  '';
 
   checkInputs = [ which ] ++ (with python3Packages; [ python pytest ]);
 
@@ -78,15 +102,15 @@ in stdenv.mkDerivation rec {
     python3 -m pytest test/
   '';
 
+  postInstall = lib.optionalString isFuse3 ''
+    mkdir $common
+    mv $out/etc $common
+  '';
+
   doCheck = false; # v2: no tests, v3: all tests get skipped in a sandbox
 
-  postFixup = "cd $out\n" + (if isFuse3 then ''
-    install -D -m444 etc/fuse.conf $common/etc/fuse.conf
-    install -D -m444 etc/udev/rules.d/99-fuse3.rules $common/etc/udev/rules.d/99-fuse.rules
-  '' else ''
-    cp ${fusePackages.fuse_3.common}/etc/fuse.conf etc/fuse.conf
-    cp ${fusePackages.fuse_3.common}/etc/udev/rules.d/99-fuse.rules etc/udev/rules.d/99-fuse.rules
-  '');
+  # Don't pull in SUID `fusermount{,3}` binaries into development environment.
+  propagatedBuildOutputs = [ "out" ];
 
   meta = with lib; {
     description = "Library that allows filesystems to be implemented in user space";
@@ -102,6 +126,7 @@ in stdenv.mkDerivation rec {
     changelog = "https://github.com/libfuse/libfuse/releases/tag/fuse-${version}";
     platforms = platforms.linux;
     license = with licenses; [ gpl2Only lgpl21Only ];
-    maintainers = [ maintainers.primeos ];
+    maintainers = with maintainers; [ primeos oxalica ];
+    outputsToInstall = [ "bin" ];
   };
 }
