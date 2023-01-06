144e3d12f388c6bb446b3c5e536e48b300e480a0
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-06 11:23:29 +0000

    mdbook-mermaid: 0.12.1 -> 0.12.6

--- pkgs/tools/text/mdbook-mermaid/default.nix
+++ pkgs/tools/text/mdbook-mermaid/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mdbook-mermaid";
-  version = "0.12.1";
+  version = "0.12.6";
 
   src = fetchFromGitHub {
     owner = "badboy";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-22JmV4cFfUTwiweQP0Em2VbqmTUui2yWnbhKagprQ4Q=";
+    hash = "sha256-1mSSnAfsg9AEnDAgINrSLIeu9O2vLqchZPSH12cjATk=";
   };
 
-  cargoHash = "sha256-tztzmfWOtqFGWERMWgBSAqvT+hKfhBWL9KW4awixXk0=";
+  cargoHash = "sha256-9PMBHVpf8bDuSIYKrIFZjmBE2icejPTML+hhZ4DLq/Y=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ CoreServices ];
 
