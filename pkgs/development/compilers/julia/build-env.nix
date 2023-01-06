c47be7836a645f38c85c5e76edf5a66451b523c4
Federico Beffa <beffa@fbengineering.ch>
2023-01-06 21:40:22 +0100

    julia: Make Julia's fontonfig find system files

--- pkgs/development/compilers/julia/build-env.nix
+++ pkgs/development/compilers/julia/build-env.nix
@@ -13,12 +13,20 @@ buildEnv {
 
     pathsToLink = ["/share/julia" ];
 
+    # FONTCONFIG_FILE is needed to make Julia's fontconfig artifact
+    # find the system fonts.
+    #
+    # On Wayland, without setting GDK_BACKEND to X11, we get
+    # "Gdk-CRITICAL" messages.  We may remove it in the future. The
+    # '*' means that if X11 is not available, it will try other
+    # backends.
     postBuild = ''
       makeWrapper ${julia}/bin/julia $out/bin/julia \
         --set JULIA_LOAD_PATH "$JULIA_LOAD_PATH:$out/share/julia/packages" \
-        --set JULIA_DEPOT_PATH "$JULIA_DEPOT_PATH:$out/share/julia"
+        --set JULIA_DEPOT_PATH "$JULIA_DEPOT_PATH:$out/share/julia" \
+        --set FONTCONFIG_FILE "/etc/fonts/fonts.conf" \
+        --set GDK_BACKEND "x11,*"
     '';
 
     passthru = { inherit julia; };
 }
-
