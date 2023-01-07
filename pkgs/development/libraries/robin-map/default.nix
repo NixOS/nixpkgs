2f9b6bef94f6834438b18d8158be8b439b8aeaa1
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-07 23:16:03 +0100

    robin-map: add changelog to meta

--- pkgs/development/libraries/robin-map/default.nix
+++ pkgs/development/libraries/robin-map/default.nix
@@ -1,4 +1,5 @@
-{ lib, stdenv
+{ lib
+, stdenv
 , fetchFromGitHub
 , cmake
 }:
@@ -10,15 +11,18 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "Tessil";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-axVMJHTnGW2c4kGcYhEEAvKbVKYA2oxiYfwjiz7xh6Q=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-axVMJHTnGW2c4kGcYhEEAvKbVKYA2oxiYfwjiz7xh6Q=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+  ];
 
   meta = with lib; {
-    homepage = "https://github.com/Tessil/robin-map";
     description = "C++ implementation of a fast hash map and hash set using robin hood hashing";
+    homepage = "https://github.com/Tessil/robin-map";
+    changelog = "https://github.com/Tessil/robin-map/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ goibhniu ];
     platforms = platforms.unix;
