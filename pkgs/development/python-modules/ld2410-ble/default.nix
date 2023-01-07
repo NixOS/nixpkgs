0d558f17b1c81794240ddecc21ba2d8b801e541b
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-07 12:54:11 +0100

    python310Packages.ld2410-ble: init at 0.1.1

---
+++ pkgs/development/python-modules/ld2410-ble/default.nix
@@ -0,0 +1,56 @@
+{ lib
+, async-timeout
+, bleak
+, bleak-retry-connector
+, buildPythonPackage
+, fetchFromGitHub
+, poetry-core
+, pytestCheckHook
+, pythonOlder
+}:
+
+buildPythonPackage rec {
+  pname = "ld2410-ble";
+  version = "0.1.1";
+  format = "pyproject";
+
+  disabled = pythonOlder "3.9";
+
+  src = fetchFromGitHub {
+    owner = "930913";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "sha256-D8Z3OXlXWsaN0Ck9gx//J9TCaQmirYDbaXK7aTpI7ns=";
+  };
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
+
+  propagatedBuildInputs = [
+    async-timeout
+    bleak
+    bleak-retry-connector
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace " --cov=ld2410_ble --cov-report=term-missing:skip-covered" ""
+  '';
+
+  pythonImportsCheck = [
+    "ld2410_ble"
+  ];
+
+  meta = with lib; {
+    description = "Library for the LD2410B modules from HiLinks";
+    homepage = "https://github.com/930913/ld2410-ble";
+    changelog = "https://github.com/930913/ld2410-ble/blob/v${version}/CHANGELOG.md";
+    license = with licenses; [ mit ];
+    maintainers = with maintainers; [ fab ];
+  };
+}
