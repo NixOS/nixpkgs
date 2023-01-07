31a4574093f9d19de4c7093bc81a86c0e6d579d7
Adam Joseph <adam@westernsemico.com>
2023-01-06 20:50:08 -0800

    qemacs: 0.3.3 -> 5.4.1c

--- pkgs/applications/editors/qemacs/default.nix
+++ pkgs/applications/editors/qemacs/default.nix
@@ -1,38 +1,37 @@
 { lib
 , stdenv
-, fetchurl
+, fetchFromGitHub
 , buildPackages
+, which
+, texi2html
 , enableX11 ? true
 , libX11, libXext, libXv, libpng
 }:
 
 stdenv.mkDerivation rec {
   pname = "qemacs";
-  version = "0.3.3";
+  version = "5.4.1c";
 
-  src = fetchurl {
-    url = "https://bellard.org/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "156z4wpj49i6j388yjird5qvrph7hz0grb4r44l4jf3q8imadyrg";
+  src = fetchFromGitHub {
+    owner = "qemacs";
+    repo = "qemacs";
+    rev = "216b3ff8b77ff138aec22045522d5601b7390e58";
+    hash = "sha256-ngVaZZdr/Ym9YswLqzUtDytC0K7L9mKgORopLghGH3k=";
   };
 
-  postPatch = lib.optionalString (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+  postPatch = ''
     substituteInPlace Makefile --replace \
-      './fbftoqe $(FONTS) > $@' \
-      '${buildPackages.qemacs}/bin/fbftoqe $(FONTS) > $@'
-  '' + ''
-    substituteInPlace Makefile --replace \
-      '$(HOST_CC) $(LDFLAGS) -o $@ html2png.o $(OBJS)' \
-      '$(CC) $(LDFLAGS) -o $@ html2png.o $(OBJS)'
-    substituteInPlace Makefile --replace \
-      'install -m 755 -s' \
-      "install -m 755 -s --strip-program=''${STRIP}"
+      '$(INSTALL) -m 755 -s' \
+      '$(INSTALL) -m 755 -s --strip-program=${stdenv.cc.targetPrefix}strip'
   '';
 
+  nativeBuildInputs = [ which texi2html ];
   buildInputs = lib.optionals enableX11 [ libpng libX11 libXext libXv ];
+
+  enableParallelBuilding = true;
 
   configureFlags = [
     "--cross-prefix=${stdenv.cc.targetPrefix}"
-    "--extra-cflags=-Ilibqhtml"
   ] ++ lib.optionals (!enableX11) [
     "--disable-x11"
   ];
@@ -45,10 +44,6 @@ stdenv.mkDerivation rec {
 
   preInstall = ''
     mkdir -p $out/bin $out/man
-  '';
-
-  postInstall = ''
-    install -Dt $out/bin fbftoqe
   '';
 
   meta = with lib; {
