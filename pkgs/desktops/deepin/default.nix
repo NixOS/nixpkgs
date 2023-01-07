69f53fac1f417c25444405ffe6f7de0a3e1ad9f4
rewine <luhongxu@deepin.org>
2023-01-07 17:26:03 +0800

    dwayland: init at 5.24.3-deepin.1.4

--- pkgs/desktops/deepin/default.nix
+++ pkgs/desktops/deepin/default.nix
@@ -12,6 +12,7 @@ let
     qt5platform-plugins = callPackage ./library/qt5platform-plugins { };
     qt5integration = callPackage ./library/qt5integration { };
     deepin-wayland-protocols = callPackage ./library/deepin-wayland-protocols { };
+    dwayland = callPackage ./library/dwayland { };
     dde-qt-dbus-factory = callPackage ./library/dde-qt-dbus-factory { };
     disomaster = callPackage ./library/disomaster { };
     docparser = callPackage ./library/docparser { };
