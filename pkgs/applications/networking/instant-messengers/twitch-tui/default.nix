2eb019b74fcb13baf86253b83d3512708d209023
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-07 23:13:10 +0100

    twitch-tui: add changelog to meta

--- pkgs/applications/networking/instant-messengers/twitch-tui/default.nix
+++ pkgs/applications/networking/instant-messengers/twitch-tui/default.nix
@@ -14,18 +14,26 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "Xithrius";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-4gEE2JCYNxPOV47w/wMRvYn5YJdgvlYl+fkk6qcXLr8=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-4gEE2JCYNxPOV47w/wMRvYn5YJdgvlYl+fkk6qcXLr8=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ Security ];
-
   cargoHash = "sha256-IYk01mueNZu791LPdkB79VaxsFXZbqEFDbpw1ckYTMo=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+  ] ++ lib.optionals stdenv.isDarwin [
+    Security
+  ];
 
   meta = with lib; {
     description = "Twitch chat in the terminal";
     homepage = "https://github.com/Xithrius/twitch-tui";
+    changelog = "https://github.com/Xithrius/twitch-tui/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = [ maintainers.taha ];
   };
