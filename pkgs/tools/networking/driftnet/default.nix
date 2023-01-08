4f21c44a98f81db7220abb4340926ca88da97555
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-08 10:24:22 +0100

    driftnet: 1.3.0 -> 1.4.0

--- pkgs/tools/networking/driftnet/default.nix
+++ pkgs/tools/networking/driftnet/default.nix
@@ -11,32 +11,21 @@
 , libpng
 , libuv
 , libwebsockets
+, libwebp
 , openssl
 , pkg-config
 }:
 
 stdenv.mkDerivation rec {
   pname = "driftnet";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchFromGitHub {
     owner = "deiv";
     repo = "driftnet";
     rev = "refs/tags/v${version}";
-    sha256 = "0kd22aqb25kf54jjv3ml8wy8xm7lmbf0xz1wfp31m08cbzsbizib";
+    hash = "sha256-szmezYnszlRanq8pMD0CIGA+zTYGSwSHcDaZ2Gx1KCA=";
   };
-
-  # https://github.com/deiv/driftnet/pull/33
-  # remove on version bump from 1.3.0
-  patches = [
-    (fetchpatch {
-      name = "fix-darwin-build";
-      url = "https://github.com/deiv/driftnet/pull/33/commits/bef5f3509ab5710161e9e21ea960a997eada534f.patch";
-      sha256 = "1b7p9fkgp7dxv965l7q7y632s80h3nnrkaqnak2h0hakwv0i4pvm";
-    })
-    # https://github.com/deiv/driftnet/issues/37
-    ./libwebsockets-4.3.0.patch
-  ];
 
   enableParallelBuilding = true;
 
@@ -55,6 +44,7 @@ stdenv.mkDerivation rec {
     libpng
     libuv
     libwebsockets
+    libwebp
     openssl
   ];
 
