3d870cd8af53203e5dfe89d6b18c04a783073f00
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-08 09:56:31 +0100

    python310Packages.pydmd: add changelog to meta

--- pkgs/development/python-modules/pydmd/default.nix
+++ pkgs/development/python-modules/pydmd/default.nix
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "mathLab";
     repo = "PyDMD";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-0ss7yyT6u0if+YjBYNbKtx5beJU43JC1LD9rqHPKBS8=";
+    hash = "sha256-0ss7yyT6u0if+YjBYNbKtx5beJU43JC1LD9rqHPKBS8=";
   };
 
   propagatedBuildInputs = [
@@ -50,6 +50,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python Dynamic Mode Decomposition";
     homepage = "https://mathlab.github.io/PyDMD/";
+    changelog = "https://github.com/mathLab/PyDMD/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ yl3dy ];
     broken = stdenv.hostPlatform.isAarch64;
