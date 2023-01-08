8bb2773a1edeaf44bc9fee889fb2a17bf4eb9ca7
Ben Booth <benwbooth@gmail.com>
2023-01-08 15:51:03 -0800

    opelens: init at 6.3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30360,6 +30360,8 @@ with pkgs;
 
   lens = callPackage ../applications/networking/cluster/lens { };
 
+  openlens = callPackage ../applications/networking/cluster/openlens { };
+
   leo-editor = libsForQt5.callPackage ../applications/editors/leo-editor { };
 
   libowfat = callPackage ../development/libraries/libowfat { };
