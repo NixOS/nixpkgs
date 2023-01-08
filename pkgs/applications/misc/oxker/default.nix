406986499f8d5e4423219342fad55a3727d16d0c
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-08 11:36:19 +0000

    oxker: 0.1.10 -> 0.1.11

--- pkgs/applications/misc/oxker/default.nix
+++ pkgs/applications/misc/oxker/default.nix
@@ -2,14 +2,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "oxker";
-  version = "0.1.10";
+  version = "0.1.11";
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-2NX2iW3cT9027j2gUsDTtdIFDmJKIGPfSzrGGwvK/VA=";
+    sha256 = "sha256-O4fVEYstDkVHn7fBVOGu1ok9K9xiO9uLx0+vb6qMZoA=";
   };
 
-  cargoSha256 = "sha256-//GI+roOsCLkKgMDUDK0YhJWmeIaYCMBt9r14+Rz8UQ=";
+  cargoHash = "sha256-LSMAE24E8Is/ejUE/2vogP0GmpF+9oO2pJoQOZ8OfU8=";
 
   meta = with lib; {
     description = "A simple tui to view & control docker containers";
