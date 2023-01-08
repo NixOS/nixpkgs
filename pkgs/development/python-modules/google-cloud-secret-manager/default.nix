4b2700e73ae6410ceafb92e3d5df65e8b797b9ef
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-08 14:43:00 +0000

    python310Packages.google-cloud-secret-manager: 2.13.0 -> 2.14.0

--- pkgs/development/python-modules/google-cloud-secret-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-secret-manager/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-secret-manager";
-  version = "2.13.0";
+  version = "2.14.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-1BBOEdkh0jVBHtSVEnOsXtaCPF/GuXB90FCZeOmue9I=";
+    hash = "sha256-x0Ktu4o1I+eEJXUjMjowwxW7967DdjZWAfcs331qP7s=";
   };
 
   propagatedBuildInputs = [
