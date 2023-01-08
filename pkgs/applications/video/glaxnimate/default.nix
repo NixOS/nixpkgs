92cbb320902ed6953a2fb9d001c88670711d9c3e
Tobias Bora <tobias.bora@gmail.com>
2023-01-08 11:49:23 +0100

    Glaxnimate: init at 0.5.1

---
+++ pkgs/applications/video/glaxnimate/default.nix
@@ -0,0 +1,51 @@
+{ lib
+, stdenv
+, fetchFromGitLab
+, cmake
+, zlib
+, potrace
+, ffmpeg
+, libarchive
+, python3
+, qtbase
+, qttools
+, wrapQtAppsHook
+  # Python3
+}:
+
+stdenv.mkDerivation rec {
+  pname = "glaxnimate";
+  version = "0.5.1";
+
+  src = fetchFromGitLab {
+    owner = "mattbas";
+    repo = "${pname}";
+    rev = "${version}";
+    sha256 = "G4ykcOvXXnVIQZUYpRIrALtDSsGqxMvDtcmobjjtlKw=";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [
+    cmake
+    wrapQtAppsHook
+  ];
+  
+  buildInputs = [
+    zlib
+    potrace
+    ffmpeg
+    # gst_all_1.gst-libav # Note that since gst-libav-1.6, libav is actually ffmpeg
+    libarchive
+    python3
+    qtbase
+    qttools
+  ];
+
+  meta = with lib; {
+    homepage = "https://gitlab.com/mattbas/glaxnimate";
+    description = "Simple vector animation program.";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ tobiasBora ];
+    platforms = platforms.linux;
+  };
+}
