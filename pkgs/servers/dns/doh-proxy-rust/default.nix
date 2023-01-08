208154b7eda4214e261723701c852d595738e187
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-08 16:55:30 +0000

    doh-proxy-rust: 0.9.4 -> 0.9.6

--- pkgs/servers/dns/doh-proxy-rust/default.nix
+++ pkgs/servers/dns/doh-proxy-rust/default.nix
@@ -2,15 +2,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "doh-proxy-rust";
-  version = "0.9.4";
+  version = "0.9.6";
 
   src = fetchCrate {
     inherit version;
     crateName = "doh-proxy";
-    sha256 = "sha256-IuLNgyPiAPYu440jMtpXxEuQDIn9TUMjnD7y8WB+Ujs=";
+    sha256 = "sha256-7eKqCiafzmwk0suH8GviRVBmmvhBd5/R4aF9cSvSyNU=";
   };
 
-  cargoSha256 = "sha256-qrLhRNaGG7n9UPtkqNkJvnf+w9P0iLQ7MkIxnWYqYLM=";
+  cargoHash = "sha256-+IlVjordlMgf8srXtQVLMXRbYs+4htDP+NToVXxPqR4=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ Security libiconv ];
 
