1d06489f0075fc1ccfca69db18decf219628b27b
figsoda <figsoda@pm.me>
2023-01-08 18:26:41 -0500

    cargo-hakari: 0.9.17 -> 0.9.18
    Diff: https://github.com/guppy-rs/guppy/compare/cargo-hakari-0.9.17...cargo-hakari-0.9.18
    
    Changelog: https://github.com/guppy-rs/guppy/blob/cargo-hakari-0.9.18/tools/cargo-hakari/CHANGELOG.md

--- pkgs/development/tools/rust/cargo-hakari/default.nix
+++ pkgs/development/tools/rust/cargo-hakari/default.nix
@@ -1,15 +1,20 @@
-{ lib, rustPlatform, fetchCrate }:
+{ lib, rustPlatform, fetchFromGitHub }:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-hakari";
-  version = "0.9.17";
+  version = "0.9.18";
 
-  src = fetchCrate {
-    inherit pname version;
-    sha256 = "sha256-FgG9sdXZhSlX4p3I9WL5ORN7FCg4Zgt/Y+GRCWhIoP8=";
+  src = fetchFromGitHub {
+    owner = "guppy-rs";
+    repo = "guppy";
+    rev = "cargo-hakari-${version}";
+    sha256 = "sha256-ZjU+0e0GmLCC9cha1RcTV0vEbuRi/SmU9qaNONIB7nI=";
   };
 
-  cargoHash = "sha256-pYjjiQUnBfZ9wQgXhm4c+A7zMAF9k/Mzl5ccPX407/A=";
+  cargoHash = "sha256-/KH+h+4xfU2fitWv+IrLkwgiQ8tVjm41DiZtbrerRZ4=";
+
+  cargoBuildFlags = [ "-p" "cargo-hakari" ];
+  cargoTestFlags = [ "-p" "cargo-hakari" ];
 
   meta = with lib; {
     description = "Manage workspace-hack packages to speed up builds in large workspaces.";
