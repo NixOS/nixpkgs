8434a50deea516d44f0e704170ecc292da6a76a5
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-09 22:34:27 +0100

    python310Packages.hstspreload: 2022.12.1 -> 2023.1.1
    Diff: https://github.com/sethmlarson/hstspreload/compare/2022.12.1...2023.1.1

--- pkgs/development/python-modules/hstspreload/default.nix
+++ pkgs/development/python-modules/hstspreload/default.nix
@@ -6,7 +6,7 @@
 
 buildPythonPackage rec {
   pname = "hstspreload";
-  version = "2022.12.1";
+  version = "2023.1.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
     owner = "sethmlarson";
     repo = pname;
     rev = version;
-    sha256 = "sha256-W1WJWG5R7Sucdw6TzsGFs5mH6BoUfA8URTgWlnRXa14=";
+    sha256 = "sha256-MF+pRP0KluF7LrSkfxs6ZSEXyqmr51mUqUn01dLdUdQ=";
   };
 
   # Tests require network connection
