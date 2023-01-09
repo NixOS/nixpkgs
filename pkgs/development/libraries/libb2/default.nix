69faaa7799faf200f94adaef5d15166bc20e9399
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-01-09 04:48:49 +0100

    libb2: Use autoreconfHook and build in parallel

--- pkgs/development/libraries/libb2/default.nix
+++ pkgs/development/libraries/libb2/default.nix
@@ -1,4 +1,10 @@
-{ lib, stdenv, fetchFromGitHub, autoconf, automake, libtool, pkg-config }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoreconfHook
+, libtool
+, pkg-config
+}:
 
 stdenv.mkDerivation rec {
   pname = "libb2";
@@ -7,18 +13,19 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "BLAKE2";
     repo = "libb2";
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     sha256 = "0qj8aaqvfcavj1vj5asm4pqm03ap7q8x4c2fy83cqggvky0frgya";
   };
 
-  preConfigure = ''
-    patchShebangs autogen.sh
-    ./autogen.sh
-  '';
+  nativeBuildInputs = [
+    autoreconfHook
+    libtool
+    pkg-config
+  ];
 
   configureFlags = lib.optional stdenv.hostPlatform.isx86 "--enable-fat=yes";
 
-  nativeBuildInputs = [ autoconf automake libtool pkg-config ];
+  enableParallelBuilding = true;
 
   doCheck = true;
 
