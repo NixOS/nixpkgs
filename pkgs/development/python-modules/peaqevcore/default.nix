f269050b8460e69dec2c7f8cc13bb2bf008e1e24
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-09 10:52:44 +0000

    python310Packages.peaqevcore: 10.1.4 -> 10.2.6

--- pkgs/development/python-modules/peaqevcore/default.nix
+++ pkgs/development/python-modules/peaqevcore/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "peaqevcore";
-  version = "10.1.4";
+  version = "10.2.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-h3kNmWa9ZOpI0DG49H3n1MPZu753nrdzSIh8V5N3H6I=";
+    hash = "sha256-uQPz7IB14dFrupZIA87jG5P7USaGKs0WWlgbaLAEPp4=";
   };
 
   postPatch = ''
