63210fb7d01bdbcb3183b4fc6cab9f709aadd241
Jordan Isaacs <mail@jdisaacs.com>
2023-01-09 05:30:12 -0800

    waylock: 0.4.2 -> 0.6.0
    Also adds `-Dcpu=baseline` to fix: https://github.com/NixOS/nixpkgs/issues/185665

--- pkgs/applications/misc/waylock/default.nix
+++ pkgs/applications/misc/waylock/default.nix
@@ -12,13 +12,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "waylock";
-  version = "0.4.2";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "ifreund";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-yWjWcnGa4a+Dpc82H65yr8H7v88g/tDq0FSguubhbEI=";
+    hash = "sha256-AujBvDy10e5HhezCQcXpBUVlktRKNseLxRKdI+gtH6w=";
     fetchSubmodules = true;
   };
 
@@ -38,7 +38,7 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     runHook preInstall
-    zig build -Drelease-safe -Dman-pages --prefix $out install
+    zig build -Drelease-safe -Dman-pages -Dcpu=baseline --prefix $out install
     runHook postInstall
   '';
 
