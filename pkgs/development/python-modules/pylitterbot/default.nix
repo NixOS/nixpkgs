7efd848031cfef36e52f35cf92a4c31fb86525f7
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-09 10:41:10 +0100

    python310Packages.pylitterbot: 2022.12.0 -> 2023.1.0
    Diff: https://github.com/natekspencer/pylitterbot/compare/refs/tags/2022.12.0...2023.1.0
    
    Changelog: https://github.com/natekspencer/pylitterbot/releases/tag/2023.1.0

--- pkgs/development/python-modules/pylitterbot/default.nix
+++ pkgs/development/python-modules/pylitterbot/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "pylitterbot";
-  version = "2022.12.0";
+  version = "2023.1.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "natekspencer";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-uqiSNqM1HKNAipIKKsUHv9mPfdk01ZrNWMXIzhgxxjU=";
+    hash = "sha256-XUiDZW3r8I8Vyx30UhopQ6S0HpSi0VNgvyu2RMNt2Eg=";
   };
 
   nativeBuildInputs = [
