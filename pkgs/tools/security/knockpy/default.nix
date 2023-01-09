956a398e46fb92d8b451b222a71630ab750f2191
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-09 21:34:28 +0100

    knockpy: add changelog to meta

--- pkgs/tools/security/knockpy/default.nix
+++ pkgs/tools/security/knockpy/default.nix
@@ -33,6 +33,7 @@ python3.pkgs.buildPythonApplication rec {
   meta = with lib; {
     description = "Tool to scan subdomains";
     homepage = "https://github.com/guelfoweb/knock";
+    changelog = "https://github.com/guelfoweb/knock/releases/tag/${version}";
     license = with licenses; [ gpl3Only ];
     maintainers = with maintainers; [ fab ];
   };
