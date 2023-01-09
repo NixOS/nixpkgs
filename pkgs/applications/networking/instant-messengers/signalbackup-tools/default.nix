52426daa46b81488bfd204911f027b96cd710ae5
Malo Bourgon <mbourgon@gmail.com>
2023-01-09 11:20:05 -0800

    signalbackup-tools: 20221227-1 -> 20230109-1
    Diff: https://github.com/bepaald/signalbackup-tools/compare/20221227-1...20230109-1

--- pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
+++ pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "signalbackup-tools";
-  version = "20221227-1";
+  version = "20230109-1";
 
   src = fetchFromGitHub {
     owner = "bepaald";
     repo = pname;
     rev = version;
-    sha256 = "sha256-yOOKgB7MO9LW6qkr/JZOYtteQTW/Yms4CMAg4EIJGc8=";
+    sha256 = "sha256-wPx1W0eaQHpA/jcZ+A7YFL5FwPqz12j/jPgxa1JeEM8=";
   };
 
   postPatch = ''
