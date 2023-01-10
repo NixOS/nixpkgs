9c9c1bc9a91da132c9fb3bfbc9c49d821f8eb75c
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-10 14:43:31 +0100

    python3Packages.django-import-export: add changelog to meta

--- pkgs/development/python-modules/django-import-export/default.nix
+++ pkgs/development/python-modules/django-import-export/default.nix
@@ -1,10 +1,11 @@
 { lib
-, fetchFromGitHub
 , buildPythonPackage
-, python
 , chardet
-, django
 , diff-match-patch
+, django
+, fetchFromGitHub
+, python
+, pythonOlder
 , pytz
 , tablib
 }:
@@ -12,18 +13,27 @@
 buildPythonPackage rec {
   pname = "django-import-export";
   version = "2.8.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "django-import-export";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-km6TQq4OZZtx9/lgBzS3tEifAYjkkUX//9FRATDLX/0=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-km6TQq4OZZtx9/lgBzS3tEifAYjkkUX//9FRATDLX/0=";
   };
 
-  propagatedBuildInputs = [ diff-match-patch django tablib ]
-    ++ (with tablib.optional-dependencies; html ++ ods ++ xls ++ xlsx ++ yaml);
+  propagatedBuildInputs = [
+    diff-match-patch
+    django
+    tablib
+  ] ++ (with tablib.optional-dependencies; html ++ ods ++ xls ++ xlsx ++ yaml);
 
-  checkInputs = [ chardet pytz ];
+  checkInputs = [
+    chardet
+    pytz
+  ];
 
   checkPhase = ''
     runHook preCheck
@@ -31,11 +41,14 @@ buildPythonPackage rec {
     runHook postCheck
   '';
 
-  pythonImportsCheck = [ "import_export" ];
+  pythonImportsCheck = [
+    "import_export"
+  ];
 
   meta = with lib; {
     description = "Django application and library for importing and exporting data with admin integration";
     homepage = "https://github.com/django-import-export/django-import-export";
+    changelog = "https://github.com/django-import-export/django-import-export/blob/${version}/docs/changelog.rst";
     license = licenses.bsd2;
     maintainers = with maintainers; [ sephi ];
   };
