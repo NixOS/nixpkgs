bdf4d9dc659da701bf81762289cd1685a6b7d8ee
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-10 21:30:09 +0100

    python310Packages.angr: 9.2.32 -> 9.2.33

--- pkgs/development/python-modules/angr/default.nix
+++ pkgs/development/python-modules/angr/default.nix
@@ -31,7 +31,7 @@
 
 buildPythonPackage rec {
   pname = "angr";
-  version = "9.2.32";
+  version = "9.2.33";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -40,7 +40,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-nyFEyOo4qtkyjAmkOV0JEOKAKWJKwjRrywKqP2yoJ54=";
+    hash = "sha256-Uq/weFiqbfGLqYlu5hH/l++YglqCGpL2NtNccI3gOqQ=";
   };
 
   propagatedBuildInputs = [
