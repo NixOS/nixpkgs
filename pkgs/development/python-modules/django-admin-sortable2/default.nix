a00907ef34aa36ff3f52cad9c2c68eb24285734a
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-10 14:18:47 +0100

    python3Packages.django-admin-sortable2: add changelog to meta

--- pkgs/development/python-modules/django-admin-sortable2/default.nix
+++ pkgs/development/python-modules/django-admin-sortable2/default.nix
@@ -1,17 +1,29 @@
-{ lib, buildPythonPackage, django_4, fetchPypi }:
+{ lib
+, buildPythonPackage
+, django_4
+, fetchPypi
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "django-admin-sortable2";
   version = "2.1.4";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit version pname;
     sha256 = "sha256-fcCP8PvOWFlQL4CdEIwElfOw2UYJJp6u27EjfgMISss=";
   };
 
-  propagatedBuildInputs = [ django_4 ];
+  propagatedBuildInputs = [
+    django_4
+  ];
 
-  pythonImportsCheck = [ "adminsortable2" ];
+  pythonImportsCheck = [
+    "adminsortable2"
+  ];
 
   # Tests are very slow (end-to-end with playwright)
   doCheck = false;
@@ -19,6 +31,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Generic drag-and-drop ordering for objects in the Django admin interface";
     homepage = "https://github.com/jrief/django-admin-sortable2";
+    changelog = "https://github.com/jrief/django-admin-sortable2/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ sephi ];
   };
