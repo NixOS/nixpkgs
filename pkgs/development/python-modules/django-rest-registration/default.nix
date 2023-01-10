3f3607724fbe3adbcc7a4142f3224bef73357eed
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-10 14:12:20 +0100

    python3Packages.django-rest-registration: add changelog to meta

--- pkgs/development/python-modules/django-rest-registration/default.nix
+++ pkgs/development/python-modules/django-rest-registration/default.nix
@@ -1,11 +1,11 @@
 { lib
-, fetchFromGitHub
 , buildPythonPackage
-, python
-, pytestCheckHook
 , django
 , djangorestframework
+, fetchFromGitHub
 , pytest-django
+, pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
@@ -13,18 +13,28 @@ buildPythonPackage rec {
   version = "0.7.3";
   format = "setuptools";
 
+  disabled = pythonOlder "3.7";
+
   src = fetchFromGitHub {
     owner = "apragacz";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-JoIeVjl5s60ilq9kU28Jo+GaYRKU61hoqy1GzYmMdZQ=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-JoIeVjl5s60ilq9kU28Jo+GaYRKU61hoqy1GzYmMdZQ=";
   };
 
-  propagatedBuildInputs = [ django djangorestframework ];
+  propagatedBuildInputs = [
+    django
+    djangorestframework
+  ];
 
-  checkInputs = [ pytestCheckHook pytest-django ];
+  checkInputs = [
+    pytestCheckHook
+    pytest-django
+  ];
 
-  pythonImportsCheck = [ "rest_registration" ];
+  pythonImportsCheck = [
+    "rest_registration"
+  ];
 
   disabledTests = [
     # This test fails on Python 3.10
@@ -36,6 +46,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "User-related REST API based on the awesome Django REST Framework";
     homepage = "https://github.com/apragacz/django-rest-registration/";
+    changelog = "https://github.com/apragacz/django-rest-registration/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ sephi ];
   };
