7c6de8f60aee4133d243ce9c96c26e8099b8c988
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-10 05:10:24 +0000

    rtsp-simple-server: 0.21.0 -> 0.21.1

--- pkgs/servers/rtsp-simple-server/default.nix
+++ pkgs/servers/rtsp-simple-server/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "rtsp-simple-server";
-  version = "0.21.0";
+  version = "0.21.1";
 
   src = fetchFromGitHub {
     owner = "aler9";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-iLNdl6V+px/ri9uJzOrxktxJYqAJA5JWBd18ZHSLQjQ=";
+    hash = "sha256-hYO/MMZBb9YczvIq4mn1XE9rhnaTg9t35by/GHtvdJQ=";
   };
 
-  vendorSha256 = "sha256-48i0hsAho4dI79a/i24GlKAaC/yNGKt0uA+qCy5QTok=";
+  vendorHash = "sha256-Q1zeKGu20kOpjqdLCB7stuWBqi/2EyWK7vMRAYUCJKg=";
 
   # Tests need docker
   doCheck = false;
