2a5d54522cf23c680509f84333c71462e8ab79a6
Ben Darwin <bcdarwin@gmail.com>
2023-01-10 16:20:30 -0500

    python310Packages.pymc: 5.0.1 -> 5.0.2; unbreak

--- pkgs/development/python-modules/pymc/default.nix
+++ pkgs/development/python-modules/pymc/default.nix
@@ -1,6 +1,4 @@
 { lib
-, aeppl
-, aesara
 , arviz
 , buildPythonPackage
 , cachetools
@@ -8,38 +6,33 @@
 , fastprogress
 , fetchFromGitHub
 , numpy
+, pytensor
 , pythonOlder
-, pythonRelaxDepsHook
 , scipy
 , typing-extensions
 }:
 
 buildPythonPackage rec {
   pname = "pymc";
-  version = "5.0.1";
+  version = "5.0.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "pymc-devs";
-    repo = "pymc";
+    repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-uWvzWbZyRRE8L9X9+azmN+1JYahwwNSYCk2fQ/C8Yi0=";
+    hash = "sha256-ffNWSSzoRLFmYzN9sm5Z1j6WtEoFzGkCQxpBC0NlpRc=";
   };
 
-  nativeBuildInputs = [
-    pythonRelaxDepsHook
-  ];
-
   propagatedBuildInputs = [
-    aeppl
-    aesara
     arviz
     cachetools
     cloudpickle
     fastprogress
     numpy
+    pytensor
     scipy
     typing-extensions
   ];
@@ -48,11 +41,6 @@ buildPythonPackage rec {
     substituteInPlace setup.py \
       --replace ', "pytest-cov"' ""
   '';
-
-  pythonRelaxDeps = [
-    "aesara"
-    "aeppl"
-  ];
 
   # The test suite is computationally intensive and test failures are not
   # indicative for package usability hence tests are disabled by default.
