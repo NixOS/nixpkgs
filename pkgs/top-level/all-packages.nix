268feffcaac9f5fa37d4be7d64d43d32e2088170
LoveIsGrief <loveisgrief@tuta.io>
2023-01-10 23:13:19 +0100

    simple-dlna-browser: Add new package
    A useful CLI script I found that's way light than VLC and other GUI apps since SSDP
     is pretty much a text-based protocol built on top of HTTP.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1455,6 +1455,8 @@ with pkgs;
 
   sgrep = callPackage ../tools/text/sgrep { };
 
+  simple-dlna-browser = callPackage ../tools/networking/simple-dlna-browser { };
+
   sorted-grep = callPackage ../tools/text/sorted-grep { };
 
   smbscan = callPackage ../tools/security/smbscan { };
