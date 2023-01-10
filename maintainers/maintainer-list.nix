22d196641678903ce90cc1f4138bc167b65f32df
WhiteBlackGoose <wbg@angouri.org>
2023-01-10 13:43:59 +0300

    maintainers: Add WhiteBlackGoose as a maintainer

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16304,6 +16304,15 @@
       fingerprint = "69C9 876B 5797 1B2E 11C5  7C39 80A1 F76F C9F9 54AE";
     }];
   };
+  WhiteBlackGoose = {
+    email = "wbg@angouri.org";
+    github = "WhiteBlackGoose";
+    githubId = 31178401;
+    name = "WhiteBlackGoose";
+    keys = [{
+      fingerprint = "640B EDDE 9734 310A BFA3  B257 52ED AE6A 3995 AFAB";
+    }];
+  };
   wuyoli = {
     name = "wuyoli";
     email = "wuyoli@tilde.team";
