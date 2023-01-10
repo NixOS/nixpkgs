30957b299ab8b771e62dc906e6f8fab5283045c9
Matthias Beyer <mail@beyermatthias.de>
2023-01-10 09:01:35 +0100

    cargo-llvm-cov: 0.5.4 -> 0.5.5
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/development/tools/rust/cargo-llvm-cov/default.nix
+++ pkgs/development/tools/rust/cargo-llvm-cov/default.nix
@@ -6,13 +6,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-llvm-cov";
-  version = "0.5.4";
+  version = "0.5.5";
 
   src = fetchzip {
     url = "https://crates.io/api/v1/crates/${pname}/${version}/download#${pname}-${version}.tar.gz";
-    sha256 = "sha256-xBWp9+Vv1Ho63WWlvbvLU+h0dh5CpUamlRQtmX6rOeY=";
+    sha256 = "sha256-L+byatz2Pq30+Gw0lq1/7Q1FJXi6z+J/nhySFBAfoMo=";
   };
-  cargoSha256 = "sha256-GKrqB6BtRbhtOlD2YeEMh6aqblPNEJjkR+tAsSpOFig=";
+  cargoSha256 = "sha256-/8kza5fW23UYlebchYtgo2GU8M7LXNXSKV2dQ6xZllw=";
 
   # skip tests which require llvm-tools-preview
   checkFlags = [
