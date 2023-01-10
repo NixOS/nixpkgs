f2af8bd5327073754a956edbb4a175a450ce907f
Antti Keränen <detegr@rbx.email>
2023-01-10 21:04:11 +0200

    jfrog-cli: init at 2.32.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38484,4 +38484,6 @@ with pkgs;
   alsa-scarlett-gui = callPackage ../applications/audio/alsa-scarlett-gui { };
 
   tuner = callPackage ../applications/audio/tuner { };
+
+  jfrog-cli = callPackage ../tools/misc/jfrog-cli { };
 }
