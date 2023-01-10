342848bb592289e71aa30c97b0f2e2da229a5344
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-10 14:22:49 +0100

    python3Packages.pytest-playwright: add changelog to meta

--- pkgs/development/python-modules/pytest-playwright/default.nix
+++ pkgs/development/python-modules/pytest-playwright/default.nix
@@ -8,29 +8,38 @@
 , python-slugify
 , pythonOlder
 , setuptools-scm
-, python
 , django
 }:
 
 buildPythonPackage rec {
   pname = "pytest-playwright";
   version = "0.3.0";
+  format = "setuptools";
+
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "playwright-pytest";
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     hash = "sha256-fHzQxbQBSEkCFu/ualjzSmIt3SiEa2ktTvIJKPZLT9Q=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
-  nativeBuildInputs = [ setuptools-scm ];
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
 
-  buildInputs = [ pytest ];
+  buildInputs = [
+    pytest
+  ];
 
-  propagatedBuildInputs = [ playwright pytest-base-url python-slugify ];
+  propagatedBuildInputs = [
+    playwright
+    pytest-base-url
+    python-slugify
+  ];
 
   # Most of the tests rely on network access, or on unavailable browsers such as
   # msedge, chrome or webkit
@@ -40,11 +49,14 @@ buildPythonPackage rec {
     export PLAYWRIGHT_BROWSERS_PATH=${playwright.browsers}
   '';
 
-  pythonImportsCheck = [ "pytest_playwright" ];
+  pythonImportsCheck = [
+    "pytest_playwright"
+  ];
 
   meta = with lib; {
     description = "Pytest plugin to write end-to-end browser tests with Playwright";
     homepage = "https://github.com/microsoft/playwright-pytest";
+    changelog = "https://github.com/microsoft/playwright-pytest/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ sephi ];
   };
