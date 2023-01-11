3da1b80873ce6ef94c12573205fdc8af98c963b9
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-11 21:40:09 +0100

    python310Packages.aioaladdinconnect: 0.1.50 -> 0.1.52
    Changelog: https://github.com/mkmer/AIOAladdinConnect/releases/tag/0.1.52

--- pkgs/development/python-modules/aioaladdinconnect/default.nix
+++ pkgs/development/python-modules/aioaladdinconnect/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "aioaladdinconnect";
-  version = "0.1.50";
+  version = "0.1.52";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "AIOAladdinConnect";
     inherit version;
-    hash = "sha256-5IeqIEIQzPq5GkuAf/92J0SBI3Lu1ftJ4UOj4oQLQC4=";
+    hash = "sha256-lpEpdc2izNP1xSnGsG1WMvrRqRToNrLpZn225XnZ+pI=";
   };
 
   propagatedBuildInputs = [
