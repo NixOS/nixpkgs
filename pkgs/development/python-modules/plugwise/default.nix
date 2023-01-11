c17f9eac643d795ca80d8f0bb303754c55238437
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-11 12:00:32 +0100

    python310Packages.plugwise: 0.27.1 -> 0.27.2
    Diff: https://github.com/plugwise/python-plugwise/compare/refs/tags/v0.27.1...v0.27.2
    
    Changelog: https://github.com/plugwise/python-plugwise/releases/tag/v0.27.2

--- pkgs/development/python-modules/plugwise/default.nix
+++ pkgs/development/python-modules/plugwise/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "plugwise";
-  version = "0.27.1";
+  version = "0.27.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = "python-plugwise";
     rev = "refs/tags/v${version}";
-    hash = "sha256-56KJQBYaQK08o9W6ZLWHCLCVPCLHh5Q+GTZqTTexn90=";
+    hash = "sha256-X2nmOZT8JlPOWySV45wnnR4BEANa+pxBI5kzVQpQPvg=";
   };
 
   propagatedBuildInputs = [
