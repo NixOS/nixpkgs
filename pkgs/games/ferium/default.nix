66fac9b1cf4ac50a5578bc484410bf1e7e8d2898
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-12 21:26:09 +0000

    ferium: 4.2.2 -> 4.3.3

--- pkgs/games/ferium/default.nix
+++ pkgs/games/ferium/default.nix
@@ -2,18 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ferium";
-  version = "4.2.2";
+  version = "4.3.3";
 
   src = fetchFromGitHub {
     owner = "gorilla-devs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-CtqnPHTFkfHE8IolqSlGt8n1fsLXCKMiBt8fQzLBPYk=";
+    sha256 = "sha256-obfPMD5zv2Q4ztDLCo4W8k6H80OKDQl/cDD8B5VnQMY=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin [ Security ];
 
-  cargoSha256 = "sha256-EYfqdGpaAWDmvqov/k81kE9frHYNKOTHakCrHN7zpU4=";
+  cargoHash = "sha256-xp7Oy6cKFTdc6KsXJFtyEYRnGazjxf/4HpKcIsAt+ko=";
 
   # Disable the GUI file picker so that GTK/XDG dependencies aren't used
   buildNoDefaultFeatures = true;
