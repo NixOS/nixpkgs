e54888312c4158a5c4a29eb2b3985315f6d6e145
Felix Buehler <account@buehler.rocks>
2023-01-12 23:06:16 +0100

    nix-generate-from-cpan: deprecate phases

--- maintainers/scripts/nix-generate-from-cpan.nix
+++ maintainers/scripts/nix-generate-from-cpan.nix
@@ -9,15 +9,14 @@ stdenv.mkDerivation {
     perl GetoptLongDescriptive CPANPLUS Readonly LogLog4perl
   ];
 
-  phases = [ "installPhase" ];
+  dontUnpack = true;
 
-  installPhase =
-    ''
-      mkdir -p $out/bin
-      cp ${./nix-generate-from-cpan.pl} $out/bin/nix-generate-from-cpan
-      patchShebangs $out/bin/nix-generate-from-cpan
-      wrapProgram $out/bin/nix-generate-from-cpan --set PERL5LIB $PERL5LIB
-    '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${./nix-generate-from-cpan.pl} $out/bin/nix-generate-from-cpan
+    patchShebangs $out/bin/nix-generate-from-cpan
+    wrapProgram $out/bin/nix-generate-from-cpan --set PERL5LIB $PERL5LIB
+  '';
 
   meta = {
     maintainers = with lib.maintainers; [ eelco ];
