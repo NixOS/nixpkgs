519541a182c4694a257c80057725d2a373a38b9e
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-12 23:05:36 +0000

    checkstyle: 10.5.0 -> 10.6.0

--- pkgs/development/tools/analysis/checkstyle/default.nix
+++ pkgs/development/tools/analysis/checkstyle/default.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchurl, makeWrapper, jre }:
 
 stdenv.mkDerivation rec {
-  version = "10.5.0";
+  version = "10.6.0";
   pname = "checkstyle";
 
   src = fetchurl {
     url = "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${version}/checkstyle-${version}-all.jar";
-    sha256 = "sha256-GbPVA/Z9R0P9prJTFJUCTV9pq9/Cbl6UOjwhAlhe9BA=";
+    sha256 = "sha256-ghGrRu7a9OUVti6P4QRrGFSmQqX8XCfn7aBHR4Q7RCI=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
