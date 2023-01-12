9cea6845589ba394ea02d412374b950280ff4e22
Robert Schütz <nix@dotlambda.de>
2023-01-12 06:30:00 -0800

    python310Packages.pytibber: 0.26.7 -> 0.26.8
    https://github.com/Danielhiversen/pyTibber/releases/tag/0.26.8

--- pkgs/development/python-modules/pytibber/default.nix
+++ pkgs/development/python-modules/pytibber/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "pytibber";
-  version = "0.26.7";
+  version = "0.26.8";
   format = "setuptools";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "Danielhiversen";
     repo = "pyTibber";
     rev = "refs/tags/${version}";
-    hash = "sha256-T2J31+H/cO000qi2AlEGaFtAu0fl7u1LAA/QUxRAiK8=";
+    hash = "sha256-q3mlAlcetg01vHxxSIZjyDPxAqUWcdyzK4qsVmQAreA=";
   };
 
   propagatedBuildInputs = [
