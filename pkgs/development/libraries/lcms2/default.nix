b4fd6c48c02789381f8e3eb53bf6820de566ccd8
Anthony Roussel <anthony@roussel.dev>
2023-01-12 13:18:10 +0100

    lcms2: 2.13.1 -> 2.14

--- pkgs/development/libraries/lcms2/default.nix
+++ pkgs/development/libraries/lcms2/default.nix
@@ -1,24 +1,32 @@
-{ lib, stdenv, fetchurl, libtiff, libjpeg, zlib }:
+{ lib, stdenv, fetchFromGitHub, libtiff, libjpeg, nix-update-script, zlib }:
 
 stdenv.mkDerivation rec {
   pname = "lcms2";
-  version = "2.13.1";
-
-  src = fetchurl {
-    url = "mirror://sourceforge/lcms/${pname}-${version}.tar.gz";
-    sha256 = "sha256-1HPnlueyfFrwG9bRVS1CtFtDRX5xgs6ZA/OLt0ggO4g=";
-  };
+  version = "2.14";
 
   outputs = [ "bin" "dev" "out" ];
+
+  src = fetchFromGitHub {
+    owner = "mm2";
+    repo = "Little-CMS";
+    rev = "lcms${version}";
+    sha256 = "sha256-E4D25wRUPNvcaCR9Hx72Hfsb3sc7P5yb8EgfExFxZFg=";
+  };
 
   propagatedBuildInputs = [ libtiff libjpeg zlib ];
 
   # See https://trac.macports.org/ticket/60656
-  LDFLAGS = if stdenv.hostPlatform.isDarwin then "-Wl,-w" else null;
+  LDFLAGS = lib.optionalString stdenv.hostPlatform.isDarwin "-Wl,-w";
+
+  passthru.updateScript = nix-update-script {
+    attrPath = pname;
+    extraArgs = [ "--version-regex" "lcms([0-9.]+)" ];
+  };
 
   meta = with lib; {
     description = "Color management engine";
-    homepage = "http://www.littlecms.com/";
+    homepage = "https://www.littlecms.com";
+    changelog = "https://github.com/mm2/Little-CMS/blob/lcms${version}/ChangeLog";
     license = licenses.mit;
     platforms = platforms.all;
   };
