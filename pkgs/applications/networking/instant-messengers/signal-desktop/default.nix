49b629167090f9cc586929c90c1a14e567e9b7af
Jonas Heinrich <onny@project-insanity.org>
2023-01-12 15:15:30 +0100

    signal-desktop: 6.1.0 -> 6.2.0

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -1,8 +1,8 @@
 { callPackage }: builtins.mapAttrs (pname: attrs: callPackage ./generic.nix (attrs // { inherit pname; })) {
   signal-desktop = {
     dir = "Signal";
-    version = "6.1.0";
-    hash = "sha256-70IQ/2yjHbez8SpZxqZKa/XWIEYA3cN7JAIM9kgjN30=";
+    version = "6.2.0";
+    hash = "sha256-auOcMlwKPj5rsnlhK34sYe4JxlHCjb3e2ye8Cs12Qtc=";
   };
   signal-desktop-beta = {
     dir = "Signal Beta";
