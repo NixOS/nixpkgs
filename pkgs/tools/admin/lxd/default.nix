6cd9183e7e8317c09492112a73e1ce3d909ad4b7
Mario Rodas <marsam@users.noreply.github.com>
2023-01-12 04:20:00 +0000

    lxd: 5.9 -> 5.10

--- pkgs/tools/admin/lxd/default.nix
+++ pkgs/tools/admin/lxd/default.nix
@@ -32,14 +32,14 @@
 
 buildGoModule rec {
   pname = "lxd";
-  version = "5.9";
+  version = "5.10";
 
   src = fetchurl {
     urls = [
       "https://linuxcontainers.org/downloads/lxd/lxd-${version}.tar.gz"
       "https://github.com/lxc/lxd/releases/download/lxd-${version}/lxd-${version}.tar.gz"
     ];
-    sha256 = "sha256-okz3++PlUno03tp+jpLxfAWlFJhyOCH2mxRtHo5YEX8=";
+    sha256 = "sha256-sYJkPr/tE22xJEjKX7fMjOLQ9zBDm52UjqbVLrm39zU=";
   };
 
   vendorSha256 = null;
