7b26ee0ebf7c074d7929ac7dda0ab4403bcdef7a
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-01-12 17:25:26 +0200

    pv-migrate: init at 1.0.1

---
+++ pkgs/applications/networking/cluster/pv-migrate/default.nix
@@ -0,0 +1,36 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "pv-migrate";
+  version = "1.0.1";
+
+  src = fetchFromGitHub {
+    owner = "utkuozdemir";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-I66J1/N8Ln7KBQfzg39wdZuM6PeJGn1HiNK2YVzDySw";
+  };
+
+  subPackages = [ "cmd/pv-migrate" ];
+
+  vendorSha256 = "sha256-/klqOfM0ZhbzZWOLm0pA0/RB84kvfEzFJN1OQUVSNEA";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=v${version}"
+    "-X main.commit=${src.rev}"
+  ];
+
+  preBuild = ''
+    ldflags+=" -X main.date=$(date +%Y-%m-%d-%T)"
+  '';
+
+  meta = with lib; {
+    description = "CLI tool to easily migrate Kubernetes persistent volumes ";
+    homepage = "https://github.com/utkuozdemir/pv-migrate";
+    changelog = "https://github.com/utkuozdemir/pv-migrate/releases/tag/${version}";
+    license = licenses.afl20;
+    maintainers = [ maintainers.ivankovnatsky ];
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37702,6 +37702,8 @@ with pkgs;
 
   prow = callPackage ../applications/networking/cluster/prow { };
 
+  pv-migrate = callPackage ../applications/networking/cluster/pv-migrate { };
+
   tagref = callPackage ../tools/misc/tagref { };
 
   tcat = callPackage ../tools/misc/tcat { };
