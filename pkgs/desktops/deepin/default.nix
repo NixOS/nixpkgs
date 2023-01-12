216ac3a5e36b94edc02ec875cfacd67f949e9e5e
rewine <luhongxu@deepin.org>
2023-01-12 14:05:08 +0800

    deepin-sound-theme: init at 15.10.6

--- pkgs/desktops/deepin/default.nix
+++ pkgs/desktops/deepin/default.nix
@@ -26,6 +26,7 @@ let
     #### ARTWORK
     deepin-icon-theme = callPackage ./artwork/deepin-icon-theme { };
     deepin-gtk-theme = callPackage ./artwork/deepin-gtk-theme { };
+    deepin-sound-theme = callPackage ./artwork/deepin-sound-theme { };
   };
 in
 lib.makeScope libsForQt5.newScope packages
