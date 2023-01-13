a8d573e9b04fb50e30eb98647af9e076e0202221
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-13 11:41:35 +0000

    python310Packages.zigpy: 0.53.0 -> 0.53.1

--- pkgs/development/python-modules/zigpy/default.nix
+++ pkgs/development/python-modules/zigpy/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "zigpy";
-  version = "0.53.0";
+  version = "0.53.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zigpy";
     rev = "refs/tags/${version}";
-    hash = "sha256-9KTBzPklQpqzLOQYRz6Kd0wUSHr2sfypvTu0X7om31Y=";
+    hash = "sha256-1ey1JDxRF9zYSTn1EKWz1gcCw5WrCCH1eNyp6KOO8eI=";
   };
 
   propagatedBuildInputs = [
