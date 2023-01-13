2de90e2e841f15ed1ee7556a36d2fbb6dae540d4
ajs124 <git@ajs124.de>
2023-01-13 01:24:22 +0100

    umurmur: fix build with openssl_3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33060,9 +33060,7 @@ with pkgs;
 
   uhhyou.lv2 = callPackage ../applications/audio/uhhyou.lv2 { };
 
-  umurmur = callPackage ../applications/networking/umurmur {
-    openssl = openssl_1_1;
-  };
+  umurmur = callPackage ../applications/networking/umurmur { };
 
   udocker = callPackage ../tools/virtualization/udocker { };
 
