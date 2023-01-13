5b0d8e8b16c2a39f02b2d2edc96b9552fb1234d7
Adam Joseph <adam@westernsemico.com>
2023-01-13 05:31:24 -0800

    https://github.com/NixOS/nixpkgs/pull/208685#pullrequestreview-1245851754

--- pkgs/os-specific/linux/firmware/ath9k/default.nix
+++ pkgs/os-specific/linux/firmware/ath9k/default.nix
@@ -9,21 +9,8 @@
 , enableUnstable ? false
 }:
 
+let common =
 stdenv.mkDerivation (finalAttrs: {
-  pname = "ath9k-htc-blobless-firmware";
-  version = if enableUnstable then "unstable-2022-05-22" else "1.4.0";
-
-  src = fetchFromGitHub ({
-    owner = "qca";
-    repo = "open-ath9k-htc-firmware";
-  } // (if enableUnstable then {
-    rev = "d856466a068afe4069335257c0d28295ff777d92";
-    hash = "sha256-9OE6qYGABeXjf1r/Depd+811EJ2e8I0Ni5ePHSOh9G4=";
-  } else {
-    rev = finalAttrs.version;
-    hash = "sha256-Q/A0ryIC5E1pt2Sh7o79gxHbe4OgdlrwflOWtxWSS5o=";
-  }));
-
   postPatch = ''
     patchShebangs target_firmware/firmware-crc.pl
   '';
@@ -67,11 +54,12 @@ stdenv.mkDerivation (finalAttrs: {
 
   passthru = {
     updateScript = writeScript "${finalAttrs.pname}-${finalAttrs.version}-updateScript" ''
-      nix-shell '<nixpkgs>' -A ${finalAttrs.pname}${lib.optionalString enableUnstable "-unstable"}.passthru.update \
+      nix-shell '<nixpkgs>' -A ${finalAttrs.pname}.passthru.update \
       > pkgs/os-specific/linux/firmware/ath9k/urls-and-hashes-${finalAttrs.version}.nix
     '';
-    update = stdenv.mkDerivation {
+    update = stdenv.mkDerivation (finalAttrs: {
       name = "${finalAttrs.pname}-${finalAttrs.version}-update";
+      extraHashes = "";
       shellHook = ''
         echo 'rec {'
         echo '  BASEDIR = "$NIX_BUILD_TOP";'
@@ -80,24 +68,11 @@ stdenv.mkDerivation (finalAttrs: {
           | sed 's_\([^ ]*\) = \(.*\)_  \1 = "\2\";_' \
           | tr \( \{ \
           | tr \) \}
-      ''
-      # sha256 checksums were not added to upstream's Makefile until
-      # after the 1.4.0 release.  The following line is needed for
-      # the `enableUnstable==false` build but not for the
-      # `enableUnstable==true` build.  We can remove the lines below
-      # as soon as `enableUnstable==false` points to a version
-      # greater than 1.4.0.
-      + lib.optionalString (finalAttrs.version == "1.4.0") ''
-        echo 'GCC_SUM = "sha256-kuYcbcOgpEnmLXKjgYX9pVAWioZwLeoHEl69PsOZYoI=";'
-        echo 'MPFR_SUM = "sha256-e2bD8T3IOF8IJkyAWFPz4aju2rgHHVgvPmYZccms1f0=";'
-        echo 'MPC_SUM = "sha256-7VqBXP6lJdx3jfDLN0aLnBtVSq8w2TKLFDHKcFt0AP8=";'
-        echo 'GMP_SUM = "sha256-H1iKrMxBu5rtlG+f44Uhwm2LKQ0APF34B/ZWkPKq3sk=";'
-        echo 'BINUTILS_SUM = "sha256-KrLlsD4IbRLGKV+DGtrUaz4UEKOiNJM6Lo+sZssuehk=";'
-      '' + ''
+      '' + finalAttrs.extraHashes + ''
         echo '}'
         exit
       '';
-    };
+    });
   };
 
   meta = {
@@ -148,4 +123,48 @@ stdenv.mkDerivation (finalAttrs: {
     downloadPage = "https://github.com/qca/open-ath9k-htc-firmware";
     changelog = "https://github.com/qca/open-ath9k-htc-firmware/tags";
   };
-})
+});
+
+  stable =
+    let version = "1.4.0"; in
+    common.overrideAttrs(prevAttrs: {
+      pname = "ath9k-htc-blobless-firmware";
+      inherit version;
+      src = fetchFromGitHub {
+        owner = "qca";
+        repo = "open-ath9k-htc-firmware";
+        rev = version;
+        hash = "sha256-Q/A0ryIC5E1pt2Sh7o79gxHbe4OgdlrwflOWtxWSS5o=";
+      };
+      passthru.update = prevAttrs.passthru.update.overrideAttrs(prevAttrs: {
+        # sha256 checksums were not added to upstream's Makefile until
+        # after the 1.4.0 release.  The following line is needed for
+        # the `enableUnstable==false` build but not for the
+        # `enableUnstable==true` build.  We can remove the lines below
+        # as soon as `enableUnstable==false` points to a version
+        # greater than 1.4.0.
+        extraHashes = ''
+        echo 'GCC_SUM = "sha256-kuYcbcOgpEnmLXKjgYX9pVAWioZwLeoHEl69PsOZYoI=";'
+        echo 'MPFR_SUM = "sha256-e2bD8T3IOF8IJkyAWFPz4aju2rgHHVgvPmYZccms1f0=";'
+        echo 'MPC_SUM = "sha256-7VqBXP6lJdx3jfDLN0aLnBtVSq8w2TKLFDHKcFt0AP8=";'
+        echo 'GMP_SUM = "sha256-H1iKrMxBu5rtlG+f44Uhwm2LKQ0APF34B/ZWkPKq3sk=";'
+        echo 'BINUTILS_SUM = "sha256-KrLlsD4IbRLGKV+DGtrUaz4UEKOiNJM6Lo+sZssuehk=";'
+      '';
+      });
+    });
+
+  unstable = common.overrideAttrs(prevAttrs: {
+    pname = "ath9k-htc-blobless-firmware-unstable";
+    version = "2022-05-22";
+    src = fetchFromGitHub {
+      owner = "qca";
+      repo = "open-ath9k-htc-firmware";
+      rev = "d856466a068afe4069335257c0d28295ff777d92";
+      hash = "sha256-9OE6qYGABeXjf1r/Depd+811EJ2e8I0Ni5ePHSOh9G4=";
+    };
+  });
+
+in
+  if enableUnstable
+  then unstable
+  else stable
