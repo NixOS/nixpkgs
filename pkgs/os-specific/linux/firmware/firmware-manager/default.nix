03214405bbfb25e6a7168e98715afb19dcfa6dba
Nick Cao <nickcao@nichi.co>
2023-01-13 10:28:28 +0800

    firmware-manager: 0.1.2 -> unstable-2022-12-09

--- pkgs/os-specific/linux/firmware/firmware-manager/default.nix
+++ pkgs/os-specific/linux/firmware/firmware-manager/default.nix
@@ -1,32 +1,56 @@
-{ rustPlatform, lib, fetchFromGitHub, xz, pkg-config, openssl, dbus, glib, udev, cairo, pango, atk, gdk-pixbuf, gtk3, wrapGAppsHook }:
-rustPlatform.buildRustPackage rec {
+{ lib
+, stdenv
+, rustPlatform
+, fetchFromGitHub
+, xz
+, pkg-config
+, openssl
+, dbus
+, glib
+, udev
+, cairo
+, pango
+, atk
+, gdk-pixbuf
+, gtk3
+, wrapGAppsHook
+}:
+
+stdenv.mkDerivation rec {
   pname = "firmware-manager";
-  version = "0.1.2";
+  version = "unstable-2022-12-09";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-aKatdjHa/k7j48upkR1O6PFxCUfJYE3KhhzZ9Ohe0Jc=";
+    rev = "9be8160346689bd74f95db7897884a91fa48afe3";
+    sha256 = "sha256-zZk2RVghhKxETSVv/Jtv8Wq6+ITx/BudE/o7h4jKk5M=";
   };
 
-  nativeBuildInputs = [ pkg-config wrapGAppsHook ];
-
-  buildInputs = [ xz openssl dbus glib udev cairo pango atk gdk-pixbuf gtk3 ];
-
-  depsExtraArgs.postPatch = "make prefix='$(out)' toml-gen";
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    inherit src;
+    sha256 = "sha256-3drsOmlmy1xXRWg7WMDNN+iuVmPYf60sDLIdCvu4rEw=";
+  };
 
   postPatch = ''
-    sed -i 's|etc|$(prefix)/etc|' Makefile
+    substituteInPlace Makefile --replace '$(DESTDIR)/etc' '$(DESTDIR)$(prefix)/etc'
   '';
 
-  buildPhase = "make prefix='$(out)'";
+  nativeBuildInputs = with rustPlatform; [
+    rust.cargo
+    rust.rustc
+    pkg-config
+    cargoSetupHook
+    wrapGAppsHook
+  ];
 
-  installPhase = "make prefix='$(out)' install";
+  buildInputs = [
+    openssl
+    gtk3
+    udev
+  ];
 
-  cargoSha256 = "sha256-BUo77ERHvuc8IkDdU3Z/gZZicNHT26IbAgEBnVM3O4U=";
-
-  doCheck = false;
+  makeFlags = [ "prefix=$(out)" ];
 
   meta = {
     description = "Graphical frontend for firmware management";
