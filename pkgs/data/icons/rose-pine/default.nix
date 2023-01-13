e02cd8e9286fbbf27afefbc6032b4f035cca242d
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-12 22:58:33 -0600

    rose-pine-gtk: format

--- pkgs/data/icons/rose-pine/default.nix
+++ pkgs/data/icons/rose-pine/default.nix
@@ -3,36 +3,36 @@
   lib,
   fetchFromGitHub,
 }:
-  stdenv.mkDerivation rec {
-    pname = "rose-pine-icon-theme";
-    version = "unstable-2022-09-01";
+stdenv.mkDerivation rec {
+  pname = "rose-pine-icon-theme";
+  version = "unstable-2022-09-01";
 
-    src = fetchFromGitHub {
-      owner = "rose-pine";
-      repo = "gtk";
-      rev = "7a4c40989fd42fd8d4a797f460c79fc4a085c304";
-      sha256 = "0q74wjyrsjyym770i3sqs071bvanwmm727xzv50wk6kzvpyqgi67";
-    };
+  src = fetchFromGitHub {
+    owner = "rose-pine";
+    repo = "gtk";
+    rev = "7a4c40989fd42fd8d4a797f460c79fc4a085c304";
+    sha256 = "0q74wjyrsjyym770i3sqs071bvanwmm727xzv50wk6kzvpyqgi67";
+  };
 
-    # avoid the makefile which is only for the theme maintainers
-    dontBuild = true;
+  # avoid the makefile which is only for the theme maintainers
+  dontBuild = true;
 
-    installPhase = ''
-      runHook preInstall
+  installPhase = ''
+    runHook preInstall
 
-      mkdir -p $out/share/icons
-      mv icons/rose-pine-icons $out/share/icons/rose-pine
-      mv icons/rose-pine-dawn-icons $out/share/icons/rose-pine-dawn
-      mv icons/rose-pine-moon-icons $out/share/icons/rose-pine-moon
+    mkdir -p $out/share/icons
+    mv icons/rose-pine-icons $out/share/icons/rose-pine
+    mv icons/rose-pine-dawn-icons $out/share/icons/rose-pine-dawn
+    mv icons/rose-pine-moon-icons $out/share/icons/rose-pine-moon
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
-    meta = with lib; {
-      description = "Rosé Pine icon theme for GTK";
-      homepage = "https://github.com/rose-pine/gtk";
-      license = licenses.gpl3Only;
-      platforms = platforms.linux;
-      maintainers = with maintainers; [romildo the-argus];
-    };
-  }
+  meta = with lib; {
+    description = "Rosé Pine icon theme for GTK";
+    homepage = "https://github.com/rose-pine/gtk";
+    license = licenses.gpl3Only;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [romildo the-argus];
+  };
+}
--- pkgs/data/themes/rose-pine-gtk/default.nix
+++ pkgs/data/themes/rose-pine-gtk/default.nix
@@ -4,48 +4,48 @@
   lib,
   gnome-themes-extra,
   gtk-engine-murrine,
-  gtk_engines
+  gtk_engines,
 }:
-  stdenv.mkDerivation rec {
-    pname = "rose-pine-${variant}-gtk-theme";
-    version = "unstable-2022-09-01";
+stdenv.mkDerivation rec {
+  pname = "rose-pine-${variant}-gtk-theme";
+  version = "unstable-2022-09-01";
 
-    src = fetchFromGitHub {
-      owner = "rose-pine";
-      repo = "gtk";
-      rev = "7a4c40989fd42fd8d4a797f460c79fc4a085c304";
-      sha256 = "0q74wjyrsjyym770i3sqs071bvanwmm727xzv50wk6kzvpyqgi67";
-    };
+  src = fetchFromGitHub {
+    owner = "rose-pine";
+    repo = "gtk";
+    rev = "7a4c40989fd42fd8d4a797f460c79fc4a085c304";
+    sha256 = "0q74wjyrsjyym770i3sqs071bvanwmm727xzv50wk6kzvpyqgi67";
+  };
 
-    buildInputs = [
-      gnome-themes-extra # adwaita engine for Gtk2
-      gtk_engines # pixmap engine for Gtk2
-    ];
+  buildInputs = [
+    gnome-themes-extra # adwaita engine for Gtk2
+    gtk_engines # pixmap engine for Gtk2
+  ];
 
-    propagatedUserEnvPkgs = [
-      gtk-engine-murrine # murrine engine for Gtk2
-    ];
+  propagatedUserEnvPkgs = [
+    gtk-engine-murrine # murrine engine for Gtk2
+  ];
 
-    # avoid the makefile which is only for theme maintainers
-    dontBuild = true;
+  # avoid the makefile which is only for theme maintainers
+  dontBuild = true;
 
-    installPhase = ''
-      runHook preInstall
+  installPhase = ''
+    runHook preInstall
 
-      mkdir -p $out/share/themes
-      mv gtk3/rose-pine-gtk $out/share/themes/rose-pine
-      mv gtk3/rose-pine-moon-gtk $out/share/themes/rose-pine-moon
-      mv gtk3/rose-pine-dawn-gtk $out/share/themes/rose-pine-dawn
-      mv gnome_shell/moon/gnome-shell $out/share/themes/rose-pine-moon
+    mkdir -p $out/share/themes
+    mv gtk3/rose-pine-gtk $out/share/themes/rose-pine
+    mv gtk3/rose-pine-moon-gtk $out/share/themes/rose-pine-moon
+    mv gtk3/rose-pine-dawn-gtk $out/share/themes/rose-pine-dawn
+    mv gnome_shell/moon/gnome-shell $out/share/themes/rose-pine-moon
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
-    meta = with lib; {
-      description = "Rosé Pine theme for GTK";
-      homepage = "https://github.com/rose-pine/gtk";
-      license = licenses.gpl3Only;
-      platforms = platforms.linux;
-      maintainers = with maintainers; [romildo the-argus];
-    };
-  }
+  meta = with lib; {
+    description = "Rosé Pine theme for GTK";
+    homepage = "https://github.com/rose-pine/gtk";
+    license = licenses.gpl3Only;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [romildo the-argus];
+  };
+}
