de6b61ffc6f893a5f540f20cc38ee8bc0d7be332
Alyssa Ross <hi@alyssa.is>
2023-01-13 19:22:44 +0000

    libcxxrt: init at unstable-2022-08-08

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20589,6 +20589,12 @@ with pkgs;
 
   libcutl = callPackage ../development/libraries/libcutl { };
 
+  libcxxrt = callPackage ../development/libraries/libcxxrt {
+    stdenv = if stdenv.hostPlatform.useLLVM or false
+             then overrideCC stdenv buildPackages.llvmPackages.tools.clangNoLibcxx
+             else stdenv;
+  };
+
   libdaemon = callPackage ../development/libraries/libdaemon { };
 
   libdatrie = callPackage ../development/libraries/libdatrie { };
