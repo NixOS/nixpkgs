b8714f08a47abf6163955a5e218d896161bfe774
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2023-01-14 13:17:04 +0100

    dtv-scan-tables: update/extend meta descriptions
    * `description`: add some keywords that help
      users in need of transponder tables to find this package.
    * `longDescription`: add

--- pkgs/data/misc/dtv-scan-tables/default.nix
+++ pkgs/data/misc/dtv-scan-tables/default.nix
@@ -26,9 +26,20 @@ stdenv.mkDerivation rec {
   allowedReferences = [ ];
 
   meta = with lib; {
-    description = "Digital TV scan tables";
+    description = "Digital TV (DVB) channel/transponder scan tables";
     homepage = "https://github.com/tvheadend/dtv-scan-tables";
     license = with licenses; [ gpl2Only lgpl21Only ];
+    longDescription = ''
+      When scanning for dvb channels,
+      most applications require an initial set of
+      transponder coordinates (frequencies etc.).
+      These coordinates differ, depending of the
+      receiver's location or on the satellite.
+      The package delivers a collection of transponder
+      tables ready to be used by software like "dvbv5-scan".
+      The package at hand is maintained and used by tvheadend,
+      it is a fork of the original one hosted by linuxtv.org.
+    '';
     maintainers = with maintainers; [ ];
   };
 }
