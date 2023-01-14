141ba0aaa1ad878873a09c1160d0756e739ae297
zowoq <59103226+zowoq@users.noreply.github.com>
2023-01-15 07:04:05 +1000

    talosctl: 1.3.1 -> 1.3.2
    https://github.com/siderolabs/talos/releases/tag/v1.3.2

--- pkgs/applications/networking/cluster/talosctl/default.nix
+++ pkgs/applications/networking/cluster/talosctl/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "talosctl";
-  version = "1.3.1";
+  version = "1.3.2";
 
   src = fetchFromGitHub {
     owner = "siderolabs";
     repo = "talos";
     rev = "v${version}";
-    sha256 = "sha256-K5eslPo4Ou/JJoZGASJsZ331Nyc14Dva+cead3ngw0U=";
+    hash = "sha256-o037LemAZhsHVIfAU0yVoNNbJ6g8ehlA4lFLN78Q8dY=";
   };
 
-  vendorSha256 = "sha256-N+sFJtC+9VCt3GJoAbRsNl7vCXiFFlewsIkwTdleUhk=";
+  vendorHash = "sha256-HPuVDfVxbHbNaCRg/D4VeW0finbFB4eageiAzoax1wk=";
 
   ldflags = [ "-s" "-w" ];
 
