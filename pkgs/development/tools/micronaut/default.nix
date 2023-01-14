92dca3d39f1ab3db606f1869baf658bc8b292013
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-14 00:34:44 +0000

    micronaut: 3.7.4 -> 3.8.1

--- pkgs/development/tools/micronaut/default.nix
+++ pkgs/development/tools/micronaut/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "micronaut";
-  version = "3.7.4";
+  version = "3.8.1";
 
   src = fetchzip {
     url = "https://github.com/micronaut-projects/micronaut-starter/releases/download/v${version}/micronaut-cli-${version}.zip";
-    sha256 = "sha256-VUavNwj5MCMZFV9WASF3olOdZb4JNBYbcQdgUfvPS8k=";
+    sha256 = "sha256-DBWPKy17pYU5VWEOxnaP2AKoiskNCBbt9QqO6xxf7eI=";
   };
 
   nativeBuildInputs = [ makeWrapper installShellFiles ];
