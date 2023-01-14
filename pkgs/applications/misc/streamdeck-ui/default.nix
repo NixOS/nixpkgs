9c9850c8ab771248bc9f42fa242b5887589a16f1
Majiir Paktu <majiir@nabaal.net>
2023-01-14 17:25:07 -0500

    streamdeck-ui: update xlib version constraint (#210757)

--- pkgs/applications/misc/streamdeck-ui/default.nix
+++ pkgs/applications/misc/streamdeck-ui/default.nix
@@ -26,6 +26,11 @@ python3Packages.buildPythonApplication rec {
       url = "https://github.com/timothycrosley/streamdeck-ui/commit/e271656c1f47b1619d1b942e2ebb01ab2d6a68a9.patch";
       hash = "sha256-wqYwX6eSqMnW6OG7wSprD62Dz818ayFduVrqW9E/ays=";
     })
+    (fetchpatch {
+      name = "update-python-xlib-0.33.patch";
+      url = "https://github.com/timothycrosley/streamdeck-ui/commit/07d7fdd33085b413dd26b02d8a02820edad2d568.patch";
+      hash = "sha256-PylTrbfB8RJ0+kbgJlRdcvfdahGoob8LabwhuFNsUpY=";
+    })
   ];
 
   desktopItems = [ (makeDesktopItem {
