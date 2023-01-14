0b1ecd675d7741ce812b05b4383df0ce280ef42d
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-14 07:55:03 +0000

    gtkwave: 3.3.113 -> 3.3.114

--- pkgs/applications/science/electronics/gtkwave/default.nix
+++ pkgs/applications/science/electronics/gtkwave/default.nix
@@ -16,11 +16,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gtkwave";
-  version = "3.3.113";
+  version = "3.3.114";
 
   src = fetchurl {
     url = "mirror://sourceforge/gtkwave/${pname}-gtk3-${version}.tar.gz";
-    sha256 = "sha256-bv81puBSiFVkO2hNk63rMchbTSN3KtSnyHnL0/apQe0=";
+    sha256 = "sha256-zzpfyrs3qWckzeLkp0A73ye1HpFGzmenlNvkcBfPhKw=";
   };
 
   nativeBuildInputs = [ pkg-config wrapGAppsHook ];
