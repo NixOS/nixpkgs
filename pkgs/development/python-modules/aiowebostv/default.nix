dbd6053de7ff5acb49efce6dc8a399aa54603cb0
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-15 10:04:07 +0100

    python310Packages.aiowebostv: add changelog to meta

--- pkgs/development/python-modules/aiowebostv/default.nix
+++ pkgs/development/python-modules/aiowebostv/default.nix
@@ -33,6 +33,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Module to interact with LG webOS based TV devices";
     homepage = "https://github.com/home-assistant-libs/aiowebostv";
+    changelog = "https://github.com/home-assistant-libs/aiowebostv/releases/tag/v${version}";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ fab ];
   };
