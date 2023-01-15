a9f6015a2c24822c5eea91b087d411441010e3c1
Milan Hauth <milahu@gmail.com>
2023-01-15 08:37:36 +0100

    fixup

--- pkgs/development/tools/qtcreator/default.nix
+++ pkgs/development/tools/qtcreator/default.nix
@@ -65,11 +65,13 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
+/*
   postPatch = ''
     stat src/libs/extensionsystem/pluginmanager.cpp
     cp ${./src/qt-creator-opensource-src-8.0.1/src/libs/extensionsystem/pluginmanager.cpp} src/libs/extensionsystem/pluginmanager.cpp
     cp ${./src/qt-creator-opensource-src-8.0.1/src/plugins/welcome/welcomeplugin.cpp} src/plugins/welcome/welcomeplugin.cpp
   '';
+*/
 
   #buildFlags = optional withDocumentation "docs";
 
