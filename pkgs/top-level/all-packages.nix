c98d09461e3781b633d0c1edca51d5cddced6c0b
Kimmo Suominen <kimmo@suominen.com>
2023-01-15 11:33:23 +0200

    colorless: init at 109

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1341,6 +1341,8 @@ with pkgs;
 
   clematis = callPackage ../tools/misc/clematis { };
 
+  colorless = callPackage ../tools/misc/colorless { };
+
   cope = callPackage ../tools/misc/cope { };
 
   ejson2env = callPackage ../tools/admin/ejson2env { };
