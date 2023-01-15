e3cff492974dc38355aaa7bb3211f09d65bb6b56
Bobby Rong <rjl931189261@126.com>
2023-01-15 11:01:45 +0800

    gnome2.libgnomeprint: remove
    Unmaintained and no package depends on it.

--- pkgs/desktops/gnome-2/default.nix
+++ pkgs/desktops/gnome-2/default.nix
@@ -14,8 +14,6 @@ lib.makeScope pkgs.newScope (self: with self; {
 
   libglade = callPackage ./platform/libglade { };
 
-  libgnomeprint = callPackage ./platform/libgnomeprint { };
-
   libgnomecups = callPackage ./platform/libgnomecups { };
 
   libgtkhtml = callPackage ./platform/libgtkhtml { };
@@ -78,6 +76,7 @@ lib.makeScope pkgs.newScope (self: with self; {
   gnome_python = throw "gnome2.gnome_python has been removed"; # 2023-01-14
   gnome_python_desktop = throw "gnome2.gnome_python_desktop has been removed"; # 2023-01-14
   libglademm = throw "gnome2.libglademm has been removed"; # 2022-01-15
+  libgnomeprint = throw "gnome2.libgnomeprint has been removed"; # 2023-01-15
   libgnomeprintui = throw "gnome2.libgnomeprintui has been removed"; # 2023-01-15
   python_rsvg = throw "gnome2.python_rsvg has been removed"; # 2023-01-14
 })
