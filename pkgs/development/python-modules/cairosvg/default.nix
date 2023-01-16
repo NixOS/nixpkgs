f97f850c8abc18e618dab2f64410106f7b4fd32d
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-16 03:03:00 +0000

    python310Packages.cairosvg: 2.5.2 -> 2.6.0

--- pkgs/development/python-modules/cairosvg/default.nix
+++ pkgs/development/python-modules/cairosvg/default.nix
@@ -12,12 +12,12 @@
 
 buildPythonPackage rec {
   pname = "CairoSVG";
-  version = "2.5.2";
+  version = "2.6.0";
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-sLmSnPXboAUXjXRqgDb88AJVUPSYylTbYYczIjhHg7w=";
+    sha256 = "sha256-1eyT6QEBs7boKqJF0FRu6bAWz9oLY0RnUVmDDYU9XQQ=";
   };
 
   propagatedBuildInputs = [ cairocffi cssselect2 defusedxml pillow tinycss2 ];
