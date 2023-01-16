cc51fd4319a5277d9fc0633288f61eb6ddde278f
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-16 14:49:41 +0100

    python310Packages.openerz-api: add changelog to meta

--- pkgs/development/python-modules/openerz-api/default.nix
+++ pkgs/development/python-modules/openerz-api/default.nix
@@ -10,13 +10,15 @@
 buildPythonPackage rec {
   pname = "openerz-api";
   version = "0.1.0";
+  format = "setuptools";
+
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "misialq";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "10kxsmaz2rn26jijaxmdmhx8vjdz8hrhlrvd39gc8yvqbjwhi3nw";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-3I4IuVx4e8ReGm1nCjNEv8mNOqytdiWjNMJm8VXVfYI=";
   };
 
   propagatedBuildInputs = [
@@ -28,11 +30,14 @@ buildPythonPackage rec {
     testfixtures
   ];
 
-  pythonImportsCheck = [ "openerz_api" ];
+  pythonImportsCheck = [
+    "openerz_api"
+  ];
 
   meta = with lib; {
     description = "Python module to interact with the OpenERZ API";
     homepage = "https://github.com/misialq/openerz-api";
+    changelog = "https://github.com/misialq/openerz-api/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
