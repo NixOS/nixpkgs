c8b20e1c4168642f9fc92fb8d7fa465eb7a18c5c
ajs124 <git@ajs124.de>
2023-01-16 19:17:36 +0100

    apk-tools: don't pin to openssl_1_1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2542,7 +2542,6 @@ with pkgs;
 
   apk-tools = callPackage ../tools/package-management/apk-tools {
     lua = lua5_3;
-    openssl = openssl_1_1;
   };
 
   apkid = callPackage ../development/tools/apkid { };
