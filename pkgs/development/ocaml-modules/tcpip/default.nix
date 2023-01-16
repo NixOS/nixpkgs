f2b2b533b22161c076d25f75eb07a6356da347fa
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-01-16 07:41:15 +0100

    ocamlPackages.tcpip: use Dune 3

--- pkgs/development/ocaml-modules/tcpip/default.nix
+++ pkgs/development/ocaml-modules/tcpip/default.nix
@@ -15,7 +15,7 @@ buildDunePackage rec {
   pname = "tcpip";
   version = "7.1.2";
 
-  useDune2 = true;
+  duneVersion = "3";
 
   src = fetchurl {
     url = "https://github.com/mirage/mirage-${pname}/releases/download/v${version}/${pname}-${version}.tbz";
