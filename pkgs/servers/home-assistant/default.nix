caca015dc18fb8d279d9f955960ae75c86b81929
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-01-17 10:25:43 +0100

    home-assistant: 2023.1.4 -> 2023.1.5
    https://github.com/home-assistant/core/releases/tag/2023.1.5

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -243,7 +243,7 @@ let
   extraPackagesFile = writeText "home-assistant-packages" (lib.concatMapStringsSep "\n" (pkg: pkg.pname) extraBuildInputs);
 
   # Don't forget to run parse-requirements.py after updating
-  hassVersion = "2023.1.4";
+  hassVersion = "2023.1.5";
 
 in python.pkgs.buildPythonApplication rec {
   pname = "homeassistant";
@@ -261,7 +261,7 @@ in python.pkgs.buildPythonApplication rec {
     owner = "home-assistant";
     repo = "core";
     rev = "refs/tags/${version}";
-    hash = "sha256-up6AKdrbiLuVf58yDUGlNME7qVFbegnnb3zoTF4Gj3s=";
+    hash = "sha256-gqWkj90Vw+Pne0Iseet1Jz0Eh3YpUiqspGltvU5Pxro=";
   };
 
   # leave this in, so users don't have to constantly update their downstream patch handling
