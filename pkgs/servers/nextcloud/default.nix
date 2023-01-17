ff2fd5b4d8c5eb8084126b8b10121f8577c3cd8b
Yaya <mak@nyantec.com>
2023-01-17 11:00:20 +0000

    nextcloud25: 25.0.2 -> 25.0.3
    https://nextcloud.com/changelog/#latest25

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -55,8 +55,8 @@ in {
   };
 
   nextcloud25 = generic {
-    version = "25.0.2";
-    sha256 = "d6ab40faa108937bda42395f570ff111f4c97343b55be1420024da3177e37d59";
+    version = "25.0.3";
+    sha256 = "4b2b1423736ef92469096fe24f61c24cad87a34e07c1c7a81b385d3ea25c00ec";
   };
 
   # tip: get the sha with:
