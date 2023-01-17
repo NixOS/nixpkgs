NixOS/nixpkgs

commit 1693ed2be9e6904c9a4621da37b2c5a7c5529789

fzf: wrap 'perl' in scripts with LOCALE_ARCHIVE
Signed-off-by: Austin Seipp <aseipp@pobox.com>

pkgs/tools/misc/fzf/default.nix
@@ -1,4 +1,6 @@
-{ lib
+{ stdenv
+, lib
+, pkgs
 , buildGoModule
 , fetchFromGitHub
 , writeText
@@ -6,7 +8,20 @@
 , installShellFiles
 , ncurses
 , perl
+, glibcLocales
 }:
+
+let
+  # on Linux, wrap perl in the bash completion scripts with the glibc locales,
+  # so that using the shell completion (ctrl+r, etc) doesn't result in ugly
+  # warnings on non-nixos machines
+  ourPerl = if stdenv.isDarwin then perl else (
+    pkgs.writers.writeBashBin "perl" ''
+      #!${pkgs.runtimeShell}
+      export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive"
+      exec ${perl}/bin/perl "$@"
+    '');
+in
 buildGoModule rec {
   pname = "fzf";
   version = "0.35.1";
@@ -42,7 +57,7 @@ buildGoModule rec {
     # Has a sneaky dependency on perl
     # Include first args to make sure we're patching the right thing
     substituteInPlace shell/key-bindings.bash \
-      --replace " perl -n " " ${perl}/bin/perl -n "
+      --replace " perl -n " " ${ourPerl}/bin/perl -n "
   '';
 
   postInstall = ''

commit bcb2b707e0734d5a321a463a4e10c1fd476be8c6

fzf: only wrap perl on Linux
As the comment says, this should only have happened on Linux, not all
non-Darwin platforms.

Fixes pkgsCross.x86_64-netbsd.fzf.

Fixes: 1693ed2be9e ("fzf: wrap 'perl' in scripts with LOCALE_ARCHIVE")

pkgs/tools/misc/fzf/default.nix
@@ -15,7 +15,7 @@ let
   # on Linux, wrap perl in the bash completion scripts with the glibc locales,
   # so that using the shell completion (ctrl+r, etc) doesn't result in ugly
   # warnings on non-nixos machines
-  ourPerl = if stdenv.isDarwin then perl else (
+  ourPerl = if !stdenv.isLinux then perl else (
     writeShellScriptBin "perl" ''
       export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive"
       exec ${perl}/bin/perl "$@"