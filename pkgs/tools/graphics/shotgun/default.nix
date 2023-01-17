122499234b03facb535358e0ed4d95f971413224
figsoda <figsoda@pm.me>
2023-01-17 12:06:52 -0500

    shotgun: fix build, add figsoda as a maintainer

--- pkgs/tools/graphics/shotgun/default.nix
+++ pkgs/tools/graphics/shotgun/default.nix
@@ -1,12 +1,8 @@
-{ lib, fetchFromGitHub, rustPlatform, pkg-config, libXrandr, libX11 }:
+{ lib, rustPlatform, fetchFromGitHub, pkg-config, libXrandr, libX11 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "shotgun";
   version = "2.3.1";
-
-  nativeBuildInputs = [ pkg-config ];
-
-  buildInputs = [ libXrandr libX11 ];
 
   src = fetchFromGitHub {
     owner = "neXromancers";
@@ -17,12 +13,20 @@ rustPlatform.buildRustPackage rec {
 
   cargoSha256 = "sha256-UOchXeBX+sDuLhwWQRVFCj9loJUyr4IltiAKsQoh5/c=";
 
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ libXrandr libX11 ];
+
+  # build script tries to run git to get the current tag
+  postPatch = ''
+    echo "fn main() {}" > build.rs
+  '';
+
   meta = with lib; {
     description = "Minimal X screenshot utility";
     homepage = "https://github.com/neXromancers/shotgun";
     license = with licenses; [ mpl20 ];
-    maintainers = with maintainers; [ lumi ];
+    maintainers = with maintainers; [ figsoda lumi ];
     platforms = platforms.linux;
-    badPlatforms = [ "aarch64-linux" ];
   };
 }
