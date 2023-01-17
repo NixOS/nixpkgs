b48c6ab66b77b25af31490ab089fa6ad750b0d72
ChaosAttractor <lostattractor@gmail.com>
2023-01-17 16:24:54 +0800

    yesplaymusic: init at 0.4.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38554,6 +38554,8 @@ with pkgs;
 
   xmcp = callPackage ../tools/X11/xmcp { };
 
+  yesplaymusic = callPackage ../applications/audio/yesplaymusic { };
+
   ymuse = callPackage ../applications/audio/ymuse { };
 
   zk = callPackage ../applications/office/zk {};
