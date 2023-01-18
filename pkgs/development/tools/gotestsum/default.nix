ba2f59a4661b42e2452cda7667d021dc9c8c839c
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-18 23:04:06 +0100

    gotestsum: add changelog to meta

--- pkgs/development/tools/gotestsum/default.nix
+++ pkgs/development/tools/gotestsum/default.nix
@@ -1,4 +1,7 @@
-{ lib, fetchFromGitHub, buildGoModule }:
+{ lib
+, fetchFromGitHub
+, buildGoModule
+}:
 
 buildGoModule rec {
   pname = "gotestsum";
@@ -7,20 +10,25 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "gotestyourself";
     repo = "gotestsum";
-    rev = "v${version}";
-    sha256 = "sha256-22srQmvbVu8eWVAbLDZG93yod/bJS6hfoc/YwFs64pY=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-22srQmvbVu8eWVAbLDZG93yod/bJS6hfoc/YwFs64pY=";
   };
 
   vendorHash = "sha256-zUqa6xlDV12ZV4N6+EZ7fLPsL8U+GB7boQ0qG9egvm0=";
 
   doCheck = false;
 
-  ldflags = [ "-s" "-w" "-X gotest.tools/gotestsum/cmd.version=${version}" ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X gotest.tools/gotestsum/cmd.version=${version}"
+  ];
 
   subPackages = [ "." ];
 
   meta = with lib; {
     homepage = "https://github.com/gotestyourself/gotestsum";
+    changelog = "https://github.com/gotestyourself/gotestsum/releases/tag/v${version}";
     description = "A human friendly `go test` runner";
     platforms = platforms.linux ++ platforms.darwin;
     license = licenses.asl20;
