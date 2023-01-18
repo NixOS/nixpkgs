0d103826a5e888939dd9e65a3d2abc5ff3b1f7ed
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-18 15:46:12 +0100

    python310Packages.archinfo: 9.2.33 -> 9.2.34

--- pkgs/development/python-modules/archinfo/default.nix
+++ pkgs/development/python-modules/archinfo/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "archinfo";
-  version = "9.2.33";
+  version = "9.2.34";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-6y5GkoC/U2tNHnKxLPyDxrf3zZkDNPbje0RQRkaKDuY=";
+    hash = "sha256-RsIknVuhDzPbVJudjilNnMjBTH6cxPROQEXxRiAbDEs=";
   };
 
   nativeBuildInputs = [
