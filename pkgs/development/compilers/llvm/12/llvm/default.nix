6cb0ad55abf05cfd3de61c182620f09a2db40832
Zhaofeng Li <hello@zhaofeng.li>
2023-01-18 15:13:15 -0700

    llvmPackages_{12,13,14}: Disable tests on RISC-V
    Same rationale as in 125132.

--- pkgs/development/compilers/llvm/12/llvm/default.nix
+++ pkgs/development/compilers/llvm/12/llvm/default.nix
@@ -254,7 +254,8 @@ in stdenv.mkDerivation (rec {
     cp NATIVE/bin/llvm-config $dev/bin/llvm-config-native
   '';
 
-  doCheck = stdenv.isLinux && (!stdenv.isx86_32) && (!stdenv.hostPlatform.isMusl)
+  # RISC-V: Tests do not pass (https://github.com/NixOS/nixpkgs/pull/125132)
+  doCheck = stdenv.isLinux && (!stdenv.isx86_32) && (!stdenv.hostPlatform.isMusl) && (!stdenv.hostPlatform.isRiscV)
     && (stdenv.hostPlatform == stdenv.buildPlatform);
 
   checkTarget = "check-all";
