2eb3417f757ec217c659c93d6910e49e6d45959b
kolaente <k@knt.li>
2023-01-18 19:10:28 +0100

    vikunja-frontend: 0.20.1 -> 0.20.2

--- pkgs/servers/web-apps/vikunja/frontend.nix
+++ pkgs/servers/web-apps/vikunja/frontend.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "vikunja-frontend";
-  version = "0.20.1";
+  version = "0.20.2";
   src = fetchurl {
     url = "https://dl.vikunja.io/frontend/${pname}-${version}.zip";
-    sha256 = "sha256-iKneTo+WVllmCRcJevbQFXfwSBhCY/V+aqTwfoT1XIc=";
+    sha256 = "sha256-7WvitR40eJPPdqwZm8C7spvEIdFIY3SGc/w4VY7spgk=";
   };
 
   nativeBuildInputs = [ unzip ];
