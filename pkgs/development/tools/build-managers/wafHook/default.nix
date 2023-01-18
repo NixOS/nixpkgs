46a1de1faec865b2b4ebb3d5513ca8aa624cd64d
Alyssa Ross <hi@alyssa.is>
2023-01-18 16:29:22 +0000

    treewide: add names to all setup hooks

--- pkgs/development/tools/build-managers/wafHook/default.nix
+++ pkgs/development/tools/build-managers/wafHook/default.nix
@@ -1,6 +1,7 @@
 { lib, stdenv, pkgs, makeSetupHook, waf }:
 
 makeSetupHook {
+  name = "waf-hook";
   substitutions = {
     inherit waf;
     crossFlags = lib.optionalString (stdenv.hostPlatform.system != stdenv.targetPlatform.system)
