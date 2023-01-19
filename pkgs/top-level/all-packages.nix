b3dead4ffbd5b35ba38022c6be733212bb737b2e
Bouke van der Bijl <i@bou.ke>
2023-01-19 13:41:47 +0100

    openmvs: init at 2.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10289,6 +10289,8 @@ with pkgs;
 
   openmvg = callPackage ../applications/science/misc/openmvg { };
 
+  openmvs = callPackage ../applications/science/misc/openmvs { };
+
   openntpd = callPackage ../tools/networking/openntpd { };
 
   openntpd_nixos = openntpd.override {
