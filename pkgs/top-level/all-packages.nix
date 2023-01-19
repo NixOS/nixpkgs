4e9c02bcc709fe1737a746add0e8e0109133d808
Kyle Sferrazza <git@kylesferrazza.com>
2023-01-19 00:24:02 -0800

    watchman: 4.9.0 -> 2023.01.30.00
    Co-authored-by: Andrew Hamon <and.ham95@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13082,8 +13082,7 @@ with pkgs;
   watchlog = callPackage ../tools/misc/watchlog { };
 
   watchman = callPackage ../development/tools/watchman {
-    inherit (darwin.apple_sdk.frameworks) CoreServices;
-    autoconf = buildPackages.autoconf269;
+    inherit (darwin.apple_sdk_11_0.frameworks) CoreServices;
   };
 
   wavefunctioncollapse = callPackage ../tools/graphics/wavefunctioncollapse {};
