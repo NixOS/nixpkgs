0ff2fbf3543e01e07ab76690520eaeb703903923
Bouke van der Bijl <i@bou.ke>
2023-01-19 11:59:09 +0100

    openmvg: 2.0 -> unstable-2022-12-30
    The installation instructions recommend latest master. Fixes build on
    arm and darwin.

--- pkgs/applications/science/misc/openmvg/default.nix
+++ pkgs/applications/science/misc/openmvg/default.nix
@@ -8,14 +8,14 @@
 , enableDocs ? false }:
 
 stdenv.mkDerivation rec {
-  version = "2.0";
+  version = "unstable-2022-12-30";
   pname = "openmvg";
 
   src = fetchFromGitHub {
     owner = "openmvg";
     repo = "openmvg";
-    rev = "v${version}";
-    sha256 = "sha256-6F/xUgZpqY+v6CpwTBhIXI4JdT8HVB0P5JzOL66AVd8=";
+    rev = "e1bbfe801986cd7171f36443a1573b0f69f3702d";
+    sha256 = "sha256-DngfmejNFw5pogTo7Ec5aUey2LUQIojvJybLmtCfvVY=";
     fetchSubmodules = true;
   };
 
@@ -40,11 +40,10 @@ stdenv.mkDerivation rec {
   hardeningDisable = [ "all" ];
 
   meta = {
-    broken = (stdenv.isLinux && stdenv.isAarch64);
     description = "A library for computer-vision scientists and targeted for the Multiple View Geometry community";
     homepage = "https://openmvg.readthedocs.io/en/latest/";
     license = lib.licenses.mpl20;
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ mdaiter ];
   };
 }
