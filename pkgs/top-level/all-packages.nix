a6a37794a29f11f768ed7e84c8623d513171c32e
Kyle Sferrazza <git@kylesferrazza.com>
2023-01-19 00:15:46 -0800

    fbthrift: init at 2023.01.30.00
    Co-authored-by: Andrew Hamon <and.ham95@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19528,6 +19528,8 @@ with pkgs;
     autoreconfHook = buildPackages.autoreconfHook269;
   };
 
+  fbthrift = callPackage ../development/libraries/fbthrift { };
+
   fcgi = callPackage ../development/libraries/fcgi { };
 
   fcl = callPackage ../development/libraries/fcl { };
