f8aa3c0fcf393ea26fdf725459b10a0942a7d2c8
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-19 08:42:27 +0100

    python310Packages.tesla-powerwall: add changelog to meta

--- pkgs/development/python-modules/tesla-powerwall/default.nix
+++ pkgs/development/python-modules/tesla-powerwall/default.nix
@@ -41,6 +41,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "API for Tesla Powerwall";
     homepage = "https://github.com/jrester/tesla_powerwall";
+    changelog = "https://github.com/jrester/tesla_powerwall/blob/v${version}/CHANGELOG";
     license = licenses.mit;
     maintainers = with maintainers; [ dotlambda ];
   };
