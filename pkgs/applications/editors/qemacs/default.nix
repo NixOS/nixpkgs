782214c515e9ad7f3befa3549314092fa4e53453
Adam Joseph <adam@westernsemico.com>
2023-01-19 20:10:47 -0800

    put postPatch back after src

--- pkgs/applications/editors/qemacs/default.nix
+++ pkgs/applications/editors/qemacs/default.nix
@@ -15,6 +15,19 @@ stdenv.mkDerivation rec {
     sha256 = "156z4wpj49i6j388yjird5qvrph7hz0grb4r44l4jf3q8imadyrg";
   };
 
+  postPatch = lib.optionalString (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    substituteInPlace Makefile --replace \
+      './fbftoqe $(FONTS) > $@' \
+      '${buildPackages.qemacs}/bin/fbftoqe $(FONTS) > $@'
+  '' + ''
+    substituteInPlace Makefile --replace \
+      '$(HOST_CC) $(LDFLAGS) -o $@ html2png.o $(OBJS)' \
+      '$(CC) $(LDFLAGS) -o $@ html2png.o $(OBJS)'
+    substituteInPlace Makefile --replace \
+      'install -m 755 -s' \
+      "install -m 755 -s --strip-program=''${STRIP}"
+  '';
+
   buildInputs = lib.optionals enableX11 [ libpng libX11 libXext libXv ];
 
   configureFlags = [
@@ -29,19 +42,6 @@ stdenv.mkDerivation rec {
     "HOST_CC=${buildPackages.stdenv.cc}/bin/cc"
     "CC=${stdenv.cc.targetPrefix}cc"
   ];
-
-  postPatch = lib.optionalString (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    substituteInPlace Makefile --replace \
-      './fbftoqe $(FONTS) > $@' \
-      '${buildPackages.qemacs}/bin/fbftoqe $(FONTS) > $@'
-  '' + ''
-    substituteInPlace Makefile --replace \
-      '$(HOST_CC) $(LDFLAGS) -o $@ html2png.o $(OBJS)' \
-      '$(CC) $(LDFLAGS) -o $@ html2png.o $(OBJS)'
-    substituteInPlace Makefile --replace \
-      'install -m 755 -s' \
-      "install -m 755 -s --strip-program=''${STRIP}"
-  '';
 
   preInstall = ''
     mkdir -p $out/bin $out/man
