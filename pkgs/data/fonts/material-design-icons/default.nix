6dd6c2b7b30a112592f194e6ed0e6a3669215132
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2023-01-20 13:24:01 +0800

    material-design-icons: convert to `stdenvNoCC.mkDerivation`

--- pkgs/data/fonts/material-design-icons/default.nix
+++ pkgs/data/fonts/material-design-icons/default.nix
@@ -1,24 +1,30 @@
-{ lib, fetchFromGitHub }:
+{ lib, fetchFromGitHub, stdenvNoCC }:
 
-let
+stdenvNoCC.mkDerivation rec {
+  pname = "material-design-icons";
   version = "7.0.96";
-in fetchFromGitHub {
-  name = "material-design-icons-${version}";
-  owner  = "Templarian";
-  repo   = "MaterialDesign-Webfont";
-  rev    = "v${version}";
 
-  postFetch = ''
-    mkdir -p $out/share/fonts/{eot,truetype,woff,woff2}
-    mv $out/fonts/*.eot $out/share/fonts/eot/
-    mv $out/fonts/*.ttf $out/share/fonts/truetype/
-    mv $out/fonts/*.woff $out/share/fonts/woff/
-    mv $out/fonts/*.woff2 $out/share/fonts/woff2/
-    shopt -s extglob dotglob
-    rm -rf $out/!(share)
-    shopt -u extglob dotglob
+  dontBuild = true;
+
+  src = fetchFromGitHub {
+    owner = "Templarian";
+    repo = "MaterialDesign-Webfont";
+    rev = "v${version}";
+    sha256 = "sha256-d4weq1V8Pk0xJxzTVaiyMhkMHvrH3SBkUzRQT3w7n0U=";
+    sparseCheckout = [ "fonts" ];
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/share/fonts/"{eot,truetype,woff,woff2}
+    cp fonts/*.eot "$out/share/fonts/eot/"
+    cp fonts/*.ttf "$out/share/fonts/truetype/"
+    cp fonts/*.woff "$out/share/fonts/woff/"
+    cp fonts/*.woff2 "$out/share/fonts/woff2/"
+
+    runHook postInstall
   '';
-  sha256 = "sha256-l60LRXLwLh+7Ls3kMTJ5eDTVpVMcqtshMv/ehIk8fCk=";
 
   meta = with lib; {
     description = "7000+ Material Design Icons from the Community";
