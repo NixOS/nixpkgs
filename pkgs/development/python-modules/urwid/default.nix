184e1bd255d611d985fa5f3c7195fdd7464013c5
Robert Schütz <mail@dotlambda.de>
2023-01-20 11:42:04 -0800

    python311Packages.urwid: fix tests
    apply https://github.com/urwid/urwid/pull/517

--- pkgs/development/python-modules/urwid/default.nix
+++ pkgs/development/python-modules/urwid/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , isPy3k
 , glibcLocales
 }:
@@ -14,6 +15,15 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "588bee9c1cb208d0906a9f73c613d2bd32c3ed3702012f51efe318a3f2127eae";
   };
+
+  patches = [
+    # https://github.com/urwid/urwid/pull/517
+    (fetchpatch {
+      name = "python311-compat.patch";
+      url = "https://github.com/urwid/urwid/commit/42c1ed1eeb663179b265bae9b384d7ec11c8a9b5.patch";
+      hash = "sha256-Oz8O/M6AdqbB6C/BB5rtxp8FgdGhZUxkSxKIyq5Dmho=";
+    })
+  ];
 
   # tests need to be able to set locale
   LC_ALL = "en_US.UTF-8";
