78afd4d4727fecbd353d52d0a8a478db9285f024
John D. Boy <2187261+jboynyc@users.noreply.github.com>
2023-01-20 22:14:05 +0100

    minimacy: init at 0.6.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15099,6 +15099,8 @@ with pkgs;
 
   microscheme = callPackage ../development/compilers/microscheme { };
 
+  minimacy = callPackage ../development/compilers/minimacy { };
+
   mint = callPackage ../development/compilers/mint { crystal = crystal_1_2; };
 
   mitama-cpp-result = callPackage ../development/libraries/mitama-cpp-result { };
