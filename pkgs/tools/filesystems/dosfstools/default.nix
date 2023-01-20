c0762a8dee60f5bbfc477b38a0f218ca33a7e03a
Alyssa Ross <hi@alyssa.is>
2023-01-20 21:09:33 +0000

    dosfstools: fix build for FreeBSD

--- pkgs/tools/filesystems/dosfstools/default.nix
+++ pkgs/tools/filesystems/dosfstools/default.nix
@@ -12,11 +12,15 @@ stdenv.mkDerivation rec {
   };
 
   patches = [
-    # macOS build fixes backported from master
+    # macOS and FreeBSD build fixes backported from master
     # TODO: remove on the next release
     (fetchpatch {
       url = "https://github.com/dosfstools/dosfstools/commit/77ffb87e8272760b3bb2dec8f722103b0effb801.patch";
       sha256 = "sha256-xHxIs3faHK/sK3vAVoG8JcTe4zAV+ZtkozWIIFBvPWI=";
+    })
+    (fetchpatch {
+      url = "https://github.com/dosfstools/dosfstools/commit/2d3125c4a74895eae1f66b93287031d340324524.patch";
+      sha256 = "nlIuRDsNjk23MKZL9cZ05odOfTXvsyQaKcv/xEr4c+U=";
     })
   ];
 
@@ -37,7 +41,7 @@ stdenv.mkDerivation rec {
   meta = {
     description = "Utilities for creating and checking FAT and VFAT file systems";
     homepage = "https://github.com/dosfstools/dosfstools";
-    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    platforms = lib.platforms.unix;
     license = lib.licenses.gpl3;
   };
 }
