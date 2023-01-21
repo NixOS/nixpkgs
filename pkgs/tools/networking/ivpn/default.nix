01dec0c6cb6f4e2737a680d7d2b6e92f216dd5e5
Sandro <sandro.jaeckel@gmail.com>
2023-01-21 18:47:00 +0100

    Apply suggestions from code review

--- pkgs/tools/networking/ivpn/default.nix
+++ pkgs/tools/networking/ivpn/default.nix
@@ -7,19 +7,25 @@
 builtins.mapAttrs (pname: attrs: buildGoModule (attrs // rec {
   inherit pname;
   version = "3.10.0";
+
   src = fetchFromGitHub {
     owner = "ivpn";
     repo = "desktop-app";
     rev = "v${version}";
     hash = "sha256-oX1PWIBPDcvBTxstEiN2WosiVUNXJoloppkpcABSi7Y=";
   };
+
   ldflags = [
     "-s"
     "-w"
     "-X github.com/ivpn/desktop-app/daemon/version._version=${version}"
     "-X github.com/ivpn/desktop-app/daemon/version._time=1970-01-01"
   ];
-  postInstall = "mv $out/bin/{${attrs.modRoot},${pname}}";
+
+  postInstall = ''
+    mv $out/bin/{${attrs.modRoot},${pname}}
+  '';
+
   meta = with lib; {
     description = "Official IVPN Desktop app";
     homepage = "https://www.ivpn.net/apps";
@@ -35,6 +41,6 @@ builtins.mapAttrs (pname: attrs: buildGoModule (attrs // rec {
   ivpn-service = {
     modRoot = "daemon";
     vendorHash = "sha256-9Rk6ruMpyWtQe+90kw4F8OLq7/JcDSrG6ufkfcrS4W8=";
-    buildInputs = [wirelesstools];
+    buildInputs = [ wirelesstools ];
   };
 }
