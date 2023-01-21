5f65fb8ed8bcf8c51427c340381e355fedee4d23
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-21 16:21:26 +0000

    opentelemetry-collector: 0.68.0 -> 0.69.1

--- pkgs/tools/misc/opentelemetry-collector/default.nix
+++ pkgs/tools/misc/opentelemetry-collector/default.nix
@@ -12,17 +12,17 @@ let
 in
 buildGoModule rec {
   pname = "opentelemetry-collector";
-  version = "0.68.0";
+  version = "0.69.1";
 
   src = fetchFromGitHub {
     owner = "open-telemetry";
     repo = "opentelemetry-collector";
     rev = "v${version}";
-    sha256 = "sha256-hb+T4sEYagraqiAHjoy6rp+wFtsyuYmK+biX78TV7cA=";
+    sha256 = "sha256-p/epsqaDQNPS2gd3fN/Ny0XtGIeoKI7hdZVI1bqvg4s=";
   };
   # there is a nested go.mod
   sourceRoot = "source/cmd/otelcorecol";
-  vendorSha256 = "sha256-WvZzAZtkkzWjE1TBzR4bCUdux6YyZ1VUl15hKRjNPyE=";
+  vendorHash = "sha256-AIn38bjnYX9gAaKXTyIA7Lv5/oRzy3BMK5Q+9JvapFI=";
 
   preBuild = ''
     # set the build version, can't be done via ldflags
