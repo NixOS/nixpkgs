087b38754583e3f1f661ccd14b9b1084d24c871c
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-21 18:42:57 +0100

    ticker: add changelog to meta

--- pkgs/applications/misc/ticker/default.nix
+++ pkgs/applications/misc/ticker/default.nix
@@ -10,14 +10,16 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "achannarasappa";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-7FSyW71NWmWmBNQ5QUqMJ4x9WLXpm0kvvjdjzx1yk/M=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-7FSyW71NWmWmBNQ5QUqMJ4x9WLXpm0kvvjdjzx1yk/M=";
   };
 
   vendorHash = "sha256-6bosJ2AlbLZ551tCNPmvNyyReFJG+iS3SYUFti2/CAw=";
 
   ldflags = [
-    "-s" "-w" "-X github.com/achannarasappa/ticker/cmd.Version=v${version}"
+    "-s"
+    "-w"
+    "-X github.com/achannarasappa/ticker/cmd.Version=v${version}"
   ];
 
   # Tests require internet
@@ -26,6 +28,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "Terminal stock ticker with live updates and position tracking";
     homepage = "https://github.com/achannarasappa/ticker";
+    changelog = "https://github.com/achannarasappa/ticker/releases/tag/v${version}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ siraben ];
   };
