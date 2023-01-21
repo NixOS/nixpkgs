a4d747ca2fcafb868d99055f5dc7dfe490b99e3f
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-21 11:48:52 +0100

    python310Packages.flake8-bugbear: update disabled

--- pkgs/development/python-modules/flake8-bugbear/default.nix
+++ pkgs/development/python-modules/flake8-bugbear/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   version = "23.1.20";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "PyCQA";
