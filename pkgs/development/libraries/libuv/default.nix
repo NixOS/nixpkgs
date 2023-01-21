c040a98a33b29ba58571702e606c322c22dd15ad
Adam Joseph <adam@westernsemico.com>
2023-01-21 01:18:29 -0800

    libuv: expand the steaming pile of unsandboxable tests
    https://logs.nix.ci/?key=nixos/nixpkgs.209870&attempt_id=0d5c4dad-7738-4204-ad65-25f6ab51a5cf

--- pkgs/development/libraries/libuv/default.nix
+++ pkgs/development/libraries/libuv/default.nix
@@ -21,6 +21,10 @@ stdenv.mkDerivation rec {
       "get_passwd" # passed on NixOS but failed on other Linuxes
       "tcp_writealot" "udp_multicast_join" "udp_multicast_join6" # times out sometimes
       "fs_fstat" # https://github.com/libuv/libuv/issues/2235#issuecomment-1012086927
+
+      # Assertion failed in test/test-tcp-bind6-error.c on line 60: r == UV_EADDRINUSE
+      # Assertion failed in test/test-tcp-bind-error.c on line 99: r == UV_EADDRINUSE
+      "tcp_bind6_error_addrinuse" "tcp_bind_error_addrinuse_listen"
     ] ++ lib.optionals stdenv.isDarwin [
         # Sometimes: timeout (no output), failed uv_listen. Someone
         # should report these failures to libuv team. There tests should
