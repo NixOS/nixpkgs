6854e0e1989aec22adf3acd25e30b2f8711a9c6a
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-21 14:40:58 +0100

    python310Packages.yfinance: 0.2.3 -> 0.2.4
    Diff: https://github.com/ranaroussi/yfinance/compare/refs/tags/0.2.3...0.2.4
    
    Changelog: https://github.com/ranaroussi/yfinance/blob/0.2.4/CHANGELOG.rst

--- pkgs/development/python-modules/yfinance/default.nix
+++ pkgs/development/python-modules/yfinance/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "yfinance";
-  version = "0.2.3";
+  version = "0.2.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "ranaroussi";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-1xO+zz+syuiWeZAxnRIV0va8WOIW2P9elRBtHDk7w1M=";
+    hash = "sha256-WWqRj6It2sYZk1gV3+D94wFbOfzBhIngygalTSwVwaI=";
   };
 
   propagatedBuildInputs = [
