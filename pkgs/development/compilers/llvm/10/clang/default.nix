74ea4fe24fb8fe40732ca4f16a70de36ba01ff61
Robert Scott <code@humanleg.org.uk>
2023-01-21 17:39:01 +0000

    llvm*Packages.clang: mark hardeningUnsupportedFlags fortify3
    it's very unclear what the status of FORTIFY_SOURCE=3 support
    in clang is

--- pkgs/development/compilers/llvm/10/clang/default.nix
+++ pkgs/development/compilers/llvm/10/clang/default.nix
@@ -91,8 +91,9 @@ let
     '';
 
     passthru = {
-      isClang = true;
       inherit libllvm;
+      isClang = true;
+      hardeningUnsupportedFlags = [ "fortify3" ];
     };
 
     meta = llvm_meta // {
