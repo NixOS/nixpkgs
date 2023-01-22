cadb3aff00076cd189473ca9fd2f7ff2049c8ffe
sternenseemann <sternenseemann@systemli.org>
2023-01-22 02:17:29 +0100

    haskellPackages: support hadrian libdir layout

--- pkgs/development/haskell-modules/with-packages-wrapper.nix
+++ pkgs/development/haskell-modules/with-packages-wrapper.nix
@@ -53,7 +53,8 @@ let
   ghcCommand = "${ghc.targetPrefix}${ghcCommand'}";
   ghcCommandCaps= lib.toUpper ghcCommand';
   libDir        = if isHaLVM then "$out/lib/HaLVM-${ghc.version}"
-                  else "$out/lib/${ghc.targetPrefix}${ghc.haskellCompilerName}";
+                  else "$out/lib/${ghc.targetPrefix}${ghc.haskellCompilerName}"
+                    + lib.optionalString (ghc ? hadrian) "/lib";
   docDir        = "$out/share/doc/ghc/html";
   packageCfgDir = "${libDir}/package.conf.d";
   paths         = lib.concatLists (
