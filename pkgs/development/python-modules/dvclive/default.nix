8af586882a1c42bbb172973e48bb949b2457ec6b
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-22 11:14:29 +0100

    python310Packages.dvclive: add changelog to meta

--- pkgs/development/python-modules/dvclive/default.nix
+++ pkgs/development/python-modules/dvclive/default.nix
@@ -41,6 +41,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library for logging machine learning metrics and other metadata in simple file formats";
     homepage = "https://github.com/iterative/dvclive";
+    changelog = "https://github.com/iterative/dvclive/releases/tag/${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
