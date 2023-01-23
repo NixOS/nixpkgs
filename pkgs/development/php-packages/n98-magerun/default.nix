0c3d4309eb5b688898550c0145f745316e07a643
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2023-01-23 11:17:50 +0000

    Added n98-magerun

---
+++ pkgs/development/php-packages/n98-magerun/default.nix
@@ -0,0 +1,36 @@
+{ mkDerivation, fetchurl, makeWrapper, unzip, lib, php }:
+
+let
+  pname = "n98-magerun";
+  version = "2.3.0";
+in
+mkDerivation {
+  inherit pname version;
+
+  src = fetchurl {
+    url = "https://files.magerun.net/n98-magerun-2.3.0.phar";
+    sha256 = "b3e09dafccd4dd505a073c4e8789d78ea3def893cfc475a214e1154bff3aa8e4";
+  };
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    install -D $src $out/libexec/n98-magerun/n98-magerun.phar
+    makeWrapper ${php}/bin/php $out/bin/n98-magerun \
+      --add-flags "$out/libexec/n98-magerun/n98-magerun.phar" \
+      --prefix PATH : ${lib.makeBinPath [ unzip ]}
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "The swiss army knife for Magento developers";
+    license = licenses.mit;
+    homepage = "https://magerun.net/";
+    changelog = "https://magerun.net/category/magerun/";
+    maintainers = with maintainers; [ offline ] ++ teams.php.members;
+  };
+}
