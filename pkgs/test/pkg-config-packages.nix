71e6ba4523a6ae92e26747794ed5374e44910992
Robert Hensing <robert@roberthensing.nl>
2023-01-23 19:08:46 +0100

    tests.pkg-configPackages: Filter out unsupported packages

--- pkgs/test/pkg-config-packages.nix
+++ pkgs/test/pkg-config-packages.nix
@@ -1,6 +1,8 @@
 { lib, pkg-config, pkg-configPackages, runCommand }:
 let
-  allTests = lib.mapAttrs (k: v: if v == null then null else makePkgConfigTest k v) pkg-configPackages;
+  inherit (lib.strings) escapeNixIdentifier;
+
+  allTests = lib.mapAttrs (k: v: if v == null then null else makePkgConfigTestMaybe k v) pkg-configPackages;
 
   # nix-build rejects attribute names with periods
   # This will build those regardless.
@@ -9,6 +11,26 @@ let
   } ''
     touch $out
   '';
+
+  makePkgConfigTestMaybe = moduleName: pkg:
+    if ! lib.isDerivation pkg
+    then
+      throw "pkg-config module `${escapeNixIdentifier moduleName}` is not defined to be a derivation. Please check the attribute value for `${escapeNixIdentifier moduleName}` in `pkgs/top-level/pkg-config-packages.nix` in Nixpkgs."
+
+    else if ! pkg?meta.unsupported
+    then
+      throw "pkg-config module `${escapeNixIdentifier moduleName}` does not have a `meta.unsupported` attribute. This can't be right. Please check the attribute value for `${escapeNixIdentifier moduleName}` in `pkgs/top-level/pkg-config-packages.nix` in Nixpkgs."
+
+    else if pkg.meta.unsupported
+    then
+      # We return `null` instead of doing a `filterAttrs`, because with
+      # `filterAttrs` the evaluator would not be able to return the attribute
+      # set without first evaluating all of the attribute _values_. This would
+      # be rather expensive, and severly slow down the use case of getting a
+      # single test, which we want to do in `passthru.tests`, or interactively.
+      null
+
+    else makePkgConfigTest moduleName pkg;
 
   makePkgConfigTest = moduleName: pkg: runCommand "check-pkg-config-${moduleName}" {
     nativeBuildInputs = [ pkg-config ];
