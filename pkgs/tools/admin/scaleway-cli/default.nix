ae4b48d349dee89db8bb3fb9e03fc017ec782c3e
techknowlogick <techknowlogick@gitea.io>
2023-01-24 12:00:43 -0500

    scaleway-cli: 2.9.0 -> 2.10.0
    Diff: https://github.com/scaleway/scaleway-cli/compare/v2.9.0...v2.10.0

--- pkgs/tools/admin/scaleway-cli/default.nix
+++ pkgs/tools/admin/scaleway-cli/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "scaleway-cli";
-  version = "2.9.0";
+  version = "2.10.0";
 
   src = fetchFromGitHub {
     owner = "scaleway";
     repo = "scaleway-cli";
     rev = "v${version}";
-    sha256 = "sha256-Sh7K0PgjX2/vio2E8/qdiP+WC2SoGpS9oU4UizV+sOs=";
+    sha256 = "sha256-Xy4arniOrCln58swFExd0ZoU6wymKYFinReMs7YudeY=";
   };
 
-  vendorHash = "sha256-sYvq502huyCxI4zpPLfPVyyKqTclrfxL9idCGW4Xops=";
+  vendorHash = "sha256-BNiF90jovSyCcRGfv6Kya4fLpqV2VF2Qa3QqdyQio5c=";
 
   ldflags = [
     "-w"
