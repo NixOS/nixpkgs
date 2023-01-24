09887a1f134e969146e050e8bd602fe97c368f4f
Aaron Jheng <wentworth@outlook.com>
2023-01-24 11:50:24 +0000

    python3Packages.ipython: 8.4.0 -> 8.10.0

--- pkgs/development/python-modules/ipython/default.nix
+++ pkgs/development/python-modules/ipython/default.nix
@@ -2,7 +2,6 @@
 , stdenv
 , buildPythonPackage
 , fetchPypi
-, fetchpatch
 , pythonOlder
 
 # Build dependencies
@@ -28,25 +27,14 @@
 
 buildPythonPackage rec {
   pname = "ipython";
-  version = "8.4.0";
+  version = "8.10.0";
   format = "pyproject";
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f2db3a10254241d9b447232cec8b424847f338d9d36f9a577a6192c332a46abd";
+    hash = "sha256-sTodbB9YGL04jbU7cQfRdFQSmnDeK4dIHVVdrt5etJ4=";
   };
-
-  patches = [
-    (fetchpatch {
-      # The original URL might not be very stable, so let's prefer a copy.
-      urls = [
-        "https://raw.githubusercontent.com/bmwiedemann/openSUSE/9b35e4405a44aa737dda623a7dabe5384172744c/packages/p/python-ipython/ipython-pr13714-xxlimited.patch"
-        "https://github.com/ipython/ipython/pull/13714.diff"
-      ];
-      sha256 = "XPOcBo3p8mzMnP0iydns9hX8qCQXTmRgRD0TM+FESCI=";
-    })
-  ];
 
   nativeBuildInputs = [
     setuptools
