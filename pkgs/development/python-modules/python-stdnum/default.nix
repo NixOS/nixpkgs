c73fbbd79ee5875eea13d80b1cae00f6e8db44e3
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-24 20:45:14 +0100

    python310Packages.python-stdnum: add changelog to meta

--- pkgs/development/python-modules/python-stdnum/default.nix
+++ pkgs/development/python-modules/python-stdnum/default.nix
@@ -28,6 +28,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python module to handle standardized numbers and codes";
     homepage = "https://arthurdejong.org/python-stdnum/";
+    changelog = "https://github.com/arthurdejong/python-stdnum/blob/${version}/ChangeLog";
     license = licenses.lgpl21Plus;
     maintainers = with maintainers; [ johbo ];
   };
