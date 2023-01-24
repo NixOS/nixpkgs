fa3c358e9e3e64127de0d85197ea292c3b9e389c
amesgen <amesgen@amesgen.de>
2023-01-24 23:12:18 +0100

    gh-actions-cache: init at 1.0.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1397,6 +1397,8 @@ with pkgs;
 
   gfshare = callPackage ../tools/security/gfshare { };
 
+  gh-actions-cache = callPackage ../tools/misc/gh-actions-cache { };
+
   gh-cal = callPackage ../tools/misc/gh-cal {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
