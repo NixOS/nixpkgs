730163b3ed11ec6e988ace1a803894ba27199b56
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-01-25 00:08:55 -0600

    tut: 1.0.34 -> 2.0.0

--- pkgs/applications/misc/tut/default.nix
+++ pkgs/applications/misc/tut/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "tut";
-  version = "1.0.34";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "RasmusLindroth";
     repo = pname;
     rev = version;
-    sha256 = "sha256-AnuPTv9W+2yDcM803DZaNIn4S7A78JEv6S8pA18whVA=";
+    sha256 = "sha256-Oypl80UsYRRSIMvHDPSS6rrvzjlxZ/UKDNDGc2Yan+w=";
   };
 
-  vendorHash = "sha256-go7eZHhrQ1ZcLOn56a3Azn3eRyAesAkgLabPbwzKtds=";
+  vendorHash = "sha256-qeYgkF9sIJ0slRarXbCHZ+1JmtZwXDnrJIpRKGOoW5Q=";
 
   meta = with lib; {
     description = "A TUI for Mastodon with vim inspired keys";
