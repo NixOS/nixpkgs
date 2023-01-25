b7c4364f4b18e6ad370e6b1d6abc945298f87f64
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-25 09:24:53 +0100

    python310Packages.google-cloud-securitycenter: update disabled

--- pkgs/development/python-modules/google-cloud-securitycenter/default.nix
+++ pkgs/development/python-modules/google-cloud-securitycenter/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   version = "1.18.2";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
