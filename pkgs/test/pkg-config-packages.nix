2d4e78fb8bd3420d31879f5669160b1a62d47fb0
Robert Hensing <robert@roberthensing.nl>
2023-01-25 11:54:02 +0100

    tests.pkg-configPackages: Copy meta attributes for licensing concerns

--- pkgs/test/pkg-config-packages.nix
+++ pkgs/test/pkg-config-packages.nix
@@ -43,6 +43,24 @@ let
     nativeBuildInputs = [ pkg-config ];
     buildInputs = [ pkg ];
     inherit moduleName;
+    meta = {
+      description = "Test whether ${pkg.name} exposes pkg-config module ${moduleName}";
+    }
+    # Make sure licensing info etc is preserved, as this is a concern for e.g. cache.nixos.org,
+    # as hydra can't check this meta info in dependencies.
+    # The test itself is just Nixpkgs, with MIT license.
+    // builtins.intersectAttrs
+        {
+          available = throw "unused";
+          broken = throw "unused";
+          insecure = throw "unused";
+          license = throw "unused";
+          maintainers = throw "unused";
+          platforms = throw "unused";
+          unfree = throw "unused";
+          unsupported = throw "unused";
+        }
+        pkg.meta;
   } ''
     echo "checking pkg-config module $moduleName in $buildInputs"
     set +e
