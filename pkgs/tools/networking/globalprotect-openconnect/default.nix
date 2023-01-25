eafe34f129cb4190b74bd585afec37efe1d039e1
Guangqing Chen <hi@goushi.me>
2023-01-25 17:35:47 +0800

    globalprotect-openconnect: 1.4.8 -> 1.4.9

--- pkgs/tools/networking/globalprotect-openconnect/default.nix
+++ pkgs/tools/networking/globalprotect-openconnect/default.nix
@@ -1,22 +1,19 @@
 { stdenv, lib, fetchFromGitHub
-, cmake, qtwebsockets, qtwebengine, wrapQtAppsHook, openconnect
+, cmake, qtwebsockets, qtwebengine, qtkeychain, wrapQtAppsHook, openconnect
 }:
 
 stdenv.mkDerivation rec {
   pname = "globalprotect-openconnect";
-  version = "1.4.8";
+  version = "1.4.9";
 
-  src = fetchFromGitHub {
-    owner = "yuezk";
-    repo = "GlobalProtect-openconnect";
-    fetchSubmodules = true;
-    rev = "v${version}";
-    sha256 = "sha256-PQAlGeHVayImKalCNv2SwPcxD0ts4BVSqeo1hKYmnMA=";
+  src = builtins.fetchTarball {
+    url = "https://github.com/yuezk/GlobalProtect-openconnect/releases/download/v${version}/globalprotect-openconnect-${version}.tar.gz";
+    sha256 = "0b7s3gf0gznlpmf9dxfq77254zlxmpajhzzn3scrdrvf413sjl0f";
   };
 
   nativeBuildInputs = [ cmake wrapQtAppsHook ];
 
-  buildInputs = [ openconnect qtwebsockets qtwebengine ];
+  buildInputs = [ openconnect qtwebsockets qtwebengine qtkeychain ];
 
   patchPhase = ''
     substituteInPlace GPService/gpservice.h \
