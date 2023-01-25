67e898c12e0d70b19cd699cf87ef4890c0295bec
Nathan Viets <nathan.g.viets@gmail.com>
2023-01-24 23:07:55 -0600

    nng: 1.5.2 -> 1.6.0-prerelease and rPackages.nanonext dependency

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -362,6 +362,7 @@ let
     mvabund = [ pkgs.gsl ];
     mwaved = [ pkgs.fftw.dev ];
     mzR = with pkgs; [ zlib netcdf ];
+    nanonext = with pkgs; [ mbedtls nng ];
     ncdf4 = [ pkgs.netcdf ];
     nloptr = with pkgs; [ nlopt pkg-config libiconv ];
     n1qn1 = [ pkgs.gfortran ];
