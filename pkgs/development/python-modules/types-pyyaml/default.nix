5e3079f21aa2aa897c5ae3b870f4b289e22c8a15
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-25 07:49:30 +0000

    python310Packages.types-pyyaml: 6.0.12.2 -> 6.0.12.3

--- pkgs/development/python-modules/types-pyyaml/default.nix
+++ pkgs/development/python-modules/types-pyyaml/default.nix
@@ -5,13 +5,13 @@
 
 buildPythonPackage rec {
   pname = "types-pyyaml";
-  version = "6.0.12.2";
+  version = "6.0.12.3";
   format = "setuptools";
 
   src = fetchPypi {
     pname = "types-PyYAML";
     inherit version;
-    sha256 = "sha256-aECBmHHJLe6+aiBn+4AMEbigY2MutOPnVZFOerNgToM=";
+    sha256 = "sha256-F84Xs+rY8G5BajsdW43cbLgqQiuyACVN2LRpQ0sEX/w=";
   };
 
   # Module doesn't have tests
