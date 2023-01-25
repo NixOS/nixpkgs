e21ccacb5bdc859a5de1b90e5d7db0aab8791737
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-25 07:02:07 +0000

    vorta: 0.8.9 -> 0.8.10

--- pkgs/applications/backup/vorta/default.nix
+++ pkgs/applications/backup/vorta/default.nix
@@ -8,13 +8,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "vorta";
-  version = "0.8.9";
+  version = "0.8.10";
 
   src = fetchFromGitHub {
     owner = "borgbase";
     repo = "vorta";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-5RZXHMov3CX0zRprs7sgz0+cGEdESLrg4I2glRnTkcU=";
+    sha256 = "sha256-okxZ1kALB5l94ShStAJLfzD37MLhob8MCzhbqfVISkw=";
   };
 
   nativeBuildInputs = [ wrapQtAppsHook ];
