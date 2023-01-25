5128a000ada7cf9425d8e62676796d9accc4ce76
Richard Davis <davisrichard437@gmail.com>
2023-01-24 20:30:46 -0500

    gscreenshot: init at 3.4.0
    https://github.com/thenaterhood/gscreenshot/releases/tag/v3.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30623,6 +30623,8 @@ with pkgs;
 
   grisbi = callPackage ../applications/office/grisbi { gtk = gtk3; };
 
+  gscreenshot = callPackage ../applications/graphics/gscreenshot { };
+
   gtkpod = callPackage ../applications/audio/gtkpod { };
 
   q4wine = libsForQt5.callPackage ../applications/misc/q4wine { };
