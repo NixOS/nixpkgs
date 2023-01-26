8cc7194fd0cbd29477468cd5ede36566d64da912
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-26 02:28:32 +0000

    python310Packages.wasabi: 1.1.0 -> 1.1.1

--- pkgs/development/python-modules/wasabi/default.nix
+++ pkgs/development/python-modules/wasabi/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "wasabi";
-  version = "1.1.0";
+  version = "1.1.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-RaYTplXhFKsnL1rxRMNR+VT92S2ym3z6buIQuCwZeeU=";
+    sha256 = "sha256-9e58YJAngRvRbmIPL9enMZRmAFhI5BsFGmIFOrj9cNY=";
   };
 
   nativeCheckInputs = [
