9d663f7fcd1fbae2c1895151d8a98234593a6d39
Karel Kočí <cynerd@email.cz>
2023-01-26 16:19:05 +0100

    setuptools-rust: fix cross compilation
    The setuptools-rust requires some environment variables to really
    perform cross build, otherwise it just builds for build platform.
    
    This adds setup hook that introduces these environment variables.
    
    There are three variables.
    The PYO3_CROSS_LIB_DIR has to point to the target's Python library
    directory. This has to be directory for the target not for the build or
    host. We have to choose the correct target Python. I am unsure how to do
    that simply in nixpkgs and this this implementations just delays this
    and waits for the correct Python when package using this hook is build.
    The CARGO_BUILD_TARGET triggers cross compilation in setuptools-rust.
    This is simply the Rust target specification.
    The CARGO_TARGET_*_LINKER variable should not be essentially required
    but setuptools-rust probably mangles the Rust build environment somewhat
    and that results to the missing linker. By explicitly specifying it
    using the environment variable we force the correct linker.

--- pkgs/development/interpreters/python/hooks/default.nix
+++ pkgs/development/interpreters/python/hooks/default.nix
@@ -160,6 +160,20 @@ in {
       };
     } ./setuptools-check-hook.sh) {};
 
+    setuptoolsRustBuildHook = callPackage ({ makePythonHook, setuptools-rust, rust }:
+      makePythonHook {
+        name = "setuptools-rust-setup-hook";
+        propagatedBuildInputs = [ setuptools-rust ];
+        substitutions = {
+          pyLibDir = "${python}/lib/${python.libPrefix}";
+          cargoBuildTarget = rust.toRustTargetSpec stdenv.hostPlatform;
+          cargoLinkerVar = lib.toUpper (
+              builtins.replaceStrings ["-"] ["_"] (
+                rust.toRustTarget stdenv.hostPlatform));
+          targetLinker = "${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc";
+        };
+      } ./setuptools-rust-hook.sh) {};
+
   unittestCheckHook = callPackage ({ makePythonHook }:
     makePythonHook {
       name = "unittest-check-hook";
