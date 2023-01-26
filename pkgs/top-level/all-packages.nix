89bd9fcc57e5d2f650ca66abc2f8a8c071fce0f1
Philipp Schuster <phip1611@gmail.com>
2023-01-26 14:41:39 +0100

    ttfb: init at 1.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12648,6 +12648,8 @@ with pkgs;
 
   oysttyer = callPackage ../applications/networking/instant-messengers/oysttyer { };
 
+  ttfb = callPackage ../development/tools/ttfb { };
+
   twilight = callPackage ../tools/graphics/twilight {
     libX11 = xorg.libX11;
   };
