827995378da3eee0f783c36de36cbed6cda2ca65
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-26 13:51:57 +0100

    python310Packages.databricks-sql-connector: add changelog to meta

--- pkgs/development/python-modules/databricks-sql-connector/default.nix
+++ pkgs/development/python-modules/databricks-sql-connector/default.nix
@@ -6,6 +6,7 @@
 , pyarrow
 , poetry-core
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
@@ -13,11 +14,13 @@ buildPythonPackage rec {
   version = "2.2.1";
   format = "pyproject";
 
+  disabled = pythonOlder "3.7";
+
   src = fetchFromGitHub {
     owner = "databricks";
     repo = "databricks-sql-python";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-EMLUXGeVGIXFeaMvaJ+crivRZtOt7W/LCycIO2gwqXA=";
+    hash = "sha256-EMLUXGeVGIXFeaMvaJ+crivRZtOt7W/LCycIO2gwqXA=";
   };
 
   postPatch = ''
@@ -39,11 +42,18 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  pytestFlagsArray = [ "tests/unit" ];
+  pytestFlagsArray = [
+    "tests/unit"
+  ];
+
+  pythonImportsCheck = [
+    "databricks"
+  ];
 
   meta = with lib; {
     description = "Databricks SQL Connector for Python";
     homepage = "https://docs.databricks.com/dev-tools/python-sql-connector.html";
+    changelog = "https://github.com/databricks/databricks-sql-python/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ harvidsen ];
   };
