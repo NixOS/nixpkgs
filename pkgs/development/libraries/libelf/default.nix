NixOS/nixpkgs

commit fc04308b0adc420fdf8ef9533770ef45edb09eee

libelf: fix build on NetBSD
This is a better solution than disabling native language entirely,
like on Darwin.

pkgs/development/libraries/libelf/default.nix
@@ -1,5 +1,5 @@
 { lib, stdenv
-, fetchurl, autoreconfHook, gettext
+, fetchurl, autoreconfHook, gettext, netbsd
 }:
 
 # Note: this package is used for bootstrapping fetchurl, and thus
@@ -33,7 +33,8 @@ stdenv.mkDerivation rec {
        # on Darwin, so disable NLS for now.
     ++ lib.optional stdenv.hostPlatform.isDarwin "--disable-nls";
 
-  nativeBuildInputs = [ gettext ]
+  nativeBuildInputs =
+    if stdenv.hostPlatform.isNetBSD then [ netbsd.gencat ] else [ gettext ]
        # Need to regenerate configure script with newer version in order to pass
        # "mr_cv_target_elf=yes", but `autoreconfHook` brings in `makeWrapper`
        # which doesn't work with the bootstrapTools bash, so can only do this

commit 32a79cb150f2da0d7b3d12869651d5a3e60f0c71

libelf: use autoreconfHook for NetBSD cross
This was a precedence mistake when I added this conditional — the ++
that adds autoreconfHook only took effect in the else branch.

Fixes: fc04308b0ad ("libelf: fix build on NetBSD")

pkgs/development/libraries/libelf/default.nix
@@ -42,7 +42,7 @@ stdenv.mkDerivation rec {
 
   strictDeps = true;
   nativeBuildInputs =
-    if stdenv.hostPlatform.isNetBSD then [ netbsd.gencat ] else [ gettext ]
+    (if stdenv.hostPlatform.isNetBSD then [ netbsd.gencat ] else [ gettext ])
        # Need to regenerate configure script with newer version in order to pass
        # "mr_cv_target_elf=yes", but `autoreconfHook` brings in `makeWrapper`
        # which doesn't work with the bootstrapTools bash, so can only do this