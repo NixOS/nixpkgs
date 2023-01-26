7c9bdd8f66764dd0343557e42530fe9945c19d0e
Sandro Jäckel <sandro.jaeckel@sap.com>
2023-01-26 17:08:55 +0100

    buildGoModule: forward prePatch, postPatch and postBuild to vendoring
    to be more prediactable

--- pkgs/build-support/go/module.nix
+++ pkgs/build-support/go/module.nix
@@ -83,9 +83,12 @@ let
     inherit (args) src;
     inherit (go) GOOS GOARCH;
 
+    prePatch = args.prePatch or "";
     patches = args.patches or [];
     patchFlags = args.patchFlags or [];
+    postPatch = args.postPatch or "";
     preBuild = args.preBuild or "";
+    postBuild = args.postBuild or "";
     sourceRoot = args.sourceRoot or "";
 
     GO111MODULE = "on";
