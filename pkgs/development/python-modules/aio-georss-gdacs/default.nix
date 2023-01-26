4ea53da6b20b0528114dbf0e7d0eccffff8c07b8
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-26 21:13:29 +0100

    python310Packages.aio-georss-gdacs: 0.7 -> 0.8
    Changelog: https://github.com/exxamalte/python-aio-georss-gdacs/releases/tag/v0.8

--- pkgs/development/python-modules/aio-georss-gdacs/default.nix
+++ pkgs/development/python-modules/aio-georss-gdacs/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "aio-georss-gdacs";
-  version = "0.7";
+  version = "0.8";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "exxamalte";
     repo = "python-aio-georss-gdacs";
     rev = "refs/tags/v${version}";
-    hash = "sha256-6z0l0PcFTQgOBj8cBgBMPJIqWG53u7h4WbYkBqU4FNE=";
+    hash = "sha256-1mpOWd4Z2gTQtRewWfZsfEtmS6i5uMPAMTlC8UpawxM=";
   };
 
   propagatedBuildInputs = [
