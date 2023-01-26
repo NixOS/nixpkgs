38871d731539ef3f9adcf10f80e1bf841c5264f6
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-26 15:40:45 +0000

    python310Packages.pulumi-aws: 5.27.0 -> 5.28.0

--- pkgs/development/python-modules/pulumi-aws/default.nix
+++ pkgs/development/python-modules/pulumi-aws/default.nix
@@ -12,7 +12,7 @@
 buildPythonPackage rec {
   pname = "pulumi-aws";
   # Version is independant of pulumi's.
-  version = "5.27.0";
+  version = "5.28.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "pulumi";
     repo = "pulumi-aws";
     rev = "refs/tags/v${version}";
-    hash = "sha256-JNiklXAAyHFLdspglGLokmHOiffDS1I5igsmupla1Qo=";
+    hash = "sha256-7lO3GbVPG70u+PzS/22d6i6xIRXiTMc/iUXrZurD2LU=";
   };
 
   sourceRoot = "${src.name}/sdk/python";
