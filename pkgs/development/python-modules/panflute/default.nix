1d3c447b22869c10c993f58a0bc7f43e625492d6
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-26 09:42:08 +0000

    python310Packages.panflute: 2.2.3 -> 2.3.0

--- pkgs/development/python-modules/panflute/default.nix
+++ pkgs/development/python-modules/panflute/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec{
   pname = "panflute";
-  version = "2.2.3";
+  version = "2.3.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-AJMz+gt8Wa45aFZSLm15RjiiJlJnkWC4Lobk8o8Pu8Y=";
+    hash = "sha256-zv2d/EjM2XMqU9tXYQcB0igG2jl6ipflzI3AcLVYZco=";
   };
 
   propagatedBuildInputs = [
