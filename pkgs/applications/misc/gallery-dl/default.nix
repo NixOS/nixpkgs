1cd2689691cfd5b10d2e43142d0f22f6b9663cac
Mario Rodas <marsam@users.noreply.github.com>
2023-01-27 04:20:00 +0000

    gallery-dl: 1.24.4 -> 1.24.5
    https://github.com/mikf/gallery-dl/releases/tag/v1.24.5

--- pkgs/applications/misc/gallery-dl/default.nix
+++ pkgs/applications/misc/gallery-dl/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonApplication rec {
   pname = "gallery-dl";
-  version = "1.24.4";
+  version = "1.24.5";
   format = "setuptools";
 
   src = fetchPypi {
     inherit version;
     pname = "gallery_dl";
-    sha256 = "sha256-g+nbHpbo6O4zoDJm6MRwCXY9pM73NV7RKR59s+8LM10=";
+    sha256 = "sha256-P71JiGI9PpWMAlgk5TwQa/h3AUEZSEZ6/MahY+IIy9M=";
   };
 
   propagatedBuildInputs = [
