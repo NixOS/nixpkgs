63ef758151c7edb219398dd6b649ceabd0ce5fe2
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-27 20:28:28 +0000

    python310Packages.pydevccu: 0.1.4 -> 0.1.5

--- pkgs/development/python-modules/pydevccu/default.nix
+++ pkgs/development/python-modules/pydevccu/default.nix
@@ -6,7 +6,7 @@
 
 buildPythonPackage rec {
   pname = "pydevccu";
-  version = "0.1.4";
+  version = "0.1.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
     owner = "danielperna84";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-VneuAnt9HkIRBeK2T71MIHbZbz6zY1vVy5vOYZ+82gM=";
+    hash = "sha256-scgWD40jjt8qPGygV11Rfd4Z1voBlErScabLLcmQqGg=";
   };
 
   # Module has no tests
