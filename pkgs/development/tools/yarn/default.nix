0e6e5de21e2ee9f47b53cd5e4c492e74a2300d28
Mario Rodas <marsam@users.noreply.github.com>
2023-01-27 04:20:00 +0000

    yarn: install bash completions

--- pkgs/development/tools/yarn/default.nix
+++ pkgs/development/tools/yarn/default.nix
@@ -1,5 +1,13 @@
-{ lib, stdenv, nodejs, fetchzip, testers, yarn }:
+{ lib, stdenv, nodejs, fetchFromGitHub, fetchzip, testers, yarn }:
 
+let
+  completion = fetchFromGitHub {
+    owner = "dsifford";
+    repo = "yarn-completion";
+    rev = "v0.17.0";
+    hash = "sha256-z7KPXeYPPRuaEPxgY6YqsLt9n8cSsW3n2FhOzVde1HU=";
+  };
+in
 stdenv.mkDerivation rec {
   pname = "yarn";
   version = "1.22.19";
@@ -12,10 +20,11 @@ stdenv.mkDerivation rec {
   buildInputs = [ nodejs ];
 
   installPhase = ''
-    mkdir -p $out/{bin,libexec/yarn/}
+    mkdir -p $out/{bin,libexec/yarn/,share/bash-completion/completions/}
     cp -R . $out/libexec/yarn
     ln -s $out/libexec/yarn/bin/yarn.js $out/bin/yarn
     ln -s $out/libexec/yarn/bin/yarn.js $out/bin/yarnpkg
+    ln -s ${completion}/yarn-completion.bash $out/share/bash-completion/completions/yarn.bash
   '';
 
   passthru.tests = testers.testVersion { package = yarn; };
