0288ee587fa2a977b7d7cc6c3b452a76f26b1cb7
Naïm Favier <n@monade.li>
2023-01-27 16:45:55 +0100

    maintainers: clarify what fields are required

--- lib/tests/maintainers.nix
+++ lib/tests/maintainers.nix
@@ -1,5 +1,6 @@
 # to run these tests (and the others)
 # nix-build nixpkgs/lib/tests/release.nix
+# These tests should stay in sync with the comment in maintainers/maintainers-list.nix
 { # The pkgs used for dependencies for the testing itself
   pkgs ? import ../.. {}
 , lib ? pkgs.lib
