d28f3a66fcbe895849afb4a07ed3fadf38ebad8f
fortuneteller2k <lythe1107@gmail.com>
2023-01-27 21:21:11 +0800

    python3Packages.dasbus: 1.7 -> unstable-11-10-2022
    change format to pyproject
    add hatchling as a build time dep
    test with pytestCheckHook

--- pkgs/development/python-modules/dasbus/default.nix
+++ pkgs/development/python-modules/dasbus/default.nix
@@ -1,16 +1,20 @@
-{ lib, buildPythonPackage, fetchPypi, pygobject3, dbus }:
+{ lib, buildPythonPackage, fetchFromGitHub, pygobject3, dbus, hatchling, pytestCheckHook }:
 
 buildPythonPackage rec {
   pname = "dasbus";
-  version = "1.7";
+  version = "unstable-11-10-2022";
+  format = "pyproject";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-qIUNhBrf6O5fe7n4LPRJq5tJUNwGM4lwcXGODQA2tvY=";
+  src = fetchFromGitHub {
+    owner = "rhinstaller";
+    repo = pname;
+    rev = "64b6b4d9e37cd7e0cbf4a7bf75faa7cdbd01086d";
+    hash = "sha256-TmhhDrfpP+nUErAd7dUb+RtGBRtWwn3bYOoIqa0VRoc=";
   };
 
+  nativeBuildInputs = [ hatchling ];
   propagatedBuildInputs = [ pygobject3 ];
-  nativeCheckInputs = [ dbus ];
+  nativeCheckInputs = [ dbus pytestCheckHook ];
 
   meta = with lib; {
     homepage = "https://github.com/rhinstaller/dasbus";
