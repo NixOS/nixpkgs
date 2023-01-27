b75176dfdc86ad63c40e52bb60c9c843d87e2041
tricktron <tgagnaux@gmail.com>
2023-01-27 01:45:01 +0100

    lima-bin: init binary derivation at 0.14.2 (#209171)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37352,6 +37352,8 @@ with pkgs;
     inherit (darwin) sigtool;
   };
 
+  lima-bin = callPackage ../applications/virtualization/lima/bin.nix { };
+
   logtop = callPackage ../tools/misc/logtop { };
 
   igraph = callPackage ../development/libraries/igraph { };
