a163f260776bd43d6ce95bc58ef257bc86225d17
Mario Rodas <marsam@users.noreply.github.com>
2023-01-27 04:20:00 +0000

    python310Packages.sphinx-thebe: 0.2.0 -> 0.2.1
    https://github.com/executablebooks/sphinx-thebe/releases/tag/v0.2.1

--- pkgs/development/python-modules/sphinx-thebe/default.nix
+++ pkgs/development/python-modules/sphinx-thebe/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "sphinx-thebe";
-  version = "0.2.0";
+  version = "0.2.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-CHZ6WacLlFhpGyujW7b2KkRSlGmUR3rlg5ulPMsKUoc=";
+    sha256 = "f4c8c1542054f991b73fcb28c4cf21697e42aba2f83f22348c1c851b82766583";
   };
 
   propagatedBuildInputs = [ sphinx ];
