e1e6e3806a62f0da6a5d576000693407ac207b84
rewine <lhongxu@outlook.com>
2023-01-27 23:44:49 +0800

    deepin.gio-qt: enable build docs

--- pkgs/desktops/deepin/library/gio-qt/default.nix
+++ pkgs/desktops/deepin/library/gio-qt/default.nix
@@ -6,7 +6,9 @@
 , wrapQtAppsHook
 , glibmm
 , doxygen
-, buildDocs ? false
+, qttools
+, qtbase
+, buildDocs ? true
 }:
 
 stdenv.mkDerivation rec {
@@ -20,17 +22,24 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-dlY1CTlXywgGZUonBBe3cDwx8h2xXrPY6Ft/D59nlug=";
   };
 
-  nativeBuildInputs = [ cmake pkg-config wrapQtAppsHook ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    wrapQtAppsHook
+  ] ++ lib.optional buildDocs [ doxygen qttools.dev ];
 
   cmakeFlags = [
     "-DCMAKE_INSTALL_LIBDIR=lib"
     "-DPROJECT_VERSION=${version}"
-  ]
-  ++ lib.optional (!buildDocs) [ "-DBUILD_DOCS=OFF" ];
-
-  buildInputs = lib.optional buildDocs doxygen;
+  ] ++ lib.optional (!buildDocs) [ "-DBUILD_DOCS=OFF" ];
 
   propagatedBuildInputs = [ glibmm ];
+
+  preConfigure = ''
+    # qt.qpa.plugin: Could not find the Qt platform plugin "minimal"
+    # A workaround is to set QT_PLUGIN_PATH explicitly
+    export QT_PLUGIN_PATH=${qtbase.bin}/${qtbase.qtPluginPrefix}
+  '';
 
   meta = with lib; {
     description = "Gio wrapper for Qt applications";
