NixOS/nixpkgs

commit 4553e6237ff3eb7ddb44897d4782d9d1314c4551

python3Packages.aiohttp: Relax charset-normalizer constraint
and set format.

pkgs/development/python-modules/aiohttp/default.nix
@@ -30,6 +30,8 @@
 buildPythonPackage rec {
   pname = "aiohttp";
   version = "3.8.3";
+  format = "pyproject";
+
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
@@ -39,6 +41,9 @@ buildPythonPackage rec {
 
   postPatch = ''
     sed -i '/--cov/d' setup.cfg
+
+    substituteInPlace setup.cfg \
+      --replace "charset-normalizer >=2.0, < 3.0" "charset-normalizer >=2.0, < 4.0"
   '';
 
   propagatedBuildInputs = [

commit 5a5376ac65b11a5ababa348163266f16312bd954

python310Packages.aiohttp: add missing inputs: setuptools, wheel
Builds of aiohttpd have been failing on powerpc64le since
4553e6237ff3eb7ddb44897d4782d9d1314c4551 with:

```
python3.10-aiohttp>   File "/nix/store/mhw4rs4xb20acvn55iw5i5k5rhaqidnq-python3-3.10.9/lib/python3.10/importlib/__init__.py", line 126, in import_module
python3.10-aiohttp>     return _bootstrap._gcd_import(name[level:], package, level)
python3.10-aiohttp>   File "<frozen importlib._bootstrap>", line 1050, in _gcd_import
python3.10-aiohttp>   File "<frozen importlib._bootstrap>", line 1027, in _find_and_load
python3.10-aiohttp>   File "<frozen importlib._bootstrap>", line 992, in _find_and_load_unlocked
python3.10-aiohttp>   File "<frozen importlib._bootstrap>", line 241, in _call_with_frames_removed
python3.10-aiohttp>   File "<frozen importlib._bootstrap>", line 1050, in _gcd_import
python3.10-aiohttp>   File "<frozen importlib._bootstrap>", line 1027, in _find_and_load
python3.10-aiohttp>   File "<frozen importlib._bootstrap>", line 1004, in _find_and_load_unlocked
python3.10-aiohttp> ModuleNotFoundError: No module named 'setuptools'
python3.10-aiohttp> note: keeping build directory '/nix/tmp/nix-build-python3.10-aiohttp-3.8.3.drv-5'
```

This commit adds `setuptools` and `wheel` as `nativeBuildInputs` to
fix this.

pkgs/development/python-modules/aiohttp/default.nix
@@ -3,6 +3,8 @@
 , buildPythonPackage
 , fetchPypi
 , pythonOlder
+, setuptools
+, wheel
 # install_requires
 , attrs
 , charset-normalizer
@@ -45,6 +47,11 @@ buildPythonPackage rec {
     substituteInPlace setup.cfg \
       --replace "charset-normalizer >=2.0, < 3.0" "charset-normalizer >=2.0, < 4.0"
   '';
+
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
 
   propagatedBuildInputs = [
     attrs