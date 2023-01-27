fc5d164f46f6bed9c79d272a14a918fe94dd5314
Mario Rodas <marsam@users.noreply.github.com>
2023-01-27 04:20:00 +0000

    papis: add marsam to maintainers

--- pkgs/development/python-modules/papis/default.nix
+++ pkgs/development/python-modules/papis/default.nix
@@ -114,6 +114,6 @@ buildPythonPackage rec {
     homepage = "https://papis.readthedocs.io/";
     changelog = "https://github.com/papis/papis/blob/v${version}/CHANGELOG.md";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ nico202 teto ];
+    maintainers = with maintainers; [ nico202 teto marsam ];
   };
 }
