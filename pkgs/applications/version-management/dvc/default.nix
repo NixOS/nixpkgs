4b4fa338024c29b94dfe4e8c65d45249a7c44350
Matt Melling <mattmelling@fastmail.com>
2023-01-27 17:37:22 +0000

    dvc: 2.17.0 -> 2.43.1

--- pkgs/applications/version-management/dvc/default.nix
+++ pkgs/applications/version-management/dvc/default.nix
@@ -10,21 +10,21 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "dvc";
-  version = "2.17.0";
-  format = "setuptools";
+  version = "2.43.1";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "iterative";
     repo = pname;
     rev = version;
-    hash = "sha256-P0J+3TNHGqMw3krfs1uLnf8nEiIBK6UrrB37mY+fBA0=";
+    hash = "sha256-FwJErwAVWFZ95wzBalGi9o+8BTtcGvnC9uQE1qTUaBs=";
   };
 
   postPatch = ''
-    substituteInPlace setup.cfg \
-      --replace "grandalf==0.6" "grandalf" \
-      --replace "scmrepo==0.0.25" "scmrepo" \
-      --replace "pathspec>=0.9.0,<0.10.0" "pathspec"
+    substituteInPlace pyproject.toml \
+      --replace "scmrepo==0.1.6" "scmrepo" \
+      --replace "iterative-telemetry==0.0.6" "iterative-telemetry" \
+      --replace "dvc-data==0.35.1" "dvc-data"
     substituteInPlace dvc/daemon.py \
       --subst-var-by dvc "$out/bin/dcv"
   '';
@@ -34,32 +34,29 @@ python3.pkgs.buildPythonApplication rec {
   ];
 
   propagatedBuildInputs = with python3.pkgs; [
-    aiohttp-retry
     appdirs
     colorama
     configobj
-    dictdiffer
-    diskcache
     distro
     dpath
     dvclive
     dvc-data
     dvc-render
+    dvc-studio-client
     dvc-task
     flatten-dict
     flufl_lock
     funcy
     grandalf
-    nanotime
+    hydra-core
+    iterative-telemetry
     networkx
     packaging
     pathspec
-    ply
     psutil
     pydot
     pygtrie
     pyparsing
-    python-benedict
     requests
     rich
     ruamel-yaml
@@ -73,16 +70,13 @@ python3.pkgs.buildPythonApplication rec {
     voluptuous
     zc_lockfile
   ] ++ lib.optionals enableGoogle [
-    gcsfs
-    google-cloud-storage
+    dvc-gs
   ] ++ lib.optionals enableAWS [
-    aiobotocore
-    boto3
-    s3fs
+    dvc-s3
   ] ++ lib.optionals enableAzure [
     dvc-azure
   ] ++ lib.optionals enableSSH [
-    bcrypt
+    dvc-ssh
   ] ++ lib.optionals (pythonOlder "3.8") [
     importlib-metadata
   ] ++ lib.optionals (pythonOlder "3.9") [
