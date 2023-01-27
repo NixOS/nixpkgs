52ce28bd7a7447df7097e6ebfd3e5478e390b3f5
Jason Yundt <jason@jasonyundt.email>
2023-01-26 19:33:29 -0500

    ecwolf: fix GTK game selector
    ECWolf 1.3.3 has an optional dependency on GTK2 for its GTK IWAD
    selector. ECWolf 1.4.0 uses GTK3 instead of GTK2 for the GTK IWAD
    selector [1]. d0d65badfcb (ecwolf: 1.3.3 -> 1.4.0, 2023-01-19) should
    have replaced the gtk2 build input with gtk3, but it didn’t [2]. This
    commit fixes that mistake.
    
    [1]: <https://bitbucket.org/ecwolf/ecwolf/commits/587d145ae9f7aa3734493472ab6edb84032c3902>
    [2]: <https://github.com/NixOS/nixpkgs/pull/211602#discussion_r1086152074>

--- pkgs/games/ecwolf/default.nix
+++ pkgs/games/ecwolf/default.nix
@@ -11,7 +11,7 @@
 , SDL2
 , SDL2_net
 , SDL2_mixer
-, gtk2
+, gtk3
 }:
 
 stdenv.mkDerivation rec {
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ cmake copyDesktopItems pkg-config ];
-  buildInputs = [ zlib bzip2 libjpeg SDL2 SDL2_net SDL2_mixer gtk2 ];
+  buildInputs = [ zlib bzip2 libjpeg SDL2 SDL2_net SDL2_mixer gtk3 ];
 
   desktopItems = [
     (makeDesktopItem {
