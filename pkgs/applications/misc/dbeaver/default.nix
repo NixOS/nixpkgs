4674df15aeb3c32b02d516f0c9678098eeadec71
Dor Zion <DorZion@users.noreply.github.com>
2023-01-28 20:27:20 +0200

    dbeaver: 22.2.2 -> 22.3.3

--- pkgs/applications/misc/dbeaver/default.nix
+++ pkgs/applications/misc/dbeaver/default.nix
@@ -23,16 +23,16 @@
   inherit maven; # use overridden maven version (see dbeaver's entry in all-packages.nix)
 }) rec {
   pname = "dbeaver";
-  version = "22.2.2"; # When updating also update mvnSha256
+  version = "22.3.3"; # When updating also update mvnSha256
 
   src = fetchFromGitHub {
     owner = "dbeaver";
     repo = "dbeaver";
     rev = version;
-    sha256 = "sha256-TUdtrhQ1JzqZx+QNauNA1P/+WDSSeOGIgGX3SdS0JTI=";
+    sha256 = "sha256-9lGET8n6GVsYYoJ6dmLFi14smpSUWDDOWJK85F44yn8=";
   };
 
-  mvnSha256 = "uu7UNRIuAx2GOh4+YxxoGRcV5QO8C72q32e0ynJdgFo=";
+  mvnSha256 = "vS7lPTDfMiA0m0nR8B2x8t5Qag031jxxjfOeEXJ/SaE=";
   mvnParameters = "-P desktop,all-platforms";
 
   nativeBuildInputs = [
