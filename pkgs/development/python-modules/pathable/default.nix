386bd0d7e920e0ce78cd69aeffecb2b0193d7da2
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-28 12:43:41 +0100

    python310Packages.pathable: add changelog to meta

--- pkgs/development/python-modules/pathable/default.nix
+++ pkgs/development/python-modules/pathable/default.nix
@@ -39,6 +39,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library for object-oriented paths";
     homepage = "https://github.com/p1c2u/pathable";
+    changelog = "https://github.com/p1c2u/pathable/releases/tag/${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
