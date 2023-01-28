01c2d762f301734ac644e067c7cac56f96795c23
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-28 02:58:32 +0000

    diamond: 2.0.15 -> 2.1.0

--- pkgs/applications/science/biology/diamond/default.nix
+++ pkgs/applications/science/biology/diamond/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "diamond";
-  version = "2.0.15";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "bbuchfink";
     repo = "diamond";
     rev = "v${version}";
-    sha256 = "17z9vwj58i1zc22gv4qscx0dk3nxf5ix443gxsibh3a5zsnc6dkg";
+    sha256 = "sha256-tcUVWfb/EtJ6heJjQ3989uNjkWa6FocReJ5gjE35AMY=";
   };
 
 
