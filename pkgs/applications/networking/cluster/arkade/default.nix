52a50d54095f58f540899abea5a1e1ab91b3e011
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-29 17:08:38 +0000

    arkade: 0.8.56 -> 0.8.59

--- pkgs/applications/networking/cluster/arkade/default.nix
+++ pkgs/applications/networking/cluster/arkade/default.nix
@@ -7,20 +7,20 @@
 
 buildGoModule rec {
   pname = "arkade";
-  version = "0.8.56";
+  version = "0.8.59";
 
   src = fetchFromGitHub {
     owner = "alexellis";
     repo = "arkade";
     rev = version;
-    sha256 = "sha256-iU0/ovnclq/jwhi1d6zY4tz56ZB7FT0Mbpi2tEjb9QI=";
+    sha256 = "sha256-uvvxlU5c5MNYVqu0btL4xpjyvb/fDbFPGWkIzru84Z8=";
   };
 
   CGO_ENABLED = 0;
 
   nativeBuildInputs = [ installShellFiles ];
 
-  vendorHash = "sha256-DsLW5l7TxlXRFZDhtRfq5YqVT3XR7EZGNIoPiksHNck=";
+  vendorHash = "sha256-WQsnBBx05wXKDS7h3bhipTqZTQLxRjVLObuMNUfAiTk=";
 
   # Exclude pkg/get: tests downloading of binaries which fail when sandbox=true
   subPackages = [
