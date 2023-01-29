173d592454fdaa50e58848fe78b00e71079433b9
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-29 10:59:34 +0100

    python310Packages.prayer-times-calculator: add changelog to meta

--- pkgs/development/python-modules/prayer-times-calculator/default.nix
+++ pkgs/development/python-modules/prayer-times-calculator/default.nix
@@ -8,13 +8,15 @@
 buildPythonPackage rec {
   pname = "prayer-times-calculator";
   version = "0.0.7";
+  format = "setuptools";
+
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "uchagani";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-fIfv10oi5Lv1dj5Y5CYI94/UdWG3bAY/ENCiMTkO0RE=";
+    hash = "sha256-fIfv10oi5Lv1dj5Y5CYI94/UdWG3bAY/ENCiMTkO0RE=";
   };
 
   propagatedBuildInputs = [
@@ -24,11 +26,14 @@ buildPythonPackage rec {
   # Project has no tests
   doCheck = false;
 
-  pythonImportsCheck = [ "prayer_times_calculator" ];
+  pythonImportsCheck = [
+    "prayer_times_calculator"
+  ];
 
   meta = with lib; {
     description = "Python client for the Prayer Times API";
     homepage = "https://github.com/uchagani/prayer-times-calculator";
+    changelog = "https://github.com/uchagani/prayer-times-calculator/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
