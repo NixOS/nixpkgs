1b2405a68beab80c10440b61ef5b43926b0af9cc
R. Ryantm <ryantm-bot@ryantm.com>
2023-01-29 20:05:31 +0000

    codeql: 2.12.0 -> 2.12.1

--- pkgs/development/tools/analysis/codeql/default.nix
+++ pkgs/development/tools/analysis/codeql/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "codeql";
-  version = "2.12.0";
+  version = "2.12.1";
 
   dontConfigure = true;
   dontBuild = true;
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   src = fetchzip {
     url = "https://github.com/github/codeql-cli-binaries/releases/download/v${version}/codeql.zip";
-    sha256 = "sha256-V+UXodw84NGZPi3Ws4hKyftqnGibn9uFgrkGXxpRAZc=";
+    sha256 = "sha256-rJdB1G2/Fbs5zwfXF/Q79k1IztkOJrL7b0sjH5kcEMs=";
   };
 
   nativeBuildInputs = [
