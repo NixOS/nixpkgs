03ed40600c93dd8ae8c64428d39bd963455bd8ee
zendo <linzway@qq.com>
2023-01-29 16:54:06 +0800

    lxgw-neoxihei: init at 1.005

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27241,6 +27241,8 @@ with pkgs;
     inherit (plasma5Packages) breeze-icons;
   };
 
+  lxgw-neoxihei = callPackage ../data/fonts/lxgw-neoxihei { };
+
   lxgw-wenkai = callPackage ../data/fonts/lxgw-wenkai { };
 
   maia-icon-theme = libsForQt5.callPackage ../data/icons/maia-icon-theme { };
