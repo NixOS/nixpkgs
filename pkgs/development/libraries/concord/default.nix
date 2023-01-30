98c5f438fea5096f93fc0c9484d76b366fa00a32
mrtuxa <mrtuxa@leipzig.freifunk.net>
2023-01-30 16:40:08 +0100

    added concord Package

---
+++ pkgs/development/libraries/concord/default.nix
@@ -0,0 +1,33 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, curl
+}:
+
+stdenv.mkDerivation rec {
+  pname = "concord";
+  version = "2.2.0";
+
+  src = fetchFromGitHub {
+    owner = "Cogmasters";
+    repo = "concord";
+    rev = "v${version}";
+    hash = "sha256-+nuVyqA/V/J6DWAeIs9Pv90ry3px1gJsF460qvfSEH8="; # Fill this in based on the fetch error
+  };
+
+  buildInputs = [
+    curl
+  ];
+
+  installFlags = [
+  	"PREFIX=${"out"}"
+  ];
+
+  meta = with lib; {
+    description = "A Discord API wrapper library made in C";
+    homepage = "https://github.com/Cogmasters/concord";
+    license = licenses.mit;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ mrtuxa ];
+  };
+}
