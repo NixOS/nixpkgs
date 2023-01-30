6997aefc84d092acf94ef0b7d97e1fe6e01af7d2
Bobby Rong <rjl931189261@126.com>
2023-01-30 19:24:17 +0800

    cinnamon.cinnamon-common: use correct python for scrollbar-test-widget.py
    Loading Themes module
    Traceback (most recent call last):
      File "/xxxxxx/share/cinnamon/cinnamon-settings/bin/scrollbar-test-widget.py", line 6, in <module>
        import gi
    ModuleNotFoundError: No module named 'gi'
    
    This file is being called as
    Gio.Subprocess.new(['python3', '/xxxxxx/scrollbar-test-widget.py', str(self.content_widget.get_id())], Gio.SubprocessFlags.NONE)

--- pkgs/desktops/cinnamon/cinnamon-common/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-common/default.nix
@@ -53,6 +53,23 @@
 , perl
 }:
 
+let
+  pythonEnv = python3.withPackages (pp: with pp; [
+    dbus-python
+    setproctitle
+    pygobject3
+    pycairo
+    pp.xapp # don't omit `pp.`, see #213561
+    pillow
+    pyinotify # for looking-glass
+    pytz
+    tinycss2
+    python-pam
+    pexpect
+    distro
+    requests
+  ]);
+in
 stdenv.mkDerivation rec {
   pname = "cinnamon-common";
   version = "5.6.7";
@@ -70,21 +87,6 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    (python3.withPackages (pp: with pp; [
-      dbus-python
-      setproctitle
-      pygobject3
-      pycairo
-      python3.pkgs.xapp # The scope prefix is required
-      pillow
-      pyinotify # for looking-glass
-      pytz
-      tinycss2
-      python-pam
-      pexpect
-      distro
-      requests
-    ]))
     atk
     cacert
     cinnamon-control-center
@@ -106,6 +108,7 @@ stdenv.mkDerivation rec {
     muffin
     networkmanager
     polkit
+    pythonEnv
     libxml2
     libgnomekbd
     gst_all_1.gstreamer
@@ -153,6 +156,8 @@ stdenv.mkDerivation rec {
       {} +
 
     sed "s|/usr/share/sounds|/run/current-system/sw/share/sounds|g" -i ./files/usr/share/cinnamon/cinnamon-settings/bin/SettingsWidgets.py
+
+    sed "s|'python3'|'${pythonEnv.interpreter}'|g" -i ./files/usr/share/cinnamon/cinnamon-settings/bin/CinnamonGtkSettings.py
 
     sed "s|/usr/share/%s|/run/current-system/sw/share/%s|g" -i ./files/usr/share/cinnamon/cinnamon-settings/modules/cs_themes.py
 
