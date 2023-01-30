2843cd434ead9020f0b400397cb2659fca53be9d
0x4A6F <0x4A6F@users.noreply.github.com>
2023-01-30 19:31:58 +0100

    w_scan2: init at 1.0.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29098,6 +29098,8 @@ with pkgs;
 
   w_scan = callPackage ../applications/video/w_scan { };
 
+  w_scan2 = callPackage ../applications/video/w_scan2 { };
+
   wavrsocvt = callPackage ../applications/misc/audio/wavrsocvt { };
 
   welle-io = libsForQt5.callPackage ../applications/radio/welle-io { };
