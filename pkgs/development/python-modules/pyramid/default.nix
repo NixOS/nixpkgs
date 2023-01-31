2f44a31490567dbf66922b4e8d1e897cf082ed5c
Fabian Affolter <mail@fabian-affolter.ch>
2023-01-31 08:48:20 +0100

    python311Packages.pyramid: update meta
    - disable on unsupported Python releases

--- pkgs/development/python-modules/pyramid/default.nix
+++ pkgs/development/python-modules/pyramid/default.nix
@@ -13,28 +13,48 @@
 , webob
 , zope_deprecation
 , zope_interface
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pyramid";
   version = "2.0.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-+r/XRQOeJq1bCRX8OW6HJcD4o9F7lB+WEezR7XbP59o=";
+    hash = "sha256-+r/XRQOeJq1bCRX8OW6HJcD4o9F7lB+WEezR7XbP59o=";
   };
 
-  nativeCheckInputs = [ webtest zope_component ];
+  propagatedBuildInputs = [
+    hupper
+    pastedeploy
+    plaster
+    plaster-pastedeploy
+    repoze_lru
+    translationstring
+    venusian
+    webob
+    zope_deprecation
+    zope_interface
+  ];
 
-  propagatedBuildInputs = [ hupper pastedeploy plaster plaster-pastedeploy repoze_lru translationstring venusian webob zope_deprecation zope_interface ];
+  nativeCheckInputs = [
+    webtest
+    zope_component
+  ];
 
-  pythonImportsCheck = [ "pyramid" ];
+  pythonImportsCheck = [
+    "pyramid"
+  ];
 
   meta = with lib; {
-    description = "The Pyramid Web Framework, a Pylons project";
+    description = "Python web framework";
     homepage = "https://trypyramid.com/";
+    changelog = "https://github.com/Pylons/pyramid/blob/${version}/CHANGES.rst";
     license = licenses.bsd0;
     maintainers = with maintainers; [ domenkozar ];
   };
-
 }
