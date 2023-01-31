10c516b2b6c9e388c56937dec7fc34526c5cb631
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-01-31 03:20:33 +0100

    python3Packages.magic-wormhole-mailbox-server: add patch for Python 3.11

--- pkgs/development/python-modules/magic-wormhole-mailbox-server/default.nix
+++ pkgs/development/python-modules/magic-wormhole-mailbox-server/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , six
 , attrs
 , twisted
@@ -19,6 +20,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "1af10592909caaf519c00e706eac842c5e77f8d4356215fe9c61c7b2258a88fb";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-for-python-3.11.patch";
+      url = "https://github.com/magic-wormhole/magic-wormhole-mailbox-server/commit/4b358859ba80de37c3dc0a5f67ec36909fd48234.patch";
+      hash = "sha256-RzZ5kD+xhmFYusVzAbGE+CODXtJVR1zN2rZ+VGApXiQ=";
+    })
+  ];
 
   propagatedBuildInputs = [
     attrs
