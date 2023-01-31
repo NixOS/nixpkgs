819381b5817bbc5c2411e5976e80ac9bb2dd8a59
zowoq <59103226+zowoq@users.noreply.github.com>
2023-01-31 18:06:07 +1000

    buildRustPackage: drop cacert
    not needed here, set by fetchCargoTarball

--- pkgs/build-support/rust/build-rust-package/default.nix
+++ pkgs/build-support/rust/build-rust-package/default.nix
@@ -4,7 +4,6 @@
 , rust
 , stdenv
 , callPackage
-, cacert
 , cargoBuildHook
 , cargoCheckHook
 , cargoInstallHook
@@ -124,7 +123,6 @@ stdenv.mkDerivation ((removeAttrs args [ "depsExtraArgs" "cargoUpdateHook" "carg
       inherit cargo cargo-auditable;
     })
   ] ++ [
-    cacert
     cargoBuildHook
     (if useNextest then cargoNextestHook else cargoCheckHook)
     cargoInstallHook
