c77badc9677894a4f1c05a1d6359b0d849cb4433
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-01 23:44:02 +0100

    python3Packages.devolo-plc-api: 1.0.0 -> 1.1.0
    Diff: https://github.com/2Fake/devolo_plc_api/compare/refs/tags/v1.0.0...v1.1.0
    
    Changelog: https://github.com/2Fake/devolo_plc_api/releases/tag/v1.1.0

--- pkgs/development/python-modules/devolo-plc-api/default.nix
+++ pkgs/development/python-modules/devolo-plc-api/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "devolo-plc-api";
-  version = "1.0.0";
+  version = "1.1.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "2Fake";
     repo = "devolo_plc_api";
     rev = "refs/tags/v${version}";
-    hash = "sha256-W5wOedxl3ES5xUt98zwRTk0X23MPuJ7J3QjVCjeDM+o=";
+    hash = "sha256-xM7g6q18A+qmOhQeey4uxs6ow6Hf5YKDdbpXwYr2RXo=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
