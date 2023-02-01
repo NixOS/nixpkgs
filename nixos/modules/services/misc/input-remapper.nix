bdba7d16ec0a2d7376824339b682a8c50b76ed0a
pennae <github@quasiparticle.net>
2023-02-01 06:12:57 +0100

    nixos/*: remove trailing period in mkEnableOptions
    those are added by mkEnableOption, and .. is replaced to … by markdown
    processing.

--- nixos/modules/services/misc/input-remapper.nix
+++ nixos/modules/services/misc/input-remapper.nix
@@ -6,7 +6,7 @@ let cfg = config.services.input-remapper; in
 {
   options = {
     services.input-remapper = {
-      enable = mkEnableOption (lib.mdDoc "input-remapper, an easy to use tool to change the mapping of your input device buttons.");
+      enable = mkEnableOption (lib.mdDoc "input-remapper, an easy to use tool to change the mapping of your input device buttons");
       package = mkPackageOptionMD pkgs "input-remapper" { };
       enableUdevRules = mkEnableOption (lib.mdDoc "udev rules added by input-remapper to handle hotplugged devices. Currently disabled by default due to https://github.com/sezanzeb/input-remapper/issues/140");
       serviceWantedBy = mkOption {
