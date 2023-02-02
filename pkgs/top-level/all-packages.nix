83838cf7b75a4738000b349856c5366a83330f47
Adam Stephens <adam@valkor.net>
2023-02-02 11:26:53 -0500

    s0ix-selftest-tool: init at 2022-11-04

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1525,6 +1525,8 @@ with pkgs;
 
   redfang = callPackage ../tools/networking/redfang { };
 
+  s0ix-selftest-tool = callPackage ../tools/system/s0ix-selftest-tool { };
+
   scarab = callPackage ../tools/games/scarab { };
 
   sdbus-cpp = callPackage ../development/libraries/sdbus-cpp { };
