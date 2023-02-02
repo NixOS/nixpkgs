3c74b3d749509a2e0253ef5b1c06a394cbe63c37
kilianar <mail@kilianar.de>
2023-02-02 22:51:21 +0100

    signal-desktop-beta: 6.4.0-beta.1 -> 6.5.0-beta.2
    https://github.com/signalapp/Signal-Desktop/releases/tag/v6.5.0-beta.2

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -6,7 +6,7 @@
   };
   signal-desktop-beta = {
     dir = "Signal Beta";
-    version = "6.4.0-beta.1";
-    hash = "sha256-GR7RWFT20i5dx6XYrp73inCOQ2Hj2UjSXf5jmjfDKMU=";
+    version = "6.5.0-beta.2";
+    hash = "sha256-cAX9oU3bJrTOH3RbbfUK+49OiRSLjEZLdpJNOMAa94I=";
   };
 }
