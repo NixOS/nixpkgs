0341e1f6935e69d1f5389deb6988c316b14ff31d
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-02 09:51:05 +0100

    python310Packages.mypy-boto3-s3: 1.26.58 -> 1.26.62

--- pkgs/development/python-modules/mypy-boto3-s3/default.nix
+++ pkgs/development/python-modules/mypy-boto3-s3/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "mypy-boto3-s3";
-  version = "1.26.58";
+  version = "1.26.62";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-AqqVFIdxR9qZbqYunT0ybWsz9GycILJvHkX9ElugNRg=";
+    hash = "sha256-qBf/8o/ualbYlkEK4KbYSP/kNUgK43rBxIGUDZYOz+U=";
   };
 
   propagatedBuildInputs = [
