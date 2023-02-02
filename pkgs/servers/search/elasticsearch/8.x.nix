b9e804ec88f0527c80dc22124ce47d5d68086cf2
Abhishek Adhikari <abhiayush23@gmail.com>
2023-02-02 18:54:09 +0530

    updated indentation to spaces

--- pkgs/servers/search/elasticsearch/8.x.nix
+++ pkgs/servers/search/elasticsearch/8.x.nix
@@ -36,13 +36,13 @@ stdenv.mkDerivation rec {
   patches = [ ./es-home-6.x.patch ];
 
   postPatch = ''
-    		substituteInPlace bin/elasticsearch-env --replace \
-    			"ES_CLASSPATH=\"\$ES_HOME/lib/*\"" \
-    			"ES_CLASSPATH=\"$out/lib/*\""
-    		substituteInPlace bin/elasticsearch-cli --replace \
-    			"ES_CLASSPATH=\"\$ES_CLASSPATH:\$ES_HOME/\$additional_classpath_directory/*\"" \
-    			"ES_CLASSPATH=\"\$ES_CLASSPATH:$out/\$additional_classpath_directory/*\""
-    	'';
+    substituteInPlace bin/elasticsearch-env --replace \
+      "ES_CLASSPATH=\"\$ES_HOME/lib/*\"" \
+      "ES_CLASSPATH=\"$out/lib/*\""
+    substituteInPlace bin/elasticsearch-cli --replace \
+      "ES_CLASSPATH=\"\$ES_CLASSPATH:\$ES_HOME/\$additional_classpath_directory/*\"" \
+      "ES_CLASSPATH=\"\$ES_CLASSPATH:$out/\$additional_classpath_directory/*\""
+  '';
 
   nativeBuildInputs = [ makeWrapper ]
     ++ lib.optional (!stdenv.hostPlatform.isDarwin) autoPatchelfHook;
@@ -52,16 +52,16 @@ stdenv.mkDerivation rec {
   runtimeDependencies = [ zlib ];
 
   installPhase = ''
-    		mkdir -p $out
-    		cp -R bin config lib modules plugins $out
-    		chmod +x $out/bin/*
-    		substituteInPlace $out/bin/elasticsearch \
-    			--replace 'bin/elasticsearch-keystore' "$out/bin/elasticsearch-keystore"
-    		wrapProgram $out/bin/elasticsearch \
-    			--prefix PATH : "${makeBinPath [ util-linux coreutils gnugrep ]}" \
-    			--set ES_JAVA_HOME "${jre_headless}"
-    		wrapProgram $out/bin/elasticsearch-plugin --set ES_JAVA_HOME "${jre_headless}"
-    	'';
+    mkdir -p $out
+    cp -R bin config lib modules plugins $out
+    chmod +x $out/bin/*
+    substituteInPlace $out/bin/elasticsearch \
+      --replace 'bin/elasticsearch-keystore' "$out/bin/elasticsearch-keystore"
+    wrapProgram $out/bin/elasticsearch \
+      --prefix PATH : "${makeBinPath [ util-linux coreutils gnugrep ]}" \
+      --set ES_JAVA_HOME "${jre_headless}"
+    wrapProgram $out/bin/elasticsearch-plugin --set ES_JAVA_HOME "${jre_headless}"
+  '';
 
   passthru = { enableUnfree = true; };
 
