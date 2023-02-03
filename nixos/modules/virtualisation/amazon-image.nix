fcee9937e729d8653fb7fdba421e997597342ff1
Nicholas Sielicki <github@opensource.nslick.com>
2023-02-03 01:50:28 +0000

    ena: 2.8.0 -> 2.8.2
    * ena: 2.8.0 -> 2.8.2
    
    * fixes build on kernel >= 5.17
    
    Signed-off-by: Nicholas Sielicki <git@opensource.nslick.com>
    (cherry picked from commit 33397faec9d1e7672605afdff749b6a9d6378d02)
    
    * ena: remove broken
    
    ---------
    
    Co-authored-by: Nicholas Sielicki <git@opensource.nslick.com>
    Co-authored-by: superherointj <5861043+superherointj@users.noreply.github.com>

--- nixos/modules/virtualisation/amazon-image.nix
+++ nixos/modules/virtualisation/amazon-image.nix
@@ -37,9 +37,6 @@ in
       { assertion = cfg.efi -> cfg.hvm;
         message = "EC2 instances using EFI must be HVM instances.";
       }
-      { assertion = versionOlder config.boot.kernelPackages.kernel.version "5.17";
-        message = "ENA driver fails to build with kernel >= 5.17";
-      }
     ];
 
     boot.growPartition = cfg.hvm;
