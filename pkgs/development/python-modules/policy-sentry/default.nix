d21f09e370dcf7977a1370913397e224f770b398
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-03 12:32:24 +0100

    python310Packages.policy-sentry: add changelog to meta

--- pkgs/development/python-modules/policy-sentry/default.nix
+++ pkgs/development/python-modules/policy-sentry/default.nix
@@ -13,13 +13,15 @@
 buildPythonPackage rec {
   pname = "policy-sentry";
   version = "0.12.6";
-  disabled = pythonOlder "3.6";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "salesforce";
     repo = "policy_sentry";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-odtMbPHty3NUqz+4UAw+8dsK6AMZer41/BAX8cK5Rek=";
+    hash = "sha256-odtMbPHty3NUqz+4UAw+8dsK6AMZer41/BAX8cK5Rek=";
   };
 
   propagatedBuildInputs = [
@@ -34,11 +36,14 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "policy_sentry" ];
+  pythonImportsCheck = [
+    "policy_sentry"
+  ];
 
   meta = with lib; {
     description = "Python module for generating IAM least privilege policies";
     homepage = "https://github.com/salesforce/policy_sentry";
+    changelog = "https://github.com/salesforce/policy_sentry/releases/tag/${version}";
     license = licenses.bsd3;
     maintainers = with maintainers; [ fab ];
   };
