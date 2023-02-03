2e03c35b5f2cf985e51caa5908e59267d04c97c5
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-03 08:55:28 +0000

    fontconfig: 2.14.0 -> 2.14.2

--- pkgs/development/libraries/fontconfig/default.nix
+++ pkgs/development/libraries/fontconfig/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fontconfig";
-  version = "2.14.0";
+  version = "2.14.2";
 
   outputs = [ "bin" "dev" "lib" "out" ]; # $out contains all the config
 
   src = fetchurl {
     url = "https://www.freedesktop.org/software/fontconfig/release/${pname}-${version}.tar.xz";
-    sha256 = "3L64TJx0u/2xM9U1/hx77cnyIhqNrzkUuYTETFIOm6w=";
+    sha256 = "sha256-26aVtXvOFQI9LO7e+CBiwrkl5R9dTMSu9zbPE/YKRos=";
   };
 
   nativeBuildInputs = [
