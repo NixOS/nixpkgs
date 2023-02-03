640c102321b244edb0b2156cd27ecb16620bd19c
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-02-03 21:11:36 +0000

    openjfx11: add with{Media,Webkit} options

--- pkgs/development/compilers/openjdk/openjfx/11.nix
+++ pkgs/development/compilers/openjdk/openjfx/11.nix
@@ -1,6 +1,9 @@
 { stdenv, lib, fetchFromGitHub, writeText, gradle_7, pkg-config, perl, cmake
 , gperf, gtk2, gtk3, libXtst, libXxf86vm, glib, alsa-lib, ffmpeg_4-headless, python3, ruby, icu68
-, openjdk11-bootstrap }:
+, openjdk11-bootstrap
+, withMedia ? true
+, withWebKit ? false
+}:
 
 let
   major = "11";
@@ -81,8 +84,8 @@ in makePackage {
   pname = "openjfx-modular-sdk";
 
   gradleProperties = ''
-    COMPILE_MEDIA = true
-    COMPILE_WEBKIT = false
+    COMPILE_MEDIA = ${lib.boolToString withMedia}
+    COMPILE_WEBKIT = ${lib.boolToString withWebKit}
   '';
 
   preBuild = ''
