51f802be9d7450e27e3152966f9a74b8e32395fc
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-03 15:49:42 +0100

    home-assistant: Update dependencies and tests

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -2970,7 +2970,8 @@
       pyqvrpro
     ];
     "qwikswitch" = ps: with ps; [
-    ]; # missing inputs: pyqwikswitch
+      pyqwikswitch
+    ];
     "rachio" = ps: with ps; [
       pyturbojpeg
       aiohttp-cors
@@ -4899,6 +4900,7 @@
     "qingping"
     "qld_bushfire"
     "qnap_qsw"
+    "qwikswitch"
     "rachio"
     "radarr"
     "radio_browser"
