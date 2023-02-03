9b668e0734a0d02be4e78dff768a5a2a012d85e6
Alper Çelik <dev.alpercelik@gmail.com>
2023-02-04 02:52:42 +0300

    magic-enum: init at 0.8.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22001,6 +22001,8 @@ with pkgs;
 
   opencl-clang = callPackage ../development/libraries/opencl-clang { };
 
+  magic-enum = callPackage ../development/libraries/magic-enum { };
+
   mapnik = callPackage ../development/libraries/mapnik {
     harfbuzz = harfbuzz.override {
       withIcu = true;
