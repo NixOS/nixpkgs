63e3f8da0977526c783839c1ed3854d5681a80ad
Jon Seager <jon@sgrs.uk>
2023-02-03 09:27:35 +0000

    nixos/multipass: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1364,6 +1364,7 @@
   ./virtualisation/lxc.nix
   ./virtualisation/lxcfs.nix
   ./virtualisation/lxd.nix
+  ./virtualisation/multipass.nix
   ./virtualisation/nixos-containers.nix
   ./virtualisation/oci-containers.nix
   ./virtualisation/openstack-options.nix
