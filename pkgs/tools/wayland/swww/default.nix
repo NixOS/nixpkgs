9377f94ac5c69eaeca0a2a9d13a41ea7f41c0b7d
Mateo Diaz <mateodd@icloud.com>
2023-02-03 22:25:04 -0800

    swww: v0.5.0 -> v0.7.0

--- pkgs/tools/wayland/swww/default.nix
+++ pkgs/tools/wayland/swww/default.nix
@@ -1,17 +1,16 @@
 { config, lib, pkgs, fetchFromGitHub, rustPlatform, pkg-config, lz4
 , libxkbcommon, ... }:
-
 rustPlatform.buildRustPackage rec {
   pname = "swww";
-  version = "0.5.0";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "Horus645";
     repo = pname;
-    rev = "d45ab41a0c83b6f49a0279618e91ddfc0853bb6a";
-    sha256 = "K9ZH/774BmzjEFj3gxzTALQex0T12B8ZuGEB878Qbc0=";
+    rev = "v0.7.0";
+    sha256 = "ofJTL+9izMmwENaEVYQ7rjpoEFR1J2szkJxzYb09N7g=";
   };
-  cargoSha256 = "sha256-boCXh9QFfxxtqxElrEHtBJMeCQikaUoytIVshwXRA10=";
+  cargoSha256 = "sha256-ntulNQUQZ42CGvwnnQSMQsr/MDSNY/5/Aq2Ew9QItn8=";
   buildInputs = [ lz4 libxkbcommon ];
   doCheck = false; # Integration tests do not work in sandbox enviroment
   nativeBuildInputs = [ pkg-config ];
@@ -19,7 +18,6 @@ rustPlatform.buildRustPackage rec {
     description =
       "Efficient animated wallpaper daemon for wayland, controlled at runtime";
     homepage = "https://github.com/Horus645/swww";
-    maintainers = with maintainers; [ mateodd25 ];
     license = licenses.gpl3;
     platforms = platforms.linux;
   };
