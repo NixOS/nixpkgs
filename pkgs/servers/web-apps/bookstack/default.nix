7c4b648ef4cc1141e3a23bd7131189747fe4f4fa
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-04 17:30:05 +0000

    bookstack: 23.01 -> 23.01.1

--- pkgs/servers/web-apps/bookstack/default.nix
+++ pkgs/servers/web-apps/bookstack/default.nix
@@ -16,13 +16,13 @@ let
 
 in package.override rec {
   pname = "bookstack";
-  version = "23.01";
+  version = "23.01.1";
 
   src = fetchFromGitHub {
     owner = "bookstackapp";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0d2hqqa4x133ni8j0dijzn9653iq44ax4rly85g9kiwrjd0l17cw";
+    sha256 = "sha256-S4yGys1Lc2FAd3RKI4KdE9X12rsQyVcPQ+Biwwrnb0I=";
   };
 
   meta = with lib; {
