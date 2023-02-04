73e3b979ac8b22af019a6e77a1ae307d4db5a50a
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-04 07:22:03 +0000

    argocd: 2.5.9 -> 2.5.10

--- pkgs/applications/networking/cluster/argocd/default.nix
+++ pkgs/applications/networking/cluster/argocd/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "argocd";
-  version = "2.5.9";
+  version = "2.5.10";
 
   src = fetchFromGitHub {
     owner = "argoproj";
     repo = "argo-cd";
     rev = "v${version}";
-    sha256 = "sha256-RZ3bcuJmUUnucD2lhfxLU8vbrorVUowF8hAW9NGSmbI=";
+    sha256 = "sha256-5UzDfOPuDM5ADsXFwWa9aDM32I5s/TKg4r7idH/X3AQ=";
   };
 
   proxyVendor = true; # darwin/linux hash mismatch
-  vendorHash = "sha256-F5EY1/WWRPBN5fqp2J2mdpIzL1gNKR0ltzSdarT6dFw=";
+  vendorHash = "sha256-rkgwfSmUmxmCxbS/0r5sU+XJTGZ6NTZ+VIXpBElT0uA=";
 
   # Set target as ./cmd per cli-local
   # https://github.com/argoproj/argo-cd/blob/master/Makefile#L227
