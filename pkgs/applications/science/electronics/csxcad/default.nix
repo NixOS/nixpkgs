89c92b160a5af9b316ef62aa8acacf021335b1c5
Ben Darwin <bcdarwin@gmail.com>
2023-02-04 13:49:28 -0500

    csxcad: unpin vtk

--- pkgs/applications/science/electronics/csxcad/default.nix
+++ pkgs/applications/science/electronics/csxcad/default.nix
@@ -5,7 +5,7 @@
 , tinyxml
 , hdf5
 , cgal_5
-, vtk_8
+, vtk
 , boost
 , gmp
 , mpfr
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
     boost
     gmp
     mpfr
-    vtk_8
+    vtk
     fparser
     tinyxml
     hdf5
