2052689500095ff5d862d257cae3d427364f760c
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-04 20:58:10 +0100

    findomain: add changelog to meta

--- pkgs/tools/networking/findomain/default.nix
+++ pkgs/tools/networking/findomain/default.nix
@@ -15,14 +15,21 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "Edu4rdSHL";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-9mtXtBq08lL6qQg1Pq1WNwbkG0yi99mCpxNuBvr14ms=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-9mtXtBq08lL6qQg1Pq1WNwbkG0yi99mCpxNuBvr14ms=";
   };
 
   cargoHash = "sha256-pKNqO43aFXZ/cbjNWt3tmBBbSTSKqVF7biNCPI1flvI=";
 
-  nativeBuildInputs = [ installShellFiles perl ];
-  buildInputs = lib.optionals stdenv.isDarwin [ libiconv Security ];
+  nativeBuildInputs = [
+    installShellFiles
+    perl
+  ];
+
+  buildInputs = lib.optionals stdenv.isDarwin [
+    libiconv
+    Security
+  ];
 
   postInstall = ''
     installManPage ${pname}.1
@@ -31,6 +38,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "The fastest and cross-platform subdomain enumerator";
     homepage = "https://github.com/Edu4rdSHL/findomain";
+    changelog = "https://github.com/Findomain/Findomain/releases/tag/${version}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ Br1ght0ne ];
   };
