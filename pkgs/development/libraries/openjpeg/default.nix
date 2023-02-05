NixOS/nixpkgs

commit 3404d6b0f764b640d0c5a3d59a496039e914f8c3

openjpeg: move jpylizer to nativeCheckInputs
This is useful when cross-compiling to platforms that we can't build
Python for (and also just more semantically correct).

pkgs/development/libraries/openjpeg/default.nix
@@ -55,10 +55,10 @@ stdenv.mkDerivation rec {
     ++ lib.optional (jpipLibSupport) jdk;
 
   doCheck = (!stdenv.isAarch64 && !stdenv.hostPlatform.isPower64); # tests fail on aarch64-linux and powerpc64
-
+  nativeCheckInputs = [ jpylyzer ];
   checkPhase = ''
     substituteInPlace ../tools/ctest_scripts/travis-ci.cmake \
-      --replace "JPYLYZER_EXECUTABLE=" "JPYLYZER_EXECUTABLE=\"${jpylyzer}/bin/jpylyzer\" # "
+      --replace "JPYLYZER_EXECUTABLE=" "JPYLYZER_EXECUTABLE=\"jpylyzer\" # "
     OPJ_SOURCE_DIR=.. ctest -S ../tools/ctest_scripts/travis-ci.cmake
   '';
 

commit 1ae5f0d5c9967492f54299ffc3c031a25540099b

openjpeg: fix tests
It seems that in the few weeks between me writing the previous patch
and submitting it for review, I had forgotten I hadn't tested the
non-cross case yet, and so hadn't caught that the tests didn't
actually work any more.  Oops!

Fixes: 3404d6b0f76 ("openjpeg: move jpylizer to nativeCheckInputs")

pkgs/development/libraries/openjpeg/default.nix
@@ -58,7 +58,7 @@ stdenv.mkDerivation rec {
   nativeCheckInputs = [ jpylyzer ];
   checkPhase = ''
     substituteInPlace ../tools/ctest_scripts/travis-ci.cmake \
-      --replace "JPYLYZER_EXECUTABLE=" "JPYLYZER_EXECUTABLE=\"jpylyzer\" # "
+      --replace "JPYLYZER_EXECUTABLE=" "JPYLYZER_EXECUTABLE=\"$(command -v jpylyzer)\" # "
     OPJ_SOURCE_DIR=.. ctest -S ../tools/ctest_scripts/travis-ci.cmake
   '';
 