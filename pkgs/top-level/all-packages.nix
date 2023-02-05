75b28a6d37785e39100ae5de77ccaa4075b8d59d
Colin Arnott <colin@urandom.co.uk>
2023-02-05 23:35:30 +0000

    mkNextcloudDerivation: expose generic as function
    As a remediation to #212912, because it is not acceptable to introduce
    pre-release versions of software into nixpkgs, it was proposed that we
    allow consumers to call generic from external derivations. As such, we
    have exposed it as mkNextcloudDerivation, in line with other packages.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10606,7 +10606,7 @@ with pkgs;
   grocy = callPackage ../servers/grocy { };
 
   inherit (callPackages ../servers/nextcloud {})
-    nextcloud28 nextcloud29;
+    mkNextcloudDerivation nextcloud28 nextcloud29;
 
   nextcloud28Packages = callPackage ../servers/nextcloud/packages {
     apps = lib.importJSON ../servers/nextcloud/packages/28.json;
@@ -10614,7 +10614,6 @@ with pkgs;
   nextcloud29Packages = callPackage ../servers/nextcloud/packages {
     apps = lib.importJSON ../servers/nextcloud/packages/29.json;
   };
-
 
   nextcloud-client = libsForQt5.callPackage ../applications/networking/nextcloud-client { };
 
