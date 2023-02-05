bb9016f15a68890e536f93cf010f299e6b89ffa5
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-05 18:21:04 +0000

    cemu: 2.0-22 -> 2.0-26

--- pkgs/applications/emulators/cemu/default.nix
+++ pkgs/applications/emulators/cemu/default.nix
@@ -31,13 +31,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cemu";
-  version = "2.0-22";
+  version = "2.0-26";
 
   src = fetchFromGitHub {
     owner = "cemu-project";
     repo = "Cemu";
     rev = "v${version}";
-    hash = "sha256-ZQfJHQnT5mV6GC3dO6QV1fGsnyZMYqXiVdBSsimL5yU=";
+    hash = "sha256-+y+PJE2biRvuxIwrFVMjmkZyD8/zhHVMw6vzNKlsOZE=";
   };
 
   patches = [
