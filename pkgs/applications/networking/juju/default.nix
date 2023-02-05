8441676da3049b54259fad1570e61ea8cd69d5bb
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-05 21:48:49 +0000

    juju: 2.9.38 -> 3.0.2

--- pkgs/applications/networking/juju/default.nix
+++ pkgs/applications/networking/juju/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "juju";
-  version = "2.9.38";
+  version = "3.0.2";
 
   src = fetchFromGitHub {
     owner = "juju";
     repo = "juju";
     rev = "juju-${version}";
-    sha256 = "sha256-QTO6MHiFx3nDXDaaVy7rCiR0ttBXt5oAw94/ZDTICOM=";
+    sha256 = "sha256-MZZ4xZpZM/DCc2FFti84E3DZMKOCy2YKZlTnz6FxSOo=";
   };
 
-  vendorHash = "sha256-QOu1azw3OUWaz7MRFGZ5CGX4bVegbFYp76/XpesnaUM=";
+  vendorHash = "sha256-y9ZcsPY7yKA2Ls72Z3hP3wSOh01y7ZKrebEmxIFhxhU=";
 
   # Disable tests because it attempts to use a mongodb instance
   doCheck = false;
