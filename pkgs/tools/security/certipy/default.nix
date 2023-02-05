bccc8d15721dc8d3b670ca97fccda3a4adb64453
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-05 10:33:44 +0100

    certipy: add changelog to meta

--- pkgs/tools/security/certipy/default.nix
+++ pkgs/tools/security/certipy/default.nix
@@ -6,11 +6,12 @@
 python3.pkgs.buildPythonApplication rec {
   pname = "certipy";
   version = "2.0.9";
+  format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "ly4k";
     repo = "Certipy";
-    rev = version;
+    rev = "refs/tags/${version}";
     hash = "sha256-84nGRKZ0UlMDAZ1Wo5Hgy9XSAyEh0Tio9+3OZVFZG5k=";
   };
 
@@ -34,6 +35,7 @@ python3.pkgs.buildPythonApplication rec {
   meta = with lib; {
     description = "Tool to enumerate and abuse misconfigurations in Active Directory Certificate Services";
     homepage = "https://github.com/ly4k/Certipy";
+    changelog = "https://github.com/ly4k/Certipy/releases/tag/${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
