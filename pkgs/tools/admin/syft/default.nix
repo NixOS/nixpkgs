fe4f28b807beeee8576fada03838ab8bfffcdc79
Mario Rodas <marsam@users.noreply.github.com>
2023-02-05 20:30:00 +0000

    syft: 0.69.1 -> 0.70.0
    https://github.com/anchore/syft/releases/tag/v0.70.0

--- pkgs/tools/admin/syft/default.nix
+++ pkgs/tools/admin/syft/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "syft";
-  version = "0.69.1";
+  version = "0.70.0";
 
   src = fetchFromGitHub {
     owner = "anchore";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-1LVJwPMNiqq8oJc+eARBykFSDu19eHjbX40I7JohXyg=";
+    hash = "sha256-Dr0kVJk2LyyRFZq1fZBQSLb7z/AfCm8Y+tIMm8JmyJo=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -22,7 +22,7 @@ buildGoModule rec {
   };
   # hash mismatch with darwin
   proxyVendor = true;
-  vendorHash = "sha256-qEFA0CV7VVo3N7/KJwNLzshGaqg1RnHCOuCRGJw43DI=";
+  vendorHash = "sha256-UB0ltY4CYlsH4CyQZSOHDH8C9jSCL0TCri33H3oPH/0=";
 
   nativeBuildInputs = [ installShellFiles ];
 
