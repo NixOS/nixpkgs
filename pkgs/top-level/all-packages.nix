2051d47229cd9329297eb1f6a689b23cf9d4a80a
Phillip Seeber <phillip.seeber@uni-jena.de>
2023-02-06 16:53:51 +0100

    mopac: init at 22.0.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36021,6 +36021,8 @@ with pkgs;
 
   molden = callPackage ../applications/science/chemistry/molden { };
 
+  mopac = callPackage ../applications/science/chemistry/mopac { };
+
   octopus = callPackage ../applications/science/chemistry/octopus { };
 
   openlp = libsForQt5.callPackage ../applications/misc/openlp { };
