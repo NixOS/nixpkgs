ac2d27b1c27eeed834652bed2b0cd30fbfe2e84e
Francesco Gazzetta <fgaz@fgaz.me>
2023-02-06 15:33:36 +0100

    openrgb-plugin-effects: init at 0.8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10333,7 +10333,12 @@ with pkgs;
 
   openrgb = libsForQt5.callPackage ../applications/misc/openrgb { };
 
-  openrgb-with-all-plugins = openrgb.withPlugins [ openrgb-plugin-hardwaresync ];
+  openrgb-with-all-plugins = openrgb.withPlugins [
+    openrgb-plugin-effects
+    openrgb-plugin-hardwaresync
+  ];
+
+  openrgb-plugin-effects = libsForQt5.callPackage ../applications/misc/openrgb-plugins/effects { };
 
   openrgb-plugin-hardwaresync = libsForQt5.callPackage ../applications/misc/openrgb-plugins/hardwaresync { };
 
