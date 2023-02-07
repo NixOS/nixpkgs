308138f92cdea12489e6e3b0d6e5aa82c68275b8
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-07 11:03:11 +0000

    jackett: 0.20.2916 -> 0.20.2986
    (cherry picked from commit 58c9772bb79885caf42309d430363e09a9dcd9aa)

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -9,13 +9,13 @@
 
 buildDotnetModule rec {
   pname = "jackett";
-  version = "0.20.2916";
+  version = "0.20.2986";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    hash = "sha512-tYIPpEAqN+ENYN/NJcGWnk3NpJm0MeTBwAp7nu21QWED88sKkHatM/z+puibWxM46WMb7kyPtCmyA//aP08mJQ==";
+    hash = "sha512-IbcIn2n5Cob+Ewp0lkkIENuo00HBlNy6V5pKARC6EgBDRKbB+YXETDX1Ko3Dsl0x6mxZgoTCbzEIPxD+Y2J4Rw==";
   };
 
   projectFile = "src/Jackett.Server/Jackett.Server.csproj";
