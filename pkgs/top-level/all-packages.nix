a5b2cf49b93a4c5edb190fd216355dfde6d84280
fleaz <mail@felixbreidenstein.de>
2023-02-07 16:30:52 +0100

    gcfflasher: init at 4.0.3-beta

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7353,6 +7353,8 @@ with pkgs;
     gtk = gtk2-x11;
   };
 
+  gcfflasher = callPackage ../applications/misc/gcfflasher { };
+
   gdmap = callPackage ../tools/system/gdmap { };
 
   gef = callPackage ../development/tools/misc/gef { };
