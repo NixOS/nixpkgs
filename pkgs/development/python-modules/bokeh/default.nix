2272187eac320faf2667ee704f51459b0c4e941f
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-08 15:21:14 +0100

    python310Packages.bokeh: 2.4.3 -> 3.2.0
    https://docs.bokeh.org/en/latest/docs/releases.html#release-3-2-0
    
    Changes the build to a source build and enables tests.
    
    To enable the tests we have to fetch the sources from Git, which in turn
    requires us to build the static files from source.
    
    Bokeh uses setuptools-git-versioning, which has no version override, so
    we also fix the broken fallback version in the package metadata.
    
    We skip sampledata tests, because that would require an additional
    download, and we skip selenium tests because they are crashing with
    chromium/chromedriver and getting out of sync with geckodriver/firefox.

--- pkgs/development/python-modules/bokeh/default.nix
+++ pkgs/development/python-modules/bokeh/default.nix
@@ -1,57 +1,110 @@
-{ buildPythonPackage
-, fetchPypi
-, futures ? null
-, isPy27
-, isPyPy
+{ lib
+, buildNpmPackage
+, buildPythonPackage
+, fetchFromGitHub
+, pythonOlder
+
+# build
+, colorama
+, setuptools
+
+# runtime
+, contourpy
 , jinja2
-, lib
-, mock
-, numpy
 , nodejs
+, numpy
 , packaging
-, pillow
-#, pytestCheckHook#
-, pytest
-, python-dateutil
-, pyyaml
-, selenium
-, six
-, substituteAll
-, tornado
-, typing-extensions
-, pytz
-, flaky
-, networkx
-, beautifulsoup4
-, requests
-, nbconvert
-, icalendar
 , pandas
-, pythonImportsCheckHook
+, pillow
+, pyyaml
+, tornado
+, xyzservices
+
+# tests
+, beautifulsoup4
+, git
+, json5
+, nbconvert
+, pytest-asyncio
+, pytest-html
+, pytest-rerunfailures
+, pytest-xdist
+, pytestCheckHook
+, requests
+, requests-unixsocket
+, selenium
 }:
 
-buildPythonPackage rec {
-  pname = "bokeh";
-  # update together with panel which is not straightforward
-  version = "2.4.3";
+let
+  # update together with panel, which is not straightforward
+  version = "3.2.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-7zOAEWGvN5Zlq3o0aE8iCYYeOu/VyAOiH7u5nZSHSwM=";
+  src = fetchFromGitHub {
+    owner = "bokeh";
+    repo = "bokeh";
+    rev = "refs/tags/${version}";
+    hash = "sha256-YgmGpMtIeuEHkWg7tx4ItBOQfdsVripXg+Aa3+KdsAA=";
   };
 
-  patches = [
-    (substituteAll {
-      src = ./hardcode-nodejs-npmjs-paths.patch;
-      node_bin = "${nodejs}/bin/node";
-      npm_bin = "${nodejs}/bin/npm";
-    })
-  ];
+  bokehjs = buildNpmPackage {
+    pname = "bokehjs";
+    inherit version;
 
-  disabled = isPyPy || isPy27;
+    inherit src;
+    sourceRoot = "source/bokehjs";
+
+    npmDepsHash = "sha256-hUoofSZkFY/h5UoPVldYiQW8OtQ8W786ZUgpmIwTdeI=";
+
+    preInstall = ''
+      # install files that are in the upstream tarball but not covered
+      # by the `files` definition in package.json
+      # Has to happen in preInstall, because pruning happens during
+      # installPhase.
+      PREFIX=$out/lib/node_modules/@bokeh/bokehjs/build
+      mkdir -p $PREFIX/{js,lib}
+
+      cp -R ./node_modules/typescript/lib/lib.*.ts $PREFIX/lib/
+      cp ./build/js/{bokeh.json,compiler.js} $PREFIX/js/
+      cp -R ./build/js/compiler $PREFIX/js/
+    '';
+  };
+in
+buildPythonPackage rec {
+  pname = "bokeh";
+  inherit version;
+  format = "pyproject";
+
+  disabled = pythonOlder "3.9";
+
+  inherit src;
 
   nativeBuildInputs = [
-    pythonImportsCheckHook
+    colorama
+    setuptools
+  ];
+
+  postPatch = ''
+    # workaround for setuptools-git-versioning not supporting overrides
+    # https://github.com/dolfinus/setuptools-git-versioning/issues/77
+    substituteInPlace pyproject.toml \
+      --replace 'dynamic = ["version"]' 'version = "${version}"'
+
+    # inject fully built artifacts
+    rm -r src/bokeh/server/static
+    ln -s "${bokehjs}/lib/node_modules/@bokeh/bokehjs/build" "src/bokeh/server/static"
+    touch PKG-INFO
+  '';
+
+  propagatedBuildInputs = [
+    contourpy
+    jinja2
+    numpy
+    packaging
+    pandas
+    pillow
+    pyyaml
+    tornado
+    xyzservices
   ];
 
   pythonImportsCheck = [
@@ -59,42 +112,72 @@ buildPythonPackage rec {
   ];
 
   nativeCheckInputs = [
-    mock
-    pytest
-    pillow
-    selenium
-    pytz
-    flaky
-    networkx
     beautifulsoup4
-    requests
+    git
+    json5
     nbconvert
-    icalendar
-    pandas
+    nodejs
+    pytest-asyncio
+    pytest-html
+    pytest-rerunfailures
+    pytest-xdist
+    pytestCheckHook
+    requests
+    requests-unixsocket
+    selenium
   ];
 
-  propagatedBuildInputs = [
-    pillow
-    jinja2
-    python-dateutil
-    six
-    pyyaml
-    tornado
-    numpy
-    packaging
-    typing-extensions
-  ]
-  ++ lib.optionals ( isPy27 ) [
-    futures
+  dontUsePytestXdist = true;
+
+  __darwinAllowLocalNetworking = true;
+
+  pytestFlagsArray = [
+    # sampledata needs to be downloaded, selenium tests are brittle
+    "-m" "'not sampledata and not selenium'"
+    # limit parallelism, so we don't run into socket bind conflicts
+    "--numprocesses=$(( $NIX_BUILD_CORES > 4 ? 4 : $NIX_BUILD_CORES ))"
+    # give some tests more chances to complete
+    "--reruns=3"
   ];
 
-  # This test suite is a complete pain. Somehow it can't find its fixtures.
-  doCheck = false;
+  disabledTestPaths = [
+    # tries to fetch node modules
+    "tests/test_bokehjs.py"
+    # expects to be executed in a git repository
+    "tests/test_defaults.py"
+    # fails to find examples
+    "tests/test_examples.py"
+    # linters
+    "tests/codebase"
+    # ignore selenium tests, not all are marked apparently
+    "tests/unit/bokeh/io/test_webdriver.py"
+  ];
 
-  meta = {
+  disabledTests = [
+    # assert filename and filename.endswith(("py.test", "pytest", "py.test-script.py", "pytest-script.py"))
+    # assert (None)
+    "test_detect_current_filename"
+    # tries to fetch node modules
+    "test_ext_commands"
+    # AssertionError: assert [{'extends': ...rop3'}], ...}] == [{'name': 'te...ro...
+    "test_serialization_data_models"
+    # Failed: Did not find pattern in process output
+    "test_websocket_max_message_size_printed_out"
+    # FileNotFoundError: [Errno 2] No such file or directory: '/build/source/tests/unit/bokeh/embed/latex_label/dist/latex_label.js
+    # FileNotFoundError: [Errno 2] No such file or directory: '/build/source/tests/unit/bokeh/embed/ext_package_no_main/dist/ext_package_no_main.js'
+    "test_with_CDN_resources"
+    "test_with_INLINE_resources"
+  ];
+
+  passthru = {
+    inherit bokehjs;
+  };
+
+  meta = with lib; {
     description = "Statistical and novel interactive HTML plots for Python";
     homepage = "https://github.com/bokeh/bokeh";
-    license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [ orivej ];
+    changelog = "https://docs.bokeh.org/en/latest/docs/releases.html#release-${replaceStrings ["."] ["-"] version}";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ orivej ];
   };
 }
