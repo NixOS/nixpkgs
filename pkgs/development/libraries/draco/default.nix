3f23a0c15b1194ac03954df53342b7d8e0e70247
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-08 20:51:42 +0000

    draco: 1.5.5 -> 1.5.6

--- pkgs/development/libraries/draco/default.nix
+++ pkgs/development/libraries/draco/default.nix
@@ -16,14 +16,14 @@ let
   cmakeBool = b: if b then "ON" else "OFF";
 in
 stdenv.mkDerivation rec {
-  version = "1.5.5";
+  version = "1.5.6";
   pname = "draco";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "draco";
     rev = version;
-    sha256 = "sha256-WYWEUfBPz/Pt7sE8snG3/LnOA3DEUm/SUVLtsH7zG5g=";
+    sha256 = "sha256-2YQMav0JJMbJ2bvnN/Xv90tjE/OWLbrZDO4WlaOvcfI=";
     fetchSubmodules = true;
   };
 
