66cb02adae0ee37b40fe4a4cf51b7afa565605e1
Mario Rodas <marsam@users.noreply.github.com>
2023-02-08 04:20:00 +0000

    esbuild: 0.17.5 -> 0.17.7
    https://github.com/evanw/esbuild/releases/tag/v0.17.6
    https://github.com/evanw/esbuild/releases/tag/v0.17.7

--- pkgs/development/tools/esbuild/default.nix
+++ pkgs/development/tools/esbuild/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "esbuild";
-  version = "0.17.5";
+  version = "0.17.7";
 
   src = fetchFromGitHub {
     owner = "evanw";
     repo = "esbuild";
     rev = "v${version}";
-    hash = "sha256-QLV6+1se3F7naG3jgXtSfmoTR0d7cFByRznzf+xjDpk=";
+    hash = "sha256-dfXDGXn8Az6JRTcYB/gZgHLxQUKgsWhRXl2h0RqVZ4w=";
   };
 
   vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
