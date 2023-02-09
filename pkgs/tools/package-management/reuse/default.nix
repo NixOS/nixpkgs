c9ae110cf49c960c013ac71f819fb2dd51c3511e
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-09 09:55:56 +0000

    reuse: 1.1.0 -> 1.1.1

--- pkgs/tools/package-management/reuse/default.nix
+++ pkgs/tools/package-management/reuse/default.nix
@@ -2,14 +2,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "reuse";
-  version = "1.1.0";
+  version = "1.1.1";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "fsfe";
     repo = "reuse-tool";
-    rev = "v${version}";
-    hash = "sha256-bjUDImMFwMhRjCa7XzGlqR8h+KfTsyxonrQlRGgApwo=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-4L5VQtjpJ1P95S3vkbgLYTO/lTFReGiSAVuWSwh14i4=";
   };
 
   nativeBuildInputs = with python3Packages; [
