275c67a37dcc781e612365eb464c8a9bc5d03622
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-02-09 06:05:45 +0100

    ligolo-ng: 0.4.2 -> 0.4.3

--- pkgs/tools/networking/ligolo-ng/default.nix
+++ pkgs/tools/networking/ligolo-ng/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "ligolo-ng";
-  version = "0.4.2";
+  version = "0.4.3";
 
   src = fetchFromGitHub {
     owner = "tnpitsecurity";
     repo = "ligolo-ng";
     rev = "v${version}";
-    sha256 = "sha256-BuKSIJGeHuHfzcaADgGqKyQ6oy5RAUHyRs8e+d/Nf+0=";
+    hash = "sha256-O/qiznQs+x7qBYXVItd0W7a0irEzRf0We7kW7HHLqcw=";
   };
 
   postConfigure = ''
@@ -24,7 +24,7 @@ buildGoModule rec {
   meta = with lib; {
     homepage = "https://github.com/tnpitsecurity/ligolo-ng";
     description = "A tunneling/pivoting tool that uses a TUN interface";
-    platforms = platforms.linux;
+    platforms = platforms.unix;
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ elohmeier ];
   };
