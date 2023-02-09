906c97b357759ef39033355fd38caeae20fe69a1
Robert Schütz <nix@dotlambda.de>
2023-02-08 17:07:24 -0800

    python310Packages.trove-classifiers: 2023.1.20 -> 2023.2.8
    Changelog: https://github.com/pypa/trove-classifiers/releases/tag/2023.2.8

--- pkgs/development/python-modules/trove-classifiers/default.nix
+++ pkgs/development/python-modules/trove-classifiers/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "trove-classifiers";
-  version = "2023.1.20";
+  version = "2023.2.8";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-7T/U4dLqd8qe03k4BYJWbmADpU1wo+VSGpsqeJZgk2I=";
+    hash = "sha256-O2lg+5bB1MyZiL3BuQvNZfz12YQ9iE38hr1nT/gaTeo=";
   };
 
   nativeBuildInputs = [
