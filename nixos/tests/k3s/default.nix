52a153aae20d999854d7ece49b865e95b929e490
Jairo Llopis <yajo.sk8@gmail.com>
2023-02-09 09:57:37 +0000

    k3s: test all versions
    Since https://github.com/NixOS/nixpkgs/issues/213943 got fixed, only the main k3s derivation is tested.
    
    Here I changed the tests a bit to make them test all provided k3s derivations
    
    @moduon MT-1718

--- nixos/tests/k3s/default.nix
+++ nixos/tests/k3s/default.nix
@@ -1,9 +1,21 @@
 { system ? builtins.currentSystem
 , pkgs ? import ../../.. { inherit system; }
+, lib ? pkgs.lib
 }:
+let
+  allK3s = {
+    inherit (pkgs)
+      k3s
+      k3s_1_23
+      k3s_1_24
+      k3s_1_25
+      k3s_1_26
+      ;
+  };
+in
 {
   # Run a single node k3s cluster and verify a pod can run
-  single-node = import ./single-node.nix { inherit system pkgs; };
+  single-node = lib.mapAttrs (_: k3s: import ./single-node.nix { inherit system pkgs k3s; }) allK3s;
   # Run a multi-node k3s cluster and verify pod networking works across nodes
-  multi-node = import ./multi-node.nix { inherit system pkgs; };
+  multi-node = lib.mapAttrs (_: k3s: import ./multi-node.nix { inherit system pkgs k3s; }) allK3s;
 }
