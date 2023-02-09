33cfeb95639bde972f3f24c560891e24ca6009b7
Robert Schütz <nix@dotlambda.de>
2023-02-09 15:50:56 -0800

    Revert "python3Packages.flax: 0.6.1 -> 0.6.3"
    This reverts commit fe0048c7f8d869993b486b243dec94204bcf0c08 which broke
    flax due to https://github.com/google/flax/issues/2707.

--- pkgs/development/python-modules/flax/default.nix
+++ pkgs/development/python-modules/flax/default.nix
@@ -17,13 +17,13 @@
 
 buildPythonPackage rec {
   pname = "flax";
-  version = "0.6.3";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-4BYfrwEddA2LCMyDO6PBBYdMVTqqDxhzMCZ5JIIml3g=";
+    sha256 = "sha256-fZiODo+izOwGjCCTvi11GvUG/VQL1DV9bNXKjvIIw4A=";
   };
 
   buildInputs = [ jaxlib ];
