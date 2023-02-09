87837a5fcf7dd9c58bb80f15a8f046a2abc8a4c8
Astro <astro@spaceboyz.net>
2023-02-09 01:13:21 +0100

    build-support/rust/sysroot: update Cargo.lock

--- pkgs/build-support/rust/build-rust-package/sysroot/default.nix
+++ pkgs/build-support/rust/build-rust-package/sysroot/default.nix
@@ -14,7 +14,7 @@ in rustPlatform.buildRustPackage {
 
   RUSTC_BOOTSTRAP = 1;
   __internal_dontAddSysroot = true;
-  cargoSha256 = "0y6dqfhsgk00y3fv5bnjzk0s7i30nwqc1rp0xlrk83hkh80x81mw";
+  cargoSha256 = "sha256-zgkwevitxsu1C4OgGTsqNSc0gDxaNXYK1WPbfER48d0=";
 
   doCheck = false;
 
