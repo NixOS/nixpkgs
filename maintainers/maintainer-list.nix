d46baa202207647675325c724168b28c6ce035da
toastal <toastal@posteo.net>
2023-02-09 16:50:58 +0700

    maintainers: update toastal matrix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14864,7 +14864,7 @@
   };
   toastal = {
     email = "toastal+nix@posteo.net";
-    matrix = "@toastal:matrix.org";
+    matrix = "@toastal:chat.mozilla.org";
     github = "toastal";
     githubId = 561087;
     name = "toastal";
