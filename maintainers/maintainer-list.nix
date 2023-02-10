7ca8fddf5761afa278bc7dfed041cf3fc01e57e0
Pavel Sobolev <paveloom@riseup.net>
2023-02-10 16:57:12 +0300

    maintainers: add paveloom

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11305,6 +11305,12 @@
     githubId = 15645854;
     name = "Brad Christensen";
   };
+  paveloom = {
+    email = "paveloom@riseup.net";
+    github = "paveloom";
+    githubId = 49961859;
+    name = "Pavel Sobolev";
+  };
   payas = {
     email = "relekarpayas@gmail.com";
     github = "bhankas";
