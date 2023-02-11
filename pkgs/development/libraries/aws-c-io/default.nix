f6a7a4572fd57684f4b3869ec46f9715af79dc8f
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-11 09:58:24 +0000

    aws-c-io: 0.13.14 -> 0.13.15

--- pkgs/development/libraries/aws-c-io/default.nix
+++ pkgs/development/libraries/aws-c-io/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "aws-c-io";
-  version = "0.13.14";
+  version = "0.13.15";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-3ROSBjRvl036dslLZF5WgdBnlRvafrNaRzt65wfnk0s=";
+    sha256 = "sha256-UunYt5S8Xz/EZOojl+by3LaHidBE/P4Uwppm4SeHVP0=";
   };
 
   nativeBuildInputs = [ cmake ];
