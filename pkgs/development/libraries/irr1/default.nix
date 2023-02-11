9937c807887d7e784c86b346cfd135813284a771
Anderson Torres <torres.anderson.85@protonmail.com>
2023-02-11 15:25:22 -0300

    irr1: add meta.changelog

--- pkgs/development/libraries/irr1/default.nix
+++ pkgs/development/libraries/irr1/default.nix
@@ -4,25 +4,25 @@
 , cmake
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (self: {
   pname = "irr1";
   version = "1.9.4";
 
   src = fetchFromGitHub {
     owner = "berndporr";
     repo = "iir1";
-    rev = version;
+    rev = self.version;
     hash = "sha256-T8gl51IkZIGq+6D5ge4Kb3wm5aw7Rhphmnf6TTGwHbs=";
   };
 
   nativeBuildInputs = [ cmake ];
 
   meta = {
-    description = "A DSP IIR realtime filter library written in C++";
-    downloadPage = "https://github.com/berndporr/iir1";
     homepage = "http://berndporr.github.io/iir1/";
+    description = "A DSP IIR realtime filter library written in C++";
+    changelog = "https://github.com/berndporr/iir1/releases/tag/${self.version}";
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.AndersonTorres ];
     platforms = lib.platforms.unix;
   };
-}
+})
