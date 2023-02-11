51f387beffc5cb9a37d2b5cb3d25ea9594e5525e
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-02-11 09:04:08 +0100

    nushell: fix build on x86_64-darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26203,8 +26203,8 @@ with pkgs;
 
   nsh = callPackage ../shells/nsh { };
 
-  nushell = callPackage ../shells/nushell {
-    inherit (darwin.apple_sdk.frameworks) AppKit Foundation Security;
+  nushell = darwin.apple_sdk_11_0.callPackage ../shells/nushell {
+    inherit (darwin.apple_sdk_11_0.frameworks) AppKit Security;
     inherit (darwin.apple_sdk) sdk;
   };
 
