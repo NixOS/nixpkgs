NixOS/nixpkgs

commit 519f9b3d298e84a6ad2efda06d166203681cac33

v8_8_x: mark broken with GCC 12

pkgs/development/libraries/v8/8_x.nix
@@ -167,5 +167,6 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ cstrahan proglodyte matthewbauer ];
     platforms = platforms.unix;
     license = licenses.bsd3;
+    broken = lib.versionAtLeast stdenv.cc.version "12";
   };
 }

commit a302856aaac3023c2be10ea4632b90cf146ad9ee

v8_8_x: restrict compiler version check to GCC
Fixes: 519f9b3d298 ("v8_8_x: mark broken with GCC 12")

pkgs/development/libraries/v8/8_x.nix
@@ -167,6 +167,6 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ cstrahan proglodyte matthewbauer ];
     platforms = platforms.unix;
     license = licenses.bsd3;
-    broken = lib.versionAtLeast stdenv.cc.version "12";
+    broken = stdenv.cc.isGNU && lib.versionAtLeast stdenv.cc.version "12";
   };
 }