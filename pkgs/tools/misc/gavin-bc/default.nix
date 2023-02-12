792cc87a6b0a6b8b152e8ee1e07d70608a37c55e
Anderson Torres <torres.anderson.85@protonmail.com>
2023-02-12 17:11:12 -0300

    gavin-bc: init at 6.2.4

---
+++ pkgs/tools/misc/gavin-bc/default.nix
@@ -0,0 +1,25 @@
+{ lib
+, stdenv
+, fetchFromGitea
+}:
+
+stdenv.mkDerivation (self: {
+  pname = "gavin-bc";
+  version = "6.2.4";
+
+  src = fetchFromGitea {
+    domain = "git.gavinhoward.com";
+    owner = "gavin";
+    repo = "bc";
+    rev = self.version;
+    hash = "sha256-KQheSyBbxh2ROOvwt/gqhJM+qWc+gDS/x4fD6QIYUWw=";
+  };
+
+  meta = {
+    homepage = "https://git.gavinhoward.com/gavin/bc";
+    description = "Gavin Howard's BC calculator implementation";
+    license = lib.licenses.bsd2;
+    maintainers = [ lib.maintainers.AndersonTorres ];
+    platforms = lib.platforms.unix;
+  };
+})
