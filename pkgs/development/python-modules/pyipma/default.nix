c7bf2624e969e08cce94b15d327639b27c0776ed
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-12 13:06:55 +0100

    python310Packages.pyipma: add changelog to meta

--- pkgs/development/python-modules/pyipma/default.nix
+++ pkgs/development/python-modules/pyipma/default.nix
@@ -52,6 +52,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library to retrieve information from Instituto Português do Mar e Atmosfera";
     homepage = "https://github.com/dgomes/pyipma";
+    changelog = "https://github.com/dgomes/pyipma/releases/tag/${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
