dd363844d277284af4e64d58a52dd702419b2329
Nikolay Korotkiy <sikmir@disroot.org>
2023-02-12 18:29:29 +0300

    picotool: remove installPhase

--- pkgs/development/tools/picotool/default.nix
+++ pkgs/development/tools/picotool/default.nix
@@ -15,12 +15,6 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ cmake pkg-config ];
   cmakeFlags = [ "-DPICO_SDK_PATH=${pico-sdk}/lib/pico-sdk" ];
 
-  installPhase = ''
-    runHook preInstall
-    install -Dm755 ./picotool -t $out/bin
-    runHook postInstall
-  '';
-
   meta = with lib; {
     homepage = "https://github.com/raspberrypi/picotool";
     description = "Tool for interacting with a RP2040 device in BOOTSEL mode, or with a RP2040 binary";
