NixOS/nixpkgs

commit afc23a5a3b42ce7498743edfb7ffc32d74a78023

python3Packages.mediapy: 1.1.0 -> 1.1.2

pkgs/development/python-modules/mediapy/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage rec {
   pname = "mediapy";
-  version = "1.1.0";
+  version = "1.1.2";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-CejgiCiW7an1GpKB5MUiA1Alkigv3RmfTq0um9pc93E=";
+    hash = "sha256-uz+Pv3DvmhDajIqNpCj+2HsRT7Dh7Ks5yryhiEa8KOI=";
   };
 
   propagatedBuildInputs = [ ipython matplotlib numpy pillow ];

commit 55070e598e0e03d1d116c49b9eff322ef07c6ac6

python3.pkgs.mediapy: fix build
Changed to flit in 1.1.2.

Fixes: afc23a5a3b4 ("python3Packages.mediapy: 1.1.0 -> 1.1.2")

pkgs/development/python-modules/mediapy/default.nix
@@ -21,6 +21,8 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [ ipython matplotlib numpy pillow ];
 
+  format = "flit";
+
   pythonImportsCheck = [ "mediapy" ];
 
   meta = with lib; {