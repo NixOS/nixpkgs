0d48da7e29148184fcb8372a1fd76a92d08ebd06
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-13 06:07:10 +0000

    python310Packages.vowpalwabbit: 9.6.0 -> 9.7.0

--- pkgs/development/python-modules/vowpalwabbit/default.nix
+++ pkgs/development/python-modules/vowpalwabbit/default.nix
@@ -18,11 +18,11 @@
 
 buildPythonPackage rec {
   pname = "vowpalwabbit";
-  version = "9.6.0";
+  version = "9.7.0";
 
   src = fetchPypi{
     inherit pname version;
-    sha256 = "sha256-LHs98lccPjub5GDPB3dxtkOYCDxri04CVpSSsypa4xI=";
+    sha256 = "sha256-O3WnG3JQ7CEHMPYTmE9pgnS8YZs717P6wBKBnXIvvFI=";
   };
 
   nativeBuildInputs = [
