3aab0493d84c4786944a3c589e200ea0afa15ba7
Tobias Markus <tobias@miglix.eu>
2023-02-14 00:06:56 +0100

    scons: 4.3.0 -> 4.4.0
    Release notes: https://github.com/SCons/scons/releases/tag/4.4.0

--- pkgs/development/tools/build-managers/scons/default.nix
+++ pkgs/development/tools/build-managers/scons/default.nix
@@ -7,10 +7,10 @@ let
 in {
   scons_3_1_2 = (mkScons {
     version = "3.1.2";
-    sha256 = "1yzq2gg9zwz9rvfn42v5jzl3g4qf1khhny6zfbi2hib55zvg60bq";
+    hash = "sha256-eAHz9i9lRSjict94C+EMDpM36JdlC2Ldzunzn94T+Ps=";
   });
   scons_latest = mkScons {
-    version = "4.3.0";
-    sha256 = "1v5x2zqnlxg19i58782279x3zqyz2l3j79k41jf2k4a499sq3z1f";
+    version = "4.4.0";
+    hash = "sha256-PUOyMDqSSBbqDhs0X/BMmz4ntT6t8PJgEvwMKbAZaF8=";
   };
 }
