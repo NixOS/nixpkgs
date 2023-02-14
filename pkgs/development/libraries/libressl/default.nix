53abbbbd5ad16f8641bdc91cbee9b196f1e005b5
Ruud van Asseldonk <dev@veniogames.com>
2023-02-14 21:00:47 +0100

    libressl_3_4: backport security fix
    (cherry picked from commit b958f017b7f27de0064cbc3e1a16e2020f2acce3)

--- pkgs/development/libraries/libressl/default.nix
+++ pkgs/development/libraries/libressl/default.nix
@@ -87,6 +87,16 @@ in {
   libressl_3_4 = generic {
     version = "3.4.3";
     hash = "sha256-/4i//jVIGLPM9UXjyv5FTFAxx6dyFwdPUzJx1jw38I0=";
+    patches = [
+      (fetchpatch {
+        # https://marc.info/?l=libressl&m=167582148932407&w=2
+        name = "backport-type-confusion-fix.patch";
+        url = "https://raw.githubusercontent.com/libressl/portable/30dc760ed1d7c70766b135500950d8ca9d17b13a/patches/x509_genn.c.diff";
+        sha256 = "sha256-N9jsOueqposDWZwaR+n/v/cHgNiZbZ644d8/wKjN2/M=";
+        stripLen = 2;
+        extraPrefix = "crypto/";
+      })
+    ];
   };
 
   libressl_3_5 = generic {
