3926d8c482fcb577f22e2c68ed36d537fb6a927c
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-02-14 17:53:57 +0000

    graalvmCEPackages.ruby-installable-svm: disable broken test in darwin

--- pkgs/development/compilers/graalvm/community-edition/ruby-installable-svm.nix
+++ pkgs/development/compilers/graalvm/community-edition/ruby-installable-svm.nix
@@ -30,6 +30,9 @@ graalvmCEPackages.buildGraalvmProduct rec {
     export LANG=C
     export LC_ALL=C
     $out/bin/ruby -e 'puts(1 + 1)'
-    echo '1 + 1' | $out/bin/irb
+    ${# broken in darwin with sandbox enabled
+      lib.optionalString stdenv.isLinux ''
+      echo '1 + 1' | $out/bin/irb
+    ''}
   '';
 }
