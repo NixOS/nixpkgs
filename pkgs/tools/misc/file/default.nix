2e759a9a92e4085582ba063f1625a8e948c25d80
K900 <me@0upti.me>
2023-02-14 20:18:50 +0300

    file: backport another regression fix

--- pkgs/tools/misc/file/default.nix
+++ pkgs/tools/misc/file/default.nix
@@ -22,6 +22,12 @@ stdenv.mkDerivation rec {
     # Needed for strip-nondeterminism.
     # https://salsa.debian.org/reproducible-builds/strip-nondeterminism/-/issues/20
     ./pyzip.patch
+
+    # Backport fix for --uncompress always detecting contents as "empty"
+    (fetchurl {
+      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/sys-apps/file/files/file-5.44-decompress-empty.patch?h=dfc57da515a2aaf085bea68267cc727f1bfaa691";
+      hash = "sha256-fUzRQAlLWczBmR5iA1Gk66mHjP40MJcMdgCtm2+u1SQ=";
+    })
   ];
 
   strictDeps = true;
