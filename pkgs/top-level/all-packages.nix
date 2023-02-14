bde026b382ad055731adf20dde7cde22a91a5eb6
Frederik Rietdijk <fridh@fridh.nl>
2023-02-14 18:08:00 +0100

    eccodes: build with gfortran 12 on darwin
    this makes the build pass

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18729,6 +18729,7 @@ with pkgs;
   eccodes = callPackage ../development/libraries/eccodes {
     pythonPackages = python3Packages;
     stdenv = if stdenv.isDarwin then gccStdenv else stdenv;
+    gfortran = if stdenv.isDarwin then gfortran12 else gfortran;
   };
 
   eclib = callPackage ../development/libraries/eclib {};
