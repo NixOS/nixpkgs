NixOS/nixpkgs

commit bf928e557940be4e10042b1a82d9dcce1776f6ba

libinput: 1.21.0 → 1.22.1
https://lists.freedesktop.org/archives/wayland-devel/2022-November/042515.html
https://gitlab.freedesktop.org/libinput/libinput/-/compare/1.21.0...1.22.1

pkgs/development/libraries/libinput/default.nix
@@ -45,7 +45,7 @@ in
 
 stdenv.mkDerivation rec {
   pname = "libinput";
-  version = "1.21.0";
+  version = "1.22.1";
 
   outputs = [ "bin" "out" "dev" ];
 
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
     owner = "libinput";
     repo = "libinput";
     rev = version;
-    sha256 = "R94BdrjI4szNbVtQ+ydRNUg9clR8mkRL7+GE9b2FcDs=";
+    sha256 = "RgwEp60Anr+CpJws6srIv/Qzk2r9NoekeNQ0UT3FRZ0=";
   };
 
   patches = [

commit 3c318b78337a77401dd399adafce25561b30b7d0

libinput: don't try to create /etc in build
How libinput tries to create /etc changed in upstream commit
05501cd34f9c ("meson: use install_emptydir to create directory"),
so we have to update our sed expression.

This was missed in when libinput was updated in Nixpkgs due to a Nix
bug[1], which made the sandbox's /etc writeable.

[1]: https://github.com/NixOS/nix/issues/7813

Fixes: bf928e55794 ("libinput: 1.21.0 → 1.22.1")

pkgs/development/libraries/libinput/default.nix
@@ -113,8 +113,8 @@ stdenv.mkDerivation rec {
       test/check-leftover-udev-rules.sh \
       test/helper-copy-and-exec-from-tmp.sh
 
-    # Don't create an empty /etc directory.
-    sed -i "/install_subdir('libinput', install_dir : dir_etc)/d" meson.build
+    # Don't create an empty directory under /etc.
+    sed -i "/install_emptydir(dir_etc \/ 'libinput')/d" meson.build
   '';
 
   passthru = {