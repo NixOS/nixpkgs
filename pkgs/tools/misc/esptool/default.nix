db7f30dae56a777d117028e109a7a7cb0eb98bd4
Robert Schütz <nix@dotlambda.de>
2023-02-14 13:40:38 -0800

    esptool: 4.4 -> 4.5
    Diff: https://github.com/espressif/esptool/compare/v4.4...v4.5
    
    Changelog: https://github.com/espressif/esptool/releases/tag/v4.5

--- pkgs/tools/misc/esptool/default.nix
+++ pkgs/tools/misc/esptool/default.nix
@@ -1,28 +1,18 @@
 { lib
 , fetchFromGitHub
-, fetchpatch
 , python3
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "esptool";
-  version = "4.4";
+  version = "4.5";
 
   src = fetchFromGitHub {
     owner = "espressif";
     repo = "esptool";
     rev = "v${version}";
-    hash = "sha256-haLwf3loOvqdqQN/iuVBciQ6nCnuc9AqqOGKvDwLBHE=";
+    hash = "sha256-SwMdemCk3e3RyXTzoXIqDRywpg3ogE9nQjXGBz0BjwE=";
   };
-
-  patches = [
-    ./test-call-bin-directly.patch
-    (fetchpatch {
-      name = "bitstring-4-compatibility.patch";
-      url = "https://github.com/espressif/esptool/commit/ee27a6437576797d5f58c31e1c39f3a232a71df0.patch";
-      hash = "sha256-8/AzR3HK79eQQRSaGEKU4YKn/piPCPjm/G9pvizKuUE=";
-    })
-  ];
 
   propagatedBuildInputs = with python3.pkgs; [
     bitstring
@@ -34,19 +24,18 @@ python3.pkgs.buildPythonApplication rec {
 
   nativeCheckInputs = with python3.pkgs; [
     pyelftools
-    pytest
+    pytestCheckHook
   ];
 
   # tests mentioned in `.github/workflows/test_esptool.yml`
   checkPhase = ''
     runHook preCheck
 
-    export ESPSECURE_PY=$out/bin/espsecure.py
-    export ESPTOOL_PY=$out/bin/esptool.py
-    ${python3.interpreter} test/test_imagegen.py
-    ${python3.interpreter} test/test_espsecure.py
-    ${python3.interpreter} test/test_merge_bin.py
-    ${python3.interpreter} test/test_modules.py
+    pytest test/test_imagegen.py
+    pytest test/test_espsecure.py
+    pytest test/test_merge_bin.py
+    pytest test/test_image_info.py
+    pytest test/test_modules.py
 
     runHook postCheck
   '';
