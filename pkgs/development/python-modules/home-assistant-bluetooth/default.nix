28fe8687bdc8d9cc0a9066b475dc92a70585b6eb
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-14 07:49:01 +0100

    python310Packages.home-assistant-bluetooth: 1.9.2 -> 1.9.3
    Diff: https://github.com/home-assistant-libs/home-assistant-bluetooth/compare/refs/tags/v1.9.2...v1.9.3
    
    Changelog: https://github.com/home-assistant-libs/home-assistant-bluetooth/blob/v1.9.3/CHANGELOG.md

--- pkgs/development/python-modules/home-assistant-bluetooth/default.nix
+++ pkgs/development/python-modules/home-assistant-bluetooth/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "home-assistant-bluetooth";
-  version = "1.9.2";
+  version = "1.9.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "home-assistant-libs";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-8RGwWx/I6Log7qyGiP+LAWC1GCG2A7UEXnO5fGoTsb4=";
+    hash = "sha256-7wZocfTYTwTBwm61hKmIS4xlHq2nSvC6p8SlklnHq4M=";
   };
 
   postPatch = ''
