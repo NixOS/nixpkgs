2cb7eb70eac06bb05a15d52040e509e94bbe8153
Sean Buckley <sean.bck@gmail.com>
2023-02-15 13:02:08 -0500

    brave: 1.48.158 -> 1.48.164
    https://community.brave.com/t/release-channel-1-48-164/470577/1

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -90,11 +90,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "brave";
-  version = "1.48.158";
+  version = "1.48.164";
 
   src = fetchurl {
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-    sha256 = "sha256-hgV8+Mjth43KItly98lTh3Ur+h9vA9BfJxOqMGl1ndY=";
+    sha256 = "sha256-aZqxMx1hbk5vkVMETsoSvn5cgc9jYUeMWfRwCN5s/c0=";
   };
 
   dontConfigure = true;
