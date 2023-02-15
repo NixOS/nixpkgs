b8701174d480381223c582cb8b449b6ed612a8ad
toastal <toastal@posteo.net>
2023-02-15 17:23:58 +0700

    amdvlk: 2022.Q4.4 → 2023.Q1.2
    Tag: https://github.com/GPUOpen-Drivers/AMDVLK/releases/tag/v-2023.Q1.2

--- pkgs/development/libraries/amdvlk/default.nix
+++ pkgs/development/libraries/amdvlk/default.nix
@@ -25,13 +25,13 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "amdvlk";
-  version = "2022.Q4.4";
+  version = "2023.Q1.2";
 
   src = fetchRepoProject {
     name = "${pname}-src";
     manifest = "https://github.com/GPUOpen-Drivers/AMDVLK.git";
     rev = "refs/tags/v-${version}";
-    sha256 = "sha256-MKU7bfjrvH4M2kON2tr5463nYjN1xoGAknsC9YmklEc=";
+    sha256 = "sha256-QNjBLOnSfCTA+5qLqejAqJv9eIWAEVNc/VrhidGjmTc=";
   };
 
   buildInputs = [
