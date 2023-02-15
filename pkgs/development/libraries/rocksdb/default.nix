5dfdb62adf9a5e6c3ada09ebd1775dc93f892c4a
HuangYi <huang@crypto.com>
2023-02-15 15:24:35 +0800

    rocksdb: fix cross-compiling on darwin
    install_name_tool not found when crossing compiling on darwin

--- pkgs/development/libraries/rocksdb/default.nix
+++ pkgs/development/libraries/rocksdb/default.nix
@@ -79,7 +79,7 @@ stdenv.mkDerivation rec {
     mkdir -p $tools/bin
     cp tools/{ldb,sst_dump}${stdenv.hostPlatform.extensions.executable} $tools/bin/
   '' + lib.optionalString stdenv.isDarwin ''
-    ls -1 $tools/bin/* | xargs -I{} install_name_tool -change "@rpath/librocksdb.7.dylib" $out/lib/librocksdb.dylib {}
+    ls -1 $tools/bin/* | xargs -I{} ${stdenv.cc.targetPrefix}install_name_tool -change "@rpath/librocksdb.7.dylib" $out/lib/librocksdb.dylib {}
   '' + lib.optionalString (stdenv.isLinux && enableShared) ''
     ls -1 $tools/bin/* | xargs -I{} patchelf --set-rpath $out/lib:${stdenv.cc.cc.lib}/lib {}
   '';
