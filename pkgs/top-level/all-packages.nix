379bb7c2afe275ccef2c64b1ec471b73a0bace9d
GenericNerdyUsername <genericnerdyusername@proton.me>
2023-02-15 13:01:55 +0000

    xemu: init at 0.7.84

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2263,6 +2263,8 @@ with pkgs;
 
   xcpc = callPackage ../applications/emulators/xcpc { };
 
+  xemu = callPackage ../applications/emulators/xemu { };
+
   yapesdl = callPackage ../applications/emulators/yapesdl { };
 
   zesarux = callPackage ../applications/emulators/zesarux { };
