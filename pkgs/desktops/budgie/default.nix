1c9ad3ec47cd529d0681f602d4337cd91d57fd75
Federico Damián Schonborn <fdschonborn@gmail.com>
2023-02-16 19:14:29 -0300

    budgie.budgie-control-center: init at 1.2.0
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- pkgs/desktops/budgie/default.nix
+++ pkgs/desktops/budgie/default.nix
@@ -1,6 +1,7 @@
 { lib, pkgs }:
 
 lib.makeScope pkgs.newScope (self: with self; {
+  budgie-control-center = callPackage ./budgie-control-center { };
   budgie-desktop = callPackage ./budgie-desktop { };
   budgie-desktop-view = callPackage ./budgie-desktop-view { };
   budgie-screensaver = callPackage ./budgie-screensaver { };
