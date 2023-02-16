05e4fd7a6cbf1c8700028304b2ba6d02dcf359e9
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-17 00:20:46 +0100

    python310Packages.types-python-dateutil: 2.8.19.6 -> 2.8.19.7

--- pkgs/development/python-modules/types-python-dateutil/default.nix
+++ pkgs/development/python-modules/types-python-dateutil/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "types-python-dateutil";
-  version = "2.8.19.2";
+  version = "2.8.19.7";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-5uMs4Y83dlsIxGYiKHvI2BNtwMVi2a1bj9FYxZlj16c=";
+    hash = "sha256-evWl0bgKsd+guk2Hn6yzgug2piwtQIwqUJvkaA/Yscg=";
   };
 
   # Modules doesn't have tests
