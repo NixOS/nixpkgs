db9b8f62b87466a08f0d92df2677db9849ddd025
Federico Damián Schonborn <fdschonborn@gmail.com>
2023-02-16 17:27:35 -0300

    budgie.budgie-desktop-view: init at 1.2.1
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- pkgs/desktops/budgie/default.nix
+++ pkgs/desktops/budgie/default.nix
@@ -2,5 +2,6 @@
 
 lib.makeScope pkgs.newScope (self: with self; {
   budgie-desktop = callPackage ./budgie-desktop { };
+  budgie-desktop-view = callPackage ./budgie-desktop-view { };
   budgie-screensaver = callPackage ./budgie-screensaver { };
 })
