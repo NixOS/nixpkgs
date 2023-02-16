f412a37e355727d33271f96c3d41548770c192b3
Jonas Heinrich <onny@project-insanity.org>
2023-02-16 11:52:35 +0100

    nixos/networkd-dispatcher: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -912,6 +912,7 @@
   ./services/networking/ndppd.nix
   ./services/networking/nebula.nix
   ./services/networking/netbird.nix
+  ./services/networking/networkd-dispatcher.nix
   ./services/networking/networkmanager.nix
   ./services/networking/nextdns.nix
   ./services/networking/nftables.nix
