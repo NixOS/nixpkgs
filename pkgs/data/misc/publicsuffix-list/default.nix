c41884241c0ac8973a4be5b54925c7f7b21e46dd
Bheesham Persaud <me@bheesham.com>
2023-02-16 19:19:54 -0500

    publicsuffix-list: unstable-2021-09-03 -> unstable-2023-02-16

--- pkgs/data/misc/publicsuffix-list/default.nix
+++ pkgs/data/misc/publicsuffix-list/default.nix
@@ -2,13 +2,13 @@
 
 let
   pname = "publicsuffix-list";
-  version = "2021-09-03";
+  version = "2023-02-16";
 in fetchFromGitHub {
   name = "${pname}-${version}";
   owner = "publicsuffix";
   repo = "list";
-  rev = "2533d032871e1ef1f410fc0754b848d4587c8021";
-  sha256 = "sha256-Q8uIXM1CMu8dlWcVoL17M1XRGu3kG7Y7jpx0oHQh+2I=";
+  rev = "8ec4d3049fe139f92937b6137155c33b81dcaf18";
+  sha256 = "sha256-wA8zk0iADFNP33veIf+Mfx22zdMzHsMNWEizMp1SnuA=";
 
   postFetch = ''
     install -Dm0444 $out/public_suffix_list.dat $out/tests/test_psl.txt -t $out/share/publicsuffix
