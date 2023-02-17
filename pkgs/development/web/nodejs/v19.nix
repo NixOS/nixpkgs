4c52bc162af2ddac5a32d02a6841f6209f863ace
Mario Rodas <marsam@users.noreply.github.com>
2023-02-17 04:20:00 +0000

    nodejs-19_x: 19.6.0 -> 19.6.1
    https://github.com/nodejs/node/releases/tag/v19.6.1
    (cherry picked from commit d42631d6c59fabbb3b4a6109cce8f22e75761b2f)

--- pkgs/development/web/nodejs/v19.nix
+++ pkgs/development/web/nodejs/v19.nix
@@ -9,8 +9,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "19.6.0";
-  sha256 = "sha256-UZxtVCqfmW8AwaPTsuXPUrfbmY2V9s7VqJPRagPeM+o=";
+  version = "19.6.1";
+  sha256 = "sha256-dxDmwoUclWvkkm/CVAZ48/3fjm04Juo6qbjCjW6omps=";
   patches = [
     ./revert-arm64-pointer-auth.patch
     ./disable-darwin-v8-system-instrumentation-node19.patch
