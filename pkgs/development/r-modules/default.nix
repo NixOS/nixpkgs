fd26819f9b27c498565f10eae0954682027d9a6e
nviets <nathan.g.viets@gmail.com>
2023-02-16 22:25:35 -0500

    rPackages.quarto: add quarto dependency

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -1044,6 +1044,13 @@ let
       '';
     });
 
+    quarto = old.quarto.overrideDerivation (attrs: {
+      postPatch = ''
+        substituteInPlace "R/quarto.R" \
+          --replace "path_env <- Sys.getenv(\"QUARTO_PATH\", unset = NA)" "path_env <- Sys.getenv(\"QUARTO_PATH\", unset = '${lib.getBin pkgs.quarto}/bin/quarto')"
+      '';
+    });
+
     s2 = old.s2.overrideDerivation (attrs: {
       PKGCONFIG_CFLAGS = "-I${pkgs.openssl.dev}/include";
       PKGCONFIG_LIBS = "-Wl,-rpath,${lib.getLib pkgs.openssl}/lib -L${lib.getLib pkgs.openssl}/lib -lssl -lcrypto";
