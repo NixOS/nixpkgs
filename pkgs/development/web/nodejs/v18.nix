a628b11b19093362a3be3668c1772131bc648f2b
Mario Rodas <marsam@users.noreply.github.com>
2023-02-17 04:20:00 +0000

    nodejs-18_x: 18.14.0 -> 18.14.1
    https://github.com/nodejs/node/releases/tag/v18.14.1

--- pkgs/development/web/nodejs/v18.nix
+++ pkgs/development/web/nodejs/v18.nix
@@ -1,24 +1,17 @@
-{ callPackage, openssl, fetchpatch, python3, enableNpm ? true }:
+{ callPackage, fetchpatch, openssl, python3, enableNpm ? true }:
 
 let
   buildNodejs = callPackage ./nodejs.nix {
     inherit openssl;
     python = python3;
   };
+
 in
 buildNodejs {
   inherit enableNpm;
-  version = "18.12.0";
-  sha256 = "sha256-c6fwHimZ6xl3Y87WZqbNVErVgOrvtz4KhJYDs+gE9C4=";
+  version = "18.14.1";
+  sha256 = "sha256-7sNTQ4Jm/QrvU6lEa+ELMu5udNCOMt1UVLOC/2eT2gQ=";
   patches = [
-    (fetchpatch {
-      # Fixes cross compilation to aarch64-linux by reverting https://github.com/nodejs/node/pull/43200
-      name = "revert-arm64-pointer-auth.patch";
-      url = "https://github.com/nodejs/node/pull/43200/commits/d42c42cc8ac652ab387aa93205aed6ece8a5040a.patch";
-      sha256 = "sha256-ipGzg4lEoftTJbt6sW+0QJO/AZqHvUkFKe0qlum+iLY=";
-      revert = true;
-    })
-
     ./disable-darwin-v8-system-instrumentation.patch
     ./bypass-darwin-xcrun-node16.patch
   ];
