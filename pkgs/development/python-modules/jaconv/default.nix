af1a1062417456fb9188283daa502631c9a469f4
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-18 01:20:21 +0100

    python310Packages.jaconv: 0.3.1 -> 0.3.4
    Diff: https://github.com/ikegami-yukino/jaconv/compare/refs/tags/v0.3.1...v0.3.4
    
    Changelog: https://github.com/ikegami-yukino/jaconv/blob/v0.3.4/CHANGES.rst

--- pkgs/development/python-modules/jaconv/default.nix
+++ pkgs/development/python-modules/jaconv/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "jaconv";
-  version = "0.3.1";
+  version = "0.3.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "ikegami-yukino";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-uzGHvklFHVoNloZauczgITeHQIgYQAfI9cjLWgG/vyI=";
+    hash = "sha256-9ruhOLaYNESeKOwJs3IN6ct66fSq7My9DOyA7/cH3d0=";
   };
 
   nativeCheckInputs = [
