3784756e4f114133a891ea7d13e68545d4a10200
Vladimír Čunát <v@cunat.cz>
2023-02-18 20:53:08 +0100

    thunderbird-bin: fix update.nix after PR #214996

--- pkgs/applications/networking/browsers/firefox-bin/update.nix
+++ pkgs/applications/networking/browsers/firefox-bin/update.nix
@@ -1,5 +1,6 @@
 { pname
 , channel
+, lib
 , writeScript
 , xidel
 , coreutils
@@ -46,7 +47,7 @@ in writeScript "update-${pname}" ''
            grep "^[0-9]" | \
            sort --version-sort | \
            grep -v "funnelcake" | \
-           grep -e "${lib.optionalString isBeta "b"}\([[:digit:]]\|[[:digit:]][[:digit:]]\)$" | ${lib.optionalString (not isBeta) "grep -v \"b\" |"} \
+           grep -e "${lib.optionalString isBeta "b"}\([[:digit:]]\|[[:digit:]][[:digit:]]\)$" | ${lib.optionalString (!isBeta) "grep -v \"b\" |"} \
            tail -1`
 
   curl --silent -o $HOME/shasums "$url$version/SHA256SUMS"
