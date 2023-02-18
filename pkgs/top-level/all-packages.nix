48642c634e51b949d3ea01f09c28c83f9aa824d9
zowoq <59103226+zowoq@users.noreply.github.com>
2023-02-18 20:58:23 +1000

    podman: remove wrapper
    trying to get all of the podman functionality to work with the wrapper
     is becoming more complicated with each release, it isn't sustainable
    
    removing the wrapper does mean that using extraPackages will need to build from source
    
    - remove unnecessary serviceConfig overrides
    
    - set HELPER_BINARIES_DIR to libexec/podman
    
    - use install.bin target on linux for podman/tmpfiles
      - also installs quadlet/rootlessport in libexec
    
    - symlink binaries from helpersBin into HELPER_BINARIES_DIR
    
    - remove unnecessary rootlessport output
    
    - remove unnecessary substituteInPlace

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11074,8 +11074,7 @@ with pkgs;
 
   pocketbase = callPackage ../servers/pocketbase { };
 
-  podman = callPackage ../applications/virtualization/podman/wrapper.nix { };
-  podman-unwrapped = callPackage ../applications/virtualization/podman { };
+  podman = callPackage ../applications/virtualization/podman { };
 
   podman-compose = python3Packages.callPackage ../applications/virtualization/podman-compose {};
 
