5ad44b55305bc175e01ed65f28ef05d0d948001a
sternenseemann <sternenseemann@systemli.org>
2023-02-18 20:33:07 +0100

    haskellPackages.guardian: fix build by joining cabal-install overlay
    The package depends on cabal-install and Cabal which need later versions
    of a few packages than we have in our default package set.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -41,9 +41,25 @@ self: super: {
     {
       cabal-install = super.cabal-install.overrideScope cabalInstallOverlay;
       cabal-install-solver = super.cabal-install-solver.overrideScope cabalInstallOverlay;
+
+      guardian = lib.pipe
+        # Needs cabal-install >= 3.8 /as well as/ matching Cabal
+        (super.guardian.overrideScope (self: super:
+          cabalInstallOverlay self super // {
+            # Needs at least path-io 1.8.0 due to canonicalizePath changes
+            path-io = self.path-io_1_8_0;
+          }
+        ))
+        [
+          # Tests need internet access (run stack)
+          dontCheck
+          # May as well…
+          (self.generateOptparseApplicativeCompletions [ "guardian" ])
+        ];
     }
   ) cabal-install
     cabal-install-solver
+    guardian
   ;
 
 
