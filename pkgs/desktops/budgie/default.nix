297cfa71e64a53016f17d4aedf8b312843200b7c
Federico Damián Schonborn <fdschonborn@gmail.com>
2023-02-17 23:00:01 -0300

    budgie.budgie-gsettings-overrides: init
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- pkgs/desktops/budgie/default.nix
+++ pkgs/desktops/budgie/default.nix
@@ -5,5 +5,6 @@ lib.makeScope pkgs.newScope (self: with self; {
   budgie-control-center = callPackage ./budgie-control-center { };
   budgie-desktop = callPackage ./budgie-desktop { };
   budgie-desktop-view = callPackage ./budgie-desktop-view { };
+  budgie-gsettings-overrides = callPackage ./budgie-gsettings-overrides { };
   budgie-screensaver = callPackage ./budgie-screensaver { };
 })
