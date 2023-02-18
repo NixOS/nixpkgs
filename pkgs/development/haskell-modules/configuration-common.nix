1f99c7d53c68449e19a6b4234258e2e3c249dc90
sternenseemann <sternenseemann@systemli.org>
2023-02-18 19:09:12 +0100

    haskellPackages.swarm: use now required brick 1.6

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2269,7 +2269,7 @@ self: super: {
   # 2022-11-15: Needs newer witch package and brick 1.3 which in turn works with text-zipper 0.12
   # Other dependencies are resolved with doJailbreak for both swarm and brick_1_3
   swarm = doJailbreak (super.swarm.override {
-    brick = doJailbreak (dontCheck super.brick_1_3);
+    brick = doJailbreak (dontCheck super.brick_1_6);
   });
 
   # Too strict upper bound on bytestring
