76d6ff8946dc7ecd84a16c90e0929822f298b696
Camille M. <camillemondon@free.fr>
2023-02-18 22:18:01 +0100

    added texliveScheme and settings option

--- nixos/modules/services/web-apps/overleaf.nix
+++ nixos/modules/services/web-apps/overleaf.nix
@@ -2,56 +2,6 @@
 
 let
   cfg = config.services.overleaf;
-  env = {
-    CHAT_HOST = "localhost";
-    CLSI_HOST = "localhost";
-    CONTACTS_HOST = "localhost";
-    DOCSTORE_HOST = "localhost";
-    DOCUMENT_UPDATER_HOST = "localhost";
-    FILESTORE_HOST = "localhost";
-    GRACEFUL_SHUTDOWN_DELAY = "0";
-    LISTEN_ADDRESS = "localhost";
-    MONGO_HOST = "localhost";
-    MONGO_CONNECTION_STRING = "mongodb://localhost:27017/overleaf";
-    NOTIFICATIONS_HOST = "localhost";
-    REALTIME_HOST = "localhost";
-    OVERLEAF_REDIS_PATH = "/run/redis-overleaf/redis.sock";
-    #OVERLEAF_REDIS_HOST = "localhost";
-    #OVERLEAF_REDIS_PORT = "6379";
-    #OVERLEAF_REDIS_PASS = "changeme";
-    SPELLING_HOST = "localhost";
-    TRACK_CHANGES_HOST = "localhost";
-    WEBPACK_HOST = "localhost";
-    WEB_API_PASSWORD = "overleaf";
-    WEB_API_USER = "overleaf";
-    WEB_API_PORT = "3032";
-    WEB_API_HOST = "localhost";
-    WEB_HOST = "localhost";
-    WEB_PORT = "3032";
-    DATA_DIR = "/var/lib/overleaf";
-    NODE_ENV = "production";
-    OVERLEAF_CONFIG = "${pkgs.overleaf}/share/server-ce/config/settings.js";
-    OVERLEAF_FPH_DISPLAY_NEW_PROJECTS = "true";
-    OVERLEAF_MONGO_URL = "mongodb://localhost:27017/overleaf";
-    OVERLEAF_SESSION_SECRET = "hello";
-    OVERLEAF_HISTORY_BACKEND = "fs";
-    OVERLEAF_HISTORY_BLOBS_BUCKET = "/var/lib/overleaf/data/history/overleaf-blobs";
-    OVERLEAF_HISTORY_PROJECT_BLOBS_BUCKET = "/var/lib/overleaf/data/history/overleaf-project-blobs";
-    OVERLEAF_HISTORY_CHUNKS_BUCKET = "/var/lib/overleaf/data/history/overleaf-chunks";
-    OVERLEAF_HISTORY_ZIPS_BUCKET = "/var/lib/overleaf/data/history/overleaf-zips";
-    OVERLEAF_HISTORY_ANALYTICS_BUCKET = "/var/lib/overleaf/data/history/overleaf-analytics";
-    ADMIN_PRIVILEGE_AVAILABLE = "true";
-    OVERLEAF_SITE_URL = "https://verso.saumon.network";
-    OVERLEAF_ADMIN_EMAIL = "postmaster@mondon.me";
-    #OVERLEAF_SITE_LANGUAGE = "fr";
-    OVERLEAF_APP_NAME = "Verso, powered by SaumonNetwork";
-    #OVERLEAF_EMAIL_FROM_ADDRESS = "";
-    #OVERLEAF_EMAIL_SMTP_HOST = "";
-    #OVERLEAF_EMAIL_SMTP_PORT = "587";
-    #OVERLEAF_EMAIL_SMTP_SECURE = "true";
-    #OVERLEAF_EMAIL_SMTP_USER = "";
-    #OVERLEAF_EMAIL_SMTP_PASS = "";
-  };
 in
 with lib;
 
@@ -74,17 +24,88 @@ with lib;
       description = mdDoc ''The type of MongoDB to enable. One of "mongodb", "ferretdb" or "none"'';
     };
 
-    enableNginxConfig = mkEnableOption (mdDoc ''Nginx config'');
+    texliveScheme = mkOption {
+      type = types.str;
+      default = "basic";
+      example = "full";
+      description = mdDoc ''The package for TeX Live. See
+      <https://search.nixos.org/packages?query=texlive.combined>
+      for available options.'';
+    };
+
+    enableNginxConfig = mkEnableOption (mdDoc ''Nginx example config'');
+
+    settings = lib.mkOption {
+      type = lib.types.submodule { freeformType = with lib.types; attrsOf str; };
+      default = { };
+      example = {
+        OVERLEAF_REDIS_HOST = "localhost";
+        OVERLEAF_REDIS_PORT = "6379";
+        OVERLEAF_REDIS_PASS = "changeme";
+      };
+      description = lib.mdDoc ''
+        Additional configuration for Overleaf, see
+        for supported values.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
+    services.overleaf.settings = {
+      CHAT_HOST = "localhost";
+      CLSI_HOST = "localhost";
+      CONTACTS_HOST = "localhost";
+      DOCSTORE_HOST = "localhost";
+      DOCUMENT_UPDATER_HOST = "localhost";
+      FILESTORE_HOST = "localhost";
+      GRACEFUL_SHUTDOWN_DELAY = "0";
+      LISTEN_ADDRESS = "localhost";
+      MONGO_HOST = "localhost";
+      MONGO_CONNECTION_STRING = "mongodb://localhost:27017/overleaf";
+      NOTIFICATIONS_HOST = "localhost";
+      REALTIME_HOST = "localhost";
+      OVERLEAF_REDIS_PATH = "/run/redis-overleaf/redis.sock";
+      SPELLING_HOST = "localhost";
+      TRACK_CHANGES_HOST = "localhost";
+      WEBPACK_HOST = "localhost";
+      WEB_API_PASSWORD = "overleaf";
+      WEB_API_USER = "overleaf";
+      WEB_API_PORT = "3032";
+      WEB_API_HOST = "localhost";
+      WEB_HOST = "localhost";
+      WEB_PORT = "3032";
+      DATA_DIR = "/var/lib/overleaf";
+      NODE_ENV = "production";
+      OVERLEAF_CONFIG = "${pkgs.overleaf}/share/server-ce/config/settings.js";
+      OVERLEAF_FPH_DISPLAY_NEW_PROJECTS = "true";
+      OVERLEAF_MONGO_URL = "mongodb://localhost:27017/overleaf";
+      OVERLEAF_SESSION_SECRET = "hello";
+      OVERLEAF_HISTORY_BACKEND = "fs";
+      OVERLEAF_HISTORY_BLOBS_BUCKET = "/var/lib/overleaf/data/history/overleaf-blobs";
+      OVERLEAF_HISTORY_PROJECT_BLOBS_BUCKET = "/var/lib/overleaf/data/history/overleaf-project-blobs";
+      OVERLEAF_HISTORY_CHUNKS_BUCKET = "/var/lib/overleaf/data/history/overleaf-chunks";
+      OVERLEAF_HISTORY_ZIPS_BUCKET = "/var/lib/overleaf/data/history/overleaf-zips";
+      OVERLEAF_HISTORY_ANALYTICS_BUCKET = "/var/lib/overleaf/data/history/overleaf-analytics";
+      ADMIN_PRIVILEGE_AVAILABLE = "true";
+      OVERLEAF_SITE_URL = "https://verso.saumon.network";
+      OVERLEAF_ADMIN_EMAIL = "postmaster@mondon.me";
+      #OVERLEAF_SITE_LANGUAGE = "fr";
+      OVERLEAF_APP_NAME = "Verso, powered by SaumonNetwork";
+      #OVERLEAF_EMAIL_FROM_ADDRESS = "";
+      #OVERLEAF_EMAIL_SMTP_HOST = "";
+      #OVERLEAF_EMAIL_SMTP_PORT = "587";
+      #OVERLEAF_EMAIL_SMTP_SECURE = "true";
+      #OVERLEAF_EMAIL_SMTP_USER = "";
+      #OVERLEAF_EMAIL_SMTP_PASS = "";
+    };
+
     systemd.services =
       let activateServices = service: {
         "overleaf-${service}" = {
           description = "Overleaf ${service}";
           wantedBy = [ "multi-user.target" ];
-          environment = env;
-          path = with pkgs; [ texlive.combined.scheme-full ];
+          environment = cfg.settings;
+          path = with pkgs; [ texlive.combined.scheme-${cfg.texliveScheme} ];
           serviceConfig = {
             Type = "simple";
             ExecStart = "${pkgs.overleaf}/bin/overleaf-${service}";
@@ -122,9 +143,9 @@ with lib;
           };
         };
       }
-      (mkIf (cfg.mongoDB == "ferretdb") {
-        postgresql.environment.LC_ALL = "en_US.UTF-8";
-      })]);
+        (mkIf (cfg.mongoDB == "ferretdb") {
+          postgresql.environment.LC_ALL = "en_US.UTF-8";
+        })]);
 
     users.users.overleaf = {
       isSystemUser = true;
@@ -134,8 +155,6 @@ with lib;
     };
 
     users.groups.overleaf = { };
-
-    #services.redis.servers."".enable = true;
 
     services.redis.servers.overleaf = mkIf cfg.enableRedis {
       enable = true;
