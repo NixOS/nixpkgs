d0db621dbb0d78fd1a8f156e9a14417d04dcd44b
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-19 00:17:40 +0100

    python310Packages.python-kasa: 0.5.0 -> 0.5.1
    Diff: https://github.com/python-kasa/python-kasa/compare/refs/tags/0.5.0...0.5.1

--- pkgs/development/python-modules/python-kasa/default.nix
+++ pkgs/development/python-modules/python-kasa/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "python-kasa";
-  version = "0.5.0";
+  version = "0.5.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-9zCUYB+TYgDMoTHpR0u42Usq2EKp8vtzlTgU82eXxl8=";
+    sha256 = "sha256-vp2r842f9A2lEFLhUcHyGZavAWT4Ke9mH+FAlGucdqo=";
   };
 
   nativeBuildInputs = [
