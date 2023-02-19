f7fb1b13e19800d770d6034329861cb0f96a5841
JB Gosselin <1536838+dennajort@users.noreply.github.com>
2023-02-19 11:49:45 -0500

    python310Packages.okta: init at 2.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6643,6 +6643,8 @@ self: super: with self; {
 
   oemthermostat = callPackage ../development/python-modules/oemthermostat { };
 
+  okta = callPackage ../development/python-modules/okta { };
+
   olefile = callPackage ../development/python-modules/olefile { };
 
   oletools = callPackage ../development/python-modules/oletools { };
