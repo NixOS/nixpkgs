eb62c13a1b34961a4485ebf8132dbf41a6fe2f18
Anderson Torres <torres.anderson.85@protonmail.com>
2023-02-19 07:12:58 -0300

    Revert "cimg: 3.2.0 -> 3.2.1"

--- pkgs/development/libraries/cimg/default.nix
+++ pkgs/development/libraries/cimg/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cimg";
-  version = "3.2.1";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "dtschump";
     repo = "CImg";
     rev = "v.${version}";
-    hash = "sha256-MPkZGKewusCw5TsW5NOtnrjqEK2dxRSCal1fn7Yiaio=";
+    hash = "sha256-laLi3ks5r0C61LDoCEgVqmqZ7/C18qQKxPm4zmQrw78=";
   };
 
   outputs = [ "out" "doc" ];
