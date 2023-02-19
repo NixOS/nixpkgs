2f33066fe3d952a5c05bb96045290b647ecd9266
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-19 04:23:30 +0000

    python310Packages.pysoundfile: 0.11.0 -> 0.12.1

--- pkgs/development/python-modules/soundfile/default.nix
+++ pkgs/development/python-modules/soundfile/default.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "soundfile";
-  version = "0.11.0";
+  version = "0.12.1";
   # https://github.com/bastibe/python-soundfile/issues/157
   disabled = isPyPy || stdenv.isi686;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-kxc4ock+hoTC0+HVFKxjRAzoJ+x4PqCi0+RzDj3FjBg=";
+    sha256 = "sha256-6OEBeyzx3adnrvGdL9nuXr4H4FDUMPd6Cnxmugi4za4=";
   };
 
   postPatch = ''
