358b60c804af0fc3cfa497126d4d78215092588f
Enno Richter <enno@nerdworks.de>
2023-02-20 07:52:48 +0100

    python3Packages.mlxtend: fix test configuration

--- pkgs/development/python-modules/mlxtend/default.nix
+++ pkgs/development/python-modules/mlxtend/default.nix
@@ -1,20 +1,20 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, isPy27
 , pytestCheckHook
-, scipy
-, numpy
-, scikit-learn
-, pandas
-, matplotlib
 , joblib
+, keras
+, matplotlib
+, numpy
+, pandas
+, scikit-learn
+, scipy
+, tensorflow
 }:
 
 buildPythonPackage rec {
   pname = "mlxtend";
   version = "0.21.0";
-  disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "rasbt";
@@ -24,28 +24,47 @@ buildPythonPackage rec {
   };
 
   nativeCheckInputs = [ pytestCheckHook ];
-  # image tests download files over the network
-  pytestFlagsArray = [ "-sv" "--ignore=mlxtend/image" ];
-  # Fixed in master, but failing in release version
-  # see: https://github.com/rasbt/mlxtend/pull/721
-  disabledTests = [ "test_variance_explained_ratio" ];
+  pytestFlagsArray = [ "-sv" ];
+  disabledTestPaths = [
+    # tests download files over the network
+    "mlxtend/image"
+    "mlxtend/evaluate/tests/test_feature_importance.py"
+    # tests that import deprecated sklearn datasets
+    "mlxtend/evaluate/f_test.py"
+    "mlxtend/feature_selection/tests/test_column_selector.py"
+    "mlxtend/feature_selection/tests/test_exhaustive_feature_selector.py"
+    "mlxtend/feature_selection/tests/test_sequential_feature_selector.py"
+    "mlxtend/feature_selection/tests/test_sequential_feature_selector_feature_groups.py"
+  ];
+  disabledTests = [
+    # Fixed in master, but failing in release version
+    # see: https://github.com/rasbt/mlxtend/pull/721
+    "test_variance_explained_ratio"
+
+    # Unmaintained - TypeError: LinearRegression.__init__() got an unexpected keyword argument 'normalize'
+    "test_sparsedataframe_notzero_column"
+    "test_multivariate_class"
+  ];
 
   propagatedBuildInputs = [
-    scipy
-    numpy
-    scikit-learn
-    pandas
-    matplotlib
     joblib
+    matplotlib
+    numpy
+    pandas
+    scikit-learn
+    scipy
+  ];
+
+  checkInputs = [
+    keras
+    tensorflow
   ];
 
   meta = with lib; {
     description = "A library of Python tools and extensions for data science";
     homepage = "https://github.com/rasbt/mlxtend";
-    license= licenses.bsd3;
+    license = licenses.bsd3;
     maintainers = with maintainers; [ evax ];
     platforms = platforms.unix;
-    # incompatible with nixpkgs scikit-learn version
-    broken = true;
   };
 }
