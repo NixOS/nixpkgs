9713bb59c080d403537aa7dae52531b2868a08f1
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-20 12:23:14 +0100

    python310Packages.tempest: disable on unsupported Python releases

--- pkgs/development/python-modules/tempest/default.nix
+++ pkgs/development/python-modules/tempest/default.nix
@@ -15,6 +15,7 @@
 , oslo-serialization
 , oslo-utils
 , fixtures
+, pythonOlder
 , pyyaml
 , subunit
 , stevedore
@@ -30,6 +31,9 @@
 buildPythonPackage rec {
   pname = "tempest";
   version = "33.0.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
