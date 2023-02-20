8f1dd2d928faddd2ba66d94ee3773c6184ef46b2
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-20 23:50:56 +0100

    python310Packages.pyinsteon: 1.3.0 -> 1.3.2
    Diff: https://github.com/pyinsteon/pyinsteon/compare/refs/tags/1.3.0...1.3.2
    
    Changelog: https://github.com/pyinsteon/pyinsteon/releases/tag/1.3.2

--- pkgs/development/python-modules/pyinsteon/default.nix
+++ pkgs/development/python-modules/pyinsteon/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pyinsteon";
-  version = "1.3.0";
+  version = "1.3.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-yxWogOcZV0ZvV0a61YF47nqei+LGwD/Xz4QqNeJQtFA=";
+    hash = "sha256-sLJ3pmaF/Fa0rAxA5YaNO9MHWEyRdkc/9WNkly4IPwQ=";
   };
 
   nativeBuildInputs = [
