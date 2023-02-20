23dc76fd2248d51625f3375d506416415dfba7d6
sternenseemann <sternenseemann@systemli.org>
2023-02-20 12:24:52 +0100

    haskell.compiler.ghcHEAD: fix hadrianFlagsArray handling

--- pkgs/development/compilers/ghc/common-hadrian.nix
+++ pkgs/development/compilers/ghc/common-hadrian.nix
@@ -426,10 +426,10 @@ stdenv.mkDerivation ({
     runHook preBuild
 
     # hadrianFlagsArray is created in preConfigure
-    echo "hadrianFlags: $hadrianFlags ''${hadrianFlagsArray}"
+    echo "hadrianFlags: $hadrianFlags ''${hadrianFlagsArray[@]}"
 
     # We need to go via the bindist for installing
-    hadrian $hadrianFlags "''${hadrianFlagsArray}" binary-dist-dir
+    hadrian $hadrianFlags "''${hadrianFlagsArray[@]}" binary-dist-dir
 
     runHook postBuild
   '';
