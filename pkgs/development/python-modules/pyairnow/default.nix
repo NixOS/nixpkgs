aa1c246ecff4e483f7e3542d1d44103313d7b4f9
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-20 23:02:48 +0100

    python310Packages.pyairnow: 1.1.0 -> 1.2.0
    Diff: https://github.com/asymworks/pyairnow/compare/refs/tags/v1.1.0...v1.2.0
    
    Changelog: https://github.com/asymworks/pyairnow/blob/v1.2.0/CHANGELOG.md

--- pkgs/development/python-modules/pyairnow/default.nix
+++ pkgs/development/python-modules/pyairnow/default.nix
@@ -7,43 +7,42 @@
 , pytest-aiohttp
 , poetry-core
 , pytest-asyncio
-, pytest-cov
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pyairnow";
-  version = "1.1.0";
+  version = "1.2.0";
   format = "pyproject";
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "asymworks";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "1hkpfl8rdwyzqrr1drqlmcw3xpv3pi1jf19h1divspbzwarqxs1c";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-5i/O+u6BTqx0TajprAhnAa1ao3EzbBeBeKsczW05WRk=";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "switch-to-poetry-core.patch";
-      url = "https://github.com/asymworks/pyairnow/commit/f7a01733a41c648563fc2fe4b559f61ef08b9153.patch";
-      hash = "sha256-lcHnFP3bwkPTi9Zq1dZtShLKyXcxO0XoDF+PgjbWOqs=";
-    })
+  nativeBuildInputs = [
+    poetry-core
   ];
 
-  nativeBuildInputs = [ poetry-core ];
-
-  propagatedBuildInputs = [ aiohttp ];
+  propagatedBuildInputs = [
+    aiohttp
+  ];
 
   nativeCheckInputs = [
     aioresponses
     pytest-asyncio
     pytest-aiohttp
-    pytest-cov
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "pyairnow" ];
+  pythonImportsCheck = [
+    "pyairnow"
+  ];
 
   meta = with lib; {
     description = "Python wrapper for EPA AirNow Air Quality API";
