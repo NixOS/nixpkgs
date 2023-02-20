1fe9b33a8fefca1b222281c85393ea05d8eb48e8
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-20 06:51:22 +0000

    python310Packages.flake8-bugbear: 23.1.20 -> 23.2.13

--- pkgs/development/python-modules/flake8-bugbear/default.nix
+++ pkgs/development/python-modules/flake8-bugbear/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "flake8-bugbear";
-  version = "23.1.20";
+  version = "23.2.13";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "PyCQA";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-JO6S9LjCEZKeUKxUpkqEw+RJ47HLbwQOOOKhHwnmWVI=";
+    hash = "sha256-4iOt6KQU8QkNdmGBIyevfwqvbKpylV+BN49GjeX68xE=";
   };
 
   propagatedBuildInputs = [
