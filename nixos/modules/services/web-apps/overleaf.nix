8daa53c3adf81437a7ae3af6a63ff9a829cc250b
Camille M. <camillemondon@free.fr>
2023-02-21 06:26:07 +0100

    added engines

--- nixos/modules/services/web-apps/overleaf.nix
+++ nixos/modules/services/web-apps/overleaf.nix
@@ -3,9 +3,106 @@
 with lib;
 let
   cfg = config.services.overleaf;
+
   ferret = pkgs.ferretdb.overrideAttrs (final: prev: {
     patches = [ ./fields.patch ];
   });
+
+  sagetex-fixed = pkgs.sagetex.overrideAttrs (final: prev: {
+    buildInputs = with pkgs; [
+      sage
+      texlive.combined.scheme-full
+    ];
+
+    buildPhase = ''
+      make all
+    '';
+    installPhase = prev.installPhase + ''
+      mkdir $out/bin
+      cp -va *.py "$out/bin"
+    '';
+
+    HOME = "/tmp/test";
+  });
+
+  overleafServices = [
+    "chat"
+    "clsi"
+    "contacts"
+    "docstore"
+    "document-updater"
+    "filestore"
+    "notifications"
+    "project-history"
+    "real-time"
+    "track-changes"
+    "web"
+  ] ++ (optional (cfg.dicts != [ ]) "spelling");
+
+  latexmkrc = pkgs.writeText "latexmkrc" (optionalString (elem "pythontex" cfg.engines) ''
+    #############
+    # PythonTeX #
+    #############
+    $pytexmod = 'python3 ${head pkgs.texlive.pythontex.pkgs}/scripts/pythontex/pythontex3.py';
+
+    $go_mode = 1;
+
+    add_cus_dep('pytxcode', 'tex', 0, 'pythontex');
+    add_cus_dep('pytxcode', 'Rtex', 0, 'pythontex');
+    sub pythontex {
+      return system("$pytexmod --error-exit-code false output &> ./pythontex.log") ;
+    }
+
+    $pdflatex = 'internal mylatex %S pdflatex -synctex=1 -interaction=batchmode %O %S';
+    $latex = 'internal mylatex %S latex -synctex=1 -interaction=batchmode %O %S';
+    $lualatex = 'internal mylatex %S lualatex -synctex=1 -interaction=batchmode %O %S';
+    $xelatex = 'internal mylatex %S latex -synctex=1 -interaction=batchmode %O %S';
+
+    use File::Basename;
+
+    sub mylatex {
+      (my $source, my $dir, my $ext) = fileparse(shift, qr/.tex/);
+      if (! $source == 'output') {
+        if (-l "$source.pytxcode"){
+          unlink "$source.pytxcode";
+        };
+        if (-l "pythontex-files-$source"){
+          unlink "pythontex-files-$source";
+        };
+        symlink 'output.pytxcode', "$source.pytxcode";
+        symlink 'pythontex-files-output', "pythontex-files-$source";
+      };
+      return system @_;
+    }
+
+  '' + optionalString (elem "knitr" cfg.engines) ''
+    #########
+    # Knitr #
+    #########
+    add_cus_dep( 'Rtex', 'tex', 0, 'do_knitr');
+    sub do_knitr {
+      Run_subst(qq{Rscript -e 'library("knitr");
+        opts_knit\$set(concordance=T);
+        knitr::knit(%S, output=%D);' &> ./knitr.log});
+    }
+
+  '' + optionalString (elem "sagetex" cfg.engines) ''
+    ###########
+    # SageTeX #
+    ###########
+
+    $sagetexrun = 'sage ${sagetex-fixed}/tex/latex/sagetex/sagetex-run.py';
+
+    $latex = "$latex ; $sagetexrun %B";
+    $pdflatex = "$pdflatex ; $sagetexrun %B";
+
+    add_cus_dep('sage', 'sout', 0, 'makesout');
+    $hash_calc_ignore_pattern{'sage'} = '^( st.goboom|print .SageT)';
+    sub makesout {
+      system( "sage \"$_[0].sage\" &> sagetex.log" );
+    }
+
+  '' + cfg.latexmkrc);
 in
 
 {
@@ -93,9 +190,56 @@ in
     path = mkOption {
       type = with types; listOf package;
       default = [ ];
-      example = literalExpression "with pkgs; [ R (python3.pkgs.withPackages (py: py.pygments)) ]";
+      example = literalExpression "with pkgs; [ qpdf ]";
       description = lib.mdDoc ''
         Additional packages to place in the path of the Overleaf services.
+      '';
+    };
+
+    engines = mkOption {
+      type = with types; listOf str;
+      default = [ ];
+      example = literalExpression ''[ "sagetex" ]'';
+      description = lib.mdDoc ''
+        Additional engines to execute code when compiling LateX documents. Choose among
+        "sagetex", "pythontex" and "knitr".
+      '';
+    };
+
+    latexmkrc = mkOption {
+      type = types.lines;
+      default = "";
+      description = lib.mdDoc ''
+        Extra contents appended to the LaTeX configuration file,
+        .latexmkrc.
+      '';
+    };
+
+    dicts = mkOption {
+      type = with types; listOf package;
+      default = [ ];
+      example = literalExpression ''with pkgs.aspellDicts; [ en fr ]'';
+      description = lib.mdDoc ''
+        Additional languages to add to overleaf spell check engine,
+        based on aspell.
+      '';
+    };
+
+    extraPythonPackages = mkOption {
+      type = with types; listOf package;
+      default = [ ];
+      example = literalExpression ''with pkgs.python3Packages; [ matplotlib ]'';
+      description = lib.mdDoc ''
+        Additional packages to add to the ```pythontex``` engine.
+      '';
+    };
+
+    extraRPackages = mkOption {
+      type = with types; listOf package;
+      default = [ ];
+      example = literalExpression ''with pkgs.rPackages; [ ggplot2 ]'';
+      description = lib.mdDoc ''
+        Additional R packages to use with the ```knitr``` engine.
       '';
     };
   };
@@ -106,7 +250,6 @@ in
         NODE_ENV = "production";
         OVERLEAF_CONFIG = "${pkgs.overleaf}/share/server-ce/config/settings.js";
         DATA_DIR = mkDefault "/var/lib/overleaf";
-        # OVERLEAF_MONGO_URL = mkDefault "mongodb://overleaf@%2Ftmp%2Fmongodb-27017.sock/overleaf";
         OVERLEAF_MONGO_URL = mkDefault "mongodb://localhost:27017/overleaf";
         OVERLEAF_REDIS_PATH = mkDefault "/run/redis-overleaf/redis.sock";
         WEB_PORT = mkDefault "3032";
@@ -115,7 +258,7 @@ in
         OVERLEAF_FPH_DISPLAY_NEW_PROJECTS = mkDefault "true";
       };
 
-      systemd.targets.overleaf = { };
+      systemd.targets.overleaf.requires = map (service: "overleaf-${service}.service") overleafServices;
 
       systemd.services =
         let activateServices = service: {
@@ -123,38 +266,39 @@ in
             description = "Overleaf ${service}";
             wantedBy = [ "overleaf.target" "multi-user.target" ];
             environment = cfg.settings;
-            path = with pkgs; [ cfg.texlivePackage (aspellWithDicts (ps: [ ps.en ps.fr ])) ] ++ cfg.path;
+            path = with pkgs; [ cfg.texlivePackage qpdf (aspellWithDicts (ps: with ps; cfg.dicts)) ] ++ cfg.path;
             serviceConfig = {
               Type = "simple";
               ExecStart = pkgs.writeShellScript "overleaf-${service}" ''
+                # This sources the secrets as environment variables, less secure but avoids a patch:
                 for secret in $(ls $CREDENTIALS_DIRECTORY)
                 do
                   export $secret=$(cat $CREDENTIALS_DIRECTORY/$secret)
                 done
+
+                # This softlinks the LaTeX configuration files to the home of Overleaf:
+                ${optionalString (cfg.engines != []) "ln -sf ${latexmkrc} /var/lib/overleaf/.latexmkrc"}
+
                 exec ${pkgs.overleaf}/bin/overleaf-${service}
               '';
               StateDirectory = "overleaf";
               WorkingDirectory = "/var/lib/overleaf";
               User = "overleaf";
               LoadCredential = mapAttrsToList (name: path: "${name}:${path}") cfg.secrets;
+              ProtectHome = true;
+              ProtectSystem = true;
+              PrivateDevices = true;
+              ProtectHostname = true;
+              ProtectClock = true;
+              ProtectKernelTunables = true;
+              ProtectKernelModules = true;
+              ProtectKernelLogs = true;
+              ProtectControlGroups = true;
             };
           };
         };
         in
-        mkMerge (map activateServices [
-          "chat"
-          "clsi"
-          "contacts"
-          "docstore"
-          "document-updater"
-          "filestore"
-          "notifications"
-          "project-history"
-          "real-time"
-          "spelling"
-          "track-changes"
-          "web"
-        ]);
+        mkMerge (map activateServices overleafServices);
 
       users.users.overleaf = {
         isSystemUser = true;
@@ -191,12 +335,15 @@ in
         user = "overleaf";
         port = 0;
       };
+    }
 
-      services.mongodb = mkIf (cfg.mongodbType == "mongodb") {
+    (mkIf (cfg.mongodbType == "mongodb") {
+      services.mongodb = {
         enable = true;
         package = pkgs.mongodb-4_2;
       };
-    }
+    })
+
     (mkIf (cfg.mongodbType == "ferretdb") {
       systemd.services.ferretdb = {
         description = "FerretDB";
@@ -229,5 +376,24 @@ in
         }];
       };
     })
+
+    (mkIf (elem "sagetex" cfg.engines) {
+      services.overleaf.texlivePackage = with pkgs; mkOverride 98 (texlive.combine {
+        inherit (texlive) scheme-full;
+        sagetex = { pkgs = [ sagetex-fixed ]; };
+      });
+      services.overleaf.path = [ pkgs.sage sagetex-fixed ];
+      services.overleaf.settings.PYTHONPATH = with pkgs.python3.pkgs; makePythonPath ([ pygments ] ++ cfg.extraPythonPackages) + ":${sagetex-fixed}/bin";
+    })
+
+    (mkIf (elem "knitr" cfg.engines) {
+      services.overleaf.path = with pkgs; [ (rWrapper.override { packages = with rPackages; [ knitr patchSynctex ] ++ cfg.extraRPackages; }) ];
+    })
+
+    (mkIf (elem "pythontex" cfg.engines) {
+      services.overleaf.texlivePackage = mkOverride 99 pkgs.texlive.combined.scheme-full;
+      services.overleaf.path = [ (pkgs.python3.withPackages (p: with p; [ pygments ] ++ cfg.extraPythonPackages)) ];
+    })
   ]);
 }
+
