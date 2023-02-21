7f6d65ce3b5dced94a18d92ad74449a7b690fbb4
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-21 23:19:38 +0100

    python310Packages.ailment: 9.2.38 -> 9.2.39

--- pkgs/development/python-modules/ailment/default.nix
+++ pkgs/development/python-modules/ailment/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "ailment";
-  version = "9.2.38";
+  version = "9.2.39";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-nhvFBXOoxCWaSjUdPyQP234URI50DM3ZoweRK9d1LIA=";
+    hash = "sha256-JO46r+eNBmMzCaXvpcQU3pnn833ABeMYnFlxh7bTHB8=";
   };
 
   nativeBuildInputs = [
