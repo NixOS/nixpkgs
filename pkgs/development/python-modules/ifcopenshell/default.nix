44231f7de9b1bd2a5b6663f7e46e9f7154624195
GenericNerdyUsername <genericnerdyusername@proton.me>
2023-02-21 12:38:42 +0000

    ifcopenshell: 210410 -> 0.7.0a10

--- pkgs/development/python-modules/ifcopenshell/default.nix
+++ pkgs/development/python-modules/ifcopenshell/default.nix
@@ -1,12 +1,15 @@
-{ lib, stdenv
+{ lib
+, stdenv
 , buildPythonPackage
 , fetchFromGitHub
-, gcc10
 , cmake
 , boost179
 , icu
 , swig
 , pcre
+, gmp
+, mpfr
+, cgal_5
 , opencascade-occt
 , opencollada
 , libxml2
@@ -14,18 +17,18 @@
 
 buildPythonPackage rec {
   pname = "ifcopenshell";
-  version = "210410";
+  version = "0.7.0a10";
   format = "other";
 
   src = fetchFromGitHub {
-    owner  = "IfcOpenShell";
-    repo   = "IfcOpenShell";
-    rev    = "blenderbim-${version}";
+    owner = "IfcOpenShell";
+    repo = "IfcOpenShell";
+    rev = "v${version}";
     fetchSubmodules = true;
-    sha256 = "1g52asxrqcfj01iqvf03k3bb6rg3v04hh1wc3nmn329a2lwjbxpw";
+    hash = "sha256-wZhdU+yuWNEB2B574q2XAKjQdpvPHR+68hQRkNzhRvc=";
   };
 
-  nativeBuildInputs = [ gcc10 cmake ];
+  nativeBuildInputs = [ cmake ];
 
   buildInputs = [
     boost179
@@ -38,23 +41,34 @@ buildPythonPackage rec {
     cd cmake
   '';
 
-  PYTHONUSERBASE=".";
+  PYTHONUSERBASE = ".";
   cmakeFlags = [
     "-DUSERSPACE_PYTHON_PREFIX=ON"
+    "-DBUILD_EXAMPLES=OFF"
+    "-DENABLE_BUILD_OPTIMIZATIONS=ON"
     "-DOCC_INCLUDE_DIR=${opencascade-occt}/include/opencascade"
     "-DOCC_LIBRARY_DIR=${opencascade-occt}/lib"
+    "-DCOLLADA_SUPPORT=ON"
     "-DOPENCOLLADA_INCLUDE_DIR=${opencollada}/include/opencollada"
     "-DOPENCOLLADA_LIBRARY_DIR=${opencollada}/lib/opencollada"
+    "-DPCRE_LIBRARY_DIR=${pcre.out}/lib"
+    "-DHDF5_SUPPORT=OFF"
+    "-DBoost_INCLUDE_DIR=${boost179.dev}/include"
+    "-DBoost_LIBRARY_DIR=${boost179}/lib"
+    "-DCGAL_INCLUDE_DIR=${cgal_5}/include"
+    "-DGMP_LIBRARY_DIR=${gmp}/lib"
+    "-DGMP_INCLUDE_DIR=${gmp.dev}/include"
+    "-DMPFR_LIBRARY_DIR=${mpfr}/lib"
+    "-DMPFR_INCLUDE_DIR=${mpfr.dev}/include"
     "-DSWIG_EXECUTABLE=${swig}/bin/swig"
     "-DLIBXML2_INCLUDE_DIR=${libxml2.dev}/include/libxml2"
     "-DLIBXML2_LIBRARIES=${libxml2.out}/lib/libxml2${stdenv.hostPlatform.extensions.sharedLibrary}"
   ];
 
   meta = with lib; {
-    broken = stdenv.isDarwin;
     description = "Open source IFC library and geometry engine";
-    homepage    = "http://ifcopenshell.org/";
-    license     = licenses.lgpl3;
+    homepage = "http://ifcopenshell.org/";
+    license = licenses.lgpl3;
     maintainers = with maintainers; [ fehnomenal ];
   };
 }
