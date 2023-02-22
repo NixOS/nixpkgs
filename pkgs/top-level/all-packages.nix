9bdc58003708d7af43a52fbe1763c068966d3b55
Charles Hall <charles@computer.surgery>
2023-02-22 15:02:53 -0800

    pulldown-cmark: init at 0.9.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11200,6 +11200,8 @@ with pkgs;
 
   pubs = callPackage ../tools/misc/pubs {};
 
+  pulldown-cmark = callPackage ../tools/typesetting/pulldown-cmark { };
+
   pulumictl = callPackage ../development/tools/pulumictl { };
 
   pure-prompt = callPackage ../shells/zsh/pure-prompt { };
