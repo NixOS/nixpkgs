dabddbfcc7c4a557ded815d5b12cab8e92579e68
Robert Schütz <nix@dotlambda.de>
2023-02-21 22:40:27 -0800

    python310Packages.bimmer-connected: 0.12.1 -> 0.12.3
    Diff: https://github.com/bimmerconnected/bimmer_connected/compare/refs/tags/0.12.1...0.12.3

--- pkgs/development/python-modules/bimmer-connected/default.nix
+++ pkgs/development/python-modules/bimmer-connected/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "bimmer-connected";
-  version = "0.12.1";
+  version = "0.12.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "bimmerconnected";
     repo = "bimmer_connected";
     rev = "refs/tags/${version}";
-    hash = "sha256-wLQ2UkedLSwfbUqmb85QgsDYh0zcbgQOMnhbRHW5Bnw=";
+    hash = "sha256-LkA92pc5lXScB/+O+tdh+Vi/m9IdhNehVeGgg52lNm4=";
   };
 
   nativeBuildInputs = [
