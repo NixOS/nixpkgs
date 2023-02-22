4f84f0c87f9bd32fc476063ed0c2c00d695258f0
sternenseemann <sternenseemann@systemli.org>
2023-02-22 16:23:20 +0100

    mandoc: change OSNAME to Nixpkgs to match groff
    See also
    
    * https://github.com/NixOS/nixpkgs/pull/214307
    * https://github.com/NixOS/nixpkgs/pull/213256

--- pkgs/tools/misc/mandoc/default.nix
+++ pkgs/tools/misc/mandoc/default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
   configureLocal = ''
     MANPATH_DEFAULT="/run/current-system/sw/share/man"
     MANPATH_BASE="$MANPATH_DEFAULT"
-    OSNAME="NixOS"
+    OSNAME="Nixpkgs"
     PREFIX="$out"
     LD_OHASH="-lutil"
     # Use symlinks instead of hardlinks (more commonly used in nixpkgs)
