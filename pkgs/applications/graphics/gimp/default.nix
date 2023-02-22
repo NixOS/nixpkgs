47c05763ccb0ecb7cd5c0438aa4d6b140b66f652
Jan Tojnar <jtojnar@gmail.com>
2023-02-22 21:19:29 +0100

    gimp: 2.10.32 → 2.10.34
    https://www.gimp.org/news/2023/02/27/gimp-2-10-34-released/
    https://gitlab.gnome.org/GNOME/gimp/-/compare/GIMP_2_10_32...GIMP_2_10_34

--- pkgs/applications/graphics/gimp/default.nix
+++ pkgs/applications/graphics/gimp/default.nix
@@ -55,13 +55,13 @@ let
   python = python2.withPackages (pp: [ pp.pygtk ]);
 in stdenv.mkDerivation rec {
   pname = "gimp";
-  version = "2.10.32";
+  version = "2.10.34";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "http://download.gimp.org/pub/gimp/v${lib.versions.majorMinor version}/${pname}-${version}.tar.bz2";
-    sha256 = "PxXHBVSvXcwbRubcaPPY8KbMn+VrbXisCMD9hZq4miU=";
+    sha256 = "hABGQtNRs5ikKTzX/TWSBEqUTwW7UoUO5gaPJHxleqM=";
   };
 
   patches = [
