b6ac0f0b4511213c9a04dfd1708f1d69ac6e1da4
Nikolay Korotkiy <sikmir@disroot.org>
2023-02-23 19:00:27 +0300

    martin: 0.7.0 → 0.7.2

--- pkgs/servers/geospatial/martin/default.nix
+++ pkgs/servers/geospatial/martin/default.nix
@@ -2,22 +2,50 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "martin";
-  version = "0.7.0";
+  version = "0.7.2";
 
   src = fetchFromGitHub {
     owner = "maplibre";
     repo = "martin";
     rev = "v${version}";
-    hash = "sha256-UIAsij4fFxGAoKluQFAtrfgKIteM+LQtAKRDvRaNLSg=";
+    hash = "sha256-F7CAP7PrG71EdAe2hb13L/fKSiFyNHYHHweqg2GiJeU=";
   };
 
-  cargoHash = "sha256-NtPI8MZNUn+QYPuG9WNMVZJW6jmyi5gEfNw8MrkiwUQ=";
+  cargoHash = "sha256-/bIMQJ2+39PShVx/W/tOeD+EjPNLw4NianwJl9wkwmk=";
 
   nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [ openssl ] ++ lib.optional stdenv.isDarwin Security;
 
-  doCheck = false;
+  checkFlags = [
+    "--skip function_source_schemas"
+    "--skip function_source_tile"
+    "--skip function_source_tilejson"
+    "--skip pg_get_function_tiles"
+    "--skip pg_get_function_source_ok_rewrite"
+    "--skip pg_get_function_source_ok"
+    "--skip pg_get_composite_source_tile_minmax_zoom_ok"
+    "--skip pg_get_function_source_query_params_ok"
+    "--skip pg_get_composite_source_tile_ok"
+    "--skip pg_get_catalog"
+    "--skip pg_get_composite_source_ok"
+    "--skip pg_get_health_returns_ok"
+    "--skip pg_get_table_source_ok"
+    "--skip pg_get_table_source_rewrite"
+    "--skip pg_null_functions"
+    "--skip utils::test_utils::tests::test_bad_os_str"
+    "--skip utils::test_utils::tests::test_get_env_str"
+    "--skip pg_get_table_source_multiple_geom_tile_ok"
+    "--skip pg_get_table_source_tile_minmax_zoom_ok"
+    "--skip pg_tables_feature_id"
+    "--skip pg_get_table_source_tile_ok"
+    "--skip table_source_schemas"
+    "--skip tables_srid_ok"
+    "--skip tables_tile_ok"
+    "--skip table_source"
+    "--skip tables_tilejson"
+    "--skip tables_multiple_geom_ok"
+  ];
 
   meta = with lib; {
     description = "Blazing fast and lightweight PostGIS vector tiles server";
