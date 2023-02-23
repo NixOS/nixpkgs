3f4b2c4334ef19bc1c39aac3e7e31538b0eadbfc
fleaz <mail@felixbreidenstein.de>
2023-02-23 14:58:55 +0100

    python310Packages.pytapo: 2.9.2 -> 3.1.6

--- pkgs/development/python-modules/pytapo/default.nix
+++ pkgs/development/python-modules/pytapo/default.nix
@@ -5,22 +5,24 @@
 # propagates
 , pycryptodome
 , requests
+, rtp
 , urllib3
 }:
 
 buildPythonPackage rec {
   pname = "pytapo";
-  version = "2.9.2";
+  version = "3.1.6";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-LW14uDQBqIVsigOzO0bNTpjY7Fk0IWAeDMPEuWM/nOo=";
+    hash = "sha256-SV9jj4N+6ggvYTz96iAiI4T7GikgGpDzFo4gIOYETU0=";
   };
 
   propagatedBuildInputs = [
     pycryptodome
     requests
+    rtp
     urllib3
   ];
 
@@ -34,7 +36,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library for communication with Tapo Cameras ";
     homepage = "https://github.com/JurajNyiri/pytapo";
-    license = with licenses; [ mit ];
+    license = licenses.mit;
     maintainers = with maintainers; [ fleaz ];
   };
 }
