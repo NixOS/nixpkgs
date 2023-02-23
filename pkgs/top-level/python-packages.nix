dab946e350e7c14326cb1bff588ccff97d862b70
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-24 00:50:29 +0100

    python310Packages.python-otbr-api: init at 1.0.5
    Co-Authored-By: Martin Weinelt <hexa@darmstadt.ccc.de>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7393,6 +7393,8 @@ self: super: with self; {
 
   python-memcached = callPackage ../development/python-modules/python-memcached { };
 
+  python-otbr-api = callPackage ../development/python-modules/python-otbr-api { };
+
   python-openems = callPackage ../development/python-modules/python-openems { };
 
   python-openzwave-mqtt = callPackage ../development/python-modules/python-openzwave-mqtt { };
