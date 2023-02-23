205fef443ec061e4d896f7af0fc082276edfa260
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-23 09:03:18 +0100

    python310Packages.omnikinverter: add changelog to meta

--- pkgs/development/python-modules/omnikinverter/default.nix
+++ pkgs/development/python-modules/omnikinverter/default.nix
@@ -55,6 +55,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python module for the Omnik Inverter";
     homepage = "https://github.com/klaasnicolaas/python-omnikinverter";
+    changelog = "https://github.com/klaasnicolaas/python-omnikinverter/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
