0ea90eb0b6cb1f83b0046131fae3a92b89c9e9b1
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-24 22:40:27 +0100

    python310Packages.commoncode: update disabled

--- pkgs/development/python-modules/commoncode/default.nix
+++ pkgs/development/python-modules/commoncode/default.nix
@@ -22,7 +22,7 @@ buildPythonPackage rec {
   version = "31.0.2";
   format = "pyproject";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
