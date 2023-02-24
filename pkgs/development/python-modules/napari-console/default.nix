81a543c59af2743fd02e66b727606fc1e87d0070
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-24 16:18:47 +0100

    python310Packages.napari-console: equalize

--- pkgs/development/python-modules/napari-console/default.nix
+++ pkgs/development/python-modules/napari-console/default.nix
@@ -1,28 +1,46 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, setuptools-scm
-, pytestCheckHook
-, pytest
-, ipython
-, ipykernel
-, qtconsole
-, napari-plugin-engine
 , imageio
-}: buildPythonPackage rec {
+, ipykernel
+, ipython
+, napari-plugin-engine
+, pytest
+, pytestCheckHook
+, qtconsole
+, setuptools-scm
+}:
+
+buildPythonPackage rec {
   pname = "napari-console";
   version = "0.0.4";
+
   src = fetchFromGitHub {
     owner = "napari";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-aVdYOzkZ+dqB680oDjNCg6quXU+QgUZI09E/MSTagyA=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-aVdYOzkZ+dqB680oDjNCg6quXU+QgUZI09E/MSTagyA=";
   };
-  nativeBuildInputs = [ setuptools-scm ];
-  # setup.py somehow requires pytest
-  propagatedBuildInputs = [ pytest ipython ipykernel napari-plugin-engine imageio qtconsole ];
-  chechInputs = [ pytestCheckHook ];
+
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
+
+  # setup.py somehow requires pytest
+  propagatedBuildInputs = [
+    imageio
+    ipykernel
+    ipython
+    napari-plugin-engine
+    pytest
+    qtconsole
+  ];
+
+  chechInputs = [
+    pytestCheckHook
+  ];
 
   meta = with lib; {
     description = "A plugin that adds a console to napari";
