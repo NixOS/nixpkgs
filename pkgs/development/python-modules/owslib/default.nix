f4f3680df62a3c0842dcd07ea3d93706658aecbc
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-02-24 16:50:40 +0200

    python310Packages.owslib: update meta
    Co-authored-by: Robert Schütz <github@dotlambda.de>

--- pkgs/development/python-modules/owslib/default.nix
+++ pkgs/development/python-modules/owslib/default.nix
@@ -61,7 +61,7 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
-    description = "client for Open Geospatial Consortium web service interface standards";
+    description = "Client for Open Geospatial Consortium web service interface standards";
     homepage = "https://www.osgeo.org/projects/owslib/";
     changelog = "https://github.com/geopython/OWSLib/blob/${version}/CHANGES.rst";
     license = licenses.bsd3;
