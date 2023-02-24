6152eed6a5277d1e6b4c68592b8a9796615db438
Artturi <Artturin@artturin.com>
2023-02-24 05:21:20 +0200

    Revert "python310Packages.kiwisolver: fix NIX_CFLAGS_COMPILE definition"

--- pkgs/development/python-modules/kiwisolver/default.nix
+++ pkgs/development/python-modules/kiwisolver/default.nix
@@ -20,7 +20,9 @@ buildPythonPackage rec {
     hash = "sha256-1BmXUZ/LpKHkbrSi/jG8EvD/lXsrgbrCjbJHRPMz6VU=";
   };
 
-  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin "-I${lib.getDev libcxx}/include/c++/v1";
+  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin [
+    "-I${lib.getDev libcxx}/include/c++/v1"
+  ];
 
   nativeBuildInputs = [
     setuptools-scm
