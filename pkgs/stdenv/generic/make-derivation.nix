c18190fd697d731b04407e449b0c00119f6e966f
Artturin <Artturin@artturin.com>
2023-02-24 06:44:45 +0200

    stdenv: run toString on lists in env to allow lists in env
    will make appending to lists in env in overrideAttrs more ergonomic due
    to no longer needing to add spaces manually
    
    ```
    nix-repl> keepassxc.NIX_CFLAGS_COMPILE
    "-Wno-old-style-cast -Wno-error -D__BIG_ENDIAN__=0"
    
    nix-repl> (keepassxc.overrideAttrs(oA: { passthru.oA = oA; })).oA.env.NIX_CFLAGS_COMPILE
    [ "-Wno-old-style-cast" "-Wno-error" "-D__BIG_ENDIAN__=0" ]
    
    nix-repl> (keepassxc.overrideAttrs(oA: { passthru.oA = oA; })).oA.NIX_CFLAGS_COMPILE
    error:
           … while evaluating the attribute 'oA.NIX_CFLAGS_COMPILE'
    
             at «string»:1:32:
    
                1| (keepassxc.overrideAttrs(oA: { passthru.oA = oA; })).oA.NIX_CFLAGS_COMPILE
                 |                                ^
    
           error: attribute 'NIX_CFLAGS_COMPILE' missing
    
           at «string»:1:1:
    
                1| (keepassxc.overrideAttrs(oA: { passthru.oA = oA; })).oA.NIX_CFLAGS_COMPILE
                 | ^
    ```

--- pkgs/stdenv/generic/make-derivation.nix
+++ pkgs/stdenv/generic/make-derivation.nix
@@ -537,8 +537,8 @@ else let
     assert lib.assertMsg (overlappingNames == [ ])
       "The ‘env’ attribute set cannot contain any attributes passed to derivation. The following attributes are overlapping: ${lib.concatStringsSep ", " overlappingNames}";
     lib.mapAttrs
-      (n: v: assert lib.assertMsg (lib.isString v || lib.isBool v || lib.isInt v || lib.isDerivation v)
-        "The ‘env’ attribute set can only contain derivation, string, boolean or integer attributes. The ‘${n}’ attribute is of type ${builtins.typeOf v}."; v)
+      (n: v: assert lib.assertMsg (lib.isString v || lib.isBool v || lib.isInt v || lib.isDerivation v || lib.isList v)
+        "The ‘env’ attribute set can only contain derivation, list, string, boolean or integer attributes. The ‘${n}’ attribute is of type ${builtins.typeOf v}."; (if lib.isList v then toString v else v))
       env;
 
 in
