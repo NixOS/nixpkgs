e07accff8b94ff4e8801dd0731593584dbdf9c8e
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-24 23:26:17 +0100

    python310Packages.yalexs-ble: 2.0.2 -> 2.0.3
    Changelog: https://github.com/bdraco/yalexs-ble/blob/v2.0.3/CHANGELOG.md

--- pkgs/development/python-modules/yalexs-ble/default.nix
+++ pkgs/development/python-modules/yalexs-ble/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "yalexs-ble";
-  version = "2.0.2";
+  version = "2.0.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "bdraco";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-Ev2m3YWiTWOBgK3dr7Gv8Od4tm5Rh4hmB5VJdL7MEOg=";
+    hash = "sha256-ka1CkDm8ahyj0VaxfRIHOt5rqrVWj2EZa2LXE2gqXlI=";
   };
 
   nativeBuildInputs = [
