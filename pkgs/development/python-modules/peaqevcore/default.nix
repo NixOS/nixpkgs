2611a7fdc00e77b01c0b159f25a33a6b8d480dec
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-24 23:23:24 +0100

    python310Packages.peaqevcore: 12.0.2 -> 12.1.6

--- pkgs/development/python-modules/peaqevcore/default.nix
+++ pkgs/development/python-modules/peaqevcore/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "peaqevcore";
-  version = "12.0.2";
+  version = "12.1.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-TfIzTbVXhS+DxroxBqfodS4HAQBPJQ4/Vo4Jh8VjYfM=";
+    hash = "sha256-6DuvA1HAMr4A9mNH9jFKH9IWOt4010iZewldCDWWmPE=";
   };
 
   postPatch = ''
