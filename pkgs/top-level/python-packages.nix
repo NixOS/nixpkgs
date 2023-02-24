4e908eeed3946992924d908666de8c4111536c03
Mario Rodas <marsam@users.noreply.github.com>
2023-02-24 04:20:00 +0000

    python310Packages.sphinxcontrib-jquery: rename from sphinx-jquery
    The package name used by the project itself is `sphinxcontrib.jquery` [1]
    
    [1] https://pypi.org/project/sphinxcontrib.jquery/

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10932,6 +10932,8 @@ self: super: with self; {
 
   sphinxcontrib_httpdomain = callPackage ../development/python-modules/sphinxcontrib_httpdomain { };
 
+  sphinxcontrib-jquery = callPackage ../development/python-modules/sphinxcontrib-jquery { };
+
   sphinxcontrib-jsmath = callPackage ../development/python-modules/sphinxcontrib-jsmath { };
 
   sphinxcontrib-katex = callPackage ../development/python-modules/sphinxcontrib-katex { };
@@ -10971,8 +10973,6 @@ self: super: with self; {
   sphinx = callPackage ../development/python-modules/sphinx { };
 
   sphinx-argparse = callPackage ../development/python-modules/sphinx-argparse { };
-
-  sphinx-jquery = callPackage ../development/python-modules/sphinx-jquery { };
 
   sphinx-autobuild = callPackage ../development/python-modules/sphinx-autobuild { };
 
