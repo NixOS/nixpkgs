620589829a8aecf1437d9c4005536dede5249cb0
Jan Tojnar <jtojnar@gmail.com>
2023-02-25 03:04:57 +0000

    xdg-desktop-portal-gnome: 43.1 → 44.beta
    https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome/-/compare/43.1...44.beta
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/xdg-desktop-portal-gnome/default.nix
+++ pkgs/development/libraries/xdg-desktop-portal-gnome/default.nix
@@ -20,11 +20,11 @@
 
 stdenv.mkDerivation rec {
   pname = "xdg-desktop-portal-gnome";
-  version = "43.1";
+  version = "44.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "Ca22bG2RU+bwXfZtqirWKl3g42Zl6dIpUXO7DdxTtM0=";
+    sha256 = "g6GQnwPOHm+rByv38TahQVmxScc5mwkfkB8ND0Z4VeI=";
   };
 
   nativeBuildInputs = [
