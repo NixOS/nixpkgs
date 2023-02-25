66d833402d02b09031a97ec4460702468f53e3b7
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-02-25 11:19:59 +0200

    widevine-cdm: 4.10.2449.0 -> 4.10.2557.0

--- pkgs/applications/networking/browsers/misc/widevine-cdm.nix
+++ pkgs/applications/networking/browsers/misc/widevine-cdm.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "widevine-cdm";
-  version = "4.10.2449.0";
+  version = "4.10.2557.0";
 
   src = fetchzip {
     url = "https://dl.google.com/widevine-cdm/${version}-linux-x64.zip";
-    sha256 = "sha256-f2kAkP+s3fB+krEZsiujEoI4oznkzSyaIB/CRJZWlXE=";
+    hash = "sha256-XxTjuPjWy06SmHC6GaIVIp3zD76isCVATWwwdZljntE=";
     stripRoot = false;
   };
 
