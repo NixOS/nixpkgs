bb82baf13c2e9297f88c6a24de59aa84908db157
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-02-25 10:43:02 -0600

    linux: 5.4.232 -> 5.4.233

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.232";
+  version = "5.4.233";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "14qf8ddyhcs1ga1zqj4q2si6nc48sfc67nn9nigsnbcbg5g6lrdh";
+    sha256 = "09vnp4qcv7kwahbbvjznnv7pxq1cvbn11n0rn5rzx97jnia5f7js";
   };
 } // (args.argsOverride or {}))
