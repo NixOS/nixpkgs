767906cad119a2b22835d00996fbc91ee6e67d05
Jan Tojnar <jtojnar@gmail.com>
2023-02-25 03:02:33 +0000

    gnome.gnome-system-monitor: 42.0 → 44.beta
    https://gitlab.gnome.org/GNOME/gnome-system-monitor/-/compare/42.0...44.beta
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-system-monitor/default.nix
+++ pkgs/desktops/gnome/core/gnome-system-monitor/default.nix
@@ -23,11 +23,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-system-monitor";
-  version = "42.0";
+  version = "44.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-system-monitor/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "EyOdIgMiAaIr0pgzxXW2hIFnANLeFooVMCI1d8XAddw=";
+    sha256 = "Egfvkub0i3xayAddrZlqML0YW1Ub2EuIJy6jnUDWD/I=";
   };
 
   patches = [
