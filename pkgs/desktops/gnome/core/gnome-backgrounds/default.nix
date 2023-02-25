9d062c1c30dcdc33b45f4043df6b4f33f324ccb6
Jan Tojnar <jtojnar@gmail.com>
2023-02-25 03:01:34 +0000

    gnome.gnome-backgrounds: 43.1 → 44.beta
    https://gitlab.gnome.org/GNOME/gnome-backgrounds/-/compare/43.1...44.beta
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-backgrounds/default.nix
+++ pkgs/desktops/gnome/core/gnome-backgrounds/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-backgrounds";
-  version = "43.1";
+  version = "44.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-backgrounds/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "jkWcrTinV0aDpV5ed2kOZYxvn+ruycuCA5qyW6K8oF0=";
+    sha256 = "UBPTQ4gq+84r35GoiwewsNkG1m3jY8+UOG/oaLbg6cA=";
   };
 
   patches = [
