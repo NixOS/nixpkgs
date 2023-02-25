6d265b44590e6877f09822e277982552337d04f5
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-25 20:14:22 +0000

    cariddi: 1.2.1 -> 1.3.0

--- pkgs/tools/security/cariddi/default.nix
+++ pkgs/tools/security/cariddi/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "cariddi";
-  version = "1.2.1";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "edoardottt";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-8Z2iswjl85rsIhHMAGD3kYJanBWToWBVidglWMg7omw=";
+    sha256 = "sha256-pO1FXlkaQveDIfMSWiLB9QvVxmFJixc/HHcEuhg5KmY=";
   };
 
-  vendorSha256 = "sha256-mXzI3NF1afMvQ4STPpbehoarfOT35P01IotXPVYNnio=";
+  vendorHash = "sha256-zDKByBISZNRb4sMCrHKGlp4EBtifBfj92tygcaBH/Fc=";
 
   meta = with lib; {
     description = "Crawler for URLs and endpoints";
