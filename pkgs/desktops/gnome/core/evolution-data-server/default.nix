e26648b986888534a6779c59d029a1b151668f14
Bobby Rong <rjl931189261@126.com>
2023-02-25 05:45:08 +0000

    evolution-data-server: 3.46.4 → 3.47.2
    https://gitlab.gnome.org/GNOME/evolution-data-server/-/compare/3.46.4...3.47.2
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/evolution-data-server/default.nix
+++ pkgs/desktops/gnome/core/evolution-data-server/default.nix
@@ -50,13 +50,13 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution-data-server";
-  version = "3.46.4";
+  version = "3.47.2";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution-data-server/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "pZslQUXFn6zXx7U4LbeNxfDtH2pum4/n1edZWfk8DMg=";
+    sha256 = "VRRMHzPmUD5BMKAd3Ysot1uvmj6Xab4OJ1Yd/sy830Y=";
   };
 
   patches = [
