b905b8903b65c06e8a949b76216d89365907a220
Bobby Rong <rjl931189261@126.com>
2023-02-25 20:52:58 +0800

    libdex: init at 0.1.0
    https://gitlab.gnome.org/GNOME/gnome-builder/-/commit/17f35418d26d0b1a6077bd38fcbbd3961085cd43
    https://gitlab.gnome.org/GNOME/libdex
    
    Co-authored-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21149,6 +21149,8 @@ with pkgs;
 
   libdecor = callPackage ../development/libraries/libdecor { };
 
+  libdex = callPackage ../development/libraries/libdex { };
+
   libdigidocpp = callPackage ../development/libraries/libdigidocpp { };
 
   libdiscid = callPackage ../development/libraries/libdiscid { };
