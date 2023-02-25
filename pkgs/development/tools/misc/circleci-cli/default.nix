b4d9b64de749ac2cc60cfdbdddf76dc52c078057
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-25 02:09:09 +0000

    circleci-cli: 0.1.23391 -> 0.1.23667

--- pkgs/development/tools/misc/circleci-cli/default.nix
+++ pkgs/development/tools/misc/circleci-cli/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "circleci-cli";
-  version = "0.1.23391";
+  version = "0.1.23667";
 
   src = fetchFromGitHub {
     owner = "CircleCI-Public";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-mTqrckoYbxtlwonXp6mtbXSpmVB3WfSXaOkyycBVjx0=";
+    sha256 = "sha256-VrRdcp03B/Q3tUD/WKKKr7svNiOXFMM2s6EuIda6z6g=";
   };
 
-  vendorHash = "sha256-7k5jmXvNtxezialavNz8NM7esl2vpXcjRW2n/9GAycs=";
+  vendorHash = "sha256-6l2cblbpIORSqhu2rjzmDl/7y/Vki7fX/b5002jDKd0=";
 
   nativeBuildInputs = [ installShellFiles ];
 
