6dd6e88a881432cadaa7e50a0ff94116ba1ddd7a
Thomas Gerbet <thomas@gerbet.me>
2023-02-25 10:01:44 +0100

    cosign: 1.13.1 -> 2.0.0
    https://github.com/sigstore/cosign/releases/tag/v2.0.0
    https://github.com/sigstore/cosign/blob/69c9b37f2a021326c67b2aa9552c790e12ae5fb8/CHANGELOG.md

--- pkgs/tools/security/cosign/default.nix
+++ pkgs/tools/security/cosign/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "cosign";
-  version = "1.13.1";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "sigstore";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-R7MhfAnVJJ2NK8zV408xAk8Q6aWn9Gw6DOmFFX26x1Q=";
+    sha256 = "sha256-919oxYi4e56EhSBN0FdcEZBA430owaDnKHkgTneScXw=";
   };
 
   buildInputs = lib.optional (stdenv.isLinux && pivKeySupport) (lib.getDev pcsclite)
@@ -16,7 +16,7 @@ buildGoModule rec {
 
   nativeBuildInputs = [ pkg-config installShellFiles ];
 
-  vendorSha256 = "sha256-DpPEDttQnRGHVNiIpMGj14KvZEGR0Y80sZOffjQ3UHk=";
+  vendorSha256 = "sha256-DtFywktiGHlsdOFVpKUtKLYXJYwQYy1VISfUYVXlOG8=";
 
   subPackages = [
     "cmd/cosign"
@@ -35,8 +35,9 @@ buildGoModule rec {
     # test all paths
     unset subPackages
 
+    rm pkg/cosign/ctlog_test.go # Require network access
     rm pkg/cosign/tlog_test.go # Require network access
-    rm pkg/cosign/verify_test.go # Require network access
+    rm cmd/cosign/cli/verify/verify_blob_attestation_test.go # Require network access
   '';
 
   postInstall = ''
