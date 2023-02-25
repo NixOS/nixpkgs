c3448c1b55c1318da0f325bc27833cd2bc9e45c8
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-25 18:41:51 +0000

    wangle: 2023.02.13.00 -> 2023.02.20.00

--- pkgs/development/libraries/wangle/default.nix
+++ pkgs/development/libraries/wangle/default.nix
@@ -18,13 +18,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wangle";
-  version = "2023.02.13.00";
+  version = "2023.02.20.00";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "wangle";
     rev = "v${version}";
-    sha256 = "sha256-Qt+wW9y16ESVBAU3BkdZc/ho1eZNLcCd/+NaUbnJjKU=";
+    sha256 = "sha256-islo6jbilDaWc48GfipXuPhUaY2Gn95PDjuJsEDnn50=";
   };
 
   nativeBuildInputs = [ cmake ];
