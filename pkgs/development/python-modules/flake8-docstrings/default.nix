8f83da4938405cfaed7f943ee4344d43c79b74dd
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-25 13:44:22 +0100

    python310Packages.flake8-docstrings: update homepage and add changelog

--- pkgs/development/python-modules/flake8-docstrings/default.nix
+++ pkgs/development/python-modules/flake8-docstrings/default.nix
@@ -20,7 +20,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Extension for flake8 which uses pydocstyle to check docstrings";
-    homepage = "https://gitlab.com/pycqa/flake8-docstrings";
+    homepage = "https://github.com/pycqa/flake8-docstrings";
+    changelog = "https://github.com/PyCQA/flake8-docstrings/blob/${version}/HISTORY.rst";
     license = licenses.mit;
     maintainers = with maintainers; [ smaret ];
   };
