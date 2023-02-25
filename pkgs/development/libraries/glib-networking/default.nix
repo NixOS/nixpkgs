744b94e8df610fb3148f4e9888e6283f60bcef5a
Jan Tojnar <jtojnar@gmail.com>
2023-02-25 03:00:34 +0000

    glib-networking: 2.74.0 → 2.76.beta
    https://gitlab.gnome.org/GNOME/glib-networking/-/compare/2.74.0...2.76.beta
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/glib-networking/default.nix
+++ pkgs/development/libraries/glib-networking/default.nix
@@ -19,13 +19,13 @@
 
 stdenv.mkDerivation rec {
   pname = "glib-networking";
-  version = "2.74.0";
+  version = "2.76.beta";
 
   outputs = [ "out" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "HxharvCUEj+OJdj6VWYbP9cQIBY6AXSts1o3aFzaYTs=";
+    sha256 = "wKtbDzWDMlIQWOsH88mj/awOelIBxBMKVnvZ1s0/HaE=";
   };
 
   patches = [
