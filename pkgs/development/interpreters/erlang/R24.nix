6fad8ba8511cd3e9e8143d32d9b0cbf2d377f903
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-25 14:56:01 +0000

    erlangR24: 24.3.4.8 -> 24.3.4.9

--- pkgs/development/interpreters/erlang/R24.nix
+++ pkgs/development/interpreters/erlang/R24.nix
@@ -1,6 +1,6 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "24.3.4.8";
-  sha256 = "sha256-NwnGqFEhVi96fEPLKoAqJgvWNZEsRGtE/3HP0eRthuA=";
+  version = "24.3.4.9";
+  sha256 = "sha256-toM2AoPAle+eNKg0to3r/EYT2taJ9OwKvde4Jr++ZE0=";
 }
