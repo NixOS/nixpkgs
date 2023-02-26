867a6363d4605805e746758c4f10d7fddbedc9f1
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-26 11:59:34 +0100

    python310Packages.django-cleanup: equalize

--- pkgs/development/python-modules/django-cleanup/default.nix
+++ pkgs/development/python-modules/django-cleanup/default.nix
@@ -1,15 +1,21 @@
-{ lib, buildPythonPackage, fetchPypi, django
+{ lib
+, buildPythonPackage
+, fetchPypi
+, django
 }:
 
 buildPythonPackage rec {
   pname = "django-cleanup";
   version = "7.0.0";
+
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-KKlp+InGYeug2UOJeGk5gPCUgsl5g70I7lKVXa6NceQ=";
   };
 
-  nativeCheckInputs = [ django ];
+  nativeCheckInputs = [
+    django
+  ];
 
   meta = with lib; {
     description = "Automatically deletes old file for FileField and ImageField. It also deletes files on models instance deletion";
