9d921c41977e93a7917da637f80cb401d5be1302
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-26 12:05:27 +0100

    python310Packages.criticality-score: add changelog to meta

--- pkgs/development/python-modules/criticality-score/default.nix
+++ pkgs/development/python-modules/criticality-score/default.nix
@@ -18,6 +18,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python tool for computing the Open Source Project Criticality Score.";
     homepage = "https://github.com/ossf/criticality_score";
+    changelog = "https://github.com/ossf/criticality_score/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ wamserma ];
   };
