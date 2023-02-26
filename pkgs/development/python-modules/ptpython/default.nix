03337d130cd17203525896c5dd31ae3dd899a5c0
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-26 11:38:56 +0000

    python310Packages.ptpython: 3.0.22 -> 3.0.23

--- pkgs/development/python-modules/ptpython/default.nix
+++ pkgs/development/python-modules/ptpython/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "ptpython";
-  version = "3.0.22";
+  version = "3.0.23";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-wJezJEBgFKAFxrCfSTu73eVx5BI2CvK6FVGHOhDDbPg=";
+    hash = "sha256-n8m+wsxRvEAAwSJNjFYkHOikBrPUnsjcJm94zTzQS6Q=";
   };
 
   propagatedBuildInputs = [
