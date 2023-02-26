526fa7eefbb8325bc10f853a6b4e9f10429d84fe
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-27 00:30:17 +0100

    python310Packages.pyoverkiz: 1.7.4 -> 1.7.6
    Diff: https://github.com/iMicknl/python-overkiz-api/compare/refs/tags/v1.7.4...v1.7.6
    
    Changelog: https://github.com/iMicknl/python-overkiz-api/releases/tag/v1.7.6

--- pkgs/development/python-modules/pyoverkiz/default.nix
+++ pkgs/development/python-modules/pyoverkiz/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "pyoverkiz";
-  version = "1.7.4";
+  version = "1.7.6";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "iMicknl";
     repo = "python-overkiz-api";
     rev = "refs/tags/v${version}";
-    hash = "sha256-I115lMEg/baWuUZThfisNYSck/QdiYecjhhRqgSkLB8=";
+    hash = "sha256-nmXOmoPH8w4Soj8lhI7wl3uYVmKw3xSuIkmCF0XI7RI=";
   };
 
   postPatch = ''
