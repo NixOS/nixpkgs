c73a83b29d0b2eb99479928f7dfdba26dce121dc
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-26 08:00:32 +0000

    fastly: 6.0.6 -> 7.0.0

--- pkgs/misc/fastly/default.nix
+++ pkgs/misc/fastly/default.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "fastly";
-  version = "6.0.6";
+  version = "7.0.0";
 
   src = fetchFromGitHub {
     owner = "fastly";
     repo = "cli";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ZAYzZBOs/A/2krpO4FcGO2W1Afr3Qz1yDAZgG6SPirU=";
+    hash = "sha256-+fXx/fVXOAjwo+TLP1pDTVge3VSvTLSGzTv7dSTeXxc=";
     # The git commit is part of the `fastly version` original output;
     # leave that output the same in nixpkgs. Use the `.git` directory
     # to retrieve the commit SHA, and remove the directory afterwards,
@@ -31,7 +31,7 @@ buildGoModule rec {
     "cmd/fastly"
   ];
 
-  vendorHash = "sha256-ORS74tFYWlVS+vJXNJyoqpUtg7nqi3fuqV1MwudDJks=";
+  vendorHash = "sha256-oQwlhlZHpUvGaOyozBpK5W9glzo8VKH7S3vih5c15OA=";
 
   nativeBuildInputs = [
     installShellFiles
