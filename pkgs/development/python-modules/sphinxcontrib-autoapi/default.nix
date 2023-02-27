10aa1789a9dcc7010ce68ebfe04c6fea10efed02
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 01:50:20 +0100

    python310Packages.sphinxcontrib-autoapi: 2.0.0 -> 2.0.1
    https://github.com/readthedocs/sphinx-autoapi/blob/v2.0.1/CHANGELOG.rst

--- pkgs/development/python-modules/sphinxcontrib-autoapi/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-autoapi/default.nix
@@ -9,19 +9,20 @@
 , pyyaml
 , sphinx
 , stdenv
+, typing-extensions
 , unidecode
 }:
 
 buildPythonPackage rec {
   pname = "sphinx-autoapi";
-  version = "2.0.0";
+  version = "2.0.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-l9zxtbVM0Njv74Z1lOSk8+LTosDsHlqJHgphvHcEYAY=";
+    hash = "sha256-zfR5aMIIUvT+sMzv0J5BS7ggr4r4+C+rFaJLCaPRuro=";
   };
 
   propagatedBuildInputs = [
@@ -30,6 +31,8 @@ buildPythonPackage rec {
     pyyaml
     sphinx
     unidecode
+  ] ++ lib.optionals (pythonOlder "3.11") [
+    typing-extensions
   ];
 
   nativeCheckInputs = [
@@ -43,8 +46,13 @@ buildPythonPackage rec {
 
   meta = with lib; {
     homepage = "https://github.com/readthedocs/sphinx-autoapi";
+    changelog = "https://github.com/readthedocs/sphinx-autoapi/blob/v${version}/CHANGELOG.rst";
     description = "Provides 'autodoc' style documentation";
-    longDescription = "Sphinx AutoAPI provides 'autodoc' style documentation for multiple programming languages without needing to load, run, or import the project being documented.";
+    longDescription = ''
+      Sphinx AutoAPI provides 'autodoc' style documentation for
+      multiple programming languages without needing to load, run, or
+      import the project being documented.
+    '';
     license = licenses.mit;
     maintainers = with maintainers; [ karolchmist ];
   };
