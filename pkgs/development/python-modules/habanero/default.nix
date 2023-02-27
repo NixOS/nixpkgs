f0d84dd8d136cabd239f3f36a6fa1b34aff89ad1
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:22 +0100

    python3Packages.habanero: 1.2.2 -> 1.2.3

--- pkgs/development/python-modules/habanero/default.nix
+++ pkgs/development/python-modules/habanero/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "habanero";
-  version = "1.2.2";
+  version = "1.2.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "sckott";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-i6tgMEiaDcaBR8XfGvEMXQfTaDp1RJRosj/EfF1dQU4=";
+    hash = "sha256-IQp85Cigs0in3X07a9d45nMC3X2tAkPzl5hFVhfr00o=";
   };
 
   propagatedBuildInputs = [
