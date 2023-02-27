c29e2c0cb7ce76cb1dd1bb2c93e89fad77f5a666
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:42 +0100

    python3Packages.pytest-localserver: 0.7.0 -> 0.7.1

--- pkgs/development/python-modules/pytest-localserver/default.nix
+++ pkgs/development/python-modules/pytest-localserver/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "pytest-localserver";
-  version = "0.7.0";
+  version = "0.7.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-8ZtJDHyh7QW/5LxrSQgRgFph5ycfBCTTwPpNQ4k6Xc0=";
+    sha256 = "sha256-53tGVVy0OJMdIZxUvPn1lTwEB9ZFQ2pgK1cwur/T/js=";
   };
 
   propagatedBuildInputs = [
