14e9e10ee75b2278ea9d4d66c08edbcfd5e9394e
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:49 +0100

    python3Packages.sexpdata: 0.0.3 -> 0.0.4

--- pkgs/development/python-modules/sexpdata/default.nix
+++ pkgs/development/python-modules/sexpdata/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "sexpdata";
-  version = "0.0.3";
+  version = "0.0.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1ac827a616c5e87ebb60fd6686fb86f8a166938c645f4089d92de3ffbdd494e0";
+    sha256 = "sha256-5Xb2Lq0y1QQeYw+UV9LBp1RNf+XdlqSbVRWSORFcN3M=";
   };
 
   doCheck = false;
