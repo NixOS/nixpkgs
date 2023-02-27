f7db42ebc4b6a406aa448dd19b1951ba7c9457f4
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:15:01 +0100

    python3Packages.zstandard: 0.19.0 -> 0.20.0

--- pkgs/development/python-modules/zstandard/default.nix
+++ pkgs/development/python-modules/zstandard/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "zstandard";
-  version = "0.19.0";
+  version = "0.20.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-MdEvzZQt2Nv1LKX2sbvih/ROXVUaCBqYP/PqIIKGeGM=";
+    sha256 = "sha256-YT2q3XLHGxSIdCyvssOzgcOdDJu4xswVeqLV6kXMLvw=";
   };
 
   propagatedNativeBuildInputs = [
