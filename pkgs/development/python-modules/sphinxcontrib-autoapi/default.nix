9aa911312e98fa318b0978d473971e58864886c0
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 01:50:20 +0100

    python310Packages.sphinxcontrib-autoapi: 2.0.0 -> 2.0.1
    https://github.com/readthedocs/sphinx-autoapi/blob/v2.0.1/CHANGELOG.rst

--- pkgs/development/python-modules/sphinxcontrib-autoapi/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-autoapi/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "sphinx-autoapi";
-  version = "2.0.0";
+  version = "2.0.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-l9zxtbVM0Njv74Z1lOSk8+LTosDsHlqJHgphvHcEYAY=";
+    hash = "sha256-zfR5aMIIUvT+sMzv0J5BS7ggr4r4+C+rFaJLCaPRuro=";
   };
 
   propagatedBuildInputs = [
@@ -43,8 +43,13 @@ buildPythonPackage rec {
 
   meta = with lib; {
     homepage = "https://github.com/readthedocs/sphinx-autoapi";
+    changelog = "https://github.com/readthedocs/sphinx-autoapi/blob/v${version}/CHANGELOG.rst";
     description = "Provides 'autodoc' style documentation";
-    longDescription = "Sphinx AutoAPI provides 'autodoc' style documentation for multiple programming languages without needing to load, run, or import the project being documented.";
+    longDescription = ''
+      Sphinx AutoAPI provides 'autodoc' style documentation for
+      multiple programming languages without needing to load, run, or
+      import the project being documented.
+    '';
     license = licenses.mit;
     maintainers = with maintainers; [ karolchmist ];
   };
