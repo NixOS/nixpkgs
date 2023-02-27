8c65972b9eeb481036a9d8b94e2053b2519a6b81
Alyssa Ross <hi@alyssa.is>
2023-02-27 11:37:40 +0000

    clang_15: mark hardeningUnsupportedFlags fortify3
    Port of 74ea4fe24fb ("llvm*Packages.clang: mark
    hardeningUnsupportedFlags fortify3").

--- pkgs/development/compilers/llvm/15/clang/default.nix
+++ pkgs/development/compilers/llvm/15/clang/default.nix
@@ -87,8 +87,9 @@ let
     '';
 
     passthru = {
-      isClang = true;
       inherit libllvm;
+      isClang = true;
+      hardeningUnsupportedFlags = [ "fortify3" ];
     };
 
     meta = llvm_meta // {
