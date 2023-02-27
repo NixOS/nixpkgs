42caad0ad061d5286c49e1af5b609784dffd1774
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:14 +0100

    python3Packages.dash: 2.7.0 -> 2.8.1

--- pkgs/development/python-modules/dash/default.nix
+++ pkgs/development/python-modules/dash/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "dash";
-  version = "2.7.0";
+  version = "2.8.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = "plotly";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-kxat6CjX4xPEtlhRiYJF5wN2Luds7DduZyiUA9/kKWY=";
+    hash = "sha256-6FsLvLsqnkSt/i27q/JJGfNh2zxKeA0t6VYNPCzhR0w=";
   };
 
   propagatedBuildInputs = [
