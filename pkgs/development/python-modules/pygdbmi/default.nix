7de86e080cf776c30b97fddb3aaaed2c0e8a9275
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:38 +0100

    python3Packages.pygdbmi: 0.10.0.0 -> 0.11.0.0

--- pkgs/development/python-modules/pygdbmi/default.nix
+++ pkgs/development/python-modules/pygdbmi/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "pygdbmi";
-  version = "0.10.0.0";
+  version = "0.11.0.0";
 
   src = fetchFromGitHub {
     owner = "cs01";
     repo = "pygdbmi";
-    rev = version;
-    sha256 = "0a6b3zyxwdcb671c6lrwxm8fhvsbjh0m8hf1r18m9dha86laimjr";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-JqEDN8Pg/JttyYQbwkxKkLYuxVnvV45VlClD23eaYyc=";
   };
 
   nativeCheckInputs = [ gdb ];
