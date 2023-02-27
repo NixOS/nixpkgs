0e7c9c726106bb9130bf8e26a0279b26c67c9bc0
Hans Christian Schmitz <git@hcsch.eu>
2023-02-27 01:07:24 +0100

    owncloud-client: 2.11.0.8354 -> 3.2.0.10193
    https://github.com/owncloud/client/blob/718af2f484d20437de8c237f5b177c60c0a3960d/CHANGELOG.md

--- pkgs/applications/networking/owncloud-client/default.nix
+++ pkgs/applications/networking/owncloud-client/default.nix
@@ -1,16 +1,34 @@
-{ lib, fetchurl, mkDerivation, cmake, extra-cmake-modules, pkg-config, qtbase, qtkeychain, sqlite, libsecret }:
+{ lib, fetchurl, fetchFromGitHub, mkDerivation, cmake, extra-cmake-modules, pkg-config, qttools, qtbase, qtkeychain, qtdeclarative, inotify-tools, sqlite, libsecret }:
 
+let
+  libre-graph-api = mkDerivation rec {
+    pname = "libre-graph-api-cpp-qt-client";
+    version = "1.0.1";
+
+    src = fetchFromGitHub {
+      owner = "owncloud";
+      repo = pname;
+      rev = "31ca753c65864a494f92654f7443f57d39a72072";
+      sha256 = "sha256-oHWhiSGcgvfImND4PdkWCdS2AGjwwim5oGSpwO8BstM=";
+    };
+
+    sourceRoot = "source/client";
+
+    nativeBuildInputs = [ pkg-config cmake extra-cmake-modules ];
+    buildInputs = [ qtbase ];
+  };
+in
 mkDerivation rec {
   pname = "owncloud-client";
-  version = "2.11.0.8354";
+  version = "3.2.0.10193";
 
   src = fetchurl {
     url = "https://download.owncloud.com/desktop/ownCloud/stable/${version}/source/ownCloud-${version}.tar.xz";
-    sha256 = "sha256-YraWvGgeF5b1+3i5Jlk+bwvAULr7KFOSX8y0ZoPpljI=";
+    sha256 = "sha256-QcNKpvXOgzHRUSVJaQt8ncKkViBan65RhDCJJOspH3Y=";
   };
 
-  nativeBuildInputs = [ pkg-config cmake extra-cmake-modules ];
-  buildInputs = [ qtbase qtkeychain sqlite libsecret ];
+  nativeBuildInputs = [ pkg-config cmake extra-cmake-modules qttools ];
+  buildInputs = [ qtbase qtdeclarative qtkeychain inotify-tools sqlite libsecret libre-graph-api ];
 
   qtWrapperArgs = [
     "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libsecret ]}"
