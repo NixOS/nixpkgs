55ed3f7f70e8c054750daa150396507c0fa50573
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:26 +0100

    python3Packages.jenkinsapi: 0.3.11 -> 0.3.13

--- pkgs/development/python-modules/jenkinsapi/default.nix
+++ pkgs/development/python-modules/jenkinsapi/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "jenkinsapi";
-  version = "0.3.11";
+  version = "0.3.13";
   format = "setuptools";
 
   disabled = pythonAtLeast "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "a212a244b0a6022a61657746c8120ac9b6db83432371b345154075eb8faceb61";
+    sha256 = "sha256-JGqYpj5h9UoV0WEFyxVIjFZwc030HobHrw1dnAryQLk=";
   };
 
   propagatedBuildInputs = [ pytz requests ];
