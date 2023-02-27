37eba45604e13fef6583cc7479b2ec0026cf24ff
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:35 +0100

    python3Packages.class-registry: 3.0.5 -> 4.0.6

--- pkgs/development/python-modules/phx-class-registry/default.nix
+++ pkgs/development/python-modules/phx-class-registry/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "class-registry";
-  version = "3.0.5";
+  version = "4.0.6";
   disabled = pythonOlder "3.5";
 
   src = fetchFromGitHub {
     owner = "todofixthis";
     repo = pname;
-    rev = version;
-    sha256 = "0gpvq4a6qrr2iki6b4vxarjr1jrsw560m2qzm5bb43ix8c8b7y3q";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-kSEHgzBgnAq5rMv2HbmGl+9CUzsmzUzPQWr+5q8mcsA=";
   };
 
   nativeCheckInputs = [
