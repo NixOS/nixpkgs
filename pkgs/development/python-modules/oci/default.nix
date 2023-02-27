fc14705f97f6f42de392b3a61e5d4a6f9614d6e2
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-27 13:59:58 +0100

    python310Packages.oci: 2.92.0 -> 2.93.0
    Diff: https://github.com/oracle/oci-python-sdk/compare/refs/tags/v2.92.0...v2.93.0
    
    Changelog: https://github.com/oracle/oci-python-sdk/blob/v2.93.0/CHANGELOG.rst

--- pkgs/development/python-modules/oci/default.nix
+++ pkgs/development/python-modules/oci/default.nix
@@ -7,22 +7,32 @@
 , pyopenssl
 , python-dateutil
 , pythonOlder
+, pythonRelaxDepsHook
 , pytz
 }:
 
 buildPythonPackage rec {
   pname = "oci";
-  version = "2.92.0";
+  version = "2.93.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "oracle";
     repo = "oci-python-sdk";
     rev = "refs/tags/v${version}";
-    hash = "sha256-evxY/1MhSCZOTjhH/2tI5amnZpd+fCdSdFBD4W9uZ+c=";
+    hash = "sha256-yH9SCrTASP+O9VLVs6tliteCOWi+Pc7nDOgiboHQE4s=";
   };
+
+  pythonRelaxDeps = [
+    "cryptography"
+    "pyOpenSSL"
+  ];
+
+  nativeBuildInputs = [
+    pythonRelaxDepsHook
+  ];
 
   propagatedBuildInputs = [
     certifi
@@ -32,13 +42,6 @@ buildPythonPackage rec {
     python-dateutil
     pytz
   ];
-
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace "configparser==4.0.2 ; python_version < '3'" "" \
-      --replace "cryptography>=3.2.1,<=37.0.2" "cryptography" \
-      --replace "pyOpenSSL>=17.5.0,<=22.0.0" "pyOpenSSL"
-  '';
 
   # Tests fail: https://github.com/oracle/oci-python-sdk/issues/164
   doCheck = false;
