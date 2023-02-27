5f351a0f8102608df4195e729696991e4ba8201e
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:26 +0100

    python3Packages.json-stream: 1.5.1 -> 2.2.0

--- pkgs/development/python-modules/json-stream/default.nix
+++ pkgs/development/python-modules/json-stream/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "json-stream";
-  version = "1.5.1";
+  version = "2.2.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-htajifmbXtivUwsORzBzJA68nJCACcL75kiBysVYCxY=";
+    hash = "sha256-Vg6zF4iR4YqVAsx93Gy5mO2JNldm2f7BhNBtjzVY82w=";
   };
 
   nativeBuildInputs = [
