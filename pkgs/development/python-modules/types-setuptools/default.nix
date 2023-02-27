d0434d9488961a32f9ba96e9064c34249e496d9d
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:56 +0100

    python3Packages.types-setuptools: 65.6.0.2 -> 67.4.0.3

--- pkgs/development/python-modules/types-setuptools/default.nix
+++ pkgs/development/python-modules/types-setuptools/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "types-setuptools";
-  version = "65.6.0.2";
+  version = "67.4.0.3";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-rWDM8B1ibel2IiREjzbBPgZg6GOv1twR2Xmzc5psfSQ=";
+    sha256 = "sha256-GelY39vxxaYo5UwqfuhJNQUa+3J40MHNsIrBlHV+47E=";
   };
 
   # Module doesn't have tests
