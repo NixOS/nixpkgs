652980ea3b7599bb67848fe886168f0798646c90
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:58 +0100

    python3Packages.voluptuous-serialize: 2.5.0 -> 2.6.0

--- pkgs/development/python-modules/voluptuous-serialize/default.nix
+++ pkgs/development/python-modules/voluptuous-serialize/default.nix
@@ -8,15 +8,15 @@
 
 buildPythonPackage rec  {
   pname = "voluptuous-serialize";
-  version = "2.5.0";
+  version = "2.6.0";
 
   disabled = !isPy3k;
 
   src = fetchFromGitHub {
     owner = "home-assistant-libs";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-8rWMz8tBanxHdU/F4HhBxxz3ltqbdRoP4JED2dmZfTk=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-vvreXSQDkA3JkZpOKZqJgMRyObJX/cSR8r+A26h9fNE=";
   };
 
   propagatedBuildInputs = [ voluptuous ];
