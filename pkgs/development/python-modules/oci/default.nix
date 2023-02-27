45c2057059a8db33627c6e2c7e6720758454de19
Fabian Affolter <mail@fabian-affolter.ch>
2023-02-27 13:53:57 +0100

    python310Packages.oci: update homepage and add changelog

--- pkgs/development/python-modules/oci/default.nix
+++ pkgs/development/python-modules/oci/default.nix
@@ -49,7 +49,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Oracle Cloud Infrastructure Python SDK";
-    homepage = "https://oracle-cloud-infrastructure-python-sdk.readthedocs.io/";
+    homepage = "https://github.com/oracle/oci-python-sdk";
+    changelog = "https://github.com/oracle/oci-python-sdk/blob/v${version}/CHANGELOG.rst";
     license = with licenses; [ asl20 /* or */ upl ];
     maintainers = with maintainers; [ ilian ];
   };
