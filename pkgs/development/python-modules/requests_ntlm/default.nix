f66eb56a49cf64ed97b30bfa543b72dd7cbada50
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:46 +0100

    python3Packages.requests_ntlm: 1.1.0 -> 1.2.0

--- pkgs/development/python-modules/requests_ntlm/default.nix
+++ pkgs/development/python-modules/requests_ntlm/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "requests_ntlm";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9189c92e8c61ae91402a64b972c4802b2457ce6a799d658256ebf084d5c7eb71";
+    sha256 = "sha256-M8KF9QdOMXy90zjRma+kanwBEy5cER02vUFVNOm5Fqg=";
   };
 
   propagatedBuildInputs = [ ntlm-auth requests ];
