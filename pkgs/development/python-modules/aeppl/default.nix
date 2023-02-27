35ade2dd52e223cc3ec150a1d41ac9d39dc08494
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:04 +0100

    python3Packages.aeppl: 0.0.50 -> 0.1.2

--- pkgs/development/python-modules/aeppl/default.nix
+++ pkgs/development/python-modules/aeppl/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "aeppl";
-  version = "0.0.50";
+  version = "0.1.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "aesara-devs";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-cc41MspG2mXlNwLz7ViPPqXH/ayskVmms5SXqBo9g3Y=";
+    hash = "sha256-SxMuYKnV4VBv38CcAI7NCvHutSEB+coHnw5b1RPEpzY=";
   };
 
   propagatedBuildInputs = [
