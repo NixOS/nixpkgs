135ea2424c09577c8dabe27244a022ca16111a14
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:20 +0100

    python3Packages.google-api-python-client: 2.75.0 -> 2.79.0

--- pkgs/development/python-modules/google-api-python-client/default.nix
+++ pkgs/development/python-modules/google-api-python-client/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-api-python-client";
-  version = "2.75.0";
+  version = "2.79.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-DxCaK3HxTJp7SCMf7PzdOrlYYeqav1TwYKxFwgT63D0=";
+    sha256 = "sha256-V3wK6uHrPHVOrLkSLTadZ2Cf73WbxqT6Fsr+q08wAZs=";
   };
 
   propagatedBuildInputs = [
