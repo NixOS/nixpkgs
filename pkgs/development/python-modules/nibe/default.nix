c667d8a4b06fd15a6683d6dbc732594c982ba147
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:32 +0100

    python3Packages.nibe: 1.6.0 -> 2.0.0

--- pkgs/development/python-modules/nibe/default.nix
+++ pkgs/development/python-modules/nibe/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "nibe";
-  version = "1.6.0";
+  version = "2.0.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "yozik04";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-6pQsVGb26FpoV2LgOrs+Cfq2rATRqbljrVJ+NsZUSuc=";
+    hash = "sha256-e5rKtVFSlB4sFBrBHKrZmHq/sJEL9VZejSpUgeCwCzw=";
   };
 
   nativeBuildInputs = [
