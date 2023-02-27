964282500d945af54abc721b3d5f63034b1f3cba
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:26 +0100

    python3Packages.jupyter_client: 7.4.8 -> 8.0.3

--- pkgs/development/python-modules/jupyter-client/default.nix
+++ pkgs/development/python-modules/jupyter-client/default.nix
@@ -15,12 +15,12 @@
 
 buildPythonPackage rec {
   pname = "jupyter_client";
-  version = "7.4.8";
+  version = "8.0.3";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-EJo8M7YqnPZaqDJYUKCZmnlfrBVdneT3VVrvXzEO41o=";
+    sha256 = "sha256-7WVJi+pth2752No+DbPdM8XRKfWyZF9WrgOZN4KWa9A=";
   };
 
   nativeBuildInputs = [
