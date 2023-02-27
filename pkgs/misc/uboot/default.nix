614b63922b6fd8bcc28760fa52c495f55fa35800
Jade Lovelace <software@lfcode.ca>
2023-02-26 21:25:28 -0800

    ubootTools: add man pages
    As far as I can tell, there are no makefile targets to install these, or
    anything like that, so we have to do it by hand.
    
    This adds man pages for dumpimage, kwboot, mkeficapsule, and mkimage.
    There are no man pages in other sections than 1.

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -8,6 +8,7 @@
 , fetchurl
 , flex
 , gnutls
+, installShellFiles
 , libuuid
 , meson-tools
 , ncurses
@@ -66,6 +67,7 @@ let
       bison
       dtc
       flex
+      installShellFiles
       openssl
       (buildPackages.python3.withPackages (p: [
         p.libfdt
@@ -136,6 +138,12 @@ in {
     dontStrip = false;
     extraMeta.platforms = lib.platforms.linux;
     extraMakeFlags = [ "HOST_TOOLS_ALL=y" "CROSS_BUILD_TOOLS=1" "NO_SDL=1" "tools" ];
+
+    outputs = [ "out" "man" ];
+
+    postInstall = ''
+      installManPage doc/*.1
+    '';
     filesToInstall = [
       "tools/dumpimage"
       "tools/fdtgrep"
