c250e04b4ea9a02ec5d45595b1da0c36aa1a4df5
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:11 +0100

    python3Packages.cachelib: 0.9.0 -> 0.10.2

--- pkgs/development/python-modules/cachelib/default.nix
+++ pkgs/development/python-modules/cachelib/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "cachelib";
-  version = "0.9.0";
+  version = "0.10.2";
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "pallets";
     repo = pname;
     rev = "refs/tags/${version}";
-    sha256 = "sha256-LO1VdirKWXIAy3U8oRtnFI58qO+yn6Vm5bZdCjdgKwo=";
+    sha256 = "sha256-2V2FvZC8jM84fZEdK9ShzFrjO8goOQsN6cnJTHDDL9E=";
   };
 
   nativeCheckInputs = [
