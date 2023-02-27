e25bcd0c3a01ce721ecf19b5a1d10389cab7afb0
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:17 +0100

    python3Packages.dvc-objects: 0.19.3 -> 0.20.0

--- pkgs/development/python-modules/dvc-objects/default.nix
+++ pkgs/development/python-modules/dvc-objects/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "dvc-objects";
-  version = "0.19.3";
+  version = "0.20.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "iterative";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-oKK+BhOgdRPZZAACgxgmr9rlzEH9yWmvbmx09d42u/Y=";
+    hash = "sha256-T7vcnxxN0YUHoDVc98f0QwiMqT88lx8NejTsDfBY5SY=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
