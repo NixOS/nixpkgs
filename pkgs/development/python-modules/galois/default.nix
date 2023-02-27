4a10124852d47845729cd452f691442ff1826478
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-27 02:14:19 +0100

    python3Packages.galois: 0.3.2 -> 0.3.3

--- pkgs/development/python-modules/galois/default.nix
+++ pkgs/development/python-modules/galois/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "galois";
-  version = "0.3.2";
+  version = "0.3.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "mhostetter";
     repo = "galois";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-8YxC0QYZqgUIKlYLXnm5+tfojQOmOX63dD7cxBO+0Gg=";
+    sha256 = "sha256-nlNy4+GwStyuCD8f47w07NhNmepxBRtu6PXiL31vzwA=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
