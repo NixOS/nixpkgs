5d25d94316ca730d2a58ef0c4a4d64c4b11bd63b
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2023-02-28 13:05:41 +0200

    eks-node-viewer: 0.1.0 -> 0.1.3

--- pkgs/applications/networking/cluster/eks-node-viewer/default.nix
+++ pkgs/applications/networking/cluster/eks-node-viewer/default.nix
@@ -2,20 +2,21 @@
 
 buildGoModule rec {
   pname = "eks-node-viewer";
-  version = "0.1.0";
+  version = "0.1.3";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-PqZVVfq/cUtkmpruJfxphCeQWMXKEmE2AgulgUX9310";
+    sha256 = "sha256-k3UezzrLwv4eL8n2qefP8ae4ekNLhcHc8PMH3+Zq92c";
   };
 
-  vendorSha256 = "sha256-PJ6TakF2yN8eB/SV5Dx164lDZDi4Hr4N2ZW8dzz8jcg";
+  vendorSha256 = "sha256-A7XGbGTbx1qr/zpfXyJqIDb3EU8IVGO6sTNg5bg/jMk";
 
   meta = with lib; {
     description = "Tool to visualize dynamic node usage within a cluster";
     homepage = "https://github.com/awslabs/eks-node-viewer";
+    changelog = "https://github.com/awslabs/eks-node-viewer/releases/tag/${version}";
     license = licenses.afl20;
     maintainers = [ maintainers.ivankovnatsky ];
   };
