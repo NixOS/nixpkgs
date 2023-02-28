9cf847bac67a38a91e2a8254ecf52d904a8bbcd5
Adam Stephens <adam@valkor.net>
2023-02-28 09:31:09 -0500

    gleam: 0.25.3 -> 0.26.2

--- pkgs/development/compilers/gleam/default.nix
+++ pkgs/development/compilers/gleam/default.nix
@@ -1,22 +1,22 @@
-{ lib, stdenv, rustPlatform, fetchFromGitHub, pkg-config, openssl, Security, libiconv }:
+{ lib, stdenv, rustPlatform, fetchFromGitHub, git, pkg-config, openssl, Security, libiconv }:
 
 rustPlatform.buildRustPackage rec {
   pname = "gleam";
-  version = "0.25.3";
+  version = "0.26.2";
 
   src = fetchFromGitHub {
     owner = "gleam-lang";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-JT9NUca+DaqxT36heaNKijIuqdnSvrYCfY2uM7wTOGo=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-NLUAKNWbKPAf81ce1OWTy/pnDcF2LfF12825DfE8AWw=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [ git pkg-config ];
 
   buildInputs = [ openssl ] ++
     lib.optionals stdenv.isDarwin [ Security libiconv ];
 
-  cargoSha256 = "sha256-YPyGCd4//yta3jy5tWB4C5yRgxNbfG+hGF5/QSch/6M=";
+  cargoSha256 = "sha256-IOZEdoQ5cqXjhgqNzOg4h5t8i3JDxfjamLFdesEy9Z8=";
 
   meta = with lib; {
     description = "A statically typed language for the Erlang VM";
