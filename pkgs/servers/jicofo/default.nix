e7f9dd159602c41344438378c94958c622078d14
R. Ryantm <ryantm-bot@ryantm.com>
2023-02-28 02:20:23 +0000

    jicofo: 1.0-940 -> 1.0-996

--- pkgs/servers/jicofo/default.nix
+++ pkgs/servers/jicofo/default.nix
@@ -2,10 +2,10 @@
 
 let
   pname = "jicofo";
-  version = "1.0-940";
+  version = "1.0-996";
   src = fetchurl {
     url = "https://download.jitsi.org/stable/${pname}_${version}-1_all.deb";
-    sha256 = "vx7aUHfKxG+tZ0sM8eWr1tTKf//bMxdKVhE5I4P4mLo=";
+    sha256 = "X0dDBuLQQGG8gicbVZW+KkmGNX4DlMbEIzM/dzjPglE=";
   };
 in
 stdenv.mkDerivation {
