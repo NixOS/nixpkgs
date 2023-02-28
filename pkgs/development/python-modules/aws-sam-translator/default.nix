be2abcd190f14f675fb8dc85302c3e18be35b490
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-28 23:02:43 +0100

    fixup! python3Packages.aws-sam-translator: 1.55.0 -> 1.59.0

--- pkgs/development/python-modules/aws-sam-translator/default.nix
+++ pkgs/development/python-modules/aws-sam-translator/default.nix
@@ -2,6 +2,7 @@
 , boto3
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , jsonschema
 , mock
 , parameterized
@@ -17,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "aws-sam-translator";
-  version = "1.59.0";
+  version = "1.58.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -26,7 +27,7 @@ buildPythonPackage rec {
     owner = "aws";
     repo = "serverless-application-model";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-8uhScBlaVkavp8mYwQvONTZoVXibTMefcvtVKM44gKY=";
+    hash = "sha256-GFHxSGcJeeGMq4e+iD1+paYBKV72tStxHJgefdZSGiI=";
   };
 
   propagatedBuildInputs = [
@@ -34,6 +35,14 @@ buildPythonPackage rec {
     jsonschema
     pydantic
     typing-extensions
+  ];
+
+  patches = [
+    (fetchpatch {
+      # relax typing-extenions dependency
+      url = "https://github.com/aws/serverless-application-model/commit/d1c26f7ad9510a238ba570d511d5807a81379d0a.patch";
+      hash = "sha256-nh6MtRgi0RrC8xLkLbU6/Ec0kYtxIG/fgjn/KLiAM0E=";
+    })
   ];
 
   postPatch = ''
@@ -60,11 +69,14 @@ buildPythonPackage rec {
 
   pytestFlagsArray = [
     # samtranslator.translator.arn_generator.NoRegionFound: AWS Region cannot be found
+    "--deselect tests/plugins/application/test_serverless_app_plugin.py::TestServerlessAppPlugin_on_before_and_on_after_transform_template::test_time_limit_exceeds_between_combined_sar_calls"
     "--deselect tests/plugins/application/test_serverless_app_plugin.py::TestServerlessAppPlugin_on_before_transform_template_translate::test_sar_success_one_app"
     "--deselect tests/plugins/application/test_serverless_app_plugin.py::TestServerlessAppPlugin_on_before_transform_template_translate::test_sar_throttling_doesnt_stop_processing"
     "--deselect tests/plugins/application/test_serverless_app_plugin.py::TestServerlessAppPlugin_on_before_transform_template_translate::test_sleep_between_sar_checks"
     "--deselect tests/plugins/application/test_serverless_app_plugin.py::TestServerlessAppPlugin_on_before_transform_template_translate::test_unexpected_sar_error_stops_processing"
-    "--deselect tests/plugins/application/test_serverless_app_plugin.py::TestServerlessAppPlugin_on_before_and_on_after_transform_template::test_time_limit_exceeds_between_combined_sar_calls"
+    "--deselect tests/model/eventsources/test_msk_event_source.py::MSKEventSource::test_get_policy_arn"
+    "--deselect tests/swagger/test_swagger.py::TestSwaggerEditor_add_lambda_integration::test_must_add_credentials_to_the_integration"
+    "--deselect tests/swagger/test_swagger.py::TestSwaggerEditor_add_lambda_integration::test_must_add_credentials_to_the_integration_overrides"
     "--deselect tests/unit/test_region_configuration.py::TestRegionConfiguration::test_is_service_supported_positive_4_ec2"
   ];
 
