3df36c5ef5340bd79932fedb12fafcf347d6626c
Sebastiaan Alvarez Rodriguez <24595187+Sebastiaan-Alvarez-Rodriguez@users.noreply.github.com>
2023-02-28 15:06:33 +0100

    Update redis-plus-plus
    Bumped version `1.35` -> `1.37`, now supporting async interface

--- pkgs/development/libraries/redis-plus-plus/default.nix
+++ pkgs/development/libraries/redis-plus-plus/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, cmake, hiredis
+{ lib, stdenv, fetchFromGitHub, cmake, hiredis, libuv
 , enableShared ? !stdenv.hostPlatform.isStatic
 , enableStatic ? stdenv.hostPlatform.isStatic
 }:
@@ -8,17 +8,17 @@ assert enableShared || enableStatic;
 
 stdenv.mkDerivation rec {
   pname = "redis-plus-plus";
-  version = "1.3.5";
+  version = "1.3.7";
 
   src = fetchFromGitHub {
     owner = "sewenew";
     repo = "redis-plus-plus";
     rev = version;
-    sha256 = "sha256-5tjadh3Ku7lrJn4tbi8TjTH6N0+QB2ER9xuO51cK/LU=";
+    sha256 = "sha256-QCNN85syxw2EGPdyTV3bL0txcHl7t2YhsKwK9lgnexY=";
   };
 
   nativeBuildInputs = [ cmake ];
-  propagatedBuildInputs = [ hiredis ];
+  propagatedBuildInputs = [ hiredis libuv ];
 
   cmakeFlags = [
     "-DREDIS_PLUS_PLUS_BUILD_TEST=OFF"
@@ -26,6 +26,8 @@ stdenv.mkDerivation rec {
     "-DREDIS_PLUS_PLUS_BUILD_SHARED=OFF"
   ] ++ lib.optionals (!enableStatic) [
     "-DREDIS_PLUS_PLUS_BUILD_STATIC=OFF"
+  ] ++ [
+    "-DREDIS_PLUS_PLUS_BUILD_ASYNC=libuv"
   ];
 
   meta = with lib; {
@@ -33,6 +35,6 @@ stdenv.mkDerivation rec {
     description = "Redis client written in C++";
     license = licenses.asl20;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ wheelsandmetal ];
+    maintainers = with maintainers; [ wheelsandmetal sebastiaan ];
   };
 }
