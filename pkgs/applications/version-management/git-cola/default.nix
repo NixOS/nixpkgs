6f1d2013c0f6df93cd0164b3cdcc24eaed5d46f8
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-02-28 16:02:06 +0000

    git-cola: run tests

--- pkgs/applications/version-management/git-cola/default.nix
+++ pkgs/applications/version-management/git-cola/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, python3Packages, gettext, git, qt5 }:
+{ stdenv, lib, fetchFromGitHub, python3Packages, gettext, git, qt5 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "git-cola";
@@ -11,11 +11,23 @@ python3Packages.buildPythonApplication rec {
     hash = "sha256-s+acQo9b+ZQ31qXBf0m8ajXYuYEQzNybmX9nw+c0DQY=";
   };
 
-  buildInputs = [ git gettext ];
-  propagatedBuildInputs = with python3Packages; [ pyqt5 qtpy send2trash ];
-  nativeBuildInputs = [ qt5.wrapQtAppsHook ];
+  # TODO: remove in the next release since upstream removed pytest-flake8
+  # https://github.com/git-cola/git-cola/commit/6c5c5c6c888ee1a095fc1ca5521af9a03b833205
+  postPatch = ''
+    substituteInPlace pytest.ini \
+      --replace "--flake8" ""
+  '';
 
-  doCheck = false;
+  propagatedBuildInputs = with python3Packages; [ git pyqt5 qtpy send2trash ];
+  nativeBuildInputs = [ gettext qt5.wrapQtAppsHook ];
+  nativeCheckInputs = with python3Packages; [ git pytestCheckHook ];
+
+  disabledTestPaths = [
+    "qtpy/"
+    "contrib/win32"
+  ] ++ lib.optionals stdenv.isDarwin [
+    "cola/inotify.py"
+  ];
 
   preFixup = ''
     makeWrapperArgs+=("''${qtWrapperArgs[@]}")
