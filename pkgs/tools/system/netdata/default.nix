7fb6f1208acd20f6ba672935b4493e8ef558d58d
Izorkin <izorkin@elven.pw>
2023-02-28 10:07:32 +0300

    netdata: fix start cgroup-network-helper.sh

--- pkgs/tools/system/netdata/default.nix
+++ pkgs/tools/system/netdata/default.nix
@@ -107,6 +107,7 @@ stdenv.mkDerivation rec {
 
   postFixup = ''
     wrapProgram $out/bin/netdata-claim.sh --prefix PATH : ${lib.makeBinPath [ openssl ]}
+    wrapProgram $out/libexec/netdata/plugins.d/cgroup-network-helper.sh --prefix PATH : ${lib.makeBinPath [ bash ]}
   '';
 
   enableParallelBuild = true;
