47edf9cac8bcb9e9ea4fd5fb5d5b566cc083914d
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-02-28 01:42:37 +0100

    python310Packages.sphinx-autoapi: Rename from sphinxcontrib-autoapi

--- pkgs/top-level/python-aliases.nix
+++ pkgs/top-level/python-aliases.nix
@@ -257,6 +257,7 @@ mapAliases ({
   SPARQLWrapper = sparqlwrapper;
   sphinx-jquery = sphinxcontrib-jquery; # added 2023-02-24
   sphinx_rtd_theme = sphinx-rtd-theme; # added 2022-08-03
+  sphinxcontrib-autoapi = sphinx-autoapi; # added 2023-02=28
   sphinxcontrib_plantuml = sphinxcontrib-plantuml; # added 2021-08-02
   sqlalchemy_migrate = sqlalchemy-migrate; # added 2021-10-28
   SQLAlchemy-ImageAttach = throw "sqlalchemy-imageattach has been removed as it is incompatible with sqlalchemy 1.4 and unmaintained"; # added 2022-04-23
