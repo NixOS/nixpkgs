2995a890b54758eea7fb5e78ea8ad6a219fe7a6e
Robert Schütz <nix@dotlambda.de>
2023-02-28 19:10:57 -0800

    bitwarden-cli: build from source

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3356,7 +3356,7 @@ with pkgs;
 
   bitwarden = callPackage ../tools/security/bitwarden { };
 
-  inherit (nodePackages) bitwarden-cli;
+  bitwarden-cli = callPackage ../tools/security/bitwarden/cli.nix { };
 
   bitwarden-menu = python3Packages.callPackage ../applications/misc/bitwarden-menu { };
 
