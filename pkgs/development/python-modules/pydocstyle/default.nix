43c51bfa6efe4050b4505d2816a5419ab27303d0
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-03-01 03:13:15 +0100

    fixup! python3Packages.pydocstyle: 6.1.1 -> 6.3.0

--- pkgs/development/python-modules/pydocstyle/default.nix
+++ pkgs/development/python-modules/pydocstyle/default.nix
@@ -1,32 +1,44 @@
 { lib
 , buildPythonPackage
-, isPy3k
+, pythonOlder
 , fetchFromGitHub
+, poetry-core
 , snowballstemmer
+, tomli
 , pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "pydocstyle";
   version = "6.3.0";
-  disabled = !isPy3k;
+  format = "pyproject";
 
-  format = "setuptools";
+  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "PyCQA";
-    repo = pname;
+    repo = "pydocstyle";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-MjRrnWu18f75OjsYIlOLJK437X3eXnlW8WkkX7vdS6k=";
+    hash = "sha256-MjRrnWu18f75OjsYIlOLJK437X3eXnlW8WkkX7vdS6k=";
   };
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
 
   propagatedBuildInputs = [
     snowballstemmer
+  ] ++ lib.optionals (pythonOlder "3.11") [
+    tomli
+  ];
+
+  passthru.optional-dependencies.toml = [
+    tomli
   ];
 
   nativeCheckInputs = [
     pytestCheckHook
-  ];
+  ] ++ passthru.optional-dependencies.toml;
 
   disabledTestPaths = [
     "src/tests/test_integration.py" # runs pip install
@@ -35,6 +47,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python docstring style checker";
     homepage = "https://github.com/PyCQA/pydocstyle";
+    changelog = "https://github.com/PyCQA/pydocstyle/blob/${version}/docs/release_notes.rst";
     license = licenses.mit;
     maintainers = with maintainers; [ dzabraev ];
   };
