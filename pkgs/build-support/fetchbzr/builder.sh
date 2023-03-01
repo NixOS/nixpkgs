b82b2b5303a25bb45432c4fe2f00961bba5e8c56
Mario Rodas <marsam@users.noreply.github.com>
2023-03-01 04:20:00 +0000

    fetchbzr: set cache directory to tmpdir
    Breezy >3.3 writes the connections information to a cache directory,
    which fails on sandbox.

--- pkgs/build-support/fetchbzr/builder.sh
+++ pkgs/build-support/fetchbzr/builder.sh
@@ -5,4 +5,4 @@ echo "exporting \`$url' (revision $rev) into \`$out'"
 
 # Perform a lightweight checkout so that we don't end up importing
 # all the repository's history.
-BZR_LOG=/dev/null bzr -Ossl.cert_reqs=none export -r "$rev" --format=dir "$out" "$url"
+XDG_CACHE_HOME="$TMPDIR" BRZ_LOG=/dev/null bzr -Ossl.cert_reqs=none export -r "$rev" --format=dir "$out" "$url"
