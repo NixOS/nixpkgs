fe6b447c430b23f508af608aff740e0c644297d7
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-03-01 04:04:20 +0000

    ocamlPackages.elina: fixup build by using older make

--- pkgs/development/ocaml-modules/elina/default.nix
+++ pkgs/development/ocaml-modules/elina/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchurl, perl, gmp, mpfr, ocaml, findlib, camlidl, apron }:
+{ stdenv, lib, fetchurl, gnumake42, perl, gmp, mpfr, ocaml, findlib, camlidl, apron }:
 
 stdenv.mkDerivation rec {
   version = "1.1";
@@ -8,7 +8,8 @@ stdenv.mkDerivation rec {
     sha256 = "1nymykskq1yx87y4xl6hl9i4q6kv0qaq25rniqgl1bfn883p1ysc";
   };
 
-  nativeBuildInputs = [ perl ocaml findlib camlidl ];
+  # fails with make 4.4
+  nativeBuildInputs = [ gnumake42 perl ocaml findlib camlidl ];
 
   propagatedBuildInputs = [ apron gmp mpfr ];
 
