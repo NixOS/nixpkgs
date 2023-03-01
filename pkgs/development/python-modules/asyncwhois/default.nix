c457b8f256043aba5dacc4aa844fc03c3fc581a5
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-01 08:40:54 +0100

    python310Packages.asyncwhois: 1.0.2 -> 1.0.3
    Diff: https://github.com/pogzyb/asyncwhois/compare/refs/tags/v1.0.2...v1.0.3
    
    Changelog: https://github.com/pogzyb/asyncwhois/releases/tag/v1.0.3

--- pkgs/development/python-modules/asyncwhois/default.nix
+++ pkgs/development/python-modules/asyncwhois/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "asyncwhois";
-  version = "1.0.2";
+  version = "1.0.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "pogzyb";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-MYK09kszv7CTvZjdA9YQFfhlJ/A5d/aebLRaiMlnuB0=";
+    hash = "sha256-h2RAUedcVTlX/DmaXewSQfw9gb0rE8NG9hdNe+exqqk=";
   };
 
   propagatedBuildInputs = [
