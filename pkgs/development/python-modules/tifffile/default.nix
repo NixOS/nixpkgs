288eaa68c3e27716ecf752d9f65634520fd42c16
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-01 10:25:24 +0100

    python310Packages.tifffile: update disabled

--- pkgs/development/python-modules/tifffile/default.nix
+++ pkgs/development/python-modules/tifffile/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   version = "2023.2.3";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
