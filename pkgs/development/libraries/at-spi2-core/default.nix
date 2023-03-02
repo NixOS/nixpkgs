NixOS/nixpkgs

commit e31262c3eef88eee80d950f0fd604f4ecaa634bb

at-spi2-core: make introspection unconditional
We don't need to make it conditional anymore, this added line broke the gtk3 build.
With this the (cross) gtk3 build is working again.

pkgs/development/libraries/at-spi2-core/default.nix
@@ -58,7 +58,7 @@ stdenv.mkDerivation rec {
   doCheck = false;
 
   mesonFlags = [
-    "-Dintrospection=${if stdenv.buildPlatform == stdenv.hostPlatform then "yes" else "no"}"
+    "-Dintrospection=yes"
     # Provide dbus-daemon fallback when it is not already running when
     # at-spi2-bus-launcher is executed. This allows us to avoid
     # including the entire dbus closure in libraries linked with

commit 8ea1ca271b3df191f8e0eef50ba03561fbca1f0d

at-spi2-core: re-disable introspection if no emulator
e.g. when cross compiling from Linux to FreeBSD.

Fixes: e31262c3eef ("at-spi2-core: make introspection unconditional")

pkgs/development/libraries/at-spi2-core/default.nix
@@ -5,6 +5,8 @@
 , ninja
 , pkg-config
 , gobject-introspection
+, buildPackages
+, withIntrospection ? stdenv.hostPlatform.emulatorAvailable buildPackages
 , gsettings-desktop-schemas
 , makeWrapper
 , dbus
@@ -30,11 +32,13 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [
+    glib
     meson
     ninja
     pkg-config
-    gobject-introspection
     makeWrapper
+  ] ++ lib.optionals withIntrospection [
+    gobject-introspection
   ];
 
   buildInputs = [
@@ -58,7 +62,6 @@ stdenv.mkDerivation rec {
   doCheck = false;
 
   mesonFlags = [
-    "-Dintrospection=yes"
     # Provide dbus-daemon fallback when it is not already running when
     # at-spi2-bus-launcher is executed. This allows us to avoid
     # including the entire dbus closure in libraries linked with