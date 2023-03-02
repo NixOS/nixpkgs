d8fe6839b0dc3683edea9c4c2a6630dd5d2507f3
Robert Schütz <nix@dotlambda.de>
2023-03-02 14:42:13 +0100

    python310Packages.faust-cchardet: init at 2.1.18

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3327,6 +3327,8 @@ self: super: with self; {
 
   fasttext-predict = callPackage ../development/python-modules/fasttext-predict { };
 
+  faust-cchardet = callPackage ../development/python-modules/faust-cchardet { };
+
   favicon = callPackage ../development/python-modules/favicon { };
 
   fb-re2 = callPackage ../development/python-modules/fb-re2 { };
