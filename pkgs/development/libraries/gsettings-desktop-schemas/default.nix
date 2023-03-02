NixOS/nixpkgs

commit 24723644acd2d50aa30cd4d6829cc2d8949d5824

gsettings-desktop-schemas: build gir when cross

pkgs/development/libraries/gsettings-desktop-schemas/default.nix
@@ -30,10 +30,6 @@ stdenv.mkDerivation rec {
     gobject-introspection
   ];
 
-  mesonFlags = [
-    "-Dintrospection=${lib.boolToString (stdenv.buildPlatform == stdenv.hostPlatform)}"
-  ];
-
   postPatch = ''
     chmod +x build-aux/meson/post-install.py
     patchShebangs build-aux/meson/post-install.py

commit 7c643c33121fd54162f7e81c148b776e903c05d2

gsettings-desktop-schemas: re-disable introspection if no emulator
e.g. when cross compiling from Linux to FreeBSD.

Fixes: 24723644acd ("gsettings-desktop-schemas: build gir when cross")

pkgs/development/libraries/gsettings-desktop-schemas/default.nix
@@ -3,6 +3,8 @@
 , pkg-config
 , glib
 , gobject-introspection
+, buildPackages
+, withIntrospection ? stdenv.hostPlatform.emulatorAvailable buildPackages
 , meson
 , ninja
 , python3
@@ -27,7 +29,12 @@ stdenv.mkDerivation rec {
     ninja
     pkg-config
     python3
+  ] ++ lib.optionals withIntrospection [
     gobject-introspection
+  ];
+
+  mesonFlags = [
+    (lib.mesonBool "introspection" withIntrospection)
   ];
 
   postPatch = ''