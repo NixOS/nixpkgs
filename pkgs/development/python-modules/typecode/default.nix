7c2f6c6f4a8d124d8242ecc14a5acb31f7766056
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-02 12:31:37 +0100

    python310Packages.typecode: fix invalid version specifier

--- pkgs/development/python-modules/typecode/default.nix
+++ pkgs/development/python-modules/typecode/default.nix
@@ -25,6 +25,12 @@ buildPythonPackage rec {
     hash = "sha256-pRGLU/xzQQqDZMIsrq1Fy7VgGIpFjnHtpmO+yL7t4g8=";
   };
 
+  postPatch = ''
+    # PEP440 support was removed in newer setuptools, https://github.com/nexB/typecode/pull/31
+    substituteInPlace setup.cfg \
+      --replace ">=3.6.*" ">=3.6"
+  '';
+
   dontConfigure = true;
 
   nativeBuildInputs = [
