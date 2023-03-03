a231e6351e9353b99c24e4255b1b39db725a3f38
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-03 21:52:12 +0100

    python310Packages.pysnmplib: 5.0.21 -> 5.0
    Diff: https://github.com/pysnmp/pysnmp/compare/refs/tags/v5.0.21...v5.0
    
    Changelog: https://github.com/pysnmp/pysnmp/releases/tag/v5.0

--- pkgs/development/python-modules/pysnmplib/default.nix
+++ pkgs/development/python-modules/pysnmplib/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "pysnmplib";
-  version = "5.0.21";
+  version = "5.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "pysnmp";
     repo = "pysnmp";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ejRIlRxDbdZgy7wHilSzunRTGn1uO6YLLmLxNrrHKss=";
+    hash = "sha256-+is/pa4ONfG6QX/lWdIPMrSJ7r/w2gIkUihXcSrmWZM=";
   };
 
   nativeBuildInputs = [
