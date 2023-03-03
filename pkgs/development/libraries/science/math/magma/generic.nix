dd2b27692e8a32316d263b938bddfa515eb2775a
Someone Serge <sergei.kozlukov@aalto.fi>
2023-03-03 03:23:40 +0200

    magma: explain `cudaSupport ? true`

--- pkgs/development/libraries/science/math/magma/generic.nix
+++ pkgs/development/libraries/science/math/magma/generic.nix
@@ -8,6 +8,11 @@
 { blas
 , cmake
 , cudaPackages
+  # FIXME: cuda being unfree means ofborg won't eval "magma".
+  # respecting config.cudaSupport -> false by default
+  # -> ofborg eval -> throws "no GPU targets specified".
+  # Probably should delete everything but "magma-cuda" and "magma-hip"
+  # from all-packages.nix
 , cudaSupport ? true
 , fetchurl
 , gfortran
