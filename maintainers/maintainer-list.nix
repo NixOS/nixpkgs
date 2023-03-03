7e0ad3d95e732a18721751118c20a22acddba054
MortenSlingsby <morten.slingsby@eviny.no>
2023-03-03 15:35:40 +0100

    maintainers: Add mslingsby

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10572,6 +10572,12 @@
     githubId = 133448;
     name = "Mikołaj Siedlarek";
   };
+  mslingsby = {
+    email = "morten.slingsby@eviny.no";
+    github = "MortenSlingsby";
+    githubId = 111859550;
+    name = "Morten Slingsby";
+  };
   msm = {
     email = "msm@tailcall.net";
     github = "msm-code";
