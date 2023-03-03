a01aa125cce4b314836ac6af1e213574b4003eb5
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-03 17:35:10 +0100

    python310Packages.py_stringmatching: update meta

--- pkgs/development/python-modules/py_stringmatching/default.nix
+++ pkgs/development/python-modules/py_stringmatching/default.nix
@@ -20,8 +20,9 @@ buildPythonPackage rec {
   propagatedBuildInputs = [ numpy six ];
 
   meta = with lib; {
-    description = "A Python string matching library including string tokenizers and string similarity measures";
-    homepage =  "https://sites.google.com/site/anhaidgroup/projects/magellan/py_stringmatching";
+    description = "Python string matching library including string tokenizers and string similarity measures";
+    homepage = "https://github.com/anhaidgroup/py_stringmatching";
+    changelog = "https://github.com/anhaidgroup/py_stringmatching/blob/v${version}/CHANGES.txt";
     license = licenses.bsd3;
     maintainers = with maintainers; [ ixxie ];
   };
