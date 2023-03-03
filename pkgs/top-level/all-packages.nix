ac62a206b79eee171cfcf85d209ae4e35694b70f
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-03-04 00:25:59 +0200

    root: remove -lgcc from NIX_LDFLAGS

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37522,8 +37522,6 @@ with pkgs;
   root = callPackage ../applications/science/misc/root {
     python = python3;
     inherit (darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
-    # https://github.com/NixOS/nixpkgs/issues/201254
-    stdenv = if stdenv.isLinux && stdenv.isAarch64 && stdenv.cc.isGNU then gcc12Stdenv else stdenv;
   };
 
   root5 = lowPrio (callPackage ../applications/science/misc/root/5.nix {
