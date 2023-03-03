NixOS/nixpkgs

commit fede9624c8968f0daa7eb1de31f40478a4ffaff3

viber: fix no internet error

pkgs/applications/networking/instant-messengers/viber/default.nix
@@ -1,7 +1,7 @@
 {fetchurl, lib, stdenv, dpkg, makeWrapper,
  alsa-lib, cups, curl, dbus, expat, fontconfig, freetype, glib, gst_all_1,
  harfbuzz, libcap, libGL, libGLU, libpulseaudio, libxkbcommon, libxml2, libxslt,
- nspr, nss, openssl, systemd, wayland, xorg, zlib, ...
+ nspr, nss, openssl_1_1, systemd, wayland, xorg, zlib, ...
 }:
 
 stdenv.mkDerivation {

commit 8f386acc47fa8318e29bbbfe22b2c2ff20aab6e9

viber: fix build with openssl
broken by
https://github.com/NixOS/nixpkgs/commit/fede9624c8968f0daa7eb1de31f40478a4ffaff3

pkgs/applications/networking/instant-messengers/viber/default.nix
@@ -39,7 +39,7 @@ stdenv.mkDerivation {
       libxslt
       nspr
       nss
-      openssl
+      openssl_1_1
       stdenv.cc.cc
       systemd
       wayland