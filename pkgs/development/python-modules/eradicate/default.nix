e1806091d7374613c403414ae0debb58e6a7c814
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-04 18:59:44 +0100

    python310Packages.eradicate: 2.1.0 -> 2.2.0
    Diff: https://github.com/wemake-services/eradicate/compare/refs/tags/2.1.0...2.2.0
    
    Changelog: https://github.com/wemake-services/eradicate/releases/tag/2.2.0

--- pkgs/development/python-modules/eradicate/default.nix
+++ pkgs/development/python-modules/eradicate/default.nix
@@ -1,16 +1,35 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
+, pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "eradicate";
-  version = "2.1.0";
+  version = "2.2.0";
+  format = "setuptools";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-qsc4SrJbG/IcTAEt6bS/g5iUWhTJjJEVRbLqUKtVgBQ=";
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "wemake-services";
+    repo = pname;
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-pVjvzW3UVeLMLLYcU0SIE19GEHFmouoA/JKSweTZSGo=";
   };
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "eradicate"
+  ];
+
+  pytestFlagsArray = [
+    "test_eradicate.py"
+  ];
 
   meta = with lib; {
     description = "Library to remove commented-out code from Python files";
