1b88a5bc22dc96281226580fa87170fa3f485aa5
Sefa Eyeoglu <contact@scrumplex.net>
2023-03-04 19:31:27 +0100

    openvr: init at 1.23.8
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22810,6 +22810,8 @@ with pkgs;
 
   openvdb = callPackage ../development/libraries/openvdb {};
 
+  openvr = callPackage ../development/libraries/openvr { };
+
   inherit (callPackages ../development/libraries/libressl { })
     libressl_3_4
     libressl_3_5
