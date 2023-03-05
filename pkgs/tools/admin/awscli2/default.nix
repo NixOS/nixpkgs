783dd8d4fe4702b6d1cc0e5d539337eab29237d3
Anthony Roussel <anthony@roussel.dev>
2023-03-05 19:29:58 +0100

    awscli2: 2.10.4 -> 2.11.0

--- pkgs/tools/admin/awscli2/default.nix
+++ pkgs/tools/admin/awscli2/default.nix
@@ -25,14 +25,14 @@ let
 in
 with py.pkgs; buildPythonApplication rec {
   pname = "awscli2";
-  version = "2.10.4"; # N.B: if you change this, check if overrides are still up-to-date
+  version = "2.11.0"; # N.B: if you change this, check if overrides are still up-to-date
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = "aws-cli";
     rev = version;
-    hash = "sha256-NXsXAyxlhW63vWP7oBh2hh0G+KjmGAZVRSdRUac+lgk=";
+    hash = "sha256-i5ZGq51ZKEkyt6+d/Q4NzGS/zEaoqNql2IPZ2xlTV6k=";
   };
 
   postPatch = ''
