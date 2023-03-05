c97391e6dd485854f01c0d2f9de5042297ca24be
Jörg Thalheim <joerg@thalheim.io>
2023-03-05 13:29:04 +0100

    python3.pkgs.klein: 2022-06-26 -> 2022-12-05

--- pkgs/development/python-modules/klein/default.nix
+++ pkgs/development/python-modules/klein/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "klein";
-  version = "unstable-2022-06-26";
+  version = "unstable-2022-12-05";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -26,8 +26,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "twisted";
     repo = pname;
-    rev = "d8c2b92a3c77aa64c596696fb6f07172ecf94a74";
-    hash = "sha256-RDZqavkteUbARV78OctZtLIrE4RoYDVAanjwE5i/ZeM=";
+    rev = "022a25adc2056aa9c09ad352c42d381a04f7401a";
+    hash = "sha256-Vyak26f9MX2v8hLUzKcVqXWoVRUP6UlgF5gYlwA0Yxs=";
   };
 
   propagatedBuildInputs = [
@@ -47,8 +47,15 @@ buildPythonPackage rec {
     treq
   ];
 
+  # twisted.trial command hangs also all tests succceeds.
+  # so we just stop after the testsuite finishes..
   checkPhase = ''
-    ${python.interpreter} -m twisted.trial -j $NIX_BUILD_CORES klein
+    while read line; do
+        echo "$line"
+        if echo "$line" | grep -Eq 'PASSED \(successes=[0-9]+\)'; then
+            break
+        fi
+    done < <(${python.interpreter} -m twisted.trial -j $NIX_BUILD_CORES klein)
   '';
 
   pythonImportsCheck = [
