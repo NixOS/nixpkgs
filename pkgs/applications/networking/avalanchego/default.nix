8e3984de4c7334e0d6077e834b7e0212c7e9d585
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-05 19:59:04 +0000

    avalanchego: 1.9.9 -> 1.9.10

--- pkgs/applications/networking/avalanchego/default.nix
+++ pkgs/applications/networking/avalanchego/default.nix
@@ -8,16 +8,16 @@
 
 buildGoModule rec {
   pname = "avalanchego";
-  version = "1.9.9";
+  version = "1.9.10";
 
   src = fetchFromGitHub {
     owner = "ava-labs";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-xaFSqcVVRXy1JYOWGnzoPKLnnxk9NgZ3FIZR3qydOLM=";
+    hash = "sha256-lzobtncxL/7Lf/+kVzIdyP0dTzerMJRAoT7OBFdeEgc=";
   };
 
-  vendorHash = "sha256-ZCIy1cQ62MNLor39Pi0/nU6KsbgG1z7SowMpj9tXLT8=";
+  vendorHash = "sha256-IxPJBpOSqcramegQ+M/U9p6ls6dStOi0OUdddDj11d0=";
   # go mod vendor has a bug, see: https://github.com/golang/go/issues/57529
   proxyVendor = true;
 
