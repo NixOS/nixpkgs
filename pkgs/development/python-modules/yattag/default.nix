a22c1ad4658695d83a0c2511e86dc4703abf24e7
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-06 16:29:17 +0100

    python310Packages.yattag: disable on unsupported Python releases

--- pkgs/development/python-modules/yattag/default.nix
+++ pkgs/development/python-modules/yattag/default.nix
@@ -1,12 +1,19 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "yattag";
   version = "1.15.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-lg+lS+EinZb0MXgTPgsZXAAzkf3Ens22tptzdNtr5BY=";
+    hash = "sha256-lg+lS+EinZb0MXgTPgsZXAAzkf3Ens22tptzdNtr5BY=";
   };
 
   meta = with lib; {
