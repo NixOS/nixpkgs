9ee81c14710817c45b0827d35015e930864d69fe
Maximilian Bosch <maximilian@mbosch.me>
2023-03-06 12:32:09 +0100

    element-desktop: bump electron to v23

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3943,7 +3943,7 @@ with pkgs;
 
   element-desktop = callPackage ../applications/networking/instant-messengers/element/element-desktop.nix {
     inherit (darwin.apple_sdk.frameworks) Security AppKit CoreServices;
-    electron = electron_20;
+    electron = electron_23;
   };
   element-desktop-wayland = writeScriptBin "element-desktop" ''
     #!/bin/sh
