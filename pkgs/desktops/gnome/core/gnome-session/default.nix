f4f8d9e5146d9d13348ddc2da46862c58e60908e
Jan Tojnar <jtojnar@gmail.com>
2023-03-06 15:37:03 +0000

    gnome.gnome-session: 43.0 → 44.rc
    https://gitlab.gnome.org/GNOME/gnome-session/-/compare/43.0...44.rc
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-session/default.nix
+++ pkgs/desktops/gnome/core/gnome-session/default.nix
@@ -30,13 +30,13 @@
 stdenv.mkDerivation rec {
   pname = "gnome-session";
   # Also bump ./ctl.nix when bumping major version.
-  version = "43.0";
+  version = "44.rc";
 
   outputs = [ "out" "sessions" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-session/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "P7mUlQE4XIwUYY548XjZUt+YrYyRCA9MXhVoxzk64fI=";
+    sha256 = "A2WZUL53yX2O/92oOJJqX/3H0F/fmBNvwOl4djTnGkE=";
   };
 
   patches = [
