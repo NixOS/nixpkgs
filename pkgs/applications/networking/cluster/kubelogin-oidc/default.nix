5f60ed186f842d4818de7a6615ad64dbc1c56294
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-06 18:16:22 +0000

    kubelogin-oidc: 1.26.0 -> 1.27.0

--- pkgs/applications/networking/cluster/kubelogin-oidc/default.nix
+++ pkgs/applications/networking/cluster/kubelogin-oidc/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "kubelogin";
-  version = "1.26.0";
+  version = "1.27.0";
 
   src = fetchFromGitHub {
     owner = "int128";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-A8J381KNhQbWZ+68P8+1xj/lEEc/+YiJ80LqRQatdFQ=";
+    sha256 = "sha256-oBgth4lAQP4UrFIk/AErlfyyCgPrugs5wQJDFxqGum0=";
   };
 
   subPackages = ["."];
 
-  vendorSha256 = "sha256-V+O3yFxGJTcFETD2qYOurQUbME5NvRNQTr43OkxXFFE=";
+  vendorHash = "sha256-IJCbh1ryyk0r72SrVEiI7K5nIFf1+UGjTkXaNKpGsmo=";
 
   # Rename the binary instead of symlinking to avoid conflict with the
   # Azure version of kubelogin
