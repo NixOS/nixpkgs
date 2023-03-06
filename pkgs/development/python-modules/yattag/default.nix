c37be28c5ce35a9ce27f282aad7b3ee5b00dd7af
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-06 16:27:35 +0100

    python310Packages.yattag: update meta

--- pkgs/development/python-modules/yattag/default.nix
+++ pkgs/development/python-modules/yattag/default.nix
@@ -10,8 +10,9 @@ buildPythonPackage rec {
   };
 
   meta = with lib; {
-    description = "Generate HTML or XML in a pythonic way. Pure python alternative to web template engines. Can fill HTML forms with default values and error messages.";
-    license = [ licenses.lgpl21 ];
+    description = "Library to generate HTML or XML";
     homepage = "https://www.yattag.org/";
+    license = licenses.lgpl21Only;
+    maintainers = with maintainers; [ ];
   };
 }
