fce04513bdbd1befff2fb9d79606472ebc6818d5
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-06 15:14:15 +0000

    python310Packages.yattag: 1.15.0 -> 1.15.1

--- pkgs/development/python-modules/yattag/default.nix
+++ pkgs/development/python-modules/yattag/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "yattag";
-  version = "1.15.0";
+  version = "1.15.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-ap/z/TMKYCh4BOpzcKhBZrYk6/S3rZ7XiLfFd3m2UmM=";
+    sha256 = "sha256-lg+lS+EinZb0MXgTPgsZXAAzkf3Ens22tptzdNtr5BY=";
   };
 
   meta = with lib; {
