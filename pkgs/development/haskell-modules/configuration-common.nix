c54e50710205e56d0a5ccd60ed68f84398b54b8e
Aaron VonderHaar <gruen0aermel@gmail.com>
2023-03-05 17:08:38 -0800

    taskell: remove doJailbreak
    taskell >= 1.11.0 no longer requires jailbreak

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1780,10 +1780,7 @@ self: super: {
   # https://github.com/serokell/haskell-crypto/issues/25
   crypto-sodium = dontCheck super.crypto-sodium;
 
-  # Too strict version bounds on a bunch of libraries:
-  # https://github.com/smallhadroncollider/taskell/issues/100
-  # May be possible to remove at the next release (1.11.0)
-  taskell = (doJailbreak super.taskell).override {
+  taskell = super.taskell.override {
     # Does not support brick >= 1.0
     # https://github.com/smallhadroncollider/taskell/issues/125
     brick = self.brick_0_70_1;
