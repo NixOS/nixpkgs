d7b83599bf83431513a048f9ee90e2bd4896eb6f
Jan Tojnar <jtojnar@gmail.com>
2023-03-06 15:37:21 +0000

    gnome.nautilus: 44.beta → 44.rc
    https://gitlab.gnome.org/GNOME/nautilus/-/compare/44.beta...44.rc
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/nautilus/default.nix
+++ pkgs/desktops/gnome/core/nautilus/default.nix
@@ -38,13 +38,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nautilus";
-  version = "44.beta";
+  version = "44.rc";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "TEFVqfkyLjXuzUTPW1Ja/WSFlpmHb5VJELuW0bvKXlw=";
+    sha256 = "+M5JDZq6REfP+nhrLOwYUgQC6kIZa5dmo5FJOdo0wQ4=";
   };
 
   patches = [
