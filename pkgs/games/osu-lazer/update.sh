2b37a4822c57187c5fe50bc7480108571da7be7a
StepBroBD <Hi@StepBroBD.com>
2023-03-06 22:57:23 -0700

    osu-lazer-bin: adding support for darwin systems
    * osu-lazer-bin: added darwin binary distribution for osu-lazer
    
    * osu-lazer-bin: sha256 and metadata changes
    
    * maintainers: add stepbrobd
    
    * osu-lazer-bin: fixed sha256 for x86_64-darwin
    
    * osu-lazer-bin: adding support for darwin systems (delan/nixpkgs#2)
    
    * osu-lazer-bin: follows formatting guideline provided by #219954
    
    * osu-lazer-bin: fixed sha256 for all platforms using new update-bin.sh
    
    * osu-lazer-bin: added a new update script for osu-lazer-bin
    
    * osu-lazer-bin: passthru.updateScript
    
    * osu-lazer-bin: add unzip to update script

--- pkgs/games/osu-lazer/update.sh
+++ pkgs/games/osu-lazer/update.sh
@@ -3,35 +3,19 @@
 set -eo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-deps_file="$(realpath ./deps.nix)"
-bin_file="$(realpath ./bin.nix)"
+deps_file="$(realpath "./deps.nix")"
 
 new_version="$(curl -s "https://api.github.com/repos/ppy/osu/releases?per_page=1" | jq -r '.[0].name')"
 old_version="$(sed -nE 's/\s*version = "(.*)".*/\1/p' ./default.nix)"
 if [[ "$new_version" == "$old_version" ]]; then
-  echo "Up to date"
-  exit 0
+    echo "Up to date"
+    exit 0
 fi
 
 cd ../../..
 
 if [[ "$1" != "--deps-only" ]]; then
-  update-source-version osu-lazer "$new_version"
-  sed -Ei.bak '/ *version = "/s/".+"/"'"$new_version"'"/' "$bin_file"
-  rm "$bin_file.bak"
-  for pair in \
-    'aarch64-darwin osu.app.Apple.Silicon.zip' \
-    'x86_64-darwin osu.app.Intel.zip' \
-    'x86_64-linux osu.AppImage' \
-  ; do
-    set -- $pair
-    nix-prefetch-url "https://github.com/ppy/osu/releases/download/$new_version/$2" | while read -r hash; do
-      nix --extra-experimental-features nix-command hash to-sri --type sha256 "$hash" | while read -r hash; do
-        sed -Ei.bak '/ *'"$1"' = "sha256-/s@".+"@"'"$hash"'"@' "$bin_file"
-        rm "$bin_file.bak"
-      done
-    done
-  done
+    update-source-version osu-lazer "$new_version"
 fi
 
 $(nix-build . -A osu-lazer.fetch-deps --no-out-link) "$deps_file"
