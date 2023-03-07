018d30a619f2f4d9c347aa27aff7d336e98e05de
Brandon Owen <brandon.owen@hotmail.com>
2023-03-07 14:17:41 +1100

    tiledb: 2.3.3 -> 2.18.0
    Co-Authored-By: Brandon Owen <brandon.owen@hotmail.com>

--- pkgs/development/libraries/tiledb/default.nix
+++ pkgs/development/libraries/tiledb/default.nix
@@ -11,30 +11,37 @@
 , openssl
 , boost
 , libpqxx
+, git
 , clang-tools
 , catch2
 , python3
 , gtest
 , doxygen
+, ninja
+, curl
+, zip
+, unzip
 , fixDarwinDylibNames
 , useAVX2 ? stdenv.hostPlatform.avx2Support
 }:
 
 stdenv.mkDerivation rec {
   pname = "tiledb";
-  version = "2.8.3";
+  version = "2.18.0";
 
   src = fetchFromGitHub {
     owner = "TileDB-Inc";
     repo = "TileDB";
     rev = version;
-    hash = "sha256-HKMVwrPnk9/mukH3mJ2LEAvA9LBF4PcgBZjbbLhO9qU=";
+    hash = "sha256-2tSk1VFY+iIgzThZ7Ruy6xDpRZDn0sG3OJgaqqgDpGw=";
+    fetchSubmodules = true;
   };
 
   # (bundled) blosc headers have a warning on some archs that it will be using
   # unaccelerated routines.
   cmakeFlags = [
     "-DTILEDB_WERROR=0"
+    "-DCMAKE_MAKE_PROGRAM=ninja"
   ] ++ lib.optional (!useAVX2) "-DCOMPILER_SUPPORTS_AVX2=FALSE";
 
   nativeBuildInputs = [
@@ -42,6 +49,10 @@ stdenv.mkDerivation rec {
     cmake
     python3
     doxygen
+    ninja
+    curl
+    zip
+    unzip
   ] ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames;
 
   nativeCheckInputs = [
@@ -59,6 +70,7 @@ stdenv.mkDerivation rec {
     openssl
     boost
     libpqxx
+    git
   ];
 
   # emulate the process of pulling catch down
