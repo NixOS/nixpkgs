a8a12fdf8538e363846af8634b2a663595a8e686
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-08 02:08:38 +0000

    erdtree: 1.2.0 -> 1.3.0

--- pkgs/tools/system/erdtree/default.nix
+++ pkgs/tools/system/erdtree/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "erdtree";
-  version = "1.2.0";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "solidiquis";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-VSIeEyMFY10aHLCRmTh0EaGa08KPqrStsPLrssDT0TE=";
+    hash = "sha256-xPMOjhp4voT2Ad30WtAyA0MT917xt3Sd++KhLHmciA0=";
   };
 
-  cargoHash = "sha256-0kDRrsiGJw4iv4CD3FRE4zIBfGO352vnp2KD1RiZafg=";
+  cargoHash = "sha256-euthKq/5X5bCxV8qAAHyMm4nPPSWCvGRCfx0a1kwr/c=";
 
   meta = with lib; {
     description = "File-tree visualizer and disk usage analyzer";
