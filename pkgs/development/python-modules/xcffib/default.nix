acc31584b6bca18f85d6d14b8d488b19195c4032
Robert Schütz <nix@dotlambda.de>
2023-03-09 12:53:18 -0800

    python310Packages.xcffib: remove superfluous dependency

--- pkgs/development/python-modules/xcffib/default.nix
+++ pkgs/development/python-modules/xcffib/default.nix
@@ -1,9 +1,9 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , xorg
 , cffi
-, six
 , pytestCheckHook
 }:
 
@@ -16,12 +16,20 @@ buildPythonPackage rec {
     hash = "sha256-8yMCFEf55zB40hu5KMSPTavq6z87N+gDxta5hzXoFIM=";
   };
 
-  patchPhase = ''
+  patches = [
+    (fetchpatch {
+      name = "remove-leftover-six-import.patch";
+      url = "https://github.com/tych0/xcffib/commit/8a488867d30464913706376ca3a9f4c98ca6c5cf.patch";
+      hash = "sha256-wEms0gC7tVqtmKMjjpH/34kdQ6HUV0h67bUGbgijlqw=";
+    })
+  ];
+
+  postPatch = ''
     # Hardcode cairo library path
     sed -e 's,ffi\.dlopen(,&"${xorg.libxcb.out}/lib/" + ,' -i xcffib/__init__.py
   '';
 
-  propagatedBuildInputs = [ cffi six ];
+  propagatedBuildInputs = [ cffi ];
 
   propagatedNativeBuildInputs = [ cffi ];
 
