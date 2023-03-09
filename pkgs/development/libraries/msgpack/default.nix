18cf5aa782ddbc4760730822617c70a76437241c
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-09 11:27:06 +0100

    msgpack: 3.3.0 -> 6.0.0
    Changelog: https://github.com/msgpack/msgpack-c/releases/tag/cpp-6.0.0

--- pkgs/development/libraries/msgpack/default.nix
+++ pkgs/development/libraries/msgpack/default.nix
@@ -1,12 +1,36 @@
-{ callPackage, fetchFromGitHub, ... } @ args:
+{ lib
+, stdenv
+, boost
+, cmake
+, fetchFromGitHub
+}:
 
-callPackage ./generic.nix (args // rec {
-  version = "3.3.0";
+stdenv.mkDerivation rec {
+  pname = "msgpack";
+  version = "6.0.0";
 
   src = fetchFromGitHub {
-    owner  = "msgpack";
-    repo   = "msgpack-c";
-    rev    = "cpp-${version}";
-    sha256 = "02dxgzxlwn8g9ca2j4m0rjvdq1k2iciy6ickj615daz5w8pcjajd";
+    owner = "msgpack";
+    repo = "msgpack-c";
+    rev = "refs/tags/cpp-${version}";
+    hash = "sha256-p0eLd0fHhsgnRomubYadumMNiC2itdePJC9B55m49LI=";
   };
-})
+
+  nativeBuildInputs = [
+    boost
+    cmake
+  ];
+
+  cmakeFlags = lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) [
+    "-DMSGPACK_BUILD_EXAMPLES=OFF"
+  ];
+
+  meta = with lib; {
+    description = "MessagePack implementation for C and C++";
+    homepage = "https://msgpack.org";
+    changelog = "https://github.com/msgpack/msgpack-c/releases/tag/cpp-${version}";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ redbaron ];
+    platforms = platforms.all;
+  };
+}
