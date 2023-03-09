e977a4f8a0e227c7c1b0c17a701efec73d1c9983
Alexander Sieg <alex@xanderio.de>
2023-03-09 11:55:48 +0100

    gitlab-elasticsearch-indexer: init at 4.3.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8135,6 +8135,8 @@ with pkgs;
 
   gitlab-workhorse = callPackage ../applications/version-management/gitlab/gitlab-workhorse { };
 
+  gitlab-elasticsearch-indexer = callPackage ../applications/version-management/gitlab/gitlab-elasticsearch-indexer { };
+
   gitleaks = callPackage ../tools/security/gitleaks { };
 
   gitaly = callPackage ../applications/version-management/gitlab/gitaly {
