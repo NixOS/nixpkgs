fb4e70b0dcba920df9075254a2a8282b0d30e335
Robert Schütz <nix@dotlambda.de>
2023-03-08 18:04:21 -0800

    python310Packages.fritzconnection: 1.11.0 -> 1.12.0
    Diff: https://github.com/kbr/fritzconnection/compare/refs/tags/1.11.0...1.12.0
    
    Changelog: https://fritzconnection.readthedocs.io/en/1.12.0/sources/version_history.html

--- pkgs/development/python-modules/fritzconnection/default.nix
+++ pkgs/development/python-modules/fritzconnection/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "fritzconnection";
-  version = "1.11.0";
+  version = "1.12.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "kbr";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-BHlOX5STnKaIOM5tiBZ9JHNQIYVZfIPDMuSw6DAJliA=";
+    hash = "sha256-1giXmmyuy+qrY6xV3yZn4kcDd6w6l8uCL4ozcZE4N00=";
   };
 
   propagatedBuildInputs = [
@@ -28,6 +28,10 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
+  preCheck = ''
+    export HOME=$TEMP
+  '';
+
   pythonImportsCheck = [
     "fritzconnection"
   ];
@@ -35,7 +39,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python module to communicate with the AVM Fritz!Box";
     homepage = "https://github.com/kbr/fritzconnection";
-    changelog = "https://fritzconnection.readthedocs.io/en/${version}/sources/changes.html";
+    changelog = "https://fritzconnection.readthedocs.io/en/${version}/sources/version_history.html";
     license = licenses.mit;
     maintainers = with maintainers; [ dotlambda valodim ];
   };
