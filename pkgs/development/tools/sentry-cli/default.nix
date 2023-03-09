6e825d6f548078e07f585d0002559361b2ebc969
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-09 06:07:03 +0000

    sentry-cli: 2.14.3 -> 2.14.4

--- pkgs/development/tools/sentry-cli/default.nix
+++ pkgs/development/tools/sentry-cli/default.nix
@@ -9,13 +9,13 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "sentry-cli";
-  version = "2.14.3";
+  version = "2.14.4";
 
   src = fetchFromGitHub {
     owner = "getsentry";
     repo = "sentry-cli";
     rev = version;
-    sha256 = "sha256-l2/gwguNQAcnBGQxVGBUecIFbp8CHrrqDrjRFxaeVe4=";
+    sha256 = "sha256-7uZPtMu6U6T5TwwnMy/xy7dKVl77Exmj1U3ue6S/rrI=";
   };
   doCheck = false;
 
@@ -25,7 +25,7 @@ rustPlatform.buildRustPackage rec {
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ Security SystemConfiguration ];
   nativeBuildInputs = [ pkg-config ];
 
-  cargoHash = "sha256-h9rkTnGFMclTTyUy39rpKiorcy3953u2nbHiydOAmqI=";
+  cargoHash = "sha256-tezeakmPtujtdr1nc2anaJAWCJ3lS2L/LsH8qa1O3tI=";
 
   meta = with lib; {
     homepage = "https://docs.sentry.io/cli/";
