c42b85cfd2633d6f07de5542f6f9dd8897fd61d2
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-10 00:41:05 +0000

    ventoy-bin-full: 1.0.88 -> 1.0.89

--- pkgs/tools/cd-dvd/ventoy-bin/default.nix
+++ pkgs/tools/cd-dvd/ventoy-bin/default.nix
@@ -51,13 +51,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "ventoy-bin";
-  version = "1.0.88";
+  version = "1.0.89";
 
   src = let
     inherit (finalAttrs) version;
   in fetchurl {
     url = "https://github.com/ventoy/Ventoy/releases/download/v${version}/ventoy-${version}-linux.tar.gz";
-    hash = "sha256-mg1dzXREIhO9LsoCEauuBR9ESGHM3RvoFN+5vHU0HDA=";
+    hash = "sha256-dPBMABzmITUenOe57BD5EmofZeXC9v6IpW7m+OhhYdA=";
   };
 
   patches = [
