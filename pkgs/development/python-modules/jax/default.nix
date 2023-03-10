NixOS/nixpkgs

commit 3bc5413529dacc3909992a3fc9516d36db500e73

python310Packages.jax: Disable tests that fail with numpy 1.24
https://hydra.nixos.org/build/211633749

pkgs/development/python-modules/jax/default.nix
@@ -69,6 +69,12 @@ buildPythonPackage rec {
   disabledTests = [
     # Exceeds tolerance when the machine is busy
     "test_custom_linear_solve_aux"
+    # UserWarning: Explicitly requested dtype <class 'numpy.float64'>
+    #  requested in astype is not available, and will be truncated to
+    # dtype float32. (With numpy 1.24)
+    "LaxBackedScipyStatsTests::testKde3"
+    "LaxBackedScipyStatsTests::testKde5"
+    "LaxBackedScipyStatsTests::testKde6"
   ] ++ lib.optionals usingMKL [
     # See
     #  * https://github.com/google/jax/issues/9705

commit 002beda20d89fbcdfc15649b634c424bcbe9852f

python310Packages.jax: Fix disabled test specifiers
Fixes: 3bc5413

pkgs/development/python-modules/jax/default.nix
@@ -72,9 +72,9 @@ buildPythonPackage rec {
     # UserWarning: Explicitly requested dtype <class 'numpy.float64'>
     #  requested in astype is not available, and will be truncated to
     # dtype float32. (With numpy 1.24)
-    "LaxBackedScipyStatsTests::testKde3"
-    "LaxBackedScipyStatsTests::testKde5"
-    "LaxBackedScipyStatsTests::testKde6"
+    "testKde3"
+    "testKde5"
+    "testKde6"
   ] ++ lib.optionals usingMKL [
     # See
     #  * https://github.com/google/jax/issues/9705