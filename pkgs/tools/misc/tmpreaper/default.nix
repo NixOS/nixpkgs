abf710e17bbd8637f9e32393db1ac92a3cf6d92f
mar-jac <55053226+mar-jac@users.noreply.github.com>
2023-03-11 00:38:16 -0500

    Update default.nix

--- pkgs/tools/misc/tmpreaper/default.nix
+++ pkgs/tools/misc/tmpreaper/default.nix
@@ -1,29 +1,30 @@
-{ stdenv, fetchurl, autoreconfHook, e2fsprogs }:
+{ stdenv, fetchurl, autoconf, automake }:
 
 stdenv.mkDerivation rec {
   name = "tmpreaper-${version}";
   version = "1.6.17";
 
   src = fetchurl {
-    url = "https://deb.debian.org/debian/pool/main/t/tmpreaper/tmpreaper_${version}.orig.tar.gz";
-    sha256 = "1ca94d156eb68160ec9b6ed8b97d70fbee996de21437f0cf7d0c3b46709fecbc";
+    url = "https://deb.debian.org/debian/pool/main/t/tmpreaper/tmpreaper_${version}.tar.gz";
+    sha256 = if stdenv.hostPlatform.system == "x86_64-darwin" then {
+      ventura = "714d2ff483bc2027650c7fd229d3244fae4213e7d1a26a73e14e75f4fa1c61b2";
+      arm64_ventura = "6f9987a8435e18ce63b2214cd6bedbb56902c09c8cf88614c0dfab73d418c003";
+      default = "1ca94d156eb68160ec9b6ed8b97d70fbee996de21437f0cf7d0c3b46709fecbc";
+    } else {
+      default = "1ca94d156eb68160ec9b6ed8b97d70fbee996de21437f0cf7d0c3b46709fecbc";
+    };
+
+    sha256 = if stdenv.hostPlatform.system == "x86_64-darwin" then
+      if stdenv.hostPlatform.cpu == "x86_64" then sha256.default
+      else sha256.arm64_ventura
+    else sha256.default;
   };
 
-  dependsBuild = [ autoreconfHook ];
-  dependsRun = [ e2fsprogs ];
+  buildInputs = [ autoconf automake ];
 
-  buildInputs = [ autoreconfHook ];
-
-  preConfigure = ''
-    autoreconf -vfi
-  '';
-
-  doCheck = true;
-
-  meta = with stdenv.lib; {
-    description = "Cleans up files in directories based on their age and/or size.";
-    homepage = https://packages.debian.org/stable/tmpreaper;
-    license = licenses.gpl3;
-    maintainers = [ maintainers.joachifm ];
+  meta = {
+    description = "Clean up files in directories based on their age";
+    license = "GPL-2.0-only";
+    maintainers = with stdenv.lib.maintainers; [ zimbatm ];
   };
 }
