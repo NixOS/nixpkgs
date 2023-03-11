3269871ab02ccb7fd9905465cf2e447f0da13b7a
Adam Joseph <adam@westernsemico.com>
2023-03-10 20:59:53 -0800

    qt5.qtwayland: fix cross compilation

--- pkgs/development/libraries/qt-5/modules/qtwayland.nix
+++ pkgs/development/libraries/qt-5/modules/qtwayland.nix
@@ -1,11 +1,17 @@
-{ qtModule, qtbase, qtquickcontrols, wayland, pkg-config }:
+{ lib
+, stdenv
+, qtModule, qtbase, qtquickcontrols, wayland, pkg-config, fetchpatch
+, buildPackages }:
 
 qtModule {
   pname = "qtwayland";
   qtInputs = [ qtbase qtquickcontrols ];
   buildInputs = [ wayland ];
-  nativeBuildInputs = [ pkg-config ];
-  outputs = [ "out" "dev" "bin" ];
+  nativeBuildInputs = [
+    pkg-config
+    buildPackages.wayland   # for wayland-scanner
+  ];
+  outputs = [ "out" "dev" ] ++ lib.optionals (stdenv.buildPlatform.canExecute stdenv.hostPlatform) [ "bin" ];
   patches = [
     # NixOS-specific, ensure that app_id is correctly determined for
     # wrapped executables from `wrapQtAppsHook` (see comment in patch for further
