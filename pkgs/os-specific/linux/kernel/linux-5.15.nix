745a43039007b866fa0737e283d1ed3f34cce591
Maximilian Bosch <maximilian@mbosch.me>
2023-03-11 21:08:02 +0100

    linux: 5.15.99 -> 5.15.100

--- pkgs/os-specific/linux/kernel/linux-5.15.nix
+++ pkgs/os-specific/linux/kernel/linux-5.15.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.15.99";
+  version = "5.15.100";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0ih3pqxv8kwc8awkd7f5cib9zdnmz6hczqfcz62s7bppjchbicwr";
+    sha256 = "09qlihw3pmb52kdsrm9pd6vbvfkrxrvpyzw896dqgbf11y3wx97p";
   };
 } // (args.argsOverride or { }))
