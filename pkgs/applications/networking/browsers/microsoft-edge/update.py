e3e9c2d58a78ccfed4a4ec483b53637ee34ff972
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-03-11 15:15:04 +0800

    microsoft-edge: Replace comments by functions and add confirmation prompt
    Co-authored-by: Rik Huijzer <t.h.huijzer@rug.nl>

--- pkgs/applications/networking/browsers/microsoft-edge/update.py
+++ pkgs/applications/networking/browsers/microsoft-edge/update.py
@@ -8,45 +8,63 @@ import base64
 from debian.deb822 import Packages
 from debian.debian_support import Version
 
-# Download and extract the package list file
-packages_url = 'https://packages.microsoft.com/repos/edge/dists/stable/main/binary-amd64/Packages.gz'
-handle = request.urlopen(packages_url)
-data = gzip.GzipFile(fileobj=handle).read()
+def packages():
+  packages_url = 'https://packages.microsoft.com/repos/edge/dists/stable/main/binary-amd64/Packages.gz'
+  handle = request.urlopen(packages_url)
+  data = gzip.GzipFile(fileobj=handle).read()
+  return data
 
-# Find latest version for each channel
-latest_packages: dict[str, Packages] = {}
-for package in Packages.iter_paragraphs(data, use_apt_pkg=False):
-  name: str = package['Package']
-  if not name.startswith('microsoft-edge-'):
-    continue
-  channel = name.replace('microsoft-edge-', '')
-  if channel not in latest_packages:
-    latest_packages[channel] = package
-  else:
-    old_package = latest_packages[channel]
-    if old_package.get_version() < package.get_version():  # type: ignore
+def latest_packages(packages):
+  latest_packages: dict[str, Packages] = {}
+  for package in Packages.iter_paragraphs(packages, use_apt_pkg=False):
+    name: str = package['Package']
+    if not name.startswith('microsoft-edge-'):
+      continue
+    channel = name.replace('microsoft-edge-', '')
+    if channel not in latest_packages:
       latest_packages[channel] = package
+    else:
+      old_package = latest_packages[channel]
+      if old_package.get_version() < package.get_version():  # type: ignore
+        latest_packages[channel] = package
+  return latest_packages
 
-# Generate a Nix expression for each channel
-channel_strs: list[str] = []
-for channel, package in latest_packages.items():
-  match = Version.re_valid_version.match(package['Version'])
-  assert match is not None
+def nix_expressions(latest):
+  channel_strs: list[str] = []
+  for channel, package in latest.items():
+    print(f"Processing {channel} {package['Version']}")
+    match = Version.re_valid_version.match(package['Version'])
+    assert match is not None
 
-  version = match.group('upstream_version')
-  revision = match.group('debian_revision')
-  sri = 'sha256-' + base64.b64encode(bytes.fromhex(package['SHA256'])).decode('ascii')
+    version = match.group('upstream_version')
+    revision = match.group('debian_revision')
+    sri = 'sha256-' + base64.b64encode(bytes.fromhex(package['SHA256'])).decode('ascii')
 
-  channel_strs.append(f'''
-  {channel} = import ./browser.nix {{
-    channel = "{channel}";
-    version = "{version}";
-    revision = "{revision}";
-    sha256 = "{sri}";
-  }};''')
+    channel_strs.append(f'''
+    {channel} = import ./browser.nix {{
+      channel = "{channel}";
+      version = "{version}";
+      revision = "{revision}";
+      sha256 = "{sri}";
+    }};''')
+  return channel_strs
 
-# Write the final expression
-nix_expr = f'''{{{'  '.join(channel_strs)}
-}}'''
-with open('default.nix', 'w') as f:
-  f.write(nix_expr)
+def write_expression():
+  latest = latest_packages(packages())
+  channel_strs = nix_expressions(latest)
+  nix_expr = f'''{{{''.join(channel_strs)}
+  }}'''
+
+  print(f'''
+This script will update the default.nix file with the following contents:
+{nix_expr}\n
+Are you sure you want to continue [y/N]?
+  ''')
+  response = input('> ')
+  if response.lower() != 'y':
+    print('Aborting')
+    exit(1)
+  with open('default.nix', 'w') as f:
+    f.write(nix_expr)
+
+write_expression()
