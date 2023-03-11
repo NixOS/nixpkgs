a7e8719197e4ee8048310c9a9030907b62dd4785
Jan Tojnar <jtojnar@gmail.com>
2023-03-11 18:58:32 +0000

    glib: 2.75.4 → 2.76.0
    https://gitlab.gnome.org/GNOME/glib/-/compare/2.75.4...2.76.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/glib/default.nix
+++ pkgs/development/libraries/glib/default.nix
@@ -56,11 +56,11 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "glib";
-  version = "2.75.4";
+  version = "2.76.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/glib/${lib.versions.majorMinor finalAttrs.version}/glib-${finalAttrs.version}.tar.xz";
-    sha256 = "Fs4ku488DqO9vak3wJC5O7i1rS1Bfl5eQsFKpM9ratE=";
+    sha256 = "Ulu3A7gHFC4a7lzPIiw0ToBkshwMRWd+9ZTlh4dMZ5c=";
   };
 
   patches = lib.optionals stdenv.isDarwin [
@@ -115,27 +115,6 @@ stdenv.mkDerivation (finalAttrs: {
     # Disable flaky test.
     # https://gitlab.gnome.org/GNOME/glib/-/issues/820
     ./skip-timer-test.patch
-
-    # Fix copying files on btrfs.
-    # https://gitlab.gnome.org/GNOME/glib/-/merge_requests/3309
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/glib/-/commit/406143b072ec92c2810f93441f037793215b04d9.patch";
-      hash = "sha256-s2OZZWNgf+gp9TwhLR2nOL9OgV9jUyBrJN8FR8zcE1I=";
-    })
-
-    # Fix GJS tests.
-    # https://gitlab.gnome.org/GNOME/glib/-/issues/2932
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/glib/-/commit/4e62e55d6d0109012869a589eae2371e836e9cb3.patch";
-      hash = "sha256-BadhneyDTINPMkNZ+Z/sqUtuHXipHE5gbXLki7dFmmA=";
-    })
-
-    # Fix g_file_info_get_is_backup.
-    # https://gitlab.gnome.org/GNOME/glib/-/merge_requests/3311
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/glib/-/commit/448a163bee9906ba3937ea8cdb4a08afeb6f9dc3.patch";
-      hash = "sha256-YXMcmY4RnV9wUkM6Lrcu8o71ir+k2p385rb52wogHQc=";
-    })
   ];
 
   outputs = [ "bin" "out" "dev" "devdoc" ];
