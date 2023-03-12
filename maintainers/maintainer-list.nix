5ffd1b17f84f5cc3c6bb33fde515a883e850dc09
Felipe Silva <hey@felipeqq2.rocks>
2023-03-12 12:28:40 -0300

    maintainers: update felipeqq2's gpg key

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4895,7 +4895,7 @@
     email = "nixpkgs@felipeqq2.rocks";
     github = "felipeqq2";
     githubId = 71830138;
-    keys = [{ fingerprint = "F5F0 2BCE 3580 BF2B 707A  AA8C 2FD3 4A9E 2671 91B8"; }];
+    keys = [{ fingerprint = "7391 BF2D A2C3 B2C9 BE25  ACA9 C7A7 4616 F302 5DF4"; }];
     matrix = "@felipeqq2:pub.solar";
   };
   felixscheinost = {
