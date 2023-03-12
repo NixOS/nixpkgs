2cbc1115895c713a9bc6e9068f98bd997a0f4f1c
techknowlogick <techknowlogick@gitea.io>
2023-03-12 01:49:43 -0500

    svix-cli: init at 0.21.1

---
+++ pkgs/by-name/sv/svix-cli/package.nix
@@ -0,0 +1,33 @@
+{ lib, fetchFromGitHub, buildGoModule }:
+
+buildGoModule rec {
+  version = "0.21.1";
+  pname = "svix-cli";
+  revision = "v${version}";
+
+  src = fetchFromGitHub {
+    owner = "svix";
+    repo = pname;
+    rev = revision;
+    hash = "sha256-bHcxhJs4Nu/hdiftQFZMx4M5iqFtpOzrsvXOgo9NlDc=";
+  };
+
+  vendorHash = "sha256-qSzEpxktdAV+mHa+586mKvpclCpXR6sE7HNcPZywd4s=";
+
+  # Increase minimum go version to 1.17 as the build fails with 1.16
+  # due to modules requiring code that was introduced in 1.17
+  # PR submitted upstream: https://github.com/svix/svix-cli/pull/103
+  patches = [ ./increase-minimum-go.patch ];
+
+  subPackages = [ "." ];
+
+  ldflags =
+    [ "-s" "-w" "-X github.com/svix/svix-cli/version.Version=v${version}" ];
+
+  meta = with lib; {
+    description = "A CLI for interacting with the Svix API";
+    homepage = "https://github.com/svix/svix-cli/";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ techknowlogick ];
+  };
+}
