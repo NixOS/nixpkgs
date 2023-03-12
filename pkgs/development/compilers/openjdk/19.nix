bb1d5781e4b13d659c3cf087dcdd9c596a4f2d2d
Sergei Trofimovich <slyich@gmail.com>
2023-03-12 12:22:32 +0000

    openjdk{15..19}: fix position location
    Before this change `ix edit -f. jdk` command pointed at shared metadata.

--- pkgs/development/compilers/openjdk/19.nix
+++ pkgs/development/compilers/openjdk/19.nix
@@ -167,6 +167,7 @@ let
 
     disallowedReferences = [ openjdk19-bootstrap ];
 
+    pos = builtins.unsafeGetAttrPos "feature" version;
     meta = import ./meta.nix lib version.feature;
 
     passthru = {
