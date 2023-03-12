c4c109f8215ee52e08eb65aba4f4833e14b1b476
Edvin Källström <edvin.kallstrom@protonmail.com>
2023-03-12 22:15:47 +0100

    wmenu: init at 0.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39091,6 +39091,8 @@ with pkgs;
 
   wfuzz = with python3Packages; toPythonApplication wfuzz;
 
+  wmenu = callPackage ../applications/misc/wmenu { };
+
   bemenu = callPackage ../applications/misc/bemenu { };
 
   _9menu = callPackage ../applications/misc/9menu { };
