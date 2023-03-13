d742e50b2f36832c88699bb71bb6362f8baf5582
Charles Duffy <charles@dyfis.net>
2023-03-13 15:20:35 -0500

    osslsigncode: per review feedback, pull patches from github rather than vendoring into the tree

--- pkgs/development/tools/osslsigncode/default.nix
+++ pkgs/development/tools/osslsigncode/default.nix
@@ -2,6 +2,7 @@
 , stdenv
 , fetchFromGitHub
 , cmake
+, fetchpatch
 , pkg-config
 , curl
 , openssl
@@ -18,7 +19,18 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-33uT9PFD1YEIMzifZkpbl2EAoC98IsM72K4rRjDfh8g=";
   };
 
-  patches = [ ./darwin-cmake.patch ];
+  patches = [
+    # Cygwin patch is prereq for Darwin fix applying -- committed to master after 2.5 release
+    (fetchpatch {
+      url = "https://github.com/mtrojnar/osslsigncode/commit/1c678bf926b78c947b14c46c3ce88e06268c738e.patch";
+      sha256 = "sha256-vOBMGIJ3PHJTvmsXRRfAUJRi7P929PcfmrUiRuM0pf4=";
+    })
+    # Fix build on Darwin when clang not identified as Apple (https://github.com/mtrojnar/osslsigncode/pull/247)
+    (fetchpatch {
+      url = "https://github.com/charles-dyfis-net/osslsigncode/commit/b2ed89b35c8a26faa7eb6515fecaff3c4c5f7fed.patch";
+      sha256 = "sha256-FGKZK/IzHbbkTzSoAtpC75z79d5+qQvvJrjEDY31WJ0=";
+    })
+  ];
 
   nativeBuildInputs = [ cmake pkg-config ];
 
