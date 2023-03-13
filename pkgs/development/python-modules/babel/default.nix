a9b7d738fc1437f8d796607ed31093921685b2b4
Vladimír Čunát <v@cunat.cz>
2023-03-13 14:40:12 +0100

    Revert "python3Packages.babel: revert test fixes for now"
    This reverts commit 3607a737006fe222c1eff66ae78bbc1ea5993fb4.
    i.e. it reapplies commits 86fc053269358 and cebf6aa8fd0d.

--- pkgs/development/python-modules/babel/default.nix
+++ pkgs/development/python-modules/babel/default.nix
@@ -1,8 +1,20 @@
-{ stdenv, lib, buildPythonPackage, fetchPypi, pythonOlder, pytz, pytestCheckHook, freezegun }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pythonAtLeast
+, pythonOlder
+
+# tests
+, freezegun
+, pytestCheckHook
+, pytz
+}:
 
 buildPythonPackage rec {
   pname = "babel";
   version = "2.12.1";
+  format = "setuptools";
+
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
@@ -11,12 +23,30 @@ buildPythonPackage rec {
     hash = "sha256-zC2ZmZzQHURCCuclohyeNxGzqtx5dtYUf2IthYGWNFU=";
   };
 
-  propagatedBuildInputs = lib.optional (pythonOlder "3.9") pytz;
+  propagatedBuildInputs = lib.optionals (pythonOlder "3.9") [
+    pytz
+  ];
 
-  nativeCheckInputs = [ pytestCheckHook freezegun ];
+  # including backports.zoneinfo for python<3.9 yields infinite recursion
+  doCheck = pythonAtLeast "3.9";
+
+  nativeCheckInputs = [
+    # via setup.py
+    freezegun
+    pytestCheckHook
+    # via tox.ini
+    pytz
+  ];
+
+  disabledTests = [
+    # fails on days switching from and to daylight saving time in EST
+    # https://github.com/python-babel/babel/issues/988
+    "test_format_time"
+  ];
 
   meta = with lib; {
     homepage = "https://babel.pocoo.org/";
+    changelog = "https://github.com/python-babel/babel/releases/tag/v${version}";
     description = "Collection of internationalizing tools";
     license = licenses.bsd3;
     maintainers = with maintainers; [ SuperSandro2000 ];
