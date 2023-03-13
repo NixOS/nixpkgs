de47bab216d86de41452125f70dd5e454b0dfcd8
K900 <me@0upti.me>
2023-03-13 14:41:24 +0300

    linux: 6.2.5 -> 6.2.6
    (cherry picked from commit b088383b8c19834c5daa4b476b533433daacaa63)

--- pkgs/os-specific/linux/kernel/linux-6.2.nix
+++ pkgs/os-specific/linux/kernel/linux-6.2.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.2.5";
+  version = "6.2.6";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "1fg5rq40fi7xkx3pw54538gydygbizg492aqfnh0hn3fry903av5";
+    sha256 = "179x1fqgi3drg1q1xy0648hvy7cpc79yzn2r248rq4mprvbz3qhz";
   };
 } // (args.argsOverride or { }))
