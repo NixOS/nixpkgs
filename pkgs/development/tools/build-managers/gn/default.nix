94310476ddadae751a911f1c709002db9e48cb0d
Steven Keuchel <steven.keuchel@gmail.com>
2023-03-13 12:50:31 +0100

    gn: 2020-03-09 -> 2024-01-22

--- pkgs/development/tools/build-managers/gn/default.nix
+++ pkgs/development/tools/build-managers/gn/default.nix
@@ -1,10 +1,10 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix args {
-  # Note: Please use the recommended version for Chromium, e.g.:
-  # https://git.archlinux.org/svntogit/packages.git/tree/trunk/chromium-gn-version.sh?h=packages/gn
-  rev = "fd3d768bcfd44a8d9639fe278581bd9851d0ce3a";
-  revNum = "1718"; # git describe HEAD --match initial-commit | cut -d- -f3
-  version = "2020-03-09";
-  sha256 = "1asc14y8by7qcn10vbk467hvx93s30pif8r0brissl0sihsaqazr";
+  # Note: Please use the recommended version for Chromium stabe, i.e. from
+  # <nixpkgs>/pkgs/applications/networking/browsers/chromium/upstream-info.nix
+  rev = "f99e015ac35f689cfdbf46e4eb174e5d2da78d8e";
+  revNum = "2140"; # git describe $rev --match initial-commit | cut -d- -f3
+  version = "2024-01-22";
+  sha256 = "sha256-UhdDsq9JyP0efGpAaJ/nLp723BbjM6pkFPcAnQbgMKY=";
 }
