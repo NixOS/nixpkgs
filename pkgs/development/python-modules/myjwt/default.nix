0d184f9c9d533c567090a19978fadf2bba93239a
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-13 17:22:56 +0100

    python310Packages.myjwt: add changelog to meta

--- pkgs/development/python-modules/myjwt/default.nix
+++ pkgs/development/python-modules/myjwt/default.nix
@@ -59,6 +59,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "CLI tool for testing vulnerabilities of JSON Web Tokens (JWT)";
     homepage = "https://github.com/mBouamama/MyJWT";
+    changelog = "https://github.com/tyki6/MyJWT/releases/tag/${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
     # Build failures
