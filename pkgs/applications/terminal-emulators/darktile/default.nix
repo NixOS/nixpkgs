3a9e8b3eeb02f4008cb550496a9c7dd4b3dee503
Pavel Zolotarevskiy <contact@flexagoon.com>
2023-03-13 16:10:47 +0000

    darktile: transfer maintainership to sogreatandpowerfull

--- pkgs/applications/terminal-emulators/darktile/default.nix
+++ pkgs/applications/terminal-emulators/darktile/default.nix
@@ -60,6 +60,6 @@ stdenv.mkDerivation rec {
     downloadPage = "https://github.com/liamg/darktile/releases";
     changelog = "https://github.com/liamg/darktile/releases/tag/v${version}";
     license = licenses.mit;
-    maintainers = with maintainers; [ flexagoon ];
+    maintainers = with maintainers; [ sogreatandpowerfull ];
   };
 }
