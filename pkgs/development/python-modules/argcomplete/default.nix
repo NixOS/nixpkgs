c921484a9814523da13327b359fd31ca321d0797
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-14 08:31:41 +0100

    python310Packages.argcomplete: add changelog to meta

--- pkgs/development/python-modules/argcomplete/default.nix
+++ pkgs/development/python-modules/argcomplete/default.nix
@@ -33,6 +33,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Bash tab completion for argparse";
     homepage = "https://kislyuk.github.io/argcomplete/";
+    changelog = "https://github.com/kislyuk/argcomplete/blob/v${version}/Changes.rst";
     license = licenses.asl20;
     maintainers = with maintainers; [ womfoo ];
   };
