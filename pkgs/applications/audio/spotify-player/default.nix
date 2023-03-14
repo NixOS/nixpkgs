fccc21ef3db5c088cde36e216e8432e8d3474bd3
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-14 09:21:21 +0100

    spotify-player: add changelog to emta

--- pkgs/applications/audio/spotify-player/default.nix
+++ pkgs/applications/audio/spotify-player/default.nix
@@ -16,8 +16,8 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "aome510";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-KHbeCnsdHP7Zsj9KeVLuumcVOW6m7Tz1GgBBQ25Rbyo=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-KHbeCnsdHP7Zsj9KeVLuumcVOW6m7Tz1GgBBQ25Rbyo=";
   };
 
   cargoHash = "sha256-51xKCiGdvJ8k9ArWBCazJGgRljqHxZiyTdes4i7JZH8=";
@@ -46,6 +46,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "A command driven spotify player";
     homepage = "https://github.com/aome510/spotify-player";
+    changelog = "https://github.com/aome510/spotify-player/releases/tag/v${version}";
     mainProgram = "spotify_player";
     license = licenses.mit;
     maintainers = with maintainers; [ dit7ya ];
