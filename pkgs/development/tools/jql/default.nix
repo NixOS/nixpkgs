2d851c7d97860c3ebce0fda7bc4b5678bd385c94
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-14 09:22:53 +0100

    jql: add changelog to meta

--- pkgs/development/tools/jql/default.nix
+++ pkgs/development/tools/jql/default.nix
@@ -1,4 +1,7 @@
-{ lib, fetchFromGitHub, rustPlatform }:
+{ lib
+, fetchFromGitHub
+, rustPlatform
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "jql";
@@ -7,8 +10,8 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "yamafaktory";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-gFPN3aSukh0QMfGLn65icf5ZyYb8Y+r+GMdG2gm2InY=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-gFPN3aSukh0QMfGLn65icf5ZyYb8Y+r+GMdG2gm2InY=";
   };
 
   cargoHash = "sha256-XJW0TDRJdLwgWDm5ZBSCUj5VS5ZowGCr6tHV0MpZuvI=";
@@ -16,6 +19,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "A JSON Query Language CLI tool built with Rust";
     homepage = "https://github.com/yamafaktory/jql";
+    changelog = "https://github.com/yamafaktory/jql/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ akshgpt7 ];
   };
