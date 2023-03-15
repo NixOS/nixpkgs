185cca3e505c9c724a31cacefdeba0115b578450
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-15 13:23:01 +0000

    lefthook: 1.3.3 -> 1.3.4

--- pkgs/applications/version-management/lefthook/default.nix
+++ pkgs/applications/version-management/lefthook/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "lefthook";
-  version = "1.3.3";
+  version = "1.3.4";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "evilmartians";
     repo = "lefthook";
-    sha256 = "sha256-9XmLQPkc8we5wRZe5+bhL1b9lxWX6JAQeF4DmRXBgew=";
+    sha256 = "sha256-D+QI29i7xBhOFeA05iGWoFh5zv9Se8Q3Vxiu8iHfzbE=";
   };
 
   vendorHash = "sha256-VeR/lyrQrjXWvHdxpG4H+XPlAud9rrlzX8GqhVzn1sg=";
