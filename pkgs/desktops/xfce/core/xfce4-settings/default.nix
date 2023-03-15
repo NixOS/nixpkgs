e5fdb2fce869bc2e337d2d8018ed96a9f451a195
Bobby Rong <rjl931189261@126.com>
2023-03-15 15:36:18 +0800

    xfce.xfce4-settings: 4.18.1 -> 4.18.2
    https://gitlab.xfce.org/xfce/xfce4-settings/-/compare/xfce4-settings-4.18.1...xfce4-settings-4.18.2

--- pkgs/desktops/xfce/core/xfce4-settings/default.nix
+++ pkgs/desktops/xfce/core/xfce4-settings/default.nix
@@ -16,9 +16,9 @@
 mkXfceDerivation {
   category = "xfce";
   pname = "xfce4-settings";
-  version = "4.18.1";
+  version = "4.18.2";
 
-  sha256 = "sha256-Uy5dObnMV+fpt8RdyFOsYVPN8Dyx1zzOu0pDak01ipQ=";
+  sha256 = "sha256-u8xto4tP9fsaPaY6dzv4Bj/C6qvltT5wXJlV+TzP5uE=";
 
   postPatch = ''
     for f in xfsettingsd/pointers.c dialogs/mouse-settings/main.c; do
