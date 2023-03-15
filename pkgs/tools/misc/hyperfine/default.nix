aa050358f457c7bc80429e91b5d4bc15f54f5390
Merlin <megoettlinger@gmail.com>
2023-03-15 15:30:52 +0100

    Update pkgs/tools/misc/hyperfine/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/tools/misc/hyperfine/default.nix
+++ pkgs/tools/misc/hyperfine/default.nix
@@ -20,8 +20,10 @@ rustPlatform.buildRustPackage rec {
 
   nativeBuildInputs = [ installShellFiles ];
   buildInputs = lib.optional stdenv.isDarwin Security;
-  propagatedBuildInputs = [ bash.out # hyperfine uses 'sh -c' which gets symlinked as alias for bash by this derivation
-                          ];
+  propagatedBuildInputs = [
+    # hyperfine uses 'sh -c' which gets symlinked as alias for bash by this derivation
+    bash.out 
+  ];
 
   postInstall = ''
     installManPage doc/hyperfine.1
