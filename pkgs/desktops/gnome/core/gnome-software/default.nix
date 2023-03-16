0aae37164a1c1b4a4a2ebeb433c3e78c2f37af5f
Jan Tojnar <jtojnar@gmail.com>
2023-03-16 16:13:45 +0000

    gnome.gnome-software: 44.rc → 44.0
    https://gitlab.gnome.org/GNOME/gnome-software/-/compare/44.rc...44.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-software/default.nix
+++ pkgs/desktops/gnome/core/gnome-software/default.nix
@@ -45,11 +45,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gnome-software";
-  version = "44.rc";
+  version = "44.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-software/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "ErgEYOCoAi7+/+EfU0gzO0MZQVNxNIqrmTRye8SQrIY=";
+    sha256 = "YZcZ+VKeC7Ha0w+tu3gNex2ZlAptsfcd9RvHNzQYMK8=";
   };
 
   patches = [
