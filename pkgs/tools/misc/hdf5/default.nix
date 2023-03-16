b89323329c60a09936d76b9c6fad669b3b5da0a2
Markus Kowalewski <markus.kowalewski@fysik.su.se>
2023-03-16 11:12:32 +0100

    hdf5: fix dev output, remove refs to /build from compiler wrappers

--- pkgs/tools/misc/hdf5/default.nix
+++ pkgs/tools/misc/hdf5/default.nix
@@ -91,6 +91,18 @@ stdenv.mkDerivation rec {
     moveToOutput 'bin/h5pcc' "''${!outputDev}"
   '';
 
+  # Remove reference to /build, which get introduced
+  # into AM_CPPFLAGS since hdf5-1.14.0. Cmake of various
+  # packages using HDF5 gets confused trying access the non-existent path.
+  postFixup = ''
+    for i in h5cc h5pcc h5c++; do
+      if [ -f $dev/bin/$i ]; then
+        substituteInPlace $dev/bin/$i --replace \
+          '-I/build/hdf5-${version}/src/H5FDsubfiling' ""
+      fi
+    done
+  '';
+
   passthru.tests = {
     inherit (python3.pkgs) h5py;
   };
