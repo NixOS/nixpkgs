20fe8b2871b3d53e3443d8e2a8f661fe0d46f3e9
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-17 01:20:39 +0100

    python310Packages.passlib: libxcrypt related failures also affect linux

--- pkgs/development/python-modules/passlib/default.nix
+++ pkgs/development/python-modules/passlib/default.nix
@@ -1,20 +1,23 @@
 { lib
-, stdenv
 , buildPythonPackage
 , fetchPypi
 , argon2-cffi
 , bcrypt
 , cryptography
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "passlib";
   version = "1.7.4";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "defd50f72b65c5402ab2c573830a6978e5f202ad0d984793c8dde2c4152ebe04";
+    hash = "sha256-3v1Q9ytlxUAqssVzgwppeOXyAq0NmEeTyN3ixBUuvgQ";
   };
 
   passthru.optional-dependencies = {
@@ -29,11 +32,14 @@ buildPythonPackage rec {
   ++ passthru.optional-dependencies.bcrypt
   ++ passthru.optional-dependencies.totp;
 
+  pythonImportsCheck = [
+    "passlib"
+  ];
+
   disabledTests = [
     # timming sensitive
     "test_dummy_verify"
     "test_encrypt_cost_timing"
-  ] ++ lib.optionals stdenv.isDarwin [
     # These tests fail because they don't expect support for algorithms provided through libxcrypt
     "test_82_crypt_support"
   ];
