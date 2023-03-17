b26bcd8dd305cf1247dcfb677dbbb34c8109548c
K900 <me@0upti.me>
2023-03-18 01:39:35 +0300

    ksmtp: drop unused patch

--- pkgs/applications/kde/ksmtp/default.nix
+++ pkgs/applications/kde/ksmtp/default.nix
@@ -13,5 +13,4 @@ mkDerivation {
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [ kcoreaddons kio kmime ];
   propagatedBuildInputs = [ cyrus_sasl ];
-  patches = [ ./0001-Use-KDE_INSTALL_TARGETS_DEFAULT_ARGS-when-installing.patch ];
 }
