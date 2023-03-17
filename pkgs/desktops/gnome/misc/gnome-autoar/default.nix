2bc91b0a711dd85344b88f8adb49c1c59da79ae8
Jan Tojnar <jtojnar@gmail.com>
2023-03-17 18:36:49 +0000

    gnome.gnome-autoar: 0.4.3 → 0.4.4
    https://gitlab.gnome.org/GNOME/gnome-autoar/-/compare/0.4.3...0.4.4
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/misc/gnome-autoar/default.nix
+++ pkgs/desktops/gnome/misc/gnome-autoar/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-autoar";
-  version = "0.4.3";
+  version = "0.4.4";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-autoar/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "e98HiVU0lqvdw8ljsM5zY4BcDALAJf7d68qsx4cknog=";
+    sha256 = "wK++MzvPPLFEGh9XTMjsexuBl3eRRdTt7uKJb9rPw8I=";
   };
 
   nativeBuildInputs = [
