69425e7a49ce91e80780ce8b97a609d1ea103048
mar-jac <marjac@meta.com>
2023-03-17 14:19:36 -0700

    tmpreaper: init at 1.6.17
    This package provides a program that can be used to clean out temporary-file directories.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12798,6 +12798,8 @@ with pkgs;
 
   tmpwatch = callPackage ../tools/misc/tmpwatch  { };
 
+  tmpreaper = callPackage ../tools/misc/tmpreaper  { };
+
   tmpmail = callPackage ../applications/networking/tmpmail { };
 
   tmux = callPackage ../tools/misc/tmux { };
