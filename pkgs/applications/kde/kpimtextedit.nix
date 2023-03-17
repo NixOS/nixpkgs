a5a292c7a82c694129c1045a4ee8a5078db37d71
K900 <me@0upti.me>
2023-03-18 01:58:24 +0300

    treewide: clear out remaining qt5 conditionals

--- pkgs/applications/kde/kpimtextedit.nix
+++ pkgs/applications/kde/kpimtextedit.nix
@@ -2,7 +2,7 @@
   mkDerivation, lib, kdepimTeam,
   extra-cmake-modules, kdoctools,
   grantlee, kcodecs, kconfigwidgets, kemoticons, ki18n, kiconthemes, kio,
-  kdesignerplugin, ktextwidgets, sonnet, syntax-highlighting, qtbase, qttools,
+  kdesignerplugin, ktextwidgets, sonnet, syntax-highlighting, qttools,
   qtspeech
 }:
 
@@ -11,7 +11,6 @@ mkDerivation {
   meta = {
     license = with lib.licenses; [ gpl2Plus lgpl21Plus fdl12Plus ];
     maintainers = kdepimTeam;
-    broken = lib.versionOlder qtbase.version "5.13.0";
   };
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [
