e1fbf2b30b82f53a4a594ef1d12c09db2306436a
mar-jac <marjac@meta.com>
2023-03-17 13:43:33 -0700

    tmpreaper: init at 1.6.17
    This package provides a program that can be used to clean out temporary-file directories.

---
+++ pkgs/tools/misc/tmpreaper/default.nix
@@ -0,0 +1,29 @@
+{ stdenv, fetchurl, autoconf, automake }:
+
+let
+  inherit (stdenv.lib) optional;
+  sha256-default = "1ca94d156eb68160ec9b6ed8b97d70fbee996de21437f0cf7d0c3b46709fecbc";
+in
+stdenv.mkDerivation rec {
+  pname = "tmpreaper";
+  version = "1.6.17";
+
+  buildInputs = [ autoconf automake ];
+
+  installPhase = ''
+    mkdir -p $out/sbin
+    cp tmpreaper $out/sbin
+    mkdir -p $out/share/man/man8
+    cp tmpreaper.8 $out/share/man/man8
+  '';
+
+  doCheck = false;
+
+  src = fetchurl {
+    url = "https://deb.debian.org/debian/pool/main/t/tmpreaper/tmpreaper_${version}.tar.gz";
+    sha256 = sha256-default;
+  };
+  homepage = "https://packages.debian.org/sid/tmpreaper";
+  description = "Clean up files in directories based on their age";
+  license = "GPL-2.0-only";
+}
