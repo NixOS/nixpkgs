ff19b78ba70fd024ab49112725299587110475c9
Mario Rodas <marsam@users.noreply.github.com>
2023-03-18 05:40:00 +0000

    checkstyle: 10.9.1 -> 10.9.2
    https://github.com/checkstyle/checkstyle/releases/tag/checkstyle-10.9.2

--- pkgs/development/tools/analysis/checkstyle/default.nix
+++ pkgs/development/tools/analysis/checkstyle/default.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchurl, makeWrapper, jre }:
 
 stdenv.mkDerivation rec {
-  version = "10.9.1";
+  version = "10.9.2";
   pname = "checkstyle";
 
   src = fetchurl {
     url = "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${version}/checkstyle-${version}-all.jar";
-    sha256 = "sha256-Du6iHNhxWWL9HncPLpOga4B+HyBpiqw5IzKGknjtWbM=";
+    sha256 = "sha256-XsWzSg/bwP+O454oQSVDrvmjWKVZpLyfGB47olqRSUY=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
