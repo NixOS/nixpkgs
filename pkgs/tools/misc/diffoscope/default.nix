7f90e4f465a7b6c88e88a4f24f637436b6204632
Raito Bezarius <masterancpp@gmail.com>
2023-03-18 16:13:21 +0100

    diffoscope: move unfree dependencies behind a enableUnfree flag
    (cherry picked from commit 21332b8fd5031fda106cf22c14257a48e827a89f)

--- pkgs/tools/misc/diffoscope/default.nix
+++ pkgs/tools/misc/diffoscope/default.nix
@@ -3,7 +3,8 @@
 , e2fsprogs, enjarify, file, findutils, fontforge-fonttools, ffmpeg, fpc, gettext, ghc, ghostscriptX, giflib, gnumeric, gnupg, gnutar
 , gzip, html2text, hdf5, imagemagick, jdk, libarchive, libcaca, llvm, lz4, mono, ocaml, oggvideotools, openssh, openssl, pdftk, pgpdump, poppler_utils, procyon, qemu, R
 , radare2, sng, sqlite, squashfsTools, tcpdump, ubootTools, odt2txt, unzip, wabt, xmlbeans, xxd, xz, zip, zstd
-, enableBloat ? false
+, enableBloat ? true
+, enableUnfree ? false
 # updater only
 , writeScript
 }:
@@ -54,10 +55,15 @@ python3Packages.buildPythonApplication rec {
       python-magic progressbar33 pypdf2 tlsh
     ])
     ++ lib.optionals stdenv.isLinux [ python3Packages.pyxattr python3Packages.rpm acl cdrkit dtc ]
-    ++ lib.optionals enableBloat ([
-      abootimg apksigcopier apksigner apktool cbfstool colord enjarify ffmpeg fpc ghc ghostscriptX giflib gnupg gnumeric
-      hdf5 imagemagick libcaca llvm jdk mono ocaml odt2txt oggvideotools openssh pdftk poppler_utils procyon qemu R tcpdump ubootTools wabt radare2 xmlbeans
-    ] ++ (with python3Packages; [ androguard binwalk guestfs h5py pdfminer-six ]));
+    ++ lib.optionals enableBloat (
+      [
+        abootimg apksigcopier apksigner cbfstool colord enjarify ffmpeg fpc ghc ghostscriptX giflib gnupg gnumeric
+        hdf5 imagemagick libcaca llvm jdk mono ocaml odt2txt oggvideotools openssh pdftk poppler_utils procyon qemu R tcpdump ubootTools wabt radare2 xmlbeans
+      ]
+      ++ (with python3Packages; [ androguard binwalk guestfs h5py pdfminer-six ])
+      # `apktool` depend on `build-tools` which requires Android SDK acceptance, therefore, the whole thing is unfree.
+      ++ lib.optionals enableUnfree [ apktool ]
+    );
 
   nativeCheckInputs = with python3Packages; [ pytestCheckHook ] ++ pythonPath;
 
@@ -121,7 +127,7 @@ python3Packages.buildPythonApplication rec {
     '';
     homepage = "https://diffoscope.org/";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ dezgeg danielfullmer ];
+    maintainers = with maintainers; [ dezgeg danielfullmer raitobezarius ];
     platforms = platforms.unix;
   };
 }
