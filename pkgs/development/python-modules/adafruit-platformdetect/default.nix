2d4c695eda68f21cb113678e1aff7db24ed096b8
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-18 11:15:15 +0100

    python310Packages.adafruit-platformdetect: 3.41.0 -> 3.42.0
    Changelog: https://github.com/adafruit/Adafruit_Python_PlatformDetect/releases/tag/3.42.0

--- pkgs/development/python-modules/adafruit-platformdetect/default.nix
+++ pkgs/development/python-modules/adafruit-platformdetect/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "adafruit-platformdetect";
-  version = "3.41.0";
+  version = "3.42.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "Adafruit-PlatformDetect";
     inherit version;
-    hash = "sha256-NWdY1ykuF8mYxXPCwaVq6mEkQXHrUmhEy/BXDFYn2V0=";
+    hash = "sha256-nFpPJKQv7UNsza1PAcTsZNVp9nFVa/pHlvNRVM4UIUY=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
