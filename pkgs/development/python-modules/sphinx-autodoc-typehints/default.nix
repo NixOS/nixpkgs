19e807bd2c00229700a3e6149636591112693f21
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-03-18 11:46:17 +0100

    python310Packages.sphinx-autodoc-typehints: 1.23.4 -> 1.22
    https://github.com/tox-dev/sphinx-autodoc-typehints/releases/tag/1.22

--- pkgs/development/python-modules/sphinx-autodoc-typehints/default.nix
+++ pkgs/development/python-modules/sphinx-autodoc-typehints/default.nix
@@ -10,7 +10,7 @@
 
 let
   pname = "sphinx-autodoc-typehints";
-  version = "1.23.4";
+  version = "1.22";
 in
 
 buildPythonPackage {
@@ -22,7 +22,7 @@ buildPythonPackage {
   src = fetchPypi {
     pname = "sphinx_autodoc_typehints";
     inherit version;
-    hash = "sha256-tHw6aQ0QGiWdmqkgzFfH6nn7cF15bA1Xyxfpm9HYCjQ=";
+    hash = "sha256-cfyi1e7psDQgTkxoarILTY9euUCTliFryubIfDjhjqY=";
   };
 
   nativeBuildInputs = [
