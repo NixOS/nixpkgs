93d9358ca35608ba084d7f86488ff1025003960c
Vinny Meller <vinnymeller@gmail.com>
2023-03-18 04:27:26 -0500

    maintainers: Add vinnymeller

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15859,6 +15859,12 @@
       fingerprint = "AEF2 3487 66F3 71C6 89A7  3600 95A4 2FE8 3535 25F9";
     }];
   };
+  vinnymeller = {
+    email = "vinnymeller@proton.me";
+    github = "vinnymeller";
+    githubId = 19894025;
+    name = "Vinny Meller";
+  };
   vinymeuh = {
     email = "vinymeuh@gmail.com";
     github = "vinymeuh";
