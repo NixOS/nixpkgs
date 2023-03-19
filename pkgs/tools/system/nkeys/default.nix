2eefc791b4a2171bbbc1f70d4519405a6ad3f782
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-19 12:33:18 +0100

    nkeys: add changelog to meta

--- pkgs/tools/system/nkeys/default.nix
+++ pkgs/tools/system/nkeys/default.nix
@@ -10,8 +10,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "nats-io";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-ePpFzwjFKcm/xgt9TBl1CVnJYxO389rV9uLONeUeX0c=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-ePpFzwjFKcm/xgt9TBl1CVnJYxO389rV9uLONeUeX0c=";
   };
 
   vendorHash = "sha256-ozK0vimYs7wGplw1QhSu+q8R+YsIYHU4m08a7K6i78I=";
@@ -19,6 +19,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "Public-key signature system for NATS";
     homepage = "https://github.com/nats-io/nkeys";
+    changelog = "https://github.com/nats-io/nkeys/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
     mainProgram = "nk";
