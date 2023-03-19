78215ece2efd3d4490af39b9719ae0395180d049
Jichao Ouyang <oyanglulu@gmail.com>
2023-03-19 23:50:38 +1100

    buildBunModules: linux hono has different sha

--- pkgs/test/bun/default.nix
+++ pkgs/test/bun/default.nix
@@ -1,7 +1,10 @@
-{ pkgs }:
+{ pkgs, stdenv }:
 pkgs.buildBunModules {
   pname = "bun-hono-example";
   version = "1.0.87";
-  vendorSha256 = "sha256-ne/9TOoH69/DAtbR0vzEMWy2yTc3F2mRJsOU7ANEGLs=";
+  vendorSha256 = if stdenv.isLinux then
+    "sha256-hXe7j4HED6+DqMzBepsnz5HPAQQU+RKaE5gxTkYvAOE="
+  else
+    "sha256-ne/9TOoH69/DAtbR0vzEMWy2yTc3F2mRJsOU7ANEGLs=";
   src = ./.;
 }
