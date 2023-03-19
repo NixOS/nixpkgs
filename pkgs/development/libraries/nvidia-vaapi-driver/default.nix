cc6ef4ea2a55cf96f63c85814c854dde47dab2e7
Nick Cao <nickcao@nichi.co>
2023-03-19 16:34:14 +0800

    nvidia-vaapi-driver: 0.0.8 -> 0.0.9
    Changes: https://github.com/elFarto/nvidia-vaapi-driver/compare/v0.0.8...v0.0.9

--- pkgs/development/libraries/nvidia-vaapi-driver/default.nix
+++ pkgs/development/libraries/nvidia-vaapi-driver/default.nix
@@ -4,6 +4,7 @@
 , meson
 , ninja
 , pkg-config
+, libdrm
 , libGL
 , gst_all_1
 , nv-codec-headers-11
@@ -13,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nvidia-vaapi-driver";
-  version = "0.0.8";
+  version = "0.0.9";
 
   src = fetchFromGitHub {
     owner = "elFarto";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-RMFkClaWoFNeSglV5otS/rzI6JNQMiAHDzH3DoEHA5I=";
+    sha256 = "sha256-mQtprgm6QonYiMUPPIcCbWxPQ/b2XuQiOkROZNPYaQk=";
   };
 
   nativeBuildInputs = [
@@ -30,6 +31,7 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    libdrm
     libGL
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-bad
@@ -44,6 +46,7 @@ stdenv.mkDerivation rec {
   meta = with lib;{
     homepage = "https://github.com/elFarto/nvidia-vaapi-driver";
     description = "A VA-API implemention using NVIDIA's NVDEC";
+    changelog = "https://github.com/elFarto/nvidia-vaapi-driver/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers;[ nickcao ];
   };
