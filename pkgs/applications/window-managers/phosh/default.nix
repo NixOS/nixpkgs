04a9643bf90ce5ee2463be2b2d136423888bdd77
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2023-03-20 08:13:05 +1100

    phosh: 0.25.1 -> 0.25.2

--- pkgs/applications/window-managers/phosh/default.nix
+++ pkgs/applications/window-managers/phosh/default.nix
@@ -36,7 +36,7 @@
 
 stdenv.mkDerivation rec {
   pname = "phosh";
-  version = "0.25.1";
+  version = "0.25.2";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
@@ -45,7 +45,7 @@ stdenv.mkDerivation rec {
     repo = pname;
     rev = "v${version}";
     fetchSubmodules = true; # including gvc and libcall-ui which are designated as subprojects
-    sha256 = "sha256-ysAZdmkFEuqJDTPe246F2I4Qp+fjtomia42PS8BuMM8=";
+    sha256 = "sha256-A64lkILBYaZnAoh6Yxs+8v6AxqujFVPV2hH4wom3BkU=";
   };
 
   nativeBuildInputs = [
