08f1b8007b076fe4e77462219c453a8dac0019f2
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2023-03-20 08:06:45 +1100

    calls: 43.3 -> 44.0

--- pkgs/applications/networking/calls/default.nix
+++ pkgs/applications/networking/calls/default.nix
@@ -33,7 +33,7 @@
 
 stdenv.mkDerivation rec {
   pname = "calls";
-  version = "43.3";
+  version = "44.0";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
@@ -41,7 +41,7 @@ stdenv.mkDerivation rec {
     repo = pname;
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-GNICAk9SPrLc+zm3tHVwkQdiS20j4MVktGIbNWEEMHs=";
+    hash = "sha256-coIGj13uU5rXYUbBR4Pn8WD0topTlBUaVPaTt4l+sd8=";
   };
 
   outputs = [ "out" "devdoc" ];
