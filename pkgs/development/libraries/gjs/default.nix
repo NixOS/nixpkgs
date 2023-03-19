6d9aa98c4133fe837ad6dafce53f5d023b5f0074
Bobby Rong <rjl931189261@126.com>
2023-03-19 09:44:16 +0000

    gjs: 1.75.90 → 1.76.0
    https://gitlab.gnome.org/GNOME/gjs/-/compare/1.75.90...1.76.0
    
    Changelog-Reviewed-By: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/gjs/default.nix
+++ pkgs/development/libraries/gjs/default.nix
@@ -31,13 +31,13 @@ let
   ];
 in stdenv.mkDerivation rec {
   pname = "gjs";
-  version = "1.75.90";
+  version = "1.76.0";
 
   outputs = [ "out" "dev" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gjs/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-Yv04Az03T/cU62rjtUWSWbHFN/2CLewdkpRIA6JEX3I=";
+    sha256 = "sha256-pj8VaWSxNgU+q1HqATEU59fBk7dRjSjAQLawLDyTOm0=";
   };
 
   patches = [
