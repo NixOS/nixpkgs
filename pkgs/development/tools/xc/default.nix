5c31b1b601ac029998d73ef6108ee2fe6341a7cb
joerdav <joe.davidson.21111@gmail.com>
2023-03-20 10:17:48 +0000

    xc: 0.1.181 -> 0.2.0

--- pkgs/development/tools/xc/default.nix
+++ pkgs/development/tools/xc/default.nix
@@ -1,17 +1,28 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub, installShellFiles }:
 
 buildGoModule rec {
   pname = "xc";
-  version = "0.1.181";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "joerdav";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-C6qZdO6+n9BWm69y09kvnEBF45sB6bfOfmteNO2x68I=";
+    sha256 = "sha256-ACOi9MdsHZ7Td8pcYGj6Oy7uE/g/Mx7B+Uqw6K9hIrE=";
   };
 
-  vendorHash = "sha256-cySflcTuAzbFZbtXmzZ98nfY8HUq1UedONTtKP4EICs=";
+  vendorHash = "sha256-YETiKG8/+DFd9paqy58YYzCKZ47cWlhnUWHjrw5CrDI=";
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  postInstall = ''
+    local INSTALL="$out/bin/xc"
+    installShellCompletion --cmd xc \
+      --bash <(echo "complete -C $INSTALL xc") \
+      --zsh <(echo "complete -o nospace -C $INSTALL xc")
+  '';
 
   meta = with lib; {
     homepage = "https://xcfile.dev/";
