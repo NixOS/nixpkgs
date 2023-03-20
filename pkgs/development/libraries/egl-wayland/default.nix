da2aa5a0db476cdf76acdf101f48173821138264
OPNA2608 <christoph.neidahl@gmail.com>
2023-03-20 18:43:18 +0100

    eglexternalplatform: init at 1.1

--- pkgs/development/libraries/egl-wayland/default.nix
+++ pkgs/development/libraries/egl-wayland/default.nix
@@ -2,6 +2,7 @@
 , stdenv
 , fetchFromGitHub
 , fetchpatch
+, eglexternalplatform
 , pkg-config
 , meson
 , ninja
@@ -13,35 +14,7 @@
 , wayland-protocols
 }:
 
-let
-  eglexternalplatform = stdenv.mkDerivation {
-    pname = "eglexternalplatform";
-    version = "1.1";
-
-    src = fetchFromGitHub {
-      owner = "Nvidia";
-      repo = "eglexternalplatform";
-      rev = "7c8f8e2218e46b1a4aa9538520919747f1184d86";
-      sha256 = "0lr5s2xa1zn220ghmbsiwgmx77l156wk54c7hybia0xpr9yr2nhb";
-    };
-
-    installPhase = ''
-      mkdir -p "$out/include/"
-      cp interface/eglexternalplatform.h "$out/include/"
-      cp interface/eglexternalplatformversion.h "$out/include/"
-
-      substituteInPlace eglexternalplatform.pc \
-        --replace "/usr/include/EGL" "$out/include"
-      mkdir -p "$out/share/pkgconfig"
-      cp eglexternalplatform.pc "$out/share/pkgconfig/"
-    '';
-
-    meta = with lib; {
-      license = licenses.mit;
-    };
-  };
-
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "egl-wayland";
   version = "1.1.11";
 
@@ -75,7 +48,6 @@ in stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    eglexternalplatform
     libGL
     libX11
     libdrm
@@ -83,11 +55,14 @@ in stdenv.mkDerivation rec {
     wayland-protocols
   ];
 
+  propagatedBuildInputs = [
+    eglexternalplatform
+  ];
+
   postFixup = ''
     # Doubled prefix in pc file after postbuild hook replaces includedir prefix variable with dev output path
     substituteInPlace $dev/lib/pkgconfig/wayland-eglstream.pc \
-      --replace "=$dev/$dev" "=$dev" \
-      --replace "Requires:" "Requires.private:"
+      --replace "=$dev/$dev" "=$dev"
   '';
 
   meta = with lib; {
