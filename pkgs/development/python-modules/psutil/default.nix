31bc4ea688643894bbf6463bddbd7599b796cd11
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-03-20 15:47:08 -0400

    python3Packages.psutil: Skip another test with a build impurity
    The outcome of the temperature sensor tests depends on hardware sensors
    present on the build machine so they can cause the build to succeed due just
    to build system configuration.

--- pkgs/development/python-modules/psutil/default.nix
+++ pkgs/development/python-modules/psutil/default.nix
@@ -42,15 +42,19 @@ buildPythonPackage rec {
   # - cpu_times was flaky on darwin
   # - the other disabled tests are likely due to sanboxing (missing specific errors)
   pytestFlagsArray = [
+    # Note: $out must be referenced as test import paths are relative
     "$out/${python.sitePackages}/psutil/tests/test_system.py"
   ];
 
-  # Note: $out must be referenced as test import paths are relative
   disabledTests = [
+    # Some of the tests have build-system hardware-based impurities (like
+    # reading temperature sensor values).  Disable them to avoid the failures
+    # that sometimes result.
     "cpu_freq"
     "cpu_times"
     "disk_io_counters"
     "sensors_battery"
+    "sensors_temperatures"
     "user"
     "test_disk_partitions" # problematic on Hydra's Linux builders, apparently
   ];
