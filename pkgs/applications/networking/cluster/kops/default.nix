c7834ab085028622c1fc5447c1a7915b4913694e
Michael Adler <therisen06@gmail.com>
2023-03-20 10:05:20 +0100

    kops: 1.25.3 -> 1.25.4

--- pkgs/applications/networking/cluster/kops/default.nix
+++ pkgs/applications/networking/cluster/kops/default.nix
@@ -56,7 +56,7 @@ rec {
   };
 
   kops_1_25 = mkKops rec {
-    version = "1.25.3";
+    version = "1.25.4";
     sha256 = "sha256-Q40d62D+H7CpLmrjweCy75U3LgnHEV2pFZs2Ze+koqo=";
     rev = "v${version}";
   };
