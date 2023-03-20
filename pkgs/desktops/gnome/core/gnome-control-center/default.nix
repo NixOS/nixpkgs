3ff89d67b6d20ca9a2762cd3cc5c013249afe0b3
Jan Tojnar <jtojnar@gmail.com>
2023-03-20 00:57:49 +0000

    gnome.gnome-control-center: 44.rc → 44.0
    https://gitlab.gnome.org/GNOME/gnome-control-center/-/compare/44.rc...44.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-control-center/default.nix
+++ pkgs/desktops/gnome/core/gnome-control-center/default.nix
@@ -64,11 +64,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-control-center";
-  version = "44.rc";
+  version = "44.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-9AsZeClzh0y3go70oGV75ZcZoCbfat1uc5kqwuerUQI=";
+    sha256 = "sha256-vb+rTPI9BXNAltsfn2+sfu0/y52jK/Sx8m7ToE5avGY=";
   };
 
   patches = [
