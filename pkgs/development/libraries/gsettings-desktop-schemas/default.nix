1a1e543c7e55171b7dae9b95bc1994a9a8f36b3c
Jan Tojnar <jtojnar@gmail.com>
2023-03-20 00:58:52 +0000

    gsettings-desktop-schemas: 44.beta → 44.0
    https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas/-/compare/44.beta...44.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/libraries/gsettings-desktop-schemas/default.nix
+++ pkgs/development/libraries/gsettings-desktop-schemas/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gsettings-desktop-schemas";
-  version = "44.beta";
+  version = "44.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "Fk7gdvQpMIM3CGeOsxgdBv4u/Mv5RNqRHMgIgcPxKtQ=";
+    sha256 = "6y3kXK2QWZSEnmQqYjret11BshsGJtQNKge46igf7A4=";
   };
 
   strictDeps = true;
