cdb779463d484d51491bf874404a098607d3e34d
Jan Tojnar <jtojnar@gmail.com>
2023-03-20 00:58:38 +0000

    gnome-desktop: 44.beta → 44.0
    https://gitlab.gnome.org/GNOME/gnome-desktop/-/compare/44.beta...44.0

--- pkgs/development/libraries/gnome-desktop/default.nix
+++ pkgs/development/libraries/gnome-desktop/default.nix
@@ -27,13 +27,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-desktop";
-  version = "44.beta";
+  version = "44.0";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-desktop/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-Km93B82bnMdzW7ryxbTFz4BZbvn0WxS7Nddt5H0a3kU=";
+    sha256 = "sha256-QsdzdF2EuhS8HPHExvRgYUiAOlzTN5QcY5ZHlfPFnUI=";
   };
 
   patches = lib.optionals stdenv.isLinux [
