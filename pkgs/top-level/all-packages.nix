415b71e437660b1cb130015e2472aab557675400
Nathan Viets <nathan.g.viets@gmail.com>
2023-03-21 16:30:54 -0500

    Initialize framework to support multiple versions of R. Follows a convention similar to what's done for Julia.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24389,7 +24389,8 @@ with pkgs;
 
   ### DEVELOPMENT / R MODULES
 
-  R = callPackage ../applications/science/math/R {
+  R = R_42;
+  R_42 = callPackage ../applications/science/math/R/4.2.nix {
     # TODO: split docs into a separate output
     texLive = texlive.combine {
       inherit (texlive) scheme-small inconsolata helvetic texinfo fancyvrb cm-super rsfs;
