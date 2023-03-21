10d99268086aaa8ce1f0d448e27835ab6c7d27c3
ajs124 <git@ajs124.de>
2023-03-21 17:15:04 +0100

    sbsigntool: 0.9.4 -> 0.9.5
    https://git.kernel.org/pub/scm/linux/kernel/git/jejb/sbsigntools.git/commit/?id=9cfca9fe7aa7a8e29b92fe33ce8433e212c9a8ba

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8162,9 +8162,7 @@ with pkgs;
 
   sbctl = callPackage ../tools/security/sbctl { };
 
-  sbsigntool = callPackage ../tools/security/sbsigntool {
-    openssl = openssl_1_1;
-  };
+  sbsigntool = callPackage ../tools/security/sbsigntool { };
 
   sonic-server = callPackage ../servers/search/sonic-server { };
 
