91a1291d2b767aab6d14ef8fb0d38b098da3ea6e
Aroun <zahrun@murena.io>
2023-03-21 23:38:09 +0530

    virtualbox: add pointer to related nix options

--- pkgs/applications/virtualization/virtualbox/default.nix
+++ pkgs/applications/virtualization/virtualbox/default.nix
@@ -229,6 +229,12 @@ in stdenv.mkDerivation {
 
   meta = {
     description = "PC emulator";
+    longDescription = ''
+      VirtualBox is a powerful x86 and AMD64/Intel64 virtualization product for enterprise as well as home use.
+
+      To install on NixOS, please use the option `virtualisation.virtualbox.host.enable = true`.
+      Please also check other options under `virtualisation.virtualbox`.
+    '';
     sourceProvenance = with lib.sourceTypes; [
       fromSource
       binaryNativeCode
