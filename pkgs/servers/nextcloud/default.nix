02187a59cc217d0c411ca5148c9c7935ab7f2166
Raito Bezarius <masterancpp@gmail.com>
2023-03-21 16:41:47 +0100

    nextcloud26: init at 26.0.0
    Non-breaking variant of #222372 for stable.

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -15,7 +15,8 @@ let
       inherit sha256;
     };
 
-    patches = [ (./patches + "/v${major}/0001-Setup-remove-custom-dbuser-creation-behavior.patch") ];
+    # This patch is only necessary for NC version <26.
+    patches = lib.optional (lib.versionOlder major "26") (./patches + "/v${major}/0001-Setup-remove-custom-dbuser-creation-behavior.patch");
 
     passthru.tests = nixosTests.nextcloud;
 
@@ -58,6 +59,11 @@ in {
   nextcloud25 = generic {
     version = "25.0.4";
     sha256 = "sha256-wyUeAIOpQwPi1piLNS87Mwgqeacmsw/3RnCbD+hpoaY=";
+  };
+
+  nextcloud26 = generic {
+    version = "26.0.0";
+    sha256 = "sha256-8WMVA2Ou6TZuy1zVJZv2dW7U8HPOp4tfpRXK2noNDD0=";
   };
 
   # tip: get the sha with:
