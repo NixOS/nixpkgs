f765fb1adc3f86eea09be88d20160e5d588be8c4
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-21 09:58:47 +0100

    python310Packages.py-serializable: init at 0.12.0

---
+++ pkgs/development/python-modules/py-serializable/default.nix
@@ -0,0 +1,51 @@
+{ lib
+, buildPythonPackage
+, defusedxml
+, fetchFromGitHub
+, lxml
+, poetry-core
+, pytestCheckHook
+, pythonOlder
+, xmldiff
+}:
+
+buildPythonPackage rec {
+  pname = "py-serializable";
+  version = "0.12.0";
+  format = "pyproject";
+
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "madpah";
+    repo = "serializable";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-TnO8mkRJfdTO1sA26bqh46EMes2TpLXJwpDdrvPPC9g=";
+  };
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
+
+  propagatedBuildInputs = [
+    defusedxml
+  ];
+
+  nativeCheckInputs = [
+    lxml
+    pytestCheckHook
+    xmldiff
+  ];
+
+  pythonImportsCheck = [
+    "serializable"
+  ];
+
+  meta = with lib; {
+    description = "Pythonic library to aid with serialisation and deserialisation to/from JSON and XML";
+    homepage = "https://github.com/madpah/serializable";
+    changelog = "https://github.com/madpah/serializable/blob/${version}/CHANGELOG.md";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ fab ];
+  };
+}
