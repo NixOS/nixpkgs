84644d034649d12326ddd1a7cd592557c9568e87
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-21 06:30:16 +0000

    snd: 23.1 -> 23.2

--- pkgs/applications/audio/snd/default.nix
+++ pkgs/applications/audio/snd/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "snd";
-  version = "23.1";
+  version = "23.2";
 
   src = fetchurl {
     url = "mirror://sourceforge/snd/snd-${version}.tar.gz";
-    sha256 = "sha256-zNXA/HFUCTxKzeKde/XVK239dInnxkPBmxXrlicl1fI=";
+    sha256 = "sha256-MLBFK34RgpEoK2reA+Ik35pY5YuzetDU1Wz9yAPQhEc=";
   };
 
   nativeBuildInputs = [ pkg-config ];
