3c6e8cb817311d6749df33b6a124be2147e5d85f
Sylvain Fankhauser <sephi@fhtagn.top>
2023-03-21 07:28:54 +0100

    nixos/tests/caddy2-etag: use caddy module

--- nixos/tests/caddy2-etag.nix
+++ nixos/tests/caddy2-etag.nix
@@ -2,11 +2,12 @@ import ./common/etag {
   name = "caddy2-etag";
 
   serverConfig = { config, pkgs, ... }: {
-    systemd.services.caddy2 = {
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.caddy2}/bin/caddy file-server -listen :80 -root ${config.test-support.etag.root}";
-      };
+    services.caddy = {
+      enable = true;
+      virtualHosts."server:80".extraConfig = ''
+        root * ${config.test-support.etag.root}
+        file_server
+      '';
     };
   };
 }
