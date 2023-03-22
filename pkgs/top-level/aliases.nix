ea97d50ff088aaa1169eebadccb0fbcba6ecafde
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-03-22 18:51:04 +0200

    ogre1_9: drop

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1107,6 +1107,7 @@ mapAliases ({
   ocz-ssd-guru = throw "ocz-ssd-guru has been removed due to there being no source available"; # Added 2021-07-12
   odpdown = throw "odpdown has been removed because it lacks python3 support"; # Added 2022-04-25
   ofp = throw "ofp is not compatible with odp-dpdk";
+  ogre1_9 = throw "ogre1_9 has been removed, use ogre instead"; # Added 2023-03-22
   olifant = throw "olifant has been removed from nixpkgs, as it was unmaintained"; # Added 2021-08-05
   opa = throw "opa has been removed from nixpkgs as upstream has abandoned the project"; # Added 2023-03-21
   opam_1_2 = throw "'opam_1_2' has been renamed to/replaced by 'opam'"; # Added 2023-03-08
