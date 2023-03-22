2235dd4616b3e228e9811ebc6892c08b6a97f500
Luflosi <luflosi@luflosi.de>
2023-03-22 16:42:47 +0100

    kubo: 0.18.1 -> 0.19.0
    https://github.com/ipfs/kubo/releases/tag/v0.19.0

--- pkgs/applications/networking/kubo/default.nix
+++ pkgs/applications/networking/kubo/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "kubo";
-  version = "0.18.1"; # When updating, also check if the repo version changed and adjust repoVersion below
+  version = "0.19.0"; # When updating, also check if the repo version changed and adjust repoVersion below
   rev = "v${version}";
 
   passthru.repoVersion = "13"; # Also update kubo-migrator when changing the repo version
@@ -10,7 +10,7 @@ buildGoModule rec {
   # Kubo makes changes to it's source tarball that don't match the git source.
   src = fetchurl {
     url = "https://github.com/ipfs/kubo/releases/download/${rev}/kubo-source.tar.gz";
-    hash = "sha256-xAOx4QOdD5MfFLqKTQRGMOnTZt14H523/quOLY5gBMk=";
+    hash = "sha256-PAxWijTAGOJKRDIKaulNPr/4uOJkm+vqNWAY7AjhFUw=";
   };
 
   # tarball contains multiple files/directories
