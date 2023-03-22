1d31b69cddb893e18a76908f9e57b39f344113d4
Daniel Goertzen <daniel.goertzen@gmail.com>
2023-03-22 12:43:07 -0500

    linux-kernels/linuxConfig: add kernelPatches
    Kernel patches may contain new Kconfig elements
    so they should be applied to src before generating
    a config.

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -630,6 +630,7 @@ in {
   # Derive one of the default .config files
   linuxConfig = {
     src,
+    kernelPatches ? [],
     version ? (builtins.parseDrvName src.name).version,
     makeTarget ? "defconfig",
     name ? "kernel.config",
@@ -637,6 +638,7 @@ in {
     inherit name src;
     depsBuildBuild = [ buildPackages.stdenv.cc ]
       ++ lib.optionals (lib.versionAtLeast version "4.16") [ buildPackages.bison buildPackages.flex ];
+    patches = map (p: p.patch) kernelPatches;  # Patches may include new configs.
     postPatch = ''
       patchShebangs scripts/
     '';
