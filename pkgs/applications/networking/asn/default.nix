0c8f5d0619ff4c3de7b45e6fcf72e6475eb199ef
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-22 02:46:28 +0000

    asn: 0.72.1 -> 0.73

--- pkgs/applications/networking/asn/default.nix
+++ pkgs/applications/networking/asn/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation rec {
   pname = "asn";
-  version = "0.72.1";
+  version = "0.73";
 
   src = fetchFromGitHub {
     owner = "nitefood";
     repo = "asn";
     rev = "v${version}";
-    sha256 = "sha256-2JGc1PhvmuZFT6c6UcxHKttHqWF+NsekqnYtdGKgU2U=";
+    sha256 = "sha256-z0vjYU6k+6p/lASqOase91aqkUvmuy8sUBjsq2OPnxE=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
