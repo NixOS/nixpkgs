ab43bd9808013b2b05dd98667076d8ffbc46589a
Someone Serge <sergei.kozlukov@aalto.fi>
2023-03-23 20:35:01 +0200

    python3Packages.torchinfo: 1.64 -> 1.7.2
    catch up with pytorch 2.0.0 and updated interfaces

--- pkgs/development/python-modules/torchinfo/default.nix
+++ pkgs/development/python-modules/torchinfo/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "torchinfo";
-  version = "1.64";
+  version = "1.7.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "TylerYep";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-gcl8RxCD017FP4LtB60WVtOh7jg2Otv/vNd9hKneEAU=";
+    hash = "sha256-O+I7BNQ5moV/ZcbbuP/IFoi0LO0WsGHBbSfgPmFu1Ec=";
   };
 
   propagatedBuildInputs = [
@@ -35,6 +35,11 @@ buildPythonPackage rec {
     "test_eval_order_doesnt_matter"
     # AssertionError in output
     "test_google"
+  ];
+
+  disabledTestPaths = [
+    # Wants "compressai", which we don't package (2023-03-23)
+    "tests/torchinfo_xl_test.py"
   ];
 
   pythonImportsCheck = [
