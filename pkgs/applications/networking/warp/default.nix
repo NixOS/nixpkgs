72e4261858e233b977be7e1f199a3d774df7e533
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-03-23 10:02:51 +0800

    warp: 0.4 -> 0.5.1

--- pkgs/applications/networking/warp/default.nix
+++ pkgs/applications/networking/warp/default.nix
@@ -17,14 +17,14 @@
 
 stdenv.mkDerivation rec {
   pname = "warp";
-  version = "0.4";
+  version = "0.5.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "World";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-c8X0kedfM8DPTEQAbh8cXIfEvxG2cdUD3twVHs0/k7U";
+    hash = "sha256-SZL5OHg6nnEuyamrLT37hX0o4UUFldXdGHl4R3pO1bQ=";
   };
 
   postPatch = ''
@@ -34,7 +34,7 @@ stdenv.mkDerivation rec {
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-TS/Q6T3bPjkk1bfINTR6bcPy8fnbAxbKJUrx2pYsPWY=";
+    hash = "sha256-Pjzw0R6O489akpzlF7MCFsOQmmaY4MkiFMPmseH5QwA=";
   };
 
   nativeBuildInputs = [
