6c4f7960cda8f54663bdaf4f6bd37c4696f2b861
Azat Bahawi <azat@bahawi.net>
2023-03-23 21:35:34 +0300

    grafanaPlugins.redis-app: init at 2.2.1

--- pkgs/servers/monitoring/grafana/plugins/plugins.nix
+++ pkgs/servers/monitoring/grafana/plugins/plugins.nix
@@ -10,5 +10,6 @@
   grafana-piechart-panel = callPackage ./grafana-piechart-panel { };
   grafana-polystat-panel = callPackage ./grafana-polystat-panel { };
   grafana-worldmap-panel = callPackage ./grafana-worldmap-panel { };
+  redis-app = callPackage ./redis-app { };
   redis-datasource = callPackage ./redis-datasource { };
 }
