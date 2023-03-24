87e5d2c6462974816610cad3fbc8753bf3263d06
R. Ryantm <ryantm-bot@ryantm.com>
2023-03-24 01:34:34 +0000

    pritunl-client: 1.3.3467.51 -> 1.3.3474.95

--- pkgs/tools/networking/pritunl-client/default.nix
+++ pkgs/tools/networking/pritunl-client/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "pritunl-client";
-  version = "1.3.3467.51";
+  version = "1.3.3474.95";
 
   src = fetchFromGitHub {
     owner = "pritunl";
     repo = "pritunl-client-electron";
     rev = version;
-    sha256 = "sha256-US1A3PjWxycXzOW/1Mi8GVkxvH6xCFq4mo5gU49vytU=";
+    sha256 = "sha256-7V+hw/DfyRooef9gwZ5HjcfvabtA2HltJhwwSrQH0X4=";
   };
 
   modRoot = "cli";
