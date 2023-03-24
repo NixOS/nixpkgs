60c727df781ce8f2046f1af3f05a628e0851e4d2
Raito Bezarius <masterancpp@gmail.com>
2023-03-24 21:13:56 +0100

    zfs: latest compatible kernel is 6.1 rather than 6.2

--- pkgs/os-specific/linux/zfs/default.nix
+++ pkgs/os-specific/linux/zfs/default.nix
@@ -235,16 +235,11 @@ in {
   zfsUnstable = common {
     # check the release notes for compatible kernels
     # NOTE:
-    #   zfs-2.1.9<=x<=2.1.10 is broken with aarch64-linux-6.2
+    #   zfs-2.1.9<=x<=2.1.10 is broken with aarch64-linux-6.2 and x86_64-linux-6.2
+    #   https://github.com/openzfs/zfs/issues/14658
     #   for future releases, please delete this condition.
-    kernelCompatible =
-      if stdenv'.isx86_64
-      then kernel.kernelOlder "6.3"
-      else kernel.kernelOlder "6.2";
-    latestCompatibleLinuxPackages =
-      if stdenv'.isx86_64
-      then linuxPackages_6_2
-      else linuxPackages_6_1;
+    kernelCompatible = kernel.kernelOlder "6.2";
+    latestCompatibleLinuxPackages = linuxPackages_6_1;
 
     # this package should point to a version / git revision compatible with the latest kernel release
     # IMPORTANT: Always use a tagged release candidate or commits from the
