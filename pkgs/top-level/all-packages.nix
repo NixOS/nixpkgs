c088302400a66a3abeb62cfccd6f469a0a43de44
traxys <quentin+dev@familleboyer.net>
2023-03-25 15:17:13 +0100

    sca2d: init at 0.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18758,6 +18758,8 @@ with pkgs;
 
   semantik = libsForQt5.callPackage ../applications/office/semantik { };
 
+  sca2d = callPackage ../development/tools/sca2d {  };
+
   sconsPackages = dontRecurseIntoAttrs (callPackage ../development/tools/build-managers/scons { });
   scons = sconsPackages.scons_4_1_0;
 
