38ceedbbec59746cf66bed10625867c9737fbea6
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-25 23:02:13 +0100

    python310Packages.pynuki: add changelog to meta

--- pkgs/development/python-modules/pynuki/default.nix
+++ pkgs/development/python-modules/pynuki/default.nix
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "pschmitt";
     repo = pname;
-    rev = version;
+    rev = "refs/tags/${version}";
     hash = "sha256-I0eAhgThSBEmJF6mYw+0Bh1kCUqEMFnCx+4n7l3Hf14=";
   };
 
@@ -40,6 +40,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python bindings for nuki.io bridges";
     homepage = "https://github.com/pschmitt/pynuki";
+    changelog = "https://github.com/pschmitt/pynuki/releases/tag/${version}";
     license = with licenses; [ gpl3Only ];
     maintainers = with maintainers; [ fab ];
   };
