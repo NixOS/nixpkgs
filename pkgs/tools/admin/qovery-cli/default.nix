74e02366778e816e6f3981c9d4a625ebf25f1e54
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-25 17:54:18 +0100

    qovery-cli: 0.52.2 -> 0.53.1
    Diff: https://github.com/Qovery/qovery-cli/compare/v0.52.2...v0.53.1
    
    Changelog: https://github.com/Qovery/qovery-cli/releases/tag/v0.53.1

--- pkgs/tools/admin/qovery-cli/default.nix
+++ pkgs/tools/admin/qovery-cli/default.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "qovery-cli";
-  version = "0.52.2";
+  version = "0.53.1";
 
   src = fetchFromGitHub {
     owner = "Qovery";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-vZBNoDh/QnrTQrDubocA3FL7Cn5M1n/w+hE99GF9QC4=";
+    hash = "sha256-vxCDb4qiq7/19wvtWQRCzGxUz+FvWyeKSQIpsiUtwGs=";
   };
 
   vendorHash = "sha256-V7yPXSN+3H8NkD384MkvKbymNQ/O2Q9HoMO4M8mzVto=";
