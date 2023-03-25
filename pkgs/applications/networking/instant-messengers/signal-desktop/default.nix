f14564a4d47aebb0da77dc1b8870f8d9b009c59d
Eduardo Quiros <eduardo@eduardoquiros.com>
2023-03-24 19:03:46 -0600

    signal-desktop: 6.10.1 -> 6.12.0, signal-desktop-beta: 6.11.0-beta.2 -> 6.13.0-beta.1

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -1,12 +1,12 @@
 { callPackage }: builtins.mapAttrs (pname: attrs: callPackage ./generic.nix (attrs // { inherit pname; })) {
   signal-desktop = {
     dir = "Signal";
-    version = "6.10.1";
-    hash = "sha256-uWwRgP9iYirZU9x3QtS5lRGI7vLOOtX4B4fgVuYxkho=";
+    version = "6.12.0";
+    hash = "sha256-nhUrxwcE53QG2akSHCFKJR62ydwZ7d4LI+l4o1bgKJo=";
   };
   signal-desktop-beta = {
     dir = "Signal Beta";
-    version = "6.11.0-beta.2";
-    hash = "sha256-tw8VsPC0shKIN13ICD0PVKhKxA7rdj16r2lP2UEJGsY=";
+    version = "6.13.0-beta.1";
+    hash = "sha256-An76VcyuDvTVgXL8sVx0El5EognReiiUBTbRxNmMOrQ=";
   };
 }
