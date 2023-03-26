dc817e47cf2f8281a4155419ac08817a6a7fec39
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-03-26 23:55:56 +0200

    python310Packages.limits: 3.2.0 -> 3.5.0

--- pkgs/development/python-modules/limits/default.nix
+++ pkgs/development/python-modules/limits/default.nix
@@ -4,6 +4,7 @@
 , fetchFromGitHub
 , etcd3
 , hiro
+, importlib-resources
 , packaging
 , pymemcache
 , pymongo
@@ -18,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "limits";
-  version = "3.2.0";
+  version = "3.5.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -33,11 +34,12 @@ buildPythonPackage rec {
     postFetch = ''
       rm "$out/limits/_version.py"
     '';
-    hash = "sha256-zMU2MU7MFTWSig2j1PaBLPtKM5/7gNkFajKXw3A+fIQ=";
+    hash = "sha256-O4yENynvon9xM8F/r0NMSpSh6Hl2EoTcXgldrwzo24M=";
   };
 
   propagatedBuildInputs = [
     deprecated
+    importlib-resources
     packaging
     setuptools
     typing-extensions
