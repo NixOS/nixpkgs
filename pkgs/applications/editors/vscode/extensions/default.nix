9eb3f1444b7f0af0df2101a581ce8dd8fba8b197
Joscha <joscha@plugh.de>
2023-03-26 23:17:13 +0200

    vscode-extensions.thenuprojectcontributors.vscode-nushell-lang: 0.8.0 -> 1.1.0

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -2778,8 +2778,8 @@ let
         mktplcRef = {
           name = "vscode-nushell-lang";
           publisher = "thenuprojectcontributors";
-          version = "0.8.0";
-          sha256 = "0065ckgpsalqgv9zw8gvxxkqzwl7mjp6mydnlm1m3y9qxlfl010s";
+          version = "1.1.0";
+          sha256 = "sha256-7v4q0OEqv7q2ejHp4lph2Dsqg0GWE65pxyz9goQEm8g=";
         };
         meta.license = lib.licenses.mit;
       };
