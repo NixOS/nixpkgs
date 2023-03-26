53feaedd8b567d52fea0239ff95eb312424ae067
Ran Xiao <ben.xiao@me.com>
2023-03-27 09:54:12 +1100

    python3.pkgs.mmcv: init at 1.7.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6078,6 +6078,8 @@ self: super: with self; {
     enablePython = true;
   });
 
+  mmcv = callPackage ../development/python-modules/mmcv { };
+
   mmh3 = callPackage ../development/python-modules/mmh3 { };
 
   mmpython = callPackage ../development/python-modules/mmpython { };
