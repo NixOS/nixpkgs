a777753eb26c96144d58f899b5893509a87b6f5a
Lily Foster <lily@lily.flowers>
2023-03-27 17:41:32 -0400

    fetchYarnDeps: add more git url tests

--- pkgs/build-support/node/fetch-yarn-deps/tests/default.nix
+++ pkgs/build-support/node/fetch-yarn-deps/tests/default.nix
@@ -13,4 +13,8 @@
     yarnLock = ./github.lock;
     sha256 = "sha256-DIKrhDKoqm7tHZmcuh9eK9VTqp6BxeW0zqDUpY4F57A=";
   };
+  gitUrlDep = testers.invalidateFetcherByDrvHash fetchYarnDeps {
+    yarnLock = ./giturl.lock;
+    sha256 = "sha256-VPnyqN6lePQZGXwR7VhbFnP7/0/LB621RZwT1F+KzVQ=";
+  };
 }
