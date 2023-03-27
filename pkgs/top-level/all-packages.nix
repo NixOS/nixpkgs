5d2cc163496d61c500166050cac4f988d0be7a7e
Matthieu Coudron <mcoudron@hotmail.com>
2023-03-27 15:07:44 +0200

    playwright-driver: init at 1.27.1
    it was actually moved from
    pkgs/development/python-modules/playwright/default.nix to its own
    pkgs/development/web/playwright/driver.nix .
    
    I am trying to package the typescript version of playwright and the
    browsers are needed there, it's more convenient to split them away from the python module.
    
    Careful playwright.browsers is not accessible anymore.
    
    Apply suggestions from code review
    
    Co-authored-by: Yannik Rödel <hey@yannik.info>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11134,6 +11134,8 @@ with pkgs;
 
   playwright = with python3Packages; toPythonApplication playwright;
 
+  playwright-driver = callPackage ../development/web/playwright/driver.nix { };
+
   please = callPackage ../tools/security/please { };
 
   plecost = callPackage ../tools/security/plecost { };
