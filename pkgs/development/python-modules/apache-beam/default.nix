NixOS/nixpkgs

commit f8264a5b1e7832d93d8a22816f5133ff891724b6

tree-wide: mark broken packages as such
Found while changing ffmpeg from 4 to 5; these all depend on ffmpeg either
directly or transitively.

pkgs/development/python-modules/apache-beam/default.nix
@@ -207,5 +207,6 @@ buildPythonPackage rec {
     homepage = "https://beam.apache.org/";
     license = licenses.asl20;
     maintainers = with maintainers; [ ndl ];
+    broken = true;
   };
 }

pkgs/applications/blockchains/bitcoin-unlimited/default.nix
@@ -58,7 +58,7 @@ stdenv.mkDerivation rec {
     homepage = "https://www.bitcoinunlimited.info/";
     maintainers = with maintainers; [ DmitryTsygankov ];
     license = licenses.mit;
-    broken = stdenv.isDarwin;
+    broken = true;
     platforms = platforms.unix;
   };
 }

pkgs/applications/blockchains/dogecoin/default.nix
@@ -54,5 +54,6 @@ stdenv.mkDerivation rec {
     license = licenses.mit;
     maintainers = with maintainers; [ edwtjo offline ];
     platforms = platforms.unix;
+    broken = true;
   };
 }

pkgs/applications/blockchains/pivx/default.nix
@@ -64,7 +64,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    broken = (stdenv.isLinux && stdenv.isAarch64) || stdenv.isDarwin;
+    broken = true;
     description = "An open source crypto-currency focused on fast private transactions";
     longDescription = ''
       PIVX is an MIT licensed, open source, blockchain-based cryptocurrency with

pkgs/applications/graphics/fluxus/default.nix
@@ -72,5 +72,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2;
     homepage = "http://www.pawfal.org/fluxus/";
     maintainers = [ maintainers.brainrape ];
+    broken = true;
   };
 }

pkgs/applications/graphics/gnome-decoder/default.nix
@@ -78,5 +78,6 @@ clangStdenv.mkDerivation rec {
     platforms = platforms.linux;
     mainProgram = "decoder";
     maintainers = with maintainers; [ zendo ];
+    broken = true;
   };
 }

pkgs/applications/terminal-emulators/syncterm/default.nix
@@ -32,7 +32,8 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     # error: unsupported option '-fsanitize=safe-stack' for target 'x86_64-apple-darwin'
-    broken = (stdenv.isLinux && stdenv.isAarch64) || stdenv.isDarwin;
+    # broken = (stdenv.isLinux && stdenv.isAarch64) || stdenv.isDarwin;
+    broken = true; # sendmsg.c:(.text+0x1099): undefined reference to `pthread_yield'
     homepage = "https://syncterm.bbsdev.net/";
     description = "BBS terminal emulator";
     maintainers = with maintainers; [ embr ];

pkgs/applications/video/rtabmap/default.nix
@@ -60,5 +60,6 @@ stdenv.mkDerivation rec {
     license = licenses.bsd3;
     maintainers = with maintainers; [ ckie ];
     platforms = with platforms; linux;
+    broken = true;
   };
 }

commit 821e8d088453fac66705967b0aebda37aea61ea7

python3Packages.apache-beam: fix build
Package was recently marked broken by https://github.com/NixOS/nixpkgs/commit/f8264a5b1e7832d93d8a22816f5133ff891724b6.

pkgs/development/python-modules/apache-beam/default.nix
@@ -200,6 +200,8 @@ buildPythonPackage rec {
     "test_get_output_batch_type"
     "test_pformat_namedtuple_with_unnamed_fields"
     "test_row_coder_fail_early_bad_schema"
+    # See https://github.com/apache/beam/issues/26004.
+    "test_batch_encode_decode"
   ];
 
   meta = with lib; {
@@ -207,6 +209,5 @@ buildPythonPackage rec {
     homepage = "https://beam.apache.org/";
     license = licenses.asl20;
     maintainers = with maintainers; [ ndl ];
-    broken = true;
   };
 }