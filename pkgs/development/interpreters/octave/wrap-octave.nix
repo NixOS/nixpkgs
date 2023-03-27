1825e29d1c3d6f1f0306e740c417ccd8ad3399ef
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-03-27 20:55:21 +0200

    trivial-builders: move into the directory named alike
    While searching for something different I wondered why there is a
    trivial-builders.nix file next to the trivial-builders directory where
    only tests live. Lets fix that.

--- pkgs/development/interpreters/octave/wrap-octave.nix
+++ pkgs/development/interpreters/octave/wrap-octave.nix
@@ -1,10 +1,9 @@
-{ lib
-, octave
+{ octave
 , makeSetupHook
 , makeWrapper
 }:
 
-# Defined in trivial-builders.nix
+# Defined in trivial-builders
 # Imported as wrapOctave in octave/default.nix and passed to octave's buildEnv
 # as nativeBuildInput
 # Each of the substitutions is available in the wrap.sh script as @thingSubstituted@
