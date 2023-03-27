a3983c8856895190bcfc5f6b59f2f279059de1a3
Yureka <yuka@yuka.dev>
2023-03-27 18:08:55 +0200

    gitlab: 15.9.3 -> 15.10.0 (#223367)
    https://about.gitlab.com/releases/2023/03/22/gitlab-15-10-released/

--- pkgs/applications/version-management/gitlab/gitlab-shell/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-shell/default.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "gitlab-shell";
-  version = "14.17.0";
+  version = "14.18.0";
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-shell";
     rev = "v${version}";
-    sha256 = "sha256-GTy+1O6Z7i+L8SSSQpYnN6SRfqDutkJYrj66tpX4f5M=";
+    sha256 = "sha256-dMxWnv+YfoDy9rhuCx+JIxFyjHejttkkqkQ4owdI/4g=";
   };
 
   buildInputs = [ ruby libkrb5 ];
 
   patches = [ ./remove-hardcoded-locations.patch ];
 
-  vendorSha256 = "sha256-mFGkT5lyQfcaGz503vgqx/kP5S0V2Ks5HDmjx+BxZzg=";
+  vendorSha256 = "sha256-zqZMZvYteOWTgDnlX8H1i8e/QTbAoTPD6ZNsHsCcLdM=";
 
   postInstall = ''
     cp -r "$NIX_BUILD_TOP/source"/bin/* $out/bin
--- pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
@@ -5,7 +5,7 @@ in
 buildGoModule rec {
   pname = "gitlab-workhorse";
 
-  version = "15.9.3";
+  version = "15.10.0";
 
   src = fetchFromGitLab {
     owner = data.owner;
@@ -16,7 +16,7 @@ buildGoModule rec {
 
   sourceRoot = "source/workhorse";
 
-  vendorSha256 = "sha256-wvoMBzRGp8RX2kHKKz3REDq8YLTsHbDayEMYi7hSzTE=";
+  vendorSha256 = "sha256-HG/QobU1LsFhbNF4lNq9tLwjcLD2l3TXnsJ+tjqc0Q8=";
   buildInputs = [ git ];
   ldflags = [ "-X main.Version=${version}" ];
   doCheck = false;
