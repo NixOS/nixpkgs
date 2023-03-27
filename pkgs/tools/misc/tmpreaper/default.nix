3516b935f553f16f725668ff2ebb88f13e212e6e
mar-jac <marjac@meta.com>
2023-03-27 09:11:32 -0400

    Update default.nix

--- pkgs/tools/misc/tmpreaper/default.nix
+++ pkgs/tools/misc/tmpreaper/default.nix
@@ -1,27 +1,12 @@
-{ stdenv, fetchurl, autoconf, automake }:
+{ sstdenv, fetchurl, bash, findutils, gawk, grep, sed, tar }:
 
-let
-  inherit (stdenv.lib) optional;
-  sha256-default = "1ca94d156eb68160ec9b6ed8b97d70fbee996de21437f0cf7d0c3b46709fecbc";
-in
+
 stdenv.mkDerivation rec {
   pname = "tmpreaper";
   version = "1.6.17";
-
-  buildInputs = [ autoconf automake ];
-
-  installPhase = ''
-    mkdir -p $out/sbin
-    cp tmpreaper $out/sbin
-    mkdir -p $out/share/man/man8
-    cp tmpreaper.8 $out/share/man/man8
-  '';
-
-  doCheck = false;
-
   src = fetchurl {
     url = "https://deb.debian.org/debian/pool/main/t/tmpreaper/tmpreaper_${version}.tar.gz";
-    sha256 = sha256-default;
+    sha256 = "1ca94d156eb68160ec9b6ed8b97d70fbee996de21437f0cf7d0c3b46709fecbc";;
   };
 
   meta = {
@@ -31,4 +16,5 @@ stdenv.mkDerivation rec {
     license = "GPL-2.0-only";
   };
 
+  buildInputs = [ bash findutils gawk grep sed tar ];
 }
