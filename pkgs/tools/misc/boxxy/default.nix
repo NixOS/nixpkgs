cf3cecaba54be32ce56ccd25fac2e6da44649dcd
figsoda <figsoda@pm.me>
2023-03-27 23:37:08 -0400

    boxxy: 0.5.1 -> 0.6.0
    Diff: https://github.com/queer/boxxy/compare/v0.5.1...v0.6.0

--- pkgs/tools/misc/boxxy/default.nix
+++ pkgs/tools/misc/boxxy/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "boxxy";
-  version = "0.5.1";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "queer";
     repo = "boxxy";
     rev = "v${version}";
-    hash = "sha256-e6AkxC3TP2StApPSWaadLz/AFmaKBKCrS5HuocvmfQs=";
+    hash = "sha256-05bGVtjbyd9e/Ega/Lv1Yy4gVDlJfygxD1g8bu5FuWY=";
   };
 
-  cargoHash = "sha256-E3IX3JSc8Rq8Qnx6Lk+PQgGfHptidxPaOqejYJgudlw=";
+  cargoHash = "sha256-uzgMqfQCUzOOYCirLWsH2xm02T0UZXcMKCMmM45AkBg=";
 
   meta = with lib; {
     description = "Puts bad Linux applications in a box with only their files";
