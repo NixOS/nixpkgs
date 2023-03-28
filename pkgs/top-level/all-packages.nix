f8dca33e50eda25b56d9d4648cc8e901ffa5909c
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-28 14:44:27 +0800

    onagre: init at 1.0.0-alpha.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25147,6 +25147,8 @@ with pkgs;
 
   olaris-server = callPackage ../servers/olaris { };
 
+  onagre = callPackage ../applications/misc/onagre { };
+
   onlyoffice-documentserver = callPackage ../servers/onlyoffice-documentserver { };
 
   outline = callPackage ../servers/web-apps/outline (lib.fix (super: {
