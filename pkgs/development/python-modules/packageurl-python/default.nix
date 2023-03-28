50a63ac98d1063e57dff5f7d743ce546777431d9
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-28 08:41:10 +0200

    python310Packages.packageurl-python: add changelog to meta

--- pkgs/development/python-modules/packageurl-python/default.nix
+++ pkgs/development/python-modules/packageurl-python/default.nix
@@ -28,6 +28,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python parser and builder for package URLs";
     homepage = "https://github.com/package-url/packageurl-python";
+    changelog = "https://github.com/package-url/packageurl-python/blob/v${version}/CHANGELOG.rst";
     license = licenses.mit;
     maintainers = with maintainers; [ armijnhemel ];
   };
