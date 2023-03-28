ff348a29304aa3d03fece32da892f6b1702eeca1
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-03-28 00:48:47 -0300

    Apply suggestions from code review
    Co-authored-by: figsoda <figsoda@pm.me>

--- pkgs/tools/misc/svu/default.nix
+++ pkgs/tools/misc/svu/default.nix
@@ -13,8 +13,12 @@ buildGoModule rec {
 
   vendorHash = "sha256-+e1oL08KvBSNaRepGR2SBBrEDJaGxl5V9rOBysGEfQs=";
 
-  ldflags =
-    [ "-s" "-w" "-X main.version=${version}" "-X main.builtBy=nixpkgs" ];
+  ldflags = [ "-s" "-w" "-X=main.version=${version}" "-X=main.builtBy=nixpkgs" ];
+
+  # test assumes source directory to be a git repository
+  postPatch = ''
+    rm internal/git/git_test.go
+  '';
 
   passthru.tests.version = testers.testVersion { package = svu; };
 
