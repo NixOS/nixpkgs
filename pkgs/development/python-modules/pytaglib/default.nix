8066817ecc0d44859733883c52d82fa5175137bb
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-28 08:38:49 +0200

    python310Packages.pytaglib: disable on unsupported Python releases
    - add format

--- pkgs/development/python-modules/pytaglib/default.nix
+++ pkgs/development/python-modules/pytaglib/default.nix
@@ -4,11 +4,15 @@
 , taglib
 , cython
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pytaglib";
   version = "2.0.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "supermihi";
