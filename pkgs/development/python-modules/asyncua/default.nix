025027cb010bdeeb0ca070d222857a5a8abc3d82
Fabian Affolter <mail@fabian-affolter.ch>
2023-03-28 09:31:32 +0200

    python310Packages.asyncua: remove asynctest

--- pkgs/development/python-modules/asyncua/default.nix
+++ pkgs/development/python-modules/asyncua/default.nix
@@ -1,19 +1,17 @@
 { lib
-, buildPythonPackage
-, fetchFromGitHub
-, aiosqlite
 , aiofiles
-, pytz
-, python-dateutil
-, sortedcontainers
+, aiosqlite
+, buildPythonPackage
 , cryptography
-, typing-extensions
-, importlib-metadata
-, pytestCheckHook
+, fetchFromGitHub
 , pytest-asyncio
 , pytest-mock
-, asynctest
+, pytestCheckHook
+, python-dateutil
 , pythonOlder
+, pytz
+, sortedcontainers
+, typing-extensions
 }:
 
 buildPythonPackage rec {
@@ -21,7 +19,7 @@ buildPythonPackage rec {
   version = "1.0.1";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "FreeOpcUa";
@@ -29,6 +27,12 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-6A4z+tiQ2oUlB9t44wlW64j5sjWFMAgqT3Xt0FdJCBs=";
   };
+
+  postPatch = ''
+    # https://github.com/FreeOpcUa/opcua-asyncio/issues/1263
+    substituteInPlace setup.py \
+      --replace ", 'asynctest'" ""
+  '';
 
   propagatedBuildInputs = [
     aiosqlite
@@ -38,23 +42,21 @@ buildPythonPackage rec {
     sortedcontainers
     cryptography
     typing-extensions
-  ] ++ lib.optionals (pythonOlder "3.8") [
-    importlib-metadata
-  ];
-
-  pythonImportsCheck = [
-    "asyncua"
   ];
 
   nativeCheckInputs = [
     pytestCheckHook
     pytest-asyncio
     pytest-mock
-    asynctest
+  ];
+
+  pythonImportsCheck = [
+    "asyncua"
   ];
 
   disabledTests = [
-    "test_cli_tools_which_require_sigint" # Hard coded path only works from root of src
+    # Hard coded path only works from root of src
+    "test_cli_tools_which_require_sigint"
   ];
 
   meta = with lib; {
