5390c9bfc9d8c615fc70f8937e02f4dda3311b7f
Ionut Nechita <ionut_n2001@yahoo.com>
2023-03-29 08:44:19 +0300

    maintainers: add ionutnechita
    Description:
     - added ionutnechita in list of maintainers
    
    Change-Id: Ide414ebb8b940c299498c62c24efea0965f02a3a
    Signed-off-by: Ionut Nechita ionut_n2001@yahoo.com

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6587,6 +6587,12 @@
     githubId = 54999;
     name = "Ariel Nunez";
   };
+  ionutnechita = {
+    email = "ionut_n2001@yahoo.com";
+    github = "ionutnechita";
+    githubId = 9405900;
+    name = "Ionut Nechita";
+  };
   iopq = {
     email = "iop_jr@yahoo.com";
     github = "iopq";
