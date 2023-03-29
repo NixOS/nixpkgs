83cb871d8b40a78c4b4a541fc8c69056b77d96e4
Anthony Roussel <anthony@roussel.dev>
2023-03-29 22:18:28 +0200

    bundler: 2.4.9 -> 2.4.10

--- pkgs/development/ruby-modules/bundler/default.nix
+++ pkgs/development/ruby-modules/bundler/default.nix
@@ -4,8 +4,8 @@ buildRubyGem rec {
   inherit ruby;
   name = "${gemName}-${version}";
   gemName = "bundler";
-  version = "2.4.9";
-  source.sha256 = "sha256-biRQBhoRrRwbkxVR7Se6YOnqzuFI4l4dc6Z92JRFhNM=";
+  version = "2.4.10";
+  source.sha256 = "sha256-uYBvqUQGOmqGdqj57Ux8d2o2w7yC8mxXYIZ6AoW0oSE=";
   dontPatchShebangs = true;
 
   postFixup = ''
