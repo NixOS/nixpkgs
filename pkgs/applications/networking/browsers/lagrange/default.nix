21d1721fd42b484432983ad5d6db9c60c6d062fd
Nikolay Korotkiy <sikmir@disroot.org>
2023-03-30 23:44:42 +0400

    lagrange: 1.15.6 → 1.15.7

--- pkgs/applications/networking/browsers/lagrange/default.nix
+++ pkgs/applications/networking/browsers/lagrange/default.nix
@@ -1,7 +1,6 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, fetchpatch
 , nix-update-script
 , cmake
 , pkg-config
@@ -18,22 +17,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lagrange";
-  version = "1.15.6";
+  version = "1.15.7";
 
   src = fetchFromGitHub {
     owner = "skyjake";
     repo = "lagrange";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-V9zrwSAflatGcN5cOOzHyyW73FN3rU+l5xUlPwy8Huk=";
+    hash = "sha256-p6L2Ak8QaRsSs2/Xjbbod9UFy79vjO5C+YcArofe2Ck=";
   };
-
-  patches = [
-    # https://github.com/skyjake/lagrange/issues/589
-    (fetchpatch {
-      url = "https://github.com/skyjake/lagrange/commit/e8a4dad6930d16aa0811d04d06432cd6b59b472e.patch";
-      hash = "sha256-60YPmZPalnoo9AjwqKpswHkKAM/hKSIOapgPwSi4Qzk=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake pkg-config zip ];
 
