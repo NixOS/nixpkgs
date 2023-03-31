a56b0af5ff980f795b223bac9a03dcf8460ca5aa
Maurice Zhou <jasper@apvc.uk>
2023-03-31 08:04:22 +0200

    maintainers: add ne9z
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10678,6 +10678,12 @@
     githubId = 137805;
     name = "Alexander Tsvyashchenko";
   };
+  ne9z = {
+    email = "yuchen@apvc.uk";
+    github = "ne9z";
+    githubId = 77314501;
+    name = "Maurice Zhou";
+  };
   nebulka = {
     email = "arapun@proton.me";
     github = "nebulka1";
