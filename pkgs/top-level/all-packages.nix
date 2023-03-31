af0fc32127e229f35b235e5bd73d5fd34e6b8a5e
Artturin <Artturin@artturin.com>
2023-03-31 21:21:53 +0300

    perlPackages: use makeScopeWithSplicing

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24451,7 +24451,7 @@ with pkgs;
 
   ### DEVELOPMENT / PERL MODULES
 
-  perlInterpreters = callPackages ../development/interpreters/perl { };
+  perlInterpreters = import ../development/interpreters/perl { inherit callPackage; };
   inherit (perlInterpreters) perl534 perl536 perldevel;
 
   perl534Packages = recurseIntoAttrs perl534.pkgs;
