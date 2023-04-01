cfc3d04a275b41b84292c77bd1a9d3eee27897da
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-01 17:53:11 +0200

    python310Packages.ansible-doctor: 2.0.2 -> 2.0.3
    Changelog: https://github.com/thegeeklab/ansible-doctor/releases/tag/v2.0.3

--- pkgs/development/python-modules/ansible-doctor/default.nix
+++ pkgs/development/python-modules/ansible-doctor/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "ansible-doctor";
-  version = "2.0.2";
+  version = "2.0.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "thegeeklab";
     repo = "ansible-doctor";
     rev = "refs/tags/v${version}";
-    hash = "sha256-hbHQbYc/cOqbeubAMa0J+UtI00jtyG/WUBe0xcSaGSI=";
+    hash = "sha256-rhXhz6aUQ9hw83cfHmOLXMyyLQc7pSCGgurFhuglKjU=";
   };
 
   pythonRelaxDeps = true;
