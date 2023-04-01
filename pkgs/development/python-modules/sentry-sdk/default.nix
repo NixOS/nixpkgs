c7abb7924daaa05fe02bf3f2052bc167a0b118fa
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-01 15:03:09 +0200

    python310Packages.sentry-sdk: 1.16.0 -> 1.17.0
    Diff: https://github.com/getsentry/sentry-python/compare/refs/tags/1.16.0...1.17.0
    
    Changelog: https://github.com/getsentry/sentry-python/blob/1.17.0/CHANGELOG.md

--- pkgs/development/python-modules/sentry-sdk/default.nix
+++ pkgs/development/python-modules/sentry-sdk/default.nix
@@ -40,7 +40,7 @@
 
 buildPythonPackage rec {
   pname = "sentry-sdk";
-  version = "1.16.0";
+  version = "1.17.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -49,7 +49,7 @@ buildPythonPackage rec {
     owner = "getsentry";
     repo = "sentry-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-hJ6OikRro9YUEX8rqMs/JkTvM9aTabEj4E8iNQ71gEc=";
+    hash = "sha256-BcoCYyCz4tkb75P/z+eGQolRB0qxrdvTH+wV90zN884=";
   };
 
   propagatedBuildInputs = [
