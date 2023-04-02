d104ffce410f664b123d590163f544d6c15132cd
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-02 19:36:46 +0000

    automatic-timezoned: 1.0.78 -> 1.0.81

--- pkgs/tools/system/automatic-timezoned/default.nix
+++ pkgs/tools/system/automatic-timezoned/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "automatic-timezoned";
-  version = "1.0.78";
+  version = "1.0.81";
 
   src = fetchFromGitHub {
     owner = "maxbrunet";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-dYRg2WhPpNL20IrpWK2KuzVSBd/8Mq+CHvtp/RRAQqA=";
+    sha256 = "sha256-i+l+nYfGUXzpbqINWsX2U/CHQ7sMvZyvrLakItyLE6I=";
   };
 
-  cargoHash = "sha256-5x8DXojoPN4qFxGWrtD2ieSkkwmnKdw85JsnpvmFxRM=";
+  cargoHash = "sha256-wRzi/efAfC6PGJFATB4EQjyTRkxsn5wBiY2DJom4npY=";
 
   meta = with lib; {
     description = "Automatically update system timezone based on location";
