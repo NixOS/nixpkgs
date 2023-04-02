842d01c0c169fc45b1437328ec3d7a8648ca74ad
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-04-02 11:13:22 -0400

    emptty: add compilation options to package inputs

--- pkgs/applications/display-managers/emptty/default.nix
+++ pkgs/applications/display-managers/emptty/default.nix
@@ -3,9 +3,13 @@
 , lib
 , libX11
 , pam
+, pam_u2f
 , stdenv
+, wayland
+, noX11 ? false
+, noU2f ? false
+,
 }:
-
 buildGoModule rec {
   pname = "emptty";
   version = "0.9.1";
@@ -13,13 +17,39 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "tvrzna";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-CbTPJgnKMWMXdG6Hr8xT9ae4Q9MxAfhITn5WSCzCmI4=";
+    rev = "18de1cefcbff00e3468abe5573a2af2d848e7553";
+    sha256 = "0jzhcbw1ckpsmmxm2g51lvkspsrl7lkn9xfifkk97mvm22qs4p49";
   };
 
-  buildInputs = [ pam libX11 ];
+  buildInputs =
+    [ wayland pam ]
+    ++ (lib.lists.optionals (!noX11) [ libX11 ])
+    ++ (lib.lists.optionals (!noU2f) [ pam_u2f ]);
 
   vendorHash = "sha256-tviPb05puHvBdDkSsRrBExUVxQy+DzmkjB+W9W2CG4M=";
+
+  tags = lib.lists.optionals noX11 [ "noxlib" ];
+
+  buildPhase = ''
+    runHook preBuild
+    make build
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    make DESTDIR=$out install
+    make DESTDIR=$out install-manual
+    # make DESTDIR=$out install-systemd
+
+    if [ -d $out/usr ]; then
+      mv $out/usr/* $out
+      rmdir $out/usr
+    fi
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "Dead simple CLI Display Manager on TTY";
