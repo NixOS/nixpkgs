1a8b3250b32334a70ed270cc1ca56fb2730f4de4
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-04-02 02:14:22 -0400

    python310Packages.torchaudio: init at 2.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11850,6 +11850,8 @@ self: super: with self; {
 
   torch-tb-profiler = callPackage ../development/python-modules/torch-tb-profiler/default.nix { };
 
+  torchaudio = callPackage ../development/python-modules/torchaudio { };
+
   torchaudio-bin = callPackage ../development/python-modules/torchaudio/bin.nix { };
 
   torchgpipe = callPackage ../development/python-modules/torchgpipe { };
