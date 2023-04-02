39762330f5ac93cd7bd77763a03f40b9dddf35bc
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-02 17:45:15 +0000

    ddccontrol-db: 20230223 -> 20230328

--- pkgs/data/misc/ddccontrol-db/default.nix
+++ pkgs/data/misc/ddccontrol-db/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ddccontrol-db";
-  version = "20230223";
+  version = "20230328";
 
   src = fetchFromGitHub {
     owner = "ddccontrol";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ehmMSriBYVOeKbXDybpnbYwBEQJcN0NQJ3zaneMxFmQ=";
+    sha256 = "sha256-74HZqgIEDCKeByPFsuUy3A9zutc5ALNxCrRipi7nbI4=";
   };
 
   nativeBuildInputs = [ autoreconfHook intltool ];
