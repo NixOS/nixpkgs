e2c0f0da768f6c21a9dc6a3d48cdc49b51b7a6e5
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-02 07:11:05 +0000

    brev-cli: 0.6.211 -> 0.6.213

--- pkgs/development/misc/brev-cli/default.nix
+++ pkgs/development/misc/brev-cli/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "brev-cli";
-  version = "0.6.211";
+  version = "0.6.213";
 
   src = fetchFromGitHub {
     owner = "brevdev";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-cDG++heWXGJw6ctho/lJfHZ9eTrsmexFzh0i+tIDpMI=";
+    sha256 = "sha256-ABYTOS+pBhlojrZw6V1GEaJJOl4x7GgZfULanBvxuy4=";
   };
 
   vendorHash = "sha256-IR/tgqh8rS4uN5jSOcopCutbHCKHSU9icUfRhOgu4t8=";
