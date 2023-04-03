39e47af08b0b2a2504746155723125c09ad20c99
Garry Filakhtov <filakhtov@gmail.com>
2023-04-03 10:33:40 +1000

    systemdMinimal: re-enable kmod integration
    My previous attempt to slim things down for systemdMinimal ended up breaking the Mobile NixOS use case, as it uses udevd from the systemdMinimal package. Re-enabling the `withKmod` attribute to address this problem.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27144,7 +27144,6 @@ with pkgs;
     withHomed = false;
     withHwdb = false;
     withImportd = false;
-    withKmod = false;
     withLibBPF = false;
     withLibidn2 = false;
     withLocaled = false;
