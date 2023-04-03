8be794b197fd5e9146aeb2903044121f362b70fe
ajs124 <git@ajs124.de>
2023-04-03 12:40:49 +0200

    nginx: sha256 -> hash

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -2,5 +2,5 @@
 
 callPackage ./generic.nix args {
   version = "1.23.4";
-  sha256 = "sha256-1DMA42uySafm7cYLyhsPw3Kguvzi80bXas+2d6h5D8A=";
+  hash = "sha256-1DMA42uySafm7cYLyhsPw3Kguvzi80bXas+2d6h5D8A=";
 }
