eae9398010a6fd99971cd8047538186b8c0f3276
Tobias Markus <tobias@miglix.eu>
2023-04-03 12:57:50 +0200

    clp: Change license to epl20

--- pkgs/applications/science/math/clp/default.nix
+++ pkgs/applications/science/math/clp/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
   doCheck = true;
 
   meta = with lib; {
-    license = licenses.epl10;
+    license = licenses.epl20;
     homepage = "https://github.com/coin-or/Clp";
     description = "An open-source linear programming solver written in C++";
     platforms = platforms.darwin ++ [ "x86_64-linux" ];
