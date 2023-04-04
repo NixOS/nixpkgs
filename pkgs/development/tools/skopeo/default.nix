0b7f87614bcf9882b46a3df538c357eba6517f73
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2023-04-04 11:20:04 +0300

    skopeo: add developer-guy to maintainers list
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

--- pkgs/development/tools/skopeo/default.nix
+++ pkgs/development/tools/skopeo/default.nix
@@ -72,7 +72,7 @@ buildGoModule rec {
     changelog = "https://github.com/containers/skopeo/releases/tag/${src.rev}";
     description = "A command line utility for various operations on container images and image repositories";
     homepage = "https://github.com/containers/skopeo";
-    maintainers = with maintainers; [ lewo ] ++ teams.podman.members;
+    maintainers = with maintainers; [ lewo developer-guy ] ++ teams.podman.members;
     license = licenses.asl20;
   };
 }
