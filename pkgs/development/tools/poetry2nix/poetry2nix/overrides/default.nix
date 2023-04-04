f05c29cc069b93d1a5c314727d10afc298d8d61f
Raito Bezarius <masterancpp@gmail.com>
2023-04-04 13:37:58 +0200

    poetry2nix.overrides: remove LLVM5, LLVM6

--- pkgs/development/tools/poetry2nix/poetry2nix/overrides/default.nix
+++ pkgs/development/tools/poetry2nix/poetry2nix/overrides/default.nix
@@ -1095,9 +1095,9 @@ lib.composeManyExtensions [
             else if (lib.versionOlder old.version "0.28.0" && lib.versionAtLeast old.version "0.27.0") then
               pkgs.llvmPackages_7.llvm
             else if (lib.versionOlder old.version "0.27.0" && lib.versionAtLeast old.version "0.23.0") then
-              pkgs.llvmPackages_6.llvm
+              throw "LLVM6 has been removed from nixpkgs; upgrade llvmlite or use older nixpkgs"
             else if (lib.versionOlder old.version "0.23.0" && lib.versionAtLeast old.version "0.21.0") then
-              pkgs.llvmPackages_5.llvm
+              throw "LLVM5 has been removed from nixpkgs; upgrade llvmlite or use older nixpkgs"
             else
               pkgs.llvm; # Likely to fail.
         in
