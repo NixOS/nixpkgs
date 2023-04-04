24aff837bd92cd9dfc5548d712c6d46ac8727413
Max Ammann <max@maxammann.org>
2023-04-03 22:23:54 -0400

    tracy: update from 0.9 to 0.9.1

--- pkgs/development/tools/tracy/default.nix
+++ pkgs/development/tools/tracy/default.nix
@@ -6,13 +6,13 @@ let
   disableLTO = stdenv.cc.isClang && stdenv.isDarwin;  # workaround issue #19098
 in stdenv.mkDerivation rec {
   pname = "tracy";
-  version = "0.9";
+  version = "0.9.1";
 
   src = fetchFromGitHub {
     owner = "wolfpld";
     repo = "tracy";
     rev = "v${version}";
-    sha256 = "sha256-cdVkY1dSag37JdbsoJp2/0QHO5G+zsftqwBVqRpMiew=";
+    sha256 = "sha256-K1lQNRS8+ju9HyKNVXtHqslrPWcPgazzTitvwkIO3P4";
   };
 
   nativeBuildInputs = [ pkg-config ];
