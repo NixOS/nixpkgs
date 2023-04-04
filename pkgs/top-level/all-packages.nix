55dd25e0adea9ed23de3b5260761b1bad0276019
Martino Fontana <tinozzo123@tutanota.com>
2023-04-04 16:32:00 +0200

    mgba: cleanup
    - Remove unused imagemagick dependency
    - Use default ffmpeg

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2243,8 +2243,7 @@ with pkgs;
 
   melonDS = libsForQt5.callPackage ../applications/emulators/melonDS { };
 
-  mgba = callPackage ../applications/emulators/mgba {
-  };
+  mgba = libsForQt5.callPackage ../applications/emulators/mgba { };
 
   mupen64plus = callPackage ../applications/emulators/mupen64plus { };
 
