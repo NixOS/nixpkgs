86d052f7d44af5a193d99ac84f1bc3828fcfbd43
Kenny MacDermid <kenny@macdermid.ca>
2023-04-04 20:23:07 -0300

    kind: 0.17.0 -> 0.18.0
    Release notes:
    https://github.com/kubernetes-sigs/kind/releases/tag/v0.18.0

--- pkgs/development/tools/kind/default.nix
+++ pkgs/development/tools/kind/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "kind";
-  version = "0.17.0";
+  version = "0.18.0";
 
   src = fetchFromGitHub {
     rev    = "v${version}";
     owner  = "kubernetes-sigs";
     repo   = "kind";
-    sha256 = "sha256-YAa5Dr8Pc6P3RZ3SCiyi7zwmVd5tPalM88R8bxgg6JU=";
+    sha256 = "sha256-ei0klTKLTyxhCkwhG/CSswFd1JPimCMR32pKwPYPvQU=";
   };
 
   patches = [
