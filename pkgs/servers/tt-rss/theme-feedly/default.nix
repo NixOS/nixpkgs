46796330dc624cfb60d694e4578a84c835ff563a
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-04 05:30:22 +0000

    tt-rss-theme-feedly: 3.1.0 -> 4.1.0

--- pkgs/servers/tt-rss/theme-feedly/default.nix
+++ pkgs/servers/tt-rss/theme-feedly/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "tt-rss-theme-feedly";
-  version = "3.1.0";
+  version = "4.1.0";
 
   src = fetchFromGitHub {
     owner = "levito";
     repo = "tt-rss-feedly-theme";
     rev = "v${version}";
-    sha256 = "sha256-sHKht4EXKIibk+McMR+fKv7eZFJsGgZWhfxlLssA/Sw=";
+    sha256 = "sha256-3mD1aY7gjdvucRzY7sLmZ1RsHtraAg1RGE/3uDp6/o4=";
   };
 
   dontBuild = true;
