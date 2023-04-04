685febb5e616805284ae931ffe86b112f584ccef
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-04 14:24:59 +0200

    alterx: init at 0.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -258,6 +258,8 @@ with pkgs;
 
   aeskeyfind = callPackage ../tools/security/aeskeyfind { };
 
+  alterx = callPackage ../tools/security/alterx { };
+
   asn = callPackage ../applications/networking/asn { };
 
   asnmap = callPackage ../tools/security/asnmap { };
