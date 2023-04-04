6f64bb9f3bb75d36ddda565e5263ad048f9a3b67
roy <a365039311@gmail.com>
2023-04-05 06:30:32 +0800

    fix x11docker dependencies

--- pkgs/applications/virtualization/x11docker/default.nix
+++ pkgs/applications/virtualization/x11docker/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, makeWrapper, nx-libs, xorg, getopt, gnugrep, gawk, ps, mount, iproute2 }:
+{ lib, stdenv, fetchFromGitHub, makeWrapper, nx-libs, xorg, getopt, gnugrep, gawk, ps, mount, iproute2,python3, jq, xorg.libxcvt, wmctrl, xdotool, xclip, xpra }:
 stdenv.mkDerivation rec {
   pname = "x11docker";
   version = "7.6.0";
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   installPhase = ''
     install -D x11docker "$out/bin/x11docker";
     wrapProgram "$out/bin/x11docker" \
-      --prefix PATH : "${lib.makeBinPath [ getopt gnugrep gawk ps mount iproute2 nx-libs xorg.xdpyinfo xorg.xhost xorg.xinit ]}"
+      --prefix PATH : "${lib.makeBinPath [ getopt gnugrep gawk ps mount iproute2 nx-libs xorg.xdpyinfo xorg.xhost xorg.xinit python3 jq xorg.libxcvt wmctrl xdotool  xclip xpra ]}"
   '';
 
   meta = {
