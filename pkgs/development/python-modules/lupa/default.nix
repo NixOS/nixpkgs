a32219871ee0044f8cf033fbeb9506304deb9867
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-04 15:45:22 +0200

    python310Packages.lupa: disable on unsupported Python releases
    - add format

--- pkgs/development/python-modules/lupa/default.nix
+++ pkgs/development/python-modules/lupa/default.nix
@@ -2,20 +2,28 @@
 , buildPythonPackage
 , cython
 , fetchPypi
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "lupa";
   version = "2.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-rT/vSGvnrd3TSf6anDk3iQYTEs+Y68UztIm+NPSEy3k=";
   };
 
-  nativeBuildInputs = [ cython ];
+  nativeBuildInputs = [
+    cython
+  ];
 
-  pythonImportsCheck = [ "lupa" ];
+  pythonImportsCheck = [
+    "lupa"
+  ];
 
   meta = with lib; {
     description = "Lua in Python";
