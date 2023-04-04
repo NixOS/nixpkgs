19c7cb505c4f7eeabb4c006ad85dfc684a78decc
williamvds <william@williamvds.me>
2023-04-04 22:00:15 +0100

    pihole-ftl: init at 5.25.1
    Include a patch which removes the termcap linking. Without this patch,
    pihole-ftl fails to link with the following error:
    
    > /nix/store/fhzz4yrdy17czwc9i4swhlpcp445inzb-binutils-2.40/bin/ld: shell.c:(.text+0x2e282): undefined reference to `stifle_history'
    > /nix/store/fhzz4yrdy17czwc9i4swhlpcp445inzb-binutils-2.40/bin/ld: shell.c:(.text+0x2e28a): undefined reference to `write_history'
    > /nix/store/fhzz4yrdy17czwc9i4swhlpcp445inzb-binutils-2.40/bin/ld: shell.c:(.text+0x2e886): undefined reference to `read_history'
    > /nix/store/fhzz4yrdy17czwc9i4swhlpcp445inzb-binutils-2.40/bin/ld: database/CMakeFiles/sqlite3.dir/shell.c.o: in function `readline_completion':
    > shell.c:(.text+0x7792): undefined reference to `rl_completion_matches'
    > /nix/store/fhzz4yrdy17czwc9i4swhlpcp445inzb-binutils-2.40/bin/ld: warning: creating DT_TEXTREL in a PIE
    > collect2: error: ld returned 1 exit status
    
    Also remove the reference to `/opt/pihole/libs`, to prevent non-sandboxed paths
    from being accessed at build time.
    
    Co-Authored-By: JamieMagee <jamie.magee@gmail.com>
    Co-Authored-By: nuxeh <1516017+nuxeh@users.noreply.github.com>

---
+++ pkgs/by-name/pi/pihole-ftl/package.nix
@@ -0,0 +1,69 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, gmp
+, libidn
+, nettle
+, readline
+, ncurses
+, xxd
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "pihole-ftl";
+  version = "5.25.1";
+
+  src = fetchFromGitHub {
+    owner = "pi-hole";
+    repo = "FTL";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-LG/btYXRUJ97CpOB4OCmzkd+FOSN5E6sXV+kLt63GG8=";
+  };
+
+  patches = [
+    ./0000-fix-readline-linking.patch
+    # Nix enables fortification, avoid duplicating the compile definition
+    ./0001-remove-fortify-source.patch
+    ./0002-fix-use-after-free.patch
+  ];
+
+  nativeBuildInputs = [ cmake xxd ];
+
+  buildInputs = [ gmp libidn nettle readline ncurses ];
+
+  postPatch = [
+    # Swap the static library suffix options to shared for non-static builds
+    (lib.optionalString (!stdenv.hostPlatform.isStatic) ''
+      substituteInPlace src/CMakeLists.txt \
+        --replace "CMAKE_STATIC_LIBRARY_SUFFIX" "CMAKE_SHARED_LIBRARY_SUFFIX"
+    '')
+
+    ''
+      substituteInPlace src/version.h.in \
+        --replace "@GIT_VERSION@" "v${finalAttrs.version}" \
+        --replace "@GIT_DATE@" "1970-01-01" \
+        --replace "@GIT_BRANCH@" "master" \
+        --replace "@GIT_TAG@" "v${finalAttrs.version}" \
+        --replace "@GIT_HASH@" "builtfromreleasetarball"
+    ''
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp pihole-FTL $out/bin
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Pi-hole FTL engine";
+    homepage = "https://github.com/pi-hole/FTL";
+    license = licenses.eupl12;
+    maintainers = with maintainers; [ williamvds ];
+    platforms = platforms.linux;
+    mainProgram = "pihole-FTL";
+  };
+})
