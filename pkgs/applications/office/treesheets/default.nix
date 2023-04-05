fb8e6e65b5d275a76f735d216586dbb4c12d1d9a
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-05 17:31:45 +0000

    treesheets: unstable-2023-03-18 -> unstable-2023-04-04

--- pkgs/applications/office/treesheets/default.nix
+++ pkgs/applications/office/treesheets/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "treesheets";
-  version = "unstable-2023-03-18";
+  version = "unstable-2023-04-04";
 
   src = fetchFromGitHub {
     owner = "aardappel";
     repo = "treesheets";
-    rev = "fd73698ba15b36cfd1f1726e51558405a56c6cd2";
-    sha256 = "kGpze5aBbBjhLTU9jV2xX44iO82skgbxVKc0lHndezw=";
+    rev = "a7d0065763c0bf0cfc2b410b17cb1fce33619616";
+    sha256 = "wDAheXzObiqFkZ2ZVQAzMVZT5mzIEzxh6jNwPiPoWis=";
   };
 
   nativeBuildInputs = [
