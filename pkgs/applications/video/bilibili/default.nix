c5a3aeba6fe5bd254334aa3119902d7c2487b110
Jedsek <jedsek@qq.com>
2023-04-05 21:13:50 +0800

    bilibili: init at 1.9.2-1

---
+++ pkgs/applications/video/bilibili/default.nix
@@ -0,0 +1,48 @@
+{ lib
+, stdenv
+, fetchurl
+, electron
+, makeWrapper
+}:
+
+stdenv.mkDerivation rec {
+  pname = "bilibili";
+  version = "1.9.2-1";
+  src = fetchurl {
+    url = "https://github.com/msojocs/bilibili-linux/releases/download/v${version}/io.github.msojocs.bilibili_${version}_amd64.deb";
+    hash = "sha256-y3dUBImvcIG89m82RaIOa0cxJXIAIGa+n3FJkASacaY=";
+  };
+
+  unpackPhase = ''
+    runHook preUnpack
+
+    ar x $src
+    tar xf data.tar.xz
+
+    runHook postUnpack
+  '';
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp -r usr/share $out/share
+    sed -i "s|Exec=.*|Exec=$out/bin/bilibili|" $out/share/applications/*.desktop
+    cp -r opt/apps/io.github.msojocs.bilibili/files/bin/app $out/opt
+    makeWrapper ${electron}/bin/electron $out/bin/bilibili \
+      --argv0 "bilibili" \
+      --add-flags "$out/opt/app.asar"
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Electron-based bilibili desktop client";
+    homepage = "https://github.com/msojocs/bilibili-linux";
+    license = licenses.mit;
+    maintainers = with maintainers; [ jedsek ];
+    platforms = platforms.unix;
+  };
+}
