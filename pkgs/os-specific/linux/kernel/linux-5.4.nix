8b6c811bfb9213cbf52cc9cfd5b7b2178afba6d0
Alyssa Ross <hi@alyssa.is>
2023-04-05 11:14:09 +0000

    linux: 5.4.239 -> 5.4.240
    (cherry picked from commit 7cc8cea05b8156d6ae91c5e7ec36a4fa011089ee)

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.239";
+  version = "5.4.240";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0nw02lqkvachksyan4n11q0g6jhm1ii7d5zs276n46891ypn80m9";
+    sha256 = "0ihf0rqhx7dav3k3igk29962sscb1xyniy2gx8chyllprr0z126w";
   };
 } // (args.argsOverride or {}))
