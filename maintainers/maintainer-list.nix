a51ac639c0e9394de23bfbd19c5fe9958d4f2370
Stef Dunlap <stef@kindrobot.ca>
2023-04-05 13:32:43 -0400

    maintainers: add kindrowboat

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8024,6 +8024,12 @@
     githubId = 843652;
     name = "Kim Burgess";
   };
+  kindrowboat = {
+    email = "hello@kindrobot.ca";
+    github = "kindrowboat";
+    githubId = 777773;
+    name = "Stef Dunlap";
+  };
   kini = {
     email = "keshav.kini@gmail.com";
     github = "kini";
