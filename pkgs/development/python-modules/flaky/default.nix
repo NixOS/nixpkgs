7373272e86e16a0e26f50453dcbb340639bd9eb8
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-04-06 16:25:11 +0200

    python310Packages.flaky: Drop nose tests
    Nose tests is not getting updated for python3 and shouldn't be used
    anymore. In fact it can't be used on pypy, since it does not come with a
    2to3 utility.

--- pkgs/development/python-modules/flaky/default.nix
+++ pkgs/development/python-modules/flaky/default.nix
@@ -2,27 +2,29 @@
 , buildPythonPackage
 , fetchPypi
 , mock
-, nose
 , pytest
 }:
 
 buildPythonPackage rec {
   pname = "flaky";
   version = "3.7.0";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "3ad100780721a1911f57a165809b7ea265a7863305acb66708220820caf8aa0d";
+    hash = "sha256-OtEAeAchoZEfV6FlgJt+omWnhjMFrLZnCCIIIMr4qg0=";
   };
 
-  nativeCheckInputs = [ mock nose pytest ];
+  nativeCheckInputs = [
+    mock
+    pytest
+  ];
 
   checkPhase = ''
     # based on tox.ini
     pytest -k 'example and not options' --doctest-modules test/test_pytest/
     pytest -k 'example and not options' test/test_pytest/
     pytest -p no:flaky test/test_pytest/test_flaky_pytest_plugin.py
-    nosetests --with-flaky --force-flaky --max-runs 2 test/test_nose/test_nose_options_example.py
     pytest --force-flaky --max-runs 2  test/test_pytest/test_pytest_options_example.py
   '';
 
