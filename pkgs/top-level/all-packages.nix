0823a787a739767d1d012c057bad2e4f331d645e
snicket2100 <57048005+snicket2100@users.noreply.github.com>
2023-04-06 20:59:45 +0200

    wmbusmeters: init at 1.12.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34668,6 +34668,8 @@ with pkgs;
 
   wllvm = callPackage  ../development/tools/wllvm { };
 
+  wmbusmeters = callPackage ../applications/radio/wmbusmeters { };
+
   wmname = callPackage ../applications/misc/wmname { };
 
   wmctrl = callPackage ../tools/X11/wmctrl { };
