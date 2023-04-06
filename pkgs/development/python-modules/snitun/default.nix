8e5fc46b9a41549fd9ef2721e8b44c4444cf09f0
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-04-06 13:29:54 +0200

    python311Packages.snitun: Disable failing test

--- pkgs/development/python-modules/snitun/default.nix
+++ pkgs/development/python-modules/snitun/default.nix
@@ -7,6 +7,7 @@
 , fetchFromGitHub
 , pytest-aiohttp
 , pytestCheckHook
+, pythonAtLeast
 , pythonOlder
 }:
 
@@ -42,6 +43,9 @@ buildPythonPackage rec {
     "test_snitun_single_runner_throttling"
     # ConnectionResetError: [Errno 54] Connection reset by peer
     "test_peer_listener_timeout"
+  ] ++ lib.optional (pythonAtLeast "3.11") [
+    # TypeError: Passing coroutines is forbidden, use tasks explicitly.
+    "test_snitun_runner_updown"
   ];
 
   pythonImportsCheck = [ "snitun" ];
