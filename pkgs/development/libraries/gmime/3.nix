168d7a5b28c8bb6f6d92b7445439d1c953dc20d9
Adam Joseph <adam@westernsemico.com>
2023-04-05 19:34:25 -0700

    gmime/3.x: fix cross compilation
    Thanks to Void Linux for the trick that makes this work.  Their
    workaround can be found in 48ce73c6cca6bfbade802f513a7b19d4e18ed583
    here:
    
    https://github.com/void-linux/void-packages/commit/48ce73c6cca6bfbade802f513a7b19d4e18ed583

--- pkgs/development/libraries/gmime/3.nix
+++ pkgs/development/libraries/gmime/3.nix
@@ -1,5 +1,8 @@
 { lib, stdenv, fetchurl, pkg-config, glib, zlib, gnupg, gpgme, libidn2, libunistring, gobject-introspection
-, vala }:
+, vala
+, gtk-doc
+, autoreconfHook
+}:
 
 stdenv.mkDerivation rec {
   version = "3.2.12";
@@ -12,17 +15,29 @@ stdenv.mkDerivation rec {
 
   outputs = [ "out" "dev" ];
 
-  buildInputs = [ vala gobject-introspection zlib gpgme libidn2 libunistring ];
-  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ vala zlib gpgme libidn2 libunistring ];
+  nativeBuildInputs = [ pkg-config autoreconfHook gobject-introspection gtk-doc libidn2 ];
   propagatedBuildInputs = [ glib ];
-  configureFlags = [
+  configureFlags = lib.optionals (stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
     "--enable-introspection=yes"
     "--enable-vala=yes"
+  ] ++ lib.optionals (!(stdenv.buildPlatform.canExecute stdenv.hostPlatform)) [
+    # from Void Linux commit 48ce73c6cca6bfbade802f513a7b19d4e18ed583
+    "am_cv_func_iconv=yes"
+    "am_cv_func_iconv_works=yes"
+    "am_cv_lib_iconv=no"
+    "ac_cv_have_iconv_detect_h=yes"
+    "am_cv_proto_iconv_arg1="
+    "--enable-vala=no"
   ];
 
   postPatch = ''
     substituteInPlace tests/testsuite.c \
       --replace /bin/rm rm
+  ''
+  # from Void Linux commit 48ce73c6cca6bfbade802f513a7b19d4e18ed583
+  + lib.optionalString (!(stdenv.buildPlatform.canExecute stdenv.hostPlatform)) ''
+    cp --no-preserve=all ${./iconv-detect.h} iconv-detect.h
   '';
 
   nativeCheckInputs = [ gnupg ];
