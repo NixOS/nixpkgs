fd3641ec26038e0f23260a7ae3c20f9d412a0f7a
Val Lorentz <progval+git+nix@progval.net>
2023-04-06 11:26:26 +0200

    mrustc: 0.9 -> 0.10
    And switch from Rust 1.29 to 1.54, as this is the top version
    supported by mrustc

--- pkgs/development/compilers/mrustc/minicargo.nix
+++ pkgs/development/compilers/mrustc/minicargo.nix
@@ -12,14 +12,14 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
   makefile = "minicargo.mk";
-  makeFlags = [ "tools/bin/minicargo" ];
+  makeFlags = [ "bin/minicargo" ];
 
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin
-    cp tools/bin/minicargo $out/bin
+    cp bin/minicargo $out/bin
 
-    # without it, minicargo defaults to "<minicargo_path>/../../bin/mrustc"
+    # without it, minicargo defaults to "<minicargo_path>/../bin/mrustc"
     wrapProgram "$out/bin/minicargo" --set MRUSTC_PATH ${mrustc}/bin/mrustc
     runHook postInstall
   '';
