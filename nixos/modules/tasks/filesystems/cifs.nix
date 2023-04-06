8834447b7dc3528748c69b2d9a7adc0e0112bb22
Ruby Iris Juric <ruby@srxl.me>
2023-04-06 18:47:44 +1000

    nixos/cifs: configure cifs.upcall/idmap to be used when needed

--- nixos/modules/tasks/filesystems/cifs.nix
+++ nixos/modules/tasks/filesystems/cifs.nix
@@ -4,6 +4,8 @@ with lib;
 
 let
 
+  enabled = any (fs: fs == "cifs") config.boot.supportedFilesystems;
+
   inInitrd = any (fs: fs == "cifs") config.boot.initrd.supportedFilesystems;
 
 in
@@ -11,7 +13,7 @@ in
 {
   config = {
 
-    system.fsPackages = mkIf (any (fs: fs == "cifs") config.boot.supportedFilesystems) [ pkgs.cifs-utils ];
+    system.fsPackages = mkIf enabled [ pkgs.cifs-utils ];
 
     boot.initrd.availableKernelModules = mkIf inInitrd
       [ "cifs" "nls_utf8" "hmac" "md4" "ecb" "des_generic" "sha256" ];
@@ -20,6 +22,22 @@ in
       ''
         copy_bin_and_libs ${pkgs.cifs-utils}/sbin/mount.cifs
       '';
+
+    programs.keyutils = mkIf enabled {
+      enable = true;
+      keyPrograms = [
+        {
+          op = "create";
+          type = "cifs.spnego";
+          command = "${pkgs.cifs-utils}/bin/cifs.upcall %k";
+        }
+        {
+          op = "create";
+          type = "cifs.idmap";
+          command = "${pkgs.cifs-utils}/bin/cifs.idmap %k";
+        }
+      ];
+    };
 
   };
 }
