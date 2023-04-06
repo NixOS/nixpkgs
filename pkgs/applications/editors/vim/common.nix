2d0cb31053f4db49efaca62edf3bc33bf9a4bb8b
Janne Heß <janne@hess.ooo>
2023-04-06 12:27:20 +0200

    vim: Fix rvim :diffpatch security issue

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,20 @@
-{ lib, fetchFromGitHub }:
+{ lib, fetchFromGitHub, fetchpatch, applyPatches }:
 rec {
   version = "9.0.0609";
 
-  src = fetchFromGitHub {
-    owner = "vim";
-    repo = "vim";
-    rev = "v${version}";
-    hash = "sha256-UBj3pXY6rdekKnCX/V/4o8LLBMZkNs1U4Z4KuvisIYQ=";
+  src = applyPatches {
+    src = fetchFromGitHub {
+      owner = "vim";
+      repo = "vim";
+      rev = "v${version}";
+      hash = "sha256-UBj3pXY6rdekKnCX/V/4o8LLBMZkNs1U4Z4KuvisIYQ=";
+    };
+    patches = [
+      (fetchpatch {
+        url = "https://github.com/vim/vim/commit/23a971da506249fc8388f06cd5c011b83406ac5c.patch";
+        hash = "sha256-G9WiIU6gyIsfL6KL4qku3sgxtp0aitol60VJSUmSKdo=";
+      })
+    ];
   };
 
   enableParallelBuilding = true;
