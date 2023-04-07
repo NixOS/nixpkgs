9dc1574013cf5d8529e7909d3c78fb412197e3ad
Anderson Torres <torres.anderson.85@protonmail.com>
2023-04-07 00:56:08 -0300

    nux: refactor

--- pkgs/tools/misc/nux/default.nix
+++ pkgs/tools/misc/nux/default.nix
@@ -5,31 +5,34 @@
 , installShellFiles
 }:
 
-rustPlatform.buildRustPackage rec {
+let
   pname = "nux";
   version = "0.1.4";
+in
+rustPlatform.buildRustPackage {
+  inherit pname version;
 
   src = fetchFromGitHub {
     owner = "NuxPackage";
     repo = pname;
     rev = version;
-    sha256 = "sha256-k3HRaWN8/MTZRGWBxI8RRK0tcSYBbSLs3vHkUdLGTc8";
+    hash = "sha256-k3HRaWN8/MTZRGWBxI8RRK0tcSYBbSLs3vHkUdLGTc8";
   };
 
-  cargoSha256 = "sha256-wfUr3dcdALMEgJ6CaXhK4Gqk6xflCnov9tELA63drV4=";
+  cargoHash = "sha256-wfUr3dcdALMEgJ6CaXhK4Gqk6xflCnov9tELA63drV4=";
 
-  preFixup = ''
+  nativeBuildInputs = [ asciidoctor installShellFiles ];
+
+  postInstall = ''
     installManPage $releaseDir/build/nux-*/out/nux.1
     installShellCompletion $releaseDir/build/nux-*/out/nux.{bash,fish}
     installShellCompletion $releaseDir/build/nux-*/out/_nux
-
   '';
-  nativeBuildInputs = [ asciidoctor installShellFiles ];
 
-  meta = with lib; {
-    description = "A wrapper over the nix cli";
+  meta = {
     homepage = "https://github.com/NuxPackage/nux";
-    license = with licenses; [ gpl3 ];
-    maintainers = with maintainers; [ drzoidberg ];
+    description = "A wrapper over the nix cli";
+    license = with lib.licenses; [ gpl3Plus ];
+    maintainers = with lib.maintainers; [ ];
   };
 }
