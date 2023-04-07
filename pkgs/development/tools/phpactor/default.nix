517b44b74dca8ad7b7be364f1d5cffb83a8aca0c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-07 12:22:56 +0200

    Use separate derivation for vendor

--- pkgs/development/tools/phpactor/default.nix
+++ pkgs/development/tools/phpactor/default.nix
@@ -11,45 +11,61 @@ stdenvNoCC.mkDerivation rec {
     hash = "sha256-jWZgBEaffjQ5wCStSEe+eIi7BJt6XAQFEjmq5wvW5V8=";
   };
 
-  nativeBuildInputs = [
-    php.packages.composer
-  ];
+  vendor = stdenvNoCC.mkDerivation rec {
+    pname = "phpactor-vendor";
+
+    inherit src version;
+
+    dontPatchShebangs = true;
+
+    nativeBuildInputs = [
+      php
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+
+      substituteInPlace composer.json \
+        --replace '"config": {' '"config": { "autoloader-suffix": "Phpactor",'
+      ${php.packages.composer}/bin/composer install --no-interaction --optimize-autoloader --no-dev --no-scripts
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp -ar ./vendor $out/
+
+      runHook postInstall
+    '';
+
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
+    outputHash = {
+        aarch64-linux = "sha256-DeTJV0kBV83vJXgrK7qiTHxXjBQY+SNwj1HhFi48cMw=";
+        x86_64-linux = "sha256-7N6PGtk+pEzw7jsJzQ4s5jVOOiY6OW/1dAYnhOy2grE=";
+        x86_64-darwin = "sha256-IERtY7Eb1OMDagnblKMIk33Z0VO/qnhhI0UIAlTnDCY=";
+      }.${stdenvNoCC.system};
+  };
 
   buildInputs = [
     php
   ];
 
-  buildPhase = ''
-    runHook preBuild
-
-    patchShebangs ./bin/phpactor
-
-    substituteInPlace composer.json \
-      --replace '"config": {' '"config": { "autoloader-suffix": "Phpactor",' \
-      --replace '"name": "phpactor/phpactor",' '"name": "phpactor/phpactor", "version": "${src.rev}",'
-
-    ${php.packages.composer}/bin/composer install --no-interaction --optimize-autoloader --no-dev --no-scripts
-
-    runHook postBuild
-  '';
+  dontBuild = true;
 
   installPhase = ''
     runHook preInstall
 
+    cp -r ${vendor}/vendor .
     mkdir -p $out/share/php/phpactor $out/bin
     cp -r . $out/share/php/phpactor
     ln -s $out/share/php/phpactor/bin/phpactor $out/bin/phpactor
 
     runHook postInstall
   '';
-
-  outputHashMode = "recursive";
-  outputHashAlgo = "sha256";
-  outputHash = {
-      aarch64-linux = "sha256-DeTJV0kBV83vJXgrK7qiTHxXjBQY+SNwj1HhFi48cMw=";
-      x86_64-linux = "sha256-2ddQ0jrNPuiiFRw9kxhnNwcA1GYKpCBlgo0/p+W/q+c=";
-      x86_64-darwin = "sha256-IERtY7Eb1OMDagnblKMIk33Z0VO/qnhhI0UIAlTnDCY=";
-    }.${stdenvNoCC.system};
 
   meta = {
     description = "Mainly a PHP Language Server";
