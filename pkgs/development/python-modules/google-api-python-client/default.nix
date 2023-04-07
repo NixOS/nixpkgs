052463fa20757074f8e7ac6e41d91a4ac5caceca
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-07 20:55:23 +0000

    python310Packages.google-api-python-client: 2.83.0 -> 2.84.0

--- pkgs/development/python-modules/google-api-python-client/default.nix
+++ pkgs/development/python-modules/google-api-python-client/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-api-python-client";
-  version = "2.83.0";
+  version = "2.84.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0HUJ8bLSskJzY7RU25lvehXhdRpIz8ryhCcFBWDdUc8=";
+    hash = "sha256-w5j9b56tC+I6reOycExyxRRt8OM1LY/5EBKGB34bAQo=";
   };
 
   propagatedBuildInputs = [
