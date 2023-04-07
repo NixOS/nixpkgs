21f383d94f599ad6a138755d33d7ca45ba187e28
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-07 10:21:33 +0200

    Fix fixed-output derivation hash and other minor things

--- pkgs/development/tools/phpactor/default.nix
+++ pkgs/development/tools/phpactor/default.nix
@@ -11,37 +11,43 @@ stdenvNoCC.mkDerivation rec {
     hash = "sha256-jWZgBEaffjQ5wCStSEe+eIi7BJt6XAQFEjmq5wvW5V8=";
   };
 
-  buildInputs = [
+  nativeBuildInputs = [
     composer
     php
   ];
 
   buildPhase = ''
+    runHook preBuild
+
     patchShebangs .
     substituteInPlace composer.json \
       --replace '"config": {' '"config": { "autoloader-suffix": "Phpactor",' \
       --replace '"name": "phpactor/phpactor",' '"name": "phpactor/phpactor", "version": "${version}",'
 
-    composer dump-autoload --optimize --no-dev
     composer install --no-interaction --optimize-autoloader --no-dev
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
 
     mkdir -p $out/share/php/phpactor $out/bin
     cp -r . $out/share/php/phpactor
     ln -s $out/share/php/phpactor/bin/phpactor $out/bin/phpactor
-  '';
 
-  dontInstall = true;
+    runHook postInstall
+  '';
 
   outputHashMode = "recursive";
   outputHashAlgo = "sha256";
-  outputHash = "sha256-Vf7tuzKekbWju0Tgef3MTAMi59dDxILV4A3DSRUORts=";
+  outputHash = "sha256-kqwXaxNKCJNT/qWWqW2EUaM7BPlM4jrV+dGegD2J/uQ=";
 
-  meta = with lib; {
+  meta = {
     description = "Mainly a PHP Language Server";
     homepage = "https://github.com/phpactor/phpactor";
-    license = licenses.mit;
-    maintainers = with maintainers; [ ryantm ];
-    platforms = platforms.unix;
+    license = lib.licenses.mit;
+    maintainers = lib.teams.php.members ++ [ lib.maintainers.ryantm ];
   };
 
 }
