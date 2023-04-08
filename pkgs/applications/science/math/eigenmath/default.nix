027d18d6a748d892c8d87db3fa4a6e791f480839
Nick Cao <nickcao@nichi.co>
2023-04-08 23:19:59 +0800

    eigenmath: enable tests

--- pkgs/applications/science/math/eigenmath/default.nix
+++ pkgs/applications/science/math/eigenmath/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, buildPackages
 , unstableGitUpdater
 }:
 
@@ -15,11 +16,23 @@ stdenv.mkDerivation rec {
     hash = "sha256-GZkC/Tvep7fL5nJyz0ZN7z0lUhGX4EJlXVswwAyegUE=";
   };
 
+  checkPhase = let emulator = stdenv.hostPlatform.emulator buildPackages; in ''
+    runHook preCheck
+
+    for testcase in selftest1 selftest2; do
+      ${emulator} ./eigenmath "test/$testcase"
+    done
+
+    runHook postCheck
+  '';
+
   installPhase = ''
     runHook preInstall
     install -Dm555 eigenmath "$out/bin/eigenmath"
     runHook postInstall
   '';
+
+  doCheck = true;
 
   passthru = {
     updateScript = unstableGitUpdater { };
