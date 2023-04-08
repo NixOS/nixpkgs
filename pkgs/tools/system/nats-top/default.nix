2669f6d35402ccf898b4e5772a26ddd128203621
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-08 23:16:06 +0200

    nats-top: add changelog to meta

--- pkgs/tools/system/nats-top/default.nix
+++ pkgs/tools/system/nats-top/default.nix
@@ -10,15 +10,16 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "nats-io";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-ZSPv4meyIYqNJm6SvqnpOjTtRGvfkUOAxn3JHmK5UEQ=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-ZSPv4meyIYqNJm6SvqnpOjTtRGvfkUOAxn3JHmK5UEQ=";
   };
 
-  vendorSha256 = "sha256-8UcHRFt/O8RgZRxODIJZ16zvBi7FmadYdA/NUH9kfEo=";
+  vendorHash = "sha256-8UcHRFt/O8RgZRxODIJZ16zvBi7FmadYdA/NUH9kfEo=";
 
   meta = with lib; {
     description = "top-like tool for monitoring NATS servers";
     homepage = "https://github.com/nats-io/nats-top";
+    changelog = "https://github.com/nats-io/nats-top/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
