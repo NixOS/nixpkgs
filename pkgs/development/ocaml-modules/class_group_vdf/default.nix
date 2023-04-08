26214bb8887b44132615a5c4b634125301f3935d
Et7f3 <cadeaudeelie@gmail.com>
2023-04-08 07:27:25 +0200

    ocamlPackages.class_group_vdf: Bump MACOSX_DEPLOYMENT_TARGET to fix build

--- pkgs/development/ocaml-modules/class_group_vdf/default.nix
+++ pkgs/development/ocaml-modules/class_group_vdf/default.nix
@@ -4,7 +4,7 @@
 , alcotest, bisect_ppx
 }:
 
-buildDunePackage rec {
+buildDunePackage (rec {
   pname = "class_group_vdf";
   version = "0.0.4";
   duneVersion = "3";
@@ -43,8 +43,13 @@ buildDunePackage rec {
   meta = {
     description = "Verifiable Delay Functions bindings to Chia's VDF";
     homepage = "https://gitlab.com/nomadic-labs/tezos";
-    broken = stdenv.isDarwin && stdenv.isx86_64;
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.ulrikstrid ];
   };
 }
+# Darwin sdk on intel target 10.12 (2016) at the time of writing. It is likely that host will be at least 10.14 (2018). This fix allow it to build and run on 10.14 and build on 10.12 (but don't run).
+// lib.optionalAttrs (lib.versionOlder stdenv.hostPlatform.darwinMinVersion "10.14" && stdenv.hostPlatform.isMacOS && stdenv.hostPlatform.isx86_64) {
+  preHook = ''
+    export MACOSX_DEPLOYMENT_TARGET=10.14
+  '';
+})
