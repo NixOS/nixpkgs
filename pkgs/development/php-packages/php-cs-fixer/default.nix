b498bb0d6026d4ce04b7b2e71fccabc6a7b09590
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-09 19:14:43 +0200

    phpPackages.php-cs-fixer: use `buildComposerProject` builder

--- pkgs/development/php-packages/php-cs-fixer/default.nix
+++ pkgs/development/php-packages/php-cs-fixer/default.nix
@@ -1,35 +1,26 @@
-{ mkDerivation, fetchurl, makeWrapper, lib, php }:
+{ fetchFromGitHub, lib, php }:
 
-let
+php.buildComposerProject (finalAttrs: {
   pname = "php-cs-fixer";
   version = "3.16.0";
-in
-mkDerivation {
-  inherit pname version;
 
-  src = fetchurl {
-    url = "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v${version}/php-cs-fixer.phar";
-    sha256 = "sha256-B4VzfsSwcffR/t4eREMLH9jRWCTumYel6GM4rpumVBY=";
+  src = fetchFromGitHub {
+    owner = "FriendsOfPHP";
+    repo = "PHP-CS-Fixer";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-d8oypyaR8B7DHP92UtnRLuUv4SRKAOJ8cGDLxw3IlwM=";
   };
 
-  dontUnpack = true;
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/php-cs-fixer/php-cs-fixer.phar
-    makeWrapper ${php}/bin/php $out/bin/php-cs-fixer \
-      --add-flags "$out/libexec/php-cs-fixer/php-cs-fixer.phar"
-    runHook postInstall
-  '';
+  # TODO: Open a PR against https://github.com/PHP-CS-Fixer/PHP-CS-Fixer
+  # Missing `composer.lock` from the repository.
+  composerLock = ./composer.lock;
+  vendorHash = "sha256-pNnREHUMgj50yrVudYqWCDoflzFnGRjStTuCtVsjVPI=";
 
   meta = with lib; {
-    changelog = "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/releases/tag/${version}";
+    changelog = "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/releases/tag/${finalAttrs.version}";
     description = "A tool to automatically fix PHP coding standards issues";
     license = licenses.mit;
     homepage = "https://cs.symfony.com/";
     maintainers = with maintainers; [ jtojnar ] ++ teams.php.members;
   };
-}
+})
