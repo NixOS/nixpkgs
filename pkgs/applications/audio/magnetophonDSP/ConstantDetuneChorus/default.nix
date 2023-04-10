7735e9b13843365cbc57e78e7d9e30a62de27f49
Bart Brouns <bart@magnetophon.nl>
2023-04-11 00:49:04 +0200

    constant-detune-chorus: mark broken
    compiling it gives the error:
    ERROR3 : n is NaN in an Interval

--- pkgs/applications/audio/magnetophonDSP/ConstantDetuneChorus/default.nix
+++ pkgs/applications/audio/magnetophonDSP/ConstantDetuneChorus/default.nix
@@ -33,5 +33,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/magnetophon/constant-detune-chorus";
     license = lib.licenses.gpl3;
     maintainers = [ lib.maintainers.magnetophon ];
+    # ERROR3 : n is NaN in an Interval
+    broken = true;
   };
 }
