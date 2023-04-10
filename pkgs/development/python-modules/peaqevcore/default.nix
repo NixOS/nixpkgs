246d8f37ae3cd98e6c12a11531e26c30a0353497
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-10 21:02:30 +0200

    python310Packages.peaqevcore: 15.1.1 -> 15.2.1

--- pkgs/development/python-modules/peaqevcore/default.nix
+++ pkgs/development/python-modules/peaqevcore/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "peaqevcore";
-  version = "15.1.1";
+  version = "15.2.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-7cr37kvNgPPTYJd5r1RXy7qFNN0nXI74YHPg9k8ZDfw=";
+    hash = "sha256-71ea8r1g52SyWxG+aTl53WanM5z4XAj9k5E26ivpYoE=";
   };
 
   postPatch = ''
