398a1d6aebe2c5f602130e310985f480e037cdd4
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-10 04:29:24 +0000

    python310Packages.yfinance: 0.2.14 -> 0.2.16

--- pkgs/development/python-modules/yfinance/default.nix
+++ pkgs/development/python-modules/yfinance/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "yfinance";
-  version = "0.2.14";
+  version = "0.2.16";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "ranaroussi";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-KUG8hixdfLg1qBvwU1s6HFORWKCdSY54wLo0onhXmE0=";
+    hash = "sha256-OcGmRSsUk2v+zpEWtOanuZLupR9hR+wbEMln00/uCms=";
   };
 
   propagatedBuildInputs = [
