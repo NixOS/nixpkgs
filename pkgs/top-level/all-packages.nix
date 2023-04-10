6deb86f6138265a715dd005c310ad33a2e2865ff
Adam Joseph <adam@westernsemico.com>
2023-04-09 20:10:31 -0700

    all-packages.nix: expose arm-trusted-firmware
    This commit exposes
    
    ```
      arm-trusted-firmware = callPackage ../misc/arm-trusted-firmware { }
    ```
    
    at the top level.  Without this it is impossible for downstream
    users like [ownerboot](https://git.sr.ht/~amjoseph/ownerboot) to
    override options like `unfreeIncludeHDCPBlob` and dependencies like
    `openssl`.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26148,7 +26148,8 @@ with pkgs;
     alsa-ucm-conf
     alsa-utils;
 
-  inherit (callPackage ../misc/arm-trusted-firmware {})
+  arm-trusted-firmware = callPackage ../misc/arm-trusted-firmware { };
+  inherit (arm-trusted-firmware)
     buildArmTrustedFirmware
     armTrustedFirmwareTools
     armTrustedFirmwareAllwinner
