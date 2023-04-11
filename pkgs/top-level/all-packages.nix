d978b588655200a5510642760e9dfbbd83242119
Atemu <atemu.main@gmail.com>
2023-04-11 16:11:34 +0200

    buildFHSEnvChroot: deprecate

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -372,7 +372,7 @@ with pkgs;
   buildEnv = callPackage ../build-support/buildenv { }; # not actually a package
 
   buildFHSEnv = buildFHSEnvBubblewrap;
-  buildFHSEnvChroot = callPackage ../build-support/build-fhsenv-chroot { };
+  buildFHSEnvChroot = callPackage ../build-support/build-fhsenv-chroot { }; # Deprecated; use buildFHSEnv/buildFHSEnvBubblewrap
   buildFHSEnvBubblewrap = callPackage ../build-support/build-fhsenv-bubblewrap { };
 
   buildMaven = callPackage ../build-support/build-maven.nix { };
