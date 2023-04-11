a4ce42b73da918f3425ec071c3853b7e9fe52e20
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-11 21:37:33 +0200

    phpPackages.php-parallel-lint: use `builbComposerProject` builder

--- pkgs/development/php-packages/php-parallel-lint/default.nix
+++ pkgs/development/php-packages/php-parallel-lint/default.nix
@@ -1,43 +1,20 @@
-{ mkDerivation, fetchFromGitHub, makeWrapper, lib, php, php81 }:
-let
+{ lib, fetchFromGitHub, php }:
+
+php.buildComposerProject (finalAttrs: {
   pname = "php-parallel-lint";
   version = "1.3.2";
-in
-mkDerivation {
-  inherit pname version;
 
   src = fetchFromGitHub {
     owner = "php-parallel-lint";
     repo = "PHP-Parallel-Lint";
-    rev = "v${version}";
-    # `.gitattibutes` exclude `box.json` from the archive produced git.
-    forceFetchGit = true;
-    sha256 = "SPP1ynxJad2m5wknGt8z94fW7Ucx8nqLvwZVmlylOgM=";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-pTHH19HwqyOj5pSmH7l0JlntNVtMdu4K9Cl+qyrrg9U=";
   };
 
-  nativeBuildInputs = [
-    makeWrapper
-    php.packages.composer
-    # box is only available for PHP ≥ 8.1 but the purpose of this tool is to validate
-    # that project does not use features not available on older PHP versions.
-    php81.packages.box
-  ];
-
-  buildPhase = ''
-    runHook preBuild
-    composer dump-autoload
-    box compile
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D parallel-lint.phar $out/libexec/php-parallel-lint/php-parallel-lint.phar
-    makeWrapper ${php}/bin/php $out/bin/php-parallel-lint \
-      --add-flags "$out/libexec/php-parallel-lint/php-parallel-lint.phar"
-    runHook postInstall
-  '';
+  # TODO: Open a PR against https://github.com/php-parallel-lint/PHP-Parallel-Lint
+  # Missing `composer.lock` from the repository.
+  composerLock = ./composer.lock;
+  vendorHash = "sha256-X5hBkdwtNDiRxxbjBUO3m2Xg4LTegot5UuSy4IpajP8=";
 
   meta = with lib; {
     description = "Tool to check syntax of PHP files faster than serial check with fancier output";
@@ -45,4 +22,4 @@ mkDerivation {
     homepage = "https://github.com/php-parallel-lint/PHP-Parallel-Lint";
     maintainers = with maintainers; [ jtojnar ] ++ teams.php.members;
   };
-}
+})
