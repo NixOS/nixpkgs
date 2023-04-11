6cc0b5f0a64fa83c162f4aaf900ec39e418f9e76
Chris Pattison <chpattison@gmail.com>
2023-04-10 20:37:06 -0700

    python310Packages.galois: fix broken dep constraints

--- pkgs/development/python-modules/galois/default.nix
+++ pkgs/development/python-modules/galois/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , setuptools-scm
 , pythonOlder
+, pythonRelaxDepsHook
 , fetchFromGitHub
 , pytestCheckHook
 , pytest-xdist
@@ -28,6 +29,7 @@ buildPythonPackage rec {
 
   nativeBuildInputs = [
     setuptools-scm
+    pythonRelaxDepsHook
   ];
 
   propagatedBuildInputs = [
@@ -41,11 +43,7 @@ buildPythonPackage rec {
     pytest-xdist
   ];
 
-  postPatch = ''
-     substituteInPlace pyproject.toml \
-       --replace "numpy >= 1.18.4, < 1.24" "numpy >= 1.18.4" \
-       --replace "numba >= 0.53, < 0.57" "numba >= 0.53" \
-    '';
+  pythonRelaxDeps = [ "numpy" "numba" ];
 
   pythonImportsCheck = [ "galois" ];
 
