096c1aab6ed71869bdb92499b7744514781918d4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-11 21:17:00 +0200

    pdepend: use `builbComposerProject` builder

--- pkgs/development/php-packages/pdepend/default.nix
+++ pkgs/development/php-packages/pdepend/default.nix
@@ -1,41 +1,31 @@
-{ stdenv, fetchurl, makeWrapper, lib, php }:
+{ lib, fetchFromGitHub, php }:
 
-let
+php.buildComposerProject (finalAttrs: {
   pname = "pdepend";
   version = "2.14.0";
-in
-stdenv.mkDerivation {
-  inherit pname version;
 
-  src = fetchurl {
-    url = "https://github.com/pdepend/pdepend/releases/download/${version}/pdepend.phar";
-    sha256 = "sha256-t6Yf+z/8O/tZuYoLAZo2G5bORh8XPeEMdK57dWjHsmk=";
+  src = fetchFromGitHub {
+    owner = "pdepend";
+    repo = "pdepend";
+    rev = finalAttrs.version;
+    hash = "sha256-ZmgMuOpUsx5JWTcPRS6qKbTWZvuOrBVOVdPMcvvTV20=";
   };
 
-  dontUnpack = true;
+  # TODO: Open a PR against https://github.com/pdepend/pdepend
+  # Missing `composer.lock` from the repository.
+  composerLock = ./composer.lock;
+  vendorHash = "sha256-DqMj3LrkpmKCxyQbr3/YKqwWOuJew7wHVwuxDf2pOfY=";
 
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/pdepend/pdepend.phar
-    makeWrapper ${php}/bin/php $out/bin/pdepend \
-      --add-flags "$out/libexec/pdepend/pdepend.phar"
-    runHook postInstall
-  '';
-
-  meta = with lib; {
+  meta = {
     description = "An adaptation of JDepend for PHP";
     homepage = "https://github.com/pdepend/pdepend";
-    license = licenses.bsd3;
+    license = lib.licenses.bsd3;
     longDescription = "
       PHP Depend is an adaptation of the established Java
       development tool JDepend. This tool shows you the quality
       of your design in terms of extensibility, reusability and
       maintainability.
     ";
-    maintainers = teams.php.members;
-    platforms = platforms.all;
+    maintainers = lib.teams.php.members;
   };
-}
+})
