acf13e56823961794fcc64162fca715f2e65fe37
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-11 21:58:00 +0200

    phpPackages.phan: use `builbComposerProject` builder

--- pkgs/development/php-packages/phan/default.nix
+++ pkgs/development/php-packages/phan/default.nix
@@ -1,33 +1,23 @@
-{ lib
-, php
-, mkDerivation
-, fetchurl
-, makeWrapper
-}:
-let
-  php' = php.withExtensions ({ enabled, all }: enabled ++ [ all.ast ]);
-in
-mkDerivation rec {
-  pname = "phan";
-  version = "5.4.1";
+{ lib, fetchFromGitHub, php }:
 
-  src = fetchurl {
-    url = "https://github.com/phan/phan/releases/download/${version}/phan.phar";
-    hash = "sha256-DJr1BWAfNI3hYvmBui5Dp+n7ec+f+gkOso21KEd6m8I=";
+php.buildComposerProject (finalAttrs: {
+  pname = "phan";
+  version = "5.4.2";
+
+  src = fetchFromGitHub {
+    owner = "phan";
+    repo = "phan";
+    rev = finalAttrs.version;
+    hash = "sha256-AoY0+2v9wXQQzrNLo3xnuq8djZg15epiV748/pUtBqM=";
   };
 
-  dontUnpack = true;
+  php = php.buildEnv {
+    extensions = ({ enabled, all }: enabled ++ (with all; [
+      ast
+    ]));
+  };
 
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/phan/phan.phar
-    makeWrapper ${php'}/bin/php $out/bin/phan \
-      --add-flags "$out/libexec/phan/phan.phar"
-    runHook postInstall
-  '';
+  vendorHash = "sha256-ARhcc7Cu8SHzy6dlj+iFjEuupnxdKX7XjLJ3nPNqzO4=";
 
   meta = with lib; {
     description = "Static analyzer for PHP";
@@ -37,6 +27,6 @@ mkDerivation rec {
     '';
     license = licenses.mit;
     homepage = "https://github.com/phan/phan";
-    maintainers = [ maintainers.apeschar ];
+    maintainers = with maintainers; [ apeschar ] ++ teams.php.members;
   };
-}
+})
