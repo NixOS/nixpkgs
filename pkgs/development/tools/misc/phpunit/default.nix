f3b67e4572fddfa81c22eda238f322455a6b7be6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-11 20:09:31 +0200

    phpunit: use `buildComposerProject` builder

--- pkgs/development/tools/misc/phpunit/default.nix
+++ pkgs/development/tools/misc/phpunit/default.nix
@@ -1,36 +1,27 @@
-{ stdenv, fetchurl, makeWrapper, lib, php }:
+{ lib, fetchFromGitHub, php }:
 
-let
+php.buildComposerProject (finalAttrs: {
   pname = "phpunit";
   version = "10.1.2";
-in
-stdenv.mkDerivation {
-  inherit pname version;
 
-  src = fetchurl {
-    url = "https://phar.phpunit.de/phpunit-${version}.phar";
-    hash = "sha256-X6upMANmKU2H8Qs0pOd/uRL2GdH/JPyOAQ/P8Z5yNl8=";
+  src = fetchFromGitHub {
+    owner = "sebastianbergmann";
+    repo = "phpunit";
+    rev = finalAttrs.version;
+    hash = "sha256-2TIiJ2ZXsad6ko81wOnqG9uGbfchWFiS7OEjz/3d958=";
   };
 
-  dontUnpack = true;
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/phpunit/phpunit.phar
-    makeWrapper ${php}/bin/php $out/bin/phpunit \
-      --add-flags "$out/libexec/phpunit/phpunit.phar"
-    runHook postInstall
-  '';
+  # TODO: Open a PR against https://github.com/sebastianbergmann/phpunit
+  # Missing `composer.lock` from the repository.
+  composerLock = ./composer.lock;
+  vendorHash = "sha256-vvTEAhXDBbszbhUy5J/YIbzlv1xrMLjmp4lo0Cnhmmg=";
 
   meta = with lib; {
     description = "PHP Unit Testing framework";
     license = licenses.bsd3;
     homepage = "https://phpunit.de";
-    changelog = "https://github.com/sebastianbergmann/phpunit/blob/${version}/ChangeLog-${lib.versions.majorMinor version}.md";
+    changelog = "https://github.com/sebastianbergmann/phpunit/blob/${finalAttrs.version}/ChangeLog-${lib.versions.majorMinor finalAttrs.version}.md";
     maintainers = with maintainers; [ onny ] ++ teams.php.members;
     platforms = platforms.all;
   };
-}
+})
