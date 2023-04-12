470b09cf881cfc3502789d74012538bf3beebb39
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-12 21:40:28 +0200

    python310Packages.ovh: 1.0.1 -> 1.1.0
    Changelog: https://github.com/ovh/python-ovh/blob/v1.1.0/CHANGELOG.md

--- pkgs/development/python-modules/ovh/default.nix
+++ pkgs/development/python-modules/ovh/default.nix
@@ -1,44 +1,43 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, mock
-, nose
+, pytestCheckHook
+, pythonOlder
 , requests
-, yanc
 }:
 
 buildPythonPackage rec {
   pname = "ovh";
-  version = "1.0.0";
+  version = "1.1.0";
   format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-IQzwu0gwfPNPOLQLCO99KL5Hu2094Y+acQBFXVGzHhU=";
+    hash = "sha256-EI+bWjtHEZPOSkWJx3gvS8y//gugMWl3TrBHKsKO9nk=";
   };
 
   propagatedBuildInputs = [
     requests
   ];
 
-  preCheck = ''
-    export HOME=$(mktemp -d)
-  '';
-
   nativeCheckInputs = [
-    mock
-    nose
-    yanc
+    pytestCheckHook
   ];
 
-  # requires network
-  checkPhase = ''
-    nosetests . \
-      -e test_config_get_conf \
-      -e test_config_get_custom_conf \
-      -e test_endpoints \
-      -e test_init_from_custom_config
-  '';
+  pythonImportsCheck = [
+    "ovh"
+  ];
+
+  disabledTests = [
+    # Tests require network access
+    "test_config_from_files"
+    "test_config_from_given_config_file"
+    "test_config_from_invalid_ini_file"
+    "test_config_from_only_one_file"
+    "test_endpoints"
+  ];
 
   meta = with lib; {
     description = "Thin wrapper around OVH's APIs";
