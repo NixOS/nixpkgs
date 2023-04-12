8c2f6a1d9e21236251dfde2e9ae0a23ab9266e38
Moritz 'e1mo' Fromm <git@e1mo.de>
2023-04-12 13:03:33 +0200

    dokuwiki: 2022-07-31a -> 2023-04-04
    See changelog at
    <https://www.dokuwiki.org/changes#release_2023-04-04_jack_jackrum> for
    list of (significant) changes.

--- pkgs/servers/web-apps/dokuwiki/default.nix
+++ pkgs/servers/web-apps/dokuwiki/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "dokuwiki";
-  version = "2022-07-31a";
+  version = "2023-04-04";
 
   src = fetchFromGitHub {
-    owner = "splitbrain";
+    owner = "dokuwiki";
     repo = pname;
-    rev = "release_stable_${version}";
-    sha256 = "sha256-gtWEtc3kbMokKycTx71XXblkDF39i926uN2kU3oOeVw=";
+    rev = "release-${version}";
+    sha256 = "sha256-QJnXKsEhvEcE88wvfMZR2j7X/pW8+28zlEnxhvhl+44=";
   };
 
   preload = writeText "preload.php" ''
