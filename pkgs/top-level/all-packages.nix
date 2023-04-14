188be504d7bf6fb8a3743a2d5e2aafe0d0a81805
Lily Foster <lily@lily.flowers>
2023-04-14 13:13:15 -0400

    hotdoc: init at 0.13.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8416,6 +8416,8 @@ with pkgs;
 
   hostname-debian = callPackage ../tools/networking/hostname-debian { };
 
+  hotdoc = python3Packages.callPackage ../development/tools/hotdoc { };
+
   hotpatch = callPackage ../development/libraries/hotpatch { };
 
   hotspot = libsForQt5.callPackage ../development/tools/analysis/hotspot { };
