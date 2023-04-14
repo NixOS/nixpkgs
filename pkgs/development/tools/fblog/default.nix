d5334e8dd6192584c0e5b280cfa9a866c8d504be
figsoda <figsoda@pm.me>
2023-04-14 10:10:48 -0400

    fblog: 4.2.0 -> 4.3.0
    Diff: https://github.com/brocode/fblog/compare/v4.2.0...v4.3.0

--- pkgs/development/tools/fblog/default.nix
+++ pkgs/development/tools/fblog/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "fblog";
-  version = "4.2.0";
+  version = "4.3.0";
 
   src = fetchFromGitHub {
     owner = "brocode";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-A5/TwISGsk9L0Z/ZF9Z/fxiMG+rcPgtAr2BNVJ+1Xfo=";
+    sha256 = "sha256-7+LoCYFI75268Qg2b64I1OOEMZuuxSI9S9Z0/XRLc70=";
   };
 
-  cargoSha256 = "sha256-BrhYHUx28Sb6eNU3nTMlMewtokyY6+I5ww+bBVshQqk=";
+  cargoSha256 = "sha256-VMuCSX+FSssha472Lxij2DAYmfmTxLbDWFiRuN9488Q=";
 
   meta = with lib; {
     description = "A small command-line JSON log viewer";
