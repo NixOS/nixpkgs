33f834312c27f11f1e781dac4468c0af6f3039b0
Dacio Romero <dacioromero@gmail.com>
2023-04-13 19:45:48 -0700

    move disabled to below version, use pythonOlder

--- pkgs/development/python-modules/syndom/default.nix
+++ pkgs/development/python-modules/syndom/default.nix
@@ -4,16 +4,17 @@
 , pkg-config
 , meson
 , ninja
-, isPy3k
 , pugixml
 , fmt
 , html-tidy
 , pybind11
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "syndom";
   version = "unstable-2023-03-25";
+  disabled = pythonOlder "3.6";
 
   format = "other";
 
@@ -23,9 +24,6 @@ buildPythonPackage rec {
     rev = "75920321062d682437f3fb0319dad227d8b18f6c";
     sha256 = "sha256-fOlE9CsNcmGkVBXaqYHxLDWB8voeRp46+dZYIJIwg7o=";
   };
-
-  # Python 2.x is not supported.
-  disabled = !isPy3k;
 
   nativeBuildInputs = [
     meson
