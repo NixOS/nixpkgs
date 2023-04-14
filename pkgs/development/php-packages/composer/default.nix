6e0a609dd741d7a0f61787c23d0a817e8cb4c328
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-14 12:33:57 +0200

    phpPackages.composer: use `buildComposerProject` builder

--- pkgs/development/php-packages/composer/default.nix
+++ pkgs/development/php-packages/composer/default.nix
@@ -1,27 +1,49 @@
-{ mkDerivation, fetchurl, makeBinaryWrapper, unzip, lib, php }:
+{ mkDerivation, lib, fetchFromGitHub, php, makeBinaryWrapper, fetchurl, unzip }:
 
-mkDerivation rec {
+php.buildComposerProject (finalAttrs: {
   pname = "composer";
   version = "2.5.5";
 
-  src = fetchurl {
-    url = "https://github.com/composer/composer/releases/download/${version}/composer.phar";
-    sha256 = "sha256-VmptHPS+HMOsiC0qKhOBf/rlTmD1qnyRN0NIEKWAn/w=";
+  composer = mkDerivation (finalComposerAttrs: {
+    inherit (finalAttrs) version;
+    pname = "composer-phar";
+
+    src = fetchurl {
+      url = "https://github.com/composer/composer/releases/download/${finalAttrs.version}/composer.phar";
+      sha256 = "sha256-VmptHPS+HMOsiC0qKhOBf/rlTmD1qnyRN0NIEKWAn/w=";
+    };
+
+    dontUnpack = true;
+
+    nativeBuildInputs = [ makeBinaryWrapper ];
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/bin
+      install -D $src $out/libexec/composer/composer.phar
+      makeWrapper ${php}/bin/php $out/bin/composer \
+        --add-flags "$out/libexec/composer/composer.phar" \
+        --prefix PATH : ${lib.makeBinPath [ unzip ]}
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+      description = "Dependency Manager for PHP, shipped from the PHAR file";
+      license = licenses.mit;
+      homepage = "https://getcomposer.org/";
+      changelog = "https://github.com/composer/composer/releases/tag/${version}";
+      maintainers = with maintainers; [ offline ] ++ teams.php.members;
+    };
+  });
+
+  src = fetchFromGitHub {
+    owner = "composer";
+    repo = "composer";
+    rev = finalAttrs.version;
+    hash = "sha256-eOZVJFa0GViO/jcFIonhJxAHD2DdpLOOmOPtqGMMl2w=";
   };
 
-  dontUnpack = true;
-
-  nativeBuildInputs = [ makeBinaryWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/composer/composer.phar
-    makeWrapper ${php}/bin/php $out/bin/composer \
-      --add-flags "$out/libexec/composer/composer.phar" \
-      --prefix PATH : ${lib.makeBinPath [ unzip ]}
-    runHook postInstall
-  '';
+  vendorHash = "sha256-gv6HMd6qDf1l2jtXdI9sqfWhcxlDknU2sudhGjq9Zyw=";
 
   meta = with lib; {
     description = "Dependency Manager for PHP";
@@ -30,4 +52,4 @@ mkDerivation rec {
     changelog = "https://github.com/composer/composer/releases/tag/${version}";
     maintainers = with maintainers; [ offline ] ++ teams.php.members;
   };
-}
+})
