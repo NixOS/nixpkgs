cfd3894b8e20b438ab78aabc163b5dccef4b2202
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-15 19:02:30 +0200

    wp-cli: use `buildComposerProject` builder

--- pkgs/development/tools/wp-cli/default.nix
+++ pkgs/development/tools/wp-cli/default.nix
@@ -1,59 +1,41 @@
-{ stdenv
-, lib
-, fetchurl
-, formats
-, installShellFiles
-, makeWrapper
-, php
-}:
+{ lib, fetchFromGitHub, php, makeBinaryWrapper }:
 
-let
-  version = "2.6.0";
-
-  completion = fetchurl {
-    url = "https://raw.githubusercontent.com/wp-cli/wp-cli/v${version}/utils/wp-completion.bash";
-    hash = "sha256-RDygYQzK6NLWrOug7EqnkpuH7Wz1T2Zq/tGNZjoYo5U=";
-  };
-
-  ini = (formats.ini { }).generate "php.ini" {
-    PHP.memory_limit = -1; # no limit as composer uses a lot of memory
-    Phar."phar.readonly" = "Off";
-  };
-
-in
-stdenv.mkDerivation rec {
+php.buildComposerProject (finalAttrs: {
   pname = "wp-cli";
-  inherit version;
+  version = "2.8.1";
 
-  src = fetchurl {
-    url = "https://github.com/wp-cli/wp-cli/releases/download/v${version}/${pname}-${version}.phar";
-    hash = "sha256-0WZSjKtgvIIpwGcp5wc4OPu6aNaytXRQTLAniDXIeIg=";
+  src = fetchFromGitHub {
+    owner = "wp-cli";
+    repo = "wp-cli";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-lLuOozVbaaKbfUemhcQBD1t4dqixX+Vw7f+XXc9NsnU=";
   };
 
-  nativeBuildInputs = [ installShellFiles makeWrapper ];
+  # TODO: Open a PR against https://github.com/wp-cli/wp-cli
+  # Missing `composer.lock` from the repository.
+  composerLock = ./composer.lock;
+  vendorHash = "sha256-ROTimNrEoVzfVbGm1yYqA32XDzGVn5beIoFNetAAiyo=";
 
-  buildCommand = ''
-    dir=$out/share/wp-cli
-    install -Dm444 ${src}        $dir/wp-cli
-    install -Dm444 ${ini}        $dir/php.ini
-    installShellCompletion --bash --name wp ${completion}
+  nativeBuildInputs = [
+    makeBinaryWrapper
+  ];
 
-    mkdir -p $out/bin
-    makeWrapper ${lib.getBin php}/bin/php $out/bin/wp \
-      --add-flags "-c $dir/php.ini" \
-      --add-flags "-f $dir/wp-cli" \
-      --add-flags "--"
+  php = php.buildEnv {
+    extraConfig = ''
+      phar.readonly=0
+    '';
+  };
 
-    # this is a very basic run test
-    $out/bin/wp --info >/dev/null
+  postInstall = ''
+    wrapProgram $out/bin/wp \
+      --prefix PATH ":" ${lib.makeBinPath [ php ]}
   '';
 
-  meta = with lib; {
+  meta = {
     description = "A command line interface for WordPress";
     homepage = "https://wp-cli.org";
-    license = licenses.mit;
-    maintainers = with maintainers; [ peterhoeg ];
-    platforms = platforms.all;
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.peterhoeg ];
     mainProgram = "wp";
   };
-}
+})
