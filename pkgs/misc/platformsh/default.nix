cfa72cdfa37ffb169b3f6d439023185b44ba0a1d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-15 18:23:22 +0200

    platformsh: use `buildComposerProject` builder

--- pkgs/misc/platformsh/default.nix
+++ pkgs/misc/platformsh/default.nix
@@ -1,44 +1,17 @@
-{ stdenv, fetchurl, makeWrapper, writeShellScript, lib, php, curl, jq, common-updater-scripts }:
+{ lib, fetchFromGitHub, php }:
 
-let
+php.buildComposerProject (finalAttrs: {
   pname = "platformsh";
-  version = "3.79.2";
-in
-stdenv.mkDerivation {
-  inherit pname version;
+  version = "4.4.1";
 
-  src = fetchurl {
-    url = "https://github.com/platformsh/platformsh-cli/releases/download/v${version}/platform.phar";
-    sha256 = "sha256-STGMKWgI4C6ccg8DGUhdnEENOB2//gtpU0ljM4cQCXI=";
+  src = fetchFromGitHub {
+    owner = "platformsh";
+    repo = "legacy-cli";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-1DCv4M7nCDrBeMoULO8kWQkMgEYGZmBaBfbsVpMCOVA=";
   };
 
-  dontUnpack = true;
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/platformsh/platform.phar
-    makeWrapper ${php}/bin/php $out/bin/platform \
-      --add-flags "$out/libexec/platformsh/platform.phar"
-    runHook postInstall
-  '';
-
-  passthru = {
-      updateScript = writeShellScript "update-${pname}" ''
-        set -o errexit
-        export PATH="${lib.makeBinPath [ curl jq common-updater-scripts ]}"
-        NEW_VERSION=$(curl -s https://api.github.com/repos/platformsh/platformsh-cli/releases/latest | jq .tag_name --raw-output)
-
-        if [[ "v${version}" = "$NEW_VERSION" ]]; then
-            echo "The new version same as the old version."
-            exit 0
-        fi
-
-        update-source-version "platformsh" "$NEW_VERSION"
-      '';
-    };
+  vendorHash = "sha256-VjM3Q1iBIojsLSDmUPY+iN2N4UnaZwJNataH3x4YHr4=";
 
   meta = with lib; {
     description = "The unified tool for managing your Platform.sh services from the command line.";
@@ -46,6 +19,5 @@ stdenv.mkDerivation {
     license = licenses.mit;
     maintainers = with maintainers; [ shyim ];
     mainProgram = "platform";
-    platforms = platforms.all;
   };
-}
+})
