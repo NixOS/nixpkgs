65640c3f4403a294f9cf721d23d1d1e130c91290
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-15 04:08:51 +0200

    drush: removed, standalone installation are no more supported

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -403,6 +403,7 @@ mapAliases ({
   double_conversion = throw "'double_conversion' has been renamed to/replaced by 'double-conversion'"; # Converted to throw 2022-02-22
   draftsight = throw "draftsight has been removed, no longer available as freeware"; # Added 2020-08-14
   dragon-drop = throw "'dragon-drop' has been removed in favor of 'xdragon'"; # Added 2022-04-10;
+  drush = throw "drush as a standalone package has been removed because it's no longer maintained as such and has been replaced by `drush-launcher`.";
   dtv-scan-tables_linuxtv = dtv-scan-tables;  # Added 2023-03-03
   dtv-scan-tables_tvheadend = dtv-scan-tables;  # Added 2023-03-03
   dust = throw "dust has been removed: abandoned by upstream"; # Added 2022-04-21
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18096,7 +18096,7 @@ with pkgs;
 
   drm_info = callPackage ../development/tools/drm_info { };
 
-  drush = callPackage ../development/tools/misc/drush { };
+  drush-launcher = callPackage ../development/tools/misc/drush-launcher { };
 
   dura = callPackage ../development/tools/misc/dura {
     inherit (darwin.apple_sdk.frameworks) Security;
