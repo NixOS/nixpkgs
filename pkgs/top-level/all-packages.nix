a671c0a295872dc712797c7b660aa7778c0522f0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-15 04:20:19 +0200

    robo: init at 3.0.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11839,6 +11839,8 @@ with pkgs;
 
   rnv = callPackage ../tools/text/xml/rnv { };
 
+  robo = callPackage ../development/tools/robo { };
+
   rosie = callPackage ../tools/text/rosie { };
 
   rounded-mgenplus = callPackage ../data/fonts/rounded-mgenplus { };
