7ae4d8cff2f070d87431c73aa2c3ebed0ccf9a24
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-15 18:23:22 +0200

    platformsh: use `buildComposerProject` builder

--- pkgs/misc/platformsh/default.nix
+++ pkgs/misc/platformsh/default.nix
@@ -1,51 +1,23 @@
-{ stdenv, fetchurl, makeWrapper, writeShellScript, lib, php, curl, jq, common-updater-scripts }:
+{ lib, fetchFromGitHub, php }:
 
-let
+php.buildComposerProject (finalAttrs: {
   pname = "platformsh";
-  version = "3.79.2";
-in
-stdenv.mkDerivation {
-  inherit pname version;
+  version = "4.6.1";
 
-  src = fetchurl {
-    url = "https://github.com/platformsh/platformsh-cli/releases/download/v${version}/platform.phar";
-    sha256 = "sha256-STGMKWgI4C6ccg8DGUhdnEENOB2//gtpU0ljM4cQCXI=";
+  src = fetchFromGitHub {
+    owner = "platformsh";
+    repo = "legacy-cli";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-YbzgPa0Y2PYTMZcWHdiejNHt+XgwJ6ZtahpV1RYs9OQ=";
   };
 
-  dontUnpack = true;
+  vendorHash = "sha256-IcJyRLO1VJ+4oskv36E9ejtYD23aBeB9aRM30WcEIPU=";
 
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/platformsh/platform.phar
-    makeWrapper ${php}/bin/php $out/bin/platform \
-      --add-flags "$out/libexec/platformsh/platform.phar"
-    runHook postInstall
-  '';
-
-  passthru = {
-      updateScript = writeShellScript "update-${pname}" ''
-        set -o errexit
-        export PATH="${lib.makeBinPath [ curl jq common-updater-scripts ]}"
-        NEW_VERSION=$(curl -s https://api.github.com/repos/platformsh/platformsh-cli/releases/latest | jq .tag_name --raw-output)
-
-        if [[ "v${version}" = "$NEW_VERSION" ]]; then
-            echo "The new version same as the old version."
-            exit 0
-        fi
-
-        update-source-version "platformsh" "$NEW_VERSION"
-      '';
-    };
-
-  meta = with lib; {
+  meta = {
     description = "The unified tool for managing your Platform.sh services from the command line.";
     homepage = "https://github.com/platformsh/platformsh-cli";
-    license = licenses.mit;
-    maintainers = with maintainers; [ shyim ];
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.shyim ];
     mainProgram = "platform";
-    platforms = platforms.all;
   };
-}
+})
