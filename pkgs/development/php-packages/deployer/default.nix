8b1ea340a27831a0cf58f0177e7ddc379c6ba108
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-04-15 18:02:47 +0200

    phpPackages.deployer: use `buildComposerProject` builder

--- pkgs/development/php-packages/deployer/default.nix
+++ pkgs/development/php-packages/deployer/default.nix
@@ -1,36 +1,23 @@
-{ mkDerivation, fetchurl, makeWrapper, installShellFiles, lib, php }:
+{ lib, fetchFromGitHub, php }:
 
-mkDerivation rec {
+php.buildComposerProject (finalAttrs: {
   pname = "deployer";
-  version = "6.8.0";
+  version = "7.3.1";
 
-  src = fetchurl {
-    url = "https://deployer.org/releases/v${version}/${pname}.phar";
-    sha256 = "09mxwfa7yszsiljbkxpsd4sghqngl08cn18v4g1fbsxp3ib3kxi5";
+  src = fetchFromGitHub {
+    owner = "deployphp";
+    repo = "deployer";
+    rev = "v${finalAttrs.version}^";
+    hash = "sha256-bsJcozAVvRXUNdxRdBh5SB6qESUSXkkPeEWF50IiboQ=";
   };
 
-  dontUnpack = true;
-
-  nativeBuildInputs = [ makeWrapper installShellFiles ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    install -D $src $out/libexec/deployer/deployer.phar
-    makeWrapper ${php}/bin/php $out/bin/dep --add-flags "$out/libexec/deployer/deployer.phar"
-
-    # fish support currently broken: https://github.com/deployphp/deployer/issues/2527
-    installShellCompletion --cmd dep \
-      --bash <($out/bin/dep autocomplete --install) \
-      --zsh <($out/bin/dep autocomplete --install)
-    runHook postInstall
-  '';
+  vendorHash = "sha256-GEzHjd26Hl+4JYxcFhURQu8JOGQViSjZU/I+9I2aaFU=";
 
   meta = with lib; {
     description = "A deployment tool for PHP";
     license = licenses.mit;
     homepage = "https://deployer.org/";
     mainProgram = "dep";
-    maintainers = with maintainers; teams.php.members;
+    maintainers = teams.php.members;
   };
-}
+})
