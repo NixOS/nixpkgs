0495725a1f8e9df1ac3996459a9bb82305e69242
hacker1024 <hacker1024@users.sourceforge.net>
2023-04-17 03:01:32 +1000

    flutter.buildFlutterApplication: Introduce a package override repository
    Some Flutter packages require additional attribute values to be added to buildFlutterApplication, to add things like libraries and environment variables.
    
    To prevent duplication in applications that use the packages, a repository of package overrides is kept. buildFlutterApplication will look for package overrides for each dependency, and apply them by calling overrideAttrs on itself.

--- pkgs/build-support/flutter/default.nix
+++ pkgs/build-support/flutter/default.nix
@@ -12,6 +12,7 @@
 { pubGetScript ? "flutter pub get"
 , flutterBuildFlags ? [ ]
 , runtimeDependencies ? [ ]
+, customPackageOverrides ? { }
 , vendorHash
 , pubspecLockFile ? null
 , nativeBuildInputs ? [ ]
@@ -31,79 +32,90 @@ let
     buildDrvArgs = args;
   };
 
+  baseDerivation = llvmPackages_13.stdenv.mkDerivation (finalAttrs: args // {
+    inherit flutterBuildFlags runtimeDependencies;
+
+    outputs = [ "out" "debug" ];
+
+    nativeBuildInputs = [
+      makeWrapper
+      deps
+      flutter
+    ] ++ nativeBuildInputs;
+
+    configurePhase = ''
+      runHook preConfigure
+
+      ${flutterSetupScript}
+
+      runHook postConfigure
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+
+      mkdir -p build/flutter_assets/fonts
+
+      flutter packages get --offline -v
+      flutter build linux -v --release --split-debug-info="$debug" ${builtins.concatStringsSep " " (map (flag: "\"${flag}\"") finalAttrs.flutterBuildFlags)}
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      built=build/linux/*/release/bundle
+
+      mkdir -p $out/bin
+      mv $built $out/app
+
+      for f in $(find $out/app -iname "*.desktop" -type f); do
+        install -D $f $out/share/applications/$(basename $f)
+      done
+
+      for f in $(find $out/app -maxdepth 1 -type f); do
+        ln -s $f $out/bin/$(basename $f)
+      done
+
+      # make *.so executable
+      find $out/app -iname "*.so" -type f -exec chmod +x {} +
+
+      # remove stuff like /build/source/packages/ubuntu_desktop_installer/linux/flutter/ephemeral
+      for f in $(find $out/app -executable -type f); do
+        if patchelf --print-rpath "$f" | grep /build; then # this ignores static libs (e,g. libapp.so) also
+          echo "strip RPath of $f"
+          newrp=$(patchelf --print-rpath $f | sed -r "s|/build.*ephemeral:||g" | sed -r "s|/build.*profile:||g")
+          patchelf --set-rpath "$newrp" "$f"
+        fi
+      done
+
+      runHook postInstall
+    '';
+
+    postFixup = ''
+      # Add runtime library dependencies to the LD_LIBRARY_PATH.
+      # For some reason, the RUNPATH of the executable is not used to load dynamic libraries in dart:ffi with DynamicLibrary.open().
+      #
+      # This could alternatively be fixed with patchelf --add-needed, but this would cause all the libraries to be opened immediately,
+      # which is not what application authors expect.
+      for f in "$out"/bin/*; do
+        wrapProgram "$f" \
+          --suffix LD_LIBRARY_PATH : '${lib.makeLibraryPath finalAttrs.runtimeDependencies}'
+      done
+
+      ${postFixup}
+    '';
+  });
+
+  packageOverrideRepository = (callPackage ../../development/compilers/flutter/package-overrides { }) // customPackageOverrides;
+  packages = callPackage ../dart/list-dart-deps { dart = flutter; } deps;
+  productPackages = builtins.filter (package: package.kind != "dev") packages;
 in
-llvmPackages_13.stdenv.mkDerivation (finalAttrs: args // {
-  inherit flutterBuildFlags runtimeDependencies;
-
-  outputs = [ "out" "debug" ];
-
-  nativeBuildInputs = [
-    makeWrapper
-    deps
-    flutter
-  ] ++ nativeBuildInputs;
-
-  configurePhase = ''
-    runHook preConfigure
-
-    ${flutterSetupScript}
-
-    runHook postConfigure
-  '';
-
-  buildPhase = ''
-    runHook preBuild
-
-    mkdir -p build/flutter_assets/fonts
-
-    flutter packages get --offline -v
-    flutter build linux -v --release --split-debug-info="$debug" ${builtins.concatStringsSep " " (map (flag: "\"${flag}\"") finalAttrs.flutterBuildFlags)}
-
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    built=build/linux/*/release/bundle
-
-    mkdir -p $out/bin
-    mv $built $out/app
-
-    for f in $(find $out/app -iname "*.desktop" -type f); do
-      install -D $f $out/share/applications/$(basename $f)
-    done
-
-    for f in $(find $out/app -maxdepth 1 -type f); do
-      ln -s $f $out/bin/$(basename $f)
-    done
-
-    # make *.so executable
-    find $out/app -iname "*.so" -type f -exec chmod +x {} +
-
-    # remove stuff like /build/source/packages/ubuntu_desktop_installer/linux/flutter/ephemeral
-    for f in $(find $out/app -executable -type f); do
-      if patchelf --print-rpath "$f" | grep /build; then # this ignores static libs (e,g. libapp.so) also
-        echo "strip RPath of $f"
-        newrp=$(patchelf --print-rpath $f | sed -r "s|/build.*ephemeral:||g" | sed -r "s|/build.*profile:||g")
-        patchelf --set-rpath "$newrp" "$f"
-      fi
-    done
-
-    runHook postInstall
-  '';
-
-  postFixup = ''
-    # Add runtime library dependencies to the LD_LIBRARY_PATH.
-    # For some reason, the RUNPATH of the executable is not used to load dynamic libraries in dart:ffi with DynamicLibrary.open().
-    #
-    # This could alternatively be fixed with patchelf --add-needed, but this would cause all the libraries to be opened immediately,
-    # which is not what application authors expect.
-    for f in "$out"/bin/*; do
-      wrapProgram "$f" \
-        --suffix LD_LIBRARY_PATH : '${lib.makeLibraryPath finalAttrs.runtimeDependencies}'
-    done
-
-    ${postFixup}
-  '';
-})
+builtins.foldl'
+  (prev: package:
+  if packageOverrideRepository ? ${package.name}
+  then prev.overrideAttrs packageOverrideRepository.${package.name}
+  else prev)
+  baseDerivation
+  productPackages
