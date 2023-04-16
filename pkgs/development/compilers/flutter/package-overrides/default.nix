f151560cf02d9b43b1ba9aaa5836262a10426dd3
hacker1024 <hacker1024@users.sourceforge.net>
2023-04-17 03:08:03 +1000

    flutter: package-overrides: olm: init

--- pkgs/development/compilers/flutter/package-overrides/default.nix
+++ pkgs/development/compilers/flutter/package-overrides/default.nix
@@ -4,4 +4,5 @@
   flutter_secure_storage_linux = callPackage ./flutter-secure-storage-linux { };
   handy_window = callPackage ./handy-window { };
   matrix = callPackage ./matrix { };
+  olm = callPackage ./olm { };
 }
