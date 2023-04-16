6b4d7301fe456002729dfbade9609c787b29684a
Mario Rodas <marsam@users.noreply.github.com>
2023-04-16 04:20:00 +0000

    arkade: 0.9.11 -> 0.9.12
    https://github.com/alexellis/arkade/releases/tag/0.9.12

--- pkgs/applications/networking/cluster/arkade/default.nix
+++ pkgs/applications/networking/cluster/arkade/default.nix
@@ -7,20 +7,20 @@
 
 buildGoModule rec {
   pname = "arkade";
-  version = "0.9.11";
+  version = "0.9.12";
 
   src = fetchFromGitHub {
     owner = "alexellis";
     repo = "arkade";
     rev = version;
-    sha256 = "sha256-Bj/HfivAd7SmypBMOBPsa/7d0qG+FckAO2zcqNKY+kQ=";
+    sha256 = "sha256-90X+MRZXjKoXsUvfsUQUZcd90XcOIqyVXBbvEtPynPk=";
   };
 
   CGO_ENABLED = 0;
 
   nativeBuildInputs = [ installShellFiles ];
 
-  vendorHash = "sha256-QLD2Hjjn/Eo4LSn4bvvoEl5SS3p1ELWICaI3UhAg2/8=";
+  vendorHash = "sha256-/NJ5Y0uN9gAeYvuPWFSFuL83vOS9S8WJeCSZUkOLFMU=";
 
   # Exclude pkg/get: tests downloading of binaries which fail when sandbox=true
   subPackages = [
