b159ec94dcc9f263dd1204644df539ba0324f116
Jonas Heinrich <onny@project-insanity.org>
2023-04-16 10:23:07 +0200

    python3Packages.mpv: 1.0.1 -> 1.0.3

--- pkgs/development/python-modules/mpv/default.nix
+++ pkgs/development/python-modules/mpv/default.nix
@@ -1,18 +1,31 @@
-{ lib, stdenv, buildPythonPackage, fetchFromGitHub, python, isPy27
+{ lib
+, stdenv
+, buildPythonPackage
+, fetchFromGitHub
+, python
+, isPy27
 , mpv
+, setuptools
+, pytestCheckHook
+, xvfbwrapper
+, xorgserver
 }:
 
 buildPythonPackage rec {
   pname = "mpv";
-  version = "1.0.1";
+  version = "1.0.3";
+  format = "pyproject";
+
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "jaseg";
     repo = "python-mpv";
     rev = "v${version}";
-    hash = "sha256-UCJ1PknnWQiFciTEMxTUqDzz0Z8HEWycLuQqYeyQhoM=";
+    hash = "sha256-BM9vb0KPA9ChZC2bJ3f8F9PrCkwIom1lQTzbCo5UOcc=";
   };
+
+  propagatedBuildInputs = [ setuptools ];
 
   buildInputs = [ mpv ];
 
@@ -22,13 +35,21 @@ buildPythonPackage rec {
                 'sofile = "${mpv}/lib/libmpv${stdenv.targetPlatform.extensions.sharedLibrary}"'
   '';
 
-  # tests impure, will error if it can't load libmpv.so
-  doCheck = false;
+  nativeCheckInputs = [
+    xorgserver
+  ];
+
+  checkInputs = [
+    pytestCheckHook
+    xvfbwrapper
+  ];
+
   pythonImportsCheck = [ "mpv" ];
 
   meta = with lib; {
     description = "A python interface to the mpv media player";
     homepage = "https://github.com/jaseg/python-mpv";
     license = licenses.agpl3Plus;
+    maintainers = with maintainers; [ onny ];
   };
 }
