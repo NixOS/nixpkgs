b4f4557d85243654a2f793e817519ab325fda954
Florian Klink <flokli@flokli.de>
2023-04-18 10:16:05 +0200

    gnomeExtensions.pop-shell: unstable-2022-10-11 -> unstable-2023-04-05
    This supports GNOME 44.

--- pkgs/desktops/gnome/extensions/pop-shell/default.nix
+++ pkgs/desktops/gnome/extensions/pop-shell/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-pop-shell";
-  version = "unstable-2022-10-11";
+  version = "unstable-2023-04-05";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "shell";
-    rev = "886a069c0582b371e90ac3602b1747ea5fba616c";
-    hash = "sha256-DwJ3IFhYuEW0LQrmN/mOspEiudj4X5BCLhdOyP74EVs=";
+    rev = "fab66e19a91c47495a213218f0a247cbc5f58c1f";
+    hash = "sha256-8eHiZyQzORvdZVgA0bgbmv50w+qhxcK2ItcvCjBJ0eE=";
   };
 
   nativeBuildInputs = [ glib nodePackages.typescript gjs ];
