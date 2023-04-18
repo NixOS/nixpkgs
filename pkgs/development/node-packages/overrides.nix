e597eb631b84417af1d67c358f7d6d33a55b5ece
Malo Bourgon <mbourgon@gmail.com>
2023-04-18 11:32:41 -0700

    nodePackages.firebase-tools: fix darwin build

--- pkgs/development/node-packages/overrides.nix
+++ pkgs/development/node-packages/overrides.nix
@@ -176,6 +176,10 @@ final: prev: {
     '';
   };
 
+  firebase-tools = prev.firebase-tools.override {
+    nativeBuildInputs = lib.optionals stdenv.isDarwin  [ pkgs.xcbuild ];
+  };
+
   flood = prev.flood.override {
     buildInputs = [ final.node-pre-gyp ];
   };
