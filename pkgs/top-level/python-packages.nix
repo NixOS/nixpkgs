22114b44bd01225ee690cb87962d927b54d54319
Gaetan Lepage <gaetan@glepage.com>
2023-04-18 17:06:01 +0200

    python3Packages.ml-dtypes: init at 0.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6564,6 +6564,8 @@ self: super: with self; {
 
   ml-collections = callPackage ../development/python-modules/ml-collections { };
 
+  ml-dtypes = callPackage ../development/python-modules/ml-dtypes { };
+
   mlflow = callPackage ../development/python-modules/mlflow { };
 
   mlrose = callPackage ../development/python-modules/mlrose { };
