d3648861353cf50fa8e315c4b2998ac1dbb92858
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-18 08:52:29 +0200

    python310Packages.meraki: add changelog to meta

--- pkgs/development/python-modules/meraki/default.nix
+++ pkgs/development/python-modules/meraki/default.nix
@@ -27,6 +27,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Provides all current Meraki dashboard API calls to interface with the Cisco Meraki cloud-managed platform";
     homepage = "https://github.com/meraki/dashboard-api-python";
+    changelog = "https://github.com/meraki/dashboard-api-python/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ dylanmtaylor ];
   };
