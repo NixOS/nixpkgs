e4d53d9d7268254d615fa9da6c45d71080bb74d0
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-18 12:05:27 +0000

    clj-kondo: 2023.03.17 -> 2023.04.14

--- pkgs/development/tools/clj-kondo/default.nix
+++ pkgs/development/tools/clj-kondo/default.nix
@@ -2,11 +2,11 @@
 
 buildGraalvmNativeImage rec {
   pname = "clj-kondo";
-  version = "2023.03.17";
+  version = "2023.04.14";
 
   src = fetchurl {
     url = "https://github.com/clj-kondo/${pname}/releases/download/v${version}/${pname}-${version}-standalone.jar";
-    sha256 = "sha256-hI/0kYAQtkDSu8LE8CO6+2zDA6OOK/MdybsLQEPMkCk=";
+    sha256 = "sha256-SuhLt0FNZNRX803Doa2xT9a8n35WxDtOwxXj+dZ7YXc=";
   };
 
   extraNativeImageBuildArgs = [
