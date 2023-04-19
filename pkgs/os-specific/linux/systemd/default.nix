NixOS/nixpkgs

commit 0da24fa3ba069404faf7820767e01dd8e7ca1c3e

systemd: disable BPF for ARMv5
ARMv5 does not have hardware floating point, so can't build systemd's
BPF code.

pkgs/os-specific/linux/systemd/default.nix
@@ -93,6 +93,7 @@
 , withHwdb ? true
 , withImportd ? !stdenv.hostPlatform.isMusl
 , withLibBPF ? lib.versionAtLeast buildPackages.llvmPackages.clang.version "10.0"
+    && stdenv.hostPlatform.isAarch -> lib.versionAtLeast stdenv.hostPlatform.parsed.cpu.version "6" # assumes hard floats
     && !stdenv.hostPlatform.isMips64   # see https://github.com/NixOS/nixpkgs/pull/194149#issuecomment-1266642211
 , withLocaled ? true
 , withLogind ? true

commit 252d8459ff500ed248fe29d9dafaafd0f5a2371b

systemd: fix precedence in withLibBPF condition
The Clang version check was being ignored, as long as the CPU version
was greater than 6.  Oops.

Fixes: 0da24fa3ba0 ("systemd: disable BPF for ARMv5")

pkgs/os-specific/linux/systemd/default.nix
@@ -98,7 +98,7 @@
 , withImportd ? !stdenv.hostPlatform.isMusl
 , withKmod ? true
 , withLibBPF ? lib.versionAtLeast buildPackages.llvmPackages.clang.version "10.0"
-    && stdenv.hostPlatform.isAarch -> lib.versionAtLeast stdenv.hostPlatform.parsed.cpu.version "6" # assumes hard floats
+    && (stdenv.hostPlatform.isAarch -> lib.versionAtLeast stdenv.hostPlatform.parsed.cpu.version "6") # assumes hard floats
     && !stdenv.hostPlatform.isMips64   # see https://github.com/NixOS/nixpkgs/pull/194149#issuecomment-1266642211
 , withLibidn2 ? true
 , withLocaled ? true