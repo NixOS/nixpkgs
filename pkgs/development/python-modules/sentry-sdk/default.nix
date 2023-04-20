5ae746ffbde9b79335a9f796f6a41bf82023b9bf
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-04-20 20:39:38 +0200

    python310Packages.sentry-sdk: 1.18.0 -> 1.20.0
    https://github.com/getsentry/sentry-python/releases/tag/1.19.0
    https://github.com/getsentry/sentry-python/releases/tag/1.19.1
    https://github.com/getsentry/sentry-python/releases/tag/1.20.0

--- pkgs/development/python-modules/sentry-sdk/default.nix
+++ pkgs/development/python-modules/sentry-sdk/default.nix
@@ -40,7 +40,7 @@
 
 buildPythonPackage rec {
   pname = "sentry-sdk";
-  version = "1.18.0";
+  version = "1.20.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -49,7 +49,7 @@ buildPythonPackage rec {
     owner = "getsentry";
     repo = "sentry-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-4HOJ6ouUU6RqtxYOsnoojT4aQ/h8YzcLHTO8lFZecZw=";
+    hash = "sha256-fAlEh3S95Dyw1xHK2XaqbA6xUsASXbhzELGZTH/G+kg=";
   };
 
   propagatedBuildInputs = [
