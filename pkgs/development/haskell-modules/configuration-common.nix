e60cbf54246d86ee7e5d6086f27318de6e3ce0c4
Joachim Breitner <mail@joachim-breitner.de>
2023-04-21 14:31:06 +0200

    Stash incomplete work on minimizing texdoc installation

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -746,7 +746,21 @@ self: super: {
   Southpaw = dontDistribute super.Southpaw;
 
   # Add pdflatex to let it build docs (ends up in lhs2tex.doc)
-  lhs2tex = addBuildTools [ pkgs.texlive.combined.scheme-full ] super.lhs2tex;
+  lhs2tex = addBuildTools [
+    (pkgs.texlive.combine { inherit (pkgs.texlive)
+      scheme-basic
+      koma-script
+      stmaryrd
+      paralist
+      relsize
+      psnfss
+      palatino
+      mathpazo
+      rsfs
+      ec
+      ;
+    })
+  ] super.lhs2tex;
 
   # https://ghc.haskell.org/trac/ghc/ticket/9825
   vimus = overrideCabal (drv: { broken = pkgs.stdenv.isLinux && pkgs.stdenv.isi686; }) super.vimus;
