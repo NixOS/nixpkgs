d0e1f5ebce855fa0d8dec005153e0996cd0844ce
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-21 22:19:52 +0200

    yara: 4.3.0 -> 4.3.1
    Diff: https://github.com/VirusTotal/yara/compare/v4.3.0...v4.3.1

--- pkgs/tools/security/yara/default.nix
+++ pkgs/tools/security/yara/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation rec {
   pname = "yara";
-  version = "4.3.0";
+  version = "4.3.1";
 
   src = fetchFromGitHub {
     owner = "VirusTotal";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-xjGlK0jUDpkDXnI0odErtF+Xcx0I/orD0v5EZw8mhvs=";
+    hash = "sha256-Q+Q52W/MhurG3x0CIr0nv31qc4bdaLDk9AGGpMxKOcI=";
   };
 
   nativeBuildInputs = [
