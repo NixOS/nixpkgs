73ef9868cfac99fcd8e4a840e43c29faaeafb23b
Jan Tojnar <jtojnar@gmail.com>
2023-04-21 21:43:30 +0200

    webkitgtk: 2.40.0 → 2.40.1
    https://webkitgtk.org/2023/04/20/webkitgtk2.40.1-released.html
    https://webkitgtk.org/security/WSA-2023-0003.html
    
    CVE-2022-0108
    CVE-2022-32885
    CVE-2023-27932
    CVE-2023-27954
    CVE-2023-28205

--- pkgs/development/libraries/webkitgtk/default.nix
+++ pkgs/development/libraries/webkitgtk/default.nix
@@ -71,7 +71,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "webkitgtk";
-  version = "2.40.0";
+  version = "2.40.1";
   name = "${finalAttrs.pname}-${finalAttrs.version}+abi=${if lib.versionAtLeast gtk3.version "4.0" then "6.0" else "4.${if lib.versions.major libsoup.version == "2" then "0" else "1"}"}";
 
   outputs = [ "out" "dev" "devdoc" ];
@@ -82,7 +82,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://webkitgtk.org/releases/webkitgtk-${finalAttrs.version}.tar.xz";
-    hash = "sha256-pGB+ob+JZp6JscssY/quxRP5PeCbauYMxx1qiqt6s5M=";
+    hash = "sha256-ZOUmmE+M0hYe8DrpSa+ZwAL/Mz1hXmOGtGAWSjwbfvY=";
   };
 
   patches = lib.optionals stdenv.isLinux [
