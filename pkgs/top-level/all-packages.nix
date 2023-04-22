4c4d57b78da137b99c502463efbac5cdb13b3b37
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-04-22 14:28:33 +0800

    flowtime: init at 3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29926,6 +29926,8 @@ with pkgs;
 
   expenses = callPackage ../applications/misc/expenses { };
 
+  flowtime = callPackage ../applications/misc/flowtime { };
+
   fnott = callPackage ../applications/misc/fnott { };
 
   furnace = callPackage ../applications/audio/furnace {
