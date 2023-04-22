72f8bb1c8201eafd8c4b808c62fbdde7f0353637
traxys <quentin+dev@familleboyer.net>
2023-04-22 20:14:09 +0200

    python310Packages.watchdog-gevent: init at 0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15421,6 +15421,8 @@ self: super: with self; {
     inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices;
   };
 
+  watchdog-gevent = callPackage ../development/python-modules/watchdog-gevent { };
+
   watchfiles = callPackage ../development/python-modules/watchfiles {
     inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices;
   };
