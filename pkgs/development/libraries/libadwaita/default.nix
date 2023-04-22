c3e9a94359522691d47b60b0df972c09babe237d
Robert Schütz <nix@dotlambda.de>
2023-04-22 16:04:14 -0700

    libadwaita: 1.3.1 -> 1.3.2
    Diff: https://gitlab.gnome.org/GNOME/libadwaita/-/compare/1.3.1...1.3.2
    
    Changelog: https://gitlab.gnome.org/GNOME/libadwaita/-/blob/1.3.2/NEWS

--- pkgs/development/libraries/libadwaita/default.nix
+++ pkgs/development/libraries/libadwaita/default.nix
@@ -20,7 +20,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libadwaita";
-  version = "1.3.1";
+  version = "1.3.2";
 
   outputs = [ "out" "dev" "devdoc" ];
   outputBin = "devdoc"; # demo app
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
     owner = "GNOME";
     repo = "libadwaita";
     rev = version;
-    hash = "sha256-RIJtlSBZX4+rMOGQaFn31CCEKkWtPjtzO4fcX+iApvs=";
+    hash = "sha256-9Qha8xN3lC/t5dQNYPbgMX6HAKgEk80pyycrd5MGYLo=";
   };
 
   depsBuildBuild = [
