NixOS/nixpkgs

commit fa8367c2d50781f3e49ed424ea61af0c77615069

linux_6_1: rebuild on x86_64-linux
Just one-off work around these hash mismatches:
https://hydra.nixos.org/build/216994032#tabs-buildsteps
(I don't know a better way, I'm afraid)

pkgs/os-specific/linux/kernel/linux-6.1.nix
@@ -15,4 +15,6 @@ buildLinux (args // rec {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
     sha256 = "149h95r5msvqah868zd36y92ls9h41cr1rb5vzinl20mxdn46wnb";
   };
+  # TODO: possible to remove after any rebuild, e.g. after update.
+  extraConfig = lib.optionalString (buildPackages.system == "x86_64-linux") "\n";
 } // (args.argsOverride or { }))

commit 6c43a3495a11e261e5f41e5d7eda2d71dae1b2fe

linux_6_1: fixup evaluation without aliases
I'm sorry, really.  Broken by me in fa8367c2d507

pkgs/os-specific/linux/kernel/linux-6.1.nix
@@ -16,5 +16,5 @@ buildLinux (args // rec {
     sha256 = "149h95r5msvqah868zd36y92ls9h41cr1rb5vzinl20mxdn46wnb";
   };
   # TODO: possible to remove after any rebuild, e.g. after update.
-  extraConfig = lib.optionalString (buildPackages.system == "x86_64-linux") "\n";
+  extraConfig = lib.optionalString (buildPackages.stdenv.system == "x86_64-linux") "\n";
 } // (args.argsOverride or { }))