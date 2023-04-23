c8f2f13fdaef8021db6ca1c834caba6ef141fc0f
06kellyjac <dev@j-k.io>
2023-04-23 12:52:55 +0100

    cargo-geiger: 0.11.5 -> 0.11.6
    Diff: https://github.com/rust-secure-code/cargo-geiger/compare/cargo-geiger-0.11.5...cargo-geiger-0.11.6
    
    Changelog: https://github.com/rust-secure-code/cargo-geiger/blob/cargo-geiger-0.11.6/CHANGELOG.md

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16148,9 +16148,7 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
   cargo-fuzz = callPackage ../development/tools/rust/cargo-fuzz { };
-  cargo-geiger = callPackage ../development/tools/rust/cargo-geiger {
-    inherit (darwin.apple_sdk.frameworks) Security CoreFoundation;
-  };
+  cargo-geiger = callPackage ../development/tools/rust/cargo-geiger { };
 
   cargo-hf2 = callPackage ../development/tools/rust/cargo-hf2 {
     inherit (darwin.apple_sdk.frameworks) AppKit;
