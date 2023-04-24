a541b37190bdb1a5b4c9bc1bd091cd9448109462
Cynthia Fox <cyntheticfox@gh0st.sh>
2023-04-24 19:24:41 -0400

    maintainers: Add `cyntheticfox` key fingerprint

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3307,6 +3307,9 @@
     email = "cyntheticfox@gh0st.sh";
     github = "cyntheticfox";
     githubId = 17628961;
+    keys = [{
+      fingerprint = "73C1 C5DF 51E7 BB92 85E9  A262 5960 278C E235 F821";
+    }];
     matrix = "@houstdav000:gh0st.ems.host";
     name = "Cynthia Fox";
   };
