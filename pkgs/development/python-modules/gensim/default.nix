ec03d863254bde031d6f41f19fd115aa8ae31b9f
Kelvin John Falk Szolnoky <me@kelszo.com>
2023-04-24 12:50:34 +0200

    python3Packages.gensim: 4.3.0 -> 4.3.1

--- pkgs/development/python-modules/gensim/default.nix
+++ pkgs/development/python-modules/gensim/default.nix
@@ -6,7 +6,6 @@
 , numpy
 , scipy
 , smart-open
-, testfixtures
 , pyemd
 , pytestCheckHook
 , pythonOlder
@@ -14,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "gensim";
-  version = "4.3.0";
+  version = "4.3.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ZL1+ximQIVh4gi6LJWnRg1BU9WzfU2AN3+mSfjHztI0=";
+    hash = "sha256-i18RwOalMICGtI6PaEEiOk+ho31RNoRhK37oVLUzAV8=";
   };
 
   nativeBuildInputs = [
@@ -56,8 +55,5 @@ buildPythonPackage rec {
     homepage = "https://radimrehurek.com/gensim/";
     license = licenses.lgpl21Only;
     maintainers = with maintainers; [ jyp ];
-    # python310 errors as: No matching distribution found for FuzzyTM>=0.4.0
-    # python311 errors as: longintrepr.h: No such file or directory
-    broken = true; # At 2023-02-05
   };
 }
