fefbedb149024dc55f640fb0faf5b79af5012134
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-24 13:33:05 +0000

    kodiPackages.arteplussept: 1.1.8 -> 1.1.9

--- pkgs/applications/video/kodi/addons/arteplussept/default.nix
+++ pkgs/applications/video/kodi/addons/arteplussept/default.nix
@@ -3,11 +3,11 @@
 buildKodiAddon rec {
   pname = "arteplussept";
   namespace = "plugin.video.arteplussept";
-  version = "1.1.8";
+  version = "1.1.9";
 
   src = fetchzip {
     url = "https://mirrors.kodi.tv/addons/nexus/${namespace}/${namespace}-${version}.zip";
-    hash = "sha256-1f+oEHEhSsDMXH7WssSVjjuDjK3UNyMiNhaw7mh/xjI=";
+    hash = "sha256-B4IIYwWrQ5U+aPl9nzAN3HWaQjHX1G+hxpicBgBAwiA=";
   };
 
   propagatedBuildInputs = [
