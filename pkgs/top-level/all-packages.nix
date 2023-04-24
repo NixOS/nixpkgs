5819fa495dde0b929330b9dd9926c8ad9991bf89
06kellyjac <dev@j-k.io>
2023-04-24 15:24:36 +0100

    netassert: init at 2.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10205,6 +10205,8 @@ with pkgs;
 
   ndisc6 = callPackage ../tools/networking/ndisc6 { };
 
+  netassert = callPackage ../tools/networking/netassert { };
+
   netboot = callPackage ../tools/networking/netboot { };
 
   netbootxyz-efi = callPackage ../tools/misc/netbootxyz-efi { };
