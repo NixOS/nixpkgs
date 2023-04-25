0807433107415671756c9547b802ffcec5d48a5b
Robert Scott <code@humanleg.org.uk>
2023-04-25 22:48:34 +0100

    openvswitch: 3.0.3 -> 3.1.1

--- pkgs/os-specific/linux/openvswitch/default.nix
+++ pkgs/os-specific/linux/openvswitch/default.nix
@@ -1,4 +1,4 @@
 import ./generic.nix {
-  version = "3.0.3";
-  hash = "sha256-9QZlCnbx8s7737WD80NBIQLxcKvFXG/4dWd5Jrm9lGM=";
+  version = "3.1.1";
+  hash = "sha256-YEiRg6RNO5WlUiQHIhfF9tN6oRvhKnV2JRDO25Ok4gQ=";
 }
