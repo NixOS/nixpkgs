3e7069bb47e90254811052aff1c295f33ac9f4c7
IndeedNotJames <git@indeednotjames.com>
2023-04-25 16:54:08 +0200

    nixosTests.consul-template: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -146,6 +146,7 @@ in {
   collectd = handleTest ./collectd.nix {};
   connman = handleTest ./connman.nix {};
   consul = handleTest ./consul.nix {};
+  consul-template = handleTest ./consul-template.nix {};
   containers-bridge = handleTest ./containers-bridge.nix {};
   containers-custom-pkgs.nix = handleTest ./containers-custom-pkgs.nix {};
   containers-ephemeral = handleTest ./containers-ephemeral.nix {};
