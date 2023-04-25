4df0c24a5d095b891746af989dbe8c8201d09286
GenericNerdyUsername <genericnerdyusername@proton.me>
2023-04-25 16:44:21 +0100

    elmerfem: switch from OCE to OCC

--- pkgs/applications/science/physics/elmerfem/default.nix
+++ pkgs/applications/science/physics/elmerfem/default.nix
@@ -1,4 +1,20 @@
-{ lib, stdenv, fetchFromGitHub, cmake, git, gfortran, mpi, blas, liblapack, pkg-config, libGL, libGLU, opencascade, libsForQt5, tbb, vtkWithQt5 }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, git
+, gfortran
+, mpi
+, blas
+, liblapack
+, pkg-config
+, libGL
+, libGLU
+, opencascade-occt
+, libsForQt5
+, tbb
+, vtkWithQt5
+}:
 
 stdenv.mkDerivation rec {
   pname = "elmerfem";
@@ -7,8 +23,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "elmercsc";
     repo = pname;
-    rev = "39c8784b6e4543a6bf560b5d597e0eec1eb06343";
-    hash = "sha256-yyxgFvlS+I4PouDL6eD4ZrXuONTDejCSYKq2AwQ0Iug=";
+    rev = "d1d49839b70a6a71336a55f512d3cc03b1d2fc78";
+    hash = "sha256-NhzvU0sZFRygYFs39baFPhDh6aeO/I4BEazrNTB2u80=";
   };
 
   hardeningDisable = [ "format" ];
@@ -28,7 +44,7 @@ stdenv.mkDerivation rec {
     libsForQt5.qwt
     libGL
     libGLU
-    opencascade
+    opencascade-occt
     tbb
     vtkWithQt5
   ];
@@ -40,16 +56,17 @@ stdenv.mkDerivation rec {
   storepath = placeholder "out";
 
   cmakeFlags = [
-  "-DELMER_INSTALL_LIB_DIR=${storepath}/lib"
-  "-DWITH_OpenMP:BOOLEAN=TRUE"
-  "-DWITH_MPI:BOOLEAN=TRUE"
-  "-DWITH_QT5:BOOLEAN=TRUE"
-  "-DWITH_OCC:BOOLEAN=TRUE"
-  "-DWITH_VTK:BOOLEAN=TRUE"
-  "-DWITH_ELMERGUI:BOOLEAN=TRUE"
-  "-DCMAKE_INSTALL_LIBDIR=lib"
-  "-DCMAKE_INSTALL_INCLUDEDIR=include"
-  "-DCMAKE_OpenGL_GL_PREFERENCE=GLVND"
+    "-DELMER_INSTALL_LIB_DIR=${storepath}/lib"
+    "-DWITH_OpenMP:BOOLEAN=TRUE"
+    "-DWITH_MPI:BOOLEAN=TRUE"
+    "-DWITH_QT5:BOOLEAN=TRUE"
+    "-DWITH_OCC:BOOLEAN=TRUE"
+    "-DWITH_VTK:BOOLEAN=TRUE"
+    # Breaks compilation, uncomment at next release
+    #"-DWITH_ELMERGUI:BOOLEAN=TRUE"
+    "-DCMAKE_INSTALL_LIBDIR=lib"
+    "-DCMAKE_INSTALL_INCLUDEDIR=include"
+    "-DCMAKE_OpenGL_GL_PREFERENCE=GLVND"
   ];
 
   meta = with lib; {
