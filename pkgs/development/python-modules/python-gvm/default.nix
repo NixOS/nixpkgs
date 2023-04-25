066988ab3d86dc6ef5f19b680de97476cadadc35
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-25 22:26:50 +0200

    python310Packages.python-gvm: 23.4.0 -> 23.4.1
    Diff: https://github.com/greenbone/python-gvm/compare/refs/tags/v23.4.0...v23.4.1

--- pkgs/development/python-modules/python-gvm/default.nix
+++ pkgs/development/python-modules/python-gvm/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "python-gvm";
-  version = "23.4.0";
+  version = "23.4.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "greenbone";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-qpPoE5QSm6JwBG3842gjxGeSd87yY2/T/HFi4k8U/qY=";
+    hash = "sha256-RuyOhFerWSJ/JEQTruv1gh/ieO2X99E33VW28assflY=";
   };
 
   nativeBuildInputs = [
