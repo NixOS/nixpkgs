de64d9d6bfe1e99adc94b78ec6638f1d4fac6365
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-26 19:22:17 +0000

    python310Packages.schedule: 1.1.0 -> 1.2.0

--- pkgs/development/python-modules/schedule/default.nix
+++ pkgs/development/python-modules/schedule/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "schedule";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e6ca13585e62c810e13a08682e0a6a8ad245372e376ba2b8679294f377dfc8e4";
+    sha256 = "sha256-tK1peq+6cYTJ62oeLrxB94FUckKs3ozq6aCiWwTAki0=";
   };
 
   buildInputs = [ mock ];
