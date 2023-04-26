253aa28f82ccdcb1a944320fc22a39d92f703424
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-26 22:53:38 +0200

    python310Packages.schedule: disable on unsupported Python releases
    - add format

--- pkgs/development/python-modules/schedule/default.nix
+++ pkgs/development/python-modules/schedule/default.nix
@@ -2,18 +2,24 @@
 , buildPythonPackage
 , fetchPypi
 , mock
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "schedule";
   version = "1.2.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-tK1peq+6cYTJ62oeLrxB94FUckKs3ozq6aCiWwTAki0=";
+    hash = "sha256-tK1peq+6cYTJ62oeLrxB94FUckKs3ozq6aCiWwTAki0=";
   };
 
-  buildInputs = [ mock ];
+  buildInputs = [
+    mock
+  ];
 
   preCheck = ''
     # https://github.com/dbader/schedule/issues/488
@@ -27,5 +33,6 @@ buildPythonPackage rec {
     homepage = "https://github.com/dbader/schedule";
     changelog = "https://github.com/dbader/schedule/blob/${version}/HISTORY.rst";
     license = licenses.mit;
+    maintainers = with maintainers; [ ];
   };
 }
