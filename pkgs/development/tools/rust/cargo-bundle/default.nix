5441ad01934dcf987ecace5089c3d1aa456adb24
figsoda <figsoda@pm.me>
2023-04-26 11:06:00 -0400

    cargo-bundle: unstable-2023-03-17 -> 0.6.0
    Diff: https://github.com/burtonageo/cargo-bundle/compare/eb9fe1b0880c7c0e929a93edaddcb0a61cd3f0d4...v0.6.0

--- pkgs/development/tools/rust/cargo-bundle/default.nix
+++ pkgs/development/tools/rust/cargo-bundle/default.nix
@@ -8,19 +8,18 @@
 , wayland
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "cargo-bundle";
-  # the latest stable release fails to build on darwin
-  version = "unstable-2023-03-17";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "burtonageo";
     repo = "cargo-bundle";
-    rev = "eb9fe1b0880c7c0e929a93edaddcb0a61cd3f0d4";
-    hash = "sha256-alO+Q9IK5Hz09+TqHWsbjuokxISKQfQTM6QnLlUNydw=";
+    rev = "v${version}";
+    hash = "sha256-MKr6qLWWrU3thBhKpmH8OxIEKvzb0/cwhyLJlE9B8lA=";
   };
 
-  cargoHash = "sha256-h+QPbwYTJk6dieta/Q+VAhYe8/YH/Nik6gslzUn0YxI=";
+  cargoHash = "sha256-xZewlGRfxndUO43cjiYRw5gQMJgvJ5Iu/RxoQeAOR2k=";
 
   nativeBuildInputs = [
     pkg-config
