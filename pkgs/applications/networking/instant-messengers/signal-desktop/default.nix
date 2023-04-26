8ded035a7830a735928c7176173aa936de357105
gp2112 <me@guip.dev>
2023-04-26 00:46:30 -0300

    signal-desktop: 6.15.0 -> 6.16.0; signal-desktop-beta: 6.16.0-beta.1 -> 6.17.0-beta.1
    (cherry picked from commit d9a5d373b97981efc2428188c3bf3dbc7369b7a1)

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -1,12 +1,12 @@
 { callPackage }: builtins.mapAttrs (pname: attrs: callPackage ./generic.nix (attrs // { inherit pname; })) {
   signal-desktop = {
     dir = "Signal";
-    version = "6.15.0";
-    hash = "sha256-uZXFnbDe49GrjKm4A0lsOTGV8Xqg0+oC0+AwRMKykfY=";
+    version = "6.16.0";
+    hash = "sha256-q7z7TS16RORPbEMJBEmF3m2q4IdD3dM1xqv1DfgM9Zs=";
   };
   signal-desktop-beta = {
     dir = "Signal Beta";
-    version = "6.16.0-beta.1";
-    hash = "sha256-J7YPuQetfob8Ybab+c5W0Z4Urzi4AtEJAnIVRIGtv0Q=";
+    version = "6.17.0-beta.1";
+    hash = "sha256-8Ae+IrwDRxcF5JhrDqEhimQqyCtDYWm/pOrcpKgAo2w=";
   };
 }
