efd36a73241fd3992ff160d1bb44d4fdd72f3c97
Georges <georges.alkhouri@gmail.com>
2023-04-26 10:14:22 +0200

    yai: init at 0.6.0
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39619,6 +39619,8 @@ with pkgs;
 
   yadm = callPackage ../applications/version-management/yadm { };
 
+  yai = callPackage ../tools/misc/yai { };
+
   yamale = with python3Packages; toPythonApplication yamale;
 
   yamdi = callPackage ../tools/video/yamdi { };
