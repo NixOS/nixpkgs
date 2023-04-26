10711f41e511e26c70d460c211d7967712db6377
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-04-26 15:36:41 -0600

    digikam: 7.9.0 → 7.10.0

--- pkgs/applications/graphics/digikam/default.nix
+++ pkgs/applications/graphics/digikam/default.nix
@@ -56,11 +56,11 @@
 
 mkDerivation rec {
   pname   = "digikam";
-  version = "7.9.0";
+  version = "7.10.0";
 
   src = fetchurl {
     url = "mirror://kde/stable/${pname}/${version}/digiKam-${version}.tar.xz";
-    sha256 = "sha256-w7gKvAkNo8u8QuZ6QDCA1/X+CnyYaYc1vaVWxgMUurQ=";
+    sha256 = "sha256-o/MPAbfRttWFgivNXr+N9p4P8CRWOnJGLr+AadvaIuE=";
   };
 
   nativeBuildInputs = [ cmake doxygen extra-cmake-modules kdoctools wrapGAppsHook ];
