3c015772bf5396fb0b4e571d9497299cd3477c25
Nick Cao <nickcao@nichi.co>
2023-04-26 23:35:34 +0800

    matio: build with cmake, fixes cross compilation

--- pkgs/development/libraries/matio/default.nix
+++ pkgs/development/libraries/matio/default.nix
@@ -1,11 +1,23 @@
-{ lib, stdenv, fetchurl }:
+{ lib
+, stdenv
+, fetchurl
+, cmake
+}:
+
 stdenv.mkDerivation rec {
   pname = "matio";
   version = "1.5.23";
+
   src = fetchurl {
     url = "mirror://sourceforge/matio/${pname}-${version}.tar.gz";
     sha256 = "sha256-n5Hq5mHfRupTwxGhstz/cgUQlbAjxhLXy/wJQGyfTW4=";
   };
+
+  nativeBuildInputs = [ cmake ];
+
+  cmakeFlags = [
+    "-DMATIO_MAT73=OFF" # requires hdf5 with does not cross compile yet
+  ];
 
   meta = with lib; {
     description = "A C library for reading and writing Matlab MAT files";
