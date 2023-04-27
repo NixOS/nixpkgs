0c97d307e68209414977ae270db9d9afafde21b6
Minijackson <minijackson@riseup.net>
2023-04-27 22:10:35 +0200

    netbox: 3.4.10 -> 3.5.0

--- pkgs/servers/web-apps/netbox/default.nix
+++ pkgs/servers/web-apps/netbox/default.nix
@@ -23,8 +23,8 @@ in
   };
 
   netbox = callPackage generic {
-    version = "3.4.10";
-    hash = "sha256-STNukh/UYAiZ5amLfpmzIcv8OlRqW0zU+66ShfDiGkc=";
+    version = "3.5.0";
+    hash = "sha256-LsUitX/e+ec/9mRBw+cbGOG2Idl9ZQwf/vxIC3YS5LU=";
     extraPatches = [
       # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
       ./config.patch
