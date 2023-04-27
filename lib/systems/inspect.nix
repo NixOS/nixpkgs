edcad332d938a97d09d3b3664ed4618eaf1d4938
jackyliu16 <18922251299@163.com>
2023-04-27 20:04:17 +0300

    lib.platforms.loongarch64: init

--- lib/systems/inspect.nix
+++ lib/systems/inspect.nix
@@ -49,6 +49,7 @@ rec {
     isM68k         = { cpu = { family = "m68k"; }; };
     isS390         = { cpu = { family = "s390"; }; };
     isS390x        = { cpu = { family = "s390"; bits = 64; }; };
+    isLoongArch64  = { cpu = { family = "loongarch"; bits = 64; }; };
     isJavaScript   = { cpu = cpuTypes.javascript; };
 
     is32bit        = { cpu = { bits = 32; }; };
