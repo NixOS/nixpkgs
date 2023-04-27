f3db2f8c81e08737eef11aa1636f3e8349b5c934
Thomas Gerbet <thomas@gerbet.me>
2023-04-27 11:16:23 +0200

    python3Packages.nbdime: 3.1.1 -> 3.2.0, unmark broken
    https://github.com/jupyter/nbdime/compare/3.1.1...3.2.0

--- pkgs/development/python-modules/nbdime/default.nix
+++ pkgs/development/python-modules/nbdime/default.nix
@@ -28,14 +28,14 @@
 
 buildPythonPackage rec {
   pname = "nbdime";
-  version = "3.1.1";
+  version = "3.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Z3ZzIOlxN09wGhdapZq9OlVHIwOdOfrpCOctFjMNZIs=";
+    hash = "sha256-5Q0aDPZy4CNW4Q5tPQrqjoYSpTSubhub/VmsO2DEBes=";
   };
 
   nativeBuildInputs = [
@@ -92,7 +92,5 @@ buildPythonPackage rec {
     description = "Tools for diffing and merging of Jupyter notebooks.";
     license = licenses.bsd3;
     maintainers = with maintainers; [ tbenst ];
-    # https://github.com/jupyter/nbdime/issues/645
-    broken = lib.versionAtLeast jupyter-server.version "2";
   };
 }
