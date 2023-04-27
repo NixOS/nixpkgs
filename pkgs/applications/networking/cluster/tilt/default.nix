2b8f96c360abde583fe8da37c9b82f8ffbd9ded0
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-27 05:44:21 +0000

    tilt: 0.31.2 -> 0.32.2

--- pkgs/applications/networking/cluster/tilt/default.nix
+++ pkgs/applications/networking/cluster/tilt/default.nix
@@ -5,15 +5,15 @@ buildGoModule rec {
   /* Do not use "dev" as a version. If you do, Tilt will consider itself
     running in development environment and try to serve assets from the
     source tree, which is not there once build completes.  */
-  version = "0.31.2";
+  version = "0.32.2";
 
   src = fetchFromGitHub {
     owner  = "tilt-dev";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "sha256-Wn7e2g1KPnFgFuRPUh3g0FW/m0qRHV5reO+AZbhbaC8=";
+    sha256 = "sha256-YB/stG+7gxIaB+vMx8PwmUm4W32fCNeRnVrOvOXba5k=";
   };
-  vendorSha256 = null;
+  vendorHash = null;
 
   subPackages = [ "cmd/tilt" ];
 
