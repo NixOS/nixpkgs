e5813d69a2f62000d45e35beef2aa25d69162f58
misuzu <bakalolka@gmail.com>
2023-04-27 19:09:21 +0300

    python310Packages.ltpycld2: init at 0.42

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5941,6 +5941,8 @@ self: super: with self; {
 
   lsprotocol = callPackage ../development/python-modules/lsprotocol { };
 
+  ltpycld2 = callPackage ../development/python-modules/ltpycld2 { };
+
   luddite = callPackage ../development/python-modules/luddite { };
 
   ludios_wpull = callPackage ../development/python-modules/ludios_wpull { };
