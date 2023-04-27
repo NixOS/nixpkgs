d0f2ea3f537f51d832ba83b73f5c6667cef0ac65
roblabla <unfiltered@roblab.la>
2023-04-27 10:56:34 +0200

    python310Packages.pillow: 9.4.0 -> 9.5.0

--- pkgs/development/python-modules/pillow/default.nix
+++ pkgs/development/python-modules/pillow/default.nix
@@ -4,6 +4,7 @@
 , pythonOlder
 , fetchPypi
 , isPyPy
+, fetchpatch
 , defusedxml, olefile, freetype, libjpeg, zlib, libtiff, libwebp, libxcrypt, tcl, lcms2, tk, libX11
 , libxcb, openjpeg, libimagequant, pyroma, numpy, pytestCheckHook
 # for passthru.tests
@@ -12,7 +13,7 @@
 
 import ./generic.nix (rec {
   pname = "pillow";
-  version = "9.4.0";
+  version = "9.5.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,8 +21,32 @@ import ./generic.nix (rec {
   src = fetchPypi {
     pname = "Pillow";
     inherit version;
-    hash = "sha256-ocLXeARI65P7zDeJvzkWqlcg2ULjeUX0BWaAMX8c0j4=";
+    hash = "sha256-v1SEedM2cm16Ds6252fhefveN4M65CeUYCYxoHDWMPE=";
   };
+
+  # Reverts 04cf5e2cfc5dc1676efd9f5c8d605ddfccb0f9ee, which prevents pillow from
+  # finding zlib. This has been fixed upstream, so this patch can be removed on
+  # next pillow update.
+  patches = [
+    (fetchpatch {
+      name = "revert-multiple-paths-in-pkgconfig1.patch";
+      url = "https://github.com/python-pillow/pillow/commit/17a0a2ee3eeb9df6e9fcf894d204911c7e6e4eef.patch";
+      sha256 = "sha256-wAJfCYhBmXaVcVMwNBTk7kCpuJucAmetJTWtL155Ybc=";
+      revert = true;
+    })
+    (fetchpatch {
+      name = "revert-multiple-paths-in-pkgconfig2.patch";
+      url = "https://github.com/python-pillow/pillow/commit/a0492f796876c2a9b8ba445d72c771b84eff93a5.patch";
+      sha256 = "sha256-vCBRczrl9v5QWrYkt85Nb06+ZXKt9GxTxg3djn4/m2o=";
+      revert = true;
+    })
+    (fetchpatch {
+      name = "revert-multiple-paths-in-pkgconfig3.patch";
+      url = "https://github.com/python-pillow/pillow/commit/04cf5e2cfc5dc1676efd9f5c8d605ddfccb0f9ee.patch";
+      sha256 = "sha256-7uvNEUk6fBCBkwcqg6njhsAJGla11diz8KY966zsxew";
+      revert = true;
+    })
+  ];
 
   passthru.tests = {
     inherit imageio matplotlib pilkit pydicom reportlab;
