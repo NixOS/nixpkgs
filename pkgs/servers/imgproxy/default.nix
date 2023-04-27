fd2c8bfaa356ca0b7001cd49d7a1578eaeaf5dce
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-27 12:57:02 +0000

    imgproxy: 3.15.0 -> 3.16.0

--- pkgs/servers/imgproxy/default.nix
+++ pkgs/servers/imgproxy/default.nix
@@ -3,16 +3,16 @@
 
 buildGoModule rec {
   pname = "imgproxy";
-  version = "3.15.0";
+  version = "3.16.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
-    sha256 = "sha256-Y0QxjIY8ac/6MAdqKDEv3/et1+ysCMkV522jxDP72Js=";
+    sha256 = "sha256-TdJRoburmJl2Ez0wzJnqm2jpJpFOSOmaBx0O9Z/8DXQ=";
     rev = "v${version}";
   };
 
-  vendorHash = "sha256-6rEGuuw3UJyWeaIm9v5P8/V0Nxd9ySe0PCf0rWRDB9s=";
+  vendorHash = "sha256-EtNFSAx8YcRhzgV3IdrZaCM6fOd284iuTperCFECsL8=";
 
   nativeBuildInputs = [ pkg-config ];
 
