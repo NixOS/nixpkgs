c4604c0bc00d251f760f259a9f3b7fb61ff49953
Fabian Freihube <fabian@freihube.dev>
2023-04-27 18:35:36 +0200

    labelImg: 1.8.3 -> 1.8.6

--- pkgs/applications/science/machine-learning/labelimg/default.nix
+++ pkgs/applications/science/machine-learning/labelimg/default.nix
@@ -1,21 +1,33 @@
-{ lib, python3Packages, fetchFromGitHub, qt5 }:
+{ lib
+, python3Packages
+, fetchFromGitHub
+, fetchpatch
+, qt5
+}:
   python3Packages.buildPythonApplication rec {
     pname = "labelImg";
-    version = "1.8.3";
+    version = "1.8.6";
     src = fetchFromGitHub {
       owner = "tzutalin";
       repo = "labelImg";
       rev = "v${version}";
-      sha256 = "07v106fzlmxrbag4xm06m4mx9m0gckb27vpwsn7sap1bbgc1pap5";
+      sha256 = "sha256-RJxCtiDOePajlrjy9cpKETSKsWlH/Dlu1iFMj2aO4XU=";
     };
     nativeBuildInputs = with python3Packages; [
       pyqt5
       qt5.wrapQtAppsHook
     ];
+    patches = [
+      # fixes https://github.com/heartexlabs/labelImg/issues/838
+      # can be removed after next upstream version bump
+      (fetchpatch {
+        url = "https://github.com/heartexlabs/labelImg/commit/5c38b6bcddce895d646e944e3cddcb5b43bf8b8b.patch";
+        hash = "sha256-BmbnJS95RBfoNQT0E6JDJ/IZfBa+tv1C69+RVOSFdRA=";
+      })
+    ];
     propagatedBuildInputs = with python3Packages; [
       pyqt5
       lxml
-      sip_4
     ];
     preBuild = ''
       make qt5py3
