c4a25e5e680107d2eccc60b00d6ef3073f985b0e
InfraBot <infra@marigold.dev>
2023-04-27 13:55:32 +0000

    ligo: 0.60.0 -> 0.64.1

--- pkgs/development/compilers/ligo/default.nix
+++ pkgs/development/compilers/ligo/default.nix
@@ -10,12 +10,12 @@
 
 ocamlPackages.buildDunePackage rec {
   pname = "ligo";
-  version = "0.60.0";
+  version = "0.64.1";
   src = fetchFromGitLab {
     owner = "ligolang";
     repo = "ligo";
     rev = version;
-    sha256 = "sha256-gyMSpy+F3pF2Kv1ygUs20mrspJ6GtJ6ySyZD7zfZj2w=";
+    sha256 = "sha256-DrGDG8TOrBB7uDHeEK/2BKfhEZqiP2KU0/3NIld3zNQ=";
     fetchSubmodules = true;
   };
 
