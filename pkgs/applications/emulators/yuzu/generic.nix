f08af5a3d41eeaa2eff918020d543ecb6b7ffcb1
K900 <me@0upti.me>
2023-04-28 23:41:09 +0300

    yuzu: reanimate, clean up, switch to qt6

--- pkgs/applications/emulators/yuzu/generic.nix
+++ pkgs/applications/emulators/yuzu/generic.nix
@@ -1,22 +1,25 @@
-{ pname
-, version
+{ version
 , src
 , branch
 , compat-list
 
 , lib
 , stdenv
-, runCommandLocal
-, substituteAll
 , wrapQtAppsHook
 , alsa-lib
 , boost
-, catch2
+, catch2_3
 , cmake
+, cpp-jwt
+, cubeb
+, discord-rpc
 , doxygen
+, enet
 , ffmpeg
-, fmt_8
+, fmt
 , glslang
+, httplib
+, inih
 , libjack2
 , libopus
 , libpulseaudio
@@ -29,7 +32,9 @@
 , pkg-config
 , python3
 , qtbase
+, qtmultimedia
 , qttools
+, qtwayland
 , qtwebengine
 , rapidjson
 , SDL2
@@ -42,8 +47,10 @@
 , zstd
 }:
 
-stdenv.mkDerivation rec {
-  inherit pname version src;
+stdenv.mkDerivation {
+  pname = "yuzu-${branch}";
+
+  inherit version src;
 
   nativeBuildInputs = [
     cmake
@@ -57,49 +64,64 @@ stdenv.mkDerivation rec {
   buildInputs = [
     alsa-lib
     boost
-    catch2
+    catch2_3
+    cpp-jwt
+    cubeb
+    discord-rpc
+    # intentionally omitted: dynarmic - prefer vendored version for compatibility
+    enet
     ffmpeg
-    fmt_8
+    fmt
     glslang
+    httplib
+    inih
     libjack2
     libopus
     libpulseaudio
     libusb1
     libva
     libzip
+    # intentionally omitted: LLVM - heavy, only used for stack traces in the debugger
     lz4
     nlohmann_json
     qtbase
+    qtmultimedia
     qttools
+    qtwayland
     qtwebengine
     rapidjson
     SDL2
     sndio
     speexdsp
     udev
+    vulkan-headers
+    # intentionally omitted: xbyak - prefer vendored version for compatibility
     zlib
     zstd
   ];
-
-  doCheck = true;
 
   # This changes `ir/opt` to `ir/var/empty` in `externals/dynarmic/src/dynarmic/CMakeLists.txt`
   # making the build fail, as that path does not exist
   dontFixCmake = true;
 
-  # -Werror causes build failures for deprecation warnings introduced by transitive dependency updates
-  postPatch = ''
-    sed -i '/-Werror/d' src/common/CMakeLists.txt
-  '';
-
   cmakeFlags = [
-    "-DYUZU_USE_BUNDLED_QT=OFF"
-    "-DYUZU_USE_BUNDLED_FFMPEG=OFF"
-    "-DYUZU_USE_BUNDLED_OPUS=OFF"
-    "-DYUZU_USE_EXTERNAL_SDL2=OFF"
+    # actually has a noticeable performance impact
+    "-DYUZU_ENABLE_LTO=ON"
 
+    # build with qt6
+    "-DENABLE_QT6=ON"
     "-DENABLE_QT_TRANSLATION=ON"
+
+    # use system libraries
+    "-DYUZU_USE_EXTERNAL_SDL2=OFF"
+    "-DYUZU_USE_EXTERNAL_VULKAN_HEADERS=OFF"
+
+    # don't check for missing submodules
+    "-DYUZU_CHECK_SUBMODULES=OFF"
+
+    # enable some optional features
     "-DYUZU_USE_QT_WEB_ENGINE=ON"
+    "-DYUZU_USE_QT_MULTIMEDIA=ON"
     "-DUSE_DISCORD_PRESENCE=ON"
 
     # We dont want to bother upstream with potentially outdated compat reports
@@ -107,21 +129,17 @@ stdenv.mkDerivation rec {
     "-DENABLE_COMPATIBILITY_LIST_DOWNLOAD=OFF" # We provide this deterministically
   ];
 
+  # Fixes vulkan detection.
+  # FIXME: patchelf --add-rpath corrupts the binary for some reason, investigate
   qtWrapperArgs = [
-    # Fixes vulkan detection
     "--prefix LD_LIBRARY_PATH : ${vulkan-loader}/lib"
-    # Without yuzu doesnt start on wayland. See https://github.com/yuzu-emu/yuzu/issues/6088
-    "--set QT_QPA_PLATFORM xcb"
   ];
 
   preConfigure = ''
-    # This prevents a check for submodule directories.
-    rm -f .gitmodules
-
     # see https://github.com/NixOS/nixpkgs/issues/114044, setting this through cmakeFlags does not work.
     cmakeFlagsArray+=(
-      "-DTITLE_BAR_FORMAT_IDLE=yuzu ${branch} ${version}"
-      "-DTITLE_BAR_FORMAT_RUNNING=yuzu ${branch} ${version} | {3}"
+      "-DTITLE_BAR_FORMAT_IDLE=yuzu | ${branch} ${version} (nixpkgs) {}"
+      "-DTITLE_BAR_FORMAT_RUNNING=yuzu | ${branch} ${version} (nixpkgs) | {}"
     )
   '';
 
@@ -130,12 +148,7 @@ stdenv.mkDerivation rec {
     ln -sf ${compat-list} ./dist/compatibility_list/compatibility_list.json
   '';
 
-  passthru.updateScript = runCommandLocal "yuzu-${branch}-updateScript" {
-    script = substituteAll {
-      src = ./update.sh;
-      inherit branch;
-    };
-  } "install -Dm755 $script $out";
+  passthru.updateScript = ./update.sh;
 
   meta = with lib; {
     homepage = "https://yuzu-emu.org";
@@ -143,8 +156,8 @@ stdenv.mkDerivation rec {
     description = "The ${branch} branch of an experimental Nintendo Switch emulator written in C++";
     longDescription = ''
       An experimental Nintendo Switch emulator written in C++.
-      Using the mainline branch is recommanded for general usage.
-      Using the early-access branch is recommanded if you would like to try out experimental features, with a cost of stability.
+      Using the mainline branch is recommended for general usage.
+      Using the early-access branch is recommended if you would like to try out experimental features, with a cost of stability.
     '';
     mainProgram = "yuzu";
     platforms = [ "x86_64-linux" ];
@@ -158,6 +171,7 @@ stdenv.mkDerivation rec {
       ivar
       joshuafern
       sbruder
+      k900
     ];
   };
 }
