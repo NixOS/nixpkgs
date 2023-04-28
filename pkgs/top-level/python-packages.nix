de2da79e222c59635c1c2e48e5ead325d9ee3acb
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-28 22:24:59 +0200

    python310Packages.sonos-websocket: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11020,6 +11020,8 @@ self: super: with self; {
 
   sonarr = callPackage ../development/python-modules/sonarr { };
 
+  sonos-websocket = callPackage ../development/python-modules/sonos-websocket { };
+
   sopel = callPackage ../development/python-modules/sopel { };
 
   sorl_thumbnail = callPackage ../development/python-modules/sorl_thumbnail { };
