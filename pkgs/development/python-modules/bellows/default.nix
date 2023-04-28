452166c9c06bcb9bd5451157a16c328d9ca688c9
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-28 10:29:06 +0200

    python310Packages.bellows: 0.35.1 -> 0.35.2
    Diff: https://github.com/zigpy/bellows/compare/refs/tags/0.35.1...0.35.2
    
    Changelog: https://github.com/zigpy/bellows/releases/tag/0.35.2

--- pkgs/development/python-modules/bellows/default.nix
+++ pkgs/development/python-modules/bellows/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "bellows";
-  version = "0.35.1";
+  version = "0.35.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "bellows";
     rev = "refs/tags/${version}";
-    hash = "sha256-o2806cXjtt+yMeSdpEq4KOlIlDsvf7qCUO2TBzkt5uY=";
+    hash = "sha256-I9PXFdRXHQ7OnzCdB631nlfnkpConl+j5Z3iiH7RcA4=";
   };
 
   propagatedBuildInputs = [
