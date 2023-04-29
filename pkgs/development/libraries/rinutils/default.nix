0e0cae101f92fee87406f0da6cb595a3d0558487
Mario Rodas <marsam@users.noreply.github.com>
2023-04-29 23:18:00 +0000

    rinutils: update meta

--- pkgs/development/libraries/rinutils/default.nix
+++ pkgs/development/libraries/rinutils/default.nix
@@ -1,16 +1,13 @@
-{
-  stdenv, lib, fetchurl,
-  cmake, perl,
+{ stdenv
+, lib
+, fetchurl
+, cmake
+, perl
 }:
 
 stdenv.mkDerivation rec {
   pname = "rinutils";
   version = "0.10.2";
-
-  meta = with lib; {
-    homepage = "https://github.com/shlomif/rinutils";
-    license = licenses.mit;
-  };
 
   src = fetchurl {
     url = "https://github.com/shlomif/${pname}/releases/download/${version}/${pname}-${version}.tar.xz";
@@ -25,4 +22,13 @@ stdenv.mkDerivation rec {
     substituteInPlace librinutils.pc.in \
       --replace '$'{exec_prefix}/@RINUTILS_INSTALL_MYLIBDIR@ @CMAKE_INSTALL_FULL_LIBDIR@
   '';
+
+  meta = with lib; {
+    description = "C11 / gnu11 utilities C library by Shlomi Fish / Rindolf";
+    homepage = "https://github.com/shlomif/rinutils";
+    changelog = "https://github.com/shlomif/rinutils/raw/${version}/NEWS.asciidoc";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ];
+    platforms = platforms.all;
+  };
 }
