1d5111d07ad3cea1fab94e2b88d694a801d5b5a3
Camille M. <camillemondon@free.fr>
2023-04-29 18:32:48 +0200

    overleaf: support gitbridge and build deps in other derivation

--- pkgs/servers/overleaf/default.nix
+++ pkgs/servers/overleaf/default.nix
@@ -8,76 +8,96 @@
 , fixup_yarn_lock
 , nodejs-16_x
 , yarn
+, callPackage
 }:
 
-stdenv.mkDerivation rec {
+let
   pname = "overleaf";
-  version = "3.5.0";
+  version = "3.5";
 
-  src = fetchFromGitHub {
-    owner = "overleaf";
-    repo = "overleaf";
-    rev = "d539aaf2262149361590514fe27fd5c8e0fab111";
-    hash = "sha256-HuK9s7040sH9lBHb6yGO+fll2hrpdTIGIYIUujVkhqw=";
+  deps = stdenv.mkDerivation {
+    name = "${pname}-${version}-deps";
+
+    src = fetchFromGitHub {
+      owner = "overleaf";
+      repo = "overleaf";
+      rev = "d539aaf2262149361590514fe27fd5c8e0fab111";
+      hash = "sha256-HuK9s7040sH9lBHb6yGO+fll2hrpdTIGIYIUujVkhqw=";
+    };
+
+    patches = [ ./versions.patch ];
+
+    postPatch = ''
+      # Add the custom yarn.lock
+      rm package-lock.json
+      cp ${./yarn.lock} yarn.lock
+      chmod +rw yarn.lock
+      cp ${./package.json} package.json
+
+      # Replace hard-coded values in settings by environment variables
+      find . -type f -not -path '*/\.*' -exec sed -E -i "s|SHARELATEX_|OVERLEAF_|g" {} +
+      (
+        cd server-ce/config
+        sed -E -i "s!'dockerhost',!undefined,\n      path: process.env.OVERLEAF_REDIS_PATH || undefined,!" ./settings.js
+        sed -E -i "s!'6379'!undefined!" ./settings.js
+        sed -E -i "s|httpAuthUser = 'sharelatex'|httpAuthUser = process.env.WEB_API_USER|" ./settings.js
+        sed -E -i "s|'/var/lib/sharelatex(.*)'|\`\''${process.env.DATA_DIR}\1\`|" ./settings.js
+        sed -E -i "s!'http://localhost:3000'!\`http://\''${process.env.WEB_API_HOST || process.env.WEB_HOST || 'localhost'}:\''${process.env.WEB_API_PORT || process.env.WEB_PORT || 3000}\`!" ./settings.js
+      )
+    '';
+
+    buildInputs = [ pkgs.jq python3 fixup_yarn_lock nodejs-16_x nodejs-16_x.pkgs.node-pre-gyp nodejs-16_x.pkgs.node-gyp yarn ];
+
+    offlineCache = fetchYarnDeps {
+      yarnLock = ./yarn.lock;
+      sha256 = "sha256-ci68170df9o3fJuw8AH/qdJR8NZ2o9OeAg8gfjycHd0=";
+    };
+
+    buildPhase = ''
+      runHook preBuild
+      export HOME=$PWD
+      export NODE_OPTIONS=--openssl-legacy-provider
+      fixup_yarn_lock yarn.lock
+      yarn config --offline set yarn-offline-mirror $offlineCache
+      yarn --offline --frozen-lockfile --ignore-scripts --ignore-engines
+      (
+        cd node_modules/bcrypt
+        export CPPFLAGS="-I${nodejs-16_x}/include/node"
+        node-pre-gyp install --prefer-offline --build-from-source --nodedir=${nodejs-16_x}/include/node
+      )
+      (
+        cd node_modules/diskusage
+        export CPPFLAGS="-I${nodejs-16_x}/include/node"
+        node-gyp configure --nodedir=${nodejs-16_x}/include/node
+        node-gyp build --nodedir=${nodejs-16_x}/include/node
+      )
+      patchShebangs node_modules
+    '';
+
+    installPhase = ''
+      mkdir $out
+      cp -r . $out
+    '';
   };
+in
 
-  patches = [ ./versions.patch ];
-
-  postPatch = ''
-    # Add the custom yarn.lock
-    rm package-lock.json
-    cp ${./yarn.lock} yarn.lock
-    chmod +rw yarn.lock
-    cp ${./package.json} package.json
-
-    # Replace hard-coded values in settings by environment variables
-    find . -type f -not -path '*/\.*' -exec sed -E -i "s|SHARELATEX_|OVERLEAF_|g" {} +
-    (
-      cd server-ce/config
-      sed -E -i "s!'dockerhost',!undefined,\n      path: process.env.OVERLEAF_REDIS_PATH || undefined,!" ./settings.js
-      sed -E -i "s!'6379'!undefined!" ./settings.js
-      sed -E -i "s|httpAuthUser = 'sharelatex'|httpAuthUser = process.env.WEB_API_USER|" ./settings.js
-      sed -E -i "s|'/var/lib/sharelatex(.*)'|\`\''${process.env.DATA_DIR}\1\`|" ./settings.js
-      sed -E -i "s!'http://localhost:3000'!\`http://\''${process.env.WEB_API_HOST || process.env.WEB_HOST || 'localhost'}:\''${process.env.WEB_API_PORT || process.env.WEB_PORT || 3000}\`!" ./settings.js
-    )
-  '';
-
-  buildInputs = [ pkgs.jq python3 fixup_yarn_lock nodejs-16_x nodejs-16_x.pkgs.node-pre-gyp nodejs-16_x.pkgs.node-gyp yarn makeWrapper ];
-
-  offlineCache = fetchYarnDeps {
-    yarnLock = ./yarn.lock;
-    sha256 = "sha256-ci68170df9o3fJuw8AH/qdJR8NZ2o9OeAg8gfjycHd0=";
-  };
+stdenv.mkDerivation {
+  pname = "overleaf";
+  version = "3.5";
+  src = deps;
+  buildInputs = [ nodejs-16_x makeWrapper ];
 
   buildPhase = ''
-    runHook preBuild
-    export HOME=$PWD
-    export NODE_OPTIONS=--openssl-legacy-provider
-    fixup_yarn_lock yarn.lock
-    yarn config --offline set yarn-offline-mirror $offlineCache
-    yarn --offline --frozen-lockfile --ignore-scripts --ignore-engines
-    (
-      cd node_modules/bcrypt
-      export CPPFLAGS="-I${nodejs-16_x}/include/node"
-      node-pre-gyp install --prefer-offline --build-from-source --nodedir=${nodejs-16_x}/include/node
-    )
-    (
-      cd node_modules/diskusage
-      export CPPFLAGS="-I${nodejs-16_x}/include/node"
-      node-gyp configure --nodedir=${nodejs-16_x}/include/node
-      node-gyp build --nodedir=${nodejs-16_x}/include/node
-    )
-    patchShebangs node_modules
     (
       cd services/web
       npm run webpack:production
     )
-    runHook postBuild
   '';
 
   installPhase = ''
-    mkdir -p $out/share
+    mkdir -p $out/{share,bin}
     cp -r . $out/share
+    ln -s ${callPackage ./gitbridge.nix {}}/bin/overleaf-gitbridge $out/bin/overleaf-gitbridge
   '';
 
   postFixup = ''
