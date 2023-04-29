38fb742663922a8e55255bbef6c6bf1fa067738b
D Anzorge <d.anzorge@gmail.com>
2023-04-29 15:48:10 +0200

    rustPlatform.importCargoLock: fix [package] section handling
    Members of the [package] table in Cargo.toml can be either subtables, or
    values like strings and bools. Python is happy to check for membership
    of "workspace" in a string, since Python strings are iterables, but if
    the value is a bool, Python will throw an exception.

--- pkgs/build-support/rust/test/import-cargo-lock/default.nix
+++ pkgs/build-support/rust/test/import-cargo-lock/default.nix
@@ -14,7 +14,7 @@
   v1 = callPackage ./v1 { };
   gitDependencyWorkspaceInheritance = callPackage ./git-dependency-workspace-inheritance {
     replaceWorkspaceValues = writers.writePython3 "replace-workspace-values"
-      { libraries = with python3Packages; [ tomli tomli-w ]; flakeIgnore = [ "E501" ]; }
+      { libraries = with python3Packages; [ tomli tomli-w ]; flakeIgnore = [ "E501" "W503" ]; }
       (builtins.readFile ../../replace-workspace-values.py);
   };
 }
