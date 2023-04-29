71ef2855a806af91996ff53794c946af1fde70a5
R. Ryantm <ryantm-bot@ryantm.com>
2023-04-29 08:10:59 +0000

    mongodb-compass: 1.36.3 -> 1.36.4

--- pkgs/tools/misc/mongodb-compass/default.nix
+++ pkgs/tools/misc/mongodb-compass/default.nix
@@ -33,7 +33,7 @@ xorg,
 }:
 
 let
-  version = "1.36.3";
+  version = "1.36.4";
 
   rpath = lib.makeLibraryPath [
     alsa-lib
@@ -82,7 +82,7 @@ let
     if stdenv.hostPlatform.system == "x86_64-linux" then
       fetchurl {
         url = "https://downloads.mongodb.com/compass/mongodb-compass_${version}_amd64.deb";
-        sha256 = "sha256-T4OBt47W0FTFmOQdp8T8CVlhrgmGP7pYYY1ijgfhzVA=";
+        sha256 = "sha256-GAg5zj2ETXdXVfVwNvf4VjRVEHePNWf28xMDsTyjiEs=";
       }
     else
       throw "MongoDB compass is not supported on ${stdenv.hostPlatform.system}";
