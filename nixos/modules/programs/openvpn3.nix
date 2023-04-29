22a1bea971b8cc3b4d74821af8199b0dc165b41d
name_snrl <Demogorgon-74@ya.ru>
2023-04-30 04:14:46 +0500

    nixos/openvpn3: generate default configuration

--- nixos/modules/programs/openvpn3.nix
+++ nixos/modules/programs/openvpn3.nix
@@ -7,7 +7,14 @@ let
 in
 {
   options.programs.openvpn3 = {
-    enable = mkEnableOption (lib.mdDoc "the openvpn3 client");
+    enable = mkEnableOption (mdDoc "the openvpn3 client");
+
+    package = mkOption {
+      default = pkgs.openvpn3;
+      defaultText = literalExpression "pkgs.openvpn3";
+      type = types.package;
+      description = mdDoc "The openvpn3 package.";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -23,6 +30,31 @@ in
 
     users.groups.openvpn = {
       gid = config.ids.gids.openvpn;
+    };
+
+    systemd.tmpfiles.rules = [
+      "d /var/lib/openvpn3 0750 openvpn openvpn - -"
+    ];
+
+    systemd.services.openvpn3-netcfg-setup = {
+      description = "Generate openvpn3 configuration";
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig.Type = "oneshot";
+      script = ''
+        ${cfg.package}/bin/openvpn3-admin init-config --write-configs
+        ${if config.services.resolved.enable
+        then ''
+          ${cfg.package}/bin/openvpn3-admin netcfg-service --config-unset resolv-conf
+          ${cfg.package}/bin/openvpn3-admin netcfg-service --config-set systemd-resolved yes
+        ''
+        else ''
+          ${cfg.package}/bin/openvpn3-admin netcfg-service --config-unset systemd-resolved
+          ${cfg.package}/bin/openvpn3-admin netcfg-service --config-set resolv-conf /etc/resolv.conf
+        ''}
+        service="$(busctl status net.openvpn.v3.netcfg | grep Unit=dbus | cut -d= -f2)"
+        systemctl restart "$service"
+      '';
     };
 
     environment.systemPackages = with pkgs; [
