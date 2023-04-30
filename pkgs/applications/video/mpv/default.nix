9248e0074c54c22ea4a6765bba379a67e4df6c26
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-04-30 02:43:20 +0200

    mpv: make vaapi support depending on x11/wayland
    it requires either x11 or wayland to be enabled and fails configure if
    not so

--- pkgs/applications/video/mpv/default.nix
+++ pkgs/applications/video/mpv/default.nix
@@ -69,7 +69,7 @@
 , speexSupport       ? true,           speex
 , swiftSupport       ? stdenv.isDarwin && stdenv.isAarch64, swift
 , theoraSupport      ? true,           libtheora
-, vaapiSupport       ? stdenv.isLinux, libva
+, vaapiSupport       ? x11Support || waylandSupport, libva
 , vapoursynthSupport ? false,          vapoursynth
 , vdpauSupport       ? true,           libvdpau
 , xineramaSupport    ? stdenv.isLinux, libXinerama
