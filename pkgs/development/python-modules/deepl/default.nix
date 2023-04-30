b1eddc8769dc33e19e09fdf8ce5b3c6b3c1e7437
Fabian Affolter <mail@fabian-affolter.ch>
2023-04-30 21:26:39 +0200

    python310Packages.deepl: 1.13.0 -> 1.14.0

--- pkgs/development/python-modules/deepl/default.nix
+++ pkgs/development/python-modules/deepl/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "deepl";
-  version = "1.13.0";
+  version = "1.14.0";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Rq7L/rgfJJ3ZspuL1IfZv+x60t8cZRPkrVryJf12WLk=";
+    hash = "sha256-jUHxyx+b1OICJHAs8lh5NVtl+MExyEYM/yfs2qz6fv4=";
   };
 
   nativeBuildInputs = [
