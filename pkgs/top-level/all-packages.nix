f5332168e9a7a4799e515946684025482fe7807a
Rampoina <rampoina@protonmail.com>
2023-04-30 17:41:20 +0200

    marble-marcher-ce: init at 1.4.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36301,6 +36301,8 @@ with pkgs;
 
   manaplus = callPackage ../games/manaplus { stdenv = gcc11Stdenv; };
 
+  marble-marcher-ce = callPackage ../games/marble-marcher-ce { };
+
   mars = callPackage ../games/mars { };
 
   megaglest = callPackage ../games/megaglest { };
