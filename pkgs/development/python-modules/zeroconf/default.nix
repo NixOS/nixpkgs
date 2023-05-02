a9c0286bb1e6bed3dcfc43cfb6c616deab128a23
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-02 23:39:27 +0200

    python310Packages.zeroconf: 0.58.2 -> 0.60.0
    Diff: https://github.com/jstasiak/python-zeroconf/compare/refs/tags/0.58.2...0.60.0
    
    Changelog: https://github.com/python-zeroconf/python-zeroconf/releases/tag/0.60.0

--- pkgs/development/python-modules/zeroconf/default.nix
+++ pkgs/development/python-modules/zeroconf/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "zeroconf";
-  version = "0.58.2";
+  version = "0.60.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "jstasiak";
     repo = "python-zeroconf";
     rev = "refs/tags/${version}";
-    hash = "sha256-phwGnAosPuH9zj3lS8o78bQohGAllICpbn1cNgRmh0Y=";
+    hash = "sha256-8DAklQhvZ5/NSWDVZQEaRHz+wb05tMPnVT8TvTho+Sc=";
   };
 
   nativeBuildInputs = [
