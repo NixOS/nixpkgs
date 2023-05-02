d5906eec93f5ea1cef12aec7c5eae97eb1fefc34
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-05-02 14:47:58 -0400

    buildbot: partially fix updateScript
    Update the attribute names in the update script that were changed in #225195.
    I can't figure out how to automatically update buildbot-pkg, because it is no
    longer exposed externally.

--- pkgs/development/tools/continuous-integration/buildbot/update.sh
+++ pkgs/development/tools/continuous-integration/buildbot/update.sh
@@ -2,11 +2,13 @@
 #!nix-shell -i bash -p nix-update
 set -eu -o pipefail
 
-nix-update python3Packages.buildbot
-nix-update --version=skip python3Packages.buildbot-worker
-nix-update --version=skip python3Packages.buildbot-pkg
-nix-update --version=skip python3Packages.buildbot-plugins.www
-nix-update --version=skip python3Packages.buildbot-plugins.console-view
-nix-update --version=skip python3Packages.buildbot-plugins.waterfall-view
-nix-update --version=skip python3Packages.buildbot-plugins.grid-view
-nix-update --version=skip python3Packages.buildbot-plugins.wsgi-dashboards
+nix-update buildbot
+nix-update --version=skip buildbot-worker
+# Cannot be auto-updated because it is not externally visible
+# nix-update --version=skip buildbot-pkg
+nix-update --version=skip buildbot-plugins.www
+nix-update --version=skip buildbot-plugins.console-view
+nix-update --version=skip buildbot-plugins.waterfall-view
+nix-update --version=skip buildbot-plugins.grid-view
+nix-update --version=skip buildbot-plugins.wsgi-dashboards
+nix-update --version=skip buildbot-plugins.badges
