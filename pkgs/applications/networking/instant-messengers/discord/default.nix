4b46618622229af99e6b6e00090287dc8bae86e6
jopejoe1 <johannes@joens.email>
2023-05-02 12:45:39 +0200

    discord: add jopejoe1 as maintainer
    (cherry picked from commit 985e990c456bdf54e1f9c7e1022117ec46c9b047)

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -59,7 +59,7 @@ let
     downloadPage = "https://discordapp.com/download";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
-    maintainers = with maintainers; [ MP2E devins2518 artturin infinidoge ];
+    maintainers = with maintainers; [ MP2E artturin infinidoge jopejoe1 ];
     platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
   };
   package =
