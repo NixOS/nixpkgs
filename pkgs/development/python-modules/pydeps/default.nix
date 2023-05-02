8bacab231d00d091c2403b7d1c279980e7c96587
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-02 19:18:39 +0200

    python311Packages.pydeps: 1.12.2 -> 1.12.3
    Diff: https://github.com/thebjorn/pydeps/compare/refs/tags/v1.12.2...v1.12.3
    
    Changelog: https://github.com/thebjorn/pydeps/releases/tag/v1.12.3

--- pkgs/development/python-modules/pydeps/default.nix
+++ pkgs/development/python-modules/pydeps/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "pydeps";
-  version = "1.12.2";
+  version = "1.12.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "thebjorn";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-c5A9iUq2M2PL76pi5v4AMqOsYLYYKN7ugYd8w7VfrYk=";
+    hash = "sha256-Zw5LGsNie2BAsJUWfL16HRdATpb5twkB5trBbIHNCc4=";
   };
 
   buildInputs = [
