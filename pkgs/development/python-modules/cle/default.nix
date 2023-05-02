f9272eb82e3a0917d0a4a0a301f094a3e5a98a3f
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-02 23:51:44 +0200

    python310Packages.cle: 9.2.48 -> 9.2.49

--- pkgs/development/python-modules/cle/default.nix
+++ pkgs/development/python-modules/cle/default.nix
@@ -16,7 +16,7 @@
 
 let
   # The binaries are following the argr projects release cycle
-  version = "9.2.48";
+  version = "9.2.49";
 
   # Binary files from https://github.com/angr/binaries (only used for testing and only here)
   binaries = fetchFromGitHub {
@@ -38,7 +38,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-D5qIuu8pqnkroU3ChmhseVitLrUJjwXr01MG7ing2Zk=";
+    hash = "sha256-xNYAYXKrfpvY9oYPmiR6GNaWAIUi9w1T9YznosIABSs=";
   };
 
   nativeBuildInputs = [
