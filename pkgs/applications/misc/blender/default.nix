NixOS/nixpkgs

commit edcd3849a6f4c11ac7c5fc8092a6deb0c2f7bf8f

blender: Fix build after changes in Alembic package

pkgs/applications/misc/blender/default.nix
@@ -95,6 +95,9 @@ stdenv.mkDerivation rec {
   cmakeFlags =
     [
       "-DWITH_ALEMBIC=ON"
+      # Blender supplies its own FindAlembic.cmake (incompatible with the Alembic-supplied config file)
+      "-DALEMBIC_INCLUDE_DIR=${lib.getDev alembic}/include"
+      "-DALEMBIC_LIBRARY=${lib.getLib alembic}/lib/libAlembic.so"
       "-DWITH_MOD_OCEANSIM=ON"
       "-DWITH_CODEC_FFMPEG=ON"
       "-DWITH_CODEC_SNDFILE=ON"

commit 3c4933f0905f5a880e06aaeb902017aa334fa675

blender: mark broken on darwin
Has been broken likely since edcd3849a6f4c11ac7c5fc8092a6deb0c2f7bf8f.

First failing Hydra: https://hydra.nixos.org/build/211894618

Notes from attempt to fix:

- The initial breakage may be easy to fix, but the upgrade to 3.4 added
  a lot more hurdles
- The patch fails to apply, but is trivially naïvely fixable
- At least some of the existing darwin-specific substituteInPlace no
  longer find anything
- When building with naïve fix for patch, fails with:
   > Could NOT find USD (missing: USD_LIBRARY USD_INCLUDE_DIR)
  for reasons that are not clear to me

The package has been broken for two months and no open issues, so
marking as broken as it’s not going to fix itself and if there is will
to fix it, someone can. For now, leaving the darwin conditionals in
place so not as to make a would-be-fixer’s job harder.

pkgs/applications/misc/blender/default.nix
@@ -181,6 +181,7 @@ stdenv.mkDerivation rec {
     # OptiX, enabled with cudaSupport, is non-free.
     license = with licenses; [ gpl2Plus ] ++ optional cudaSupport unfree;
     platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
+    broken = stdenv.isDarwin;
     maintainers = with maintainers; [ goibhniu veprbl ];
   };
 }