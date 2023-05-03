1519ef71b42d0a226bd2ed31d869f4ac3b19f37d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-05-03 22:34:00 +0300

    stanc: 2.32.0 -> 2.32.1

--- pkgs/development/compilers/stanc/default.nix
+++ pkgs/development/compilers/stanc/default.nix
@@ -5,7 +5,7 @@
 
 ocamlPackages.buildDunePackage rec {
   pname = "stanc";
-  version = "2.32.0";
+  version = "2.32.1";
 
   minimalOCamlVersion = "4.12";
 
@@ -13,7 +13,7 @@ ocamlPackages.buildDunePackage rec {
     owner = "stan-dev";
     repo = "stanc3";
     rev = "v${version}";
-    hash = "sha256-DZJb5Zcfki6vev1n+9zyokQTxEounCvTeYyH4BvSTZ0=";
+    hash = "sha256-dngmZXVb59uV7EugYIZAqE1gk66em7iuzllbbPWK2xk=";
   };
 
   # Error: This expression has type [ `Use_Sys_unix ]
