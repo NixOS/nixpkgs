ea27455fbd97bf70d7462cfd6eb815f4f7f7c177
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-03 05:46:31 +0000

    bazarr: 1.2.0 -> 1.2.1

--- pkgs/servers/bazarr/default.nix
+++ pkgs/servers/bazarr/default.nix
@@ -8,13 +8,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "bazarr";
-  version = "1.2.0";
+  version = "1.2.1";
 
   sourceRoot = ".";
 
   src = fetchurl {
     url = "https://github.com/morpheus65535/bazarr/releases/download/v${version}/bazarr.zip";
-    sha256 = "sha256-rlph8On/dc9Xyx8/KQzp4vX49wY4fr1oTtBEyfVhrsc=";
+    sha256 = "sha256-PuVK1jrNjxagESYvgqRBfxzsV/KxFhTdOyliO8smwec=";
   };
 
   nativeBuildInputs = [ unzip makeWrapper ];
