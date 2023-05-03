1a7ab2f7b26a12f33e02dbb9fbd8538980fdca66
figsoda <figsoda@pm.me>
2023-05-03 10:25:28 -0400

    nerdfix: 0.2.2 -> 0.2.3
    Diff: https://github.com/loichyan/nerdfix/compare/v0.2.2...v0.2.3
    
    Changelog: https://github.com/loichyan/nerdfix/blob/v0.2.3/CHANGELOG.md

--- pkgs/tools/text/nerdfix/default.nix
+++ pkgs/tools/text/nerdfix/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "nerdfix";
-  version = "0.2.2";
+  version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "loichyan";
     repo = "nerdfix";
     rev = "v${version}";
-    hash = "sha256-cC8BuDTYpXKAtbIylpLQKZG84N/kMp0jirqJanvEvZo=";
+    hash = "sha256-2YmlwHTll5BI1mV9/zEw86g302U203vpSNrN6q0x+es=";
   };
 
-  cargoHash = "sha256-K5bZRHce71hFvw7Ipbk4nazg/Wh4I161/MTQmTZakrQ=";
+  cargoHash = "sha256-hza+Vuxg9iIcXaYlPuhxZsxJ3EnYrjHzdWthXyCnEYc=";
 
   meta = with lib; {
     description = "Nerdfix helps you to find/fix obsolete nerd font icons in your project";
