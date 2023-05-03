197b1cddaec1a77347431692feaa99602d4f881b
Ryan Rix <ryan@whatthefuck.computer>
2023-05-03 16:32:39 -0700

    koreader: 2022.08 -> 2023.04

--- pkgs/applications/misc/koreader/default.nix
+++ pkgs/applications/misc/koreader/default.nix
@@ -10,15 +10,25 @@
 , SDL2
 , noto-fonts
 , nerdfonts }:
-let font-droid = nerdfonts.override { fonts = [ "DroidSansMono" ]; };
+let
+  font-droid = nerdfonts.override { fonts = [ "DroidSansMono" ]; };
+
+  patchedLuajit = luajit.overrideAttrs (old: {
+    patches = (old.patches or []) ++ [
+      (fetchurl {
+        url = "https://github.com/koreader/koreader-base/raw/4216c40d662660c9bbc48e79893b437e97518254/thirdparty/luajit/koreader-luajit-enable-table_pack.patch";
+        hash = "sha256-SKaKsb8AHW82KXPErep6+/RZ+d9WPX2ULZPowgkmmQM=";
+      })
+    ];
+  });
 in stdenv.mkDerivation rec {
   pname = "koreader";
-  version = "2022.08";
+  version = "2023.04";
 
   src = fetchurl {
     url =
       "https://github.com/koreader/koreader/releases/download/v${version}/koreader-${version}-amd64.deb";
-    sha256 = "sha256-+JBJNJTAnC5gpuo8cehfe/3YwGIW5iFA8bZ8nfz9qsk=";
+    sha256 = "sha256-tRUeRB1+UcWT49dchN0YDvd0L5n1YRdtMSFc8yy6m5o=";
   };
 
   sourceRoot = ".";
@@ -27,7 +37,7 @@ in stdenv.mkDerivation rec {
     glib
     gnutar
     gtk3-x11
-    luajit
+    patchedLuajit
     sdcv
     SDL2
   ];
@@ -39,14 +49,23 @@ in stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out
     cp -R usr/* $out/
-    ln -sf ${luajit}/bin/luajit $out/lib/koreader/luajit
+    ln -sf ${patchedLuajit}/bin/luajit $out/lib/koreader/luajit
     ln -sf ${sdcv}/bin/sdcv $out/lib/koreader/sdcv
     ln -sf ${gnutar}/bin/tar $out/lib/koreader/tar
     find $out -xtype l -delete
-    for i in ${noto-fonts}/share/fonts/truetype/noto/*; do
-        ln -s "$i" $out/lib/koreader/fonts/noto/
-    done
-    ln -s "${font-droid}/share/fonts/opentype/NerdFonts/Droid Sans Mono Nerd Font Complete Mono.otf" $out/lib/koreader/fonts/droid/DroidSansMono.ttf
+    # TODO: fix font file names either here or in font.lua
+    # for i in ${noto-fonts}/share/fonts/truetype/*ttf; do
+    #     ln -s "$i" $out/lib/koreader/fonts/noto/
+    # done
+
+    ln -s "${noto-fonts}/share/fonts/noto/NotoSans[wdth,wght].ttf" $out/lib/koreader/fonts/noto/NotoSans-Regular.ttf
+    ln -s "${noto-fonts}/share/fonts/noto/NotoSans[wdth,wght].ttf" $out/lib/koreader/fonts/noto/NotoSans-Bold.ttf
+    ln -s "${noto-fonts}/share/fonts/noto/NotoSans-Italic[wdth,wght].ttf" $out/lib/koreader/fonts/noto/NotoSans-Italic.ttf
+    ln -s "${noto-fonts}/share/fonts/noto/NotoNaskhArabicUI[wght].ttf" $out/lib/koreader/fonts/noto/NotoSansArabicUI-Regular.ttf
+    ln -s "${noto-fonts}/share/fonts/noto/NotoSansDevanagari[wdth,wght].ttf" $out/lib/koreader/fonts/noto/NotoSansDevanagariUI-Regular.ttf
+    ln -s "${noto-fonts}/share/fonts/noto/NotoSansBengali[wdth,wght].ttf" $out/lib/koreader/fonts/noto/NotoSansBengaliUI-Regular.ttf
+
+    ln -s "${font-droid}/share/fonts/opentype/NerdFonts/DroidSansMNerdFontMono-Regular.otf" $out/lib/koreader/fonts/droid/DroidSansMono.ttf
     wrapProgram $out/bin/koreader --prefix LD_LIBRARY_PATH : ${
       lib.makeLibraryPath [ gtk3-x11 SDL2 glib ]
     }
