a32f40876bea047ec58f9fba48cfe7a612afe2cf
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-05-03 11:29:02 +0200

    dnscontrol: 3.31.2 -> 3.31.3
    Diff: https://github.com/StackExchange/dnscontrol/compare/v3.31.2...v3.31.3
    
    Changelog: https://github.com/StackExchange/dnscontrol/releases/tag/v3.31.3

--- pkgs/applications/networking/dnscontrol/default.nix
+++ pkgs/applications/networking/dnscontrol/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "dnscontrol";
-  version = "3.31.2";
+  version = "3.31.3";
 
   src = fetchFromGitHub {
     owner = "StackExchange";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-vKfbL2a/5rTVsG0rDs/D0t5eXDNWlbwURI2FYzGu9lY=";
+    sha256 = "sha256-jGM/Of5/wSMs7cedbGmgJC05gxxHOCAjjEZ2Qmlxgew=";
   };
 
-  vendorHash = "sha256-BE/UnJw5elHYmyB+quN89ZkrlMcTjaVN0T2+h8cpPS8=";
+  vendorHash = "sha256-N7KS48Kr9SipliZ9JhMo2u9pRoE8+pxhC8B/YcZlNyg=";
 
   subPackages = [ "." ];
 
