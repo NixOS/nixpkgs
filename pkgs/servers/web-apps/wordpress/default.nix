64c3fd2bc82a6e47df20aaccfbf6121644921ff6
Jonas Heinrich <onny@project-insanity.org>
2023-05-03 22:14:59 +0200

    wordpress6_2: init at 6.2

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -8,4 +8,8 @@
     version = "6.1.1";
     hash = "sha256-IR6FSmm3Pd8cCHNQTH1oIaLYsEP1obVjr0bDJkD7H60=";
   };
+  wordpress6_2 = {
+    version = "6.2";
+    hash = "sha256-FDEo3rZc7SU9yqAplUScSMUWOEVS0e/PsrOPjS9m+QQ=";
+  };
 }
