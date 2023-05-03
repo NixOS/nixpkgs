5696e84ee7be09cebdd88950fd13a132895e8bf9
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-03 11:28:57 +0000

    python310Packages.browser-cookie3: 0.17.0 -> 0.18.0

--- pkgs/development/python-modules/browser-cookie3/default.nix
+++ pkgs/development/python-modules/browser-cookie3/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "browser-cookie3";
-  version = "0.17.0";
+  version = "0.18.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-fyyZ7pkNlD6MH1+SRUYjU0mXMUsZJ3qhOqJrcmJ/hgc=";
+    hash = "sha256-bSP6likSwEbxN4S9qbJmPcs8joc5e10FiqVL9gE7ni8=";
   };
 
   propagatedBuildInputs = [
