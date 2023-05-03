eec6471a0a2a5b13f4bf7eac204a9422ec3e3973
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-03 20:35:48 +0200

    python310Packages.bluetooth-auto-recovery: 1.0.3 -> 1.1.1
    Diff: https://github.com/Bluetooth-Devices/bluetooth-auto-recovery/compare/refs/tags/v1.0.3...v1.1.1
    
    Changelog: https://github.com/Bluetooth-Devices/bluetooth-auto-recovery/blob/v1.1.1/CHANGELOG.md

--- pkgs/development/python-modules/bluetooth-auto-recovery/default.nix
+++ pkgs/development/python-modules/bluetooth-auto-recovery/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "bluetooth-auto-recovery";
-  version = "1.0.3";
+  version = "1.1.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-gDypj2Vud6JtbGREPotvawgcsu5hbf92gJxxutWHcII=";
+    hash = "sha256-Kr8KzegMlRYgAwL+oHdb9A+/pTL+Ckpuu21CtraMwXg=";
   };
 
   nativeBuildInputs = [
