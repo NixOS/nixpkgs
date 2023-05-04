6538d97fd4bdb2104f397d245614c46fe7d14629
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-04 14:36:25 +0200

    python310Packages.django_3: 3.2.18 -> 3.2.19
    https://docs.djangoproject.com/en/dev/releases/3.2.19/
    
    Fixes: CVE-2023-31047

--- pkgs/development/python-modules/django/3.nix
+++ pkgs/development/python-modules/django/3.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "django";
-  version = "3.2.18";
+  version = "3.2.19";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "Django";
     inherit version;
-    hash = "sha256-CCCN/okutk//BzynQ7O5UjERBPk55/ba6VT+ctzFM7o=";
+    hash = "sha256-AxNluuloFNoZwQcGIYxE3/O2VMxN4gqYvS0pub3kafA=";
   };
 
   patches = [
