497562f7d7c427001268ceced5b1da42a1b3ee2a
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-05 00:00:46 +0200

    python311Packages.funcy: add changelog to meta
    - add format
    - disable on unsupported Python releases

--- pkgs/development/python-modules/funcy/default.nix
+++ pkgs/development/python-modules/funcy/default.nix
@@ -1,11 +1,15 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "funcy";
   version = "2.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
@@ -18,7 +22,9 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Collection of fancy functional tools focused on practicality";
     homepage = "https://funcy.readthedocs.org/";
+    changelog = "https://github.com/Suor/funcy/blob/2.0/CHANGELOG";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ ];
   };
 
 }
