0782eb2b02348cc501a3eb8a6cfcf0d384a0decf
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-04 23:08:08 +0200

    python311Packages.dbus-fast: 1.85.0 -> 1.86.0
    Diff: https://github.com/Bluetooth-Devices/dbus-fast/compare/refs/tags/v1.85.0...v1.86.0
    
    Changelog: https://github.com/Bluetooth-Devices/dbus-fast/releases/tag/v1.86.0

--- pkgs/development/python-modules/dbus-fast/default.nix
+++ pkgs/development/python-modules/dbus-fast/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "dbus-fast";
-  version = "1.85.0";
+  version = "1.86.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-pl5Qs7llmUna+i85hMl14UhTDkibPEcMaRnsPM7ODFg=";
+    hash = "sha256-YSkSnRQqalHpRVJx5PUO8EXXV8V6jRNpycO/GqNWmIM=";
   };
 
   nativeBuildInputs = [
