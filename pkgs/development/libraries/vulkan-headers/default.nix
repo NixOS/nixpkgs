afdf6855307090855dda3e075536613bb8dd6878
PedroHLC ☭ <root@pedrohlc.com>
2023-05-04 07:53:07 -0300

    Revert "Merge pull request #228870 from K900/vulkan-updates"
    This reverts commit b3a0bb7d5bde10ffe171f319eece7fcca89dd847, reversing
    changes made to b2f87d74f97674c9cb982e00124684d5825ff281.

--- pkgs/development/libraries/vulkan-headers/default.nix
+++ pkgs/development/libraries/vulkan-headers/default.nix
@@ -1,15 +1,15 @@
 { lib, stdenv, fetchFromGitHub, cmake }:
 stdenv.mkDerivation rec {
   pname = "vulkan-headers";
-  version = "1.3.249";
+  version = "1.3.243.0";
 
   nativeBuildInputs = [ cmake ];
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "Vulkan-Headers";
-    rev = "v${version}";
-    hash = "sha256-PLqF9lO7vWvgRZvXLmOcNhTgkB+3TXUa0eoALwDc5Ws=";
+    rev = "sdk-${version}";
+    hash = "sha256-iitEA/x9QpbQrYTcV0OzBgnY6bQFhIm+mVq1ryIQ3+0=";
   };
 
   passthru.updateScript = ./update.sh;
