8dc6ea5d4298c0753aaf78483e3f83907f9dbc87
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-04 22:53:37 +0200

    python311Packages.aionotion: 2023.04.2 -> 2023.05.0
    Diff: https://github.com/bachya/aionotion/compare/2023.04.2...2023.05.0

--- pkgs/development/python-modules/aionotion/default.nix
+++ pkgs/development/python-modules/aionotion/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aionotion";
-  version = "2023.04.2";
+  version = "2023.05.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "bachya";
     repo = pname;
     rev = version;
-    hash = "sha256-pMBUhCm16+Zs6xZExLB4Z5y+OKNHX+utjsfMLeYUSWY=";
+    hash = "sha256-wnyM8ERHJydhBdX9ZAskGdvIrvZNhYh/UVJv/JdxChE=";
   };
 
   nativeBuildInputs = [
