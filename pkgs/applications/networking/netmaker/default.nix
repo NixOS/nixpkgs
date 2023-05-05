9c056e89680151b3b6eaaba89a54cab7726493c4
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-05 17:30:30 +0000

    netmaker: 0.18.7 -> 0.19.0

--- pkgs/applications/networking/netmaker/default.nix
+++ pkgs/applications/networking/netmaker/default.nix
@@ -10,16 +10,16 @@
 
 buildGoModule rec {
   pname = "netmaker";
-  version = "0.18.7";
+  version = "0.19.0";
 
   src = fetchFromGitHub {
     owner = "gravitl";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-XnBz5dUBu6VqxLFsBXOvdLu/LsrfyEp9MLR/+nNggBk=";
+    hash = "sha256-wiexultPliYD3WrLVtWUdLs762OzLAmoH66phwjOuUw=";
   };
 
-  vendorHash = "sha256-a2ecHdxX82/JScRPGKpgEtrISD7qkPoZyv9kvO6SzaQ=";
+  vendorHash = "sha256-Msvonap1soJExzBymouY8kZJnHT4SIwpfJjBgpkO2Rw=";
 
   inherit subPackages;
 
