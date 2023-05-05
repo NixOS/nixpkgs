35400ca2a545423de049fcf7b06aca9dc32971ab
Felix Buehler <account@buehler.rocks>
2023-05-05 23:35:37 +0200

    ariang: 1.3.3 -> 1.3.4

--- pkgs/servers/ariang/node-composition.nix
+++ pkgs/servers/ariang/node-composition.nix
@@ -2,7 +2,7 @@
 
 {pkgs ? import <nixpkgs> {
     inherit system;
-  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs_14"}:
+  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs-18_x"}:
 
 let
   nodeEnv = import ../../development/node-packages/node-env.nix {
