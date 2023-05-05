6dfe6cbf6be6f4f29a73ad89454b186deec5844d
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-05 15:36:44 +0000

    zsh-autocomplete: 22.01.21 -> 23.05.02

--- pkgs/shells/zsh/zsh-autocomplete/default.nix
+++ pkgs/shells/zsh/zsh-autocomplete/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "zsh-autocomplete";
-  version = "22.01.21";
+  version = "23.05.02";
 
   src = fetchFromGitHub {
     owner = "marlonrichert";
     repo = "zsh-autocomplete";
     rev = version;
-    sha256 = "sha256-+UziTYsjgpiumSulrLojuqHtDrgvuG91+XNiaMD7wIs=";
+    sha256 = "sha256-HVt7JGkNxg9Kb29x95mxq/vtQ1dBbNeQlb9lK2qTBSI=";
   };
 
   strictDeps = true;
