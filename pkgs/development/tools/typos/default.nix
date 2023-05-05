c6d51780cdc7de89f052b4c171d8ce3ef4062eaa
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-05 04:48:48 +0000

    typos: 1.14.8 -> 1.14.9

--- pkgs/development/tools/typos/default.nix
+++ pkgs/development/tools/typos/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "typos";
-  version = "1.14.8";
+  version = "1.14.9";
 
   src = fetchFromGitHub {
     owner = "crate-ci";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-x7Pcg2zgu2s+oLkOJj+Eo/Gs48BJO6+JATckMqaeaj4=";
+    hash = "sha256-dfUXH7MRTnHYSqNJzlT0fUn/Er0wrTARq3ZuOdWToow=";
   };
 
-  cargoHash = "sha256-4se9/lcVWAWhbi0i3FDGQraK5KhPZ6ongc2wmJV4gI0=";
+  cargoHash = "sha256-+u/3XtC/HxtAsX4dRf74u0BLh872Y2kK+BnbWqUnUdo=";
 
   meta = with lib; {
     description = "Source code spell checker";
