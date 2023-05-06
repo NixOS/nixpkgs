d1446b54974568382e6e77ccf8304a62be17d45b
Gregor Godbersen <git@doamo.de>
2023-05-06 17:16:05 +0200

    photoprism: 221118-e58fee0fb -> 230506-9de9a3540

--- pkgs/servers/photoprism/backend.nix
+++ pkgs/servers/photoprism/backend.nix
@@ -19,7 +19,7 @@ buildGoModule rec {
     substituteInPlace internal/commands/passwd.go --replace '/bin/stty' "${coreutils}/bin/stty"
   '';
 
-  vendorSha256 = "sha256-59ZwijPCcmhMPeh7v8EdqzraqRx+HhK6VnUk0JvAbbU=";
+  vendorSha256 = "sha256-OyHescVn73kNF+Ji07Ea+pQC+IRx8/ywb/RjfLHH84k=";
 
   subPackages = [ "cmd/photoprism" ];
 
