19bb652ec2dcbc66efd74e6cefc11e1b56a4cb33
Brendan Tobolaski <brendan@tobolaski.com>
2023-05-05 19:49:24 -0500

    logseq: Add libstdc++ to the wrapper
    When launching logseq, it outputs Error: libstdc++.so.6: cannot open
    shared object file but, continues operatiing. This makes the log message
    disappear but does not seem to change its ability to function.

--- pkgs/applications/misc/logseq/default.nix
+++ pkgs/applications/misc/logseq/default.nix
@@ -56,7 +56,8 @@ stdenv.mkDerivation rec {
     makeWrapper ${electron}/bin/electron $out/bin/${pname} \
       --set "LOCAL_GIT_DIRECTORY" ${git} \
       --add-flags $out/share/${pname}/resources/app \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ stdenv.cc.cc.lib ]}"
   '';
 
   passthru.updateScript = nix-update-script { };
