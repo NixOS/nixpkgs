4207486111d2b2cc20acffc7e21c1c06900ea785
HaxSam <haxsam@pm.me>
2023-05-06 11:02:07 +0200

    ani-cli: cleanup package

--- pkgs/applications/video/ani-cli/default.nix
+++ pkgs/applications/video/ani-cli/default.nix
@@ -4,10 +4,18 @@
 , lib
 , gnugrep
 , gnused
-, wget
+, curl
 , fzf
 , mpv
+, vlc
+, iina
+, catt
+, syncplay
+, ffmpeg
 , aria2
+, player ? "mpv"
+, chromecast ? "false"
+, sync ? "false"
 }:
 
 stdenvNoCC.mkDerivation rec {
@@ -22,23 +30,24 @@ stdenvNoCC.mkDerivation rec {
   };
 
   nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ gnugrep gnused curl fzf ffmpeg aria2 ]
+                ++ lib.optional (player == "mpv") mpv
+                ++ lib.optional (player == "vlc") vlc
+                ++ lib.optional (player == "iina") iina
+                ++ lib.optional (chromecast != "false") catt
+                ++ lib.optional (sync != "false") syncplay;
 
   installPhase = ''
-    runHook preInstall
-
     install -Dm755 ani-cli $out/bin/ani-cli
-
     wrapProgram $out/bin/ani-cli \
-      --prefix PATH : ${lib.makeBinPath [ gnugrep gnused wget fzf mpv aria2 ]}
-
-    runHook postInstall
+      --prefix PATH : ${lib.makeBinPath buildInputs}
   '';
 
   meta = with lib; {
     homepage = "https://github.com/pystardust/ani-cli";
     description = "A cli tool to browse and play anime";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ skykanin ];
+    maintainers = with maintainers; [ skykanin haxsam ];
     platforms = platforms.unix;
   };
 }
