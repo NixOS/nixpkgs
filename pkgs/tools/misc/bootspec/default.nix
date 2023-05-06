2bc1fe126618ade464f01cca22f45129aed1ddf2
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-06 02:18:51 +0000

    bootspec: 0.1.0 -> 0.2.0

--- pkgs/tools/misc/bootspec/default.nix
+++ pkgs/tools/misc/bootspec/default.nix
@@ -4,16 +4,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "bootspec";
-  version = "0.1.0";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "DeterminateSystems";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Gf6cIFympRIZo6vzQIX3sQ3ycLlmkDRXtEd2IYH7LQo=";
+    hash = "sha256-6hgC/bOtzmVu+/pSVMpW4IkwNNemI2k/ykzxCibQUok=";
   };
 
-  cargoHash = "sha256-8qm9aUvH1EbZ5Jmtw+86KdNyLbYJ7BVExTyyexirTyw=";
+  cargoHash = "sha256-l9W7MzeL1kiTvNe7QbP2bt8vqbnGrqK44UTlRRNRcYw=";
 
   meta = with lib; {
     description = "Implementation of RFC-0125's datatype and synthesis tooling";
