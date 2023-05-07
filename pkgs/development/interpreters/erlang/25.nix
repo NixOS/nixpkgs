dca5532206e12479c351f7876c84823a7a74582d
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-07 05:48:45 +0000

    erlang_25: 25.3.1 -> 25.3.2

--- pkgs/development/interpreters/erlang/25.nix
+++ pkgs/development/interpreters/erlang/25.nix
@@ -1,6 +1,6 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "25.3.1";
-  sha256 = "dMMw4UbZMsmb3t0S2X2EpbTKZPKTaWWbSP4OUGWnC4c=";
+  version = "25.3.2";
+  sha256 = "cuFeHwyAyS8R7rXcJJxzE7LTZGgDr4x3qMZx63PIowM=";
 }
