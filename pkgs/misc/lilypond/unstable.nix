a46d56ac76977ae4fc9c9dd55f5410fc4de4ac26
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-07 20:36:18 +0000

    lilypond-unstable: 2.25.3 -> 2.25.4

--- pkgs/misc/lilypond/unstable.nix
+++ pkgs/misc/lilypond/unstable.nix
@@ -1,10 +1,10 @@
 { lib, fetchurl, lilypond }:
 
 lilypond.overrideAttrs (oldAttrs: rec {
-  version = "2.25.3";
+  version = "2.25.4";
   src = fetchurl {
     url = "https://lilypond.org/download/sources/v${lib.versions.majorMinor version}/lilypond-${version}.tar.gz";
-    sha256 = "sha256-CVMMzL31NWd6PKf66m0ctBXFpqHMwxQibuifaU9lftg=";
+    sha256 = "sha256-O7YQc00774Nz6KIGC1Za1HBvKaHmUjXeKkZs0YR1HUA=";
   };
 
   passthru.updateScript = {
