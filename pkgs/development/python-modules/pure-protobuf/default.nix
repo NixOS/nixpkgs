f3451043ed13ad92578049e9e1053ba691bedb8c
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-07 17:54:29 +0000

    python310Packages.pure-protobuf: 2.2.3 -> 2.3.0

--- pkgs/development/python-modules/pure-protobuf/default.nix
+++ pkgs/development/python-modules/pure-protobuf/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "pure-protobuf";
-  version = "2.2.3";
+  version = "2.3.0";
 
   format = "pyproject";
   disabled = pythonOlder "3.7";
@@ -21,8 +21,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "eigenein";
     repo = "protobuf";
-    rev = version;
-    hash = "sha256-FsVWlYPav4uusdEPXc5hScLeNJWfbSjGOLuZ7yZXyCw=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-nJ3F8dUrqMeWqTV9ErGqrMvofJwBKwNUDfxWIqFh4nY=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
