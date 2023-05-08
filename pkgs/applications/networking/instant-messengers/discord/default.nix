2562f4dd61558102c09efed45d330992b6f94913
Sefa Eyeoglu <contact@scrumplex.net>
2023-05-08 08:44:09 +0200

    discord: add Scrumplex to maintainers
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -59,7 +59,7 @@ let
     downloadPage = "https://discordapp.com/download";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
-    maintainers = with maintainers; [ MP2E artturin infinidoge jopejoe1 ];
+    maintainers = with maintainers; [ MP2E Scrumplex artturin infinidoge jopejoe1 ];
     platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
   };
   package =
