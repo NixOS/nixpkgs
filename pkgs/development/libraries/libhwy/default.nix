NixOS/nixpkgs

commit 7328b78fb58b8bb26d29b9421d2fa6b55bcf2199

libhwy: 0.15.0 -> 1.0.2

pkgs/development/libraries/libhwy/default.nix
@@ -1,25 +1,15 @@
-{ lib, stdenv, cmake, ninja, gtest, fetchpatch, fetchFromGitHub }:
+{ lib, stdenv, cmake, ninja, gtest, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "libhwy";
-  version = "0.15.0";
+  version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "highway";
     rev = version;
-    sha256 = "sha256-v2HyyHtBydr7QiI83DW1yRv2kWjUOGxFT6mmdrN9XPo=";
+    hash = "sha256-CHzDLzOnu/QfejWiRKE9I5UUyRxoEooNtYVe8FQwu7c=";
   };
-
-  patches = [
-    # Remove on next release
-    # https://github.com/google/highway/issues/460
-    (fetchpatch {
-      name = "hwy-add-missing-includes.patch";
-      url = "https://github.com/google/highway/commit/8ccab40c2f931aca6004d175eec342cc60f6baec.patch";
-      sha256 = "sha256-wlp5gIvK2+OlKtsZwxq/pXTbESkUtimHXaYDjcBzmQ0=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake ninja ];
 
@@ -27,7 +17,7 @@ stdenv.mkDerivation rec {
   dontUseCmakeBuildDir = true;
 
   cmakeFlags = let
-    libExt = stdenv.hostPlatform.extensions.library;
+    libExt = stdenv.hostPlatform.extensions.sharedLibrary;
   in [
     "-GNinja"
     "-DCMAKE_INSTALL_LIBDIR=lib"
@@ -37,6 +27,8 @@ stdenv.mkDerivation rec {
     "-DGTEST_INCLUDE_DIR=${lib.getDev gtest}/include"
     "-DGTEST_LIBRARY=${lib.getLib gtest}/lib/libgtest${libExt}"
     "-DGTEST_MAIN_LIBRARY=${lib.getLib gtest}/lib/libgtest_main${libExt}"
+  ] ++ lib.optionals stdenv.hostPlatform.isAarch32 [
+    "-DHWY_CMAKE_ARM7=ON"
   ];
 
   # hydra's darwin machines run into https://github.com/libjxl/libjxl/issues/408

commit 0c9cfdd0e54741b082ae9a053298956fc4d3d386

pkgsStatic.libhwy: fix build
This was changed in 7328b78fb58, but I don't understand why — the only
difference should be in static builds, which worked before that change
and didn't work after.

Fixes: 7328b78fb58 ("libhwy: 0.15.0 -> 1.0.2")

pkgs/development/libraries/libhwy/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
   dontUseCmakeBuildDir = true;
 
   cmakeFlags = let
-    libExt = stdenv.hostPlatform.extensions.sharedLibrary;
+    libExt = stdenv.hostPlatform.extensions.library;
   in [
     "-GNinja"
     "-DCMAKE_INSTALL_LIBDIR=lib"