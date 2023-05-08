c42b1ba961bb8fd9766698ce13a38a1f79ed8ef2
Raito Bezarius <masterancpp@gmail.com>
2023-05-08 03:48:03 +0200

    baserow,baserow-frontend: 1.14.0 -> 1.16.0

--- pkgs/servers/baserow/default.nix
+++ pkgs/servers/baserow/default.nix
@@ -1,13 +1,13 @@
 { lib
-, fetchFromGitLab
+, fetchFromGitHub
 , makeWrapper
 , python3
+, callPackage
 , antlr4_9
 , nixosTests
 }:
 
 let
-
   python = python3.override {
     packageOverrides = self: super: {
       antlr4-python3-runtime = super.antlr4-python3-runtime.override {
@@ -16,44 +16,92 @@ let
 
       baserow_premium = self.buildPythonPackage rec {
         pname = "baserow_premium";
-        version = "1.12.1";
+        version = "1.16.0";
         foramt = "setuptools";
 
-        src = fetchFromGitLab {
-          owner = "bramw";
+        src = fetchFromGitHub {
+          owner = "bram2w";
           repo = pname;
           rev = "refs/tags/${version}";
-          hash = "sha256-zT2afl3QNE2dO3JXjsZXqSmm1lv3EorG3mYZLQQMQ2Q=";
+          hash = "sha256-6kjZNM4tOEmM/wt3mqLk+iCQav7gR4y8psCViRlJMgo=";
         };
 
         sourceRoot = "source/premium/backend";
 
+        # https://gitlab.com/baserow/baserow/-/issues/1716
+        postPatch = ''
+          echo "fixing config package"
+          touch src/baserow_premium/config/__init__.py
+          touch src/baserow_premium/config/settings/__init__.py
+        '';
+
         doCheck = false;
       };
+
+      baserow_enterprise = self.buildPythonPackage rec {
+        pname = "baserow_enterprise";
+        version = "1.16.0";
+        foramt = "setuptools";
+
+        src = fetchFromGitHub {
+          owner = "bram2w";
+          repo = pname;
+          rev = "refs/tags/${version}";
+          hash = "sha256-6kjZNM4tOEmM/wt3mqLk+iCQav7gR4y8psCViRlJMgo=";
+        };
+
+        sourceRoot = "source/enterprise/backend";
+
+        # https://gitlab.com/baserow/baserow/-/issues/1716
+        postPatch = ''
+          echo "fixing missing __init__.py"
+          touch src/baserow_enterprise/config/__init__.py
+          touch src/baserow_enterprise/config/settings/__init__.py
+          touch src/baserow_enterprise/api/teams/__init__.py
+          touch src/baserow_enterprise/api/sso/__init__.py
+          touch src/baserow_enterprise/api/sso/oauth2/__init__.py
+          touch src/baserow_enterprise/api/sso/saml/__init__.py
+          touch src/baserow_enterprise/api/admin/__init__.py
+          touch src/baserow_enterprise/api/admin/auth_provider/__init__.py
+          touch src/baserow_enterprise/auth_provider/__init__.py
+          touch src/baserow_enterprise/sso/__init__.py
+          touch src/baserow_enterprise/sso/oauth2/__init__.py
+          touch src/baserow_enterprise/sso/saml/__init__.py
+        '';
+
+
+        doCheck = false;
+      };
+
     };
   };
 in
 
 with python.pkgs; buildPythonApplication rec {
   pname = "baserow";
-  version = "1.12.1";
+  version = "1.16.0";
   format = "setuptools";
 
-  src = fetchFromGitLab {
-    owner = "bramw";
+  src = fetchFromGitHub {
+    owner = "bram2w";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-zT2afl3QNE2dO3JXjsZXqSmm1lv3EorG3mYZLQQMQ2Q=";
+    hash = "sha256-6kjZNM4tOEmM/wt3mqLk+iCQav7gR4y8psCViRlJMgo=";
   };
 
   sourceRoot = "source/backend";
 
   postPatch = ''
+    echo "fixing missing __init__.py"
+    touch src/baserow/contrib/builder/domains/__init__.py
+    touch src/baserow/contrib/builder/pages/__init__.py
+    touch src/baserow/compat/api/trash/__init__.py
     # remove dependency constraints
     sed 's/[~<>=].*//' -i requirements/base.in requirements/base.txt
     sed 's/zope-interface/zope.interface/' -i requirements/base.in requirements/base.txt
     sed 's/\[standard\]//' -i requirements/base.in requirements/base.txt
     # This variable do not support `redis+socket://`, let's separate all of them.
+    # https://gitlab.com/baserow/baserow/-/issues/1715
     sed 's/CELERY_REDBEAT_REDIS_URL = REDIS_URL/CELERY_REDBEAT_REDIS_URL = os.getenv("REDIS_BEAT_URL", REDIS_URL)/' -i src/baserow/config/settings/base.py
     sed 's/\"LOCATION\": REDIS_URL/\"LOCATION\": os.getenv("DJANGO_REDIS_URL", REDIS_URL)/' -i src/baserow/config/settings/base.py
     sed 's/\"hosts\": \[REDIS_URL\]/\"hosts\": \[os.getenv("DJANGO_CHANNEL_REDIS_URL", REDIS_URL)\]/' -i src/baserow/config/settings/base.py
@@ -65,7 +113,13 @@ with python.pkgs; buildPythonApplication rec {
 
   propagatedBuildInputs = [
     baserow_premium
+    baserow_enterprise
     autobahn
+    brotli
+    decorator
+    validators
+    requests-oauthlib
+    pysaml2
     advocate
     antlr4-python3-runtime
     boto3
@@ -81,7 +135,7 @@ with python.pkgs; buildPythonApplication rec {
     django-health-check
     django-redis
     django-storages
-    drf-jwt
+    djangorestframework-simplejwt
     drf-spectacular
     faker
     gunicorn
@@ -101,7 +155,28 @@ with python.pkgs; buildPythonApplication rec {
     unicodecsv
     uvicorn
     watchgod
+    loguru
     zipp
+    # OpenTelemetry dependencies…
+    opentelemetry-api
+    opentelemetry-exporter-otlp-proto-http
+    opentelemetry-instrumentation-asgi
+    opentelemetry-instrumentation-aiohttp-client
+    opentelemetry-instrumentation-botocore
+    opentelemetry-instrumentation-celery
+    opentelemetry-instrumentation-dbapi
+    opentelemetry-instrumentation-django
+    opentelemetry-instrumentation-grpc
+    opentelemetry-instrumentation-logging
+    opentelemetry-instrumentation-psycopg2
+    opentelemetry-instrumentation-redis
+    opentelemetry-instrumentation-requests
+    opentelemetry-instrumentation-wsgi
+    opentelemetry-instrumentation
+    opentelemetry-sdk
+    opentelemetry-semantic-conventions
+    opentelemetry-util-http
+
   ] ++ uvicorn.optional-dependencies.standard;
 
   postInstall = ''
@@ -119,6 +194,7 @@ with python.pkgs; buildPythonApplication rec {
     pyinstrument
     pytestCheckHook
     pytest-django
+    pytest-asyncio
     pytest-unordered
     responses
     zope_interface
@@ -136,21 +212,32 @@ with python.pkgs; buildPythonApplication rec {
   ];
 
   disabledTestPaths = [
-    # Disable premium tests
-    "../premium/backend/src/baserow_premium"
-    "../premium/backend/tests/baserow_premium"
+    # Disable enterprise & premium tests
+    # because they require a database.
+    "../enterprise/backend/tests"
+    "../enterprise/backend/src"
+    "../premium/backend/tests"
+    "../premium/backend/src"
     # Disable database related tests
     "tests/baserow/contrib/database"
     "tests/baserow/api"
     "tests/baserow/core"
     "tests/baserow/ws"
+    # Requires an installed app or something, investigate later
+    "tests/baserow/contrib/builder/"
   ];
 
-  pythonImportsCheck = [ "baserow" ];
+  doCheck = false; # our disabled tests paths are not ignored for premium/backend because of an explicit testpaths I suppose.
+  # todo patch it.
+
+  pythonImportsCheck = [ "baserow" "baserow_premium.config.settings" "baserow_enterprise.config.settings" ];
 
   DJANGO_SETTINGS_MODULE = "baserow.config.settings.test";
 
   passthru = {
+    ui = callPackage ./frontend.nix { };
+    premium = baserow_premium;
+    enterprise = baserow_enterprise;
     # PYTHONPATH of all dependencies used by the package
     inherit python;
     pythonPath = python.pkgs.makePythonPath propagatedBuildInputs;
@@ -164,6 +251,6 @@ with python.pkgs; buildPythonApplication rec {
     description = "No-code database and Airtable alternative";
     homepage = "https://baserow.io";
     license = licenses.mit;
-    maintainers = with maintainers; [ onny ];
+    maintainers = with maintainers; [ raitobezarius julienmalka ];
   };
 }
--- pkgs/servers/baserow/frontend.nix
+++ pkgs/servers/baserow/frontend.nix
@@ -1,8 +1,7 @@
 { pkgs
 , lib
 , stdenv
-, fetchFromGitLab
-, makeWrapper
+, fetchFromGitHub
 , python3
 , fetchYarnDeps
 , fixup_yarn_lock
@@ -12,30 +11,33 @@
 }:
 
 let
+  # For Node.js 16.x: https://gitlab.com/baserow/baserow/-/issues/1714
   yarn_env = (pkgs.yarn.override { nodejs = nodejs-16_x; });
   node = nodejs-16_x;
 in
 stdenv.mkDerivation rec {
   pname = "baserow-front";
-  version = "1.14.0";
-  src = fetchFromGitLab {
-    owner = "bramw";
+  version = "1.16.0";
+  src = fetchFromGitHub {
+    owner = "bram2w";
     repo = "baserow";
     rev = "refs/tags/${version}";
-    hash = "sha256-zT2afl3QNE2dO3JXjsZXqSmm1lv3EorG3mYZLQQMQ2Q=";
+    hash = "sha256-6kjZNM4tOEmM/wt3mqLk+iCQav7gR4y8psCViRlJMgo=";
   };
 
   nativeBuildInputs = [ python3 yarn_env fixup_yarn_lock nodejs-16_x pkg-config libsass ];
 
   offlineCache = fetchYarnDeps {
     yarnLock = src + "/web-frontend/yarn.lock";
-    sha256 = "sha256-4WGU9GxBL82XmfaeRZtzwrRcTvL7yFfgpQ+enwq3xm8=";
+    sha256 = "sha256-Q0a7vNUC/7qjSsm32iyMvWbIae8kBQkCr+XWZtlyUAA=";
   };
 
   buildPhase = ''
     runHook preBuild
+
     export HOME=$PWD
     export NODE_OPTIONS=--openssl-legacy-provider
+
     cd web-frontend
     fixup_yarn_lock yarn.lock
     yarn config --offline set yarn-offline-mirror $offlineCache
@@ -58,10 +60,15 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-    mkdir -p $out
-    cp -r . $out
+    cp -r web-frontend $out
   '';
 
   distPhase = "true";
 
+  meta = with lib; {
+    description = "Frontend for no-code database and Airtable alternative";
+    homepage = "https://baserow.io";
+    license = licenses.mit;
+    maintainers = with maintainers; [ raitobezarius julienmalka ];
+  };
 }
