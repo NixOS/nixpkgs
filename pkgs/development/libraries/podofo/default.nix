bfe413292055f0d0ef86e678f2b1d3ef1a2643ba
Robert Scott <code@humanleg.org.uk>
2023-05-08 20:06:17 +0100

    podofo: 0.9.8 -> 0.10.0

--- pkgs/development/libraries/podofo/default.nix
+++ pkgs/development/libraries/podofo/default.nix
@@ -1,26 +1,61 @@
-{ lib, stdenv, fetchurl, cmake, zlib, freetype, libjpeg, libtiff, fontconfig
-, openssl, libpng, lua5, pkg-config, libidn, expat
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, expat
+, fontconfig
+, freetype
+, libidn
+, libjpeg
+, libpng
+, libtiff
+, libxml2
+, lua5
+, openssl
+, pkg-config
+, zlib
+, catch2
 }:
 
-stdenv.mkDerivation rec {
-  version = "0.9.8";
+stdenv.mkDerivation (finalAttrs: rec {
+  version = "0.10.0";
   pname = "podofo";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/podofo/${pname}-${version}.tar.gz";
-    sha256 = "sha256-XeYH4V8ZK4rZBzgwB1nYjeoPXM3OO/AASKDJMrxkUVQ=";
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = version;
+    hash = "sha256-W61ufT/YT8DLVpGM2ai7ZFE6mR+vassyK8Ool/nAfU4=";
+    fetchSubmodules = true;  # includes test data
   };
+
+  prePatch = ''
+    # remove any possibility it may try to use these
+    rm -r extern/deps
+  '';
 
   outputs = [ "out" "dev" "lib" ];
 
   nativeBuildInputs = [ cmake pkg-config ];
 
-  buildInputs = [ zlib freetype libjpeg libtiff fontconfig openssl libpng
-                  libidn expat lua5 ];
+  buildInputs = [
+    expat
+    fontconfig
+    freetype
+    libidn
+    libjpeg
+    libpng
+    libtiff
+    libxml2
+    lua5
+    openssl
+    zlib
+  ];
+
+  nativeCheckInputs = [ catch2 ];
 
   cmakeFlags = [
-    "-DPODOFO_BUILD_SHARED=ON"
-    "-DPODOFO_BUILD_STATIC=OFF"
+    "-DPODOFO_BUILD_TEST=${if finalAttrs.finalPackage.doCheck then "ON" else "OFF"}"
     "-DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON"
   ];
 
@@ -28,10 +63,12 @@ stdenv.mkDerivation rec {
     moveToOutput lib "$lib"
   '';
 
+  doCheck = true;
+
   meta = with lib; {
     homepage = "https://podofo.sourceforge.net";
     description = "A library to work with the PDF file format";
     platforms = platforms.all;
     license = with licenses; [ gpl2Plus lgpl2Plus ];
   };
-}
+})
