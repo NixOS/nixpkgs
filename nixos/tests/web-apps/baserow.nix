5622f49a0fccbb59c9afb8a824279b2a69028663
Raito Bezarius <masterancpp@gmail.com>
2023-05-08 03:48:27 +0200

    nixos/tests/baserow: idk how to make this test pass srsly

--- nixos/tests/web-apps/baserow.nix
+++ nixos/tests/web-apps/baserow.nix
@@ -9,12 +9,37 @@ import ../make-test-python.nix ({ lib, pkgs, ... }: {
     services.baserow = {
       enable = true;
       environment = {
+        # Needed for the frontend.
         BASEROW_PUBLIC_URL = "http://localhost";
+        # Ensure we are allowed to do those requests.
+        BASEROW_EXTRA_ALLOWED_HOSTS	= "localhost,[::1],127.0.0.1";
       };
       # Do not do this in production.
       secretFile = pkgs.writeText "secret" ''
       SECRET_KEY=aaaaaaaaaaaaaaaaaaaaaaaa
       '';
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts."localhost" = {
+        locations."~ ^/(api|ws)/" = {
+          proxyPass = "http://[::1]:8000";
+          proxyWebsockets = true;
+        };
+        locations."/media/" = {
+          extraConfig = ''
+            if ($arg_dl) {
+              add_header Content-disposition "attachment; filename=$arg_dl";
+            }
+            alias /var/lib/baserow/media;
+          '';
+        };
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+        };
+      };
     };
   };
 
@@ -22,14 +47,15 @@ import ../make-test-python.nix ({ lib, pkgs, ... }: {
     machine.start()
     machine.wait_for_unit("baserow.target")
     machine.wait_for_open_port(8000)
+    machine.wait_for_unit("nginx.service")
+    machine.wait_for_open_port(80)
 
+    # Backend
     print(machine.succeed(
-        "curl -H 'Host: localhost' -sSfL http://[::1]:8000/api/settings/"
+        "curl -sSfL http://localhost/api/settings/"
     ))
-    # [::1] is not an allowed host.
-    machine.fail(
-        "curl -sSfL http://[::1]:8000/api/settings/"
-    )
 
+    # Frontend
+    machine.wait_for_open_port(3000)
   '';
 })
