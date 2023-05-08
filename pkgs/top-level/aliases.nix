7a6a78c9700cf0205975ae07e0b04c2e3442fc8c
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-05-09 00:54:59 +0300

    insync-v3: remove
    insync-v3 has been merged into the insync package; use insync instead.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -714,6 +714,7 @@ mapAliases ({
   imagemagick7_light = imagemagick_light; # Added 2021-02-22
   imlib = throw "imlib has been dropped due to the lack of maintenance from upstream since 2004"; # Added 2023-01-04
   impressive = throw "impressive has been removed due to lack of released python 2 support and maintainership in nixpkgs"; # Added 2022-01-27
+  insync-v3 = throw "insync-v3 has been merged into the insync package; use insync instead"; #Added 2023-05-13
   i-score = throw "i-score has been removed: abandoned upstream"; # Added 2020-11-21
   inboxer = throw "inboxer has been removed as it is no longer maintained and no longer works as Google shut down the inbox service this package wrapped";
   index-fm = libsForQt5.mauiPackages.index; # added 2022-05-17
