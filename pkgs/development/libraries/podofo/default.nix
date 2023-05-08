9d31d1d616e64c27f19b33c1d9fcb3842f516b35
Robert Scott <code@humanleg.org.uk>
2023-05-08 20:06:17 +0100

    podofo: enable tests

--- pkgs/development/libraries/podofo/default.nix
+++ pkgs/development/libraries/podofo/default.nix
@@ -14,6 +14,7 @@
 , openssl
 , pkg-config
 , zlib
+, catch2
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -24,8 +25,14 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "podofo";
     repo = "podofo";
     rev = finalAttrs.version;
-    hash = "sha256-Z9mVAo2dITEtTdqA2sftaLZSCiTbGS02RYxfNcEwd1c=";
+    hash = "sha256-W61ufT/YT8DLVpGM2ai7ZFE6mR+vassyK8Ool/nAfU4=";
+    fetchSubmodules = true;  # includes test data
   };
+
+  prePatch = ''
+    # remove any possibility it may try to use these
+    rm -r extern/deps
+  '';
 
   outputs = [ "out" "dev" "lib" ];
 
@@ -50,12 +57,16 @@ stdenv.mkDerivation (finalAttrs: {
 
   cmakeFlags = [
     "-DPODOFO_BUILD_STATIC=${if stdenv.hostPlatform.isStatic then "ON" else "OFF"}"
+    "-DPODOFO_BUILD_TEST=${if finalAttrs.finalPackage.doCheck then "ON" else "OFF"}"
     "-DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON"
   ];
 
   postInstall = ''
     moveToOutput lib "$lib"
   '';
+
+  doCheck = true;
+  nativeCheckInputs = [ catch2 ];
 
   meta = {
     homepage = "https://github.com/podofo/podofo";
