d9385a3df161e906f44f8535fc4012c39451797a
aleksana <me@aleksana.moe>
2023-05-09 03:32:19 +0800

    gabutdm: init at 2.1.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1496,6 +1496,8 @@ with pkgs;
 
   copier = callPackage ../tools/misc/copier { };
 
+  gabutdm = callPackage ../applications/networking/gabutdm { };
+
   gamemode = callPackage ../tools/games/gamemode {
     libgamemode32 = pkgsi686Linux.gamemode.lib;
   };
