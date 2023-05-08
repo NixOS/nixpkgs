9b9eaf0208516f4f72ad64b2c6d8e672ccf480d1
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-05-09 00:54:45 +0300

    insync: superceded by insync-v3

--- pkgs/applications/networking/insync/default.nix
+++ pkgs/applications/networking/insync/default.nix
@@ -1,34 +1,73 @@
-{ lib, stdenv, fetchurl, makeWrapper, autoPatchelfHook }:
+{ stdenv
+, lib
+, fetchurl
+, makeWrapper
+, dpkg
+, libxcb
+, libGL
+, nss
+, libthai
+, wayland
+, alsa-lib
+, qtvirtualkeyboard
+, qtwebchannel
+, qtwebsockets
+, qtlocation
+, qtwebengine
+, autoPatchelfHook
+, wrapQtAppsHook
+}:
 
 stdenv.mkDerivation rec {
   pname = "insync";
-  version = "1.5.7.37371";
-  src =
-    if stdenv.hostPlatform.system == "x86_64-linux" then
-      fetchurl {
-        url = "http://s.insynchq.com/builds/insync-portable_${version}_amd64.tar.bz2";
-        sha256 = "1cm3q6y2crw6pcsvh21sbkmh1hin7xl4fyslc96nbyql8rxsky5n";
-      }
-    else
-      throw "${pname}-${version} is not supported on ${stdenv.hostPlatform.system}";
+  version = "3.3.5.40925";
 
-  nativeBuildInputs = [ makeWrapper autoPatchelfHook ];
+  src = fetchurl {
+    url = "http://s.insynchq.com/builds/${pname}_${version}-focal_amd64.deb";
+    sha256 = "sha256-lYlG/8d7teX98F5eDxm4EdBfFs7Sz3Td4kKLC6KZqnQ=";
+  };
 
   postPatch = ''
-    patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" client/insync-portable
+    substituteInPlace usr/bin/insync --replace /usr/lib/insync $out/usr/lib/insync
+  '';
+
+  buildInputs = [
+    alsa-lib
+    libGL
+    libthai
+    libxcb
+    nss
+    qtlocation
+    qtvirtualkeyboard
+    qtwebchannel
+    qtwebengine
+    qtwebsockets
+    wayland
+  ];
+
+  nativeBuildInputs = [ autoPatchelfHook dpkg makeWrapper wrapQtAppsHook ];
+
+  unpackPhase = ''
+    dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner
   '';
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp -a client $out/client
-    makeWrapper $out/client/insync-portable $out/bin/insync --set LC_TIME C
+    mkdir -p $out/bin $out/lib $out/share
+    cp -R usr/* $out/
+    rm $out/lib/insync/libGLX.so.0
+    rm $out/lib/insync/libQt5*
+    sed -i 's|/usr/lib/insync|/lib/insync|' "$out/bin/insync"
+    wrapQtApp "$out/lib/insync/insync"
   '';
 
-  meta = {
+  dontConfigure = true;
+  dontBuild = true;
+
+  meta = with lib; {
     platforms = ["x86_64-linux"];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
-    license = lib.licenses.unfree;
-    maintainers = [ ];
+    license = licenses.unfree;
+    maintainers = with maintainers; [ ];
     homepage = "https://www.insynchq.com";
     description = "Google Drive sync and backup with multiple account support";
     longDescription = ''
