0c26ebe5e204966cd343298f678414a4225bd786
Jonathan Zielinski <jozi@dbc.dk>
2023-05-08 16:30:58 +0200

    nixos/devpi-server: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1253,6 +1253,7 @@
   ./services/web-servers/apache-httpd/default.nix
   ./services/web-servers/caddy/default.nix
   ./services/web-servers/darkhttpd.nix
+  ./services/web-servers/devpi-server.nix
   ./services/web-servers/fcgiwrap.nix
   ./services/web-servers/garage.nix
   ./services/web-servers/hitch/default.nix
