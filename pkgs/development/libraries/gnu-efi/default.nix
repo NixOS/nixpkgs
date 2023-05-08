6efc3981aab2b94545beef6e17a467bedfe748cf
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-08 01:36:04 +0000

    gnu-efi: 3.0.15 -> 3.0.17

--- pkgs/development/libraries/gnu-efi/default.nix
+++ pkgs/development/libraries/gnu-efi/default.nix
@@ -3,11 +3,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnu-efi";
-  version = "3.0.15";
+  version = "3.0.17";
 
   src = fetchurl {
     url = "mirror://sourceforge/gnu-efi/${pname}-${version}.tar.bz2";
-    sha256 = "sha256-kxole5xcG6Zf9Rnxg3PEOKJoJfLbeGaxY+ltGxaPIOo=";
+    sha256 = "sha256-eAfpAzSTQ6ehQuu5NHA6KHIjXolojPWGwDKwoQh7yvQ=";
   };
 
   buildInputs = [ pciutils ];
