1f008595bacd4310d06c82a2ddb180c465aa966d
Matthieu Coudron <mcoudron@hotmail.com>
2023-05-09 02:13:33 +0200

    memento: init at v1.1.0
    a reader with kanji reading
    
    There is an optional manga-ocr dependency that I would like to support
    but in a second time see https://github.com/ripose-jp/Memento/issues/159

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32402,6 +32402,8 @@ with pkgs;
 
   mailspring = callPackage ../applications/networking/mailreaders/mailspring { };
 
+  memento = libsForQt5.callPackage ../applications/video/memento { };
+
   mm = callPackage ../applications/networking/instant-messengers/mm { };
 
   mm-common = callPackage ../development/libraries/mm-common { };
