a9bdbcbad0445987b9de38583293fb33b0e080b4
natsukium <tomoya.otabi@gmail.com>
2023-05-09 22:53:21 +0900

    python3Packages.langchain: 0.0.161 -> 0.0.162
    https://github.com/hwchase17/langchain/releases/tag/v0.0.162

--- pkgs/development/python-modules/langchain/default.nix
+++ pkgs/development/python-modules/langchain/default.nix
@@ -62,7 +62,7 @@
 
 buildPythonPackage rec {
   pname = "langchain";
-  version = "0.0.161";
+  version = "0.0.162";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -71,7 +71,7 @@ buildPythonPackage rec {
     owner = "hwchase17";
     repo = "langchain";
     rev = "refs/tags/v${version}";
-    hash = "sha256-zbos4ONNOs74OqTBnOnvqEsuDjTqDxCheylDggSVJt8=";
+    hash = "sha256-K+KogFVv217SSGBMg3adHt9O2Henv97gq3CdsAEt2vM=";
   };
 
   postPatch = ''
