6dee6324c73b6b4004ac4a766a2a91768d95c4cb
s1341 <github@shmarya.net>
2023-05-09 17:50:58 +0300

    keycloak.scim-keycloak-user-storage-api: unstable-20230103 -> unstable-20230412

--- pkgs/servers/keycloak/scim-keycloak-user-storage-spi/default.nix
+++ pkgs/servers/keycloak/scim-keycloak-user-storage-spi/default.nix
@@ -7,13 +7,13 @@
 
 javaPackages.mavenfod rec {
   pname = "scim-keycloak-user-storage-spi";
-  version = "unstable-2023-01-03";
+  version = "unstable-2023-04-12";
 
   src = fetchFromGitHub {
     owner = "justin-stephenson";
     repo = "scim-keycloak-user-storage-spi";
-    rev = "1be97049edf096ca0d9a78d988623d5d3f310fb1";
-    hash = "sha256-aGHInyy+VgyfjrXeZ6T6jfI00xaCwrRTehnew+mWYnQ=";
+    rev = "e2a78d9dddbb21a42a9aaeb5c72b5ed1ef76d2a0";
+    hash = "sha256-xFEXMZw575hONMG9ZNQ+5xKEVeKvGyzurqbAW48Mrg8=";
   };
 
   mvnHash = "sha256-CK42d+Ta1/XNQWCLaje6sI+C90YvzUcteuasVkUPfCk=";
