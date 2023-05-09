7203705c234b260fdfcf7f7f87b90fd140a330ff
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-10 01:45:54 +0200

    python310Packages.installer: bootstrap package

--- pkgs/development/python-modules/installer/default.nix
+++ pkgs/development/python-modules/installer/default.nix
@@ -1,36 +1,64 @@
 { lib
 , buildPythonPackage
-, pythonOlder
 , fetchFromGitHub
-, pytestCheckHook
+
+# bootstrap
 , flit-core
+, python
+
+# tests
+, installer
 , mock
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "installer";
   version = "0.7.0";
-  format = "pyproject";
+  format = "other";
 
   src = fetchFromGitHub {
     owner = "pradyunsg";
-    repo = pname;
-    rev = version;
+    repo = "installer";
+    rev = "refs/tags/${version}";
     hash = "sha256-thHghU+1Alpay5r9Dc3v7ATRFfYKV8l9qR0nbGOOX/A=";
   };
 
-  nativeBuildInputs = [ flit-core ];
+  nativeBuildInputs = [
+    flit-core
+  ];
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+    ls -lah
+    mkdir -p $out/${python.sitePackages}
+    cp -a src/installer* $out/${python.sitePackages}
+    ${python.interpreter} -m compileall $out/${python.sitePackages}
+    runHook postInstall
+  '';
+
+  pythonImportsCheck = [
+    "installer"
+  ];
+
+  doCheck = false;
 
   nativeCheckInputs = [
-    pytestCheckHook
     mock
+    pytestCheckHook
   ];
+
+  passthru.tests = {
+    pytest = installer.overridePythonAttrs (oldAttrs: { doCheck = true; });
+  };
 
   meta = with lib; {
     changelog = "https://github.com/pypa/installer/blob/${src.rev}/docs/changelog.md";
     homepage = "https://github.com/pradyunsg/installer";
     description = "A low-level library for installing a Python package from a wheel distribution";
     license = licenses.mit;
-    maintainers = with maintainers; [ cpcloud fridh ];
+    maintainers = teams.python.members;
   };
 }
