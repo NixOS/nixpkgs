f3166d213c7390c94aedffb3639463cbd442eacd
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-10 03:27:07 +0200

    python310Packages.click: fix compat with pytest>=7.3.0

--- pkgs/development/python-modules/click/default.nix
+++ pkgs/development/python-modules/click/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
+, fetchpatch
 , importlib-metadata
 , pytestCheckHook
 
@@ -22,6 +23,15 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-doLcivswKXABZ0V16gDRgU2AjWo2r0Fagr1IHTe6e44=";
   };
+
+  patches = [
+    (fetchpatch {
+      # pytest 7.3.0 compat
+      name = "click-pytest-7.3.0-compat.patch";
+      url = "https://github.com/pallets/click/commit/0f5fbb7e4d893945b4c07cfe4f81cd82fb2900ed.patch";
+      hash = "sha256-Xgkrn81K9Qzz1T3lfGHCm6rIl/omG+DhmCa6jj7cauw=";
+    })
+  ];
 
   propagatedBuildInputs = lib.optionals (pythonOlder "3.8") [
     importlib-metadata
