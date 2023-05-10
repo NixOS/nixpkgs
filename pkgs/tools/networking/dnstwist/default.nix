20147f981da647f59eda3ca9ce3bb7253cfa32c8
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-10 07:34:17 +0200

    dnstwist: 20230413 -> 20230509
    Changelog: https://github.com/elceef/dnstwist/releases/tag/20230509

--- pkgs/tools/networking/dnstwist/default.nix
+++ pkgs/tools/networking/dnstwist/default.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "dnstwist";
-  version = "20230413";
+  version = "20230509";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "elceef";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-YYERI+GNRP2wGWhYAy6m8QI4alFZniCSWp9/fdb9mpM=";
+    hash = "sha256-dJ/LI5mQJAYsqlOk2vP6h8Iz6ZdqTi9i4fgtjVaBkuE=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
