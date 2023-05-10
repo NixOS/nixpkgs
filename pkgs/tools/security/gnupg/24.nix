8a74c9bd31e17e5e817f4757deabc94e6bd5f6be
David McFarland <corngood@gmail.com>
2023-05-10 13:27:12 -0300

    gnupg: remove systemd user config
    This was removed upstream in:
    
    https://github.com/gpg/gnupg/commit/eae28f1bd4a5632e8f8e85b7248d1c4d4a10a5ed

--- pkgs/tools/security/gnupg/24.nix
+++ pkgs/tools/security/gnupg/24.nix
@@ -63,12 +63,6 @@ stdenv.mkDerivation rec {
       rm $f
     done
   '' else ''
-    mkdir -p $out/lib/systemd/user
-    for f in doc/examples/systemd-user/*.{service,socket} ; do
-      substitute $f $out/lib/systemd/user/$(basename $f) \
-        --replace /usr/bin $out/bin
-    done
-
     # add gpg2 symlink to make sure git does not break when signing commits
     ln -s $out/bin/gpg $out/bin/gpg2
 
