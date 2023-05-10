457acb926502628f935e22faee72bce497c2f37b
Robert Helgesson <robert@rycee.net>
2023-05-10 22:09:22 +0200

    libcpr: init at 1.10.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21404,6 +21404,8 @@ with pkgs;
 
   libcollectdclient = callPackage ../development/libraries/libcollectdclient { };
 
+  libcpr = callPackage ../development/libraries/libcpr { };
+
   libcredis = callPackage ../development/libraries/libcredis { };
 
   libctb = callPackage ../development/libraries/libctb { };
