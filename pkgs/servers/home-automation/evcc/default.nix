e63e1e698ebba1a5e0d27d89a7729c76deacdd50
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-10 22:07:23 +0200

    evcc: 0.117.0 -> 0.117.1
    https://github.com/evcc-io/evcc/releases/tag/0.117.1

--- pkgs/servers/home-automation/evcc/default.nix
+++ pkgs/servers/home-automation/evcc/default.nix
@@ -16,16 +16,16 @@
 
 buildGoModule rec {
   pname = "evcc";
-  version = "0.117.0";
+  version = "0.117.1";
 
   src = fetchFromGitHub {
     owner = "evcc-io";
     repo = pname;
     rev = version;
-    hash = "sha256-tvZh6yBzasR4GMEfzLQlvfe8UDpS4rBmm5Ww6GX//xI=";
+    hash = "sha256-y8hc/5z92mkIio8+UFan2PDLPyewty5GuFyIKO2IEo8=";
   };
 
-  vendorHash = "sha256-+KygZ2aa6JB8uZLrDjoDhxvLHstMmfaV+loVFtEG3Xo=";
+  vendorHash = "sha256-3EHdjRCzrty7BnaSG4TAf52jRl0AVS6baSl2XhYUH0A=";
 
   npmDeps = fetchNpmDeps {
     inherit src;
