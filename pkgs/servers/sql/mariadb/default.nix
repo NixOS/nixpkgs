d46e3b6a84a5d6c0ed73c715a0cf97672aea5cb6
ajs124 <git@ajs124.de>
2023-05-10 18:11:53 +0200

    mariadb_1011: 10.11.2 -> 10.11.3
    https://mariadb.com/kb/en/mariadb-10-11-3-release-notes/

--- pkgs/servers/sql/mariadb/default.nix
+++ pkgs/servers/sql/mariadb/default.nix
@@ -270,9 +270,9 @@ in
       inherit (self.darwin.apple_sdk.frameworks) CoreServices;
     };
     mariadb_1011 = self.callPackage generic {
-      # Supported until 2028-02-16
-      version = "10.11.2";
-      hash = "sha256-HIne4MrtD2i8Kh0gPrmKEjFQ5qF59u4PH8C6Pwjccdw=";
+      # Supported until 2028-02-16. TODO: make this the default, at some point
+      version = "10.11.3";
+      hash = "sha256-sGWw8ypun9R55Wb9ZnFFA3mIbY3aLZp++TCvHlwmwMc=";
       inherit (self.darwin) cctools;
       inherit (self.darwin.apple_sdk.frameworks) CoreServices;
     };
