537d611a75e375716a5a0e75ad50190f082da743
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-10 23:04:17 +0200

    nixos/sshd: Remove algorithms that do MAC-then-encrypt
    Algorithms with the -etm suffix calculate the MAC after encryption,
    which is generally considered safer.

--- nixos/modules/services/networking/ssh/sshd.nix
+++ nixos/modules/services/networking/ssh/sshd.nix
@@ -365,9 +365,6 @@ in
                 "hmac-sha2-512-etm@openssh.com"
                 "hmac-sha2-256-etm@openssh.com"
                 "umac-128-etm@openssh.com"
-                "hmac-sha2-512"
-                "hmac-sha2-256"
-                "umac-128@openssh.com"
               ];
               description = lib.mdDoc ''
                 Allowed MACs
