8d311b355dfb1ee80cdb926ca8b1c837a145edcc
Jack Leightcap <jack@leightcap.com>
2023-05-10 16:02:35 -0400

    python310Packages.crc: init at 4.2.0
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2099,6 +2099,8 @@ self: super: with self; {
 
   crayons = callPackage ../development/python-modules/crayons { };
 
+  crc = callPackage ../development/python-modules/crc { };
+
   crc16 = callPackage ../development/python-modules/crc16 { };
 
   crc32c = callPackage ../development/python-modules/crc32c { };
