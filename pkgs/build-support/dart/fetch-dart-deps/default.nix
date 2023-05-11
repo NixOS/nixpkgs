fdbb342402218c57121bbe3911c520f93738730a
hacker1024 <hacker1024@users.sourceforge.net>
2023-05-12 02:43:44 +1000

    dart: Properly set the dependency list derivation name

--- pkgs/build-support/dart/fetch-dart-deps/default.nix
+++ pkgs/build-support/dart/fetch-dart-deps/default.nix
@@ -169,7 +169,7 @@ let
       dart pub deps --json | jq .packages > $out
       runHook postBuild
     '';
-  } // buildDrvInheritArgs);
+  } // (removeAttrs buildDrvInheritArgs [ "name" "pname" ]));
 
   # As of Dart 3.0.0, Pub checks the revision of cached Git-sourced packages.
   # Git must be wrapped to return a positive result, as the real .git directory is wiped
