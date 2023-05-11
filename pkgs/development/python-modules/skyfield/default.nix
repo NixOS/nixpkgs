75acbc190e872cd72e7f8e785ca1d39d2c0aa1f0
Zane van Iperen <zane@zanevaniperen.com>
2023-05-11 21:07:05 +1000

    python3Packages.skyfield: work-around broken tests on aarch64

--- pkgs/development/python-modules/skyfield/default.nix
+++ pkgs/development/python-modules/skyfield/default.nix
@@ -13,6 +13,13 @@ buildPythonPackage rec {
     hash = "sha256-kZrXNVE+JGPGiVsd6CTwOqfciYLsD2A4pTS3FpqO+Dk=";
   };
 
+  # Fix broken tests on "exotic" platforms.
+  # https://github.com/skyfielders/python-skyfield/issues/582#issuecomment-822033858
+  postPatch = ''
+    substituteInPlace skyfield/tests/test_planetarylib.py \
+      --replace "if IS_32_BIT" "if True"
+  '';
+
   propagatedBuildInputs = [ certifi numpy sgp4 jplephem ];
 
   nativeCheckInputs = [ pandas ipython matplotlib assay ];
