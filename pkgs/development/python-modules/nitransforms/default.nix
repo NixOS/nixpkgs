07978448b20865d53888a5e1d874c72a64d3a038
Ben Darwin <bcdarwin@gmail.com>
2023-05-10 22:37:54 -0400

    python310Packages.nitransforms: unbreak

--- pkgs/development/python-modules/nitransforms/default.nix
+++ pkgs/development/python-modules/nitransforms/default.nix
@@ -2,9 +2,11 @@
 , buildPythonPackage
 , fetchPypi
 , pythonOlder
+, pythonRelaxDepsHook
 , h5py
 , nibabel
 , numpy
+, scipy
 , setuptools-scm
 , toml
 }:
@@ -19,8 +21,11 @@ buildPythonPackage rec {
     hash = "sha256-iV9TEIGogIfbj+fmOGftoQqEdtZiewbHEw3hYlMEP4c=";
   };
 
+  nativeBuildInputs = [ pythonRelaxDepsHook ];
   buildInputs = [ setuptools-scm toml ];
-  propagatedBuildInputs = [ h5py nibabel numpy ];
+  propagatedBuildInputs = [ h5py nibabel numpy scipy ];
+
+  pythonRelaxDeps = [ "scipy" ];
 
   doCheck = false;
   # relies on data repo (https://github.com/nipreps-data/nitransforms-tests);
