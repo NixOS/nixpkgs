8a6c351d3c4ca69d9815ffa1b751ea2d07b8cfb8
Reynald Affeldt <reynald.affeldt@aist.go.jp>
2023-05-11 22:00:54 +0900

    sort coq packages list

--- pkgs/top-level/coq-packages.nix
+++ pkgs/top-level/coq-packages.nix
@@ -82,11 +82,11 @@ let
       mathcomp-bigenough = callPackage ../development/coq-modules/mathcomp-bigenough {};
       mathcomp-classical = self.mathcomp-analysis.classical;
       mathcomp-finmap = callPackage ../development/coq-modules/mathcomp-finmap {};
+      mathcomp-infotheo = callPackage ../development/coq-modules/mathcomp-infotheo {};
       mathcomp-real-closed = callPackage ../development/coq-modules/mathcomp-real-closed {};
       mathcomp-tarjan = callPackage ../development/coq-modules/mathcomp-tarjan {};
       mathcomp-word = callPackage ../development/coq-modules/mathcomp-word {};
       mathcomp-zify = callPackage ../development/coq-modules/mathcomp-zify {};
-      mathcomp-infotheo = callPackage ../development/coq-modules/mathcomp-infotheo {};
       metacoq = callPackage ../development/coq-modules/metacoq { };
       metacoq-template-coq = self.metacoq.template-coq;
       metacoq-pcuic        = self.metacoq.pcuic;
