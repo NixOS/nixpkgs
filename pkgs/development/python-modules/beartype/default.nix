99b071ad0e1df99b19038772d3cd87cb196396d4
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-11 05:46:17 +0200

    python311Packages.beartype: update disabled
    - add changelog to meta

--- pkgs/development/python-modules/beartype/default.nix
+++ pkgs/development/python-modules/beartype/default.nix
@@ -10,7 +10,7 @@ buildPythonPackage rec {
   version = "0.14.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
@@ -28,6 +28,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Fast runtime type checking for Python";
     homepage = "https://github.com/beartype/beartype";
+    changelog = "https://github.com/beartype/beartype/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ bcdarwin ];
   };
