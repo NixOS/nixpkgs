d05b99995b6a8bec3f684516c20c49ddd88c3c62
Vladyslav M <dywedir@gra.red>
2023-05-12 21:41:29 +0300

    synapse-bt: 1.0 -> unstable-2023-02-16, build with openssl 3.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32766,7 +32766,6 @@ with pkgs;
 
   synapse-bt = callPackage ../applications/networking/p2p/synapse-bt {
     inherit (darwin.apple_sdk.frameworks) CoreServices Security;
-    openssl = openssl_1_1;
   };
 
   synfigstudio = callPackage ../applications/graphics/synfigstudio { };
