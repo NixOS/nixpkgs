e7d529218799360bdca1f8d4484865e33f66cecc
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-12 22:16:21 +0200

    odp-dpdk: Build against openssl3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27199,9 +27199,7 @@ with pkgs;
 
   odin = callPackage ../development/compilers/odin { };
 
-  odp-dpdk = callPackage ../os-specific/linux/odp-dpdk {
-    openssl = openssl_1_1;
-  };
+  odp-dpdk = callPackage ../os-specific/linux/odp-dpdk { };
 
   odroid-xu3-bootloader = callPackage ../tools/misc/odroid-xu3-bootloader { };
 
