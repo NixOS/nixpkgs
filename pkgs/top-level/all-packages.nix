1640e4aea2b97c765d3d1eb7ba621fbd9d147c13
figsoda <figsoda@pm.me>
2023-05-12 13:41:38 -0400

    dogdns: update Cargo.lock and switch to openssl 3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6867,7 +6867,6 @@ with pkgs;
   dog = callPackage ../tools/system/dog { };
 
   dogdns = callPackage ../tools/networking/dogdns {
-    openssl = openssl_1_1;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
