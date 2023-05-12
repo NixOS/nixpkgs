f0f34d3fe5c43addf49caf44415553ae6453a5b7
Anish Lakhwara <anish+github@lakhwara.com>
2023-05-12 20:27:48 +1000

    Fix: FreshRSS waits until the database has started to update
    Ensure that the database has started before calling the systemd unit that updates freshRSS so it doesn't fail on updating databases.

--- nixos/modules/services/web-apps/freshrss.nix
+++ nixos/modules/services/web-apps/freshrss.nix
@@ -3,7 +3,9 @@
 with lib;
 let
   cfg = config.services.freshrss;
-
+  useMysql = cfg.database.type == "mysql";
+  usePostgresql = cfg.database.type == "postgres";
+  useSqlite = cfg.database.type == "sqlite";
   poolName = "freshrss";
 in
 {
@@ -273,7 +275,7 @@ in
 
       systemd.services.freshrss-updater = {
         description = "FreshRSS feed updater";
-        after = [ "freshrss-config.service" ];
+        after = [ "freshrss-config.service" ] ++ lib.optional usePostgresql "postgresql.service" ++ lib.optional useMysql "mysql.service";;
         wantedBy = [ "multi-user.target" ];
         startAt = "*:0/5";
         environment = {
