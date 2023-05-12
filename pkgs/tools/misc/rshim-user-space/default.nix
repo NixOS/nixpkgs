5435eaaa4d34d941c9493da7a3918d4925162469
nikstur <nikstur@outlook.com>
2023-05-12 14:45:27 +0200

    nixos/rshim: init

--- pkgs/tools/misc/rshim-user-space/default.nix
+++ pkgs/tools/misc/rshim-user-space/default.nix
@@ -1,7 +1,6 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, autoreconfHook
 , autoconf
 , automake
 , pkg-config
@@ -22,7 +21,6 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [
-    autoreconfHook
     autoconf
     automake
     pkg-config
