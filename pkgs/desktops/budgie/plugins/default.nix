813b23d9f155a11f37edf006b741b0e837f156c4
Federico Damián Schonborn <fdschonborn@gmail.com>
2023-05-11 21:08:03 -0300

    budgiePlugins.budgie-user-indicator-redux: init at 1.0.1
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- pkgs/desktops/budgie/plugins/default.nix
+++ pkgs/desktops/budgie/plugins/default.nix
@@ -1,3 +1,4 @@
 { callPackage }: {
   budgie-analogue-clock-applet = callPackage ./budgie-analogue-clock-applet { };
+  budgie-user-indicator-redux = callPackage ./budgie-user-indicator-redux { };
 }
