f4fb689471ce598ba908837e437f263f414be9bf
Bart Brouns <bart@magnetophon.nl>
2023-05-13 23:21:56 +0200

    faustPhysicalModeling: 2.54.9 -> 2.59.6

--- pkgs/applications/audio/faustPhysicalModeling/default.nix
+++ pkgs/applications/audio/faustPhysicalModeling/default.nix
@@ -1,13 +1,13 @@
 { stdenv, lib, fetchFromGitHub, faust2jaqt, faust2lv2 }:
 stdenv.mkDerivation rec {
   pname = "faustPhysicalModeling";
-  version = "2.54.9";
+  version = "2.59.6";
 
   src = fetchFromGitHub {
     owner = "grame-cncm";
     repo = "faust";
     rev = version;
-    sha256 = "sha256-1ZS7SVTWI1vNOGycZIDyKLgwfNooIGDa8Wmr6qfFSkU=";
+    sha256 = "sha256-Z/hAq6JlhlWBzWlodwQW/k9AkozVeMXmbVhkicNZ5os=";
   };
 
   buildInputs = [ faust2jaqt faust2lv2 ];
