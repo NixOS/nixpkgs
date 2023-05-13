2e121a25dcae0e364150f73659b6a3eeaee831a1
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-13 09:31:25 +0200

    openapi-generator-cli:  add changelog to meta

--- pkgs/tools/networking/openapi-generator-cli/default.nix
+++ pkgs/tools/networking/openapi-generator-cli/default.nix
@@ -31,9 +31,10 @@ let this = stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Allows generation of API client libraries (SDK generation), server stubs and documentation automatically given an OpenAPI Spec";
     homepage = "https://github.com/OpenAPITools/openapi-generator";
+    changelog = "https://github.com/OpenAPITools/openapi-generator/releases/tag/v${version}";
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     license = licenses.asl20;
-    maintainers = [ maintainers.shou ];
+    maintainers = with maintainers; [ shou ];
   };
 
   passthru.tests.example = callPackage ./example.nix {
