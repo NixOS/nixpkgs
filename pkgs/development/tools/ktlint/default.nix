b4ff6d58ee4f2d67742ec46f62de1ff16786a1c0
SubhrajyotiSen <subhrajyoti12@gmail.com>
2023-05-13 12:38:05 +0530

    ktlint: 0.49.0 -> 0.49.1

--- pkgs/development/tools/ktlint/default.nix
+++ pkgs/development/tools/ktlint/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ktlint";
-  version = "0.49.0";
+  version = "0.49.1";
 
   src = fetchurl {
     url = "https://github.com/pinterest/ktlint/releases/download/${version}/ktlint";
-    sha256 = "1vm064b591lp5yygryz0p0zdfwlp1nhl5dv2nzx0y92j3911q0yz";
+    sha256 = "1k2byxqvgr2xll4jj0ck8w3qdgkvjhwkag18inxjakcl99knygrb";
   };
 
   nativeBuildInputs = [ makeWrapper ];
