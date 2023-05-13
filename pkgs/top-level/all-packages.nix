9818d120be614c478f569c21d4b9d358632e651e
Sergei Trofimovich <slyich@gmail.com>
2023-05-13 17:20:48 +0100

    libgcc: remove unused package
    `libgcc` is not usable as is and as a result no packages use it today.
    `gcc.cc.libgcc` should provide the same library. But ideally it should
    not be used at all as it's an implementation detail of `gcc` support
    libraries.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18018,10 +18018,6 @@ with pkgs;
 
   librarian-puppet-go = callPackage ../development/tools/librarian-puppet-go { };
 
-  libgcc = callPackage ../development/libraries/gcc/libgcc {
-    stdenvNoLibs = gccStdenvNoLibs; # cannot be built with clang it seems
-  };
-
   # This is for e.g. LLVM libraries on linux.
   gccForLibs =
     if stdenv.targetPlatform == stdenv.hostPlatform && targetPackages.stdenv.cc.isGNU
