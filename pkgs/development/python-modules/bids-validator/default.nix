b40d3f1e2e7d2107cfe7a128e7ddde9c1cf7f95b
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-13 09:53:26 +0200

    python311Packages.bids-validator: add changelog to meta

--- pkgs/development/python-modules/bids-validator/default.nix
+++ pkgs/development/python-modules/bids-validator/default.nix
@@ -1,11 +1,11 @@
-{ buildPythonPackage
-, lib
+{ lib
+, buildPythonPackage
 , fetchPypi
 }:
 
 buildPythonPackage rec {
-  version = "1.11.0";
   pname = "bids-validator";
+  version = "1.11.0";
 
   src = fetchPypi {
     inherit pname version;
@@ -15,11 +15,14 @@ buildPythonPackage rec {
   # needs packages which are not available in nixpkgs
   doCheck = false;
 
-  pythonImportsCheck = [ "bids_validator" ];
+  pythonImportsCheck = [
+    "bids_validator"
+  ];
 
   meta = with lib; {
     description = "Validator for the Brain Imaging Data Structure";
     homepage = "https://github.com/bids-standard/bids-validator";
+    changelog = "https://github.com/bids-standard/bids-validator/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ jonringer ];
   };
