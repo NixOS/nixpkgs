179ba02c2b93e6b9dc04c52234d0e428da4a3078
figsoda <figsoda@pm.me>
2023-05-14 16:45:41 -0400

    nerdfix: 0.3.0 -> 0.3.1
    Diff: https://github.com/loichyan/nerdfix/compare/v0.3.0...v0.3.1
    
    Changelog: https://github.com/loichyan/nerdfix/blob/v0.3.1/CHANGELOG.md

--- pkgs/tools/text/nerdfix/default.nix
+++ pkgs/tools/text/nerdfix/default.nix
@@ -1,30 +1,20 @@
 { lib
 , rustPlatform
 , fetchFromGitHub
-, fetchpatch
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "nerdfix";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "loichyan";
     repo = "nerdfix";
     rev = "v${version}";
-    hash = "sha256-71P0ESPLTUq8z0mSU4v8KmS069DNUi5fPHz01Kg3aKg=";
+    hash = "sha256-cqTaup/MrtLcBIoY+1vQLLlU+Cmu3iODH4jmZImjGrg=";
   };
 
-  cargoHash = "sha256-XAelHpTVvz+jsDzfB+jsEuUdB0hN7c+hVDvCyOixx9E=";
-
-  patches = [
-    # fixes failing tests due to outdated snapshots
-    (fetchpatch {
-      name = "test-cli-udpate-stdout.patch";
-      url = "https://github.com/loichyan/nerdfix/commit/4070f9e894337ca7d3f7641258428ad6d7cd6332.patch";
-      hash = "sha256-oDuHKgoMcOaO1mtBbT1Uwn5ZUp/FvqsD4S+A1LdOhcE=";
-    })
-  ];
+  cargoHash = "sha256-V/M70ARqOyN0f/uudWPHc4bGc3WXK3PpcM8r2MBEWAs=";
 
   meta = with lib; {
     description = "Nerdfix helps you to find/fix obsolete nerd font icons in your project";
