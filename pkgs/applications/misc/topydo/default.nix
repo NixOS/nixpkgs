5fc7480c9424362651eb089b0a8ebddbe122364e
figsoda <figsoda@pm.me>
2023-05-14 16:56:59 -0400

    topydo: apply patch to fix test

--- pkgs/applications/misc/topydo/default.nix
+++ pkgs/applications/misc/topydo/default.nix
@@ -1,4 +1,4 @@
-{ lib, python3Packages, fetchFromGitHub, glibcLocales }:
+{ lib, python3Packages, fetchFromGitHub, fetchpatch, glibcLocales }:
 
 with python3Packages;
 
@@ -12,6 +12,15 @@ buildPythonApplication rec {
     rev = version;
     sha256 = "1lpfdai0pf90ffrzgmmkadbd86rb7250i3mglpkc82aj6prjm6yb";
   };
+
+  patches = [
+    # fixes a failing test
+    (fetchpatch {
+      name = "update-a-test-reference-ics-file.patch";
+      url = "https://github.com/topydo/topydo/commit/9373bb4702b512b10f0357df3576c129901e3ac6.patch";
+      hash = "sha256-JpyQfryWSoJDdyzbrESWY+RmRbDw1myvTlsFK7+39iw=";
+    })
+  ];
 
   propagatedBuildInputs = [
     arrow
