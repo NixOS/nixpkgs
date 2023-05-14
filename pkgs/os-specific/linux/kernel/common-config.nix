NixOS/nixpkgs

commit f5252cb7e09c0f2dee36367c9e509f45d27b09f7

linux: fix 6.4-rc1 build

pkgs/os-specific/linux/kernel/common-config.nix
@@ -314,8 +314,8 @@ let
       DRM_AMD_DC_DCN2_0 = whenBetween "5.3" "5.6" yes;
       DRM_AMD_DC_DCN2_1 = whenBetween "5.4" "5.6" yes;
       DRM_AMD_DC_DCN3_0 = whenBetween "5.9" "5.11" yes;
-      DRM_AMD_DC_DCN = whenAtLeast "5.11" yes;
-      DRM_AMD_DC_HDCP = whenAtLeast "5.5" yes;
+      DRM_AMD_DC_DCN = whenBetween "5.11" "6.4" yes;
+      DRM_AMD_DC_HDCP = whenBetween "5.5" "6.4" yes;
       DRM_AMD_DC_SI = whenAtLeast "5.10" yes;
     } // optionalAttrs (stdenv.hostPlatform.system == "x86_64-linux") {
       # Intel GVT-g graphics virtualization supports 64-bit only

commit a4aa2c04761f5d6ec312c28f07474b5c9fd826c9

linux: enable DRM_AMD_DC_FP on 6.4
In 6.4, DRM_AMD_DC_DCN is renamed to DRM_AMD_DC_FP.

Fixes: f5252cb7e09c0f2dee36367c9e509f45d27b09f7

pkgs/os-specific/linux/kernel/common-config.nix
@@ -315,6 +315,7 @@ let
       DRM_AMD_DC_DCN2_1 = whenBetween "5.4" "5.6" yes;
       DRM_AMD_DC_DCN3_0 = whenBetween "5.9" "5.11" yes;
       DRM_AMD_DC_DCN = whenBetween "5.11" "6.4" yes;
+      DRM_AMD_DC_FP = whenAtLeast "6.4" yes;
       DRM_AMD_DC_HDCP = whenBetween "5.5" "6.4" yes;
       DRM_AMD_DC_SI = whenAtLeast "5.10" yes;
     } // optionalAttrs (stdenv.hostPlatform.system == "x86_64-linux") {