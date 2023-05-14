d69ac45fbf2e027cc7a252603e04d4ca924e0dfb
Jay Rovacsek <jay@rovacsek.com>
2023-05-15 08:57:48 +1000

    maintainers: add jayrovacsek

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7085,6 +7085,12 @@
     githubId = 5579359;
     name = "Jason Yundt";
   };
+  jayrovacsek = {
+    email = "nixpkgs@jay.rovacsek.com";
+    name = "Jay Rovacsek";
+    github = "JayRovacsek";
+    githubId = 29395089;
+  };
   jb55 = {
     email = "jb55@jb55.com";
     github = "jb55";
