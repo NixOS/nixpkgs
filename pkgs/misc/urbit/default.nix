cdf661e2e5a0c233493560ddc72ac3d1a2e2c564
Hugh O'Brien <git@hughobrien.ie>
2023-05-14 18:43:57 -0400

    urbit: 2.1 -> 2.3

--- pkgs/misc/urbit/default.nix
+++ pkgs/misc/urbit/default.nix
@@ -10,15 +10,15 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "urbit";
-  version = "2.3";
+  version = "2.5";
 
   src = fetchzip {
     url = "https://github.com/urbit/vere/releases/download/vere-v${version}/${platform}.tgz";
     sha256 = {
-      x86_64-linux = "sha256-7rPkEgJdCDkfz58VYOH2AH6s/048pySpmff0tfRkPqU=";
-      aarch64-linux = "sha256-8P0BNyaV+VxS2cl3ac2Ey7YC1b2A+DbfZZVpaI3agJw=";
-      x86_64-darwin = "sha256-EHYr3lG2WeHaFjO7CNcz5Ygv5MYzuFcCoX36hEXZVoo=";
-      aarch64-darwin = "sha256-lPuavLA73NtMC/yS/L1XwPljPnWw+9mcrw4RrqbVrnA=";
+      x86_64-linux = "sha256-1Qd+bDrmsEQTYaJt1Na4FLWwA+lNib8M5j9c9Mr7hlU=";
+      aarch64-linux = "sha256-kFCi5sbnlBRFsdqiqMJ3RaNg2PWEcU2D1JNbE5cNUrA=";
+      x86_64-darwin = "sha256-Cp3q1npTSQIZyedXitNt/djFk3Nu8KDM/qzfhl+3i/o=";
+      aarch64-darwin = "sha256-KhI0okRUEYa46z/prAIYxmj+BsjhExXm+2+rTnDQkg0=";
     }.${stdenv.hostPlatform.system} or (throw "unsupported system ${stdenv.hostPlatform.system}");
   };
 
