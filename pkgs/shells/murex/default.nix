29279840b820bb0a5e78f69cb42610c240f1c71b
Mostly Void <7rat13@gmail.com>
2023-05-14 17:02:56 +0530

    murex: 3.1.3100 -> 4.1.4200

--- pkgs/shells/murex/default.nix
+++ pkgs/shells/murex/default.nix
@@ -5,23 +5,32 @@
 
 buildGoModule rec {
   pname = "murex";
-  version = "3.1.3100";
+  version = "4.1.4200";
 
   src = fetchFromGitHub {
     owner = "lmorg";
-    repo = pname;
+    repo = "murex";
     rev = "v${version}";
-    sha256 = "sha256-haYS3M3Cg+RBGbH5Af/9qcTkKroqx76r1Fb6Bf08lgY=";
+    hash = "sha256-+oJN9yRxsj+zUimr9PRxVQkbvdINmG+mVjbgUp8g91o=";
   };
 
-  vendorHash = "sha256-GKgwll9Cl+FMYwn07F7d33VXl4a9lcC7muzNvRzmR4k=";
+  vendorHash = "sha256-gbcC+SZoqM3DEhPzI3GDAW0n69aApldsgI3+e81Zqdo=";
 
-  subPackages = [ "." ];
+  ldflags = [ "-s" "-w" ];
+
+  # tests are meant to be run as
+  # murex -c 'g: behavioural/*.mx -> foreach: f { source $f }; try {test: run *}'
+  # however some tests are failing
+  doCheck = false;
+
+  postInstall = ''
+    rm $out/bin/docgen $out/bin/server $out/bin/wasmserver
+  '';
 
   meta = with lib; {
     description = "Bash-like shell and scripting environment with advanced features designed for safety and productivity";
     homepage = "https://murex.rocks";
-    license = licenses.gpl2;
     maintainers = with maintainers; [ dit7ya ];
+    license = licenses.gpl2Only;
   };
 }
