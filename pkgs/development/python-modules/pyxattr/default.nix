4aeba8ddebcd3c4ec357f10823a1d1c6bb9164e7
Jay Rovacsek <jay@rovacsek.com>
2023-05-15 23:27:20 +1000

    pyxattr: enable build for darwin hosts

--- pkgs/development/python-modules/pyxattr/default.nix
+++ pkgs/development/python-modules/pyxattr/default.nix
@@ -1,25 +1,28 @@
-{ lib
-, pkgs
+{ buildPythonPackage
 , fetchPypi
-, buildPythonPackage
+, lib
+, pkgs
+, stdenv
 }:
 
 buildPythonPackage rec {
-    pname = "pyxattr";
-    version = "0.8.1";
+  pname = "pyxattr";
+  version = "0.8.1";
 
-    src = fetchPypi {
-      inherit pname version;
-      hash = "sha256-SMV47PjqC9Q1GxdSRw4wGpCjdhx8IfAPlT3PbW+m7lo=";
-    };
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-SMV47PjqC9Q1GxdSRw4wGpCjdhx8IfAPlT3PbW+m7lo=";
+  };
 
-    # IOError: [Errno 95] Operation not supported (expected)
-    doCheck = false;
+  # IOError: [Errno 95] Operation not supported (expected)
+  doCheck = false;
 
-    buildInputs = with pkgs; [ attr ];
+  hardeningDisable = lib.optionals stdenv.isDarwin [ "strictoverflow" ];
 
-    meta = with lib; {
-      description = "A Python extension module which gives access to the extended attributes for filesystem objects available in some operating systems";
-      license = licenses.lgpl21Plus;
-    };
+  buildInputs = [ ] ++ lib.optionals stdenv.isLinux (with pkgs; [ attr ]);
+
+  meta = with lib; {
+    description = "A Python extension module which gives access to the extended attributes for filesystem objects available in some operating systems";
+    license = licenses.lgpl21Plus;
+  };
 }
