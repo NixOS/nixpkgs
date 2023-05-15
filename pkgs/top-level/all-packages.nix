f9c503b9ef3c31fba015e960e8643092c7ab5a43
harvidsen <hakon.arvidsen@eviny.no>
2023-05-15 14:39:11 +0200

    Rename redpanda package to redpanda-rpk
    The existing package called redpanda only contains the cli tool rpk.
    
    Perhaps redpanda-rpk should be moved out of servers directory since
    it is only a tool for interacting with a redpanda server?
    
    I'm unable to build redpanda-server, but I guess it contains what
    we need for running redpanda server. Why is that included under the
    rpk package passthrough?

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11709,9 +11709,9 @@ with pkgs;
 
   redmine = callPackage ../applications/version-management/redmine { };
 
-  redpanda = callPackage ../servers/redpanda { };
+  redpanda-rpk = callPackage ../servers/redpanda-rpk { };
 
-  redpanda-server = redpanda.server;
+  redpanda-server = redpanda-rpk.server;
 
   redsocks = callPackage ../tools/networking/redsocks { };
 
