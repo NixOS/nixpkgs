87bdd8efbf0f75c5355f88ab366526c524316870
Doron Behar <doron.behar@gmail.com>
2023-05-15 15:21:23 +0300

    python311Packages.toptica-lasersdk: init at 3.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15790,6 +15790,8 @@ self: super: with self; {
 
   toposort = callPackage ../development/python-modules/toposort { };
 
+  toptica-lasersdk = callPackage ../development/python-modules/toptica-lasersdk { };
+
   torch = callPackage ../development/python-modules/torch { };
 
   # Required to test triton
