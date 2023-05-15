c93a171d074192576ee83f00c0e97de66ab27974
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2023-05-15 21:38:42 +0200

    lkrg: 0.9.5 -> 0.9.6

--- pkgs/os-specific/linux/lkrg/default.nix
+++ pkgs/os-specific/linux/lkrg/default.nix
@@ -5,21 +5,14 @@ in
 stdenv.mkDerivation rec {
   name = "${pname}-${version}-${kernel.version}";
   pname = "lkrg";
-  version = "0.9.5";
+  version = "0.9.6";
 
   src = fetchFromGitHub {
     owner = "lkrg-org";
     repo = "lkrg";
     rev = "v${version}";
-    sha256 = "sha256-+yIKkTvfVbLnFBoXSKGebB1A8KqpaRmsLh8SsNuI9Dc=";
+    sha256 = "sha256-jKiSTab05+6ZZXQDKUVPKGti0E4eZaVuMZJlBKR3zGY=";
   };
-  patches = [
-    (fetchpatch {
-      name = "fix-aarch64.patch";
-      url = "https://github.com/lkrg-org/lkrg/commit/a4e5c00f13f7081b346bc3736e4c035e3d17d3f7.patch";
-      sha256 = "sha256-DPscqi+DySHwFxGuGe7P2itPkoyb3XGu5Xp2S/ezP4Y=";
-    })
-  ];
 
   hardeningDisable = [ "pic" ];
 
@@ -48,6 +41,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2Only;
     maintainers = with maintainers; [ chivay ];
     platforms = platforms.linux;
-    broken = kernel.kernelOlder "5.10" || kernel.kernelAtLeast "6.1" || isKernelRT;
+    broken = kernel.kernelOlder "5.10" || kernel.kernelAtLeast "6.4" || isKernelRT;
   };
 }
