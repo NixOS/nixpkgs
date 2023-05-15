4ee0eef07b4c00f042a1a4ceac11469379b8ad23
Doron Behar <doron.behar@gmail.com>
2023-05-15 15:21:23 +0300

    python311Packages.toptica-lasersdk: init at 3.2.0

---
+++ pkgs/development/python-modules/toptica-lasersdk/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, setuptools
+, fetchPypi
+, ifaddr
+, pyserial
+}:
+
+buildPythonPackage rec {
+  pname = "toptica-lasersdk";
+  version = "3.2.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "toptica_lasersdk";
+    inherit version;
+    hash = "sha256-UNazng4Za3CZeG7eDq0b+l7gmESEXIU8WMLWGGysmBg=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    ifaddr
+    pyserial
+  ];
+
+  pythonImportsCheck = [
+    "toptica.lasersdk.dlcpro.v2_2_0"
+  ];
+
+  meta = with lib; {
+    description = "TOPTICA Python Laser SDK";
+    homepage = "https://toptica.github.io/python-lasersdk/";
+    license = licenses.mit;
+    maintainers = with maintainers; [ doronbehar ];
+  };
+}
