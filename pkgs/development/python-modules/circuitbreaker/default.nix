a9ab04830099c7abb9a3eefc1ff9b79984fb31ff
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-16 01:23:23 +0200

    python311Packages.circuitbreaker: 1.4.0 -> 2.0.0
    Changelog: https://github.com/fabfuel/circuitbreaker/releases/tag/2.0.0

--- pkgs/development/python-modules/circuitbreaker/default.nix
+++ pkgs/development/python-modules/circuitbreaker/default.nix
@@ -1,14 +1,14 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, mock
+, pytest-mock
 , pytestCheckHook
 , pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "circuitbreaker";
-  version = "1.4.0";
+  version = "2.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -17,11 +17,11 @@ buildPythonPackage rec {
     owner = "fabfuel";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-l0ASt9CQmgJmWpRrghElbff/gaNOmxNh+Wj0C0p4jE0=";
+    hash = "sha256-jaDCMGCZZu3STluYeHDNgdEPf2DNq7bXJ0LPV3JZdk0=";
   };
 
   nativeCheckInputs = [
-    mock
+    pytest-mock
     pytestCheckHook
   ];
 
@@ -32,6 +32,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python Circuit Breaker implementation";
     homepage = "https://github.com/fabfuel/circuitbreaker";
+    changelog = "https://github.com/fabfuel/circuitbreaker/releases/tag/${version}";
     license = with licenses; [ bsd3 ];
     maintainers = with maintainers; [ fab ];
   };
