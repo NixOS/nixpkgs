672fe6521a45a8e46f5126e1c85184d189663980
Daniel Poelzleithner <poelzleithner@b1-systems.de>
2023-05-15 16:10:24 +0200

    arduinoOTA: init at 1.4.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2923,6 +2923,8 @@ with pkgs;
 
   arduino-mk = callPackage ../development/embedded/arduino/arduino-mk { };
 
+  arduinoOTA = callPackage ../development/embedded/arduino/arduinoOTA { };
+
   apio = python3Packages.callPackage ../development/embedded/fpga/apio { };
 
   apitrace = libsForQt5.callPackage ../applications/graphics/apitrace { };
