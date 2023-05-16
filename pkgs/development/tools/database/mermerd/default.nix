b03f1ceff6ca1eff154f79134b123bc4315f772c
figsoda <figsoda@pm.me>
2023-05-15 22:02:20 -0400

    mermerd: fix version command

--- pkgs/development/tools/database/mermerd/default.nix
+++ pkgs/development/tools/database/mermerd/default.nix
@@ -1,6 +1,6 @@
-{ buildGoModule
+{ lib
+, buildGoModule
 , fetchFromGitHub
-, lib
 }:
 
 buildGoModule rec {
@@ -16,15 +16,21 @@ buildGoModule rec {
 
   vendorHash = "sha256-RSCpkQymvUvY2bOkjhsyKnDa3vezUjC33Nwv0+O4OOQ=";
 
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.version=${version}"
+    "-X=main.commit=${src.rev}"
+  ];
+
   # the tests expect a database to be running
   doCheck = false;
 
   meta = with lib; {
     description = "Create Mermaid-Js ERD diagrams from existing tables";
     homepage = "https://github.com/KarnerTh/mermerd";
-    license = licenses.mit;
-    maintainers = with lib.maintainers; [ austin-artificial ];
     changelog = "https://github.com/KarnerTh/mermerd/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ austin-artificial ];
   };
-
 }
