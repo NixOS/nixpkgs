eed97cb599c5ddf4c235e32d1d42e1b28e693ff3
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-16 11:33:44 +0200

    python311Packages.azure-mgmt-signalr: update disabled

--- pkgs/development/python-modules/azure-mgmt-signalr/default.nix
+++ pkgs/development/python-modules/azure-mgmt-signalr/default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   version = "1.2.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
