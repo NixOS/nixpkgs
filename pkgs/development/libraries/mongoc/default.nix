af692f81e88b08eee22067c62cb5957c044692e5
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-16 02:50:05 +0000

    mongoc: 1.23.3 -> 1.23.4

--- pkgs/development/libraries/mongoc/default.nix
+++ pkgs/development/libraries/mongoc/default.nix
@@ -17,11 +17,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "mongoc";
-  version = "1.23.3";
+  version = "1.23.4";
 
   src = fetchzip {
     url = "https://github.com/mongodb/mongo-c-driver/releases/download/${version}/mongo-c-driver-${version}.tar.gz";
-    sha256 = "sha256-wxcBnJENL3hMzf7GKLucjw7K08tK35+0sMNWZb2mWIo=";
+    sha256 = "sha256-mJBaRaX0hphvMdTII79DVn1g2uBmpRlObMiJ673Ijpw=";
   };
 
   # https://github.com/NixOS/nixpkgs/issues/25585
