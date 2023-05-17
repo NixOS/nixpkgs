78123f3ffcdf1e66cac0145e6564b2d5b2fa2d39
Isa <hi@f2k1.de>
2023-05-17 10:16:38 +0200

    vim: 9.0.1544 -> 9.0.1562

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "9.0.1544";
+  version = "9.0.1562";
 
   src = fetchFromGitHub {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    hash = "sha256-0YQp2ybNIdrDF9YxlUlBw6H9P6V7Iv12pox1O0lSXGA=";
+    hash = "sha256-+QKh3CxSjwcJ+Rj9RHYHRKSZixkfA1ZCAPDIyV/Npt8=";
   };
 
   enableParallelBuilding = true;
