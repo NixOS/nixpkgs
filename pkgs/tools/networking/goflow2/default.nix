2c9cee29214e28413e1d7b62e4733ff71f4588ec
Yureka <yuka@yuka.dev>
2023-05-17 18:56:26 +0200

    Update pkgs/tools/networking/goflow2/default.nix
    Co-authored-by: figsoda <figsoda@pm.me>

--- pkgs/tools/networking/goflow2/default.nix
+++ pkgs/tools/networking/goflow2/default.nix
@@ -14,6 +14,12 @@ buildGoModule rec {
     hash = "sha256-YZuF3O1/Ycn2gFK9i1D/W8F16B6NEift5PYbv8yqUHk=";
   };
 
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.version=${version}"
+  ];
+
   vendorHash = "sha256-c40N6SAym9dpCuGb5I7t9sJBde2r552obot3drYCjB4=";
 
   meta = with lib; {
