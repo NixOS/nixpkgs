ebbd3bdfa871f30fd943aa58f0d54b69f6a8112b
Yueh-Shun Li <shamrocklee@posteo.net>
2023-05-18 15:36:01 +0800

    maintainers.ShamrockLee: renew information
    Change the name to my legal name (Yueh-Shun Li).
    Add email address and Matrix ID.
    Sort attributes in alphabetical order.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14318,9 +14318,11 @@
     name = "Scott Hamilton";
   };
   ShamrockLee = {
-    name = "Shamrock Lee";
+    email = "shamrocklee@posteo.net";
     github = "ShamrockLee";
     githubId = 44064051;
+    matrix = "@shamrocklee:matrix.org";
+    name = "Yueh-Shun Li";
   };
   shanesveller = {
     email = "shane@sveller.dev";
