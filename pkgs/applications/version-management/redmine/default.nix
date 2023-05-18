6201512ce2326e3f606bbc9579be74111aaab2f3
Meghea Iulian <iulian.meghea@gmail.com>
2023-05-18 11:19:43 +0300

    redmine: 4.2.10 -> 5.0.5

--- pkgs/applications/version-management/redmine/default.nix
+++ pkgs/applications/version-management/redmine/default.nix
@@ -1,13 +1,13 @@
-{ lib, stdenv, fetchurl, bundlerEnv, ruby, makeWrapper, nixosTests }:
+{ lib, stdenv, fetchurl, bundlerEnv, ruby, defaultGemConfig, makeWrapper, nixosTests }:
 
 let
-  version = "4.2.10";
+  version = "5.0.5";
   rubyEnv = bundlerEnv {
     name = "redmine-env-${version}";
 
     inherit ruby;
     gemdir = ./.;
-    groups = [ "development" "ldap" "markdown" "minimagick" "openid" "test" ];
+    groups = [ "development" "ldap" "markdown" "minimagick" "test" ];
   };
 in
   stdenv.mkDerivation rec {
@@ -16,7 +16,7 @@ in
 
     src = fetchurl {
       url = "https://www.redmine.org/releases/${pname}-${version}.tar.gz";
-      sha256 = "sha256-byY4jCOJKWJVLKSR1e/tq9QtrIiGHdnYC8M0WPZb4ek=";
+      sha256 = "sha256-qJrRxLub8CXmUnx3qxjI+vd0nJSpdcryz9u6AOsSpIE=";
     };
 
     nativeBuildInputs = [ makeWrapper ];
