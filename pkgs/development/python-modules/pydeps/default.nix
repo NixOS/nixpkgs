64e2f5dbf76497fb5ca8cf3d640fea2ee44295e6
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-18 12:43:35 +0200

    python310Packages.pydeps: 1.12.3 -> 1.12.4
    Diff: https://github.com/thebjorn/pydeps/compare/refs/tags/v1.12.3...v1.12.4
    
    Changelog: https://github.com/thebjorn/pydeps/releases/tag/v1.12.4

--- pkgs/development/python-modules/pydeps/default.nix
+++ pkgs/development/python-modules/pydeps/default.nix
@@ -6,11 +6,12 @@
 , pytestCheckHook
 , pythonOlder
 , pyyaml
+, toml
 }:
 
 buildPythonPackage rec {
   pname = "pydeps";
-  version = "1.12.3";
+  version = "1.12.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +20,7 @@ buildPythonPackage rec {
     owner = "thebjorn";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-Zw5LGsNie2BAsJUWfL16HRdATpb5twkB5trBbIHNCc4=";
+    hash = "sha256-WnxSUylD6PAqmAcN5odM68C12suuOlWp4qpq1Vu8aYY=";
   };
 
   buildInputs = [
@@ -34,6 +35,7 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     pytestCheckHook
     pyyaml
+    toml
   ];
 
   postPatch = ''
