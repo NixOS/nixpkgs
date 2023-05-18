c64d445d6c323f95d653a6964d3786d68751c650
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-18 12:58:42 +0200

    esphome: 2023.5.0. -> 2023.5.1
    https://github.com/esphome/esphome/releases/tag/2023.5.1

--- pkgs/tools/misc/esphome/default.nix
+++ pkgs/tools/misc/esphome/default.nix
@@ -16,14 +16,14 @@ let
 in
 python.pkgs.buildPythonApplication rec {
   pname = "esphome";
-  version = "2023.5.0";
+  version = "2023.5.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-prbFCetgHhPsuiXI3bhfGpOpYHPusECC8zuPhT1ZCSU=";
+    hash = "sha256-alJQgAjlxqgKPpmW3nrcUFysh1juHq4YX3t21+gL/nI=";
   };
 
   postPatch = ''
