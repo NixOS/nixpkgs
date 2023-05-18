56d1cb2a785850db761444aaba18590570f582f8
Jan Tojnar <jtojnar@gmail.com>
2023-05-18 02:24:52 +0200

    caesium-image-compressor: init at 2.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30614,6 +30614,8 @@ with pkgs;
 
   caerbannog = callPackage ../applications/misc/caerbannog { };
 
+  caesium-image-compressor = qt6Packages.callPackage ../applications/graphics/caesium-image-compressor { };
+
   cardo = callPackage ../data/fonts/cardo { };
 
   cage = callPackage ../applications/window-managers/cage {
