bc622d0d24b716c5a5982036e2075374269cf772
Atemu <atemu.main@gmail.com>
2023-05-19 14:26:28 +0200

    linux_xanmod_latest: 6.3.2 -> 6.3.3
    (cherry picked from commit ff0f0ed2363313119025ae9588daeb40d147f47a)

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -9,8 +9,8 @@ let
   };
 
   mainVariant = {
-    version = "6.3.2";
-    hash = "sha256-/+tg7fhAQZzo9iSrGi5vFXcbMqL21d+L7dTFGRp6Bo4=";
+    version = "6.3.3";
+    hash = "sha256-3vkY79dmRUITXUIStYVlrR/JVLtLH2hfH1YKCF03Wmo=";
     variant = "main";
   };
 
