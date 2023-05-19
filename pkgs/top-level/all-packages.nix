ec5067be4d5ae53c164c9aac15905c192b60bdee
Benno Bielmeier <nix@bbenno.com>
2023-05-19 02:24:23 +0200

    whatsapp-chat-exporter: init at 0.9.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13571,6 +13571,8 @@ with pkgs;
 
   whatip = callPackage ../tools/networking/whatip { };
 
+  whatsapp-chat-exporter = callPackage ../tools/misc/whatsapp-chat-exporter { };
+
   whatweb = callPackage ../tools/security/whatweb { };
 
   whipper = callPackage ../applications/audio/whipper { };
