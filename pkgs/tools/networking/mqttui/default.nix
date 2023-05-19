0b0df14f76c07e2852f1d8f831203e9671691fb2
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-19 19:12:17 +0200

    mqttui: add changelog to meta

--- pkgs/tools/networking/mqttui/default.nix
+++ pkgs/tools/networking/mqttui/default.nix
@@ -12,17 +12,18 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "EdJoPaTo";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-cezG9hdHOeTExX4OJwJ22e/PvfdySPzQGwxumavV++Q=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-cezG9hdHOeTExX4OJwJ22e/PvfdySPzQGwxumavV++Q=";
   };
 
-  cargoSha256 = "sha256-vSlziZtjyzsd346qUBEPEl8I3UlPhWHRu4+FiD1XqOo=";
+  cargoHash = "sha256-vSlziZtjyzsd346qUBEPEl8I3UlPhWHRu4+FiD1XqOo=";
 
   buildInputs = lib.optional stdenv.isDarwin Security;
 
   meta = with lib; {
     description = "Terminal client for MQTT";
     homepage = "https://github.com/EdJoPaTo/mqttui";
+    changelog = "https://github.com/EdJoPaTo/mqttui/blob/v${version}/CHANGELOG.md";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ fab ];
   };
