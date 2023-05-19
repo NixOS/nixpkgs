aa7f55ce85f96771a1e45942465955febcd30086
Mario Rodas <marsam@users.noreply.github.com>
2023-05-19 04:20:00 +0000

    resvg: install libresvg

--- pkgs/tools/graphics/resvg/default.nix
+++ pkgs/tools/graphics/resvg/default.nix
@@ -13,6 +13,16 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-toUS1JAbJ8gbOsi87SXiqoaW0X7enAh4Iha3VeCa3WY=";
 
+  cargoBuildFlags = [
+    "--package=resvg"
+    "--package=resvg-capi"
+    "--package=usvg"
+  ];
+
+  postInstall = ''
+    install -Dm644 -t $out/include crates/c-api/*.h
+  '';
+
   meta = with lib; {
     description = "An SVG rendering library";
     homepage = "https://github.com/RazrFalcon/resvg";
