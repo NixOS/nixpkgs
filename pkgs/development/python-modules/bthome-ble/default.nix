f0a81c7dc1fefec690a243a784d48cd625405489
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-20 10:25:14 +0200

    python310Packages.bthome-ble: 2.11.2 -> 2.11.3
    Diff: https://github.com/Bluetooth-Devices/bthome-ble/compare/refs/tags/v2.11.2...v2.11.3
    
    Changelog: https://github.com/bluetooth-devices/bthome-ble/blob/v2.11.3/CHANGELOG.md

--- pkgs/development/python-modules/bthome-ble/default.nix
+++ pkgs/development/python-modules/bthome-ble/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "bthome-ble";
-  version = "2.11.2";
+  version = "2.11.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-U0R4WWgXtfP1vvrGdJl70xO88YhvxwJYDnMiN4B+Waw=";
+    hash = "sha256-axumlQnD7MKGdmy1gGQNU4B/3jXffK2jTyO3BtWvp5w=";
   };
 
   nativeBuildInputs = [
