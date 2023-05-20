b3f5ab3796733608794a2727e64a5efb5e7b93db
figsoda <figsoda@pm.me>
2023-05-20 16:47:43 -0400

    typst: 0.3.0 -> 0.4.0
    Diff: https://github.com/typst/typst/compare/v0.3.0...v0.4.0
    
    Changelog: https://github.com/typst/typst/releases/tag/v0.4.0

--- pkgs/tools/typesetting/typst/default.nix
+++ pkgs/tools/typesetting/typst/default.nix
@@ -8,19 +8,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "typst";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "typst";
     repo = "typst";
     rev = "v${version}";
-    hash = "sha256-Sx4HWs52WycAxAuoJyo8t83CxnMh64P94hbPiedfztc=";
+    hash = "sha256-S8J2D87Zvyh501d8LG69in9om/nTS6Y+IDhJvjm/H0w=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "iai-0.1.1" = "sha256-EdNzCPht5chg7uF9O8CtPWR/bzSYyfYIXNdLltqdlR0=";
+      "svg2pdf-0.4.1" = "sha256-WeVP+yhqizpTdRfyoj2AUxFKhGvVJIIiRV0GTXkgLtQ=";
     };
   };
 
