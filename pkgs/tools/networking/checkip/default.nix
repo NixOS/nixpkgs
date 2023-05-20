1d4afe48fd2ca0a64b2744129b58f1dd440bcfe0
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-20 22:41:28 +0000

    checkip: 0.45.1 -> 0.46.1

--- pkgs/tools/networking/checkip/default.nix
+++ pkgs/tools/networking/checkip/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "checkip";
-  version = "0.45.1";
+  version = "0.46.1";
 
   src = fetchFromGitHub {
     owner = "jreisinger";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-GUVyeQtUNnW8yu/dhfip61jxQtgQmjBUDzsOW233laQ=";
+    sha256 = "sha256-U0jHwKmGHpaHSiOYDeYCXiufw0JjzAmhBnINmFsqOJo=";
   };
 
   vendorHash = "sha256-9/z1mtZGqrvcvq8cWBpYN7kaPHaPqtyMwMNxuRRP4Cs=";
