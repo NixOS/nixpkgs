720f315c7536b71c6102ba8074c1656f4d8f0913
Anderson Torres <torres.anderson.85@protonmail.com>
2023-05-20 20:23:43 -0300

    sdbus-cpp: reformat meta.longDescription
    To fit 80-char limit.

--- pkgs/development/libraries/sdbus-cpp/default.nix
+++ pkgs/development/libraries/sdbus-cpp/default.nix
@@ -37,14 +37,17 @@ stdenv.mkDerivation rec {
     changelog = "https://github.com/Kistler-Group/sdbus-cpp/blob/v${version}/ChangeLog";
     description = "High-level C++ D-Bus library designed to provide easy-to-use yet powerful API";
     longDescription = ''
-      sdbus-c++ is a high-level C++ D-Bus library for Linux designed to provide expressive, easy-to-use API in modern C++.
-      It adds another layer of abstraction on top of sd-bus, a nice, fresh C D-Bus implementation by systemd.
-      It's been written primarily as a replacement of dbus-c++, which currently suffers from a number of (unresolved) bugs,
-      concurrency issues and inherent design complexities and limitations.
+      sdbus-c++ is a high-level C++ D-Bus library for Linux designed to provide
+      expressive, easy-to-use API in modern C++.
+      It adds another layer of abstraction on top of sd-bus, a nice, fresh C
+      D-Bus implementation by systemd.
+      It's been written primarily as a replacement of dbus-c++, which currently
+      suffers from a number of (unresolved) bugs, concurrency issues and
+      inherent design complexities and limitations.
     '';
     mainProgram = "sdbus-c++-xml2cpp";
     license = licenses.lgpl2Only;
-    platforms = platforms.linux;
     maintainers = [ maintainers.ivar ];
+    platforms = platforms.linux;
   };
 }
