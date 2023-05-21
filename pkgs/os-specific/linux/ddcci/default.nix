3b02b2c851815ed5545ba020b88f1570074c548a
Alexandre Iooss <erdnaxe@crans.org>
2023-05-21 14:02:51 +0200

    ddcci-driver: fix kernel 6.2 and 6.3 compatibility

--- pkgs/os-specific/linux/ddcci/default.nix
+++ pkgs/os-specific/linux/ddcci/default.nix
@@ -12,6 +12,15 @@ stdenv.mkDerivation rec {
     hash = "sha256-1Z6V/AorD4aslLKaaCZpmkD2OiQnmpu3iroOPlNPtLE=";
   };
 
+  patches = [
+    # https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/merge_requests/12
+    (fetchpatch {
+      name = "kernel-6.2-6.3.patch";
+      url = "https://gitlab.com/ddcci-driver-linux/ddcci-driver-linux/-/commit/1ef6079679acc455f75057dd7097b5b494a241dc.patch";
+      hash = "sha256-2C2leS20egGY3J2tq96gsUQXYw13wBJ3ZWrdIXxmEYs=";
+    })
+  ];
+
   hardeningDisable = [ "pic" ];
 
   nativeBuildInputs = kernel.moduleBuildDependencies;
