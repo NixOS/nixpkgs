00000006e9812e28fecc192a3883e01e88053f89
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-05-21 18:53:52 +0200

    nixos/tests: init portunus

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -597,6 +597,7 @@ in {
   podman-tls-ghostunnel = handleTestOn ["aarch64-linux" "x86_64-linux"] ./podman/tls-ghostunnel.nix {};
   polaris = handleTest ./polaris.nix {};
   pomerium = handleTestOn ["x86_64-linux"] ./pomerium.nix {};
+  portunus = handleTest ./portunus.nix { };
   postfix = handleTest ./postfix.nix {};
   postfix-raise-smtpd-tls-security-level = handleTest ./postfix-raise-smtpd-tls-security-level.nix {};
   postfixadmin = handleTest ./postfixadmin.nix {};
