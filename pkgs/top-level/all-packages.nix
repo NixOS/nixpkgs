27a617a770d9186065ea7c01b713355324181849
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-21 11:53:15 +0200

    keepwn: init at 0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9013,6 +9013,8 @@ with pkgs;
 
   keepalived = callPackage ../tools/networking/keepalived { };
 
+  keepwn = callPackage ../tools/security/keepwn { };
+
   kestrel = callPackage ../tools/security/kestrel { };
 
   kexec-tools = callPackage ../os-specific/linux/kexec-tools { };
