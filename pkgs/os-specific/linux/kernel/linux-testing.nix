12197ac5632ea19912156ea679344dfbff226fe3
Raito Bezarius <masterancpp@gmail.com>
2023-05-21 23:42:12 +0200

    linux_testing: 6.4-rc2 -> 6.4-rc3
    Dropped the Maple state patch as it is already applied now.

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.4-rc2";
+  version = "6.4-rc3";
   extraMeta.branch = lib.versions.majorMinor version;
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchzip {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    hash = "sha256-CQwSN5LQxGO900QLMAXcjGhB2o+6rZgXHQ+gCJtVaeU=";
+    hash = "sha256-twGP2zNNkxJy32wCCMlkGtV1xco05FeW2s3wljwB1eM=";
   };
 
   # Should the testing kernels ever be built on Hydra?
