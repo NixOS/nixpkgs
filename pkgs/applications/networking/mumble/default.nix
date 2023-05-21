d303f93c8b5feda45ab8f3e16f6cc1c93e29a123
Felix Singer <felixsinger@posteo.net>
2023-05-21 22:21:00 +0200

    mumble: Fix bug with OpenSSL 3
    See discussion https://github.com/mumble-voip/mumble/issues/5361#issuecomment-1173001440
    
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- pkgs/applications/networking/mumble/default.nix
+++ pkgs/applications/networking/mumble/default.nix
@@ -23,6 +23,7 @@ let
     patches = [
       ./0001-BUILD-crypto-Migrate-to-OpenSSL-3.0-compatible-API.patch
       ./0002-FIX-positional-audio-Force-8-bytes-alignment-for-CCa.patch
+      ./0003-FIX-crypto-Sharing-EVP-context-between-threads-crush.patch
     ];
 
     nativeBuildInputs = [ cmake pkg-config python3 qt5.wrapQtAppsHook qt5.qttools ]
