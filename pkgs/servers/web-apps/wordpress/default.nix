c92f2244d1a31ed5ac66d3c625c332e817a1e92a
Jonas Heinrich <onny@project-insanity.org>
2023-05-22 09:18:50 +0200

    wordpress6_2: 6.2.1 -> 6.2.2

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -1,8 +1,8 @@
 { callPackage }: builtins.mapAttrs (_: callPackage ./generic.nix) rec {
   wordpress = wordpress6_2;
   wordpress6_2 = {
-    version = "6.2.1";
-    hash = "sha256-jGmOEmdj3n4bCoTJH/4DEsjTBiaEmaxBt1kA19HctU8=";
+    version = "6.2.2";
+    hash = "sha256-0qpvPauGbeP1MLHmz6gItJf80Erts7E7x28TM9AmAPk=";
   };
   wordpress6_1 = {
     version = "6.1.2";
