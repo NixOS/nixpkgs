bb820fbaf63ed18d9b382e7fd70a72840e5d7d6b
OTABI Tomoya <tomoya.otabi@gmail.com>
2023-05-23 08:10:16 +0900

    apbs: init at 3.4.1 (#230644)
    * apbs: init at 3.4.1
    
    * Update pkgs/applications/science/chemistry/apbs/default.nix
    
    ---------
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37396,6 +37396,8 @@ with pkgs;
 
   ### SCIENCE/CHEMISTY
 
+  apbs = callPackage ../applications/science/chemistry/apbs { };
+
   avogadro = callPackage ../applications/science/chemistry/avogadro {
     openbabel = openbabel2;
     eigen = eigen2;
