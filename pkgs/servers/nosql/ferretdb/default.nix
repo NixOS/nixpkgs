8530eab698650e39bc4817a6205ad937c1b097bc
Julien Malka <julien@malka.sh>
2023-05-22 18:56:52 +0200

    ferretdb: 1.1.0 -> 1.2.0
    (cherry picked from commit 7ea816fd248e9115a6cb3b5c1567334fbf475400)

--- pkgs/servers/nosql/ferretdb/default.nix
+++ pkgs/servers/nosql/ferretdb/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "ferretdb";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "FerretDB";
     repo = "FerretDB";
     rev = "v${version}";
-    sha256 = "sha256-V06NIjiT+uxN39vGhwvrU4hbrNezWReEtJdbW6BHIzE=";
+    sha256 = "sha256-m22NoeohM/rb43xOyWaGEc9P32FAhxZ5J2c706qJf4Y=";
   };
 
   postPatch = ''
@@ -19,7 +19,7 @@ buildGoModule rec {
     echo nixpkgs     > build/version/package.txt
   '';
 
-  vendorSha256 = "sha256-/lM98VTQc6glhnpETW9XbxgN2fP6dBexueByFWwv5sk=";
+  vendorSha256 = "sha256-6sddJcNzPxMarP0/QxpeWF0qXR8wT2kU6N6CtGKG1Tk=";
 
   CGO_ENABLED = 0;
 
