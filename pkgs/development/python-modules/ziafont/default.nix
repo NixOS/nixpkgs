a6fdc73e9190caa262ab9cbc2668b5eb86f27b8a
Stefan Frijters <sfrijters@gmail.com>
2023-05-22 14:46:52 +0200

    python3Packages.ziafont: 0.5 -> 0.6

--- pkgs/development/python-modules/ziafont/default.nix
+++ pkgs/development/python-modules/ziafont/default.nix
@@ -8,7 +8,9 @@
 
 buildPythonPackage rec {
   pname = "ziafont";
-  version = "0.5";
+  version = "0.6";
+
+  format = "pyproject";
 
   disabled = pythonOlder "3.8";
 
@@ -16,7 +18,7 @@ buildPythonPackage rec {
     owner = "cdelker";
     repo = pname;
     rev = version;
-    hash = "sha256-mTQ2yRG+E2nZ2g9eSg+XTzK8A1EgKsRfbvNO3CdYeLg=";
+    hash = "sha256-3ZVj1ZxbFkFDDYbsIPzo7GMWGx7f5qWZQlcGCVXv73M=";
   };
 
   nativeCheckInputs = [
