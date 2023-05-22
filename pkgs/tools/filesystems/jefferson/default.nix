9c824ed0797cf4fc9a86065fcbde1997a104ec2a
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-05-22 22:11:22 +0200

    jefferson: 0.4.2 -> 0.4.5
    - `python-lzo` is switched to `lzallright`

--- pkgs/tools/filesystems/jefferson/default.nix
+++ pkgs/tools/filesystems/jefferson/default.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "jefferson";
-  version = "0.4.2";
+  version = "0.4.5";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "onekey-sec";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-zW38vcDw4Jz5gO9IHrWRlvUznKvUyPbxkYMxn7VSTpA=";
+    hash = "sha256-PGtrvZ0cQvdiswn2Bk43c3LbIZqJyvNe5rnTPw/ipUM=";
   };
 
   nativeBuildInputs = with python3.pkgs; [
@@ -22,7 +22,7 @@ python3.pkgs.buildPythonApplication rec {
   propagatedBuildInputs = with python3.pkgs; [
     click
     cstruct
-    python-lzo
+    lzallright
   ];
 
   pythonImportsCheck = [
