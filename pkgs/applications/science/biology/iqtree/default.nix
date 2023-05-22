6d71b5d6de16abfa008340f3bf5ad51e6eb7f677
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-05-22 11:14:57 +0300

    iqtree: set platforms

--- pkgs/applications/science/biology/iqtree/default.nix
+++ pkgs/applications/science/biology/iqtree/default.nix
@@ -5,6 +5,7 @@
 , boost
 , eigen
 , zlib
+, llvmPackages
 }:
 
 stdenv.mkDerivation rec {
@@ -15,18 +16,25 @@ stdenv.mkDerivation rec {
     owner = "iqtree";
     repo = "iqtree2";
     rev = "v${version}";
-    sha256 = "sha256-5NF0Ej3M19Vd08xfmOHRhZkM1YGQ/ZlFj0HsSw1sw1w=";
+    hash = "sha256-5NF0Ej3M19Vd08xfmOHRhZkM1YGQ/ZlFj0HsSw1sw1w=";
     fetchSubmodules = true;
   };
 
   nativeBuildInputs = [ cmake ];
 
-  buildInputs = [ boost eigen zlib ];
+  buildInputs = [
+    boost
+    eigen
+    zlib
+  ] ++ lib.optionals stdenv.isDarwin [
+    llvmPackages.openmp
+  ];
 
   meta = with lib; {
     homepage = "http://www.iqtree.org/";
     description = "Efficient and versatile phylogenomic software by maximum likelihood";
     license = licenses.lgpl2;
     maintainers = with maintainers; [ bzizou ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" ];
   };
 }
