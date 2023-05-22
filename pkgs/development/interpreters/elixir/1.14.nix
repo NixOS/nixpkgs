9c9d0a0a4c2ce749127797d79315a489f35da352
Adam Stephens <adam@valkor.net>
2023-05-22 11:43:05 -0400

    elixir_1_14: 1.14.4 -> 1.14.5

--- pkgs/development/interpreters/elixir/1.14.nix
+++ pkgs/development/interpreters/elixir/1.14.nix
@@ -1,8 +1,8 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "1.14.4";
-  sha256 = "sha256-mV40pSpLrYKT43b8KXiQsaIB+ap+B4cS2QUxUoylm7c=";
-  # https://hexdocs.pm/elixir/1.14.4/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
+  version = "1.14.5";
+  sha256 = "sha256-bCCTjFT+FG1hz+0H6k/izbCmi0JgO3Kkqc3LWWCs5Po=";
+  # https://hexdocs.pm/elixir/1.14.5/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
   minimumOTPVersion = "23";
 }
