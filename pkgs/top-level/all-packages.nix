f752ef56b1e79d3d6e461c92b29990158dd2374f
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-22 14:42:19 +0200

    exabgp: init at 4.2.21
     The BGP swiss army knife of networking

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25281,6 +25281,8 @@ with pkgs;
 
   eventstore = callPackage ../servers/nosql/eventstore { };
 
+  exabgp = callPackage ../servers/networking/exabgp { };
+
   exim = callPackage ../servers/mail/exim { };
 
   fastnetmon-advanced = callPackage ../servers/fastnetmon-advanced { };
