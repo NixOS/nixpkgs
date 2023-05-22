9d2ed18ece8e1e4f54395ed06a1fcabce5235218
Emery Hemingway <ehmry@posteo.net>
2023-05-22 18:02:59 +0100

    New Aleph One scenario alephone-yuge

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36274,6 +36274,8 @@ with pkgs;
   alephone-rubicon-x = callPackage ../games/alephone/rubicon-x { };
   alephone-pathways-into-darkness =
     callPackage ../games/alephone/pathways-into-darkness { };
+  alephone-yuge =
+    callPackage ../games/alephone/yuge { };
 
   alienarena = callPackage ../games/alienarena { };
 
