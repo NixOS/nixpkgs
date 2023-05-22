a4ab26db367f55213bda183fea6feaa704f7bb24
ners <ners@gmx.ch>
2023-05-22 15:05:02 +0200

    ssdfs-utils: init at 4.24
    (cherry picked from commit 67e075221c04ae171a3fd766495d8a0616113900)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3282,6 +3282,8 @@ with pkgs;
 
   btrfs-snap = callPackage ../tools/filesystems/btrfs-snap { };
 
+  ssdfs-utils = callPackage ../tools/filesystems/ssdfs-utils { };
+
   btlejack = python3Packages.callPackage ../applications/radio/btlejack { };
 
   btrbk = callPackage ../tools/backup/btrbk { };
