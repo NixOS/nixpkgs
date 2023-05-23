315db268438383a679d04f94d5790a5524eed1f2
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-23 07:26:47 +0000

    fselect: 0.8.2 -> 0.8.3

--- pkgs/tools/misc/fselect/default.nix
+++ pkgs/tools/misc/fselect/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "fselect";
-  version = "0.8.2";
+  version = "0.8.3";
 
   src = fetchFromGitHub {
     owner = "jhspetersson";
     repo = "fselect";
     rev = version;
-    sha256 = "sha256-JhiNLlgnVIrecYNlestociTXHBxfUMTQHSzo3/ePXds=";
+    sha256 = "sha256-f0fc+gj6t1PPdMekYzuZCy2WJrjssLzdsxFoBdFRKBM=";
   };
 
-  cargoHash = "sha256-HOOxr5hBrenziai+TxatgXjMi8G3xqIM8OqdMeeKEgg=";
+  cargoHash = "sha256-nYavH8D3dQsr9tKB7PFETGp+KgTm/1EhRtAdTqbwrzQ=";
 
   nativeBuildInputs = [ installShellFiles ];
   buildInputs = lib.optional stdenv.isDarwin libiconv;
