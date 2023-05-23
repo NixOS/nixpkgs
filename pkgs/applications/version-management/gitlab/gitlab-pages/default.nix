5b28f8fef47d24b5ca327fae7cda0dcdb93aefeb
Yaya <mak@nyantec.com>
2023-05-23 11:57:03 +0000

    gitlab: 15.11.6 -> 16.0.1
    https://about.gitlab.com/releases/2023/05/22/gitlab-16-0-released/
    https://about.gitlab.com/releases/2023/05/23/critical-security-release-gitlab-16-0-1-released/
    (cherry picked from commit 22b27aa7466ba18017d49e19e507049eeb6b41e9)

--- pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "gitlab-pages";
-  version = "15.11.6";
+  version = "16.0.1";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-pages";
     rev = "v${version}";
-    sha256 = "sha256-Dl/NCsZCi5S9BKjtQzRg3mj8lzvIa4FMCqprLKXKlHw=";
+    sha256 = "sha256-RbsPWc3Dc/rMLnSID0dZmHMg3+uK91kI+DXBYPSy81w=";
   };
 
   vendorHash = "sha256-s3HHoz9URACuVVhePQQFviTqlQU7vCLOjTJPBlus1Vo=";
