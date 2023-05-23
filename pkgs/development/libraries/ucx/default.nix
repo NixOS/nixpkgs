638352894503225177e2d1909a8a0385bbaecd7b
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-23 23:43:15 +0000

    ucx: 1.14.0 -> 1.14.1

--- pkgs/development/libraries/ucx/default.nix
+++ pkgs/development/libraries/ucx/default.nix
@@ -20,13 +20,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "ucx";
-  version = "1.14.0";
+  version = "1.14.1";
 
   src = fetchFromGitHub {
     owner = "openucx";
     repo = "ucx";
     rev = "v${version}";
-    sha256 = "sha256-OSYeJfMi57KABt8l3Yj0glqx54C5cwM2FqlijszJIk4=";
+    sha256 = "sha256-oAigiCgbr27pX+kNl+RW1P10TKYFSKrHDK4U4z8WMko=";
   };
 
   nativeBuildInputs = [ autoreconfHook doxygen pkg-config ];
