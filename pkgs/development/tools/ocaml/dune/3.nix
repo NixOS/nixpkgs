794ecc0357a582153c62f7bfe7bd4f9d056790d5
Mario Rodas <marsam@users.noreply.github.com>
2023-05-23 04:20:00 +0000

    dune_3: 3.7.1 -> 3.8.0
    Diff: https://github.com/ocaml/dune/compare/None...3.8.0
    
    Changelog: https://github.com/ocaml/dune/raw/3.8.0/CHANGES.md

--- pkgs/development/tools/ocaml/dune/3.nix
+++ pkgs/development/tools/ocaml/dune/3.nix
@@ -6,11 +6,11 @@ else
 
 stdenv.mkDerivation rec {
   pname = "dune";
-  version = "3.7.1";
+  version = "3.8.0";
 
   src = fetchurl {
     url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-    sha256 = "sha256-rfw48UwBiKKtgNYUUdAR0nq4g5txdJLXrUL3y5EcVMM=";
+    sha256 = "sha256-9+KXC/q5nHdm/xoF3v9a1/gsskcavKb3JElXIpP19rQ=";
   };
 
   nativeBuildInputs = [ ocaml findlib ];
