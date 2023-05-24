56219af82241a2d15306d0423ebf4b98b1bae360
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-24 09:01:29 +0200

    python310Packages.pypinyin: update meta
    - add format
    - disable on unsupported Python releases

--- pkgs/development/python-modules/pypinyin/default.nix
+++ pkgs/development/python-modules/pypinyin/default.nix
@@ -2,11 +2,15 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pypinyin";
   version = "0.49.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "mozillazg";
@@ -31,7 +35,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Chinese Characters to Pinyin - 汉字转拼音";
     homepage = "https://github.com/mozillazg/python-pinyin";
-    changelog = "https://github.com/mozillazg/python-pinyin/blob/master/CHANGELOG.rst";
+    changelog = "https://github.com/mozillazg/python-pinyin/blob/v${version}/CHANGELOG.rst";
     license = licenses.mit;
     maintainers = teams.tts.members;
   };
