6a6c242c617f57e067f36b68c74c905d42925f5f
whyvert <91154413+whyvert0@users.noreply.github.com>
2023-05-24 10:22:46 -0400

    maintainers: add whyvert

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16927,6 +16927,11 @@
     githubId = 7121530;
     name = "Wolf Honoré";
   };
+  whyvert = {
+    github = "whyvert";
+    githubId = "91154413";
+    name = "Whyvert";
+  };
   wildsebastian = {
     name = "Sebastian Wild";
     email = "sebastian@wild-siena.com";
