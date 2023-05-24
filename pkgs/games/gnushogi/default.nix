410aa6fbc28bbbd3d798595848d91e55eea1b13d
Yongun Seong <nevivurn@nevi.dev>
2023-05-24 23:40:40 +0900

    gnushogi: fix build
    Also, mark as broken as darwin due to unsupported linker flags

--- pkgs/games/gnushogi/default.nix
+++ pkgs/games/gnushogi/default.nix
@@ -10,11 +10,19 @@ stdenv.mkDerivation rec {
     sha256 = "0a9bsl2nbnb138lq0h14jfc5xvz7hpb2bcsj4mjn6g1hcsl4ik0y";
   };
 
+  env.LDFLAGS = lib.optionalString (!stdenv.isDarwin) "-Wl,-z,muldefs";
+
+  # Makefile ignores errors, so the build may silently succeed erroneously
+  postBuild = ''
+    test -e gnushogi/gnushogi || { echo "ERROR: no binary produced"; exit 1; }
+  '';
+
   meta = with lib; {
     description = "GNU implementation of Shogi, also known as Japanese Chess";
     homepage = "https://www.gnu.org/software/gnushogi/";
     license = licenses.gpl3;
     maintainers = [ maintainers.ciil ];
     platforms = platforms.unix;
+    broken = stdenv.isDarwin; # darwin does not support -z muldefs
   };
 }
