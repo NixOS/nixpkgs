04a15cb807f492dc4d935ce3b115504391be0386
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-05-25 18:26:03 +0200

    pynitrokey: 0.4.36 -> 0.4.37
    https://github.com/Nitrokey/pynitrokey/releases/tag/v0.4.37
    (cherry picked from commit 266471ac2da94b6c7e34d2f2119c448c7195386c)

--- pkgs/tools/security/pynitrokey/default.nix
+++ pkgs/tools/security/pynitrokey/default.nix
@@ -4,12 +4,12 @@ with python3Packages;
 
 buildPythonApplication rec {
   pname = "pynitrokey";
-  version = "0.4.36";
+  version = "0.4.37";
   format = "flit";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Y+6T1iUp9TVYbAjpXVHozC6WT061r0VYv/ifu8lcN6E=";
+    hash = "sha256-KoZym1b+E0P3kRt0PTm9wCX4nO31isDIwEq38xMgDDU=";
   };
 
   propagatedBuildInputs = [
