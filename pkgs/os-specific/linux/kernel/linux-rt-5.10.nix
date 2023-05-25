4a588b3ddefded7e6be8d64bc0cfdcabd3ea9837
K900 <me@0upti.me>
2023-05-25 09:03:35 +0300

    linux-rt_5_10: 5.10.176-rt86 -> 5.10.179-rt87
    (cherry picked from commit de66762d8e74703e7b9c8117a12a479441bfb438)

--- pkgs/os-specific/linux/kernel/linux-rt-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-rt-5.10.nix
@@ -6,7 +6,7 @@
 , ... } @ args:
 
 let
-  version = "5.10.176-rt86"; # updated by ./update-rt.sh
+  version = "5.10.179-rt87"; # updated by ./update-rt.sh
   branch = lib.versions.majorMinor version;
   kversion = builtins.elemAt (lib.splitString "-" version) 0;
 in buildLinux (args // {
@@ -17,14 +17,14 @@ in buildLinux (args // {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${kversion}.tar.xz";
-    sha256 = "14zpdrrrpgxx44nxjn0rifrchnmsvvpkzpm1n82kw5q4p9h2q1yf";
+    sha256 = "0abylcqbzpxxh45kmvd9i2cig64aajz87j5c8vm3w1ab2mf49g8v";
   };
 
   kernelPatches = let rt-patch = {
     name = "rt";
     patch = fetchurl {
       url = "mirror://kernel/linux/kernel/projects/rt/${branch}/older/patch-${version}.patch.xz";
-      sha256 = "0pjf9fdhfh562mp18q8zwk4mbwj736yhmvakj0vr41ax9r3frj0l";
+      sha256 = "045pjxlrv05vpiavgnaq971ziirxa1hzkzs1fv11npp4q45lqzp3";
     };
   }; in [ rt-patch ] ++ kernelPatches;
 
