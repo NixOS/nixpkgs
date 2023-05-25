1ea2260060de2f9139f5e32f38f5134beabcb0ce
Anderson Torres <torres.anderson.85@protonmail.com>
2023-05-24 22:48:23 -0300

    fleng: init at 14
    FLENG is a low level concurrent logic programming language descended from
    Prolog.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2290,6 +2290,8 @@ with pkgs;
 
   flix = callPackage ../development/compilers/flix { };
 
+  fleng = callPackage ../development/compilers/fleng { };
+
   fsrx = callPackage ../tools/misc/fsrx { };
 
   fsuae = callPackage ../applications/emulators/fs-uae { };
