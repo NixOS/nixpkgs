8384f971878d5f2700cf8bc6b60470f88c2a6395
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-05-25 05:54:11 +0200

    coqPackages.deriving: 0.1.0 → 0.1.1

--- pkgs/development/coq-modules/deriving/default.nix
+++ pkgs/development/coq-modules/deriving/default.nix
@@ -8,11 +8,12 @@ mkCoqDerivation {
 
   inherit version;
   defaultVersion = with lib.versions; lib.switch coq.coq-version [
-    { case = range "8.11" "8.16"; out = "0.1.0"; }
+    { case = range "8.11" "8.17"; out = "0.1.1"; }
   ] null;
 
   releaseRev = v: "v${v}";
 
+  release."0.1.1".sha256 = "sha256-Gu8aInLxTXfAFE0/gWRYI046Dx3Gv1j1+gx92v/UnPI=";
   release."0.1.0".sha256 = "sha256:11crnjm8hyis1qllkks3d7r07s1rfzwvyvpijya3s6iqfh8c7xwh";
 
   propagatedBuildInputs = [ ssreflect ];
