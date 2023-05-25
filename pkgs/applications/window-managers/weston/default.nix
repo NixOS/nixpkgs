d4ef336af46af39a38911db3ec80d3eef7792160
Alyssa Ross <hi@alyssa.is>
2023-05-25 11:24:18 +0000

    weston: 11.0.2 -> 12.0.1

--- pkgs/applications/window-managers/weston/default.nix
+++ pkgs/applications/window-managers/weston/default.nix
@@ -1,43 +1,49 @@
 { lib, stdenv, fetchurl
 , meson, ninja, pkg-config, python3, wayland-scanner
 , cairo, dbus, lcms2, libdrm, libevdev, libinput, libjpeg, libxkbcommon, mesa
-, seatd, wayland, wayland-protocols
+, seatd, wayland, wayland-protocols, xcbutilcursor
 
 , demoSupport ? true
+, hdrSupport ? true, libdisplay-info
 , pangoSupport ? true, pango
 , pipewireSupport ? true, pipewire
 , rdpSupport ? true, freerdp
 , remotingSupport ? true, gst_all_1
 , vaapiSupport ? true, libva
+, vncSupport ? true, aml, neatvnc, pam
 , webpSupport ? true, libwebp
 , xwaylandSupport ? true, libXcursor, xwayland
 }:
 
 stdenv.mkDerivation rec {
   pname = "weston";
-  version = "11.0.2";
+  version = "12.0.1";
 
   src = fetchurl {
     url = "https://gitlab.freedesktop.org/wayland/weston/-/releases/${version}/downloads/weston-${version}.tar.xz";
-    hash = "sha256-ckB1LO8LfeYiuvi9U0jmP8axnwLvgklhsq3Rd9llKVI=";
+    hash = "sha256-sYWR6rJ4vBkXIPbAkVgEC3lecRivHV3cpqzZqOIDlTU=";
   };
 
   depsBuildBuild = [ pkg-config ];
   nativeBuildInputs = [ meson ninja pkg-config python3 wayland-scanner ];
   buildInputs = [
-    cairo dbus lcms2 libdrm libevdev libinput libjpeg libxkbcommon mesa seatd
+    cairo lcms2 libdrm libevdev libinput libjpeg libxkbcommon mesa seatd
     wayland wayland-protocols
-  ] ++ lib.optional pangoSupport pango
+  ] ++ lib.optional hdrSupport libdisplay-info
+    ++ lib.optional pangoSupport pango
     ++ lib.optional pipewireSupport pipewire
     ++ lib.optional rdpSupport freerdp
     ++ lib.optionals remotingSupport [ gst_all_1.gstreamer gst_all_1.gst-plugins-base ]
     ++ lib.optional vaapiSupport libva
+    ++ lib.optionals vncSupport [ aml neatvnc pam ]
     ++ lib.optional webpSupport libwebp
-    ++ lib.optionals xwaylandSupport [ libXcursor xwayland ];
+    ++ lib.optionals xwaylandSupport [ libXcursor xcbutilcursor xwayland ];
 
   mesonFlags= [
     (lib.mesonBool "backend-drm-screencast-vaapi" vaapiSupport)
+    (lib.mesonBool "backend-pipewire" pipewireSupport)
     (lib.mesonBool "backend-rdp" rdpSupport)
+    (lib.mesonBool "backend-vnc" vncSupport)
     (lib.mesonBool "demo-clients" demoSupport)
     (lib.mesonBool "image-webp" webpSupport)
     (lib.mesonBool "pipewire" pipewireSupport)
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35178,11 +35178,13 @@ with pkgs;
 
   westonLite = weston.override {
     demoSupport = false;
+    hdrSupport = false;
     pangoSupport = false;
     pipewireSupport = false;
     rdpSupport = false;
     remotingSupport = false;
     vaapiSupport = false;
+    vncSupport = false;
     webpSupport = false;
     xwaylandSupport = false;
   };
