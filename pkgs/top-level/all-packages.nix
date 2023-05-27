15259fffeb0cee7e67c3cd87185fc4c24a13d3c0
seth <getchoo@tuta.io>
2023-05-27 15:50:03 -0400

    cartridges: init at 2.0.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -395,6 +395,8 @@ with pkgs;
 
   caroline = callPackage ../development/libraries/caroline { };
 
+  cartridges = callPackage ../applications/misc/cartridges { };
+
   castget = callPackage ../applications/networking/feedreaders/castget { };
 
   castxml = callPackage ../development/tools/castxml { };
