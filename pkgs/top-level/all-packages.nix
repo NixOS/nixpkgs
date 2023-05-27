472b1d40db0d439439f1133b66423d6659ed2af3
Zhaofeng Li <hello@zhaofeng.li>
2023-05-27 11:11:49 -0600

    qemu-user-static: init
    This is synonymous with the "qemu-user-static" packages available in
    other distros.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33652,6 +33652,8 @@ with pkgs;
     inherit (darwin) sigtool;
   };
 
+  qemu-user-static = callPackage ../applications/virtualization/qemu/user-static.nix { };
+
   qemu-utils = callPackage ../applications/virtualization/qemu/utils.nix { };
 
   canokey-qemu = callPackage ../applications/virtualization/qemu/canokey-qemu.nix { };
