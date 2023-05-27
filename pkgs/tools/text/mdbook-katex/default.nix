7997e72cd9e80ba973cb8350451e0cfd0fe20d8f
figsoda <figsoda@pm.me>
2023-05-27 15:16:25 -0400

    mdbook-katex: 0.5.0 -> 0.5.1
    Diff: https://diff.rs/mdbook-katex/0.5.0/0.5.1

--- pkgs/tools/text/mdbook-katex/default.nix
+++ pkgs/tools/text/mdbook-katex/default.nix
@@ -2,14 +2,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mdbook-katex";
-  version = "0.5.0";
+  version = "0.5.1";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-6gZTJrjr3cO7f6XBrQrBKe62UsnPxGo1eXX/+ICdJfk=";
+    hash = "sha256-RDNZ6+d+UbiQ/Eb2+YbhPyGVcM8079UFsnNvch/1oAs=";
   };
 
-  cargoHash = "sha256-KHC37LNnc789anxJBADgj97tw5TwR/5ksxP3X/9pHlo=";
+  cargoHash = "sha256-An2mQ4kWGF3qk2v9VeQh700n7n/+3ShPqMfCnZmiIuc=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ CoreServices ];
 
