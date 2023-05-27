5b2bbfc97e932eda5c09af22e3c16411fd99ecb3
Dennis Gosnell <cdep.illabout@gmail.com>
2023-05-27 17:18:10 +0900

    haskell.compiler.ghc928: init at 9.2.8
    (cherry picked from commit d7d6b1c4453593888ae63cf69b85e37e3bde98e3)

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -20,6 +20,7 @@ let
     "ghc925"
     "ghc926"
     "ghc927"
+    "ghc928"
     "ghc92"
     "ghc942"
     "ghc943"
@@ -39,6 +40,7 @@ let
     "ghc925"
     "ghc926"
     "ghc927"
+    "ghc928"
     "ghc94"
     "ghc942"
     "ghc943"
@@ -215,6 +217,23 @@ in {
       llvmPackages = pkgs.llvmPackages_12;
     };
     ghc927 = callPackage ../development/compilers/ghc/9.2.7.nix {
+      bootPkgs =
+        # aarch64 ghc8107Binary exceeds max output size on hydra
+        if stdenv.hostPlatform.isAarch then
+          packages.ghc8107BinaryMinimal
+        else if stdenv.hostPlatform.isPower64 && stdenv.hostPlatform.isLittleEndian then
+          packages.ghc810
+        else
+          packages.ghc8107Binary;
+      inherit (buildPackages.python3Packages) sphinx;
+      # Need to use apple's patched xattr until
+      # https://github.com/xattr/xattr/issues/44 and
+      # https://github.com/xattr/xattr/issues/55 are solved.
+      inherit (buildPackages.darwin) xattr autoSignDarwinBinariesHook;
+      buildTargetLlvmPackages = pkgsBuildTarget.llvmPackages_12;
+      llvmPackages = pkgs.llvmPackages_12;
+    };
+    ghc928 = callPackage ../development/compilers/ghc/9.2.8.nix {
       bootPkgs =
         # aarch64 ghc8107Binary exceeds max output size on hydra
         if stdenv.hostPlatform.isAarch then
@@ -483,6 +502,11 @@ in {
     ghc927 = callPackage ../development/haskell-modules {
       buildHaskellPackages = bh.packages.ghc927;
       ghc = bh.compiler.ghc927;
+      compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-9.2.x.nix { };
+    };
+    ghc928 = callPackage ../development/haskell-modules {
+      buildHaskellPackages = bh.packages.ghc928;
+      ghc = bh.compiler.ghc928;
       compilerConfig = callPackage ../development/haskell-modules/configuration-ghc-9.2.x.nix { };
     };
     ghc92 = ghc927;
