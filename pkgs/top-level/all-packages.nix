bbefb9b2a3ebe3bb1a5629df72e442eb806422a6
Nikolay Korotkiy <sikmir@disroot.org>
2023-05-28 12:27:26 +0400

    flashmq: init at 1.4.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25363,6 +25363,8 @@ with pkgs;
 
   inherit (callPackages ../servers/firebird { }) firebird_4 firebird_3 firebird_2_5 firebird;
 
+  flashmq = callPackage ../servers/mqtt/flashmq { };
+
   freeradius = callPackage ../servers/freeradius { };
 
   freshrss = callPackage ../servers/web-apps/freshrss { };
