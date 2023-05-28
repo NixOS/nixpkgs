3d43f48a93c0b9ba6981c8533e92b816fd9ee6a7
Anderson Torres <torres.anderson.85@protonmail.com>
2023-05-28 20:45:29 -0300

    emacs29: init at 20.0.91
    Given that Emacs 29 is considered beta, it will not be aliased to `emacs`.
    And, given that Emacs 29 ships with Pure GTK feature, why not to make it
    available?

--- pkgs/applications/editors/emacs/default.nix
+++ pkgs/applications/editors/emacs/default.nix
@@ -33,6 +33,27 @@ lib.makeScope pkgs.newScope (self:
       noGui = true;
     });
 
+    emacs29 = callPackage (self.sources.emacs29) {
+      inherit gconf;
+
+      inherit (pkgs.darwin) sigtool;
+      inherit (pkgs.darwin.apple_sdk.frameworks)
+        AppKit Carbon Cocoa GSS ImageCaptureCore ImageIO IOKit OSAKit Quartz
+        QuartzCore WebKit;
+    };
+
+    emacs29-gtk3 = self.emacs29.override {
+      withGTK3 = true;
+    };
+
+    emacs29-nox = self.emacs29.override {
+      noGui = true;
+    };
+
+    emacs29-pgtk = self.emacs29.override {
+      withPgtk = true;
+    };
+
     emacs-macport = callPackage (self.sources.emacs-macport) {
       inherit gconf;
 
--- pkgs/applications/editors/emacs/sources.nix
+++ pkgs/applications/editors/emacs/sources.nix
@@ -15,6 +15,17 @@
     };
   };
 
+  emacs29 = import ./generic.nix {
+    pname = "emacs";
+    version = "29.0.91";
+    variant = "mainline";
+    src = fetchFromSavannah {
+      repo = "emacs";
+      rev = "29.0.91";
+      hash = "sha256-YU/sbIr7xX5ELJtPcTL9ZQgZtjEW5oI7YC20fQsOVSY=";
+    };
+  };
+
   emacs-macport = import ./generic.nix {
     pname = "emacs-mac";
     version = "28.2";
