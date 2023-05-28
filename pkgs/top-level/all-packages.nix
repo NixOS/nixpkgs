NixOS/nixpkgs

commit 92d15cf5f0d4a975cbc3ee8f28767937fe1be217

vtm: extend platforms

pkgs/top-level/all-packages.nix
@@ -13559,7 +13559,9 @@ with pkgs;
     SDL = SDL_sixel;
   };
 
-  vtm = callPackage ../tools/misc/vtm { };
+  vtm = callPackage ../tools/misc/vtm {
+    stdenv = if stdenv.isDarwin then clang16Stdenv else stdenv;
+  };
 
   witness = callPackage ../tools/security/witness { };
 

pkgs/tools/misc/vtm/default.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
     description = "Terminal multiplexer with window manager and session sharing";
     homepage = "https://vtm.netxs.online/";
     license = licenses.mit;
-    platforms = [ "x86_64-linux" ];
+    platforms = platforms.all;
     maintainers = with maintainers; [ ahuzik ];
   };
 }

commit 82082e931fd7199c929fb7901aac05e54cd1e18c

vtm: avoid using an alias
Regressed in 92d15cf5f0d4a9.
These aliases are deprecated; usage blocks channel via
https://hydra.nixos.org/log/rgfflnxgnghdmfqh8id62wc6b13vrh0z-nixpkgs-release-checks.drv

pkgs/top-level/all-packages.nix
@@ -13578,7 +13578,7 @@ with pkgs;
   };
 
   vtm = callPackage ../tools/misc/vtm {
-    stdenv = if stdenv.isDarwin then clang16Stdenv else stdenv;
+    stdenv = if stdenv.isDarwin then llvmPackages_16.stdenv else stdenv;
   };
 
   witness = callPackage ../tools/security/witness { };