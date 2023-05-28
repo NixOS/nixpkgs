701c1bbf464a1a6b7ed3b026c42f66d1b9105c83
Heinz Deinhart <heinz@nix.at>
2023-05-28 17:03:58 +0200

    ddnet: 17.0 -> 17.0.1
    https://ddnet.org/downloads/#17.0.1

--- pkgs/games/ddnet/default.nix
+++ pkgs/games/ddnet/default.nix
@@ -34,19 +34,19 @@
 
 stdenv.mkDerivation rec {
   pname = "ddnet";
-  version = "17.0";
+  version = "17.0.1";
 
   src = fetchFromGitHub {
     owner = "ddnet";
     repo = pname;
     rev = version;
-    hash = "sha256-boFXzARVG2At92j9gSavteAQ8qTjgJ91hIefVr/e6og=";
+    hash = "sha256-QN4hjUBQFDfyNm/f8TqgrexUAoNtGY4tKC0JcQBL8i0=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
     name = "${pname}-${version}";
     inherit src;
-    hash = "sha256-3itblnHlY1L8g/EGCi1BIWGD6INOpnvLCwJ7zL7KV4w=";
+    hash = "sha256-5Iwn8JSeOnAAXOt+QGcVx4b9WZ4Xe2aLjbiQUUnqJBQ=";
   };
 
   nativeBuildInputs = [
