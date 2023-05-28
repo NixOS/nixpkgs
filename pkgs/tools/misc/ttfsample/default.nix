d8e820603893ad8f99d8ca4d25c208151aba27cd
Stefan Schroeder <ondekoza@gmail.com>
2023-05-28 19:54:40 +0200

    Rename sha256 to hash in default.nix Conv tabs to spaces in nix-file Update pkgs/tools/misc/ttfsample/default.nix
    Co-authored-by: Alexander Groleau <alex@proof.construction>

--- pkgs/tools/misc/ttfsample/default.nix
+++ pkgs/tools/misc/ttfsample/default.nix
@@ -1,22 +1,25 @@
-{ buildGoModule , fetchFromGitHub , lib
+{ lib
+, buildGoModule
+, fetchFromGitHub
 }:
 buildGoModule rec {
-	pname = "ttfsample";
-	version = "0.3.0";
+  pname = "ttfsample";
+  version = "0.3.0";
 
-	src = fetchFromGitHub {
-		owner = "StefanSchroeder";
-		repo = "ttfsample";
-		rev = "${version}";
-		sha256 = "sha256-qB6n0xaBvjmBOOId4SLjc5Oi59DbtvLdSUw98AZcn0o=";
+  src = fetchFromGitHub {
+    owner = "StefanSchroeder";
+    repo = "ttfsample";
+    rev = "${version}";
+    hash = "sha256-qB6n0xaBvjmBOOId4SLjc5Oi59DbtvLdSUw98AZcn0o=";
+  };
 
-	};
-	vendorSha256 = "sha256-7kse3MYIj3Q0WL/fgR6TFmMGNQUSKuJM7+IqB83rwFY=";
-	meta = with lib; {
-		description = "Create images of fonts";
-		homepage = "https://github.com/StefanSchroeder/ttfsample";
-		license = licenses.mit;
-		maintainers = with maintainers; [ stefanschroeder ];
-		mainProgram = "ttfsample";
-	};
+  vendorSha256 = "sha256-7kse3MYIj3Q0WL/fgR6TFmMGNQUSKuJM7+IqB83rwFY=";
+
+  meta = with lib; {
+    description = "Create images of fonts";
+    homepage = "https://github.com/StefanSchroeder/ttfsample";
+    license = licenses.mit;
+    maintainers = with maintainers; [ stefanschroeder ];
+    mainProgram = "ttfsample";
+  };
 }
