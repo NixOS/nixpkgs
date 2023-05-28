5d3f60ac21a8b9f1b189b9965427c0cba2b23171
Lord-Valen <lord_valen@protonmail.com>
2023-05-28 00:06:00 -0400

    vscode-langservers-extracted: use buildNpmPackage

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17804,6 +17804,8 @@ with pkgs;
 
   verible = callPackage ../development/tools/language-servers/verible { };
 
+  vscode-langservers-extracted = callPackage ../development/tools/language-servers/vscode-langservers-extracted { };
+
   zls = callPackage ../development/tools/language-servers/zls {
     zig = buildPackages.zig_0_10;
   };
