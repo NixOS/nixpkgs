5fa7642c6cfe82e3828c11347d7b6d6f30077165
figsoda <figsoda@pm.me>
2023-05-27 20:21:15 -0400

    go-task: add version test

--- pkgs/development/tools/go-task/default.nix
+++ pkgs/development/tools/go-task/default.nix
@@ -1,4 +1,10 @@
-{ lib, buildGoModule, fetchFromGitHub, installShellFiles }:
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, installShellFiles
+, testers
+, go-task
+}:
 
 buildGoModule rec {
   pname = "go-task";
@@ -30,6 +36,12 @@ buildGoModule rec {
 
     installShellCompletion completion/{bash,fish,zsh}/*
   '';
+
+  passthru.tests = {
+    version = testers.testVersion {
+      package = go-task;
+    };
+  };
 
   meta = with lib; {
     homepage = "https://taskfile.dev/";
