4358d2ac4feaff46846bb3b908c72497e8146d50
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-29 17:05:45 +0000

    kyverno: 1.9.4 -> 1.9.5

--- pkgs/applications/networking/cluster/kyverno/default.nix
+++ pkgs/applications/networking/cluster/kyverno/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "kyverno";
-  version = "1.9.4";
+  version = "1.9.5";
 
   src = fetchFromGitHub {
     owner = "kyverno";
     repo = "kyverno";
     rev = "v${version}";
-    sha256 = "sha256-rpqhDnXxbWKa1WB7WBS6Ri7XiPWv3e0evCXFSBcaD6c=";
+    sha256 = "sha256-e/CZ5XiJFvj/HVSBPTyLybu5ZZNVte3Bf/qGhEK7k80=";
   };
 
   ldflags = [
