d72e8ab75d243bb9afbdd5a172b02acf6500a41c
Nick Cao <nickcao@nichi.co>
2023-05-29 18:33:08 +0800

    python310Packages.pytest-md-report: raise minimal python version to 3.7

--- pkgs/development/python-modules/pytest-md-report/default.nix
+++ pkgs/development/python-modules/pytest-md-report/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   version = "0.3.1";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
