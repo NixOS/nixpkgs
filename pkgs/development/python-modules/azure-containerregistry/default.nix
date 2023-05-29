4183f2f731173ce03136ce307ac2d9fe587ded84
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-29 08:19:52 +0200

    python310Packages.azure-containerregistry: update disabled
    - add format
    - add missing input

--- pkgs/development/python-modules/azure-containerregistry/default.nix
+++ pkgs/development/python-modules/azure-containerregistry/default.nix
@@ -1,13 +1,19 @@
-{ lib, buildPythonPackage, fetchPypi, pythonOlder
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pythonOlder
 , azure-core
 , msrest
 , msrestazure
+, isodate
 }:
 
 buildPythonPackage rec {
   pname = "azure-containerregistry";
   version = "1.1.0";
-  disabled = pythonOlder "3.6";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
@@ -15,13 +21,21 @@ buildPythonPackage rec {
     extension = "zip";
   };
 
-  propagatedBuildInputs = [ azure-core msrest msrestazure ];
+  propagatedBuildInputs = [
+    azure-core
+    msrest
+    msrestazure
+    isodate
+  ];
 
   # tests require azure-devtools which are not published (since 2020)
   # https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/containerregistry/azure-containerregistry/dev_requirements.txt
   doCheck = false;
 
-  pythonImportsCheck = [ "azure.core" "azure.containerregistry" ];
+  pythonImportsCheck = [
+    "azure.core"
+    "azure.containerregistry"
+  ];
 
   meta = with lib; {
     description = "Microsoft Azure Container Registry client library for Python";
