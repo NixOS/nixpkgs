6c4ae39db7c46f7d2a88f0222ab635c04a8e8170
Bernardo Meurer <bernardo@meurer.org>
2023-05-30 10:09:37 -0400

    linux: 5.4.243 -> 5.4.244
    (cherry picked from commit 46fb14a870bcd77b8564e295c2d3bd010009df3a)

--- pkgs/os-specific/linux/kernel/linux-5.4.nix
+++ pkgs/os-specific/linux/kernel/linux-5.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.4.243";
+  version = "5.4.244";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "017b1xhmjpmiq48pzzx36wn6jwwgaq2kgia51h7pxr7fxr7ndky3";
+    sha256 = "06x20aq7bv86ghv2sdsz3q2rmqh8h389x5zksr53fyzdjl72ixch";
   };
 } // (args.argsOverride or {}))
