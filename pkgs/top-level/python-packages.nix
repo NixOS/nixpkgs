d35388d349d89aaafdcbb6645a182817eec3118a
Brian Ryall <brian@brianryall.xyz>
2023-05-29 20:32:36 -0400

    mdformat-gfg:init at v0.3.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6106,6 +6106,8 @@ self: super: with self; {
 
   mdformat = callPackage ../development/python-modules/mdformat { };
 
+  mdformat-gfm = callPackage ../development/python-modules/mdformat-gfm { };
+
   mdformat-tables = callPackage ../development/python-modules/mdformat-tables { };
 
   mdit-py-plugins = callPackage ../development/python-modules/mdit-py-plugins { };
