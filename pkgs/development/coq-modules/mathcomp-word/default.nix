0f272b8b774cc294e1d2a784b283515d8f1537dc
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-05-30 06:38:32 +0200

    coqPackages.mathcomp-word: 2.0 → 2.1

--- pkgs/development/coq-modules/mathcomp-word/default.nix
+++ pkgs/development/coq-modules/mathcomp-word/default.nix
@@ -9,13 +9,14 @@ mkCoqDerivation {
 
   releaseRev = v: "v${v}";
 
+  release."2.1".sha256 = "sha256-oN/zvnePq64Nt7l/zIDLdhb4t9ayKxnZCsn+pzqS030=";
   release."2.0".sha256 = "sha256-x9AEFadlYiIIOxAhjv4Vc/dxdRZC7AdWQ6AByvLOFDk=";
   release."1.1".sha256 = "sha256:0jb28vgkr4xpg9d6k85rq7abpx5ch612iw9ps5w8q80q1jpjlc4z";
   release."1.0".sha256 = "sha256:0703m97rnivcbc7vvbd9rl2dxs6l8n52cbykynw61c6w9rhxspcg";
 
   inherit version;
   defaultVersion = with lib.versions; lib.switch [ coq.version mathcomp.version ] [
-    { cases = [ (range "8.12" "8.16") (isGe "1.12") ]; out = "2.0"; }
+    { cases = [ (range "8.12" "8.17") (isGe "1.12") ]; out = "2.1"; }
   ] null;
 
   propagatedBuildInputs = [ mathcomp.algebra mathcomp.ssreflect mathcomp.fingroup ];
