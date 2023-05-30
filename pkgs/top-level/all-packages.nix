bc7c676b07c2df1a23800b24389c2c9c60a73ba4
kilianar <mail@kilianar.de>
2023-05-30 13:17:26 +0200

    podofo010: init at 0.10.0
    https://github.com/podofo/podofo/releases/tag/0.10.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23560,6 +23560,8 @@ with pkgs;
 
   podofo = callPackage ../development/libraries/podofo { };
 
+  podofo010 = callPackage ../development/libraries/podofo/0.10.x.nix { };
+
   polkit = callPackage ../development/libraries/polkit { };
 
   poppler = callPackage ../development/libraries/poppler { lcms = lcms2; };
