899eccf6125312455bd092532a0322a4497c3ead
Sergei Trofimovich <slyich@gmail.com>
2023-05-30 07:36:49 +0100

    llvm_{10..13}: backport gcc-13 fixes
    Without the change `llvm` build fails on `gcc-13` fails as:
    
        [  0%] Building CXX object lib/Support/CMakeFiles/LLVMSupport.dir/Signals.cpp.o
        In file included from llvm/lib/Support/Signals.cpp:14:
        llvm/include/llvm/Support/Signals.h:119:8: error: variable or field 'CleanupOnSignal' declared void
          119 |   void CleanupOnSignal(uintptr_t Context);
              |        ^~~~~~~~~~~~~~~

--- pkgs/development/compilers/llvm/13/llvm/default.nix
+++ pkgs/development/compilers/llvm/13/llvm/default.nix
@@ -98,6 +98,20 @@ in stdenv.mkDerivation (rec {
       relative = "llvm";
       hash = "sha256-XPbvNJ45SzjMGlNUgt/IgEvM2dHQpDOe6woUJY+nUYA=";
     })
+
+    # Backport gcc-13 fixes with missing includes.
+    (fetchpatch {
+      name = "signals-gcc-13.patch";
+      url = "https://github.com/llvm/llvm-project/commit/ff1681ddb303223973653f7f5f3f3435b48a1983.patch";
+      hash = "sha256-CXwYxQezTq5vdmc8Yn88BUAEly6YZ5VEIA6X3y5NNOs=";
+      stripLen = 1;
+    })
+    (fetchpatch {
+      name = "base64-gcc-13.patch";
+      url = "https://github.com/llvm/llvm-project/commit/5e9be93566f39ee6cecd579401e453eccfbe81e5.patch";
+      hash = "sha256-PAwrVrvffPd7tphpwCkYiz+67szPRzRB2TXBvKfzQ7U=";
+      stripLen = 1;
+    })
   ] ++ lib.optional enablePolly ./gnu-install-dirs-polly.patch;
 
   postPatch = optionalString stdenv.isDarwin ''
