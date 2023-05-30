052d3f5abbbda4c323174146d86d834ce4fb36b0
Ivan Mincik <ivan.mincik@gmail.com>
2023-05-30 10:14:34 +0200

    python3Packages.rasterio: fix execution of Python test suite
    Remove installCheckPhase which was preventing rasterio Python test suite
    to run.

--- pkgs/development/python-modules/rasterio/default.nix
+++ pkgs/development/python-modules/rasterio/default.nix
@@ -68,12 +68,19 @@ buildPythonPackage rec {
   };
 
   nativeCheckInputs = [
+    boto3
     hypothesis
     packaging
     pytest-randomly
     pytestCheckHook
     shapely
   ];
+
+  doCheck = true;
+
+  preCheck = ''
+    rm -r rasterio # prevent importing local rasterio
+  '';
 
   pytestFlagsArray = [
     "-m 'not network'"
@@ -86,15 +93,6 @@ buildPythonPackage rec {
   pythonImportsCheck = [
     "rasterio"
   ];
-
-  doInstallCheck = true;
-
-  installCheckPhase = ''
-    $out/bin/rio --show-versions | grep -E "rasterio:\s${version}" > /dev/null
-    $out/bin/rio --show-versions | grep -E "GDAL:\s[0-9]+(\.[0-9]+)*" > /dev/null
-    $out/bin/rio --show-versions | grep -E "PROJ:\s[0-9]+(\.[0-9]+)*" > /dev/null
-    $out/bin/rio --show-versions | grep -E "GEOS:\s[0-9]+(\.[0-9]+)*" > /dev/null
-  '';
 
   meta = with lib; {
     description = "Python package to read and write geospatial raster data";
