20f476e5a2cf32de996fa84f8d8234b13b6ebe86
hugosenari <hugosenari@gmail.com>
2023-05-30 17:20:32 -0300

    lib.simple-options: fix type in default

--- lib/default.nix
+++ lib/default.nix
@@ -32,7 +32,7 @@ let
     # module system
     modules = callLibs ./modules.nix;
     options = callLibs ./options.nix;
-    simple-options = callLibs ./simple-options.nix;
+    simpleOptions = callLibs ./simple-options.nix;
     types = callLibs ./types.nix;
 
     # constants
