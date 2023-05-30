fece92c99f88bdb6a54bda5d1028c89c569af848
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-31 00:07:11 +0200

    python311Packages.hdfs: add changelog to meta

--- pkgs/development/python-modules/hdfs/default.nix
+++ pkgs/development/python-modules/hdfs/default.nix
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "mtth";
     repo = pname;
-    rev = version;
+    rev = "refs/tags/${version}";
     hash = "sha256-94Q3IUoX1Cb+uRqvsfpVZJ1koJSx5cQ3/XpYJ0gkQNU=";
   };
 
@@ -33,6 +33,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python API and command line interface for HDFS";
     homepage = "https://github.com/mtth/hdfs";
+    changelog = "https://github.com/mtth/hdfs/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ samuela ];
   };
