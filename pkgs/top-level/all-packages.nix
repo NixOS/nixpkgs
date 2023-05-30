27c01909e4559feb8858cd7c014c90e4b3b87362
QJoly <github@thoughtless.eu>
2023-05-30 08:10:04 +0200

    Subtitlr: init at 0.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1705,6 +1705,8 @@ with pkgs;
 
   shell-genie = callPackage  ../applications/misc/shell-genie { };
 
+  subtitlr = callPackage  ../applications/audio/subtitlr { };
+
   simple-dlna-browser = callPackage ../tools/networking/simple-dlna-browser { };
 
   sorted-grep = callPackage ../tools/text/sorted-grep { };
