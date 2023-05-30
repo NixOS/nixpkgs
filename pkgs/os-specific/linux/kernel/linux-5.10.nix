887b93a67009831d2ad14a599867a7f7fafdfa07
Bernardo Meurer <bernardo@meurer.org>
2023-05-30 10:09:30 -0400

    linux: 5.10.180 -> 5.10.181
    (cherry picked from commit 83f8f4d9beb8c1819b72e162f4a2462d5931d29e)

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.10.180";
+  version = "5.10.181";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "0a8cicvcyl5w4vi7gxhgd59ny44gj9cbv4z5pnwn9jgny55rm0ys";
+    sha256 = "1rx43dkxspris9529vl11blzhvsxnpaqr6yb3fy089az7yvwxrmc";
   };
 } // (args.argsOverride or {}))
