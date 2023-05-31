ba2187e55eeebe202c8717c5fd476b568bc63904
R. Ryantm <ryantm-bot@ryantm.com>
2023-05-31 03:24:07 +0000

    minizip-ng: 3.0.10 -> 4.0.0

--- pkgs/development/libraries/minizip-ng/default.nix
+++ pkgs/development/libraries/minizip-ng/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "minizip-ng";
-  version = "3.0.10";
+  version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = "zlib-ng";
     repo = finalAttrs.pname;
     rev = finalAttrs.version;
-    sha256 = "sha256-ynYAWF570S6MpD1WXbUC3cu+chL3+AhsMHr15l+LYVg=";
+    sha256 = "sha256-YgBOsznV1JtnpLUJeqZ06zvdB3tNbOlFhhLd1pMJhEM=";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
