2be777b47fa533d3ac07bf40c49a3eda014546da
Enno Richter <enno@nerdworks.de>
2023-05-31 08:07:43 +0200

    python3Packages.pyxlsb: init at 1.0.10

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10634,6 +10634,8 @@ self: super: with self; {
 
   pyxattr = callPackage ../development/python-modules/pyxattr { };
 
+  pyxlsb = callPackage ../development/python-modules/pyxlsb { };
+
   pyworld = callPackage ../development/python-modules/pyworld { };
 
   pyx = callPackage ../development/python-modules/pyx { };
