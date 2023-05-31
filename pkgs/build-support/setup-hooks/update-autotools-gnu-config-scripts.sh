6aa237df5e7504f0a3787dc37e0cab0146aa74ee
Sergei Trofimovich <slyich@gmail.com>
2023-05-31 08:45:31 +0100

    updateAutotoolsGnuConfigScriptsHook: preserve original timestamps
    Without the change updates to `config.guess` and `config.sub` sometimes
    triggers extra rebuilds of artifacts. Ane xample is `libtool` manpages
    during the bootstrap on RiscV:
    
        help2man: can't get `--help' info from libtoolize
        Try `--no-discard-stderr' if option outputs to stderr
        WARNING: 'help2man' is missing on your system.
                 You should only need it if you modified a dependency of a man page.
                 You may want to install the GNU Help2man package:
                 <https://www.gnu.org/software/help2man/>
        make[2]: *** [Makefile:2430: doc/libtoolize.1] Error 127
        make[2]: *** Waiting for unfinished jobs....
    
    Here `libtool` is expected to use prebuilt manpages that come with
    release tarball. But as one of it's dependencies was updated by
    `updateAutotoolsGnuConfigScriptsHook` hook it now pulls in a missing
    extra dependency.
    
    To preserve build system behaviour let's copy timestamps of original
    files as is.

--- pkgs/build-support/setup-hooks/update-autotools-gnu-config-scripts.sh
+++ pkgs/build-support/setup-hooks/update-autotools-gnu-config-scripts.sh
@@ -6,7 +6,14 @@ updateAutotoolsGnuConfigScriptsPhase() {
     for script in config.sub config.guess; do
         for f in $(find . -type f -name "$script"); do
             echo "Updating Autotools / GNU config script to a newer upstream version: $f"
-            cp -f "@gnu_config@/$script" "$f"
+            cp "@gnu_config@/$script" "$f.new"
+            if [ -e "$f" ]; then
+                # Preserve timestamps to avoid unexpected regeneration
+                # of the build system. For example `libtool` calls
+                # `help2man` when `config.guess` updates.
+                touch -r "$f" "$f.new"
+            fi
+            mv -f "$f.new" "$f"
         done
     done
 }
