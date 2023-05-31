767579a88a354b320a6e321cf98193c6d5dfad6b
Fabian Affolter <mail@fabian-affolter.ch>
2023-05-31 08:54:12 +0200

    otpclient: add changelog to meta

--- pkgs/applications/misc/otpclient/default.nix
+++ pkgs/applications/misc/otpclient/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "paolostivanin";
     repo = pname;
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     sha256 = "sha256-TklVOUkdhWDG9GqHl0Sz9fah+Xp/M8xgSuDB1q4mljM=";
   };
 
@@ -35,6 +35,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Highly secure and easy to use OTP client written in C/GTK that supports both TOTP and HOTP";
     homepage = "https://github.com/paolostivanin/OTPClient";
+    changelog = "https://github.com/paolostivanin/OTPClient/releases/tag/v${version}";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ alexbakker ];
     platforms = platforms.linux;
