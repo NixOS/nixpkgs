310b22327011fc0a5cbb3f576f2d2d1a02483760
Aaron Jheng <wentworth@outlook.com>
2023-05-31 06:36:54 +0000

    mysql80: unpin protobuf

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26031,7 +26031,6 @@ with pkgs;
     inherit (darwin) cctools developer_cmds DarwinTools;
     inherit (darwin.apple_sdk.frameworks) CoreServices;
     boost = boost177; # Configure checks for specific version.
-    protobuf = protobuf3_19;
     icu = icu69;
   };
 
