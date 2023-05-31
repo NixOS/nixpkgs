dd099cedf161007f8e2025ebcd0a427c3058d115
Mario Rodas <marsam@users.noreply.github.com>
2023-05-31 04:20:00 +0000

    postgresqlPackages.pgroonga: 3.0.3 -> 3.0.5
    https://github.com/pgroonga/pgroonga/releases/tag/3.0.5

--- pkgs/servers/sql/postgresql/ext/pgroonga.nix
+++ pkgs/servers/sql/postgresql/ext/pgroonga.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "pgroonga";
-  version = "3.0.3";
+  version = "3.0.5";
 
   src = fetchurl {
     url = "https://packages.groonga.org/source/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-DFSvF6/PfBjho6r63V1fdwap+8rr1W9KOGZIR2COXJc=";
+    hash = "sha256-5cVfZk2bFop/EQjvfObiN/AF/iv3DqDIKb/BkbEyKbE=";
   };
 
   nativeBuildInputs = [ pkg-config ];
