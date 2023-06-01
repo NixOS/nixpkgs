cc6b3cfb2a3237eff66e64702b52d5d78371d3c6
hugosenari <hugosenari@gmail.com>
2023-05-31 22:41:30 -0300

    lib.simpleOptions: function to simplify options declaration
    > A bit more readable, plus this saves some . lookups where you would otherwise have to write (builtins.), leaving only the double array index that would otherwise be spent on builtins anyway.
    > Extend the list of identifiers to remove the other lookups as well.
    by @roberth

--- lib/simple-options.nix
+++ lib/simple-options.nix
@@ -9,80 +9,82 @@
 }:
 module:
 let
+  inherit (builtins) attrNames concatStringsSep hasAttr mapAttrs throw trace typeOf;
+  inherit (lib.types) attrsOf listOf submodule;
   mkOptionW = breadcrumb: optName: optDef:
     let result = lib.mkOption (toOption breadcrumb optName optDef);
     in
       if debug then
-        builtins.trace ''
-          function: modulazy.mkOptionWrapper
+        trace ''
+          function: simple-options.mkOptionWrapper
           args:
-            breadcrumb: ${builtins.concatStringsSep "." breadcrumb}
+            breadcrumb: ${concatStringsSep "." breadcrumb};
             optName: ${optName}
-            optDef: ${builtins.concatStringsSep " " (builtins.attrNames optDef)}
-          result: ${builtins.concatStringsSep " " (builtins.attrNames result)}
+            optDef: ${concatStringsSep " " (attrNames optDef)}
+          result: ${concatStringsSep " " (attrNames result)}
         ''
         result
       else result;
 
-  toOptions = breadcrumb: options: { options = builtins.mapAttrs (mkOptionW breadcrumb) options; };
+  toOptions = breadcrumb: options: { options = mapAttrs (mkOptionW breadcrumb) options; };
 
   toType    = breadcrumb: type:
     let result =
-      if builtins.typeOf breadcrumb != "list"
-        then builtins.throw ''breadcrumb must be a list, instead it is a ${builtins.typeOf breadcrumb}'' else
-      if builtins.typeOf type       != "set"
-        then builtins.throw ''type must be a attrset, instead it is a ${builtins.typeOf type}, breadcrumb: ${builtins.concatStringsSep "." breadcrumb}'' else
-      if builtins.hasAttr "_type" type
+      if typeOf breadcrumb != "list"
+        then throw ''breadcrumb must be a list, instead it is a ${typeOf breadcrumb}'' else
+      if typeOf type       != "set"
+        then throw ''type must be a attrset, instead it is a ${typeOf type}, breadcrumb: ${concatStringsSep "." breadcrumb}'' else
+      if hasAttr "_type" type
         then type else
-      if builtins.hasAttr typeAttr type
+      if hasAttr typeAttr type
         then toType type.type else
-      if builtins.hasAttr attrAttr type
-        then lib.types.attrsOf   (toType    (breadcrumb ++ [attrAttr]) type."${attrAttr}") else
-      if builtins.hasAttr listAttr type
-        then lib.types.listOf    (toType    (breadcrumb ++ [listAttr]) type."${listAttr}") else
-      if builtins.hasAttr subMAttr type
-        then lib.types.submodule (toOptions (breadcrumb ++ [subMAttr]) type."${subMAttr}")
+      if hasAttr attrAttr type
+        then attrsOf   (toType    (breadcrumb ++ [attrAttr]) type."${attrAttr}") else
+      if hasAttr listAttr type
+        then listOf    (toType    (breadcrumb ++ [listAttr]) type."${listAttr}") else
+      if hasAttr subMAttr type
+        then submodule (toOptions (breadcrumb ++ [subMAttr]) type."${subMAttr}")
       else
-        builtins.throw ''${builtins.concatStringsSep "." breadcrumb} has no attr _type|${typeAttr}|${attrAttr}|${listAttr}|${subMAttr}'';
+        throw ''${concatStringsSep "." breadcrumb} has no attr _type|${typeAttr}|${attrAttr}|${listAttr}|${subMAttr}'';
     in
       if debug then
-        builtins.trace ''
-          function: modulazy.toType
+        trace ''
+          function: simple-options.toType
           args:
-            breadcrumb: ${builtins.concatStringsSep "." breadcrumb}
-            type: ${builtins.concatStringsSep " " (builtins.attrNames type)}
-          result: ${builtins.concatStringsSep " " (builtins.attrNames result)}
+            breadcrumb: ${concatStringsSep "." breadcrumb}
+            type: ${concatStringsSep " " (attrNames type)}
+          result: ${concatStringsSep " " (attrNames result)}
         ''
         result
       else result;
 
   toOption  = breadcrumb: optName: optDef:
     let result =
-      if builtins.typeOf breadcrumb != "list"
-        then builtins.throw ''breadcrumb must be a list, instead it is a ${builtins.typeOf breadcrumb}'' else
-      if builtins.typeOf optName    != "string"
-        then builtins.throw ''optName must be a string, instead it is a ${builtins.typeOf optName}, breadcrumb: ${builtins.concatStringsSep "." breadcrumb}'' else
-      if builtins.typeOf optDef     != "set"
-        then builtins.throw ''optDef must be a attrset, instead it is a ${builtins.typeOf optDef}, breadcrumb: ${builtins.concatStringsSep "." breadcrumb}, optName: ${optName}'' else
-      if builtins.hasAttr typeAttr optDef
+      if typeOf breadcrumb != "list"
+        then throw ''breadcrumb must be a list, instead it is a ${typeOf breadcrumb}'' else
+      if typeOf optName    != "string"
+        then throw ''optName must be a string, instead it is a ${typeOf optName}, breadcrumb: ${concatStringsSep "." breadcrumb}'' else
+      if typeOf optDef     != "set"
+        then throw ''optDef must be a attrset, instead it is a ${typeOf optDef}, breadcrumb: ${concatStringsSep "." breadcrumb}, optName: ${optName}'' else
+      if hasAttr typeAttr optDef
         then optDef else
-      if builtins.hasAttr attrAttr optDef
-        then builtins.removeAttrs optDef [attrAttr] // { type = lib.types.attrsOf   (toType    (breadcrumb ++ [optName attrAttr]) optDef."${attrAttr}");} else
-      if builtins.hasAttr listAttr optDef
-        then builtins.removeAttrs optDef [listAttr] // { type = lib.types.listOf    (toType    (breadcrumb ++ [optName listAttr]) optDef."${listAttr}");} else
-      if builtins.hasAttr subMAttr optDef
-        then builtins.removeAttrs optDef [subMAttr] // { type = lib.types.submodule (toOptions (breadcrumb ++ [optName subMAttr]) optDef."${subMAttr}");}
+      if hasAttr attrAttr optDef
+        then removeAttrs optDef [attrAttr] // { type = attrsOf   (toType    (breadcrumb ++ [optName attrAttr]) optDef."${attrAttr}");} else
+      if hasAttr listAttr optDef
+        then removeAttrs optDef [listAttr] // { type = listOf    (toType    (breadcrumb ++ [optName listAttr]) optDef."${listAttr}");} else
+      if hasAttr subMAttr optDef
+        then removeAttrs optDef [subMAttr] // { type = submodule (toOptions (breadcrumb ++ [optName subMAttr]) optDef."${subMAttr}");}
       else
-        builtins.throw ''${builtins.concatStringsSep "." (breadcrumb ++ optName)} has no attr _type|${typeAttr}|${attrAttr}|${listAttr}|${subMAttr}'';
+        throw ''${concatStringsSep "." (breadcrumb ++ optName)} has no attr _type|${typeAttr}|${attrAttr}|${listAttr}|${subMAttr}'';
     in
       if debug then
-        builtins.trace ''
-          function: modulazy.toOption
+        trace ''
+          function: simple-options.toOption
           args:
-            breadcrumb: ${builtins.concatStringsSep "." breadcrumb}
+            breadcrumb: ${concatStringsSep "." breadcrumb}
             optName: ${optName}
-            optDef: ${builtins.concatStringsSep " " (builtins.attrNames optDef)}
-          result: ${builtins.concatStringsSep " " (builtins.attrNames result)}
+            optDef: ${concatStringsSep " " (attrNames optDef)}
+          result: ${concatStringsSep " " (attrNames result)}
         ''
         result
       else result;
