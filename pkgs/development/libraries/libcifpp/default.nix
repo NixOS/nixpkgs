8544e240d8eb06b00250650fb6858df4c49d4fbf
natsukium <tomoya.otabi@gmail.com>
2023-06-01 11:07:05 +0900

    libcifpp: 5.0.8 -> 5.1.0.1
    Diff: https://github.com/PDB-REDO/libcifpp/compare/v5.0.8...v5.1.0.1

--- pkgs/development/libraries/libcifpp/default.nix
+++ pkgs/development/libraries/libcifpp/default.nix
@@ -1,22 +1,30 @@
-{ lib, stdenv, fetchFromGitHub, boost, cmake, zlib }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, boost, cmake, zlib, eigen }:
 
 stdenv.mkDerivation rec {
   pname = "libcifpp";
-  version = "5.0.8";
+  version = "5.1.0.1";
 
   src = fetchFromGitHub {
     owner = "PDB-REDO";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-KJGcopGhCWSl+ElG3BPJjBf/kvYJowOHxto6Ci1IMco=";
+    hash = "sha256-fAH7FIgJuitPUoacLnLs8uf9di5iM0c/2WHZqVjJOUE=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "add-include-compare.patch";
+      url = "https://github.com/PDB-REDO/libcifpp/commit/676c0c8dc87437e2096718fd8c0750b995e174ba.patch";
+      hash = "sha256-fbA4fgiTY93+hFct+BQuHF7uv2nG7D9PljQxq1CkReU=";
+    })
+  ];
 
   nativeBuildInputs = [ cmake ];
 
   # disable network access
   cmakeFlags = [ "-DCIFPP_DOWNLOAD_CCD=OFF" ];
 
-  buildInputs = [ boost zlib ];
+  buildInputs = [ boost zlib eigen ];
 
   meta = with lib; {
     description = "Manipulate mmCIF and PDB files";
