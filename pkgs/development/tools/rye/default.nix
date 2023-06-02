0066f2c7500db3aebf7c6cc2a2741f38f1fc157a
Alexander Koval <kovalidis@gmail.com>
2023-06-02 18:56:00 +0300

    rye: unstable-2023-04-23 -> 0.5.0

--- pkgs/development/tools/rye/default.nix
+++ pkgs/development/tools/rye/default.nix
@@ -1,6 +1,8 @@
 { lib
-, fetchFromGitHub
+, fetchgit
 , rustPlatform
+, bashInteractive
+, git
 , openssl
 , pkg-config
 , stdenv
@@ -9,23 +11,39 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rye";
-  version = "unstable-2023-04-23";
+  version = "0.5.0";
 
-  src = fetchFromGitHub {
-    owner = "mitsuhiko";
-    repo = "rye";
-    rev = "b3fe43a4e462d10784258cad03c19c9738367346";
-    hash = "sha256-q9/VUWyrP/NsuLYY1+/5teYvDJGq646WbMXptnUUUyw=";
+  src = fetchgit {
+    url = "https://github.com/mitsuhiko/${pname}.git";
+    rev = version;
+    # One of the tests runs git command so we need .git directory there
+    leaveDotGit = true;
+    sha256 = "lTE3UzmHzzRBxqLl5zngZyuF5KcCDGqxOGGUODhZAlA=";
   };
 
-  cargoHash = "sha256-eyJ6gXFVnSC1aEt5YLl5rFoa3+M73smu5wJdAN15HQM=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "dialoguer-0.10.4" = "sha256-WDqUKOu7Y0HElpPxf2T8EpzAY3mY8sSn9lf0V0jyAFc=";
+    };
+  };
+
+  # Remove the patch after the next update: https://github.com/mitsuhiko/rye/pull/275
+  patches = [ ./offline-licenses.patch ];
+
+  env = {
+    OPENSSL_NO_VENDOR = 1;
+  };
 
   nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [
+    git
     openssl
   ]
   ++ lib.optional stdenv.isDarwin SystemConfiguration;
+
+  nativeCheckInputs = [ git ];
 
   meta = with lib; {
     description = "A tool to easily manage python dependencies and environments";
