4acd9c04d645076eda9bae71b16e84a9f10dea5a
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-06-03 01:01:44 +0200

    python310Packages.python-daemon: 2.3.0 -> 3.0.1
    https://pagure.io/python-daemon/blob/067759fb4c802275d6d3b7dc34115ffb69bb79d5/f/ChangeLog

--- pkgs/development/python-modules/python-daemon/default.nix
+++ pkgs/development/python-modules/python-daemon/default.nix
@@ -1,54 +1,49 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+
+# build-system
 , docutils
+, packaging
+, setuptools
+
+# dependencies
 , lockfile
+
+# tests
 , pytestCheckHook
+, pythonOlder
 , testscenarios
 , testtools
-, twine
-, python
-, pythonOlder
-, fetchpatch
 }:
 
 buildPythonPackage rec {
   pname = "python-daemon";
-  version = "2.3.0";
-  format = "setuptools";
+  version = "3.0.1";
+  format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "bda993f1623b1197699716d68d983bb580043cf2b8a66a01274d9b8297b0aeaf";
+    hash = "sha256-bFdFI3L36v9Ak0ocA60YJr9eeTVY6H/vSRMeZGS02uU=";
   };
 
   nativeBuildInputs = [
-    twine
+    setuptools
+    packaging
+    docutils
   ];
 
   propagatedBuildInputs = [
-    docutils
     lockfile
+    setuptools
   ];
 
   nativeCheckInputs = [
     pytestCheckHook
     testscenarios
     testtools
-  ];
-
-  patches = [
-    # Should be fixed in the next release
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/python-daemon/raw/rawhide/f/python-daemon-safe_hasattr.patch";
-      hash = "sha256-p5epAlM/sdel01oZkSI1vahUZYX8r90WCJuvBnfMaus=";
-    })
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/python-daemon/raw/rawhide/f/tests-remove-duplicate-mocking.patch";
-      hash = "sha256-5b/dFR3Z8xaPw8AZU95apDZd4ZfmMQhAmavWkVaJog8=";
-    })
   ];
 
   disabledTestPaths = [
@@ -77,7 +72,6 @@ buildPythonPackage rec {
     "daemon"
     "daemon.daemon"
     "daemon.pidfile"
-    "daemon.runner"
   ];
 
   meta = with lib; {
