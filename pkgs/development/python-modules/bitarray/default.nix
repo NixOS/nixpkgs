6ddd842a4a90a700a6f997befd5f1d241f57873d
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-03 23:39:28 +0200

    python310Packages.bitarray: 2.7.3 -> 2.7.4
    Changelog: https://github.com/ilanschnell/bitarray/raw/2.7.4/CHANGE_LOG

--- pkgs/development/python-modules/bitarray/default.nix
+++ pkgs/development/python-modules/bitarray/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "bitarray";
-  version = "2.7.3";
+  version = "2.7.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-9xJWoyYJsDatrZMuEii2amtOLK5r45fliN3Aur2aeLk=";
+    hash = "sha256-FD1PZeH0WlM+E1Ib4dxVengjF+z3ZSDqvVqQOybssYc=";
   };
 
   checkPhase = ''
