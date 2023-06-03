be21cf8b9912abddf23a044d7caf1a3d18af5483
Robert Scott <code@humanleg.org.uk>
2023-06-03 19:43:17 +0100

    podofo: add patches for CVE-2023-2241, CVE-2023-31555, CVE-2023-31556 & CVE-2023-31568

--- pkgs/development/libraries/podofo/default.nix
+++ pkgs/development/libraries/podofo/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , cmake
 , expat
 , fontconfig
@@ -33,6 +34,34 @@ stdenv.mkDerivation (finalAttrs: {
     # remove any possibility it may try to use these
     rm -r extern/deps
   '';
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2023-2241.patch";
+      url = "https://github.com/podofo/podofo/commit/535a786f124b739e3c857529cecc29e4eeb79778.patch";
+      hash = "sha256-c2Ae9eOxSvKzzoyOZw4j5xBjj6SM5gmfq2dwnpbgMBg=";
+    })
+    (fetchpatch {
+      name = "CVE-2023-31555.patch";
+      url = "https://github.com/podofo/podofo/commit/3759eb6aae7c01f2d8670f16ac46f5e116c7f468.patch";
+      hash = "sha256-r726T58TEPwEicM+DFgOliIvyzDqYwlQNCd0z6P0cZI=";
+    })
+    (fetchpatch {
+      name = "CVE-2023-31556.part-1.patch";
+      url = "https://github.com/podofo/podofo/commit/657c46c1e192b23f6903a5c57f52b7bdc2c597ef.patch";
+      hash = "sha256-VGZ5vgjBtzFcLR+Oi90/2iXgHpwvpTt1yXGKBXNoqps=";
+    })
+    (fetchpatch {
+      name = "CVE-2023-31556.part-2.patch";
+      url = "https://github.com/podofo/podofo/commit/8d3e9104ea10f8b53a0b5a2a806e6388acd41a40.patch";
+      hash = "sha256-UHfRiBzNcJdkcDE5iXi0wN78YfGiUyMqpqXDDpItRaE=";
+    })
+    (fetchpatch {
+      name = "CVE-2023-31568.patch";
+      url = "https://github.com/podofo/podofo/commit/29d59f604b37159e938a2f46acd4856cfd1e7bac.patch";
+      hash = "sha256-Pwm5jyNYiDyzZaPMejl9REQXu588q3fatNZv0JVuE9E=";
+    })
+  ];
 
   outputs = [ "out" "dev" "lib" ];
 
