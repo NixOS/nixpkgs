1dbc3047591787a4b8cd4001124ee0a3389d77d4
Vladimír Čunát <v@cunat.cz>
2023-06-04 20:46:53 +0200

    google-cloud-cpp: extend .meta.platforms
    The default is x86_64-linux only, but we build many packages
    for aarch64-linux on Hydra that depend on this (possibly indirectly),
    and it can be cumbersome when a dependency doesn't have its own job.

--- pkgs/development/libraries/google-cloud-cpp/default.nix
+++ pkgs/development/libraries/google-cloud-cpp/default.nix
@@ -126,6 +126,7 @@ stdenv.mkDerivation rec {
     license = with licenses; [ asl20 ];
     homepage = "https://github.com/googleapis/google-cloud-cpp";
     description = "C++ Idiomatic Clients for Google Cloud Platform services";
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
     maintainers = with maintainers; [ cpcloud ];
   };
 }
