7e9f117e72e9b860f2a9d9c04fc2007240c039f1
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-04 13:03:20 +0000

    ocenaudio: 3.11.25 -> 3.12.2

--- pkgs/applications/audio/ocenaudio/default.nix
+++ pkgs/applications/audio/ocenaudio/default.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ocenaudio";
-  version = "3.11.25";
+  version = "3.12.2";
 
   src = fetchurl {
     url = "https://www.ocenaudio.com/downloads/index.php/ocenaudio_debian9_64.deb?version=${version}";
-    sha256 = "sha256-B14xM4/E6TQZGLZifvIFA4JxLPo0hNah9PIyquS9TzI=";
+    sha256 = "sha256-AzQAEU5o3m1cFCxeyT32AzX4WUk3DliBPdw+cfQPyKU=";
   };
 
   nativeBuildInputs = [
