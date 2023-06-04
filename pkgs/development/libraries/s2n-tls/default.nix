6ce8a3e5fa02607838a292a79181584fe3ab32c9
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-04 17:03:11 +0000

    s2n-tls: 1.3.44 -> 1.3.45

--- pkgs/development/libraries/s2n-tls/default.nix
+++ pkgs/development/libraries/s2n-tls/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "s2n-tls";
-  version = "1.3.44";
+  version = "1.3.45";
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-8YF9PhxTrXQBTUJvTrJZFDVijQecTeZ1ayGuvQRqGEE=";
+    sha256 = "sha256-ZipZXCwat3H0NoUX9T6XB7/qMRvViB2QrYpgj0U41nU=";
   };
 
   nativeBuildInputs = [ cmake ];
