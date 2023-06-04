e635ba6b59ead3792dbb6910df58418e17f25a73
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-04 10:21:30 +0200

    python310Packages.python-nomad: disable on unsupported Python releases
    - add format
    - equalize

--- pkgs/development/python-modules/python-nomad/default.nix
+++ pkgs/development/python-modules/python-nomad/default.nix
@@ -1,20 +1,32 @@
-{ lib, buildPythonPackage, fetchPypi, requests }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, requests
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "python-nomad";
   version = "2.0.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-5IyHNw1ArE8fU9DoSQMGkDI9d/OiR1YI/7nTPeFIK+A=";
   };
 
-  propagatedBuildInputs = [ requests ];
+  propagatedBuildInputs = [
+    requests
+  ];
 
   # Tests require nomad agent
   doCheck = false;
 
-  pythonImportsCheck = [ "nomad" ];
+  pythonImportsCheck = [
+    "nomad"
+  ];
 
   meta = with lib; {
     description = "Python client library for Hashicorp Nomad";
