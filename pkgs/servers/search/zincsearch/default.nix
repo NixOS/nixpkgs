03c659190515317c5164ae6239a9556b6c1ac12a
Mostly Void <dit7ya@users.noreply.github.com>
2023-06-05 04:22:20 +0530

    zincsearch: 0.3.5 -> 0.4.5, rename from zinc (#218017)
    * zincsearch: 0.3.5 -> 0.4.5, rename from zinc
    
    Renamed existing zinc package to zincsearch following upstream.
    
    * Update pkgs/top-level/aliases.nix
    
    ---------
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/servers/search/zincsearch/default.nix
+++ pkgs/servers/search/zincsearch/default.nix
@@ -3,13 +3,14 @@
 , fetchFromGitHub
 , buildNpmPackage
 }:
+
 let
-  version = "0.3.6";
+  version = "0.4.5";
   src = fetchFromGitHub {
     owner = "zinclabs";
-    repo = "zinc";
+    repo = "zincsearch";
     rev = "v${version}";
-    sha256 = "sha256-7pPVX/jdHN7EMss6/uRZqJO+zDfDLZv/iG5iboB+s64=";
+    hash = "sha256-V4eI7cur+KI+PEcplQJdTBaT0czpN0ENN30W5rrzE+k=";
   };
 
   webui = buildNpmPackage {
@@ -18,9 +19,11 @@ let
 
     sourceRoot = "source/web";
 
-    npmDepsHash = "sha256-clRijS+hxWc1LwlAKjEEk/6XPBYC6CcLq5g/ry4a04g=";
+    npmDepsHash = "sha256-2AjUaEOn2Tj+X4f42SvNq1kX07WxkB1sl5KtGdCjbdw=";
 
-    CYPRESS_INSTALL_BINARY = 0; # cypress tries to download binaries otherwise
+    env = {
+      CYPRESS_INSTALL_BINARY = 0; # cypress tries to download binaries otherwise
+    };
 
     installPhase = ''
       mkdir -p $out/share
@@ -28,28 +31,27 @@ let
     '';
   };
 in
+
 buildGoModule rec {
-  pname = "zinc";
+  pname = "zincsearch";
   inherit src version;
 
   preBuild = ''
     cp -r ${webui}/share/zinc-ui web/dist
   '';
 
-  vendorHash = "sha256-akjb0cxHbITKS26c+7lVSHWO/KRoQVVKzAOra+tdAD8=";
-  subPackages = [ "cmd/zinc" ];
-
-  CGO_ENABLED = 0;
+  vendorHash = "sha256-TYs1iRUA4WC3iMS508c7jUC3Co86CKYjL5WFlnoyPhg=";
+  subPackages = [ "cmd/zincsearch" ];
 
   ldflags = [
     "-s"
     "-w"
-    "-X github.com/zinclabs/zinc/pkg/meta.Version=${version}"
+    "-X github.com/zinclabs/zincsearch/pkg/meta.Version=${version}"
   ];
 
   meta = with lib; {
     description = "A lightweight alternative to elasticsearch that requires minimal resources, written in Go";
-    homepage = "https://github.com/zinclabs/zinc";
+    homepage = "https://zinc.dev";
     license = licenses.asl20;
     maintainers = with maintainers; [ dit7ya ];
   };
