e061b7cf31bc00f0daed1be870cefbfbf064cfa8
LoveIsGrief <loveisgrief@tuta.io>
2023-06-04 17:11:41 +0200

    zkfuse: Add description
    Couldn't find a homepage for it as it seems to be within the sourcetree of zookeeper

--- pkgs/tools/filesystems/zkfuse/default.nix
+++ pkgs/tools/filesystems/zkfuse/default.nix
@@ -34,6 +34,8 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
+    description = "ZooKeeper FUSE (File System in Userspace)";
+    homepage = "https://zookeeper.apache.org";
     platforms = platforms.linux;
     maintainers = with maintainers; [ cstrahan ztzg ];
     license = licenses.asl20;
