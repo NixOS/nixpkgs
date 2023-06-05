986fd3f68190d7aeffa9545f5fb45e9130cdde67
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-05 09:41:06 +0200

    python311Packages.hass-nabucasa: 0.66.2 -> 0.67.1
    Diff: https://github.com/nabucasa/hass-nabucasa/compare/refs/tags/0.66.2...0.67.1
    
    Changelog: https://github.com/NabuCasa/hass-nabucasa/releases/tag/0.67.1

--- pkgs/development/python-modules/hass-nabucasa/default.nix
+++ pkgs/development/python-modules/hass-nabucasa/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "hass-nabucasa";
-  version = "0.66.2";
+  version = "0.67.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "nabucasa";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-LlVT5WRd2uhUaghThJ5ghPbX40QjqTenUC4txMx3Jlo=";
+    hash = "sha256-/cu7+fMbe9kyWGGBGBwjsiA6PXoUJZ7IFzpEcnVSLn0=";
   };
 
   postPatch = ''
