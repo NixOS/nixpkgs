ec951383d5aeaf16e340c79cba6f257640549d64
Bobby Rong <rjl931189261@126.com>
2023-06-05 23:38:56 +0800

    cinnamon.mint-cursor-themes: 1.0.1 -> 1.0.2
    https://github.com/linuxmint/mint-cursor-themes/compare/e17f8a46...d2c1428b

--- pkgs/desktops/cinnamon/mint-cursor-themes/default.nix
+++ pkgs/desktops/cinnamon/mint-cursor-themes/default.nix
@@ -5,14 +5,14 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "mint-cursor-themes";
-  version = "1.0.1";
+  version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     # They don't really do tags, this is just a named commit.
-    rev = "e17f8a4620827235dabbe5221bd0ee8c44dad0d5";
-    hash = "sha256-yLUmIVh884uDVkNil7qxf6t/gykipzBvPgzwmY3zvQk=";
+    rev = "d2c1428b499a347c291dafb13c89699fdbdd4be7";
+    hash = "sha256-i2Wf+OKwal9G5hkcAdmGSgX6txu1AHajqqPJdhpJoA0=";
   };
 
   installPhase = ''
