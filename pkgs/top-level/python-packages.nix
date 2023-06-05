9e3a7611460825f134af228854bb66d81c7bf295
Tomas Kala <tomas.kala@blindspot.ai>
2023-06-05 15:42:46 +0200

    Do not override protobuf-{core,python}

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9837,8 +9837,6 @@ in {
     inherit (pkgs.darwin.apple_sdk.frameworks) Foundation Security;
     flatbuffers-core = pkgs.flatbuffers;
     flatbuffers-python = self.flatbuffers;
-    protobuf-core = pkgs.protobuf;
-    protobuf-python = self.protobuf;
     lmdb-core = pkgs.lmdb;
   };
 
