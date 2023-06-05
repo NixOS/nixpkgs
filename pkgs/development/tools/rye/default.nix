78c229b1a694ff992574943a591035cd23882fcb
Alexander Koval <kovalidis@gmail.com>
2023-06-05 14:40:09 +0300

    Update rye to 0.6.0

--- pkgs/development/tools/rye/default.nix
+++ pkgs/development/tools/rye/default.nix
@@ -11,14 +11,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rye";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchgit {
     url = "https://github.com/mitsuhiko/${pname}.git";
     rev = version;
     # One of the tests runs git command so we need .git directory there
     leaveDotGit = true;
-    sha256 = "lTE3UzmHzzRBxqLl5zngZyuF5KcCDGqxOGGUODhZAlA=";
+    sha256 = "llm4aqMfaVf3VbHudWjb9V2GFgJpP9S211Ui7xXdrAU=";
   };
 
   cargoLock = {
@@ -27,9 +27,6 @@ rustPlatform.buildRustPackage rec {
       "dialoguer-0.10.4" = "sha256-WDqUKOu7Y0HElpPxf2T8EpzAY3mY8sSn9lf0V0jyAFc=";
     };
   };
-
-  # Remove the patch after the next update: https://github.com/mitsuhiko/rye/pull/275
-  patches = [ ./offline-licenses.patch ];
 
   env = {
     OPENSSL_NO_VENDOR = 1;
