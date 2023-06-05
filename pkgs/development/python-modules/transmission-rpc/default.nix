c6f2bbd97a3178c2d7ec3e3eb21edb24e31c3923
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-05 09:25:57 +0200

    python311Packages.transmission-rpc: 4.2.2 -> 4.3.0
    Diff: https://github.com/Trim21/transmission-rpc/compare/refs/tags/v4.2.2...v4.3.0
    
    Changelog: https://github.com/trim21/transmission-rpc/releases/tag/v4.3.0

--- pkgs/development/python-modules/transmission-rpc/default.nix
+++ pkgs/development/python-modules/transmission-rpc/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "transmission-rpc";
-  version = "4.2.2";
+  version = "4.3.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "Trim21";
     repo = "transmission-rpc";
     rev = "refs/tags/v${version}";
-    hash = "sha256-8gNGBfU7k2SvpNqRGkT9BXyAyKizUCXY8Unuqw5IICE=";
+    hash = "sha256-kcrPUBpx8puhG4de55tDGDmOnJPAOZzr+zC5gRd8b0s=";
   };
 
   nativeBuildInputs = [
