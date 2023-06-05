07961f5341722ac039f0cb7168cd78941301643a
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-05 19:53:21 +0000

    python310Packages.jupyterlab-lsp: 4.0.1 -> 4.2.0

--- pkgs/development/python-modules/jupyterlab-lsp/default.nix
+++ pkgs/development/python-modules/jupyterlab-lsp/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "jupyterlab-lsp";
-  version = "4.0.1";
+  version = "4.2.0";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-1VPRfs+F24h2xJeoJglZQpuCcPDk6Ptf8cWrAW3G5to=";
+    hash = "sha256-OqsByMrAQKjTqev6QIUiOwVLf71iGdPHtWD2qXZsovM=";
   };
 
   propagatedBuildInputs = [
