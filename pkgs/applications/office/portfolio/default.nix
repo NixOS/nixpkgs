e323e15244f675efc486c48cc94506c6a1bd4e9d
Shawn8901 <shawn8901@googlemail.com>
2023-06-05 22:45:07 +0200

    portfolio: 0.62.1 -> 0.63.1

--- pkgs/applications/office/portfolio/default.nix
+++ pkgs/applications/office/portfolio/default.nix
@@ -27,11 +27,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "PortfolioPerformance";
-  version = "0.62.1";
+  version = "0.63.1";
 
   src = fetchurl {
     url = "https://github.com/buchen/portfolio/releases/download/${version}/PortfolioPerformance-${version}-linux.gtk.x86_64.tar.gz";
-    hash = "sha256-tENX2F+kZSrltvaZm3ZaUld3es8Bt8LCpEVGxAOFVm0=";
+    hash = "sha256-zMlRxj10NS47Q88UWM1s0WvhhQXS4I00JyzmigyYOmE=";
   };
 
   nativeBuildInputs = [
