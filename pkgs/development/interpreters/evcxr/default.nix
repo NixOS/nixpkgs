f4d46a749e5700342eace78eec05e75e4f85eb14
figsoda <figsoda@pm.me>
2023-06-05 15:47:35 -0400

    evcxr: fix build with rust 1.70

--- pkgs/development/interpreters/evcxr/default.nix
+++ pkgs/development/interpreters/evcxr/default.nix
@@ -20,11 +20,15 @@ rustPlatform.buildRustPackage rec {
   buildInputs = lib.optionals stdenv.isDarwin
     [ libiconv CoreServices Security ];
 
-  # test broken with rust 1.69:
-  # * https://github.com/evcxr/evcxr/issues/294
-  # * https://github.com/NixOS/nixpkgs/issues/229524
   checkFlags = [
+    # test broken with rust 1.69:
+    # * https://github.com/evcxr/evcxr/issues/294
+    # * https://github.com/NixOS/nixpkgs/issues/229524
     "--skip=check_for_errors"
+
+    # test broken with rust 1.70
+    # * https://github.com/evcxr/evcxr/issues/295
+    "--skip=partially_inferred_variable_type"
   ];
 
   postInstall = let
