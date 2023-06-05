9059ba30824b6e0c3a122c5721a977f082d937c1
Janik H <janik@aq0.de>
2023-06-05 18:06:12 +0200

    python3Packages.django-leaflet: init at 0.29.0

---
+++ pkgs/development/python-modules/django-leaflet/default.nix
@@ -0,0 +1,38 @@
+{ pkgs
+, buildPythonPackage
+, django
+, fetchPypi
+, pythonOlder
+}:
+
+buildPythonPackage rec {
+  pname = "django-leaflet";
+  version = "0.29.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-L23Ixxh/0i5itvK3tC7thpIPgb7DEqplSYHr5byOCGY=";
+  };
+
+  propagatedBuildInputs = [
+    django
+  ];
+
+  # The tests seem to be impure.
+  # They are throwing a error about unset configs:
+  # > django.core.exceptions.ImproperlyConfigured: Requested setting LEAFLET_CONFIG, but settings are not configured. You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.
+  doCheck = false;
+
+  # This dosn't work either because of the same exception as above
+  # pythonImportsCheck = [ "leaflet" ];
+
+  meta = with pkgs.lib; {
+    description = "Allows you to use Leaflet in your Django projects";
+    homepage = "https://github.com/makinacorpus/django-leaflet";
+    license = licenses.lgpl3Only;
+    maintainers = with maintainers; [ janik ];
+  };
+}
