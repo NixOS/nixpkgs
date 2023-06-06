5e932befc8bd304cbb41e791295b0ef63702a13d
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-06-06 13:34:03 -0400

    python3Packages.duckdb-engine: 0.7.0 -> 0.7.3

--- pkgs/development/python-modules/duckdb-engine/default.nix
+++ pkgs/development/python-modules/duckdb-engine/default.nix
@@ -7,13 +7,14 @@
 , hypothesis
 , ipython-sql
 , poetry-core
+, snapshottest
 , sqlalchemy
 , typing-extensions
 }:
 
 buildPythonPackage rec {
   pname = "duckdb-engine";
-  version = "0.7.0";
+  version = "0.7.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +23,7 @@ buildPythonPackage rec {
     repo = "duckdb_engine";
     owner = "Mause";
     rev = "refs/tags/v${version}";
-    hash = "sha256-qLQjFkud9DivLQ9PignLrXlUVOAxsd28s7+2GdC5jKE=";
+    hash = "sha256-Z9m1+Bc/csWKdPDuwf82xX0qOiD1Y5LBgJjUlLntAO8=";
   };
 
   nativeBuildInputs = [
@@ -38,15 +39,19 @@ buildPythonPackage rec {
     export HOME="$(mktemp -d)"
   '';
 
-  # this test tries to download the httpfs extension
   disabledTests = [
+    # this test tries to download the httpfs extension
     "test_preload_extension"
+    # test should be skipped based on sqlalchemy version but isn't and fails
+    "test_commit"
   ];
 
   nativeCheckInputs = [
     pytestCheckHook
     hypothesis
     ipython-sql
+    # TODO(cpcloud): include pandas here when it supports sqlalchemy 2.0
+    snapshottest
     typing-extensions
   ];
 
