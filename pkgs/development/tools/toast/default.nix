4fd08ff51134e3c4d30d58ec32c6de6f2da40809
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-06 05:22:32 +0000

    toast: 0.47.3 -> 0.47.4

--- pkgs/development/tools/toast/default.nix
+++ pkgs/development/tools/toast/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "toast";
-  version = "0.47.3";
+  version = "0.47.4";
 
   src = fetchFromGitHub {
     owner = "stepchowfun";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-rPS0jLlfZFeSHY/zdD1mAPm+00UPJAQuPnK7/hCHeGg=";
+    sha256 = "sha256-9uIZPmvjRjR9rRVp+rfLEjp6yDmf+87OglKqOwlRSEQ=";
   };
 
-  cargoHash = "sha256-zCM9h4sJlfeXKyNy5LWrPaAmo2+/um0WSoJcYchYa/E=";
+  cargoHash = "sha256-cO2mO7ZtFuoIs58Y53xb4q+Cr5V94WTEgYkOYB0aGvY=";
 
   checkFlags = [ "--skip=format::tests::code_str_display" ]; # fails
 
