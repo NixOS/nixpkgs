6e192c4489ea44c48876faa11060798fb9502146
Jared Baur <jaredbaur@fastmail.com>
2023-06-06 16:37:46 -0700

    nixos/activation: Add pre-switch checks
    Add an option for shell script fragments that are ran before switching
    to a new NixOS system configuration (pre installation of bootloader or
    system activation). Also add a new subcommand for
    switch-to-configuration called "check" that will cause the program to
    always exit after checks are ran.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1612,6 +1612,7 @@
   ./services/x11/xserver.nix
   ./system/activation/activatable-system.nix
   ./system/activation/activation-script.nix
+  ./system/activation/pre-switch-check.nix
   ./system/activation/specialisation.nix
   ./system/activation/switchable-system.nix
   ./system/activation/bootspec.nix
