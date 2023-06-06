4a1b535b25d00be8320266746781e458a7efc991
K900 <me@0upti.me>
2023-06-06 18:35:43 +0300

    linux: 5.15.114 -> 5.15.115
    (cherry picked from commit d77a40536993382b483addb82af3b3e57957b90a)

--- pkgs/os-specific/linux/kernel/linux-5.15.nix
+++ pkgs/os-specific/linux/kernel/linux-5.15.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.15.114";
+  version = "5.15.115";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "1lkpa9wv1qj90qdzzi71qf5dyy7mi95fixx3ymdp6xwz45fym0g9";
+    sha256 = "050j9z0wg5glgxwbmzfq4l8lv8y7g3nc2rz8342yjdcjfxh6h1qv";
   };
 } // (args.argsOverride or { }))
