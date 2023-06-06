d5e01b29b28621528f6428deabfdd5d42390676a
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-06 09:39:45 +0200

    grype: 0.62.2 -> 0.62.3
    Diff: https://github.com/anchore/grype/compare/refs/tags/v0.62.2...v0.62.3
    
    Changelog: https://github.com/anchore/grype/releases/tag/v0.62.3

--- pkgs/tools/security/grype/default.nix
+++ pkgs/tools/security/grype/default.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "grype";
-  version = "0.62.2";
+  version = "0.62.3";
 
   src = fetchFromGitHub {
     owner = "anchore";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-11uxu9oa0lSZH+zd+Uj2P9flw9wp9UD7bXKP4jxXDoE=";
+    hash = "sha256-GZj0grVqpIE+BwCUpcDnoa7Lnm+FXu4wNbfINA9uuzQ=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -28,7 +28,7 @@ buildGoModule rec {
 
   proxyVendor = true;
 
-  vendorHash = "sha256-n0daAFCP0KtS1mQGfq/EQv2m6c8jjdbFR6YWSeClHBg=";
+  vendorHash = "sha256-pK8Gse/Qb+Z3QHEq35gB72Lvsm7ot4HmH62sxhVopv8=";
 
   nativeBuildInputs = [
     installShellFiles
