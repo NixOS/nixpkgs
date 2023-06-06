cb4163d13e6fa8b9d502e54addb11b9cf9dd268e
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-06 22:43:39 +0200

    python311Packages.sqltrie: 0.3.1 -> 0.4.0
    Diff: https://github.com/iterative/sqltrie/compare/refs/tags/0.3.1...0.4.0
    
    Changelog: https://github.com/iterative/sqltrie/releases/tag/0.4.0

--- pkgs/development/python-modules/sqltrie/default.nix
+++ pkgs/development/python-modules/sqltrie/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "sqltrie";
-  version = "0.3.1";
+  version = "0.4.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "iterative";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-e/3Tq2H9I0zvqq0+q1c3nbz2UiMNX7JpVpVC9suvYRM=";
+    hash = "sha256-GNTp7ryll3V0nmaLIfr3E8krRLcLwFviZfLI0LK1Na0=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
