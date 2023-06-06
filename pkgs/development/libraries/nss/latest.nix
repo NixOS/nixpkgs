6dc9422e647ef38ed679f6b2c17ebe3957200c3e
ajs124 <git@ajs124.de>
2023-06-06 14:21:57 +0200

    nss: 3.89.1 -> 3.90
    https://github.com/nss-dev/nss/blob/NSS_3_90_BRANCH/doc/rst/releases/nss_3_90.rst
    (cherry picked from commit 871fd1b210130a903985d61444c8a7903d3b3b8d)

--- pkgs/development/libraries/nss/latest.nix
+++ pkgs/development/libraries/nss/latest.nix
@@ -5,6 +5,6 @@
 #       Example: nix-shell ./maintainers/scripts/update.nix --argstr package cacert
 
 import ./generic.nix {
-  version = "3.89.1";
-  hash = "sha256-OtrtuecMPF9AYDv2CgHjNhkKbb4Bkp05XxawH+hKAVY=";
+  version = "3.90";
+  hash = "sha256-ms1lNMQdjq0Z/Kb8s//+0vnwnEN8PXn+5qTuZoqqk7Y=";
 }
