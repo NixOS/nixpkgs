99f75fb74636bc77b8ef57b862157393680b446c
K900 <me@0upti.me>
2023-06-06 18:36:00 +0300

    linux: 6.3.5 -> 6.3.6

--- pkgs/os-specific/linux/kernel/linux-6.3.nix
+++ pkgs/os-specific/linux/kernel/linux-6.3.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.3.5";
+  version = "6.3.6";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "0pl2zypsmrnna69850jadccffxwg9xdfkldg0sv8m44b7n64gkgm";
+    sha256 = "1vr418rf0zd16fq4vaxda1w2ccng2n9l0ysakmgz9xqbz86iysks";
   };
 } // (args.argsOverride or { }))
