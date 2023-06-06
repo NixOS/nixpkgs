342c6ce3c9d0d14f5bde254f3cb671bbe9e05fb6
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-06-06 12:06:56 -0400

    python3Packages.sqlglot: 10.5.2 -> 15.0.0

--- pkgs/development/python-modules/sqlglot/default.nix
+++ pkgs/development/python-modules/sqlglot/default.nix
@@ -5,11 +5,11 @@
 , pytestCheckHook
 , python-dateutil
 , duckdb
-, pyspark
+, setuptools-scm
 }:
 buildPythonPackage rec {
   pname = "sqlglot";
-  version = "10.5.2";
+  version = "15.0.0";
 
   disabled = pythonOlder "3.8";
 
@@ -17,12 +17,17 @@ buildPythonPackage rec {
     repo = "sqlglot";
     owner = "tobymao";
     rev = "v${version}";
-    hash = "sha256-ZFc2aOhCTRFlrzgnYDSdIZxRqKZ8FvkYSZRU0OMHI34=";
+    hash = "sha256-F95JUYUgimWMyfPAV55e+bOcvWy5A95djKcJuSQ8RRs=";
   };
 
+  SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+  nativeBuildInputs = [ setuptools-scm ];
+
+  # optional dependency used in the sqlglot optimizer
   propagatedBuildInputs = [ python-dateutil ];
 
-  nativeCheckInputs = [ pytestCheckHook duckdb pyspark ];
+  nativeCheckInputs = [ pytestCheckHook duckdb ];
 
   # these integration tests assume a running Spark instance
   disabledTestPaths = [ "tests/dataframe/integration" ];
