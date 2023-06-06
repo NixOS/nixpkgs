89bd325e3d8a56c330812b415cee55b376358e1d
K900 <me@0upti.me>
2023-06-06 18:35:54 +0300

    linux: 6.1.31 -> 6.1.32

--- pkgs/os-specific/linux/kernel/linux-6.1.nix
+++ pkgs/os-specific/linux/kernel/linux-6.1.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.1.31";
+  version = "6.1.32";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "1hbkw290kmf1dj8a255ik1gk5fk458c88m348dwrc3lrl6xifsg8";
+    sha256 = "0v0saai735jzaj3dbpgp6sbdi95n3lmv78pbg1xy9fd2kfhbg23w";
   };
 } // (args.argsOverride or { }))
