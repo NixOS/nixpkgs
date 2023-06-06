011e5ba1b2972f230a9d2662445e8232600dc754
Niols <niols@niols.fr>
2023-06-06 14:09:06 +0200

    ocamlPackages.tdigest: init at 2.1.1

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1577,6 +1577,8 @@ let
 
     tcslib = callPackage ../development/ocaml-modules/tcslib { };
 
+    tdigest = callPackage ../development/ocaml-modules/tdigest { };
+
     telegraml = callPackage ../development/ocaml-modules/telegraml { };
 
     terminal = callPackage ../development/ocaml-modules/terminal { };
