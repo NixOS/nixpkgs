fc1d09c5549f9dc420986f30f19cbc4b8b36dc37
Sandro <sandro.jaeckel@gmail.com>
2023-06-06 10:27:47 +0200

    Update pkgs/tools/compression/pigz/default.nix

--- pkgs/tools/compression/pigz/default.nix
+++ pkgs/tools/compression/pigz/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitHub {
       owner = "madler";
-      repo = "${pname}";
+      repo = pname;
       rev = "refs/tags/v${version}";
       sha256 = "sha256-RYp3vRwlI6S/lcib+3t7qLYFWv11GUnj1Cmxm9eaVro=";
   };
