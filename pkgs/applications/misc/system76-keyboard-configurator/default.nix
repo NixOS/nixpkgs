df90284f1dfda4ed78cb9911318d681bb6ac49ac
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-06 02:55:10 +0000

    system76-keyboard-configurator: 1.3.3 -> 1.3.4

--- pkgs/applications/misc/system76-keyboard-configurator/default.nix
+++ pkgs/applications/misc/system76-keyboard-configurator/default.nix
@@ -6,13 +6,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "system76-keyboard-configurator";
-  version = "1.3.3";
+  version = "1.3.4";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "keyboard-configurator";
     rev = "v${version}";
-    sha256 = "sha256-8Mb07OlmYl/dNxCdBrAq7mgXZvi0oqtt76UX8TMWUPY=";
+    sha256 = "sha256-RaJOtlnjxYiA6WrKRmIG3McAzc35zR3uMScLy2kLx98=";
   };
 
   nativeBuildInputs = [
@@ -28,7 +28,7 @@ rustPlatform.buildRustPackage rec {
     udev
   ];
 
-  cargoHash = "sha256-3IAljoL4cabZ9rpgqPrgG7ofwETHS/9OlBKjxTwCDTU=";
+  cargoHash = "sha256-qoOaLDzJ9WqCnI/kBSiu6DZx1HWU8Cd8ch6VVcgIfB4=";
 
   meta = with lib; {
     description = "Keyboard configuration application for System76 keyboards and laptops";
