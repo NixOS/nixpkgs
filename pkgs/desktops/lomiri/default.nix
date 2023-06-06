cd3383596b2837ea168f7daf83f407b77e12aef3
OPNA2608 <christoph.neidahl@gmail.com>
2023-06-06 23:49:11 +0200

    lomiri.lomiri-api: init at 0.2.0

--- pkgs/desktops/lomiri/default.nix
+++ pkgs/desktops/lomiri/default.nix
@@ -9,6 +9,7 @@ let
   in {
     #### Development tools / libraries
     cmake-extras = callPackage ./development/cmake-extras { };
+    lomiri-api = callPackage ./development/lomiri-api { };
   };
 in
   lib.makeScope libsForQt5.newScope packages
