6cf4161be53e12950c49206f0802f225016dc2ad
Tomas Drtina <tdrtina-ext@arista.com>
2023-06-06 20:11:27 +0200

    ycmd: unpin abseil-cpp

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19758,7 +19758,6 @@ with pkgs;
   ycmd = callPackage ../development/tools/misc/ycmd {
     inherit (darwin.apple_sdk.frameworks) Cocoa;
     python = python3;
-    abseil-cpp = abseil-cpp_202103;
   };
 
   yodl = callPackage ../development/tools/misc/yodl { };
