79892d9c4f4e787f36a6dab08b54fc26c8ecbe71
OPNA2608 <christoph.neidahl@gmail.com>
2023-06-06 23:52:49 +0200

    lomiri.gmenuharness: init at 0.1.4

--- pkgs/desktops/lomiri/default.nix
+++ pkgs/desktops/lomiri/default.nix
@@ -9,6 +9,7 @@ let
   in {
     #### Development tools / libraries
     cmake-extras = callPackage ./development/cmake-extras { };
+    gmenuharness = callPackage ./development/gmenuharness { };
     lomiri-api = callPackage ./development/lomiri-api { };
   };
 in
