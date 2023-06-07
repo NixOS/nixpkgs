6bf61a6b83680e54524119d0a7ad27dcacf45bdf
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-06-07 21:31:06 +0200

    python310Packages.xiaomi-ble: 0.17.1 -> 0.17.2
    https://github.com/Bluetooth-Devices/xiaomi-ble/releases/tag/v0.17.2

--- pkgs/development/python-modules/xiaomi-ble/default.nix
+++ pkgs/development/python-modules/xiaomi-ble/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "xiaomi-ble";
-  version = "0.17.1";
+  version = "0.17.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-CioXmv5VJBiJx97k69Sp5kaeD59p+OSL14RVse/gYUA=";
+    hash = "sha256-2zS34feelJNVarfe8GEZxBNL45IK+Owl6TlBDOTVvSs=";
   };
 
   nativeBuildInputs = [
