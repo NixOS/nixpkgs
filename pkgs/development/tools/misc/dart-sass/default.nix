1cc5d0e6980efa95b45317978f5e0b54803657d4
lelgenio <lelgenio@disroot.org>
2023-06-07 20:49:08 -0300

    dart-sass: 1.62.1 -> 1.63.3

--- pkgs/development/tools/misc/dart-sass/default.nix
+++ pkgs/development/tools/misc/dart-sass/default.nix
@@ -1,21 +1,42 @@
 { lib
 , fetchFromGitHub
 , buildDartApplication
+, buf
+, protoc-gen-dart
 }:
 
+let
+  sass-language = fetchFromGitHub {
+    owner = "sass";
+    repo = "sass";
+    rev = "refs/tags/embedded-protocol-2.0.0";
+    hash = "sha256-3qk3XbI/DpNj4oa/3ar5hqEY8LNmQsokinuKt4xV7ck=";
+  };
+in
 buildDartApplication rec {
   pname = "dart-sass";
-  version = "1.62.1";
+  version = "1.63.3";
 
   src = fetchFromGitHub {
     owner = "sass";
     repo = pname;
     rev = version;
-    hash = "sha256-U6enz8yJcc4Wf8m54eYIAnVg/jsGi247Wy8lp1r1wg4=";
+    hash = "sha256-PBFBepG7b5M9LKSQOp7+mVhtwSLgQzUTu3i1055uNBk=";
   };
 
   pubspecLockFile = ./pubspec.lock;
-  vendorHash = "sha256-Atm7zfnDambN/BmmUf4BG0yUz/y6xWzf0reDw3Ad41s=";
+  vendorHash = "sha256-ciJUjkItnwzKH6k8a5zxIbARQAEfg/GJEsKfSaygsXU=";
+
+  nativeBuildInputs = [
+    buf
+    protoc-gen-dart
+  ];
+
+  preConfigure = ''
+    mkdir -p build
+    ln -s ${sass-language} build/language
+    HOME="$TMPDIR" buf generate
+  '';
 
   dartCompileFlags = [ "--define=version=${version}" ];
 
