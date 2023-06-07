d793bb2de2f4d5ac48d6ab3df7f1b8036ec3c650
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-08 00:01:53 +0200

    past-time: add changelog to meta
    - add format

--- pkgs/tools/misc/past-time/default.nix
+++ pkgs/tools/misc/past-time/default.nix
@@ -10,11 +10,12 @@
 buildPythonApplication rec {
   pname = "past-time";
   version = "0.2.1";
+  format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "fabaff";
     repo = pname;
-    rev = version;
+    rev = "refs/tags/${version}";
     sha256 = "0yhc0630rmcx4ia9y6klpx002mavfmqf1s3jb2gz54jlccwqbfgl";
   };
 
@@ -28,11 +29,14 @@ buildPythonApplication rec {
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "past_time" ];
+  pythonImportsCheck = [
+    "past_time"
+  ];
 
   meta = with lib; {
     description = "Tool to visualize the progress of the year based on the past days";
     homepage = "https://github.com/fabaff/past-time";
+    changelog = "https://github.com/fabaff/past-time/releases/tag/${version}";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ fab ];
   };
