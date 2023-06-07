3df4355d262941b0926a934fc75c7e58f9388543
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-07 09:33:01 +0200

    python311Packages.pylast: update disabled

--- pkgs/development/python-modules/pylast/default.nix
+++ pkgs/development/python-modules/pylast/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   version = "5.2.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "pylast";
