c12fc39c0ade8f6e82cfb8eb45d0d94e3c750649
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-06-07 13:04:51 -0700

    cargo-deb: 1.42.2 -> 1.44.0

--- pkgs/development/tools/rust/cargo-deb/default.nix
+++ pkgs/development/tools/rust/cargo-deb/default.nix
@@ -2,25 +2,36 @@
 , rustPlatform
 , fetchFromGitHub
 , makeWrapper
+, binutils
 , dpkg
 }:
-
+let
+  # cargo-deb expects "strip" and "objcopy" from binutils to be on the PATH.
+  binutilsBinPath = lib.makeBinPath [ binutils ];
+in
 rustPlatform.buildRustPackage rec {
   pname = "cargo-deb";
-  version = "1.42.2";
+  version = "1.44.0";
 
   src = fetchFromGitHub {
     owner = "kornelski";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-s/piZ8sCdBz5zFW9i7xdVrf7dQiMjQp/ixCDjFh5SLc=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-1gA8r/4VMiBWTU0Q0rlSxANt9oK3nYx/cqq0WxvNNZs=";
   };
 
-  cargoHash = "sha256-4iJghmSXsaijNCvYyrM3dEsqCDk6zeTU92oP5Qs6tOY=";
+  cargoHash = "sha256-Fg0KkAqcjAGFEDaAge4KFAFBBfVSGE6lIoewDcLEFug=";
 
   nativeBuildInputs = [
     makeWrapper
   ];
+
+  preCheck = ''
+    export PATH="${binutilsBinPath}":$PATH
+
+    substituteInPlace tests/command.rs \
+      --replace '"stripped executable should be smaller than debug executable"' '"stripped executable {} should be smaller than debug executable {}", stripped.metadata().unwrap().len(), debug.metadata().unwrap().len()'
+  '';
 
   # This is an FHS specific assert depending on glibc location
   checkFlags = [
@@ -29,7 +40,8 @@ rustPlatform.buildRustPackage rec {
 
   postInstall = ''
     wrapProgram $out/bin/cargo-deb \
-      --prefix PATH : ${lib.makeBinPath [ dpkg ]}
+      --prefix PATH : "${lib.makeBinPath [ dpkg ]}" \
+      --prefix PATH : "${binutilsBinPath}"
   '';
 
   meta = with lib; {
