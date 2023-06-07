568b71889cbc3344489d5c3b8cf075f33f855c29
Anderson Torres <torres.anderson.85@protonmail.com>
2023-06-06 23:12:15 -0300

    Revert "shavee: move to games/doom-ports"
    This reverts commit 602aea50a14dd6f0259fe6e7ac43e121791aab11.
    
    This reverts a mistake made when moving some expressions to doom-ports.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34157,6 +34157,8 @@ with pkgs;
 
   shadowfox = callPackage ../tools/networking/shadowfox { };
 
+  shavee = callPackage ../applications/misc/shavee { };
+
   shell_gpt = callPackage ../tools/misc/shell_gpt { };
 
   shfmt = callPackage ../tools/text/shfmt { };
@@ -36230,8 +36232,6 @@ with pkgs;
   prboom-plus = callPackage ../games/doom-ports/prboom-plus { };
 
   rbdoom-3-bfg = callPackage ../games/doom-ports/rbdoom-3-bfg { };
-
-  shavee = callPackage ../games/doom-ports/shavee { };
 
   slade = callPackage ../games/doom-ports/slade {
     wxGTK = wxGTK32.override {
