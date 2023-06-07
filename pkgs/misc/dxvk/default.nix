bb4e8cfb52e3999fa7401591397a554eec928a20
Sergei Trofimovich <slyich@gmail.com>
2023-06-07 23:43:37 +0100

    pkgsCross.mingwW64.windows.mcfgthreads: add gcc13 compatible version
    Upstream `gcc-13` merged `mcfgthreads` support with a caveat: it's
    headers interface is not compatible with the patch `nixpkgs` was
    carrying in `gcc-12` and before.
    
    To keep both new (`gcc13`) and old (`_pre_gcc13`) version I held back
    previous `windows.mcfgthreads` attribute as
    `windows.mcfgthreads_pre_gcc_13`. It is used for `gcc` before 13.
    
    The change fixes the build of `pkgsCross.mingwW64.stdenv` itself and
    example program:
    
        $ nix build --impure --expr 'with import ./. {}; pkgsCross.mingwW64.re2c.override { stdenv = pkgsCross.mingwW64.gcc11Stdenv; }'

--- pkgs/misc/dxvk/default.nix
+++ pkgs/misc/dxvk/default.nix
@@ -27,8 +27,8 @@ stdenvNoCC.mkDerivation (finalAttrs:
         --subst-var-by bash ${bash} \
         --subst-var-by dxvk32 ${dxvk32} \
         --subst-var-by dxvk64 ${dxvk64} \
-        --subst-var-by mcfgthreads32 "${pkgsCross.mingw32.windows.mcfgthreads}" \
-        --subst-var-by mcfgthreads64 "${pkgsCross.mingwW64.windows.mcfgthreads}"
+        --subst-var-by mcfgthreads32 "${pkgsCross.mingw32.windows.mcfgthreads_pre_gcc_13}" \
+        --subst-var-by mcfgthreads64 "${pkgsCross.mingwW64.windows.mcfgthreads_pre_gcc_13}"
       chmod a+x $out/bin/setup_dxvk.sh
       declare -A dxvks=( [x32]=${dxvk32} [x64]=${dxvk64} )
       for arch in "''${!dxvks[@]}"; do
