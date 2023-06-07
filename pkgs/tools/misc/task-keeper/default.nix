94db23a7a6bc02e66e17eb38fc62b44b3f084c44
Manu [tennox] <tennox+git@txlab.io>
2023-06-07 20:35:42 +0100

    task-keeper: add Cargo.lock and use upstream
    I realized that this pattern is well-known and works :)
    better than using a fork

--- pkgs/tools/misc/task-keeper/default.nix
+++ pkgs/tools/misc/task-keeper/default.nix
@@ -5,15 +5,15 @@ rustPlatform.buildRustPackage rec {
   version = "0.12.2";
 
   src = fetchFromGitHub {
-    # owner = "linux-china"; # upstream, depends on pending PR: https://github.com/linux-china/task-keeper/pull/2
-    owner = "tennox"; # my fork
+    owner = "linux-china";
     repo = "task-keeper";
     rev = "55bf587871de49cd9ac703e42218e970749a689a";
     hash = "sha256-P6RUENjK2UQ5/utW2umBTVnahf1xvbvLa3PMaD/2B6I=";
   };
 
   cargoLock = {
-    lockFile = "${src}/Cargo.lock";
+    # lockFile = "${src}/Cargo.lock"; # pending PR: https://github.com/linux-china/task-keeper/pull/2
+    lockFile = ./Cargo.lock;
   };
   doCheck = false; # tests depend on many packages (java, node, python, sbt, ...) - which I'm not currently willing to set up 😅
 
