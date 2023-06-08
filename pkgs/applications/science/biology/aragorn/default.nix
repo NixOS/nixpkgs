bb317093e86714c78a21f57f2480da513544eb8f
natsukium <tomoya.otabi@gmail.com>
2023-06-08 12:15:17 +0900

    aragorn: 1.2.38 -> 1.2.41
    - change the source and homepage
    - change the license

--- pkgs/applications/science/biology/aragorn/default.nix
+++ pkgs/applications/science/biology/aragorn/default.nix
@@ -1,27 +1,39 @@
-{ lib, stdenv, fetchurl }:
+{ lib, stdenv, fetchurl, installShellFiles }:
+let
+  man = fetchurl {
+    url = "https://web.archive.org/web/20230608093053if_/http://www.ansikte.se/ARAGORN/Downloads/aragorn.1";
+    hash = "sha256-bjD22dpkQZcGR0TwMxdpaed4VZZO2NUOoAw4o66iyS4=";
+  };
+in
 
 stdenv.mkDerivation rec {
-  version = "1.2.38";
+  version = "1.2.41";
   pname = "aragorn";
 
   src = fetchurl {
-    url = "http://mbio-serv2.mbioekol.lu.se/ARAGORN/Downloads/${pname}${version}.tgz";
-    sha256 = "09i1rg716smlbnixfm7q1ml2mfpaa2fpn3hwjg625ysmfwwy712b";
+    url = "http://www.ansikte.se/ARAGORN/Downloads/aragorn${version}.c";
+    hash = "sha256-kqMcxcCwrRbU17AZkZibd18H0oFd8TX+bj6riPXpf0o=";
   };
 
+  dontUnpack = true;
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
   buildPhase = ''
-    $CC -O3 -ffast-math -finline-functions -o aragorn aragorn${version}.c
+    $CC -O3 -ffast-math -finline-functions -o aragorn $src
   '';
 
   installPhase = ''
     mkdir -p $out/bin && cp aragorn $out/bin
-    mkdir -p $out/man/1 && cp aragorn.1 $out/man/1
+    installManPage ${man}
   '';
 
   meta = with lib; {
     description = "Detects tRNA, mtRNA, and tmRNA genes in nucleotide sequences";
-    homepage = "http://mbio-serv2.mbioekol.lu.se/ARAGORN/";
-    license = licenses.gpl2;
+    homepage = "http://www.ansikte.se/ARAGORN/";
+    license = licenses.gpl3Plus;
     maintainers = [ maintainers.bzizou ];
     platforms = platforms.unix;
   };
