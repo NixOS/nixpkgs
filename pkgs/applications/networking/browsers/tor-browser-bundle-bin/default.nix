939969f44c5cb843bc7c9231d9fafbb70d4696eb
Felix Schröter <dev@felschr.com>
2023-06-08 20:02:12 +0200

    tor-browser-bundle-bin: remove inactive maintainers

--- pkgs/applications/networking/browsers/tor-browser-bundle-bin/default.nix
+++ pkgs/applications/networking/browsers/tor-browser-bundle-bin/default.nix
@@ -469,7 +469,7 @@ stdenv.mkDerivation rec {
     homepage = "https://www.torproject.org/";
     changelog = "https://gitweb.torproject.org/builders/tor-browser-build.git/plain/projects/tor-browser/Bundle-Data/Docs/ChangeLog.txt?h=maint-${version}";
     platforms = attrNames sources;
-    maintainers = with maintainers; [ offline matejc thoughtpolice joachifm hax404 KarlJoad ];
+    maintainers = with maintainers; [ joachifm hax404 ];
     mainProgram = "tor-browser";
     # MPL2.0+, GPL+, &c.  While it's not entirely clear whether
     # the compound is "libre" in a strict sense (some components place certain
