abb7c05860f00dda7bcf13b91c6573d98a8fb094
Bruno Bigras <bigras.bruno@gmail.com>
2023-06-08 01:32:51 -0400

    zrok: support most of the arches

--- pkgs/tools/networking/zrok/default.nix
+++ pkgs/tools/networking/zrok/default.nix
@@ -1,14 +1,32 @@
 { stdenv, lib, fetchzip, patchelf }:
 
+let
+  inherit (stdenv.hostPlatform) system;
+  throwSystem = throw "Unsupported system: ${system}";
+
+  plat = {
+    x86_64-linux = "linux_amd64";
+    aarch64-linux = "linux_arm64";
+    armv7l-linux = "linux_armv7";
+  }.${system} or throwSystem;
+
+  sha256 = {
+    x86_64-linux = "sha256-gcmgpvfk7bciTmotTHObvZvLPdLudAR2vQneLKN+uE4=";
+    aarch64-linux = "sha256-cZDekIW2Z7hfIY6Y4xhsmgvMLnKYo6H9BAMg9/I5a10=";
+    armv7l-linux = "sha256-Fn2e1ywuTUt58geT4hNb3YEZiJDE6TUKaKI/xpsG69c=";
+  }.${system} or throwSystem;
+in
 stdenv.mkDerivation rec {
   pname = "zrok";
   version = "0.3.6";
 
   src = fetchzip {
-    url = "https://github.com/openziti/zrok/releases/download/v${version}/zrok_${version}_linux_amd64.tar.gz";
+    url = "https://github.com/openziti/zrok/releases/download/v${version}/zrok_${version}_${plat}.tar.gz";
     stripRoot = false;
-    sha256 = "sha256-gcmgpvfk7bciTmotTHObvZvLPdLudAR2vQneLKN+uE4=";
+    inherit sha256;
   };
+
+  updateScript = ./update.sh;
 
   installPhase = let
     interpreter = "$(< \"$NIX_CC/nix-support/dynamic-linker\")";
@@ -23,7 +41,7 @@ stdenv.mkDerivation rec {
     description = "Geo-scale, next-generation sharing platform built on top of OpenZiti";
     homepage = "https://zrok.io";
     maintainers = [ lib.maintainers.bandresen ];
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "aarch64-linux" "armv7l-linux" ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     license = lib.licenses.apsl20;
   };
