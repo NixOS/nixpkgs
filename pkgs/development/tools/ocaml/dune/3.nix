b617c67c786bc0ef5493884bf751312a3b26ef2c
Ali Caglayan <alizter@gmail.com>
2023-06-08 08:18:37 +0200

    dune_3: 3.7.1 -> 3.8.1 (#233595)
    Signed-off-by: Ali Caglayan <alizter@gmail.com>
    Co-authored-by: Weijia Wang <9713184+wegank@users.noreply.github.com>

--- pkgs/development/tools/ocaml/dune/3.nix
+++ pkgs/development/tools/ocaml/dune/3.nix
@@ -6,11 +6,11 @@ else
 
 stdenv.mkDerivation rec {
   pname = "dune";
-  version = "3.7.1";
+  version = "3.8.1";
 
   src = fetchurl {
     url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-    sha256 = "sha256-rfw48UwBiKKtgNYUUdAR0nq4g5txdJLXrUL3y5EcVMM=";
+    sha256 = "sha256-lBOl1uudeWigRj3rudnxvnMCU0WAm4J5eNDBTbds+RQ=";
   };
 
   nativeBuildInputs = [ ocaml findlib ];
