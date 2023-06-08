f3f38a2130f2096b8da76e233765c1ca4e2b7862
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-06-08 21:30:37 +0200

    python310Packages.ansible-core: provide passlib
    Required for crypt on darwin.

--- pkgs/development/python-modules/ansible/core.nix
+++ pkgs/development/python-modules/ansible/core.nix
@@ -1,5 +1,4 @@
 { lib
-, callPackage
 , buildPythonPackage
 , fetchPypi
 , installShellFiles
@@ -11,6 +10,7 @@
 , ncclient
 , packaging
 , paramiko
+, passlib
 , pexpect
 , psutil
 , pycrypto
@@ -50,6 +50,7 @@ buildPythonPackage rec {
     cryptography
     jinja2
     packaging
+    passlib
     pyyaml
     resolvelib # This library is a PITA, since ansible requires a very old version of it
     # optional dependencies
