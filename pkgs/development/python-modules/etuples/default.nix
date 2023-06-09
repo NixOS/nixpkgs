b3f05ffdc1e6ef8107fc2abf5115b3f88ff48060
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-09 23:00:58 +0200

    python310Packages.etuples: add format
    - disable on unsupported Python releases

--- pkgs/development/python-modules/etuples/default.nix
+++ pkgs/development/python-modules/etuples/default.nix
@@ -6,11 +6,15 @@
 , py
 , pytestCheckHook
 , pytest-html
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "etuples";
   version = "0.3.9";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "pythological";
@@ -35,12 +39,14 @@ buildPythonPackage rec {
     "--self-contained-html"
   ];
 
-  pythonImportsCheck = [ "etuples" ];
+  pythonImportsCheck = [
+    "etuples"
+  ];
 
   meta = with lib; {
     description = "Python S-expression emulation using tuple-like objects";
     homepage = "https://github.com/pythological/etuples";
-    changelog = "https://github.com/pythological/etuples/releases";
+    changelog = "https://github.com/pythological/etuples/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ Etjean ];
   };
