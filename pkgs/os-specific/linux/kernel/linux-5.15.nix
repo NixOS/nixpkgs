951e7e83aee6b1ad6a9fa7a5ec32ed4466a05a38
K900 <me@0upti.me>
2023-06-09 16:43:41 +0300

    linux: 5.15.115 -> 5.15.116

--- pkgs/os-specific/linux/kernel/linux-5.15.nix
+++ pkgs/os-specific/linux/kernel/linux-5.15.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.15.115";
+  version = "5.15.116";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "050j9z0wg5glgxwbmzfq4l8lv8y7g3nc2rz8342yjdcjfxh6h1qv";
+    sha256 = "16hpdqlkz2g2pjcml7j55yfym6nbp0zg8f2r969wq9jkpg8wj5zn";
   };
 } // (args.argsOverride or { }))
