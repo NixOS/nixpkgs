ca70f524da4a3f738a0132dca14d4ed46ab8c998
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-09 12:44:04 +0000

    erlang_odbc: 25.3.2.1 -> 25.3.2.2

--- pkgs/development/interpreters/erlang/25.nix
+++ pkgs/development/interpreters/erlang/25.nix
@@ -1,6 +1,6 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "25.3.2.1";
-  sha256 = "4PDK18/tekJHgNGECG5hv/HvwvteR+FeN6hlpB9FC9o=";
+  version = "25.3.2.2";
+  sha256 = "TeDszsF9X4ExnOfP20ujlJpfxKK1zwhHHnPAbSnm/YQ=";
 }
