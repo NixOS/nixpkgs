489678eb8cbb15803cdd763edf7381eff948eda6
K900 <me@0upti.me>
2023-06-09 16:43:27 +0300

    linux: 4.14.316 -> 4.14.317

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.14.316";
+  version = "4.14.317";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0xlg93va7dbz2w428kiw7vr2sds3542fqq57rwyf51ykq7qii0xc";
+    sha256 = "0c1wy0m0jnjpc6scrw1y97wsg2d18vb1bi31i1qzlxvgmrd8zwlc";
   };
 } // (args.argsOverride or {}))
