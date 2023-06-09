afef178026b7ecfbb0560c817c275e651835fa99
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-09 03:35:58 +0000

    python310Packages.geoalchemy2: 0.13.1 -> 0.13.3

--- pkgs/development/python-modules/geoalchemy2/default.nix
+++ pkgs/development/python-modules/geoalchemy2/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "geoalchemy2";
-  version = "0.13.1";
+  version = "0.13.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "GeoAlchemy2";
     inherit version;
-    hash = "sha256-VyRtRK6pC0xS+EwAb2dY0OGVHrkBjxBAmocUHwIVmxM=";
+    hash = "sha256-2Fp96qmiMJAXM/dBnWv/VnS4cwZR3hoH8rZCOqSSXQk=";
   };
 
   nativeBuildInputs = [
