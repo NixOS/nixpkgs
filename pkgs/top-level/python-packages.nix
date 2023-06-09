8640b572b3b08b7392ab8a057518dbf1c25edf75
Raito Bezarius <masterancpp@gmail.com>
2023-06-09 18:22:50 +0200

    python3Packages.celery-singleton: init at 0.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1681,6 +1681,8 @@ self: super: with self; {
 
   celery-redbeat = callPackage ../development/python-modules/celery-redbeat { };
 
+  celery-singleton = callPackage ../development/python-modules/celery-singleton { };
+
   cement = callPackage ../development/python-modules/cement { };
 
   cemm = callPackage ../development/python-modules/cemm { };
