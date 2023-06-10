c21fb56736ff5f986c5ad8d647a342cc34c1d200
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-10 18:05:37 +0000

    tagref: 1.8.2 -> 1.8.3

--- pkgs/tools/misc/tagref/default.nix
+++ pkgs/tools/misc/tagref/default.nix
@@ -1,16 +1,16 @@
 { lib, fetchFromGitHub, rustPlatform }:
 rustPlatform.buildRustPackage rec {
   pname = "tagref";
-  version = "1.8.2";
+  version = "1.8.3";
 
   src = fetchFromGitHub {
     owner = "stepchowfun";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-igzlSkoVffn0X/eJrNi9u0aLc17KREuUtIwxGvsF6hc=";
+    sha256 = "sha256-BZ0by5H6yZuvksMneor02rx0kXlvO9tdpWivRDB9qDs=";
   };
 
-  cargoHash = "sha256-FNgMM+fOEbkCqRPgJmGiuoUJP9NRBxjSTwFIPyaT5d0=";
+  cargoHash = "sha256-w+YU0+/vba/fRYbLAmOloOuHMWbFiEyt3b0mfIvFE0Q=";
 
   meta = with lib; {
     description = "Tagref helps you refer to other locations in your codebase.";
