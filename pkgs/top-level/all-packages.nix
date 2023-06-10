d2c4d21c2f661fb8d1cc8536e84c5e4372416051
Weathercold <weathercold.scr@gmail.com>
2023-06-10 17:32:11 -0400

    whalebird: 4.7.4 -> 5.0.7
    changelog: <https://github.com/h3poteto/whalebird-desktop/releases>
    
    notable changes:
    - Add hide-to-tray on start option (3942)
    - Re-implement layout and sidebar (4037)
    - Show reply target message on compose (4098)
    - Show character limit in compose (4099)
    - Show quote target message on compose (4100)
    - Add validation for compose (4121)
    - Auto-complete in compose (4139)
    - Add shortcut key for post (4269)
    - !!! Update electron version to 21.4.4 (4304)
    - !!! Change license to GPL (4363)
    - Add scroll bar if there are many hashtags or lists (4421)
    - Add shortuct for reloading with CMD+R (4422)
    - Tons of dependency bumps and fixes

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35360,7 +35360,7 @@ with pkgs;
   wgnord = callPackage ../applications/networking/wgnord/default.nix { };
 
   whalebird = callPackage ../applications/misc/whalebird {
-    electron = electron_19;
+    electron = electron_21;
   };
 
   windowlab = callPackage ../applications/window-managers/windowlab { };
