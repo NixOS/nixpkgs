2f2fe4f58cf9363f9da0487e2d86a391f87a80ed
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-10 00:45:38 +0000

    python310Packages.azure-mgmt-privatedns: 1.0.0 -> 1.1.0

--- pkgs/development/python-modules/azure-mgmt-privatedns/default.nix
+++ pkgs/development/python-modules/azure-mgmt-privatedns/default.nix
@@ -6,13 +6,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "1.0.0";
+  version = "1.1.0";
   pname = "azure-mgmt-privatedns";
   disabled = isPy27;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b60f16e43f7b291582c5f57bae1b083096d8303e9d9958e2c29227a55cc27c45";
+    sha256 = "sha256-MtucYFpKj/ANNON1UdXrBrTsJnq53iph3SJ1ypWj+5g=";
     extension = "zip";
   };
 
