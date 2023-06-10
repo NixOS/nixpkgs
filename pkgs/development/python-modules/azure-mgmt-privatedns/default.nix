6c6d206566348651fbc038ab9977f06523763d5a
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-10 17:51:32 +0200

    python310Packages.azure-mgmt-privatedns: clean-up

--- pkgs/development/python-modules/azure-mgmt-privatedns/default.nix
+++ pkgs/development/python-modules/azure-mgmt-privatedns/default.nix
@@ -1,32 +1,43 @@
-{ lib, buildPythonPackage, fetchPypi, isPy27
+{ lib
 , azure-common
 , azure-mgmt-core
+, buildPythonPackage
+, fetchPypi
+, isodate
 , msrest
-, msrestazure
+, pythonOlder
+, typing-extensions
 }:
 
 buildPythonPackage rec {
-  version = "1.1.0";
   pname = "azure-mgmt-privatedns";
-  disabled = isPy27;
+  version = "1.1.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-MtucYFpKj/ANNON1UdXrBrTsJnq53iph3SJ1ypWj+5g=";
+    hash = "sha256-MtucYFpKj/ANNON1UdXrBrTsJnq53iph3SJ1ypWj+5g=";
     extension = "zip";
   };
 
   propagatedBuildInputs = [
     azure-common
     azure-mgmt-core
+    isodate
     msrest
-    msrestazure
+  ]  ++ lib.optionals (pythonOlder "3.8") [
+    typing-extensions
   ];
 
   # no tests included
   doCheck = false;
 
-  pythonImportsCheck = [ "azure.common" "azure.mgmt.privatedns" ];
+  pythonImportsCheck = [
+    "azure.common"
+    "azure.mgmt.privatedns"
+  ];
 
   meta = with lib; {
     description = "Microsoft Azure DNS Private Zones Client Library for Python";
