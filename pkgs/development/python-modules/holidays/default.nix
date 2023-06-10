766d51a350c2c803d6b0ae75168372cd4d7c563b
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-10 22:01:37 +0200

    python311Packages.holidays: 0.25 -> 0.26
    Changelog: https://github.com/dr-prodigy/python-holidays/releases/tag/v.0.26

--- pkgs/development/python-modules/holidays/default.nix
+++ pkgs/development/python-modules/holidays/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "holidays";
-  version = "0.25";
+  version = "0.26";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "dr-prodigy";
     repo = "python-holidays";
     rev = "refs/tags/v.${version}";
-    hash = "sha256-D6MCLbuNnafWMDyEc/jeyfOs0VVV92AndtNsjyFDgEg=";
+    hash = "sha256-4kRIhIjOQB23ihZBs6J6/ZriLiMD87m/xOqMXga5Ypw=";
   };
 
   propagatedBuildInputs = [
