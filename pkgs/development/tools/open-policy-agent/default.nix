ca5b5392c108e60917f0645327d1d500d4250fcb
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-10 10:15:17 +0000

    open-policy-agent: 0.53.0 -> 0.53.1

--- pkgs/development/tools/open-policy-agent/default.nix
+++ pkgs/development/tools/open-policy-agent/default.nix
@@ -11,13 +11,13 @@ assert enableWasmEval && stdenv.isDarwin -> builtins.throw "building with wasm o
 
 buildGoModule rec {
   pname = "open-policy-agent";
-  version = "0.53.0";
+  version = "0.53.1";
 
   src = fetchFromGitHub {
     owner = "open-policy-agent";
     repo = "opa";
     rev = "v${version}";
-    hash = "sha256-HfQkd0Vl48zGMTXHJoTfVLV/OHHC+T6gcA6pcewhFJU=";
+    hash = "sha256-FKBrvg9uZ93dOM+LLWA1Ae7uBMiux5l355n5r8UjBbg=";
   };
   vendorHash = null;
 
