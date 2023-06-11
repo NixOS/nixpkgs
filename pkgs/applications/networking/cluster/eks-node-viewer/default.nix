565efb4ad0b5c8c2ba1c212f75ae880787b7db82
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-11 21:56:12 +0000

    eks-node-viewer: 0.2.1 -> 0.4.0

--- pkgs/applications/networking/cluster/eks-node-viewer/default.nix
+++ pkgs/applications/networking/cluster/eks-node-viewer/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "eks-node-viewer";
-  version = "0.2.1";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-XRt9a//0mYKZKsMs2dlcsBt5ikC9ZBMeQ3Vas0eT8a8=";
+    sha256 = "sha256-p0n7ocoMBgM6i7e6yX+NDIkZBcJ3dT6VNWPihCheeC0=";
   };
 
-  vendorHash = "sha256-28TKZYZM2kddXAusxmjhrKFy+ATU7kZM4Ad7zvP/F3A=";
+  vendorHash = "sha256-L1lG+b7MiJQvLqZuLdSjh5zAaApvWdi9SZSDPvObW5w=";
 
   meta = with lib; {
     description = "Tool to visualize dynamic node usage within a cluster";
