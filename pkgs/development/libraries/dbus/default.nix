a7ce6c151a2017971bc79680da068073a06cb469
Vladimír Čunát <v@cunat.cz>
2023-06-11 10:25:57 +0200

    dbus: fill meta.changelog
    (so that r-ryantm update-bot shows it)

--- pkgs/development/libraries/dbus/default.nix
+++ pkgs/development/libraries/dbus/default.nix
@@ -115,6 +115,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Simple interprocess messaging system";
     homepage = "http://www.freedesktop.org/wiki/Software/dbus/";
+    changelog = "https://gitlab.freedesktop.org/dbus/dbus/-/blob/dbus-${version}/NEWS";
     license = licenses.gpl2Plus; # most is also under AFL-2.1
     maintainers = teams.freedesktop.members ++ (with maintainers; [ ]);
     platforms = platforms.unix;
