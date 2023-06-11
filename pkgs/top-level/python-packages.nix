c60d82fe5515b930b2df425b6583d024959f5111
ppenguin <hieronymusv@gmail.com>
2023-06-11 16:13:51 +0200

    add platformio as python package [init at 6.1.7]

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9547,6 +9547,8 @@ self: super: with self; {
 
   platformdirs = callPackage ../development/python-modules/platformdirs { };
 
+  platformio = callPackage ../development/python-modules/platformio { };
+
   playsound = callPackage ../development/python-modules/playsound { };
 
   plexapi = callPackage ../development/python-modules/plexapi { };
