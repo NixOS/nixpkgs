cd265d7ad0e1ed00becf543576f25261b592dd36
Mario Rodas <marsam@users.noreply.github.com>
2023-06-11 04:20:00 +0000

    flexget: 3.7.4 -> 3.7.5
    Diff: https://github.com/Flexget/Flexget/compare/refs/tags/v3.7.4...v3.7.5
    
    Changelog: https://github.com/Flexget/Flexget/releases/tag/v3.7.5

--- pkgs/applications/networking/flexget/default.nix
+++ pkgs/applications/networking/flexget/default.nix
@@ -6,7 +6,7 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "flexget";
-  version = "3.7.4";
+  version = "3.7.5";
   format = "pyproject";
 
   # Fetch from GitHub in order to use `requirements.in`
@@ -14,7 +14,7 @@ python3.pkgs.buildPythonApplication rec {
     owner = "Flexget";
     repo = "Flexget";
     rev = "refs/tags/v${version}";
-    hash = "sha256-x5hwuc2yXDw8qWGLRJDH4V4rmvxjBKZaogmv4SxXoz4=";
+    hash = "sha256-abnCXRlpGp3+6zjKVOtlxjcC5VdaHjMyPC9PRlABJIA=";
   };
 
   postPatch = ''
