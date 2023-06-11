191e2bffb16027577264bf8c779e71b04bdf5019
Mario Rodas <marsam@users.noreply.github.com>
2023-06-11 08:00:00 -0500

    libopus: fix build on aarch64-linux

--- pkgs/development/libraries/libopus/default.nix
+++ pkgs/development/libraries/libopus/default.nix
@@ -1,5 +1,6 @@
 { lib
 , stdenv
+, fetchpatch
 , fetchurl
 , meson
 , python3
@@ -16,6 +17,15 @@ stdenv.mkDerivation rec {
     url = "https://downloads.xiph.org/releases/opus/opus-${version}.tar.gz";
     sha256 = "sha256-ybMrQlO+WuY9H/Fu6ga5S18PKVG3oCrO71jjo85JxR8=";
   };
+
+  patches = [
+    # Fix meson build for arm64. Remove with next release
+    # https://gitlab.xiph.org/xiph/opus/-/merge_requests/59
+    (fetchpatch {
+      url = "https://gitlab.xiph.org/xiph/opus/-/commit/20c032d27c59d65b19b8ffbb2608e5282fe817eb.patch";
+      hash = "sha256-2pX+0ay5PTyHL2plameBX2L1Q4aTx7V7RGiTdhNIuE4=";
+    })
+  ];
 
   postPatch = ''
     patchShebangs meson/
