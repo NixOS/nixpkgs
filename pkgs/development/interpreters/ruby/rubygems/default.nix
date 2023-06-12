75c15fb009c2b657d55e2bdb79c6ddd8dc3291c1
Mario Rodas <marsam@users.noreply.github.com>
2023-06-12 04:20:00 +0000

    ruby: rubygems 3.4.13 -> 3.4.14
    Changelog: https://github.com/rubygems/rubygems/releases/tag/v3.4.14

--- pkgs/development/interpreters/ruby/rubygems/default.nix
+++ pkgs/development/interpreters/ruby/rubygems/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "rubygems";
-  version = "3.4.13";
+  version = "3.4.14";
 
   src = fetchurl {
     url = "https://rubygems.org/rubygems/rubygems-${version}.tgz";
-    sha256 = "sha256-s/7KCbf07zezuASA7E03t83UDY6W/fFw9bljOprXWls=";
+    hash = "sha256-l5sYQoSKOb5NkO/J05nClKygtuZ9RfgAsmyTzCLQOYM=";
   };
 
   patches = [
