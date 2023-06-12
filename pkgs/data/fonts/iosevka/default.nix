a8c0d5583c8a5d6baf968d8311082b5960b153f7
lunik1 <ch.gpg@themaw.xyz>
2023-06-12 22:39:31 +0100

    iosevka: 24.1.1 -> 24.1.3
    Diff: https://github.com/be5invis/iosevka/compare/v24.1.1...v24.1.3

--- pkgs/data/fonts/iosevka/default.nix
+++ pkgs/data/fonts/iosevka/default.nix
@@ -55,16 +55,16 @@ assert (extraParameters != null) -> set != null;
 
 buildNpmPackage rec {
   pname = if set != null then "iosevka-${set}" else "iosevka";
-  version = "24.1.1";
+  version = "24.1.3";
 
   src = fetchFromGitHub {
     owner = "be5invis";
     repo = "iosevka";
     rev = "v${version}";
-    hash = "sha256-kzf/DFks/Ovhl6uMw5Bqhq+j+alrUd4E49jgSmp2E2k=";
+    hash = "sha256-LM47tiFZ5rDGgRqvGZEoCSpij4ZEoulnsAiM2ZlP7fY=";
   };
 
-  npmDepsHash = "sha256-FKA3D05E5A0Z3wdCYLzoTyYKLay25sc3O61rOzkIN2E=";
+  npmDepsHash = "sha256-jW1g4n66AFP6fjp0vXKZiBQzDkWamSWQprIE+VkZ6rk=";
 
   nativeBuildInputs = [
     remarshal
