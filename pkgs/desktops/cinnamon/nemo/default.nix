048cc6e0c4fb6627f1d0ba27dea420fe33f768d2
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-12 08:05:00 +0000

    cinnamon.nemo: 5.6.5 -> 5.8.1

--- pkgs/desktops/cinnamon/nemo/default.nix
+++ pkgs/desktops/cinnamon/nemo/default.nix
@@ -23,13 +23,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nemo";
-  version = "5.6.5";
+  version = "5.8.1";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    sha256 = "sha256-HdDe2VE9LQqiwFrUSIctOi/ffNOmLy6SyG30EL8UA5Q=";
+    sha256 = "sha256-3XYAupJa+ap8ADZ0d2qW7UXaDs3u3KpJeg/4mS9rWJ8=";
   };
 
   patches = [
