3b7e8e1786383c1defde550537094fae0e1fe8fc
Nicolas Benes <nbenes.gh@xandea.de>
2023-06-12 12:32:32 +0200

    python3Packages.click-aliases: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1879,6 +1879,8 @@ self: super: with self; {
 
   clickclick = callPackage ../development/python-modules/clickclick { };
 
+  click-aliases = callPackage ../development/python-modules/click-aliases { };
+
   click-command-tree = callPackage ../development/python-modules/click-command-tree { };
 
   click-completion = callPackage ../development/python-modules/click-completion { };
