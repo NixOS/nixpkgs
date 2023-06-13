b992b7502b9aa6a0f4b13663b10a40cd34cc0d00
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-13 00:18:24 +0000

    ytt: 0.45.1 -> 0.45.2

--- pkgs/development/tools/ytt/default.nix
+++ pkgs/development/tools/ytt/default.nix
@@ -1,13 +1,13 @@
 { lib, buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "ytt";
-  version = "0.45.1";
+  version = "0.45.2";
 
   src = fetchFromGitHub {
     owner = "vmware-tanzu";
     repo = "carvel-ytt";
     rev = "v${version}";
-    sha256 = "sha256-YfRr3oQUuDGVrQvfUzqld4SNWOsmGP4jmo5gf8tG6Vo=";
+    sha256 = "sha256-D2m+o1rGi2CPLqaLJWHOcVeA7F0GeUu4jFneTOsP+Ak=";
   };
 
   vendorHash = null;
