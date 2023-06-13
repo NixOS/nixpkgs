1c45651756a8c2619b8753075a2ec1656dfc6e1c
figsoda <figsoda@pm.me>
2023-06-13 15:00:01 -0400

    spacer: init at 0.1.1
    https://github.com/samwho/spacer

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12619,6 +12619,8 @@ with pkgs;
 
   sozu = callPackage ../servers/sozu { };
 
+  spacer = callPackage ../tools/misc/spacer { };
+
   sparrow-unwrapped = callPackage ../applications/blockchains/sparrow {
     openimajgrabber = callPackage ../applications/blockchains/sparrow/openimajgrabber.nix {};
     openjdk = openjdk.override { enableJavaFX = true; };
