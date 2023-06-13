ee5a512c58bde881eb8e03a5e167abd6d528d7cd
Icy-Thought <icy-thought@pm.me>
2023-06-13 08:18:21 +0200

    upscayl: init at 2.5.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27986,6 +27986,8 @@ with pkgs;
 
   upower = callPackage ../os-specific/linux/upower { };
 
+  upscayl = callPackage ../applications/graphics/upscayl { };
+
   usbguard = callPackage ../os-specific/linux/usbguard { };
 
   usbguard-notifier = callPackage ../os-specific/linux/usbguard-notifier { };
