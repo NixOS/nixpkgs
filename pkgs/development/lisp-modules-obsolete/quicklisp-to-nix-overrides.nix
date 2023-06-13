ae3504632697eb76852a623f44678b5e11118df3
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-06-13 23:06:37 +0200

    treewide: treewide: use lib.optionalAttrs instead of 'then {}'
    add missing parentheses.

--- pkgs/development/lisp-modules-obsolete/quicklisp-to-nix-overrides.nix
+++ pkgs/development/lisp-modules-obsolete/quicklisp-to-nix-overrides.nix
@@ -4,7 +4,7 @@ let
   skipBuildPhase = x: {
     overrides = y: ((x.overrides y) // { buildPhase = "true"; });
   };
-  multiOverride = l: x: pkgs.lib.optionalAttrs (l != []) ((builtins.head l) x) // (multiOverride (builtins.tail l) x);
+  multiOverride = l: x: pkgs.lib.optionalAttrs (l != []) (((builtins.head l) x) // (multiOverride (builtins.tail l) x));
   lispName = (clwrapper.lisp.pname or (builtins.parseDrvName clwrapper.lisp.name).name);
   ifLispIn = l: f: if (pkgs.lib.elem lispName l) then f else (x: {});
   ifLispNotIn = l: f: if ! (pkgs.lib.elem lispName l) then f else (x: {});
