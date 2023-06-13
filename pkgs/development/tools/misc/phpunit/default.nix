d2331122e1562bb3d65bd11b021b1a7c2d5901d3
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-13 09:42:01 +0000

    phpunit: 10.2.1 -> 10.2.2

--- pkgs/development/tools/misc/phpunit/default.nix
+++ pkgs/development/tools/misc/phpunit/default.nix
@@ -2,14 +2,14 @@
 
 let
   pname = "phpunit";
-  version = "10.2.1";
+  version = "10.2.2";
 in
 stdenv.mkDerivation {
   inherit pname version;
 
   src = fetchurl {
     url = "https://phar.phpunit.de/phpunit-${version}.phar";
-    hash = "sha256-gmxcr+WIQw/SeEvJpSFLKOuTb36qUe6WdlVWIq5tClo=";
+    hash = "sha256-UAxqywIMrP42Nul0GV6mkKkXtnS7ah9rl4y1ykpJsxM=";
   };
 
   dontUnpack = true;
