4fd2768c69960454045b720eb034d2530e2ae805
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-13 19:48:58 +0000

    codespelunker: 1.0.0 -> 1.2.0

--- pkgs/development/tools/codespelunker/default.nix
+++ pkgs/development/tools/codespelunker/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "codespelunker";
-  version = "1.0.0";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "boyter";
     repo = "cs";
     rev = "v${version}";
-    hash = "sha256-NN/78paePdvYHQ4J2aQu56PvEciOXY8DxHd4ajfVCFU=";
+    hash = "sha256-kWKDr8KKD3M5MyRuEMMZXvTqflDidkMsu2fN9N0s50w=";
   };
 
   vendorHash = null;
