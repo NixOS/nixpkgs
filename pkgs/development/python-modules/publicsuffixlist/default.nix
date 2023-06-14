2ba33f697ceaf84d84506bafbf5d5d4681817adc
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-14 06:11:10 +0000

    python310Packages.publicsuffixlist: 0.10.0.20230611 -> 0.10.0.20230614

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "publicsuffixlist";
-  version = "0.10.0.20230611";
+  version = "0.10.0.20230614";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-z5mh/8sFulky9LX0LPwBuYWebjsqWc0EsPjDZMuTCX8=";
+    hash = "sha256-8tB95lcW4IQBeU5cY0YFfLKW3a0g1SQH9sJbn0cduho=";
   };
 
   passthru.optional-dependencies = {
