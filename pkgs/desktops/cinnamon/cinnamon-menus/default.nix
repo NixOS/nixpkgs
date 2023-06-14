8c4cf139b97b514dfe64d387ddba287e035c0305
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-14 07:35:51 +0000

    cinnamon.cinnamon-menus: 5.6.0 -> 5.8.0

--- pkgs/desktops/cinnamon/cinnamon-menus/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-menus/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-menus";
-  version = "5.6.0";
+  version = "5.8.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-6IOlXQhAy6YrSqybfGFUyn3Q2COvzwpj67y/k/YLNhU=";
+    hash = "sha256-AgA/DA7I9/0AJhlmgk0yAOJaZzpiQV1vM949Y6EOWVg=";
   };
 
   buildInputs = [
