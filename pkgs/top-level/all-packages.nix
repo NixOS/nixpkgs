0a4512bdaa7d2187578d5cdc7fccc1714cf47cb5
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-06-14 13:01:26 -0500

    semantic-release: init at 21.0.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12312,6 +12312,10 @@ with pkgs;
 
   selectdefaultapplication = libsForQt5.callPackage ../applications/misc/selectdefaultapplication { };
 
+  semantic-release = callPackage ../development/tools/semantic-release {
+    inherit (darwin) cctools;
+  };
+
   semgrep = python3.pkgs.callPackage ../tools/security/semgrep { };
   semgrep-core = callPackage ../tools/security/semgrep/semgrep-core.nix { };
 
