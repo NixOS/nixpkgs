d93f5343ee1b319cca0388b446058bcdad9b2d10
kilianar <mail@kilianar.de>
2023-06-14 10:58:59 +0200

    i3status-rust: 0.31.6 -> 0.31.7
    https://github.com/greshake/i3status-rust/releases/tag/v0.31.7

--- pkgs/applications/window-managers/i3/status-rust.nix
+++ pkgs/applications/window-managers/i3/status-rust.nix
@@ -15,16 +15,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "i3status-rust";
-  version = "0.31.6";
+  version = "0.31.7";
 
   src = fetchFromGitHub {
     owner = "greshake";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-sQF8POmDEUv3nY9+7Kr0Dcc3q/kpOLwCarexbCQ1zSg=";
+    hash = "sha256-LcaKBl2PAEbXYFMJUth8vgx557YTiiuZd3EHBGrsO0M=";
   };
 
-  cargoHash = "sha256-+wrZG7jzK8ksTpm6g25O/09sAnMYwmY5DYKqmUMuCFU=";
+  cargoHash = "sha256-b3YkmyWgnhWyQ/ZhU6TyOZMFwk7jY80FHEd/MUqEQzw=";
 
   nativeBuildInputs = [ pkg-config makeWrapper ];
 
