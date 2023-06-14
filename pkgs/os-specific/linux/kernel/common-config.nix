0a81cf8e7a327420340850ed6916c3d60e894f7d
Alyssa Ross <hi@alyssa.is>
2023-06-14 10:49:13 +0000

    linux: set ZPOOL=y
    ZBUD depends on ZPOOL.  If we get asked about ZPOOL first, by default
    it would be set to m, and it would then not be possible to set ZBUD=y.
    
    This fixes, for example, building Linux 5.15 for microblazeel-linux.

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -720,6 +720,7 @@ let
       ZRAM           = module;
       ZRAM_WRITEBACK = option yes;
       ZSWAP          = option yes;
+      ZPOOL          = yes;
       ZBUD           = option yes;
       ZSMALLOC       = module;
     };
