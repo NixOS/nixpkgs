c2d422e6c8491b653dec094d799d434bd858b716
K900 <me@0upti.me>
2023-06-14 18:56:05 +0300

    linux: 4.19.285 -> 4.19.286

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.19.285";
+  version = "4.19.286";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "05nwivdk4w939vrrbn5p2yai1rz7kxqa4bl5f3n6d867b59pg8da";
+    sha256 = "1788a68fbga03nkgbvai2bi89v826915829727j4zcilyc21b127";
   };
 } // (args.argsOverride or {}))
