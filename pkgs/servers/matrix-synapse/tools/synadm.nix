4e52dd0fb34dc00a11597c074e8e5e9fa9b92c19
Andrei Zmievski <andrei@zmievski.org>
2023-06-14 09:38:45 -0700

    synadm: 0.41.2 -> 0.41.3
    https://github.com/JOJ0/synadm/releases/tag/v0.41.3

--- pkgs/servers/matrix-synapse/tools/synadm.nix
+++ pkgs/servers/matrix-synapse/tools/synadm.nix
@@ -6,12 +6,12 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "synadm";
-  version = "0.41.2";
+  version = "0.41.3";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-wSpgc1umBMLCc2ThfYSuNNnzqWXyEQM0XhTuOAQaiXg=";
+    hash = "sha256-gWEgLpSE77XdocAZqN1i/vR5dvYFsgsg5zs5Dj90V/o=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
