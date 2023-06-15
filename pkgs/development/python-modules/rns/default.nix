176b451a3f6bfcb1d937ab70433ee60917009e2c
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-15 23:37:52 +0200

    python311Packages.rns: 0.5.4 -> 0.5.5
    Diff: https://github.com/markqvist/Reticulum/compare/refs/tags/0.5.4...0.5.5
    
    Changelog: https://github.com/markqvist/Reticulum/releases/tag/0.5.5

--- pkgs/development/python-modules/rns/default.nix
+++ pkgs/development/python-modules/rns/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "rns";
-  version = "0.5.4";
+  version = "0.5.5";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "markqvist";
     repo = "Reticulum";
     rev = "refs/tags/${version}";
-    hash = "sha256-OFU61RX6XtC/7x3SWbRCsODTrs3k3FCTCvEapkkeWQU=";
+    hash = "sha256-gyEf6Sck+qmbnepiBoHrN9t018BwBM4iJQBjU9Iqhn4=";
   };
 
   propagatedBuildInputs = [
