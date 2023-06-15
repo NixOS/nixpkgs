368aa725c073a03c2c8999c372c4d1108f19b357
Nick Cao <nickcao@nichi.co>
2023-06-15 21:48:08 +0800

    liburing: 2.3 -> 2.4

--- pkgs/development/libraries/liburing/default.nix
+++ pkgs/development/libraries/liburing/default.nix
@@ -1,29 +1,14 @@
-{ lib, stdenv, fetchgit
-, fetchpatch
-}:
+{ lib, stdenv, fetchgit }:
 
 stdenv.mkDerivation rec {
   pname = "liburing";
-  version = "2.3";
+  version = "2.4";
 
   src = fetchgit {
     url    = "http://git.kernel.dk/${pname}";
     rev    = "liburing-${version}";
-    sha256 = "sha256-vN6lLb5kpgHTKDxwibJPS61sdelILETVtJE2BYgp79k=";
+    sha256 = "sha256-vbe9uh9AqXyPkzwD6zHoHH3JMeAJEl2FSGzny1T7diM=";
   };
-
-  patches = [
-    # Backported portability fixes from liburing master, needed for pkgsMusl.liburing
-    ./0001-Add-custom-error-function-for-tests.patch
-    ./0002-test-Use-t_error-instead-of-glibc-s-error.patch
-    ./0003-examples-Use-t_error-instead-of-glibc-s-error.patch
-
-    # More portability fixes, in the process of being upstreamed
-    (fetchpatch {
-      url = "https://github.com/axboe/liburing/pull/798/commits/0fbcc44fe1fb2dc6807660b2cff1c2995add095b.patch";
-      hash = "sha256-xOMsw0VpYGst/+Isd2Tmq8CmBDK+uyLw3KNKPnsCSoA=";
-    })
-  ];
 
   separateDebugInfo = true;
   enableParallelBuilding = true;
