f914560aa570162b3d65b4d743324a21048932ab
K900 <me@0upti.me>
2023-06-15 15:46:42 +0300

    all-packages: recurse into geph
    It's actually a set of two packages.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20662,7 +20662,7 @@ with pkgs;
   gecode_6 = qt5.callPackage ../development/libraries/gecode { };
   gecode = gecode_6;
 
-  geph = callPackages ../applications/networking/geph { };
+  geph = recurseIntoAttrs (callPackages ../applications/networking/geph { });
 
   gephi = callPackage ../applications/science/misc/gephi { };
 
