7f782df5261953f58d985eff07c2febe2ea14aa7
Vladimír Čunát <v@cunat.cz>
2023-06-15 22:41:59 +0200

    python311Packages.pyflakes: patch test

--- pkgs/development/python-modules/pyflakes/default.nix
+++ pkgs/development/python-modules/pyflakes/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
+, fetchpatch, isPy311
 , pytestCheckHook
 }:
 
@@ -17,6 +18,14 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-7IsnamtgvYDe/tJa3X5DmIHBnmSFCv2bNGKD1BZf0P0=";
   };
+
+  patches = lib.optional isPy311 # could be made unconditional on rebuild
+    (fetchpatch {
+      name = "tests-py311.patch";
+      url = "https://github.com/PyCQA/pyflakes/commit/836631f2f73d45baa4021453d89fc9fd6f52be58.diff";
+      hash = "sha256-xlgql+bN0HsGnTMkwax3ZG/5wrbkUl/kQkjlr3lsgRw=";
+    })
+  ;
 
   nativeCheckInputs = [
     pytestCheckHook
