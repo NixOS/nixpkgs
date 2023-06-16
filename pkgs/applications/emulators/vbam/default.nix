a5f81ddc2feced17319db073399af0a6d11084c8
netali <me@netali.de>
2023-06-16 08:36:06 +0200

    vbam: fix build with openal 1.23.1
    This commit fixes the build of vbam with openal 1.23.1 by applying a
    patch from the master branch of the vbam repo which isn't released in a
    new version yet.
    
    Fixes #237969

--- pkgs/applications/emulators/vbam/default.nix
+++ pkgs/applications/emulators/vbam/default.nix
@@ -43,6 +43,13 @@ stdenv.mkDerivation rec {
     gtk3
   ];
 
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/visualboyadvance-m/visualboyadvance-m/commit/1d7e8ae4edc53a3380dfea88329b8b8337db1c52.patch";
+      sha256 = "sha256-SV1waz2JSKiM6itwkqwlE3aOZCcOl8iyBr06tyYlefo=";
+    })
+  ];
+
   cmakeFlags = [
     "-DCMAKE_BUILD_TYPE='Release'"
     "-DENABLE_FFMPEG='true'"
