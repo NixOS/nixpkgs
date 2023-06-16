81d29fc10038f237d7f751066a159c090e183caa
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2023-06-16 22:00:19 +0200

    gnomeExtensions.dash-to-dock: remove  manual packaging

--- pkgs/desktops/gnome/extensions/manuallyPackaged.nix
+++ pkgs/desktops/gnome/extensions/manuallyPackaged.nix
@@ -3,7 +3,6 @@
   "arcmenu@arcmenu.com" = callPackage ./arcmenu { };
   "argos@pew.worldwidemann.com" = callPackage ./argos { };
   "clock-override@gnomeshell.kryogenix.org" = callPackage ./clock-override { };
-  "dash-to-dock@micxgx.gmail.com" = callPackage ./dash-to-dock { };
   "drop-down-terminal@gs-extensions.zzrough.org" = callPackage ./drop-down-terminal { };
   "EasyScreenCast@iacopodeenosee.gmail.com" = callPackage ./EasyScreenCast { };
   "gsconnect@andyholmes.github.io" = callPackage ./gsconnect { };
