27f983493790137287a9b02f17f1584200f3f1f9
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-06-16 19:11:59 +0200

    nixos/guacamole-client: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1193,6 +1193,7 @@
   ./services/web-apps/gotosocial.nix
   ./services/web-apps/grocy.nix
   ./services/web-apps/pixelfed.nix
+  ./services/web-apps/guacamole-client.nix
   ./services/web-apps/healthchecks.nix
   ./services/web-apps/hedgedoc.nix
   ./services/web-apps/hledger-web.nix
