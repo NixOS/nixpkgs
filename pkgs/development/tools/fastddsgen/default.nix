6bdd67fadcdc3440cbea4ac18b328ed0328e76d5
Michal Sojka <michal.sojka@cvut.cz>
2023-06-16 17:27:02 +0200

    fastddsgen: 2.4.0 -> 2.5.0

--- pkgs/development/tools/fastddsgen/default.nix
+++ pkgs/development/tools/fastddsgen/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, runtimeShell, writeText, fetchFromGitHub, gradle_7, openjdk17, git, perl, cmake }:
 let
   pname = "fastddsgen";
-  version = "2.4.0";
+  version = "2.5.0";
 
   src = fetchFromGitHub {
     owner = "eProsima";
     repo = "Fast-DDS-Gen";
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-CSlimEShTxjr6II9YSuNFc/pdbMmqVMCQA0ZJ9rl5j8=";
+    hash = "sha256-tnSZ0jyR6iPjyA3vpdZXOza/QI2ro92BOtjKp/NDriM=";
   };
 
   gradle = gradle_7;
