c7967c976681f4cbbc47e2a2beb568c454f5d379
Nick Cao <nickcao@nichi.co>
2023-06-17 09:32:20 +0800

    firefox_decrypt: unstable-2022-12-21 -> unstable-2023-05-14

--- pkgs/tools/security/firefox_decrypt/default.nix
+++ pkgs/tools/security/firefox_decrypt/default.nix
@@ -1,39 +1,29 @@
 { lib
 , fetchFromGitHub
-, stdenvNoCC
+, buildPythonApplication
+, setuptools
 , nss
-, wrapPython
 , nix-update-script
 }:
 
-stdenvNoCC.mkDerivation rec {
+buildPythonApplication rec {
   pname = "firefox_decrypt";
-  version = "unstable-2022-12-21";
+  version = "unstable-2023-05-14";
+
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "unode";
     repo = pname;
-    rev = "84bb368cc2f8d2055a8374ab1a40c403e0486859";
-    sha256 = "sha256-dyQTf6fgsQEmp++DeXl85nvyezm0Lq9onyfIdhQoGgI=";
+    rev = "ac857efde75d86dd6bd5dfca25d4a0f73b75009f";
+    sha256 = "sha256-34QS98nmrL98nzoZgeFSng8TJJc9BU1+Tzh2b+dsuCc=";
   };
 
-  nativeBuildInputs = [ wrapPython ];
-
-  buildInputs = [ nss ];
-
-  installPhase = ''
-    runHook preInstall
-
-    install -Dm 0755 firefox_decrypt.py "$out/bin/firefox_decrypt"
-
-    runHook postInstall
-  '';
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   makeWrapperArgs = [ "--prefix" "LD_LIBRARY_PATH" ":" (lib.makeLibraryPath [ nss ]) ];
-
-  postFixup = ''
-    wrapPythonPrograms
-  '';
 
   passthru.updateScript = nix-update-script {
     extraArgs = [ "--version=branch" ];
