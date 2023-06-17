2af7b2668515d4c7991943b6ff79870aaeeca6ce
Mario Rodas <marsam@users.noreply.github.com>
2023-06-17 01:06:00 +0000

    python310Packages.sexpdata: use pyproject format

--- pkgs/development/python-modules/sexpdata/default.nix
+++ pkgs/development/python-modules/sexpdata/default.nix
@@ -1,18 +1,31 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "sexpdata";
   version = "1.0.1";
+  format = "pyproject";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-b2XxFSkYkMvOXNJpwTvfH4KkzSO8YbbhUKJ1Ee5qfV4=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   doCheck = false;
+
+  pythonImportsCheck = [
+    "sexpdata"
+  ];
 
   meta = with lib; {
     description = "S-expression parser for Python";
