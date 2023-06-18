0a7a718d21a21c2d9cd2e48d2bf1d1f8f5b7621b
Mostly Void <7rat13@gmail.com>
2023-06-18 11:40:20 +0530

    codux: init at 15.4.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3322,6 +3322,8 @@ with pkgs;
 
   codespell = callPackage ../development/tools/codespell { };
 
+  codux = callPackage ../applications/editors/codux { };
+
   coolreader = libsForQt5.callPackage ../applications/misc/coolreader { };
 
   corsair = with python3Packages; toPythonApplication corsair-scan;
