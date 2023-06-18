e27b2a77320a87affea40a9f0446acea616fca32
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-18 09:43:28 +0200

    python3Packages.django-leaflet: add changelog to meta

--- pkgs/development/python-modules/django-leaflet/default.nix
+++ pkgs/development/python-modules/django-leaflet/default.nix
@@ -23,7 +23,7 @@ buildPythonPackage rec {
 
   # The tests seem to be impure.
   # They are throwing a error about unset configs:
-  # > django.core.exceptions.ImproperlyConfigured: Requested setting LEAFLET_CONFIG, but settings are not configured. You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.
+  # > django.core.exceptions.ImproperlyConfigured: Requested setting LEAFLET_CONFIG, but settings are not configured.
   doCheck = false;
 
   # This dosn't work either because of the same exception as above
@@ -32,6 +32,7 @@ buildPythonPackage rec {
   meta = with pkgs.lib; {
     description = "Allows you to use Leaflet in your Django projects";
     homepage = "https://github.com/makinacorpus/django-leaflet";
+    changelog = "https://github.com/makinacorpus/django-leaflet/blob/${version}/CHANGES";
     license = licenses.lgpl3Only;
     maintainers = with maintainers; [ janik ];
   };
