8fa58b165281b8930a50c463121190bef0d5b40d
lyonsyonii <liam@garriga.dev>
2023-06-19 00:13:53 +0200

    maintainers: add lyonsyonii

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9662,6 +9662,12 @@
     githubId = 39707188;
     name = "Lynn Dong";
   };
+  lyonsyonii = {
+    email = "liam@garriga.dev";
+    github = "LyonSyonII";
+    githubId = 69039201;
+    name = "Liam Garriga";
+  };
   m00wl = {
     name = "Moritz Lumme";
     email = "moritz.lumme@gmail.com";
