808230e172a850be5398cc61f2c7331b7db35805
Madoura <madouura@gmail.com>
2023-06-19 13:04:38 -0500

    fs2-knossos: init at 0.14.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37041,6 +37041,8 @@ with pkgs;
 
   freenukum = callPackage ../games/freenukum { };
 
+  fs2-knossos = python3Packages.callPackage ../games/fs2-knossos { };
+
   inherit (callPackages ../games/fteqw {})
     fteqw
     fteqw-dedicated
