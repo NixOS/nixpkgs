afaa3c2fe1ebdd12bbe957ad619948897486b6a5
Mario Rodas <marsam@users.noreply.github.com>
2023-06-19 04:20:00 +0000

    flow: 0.208.0 -> 0.209.0
    Diff: https://github.com/facebook/flow/compare/v0.208.0...v0.209.0
    
    Changelog: https://github.com/facebook/flow/blob/v0.209.0/Changelog.md

--- pkgs/development/tools/analysis/flow/default.nix
+++ pkgs/development/tools/analysis/flow/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "flow";
-  version = "0.208.0";
+  version = "0.209.0";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "flow";
     rev = "v${version}";
-    sha256 = "sha256-fZWSTSq8C4LwOuZaR4HtxJ5jwVKqmcIzf2zTyHeK1Cs=";
+    sha256 = "sha256-6PqDCQzK7L8ucFxNEWtJ2ExHcBz2yacxu8rbC21MywE=";
   };
 
   postPatch = ''
@@ -32,7 +32,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "A static type checker for JavaScript";
     homepage = "https://flow.org/";
-    changelog = "https://github.com/facebook/flow/raw/v${version}/Changelog.md";
+    changelog = "https://github.com/facebook/flow/blob/v${version}/Changelog.md";
     license = licenses.mit;
     platforms = ocamlPackages.ocaml.meta.platforms;
     maintainers = with maintainers; [ marsam puffnfresh ];
