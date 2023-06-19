47f95712770449202455aaaaa99b5797c8f3c755
Shane Sveller <shanesveller@gmail.com>
2023-06-19 08:47:00 -0500

    elixir: Locate generate_app.escript via defaulted argument
    (cherry picked from commit e5df5ac630e2713ea362ef335be4631a23c957fb)

--- pkgs/development/interpreters/elixir/1.15.nix
+++ pkgs/development/interpreters/elixir/1.15.nix
@@ -4,4 +4,5 @@ mkDerivation {
   sha256 = "sha256-o5MfA0UG8vpnPCH1EYspzcN62yKZQcz5uVUY47hOL9w=";
   # https://hexdocs.pm/elixir/1.15.0/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
   minimumOTPVersion = "24";
+  escriptPath = "lib/elixir/scripts/generate_app.escript";
 }
