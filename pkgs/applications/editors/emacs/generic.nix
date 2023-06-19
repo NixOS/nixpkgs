2ee8c3e4a02c6fcf3a1a1373af54b03ea6c8c6d7
adisbladis <adisbladis@gmail.com>
2023-06-20 01:59:32 +1200

    emacs: add nativeComp passthru
    As reported by @terlar in
    https://github.com/NixOS/nixpkgs/pull/235859#issuecomment-1597012885
    the removal of nativeComp in passthru breaks external tooling.
    
    This adds back `passthru.nativeComp` with a deprecation notice which sets the removal for 23.11.
    
    (cherry picked from commit 75650d79da6c841ae27cd4b1392a11d74b117c26)

--- pkgs/applications/editors/emacs/generic.nix
+++ pkgs/applications/editors/emacs/generic.nix
@@ -386,6 +386,8 @@ mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
     inherit withTreeSitter;
     pkgs = recurseIntoAttrs (emacsPackagesFor finalAttrs.finalPackage);
     tests = { inherit (nixosTests) emacs-daemon; };
+    # Backwards compatibility aliases. Remove this at some point before 23.11 release cut-off.
+    nativeComp = builtins.trace "emacs.passthru: nativeComp was renamed to withNativeCompilation and will be removed in 23.11" withNativeCompilation;
   };
 
   meta = {
