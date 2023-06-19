99ad2409dd7c4e09bd371e2688679518b315e069
Adam Stephens <adam@valkor.net>
2023-06-19 13:15:30 -0400

    leftwm: 0.4.1 -> 0.4.2

--- pkgs/applications/window-managers/leftwm/default.nix
+++ pkgs/applications/window-managers/leftwm/default.nix
@@ -6,20 +6,16 @@ in
 
 rustPlatform.buildRustPackage rec {
   pname = "leftwm";
-  version = "0.4.1";
+  version = "0.4.2";
 
   src = fetchFromGitHub {
     owner = "leftwm";
     repo = "leftwm";
     rev = version;
-    sha256 = "sha256-ZAlX8Vu4JAwQlwBOHT435Bz3g3qqK5ePm9v0cDqP8Q4=";
+    sha256 = "sha256-SjEp0gQHwq3Omhx/EPnyLeQJ50Ov0rHDxmYVWBwIDBs=";
   };
 
-  cargoSha256 = "sha256-nn/P9ZZNf1Zts4JiJ2kXWAAG/HT1GnlYHXcPijYiBlU=";
-
-  cargoPatches = [
-    ./0001-patch-version.patch
-  ];
+  cargoSha256 = "sha256-kdGqnfzO+Ev9QeZcZqISPTehEXZzCWT5S8p6JbTBreE=";
 
   buildInputs = rpathLibs;
 
