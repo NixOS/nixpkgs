b95a6993f2761b6b67b817c21f41a4f5eb99c218
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-06-19 15:46:40 +0200

    zenroom: init at 3.5.0

--- pkgs/development/interpreters/zenroom/default.nix
+++ pkgs/development/interpreters/zenroom/default.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, fetchurl 
+, fetchurl
 }:
 
 stdenv.mkDerivation (finalAttrs: {
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2504,8 +2504,8 @@ with pkgs;
   yapesdl = callPackage ../applications/emulators/yapesdl { };
 
   zesarux = callPackage ../applications/emulators/zesarux { };
-  
-  zenroom = callPackage ../development/interpreters/zenroom { }; 
+
+  zenroom = callPackage ../development/interpreters/zenroom { };
 
   zsnes = pkgsi686Linux.callPackage ../applications/emulators/zsnes { };
   zsnes2 = pkgsi686Linux.callPackage ../applications/emulators/zsnes/2.x.nix { };
