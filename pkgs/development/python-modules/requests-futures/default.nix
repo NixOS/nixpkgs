d85eb0045cb2f566d84cd57372482f564fcae506
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-20 08:19:31 +0200

    python310Packages.requests-futures: add changelog to meta

--- pkgs/development/python-modules/requests-futures/default.nix
+++ pkgs/development/python-modules/requests-futures/default.nix
@@ -1,4 +1,8 @@
-{ buildPythonPackage, fetchPypi, requests, lib }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, requests
+}:
 
 buildPythonPackage rec {
   pname = "requests-futures";
@@ -9,16 +13,21 @@ buildPythonPackage rec {
     sha256 = "sha256-9VpO+ABw4oWOfR5zEj0r+uryW5P9NDhNjd8UjitnY3M=";
   };
 
-  propagatedBuildInputs = [ requests ];
+  propagatedBuildInputs = [
+    requests
+  ];
 
-  # tests are disabled because they require being online
+  # Tests are disabled because they require being online
   doCheck = false;
 
-  pythonImportsCheck = [ "requests_futures" ];
+  pythonImportsCheck = [
+    "requests_futures"
+  ];
 
   meta = with lib; {
     description = "Asynchronous Python HTTP Requests for Humans using Futures";
     homepage = "https://github.com/ross/requests-futures";
+    changelog = "https://github.com/ross/requests-futures/blob/v${version}/CHANGELOG.md";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ applePrincess ];
   };
