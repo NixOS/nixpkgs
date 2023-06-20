NixOS/nixpkgs

commit 219b397071617fb3a71be757f64f0586be971ad4

dtc: avoid building tests in case doCheck is disabled
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

pkgs/development/compilers/dtc/default.nix
@@ -42,6 +42,12 @@ stdenv.mkDerivation rec {
       url = "https://github.com/dgibson/dtc/commit/3acde70714df3623e112cf3ec99fc9b5524220b8.patch";
       sha256 = "sha256-uLXL0Sjcn+bnMuF+A6PjUW1Rq6uNg1dQl58zbeYpP/U=";
     })
+
+    # meson: allow disabling tests
+    (fetchpatch {
+      url = "https://github.com/dgibson/dtc/commit/35f26d2921b68d97fefbd5a2b6e821a2f02ff65d.patch";
+      sha256 = "sha256-cO4f/jJX/pQL7kk4jpKUhsCVESW2ZuWaTr7z3BuvVkw=";
+    })
   ];
 
   env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
@@ -77,6 +83,7 @@ stdenv.mkDerivation rec {
   mesonAutoFeatures = "auto";
   mesonFlags = [
     (lib.mesonBool "static-build" stdenv.hostPlatform.isStatic)
+    (lib.mesonBool "tests" doCheck)
   ];
 
   postFixup = lib.optionalString stdenv.isDarwin ''

commit b949778bed83f359787cc9af76579b8155bf49b5

dtc: fix cross
Now takes into account that stdenv always disables tests when cross
compiling.

Link: https://github.com/dgibson/dtc/pull/103#pullrequestreview-1473442177
Fixes: 219b39707161 ("dtc: avoid building tests in case doCheck is disabled")

pkgs/development/compilers/dtc/default.nix
@@ -83,7 +83,7 @@ stdenv.mkDerivation (finalAttrs: {
   mesonAutoFeatures = "auto";
   mesonFlags = [
     (lib.mesonBool "static-build" stdenv.hostPlatform.isStatic)
-    (lib.mesonBool "tests" finalAttrs.doCheck)
+    (lib.mesonBool "tests" finalAttrs.finalPackage.doCheck)
   ];
 
   # Checks are broken on aarch64 darwin