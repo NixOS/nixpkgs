bb448a2c7ab62ac6210b0d74c570750201d40719
Adam Joseph <adam@westernsemico.com>
2023-06-19 18:51:50 -0700

    lib/systems: add vendors.{ibm,xilinx}

--- lib/systems/parse.nix
+++ lib/systems/parse.nix
@@ -221,7 +221,9 @@ rec {
   vendors = setTypes types.openVendor {
     apple = {};
     pc = {};
+    ibm = {};
     knuth = {};
+    xilinx = {};
 
     # Actually matters, unlocking some MinGW-w64-specific options in GCC. See
     # bottom of https://sourceforge.net/p/mingw-w64/wiki2/Unicode%20apps/
