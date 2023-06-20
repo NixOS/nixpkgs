2b0c45f123f2fccc0e187506c5f9986978c8e0ad
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-20 09:04:22 +0200

    python311Packages.pyopnsense: add changelog to meta

--- pkgs/development/python-modules/pyopnsense/default.nix
+++ pkgs/development/python-modules/pyopnsense/default.nix
@@ -14,11 +14,12 @@
 buildPythonPackage rec {
   pname = "pyopnsense";
   version = "0.4.0";
+
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-3DKlVrOtMa55gTu557pgojRpdgrO5pEZ3L+9gKoW9yg=";
+    hash = "sha256-3DKlVrOtMa55gTu557pgojRpdgrO5pEZ3L+9gKoW9yg=";
   };
 
   propagatedBuildInputs = [
@@ -34,11 +35,14 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "pyopnsense" ];
+  pythonImportsCheck = [
+    "pyopnsense"
+  ];
 
   meta = with lib; {
     description = "Python client for the OPNsense API";
     homepage = "https://github.com/mtreinish/pyopnsense";
+    changelog = "https://github.com/mtreinish/pyopnsense/releases/tag/${version}";
     license = with licenses; [ gpl3Plus ];
     maintainers = with maintainers; [ fab ];
   };
