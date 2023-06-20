53faea4ce240aae614dee423731a59ebdf58af7a
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-20 08:10:44 +0200

    python310Packages.mmh3: update meta

--- pkgs/development/python-modules/mmh3/default.nix
+++ pkgs/development/python-modules/mmh3/default.nix
@@ -12,11 +12,15 @@ buildPythonPackage rec {
     sha256 = "sha256-BWuD0E5ZVUfQQHzI5apdi6iAKor6QXtkwcMCNbU4njA=";
   };
 
-  pythonImportsCheck = [ "mmh3" ];
+  pythonImportsCheck = [
+    "mmh3"
+  ];
 
   meta = with lib; {
     description = "Python wrapper for MurmurHash3, a set of fast and robust hash functions";
-    homepage = "https://pypi.org/project/mmh3/";
+    homepage = "https://github.com/hajimes/mmh3";
+    changelog = "https://github.com/hajimes/mmh3/blob/v${version}/CHANGELOG.md";
     license = licenses.cc0;
+    maintainers = with maintainers; [ ];
   };
 }
