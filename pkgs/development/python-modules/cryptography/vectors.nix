fe46838b2145d4776f6b7cfce6e471359aaebaa8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-06-21 14:13:56 +0200

    python310Packages.cryptography: 40.0.1 -> 41.0.1

--- pkgs/development/python-modules/cryptography/vectors.nix
+++ pkgs/development/python-modules/cryptography/vectors.nix
@@ -1,16 +1,18 @@
-{ buildPythonPackage, fetchPypi, lib, cryptography }:
+{ buildPythonPackage, fetchPypi, lib, cryptography, setuptools }:
 
 buildPythonPackage rec {
   pname = "cryptography-vectors";
   # The test vectors must have the same version as the cryptography package
   inherit (cryptography) version;
-  format = "setuptools";
+  format = "pyproject";
 
   src = fetchPypi {
     pname = "cryptography_vectors";
     inherit version;
-    hash = "sha256-hGBwa1tdDOSoVXHKM4nPiPcAu2oMYTPcn+D1ovW9oEE=";
+    hash = "sha256-I7CMOXd+x7J3ShH5RdF0YwGx6I7P8uUybX9Q6g9C1YA=";
   };
+
+  nativeBuildInputs = [ setuptools ];
 
   # No tests included
   doCheck = false;
@@ -20,7 +22,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Test vectors for the cryptography package";
     homepage = "https://cryptography.io/en/latest/development/test-vectors/";
-    # Source: https://github.com/pyca/cryptography/tree/master/vectors;
+    downloadPage = "https://github.com/pyca/cryptography/tree/master/vectors";
     license = with licenses; [ asl20 bsd3 ];
     maintainers = with maintainers; [ SuperSandro2000 ];
   };
