e3db2a39c2c8982edc16a19650f578f2cd223748
Oliver Richter <oliver.richter@secunet.com>
2023-06-21 13:05:51 +0200

    add orichter as maintainer
    Signed-off-by: Oliver Richter <richter-oliver@gmx.net>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12389,6 +12389,12 @@
     githubId = 75299;
     name = "Malcolm Matalka";
   };
+  orichter = {
+    email = "richter-oliver@gmx.net";
+    github = "RichterOliver";
+    githubId = 135209509;
+    name = "Oliver Richter";
+  };
   orivej = {
     email = "orivej@gmx.fr";
     github = "orivej";
