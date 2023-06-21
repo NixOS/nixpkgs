0000007da604a09e5000d7cc8c5466670f29c210
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-06-21 13:12:20 +0200

    kubecolor: 0.0.21 -> 0.0.25, switch back to upstream, cleanup maintainers

--- pkgs/applications/networking/cluster/kubecolor/default.nix
+++ pkgs/applications/networking/cluster/kubecolor/default.nix
@@ -2,24 +2,24 @@
 
 buildGoModule rec {
   pname = "kubecolor";
-  version = "0.0.21";
+  version = "0.0.25";
 
   src = fetchFromGitHub {
-    owner = pname;
-    repo = pname;
+    owner = "hidetatz";
+    repo = "kubecolor";
     rev = "v${version}";
-    sha256 = "sha256-d1gtbpeK9vp8bwhsMOPVKmohfyEZtQuvRB36VZCB3sY=";
+    sha256 = "sha256-FyKTI7Br9BjSpmf9ch2E4EZAWM7/jowZfRrCn4GTcps=";
   };
 
-  vendorSha256 = "sha256-g5bLi0HQ7LQM+DKn5x8enXn8/9j3LFhgDjQ+YN0M7dM=";
+  vendorSha256 = "sha256-DLj7ztOFNmDru1sO+ezecQeRbIbOq49M4EcJuWLNstI=";
 
   ldflags = [ "-s" "-w" "-X main.Version=${version}" ];
 
   meta = with lib; {
     description = "Colorizes kubectl output";
-    homepage = "https://github.com/kubecolor/kubecolor";
-    changelog = "https://github.com/kubecolor/kubecolor/releases/tag/v${version}";
+    homepage = "https://github.com/hidetatz/kubecolor";
+    changelog = "https://github.com/hidetatz/kubecolor/releases/tag/v${version}";
     license = licenses.mit;
-    maintainers = with maintainers; [ ivankovnatsky SuperSandro2000 ];
+    maintainers = with maintainers; [ SuperSandro2000 ];
   };
 }
