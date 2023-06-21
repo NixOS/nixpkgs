1a66e5ce9fee4d5b340866fe899ad5211a9f8753
toonn <toonn@toonn.io>
2023-06-21 19:10:04 +0200

    darwin."10.13.6".xnu: Introduce XNU version for macOS 10.13.6
    By introducing the new XNU without disturbing the old version, we avoid
    causing a stdenv rebuild. We can still depend on this new XNU in a new
    bootstrap-tools and further source releases.
    
    I'm doing this in the hope it'll enable smaller reviewable changes
    rather than needing to understand everything all at once.

--- pkgs/os-specific/darwin/apple-source-releases/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/default.nix
@@ -342,7 +342,9 @@ developerToolsPackages_11_3_1 // macosPackages_11_0_1 // {
             # Have packages depend on each other
             # rather than previous versions.
             self."10.13.6"
-            { }."${namePath}" or namePath;
+            {
+              xnu = "xnu/10.13.6.nix";
+            }."${namePath}" or namePath;
       in
       {
         inherit (import ./macos-10.13.6.nix { applePackage' = applePackageMapping; })
@@ -358,6 +360,7 @@ developerToolsPackages_11_3_1 // macosPackages_11_0_1 // {
           mDNSResponder
           objc4
           removefile
+          xnu
           ;
       };
 }
