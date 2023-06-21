c4f0ebd3182772ea40bb2d1abe6e69c707fdffd9
Nick Cao <nickcao@nichi.co>
2023-06-21 20:39:58 +0800

    ustreamer: 5.38 -> 5.41
    Diff: https://github.com/pikvm/ustreamer/compare/v5.38...v5.41

--- pkgs/applications/video/ustreamer/default.nix
+++ pkgs/applications/video/ustreamer/default.nix
@@ -2,23 +2,20 @@
 
 stdenv.mkDerivation rec {
   pname = "ustreamer";
-  version = "5.38";
+  version = "5.41";
 
   src = fetchFromGitHub {
     owner = "pikvm";
     repo = "ustreamer";
     rev = "v${version}";
-    sha256 = "sha256-pc1Pf8KnjGPb74GbcmHaj/XCD0wjgiglaAKjnZUa6Ag=";
+    hash = "sha256-N70wBKiKfOhlAR9qOSkc6dlO44lJXHWiUYb8nwXMKxo=";
   };
 
   buildInputs = [ libbsd libevent libjpeg ];
 
-  enableParallelBuilding = true;
+  makeFlags = [ "PREFIX=${placeholder "out"}" ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ustreamer $out/bin/
-  '';
+  enableParallelBuilding = true;
 
   meta = with lib; {
     homepage = "https://github.com/pikvm/ustreamer";
