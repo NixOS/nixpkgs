13f727eeced7c5813fce75bbbe72b9861d41dea0
Maximilian Bosch <maximilian@mbosch.me>
2023-06-21 17:55:23 +0200

    linux: 6.3.8 -> 6.3.9

--- pkgs/os-specific/linux/kernel/linux-6.3.nix
+++ pkgs/os-specific/linux/kernel/linux-6.3.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.3.8";
+  version = "6.3.9";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "0m89safyzi0rklsqvii5vkg92rdmvnl4lvyk6m648bhf4lhx88s3";
+    sha256 = "0gmi55hhdw1f1qyvd04v17x596yh8wis42vmcd8vhymik49z5v21";
   };
 } // (args.argsOverride or { }))
