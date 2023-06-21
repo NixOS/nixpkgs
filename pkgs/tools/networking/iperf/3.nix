b18da9ae302892e25cb9e85c171709b69a230b3f
Franz Pletz <fpletz@fnordicwalking.de>
2023-06-21 13:23:26 +0200

    iperf: 3.12 -> 3.13

--- pkgs/tools/networking/iperf/3.nix
+++ pkgs/tools/networking/iperf/3.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "iperf";
-  version = "3.12";
+  version = "3.13";
 
   src = fetchurl {
     url = "https://downloads.es.net/pub/iperf/iperf-${version}.tar.gz";
-    sha256 = "sha256-cgNOz7an1tZ+OE4Z+27/8yNspPftTFGNfbZJxEfh/9Y=";
+    sha256 = "sha256-vuQnrrE9ai7iIHPyMmH2NxLYK++qg6yMtNtdpMK9yGU=";
   };
 
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isLinux [ lksctp-tools ];
