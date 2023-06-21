c40cd4c936b71f57e1f5df9ff97fe465d9c1ef8e
figsoda <figsoda@pm.me>
2023-06-21 10:17:51 -0400

    felix-fm: 2.4.0 -> 2.4.1
    Diff: https://github.com/kyoheiu/felix/compare/v2.4.0...v2.4.1
    
    Changelog: https://github.com/kyoheiu/felix/blob/v2.4.1/CHANGELOG.md

--- pkgs/applications/file-managers/felix-fm/default.nix
+++ pkgs/applications/file-managers/felix-fm/default.nix
@@ -9,19 +9,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "felix";
-  version = "2.4.0";
+  version = "2.4.1";
 
   src = fetchFromGitHub {
     owner = "kyoheiu";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-yaAjw0Ayg0FuR5GVpmjwA7P1KfWFkXP0YsjNLhSl1FI=";
+    sha256 = "sha256-F2Zw72RdKarrwM47a+Wqe1R1TOK97Ek5AnmAe/0MheY=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "syntect-5.0.0" = "sha256-Ql3zpfjZ5nopmqZyVOJ8qcRA8eXm6ZYzLsAnGA1+upY=";
+      "syntect-5.0.0" = "sha256-RMdO5+oHLpNlAynvNIbCI0ia4KzaOO9IYwpiX6ZTwno=";
     };
   };
 
