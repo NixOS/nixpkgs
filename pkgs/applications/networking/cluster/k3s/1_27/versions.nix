b671c2774dc8bfb617f9b76ad4b007828984cce2
Euan Kemp <euank@euank.com>
2023-06-21 23:41:56 +0900

    k3s_1_27: 1.27.1+k3s1 -> 1.27.2+k3s1
    This also required making a builder change.
    
    See https://github.com/k3s-io/k3s/issues/7475 for context.
    
    This applies to 1.26 and 1.27, it just happens that 1.27 also worked
    without it

--- pkgs/applications/networking/cluster/k3s/1_27/versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_27/versions.nix
@@ -1,14 +1,14 @@
 {
-  k3sVersion = "1.27.1+k3s1";
-  k3sCommit = "bc5b42c27908ab430101eff0db0a0b22f870bd7a";
-  k3sRepoSha256 = "1xj3blfayrsfbcsljjdaswy49hhz8yiwf1d85arnsgbn8fidswpm";
-  k3sVendorSha256 = "sha256-+sM2fjS88kxMQzra2t+jU1IaKCoJpW7p3w7lCOv5mMU=";
+  k3sVersion = "1.27.2+k3s1";
+  k3sCommit = "213d7ad499e166290872f51c63d8eaa2f1fe78b3";
+  k3sRepoSha256 = "0qjkrhmjf4fyclnpyhb059dzxghpzshrs5a5z1vc83mrz1zg6vbq";
+  k3sVendorSha256 = "sha256-ZSfQIBS8KsNkYPUH2er6iL3A02SIJwXZ5YLd3NYFl8E=";
   chartVersions = import ./chart-versions.nix;
-  k3sRootVersion = "0.12.1";
-  k3sRootSha256 = "0724yx3zk89m2239fmdgwzf9w672pik71xqrvgb7pdmknmmdn9f4";
-  k3sCNIVersion = "1.1.1-k3s1";
-  k3sCNISha256 = "14mb3zsqibj1sn338gjmsyksbm0mxv9p016dij7zidccx2rzn6nl";
-  containerdVersion = "1.6.19-k3s1";
-  containerdSha256 = "12dwqh77wplg30kdi73d90qni23agw2cwxjd2p5lchq86mpmmwwr";
+  k3sRootVersion = "0.12.2";
+  k3sRootSha256 = "1gjynvr350qni5mskgm7pcc7alss4gms4jmkiv453vs8mmma9c9k";
+  k3sCNIVersion = "1.2.0-k3s1";
+  k3sCNISha256 = "0hzcap4vbl94zsiqc66dlwjgql50gw5g6f0adag0p8yqwcy6vaw2";
+  containerdVersion = "1.7.1-k3s1";
+  containerdSha256 = "00k7nkclfxwbzcgnn8s7rkrxyn0zpk57nyy18icf23wsj352gfrn";
   criCtlVersion = "1.26.0-rc.0-k3s1";
 }
