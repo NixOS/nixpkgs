922fd056ce0bcb8a095e206f21c74cb55bbc0c5f
mdarocha <git@mdarocha.pl>
2023-06-21 17:06:26 +0200

    buildDotnetModule: pass runtimeId whenever possible and disable trimming when not allowed. This fixes up some build errors

--- pkgs/build-support/dotnet/build-dotnet-module/hooks/dotnet-install-hook.sh
+++ pkgs/build-support/dotnet/build-dotnet-module/hooks/dotnet-install-hook.sh
@@ -7,9 +7,12 @@ dotnetInstallHook() {
     runHook preInstall
 
     if [ "${selfContainedBuild-}" ]; then
-        dotnetInstallFlags+=(--runtime "@runtimeId@" "--self-contained")
+        dotnetInstallFlags+=("--self-contained")
     else
         dotnetInstallFlags+=("--no-self-contained")
+        # https://learn.microsoft.com/en-us/dotnet/core/deploying/trimming/trim-self-contained
+        # Trimming is only available for self-contained build, so force disable it here
+        dotnetInstallFlags+=("-p:PublishTrimmed=false")
     fi
 
     if [ "${useAppHost-}" ]; then
@@ -18,12 +21,19 @@ dotnetInstallHook() {
 
     dotnetPublish() {
         local -r project="${1-}"
+
+        runtimeIdFlags=()
+        if [[ "$project" == *.csproj ]] || [ "${selfContainedBuild-}" ]; then
+            runtimeIdFlags+=("--runtime @runtimeId@")
+        fi
+
         env dotnet publish ${project-} \
             -p:ContinuousIntegrationBuild=true \
             -p:Deterministic=true \
             --output "$out/lib/${pname}" \
             --configuration "@buildType@" \
             --no-build \
+            ${runtimeIdFlags[@]} \
             ${dotnetInstallFlags[@]}  \
             ${dotnetFlags[@]}
     }
@@ -36,6 +46,7 @@ dotnetInstallHook() {
              --output "$out/share" \
              --configuration "@buildType@" \
              --no-build \
+            --runtime "@runtimeId@" \
              ${dotnetPackFlags[@]}  \
              ${dotnetFlags[@]}
     }
