d1ea141237a6f77952009156580ea10b34ffe71d
Mario Rodas <marsam@users.noreply.github.com>
2023-06-21 04:20:00 +0000

    nodejs_20: 20.3.0 -> 20.3.1
    The following CVEs are fixed in this release:
    - CVE-2023-30581: mainModule.__proto__ Bypass Experimental Policy Mechanism (High)
    - CVE-2023-30584: Path Traversal Bypass in Experimental Permission Model (High)
    - CVE-2023-30587: Bypass of Experimental Permission Model via Node.js Inspector (High)
    - CVE-2023-30582: Inadequate Permission Model Allows Unauthorized File Watching (Medium)
    - CVE-2023-30583: Bypass of Experimental Permission Model via fs.openAsBlob() (Medium)
    - CVE-2023-30585: Privilege escalation via Malicious Registry Key manipulation during Node.js installer repair process (Medium)
    - CVE-2023-30586: Bypass of Experimental Permission Model via Arbitrary OpenSSL Engines (Medium)
    - CVE-2023-30588: Process interuption due to invalid Public Key information in x509 certificates (Medium)
    - CVE-2023-30589: HTTP Request Smuggling via Empty headers separated by CR (Medium)
    - CVE-2023-30590: DiffieHellman does not generate keys after setting a private key (Medium)
    
    https://github.com/nodejs/node/releases/tag/v20.3.1

--- pkgs/development/web/nodejs/v20.nix
+++ pkgs/development/web/nodejs/v20.nix
@@ -9,8 +9,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "20.3.0";
-  sha256 = "sha256-G6jUlCPtOnVykGa7PqJkk+6ct9ZWjvlIWX/J70VPdDU=";
+  version = "20.3.1";
+  sha256 = "sha256-EqgtswZpeVm0OJs1Gl+XhImGsTE/mQGw4LPYz08/mZE=";
   patches = [
     ./revert-arm64-pointer-auth.patch
     ./disable-darwin-v8-system-instrumentation-node19.patch
