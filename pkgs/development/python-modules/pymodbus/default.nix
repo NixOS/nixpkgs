cbb2f9ea685cb9143fdac697e96255ef32663a88
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-22 16:12:12 +0000

    python310Packages.pymodbus: 3.1.3 -> 3.3.2

--- pkgs/development/python-modules/pymodbus/default.nix
+++ pkgs/development/python-modules/pymodbus/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "pymodbus";
-  version = "3.1.3";
+  version = "3.3.2";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "pymodbus-dev";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-GHyDlt046v4KP9KQRnXH6F+3ikoCjbhVHEQuSdm99a8=";
+    hash = "sha256-EGJyb0AVLKN7FEoeWF4rVqmJBNbXHent9P+cxc13rQs=";
   };
 
   # Twisted asynchronous version is not supported due to a missing dependency
