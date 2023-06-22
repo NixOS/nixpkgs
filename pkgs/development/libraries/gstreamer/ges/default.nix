a6699dcbd38d7b384dd268c64a14c6a6af71c406
Lily Foster <lily@lily.flowers>
2023-06-21 20:40:00 -0400

    gst_all_1.gst-editing-services: 1.22.3 -> 1.22.4
    (cherry picked from commit 1315ca3a292d09dfe256714ba1799ec529b701c5)

--- pkgs/development/libraries/gstreamer/ges/default.nix
+++ pkgs/development/libraries/gstreamer/ges/default.nix
@@ -18,7 +18,7 @@
 
 stdenv.mkDerivation rec {
   pname = "gst-editing-services";
-  version = "1.22.3";
+  version = "1.22.4";
 
   outputs = [
     "out"
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://gstreamer.freedesktop.org/src/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-FaW85iUJseEl6FoJ7EHzIcD1SSZLUETACV/PstXAzqI=";
+    hash = "sha256-RThGz6RxoMHIAUVRvUhPvPM08ORDBZIxevuyN3Xmm9I=";
   };
 
   nativeBuildInputs = [
