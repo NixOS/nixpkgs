c57100a0ec9fa91468fbb2f23d1821a8a15cb524
ajs124 <git@ajs124.de>
2023-06-22 13:36:44 +0200

    mariadb-galera: add passthru.tests

--- pkgs/servers/sql/mariadb/galera/default.nix
+++ pkgs/servers/sql/mariadb/galera/default.nix
@@ -1,5 +1,6 @@
 { lib, stdenv, fetchFromGitHub, buildEnv
 , asio, boost, check, openssl, cmake
+, nixosTests
 }:
 
 stdenv.mkDerivation rec {
@@ -28,6 +29,10 @@ stdenv.mkDerivation rec {
     mkdir $out/lib/galera
     ln -s $out/lib/libgalera_smm.so $out/lib/galera/libgalera_smm.so
   '';
+
+  passthru.tests = {
+    inherit (nixosTests) mariadb-galera;
+  };
 
   meta = with lib; {
     description = "Galera 3 wsrep provider library";
