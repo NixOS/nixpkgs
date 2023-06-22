f13586b7fe212500ebd089c7cdd6c03614ae3479
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-06-22 11:41:38 +0200

    php83: init at 8.3.0alpha2

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -46,6 +46,7 @@
 , uwimap
 , valgrind
 , zlib
+, fetchpatch
 }:
 
 lib.makeScope pkgs.newScope (self: with self; {
@@ -332,7 +333,16 @@ lib.makeScope pkgs.newScope (self: with self; {
         }
         { name = "exif"; doCheck = false; }
         { name = "ffi"; buildInputs = [ libffi ]; }
-        { name = "fileinfo"; buildInputs = [ pcre2 ]; }
+        {
+          name = "fileinfo";
+          buildInputs = [ pcre2 ];
+          patches = lib.optionals (lib.versionAtLeast php.version "8.3") [
+            # Fix the extension unable to be loaded due to missing `get_module` function.
+            # `ZEND_GET_MODULE` macro that creates it is conditional on `COMPILE_DL_FILEINFO` being defined.
+            # https://github.com/php/php-src/issues/11408#issuecomment-1602106200
+            ../development/interpreters/php/fix-fileinfo-ext-php83.patch
+          ];
+        }
         { name = "filter"; buildInputs = [ pcre2 ]; }
         { name = "ftp"; buildInputs = [ openssl ]; }
         {
@@ -519,7 +529,18 @@ lib.makeScope pkgs.newScope (self: with self; {
           '';
           doCheck = false;
         }
-        { name = "session"; doCheck = false; }
+        { name = "session";
+          doCheck = false;
+          patches = lib.optionals (lib.versionAtLeast php.version "8.3") [
+            # Fix GH-11529: Crash after dealing with an Apache request
+            # To be removed in next alpha
+            # See https://github.com/php/php-src/issues/11529
+            (fetchpatch {
+              url = "https://github.com/php/php-src/commit/8d4370954ec610164a4503431bb0c52da6954aa7.patch";
+              hash = "sha256-w1uF9lRdfhz9I0gux0J4cvMzNS93uSHL1fYG23VLDPc=";
+            })
+          ];
+        }
         { name = "shmop"; }
         {
           name = "simplexml";
