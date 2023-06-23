dd2eef118ec4fa379bde0b356c1e36132b7addfd
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-23 14:18:06 +0000

    level-zero: 1.11.0 -> 1.12.0

--- pkgs/development/libraries/level-zero/default.nix
+++ pkgs/development/libraries/level-zero/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "level-zero";
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchFromGitHub {
     owner = "oneapi-src";
     repo = "level-zero";
     rev = "refs/tags/v${version}";
-    hash = "sha256-hCipWbhVsYYqfGXO6CFDPmxiFO7Dc0I/nCnj6lRS2go=";
+    hash = "sha256-9XfvU01nVcY2NpCqjUyPEJ+F/+A4YjjE0ktMQyEkr3Q=";
   };
 
   nativeBuildInputs = [ cmake addOpenGLRunpath ];
