53ec2d2b0f8f0f5a6f4a5196c3cb04ff336b6801
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-23 05:33:05 +0000

    python311Packages.globus-sdk: 3.21.0 -> 3.22.0

--- pkgs/development/python-modules/globus-sdk/default.nix
+++ pkgs/development/python-modules/globus-sdk/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "globus-sdk";
-  version = "3.21.0";
+  version = "3.22.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "globus";
     repo = "globus-sdk-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-1qWjg4EfN6KDxPX2jVQ/hCI2y3yCVHTpt57cUmObkQw=";
+    hash = "sha256-Dngt/vwFgRUS3pOaanlx+qOQfSzKr4/KVZ51pLXCMMA=";
   };
 
   propagatedBuildInputs = [
