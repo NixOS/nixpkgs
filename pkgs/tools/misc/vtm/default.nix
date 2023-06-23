6a7265ed5089e6cf632d76b36bb07e020d3bf36e
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-23 10:58:33 +0000

    vtm: 0.9.9m -> 0.9.9n

--- pkgs/tools/misc/vtm/default.nix
+++ pkgs/tools/misc/vtm/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "vtm";
-  version = "0.9.9m";
+  version = "0.9.9n";
 
   src = fetchFromGitHub {
     owner = "netxs-group";
     repo = "vtm";
     rev = "v${version}";
-    sha256 = "sha256-l9EMoUUM4pH+YwzdrTXhBawiW1l2OzX9ou18U5kM36c=";
+    sha256 = "sha256-THsIA8+yfMXM+WLVL8TCnD1CEZy1iB4hA7SzhdVT4hA=";
   };
 
   nativeBuildInputs = [ cmake ];
