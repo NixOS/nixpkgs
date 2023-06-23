0534d89d9f471e7451bfe67eb6477bb2f7f45d80
K900 <me@0upti.me>
2023-06-23 14:32:56 +0300

    mesa: 23.1.2 -> 23.1.3
    Changelog: https://www.mesa3d.org/relnotes/23.1.3.html

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -85,8 +85,8 @@
 */
 
 let
-  version = "23.1.2";
-  hash = "sha256-YLHzrbFWGDDBWL88aFCJQ2dPudafOEw8colpQ4WrXH4=";
+  version = "23.1.3";
+  hash = "sha256-L21zgbwQ+9LWJjrRAieFuLURBGwakEFi+PfaGO6ortk=";
 
   # Release calendar: https://www.mesa3d.org/release-calendar.html
   # Release frequency: https://www.mesa3d.org/releasing.html#schedule
