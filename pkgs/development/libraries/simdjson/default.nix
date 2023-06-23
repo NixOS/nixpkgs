abbca13f97708ea77607791739d7db5c7ce28d64
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-23 10:22:47 +0000

    simdjson: 3.1.8 -> 3.2.0

--- pkgs/development/libraries/simdjson/default.nix
+++ pkgs/development/libraries/simdjson/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "simdjson";
-  version = "3.1.8";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "simdjson";
     repo = "simdjson";
     rev = "v${version}";
-    sha256 = "sha256-j13yNzh9CnniXzjoB4oNtDwYcao6MOVgyWo9JtqT/yQ=";
+    sha256 = "sha256-6Wa/rnm5lPATeqbQJ6QC+pn8D3l/petPvokEewXYEUA=";
   };
 
   nativeBuildInputs = [ cmake ];
