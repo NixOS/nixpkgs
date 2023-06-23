ef70e3d22b9ecce3fd8c52244583e02a54bbdf6d
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-23 06:20:40 +0000

    linkerd: 2.13.4 -> 2.13.5

--- pkgs/applications/networking/cluster/linkerd/default.nix
+++ pkgs/applications/networking/cluster/linkerd/default.nix
@@ -2,7 +2,7 @@
 
 (callPackage ./generic.nix { }) {
   channel = "stable";
-  version = "2.13.4";
-  sha256 = "094i9a5l5nmygja1q73ipi01m6w1jsnr6l04g5629na72568zh6w";
+  version = "2.13.5";
+  sha256 = "0mjb0wcwyd51ap0kvkfmykh6zqijg4z2g5yxvp9aq67l984wh7sb";
   vendorSha256 = "sha256-5T3YrYr7xeRkAADeE24BPu4PYU4mHFspqAiBpS8n4Y0=";
 }
