f24c6373457d61bffe43dacdce719a4408c5a5fa
figsoda <figsoda@pm.me>
2023-06-23 10:29:25 -0400

    spacer: 0.1.6 -> 0.1.7
    Diff: https://github.com/samwho/spacer/compare/v0.1.6...v0.1.7
    
    Changelog: https://github.com/samwho/spacer/releases/tag/v0.1.7

--- pkgs/tools/misc/spacer/default.nix
+++ pkgs/tools/misc/spacer/default.nix
@@ -5,16 +5,21 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "spacer";
-  version = "0.1.6";
+  version = "0.1.7";
 
   src = fetchFromGitHub {
     owner = "samwho";
     repo = "spacer";
     rev = "v${version}";
-    hash = "sha256-KSR3KfZcPHrjKxHtgNhGS3ISR8bn8tXw9ED7OevTOsU=";
+    hash = "sha256-eFXxcOhUqyo3eUws3RCO0w+0XGlxZAonKFTphnrhHs8=";
   };
 
-  cargoHash = "sha256-Rjmy6l35pnaZTJmacoNRYvFLCRHkkJYZXLU9MVkVTfY=";
+  cargoHash = "sha256-z7igJc8HHqpiY2an4hFWoZElwgF5NUA+TFPqxuowC/w=";
+
+  # Cargo.lock is outdated
+  preConfigure = ''
+    cargo metadata --offline
+  '';
 
   meta = with lib; {
     description = "CLI tool to insert spacers when command output stops";
