cf5643ba78cc400fc3ae3bef2c3481e03f58e4fa
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-23 14:35:51 +0000

    fits-cloudctl: 0.11.7 -> 0.11.8

--- pkgs/tools/admin/fits-cloudctl/default.nix
+++ pkgs/tools/admin/fits-cloudctl/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "fits-cloudctl";
-  version = "0.11.7";
+  version = "0.11.8";
 
   src = fetchFromGitHub {
     owner = "fi-ts";
     repo = "cloudctl";
     rev = "v${version}";
-    sha256 = "sha256-wsv1d7CdrZeAOgY0a0L1ZjSJVahtfDsOzKaz3Uu2RWM=";
+    sha256 = "sha256-LdJ8EwTLn5PnbYAe36424qptV5LT4A7/sa2H21kGLAY=";
   };
 
-  vendorHash = "sha256-/lDhvmeeEiXP+mihrz6076Cm/29UeJ0QH9GW3hIHKB8=";
+  vendorHash = "sha256-O0E65+GtvoLwgc2RnQbogQg10v3DDHz4fhUaxNKhObM=";
 
   meta = with lib; {
     description = "Command-line client for FI-TS Finance Cloud Native services";
