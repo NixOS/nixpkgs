a4ea53187136d2251e35ec3b010c428d55a05591
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-24 00:36:48 +0000

    oxigraph: 0.3.17 -> 0.3.18

--- pkgs/servers/oxigraph/default.nix
+++ pkgs/servers/oxigraph/default.nix
@@ -8,17 +8,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "oxigraph";
-  version = "0.3.17";
+  version = "0.3.18";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-UsoNsGS2JWiI1EotUXjKwtR0WIk+z/5/a0AJySw4xnQ=";
+    sha256 = "sha256-0sAQ/jOnYO0P1EX+DY7rDJyzOkdAaO7hGQDFTgNJyQs=";
     fetchSubmodules = true;
   };
 
-  cargoHash = "sha256-lQVWpIhWTUQTcMaPJ1z8wJI7/EBU+YoFkC92JhLCxe8=";
+  cargoHash = "sha256-bRSv77fq+3f4X+NB75qtjXRHo50H61ytoRPTEjKp6W0=";
 
   nativeBuildInputs = [
     rustPlatform.bindgenHook
