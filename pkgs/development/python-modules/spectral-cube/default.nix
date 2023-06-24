1fe2d19d306eec7a91daf051051405a27ecc3c1e
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-24 10:33:37 +0200

    python311Packages.spectral-cube: unbreak

--- pkgs/development/python-modules/spectral-cube/default.nix
+++ pkgs/development/python-modules/spectral-cube/default.nix
@@ -1,17 +1,17 @@
 { lib
 , stdenv
-, fetchPypi
-, buildPythonPackage
 , aplpy
-, joblib
 , astropy
-, casa-formats-io
-, radio_beam
-, six
-, dask
-, pytestCheckHook
-, pytest-astropy
 , astropy-helpers
+, buildPythonPackage
+, casa-formats-io
+, dask
+, fetchPypi
+, joblib
+, pytest-astropy
+, pytestCheckHook
+, pythonOlder
+, radio_beam
 , setuptools-scm
 }:
 
@@ -19,6 +19,8 @@ buildPythonPackage rec {
   pname = "spectral-cube";
   version = "0.6.2";
   format = "pyproject";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
@@ -31,13 +33,28 @@ buildPythonPackage rec {
     setuptools-scm
   ];
 
-  propagatedBuildInputs = [ astropy casa-formats-io radio_beam joblib six dask ];
-  nativeCheckInputs = [ pytestCheckHook aplpy pytest-astropy ];
+  propagatedBuildInputs = [
+    astropy
+    casa-formats-io
+    radio_beam
+    joblib
+    dask
+  ];
+
+  nativeCheckInputs = [
+    aplpy
+    pytest-astropy
+    pytestCheckHook
+  ];
 
   # On x86_darwin, this test fails with "Fatal Python error: Aborted"
   # when sandbox = true.
   disabledTestPaths = lib.optionals stdenv.isDarwin [
     "spectral_cube/tests/test_visualization.py"
+  ];
+
+  pythonImportsCheck = [
+    "spectral_cube"
   ];
 
   meta = with lib; {
@@ -46,6 +63,5 @@ buildPythonPackage rec {
     changelog = "https://github.com/radio-astro-tools/spectral-cube/releases/tag/v${version}";
     license = licenses.bsd3;
     maintainers = with maintainers; [ smaret ];
-    broken = true;
   };
 }
