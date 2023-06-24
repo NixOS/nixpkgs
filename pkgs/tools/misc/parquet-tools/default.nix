e85e9885055d7ee2e0c8e8fe9730228e612c2a22
Nick Cao <nickcao@nichi.co>
2023-06-24 14:19:19 +0800

    parquet-tools: 0.2.12 -> 0.2.14

--- pkgs/tools/misc/parquet-tools/default.nix
+++ pkgs/tools/misc/parquet-tools/default.nix
@@ -7,7 +7,7 @@ with python3Packages;
 
 buildPythonApplication rec {
   pname = "parquet-tools";
-  version = "0.2.12";
+  version = "0.2.14";
 
   format = "pyproject";
 
@@ -15,13 +15,13 @@ buildPythonApplication rec {
     owner = "ktrueda";
     repo = "parquet-tools";
     rev = "refs/tags/${version}";
-    hash = "sha256-5bK+kW550DgBhcH5INozwGKKjM+xXblmFg2Tu2rnos4=";
+    hash = "sha256-2jIwDsxB+g37zV9hLc2VNC5YuZXTpTmr2aQ72AeHYJo=";
   };
 
   postPatch = ''
     substituteInPlace tests/test_inspect.py \
       --replace "parquet-cpp-arrow version 5.0.0" "parquet-cpp-arrow version ${pyarrow.version}" \
-      --replace "serialized_size: 2222" "serialized_size: 2221" \
+      --replace "serialized_size: 2222" "serialized_size: 2223" \
       --replace "format_version: 1.0" "format_version: 2.6"
   '';
 
@@ -50,12 +50,6 @@ buildPythonApplication rec {
     moto
     pytest-mock
     pytestCheckHook
-  ];
-
-  disabledTests = [
-    # These tests try to read Python code as parquet and fail
-    "test_local_wildcard"
-    "test_local_and_s3_wildcard_files"
   ];
 
   pythonImportsCheck = [
