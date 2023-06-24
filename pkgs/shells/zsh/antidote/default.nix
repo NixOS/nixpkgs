2f51e2c1cc953885b38653077e6123d5c3ef08c7
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-24 01:53:26 +0000

    antidote: 1.8.7 -> 1.8.8

--- pkgs/shells/zsh/antidote/default.nix
+++ pkgs/shells/zsh/antidote/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation (finalAttrs: {
-  version = "1.8.7";
+  version = "1.8.8";
   pname = "antidote";
 
   src = fetchFromGitHub {
     owner = "mattmc3";
     repo = "antidote";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-5ccGoxmaHV7w4w7qc61gjI12OppU1FjDUeYh8ELljWQ=";
+    hash = "sha256-UliND3WZkaXWIxWnSINPLylSK+iXyflhwf/JTjwp/wg=";
   };
 
   dontPatch = true;
