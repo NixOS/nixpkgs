fd16ac7f20b737739308a18f6ef6c24a457af67f
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-25 10:13:04 +0200

    python310Packages.autograd: add pythonImportsCheck

--- pkgs/development/python-modules/autograd/default.nix
+++ pkgs/development/python-modules/autograd/default.nix
@@ -1,4 +1,9 @@
-{ lib, buildPythonPackage, fetchPypi, numpy, future }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, numpy
+, future
+}:
 
 buildPythonPackage rec {
   pname = "autograd";
@@ -9,11 +14,18 @@ buildPythonPackage rec {
     hash = "sha256-hzHgigxOOJ2GlaQAcq2kUSZBwRO2ys6PTPvo636a7es=";
   };
 
-  propagatedBuildInputs = [ numpy future ];
+  propagatedBuildInputs = [
+    numpy
+    future
+  ];
 
   # Currently, the PyPI tarball doesn't contain the tests. When that has been
   # fixed, enable testing. See: https://github.com/HIPS/autograd/issues/404
   doCheck = false;
+
+  pythonImportsCheck = [
+    "autograd"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/HIPS/autograd";
