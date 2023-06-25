0be709d5416c89ae902d11fced4e57e3193f4fad
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-25 17:43:56 +0000

    tengine: 2.4.0 -> 2.4.1

--- pkgs/servers/http/tengine/default.nix
+++ pkgs/servers/http/tengine/default.nix
@@ -10,14 +10,14 @@
 with lib;
 
 stdenv.mkDerivation rec {
-  version = "2.4.0";
+  version = "2.4.1";
   pname = "tengine";
 
   src = fetchFromGitHub {
     owner = "alibaba";
     repo = pname;
     rev = version;
-    hash = "sha256-h/eSa2wCPvnX29wdtDHNQh8r1No1/YGinZNYU2MnsTM=";
+    hash = "sha256-h9PFzJc/29WFe4mW+xpJAL1Z2XqBxMEtQeN5DMgOVBA=";
   };
 
   buildInputs =
