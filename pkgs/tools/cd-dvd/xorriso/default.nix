d7d2970ae6964834e9faf2d787c58db89cb7234f
Anderson Torres <torres.anderson.85@protonmail.com>
2023-06-25 09:39:45 -0300

    xorriso: 1.5.6 -> 1.5.6.pl02

--- pkgs/tools/cd-dvd/xorriso/default.nix
+++ pkgs/tools/cd-dvd/xorriso/default.nix
@@ -1,39 +1,54 @@
-{ fetchurl, lib, stdenv, libcdio, zlib, bzip2, readline, acl, attr, libiconv }:
+{ lib
+, stdenv
+, fetchurl
+, acl
+, attr
+, bzip2
+, libcdio
+, libiconv
+, readline
+, zlib
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xorriso";
-  version = "1.5.6";
+  version = "1.5.6.pl02";
 
   src = fetchurl {
-    url = "mirror://gnu/xorriso/xorriso-${version}.tar.gz";
-    sha256 = "sha256-1La2a9BMScazWO5mR12AbW9tdIboARBqR9Mx3x8vj+s=";
+    url = "mirror://gnu/xorriso/xorriso-${finalAttrs.version}.tar.gz";
+    hash = "sha256-eG+fXfmGXMWwwf7O49LA9eBMq4yahZvRycfM1JZP2uE=";
   };
 
   doCheck = true;
 
-  buildInputs = [ libcdio zlib bzip2 readline libiconv ]
-    ++ lib.optionals stdenv.isLinux [ acl attr ];
+  buildInputs = [
+    bzip2
+    libcdio
+    libiconv
+    readline
+    zlib
+  ]
+  ++ lib.optionals stdenv.isLinux [
+    acl
+    attr
+  ];
 
   env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin "-include unistd.h";
 
-  meta = with lib; {
-    description = "ISO 9660 Rock Ridge file system manipulator";
-
-    longDescription =
-      '' GNU xorriso copies file objects from POSIX compliant filesystems
-         into Rock Ridge enhanced ISO 9660 filesystems and allows
-         session-wise manipulation of such filesystems.  It can load the
-         management information of existing ISO images and it writes the
-         session results to optical media or to filesystem objects.  Vice
-         versa xorriso is able to copy file objects out of ISO 9660
-         filesystems.
-      '';
-
-    license = licenses.gpl3Plus;
-
+  meta = {
     homepage = "https://www.gnu.org/software/xorriso/";
-
-    maintainers = [ maintainers.vrthra ];
-    platforms = platforms.unix;
+    description = "ISO 9660 Rock Ridge file system manipulator";
+    longDescription = ''
+      GNU xorriso copies file objects from POSIX compliant filesystems into Rock
+      Ridge enhanced ISO 9660 filesystems and allows session-wise manipulation
+      of such filesystems. It can load the management information of existing
+      ISO images and it writes the session results to optical media or to
+      filesystem objects.
+      Vice versa xorriso is able to copy file objects out of ISO 9660
+      filesystems.
+    '';
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ lib.maintainers.AndersonTorres ];
+    platforms = lib.platforms.unix;
   };
-}
+})
