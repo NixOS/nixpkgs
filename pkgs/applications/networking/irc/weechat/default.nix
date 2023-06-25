a86cae4bdde5567fe042f51c2857c760ac2c27f5
Naïm Favier <n@monade.li>
2023-06-25 12:11:44 +0200

    weechat: 3.8 -> 4.0.0
    https://github.com/weechat/weechat/releases/tag/v4.0.0

--- pkgs/applications/networking/irc/weechat/default.nix
+++ pkgs/applications/networking/irc/weechat/default.nix
@@ -36,14 +36,14 @@ let
   in
     assert lib.all (p: p.enabled -> ! (builtins.elem null p.buildInputs)) plugins;
     stdenv.mkDerivation rec {
-      version = "3.8";
+      version = "4.0.0";
       pname = "weechat";
 
       hardeningEnable = [ "pie" ];
 
       src = fetchurl {
-        url = "https://weechat.org/files/src/weechat-${version}.tar.bz2";
-        hash = "sha256-objxAUGvBhTkbQl4GshDP3RsCkAW4z917L9WyaVoYj4=";
+        url = "https://weechat.org/files/src/weechat-${version}.tar.xz";
+        hash = "sha256-W9de4V8zkqCtF07nxUUY2uzjh4R3ZwwH8Q1uv5iCQPk=";
       };
 
       outputs = [ "out" "man" ] ++ map (p: p.name) enabledPlugins;
@@ -84,7 +84,7 @@ let
       '';
 
       meta = {
-        homepage = "http://www.weechat.org/";
+        homepage = "https://weechat.org/";
         description = "A fast, light and extensible chat client";
         longDescription = ''
           You can find more documentation as to how to customize this package
