7b1b41f15da6bdd3e4dcd58ecdc6275bc59a914e
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-25 13:28:43 +0000

    nats-server: 2.9.18 -> 2.9.19

--- pkgs/servers/nats-server/default.nix
+++ pkgs/servers/nats-server/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "nats-server";
-  version = "2.9.18";
+  version = "2.9.19";
 
   src = fetchFromGitHub {
     owner = "nats-io";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Eda6i4kJbvX1Wzkwd9YSd3fes3kdkekKKhIt7xH8Yi8=";
+    hash = "sha256-CiDvDnnUR/jaEWU+bceH6uIr9QBtsAz+6vnQjT3sNQQ=";
   };
 
   vendorHash = "sha256-m/15F0BHCAEcNlRf71jJP5bjrrqRgX/CQm8SddpTkYw=";
