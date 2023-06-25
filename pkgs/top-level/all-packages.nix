c93757bf5f339b9246fd2d2d77e0eaae12540eef
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-25 09:57:21 +0200

    das: init at 0.3.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4621,6 +4621,8 @@ with pkgs;
 
   dapr-cli = callPackage ../development/tools/dapr/cli { };
 
+  das = callPackage ../tools/security/das { };
+
   dasel = callPackage ../applications/misc/dasel { };
 
   dasher = callPackage ../applications/accessibility/dasher { };
