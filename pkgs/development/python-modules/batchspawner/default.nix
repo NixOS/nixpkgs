190342668f6e78338c941ea1f114ae18fbb84c2e
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-25 23:39:14 +0200

    python311Packages.batchspawner: add missing input

--- pkgs/development/python-modules/batchspawner/default.nix
+++ pkgs/development/python-modules/batchspawner/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , fetchFromGitHub
 , jupyterhub
+, packaging
 , pythonOlder
 }:
 
@@ -21,6 +22,7 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [
     jupyterhub
+    packaging
   ];
 
   # Tests require a job scheduler e.g. slurm, pbs, etc.
