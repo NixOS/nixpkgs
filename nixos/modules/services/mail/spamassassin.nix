58b48cd7209fc181847ee850b090c4f5c3b74165
Luke Granger-Brown <git@lukegb.com>
2023-06-25 13:39:53 +0000

    nixos/spamassassin: add DMARC module to default config

--- nixos/modules/services/mail/spamassassin.nix
+++ nixos/modules/services/mail/spamassassin.nix
@@ -77,6 +77,7 @@ in
           loadplugin Mail::SpamAssassin::Plugin::Check
           #loadplugin Mail::SpamAssassin::Plugin::DCC
           loadplugin Mail::SpamAssassin::Plugin::DKIM
+          loadplugin Mail::SpamAssassin::Plugin::DMARC
           loadplugin Mail::SpamAssassin::Plugin::DNSEval
           loadplugin Mail::SpamAssassin::Plugin::FreeMail
           loadplugin Mail::SpamAssassin::Plugin::HeaderEval
