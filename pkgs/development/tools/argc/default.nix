6cd85c7715cf0b576221488e4b57159503564497
figsoda <figsoda@pm.me>
2023-06-24 20:03:33 -0400

    argc: 1.5.0 -> 1.6.0
    Diff: https://github.com/sigoden/argc/compare/v1.5.0...v1.6.0
    
    Changelog: https://github.com/sigoden/argc/releases/tag/v1.6.0

--- pkgs/development/tools/argc/default.nix
+++ pkgs/development/tools/argc/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "argc";
-  version = "1.5.0";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "sigoden";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-3e15knnM9CQn/nIh4H1Kd91qyE1KJ18jO0gk7UWz08Y=";
+    hash = "sha256-jtE1OwDEo533wGY1zya09LscED6zyjn0qu+HxyWXO1E=";
   };
 
-  cargoHash = "sha256-NnK3SNBZNvXoSJhMu7Gg9HjAmAws8eD051iSZEfhfIw=";
+  cargoHash = "sha256-+K551Am9sHUj/+0aFVxuf8/M/pnqofoVLPqqnBZWOZA=";
 
   nativeBuildInputs = [ installShellFiles ];
 
