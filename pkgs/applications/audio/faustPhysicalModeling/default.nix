facc2cdac9cf164888be8c2a7e945dbc2bf96e18
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-25 04:20:38 +0000

    faustPhysicalModeling: 2.59.6 -> 2.60.3

--- pkgs/applications/audio/faustPhysicalModeling/default.nix
+++ pkgs/applications/audio/faustPhysicalModeling/default.nix
@@ -1,13 +1,13 @@
 { stdenv, lib, fetchFromGitHub, faust2jaqt, faust2lv2 }:
 stdenv.mkDerivation rec {
   pname = "faustPhysicalModeling";
-  version = "2.59.6";
+  version = "2.60.3";
 
   src = fetchFromGitHub {
     owner = "grame-cncm";
     repo = "faust";
     rev = version;
-    sha256 = "sha256-Z/hAq6JlhlWBzWlodwQW/k9AkozVeMXmbVhkicNZ5os=";
+    sha256 = "sha256-kaKDZKs/UsrqYlGmGgpSRcqN7FypxLCcIF72klovD4k=";
   };
 
   buildInputs = [ faust2jaqt faust2lv2 ];
