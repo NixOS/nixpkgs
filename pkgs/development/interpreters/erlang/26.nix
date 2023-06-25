12bd7ee3e0e7886af5648441d2e370face79b8a6
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-25 17:03:56 +0000

    erlang_26: 26.0 -> 26.0.1

--- pkgs/development/interpreters/erlang/26.nix
+++ pkgs/development/interpreters/erlang/26.nix
@@ -1,7 +1,7 @@
 { lib, mkDerivation }:
 
 mkDerivation {
-  version = "26.0";
-  sha256 = "sha256-7z5LkCLyjqGlo48XlcwAUiu1FkmAAewEGnP30QDDme8=";
+  version = "26.0.1";
+  sha256 = "sha256-uvBJu3hAg2L2yuh/D42tEy2Gr3Adk9/o/ssgy60T4rY=";
 }
 
