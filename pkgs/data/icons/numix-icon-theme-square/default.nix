b1cec0086b719e6eca7ea1f6d81a712bf86a1f0e
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-25 17:30:01 +0000

    numix-icon-theme-square: 23.06.11 -> 23.06.21

--- pkgs/data/icons/numix-icon-theme-square/default.nix
+++ pkgs/data/icons/numix-icon-theme-square/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "numix-icon-theme-square";
-  version = "23.06.11";
+  version = "23.06.21";
 
   src = fetchFromGitHub {
     owner = "numixproject";
     repo = pname;
     rev = version;
-    sha256 = "sha256-pm3sEx3OJkS4sz+sg09HeTSdQZTdjyxJCsjYWCA4mN0=";
+    sha256 = "sha256-eJELv8lRgV687vu0PQfvZoSTaPtVyTniRINRxIIVjJg=";
   };
 
   nativeBuildInputs = [ gtk3 ];
