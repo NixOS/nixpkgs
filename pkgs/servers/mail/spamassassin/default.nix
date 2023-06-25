7cd151d299c7b9ec1dcf6de15c6ea7dc6d345588
Luke Granger-Brown <git@lukegb.com>
2023-06-25 13:35:32 +0000

    spamassassin: switch back to Perl-bundled ExtUtils::MakeMaker
    It's past the required minimum version now, so we can drop this comment
    and the explicit dependency.

--- pkgs/servers/mail/spamassassin/default.nix
+++ pkgs/servers/mail/spamassassin/default.nix
@@ -9,14 +9,7 @@ perlPackages.buildPerlPackage rec {
     hash = "sha256-5aoXBQowvHK6qGr9xgSMrepNHsLsxh14dxegWbgxnog=";
   };
 
-  # ExtUtil::MakeMaker is bundled with Perl, but the bundled version
-  # causes build errors for aarch64-darwin, so we override it with the
-  # latest version.  We can drop the dependency to go back to the
-  # bundled version when the version that comes with Perl is ≥7.57_02.
-  #
-  # Check the version bundled with Perl like this:
-  #   perl -e 'use ExtUtils::MakeMaker qw($VERSION); print "$VERSION\n"'
-  nativeBuildInputs = [ makeWrapper perlPackages.ExtUtilsMakeMaker ];
+  nativeBuildInputs = [ makeWrapper ];
   buildInputs = (with perlPackages; [
     HTMLParser NetCIDRLite NetDNS NetAddrIP DBFile HTTPDate MailDKIM LWP
     LWPProtocolHttps IOSocketSSL DBI EncodeDetect IPCountry NetIdent
