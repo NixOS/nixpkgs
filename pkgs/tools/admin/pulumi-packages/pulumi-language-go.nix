f2d4baf4113c3bc8bcfa8f1fd3b3d024efc9c3c3
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-06-26 15:17:26 +0300

    pulumiPackages.pulumi-language-go: fix build

--- pkgs/tools/admin/pulumi-packages/pulumi-language-go.nix
+++ pkgs/tools/admin/pulumi-packages/pulumi-language-go.nix
@@ -6,19 +6,19 @@ buildGoModule rec {
   pname = "pulumi-language-go";
   inherit (pulumi) version src;
 
-  sourceRoot = "${src.name}/sdk";
+  sourceRoot = "${src.name}/sdk/go/pulumi-language-go";
 
-  vendorHash = pulumi.sdkVendorHash;
-
-  subPackages = [
-    "go/pulumi-language-go"
-  ];
+  vendorHash = "sha256-6/umLzw7HMplP/cJknBsWmiwAnc+YM4tIz4Zl2QMTOQ=";
 
   ldflags = [
     "-s"
     "-w"
     "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${version}"
   ];
+
+  # go: inconsistent vendoring in ...
+  doCheck = false;
+
   meta = with lib; {
     description = "Golang language host plugin for Pulumi";
     homepage = "https://github.com/pulumi/pulumi/tree/master/sdk/go";
