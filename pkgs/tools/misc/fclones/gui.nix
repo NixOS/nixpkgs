2407777b8f122344170ea5fe45ac670ea9cdc5bc
figsoda <figsoda@pm.me>
2023-06-26 09:35:25 -0400

    fclones-gui: 0.1.2 -> 0.1.3
    Diff: https://diff.rs/fclones-gui/0.1.2/0.1.3

--- pkgs/tools/misc/fclones/gui.nix
+++ pkgs/tools/misc/fclones/gui.nix
@@ -12,14 +12,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "fclones-gui";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-ge2l414kYHK3y4V837GTQ5sSxVRlU8ZYyGdBj4+vUL8=";
+    hash = "sha256-SQJ6CZlvu4V9Rs+rhH4jMf0AVWs71KvRUnUxGPlgj80=";
   };
 
-  cargoHash = "sha256-rDAUA75KCWlhf13bCucV5w9WAJ+Uw+s8sUCCeWBYJeA=";
+  cargoHash = "sha256-8WLYbEbPrR8c0y+Uaxo6YGiFRt7FLHZM+1O/UZq0c7g=";
 
   nativeBuildInputs = [
     pkg-config
