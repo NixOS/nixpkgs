8def8dedd69c11bffca90bea592394c78c9b092a
figsoda <figsoda@pm.me>
2023-06-26 09:19:27 -0400

    cargo-hakari: 0.9.25 -> 0.9.26
    Diff: https://github.com/guppy-rs/guppy/compare/cargo-hakari-0.9.25...cargo-hakari-0.9.26
    
    Changelog: https://github.com/guppy-rs/guppy/blob/cargo-hakari-0.9.26/tools/cargo-hakari/CHANGELOG.md

--- pkgs/development/tools/rust/cargo-hakari/default.nix
+++ pkgs/development/tools/rust/cargo-hakari/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-hakari";
-  version = "0.9.25";
+  version = "0.9.26";
 
   src = fetchFromGitHub {
     owner = "guppy-rs";
     repo = "guppy";
     rev = "cargo-hakari-${version}";
-    sha256 = "sha256-cKnu28L8tjndjtHuClLIpiwTzf6YoN37vw1sE7xrNzQ=";
+    sha256 = "sha256-CQ0bpc5pmPMQMQ+8mcrUwo19zqyfkk5pE/lWRr9azXs=";
   };
 
-  cargoHash = "sha256-PoTwS9oS8Nmkdp3ZvdZRdk028SIx6D7kfsiRksyTpKo=";
+  cargoHash = "sha256-F6+GRn3l849vRVKE1KhLMnxwR5NWu1b78xTxrHi817A=";
 
   cargoBuildFlags = [ "-p" "cargo-hakari" ];
   cargoTestFlags = [ "-p" "cargo-hakari" ];
