cde717d60c9b205b21bd9ab0e2724b116b2e19af
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-26 09:24:52 +0200

    python311Packages.pyezviz: 0.2.1.3 -> 0.2.1.6
    Diff: https://github.com/baqs/pyEzviz/compare/refs/tags/0.2.1.3...0.2.1.6
    
    Changelog: https://github.com/BaQs/pyEzviz/releases/tag/0.2.1.6

--- pkgs/development/python-modules/pyezviz/default.nix
+++ pkgs/development/python-modules/pyezviz/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "pyezviz";
-  version = "0.2.1.3";
+  version = "0.2.1.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "baqs";
     repo = "pyEzviz";
     rev = "refs/tags/${version}";
-    hash = "sha256-myf8+yGXx9Ry1NuqRLYLh3I2ubS1DLfhQF1mvp5xiwc=";
+    hash = "sha256-3NeXAqEoRJMzg/Io9WF93GFJHEjAaHNn4/lBVKc2Q3Q=";
   };
 
   propagatedBuildInputs = [
