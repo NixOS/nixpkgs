e44b6eb54fe34dd3601789d386c4f61750cf5ad4
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-26 03:02:28 +0000

    bazel-gazelle: 0.31.0 -> 0.31.1

--- pkgs/development/tools/bazel-gazelle/default.nix
+++ pkgs/development/tools/bazel-gazelle/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "bazel-gazelle";
-  version = "0.31.0";
+  version = "0.31.1";
 
   src = fetchFromGitHub {
     owner = "bazelbuild";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Y1otQcGtG7jIJE5G0DET8bFRVL6AEG0r98X5PufT7Lw=";
+    sha256 = "sha256-avc92qQS2d8MiQVRUYEiMOlsfgQFJ6HZG01rmZeC9Fo=";
   };
 
   vendorHash = null;
