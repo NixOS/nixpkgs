c44ab108b2fc705f516fef6b4b4de42b29715f11
Vincenzo Mantova <1962985+xworld21@users.noreply.github.com>
2023-06-26 22:09:19 +0100

    python3Packages.cson: init at 0.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2199,6 +2199,8 @@ self: super: with self; {
 
   crytic-compile = callPackage ../development/python-modules/crytic-compile { };
 
+  cson  = callPackage ../development/python-modules/cson { };
+
   csrmesh  = callPackage ../development/python-modules/csrmesh { };
 
   csscompressor = callPackage ../development/python-modules/csscompressor { };
