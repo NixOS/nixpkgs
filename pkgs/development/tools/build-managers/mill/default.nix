29ace74e8cd767bec80e00a20e47682c57a0c5ac
Fabián Heredia Montiel <fabian@deal-engine.com>
2023-06-26 15:57:30 -0600

    mill: 0.11.0 → 0.11.1

--- pkgs/development/tools/build-managers/mill/default.nix
+++ pkgs/development/tools/build-managers/mill/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mill";
-  version = "0.11.0";
+  version = "0.11.1";
 
   src = fetchurl {
     url = "https://github.com/com-lihaoyi/mill/releases/download/${version}/${version}-assembly";
-    hash = "sha256-LB7np8LTes97XWnOHxCTYXY5BRZxxJJ4EpMsG4zUHNQ=";
+    hash = "sha256-qG+Ddn0BHUZX1VX5hO84exgRz8YuUgYF/fH6MmgkrXE";
   };
 
   nativeBuildInputs = [ makeWrapper ];
