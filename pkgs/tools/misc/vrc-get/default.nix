d76872fefd6788de699e7f2dedab6ab09f0edf33
Luna Simons <luna@bddvlpr.com>
2023-06-26 02:58:10 +0200

    vrc-get: meta and versioning changes

--- pkgs/tools/misc/vrc-get/default.nix
+++ pkgs/tools/misc/vrc-get/default.nix
@@ -2,12 +2,12 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "vrc-get";
-  version = "v1.1.1";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "anatawa12";
     repo = pname;
-    rev = version;
+    rev = "v${version}";
     sha256 = "16r3b2w4xfw1lwxp8ib23f76i4zgl8hk7q2g82yw6hxjimq69sy2";
   };
 
@@ -18,10 +18,10 @@ rustPlatform.buildRustPackage rec {
   # Make openssl-sys use pkg-config.
   OPENSSL_NO_VENDOR = 1;
 
-  cargoSha256 = "0szwr8r1y7w82xfx24ffxwqa9gv8m35k30nycfn8vng54vba1rcy";
+  cargoSha256 = "1f76y7hzh4pk7gj0w5gqb65w8qvkfx1bxx9z3w9vdqdkz39439rf";
 
   meta = with lib; {
-    description = "Open Source command line client of VRChat Package Manager, the main feature of VRChat Creator Companion (VCC), that supports Windows, Linux, and macOS.";
+    description = "Command line client of VRChat Package Manager, the main feature of VRChat Creator Companion (VCC)";
     homepage = "https://github.com/anatawa12/vrc-get";
     license = licenses.mit;
     maintainers = with maintainers; [ bddvlpr ];
