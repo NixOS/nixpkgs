f06c6f75232bef6535f608900c8c05c10a52ba18
figsoda <figsoda@pm.me>
2023-06-26 10:11:54 -0400

    typos: 1.15.5 -> 1.15.6
    Diff: https://github.com/crate-ci/typos/compare/v1.15.5...v1.15.6
    
    Changelog: https://github.com/crate-ci/typos/blob/v1.15.6/CHANGELOG.md

--- pkgs/development/tools/typos/default.nix
+++ pkgs/development/tools/typos/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "typos";
-  version = "1.15.5";
+  version = "1.15.6";
 
   src = fetchFromGitHub {
     owner = "crate-ci";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-5QrUB7p78HmwTUagYKNbFxndUKdeGd51gpqIO1O/8H4=";
+    hash = "sha256-J78EJi2PXa3a83gP05b+ufDyq+BT9e1h7E/QiYs5mls=";
   };
 
-  cargoHash = "sha256-ZNWG3eKnT2LFywIQO1iLIhaUKrINdwbCsSbC55wFrF4=";
+  cargoHash = "sha256-9WUzWWwZHC9OPw18Xi3krkIfNH/gdB6VPhrL80RLCvk=";
 
   meta = with lib; {
     description = "Source code spell checker";
