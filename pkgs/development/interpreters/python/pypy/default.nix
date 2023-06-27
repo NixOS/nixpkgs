d16cbdfda9d18cdd0a60bf9e85f4cd2be5c20af3
Sophie Tauchert <sophie@999eagle.moe>
2023-06-27 22:06:23 +0200

    pypy3: fix sitecustomize
    This is a stop-gap solution for pypy3.withPackages to actually find
    the correct site-packages directory

--- pkgs/development/interpreters/python/pypy/default.nix
+++ pkgs/development/interpreters/python/pypy/default.nix
@@ -123,7 +123,8 @@ in with passthru; stdenv.mkDerivation rec {
     ln -s $out/${executable}-c/lib-python/${if isPy3k then "3" else pythonVersion} $out/lib/${libPrefix}
 
     # Include a sitecustomize.py file
-    cp ${../sitecustomize.py} $out/${if isPy38OrNewer then sitePackages else "lib/${libPrefix}/${sitePackages}"}/sitecustomize.py
+    mkdir -p $out/${libPrefix}-c/${if isPy38OrNewer then sitePackages else "lib/${libPrefix}/${sitePackages}"}
+    cp ${../sitecustomize.py} $out/${libPrefix}-c/${if isPy38OrNewer then sitePackages else "lib/${libPrefix}/${sitePackages}"}/sitecustomize.py
 
     runHook postInstall
   '';
