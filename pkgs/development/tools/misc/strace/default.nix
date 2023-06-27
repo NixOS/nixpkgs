41e2e65549e13a09497a276e90f7e7f59bb45257
Maximilian Bosch <maximilian@mbosch.me>
2023-06-27 23:41:43 +0200

    strace: 6.3 -> 6.4
    ChangeLog: https://github.com/strace/strace/releases/tag/v6.4
    (cherry picked from commit 59e5cf29530b38886014b07431aaf9248bf0497d)

--- pkgs/development/tools/misc/strace/default.nix
+++ pkgs/development/tools/misc/strace/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "strace";
-  version = "6.3";
+  version = "6.4";
 
   src = fetchurl {
     url = "https://strace.io/files/${version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-4Xh44wFQbBzDAWERGK0U7+5/i872Oyes5dKQrM57tzE=";
+    sha256 = "sha256-J5h9usV/39JgxttNyDKN81yVxoZ8ij1DcdWc3PTrkjg=";
   };
 
   depsBuildBuild = [ buildPackages.stdenv.cc ];
