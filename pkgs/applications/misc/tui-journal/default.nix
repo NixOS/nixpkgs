3254fd4715b4c5f8dc2c746f091bb03edcaf6486
figsoda <figsoda@pm.me>
2023-06-27 19:36:52 -0400

    tui-journal: init at 0.2.0
    https://github.com/AmmarAbouZor/tui-journal

---
+++ pkgs/applications/misc/tui-journal/default.nix
@@ -0,0 +1,44 @@
+{ lib
+, rustPlatform
+, fetchFromGitHub
+, pkg-config
+, libgit2
+, openssl
+, zlib
+, stdenv
+, darwin
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "tui-journal";
+  version = "0.2.0";
+
+  src = fetchFromGitHub {
+    owner = "AmmarAbouZor";
+    repo = "tui-journal";
+    rev = "v${version}";
+    hash = "sha256-B3GxxkFT2Z7WtV9RSmtKBjvzRRqmcoukUKc6LUZ/JyM=";
+  };
+
+  cargoHash = "sha256-DCKW8eGLSTx9U7mkGruPphzFpDlpL8ULCOKhj6HJwhw=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    libgit2
+    openssl
+    zlib
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Security
+  ];
+
+  meta = with lib; {
+    description = "Your journal app if you live in a terminl";
+    homepage = "https://github.com/AmmarAbouZor/tui-journal";
+    changelog = "https://github.com/AmmarAbouZor/tui-journal/blob/${src.rev}/CHANGELOG.ron";
+    license = licenses.mit;
+    maintainers = with maintainers; [ figsoda ];
+  };
+}
