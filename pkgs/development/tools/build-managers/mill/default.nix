e87cb08ecff62fe0a8026e6b66b8050d1d50a008
Fabián Heredia Montiel <fabian@deal-engine.com>
2023-06-27 09:08:27 -0600

    mill: add zenithal as maintainer

--- pkgs/development/tools/build-managers/mill/default.nix
+++ pkgs/development/tools/build-managers/mill/default.nix
@@ -46,7 +46,7 @@ stdenv.mkDerivation rec {
       SBT, but can also be extended to support any other language or platform via
       modules (written in Java or Scala) or through an external subprocesses.
     '';
-    maintainers = with maintainers; [ scalavision ];
+    maintainers = with maintainers; [ scalavision zenithal ];
     platforms = lib.platforms.all;
   };
 }
