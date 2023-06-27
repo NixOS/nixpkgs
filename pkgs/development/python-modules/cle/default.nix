fb9985b4f91e16cb37cdd92c9ded0b22cc491557
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-27 22:43:59 +0200

    python311Packages.cle: 9.2.56 -> 9.2.57
    Diff: https://github.com/angr/cle/compare/refs/tags/v9.2.56...v9.2.57

--- pkgs/development/python-modules/cle/default.nix
+++ pkgs/development/python-modules/cle/default.nix
@@ -16,7 +16,7 @@
 
 let
   # The binaries are following the argr projects release cycle
-  version = "9.2.56";
+  version = "9.2.57";
 
   # Binary files from https://github.com/angr/binaries (only used for testing and only here)
   binaries = fetchFromGitHub {
@@ -38,7 +38,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-c4uuM7ZZPfykGkYrpuE2JJqe5/pTZuR+IvPC+HA7laA=";
+    hash = "sha256-xROJl7Mpdpcz1Mio7ChHR/9hHTgp+SQkawGkl6F+NDM=";
   };
 
   nativeBuildInputs = [
