b297d887669e89d7d6e3f3dc0417db6e418e0a35
Martin Weinelt <mweinelt@users.noreply.github.com>
2023-06-27 18:03:31 +0200

    Update pkgs/development/python-modules/camel-converter/default.nix
    Co-authored-by: Nick Cao <nickcao@nichi.co>

--- pkgs/development/python-modules/camel-converter/default.nix
+++ pkgs/development/python-modules/camel-converter/default.nix
@@ -12,7 +12,7 @@ buildPythonPackage rec {
   version = "3.0.1";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "sanders41";
