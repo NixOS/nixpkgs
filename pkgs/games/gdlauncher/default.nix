3a181b6f9d04234b8e9a21d8447baddd62bd9ef7
PassiveLemon <lemonl3mn@protonmail.com>
2023-06-27 20:03:06 -0400

    Update default.nix
    Remove some redundancies:
    ..., pkgs, rec

--- pkgs/games/gdlauncher/default.nix
+++ pkgs/games/gdlauncher/default.nix
@@ -1,17 +1,14 @@
-{ pkgs, lib, ... }:
-let
+{ appimageTools, fetchurl, lib }:
+appimageTools.wrapType2 rec {
   pname = "gdlauncher";
   version = "1.1.30";
   name = "${pname}-${version}";
-  src = pkgs.fetchurl {
+  src = fetchurl {
     url = "https://github.com/gorilla-devs/GDLauncher/releases/download/v${version}/GDLauncher-linux-setup.AppImage";
     hash = "sha256-4cXT3exhoMAK6gW3Cpx1L7cm9Xm0FK912gGcRyLYPwM=";
   };
 
-  appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
-in
-pkgs.appimageTools.wrapType2 rec {
-  inherit name src;
+  appimageContents = appimageTools.extractType2 { inherit name src; };
 
   extraInstallCommands = ''
     mv $out/bin/${name} $out/bin/${pname}
@@ -24,7 +21,7 @@ pkgs.appimageTools.wrapType2 rec {
     homepage = "https://gdlauncher.com/";
     changelog = "https://github.com/gorilla-devs/GDLauncher/releases/tag/v${version}";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ "PassiveLemon" ];
+    maintainers = with maintainers; [ PassiveLemon ];
     platforms = [ "x86_64-linux" ];
   };
 }
