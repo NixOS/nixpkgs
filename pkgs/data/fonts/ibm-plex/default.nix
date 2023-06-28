d8556152663642c04ecf03b38a52bb73efaedadb
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-06-28 20:06:00 +1000

    ibm-plex: fix hash

--- pkgs/data/fonts/ibm-plex/default.nix
+++ pkgs/data/fonts/ibm-plex/default.nix
@@ -6,7 +6,7 @@ stdenvNoCC.mkDerivation rec {
 
   src = fetchzip {
     url = "https://github.com/IBM/plex/releases/download/v${version}/OpenType.zip";
-    hash = "sha256-VwJyZMH10FXDxc7T7m8sl7raUlrarSM0Snjruo1ICHU=";
+    hash = "sha256-RvD/aeZrvltJiuAHqYMNaRsjLgTdcC1/5zqlcd4qKAA=";
   };
 
   installPhase = ''
