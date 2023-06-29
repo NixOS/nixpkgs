NixOS/nixpkgs

commit a1a4c4d6296ec93e72dac23df6965681e384af74

haskellPackages.streamly-lmdb: unbreak

pkgs/development/haskell-modules/configuration-common.nix
@@ -409,6 +409,14 @@ self: super: {
   # 2022-02-14: Strict upper bound: https://github.com/psibi/streamly-bytestring/issues/30
   streamly-bytestring = dontCheck (doJailbreak super.streamly-bytestring);
 
+  # The package requires streamly == 0.9.*.
+  # (We can remove this once the assert starts failing.)
+  streamly-lmdb = super.streamly-lmdb.override {
+    streamly =
+      assert (builtins.compareVersions pkgs.haskellPackages.streamly.version "0.9.0" < 0);
+        pkgs.haskellPackages.streamly_0_9_0;
+  };
+
   # base bound
   digit = doJailbreak super.digit;
 

pkgs/development/haskell-modules/configuration-hackage2nix/broken.yaml
@@ -5109,7 +5109,6 @@ broken-packages:
   - streamly-binary
   - streamly-cassava
   - streamly-examples
-  - streamly-lmdb
   - streamly-lz4
   - streamly-process
   - stream-monad

pkgs/development/haskell-modules/configuration-nix.nix
@@ -914,6 +914,9 @@ self: super: builtins.intersectAttrs super {
     archive = pkgs.libarchive;
   };
 
+  # Pass the correct lmdb into the package.
+  streamly-lmdb = super.streamly-lmdb.override { lmdb = pkgs.lmdb; };
+
   hlint = overrideCabal (drv: {
     postInstall = ''
       install -Dm644 data/hlint.1 -t "$out/share/man/man1"

commit 1c9d518cb68960748e7784d655f75d3b89337597

haskellPackages.streamly-lmdb: obtain deps from haskell fixpoint
This will make the package work in non-default haskell package sets. The
issue was introduced in a1a4c4d6296ec93e72dac23df6965681e384af74.

Resolves #240512.

pkgs/development/haskell-modules/configuration-common.nix
@@ -366,7 +366,7 @@ self: super: {
   streamly-lmdb = super.streamly-lmdb.override {
     streamly =
       assert (builtins.compareVersions pkgs.haskellPackages.streamly.version "0.9.0" < 0);
-        pkgs.haskellPackages.streamly_0_9_0;
+        self.streamly_0_9_0;
   };
 
   # base bound