111a51ffdfa5e064159c8d7cb2987155524e3765
Yaya <mak@nyantec.com>
2023-06-29 09:39:44 +0000

    gitlab: 16.1.0 -> 16.1.1
    https://gitlab.com/gitlab-org/gitlab/-/blob/v16.1.1-ee/CHANGELOG.md
    (cherry picked from commit 9f0b22c9b25cf37ffb42fa27a32f0456d6d45e2f)

--- pkgs/applications/version-management/gitlab/gitaly/default.nix
+++ pkgs/applications/version-management/gitlab/gitaly/default.nix
@@ -13,7 +13,7 @@
 }:
 
 let
-  version = "16.1.0";
+  version = "16.1.1";
   package_version = "v${lib.versions.major version}";
   gitaly_package = "gitlab.com/gitlab-org/gitaly/${package_version}";
 
@@ -24,7 +24,7 @@ let
       owner = "gitlab-org";
       repo = "gitaly";
       rev = "v${version}";
-      sha256 = "sha256-+Fnj9fgQQtyGMWOL5NkNON/N9p6POjAtpF2O06iKh90=";
+      sha256 = "sha256-bbAu9OIo1FT2KX186ajV5OUcvFpKUGaYPxY2S2RvXo8=";
     };
 
     vendorSha256 = "sha256-6oOFQGPwiMRQrESXsQsGzvWz9bCb0VTYIyyG/C2b3nA=";
