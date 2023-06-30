3d16ae44d4cdd87eb7ea16d54c5396c523a537d2
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-30 20:55:51 +0200

    python311Packages.decli: 0.5.2 -> 0.6.1
    Changelog: https://github.com/woile/decli/blob/v0.6.1/CHANGELOG.md

--- pkgs/development/python-modules/decli/default.nix
+++ pkgs/development/python-modules/decli/default.nix
@@ -1,22 +1,34 @@
-{ buildPythonPackage
-, lib
+{ lib
+, buildPythonPackage
 , fetchPypi
+, setuptools
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "decli";
-  version = "0.5.2";
+  version = "0.6.1";
+  format = "pyproject";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-8s3lUDSnXIGcYwx2VahExhLyWYxCwhKZFgRl32rUY60=";
+    hash = "sha256-7YjMuUdwHo5VCbeUX9pW4VDirHSmnyXUeshe8wqwwPA=";
   };
 
-  pythonImportsCheck = [ "decli" ];
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  pythonImportsCheck = [
+    "decli"
+  ];
 
   meta = with lib; {
     description = "Minimal, easy to use, declarative command line interface tool";
     homepage = "https://github.com/Woile/decli";
+    changelog = "https://github.com/woile/decli/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ lovesegfault ];
   };
