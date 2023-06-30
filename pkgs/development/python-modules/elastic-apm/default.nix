bd15d5ab5bea770cf43930fa3238ce4503d6cb0e
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-01 00:07:27 +0200

    python311Packages.elastic-apm: 6.16.1 -> 6.16.2
    Diff: https://github.com/elastic/apm-agent-python/compare/refs/tags/v6.16.1...v6.16.2
    
    Changelog: https://github.com/elastic/apm-agent-python/releases/tag/v6.16.2

--- pkgs/development/python-modules/elastic-apm/default.nix
+++ pkgs/development/python-modules/elastic-apm/default.nix
@@ -30,7 +30,7 @@
 
 buildPythonPackage rec {
   pname = "elastic-apm";
-  version = "6.16.1";
+  version = "6.16.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -39,7 +39,7 @@ buildPythonPackage rec {
     owner = "elastic";
     repo = "apm-agent-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-m/PjL8pltUpv/ewE/gksISsOhEVsyeWoq6yfd5c1j6s=";
+    hash = "sha256-JhpPreZg7DV8wTPKc7CUP+yh1LJOyc3Oa/IV3x/JMuQ=";
   };
 
   propagatedBuildInputs = [
