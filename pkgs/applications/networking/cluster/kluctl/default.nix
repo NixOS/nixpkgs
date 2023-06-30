82e01c97dc34d5b46be9880298a717dc2d935dc2
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-30 22:27:19 +0000

    kluctl: 2.20.6 -> 2.20.7

--- pkgs/applications/networking/cluster/kluctl/default.nix
+++ pkgs/applications/networking/cluster/kluctl/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "kluctl";
-  version = "2.20.6";
+  version = "2.20.7";
 
   src = fetchFromGitHub {
     owner = "kluctl";
     repo = "kluctl";
     rev = "v${version}";
-    hash = "sha256-B8+HKqIuJaH+6ViBxWfiAAmXieQKcwAW565SwUpIJKI=";
+    hash = "sha256-NcvPo+6f2EYhitzOl2VPz8MtFIsYBuOA7EJnD4TJdmI=";
   };
 
   vendorHash = "sha256-x5Zy8H7DzxU+uBCUL6edv8x2LwiIjXl5UrRUMDtUEk8=";
