e94b17e9be031a6941dd0d0c36b00531d84d52d3
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-30 10:34:40 +0000

    rssguard: 4.3.4 -> 4.4.0

--- pkgs/applications/networking/feedreaders/rssguard/default.nix
+++ pkgs/applications/networking/feedreaders/rssguard/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "rssguard";
-  version = "4.3.4";
+  version = "4.4.0";
 
   src = fetchFromGitHub {
     owner = "martinrotter";
     repo = pname;
     rev = version;
-    sha256 = "sha256-1xoMymsx5Z8oZzZtPzLWaOs0zy/E0pIsLpHgZlVdgSw=";
+    sha256 = "sha256-oUXg3BjGTJsDfA0H2RlCEyNtpld/+jzUl2n9TYiudcY=";
   };
 
   buildInputs =  [ qtwebengine qttools ];
