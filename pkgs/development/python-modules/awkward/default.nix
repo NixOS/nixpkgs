7b1af4daed58cb6544de7710cb31802a4cee0a6b
R. Ryantm <ryantm-bot@ryantm.com>
2023-06-30 21:22:07 +0000

    python310Packages.awkward: 2.2.3 -> 2.2.4

--- pkgs/development/python-modules/awkward/default.nix
+++ pkgs/development/python-modules/awkward/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "awkward";
-  version = "2.2.3";
+  version = "2.2.4";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-yx/z8lTqVWnMTp7TlH+rtAHb3cskm1iViZedhfs0EUI=";
+    hash = "sha256-v06mYdoP/WfIfz6x6+MJvS4YOsTsyWqhCyAykZ1d5v4=";
   };
 
   nativeBuildInputs = [
