38018514e88f9e724bc709554e56bfe84acb46ac
Randy Eckenrode <randy@largeandhighquality.com>
2023-07-01 13:56:34 -0400

    db: fix build with clang 16 on Darwin
    Both `_spin_lock_try` and `_spin_unlock` are private and deprecated
    APIs, which are not exported by any headers in the SDK. The build fails
    because the configure script does not define the functions before
    calling them, which is treated as error by clang 16.
    
    This patch replaces use of those APIs with `os_unfair_lock`, which is
    the recommended replacement per the deprecation messages.

--- pkgs/development/libraries/db/db-4.8.nix
+++ pkgs/development/libraries/db/db-4.8.nix
@@ -3,7 +3,8 @@
 import ./generic.nix (args // {
   version = "4.8.30";
   sha256 = "0ampbl2f0hb1nix195kz1syrqqxpmvnvnfvphambj7xjrl3iljg0";
-  extraPatches = [ ./clang-4.8.patch ./CVE-2017-10140-4.8-cwd-db_config.patch ];
+  extraPatches = [ ./clang-4.8.patch ./CVE-2017-10140-4.8-cwd-db_config.patch ]
+    ++ lib.optionals stdenv.isDarwin [ ./darwin-mutexes-4.8.patch ];
 
   drvArgs.hardeningDisable = [ "format" ];
   drvArgs.doCheck = false;
