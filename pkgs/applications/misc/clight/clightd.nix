75087f0e794e8224df009f933800e82b135b5b73
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-07-01 22:46:58 +0200

    Update pkgs/applications/misc/clight/clightd.nix

--- pkgs/applications/misc/clight/clightd.nix
+++ pkgs/applications/misc/clight/clightd.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     owner = "FedeDP";
     repo = "Clightd";
     rev = version;
-    sha256 = "sha256-Lmno/TJVCQVNzfpKNZzuDf2OM6w6rbz+zJTr3zVo/CM=";
+    hash = "sha256-Lmno/TJVCQVNzfpKNZzuDf2OM6w6rbz+zJTr3zVo/CM=";
   };
 
   # dbus-1.pc has datadir=/etc
