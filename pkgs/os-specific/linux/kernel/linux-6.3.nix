25f6f293c803f665ef72a2bc74f269983e844fe4
Raito Bezarius <masterancpp@gmail.com>
2023-07-02 01:06:17 +0200

    linux_6_3: 6.3.10 -> 6.3.11
    https://lwn.net/Articles/937081/

--- pkgs/os-specific/linux/kernel/linux-6.3.nix
+++ pkgs/os-specific/linux/kernel/linux-6.3.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.3.10";
+  version = "6.3.11";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "1qs6rmh0hk47rmz30fhjj3g7bqrz19w1ldyv6fyiq6djja3avag0";
+    sha256 = "sha256-HVo/vU1CZbbJYF1cYF2UdnPnZDryiQ5K1clGlA8SPhY=";
   };
 } // (args.argsOverride or { }))
