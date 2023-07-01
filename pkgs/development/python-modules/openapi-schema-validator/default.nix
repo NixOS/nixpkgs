3dcb1e5d1007334f94114eaad249349541666c29
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-01 23:33:41 +0200

    python311Packages.openapi-schema-validator: 0.4.4 -> 0.5.0
    Diff: https://github.com/p1c2u/openapi-schema-validator/compare/refs/tags/0.4.4...0.5.0
    
    Changelog: https://github.com/python-openapi/openapi-schema-validator/releases/tag/0.5.0

--- pkgs/development/python-modules/openapi-schema-validator/default.nix
+++ pkgs/development/python-modules/openapi-schema-validator/default.nix
@@ -1,23 +1,26 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, poetry-core
-, pytestCheckHook
 , isodate
 , jsonschema
+, poetry-core
+, pytestCheckHook
+, pythonOlder
 , rfc3339-validator
 }:
 
 buildPythonPackage rec {
   pname = "openapi-schema-validator";
-  version = "0.4.4";
+  version = "0.5.0";
   format = "pyproject";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "p1c2u";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-2XTCdp9dfzhNKCpq71pt7yEZm9abiEmFHD/114W+jOQ=";
+    hash = "sha256-uFvM1EXN8wgk972YpQ52T4CkTWeF35ykF61EmFgblpk=";
   };
 
   postPatch = ''
@@ -37,11 +40,14 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "openapi_schema_validator" ];
+  pythonImportsCheck = [
+    "openapi_schema_validator"
+  ];
 
   meta = with lib; {
     description = "Validates OpenAPI schema against the OpenAPI Schema Specification v3.0";
     homepage = "https://github.com/p1c2u/openapi-schema-validator";
+    changelog = "https://github.com/python-openapi/openapi-schema-validator/releases/tag/${version}";
     license = licenses.bsd3;
     maintainers = with maintainers; [ AluisioASG ];
   };
