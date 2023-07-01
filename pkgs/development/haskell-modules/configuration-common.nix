2eacf4f93fc9c6cf823312aaebf82f0ba496f0d3
sternenseemann <sternenseemann@systemli.org>
2023-07-01 17:53:53 +0200

    haskellPackages.singletons-*: drop obsolete overrides

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2385,10 +2385,6 @@ self: super: {
   doctest-parallel = dontCheck super.doctest-parallel;
   clash-prelude = dontCheck super.clash-prelude;
 
-  # Too strict upper bound on th-desugar, fixed in 3.1.1
-  singletons-th = assert super.singletons-th.version == "3.1"; doJailbreak super.singletons-th;
-  singletons-base = doJailbreak super.singletons-base;
-
   # Ships a broken Setup.hs
   # https://github.com/lehins/conduit-aeson/issues/1
   conduit-aeson = overrideCabal (drv: {
