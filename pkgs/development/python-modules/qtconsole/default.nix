20dc5d3179058e6f7be4b4cf94502ab28595a92f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-07-01 03:25:05 +0200

    python310Packages.qtconsole: add missing dependency on ipython_genutils

--- pkgs/development/python-modules/qtconsole/default.nix
+++ pkgs/development/python-modules/qtconsole/default.nix
@@ -4,6 +4,7 @@
 , ipykernel
 , jupyter-core
 , jupyter-client
+, ipython_genutils
 , pygments
 , pyqt5
 , pytestCheckHook
@@ -27,6 +28,7 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [
     ipykernel
+    ipython_genutils
     jupyter-core
     jupyter-client
     pygments
