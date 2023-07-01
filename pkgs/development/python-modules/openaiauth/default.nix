c370fa7514e1dca0ceed6740ab76cc9dc196e248
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-01 20:27:04 +0200

    python310Packages.openaiauth: add format
    - disable on unsupported Python releases
    - Update ordering

--- pkgs/development/python-modules/openaiauth/default.nix
+++ pkgs/development/python-modules/openaiauth/default.nix
@@ -1,12 +1,16 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 , requests
 }:
 
 buildPythonPackage rec {
   pname = "openaiauth";
   version = "2.0.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit version;
@@ -14,8 +18,11 @@ buildPythonPackage rec {
     hash = "sha256-wmVR+cN/uJ75l62uzmHqpvEcnjzi6CU0kQ2e/5LxkBw=";
   };
 
-  propagatedBuildInputs = [ requests ];
+  propagatedBuildInputs = [
+    requests
+  ];
 
+  # Module has no tests
   doCheck = false;
 
   pythonImportsCheck = [
@@ -23,10 +30,10 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
-    description = "A Python library for authenticating with the OpenAI API";
-    license = licenses.mit;
-    maintainers = with maintainers; [ realsnick ];
+    description = "Library for authenticating with the OpenAI API";
     homepage = "https://github.com/acheong08/OpenAIAuth";
     changelog = "https://github.com/acheong08/OpenAIAuth/releases/tag/${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ realsnick ];
   };
 }
