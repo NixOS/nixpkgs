32f8dd4beea32725ac68d6e56f65d6d23b97c1e2
sternenseemann <sternenseemann@systemli.org>
2023-07-01 19:00:34 +0200

    haskell.packages.ghc96: drop now unnessary version pins
    We need to upgrade http-api-data, since 0.5 (LTS 21) still doesn't
    support base >= 4.18.

--- pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
@@ -63,35 +63,18 @@ self: super: {
   #
 
   doctest = doDistribute super.doctest_0_21_1;
-  inspection-testing = doDistribute self.inspection-testing_0_5_0_1; # allows base >= 4.18
-  OneTuple = doDistribute (dontCheck super.OneTuple_0_4_1_1); # allows base >= 4.18
-  primitive = doDistribute (dontCheck self.primitive_0_7_4_0); # allows base >= 4.18
-  http-api-data = doDistribute self.http-api-data_0_5_1; # allows base >= 4.18
-  attoparsec-iso8601 = doDistribute self.attoparsec-iso8601_1_1_0_0; # for http-api-data-0.5.1
-  tagged = doDistribute self.tagged_0_8_7; # allows template-haskell-2.20
+  http-api-data = doDistribute self.http-api-data_0_6; # allows base >= 4.18
   some = doDistribute self.some_1_0_5;
-  tasty-inspection-testing = doDistribute self.tasty-inspection-testing_0_2;
   th-abstraction = doDistribute self.th-abstraction_0_5_0_0;
   th-desugar = doDistribute self.th-desugar_1_15;
-  turtle = doDistribute self.turtle_1_6_1;
-  memory = doDistribute self.memory_0_18_0;
   semigroupoids = doDistribute self.semigroupoids_6_0_0_1;
   bifunctors = doDistribute self.bifunctors_5_6_1;
-  cabal-plan = doDistribute self.cabal-plan_0_7_3_0;
   base-compat = doDistribute self.base-compat_0_13_0;
   base-compat-batteries = doDistribute self.base-compat-batteries_0_13_0;
-  semialign = doDistribute self.semialign_1_3;
-  assoc = doDistribute self.assoc_1_1;
-  strict = doDistribute self.strict_0_5;
 
   ghc-lib = doDistribute self.ghc-lib_9_6_2_20230523;
   ghc-lib-parser = doDistribute self.ghc-lib-parser_9_6_2_20230523;
   ghc-lib-parser-ex = doDistribute self.ghc-lib-parser-ex_9_6_0_0;
-
-  # allows mtl, template-haskell, text and transformers
-  hedgehog = doDistribute self.hedgehog_1_2;
-  # allows base >= 4.18
-  tasty-hedgehog = doDistribute self.tasty-hedgehog_1_4_0_1;
 
   # v0.1.6 forbids base >= 4.18
   singleton-bool = doDistribute super.singleton-bool_0_1_7;
@@ -157,8 +140,6 @@ self: super: {
       hls-stylish-haskell-plugin = null;
     };
 
-  MonadRandom = super.MonadRandom_0_6;
-  unix-compat = super.unix-compat_0_7;
   lifted-base = dontCheck super.lifted-base;
   hw-fingertree = dontCheck super.hw-fingertree;
   hw-prim = dontCheck (doJailbreak super.hw-prim);
@@ -168,9 +149,6 @@ self: super: {
   rerebase = doJailbreak super.rerebase_1_20;
   hiedb = dontCheck super.hiedb;
   retrie = dontCheck (super.retrie);
-
-  # break infinite recursion with foldable1-classes-compat's test suite, which depends on 'these'.
-  these = doDistribute (super.these_1_2.override { foldable1-classes-compat = dontCheck super.foldable1-classes-compat; });
 
   ghc-exactprint = unmarkBroken (addBuildDepends (with self.ghc-exactprint.scope; [
    HUnit Diff data-default extra fail free ghc-paths ordered-containers silently syb
