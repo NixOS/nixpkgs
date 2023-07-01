fed733188dccff7ff2748f385982cf4f41f98158
natsukium <tomoya.otabi@gmail.com>
2023-07-01 14:31:30 +0900

    python310Packages.rabbitpy: remove
    rabbitpy is unmaintained and broken

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10498,8 +10498,6 @@ self: super: with self; {
 
   r2pipe = callPackage ../development/python-modules/r2pipe { };
 
-  rabbitpy = callPackage ../development/python-modules/rabbitpy { };
-
   rachiopy = callPackage ../development/python-modules/rachiopy { };
 
   radicale_infcloud = callPackage ../development/python-modules/radicale_infcloud {
