c97ddafbb27936f60dd9648ffe411b36c08144b0
Aaron Bieber <aaron@bolddaemon.com>
2023-07-01 17:54:18 -0600

    clilol: init at 1.0.4
    clilol is command line tool for interacting with omg.lol services.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36249,6 +36249,8 @@ with pkgs;
 
   clightning = callPackage ../applications/blockchains/clightning { };
 
+  clilol = callPackage ../applications/networking/clilol { };
+
   besu = callPackage ../applications/blockchains/besu { };
 
   bitcoin-abc  = libsForQt5.callPackage ../applications/blockchains/bitcoin-abc {
