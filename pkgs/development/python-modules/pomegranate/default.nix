1e3e230b42202cd7502a4cb53f49f2c2574d2b59
Nick Cao <nickcao@nichi.co>
2023-07-02 13:23:21 +0800

    python3Packages.pomegranate: 0.14.8 -> 1.0.1
    Diff: https://github.com/jmschrei/pomegranate/compare/0652e955c400bc56df5661db3298a06854c7cce8...d9d3748c6a7e433c4661231a9dcd2c9ef19071fc

--- pkgs/development/python-modules/pomegranate/default.nix
+++ pkgs/development/python-modules/pomegranate/default.nix
@@ -2,33 +2,45 @@
 , lib
 , buildPythonPackage
 , fetchFromGitHub
-, fetchpatch
 , numpy
 , scipy
-, cython
+, scikit-learn
+, torch
+, apricot-select
 , networkx
-, joblib
-, pandas
+, pytestCheckHook
 , nose
-, pyyaml
 }:
 
 
 buildPythonPackage rec {
   pname = "pomegranate";
-  version = "0.14.8";
+  version = "1.0.1";
+  format = "setuptools";
 
   src = fetchFromGitHub {
-    repo = pname;
     owner = "jmschrei";
-    # no tags for recent versions: https://github.com/jmschrei/pomegranate/issues/974
-    rev = "0652e955c400bc56df5661db3298a06854c7cce8";
-    sha256 = "16g49nl2bgnh6nh7bd21s393zbksdvgp9l13ww2diwhplj6hlly3";
+    repo = pname;
+    rev = "d9d3748c6a7e433c4661231a9dcd2c9ef19071fc"; # release is not tagged
+    hash = "sha256-jShhC77HwZ9uzBDUGMacs1ms+Yww+AZeTi1vj7SRO1c=";
   };
 
-  propagatedBuildInputs = [ numpy scipy cython networkx joblib pyyaml ];
+  propagatedBuildInputs = [
+    numpy
+    scipy
+    scikit-learn
+    torch
+    apricot-select
+    networkx
+  ];
 
-  nativeCheckInputs = [ pandas nose ];  # as of 0.13.5, it depends explicitly on nose, rather than pytest.
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  checkInputs = [
+    nose
+  ];
 
   meta = with lib; {
     broken = stdenv.isDarwin;
