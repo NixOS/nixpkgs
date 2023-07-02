1d0d25aabadd7c4817896f9b49c620c58af93585
natsukium <tomoya.otabi@gmail.com>
2023-07-02 13:37:26 +0900

    python310Packages.langchain: 0.0.218 -> 0.0.220
    Diff: https://github.com/hwchase17/langchain/compare/refs/tags/v0.0.218...v0.0.220
    
    Changelog: https://github.com/hwchase17/langchain/releases/tag/v0.0.220

--- pkgs/development/python-modules/langchain/default.nix
+++ pkgs/development/python-modules/langchain/default.nix
@@ -79,7 +79,7 @@
 
 buildPythonPackage rec {
   pname = "langchain";
-  version = "0.0.218";
+  version = "0.0.220";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -88,7 +88,7 @@ buildPythonPackage rec {
     owner = "hwchase17";
     repo = "langchain";
     rev = "refs/tags/v${version}";
-    hash = "sha256-8ru3Rp1bRLVivfXAFNfZYfV3DgHuHNEuCrKoqL1O0zo=";
+    hash = "sha256-Br9j2mmLFxW3MHIY93m85mAVrvWVc6bnSlI71OwT8mY=";
   };
 
   postPatch = ''
