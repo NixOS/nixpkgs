bf9c4c5292b250b223be64d60ae66a84f0c78481
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-02 13:47:29 +0000

    komga: 0.165.0 -> 1.1.0

--- pkgs/servers/komga/default.nix
+++ pkgs/servers/komga/default.nix
@@ -8,11 +8,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "komga";
-  version = "0.165.0";
+  version = "1.1.0";
 
   src = fetchurl {
     url = "https://github.com/gotson/${pname}/releases/download/v${version}/${pname}-${version}.jar";
-    sha256 = "sha256-J8dpw7GzLJnLiiFSFVCoqZFQ6mI2z0zBZHdbmxMgmf8=";
+    sha256 = "sha256-uGzJgy+jfV11bZXvCMZAUdjuZasKCcv5rQBBUEidWQU=";
   };
 
   nativeBuildInputs = [
