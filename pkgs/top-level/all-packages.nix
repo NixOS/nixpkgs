8a6a09f620087fb3db9f4a8cd2de7de45de1c8f4
SharzyL <me@sharzy.in>
2023-07-02 20:51:36 +0800

    jextract: init at unstable-2023-04-14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25215,6 +25215,8 @@ with pkgs;
 
   fastjar = callPackage ../development/tools/java/fastjar { };
 
+  jextract = callPackage ../development/tools/java/jextract { };
+
   httpunit = callPackage ../development/libraries/java/httpunit { };
 
   javaCup = callPackage ../development/libraries/java/cup {
