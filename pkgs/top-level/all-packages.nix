7357b109beb2a925734f937e1a4ff8385d462cef
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-02 12:28:07 +0200

    halloy: init at 23.1-alpha1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8405,6 +8405,8 @@ with pkgs;
 
   halftone = callPackage ../applications/graphics/halftone { };
 
+  halloy = callPackage ../applications/networking/irc/halloy { };
+
   harminv = callPackage ../development/libraries/science/chemistry/harminv { };
 
   igrep = callPackage ../tools/text/igrep {
