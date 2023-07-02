b6d2db811bb6a3d891fd4621c9372b02539fbdc9
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-02 05:28:49 +0000

    dolt: 1.5.0 -> 1.7.0

--- pkgs/servers/sql/dolt/default.nix
+++ pkgs/servers/sql/dolt/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "dolt";
-  version = "1.5.0";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "dolthub";
     repo = "dolt";
     rev = "v${version}";
-    sha256 = "sha256-CSuHaQEnaBLGIMjJ6/8JzOEwQUrm9vI/a2cumyjTT8k=";
+    sha256 = "sha256-k6Q6Tu8drMFkQFpSRJaLk9Wejo5ciSoA0cjv4+3TuPE=";
   };
 
   modRoot = "./go";
   subPackages = [ "cmd/dolt" ];
-  vendorHash = "sha256-jP6jm4yNx6wzbUsGDBaRSGcD62wpyvct/q4q4ryqvpY=";
+  vendorHash = "sha256-SKIp3xdnVN0l/kRf4XXyEV3rOfPXsBEqFDn6uvHe5/Y=";
   proxyVendor = true;
   doCheck = false;
 
