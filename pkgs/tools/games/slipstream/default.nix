ae059c58a1fb991a719c6b9f3c91138e13c97f8a
mib <mib@kanp.ai>
2023-07-04 00:37:57 +0200

    slipstream: document slipstream weirdness

--- pkgs/tools/games/slipstream/default.nix
+++ pkgs/tools/games/slipstream/default.nix
@@ -24,6 +24,12 @@ mavenWithJdk.buildMavenPackage rec {
     install -Dm644 target/ftl-mod-manager-${version}.jar $out/share/java
     install -Dm644 target/modman.jar $out/share/java
 
+    # slipstream is very finniky about having specific
+    # folders at startup, so wrapper creates them for it.
+    # this is because slipstream expects to be started from
+    # archive it comes from, but we can't do that since
+    # we need the mods directory to be writable.
+    # see: https://github.com/Vhati/Slipstream-Mod-Manager/blob/85cad4ffbef8583d908b189204d7d22a26be43f8/src/main/java/net/vhati/modmanager/cli/SlipstreamCLI.java#L105
     makeWrapper ${jdk}/bin/java $out/bin/${pname} \
       --run 'echo $XDG_DATA_HOME > $HOME/debug' \
       --run '_dir="''${XDG_DATA_HOME:-$HOME/.local/share}/slipstream"' \
