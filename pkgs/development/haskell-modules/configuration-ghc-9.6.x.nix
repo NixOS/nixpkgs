eda8d2938155ab0734852a7dd31e35af7f4a7acd
sternenseemann <sternenseemann@systemli.org>
2023-07-03 12:15:37 +0200

    haskell.packages.ghc96.language-haskell-extract: patch TH code
    We have a similar patch for all GHCs >= 9.0.

--- pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
@@ -121,6 +121,12 @@ self: super: {
   # https://github.com/dreixel/syb/issues/40
   syb = dontCheck super.syb;
 
+  # Support for template-haskell >= 2.16
+  language-haskell-extract = appendPatch (pkgs.fetchpatch {
+    url = "https://gitlab.haskell.org/ghc/head.hackage/-/raw/dfd024c9a336c752288ec35879017a43bd7e85a0/patches/language-haskell-extract-0.2.4.patch";
+    sha256 = "0w4y3v69nd3yafpml4gr23l94bdhbmx8xky48a59lckmz5x9fgxv";
+  }) (doJailbreak super.language-haskell-extract);
+
   # 2023-04-03: plugins disabled for hls 1.10.0.0 based on
   #
   haskell-language-server =
