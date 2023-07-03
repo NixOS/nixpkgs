de64049098d95c04fb4c3860fed950bcfbe03278
Maximilian Bosch <maximilian@mbosch.me>
2023-07-03 14:00:54 +0200

    roundcube: 1.6.1 -> 1.6.2
    ChangeLog: https://github.com/roundcube/roundcubemail/releases/tag/1.6.2
    (cherry picked from commit b5c1e285a6ead67d307ddd3596d229fdb3ae6f48)

--- pkgs/servers/roundcube/default.nix
+++ pkgs/servers/roundcube/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "roundcube";
-  version = "1.6.1";
+  version = "1.6.2";
 
   src = fetchurl {
     url = "https://github.com/roundcube/roundcubemail/releases/download/${version}/roundcubemail-${version}-complete.tar.gz";
-    sha256 = "sha256-RsL2ujS8t+V+R8sDS/M45fx9zO3dqSEqLvO9MUbZe+0=";
+    sha256 = "sha256-yJgwfZXMSEGM+VUX71K1sAiMUAFPZ6bvgVdjQoqAQ/A=";
   };
 
   patches = [ ./0001-Don-t-resolve-symlinks-when-trying-to-find-INSTALL_P.patch ];
