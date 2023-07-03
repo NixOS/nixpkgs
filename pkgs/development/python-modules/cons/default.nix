3fd1391d450045cae791de71fa99c1f38f713adb
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-03 08:17:22 +0200

    python310Packages.cons: add format
    - disable on unsupported Python releases

--- pkgs/development/python-modules/cons/default.nix
+++ pkgs/development/python-modules/cons/default.nix
@@ -5,11 +5,15 @@
 , py
 , pytestCheckHook
 , pytest-html
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "cons";
   version = "0.4.6";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "pythological";
