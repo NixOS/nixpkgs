47e7af84d10f5a0caf9e3d33bbbb09986bb22ce1
Earth Engine <earthengine@skiff.com>
2023-07-03 07:42:29 +0000

    maintainers: remove earthengine

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4486,12 +4486,6 @@
     githubId = 424946;
     name = "James Earl Douglas";
   };
-  earthengine = {
-    email = "earthengine@skiff.com";
-    github = "ee2500";
-    githubId = 134107129;
-    name = "EarthEngine";
-  };
   ebbertd = {
     email = "daniel@ebbert.nrw";
     github = "ebbertd";
