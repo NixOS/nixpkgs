cb095a9384e2ae2e63d2fc7dc77a5f1337b84777
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-03 12:54:32 +0200

    python311Packages.androidtvremote2: 0.0.9 -> 0.0.10
    Diff: https://github.com/tronikos/androidtvremote2/compare/refs/tags/v0.0.9...v0.0.10
    
    Changelog: https://github.com/tronikos/androidtvremote2/releases/tag/v0.0.10

--- pkgs/development/python-modules/androidtvremote2/default.nix
+++ pkgs/development/python-modules/androidtvremote2/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "androidtvremote2";
-  version = "0.0.9";
+  version = "0.0.10";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "tronikos";
     repo = "androidtvremote2";
     rev = "refs/tags/v${version}";
-    hash = "sha256-gQ2PVEhX1jwd0yvMf/Z0yKvruDzpY5080x4IU2i/PJ4=";
+    hash = "sha256-LjYXQTPTFS80gI3WeJOLrKZ0C0JhGb5p1M70P7n29hc=";
   };
 
   nativeBuildInputs = [
