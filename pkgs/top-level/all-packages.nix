81bc889cc3ac6c2f8ca320c94914b258c0fef262
NotAShelf <raf@notashelf.dev>
2023-07-04 22:02:13 +0300

    prettierd: init at 0.23.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11576,6 +11576,8 @@ with pkgs;
 
   pretender = callPackage ../tools/security/pretender { };
 
+  prettierd = callPackage ../development/tools/prettierd { };
+
   pretty-simple = callPackage ../development/tools/pretty-simple { };
 
   prettyping = callPackage ../tools/networking/prettyping { };
