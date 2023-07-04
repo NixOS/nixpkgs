a16711996ec5738941ae45bb0c2d7e5d5327921b
figsoda <figsoda@pm.me>
2023-07-04 12:45:33 -0400

    zine: 0.15.0 -> 0.16.0
    Diff: https://diff.rs/zine/0.15.0/0.16.0
    
    Changelog: https://github.com/zineland/zine/releases/tag/v0.16.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36211,9 +36211,7 @@ with pkgs;
 
   zim = callPackage ../applications/office/zim { };
 
-  zine = callPackage ../applications/misc/zine {
-    inherit (darwin.apple_sdk.frameworks) CoreServices Security;
-  };
+  zine = callPackage ../applications/misc/zine { };
 
   zita-ajbridge = callPackage ../applications/audio/zita-ajbridge { };
 
