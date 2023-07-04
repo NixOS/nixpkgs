300e330a33985b4664e5e025e51acc0dcf74c2c2
Bruno Bigras <bigras.bruno@gmail.com>
2023-07-04 15:42:22 -0400

    kubectl-gadget: 0.18.0 -> 0.18.1

--- pkgs/applications/networking/cluster/kubectl-gadget/default.nix
+++ pkgs/applications/networking/cluster/kubectl-gadget/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "kubectl-gadget";
-  version = "0.18.0";
+  version = "0.18.1";
 
   src = fetchFromGitHub {
     owner = "inspektor-gadget";
     repo = "inspektor-gadget";
     rev = "v${version}";
-    hash = "sha256-dn2iN6eBAX3U20omKfi3cEPpMpg/1MUjrdnSQ6eJYRM=";
+    hash = "sha256-QB1OX5G0WkV8k84282+haQc2JVMUhsO99cpEMrHR9qY=";
   };
 
-  vendorHash = "sha256-26VQPKjUNcyKJFeoxfcm9RfozcBpms+6NDozxABnDuc=";
+  vendorHash = "sha256-5ydul1buJignI5KCn6TMYCjdJ6ni6NgYQrnrGBPADI4=";
 
   CGO_ENABLED = 0;
 
