f3f957898abb08f03e6440c5971fd7bdefb4e426
Daniel Olsen <daniel.olsen99@gmail.com>
2023-07-04 05:56:32 +0200

    nixos/gnunet: WIP: Upgrade module for multiuser and RFC42

--- nixos/modules/services/networking/gnunet.nix
+++ nixos/modules/services/networking/gnunet.nix
@@ -1,170 +1,109 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
-
+{config, lib, pkgs, ...}:
 let
-
   cfg = config.services.gnunet;
+  format = pkgs.formats.ini { };
 
-  stateDir = "/var/lib/gnunet";
-
-  configFile = with cfg;
-    ''
-      [PATHS]
-      GNUNET_HOME = ${stateDir}
-      GNUNET_RUNTIME_DIR = /run/gnunet
-      GNUNET_USER_RUNTIME_DIR = /run/gnunet
-      GNUNET_DATA_HOME = ${stateDir}/data
-
-      [ats]
-      WAN_QUOTA_IN = ${toString load.maxNetDownBandwidth} b
-      WAN_QUOTA_OUT = ${toString load.maxNetUpBandwidth} b
-
-      [datastore]
-      QUOTA = ${toString fileSharing.quota} MB
-
-      [transport-udp]
-      PORT = ${toString udp.port}
-      ADVERTISED_PORT = ${toString udp.port}
-
-      [transport-tcp]
-      PORT = ${toString tcp.port}
-      ADVERTISED_PORT = ${toString tcp.port}
-
-      ${extraOptions}
-    '';
-
+  configFile = format.generate "gnunet-config.conf" cfg.settings;
 in
-
 {
-
-  ###### interface
-
   options = {
-
     services.gnunet = {
-
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = lib.mdDoc ''
-          Whether to run the GNUnet daemon.  GNUnet is GNU's anonymous
-          peer-to-peer communication and file sharing framework.
-        '';
-      };
-
-      fileSharing = {
-        quota = mkOption {
-          type = types.int;
-          default = 1024;
-          description = lib.mdDoc ''
-            Maximum file system usage (in MiB) for file sharing.
-          '';
+      enable = lib.mkEnableOption "GNUnet daemon";
+      package = lib.mkPackageOption pkgs "gnunet" { };
+      settings = lib.mkOption {
+        type = lib.types.submodule {
+          freeformType = format.type;
+          options = {
+            transport-udp.PORT = lib.mkOption {
+              default = 2086;
+              type = lib.types.port;
+              description = "The UDP port for use by GNUnet.";
+            };
+          };
         };
-      };
-
-      udp = {
-        port = mkOption {
-          type = types.port;
-          default = 2086;  # assigned by IANA
-          description = lib.mdDoc ''
-            The UDP port for use by GNUnet.
-          '';
-        };
-      };
-
-      tcp = {
-        port = mkOption {
-          type = types.port;
-          default = 2086;  # assigned by IANA
-          description = lib.mdDoc ''
-            The TCP port for use by GNUnet.
-          '';
-        };
-      };
-
-      load = {
-        maxNetDownBandwidth = mkOption {
-          type = types.int;
-          default = 50000;
-          description = lib.mdDoc ''
-            Maximum bandwidth usage (in bits per second) for GNUnet
-            when downloading data.
-          '';
-        };
-
-        maxNetUpBandwidth = mkOption {
-          type = types.int;
-          default = 50000;
-          description = lib.mdDoc ''
-            Maximum bandwidth usage (in bits per second) for GNUnet
-            when downloading data.
-          '';
-        };
-
-        hardNetUpBandwidth = mkOption {
-          type = types.int;
-          default = 0;
-          description = lib.mdDoc ''
-            Hard bandwidth limit (in bits per second) when uploading
-            data.
-          '';
-        };
-      };
-
-      package = mkOption {
-        type = types.package;
-        default = pkgs.gnunet;
-        defaultText = literalExpression "pkgs.gnunet";
-        description = lib.mdDoc "Overridable attribute of the gnunet package to use.";
-        example = literalExpression "pkgs.gnunet_git";
-      };
-
-      extraOptions = mkOption {
-        type = types.lines;
-        default = "";
-        description = lib.mdDoc ''
-          Additional options that will be copied verbatim in `gnunet.conf`.
-          See {manpage}`gnunet.conf(5)` for details.
-        '';
       };
     };
-
   };
 
-
-  ###### implementation
-
-  config = mkIf config.services.gnunet.enable {
-
+  config = lib.mkIf cfg.enable {
     users.users.gnunet = {
       group = "gnunet";
       description = "GNUnet User";
       uid = config.ids.uids.gnunet;
     };
-
     users.groups.gnunet.gid = config.ids.gids.gnunet;
+    users.groups.gnunetdns.gid = config.ids.gids.gnunetdns;
 
-    # The user tools that talk to `gnunetd' should come from the same source,
-    # so install them globally.
-    environment.systemPackages = [ cfg.package ];
-
-    environment.etc."gnunet.conf".text = configFile;
-
-    systemd.services.gnunet = {
-      description = "GNUnet";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      restartTriggers = [ config.environment.etc."gnunet.conf".source ];
-      path = [ cfg.package pkgs.miniupnpc ];
-      serviceConfig.ExecStart = "${cfg.package}/lib/gnunet/libexec/gnunet-service-arm -c /etc/gnunet.conf";
-      serviceConfig.User = "gnunet";
-      serviceConfig.UMask = "0007";
-      serviceConfig.WorkingDirectory = stateDir;
-      serviceConfig.RuntimeDirectory = "gnunet";
-      serviceConfig.StateDirectory = "gnunet";
+    # TODO: Avoid putting these in $PATH
+    security.wrappers = let
+      mkGnunetSuid = source: {
+        setuid = true;
+        owner = "root";
+        group = "gnunet";
+        permissions = "o+rx,o-w,g+rx,g-w,o-rwx";
+        inherit source;
+      };
+      helpers = b: "${cfg.package}/lib/gnunet/libexec/${b}";
+    in {
+      gnunet-helper-vpn = mkGnunetSuid (helpers "gnunet-helper-vpn");
+      # These don't exist
+      #gnunet-helper-transport-wlan = mkGnunetSuid (helpers "gnunet-helper-transport-wlan");
+      #gnunet-helper-transport-bluetooth = mkGnunetSuid (helpers "gnunet-helper-transport-bluetooth");
+      gnunet-helper-exit = mkGnunetSuid (helpers "gnunet-helper-exit");
+      gnunet-helper-nat-server = mkGnunetSuid (helpers "gnunet-helper-nat-server");
+      gnunet-helper-nat-client = mkGnunetSuid (helpers "gnunet-helper-nat-client");
+      # > The binary should then be owned by root and be in group "gnunetdns"
+      # > and be installed SUID and only be group-executable (2750).
+      # But logically it should be 4750
+      gnunet-helper-dns = {
+        setuid = true;
+        owner = "root";
+        group = "gnunetdns";
+        permissions = "o+rx,o-w,g+rx,g-w,o-rwx";
+        source = (helpers "gnunet-helper-dns");
+      };
+      gnunet-service-dns = {
+        setgid = true;
+        owner = "root";
+        group = "gnunetdns";
+        permissions = "o+rx,o-w,g-rwx,o-rwx";
+        source = (helpers "gnunet-service-dns");
+      };
     };
 
-  };
+    services.gnunet.settings = {
+      arm = {
+        START_SYSTEM_SERVICES = lib.mkDefault "YES";
+        START_USER_SERVICES = lib.mkDefault "NO";
+      };
+      dns = {
+        BINARY = lib.mkDefault "/run/wrappers/bin/gnunet-service-dns";
+      };
+      PATHS = {
+        SUID_BINARY_PATH = lib.mkDefault "/run/wrappers/bin";
+        GNUNET_HOME = lib.mkDefault "/var/lib/gnunet";
+        GNUNET_RUNTIME_DIR = lib.mkDefault "/run/gnunet";
+        GNUNET_USER_RUNTIME_DIR = lib.mkDefault "/run/gnunet";
+        GNUNET_DATA_HOME = lib.mkDefault "/var/lib/gnunet/data";
+      };
+    };
 
+    systemd.services.gnunet = {
+      description = "GNUnet system deamon";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      path = [ cfg.package pkgs.miniupnpc ];
+      serviceConfig = {
+        ExecStart = "${cfg.package}/lib/gnunet/libexec/gnunet-service-arm -c ${configFile}";
+        User = "gnunet";
+        Group = "gnunet";
+        StateDirectory = "gnunet";
+        StateDirectoryMode = "0700";
+        WorkingDirectory = "/var/lib/gnunet";
+        RuntimeDirectory = "gnunet";
+      };
+    };
+
+    environment.systemPackages = [ cfg.package ];
+  };
 }
