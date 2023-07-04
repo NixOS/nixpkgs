f979f99b32c62e701e5f29f9acfd643ad8cf6626
Galen Huntington <galen@alumni.reed.edu>
2023-07-03 18:57:25 -0700

    maintainers: add galen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5751,6 +5751,11 @@
     githubId = 7047019;
     name = "Florent Becker";
   };
+  galen = {
+    github = "galenhuntington";
+    githubId = 1851962;
+    name = "Galen Huntington";
+  };
   gamb = {
     email = "adam.gamble@pm.me";
     github = "gamb";
