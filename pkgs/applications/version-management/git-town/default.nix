04d69226f73e2f6ff375e2b8e7dbfcb62158feb1
Aaron Jheng <wentworth@outlook.com>
2023-07-04 08:42:08 +0000

    git-town: 7.8.0 -> 9.0.0

--- pkgs/applications/version-management/git-town/default.nix
+++ pkgs/applications/version-management/git-town/default.nix
@@ -1,26 +1,17 @@
-{ lib, buildGoModule, fetchFromGitHub, fetchpatch, installShellFiles, git, testers, git-town, makeWrapper }:
+{ lib, buildGoModule, fetchFromGitHub, installShellFiles, git, testers, git-town, makeWrapper }:
 
 buildGoModule rec {
   pname = "git-town";
-  version = "7.8.0";
+  version = "9.0.0";
 
   src = fetchFromGitHub {
     owner = "git-town";
     repo = "git-town";
     rev = "v${version}";
-    sha256 = "sha256-g9ooMIMN8DN2FcWYkDC1hICCleQYdHf30PYMCit/NMI=";
+    hash = "sha256-huo0PRqc2iBBYXGBVdgtPJhbPoIqqjN2loXQ3CqVaOA=";
   };
 
-  patches = [
-    # Fix "go vet" when building using Go 1.18.
-    (fetchpatch {
-      name = "fix-go-vet-in-go-1.18.patch";
-      url = "https://github.com/git-town/git-town/commit/23eb0aca7b28c6a0afc21db553aa0e35d35891aa.patch";
-      sha256 = "sha256-EyfhKVrQxRJNrYqaZI04dJogaXs1J+bbOIu7p8g2Clc=";
-    })
-  ];
-
-  vendorSha256 = null;
+  vendorHash = null;
 
   nativeBuildInputs = [ installShellFiles makeWrapper ];
 
