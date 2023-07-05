233696e8ebca2d1bcddd330d8998682d9cb6134e
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-05 15:29:56 +0200

    python311Packages.oslo-log: disable failing test

--- pkgs/development/python-modules/oslo-log/default.nix
+++ pkgs/development/python-modules/oslo-log/default.nix
@@ -48,6 +48,8 @@ buildPythonPackage rec {
   disabledTests = [
     # not compatible with sandbox
     "test_logging_handle_error"
+    # File which is used doesn't seem not to be present
+    "test_log_config_append_invalid"
   ];
 
   pythonImportsCheck = [
@@ -55,10 +57,10 @@ buildPythonPackage rec {
   ];
 
   meta = with lib; {
-    broken = stdenv.isDarwin;
     description = "oslo.log library";
     homepage = "https://github.com/openstack/oslo.log";
     license = licenses.asl20;
     maintainers = teams.openstack.members;
+    broken = stdenv.isDarwin;
   };
 }
