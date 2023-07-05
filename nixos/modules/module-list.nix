5544598995425cddd81d19965ce36ff0fca17ef1
Paul Brien <paulbrien@proton.me>
2023-07-04 23:17:32 -0700

    nixos/unpackerr: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -718,6 +718,7 @@
   ./services/misc/tp-auto-kbbl.nix
   ./services/misc/tzupdate.nix
   ./services/misc/uhub.nix
+  ./services/misc/unpackerr.nix
   ./services/misc/weechat.nix
   ./services/misc/xmr-stak.nix
   ./services/misc/xmrig.nix
