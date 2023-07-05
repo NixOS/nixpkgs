bfe0012b30d2ac23d32126071d165febbe32b197
Sefa Eyeoglu <contact@scrumplex.net>
2023-07-05 18:58:49 +0200

    beets: fix embedart with imagick 7.1.1-12
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    (cherry picked from commit 7631229b5e6623e77dccc66d9cf78d56e169784c)

--- pkgs/tools/audio/beets/default.nix
+++ pkgs/tools/audio/beets/default.nix
@@ -1,6 +1,7 @@
 { lib
 , callPackage
 , fetchFromGitHub
+, fetchpatch
 }:
 /*
 ** To customize the enabled beets plugins, use the pluginOverrides input to the
@@ -26,6 +27,12 @@ lib.makeExtensible (self: {
       rev = "v${version}";
       hash = "sha256-fT+rCJJQR7bdfAcmeFRaknmh4ZOP4RCx8MXpq7/D8tM=";
     };
+    extraPatches = [
+      # Fix embedart with ImageMagick 7.1.1-12
+      # https://github.com/beetbox/beets/pull/4839
+      # The upstream patch does not apply on 1.6.0, as the related code has been refactored since
+      ./patches/stable-fix-embedart-imagick-7.1.1-12.patch
+    ];
   };
 
   beets-minimal = self.beets.override { disableAllPlugins = true; };
@@ -38,6 +45,15 @@ lib.makeExtensible (self: {
       rev = "50bd693057de472470ab5175fae0cdb5b75811c6";
       hash = "sha256-91v1StaByG60ryhQqByBXu6sFCjk0qT0nsUPnocSEE4=";
     };
+    extraPatches = [
+      (fetchpatch {
+        # Fix embedart with ImageMagick 7.1.1-12
+        # https://github.com/beetbox/beets/pull/4839
+        name = "fix-embedart-imagick-7.1.1-12.patch";
+        url = "https://github.com/beetbox/beets/commit/a873a191b9d25236774cec82df2ceb6399ed4ce3.patch";
+        hash = "sha256-1b3igHx0jKQkyVUlwOx6Oo3O1f3w8oZDw4xpHFw0DO0=";
+      })
+    ];
     pluginOverrides = {
       # unstable has a new plugin, so we register it here.
       limit = { builtin = true; };
