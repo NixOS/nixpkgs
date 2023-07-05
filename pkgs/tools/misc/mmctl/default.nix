8967531fe2861c36297e999aecdfcaf37cd6fc94
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-05 01:30:24 +0000

    mmctl: 7.10.2 -> 7.10.3

--- pkgs/tools/misc/mmctl/default.nix
+++ pkgs/tools/misc/mmctl/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "mmctl";
-  version = "7.10.2";
+  version = "7.10.3";
 
   src = fetchFromGitHub {
     owner = "mattermost";
     repo = "mmctl";
     rev = "v${version}";
-    sha256 = "sha256-cD2RwawWhemhIf14NFEDmREmoaOSKyIxveSy75O7fcU=";
+    sha256 = "sha256-ANoisFJnTEUgL0xKGaS19jqAHX3MT9RR4BO7hz/vm6E=";
   };
 
   vendorHash = null;
