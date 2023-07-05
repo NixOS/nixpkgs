c8e9615acd48e3523bc62fe7ad55176c23fb1a62
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-07-05 12:06:19 -0600

    linux: 6.4.1 -> 6.4.2

--- pkgs/os-specific/linux/kernel/linux-6.4.nix
+++ pkgs/os-specific/linux/kernel/linux-6.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.4.1";
+  version = "6.4.2";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "sha256-DZ2qnxwXb7E7lEf249gOgrSQQ/DTRMJHu/CbTmJb7vM=";
+    sha256 = "0b0623pvf9spb5biv1k4wzjbcj9fyf2svk69fdyb3ibn84ian9m3";
   };
 } // (args.argsOverride or { }))
