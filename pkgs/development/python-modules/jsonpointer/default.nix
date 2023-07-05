a0aa03338812ea895ec64b907626bc2f50696658
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-05 11:51:01 +0200

    python311Packages.jsonpointer: 2.3 -> 2.4
    Diff: https://github.com/stefankoegl/python-json-pointer/compare/refs/tags/v2.3...v2.4

--- pkgs/development/python-modules/jsonpointer/default.nix
+++ pkgs/development/python-modules/jsonpointer/default.nix
@@ -1,21 +1,36 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
+, pythonOlder
+, unittestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "jsonpointer";
-  version = "2.3";
+  version = "2.4";
+  format = "setuptools";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-l8ulFSbIKSgiGP65nasbHmvfjv0cQ9ydV74JPA1pyZo=";
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "stefankoegl";
+    repo = "python-json-pointer";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-Q69s++qqpxFzKznxTFITMA0iP3mm7K7q0hLvXMDkMCw=";
   };
+
+  nativeCheckInputs = [
+    unittestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "jsonpointer"
+  ];
 
   meta = with lib; {
     description = "Resolve JSON Pointers in Python";
     homepage = "https://github.com/stefankoegl/python-json-pointer";
-    license = licenses.bsd2; # "Modified BSD license, says pypi"
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ ];
   };
-
 }
