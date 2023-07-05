fad172a36672161235efec7b118e2240deaabd76
Robert Hensing <robert@roberthensing.nl>
2023-07-05 14:05:41 +0200

    nixos/config/nix-channel: Move NIX_PATH logic

--- nixos/modules/config/nix-channel.nix
+++ nixos/modules/config/nix-channel.nix
@@ -12,6 +12,22 @@ let
 in
 {
   options = {
+    nix = {
+      nixPath = mkOption {
+        type = types.listOf types.str;
+        default = [
+          "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+          "nixos-config=/etc/nixos/configuration.nix"
+          "/nix/var/nix/profiles/per-user/root/channels"
+        ];
+        description = lib.mdDoc ''
+          The default Nix expression search path, used by the Nix
+          evaluator to look up paths enclosed in angle brackets
+          (e.g. `<nixpkgs>`).
+        '';
+      };
+    };
+
     system = {
       defaultChannel = mkOption {
         internal = true;
@@ -23,6 +39,17 @@ in
   };
 
   config = mkIf cfg.enable {
+
+    environment.extraInit =
+      ''
+        if [ -e "$HOME/.nix-defexpr/channels" ]; then
+          export NIX_PATH="$HOME/.nix-defexpr/channels''${NIX_PATH:+:$NIX_PATH}"
+        fi
+      '';
+
+    environment.sessionVariables = {
+      NIX_PATH = cfg.nixPath;
+    };
 
     system.activationScripts.nix-channel = stringAfter [ "etc" "users" ]
       ''
--- nixos/modules/services/misc/nix-daemon.nix
+++ nixos/modules/services/misc/nix-daemon.nix
@@ -145,20 +145,6 @@ in
           you should increase this value.
         '';
       };
-
-      nixPath = mkOption {
-        type = types.listOf types.str;
-        default = [
-          "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-          "nixos-config=/etc/nixos/configuration.nix"
-          "/nix/var/nix/profiles/per-user/root/channels"
-        ];
-        description = lib.mdDoc ''
-          The default Nix expression search path, used by the Nix
-          evaluator to look up paths enclosed in angle brackets
-          (e.g. `<nixpkgs>`).
-        '';
-      };
     };
   };
 
@@ -242,14 +228,7 @@ in
       };
 
     # Set up the environment variables for running Nix.
-    environment.sessionVariables = cfg.envVars // { NIX_PATH = cfg.nixPath; };
-
-    environment.extraInit =
-      ''
-        if [ -e "$HOME/.nix-defexpr/channels" ]; then
-          export NIX_PATH="$HOME/.nix-defexpr/channels''${NIX_PATH:+:$NIX_PATH}"
-        fi
-      '';
+    environment.sessionVariables = cfg.envVars;
 
     nix.nrBuildUsers = mkDefault (
       if cfg.settings.auto-allocate-uids or false then 0
