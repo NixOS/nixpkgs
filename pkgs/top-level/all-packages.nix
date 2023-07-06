45235fe2df8d886a8388277b5dd2340b3960dde8
PowerUser64 <blake@blakenorth.net>
2023-07-06 02:26:44 -0700

    x42-mclk: init at v0.2.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35881,6 +35881,8 @@ with pkgs;
 
   x42-gmsynth = callPackage ../applications/audio/x42-gmsynth { };
 
+  x42-mclk = callPackage ../applications/audio/x42-mclk { };
+
   x42-plugins = callPackage ../applications/audio/x42-plugins { };
 
   xannotate = callPackage ../tools/X11/xannotate { };
