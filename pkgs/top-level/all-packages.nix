46227864e21ebfdaf53d53507e2fc544a0b7bc1a
PowerUser64 <blake@blakenorth.net>
2023-07-06 03:22:26 -0700

    x42-mtc: init at v0.2.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35881,6 +35881,8 @@ with pkgs;
 
   x42-gmsynth = callPackage ../applications/audio/x42-gmsynth { };
 
+  x42-mtc = callPackage ../applications/audio/x42-mtc { };
+
   x42-plugins = callPackage ../applications/audio/x42-plugins { };
 
   xannotate = callPackage ../tools/X11/xannotate { };
