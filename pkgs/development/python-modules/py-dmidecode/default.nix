d1861836b31964da8415fd511ebac8a91a901cb1
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-06 10:08:58 +0200

    python311Packages.py-dmidecode: 0.1.0 -> 0.1.2
    Changelog: https://github.com/zaibon/py-dmidecode/releases/tag/v0.1.2

--- pkgs/development/python-modules/py-dmidecode/default.nix
+++ pkgs/development/python-modules/py-dmidecode/default.nix
@@ -1,23 +1,43 @@
-{ lib, buildPythonPackage, fetchPypi, dmidecode }:
+{ lib
+, buildPythonPackage
+, dmidecode
+, fetchPypi
+, poetry-core
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "py-dmidecode";
-  version = "0.1.0";
+  version = "0.1.2";
+  format = "pyproject";
+
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "1bv1vmhj8h520kj6slwpz16xfmgp117yjjkfyihkl5ix6mn5zkpa";
+    pname = "py_dmidecode";
+    inherit version;
+    hash = "sha256-nMy/jOlg7yUPfGF27MN0NyVM0vuTIBuJTV2GKNP13UA=";
   };
 
-  propagatedBuildInputs = [ dmidecode ];
+  nativeBuildInputs = [
+    poetry-core
+  ];
 
-  # Project has no tests.
+  propagatedBuildInputs = [
+    dmidecode
+  ];
+
+  # Module has no tests
   doCheck = false;
-  pythonImportsCheck = [ "dmidecode" ];
+
+  pythonImportsCheck = [
+    "dmidecode"
+  ];
 
   meta = with lib; {
-    homepage = "https://github.com/zaibon/py-dmidecode/";
     description = "Python library that parses the output of dmidecode";
+    homepage = "https://github.com/zaibon/py-dmidecode/";
+    changelog = "https://github.com/zaibon/py-dmidecode/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ davidtwco ];
     platforms = platforms.linux;
