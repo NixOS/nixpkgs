2239bf96204f8cbcbbf7b56b42a298bf806541be
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-06 12:35:48 +0200

    python311Packages.google-cloud-error-reporting: 1.9.1 -> 1.9.2
    Changelog: https://github.com/googleapis/python-error-reporting/blob/v1.9.2/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-error-reporting/default.nix
+++ pkgs/development/python-modules/google-cloud-error-reporting/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-error-reporting";
-  version = "1.9.1";
+  version = "1.9.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-3N7LtFKvTdV0zBGIyUgi6tCVZX7+rbJD5Lb+xZafJNw=";
+    hash = "sha256-S+7x6gIxJDfV7Xe6DOBVbJNMREYlRFLyGo8BEpIdIow=";
   };
 
   propagatedBuildInputs = [
