433feff16891aff2aed337a397f1e81567b4972b
James Landrein <github@j4m3s.eu>
2023-07-06 14:04:29 +0200

    kube-score: 1.16.0 -> 1.17.0

--- pkgs/applications/networking/cluster/kube-score/default.nix
+++ pkgs/applications/networking/cluster/kube-score/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "kube-score";
-  version = "1.16.1";
+  version = "1.17.0";
 
   src = fetchFromGitHub {
     owner = "zegl";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-/aCuPL0bzzoNczvSdLYS8obTu8bZzO5HhBmRZ3/ArAM=";
+    hash = "sha256-/4xnUb60ARGO6hM5PQ3ZkuwjEQUT4Xnj/InIsfw2bzI=";
   };
 
-  vendorHash = "sha256-pcNdszOfsYKiASOUNKflbr89j/wb9ILQvjMJYsiGPWo=";
+  vendorHash = "sha256-UpuwkQHcNg3rohr+AdALakIdHroIySlTnXHgoUdY+EQ=";
 
   meta = with lib; {
     description = "Kubernetes object analysis with recommendations for improved reliability and security";
