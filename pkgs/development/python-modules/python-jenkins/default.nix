e0f3ae5c8f3ba7ccafadbe538279d2bd1fa2cc6f
Florian Brandes <florian.brandes@posteo.de>
2023-07-06 11:46:54 +0200

    python3Packages.python-jenkins: 1.7.0 -> 1.8.0
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/development/python-modules/python-jenkins/default.nix
+++ pkgs/development/python-modules/python-jenkins/default.nix
@@ -12,15 +12,17 @@
 , requests
 , requests-mock
 , stestr
+, pythonRelaxDepsHook
 }:
 
 buildPythonPackage rec {
   pname = "python-jenkins";
-  version = "1.7.0";
+  version = "1.8.0";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "01jid5s09lr3kayr2h1z9n8h9nhyw3jxv9c4b5hrlxijknkqzvfy";
+    hash = "sha256-AYClRj9o4uARDzgrQkjSKEvGhIHbShb8v2H09VgBwx8=";
   };
 
   # test uses timeout mechanism unsafe for use with the "spawn"
@@ -30,16 +32,23 @@ buildPythonPackage rec {
       --replace test_jenkins_open_no_timeout dont_test_jenkins_open_no_timeout
   '';
 
+  nativeBuildInputs = [
+    pythonRelaxDepsHook
+  ];
+  pythonRelaxDeps = [
+    "setuptools"
+  ];
+
   buildInputs = [ mock ];
   propagatedBuildInputs = [ pbr pyyaml setuptools six multi_key_dict requests ];
 
-   nativeCheckInputs = [ stestr testscenarios requests-mock ];
-   checkPhase = ''
-     # Skip tests that fail due to setuptools>=66.0.0 rejecting PEP 440
-     # non-conforming versions. See
-     # https://github.com/pypa/setuptools/issues/2497 for details.
-     stestr run -E "tests.test_plugins.(PluginsTestScenarios.test_plugin_version_comparison|PluginsTestScenarios.test_plugin_version_object_comparison|PluginsTest.test_plugin_equal|PluginsTest.test_plugin_not_equal)"
-   '';
+  nativeCheckInputs = [ stestr testscenarios requests-mock ];
+  checkPhase = ''
+    # Skip tests that fail due to setuptools>=66.0.0 rejecting PEP 440
+    # non-conforming versions. See
+    # https://github.com/pypa/setuptools/issues/2497 for details.
+    stestr run -E "tests.test_plugins.(PluginsTestScenarios.test_plugin_version_comparison|PluginsTestScenarios.test_plugin_version_object_comparison|PluginsTest.test_plugin_equal|PluginsTest.test_plugin_not_equal)"
+  '';
 
   meta = with lib; {
     description = "Python bindings for the remote Jenkins API";
