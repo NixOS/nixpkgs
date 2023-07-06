0afdb3cfa343da7d2d134f8d747ab04e6221759c
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-06 13:21:59 +0200

    python311Packages.google-cloud-automl: 2.11.1 -> 2.11.2
    Changelog: https://github.com/googleapis/python-automl/blob/v2.11.2/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-automl/default.nix
+++ pkgs/development/python-modules/google-cloud-automl/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-automl";
-  version = "2.11.1";
+  version = "2.11.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-MFYWx791WDdZLClul+f/hNHeTEmlQWEJw5zLs5FVgh8=";
+    hash = "sha256-w4kzOe8az6zcMLoNYbfkvC5Aq5zCbSNw6+PHYZkEZqI=";
   };
 
   propagatedBuildInputs = [
