88eefacce8c86109b274afb87766ff895bd56041
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-06 09:53:07 +0200

    python311Packages.pyunifiprotect: 4.10.3 -> 4.10.4
    Diff: https://github.com/briis/pyunifiprotect/compare/refs/tags/v4.10.3...v4.10.4
    
    Changelog: https://github.com/AngellusMortis/pyunifiprotect/releases/tag/v4.10.4

--- pkgs/development/python-modules/pyunifiprotect/default.nix
+++ pkgs/development/python-modules/pyunifiprotect/default.nix
@@ -31,7 +31,7 @@
 
 buildPythonPackage rec {
   pname = "pyunifiprotect";
-  version = "4.10.3";
+  version = "4.10.4";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -40,7 +40,7 @@ buildPythonPackage rec {
     owner = "briis";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-QCQMCvPRJDNcqOIQ51Pbt+x+05ySlXk2PYio4JKFEuI=";
+    hash = "sha256-8A/9yXqiFlR2fkpnTIXOoBbOCqxtC8uvGi05WU38PCU=";
   };
 
   postPatch = ''
