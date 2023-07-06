55daa844b2d60d96ad277e3831c6ee805e2158e8
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-07-06 23:35:29 +0200

    mediawiki: 1.39.3 -> 1.39.4
    https://lists.wikimedia.org/hyperkitty/list/mediawiki-announce@lists.wikimedia.org/message/HVT3U3XYY35PSCIQPHMY4VQNF3Q6MHUO/
    
    Fixes: CVE-2023-29197, CVE-2023-36674, CVE-2023-36675

--- pkgs/servers/web-apps/mediawiki/default.nix
+++ pkgs/servers/web-apps/mediawiki/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mediawiki";
-  version = "1.39.3";
+  version = "1.39.4";
 
   src = fetchurl {
     url = "https://releases.wikimedia.org/mediawiki/${lib.versions.majorMinor version}/mediawiki-${version}.tar.gz";
-    hash = "sha256-41dpNDh2r0JJbaQ64vRyJPuMd5uPRXBcQUfG/zUizB0=";
+    hash = "sha256-nZW3xokAQ42H8BGRt8ZRJlm5KaBr9n8IqFjsLVEj4/U=";
   };
 
   postPatch = ''
