7e22b61f31b93c1d7ba1733121bbd5fb550fabec
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-06 12:19:43 +0200

    python311Packages.google-cloud-trace: 1.11.1 -> 1.11.2
    Changelog: https://github.com/googleapis/python-trace/blob/v1.11.2/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-trace/default.nix
+++ pkgs/development/python-modules/google-cloud-trace/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-trace";
-  version = "1.11.1";
+  version = "1.11.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-twaJlx/2W1BQCWZMTQbZOp74oFecL2vwZkAxK/XwE8E=";
+    hash = "sha256-pn2ofYfcHBxAw7IK5lKqf1AGj8e+DU8W++9XVFPZ5Ws=";
   };
 
   propagatedBuildInputs = [
