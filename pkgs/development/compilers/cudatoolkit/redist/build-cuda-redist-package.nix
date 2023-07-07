3f5805d8fc32d2e25a7504a93be1068b0471ab0b
Someone Serge <sergei.kozlukov@aalto.fi>
2023-07-07 14:49:43 +0300

    cudaPackages: add jetson support

--- pkgs/development/compilers/cudatoolkit/redist/build-cuda-redist-package.nix
+++ pkgs/development/compilers/cudatoolkit/redist/build-cuda-redist-package.nix
@@ -4,21 +4,58 @@
 , fetchurl
 , autoPatchelfHook
 , autoAddOpenGLRunpathHook
+, manifestAttribute ? null
 }:
 
 pname:
 attrs:
 
 let
-  arch = "linux-x86_64";
+  systemToManifestAttribute = {
+    "x86_64-linux" = [ "linux-x86_64" ];
+    "aarch64-linux" = [ "linux-aarch64" "linux-sbsa" ];
+    "powerpc64le-linux" = [ "linux-ppc64le" ];
+  };
+  systemToManifestAttributeOrDefault = default: system:
+    if builtins.hasAttr system systemToManifestAttribute && builtins.length systemToManifestAttribute.${system} > 0
+    then builtins.head systemToManifestAttribute.${system}
+    else default; # Avoids evaluation errors on missing attributes
+  isSystemSupported = system: builtins.any (at: builtins.hasAttr at attrs) systemToManifestAttribute.${system};
+  supportedPlatforms = builtins.concatMap
+    (system: if isSystemSupported system then [ system ] else [ ])
+    (builtins.attrNames systemToManifestAttribute);
+
+  inherit (stdenv) system;
+
+  renames = {
+    manifestAttribute =
+      if manifestAttribute == null
+      then systemToManifestAttributeOrDefault "linux-x86_64" system
+      else manifestAttribute;
+  };
 in
+let
+  inherit (renames) manifestAttribute;
+
+  # Generally, we avoid assertions in favour of setting the `meta.broken`
+  # attribute, which can be overridden by the user. These tests are assertions
+  # because if they fail, they likely fail even without any overrides, and their
+  # failure signals a coding error right in this file.
+  validAttributeChoice = lib.assertMsg (builtins.hasAttr manifestAttribute attrs) "Trying to evaluate ${pname} for ${system}, but the chosen attribue (${manifestAttribute}) isn't in the manifest";
+in
+
+assert validAttributeChoice;
+assert builtins.all
+  (system: builtins.elem system lib.platforms.all)
+  (builtins.attrNames systemToManifestAttribute);
+
 backendStdenv.mkDerivation {
   inherit pname;
   inherit (attrs) version;
 
-  src = assert (lib.hasAttr arch attrs); fetchurl {
-    url = "https://developer.download.nvidia.com/compute/cuda/redist/${attrs.${arch}.relative_path}";
-    inherit (attrs.${arch}) sha256;
+  src = fetchurl {
+    url = "https://developer.download.nvidia.com/compute/cuda/redist/${attrs.${manifestAttribute}.relative_path}";
+    inherit (attrs.${manifestAttribute}) sha256;
   };
 
   nativeBuildInputs = [
@@ -56,11 +93,12 @@ backendStdenv.mkDerivation {
   '';
 
   passthru.stdenv = backendStdenv;
+  passthru.manifestAttribute = manifestAttribute;
 
   meta = {
     description = attrs.name;
     license = lib.licenses.unfree;
     maintainers = lib.teams.cuda.members;
-    platforms = lib.optionals (lib.hasAttr arch attrs) [ "x86_64-linux" ];
+    platforms = supportedPlatforms;
   };
 }
