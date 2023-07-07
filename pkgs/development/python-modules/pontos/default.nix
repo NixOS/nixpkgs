009ab7fd3056f8dbabc0e32c7e4347b7ab510323
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-07 10:39:50 +0200

    python311Packages.pontos: 23.7.2 -> 23.7.3
    Diff: https://github.com/greenbone/pontos/compare/refs/tags/v23.7.2...23.7.3
    
    Changelog: https://github.com/greenbone/pontos/releases/tag/v23.7.3

--- pkgs/development/python-modules/pontos/default.nix
+++ pkgs/development/python-modules/pontos/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pontos";
-  version = "23.7.2";
+  version = "23.7.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "greenbone";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-wdnYHpIrSGEOBC1mw8aHXCcSney0IDc1MKSFsbGNjq0=";
+    hash = "sha256-c4arZrALpvJG7HJhH+nuE8bb5BB7CAUcdcDQdGi4X1M=";
   };
 
   nativeBuildInputs = [
