ab3a39038620e66ec58e7d0778042fb136adc473
Niols <niols@niols.fr>
2023-07-07 17:34:24 +0000

    headache: 1.06 -> 1.07

--- pkgs/development/tools/headache/default.nix
+++ pkgs/development/tools/headache/default.nix
@@ -4,17 +4,17 @@ with ocamlPackages;
 
 buildDunePackage rec {
   pname = "headache";
-  version = "1.06";
+  version = "1.07";
 
   src = fetchFromGitHub {
     owner = "frama-c";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-BA7u09MKYMyspFX8AcAkDVA6UUG5DKAdbIDdt+b3Fc4=";
+    sha256 = "sha256-RL80ggcJSJFu2UTECUNP6KufRhR8ZnG7sQeYzhrw37g=";
   };
 
   propagatedBuildInputs = [
-    (camomile.override { version = "1.0.2"; })
+    camomile
   ];
 
   meta = with lib; {
