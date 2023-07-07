9be0bfb5e06ce2d88b40df1235142f99389b39c4
Naïm Favier <n@monade.li>
2023-07-07 19:10:03 +0200

    runCommand: don't set meta.position if meta is given
    Fixes #242093 without breaking #237589
    
    If `meta` is given we probably already have position information, so no
    need to get it from the arguments.

--- pkgs/build-support/trivial-builders/default.nix
+++ pkgs/build-support/trivial-builders/default.nix
@@ -87,6 +87,8 @@ rec {
       inherit buildCommand name;
       passAsFile = [ "buildCommand" ]
         ++ (derivationArgs.passAsFile or []);
+    }
+    // lib.optionalAttrs (! derivationArgs?meta) {
       pos = let args = builtins.attrNames derivationArgs; in
         if builtins.length args > 0
         then builtins.unsafeGetAttrPos (builtins.head args) derivationArgs
