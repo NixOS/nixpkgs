a5e79196216477da64949c13f493437815c149cf
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-07-07 23:02:18 -0700

    tandoor-recipes, tandoor-recipes.frontend: adapt update script to new nix-prefetch-github
    We also take the opportunity also to migrate from "sha256" and
    "yarnSha256" attributes to "hash" and "yarnHash" attributes.

--- pkgs/applications/misc/tandoor-recipes/common.nix
+++ pkgs/applications/misc/tandoor-recipes/common.nix
@@ -6,10 +6,10 @@ rec {
     owner = "TandoorRecipes";
     repo = "recipes";
     rev = version;
-    sha256 = "sha256-ZGPXcpicDYCE+J9mC2Dk/Ds2NYfUETuKXqHxpAGH86w=";
+    hash = "sha256-ZGPXcpicDYCE+J9mC2Dk/Ds2NYfUETuKXqHxpAGH86w=";
   };
 
-  yarnSha256 = "sha256-LJ0uL66tcK6zL8Mkd2UB8dHsslMTtf8wQmgbZdvOT6s=";
+  yarnHash = "sha256-LJ0uL66tcK6zL8Mkd2UB8dHsslMTtf8wQmgbZdvOT6s=";
 
   meta = with lib; {
     homepage = "https://tandoor.dev/";
