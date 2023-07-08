bab312ab3c55269822a2a4f052112dae7f42909e
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-08 07:31:39 +0000

    temporal: 1.21.0 -> 1.21.1

--- pkgs/applications/networking/cluster/temporal/default.nix
+++ pkgs/applications/networking/cluster/temporal/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "temporal";
-  version = "1.21.0";
+  version = "1.21.1";
 
   src = fetchFromGitHub {
     owner = "temporalio";
     repo = "temporal";
     rev = "v${version}";
-    hash = "sha256-PhJLO+0JoSGS/aN6ZZoCwSypm8hihwAjsav+l4NSNZo=";
+    hash = "sha256-fa8UQk3E1XhMqd7E9VRYOv6RLQW8smSUw48FeYBqmSU=";
   };
 
   vendorHash = "sha256-rgUdoFR7Qcp1h7v63DAWwx6NWSwWrJ6C6/b2tx2kCCw=";
