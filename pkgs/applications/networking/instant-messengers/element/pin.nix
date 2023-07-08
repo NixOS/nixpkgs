39f8aa3001e1eea5297d11865cc9081ae145ecbf
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-07-07 23:02:22 -0700

    element-desktop, element-web: adapt update script to new nix-prefetch-github

--- pkgs/applications/networking/instant-messengers/element/pin.nix
+++ pkgs/applications/networking/instant-messengers/element/pin.nix
@@ -1,9 +1,9 @@
 {
   "version" = "1.11.35";
   "hashes" = {
-    "desktopSrcHash" = "8BP7PC0ZqE3d0K1AxmG05Xh3Ze1dAOcBVW9ADW4YAjY=";
-    "desktopYarnHash" = "1k8ih7z9hxm38kbvnfimd0djwqlrs62s8i0hc6d6ii10l3binkzp";
-    "webSrcHash" = "IM1M8iygeya8hw0uVjV4EK/jGG4UyQUTviYAvAjI7k4=";
-    "webYarnHash" = "0lr5cgs8nhdjrv43pcyhq4ysrz8bncx0j969j82l0chq3nzdml5b";
+    "desktopSrcHash" = "sha256-8BP7PC0ZqE3d0K1AxmG05Xh3Ze1dAOcBVW9ADW4YAjY=";
+    "desktopYarnHash" = "sha256-908b16AgxGiaYRBEpIXRmWIuG2g1OrvXRKN2mP6BEc0=";
+    "webSrcHash" = "sha256-IM1M8iygeya8hw0uVjV4EK/jGG4UyQUTviYAvAjI7k4=";
+    "webYarnHash" = "sha256-q9Davh0YMkAFkskkCTqzC/2sPcHQszvIzrJBi/RjJVM=";
   };
 }
