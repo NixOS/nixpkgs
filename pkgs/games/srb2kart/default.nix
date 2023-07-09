a87d3406ea8dc2952d581ff842ccc1191c357cf7
Donovan Glover <donovan@dglover.co>
2023-07-09 15:44:29 -0400

    srb2kart: 1.3 -> 1.6
    Based on #187320

--- pkgs/games/srb2kart/default.nix
+++ pkgs/games/srb2kart/default.nix
@@ -2,7 +2,6 @@
 , stdenv
 , fetchurl
 , fetchFromGitHub
-, substituteAll
 , cmake
 , curl
 , nasm
@@ -12,32 +11,56 @@
 , SDL2
 , SDL2_mixer
 , zlib
+, p7zip
+, makeWrapper
+, makeDesktopItem
+, copyDesktopItems
 }:
 
-let
-
-release_tag = "v1.3";
-
-installer = fetchurl {
-  url = "https://github.com/STJr/Kart-Public/releases/download/${release_tag}/srb2kart-v13-Installer.exe";
-  sha256 = "0bk36y7wf6xfdg6j0b8qvk8671hagikzdp5nlfqg478zrj0qf6cs";
-};
-
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "srb2kart";
-  version = "1.3.0";
+  version = "1.6";
 
   src = fetchFromGitHub {
     owner = "STJr";
     repo = "Kart-Public";
-    rev = release_tag;
-    sha256 = "131g9bmc9ihvz0klsc3yzd0pnkhx3mz1vzm8y7nrrsgdz5278y49";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-5sIHdeenWZjczyYM2q+F8Y1SyLqL+y77yxYDUM3dVA0=";
+  };
+
+  assets = stdenv.mkDerivation {
+    pname = "srb2kart-data";
+    version = finalAttrs.version;
+
+    nativeBuildInputs = [ p7zip ];
+
+    src = fetchurl {
+      url = "https://github.com/STJr/Kart-Public/releases/download/v${finalAttrs.version}/AssetsLinuxOnly.zip";
+      sha256 = "sha256-ejhPuZ1C8M9B0S4+2HN1T5pbormT1eVL3nlivqOszdE=";
+    };
+
+    unpackPhase = ''
+      7z -y x "$src"
+    '';
+
+    sourceRoot = ".";
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/share/srb2kart
+      cp -r mdls/ *.kart *.dat *.pdf *.srb $out/share/srb2kart/
+
+      runHook postInstall
+    '';
   };
 
   nativeBuildInputs = [
     cmake
     nasm
     unzip
+    makeWrapper
+    copyDesktopItems
   ];
 
   buildInputs = [
@@ -50,39 +73,45 @@ in stdenv.mkDerivation rec {
   ];
 
   cmakeFlags = [
-    #"-DSRB2_ASSET_DIRECTORY=/build/source/assets"
+    "-DSRB2_ASSET_DIRECTORY=${finalAttrs.assets}/share/srb2kart"
     "-DGME_INCLUDE_DIR=${game-music-emu}/include"
     "-DSDL2_MIXER_INCLUDE_DIR=${lib.getDev SDL2_mixer}/include/SDL2"
     "-DSDL2_INCLUDE_DIR=${lib.getDev SDL2}/include/SDL2"
   ];
 
-  patches = [
-    ./wadlocation.patch
+  # Desktop icon
+  desktopItems = [
+    (makeDesktopItem rec {
+      name = "Sonic Robo Blast 2 Kart";
+      exec = finalAttrs.pname;
+      icon = finalAttrs.pname;
+      comment = "Kart racing mod based on SRB2";
+      desktopName = name;
+      genericName = name;
+      categories = [ "Game" ];
+    })
   ];
 
-  postPatch = ''
-    substituteInPlace src/sdl/i_system.c \
-        --replace '@wadlocation@' $out
-  '';
+  installPhase = ''
+    runHook preInstall
 
-  preConfigure = ''
-    mkdir assets/installer
-    pushd assets/installer
-    unzip ${installer} "*.kart" srb2.srb
-    popd
-  '';
+    mkdir -p $out/bin $out/share/applications $out/share/pixmaps $out/share/icons
 
-  postInstall = ''
-    mkdir -p $out/bin $out/share/games/SRB2Kart
-    mv $out/srb2kart* $out/bin/
-    mv $out/*.kart $out/share/games/SRB2Kart
+    cp ../srb2.png $out/share/pixmaps/srb2kart.png
+    cp ../srb2.png $out/share/icons/srb2kart.png
+
+    cp bin/srb2kart $out/bin/srb2kart
+    wrapProgram $out/bin/srb2kart \
+      --set SRB2WADDIR "${finalAttrs.assets}/share/srb2kart"
+
+    runHook postInstall
   '';
 
   meta = with lib; {
-    description = "SRB2Kart is a classic styled kart racer";
-    homepage = "https://mb.srb2.org/threads/srb2kart.25868/";
+    description = "A kart racing mod based on the fangame Sonic Robo Blast 2";
+    homepage = "https://mb.srb2.org/addons/srb2kart.2435/";
     platforms = platforms.linux;
     license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ viric ];
+    maintainers = with maintainers; [ viric donovanglover ];
   };
-}
+})
