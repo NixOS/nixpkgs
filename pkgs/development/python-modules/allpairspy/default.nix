e9ffb19d7949b14f49020cb08e7010ed58c01499
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-10 08:30:51 +0000

    python310Packages.allpairspy: 2.5.0 -> 2.5.1

--- pkgs/development/python-modules/allpairspy/default.nix
+++ pkgs/development/python-modules/allpairspy/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "allpairspy";
-  version = "2.5.0";
+  version = "2.5.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "9358484c91abe74ba18daf9d6d6904c5be7cc8818397d05248c9d336023c28b1";
+    sha256 = "sha256-9p0xo7Vu7hGdHsYGPpxzLdRPu6NS73OMsi2WmfxACf4=";
   };
 
   propagatedBuildInputs = [ six ];
