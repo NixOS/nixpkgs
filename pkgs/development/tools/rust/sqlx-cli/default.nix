6c4a8afd9c629fb5b9d99a1ee0019dfb2fe87cf6
Simon Menke <simon.menke@gmail.com>
2023-07-10 09:58:51 +0200

    sqlx-cli: 0.6.2 -> 0.7.0

--- pkgs/development/tools/rust/sqlx-cli/default.nix
+++ pkgs/development/tools/rust/sqlx-cli/default.nix
@@ -15,25 +15,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "sqlx-cli";
-  version = "0.6.2";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "launchbadge";
     repo = "sqlx";
     rev = "v${version}";
-    sha256 = "sha256-pQlrKjhOJfjNEmLxqnFmmBY1naheZUsaq2tGdLKGxjg=";
+    sha256 = "sha256-f9DCavvqq/a+1wusKlc3jOjyFRVMIAHGCryZxV5qews=";
   };
 
-  patches = [
-    # https://github.com/launchbadge/sqlx/pull/2228
-    (fetchpatch {
-      name = "fix-rust-1.65-compile.patch";
-      url = "https://github.com/launchbadge/sqlx/commit/2fdf85b212332647dc4ac47e087df946151feedf.patch";
-      hash = "sha256-5BCuIwmECe9qQrdYll7T+UOGwuTBolWEhKNE7GcZqJw=";
-    })
-  ];
-
-  cargoSha256 = "sha256-AbA8L7rkyZfKW0vvjyrcW5eU6jGD+zAqIcEUOJmeqJs=";
+  cargoSha256 = "sha256-9vbrehtfw6ctIF7uXZPvODx3kkxz+m9h2Uv+2t45I0w=";
 
   doCheck = false;
   cargoBuildFlags = [ "-p sqlx-cli" ];
