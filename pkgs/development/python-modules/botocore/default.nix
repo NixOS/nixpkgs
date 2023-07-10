8abcc09cd3778c5b4e5b32f050cfc18e2ec4a323
John Soo <john.soo@arista.com>
2023-07-10 13:03:12 -0700

    python3Packages.botocore: 1.24.11 -> 1.24.12
    # Conflicts:
    #	pkgs/development/python-modules/botocore/default.nix

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -13,11 +13,11 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.23.35"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.24.12"; # N.B: if you change this, change boto3 and awscli to a matching version
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "5be6ba6c5ea71c256da8a5023bf9c278847c4b90fdb40f2c4c3bdb21ca11ff28";
+    sha256 = "sha256-AXSZmgSwouQkVxBgk6zps2+pR3KkQtm89gdQJj0dBz4=";
   };
 
   propagatedBuildInputs = [
