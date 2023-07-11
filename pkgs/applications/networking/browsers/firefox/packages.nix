0565f0e6c5e7a90742a155a0557c92b096c8c235
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-07-11 17:32:43 +0200

    firefox-devedition-unwrapped: 116.0b2 -> 116.0b3

--- pkgs/applications/networking/browsers/firefox/packages.nix
+++ pkgs/applications/networking/browsers/firefox/packages.nix
@@ -56,12 +56,12 @@
 
   firefox-devedition = buildMozillaMach rec {
     pname = "firefox-devedition";
-    version = "116.0b2";
+    version = "116.0b3";
     applicationName = "Mozilla Firefox Developer Edition";
     branding = "browser/branding/aurora";
     src = fetchurl {
       url = "mirror://mozilla/devedition/releases/${version}/source/firefox-${version}.source.tar.xz";
-      sha512 = "8e186aa7d74b57341589a9737c779267874445ef38a7f32a9b9093694d411a3a2c38b723fb7c676bb69c666ad34c82230900fc690ecc306e9d59150622163bc2";
+      sha512 = "61b07bc9a706af8e947a0f206edb219650f8d64da5db821ca115375e379a11e4623dfe4e5b8734d0992e048ad8017152f8540e921105c3a266ad9347ef7b88e9";
     };
 
     meta = {
