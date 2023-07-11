c9c980702f12ea6336d99d02856d49c325af9cdf
aditya <bluenerd@protonmail.com>
2023-07-11 23:18:10 +0530

    Use SRI hashes

--- pkgs/applications/networking/browsers/microsoft-edge/default.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/default.nix
@@ -3,18 +3,18 @@
     channel = "stable";
     version = "114.0.1823.79";
     revision = "1";
-    sha256 = "sha256-17212c206c060f35f6dac70a77c2faeeec398a9a8dd5ef42ac2faa0024416518";
+    sha256 = "sha256-FyEsIGwGDzX22scKd8L67uw5ipqN1e9CrC+qACRBZRg=";
   };
   beta = import ./browser.nix {
     channel = "beta";
     version = "115.0.1901.165";
     revision = "1";
-    sha256 = "sha256-d8351690622d8acb0b1ad2403032873633033e1b1834a690549df4fad325928c";
+    sha256 = "sha256-2DUWkGItissLGtJAMDKHNjMDPhsYNKaQVJ30+tMlkow=";
   };
   dev = import ./browser.nix {
     channel = "dev";
     version = "116.0.1938.10";
     revision = "1";
-    sha256 = "sha256-3505da2e65fc02d2c458f924cc003dd177e615f970ba5c55447b4827e9f10a4e";
+    sha256 = "sha256-NQXaLmX8AtLEWPkkzAA90XfmFflwulxVRHtIJ+nxCk4=";
   };
 }
