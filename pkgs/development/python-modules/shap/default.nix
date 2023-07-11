04722887b4c7d981ea9011e5bb2316046c997f9c
natsukium <tomoya.otabi@gmail.com>
2023-07-11 12:25:28 +0900

    python310Packages.shap: 0.41.0 -> 0.42.0
    Diff: https://github.com/slundberg/shap/compare/refs/tags/v0.41.0...v0.42.0
    
    Changelog: https://github.com/slundberg/shap/releases/tag/v0.42.0

--- pkgs/development/python-modules/shap/default.nix
+++ pkgs/development/python-modules/shap/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , fetchFromGitHub
 , writeText
+, fetchpatch
 , isPy27
 , pytestCheckHook
 , pytest-mpl
@@ -27,15 +28,23 @@
 
 buildPythonPackage rec {
   pname = "shap";
-  version = "0.41.0";
+  version = "0.42.0";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "slundberg";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-rYVWQ3VRvIObSQPwDRsxhTOGOKNkYkLtiHzVwoB3iJ0=";
+    hash = "sha256-VGlswr9ywHk4oKSmmAzEC7+E0V2XEFlg19zXVktUdhc=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "fix-circular-import-error.patch";
+      url = "https://github.com/slundberg/shap/commit/ce118526b19b4a206cf8b496c2cd2b215ef7a91b.patch";
+      hash = "sha256-n2yFjFgc2VSFKb4ZJx775HblULWfnQSEnqjfPa8AOt0=";
+    })
+  ];
 
   propagatedBuildInputs = [
     numpy
@@ -58,7 +67,7 @@ buildPythonPackage rec {
     # tests that try to access the network will raise, get caught, be marked as skipped and tagged as xfailed.
     conftestSkipNetworkErrors = writeText "conftest.py" ''
       from _pytest.runner import pytest_runtest_makereport as orig_pytest_runtest_makereport
-      import urllib, requests
+      import urllib, requests, transformers
 
       class NetworkAccessDeniedError(RuntimeError): pass
       def deny_network_access(*a, **kw):
@@ -68,6 +77,7 @@ buildPythonPackage rec {
       requests.get  = deny_network_access
       urllib.request.urlopen = deny_network_access
       urllib.request.Request = deny_network_access
+      transformers.AutoTokenizer.from_pretrained = deny_network_access
 
       def pytest_runtest_makereport(item, call):
         tr = orig_pytest_runtest_makereport(item, call)
@@ -81,14 +91,11 @@ buildPythonPackage rec {
     # when importing the local copy the extension is not found
     rm -r shap
 
-    # coverage testing is a waste considering how much we have to skip
-    substituteInPlace pytest.ini \
-      --replace "--cov=shap --cov-report=term-missing" ""
-
     # Add pytest hook skipping tests that access network.
     # These tests are marked as "Expected fail" (xfail)
     cat ${conftestSkipNetworkErrors} >> tests/conftest.py
   '';
+
   nativeCheckInputs = [
     pytestCheckHook
     pytest-mpl
@@ -106,29 +113,16 @@ buildPythonPackage rec {
     sentencepiece
   ];
   disabledTestPaths = [
-    # takes forever without GPU acceleration
-    "tests/explainers/test_deep.py"
-    "tests/explainers/test_gradient.py"
-    # requires GPU. We skip here instead of having pytest repeatedly check for GPU
-    "tests/explainers/test_gpu_tree.py"
     # The resulting plots look sane, but does not match pixel-perfectly with the baseline.
     # Likely due to a matplotlib version mismatch, different backend, or due to missing fonts.
     "tests/plots/test_summary.py" # FIXME: enable
-    # 100% of the tests in these paths require network
-    "tests/explainers/test_explainer.py"
-    "tests/explainers/test_exact.py"
-    "tests/explainers/test_partition.py"
-    "tests/maskers/test_fixed_composite.py"
-    "tests/maskers/test_text.py"
-    "tests/models/test_teacher_forcing_logits.py"
-    "tests/models/test_text_generation.py"
   ];
   disabledTests = [
-    # unstable. A xgboost-enabled test. possibly related: https://github.com/slundberg/shap/issues/2480
-    "test_provided_background_tree_path_dependent"
+    # The same reason as above test_summary.py
+    "test_simple_bar_with_cohorts_dict"
+    "test_random_summary_violin_with_data2"
+    "test_random_summary_layered_violin_with_data2"
   ];
-
-  #pytestFlagsArray = ["-x" "-W" "ignore"]; # uncomment this to debug
 
   pythonImportsCheck = [
     "shap"
@@ -150,8 +144,5 @@ buildPythonPackage rec {
     license = licenses.mit;
     maintainers = with maintainers; [ evax ];
     platforms = platforms.unix;
-    # No support for scikit-learn > 1.2
-    # https://github.com/slundberg/shap/issues/2866
-    broken = true;
   };
 }
