db624d3572a92b4c538dee4acf9af18efe4a4101
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2023-07-11 12:41:18 +0200

    maintainers: add taj-ny

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15940,6 +15940,12 @@
     githubId = 321799;
     name = "Paul Colomiets";
   };
+  taj-ny = {
+    email = "nixpkgs@taj-ny.dev";
+    github = "taj-ny";
+    githubId = 79316397;
+    name = "Marcin Woźniak";
+  };
   takagiy = {
     email = "takagiy.4dev@gmail.com";
     github = "takagiy";
