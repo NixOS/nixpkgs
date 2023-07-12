d1ef125f131c2898fed91adeadd6f5ddbfe4aedd
adisbladis <adisbladis@gmail.com>
2023-07-12 20:18:25 +1200

    cgif: init at 0.3.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30289,6 +30289,8 @@ with pkgs;
 
   boops = callPackage ../applications/audio/boops { };
 
+  cgif = callPackage ../tools/graphics/cgif { };
+
   ChowCentaur  = callPackage ../applications/audio/ChowCentaur { };
 
   ChowKick  = callPackage ../applications/audio/ChowKick { };
