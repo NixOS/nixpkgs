73e1e20e54aec4eec7a5fb98d7902099aded1243
Nick Cao <nickcao@nichi.co>
2023-07-12 10:09:57 +0800

    thunderbirdPackages.thunderbird: default to thunderbird-115

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -1,7 +1,7 @@
 { stdenv, lib, buildMozillaMach, callPackage, fetchurl, fetchpatch, nixosTests }:
 
 rec {
-  thunderbird = thunderbird-102;
+  thunderbird = thunderbird-115;
 
   thunderbird-102 = (buildMozillaMach rec {
     pname = "thunderbird";
