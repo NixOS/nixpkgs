633b9d6bcf08f97bba63782e06bf3e905ddb278d
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-12 20:20:18 +0000

    python310Packages.cpyparsing: 2.4.7.1.2.1 -> 2.4.7.2.1.1

--- pkgs/development/python-modules/cpyparsing/default.nix
+++ pkgs/development/python-modules/cpyparsing/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "cpyparsing";
-  version = "2.4.7.1.2.1";
+  version = "2.4.7.2.1.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "evhub";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-HJ0I5DKZ2WV+1pXZCvJHA7Wih3Gkn7vL/ojXnTssKxw=";
+    hash = "sha256-ZJKWJhqhnTbTAf/48Whq2mSNIp/Ar17syFWvpD3w4fE=";
   };
 
   nativeBuildInputs = [
