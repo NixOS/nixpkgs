0c6070c15ebfeb5a67dff99c465cc54f778b3fa0
adisbladis <adisbladis@gmail.com>
2023-07-12 20:24:16 +1200

    vips: Add cgif

--- pkgs/tools/graphics/vips/default.nix
+++ pkgs/tools/graphics/vips/default.nix
@@ -35,6 +35,7 @@
 , libjxl
 , openslide
 , libheif
+, cgif
 }:
 
 stdenv.mkDerivation rec {
@@ -92,6 +93,7 @@ stdenv.mkDerivation rec {
     libjxl
     openslide
     libheif
+    cgif
   ] ++ lib.optionals stdenv.isDarwin [ ApplicationServices Foundation ];
 
   # Required by .pc file
@@ -100,7 +102,6 @@ stdenv.mkDerivation rec {
   ];
 
   mesonFlags = [
-    "-Dcgif=disabled"
     "-Dpdfium=disabled"
     "-Dnifti=disabled"
   ] ++ lib.optionals (!stdenv.isDarwin) [
