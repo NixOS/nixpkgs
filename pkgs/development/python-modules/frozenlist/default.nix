0815b5a7fe11110eccacebb9e5c88de4dc5cd5e1
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-13 06:54:15 +0200

    python311Packages.frozenlist: 1.3.3 -> 1.4.0
    Diff: https://github.com/aio-libs/frozenlist/compare/refs/tags/v1.3.3...v1.4.0
    
    Changelog: https://github.com/aio-libs/frozenlist/blob/v1.4.0/CHANGES.rst

--- pkgs/development/python-modules/frozenlist/default.nix
+++ pkgs/development/python-modules/frozenlist/default.nix
@@ -8,16 +8,16 @@
 
 buildPythonPackage rec {
   pname = "frozenlist";
-  version = "1.3.3";
+  version = "1.4.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "aio-libs";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-lJWRdXvuzyvJwNSpv0+ojY4rwws3jwDtlLOqYyLPrZc=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-sI6jnrTxDbW0sNVodpCjBnA31VAAmunwMp9s8GkoHGI=";
   };
 
   nativeBuildInputs = [
@@ -30,7 +30,7 @@ buildPythonPackage rec {
 
   postPatch = ''
     substituteInPlace pytest.ini \
-      --replace "--cov=frozenlist" ""
+      --replace " --cov=frozenlist --cov=tests/" ""
   '';
 
   preBuild = ''
@@ -44,6 +44,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python module for list-like structure";
     homepage = "https://github.com/aio-libs/frozenlist";
+    changelog = "https://github.com/aio-libs/frozenlist/blob/v${version}/CHANGES.rst";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ fab ];
   };
