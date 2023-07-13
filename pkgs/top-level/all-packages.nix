34692813e8145074722a284aa13b6c3ed4892694
OPNA2608 <christoph.neidahl@gmail.com>
2023-07-13 19:33:29 +0200

    libayatana-common: init at 0.9.8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21961,6 +21961,10 @@ with pkgs;
 
   libavif = callPackage ../development/libraries/libavif { };
 
+  libayatana-common = callPackage ../development/libraries/libayatana-common {
+    inherit (lomiri) cmake-extras;
+  };
+
   libb2 = callPackage ../development/libraries/libb2 { };
 
   libbacktrace = callPackage ../development/libraries/libbacktrace { };
