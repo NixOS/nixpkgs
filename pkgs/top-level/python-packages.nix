7764e28784180311051541497bc19a7ec890dbda
happysalada <raphael@megzari.com>
2023-07-13 18:48:49 +0800

    python310Packages.xformers: init at 0.0.20

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13487,6 +13487,8 @@ self: super: with self; {
     inherit (pkgs) graphviz;
   };
 
+  xformers = callPackage ../development/python-modules/xformers { };
+
   xgboost = callPackage ../development/python-modules/xgboost {
     inherit (pkgs) xgboost;
   };
