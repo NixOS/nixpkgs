e7059632c66003bc12d81559e471519a24481585
Colin <colin@uninsane.org>
2023-07-13 09:27:37 +0000

    nixos/trust-dns: init
    Co-authored-by: Yt <happysalada@tuta.io>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1059,6 +1059,7 @@
   ./services/networking/tox-node.nix
   ./services/networking/toxvpn.nix
   ./services/networking/trickster.nix
+  ./services/networking/trust-dns.nix
   ./services/networking/tvheadend.nix
   ./services/networking/twingate.nix
   ./services/networking/ucarp.nix
