9c4074e96a8db90123c79630400aebdc657de5fe
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-13 11:35:45 +0000

    lefthook: 1.4.3 -> 1.4.4

--- pkgs/applications/version-management/lefthook/default.nix
+++ pkgs/applications/version-management/lefthook/default.nix
@@ -6,7 +6,7 @@
 
 let
   pname = "lefthook";
-  version = "1.4.3";
+  version = "1.4.4";
 in
 buildGoModule rec {
   inherit pname version;
@@ -15,10 +15,10 @@ buildGoModule rec {
     owner = "evilmartians";
     repo = "lefthook";
     rev = "v${version}";
-    hash = "sha256-y3oTGFZ3AXlNVt3NDfjM3audxZ4/zqt1SME+2g8nut8=";
+    hash = "sha256-YVnf+ieYnvNQkw6W2gPBFiZLknaBknptv4ltvGKdw04=";
   };
 
-  vendorHash = "sha256-xeOWbfKy+LeInxcRM9evE/kmqlWlKy0mcHopWpc/DO0=";
+  vendorHash = "sha256-QKprfszbWqegvIJ2J+f3gxLpkpZgfuLP5HjoMwyCi5M=";
 
   nativeBuildInputs = [ installShellFiles ];
 
