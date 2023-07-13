34e9cd5825ccd329adc70825947ec6280ef8cbcc
Robert Schütz <nix@dotlambda.de>
2023-07-12 21:04:34 -0700

    irrd: mark insecure

--- pkgs/servers/irrd/default.nix
+++ pkgs/servers/irrd/default.nix
@@ -33,6 +33,66 @@ let
           ];
         });
 
+        hiredis = super.hiredis.overridePythonAttrs (old: {
+          meta = old.meta // {
+            knownVulnerabilities = [
+              "CVE-2021-32765"
+            ];
+          };
+        });
+
+        pydantic = super.pydantic.overridePythonAttrs (old: {
+          meta = old.meta // {
+            knownVulnerabilities = [
+              "CVE-2020-10735"
+            ];
+          };
+        });
+
+        redis = super.redis.overridePythonAttrs (old: {
+          meta = old.meta // {
+            knownVulnerabilities = [
+              "CVE-2023-28858"
+              "CVE-2023-28859"
+            ];
+          };
+        });
+
+        requests = super.requests.overridePythonAttrs (old: {
+          meta = old.meta // {
+            knownVulnerabilities = [
+              "CVE-2023-32681"
+            ];
+          };
+        });
+
+        sqlalchemy = super.sqlalchemy.overridePythonAttrs (old: {
+          meta = old.meta // {
+            knownVulnerabilities = [
+              "PVE-2022-51668"
+            ];
+          };
+        });
+
+        starlette = super.starlette.overridePythonAttrs (old: {
+          meta = old.meta // {
+            knownVulnerabilities = [
+              "CVE-2023-29159"
+              "CVE-2023-30798"
+            ];
+          };
+        });
+
+        ujson = super.ujson.overridePythonAttrs (old: {
+          meta = old.meta // {
+            knownVulnerabilities = [
+              "CVE-2021-45958"
+              "CVE-2022-31116"
+              "CVE-2022-31117"
+            ];
+          };
+        });
+
       })
     ];
   }).python.pkgs;
