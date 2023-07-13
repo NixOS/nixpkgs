9154d24a2ef605474ca70b11006f4755d614f4bc
figsoda <figsoda@pm.me>
2023-07-13 09:56:36 -0400

    cargo-temp: 0.2.16 -> 0.2.17
    Diff: https://github.com/yozhgoor/cargo-temp/compare/v0.2.16...v0.2.17
    
    Changelog: https://github.com/yozhgoor/cargo-temp/releases/tag/v0.2.17

--- pkgs/development/tools/rust/cargo-temp/default.nix
+++ pkgs/development/tools/rust/cargo-temp/default.nix
@@ -2,20 +2,21 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-temp";
-  version = "0.2.16";
+  version = "0.2.17";
 
   src = fetchFromGitHub {
     owner = "yozhgoor";
-    repo = pname;
+    repo = "cargo-temp";
     rev = "v${version}";
-    sha256 = "sha256-9rFczpVboJ+TPQzuegFj8RGYBel+4n5iY4B0sruK5wc=";
+    hash = "sha256-nDUtmCbvWK6bdcKvlx3KYMy9w8IOLL6jMWOafX704eQ=";
   };
 
-  cargoSha256 = "sha256-uIgDs7dFJjZgOE/y3T11N3zl8AwRvIyJbIC7wD7Nr7Q=";
+  cargoHash = "sha256-DxeZGQqi681JoCZo0Iq8+TgyF7a8No9Crm4NypUeDDA=";
 
   meta = with lib; {
     description = "A CLI tool that allow you to create a temporary new Rust project using cargo with already installed dependencies";
     homepage = "https://github.com/yozhgoor/cargo-temp";
+    changelog = "https://github.com/yozhgoor/cargo-temp/releases/tag/${src.rev}";
     license = with licenses; [ mit /* or */ asl20 ];
     maintainers = with maintainers; [ figsoda ];
   };
