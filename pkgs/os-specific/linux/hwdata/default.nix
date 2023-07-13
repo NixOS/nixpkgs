68b134cdfc3561336e88020982e9ed1cbb239a5f
PedroHLC ☭ <root@pedrohlc.com>
2023-07-13 13:15:40 -0300

    hwdata: 0.371 -> 0.372

--- pkgs/os-specific/linux/hwdata/default.nix
+++ pkgs/os-specific/linux/hwdata/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "hwdata";
-  version = "0.371";
+  version = "0.372";
 
   src = fetchFromGitHub {
     owner = "vcrhonek";
     repo = "hwdata";
     rev = "v${version}";
-    sha256 = "sha256-bK61nvuzm8LTotVSBtGyBMELZPqoENkPM4NKtgEx9qw=";
+    hash = "sha256-XC0U5UsOjTveRj1b0e1TBlYv/tKebSOu/YEGt/rmAHw=";
   };
 
   postPatch = ''
