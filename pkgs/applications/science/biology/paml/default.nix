d2c506948b538b6c0b178e6db59aad47a747f831
natsukium <tomoya.otabi@gmail.com>
2023-07-14 11:06:19 +0900

    paml: refactor

--- pkgs/applications/science/biology/paml/default.nix
+++ pkgs/applications/science/biology/paml/default.nix
@@ -1,13 +1,16 @@
-{lib, stdenv, fetchFromGitHub}:
+{ lib
+, stdenv
+, fetchFromGitHub
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "paml";
   version = "4.10.7";
 
   src = fetchFromGitHub {
     owner = "abacus-gene";
     repo = "paml";
-    rev = "refs/tags/${version}";
+    rev = "refs/tags/${finalAttrs.version}";
     hash = "sha256-P/oHaLxoQzjFuvmHyRdShHv1ayruy6O/I9w8aTyya2s=";
   };
 
@@ -18,6 +21,8 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -pv $out/bin
     cp -v codeml $out/bin
     cp -v baseml $out/bin
@@ -28,6 +33,8 @@ stdenv.mkDerivation rec {
     cp -v mcmctree $out/bin
     cp -v pamp $out/bin
     cp -v yn00 $out/bin
+
+    runHook postInstall
   '';
 
   meta = {
@@ -35,5 +42,7 @@ stdenv.mkDerivation rec {
     longDescription = "PAML is a package of programs for phylogenetic analyses of DNA or protein sequences using maximum likelihood. It is maintained and distributed for academic use free of charge by Ziheng Yang. ANSI C source codes are distributed for UNIX/Linux/Mac OSX, and executables are provided for MS Windows. PAML is not good for tree making. It may be used to estimate parameters and test hypotheses to study the evolutionary process, when you have reconstructed trees using other programs such as PAUP*, PHYLIP, MOLPHY, PhyML, RaxML, etc.";
     license     = lib.licenses.gpl3Only;
     homepage    = "http://abacus.gene.ucl.ac.uk/software/paml.html";
+    changelog = "https://github.com/abacus-gene/paml/releases/tag/${finalAttrs.src.rev}";
+    platforms = lib.platforms.unix;
   };
-}
+})
