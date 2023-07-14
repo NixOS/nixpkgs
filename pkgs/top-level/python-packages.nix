bb81d55a813b0a8e23f2fb3f456c945cef694af0
thyol <thyol@pm.me>
2023-07-14 22:22:26 +0200

    python30Packages.pyrodigal: init at 2.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7772,6 +7772,8 @@ self: super: with self; {
 
   pyproject-hooks = callPackage ../development/python-modules/pyproject-hooks { };
 
+  pyrodigal = callPackage ../development/python-modules/pyrodigal { };
+
   pypsrp = callPackage ../development/python-modules/pypsrp { };
 
   phpserialize = callPackage ../development/python-modules/phpserialize { };
