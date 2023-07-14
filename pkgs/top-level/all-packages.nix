6d157073cf3caebeeeb0964749cfee085c8d655e
Robert Schütz <nix@dotlambda.de>
2023-07-14 08:05:06 -0700

    piston-cli: use Python 3.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34173,7 +34173,7 @@ with pkgs;
 
   pistol = callPackage ../tools/misc/pistol { };
 
-  piston-cli = callPackage ../tools/misc/piston-cli { python3Packages = python39Packages; };
+  piston-cli = callPackage ../tools/misc/piston-cli { };
 
   pizarra = callPackage ../applications/graphics/pizarra { };
 
