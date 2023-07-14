af40e90a49b6620b65a8159d20c103d58769fd92
Samuel Ainsworth <sam@jurabio.com>
2023-07-14 01:53:55 -0400

    python3Packages.mhcgnomes: init at 1.8.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6367,6 +6367,8 @@ self: super: with self; {
 
   mf2py = callPackage ../development/python-modules/mf2py { };
 
+  mhcgnomes = callPackage ../development/python-modules/mhcgnomes { };
+
   micawber = callPackage ../development/python-modules/micawber { };
 
   microdata = callPackage ../development/python-modules/microdata { };
