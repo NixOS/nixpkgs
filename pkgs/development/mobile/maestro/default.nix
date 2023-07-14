23ee064d832b707251d2a73c57fdc14541db36ad
SubhrajyotiSen <subhrajyoti12@gmail.com>
2023-07-14 10:58:19 +0530

    maestro: 1.29.0 -> 1.30.0

--- pkgs/development/mobile/maestro/default.nix
+++ pkgs/development/mobile/maestro/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "maestro";
-  version = "1.29.0";
+  version = "1.30.0";
 
   src = fetchurl {
     url = "https://github.com/mobile-dev-inc/maestro/releases/download/cli-${version}/maestro.zip";
-    sha256 = "1rxpz3sa9v9z44006vxqbp0iqq7lyq0h663hdgzjq8vf0wky2gf7";
+    sha256 = "1z9mrkxwfkp07z1k3kdgsda4dpb3hl3wa8mn0vwy3avh0k3my0px";
   };
 
   dontUnpack = true;
