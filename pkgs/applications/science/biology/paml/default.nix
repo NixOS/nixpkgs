f6e2b681222f4b06ca45f7299890e89f3c3ee465
natsukium <tomoya.otabi@gmail.com>
2023-07-14 11:02:10 +0900

    paml: 4.9j -> 4.10.7
    Diff: https://github.com/abacus-gene/paml/compare/d59d538939422a31d8a0f23822c1ddb072409bfd...4.10.7
    Changelog: https://github.com/abacus-gene/paml/releases/tag/4.10.7

--- pkgs/applications/science/biology/paml/default.nix
+++ pkgs/applications/science/biology/paml/default.nix
@@ -1,22 +1,22 @@
-{stdenv, fetchurl}:
+{lib, stdenv, fetchFromGitHub}:
 
 stdenv.mkDerivation rec {
-  version = "4.9j";
   pname = "paml";
-  src = fetchurl {
-    url = "http://abacus.gene.ucl.ac.uk/software/paml${version}.tgz";
-    sha256 = "0qflf3i27x6jwks3c6q560m1q8r043ja96syah145113iz5wdalp";
+  version = "4.10.7";
+
+  src = fetchFromGitHub {
+    owner = "abacus-gene";
+    repo = "paml";
+    rev = "refs/tags/${version}";
+    hash = "sha256-P/oHaLxoQzjFuvmHyRdShHv1ayruy6O/I9w8aTyya2s=";
   };
 
-  # Workaround build failure on -fno-common toolchains like upstream
-  # gcc-10. Otherwise build fails as:
-  #   ld: /build/ccKomtcd.o:(.bss+0x4544): multiple definition of `SeqTypes';
-  #     /build/ccx7EsgU.o:(.bss+0x2a0dfdc): first defined here
-  env.NIX_CFLAGS_COMPILE = "-fcommon";
+  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin "-D_POSIX_C_SOURCE";
 
   preBuild = ''
     cd ./src/
   '';
+
   installPhase = ''
     mkdir -pv $out/bin
     cp -v codeml $out/bin
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
   meta = {
     description     = "Phylogenetic Analysis by Maximum Likelihood (PAML)";
     longDescription = "PAML is a package of programs for phylogenetic analyses of DNA or protein sequences using maximum likelihood. It is maintained and distributed for academic use free of charge by Ziheng Yang. ANSI C source codes are distributed for UNIX/Linux/Mac OSX, and executables are provided for MS Windows. PAML is not good for tree making. It may be used to estimate parameters and test hypotheses to study the evolutionary process, when you have reconstructed trees using other programs such as PAUP*, PHYLIP, MOLPHY, PhyML, RaxML, etc.";
-    license     = "non-commercial";
+    license     = lib.licenses.gpl3Only;
     homepage    = "http://abacus.gene.ucl.ac.uk/software/paml.html";
   };
 }
