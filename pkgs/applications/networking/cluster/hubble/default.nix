1c807356b6c603887be693b4afdbe1e85bef8e27
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-07-15 09:51:53 +0200

    hubble: unbreak on aarch64-linux

--- pkgs/applications/networking/cluster/hubble/default.nix
+++ pkgs/applications/networking/cluster/hubble/default.nix
@@ -39,7 +39,6 @@ buildGoModule rec {
   '';
 
   meta = with lib; {
-    broken = (stdenv.isLinux && stdenv.isAarch64);
     description = "Network, Service & Security Observability for Kubernetes using eBPF";
     license = licenses.asl20;
     homepage = "https://github.com/cilium/hubble/";
