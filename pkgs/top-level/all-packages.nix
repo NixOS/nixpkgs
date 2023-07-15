3b02e8b41da437167df9bc3f8c809df277dd1b6a
Anderson Torres <torres.anderson.85@protonmail.com>
2023-07-15 09:06:37 -0300

    rivercarro: use zigHook
    Also, a cosmetic refactor:
    
    - Reorder parameter listing
    - Use rec-less, overlay-style overridable recursive attributes (in effect since
    NixOS#119942);
    - Remove nested with (according to
    https://nix.dev/recipes/best-practices#with-scopes)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6112,7 +6112,9 @@ with pkgs;
   river = callPackage ../applications/window-managers/river { };
 
   rivercarro = callPackage ../applications/misc/rivercarro {
-    zig = buildPackages.zig_0_9;
+    zigHook = zigHook.override {
+      zig = buildPackages.zig_0_9;
+    };
   };
 
   river-luatile = callPackage ../applications/misc/river-luatile{ };
