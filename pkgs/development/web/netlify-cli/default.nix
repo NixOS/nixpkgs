3f4ee2b16146f289e6b4a6b7f6ce991ccae1770d
Mario Rodas <marsam@users.noreply.github.com>
2023-07-15 04:20:00 +0000

    netlify-cli: use buildNpmPackage

--- pkgs/development/web/netlify-cli/default.nix
+++ pkgs/development/web/netlify-cli/default.nix
@@ -1,18 +1,41 @@
-{ callPackage, fetchFromGitHub, lib, pkgs }:
-let
-  nodePackages = import ./composition.nix { inherit pkgs; };
-  sourceInfo = (lib.importJSON ./netlify-cli.json);
-in
-  nodePackages.package.override {
-    preRebuild = ''
-      export ESBUILD_BINARY_PATH="${pkgs.esbuild_netlify}/bin/esbuild"
-    '';
-    src = fetchFromGitHub {
-      inherit (sourceInfo) owner repo rev hash;
-    };
-    bypassCache = true;
-    reconstructLock = true;
-    passthru.tests.test = callPackage ./test.nix { };
-    meta.maintainers = with lib.maintainers; [ roberth ];
-    meta.mainProgram = "netlify";
-  }
+{ lib, buildNpmPackage, callPackage, esbuild_netlify, fetchFromGitHub, python3 }:
+
+buildNpmPackage rec {
+  pname = "netlify-cli";
+  version = "15.9.0";
+
+  src = fetchFromGitHub {
+    owner = "netlify";
+    repo = "cli";
+    rev = "v${version}";
+    hash = "sha256-cfQGzsZipp0HDPqVuW8QjGo23CckmOhD79r34GaY8eY=";
+  };
+
+  postPatch = ''
+    substituteInPlace package.json --replace 'is-ci ||' 'true ||'
+  '';
+
+  npmDepsHash = "sha256-cs37SFom2fR9yuGNmyZ5QATiE2P/WiBNyqF+54VdGDQ=";
+
+  nativeBuildInputs = [
+    python3
+  ];
+
+  npmInstallFlagsArray = [
+    "--omit=dev"
+  ];
+
+  env.ESBUILD_BINARY_PATH = "${esbuild_netlify}/bin/esbuild";
+
+  dontNpmBuild = true;
+
+  passthru.tests.test = callPackage ./test.nix { };
+
+  meta = with lib; {
+    description = "Netlify Command Line Interface";
+    homepage = "https://github.com/netlify/cli";
+    changelog = "https://github.com/netlify/cli/blob/v${version}/CHANGELOG.md";
+    license = licenses.mit;
+    maintainers = [ ];
+  };
+}
