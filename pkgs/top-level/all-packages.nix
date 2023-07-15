397695b1eea9bb1a58e115cba33f19a607457afb
Jan Tojnar <jtojnar@gmail.com>
2023-07-15 15:45:03 +0200

    flatpak-external-data-checker: init at 0-unstable-2023-04-27

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7746,6 +7746,8 @@ with pkgs;
     binutils = binutils-unwrapped;
   };
 
+  flatpak-external-data-checker = callPackage ../development/tools/flatpak-external-data-checker { };
+
   fltrdr = callPackage ../tools/misc/fltrdr {
     icu = icu63;
   };
