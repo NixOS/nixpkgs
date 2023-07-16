5d8252e51fc351a1509f2f97d0a5fb51f41c5881
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-07-16 20:32:28 +0800

    ictree: init at 1.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5422,6 +5422,8 @@ with pkgs;
 
   icdiff = callPackage ../tools/text/icdiff { };
 
+  ictree = callPackage ../tools/misc/ictree { };
+
   inchi = callPackage ../development/libraries/inchi { };
 
   icon-slicer = callPackage ../tools/X11/icon-slicer { };
