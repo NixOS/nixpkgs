7ab6461d80a93bdc4d252aed199613849a4ad4b3
sheepforce <phillip.seeber@googlemail.com>
2023-07-17 00:10:44 +0200

    indilib: 1.9.8 -> 2.0.2

--- pkgs/development/libraries/science/astronomy/indilib/default.nix
+++ pkgs/development/libraries/science/astronomy/indilib/default.nix
@@ -12,17 +12,18 @@
 , libjpeg
 , gsl
 , fftw
+, gtest
 }:
 
 stdenv.mkDerivation rec {
   pname = "indilib";
-  version = "1.9.8";
+  version = "2.0.2";
 
   src = fetchFromGitHub {
     owner = "indilib";
     repo = "indi";
     rev = "v${version}";
-    sha256 = "sha256-+KFuZgM/Bl6Oezq3WXjWCHefc1wvR3wOKXejmT0pw1U=";
+    hash = "sha256-GoEvWzGT3Ckv9Syif6Z2kAlnvg/Kt5I8SpGFG9kFTJo=";
   };
 
   nativeBuildInputs = [
@@ -45,14 +46,24 @@ stdenv.mkDerivation rec {
   cmakeFlags = [
     "-DCMAKE_INSTALL_LIBDIR=lib"
     "-DUDEVRULES_INSTALL_DIR=lib/udev/rules.d"
+  ] ++ lib.optional doCheck [
+    "-DINDI_BUILD_UNITTESTS=ON"
+    "-DINDI_BUILD_INTEGTESTS=ON"
   ];
+
+  checkInputs = [ gtest ];
+
+  doCheck = true;
+
+  # Socket address collisions between tests
+  enableParallelChecking = false;
 
   meta = with lib; {
     homepage = "https://www.indilib.org/";
     description = "Implementation of the INDI protocol for POSIX operating systems";
     changelog = "https://github.com/indilib/indi/releases/tag/v${version}";
     license = licenses.lgpl2Plus;
-    maintainers = with maintainers; [ hjones2199 ];
+    maintainers = with maintainers; [ hjones2199 sheepforce ];
     platforms = platforms.unix;
   };
 }
