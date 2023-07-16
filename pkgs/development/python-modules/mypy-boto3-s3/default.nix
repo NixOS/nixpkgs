bfd49bde1277bb47246b16efcb54362c44ce6b19
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-16 11:11:28 +0000

    python310Packages.mypy-boto3-s3: 1.28.3 -> 1.28.3.post2

--- pkgs/development/python-modules/mypy-boto3-s3/default.nix
+++ pkgs/development/python-modules/mypy-boto3-s3/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "mypy-boto3-s3";
-  version = "1.28.3";
+  version = "1.28.3.post2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-wrRUQTEmB5pkNKPgWnXLTK2UfHYk5TujhGy4KaIjDEs=";
+    hash = "sha256-wjt4ArgKA4ihRtfoAlVS8h1E40kCahj7dR2caY7XFLE=";
   };
 
   propagatedBuildInputs = [
