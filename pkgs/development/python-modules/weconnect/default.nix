ffae070a13afb7f6295efc0a23e0e8bef204e364
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-16 21:26:05 +0000

    python310Packages.weconnect: 0.55.0 -> 0.56.2

--- pkgs/development/python-modules/weconnect/default.nix
+++ pkgs/development/python-modules/weconnect/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "weconnect";
-  version = "0.55.0";
+  version = "0.56.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "tillsteinbach";
     repo = "WeConnect-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-+ISWPrpY/urpZZZrn6+Ii8gbrwkQMLL6gXhydXd8HqI=";
+    hash = "sha256-Z6QIEFITMjmB3JbbcfGC5JMJtAz5/3F21TRgao5lBs0=";
   };
 
   propagatedBuildInputs = [
