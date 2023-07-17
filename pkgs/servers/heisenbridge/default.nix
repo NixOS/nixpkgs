28cf9328b0ed8f319b442a93abc479fad752d6b4
Nick Cao <nickcao@nichi.co>
2023-07-17 16:39:00 +0800

    heisenbridge: 1.14.2 -> 1.14.3
    Diff: https://github.com/hifi/heisenbridge/compare/refs/tags/v1.14.2...v1.14.3

--- pkgs/servers/heisenbridge/default.nix
+++ pkgs/servers/heisenbridge/default.nix
@@ -1,43 +1,28 @@
-{ lib, fetchFromGitHub, fetchpatch, python3 }:
+{ lib, fetchFromGitHub, python3 }:
 
-let
-  python = python3.override {
-    packageOverrides = self: super: {
-      mautrix = super.mautrix.overridePythonAttrs (oldAttrs: rec {
-        version = "0.16.10";
-        src = fetchFromGitHub {
-          owner = "mautrix";
-          repo = "python";
-          rev = "v${version}";
-          hash = "sha256-YQsQ7M+mHcRdGUZp+mo46AlBmKSdmlgRdGieEG0Hu9k=";
-        };
-      });
-    };
-  };
-in
-python.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication rec {
   pname = "heisenbridge";
-  version = "1.14.2";
+  version = "1.14.3";
 
   src = fetchFromGitHub {
     owner = "hifi";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-qp0LVcmWf5lZ52h0V58S6FoIM8RLOd6Y3FRb85j7KRg=";
+    sha256 = "sha256-IKvB3L5xgAGLkN67rw2dp4Nvv0w4XbeXMcMmY7SGeNU=";
   };
 
   postPatch = ''
     echo "${version}" > heisenbridge/version.txt
   '';
 
-  propagatedBuildInputs = with python.pkgs; [
+  propagatedBuildInputs = with python3.pkgs; [
     irc
     ruamel-yaml
     mautrix
     python-socks
   ];
 
-  nativeCheckInputs = with python.pkgs; [
+  nativeCheckInputs = with python3.pkgs; [
     pytestCheckHook
   ];
 
