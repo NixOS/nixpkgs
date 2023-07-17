1733d8032aa620b7b6eae67cc8aa53747c7b905c
Vladimír Čunát <v@cunat.cz>
2023-07-17 14:57:16 +0200

    Revert Merge #240480: python310Packages.pluggy: 1.0.0 -> 1.2.0
    This reverts commit 99aa76c2e2324503a2d62a3c3f489dcaa53d564c, reversing
    changes made to c4a0fc39484f68ff9a3b6b90cb3899938ede00a4.
    
    Broke python3Packages.virtualenv; it would become a channel blocker:
    https://hydra.nixos.org/build/228038998/nixlog/307/tail

--- pkgs/development/python-modules/pluggy/default.nix
+++ pkgs/development/python-modules/pluggy/default.nix
@@ -1,27 +1,22 @@
 { buildPythonPackage
 , lib
-, fetchFromGitHub
+, fetchPypi
 , setuptools-scm
 , pythonOlder
 , importlib-metadata
-, callPackage
 }:
 
 buildPythonPackage rec {
   pname = "pluggy";
-  version = "1.2.0";
+  version = "1.0.0";
   format = "pyproject";
 
-  src = fetchFromGitHub {
-    owner = "pytest-dev";
-    repo = "pluggy";
-    rev = "refs/tags/${version}";
-    hash = "sha256-SzJu7ITdmUgusn8sz6fRBpxTMQncWIViP5NCAj4q4GM=";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "4224373bacce55f955a878bf9cfa763c1e360858e330072059e10bad68531159";
   };
 
   nativeBuildInputs = [ setuptools-scm ];
-
-  env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
 
   propagatedBuildInputs = lib.optionals (pythonOlder "3.8") [
     importlib-metadata
@@ -29,12 +24,8 @@ buildPythonPackage rec {
 
   # To prevent infinite recursion with pytest
   doCheck = false;
-  passthru.tests = {
-    pytest = callPackage ./tests.nix { };
-  };
 
   meta = {
-    changelog = "https://github.com/pytest-dev/pluggy/blob/${src.rev}/CHANGELOG.rst";
     description = "Plugin and hook calling mechanisms for Python";
     homepage = "https://github.com/pytest-dev/pluggy";
     license = lib.licenses.mit;
