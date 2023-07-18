c75eb7033642e836e4801966a17b068a577c8129
Mario Rodas <marsam@users.noreply.github.com>
2023-07-18 04:20:00 +0000

    rbw: 1.7.1 -> 1.8.1
    Changelog: https://git.tozt.net/rbw/plain/CHANGELOG.md?id=1.8.1

--- pkgs/tools/security/rbw/default.nix
+++ pkgs/tools/security/rbw/default.nix
@@ -5,7 +5,7 @@
 , openssl
 , pkg-config
 , installShellFiles
-, Security
+, darwin
 
   # rbw-fzf
 , withFzf ? false
@@ -24,20 +24,23 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rbw";
-  version = "1.7.1";
+  version = "1.8.1";
 
   src = fetchzip {
     url = "https://git.tozt.net/rbw/snapshot/rbw-${version}.tar.gz";
-    sha256 = "sha256-xE3T3iVXFaaTF90ehQiG6+dLXcsqrHeprSMUnlSsxkE=";
+    sha256 = "sha256-cRfCsuhVlTRSgndf4rZrfLG/+NgI4VshVwbQydJt2cE=";
   };
 
-  cargoHash = "sha256-eaG56FGz4smlqDPi/CJ0KB7NMEgp684X19PVWxGQutw=";
+  cargoHash = "sha256-CFhBSRqWAJJJkeMNnLwp6/pLhIZHFvdlPn7pjfJFBWw=";
 
   nativeBuildInputs = [
     installShellFiles
   ] ++ lib.optionals stdenv.isLinux [ pkg-config ];
 
-  buildInputs = lib.optionals stdenv.isDarwin [ Security ];
+  buildInputs = lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Security
+    darwin.apple_sdk.frameworks.AppKit
+  ];
 
   preConfigure = lib.optionalString stdenv.isLinux ''
     export OPENSSL_INCLUDE_DIR="${openssl.dev}/include"
