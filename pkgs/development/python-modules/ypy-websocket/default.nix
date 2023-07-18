642dac2e5e01ee1213f846eb6f203b5f31eb548d
Robert Schütz <nix@dotlambda.de>
2023-07-18 10:25:14 -0700

    python310Packages.ypy-websocket: 0.9.0 -> 0.12.1
    Diff: https://github.com/y-crdt/ypy-websocket/compare/refs/tags/v0.9.0...v0.12.1
    
    Changelog: https://github.com/y-crdt/ypy-websocket/blob/refs/tags/v0.12.1/CHANGELOG.md

--- pkgs/development/python-modules/ypy-websocket/default.nix
+++ pkgs/development/python-modules/ypy-websocket/default.nix
@@ -9,12 +9,13 @@
 , pytest-asyncio
 , pytestCheckHook
 , pythonRelaxDepsHook
+, uvicorn
 , websockets
 }:
 
 buildPythonPackage rec {
   pname = "ypy-websocket";
-  version = "0.9.0";
+  version = "0.12.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -23,7 +24,7 @@ buildPythonPackage rec {
     owner = "y-crdt";
     repo = "ypy-websocket";
     rev = "refs/tags/v${version}";
-    hash = "sha256-dHcXJavzR1BDAM9Xjm144gfNMSDEkenumI3POBfCjvQ=";
+    hash = "sha256-JsSOh7CSHUnGJmNAP87fMMsRgdj6nNna1XVe15MYqoA=";
   };
 
   pythonRelaxDeps = [
@@ -48,6 +49,7 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     pytest-asyncio
     pytestCheckHook
+    uvicorn
     websockets
   ];
 
