581d7c88bea4216fb47c2a98f38af59f2d14485d
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-07-18 17:20:28 +0200

    lib/tests: Unify documentation of individual testable files

--- lib/path/tests/prop.sh
+++ lib/path/tests/prop.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# Property tests for the `lib.path` library
-#
+# Property tests for lib/path/default.nix
 # It generates random path-like strings and runs the functions on
 # them, checking that the expected laws of the functions hold
+# Run:
+# [nixpkgs]$ lib/path/tests/prop.sh
+# or:
+# [nixpkgs]$ nix-build lib/tests/release.nix
 
 set -euo pipefail
 shopt -s inherit_errexit
