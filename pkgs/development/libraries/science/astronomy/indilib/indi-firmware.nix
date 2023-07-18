432849543039f80e73a8e96bbb369e3068cb3308
sheepforce <phillip.seeber@googlemail.com>
2023-07-18 14:46:13 +0200

    indi-full: 1.9.8 -> 2.0.2, reenable Atik, Pentax and SX drivers

--- pkgs/development/libraries/science/astronomy/indilib/indi-firmware.nix
+++ pkgs/development/libraries/science/astronomy/indilib/indi-firmware.nix
@@ -19,6 +19,7 @@
 , ffmpeg
 , version
 , src
+, autoPatchelfHook
 }:
 
 stdenv.mkDerivation rec {
@@ -26,7 +27,7 @@ stdenv.mkDerivation rec {
 
   inherit version src;
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake autoPatchelfHook ];
 
   buildInputs = [
     indilib libnova curl cfitsio libusb1 zlib boost gsl gpsd
@@ -60,7 +61,7 @@ stdenv.mkDerivation rec {
     description = "Third party firmware for the INDI astronomical software suite";
     changelog = "https://github.com/indilib/indi-3rdparty/releases/tag/v${version}";
     license = licenses.lgpl2Plus;
-    maintainers = with maintainers; [ hjones2199 ];
+    maintainers = with maintainers; [ hjones2199 sheepforce ];
     platforms = platforms.linux;
   };
 }
