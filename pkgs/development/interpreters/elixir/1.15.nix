853be7c712ac4a8b747ea590c54760422647c949
meppu <meppu@proton.me>
2023-07-18 19:09:10 +0300

    elixir_1_15: 1.15.2 -> 1.15.4

--- pkgs/development/interpreters/elixir/1.15.nix
+++ pkgs/development/interpreters/elixir/1.15.nix
@@ -1,7 +1,7 @@
 { mkDerivation }:
 mkDerivation {
-  version = "1.15.2";
-  sha256 = "sha256-JLDjLO78p1i3FqGCbgl22SZFGPxJxKGKskzAJhHV8NE=";
+  version = "1.15.4";
+  sha256 = "sha256-0DrfKQPyFX+zurCIZ6RVj9vm1lHSkJSfhiUaRpa3FFo=";
   # https://hexdocs.pm/elixir/1.15.0/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
   minimumOTPVersion = "24";
   escriptPath = "lib/elixir/scripts/generate_app.escript";
