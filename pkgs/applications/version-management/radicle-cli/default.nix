93c75661b79a056b27db5421238176ba8c622579
Luz <luz666@daum.net>
2023-07-19 23:32:25 +0200

    radicle-cli: 0.6.1 -> 0.8.0-unstable-2023-10-11

--- pkgs/applications/version-management/radicle-cli/default.nix
+++ pkgs/applications/version-management/radicle-cli/default.nix
@@ -2,60 +2,55 @@
 , stdenv
 , fetchFromGitHub
 , rustPlatform
-, pkg-config
-, cmake
 , installShellFiles
 , asciidoctor
 , DarwinTools
-, openssl
-, libusb1
 , AppKit
 , git
-, openssh
 , testers
 , radicle-cli
+, tree
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "radicle-cli";
-  version = "0.6.1";
+  version = "0.8.0-unstable-2023-10-11";
 
   src = fetchFromGitHub {
     owner = "radicle-dev";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-LS6zYpMg0LanRL2M8ioGG8Ys07TPT/3hP7geEGehwxg=";
+    repo = "heartwood";
+    rev = "c3e11057ad933aeef27ffe091be2048557cad469";
+    hash = "sha256-ZuNtxy5k2TBDjn/aQckRvk5JmK4eUF6bFCzf2PgnFUs=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "automerge-0.0.2" = "sha256-MZ1/rca8ZsEUhd3bhd502PHlBbvqAOtnWFEdp7XWmYE=";
-      "automerge-0.1.0" = "sha256-dwbmx3W13oZ1O0Uw3/D5Z0ht1BO1PmVVoWc/tLCm0/4=";
-      "cob-0.1.0" = "sha256-ewPJEx7OSr8X6e5QJ4dh2SbzZ2TDa8G4zBR5euBbABo=";
-      "libusb1-sys-0.6.2" = "sha256-577ld1xqJkHp2bqALNq5IuZivD8y+VO8vNy9Y+hfq6c=";
-      "walletconnect-0.1.0" = "sha256-fdgdhotTYBmWbR4r0OMplOwhYq1C7jkuOdhKASjH+Fs=";
-    };
-  };
+  patches = [
+    ./disable-failing-tests.patch
+  ];
+  cargoHash = "sha256-Sq9aIaUZBXPXqYLwSIJyrNtLDWS5C1Uyc28ax3YZCAE=";
 
-  # Otherwise, there are errors due to the `abigen` macro from `ethers`.
-  auditable = false;
-
-  nativeBuildInputs = [
-    pkg-config
-    cmake
-    installShellFiles
-    asciidoctor
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
+  nativeBuildInputs = [ asciidoctor installShellFiles ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
     DarwinTools
   ];
 
-  buildInputs = [
-    openssl
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    libusb1
+  buildInputs = [ ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
     AppKit
   ];
+
+  checkType = "debug";
+  nativeCheckInputs = [ git tree ];
+
+  # Targets built: rad radicle-node git-remote-rad
+  # This cargoBuildFlag ensures more binaries are generated, e.g.
+  # rad-agent, rad-cli-demo, radicle-httpd, radicle-tui, rad-init, rad-push, rad-self, rad-set-canonical-refs
+  cargoBuildFlags = ["--workspace"];
+
+  # Check if the binary reports the same version:
+  passthru.tests = {
+    version = testers.testVersion {
+      package = radicle-cli;
+      version = lib.head (lib.splitString "-" version);
+    };
+  };
 
   postInstall = ''
     for f in $(find . -name '*.adoc'); do
@@ -65,23 +60,11 @@ rustPlatform.buildRustPackage rec {
     done
   '';
 
-  nativeCheckInputs = [
-    git
-    openssh
-  ];
-  preCheck = ''
-    eval $(ssh-agent)
-  '';
-
-  passthru.tests = {
-    version = testers.testVersion { package = radicle-cli; };
-  };
-
   meta = {
     description = "Command-line tooling for Radicle, a decentralized code collaboration network";
     homepage = "https://radicle.xyz";
-    license = lib.licenses.gpl3Plus;
-    maintainers = with lib.maintainers; [ amesgen ];
+    license = [ lib.licenses.mit lib.licenses.asl20 ];
+    maintainers = with lib.maintainers; [ amesgen luz ];
     platforms = lib.platforms.unix;
     mainProgram = "rad";
   };
