890fbb83327f0bbb1e485c0ec57bebb21a580cc3
natsukium <tomoya.otabi@gmail.com>
2023-07-20 02:12:26 +0900

    python310Packages.nlpcloud: 1.0.43 -> 1.1.43
    Changelog: https://github.com/nlpcloud/nlpcloud-python/releases/tag/v1.1.43

--- pkgs/development/python-modules/nlpcloud/default.nix
+++ pkgs/development/python-modules/nlpcloud/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "nlpcloud";
-  version = "1.0.43";
+  version = "1.1.43";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-WLbmPFBiZ7utFo0cqiBKsetlhtgun/YMGTEIvMUhRnc=";
+    hash = "sha256-y3OZ5Tgd9FJmuon+9UyFmJgoASd1UyZVsWxmlPaxqEI=";
   };
 
   propagatedBuildInputs = [
