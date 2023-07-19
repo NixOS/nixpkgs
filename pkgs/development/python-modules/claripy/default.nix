2a5b15da30c6155dc914cdfd4edf43a604ecb6d6
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-19 10:40:37 +0200

    python311Packages.claripy: 9.2.59 -> 9.2.60
    Diff: https://github.com/angr/claripy/compare/refs/tags/v9.2.59...v9.2.60

--- pkgs/development/python-modules/claripy/default.nix
+++ pkgs/development/python-modules/claripy/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "claripy";
-  version = "9.2.59";
+  version = "9.2.60";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-QVbLliWVY8Si1dcIhCroYH+uF6nxrTKFsRmxP62AfPI=";
+    hash = "sha256-UpNnvHlsZkDltIDJAl8m1wx79MByRpBjUneiTW0PZr8=";
   };
 
   nativeBuildInputs = [
