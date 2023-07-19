e881a9cb1a9c172ee1a83268ca31072e83cfa814
Raito Bezarius <masterancpp@gmail.com>
2023-07-19 15:07:09 +0200

    linux_testing: 6.4-rc7 -> 6.5-rc2
    `VIDEO_STK1160_COMMON` was removed in
     7f7ac101236bd020681f122089b611eca8e507ac ("media: stk1160: Simplify the build config definition").
    
    This is now guarded for newer kernels.

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.4-rc7";
+  version = "6.5-rc2";
   extraMeta.branch = lib.versions.majorMinor version;
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchzip {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    hash = "sha256-UDhLrKe8yMvmWbS19Xt1G3SQpAUWyKrfV3v1MJ5Vep8=";
+    hash = "sha256-QP7xCXl4tNppEH9Rso64n9GJ/SBGDtjA8d1TghTtKfA=";
   };
 
   # Should the testing kernels ever be built on Hydra?
