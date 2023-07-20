62596733c0c433af06dfc13ee89a98a26ce7b532
Evils <evils.devils@protonmail.com>
2023-07-21 01:25:50 +0200

    screentest: init at unstable-2021-05-10
    unstable over 2.1 to get the solid colour feature

---
+++ pkgs/by-name/sc/screentest/package.nix
@@ -0,0 +1,37 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoreconfHook
+, intltool
+, pkg-config
+, gtk2
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "screentest";
+  version = "unstable-2021-05-10";
+
+  src = fetchFromGitHub {
+    owner = "TobiX";
+    repo = "screentest";
+    rev = "780e6cbbbbd6ba93e246e7747fe593b40c4e2747";
+    hash = "sha256-TJ47c77vQ/aRBJ2uEiFLuAR4dd4CMEo+iAAx0HCFbmA=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    intltool
+    pkg-config
+  ];
+
+  buildInputs = [ gtk2 ];
+
+  meta = with lib; {
+    description = "A simple screen testing tool";
+    homepage = "https://github.com/TobiX/screentest";
+    changelog = "https://github.com/TobiX/screentest/blob/${finalAttrs.src.rev}/NEWS";
+    license = licenses.gpl2Only;
+    maintainers = with maintainers; [ evils ];
+    platforms = platforms.unix;
+  };
+})
