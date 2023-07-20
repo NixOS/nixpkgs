ef2752b8068af39d6be9cc995a2e166cf182ef7d
Falco Peijnenburg <falco.peijnenburg@channable.com>
2023-07-20 21:25:20 +0200

    Remove inactive maintainer lluchs

--- pkgs/applications/networking/browsers/vivaldi/ffmpeg-codecs.nix
+++ pkgs/applications/networking/browsers/vivaldi/ffmpeg-codecs.nix
@@ -26,7 +26,7 @@ stdenv.mkDerivation rec {
     homepage    = "https://ffmpeg.org/";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license     = licenses.lgpl21;
-    maintainers = with maintainers; [ betaboon cawilliamson lluchs fptje ];
+    maintainers = with maintainers; [ betaboon cawilliamson fptje ];
     platforms   = [ "x86_64-linux" ];
   };
 }
