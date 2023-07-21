ce42bdf90543ccd78a56a5157a4f752d842b7636
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-07-21 00:22:47 -0700

    python310Packages.sphinxcontrib-serializinghtml: remove *-nspkg.pth file

--- pkgs/development/python-modules/sphinxcontrib-serializinghtml/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-serializinghtml/default.nix
@@ -1,18 +1,22 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, isPy27
+, setuptoolsLegacyNamespaceHook
 }:
 
 buildPythonPackage rec {
   pname = "sphinxcontrib-serializinghtml";
   version = "1.1.5";
-  disabled = isPy27;
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "aa5f6de5dfdf809ef505c4895e51ef5c9eac17d0f287933eb49ec495280b6952";
+    hash = "sha256-ql9t5d/fgJ71BcSJXlHvXJ6sF9Dyh5M+tJ7ElSgLaVI=";
   };
+
+  nativeBuildInputs = [
+    setuptoolsLegacyNamespaceHook
+  ];
 
   # Check is disabled due to circular dependency of sphinx
   doCheck = false;
