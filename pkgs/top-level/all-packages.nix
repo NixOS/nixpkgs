d217eca58df3e936dcbeaaa8d8a8639a7221c840
figsoda <figsoda@pm.me>
2023-07-21 10:07:27 -0400

    smartcrop: init at unstable-2023-03-16
    https://github.com/muesli/smartcrop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34767,6 +34767,8 @@ with pkgs;
 
   slrn = callPackage ../applications/networking/newsreaders/slrn { };
 
+  smartcrop = callPackage ../tools/graphics/smartcrop { };
+
   sniffnet = callPackage ../applications/networking/sniffnet { };
 
   sniproxy = callPackage ../applications/networking/sniproxy { };
