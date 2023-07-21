f677b440640b7e4f72fb5e0ff8cb84ff10dc2a81
Nick Cao <nickcao@nichi.co>
2023-07-21 18:18:39 +0800

    keycloak.plugins.keycloak-metrics-spi: 3.0.0 -> 4.0.0
    Diff: https://github.com/aerogear/keycloak-metrics-spi/compare/3.0.0...4.0.0

--- pkgs/servers/keycloak/keycloak-metrics-spi/default.nix
+++ pkgs/servers/keycloak/keycloak-metrics-spi/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "keycloak-metrics-spi";
-  version = "3.0.0";
+  version = "4.0.0";
 
   src = fetchurl {
     url = "https://github.com/aerogear/keycloak-metrics-spi/releases/download/${version}/keycloak-metrics-spi-${version}.jar";
-    sha256 = "1r06xjnvd7wxl64jqpms6jnl4csfwigb9n9lxcjpkgh2jzfs43sp";
+    sha256 = "sha256-D0NNBuc9YBiywGoq5hG4dcoiunyG/C2ZX73p4lS5v4s=";
   };
 
   dontUnpack = true;
