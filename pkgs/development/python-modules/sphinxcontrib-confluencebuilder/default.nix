b87aeea3d5b8d0b9eef386219a69928f342b42cb
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-07-20 23:46:19 -0700

    python310Packages.sphinxcontrib-confluencebuilder: 2.0.0 -> 2.1.1

--- pkgs/development/python-modules/sphinxcontrib-confluencebuilder/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-confluencebuilder/default.nix
@@ -1,26 +1,29 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, docutils
+, flit-core
 , sphinx
 , requests
-, jinja2
 }:
 
 buildPythonPackage rec {
   pname = "sphinxcontrib-confluencebuilder";
-  version = "2.0.0";
+  version = "2.1.1";
+  format = "pyproject";
 
   src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-RyShQt/t8tnE5ZHM0WDQ+jrvXem3y24tCpj3s6Thq+g=";
+    pname = "sphinxcontrib_confluencebuilder";
+    inherit version;
+    hash = "sha256-+gan96ZtaPsb5VBH3UFlSVqsHHZVIAEaijGxoQmCdQI=";
   };
 
+  nativeBuildInputs = [
+    flit-core
+  ];
+
   propagatedBuildInputs = [
-    docutils
     sphinx
     requests
-    jinja2
   ];
 
   # Tests are disabled due to a circular dependency on Sphinx
