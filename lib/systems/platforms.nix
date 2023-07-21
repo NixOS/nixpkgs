4058b556aa68f352c1264adba18e166d3c411d3a
Raito Bezarius <masterancpp@gmail.com>
2023-07-21 21:38:47 +0200

    lib/systems/platforms: drop `target` for x86, aarch64-multiplatform and riscv-multiplatform
    To exercise our KBUILD_IMAGE setup.

--- lib/systems/platforms.nix
+++ lib/systems/platforms.nix
@@ -14,7 +14,6 @@ rec {
       baseConfig = "defconfig";
       # Build whatever possible as a module, if not stated in the extra config.
       autoModules = true;
-      target = "bzImage";
     };
   };
 
@@ -27,7 +26,6 @@ rec {
       name = "PowerNV";
 
       baseConfig = "powernv_defconfig";
-      target = "vmlinux";
       autoModules = true;
       # avoid driver/FS trouble arising from unusual page size
       extraConfig = ''
@@ -384,7 +382,6 @@ rec {
         # which our initrd builder can't currently do easily.
         USB_XHCI_TEGRA m
       '';
-      target = "Image";
     };
     gcc = {
       arch = "armv8-a";
@@ -534,7 +531,6 @@ rec {
   riscv-multiplatform = {
     linux-kernel = {
       name = "riscv-multiplatform";
-      target = "Image";
       autoModules = true;
       baseConfig = "defconfig";
       DTB = true;
