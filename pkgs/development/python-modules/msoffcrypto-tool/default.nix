cfe43afc0ab7ca3dc72443cfcc9312954572dcaa
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-21 09:34:19 +0200

    python311Packages.msoffcrypto-tool: update disabled

--- pkgs/development/python-modules/msoffcrypto-tool/default.nix
+++ pkgs/development/python-modules/msoffcrypto-tool/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   version = "5.1.1";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "nolze";
