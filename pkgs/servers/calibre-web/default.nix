9249f60087559789a5ebc72bf4fc41c99122b96b
Vladimír Čunát <v@cunat.cz>
2023-07-21 17:03:19 +0200

    treewide: fixup the embedded sqlalchemy
    I got tired of doing this one commit per package.

--- pkgs/servers/calibre-web/default.nix
+++ pkgs/servers/calibre-web/default.nix
@@ -15,6 +15,10 @@ let
           inherit version;
           hash = "sha256-aRO4JH2KKS74MVFipRkx4rQM6RaB8bbxj2lwRSAMSjA=";
         };
+        disabledTestPaths = [
+           "test/aaa_profiling"
+           "test/ext/mypy"
+        ];
       });
     };
   };
