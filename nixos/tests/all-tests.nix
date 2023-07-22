41c5cb10da17aff167b559979f1e3a3b85dea440
Raito Bezarius <masterancpp@gmail.com>
2023-07-23 01:35:34 +0200

    nixos/tests/boot/uefi-on-legacy: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -133,6 +133,7 @@ in {
   blockbook-frontend = handleTest ./blockbook-frontend.nix {};
   blocky = handleTest ./blocky.nix {};
   boot = handleTestOn ["x86_64-linux" "aarch64-linux"] ./boot.nix {};
+  uefi-on-legacy = handleTestOn [ "x86_64-linux" ] ./boot/uefi-on-legacy.nix { };
   bootspec = handleTestOn ["x86_64-linux"] ./bootspec.nix {};
   boot-stage1 = handleTest ./boot-stage1.nix {};
   borgbackup = handleTest ./borgbackup.nix {};
