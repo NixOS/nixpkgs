208aa1b09626aec182961a9420b3d5731ab70769
Bobby Rong <rjl931189261@126.com>
2023-07-22 15:06:04 +0800

    pantheon.elementary-settings-daemon: 1.2.0 -> 1.3.0
    https://github.com/elementary/settings-daemon/releases/tag/1.3.0

--- pkgs/desktops/pantheon/services/elementary-settings-daemon/default.nix
+++ pkgs/desktops/pantheon/services/elementary-settings-daemon/default.nix
@@ -10,6 +10,7 @@
 , accountsservice
 , dbus
 , desktop-file-utils
+, fwupd
 , geoclue2
 , glib
 , gobject-introspection
@@ -22,13 +23,13 @@
 
 stdenv.mkDerivation rec {
   pname = "elementary-settings-daemon";
-  version = "1.2.0";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = "settings-daemon";
     rev = version;
-    sha256 = "sha256-5QdCj2Z31t7dxZi7ZZ5g6qLgsMyw7rM5dRw0G8uoC6o=";
+    sha256 = "sha256-464caR36oSUhxCU0utP5eMYiiBekU6W4bVIbsUoiFRI=";
   };
 
   nativeBuildInputs = [
@@ -45,6 +46,7 @@ stdenv.mkDerivation rec {
   buildInputs = [
     accountsservice
     dbus
+    fwupd
     geoclue2
     glib
     gtk3
@@ -56,7 +58,19 @@ stdenv.mkDerivation rec {
   postPatch = ''
     chmod +x meson/post_install.py
     patchShebangs meson/post_install.py
+
+    substituteInPlace data/io.elementary.settings-daemon.check-for-firmware-updates.service \
+      --replace "/usr/bin/busctl" "${systemd}/bin/busctl"
   '';
+
+  postInstall = ''
+    # https://github.com/elementary/settings-daemon/pull/75
+    mkdir -p $out/etc/xdg/autostart
+    ln -s $out/share/applications/io.elementary.settings-daemon.desktop $out/etc/xdg/autostart/io.elementary.settings-daemon.desktop
+  '';
+
+  # https://github.com/elementary/settings-daemon/pull/74
+  PKG_CONFIG_SYSTEMD_SYSTEMDSYSTEMUNITDIR = "${placeholder "out"}/lib/systemd/system";
 
   passthru = {
     updateScript = nix-update-script { };
