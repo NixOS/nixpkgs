4e31777d792de1a67b7ac2f67f133a4e44f94a8a
Anderson Torres <torres.anderson.85@protonmail.com>
2023-07-23 18:18:58 -0300

    bmake: cosmetic rewrite
    - remove `with lib;`
    - put BROKEN_TESTS under env
    - reorder phases in execution order (because it's nice!)

--- pkgs/development/tools/build-managers/bmake/default.nix
+++ pkgs/development/tools/build-managers/bmake/default.nix
@@ -13,22 +13,9 @@ stdenv.mkDerivation (finalAttrs: {
   version = "20230414";
 
   src = fetchurl {
-    url = "http://www.crufty.net/ftp/pub/sjg/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
+    url = "http://www.crufty.net/ftp/pub/sjg/bmake-${finalAttrs.version}.tar.gz";
     hash = "sha256-KcsdJqrn3p3vkr2us6rUUg6JlRzpey518LibrhuVOZ8=";
   };
-
-  # Make tests work with musl
-  # * Disable deptgt-delete_on_error test (alpine does this too)
-  # * Disable shell-ksh test (ksh doesn't compile with musl)
-  # * Fix test failing due to different strerror(3) output for musl and glibc
-  postPatch = lib.optionalString (stdenv.hostPlatform.libc == "musl") ''
-    sed -i unit-tests/Makefile \
-      -e '/deptgt-delete_on_error/d' \
-      -e '/shell-ksh/d'
-    substituteInPlace unit-tests/opt-chdir.exp --replace "File name" "Filename"
-  '';
-
-  nativeBuildInputs = [ getopt ];
 
   patches = [
     # make bootstrap script aware of the prefix in /nix/store
@@ -51,23 +38,25 @@ stdenv.mkDerivation (finalAttrs: {
     })
   ];
 
+  # Make tests work with musl
+  # * Disable deptgt-delete_on_error test (alpine does this too)
+  # * Disable shell-ksh test (ksh doesn't compile with musl)
+  # * Fix test failing due to different strerror(3) output for musl and glibc
+  postPatch = lib.optionalString (stdenv.hostPlatform.libc == "musl") ''
+    sed -i unit-tests/Makefile \
+      -e '/deptgt-delete_on_error/d' \
+      -e '/shell-ksh/d'
+    substituteInPlace unit-tests/opt-chdir.exp --replace "File name" "Filename"
+  '';
+
+  nativeBuildInputs = [ getopt ];
+
   # The generated makefile is a small wrapper for calling ./boot-strap with a
   # given op. On a case-insensitive filesystem this generated makefile clobbers
   # a distinct, shipped, Makefile and causes infinite recursion during tests
   # which eventually fail with "fork: Resource temporarily unavailable"
   configureFlags = [
     "--without-makefile"
-  ];
-
-  # Disabled tests:
-  # opt-chdir: ofborg complains about it somehow
-  # opt-keep-going-indirect: not yet known
-  # varmod-localtime: musl doesn't support TZDIR and this test relies on impure,
-  # implicit paths
-  BROKEN_TESTS = builtins.concatStringsSep " " [
-    "opt-chdir"
-    "opt-keep-going-indirect"
-    "varmod-localtime"
   ];
 
   buildPhase = ''
@@ -94,6 +83,17 @@ stdenv.mkDerivation (finalAttrs: {
     ksh
   ];
 
+  # Disabled tests:
+  # opt-chdir: ofborg complains about it somehow
+  # opt-keep-going-indirect: not yet known
+  # varmod-localtime: musl doesn't support TZDIR and this test relies on impure,
+  # implicit paths
+  env.BROKEN_TESTS = builtins.concatStringsSep " " [
+    "opt-chdir"
+    "opt-keep-going-indirect"
+    "varmod-localtime"
+  ];
+
   checkPhase = ''
     runHook preCheck
 
@@ -104,14 +104,14 @@ stdenv.mkDerivation (finalAttrs: {
 
   setupHook = ./setup-hook.sh;
 
-  meta = with lib; {
+  passthru.tests.bmakeMusl = pkgsMusl.bmake;
+
+  meta = {
     homepage = "http://www.crufty.net/help/sjg/bmake.html";
     description = "Portable version of NetBSD 'make'";
-    license = licenses.bsd3;
-    maintainers = with maintainers; [ thoughtpolice AndersonTorres ];
-    platforms = platforms.unix;
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ thoughtpolice AndersonTorres ];
+    platforms = lib.platforms.unix;
   };
-
-  passthru.tests.bmakeMusl = pkgsMusl.bmake;
 })
 # TODO: report the quirks and patches to bmake devteam (especially the Musl one)
