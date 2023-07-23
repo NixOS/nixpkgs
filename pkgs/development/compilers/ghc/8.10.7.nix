e235b45921fdb4481164df7837e0854667443357
Alex Tunstall <alex@tunstall.xyz>
2023-07-23 09:50:55 +0100

    haskell.compiler.ghc*: correctly account for ncurses in cross
    1. Explicitly set WITH_TERMINFO. We usually match GHC's behavior well,
       but it is better to tie the Nix option to make explicitly.
       Unfortunately, the same is very complicated to achieve with
       hadrian (iirc).
    2. Disable enableTerminfo if we are cross-compiling. This matches
       the behavior of GHC's build system, so we'll have to match it now.
       It also reduces the ncurses-related headache a bit.
    3. Stop passing --with-curses* flags. Unfortunately, GHC does not
       account for the fact that different platforms need different ncurses
       libraries. This is somewhat migitated by the fact that ncurses is
       only ever needed for the build platform if we are cross compiling,
       but I seem to remember it leaking into the final GHC somehow.
       A more reliable alternative is relying on the cc/ld wrapper scripts,
       as they'll always pull out the correct ncurses out of the environment
       when GHC's build system passes -lcurses.
    4. Unconditionally add ncurses to depsBuildBuild. Stage0 unconditionally
       builds terminfo (maybe the stage1 compiler needs it?), so we need to
       make sure that ncurses for the build platform is available.
    
    Co-authored-by: sternenseemann <sternenseemann@systemli.org>

--- pkgs/development/compilers/ghc/8.10.7.nix
+++ pkgs/development/compilers/ghc/8.10.7.nix
@@ -35,7 +35,10 @@
   enableShared ? !stdenv.targetPlatform.isWindows && !stdenv.targetPlatform.useiOSPrebuilt
 
 , # Whether to build terminfo.
-  enableTerminfo ? !stdenv.targetPlatform.isWindows
+  enableTerminfo ? !(stdenv.targetPlatform.isWindows
+                     # terminfo can't be built for cross
+                     || (stdenv.buildPlatform != stdenv.hostPlatform)
+                     || (stdenv.hostPlatform != stdenv.targetPlatform))
 
 , # What flavour to build. An empty string indicates no
   # specific flavour and falls back to ghc default values.
@@ -81,6 +84,8 @@ let
     endif
     BUILD_SPHINX_HTML = ${if enableDocs then "YES" else "NO"}
     BUILD_SPHINX_PDF = NO
+
+    WITH_TERMINFO = ${if enableTerminfo then "YES" else "NO"}
   '' +
   # Note [HADDOCK_DOCS]:
   # Unfortunately currently `HADDOCK_DOCS` controls both whether the `haddock`
@@ -321,7 +326,6 @@ stdenv.mkDerivation (rec {
   # `--with` flags for libraries needed for RTS linker
   configureFlags = [
     "--datadir=$doc/share/doc/ghc"
-    "--with-curses-includes=${ncurses.dev}/include" "--with-curses-libraries=${ncurses.out}/lib"
   ] ++ lib.optionals (libffi != null) [
     "--with-system-libffi"
     "--with-ffi-includes=${targetLibffi.dev}/include"
@@ -360,6 +364,8 @@ stdenv.mkDerivation (rec {
   # Used by the STAGE0 compiler to build stage1
   depsBuildBuild = [
     buildCC
+    # stage0 builds terminfo unconditionally, so we always need ncurses
+    ncurses
   ];
   # For building runtime libs
   depsBuildTarget = toolsForTarget;
--- pkgs/development/compilers/ghc/common-make-native-bignum.nix
+++ pkgs/development/compilers/ghc/common-make-native-bignum.nix
@@ -42,7 +42,10 @@
   enableShared ? with stdenv.targetPlatform; !isWindows && !useiOSPrebuilt && !isStatic
 
 , # Whether to build terminfo.
-  enableTerminfo ? !stdenv.targetPlatform.isWindows
+  enableTerminfo ? !(stdenv.targetPlatform.isWindows
+                     # terminfo can't be built for cross
+                     || (stdenv.buildPlatform != stdenv.hostPlatform)
+                     || (stdenv.hostPlatform != stdenv.targetPlatform))
 
 , # What flavour to build. An empty string indicates no
   # specific flavour and falls back to ghc default values.
@@ -88,6 +91,8 @@ let
     endif
     BUILD_SPHINX_HTML = ${if enableDocs then "YES" else "NO"}
     BUILD_SPHINX_PDF = NO
+
+    WITH_TERMINFO = ${if enableTerminfo then "YES" else "NO"}
   '' +
   # Note [HADDOCK_DOCS]:
   # Unfortunately currently `HADDOCK_DOCS` controls both whether the `haddock`
@@ -344,7 +349,6 @@ stdenv.mkDerivation (rec {
   # `--with` flags for libraries needed for RTS linker
   configureFlags = [
     "--datadir=$doc/share/doc/ghc"
-    "--with-curses-includes=${ncurses.dev}/include" "--with-curses-libraries=${ncurses.out}/lib"
   ] ++ lib.optionals (libffi != null) [
     "--with-system-libffi"
     "--with-ffi-includes=${targetLibffi.dev}/include"
@@ -387,6 +391,8 @@ stdenv.mkDerivation (rec {
   # Used by the STAGE0 compiler to build stage1
   depsBuildBuild = [
     buildCC
+    # stage0 builds terminfo unconditionally, so we always need ncurses
+    ncurses
   ];
   # For building runtime libs
   depsBuildTarget = toolsForTarget;
