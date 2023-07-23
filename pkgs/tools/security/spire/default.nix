35c4a3bda460a2403248949e71a978dad18208f0
John Soo <john.soo@arista.com>
2023-07-23 09:41:13 -0700

    drop! fix spire merge.

--- pkgs/tools/security/spire/default.nix
+++ pkgs/tools/security/spire/default.nix
@@ -2,11 +2,7 @@
 
 buildGo119Module rec {
   pname = "spire";
-<<<<<<< HEAD
-  version = "1.2.1";
-=======
   version = "1.7.0";
->>>>>>> 55f04c3bac6 (spire: 1.1.2 -> 1.7.0)
 
   outputs = [ "out" "agent" "server" ];
 
@@ -14,17 +10,10 @@ buildGo119Module rec {
     owner = "spiffe";
     repo = pname;
     rev = "v${version}";
-<<<<<<< HEAD
-    sha256 = "sha256-LK73RGSTwGhCXOglsqK8RAAldovRzliE78vi2ilTSrw=";
-  };
-
-  vendorSha256 = "sha256-am8ZTUX8Vph1Eg013NObMiSVeupS2hlHdpZ/1mO27dY=";
-=======
     sha256 = "sha256-aJ9T8OUsHNeWV05MWLet35V0YFyD7QoiExN6PTmHs3w=";
   };
 
   vendorSha256 = "sha256-4KJysqByDVuK2OU/+sGtpXtSJe4YkVe4OhRyn9tkgsg=";
->>>>>>> 55f04c3bac6 (spire: 1.1.2 -> 1.7.0)
 
   subPackages = [ "cmd/spire-agent" "cmd/spire-server" ];
 
