e8c91460284ac0da365ccd83752cf21cdcbbdbd3
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-23 23:58:26 +0000

    partclone: 0.3.23 -> 0.3.24

--- pkgs/tools/backup/partclone/default.nix
+++ pkgs/tools/backup/partclone/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "partclone";
-  version = "0.3.23";
+  version = "0.3.24";
 
   src = fetchFromGitHub {
     owner = "Thomas-Tsai";
     repo = "partclone";
     rev = version;
-    sha256 = "sha256-na9k26+GDdASZ37n0QtFuRDMtq338QOlXTf0X4raOJI=";
+    sha256 = "sha256-EY5SL8/KITEoOWq2owxQjnemYNsjYqJLLlRVFdYPoVA=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
