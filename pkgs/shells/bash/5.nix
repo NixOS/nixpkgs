99e148dd2719749c3e4c141161ca62dd177276a2
Sergei Trofimovich <slyich@gmail.com>
2023-07-24 23:23:14 +0100

    bash: fix parallel build failure on unwind_prot.o
    As reported by Robert Scott in https://github.com/NixOS/nixpkgs/pull/245066
    without the change `make -j8` build of `make` occasionally fails to
    buildin parallel. The simplest reproducer is:
    
        $$ ./configure
        $$ make unwind_prot.o
        ...
        In file included from unwind_prot.c:51:
        In file included from ./bashintl.h:30:
        ./include/gettext.h:27:11: fatal error: 'libintl.h' file not found
        # include <libintl.h>
                  ^~~~~~~~~~~
        1 error generated.
        make: * [Makefile:106: unwind_prot.o] Error 1
    
    The change adds missing ttransitive `${LIBINTL_H}` dependency for
    unwind_prot.o.

--- pkgs/shells/bash/5.nix
+++ pkgs/shells/bash/5.nix
@@ -61,6 +61,10 @@ stdenv.mkDerivation rec {
       url = "https://cgit.freebsd.org/ports/plain/shells/bash/files/patch-configure?id=3e147a1f594751a68fea00a28090d0792bee0b51";
       sha256 = "XHFMQ6eXTReNoywdETyrfQEv1rKF8+XFbQZP4YoVKFk=";
     })
+    # Apply parallel build fix pending upstream inclusion:
+    #   https://savannah.gnu.org/patch/index.php?10373
+    # Had to fetch manually to workaround -p0 default.
+    ./parallel.patch
   ];
 
   configureFlags = [
