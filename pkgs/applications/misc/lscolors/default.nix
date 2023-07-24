2cf16635698c958670f793ee7da323b2ef39fa49
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-07-24 17:41:56 +0200

    lscolors: 0.14.0 -> 0.15.0
    Diff: https://diff.rs/lscolors/0.14.0/0.15.0

--- pkgs/applications/misc/lscolors/default.nix
+++ pkgs/applications/misc/lscolors/default.nix
@@ -2,14 +2,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "lscolors";
-  version = "0.14.0";
+  version = "0.15.0";
 
   src = fetchCrate {
     inherit version pname;
-    sha256 = "sha256-YVKs/1R//oKYBSSGnj6UMFo5zDp5O3QDDdkDKF4ICBk=";
+    sha256 = "sha256-C7aM9jlChRwPvYnBjLbV+sfbTHDVVi6evIR5PvT9jN4=";
   };
 
-  cargoHash = "sha256-EJdjSFgvvwH5beW+aD1KT5G9bpW/8mdi+7c27KSkZjo=";
+  cargoHash = "sha256-93FAEhl0WFXRq1SaoLRNDd/fy7NyDbeRFgIqUWAssQE=";
 
   # setid is not allowed in the sandbox
   checkFlags = [ "--skip=tests::style_for_setid" ];
