dd4a80196d23f6b61e6b32cacab493e9cf55da3a
nyanotech <nyanotechnology@gmail.com>
2023-07-25 13:19:40 -0700

    proxmark3: Replace with rfidresearchgroup fork
    Looking at https://repology.org/project/proxmark3/versions, it seems like all the other distros that have proxmark3 packaged package the fork version. Additionally, the upstream version hasn't been updated in some time.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1413,6 +1413,8 @@ mapAliases ({
   protobuf3_11 = throw "protobuf3_11 does not receive updates anymore and has been removed"; # Added 2022-09-28
   protobuf3_17 = throw "protobuf3_17 does not receive updates anymore and has been removed"; # Added 2023-05-21
   protonup = protonup-ng; # Added 2022-11-06
+  proxmark3-rrg = proxmark3; # Added 2023-07-25
+  proxmark3-unstable = throw "removed in favor of rfidresearchgroup fork"; # Added 2023-07-25
   proxytunnel = throw "proxytunnel has been removed from nixpkgs, because it has not been update upstream since it was added to nixpkgs in 2008 and has therefore bitrotted."; # added 2021-12-15
   pulseaudio-hsphfpd = throw "pulseaudio-hsphfpd upstream has been abandoned"; # Added 2022-03-23
   pulseaudio-modules-bt = throw "pulseaudio-modules-bt has been abandoned, and is superseded by pulseaudio's native bt functionality"; # Added 2022-04-01
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11910,10 +11910,7 @@ with pkgs;
 
   prototypejs = callPackage ../development/libraries/prototypejs { };
 
-  inherit (callPackages ../tools/security/proxmark3 { gcc-arm-embedded = gcc-arm-embedded-8; })
-    proxmark3 proxmark3-unstable;
-
-  proxmark3-rrg = libsForQt5.callPackage ../tools/security/proxmark3/proxmark3-rrg.nix {
+  proxmark3 = libsForQt5.callPackage ../tools/security/proxmark3/default.nix {
     inherit (darwin.apple_sdk.frameworks) Foundation AppKit;
   };
 
