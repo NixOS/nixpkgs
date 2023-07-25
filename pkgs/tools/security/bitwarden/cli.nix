8c8d1164de84808c61a233ead50e1ee27f78a0a5
Robert Schütz <nix@dotlambda.de>
2023-07-25 11:18:32 -0700

    bitwarden-cli: 2023.5.0 -> 2023.7.0
    Diff: https://github.com/bitwarden/clients/compare/cli-v2023.5.0...cli-v2023.7.0
    
    Changelog: https://github.com/bitwarden/clients/releases/tag/cli-v2023.7.0

--- pkgs/tools/security/bitwarden/cli.nix
+++ pkgs/tools/security/bitwarden/cli.nix
@@ -11,16 +11,16 @@ let
   buildNpmPackage' = buildNpmPackage.override { nodejs = nodejs_18; };
 in buildNpmPackage' rec {
   pname = "bitwarden-cli";
-  version = "2023.5.0";
+  version = "2023.7.0";
 
   src = fetchFromGitHub {
     owner = "bitwarden";
     repo = "clients";
     rev = "cli-v${version}";
-    hash = "sha256-ELKpGSY4ZbgSk4vJnTiB+IOa8RQU8Ahy3A1mYsKtthU=";
+    hash = "sha256-Xnfjp+qRJWvxvgSODbajLxYsP2DtOYK9CXBMfIn+qwA=";
   };
 
-  npmDepsHash = "sha256-G8DEYPjEP3L4s0pr5n2ZTj8kkT0E7Po1BKhZ2hUdJuY=";
+  npmDepsHash = "sha256-vz7erDhh3BpHNadPwIXkD2PRCnbxM7e7lE0rvBEXGyc=";
 
   nativeBuildInputs = [
     python3
