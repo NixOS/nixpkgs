0f01bba32174fa0b2f6159e5ddf8337c000d7a28
sunder <evenquantity@gmail.com>
2023-07-25 17:15:11 +0300

    plots: init at 0.8.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11814,6 +11814,8 @@ with pkgs;
 
   plotinus = callPackage ../tools/misc/plotinus { };
 
+  plots = callPackage ../applications/misc/plots { };
+
   plotutils = callPackage ../tools/graphics/plotutils { };
 
   plowshare = callPackage ../tools/misc/plowshare { };
