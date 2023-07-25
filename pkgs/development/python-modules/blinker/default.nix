20fe72a45b1490eadba04a102f97172991fb4f94
Nick Cao <nickcao@nichi.co>
2023-07-25 15:11:54 +0800

    python3Packages.blinker: 1.5 -> 1.6.2

--- pkgs/development/python-modules/blinker/default.nix
+++ pkgs/development/python-modules/blinker/default.nix
@@ -1,21 +1,39 @@
 { lib
 , buildPythonPackage
-, fetchPypi
+, pythonOlder
+, fetchFromGitHub
+, setuptools
 , pytestCheckHook
+, pytest-asyncio
 }:
 
 buildPythonPackage rec {
   pname = "blinker";
-  version = "1.5";
+  version = "1.6.2";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-kj5eL2nBVfLMQtr7vXDhbj/eJNLUqiq3L744YjiJJGI=";
+  format = "pyproject";
+
+  disabled = pythonOlder "3.7";
+
+  src = fetchFromGitHub {
+    owner = "pallets-eco";
+    repo = "blinker";
+    rev = "refs/tags/${version}";
+    hash = "sha256-s74zYyExttRxHFPanw5Zqeby36Dq6aJj3IeQQGw3aes=";
   };
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  nativeBuildInputs = [
+    setuptools
+  ];
 
-  pythonImportsCheck = [ "blinker" ];
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-asyncio
+  ];
+
+  pythonImportsCheck = [
+    "blinker"
+  ];
 
   meta = with lib; {
     homepage = "https://pythonhosted.org/blinker/";
