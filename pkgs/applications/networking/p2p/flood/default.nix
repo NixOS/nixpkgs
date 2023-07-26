207166a65d5af1dccae4b9b60c0195ab2d06d729
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-07-26 22:47:00 +0100

    flood: 4.7.0 -> unstable-2023-06-03
    Bumping to current development version to fix some deprecation warns in
    Node and also issues with newer versions of qBittorrent.

--- pkgs/applications/networking/p2p/flood/default.nix
+++ pkgs/applications/networking/p2p/flood/default.nix
@@ -5,18 +5,16 @@
 
 buildNpmPackage rec {
   pname = "flood";
-  version = "4.7.0";
+  version = "unstable-2023-06-03";
 
   src = fetchFromGitHub {
     owner = "jesec";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-BR+ZGkBBfd0dSQqAvujsbgsEPFYw/ThrylxUbOksYxM=";
+    rev = "2b652f8148dab7134eeeb201b9d81dd6b8bda074";
+    hash = "sha256-wI6URPGUZUbydSgNaHN2C5IA2x/HHjBWIRT6H6iZU/0=";
   };
 
-  npmDepsHash = "sha256-tuEfyePwlOy2/mOPdXbqJskO6IowvAP4DWg8xSZwbJw=";
-
-  NODE_OPTIONS = "--openssl-legacy-provider";
+  npmDepsHash = "sha256-XmDnvq+ni5TOf3UQFc4JvGI3LiGpjbrLAocRvrW8qgk=";
 
   meta = with lib; {
     description = "Modern web UI for various torrent clients with a Node.js backend and React frontend";
