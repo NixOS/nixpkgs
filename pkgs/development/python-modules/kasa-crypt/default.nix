52a20d5ec49c9c5911e2664a7c0b1f283d80f4d1
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-26 21:23:01 +0200

    python311Packages.kasa-crypt: 0.2.1 -> 0.3.0
    Diff: https://github.com/bdraco/kasa-crypt/compare/refs/tags/v0.2.1...v0.3.0
    
    Changelog: https://github.com/bdraco/kasa-crypt/blob/0.3.0/CHANGELOG.md

--- pkgs/development/python-modules/kasa-crypt/default.nix
+++ pkgs/development/python-modules/kasa-crypt/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "kasa-crypt";
-  version = "0.2.1";
+  version = "0.3.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "bdraco";
     repo = "kasa-crypt";
     rev = "refs/tags/v${version}";
-    hash = "sha256-VAm/jHeG/LfLpouHc3mSX8Kr8u/keHY552dLzoGaPrQ=";
+    hash = "sha256-w0LdGPVsxTi7jlfS/HSBEZdjL9ZKclucnBVViwrVC6Y=";
   };
 
   postPatch = ''
