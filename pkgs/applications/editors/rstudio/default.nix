dc62f1a97623a35aefd36166a773ff2108c3569d
nviets <nathan.viets@gmail.com>
2023-07-26 13:20:38 -0500

    updated rsconnect hash

--- pkgs/applications/editors/rstudio/default.nix
+++ pkgs/applications/editors/rstudio/default.nix
@@ -62,8 +62,10 @@ let
   rsconnectSrc = fetchFromGitHub {
     owner = "rstudio";
     repo = "rsconnect";
-    rev = "e287b586e7da03105de3faa8774c63f08984eb3c";
-    sha256 = "sha256-ULyWdSgGPSAwMt0t4QPuzeUE6Bo6IJh+5BMgW1bFN+Y=";
+    #rev = "e287b586e7da03105de3faa8774c63f08984eb3c";
+    #sha256 = "sha256-ULyWdSgGPSAwMt0t4QPuzeUE6Bo6IJh+5BMgW1bFN+Y=";
+    rev = "db2bcba0d82698aca6bc6aff4f639cc531060b4b";
+    sha256 = "sha256-Lv3WBTb0oP0ibP3AB0v3+E+i2DJoADMZ6Ysfxnk3ZvE=";
   };
 
   quartoSrc = fetchFromGitHub {
