025b63b129a40683aa24ac9576746d5551709652
Robert Schütz <nix@dotlambda.de>
2023-07-25 22:17:29 -0700

    polaris-web: use buildNpmPackage

--- pkgs/servers/polaris/web.nix
+++ pkgs/servers/polaris/web.nix
@@ -1,12 +1,11 @@
 { lib
-, stdenv
-, pkgs
+, buildNpmPackage
 , fetchFromGitHub
-, nodejs
-, cypress
+, nodePackages
+, python3
 }:
 
-stdenv.mkDerivation rec {
+buildNpmPackage rec {
   pname = "polaris-web";
   version = "build-55";
 
@@ -17,49 +16,19 @@ stdenv.mkDerivation rec {
     sha256 = "2XqU4sExF7Or7RxpOK2XU9APtBujfPhM/VkOLKVDvF4=";
   };
 
+  npmDepsHash = "sha256-sSd1WSS9xsaVr9mCQueAuqceOv5SkSzqsxX9kHYC8Xo=";
+
   nativeBuildInputs = [
-    nodejs
+    nodePackages.node-gyp-build
+    python3
   ];
 
-  buildPhase =
-    let
-      nodeDependencies = (import ./node-composition.nix {
-        inherit pkgs nodejs;
-        inherit (stdenv.hostPlatform) system;
-      }).nodeDependencies.override (old: {
-        # access to path '/nix/store/...-source' is forbidden in restricted mode
-        src = src;
-        dontNpmInstall = true;
+  env.CYPRESS_INSTALL_BINARY = "0";
 
-        # ERROR: .../.bin/node-gyp-build: /usr/bin/env: bad interpreter: No such file or directory
-        # https://github.com/svanderburg/node2nix/issues/275
-        # There are multiple instances of it, hence the globstar
-        preRebuild = ''
-          shopt -s globstar
-          sed -i -e "s|#!/usr/bin/env node|#! ${pkgs.nodejs}/bin/node|" \
-            node_modules/**/node-gyp-build/bin.js \
-        '';
+  # https://github.com/parcel-bundler/parcel/issues/8005
+  env.NODE_OPTIONS = "--no-experimental-fetch";
 
-        buildInputs = [ cypress ];
-        # prevent downloading cypress, use the executable in path instead
-        CYPRESS_INSTALL_BINARY = "0";
-
-      });
-    in
-    ''
-      runHook preBuild
-
-      export PATH="${nodeDependencies}/bin:${nodejs}/bin:$PATH"
-
-      # https://github.com/parcel-bundler/parcel/issues/8005
-      export NODE_OPTIONS=--no-experimental-fetch
-
-      ln -s ${nodeDependencies}/lib/node_modules .
-      npm run production
-
-      runHook postBuild
-    '';
-
+  npmBuildScript = "production";
 
   installPhase = ''
     runHook preInstall
@@ -70,13 +39,10 @@ stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
-  passthru.updateScript = ./update-web.sh;
-
   meta = with lib; {
     description = "Web client for Polaris";
     homepage = "https://github.com/agersant/polaris-web";
     license = licenses.mit;
     maintainers = with maintainers; [ pbsds ];
-    platforms = platforms.unix;
   };
 }
