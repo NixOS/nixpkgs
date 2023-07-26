7bc72fa4711a80ed5ef9f5de42dae2bce1dd2382
Austin Horstman <khaneliman12@gmail.com>
2023-07-26 11:05:20 -0500

    maintainers: add khaneliman

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8721,6 +8721,12 @@
     githubId = 546087;
     name = "Kristoffer K. Føllesdal";
   };
+  khaneliman = {
+    email = "khaneliman12@gmail.com";
+    github = "khaneliman";
+    githubId = 1778670;
+    name = "Austin Horstman";
+  };
   khaser = {
     email = "a-horohorin@mail.ru";
     github = "khaser";
