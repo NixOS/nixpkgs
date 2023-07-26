611785d7350788a97f2889835538cbeca9ac0f9e
Karel Kočí <cynerd@email.cz>
2023-07-26 10:58:32 +0200

    uboot: init ubootEnvTools
    The U-Boot environment tools are intended to be used on the board itself
    to access U-Boot's environment variables.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28547,6 +28547,7 @@ with pkgs;
   inherit (callPackage ../misc/uboot {})
     buildUBoot
     ubootTools
+    ubootEnvTools
     ubootA20OlinuxinoLime
     ubootA20OlinuxinoLime2EMMC
     ubootBananaPi
