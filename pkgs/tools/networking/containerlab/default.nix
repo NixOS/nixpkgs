031c2c70b25ca9e155165eedf19c16356b4263d1
Janik H <janik@aq0.de>
2023-07-27 14:36:34 +0200

    containerlab: add shell completions

--- pkgs/tools/networking/containerlab/default.nix
+++ pkgs/tools/networking/containerlab/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, installShellFiles
 }:
 
 buildGoModule rec {
@@ -14,6 +15,8 @@ buildGoModule rec {
     hash = "sha256-Wo6Dfcdax8EjZRP5JBKrnxpFTVx0GV+7uaDgBY/zg6I=";
   };
 
+  nativeBuildInputs = [ installShellFiles ];
+
   vendorHash = "sha256-uaYeFzG74VTUrtql6mSi+LNh6IjDomixrWwGQMgmONc=";
 
   ldflags = [
@@ -23,6 +26,14 @@ buildGoModule rec {
     "-X" "github.com/srl-labs/containerlab/cmd.commit=${src.rev}"
     "-X" "github.com/srl-labs/containerlab/cmd.date=1970-01-01T00:00:00Z"
   ];
+
+  postInstall = ''
+    local INSTALL="$out/bin/containerlab"
+    installShellCompletion --cmd containerlab \
+      --bash <($out/bin/containerlab completion bash) \
+      --fish <($out/bin/containerlab completion fish) \
+      --zsh <($out/bin/containerlab completion zsh)
+  '';
 
   meta = with lib; {
     description = "Container-based networking lab";
