394b0d1e0cedb63f9eafe25efe3e15a31b6c1aec
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-27 01:19:28 +0000

    balena-cli: 16.7.5 -> 16.7.6

--- pkgs/tools/admin/balena-cli/default.nix
+++ pkgs/tools/admin/balena-cli/default.nix
@@ -15,12 +15,12 @@ let
   }.${system} or throwSystem;
 
   sha256 = {
-    x86_64-linux = "sha256-/Die6tpidmt3dTzW7mEnQJG5vlxue4sT44PcAa7lrgk=";
+    x86_64-linux = "sha256-USljQ/cnbSabzsZWXlZ0eeZSqkTr3wVP0ktXqZ7Fw4U=";
     x86_64-darwin = "sha256-VB5075VfQRtlLoG3lfkVz8ASoODiwleTYC2JJR51LtI=";
     aarch64-darwin = "sha256-VB5075VfQRtlLoG3lfkVz8ASoODiwleTYC2JJR51LtI=";
   }.${system} or throwSystem;
 
-  version = "16.7.5";
+  version = "16.7.6";
   src = fetchzip {
     url = "https://github.com/balena-io/balena-cli/releases/download/v${version}/balena-cli-v${version}-${plat}-standalone.zip";
     inherit sha256;
