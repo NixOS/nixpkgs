d9248a45f0941e763b1c90340060fabde726d3b4
Fabian Affolter <mail@fabian-affolter.ch>
2023-07-27 11:31:46 +0200

    python311Packages.pypitoken: update disabled

--- pkgs/development/python-modules/pypitoken/default.nix
+++ pkgs/development/python-modules/pypitoken/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   version = "7.0.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ewjoachim";
