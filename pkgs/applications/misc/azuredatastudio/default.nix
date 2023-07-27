2e666e56759134ca097e8fbe1ff2a8c72eabc5ad
kashw2 <supra4keanu@hotmail.com>
2023-07-27 14:11:47 +1000

    azuredatastudio: 1.45.0 -> 1.48.0

--- pkgs/applications/misc/azuredatastudio/default.nix
+++ pkgs/applications/misc/azuredatastudio/default.nix
@@ -72,14 +72,14 @@ in
 stdenv.mkDerivation rec {
 
   pname = "azuredatastudio";
-  version = "1.44.1";
+  version = "1.48.0";
 
   desktopItems = [ desktopItem urlHandlerDesktopItem ];
 
   src = fetchurl {
     name = "${pname}-${version}.tar.gz";
     url = "https://azuredatastudio-update.azurewebsites.net/${version}/linux-x64/stable";
-    sha256 = "sha256-6kEV331kt+/7/uWKZmTTkJX4P06CfxC8Ogq052qlUEg=";
+    sha256 = "sha256-+Z/g11ED46qlNdim6Zy8NTD2A7DG2F+o9aikGCG5iQ4=";
   };
 
   nativeBuildInputs = [
@@ -122,7 +122,7 @@ stdenv.mkDerivation rec {
   ];
 
   # this will most likely need to be updated when azuredatastudio's version changes
-  sqltoolsservicePath = "${targetPath}/resources/app/extensions/mssql/sqltoolsservice/Linux/4.7.1.6";
+  sqltoolsservicePath = "${targetPath}/resources/app/extensions/mssql/sqltoolsservice/Linux/4.11.1.1";
 
   rpath = lib.concatStringsSep ":" [
     (lib.makeLibraryPath [
