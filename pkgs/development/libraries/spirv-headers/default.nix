45257e58bd26b2e5b2cc933ebcfece5429249e0a
K900 <me@0upti.me>
2023-07-28 22:47:22 +0300

    spirv-headers: 1.3.250.0 -> unstable-2023-07-21
    Diff: https://github.com/KhronosGroup/SPIRV-Headers/compare/sdk-1.3.250.0...51b106461707f46d962554efe1bf56dee28958a3

--- pkgs/development/libraries/spirv-headers/default.nix
+++ pkgs/development/libraries/spirv-headers/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, fetchFromGitHub, cmake }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "spirv-headers";
-  version = "1.3.250.0";
+  version = "unstable-2023-07-21";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "SPIRV-Headers";
-    rev = "sdk-${version}";
-    hash = "sha256-5mVcbOAuzux/Yuo0NMwZRTsdQr46UxjrtMhT5mPHyCQ=";
+    rev = "51b106461707f46d962554efe1bf56dee28958a3";
+    hash = "sha256-b+GA2Wo+kgxZWqaqjwSuvScxux5SzHYh8OsXe/3MmUk=";
   };
 
   nativeBuildInputs = [ cmake ];
