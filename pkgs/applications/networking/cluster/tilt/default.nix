e84e4e46d5fbd3140c5a9a88ba8b2925f8643113
Raja Bhatia <raja@users.noreply.github.com>
2023-07-28 14:20:39 -0400

    tilt: 0.33.1 -> 0.33.3

--- pkgs/applications/networking/cluster/tilt/default.nix
+++ pkgs/applications/networking/cluster/tilt/default.nix
@@ -1,28 +1,32 @@
-{ stdenv, lib, buildGoModule, fetchFromGitHub }:
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
 
 buildGoModule rec {
   pname = "tilt";
   /* Do not use "dev" as a version. If you do, Tilt will consider itself
     running in development environment and try to serve assets from the
     source tree, which is not there once build completes.  */
-  version = "0.33.1";
+  version = "0.33.3";
 
   src = fetchFromGitHub {
-    owner  = "tilt-dev";
-    repo   = pname;
-    rev    = "v${version}";
-    sha256 = "sha256-3CRE+gpifV3MHyKdiiHmGwGre0ne3IjheYH0r6NMKY8=";
+    owner = "tilt-dev";
+    repo = "tilt";
+    rev = "v${version}";
+    hash = "sha256-TNZE335tH50E96yJzD26U+JbVxjU746Wa/8YDGHFeto=";
   };
+
   vendorHash = null;
 
   subPackages = [ "cmd/tilt" ];
 
   ldflags = [ "-X main.version=${version}" ];
 
-  meta = with lib; {
+  meta = {
     description = "Local development tool to manage your developer instance when your team deploys to Kubernetes in production";
     homepage = "https://tilt.dev/";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ anton-dessiatov ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ anton-dessiatov ];
   };
 }
