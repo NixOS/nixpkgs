b0e1416a6b7fff4e3c4c837d1d7b0cf4910e63e3
Robert Schütz <nix@dotlambda.de>
2023-07-28 13:31:28 -0700

    karma: use buildNpmPackage

--- pkgs/servers/monitoring/karma/default.nix
+++ pkgs/servers/monitoring/karma/default.nix
@@ -1,21 +1,12 @@
-{ pkgs
-, lib
+{ lib
 , stdenv
 , buildGoModule
-, callPackage
 , fetchFromGitHub
+, fetchNpmDeps
 , nixosTests
-, nodejs_18
+, nodejs
+, npmHooks
 }:
-
-let
-  uiNodeDependencies = (import ./node-composition.nix {
-    inherit pkgs;
-    inherit (stdenv.hostPlatform) system;
-    # pin nodejs version
-    nodejs = nodejs_18;
-   }).nodeDependencies;
-in
 
 buildGoModule rec {
   pname = "karma";
@@ -30,9 +21,21 @@ buildGoModule rec {
 
   vendorHash = "sha256-ZsXPA4KyKbc/bwkidyHNDg62mE8KlE+yIssOBZLmHVg=";
 
+  npmDeps = fetchNpmDeps {
+    src = "${src}/ui";
+    hash = "sha256-gxsq+wwD+fUACkC07DWeVkvVtipZkEcPIR5RcPsPato=";
+  };
+
+  npmRoot = "ui";
+
   nativeBuildInputs = [
-    nodejs_18
+    nodejs
+    npmHooks.npmConfigHook
   ];
+
+  overrideModAttrs = oldAttrs: {
+    nativeBuildInputs = lib.filter (drv: drv != npmHooks.npmConfigHook) oldAttrs.nativeBuildInputs;
+  };
 
   postPatch = ''
     # Since we're using node2nix packages, the NODE_INSTALL hook isn't needed in the makefile
@@ -43,32 +46,11 @@ buildGoModule rec {
   '';
 
   buildPhase = ''
-    # node will fail without this
-    export HOME=$(mktemp -d)
+    runHook preBuild
 
-    # build requires a writable .cache directory, so we'll create a
-    # temporary writable node_modules dir and link every package to it
-
-    # simply linking the node_modules directory would increase the closure size for uiNodeDependencies to >700MB
-    cp -r ${uiNodeDependencies}/lib/node_modules ./ui/
-    chmod -R +w ./ui/node_modules
-    mkdir -p ./ui/node_modules/.bin
-
-    pushd ./ui/node_modules/.bin
-
-    for x in ${uiNodeDependencies}/lib/node_modules/.bin/*; do
-      ln -sfv ./$(readlink "$x") ./$(basename "$x")
-    done
-
-    popd
-
-    mkdir -p ./ui/node_modules/.cache
-
-    # build package
     VERSION="v${version}" make -j$NIX_BUILD_CORES
 
-    # clean up
-    rm -rf ./ui/node_modules
+    runHook postBuild
   '';
 
   installPhase = ''
