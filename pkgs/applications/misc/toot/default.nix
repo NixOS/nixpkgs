c2cda99ef1115f0d0dcf19dc40052de260195c20
Sergei Trofimovich <slyich@gmail.com>
2023-07-28 19:49:47 +0100

    toot: 0.37.0 -> 0.38.1
    Changes: https://github.com/ihabunek/toot/blob/master/CHANGELOG.md

--- pkgs/applications/misc/toot/default.nix
+++ pkgs/applications/misc/toot/default.nix
@@ -2,19 +2,19 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "toot";
-  version = "0.37.0";
+  version = "0.38.1";
 
   src = fetchFromGitHub {
     owner  = "ihabunek";
     repo   = "toot";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-NmxBiFLjAW4kwuChbgR5VsAOpgE6sJOO/MmfRhotb40=";
+    sha256 = "sha256-gT9xKFanQhptt46nkdzYsZ6Vu0Nab0oRsvEHVRNf8DQ=";
   };
 
   nativeCheckInputs = with python3Packages; [ pytest ];
 
   propagatedBuildInputs = with python3Packages;
-    [ requests beautifulsoup4 future wcwidth urwid psycopg2 ];
+    [ requests beautifulsoup4 future wcwidth urwid psycopg2 tomlkit ];
 
   checkPhase = ''
     py.test
