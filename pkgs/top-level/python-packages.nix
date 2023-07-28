0e1093ec31221740831cd46dc716bffdd3b011a5
natsukium <tomoya.otabi@gmail.com>
2023-07-28 23:59:56 +0900

    python310Packages.backports_unittest-mock: remove
    we no longer need to backport to python3.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1207,8 +1207,6 @@ self: super: with self; {
 
   backports-shutil-which = callPackage ../development/python-modules/backports-shutil-which { };
 
-  backports_unittest-mock = callPackage ../development/python-modules/backports_unittest-mock { };
-
   backports_weakref = callPackage ../development/python-modules/backports_weakref { };
 
   backports-zoneinfo = callPackage ../development/python-modules/backports-zoneinfo { };
