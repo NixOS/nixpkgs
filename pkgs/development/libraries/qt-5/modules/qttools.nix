68cc02588d4659c77b9e5bfab7ff848c2b5248ca
K900 <me@0upti.me>
2023-07-29 13:10:16 +0300

    qt5: include qttranslations properly
    Qt loads its own translations from a hardcoded path, and those are used
    (among other things) for determining RTL layout preferences in applications,
    so they are definitely something we want to have.
    
    This adds a qtbase/qttools rebuild to the chain, but it's fast enough that it's
    probably fine.
    
    Another approach would be to load translation paths from the environment,
    and inject it in wrapQtAppsHook, but that seems like more complexity
    for very questionable build time savings.

--- pkgs/development/libraries/qt-5/modules/qttools.nix
+++ pkgs/development/libraries/qt-5/modules/qttools.nix
@@ -36,7 +36,7 @@ qtModule {
     "bin/macdeployqt"
   ];
 
-  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin ''-DNIXPKGS_QMLIMPORTSCANNER="${qtdeclarative.dev}/bin/qmlimportscanner"'';
+  env.NIX_CFLAGS_COMPILE = lib.optionalString (stdenv.isDarwin && qtdeclarative != null) ''-DNIXPKGS_QMLIMPORTSCANNER="${qtdeclarative.dev}/bin/qmlimportscanner"'';
 
   setupHook = ../hooks/qttools-setup-hook.sh;
 }
