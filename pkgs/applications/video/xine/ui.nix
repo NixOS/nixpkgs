1330d5a6106c5a365523eec7c8f3c1e7bced31dd
Anderson Torres <torres.anderson.85@protonmail.com>
2023-07-29 13:04:01 -0300

    xine-ui: 0.99.13 -> 0.99.14

--- pkgs/applications/video/xine/ui.nix
+++ pkgs/applications/video/xine/ui.nix
@@ -4,6 +4,13 @@
 , fetchpatch
 , autoreconfHook
 , curl
+, libXext
+, libXft
+, libXi
+, libXinerama
+, libXtst
+, libXv
+, libXxf86vm
 , libjpeg
 , libpng
 , lirc
@@ -12,41 +19,26 @@
 , readline
 , shared-mime-info
 , xine-lib
-, xorg
+, xorgproto
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xine-ui";
-  version = "0.99.13";
+  version = "0.99.14";
 
   src = fetchurl {
-    url = "mirror://sourceforge/xine/${pname}-${version}.tar.xz";
-    sha256 = "sha256-sjgtB1xysbEAOeDpAxDMhsjZEDWMU1We2C09WEIB9cU=";
+    url = "mirror://sourceforge/xine/xine-ui-${finalAttrs.version}.tar.xz";
+    hash = "sha256-1NSQ1c7OcOK7mEnJ5ILyz4evAwLUUbYUR2/cw2Qs2cM=";
   };
-
-  patches = [
-    (fetchpatch {
-      # Fix build on aarch64
-      name = "xine-ui_FTBS_aarch64.patch";
-      url = "https://salsa.debian.org/debian/xine-ui/-/raw/b2f04f64947a8975a805950e7e67b15cb44007ef/debian/patches/backport/0003-Fix-build.patch";
-      sha256 = "03f8nkm7q11v5vssl1bj500ja4ljz4y752mfk22k2g4djkwimx62";
-    })
-  ];
 
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
     shared-mime-info
   ];
+
   buildInputs = [
     curl
-    libjpeg
-    libpng
-    lirc
-    ncurses
-    readline
-    xine-lib
-  ] ++ (with xorg; [
     libXext
     libXft
     libXi
@@ -54,24 +46,32 @@ stdenv.mkDerivation rec {
     libXtst
     libXv
     libXxf86vm
+    libjpeg
+    libpng
+    lirc
+    ncurses
+    readline
+    xine-lib
     xorgproto
-  ]);
+  ];
 
   configureFlags = [ "--with-readline=${readline.dev}" ];
 
-  LIRC_CFLAGS="-I${lirc}/include";
-  LIRC_LIBS="-L ${lirc}/lib -llirc_client";
+  env = {
+    LIRC_CFLAGS="-I${lirc}/include";
+    LIRC_LIBS="-L ${lirc}/lib -llirc_client";
+  };
 
   postInstall = ''
     substituteInPlace $out/share/applications/xine.desktop \
       --replace "MimeType=;" "MimeType="
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://xine.sourceforge.net/";
     description = "Xlib-based frontend for Xine video player";
-    license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ AndersonTorres ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.linux;
   };
-}
+})
