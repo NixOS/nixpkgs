e7b1cf2594f8d0cc098991c0d7cfee3cc95841d5
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-07-28 23:01:43 -0700

    python3.pkgs.aiomysql: add build dependencies and fix constraints

--- pkgs/development/python-modules/aiomysql/default.nix
+++ pkgs/development/python-modules/aiomysql/default.nix
@@ -1,10 +1,12 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , pymysql
 , pythonOlder
 , setuptools-scm
 , setuptools-scm-git-archive
+, wheel
 }:
 
 buildPythonPackage rec {
@@ -21,9 +23,21 @@ buildPythonPackage rec {
     hash = "sha256-m/EgoBU3e+s3soXyYtACMDSjJfMLBOk/00qPtgawwQ8=";
   };
 
+  patches = [
+    # Upstream found that setuptools-scm version 7 produces wrong versions,
+    # but I don't know what that means and the build works fine.
+    (fetchpatch {
+      name = "revert-setuptools-scm-constraint.patch";
+      url = "https://github.com/aio-libs/aiomysql/commit/fb85893635d7f9c0da3b1ff8c6d0fc436357633a.patch";
+      hash = "sha256-xsxUydqfYYZ+AS/YyfKSuavyNgKUNySZWDVm6IkA+5o=";
+      revert = true;
+    })
+  ];
+
   nativeBuildInputs = [
     setuptools-scm
     setuptools-scm-git-archive
+    wheel
   ];
 
   propagatedBuildInputs = [
