52e979f9e63793ee9f4e1d3dc3c2ad02a83de885
Felix Buehler <account@buehler.rocks>
2023-07-30 22:19:16 +0200

    gtk2,gtk3: rename derivation name

--- pkgs/development/libraries/gtk/3.x.nix
+++ pkgs/development/libraries/gtk/3.x.nix
@@ -62,7 +62,7 @@ let
 in
 
 stdenv.mkDerivation (finalAttrs: {
-  pname = "gtk+3";
+  pname = "gtk3";
   version = "3.24.38";
 
   outputs = [ "out" "dev" ] ++ lib.optional withIntrospection "devdoc";
