88b2797038045ef81fa9973195b2274902c59ec9
Doron Behar <doron.behar@gmail.com>
2023-07-30 18:47:52 +0300

    xsimd: Fix exp10 darwin build error
    Disable tests that fail on Darwin, as also reported upstream.

--- pkgs/development/libraries/xsimd/default.nix
+++ pkgs/development/libraries/xsimd/default.nix
@@ -14,6 +14,18 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "sha256-l6IRzndjb95hIcFCCm8zmlNHWtKduqy2t/oml/9Xp+w=";
   };
+  patches = [
+    # Ideally, Accelerate/Accelerate.h should be used for this implementation,
+    # but it doesn't work... Needs a Darwin user to debug this. We apply this
+    # patch unconditionally, because the #if macros make sure it doesn't
+    # interfer with the Linux implementations.
+    ./fix-darwin-exp10-implementation.patch
+  ] ++ lib.optionals stdenv.isDarwin [
+    # Upstream reports:
+    # https://github.com/xtensor-stack/xsimd/issues/807
+    # https://github.com/xtensor-stack/xsimd/issues/917
+    ./disable-darwin-failing-tests.patch
+  ];
 
   nativeBuildInputs = [
     cmake
