dabc05009076462c3732f68648ea56905a689e43
Robert Schütz <nix@dotlambda.de>
2023-07-29 22:06:56 -0700

    karma-runner: init at 6.4.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18188,6 +18188,8 @@ with pkgs;
 
   espup = callPackage ../development/tools/espup { };
 
+  karma-runner = callPackage ../development/tools/karma-runner { };
+
   phpunit = callPackage ../development/tools/misc/phpunit { };
 
   teller = callPackage ../development/tools/teller { };
