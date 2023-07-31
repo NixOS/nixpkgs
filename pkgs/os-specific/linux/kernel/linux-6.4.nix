2a1caa0d1676f46b34a55690cf7fb0bff6c5d22b
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-07-31 16:57:22 -0600

    linux: 6.4.6 -> 6.4.7
    (cherry picked from commit 9dd903ca6306961590d565272b2b30c08136ee9e)

--- pkgs/os-specific/linux/kernel/linux-6.4.nix
+++ pkgs/os-specific/linux/kernel/linux-6.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.4.6";
+  version = "6.4.7";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "192ya79h3lw781ka22ibgbvdcx6maa74nyk0lqjsz2n4xybc9v71";
+    sha256 = "1n57qijg0m27wcrqll8rb1hi1n0n8ca3bzsnbz05d9ya3nv3q56y";
   };
 } // (args.argsOverride or { }))
