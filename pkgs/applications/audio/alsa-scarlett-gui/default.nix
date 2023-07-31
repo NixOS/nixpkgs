6966289136de3611901c34d5b227d0843806e3d7
matt <gnumber13@e.mail.de>
2023-07-31 10:42:11 +0200

    alsa-scarlett-gui: light refactoring

--- pkgs/applications/audio/alsa-scarlett-gui/default.nix
+++ pkgs/applications/audio/alsa-scarlett-gui/default.nix
@@ -26,8 +26,7 @@ stdenv.mkDerivation rec {
   buildInputs = [ gtk4 alsa-lib ];
   postInstall = ''
       substituteInPlace $out/share/applications/vu.b4.${pname}.desktop \
-        --replace "Exec=/bin/alsa-scarlett-gui" "Exec=$out/bin/${pname}" \
-        --replace "Icon=alsa-scarlett-gui" "Icon=${pname}"
+        --replace "Exec=/bin/alsa-scarlett-gui" "Exec=$out/bin/${pname}"
   '';
 
   # causes redefinition of _FORTIFY_SOURCE
