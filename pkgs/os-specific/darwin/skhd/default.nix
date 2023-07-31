a8b5053805b3a9734000c20475d5eeb2eafb0fd1
Austin Horstman <khaneliman12@gmail.com>
2023-07-30 21:36:19 -0500

    skhd: 0.3.5 -> 0.3.9

--- pkgs/os-specific/darwin/skhd/default.nix
+++ pkgs/os-specific/darwin/skhd/default.nix
@@ -1,17 +1,17 @@
-{ lib, stdenv, fetchFromGitHub, Carbon }:
+{ lib, stdenv, fetchFromGitHub, Carbon, Cocoa }:
 
 stdenv.mkDerivation rec {
   pname = "skhd";
-  version = "0.3.5";
+  version = "0.3.9";
 
   src = fetchFromGitHub {
     owner = "koekeishiya";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0x099979kgpim18r0vi9vd821qnv0rl3rkj0nd1nx3wljxgf7mrg";
+    sha256 = "fnkWws/g4BdHKDRhqoCpdPFUavOHdk8R7h7H1dAdAYI=";
   };
 
-  buildInputs = [ Carbon ];
+  buildInputs = [ Carbon Cocoa ];
 
   makeFlags = [ "BUILD_PATH=$(out)/bin" ];
 
