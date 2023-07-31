bff917a3ed37b1f9e705b5c07210acd295691770
R. Ryantm <ryantm-bot@ryantm.com>
2023-07-31 23:59:44 +0000

    AusweisApp2: 1.26.5 -> 1.26.7

--- pkgs/applications/misc/ausweisapp2/default.nix
+++ pkgs/applications/misc/ausweisapp2/default.nix
@@ -3,13 +3,13 @@
 
 mkDerivation rec {
   pname = "AusweisApp2";
-  version = "1.26.5";
+  version = "1.26.7";
 
   src = fetchFromGitHub {
     owner = "Governikus";
     repo = "AusweisApp2";
     rev = version;
-    hash = "sha256-6/acpPMHsMj/8GkLGNE8yg/apt8ynhN3O35lkA95HYQ=";
+    hash = "sha256-i9hfmMp0pEqtIeKc1mcyINXetzD/33aM0utL8nomVcg=";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
