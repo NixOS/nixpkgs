dc969314d833554b870ee822c5cae8d54870cccd
zowoq <59103226+zowoq@users.noreply.github.com>
2023-08-02 09:17:02 +1000

    go_1_19: 1.19.11 -> 1.19.12
    Changelog: https://go.dev/doc/devel/release#go1.19
    (cherry picked from commit fa88f4a8be9f07dba272894a2ddbdd57e9f5b204)

--- pkgs/development/compilers/go/1.19.nix
+++ pkgs/development/compilers/go/1.19.nix
@@ -47,11 +47,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "go";
-  version = "1.19.11";
+  version = "1.19.12";
 
   src = fetchurl {
     url = "https://go.dev/dl/go${version}.src.tar.gz";
-    hash = "sha256-4lyaty2BEUK39B/22lFl/sLRvl/uw+8sZrwL3stDFIk=";
+    hash = "sha256-7l1Q4Kf9dLobE3y4eWCaqu+YgL9ytdF0IQDjiucrtVc=";
   };
 
   strictDeps = true;
