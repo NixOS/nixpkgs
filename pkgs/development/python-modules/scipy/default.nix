d61583bbc8fa6cdf4796b0bfd6003292ab8d1be8
Doron Behar <doron.behar@gmail.com>
2023-08-01 17:25:34 +0300

    python3.pkgs.scipy: set HOME to a writeable directory
    Seems to be required for aarch64-darwin, but doesn't hurt any other
    platform to have this.

--- pkgs/development/python-modules/scipy/default.nix
+++ pkgs/development/python-modules/scipy/default.nix
@@ -113,7 +113,7 @@ in buildPythonPackage {
     # library where these files are cached. See also:
     # https://github.com/scipy/scipy/pull/18518#issuecomment-1562350648 And at:
     # https://github.com/scipy/scipy/pull/17965#issuecomment-1560759962
-    export XDG_CACHE_HOME=$PWD; mkdir scipy-data
+    export XDG_CACHE_HOME=$PWD; export HOME=$(mktemp -d); mkdir scipy-data
   '' + (lib.concatStringsSep "\n" (lib.mapAttrsToList (d: dpath:
     # Actually copy the datasets
     "cp ${dpath} scipy-data/${d}.dat"
