80d4b03586abb6a8b5aba586ee185191efebde45
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-01 02:26:13 +0000

    assemblyscript: 0.27.5 -> 0.27.6

--- pkgs/development/compilers/assemblyscript/default.nix
+++ pkgs/development/compilers/assemblyscript/default.nix
@@ -5,13 +5,13 @@
 
 buildNpmPackage rec {
   pname = "assemblyscript";
-  version = "0.27.5";
+  version = "0.27.6";
 
   src = fetchFromGitHub {
     owner = "AssemblyScript";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-3dWIYkiAA61xbUXJGmb/amjHmyXYoy16lLIAZR4sD5k=";
+    sha256 = "sha256-jN8P3EL3giDIZSExkjm5ZUwgkN3EIQhVpMqfx273yZU=";
   };
 
   npmDepsHash = "sha256-9ILa1qY2GpP2RckcZYcCMmgCwdXIImOm+D8nldeoQL8=";
