a8b0580c84e411f5d7dc2ad765d896a29edcbff0
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-08-02 13:45:35 +0200

    cockroachdb-bin: add aarch64-linux support

--- pkgs/servers/sql/cockroachdb/cockroachdb-bin.nix
+++ pkgs/servers/sql/cockroachdb/cockroachdb-bin.nix
@@ -6,21 +6,25 @@
 
 let
   version = "23.1.7";
-  name = "cockroachdb-${version}";
+  name = "cockroachdb";
 
   # For several reasons building cockroach from source has become
-  # nearly imposible. See https://github.com/NixOS/nixpkgs/pull/152626
+  # nearly impossible. See https://github.com/NixOS/nixpkgs/pull/152626
   # Therefore we use the pre-build release binary and wrap it with buildFHSUserEnv to
   # work on nix.
   # You can generate the hashes with
   # nix flake prefetch <url>
-  src = fetchzip (lib.getAttr stdenv.system {
-    x86_64-linux = {
-      name = "cockroachdb-release-${version}";
-      url = "https://binaries.cockroachdb.com/cockroach-v${version}.linux-amd64.tgz";
-      sha256 = "sha256-FL/zDrl+QstBp54LE9/SbIfSPorneGZSef6dcOQJbSo=";
+  srcs = {
+    aarch64-linux = fetchzip {
+      url = "https://binaries.cockroachdb.com/cockroach-v${version}.linux-arm64.tgz";
+      hash = "sha256-73qJL3o328NckH6POXv+AUvlAJextb31Vs8NGdc8dwE=";
     };
-  });
+    x86_64-linux = fetchzip {
+      url = "https://binaries.cockroachdb.com/cockroach-v${version}.linux-amd64.tgz";
+      hash = "sha256-FL/zDrl+QstBp54LE9/SbIfSPorneGZSef6dcOQJbSo=";
+    };
+  };
+  src = srcs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
 in
 buildFHSEnv {
@@ -32,7 +36,7 @@ buildFHSEnv {
     homepage = "https://www.cockroachlabs.com";
     description = "A scalable, survivable, strongly-consistent SQL database";
     license = licenses.bsl11;
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "aarch64-linux" "x86_64-linux" ];
     maintainers = with maintainers; [ rushmorem thoughtpolice neosimsim ];
   };
 }
