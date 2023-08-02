dafe5f9f1d7a18a1bcc7c294b3573adf68cd7463
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-08-01 23:34:49 -0700

    python3.pkgs.pythonRelaxDepsHook: don't propagate wheel
    The usage of wheel should be restricted to the hook. I discovered this
    when trying to remove wheel from the Python bootstrap. Some packages
    that needed wheel did not need it added explicitly because they use this
    hook. This implicit change to the dependency tree shouldn't happen (even
    though it is mostly harmless).

--- pkgs/development/interpreters/python/hooks/python-relax-deps-hook.sh
+++ pkgs/development/interpreters/python/hooks/python-relax-deps-hook.sh
@@ -84,7 +84,8 @@ pythonRelaxDepsHook() {
 
     # We generally shouldn't have multiple wheel files, but let's be safer here
     for wheel in "$pkg_name"*".whl"; do
-        @pythonInterpreter@ -m wheel unpack --dest "$unpack_dir" "$wheel"
+        PYTHONPATH="@wheel@/@pythonSitePackages@:$PYTHONPATH" \
+            @pythonInterpreter@ -m wheel unpack --dest "$unpack_dir" "$wheel"
         rm -rf "$wheel"
 
         _pythonRelaxDeps "$metadata_file"
@@ -95,7 +96,8 @@ pythonRelaxDepsHook() {
             cat "$unpack_dir/$pkg_name/$pkg_name.dist-info/METADATA"
         fi
 
-        @pythonInterpreter@ -m wheel pack "$unpack_dir/$pkg_name"
+        PYTHONPATH="@wheel@/@pythonSitePackages@:$PYTHONPATH" \
+            @pythonInterpreter@ -m wheel pack "$unpack_dir/$pkg_name"*
     done
 
     # Remove the folder since it will otherwise be in the dist output.
