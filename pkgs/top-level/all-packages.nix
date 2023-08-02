ac6aa1bc8f73f7388415f91dc7c8f6e40069bf65
Emery Hemingway <ehmry@posteo.net>
2023-08-02 14:46:22 +0100

    mosdepth: build with Nim-2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38594,7 +38594,7 @@ with pkgs;
     inherit (llvmPackages) openmp;
   };
 
-  mosdepth = callPackage ../applications/science/biology/mosdepth { };
+  mosdepth = nim2Packages.callPackage ../applications/science/biology/mosdepth { };
 
   niftyreg = callPackage ../applications/science/biology/niftyreg { };
 
