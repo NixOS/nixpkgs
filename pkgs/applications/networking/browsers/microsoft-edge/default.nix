34061e987cc445d9c5cd34f86aeb61e219a2a1c0
Leo <leo@steamr.com>
2023-08-02 14:23:53 -0600

    Update stable sha256 hash to SRI hash

--- pkgs/applications/networking/browsers/microsoft-edge/default.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/default.nix
@@ -3,7 +3,7 @@
     channel = "stable";
     version = "115.0.1901.188";
     revision = "1";
-    sha256 = "sha256-0xf2d2wxc2hjxz070nkrbgsszylwhck6x655ginjmh0qm76kf4wr";
+    sha256 = "sha256-mRM3zakYwCptfKWYbiaDnPqv9Vt5WnDA7xIK1rlownU=";
   };
   beta = import ./browser.nix {
     channel = "beta";
