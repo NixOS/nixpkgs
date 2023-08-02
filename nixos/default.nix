e2d824365f871906940a51f78be1b7f505367123
Adam Joseph <adam@westernsemico.com>
2023-08-01 19:29:35 -0700

    nixos/default.nix: accept build and host arguments
    It is now possible to cross compile the *entire* NixOS installer
    image (see https://github.com/NixOS/nixpkgs/pull/194153).
    
    Unfortunately the incantation for doing this is quite awkward and
    cryptic (it took me many tries to come up with something that
    worked!):
    
    ```
    nix-build nixos \
      -A config.system.build.sdImage \
      --arg configuration '{config,lib,pkgs,...}@args: (import ./nixos/modules/installer/sd-card/sd-image-mips64el.nix args) // { nixpkgs.hostPlatform = lib.systems.examples.octeon; nixpkgs.buildPlatform = builtins.currentSystem; }'
    ```
    
    That last line is pretty un-user-friendly.
    
    Let's give *optional* command line arguments `build` and `host`
    (just like `./configure`) to `nixos/default.nix`, so users can
    instead type something more readable:
    
    ```
    nix-build nixos \
      -A config.system.build.sdImage \
      -I nixos-config=nixos/modules/installer/sd-card/sd-image-mips64el.nix \
      --arg host '(import ./lib).systems.examples.octeon'
    ```
    
    Note that `--arg system` will request a native build, which will
    fail unless you are using remote builders.  Also, it will never
    produce a cross-compiled build.  Using `--arg host` and `--arg
    build` provides more intuitive behavior, and is the only way to get
    cross-compiled builds.

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,12 +1,31 @@
-{ configuration ? import ./lib/from-env.nix "NIXOS_CONFIG" <nixos-config>
-, system ? builtins.currentSystem
+{ lib ? import ../lib
+, configuration ? import ./lib/from-env.nix "NIXOS_CONFIG" <nixos-config>
+
+# to cross compile using the local machine as the buildPlatform, you
+# only need to set `host` (you don't need to set `build`)
+, build ? if host != null then builtins.currentSystem else null
+, host ? null
+
+# If you set `system` then nixpkgs will use that value as *both* the
+# `build` *and* the `host`.  The only time you want this is if you
+# have remote builders belonging to a different platform and you
+# want them to do native builds for that platform.  In general using
+# `build` and `host` provides more intuitive behavior, and you must
+# use those (rather than `system`) if you want to cross compile.
+, system ? if host==null && build==null then builtins.currentSystem else null
 }:
 
 let
 
   eval = import ./lib/eval-config.nix {
     inherit system;
-    modules = [ configuration ];
+    modules = [
+      configuration
+    ] ++ lib.optionals (build != null) [
+      { nixpkgs.buildPlatform = lib.mkDefault build; }
+    ] ++ lib.optionals (host != null || build != null) [
+      { nixpkgs.hostPlatform = lib.mkDefault (if host==null then build else host); }
+    ];
   };
 
 in
