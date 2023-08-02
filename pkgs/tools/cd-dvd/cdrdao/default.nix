8d08ce1f93bfec78965195851b874abf904f8cc9
Emily <vcs@emily.moe>
2023-08-02 04:05:21 +0100

    cdrdao: add patch to fix uninitialized variable

--- pkgs/tools/cd-dvd/cdrdao/default.nix
+++ pkgs/tools/cd-dvd/cdrdao/default.nix
@@ -46,6 +46,13 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://github.com/cdrdao/cdrdao/commit/105d72a61f510e3c47626476f9bbc9516f824ede.patch";
       hash = "sha256-NVIw59CSrc/HcslhfbYQNK/qSmD4QbfuV8hWYhWelX4=";
     })
+
+    # Fix undefined behaviour caused by uninitialized variable
+    # https://github.com/cdrdao/cdrdao/pull/21
+    (fetchpatch {
+      url = "https://github.com/cdrdao/cdrdao/commit/251a40ab42305c412674c7c2d391374d91e91c95.patch";
+      hash = "sha256-+nGlWw5rgc5Ns2l+6fQ4Hp2LbhO4R/I95h9WGIh/Ebw=";
+    })
   ];
 
   # we have glibc/include/linux as a symlink to the kernel headers,
