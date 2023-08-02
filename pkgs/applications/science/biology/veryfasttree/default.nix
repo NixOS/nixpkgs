9a772d76581cd1573e7c213a4340587197693226
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-08-02 18:51:11 +0200

    veryfasttree: use `finalAttrs` pattern

--- pkgs/applications/science/biology/veryfasttree/default.nix
+++ pkgs/applications/science/biology/veryfasttree/default.nix
@@ -1,13 +1,18 @@
-{ lib, stdenv, fetchFromGitHub, cmake, llvmPackages}:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, llvmPackages
+}:
 
-stdenv.mkDerivation rec {
-  pname   = "veryfasttree";
+stdenv.mkDerivation (finalAttrs: {
+  pname = "veryfasttree";
   version = "4.0.2";
 
   src = fetchFromGitHub {
     owner = "citiususc";
-    repo = pname;
-    rev = "v${version}";
+    repo = "veryfasttree";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-JMBhSxfGO3qz7Yl4s5r6zWHFefXGzu0ktEJdRUh/Uqg=";
   };
 
@@ -15,14 +20,18 @@ stdenv.mkDerivation rec {
   buildInputs = lib.optional stdenv.cc.isClang llvmPackages.openmp;
 
   installPhase = ''
+    runHook preInstall
+
     install -m755 -D VeryFastTree $out/bin/VeryFastTree
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Speeding up the estimation of phylogenetic trees for large alignments through parallelization and vectorization strategies";
-    license     = licenses.gpl3Plus;
-    homepage    = "https://github.com/citiususc/veryfasttree";
-    maintainers = with maintainers; [ thyol ];
-    platforms   = platforms.all;
+    homepage = "https://github.com/citiususc/veryfasttree";
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ thyol ];
+    platforms = lib.platforms.all;
   };
-}
+})
