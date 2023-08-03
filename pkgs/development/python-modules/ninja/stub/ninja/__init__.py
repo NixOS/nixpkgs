d36556b8a5db361933833d33876f4e8a815d1e0d
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-08-03 06:06:34 -0700

    python3.pkgs.ninja-python: replace with a stub implementation
    Instead of packaging the upstream package, which downloads Ninja from
    the web, we can stub it out to use ninja from nixpkgs.

---
+++ pkgs/development/python-modules/ninja/stub/ninja/__init__.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+import sys
+
+def _program(name, args):
+    return subprocess.call([os.path.join('@BIN_DIR@', name)] + args, close_fds=False)
+
+def ninja():
+    raise SystemExit(_program('ninja', sys.argv[1:]))
