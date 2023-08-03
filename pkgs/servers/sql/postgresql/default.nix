51d2555ca4806cc17a3ddd25ac97b7836f42c16a
Tomas Drtina <tdrtina-ext@arista.com>
2023-08-03 17:03:01 +0200

    pkgs.postgresql: add python support

--- pkgs/servers/sql/postgresql/default.nix
+++ pkgs/servers/sql/postgresql/default.nix
@@ -9,6 +9,7 @@ let
       # This is important to obtain a version of `libpq` that does not depend on systemd.
       , enableSystemd ? (lib.versionAtLeast version "9.6" && !stdenv.isDarwin)
       , gssSupport ? with stdenv.hostPlatform; !isWindows && !isStatic, libkrb5
+      , withPython ? null
 
 
       # for postgreql.pkgs
@@ -45,6 +46,7 @@ let
       ++ lib.optionals lz4Enabled [ lz4 ]
       ++ lib.optionals enableSystemd [ systemd ]
       ++ lib.optionals gssSupport [ libkrb5 ]
+      ++ lib.optionals (withPython != null) [ withPython ]
       ++ lib.optionals (!stdenv.isDarwin) [ libossp_uuid ];
 
     nativeBuildInputs = [ makeWrapper ] ++ lib.optionals icuEnabled [ pkg-config ];
@@ -72,6 +74,7 @@ let
     ] ++ lib.optionals icuEnabled [ "--with-icu" ]
       ++ lib.optionals lz4Enabled [ "--with-lz4" ]
       ++ lib.optionals gssSupport [ "--with-gssapi" ]
+      ++ lib.optionals (withPython != null) [ "--with-python" ]
       ++ lib.optionals stdenv.hostPlatform.isRiscV [ "--disable-spinlocks" ];
 
     patches =
