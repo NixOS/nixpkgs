cf4c0f090fe55e3018164a15d81dd0a374dfd43c
natsukium <tomoya.otabi@gmail.com>
2023-08-03 22:46:46 +0900

    python310Packages.laszip: refactor
    - remove unused inputs
    - move pybind11 to nativeBuildInputs
    - set dontUseCmakeConfigure
    - add meta.changelog

--- pkgs/development/python-modules/laszip/default.nix
+++ pkgs/development/python-modules/laszip/default.nix
@@ -2,13 +2,10 @@
 , buildPythonPackage
 , fetchFromGitHub
 , scikit-build-core
-, distlib
-, pytestCheckHook
-, pyproject-metadata
-, pathspec
 , pybind11
 , cmake
 , LASzip
+, pythonOlder
 }:
 
 buildPythonPackage rec {
@@ -17,31 +14,27 @@ buildPythonPackage rec {
 
   format = "pyproject";
 
+  disabled = pythonOlder "3.7";
+
   src = fetchFromGitHub {
     owner = "tmontaigu";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ujKoUm2Btu25T7ZrSGqjRc3NR1qqsQU8OwHQDSx8grY=";
+    hash = "sha256-ujKoUm2Btu25T7ZrSGqjRc3NR1qqsQU8OwHQDSx8grY=";
   };
 
   nativeBuildInputs = [
+    cmake
+    pybind11
     scikit-build-core
     scikit-build-core.optional-dependencies.pyproject
-    cmake
   ];
+
+  dontUseCmakeConfigure = true;
 
   buildInputs = [
-    pybind11
     LASzip
   ];
-
-  checkInputs = [
-    pytestCheckHook
-  ];
-
-  preBuild = ''
-    cd ..
-  '';
 
   # There are no tests
   doCheck = false;
@@ -51,6 +44,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Unofficial bindings between Python and LASzip made using pybind11";
     homepage = "https://github.com/tmontaigu/laszip-python";
+    changelog = "https://github.com/tmontaigu/laszip-python/blob/${src.rev}/Changelog.md";
     license = licenses.mit;
     maintainers = with maintainers; [ matthewcroughan ];
   };
