8d9eca815eda60167b88f672b17f472e7bed1a1d
Laurent CaniBot <laurent@marigold.dev>
2023-08-03 15:04:04 +0000

    ligo: 0.69.0 -> 0.71.0

--- pkgs/development/compilers/ligo/default.nix
+++ pkgs/development/compilers/ligo/default.nix
@@ -15,12 +15,12 @@
 
 ocamlPackages.buildDunePackage rec {
   pname = "ligo";
-  version = "0.69.0";
+  version = "0.71.0";
   src = fetchFromGitLab {
     owner = "ligolang";
     repo = "ligo";
     rev = version;
-    sha256 = "sha256-Swt4uihsAtHVMkc0DxATwB8FvgxwtSJTN3E5cBtyXf8=";
+    sha256 = "sha256-eEH+OlPl82NI3vtGBqDh2Rkq0zgzKlMcFpXFTEvlfrg=";
     fetchSubmodules = true;
   };
 
