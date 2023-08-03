da269e3c82bfedf580b861d6a001f3992e5b6e05
Colin <colin@uninsane.org>
2023-08-03 11:55:40 +0000

    gupnp_1_6: 1.6.4 -> 1.6.5
    this fixes the build with meson 1.2.0, and nothing more:
    - <https://gitlab.gnome.org/GNOME/gupnp/-/commits/gupnp-1.6.5>

--- pkgs/development/libraries/gupnp/1.6.nix
+++ pkgs/development/libraries/gupnp/1.6.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gupnp";
-  version = "1.6.4";
+  version = "1.6.5";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gupnp/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-1sPQNYOET6UqvgAwQxhgB/DIQUX+OwD6slmVvtqb5Vo=";
+    hash = "sha256-Q33/lwFC6EBwh6iYVfcX4g0nydduBbTNUX32IcfYiM0=";
   };
 
   depsBuildBuild = [
