b3ba35694bae2a30f9a474f1a8e297485b31c383
Hannes Braun <hannes@hannesbraun.net>
2023-08-03 17:44:22 +0200

    airwindows-lv2: 20.0 -> 22.0

--- pkgs/applications/audio/airwindows-lv2/default.nix
+++ pkgs/applications/audio/airwindows-lv2/default.nix
@@ -1,13 +1,13 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, lv2 }:
+{ lib, stdenv, fetchFromSourcehut, meson, ninja, pkg-config, lv2 }:
 
 stdenv.mkDerivation rec {
   pname = "airwindows-lv2";
-  version = "20.0";
-  src = fetchFromGitHub {
-    owner = "hannesbraun";
+  version = "22.0";
+  src = fetchFromSourcehut {
+    owner = "~hannes";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-uflvUmUzOtF3BwiLfnd+qhz+ZYyn8AKvODFs599phhU=";
+    sha256 = "sha256-u62wLRrJ45ap981Q8JmMnanc8AWQb1MJHK32PEr10I4=";
   };
 
   nativeBuildInputs = [ meson ninja pkg-config ];
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Airwindows plugins (ported to LV2)";
-    homepage = "https://github.com/hannesbraun/airwindows-lv2";
+    homepage = "https://sr.ht/~hannes/airwindows-lv2";
     license = licenses.mit;
     maintainers = [ maintainers.magnetophon ];
     platforms = platforms.unix;
