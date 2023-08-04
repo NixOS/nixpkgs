e359bfc7aed86478900d0bf33b575fb20c997353
Lewis Arias <lewisgarias@outlook.com>
2023-08-04 16:42:10 -0400

    sm64ex-coop: unstable-2023-02-22 -> unstable-2023-06-25

--- pkgs/games/sm64ex/coop.nix
+++ pkgs/games/sm64ex/coop.nix
@@ -7,13 +7,13 @@
 
 callPackage ./generic.nix {
   pname = "sm64ex-coop";
-  version = "unstable-2023-02-22";
+  version = "unstable-2023-06-25";
 
   src = fetchFromGitHub {
     owner = "djoslin0";
     repo = "sm64ex-coop";
-    rev = "8746a503086793c87860daadfaeaaf0a31b2d6cf";
-    sha256 = "sha256-iwJsq0FN9npxveIoMiB7zL5j1V72IExtEpzGj6lwLXQ=";
+    rev = "4f62baebcce44b16a2bd7b0ee903577767e31154";
+    sha256 = "sha256-WlZ8es0JnywUBEWnxlZfcL6GSF4bwqc/rcG8BM15+Lo=";
   };
 
   extraNativeBuildInputs = [
@@ -32,6 +32,7 @@ callPackage ./generic.nix {
       mkdir -p $out/lib
       cp $src/lib/bass/libbass{,_fx}${sharedLib} $out/lib
       cp $src/lib/discordsdk/libdiscord_game_sdk${sharedLib} $out/lib
+      cp -r $src/lang $out/share/sm64ex/lang
     '';
 
   extraMeta = {
