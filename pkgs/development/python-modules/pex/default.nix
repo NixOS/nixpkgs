3c71742636b50b4c868773956563f854628e0c92
phaer <hello@phaer.org>
2023-08-04 10:16:01 +0200

    python310Packages.pex: 2.1.137 -> 2.1.141
    fixes https://github.com/NixOS/nixpkgs/issues/246448,
    replaces https://github.com/NixOS/nixpkgs/pull/206043

--- pkgs/development/python-modules/pex/default.nix
+++ pkgs/development/python-modules/pex/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "pex";
-  version = "2.1.137";
+  version = "2.1.141";
   format = "flit";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ywzmz2R1fdW6TzTEYHq0hfeQnmwkzUecoozlIgXw7es=";
+    hash = "sha256-EsIurZNgWslUciz5Pc2hj2F4tAJ8hQydRWnVIWdryDc=";
   };
 
   nativeBuildInputs = [
