50031b0693992868c6cb786d3c3ea514a939fd7b
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-08-03 22:18:51 -0700

    python3.pkgs.scikit-build-core: depend on top-level cmake

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11373,7 +11373,9 @@ self: super: with self; {
 
   scikit-build = callPackage ../development/python-modules/scikit-build { };
 
-  scikit-build-core = callPackage ../development/python-modules/scikit-build-core { };
+  scikit-build-core = callPackage ../development/python-modules/scikit-build-core {
+    inherit (pkgs) cmake;
+  };
 
   scikit-fmm = callPackage ../development/python-modules/scikit-fmm { };
 
