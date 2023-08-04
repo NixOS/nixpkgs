cde30ecca423378346f9c6cae78f699fe64755d7
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-04 22:05:59 +0200

    python311Packages.plexapi: update disabled

--- pkgs/development/python-modules/plexapi/default.nix
+++ pkgs/development/python-modules/plexapi/default.nix
@@ -12,7 +12,7 @@ buildPythonPackage rec {
   version = "4.15.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "pkkid";
