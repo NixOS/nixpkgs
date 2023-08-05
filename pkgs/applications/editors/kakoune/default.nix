6bc5f7dd871dd10dca313c78b5ba84faed3fdae4
Masanori Ogino <167209+omasanori@users.noreply.github.com>
2023-08-06 00:16:22 +0900

    kakoune-unwrapped: 2022.10.31 -> 2023.08.05
    Version 2023.07.29 [1] contains feature additions and breaking changes
    to the default key bindings.
    
    Version 2023.08.05 [2] contains a bug fix.
    
    [1]: https://github.com/mawww/kakoune/releases/tag/v2023.07.29
    [2]: https://github.com/mawww/kakoune/releases/tag/v2023.08.05
    
    Changes: https://github.com/mawww/kakoune/compare/v2022.10.31...v2023.08.05
    Signed-off-by: Masanori Ogino <167209+omasanori@users.noreply.github.com>

--- pkgs/applications/editors/kakoune/default.nix
+++ pkgs/applications/editors/kakoune/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "kakoune-unwrapped";
-  version = "2022.10.31";
+  version = "2023.08.05";
   src = fetchFromGitHub {
     repo = "kakoune";
     owner = "mawww";
     rev = "v${version}";
-    sha256 = "sha256-vmzGaGl0KSjseSD/s6DXxvMUTmAle+Iv/ZP9llaFnXk=";
+    sha256 = "sha256-RR3kw39vEjsg+6cIY6cK2i3ecGHlr1yzuBKaDtGlOGo=";
   };
   makeFlags = [ "debug=no" "PREFIX=${placeholder "out"}" ];
 
