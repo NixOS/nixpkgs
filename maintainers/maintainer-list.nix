21a524d45a55ced3be20a7b96bafcf931dd8527c
Udo Sauer <udo.sauer@bevuta.com>
2023-08-06 17:40:27 +0200

    maintainers: add fernsehmuell

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5488,6 +5488,13 @@
       }
     ];
   };
+  fernsehmuell = {
+    email = "fernsehmuel@googlemail.com";
+    matrix = "@fernsehmuell:matrix.org";
+    github = "fernsehmuell";
+    githubId = 5198058;
+    name = "Udo Sauer";
+  };
   ffinkdevs = {
     email = "fink@h0st.space";
     github = "ffinkdevs";
