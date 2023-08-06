d60d5eab39ae7eb9873d240ecbe9975a77d2b079
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-06 21:13:30 +0200

    python310Packages.pyrtlsdr: add format
    - disable on unsupported Python releases

--- pkgs/development/python-modules/pyrtlsdr/default.nix
+++ pkgs/development/python-modules/pyrtlsdr/default.nix
@@ -4,18 +4,24 @@
 , fetchpatch
 , rtl-sdr
 , setuptools
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pyrtlsdr";
   version = "0.3.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-+z5YO6BzuGHo4LxeYvZvBzZekUf102SR3krWLyPkU2I=";
   };
 
-  propagatedBuildInputs = [ setuptools ];
+  propagatedBuildInputs = [
+    setuptools
+  ];
 
   postPatch = ''
     sed "s|driver_files =.*|driver_files = ['${rtl-sdr}/lib/librtlsdr.so']|" -i rtlsdr/librtlsdr.py
