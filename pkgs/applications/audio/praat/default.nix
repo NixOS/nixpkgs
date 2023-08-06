7f565d9c0dbf2097caddab114a0b162727bb39da
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-06 09:40:40 +0000

    praat: 6.3.10 -> 6.3.14

--- pkgs/applications/audio/praat/default.nix
+++ pkgs/applications/audio/praat/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "praat";
-  version = "6.3.10";
+  version = "6.3.14";
 
   src = fetchFromGitHub {
     owner = "praat";
     repo = "praat";
     rev = "v${version}";
-    sha256 = "sha256-wnw8GKMukiraZgMMzd3S2NldC/cnRSILNo+D1Rqhr4k=";
+    sha256 = "sha256-HN4w7n0nh7voL/QKhGQwCcGfzn+nyFOwluESlP3B6VM=";
   };
 
   configurePhase = ''
