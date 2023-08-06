ef47fc0055f3199acabcd0980ee41d3be82a8231
natsukium <tomoya.otabi@gmail.com>
2023-08-06 20:21:03 +0900

    python310Packages.pydeck: 0.7.1 -> 0.8.0

--- pkgs/development/python-modules/pydeck/default.nix
+++ pkgs/development/python-modules/pydeck/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, fetchpatch
 , ipykernel
 , ipywidgets
 , pythonOlder
@@ -14,23 +13,14 @@
 
 buildPythonPackage rec {
   pname = "pydeck";
-  version = "0.7.1";
+  version = "0.8.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
-  patches = [
-    # fixes build with latest setuptools
-    (fetchpatch {
-      url = "https://github.com/visgl/deck.gl/commit/9e68f73b28aa3bf0f2a887a4d8ccd2dc35677039.patch";
-      hash = "sha256-YVVoVbVdY5nV+17OwYIs9AwKGyzgKZHi655f4BLcdMU=";
-      stripLen = 2;
-    })
-  ];
-
   src = fetchPypi {
     inherit pname version;
-    sha256 = "907601c99f7510e16d27d7cb62bfa145216d166a2b5c9c50cfe2b65b032ebd2e";
+    hash = "sha256-B+3egz98/O9nSRJDURlap9zSRmPUkJ/XiY29C2+8Aew=";
   };
 
   pythonImportsCheck = [ "pydeck" ];
