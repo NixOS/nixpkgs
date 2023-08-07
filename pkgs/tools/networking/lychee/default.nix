688f3b802dd25efb422a2653ce2227463be3d668
Gaël Reyrol <me@gaelreyrol.dev>
2023-08-07 14:24:24 +0200

    lychee: 0.11.1 -> 0.13.0

--- pkgs/tools/networking/lychee/default.nix
+++ pkgs/tools/networking/lychee/default.nix
@@ -5,20 +5,27 @@
 , pkg-config
 , openssl
 , Security
+, testers
+, lychee
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "lychee";
-  version = "0.11.1";
+  version = "0.13.0";
 
   src = fetchFromGitHub {
     owner = "lycheeverse";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-fOD28O6ycRIniQz841PGJzEFGtYord/y44mdqyAmNDg=";
+    sha256 = "sha256-JUyoOtlypDWK6HxsonVzbfQAdcXk728a8gVI/5GI2fs=";
   };
 
-  cargoHash = "sha256-r089P2VOeIIW0FjkO4oqVXbrxDND4loagVfVMm5EtaE=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "criterion-0.4.0" = "sha256-0EKLRdxbH2czkZjmuaYLzkTBU687y6Iw9yqNV2TbsDw=";
+    };
+  };
 
   nativeBuildInputs = [ pkg-config ];
 
@@ -28,9 +35,16 @@ rustPlatform.buildRustPackage rec {
   # Disabled because they currently fail
   doCheck = false;
 
+  passthru.tests.version = testers.testVersion {
+    inherit version;
+    package = lychee;
+    command = "lychee --version";
+  };
+
   meta = with lib; {
     description = "A fast, async, resource-friendly link checker written in Rust.";
     homepage = "https://github.com/lycheeverse/lychee";
+    changelog = "https://github.com/lycheeverse/lychee/releases/tag/v${version}";
     license = with licenses; [ asl20 mit ];
     maintainers = with maintainers; [ tuxinaut ];
   };
