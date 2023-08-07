f44a81a41625265d276d26c121ce7ab23a67888a
Bobby Rong <rjl931189261@126.com>
2023-08-07 18:58:42 +0800

    libadwaita: 1.3.4 → 1.4.beta
    https://gitlab.gnome.org/GNOME/libadwaita/-/compare/1.3.4...1.4.beta
    
    New appstream dependency:
    
    https://github.com/GNOME/libadwaita/commit/876672aac1191e4ac26d835cb75761ce07ef69ec

--- pkgs/development/libraries/libadwaita/default.nix
+++ pkgs/development/libraries/libadwaita/default.nix
@@ -8,6 +8,7 @@
 , sassc
 , vala
 , gobject-introspection
+, appstream
 , fribidi
 , glib
 , gtk4
@@ -20,7 +21,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libadwaita";
-  version = "1.3.4";
+  version = "1.4.beta";
 
   outputs = [ "out" "dev" "devdoc" ];
   outputBin = "devdoc"; # demo app
@@ -30,7 +31,7 @@ stdenv.mkDerivation rec {
     owner = "GNOME";
     repo = "libadwaita";
     rev = version;
-    hash = "sha256-NBYIDW0sphmBT2cIB2CPsCJrjRsINxWpumJbQK5RjU8=";
+    hash = "sha256-97lhsQHdSM4ervJsD6Q65JGHhycJOQs1NeFRhE7ecIo=";
   };
 
   depsBuildBuild = [
@@ -54,6 +55,7 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
+    appstream
     fribidi
   ] ++ lib.optionals stdenv.isDarwin [
     AppKit
