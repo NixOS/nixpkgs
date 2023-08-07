06f8ee14c540c9395596f9c41ff3e04b653a0450
Mostly Void <7rat13@gmail.com>
2023-08-07 15:34:52 +0300

    ego: init at 1.1.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5025,6 +5025,8 @@ with pkgs;
 
   egglog = callPackage ../applications/science/logic/egglog { };
 
+  ego = callPackage ../tools/misc/ego { };
+
   ekam = callPackage ../development/tools/build-managers/ekam { };
 
   eksctl = callPackage ../tools/admin/eksctl { };
