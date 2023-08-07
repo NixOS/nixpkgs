589b75bdc8038edce0486829ff556004881eee70
Christoph Heiss <christoph@c8h4.io>
2023-08-08 00:43:26 +0200

    sourcehut: disable IPv6 completely for tests
    Signed-off-by: Christoph Heiss <christoph@c8h4.io>

--- nixos/tests/sourcehut.nix
+++ nixos/tests/sourcehut.nix
@@ -126,6 +126,7 @@ in
     virtualisation.diskSize = 4 * 1024;
     virtualisation.memorySize = 2 * 1024;
     networking.domain = domain;
+    networking.enableIPv6 = false;
     networking.extraHosts = ''
       ${config.networking.primaryIPAddress} builds.${domain}
       ${config.networking.primaryIPAddress} git.${domain}
