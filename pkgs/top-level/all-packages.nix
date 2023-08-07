cd804caf4899fa3d1dd364e8349b978eb694f440
mingchuan <ming@culpring.com>
2023-08-06 23:29:17 -0700

    zug: init at 0.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25648,6 +25648,8 @@ with pkgs;
 
   zix = callPackage ../development/libraries/audio/zix { };
 
+  zug = callPackage ../development/libraries/zug { };
+
   zz = callPackage ../development/compilers/zz { };
 
   zziplib = callPackage ../development/libraries/zziplib { };
