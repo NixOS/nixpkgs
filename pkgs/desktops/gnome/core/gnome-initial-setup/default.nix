3bdcdae69dbb0b9f66a53991ec636ef3ccbdbe8c
Bobby Rong <rjl931189261@126.com>
2023-08-07 07:54:29 +0000

    gnome.gnome-initial-setup: 44.0 → 45.beta
    https://gitlab.gnome.org/GNOME/gnome-initial-setup/-/compare/44.0...45.beta
    
    Install dconf presets: https://github.com/GNOME/gnome-initial-setup/commit/0c607f104e7542b95e33716134da65cb03093e12
    
    Use tecla: https://github.com/GNOME/gnome-initial-setup/commit/3dcb8709ba18b0ed1838c489638a736cddab7c7c
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-initial-setup/default.nix
+++ pkgs/desktops/gnome/core/gnome-initial-setup/default.nix
@@ -2,6 +2,7 @@
 , lib
 , fetchurl
 , substituteAll
+, dconf
 , gettext
 , meson
 , ninja
@@ -32,27 +33,29 @@
 , libadwaita
 , libnma-gtk4
 , tzdata
-, libgnomekbd
+, gnome-tecla
 , gsettings-desktop-schemas
 }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-initial-setup";
-  version = "44.0";
+  version = "45.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "WTz8bcj4KphnG5TANbl9vojvVucIeAsq0dIyTk0Eu/8=";
+    sha256 = "LK9/kq/ojZ2Hw2sc3i4zS04A6XO0UFg5Omo8EPUOBmY=";
   };
 
   patches = [
     (substituteAll {
       src = ./0001-fix-paths.patch;
-      inherit tzdata libgnomekbd;
+      inherit tzdata;
+      tecla = gnome-tecla;
     })
   ];
 
   nativeBuildInputs = [
+    dconf
     gettext
     meson
     ninja
