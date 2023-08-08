5f0e31c28d0fdae65fd54e1674a749aeed9420ad
Benjamin Isbarn <benjamin.isbarn@gmail.com>
2023-08-08 10:10:04 +0200

    photoprism: 230615-90a18f6e7 -> 230719-73fa7bbe8

--- pkgs/servers/photoprism/backend.nix
+++ pkgs/servers/photoprism/backend.nix
@@ -19,7 +19,7 @@ buildGoModule rec {
     substituteInPlace internal/commands/passwd.go --replace '/bin/stty' "${coreutils}/bin/stty"
   '';
 
-  vendorSha256 = "sha256-q3Jsc1wP7ahKSaOrcLiuatWRrXnpgPjqzdjr2dydSVs=";
+  vendorSha256 = "sha256-gg/vIekHnoABucYqFDfo8574waN4rP7nkT57U3Gil5I=";
 
   subPackages = [ "cmd/photoprism" ];
 
