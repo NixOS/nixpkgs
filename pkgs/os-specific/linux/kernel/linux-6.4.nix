59d581a608b2d930d241b0c93cc87013b47baaf5
K900 <me@0upti.me>
2023-08-09 00:15:14 +0300

    linux: 6.4.8 -> 6.4.9

--- pkgs/os-specific/linux/kernel/linux-6.4.nix
+++ pkgs/os-specific/linux/kernel/linux-6.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.4.8";
+  version = "6.4.9";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "1djqqhw542jphqsicpbn1259sgw9pwd08wwkdch31nw4kvhk97y5";
+    sha256 = "17ycanx738bgxg5wn536kbad4zzvj5ngygp9qhvz76draaca5f5q";
   };
 } // (args.argsOverride or { }))
