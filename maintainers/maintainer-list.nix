4eaf775dc14c70095b99050e8a78188b87957307
Dave Conroy <dave@tiredofit.ca>
2023-08-08 07:50:03 -0700

    maintainers: add tiredofit

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16945,6 +16945,13 @@
     githubId = 1292007;
     name = "Sébastien Maccagnoni";
   };
+  tiredofit = {
+    email = "dave@tiredofit.ca";
+    github = "tiredofit";
+    githubId = 23528985;
+    name = "Dave Conroy";
+    matrix = "@dave:tiredofit.ca";
+  };
   tirex = {
     email = "szymon@kliniewski.pl";
     name = "Szymon Kliniewski";
