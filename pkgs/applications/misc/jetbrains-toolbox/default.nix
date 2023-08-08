1c3d9b093bde93d81c4f713519f3bda29bcef6f6
Anatolii Popov <anatolii.popov@aiven.io>
2023-08-08 11:55:11 +0300

    jetbrains-toolbox: 1.28.1.15219 -> 2.0.0.16559

--- pkgs/applications/misc/jetbrains-toolbox/default.nix
+++ pkgs/applications/misc/jetbrains-toolbox/default.nix
@@ -10,11 +10,11 @@
 }:
 let
   pname = "jetbrains-toolbox";
-  version = "1.28.1.15219";
+  version = "2.0.0.16559";
 
   src = fetchzip {
     url = "https://download.jetbrains.com/toolbox/jetbrains-toolbox-${version}.tar.gz";
-    sha256 = "sha256-4P73MC5Go8wLACBtjh1y3Ao0czE/3hsSI4728mNjKxA=";
+    sha256 = "sha256-z+udyilfVwcVnaFhp6GYHCXqh1d7T5fj/TbbjMoen2I=";
     stripRoot = false;
   };
 
