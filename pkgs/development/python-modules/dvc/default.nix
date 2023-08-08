542e1dd9ac612bb200e3d1e84f1549b02c96fe46
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-08 12:15:44 +0200

    python310Packages.dvc: 3.8.1 -> 3.13.2
    Diff: https://github.com/iterative/dvc/compare/refs/tags/3.8.1...3.13.2
    
    Changelog: https://github.com/iterative/dvc/releases/tag/3.13.2

--- pkgs/development/python-modules/dvc/default.nix
+++ pkgs/development/python-modules/dvc/default.nix
@@ -55,14 +55,14 @@
 
 buildPythonPackage rec {
   pname = "dvc";
-  version = "3.8.1";
+  version = "3.13.2";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "iterative";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-ZHB2XAYjPwhCWHqq2Kro4CzDW34YBaknv9xBotCDkOw=";
+    hash = "sha256-vUL54pkm+3DMg6Mf8vRdAKp4WbV6MzEoqHWL9Gn/HDU=";
   };
 
   pythonRelaxDeps = [
