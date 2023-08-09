aa38e61ec39f3ebeaa06938f6d09b01e06b31b43
Basil Keeler <basil.keeler@outlook.com>
2023-08-09 13:17:02 -0500

    maintainers: add totalchaos

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17142,6 +17142,12 @@
     githubId = 27586264;
     name = "Tobias Schmidt";
   };
+  totalchaos = {
+    email = "basil.keeler@outlook.com";
+    github = "totalchaos05";
+    githubId = 70387628;
+    name = "Basil Keeler";
+  };
   totoroot = {
     name = "Matthias Thym";
     email = "git@thym.at";
