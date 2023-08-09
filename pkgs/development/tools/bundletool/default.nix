ec9fa5d58038b88aa3674c26f946f3ffb5cfa0a9
Mario Rodas <marsam@users.noreply.github.com>
2023-08-09 04:20:00 +0000

    bundletool: add sourceProvenance to meta

--- pkgs/development/tools/bundletool/default.nix
+++ pkgs/development/tools/bundletool/default.nix
@@ -23,6 +23,7 @@ stdenvNoCC.mkDerivation rec {
     description = "Command-line tool to manipulate Android App Bundles";
     homepage = "https://developer.android.com/studio/command-line/bundletool";
     changelog = "https://github.com/google/bundletool/releases/tag/${version}";
+    sourceProvenance = with sourceTypes; [ binaryBytecode ];
     maintainers = with maintainers; [ marsam ];
     platforms = jre_headless.meta.platforms;
     license = licenses.asl20;
