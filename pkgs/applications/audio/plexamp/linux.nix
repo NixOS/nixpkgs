378ade7f855970c718851aa4d1fe179387300c8b
klchen0112 <klchen0112@gmail.com>
2023-08-09 22:24:25 +0800

    plexamp: use sha256 in linux installation

--- pkgs/applications/audio/plexamp/linux.nix
+++ pkgs/applications/audio/plexamp/linux.nix
@@ -1,32 +1,26 @@
-{ lib, fetchurl, appimageTools, pkgs, pname, version, meta, ... }:
+{ lib, fetchurl, appimageTools, pkgs, pname, version, meta }:
 
 let
   inherit pname version meta;
-  name = "${pname}-${version}";
   src = fetchurl {
     url = "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-${version}.AppImage";
-    name = "${pname}-${version}.AppImage";
-    sha512 = "7NQmH42yzItReQ5WPdcbNPr/xed74H4UyDNlX5PGmoJRBpV1RdeuW1KRweIWfYNhw0KeqULVTjr/aCggoWp4WA==";
+    sha256 = "sha256-L0088wEhLIWwEB01E85tChRjW4zykYZSsVbLvsvcYHI=";
   };
 
   appimageContents = appimageTools.extractType2 {
-    inherit name pname version src;
+    inherit pname version src;
   };
 in
 appimageTools.wrapType2 {
-  inherit name pname version src meta;
+  inherit pname version src meta;
 
   multiArch = false; # no 32bit needed
-  extraPkgs = pkgs: appimageTools.defaultFhsEnvArgs.multiPkgs pkgs ++ [ pkgs.bash ];
 
   extraInstallCommands = ''
-    ln -s $out/bin/${pname}-${version} $out/bin/${pname}
     install -m 444 -D ${appimageContents}/plexamp.desktop $out/share/applications/plexamp.desktop
     install -m 444 -D ${appimageContents}/plexamp.png \
       $out/share/icons/hicolor/512x512/apps/plexamp.png
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' 'Exec=${pname}'
+    substituteInPlace $out/share/applications/plexamp.desktop \
+      --replace 'Exec=AppRun' 'Exec=plexamp-${version}'
   '';
-
-  passthru.updateScript = ./update-plexamp.sh;
 }
