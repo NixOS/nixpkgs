5883bfbaac10fb4afd1d97adba798a7acc9dbe8b
Mario Rodas <marsam@users.noreply.github.com>
2023-08-09 04:20:00 +0000

    nodejs_16: 16.20.1 -> 16.20.2
    Changelog: https://github.com/nodejs/node/releases/tag/v16.20.2
    (cherry picked from commit b72c3a8fa94a5607fd1aec865a9f627fae07e967)

--- pkgs/development/web/nodejs/v16.nix
+++ pkgs/development/web/nodejs/v16.nix
@@ -13,8 +13,8 @@ in
     # If you do upgrade here, please update in pkgs/top-level/release.nix
     # the permitted insecure version to ensure it gets cached for our users
     # and backport this to stable release (23.05).
-    version = "16.20.1";
-    sha256 = "sha256-g+AzgeJx8aVhkYjnrqnYXZt+EvW+KijOt41ySe0it/E=";
+    version = "16.20.2";
+    sha256 = "sha256-V28aA8RV5JGo0TK1h+trO4RlH8iXS7NjhDPdRNIsj0k=";
     patches = [
       ./disable-darwin-v8-system-instrumentation.patch
       ./bypass-darwin-xcrun-node16.patch
