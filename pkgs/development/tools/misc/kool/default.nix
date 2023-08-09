efa532e530ad62a3c96e8e1734634af22332dd2b
figsoda <figsoda@pm.me>
2023-08-09 09:52:54 -0400

    kool: 2.1.0 -> 2.1.1
    Diff: https://github.com/kool-dev/kool/compare/2.1.0...2.1.1
    
    Changelog: https://github.com/kool-dev/kool/releases/tag/2.1.1

--- pkgs/development/tools/misc/kool/default.nix
+++ pkgs/development/tools/misc/kool/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "kool";
-  version = "2.1.0";
+  version = "2.1.1";
 
   src = fetchFromGitHub {
     owner = "kool-dev";
     repo = "kool";
     rev = version;
-    hash = "sha256-dMmokaFPzunpCdkJFVc3422SEKZNIOi8nzRB99Gi5Tg=";
+    hash = "sha256-Gtw+47EQVPxiW9xjY/cWlE4aJ1aYYhpvsVQzt0EnI8I=";
   };
 
   vendorHash = "sha256-8t+OZB9jrlOVHLURPmtz0ent6COEOVMFfObe2LH1jRM=";
