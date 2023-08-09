32db1fb6e0ac37da546a4b7e6f984760e8061547
Samir Talwar <samir@functional.computer>
2023-08-09 23:34:30 +0200

    ghc942 + ghc943: Apply the Cabal patch for --enable-relocatable
    This patch was already applied to GHC 9.4.4 and up.
    
    It should also fix the build, as the aarch64/darwin separate output fix depends upon it.

--- pkgs/development/compilers/ghc/9.4.2.nix
+++ pkgs/development/compilers/ghc/9.4.2.nix
@@ -189,6 +189,15 @@ stdenv.mkDerivation (rec {
   outputs = [ "out" "doc" ];
 
   patches = [
+    # Don't generate code that doesn't compile when --enable-relocatable is passed to Setup.hs
+    # Can be removed if the Cabal library included with ghc backports the linked fix
+    (fetchpatch {
+      url = "https://github.com/haskell/cabal/commit/6c796218c92f93c95e94d5ec2d077f6956f68e98.patch";
+      stripLen = 1;
+      extraPrefix = "libraries/Cabal/";
+      sha256 = "sha256-yRQ6YmMiwBwiYseC5BsrEtDgFbWvst+maGgDtdD0vAY=";
+    })
+
     # Fix docs build with sphinx >= 6.0
     # https://gitlab.haskell.org/ghc/ghc/-/issues/22766
     (fetchpatch {
