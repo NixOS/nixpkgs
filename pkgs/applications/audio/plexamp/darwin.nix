7c45a46aa967da701a5d0d8fcd32bf4c376db686
klchen0112 <klchen0112@gmail.com>
2023-08-09 22:24:50 +0800

    plexamp: fix darwin pkgs

--- pkgs/applications/audio/plexamp/darwin.nix
+++ pkgs/applications/audio/plexamp/darwin.nix
@@ -5,31 +5,24 @@
 , pname
 , version
 , meta
-, ...
 }:
 
 stdenv.mkDerivation {
   inherit pname version meta;
   src =
     if stdenv.isAarch64 then
-      (fetchurl
+      fetchurl
         {
           url = "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-${version}-arm64.dmg";
           sha256 = "sha256-VmADu5vC2SZs930mgSw0uF69xSsPfTy/1feBYTHM55g=";
-        })
+        }
     else
-      (
-        fetchurl
-          {
-            url = "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-${version}.dmg";
-            sha256 = "sha256-gXJySo/8LwDkyR0mJNou5BD+7oxx6Crmcatf/uyMTjQ=";
-          }
-      );
+      fetchurl {
+        url = "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-${version}.dmg";
+        sha256 = "sha256-gXJySo/8LwDkyR0mJNou5BD+7oxx6Crmcatf/uyMTjQ=";
+      };
 
-  dontPatch = true;
-  dontConfigure = true;
   dontBuild = true;
-  dontFixup = true;
 
   nativeBuildInputs = [ undmg ];
 
