c0481cec39b5e59611b3b7f541f8edbd36c0c225
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-08-09 16:33:44 -0700

    python3.pkgs.protobuf: fix build after updating protobuf to 3.23.4

--- pkgs/development/python-modules/protobuf/default.nix
+++ pkgs/development/python-modules/protobuf/default.nix
@@ -3,9 +3,11 @@
 , fetchpatch
 , isPyPy
 , lib
+, numpy
 , protobuf
 , pytestCheckHook
 , pythonAtLeast
+, substituteAll
 , tzdata
 }:
 
@@ -32,6 +34,12 @@ buildPythonPackage {
       extraPrefix = "";
       hash = "sha256-a/12C6yIe1tEKjsMxcfDAQ4JHolA8CzkN7sNG8ZspPs=";
     })
+  ] ++ lib.optionals (lib.versionAtLeast protobuf.version "3.22") [
+    # Replace the vendored abseil-cpp with nixpkgs'
+    (substituteAll {
+      src = ./use-nixpkgs-abseil-cpp.patch;
+      abseil_cpp_include_path = "${lib.getDev protobuf.abseil-cpp}/include";
+    })
   ];
 
   prePatch = ''
@@ -39,6 +47,19 @@ buildPythonPackage {
       echo "Python library version mismatch. Derivation version: $version, actual: $(<../version.json)"
       exit 1
     fi
+  '';
+
+  # Remove the line in setup.py that forces compiling with C++14. Upstream's
+  # CMake build has been updated to support compiling with other versions of
+  # C++, but the Python build has not. Without this, we observe compile-time
+  # errors using GCC.
+  #
+  # Fedora appears to do the same, per this comment:
+  #
+  #   https://github.com/protocolbuffers/protobuf/issues/12104#issuecomment-1542543967
+  #
+  postPatch = ''
+    sed -i "/extra_compile_args.append('-std=c++14')/d" setup.py
   '';
 
   nativeBuildInputs = lib.optional isPyPy tzdata;
@@ -54,6 +75,8 @@ buildPythonPackage {
 
   nativeCheckInputs = [
     pytestCheckHook
+  ] ++ lib.optionals (lib.versionAtLeast protobuf.version "3.22") [
+    numpy
   ];
 
   disabledTests = lib.optionals isPyPy [
@@ -64,6 +87,18 @@ buildPythonPackage {
     "testUnknownFieldsNoMemoryLeak"
     # assertion is not raised for some reason
     "testStrictUtf8Check"
+  ];
+
+  disabledTestPaths = lib.optionals (lib.versionAtLeast protobuf.version "3.23") [
+    # The following commit (I think) added some internal test logic for Google
+    # that broke generator_test.py. There is a new proto file that setup.py is
+    # not generating into a .py file. However, adding this breaks a bunch of
+    # conflict detection in descriptor_test.py that I don't understand. So let's
+    # just disable generator_test.py for now.
+    #
+    #   https://github.com/protocolbuffers/protobuf/commit/5abab0f47e81ac085f0b2d17ec3b3a3b252a11f1
+    #
+    "google/protobuf/internal/generator_test.py"
   ];
 
   pythonImportsCheck = [
