61b3fac6463728a029ecad0a49044a34f6b9b953
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-09 10:47:22 +0000

    python310Packages.qcs-api-client: 0.21.6 -> 0.23.0

--- pkgs/development/python-modules/qcs-api-client/default.nix
+++ pkgs/development/python-modules/qcs-api-client/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "qcs-api-client";
-  version = "0.21.6";
+  version = "0.23.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     owner = "rigetti";
     repo = "qcs-api-client-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-1vXqwir3lAM+m/HGHWuXl20muAOasEWo1H0RjUCShTM=";
+    hash = "sha256-g7O9kdAEm/5s5JakIlaYgRpsReGDMtpReHBGfif2NCA=";
   };
 
   patches = [
