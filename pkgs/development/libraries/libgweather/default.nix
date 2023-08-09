e93e524a50d50b24d2230a80ef48c29e63818e46
Bobby Rong <rjl931189261@126.com>
2023-08-09 09:31:35 +0000

    libgweather: 4.2.0 → 4.3.2
    https://gitlab.gnome.org/GNOME/libgweather/-/compare/4.2.0...4.3.2

--- pkgs/development/libraries/libgweather/default.nix
+++ pkgs/development/libraries/libgweather/default.nix
@@ -21,13 +21,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libgweather";
-  version = "4.2.0";
+  version = "4.3.2";
 
   outputs = [ "out" "dev" ] ++ lib.optional withIntrospection "devdoc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "r4qBLaDYl2oADh1iVywlYIaoFzI/vzWwZtv92NLKYgM=";
+    sha256 = "VZ2oFQIIvmGvX1KdZxKqiDbMrvCGahZQItlBp9L7OCY=";
   };
 
   patches = [
