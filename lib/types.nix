5441810d2a92481dd8348bde9dc6b2871935d763
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-08-10 23:46:28 +0200

    Revert "lib.types.string: Use lib.warn instead of deprecationMessage"
    This reverts commit 5075a514030bd31f030eeac85ff7f0ebfdf1651a.
    
    There's no option location in the lib.warn message, not even if you make
    it fail using NIX_ABORT_ON_WARN
    
    So let's revert the switch to lib.warn for now until a better solution
    is figured out

--- lib/types.nix
+++ lib/types.nix
@@ -436,12 +436,10 @@ rec {
 
     # Deprecated; should not be used because it quietly concatenates
     # strings, which is usually not what you want.
-    # We use a lib.warn because `deprecationMessage` doesn't trigger in nested types such as `attrsOf string`
-    string = lib.warn
-      "The type `types.string` is deprecated. See https://github.com/NixOS/nixpkgs/pull/66346 for better alternative types."
-      (separatedString "" // {
-        name = "string";
-      });
+    string = separatedString "" // {
+      name = "string";
+      deprecationMessage = "See https://github.com/NixOS/nixpkgs/pull/66346 for better alternative types.";
+    };
 
     passwdEntry = entryType: addCheck entryType (str: !(hasInfix ":" str || hasInfix "\n" str)) // {
       name = "passwdEntry ${entryType.name}";
