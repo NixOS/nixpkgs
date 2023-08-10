775ee27adc5376f484e94a87d310e51d7a68f0a7
natsukium <tomoya.otabi@gmail.com>
2023-08-10 13:16:30 +0900

    python310Packages.sparse: relax numba version

--- pkgs/development/python-modules/sparse/default.nix
+++ pkgs/development/python-modules/sparse/default.nix
@@ -2,10 +2,12 @@
 , buildPythonPackage
 , dask
 , fetchPypi
+, fetchpatch
 , numba
 , numpy
 , pytestCheckHook
 , pythonOlder
+, pythonRelaxDepsHook
 , scipy
 }:
 
@@ -20,6 +22,25 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-X1gno39s1vZzClQfmUyVxgo64jKeAfS6Ic7VM5rqAJg=";
   };
+
+  patches = [
+    # https://github.com/pydata/sparse/issues/594
+    (fetchpatch {
+      name = "fix-test.patch";
+      url = "https://github.com/pydata/sparse/commit/a55651d630efaea6fd2758d083c6d02333b0eebe.patch";
+      hash = "sha256-Vrx7MDlKtA8fOuFZenEkvgA70Hzm+p/4SPZuCvwtLuo=";
+    })
+  ];
+
+  nativeBuildInputs = [
+    pythonRelaxDepsHook
+  ];
+
+  pythonRelaxDeps = [
+    # the version of numba may not be set correctly until the next release, 0.58
+    # see the comment of https://github.com/NixOS/nixpkgs/pull/247678
+    "numba"
+  ];
 
   propagatedBuildInputs = [
     numba
