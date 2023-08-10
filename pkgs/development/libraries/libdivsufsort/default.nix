ec666a43b02b644c44995631730a087517fef0c6
Markus Theil <theil.markus@gmail.com>
2023-08-10 11:33:19 +0200

    libdivsufsort: switch src from orphaned google code to github
    Signed-off-by: Markus Theil <theil.markus@gmail.com>

--- pkgs/development/libraries/libdivsufsort/default.nix
+++ pkgs/development/libraries/libdivsufsort/default.nix
@@ -1,12 +1,14 @@
-{lib, stdenv, fetchurl, cmake}:
+{lib, stdenv, fetchFromGitHub, cmake}:
 
 stdenv.mkDerivation rec {
   pname = "libdivsufsort";
   version = "2.0.1";
 
-  src = fetchurl {
-    url = "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/libdivsufsort/libdivsufsort-${version}.tar.bz2";
-    sha256 = "1g0q40vb2k689bpasa914yi8sjsmih04017mw20zaqqpxa32rh2m";
+  src = fetchFromGitHub {
+    owner = "y-256";
+    repo = pname;
+    rev = "${version}";
+    hash = "sha256-4p+L1bq9SBgWSHXx+WYWAe60V2g1AN+zlJvC+F367Tk=";
   };
 
   nativeBuildInputs = [ cmake ];
