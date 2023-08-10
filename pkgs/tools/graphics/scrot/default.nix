1d56cde48b77bd4536e5de0042e80729fcd8989f
Norbert Melzer <timmelzer@gmail.com>
2023-08-10 08:56:51 +0200

    scrot: provide meta.mainProgram

--- pkgs/tools/graphics/scrot/default.nix
+++ pkgs/tools/graphics/scrot/default.nix
@@ -43,6 +43,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     homepage = "https://github.com/resurrecting-open-source-projects/scrot";
     description = "A command-line screen capture utility";
+    mainProgram = "scrot";
     platforms = platforms.linux;
     maintainers = with maintainers; [ globin ];
     license = licenses.mitAdvertising;
