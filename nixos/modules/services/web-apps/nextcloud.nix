73032d413860bb5a4fa126ea2d49159cfab1dcb9
Jonas Heinrich <onny@project-insanity.org>
2023-08-10 13:16:29 +0200

    nixos/nextcloud: Add ensureUsers option

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -702,6 +702,43 @@ in {
         '';
       };
     };
+
+    ensureUsers = mkOption {
+      default = {};
+      description = lib.mdDoc ''
+        List of user accounts which get automatically created if they don't
+        exist yet. This option does not delete accounts which are not listed
+        anymore.
+      '';
+      example = {
+        user1 = {
+          passwordFile = /secrets/user1-localhost;
+          email = "user1@localhost";
+        };
+        user2 = {
+          passwordFile = /secrets/user2-localhost;
+          email = "user2@localhost";
+        };
+      };
+      type = types.attrsOf (types.submodule {
+        options = {
+          passwordFile = mkOption {
+            type = types.path;
+            example = "/path/to/file";
+            default = null;
+            description = lib.mdDoc ''
+              Specifies the path to a file containing the
+              clear text password for the user.
+            '';
+          };
+          email = mkOption {
+            type = types.str;
+            example = "user1@localhost";
+            default = null;
+          };
+        };
+      });
+    };
   };
 
   config = mkIf cfg.enable (mkMerge [
@@ -1018,6 +1055,21 @@ in {
             ''}
 
             ${occSetTrustedDomainsCmd}
+
+            ${optionalString (cfg.ensureUsers != {}) ''
+              ${concatStringsSep "\n" (mapAttrsToList (name: cfg: ''
+                if ${occ}/bin/nextcloud-occ user:info "${name}" | grep "user not found"; then
+                  export OC_PASS="$(cat ${escapeShellArg cfg.passwordFile})"
+                  ${occ}/bin/nextcloud-occ user:add --password-from-env "${name}"
+                fi
+                if ! ${occ}/bin/nextcloud-occ user:info "${name}" | grep "user not found"; then
+                  ${optionalString (cfg.email != null) ''
+                    ${occ}/bin/nextcloud-occ user:setting "${name}" settings email "${cfg.email}"
+                  ''}
+                fi
+              '') cfg.ensureUsers)}
+            ''}
+
           '';
           serviceConfig.Type = "oneshot";
           serviceConfig.User = "nextcloud";
