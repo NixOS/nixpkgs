cecc72d86290ef4b091305fae85d08ddba0f087c
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-10 00:48:41 +0000

    python311Packages.yamlloader: 1.2.2 -> 1.3.2

--- pkgs/development/python-modules/yamlloader/default.nix
+++ pkgs/development/python-modules/yamlloader/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "yamlloader";
-  version = "1.2.2";
+  version = "1.3.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-NWaf17n4xrONuGGlFwFULEJnK0boq2MlNIaoy4N3toc=";
+    hash = "sha256-fb2YQh2AkMUhZV8bBsoDAGfynfUlOoh4EmvOOpD1aBc=";
   };
 
   propagatedBuildInputs = [
