db3c3b8d43eafdb13c160bb9f23a985c2f498038
figsoda <figsoda@pm.me>
2023-08-10 17:24:35 -0400

    typst-lsp: 0.7.2 -> 0.8.0
    Diff: https://github.com/nvarner/typst-lsp/compare/v0.7.2...v0.8.0
    
    Changelog: https://github.com/nvarner/typst-lsp/releases/tag/v0.8.0

--- pkgs/development/tools/language-servers/typst-lsp/default.nix
+++ pkgs/development/tools/language-servers/typst-lsp/default.nix
@@ -1,34 +1,49 @@
 { lib
 , rustPlatform
 , fetchFromGitHub
+, pkg-config
+, openssl
+, stdenv
+, darwin
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "typst-lsp";
-  version = "0.7.2";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "nvarner";
-    repo = pname;
+    repo = "typst-lsp";
     rev = "v${version}";
-    hash = "sha256-z6IG0qJXzwisazR/tLq6dwsZzgzhYKh/NnKmnYySS18=";
+    hash = "sha256-jYyWPA/ifCtxujPZK7am6O0d/VpRW8hxGfd83wUtT6U=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "elsa-1.8.1" = "sha256-/85IriplPxx24TE/CsvjIsve100QUZiVqS0TWgPFRbw=";
-      "svg2pdf-0.5.0" = "sha256-v/ARFI+Uw5KtLe2F3ty9u3uKkWSradRmLnD2VJ+jmSI=";
-      "typst-0.5.0" = "sha256-obUe9OVQ8M7MORudQGN7zaYjUv4zjeh7XidHHmUibTA=";
+      "svg2pdf-0.5.0" = "sha256-yBQpvDAnJ7C0PWIM/o0PzOg9JlDZCEiVdCTDDPSwrmE=";
+      "typst-0.6.0" = "sha256-8e6BNffKgAUNwic4uEfDh77y2nIyYt9BwZr+ypv+d5A=";
     };
   };
 
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Security
+  ];
+
   patches = [
-    # typst-library tries to access the workspace with include_bytes, which
-    # fails when it is vendored as its own separate crate
-    # this patch moves the required assets into the crate and fixes the issue
-    # see https://github.com/typst/typst/pull/1515
-    ./move-typst-assets.patch
+    # `rustPlatform.importCargoLock` has trouble parsing the `??` in the url
+    ./remove-svg2pdf-patch.patch
+  ];
+
+  checkFlags = [
+    # requires internet access
+    "--skip=workspace::package::external::repo::test::full_download"
   ];
 
   meta = with lib; {
