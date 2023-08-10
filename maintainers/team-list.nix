a8b91a52e5f32ee5c104522025d103a9d1ba58f8
figsoda <figsoda@pm.me>
2023-08-09 21:34:28 -0400

    maintainers/teams: add zig team

--- maintainers/team-list.nix
+++ maintainers/team-list.nix
@@ -891,4 +891,14 @@ with lib.maintainers; {
     shortName = "Xfce";
     enableFeatureFreezePing = true;
   };
+
+  zig = {
+    members = [
+      AndersonTorres
+      figsoda
+    ];
+    scope = "Maintain the Zig compiler toolchain and nixpkgs integration.";
+    shortName = "Zig";
+    enableFeatureFreezePing = true;
+  };
 }
