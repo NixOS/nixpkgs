c31c293bba787637992863739704013bf85a153d
K900 <me@0upti.me>
2023-08-11 14:01:41 +0300

    linux: 4.19.290 -> 4.19.291
    (cherry picked from commit 44e047a49500a61f3ab75649caf51f85e6836b0f)

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "4.19.290";
+  version = "4.19.291";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1kqflbf78aamnbdc2v973qk9ymignl1cy6q0rpw865brgm5y59cz";
+    sha256 = "0cxmq8mrkw179jb8sqvad3dskllwn579g2lxcjn21jyqsf85nwz6";
   };
 } // (args.argsOverride or {}))
