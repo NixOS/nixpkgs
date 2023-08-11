eec1488f889b7be6906a1e795b70c0787c342365
emilylange <git@emilylange.de>
2023-08-11 23:25:42 +0200

    grafana-agent: 0.35.2 -> 0.35.3
    https://github.com/grafana/agent/releases/tag/v0.35.3
    https://github.com/grafana/agent/blob/v0.35.3/CHANGELOG.md
    diff: https://github.com/grafana/agent/compare/v0.35.2...v0.35.3

--- pkgs/servers/monitoring/grafana-agent/default.nix
+++ pkgs/servers/monitoring/grafana-agent/default.nix
@@ -10,16 +10,16 @@
 
 buildGoModule rec {
   pname = "grafana-agent";
-  version = "0.35.2";
+  version = "0.35.3";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "agent";
     rev = "v${version}";
-    hash = "sha256-jotJe7DIPYNekAxiMdghdykEXVD7Pk/MPWSH2XjhkL8=";
+    hash = "sha256-3JfJISoziIcB2Mx2gSYjegjQwqGipUtvT927QSezuq4=";
   };
 
-  vendorHash = "sha256-MqUkGKOzx8Qo9xbD9GdUryVwKjpVUOXFo2x0/2uz8Uk=";
+  vendorHash = "sha256-vzrp20Mg6AA0h3+5+qbKRa7nhx/hgiIHG6RNXLATpHE=";
   proxyVendor = true; # darwin/linux hash mismatch
 
   ldflags = let
