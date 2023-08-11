3fa414184b375e0eee771477d8e4db5462430ab7
Jörg Thalheim <Mic92@users.noreply.github.com>
2023-08-11 06:45:17 +0100

    Update pkgs/development/python-modules/numba/default.nix
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>

--- pkgs/development/python-modules/numba/default.nix
+++ pkgs/development/python-modules/numba/default.nix
@@ -82,6 +82,8 @@ in buildPythonPackage rec {
 
   postPatch = ''
     substituteInPlace setup.py --replace "version=versioneer.get_version()" "version='0.57.1'"
+    substituteInPlace numba/_version.py \
+      --replace 'git_refnames = ""' 'git_refnames = "0.57.1"'
   '';
 
   postFixup = lib.optionalString cudaSupport ''
