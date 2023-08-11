1b4e3c2a2b84f6d9ce946dba64fe4a4d2d7504bd
K900 <me@0upti.me>
2023-08-11 14:02:40 +0300

    linux: 6.4.9 -> 6.4.10
    (cherry picked from commit 8e593131e066e276bd06e5e327889ad8bd7daf80)

--- pkgs/os-specific/linux/kernel/linux-6.4.nix
+++ pkgs/os-specific/linux/kernel/linux-6.4.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.4.9";
+  version = "6.4.10";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    sha256 = "17ycanx738bgxg5wn536kbad4zzvj5ngygp9qhvz76draaca5f5q";
+    sha256 = "0fgjym6y0zj7wz1byqhxmv3pc3wq412vm1dxbj4gv23pm6r3y2wq";
   };
 } // (args.argsOverride or { }))
