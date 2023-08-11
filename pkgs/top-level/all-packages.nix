b67b3bbf128ddac9689c2f09e71d3b459fe740d7
aleksana <me@aleksana.moe>
2023-08-11 20:24:24 +0800

    pokeget-rs: init at 1.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32176,6 +32176,8 @@ with pkgs;
 
   pmbootstrap = python3Packages.callPackage ../tools/misc/pmbootstrap { };
 
+  pokeget-rs = callPackage ../tools/misc/pokeget-rs { };
+
   popura = callPackage ../tools/networking/popura { };
 
   pureref = callPackage ../applications/graphics/pureref { };
