19a3eb1650d23bd1bda115b3355c4ee39d969cbf
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-11 02:50:27 +0000

    python310Packages.aiohomekit: 2.6.14 -> 2.6.15

--- pkgs/development/python-modules/aiohomekit/default.nix
+++ pkgs/development/python-modules/aiohomekit/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "aiohomekit";
-  version = "2.6.14";
+  version = "2.6.15";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "Jc2k";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-JPI1+sQVZe2/5YxW6OxZRosp36e93KEW+V1DAcLaYNY=";
+    hash = "sha256-PX2OIgfVOlCEudObJrz/WRQXW7c6Gq9PQqD52D3lmmo=";
   };
 
   nativeBuildInputs = [
