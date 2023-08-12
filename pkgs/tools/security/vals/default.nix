9e3622c85fb6877d83ecf6ccc591f7d52701aa1f
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-12 13:12:31 +0000

    vals: 0.26.1 -> 0.26.2

--- pkgs/tools/security/vals/default.nix
+++ pkgs/tools/security/vals/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "vals";
-  version = "0.26.1";
+  version = "0.26.2";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "variantdev";
     repo = pname;
-    sha256 = "sha256-gICEqwt34pllvxA8JVc0rCQ2F3w6wT96eKTTxE0j398=";
+    sha256 = "sha256-WTUdb2LF/50KT3BqwbvKu4TFocbYBdEAoD3IQiPD2bs=";
   };
 
   vendorHash = "sha256-6DJiqDEgEHQbyIt4iShoBnagBvspd3W3vD56/FGjESs=";
