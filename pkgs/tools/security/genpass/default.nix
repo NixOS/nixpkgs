5873b652eee5eb9553199b3cf6f334768201e7e9
Aaron Jheng <wentworth@outlook.com>
2023-08-12 10:25:43 +0800

    genpass: 0.5.1 -> 0.5.14

--- pkgs/tools/security/genpass/default.nix
+++ pkgs/tools/security/genpass/default.nix
@@ -1,4 +1,5 @@
-{ lib, stdenv
+{ lib
+, stdenv
 , fetchgit
 , rustPlatform
 , CoreFoundation
@@ -7,21 +8,21 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "genpass";
-  version = "0.5.1";
+  version = "0.5.27";
 
   src = fetchgit {
-    url = "https://git.sr.ht/~cyplo/genpass";
+    url = "https://git.cyplo.dev/cyplo/genpass.git";
     rev = "v${version}";
-    sha256 = "UyEgOlKtDyneRteN3jHA2BJlu5U1HFL8HA2MTQz5rns=";
+    hash = "sha256-XwbAeAeTPILxSV5f76jlpTBfBnX20WK4O1+jYalkKEY=";
   };
 
-  cargoSha256 = "ls3tzZ+gtZQlObmbtwJDq6N/f5nY+Ps7RL5R/fR5Vgg=";
+  cargoHash = "sha256-93XKQ7KdjCOrfs+6cgOvwYe9FBTxUFbQ0SUJhrpxQC0=";
 
   buildInputs = lib.optionals stdenv.isDarwin [ CoreFoundation libiconv Security ];
 
   meta = with lib; {
     description = "A simple yet robust commandline random password generator";
-    homepage = "https://sr.ht/~cyplo/genpass/";
+    homepage = "https://git.cyplo.dev/cyplo/genpass";
     license = licenses.agpl3Only;
     maintainers = with maintainers; [ cyplo ];
   };
