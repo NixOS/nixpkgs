f54556ebc3f926ae964c51c96942a382fe2ee811
Robert Scott <code@humanleg.org.uk>
2023-08-13 23:42:17 +0100

    graphicsmagick: 1.3.39 -> 1.3.41

--- pkgs/applications/graphics/graphicsmagick/default.nix
+++ pkgs/applications/graphics/graphicsmagick/default.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "graphicsmagick";
-  version = "1.3.39";
+  version = "1.3.41";
 
   src = fetchurl {
     url = "mirror://sourceforge/graphicsmagick/GraphicsMagick-${version}.tar.xz";
-    sha256 = "sha256-4wscpY6HPQoe4gg4RyRCTbLTwzpUA04mHRTo+7j40E8=";
+    sha256 = "sha256-t0GxG6hhYttNTsGzVJiadz9zxAci0RSCOfbGnJ8Epqo=";
   };
 
   patches = [
