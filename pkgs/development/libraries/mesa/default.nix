195ccf5b40b9d789a234e61f21a079efdb51d86e
Vladimír Čunát <v@cunat.cz>
2023-08-13 09:18:13 +0200

    mesa: 23.1.3 -> 23.1.5
    https://docs.mesa3d.org/relnotes/23.1.5.html
    The issues we heard about >= 23.1.4 appear to have been just
    the usual sensitivity to version mismatches in NixOS mesa.

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -86,8 +86,8 @@
 */
 
 let
-  version = "23.1.3";
-  hash = "sha256-L21zgbwQ+9LWJjrRAieFuLURBGwakEFi+PfaGO6ortk=";
+  version = "23.1.5";
+  hash = "sha256-PPiFdv3r8k/EBHBnk2ExyQy2VBwnNlmWt5tmHewfsVM=";
 
   # Release calendar: https://www.mesa3d.org/release-calendar.html
   # Release frequency: https://www.mesa3d.org/releasing.html#schedule
