7301b48f7f7a085fe12834a269ff8aa8bd9fe152
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-13 11:50:30 +0200

    python311Packages.goocalendar: 0.7.2 -> 0.8.0

--- pkgs/development/python-modules/goocalendar/default.nix
+++ pkgs/development/python-modules/goocalendar/default.nix
@@ -11,13 +11,13 @@
 
 buildPythonPackage rec {
   pname = "GooCalendar";
-  version = "0.7.2";
+  version = "0.8.0";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "318b3b7790ac9d6d98881eee3b676fc9c17fc15d21dcdaff486e3c303333b41a";
+    sha256 = "sha256-LwL5TLRkD6ALucabLUeB0k4rIX+O/aW2ebS2rZPjIUs=";
   };
 
   nativeBuildInputs = [
