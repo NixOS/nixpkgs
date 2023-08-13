77f0026eaae861921a7a7d649c345e585bb284ea
Anderson Torres <torres.anderson.85@protonmail.com>
2023-08-13 14:48:40 -0300

    maintainers: remove kototama

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9159,12 +9159,6 @@
     githubId = 2037002;
     name = "Konstantinos";
   };
-  kototama = {
-    email = "kototama@posteo.jp";
-    github = "kototama";
-    githubId = 128620;
-    name = "Kototama";
-  };
   kouyk = {
     email = "skykinetic@stevenkou.xyz";
     github = "kouyk";
