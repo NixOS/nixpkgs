f53d2faa867089c9220c21e0991972db94d21bbc
arcnmx <git@git.arcn.mx>
2023-08-14 10:52:15 -0700

    tailscale: meta.mainProgram
    See #246386

--- pkgs/servers/tailscale/default.nix
+++ pkgs/servers/tailscale/default.nix
@@ -42,6 +42,7 @@ buildGoModule {
     homepage = "https://tailscale.com";
     description = "The node agent for Tailscale, a mesh VPN built on WireGuard";
     license = licenses.bsd3;
+    mainProgram = "tailscale";
     maintainers = with maintainers; [ danderson mbaillie twitchyliquid64 jk ];
   };
 }
