5de32df9bc1bab57a96247ca94ba31c05c27a638
Robert Scott <code@humanleg.org.uk>
2023-08-14 18:11:27 +0100

    python3Packages.tornado: add patch for CVE-2023-28370

--- pkgs/development/python-modules/tornado/default.nix
+++ pkgs/development/python-modules/tornado/default.nix
@@ -2,6 +2,7 @@
 , python
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , pytestCheckHook
 }:
 
@@ -15,6 +16,14 @@ buildPythonPackage rec {
     rev = "v${version}";
     hash = "sha256-IV0QN3GqoclFo9kWJVc21arypmBkvUClo86Zmt/Gv6E=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2023-28370.patch";
+      url = "https://github.com/tornadoweb/tornado/commit/32ad07c54e607839273b4e1819c347f5c8976b2f.patch";
+      hash = "sha256-2dpPHkNThOaZD8T2g1vb/I5WYZ/vy/t690539uprJyc=";
+    })
+  ];
 
   nativeCheckInputs = [
     pytestCheckHook
