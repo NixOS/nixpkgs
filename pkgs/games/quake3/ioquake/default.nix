bbc9b757ebe701a061e06d9fe0cdc419027ec287
TheBrainScrambler <34945377+TheBrainScrambler@users.noreply.github.com>
2023-08-14 16:15:00 +0000

    ioquake3: fix incorrect license gpl2Only -> gpl2Plus

--- pkgs/games/quake3/ioquake/default.nix
+++ pkgs/games/quake3/ioquake/default.nix
@@ -59,7 +59,7 @@ stdenv.mkDerivation {
   meta = with lib; {
     homepage = "https://ioquake3.org/";
     description = "First person shooter engine based on the Quake 3: Arena and Quake 3: Team Arena";
-    license = licenses.gpl2Only;
+    license = licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ rvolosatovs eelco abbradar ];
   };
