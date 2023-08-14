NixOS/nixpkgs

commit 70e9e2cd8132bb8b80ff82867f021ac76165010e

plasma-pass: 1.2.0 -> 1.2.1
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

pkgs/tools/security/plasma-pass/default.nix
@@ -8,14 +8,19 @@
 
 mkDerivation rec {
   pname = "plasma-pass";
-  version = "1.2.0";
+  version = "1.2.1";
 
   src = fetchFromGitLab {
     domain = "invent.kde.org";
     owner = "plasma";
     repo = "plasma-pass";
-    rev = "v${version}";
-    sha256 = "1w2mzxyrh17x7da62b6sg1n85vnh1q77wlrfxwfb1pk77y59rlf1";
+    sha256 = "sha256-lCNskOXkSIcMPcMnTWE37sDCXfmtP0FhyMzxeF6L0iU=";
+
+    # So the tag is actually "v0.2.1" but the released version is later than
+    # 1.2.0 and the "release" on the gitlab page also says "1.2.1".
+    # I guess they just messed up the tag subject and description.
+    # Maintainer of plasma-pass was notified about this 2023-08-13
+    rev = "v0.2.1";
   };
 
   buildInputs  = [

commit e6653c7a1e7ed8846ac5e3ceeb964366fddcb919

plasma-pass: Fix fetch tag
Fixes: 70e9e2cd8132bb8b80ff82867f021ac76165010e ("plasma-pass: 1.2.0 -> 1.2.1")
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

pkgs/tools/security/plasma-pass/default.nix
@@ -15,12 +15,7 @@ mkDerivation rec {
     owner = "plasma";
     repo = "plasma-pass";
     sha256 = "sha256-lCNskOXkSIcMPcMnTWE37sDCXfmtP0FhyMzxeF6L0iU=";
-
-    # So the tag is actually "v0.2.1" but the released version is later than
-    # 1.2.0 and the "release" on the gitlab page also says "1.2.1".
-    # I guess they just messed up the tag subject and description.
-    # Maintainer of plasma-pass was notified about this 2023-08-13
-    rev = "v0.2.1";
+    rev = "v${version}";
   };
 
   buildInputs  = [