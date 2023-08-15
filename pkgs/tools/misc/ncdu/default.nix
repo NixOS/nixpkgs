e0adec00c4fb8abdee60e045116a628f870e72a7
Tarcísio Genaro Rodrigues <tarcisio.genaro@gmail.com>
2023-08-15 16:40:24 -0300

    Revert "ncdu: 2.2.2 -> 2.3"
    This reverts commit 0b6d395909219e21fbd55dd6b321fc1f06a424ab.

--- pkgs/tools/misc/ncdu/default.nix
+++ pkgs/tools/misc/ncdu/default.nix
@@ -2,31 +2,25 @@
 , stdenv
 , fetchurl
 , ncurses
-, zig_0_11
-, installShellFiles
+, zig_0_10
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ncdu";
-  version = "2.3";
+  version = "2.2.2";
 
   src = fetchurl {
     url = "https://dev.yorhel.nl/download/ncdu-${finalAttrs.version}.tar.gz";
-    hash = "sha256-u84dHHDxJHZxvk6iE12MUs0ppwivXtYs7Np9xqgACjw=";
+    hash = "sha256-kNkgAk51Ixi0aXds5X4Ds8cC1JMprZglruqzbDur+ZM=";
   };
 
   nativeBuildInputs = [
-    zig_0_11.hook
-    installShellFiles
+    zig_0_10.hook
   ];
 
   buildInputs = [
     ncurses
   ];
-
-  postInstall = ''
-    installManPage ncdu.1
-  '';
 
   meta = {
     homepage = "https://dev.yorhel.nl/ncdu";
@@ -34,7 +28,6 @@ stdenv.mkDerivation (finalAttrs: {
     changelog = "https://dev.yorhel.nl/ncdu/changes2";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ pSub rodrgz ];
-    inherit (zig_0_11.meta) platforms;
-    mainProgram = "ncdu";
+    inherit (zig_0_10.meta) platforms;
   };
 })
