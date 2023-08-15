e403416ffe47aa5ba1af5b9b011280e2a442f572
Bobby Rong <rjl931189261@126.com>
2023-08-15 15:01:38 +0800

    gnome.mutter43: Fix build with sysprof 45
    ... which provides sysprof-6 instead of sysprof-4.
    
    It should be safe to use sysprof-6 since it is only used
    for org.gnome.Sysprof3.Profiler D-Bus interface path,
    which did not change.
    
    Acked-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/mutter/43/default.nix
+++ pkgs/desktops/gnome/core/mutter/43/default.nix
@@ -136,6 +136,10 @@ stdenv.mkDerivation (finalAttrs: {
 
   postPatch = ''
     patchShebangs src/backends/native/gen-default-modes.py
+
+    # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3187
+    substituteInPlace meson.build \
+      --replace "dependency('sysprof-4')" "dependency('sysprof-6')"
   '';
 
   postInstall = ''
