61f33e8d5cbd766c9fe3dce68e050de0f75f5db4
Mario Rodas <marsam@users.noreply.github.com>
2023-08-15 04:20:00 +0000

    commit-mono: 1.132 -> 1.134
    Diff: https://github.com/eigilnikolajsen/commit-mono/compare/1.132...1.134
    
    Changelog: https://github.com/eigilnikolajsen/commit-mono/releases/tag/1.134

--- pkgs/data/fonts/commit-mono/default.nix
+++ pkgs/data/fonts/commit-mono/default.nix
@@ -4,11 +4,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "commit-mono";
-  version = "1.132";
+  version = "1.134";
 
   src = fetchzip {
     url = "https://github.com/eigilnikolajsen/commit-mono/releases/download/${version}/CommitMono-${version}.zip";
-    sha256 = "sha256-a9zxzjfOFmqemSIb4Tav0l7YtKvbyizDy+1dwPuZ4d4=";
+    sha256 = "sha256-uOw5xwbLYYVDRfbzMwNn2xNYOOQ7Mqu8xRlsg4pArSs=";
     stripRoot = false;
   };
 
@@ -27,6 +27,7 @@ stdenvNoCC.mkDerivation rec {
   meta = with lib; {
     description = "An anonymous and neutral programming typeface focused on creating a better reading experience";
     homepage = "https://commitmono.com/";
+    changelog = "https://github.com/eigilnikolajsen/commit-mono/releases/tag/${version}";
     license = licenses.ofl;
     maintainers = [ maintainers.yoavlavi ];
     platforms = platforms.all;
