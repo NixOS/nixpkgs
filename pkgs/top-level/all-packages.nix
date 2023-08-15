02848fc11ca3f0302b12790ceaf0ed6bf2929c37
Bobby Rong <rjl931189261@126.com>
2023-08-15 16:02:34 +0800

    snapshot: init at 45.beta
    https://gitlab.gnome.org/GNOME/snapshot/-/commits/45.beta
    
    If you are having issues, try these environment variables to get more info:
    
    - RUST_LOG=snapshot=debug,aperture=debug
    - RUST_BACKTRACE=1
    - GST_DEBUG=3
    
    Still need to see if we should study anything from cheese.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13408,6 +13408,8 @@ with pkgs;
 
   sonar-scanner-cli = callPackage ../tools/security/sonar-scanner-cli { };
 
+  snapshot = callPackage ../applications/graphics/snapshot { };
+
   solvespace = callPackage ../applications/graphics/solvespace { };
 
   sonarr = callPackage ../servers/sonarr { };
