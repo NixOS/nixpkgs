a9a29b70590d95761044fb976b8135c9087ee24b
Sefa Eyeoglu <contact@scrumplex.net>
2023-08-15 08:57:31 +0200

    lib/meta.nix: recommend use of getExe' in getExe warning
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- lib/meta.nix
+++ lib/meta.nix
@@ -147,7 +147,7 @@ rec {
     let
       y = x.meta.mainProgram or (
         # This could be turned into an error when 23.05 is at end of life
-        lib.warn "getExe: Package ${lib.strings.escapeNixIdentifier x.meta.name or x.pname or x.name} does not have the meta.mainProgram attribute. We'll assume that the main program has the same name for now, but this behavior is deprecated, because it leads to surprising errors when the assumption does not hold. If the package has a main program, please set `meta.mainProgram` in its definition to make this warning go away. Otherwise, if the package does not have a main program, or if you don't control its definition, specify the full path to the program, such as \"\${lib.getBin foo}/bin/bar\"."
+        lib.warn "getExe: Package ${lib.strings.escapeNixIdentifier x.meta.name or x.pname or x.name} does not have the meta.mainProgram attribute. We'll assume that the main program has the same name for now, but this behavior is deprecated, because it leads to surprising errors when the assumption does not hold. If the package has a main program, please set `meta.mainProgram` in its definition to make this warning go away. Otherwise, if the package does not have a main program, or if you don't control its definition, use getExe' to specify the name to the program, such as lib.getExe' foo \"bar\"."
           lib.getName
           x
       );
