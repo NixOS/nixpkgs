80dcf0e37bd2e8f168e32149f24183f48c5e68d6
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-15 18:26:25 +0000

    zef: 0.18.2 -> 0.18.3

--- pkgs/development/interpreters/rakudo/zef.nix
+++ pkgs/development/interpreters/rakudo/zef.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "zef";
-  version = "0.18.2";
+  version = "0.18.3";
 
   src = fetchFromGitHub {
     owner = "ugexe";
     repo = "zef";
     rev = "v${version}";
-    sha256 = "sha256-0EWajziWoxWLGaj54FfvEMNPPTc2Wb6O050o2qWGJ9c=";
+    sha256 = "sha256-/H8wHDMl2lJElsjNcNmTrijIeL1ohOkDzrO7LuOPhi4=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
