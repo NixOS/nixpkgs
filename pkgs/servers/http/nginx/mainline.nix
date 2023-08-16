f4e49466ef60f2c1e4895f18fad622411cc559ac
Izorkin <izorkin@elven.pw>
2023-08-16 16:09:52 +0300

    nginxMainline: 1.25.1 -> 1.25.2

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix args {
-  version = "1.25.1";
-  hash = "sha256-8JBxrEbg6jrcAAjvC6yiKfxrS+RTO675u7+6feKahgI=";
+  version = "1.25.2";
+  hash = "sha256-Bd1tk1bWanTmEDXypCFi+MdUyXzxumTnqAG6FY1sBxE=";
 }
