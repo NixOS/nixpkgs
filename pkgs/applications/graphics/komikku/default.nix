9c536a29110d1923a9984005f9b1a62d9da17958
Chuang Zhu <git@chuang.cz>
2023-08-17 01:07:04 +0800

    komikku: 1.22.0 -> 1.23.0

--- pkgs/applications/graphics/komikku/default.nix
+++ pkgs/applications/graphics/komikku/default.nix
@@ -19,7 +19,7 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "komikku";
-  version = "1.22.0";
+  version = "1.23.0";
 
   format = "other";
 
@@ -27,7 +27,7 @@ python3.pkgs.buildPythonApplication rec {
     owner = "valos";
     repo = "Komikku";
     rev = "v${version}";
-    hash = "sha256-6Pqa3qNnH8WF/GK4CLyEmLoPm4A6Q3Gri2x0whf6WTY=";
+    hash = "sha256-duWAOod2co62NJ5Jk+7eWTf2LcfV5ZbFw0BhrbdGdUY=";
   };
 
   nativeBuildInputs = [
@@ -87,6 +87,7 @@ python3.pkgs.buildPythonApplication rec {
     description = "Manga reader for GNOME";
     homepage = "https://valos.gitlab.io/Komikku/";
     license = licenses.gpl3Plus;
+    changelog = "https://gitlab.com/valos/Komikku/-/releases/v${version}";
     maintainers = with maintainers; [ chuangzhu infinitivewitch ];
   };
 }
