a8b97115eb5410de4dc79f2caa036478c65b3aba
Robert Schütz <nix@dotlambda.de>
2023-08-15 20:15:02 -0700

    matrix-alertmanager: 0.5.0 -> 0.7.2
    Diff: https://github.com/jaywink/matrix-alertmanager/compare/v0.5.0...v0.7.2
    
    Changelog: https://github.com/jaywink/matrix-alertmanager/blob/v0.7.2/CHANGELOG.md

--- pkgs/servers/monitoring/matrix-alertmanager/default.nix
+++ pkgs/servers/monitoring/matrix-alertmanager/default.nix
@@ -6,13 +6,13 @@
 
 buildNpmPackage rec {
   pname = "matrix-alertmanager";
-  version = "0.5.0";
+  version = "0.7.2";
 
   src = fetchFromGitHub {
     owner = "jaywink";
     repo = pname;
     rev = "v${version}";
-    sha256 = "M3/8viRCRiVJGJSHidP6nG8cr8wOl9hMFY/gzdSRN+4=";
+    hash = "sha256-7rsY/nUiuSVkM8fbPPa9DB3c+Uhs+Si/j1Jzls6d2qc=";
   };
 
   postPatch = ''
@@ -20,11 +20,12 @@ buildNpmPackage rec {
     mv package.json.tmp package.json
   '';
 
-  npmDepsHash = "sha256-hR9Q/8sLxSf916BARBgTKmwonv5JqSSkfvOfYL9SdeU=";
+  npmDepsHash = "sha256-OI/zlz03YQwUnpOiHAVQfk8PWKsurldpp0PbF1K9zbM=";
 
   dontNpmBuild = true;
 
   meta = with lib; {
+    changelog = "https://github.com/jaywink/matrix-alertmanager/blob/${src.rev}/CHANGELOG.md";
     description = "Bot to receive Alertmanager webhook events and forward them to chosen rooms";
     homepage = "https://github.com/jaywink/matrix-alertmanager";
     license = licenses.mit;
