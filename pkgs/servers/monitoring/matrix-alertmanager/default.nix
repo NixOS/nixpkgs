265cff66545c18a8cdcca8bb29a509cf8f2883cc
Robert Schütz <nix@dotlambda.de>
2023-08-15 20:13:07 -0700

    matrix-alertmanager: use buildNpmPackage

--- pkgs/servers/monitoring/matrix-alertmanager/default.nix
+++ pkgs/servers/monitoring/matrix-alertmanager/default.nix
@@ -1,6 +1,10 @@
-{ lib, callPackage, mkYarnPackage, fetchYarnDeps, fetchFromGitHub, nodejs }:
+{ lib
+, buildNpmPackage
+, fetchFromGitHub
+, jq
+}:
 
-mkYarnPackage rec {
+buildNpmPackage rec {
   pname = "matrix-alertmanager";
   version = "0.5.0";
 
@@ -11,29 +15,19 @@ mkYarnPackage rec {
     sha256 = "M3/8viRCRiVJGJSHidP6nG8cr8wOl9hMFY/gzdSRN+4=";
   };
 
-  packageJSON = ./package.json;
-  yarnLock = ./yarn.lock;
-
-  offlineCache = fetchYarnDeps {
-    inherit yarnLock;
-    sha256 = lib.fileContents ./yarn-hash;
-  };
-
-  prePatch = ''
-    cp ${./package.json} ./package.json
-  '';
-  postInstall = ''
-    sed '1 s;^;#!${nodejs}/bin/node\n;' -i $out/libexec/matrix-alertmanager/node_modules/matrix-alertmanager/src/app.js
-    chmod +x $out/libexec/matrix-alertmanager/node_modules/matrix-alertmanager/src/app.js
+  postPatch = ''
+    ${lib.getExe jq} '. += {"bin": "src/app.js"}' package.json > package.json.tmp
+    mv package.json.tmp package.json
   '';
 
-  passthru.updateScript = callPackage ./update.nix {};
+  npmDepsHash = "sha256-hR9Q/8sLxSf916BARBgTKmwonv5JqSSkfvOfYL9SdeU=";
+
+  dontNpmBuild = true;
 
   meta = with lib; {
     description = "Bot to receive Alertmanager webhook events and forward them to chosen rooms";
     homepage = "https://github.com/jaywink/matrix-alertmanager";
     license = licenses.mit;
     maintainers = with maintainers; [ yuka ];
-    platforms = platforms.all;
   };
 }
