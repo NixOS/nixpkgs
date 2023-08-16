917b90ecec1909c2b671021fd4d7d3d6d17cecc3
Bobby Rong <rjl931189261@126.com>
2023-08-16 13:43:34 +0800

    budgie.magpie: Fix build with sysprof 45
    Same as what we do in mutter 43.

--- pkgs/desktops/budgie/magpie/default.nix
+++ pkgs/desktops/budgie/magpie/default.nix
@@ -126,6 +126,10 @@ stdenv.mkDerivation (finalAttrs: {
     patchShebangs src/backends/native/gen-default-modes.py
     # Magpie does not install any .desktop files
     substituteInPlace scripts/mesonPostInstall.sh --replace "update-desktop-database" "# update-desktop-database"
+
+    # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3187
+    substituteInPlace meson.build \
+      --replace "dependency('sysprof-4')" "dependency('sysprof-6')"
   '';
 
   postFixup = ''
