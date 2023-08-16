dc8c0037642220239b4a7be14b3fb943d14c0c3f
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-16 23:38:10 +0200

    python310Packages.nbxmpp: 4.3.1 -> 4.3.2
    Diff: https://dev.gajim.org/gajim/python-nbxmpp/-/compare/4.3.1...4.3.2

--- pkgs/development/python-modules/nbxmpp/default.nix
+++ pkgs/development/python-modules/nbxmpp/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "nbxmpp";
-  version = "4.3.1";
+  version = "4.3.2";
 
   disabled = pythonOlder "3.10";
 
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "gajim";
     repo = "python-nbxmpp";
     rev = version;
-    hash = "sha256-8Fh4sgQps6zUEN8o5ljrDIbRlbSZIMncbqh/qAnyOkw=";
+    hash = "sha256-vSLWaGYST1nut+0KAzURRKsr6XRtmYYTrkJiQEK3wa4=";
   };
 
   format = "pyproject";
