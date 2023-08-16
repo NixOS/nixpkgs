4015ea192a3261e76a07b6a2d46e734eadbdef02
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-17 00:38:07 +0200

    python310Packages.httptools: add changelog to meta

--- pkgs/development/python-modules/httptools/default.nix
+++ pkgs/development/python-modules/httptools/default.nix
@@ -14,15 +14,18 @@ buildPythonPackage rec {
     hash = "sha256-n8bkCa04y9aLF3zVFY/EBCx5a4LKiNmex48HvtbGt5Y=";
   };
 
-  # tests are not included in pypi tarball
+  # Tests are not included in pypi tarball
   doCheck = false;
 
-  pythonImportsCheck = [ "httptools" ];
+  pythonImportsCheck = [
+    "httptools"
+  ];
 
   meta = with lib; {
     description = "A collection of framework independent HTTP protocol utils";
     homepage = "https://github.com/MagicStack/httptools";
+    changelog = "https://github.com/MagicStack/httptools/releases/tag/v${version}";
     license = licenses.mit;
-    maintainers = [ ];
+    maintainers = with maintainers; [ ];
   };
 }
