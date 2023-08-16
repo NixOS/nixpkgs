17e05cd1f2d8efebb32e569b44c4e9e3bd7cbefc
Leandro Reina <leandro.reina@ororatech.com>
2023-08-16 13:30:33 +0200

    maintainers/teams: add ororatech

--- maintainers/team-list.nix
+++ maintainers/team-list.nix
@@ -668,6 +668,14 @@ with lib.maintainers; {
     shortName = "OpenStack";
   };
 
+  ororatech = {
+    # email: nixdevs@ororatech.com
+    shortName = "OroraTech GmbH. employees";
+    scope = "Team for packages maintained by employees of OroraTech GmbH.";
+    # Edits to this list should only be done by an already existing member.
+    members = [ ];
+  };
+
   pantheon = {
     members = [
       davidak
