3f2a40e34df69b2e5a17edff820f692b07523677
Ilan Joselevich <personal@ilanjoselevich.com>
2023-08-16 00:07:08 +0000

    libvirt: 9.5.0 -> 9.6.0

--- pkgs/development/libraries/libvirt/default.nix
+++ pkgs/development/libraries/libvirt/default.nix
@@ -114,13 +114,13 @@ stdenv.mkDerivation rec {
   # NOTE: You must also bump:
   # <nixpkgs/pkgs/development/python-modules/libvirt/default.nix>
   # SysVirt in <nixpkgs/pkgs/top-level/perl-packages.nix>
-  version = "9.5.0";
+  version = "9.6.0";
 
   src = fetchFromGitLab {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-u+J1ejv7JH6Lcwk8zDVUS8Vk806WvG59rLAZr0UOqj0=";
+    sha256 = "sha256-dQr6bUaZOX1MN+MZxbsPqbv3bsyyWBM0SBYlSnV04K0=";
     fetchSubmodules = true;
   };
 
