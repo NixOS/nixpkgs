35b13c44e34f274b96570fe974d1cda6d097fce9
Federico Damián Schonborn <fdschonborn@gmail.com>
2023-08-15 21:56:57 -0300

    fastfetch: 1.12.2 -> 2.0.0
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- pkgs/tools/misc/fastfetch/default.nix
+++ pkgs/tools/misc/fastfetch/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , chafa
 , cmake
 , dbus
@@ -24,6 +25,7 @@
 , vulkan-loader
 , wayland
 , xfce
+, yyjson
 , zlib
 , AppKit
 , Cocoa
@@ -40,14 +42,22 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "1.12.2";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "fastfetch-cli";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-l9fIm7+dBsOqGoFUYtpYESAjDy3496rDTUDQjbNU4U0=";
+    hash = "sha256-mXbkzPlX1OsK+ahUSJWktV5D7Mo2zkhXgXP54QjbIR4=";
   };
+
+  patches = [
+    # Don't fetch yyjson.
+    (fetchpatch {
+      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/app-misc/fastfetch/files/fastfetch-2.0.0-dont-fetch-yyjson.patch";
+      hash = "sha256-mOykwXSuad8BrUBmjX39EmQb0/hnKezgmWe8cpAybsw=";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
@@ -59,6 +69,7 @@ stdenv.mkDerivation (finalAttrs: {
     chafa
     imagemagick_light
     sqlite
+    yyjson
   ]
   ++ lib.optionals stdenv.isLinux [
     dbus
@@ -114,7 +125,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Like neofetch, but much faster because written in C";
-    inherit (finalAttrs.src.meta) homepage;
+    homepage = "https://github.com/fastfetch-cli/fastfetch";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ gerg-l khaneliman ];
     platforms = lib.platforms.all;
