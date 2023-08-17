f7b23c206a0c0869676880f7fb85bb1587a0ecbc
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-17 16:46:12 +0000

    python310Packages.irc: 20.1.0 -> 20.3.0

--- pkgs/development/python-modules/irc/default.nix
+++ pkgs/development/python-modules/irc/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "irc";
-  version = "20.1.0";
+  version = "20.3.0";
   format = "pyproject";
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-tvc3ky3UeR87GOMZ3nt9rwLSKFpr6iY9EB9NjlU4B+w=";
+    hash = "sha256-JFteqYqwAlZnYx53alXjGRfmDvcIxgEC8hmLyfURMjY=";
   };
 
   nativeBuildInputs = [ setuptools-scm ];
