10a6da65eddbce035afe5d4b7e0a59f57e26fb25
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2023-08-16 23:26:10 -0400

    beekeeper-studio: 3.6.2 -> 3.9.20

--- pkgs/development/tools/database/beekeeper-studio/default.nix
+++ pkgs/development/tools/database/beekeeper-studio/default.nix
@@ -2,27 +2,26 @@
 
 let
   pname = "beekeeper-studio";
-  version = "3.6.2";
-  name = "${pname}-${version}";
+  version = "3.9.20";
 
   src = fetchurl {
     url = "https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v${version}/Beekeeper-Studio-${version}.AppImage";
     name = "${pname}-${version}.AppImage";
-    sha512 = "sha512-an4Gqx2mx/rnkLe/LUAz3qRdrqWBcrWcdCiNi8Hz1OKBp1SWN3acU8RppIM0uwlrcBkjnigbbM5DZ2o+svA23A==";
+    hash = "sha512-nUDRWoxN/jH4yRbV89borr6aohldHWjczfQFbsQz5peB8xx/j1x9JuO5IAOK/kYrHX7zRf1bJ0qPlwgLoi+wGg==";
   };
 
   appimageContents = appimageTools.extractType2 {
-    inherit name src;
+    inherit pname version src;
   };
 in
 appimageTools.wrapType2 {
-  inherit name src;
+  inherit pname version src;
 
   multiArch = false; # no 32bit needed
   extraPkgs = pkgs: appimageTools.defaultFhsEnvArgs.multiPkgs pkgs ++ [ pkgs.bash ];
 
   extraInstallCommands = ''
-    ln -s $out/bin/${name} $out/bin/${pname}
+    ln -s $out/bin/${pname}-${version} $out/bin/${pname}
     install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
     install -m 444 -D ${appimageContents}/${pname}.png \
       $out/share/icons/hicolor/512x512/apps/${pname}.png
@@ -30,12 +29,13 @@ appimageTools.wrapType2 {
       --replace 'Exec=AppRun' 'Exec=${pname}'
   '';
 
-  meta = with lib; {
-    description = "Modern and easy to use SQL client for MySQL, Postgres, SQLite, SQL Server, and more. Linux, MacOS, and Windows";
+  meta = {
+    description = "Modern and easy to use SQL client for MySQL, Postgres, SQLite, SQL Server, and more";
     homepage = "https://www.beekeeperstudio.io";
     changelog = "https://github.com/beekeeper-studio/beekeeper-studio/releases/tag/v${version}";
-    license = licenses.mit;
-    maintainers = with maintainers; [ milogert alexnortung ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ alexnortung eclairevoyant milogert ];
     platforms = [ "x86_64-linux" ];
+    mainProgram = pname;
   };
 }
