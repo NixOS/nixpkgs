479b7623c500a17a0090facdd566c339db57601f
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-17 23:17:51 +0200

    python311Packages.imageio: 2.28.1 -> 2.31.1
    Changelog: https://github.com/imageio/imageio/blob/v2.31.1/CHANGELOG.md

--- pkgs/development/python-modules/imageio/default.nix
+++ pkgs/development/python-modules/imageio/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "imageio";
-  version = "2.28.1";
+  version = "2.31.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-XbUIe+XIFOz34sfTChoVyX7Kl9jCbzHdxU12fUpDvOg=";
+    hash = "sha256-+ENqAq8C/WPyctq1D31iNUejjw4EpKc+KwKuG4sYDyc=";
   };
 
   patches = lib.optionals (!stdenv.isDarwin) [
