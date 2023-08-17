da11d3550792fc8fcb598f1925bcedb450658528
Anthony Roussel <anthony@roussel.dev>
2023-08-17 18:07:36 +0200

    amazon-ec2-utils: add anthonyroussel to maintainers

--- pkgs/tools/admin/amazon-ec2-utils/default.nix
+++ pkgs/tools/admin/amazon-ec2-utils/default.nix
@@ -73,6 +73,6 @@ stdenv.mkDerivation rec {
     description = "Contains a set of utilities and settings for Linux deployments in EC2";
     homepage = "https://github.com/amazonlinux/amazon-ec2-utils";
     license = licenses.mit;
-    maintainers = with maintainers; [ ketzacoatl thefloweringash ];
+    maintainers = with maintainers; [ ketzacoatl thefloweringash anthonyroussel ];
   };
 }
