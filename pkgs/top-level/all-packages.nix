72fe04286e8f4f60a21d384788e59dbe94764434
Adam Joseph <adam@westernsemico.com>
2023-08-16 23:28:37 -0700

    gcc: if atLeast 11, use deduplicated version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15803,6 +15803,7 @@ with pkgs;
           deduplicatedVersions = { # map from majorVersion to exact version
             "13" = "13.1.0";
             "12" = "12.3.0";
+            "11" = "11.4.0";
           };
           deduplicated = deduplicatedVersions ? "${version}";
           path = if deduplicated
