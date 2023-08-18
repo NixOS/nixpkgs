238fcb9281daaa47d20d72ff6daa3a2b610acded
Rafael Fernández López <ereslibre@ereslibre.es>
2023-08-18 23:26:55 +0200

    wasm-tools: 1.0.38 -> 1.0.39

--- pkgs/tools/misc/wasm-tools/default.nix
+++ pkgs/tools/misc/wasm-tools/default.nix
@@ -5,19 +5,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wasm-tools";
-  version = "1.0.38";
+  version = "1.0.39";
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = pname;
     rev = "${pname}-${version}";
-    hash = "sha256-SGNIGOpkREGu/bmojOh+TPlg75rKoa04u+BDiida6dI=";
+    hash = "sha256-NohA/2yzsEtMECvnntPyWvhEdVL63elmw0B+4Med6OE=";
     fetchSubmodules = true;
   };
 
   cargoLock.lockFile = ./Cargo.lock;
   postPatch = ''
-    ln -s ${./Cargo.lock} Cargo.lock
+    ln -sf ${./Cargo.lock} Cargo.lock
   '';
 
   cargoBuildFlags = [ "--package" "wasm-tools" ];
