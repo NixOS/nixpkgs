c53ef8a0a9c057d312f2e6b4b786a31a99a533db
Dawid Dziurla <dawidd0811@gmail.com>
2023-08-18 08:56:53 +0200

    molecule: init at 5.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6694,6 +6694,8 @@ self: super: with self; {
 
   mohawk = callPackage ../development/python-modules/mohawk { };
 
+  molecule = callPackage ../development/python-modules/molecule { };
+
   molecule-plugins = callPackage ../development/python-modules/molecule/plugins.nix { };
 
   monai = callPackage ../development/python-modules/monai { };
