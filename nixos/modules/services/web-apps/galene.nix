64a71aea985b7116f9334e9ecf5f9cc83ed46fb7
Alexandre Iooss <erdnaxe@crans.org>
2023-08-18 13:43:46 +0200

    nixos/galene: do not restrict AF_NETLINK
    Built-in TURN server requires AF_NETLINK address family.

--- nixos/modules/services/web-apps/galene.nix
+++ nixos/modules/services/web-apps/galene.nix
@@ -186,7 +186,7 @@ in
           ProtectSystem = "strict";
           ReadWritePaths = cfg.recordingsDir;
           RemoveIPC = true;
-          RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
+          RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_NETLINK" ];
           RestrictNamespaces = true;
           RestrictRealtime = true;
           RestrictSUIDSGID = true;
