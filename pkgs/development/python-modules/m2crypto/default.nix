c02f83305756e2ef4e957d8025f1cc747e84fc25
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-18 22:20:20 +0200

    python311Packages.m2crypto: 0.38.0 -> 0.39.0
    Changelog: https://gitlab.com/m2crypto/m2crypto/-/blob/0.39.0/CHANGES

--- pkgs/development/python-modules/m2crypto/default.nix
+++ pkgs/development/python-modules/m2crypto/default.nix
@@ -1,6 +1,5 @@
 { lib
 , buildPythonPackage
-, fetchpatch
 , fetchPypi
 , openssl
 , parameterized
@@ -11,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "m2crypto";
-  version = "0.38.0";
+  version = "0.39.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -19,31 +18,8 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "M2Crypto";
     inherit version;
-    hash = "sha256-mfImCjCQHJSajcbV+CzVMS/7iryS52YzuvIxu7yy3ss=";
+    hash = "sha256-JMD0cTWLixmtTIqp2hLoaAMLZcH9syedAG32DJUBM4o=";
   };
-
-  patches = [
-    # Use OpenSSL_version_num() instead of unrealiable parsing of .h file.
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/m2crypto/raw/42951285c800f72e0f0511cec39a7f49e970a05c/f/m2crypto-MR271-opensslversion.patch";
-      hash = "sha256-e1/NHgWza+kum76MUFSofq9Ko3pML67PUfqWjcwIl+A=";
-    })
-    # Changed required to pass tests on OpenSSL 3.0
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/m2crypto/raw/42951285c800f72e0f0511cec39a7f49e970a05c/f/m2crypto-0.38-ossl3-tests.patch";
-      hash = "sha256-B6JKoPh76+CIna6zmrvFj50DIp3pzg8aKyzz+Q5hqQ0=";
-    })
-    # Allow EVP tests fail on non-FIPS algorithms
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/m2crypto/raw/42951285c800f72e0f0511cec39a7f49e970a05c/f/m2crypto-0.38-ossl3-tests-evp.patch";
-      hash = "sha256-jMUAphVBQMFaOJSeYUCQMV3WSe9VDQqG6GY5fDQXZnA=";
-    })
-    # Fix the readline test https://gitlab.com/m2crypto/m2crypto/-/issues/286
-    (fetchpatch {
-      url = "https://gitlab.com/m2crypto/m2crypto/-/commit/b8addc7ad9990d1ba3786830ebd74aa8c939849d.patch";
-      hash = "sha256-M5mrmJVCT0fASvERLKa/MR+EY2hzRGIkyUfguVBPKNk=";
-    })
-  ];
 
   nativeBuildInputs = [
     swig2
