7e03f66725679528c73160da56565a2d10998884
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-18 13:50:16 +0000

    esphome: 2023.7.1 -> 2023.8.1

--- pkgs/tools/misc/esphome/default.nix
+++ pkgs/tools/misc/esphome/default.nix
@@ -16,14 +16,14 @@ let
 in
 python.pkgs.buildPythonApplication rec {
   pname = "esphome";
-  version = "2023.7.1";
+  version = "2023.8.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-warrNm07YXD0TEb91JQPr9ouDh7lC+YCQYOM3fed3Es=";
+    hash = "sha256-a85nNUVaa7CbYdI/xM30fp7i1gMRz18Ior4mwseFZqU=";
   };
 
   postPatch = ''
