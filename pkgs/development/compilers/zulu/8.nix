843430e78e23096817de24abef22ef00cc4bd41a
pokon548 <pokon548@hotmail.com>
2023-08-19 16:55:01 +0800

    zulu8: 8.68.0.19 -> 8.72.0.17

--- pkgs/development/compilers/zulu/8.nix
+++ pkgs/development/compilers/zulu/8.nix
@@ -21,15 +21,15 @@
 }:
 
 let
-  version = "8.68.0.19";
-  openjdk = "8.0.362";
+  version = "8.72.0.17";
+  openjdk = "8.0.382";
 
-  sha256_linux = "sha256-jNty0iJoXG+sp7v2fGCrwZWCSZfQ4tkYe8ERixQMKL0=";
-  sha256_darwin = "sha256-3/P3puM6a7tCHP5eZM6IzbdPrqnhY1dTa7QWss9M08M=";
+  sha256_linux = "sha256-exWlbyrgBb7aD4daJps9qtFP+hKWkwbMdFR4OFslupY=";
+  sha256_darwin = "sha256-3dTPIPGUeT6nb3gncNvEa4VTRyQIBJpp8oZadrT2ToE=";
 
   platform = if stdenv.isDarwin then "macosx" else "linux";
   hash = if stdenv.isDarwin then sha256_darwin else sha256_linux;
-  extension = if stdenv.isDarwin then "zip" else "tar.gz";
+  extension = "tar.gz";
 
   runtimeDependencies = [
     cups
