afab19410d84d2b3b501d167a35c5e9ebe9427ea
Dee Anzorge <d.anzorge@gmail.com>
2023-08-19 02:24:38 +0200

    streamlink: 6.0.1 -> 6.1.0
    https://github.com/streamlink/streamlink/releases/tag/6.1.0

--- pkgs/applications/video/streamlink/default.nix
+++ pkgs/applications/video/streamlink/default.nix
@@ -6,12 +6,12 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "streamlink";
-  version = "6.0.1";
+  version = "6.1.0";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0Qpil/bh2F+WaG0zv4yxEzx6e1fv3t9xed+nhT+NC7U=";
+    hash = "sha256-FwsgJ9TYBzCHxYlBwxrsOEy/mQH8tAH4JOkZrjh8Q4U=";
   };
 
   nativeCheckInputs = with python3Packages; [
