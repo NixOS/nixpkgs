838d88ef4723c7f42316e7c3b9d7e620e53cc2f1
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-19 11:39:51 +0200

    python310Packages.pomegranate: 0.14.8 -> 1.0.0
    Diff: jmschrei/pomegranate@refs/tags/v0.14.8...v1.0.0
    
    Changelog: https://github.com/jmschrei/pomegranate/releases/tag/v1.0.0

--- pkgs/development/python-modules/pomegranate/default.nix
+++ pkgs/development/python-modules/pomegranate/default.nix
@@ -1,40 +1,65 @@
-{ stdenv
-, lib
+{ lib
+, stdenv
+, apricot-select
 , buildPythonPackage
 , fetchFromGitHub
-, fetchpatch
-, numpy
-, scipy
-, cython
 , networkx
-, joblib
-, pandas
 , nose
-, pyyaml
+, numpy
+, pytestCheckHook
+, pythonOlder
+, scikit-learn
+, scipy
+, torch
 }:
-
 
 buildPythonPackage rec {
   pname = "pomegranate";
-  version = "0.14.8";
+  version = "1.0.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
-    repo = pname;
     owner = "jmschrei";
-    # no tags for recent versions: https://github.com/jmschrei/pomegranate/issues/974
-    rev = "0652e955c400bc56df5661db3298a06854c7cce8";
-    sha256 = "16g49nl2bgnh6nh7bd21s393zbksdvgp9l13ww2diwhplj6hlly3";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "sha256-EnxKlRRfsOIDLAhYOq7bUSbI/NvPoSyYCZ9D5VCXFGQ=";
   };
 
-  propagatedBuildInputs = [ numpy scipy cython networkx joblib pyyaml ];
+  propagatedBuildInputs = [
+    apricot-select
+    networkx
+    numpy
+    scikit-learn
+    scipy
+    torch
+  ];
 
-  nativeCheckInputs = [ pandas nose ];  # as of 0.13.5, it depends explicitly on nose, rather than pytest.
+  nativeCheckInputs = [
+    nose
+    pytestCheckHook
+  ];
+
+  disabledTests = [
+    # ValueError and AssertionError
+    "test_categorical_chow"
+    "test_categorical_exact"
+    "test_categorical_learn"
+    "test_learn_structure"
+    "test_sample"
+  ];
+
+  pythonImportsCheck = [
+    "pomegranate"
+  ];
 
   meta = with lib; {
-    broken = stdenv.isDarwin;
-    description = "Probabilistic and graphical models for Python, implemented in cython for speed";
+    description = "Probabilistic and graphical models for Python";
     homepage = "https://github.com/jmschrei/pomegranate";
+    changelog = "https://github.com/jmschrei/pomegranate/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ rybern ];
+    broken = stdenv.isDarwin;
   };
 }
