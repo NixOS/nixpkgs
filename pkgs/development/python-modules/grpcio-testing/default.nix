3199cf23621dfec14f671e1a82245c418c0946b3
Mario Rodas <marsam@users.noreply.github.com>
2023-08-19 04:20:00 +0000

    python310Packages.grpcio-testing: 1.56.2 -> 1.57.0

--- pkgs/development/python-modules/grpcio-testing/default.nix
+++ pkgs/development/python-modules/grpcio-testing/default.nix
@@ -9,19 +9,19 @@
 
 buildPythonPackage rec {
   pname = "grpcio-testing";
-  version = "1.56.2";
+  version = "1.57.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Cg1dONvESb1ayz5ogv9AWHywqd0LdVsRDpIwtos+0SA=";
+    hash = "sha256-xFMqJlY+Ehn0K3bXqdiGEvL2X3Pm7IPhHHPWTi8ecOk=";
   };
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace "'grpcio>={version}'.format(version=grpc_version.VERSION)" "'grpcio'"
+      --replace '"grpcio>={version}".format(version=grpc_version.VERSION)' '"grpcio"'
   '';
 
   propagatedBuildInputs = [
