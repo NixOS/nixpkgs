2057c1e0aee3c01a56105ee32e63f3ded41983d6
traxys <quentin+dev@familleboyer.net>
2023-08-19 10:43:01 +0200

    python310Packages.html-tag-names: init at 0.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4954,6 +4954,8 @@ self: super: with self; {
 
   html-sanitizer = callPackage ../development/python-modules/html-sanitizer { };
 
+  html-tag-names = callPackage ../development/python-modules/html-tag-names { };
+
   html-text = callPackage ../development/python-modules/html-text { };
 
   htseq = callPackage ../development/python-modules/htseq { };
