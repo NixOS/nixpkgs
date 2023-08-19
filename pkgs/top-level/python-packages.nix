df53ba2ca036dda30f3736ee6dbed79a18226800
traxys <quentin+dev@familleboyer.net>
2023-08-19 10:30:30 +0200

    python310Packages.cssbeautifier: init at 1.14.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2331,6 +2331,8 @@ self: super: with self; {
 
   csrmesh  = callPackage ../development/python-modules/csrmesh { };
 
+  cssbeautifier = callPackage ../development/python-modules/cssbeautifier { };
+
   csscompressor = callPackage ../development/python-modules/csscompressor { };
 
   cssmin = callPackage ../development/python-modules/cssmin { };
