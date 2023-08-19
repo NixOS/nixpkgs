f64d4203b6c5707fab623af895923dc6d262c866
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2023-08-19 13:59:45 +0300

    vscode-wikitext: add rapiteanu as maintainer

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -2929,6 +2929,7 @@ let
           downloadPage = "https://marketplace.visualstudio.com/items?itemName=RoweWilsonFrederiskHolme.wikitext";
           homepage = "https://github.com/Frederisk/Wikitext-VSCode-Extension";
           license = lib.licenses.mit;
+          maintainers = [ lib.maintainers.rapiteanu ];
         };
       };
 
