b8ecbc9e1309555b536fe3168dd887e709188161
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-20 20:58:08 +0200

    python311Packages.fido2: add changelog to meta

--- pkgs/development/python-modules/fido2/default.nix
+++ pkgs/development/python-modules/fido2/default.nix
@@ -32,6 +32,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Provides library functionality for FIDO 2.0, including communication with a device over USB.";
     homepage = "https://github.com/Yubico/python-fido2";
+    changelog = "https://github.com/Yubico/python-fido2/releases/tag/${version}";
     license = licenses.bsd2;
     maintainers = with maintainers; [ prusnak ];
   };
