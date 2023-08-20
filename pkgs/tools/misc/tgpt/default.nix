644fe2415a4cd118b6bfe8da04b2470f51180dde
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-20 12:34:43 +0200

    tgpt: 1.7.1 -> 1.7.2
    Diff: https://github.com/aandrew-me/tgpt/compare/refs/tags/v1.7.1...v1.7.2
    
    Changelog: https://github.com/aandrew-me/tgpt/releases/tag/v1.7.2

--- pkgs/tools/misc/tgpt/default.nix
+++ pkgs/tools/misc/tgpt/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "tgpt";
-  version = "1.7.1";
+  version = "1.7.2";
 
   src = fetchFromGitHub {
     owner = "aandrew-me";
     repo = "tgpt";
     rev = "refs/tags/v${version}";
-    hash = "sha256-WJYSipaq3lL5NscfQQCxuX5maprn9Z3LSQ0Dkn9edKo=";
+    hash = "sha256-e1OPDhZuRbm6DqvmB1ZiXVRSnjaZEFi2gSIMnfIIHKE=";
   };
 
   vendorHash = "sha256-2I5JJWxM6aZx0eZu7taUTL11Y/5HIrXYC5aezrTbbsM=";
