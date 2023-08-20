3b3c14114624fa834fdeca7e97e04d76af66dd18
John Garcia <jgarcia3788@yahoo.co.uk>
2023-08-20 13:45:28 +0100

    get_iplayer: Fix: Add missing runHook calls

--- pkgs/applications/misc/get_iplayer/default.nix
+++ pkgs/applications/misc/get_iplayer/default.nix
@@ -22,11 +22,14 @@ perlPackages.buildPerlPackage rec {
   outputs = [ "out" "man" ];
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin $out/share/man/man1
     cp get_iplayer $out/bin
     wrapProgram $out/bin/get_iplayer --suffix PATH : ${lib.makeBinPath [ atomicparsley ffmpeg flvstreamer rtmpdump ]} --prefix PERL5LIB : $PERL5LIB
     cp get_iplayer.1 $out/share/man/man1
+    runHook postInstall
   '';
+
   postInstall = lib.optionalString stdenv.isDarwin ''
     shortenPerlShebang $out/bin/.get_iplayer-wrapped
   '';
