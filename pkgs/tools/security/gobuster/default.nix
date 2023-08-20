40086199fc37e508c21b3c0198c085e303bc4738
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-20 12:56:31 +0200

    gobuster: 3.5.0 -> 3.6.0
    Diff: https://github.com/OJ/gobuster/compare/v3.5.0...v3.6.0
    
    Changelog: https://github.com/OJ/gobuster/releases/tag/v3.6.0

--- pkgs/tools/security/gobuster/default.nix
+++ pkgs/tools/security/gobuster/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "gobuster";
-  version = "3.5.0";
+  version = "3.6.0";
 
   src = fetchFromGitHub {
     owner = "OJ";
     repo = "gobuster";
     rev = "v${version}";
-    hash = "sha256-Ohv/FgMbniItbrcrncAe9QKVjrhxoZ80BGYJmJtJpPk=";
+    hash = "sha256-LZL9Zje2u0v6iAQinfjflvusV57ys5J5Il6Q7br3Suc=";
   };
 
-  vendorHash = "sha256-ZbY5PyXKcTB9spVGfW2Qhj8SV9alOSH0DyXx1dh/NgQ=";
+  vendorHash = "sha256-w+G5PsWXhKipjYIHtz633sia+Wg9FSFVpcugEl8fp0E=";
 
   meta = with lib; {
     description = "Tool used to brute-force URIs, DNS subdomains, Virtual Host names on target web servers";
