72cc7227400363a5b5ad931f3a82e588e1bcc55d
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2023-08-20 14:58:33 -0600

    pre-commit: fix import bug for built-in hooks
    Pre-commit's "system" language is a sort of escape hach from the
    encapsulation which pre-commit typically provides.  It just runs
    commands without thinking too hard about setting up or tearing down
    environments.
    
    But it is used for two things: user-provided hooks which reference
    things in calling environment, and pre-commit-provided hooks which
    live in the pre-commit package (I'm calling these "meta" hooks).
    
    The challenge becomes: which PATH and PYTHONPATH variables do we expose
    to pre-commit?  If it's the calling environment, the meta hooks break,
    and if it's the packaged environment, the external-facing system hooks
    break.
    
    In order to make both of these work at the same time, this commit
    separates 'system' into two languages: 'system', and 'meta'. System
    hooks get access to the calling environment, and meta hooks get access
    to the packaged environment.
    
    Co-Authored-By: Victor Engmark <victor@engmark.name>

--- pkgs/tools/misc/pre-commit/default.nix
+++ pkgs/tools/misc/pre-commit/default.nix
@@ -33,6 +33,7 @@ buildPythonApplication rec {
 
   patches = [
     ./languages-use-the-hardcoded-path-to-python-binaries.patch
+    ./system-hooks-use-calling-environment-paths.patch
     ./hook-tmpl.patch
     ./pygrep-pythonpath.patch
   ];
@@ -107,11 +108,23 @@ buildPythonApplication rec {
     deactivate
   '';
 
-  # Propagating dependencies leaks them through $PYTHONPATH which causes issues
-  # when used in nix-shell.
   postFixup = ''
+    # Propagating inputs causes them to show up in $PYTHONPATH in a nix shell,
+    # this is problematic because they might conflict with the user's dependencies,
+    # so we remove them.
     rm $out/nix-support/propagated-build-inputs
+
+    # the wrapper script modifies PATH and PYTHONPATH to point into the nix store
+    # this prevents 'system' type hooks from referencing the calling environment
+    # save untainted copies of that environment for such cases
+    sed -i '2i\export "OUTER_PYTHONPATH=$PYTHONPATH"' $out/bin/pre-commit
+    sed -i '2i\export "OUTER_PATH=$PATH"' $out/bin/pre-commit
   '';
+
+  # Pre-commit also has built-in hooks which it needs to import from the nix store
+  # we hid them from user devshells by removing propated-build-inputs, but now we
+  # need to restore them for pre-commit's use (but only inside of the wrapper).
+  makeWrapperArgs = ''--set PYTHONPATH $PYTHONPATH'';
 
   disabledTests = [
     # ERROR: The install method you used for conda--probably either `pip install conda`
@@ -181,8 +194,8 @@ buildPythonApplication rec {
     "pre_commit"
   ];
 
-  passthru.tests.version = testers.testVersion {
-    package = pre-commit;
+  passthru.tests = callPackage ./tests.nix {
+    inherit git pre-commit;
   };
 
   meta = with lib; {
