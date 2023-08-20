5dcaeac10444b30ad487dbe9615a516214c804f8
Artturin <Artturin@artturin.com>
2023-08-20 10:40:24 +0300

    nixos/zfs: disable redundant scheduler
    I (according to the comment) wrote this somewhere and since then it has
    spread to many configs.
    
    https://github.com/search?q=artturin+ENV%7BID_FS_TYPE%7D%3D%3D%22zfs_member%22%2C+ATTR%7B..%2Fqueue%2Fscheduler%7D%3D%22none%22&type=code
    
    https://github.com/NixOS/nixpkgs/issues/169457#issuecomment-1523873402

--- nixos/modules/tasks/filesystems/zfs.nix
+++ nixos/modules/tasks/filesystems/zfs.nix
@@ -662,6 +662,11 @@ in
         ];
       };
 
+      # ZFS already has its own scheduler. Without this my(@Artturin) computer froze for a second when I nix build something.
+      services.udev.extraRules = ''
+        ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+      '';
+
       environment.etc = genAttrs
         (map
           (file: "zfs/zed.d/${file}")
