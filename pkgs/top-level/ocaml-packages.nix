991356c8074eedea2ee40879188b9a8a622b3b4b
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-08-21 07:33:39 +0200

    ocamlPackages.qcheck-lin: init at 0.2

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1464,6 +1464,8 @@ let
 
     qcheck-core = callPackage ../development/ocaml-modules/qcheck/core.nix { };
 
+    qcheck-lin = callPackage ../development/ocaml-modules/qcheck/lin.nix { };
+
     qcheck-multicoretests-util = callPackage ../development/ocaml-modules/qcheck/multicoretests-util.nix { };
 
     qcheck-ounit = callPackage ../development/ocaml-modules/qcheck/ounit.nix { };
