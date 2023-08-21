f4fbfce674e8d9a91fd3f6a7e1473462e95eae99
Markus Kowalewski <markus.kowalewski@gmail.com>
2023-08-21 22:16:10 +0200

    slurm: 23.02.3.1 -> 23.02.4.1
    (cherry picked from commit 933880f90414bad30cd2c06907d1dae49140ce13)

--- pkgs/servers/computing/slurm/default.nix
+++ pkgs/servers/computing/slurm/default.nix
@@ -14,7 +14,7 @@
 
 stdenv.mkDerivation rec {
   pname = "slurm";
-  version = "23.02.3.1";
+  version = "23.02.4.1";
 
   # N.B. We use github release tags instead of https://www.schedmd.com/downloads.php
   # because the latter does not keep older releases.
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
     repo = "slurm";
     # The release tags use - instead of .
     rev = "${pname}-${builtins.replaceStrings ["."] ["-"] version}";
-    sha256 = "sha256-YMm8a7uyZpPEuWQcgddX+0Me+ndgbYoDnC+sbmN/Hh8=";
+    sha256 = "sha256-sNZqC6F7Q05AqkKVVUG4mCO3JXRjVZBppt3VgRHf5+o=";
   };
 
   outputs = [ "out" "dev" ];
