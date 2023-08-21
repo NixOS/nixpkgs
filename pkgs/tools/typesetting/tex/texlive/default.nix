2379bc3d272f81e8fcf747705a170e667ea8bdde
apfelkuchen06 <apfelkuchen@hrnz.li>
2023-08-21 22:41:54 +0200

    texlive: indent body of attrset
    this is purely a whitespace change that was separated from the previous commit
    to make the diff easier to read

--- pkgs/tools/typesetting/tex/texlive/default.nix
+++ pkgs/tools/typesetting/tex/texlive/default.nix
@@ -75,83 +75,82 @@ let
         inherit tlpdb;
       };
     in  {
+      inherit callPackage;
 
-    inherit callPackage;
+      tlpdb = {
+        # nested in an attribute set to prevent them from appearing in search
+        nix = tlpdbNix;
+        xz = tlpdbxz;
+      };
 
-    tlpdb = {
-      # nested in an attribute set to prevent them from appearing in search
-      nix = tlpdbNix;
-      xz = tlpdbxz;
-    };
+      bin = assert assertions; callPackage ./bin.nix { };
 
-    bin = assert assertions; callPackage ./bin.nix { };
+      combine = assert assertions; callPackage ./combine.nix { };
 
-    combine = assert assertions; callPackage ./combine.nix { };
+      pkgs = let
+        buildTeXLivePackage = callPackage ./build-texlive-package.nix { texliveBinaries = self.bin; };
 
-    pkgs = let
-      buildTeXLivePackage = callPackage ./build-texlive-package.nix { texliveBinaries = self.bin; };
+        overrides = callPackage ./tlpdb-overrides.nix { tlpdbxz = self.tlpdb.xz; };
 
-      overrides = callPackage ./tlpdb-overrides.nix { tlpdbxz = self.tlpdb.xz; };
+        overriddenTlpdb = overrides tlpdb;
 
-      overriddenTlpdb = overrides tlpdb;
+        makePackageSet = tlpdb:
+          lib.mapAttrs (pname: { revision, extraRevision ? "", ... }@args:
+            buildTeXLivePackage (args
+              # NOTE: the fixed naming scheme must match generate-fixed-hashes.nix
+              // { inherit mirrors pname; fixedHashes = fixedHashes."${pname}-${toString revision}${extraRevision}" or { }; }
+              // lib.optionalAttrs (args ? deps) { deps = map (n: self.pkgs.${n}) (args.deps or [ ]); })
+          ) tlpdb;
 
-      makePackageSet = tlpdb:
-        lib.mapAttrs (pname: { revision, extraRevision ? "", ... }@args:
-          buildTeXLivePackage (args
-            # NOTE: the fixed naming scheme must match generate-fixed-hashes.nix
-            // { inherit mirrors pname; fixedHashes = fixedHashes."${pname}-${toString revision}${extraRevision}" or { }; }
-            // lib.optionalAttrs (args ? deps) { deps = map (n: self.pkgs.${n}) (args.deps or [ ]); })
-        ) tlpdb;
-
-    in lib.makeOverridable makePackageSet overriddenTlpdb;
+      in lib.makeOverridable makePackageSet overriddenTlpdb;
 
 
-    # Pre-defined combined packages for TeX Live schemes,
-    # to make nix-env usage more comfortable and build selected on Hydra.
-    combined = with lib;
-      let
-        # these license lists should be the sorted union of the licenses of the packages the schemes contain.
-        # The correctness of this collation is tested by tests.texlive.licenses
-        licenses = with lib.licenses; {
-          scheme-basic = [ free gfl gpl1Only gpl2 gpl2Plus knuth lgpl21 lppl1 lppl13c mit ofl publicDomain ];
-          scheme-context = [ bsd2 bsd3 cc-by-sa-40 free gfl gfsl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus knuth lgpl2 lgpl21
-            lppl1 lppl13c mit ofl publicDomain x11 ];
-          scheme-full = [ artistic1-cl8 artistic2 asl20 bsd2 bsd3 bsdOriginal cc-by-10 cc-by-40 cc-by-sa-10 cc-by-sa-20
-            cc-by-sa-30 cc-by-sa-40 cc0 fdl13Only free gfl gfsl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus isc knuth
-            lgpl2 lgpl21 lgpl3 lppl1 lppl12 lppl13a lppl13c mit ofl publicDomain x11 ];
-          scheme-gust = [ artistic1-cl8 asl20 bsd2 bsd3 cc-by-40 cc-by-sa-40 cc0 fdl13Only free gfl gfsl gpl1Only gpl2
-            gpl2Plus gpl3 gpl3Plus knuth lgpl2 lgpl21 lppl1 lppl12 lppl13a lppl13c mit ofl publicDomain x11 ];
-          scheme-infraonly = [ gpl2 gpl2Plus lgpl21 ];
-          scheme-medium = [ artistic1-cl8 asl20 bsd2 bsd3 cc-by-40 cc-by-sa-20 cc-by-sa-30 cc-by-sa-40 cc0 fdl13Only
-            free gfl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus isc knuth lgpl2 lgpl21 lgpl3 lppl1 lppl12 lppl13a lppl13c mit ofl
-            publicDomain x11 ];
-          scheme-minimal = [ free gpl1Only gpl2 gpl2Plus knuth lgpl21 lppl1 lppl13c mit ofl publicDomain ];
-          scheme-small = [ asl20 cc-by-40 cc-by-sa-40 cc0 fdl13Only free gfl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus knuth
-            lgpl2 lgpl21 lppl1 lppl12 lppl13a lppl13c mit ofl publicDomain x11 ];
-          scheme-tetex = [ artistic1-cl8 asl20 bsd2 bsd3 cc-by-40 cc-by-sa-10 cc-by-sa-20 cc-by-sa-30 cc-by-sa-40 cc0
-            fdl13Only free gfl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus isc knuth lgpl2 lgpl21 lgpl3 lppl1 lppl12 lppl13a
-            lppl13c mit ofl publicDomain x11];
-        };
-      in recurseIntoAttrs (
-      mapAttrs
-        (pname: attrs:
-          addMetaAttrs rec {
-            description = "TeX Live environment for ${pname}";
-            platforms = lib.platforms.all;
-            maintainers = with lib.maintainers;  [ veprbl ];
-            license = licenses.${pname};
+      # Pre-defined combined packages for TeX Live schemes,
+      # to make nix-env usage more comfortable and build selected on Hydra.
+      combined = with lib;
+        let
+          # these license lists should be the sorted union of the licenses of the packages the schemes contain.
+          # The correctness of this collation is tested by tests.texlive.licenses
+          licenses = with lib.licenses; {
+            scheme-basic = [ free gfl gpl1Only gpl2 gpl2Plus knuth lgpl21 lppl1 lppl13c mit ofl publicDomain ];
+            scheme-context = [ bsd2 bsd3 cc-by-sa-40 free gfl gfsl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus knuth lgpl2 lgpl21
+              lppl1 lppl13c mit ofl publicDomain x11 ];
+            scheme-full = [ artistic1-cl8 artistic2 asl20 bsd2 bsd3 bsdOriginal cc-by-10 cc-by-40 cc-by-sa-10 cc-by-sa-20
+              cc-by-sa-30 cc-by-sa-40 cc0 fdl13Only free gfl gfsl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus isc knuth
+              lgpl2 lgpl21 lgpl3 lppl1 lppl12 lppl13a lppl13c mit ofl publicDomain x11 ];
+            scheme-gust = [ artistic1-cl8 asl20 bsd2 bsd3 cc-by-40 cc-by-sa-40 cc0 fdl13Only free gfl gfsl gpl1Only gpl2
+              gpl2Plus gpl3 gpl3Plus knuth lgpl2 lgpl21 lppl1 lppl12 lppl13a lppl13c mit ofl publicDomain x11 ];
+            scheme-infraonly = [ gpl2 gpl2Plus lgpl21 ];
+            scheme-medium = [ artistic1-cl8 asl20 bsd2 bsd3 cc-by-40 cc-by-sa-20 cc-by-sa-30 cc-by-sa-40 cc0 fdl13Only
+              free gfl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus isc knuth lgpl2 lgpl21 lgpl3 lppl1 lppl12 lppl13a lppl13c mit ofl
+              publicDomain x11 ];
+            scheme-minimal = [ free gpl1Only gpl2 gpl2Plus knuth lgpl21 lppl1 lppl13c mit ofl publicDomain ];
+            scheme-small = [ asl20 cc-by-40 cc-by-sa-40 cc0 fdl13Only free gfl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus knuth
+              lgpl2 lgpl21 lppl1 lppl12 lppl13a lppl13c mit ofl publicDomain x11 ];
+            scheme-tetex = [ artistic1-cl8 asl20 bsd2 bsd3 cc-by-40 cc-by-sa-10 cc-by-sa-20 cc-by-sa-30 cc-by-sa-40 cc0
+              fdl13Only free gfl gpl1Only gpl2 gpl2Plus gpl3 gpl3Plus isc knuth lgpl2 lgpl21 lgpl3 lppl1 lppl12 lppl13a
+              lppl13c mit ofl publicDomain x11];
+          };
+        in recurseIntoAttrs (
+        mapAttrs
+          (pname: attrs:
+            addMetaAttrs rec {
+              description = "TeX Live environment for ${pname}";
+              platforms = lib.platforms.all;
+              maintainers = with lib.maintainers;  [ veprbl ];
+              license = licenses.${pname};
+            }
+            (self.combine {
+              ${pname} = attrs;
+              extraName = "combined" + lib.removePrefix "scheme" pname;
+              extraVersion = with version; if final then "-final" else ".${year}${month}${day}";
+            })
+          )
+          { inherit (self.pkgs)
+              scheme-basic scheme-context scheme-full scheme-gust scheme-infraonly
+              scheme-medium scheme-minimal scheme-small scheme-tetex;
           }
-          (self.combine {
-            ${pname} = attrs;
-            extraName = "combined" + lib.removePrefix "scheme" pname;
-            extraVersion = with version; if final then "-final" else ".${year}${month}${day}";
-          })
-        )
-        { inherit (self.pkgs)
-            scheme-basic scheme-context scheme-full scheme-gust scheme-infraonly
-            scheme-medium scheme-minimal scheme-small scheme-tetex;
-        }
-    );
+      );
   };
 
   texlive = makeScopeWithSplicing' {
