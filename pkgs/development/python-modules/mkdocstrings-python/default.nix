9cac0ea125d5e245c098cc55e2ee6554c3566e9b
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-21 18:09:38 +0200

    python310Packages.mkdocstrings-python: update disabled

--- pkgs/development/python-modules/mkdocstrings-python/default.nix
+++ pkgs/development/python-modules/mkdocstrings-python/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   version = "1.5.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "mkdocstrings";
