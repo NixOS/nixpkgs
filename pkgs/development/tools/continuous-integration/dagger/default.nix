8da939faa14c83da750472283c6092bb99dc7323
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-22 15:52:31 +0000

    dagger: 0.6.4 -> 0.8.4

--- pkgs/development/tools/continuous-integration/dagger/default.nix
+++ pkgs/development/tools/continuous-integration/dagger/default.nix
@@ -2,23 +2,23 @@
 
 buildGoModule rec {
   pname = "dagger";
-  version = "0.6.4";
+  version = "0.8.4";
 
   src = fetchFromGitHub {
     owner = "dagger";
     repo = "dagger";
     rev = "v${version}";
-    hash = "sha256-ms736KXQjqWnIU7WHQ4k1r6qwCp07N6tufCx9s9biVc=";
+    hash = "sha256-iFuPbSat555QHPqqP6j/6uTid19x1+OtRHADmGxTYzs=";
   };
 
-  vendorHash = "sha256-zssmvu1s7O2DdvjxUYkR13CKOe4EYn50RFRRIPBJ9qg=";
+  vendorHash = "sha256-DWmHq8BIR00QTh3ZcbEgTtbHwTmsMFAhV7kQVRSKNdQ=";
   proxyVendor = true;
 
   subPackages = [
     "cmd/dagger"
   ];
 
-  ldflags = [ "-s" "-w" "-X=github.com/dagger/dagger/internal/engine.Version=${version}" ];
+  ldflags = [ "-s" "-w" "-X github.com/dagger/dagger/engine.Version=${version}" ];
 
   passthru.tests.version = testers.testVersion {
     package = dagger;
