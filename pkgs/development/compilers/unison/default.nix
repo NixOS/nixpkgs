fedfaffdbe7f6a58512b8f3edd10363044133016
Rafael Fernández López <ereslibre@ereslibre.es>
2023-08-22 10:21:44 +0200

    unison: M5c -> M5d

--- pkgs/development/compilers/unison/default.nix
+++ pkgs/development/compilers/unison/default.nix
@@ -11,17 +11,17 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "unison-code-manager";
-  version = "M5c";
+  version = "M5d";
 
   src = if stdenv.isDarwin then
     fetchurl {
       url = "https://github.com/unisonweb/unison/releases/download/release/${finalAttrs.version}/ucm-macos.tar.gz";
-      hash = "sha256-LTpsKwiV0ZxReLcuzoJYuMP1jN6v8M/z6mUqH9s5A+g=";
+      hash = "sha256-P4IyGAliHsNf33N6v+SdSrEgv4Hr2e/pN/lT+BcAEXk=";
     }
   else
     fetchurl {
       url = "https://github.com/unisonweb/unison/releases/download/release/${finalAttrs.version}/ucm-linux.tar.gz";
-      hash = "sha256-6gSX8HOv/K4zFTz1O4VvrpWR9+iQyLOO6vIRv6oVw/c=";
+      hash = "sha256-sjnTzPDTkj8ct6JiOrQ/wee9yGlb9XqVDsXXXAPtZqA=";
     };
 
   # The tarball is just the prebuilt binary, in the archive root.
