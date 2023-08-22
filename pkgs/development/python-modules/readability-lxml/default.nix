262ff67b07ee70ad6cb53b761768f1001d0c2e3c
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-08-22 10:28:41 +0200

    Update pkgs/development/python-modules/readability-lxml/default.nix
    Co-authored-by: OTABI Tomoya <tomoya.otabi@gmail.com>

--- pkgs/development/python-modules/readability-lxml/default.nix
+++ pkgs/development/python-modules/readability-lxml/default.nix
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     timeout-decorator
   ];
 
-  disabledTests = [
+  disabledTests = lib.optionals stdenv.isDarwin [
     # Test is broken on darwin. Fix in master from https://github.com/buriy/python-readability/pull/178
     "test_many_repeated_spaces"
   ];
