3c21c5450620c354e19d4597f85c41e99c0e10d1
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-22 18:20:29 +0200

    python311Packages.zeroconf: 0.80.0 -> 0.81.0
    Diff: https://github.com/jstasiak/python-zeroconf/compare/refs/tags/0.80.0...0.81.0
    
    Changelog: https://github.com/python-zeroconf/python-zeroconf/releases/tag/0.81.0

--- pkgs/development/python-modules/zeroconf/default.nix
+++ pkgs/development/python-modules/zeroconf/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "zeroconf";
-  version = "0.80.0";
+  version = "0.81.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "jstasiak";
     repo = "python-zeroconf";
     rev = "refs/tags/${version}";
-    hash = "sha256-+NxLQGgTFHOPyOs8yoZvtZj0D42V6qma+PHgTGwPJsg=";
+    hash = "sha256-04kB3geiLG0fpAOhPNuT7aLX6qvpIHf3ofNO6POTYn8=";
   };
 
   nativeBuildInputs = [
