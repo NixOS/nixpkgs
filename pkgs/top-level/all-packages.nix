ebcc9d17c2219ffc3d9cdea5ae3f888abaac3bdc
figsoda <figsoda@pm.me>
2023-08-22 20:15:28 -0400

    safecloset: init at 1.3.0
    https://github.com/Canop/safecloset

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12809,6 +12809,8 @@ with pkgs;
 
   safe-rm = callPackage ../tools/system/safe-rm { };
 
+  safecloset = callPackage ../applications/misc/safecloset { };
+
   safeeyes = callPackage ../applications/misc/safeeyes { };
 
   sagoin = callPackage ../tools/misc/sagoin { };
