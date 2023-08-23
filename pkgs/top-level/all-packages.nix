dfe898c8014622334a2ad541f1837b5319905b34
Sefa Eyeoglu <contact@scrumplex.net>
2023-08-24 01:47:22 +0200

    glfw-wayland-minecraft: init at unstable-2023-06-01 (#243950)
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21578,6 +21578,7 @@ with pkgs;
   glfw-wayland = glfw.override {
     waylandSupport = true;
   };
+  glfw-wayland-minecraft = callPackage ../development/libraries/glfw/3.x-wayland-minecraft.nix {};
   glfw2 = callPackage ../development/libraries/glfw/2.x.nix { };
   glfw3 = callPackage ../development/libraries/glfw/3.x.nix {
     inherit (darwin.apple_sdk.frameworks) Carbon Cocoa Kernel OpenGL;
