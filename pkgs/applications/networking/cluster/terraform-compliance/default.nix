59bea0616f43e3a73b320eb01286f22e7e068388
kashw2 <supra4keanu@hotmail.com>
2023-08-23 10:38:04 +1000

    terraform-compliance: 1.3.34 -> 1.3.43

--- pkgs/applications/networking/cluster/terraform-compliance/default.nix
+++ pkgs/applications/networking/cluster/terraform-compliance/default.nix
@@ -5,20 +5,21 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "terraform-compliance";
-  version = "1.3.34";
+  version = "1.3.43";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "terraform-compliance";
     repo = "cli";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-1TFLpBwkpMMdiJJfVvDXlJg4SXWQ8VV605wMFGU+InQ=";
+    sha256 = "sha256-Otz0jXSqqg8u8jVnqHTpPW/oyb+Qq2NywgxYPtIAKEc=";
   };
 
   postPatch = ''
     substituteInPlace setup.py \
       --replace "IPython==7.16.1" "IPython" \
-      --replace "diskcache==5.1.0" "diskcache>=5.1.0"
+      --replace "diskcache==5.1.0" "diskcache>=5.1.0" \
+      --replace "radish-bdd==0.13.1" "radish-bdd" \
   '';
 
   propagatedBuildInputs = with python3.pkgs; [
@@ -33,6 +34,7 @@ python3.pkgs.buildPythonApplication rec {
     netaddr
     radish-bdd
     semver
+    orjson
   ];
 
   nativeCheckInputs = with python3.pkgs; [
