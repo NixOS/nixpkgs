f901eb6567c37bcbfed50bd31d5ecae021df5510
Anderson Torres <torres.anderson.85@protonmail.com>
2023-08-22 22:31:36 -0300

    wafHook: assimilated under waf.passthru.hook

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14825,7 +14825,7 @@ with pkgs;
   volumeicon = callPackage ../tools/audio/volumeicon { };
 
   waf = callPackage ../development/tools/build-managers/waf { };
-  wafHook = callPackage ../development/tools/build-managers/waf/hook.nix { };
+  wafHook = waf.hook;
 
   waf-tester = callPackage ../tools/security/waf-tester { };
 
