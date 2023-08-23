dda04f9cfdebcbc297506f331a5590452e20e308
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-08-23 13:24:30 +0200

    obs-studio-plugins.input-overlay: 5.0.0 -> 5.0.4
    Diff: https://github.com/univrsal/input-overlay/compare/v5.0.0...v5.0.4
    
    https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=obs-plugin-input-overlay
    
    Co-authored-by: Franz Pletz <fpletz@fnordicwalking.de>

--- pkgs/applications/video/obs-studio/plugins/input-overlay.nix
+++ pkgs/applications/video/obs-studio/plugins/input-overlay.nix
@@ -8,25 +8,34 @@
 , xorg
 , libxkbcommon
 , libxkbfile
+, SDL2
 }:
 
 stdenv.mkDerivation rec {
   pname = "obs-input-overlay";
-  version = "5.0.0";
+  version = "5.0.4";
   src = fetchFromGitHub {
     owner = "univrsal";
     repo = "input-overlay";
     rev = "v${version}";
-    sha256 = "sha256-kpVAvQpBU8TxHAFcx/ok67++4MHh5saoRHJc5XpY4YQ=";
+    sha256 = "sha256-MON68yjHUOF/bggWqhw8D0+23HpKQN3jWs+5lLAacaQ=";
     fetchSubmodules = true;
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
   buildInputs = [
-    obs-studio libuiohook qtbase
+    obs-studio libuiohook qtbase SDL2
     xorg.libX11 xorg.libXau xorg.libXdmcp xorg.libXtst xorg.libXext
     xorg.libXi xorg.libXt xorg.libXinerama libxkbcommon libxkbfile
   ];
+
+  cmakeFlags = [
+    "-DCMAKE_CXX_FLAGS=-msse4.1"
+  ];
+
+  postUnpack = ''
+    sed -i '/set(CMAKE_CXX_FLAGS "-march=native")/d' 'source/CMakeLists.txt'
+  '';
 
   postInstall = ''
     mkdir $out/lib $out/share
