3fc4a5dc8c624669afc3f2119c66ff586022500d
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-08-23 21:48:57 +0100

    retool: init at unstable-2023-08-24

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6212,6 +6212,8 @@ with pkgs;
 
   reg = callPackage ../tools/virtualization/reg { };
 
+  retool = callPackage ../applications/misc/retool { };
+
   rex = callPackage ../tools/system/rex { };
 
   river = callPackage ../applications/window-managers/river { };
