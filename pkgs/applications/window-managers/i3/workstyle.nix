89bae6c2fac7a243e9e83cc492fb575c92471f9a
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-08-23 15:29:43 +0200

    Add cargo hash, fix rev and hash

--- pkgs/applications/window-managers/i3/workstyle.nix
+++ pkgs/applications/window-managers/i3/workstyle.nix
@@ -5,16 +5,26 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "workstyle";
-  version = "8bde72d9a9dd67e0fc7c0545faca53df23ed3753";
+  version = "unstable-2023-08-23";
 
   src = fetchFromGitHub {
     owner = "pierrechevalier83";
     repo = pname;
-    rev = "f2023750d802259ab3ee7d7d1762631ec157a0b1";
-    sha256 = "04xds691sw4pi2nq8xvdhn0312wwia60gkd8b1bjqy11zrqbivbx";
+    rev = "8bde72d9a9dd67e0fc7c0545faca53df23ed3753";
+    sha256 = "sha256-yhnt7edhgVy/cZ6FpF6AZWPoeMeEKTXP+87no2KeIYU=";
   };
 
-  cargoSha256 = "sha256-sefZLI9btbS3SwNL2x20WDtO1yR1jBCT3D85c6V9YGE=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "swayipc-3.0.1" = "sha256-3Jhz3+LhncSRvo3n7Dh5d+RWQSvEff9teuaDZLLLEHk=";
+    #   "flash-lso-0.5.0" = "sha256-9uH3quxRzLtmHJs5WF/GRxWkXL/KFyOl182HKcHNnuc=";
+    #   "gc-arena-0.2.2" = "sha256-/H9VcTesBD+IA7bUf208b0HQ/cIUDAz9TJBBywf6akA=";
+    #   "h263-rs-0.1.0" = "sha256-4kBg09VHyiQTvUbvcTb5g/BVcOpRFZ1fVEuRWXv5XwE=";
+    #   "nellymoser-rs-0.1.2" = "sha256-GykDQc1XwySOqfxW/OcSxkKCFJyVmwSLy/CEBcwcZJs=";
+    #   "nihav_codec_support-0.1.0" = "sha256-rE9AIiQr+PnHC9xfDQULndSfFHSX4sqKkCAQYVNaJcQ=";
+    };
+  };
 
   doCheck = false; # No tests
 
