931a0544bf5ad31b5a56047fd743faf79f9e3c4f
Adam Rizkalla <adam@saronic.com>
2023-08-24 11:12:16 -0500

    linux-wifi-hotspot: remove TKIP usage in create_ap

--- pkgs/os-specific/linux/linux-wifi-hotspot/default.nix
+++ pkgs/os-specific/linux/linux-wifi-hotspot/default.nix
@@ -61,6 +61,13 @@ stdenv.mkDerivation rec {
       --replace "/usr" "$out"
   '';
 
+  # Since TKIP is deprecated in hostapd, we need to modify create_ap to not use it
+  # See: https://github.com/NixOS/nixpkgs/commit/4bec3f204362fa22a0740c8a572ffef3b322596d
+  preBuild = ''
+    substituteInPlace ./src/scripts/create_ap \
+      --replace "wpa_pairwise=TKIP CCMP" "wpa_pairwise=CCMP"
+  '';
+
   makeFlags = [
     "PREFIX=${placeholder "out"}"
   ];
