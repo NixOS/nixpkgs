873f0ced8fd44d74624e41b1ef93ed4b729735a9
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-25 00:25:58 +0200

    python311Packages.types-redis: 4.6.0.4 -> 4.6.0.5

--- pkgs/development/python-modules/types-redis/default.nix
+++ pkgs/development/python-modules/types-redis/default.nix
@@ -7,12 +7,12 @@
 
 buildPythonPackage rec {
   pname = "types-redis";
-  version = "4.6.0.4";
+  version = "4.6.0.5";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-xHWp089z3WlsOIfTBkQyP8VvXgCvlhUQNbO1tSh1ybM=";
+    hash = "sha256-XxedEL08qZWoE0qvzd/D4S1SsghDfEUp7yfmisswHzg=";
   };
 
   propagatedBuildInputs = [
