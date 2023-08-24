e43cccf79b3503320fc40924fe8998636cb8e935
Anthony Roussel <anthony@roussel.dev>
2023-08-24 22:50:16 +0200

    usql: 0.14.10 -> 0.15.0
    See https://github.com/xo/usql/releases/tag/v0.15.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41282,7 +41282,9 @@ with pkgs;
 
   usb-reset = callPackage ../applications/misc/usb-reset { };
 
-  usql = callPackage ../applications/misc/usql { };
+  usql = callPackage ../applications/misc/usql {
+    buildGoModule = buildGo121Module;
+  };
 
   utf8cpp = callPackage ../development/libraries/utf8cpp { };
 
