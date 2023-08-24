3434a2dde01ff31f834fcf5ba328baf3d2c889a1
Lennon Day-Reynolds <lennon@rcoder.net>
2023-08-23 20:17:41 -0700

    update zt1 from 1.10.6 -> 1.12.0 and remove obsolete iproute2 dependency (which should help darwin compat)

--- pkgs/tools/networking/zerotierone/default.nix
+++ pkgs/tools/networking/zerotierone/default.nix
@@ -2,10 +2,8 @@
 , stdenv
 , rustPlatform
 , fetchFromGitHub
-
 , buildPackages
 , cargo
-, iproute2
 , lzo
 , openssl
 , pkg-config
@@ -16,13 +14,13 @@
 
 let
   pname = "zerotierone";
-  version = "1.10.6";
+  version = "1.12.0";
 
   src = fetchFromGitHub {
     owner = "zerotier";
     repo = "ZeroTierOne";
     rev = version;
-    sha256 = "sha256-mapFKeF+8jMGkxSuHaw5oUdTdSQgAdxEwF/S6iyVLbY=";
+    sha256 = "sha256-O1KRclWyLu23cbnz97CDExmE3Gv8yaK8IoFKM2Ix4y0=";
   };
 
 in stdenv.mkDerivation {
@@ -62,8 +60,6 @@ in stdenv.mkDerivation {
     lzo
     openssl
     zlib
-  ] ++ lib.optional stdenv.isLinux [
-    iproute2
   ];
 
   enableParallelBuilding = true;
