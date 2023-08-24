4b88c4256bdd7dc947e05746babf2cbee24a5644
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-24 19:26:29 +0000

    python310Packages.pynisher: 1.0.8 -> 1.0.9

--- pkgs/development/python-modules/pynisher/default.nix
+++ pkgs/development/python-modules/pynisher/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "pynisher";
-  version = "1.0.8";
+  version = "1.0.9";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-BTH34zv7OAaVKc20VASfv3QaxAt/Y14Dkd/PBo/V6vo=";
+    hash = "sha256-hlN5uUlgmcipQqmr22rB245oEXOUe5WB9jWo7MXXViE=";
   };
 
   propagatedBuildInputs = [
