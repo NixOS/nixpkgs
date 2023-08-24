8fa6057e77d4205e91b92fc26892b1e2d0366c5d
happysalada <raphael@megzari.com>
2023-08-24 22:53:13 +0800

    python310Packages.pdf2docx: init at 0.5.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7808,6 +7808,8 @@ self: super: with self; {
 
   pdb2pqr = callPackage ../development/python-modules/pdb2pqr { };
 
+  pdf2docx = callPackage ../development/python-modules/pdf2docx { };
+
   pdf2image = callPackage ../development/python-modules/pdf2image { };
 
   pdfkit = callPackage ../development/python-modules/pdfkit { };
