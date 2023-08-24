5fef40ac2844f0149e07eddcae56fa27c02aecb9
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-24 23:32:41 +0200

    python311Packages.twilio: 8.6.0 -> 8.7.0
    Diff: https://github.com/twilio/twilio-python/compare/refs/tags/8.6.0...8.7.0
    
    Changelog: https://github.com/twilio/twilio-python/blob/8.7.0/CHANGES.md

--- pkgs/development/python-modules/twilio/default.nix
+++ pkgs/development/python-modules/twilio/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "twilio";
-  version = "8.6.0";
+  version = "8.7.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "twilio";
     repo = "twilio-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-ATspn/cY9DJahcsesnzwDOCEKYoUbqSoeOR3+kLou0s=";
+    hash = "sha256-wzsAHW0OlorLVI6nnDjv8WP1dpzyrmtE4OfwzFnZOH4=";
   };
 
   propagatedBuildInputs = [
