2137e9dcbcdc45d27400a53e05cd19b4782bb8fa
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-24 16:32:03 +0200

    python311Packages.aioairzone: update disabled

--- pkgs/development/python-modules/aioairzone/default.nix
+++ pkgs/development/python-modules/aioairzone/default.nix
@@ -11,7 +11,7 @@ buildPythonPackage rec {
   version = "0.6.7";
   format = "pyproject";
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "Noltari";
