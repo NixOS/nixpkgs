46979f197bd8e55f8f8852b82f9545aa47ae0014
Hans Baier <hansfbaier@gmail.com>
2023-08-25 15:25:57 +0700

    python310Packages.fasm: init at 0.0.2.post100

---
+++ pkgs/development/python-modules/fasm/default.nix
@@ -0,0 +1,18 @@
+{ lib, buildPythonPackage, fetchPypi }:
+
+buildPythonPackage rec {
+  pname = "fasm";
+  version = "0.0.2.post100";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "3b840a6b1184b5f1568635b1adab28147947522707d41ceba02d5ed0a0877279";
+  };
+
+  meta = with lib; {
+    homepage = "https://github.com/chipsalliance/fasm/";
+    description = "FPGA Assembly (FASM) Parser and Generator ";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ hansfbaier ];
+  };
+}
