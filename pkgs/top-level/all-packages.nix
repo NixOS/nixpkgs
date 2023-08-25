f938e1ac056804e432c01a8cb0873de90d4d7c70
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2023-08-25 19:01:29 -0400

    ssl-proxy: init at 0.2.7
    Ssl-proxy is a reverse proxy that makes it easy to put up and sign ssl
    certificates. Homepage: https://github.com/suyashkumar/ssl-proxy

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -42172,4 +42172,6 @@ with pkgs;
   wpm = callPackage ../applications/misc/wpm { };
 
   yazi = callPackage ../applications/file-managers/yazi { inherit (darwin.apple_sdk.frameworks) Foundation; };
+
+  ssl-proxy = callPackage ../tools/networking/ssl-proxy { };
 }
