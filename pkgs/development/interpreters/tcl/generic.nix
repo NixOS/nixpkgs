1e3148d3fe8b2864ca1fa6340f0701f1d424274a
Ryan Gonzalez <git@refi64.dev>
2023-08-25 16:58:14 -0500

    tcl.tclPackageHook: Use makeBinaryWrapper
    Given the number of entries in TCLLIBPATH when using tcllib, this
    results in some massive startup time speedups:
    
    ```
    $ nix shell 'nixpkgs#tcl2048' -c sh -c 'echo q | time 2048'
    ```
    
    takes .44s on master but only .01s with this change.

--- pkgs/development/interpreters/tcl/generic.nix
+++ pkgs/development/interpreters/tcl/generic.nix
@@ -62,7 +62,7 @@ let
         libdir = "lib/${libPrefix}";
         tclPackageHook = callPackage ({ buildPackages }: makeSetupHook {
           name = "tcl-package-hook";
-          propagatedBuildInputs = [ buildPackages.makeWrapper ];
+          propagatedBuildInputs = [ buildPackages.makeBinaryWrapper ];
         } ./tcl-package-hook.sh) {};
         # verify that Tcl's clock library can access tzdata
         tests.tzdata = runCommand "${pname}-test-tzdata" {} ''
