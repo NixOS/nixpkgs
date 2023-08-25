c99f6eae3f973461c7da2a6c79e0e3722ca9b63c
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-25 22:39:53 +0000

    kaniko: 1.13.0 -> 1.14.0

--- pkgs/applications/networking/cluster/kaniko/default.nix
+++ pkgs/applications/networking/cluster/kaniko/default.nix
@@ -9,13 +9,13 @@
 
 buildGoModule rec {
   pname = "kaniko";
-  version = "1.13.0";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "GoogleContainerTools";
     repo = "kaniko";
     rev = "v${version}";
-    hash = "sha256-bzMhK60BwJ7A1sGV0rutLOfgvbH/deDQNFZ8BB1hREc=";
+    hash = "sha256-sDZg2eKTwy3Y7Uaky4rz7EuU1EKY/S4VAEaj7GMN6Uo=";
   };
 
   vendorHash = null;
