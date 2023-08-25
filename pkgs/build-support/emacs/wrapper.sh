789440d0dc168339ee7809ad5ffffc667e72b4f0
Lin Jian <me@linj.tech>
2023-08-25 10:50:05 +0800

    emacsWithPackages: add a note for EMACSNATIVELOADPATH

--- pkgs/build-support/emacs/wrapper.sh
+++ pkgs/build-support/emacs/wrapper.sh
@@ -23,6 +23,11 @@ else
     newLoadPath+=("")
 fi
 
+# NOTE: Even though we treat EMACSNATIVELOADPATH like EMACSLOADPATH in
+# this wrapper, empty elements in EMACSNATIVELOADPATH have no special
+# meaning for Emacs.  Only non-empty elements in EMACSNATIVELOADPATH
+# will be prepended to native-comp-eln-load-path.
+# https://git.savannah.gnu.org/cgit/emacs.git/tree/lisp/startup.el?id=3685387e609753293c4518be75e77c659c3b2d8d#n599
 if [[ -n $EMACSNATIVELOADPATH ]]
 then
     while read -rd: entry
