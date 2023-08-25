d516998a89d36c8b25ec5600750d489b02d61fe4
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-25 13:29:53 +0000

    clojure: 1.11.1.1386 -> 1.11.1.1405

--- pkgs/development/interpreters/clojure/default.nix
+++ pkgs/development/interpreters/clojure/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "clojure";
-  version = "1.11.1.1386";
+  version = "1.11.1.1405";
 
   src = fetchurl {
     # https://github.com/clojure/brew-install/releases
     url = "https://github.com/clojure/brew-install/releases/download/${finalAttrs.version}/clojure-tools-${finalAttrs.version}.tar.gz";
-    hash = "sha256-e5RLnsydCZKRv6P/yC8FxK5AgK0Gj6YJw7E41neGYsM=";
+    hash = "sha256-sqKhnddOy2rKcYtM2rSiaHIihoajZ8GBfBfyU4oPtXQ=";
   };
 
   nativeBuildInputs = [
