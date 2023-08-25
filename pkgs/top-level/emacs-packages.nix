3a3243af4dbe2ebf02d24699a3053adf58b9b970
Lin Jian <me@linj.tech>
2023-08-25 11:13:47 +0800

    emacs: remove outdated doc about package initialization
    Since Emacs 27[1], there is no need to call package-initialize in the
    init file.
    
    [1]: https://git.savannah.gnu.org/cgit/emacs.git/tree/etc/NEWS.27?id=cae528457cb862dc886a34240c9d4c73035b6659#n227

--- pkgs/top-level/emacs-packages.nix
+++ pkgs/top-level/emacs-packages.nix
@@ -7,18 +7,11 @@
 # Alternative: use `emacs`, install everything to a system or user profile
 # and then add this at the start your `init.el`:
 /*
-  (require 'package)
-
-  ;; optional. makes unpure packages archives unavailable
-  (setq package-archives nil)
-
   ;; optional. use this if you install emacs packages to the system profile
   (add-to-list 'package-directory-list "/run/current-system/sw/share/emacs/site-lisp/elpa")
 
   ;; optional. use this if you install emacs packages to user profiles (with nix-env)
   (add-to-list 'package-directory-list "~/.nix-profile/share/emacs/site-lisp/elpa")
-
-  (package-initialize)
 */
 
 { pkgs'
