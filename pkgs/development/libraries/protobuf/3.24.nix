7bfb199b7ce3b5b199070782a37953eef84c2991
Gaetan Lepage <gaetan@glepage.com>
2023-08-25 23:33:35 +0200

    protobuf: 3.24.1 -> 3.24.2

--- pkgs/development/libraries/protobuf/3.24.nix
+++ pkgs/development/libraries/protobuf/3.24.nix
@@ -1,6 +1,6 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic-v3-cmake.nix ({
-  version = "3.24.1";
-  sha256 = "sha256-70gKH18Ak3TJisscj8JsbOJavOFxbWbVEtGjTXUlqSI=";
+  version = "3.24.2";
+  sha256 = "sha256-yVLszyVtsz1CCzeOkioL4O3mWTFKKVBUyOhwDbC5UqE=";
 } // args)
