970b00cdd167c9682fbac74adc47d8f10ff1c514
John Garcia <jgarcia3788@yahoo.co.uk>
2023-08-25 22:42:02 +0100

    Corrected icon url & hash

--- pkgs/applications/misc/remnote/default.nix
+++ pkgs/applications/misc/remnote/default.nix
@@ -8,8 +8,8 @@ let
     sha256 = "sha256-z/LTsX65rN+AJbpCTzIabpgUadFe6SnRRDKOkexZyjQ=";
   };
   icon = fetchurl {
-    url = "https://www.remnote.io/remnote.png";
-    sha256 = "sha256-BylzKYH4Kk4N6AI1FrvOtpZEpRSUqBy3zuHh+XMjhA4=";
+    url = "https://www.remnote.io/icon.png";
+    sha256 = "sha256-r5D7fNefKPdjtmV7f/88Gn3tqeEG8LGuD4nHI/sCk94=";
   };
   appexec = appimageTools.wrapType2 {
     inherit pname version src;
