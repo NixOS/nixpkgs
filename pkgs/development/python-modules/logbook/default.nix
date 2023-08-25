1fa7b1e2675c13b9bc13542d9829b45650d7400e
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-25 13:41:25 +0200

    python311Packages.logbook: equalize content

--- pkgs/development/python-modules/logbook/default.nix
+++ pkgs/development/python-modules/logbook/default.nix
@@ -1,4 +1,11 @@
-{ lib, buildPythonPackage, fetchPypi, isPy3k, pytest, mock, brotli }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, isPy3k
+, pytest
+, mock
+, brotli
+}:
 
 buildPythonPackage rec {
   pname = "logbook";
@@ -10,9 +17,15 @@ buildPythonPackage rec {
     sha256 = "1s1gyfw621vid7qqvhddq6c3z2895ci4lq3g0r1swvpml2nm9x36";
   };
 
-  nativeCheckInputs = [ pytest ] ++ lib.optionals (!isPy3k) [ mock ];
+  nativeCheckInputs = [
+    pytest
+  ] ++ lib.optionals (!isPy3k) [
+    mock
+  ];
 
-  propagatedBuildInputs = [ brotli ];
+  propagatedBuildInputs = [
+    brotli
+  ];
 
   checkPhase = ''
     find tests -name \*.pyc -delete
