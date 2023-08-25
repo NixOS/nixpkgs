8e12dc645b8914e6af950b570977964b41a19b9a
John Garcia <jgarcia3788@yahoo.co.uk>
2023-08-25 21:33:45 +0100

    added icon source

--- pkgs/applications/misc/remnote/default.nix
+++ pkgs/applications/misc/remnote/default.nix
@@ -3,10 +3,13 @@
 appimageTools.wrapType2 rec {
   pname = "remnote";
   version = "1.12.18";
-
   src = fetchurl {
     url = "https://download.remnote.io/remnote-desktop/RemNote-${version}.AppImage";
     sha256 = "sha256-z/LTsX65rN+AJbpCTzIabpgUadFe6SnRRDKOkexZyjQ=";
+  };
+  icon = fetchurl {
+    url = "https://www.remnote.io/remnote.png";
+    sha256 = "sha256-BylzKYH4Kk4N6AI1FrvOtpZEpRSUqBy3zuHh+XMjhA4=";
   };
 
   meta = with lib; {
