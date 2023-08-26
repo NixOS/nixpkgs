5000fb7c4791b4948b41ce906e5a5db0e4ecb62c
Red Star Over Earth <rs0vere@proton.me>
2023-08-26 17:31:47 +0800

    kcp: init at 1.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22328,6 +22328,8 @@ with pkgs;
 
   judy = callPackage ../development/libraries/judy { };
 
+  kcp = callPackage ../development/libraries/kcp { };
+
   kddockwidgets = libsForQt5.callPackage ../development/libraries/kddockwidgets { };
 
   keybinder = callPackage ../development/libraries/keybinder {
