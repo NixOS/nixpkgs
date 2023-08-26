a290088051863353e97f1e556c434f12093be17a
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-26 13:10:18 +0200

    python311Packages.pathspec: 0.11.0 -> 0.11.2
    Changelog: https://github.com/cpburnz/python-pathspec/blob/v0.11.2/CHANGES.rst

--- pkgs/development/python-modules/pathspec/default.nix
+++ pkgs/development/python-modules/pathspec/default.nix
@@ -14,12 +14,14 @@
 
 buildPythonPackage rec {
   pname = "pathspec";
-  version = "0.11.0";
+  version = "0.11.2";
   format = "pyproject";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ZNM41OCRTpHBeSMh5pB7Wlk/GrGFHef8JpVXohsw67w=";
+    hash = "sha256-4NjQrC8S2mGVbrIwa2n5RptC9N6w88tu1HuczpmWztM=";
   };
 
   nativeBuildInputs = [
@@ -38,11 +40,11 @@ buildPythonPackage rec {
     inherit awsebcli black hatchling yamllint;
   };
 
-  meta = {
+  meta = with lib; {
     description = "Utility library for gitignore-style pattern matching of file paths";
     homepage = "https://github.com/cpburnz/python-path-specification";
     changelog = "https://github.com/cpburnz/python-pathspec/blob/v${version}/CHANGES.rst";
-    license = lib.licenses.mpl20;
-    maintainers = with lib.maintainers; [ copumpkin ];
+    license = licenses.mpl20;
+    maintainers = with maintainers; [ copumpkin ];
   };
 }
