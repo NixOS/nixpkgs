59a7584c68241bc080c09349e976887af1f5fc2b
Mario Rodas <marsam@users.noreply.github.com>
2023-08-26 04:20:00 +0000

    luau: 0.591 -> 0.592
    Diff: https://github.com/Roblox/luau/compare/0.591...0.592
    
    Changelog: https://github.com/Roblox/luau/releases/tag/0.592

--- pkgs/development/interpreters/luau/default.nix
+++ pkgs/development/interpreters/luau/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "luau";
-  version = "0.591";
+  version = "0.592";
 
   src = fetchFromGitHub {
     owner = "Roblox";
     repo = "luau";
     rev = version;
-    hash = "sha256-eCYB+B5bglUcF+xAAHBWwwgDrvY9NatmcwL/GUnIhOk=";
+    hash = "sha256-S0Sr28jPnFBurLlCCj3BzTLh7yvzEPlR0MAUz7cDf1k=";
   };
 
   nativeBuildInputs = [ cmake ];
