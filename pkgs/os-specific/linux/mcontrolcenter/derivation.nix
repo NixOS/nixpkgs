16007bb5769e68793a249b7806944a77be1a981c
Nadim Kobeissi <nadim@symbolic.software>
2023-08-27 19:25:30 +0200

    mcontrolcenter: fix formatting

--- pkgs/os-specific/linux/mcontrolcenter/derivation.nix
+++ pkgs/os-specific/linux/mcontrolcenter/derivation.nix
@@ -41,22 +41,20 @@ stdenv.mkDerivation (finalAttrs: rec {
   ];
 
   installPhase = ''
-    	install -Dm755 mcontrolcenter $out/bin/mcontrolcenter
-    	install -Dm755 helper/mcontrolcenter-helper $out/libexec/mcontrolcenter-helper
-    	install -Dm644  $src/resources/mcontrolcenter.svg $out/share/icons/hicolor/32x32/apps/mcontrolcenter.svg
-    	install -Dm644 $src/src/helper/mcontrolcenter-helper.conf $out/share/dbus-1/system.d/mcontrolcenter-helper.conf
-      mkdir -p $out/share/dbus-1/system-services
-      echo "[D-BUS Service]" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
-      echo "Name=mcontrolcenter.helper" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
-      echo "Exec=$out/libexec/mcontrolcenter-helper" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
-      echo "User=root" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
+    install -Dm755 mcontrolcenter $out/bin/mcontrolcenter
+    install -Dm755 helper/mcontrolcenter-helper $out/libexec/mcontrolcenter-helper
+    install -Dm644  $src/resources/mcontrolcenter.svg $out/share/icons/hicolor/32x32/apps/mcontrolcenter.svg
+    install -Dm644 $src/src/helper/mcontrolcenter-helper.conf $out/share/dbus-1/system.d/mcontrolcenter-helper.conf
+    mkdir -p $out/share/dbus-1/system-services
+    echo "[D-BUS Service]" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
+    echo "Name=mcontrolcenter.helper" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
+    echo "Exec=$out/libexec/mcontrolcenter-helper" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
+    echo "User=root" >> $out/share/dbus-1/system-services/mcontrolcenter.helper.service
   '';
 
   meta = with lib; {
     homepage = "https://github.com/dmitry-s93/MControlCenter";
-    description = ''
-      An application that allows you to change the settings of MSI laptops running Linux.";
-    '';
+    description = "An application that allows you to change the settings of MSI laptops running Linux.";
     licencse = licenses.gpl3;
     platforms = with platforms; linux;
     # maintainers = [ maintainers.nadimkobeissi ];
