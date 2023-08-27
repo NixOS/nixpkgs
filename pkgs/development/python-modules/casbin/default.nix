6bccb5287af838353ef766450f4c5c1ff40412a6
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-27 18:21:38 +0200

    python311Packages.casbin: 1.23.1 -> 1.24.0
    Diff: https://github.com/casbin/pycasbin/compare/refs/tags/v1.23.1...v1.24.0
    
    Changelog: https://github.com/casbin/pycasbin/blob/v1.24.0/CHANGELOG.md

--- pkgs/development/python-modules/casbin/default.nix
+++ pkgs/development/python-modules/casbin/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "casbin";
-  version = "1.23.1";
+  version = "1.24.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = "pycasbin";
     rev = "refs/tags/v${version}";
-    hash = "sha256-jL02G4Z2Lhy/02Lb7aSUDEKg2h34UXJbwMFaDSPgc+U=";
+    hash = "sha256-IECJlK1DQl/ndLnreZ/xQ5F06eCgFWZe3fF5LLdqLzo=";
   };
 
   propagatedBuildInputs = [
