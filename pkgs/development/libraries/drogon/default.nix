146b5cde0384dac3ca64351f83c02331072be279
Robert Scott <code@humanleg.org.uk>
2023-08-27 18:12:03 +0100

    drogon: 1.8.3 -> 1.8.5

--- pkgs/development/libraries/drogon/default.nix
+++ pkgs/development/libraries/drogon/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "drogon";
-  version = "1.8.3";
+  version = "1.8.5";
 
   src = fetchFromGitHub {
     owner = "drogonframework";
     repo = "drogon";
     rev = "v${version}";
-    sha256 = "sha256-I3f/8TMGm1G4CFMJJLyiVYso9yTHjeLefS5eaGySvC4=";
+    sha256 = "sha256-VvN71A0Nhe/Zdi9M1YL5mxoaiMfd2bfV53dfMBuDZd8=";
     fetchSubmodules = true;
   };
 
@@ -43,18 +43,14 @@ stdenv.mkDerivation rec {
     # this part of the test would normally fail because it attempts to configure a CMake project that uses find_package on itself
     # this patch makes drogon and trantor visible to the test
     ./fix_find_package.patch
-
-    # see https://github.com/drogonframework/drogon/issues/1491
-    (fetchpatch {
-      url = "https://github.com/drogonframework/drogon/commit/7d87d7e0b264ce53aa5ee006fb022d3516c9d666.patch";
-      sha256 = "sha256-C4zH9oNMfhkaeVNvZuBuzu1v2vNgg/t+YPitbrmHg+Y=";
-    })
   ];
 
   # modifying PATH here makes drogon_ctl visible to the test
   installCheckPhase = ''
-    cd ..
-    PATH=$PATH:$out/bin bash test.sh
+    (
+      cd ..
+      PATH=$PATH:$out/bin bash test.sh
+    )
   '';
 
   doInstallCheck = true;
