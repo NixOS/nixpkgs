a0e4380c5108b908d331dab756c247dba36f09a5
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-27 07:12:04 +0000

    webex: 43.2.0.25211 -> 43.8.0.26955
    (cherry picked from commit 89c4fdc1abe35924aeff30ff3fdc0afe0af0c284)

--- pkgs/applications/networking/instant-messengers/webex/default.nix
+++ pkgs/applications/networking/instant-messengers/webex/default.nix
@@ -56,11 +56,11 @@
 
 stdenv.mkDerivation rec {
   pname = "webex";
-  version = "43.2.0.25211";
+  version = "43.8.0.26955";
 
   src = fetchurl {
-    url = "https://binaries.webex.com/WebexDesktop-Ubuntu-Gold/20230214022524/Webex_ubuntu.7z";
-    sha256 = "c58a0da26c8f64302cc612c60980dbd68c074d6d8a567b3d870d7d6d06b420ad";
+    url = "https://binaries.webex.com/WebexDesktop-Ubuntu-Gold/20230814223338/Webex_ubuntu.7z";
+    sha256 = "6c754d65fffbcbbb2ca303e1f8c1e5688da190eea3d3b67c77361abc6e2bb85b";
   };
 
   nativeBuildInputs = [
