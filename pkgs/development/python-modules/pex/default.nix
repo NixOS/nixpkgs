c09f900a5e19ecb6f07de7bc60ae7e96733b31d6
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-08-27 17:45:45 -0700

    python310Packages.pex: switch format from flit to pyproject

--- pkgs/development/python-modules/pex/default.nix
+++ pkgs/development/python-modules/pex/default.nix
@@ -1,5 +1,6 @@
 { lib
 , buildPythonPackage
+, fetchpatch
 , fetchPypi
 , flit-core
 , pythonOlder
@@ -8,7 +9,7 @@
 buildPythonPackage rec {
   pname = "pex";
   version = "2.1.144";
-  format = "flit";
+  format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
@@ -16,6 +17,15 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-wugHG/ZfyfNgd2znDS7FXu34LsUVMcwDswFkh4lRIXo=";
   };
+
+  patches = [
+    # https://github.com/pantsbuild/pex/pull/2229
+    (fetchpatch {
+      name = "unpin-flit-core-dependency.patch";
+      url = "https://github.com/pantsbuild/pex/commit/02754ddd43b9ed81c8bda557bef7516dc37da3e5.patch";
+      hash = "sha256-9FHObjtA7cgh8/vltPGyrhENJotHeM08RxTUwQ3y/38=";
+    })
+  ];
 
   nativeBuildInputs = [
     flit-core
