b516af56c13c1d97e8bef2aaed78aaf035d7bc2c
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-28 09:13:02 +0200

    python310Packages.djangorestframework-simplejwt: add changelog to meta

--- pkgs/development/python-modules/djangorestframework-simplejwt/default.nix
+++ pkgs/development/python-modules/djangorestframework-simplejwt/default.nix
@@ -43,6 +43,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "JSON Web Token authentication plugin for Django REST Framework";
     homepage = "https://github.com/davesque/django-rest-framework-simplejwt";
+    changelog = "https://github.com/jazzband/djangorestframework-simplejwt/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ arnoldfarkas ];
   };
