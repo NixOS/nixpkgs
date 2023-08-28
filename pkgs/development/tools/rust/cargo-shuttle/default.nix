49592646811c163374d39fb2372e85785c5695a6
figsoda <figsoda@pm.me>
2023-08-28 12:39:24 -0400

    cargo-shuttle: 0.24.0 -> 0.25.1
    Diff: https://github.com/shuttle-hq/shuttle/compare/v0.24.0...v0.25.1
    
    Changelog: https://github.com/shuttle-hq/shuttle/releases/tag/v0.25.1

--- pkgs/development/tools/rust/cargo-shuttle/default.nix
+++ pkgs/development/tools/rust/cargo-shuttle/default.nix
@@ -10,13 +10,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-shuttle";
-  version = "0.24.0";
+  version = "0.25.1";
 
   src = fetchFromGitHub {
     owner = "shuttle-hq";
     repo = "shuttle";
     rev = "v${version}";
-    hash = "sha256-21ImDhdT07fc+eKhOWU4z/SQWAO2meq4PITKsVSSG9Y=";
+    hash = "sha256-UB9S3Ougd7t+D3oAYE9i6AUY76bNKrr605W6GWh1vb0=";
   };
 
   cargoLock = {
