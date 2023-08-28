3f9b79207b9f28c81939b1d306c76154ce6e4f47
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-28 13:22:29 +0000

    python310Packages.approvaltests: 8.4.1 -> 9.0.0

--- pkgs/development/python-modules/approvaltests/default.nix
+++ pkgs/development/python-modules/approvaltests/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "approvaltests";
-  version = "8.4.1";
+  version = "9.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "approvals";
     repo = "ApprovalTests.Python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-BuFiNZZ7228CKJ97mVK2S8Siqe040EYV5pElVtwuVf4=";
+    hash = "sha256-tyUPXeMdFuzlBY/HrGHLDEwYngzBELayaVVfEh92lbE=";
   };
 
   propagatedBuildInputs = [
