4732cbf3f88f451760345e9cdb54babbb543e22e
ajs124 <git@ajs124.de>
2023-08-28 15:02:59 +0200

    nixos/restic: use openssh as configured in programs.ssh

--- nixos/modules/services/backup/restic.nix
+++ nixos/modules/services/backup/restic.nix
@@ -331,7 +331,7 @@ in
                 nameValuePair (rcloneAttrToConf name) (toRcloneVal value)
               )
               backup.rcloneConfig);
-            path = [ pkgs.openssh ];
+            path = [ config.programs.ssh.package ];
             restartIfChanged = false;
             serviceConfig = {
               Type = "oneshot";
