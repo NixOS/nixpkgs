cba36e6fae7157f897eb7536520600757d1d1faf
figsoda <figsoda@pm.me>
2023-08-28 17:36:18 -0400

    python310Packages.universal-pathlib: 0.1.1 -> 0.1.2
    Changelog: https://github.com/fsspec/universal_pathlib/releases/tag/v0.1.2

--- pkgs/development/python-modules/universal-pathlib/default.nix
+++ pkgs/development/python-modules/universal-pathlib/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "universal-pathlib";
-  version = "0.1.1";
+  version = "0.1.2";
   format = "pyproject";
 
   src = fetchPypi {
     pname = "universal_pathlib";
     inherit version;
-    hash = "sha256-UhTIbyEP6rm+W8wALwQtZeruw8CTP9ZlpDNstw6GxZ0=";
+    hash = "sha256-aJ4nAf717U7RmIjQrKCzVC/iSOCbEK3EMfX3R/hoBSo=";
   };
 
   nativeBuildInputs = [
