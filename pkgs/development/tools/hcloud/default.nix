21c0a31d648a3db6f2d42b9b824a35afaf034c31
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-28 00:22:38 +0000

    hcloud: 1.36.0 -> 1.37.0

--- pkgs/development/tools/hcloud/default.nix
+++ pkgs/development/tools/hcloud/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "hcloud";
-  version = "1.36.0";
+  version = "1.37.0";
 
   src = fetchFromGitHub {
     owner = "hetznercloud";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-BmV+g0Geue41KNcB++TaoSsuGG1HA+uH5GHye7QRWOM=";
+    sha256 = "sha256-6UQaO2ArAYd6Lr1maciC83k1GlR8FLx+acAZh6SjI3g=";
   };
 
-  vendorHash = "sha256-eGeaH9nIjBSZLxNlsQtas122eEXrIbrGn/GYVB4KhvY=";
+  vendorHash = "sha256-mxAG3o3IY70xn8WymUzF96Q2XWwQ0efWrrw1VV4Y8HU=";
 
   ldflags = [
     "-s" "-w"
