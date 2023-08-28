408c1672e0756f1f80af01a5c2d488d654850f80
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-28 17:35:45 +0200

    python311Packages.mypy-boto3-builder: 7.17.3 -> 7.18.0
    Diff: https://github.com/youtype/mypy_boto3_builder/compare/refs/tags/7.17.3...7.18.0
    
    Changelog: https://github.com/youtype/mypy_boto3_builder/releases/tag/7.18.0

--- pkgs/development/python-modules/mypy-boto3-builder/default.nix
+++ pkgs/development/python-modules/mypy-boto3-builder/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "mypy-boto3-builder";
-  version = "7.17.3";
+  version = "7.18.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.10";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "youtype";
     repo = "mypy_boto3_builder";
     rev = "refs/tags/${version}";
-    hash = "sha256-ziJb/aIvK8zZ2NwCKtyGHNQ0LM0Sro6//oAESlku0kI=";
+    hash = "sha256-Xm+G9NSiGLv4oLcC1JqaLbM5Wh1Vp1q6CeNcVPWvJU4=";
   };
 
   nativeBuildInputs = [
