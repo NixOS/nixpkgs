18fe320058a7ae3e4ca71762e09ccee50239740a
Kiran Ostrolenk <kiran@ostrolenk.co.uk>
2023-08-29 20:57:38 +0100

    maintainers: update CardboardTurkey's email and key

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2732,9 +2732,12 @@
   };
   CardboardTurkey = {
     name = "Kiran Ostrolenk";
-    email = "kostrolenk@gmail.com";
+    email = "kiran@ostrolenk.co.uk";
     github = "CardboardTurkey";
     githubId = 34030186;
+    keys = [{
+      fingerprint = "8BC7 74E4 A2EC 7507 3B61  A647 0BBB 1C8B 1C36 39EE";
+    }];
   };
   carlosdagos = {
     email = "m@cdagostino.io";
