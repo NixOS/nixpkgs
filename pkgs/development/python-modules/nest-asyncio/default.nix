eb8739b43f0e331729b035875afebcc200c010b3
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-29 21:38:04 +0200

    python311Packages.nest-asyncio: 1.5.6 -> 1.5.7

--- pkgs/development/python-modules/nest-asyncio/default.nix
+++ pkgs/development/python-modules/nest-asyncio/default.nix
@@ -5,13 +5,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "1.5.6";
+  version = "1.5.7";
   pname = "nest_asyncio";
   disabled = !(pythonAtLeast "3.5");
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0mfMH/eUQD999pKWTR0qP6lBj/6io/aFmkOf9IL+8pA=";
+    hash = "sha256-aoD3uY8k2Qg+0kYIl3wJ3WCNg/kczMJMnSy6bRDgHBA=";
   };
 
   # tests not packaged with source dist as of 1.3.2/1.3.2, and
