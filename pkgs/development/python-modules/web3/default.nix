8709579b0272334273b50caecc2a492e2979fe14
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-08-30 18:40:31 +0300

    python311Packages.web3: add hellwolf to maintainers

--- pkgs/development/python-modules/web3/default.nix
+++ pkgs/development/python-modules/web3/default.nix
@@ -67,6 +67,6 @@ buildPythonPackage rec {
     description = "Web3 library for interactions";
     homepage = "https://github.com/ethereum/web3";
     license = licenses.mit;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ hellwolf ];
   };
 }
