addbdb1cbbfba0936eb22bd4d68bee899ace7c8b
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-30 16:22:23 +0000

    simplotask: 1.11.3 -> 1.11.4

--- pkgs/tools/admin/simplotask/default.nix
+++ pkgs/tools/admin/simplotask/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "simplotask";
-  version = "1.11.3";
+  version = "1.11.4";
 
   src = fetchFromGitHub {
     owner = "umputun";
     repo = "spot";
     rev = "v${version}";
-    hash = "sha256-sNYC8QQmVdBjHU3/pDD4TGLDxKsMLyMmZa7wBVlWnGM=";
+    hash = "sha256-G/T7LF8ujlCSbK1Lv2h43OZVx52yrLDr+6e/WxhaHOo=";
   };
 
   vendorHash = null;
