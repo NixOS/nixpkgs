eaa82203d637621fee4d93cddf7bc0762f1cdadc
Nicolas Benes <nbenes.gh@xandea.de>
2023-08-30 11:36:07 +0200

    mullvad-browser: 12.5.2 -> 12.5.3
    https://github.com/mullvad/mullvad-browser/releases/tag/12.5.3
    (cherry picked from commit 0b636ab4deb60161b899cfcfc3c1d2d2f2ebbff6)

--- pkgs/applications/networking/browsers/mullvad-browser/default.nix
+++ pkgs/applications/networking/browsers/mullvad-browser/default.nix
@@ -78,7 +78,7 @@ let
       ++ lib.optionals mediaSupport [ ffmpeg ]
   );
 
-  version = "12.5.2";
+  version = "12.5.3";
 
   sources = {
     x86_64-linux = fetchurl {
@@ -86,7 +86,7 @@ let
         "https://cdn.mullvad.net/browser/${version}/mullvad-browser-linux64-${version}_ALL.tar.xz"
         "https://github.com/mullvad/mullvad-browser/releases/download/${version}/mullvad-browser-linux64-${version}_ALL.tar.xz"
       ];
-      hash = "sha256-sVVgQTpPQFiG1mEIih0CemNV5qjC2l+JTxef37/nC9k=";
+      hash = "sha256-vnxpmZSqPe7wE4USDbYGm+5k9J/nuUk2uJx4CmwFPvw=";
     };
   };
 
