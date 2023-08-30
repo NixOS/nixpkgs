4a888f832b66bdf4048e038de52d37444b62d04c
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-30 09:54:52 +0200

    python311Packages.aioesphomeapi: 16.0.2 -> 16.0.3
    Diff: https://github.com/esphome/aioesphomeapi/compare/refs/tags/v16.0.2...v16.0.3
    
    Changelog: https://github.com/esphome/aioesphomeapi/releases/tag/v16.0.3

--- pkgs/development/python-modules/aioesphomeapi/default.nix
+++ pkgs/development/python-modules/aioesphomeapi/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aioesphomeapi";
-  version = "16.0.2";
+  version = "16.0.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "esphome";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-lnMB66cpF4RsqthZS2Xn4BpniSWHBJlwhYLgDCB1IIc=";
+    hash = "sha256-x4kH8riHZbVibgwR2DmWB4tsswpub4m2LwnypVVUgqM=";
   };
 
   propagatedBuildInputs = [
