5681d9ecbc071083418efda57d8c6c4b2fb1c568
Tom Wieczorek <tom@bibbu.net>
2023-08-30 18:19:55 +0200

    yx: init at 1.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15078,6 +15078,8 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
+  yx = callPackage ../tools/text/yx { };
+
   zarchive = callPackage ../tools/archivers/zarchive { };
 
   zprint = callPackage ../development/tools/zprint { };
