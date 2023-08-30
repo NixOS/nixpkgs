e0192b625cf591f12831f2a3752349c8ed8ac775
Matthias Thym <git@thym.at>
2023-08-30 16:10:10 +0200

    ipscan: refactor meta and add totoroot as maintainer

--- pkgs/tools/security/ipscan/default.nix
+++ pkgs/tools/security/ipscan/default.nix
@@ -42,12 +42,13 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    description = "Fast and friendly network scanner";
+    description = "Angry IP Scanner - fast and friendly network scanner";
     homepage = "https://angryip.org";
+    downloadPage = "https://github.com/angryip/ipscan/releases/tag/${version}";
     changelog = "https://github.com/angryip/ipscan/blob/${version}/CHANGELOG";
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     license = licenses.gpl2Only;
     platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers; [ kylesferrazza ];
+    maintainers = with maintainers; [ kylesferrazza totoroot ];
   };
 }
