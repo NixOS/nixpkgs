a1244e36f9e4300431130a2c5e83d2e3a6c747ba
Lin Jian <me@linj.tech>
2023-08-30 20:50:12 +0800

    emacsPackages.ebuild-mode: switch to melpaBuild
    Advantages:
    - info manual is built and installed
    - conform to package.el
      without any config, open a foo.ebuild file and its major-mode is set
      to ebuild-mode.

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/ebuild-mode/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/ebuild-mode/default.nix
@@ -1,6 +1,6 @@
-{ lib, trivialBuild, fetchurl }:
+{ lib, melpaBuild, fetchurl, writeText }:
 
-trivialBuild rec {
+melpaBuild rec {
   pname = "ebuild-mode";
   version = "1.65";
 
@@ -8,6 +8,14 @@ trivialBuild rec {
     url = "https://dev.gentoo.org/~ulm/emacs/${pname}-${version}.tar.xz";
     sha256 = "sha256-vJ+UlPMIuZ02da9R67wIq2dVaWElu/sYmWx2KgBQ9B8=";
   };
+
+  commit = "not-used-but-has-to-be-set";
+
+  recipe = writeText "recipe" ''
+    (ebuild-mode
+      :url "https://anongit.gentoo.org/git/proj/ebuild-mode.git"
+      :fetcher git)
+  '';
 
   meta = with lib; {
     description = "Major modes for Gentoo package files";
