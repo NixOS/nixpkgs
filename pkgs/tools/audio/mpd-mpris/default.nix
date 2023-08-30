4c8c2b7baae2645ba72534cbe6828515423890ef
Doron Behar <doron.behar@gmail.com>
2023-08-30 09:59:18 +0300

    mpd-mpris: use upstream patch for Exec path fix

--- pkgs/tools/audio/mpd-mpris/default.nix
+++ pkgs/tools/audio/mpd-mpris/default.nix
@@ -15,6 +15,14 @@ buildGoModule rec {
     sha256 = "sha256-QxPkGWpCWiyEbChH9SHeD+SiV8k0c/G7MG/azksP3xU=";
   };
 
+  patches = [
+    # Makes Exec= path not absolute, see https://github.com/natsukagami/mpd-mpris/pull/42
+    (fetchpatch {
+      url = "https://github.com/natsukagami/mpd-mpris/commit/8a5b53b1aa3174c3ccb1db24fb4e39f90012b98f.patch";
+      hash = "sha256-LArPq+RRPJOs0je1olqg+pK7nvU7UIlrpGtHv2PhIY4=";
+    })
+  ];
+
   vendorHash = "sha256-HCDJrp9WFB1z+FnYpOI5e/AojtdnpN2ZNtgGVaH/v/Q=";
 
   doCheck = false;
@@ -22,8 +30,6 @@ buildGoModule rec {
   subPackages = [ "cmd/${pname}" ];
 
   postInstall = ''
-    substituteInPlace mpd-mpris.service \
-      --replace /usr/bin $out/bin
     mkdir -p $out/lib/systemd/user
     cp mpd-mpris.service $out/lib/systemd/user
   '';
