ddd4ff164ea736d25df33d86b494e0ae19c5e5ec
R. Ryantm <ryantm-bot@ryantm.com>
2023-08-31 23:43:36 +0000

    aspectj: 1.9.19 -> 1.9.20

--- pkgs/development/compilers/aspectj/default.nix
+++ pkgs/development/compilers/aspectj/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "aspectj";
-  version = "1.9.19";
+  version = "1.9.20";
   builder = ./builder.sh;
 
   src = let
     versionSnakeCase = builtins.replaceStrings ["."] ["_"] version;
   in fetchurl {
     url = "https://github.com/eclipse/org.aspectj/releases/download/V${versionSnakeCase}/aspectj-${version}.jar";
-    sha256 = "sha256-Oujyg05yvtcyfLmqonc++GX9AyFKwfIzITOHDz0px0M=";
+    sha256 = "sha256-zrU7JlEyUwoYxQ+sTaJM4YGVW5NucDXDiEao4glJAk0=";
   };
 
   inherit jre;
