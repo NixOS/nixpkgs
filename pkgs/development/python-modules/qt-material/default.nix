d82467f0172142e0b3b133159d2460fbd7f99185
mib <mib@mib.dev>
2023-08-31 18:57:18 +0200

    python3Packages.qt-material: init at 2.14

---
+++ pkgs/development/python-modules/qt-material/default.nix
@@ -0,0 +1,26 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, jinja2
+}:
+
+buildPythonPackage rec {
+  pname = "qt-material";
+  version = "2.14";
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-tdu1relyF8964za7fAR8kL6zncfyBIpJjJFq1fL3riM=";
+  };
+
+  propagatedBuildInputs = [
+    jinja2
+  ];
+
+  meta = with lib; {
+    description = "Material inspired stylesheet for PySide2, PySide6, PyQt5 and PyQt6";
+    homepage = "https://pypi.org/project/qt-material";
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ mib ];
+  };
+}
