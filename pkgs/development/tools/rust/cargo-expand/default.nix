334ce2392b9532c60c86ff62ff5e5b8e4d7bf0df
figsoda <figsoda@pm.me>
2023-08-31 08:46:40 -0400

    cargo-expand: 1.0.64 -> 1.0.65
    Diff: https://github.com/dtolnay/cargo-expand/compare/1.0.64...1.0.65
    
    Changelog: https://github.com/dtolnay/cargo-expand/releases/tag/1.0.65

--- pkgs/development/tools/rust/cargo-expand/default.nix
+++ pkgs/development/tools/rust/cargo-expand/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-expand";
-  version = "1.0.64";
+  version = "1.0.65";
 
   src = fetchFromGitHub {
     owner = "dtolnay";
     repo = pname;
     rev = version;
-    sha256 = "sha256-8DBkF2JRrsrZTW6gNBiUyVpwFvOpoZnm2vsR4O/6c6c=";
+    sha256 = "sha256-fPrke89Nlr9Yj0bkB6HTwMymQsjAQ+5+o7iVPGH7Tyc=";
   };
 
-  cargoHash = "sha256-r5qRxc8Jt5OBWCiBhFs8zPoxbuercsQ7gw5FfOKH3ow=";
+  cargoHash = "sha256-Q7nBKdwZlL/HleuiniuvErGF6Avf58xR++cJ7PP8RME=";
 
   meta = with lib; {
     description = "A utility and Cargo subcommand designed to let people expand macros in their Rust source code";
