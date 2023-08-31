d7c9dc08d9d9a817dd8611886b1ff2b2f958c812
Fabian Affolter <mail@fabian-affolter.ch>
2023-08-31 09:51:38 +0200

    python311Packages.pyinsteon: update disabled

--- pkgs/development/python-modules/pyinsteon/default.nix
+++ pkgs/development/python-modules/pyinsteon/default.nix
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   version = "1.5.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = pname;
