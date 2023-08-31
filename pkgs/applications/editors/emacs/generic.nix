NixOS/nixpkgs

commit f607a7851619a6efda5f7c57f0345018a3ea403c

emacs-macport: build on LLVM 14
- Build on `apple_sdk_11_0` to support `aarch64-darwin`
- Add frameworks required for building on newer SDKs
  - `Accelerate`, `UniformTypeIdentifiers`
- Include header to work around `CF_NOESCAPE` issue circa LLVM 7.0

pkgs/applications/editors/emacs/generic.nix
@@ -42,7 +42,7 @@
 , libtiff
 , libwebp
 , libxml2
-, llvmPackages_6
+, llvmPackages_14
 , m17n_lib
 , makeWrapper
 , motif
@@ -99,6 +99,7 @@
   else "lucid")
 
 # macOS dependencies for NS and macPort
+, Accelerate
 , AppKit
 , Carbon
 , Cocoa
@@ -109,6 +110,7 @@
 , OSAKit
 , Quartz
 , QuartzCore
+, UniformTypeIdentifiers
 , WebKit
 }:
 
@@ -135,7 +137,7 @@ let
   ];
 
   inherit (if variant == "macport"
-           then llvmPackages_6.stdenv
+           then llvmPackages_14.stdenv
            else stdenv) mkDerivation;
 in
 mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
@@ -287,6 +289,7 @@ mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
     GSS
     ImageIO
   ] ++ lib.optionals (variant == "macport") [
+    Accelerate
     AppKit
     Carbon
     Cocoa
@@ -294,6 +297,7 @@ mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
     OSAKit
     Quartz
     QuartzCore
+    UniformTypeIdentifiers
     WebKit
     # TODO are these optional?
     GSS
@@ -336,6 +340,11 @@ mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
   ++ lib.optional withXinput2 "--with-xinput2"
   ++ lib.optional withXwidgets "--with-xwidgets"
   ;
+
+  # Fixes intermittent segfaults when compiled with LLVM >= 7.0.
+  # See https://github.com/NixOS/nixpkgs/issues/127902
+  env.NIX_CFLAGS_COMPILE = lib.optionalString (variant == "macport")
+    "-include ${./macport_noescape_noop.h}";
 
   enableParallelBuilding = true;
 

pkgs/applications/editors/emacs/default.nix
@@ -8,9 +8,9 @@ lib.makeScope pkgs.newScope (self:
       inherit gconf;
 
       inherit (pkgs.darwin) sigtool;
-      inherit (pkgs.darwin.apple_sdk.frameworks)
-        AppKit Carbon Cocoa GSS ImageCaptureCore ImageIO IOKit OSAKit Quartz
-        QuartzCore WebKit;
+      inherit (pkgs.darwin.apple_sdk_11_0.frameworks)
+        Accelerate AppKit Carbon Cocoa GSS ImageCaptureCore ImageIO IOKit OSAKit
+        Quartz QuartzCore UniformTypeIdentifiers WebKit;
     };
   in {
     sources = import ./sources.nix {

pkgs/applications/editors/emacs/macport_noescape_noop.h
@@ -1,0 +1,31 @@
+#ifndef NOESCAPE_NOOP_H_
+#define NOESCAPE_NOOP_H_
+
+// First, do some work to get definitions for *_WIDTH. Normally, Emacs would
+// have these defined by headers in-tree, but clang's headers clash with those.
+// Due to how include paths work, we have to include clang headers if we want to
+// mess with CoreFoundation definitions.
+#pragma push_macro("__STDC_VERSION__")
+// Make the preprocessor think that we're on C2x. The macros we want are gated
+// on it.
+#undef __STDC_VERSION__
+#define __STDC_VERSION__ 202000L
+// Include limits.h first, as stdint.h includes it.
+#include <limits.h>
+
+// XX: clang's stdint.h is shy and won't give us its defs unless it thinks it's
+// in freestanding mode.
+#undef __STDC_HOSTED__
+#include <stdint.h>
+#define __STDC_HOSTED__ 1
+
+#pragma pop_macro("__STDC_VERSION__")
+
+// Now, pull in the header that defines CF_NOESCAPE.
+#include <CoreFoundation/CFBase.h>
+
+// Redefine CF_NOESCAPE as empty.
+#undef CF_NOESCAPE
+#define CF_NOESCAPE
+
+#endif // NOESCAPE_NOOP_H_

pkgs/top-level/all-packages.nix
@@ -31492,7 +31492,7 @@ with pkgs;
 
   em = callPackage ../applications/editors/em { };
 
-  inherit (recurseIntoAttrs (callPackage ../applications/editors/emacs { }))
+  inherit (recurseIntoAttrs (darwin.apple_sdk_11_0.callPackage ../applications/editors/emacs { }))
     emacs28
     emacs28-gtk2
     emacs28-gtk3

commit dc56cbd5091549b6b14751e97e61044b6b4582ca

emacs: fix `env` shallow merge
Fixes regression caused by #252244.

`env` was first defined in its own attrset, which was merged with a
second attrset:

```nix
...
{
   env = {
     NATIVE_FULL_AOT = "1";
     LIBRARY_PATH = lib.concatStringsSep ":" libGccJitLibraryPaths;
   };
} // {
...
   env.NIX_CFLAGS_COMPILE = ...
...
}
```

In this situation, the `env` from the first attrset is not preserved,
since `//` does a shallow merge.

Signed-off-by: Andrew Pan <a@tny.town>

pkgs/applications/editors/emacs/generic.nix
@@ -140,12 +140,7 @@ let
            then llvmPackages_14.stdenv
            else stdenv) mkDerivation;
 in
-mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
-  env = {
-    NATIVE_FULL_AOT = "1";
-    LIBRARY_PATH = lib.concatStringsSep ":" libGccJitLibraryPaths;
-  };
-} // {
+mkDerivation (finalAttrs: {
   pname = pname
           + (if noGui then "-nox"
              else if variant == "macport" then "-macport"
@@ -341,10 +336,14 @@ mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
   ++ lib.optional withXwidgets "--with-xwidgets"
   ;
 
-  # Fixes intermittent segfaults when compiled with LLVM >= 7.0.
-  # See https://github.com/NixOS/nixpkgs/issues/127902
-  env.NIX_CFLAGS_COMPILE = lib.optionalString (variant == "macport")
-    "-include ${./macport_noescape_noop.h}";
+  env = lib.optionalAttrs withNativeCompilation {
+    NATIVE_FULL_AOT = "1";
+    LIBRARY_PATH = lib.concatStringsSep ":" libGccJitLibraryPaths;
+  } // lib.optionalAttrs (variant == "macport") {
+    # Fixes intermittent segfaults when compiled with LLVM >= 7.0.
+    # See https://github.com/NixOS/nixpkgs/issues/127902
+    NIX_CFLAGS_COMPILE = "-include ${./macport_noescape_noop.h}";
+  };
 
   enableParallelBuilding = true;
 
@@ -405,4 +404,4 @@ mkDerivation (finalAttrs: (lib.optionalAttrs withNativeCompilation {
   meta = meta // {
     broken = !(stdenv.buildPlatform.canExecute stdenv.hostPlatform);
   };
-}))
+})