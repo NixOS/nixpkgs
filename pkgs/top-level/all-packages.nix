02601f61b943b53937213b7bcc3f4ac2f8c87c12
natsukium <tomoya.otabi@gmail.com>
2023-08-31 17:26:38 +0900

    parallel-hashmap: init at 1.3.11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24566,6 +24566,8 @@ with pkgs;
 
   paperkey = callPackage ../tools/security/paperkey { };
 
+  parallel-hashmap = callPackage ../development/libraries/parallel-hashmap { };
+
   parsero = callPackage ../tools/security/parsero { };
 
   pcaudiolib = callPackage ../development/libraries/pcaudiolib { };
