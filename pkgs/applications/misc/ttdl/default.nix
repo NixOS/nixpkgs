a27bb795e5f6b580eb351c08b65a0579ddf1bf78
3JlOy_PYCCKUI <3jl0y_pycckui@riseup.net>
2023-08-31 22:48:39 +0300

    ttdl: 4.0.0 -> 4.1.0

--- pkgs/applications/misc/ttdl/default.nix
+++ pkgs/applications/misc/ttdl/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ttdl";
-  version = "4.0.0";
+  version = "4.1.0";
 
   src = fetchFromGitHub {
     owner = "VladimirMarkelov";
     repo = "ttdl";
     rev = "v${version}";
-    sha256 = "sha256-er2IDPVFbcuT0COBCpBymNVBKtETdWjzR2330WUBp6U=";
+    sha256 = "sha256-twl72feo1PpWZbs50a962pvvD5YUHfZRW9OjY/meYVo=";
   };
 
-  cargoHash = "sha256-WcqkvnXRgsDJM7p2WGa5lmeeuwNwBy2ZQATVkTHRX7Q=";
+  cargoHash = "sha256-ZoVPC/PyMm+yuDYhVFykIBk0T5RNLAfmIT36Tl/dxCo=";
 
   meta = with lib; {
     description = "A CLI tool to manage todo lists in todo.txt format";
