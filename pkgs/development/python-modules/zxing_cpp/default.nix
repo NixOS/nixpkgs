55ca056cbd4ae56dc6adfd2ccad64df727014f05
Anderson Torres <torres.anderson.85@protonmail.com>
2023-09-01 20:28:31 -0300

    python3.pkgs.zxing_cpp: refactor
    - Inherit basically everything from zxing-cpp
    - Remove patches

--- pkgs/development/python-modules/zxing_cpp/default.nix
+++ pkgs/development/python-modules/zxing_cpp/default.nix
@@ -1,20 +1,18 @@
-{ buildPythonPackage
-, lib
+{ lib
+, buildPythonPackage
 , cmake
-, pybind11
-, zxing-cpp
 , numpy
 , pillow
+, pybind11
+, zxing-cpp
 }:
 
 buildPythonPackage rec {
   pname = "zxing_cpp";
-  inherit (zxing-cpp) src version;
+  inherit (zxing-cpp) src version meta;
 
   sourceRoot = "${src.name}/wrappers/python";
-  patches = [
-    ./use-nixpkgs-pybind11.patch
-  ];
+
   dontUseCmakeConfigure = true;
 
   propagatedBuildInputs = [
@@ -29,21 +27,4 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     pillow
   ];
-
-  meta = with lib; {
-    homepage = "https://github.com/zxing-cpp/zxing-cpp";
-    description = "Python bindings for C++ port of zxing (a Java barcode image processing library)";
-    longDescription = ''
-      ZXing-C++ ("zebra crossing") is an open-source, multi-format 1D/2D barcode
-      image processing library implemented in C++.
-
-      It was originally ported from the Java ZXing Library but has been
-      developed further and now includes many improvements in terms of quality
-      and performance. It can both read and write barcodes in a number of
-      formats.
-    '';
-    license = licenses.asl20;
-    maintainers = with maintainers; [ lukegb ];
-    platforms = with platforms; unix;
-  };
 }
