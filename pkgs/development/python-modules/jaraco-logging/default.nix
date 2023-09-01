61e80c46c3fc55146d865e2f5a0a84d933ef957e
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-01 14:10:23 +0200

    python310Packages.jaraco-logging: add changelog to meta

--- pkgs/development/python-modules/jaraco-logging/default.nix
+++ pkgs/development/python-modules/jaraco-logging/default.nix
@@ -18,7 +18,9 @@ buildPythonPackage rec {
     hash = "sha256-X8ssPxI1HU1QN1trYPOJnFg3sjLxotj/y6/17NK3M+c=";
   };
 
-  pythonNamespaces = [ "jaraco" ];
+  pythonNamespaces = [
+    "jaraco"
+  ];
 
   nativeBuildInputs = [
     setuptools
@@ -33,11 +35,14 @@ buildPythonPackage rec {
   # test no longer packaged with pypi
   doCheck = false;
 
-  pythonImportsCheck = [ "jaraco.logging" ];
+  pythonImportsCheck = [
+    "jaraco.logging"
+  ];
 
   meta = with lib; {
     description = "Support for Python logging facility";
     homepage = "https://github.com/jaraco/jaraco.logging";
+    changelog = "https://github.com/jaraco/jaraco.logging/blob/v${version}/NEWS.rst";
     license = licenses.mit;
     maintainers = with maintainers; [ ];
   };
