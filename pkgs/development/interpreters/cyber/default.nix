9fcb9d577fe7376111bfb75f14ac76bfede44272
figsoda <figsoda@pm.me>
2023-09-01 14:52:41 -0400

    cyber: unstable-2023-08-24 -> unstable-2023-09-01
    Diff: https://github.com/fubark/cyber/compare/be76bc13590285cffa502c3c97470a80ff1f27bd...1dae891be5ca1e64ad8ab9d60be0b30e1ef28439

--- pkgs/development/interpreters/cyber/default.nix
+++ pkgs/development/interpreters/cyber/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cyber";
-  version = "unstable-2023-08-24";
+  version = "unstable-2023-09-01";
 
   src = fetchFromGitHub {
     owner = "fubark";
     repo = "cyber";
-    rev = "be76bc13590285cffa502c3c97470a80ff1f27bd";
-    hash = "sha256-DhGp+vHz+FfF9ZGopQshF2t0Q4/yeN7CEpIlPliPBgQ=";
+    rev = "1dae891be5ca1e64ad8ab9d60be0b30e1ef28439";
+    hash = "sha256-5GCIdk6XCJIXZLFsNMzo15Qhtj7zd/DOcARe8GXF2lc=";
   };
 
   nativeBuildInputs = [
