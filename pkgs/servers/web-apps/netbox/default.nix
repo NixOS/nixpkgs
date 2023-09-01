da27f8fe69619961bedb1ad76f03c1abdf1c5c22
Raito Bezarius <masterancpp@gmail.com>
2023-09-02 00:10:08 +0200

    netbox_3_6: init, netbox_3_3: drop
    We introduce 3.6 series:
    https://github.com/netbox-community/netbox/releases/tag/v3.6.0
    
    We drop 3.3 series which should be, by now, unused by everyone.
    
    We do the adequate dance for the tests (upgrade from 3.5 to 3.6).

--- pkgs/servers/web-apps/netbox/default.nix
+++ pkgs/servers/web-apps/netbox/default.nix
@@ -3,28 +3,6 @@ let
   generic = import ./generic.nix;
 in
 {
-  netbox_3_3 = callPackage generic {
-    version = "3.3.10";
-    hash = "sha256-MeOfTU5IxNDoUh7FyvwAQNRC/CE0R6p40WnlF+3RuxA=";
-    extraPatches = [
-      # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
-      ./config_3_3.patch
-      ./graphql-3_2_0.patch
-      # fix compatibility ith django 4.1
-      (fetchpatch {
-        url = "https://github.com/netbox-community/netbox/pull/10341/commits/ce6bf9e5c1bc08edc80f6ea1e55cf1318ae6e14b.patch";
-        sha256 = "sha256-aCPQp6k7Zwga29euASAd+f13hIcZnIUu3RPAzNPqgxc=";
-      })
-    ];
-
-    tests = {
-      netbox = nixosTests.netbox_3_3;
-      inherit (nixosTests) netbox-upgrade;
-    };
-    maintainers = with lib.maintainers; [ n0emis raitobezarius ];
-    eol = true;
-  };
-
   netbox = callPackage generic {
     version = "3.5.9";
     hash = "sha256-CJbcuCyTuihDXrObSGyJi2XF+zgWAwcJzjxtkX8pmKs=";
@@ -37,5 +15,20 @@ in
     };
 
     maintainers = with lib.maintainers; [ minijackson n0emis raitobezarius ];
+  };
+
+  netbox_3_6 = callPackage generic {
+    version = "3.6.0";
+    hash = "sha256-LLyhys9T5cNepxYeMGVSeuZ/HJNAO0/iWxA9o5Xa+2M=";
+    extraPatches = [
+      # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
+      ./config.patch
+    ];
+    tests = {
+      netbox = nixosTests.netbox_3_6;
+      inherit (nixosTests) netbox-upgrade;
+    };
+
+    maintainers = with lib.maintainers; [ raitobezarius ];
   };
 }
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11046,7 +11046,7 @@ with pkgs;
   netbootxyz-efi = callPackage ../tools/misc/netbootxyz-efi { };
 
   inherit (callPackage ../servers/web-apps/netbox { })
-    netbox_3_3 netbox;
+    netbox_3_3 netbox netbox_3_6;
 
   netbox2netshot = callPackage ../tools/admin/netbox2netshot { };
 
