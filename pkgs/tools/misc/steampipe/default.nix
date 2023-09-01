163832681ea15f5f7646a19b61670d031f58d198
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-01 22:29:28 +0000

    steampipe: 0.20.10 -> 0.20.11

--- pkgs/tools/misc/steampipe/default.nix
+++ pkgs/tools/misc/steampipe/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "steampipe";
-  version = "0.20.10";
+  version = "0.20.11";
 
   src = fetchFromGitHub {
     owner = "turbot";
     repo = "steampipe";
     rev = "v${version}";
-    sha256 = "sha256-v0ZVyiFHiRsn6ARA7sLCfNfoljTFegCXb1/C4sY0vfg=";
+    sha256 = "sha256-idMVQyRpWc4ZT9GZvQvShwIY9I9t9MArfih57trhJvc=";
   };
 
   vendorHash = "sha256-VuUzo+afUazXH7jaR4Qm5Kfr6qiyHqdGLJWS3MX8oOA=";
