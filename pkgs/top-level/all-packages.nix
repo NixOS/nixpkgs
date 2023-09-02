e66d4940421639c89ecb532a8f86958ddbee5c7e
figsoda <figsoda@pm.me>
2023-09-02 18:32:25 -0400

    pysensation: init at 1.0.0
    https://github.com/mimseyedi/pysentation

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12327,6 +12327,8 @@ with pkgs;
 
   pydeps = with python3Packages; toPythonApplication pydeps;
 
+  pysentation = callPackage ../applications/misc/pysentation { };
+
   python-launcher = callPackage ../development/tools/misc/python-launcher { };
 
   pytrainer = callPackage ../applications/misc/pytrainer { };
