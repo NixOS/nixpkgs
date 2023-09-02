f51b59e2ddb7b38c61a869617b805ea9c913ab34
Isidor Zeuner <nix@quidecco.pl>
2023-09-02 22:34:51 +0200

    gf2x: switch to inria gitlab as predicted before

--- pkgs/development/libraries/gf2x/default.nix
+++ pkgs/development/libraries/gf2x/default.nix
@@ -9,10 +9,8 @@ stdenv.mkDerivation rec {
   pname = "gf2x";
   version = "1.3.0";
 
-  # upstream has plans to move to gitlab:
-  # https://github.com/NixOS/nixpkgs/pull/45299#issuecomment-564477936
   src = fetchgit {
-    url = "https://scm.gforge.inria.fr/anonscm/git/gf2x/gf2x.git";
+    url = "https://gitlab.inria.fr/gf2x/gf2x";
     rev = "gf2x-${version}";
     sha256 = "04g5jg0i4vz46b4w2dvbmahwzi3k6b8g515mfw7im1inc78s14id";
   };
