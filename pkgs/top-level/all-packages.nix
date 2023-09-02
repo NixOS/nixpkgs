6e5c5bdaad5bb7be35a451d862c713f2963933bb
Adrian Pistol <vifino@posteo.net>
2023-09-02 11:38:26 +0200

    uftpd: init at 2.15

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14195,6 +14195,8 @@ with pkgs;
 
   uftrace = callPackage ../development/tools/uftrace { };
 
+  uftpd = callPackage ../servers/ftp/uftpd {};
+
   uget = callPackage ../tools/networking/uget { };
 
   uget-integrator = callPackage ../tools/networking/uget-integrator { };
