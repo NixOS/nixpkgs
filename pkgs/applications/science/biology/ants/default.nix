35145f9df092a476a7995018ec1115c97d426fee
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-09-02 22:17:19 +0200

    ants: reformat file using `nixpkgs-fmt`

--- pkgs/applications/science/biology/ants/default.nix
+++ pkgs/applications/science/biology/ants/default.nix
@@ -1,4 +1,12 @@
-{ lib, stdenv, fetchFromGitHub, cmake, makeWrapper, itk, vtk, Cocoa }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, makeBinaryWrapper
+, itk
+, vtk
+, Cocoa
+}:
 
 stdenv.mkDerivation rec {
   pname = "ANTs";
@@ -11,10 +19,22 @@ stdenv.mkDerivation rec {
     hash = "sha256-rSibcsprhMC1qsuZN8ou32QPLf8n62BiDzpnTRWRx0Q=";
   };
 
-  nativeBuildInputs = [ cmake makeWrapper ];
-  buildInputs = [ itk vtk ] ++ lib.optionals stdenv.isDarwin [ Cocoa ];
+  nativeBuildInputs = [
+    cmake
+    makeBinaryWrapper
+  ];
 
-  cmakeFlags = [ "-DANTS_SUPERBUILD=FALSE" "-DUSE_VTK=TRUE" ];
+  buildInputs = [
+    itk
+    vtk
+  ] ++ lib.optionals stdenv.isDarwin [
+    Cocoa
+  ];
+
+  cmakeFlags = [
+    "-DANTS_SUPERBUILD=FALSE"
+    "-DUSE_VTK=TRUE"
+  ];
 
   postInstall = ''
     for file in $out/bin/*; do
@@ -22,11 +42,11 @@ stdenv.mkDerivation rec {
     done
   '';
 
-  meta = with lib; {
-    homepage = "https://github.com/ANTsX/ANTs";
+  meta = {
     description = "Advanced normalization toolkit for medical image registration and other processing";
-    maintainers = with maintainers; [ bcdarwin ];
-    platforms = platforms.unix;
-    license = licenses.bsd3;
+    homepage = "https://github.com/ANTsX/ANTs";
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ bcdarwin ];
+    platforms = lib.platforms.unix;
   };
 }
