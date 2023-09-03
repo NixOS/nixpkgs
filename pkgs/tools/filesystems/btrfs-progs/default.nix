c63332adf6bcdbef9b897b7a67ee56c53ae2c09d
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-03 09:03:08 +0000

    btrfs-progs: 6.3.3 -> 6.5

--- pkgs/tools/filesystems/btrfs-progs/default.nix
+++ pkgs/tools/filesystems/btrfs-progs/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "btrfs-progs";
-  version = "6.3.3";
+  version = "6.5";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    sha256 = "sha256-S+MAFXYCcNCBZCzAAjqL/ufLZXoqoFVkTmpW1oaVuW4=";
+    sha256 = "sha256-j1B6DN9qizcthi2+SUP+hMZtyyCI48/eLPs7F26sHBw=";
   };
 
   nativeBuildInputs = [
