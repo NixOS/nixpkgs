5c81ba32c4d04af0e0977632b0cda0f02e303842
Knarkzel <85593302+knarkzel@users.noreply.github.com>
2023-09-03 06:55:57 +0200

    Add changes

--- pkgs/applications/networking/instant-messengers/dorion/default.nix
+++ pkgs/applications/networking/instant-messengers/dorion/default.nix
@@ -1,29 +1,33 @@
-{
-  stdenv,
-  lib,
-  dpkg,
-  fetchurl,
-  autoPatchelfHook,
-  wrapGAppsHook,
-  webkitgtk,
-  glib-networking,
-  libappindicator,
-  libayatana-appindicator,
-  gst_all_1,
+{ lib
+, stdenv
+, dpkg
+, fetchurl
+, autoPatchelfHook
+, wrapGAppsHook
+, webkitgtk
+, glib-networking
+, libappindicator
+, libayatana-appindicator
+, gst_all_1
 }:
-stdenv.mkDerivation rec {
+
+stdenv.mkDerivation (finalAttrs: {
   name = "dorion";
   version = "1.0.1";
 
   src = fetchurl {
-    url = "https://github.com/SpikeHD/Dorion/releases/download/v${version}/Dorion_${version}_amd64.deb";
-    sha256 = "sha256-IYqJ5mz+XGHf4GVSW2Mq/z8xWLs4Y4KRWZ6fAtIg2tk=";
+    url = "https://github.com/SpikeHD/Dorion/releases/download/v${finalAttrs.version}/Dorion_${finalAttrs.version}_amd64.deb";
+    hash = "sha256-IYqJ5mz+XGHf4GVSW2Mq/z8xWLs4Y4KRWZ6fAtIg2tk=";
   };
 
+  unpackCmd = ''
+    dpkg-deb -x $curSrc source
+  '';
+  
   runtimeDependencies = [
+    glib-networking
     libappindicator
     libayatana-appindicator
-    glib-networking
   ];
 
   nativeBuildInputs = [
@@ -40,23 +44,25 @@ stdenv.mkDerivation rec {
     gst_all_1.gst-plugins-bad
   ];
 
-  unpackCmd = "dpkg-deb -x $curSrc source";
-
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     mv usr/share/ $out
     mkdir -p $out/usr/lib/dorion/data/usr
     mv usr/{bin,lib} $out/usr/lib/dorion/data/usr
     ln -s $out/usr/lib/dorion/data/usr/bin/dorion $out/bin/dorion
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "Tiny alternative Discord client with a smaller footprint, themes and plugins, multi-profile, and more!";
     homepage = "https://github.com/SpikeHD/Dorion";
-    maintainers = with maintainers; [knarkzel nyanbinary];
+    maintainers = with maintainers; [ knarkzel nyanbinary ];
     license = licenses.gpl3Only;
-    sourceProvenance = with sourceTypes; [binaryNativeCode];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     platforms = platforms.linux;
     mainProgram = "dorion";
   };
-}
+})
