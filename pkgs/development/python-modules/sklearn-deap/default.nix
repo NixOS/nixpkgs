4a65ac398b822a8e3572d2764a0b5f47ccc0c27b
natsukium <tomoya.otabi@gmail.com>
2023-09-03 12:46:14 +0900

    python310Packages.sklearn-deap: refactor
    - add format
    - add unittestCheckHook to nativeCheckInputs
    - enable pythonImportsCheck
    - change the license to MIT from LGPL3

--- pkgs/development/python-modules/sklearn-deap/default.nix
+++ pkgs/development/python-modules/sklearn-deap/default.nix
@@ -1,13 +1,23 @@
-{ lib, buildPythonPackage, fetchFromGitHub, fetchpatch, numpy, scipy, deap, scikit-learn, python }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, fetchpatch
+, deap
+, numpy
+, scikit-learn
+, scipy
+, unittestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "sklearn-deap";
   version = "0.3.0";
+  format = "setuptools";
 
   # No tests in Pypi
   src = fetchFromGitHub {
     owner = "rsteca";
-    repo = pname;
+    repo = "sklearn-deap";
     rev = version;
     hash = "sha256-bXBHlv1pIOyDLKCBeffyHaTZ7gNiZNl0soa73e8E4/M=";
   };
@@ -26,14 +36,16 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [ numpy scipy deap scikit-learn ];
 
-  checkPhase = ''
-    ${python.interpreter} test.py
-  '';
+  nativeCheckInputs = [
+    unittestCheckHook
+  ];
+
+  pythonImportsCheck = [ "evolutionary_search" ];
 
   meta = with lib; {
     description = "Use evolutionary algorithms instead of gridsearch in scikit-learn";
     homepage = "https://github.com/rsteca/sklearn-deap";
-    license = licenses.lgpl3;
+    license = licenses.mit;
     maintainers = with maintainers; [ psyanticy ];
   };
 }
