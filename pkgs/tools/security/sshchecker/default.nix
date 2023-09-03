f3171ea8bc7a26a096d0d0e67cdab76a55ba438c
Aaron Jheng <wentworth@outlook.com>
2023-09-03 18:02:32 +0800

    lazytools: use sri hash

--- pkgs/tools/security/sshchecker/default.nix
+++ pkgs/tools/security/sshchecker/default.nix
@@ -11,10 +11,10 @@ buildGoModule rec {
     owner = "lazytools";
     repo = pname;
     rev = "v${version}";
-    sha256 = "139b850h1w0392k8jcgj22jscsl2l60b5kk0n8378b6g57ikmis0";
+    hash = "sha256-QMc64ynPLHQGsmDOsoChgmqmpRDyMYmmSAPwAEFBK40=";
   };
 
-  vendorSha256 = "19hdaf7d6lvwrl5rc1srrjsjx57g25cy4lvw0vvs6j52impdk6ak";
+  vendorHash = "sha256-U5nZbo2iSKP3BnxT4lkR75QutcxZB5YLzXxT045TDaY=";
 
   meta = with lib; {
     description = "Dedicated SSH brute-forcing tool";
