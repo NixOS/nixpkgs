2c9005e5fac5c4ab2532cd83e0422add1c3f7b31
natsukium <tomoya.otabi@gmail.com>
2023-09-04 18:04:55 +0900

    python311Packages.resolvelib: 0.5.5 -> 1.0.1
    Diff: https://github.com/sarugaku/resolvelib/compare/0.5.5...1.0.1
    
    Changelog: https://github.com/sarugaku/resolvelib/blob/1.0.1/CHANGELOG.rst

--- pkgs/development/python-modules/resolvelib/default.nix
+++ pkgs/development/python-modules/resolvelib/default.nix
@@ -8,19 +8,14 @@
 
 buildPythonPackage rec {
   pname = "resolvelib";
-  # Currently this package is only used by Ansible and breaking changes
-  # are frequently introduced, so when upgrading ensure the new version
-  # is compatible with Ansible
-  # https://github.com/NixOS/nixpkgs/pull/128636
-  # https://github.com/ansible/ansible/blob/devel/requirements.txt
-  version = "0.5.5";
+  version = "1.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "sarugaku";
     repo = "resolvelib";
     rev = version;
-    sha256 = "198vfv78hilpg0d0mjzchzp9zk6239wnra61vlsgwpcgz66d2bgv";
+    hash = "sha256-oxyPn3aFPOyx/2aP7Eg2ThtPbyzrFT1JzWqy6GqNbzM=";
   };
 
   build-system = [
@@ -30,17 +25,6 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     commentjson
     pytestCheckHook
-  ];
-  # TODO: reenable after updating to >= 1.0.0
-  # https://github.com/sarugaku/resolvelib/issues/114
-  disabledTests = [
-    "shared_parent_dependency"
-    "deep_complex_conflict"
-    "shared_parent_dependency_with_swapping"
-    "spapping_and_rewinding"
-    "pruned_unresolved_orphan"
-    "conflict_common_parent"
-    "same-package"
   ];
 
   pythonImportsCheck = [
