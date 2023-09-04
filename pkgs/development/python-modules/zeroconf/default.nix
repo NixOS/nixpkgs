34398795e8d465a37e7e7d461b6b865c333503b4
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-04 22:51:02 +0200

    python311Packages.zeroconf: 0.94.0 -> 0.95.0
    Diff: https://github.com/jstasiak/python-zeroconf/compare/refs/tags/0.94.0...0.95.0
    
    Changelog: https://github.com/python-zeroconf/python-zeroconf/releases/tag/0.95.0

--- pkgs/development/python-modules/zeroconf/default.nix
+++ pkgs/development/python-modules/zeroconf/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "zeroconf";
-  version = "0.94.0";
+  version = "0.95.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "jstasiak";
     repo = "python-zeroconf";
     rev = "refs/tags/${version}";
-    hash = "sha256-7ZqnFL10tce/7EnMJSRRwTwTuAKX8q2ABKlsN7k22ms=";
+    hash = "sha256-Zn+WIrgRoBDWBEY74FTNFHSsNR2Xujay/at85OPNfrw=";
   };
 
   nativeBuildInputs = [
