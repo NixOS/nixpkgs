65f9a7b416427d3c52ed68cc2020dafa76adc108
Anthony Roussel <anthony@roussel.dev>
2023-09-04 12:44:51 +0200

    dgoss: add anthonyroussel to maintainers

--- pkgs/tools/misc/dgoss/default.nix
+++ pkgs/tools/misc/dgoss/default.nix
@@ -43,6 +43,6 @@ resholve.mkDerivation rec {
     description = "Convenience wrapper around goss that aims to bring the simplicity of goss to docker containers";
     license = licenses.asl20;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ hyzual ];
+    maintainers = with maintainers; [ hyzual anthonyroussel ];
   };
 }
