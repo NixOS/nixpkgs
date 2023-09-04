f5195c0effb1ae1759a527268fb94bc2173c02ea
Kai Borowiak <kai.borowiak@secunet.com>
2023-09-04 11:57:51 +0200

    build: add missing dependencies

--- pkgs/development/python-modules/restinstance/default.nix
+++ pkgs/development/python-modules/restinstance/default.nix
@@ -1,8 +1,7 @@
 { lib
 , fetchFromGitHub
 , buildPythonPackage
-, jsonschema
-, nox
+, python3Packages
 , pythonOlder
 }:
 
@@ -13,6 +12,39 @@ buildPythonPackage rec {
 
   disabled = pythonOlder "3.7";
 
+  flex = python3Packages.buildPythonPackage rec {
+    pname = "flex";
+    version = "6.14.1";
+    src = python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-KS7Wo38awKEK2GafXOuC6LoxBsFsVAkIIJJ7rIsLKes=";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with python3Packages; [
+      rfc3987
+      click
+      jsonpointer
+      setuptools
+      six
+      strict-rfc3339
+      pyyaml
+      requests
+      validate-email
+   ];
+  };
+
+  genson = python3Packages.buildPythonPackage rec {
+      pname = "genson";
+      version = "v.1.2.2";
+      src = fetchFromGitHub {
+          owner = "wolverdude";
+          repo = "GenSON";
+          rev = "6bf083488de2804c3ff5bb03b05761ff31fac30c";
+          sha256 = "sha256-3RaGY/F//Zt3aOH5ZO+jvRc6vn0+yT14MTYg6yCzO3w=";
+      };
+      doCheck = false;
+  };
+
   src = fetchFromGitHub {
     owner = "asyrjasalo";
     repo = pname;
@@ -20,15 +52,31 @@ buildPythonPackage rec {
     hash = "sha256-ed0gJy0ns+Xs7DO0P7c8Jea9qI5A+tImUEG5Xz8Q3Ng=";
   };
 
-  nativeCheckInputs = [ jsonschema nox ];
+  nativeCheckInputs = with python3Packages; [
+    jsonschema
+    nox
+  ];
 
   checkPhase = ''
     nox -s test
   '';
 
+  propagatedBuildInputs = with python3Packages; [
+    flex
+    genson
+    jsonpath-ng
+    pyyaml
+    pygments
+    pytz
+    requests
+    robotframework
+    tzlocal
+  ];
+
   pythonImportsCheck = [
     "REST"
   ];
+
   meta = with lib; {
     description = "Robot Framework library for RESTful JSON APIs";
     homepage = "https://asyrjasalo.github.io/RESTinstance";
