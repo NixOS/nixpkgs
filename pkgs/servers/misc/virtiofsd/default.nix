2ca2346b60f72fc75bcc570367e24e1d68d55b18
Astro <astro@spaceboyz.net>
2023-09-05 18:46:09 +0200

    virtiofsd: add astro to maintainers

--- pkgs/servers/misc/virtiofsd/default.nix
+++ pkgs/servers/misc/virtiofsd/default.nix
@@ -22,7 +22,7 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     homepage = "https://gitlab.com/virtio-fs/virtiofsd";
     description = "vhost-user virtio-fs device backend written in Rust";
-    maintainers = with maintainers; [ qyliss ];
+    maintainers = with maintainers; [ qyliss astro ];
     platforms = platforms.linux;
     license = with licenses; [ asl20 /* and */ bsd3 ];
   };
