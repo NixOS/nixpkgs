b687f0d5dc0500ffcaa0555867778e76e873fafa
Bobby Rong <rjl931189261@126.com>
2023-09-05 15:11:26 +0000

    libgit2-glib: 1.1.0 → 1.2.0
    https://gitlab.gnome.org/GNOME/libgit2-glib/-/compare/1.1.0...1.2.0

--- pkgs/development/libraries/libgit2-glib/default.nix
+++ pkgs/development/libraries/libgit2-glib/default.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libgit2-glib";
-  version = "1.1.0";
+  version = "1.2.0";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "w43XV12vgUHh5CIzOldfr2XzySEMCOg+mBuI3UG/HvM=";
+    sha256 = "EzHa2oOPTh9ZGyZFnUQSajJd52LcPNJhU6Ma+9/hgZA=";
   };
 
   nativeBuildInputs = [
