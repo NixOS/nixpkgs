10e5871142adfe84aeabe1e65f0b51abf276d163
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-05 20:50:49 +0200

    checkov: 2.4.25 -> 2.4.27
    Diff: https://github.com/bridgecrewio/checkov/compare/refs/tags/2.4.25...2.4.27
    
    Changelog: https://github.com/bridgecrewio/checkov/releases/tag/2.4.27

--- pkgs/development/tools/analysis/checkov/default.nix
+++ pkgs/development/tools/analysis/checkov/default.nix
@@ -22,14 +22,14 @@ with py.pkgs;
 
 buildPythonApplication rec {
   pname = "checkov";
-  version = "2.4.25";
+  version = "2.4.27";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "bridgecrewio";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-IVmC3/TbdVjbbWBY8Buw7CxiZs2D0X375D2dCT3UnM4=";
+    hash = "sha256-HTYxPHCMDYRg4UR2Nm7cHNP9mi0tOPjCTzv3z175YE4=";
   };
 
   patches = [
