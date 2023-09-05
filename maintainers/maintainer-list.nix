2d43f32fb93879c0aaeeb685f1e81a05367d4da9
SamueleFacenda <samuele.facenda@gmail.com>
2023-09-05 10:19:24 +0200

    maintainers: add samuelefacenda
    Signed-off-by: SamueleFacenda <samuele.facenda@gmail.com>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17052,6 +17052,15 @@
     githubId = 132835;
     name = "Samuel Dionne-Riel";
   };
+  samuelefacenda = {
+    name = "Samuele Facenda";
+    email = "samuele.facenda@gmail.com";
+    github = "SamueleFacenda";
+    githubId = 92163673;
+    keys = [{
+      fingerprint = "3BA5 A3DB 3239 E2AC 1F3B  68A0 0DB8 3F58 B259 6271";
+    }];
+  };
   samuel-martineau = {
     name = "Samuel Martineau";
     email = "samuel@smartineau.me";
