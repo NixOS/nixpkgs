b06431985522887ed57f16f91c2d38a639ea7c7f
Yureka <yuka@yuka.dev>
2023-09-06 15:45:13 +0200

    Revert "gitlab-container-registry: 3.79.0 -> 3.82.0"
    This reverts commit 24ae85497f8b2b2e921e2029bbf84d8036de0841.

--- pkgs/applications/version-management/gitlab/gitlab-container-registry/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-container-registry/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "gitlab-container-registry";
-  version = "3.82.0";
+  version = "3.79.0";
   rev = "v${version}-gitlab";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "container-registry";
     inherit rev;
-    sha256 = "sha256-umlpGpeN7sWe8524+wjdYYJegLdc+eQqlgySLWL0a+k=";
+    sha256 = "sha256-JOXJ8HSTf7yW78SlYzbdn7IAbSwv/UE/W3oSJRyGrAQ=";
   };
 
-  vendorHash = "sha256-hFGuzTM9+Zb8BmUoFG059eqM53AzOmi1DeBnF68WSoc=";
+  vendorHash = "sha256-4cxfyG1uhqgIheAVDQbIDb4MRSqAUtLgxGYt5MuNfKw=";
 
   patches = [
     ./Disable-inmemory-storage-driver-test.patch
