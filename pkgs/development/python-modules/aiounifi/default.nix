83659c7c6eb4afdc3ab312aa51283717d329cf53
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-07 00:30:33 +0200

    python311Packages.aiounifi: 61 -> 61
    https://github.com/Kane610/aiounifi/releases/tag/v61

--- pkgs/development/python-modules/aiounifi/default.nix
+++ pkgs/development/python-modules/aiounifi/default.nix
@@ -1,7 +1,6 @@
 { lib
 , aiohttp
 , aioresponses
-, async-timeout
 , buildPythonPackage
 , fetchFromGitHub
 , orjson
@@ -10,25 +9,39 @@
 , pytestCheckHook
 , pythonOlder
 , segno
+, setuptools
+, wheel
 }:
 
 buildPythonPackage rec {
   pname = "aiounifi";
-  version = "55";
-  format = "setuptools";
+  version = "61";
+  format = "pyproject";
 
-  disabled = pythonOlder "3.9";
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "Kane610";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-JvuP1Rhq01Y9KbfAJpawUQNWfxvlf9LY82RvXok4tgw=";
+    hash = "sha256-tzP20KDPCq1/fJY+OfEpo3LMbP662ROh2aPI4nmDp0Y=";
   };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace "setuptools==" "setuptools>=" \
+      --replace "wheel==" "wheel>="
+
+    sed -i '/--cov=/d' pyproject.toml
+  '';
+
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
 
   propagatedBuildInputs = [
     aiohttp
-    async-timeout
     orjson
     segno
   ];
