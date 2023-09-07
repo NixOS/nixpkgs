fa7a6597d79ec9fa4bc8dbe38751c1b1df96f874
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-07 15:45:16 +0200

    python311Packages.pywaze: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10790,6 +10790,8 @@ self: super: with self; {
 
   pywayland = callPackage ../development/python-modules/pywayland { };
 
+  pywaze = callPackage ../development/python-modules/pywaze { };
+
   pywbem = callPackage ../development/python-modules/pywbem {
     inherit (pkgs) libxml2;
   };
