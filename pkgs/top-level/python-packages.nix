618e9a86e16117ad0b437b461ba674a6ba97c03e
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-07 09:54:44 +0200

    python311Packages.pyenphase: init at 1.9.1
    https://github.com/pyenphase/pyenphase

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9046,6 +9046,8 @@ self: super: with self; {
     inherit (pkgs) enchant2;
   };
 
+  pyenphase = callPackage ../development/python-modules/pyenphase { };
+
   pyenvisalink = callPackage ../development/python-modules/pyenvisalink { };
 
   pyephember = callPackage ../development/python-modules/pyephember { };
