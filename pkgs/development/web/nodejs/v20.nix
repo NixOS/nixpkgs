695d8aa69b64e18e0c9dfb75686350e08ad51de9
Mario Rodas <marsam@users.noreply.github.com>
2023-09-07 04:20:00 +0000

    nodejs_20: 20.5.1 -> 20.6.0
    Changelog: https://github.com/nodejs/node/releases/tag/v20.6.0

--- pkgs/development/web/nodejs/v20.nix
+++ pkgs/development/web/nodejs/v20.nix
@@ -8,8 +8,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "20.5.1";
-  sha256 = "sha256-Q5xxqi84woYWV7+lOOmRkaVxJYBmy/1FSFhgScgTQZA=";
+  version = "20.6.0";
+  sha256 = "sha256-nvtcunqPSxjTiw19N6mzDe1zOQyE44DPTeianTCn1vo=";
   patches = [
     ./revert-arm64-pointer-auth.patch
     ./disable-darwin-v8-system-instrumentation-node19.patch
