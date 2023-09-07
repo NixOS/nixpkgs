931f777bb686c753a1a7e04b871d49e72cc24cfc
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-07 23:44:22 +0000

    python310Packages.gspread: 5.11.0 -> 5.11.1

--- pkgs/development/python-modules/gspread/default.nix
+++ pkgs/development/python-modules/gspread/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "gspread";
-  version = "5.11.0";
+  version = "5.11.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "burnash";
     repo = "gspread";
     rev = "refs/tags/v${version}";
-    hash = "sha256-FgAdsHj6Iv08cl5GwdKyGgjVN3pylZUu/plRq/DxVbE=";
+    hash = "sha256-a8A47il9NrMdHkSX4YmQj4VIAYDXK5V+FUdwv+LGIfQ=";
   };
 
   nativeBuildInputs = [
