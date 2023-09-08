5265e8864a7006b8d441fce343a4f3812bd5a6fb
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-08 08:33:27 +0000

    mssql_jdbc: 12.4.0 -> 12.4.1

--- pkgs/servers/sql/mssql/jdbc/default.nix
+++ pkgs/servers/sql/mssql/jdbc/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mssql-jdbc";
-  version = "12.4.0";
+  version = "12.4.1";
 
   src = fetchurl {
     url = "https://github.com/Microsoft/mssql-jdbc/releases/download/v${version}/mssql-jdbc-${version}.jre8.jar";
-    sha256 = "sha256-j9y2czUOVKJigtOsv/B+wQYSVPoKPbb5hLvv0Q3suzs=";
+    sha256 = "sha256-Ci59MLU50Dl+rmUstX7c/BdAJJ/GCCYLqcyrxGr2g7E=";
   };
 
   dontUnpack = true;
