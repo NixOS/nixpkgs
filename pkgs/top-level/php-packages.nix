017157153b689752881977e9e4a46027271c8491
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-09-08 12:37:24 +0200

    php: run `genfiles` for building extensions too

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -31,6 +31,7 @@
 , pam
 , pcre2
 , postgresql
+, bison
 , re2c
 , readline
 , rsync
@@ -101,13 +102,21 @@ lib.makeScope pkgs.newScope (self: with self; {
         autoconf
         pkg-config
         re2c
+        bison
       ];
 
       inherit configureFlags internalDeps buildInputs zendExtension doCheck;
 
       preConfigurePhases = [
+        "genfiles"
         "cdToExtensionRootPhase"
       ];
+
+      genfiles = ''
+        if [ -f "scripts/dev/genfiles" ]; then
+          ./scripts/dev/genfiles
+        fi
+      '';
 
       cdToExtensionRootPhase = ''
         # Go to extension source root.
