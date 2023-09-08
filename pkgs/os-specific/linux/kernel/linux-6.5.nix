a3c4740523b50934c20b28d6a67f0f5fa2f29075
Maximilian Bosch <maximilian@mbosch.me>
2023-09-08 10:44:47 +0200

    linux: 6.5.1 -> 6.5.2

--- pkgs/os-specific/linux/kernel/linux-6.5.nix
+++ pkgs/os-specific/linux/kernel/linux-6.5.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.5.1";
+  version = "6.5.2";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
