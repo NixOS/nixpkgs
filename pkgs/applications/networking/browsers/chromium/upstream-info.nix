14d4cb079b2bafb3256678144c2e90e7688e95ac
networkException <git@nwex.de>
2023-09-09 00:39:31 +0200

    chromium: 116.0.5845.140 -> 116.0.5845.179
    https://chromereleases.googleblog.com/2023/09/stable-channel-update-for-desktop.html
    
    This update includes 4 security fixes.
    
    CVEs:
    CVE-2023-4761 CVE-2023-4762 CVE-2023-4763 CVE-2023-4764

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -41,9 +41,9 @@
         version = "2023-06-09";
       };
     };
-    sha256 = "0v3i688wszchyfs10zgax6yyig2wrdb38fhlzmlsbfh5vawpg5pq";
-    sha256bin64 = "1y442x3n76x9ahsw45m8yw65854h7b5zpmp4ipyvlwm5fx15zn6d";
-    version = "116.0.5845.140";
+    sha256 = "09b0i48sr5ynlhpya4lwnhgp081q4lqd23cc5l59dsxzh5ivbycb";
+    sha256bin64 = "1d49qcjh5mhfzqzjn4ilj23dpzd6nyl1pij5iv43dwxl8z2r3l3m";
+    version = "116.0.5845.179";
   };
   ungoogled-chromium = {
     deps = {
