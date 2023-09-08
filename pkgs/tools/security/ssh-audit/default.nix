717e80b7e069eabaa5e21a4d9dd152f25ecd3f4f
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-08 12:42:22 +0200

    ssh-audit: 2.9.0 -> 3.0.0
    Diff: https://github.com/jtesta/ssh-audit/compare/v2.9.0...v3.0.0

--- pkgs/tools/security/ssh-audit/default.nix
+++ pkgs/tools/security/ssh-audit/default.nix
@@ -2,13 +2,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "ssh-audit";
-  version = "2.9.0";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "jtesta";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-WrED2cSoqR276iOma+pZq/Uu1vQWJmtJvsI73r8ivJA=";
+    sha256 = "sha256-+v+DLZPDC5uffTIJPzMvY/nLoy7BGiAsTddjNZZhTpo=";
   };
 
   nativeCheckInputs = with python3Packages; [
