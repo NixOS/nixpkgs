5c005d9904b5ba9d42360e76f50a25359987e7fa
Patsakula Nikita <nikita.patsakula@gmail.com>
2023-09-08 14:17:10 +0300

    python311Packages.slovnet: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11958,6 +11958,8 @@ self: super: with self; {
 
   slob = callPackage ../development/python-modules/slob { };
 
+  slovnet = callPackage ../development/python-modules/slovnet { };
+
   slowapi = callPackage ../development/python-modules/slowapi { };
 
   slugid = callPackage ../development/python-modules/slugid { };
