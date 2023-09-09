0f5391d982709145c0c0ade00e0d25e199cbec7d
Shogo Takata <peshogo@gmail.com>
2023-09-09 10:28:04 +0900

    circt: 1.53.0 -> 1.54.0

--- pkgs/development/compilers/circt/default.nix
+++ pkgs/development/compilers/circt/default.nix
@@ -13,12 +13,12 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "circt";
-  version = "1.53.0";
+  version = "1.54.0";
   src = fetchFromGitHub {
     owner = "llvm";
     repo = "circt";
     rev = "firtool-${version}";
-    sha256 = "sha256-F3pGXZC4jSG9TV2sc5G9bQtKMY6xcPknwO0fqQ6uBoA=";
+    sha256 = "sha256-jHDQl6UJTyNGZ4PUTEiZCIN/RSRbBxlaVutkwrWbK9M=";
     fetchSubmodules = true;
   };
 
