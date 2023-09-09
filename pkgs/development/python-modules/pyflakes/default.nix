bdf58ac4721da727fdda35e83ff058c8527f5ade
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-09 10:07:10 +0200

    python311Packages.pyflakes: 3.0.1 -> 3.1.0
    Changelog: https://github.com/PyCQA/pyflakes/blob/3.1.0/NEWS.rst

--- pkgs/development/python-modules/pyflakes/default.nix
+++ pkgs/development/python-modules/pyflakes/default.nix
@@ -1,37 +1,29 @@
 { lib
 , buildPythonPackage
-, pythonOlder
 , fetchPypi
-, fetchpatch, isPy311
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pyflakes";
-  version = "3.0.1";
-
-  disabled = pythonOlder "3.6";
-
+  version = "3.1.0";
   format = "setuptools";
+
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-7IsnamtgvYDe/tJa3X5DmIHBnmSFCv2bNGKD1BZf0P0=";
+    hash = "sha256-oKrgNMRE2wBxqgd5crpHaNQMgw2VOf1Fv0zT+PaZLvw=";
   };
-
-  patches = lib.optional isPy311 # could be made unconditional on rebuild
-    (fetchpatch {
-      name = "tests-py311.patch";
-      url = "https://github.com/PyCQA/pyflakes/commit/836631f2f73d45baa4021453d89fc9fd6f52be58.diff";
-      hash = "sha256-xlgql+bN0HsGnTMkwax3ZG/5wrbkUl/kQkjlr3lsgRw=";
-    })
-  ;
 
   nativeCheckInputs = [
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "pyflakes" ];
+  pythonImportsCheck = [
+    "pyflakes"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/PyCQA/pyflakes";
