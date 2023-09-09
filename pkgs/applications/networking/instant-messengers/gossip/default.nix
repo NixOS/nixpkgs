3c745cd0a5e8689cc69fc28f8b09b25fb4403a45
Matthias Beyer <mail@beyermatthias.de>
2023-09-09 11:15:20 +0000

    gossip: 0.7.0 -> unstable-2023-09-07
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/applications/networking/instant-messengers/gossip/default.nix
+++ pkgs/applications/networking/instant-messengers/gossip/default.nix
@@ -10,13 +10,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gossip";
-  version = "0.7.0";
+  version = "unstable-2023-09-07";
 
   src = fetchFromGitHub {
     owner = "mikedilger";
     repo = "gossip";
-    rev = "v${version}";
-    hash = "sha256-Sh9f/cDzPdz/u/xfxfjzg57hs+5AldI60Qm4h/2ipcU=";
+    rev = "7b33619a684d0693c59de99b8275c371fabb6bf2";
+    hash = "sha256-b2hEaXEfxqB+TzrnuzUS+k6cYomsUvr7iFXAwk4cO+Y=";
   };
 
   cargoLock = {
@@ -25,10 +25,12 @@ rustPlatform.buildRustPackage rec {
       "ecolor-0.22.0" = "sha256-4ZGgtUWIDcgZfMkCdYpUK/pHjdsotVVr/4Aa1W8tNl8=";
       "egui-video-0.1.0" = "sha256-HPnZ0Do2hvWCSGCxv+NZHNVKYOOWufJURcs7w+Ctazo=";
       "ffmpeg-next-6.0.0" = "sha256-k8aGyNdaDs85bb3nTWwmnIyQiMIpC/C6HyFMbWZoMDU=";
-      "gossip-relay-picker-0.2.0-unstable" = "sha256-Aq8RHg/WJh4cLw8IuXb6J0Pt614JLCe1qajhV4QCXs0=";
-      "nostr-types-0.7.0-unstable" = "sha256-bzVWo7E5LHd27NDBkjftRQPfj8RQ2DysxzWnWbTAXik==";
+      "gossip-relay-picker-0.2.0-unstable" = "sha256-FY3bwwSv4uKqwu6tnrZgwPttS6n731gYR7OJMjpsM7Y=";
+      "heed-0.20.0-alpha.4" = "sha256-fnT32ntkZB79AIg7c9+5Bry/Dlc5+VgOaoUyy1JYGMc=";
+      "nostr-types-0.7.0-unstable" = "sha256-RfeqVRnE2a2urFvqMp9HDWboHpYYiUgn/QCRj8GVvko=";
       "qrcode-0.12.0" = "sha256-onnoQuMf7faDa9wTGWo688xWbmujgE9RraBialyhyPw=";
       "sdl2-0.35.2" = "sha256-qPId64Y6UkVkZJ+8xK645at5fv3mFcYaiInb0rGUfL4=";
+      "speedy-0.8.6" = "sha256-ltJQud1kEYkw7L2sZgPnD/teeXl2+FKgyX9kk2IC2Xg=";
     };
   };
 
