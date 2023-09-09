0d9479d298f4eaad7d5d13eb82f554316f63b686
Anderson Torres <torres.anderson.85@protonmail.com>
2023-09-09 20:42:10 -0300

    windowmaker: rewrite

--- pkgs/applications/window-managers/windowmaker/default.nix
+++ pkgs/applications/window-managers/windowmaker/default.nix
@@ -1,43 +1,76 @@
-{ lib, stdenv, fetchurl, pkg-config
-, libX11, libXext, libXft, libXmu, libXinerama, libXrandr, libXpm
-, imagemagick, libpng, libjpeg, libexif, libtiff, giflib, libwebp }:
+{ lib
+, stdenv
+, fetchurl
+, pkg-config
+, libX11
+, libXext
+, libXft
+, libXmu
+, libXinerama
+, libXrandr
+, libXpm
+, imagemagick
+, libpng
+, libjpeg
+, libexif
+, libtiff
+, giflib
+, libwebp
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "windowmaker";
   version = "0.95.9";
-  srcName = "WindowMaker-${version}";
 
   src = fetchurl {
-    url = "http://windowmaker.org/pub/source/release/${srcName}.tar.gz";
-    sha256 = "055pqvlkhipyjn7m6bb3fs4zz9rd1ynzl0mmwbhp05ihc3zmh8zj";
+    url = "http://windowmaker.org/pub/source/release/WindowMaker-${finalAttrs.version}.tar.gz";
+    hash = "sha256-8iNY/2AwFnDh4rUC+q0PLaf/iXZjLVOPlf5GOOnGtxQ=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+  ];
 
-  buildInputs = [ libX11 libXext libXft libXmu libXinerama libXrandr libXpm
-                  imagemagick libpng libjpeg libexif libtiff giflib libwebp ];
+  buildInputs = [
+    giflib
+    imagemagick
+    libX11
+    libXext
+    libXft
+    libXinerama
+    libXmu
+    libXpm
+    libXrandr
+    libexif
+    libjpeg
+    libpng
+    libtiff
+    libwebp
+  ];
 
   configureFlags = [
-    "--with-x"
+    "--disable-magick" # Many distros reported imagemagick fails to be found
     "--enable-modelock"
     "--enable-randr"
     "--enable-webp"
-    "--disable-magick" # Many distros reported imagemagick fails to be found
+    "--with-x"
   ];
 
-  meta = with lib; {
+  meta = {
     homepage = "http://windowmaker.org/";
     description = "NeXTSTEP-like window manager";
     longDescription = ''
-      Window Maker is an X11 window manager originally designed to
-      provide integration support for the GNUstep Desktop
-      Environment. In every way possible, it reproduces the elegant look
-      and feel of the NEXTSTEP user interface. It is fast, feature rich,
-      easy to configure, and easy to use. It is also free software, with
-      contributions being made by programmers from around the world.
+      Window Maker is an X11 window manager originally designed to provide
+      integration support for the GNUstep Desktop Environment. In every way
+      possible, it reproduces the elegant look and feel of the NEXTSTEP user
+      interface. It is fast, feature rich, easy to configure, and easy to
+      use. It is also free software, with contributions being made by
+      programmers from around the world.
     '';
-    license = licenses.gpl2Plus;
-    platforms = platforms.linux;
-    maintainers = [ maintainers.AndersonTorres ];
+    changelog = "https://www.windowmaker.org/news/";
+    license = lib.licenses.gpl2Plus;
+    mainProgram = "wmaker";
+    maintainers = [ lib.maintainers.AndersonTorres ];
+    platforms = lib.platforms.linux;
   };
-}
+})
