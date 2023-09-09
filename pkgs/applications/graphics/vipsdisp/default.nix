40c3e0a4b33a9cf2929770844fa9a7c7a30986e0
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-09 22:52:23 +0800

    vipsdisp: 2.5.1 -> 2.6.0

--- pkgs/applications/graphics/vipsdisp/default.nix
+++ pkgs/applications/graphics/vipsdisp/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "vipsdisp";
-  version = "2.5.1";
+  version = "2.6.0";
 
   src = fetchFromGitHub {
     owner = "jcupitt";
     repo = "vipsdisp";
     rev = "v${version}";
-    hash = "sha256-hx7daXVarV4JdxZfwnTHsuxxijCRP17gkOjicI3EFlM=";
+    hash = "sha256-iLjS3vBhdPPQNtIaM++xKekYTsr1X9f6ED2A7DYV7Lc=";
   };
 
   postPatch = ''
