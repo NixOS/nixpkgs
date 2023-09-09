f831afe48c4e527938e8202b1ea86e1f691a7c95
Georg Haas <hax404foogit@hax404.de>
2023-09-09 19:46:33 +0200

    nixos/teeworlds: migrate to rfc42 settings

--- nixos/modules/services/games/teeworlds.nix
+++ nixos/modules/services/games/teeworlds.nix
@@ -4,93 +4,119 @@ with lib;
 
 let
   cfg = config.services.teeworlds;
-  register = cfg.register;
-
-  teeworldsConf = pkgs.writeText "teeworlds.cfg" ''
-    sv_port ${toString cfg.port}
-    sv_register ${if cfg.register then "1" else "0"}
-    ${optionalString (cfg.name != null) "sv_name ${cfg.name}"}
-    ${optionalString (cfg.motd != null) "sv_motd ${cfg.motd}"}
-    ${optionalString (cfg.password != null) "password ${cfg.password}"}
-    ${optionalString (cfg.rconPassword != null) "sv_rcon_password ${cfg.rconPassword}"}
-    ${concatStringsSep "\n" cfg.extraOptions}
-  '';
-
+  configFile = pkgs.writeText "teeworlds.conf" (concatStringsSep "\n" (mapAttrsToList (key: value: "${key} ${toString value}") cfg.settings));
 in
 {
-  options = {
-    services.teeworlds = {
-      enable = mkEnableOption (lib.mdDoc "Teeworlds Server");
+  imports = [
+    (mkRenamedOptionModule
+      [ "services" "teeworlds" "name" ]
+      [ "services" "teeworlds" "settings" "sv_name" ])
+    (mkRenamedOptionModule
+      [ "services" "teeworlds" "register" ]
+      [ "services" "teeworlds" "settings" "sv_register" ])
+    (mkRenamedOptionModule
+      [ "services" "teeworlds" "motd" ]
+      [ "services" "teeworlds" "settings" "sv_motd" ])
+    (mkRenamedOptionModule
+      [ "services" "teeworlds" "password" ]
+      [ "services" "teeworlds" "settings" "password" ])
+    (mkRenamedOptionModule
+      [ "services" "teeworlds" "rcon_password" ]
+      [ "services" "teeworlds" "settings" "sv_rcon_password" ])
+    (mkRenamedOptionModule
+      [ "services" "teeworlds" "port" ]
+      [ "services" "teeworlds" "settings" "sv_port" ])
+    (mkRemovedOptionModule [ "services" "teeworlds" "extraOptions" ]
+      "Use services.teeworlds.settings instead.")
+  ];
 
-      openPorts = mkOption {
-        type = types.bool;
-        default = false;
-        description = lib.mdDoc "Whether to open firewall ports for Teeworlds";
-      };
+  options.services.teeworlds = {
+    enable = mkEnableOption (lib.mdDoc "Teeworlds Server");
 
-      name = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = lib.mdDoc ''
-          Name of the server. Defaults to 'unnamed server'.
-        '';
-      };
+    openPorts = mkOption {
+      type = types.bool;
+      default = false;
+      description = lib.mdDoc ''
+        Whether to open firewall ports for Teeworlds
+      '';
+    };
 
-      register = mkOption {
-        type = types.bool;
-        example = true;
-        default = false;
-        description = lib.mdDoc ''
-          Whether the server registers as public server in the global server list. This is disabled by default because of privacy.
-        '';
-      };
+    settings = mkOption {
+      default = {};
+      example = ''
+        {
+          sv_motd = "Welcome to this teeworlds ctf server!";
+          sv_map = "ctf1";
+          sv_gametype = "ctf";
+        }
+      '';
+      description = lib.mdDoc ''
+        See [Teeworlds Documentation](https://www.teeworlds.com/?page=docs&wiki=server_settings) for all configuration options.
+      '';
+      type = lib.types.submodule {
+        freeformType = with lib.types; attrsOf (nullOr (oneOf [ str int bool ]));
 
-      motd = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = lib.mdDoc ''
-          Set the server message of the day text.
-        '';
-      };
+        options = {
+          sv_name = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = lib.mdDoc ''
+              Name of the server. Defaults to 'unnamed server'.
+            '';
+          };
 
-      password = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = lib.mdDoc ''
-          Password to connect to the server.
-        '';
-      };
+          sv_register = mkOption {
+            type = types.int;
+            example = 1;
+            default = 0;
+            description = lib.mdDoc ''
+              Whether the server registers as public server in the global server list. This is disabled (0) by default because of privacy.
+            '';
+          };
 
-      rconPassword = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = lib.mdDoc ''
-          Password to access the remote console. If not set, a randomly generated one is displayed in the server log.
-        '';
-      };
+          sv_motd = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = lib.mdDoc ''
+              Set the server message of the day text.
+            '';
+          };
 
-      port = mkOption {
-        type = types.port;
-        default = 8303;
-        description = lib.mdDoc ''
-          Port the server will listen on.
-        '';
-      };
+          password = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = lib.mdDoc ''
+              Password to connect to the server.
+            '';
+          };
 
-      extraOptions = mkOption {
-        type = types.listOf types.str;
-        default = [];
-        description = lib.mdDoc ''
-          Extra configuration lines for the {file}`teeworlds.cfg`. See [Teeworlds Documentation](https://www.teeworlds.com/?page=docs&wiki=server_settings).
-        '';
-        example = [ "sv_map dm1" "sv_gametype dm" ];
+          sv_rcon_password = mkOption {
+            type = types.nullOr types.str;
+            default = null;
+            description = lib.mdDoc ''
+              Password to access the remote console. If not set, a randomly generated one is displayed in the server log.
+            '';
+          };
+
+          sv_port = mkOption {
+            type = types.port;
+            default = 8303;
+            description = lib.mdDoc ''
+              Port the server will listen on.
+            '';
+          };
+        };
       };
+      apply = value:
+        if isBool value
+        then (if value then 1 else 0)
+        else value;
     };
   };
 
   config = mkIf cfg.enable {
     networking.firewall = mkIf cfg.openPorts {
-      allowedUDPPorts = [ cfg.port ];
+      allowedUDPPorts = [ cfg.settings.sv_port ];
     };
 
     systemd.services.teeworlds = {
@@ -100,7 +126,7 @@ in
 
       serviceConfig = {
         DynamicUser = true;
-        ExecStart = "${pkgs.teeworlds}/bin/teeworlds_srv -f ${teeworldsConf}";
+        ExecStart = "${pkgs.teeworlds}/bin/teeworlds_srv -f ${configFile}";
 
         # Hardening
         CapabilityBoundingSet = false;
