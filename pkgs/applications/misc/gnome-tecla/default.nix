b17643a71295781295c37d695497bb20db2653bf
Bobby Rong <rjl931189261@126.com>
2023-09-09 14:12:09 +0000

    gnome-tecla: 45.alpha → 45.rc
    https://gitlab.gnome.org/GNOME/tecla/-/compare/45.alpha...45.rc
    
    As said in NEWS: "Build fixes for NixOS"
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/applications/misc/gnome-tecla/default.nix
+++ pkgs/applications/misc/gnome-tecla/default.nix
@@ -15,17 +15,12 @@
 
 stdenv.mkDerivation rec {
   pname = "tecla";
-  version = "45.alpha";
+  version = "45.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    hash = "sha256-dl96ZD5aHaqd7ywMg988ZRrkveBVM9ZO4VWeuzxOjr0=";
+    hash = "sha256-CoXf7zXZ2mwuGAE/ow4NkBTyu+2qt1PUU6LgRzeQvJU=";
   };
-
-  postPatch = ''
-    # https://gitlab.gnome.org/GNOME/tecla/-/merge_requests/8
-    substituteInPlace src/tecla-util.c --replace "xdg[PATH_MAX]" "xdg[1024]"
-  '';
 
   nativeBuildInputs = [
     meson
