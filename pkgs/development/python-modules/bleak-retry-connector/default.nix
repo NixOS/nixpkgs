c9aed51ddc507385d92e3729fb42bc822099b132
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-10 13:54:12 +0200

    python311Packages.bleak-retry-connector: 3.1.2 -> 3.1.3
    Diff: https://github.com/Bluetooth-Devices/bleak-retry-connector/compare/refs/tags/v3.1.2...v3.1.3
    
    Changelog: https://github.com/bluetooth-devices/bleak-retry-connector/blob/v3.1.3/CHANGELOG.md

--- pkgs/development/python-modules/bleak-retry-connector/default.nix
+++ pkgs/development/python-modules/bleak-retry-connector/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "bleak-retry-connector";
-  version = "3.1.2";
+  version = "3.1.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-q+J4PUBm42jBcLbiywWwDZxUI0Gsl8GjnLPwniWp+Kw=";
+    hash = "sha256-Nd/9mUtEEhCiJSF677lsE5UhMrbWiIl3ktQ7FjtyYlQ=";
   };
 
   postPatch = ''
