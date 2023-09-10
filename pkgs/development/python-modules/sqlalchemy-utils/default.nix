4567d5ee3ecf0d6eecf088b35a649ca73f62429c
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-10 09:27:13 +0200

    python311Packages.sqlalchemy-utils: 0.40.0 -> 0.41.1
    Changelog: https://github.com/kvesteri/sqlalchemy-utils/releases/tag/0.41.1

--- pkgs/development/python-modules/sqlalchemy-utils/default.nix
+++ pkgs/development/python-modules/sqlalchemy-utils/default.nix
@@ -1,49 +1,44 @@
 { lib
-, buildPythonPackage
-, fetchPypi
-, pythonOlder
-
-# runtime
-, importlib-metadata
-, sqlalchemy
-
-# optionals
-, babel
 , arrow
-, pendulum
-#, intervals
-, phonenumbers
-, passlib
+, babel
+, backports-zoneinfo
+, buildPythonPackage
 , colour
-, python-dateutil
-, furl
 , cryptography
-
-# tests
-, pytestCheckHook
-, pygments
-, jinja2
 , docutils
+, fetchPypi
 , flexmock
+, furl
+, importlib-metadata
+#, intervals
+, jinja2
+, passlib
+, pendulum
+, pg8000
+, phonenumbers
 , psycopg2
 , psycopg2cffi
-, pg8000
-, pytz
-, backports-zoneinfo
+, pygments
 , pymysql
 , pyodbc
-
+, pytestCheckHook
+, python-dateutil
+, pythonOlder
+, pytz
+, sqlalchemy
 }:
 
 buildPythonPackage rec {
   pname = "sqlalchemy-utils";
-  version = "0.40.0";
+  version = "0.41.1";
   format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit version;
     pname = "SQLAlchemy-Utils";
-    hash = "sha256-r4AwiaeSmAP662FzuQ8p0aZ60C8dHnMvQLBUqOs8c3A=";
+    hash = "sha256-ohgb/wHuuER544Vx0sBxjrUgQvmv2MGU0NAod+hLfXQ=";
   };
 
   patches = [
@@ -57,47 +52,69 @@ buildPythonPackage rec {
   ];
 
   passthru.optional-dependencies = {
-    babel = [ babel ];
-    arrow = [ arrow ];
-    pendulum = [ pendulum ];
+    babel = [
+      babel
+    ];
+    arrow = [
+      arrow
+    ];
+    pendulum = [
+      pendulum
+    ];
     #intervals = [ intervals ];
-    phone = [ phonenumbers ];
-    password = [ passlib ];
-    color = [ colour ];
-    timezone = [ python-dateutil ];
-    url = [ furl ];
-    encrypted = [ cryptography ];
+    phone = [
+      phonenumbers
+    ];
+    password = [
+      passlib
+    ];
+    color = [
+      colour
+    ];
+    timezone = [
+      python-dateutil
+    ];
+    url = [
+      furl
+    ];
+    encrypted = [
+      cryptography
+    ];
   };
 
   nativeCheckInputs = [
-    pytestCheckHook
-    pygments
-    jinja2
     docutils
     flexmock
+    jinja2
+    pg8000
     psycopg2
     psycopg2cffi
-    pg8000
-    pytz
-    python-dateutil
+    pygments
     pymysql
     pyodbc
+    pytestCheckHook
+    pytz
   ]
   ++ lib.flatten (builtins.attrValues passthru.optional-dependencies)
   ++ lib.optionals (pythonOlder "3.9") [
     backports-zoneinfo
   ];
 
-  pytestFlagsArray = [
-    "--deselect tests/functions/test_database.py::TestDatabasePostgresCreateDatabaseCloseConnection::test_create_database_twice"
-    "--deselect tests/functions/test_database.py::TestDatabasePostgresPg8000::test_create_and_drop"
-    "--deselect tests/functions/test_database.py::TestDatabasePostgresPsycoPG2CFFI::test_create_and_drop"
+  disabledTests = [
+    # Tests are failing
+    "test_create_database_twice"
+    "test_create_and_drop"
+    "test_create_and_drop"
+  ];
+
+  pythonImportsCheck = [
+    "sqlalchemy_utils"
   ];
 
   meta = with lib; {
-    changelog = "https://github.com/kvesteri/sqlalchemy-utils/releases/tag/${version}";
-    homepage = "https://github.com/kvesteri/sqlalchemy-utils";
     description = "Various utility functions and datatypes for SQLAlchemy";
+    homepage = "https://github.com/kvesteri/sqlalchemy-utils";
+    changelog = "https://github.com/kvesteri/sqlalchemy-utils/releases/tag/${version}";
     license = licenses.bsd3;
     maintainers = with maintainers; [ eadwu ];
   };
