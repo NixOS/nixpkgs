fd28b8c59265a8af95157f7ea6d36b0dc3d7260a
Bobby Rong <rjl931189261@126.com>
2023-09-10 16:02:14 +0000

    gnome.gnome-shell: 45.beta.1 → 45.rc
    https://gitlab.gnome.org/GNOME/gnome-shell/-/compare/45.beta.1...45.rc
    
    Started using mtk.
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-shell/default.nix
+++ pkgs/desktops/gnome/core/gnome-shell/default.nix
@@ -67,13 +67,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "gnome-shell";
-  version = "45.beta.1";
+  version = "45.rc";
 
   outputs = [ "out" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-shell/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "sf/3O3k4UJIpL7jpsH5xdDRuW7XTqV9cgFn/y1UGJpU=";
+    sha256 = "EDHGanDrnNcTCvrk/dzuF2DoqED1tWLv7D7D+oE66zo=";
   };
 
   patches = [
