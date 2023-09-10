5a46feaabcc21693fdbba545b377e99cc389cebd
Ryan Burns <rtburns@protonmail.com>
2023-09-09 17:31:09 -0700

    libbutl: 0.15.0 -> 0.16.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18898,6 +18898,7 @@ with pkgs;
   # Dependency of build2, must also break cycle for this
   libbutl = callPackage ../development/libraries/libbutl {
     build2 = build2.bootstrap;
+    inherit (darwin) DarwinTools;
   };
 
   libbpkg = callPackage ../development/libraries/libbpkg { };
