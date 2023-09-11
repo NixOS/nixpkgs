2f5a505cabb19d34e63620293665ff721b25fd5c
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-11 18:07:09 +0000

    libcef: 116.0.20 -> 116.0.21

--- pkgs/development/libraries/libcef/default.nix
+++ pkgs/development/libraries/libcef/default.nix
@@ -66,16 +66,16 @@ let
       projectArch = "x86_64";
     };
   };
-  platforms."aarch64-linux".sha256 = "0xzgcnh45x3sqg0jndp9g08zy9bvzvmvfz8imj12j55vkm6f7kl6";
-  platforms."x86_64-linux".sha256 = "1942mwlyrz5pxlx9kcnz85rqbz8q8slkivx0001z30l7a6pizgg5";
+  platforms."aarch64-linux".sha256 = "1d3ign2bhv1821k0jgmakzgqlpwy358iggrgxnbxswa42ckk9m6d";
+  platforms."x86_64-linux".sha256 = "188hd7b11963f23y9rb0n747ssffdc80cdr1hpgwn55cmwhd8gbj";
 
   platformInfo = builtins.getAttr stdenv.targetPlatform.system platforms;
 in
 stdenv.mkDerivation rec {
   pname = "cef-binary";
-  version = "116.0.20";
-  gitRevision = "d6abd3c";
-  chromiumVersion = "116.0.5845.180";
+  version = "116.0.21";
+  gitRevision = "9c7dc32";
+  chromiumVersion = "116.0.5845.181";
 
   src = fetchurl {
     url = "https://cef-builds.spotifycdn.com/cef_binary_${version}+g${gitRevision}+chromium-${chromiumVersion}_${platformInfo.platformStr}_minimal.tar.bz2";
