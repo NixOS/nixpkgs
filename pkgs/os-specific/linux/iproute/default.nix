NixOS/nixpkgs

commit 620f9f11f83de7fa68b346ff80adfc9914c50ce6

iproute2: Dont hardcode gcc
Fix linux clang stdenv

`nix build --impure --expr 'with import ./. { config = { replaceStdenv = ({ pkgs }: pkgs.clangStdenv); }; }; iproute2'`

pkgs/os-specific/linux/iproute/default.nix
@@ -27,9 +27,12 @@ stdenv.mkDerivation rec {
     })
   ];
 
-  preConfigure = ''
+  postPatch = ''
     # Don't try to create /var/lib/arpd:
     sed -e '/ARPDDIR/d' -i Makefile
+
+    substituteInPlace Makefile \
+      --replace "CC := gcc" "CC ?= $CC"
   '';
 
   outputs = [ "out" "dev" ];

commit e270be8f44a36a24be946107479c8b2f1bfcc082

iproute2: fix cross regression from 620f9f11f83de7fa68b346ff80adfc9914c50ce6

pkgs/os-specific/linux/iproute/default.nix
@@ -46,6 +46,8 @@ stdenv.mkDerivation rec {
     "SHARED_LIBS=n"
     # all build .so plugins:
     "TC_CONFIG_NO_XT=y"
+  ] ++ lib.optionals (stdenv.buildPlatform != stdenv.hostPlatform) [
+    "HOSTCC=$(CC_FOR_BUILD)"
   ];
 
   buildFlags = [