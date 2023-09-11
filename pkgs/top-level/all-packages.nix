24d00917fb54cda381bb705e5cf5da93e169ee0f
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-09-11 22:29:28 +0200

    mathmod: init at 11.1-unstable-2024-01-26

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38929,6 +38929,8 @@ with pkgs;
     version = "11";
   };
 
+  mathmod = libsForQt5.callPackage ../applications/science/math/mathmod { };
+
   metis = callPackage ../development/libraries/science/math/metis { };
 
   nauty = callPackage ../applications/science/math/nauty { };
