e9baff99a333b60f4fa6908aa92eb4f18f66401a
Stefan Schroeder <ondekoza@gmail.com>
2023-09-11 22:36:05 +0200

    Update pkgs/tools/misc/ttfsample/default.nix
    Co-authored-by: Alexander Groleau <alex@proof.construction>

--- pkgs/tools/misc/ttfsample/default.nix
+++ pkgs/tools/misc/ttfsample/default.nix
@@ -3,22 +3,23 @@
 , fetchFromGitHub
 }:
 buildGoModule rec {
-        pname = "ttfsample";
-        version = "0.3.0";
+  pname = "ttfsample";
+  version = "0.3.0";
 
-        src = fetchFromGitHub {
-                owner = "StefanSchroeder";
-                repo = "ttfsample";
-                rev = "${version}";
-                hash = "sha256-qB6n0xaBvjmBOOId4SLjc5Oi59DbtvLdSUw98AZcn0o=";
+  src = fetchFromGitHub {
+    owner = "StefanSchroeder";
+    repo = "ttfsample";
+    rev = "${version}";
+    hash = "sha256-qB6n0xaBvjmBOOId4SLjc5Oi59DbtvLdSUw98AZcn0o=";
+  };
 
-        };
-        vendorSha256 = "sha256-7kse3MYIj3Q0WL/fgR6TFmMGNQUSKuJM7+IqB83rwFY=";
-        meta = with lib; {
-                description = "Create images of fonts";
-                homepage = "https://github.com/StefanSchroeder/ttfsample";
-                license = licenses.mit;
-                maintainers = with maintainers; [ stefanschroeder ];
-                mainProgram = "ttfsample";
-        };
+  vendorSha256 = "sha256-7kse3MYIj3Q0WL/fgR6TFmMGNQUSKuJM7+IqB83rwFY=";
+
+  meta = with lib; {
+    description = "Create images of fonts";
+    homepage = "https://github.com/StefanSchroeder/ttfsample";
+    license = licenses.mit;
+    maintainers = with maintainers; [ stefanschroeder ];
+    mainProgram = "ttfsample";
+  };
 }
