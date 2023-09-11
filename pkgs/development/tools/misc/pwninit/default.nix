f41869d786df6cc74d763c3dcd97ae53d3585fda
SuperKenVery <39673849+SuperKenVery@users.noreply.github.com>
2023-09-11 17:43:04 +0800

    pwninit: Add elfutil as buildInput
    The original file is missing this dependecy.

--- pkgs/development/tools/misc/pwninit/default.nix
+++ pkgs/development/tools/misc/pwninit/default.nix
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-Tskbwavr+MFa8wmwaFGe7o4/6ZpZqczzwOnqFR66mmM=";
   };
 
-  buildInputs = [ openssl xz ] ++ lib.optionals stdenv.isDarwin [ Security ];
+  buildInputs = [ openssl xz elfutils ] ++ lib.optionals stdenv.isDarwin [ Security ];
   nativeBuildInputs = [ pkg-config ];
   doCheck = false; # there are no tests to run
 
