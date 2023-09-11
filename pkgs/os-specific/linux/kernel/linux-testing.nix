a5dc55fac937c10ffbbb56474569914ca9eea0a4
Kiskae <Kiskae@users.noreply.github.com>
2023-09-11 12:25:02 +0200

    linux_testing: 6.5-rc7 -> 6.6-rc1
    rc1: https://lwn.net/Articles/944122/

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.5-rc7";
+  version = "6.6-rc1";
   extraMeta.branch = lib.versions.majorMinor version;
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchzip {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    hash = "sha256-5rIcyXN54o9P+RxHZkI/BTO2Ox6uJ0Fi9NVcrN1HczQ=";
+    hash = "sha256-DRai7HhWVtRB0GiRCvCv2JM2TFKRsZ60ohD6GW0b8As=";
   };
 
   # Should the testing kernels ever be built on Hydra?
