be2e62a36d5e5d40e8f836e0ff04f8f10d070998
Aidan Gauland <aidalgol@fastmail.net>
2023-09-12 10:43:05 +1200

    nushellPlugins: normalise formatting across similar derivations

--- pkgs/shells/nushell/plugins/gstat.nix
+++ pkgs/shells/nushell/plugins/gstat.nix
@@ -12,13 +12,17 @@ rustPlatform.buildRustPackage rec {
   pname = "nushell_plugin_gstat";
   inherit (nushell) version src;
   cargoHash = "sha256-6luY3SIRRd9vaY9KIJcj8Q974FW0LtAvRjVpdpzkdLo=";
+
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ Security ];
   cargoBuildFlags = [ "--package nu_plugin_gstat" ];
+
   checkPhase = ''
     cargo test --manifest-path crates/nu_plugin_gstat/Cargo.toml
   '';
+
   passthru.updateScript = nix-update-script { };
+
   meta = with lib; {
     description = "A git status plugin for Nushell";
     homepage = "https://github.com/nushell/nushell/tree/${version}/crates/nu_plugin_gstat";
