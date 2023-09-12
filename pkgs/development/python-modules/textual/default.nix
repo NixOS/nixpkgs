476a4158be2a1ec7eb215fe561d9f01598b47c3e
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-12 02:44:55 +0000

    python310Packages.textual: 0.35.1 -> 0.36.0

--- pkgs/development/python-modules/textual/default.nix
+++ pkgs/development/python-modules/textual/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "textual";
-  version = "0.35.1";
+  version = "0.36.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     owner = "Textualize";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-WOYS1bovS6OGmFnJaxvEpqM3jRSzQg1M0vQGv1yfcnw=";
+    hash = "sha256-GH5GhXHA/6r3UNeM4YW+khyh1HnyUQBFcSNFaJwFz9c=";
   };
 
   nativeBuildInputs = [
