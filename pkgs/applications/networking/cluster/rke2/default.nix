1b2ce448169fb48373aff61151d6f0b823a98ff1
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-12 09:10:59 +0000

    rke2: 1.27.3+rke2r1 -> 1.27.5+rke2r1

--- pkgs/applications/networking/cluster/rke2/default.nix
+++ pkgs/applications/networking/cluster/rke2/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "rke2";
-  version = "1.27.3+rke2r1";
+  version = "1.27.5+rke2r1";
 
   src = fetchFromGitHub {
     owner = "rancher";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-M/3F97iNeXdMMhs0eoPODeBC6Jp+yo/PwlPiG28SfYU=";
+    hash = "sha256-LKVz/oKt3WDf84KEEj4dRyjkRWZIWbOnEgG03EHvfGQ=";
   };
 
-  vendorHash = "sha256-7Za8PQr22kvZBvoYRVbI4bXUvGWkfILQC+kAmw9ZCro=";
+  vendorHash = "sha256-Ck3/sMvCLoXKtOIhn0uE8hHdTlPFjIT04l3zoZQNKPs=";
 
   postPatch = ''
     # Patch the build scripts so they work in the Nix build environment.
