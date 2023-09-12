437f6fd4c3776d685f8e88d4af9377d89716c308
figsoda <figsoda@pm.me>
2023-09-12 08:39:08 -0400

    gitnr: 0.1.0 -> 0.1.1
    Diff: https://github.com/reemus-dev/gitnr/compare/v0.1.0...v0.1.1
    
    Changelog: https://github.com/reemus-dev/gitnr/blob/v0.1.1/CHANGELOG.md

--- pkgs/applications/version-management/gitnr/default.nix
+++ pkgs/applications/version-management/gitnr/default.nix
@@ -11,16 +11,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gitnr";
-  version = "0.1.0";
+  version = "0.1.1";
 
   src = fetchFromGitHub {
     owner = "reemus-dev";
     repo = "gitnr";
     rev = "v${version}";
-    hash = "sha256-5HZT/e53e2dUMFnT+4a5GJk3JqJu5+62yxrsnNfSqD8=";
+    hash = "sha256-0LuQqDNyMd98cHCG3JDyRx/2hhjNlcGQ7n61Z264WzA=";
   };
 
-  cargoHash = "sha256-rO8qHa+GI76s5CN52aMa58W8ERuTWQtQ96jpLbXKzOs=";
+  cargoHash = "sha256-H9aLOHdd2UP2YH/dptTwE0bzfFAJk7jwp3ecd0w8bjY=";
 
   nativeBuildInputs = [
     pkg-config
