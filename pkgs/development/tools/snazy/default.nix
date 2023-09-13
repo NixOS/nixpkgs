88d3a5724278804b2698b344fd9efbc8e5f354a5
figsoda <figsoda@pm.me>
2023-09-13 09:09:05 -0400

    snazy: 0.51.3 -> 0.52.0
    Diff: https://github.com/chmouel/snazy/compare/0.51.3...0.52.0
    
    Changelog: https://github.com/chmouel/snazy/releases/tag/0.52.0

--- pkgs/development/tools/snazy/default.nix
+++ pkgs/development/tools/snazy/default.nix
@@ -1,32 +1,21 @@
 { lib
 , rustPlatform
 , fetchFromGitHub
-, fetchpatch
 , installShellFiles
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "snazy";
-  version = "0.51.3";
+  version = "0.52.0";
 
   src = fetchFromGitHub {
     owner = "chmouel";
     repo = pname;
     rev = version;
-    hash = "sha256-YE11ypzOhRNjoi+X9Khp6qxqhD1f/hslr1t2cEeUTbs=";
+    hash = "sha256-ax16BO2I+LgaVilqrsVToulBRcyr0C/QUtrdn0nUfBU=";
   };
 
-  cargoHash = "sha256-8oT9tdGeU/1mtgf470Ps4EwQmWxPhxAzmA8D30UG60o=";
-
-  cargoPatches = [
-    # update Cargo.toml to fix the version
-    # https://github.com/chmouel/snazy/pull/217
-    (fetchpatch {
-      name = "update-version-in-cargo-toml.patch";
-      url = "https://github.com/chmouel/snazy/commit/199f560e12d07c07c240bc91e7f929831af2cc4d.patch";
-      hash = "sha256-X1oi4Mf1m/k/HYYJvqIrN14JJSEPUmWJt9PhzLiyYUs=";
-    })
-  ];
+  cargoHash = "sha256-go6y/NH3vFb8xtAGVpgy0GQfMfzXfn8hI+tJnUdCFAU=";
 
   nativeBuildInputs = [ installShellFiles ];
 
