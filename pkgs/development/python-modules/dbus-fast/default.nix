8a17ac4a03bb9b0c42d2a63b57db726f87fa761f
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-13 23:09:38 +0200

    python311Packages.dbus-fast: 2.6.0 -> 2.7.0
    Diff: https://github.com/Bluetooth-Devices/dbus-fast/compare/refs/tags/v2.6.0...2.7.0
    
    Changelog: https://github.com/Bluetooth-Devices/dbus-fast/releases/tag/v2.7.0

--- pkgs/development/python-modules/dbus-fast/default.nix
+++ pkgs/development/python-modules/dbus-fast/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "dbus-fast";
-  version = "2.6.0";
+  version = "2.7.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-zOrPFLrwW9kWE/IypFU1r/DK2qZBEezT7E7BgsXoZqI=";
+    hash = "sha256-o75N/msocSYBe3tTLYGJbqMnbiQb/t3nfJIDDr6kPxM=";
   };
 
   # The project can build both an optimized cython version and an unoptimized
