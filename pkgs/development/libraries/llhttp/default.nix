8dac2c361f6060656d1a47e453ad1257a05e680d
Mario Rodas <marsam@users.noreply.github.com>
2023-09-13 04:20:00 +0000

    llhttp: use finalAttrs pattern

--- pkgs/development/libraries/llhttp/default.nix
+++ pkgs/development/libraries/llhttp/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, cmake }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "llhttp";
   version = "9.1.0";
 
   src = fetchFromGitHub {
     owner = "nodejs";
     repo = "llhttp";
-    rev = "release/v${version}";
+    rev = "release/v${finalAttrs.version}";
     hash = "sha256-DWRo9mVpmty/Ec+pKqPTZqwOlYJD+SmddwEui7P/694=";
   };
 
@@ -22,9 +22,9 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Port of http_parser to llparse";
     homepage = "https://llhttp.org/";
-    changelog = "https://github.com/nodejs/llhttp/releases/tag/${src.rev}";
+    changelog = "https://github.com/nodejs/llhttp/releases/tag/release/v${finalAttrs.version}";
     license = licenses.mit;
     maintainers = [ maintainers.marsam ];
     platforms = platforms.all;
   };
-}
+})
