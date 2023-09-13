9b07b4d2d050b9c4e2f2b88c6b731d9d8cafd3db
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-13 00:04:16 +0000

    nexttrace: 1.1.7-1 -> 1.2.1.1

--- pkgs/tools/networking/nexttrace/default.nix
+++ pkgs/tools/networking/nexttrace/default.nix
@@ -2,15 +2,15 @@
 
 buildGoModule rec {
   pname = "nexttrace";
-  version = "1.1.7-1";
+  version = "1.2.1.1";
 
   src = fetchFromGitHub {
     owner = "sjlleo";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-ZMbX37gi9aGamDtoTdfUMiCPieP4DhjBSE5CIJLK6Z0=";
+    sha256 = "sha256-B3NHY4wYCa3lR3XPqhjcrgf9iA4Bz/8bKrwxdNSa1Bk=";
   };
-  vendorHash = "sha256-u5EIzYWr81tmMmImoRH0wT7aD3/0tx+W3CXeymWVACM=";
+  vendorHash = "sha256-8etZelvdUmHNWC0FnSX9oO3reuhB7xIzd/KxPTt6Szc=";
 
   doCheck = false; # Tests require a network connection.
 
