fe16955be79c29a643773fe115c09c88d2924cfe
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-09-13 06:50:15 +0200

    ocamlPackages.dolmen: 0.6 → 0.9

--- pkgs/development/ocaml-modules/dolmen/loop.nix
+++ pkgs/development/ocaml-modules/dolmen/loop.nix
@@ -1,12 +1,13 @@
 { buildDunePackage, dolmen, dolmen_type
 , gen
+, pp_loc
 }:
 
 buildDunePackage {
   pname = "dolmen_loop";
   inherit (dolmen) src version;
 
-  propagatedBuildInputs = [ dolmen dolmen_type gen ];
+  propagatedBuildInputs = [ dolmen dolmen_type gen pp_loc ];
 
   meta = dolmen.meta // {
     description = "A tool library for automated deduction tools";
