tervis-unikie/nixpkgs-spectrum

nftables: add option to disable interactive
I think "CLI" is a very strange name for this, but that's what
upstream calls it.

pkgs/os-specific/linux/nftables/default.nix
@@ -1,8 +1,9 @@
 { lib, stdenv, fetchurl, pkg-config, bison, file, flex
 , asciidoc, libxslt, findXMLCatalogs, docbook_xml_dtd_45, docbook_xsl
 , libmnl, libnftnl, libpcap
-, gmp, jansson, readline
+, gmp, jansson
 , withDebugSymbols ? false
+, withCli ? true, readline
 , withPython ? false , python3
 , withXtables ? true , iptables
 }:
@@ -25,8 +26,9 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     libmnl libnftnl libpcap
-    gmp jansson readline
-  ] ++ optional withXtables iptables
+    gmp jansson
+  ] ++ optional withCli readline
+    ++ optional withXtables iptables
     ++ optional withPython python3;
 
   preConfigure = ''
@@ -35,7 +37,7 @@ stdenv.mkDerivation rec {
 
   configureFlags = [
     "--with-json"
-    "--with-cli=readline"  # TODO: maybe switch to editline
+    (lib.withFeatureAs withCli "cli" "readline")
   ] ++ optional (!withDebugSymbols) "--disable-debug"
     ++ optional (!withPython) "--disable-python"
     ++ optional withPython "--enable-python"

NixOS/nixpkgs

pkgs/os-specific/linux/nftables/default.nix
@@ -1,9 +1,10 @@
 { lib, stdenv, fetchurl, pkg-config, bison, flex
 , asciidoc, libxslt, findXMLCatalogs, docbook_xml_dtd_45, docbook_xsl
 , libmnl, libnftnl, libpcap
-, gmp, jansson, libedit
+, gmp, jansson
 , autoreconfHook
 , withDebugSymbols ? false
+, withCli ? true, libedit
 , withPython ? false, python3
 , withXtables ? true, iptables
 , nixosTests
@@ -26,8 +27,9 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     libmnl libnftnl libpcap
-    gmp jansson libedit
-  ] ++ lib.optional withXtables iptables
+    gmp jansson
+  ] ++ lib.optional withCli libedit
+    ++ lib.optional withXtables iptables
     ++ lib.optionals withPython [
       python3
       python3.pkgs.setuptools
@@ -35,7 +37,7 @@ stdenv.mkDerivation rec {
 
   configureFlags = [
     "--with-json"
-    "--with-cli=editline"
+    (lib.withFeatureAs withCli "cli" "editline")
   ] ++ lib.optional (!withDebugSymbols) "--disable-debug"
     ++ lib.optional (!withPython) "--disable-python"
     ++ lib.optional withPython "--enable-python"