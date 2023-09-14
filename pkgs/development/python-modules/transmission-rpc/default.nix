30c3bfaf306f169488e82f75b38ff80e10dbc6d8
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-14 08:38:52 +0000

    python310Packages.transmission-rpc: 6.0.0 -> 7.0.0

--- pkgs/development/python-modules/transmission-rpc/default.nix
+++ pkgs/development/python-modules/transmission-rpc/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "transmission-rpc";
-  version = "6.0.0";
+  version = "7.0.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "Trim21";
     repo = "transmission-rpc";
     rev = "refs/tags/v${version}";
-    hash = "sha256-gRyxQ6Upc1YBRhciVfyt0IGjv8K8ni4I1ODRS6o3tHA=";
+    hash = "sha256-66TKUi4rNZDMWPncyxgHY6oW62DVOQLSWO1RevHG7EY=";
   };
 
   nativeBuildInputs = [
