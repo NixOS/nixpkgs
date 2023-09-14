90b23c45d0059c30bb8e6aec87fd4437f6c6bf4d
Alyssa Ross <hi@alyssa.is>
2023-09-14 09:17:26 +0000

    pkgsMusl.connman: fix hash for patch
    The hash was changed despite the patch not changing at all.
    Switch back to the hash we had before, but in SRI format.
    
    Fixes: 774c20b68467 ("connman: 1.41 -> 1.42")

--- pkgs/tools/networking/connman/connman/default.nix
+++ pkgs/tools/networking/connman/connman/default.nix
@@ -73,7 +73,7 @@ stdenv.mkDerivation rec {
     # Fix Musl build by avoiding a Glibc-only API.
     (fetchpatch {
       url = "https://git.alpinelinux.org/aports/plain/community/connman/libresolv.patch?id=e393ea84386878cbde3cccadd36a30396e357d1e";
-      hash = "sha256-7Q1bp8rD/gGVYUqnIXqjr9vypR8jlC926p3KYWl9kLw=";
+      hash = "sha256-gzQUkUEunqTuSE78K0Ujvre/rx1KP1igQKy3e2i14s0=";
     })
   ];
 
