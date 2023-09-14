NixOS/nixpkgs

commit e50edcb50693ef73a2eef9adfd1614b0f9a82370

kde/gear: 23.04.3 -> 23.08.0

pkgs/applications/kde/neochat.nix
@@ -1,6 +1,5 @@
 { mkDerivation
 , lib
-, fetchpatch
 
 , cmake
 , extra-cmake-modules
@@ -32,14 +31,6 @@
 
 mkDerivation {
   pname = "neochat";
-
-  patches = [
-    (fetchpatch {
-      name = "libquotient-0.8.patch";
-      url = "https://invent.kde.org/network/neochat/-/commit/d9d5e17be2a2057ab2ee545561fab721cb211f7f.patch";
-      hash = "sha256-y1PEehFCW+69OH8YvL3SUGOb8Hhyf8xwRvSZzJ5J5Wc=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake

pkgs/applications/kde/akonadi-calendar.nix
@@ -2,7 +2,7 @@
   mkDerivation, lib, kdepimTeam,
   extra-cmake-modules, kdoctools,
   akonadi, akonadi-contacts, kcalendarcore, kcalutils, kcontacts,
-  kidentitymanagement, kio, kmailtransport,
+  kidentitymanagement, kio, kmailtransport, messagelib
 }:
 
 mkDerivation {
@@ -14,7 +14,7 @@ mkDerivation {
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   propagatedBuildInputs = [
     akonadi akonadi-contacts kcalendarcore kcalutils kcontacts kidentitymanagement
-    kio kmailtransport
+    kio kmailtransport messagelib
   ];
   outputs = [ "out" "dev" ];
   postInstall = ''

pkgs/applications/kde/angelfish.nix
@@ -4,6 +4,7 @@
 , cmake
 , corrosion
 , extra-cmake-modules
+, futuresql
 , kconfig
 , kcoreaddons
 , kdbusaddons
@@ -13,6 +14,7 @@
 , knotifications
 , kpurpose
 , kwindowsystem
+, qcoro
 , qtfeedback
 , qtquickcontrols2
 , qqc2-desktop-style
@@ -22,7 +24,7 @@
 , srcs
 
 # provided as callPackage input to enable easier overrides through overlays
-, cargoSha256 ? "sha256-Wthw7foadXO6jYJO1TB4OOYtpwnp8iCdda4tdiYg41A="
+, cargoSha256 ? "sha256-FI94TU3MgIl1tcjwJnzb2PKO1rbZ3uRB1mzXXkNU95I="
 }:
 
 mkDerivation rec {
@@ -45,6 +47,7 @@ mkDerivation rec {
   ];
 
   buildInputs = [
+    futuresql
     kconfig
     kcoreaddons
     kdbusaddons
@@ -54,6 +57,7 @@ mkDerivation rec {
     knotifications
     kpurpose
     kwindowsystem
+    qcoro
     qtfeedback
     qtquickcontrols2
     qqc2-desktop-style

pkgs/applications/kde/arianna.nix
@@ -1,51 +1,52 @@
 { lib
 , mkDerivation
-, fetchurl
 , cmake
 , extra-cmake-modules
-, pkg-config
-, baloo
-, kfilemetadata
-, kirigami2
-, kirigami-addons
-, kitemmodels
-, kquickcharts
-, plasma-framework
-, qqc2-desktop-style
 , qtbase
+, qtdeclarative
 , qtquickcontrols2
-, qtwebsockets
+, qtwebchannel
 , qtwebengine
+, qtwebsockets
+, baloo
+, karchive
+, kconfig
+, kcoreaddons
+, kdbusaddons
+, kfilemetadata
+, ki18n
+, kirigami-addons
+, kquickcharts
+, kwindowsystem
+, qqc2-desktop-style
 }:
 
-mkDerivation rec {
+mkDerivation {
   pname = "arianna";
-  version = "1.1.0";
-
-  src = fetchurl {
-    url = "mirror://kde/stable/arianna/arianna-${version}.tar.xz";
-    hash = "sha256-C60PujiUTyw2DwImu8PVmU687CP9CuWZ+d8LuZKthKY=";
-  };
 
   nativeBuildInputs = [
     cmake
     extra-cmake-modules
-    pkg-config
   ];
 
   buildInputs = [
-    baloo
-    kfilemetadata
-    kirigami2
-    kirigami-addons
-    kitemmodels
-    kquickcharts
-    plasma-framework
-    qqc2-desktop-style
     qtbase
+    qtdeclarative
     qtquickcontrols2
-    qtwebsockets
+    qtwebchannel
     qtwebengine
+    qtwebsockets
+    baloo
+    karchive
+    kconfig
+    kcoreaddons
+    kdbusaddons
+    kfilemetadata
+    ki18n
+    kirigami-addons
+    kquickcharts
+    kwindowsystem
+    qqc2-desktop-style
   ];
 
   meta = with lib; {

pkgs/applications/kde/audiotube.nix
@@ -4,12 +4,14 @@
 , extra-cmake-modules
 , wrapGAppsHook
 
+, futuresql
 , gst_all_1
 , kcoreaddons
 , kcrash
 , ki18n
 , kirigami2
 , kirigami-addons
+, qcoro
 , qtimageformats
 , qtmultimedia
 , qtquickcontrols2
@@ -27,11 +29,13 @@ mkDerivation rec {
   ];
 
   buildInputs = [
+    futuresql
     kcoreaddons
     kcrash
     ki18n
     kirigami2
     kirigami-addons
+    qcoro
     qtimageformats
     qtmultimedia
     qtquickcontrols2

pkgs/applications/kde/cantor.nix
@@ -93,12 +93,7 @@ mkDerivation {
 
   qtWrapperArgs = [
     "--prefix PATH : ${placeholder "out"}/bin"
-  ];
-
-  postInstall = lib.optionalString withSage ''
-    wrapProgram $out/share/cantor/sagebackend/cantor-execsage \
-      --prefix PATH : ${sage-with-env}/bin
-  '';
+  ] ++ lib.optional withSage "--prefix PATH : ${sage-with-env}/bin";
 
   meta = with lib; {
     description = "Front end to powerful mathematics and statistics packages";

pkgs/applications/kde/default.nix
@@ -73,6 +73,7 @@ let
       akonadiconsole = callPackage ./akonadiconsole.nix {};
       akregator = callPackage ./akregator.nix {};
       analitza = callPackage ./analitza.nix {};
+      arianna = callPackage ./arianna.nix {};
       ark = callPackage ./ark {};
       baloo-widgets = callPackage ./baloo-widgets.nix {};
       bomber = callPackage ./bomber.nix {};
@@ -99,7 +100,7 @@ let
       kaccounts-providers = callPackage ./kaccounts-providers.nix {};
       kaddressbook = callPackage ./kaddressbook.nix {};
       kalarm = callPackage ./kalarm.nix {};
-      kalendar = callPackage ./kalendar.nix {};
+      merkuro = callPackage ./merkuro.nix {};
       kalzium = callPackage ./kalzium.nix {};
       kamoso = callPackage ./kamoso.nix {};
       kapman = callPackage ./kapman.nix {};
@@ -134,7 +135,6 @@ let
       kdiamond = callPackage ./kdiamond.nix {};
       keditbookmarks = callPackage ./keditbookmarks.nix {};
       kfind = callPackage ./kfind.nix {};
-      kfloppy = callPackage ./kfloppy.nix {};
       kgeography = callPackage ./kgeography.nix {};
       kget = callPackage ./kget.nix {};
       kgpg = callPackage ./kgpg.nix {};

pkgs/applications/kde/fetch.sh
@@ -1,1 +1,1 @@
-WGET_ARGS=( https://download.kde.org/stable/release-service/23.04.3/src -A '*.tar.xz' )
+WGET_ARGS=( https://download.kde.org/stable/release-service/23.08.0/src -A '*.tar.xz' )

commit d1e3c1acc7b9ed2e0e1a112a1f5600cee1397283

neochat: add missing runtime dependencies
Without these, it fails to launch, e.g.:

> QQmlApplicationEngine failed to load component
> qrc:/main.qml:20:5: Type RoomList.Page unavailable
> qrc:/RoomList/Page.qml:87:20: Type ExploreComponent unavailable
> qrc:/RoomList/ExploreComponent.qml: Type org.kde.neochat/OsmLocationPlugin unavailable
> qrc:/OsmLocationPlugin.qml:7:1: module "QtLocation" is not installed

Fixes: e50edcb50693 ("kde/gear: 23.04.3 -> 23.08.0")

pkgs/applications/kde/neochat.nix
@@ -15,6 +15,7 @@
 , kirigami2
 , kitemmodels
 , knotifications
+, kquickcharts
 , kquickimageedit
 , libpulseaudio
 , libquotient
@@ -24,6 +25,7 @@
 , qqc2-desktop-style
 , qtgraphicaleffects
 , qtkeychain
+, qtlocation
 , qtmultimedia
 , qtquickcontrols2
 , sonnet
@@ -49,6 +51,7 @@ mkDerivation {
     kirigami2
     kitemmodels
     knotifications
+    kquickcharts
     kquickimageedit
     libpulseaudio
     libquotient
@@ -57,6 +60,7 @@ mkDerivation {
     qcoro
     qtgraphicaleffects
     qtkeychain
+    qtlocation
     qtmultimedia
     qtquickcontrols2
     qqc2-desktop-style