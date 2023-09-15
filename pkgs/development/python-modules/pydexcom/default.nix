b05d0adbf8d67e75729ed8c9065c057b81e5a657
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:06:46 +0200

    python3Packages.pydexcom: 0.2.3 -> 0.3.1

--- pkgs/development/python-modules/pydexcom/default.nix
+++ pkgs/development/python-modules/pydexcom/default.nix
@@ -1,19 +1,29 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, setuptools
+, setuptools-scm
 , requests
 }:
 
 buildPythonPackage rec {
   pname = "pydexcom";
-  version = "0.2.3";
+  version = "0.3.1";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "gagebenne";
     repo = pname;
-    rev = version;
-    hash = "sha256-ItDGnUUUTwCz4ZJtFVlMYjjoBPn2h8QZgLzgnV2T/Qk=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-VZ8Y8W3oEQ3W8eubMbHLfQAXK8cL6+OTyBFwtEe0cBE=";
   };
+
+  env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
+
+  nativeBuildInputs = [
+    setuptools
+    setuptools-scm
+  ];
 
   propagatedBuildInputs = [ requests ];
 
