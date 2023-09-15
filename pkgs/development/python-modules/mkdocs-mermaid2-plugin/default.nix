418d300e74d6f1727f74caf03aa4cadde1375206
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:06:30 +0200

    python3Packages.mkdocs-mermaid2-plugin: 1.0.8 -> 1.1.0

--- pkgs/development/python-modules/mkdocs-mermaid2-plugin/default.nix
+++ pkgs/development/python-modules/mkdocs-mermaid2-plugin/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "mkdocs-mermaid2-plugin";
-  version = "1.0.8";
+  version = "1.1.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "fralau";
     repo = "mkdocs-mermaid2-plugin";
     rev = "refs/tags/v${version}";
-    hash = "sha256-0h/EMfp6D14ZJcQe3U2r/RQ/VNejOK9bLP6AMNiB0Rc=";
+    hash = "sha256-9vYLkGUnL+rnmZntcgFzOvXQdf6angb9DRsmrBjnPUY=";
   };
 
   propagatedBuildInputs = [
