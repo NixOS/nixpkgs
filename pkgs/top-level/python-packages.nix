1a88442d4a62c90601a726f3b393de2901a02d9c
Raito Bezarius <masterancpp@gmail.com>
2023-09-15 22:51:53 +0200

    python3Packages.bubop: init at 0.1.12
    bubop is a set of helper libraries for Bergercookie's program ecosystem
    which we will introduce later.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1772,6 +1772,8 @@ self: super: with self; {
 
   btsocket = callPackage ../development/python-modules/btsocket { };
 
+  bubop = callPackage ../development/python-modules/bubop { };
+
   bucketstore = callPackage ../development/python-modules/bucketstore { };
 
   bugsnag = callPackage ../development/python-modules/bugsnag { };
