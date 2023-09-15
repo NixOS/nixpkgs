82fa131cfeeed1be063fa04738e55fe0dc47d493
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:06:56 +0200

    python3Packages.python-ndn: 0.3-3 -> 0.4.1

--- pkgs/development/python-modules/python-ndn/default.nix
+++ pkgs/development/python-modules/python-ndn/default.nix
@@ -13,15 +13,15 @@
 
 buildPythonPackage rec {
   pname = "python-ndn";
-  version = "0.3-3";
+  version = "0.4.1";
 
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "named-data";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-XS71oIydyLYtx6OQGO5NrhjVSyZxnhufrZ1y/6TffPo=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-ArTP4LQu7VNjI/N13gMTc1SDiNmW5l4GdLYOk8JEfKg=";
   };
 
   disabled = pythonOlder "3.9";
