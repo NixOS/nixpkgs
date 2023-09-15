28d072cb691193e47b18e3f843167beb7572ac0d
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-15 23:56:53 +0000

    alsa-lib: 1.2.9 -> 1.2.10

--- pkgs/os-specific/linux/alsa-project/alsa-lib/default.nix
+++ pkgs/os-specific/linux/alsa-project/alsa-lib/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "alsa-lib";
-  version = "1.2.9";
+  version = "1.2.10";
 
   src = fetchurl {
     url = "mirror://alsa/lib/${finalAttrs.pname}-${finalAttrs.version}.tar.bz2";
-    hash = "sha256-3JxkP9xMz9BXLMaFhY3UHgivtYPzBGCzF+QYgnX2FbI=";
+    hash = "sha256-yGpFqEYzGxsKpua+EAvip675Lv1AXPa6x+74F0uqkg4=";
   };
 
   patches = [
