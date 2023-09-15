78561bca9548cd59e28bbe0597bd8c4a1616f625
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-15 07:54:55 +0000

    python310Packages.rns: 0.5.7 -> 0.5.8

--- pkgs/development/python-modules/rns/default.nix
+++ pkgs/development/python-modules/rns/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "rns";
-  version = "0.5.7";
+  version = "0.5.8";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "markqvist";
     repo = "Reticulum";
     rev = "refs/tags/${version}";
-    hash = "sha256-0WNgJKhxK4WjYQ0n7ofqrRxf4m9uWn2ygcZiv3uhrhM=";
+    hash = "sha256-6NGEXglo3J9Buz4Qm5tOHnXWvEf/NHSp2utfHZOPWT4=";
   };
 
   propagatedBuildInputs = [
