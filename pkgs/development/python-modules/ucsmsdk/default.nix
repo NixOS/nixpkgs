0424699b657a6a207884068558437a67828d9631
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:07:20 +0200

    python3Packages.ucsmsdk: 0.9.14 -> 0.9.15

--- pkgs/development/python-modules/ucsmsdk/default.nix
+++ pkgs/development/python-modules/ucsmsdk/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "ucsmsdk";
-  version = "0.9.14";
+  version = "0.9.15";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "CiscoUcs";
     repo = "ucsmsdk";
-    rev = "v${version}";
-    hash = "sha256-lSkURvKRgW+qV1A8OT4WYsMGlxxIqaFnxQ3Rnlixdw0=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-xNRfsIfhoVI5ORWn6NmLCuMMJregIZWQ20QBiBsA1Pc=";
   };
 
   propagatedBuildInputs = [
