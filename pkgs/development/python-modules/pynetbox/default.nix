a9a00d462f7d682f0d8f45c813e1d8667a12f6d7
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-15 10:41:50 +0000

    python310Packages.pynetbox: 7.1.0 -> 7.2.0

--- pkgs/development/python-modules/pynetbox/default.nix
+++ pkgs/development/python-modules/pynetbox/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "pynetbox";
-  version = "7.1.0";
+  version = "7.2.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "netbox-community";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-E79n4E386bSxDRzxcjCIvK0Z3r78HsFjGIXqjqQ1zyE=";
+    hash = "sha256-rYqwZIqcNeSpXsICL8WGLJ3Tcnwnnm6gvRBEJ/5iE/Q=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
