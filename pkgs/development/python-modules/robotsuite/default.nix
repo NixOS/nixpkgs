297ec0ede2610b06702a46f80c4e8f5d320792bd
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:07:03 +0200

    python3Packages.robotsuite: 2.3.1 -> 2.3.2

--- pkgs/development/python-modules/robotsuite/default.nix
+++ pkgs/development/python-modules/robotsuite/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "robotsuite";
-  version = "2.3.1";
+  version = "2.3.2";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-iugVKUPl6HTTO8K1EbSqAk1fl/fsEPoOcsOnnAgcEas=";
+    hash = "sha256-sPmOoR5K+gMfyPk2QMbiDNmWPRcqKrsz6ZPBAKR/3XY=";
   };
 
   propagatedBuildInputs = [
