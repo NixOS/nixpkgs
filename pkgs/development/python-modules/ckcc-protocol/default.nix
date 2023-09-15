7850a3cfa6775489e4e4e5dc97955148e01c0234
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-15 08:09:23 +0200

    python310Packages.ckcc-protocol: add format

--- pkgs/development/python-modules/ckcc-protocol/default.nix
+++ pkgs/development/python-modules/ckcc-protocol/default.nix
@@ -11,6 +11,8 @@
 buildPythonPackage rec {
   pname = "ckcc-protocol";
   version = "1.4.0";
+  format = "setuptools";
+
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
@@ -18,16 +20,24 @@ buildPythonPackage rec {
     hash = "sha256-zZPU0+MwjqRYCqa+W0YTqCZv2WsMwa9R5xaN7ye77OU=";
   };
 
-  propagatedBuildInputs = [ click ecdsa hidapi pyaes ];
+  propagatedBuildInputs = [
+    click
+    ecdsa
+    hidapi
+    pyaes
+  ];
 
   # Project has no tests
   doCheck = false;
-  pythonImportsCheck = [ "ckcc" ];
+
+  pythonImportsCheck = [
+    "ckcc"
+  ];
 
   meta = with lib; {
     description = "Communicate with your Coldcard using Python";
     homepage = "https://github.com/Coldcard/ckcc-protocol";
     license = licenses.mit;
-    maintainers = [ maintainers.hkjn ];
+    maintainers = with maintainers; [ hkjn ];
   };
 }
