a65a59815c1e176afb9a8d46efdd06e7fe7c8e8e
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:07:04 +0200

    python3Packages.rpy2: 3.5.12 -> 3.5.14

--- pkgs/development/python-modules/rpy2/default.nix
+++ pkgs/development/python-modules/rpy2/default.nix
@@ -24,13 +24,13 @@
 }:
 
 buildPythonPackage rec {
-    version = "3.5.12";
+    version = "3.5.14";
     pname = "rpy2";
 
     disabled = isPyPy;
     src = fetchPypi {
       inherit version pname;
-      hash = "sha256-7q33lP0qpUj4hWjGodJufDgQzUp2Soeyw7MdMZQtbUU=";
+      hash = "sha256-X0auMdNuEXvjZq1K4CSTwBWsa6WevjtM1yAAdTMvxIE=";
     };
 
     patches = [
