a08d40ce2b4ecbfe9ff2c1786e7e9a2082b4a3e3
techknowlogick <techknowlogick@gitea.io>
2023-09-15 03:04:44 -0400

    boundary: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3428,8 +3428,6 @@ with pkgs;
 
   boxxy = callPackage ../tools/misc/boxxy { };
 
-  boundary = callPackage ../tools/networking/boundary { };
-
   chamber = callPackage ../tools/admin/chamber {  };
 
   chaos = callPackage ../tools/networking/chaos {  };
