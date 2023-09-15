6be0873b6d46a6df0a3e780113f25046f788f08e
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:05:58 +0200

    python3Packages.cryptolyzer: 0.8.4 -> 0.10.0

--- pkgs/development/python-modules/cryptolyzer/default.nix
+++ pkgs/development/python-modules/cryptolyzer/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "cryptolyzer";
-  version = "0.8.4";
+  version = "0.10.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "CryptoLyzer";
     inherit version;
-    hash = "sha256-7qMNz8F0B3bph/nHkS1VrkcF1bETWAklXFOlMWu3K9s=";
+    hash = "sha256-jb2yin3W+VfqFWJbQOXNml0xJEbSr1SfSSFupfxBHRs=";
   };
 
   propagatedBuildInputs = [
