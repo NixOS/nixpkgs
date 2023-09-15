276a581727e6a0d838620df03a7f03c195f36fe9
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-15 23:46:44 +0200

    python311Packages.python-engineio: 4.6.1 -> 4.7.1
    Diff: https://github.com/miguelgrinberg/python-engineio/compare/refs/tags/v4.6.1...v4.7.1
    
    Changelog: https://github.com/miguelgrinberg/python-engineio/blob/v4.7.1/CHANGES.md

--- pkgs/development/python-modules/python-engineio/default.nix
+++ pkgs/development/python-modules/python-engineio/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "python-engineio";
-  version = "4.6.1";
+  version = "4.7.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "miguelgrinberg";
     repo = "python-engineio";
     rev = "refs/tags/v${version}";
-    hash = "sha256-za2JY5Gu9MEqi3W1zxcuwYiJ5XLc43ig6Hdx/4JwDbY=";
+    hash = "sha256-jHXpPnrQlIpmQ2sY4y6AUx/6W8Pf+683s4NmmlwZO58=";
   };
 
   nativeCheckInputs = [
