a889a89d46acdf781cfd9e952d36b087c7737e62
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-16 09:20:47 +0200

    python311Packages.types-mock: add format
    - update meta

--- pkgs/development/python-modules/types-mock/default.nix
+++ pkgs/development/python-modules/types-mock/default.nix
@@ -6,16 +6,20 @@
 buildPythonPackage rec {
   pname = "types-mock";
   version = "5.1.0.2";
+  format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-3kjTdm5OMDTv2LJtqeCeSQjzHlMTel/i778fur7haZY=";
+    hash = "sha256-3kjTdm5OMDTv2LJtqeCeSQjzHlMTel/i778fur7haZY=";
   };
 
-  meta = {
-    description = "This is a PEP 561 type stub package for the mock package. It can be used by type-checking tools like mypy, pyright, pytype, PyCharm, etc. to check code that uses mock.";
+  # Module has no tests
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Type stub package for the mock package";
     homepage = "https://pypi.org/project/types-mock";
-    license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ ];
+    license = licenses.asl20;
+    maintainers = with maintainers; [ ];
   };
 }
