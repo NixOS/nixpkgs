76aee2e1f3b5944034c1bc152c9bb6f8ac8f6df4
Randy Eckenrode <randy@largeandhighquality.com>
2023-09-16 09:35:31 -0400

    darwin.LibsystemCross: define on x86_64-darwin

--- pkgs/top-level/darwin-packages.nix
+++ pkgs/top-level/darwin-packages.nix
@@ -70,6 +70,8 @@ makeScopeWithSplicing' {
         then apple_sdk.frameworks
         else appleSourcePackages
     ) Security;
+  } // lib.optionalAttrs (!useAppleSDKLibs) {
+    LibsystemCross = self.Libsystem;
   };
 in
 
