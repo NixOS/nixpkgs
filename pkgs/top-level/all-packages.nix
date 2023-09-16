39454b6db858552e4713aa486c15cdc76d901961
Anderson Torres <torres.anderson.85@protonmail.com>
2023-09-16 20:44:08 -0300

    katriawm: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9716,8 +9716,6 @@ with pkgs;
 
   katana = callPackage ../tools/security/katana { };
 
-  katriawm = callPackage ../applications/window-managers/katriawm { };
-
   kbdd = callPackage ../applications/window-managers/kbdd { };
 
   kbs2 = callPackage ../tools/security/kbs2 {
