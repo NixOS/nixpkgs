b0c2a800d67abf7c27d55b80d4f6c54e19a0b5d4
Artturin <Artturin@artturin.com>
2023-09-17 05:07:55 +0300

    tcl.tclPackageHook: inherit maintainers and platforms

--- pkgs/development/interpreters/tcl/generic.nix
+++ pkgs/development/interpreters/tcl/generic.nix
@@ -63,6 +63,9 @@ let
         tclPackageHook = callPackage ({ buildPackages }: makeSetupHook {
           name = "tcl-package-hook";
           propagatedBuildInputs = [ buildPackages.makeBinaryWrapper ];
+          meta = {
+            inherit (meta) maintainers platforms;
+          };
         } ./tcl-package-hook.sh) {};
         # verify that Tcl's clock library can access tzdata
         tests.tzdata = runCommand "${pname}-test-tzdata" {} ''
