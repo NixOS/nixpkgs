e23576ca09d2270228e3e522f426cc0d2a39b330
Eldritch Cookie <eldritch.cookie@disroot.org>
2023-09-17 18:53:26 -0300

    gcs: 4.8.0 -> 5.16.0
    from 4.x to 5.x there was a rewrite of the application to go.

--- pkgs/games/gcs/default.nix
+++ pkgs/games/gcs/default.nix
@@ -1,78 +1,71 @@
-{ lib, stdenv, fetchFromGitHub, runCommand
-, jdk8, ant
-, jre8, makeWrapper
+{ lib
+, buildGoModule
+, pkg-config
+, libX11
+, libXcursor
+, libXrandr
+, libXinerama
+, libXi
+, libXxf86vm
+, libGL
+, freetype
+, fontconfig
+, zlib
+, harfbuzz
+, openjpeg
+, jbig2dec
+, libjpeg_turbo
+, gumbo
+, mupdf
+, fetchFromGitHub
 }:
+buildGoModule rec {
+  pname = "gcs";
+  version = "5.16.0";
 
-let
-  gcs = fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "richardwilkes";
     repo = "gcs";
-    rev = "gcs-4.8.0";
-    sha256 = "0k8am8vfwls5s2z4zj1p1aqy8gapn5vbr9zy66s5g048ch8ah1hm";
+    rev = "v${version}";
+    hash = "sha256-1xiAFA28VDCYyP/4lM3yuznbd+1HUPrinf7s/vBSrD8=";
   };
-  appleStubs = fetchFromGitHub {
-    owner = "richardwilkes";
-    repo = "apple_stubs";
-    rev = "gcs-4.3.0";
-    sha256 = "0m1qw30b19s04hj7nch1mbvv5s698g5dr1d1r7r07ykvk1yh7zsa";
-  };
-  toolkit = fetchFromGitHub {
-    owner = "richardwilkes";
-    repo = "toolkit";
-    rev = "gcs-4.8.0";
-    sha256 = "1ciwwh0wxk3pzsj6rbggsbg3l2f741qy7yx1ca4v7vflsma84f1n";
-  };
-  library = fetchFromGitHub {
-    owner = "richardwilkes";
-    repo = "gcs_library";
-    rev = "gcs-4.8.0";
-    sha256 = "085jpp9mpv5kw00zds9sywmfq31mrlbrgahnwcjkx0z9i22amz4g";
-  };
-in stdenv.mkDerivation rec {
-  pname = "gcs";
-  version = "4.8.0";
+  nativeBuildInputs = [ pkg-config ];
 
-  src = runCommand "${pname}-${version}-src" { preferLocalBuild = true; } ''
-    mkdir -p $out
-    cd $out
+  buildInputs = [
+    libX11
+    libXcursor
+    libXrandr
+    libXinerama
+    libXi
+    libXxf86vm
+    libGL
+    freetype
+    fontconfig
+    zlib
+    harfbuzz
+    openjpeg.dev
+    jbig2dec
+    libjpeg_turbo
+    gumbo
+    mupdf.dev
+  ];
 
-    cp -r ${gcs} gcs
-    cp -r ${appleStubs} apple_stubs
-    cp -r ${toolkit} toolkit
-    cp -r ${library} gcs_library
+  vendorHash = "sha256-3CPYp4Dh+8s5vnMrCtL5w4lTs9maT7dns0HJqHtxm9o=";
+
+  doCheck = false;
+
+  # for some reason the preBuild runs both for the fetching derivation and the normal derivation
+  preBuild = ''
+    if [[ -e vendor/github.com/richardwilkes/pdf ]]; then
+      substituteInPlace vendor/github.com/richardwilkes/pdf/pdf.go \
+      --replace "linux LDFLAGS: -L\''${SRCDIR}/lib -lmupdf_linux_amd64 -lm" "pkg-config: mupdf zlib harfbuzz libopenjp2 jbig2dec gumbo"
+    fi
   '';
-
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ jdk8 jre8 ant ];
-  buildPhase = ''
-    cd apple_stubs
-    ant
-
-    cd ../toolkit
-    ant
-
-    cd ../gcs
-    ant
-
-    cd ..
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin $out/share/java
-
-    find gcs/libraries toolkit/libraries apple_stubs/ \( -name '*.jar' -and -not -name '*-src.jar' \) -exec cp '{}' $out/share/java ';'
-
-    makeWrapper ${jre8}/bin/java $out/bin/gcs \
-      --set GCS_LIBRARY ${library} \
-      --add-flags "-cp $out/share/java/gcs-${version}.jar com.trollworks.gcs.app.GCS"
-  '';
-
   meta = with lib; {
     description = "A stand-alone, interactive, character sheet editor for the GURPS 4th Edition roleplaying game system";
     homepage = "https://gurpscharactersheet.com/";
     sourceProvenance = with sourceTypes; [
       fromSource
-      binaryBytecode  # source bundles dependencies as jars
     ];
     license = licenses.mpl20;
     platforms = platforms.all;
