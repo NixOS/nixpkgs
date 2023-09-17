94ab517070c5090ace7eaaaa95bfce6ec9db07c1
Bjørn Forsman <bjorn.forsman@gmail.com>
2023-09-17 19:38:34 +0200

    deconz: update meta
    * Set meta.mainProgram to silence lib.getExe warning.
    * Fix meta.platforms - the .deb file we use as source only supports
      x86_64 linux.
    * Set meta.sourceProvenance - the .deb file we use as source contains
      x86_64 linux binaries.

--- pkgs/servers/deconz/default.nix
+++ pkgs/servers/deconz/default.nix
@@ -77,7 +77,9 @@ stdenv.mkDerivation rec {
     description = "Manage Zigbee network with ConBee, ConBee II or RaspBee hardware";
     homepage = "https://www.dresden-elektronik.com/wireless/software/deconz.html";
     license = licenses.unfree;
-    platforms = with platforms; linux;
+    platforms = with platforms; [ "x86_64-linux" ];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     maintainers = with maintainers; [ bjornfor ];
+    mainProgram = "deCONZ";
   };
 }
