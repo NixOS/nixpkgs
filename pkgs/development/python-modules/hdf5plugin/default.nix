dd71290beba470672980bb16b7eb3bcad54331c2
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-09-17 22:13:22 +0200

    python3Packages.hdf5plugin: 4.1.3 -> 4.2.0
    Diff: https://github.com/silx-kit/hdf5plugin/compare/refs/tags/v4.1.3...v4.2.0

--- pkgs/development/python-modules/hdf5plugin/default.nix
+++ pkgs/development/python-modules/hdf5plugin/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "hdf5plugin";
-  version = "4.1.3";
+  version = "4.2.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "silx-kit";
     repo = "hdf5plugin";
     rev = "refs/tags/v${version}";
-    hash = "sha256-0emCZ+r5dCRBT2xaNsgYskcGcLF/9Jf6K7FFi/PA+60=";
+    hash = "sha256-y0iDPAfm66FdclPREPnvurupWd9ZUgz8PqFd/JoapDc=";
   };
 
   propagatedBuildInputs = [
