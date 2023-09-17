1fe241afef603625deece85c230365b693fa2297
honnip <me@honnip.page>
2023-09-17 16:12:44 +0900

    pretendard: 1.3.4 -> 1.3.5

--- pkgs/data/fonts/pretendard/default.nix
+++ pkgs/data/fonts/pretendard/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenvNoCC, fetchzip }:
 
 let
-  version = "1.3.4";
+  version = "1.3.5";
 
   mkPretendard = { pname, typeface, hash }:
     stdenvNoCC.mkDerivation {
@@ -35,18 +35,18 @@ in
   pretendard = mkPretendard {
     pname = "pretendard";
     typeface = "Pretendard";
-    hash = "sha256-rkjT0zuNDBu/TF21o8DH1K9AqimTRvYkntmqwqVdgXQ=";
+    hash = "sha256-xCEZlwTPhrNIO6WODl55wo2oin+iMYOL/rVaEybpzr0=";
   };
 
   pretendard-jp = mkPretendard {
     pname = "pretendard-jp";
     typeface = "PretendardJP";
-    hash = "sha256-u9XT6Xyn6B+TNRd7Pg8z4QEh6gkG1t4084sxkZioaIc=";
+    hash = "sha256-x0G7ULzkIJqZlK995+wWKHXZdWryUTRouGTa5LsJQzk=";
   };
 
   pretendard-std = mkPretendard {
     pname = "pretendard-std";
     typeface = "PretendardStd";
-    hash = "sha256-JgaCRXMntf/EJDS8Mm+xJemW4fcrOXnAPgEtvi7/Qok=";
+    hash = "sha256-/I8LZhFB86/+o+IzUP+bSIq7scKPOL7k/6/Bom0ZSqg=";
   };
 }
