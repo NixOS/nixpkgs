962a87887d1872a5d29eb4efe0fbce8aed17a069
Anderson Torres <torres.anderson.85@protonmail.com>
2023-09-18 19:28:16 -0300

    less: 633 -> 643
    - finalAttrs
    - split output
    - meta.mainProgram and meta.changelog
    
    Co-authored-by: Jack Maloney <jmmaloney4@gmail.com>

--- pkgs/by-name/le/less/package.nix
+++ pkgs/by-name/le/less/package.nix
@@ -5,33 +5,37 @@
 , pcre2
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "less";
-  version = "633";
+  version = "643";
 
   # Only tarballs on the website are valid releases,
   # other versions, e.g. git tags are considered snapshots.
   src = fetchurl {
-    url = "https://www.greenwoodsoftware.com/less/less-${version}.tar.gz";
-    hash = "sha256-LyAdZLgouIrzbf5s/bo+CBns4uRG6+YiSBMgmq7+0E8=";
+    url = "https://www.greenwoodsoftware.com/less/less-${finalAttrs.version}.tar.gz";
+    hash = "sha256-KRG1QyyDb6CEyKLmj2zWMSNywCalj6qpiGJzHItgUug=";
   };
-
-  configureFlags = [
-    # Look for ‘sysless’ in /etc.
-    "--sysconfdir=/etc"
-    "--with-regex=pcre2"
-  ];
 
   buildInputs = [
     ncurses
     pcre2
   ];
 
-  meta = with lib; {
+  outputs = [ "out" "man" ];
+
+  configureFlags = [
+    # Look for 'sysless' in /etc.
+    "--sysconfdir=/etc"
+    "--with-regex=pcre2"
+  ];
+
+  meta = {
     homepage = "https://www.greenwoodsoftware.com/less/";
-    description = "A more advanced file pager than ‘more’";
-    platforms = platforms.unix;
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ eelco dtzWill ];
+    description = "A more advanced file pager than 'more'";
+    changelog = "https://www.greenwoodsoftware.com/less/news.${finalAttrs.version}.html";
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "less";
+    maintainers = with lib.maintainers; [ eelco dtzWill ];
+    platforms = lib.platforms.unix;
   };
-}
+})
