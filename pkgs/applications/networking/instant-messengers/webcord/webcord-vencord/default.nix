8fd63b1c8c59412d72a0185c2493ae120d13d9aa
Ludovico Piero <lewdovico@gnuweeb.org>
2023-09-18 12:24:07 +0900

    webcord-vencord: override electron to use electron_24
    Latest Webcord update changed to downgrade Electron to version 25,
    which unfortunately resulted in a segfault when launching Webcord-Vencord on Wayland.
    Therefore, we decided to revert the downgrade for now.
    It's possible that this issue will be resolved in Electron version 27

--- pkgs/applications/networking/instant-messengers/webcord/webcord-vencord/default.nix
+++ pkgs/applications/networking/instant-messengers/webcord/webcord-vencord/default.nix
@@ -2,9 +2,9 @@
 , substituteAll
 , lib
 , vencord-web-extension
+, electron_24
 }:
-
-webcord.overrideAttrs (old: {
+(webcord.overrideAttrs (old: {
   pname = "webcord-vencord";
 
   patches = (old.patches or [ ]) ++ [
@@ -18,4 +18,11 @@ webcord.overrideAttrs (old: {
     description = "Webcord with Vencord web extension";
     maintainers = with maintainers; [ FlafyDev NotAShelf ];
   };
-})
+})).override {
+  /*
+    Latest Webcord update changed to downgrade Electron to version 25,
+    which unfortunately resulted in a segfault when launching `webcord-vencord` on Wayland.
+    Therefore, we decided to revert the downgrade for now.
+  */
+  electron_25 = electron_24;
+}
