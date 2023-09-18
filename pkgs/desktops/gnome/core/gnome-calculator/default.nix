28fefd92a7477866df8d9cc1402d70248dd24f63
Bobby Rong <rjl931189261@126.com>
2023-09-18 13:38:38 +0000

    gnome.gnome-calculator: 45.0.1 → 45.0.2
    https://gitlab.gnome.org/GNOME/gnome-calculator/-/compare/45.0.1...45.0.2
    
    Just translations.
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-calculator/default.nix
+++ pkgs/desktops/gnome/core/gnome-calculator/default.nix
@@ -25,11 +25,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-calculator";
-  version = "45.0.1";
+  version = "45.0.2";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-calculator/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "ahjUooluFITj3HE+BmYCBmmlgLdAGS91TV+1YbPQd3I=";
+    sha256 = "fcvzI4SJcXHL5Ug+xmTZlOXnVekSrh35EWJPA8kIZ8I=";
   };
 
   nativeBuildInputs = [
