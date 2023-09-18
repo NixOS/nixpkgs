58eb45b0fe0ec308f7bdfaf5534ce1213ee22a82
Atemu <atemu.main@gmail.com>
2023-09-18 12:17:10 +0200

    warp: add Darwin support

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36527,7 +36527,9 @@ with pkgs;
 
   waon = callPackage ../applications/audio/waon { };
 
-  warp = callPackage ../applications/networking/warp { };
+  warp = callPackage ../applications/networking/warp {
+    inherit (darwin.apple_sdk.frameworks) Security Foundation;
+  };
 
   warpd = callPackage ../applications/misc/warpd { };
 
