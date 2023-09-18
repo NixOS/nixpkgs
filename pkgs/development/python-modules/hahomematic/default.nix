03d6f3538902af1aae6e8b953a76ccdf710befce
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-18 18:06:55 +0200

    python311Packages.hahomematic: 2023.9.1 -> 2023.9.2
    Diff: https://github.com/danielperna84/hahomematic/compare/refs/tags/2023.9.1...2023.9.2
    
    Changelog: https://github.com/danielperna84/hahomematic/releases/tag/2023.9.2

--- pkgs/development/python-modules/hahomematic/default.nix
+++ pkgs/development/python-modules/hahomematic/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "hahomematic";
-  version = "2023.9.1";
+  version = "2023.9.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.11";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "danielperna84";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-EItguRqgf6oCKUPdW4cH5kGeZqn4Ke+7gVYhcYrhhjk=";
+    hash = "sha256-h8zSj1jmqaQtJl3lPYGj7kEBucK7DAOkdUcDo7Opy7w=";
   };
 
   nativeBuildInputs = [
