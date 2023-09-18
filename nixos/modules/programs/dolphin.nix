b8f93e7786c53a8755af4208fb162d54a8b4df8f
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2023-09-18 23:43:18 +0800

    nixos/dolphin: add extraPackages

--- nixos/modules/programs/dolphin.nix
+++ nixos/modules/programs/dolphin.nix
@@ -2,6 +2,9 @@
 
 with lib;
 
+let
+  cfg = config.programs.dolphin;
+in
 {
   meta.maintainers = with maintainers; [ MakiseKurisu ];
 
@@ -13,10 +16,31 @@ with lib;
       This option will install additional depencencies to mimic the experience
       of Dolphin in a normal KDE Plasma setup
     '');
+
+    extraPackages = mkOption {
+      type = with types; listOf package;
+      default = with pkgs.libsForQt5; [];
+      defaultText = literalExpression ''
+        with pkgs.libsForQt5; [];
+      '';
+      description = lib.mdDoc ''
+        Extra packages that enhance Dolphin's functionality.
+
+        This option reduces the clutter in environment.systemPackages, and also
+        serves as the documentation for possible optional packages.
+      '';
+      example = literalExpression ''
+        with pkgs.libsForQt5; [
+          dolphin-plugins
+          kio-extras
+          kdegraphics-thumbnailers
+        ];
+      '';
+    };
   };
 
   # implementation
-  config = mkIf config.programs.dolphin.enable {
+  config = mkIf cfg.enable {
 
     environment = {
 
@@ -35,7 +59,7 @@ with lib;
         konsole # for terminal panel
         breeze-qt5 # for theme
         breeze-icons # for icons
-      ];
+      ] ++ cfg.extraPackages;
     };
 
     services = {
