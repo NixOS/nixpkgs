57be09006fd7abf9e25fcfce34a840628c140c1a
Anthony Roussel <anthony@roussel.dev>
2023-09-19 16:23:09 +0200

    goss: use wrapProgram to set $PATH for sh, getent, systemctl
    This will fix the failing `command` resource of `goss` which requires
    the `sh` binary in the `PATH`.
    
    This happens when running `goss` as SystemD service with a `command`
    ressource, in that case, `goss` fails with a `sh: command not found`
    error.
    
    See
    
    * https://github.com/goss-org/goss/blob/v0.4.2/system/command_posix.go#L8
    * https://github.com/goss-org/goss/blob/v0.4.2/system/service_systemd.go#L37
    * https://github.com/goss-org/goss/blob/v0.4.2/system/file.go#L230

--- pkgs/tools/misc/goss/default.nix
+++ pkgs/tools/misc/goss/default.nix
@@ -1,8 +1,13 @@
-{ buildGoModule
+{ bash
+, buildGoModule
 , fetchFromGitHub
+, getent
 , goss
-, nix-update-script
 , lib
+, makeWrapper
+, nix-update-script
+, stdenv
+, systemd
 , testers
 }:
 
@@ -26,11 +31,21 @@ buildGoModule rec {
     "-s" "-w" "-X main.version=v${version}"
   ];
 
+  nativeBuildInputs = [ makeWrapper ];
+
   checkFlags = [
     # Prometheus tests are skipped upstream
     # See https://github.com/goss-org/goss/blob/master/ci/go-test.sh
     "-skip" "^TestPrometheus"
   ];
+
+  postInstall = let
+    runtimeDependencies = [ bash getent ]
+      ++ lib.optionals (!stdenv.isDarwin) [ systemd ];
+  in ''
+    wrapProgram $out/bin/goss \
+      --prefix PATH : "${lib.makeBinPath runtimeDependencies}"
+  '';
 
   passthru = {
     tests.version = testers.testVersion {
