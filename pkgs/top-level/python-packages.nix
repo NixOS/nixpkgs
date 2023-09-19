aadcf41da094eb0774a68c73f6fcd508a8a7176d
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-19 04:24:11 +0200

    python311Packages.snaptime: init at 0.2.4
    Packaged as a test dependency for maya.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12101,6 +12101,8 @@ self: super: with self; {
 
   snapshottest = callPackage ../development/python-modules/snapshottest { };
 
+  snaptime = callPackage ../development/python-modules/snaptime { };
+
   sniffio = callPackage ../development/python-modules/sniffio { };
 
   snitun = callPackage ../development/python-modules/snitun { };
