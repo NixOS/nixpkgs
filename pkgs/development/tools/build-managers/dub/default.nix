68067af675910d11cb0c1f4ee4a8d00ccdf8e722
Jeremy <jtbx@duck.com>
2023-09-19 16:57:05 +1200

    dub: 1.30.0 -> 1.33.0

--- pkgs/development/tools/build-managers/dub/default.nix
+++ pkgs/development/tools/build-managers/dub/default.nix
@@ -4,7 +4,7 @@ assert dcompiler != null;
 
 stdenv.mkDerivation rec {
   pname = "dub";
-  version = "1.30.0";
+  version = "1.33.0";
 
   enableParallelBuilding = true;
 
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
     owner = "dlang";
     repo = "dub";
     rev = "v${version}";
-    sha256 = "sha256-iVl7bjblvIxvrUX7Phq6h4AIAmZjNVkGYYFA1hhsE7c=";
+    sha256 = "sha256-4Mha7WF6cg3DIccfpvOnheuvgfziv/7wo8iFsPXO4yY=";
   };
 
   dubvar = "\\$DUB";
@@ -137,6 +137,7 @@ stdenv.mkDerivation rec {
     rm -r test/path-subpackage-ref
     rm -r test/sdl-package-simple
     rm -r test/dpath-variable # requires execution of dpath-variable.sh
+    rm -r test/use-c-sources
 
     ./test/run-unittest.sh
   '';
