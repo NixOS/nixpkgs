b0852a669d324476d786756d53fb8e1ff80c39ef
K900 <me@0upti.me>
2023-09-19 14:32:41 +0300

    linux-6.5: hash -> sha256
    That's what the updater script expects, at least for now
    
    (cherry picked from commit 2c73000ef028fe16f8d242bf1727fd0cdbb9d272)

--- pkgs/os-specific/linux/kernel/linux-6.5.nix
+++ pkgs/os-specific/linux/kernel/linux-6.5.nix
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-    hash = "sha256-TKwT97F72Nz5AyrWj5Ejq1MT1pjJ9ZQWBDFlFQdj608=";
+    sha256 = "sha256-TKwT97F72Nz5AyrWj5Ejq1MT1pjJ9ZQWBDFlFQdj608=";
   };
 } // (args.argsOverride or { }))
