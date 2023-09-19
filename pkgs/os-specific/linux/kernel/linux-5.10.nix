cdc7c3a9f50b073633a57a4d7880916651ce6244
K900 <me@0upti.me>
2023-09-19 14:33:02 +0300

    linux: 5.10.194 -> 5.10.195

--- pkgs/os-specific/linux/kernel/linux-5.10.nix
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.10.194";
+  version = "5.10.195";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = versions.pad 3 version;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "15fr7krhpmqz0xqjg78m2xvfllbni3xh8xyhxh9ni31ppd3mw394";
+    sha256 = "0n4vg2i9sq89wnz85arlyvwysh9s83cgzs5bk2wh98bivi5fwfs1";
   };
 } // (args.argsOverride or {}))
