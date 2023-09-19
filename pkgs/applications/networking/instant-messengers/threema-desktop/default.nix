0c26b3cb92bf02df0fdfee7e0e79e411e2028e22
P. R. d. O. <d.ol.rod@tutanota.com>
2023-09-19 06:55:28 -0600

    threema-desktop: 1.2.31 -> 1.2.36

--- pkgs/applications/networking/instant-messengers/threema-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/threema-desktop/default.nix
@@ -1,15 +1,27 @@
-{ lib, stdenv, fetchurl, dpkg, autoPatchelfHook, makeWrapper, electron
-, alsa-lib, glibc, gtk3, libxshmfence, mesa, nss }:
+{ lib
+, stdenv
+, fetchurl
+, dpkg
+, autoPatchelfHook
+, makeWrapper
+, electron
+, alsa-lib
+, glibc
+, gtk3
+, libxshmfence
+, mesa
+, nss
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "threema-desktop";
-  version = "1.2.31";
+  version = "1.2.36";
 
   src = fetchurl {
     # As Threema only offers a Latest Release url, the plan is to upload each
     # new release url to web.archive.org until their Github releases page gets populated.
-    url = "https://web.archive.org/web/20230731230034if_/https://releases.threema.ch/web-electron/v1/release/Threema-Latest.deb";
-    hash = "sha256-eZ/bjcSnrnzub1G4sbwPn3GCTwhDfFuYv9Plf5SJL90=";
+    url = "https://web.archive.org/web/20230918134428/https://releases.threema.ch/web-electron/v1/release/Threema-Latest.deb";
+    hash = "sha256-CunyfAU33Cq6oCTraS7LigP3ezI+MCSYZEv12tXy/xc=";
   };
 
   nativeBuildInputs = [
@@ -18,10 +30,14 @@ stdenv.mkDerivation rec {
     makeWrapper
   ];
 
-  buildInputs = [ alsa-lib glibc gtk3 libxshmfence mesa nss ];
-
-  dontBuild = true;
-  dontConfigure = true;
+  buildInputs = [
+    alsa-lib
+    glibc
+    gtk3
+    libxshmfence
+    mesa
+    nss
+  ];
 
   unpackPhase = ''
     # Can't unpack with the common dpkg-deb -x method
@@ -34,6 +50,8 @@ stdenv.mkDerivation rec {
     # This will cause confusion, not needed
     rm -r usr/bin
     mv usr $out
+    # https://github.com/NixOS/nixpkgs/issues/254798#issuecomment-1722604390, this is not used
+    rm $out/lib/threema/threema-web
 
     runHook postInstall
   '';
@@ -50,6 +68,7 @@ stdenv.mkDerivation rec {
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.agpl3Only;
     maintainers = with maintainers; [ wolfangaukang ];
+    mainProgram = "threema";
     platforms = [ "x86_64-linux" ];
   };
 }
