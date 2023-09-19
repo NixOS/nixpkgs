NixOS/nixpkgs

commit 4afa887b8a69d8d8ac2bc23513fad75e82fbb466

cloud-hypervisor: run more tests

pkgs/applications/virtualization/cloud-hypervisor/default.nix
@@ -34,8 +34,11 @@ rustPlatform.buildRustPackage rec {
 
   OPENSSL_NO_VENDOR = true;
 
-  # Integration tests require root.
-  cargoTestFlags = [ "--bins" ];
+  cargoTestFlags = [
+    "--workspace"
+    "--bins" "--lib" # Integration tests require root.
+    "--exclude" "net_util" # Tries to access /dev/net/tun
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/cloud-hypervisor/cloud-hypervisor";

commit d37f41af2ca1884b84d9440d56579efe88a3205b

cloud-hypervisor: disable vmm tests
These require KVM, which we can't assume is available in builders.

Fixes: 4afa887b8a69 ("cloud-hypervisor: run more tests")

pkgs/applications/virtualization/cloud-hypervisor/default.nix
@@ -37,7 +37,8 @@ rustPlatform.buildRustPackage rec {
   cargoTestFlags = [
     "--workspace"
     "--bins" "--lib" # Integration tests require root.
-    "--exclude" "net_util" # Tries to access /dev/net/tun
+    "--exclude" "net_util" # /dev/net/tun
+    "--exclude" "vmm"      # /dev/kvm
   ];
 
   meta = with lib; {