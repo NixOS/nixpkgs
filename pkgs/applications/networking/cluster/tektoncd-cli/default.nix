24b86174da2f583d5b1ea14960a81495898a2e4a
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-19 11:32:01 +0000

    tektoncd-cli: 0.31.2 -> 0.32.0

--- pkgs/applications/networking/cluster/tektoncd-cli/default.nix
+++ pkgs/applications/networking/cluster/tektoncd-cli/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "tektoncd-cli";
-  version = "0.31.2";
+  version = "0.32.0";
 
   src = fetchFromGitHub {
     owner = "tektoncd";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-QBJ8UoR61XOFT6u5glpL+DHaj5Kyqq73r0pyteINQNA=";
+    sha256 = "sha256-Ilue0stXko8bkMMzXEHrdgJYIV5ZcI39hwFUya8X4ac=";
   };
 
   vendorHash = null;
