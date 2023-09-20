f35534ca8065e803cb3708f25eaaa7930bbf74ba
Gabriel Simmer <g@gmem.ca>
2023-09-20 17:52:59 +0100

    nushell: 0.84.0 -> 0.85.0

--- pkgs/shells/nushell/plugins/gstat.nix
+++ pkgs/shells/nushell/plugins/gstat.nix
@@ -12,9 +12,9 @@ let
 in
 rustPlatform.buildRustPackage {
   inherit pname;
-  version = "0.84.0";
+  version = "0.85.0";
   src = nushell.src;
-  cargoHash = "sha256-RcwCYfIEV0+NbZ99uWaCOLqLap3wZ4qXIsc02fqkBSQ=";
+  cargoHash = "sha256-Fj70uKYzEKxeZeNrqlwM7ZFJ+K1tz10RqLndrdY40CE=";
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ Security ];
   cargoBuildFlags = [ "--package nu_plugin_gstat" ];
