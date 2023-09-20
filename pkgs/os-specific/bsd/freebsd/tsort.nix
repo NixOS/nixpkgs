00a6e66d002ef97143a5c5e32caf590998d5005e
Audrey Dutcher <rhelmot@emotionlabs.io>
2023-09-19 23:54:31 -0700

    unbreak freebsd13

--- pkgs/os-specific/bsd/freebsd/tsort.nix
+++ pkgs/os-specific/bsd/freebsd/tsort.nix
@@ -1,14 +1,15 @@
-{ mkDerivation, buildPackages, buildFreebsd, ... }:
+{ mkDerivation, buildPackages, buildFreebsd, lib, hostVersion, ... }:
 mkDerivation {
   path = "usr.bin/tsort";
   extraPaths = ["sys/conf/newvers.sh" "sys/sys/param.h"];  # TODO this was added to fix a error which appeared but may be spurious
   makeFlags = [
     "STRIP=-s" # flag to install, not command
+  ] ++ lib.optionals (hostVersion == "freebsd14") [
     "TESTSDIR=${builtins.placeholder "test"}"
   ];
   nativeBuildInputs = [
     buildPackages.bsdSetupHook buildFreebsd.freebsdSetupHook
     buildFreebsd.makeMinimal buildFreebsd.install buildPackages.mandoc buildPackages.groff  # TODO bmake???
   ];
-  outputs = [ "out" "test" ];
+  outputs = [ "out" ] ++ lib.optionals (hostVersion == "frebsd14") [ "test" ];
 }
