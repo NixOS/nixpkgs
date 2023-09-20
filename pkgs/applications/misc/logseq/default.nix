92024134ff16550d25c5ef6c59758d71919367c4
kilianar <mail@kilianar.de>
2023-09-20 18:59:18 +0200

    logseq: 0.9.17 -> 0.9.18
    https://github.com/logseq/logseq/releases/tag/0.9.18

--- pkgs/applications/misc/logseq/default.nix
+++ pkgs/applications/misc/logseq/default.nix
@@ -14,11 +14,11 @@ stdenv.mkDerivation (finalAttrs: let
 
 in {
   pname = "logseq";
-  version = "0.9.17";
+  version = "0.9.18";
 
   src = fetchurl {
     url = "https://github.com/logseq/logseq/releases/download/${version}/logseq-linux-x64-${version}.AppImage";
-    hash = "sha256-1CXr/evINfB+VwLQBeuVhq0rCzRVM1ULQC3epYECN+I=";
+    hash = "sha256-+2BnVW0qWSJ/PIY3zl1c9qzcP9DZVp6E9B7AI6LqibE=";
     name = "${pname}-${version}.AppImage";
   };
 
