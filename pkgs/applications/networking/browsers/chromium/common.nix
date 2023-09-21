cc483896902d731d05268f7ba85cc94be6c0c638
Alexander V. Nikolaev <avn@avnik.info>
2023-09-21 16:21:24 +0300

    Don't depend on target rustc
    We need a "build" rustc here, not "target" one.

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -388,7 +388,7 @@ let
       # Use nixpkgs Rust compiler instead of the one shipped by Chromium.
       # We do intentionally not set rustc_version as nixpkgs will never do incremental
       # rebuilds, thus leaving this empty is fine.
-      rust_sysroot_absolute = "${rustc}";
+      rust_sysroot_absolute = "${buildPackages.rustc}";
       # Building with rust is disabled for now - this matches the flags in other major distributions.
       enable_rust = false;
     } // lib.optionalAttrs (!(stdenv.buildPlatform.canExecute stdenv.hostPlatform)) {
