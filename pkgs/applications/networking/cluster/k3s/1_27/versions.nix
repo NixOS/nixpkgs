0188e42d0a92d75a4f656d7dcb5436da5c07dc76
Aaron Jheng <wentworth@outlook.com>
2023-09-21 23:27:34 +0800

    k3s: vendorSha256 -> vendorHash

--- pkgs/applications/networking/cluster/k3s/1_27/versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_27/versions.nix
@@ -2,7 +2,7 @@
   k3sVersion = "1.27.6+k3s1";
   k3sCommit = "bd04941a294793ec92e8703d5e5da14107902e88";
   k3sRepoSha256 = "04chr8gp0yprihigy1yzhvi2baby053fav384gq0sjq6bkp3fzd8";
-  k3sVendorSha256 = "sha256-LH9OsBK0Pq/NGEHprbIgYKQsslYdR3i4LYVvo5P0K+8=";
+  k3sVendorHash = "sha256-LH9OsBK0Pq/NGEHprbIgYKQsslYdR3i4LYVvo5P0K+8=";
   chartVersions = import ./chart-versions.nix;
   k3sRootVersion = "0.12.2";
   k3sRootSha256 = "1gjynvr350qni5mskgm7pcc7alss4gms4jmkiv453vs8mmma9c9k";
