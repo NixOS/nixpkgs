00a343b1b189e9f276b57ebc8b82bf89b135ed1d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-09-22 01:16:04 +0200

    harmonia: unpin nix unstable

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9097,9 +9097,7 @@ with pkgs;
 
   hardinfo = callPackage ../tools/system/hardinfo { };
 
-  harmonia = callPackage ../tools/package-management/harmonia {
-    nix = nixVersions.unstable;
-  };
+  harmonia = callPackage ../tools/package-management/harmonia { };
 
   hayagriva = callPackage ../tools/typesetting/hayagriva { };
 
