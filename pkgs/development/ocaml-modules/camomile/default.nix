e003e806d9caa1385db1f9d1fd495f364cef7398
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-09-22 13:14:43 +0200

    ocamlPackages.camomile: fix build on darwin

--- pkgs/development/ocaml-modules/camomile/default.nix
+++ pkgs/development/ocaml-modules/camomile/default.nix
@@ -1,6 +1,12 @@
-{ lib, fetchFromGitHub, nix-update-script
+{ lib
+, stdenv
 , buildDunePackage
-, dune-site, camlp-streams
+, fetchFromGitHub
+, writeScriptBin
+, darwin
+, dune-site
+, camlp-streams
+, nix-update-script
 }:
 
 buildDunePackage rec {
@@ -15,6 +21,18 @@ buildDunePackage rec {
     rev = "v${version}";
     hash = "sha256-HklX+VPD0Ta3Knv++dBT2rhsDSlDRH90k4Cj1YtWIa8=";
   };
+
+  nativeBuildInputs = lib.optionals stdenv.isDarwin [
+    # libc++abi: terminating with uncaught exception of type SigTool::NotAMachOFileException: std::exception
+    (writeScriptBin "codesign" ''
+      #!${stdenv.shell}
+    '')
+  ];
+
+  propagatedNativeBuildInputs = [
+    # Error: Program codesign not found in the tree or in PATH
+    darwin.sigtool
+  ];
 
   propagatedBuildInputs = [
     dune-site
