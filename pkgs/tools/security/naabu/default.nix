34a8b48ffc988ea1a57a91bf16a0d07262deaeb8
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-22 22:21:47 +0200

    naabu: 2.1.7 -> 2.1.8
    Diff: https://github.com/projectdiscovery/naabu/compare/refs/tags/v2.1.7...v2.1.8
    
    Changelog: https://github.com/projectdiscovery/naabu/releases/tag/v2.1.8

--- pkgs/tools/security/naabu/default.nix
+++ pkgs/tools/security/naabu/default.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "naabu";
-  version = "2.1.7";
+  version = "2.1.8";
 
   src = fetchFromGitHub {
     owner = "projectdiscovery";
     repo = "naabu";
     rev = "refs/tags/v${version}";
-    hash = "sha256-x6TmV8c5p9Uuc9uJG3+FNNpdmzdzgQpsyO29dly7PuU=";
+    hash = "sha256-/EVlg0NhWJRowHttwYq+i/3BhO5o8w74kRLWIjSI6AQ=";
   };
 
-  vendorHash = "sha256-9LIPRiLKszfz9Gj26G03TPHOqCXi1s3CYiaadInlD84=";
+  vendorHash = "sha256-LoYArDsCJCDSI02nFSUVemYNeF+q5xAjV2QyWjMOXIY=";
 
   buildInputs = [
     libpcap
