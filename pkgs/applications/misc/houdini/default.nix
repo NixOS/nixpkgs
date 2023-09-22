429be0eb30691c483efbee6d8b06816035022c3e
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-09-22 15:03:30 +0200

    houdini: fix missing quotation marks in wrappers

--- pkgs/applications/misc/houdini/default.nix
+++ pkgs/applications/misc/houdini/default.nix
@@ -80,7 +80,7 @@ buildFHSEnv rec {
       mkdir -p $out/$(dirname $executable)
 
       echo "#!${stdenv.shell}" >> $out/$executable
-      echo "$WRAPPER ${unwrapped}/$executable \$@" >> $out/$executable
+      echo "$WRAPPER ${unwrapped}/$executable \"\$@\"" >> $out/$executable
     done
 
     cd $out
@@ -93,6 +93,6 @@ buildFHSEnv rec {
   ];
 
   runScript = writeScript "${name}-wrapper" ''
-    exec $@
+    exec "$@"
   '';
 }
