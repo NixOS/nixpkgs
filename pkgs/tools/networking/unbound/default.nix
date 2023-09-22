NixOS/nixpkgs

commit 44500c34d49073bd80a13546414ea156ebd72c9c

unbound: 1.17.1 -> 1.18.0
https://nlnetlabs.nl/news/2023/Aug/30/unbound-1.18.0-released/

pkgs/tools/networking/unbound/default.nix
@@ -48,11 +48,11 @@
 
 stdenv.mkDerivation rec {
   pname = "unbound";
-  version = "1.17.1";
+  version = "1.18.0";
 
   src = fetchurl {
     url = "https://nlnetlabs.nl/downloads/unbound/unbound-${version}.tar.gz";
-    hash = "sha256-7kCFzszhJYTmAPPYFKKPqCLfqs7B+UyEv9Z/ilVxpfQ=";
+    hash = "sha256-PalUkKhc/2Qg8m+uC4Skn1ES3xvxt/w0+HJPAggstxI=";
   };
 
   outputs = [ "out" "lib" "man" ]; # "dev" would only split ~20 kB

commit cf51455436024b2e4cf2cf6658eebf0ffa1bed3a

unbound: backport fix for libunbound with nettle
Link: https://github.com/NixOS/nixpkgs/pull/252325#issuecomment-1731160655
Link: https://github.com/NLnetLabs/unbound/issues/942
Fixes: 44500c34d490 ("unbound: 1.17.1 -> 1.18.0")

pkgs/tools/networking/unbound/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchurl
+, fetchpatch
 , openssl
 , nettle
 , expat
@@ -54,6 +55,15 @@ stdenv.mkDerivation rec {
     url = "https://nlnetlabs.nl/downloads/unbound/unbound-${version}.tar.gz";
     hash = "sha256-PalUkKhc/2Qg8m+uC4Skn1ES3xvxt/w0+HJPAggstxI=";
   };
+
+  patches = [
+    # Backport: fix libunbound with nettle.
+    (fetchpatch {
+      url = "https://github.com/NLnetLabs/unbound/commit/654a7eab62cbd1844d483cc4a0f2cf2fbcbaf00a.patch";
+      excludes = [ "doc/Changelog" ];
+      hash = "sha256-n3FCeZESFrrn6Wcf28Hb8WZs1eMHWjbsf2WCFOXU3lI=";
+    })
+  ];
 
   outputs = [ "out" "lib" "man" ]; # "dev" would only split ~20 kB
 