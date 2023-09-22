9627a55771cd8c770962eb59edb5f32b0880fd95
Gaetan Lepage <gaetan@glepage.com>
2023-09-22 14:01:38 +0200

    python310Packages.hyperpyyaml: 1.2.1 -> 1.2.2

--- pkgs/development/python-modules/hyperpyyaml/default.nix
+++ pkgs/development/python-modules/hyperpyyaml/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, fetchpatch
 , pyyaml
 , ruamel-yaml
 , pytestCheckHook
@@ -9,24 +8,15 @@
 
 buildPythonPackage rec {
   pname = "hyperpyyaml";
-  version = "1.2.1";
+  version = "1.2.2";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "speechbrain";
     repo = "hyperpyyaml";
     rev = "refs/tags/v${version}";
-    hash = "sha256-tC4kLJAY9MVgjWwU2Qu0rPCVDw7CjKVIciRZgYhnR9I=";
+    hash = "sha256-eA4/wXmqlqomfRbJNi7dkBRoxneCbCbURSPvASF2sgA=";
   };
-
-  patches = [
-    # This patch is needed to comply with the newer versions of ruamel.yaml.
-    # PR to upstream this change: https://github.com/speechbrain/HyperPyYAML/pull/23
-    (fetchpatch {
-      url = "https://github.com/speechbrain/HyperPyYAML/commit/95c6133686c42764770a77429eab55f6dfe5581c.patch";
-      hash = "sha256-WrHDo17f5pYNXSSqI8t1tlAloYms9oLFup7D2qCKwWw=";
-    })
-  ];
 
   propagatedBuildInputs = [
     pyyaml
