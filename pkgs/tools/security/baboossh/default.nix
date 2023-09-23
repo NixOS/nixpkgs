f4369736f0742ef99037c2e63ed534c76f29d13d
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-09-23 04:24:39 +0200

    baboossh: patch support for paramiko 3.3.1

--- pkgs/tools/security/baboossh/default.nix
+++ pkgs/tools/security/baboossh/default.nix
@@ -15,6 +15,25 @@ python3.pkgs.buildPythonApplication rec {
     hash = "sha256-dorIqnJuAS/y9W6gyt65QjwGwx4bJHKLmdqRPzY25yA=";
   };
 
+  # https://github.com/cybiere/baboossh/issues/11
+  postPatch = let
+    py3compat-u-implementation = ''
+      def u(s, encoding="utf8"):
+        if isinstance(s, bytes): return s.decode(encoding)
+        elif isinstance(s, str): return s
+        else: raise TypeError("Expected unicode or bytes, got {!r}".format(s))
+    '';
+  in ''
+    py3compat_fnames=(
+      baboossh/connection.py
+      baboossh/ext_dir/payload_exec.py
+      baboossh/ext_dir/payload_shell.py
+      baboossh/ext_dir/payload_gather.py
+    )
+    substituteInPlace "''${py3compat_fnames[@]}" \
+      --replace "from paramiko.py3compat import u" '${py3compat-u-implementation}'
+  '';
+
   propagatedBuildInputs = with python3.pkgs; [
     cmd2
     tabulate
