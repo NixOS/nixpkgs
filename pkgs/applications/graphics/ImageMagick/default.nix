58c5f021cc670b7f1765b796761d4ab3de1ef75c
Mario Rodas <marsam@users.noreply.github.com>
2023-09-23 10:29:00 +0000

    imagemagick: add changelog to meta

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -133,6 +133,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = with lib; {
     homepage = "http://www.imagemagick.org/";
+    changelog = "https://github.com/ImageMagick/Website/blob/main/ChangeLog.md";
     description = "A software suite to create, edit, compose, or convert bitmap images";
     pkgConfigModules = [ "ImageMagick" "MagickWand" ];
     platforms = platforms.linux ++ platforms.darwin;
