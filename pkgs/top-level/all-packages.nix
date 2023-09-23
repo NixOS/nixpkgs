d7da032704648dcf603baacefc6ee67464e0161e
Anderson Torres <torres.anderson.85@protonmail.com>
2023-09-23 16:18:52 -0300

    labwc: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33512,7 +33512,7 @@ with pkgs;
 
   lame = callPackage ../development/libraries/lame { };
 
-  labwc = callPackage ../applications/window-managers/labwc {
+  labwc = callPackage ../by-name/la/labwc/package.nix {
     wlroots = wlroots_0_16;
   };
 
