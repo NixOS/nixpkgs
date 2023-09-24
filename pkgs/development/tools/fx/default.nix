1554c89a981a4b9ca45c34c43e0902a7c7892208
figsoda <figsoda@pm.me>
2023-09-24 10:37:15 -0400

    fx: 30.0.3 -> 30.1.0
    Diff: https://github.com/antonmedv/fx/compare/30.0.3...30.1.0
    
    Changelog: https://github.com/antonmedv/fx/releases/tag/30.1.0

--- pkgs/development/tools/fx/default.nix
+++ pkgs/development/tools/fx/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "fx";
-  version = "30.0.3";
+  version = "30.1.0";
 
   src = fetchFromGitHub {
     owner = "antonmedv";
     repo = pname;
     rev = version;
-    hash = "sha256-bTXxzGf7mXQ0VfAQhaKAOYtOVAEVC71R3eRJej0zfJs=";
+    hash = "sha256-SqD3NPaeJB/bxb47PO39mwJGnSg2WBQ3RyA6PRn7z10=";
   };
 
-  vendorHash = "sha256-FyV3oaI4MKl0LKJf23XIeUmvFsa1DvQw2pq5Heza3Ws=";
+  vendorHash = "sha256-6wVcdzTYnB0Bd/YLPcbryKxCXu5genzQQ96znbn2ahw=";
 
   meta = with lib; {
     description = "Terminal JSON viewer";
