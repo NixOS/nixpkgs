ecbb5bb00c757055a30bb088e15794d41e503792
Bobby Rong <rjl931189261@126.com>
2023-09-24 09:32:15 +0000

    gnome.adwaita-icon-theme: 45.rc → 45.0
    https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/compare/45.rc...45.0
    
    No changes.
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/adwaita-icon-theme/default.nix
+++ pkgs/desktops/gnome/core/adwaita-icon-theme/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "adwaita-icon-theme";
-  version = "45.rc";
+  version = "45.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/adwaita-icon-theme/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "2Uj/rJ4xglKVgMKtG2XkkIzBk/uVadClaCZyi/mYJ6M=";
+    sha256 = "JEK/sG9ObMlb9uJoL9/5j6Xt3GiHUbnWIVxiPLTkL/E=";
   };
 
   nativeBuildInputs = [
