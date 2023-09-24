7fd9d9750d60060e11ee0d4fec39eed850bdd6cd
Emery Hemingway <ehmry@posteo.net>
2023-09-24 18:53:07 +0100

    tarlz: 0.22 -> 0.24

--- pkgs/tools/archivers/tarlz/default.nix
+++ pkgs/tools/archivers/tarlz/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "tarlz";
-  version = "0.22";
+  version = "0.24";
   outputs = [ "out" "man" "info" ];
 
   nativeBuildInputs = [ lzip texinfo ];
@@ -10,12 +10,17 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://savannah/lzip/${pname}/${pname}-${version}.tar.lz";
-    sha256 = "sha256-/M9yJvoktV0ybKsT926jSb7ERsWo33GkbTQwmaBQkdw=";
+    sha256 = "49838effe95acb29d548b7ef2ddbb4b63face40536df0d9a80a62900c7170576";
   };
 
   enableParallelBuilding = true;
   makeFlags = [ "CXX:=$(CXX)" ];
   doCheck = !stdenv.isDarwin;
+
+  preCheck = ''
+    substituteInPlace testsuite/check.sh \
+      --replace '1969-12-31T23:59:59' '''
+  '';
 
   meta = with lib; {
     homepage = "https://www.nongnu.org/lzip/${pname}.html";
