75d6a892f8844227548518afd7284d853237458d
Thiago Kenji Okada <thiagokokada@gmail.com>
2023-09-24 17:49:30 +0100

    libretro.parallel-n64: add missing Make flags

--- pkgs/applications/emulators/retroarch/cores.nix
+++ pkgs/applications/emulators/retroarch/cores.nix
@@ -714,6 +714,11 @@ in
     core = "parallel-n64";
     extraBuildInputs = [ libGLU libGL libpng ];
     makefile = "Makefile";
+    makeFlags = [
+      "HAVE_PARALLEL=1"
+      "HAVE_PARALLEL_RSP=1"
+      "ARCH=${stdenv.hostPlatform.parsed.cpu.name}"
+    ];
     postPatch = lib.optionalString stdenv.hostPlatform.isAarch64 ''
       sed -i -e '1 i\CPUFLAGS += -DARM_FIX -DNO_ASM -DARM_ASM -DDONT_WANT_ARM_OPTIMIZATIONS -DARM64' Makefile \
       && sed -i -e 's,CPUFLAGS  :=,,g' Makefile
