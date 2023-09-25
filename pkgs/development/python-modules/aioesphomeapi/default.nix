05e4efecc2f2ce874f7219c6c315f7cb7483a1c0
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-25 06:09:46 +0200

    python311Packages.aioesphomeapi: 16.0.5 -> 16.0.6
    https://github.com/esphome/aioesphomeapi/releases/tag/v16.0.6

--- pkgs/development/python-modules/aioesphomeapi/default.nix
+++ pkgs/development/python-modules/aioesphomeapi/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aioesphomeapi";
-  version = "16.0.5";
+  version = "16.0.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "esphome";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-SueK59CZTKkQPsHThs7k9eCEmc1GwaRIrw3oSK4E80E=";
+    hash = "sha256-BcFNxm4JpHaqKEhUTCXIrF18OMFxLbQHCQ9jfs+U0hc=";
   };
 
   propagatedBuildInputs = [
