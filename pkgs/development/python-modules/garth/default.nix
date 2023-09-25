627167ee3963893a94f44fc5ce8e588e64e0c7dd
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-25 20:27:32 +0200

    python311Packages.garth: 0.4.26 -> 0.4.27

--- pkgs/development/python-modules/garth/default.nix
+++ pkgs/development/python-modules/garth/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "garth";
-  version = "0.4.26";
+  version = "0.4.27";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Ezq9lZE6HTtuW396sKZ32mDvNjrkz6UHQGvLhXUjfnI=";
+    hash = "sha256-GOpYR1fudDKHbnxwAMCXrk95xYe0Pdi7pYebHua+IjM=";
   };
 
   nativeBuildInputs = [
