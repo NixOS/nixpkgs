b47447ff1277430651ef09a9c952b3f06a6c435a
Audrey Dutcher <audrey@rhelmot.io>
2023-09-25 06:52:01 -0700

    Boot 0 - target

--- pkgs/stdenv/freebsd/default.nix
+++ pkgs/stdenv/freebsd/default.nix
@@ -8,32 +8,34 @@ let inherit (localSystem) system;
   all-bootstrap-urls-table = {
     x86_64-freebsd14 = {
       bash = {
-        url = "http://192.168.1.9:8000/v04k0p2b5ps86w56h70myaw4wcpf1a5b-x86_64-freebsd14-bash";
-        hash = "sha256-F0c3LT1XBl5g2eKsaj3Y8tXT0NnaKUb+qOV5y9U4Fis=";
+        url = "http://192.168.1.9:8000/m5kfzz6pz4w8mll56pv3ksr0jxgi66fs-x86_64-freebsd14-bash";
+        hash = "sha256-1Ke4602F+pe5mUd4c8q1nSCEBHuo2JSZlBs8dZj6vn8=";
       };
       mkdir = {
-        url = "http://192.168.1.9:8000/2r8n72xjqcx91jvzilw9yimbkvbcalbp-x86_64-freebsd14-mkdir";
+        url = "http://192.168.1.9:8000/02zndx9ymf0i74nrww5rhanpy04p7fvk-x86_64-freebsd14-mkdir";
         hash = "sha256-Wje9u9mz5kiTNSoPvmvgGwxQRgLnBM+26jPadbUTkpg=";
       };
       tar = {
-        url = "http://192.168.1.9:8000/wb5q48dipl4p8f83rbgggcxab4n02r90-x86_64-freebsd14-tar";
-        hash = "sha256-NClJCZiW7p4rDllguiWthftS9TjwTWh5ZSe+7CMCSzs=";
+        url = "http://192.168.1.9:8000/732g8sama6bgfws06w96h47cc3fch51w-x86_64-freebsd14-tar";
+        hash = "sha256-uZIGRfpPMPFcI1XzDf2brWHKd8gEe1ZP9mhTYjrDo/4=";
       };
       unxz = {
-        url = "http://192.168.1.9:8000/gmqhzn12jd575xxxd59ic8g46m749g7j-x86_64-freebsd14-unxz";
+        url = "http://192.168.1.9:8000/2ypky4yn38jrwmbb1bcrrknyihgbcda9-x86_64-freebsd14-unxz";
         hash = "sha256-yVPhB2tOwGP5aieKTJw+bXdNaM2ye8foo9SJkU3bgEY=";
       };
+      chmod = {
+        url = "http://192.168.1.9:8000/74sc194l4iiiq3798mqnvnmyi2qsc2nm-x86_64-freebsd14-chmod";
+        hash = "sha256-01dHQyHH4xnnMTgKgYFN2ksgXhhnub6fBrgUFSYjRJ0=";
+      };
       bootstrapFiles = {
-        url = "http://192.168.1.9:8000/gkzdw7a5pb3xj1py6w0r7xfna597ksqy-x86_64-freebsd14-bootstrap-files.tar.gz";
-        hash = "sha256-SMFdhT6dKFmEcVw/p44u5T8FxVvvM43v7J7nZ7DAhMM=";
+        url = "http://192.168.1.9:8000/m5r8mmfv6gh8622jxyyk3rgxvdi900gw-x86_64-freebsd14-bootstrap-files.tar.xz";
+        hash = "sha256-KVB1+jwr+cSq9DyBTE0XkMCRgtPy5PMnJpKGqX2Hp9E=";
       };
     };
   };
-  fetchurlBoot = import ../../build-support/fetchurl/boot.nix {
-    inherit system;
-  };
+  fetchurlBoot = import <nix/fetchurl.nix>;
   all-bootstrap-urls = all-bootstrap-urls-table.${localSystem.system};
-  all-bootstrap-files = lib.mapAttrs (_: v: fetchurlBoot v) all-bootstrap-urls;
+  all-bootstrap-files = lib.mapAttrs (a: v: fetchurlBoot (v // { executable = a != "bootstrapFiles"; })) all-bootstrap-urls;
 in
 [
   ({}: let
@@ -41,36 +43,54 @@ in
       name = "bootstrap-tools";
       builder = all-bootstrap-files.bash;
       args = [ ./unpack-bootstrap-files.sh ];
-      inherit (all-bootstrap-files) tar unxz mkdir bootstrapFiles;
+      inherit (all-bootstrap-files) tar unxz mkdir chmod bootstrapFiles;
+      inherit system;
     };
 
     fetchurl = import ../../build-support/fetchurl {
       inherit lib;
-      stdenvNoCC = stdenv;
+      stdenvNoCC = stdenvNoCC;
       curl = bootstrap-tools;
     };
+
+    stdenvNoCC = import ../generic {
+      inherit config;
+      name = "stdenvNoCC-freebsd-boot-0";
+      buildPlatform = localSystem;
+      hostPlatform = localSystem;
+      targetPlatform = localSystem;
+      initialPath = [ bootstrap-tools ];
+      shell = "${bootstrap-tools}/bin/bash";
+      #shell = "/usr/local/bin/bash";
+      fetchurlBoot = fetchurl;
+      cc = null;
+    };
+
     stdenv = import ../generic {
+      inherit config;
       name = "stdenv-freebsd-boot-0";
       buildPlatform = localSystem;
       hostPlatform = localSystem;
       targetPlatform = localSystem;
-      inherit config;
+      initialPath = [bootstrap-tools];
       shell = "${bootstrap-tools}/bin/bash";
+      #shell = "/usr/local/bin/bash";
       fetchurlBoot = fetchurl;
       cc = import ../../build-support/cc-wrapper ({
         inherit lib;
         name = "bootstrap-tools-cc-wrapper";
-        stdenvNoCC = stdenv;
+        stdenvNoCC = stdenvNoCC;
         libc = bootstrap-tools;
         propagateDoc = false;
         nativeTools = false;
         nativeLibc = false;
         gnugrep = bootstrap-tools;
         coreutils = bootstrap-tools;
+        cc = bootstrap-tools;
         isClang = true;
         bintools = import ../../build-support/bintools-wrapper {
           inherit lib;
-          stdenvNoCC = stdenv;
+          stdenvNoCC = stdenvNoCC;
           name = "bootstrap-tools-bintools-wrapper";
           libc = bootstrap-tools;
           propagateDoc = false;
@@ -83,7 +103,12 @@ in
       });
       overrides = self: super: {
         inherit bootstrap-tools fetchurl;
+        bash = bootstrap-tools;
       };
+      preHook = ''
+          export NIX_ENFORCE_PURITY="''${NIX_ENFORCE_PURITY-1}"
+          export NIX_ENFORCE_NO_NATIVE="''${NIX_ENFORCE_NO_NATIVE-1}"
+        '';
     };
   in { inherit config overlays stdenv; })
 ]
--- pkgs/stdenv/freebsd/unpack-bootstrap-files.sh
+++ pkgs/stdenv/freebsd/unpack-bootstrap-files.sh
@@ -1,6 +1,16 @@
 $mkdir -p $out
-$unxz <$bootstrapFiles | $tar -C $out -x
-for f in $out/bin/*; do
-    $out/bin/patchelf --set-rpath $out/lib --set-interpreter $out/libexec/ld-elf.so.? $f || true
+$unxz <$bootstrapFiles | $tar --delay-directory-restore --no-same-permissions -C $out -x
+PATH=$out/bin
+$chmod +w -R $out
+for f in $out/bin/* $out/bin/.*; do
+    patchelf --set-rpath $out/lib --set-interpreter $out/libexec/ld-elf.so.? $f
 done
-$out/bin/cp $builder $mkdir $tar $unxz $out/bin
+for f in $($out/bin/find $out/lib -type f); do
+    patchelf --set-rpath $out/lib $f
+done
+for f in $($out/bin/find $out -type f); do
+    grep -I /nix/store $f >/dev/null && sed -E -i -e 's_/nix/store/[^/]+_'"$out"'_g' $f
+done
+$out/bin/cp $mkdir $tar $unxz $out/bin
+$out/bin/cp $builder $out/bin/bash
+echo $out
