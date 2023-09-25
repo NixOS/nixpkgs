385e294390b9939b9fcee67b4bf817c06a72203c
Yueh-Shun Li <shamrocklee@posteo.net>
2023-09-25 05:58:09 +0000

    caprine-bin: 2.58.0 -> 2.58.3
    Bump to patch for CVE-2023-4863
    
    Vulnerability details:
    https://github.com/advisories/GHSA-j7hp-h8jx-5pp
    
    Upstream release notes:
    https://github.com/sindresorhus/caprine/releases/tag/v2.58.2
    https://github.com/sindresorhus/caprine/releases/tag/v2.58.3
    
    (cherry picked from commit 07107cfb1fbcc2c28952b35bd7d0cb3360c6e8e2)

--- pkgs/applications/networking/instant-messengers/caprine-bin/default.nix
+++ pkgs/applications/networking/instant-messengers/caprine-bin/default.nix
@@ -1,7 +1,7 @@
 { lib, callPackage, stdenvNoCC }:
 let
   pname = "caprine";
-  version = "2.58.0";
+  version = "2.58.3";
   metaCommon = with lib; {
     description = "An elegant Facebook Messenger desktop app";
     homepage = "https://sindresorhus.com/caprine";
@@ -10,11 +10,11 @@ let
   };
   x86_64-appimage = callPackage ./build-from-appimage.nix {
     inherit pname version metaCommon;
-    sha256 = "7iK2RyA63okJLH2Xm97fFilJHzqFuP96xkUr2+ADbC4=";
+    sha256 = "sha256-w0nBQhHYzFLsNu0MxWhoju6fh4JpAKC7MWWVxwDkRYk=";
   };
   x86_64-dmg = callPackage ./build-from-dmg.nix {
     inherit pname version metaCommon;
-    sha256 = "RqK+fJJAt9W+m7zg6ZYI6PEAOa3V1UxsptEpG1qjibg=";
+    sha256 = "sha256-6Mx2ZkT2hdnaSVt2hKMMV9xc7rYPFFbxcj6vb84ojYU=";
   };
 in
 (if stdenvNoCC.isDarwin then x86_64-dmg else x86_64-appimage).overrideAttrs (oldAttrs: {
