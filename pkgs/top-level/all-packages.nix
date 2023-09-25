60fa50c058fc5939aa549a4daa50b9d0d9ee2899
tc-kaluza <tautvydas.cerniauska@kaluza.com>
2023-09-25 08:07:38 +0100

    insulator2: init at 2.12.2
    address comments

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -42097,4 +42097,6 @@ with pkgs;
   code-maat = callPackage ../development/tools/code-maat {};
 
   mdhtml = callPackage ../tools/text/mdhtml { };
+
+  insulator2 = callPackage ../applications/misc/insulator2 {};
 }
