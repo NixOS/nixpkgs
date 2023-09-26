6c73dc674663f21971d4aaf83cf18e906d3e57ca
Artturin <Artturin@artturin.com>
2023-09-26 03:57:17 +0300

    bash: Remove unnecessary explicit passing of `binutils`
    `binutils` for darwin was added in (add binutils to bash build for size)[9c153e2227d3c66c645718355b0ed381f664afa5].
    
    The override was added in (bash: provide a working binutils)[9e05276949045b8620a0ebd7de3aa75bb6af21cc].

--- pkgs/shells/bash/5.nix
+++ pkgs/shells/bash/5.nix
@@ -2,7 +2,6 @@
 , stdenv
 , buildPackages
 , fetchurl
-, binutils
 , bison
 , util-linux
 
@@ -90,7 +89,7 @@ stdenv.mkDerivation rec {
   depsBuildBuild = [ buildPackages.stdenv.cc ];
   nativeBuildInputs = [ bison ]
     ++ lib.optional withDocs texinfo
-    ++ lib.optional stdenv.hostPlatform.isDarwin binutils;
+    ++ lib.optional stdenv.hostPlatform.isDarwin stdenv.cc.bintools;
 
   buildInputs = lib.optional interactive readline;
 
