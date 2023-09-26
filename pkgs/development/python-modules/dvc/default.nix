034c58f8e271dd119549160dfa4177d002282edd
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-26 09:31:48 +0200

    python311Packages.dvc: 3.22.0 -> 3.22.1
    Diff: https://github.com/iterative/dvc/compare/refs/tags/3.22.0...3.22.1
    
    Changelog: https://github.com/iterative/dvc/releases/tag/3.22.1

--- pkgs/development/python-modules/dvc/default.nix
+++ pkgs/development/python-modules/dvc/default.nix
@@ -55,14 +55,14 @@
 
 buildPythonPackage rec {
   pname = "dvc";
-  version = "3.22.0";
+  version = "3.22.1";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "iterative";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-8L8ilGOPSfc6mW4JmmLM7VimwlFBQ6h5WIxaRnvWcm0=";
+    hash = "sha256-MhgY2ThNtRIeAdGvs4ET683yer5OIO0CMU0fJcYs7c4=";
   };
 
   pythonRelaxDeps = [
