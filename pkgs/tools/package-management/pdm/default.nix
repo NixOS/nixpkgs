189bf6b2a13a72b12d7062ccf8f42c142e1be9c9
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-26 07:15:59 +0000

    pdm: 2.9.1 -> 2.9.3

--- pkgs/tools/package-management/pdm/default.nix
+++ pkgs/tools/package-management/pdm/default.nix
@@ -31,13 +31,13 @@ in
 with python.pkgs;
 buildPythonApplication rec {
   pname = "pdm";
-  version = "2.9.1";
+  version = "2.9.3";
   format = "pyproject";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-/IAU3S/7cnF5qOwCQ8+sntOp3EU0i+HX+X0fKQrWD8s=";
+    hash = "sha256-CxGVtR6WMLWgsGPyffywEgy26ihPGkzZdaOibwhW0lM=";
   };
 
   nativeBuildInputs = [
