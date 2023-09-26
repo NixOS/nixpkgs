f39f83b43ecbad4cc149d822bd57b757022090f8
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-09-26 17:06:33 +0200

    heygpt: init at 0.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9175,6 +9175,8 @@ with pkgs;
 
   hey = callPackage ../tools/networking/hey { };
 
+  heygpt = callPackage ../tools/llm/heygpt { };
+
   hhpc = callPackage ../tools/misc/hhpc { };
 
   hiera-eyaml = callPackage ../tools/system/hiera-eyaml { };
