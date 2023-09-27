9a66522c819f46400376c8087c9343f7174fbd97
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-27 12:21:06 +0200

    python311Packages.pymyq: 3.1.6 -> 3.1.9
    Diff: https://github.com/Python-MyQ/Python-MyQ/compare/refs/tags/v3.1.6...v3.1.9
    
    Changelog: https://github.com/Python-MyQ/Python-MyQ/releases/tag/v3.1.9

--- pkgs/development/python-modules/pymyq/default.nix
+++ pkgs/development/python-modules/pymyq/default.nix
@@ -4,20 +4,27 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pkce
+, poetry-core
 , pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "pymyq";
-  version = "3.1.6";
+  version = "3.1.9";
+  pyproject = true;
+
   disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
-    owner = "arraylabs";
-    repo = pname;
+    owner = "Python-MyQ";
+    repo = "Python-MyQ";
     rev = "refs/tags/v${version}";
-    hash = "sha256-zhGCoZ7mkHlfDjEbQihtM23u+N6nfYsQhKmrloevzp8=";
+    hash = "sha256-7fGm7VGE9D6vmQkNmiB52+il3GE/rTqTqxqAORy88l4=";
   };
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
 
   propagatedBuildInputs = [
     aiohttp
@@ -34,8 +41,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Python wrapper for MyQ API";
-    homepage = "https://github.com/arraylabs/pymyq";
-    changelog = "https://github.com/arraylabs/pymyq/releases/tag/v${version}";
+    homepage = "https://github.com/Python-MyQ/Python-MyQ";
+    changelog = "https://github.com/Python-MyQ/Python-MyQ/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
