2f169dab5d17eda79f5e7017519df1a13712d68c
Ben Darwin <bcdarwin@gmail.com>
2023-09-27 15:41:38 -0400

    python310Packages.port-for: init at 0.7.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8602,6 +8602,8 @@ self: super: with self; {
 
   portend = callPackage ../development/python-modules/portend { };
 
+  port-for = callPackage ../development/python-modules/port-for { };
+
   portpicker = callPackage ../development/python-modules/portpicker { };
 
   posix_ipc = callPackage ../development/python-modules/posix_ipc { };
