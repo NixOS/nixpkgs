2f5eedf9f300e190e585047897cdda5d507c570d
Luiz Ferraz <luiz@lferraz.com>
2023-09-27 06:55:48 -0300

    turso-cli: 0.82.0 -> 0.85.1

--- pkgs/development/tools/turso-cli/default.nix
+++ pkgs/development/tools/turso-cli/default.nix
@@ -1,20 +1,27 @@
 {
   lib,
-  buildGoModule,
+  buildGo121Module,
   fetchFromGitHub,
 }:
-buildGoModule rec {
+buildGo121Module rec {
   pname = "turso-cli";
-  version = "0.82.0";
+  version = "0.85.1";
 
   src = fetchFromGitHub {
     owner = "tursodatabase";
     repo = "turso-cli";
     rev = "v${version}";
-    hash = "sha256-JFuD10EhR1/nmYPMnNsR/8PUR5ScvWyS+vhg7ZO5TpI=";
+    hash = "sha256-3JyAvxIKDVifGNJ5uOiIcaCvZ3U8u1fdvT6Xmvksnzc=";
   };
 
-  vendorHash = "sha256-Y/pg8+w6B1YQqaZ5wj8QZxiBHAG0Tf3Zec5WlVyA4eI=";
+  vendorHash = "sha256-OiLb+aLZfTLtxNWBJYyyrFXBGbsGKS5rjP+P6RQ94wE=";
+
+  # Build with production code
+  tags = ["prod"];
+  # Include version for `turso --version` reporting
+  preBuild = ''
+    echo "${version}" > internal/cmd/version.txt
+  '';
 
   # Test_setDatabasesCache fails due to /homeless-shelter: read-only file system error.
   doCheck = false;
@@ -22,6 +29,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "This is the command line interface (CLI) to Turso.";
     homepage = "https://turso.tech";
+    mainProgram = "turso";
     license = licenses.mit;
     maintainers = with maintainers; [ zestsystem kashw2 ];
   };
