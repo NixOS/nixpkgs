2809a43aa8f07e8a127cd3331d161b8d9134cd37
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-27 21:52:06 +0200

    python311Packages.ntc-templates: update disabled

--- pkgs/development/python-modules/ntc-templates/default.nix
+++ pkgs/development/python-modules/ntc-templates/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   version = "3.5.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "networktocode";
