fa579293d0c5b2c235c48701b8cbcb45b952bcc1
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-09-29 01:41:26 +0200

    python3Packages.bsuite: fix pythonImportsCheckPhase

--- pkgs/development/python-modules/bsuite/default.nix
+++ pkgs/development/python-modules/bsuite/default.nix
@@ -11,11 +11,13 @@
 , dm-env
 , plotnine
 , scikit-image
+, six
 , dm-tree
 , patsy
 , tensorflow-probability
 , dm-haiku
 , statsmodels
+, numpy
 , mizani
 , trfl
 , optax
@@ -34,19 +36,21 @@ let bsuite = buildPythonPackage rec {
     hash = "sha256-ak9McvXl7Nz5toUaPaRaJek9lurxiQiIW209GnZEjX0=";
   };
 
-  buildInputs = [
+  propagatedBuildInputs = [
     absl-py
     dm-env
     dm-tree
     frozendict
     gym
     matplotlib
+    numpy
     mizani
     pandas
     patsy
     plotnine
     scikit-image
     scipy
+    six
     statsmodels
     termcolor
   ];
