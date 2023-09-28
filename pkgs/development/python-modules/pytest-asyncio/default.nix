868eb7416adcd2c72fed79d06d6ffb1d5e052fff
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-09-29 01:42:13 +0200

    python3Packages.pytest-asyncio: fix pythonImportsCheckPhase

--- pkgs/development/python-modules/pytest-asyncio/default.nix
+++ pkgs/development/python-modules/pytest-asyncio/default.nix
@@ -47,7 +47,7 @@ buildPythonPackage rec {
   doCheck = false;
   passthru.tests.pytest = callPackage ./tests.nix {};
 
-  pythonImportsCheck = [
+  pythonImportsExtrasCheck = [
     "pytest_asyncio"
   ];
 
