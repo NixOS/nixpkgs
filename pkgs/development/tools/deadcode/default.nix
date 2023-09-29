bfa1949af3ecf3eb9e2742eb624ac84b2ce91906
Aaron Jheng <wentworth@outlook.com>
2023-09-29 01:56:03 +0000

    deadcode: unstable-2016-07-24 -> unstable-2019-04-27

--- pkgs/development/tools/deadcode/default.nix
+++ pkgs/development/tools/deadcode/default.nix
@@ -1,31 +1,32 @@
-{ buildGoPackage
-, lib
-, fetchFromGitHub
-}:
+{ buildGoModule, lib, fetchFromGitHub }:
 
-# TODO(yl): should we package https://github.com/remyoudompheng/go-misc instead of
-# the standalone extract of deadcode from it?
-buildGoPackage rec {
-  pname = "deadcode-unstable";
-  version = "2016-07-24";
-  rev = "210d2dc333e90c7e3eedf4f2242507a8e83ed4ab";
-
-  goPackagePath = "github.com/tsenart/deadcode";
-  excludedPackages = "cmd/fillswitch/test-fixtures";
+buildGoModule rec {
+  pname = "deadcode";
+  version = "unstable-2019-04-27";
 
   src = fetchFromGitHub {
-    inherit rev;
-
-    owner = "tsenart";
-    repo = "deadcode";
-    sha256 = "05kif593f4wygnrq2fdjhn7kkcpdmgjnykcila85d0gqlb1f36g0";
+    owner = "remyoudompheng";
+    repo = "go-misc";
+    rev = "2d6ac652a50e368c874b15d00ea5e346e240e906";
+    hash = "sha256-Kp6AlV86KjZARaxO9zcfIdT/zjZch5P3Nzmo/d36dBI=";
   };
+
+  vendorHash = "sha256-/Mnt7JeLoFiIBxQ/5zqTzea+CHaV7YXKPIA6BECmOdg=";
+
+  preBuild = ''
+    # Fix outdated go.mod & go.sum
+    cp ${./go.mod} go.mod
+    cp ${./go.sum} go.sum
+  '';
+
+  subPackages = [ "deadcode" ];
+
+  ldflags = [ "-s" "-w" ];
 
   meta = with lib; {
     description = "Very simple utility which detects unused declarations in a Go package";
     homepage = "https://github.com/remyoudompheng/go-misc/tree/master/deadcode";
     license = licenses.bsd3;
     maintainers = with maintainers; [ kalbasit ];
-    platforms = platforms.linux ++ platforms.darwin;
   };
 }
---
+++ pkgs/development/tools/deadcode/go.mod
@@ -0,0 +1,11 @@
+module github.com/remyoudompheng/go-misc
+
+go 1.12
+
+require (
+	github.com/remyoudompheng/go-dbus v0.0.0-20121104212943-b7232d34b1d5
+	github.com/remyoudompheng/go-liblzma v0.0.0-20190506200333-81bf2d431b96
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd
+	golang.org/x/text v0.3.0
+	golang.org/x/tools v0.0.0-20190228203856-589c23e65e65
+)
