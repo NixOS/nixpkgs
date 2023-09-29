86dbce6d0788d602df543d08dfd9b692e69739e3
Wietse de Vries <wietsedvries@gmail.com>
2023-09-29 11:46:13 +0200

    audiobookshelf: init module

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -119,6 +119,7 @@ in {
   atd = handleTest ./atd.nix {};
   atop = handleTest ./atop.nix {};
   atuin = handleTest ./atuin.nix {};
+  audiobookshelf = handleTest ./audiobookshelf.nix {};
   auth-mysql = handleTest ./auth-mysql.nix {};
   authelia = handleTest ./authelia.nix {};
   avahi = handleTest ./avahi.nix {};
