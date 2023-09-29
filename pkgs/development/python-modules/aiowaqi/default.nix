5397149059219732727307f51b32213b92b71dbe
Fabian Affolter <mail@fabian-affolter.ch>
2023-09-29 21:10:12 +0200

    python311Packages.aiowaqi: 1.1.0 -> 1.1.1
    Diff: https://github.com/joostlek/python-waqi/compare/refs/tags/v1.1.0...v1.1.1
    
    Changelog: https://github.com/joostlek/python-waqi/releases/tag/v1.1.1

--- pkgs/development/python-modules/aiowaqi/default.nix
+++ pkgs/development/python-modules/aiowaqi/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "aiowaqi";
-  version = "1.1.0";
+  version = "1.1.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.11";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "joostlek";
     repo = "python-waqi";
     rev = "refs/tags/v${version}";
-    hash = "sha256-CQCF59Tp0VE7PNHPdVzzZegLUNDkslzKapELDjZn1k4=";
+    hash = "sha256-JB1GtDLfz9FHVS7XEkHUCN2jwXvIwBBgoBisNuOpjL0=";
   };
 
   postPatch = ''
