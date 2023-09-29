62ef7214325f5bb61615f63a5898c56b0e68a673
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-09-29 12:30:20 +0800

    cargo-expand: migrate to by-name

--- pkgs/by-name/ca/cargo-expand/package.nix
+++ pkgs/by-name/ca/cargo-expand/package.nix
@@ -11,17 +11,17 @@ rustPlatform.buildRustPackage rec {
     owner = "dtolnay";
     repo = pname;
     rev = version;
-    sha256 = "sha256-ZOoMWvtuLDhbJu+qzVPHGTCqh2b3PHUggfxNtUW1DoU=";
+    hash = "sha256-ZOoMWvtuLDhbJu+qzVPHGTCqh2b3PHUggfxNtUW1DoU=";
   };
 
   cargoHash = "sha256-cTz9ZR+79yPqTaDqXjSlqXd7NxHDl6Q75N26z+vi7ck=";
 
   meta = with lib; {
-    description = "A utility and Cargo subcommand designed to let people expand macros in their Rust source code";
-    mainProgram = "cargo-expand";
+    description = "Cargo subcommand to show result of macro expansion";
     homepage = "https://github.com/dtolnay/cargo-expand";
     changelog = "https://github.com/dtolnay/cargo-expand/releases/tag/${version}";
     license = with licenses; [ mit asl20 ];
     maintainers = with maintainers; [ figsoda xrelkd ];
+    mainProgram = "cargo-expand";
   };
 }
