788e889aad2384a1c358c2ce47aa37a0ee80827d
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-09-29 12:02:29 +0800

    wit-bindgen: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14523,8 +14523,6 @@ with pkgs;
 
   wimboot = callPackage ../tools/misc/wimboot { };
 
-  wit-bindgen = callPackage ../tools/misc/wit-bindgen { };
-
   wire = callPackage ../development/tools/wire { };
 
   wireguard-tools = callPackage ../tools/networking/wireguard-tools { };
