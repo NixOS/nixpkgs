1a7003f3ab4be5038916863b87b5ba527e09b064
Vonfry <mail@vonfry.name>
2023-09-30 16:10:58 +0800

    fcitx5-rime: 5.1.1 -> 5.1.2

--- pkgs/tools/inputmethods/fcitx5/fcitx5-rime.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-rime.nix
@@ -14,11 +14,11 @@
 
 stdenv.mkDerivation rec {
   pname = "fcitx5-rime";
-  version = "5.1.1";
+  version = "5.1.2";
 
   src = fetchurl {
     url = "https://download.fcitx-im.org/fcitx5/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-qo0m/asTranm70PHPLwWCn/jX+FWNEGRKBRNNW+B28A=";
+    hash = "sha256-0WcBM6kOKATuh6I8yEzl+HkK5/k4Ku6brZ+s1ncKlpw=";
   };
 
   cmakeFlags = [
