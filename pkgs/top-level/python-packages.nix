71affb0d731d5244956830d46237bb9cb6bfdbcb
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-09-30 22:18:31 +0200

    python310Packages.pytest-spec: init at unstable-2023-06-04

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10417,6 +10417,8 @@ self: super: with self; {
 
   pytest-shutil = callPackage ../development/python-modules/pytest-shutil { };
 
+  pytest-spec = callPackage ../development/python-modules/pytest-spec { };
+
   python-status = callPackage ../development/python-modules/python-status { };
 
   python-string-utils = callPackage ../development/python-modules/python-string-utils { };
