1d953af53e724c3aa3ff3bbd6b5a12cfa978518c
Markus Kowalewski <markus.kowalewski@gmail.com>
2023-09-30 16:05:42 +0200

    cp2k: fix libxc includes (use getDev)

--- pkgs/applications/science/chemistry/cp2k/default.nix
+++ pkgs/applications/science/chemistry/cp2k/default.nix
@@ -73,7 +73,7 @@ in stdenv.mkDerivation rec {
                  -ftree-vectorize -funroll-loops -msse2 \
                  -std=f2008 \
                  -fopenmp -ftree-vectorize -funroll-loops \
-                 -I${libxc}/include -I${lib.getDev libxsmm}/include \
+                 -I${lib.getDev libxc}/include -I${lib.getDev libxsmm}/include \
                  -I${libint}/include ${lib.optionalString enableElpa "$(pkg-config --variable=fcflags elpa)"}
     LIBS       = -lfftw3 -lfftw3_threads \
                  -lscalapack -lblas -llapack \
