3782b3b5d954c660abc3cad1d799edc033b61691
Elliot Speck (Arcayr) <git@elliot.pro>
2023-10-01 04:53:02 +1100

    nixos/virtualisation: add hostname option to oci-containers.

--- nixos/modules/virtualisation/oci-containers.nix
+++ nixos/modules/virtualisation/oci-containers.nix
@@ -203,6 +203,13 @@ let
           '';
         };
 
+        hostname = mkOption {
+          type = with types; nullOr str;
+          default = null;
+          description = lib.mdDoc "The hostname of the container.";
+          example = "hello-world";
+        };
+
         extraOptions = mkOption {
           type = with types; listOf str;
           default = [];
@@ -266,6 +273,8 @@ let
       "--log-driver=${container.log-driver}"
     ] ++ optional (container.entrypoint != null)
       "--entrypoint=${escapeShellArg container.entrypoint}"
+      ++ optional (container.hostname != null)
+      "--hostname=${escapeShellArg container.hostname}"
       ++ lib.optionals (cfg.backend == "podman") [
         "--cidfile=/run/podman-${escapedName}.ctr-id"
         "--cgroups=no-conmon"
