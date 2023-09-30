10fa3455b12301bdf3a1a144b46d264bf0b72546
R. Ryantm <ryantm-bot@ryantm.com>
2023-09-30 22:54:21 +0000

    sarasa-gothic: 0.41.10 -> 0.42.0

--- pkgs/data/fonts/sarasa-gothic/default.nix
+++ pkgs/data/fonts/sarasa-gothic/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "sarasa-gothic";
-  version = "0.41.10";
+  version = "0.42.0";
 
   src = fetchurl {
     # Use the 'ttc' files here for a smaller closure size.
     # (Using 'ttf' files gives a closure size about 15x larger, as of November 2021.)
     url = "https://github.com/be5invis/Sarasa-Gothic/releases/download/v${version}/sarasa-gothic-ttc-${version}.7z";
-    hash = "sha256-RxXLjlWJEWaWph8jz1L1zZABYOm75MoxcI0cZJGBDvg=";
+    hash = "sha256-BZWOQQhkK+bQhS5MFIJ81unGDevp8WptPA/dOmf12xs=";
   };
 
   sourceRoot = ".";
