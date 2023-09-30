e2484fe737e7a8bfc3a873dea5874ab9aba22685
Christina Sørensen <christina@cafkafk.com>
2023-09-30 18:30:29 +0200

    ast-grep: fix darwin and aarch64_linux
    `test::test_load_parser` and `test::test_register_lang` broke by 0.12.1
    update (https://github.com/NixOS/nixpkgs/pull/257385).  Please check if
    this is fixed in future updates of the package
    
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>
    Co-authored-by: Artturi <Artturin@artturin.com>

--- pkgs/by-name/as/ast-grep/package.nix
+++ pkgs/by-name/as/ast-grep/package.nix
@@ -26,10 +26,14 @@ rustPlatform.buildRustPackage rec {
     # disable flaky test
     "--skip=test::test_load_parser_mac"
 
-
     # BUG: Broke by 0.12.1 update (https://github.com/NixOS/nixpkgs/pull/257385)
     # Please check if this is fixed in future updates of the package
     "--skip=verify::test_case::tests::test_unmatching_id"
+  ] ++ lib.optionals (with stdenv.hostPlatform; (isDarwin && isx86_64) || (isLinux && isAarch64)) [
+    # x86_64-darwin: source/benches/fixtures/json-mac.so\' (no such file), \'/private/tmp/nix-build-.../source/benches/fixtures/json-mac.so\' (mach-o file, but is an incompatible architecture (have \'arm64\', need \'x86_64h\' or \'x86_64\'))" })
+    # aarch64-linux: /build/source/benches/fixtures/json-linux.so: cannot open shared object file: No such file or directory"
+    "--skip=test::test_load_parser"
+    "--skip=test::test_register_lang"
   ];
 
   meta = with lib; {
