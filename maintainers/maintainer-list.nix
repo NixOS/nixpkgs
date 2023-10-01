0f5ee019be4cb7153248c53b1b75bae45f7a9ee0
mirrorwitch <mirrorwitch@transmom.love>
2023-10-01 22:38:36 +0200

    maintainers: add mirrorwitch

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11560,6 +11560,15 @@
     githubId = 1776903;
     name = "Andrew Abbott";
   };
+  mirrorwitch = {
+    email = "mirrorwitch@transmom.love";
+    github = "mirrorwitch";
+    githubId = 146672255;
+    name = "mirrorwitch";
+    keys = [{
+        fingerprint = "C3E7 F8C4 9CBC 9320 D360  B117 8516 D0FA 7D8F 58FC";
+    }];
+  };
   Misaka13514 = {
     name = "Misaka13514";
     email = "Misaka13514@gmail.com";
