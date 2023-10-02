0f90109665791cbfd1d97bf0ecdc68785d717b0f
Malo Bourgon <mbourgon@gmail.com>
2023-10-02 14:02:11 -0700

    signalbackup-tools: 20230929 -> 20231002-1
    Diff: https://github.com/bepaald/signalbackup-tools/compare/20230929...20231002-1

--- pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
+++ pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
@@ -2,13 +2,13 @@
 
 (if stdenv.isDarwin then darwin.apple_sdk_11_0.llvmPackages_14.stdenv else stdenv).mkDerivation rec {
   pname = "signalbackup-tools";
-  version = "20230929";
+  version = "20231002-1";
 
   src = fetchFromGitHub {
     owner = "bepaald";
     repo = pname;
     rev = version;
-    hash = "sha256-5U8znPKCe4auQRfysVUzXawnvnSj86MD3J2vfAwxofE=";
+    hash = "sha256-aZ3KuUjBsgww7CFdSuYMurLc5sDruEX4xuQWDBaCfPk=";
   };
 
   postPatch = ''
