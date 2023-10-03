c6031da1cc8d1d6ec0d11ba2b532d1cebfc43397
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-10-02 23:10:05 -0700

    python3Packages.mypy-boto3-cloudtrail-data: init at 1.28.36

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -150,6 +150,8 @@ rec {
 
   mypy-boto3-cloudtrail = buildMypyBoto3Package "cloudtrail" "1.28.36" "sha256-YUrRZ53Wdd4CCjq9VYRkAIGxu2wYTaqXF7Fp4DA7jM4=";
 
+  mypy-boto3-cloudtrail-data = buildMypyBoto3Package "cloudtrail-data" "1.28.36" "sha256-aUZYh0fTrcv1d56HRdu7u4CzAMiCvVlN/tKY1e7piLM=";
+
   mypy-boto3-cognito-idp = buildMypyBoto3Package "cognito-idp" "1.28.36" "sha256-pnLO62LZvr4sJsye3gWJROY+xHikSe7dX8erBTRXrPc=";
 
   mypy-boto3-ebs = buildMypyBoto3Package "ebs" "1.28.36" "sha256-w9OLKJAn9UBnA7x+uedhplSV8plZRYlBpviU9Gv1Ny8=";
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7189,6 +7189,8 @@ self: super: with self; {
 
     mypy-boto3-cloudtrail
 
+    mypy-boto3-cloudtrail-data
+
     mypy-boto3-cognito-idp
 
     mypy-boto3-ebs
