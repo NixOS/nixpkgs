c935aa00782785ac4e8934a661fc5a9ae177e115
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-10-02 23:24:05 -0700

    python3Packages.mypy-boto3-s3control: init at 1.28.36

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -616,6 +616,8 @@ rec {
 
   mypy-boto3-s3 = buildMypyBoto3Package "s3" "1.28.55" "sha256-sAiAn0SOdAdQEtT8VLAXbeC09JvDjjneMMoOdk63UFY=";
 
+  mypy-boto3-s3control = buildMypyBoto3Package "s3control" "1.28.36" "sha256-ZgJtq2jXRtgUXQrTfIZKSbF7TMTw/bV+bGlxte7kVA8=";
+
   mypy-boto3-xray = buildMypyBoto3Package "xray" "1.28.47" "sha256-1OiTpbaBm2aAls4A7ZaZBNAM8DTRuQcwNKJDq3lOKMY=";
 
 }
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7655,6 +7655,8 @@ self: super: with self; {
 
     mypy-boto3-s3
 
+    mypy-boto3-s3control
+
     mypy-boto3-xray
 
   ;
