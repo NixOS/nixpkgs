782c6e35bfac6240708784d7d2b664d963d7fb3e
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-10-02 23:20:16 -0700

    python3Packages.mypy-boto3-mgh: init at 1.28.36

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -484,6 +484,8 @@ rec {
 
   mypy-boto3-meteringmarketplace = buildMypyBoto3Package "meteringmarketplace" "1.28.36" "sha256-LiZj7Dltu+C717k1Aywtkk1faeSUAFYCMiOe5Mgoy4A=";
 
+  mypy-boto3-mgh = buildMypyBoto3Package "mgh" "1.28.36" "sha256-7AGJAF8dh5FQz46IRinvrTX/PEC8cBkS2G/LMndjNS4=";
+
   mypy-boto3-s3 = buildMypyBoto3Package "s3" "1.28.55" "sha256-sAiAn0SOdAdQEtT8VLAXbeC09JvDjjneMMoOdk63UFY=";
 
   mypy-boto3-xray = buildMypyBoto3Package "xray" "1.28.47" "sha256-1OiTpbaBm2aAls4A7ZaZBNAM8DTRuQcwNKJDq3lOKMY=";
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7523,6 +7523,8 @@ self: super: with self; {
 
     mypy-boto3-meteringmarketplace
 
+    mypy-boto3-mgh
+
     mypy-boto3-s3
 
     mypy-boto3-xray
