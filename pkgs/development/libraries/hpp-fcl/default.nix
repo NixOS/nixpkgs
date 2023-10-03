3fe05ddf40bd11ed85c448b2e3c7f8fa9c10585f
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-03 10:52:32 +0000

    hpp-fcl: 2.3.5 -> 2.3.6

--- pkgs/development/libraries/hpp-fcl/default.nix
+++ pkgs/development/libraries/hpp-fcl/default.nix
@@ -14,14 +14,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "hpp-fcl";
-  version = "2.3.5";
+  version = "2.3.6";
 
   src = fetchFromGitHub {
     owner = "humanoid-path-planner";
     repo = finalAttrs.pname;
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-jVIYP0yA1oSsUMN4vtrkfawj9Q2MwNjSrwDBTvGErg8=";
+    hash = "sha256-Y6ATYXsV8hH22XiXyvacuUhHTuNCzObPlxNX2vZGghM=";
   };
 
   strictDeps = true;
