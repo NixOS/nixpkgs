ed30e5fc51547ec5ef15cd4bf6b36459df4c3b46
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-10-02 23:13:42 -0700

    python3Packages.mypy-boto3-elbv2: init at 1.28.36

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -270,6 +270,8 @@ rec {
 
   mypy-boto3-elb = buildMypyBoto3Package "elb" "1.28.36" "sha256-c9Re0WZOn+LaK58VJk+YlI3YWn/wrgT+/TdqrOisBnM=";
 
+  mypy-boto3-elbv2 = buildMypyBoto3Package "elbv2" "1.28.42" "sha256-NXyhMxuIbuyyDifzBicvDNQB3ysbVx+azsqpTBhfnZ8=";
+
   mypy-boto3-s3 = buildMypyBoto3Package "s3" "1.28.55" "sha256-sAiAn0SOdAdQEtT8VLAXbeC09JvDjjneMMoOdk63UFY=";
 
   mypy-boto3-xray = buildMypyBoto3Package "xray" "1.28.47" "sha256-1OiTpbaBm2aAls4A7ZaZBNAM8DTRuQcwNKJDq3lOKMY=";
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7309,6 +7309,8 @@ self: super: with self; {
 
     mypy-boto3-elb
 
+    mypy-boto3-elbv2
+
     mypy-boto3-s3
 
     mypy-boto3-xray
