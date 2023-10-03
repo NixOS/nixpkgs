21605469c0b7f35dcfbf1aab1394175d9597000a
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-10-02 23:09:55 -0700

    python3Packages.mypy-boto3-cloudsearch: init at 1.28.36

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -144,6 +144,8 @@ rec {
 
   mypy-boto3-cloudhsmv2 = buildMypyBoto3Package "cloudhsmv2" "1.28.36" "sha256-mxvW/ge2gEkGF0rz4LJZCkEIveNGkt9ud5bqs4WhlBs=";
 
+  mypy-boto3-cloudsearch = buildMypyBoto3Package "cloudsearch" "1.28.36" "sha256-fEGcYuWWDHOalygjig2qdMn8A3T4vBYZIbZRHtRRQns=";
+
   mypy-boto3-cognito-idp = buildMypyBoto3Package "cognito-idp" "1.28.36" "sha256-pnLO62LZvr4sJsye3gWJROY+xHikSe7dX8erBTRXrPc=";
 
   mypy-boto3-ebs = buildMypyBoto3Package "ebs" "1.28.36" "sha256-w9OLKJAn9UBnA7x+uedhplSV8plZRYlBpviU9Gv1Ny8=";
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7183,6 +7183,8 @@ self: super: with self; {
 
     mypy-boto3-cloudhsmv2
 
+    mypy-boto3-cloudsearch
+
     mypy-boto3-cognito-idp
 
     mypy-boto3-ebs
