7ebb100e65413bef24460d417dd0a8bf319c85ec
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-10-02 23:10:34 -0700

    python3Packages.mypy-boto3-codeguru-security: init at 1.28.36

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -166,6 +166,8 @@ rec {
 
   mypy-boto3-codeguru-reviewer = buildMypyBoto3Package "codeguru-reviewer" "1.28.36" "sha256-xUkKfYEBmqA0D8RKxbf8VF0XeYOgwNWh1y7ORnj1VpM=";
 
+  mypy-boto3-codeguru-security = buildMypyBoto3Package "codeguru-security" "1.28.36" "sha256-EU6VTdhzWLofMEFF0vOXTDpJI/BkC3FpSAz1rbtbFWA=";
+
   mypy-boto3-cognito-idp = buildMypyBoto3Package "cognito-idp" "1.28.36" "sha256-pnLO62LZvr4sJsye3gWJROY+xHikSe7dX8erBTRXrPc=";
 
   mypy-boto3-ebs = buildMypyBoto3Package "ebs" "1.28.36" "sha256-w9OLKJAn9UBnA7x+uedhplSV8plZRYlBpviU9Gv1Ny8=";
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7205,6 +7205,8 @@ self: super: with self; {
 
     mypy-boto3-codeguru-reviewer
 
+    mypy-boto3-codeguru-security
+
     mypy-boto3-cognito-idp
 
     mypy-boto3-ebs
