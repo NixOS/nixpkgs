6f5b030e40f381c6f13c4a4dbd8fe249ec4ead0a
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2023-10-02 23:19:28 -0700

    python3Packages.mypy-boto3-marketplacecommerceanalytics: init at 1.28.36

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -458,6 +458,8 @@ rec {
 
   mypy-boto3-marketplace-entitlement = buildMypyBoto3Package "marketplace-entitlement" "1.28.36" "sha256-9pTcu/s4RykpnM/FsI/VuBLmLCz1/AVpWSTPORFVraY=";
 
+  mypy-boto3-marketplacecommerceanalytics = buildMypyBoto3Package "marketplacecommerceanalytics" "1.28.36" "sha256-8omV1nyC83MRbAEwCOzDvyuTHARxCDJtMCkIJ76/42Q=";
+
   mypy-boto3-s3 = buildMypyBoto3Package "s3" "1.28.55" "sha256-sAiAn0SOdAdQEtT8VLAXbeC09JvDjjneMMoOdk63UFY=";
 
   mypy-boto3-xray = buildMypyBoto3Package "xray" "1.28.47" "sha256-1OiTpbaBm2aAls4A7ZaZBNAM8DTRuQcwNKJDq3lOKMY=";
--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7497,6 +7497,8 @@ self: super: with self; {
 
     mypy-boto3-marketplace-entitlement
 
+    mypy-boto3-marketplacecommerceanalytics
+
     mypy-boto3-s3
 
     mypy-boto3-xray
