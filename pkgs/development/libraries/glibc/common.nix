1daafd9beb138f3700693509152dd618a5f4889b
edef <edef@edef.eu>
2023-10-04 10:55:28 +0000

    glibc: 2.37-39 -> 2.37-45
    Fixes CVE-2023-4911.
    
    Change-Id: I154c79a7c6822051903ef15c547c3567691b1457

--- pkgs/development/libraries/glibc/common.nix
+++ pkgs/development/libraries/glibc/common.nix
@@ -44,7 +44,7 @@
 
 let
   version = "2.37";
-  patchSuffix = "-39";
+  patchSuffix = "-45";
   sha256 = "sha256-Ilfv8RGhgV109GhW2q9AsBnB5VMVbGnUi6DL/Bu5GkM=";
 in
 
@@ -60,7 +60,7 @@ stdenv.mkDerivation ({
     [
       /* No tarballs for stable upstream branch, only https://sourceware.org/git/glibc.git and using git would complicate bootstrapping.
           $ git fetch --all -p && git checkout origin/release/2.37/master && git describe
-          glibc-2.37-39-g6529a7466c
+          glibc-2.37-45-gb4e23c75ae
           $ git show --minimal --reverse glibc-2.37.. | gzip -9n --rsyncable - > 2.37-master.patch.gz
 
          To compare the archive contents zdiff can be used.
