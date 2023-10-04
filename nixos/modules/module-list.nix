38c38bb690cf35799c4c2616566b7e8b7a85dc24
Majiir Paktu <majiir@nabaal.net>
2023-10-03 21:09:52 -0400

    nixos/wireguard-networkd: init
    Adds a networkd backend for the networking.wireguard options.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1169,6 +1169,7 @@
   ./services/networking/wg-quick.nix
   ./services/networking/wgautomesh.nix
   ./services/networking/wireguard.nix
+  ./services/networking/wireguard-networkd.nix
   ./services/networking/wpa_supplicant.nix
   ./services/networking/wstunnel.nix
   ./services/networking/x2goserver.nix
