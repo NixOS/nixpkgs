a06b7a48f0db58748e6efdfc816d89618557bfe7
Michael Brunner <michaelbrunn3r@gmail.com>
2023-10-04 11:34:47 +0200

    maintainers: add michaelBrunner

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11339,6 +11339,12 @@
     github = "michaelBelsanti";
     githubId = 62124625;
   };
+  michaelBrunner = {
+    email = "michael.brunn3r@gmail.com";
+    name = "Michael Brunner";
+    github = "MichaelBrunn3r";
+    githubId = 19626539;
+  };
   michaelCTS = {
     email = "michael.vogel@cts.co";
     name = "Michael Vogel";
