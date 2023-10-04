acd7395632460995e0b9abf88f65e80319c2a85c
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-04 02:00:46 +0000

    lsp-plugins: 1.2.10 -> 1.2.11

--- pkgs/applications/audio/lsp-plugins/default.nix
+++ pkgs/applications/audio/lsp-plugins/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "lsp-plugins";
-  version = "1.2.10";
+  version = "1.2.11";
 
   src = fetchurl {
     url = "https://github.com/sadko4u/${pname}/releases/download/${version}/${pname}-src-${version}.tar.gz";
-    sha256 = "sha256-2Yf+4TYGWF/AMI1kNvVOx9g6CSIoeZKY63qC/zJNilc=";
+    sha256 = "sha256-9zLs1J7rZkMaVQxOwihjCsKSLyb9q64pTZLVg/UVf2o=";
   };
 
   outputs = [ "out" "dev" "doc" ];
