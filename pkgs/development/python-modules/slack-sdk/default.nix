39576501a2d2aca9d3ff345bd08ad9f7157ea397
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-04 18:27:26 +0200

    python311Packages.slack-sdk: 3.22.0 -> 3.23.0
    Diff: https://github.com/slackapi/python-slack-sdk/compare/refs/tags/v3.22.0...v3.23.0
    
    Changelog: https://github.com/slackapi/python-slack-sdk/releases/tag/v3.23.0

--- pkgs/development/python-modules/slack-sdk/default.nix
+++ pkgs/development/python-modules/slack-sdk/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "slack-sdk";
-  version = "3.22.0";
+  version = "3.23.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "slackapi";
     repo = "python-slack-sdk";
     rev = "refs/tags/v${version}";
-    hash = "sha256-PRJgOAC1IJjQb1c4FAbpV8bxOPL9PTbAxNXo2MABRzc=";
+    hash = "sha256-OsPwLOnmN3kvPmbM6lOaiTWwWvy7b9pgn1X536dCkWk=";
   };
 
   propagatedBuildInputs = [
