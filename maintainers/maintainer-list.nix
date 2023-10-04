8c159a4342f8adf1bfcb67997c31552a401d3603
Jonathan Davies <jpds@protonmail.com>
2023-10-05 00:05:15 +0100

    maintainer-list.nix: Added jpds.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10175,6 +10175,11 @@
     githubId = 1918771;
     name = "Joe Doyle";
   };
+  jpds = {
+    github = "jpds";
+    githubId = 29158971;
+    name = "Jonathan Davies";
+  };
   jpentland = {
     email = "joe.pentland@gmail.com";
     github = "jpentland";
