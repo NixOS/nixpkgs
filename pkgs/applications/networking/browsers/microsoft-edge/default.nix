fd1a8e267272ff2073ae5e8af8c3444ac8a47df3
Rhys Davies <rhys@memes.nz>
2023-10-05 15:51:28 +1300

    microsoft-edge: 117.0.2045.47 -> 117.0.2045.55

--- pkgs/applications/networking/browsers/microsoft-edge/default.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/default.nix
@@ -1,20 +1,20 @@
 {
   stable = import ./browser.nix {
     channel = "stable";
-    version = "117.0.2045.47";
+    version = "117.0.2045.55";
     revision = "1";
-    sha256 = "sha256-h4iw+H8f62JEih1tWTpjxNC9+wu3hHQOM2VJid1kHNQ=";
+    sha256 = "sha256-9z+SxiM1ODTq3kalMeeV+W5QZ2VY1OB+KpiXgSxNvEM=";
   };
   beta = import ./browser.nix {
     channel = "beta";
-    version = "118.0.2088.17";
+    version = "118.0.2088.24";
     revision = "1";
-    sha256 = "sha256-3Z37M2ZQRJ5uA7NcinMlF1XEsYVv9A+ppPZZf34ye6Q=";
+    sha256 = "sha256-CrsFTGvD8WE4CcZ3Fw97f5yGzWYiFNuMfwC6WM5nATQ=";
   };
   dev = import ./browser.nix {
     channel = "dev";
-    version = "119.0.2116.0";
+    version = "119.0.2132.0";
     revision = "1";
-    sha256 = "sha256-raLRFSHZyHaxKi6EG62VIbcW29HTjTnBFw7IJFVbm5I=";
+    sha256 = "sha256-sBR1/N8k+4oaGcb2/ZLAeMGCuSuGOcSxvP3ofGfFYDU=";
   };
 }
