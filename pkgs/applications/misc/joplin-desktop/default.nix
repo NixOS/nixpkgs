f9294b2952ee5d4c65d00aad04fb76ee3fef44db
Steven Kou <skykinetic@stevenkou.xyz>
2023-10-04 22:23:50 -0400

    joplin-desktop: add window decorations for wayland

--- pkgs/applications/misc/joplin-desktop/default.nix
+++ pkgs/applications/misc/joplin-desktop/default.nix
@@ -55,7 +55,7 @@ let
       mv $out/bin/{${name},${pname}}
       source "${makeWrapper}/nix-support/setup-hook"
       wrapProgram $out/bin/${pname} \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland}}"
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations}}"
       install -Dm444 ${appimageContents}/@joplinapp-desktop.desktop -t $out/share/applications
       install -Dm444 ${appimageContents}/@joplinapp-desktop.png -t $out/share/pixmaps
       substituteInPlace $out/share/applications/@joplinapp-desktop.desktop \
