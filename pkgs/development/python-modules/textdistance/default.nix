43e2f8dcbdcebcb30af8454ae8e4780e77585454
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-05 09:49:18 +0200

    python310Packages.textdistance: add format
    - disable on unsupported Python releases

--- pkgs/development/python-modules/textdistance/default.nix
+++ pkgs/development/python-modules/textdistance/default.nix
@@ -1,11 +1,15 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "textdistance";
   version = "4.6.0";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
