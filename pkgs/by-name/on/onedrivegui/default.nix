692dbe30c1fccd0c9ad14d94d518fb7c1d405000
John Garcia <jgarcia3788@yahoo.co.uk>
2023-10-05 17:59:05 +0100

    onedrivegui: init at 1.0.3

---
+++ pkgs/by-name/on/onedrivegui/default.nix
@@ -0,0 +1,70 @@
+{ lib
+, python3Packages
+, fetchFromGitHub
+, copyDesktopItems
+, onedrive
+, makeDesktopItem
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "OneDriveGUI";
+  version = "1.0.3";
+
+  src = fetchFromGitHub {
+    owner = "bpozdena";
+    repo = "OneDriveGUI";
+    rev = "v${version}";
+    hash = "sha256-HutziAzhIDYP8upNPieL2GNrxPBHUCVs09FFxdSqeBs=";
+  };
+
+  nativeBuildInputs = [ copyDesktopItems ];
+
+  propagatedBuildInputs = [ onedrive ] ++
+  (with python3Packages; [
+    pyside6
+    requests
+  ]);
+
+  doCheck = false; # No tests defined
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "OneDriveGUI";
+      exec = "onedrivegui";
+      desktopName = "OneDriveGUI";
+      comment = "OneDrive GUI Client";
+      type = "Application";
+      icon = "OneDriveGUI";
+      terminal = false;
+      categories = [ "Utility" ];
+    })
+  ];
+
+    preBuild = ''
+    cat > setup.py << EOF
+    from setuptools import setup
+    setup(
+      name='onedrivegui',
+      version='$version',
+      scripts=[
+        'src/OneDriveGUI.py',
+      ],
+    )
+    EOF
+  '';
+
+  postInstall = ''
+    mkdir $out/lib/OneDriveGUI && cp -r src/{resources,ui} $out/lib/OneDriveGUI/
+    ln -s $out/lib/OneDriveGUI/{resources,ui} $out/bin/
+    install -Dm644 src/resources/images/OneDriveGUI.png "$out/share/icons/hicolor/48x48/apps/OneDriveGUI.png"
+    mv -v $out/bin/OneDriveGUI.py $out/bin/onedrivegui
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/bpozdena/OneDriveGUI";
+    description = "A simple GUI for Linux OneDrive Client, with multi-account support.";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ jgarcia ];
+    platforms = platforms.linux;
+  };
+}
