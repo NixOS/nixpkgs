00c05bbc65cfdabb7ab09a2e37c78b86381e94ab
maralorn <mail@maralorn.de>
2023-10-06 17:51:51 +0200

    haskell.packages.ghc810.haskell-language-server: Remove

--- pkgs/top-level/release-haskell.nix
+++ pkgs/top-level/release-haskell.nix
@@ -491,7 +491,9 @@ let
       funcmp = released;
       haskell-language-server = lib.subtractLists [
         # Support ceased as of 1.9.0.0
-        compilerNames.ghc884
+        compilerNames.ghc88
+        # Support ceased as of 2.3.0.0
+        compilerNames.ghc810
       ] released;
       hoogle = lib.subtractLists [
         compilerNames.ghc962
