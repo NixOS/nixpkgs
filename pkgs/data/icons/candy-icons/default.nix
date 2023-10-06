a8ee2804ee6a8794338c33c8b6264f7fd0cab897
clr-cera <clrcera05@gmail.com>
2023-10-05 23:45:40 -0300

    fix dir mistake

--- pkgs/data/icons/candy-icons/default.nix
+++ pkgs/data/icons/candy-icons/default.nix
@@ -3,7 +3,6 @@
   lib,
   fetchFromGitHub,
 }:
-
 stdenv.mkDerivation {
   pname = "candy-icons";
   version = "2023-10-03";
@@ -14,10 +13,10 @@ stdenv.mkDerivation {
     rev = "bcf24f3308cc5e39f3e7d1bb43cb26f627f9ba8f";
     sha256 = "sha256-NCXxH04GvrPE5mQO7JrU6SkjA+BaZokhyjKsm8m9p3g=";
   };
-
+  
   installPhase = ''
-    mkdir -p $out/share/icons
-    cp -r . $out/share/icons
+    mkdir -p $out/share/icons/candy-icons
+    cp -r . $out/share/icons/candy-icons
   '';
 
   meta = with lib; {
