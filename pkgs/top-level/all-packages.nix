aa934c226b1f5650b0fe3223a1273a7de9a027cd
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-10-06 12:46:47 +0200

    lndinit: init at 0.1.5-beta

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37299,6 +37299,8 @@ with pkgs;
 
   lndconnect = callPackage ../applications/blockchains/lndconnect { };
 
+  lndinit = callPackage ../applications/blockchains/lndinit { };
+
   lndhub-go = callPackage ../applications/blockchains/lndhub-go { };
 
   lndmanage = callPackage ../applications/blockchains/lndmanage { };
