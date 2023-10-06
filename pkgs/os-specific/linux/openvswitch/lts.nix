567e956865d52f73e3b421d36e5d3c5cb1dcd867
Mohammed Naser <mnaser@vexxhost.com>
2023-10-06 18:28:20 -0400

    openvswitch-lts: 2.17.6 -> 2.17.7

--- pkgs/os-specific/linux/openvswitch/lts.nix
+++ pkgs/os-specific/linux/openvswitch/lts.nix
@@ -1,4 +1,4 @@
 import ./generic.nix {
-  version = "2.17.6";
-  hash = "sha256-dNqvK+c0iuXdQBe6RbjaxlNB8Vn0+0paecVC/tQQENk=";
+  version = "2.17.7";
+  hash = "sha256-w8bKjjSS6liMKc7/uWlMZm/euTbeW/0a0JFsUgJ4s4U=";
 }
