23553f56e728638043c03c569fa080d414f22c20
natsukium <tomoya.otabi@gmail.com>
2023-10-07 18:34:03 +0900

    python311Packages.zope-deprecation: refactor

--- pkgs/development/python-modules/zope-deprecation/default.nix
+++ pkgs/development/python-modules/zope-deprecation/default.nix
@@ -1,12 +1,14 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, zope_testing
+, setuptools
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "zope-deprecation";
   version = "4.4.0";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "zope.deprecation";
@@ -14,12 +16,27 @@ buildPythonPackage rec {
     sha256 = "0d453338f04bacf91bbfba545d8bcdf529aa829e67b705eac8c1a7fdce66e2df";
   };
 
-  buildInputs = [ zope_testing ];
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pytestFlagsArray = [
+    "src/zope/deprecation/tests.py"
+  ];
+
+  pythonImportsCheck = [
+    "zope.deprecation"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/zopefoundation/zope.deprecation";
     description = "Zope Deprecation Infrastructure";
-    license = licenses.zpl20;
+    changelog = "https://github.com/zopefoundation/zope.deprecation/blob/${version}/CHANGES.rst";
+    license = licenses.zpl21;
     maintainers = with maintainers; [ domenkozar ];
   };
 
