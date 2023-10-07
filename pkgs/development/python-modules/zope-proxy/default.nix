afb49a7626a2e11e4abbac8225a0da54905d9efe
natsukium <tomoya.otabi@gmail.com>
2023-10-07 22:39:51 +0900

    python311Packages.zope-proxy: refactor

--- pkgs/development/python-modules/zope-proxy/default.nix
+++ pkgs/development/python-modules/zope-proxy/default.nix
@@ -1,12 +1,17 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
+, setuptools
 , zope_interface
 }:
 
 buildPythonPackage rec {
   pname = "zope-proxy";
   version = "5.0.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "zope.proxy";
@@ -14,16 +19,24 @@ buildPythonPackage rec {
     hash = "sha256-b6RMl6QStNxR4vX9Tcc8W9SZ01KA+IzSvNJviuHkV3s=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [ zope_interface ];
 
   # circular deps
   doCheck = false;
 
+  pythonImportsCheck = [
+    "zope.proxy"
+  ];
+
   meta = with lib; {
     homepage = "https://github.com/zopefoundation/zope.proxy";
     description = "Generic Transparent Proxies";
-    license = licenses.zpl20;
+    changelog = "https://github.com/zopefoundation/zope.proxy/blob/${version}/CHANGES.rst";
+    license = licenses.zpl21;
     maintainers = with maintainers; [ goibhniu ];
   };
-
 }
