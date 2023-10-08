cdcd9295eb82db87c755f2e4128ee549ced07455
rwaweber <rwaweber@chump.info>
2023-10-08 00:28:48 -0400

    confluent-cli: 3.17.0 -> 3.37.0

--- pkgs/development/tools/confluent-cli/default.nix
+++ pkgs/development/tools/confluent-cli/default.nix
@@ -2,16 +2,16 @@
 
 stdenv.mkDerivation rec {
   pname = "confluent-cli";
-  version = "3.17.0";
+  version = "3.37.0";
 
   # To get the latest version:
   # curl -L https://cnfl.io/cli | sh -s -- -l | grep -v latest | sort -V | tail -n1
   src = fetchurl (if stdenv.hostPlatform.isDarwin then {
       url = "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/${version}/confluent_${version}_darwin_amd64.tar.gz";
-      sha256 = "03104736f65591a5be9536424460d9b8c8fc8ac8b5eb646e832371397aaf7cef";
+      sha256 = "804401c4286c339097151b1605555c43cf3143637896a908c477d928f10c94e6";
     } else {
       url = "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/${version}/confluent_${version}_linux_amd64.tar.gz";
-      sha256 = "3243beca4fefd49cf067f9a4df4f5072a8ac5dac91638e9f10ef0b0544d30445";
+      sha256 = "bc907fd2875503ce9f66d461fc283928f0fcabc0901443654738e9238d5439cf";
     });
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
