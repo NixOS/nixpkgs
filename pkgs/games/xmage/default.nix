811af9cc92ef042cdcf9d080d21e765a95cb87fc
Matthias Beyer <mail@beyermatthias.de>
2023-10-08 09:30:34 +0200

    xmage: 1.4.42V6 -> 1.4.50V2
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/games/xmage/default.nix
+++ pkgs/games/xmage/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname   = "xmage";
-  version = "1.4.42V6";
+  version = "1.4.50V2";
 
   src = fetchurl {
-    url    = "https://github.com/magefree/mage/releases/download/xmage_1.4.42V6/xmage_${version}.zip";
-    sha256 = "14s4885ldi0rplqmab5m775plsqmmm0m89j402caiqm2q9mzvkhd";
+    url    = "https://github.com/magefree/mage/releases/download/xmage_${version}/xmage_${version}.zip";
+    sha256 = "sha256-t1peHYwCRy3wiIIwOD3nUyoxSOxbw6B/g++A1ofIbmg=";
   };
 
   preferLocalBuild = true;
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
     cp -rv ./* $out
 
     cat << EOS > $out/bin/xmage
-exec ${jdk8}/bin/java -Xms256m -Xmx512m -XX:MaxPermSize=384m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -jar $out/mage-client/lib/mage-client-1.4.42.jar
+exec ${jdk8}/bin/java -Xms256m -Xmx512m -XX:MaxPermSize=384m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -jar $out/mage-client/lib/mage-client-1.4.50.jar
 EOS
 
     chmod +x $out/bin/xmage
