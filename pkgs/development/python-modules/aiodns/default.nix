7980cadc15d99fa17782b2366dbedcbd482cdd8d
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-08 09:18:11 +0200

    python311Packages.aiodns: refactor

--- pkgs/development/python-modules/aiodns/default.nix
+++ pkgs/development/python-modules/aiodns/default.nix
@@ -3,35 +3,37 @@
 , fetchFromGitHub
 , pycares
 , pythonOlder
-, typing
 }:
 
 buildPythonPackage rec {
   pname = "aiodns";
   version = "3.1.1";
-  disabled = pythonOlder "3.6";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "saghul";
-    repo = pname;
+    repo = "aiodns";
     rev = "refs/tags/v${version}";
     sha256 = "sha256-JZS53kICsrXDot3CKjG30AOjkYycKpMJvC9yS3c1v5Q=";
   };
 
   propagatedBuildInputs = [
     pycares
-  ] ++ lib.optionals (pythonOlder "3.7") [
-    typing
   ];
 
   # Could not contact DNS servers
   doCheck = false;
 
-  pythonImportsCheck = [ "aiodns" ];
+  pythonImportsCheck = [
+    "aiodns"
+  ];
 
   meta = with lib; {
     description = "Simple DNS resolver for asyncio";
     homepage = "https://github.com/saghul/aiodns";
+    changelog = "https://github.com/saghul/aiodns/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
