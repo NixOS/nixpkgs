8c6f525d12a2ac58ae5107452691ecaf1d2a0c25
Isidor Zeuner <nix@quidecco.pl>
2023-10-09 07:40:25 +0200

    maintainers: add gm6k

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6424,6 +6424,10 @@
     githubId = 1447245;
     name = "Robin Gloster";
   };
+  gm6k = {
+    email = "nix@quidecco.pl";
+    name = "Isidor Zeuner";
+  };
   gmemstr = {
     email = "git@gmem.ca";
     github = "gmemstr";
