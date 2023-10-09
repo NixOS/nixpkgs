a58bd4f5f6d3a02fee4cd29f0fd9fddf17a23dae
Emily Trau <emily@downunderctf.com>
2023-10-08 23:40:43 -0700

    minimal-bootstrap.python: init at 3.13.7

--- pkgs/os-specific/linux/minimal-bootstrap/default.nix
+++ pkgs/os-specific/linux/minimal-bootstrap/default.nix
@@ -211,6 +211,12 @@ lib.makeScope
         gnumake = gnumake-musl;
       };
 
+      python = callPackage ./python {
+        gcc = gcc-latest;
+        gnumake = gnumake-musl;
+        gnutar = gnutar-latest;
+      };
+
       stage0-posix = callPackage ./stage0-posix { };
 
       inherit (self.stage0-posix)
@@ -271,6 +277,7 @@ lib.makeScope
         echo ${heirloom.tests.get-version}
         echo ${mes.compiler.tests.get-version}
         echo ${musl.tests.hello-world}
+        echo ${python.tests.get-version}
         echo ${tinycc-mes.compiler.tests.chain}
         echo ${tinycc-musl.compiler.tests.hello-world}
         echo ${xz.tests.get-version}
