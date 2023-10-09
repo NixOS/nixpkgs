NixOS/nixpkgs

commit 9588972fe983654d05df0695899bc96c46c622f9

diffutils: 3.9 -> 3.10
Fixes the issue introduced in 3.9 and makes cmp and diff work past Y2K39

pkgs/tools/text/diffutils/default.nix
@@ -7,28 +7,16 @@
 
 stdenv.mkDerivation rec {
   pname = "diffutils";
-  version = "3.9";
+  version = "3.10";
 
   src = fetchurl {
     url = "mirror://gnu/diffutils/diffutils-${version}.tar.xz";
-    hash = "sha256-2A076QogGGjeg9eNrTQTrYgWDMU7zDbrnq98INvwI/E=";
+    hash = "sha256-kOXpPMck5OvhLt6A3xY0Bjx6hVaSaFkZv+YLVWyb0J4=";
   };
-
-  patches = [
-    # Backport of a fix for 'diff -D' output.
-    # TODO: remove when updating to 3.10.
-    ./fix-diff-D.patch
-  ];
-
-  postPatch = ''
-    # avoid the need for help2man
-    # TODO: can be removed when fix-diff-D.patch is removed.
-    touch man/diff.1
-  '';
 
   outputs = [ "out" "info" ];
 
-  nativeBuildInputs = [ xz.bin ];
+  nativeBuildInputs = [ (lib.getBin xz) ];
   /* If no explicit coreutils is given, use the one from stdenv. */
   buildInputs = [ coreutils ];
 
@@ -38,10 +26,13 @@ stdenv.mkDerivation rec {
     lib.optional (coreutils != null) "PR_PROGRAM=${coreutils}/bin/pr"
     ++ lib.optional (stdenv.buildPlatform != stdenv.hostPlatform) "gl_cv_func_getopt_gnu=yes";
 
+  doCheck = true;
+
   meta = with lib; {
     homepage = "https://www.gnu.org/software/diffutils/diffutils.html";
     description = "Commands for showing the differences between files (diff, cmp, etc.)";
     license = licenses.gpl3;
     platforms = platforms.unix;
+    maintainers = with maintainers; [ das_j ];
   };
 }

pkgs/tools/text/diffutils/fix-diff-D.patch
@@ -1,15 +1,0 @@
-https://git.savannah.gnu.org/cgit/diffutils.git/patch/?id=ba08fbbb0ca5da455bf695236c57bc50e7faed50
-https://github.com/NixOS/nixpkgs/pull/213421#issuecomment-1407749912
-
-Fix 'diff -D' regression
---- a/src/diff.c
-+++ b/src/diff.c
-@@ -416,7 +416,7 @@ main (int argc, char **argv)
- 	    char *base = b;
- 	    int changes = 0;
- 
--	    for (i = 0; i < sizeof sizeof C_ifdef_group_formats; i++)
-+	    for (i = 0; i < sizeof C_ifdef_group_formats; i++)
- 	      {
- 		char ch = C_ifdef_group_formats[i];
- 		switch (ch)

commit d57fa98fc1c72e1a76892511790c3bdea9ec5939

diffutils: disable tests on aarch64 musl
The tests have never worked on aarch64 musl, so this restores the
previous behaviour on that platform of building successfully even
though the tests don't pass.

Since the tests work on x86_64 musl, and not aarch64 musl, my best
guess having not looked too closely is that the difference is because
aarch64 is stuck on ancient bootstrap tools for musl that cause all
sorts of problems.  Hopefully we can revert this once the bootstrap
tools are updated.  Having diffutils building is a prerequisite to
doing that.

Fixes: 9588972fe983 ("diffutils: 3.9 -> 3.10")

pkgs/tools/text/diffutils/default.nix
@@ -33,7 +33,7 @@ stdenv.mkDerivation rec {
     lib.optional (coreutils != null) "PR_PROGRAM=${coreutils}/bin/pr"
     ++ lib.optional (stdenv.buildPlatform != stdenv.hostPlatform) "gl_cv_func_getopt_gnu=yes";
 
-  doCheck = true;
+  doCheck = !(stdenv.buildPlatform.isAarch64 && stdenv.buildPlatform.isMusl);
 
   meta = with lib; {
     homepage = "https://www.gnu.org/software/diffutils/diffutils.html";