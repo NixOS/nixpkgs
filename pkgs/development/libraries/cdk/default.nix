d33af25faa503c4172b240dcbfc62e124365f131
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-09 19:37:31 +0000

    cdk: 5.0-20221025 -> 5.0-20230201

--- pkgs/development/libraries/cdk/default.nix
+++ pkgs/development/libraries/cdk/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "cdk";
-  version = "5.0-20221025";
+  version = "5.0-20230201";
 
   src = fetchurl {
     urls = [
       "ftp://ftp.invisible-island.net/cdk/cdk-${version}.tgz"
       "https://invisible-mirror.net/archives/cdk/cdk-${version}.tgz"
     ];
-    hash = "sha256-A8z6Icn8PWHd0P2hnaVFNZBVu+71ociC37n/SPN0avI=";
+    hash = "sha256-oxJ7Wf5QX16Jjao90VsM9yShJ0zmgWW3eb4vKdTE8vY=";
   };
 
   buildInputs = [
