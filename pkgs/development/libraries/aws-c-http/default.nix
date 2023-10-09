7c81515841d8eb88c07d9fb5ab4d261eab8fec00
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-09 14:10:15 +0000

    aws-c-http: 0.7.11 -> 0.7.13

--- pkgs/development/libraries/aws-c-http/default.nix
+++ pkgs/development/libraries/aws-c-http/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "aws-c-http";
-  version = "0.7.11";
+  version = "0.7.13";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-c-http";
     rev = "v${version}";
-    sha256 = "sha256-gKuWhXxWHqTS66yANdKLsCZRk7jeDmyYMlme4WXT5Wc=";
+    sha256 = "sha256-tPdLCFrs00tCBSy69SwXFpEHzusjgcKzYluy5SSdIJ4=";
   };
 
   nativeBuildInputs = [
