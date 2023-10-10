5e246d96c4a05dfaf36b51f2be4f22f0385df97b
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-10 11:43:12 +0200

    python311Packages.google-cloud-dataproc: 5.5.1 -> 5.6.0
    Changelog: https://github.com/googleapis/python-dataproc/blob/v5.6.0/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-dataproc/default.nix
+++ pkgs/development/python-modules/google-cloud-dataproc/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-dataproc";
-  version = "5.5.1";
+  version = "5.6.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-+tfaWdOR/sFfGih71x1VznI3gZ2quQ4h1rEUCFHZ1DQ=";
+    hash = "sha256-X6nfuIQzwtDBOLfrtq3hdBA743YszQObJNU8dEjjdg0=";
   };
 
   propagatedBuildInputs = [
