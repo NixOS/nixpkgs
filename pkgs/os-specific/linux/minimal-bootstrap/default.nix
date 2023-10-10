1f1562abac5153170c22e4d9cba1121fc8ef5885
Emily Trau <emily@downunderctf.com>
2023-10-09 22:32:13 -0700

    minimal-bootstrap.gnumake-static: init at 4.4.1

--- pkgs/os-specific/linux/minimal-bootstrap/default.nix
+++ pkgs/os-specific/linux/minimal-bootstrap/default.nix
@@ -187,6 +187,12 @@ lib.makeScope
         gnumakeBoot = gnumake;
       };
 
+      gnumake-static = callPackage ./gnumake/static.nix {
+        gcc = gcc-latest;
+        gnumake = gnumake-musl;
+        gnutar = gnutar-latest;
+      };
+
       gnupatch = callPackage ./gnupatch { tinycc = tinycc-mes; };
 
       gnused = callPackage ./gnused {
@@ -354,6 +360,8 @@ lib.makeScope
         echo ${gnugrep.tests.get-version}
         echo ${gnugrep-static.tests.get-version}
         echo ${gnum4.tests.get-version}
+        echo ${gnumake-musl.tests.get-version}
+        echo ${gnumake-static.tests.get-version}
         echo ${gnused.tests.get-version}
         echo ${gnused-mes.tests.get-version}
         echo ${gnused-static.tests.get-version}
