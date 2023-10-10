c0ae7aa88b4a2a0ecdf0e3d978dcd033674b7c10
Emily Trau <emily@downunderctf.com>
2023-10-09 21:40:19 -0700

    minimal-bootstrap.gnused-static: init at 4.9

--- pkgs/os-specific/linux/minimal-bootstrap/default.nix
+++ pkgs/os-specific/linux/minimal-bootstrap/default.nix
@@ -188,9 +188,16 @@ lib.makeScope
         tinycc = tinycc-musl;
         gnused = gnused-mes;
       };
+
       gnused-mes = callPackage ./gnused/mes.nix {
         bash = bash_2_05;
         tinycc = tinycc-bootstrappable;
+      };
+
+      gnused-static = callPackage ./gnused/static.nix {
+        gcc = gcc-latest;
+        gnumake = gnumake-musl;
+        gnutar = gnutar-latest;
       };
 
       gnutar = callPackage ./gnutar/mes.nix {
@@ -317,6 +324,7 @@ lib.makeScope
         echo ${gnum4.tests.get-version}
         echo ${gnused.tests.get-version}
         echo ${gnused-mes.tests.get-version}
+        echo ${gnused-static.tests.get-version}
         echo ${gnutar.tests.get-version}
         echo ${gnutar-latest.tests.get-version}
         echo ${gnutar-musl.tests.get-version}
