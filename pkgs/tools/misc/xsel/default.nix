ae3551fc8d3fda498d5e19c843d0218782cd96c5
Christina Sørensen <christina@cafkafk.com>
2023-10-10 15:07:14 +0200

    xsel: add maintainer cafkafk
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- pkgs/tools/misc/xsel/default.nix
+++ pkgs/tools/misc/xsel/default.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation {
     description = "Command-line program for getting and setting the contents of the X selection";
     homepage = "http://www.kfish.org/software/xsel";
     license = licenses.mit;
-    maintainers = [ ];
+    maintainers = with maintainers; [ cafkafk ];
     platforms = lib.platforms.unix;
     mainProgram = "xsel";
   };
