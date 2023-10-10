dcc9ea0c4c3590b55acd89ce5d57b8d1a8071b59
FliegendeWurst <2012gdwu+github@posteo.de>
2023-10-10 16:03:29 +0200

    wavebox: use proper API endpoint in update

--- pkgs/applications/networking/instant-messengers/wavebox/update.sh
+++ pkgs/applications/networking/instant-messengers/wavebox/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p coreutils curl gnugrep
+#!nix-shell -i bash -p coreutils curl jq
 set -euo pipefail
 
 cd $(dirname "${BASH_SOURCE[0]}")
@@ -8,7 +8,7 @@ setKV () {
     sed -i "s|$2 = \".*\"|$2 = \"${3:-}\"|" $1
 }
 
-version=$(curl -LIs --show-error -o /dev/null -w %{url_effective} 'https://download.wavebox.app/latest/stable/linux/tar' | grep -oP 'Wavebox_\K(.+)(?=.tar.gz)')
+version=$(curl "https://download.wavebox.app/stable/linux/latest.json" | jq --raw-output '.["urls"]["tar"] | match("https://download.wavebox.app/stable/linux/tar/Wavebox_(.+).tar.gz").captures[0]["string"]')
 
 sha256_linux64=$(nix-prefetch-url --quiet https://download.wavebox.app/stable/linux/tar/Wavebox_${version}.tar.gz)
 setKV ./default.nix version $version
