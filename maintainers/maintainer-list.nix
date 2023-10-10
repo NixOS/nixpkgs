71f562e0e85f1b3711f9a93aacd0f133fcba7978
jgoux <hi@jgoux.dev>
2023-10-10 22:16:39 +0200

    infisical: 0.3.7 -> 0.14.2

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8164,6 +8164,12 @@
     githubId = 6445082;
     name = "Joseph Lukasik";
   };
+  jgoux = {
+    email = "hi@jgoux.dev";
+    github = "jgoux";
+    githubId = 1443499;
+    name = "Julien Goux";
+  };
   jhh = {
     email = "jeff@j3ff.io";
     github = "jhh";
