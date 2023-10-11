c230d01f893da9126aa39a9a5b67d625933e1029
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-10-11 02:29:45 +0200

    redis: 7.0.12 -> 7.0.13
    https://github.com/redis/redis/raw/7.0.13/00-RELEASENOTES
    
    Fixes: CVE-2023-41053

--- pkgs/servers/nosql/redis/default.nix
+++ pkgs/servers/nosql/redis/default.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "redis";
-  version = "7.0.12";
+  version = "7.0.13";
 
   src = fetchurl {
     url = "https://download.redis.io/releases/${pname}-${version}.tar.gz";
-    hash = "sha256-ndg9WyeLsr8OOb/rdcPoFwAk7brxG6E7cDeylFz0irc=";
+    hash = "sha256-lwZXdNX7g4jrDYkTRY3s/LFn01bkDTHdAc0wwcw5FnM=";
   };
 
   patches = [
