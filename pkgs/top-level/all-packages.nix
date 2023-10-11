4fa5f7334a83dbfc8da2a0d5188d7703f8227222
Madoura <madouura@gmail.com>
2023-10-10 22:47:41 -0500

    rapidjson-unstable: init at unstable-2023-09-28
    Also correct rapidjson's src owner

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24699,6 +24699,8 @@ with pkgs;
 
   rapidjson = callPackage ../development/libraries/rapidjson { };
 
+  rapidjson-unstable = callPackage ../development/libraries/rapidjson/unstable.nix { };
+
   rapidxml = callPackage ../development/libraries/rapidxml { };
 
   rapidyaml = callPackage ../development/libraries/rapidyaml {};
