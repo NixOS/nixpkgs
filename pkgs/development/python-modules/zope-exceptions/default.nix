16b879f3ef6ff1ecfb1c1d23e3072f8c1a9e39e9
natsukium <tomoya.otabi@gmail.com>
2023-10-11 10:46:11 +0900

    python311Packages.zope-exceptions: refactor

--- pkgs/development/python-modules/zope-exceptions/default.nix
+++ pkgs/development/python-modules/zope-exceptions/default.nix
@@ -1,12 +1,17 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
+, setuptools
 , zope_interface
 }:
 
 buildPythonPackage rec {
   pname = "zope-exceptions";
   version = "4.6";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "zope.exceptions";
@@ -14,15 +19,24 @@ buildPythonPackage rec {
     hash = "sha256-YZ0kpMZb7Zez3QUV5zLoK2nxVdQsyUlV0b6MKCiGg80=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [ zope_interface ];
 
   # circular deps
   doCheck = false;
 
+  pythonImportsCheck = [
+    "zope.exceptions"
+  ];
+
   meta = with lib; {
     description = "Exception interfaces and implementations";
     homepage = "https://pypi.python.org/pypi/zope.exceptions";
-    license = licenses.zpl20;
+    changelog = "https://github.com/zopefoundation/zope.exceptions/blob/${version}/CHANGES.rst";
+    license = licenses.zpl21;
     maintainers = with maintainers; [ goibhniu ];
   };
 
