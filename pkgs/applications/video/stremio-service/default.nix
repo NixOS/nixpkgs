d09e35a8371957193b6e9dccd809e468a6ac9588
jackkav <jackkav@gmail.com>
2023-10-11 21:03:23 +0200

    bump 0.1.8, cp server.js to drv

--- pkgs/applications/video/stremio-service/default.nix
+++ pkgs/applications/video/stremio-service/default.nix
@@ -1,17 +1,34 @@
-{ lib, fetchFromGitHub, rustPlatform, pkg-config, openssl, gtk3 }:
+{ lib, stdenv, fetchurl, fetchFromGitHub, rustPlatform, pkg-config, openssl, gtk3 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "stremio-service";
-  version = "6e987ff";
+  version = "0.1.8";
 
-  src = fetchFromGitHub {
-    owner = "Stremio";
-    repo = pname;
-    rev = "6e987ff193696cca4b0ce78b93e3ca43477a7c37";
-    sha256 = "sha256-G3FQhsKmyyOLsNLfzwkFB/xJTwPQGnzVC6KbrXCDtjw=";
+  src = stdenv.mkDerivation rec {
+    name = "${pname}-source-${version}";
+    src = fetchFromGitHub {
+      owner = "Stremio";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "sha256-PySvGT7Za5NeQgW1OwAo3ja9FnKrQ/ofh217RkWewW0=";
+    };
+    server-js = fetchurl {
+      url = "https://dl.strem.io/server/v4.20.2/desktop/server.js";
+      sha256 = "sha256-YtoQG8ppBVpe86DjyRKhDwmHhVRYpIOkEDuH21YseWA=";
+    };
+    installPhase = ''
+      mkdir -p $out
+      cp -R ./* $out/
+      ls -al $out/resources/bin/linux/
+      cp ${server-js} $out/resources/bin/linux/server.js
+      ls -al $out/resources/bin/linux/
+    '';
   };
-  cargoBuildFlags = "--features=offline-build";
-  cargoSha256 = "sha256-NX43rF71wq/mFJJrxImUfpbnqRA9/kGb12yAjhHseYo=";
+
+  cargoBuildFeatures = [ "offline-build" "bundled" ];
+  doCheck = false;
+  cargoCheckFeatures = [ "offline-build" "bundled" ];
+  cargoSha256 = "sha256-aHIkHw2KvveuURq5yvSLXACIinQF89ZtHuV5o2fJmks=";
   buildInputs = [ gtk3 openssl ];
   nativeBuildInputs = [ pkg-config ];
   meta = with lib; {
