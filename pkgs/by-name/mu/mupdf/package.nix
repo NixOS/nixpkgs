596131cf4b1105418189022d293b360bb3b54728
Anderson Torres <torres.anderson.85@protonmail.com>
2023-10-11 11:46:17 -0300

    mupdf: refactor
    - finalAttrs
    - pkgconfigItem functionalities

--- pkgs/by-name/mu/mupdf/package.nix
+++ pkgs/by-name/mu/mupdf/package.nix
@@ -3,72 +3,95 @@
 , fetchurl
 , fetchpatch
 , fetchFromGitHub
-, copyDesktopItems
-, makeDesktopItem
-, desktopToDarwinBundle
 , buildPackages
-, pkg-config
-, freetype
-, harfbuzz
-, openjpeg
-, jbig2dec
-, libjpeg
+, copyDesktopItems
+, copyPkgconfigItems
+, cups-filters
+, curl
 , darwin
+, desktopToDarwinBundle
+, freeglut
+, freetype
+, gitUpdater
 , gumbo
-, enableX11 ? (!stdenv.isDarwin)
+, harfbuzz
+, jbig2dec
+, libGLU
 , libX11
 , libXext
 , libXi
 , libXrandr
-, enableCurl ? true
-, curl
+, libjpeg
+, makeDesktopItem
+, makePkgconfigItem
+, openjpeg
 , openssl
-, enableGL ? true
-, freeglut
-, libGLU
-, xcbuild
-, gitUpdater
-
-# for passthru.tests
-, cups-filters
+, pkg-config
 , python3
+, xcbuild
 , zathura
+, enableCurl ? true
+, enableGL ? true
+, enableX11 ? (!stdenv.isDarwin)
 }:
+
 let
-
   # OpenJPEG version is hardcoded in package source
-  openJpegVersion = with stdenv;
-    lib.versions.majorMinor (lib.getVersion openjpeg);
+  openJpegVersion = lib.versions.majorMinor (lib.getVersion openjpeg);
 
-  freeglut-mupdf = freeglut.overrideAttrs (old: rec {
-    pname = "freeglut-mupdf";
-    version = "3.0.0-r${src.rev}";
-    src = fetchFromGitHub {
-      owner = "ArtifexSoftware";
-      repo = "thirdparty-freeglut";
+  freeglut-mupdf = freeglut.overrideAttrs (old:
+    let
+      pname = "freeglut-mupdf";
+      version = "3.0.0-r${rev}";
       rev = "13ae6aa2c2f9a7b4266fc2e6116c876237f40477";
-      hash = "sha256-0fuE0lm9rlAaok2Qe0V1uUrgP4AjMWgp3eTbw8G6PMM=";
-    };
-  });
-
+    in {
+      inherit pname version;
+      src = fetchFromGitHub {
+        owner = "ArtifexSoftware";
+        repo = "thirdparty-freeglut";
+        inherit rev;
+        hash = "sha256-0fuE0lm9rlAaok2Qe0V1uUrgP4AjMWgp3eTbw8G6PMM=";
+      };
+    });
 in
-stdenv.mkDerivation rec {
-  version = "1.23.0";
+stdenv.mkDerivation (finalAttrs: {
   pname = "mupdf";
+  version = "1.23.0";
 
   src = fetchurl {
-    url = "https://mupdf.com/downloads/archive/${pname}-${version}-source.tar.gz";
-    sha256 = "sha256-3kFAaS5pMULDEeAwrBVuOO4XXXq2wb4QxcmuljhGFk4=";
+    url = "https://mupdf.com/downloads/archive/mupdf-${finalAttrs.version}-source.tar.gz";
+    hash = "sha256-3kFAaS5pMULDEeAwrBVuOO4XXXq2wb4QxcmuljhGFk4=";
   };
 
-  patches = [ ./0001-Use-command-v-in-favor-of-which.patch
-              ./0002-Add-Darwin-deps.patch
-            ];
+  patches = [
+    ./0001-Use-command-v-in-favor-of-which.patch
+    ./0002-Add-Darwin-deps.patch
+  ];
 
   postPatch = ''
     sed -i "s/__OPENJPEG__VERSION__/${openJpegVersion}/" source/fitz/load-jpx.c
     substituteInPlace Makerules --replace "(shell pkg-config" "(shell $PKG_CONFIG"
   '';
+
+  nativeBuildInputs = [
+    copyPkgconfigItems
+    pkg-config
+  ]
+  ++ lib.optional (enableGL || enableX11) copyDesktopItems
+  ++ lib.optional stdenv.isDarwin desktopToDarwinBundle;
+
+  buildInputs = [
+    freetype harfbuzz openjpeg jbig2dec libjpeg gumbo
+  ]
+  ++ lib.optionals stdenv.isDarwin [ xcbuild ]
+  ++ lib.optionals enableX11 [ libX11 libXext libXi libXrandr ]
+  ++ lib.optionals enableCurl [ curl openssl ]
+  ++ lib.optionals enableGL (
+    if stdenv.isDarwin then
+      with darwin.apple_sdk.frameworks; [ GLUT OpenGL ]
+    else
+      [ freeglut-mupdf libGLU ]
+  );
 
   # Use shared libraries to decrease size
   buildFlags = [ "shared" ];
@@ -77,24 +100,10 @@ stdenv.mkDerivation rec {
     "prefix=$(out)"
     "USE_SYSTEM_LIBS=yes"
     "PKG_CONFIG=${buildPackages.pkg-config}/bin/${buildPackages.pkg-config.targetPrefix}pkg-config"
-  ] ++ lib.optionals (!enableX11) [ "HAVE_X11=no" ]
-    ++ lib.optionals (!enableGL) [ "HAVE_GLUT=no" ];
+  ]
+  ++ lib.optionals (!enableX11) [ "HAVE_X11=no" ]
+  ++ lib.optionals (!enableGL) [ "HAVE_GLUT=no" ];
 
-  nativeBuildInputs = [ pkg-config ]
-    ++ lib.optional (enableGL || enableX11) copyDesktopItems
-    ++ lib.optional stdenv.isDarwin desktopToDarwinBundle;
-
-  buildInputs = [ freetype harfbuzz openjpeg jbig2dec libjpeg gumbo ]
-    ++ lib.optional stdenv.isDarwin xcbuild
-    ++ lib.optionals enableX11 [ libX11 libXext libXi libXrandr ]
-    ++ lib.optionals enableCurl [ curl openssl ]
-    ++ lib.optionals enableGL (
-    if stdenv.isDarwin then
-      with darwin.apple_sdk.frameworks; [ GLUT OpenGL ]
-    else
-      [ freeglut-mupdf libGLU ]
-  )
-  ;
   outputs = [ "bin" "dev" "out" "man" "doc" ];
 
   preConfigure = ''
@@ -104,11 +113,11 @@ stdenv.mkDerivation rec {
 
   desktopItems = [
     (makeDesktopItem {
-      name = pname;
-      desktopName = pname;
-      comment = meta.description;
+      name = "mupdf";
+      desktopName = "mupdf";
+      comment = finalAttrs.meta.description;
       icon = "mupdf";
-      exec = "${pname} %f";
+      exec = "mupdf %f";
       terminal = false;
       mimeTypes = [
         "application/epub+zip"
@@ -126,27 +135,36 @@ stdenv.mkDerivation rec {
     })
   ];
 
+  pkgconfigItems = [
+    (makePkgconfigItem {
+      name = "mupdf";
+      inherit (finalAttrs) version;
+      description = "Library for rendering PDF documents";
+      cflags = [ "-I${placeholder "dev"}/include" ];
+      libs = [
+        "-L${placeholder "out"}/lib"
+        "-lmupdf"
+        "-lmupdf-third"
+      ];
+      variables = {
+        prefix = "${placeholder "out"}";
+        includedir = "${placeholder "out"}/include";
+        libdir = "${placeholder "out"}/lib";
+      };
+    })
+  ];
+
   postInstall = ''
-    mkdir -p "$out/lib/pkgconfig"
-    cat >"$out/lib/pkgconfig/mupdf.pc" <<EOF
-    prefix=$out
-    libdir=$out/lib
-    includedir=$out/include
-
-    Name: mupdf
-    Description: Library for rendering PDF documents
-    Version: ${version}
-    Libs: -L$out/lib -lmupdf -lmupdf-third
-    Cflags: -I$dev/include
-    EOF
-
     moveToOutput "bin" "$bin"
-  '' + lib.optionalString (enableX11 || enableGL) ''
+  ''
+  + lib.optionalString (enableX11 || enableGL) ''
     mkdir -p $bin/share/icons/hicolor/48x48/apps
     cp docs/logo/mupdf.png $bin/share/icons/hicolor/48x48/apps
-  '' + (if enableGL then ''
+  ''
+  + (if enableGL then ''
     ln -s "$bin/bin/mupdf-gl" "$bin/bin/mupdf"
-  '' else lib.optionalString (enableX11) ''
+  ''
+     else lib.optionalString (enableX11) ''
     ln -s "$bin/bin/mupdf-x11" "$bin/bin/mupdf"
   '');
 
@@ -164,13 +182,13 @@ stdenv.mkDerivation rec {
     };
   };
 
-  meta = with lib; {
+  meta = {
     homepage = "https://mupdf.com";
     description = "Lightweight PDF, XPS, and E-book viewer and toolkit written in portable C";
-    changelog = "https://git.ghostscript.com/?p=mupdf.git;a=blob_plain;f=CHANGES;hb=${version}";
-    license = licenses.agpl3Plus;
-    maintainers = with maintainers; [ vrthra fpletz ];
-    platforms = platforms.unix;
+    changelog = "https://git.ghostscript.com/?p=mupdf.git;a=blob_plain;f=CHANGES;hb=${finalAttrs.version}";
+    license = lib.licenses.agpl3Plus;
     mainProgram = "mupdf";
+    maintainers = with lib.maintainers; [ vrthra fpletz ];
+    platforms = lib.platforms.unix;
   };
-}
+})
