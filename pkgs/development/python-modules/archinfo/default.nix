4bd5c723ae2028963b51ed6962c4a8696dd22e33
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-11 21:52:09 +0200

    python311Packages.archinfo: 9.2.71 -> 9.2.72
    Diff: https://github.com/angr/archinfo/compare/refs/tags/v9.2.71...v9.2.72

--- pkgs/development/python-modules/archinfo/default.nix
+++ pkgs/development/python-modules/archinfo/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "archinfo";
-  version = "9.2.71";
+  version = "9.2.72";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-AZMbE+/2uw7mFtBKwwLWCiVwupnB+EkUrgZFkGiKHtM=";
+    hash = "sha256-QTmp8RV2ggcktQvemG/ykjvxSzn7vjscHJCqHdlbOGM=";
   };
 
   nativeBuildInputs = [
