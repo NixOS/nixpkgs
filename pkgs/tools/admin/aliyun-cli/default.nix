f280b9c8c12bd08a514f4594c2d6227e5abdcdcc
Mario Rodas <marsam@users.noreply.github.com>
2023-10-11 12:15:00 +0000

    aliyun-cli: 3.0.182 -> 3.0.183
    Diff: https://github.com/aliyun/aliyun-cli/compare/v3.0.182...v3.0.183
    
    Changelog: https://github.com/aliyun/aliyun-cli/releases/tag/v3.0.183

--- pkgs/tools/admin/aliyun-cli/default.nix
+++ pkgs/tools/admin/aliyun-cli/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   pname = "aliyun-cli";
-  version = "3.0.182";
+  version = "3.0.183";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "aliyun";
     repo = pname;
     fetchSubmodules = true;
-    sha256 = "sha256-xnSOJudLIKq8NMBV36IN7dzi6Ynd6BpnpBQR9Vxz0VI=";
+    sha256 = "sha256-L00vQTuY4m5vGqu09FyOdM/XOIMyLZG/BSj8dktEa40=";
   };
 
   vendorHash = "sha256-S8Nthnr3wASvRyZS5UTHILPnUA+FeZJEwIvT0O39U3I=";
