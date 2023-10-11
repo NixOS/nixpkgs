f66ff3317e80761c7da045a872305a6f785958c4
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-11 12:14:48 +0000

    aliyun-cli: 3.0.181 -> 3.0.182

--- pkgs/tools/admin/aliyun-cli/default.nix
+++ pkgs/tools/admin/aliyun-cli/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   pname = "aliyun-cli";
-  version = "3.0.181";
+  version = "3.0.182";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "aliyun";
     repo = pname;
     fetchSubmodules = true;
-    sha256 = "sha256-xjOoWQyQCVoCDJMXboxFAyil7jRCWU6oIEt7gcPkIPo=";
+    sha256 = "sha256-xnSOJudLIKq8NMBV36IN7dzi6Ynd6BpnpBQR9Vxz0VI=";
   };
 
   vendorHash = "sha256-S8Nthnr3wASvRyZS5UTHILPnUA+FeZJEwIvT0O39U3I=";
