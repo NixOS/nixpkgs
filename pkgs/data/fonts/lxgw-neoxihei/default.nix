c8e9fe3984229bc9018ee62ff51c1e50f9ab0f64
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-12 21:03:28 +0000

    lxgw-neoxihei: 1.105 -> 1.106

--- pkgs/data/fonts/lxgw-neoxihei/default.nix
+++ pkgs/data/fonts/lxgw-neoxihei/default.nix
@@ -5,11 +5,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "lxgw-neoxihei";
-  version = "1.105";
+  version = "1.106";
 
   src = fetchurl {
     url = "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v${version}/LXGWNeoXiHei.ttf";
-    hash = "sha256-rufBz5u6dV91oD211JuCUP2Km3RoFwkZ1OhRxyoGxpQ=";
+    hash = "sha256-AXEOoU9gvml1bqjPTYV+mmhVGLG4R6mH8e/h3wQgySo=";
   };
 
   dontUnpack = true;
