34c65fe8df3ee0fadb18cff1777086c553b3e5eb
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-10-11 21:58:16 -0600

    panicparse: missing package call in top-level

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11290,6 +11290,8 @@ with pkgs;
   pandoc-secnos = python3Packages.callPackage ../tools/misc/pandoc-secnos { };
   pandoc-tablenos = python3Packages.callPackage ../tools/misc/pandoc-tablenos { };
 
+  panicparse = callPackage ../tools/misc/panicparse {};
+
   panoply = callPackage ../tools/misc/panoply { };
 
   patray = callPackage ../tools/audio/patray { };
