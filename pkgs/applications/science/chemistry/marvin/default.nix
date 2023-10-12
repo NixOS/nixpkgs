bc0e786a549b56233f5d638db2d8943968b4c01a
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-10-13 00:00:21 +0200

    marvin: sha256 -> hash
    Co-authored-by: Christina Sørensen <christina@cafkafk.com>

--- pkgs/applications/science/chemistry/marvin/default.nix
+++ pkgs/applications/science/chemistry/marvin/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     name = "marvin-${version}.deb";
     url = "http://dl.chemaxon.com/marvin/${version}/marvin_linux_${versions.majorMinor version}.deb";
-    sha256 = "sha256-5ycOteXcdgZaeDl3WQ95H2lD0OnnobCbmnVlfYwVdeI=";
+    hash = "sha256-5ycOteXcdgZaeDl3WQ95H2lD0OnnobCbmnVlfYwVdeI=";
   };
 
   nativeBuildInputs = [ dpkg makeWrapper ];
