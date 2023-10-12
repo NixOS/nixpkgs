2e22b70d8dfe5571fdbeac217d51122230c2587a
Madoura <madouura@gmail.com>
2023-10-12 05:04:58 -0500

    python3Packages.desktop-entry-lib: init at 3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2717,6 +2717,8 @@ self: super: with self; {
 
   derpconf = callPackage ../development/python-modules/derpconf { };
 
+  desktop-entry-lib = callPackage ../development/python-modules/desktop-entry-lib { };
+
   desktop-notifier = callPackage ../development/python-modules/desktop-notifier { };
 
   detect-secrets = callPackage ../development/python-modules/detect-secrets { };
