a33b02fa9d664f31dadc8a874eb1a5dbaa9f4ecf
Jonathan Davies <jpds@protonmail.com>
2023-10-12 23:06:19 +0100

    caddy: Allow adding external plugins
    Solves: #14671
    
    Caddy has support for plugins that are added at compile time. This exposes an
    argument `externalPlugins` that will build caddy with the specified plugins.
    
    Example:
    ```
    caddy-custom = pkgs.caddy.override {
      externalPlugins = [
        {name = "coraza"; repo = "github.com/corazawaf/coraza-caddy/v2"; version = "v2.0.0-rc.3";}
        {name = "cache-handler"; repo = "github.com/caddyserver/cache-handler"; version = "v0.9.0";}
        {name = "ratelimit"; repo = "github.com/mholt/caddy-ratelimit"; version = "2dc0d586f0b87e983757c403bc0929ddeb84a537";}
      ];
      vendorHash = "<SRI hash>";
    };
    ```
    
    Adapted from Brendan Taylor's CoreDNS change in commit 95e66809debf42dbe1e4935fd31c8c275914d2eb.

--- pkgs/servers/caddy/default.nix
+++ pkgs/servers/caddy/default.nix
@@ -1,23 +1,30 @@
 { lib
 , buildGoModule
 , fetchFromGitHub
+, gnused
 , nixosTests
 , caddy
 , testers
 , installShellFiles
+, externalPlugins ? []
+, vendorHash ? "sha256-YNcQtjPGQ0XMSog+sWlH4lG/QdbdI0Lyh/fUGqQUFaY="
 }:
+
 let
+  attrsToModules = attrs:
+    builtins.map ({name, repo, version}: "${repo}") attrs;
+  attrsToSources = attrs:
+    builtins.map ({name, repo, version}: "${repo}@${version}") attrs;
+in buildGoModule rec {
+  pname = "caddy";
   version = "2.7.5";
+
   dist = fetchFromGitHub {
     owner = "caddyserver";
     repo = "dist";
     rev = "v${version}";
     hash = "sha256-aZ7hdAZJH1PvrX9GQLzLquzzZG3LZSKOvt7sWQhTiR8=";
   };
-in
-buildGoModule {
-  pname = "caddy";
-  inherit version;
 
   src = fetchFromGitHub {
     owner = "caddyserver";
@@ -26,7 +33,7 @@ buildGoModule {
     hash = "sha256-0IZZ7mkEzZI2Y8ed//m0tbBQZ0YcCXA0/b10ntNIXUk=";
   };
 
-  vendorHash = "sha256-YNcQtjPGQ0XMSog+sWlH4lG/QdbdI0Lyh/fUGqQUFaY=";
+  inherit vendorHash;
 
   subPackages = [ "cmd/caddy" ];
 
@@ -35,7 +42,34 @@ buildGoModule {
     "-X github.com/caddyserver/caddy/v2.CustomVersion=${version}"
   ];
 
-  nativeBuildInputs = [ installShellFiles ];
+  nativeBuildInputs = [ gnused installShellFiles ];
+
+  modBuildPhase = ''
+    for module in ${builtins.toString (attrsToModules externalPlugins)}; do
+      sed -i "/standard/a _ \"$module\"" ./cmd/caddy/main.go
+    done
+    for plugin in ${builtins.toString (attrsToSources externalPlugins)}; do
+      go get $plugin
+    done
+
+    go generate
+    go mod vendor
+  '';
+
+  modInstallPhase = ''
+    mv -t vendor go.mod go.sum
+    cp -r --reflink=auto vendor "$out"
+  '';
+
+  preBuild = ''
+    chmod -R u+w vendor
+    [ -f vendor/go.mod ] && mv -t . vendor/go.{mod,sum}
+    go generate
+
+    for module in ${builtins.toString (attrsToModules externalPlugins)}; do
+      sed -i "/standard/a _ \"$module\"" ./cmd/caddy/main.go
+    done
+  '';
 
   postInstall = ''
     install -Dm644 ${dist}/init/caddy.service ${dist}/init/caddy-api.service -t $out/lib/systemd/system
