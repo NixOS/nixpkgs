NixOS/nixpkgs

commit 14d6777962337b609c6eb4337e6baa9b31ee9fe3

virglrenderer: 0.7.0 -> 0.8.1
freedesktop.org tarballs haven't been published since 0.7.0.  I've
sent a message to the mailing list (<87eev3cgcz.fsf@alyssa.is>),
suggesting newer tarballs be published.

Build system has changed from autoconf to meson+cmake/ninja, and the
vtest/util.c patch doesn't seem to be necessary any more.

pkgs/development/libraries/virglrenderer/default.nix
@@ -1,24 +1,21 @@
-{ stdenv, fetchurl, pkgconfig, libGLU, epoxy, libX11, libdrm, mesa }:
-
+{ stdenv, fetchurl, cmake, meson, ninja, pkgconfig, python3
+, libGLU, epoxy, libX11, libdrm, mesa
+}:
 
 stdenv.mkDerivation rec {
-
   pname = "virglrenderer";
-  version = "0.7.0";
+  version = "0.8.1";
 
   src = fetchurl {
-    url = "https://www.freedesktop.org/software/virgl/${pname}-${version}.tar.bz2";
-    sha256 = "041agg1d6i8hg250y30f08n3via0hs9rbijxdrfifb8ara805v0m";
+    url = "https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${version}/virglrenderer-virglrenderer-${version}.tar.bz2";
+    sha256 = "0ac39mplmy6lmcd6cljmfsrfby7m6ac3dzvmnn5pv200drl415l4";
   };
 
   buildInputs = [ libGLU epoxy libX11 libdrm mesa ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ cmake meson ninja pkgconfig python3 ];
 
-  # Fix use of fd_set without proper include
-  prePatch = ''
-    sed -e '1i#include <sys/select.h>' -i vtest/util.c
-  '';
+  dontUseCmakeConfigure = true;
 
   meta = with stdenv.lib; {
     description = "A virtual 3D GPU library that allows a qemu guest to use the host GPU for accelerated 3D rendering";
@@ -27,5 +24,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     maintainers = [ maintainers.xeji ];
   };
-
 }

commit d643e2c6a422fa236e7626590df80777bad82f83

virglrenderer: remove cmake input
I think I must have added this by mistake — it's not referenced in
either the release where I added it, or the current release.

Fixes: 14d677796233 ("virglrenderer: 0.7.0 -> 0.8.1")

pkgs/development/libraries/virglrenderer/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, cmake, meson, ninja, pkg-config, python3
+{ lib, stdenv, fetchurl, meson, ninja, pkg-config, python3
 , libGLU, libepoxy, libX11, libdrm, mesa
 }:
 
@@ -15,9 +15,7 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ libGLU libepoxy libX11 libdrm mesa ];
 
-  nativeBuildInputs = [ cmake meson ninja pkg-config python3 ];
-
-  dontUseCmakeConfigure = true;
+  nativeBuildInputs = [ meson ninja pkg-config python3 ];
 
   meta = with lib; {
     description = "A virtual 3D GPU library that allows a qemu guest to use the host GPU for accelerated 3D rendering";