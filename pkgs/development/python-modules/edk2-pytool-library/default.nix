557e764e0827d0f40b52ce8fa405afd574a8ad53
Nick Cao <nickcao@nichi.co>
2023-10-12 11:54:40 -0400

    python310Packages.edk2-pytool-library: 0.19.0 -> 0.19.1
    Diff: https://github.com/tianocore/edk2-pytool-library/compare/v0.19.0...v0.19.1
    
    Changelog: https://github.com/tianocore/edk2-pytool-library/releases/tag/v0.19.1

--- pkgs/development/python-modules/edk2-pytool-library/default.nix
+++ pkgs/development/python-modules/edk2-pytool-library/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "edk2-pytool-library";
-  version = "0.19.0";
+  version = "0.19.1";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "tianocore";
     repo = "edk2-pytool-library";
     rev = "v${version}";
-    hash = "sha256-aXwQWnhbt4D5OYYMlGLl+il/RJp6mGJLFXw8pj7TYyk=";
+    hash = "sha256-xSW9ZeE2+vo2m2KaCW8lyotkHy7WVvNX16TDQEeQbQ4=";
   };
 
   nativeBuildInputs = [
