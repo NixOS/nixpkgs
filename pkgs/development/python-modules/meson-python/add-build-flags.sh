NixOS/nixpkgs

commit 70d182be3fd458c42dd196dadd971f5f5abdf608

python3.pkgs.meson-python: populate python build flags with $mesonFlags

pkgs/development/python-modules/meson-python/add-build-flags.sh
@@ -1,0 +1,6 @@
+# Add all of mesonFlags to -Csetup-args for pypa builds
+for f in $mesonFlags; do
+  pypaBuildFlags+=" -Csetup-args=$f"
+  # This requires pip>23.0.1, see: https://meson-python.readthedocs.io/en/latest/how-to-guides/config-settings.html
+  pipBuildFlags+=" --config-settings=setup-args=$f"
+done

pkgs/development/python-modules/meson-python/default.nix
@@ -39,6 +39,9 @@ buildPythonPackage rec {
   ] ++ lib.optionals (pythonOlder "3.10") [
     typing-extensions
   ];
+  setupHooks = [
+    ./add-build-flags.sh
+  ];
 
   # Ugly work-around. Drop ninja dependency.
   # We already have ninja, but it comes without METADATA.

commit 787af710c678b479ec88971089722b27f9e81d13

python3.pkgs.meson-python: run hook in postConfigure
It's not uncommon for packages to modify mesonFlags/mesonFlagsArray in
e.g. preConfigure.  Since the hook was previously run immediately,
these changes wouldn't have been picked up.  To fix this, run the hook
after the configure phase, where we know the list of meson flags will
have been finalized.

Fixes: 70d182be3fd4 ("python3.pkgs.meson-python: populate python build flags with $mesonFlags")

pkgs/development/python-modules/meson-python/add-build-flags.sh
@@ -1,6 +1,10 @@
-# Add all of mesonFlags to -Csetup-args for pypa builds
-for f in $mesonFlags; do
-  pypaBuildFlags+=" -Csetup-args=$f"
-  # This requires pip>23.0.1, see: https://meson-python.readthedocs.io/en/latest/how-to-guides/config-settings.html
-  pipBuildFlags+=" --config-settings=setup-args=$f"
-done
+mesonPythonBuildFlagsHook() {
+  # Add all of mesonFlags to -Csetup-args for pypa builds
+  for f in $mesonFlags; do
+    pypaBuildFlags+=" -Csetup-args=$f"
+    # This requires pip>23.0.1, see: https://meson-python.readthedocs.io/en/latest/how-to-guides/config-settings.html
+    pipBuildFlags+=" --config-settings=setup-args=$f"
+  done
+}
+
+postConfigureHooks+=(mesonPythonBuildFlagsHook)