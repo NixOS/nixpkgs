f40f1e2f2c669d4ad668d804effa5bbeaa070efe
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2023-10-13 22:28:08 +0200

    haskell.packages.ghc96.fgl: 5.8.0.0 -> 5.8.1.1

--- pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
@@ -74,6 +74,7 @@ self: super: {
   bifunctors = doDistribute self.bifunctors_5_6_1;
   base-compat = doDistribute self.base-compat_0_13_0;
   base-compat-batteries = doDistribute self.base-compat-batteries_0_13_0;
+  fgl = doDistribute self.fgl_5_8_1_1;
 
   # Because we bumped the version of th-abstraction above.^
   aeson = doJailbreak super.aeson;
