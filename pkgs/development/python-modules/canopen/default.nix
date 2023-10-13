fc4a8e403daa00370555a5723586a8076bdd0276
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-13 16:55:17 +0000

    python310Packages.canopen: 2.1.0 -> 2.2.0

--- pkgs/development/python-modules/canopen/default.nix
+++ pkgs/development/python-modules/canopen/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "canopen";
-  version = "2.1.0";
+  version = "2.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-vBJrsdy2Ljs02KEuOKB7WqgxnBdFfJv+II8Lu9qQ/2E=";
+    hash = "sha256-XxhlG5325HabmILpafk0rnc+8kpFqrwzNLWGmCBI0Iw=";
   };
 
   nativeBuildInputs = [
