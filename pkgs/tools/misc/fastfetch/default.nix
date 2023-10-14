6e3e2acb8f78c4154d2ee5bd0e04873ec5b404a2
Austin Horstman <khaneliman12@gmail.com>
2023-10-14 09:00:41 -0500

    fastfetch: 2.1.0 -> 2.1.1

--- pkgs/tools/misc/fastfetch/default.nix
+++ pkgs/tools/misc/fastfetch/default.nix
@@ -42,13 +42,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "2.1.0";
+  version = "2.1.1";
 
   src = fetchFromGitHub {
     owner = "fastfetch-cli";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-4FkBq7UxwZ2YuC4t/siC3CXqyP3Lff4jW3xheAQ9M6o=";
+    hash = "sha256-I1Ej7VVhzerQowGIOmxE6pDdWbOU7x35Xmia+A2ba6s=";
   };
 
   nativeBuildInputs = [
