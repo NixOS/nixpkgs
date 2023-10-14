71cbf0b067cd576a1afaf23d7edf6f9459aae5ad
Colin Putney <colin.putney@vannevarlabs.com>
2023-10-14 00:11:32 -0600

    Fix venv creation in Python environments

--- pkgs/development/interpreters/python/cpython/default.nix
+++ pkgs/development/interpreters/python/cpython/default.nix
@@ -466,8 +466,6 @@ in with passthru; stdenv.mkDerivation {
     # Strip tests
     rm -R $out/lib/python*/test $out/lib/python*/**/test{,s}
     '' + optionalString includeSiteCustomize ''
-    # Include a sitecustomize.py file
-    cp ${../sitecustomize.py} $out/${sitePackages}/sitecustomize.py
 
     '' + optionalString stripBytecode ''
     # Determinism: deterministic bytecode
