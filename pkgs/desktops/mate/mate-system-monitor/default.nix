1c4d9e4011b56f614ffe740150f5a9ac4ae84543
Bobby Rong <rjl931189261@126.com>
2023-10-14 15:38:57 +0000

    mate.mate-system-monitor: 1.26.0 -> 1.26.1
    https://github.com/mate-desktop/mate-system-monitor/compare/v1.26.0...v1.26.1

--- pkgs/desktops/mate/mate-system-monitor/default.nix
+++ pkgs/desktops/mate/mate-system-monitor/default.nix
@@ -17,11 +17,11 @@
 
 stdenv.mkDerivation rec {
   pname = "mate-system-monitor";
-  version = "1.26.0";
+  version = "1.26.1";
 
   src = fetchurl {
     url = "https://pub.mate-desktop.org/releases/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "13rkrk7c326ng8164aqfp6i7334n7zrmbg61ncpjprbrvlx2qiw3";
+    sha256 = "HrX7m2y0qK2DCyboR6m70B1WiqvTg8Yo7p8IQJuJKOc=";
   };
 
   nativeBuildInputs = [
