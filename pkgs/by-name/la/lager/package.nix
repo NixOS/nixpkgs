8dd11c5f193394159d88e52fbadab6d8930c8a4f
nek0 <nek0@nek0.eu>
2023-10-15 03:56:35 +0200

    lager: init at 0.1.0

---
+++ pkgs/by-name/la/lager/package.nix
@@ -0,0 +1,34 @@
+{ callPackage, stdenv, lib
+, fetchFromGitHub
+, boost, immer, zug
+, pkg-config, cmake
+}:
+
+stdenv.mkDerivation rec {
+  pname = "lager";
+  version = "0.1.0";
+  src = fetchFromGitHub {
+    owner = "arximboldi";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-KTHrVV/186l4klwlcfDwFsKVoOVqWCUPzHnIbWuatbg=";
+  };
+  buildInputs = [
+    boost
+    immer
+    zug
+  ];
+  nativeBuildInputs = [
+    cmake
+  ];
+  cmakeFlags = [
+    "-Dlager_BUILD_TESTS=OFF"
+    "-Dlager_BUILD_EXAMPLES=OFF"
+  ];
+  meta = with lib; {
+    homepage    = "https://github.com/arximboldi/lager";
+    description = "library for functional interactive c++ programs";
+    license     = licenses.lgpl3Plus;
+    maintainers = with maintainers; [ nek0 ];
+  };
+}
