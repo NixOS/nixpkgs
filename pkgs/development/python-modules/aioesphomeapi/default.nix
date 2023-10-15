c86e815f7c4649ffe3eac7e7fab31706361cef99
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-15 11:46:59 +0200

    python311Packages.aioesphomeapi: 17.2.0 -> 18.0.3
    Diff: https://github.com/esphome/aioesphomeapi/compare/refs/tags/v17.2.0...18.0.0
    
    Changelog: https://github.com/esphome/aioesphomeapi/releases/tag/v18.0.0
    
    python311Packages.aioesphomeapi: 18.0.0 -> 18.0.1
    
    Diff: https://github.com/esphome/aioesphomeapi/compare/refs/tags/v18.0.0...v18.0.1
    
    Changelog: https://github.com/esphome/aioesphomeapi/releases/tag/v18.0.1
    
    python311Packages.aioesphomeapi: 18.0.1 -> 18.0.2
    
    Diff: https://github.com/esphome/aioesphomeapi/compare/refs/tags/v18.0.1...18.0.2
    
    Changelog: https://github.com/esphome/aioesphomeapi/releases/tag/v18.0.2
    
    python311Packages.aioesphomeapi: 18.0.2 -> 18.0.3
    
    Diff: https://github.com/esphome/aioesphomeapi/compare/refs/tags/v18.0.2...v18.0.3
    
    Changelog: https://github.com/esphome/aioesphomeapi/releases/tag/v18.0.3

--- pkgs/development/python-modules/aioesphomeapi/default.nix
+++ pkgs/development/python-modules/aioesphomeapi/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aioesphomeapi";
-  version = "17.2.0";
+  version = "18.0.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "esphome";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-+yPHIXJ0vHaFO2X3xN+7WIQUlCvoYlGi1N7W+H/ng/0=";
+    hash = "sha256-nV7MtdzwZozgLtDu3B92ZL9/yvXNU9Z2xZ24357ZMWo=";
   };
 
   propagatedBuildInputs = [
