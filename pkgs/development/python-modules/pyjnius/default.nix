29aa3b349c5b89148ce8a0e52baba48b950add39
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-15 09:09:42 +0000

    python310Packages.pyjnius: 1.5.0 -> 1.6.0

--- pkgs/development/python-modules/pyjnius/default.nix
+++ pkgs/development/python-modules/pyjnius/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "pyjnius";
-  version = "1.5.0";
+  version = "1.6.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ZjRuJk8eIghrh8XINonqvP7xRQrGR2/YVr6kmLLhNz4=";
+    hash = "sha256-C32+PY9Yu7e+wwyFjz+nibzBwexJMZWOn3uH9F6hQDM=";
   };
 
   nativeBuildInputs = [
