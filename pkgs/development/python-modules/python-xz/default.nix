bbe035cd905e8e385aeacaa28e3d0c62de18ad8c
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-16 10:12:47 +0200

    python310Packages.python-xz: update disabled

--- pkgs/development/python-modules/python-xz/default.nix
+++ pkgs/development/python-modules/python-xz/default.nix
@@ -10,7 +10,7 @@ buildPythonPackage rec {
   version = "0.5.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
