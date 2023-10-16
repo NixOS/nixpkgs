3bb845a89dd2125a3e1482235a5bbe11dcb22cb8
Skye <me@skye.vg>
2023-10-16 23:11:56 +0900

    vesktop: vendor esbuild

--- pkgs/by-name/ve/vesktop/package.nix
+++ pkgs/by-name/ve/vesktop/package.nix
@@ -13,6 +13,8 @@
 , jq
 , moreutils
 , nodePackages
+, esbuild
+, buildGoModule
 }:
 stdenv.mkDerivation rec {
   pname = "vesktop";
@@ -67,17 +69,64 @@ stdenv.mkDerivation rec {
 
   ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
 
-  preBuild = ''
-    export HOME=$(mktemp -d)
-    export STORE_PATH=$(mktemp -d)
+  preBuild =
+    let
+      arch = if stdenv.isx86_64 then "x64" else
+      if stdenv.isi686 then "ia32" else
+      if stdenv.isAarch64 then "arm64" else
+      abort "vesktop: could not determine arch";
+      makePackageJson = version: builtins.toFile "package.json" (builtins.toJSON {
+        inherit version;
+        name = "@esbuild/linux-${arch}";
+      });
+      esbuildVesktop = lib.getExe (esbuild.override {
+        buildGoModule = args: buildGoModule (args // rec {
+          version = "0.18.17";
+          src = fetchFromGitHub {
+            owner = "evanw";
+            repo = "esbuild";
+            rev = "v${version}";
+            hash = "sha256-OnAOomKVUIBTEgHywDSSx+ggqUl/vn/R0JdjOb3lUho=";
+          };
+          vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
+        });
+      });
+      esbuildTsx = lib.getExe (esbuild.override {
+        buildGoModule = args: buildGoModule (args // rec {
+          version = "0.17.19";
+          src = fetchFromGitHub {
+            owner = "evanw";
+            repo = "esbuild";
+            rev = "v${version}";
+            hash = "sha256-PLC7OJLSOiDq4OjvrdfCawZPfbfuZix4Waopzrj8qsU=";
+          };
+          vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
+        });
+      });
+    in
+    ''
+      export HOME=$(mktemp -d)
+      export STORE_PATH=$(mktemp -d)
 
-    cp -r ${pnpm-deps}/* "$STORE_PATH"
-    chmod -R +w "$STORE_PATH"
+      cp -r ${pnpm-deps}/* "$STORE_PATH"
+      chmod -R +w "$STORE_PATH"
 
-    pnpm config set store-dir "$STORE_PATH"
-    pnpm install --offline --frozen-lockfile --ignore-script
-    patchShebangs node_modules/{*,.*}
-  '';
+      pnpm config set store-dir "$STORE_PATH"
+      pnpm install --offline --frozen-lockfile --ignore-script
+
+      # there are two copies of esbuild with different versions in the dependency tree.
+      # `ESBUILD_BINARY_PATH` will affect both of them, so it doesn't work.
+      # so I'm constructing fake esbuild native packages in their expected places.
+      mkdir -p node_modules/@esbuild-kit/core-utils/node_modules/esbuild/node_modules/@esbuild/linux-${arch}/bin
+      cp ${makePackageJson "0.17.19"} node_modules/@esbuild-kit/core-utils/node_modules/esbuild/node_modules/@esbuild/linux-${arch}/package.json
+      cp ${esbuildTsx} node_modules/@esbuild-kit/core-utils/node_modules/esbuild/node_modules/@esbuild/linux-${arch}/bin/esbuild
+
+      mkdir -p node_modules/esbuild/node_modules/@esbuild/linux-${arch}/bin
+      cp ${makePackageJson "0.18.17"} node_modules/esbuild/node_modules/@esbuild/linux-${arch}/package.json
+      cp ${esbuildVesktop} node_modules/esbuild/node_modules/@esbuild/linux-${arch}/bin/esbuild
+
+      patchShebangs node_modules/{*,.*}
+    '';
 
   postBuild = ''
     pnpm build
