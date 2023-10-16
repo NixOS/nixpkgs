3c54f29ccae456d08f75ae1c494dbca27272fdad
Jean-François Roche <jfroche@pyxel.be>
2023-10-16 12:12:00 +0200

    dagger: remove
    It violates the dagger trademark policy to use `dagger` name and compile from
    source.
    
    closes #260848

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4954,8 +4954,6 @@ with pkgs;
 
   daemontools = callPackage ../tools/admin/daemontools { };
 
-  dagger = callPackage ../development/tools/continuous-integration/dagger { };
-
   dale = callPackage ../development/compilers/dale { };
 
   damon = callPackage ../tools/admin/damon { };
