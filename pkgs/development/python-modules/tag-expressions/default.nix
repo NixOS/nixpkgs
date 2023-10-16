e08e9c6057bcd6503cacfcb8d513d5c44bf100fa
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-16 07:30:17 +0000

    python310Packages.tag-expressions: 1.1.0 -> 2.0.0

--- pkgs/development/python-modules/tag-expressions/default.nix
+++ pkgs/development/python-modules/tag-expressions/default.nix
@@ -6,11 +6,11 @@
 
 buildPythonPackage rec {
   pname = "tag-expressions";
-  version = "1.1.0";
+  version = "2.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1c0a49c3c0357976822b03c43db8d4a1c5548e16fb07ac939c10bbd5183f529d";
+    sha256 = "sha256-/6Ym72jlgVdpel4V2W2aCKNtISDT9y5qz7+gTllUuPg=";
   };
 
   nativeCheckInputs = [ pytestCheckHook ];
