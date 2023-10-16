92c6003ad6877cdea48278684fca4f16ecfa86f4
natsukium <tomoya.otabi@gmail.com>
2023-10-16 17:59:59 +0900

    python311Packages.jinja2-git: unstable-2021-07-20 -> 1.3.0
    Diff: https://github.com/wemake-services/jinja2-git/compare/c6d19b207eb6ac07182dc8fea35251d286c82512...1.3.0
    
    Changelog: https://github.com/wemake-services/jinja2-git/blob/1.3.0/CHANGELOG.md

--- pkgs/development/python-modules/jinja2-git/default.nix
+++ pkgs/development/python-modules/jinja2-git/default.nix
@@ -7,17 +7,14 @@
 
 buildPythonPackage rec {
   pname = "jinja2-git";
-  version = "unstable-2021-07-20";
+  version = "1.3.0";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "wemake-services";
     repo = "jinja2-git";
-    # this is master, we can't patch because of poetry.lock :(
-    # luckily, there appear to have been zero API changes since then, only
-    # dependency upgrades
-    rev = "c6d19b207eb6ac07182dc8fea35251d286c82512";
-    sha256 = "0yw0318w57ksn8azmdyk3zmyzfhw0k281fddnxyf4115bx3aph0g";
+    rev = "refs/tags/${version}";
+    hash = "sha256-XuN2L3/HLcZ/WPWiCtufDOmkxj+q4I6IOgjrGQHfNLk=";
   };
 
   nativeBuildInputs = [ poetry-core ];
