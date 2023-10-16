00cf776782e47f3197c134a03496f91a53b2fa78
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-16 10:12:05 +0200

    python310Packages.python-xz: add changelog to meta

--- pkgs/development/python-modules/python-xz/default.nix
+++ pkgs/development/python-modules/python-xz/default.nix
@@ -17,18 +17,22 @@ buildPythonPackage rec {
     hash = "sha256-oYjwQ26BFFXxvaYdzp2+bw/BQwM0v/n1r9DmaLs1R3Q=";
   };
 
-  nativeBuildInputs = [ setuptools-scm ];
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
 
   # has no tests
   doCheck = false;
 
-  pythonImportsCheck = [ "xz" ];
+  pythonImportsCheck = [
+    "xz"
+  ];
 
   meta = with lib; {
     description = "Pure Python library for seeking within compressed xz files";
     homepage = "https://github.com/Rogdham/python-xz";
+    changelog = "https://github.com/Rogdham/python-xz/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with lib.maintainers; [ mxmlnkn ];
-    platforms = platforms.all;
   };
 }
