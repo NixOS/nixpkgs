2cb54d2e1f219cd30fcdd3361c72ab960576ce82
Stig Palmquist <git@stig.io>
2023-10-16 20:48:08 +0200

    perlPackages.FileFindObject: cleanups

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -9437,6 +9437,7 @@ with self; {
       url = "mirror://cpan/authors/id/S/SH/SHLOMIF/File-Find-Object-0.3.8.tar.gz";
       hash = "sha256-TlJRRt6GTt+8kJsIRGKe7O0AY7YdQYuXLu8D+ES7NRQ=";
     };
+    buildInputs = [ FileTreeCreate TestFile ];
     propagatedBuildInputs = [ ClassXSAccessor ];
     meta = {
       description = "An object oriented File::Find replacement";
