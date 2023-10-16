c2c5d261ab70892234ab72302d01158585795c99
Mynacol <Mynacol@users.noreply.github.com>
2023-10-16 21:19:03 +0200

    rss-bridge: Move substitutions into patch file
    As requested in https://github.com/NixOS/nixpkgs/pull/257641#pullrequestreview-1678477898

--- pkgs/servers/web-apps/rss-bridge/default.nix
+++ pkgs/servers/web-apps/rss-bridge/default.nix
@@ -11,14 +11,9 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-N1pbveOgJrB1M+WelKD07Jmv9Vz5NqT+IJf//L8UEnU=";
   };
 
-  postPatch = ''
-    substituteInPlace lib/RssBridge.php \
-        --replace "__DIR__ . '/../config.ini.php'" "getenv('RSSBRIDGE_DATA') . '/config.ini.php'"
-    substituteInPlace lib/bootstrap.php \
-        --replace "const PATH_CACHE = __DIR__ . '/../cache/';" "define('PATH_CACHE', getenv('RSSBRIDGE_DATA') . '/cache/');"
-    substituteInPlace lib/Configuration.php \
-        --replace "__DIR__ . '/../whitelist.txt'" "getenv('RSSBRIDGE_DATA') . '/whitelist.txt'"
-  '';
+  patches = [
+    ./paths.patch
+  ];
 
   installPhase = ''
     mkdir $out/
