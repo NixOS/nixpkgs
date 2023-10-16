64afc1b01eafe6615be4621366123aec6b8c0786
Stig Palmquist <git@stig.io>
2023-10-16 20:56:18 +0200

    perlPackages.UserIdentity: cleanups

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -27116,6 +27116,7 @@ with self; {
       url = "mirror://cpan/authors/id/M/MA/MARKOV/User-Identity-1.02.tar.gz";
       hash = "sha256-OySu5/UnjGXD8EEVsHyG5kaTTpnqQJJANj8wiZE+uJk=";
     };
+    propagatedBuildInputs = [ HashOrdered ];
     meta = {
       description = "Collect information about a user";
       homepage = "http://perl.overmeer.net/CPAN";
