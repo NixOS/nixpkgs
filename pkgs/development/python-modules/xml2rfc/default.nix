4b596ec80c3e6e9198adfb5bd1227763b15bb1f4
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-16 22:58:02 +0200

    python310Packages.xml2rfc: update disabled

--- pkgs/development/python-modules/xml2rfc/default.nix
+++ pkgs/development/python-modules/xml2rfc/default.nix
@@ -30,7 +30,7 @@ buildPythonPackage rec {
   version = "3.18.2";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "ietf-tools";
