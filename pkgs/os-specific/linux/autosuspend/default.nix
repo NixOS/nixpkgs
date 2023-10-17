6037953f3ff23038ca660d4dbbbc8055211cab60
Anthony Roussel <anthony@roussel.dev>
2023-10-18 00:30:29 +0200

    autosuspend: 4.3.0 -> 6.0.0
    https://github.com/languitar/autosuspend/releases/tag/v6.0.0
    https://github.com/languitar/autosuspend/releases/tag/v5.0.0
    https://github.com/languitar/autosuspend/releases/tag/v4.3.3
    https://github.com/languitar/autosuspend/releases/tag/v4.3.2
    https://github.com/languitar/autosuspend/releases/tag/v4.3.1

--- pkgs/os-specific/linux/autosuspend/default.nix
+++ pkgs/os-specific/linux/autosuspend/default.nix
@@ -5,13 +5,15 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "autosuspend";
-  version = "4.3.0";
+  version = "6.0.0";
+
+  disabled = python3.pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "languitar";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-gS8NNks4GaIGl7cEqWSP53I4/tIV4LypkmZ5vNOjspY=";
+    hash = "sha256-gS8NNks4GaIGl7cEqWSP53I4/tIV4LypkmZ5vNOjspY=";
   };
 
   postPatch = ''
@@ -53,8 +55,10 @@ python3.pkgs.buildPythonApplication rec {
   meta = with lib ; {
     description = "A daemon to automatically suspend and wake up a system";
     homepage = "https://autosuspend.readthedocs.io";
+    changelog = "https://github.com/languitar/autosuspend/releases/tag/v${version}";
     license = licenses.gpl2Only;
     maintainers = [ maintainers.bzizou ];
+    mainProgram = "autosuspend";
     platforms = platforms.linux;
   };
 }
