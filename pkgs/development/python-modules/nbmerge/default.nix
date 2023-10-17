305c120f313c2bdc6013fd8ea4546e3b171d3b28
natsukium <tomoya.otabi@gmail.com>
2023-10-17 14:35:00 +0900

    python311Packages.nbmerge: refactor
    - add mainProgram to meta
    - add pythonImportsCheck
    - introduce `pyproject = true`
    - add runHook to checkPhase

--- pkgs/development/python-modules/nbmerge/default.nix
+++ pkgs/development/python-modules/nbmerge/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, setuptools
 , nbformat
 , nose
 }:
@@ -8,6 +9,7 @@
 buildPythonPackage rec {
   pname = "nbmerge";
   version = "0.0.4";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jbn";
@@ -16,19 +18,33 @@ buildPythonPackage rec {
     hash = "sha256-Uqs/SO/AculHCFYcbjW08kLQX5GSU/eAwkN2iy/vhLM=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [ nbformat ];
+
   nativeCheckInputs = [ nose ];
 
   checkPhase = ''
+    runHook preCheck
+
     patchShebangs .
     nosetests -v
     PATH=$PATH:$out/bin ./cli_tests.sh
+
+    runHook postCheck
   '';
+
+  pythonImportsCheck = [
+    "nbmerge"
+  ];
 
   meta = {
     description = "A tool to merge/concatenate Jupyter (IPython) notebooks";
     inherit (src.meta) homepage;
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ];
+    mainProgram = "nbmerge";
   };
 }
