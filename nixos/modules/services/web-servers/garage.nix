5a4bea9bbbbcbcc180093a61eb435a9b24d7de63
Florian Klink <flokli@flokli.de>
2023-10-17 13:52:48 +0100

    nixos/garage: drop default package
    This gives better control over what package of garage is running, and
    makes upgrades a conscious decision.

--- nixos/modules/services/web-servers/garage.nix
+++ nixos/modules/services/web-servers/garage.nix
@@ -65,12 +65,8 @@ in
     };
 
     package = mkOption {
-      # TODO: when 23.05 is released and if Garage 0.9 is the default, put a stateVersion check.
-      default = if versionAtLeast config.system.stateVersion "23.05" then pkgs.garage_0_8
-                else pkgs.garage_0_7;
-      defaultText = literalExpression "pkgs.garage_0_7";
       type = types.package;
-      description = lib.mdDoc "Garage package to use, if you are upgrading from a major version, please read NixOS and Garage release notes for upgrade instructions.";
+      description = lib.mdDoc "Garage package to use, needs to be set explicitly. If you are upgrading from a major version, please read NixOS and Garage release notes for upgrade instructions.";
     };
   };
 
