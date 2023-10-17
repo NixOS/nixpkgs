369fdb3be8dfd6ce45a61152d161b2c9c0c8a873
Aaron Jheng <wentworth@outlook.com>
2023-10-17 14:09:12 +0800

    gptcommit: 0.5.13 -> 0.5.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8808,7 +8808,7 @@ with pkgs;
   gpt2tc = callPackage ../tools/text/gpt2tc { };
 
   gptcommit = callPackage ../development/tools/gptcommit {
-    inherit (darwin.apple_sdk.frameworks) Security;
+    inherit (darwin.apple_sdk.frameworks) Security SystemConfiguration;
   };
 
   gptman = callPackage ../tools/system/gptman { };
