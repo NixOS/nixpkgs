b6aa363fff52131a4f329c8cdde8b4896e50aa58
Richard Marko <srk@48.io>
2023-10-17 19:05:14 +0200

    nixos/tests/uboot: add timeout tests

--- nixos/tests/uboot.nix
+++ nixos/tests/uboot.nix
@@ -4,11 +4,12 @@
 }:
 let
   testing = import ../lib/testing-python.nix { inherit system pkgs; };
-  biosBin = pkgs.runCommand "u-boot.rom" {}
+  makeBiosBin = pkg: pkgs.runCommand "u-boot.rom" {}
   ''
     mkdir $out
-    cp ${pkgs.ubootQemuX86}/u-boot.rom $out/bios.bin
+    cp ${pkg}/u-boot.rom $out/bios.bin
   '';
+  biosBin = makeBiosBin pkgs.ubootQemuX86;
 in {
   extlinuxSimple = testing.makeTest {
     name = "ubootExtlinuxSimple";
@@ -62,4 +63,72 @@ in {
         machine.wait_for_unit("multi-user.target")
       '';
   };
+
+  # test for boot.loader.timeout = null
+  # which should cause uboot to display menu
+  # until selection is made
+  extlinuxNullTimeout = testing.makeTest {
+    name = "ubootExtlinuxNullTimeout";
+
+    nodes.machine = { ... }: {
+      boot.loader.grub.enable = false;
+      boot.loader.generic-extlinux-compatible.enable = true;
+      boot.loader.timeout = null;
+
+      virtualisation.bios = biosBin;
+      virtualisation.useBootLoader = true;
+      virtualisation.qemu.options = [ "-accel tcg" ];
+    };
+
+    testScript =
+      ''
+        start_all()
+        machine.wait_for_console_text("NixOS - Default")
+        machine.send_monitor_command("sendkey 1")
+        machine.send_monitor_command("sendkey ret")
+        machine.wait_for_unit("multi-user.target")
+      '';
+  };
+
+  # test for boot.loader.timeout = 0
+  # which should cause uboot to skip menu
+  # and boot instantly
+  extlinuxZeroTimeout = testing.makeTest {
+    name = "ubootExtlinuxZeroTimeout";
+
+    nodes.machine = { ... }:
+    let
+      # we use a customized uboot here
+      # that won't allow breaking boot w/o Ctrl-C
+      unbreakable = pkgs.ubootQemuX86.override {
+        extraConfig =
+          pkgs.ubootQemuX86.extraConfig +
+          ''
+            CONFIG_BOOTDELAY=0
+            CONFIG_AUTOBOOT_KEYED=y
+            CONFIG_AUTOBOOT_KEYED_CTRLC=y
+            CONFIG_AUTOBOOT_PROMPT="Hit Ctrl-C to abort in %d\n"
+          '';
+      };
+      unbreakableBiosBin = makeBiosBin unbreakable;
+    in
+    {
+      boot.loader.grub.enable = false;
+      boot.loader.generic-extlinux-compatible.enable = true;
+      boot.loader.timeout = 0;
+
+      virtualisation.bios = unbreakableBiosBin;
+      virtualisation.useBootLoader = true;
+      virtualisation.qemu.options = [ "-accel tcg" ];
+    };
+
+    testScript =
+      ''
+        start_all()
+        for i in range(100):
+          machine.send_monitor_command("sendkey ret")
+        machine.wait_for_unit("multi-user.target")
+      '';
+  };
+
 }
