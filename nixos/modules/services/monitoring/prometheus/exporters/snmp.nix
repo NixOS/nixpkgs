606ff6fdf60cfb39e45ee62f40b5160b7104d91a
Julian Jacobi <julian.jacobi@wobcom.de>
2023-10-17 12:21:23 +0200

    nixos/prometheus-snmp-exporter: add generator options
    Add following options
      - services.prometheus.exporters.snmp.generator.configuration
      - services.prometheus.exporters.snmp.generator.configurationPath
      - services.promethesu.exporters.snmp.generator.mibDirs
    
    Allows users to use snmp-exporter's config file generator in nix build
    process. When generator options set run generator at build time to
    generate snmp-exporter config from generator config file and provided
    directories containing the corresponding MIB files.

--- nixos/modules/services/monitoring/prometheus/exporters/snmp.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/snmp.nix
@@ -12,7 +12,7 @@ in
       type = types.nullOr types.path;
       default = null;
       description = lib.mdDoc ''
-        Path to a snmp exporter configuration file. Mutually exclusive with 'configuration' option.
+        Path to a snmp exporter configuration file. Mutually exclusive with 'configuration', 'generator.configurationPath' and 'generator.configuration' options.
       '';
       example = literalExpression "./snmp.yml";
     };
@@ -21,7 +21,7 @@ in
       type = types.nullOr types.attrs;
       default = null;
       description = lib.mdDoc ''
-        Snmp exporter configuration as nix attribute set. Mutually exclusive with 'configurationPath' option.
+        Snmp exporter configuration as nix attribute set. Mutually exclusive with 'configurationPath' and 'generator.configuration' options.
       '';
       example = {
         "default" = {
@@ -30,6 +30,68 @@ in
             "community" = "public";
           };
         };
+      };
+    };
+
+    generator = {
+      configurationPath = mkOption {
+        type = types.nullOr types.path;
+        default = null;
+        description = lib.mdDoc ''
+          Path to a snmp exporter configuration generator configuration. Mutually exclusive with 'configurationPath', 'configuration' and 'generator.configuration' options.
+        '';
+        example = literalExpression "./generator.yml";
+      };
+
+      configuration = mkOption {
+        type = types.nullOr types.attrs;
+        default = null;
+        description = lib.mdDoc ''
+          Snmp exporter configuration generator configuration as nix attribute set. Mutually exclusive with 'configurationPath', 'configuration' and 'generator.configurationPath' options.
+        '';
+        example = {
+          "modules" = {
+            "if_mib" = {
+              "walk" = [ "sysUpTime" "interfaces" "ifXTable" ];
+              "lookups" = [
+                {
+                  "source_indexes" = [ "ifIndex" ];
+                  "lookup" = "ifAlias";
+                }
+                {
+                  "source_indexes" = [ "ifIndex" ];
+                  "lookup" = "1.3.6.1.2.1.2.2.1.2";
+                }
+                {
+                  "source_indexes" = [ "ifIndex" ];
+                  "lookup" = "1.3.6.1.2.1.31.1.1.1.1";
+                }
+              ];
+              "overrides" = {
+                "ifAlias" = {
+                  "ignore" = true;
+                };
+                "ifDescr" = {
+                  "ignore" = true;
+                };
+                "ifName" = {
+                  "ignore" = true;
+                };
+                "ifType" = {
+                  "type" = "EnumAsInfo";
+                };
+              };
+            };
+          };
+        };
+      };
+
+      mibDirs = mkOption {
+        type = types.listOf types.path;
+        default = [];
+        description = lib.mdDoc ''
+          Snmp exporter configuration generator MIBs directories as list of paths.
+        '';
       };
     };
 
@@ -50,9 +112,22 @@ in
     };
   };
   serviceOpts = let
+    generatorConfigFile = if cfg.generator.configurationPath != null
+                          then cfg.generator.configurationPath
+                          else "${pkgs.writeText "snmp-exporter-generator.yml" (builtins.toJSON cfg.generator.configuration)}";
+
+    generatedConfigFile = pkgs.runCommand "snmp.yml" { MIBDIRS = concatStringsSep ":" cfg.generator.mibDirs; } ''
+      ln -s ${generatorConfigFile} ./generator.yml
+      ${pkgs.prometheus-snmp-exporter.outPath}/bin/generator generate && mv snmp.yml $out
+    '';
+
     configFile = if cfg.configurationPath != null
                  then cfg.configurationPath
-                 else "${pkgs.writeText "snmp-exporter-conf.yml" (builtins.toJSON cfg.configuration)}";
+                 else (
+                   if cfg.configuration != null
+                   then "${pkgs.writeText "snmp-exporter-conf.yml" (builtins.toJSON cfg.configuration)}"
+                   else generatedConfigFile
+                 );
     in {
     serviceConfig = {
       ExecStart = ''
