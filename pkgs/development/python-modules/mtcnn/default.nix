17449ad788082ab07cca7a474dde48a30728caa8
Rakesh Gupta <rakesh.gupta@formbay.com.au>
2023-10-17 12:49:40 +0530

    python3Packages.mtcnn: init at 0.1.1

--- pkgs/development/python-modules/mtcnn/default.nix
+++ pkgs/development/python-modules/mtcnn/default.nix
@@ -3,7 +3,7 @@
 , fetchPypi
 , opencv3
 , tensorflow
-, Keras
+, keras
 }:
 
 
@@ -24,7 +24,7 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [
     opencv3
-    Keras
+    keras
     tensorflow
   ];
 
