ef45139f3f617efd5a28849c553158d5edf56d54
Anderson Torres <torres.anderson.85@protonmail.com>
2023-10-17 00:10:31 -0300

    cosmocc: move to cosmopolitan
    Also, a refactor.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20921,7 +20921,7 @@ with pkgs;
 
   cog = callPackage ../development/web/cog { };
 
-  cosmocc = callPackage ../development/tools/cosmocc { };
+  inherit (cosmopolitan) cosmocc;
 
   python-cosmopolitan = callPackage ../development/interpreters/python-cosmopolitan { };
 
