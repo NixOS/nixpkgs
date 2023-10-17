5986a42f4050eced1a1a80e921c6a9a8d6923fac
Stanisław Pitucha <git@viraptor.info>
2023-10-17 14:24:33 +1100

    libcpuid: 0.6.3 -> 0.6.4

--- pkgs/tools/misc/libcpuid/default.nix
+++ pkgs/tools/misc/libcpuid/default.nix
@@ -2,24 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "libcpuid";
-  version = "0.6.3";
+  version = "0.6.4";
 
   src = fetchFromGitHub {
     owner = "anrieff";
     repo = "libcpuid";
     rev = "v${version}";
-    sha256 = "sha256-lhoHqdS5tke462guORg+PURjVmjAgviT5KJHp6PyvUA=";
+    sha256 = "sha256-Zs5GKvSasdfLqo8oErDQNAuXRG27Bm9vNwyooqbol0Q=";
   };
-
-  patches = [
-    # Fixes cross-compilation to NetBSD
-    # https://github.com/anrieff/libcpuid/pull/190
-    (fetchpatch {
-      name = "pass-pthread-to-linker.patch";
-      url = "https://github.com/anrieff/libcpuid/commit/c28436e7239f28dab0e2a3bcdbce95f41e1363b1.patch";
-      sha256 = "sha256-J2mB010JcE4si0rERjcrL9kJgbWHKaQCIZPDkmRvcq4=";
-    })
-  ];
 
   nativeBuildInputs = [ autoreconfHook ];
 
