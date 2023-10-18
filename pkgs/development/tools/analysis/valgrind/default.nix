NixOS/nixpkgs

commit cbc990308c2e4995681b827488a90cc9c30de1af

valgrind: 3.20.0 -> 3.21.0
Changes: https://valgrind.org/docs/manual/dist.news.html

pkgs/development/tools/analysis/valgrind/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "valgrind";
-  version = "3.20.0";
+  version = "3.21.0";
 
   src = fetchurl {
     url = "https://sourceware.org/pub/${pname}/${pname}-${version}.tar.bz2";
-    sha256 = "sha256-hTbAMdvgeNNC8SH6iBqezSBctaeOY5AFrVcAEb2588Y=";
+    hash = "sha256-EM4WGLs+M/rRbreVUrCj4SEXYkSKDX/OEcimJDuayXE=";
   };
 
   patches = [

commit 42cd7920ad3f37009dc12d40ad658b97b5ccb7ae

pkgsCross.x86_64-freebsd.valgrind-light: fix build
Valgrind got a bit stricter about the format of uname -r output.

Fixes: cbc990308c2e ("valgrind: 3.20.0 -> 3.21.0")

pkgs/development/tools/analysis/valgrind/default.nix
@@ -58,7 +58,7 @@ stdenv.mkDerivation rec {
 
   preConfigure = lib.optionalString stdenv.isFreeBSD ''
     substituteInPlace configure --replace '`uname -r`' \
-        ${toString stdenv.hostPlatform.parsed.kernel.version}.0
+        ${toString stdenv.hostPlatform.parsed.kernel.version}.0-
   '' + lib.optionalString stdenv.isDarwin (
     let OSRELEASE = ''
       $(awk -F '"' '/#define OSRELEASE/{ print $2 }' \