57c2850386b4d59f018bd6272d7d1532f4eedbba
K900 <me@0upti.me>
2023-10-19 14:26:37 +0300

    qt5: update to latest upstream patchset
    Fixes build with libxkbcommon 1.6.0, includes crash fix upstream.

--- pkgs/development/libraries/qt-5/5.15/default.nix
+++ pkgs/development/libraries/qt-5/5.15/default.nix
@@ -46,7 +46,6 @@ let
       ./qtbase.patch.d/0009-qtbase-qtpluginpath.patch
       ./qtbase.patch.d/0010-qtbase-assert.patch
       ./qtbase.patch.d/0011-fix-header_module.patch
-      ./qtbase.patch.d/9999-backport-dbus-crash.patch
     ];
     qtdeclarative = [
       ./qtdeclarative.patch
