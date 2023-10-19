37f8f6681c761a5788b633f1da8f1f8a940bfabc
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-10-19 02:24:29 +0200

    tests.nixpkgs-check-by-name: Intermediate error type refactoring prep
    Currently the tool prints problems right as it is checking the code
    without an intermediate error representation. However for various reasons
    it would be beneficial to have an intermediate error type:
    - It makes the code cleaner, having all errors in one place
    - It allows printing the error in different ways, e.g. for a future
      --json mode
    
    This commit prepares for an incremental refactoring for an intermediate
    error/problem representation. Most notable is that we want to be able to collect
    multiple errors/problems and not just exit on the first one.
    
    We introduce the type alias CheckResult and CheckError (later renamed to
    NixpkgsProblem), where CheckError allows collecting multiple
    CheckErrors using the utility function flatten_check_results (later
    renamed to check_result::sequence)
    
    The write_check_result function is only temporarily introduced to help
    refactoring, it's removed again in later commits.

---
+++ pkgs/test/nixpkgs-check-by-name/src/check_result.rs
@@ -0,0 +1,61 @@
+use crate::ErrorWriter;
+use itertools::{Either, Itertools};
+use std::fmt;
+use std::io;
+use std::path::PathBuf;
+
+pub enum CheckError {}
+
+impl CheckError {
+    pub fn into_result<A>(self) -> CheckResult<A> {
+        Ok(Either::Left(vec![self]))
+    }
+}
+
+impl fmt::Display for CheckError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {}
+    }
+}
+
+pub fn write_check_result<A, W: io::Write>(
+    error_writer: &mut ErrorWriter<W>,
+    check_result: CheckResult<A>,
+) -> anyhow::Result<Option<A>> {
+    match check_result? {
+        Either::Left(errors) => {
+            for error in errors {
+                error_writer.write(&error.to_string())?
+            }
+            Ok(None)
+        }
+        Either::Right(value) => Ok(Some(value)),
+    }
+}
+
+pub fn pass<A>(value: A) -> CheckResult<A> {
+    Ok(Either::Right(value))
+}
+
+pub type CheckResult<A> = anyhow::Result<Either<Vec<CheckError>, A>>;
+
+pub fn flatten_check_results<I, O>(
+    check_results: impl IntoIterator<Item = CheckResult<I>>,
+    value_transform: impl Fn(Vec<I>) -> O,
+) -> CheckResult<O> {
+    let (errors, values): (Vec<_>, Vec<_>) = check_results
+        .into_iter()
+        .collect::<anyhow::Result<Vec<_>>>()?
+        .into_iter()
+        .partition_map(|r| r);
+
+    // To combine the errors from the results we flatten all the error Vec's into a new Vec
+    // This is not very efficient, but doesn't matter because generally we should have no errors
+    let flattened_errors = errors.into_iter().flatten().collect::<Vec<_>>();
+
+    if flattened_errors.is_empty() {
+        Ok(Either::Right(value_transform(values)))
+    } else {
+        Ok(Either::Left(flattened_errors))
+    }
+}
--- pkgs/test/nixpkgs-check-by-name/src/main.rs
+++ pkgs/test/nixpkgs-check-by-name/src/main.rs
@@ -1,3 +1,4 @@
+mod check_result;
 mod eval;
 mod references;
 mod structure;
