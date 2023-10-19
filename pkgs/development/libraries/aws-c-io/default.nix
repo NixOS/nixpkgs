4c8031d98cfb660791cc03359d11348f10b82438
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-19 15:03:21 +0200

    aws-c-io: 0.13.29 -> 0.13.35
    Diff: https://github.com/awslabs/aws-c-io/compare/refs/tags/v0.13.29...v0.13.35
    
    Changelog: https://github.com/awslabs/aws-c-io/releases/tag/v0.13.35

--- pkgs/development/libraries/aws-c-io/default.nix
+++ pkgs/development/libraries/aws-c-io/default.nix
@@ -1,20 +1,38 @@
-{ lib, stdenv, fetchFromGitHub, cmake, aws-c-cal, aws-c-common, nix, s2n-tls, Security }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, aws-c-cal
+, aws-c-common
+, nix
+, s2n-tls
+, Security
+}:
 
 stdenv.mkDerivation rec {
   pname = "aws-c-io";
-  version = "0.13.29";
+  version = "0.13.35";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-ZeogbjgpbqdCioLeb34CRol1Fa5BJOloAxxgE50yfQs=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-o6ZUfKBKZZczZFCiDpZVcC58DWDbRpaO2bBfptFlxiY=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+  ];
 
-  buildInputs = [ aws-c-cal aws-c-common s2n-tls ];
-  propagatedBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ Security ];
+  buildInputs = [
+    aws-c-cal
+    aws-c-common
+    s2n-tls
+  ];
+
+  propagatedBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
+    Security
+  ];
 
   cmakeFlags = [
     "-DBUILD_SHARED_LIBS=ON"
@@ -27,6 +45,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "AWS SDK for C module for IO and TLS";
     homepage = "https://github.com/awslabs/aws-c-io";
+    changelog = "https://github.com/awslabs/aws-c-io/releases/tag/v${version}";
     license = licenses.asl20;
     platforms = platforms.unix;
     maintainers = with maintainers; [ orivej ];
