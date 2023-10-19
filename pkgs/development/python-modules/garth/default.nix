5827f21fb87a4bd1fe376e867cfec1a5944150a2
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-19 07:07:58 +0000

    python311Packages.garth: 0.4.38 -> 0.4.39

--- pkgs/development/python-modules/garth/default.nix
+++ pkgs/development/python-modules/garth/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "garth";
-  version = "0.4.38";
+  version = "0.4.39";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-c+wSXADcgl7DpJJxGUus3oA4v+DmjGwjKfp0tJbcxb8=";
+    hash = "sha256-2jmgi0evGqVgtcPRTYpTWG0wFrG1i5W7ryZJslPweNc=";
   };
 
   nativeBuildInputs = [
