89e19b57da7e7c237ef1824615665637cb9f1584
Sascha Grunert <sgrunert@redhat.com>
2023-10-19 12:15:44 +0200

    crun: 1.9.2 -> 1.10
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- pkgs/applications/virtualization/crun/default.nix
+++ pkgs/applications/virtualization/crun/default.nix
@@ -39,13 +39,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "crun";
-  version = "1.9.2";
+  version = "1.10";
 
   src = fetchFromGitHub {
     owner = "containers";
     repo = pname;
     rev = version;
-    hash = "sha256-C2VPEtHJyO7azDmvH74AoCnNaCeJ7XOLlIIe3nay4Po=";
+    hash = "sha256-deva/IPyjcGGAnUIofev3RV2ia7uI+OgSiUz/GtHhxE=";
     fetchSubmodules = true;
   };
 
