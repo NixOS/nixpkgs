b0678c6c6d0cdd33e3922e56115a610f49914590
Alyssa Ross <hi@alyssa.is>
2023-10-20 22:31:19 +0000

    meson: 1.2.2 -> 1.2.3

--- pkgs/development/tools/build-managers/meson/default.nix
+++ pkgs/development/tools/build-managers/meson/default.nix
@@ -18,13 +18,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "meson";
-  version = "1.2.2";
+  version = "1.2.3";
 
   src = fetchFromGitHub {
     owner = "mesonbuild";
     repo = "meson";
     rev = "refs/tags/${version}";
-    hash = "sha256-SujnalAooIlzKZcjt0E4tSBO0J5QRkSE0X1PVpnbIF4=";
+    hash = "sha256-dgYYz3tQDG6Z4eE77WO2dXdardxVzzGaFLQ5znPcTlw=";
   };
 
   patches = [
@@ -74,15 +74,6 @@ python3.pkgs.buildPythonApplication rec {
       excludes = [
         "docs/yaml/objects/dep.yaml"
       ];
-    })
-
-    # Revert patch breaking mesa build on bindgen:
-    #   https://github.com/mesonbuild/meson/issues/12326
-    (fetchpatch {
-      name = "bindgen-flags-revert.patch";
-      url = "https://github.com/mesonbuild/meson/commit/d5546bdceaa2f3040d16a33fcbd824ba94b8cfde.patch";
-      hash = "sha256-qOSiWGkjfxJmUfXmqV05yl7wTgAIE0Z7qZqTko9f/LE=";
-      revert = true;
     })
   ];
 
