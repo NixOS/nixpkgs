64d6fbeb998c6f775df84da78faba5f08880457a
FliegendeWurst <2012gdwu+github@posteo.de>
2023-10-20 11:09:19 +0200

    wavebox: 10.117.21-2 -> 10.118.5-2
    Refactor to remove let.
    Use nix-update in update script.

--- pkgs/applications/networking/instant-messengers/wavebox/update.sh
+++ pkgs/applications/networking/instant-messengers/wavebox/update.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p coreutils curl jq
-set -euo pipefail
-
-cd $(dirname "${BASH_SOURCE[0]}")
-
-setKV () {
-    sed -i "s|$2 = \".*\"|$2 = \"${3:-}\"|" $1
-}
+#!nix-shell -i bash -p nix-update curl jq
 
 version=$(curl "https://download.wavebox.app/stable/linux/latest.json" | jq --raw-output '.["urls"]["tar"] | match("https://download.wavebox.app/stable/linux/tar/Wavebox_(.+).tar.gz").captures[0]["string"]')
-
-sha256_linux64=$(nix-prefetch-url --quiet https://download.wavebox.app/stable/linux/tar/Wavebox_${version}.tar.gz)
-setKV ./default.nix version $version
-setKV ./default.nix sha256 "$sha256_linux64"
+nix-update wavebox --version "$version"
