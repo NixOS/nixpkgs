e2ece9e9e8e5ef37bfc1649b488cef257cff02b3
Atemu <atemu.main@gmail.com>
2023-10-21 18:21:37 +0200

    linux_xanmod: 6.1.57 -> 6.1.58
    (cherry picked from commit a744633b66dfce3f606a8f95a29011979583b70d)

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -3,8 +3,8 @@
 let
   # These names are how they are designated in https://xanmod.org.
   ltsVariant = {
-    version = "6.1.57";
-    hash = "sha256-ZzZj1Ij9L6WtjEJRcOdNtZ18oFRybNPV4uID6umC2Zg=";
+    version = "6.1.58";
+    hash = "sha256-Lnp1CSh1jLbIkEx9hLfxhdIA12iQZmywhOec9uZ7UjI=";
     variant = "lts";
   };
 
