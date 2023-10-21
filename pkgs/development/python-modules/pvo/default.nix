6e80d81f28d66e40ff0ad33d5f4b1e256bde0380
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-21 10:26:39 +0200

    python311Packages.pvo: update disabled

--- pkgs/development/python-modules/pvo/default.nix
+++ pkgs/development/python-modules/pvo/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "2.0.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.10";
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "frenck";
@@ -24,6 +24,13 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-SvsrvGwIAlj/8hdk90+rxigVrx6n3YInvF/4eux2H04=";
   };
+
+  postPatch = ''
+    # Upstream doesn't set a version for the pyproject.toml
+    substituteInPlace pyproject.toml \
+      --replace "0.0.0" "${version}" \
+      --replace "--cov" ""
+  '';
 
   nativeBuildInputs = [
     poetry-core
@@ -40,13 +47,6 @@ buildPythonPackage rec {
     pytest-asyncio
     pytestCheckHook
   ];
-
-  postPatch = ''
-    # Upstream doesn't set a version for the pyproject.toml
-    substituteInPlace pyproject.toml \
-      --replace "0.0.0" "${version}" \
-      --replace "--cov" ""
-  '';
 
   pythonImportsCheck = [
     "pvo"
