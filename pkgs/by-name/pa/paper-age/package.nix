b5b9315a6d42b8ffbfce5307bb6f3cb35e2958f6
Mario Rodas <marsam@users.noreply.github.com>
2023-10-22 04:20:00 +0000

    paper-age: 1.1.4 -> 1.2.0
    Diff: https://github.com/matiaskorhonen/paper-age/compare/v1.1.4...v1.2.0
    
    Changelog: https://github.com/matiaskorhonen/paper-age/blob/v1.2.0/CHANGELOG.md

--- pkgs/by-name/pa/paper-age/package.nix
+++ pkgs/by-name/pa/paper-age/package.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "paper-age";
-  version = "1.1.4";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "matiaskorhonen";
     repo = "paper-age";
     rev = "v${version}";
-    hash = "sha256-/8t+4iO+rYlc2WjjECq5mk8t0af/whmzatU63r9sO98=";
+    hash = "sha256-7dd1R41CDgkpFI8fUWCJfgz3lr22IjWQYW6vRYEFidc=";
   };
 
-  cargoHash = "sha256-lLY3PINWGpdnNojIPT+snvLJTH4UEM7JWXdqrOLxibY=";
+  cargoHash = "sha256-IJDV0dmOsA9gbVKGfPsN3TKJbox3JTNxldArQK6GPt8=";
 
   meta = with lib; {
     description = "Easy and secure paper backups of secrets";
