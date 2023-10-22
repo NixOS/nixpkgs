0852b76df1fbaa457cdefffc2fa9a1fde0ab148a
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-22 14:38:32 +0000

    cinny-desktop: 2.2.6 -> 3.0.0

--- pkgs/applications/networking/instant-messengers/cinny-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/cinny-desktop/default.nix
@@ -15,18 +15,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cinny-desktop";
-  version = "2.2.6";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "cinnyapp";
     repo = "cinny-desktop";
     rev = "v${version}";
-    hash = "sha256-RW6LeItIAHJk1e7qMa1MLIGb3jHvJ/KM8E9l1qR48w8=";
+    hash = "sha256-lG1EPBPO0Fr/1sXQ2RD7dXZnuOmntLeMJNENTnSCt5E=";
   };
 
   sourceRoot = "${src.name}/src-tauri";
 
-  cargoHash = "sha256-Iab/icQ9hFVh/o6egZVPa2zeKgO5WxzkluhRckcayvw=";
+  cargoHash = "sha256-27/JeQ1CeLRsN0Qct+/LcenSMaYgsD+JjRAFutz5IZw=";
 
   postPatch = ''
     substituteInPlace tauri.conf.json \
