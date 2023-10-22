7b6cd7d3ecfcb4ba6714d6277c23cbf9df7d3324
Adam Joseph <adam@westernsemico.com>
2023-10-22 06:10:00 -0700

    lib.tests.systems: include lib.systems.doubles.vc4

--- lib/tests/systems.nix
+++ lib/tests/systems.nix
@@ -36,7 +36,7 @@ lib.runTests (
 # sanity check before committing :).
 
 (with lib.systems.doubles; {
-  testall = mseteq all (linux ++ darwin ++ freebsd ++ openbsd ++ netbsd ++ illumos ++ wasi ++ windows ++ embedded ++ mmix ++ js ++ genode ++ redox);
+  testall = mseteq all (linux ++ darwin ++ freebsd ++ openbsd ++ netbsd ++ illumos ++ wasi ++ windows ++ embedded ++ mmix ++ js ++ genode ++ redox ++ vc4);
 
   testarm = mseteq arm [ "armv5tel-linux" "armv6l-linux" "armv6l-netbsd" "armv6l-none" "armv7a-linux" "armv7a-netbsd" "armv7l-linux" "armv7l-netbsd" "arm-none" "armv7a-darwin" ];
   testarmv7 = mseteq armv7 [ "armv7a-darwin" "armv7a-linux" "armv7l-linux" "armv7a-netbsd" "armv7l-netbsd" ];
