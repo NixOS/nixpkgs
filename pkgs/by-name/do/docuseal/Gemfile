1dd872b5b13086d081cf03a3c28c04480a23d669
Felix Buehler <account@buehler.rocks>
2023-10-22 21:31:28 +0200

    docuseal: init 1.7.9

---
+++ pkgs/by-name/do/docuseal/Gemfile
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '3.3.4'
+
+gem 'arabic-letter-connector', require: 'arabic-letter-connector/logic'
+gem 'aws-sdk-s3', require: false
+gem 'aws-sdk-secretsmanager', require: false
+gem 'azure-storage-blob', require: false
+gem 'bootsnap', require: false
+gem 'cancancan'
+gem 'csv'
+gem 'devise'
+gem 'devise-two-factor'
+gem 'dotenv', require: false
+gem 'email_typo'
+gem 'faraday'
+gem 'faraday-follow_redirects'
+gem 'google-cloud-storage', require: false
+gem 'hexapdf'
+gem 'image_processing'
+gem 'jwt'
+gem 'lograge'
+gem 'mysql2', require: false
+gem 'oj'
+gem 'pagy'
+gem 'pg', require: false
+gem 'premailer-rails'
+gem 'pretender'
+gem 'puma', require: false
+gem 'rack'
+gem 'rails'
+gem 'rails_autolink'
+gem 'rails-i18n'
+gem 'rotp'
+gem 'rqrcode'
+gem 'ruby-vips'
+gem 'rubyXL'
+gem 'shakapacker'
+gem 'sidekiq'
+gem 'sqlite3', require: false
+gem 'strip_attributes'
+gem 'turbo-rails'
+gem 'twitter_cldr', require: false
+gem 'tzinfo-data'
+
+group :development, :test do
+  gem 'annotate'
+  gem 'better_html'
+  gem 'bullet'
+  gem 'debug'
+  gem 'erb_lint', require: false
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'rubocop', require: false
+  gem 'rubocop-performance', require: false
+  gem 'rubocop-rails', require: false
+  gem 'rubocop-rspec', require: false
+  gem 'simplecov', require: false
+end
+
+group :development do
+  gem 'letter_opener_web'
+  gem 'web-console'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'cuprite'
+  gem 'webmock'
+end
---
+++ pkgs/by-name/do/docuseal/update.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p curl bundix git ruby_3_3 prefetch-yarn-deps pkg-config xmlstarlet nix-update nixfmt-rfc-style
+
+set -eu -o pipefail
+dir="$(dirname "$(readlink -f "$0")")"
+
+latest=$(curl https://github.com/docusealco/docuseal/tags.atom | xmlstarlet sel -N atom="http://www.w3.org/2005/Atom" -t -m /atom:feed/atom:entry -v atom:title -n | head -n1)
+
+if [[ "$(nix-instantiate -A docuseal.version --eval --json | jq -r)" = "$latest" ]];
+then
+  echo "Already using version $latest, skipping"
+  exit 0
+fi
+
+echo "Updating docuseal to $latest"
+
+repo=$(mktemp -d /tmp/docuseal-update.XXX)
+rm -f "$dir/gemset.nix" "$dir/Gemfile" "$dir/Gemfile.lock" "$dir/yarn.lock"
+
+git clone https://github.com/docusealco/docuseal.git --branch "$latest" "$repo"
+
+# fix: https://github.com/nix-community/bundix/issues/88
+BUNDLE_GEMFILE="$repo/Gemfile" bundler lock --remove-platform x86_64-linux --lockfile="$repo/Gemfile.lock"
+BUNDLE_GEMFILE="$repo/Gemfile" bundler lock --remove-platform aarch64-linux --lockfile="$repo/Gemfile.lock"
+# generate gemset.nix
+bundix --lock --lockfile="$repo/Gemfile.lock" --gemfile="$repo/Gemfile" --gemset="$dir/gemset.nix"
+
+# patch yarn.lock
+sed -i 's$, "@hotwired/turbo@https://github.com/docusealco/turbo#main"$$g' "$repo/yarn.lock"
+
+# calc yarn hash
+YARN_HASH="$(prefetch-yarn-deps "$repo/yarn.lock")"
+YARN_HASH="$(nix hash to-sri --type sha256 "$YARN_HASH")"
+
+# update
+cp "$repo/Gemfile" "$repo/Gemfile.lock" "$repo/yarn.lock" "$dir/"
+nix-update docuseal --version "$latest"
+sed -i -E -e "s#hash = \".*\"#hash = \"$YARN_HASH\"#" "$dir"/web.nix
+
+# fmt
+nixfmt "$dir/"
+
+rm -rf "$repo"
