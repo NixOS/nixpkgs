7aff471435a2a69e220b5adcb38944317aa4f6dd
Yaya <github@uwu.is>
2023-10-23 12:46:34 +0200

    gitlab: 16.4.1 -> 16.5.0
    https://gitlab.com/gitlab-org/gitlab/-/blob/v16.5.0-ee/CHANGELOG.md

--- pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "gitlab-pages";
-  version = "16.4.1";
+  version = "16.5.0";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-pages";
     rev = "v${version}";
-    hash = "sha256-aUpuzgFbxMJwKjTn+QAudOeMBSLtLTjaTmwe25f6qxg=";
+    hash = "sha256-4oeJHhArBlwz+DERznUAGrHQtq9Na/c2Sn5Jyx+xcLM=";
   };
 
-  vendorHash = "sha256-ko0ycT8HlqVfXf7tck0xcs6rDJMpHxjSoI59gTLgqDQ=";
+  vendorHash = "sha256-YG+ERETxp0BPh/V4820pMXTXu9YcodRhzme6qZJBC9Q=";
   subPackages = [ "." ];
 
   meta = with lib; {
