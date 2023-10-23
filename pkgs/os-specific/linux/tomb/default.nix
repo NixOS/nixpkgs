9481b1fe8f013a8639697ef4ccbef0309d7cf916
Anthony Roussel <anthony@roussel.dev>
2023-10-23 15:39:36 +0200

    tomb: 2.9 -> 2.10
    https://github.com/dyne/Tomb/blob/master/ChangeLog.md#210
    https://github.com/dyne/Tomb/compare/v2.9...v2.10

--- pkgs/os-specific/linux/tomb/default.nix
+++ pkgs/os-specific/linux/tomb/default.nix
@@ -1,29 +1,34 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, substituteAll
 , makeWrapper
+, getent
 , gettext
 , zsh
 , pinentry
 , cryptsetup
 , gnupg
+, libargon2
+, lsof
 , util-linux
 , e2fsprogs
-, sudo
+, testers
+, nix-update-script
 }:
 
 stdenv.mkDerivation rec {
   pname = "tomb";
-  version = "2.9";
+  version = "2.10";
 
   src = fetchFromGitHub {
     owner = "dyne";
     repo = "Tomb";
-    rev = "v${version}";
-    sha256 = "0d6vmfcf4kd0p2bcljmdnyc2fmbwvar81cc472zx86r7yc3ih102";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-lLxQJX0P6b6lbXEcrq45EsX9iKiayZ9XkhqgMfpN3/w=";
   };
 
-  buildInputs = [ sudo zsh pinentry ];
+  buildInputs = [ zsh pinentry ];
 
   nativeBuildInputs = [ makeWrapper ];
 
@@ -33,20 +38,18 @@ stdenv.mkDerivation rec {
       --replace 'TOMBEXEC=$0' 'TOMBEXEC=tomb'
   '';
 
-  doInstallCheck = true;
-  installCheckPhase = "$out/bin/tomb -h";
-
   installPhase = ''
     install -Dm755 tomb $out/bin/tomb
     install -Dm644 doc/tomb.1 $out/share/man/man1/tomb.1
 
     wrapProgram $out/bin/tomb \
-      --prefix PATH : $out/bin:${lib.makeBinPath [ cryptsetup e2fsprogs gettext gnupg pinentry util-linux ]}
+      --prefix PATH : $out/bin:${lib.makeBinPath [ cryptsetup e2fsprogs getent gettext gnupg libargon2 lsof pinentry util-linux ]}
   '';
 
   meta = with lib; {
     description = "File encryption on GNU/Linux";
     homepage = "https://www.dyne.org/software/tomb/";
+    changelog = "https://github.com/dyne/Tomb/blob/v${version}/ChangeLog.md";
     license = licenses.gpl3;
     maintainers = with maintainers; [ peterhoeg ];
     platforms = platforms.linux;
