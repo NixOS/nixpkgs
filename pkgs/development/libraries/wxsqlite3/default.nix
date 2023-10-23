d6eea9905c93781f09f6b88e494d8ae36599bc40
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-23 21:54:34 +0000

    wxsqlite3: 4.9.4 -> 4.9.6

--- pkgs/development/libraries/wxsqlite3/default.nix
+++ pkgs/development/libraries/wxsqlite3/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wxsqlite3";
-  version = "4.9.4";
+  version = "4.9.6";
 
   src = fetchFromGitHub {
     owner = "utelle";
     repo = "wxsqlite3";
     rev = "v${version}";
-    hash = "sha256-aM79DI/Kj1QEIJ1HMttlfqK/WZER9RJhQbrnbPto57U=";
+    hash = "sha256-ah9EFj15cP9soVJATVJk4XGYItxcrt4HB6ZTfpsVhS8=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
