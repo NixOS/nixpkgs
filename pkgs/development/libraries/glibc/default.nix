7acd36fbce267b11fec8edda2b9ba769d85666a0
Adam Joseph <adam@westernsemico.com>
2023-10-23 14:33:35 -0700

    glibc: weaken host==build check to canExecute

--- pkgs/development/libraries/glibc/default.nix
+++ pkgs/development/libraries/glibc/default.nix
@@ -95,7 +95,7 @@ in
         "user-defined-trusted-dirs=${libgcc}/lib"
       ];
 
-    postInstall = previousAttrs.postInstall + (if stdenv.hostPlatform == stdenv.buildPlatform then ''
+    postInstall = previousAttrs.postInstall + (if stdenv.buildPlatform.canExecute stdenv.hostPlatform then ''
       echo SUPPORTED-LOCALES=C.UTF-8/UTF-8 > ../glibc-2*/localedata/SUPPORTED
       make -j''${NIX_BUILD_CORES:-1} localedata/install-locales
     '' else lib.optionalString stdenv.buildPlatform.isLinux
