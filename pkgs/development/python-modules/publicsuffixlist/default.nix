6ae8e33459cc9b720d68cbe472fd2c200215fb01
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-23 13:34:49 +0200

    python311Packages.publicsuffixlist: 0.10.0.20231020 -> 0.10.0.20231022

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "publicsuffixlist";
-  version = "0.10.0.20231020";
+  version = "0.10.0.20231022";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-5Woh5xWIVvVvgmp4xtpU51rXSCZDH0ljPpLpkvFmO+8=";
+    hash = "sha256-sXmntukr4m876x4Hu45tHlT01rI28l7YT59u2TW8TV4=";
   };
 
   passthru.optional-dependencies = {
