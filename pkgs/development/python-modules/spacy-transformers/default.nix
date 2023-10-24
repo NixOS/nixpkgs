5baf72944012ff379968993aeab0307583d525ed
Gaetan Lepage <gaetan@glepage.com>
2023-10-24 16:30:41 +0200

    python311Packages.spacy-transformers: 1.3.0 -> 1.3.2
    Changelog: https://github.com/explosion/spacy-transformers/releases/tag/v1.3.2

--- pkgs/development/python-modules/spacy-transformers/default.nix
+++ pkgs/development/python-modules/spacy-transformers/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "spacy-transformers";
-  version = "1.3.0";
+  version = "1.3.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-vxzDGLa+LoKnLpaqG7kGLfSLxqQdW+9AXw2YzBAz0UY=";
+    hash = "sha256-xfUePKLmR1Arhs0c1ZNjca6klJdGL0o2DdGLsejE6zw=";
   };
 
   nativeBuildInputs = [
@@ -36,6 +36,7 @@ buildPythonPackage rec {
   ];
 
   pythonRelaxDeps = [
+    "spacy"
     "transformers"
   ];
 
