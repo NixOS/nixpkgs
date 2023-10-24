71eb6c7927066e2057b1252f2e440faf6d0f115f
K900 <me@0upti.me>
2023-10-24 18:06:53 +0300

    vulkan-utility-libraries: 1.3.261 -> 1.3.268

--- pkgs/development/libraries/vulkan-utility-libraries/default.nix
+++ pkgs/development/libraries/vulkan-utility-libraries/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vulkan-utility-libraries";
-  version = "1.3.261";
+  version = "1.3.268.0";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "Vulkan-Utility-Libraries";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-szkBKNcxTHMYhhHFWr5WjD91Vf/AyZaGymvlDU9ff7s=";
+    rev = "vulkan-sdk-${finalAttrs.version}";
+    hash = "sha256-O1agpzZpXiQZFYx1jPosIhxJovZtfZSLBNFj1LVB1VI=";
   };
 
   nativeBuildInputs = [ cmake python3 ];
