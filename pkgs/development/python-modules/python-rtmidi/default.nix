7aa6447b738ec1ee51e60611830a520b9fa262a3
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-10-24 15:33:07 +0200

    python311Packages.python-rtmidi: 1.5.6 -> 1.5.7
    https://github.com/SpotlightKid/python-rtmidi/blob/1.5.7/CHANGELOG.md

--- pkgs/development/python-modules/python-rtmidi/default.nix
+++ pkgs/development/python-modules/python-rtmidi/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "python-rtmidi";
-  version = "1.5.6";
+  version = "1.5.7";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "python_rtmidi";
     inherit version;
-    hash = "sha256-sqCjmbtKXhpWR3eYr9QdAioYtelU9tD/krRbuZvuNxA=";
+    hash = "sha256-3vsaSyrob/OYwjLFPu2lVOJKSfZ96ELnnOuos8p3N00=";
   };
 
   nativeBuildInputs = [
