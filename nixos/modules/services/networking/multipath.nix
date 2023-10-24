7029a335f0ae7659c67ceb2d5021e1580872f305
Will Fancher <elvishjerricco@gmail.com>
2023-10-24 17:05:32 -0400

    nixos/multipath: Fix multipath root.

--- nixos/modules/services/networking/multipath.nix
+++ nixos/modules/services/networking/multipath.nix
@@ -548,6 +548,8 @@ in {
     # to display the multipath mappings in the output of `journalctl -b`.
     boot.initrd.kernelModules = [ "dm-multipath" "dm-service-time" ];
     boot.initrd.postDeviceCommands = ''
+      mkdir -p ${builtins.storeDir}/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-${cfg.package.name}/lib/
+      ln -s $extraUtils/lib/multipath ${builtins.storeDir}/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-${cfg.package.name}/lib/multipath
       modprobe -a dm-multipath dm-service-time
       multipathd -s
       (set -x && sleep 1 && multipath -ll)
