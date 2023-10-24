923fb93e9f1853ebab62dd15b475c2d4dfcee169
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-24 17:30:44 +0000

    keycloak: 22.0.4 -> 22.0.5

--- pkgs/servers/keycloak/default.nix
+++ pkgs/servers/keycloak/default.nix
@@ -18,11 +18,11 @@ let
   '';
 in stdenv.mkDerivation rec {
   pname = "keycloak";
-  version = "22.0.4";
+  version = "22.0.5";
 
   src = fetchzip {
     url = "https://github.com/keycloak/keycloak/releases/download/${version}/keycloak-${version}.zip";
-    hash = "sha256-wl6kZmm6o9UspKdVHDqUvoDCRALp1ut7Sun/RDuEMLI=";
+    hash = "sha256-D08WPJUOIIDD9JTTq4C4+wzj/fTZvFbxVXWbVzx0MKY=";
   };
 
   nativeBuildInputs = [ makeWrapper jre ];
