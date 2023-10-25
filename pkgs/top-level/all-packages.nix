8ac2bc69b2c998e7d52c9b324e45376df37e7f3e
ciferkey <ciferkey@gmail.com>
2023-10-25 11:53:21 -0400

    pocket-casts: 0.6.0 -> 0.7.0
    (cherry picked from commit fbce7af72b1782fa422c8b06e2e879f66de5c475)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5700,7 +5700,7 @@ with pkgs;
   pn = callPackage ../tools/text/pn { };
 
   pocket-casts = callPackage ../applications/audio/pocket-casts {
-    electron = electron_22;
+    electron = electron_27;
   };
 
   portal = callPackage ../tools/misc/portal { };
