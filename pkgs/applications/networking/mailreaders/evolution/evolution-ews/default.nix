a1473757b8346eaf6c01c7ef864afdf3aed7d247
Bobby Rong <rjl931189261@126.com>
2023-10-25 13:36:55 +0000

    evolution-ews: 3.50.0 → 3.50.1
    https://gitlab.gnome.org/GNOME/evolution-ews/-/compare/3.50.0...3.50.1
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/applications/networking/mailreaders/evolution/evolution-ews/default.nix
+++ pkgs/applications/networking/mailreaders/evolution/evolution-ews/default.nix
@@ -22,11 +22,11 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution-ews";
-  version = "3.50.0";
+  version = "3.50.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "16oN6KeP7I5I7S1h6jcDqDwcy1rACv/7qyXsBRnCvqQ=";
+    sha256 = "577S3Z/AhFf3W6ufiWJV8w/TTHu8nIqV74fi4pEqCa0=";
   };
 
   patches = [
