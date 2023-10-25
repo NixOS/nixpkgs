3c18a64ea71d5d8259f53821eed7db77d3f89ac5
Bobby Rong <rjl931189261@126.com>
2023-10-25 13:24:02 +0000

    gnome.gnome-software: 45.0 → 45.1
    https://gitlab.gnome.org/GNOME/gnome-software/-/compare/45.0...45.1
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-software/default.nix
+++ pkgs/desktops/gnome/core/gnome-software/default.nix
@@ -45,11 +45,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gnome-software";
-  version = "45.0";
+  version = "45.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-software/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "YfQsL9TRm4lKJIT2kTaQMtyLKjKgFM5spNAsT6T3WhQ=";
+    sha256 = "1ySF96bgkX9k7b7daP17VyRsbr8QxaRRCLY5RmNXeKI=";
   };
 
   patches = [
