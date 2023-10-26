410bbcd2c90f7eb53e5a3aa75ac034921612055f
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-26 09:54:00 +0200

    python311Packages.peaqevcore: 19.5.6 -> 19.5.10
    Changelog: https://github.com/elden1337/peaqev-core/releases/tag/19.5.10

--- pkgs/development/python-modules/peaqevcore/default.nix
+++ pkgs/development/python-modules/peaqevcore/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "peaqevcore";
-  version = "19.5.6";
+  version = "19.5.10";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-kpSmwMDAvUC9LV/oKiMdm+J9aUEPiPlNKLnVG/eW3hI=";
+    hash = "sha256-izw41TUmqKOy34/RMHjBROQr88SChheKJVpPMaOubnE=";
   };
 
   postPatch = ''
