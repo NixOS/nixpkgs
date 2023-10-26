35511fefbbe9aa8dd4fad585995814747069efec
irealis <irealis@proton.me>
2023-10-26 11:29:15 +0200

    renderdoc: fix build
    pyside2 requires python <3.11

--- pkgs/development/tools/renderdoc/default.nix
+++ pkgs/development/tools/renderdoc/default.nix
@@ -11,8 +11,8 @@
 , vulkan-loader
 , libglvnd
 , xorg
-, python3
-, python3Packages
+, python310
+, python310Packages
 , bison
 , pcre
 , automake
@@ -42,8 +42,8 @@ mkDerivation rec {
   };
 
   buildInputs = [
-    qtbase qtsvg xorg.libpthreadstubs xorg.libXdmcp qtx11extras vulkan-loader python3
-  ] ++ (with python3Packages; [
+    qtbase qtsvg xorg.libpthreadstubs xorg.libXdmcp qtx11extras vulkan-loader python310
+  ] ++ (with python310Packages; [
     pyside2 pyside2-tools shiboken2
   ])
   ++ lib.optional waylandSupport wayland;
