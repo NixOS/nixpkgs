504943e5f7f1b6f8b71a79b227a5a82cb4bda07d
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-10-26 22:53:56 +0200

    lanraragi: refactor, apply patches, add 23.11 release notes

--- pkgs/by-name/la/lanraragi/package.nix
+++ pkgs/by-name/la/lanraragi/package.nix
@@ -10,8 +10,6 @@
 }:
 
 let
-  perlEnv = perl.withPackages (_: cpanDeps);
-
   cpanDeps = with perl.pkgs; [
     ImageMagick
     locallib
@@ -53,32 +51,36 @@ buildNpmPackage rec {
   };
 
   patches = [
+    # fetchpatch entries can be removed when updating to 0.9.0
     (fetchpatch {
-      name = "add-package-lock-json.patch"; # Can be removed when updating to 0.9.0
+      name = "add-package-lock-json.patch";
       url = "https://github.com/Difegue/LANraragi/commit/c5cd8641795bf7e40deef4ae955ea848dde44050.patch";
       hash = "sha256-XKxRzeugkIe6N4XRN6+O1wEZpxo6OzU0OaG0ywKFv38=";
     })
+    (fetchpatch {
+      name = "fix-minion-redis-password.patch";
+      url = "https://github.com/Difegue/LANraragi/commit/9277b620be29ad6f8fd6ebdf9d9169c68d30abce.patch";
+      hash = "sha256-asUy0N6PvEC1BCESpfZyl5G9f2SvVvSLOjE17ef+fAg=";
+    })
+    (fetchpatch {
+      name = "update-secret-generation.patch"; # security patch
+      url = "https://github.com/Difegue/LANraragi/compare/9277b620be29ad6f8fd6ebdf9d9169c68d30abce..d6da3f0c4736c5bc45410043dfe40d2c5b917198.patch";
+      hash = "sha256-Dlz5CfQPVwasySOI/PsuvcYpRXicUa45o/RtfXd4aVE=";
+      includes = [ "lib/LANraragi.pm" ];
+    })
     ./install.patch
+    ./loosen-dep-reqs.patch
     ./fix-paths.patch
     ./expose-password-hashing.patch
-    ./fix-minion-redis-password.patch # Should be upstreamed
   ];
 
   npmFlags = [ "--legacy-peer-deps" ];
 
   npmDepsHash = "sha256-UQsChPU5b4+r5Kv6P/3rJCGUzssiUNSKo3w4axNyJew=";
 
-  nativeBuildInputs = [
-    perl
-    makeBinaryWrapper
-    perl.pkgs.Appcpanminus
-  ] ++ cpanDeps;
+  nativeBuildInputs = [ perl makeBinaryWrapper ];
 
-  nativeCheckInputs = with perl.pkgs; [
-    TestMockObject
-    TestTrap
-    TestDeep
-  ];
+  buildInputs = [ perl ] ++ cpanDeps;
 
   buildPhase = ''
     runHook preBuild
@@ -91,8 +93,18 @@ buildNpmPackage rec {
 
   doCheck = true;
 
+  nativeCheckInputs = with perl.pkgs; [
+    TestMockObject
+    TestTrap
+    TestDeep
+  ];
+
   checkPhase = ''
     runHook preCheck
+
+    # Check if every dependency was installed
+    # explicitly call cpanm with perl as the shebang is broken on darwin
+    perl ${lib.getExe perl.pkgs.Appcpanminus} --installdeps ./tools --notest
 
     rm tests/plugins.t # Uses network
     prove -r -l -v tests
@@ -104,19 +116,23 @@ buildNpmPackage rec {
     runHook preInstall
 
     mkdir -p $out/share/lanraragi
-    cp -r lib public script templates package.json $out/share/lanraragi
+    chmod +x script/launcher.pl
+    cp -r lib public script templates package.json lrr.conf $out/share/lanraragi
 
-    makeWrapper ${perlEnv}/bin/perl $out/bin/lanraragi \
+    makeWrapper $out/share/lanraragi/script/launcher.pl $out/bin/lanraragi \
+      --prefix PERL5LIB : $PERL5LIB \
       --prefix PATH : ${lib.makeBinPath [ ghostscript ]} \
-      --add-flags "$out/share/lanraragi/script/launcher.pl -f $out/share/lanraragi/script/lanraragi"
+      --run "cp -n --no-preserve=all $out/share/lanraragi/lrr.conf ./lrr.conf 2>/dev/null || true" \
+      --add-flags "-f $out/share/lanraragi/script/lanraragi"
+
+    makeWrapper ${lib.getExe perl} $out/bin/helpers/lrr-make-password-hash \
+      --prefix PERL5LIB : $out/share/lanraragi/lib:$PERL5LIB \
+      --add-flags "-e 'use LANraragi::Controller::Config; print LANraragi::Controller::Config::make_password_hash(@ARGV[0])' 2>/dev/null"
 
     runHook postInstall
   '';
 
-  passthru = {
-    inherit perlEnv;
-    tests = { inherit (nixosTests) lanraragi; };
-  };
+  passthru.tests.module = nixosTests.lanraragi;
 
   meta = {
     changelog = "https://github.com/Difegue/LANraragi/releases/tag/${src.rev}";
@@ -128,3 +144,4 @@ buildNpmPackage rec {
     platforms = lib.platforms.unix;
   };
 }
+
