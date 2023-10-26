3757c3ceea74c988b9982a3db54382a14cd5959c
Anthony Roussel <anthony@roussel.dev>
2023-10-26 16:34:34 +0200

    postgresql_jdbc: 42.5.1 -> 42.6.0
    https://github.com/pgjdbc/pgjdbc/releases/tag/REL42.6.0
    https://jdbc.postgresql.org/changelogs/2023-03-17-42.6.0-release/

--- pkgs/development/java-modules/postgresql_jdbc/default.nix
+++ pkgs/development/java-modules/postgresql_jdbc/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "postgresql-jdbc";
-  version = "42.5.1";
+  version = "42.6.0";
 
   src = fetchMavenArtifact {
     artifactId = "postgresql";
     groupId = "org.postgresql";
-    sha256 = "sha256-iei/+os3uUh5RgEsaQzwTzEDlTBRwcGT2I7ja2jTZa4=";
+    hash = "sha256-uBfGekDJQkn9WdTmhuMyftDT0/rkJrINoPHnVlLPxGE=";
     inherit version;
   };
 
