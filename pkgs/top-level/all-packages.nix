22d77f348c5fce337f8ccd0665988a6284618db0
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2023-10-27 18:37:00 +0200

    jq-lsp: init at 2023-10-27

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -40451,6 +40451,8 @@ with pkgs;
 
   j2cli = with python3Packages; toPythonApplication j2cli;
 
+  jq-lsp = callPackage ../development/tools/language-servers/jq-lsp { };
+
   jquake = callPackage ../applications/misc/jquake { };
 
   jstest-gtk = callPackage ../tools/misc/jstest-gtk { };
