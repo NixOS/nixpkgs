15da8030ce3df5997380e6be99dcc9c32757d703
Anderson Torres <torres.anderson.85@protonmail.com>
2023-10-28 17:27:09 -0300

    scons_4_1_0: detach from sconsPackages
    This is probably a legacy release, since I have found no other package using it.

--- pkgs/development/tools/build-managers/scons/default.nix
+++ pkgs/development/tools/build-managers/scons/default.nix
@@ -5,10 +5,6 @@ let
     python = python3;
   };
 in {
-  scons_4_1_0 = mkScons {
-    version = "4.1.0";
-    sha256 = "11axk03142ziax6i3wwy9qpqp7r3i7h5jg9y2xzph9i15rv8vlkj";
-  };
   scons_latest = mkScons {
     version = "4.5.2";
     sha256 = "sha256-ziaqyV01CnmkGSGWsL6sPLJPTMq84BI+so0zcPV28HI=";
