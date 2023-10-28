97532516ca60c7726651d50f56b0b48eef0e7b2e
mdarocha <git@mdarocha.pl>
2023-10-28 10:22:47 +0200

    pycomposefile: init at 0.0.30

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9125,6 +9125,7 @@ self: super: with self; {
 
   pycketcasts = callPackage ../development/python-modules/pycketcasts { };
 
+  pycomposefile = callPackage ../development/python-modules/pycomposefile { };
   pycontrol4 = callPackage ../development/python-modules/pycontrol4 { };
 
   pycookiecheat = callPackage ../development/python-modules/pycookiecheat { };
