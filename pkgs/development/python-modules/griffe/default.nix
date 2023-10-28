95471452758a787167a2987233e477024c21c56a
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-28 22:37:51 +0200

    python311Packages.griffe: 0.36.8 -> 0.36.9
    Diff: https://github.com/mkdocstrings/griffe/compare/refs/tags/0.36.8...0.36.9
    
    Changelog: https://github.com/mkdocstrings/griffe/blob/0.36.9/CHANGELOG.md

--- pkgs/development/python-modules/griffe/default.nix
+++ pkgs/development/python-modules/griffe/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "griffe";
-  version = "0.36.8";
+  version = "0.36.9";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "mkdocstrings";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-2lpmEfHUsnqoEXXyVZqly5ADxaqk+n5GsFTemAuwokU=";
+    hash = "sha256-5j6boIy2LnB3Y0ZzheTdflON8KaQkeQS5vkaPIsETGk=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
