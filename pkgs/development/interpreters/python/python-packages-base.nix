7928373cae398108c663c05e3236e9234c2f55b9
Yueh-Shun Li <shamrocklee@posteo.net>
2023-10-28 01:56:02 +0000

    makeOverridablePythonPackage: take care of overrideAttrs
    Make it possible to mix overridePythonAttrs and overrideAttrs, i.e.
    ((<pkg>.overrideAttrs (_: { foo = "a"; })).overridePythonAttrs (_: { })).foo now works

--- pkgs/development/interpreters/python/python-packages-base.nix
+++ pkgs/development/interpreters/python/python-packages-base.nix
@@ -13,6 +13,9 @@ let
 
   # Derivations built with `buildPythonPackage` can already be overridden with `override`, `overrideAttrs`, and `overrideDerivation`.
   # This function introduces `overridePythonAttrs` and it overrides the call to `buildPythonPackage`.
+  #
+  # Overridings specified through `overridePythonAttrs` will always be applied
+  # before those specified by `overrideAttrs`, even if invoked after them.
   makeOverridablePythonPackage = f:
     let
       mirrorArgs = lib.mirrorFunctionArgs f;
@@ -30,13 +33,17 @@ let
 
       overrideWith = newArgs: args // lib.toFunction newArgs args;
       overridePythonAttrs = mirrorArgs (newArgs: makeOverridablePythonPackage f (overrideWith newArgs));
+
+      # Change the result of the function call by applying g to it
+      overrideResult = g: makeOverridablePythonPackage (mirrorArgs (args: g (f args))) origArgs;
     in
-      if builtins.isAttrs result then result
-      else if builtins.isFunction result then {
-        inherit overridePythonAttrs;
-        __functor = self: result;
-      }
-      else result);
+    if builtins.isAttrs result then result
+      // lib.optionalAttrs (result ? overrideAttrs) { overrideAttrs = fdrv: overrideResult (drv: drv.overrideAttrs fdrv); }
+    else if builtins.isFunction result then {
+      inherit overridePythonAttrs;
+      __functor = self: result;
+    }
+    else result);
 
   mkPythonDerivation = if python.isPy3k then
     ./mk-python-derivation.nix
