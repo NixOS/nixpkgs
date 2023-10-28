493f2e15bdc58a3338103d93cc37ccf0d06d74d7
paumr <paum@bdisk.eu>
2023-10-28 11:36:20 +0200

    maintainers: add paumr

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13595,6 +13595,11 @@
     githubId = 15645854;
     name = "Brad Christensen";
   };
+  paumr = {
+    github = "paumr";
+    name = "Michael Bergmeister";
+    githubId = 53442728;
+  };
   paveloom = {
     email = "paveloom@riseup.net";
     github = "paveloom";
