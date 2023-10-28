221c92c43e32f44d40e604831cb84cd7e6a321df
Anderson Torres <torres.anderson.85@protonmail.com>
2023-10-28 00:13:34 -0300

    scons: move common.nix to make-scons.nix

--- pkgs/development/tools/build-managers/scons/default.nix
+++ pkgs/development/tools/build-managers/scons/default.nix
@@ -1,7 +1,7 @@
 { callPackage, python2, python3 }:
 
 let
-  mkScons = args: callPackage (import ./common.nix args) {
+  mkScons = args: callPackage (import ./make-scons.nix args) {
     python = python3;
   };
 in {
