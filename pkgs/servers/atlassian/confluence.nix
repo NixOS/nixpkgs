5085a279ba0d4a095a67cf87d247a48d7503cc37
R. Ryantm <ryantm-bot@ryantm.com>
2023-10-29 07:02:50 +0000

    atlassian-confluence: 7.19.14 -> 8.5.2

--- pkgs/servers/atlassian/confluence.nix
+++ pkgs/servers/atlassian/confluence.nix
@@ -15,11 +15,11 @@ in
 optionalWarning (crowdProperties != null) "Using `crowdProperties` is deprecated!"
 (stdenvNoCC.mkDerivation rec {
   pname = "atlassian-confluence";
-  version = "7.19.14";
+  version = "8.5.2";
 
   src = fetchurl {
     url = "https://product-downloads.atlassian.com/software/confluence/downloads/${pname}-${version}.tar.gz";
-    sha256 = "sha256-Z4a4YZO9UnZSAZYB0FHRsX8QwX0ju3SeISsQquyA+w0=";
+    sha256 = "sha256-iRuzwT7COwwdV7voOatvaxLrko9tUlnRCRqvY9b7qqM=";
   };
 
   buildPhase = ''
