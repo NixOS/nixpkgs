580b077fbf9841f2dd431493edf6dc02ea178a61
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-10-29 23:56:08 +0100

    python311Packages.vaderSentiment: init at 3.3.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15264,6 +15264,8 @@ self: super: with self; {
 
   vaa = callPackage ../development/python-modules/vaa { };
 
+  vaderSentiment = callPackage ../development/python-modules/vaderSentiment { };
+
   validate-email = callPackage ../development/python-modules/validate-email { };
 
   validators = callPackage ../development/python-modules/validators { };
