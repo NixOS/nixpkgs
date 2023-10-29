15c261025d35f6e525609d95ea702a362049f5f6
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-10-29 15:03:42 +0800

    guix: add parameters for certain configure flags
    This is in preparation for the Nix module where it will allow the user
    to set custom store and state directory.
    
    (cherry picked from commit 092aaf841806e8a5fcdfea566bbaae2e25cea069)

--- pkgs/by-name/gu/guix/package.nix
+++ pkgs/by-name/gu/guix/package.nix
@@ -28,6 +28,10 @@
 , bzip2
 , libgcrypt
 , sqlite
+
+, stateDir ? "/var"
+, storeDir ? "/gnu/store"
+, confDir ? "/etc"
 }:
 
 stdenv.mkDerivation rec {
@@ -100,8 +104,9 @@ stdenv.mkDerivation rec {
   ];
 
   configureFlags = [
-    "--localstatedir=/var"
-    "--sysconfdir=/etc"
+    "--with-store-dir=${storeDir}"
+    "--localstatedir=${stateDir}"
+    "--sysconfdir=${confDir}"
     "--with-bash-completion-dir=$(out)/etc/bash_completion.d"
   ];
 
