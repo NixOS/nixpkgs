f57a4b0ac1b954eec0c8def2a99e2a464ac6ff7a
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2023-10-29 09:56:26 +0100

    libtiff: introduce libtiff_4_5
    With the update to libtiff 4.6 in
    0a74a54ac2600656cd0b640d5ea1c8efb5c35d68 ,
    many tiff-processing utility executables got dropped:
    
    http://www.simplesystems.org/libtiff/releases/v4.6.0.html
    
    Some of these executables can still be "restored" with
    the configure switch `--enable-tools-unsupported`,
    but unfortunatelly,
    at least hylafaxplus (maybe more packages) relies on
    utilities that even cannot be restored with this switch.
    
    The commit at hand reintroduces the old libtiff
    version 4.5.1 as `libtiff_4_5` into nixpkgs.
    It restores the old build recipe with the following changes:
    
    * passthru.updateScript is dropped as it is of no use here
    * passthru.tests is dropped as it only contains
      packages that now build with the new libtiff version
    * patches are applied for the two CVEs that are fixed in 4.6.0
    
    As libtiff 4.5 is no longer supported by libtiff developers,
    new vulnerabilities will likely go unnoticed
    unless they also affect the current version.
    To not disable hydra builds, we don't add
    `knownVulnerabilities` *for now*, but add comments to alert
    updaters of the current libtiff version so patches can
    be backported or the situation be reevaluated as a whole.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23537,6 +23537,7 @@ with pkgs;
   libtifiles2 = callPackage ../development/libraries/libtifiles2 { };
 
   libtiff = callPackage ../development/libraries/libtiff { };
+  libtiff_4_5 = callPackage ../development/libraries/libtiff/4.5.nix { };
 
   libtiger = callPackage ../development/libraries/libtiger { };
 
