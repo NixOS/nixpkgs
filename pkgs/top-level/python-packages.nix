6bb36589cda85fe3b6a769b42dd11f51722d6625
Otavio Salvador <otavio@ossystems.com.br>
2023-10-30 17:31:34 -0300

    python3Packages.zcbor: init at 0.7.0
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15980,6 +15980,8 @@ self: super: with self; {
 
   zc_lockfile = callPackage ../development/python-modules/zc_lockfile { };
 
+  zcbor = callPackage ../development/python-modules/zcbor { };
+
   zconfig = callPackage ../development/python-modules/zconfig { };
 
   zcs = callPackage ../development/python-modules/zcs { };
