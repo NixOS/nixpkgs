00138c96aa59254709ae99d9558a8380a320b730
Gaetan Lepage <gaetan@glepage.com>
2023-10-30 22:58:20 +0100

    python311Packages.objax: disable tests to fix build

--- pkgs/development/python-modules/objax/default.nix
+++ pkgs/development/python-modules/objax/default.nix
@@ -1,13 +1,17 @@
 { lib
-, fetchFromGitHub
 , buildPythonPackage
-, jax
+, fetchFromGitHub
+, fetchpatch
 , jaxlib
+, jax
 , numpy
 , parameterized
 , pillow
 , scipy
 , tensorboard
+, keras
+, pytestCheckHook
+, tensorflow
 }:
 
 buildPythonPackage rec {
@@ -17,9 +21,16 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "google";
     repo = "objax";
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     hash = "sha256-1/XmxFZfU+XMD0Mlcv4xTUYZDwltAx1bZOlPuKWQQC0=";
   };
+
+  patches = [
+    (fetchpatch {  # https://github.com/google/objax/pull/266
+      url = "https://github.com/google/objax/pull/266/commits/a1bcb71ebd92c94fec98222349d7cd57048e541d.patch";
+      hash = "sha256-MO9/LAxbghjhRU8sbYWm3xa4RPuU+5m74YU3n3hJ09s=";
+    })
+  ];
 
   # Avoid propagating the dependency on `jaxlib`, see
   # https://github.com/NixOS/nixpkgs/issues/156767
@@ -38,6 +49,25 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [
     "objax"
+  ];
+
+  # This is necessay to ignore the presence of two protobufs version (tensorflow is bringing an
+  # older version).
+  catchConflicts = false;
+
+  nativeCheckInputs = [
+    keras
+    pytestCheckHook
+    tensorflow
+  ];
+
+  pytestFlagsArray = [
+    "tests/*.py"
+  ];
+
+  disabledTests = [
+    # Test requires internet access for prefetching some weights
+    "test_pretrained_keras_weight_0_ResNet50V2"
   ];
 
   meta = with lib; {
