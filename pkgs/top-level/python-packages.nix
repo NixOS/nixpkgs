c114a76e93fad77a4601f56dff5cfcf5b88e2a45
natsukium <tomoya.otabi@gmail.com>
2023-10-30 23:21:38 +0900

    python311Packages.openapi-schema-pydantic: remove
    It has been removed since it is no longer maintained

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8423,8 +8423,6 @@ self: super: with self; {
 
   openant = callPackage ../development/python-modules/openant { };
 
-  openapi-schema-pydantic = callPackage ../development/python-modules/openapi-schema-pydantic { };
-
   openapi-schema-validator = callPackage ../development/python-modules/openapi-schema-validator { };
 
   openapi-spec-validator = callPackage ../development/python-modules/openapi-spec-validator { };
