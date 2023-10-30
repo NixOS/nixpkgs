feb1c79ff533b3685eaed9eae48b994a4b2937e9
nicoo <nicoo@mur.at>
2023-10-30 17:41:44 +0000

    open-english-wordnet: Fix merge.py
    - Upstream left the wrong version number in the script as tagged for the release
    - The output depends on file iteration order.

--- pkgs/by-name/op/open-english-wordnet/package.nix
+++ pkgs/by-name/op/open-english-wordnet/package.nix
@@ -1,5 +1,6 @@
 { lib
 , fetchFromGitHub
+, fetchpatch
 , gzip
 , python3
 , stdenvNoCC
@@ -14,6 +15,16 @@ stdenvNoCC.mkDerivation (self: {
     repo = "english-wordnet";
     rev = "${self.version}-edition";
     hash = "sha256-a1fWIp39uuJZL1aFX/r+ttLB1+kwh/XPHwphgENTQ5M=";
+  };
+
+  patches = lib.mapAttrsToList (rev: hash: fetchpatch {
+    url = "https://github.com/globalwordnet/english-wordnet/commit/${rev}.patch";
+    inherit hash;
+  }) {
+    # Upstream commit bumping the version number, accidentally ommited from the tagged release
+    "bc07902f8995b62c70f01a282b23f40f30630540" = "sha256-1e4MG/k86g3OFUhiShCCbNXnvDKrYFr1KlGVsGl++KI=";
+    # PR #982, “merge.py: Make result independent of filesystem order”
+    "6da46a48dd76a48ad9ff563e6c807b8271fc83cd" = "sha256-QkkJH7NVGy/IbeSWkotU80IGF4esz0b8mIL9soHdQtQ=";
   };
 
   # TODO(nicoo): make compression optional?
