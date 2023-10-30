89a32d286797b7158cb2e642a259456c643e0b17
K900 <me@0upti.me>
2023-10-30 20:29:12 +0300

    tempo: 2.2.3 -> 2.3.0
    Diff: https://github.com/grafana/tempo/compare/v2.2.3...v2.3.0

--- pkgs/servers/tracing/tempo/default.nix
+++ pkgs/servers/tracing/tempo/default.nix
@@ -1,25 +1,16 @@
-{ lib, buildGoModule, fetchFromGitHub, fetchpatch }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
 buildGoModule rec {
   pname = "tempo";
-  version = "2.2.3";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "tempo";
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-23wjD8HTSEGonIMAWCoKORMLIISASxlN4FeY+Bmt/+I=";
+    hash = "sha256-vqYewQT2alW9HFYRh/Ok3jFt2a+VsfqDypNaT+mngys=";
   };
-
-  patches = [
-    # Backport patch for Go 1.21 compatibility
-    # FIXME: remove after 2.3.0
-    (fetchpatch {
-      url = "https://github.com/grafana/tempo/commit/0d37e8f0edd8a96876b0a5f5ab97ef79ff04608f.patch";
-      hash = "sha256-YC59g5pdcrwJeQ4raS0Oq+fZvRBKFj4johZtGTAYpEs=";
-    })
-  ];
 
   vendorHash = null;
 
