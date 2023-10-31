0ab756441cbbf77215cc63cf034e77c04a557802
Gaetan Lepage <gaetan@glepage.com>
2023-10-31 18:37:32 +0100

    python310Packages.array-record: 0.4.1 -> 0.5.0
    Changelog: https://github.com/google/array_record/releases/tag/v0.5.0

--- pkgs/development/python-modules/array-record/default.nix
+++ pkgs/development/python-modules/array-record/default.nix
@@ -1,27 +1,37 @@
 { lib
-, absl-py
 , buildPythonPackage
-, etils
-, fetchPypi
-, importlib-resources
+, pythonOlder
 , python
+, fetchPypi
+, absl-py
+, etils
+, importlib-resources
 , typing-extensions
 , zipp
 }:
 
 buildPythonPackage rec {
   pname = "array-record";
-  version = "0.4.1";
+  version = "0.5.0";
   format = "wheel";
 
-  disabled = python.pythonVersion != "3.10";
+  # As of 2023-10-31, PyPI includes wheels for Python 3.9, 3.10, and 3.11.
+  disabled = pythonOlder "3.9";
 
-  src = fetchPypi {
+  src = let
+    pyShortVersion = "cp${builtins.replaceStrings ["."] [""] python.pythonVersion}";
+  in fetchPypi {
     inherit version format;
     pname = "array_record";
-    dist = "py310";
-    python = "py310";
-    hash = "sha256-agyO1v36ryzs09XGucE+EWrTKZZJYRyP0YTWRVf7q6g=";
+    dist = pyShortVersion;
+    python = pyShortVersion;
+    abi = pyShortVersion;
+    platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+    hash = {
+      cp39 = "sha256-BzMOVue7E1S1+5+XTcPELko81ujc9MbmqLhNsU7pqO0=";
+      cp310 = "sha256-eUD9pQu9GsbV8MPD1MiF3Ihr+zYioSOo6P15hYIwPYo=";
+      cp311 = "sha256-rAmkI3EIZPYiXrxFowfDC0Gf3kRw0uX0i6Kx6Zu+hNM=";
+    }.${pyShortVersion};
   };
 
   propagatedBuildInputs = [
