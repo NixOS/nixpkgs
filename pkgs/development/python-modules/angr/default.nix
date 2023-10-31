14b7ae9ab997b2beede5f5c2ad66b10640514878
Fabian Affolter <mail@fabian-affolter.ch>
2023-10-31 20:20:53 +0100

    python311Packages.angr: 9.2.74 -> 9.2.75
    Diff: https://github.com/angr/angr/compare/refs/tags/v9.2.74...v9.2.75

--- pkgs/development/python-modules/angr/default.nix
+++ pkgs/development/python-modules/angr/default.nix
@@ -32,7 +32,7 @@
 
 buildPythonPackage rec {
   pname = "angr";
-  version = "9.2.74";
+  version = "9.2.75";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -41,7 +41,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "angr";
     rev = "refs/tags/v${version}";
-    hash = "sha256-8t7S+VR9AqYpaAP772Wn1foVy/XN9MiEUZb5+u47G+k=";
+    hash = "sha256-tDOO0wn5gYxK1CuMWhu/i4Xk3CfcuhPvxsG0LI7BlA4=";
   };
 
   propagatedBuildInputs = [
