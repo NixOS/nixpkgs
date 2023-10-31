d38e66588095d8b8fedc0ba3581940615bc61ea9
techknowlogick <techknowlogick@gitea.com>
2023-10-31 14:36:06 -0400

    ytt: 0.46.0 -> 0.46.2
    Diff: https://github.com/vmware-tanzu/carvel-ytt/compare/v0.46.0...v0.46.2

--- pkgs/development/tools/ytt/default.nix
+++ pkgs/development/tools/ytt/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "ytt";
-  version = "0.46.0";
+  version = "0.46.2";
 
   src = fetchFromGitHub {
     owner = "vmware-tanzu";
     repo = "carvel-ytt";
     rev = "v${version}";
-    sha256 = "sha256-ZCWdOvwWXSeFzRQiiObuJqvz0ngAJ8n/0LeoGCv2vu4=";
+    sha256 = "sha256-r9LQMQffnc/g1WFJU8m+Oy0hD+DudCNiVAcbAAPy1vI=";
   };
 
   vendorHash = null;
