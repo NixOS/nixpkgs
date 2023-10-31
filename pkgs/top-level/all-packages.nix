ac16795ebb081725935707e9a2fcbc8e5456c98c
IogaMaster <iogamastercode@gmail.com>
2023-10-30 21:27:16 -0600

    wifi4wofi: Init at unstable-2023-08-24
    Closes #264009

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36514,6 +36514,8 @@ with pkgs;
 
   wofi-emoji = callPackage ../applications/misc/wofi-emoji { };
 
+  wifi4wofi = callPackage ../applications/misc/wifi4wofi { };
+
   cl-wordle = callPackage ../games/cl-wordle { };
 
   wordbook = callPackage ../applications/misc/wordbook { };
