3b92f76b5472bc2b61bc79ec16e500c0ac67f093
nikstur <nikstur@outlook.com>
2023-11-01 18:17:18 +0100

    systemd: enable sysusers by default

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28780,6 +28780,7 @@ with pkgs;
     withResolved = false;
     withShellCompletions = false;
     withSysupdate = false;
+    withSysusers = false;
     withTimedated = false;
     withTimesyncd = false;
     withTpm2Tss = false;
