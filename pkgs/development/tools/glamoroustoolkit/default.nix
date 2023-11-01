951827b40cd88fba5abc72acd1dc2d723451190a
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-01 18:26:07 +0000

    glamoroustoolkit: 1.0.1 -> 1.0.2

--- pkgs/development/tools/glamoroustoolkit/default.nix
+++ pkgs/development/tools/glamoroustoolkit/default.nix
@@ -21,12 +21,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "glamoroustoolkit";
-  version = "1.0.1";
+  version = "1.0.2";
 
   src = fetchzip {
     url = "https://github.com/feenkcom/gtoolkit-vm/releases/download/v${finalAttrs.version}/GlamorousToolkit-x86_64-unknown-linux-gnu.zip";
     stripRoot = false;
-    hash = "sha256-v63sV0HNHSU9H5rhtJcwZCuIXEGe1+BDyxV0/EqBk2E=";
+    hash = "sha256-a+B+uAxFXlWVn8b0M17TtP4WTKhMmR6//smV1nmZyxU=";
   };
 
   nativeBuildInputs = [ wrapGAppsHook ];
