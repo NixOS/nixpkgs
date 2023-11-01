1568a8e235e407fa1bdd1ddfd25df2fc7f098bdf
Damien Cassou <damien@cassou.me>
2023-11-01 11:30:41 +0100

    swagger-codegen3: add meta.mainProram

--- pkgs/tools/networking/swagger-codegen3/default.nix
+++ pkgs/tools/networking/swagger-codegen3/default.nix
@@ -30,5 +30,6 @@ stdenv.mkDerivation rec {
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     license = licenses.asl20;
     maintainers = [ maintainers._1000101 ];
+    mainProgram = "swagger-codegen3";
   };
 }
