11d0be8a8ebec14f9471115fecf58896f66b0bec
tgi74000 <tgi74@hotmail.fr>
2023-11-01 12:42:55 +0100

    maintainers: add tgi74

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17489,6 +17489,11 @@
     github = "tfmoraes";
     githubId = 351108;
   };
+  tgi74 = {
+    github = "tgi74";
+    githubId = 1854712;
+    name = "tgi74";
+  };
   tgunnoe = {
     email = "t@gvno.net";
     github = "tgunnoe";
