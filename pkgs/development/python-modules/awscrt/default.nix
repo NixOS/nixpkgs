ac6a76e8cfffcfb9af0f61dce7df964ffc34995b
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-01 18:31:15 +0100

    python311Packages.awscrt: 0.19.3 -> 0.19.7
    Changelog: https://github.com/awslabs/aws-crt-python/releases/tag/v0.19.7

--- pkgs/development/python-modules/awscrt/default.nix
+++ pkgs/development/python-modules/awscrt/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "awscrt";
-  version = "0.19.3";
+  version = "0.19.7";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-UMIzuGZBff/7szG3CuO3oHZOfY4WcYqW9mUAGFBWLvA=";
+    hash = "sha256-t/LwioUTZ0fNrJUlx83HG1ytmLW64gH6W17Gd2UXets=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin [
