29ea27427ac4b3d6407ae70cee3b86a1f116fa8e
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-02 08:08:59 +0100

    python311Packages.agate-excel: add changelog to meta

--- pkgs/development/python-modules/agate-excel/default.nix
+++ pkgs/development/python-modules/agate-excel/default.nix
@@ -19,8 +19,9 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Adds read support for excel files to agate";
-    homepage    = "https://github.com/wireservice/agate-excel";
-    license     = licenses.mit;
+    homepage = "https://github.com/wireservice/agate-excel";
+    changelog = "https://github.com/wireservice/agate-excel/blob/${version}/CHANGELOG.rst";
+    license = licenses.mit;
     maintainers = with maintainers; [ vrthra ];
   };
 }
