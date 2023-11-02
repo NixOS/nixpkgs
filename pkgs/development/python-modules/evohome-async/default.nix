16c382820bd088bd164d075830631de542b84ad3
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-02 09:29:23 +0100

    python311Packages.evohome-async: 0.3.15 -> 0.4.3

--- pkgs/development/python-modules/evohome-async/default.nix
+++ pkgs/development/python-modules/evohome-async/default.nix
@@ -2,28 +2,40 @@
 , aiohttp
 , buildPythonPackage
 , fetchFromGitHub
+, hatchling
 , pythonOlder
+, voluptuous
 }:
 
 buildPythonPackage rec {
   pname = "evohome-async";
-  version = "0.3.15";
-  disabled = pythonOlder "3.7";
+  version = "0.4.3";
+  pyproject = true;
+
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "zxdavb";
-    repo = pname;
-    rev = version;
-    hash = "sha256-/dZRlcTcea26FEpw/XDItKh4ncr/eEFQcdfIE2KIMo8=";
+    repo = "evohome-async";
+    rev = "refs/tags/${version}";
+    hash = "sha256-GDrDOwB/cgry3eRNx8IMiBoLu5xLTnG5ByuuwnWA7DY=";
   };
+
+  nativeBuildInputs = [
+    hatchling
+  ];
 
   propagatedBuildInputs = [
     aiohttp
+    voluptuous
   ];
 
   # Project has no tests
   doCheck = false;
-  pythonImportsCheck = [ "evohomeasync2" ];
+
+  pythonImportsCheck = [
+    "evohomeasync2"
+  ];
 
   meta = with lib; {
     description = "Python client for connecting to Honeywell's TCC RESTful API";
