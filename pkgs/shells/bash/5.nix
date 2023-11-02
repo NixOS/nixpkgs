667a1ad447fc82559dcf871ec94f0ebb8245499e
Adam Joseph <adam@westernsemico.com>
2023-11-02 03:52:37 -0700

    bash: avoid using unspliced dependency
    `stdenv` is spliced, but `stdenv.cc` is not, so we can't use the
    latter in dependencies.
    
    Closes #264989
    Closes #243164

--- pkgs/shells/bash/5.nix
+++ pkgs/shells/bash/5.nix
@@ -91,7 +91,7 @@ stdenv.mkDerivation rec {
 
   strictDeps = true;
   # Note: Bison is needed because the patches above modify parse.y.
-  depsBuildBuild = [ buildPackages.stdenv.cc ];
+  depsBuildBuild = [ stdenv ];
   nativeBuildInputs = [ bison ]
     ++ lib.optional withDocs texinfo
     ++ lib.optional stdenv.hostPlatform.isDarwin stdenv.cc.bintools;
