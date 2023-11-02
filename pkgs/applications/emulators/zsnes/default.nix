0e0863ee3848fe855fd02e6111b83836e5690021
Sergei Trofimovich <slyich@gmail.com>
2023-11-02 12:13:28 +0000

    zsnes: fix buffer size marking to avoid crash on _FORTIFY_SOURCE=3
    Without the change `zsnesn` startup crashes with:
    
        $ gdb zsnes
        *** buffer overflow detected ***: terminated
        ...
        #7  0x08057c14 in memset (__len=2, __ch=255, __dest=<optimized out>) at
          ...-glibc-2.38-23-dev/include/bits/string_fortified.h:59

--- pkgs/applications/emulators/zsnes/default.nix
+++ pkgs/applications/emulators/zsnes/default.nix
@@ -23,7 +23,10 @@ in stdenv.mkDerivation {
     sha256 = "1gy79d5wdaacph0cc1amw7mqm7i0716n6mvav16p1svi26iz193v";
   };
 
-  patches = [ ./zlib-1.3.patch ];
+  patches = [
+    ./zlib-1.3.patch
+    ./fortify3.patch
+  ];
 
   buildInputs = [ nasm SDL zlib libpng ncurses libGLU libGL ];
 
