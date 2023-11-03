4eea3ae9b0f72df1e4e229efbe7a703855742ce0
natsukium <tomoya.otabi@gmail.com>
2023-11-03 19:24:19 +0900

    python311Packages.flufl-i18n: refactor

--- pkgs/development/python-modules/flufl/i18n.nix
+++ pkgs/development/python-modules/flufl/i18n.nix
@@ -1,27 +1,45 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 , atpublic
 , pdm-pep517
+, pytestCheckHook
+, sybil
 }:
 
 buildPythonPackage rec {
   pname = "flufl-i18n";
   version = "4.1.1";
-  format = "pyproject";
+  pyproject = true;
 
-  nativeBuildInputs = [ pdm-pep517 ];
-  propagatedBuildInputs = [ atpublic ];
-
-  doCheck = false;
-
-  pythonImportsCheck = [ "flufl.i18n" ];
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "flufl.i18n";
     inherit version;
     hash = "sha256-wKz6aggkJ9YBJ+o75XjC4Ddnn+Zi9hlYDnliwTc7DNs=";
   };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace "--cov=flufl --cov-report=term --cov-report=xml" ""
+  '';
+
+  nativeBuildInputs = [ pdm-pep517 ];
+
+  propagatedBuildInputs = [ atpublic ];
+
+  pythonImportsCheck = [ "flufl.i18n" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    sybil
+  ];
+
+  pythonNamespaces = [
+    "flufl"
+  ];
 
   meta = with lib; {
     description = "A high level API for internationalizing Python libraries and applications";
