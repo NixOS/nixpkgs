885b390f2a28f12ae4833f8c8c57bc9f9a274952
Artturin <Artturin@artturin.com>
2023-11-03 05:21:37 +0200

    lib.makeScopeWithSplicing': pass spliced to f instead of unspliced self
    ```nix
    pyqtwebengine = pkgs.libsForQt5.callPackage ../development/python-modules/pyqtwebengine {
      pythonPackages = self;
    };
    ```
    
    ```
    nix-repl> lib.elemAt pkgsCross.aarch64-multiplatform.python3Packages.pyqtwebengine.nativeBuildInputs 11
    «derivation /nix/store/mjizwsczps5xs5cxvj5qqcmgja6nrdwl-python3.11-sip-6.7.11-aarch64-unknown-linux-gnu.drv»
    ```
    
    ```
    nix-repl> lib.elemAt pkgsCross.aarch64-multiplatform.python3Packages.pyqtwebengine.nativeBuildInputs 11
    «derivation /nix/store/2hfhw8l0mry7yjli6z4w79rlhs9g8n0k-python3.11-sip-6.7.11.drv»
    ```

--- lib/customisation.nix
+++ lib/customisation.nix
@@ -357,7 +357,7 @@ rec {
         pkgsTargetTarget = otherSplices.selfTargetTarget;
       };
       spliced = extra spliced0 // spliced0 // keep self;
-      self = f self // {
+      self = f spliced // {
         newScope = scope: newScope (spliced // scope);
         callPackage = newScope spliced; # == self.newScope {};
         # N.B. the other stages of the package set spliced in are *not*
