c8396e38c10d9a01bbe600bc64da4593f6071bfb
natsukium <tomoya.otabi@gmail.com>
2023-11-03 23:36:08 +0900

    python311Packages.lazr-delegates: refactor

--- pkgs/development/python-modules/lazr/delegates.nix
+++ pkgs/development/python-modules/lazr/delegates.nix
@@ -1,16 +1,46 @@
-{ buildPythonPackage, fetchPypi, nose, zope_interface }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, zope_interface
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "lazr-delegates";
   version = "2.0.4";
-
-  propagatedBuildInputs = [ nose zope_interface ];
-
-  doCheck = false;  # cannot import name 'ClassType' from 'types'
+  pyproject = true;
 
   src = fetchPypi {
     pname = "lazr.delegates";
     inherit version;
-    sha256 = "1rdnl85j9ayp8n85l0ciip621j9dcziz5qnmv2m7krgwgcn31vfx";
+    hash = "sha256-3e0wLHv85Xmq2NXi8uNnLckgzI2RAVqQRderJAuituU=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    zope_interface
+  ];
+
+  pythonImportsCheck = [
+    "lazr.delegates"
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pythonNamespaces = [
+    "lazr"
+  ];
+
+  meta = with lib; {
+    description = "Easily write objects that delegate behavior";
+    homepage = "https://launchpad.net/lazr.delegates";
+    changelog = "https://git.launchpad.net/lazr.delegates/tree/lazr/delegates/docs/NEWS.rst?h=${version}";
+    license = licenses.lgpl3Only;
   };
 }
