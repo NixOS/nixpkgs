61c170a59ff40a9dbcde5b7f8730b32a8fa8bee3
Aaron Honeycutt <aaron@system76.com>
2023-11-02 18:35:31 -0600

    cosmic-edit: init at unstable-2023-11-02

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30991,6 +30991,8 @@ with pkgs;
 
   cosmic-settings = callPackage ../applications/window-managers/cosmic/settings { };
 
+  cosmic-edit = callPackage ../applications/editors/cosmic-edit { };
+
   coursera-dl = callPackage ../applications/misc/coursera-dl { };
 
   coyim = callPackage ../applications/networking/instant-messengers/coyim { };
