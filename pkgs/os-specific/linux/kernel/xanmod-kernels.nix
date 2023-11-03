10a7a60623807e325ca4beb267966654c23968f2
zzzsyyy <me@zzzsy.top>
2023-11-03 16:31:02 +0800

    linux_xanmod: 6.1.60 -> 6.1.61

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -3,8 +3,8 @@
 let
   # These names are how they are designated in https://xanmod.org.
   ltsVariant = {
-    version = "6.1.60";
-    hash = "sha256-KYCeONJxyFPee4pvBLRw/MBTzPU7D2oZCrAVr3t/yPM=";
+    version = "6.1.61";
+    hash = "sha256-15dAzCcFUekBlkBfcHWiZ3gourimLp6GlUKeWAddDqo=";
     variant = "lts";
   };
 
