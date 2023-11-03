464762c58443e5862da38cc7ddef4205c2650839
Bobby Rong <rjl931189261@126.com>
2023-11-03 13:24:55 +0000

    gnome.gnome-shell: 45.0 → 45.1
    https://gitlab.gnome.org/GNOME/gnome-shell/-/compare/45.0...45.1
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/core/gnome-shell/default.nix
+++ pkgs/desktops/gnome/core/gnome-shell/default.nix
@@ -67,13 +67,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "gnome-shell";
-  version = "45.0";
+  version = "45.1";
 
   outputs = [ "out" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-shell/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "PKvkC91ON5xwgrOf+kW4vvXJz4ZAj7DYNuFXHEQyF80=";
+    sha256 = "FfykvWEpqLP5kBl/vR7ljXS2QVEK+q8Igqf6NmNPxfI=";
   };
 
   patches = [
