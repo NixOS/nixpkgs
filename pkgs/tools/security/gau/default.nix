84b5f949b45cfeae519b76611cf722f0b673f6ad
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-03 09:08:58 +0100

    gau: 2.2.0 -> 2.2.1
    Diff: https://github.com/lc/gau/compare/v2.2.0...v2.2.1

--- pkgs/tools/security/gau/default.nix
+++ pkgs/tools/security/gau/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "gau";
-  version = "2.2.0";
+  version = "2.2.1";
 
   src = fetchFromGitHub {
     owner = "lc";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-09Mf6KmTO3gper7txhvPZ/lb/cFOTDWicpu7+JrNrbo=";
+    sha256 = "sha256-AtKakeQnxRbFAbK/aQ4OQoEowN753jm4P4M57Oo3x1Y=";
   };
 
   vendorHash = "sha256-nhsGhuX5AJMHg+zQUt1G1TwVgMCxnuJ2T3uBrx7bJNs=";
