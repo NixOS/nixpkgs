f43e8afa4406c9684e9ad4e303e0a50b30e400f7
networkException <git@nwex.de>
2023-11-03 17:24:07 +0100

    chromium: 118.0.5993.117 -> 119.0.6045.105
    https://chromereleases.googleblog.com/2023/10/stable-channel-update-for-desktop_31.html
    
    This update includes 15 security fixes.
    
    CVEs:
    CVE-2023-5480 CVE-2023-5482 CVE-2023-5849 CVE-2023-5850 CVE-2023-5851
    CVE-2023-5852 CVE-2023-5853 CVE-2023-5854 CVE-2023-5855 CVE-2023-5856
    CVE-2023-5857 CVE-2023-5858 CVE-2023-5859

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -9,15 +9,15 @@
     };
     deps = {
       gn = {
-        hash = "sha256-SwlET5h5xtDlQvlt8wbG73ZfUWJr4hlWc+uQsBH5x9M=";
-        rev = "cc56a0f98bb34accd5323316e0292575ff17a5d4";
+        hash = "sha256-4jWqtsOBh96xbYk1m06G9hj2eQwW6buUXsxWsa5W6/4=";
+        rev = "991530ce394efb58fcd848195469022fa17ae126";
         url = "https://gn.googlesource.com/gn";
-        version = "2023-08-10";
+        version = "2023-09-12";
       };
     };
-    hash = "sha256-65rN17DIF+9FgZu7ohc9dM8ni6Qmqc9l1oyOcloip44=";
-    hash_deb_amd64 = "sha256-RJcyIA0TdXWRk+K2GVcHSv4OSq5c6Y7InUblao3uusc=";
-    version = "118.0.5993.117";
+    hash = "sha256-sVBZ0FnaJg1P9a2X8N1MSs8ehPSPzgfbhprb+4v0gXA=";
+    hash_deb_amd64 = "sha256-WLTTFMUvtBHvvegDFpZ+7Eht9StMyleaqXEBhPhgPTs=";
+    version = "119.0.6045.105";
   };
   ungoogled-chromium = {
     deps = {
