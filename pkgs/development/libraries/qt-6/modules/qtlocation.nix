a9d1f65ea7cc3accc446c3250c0f92c05d46cace
Adam Joseph <adam@westernsemico.com>
2023-11-04 14:29:39 -0700

    qt6.qtlocation: fix cross

--- pkgs/development/libraries/qt-6/modules/qtlocation.nix
+++ pkgs/development/libraries/qt-6/modules/qtlocation.nix
@@ -1,10 +1,14 @@
-{ qtModule
+{ lib
+, stdenv
+, qtModule
 , qtbase
 , qtdeclarative
 , qtpositioning
+, pkgsBuildHost
 }:
 
 qtModule {
   pname = "qtlocation";
   propagatedBuildInputs = [ qtbase qtdeclarative qtpositioning ];
+  nativeQtBuildInputs = [ "qtpositioning" "qtdeclarative" ];
 }
