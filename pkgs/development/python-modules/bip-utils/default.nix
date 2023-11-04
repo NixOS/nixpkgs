e9dfc59d9ec367b4b402d77ee99dc670fc5303c9
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-04 10:34:20 +0100

    python310Packages.bip-utils: update disabled

--- pkgs/development/python-modules/bip-utils/default.nix
+++ pkgs/development/python-modules/bip-utils/default.nix
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   version = "2.8.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "ebellocchia";
