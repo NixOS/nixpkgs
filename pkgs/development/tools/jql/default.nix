eae5257c9ff49091e913746c3f223d68dc5312e8
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-04 13:47:08 +0000

    jql: 7.0.4 -> 7.0.5

--- pkgs/development/tools/jql/default.nix
+++ pkgs/development/tools/jql/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "jql";
-  version = "7.0.4";
+  version = "7.0.5";
 
   src = fetchFromGitHub {
     owner = "yamafaktory";
     repo = pname;
     rev = "jql-v${version}";
-    hash = "sha256-Qy4ozTRdDF8ENyk4AB2a/4AuMSLJd/3w/q9TjGgrkPE=";
+    hash = "sha256-D+69R63er4OIMOdyoCuGVvVcUlM2PhLKEyUat1yEmLU=";
   };
 
-  cargoHash = "sha256-or69dz+wMhp8CPLzip6c6S7HpilAE2DAVkv/3IJMJWQ=";
+  cargoHash = "sha256-2/0gUavyrNxhc2j+ECPsvMLKCkZdSClivEcJVE1zFgA=";
 
   meta = with lib; {
     description = "A JSON Query Language CLI tool built with Rust";
