164663147ccd5ab57ed76a6757ec29f78ac44d3c
natsukium <tomoya.otabi@gmail.com>
2023-11-04 14:31:47 +0900

    python311Packages.zope-event: refactor

--- pkgs/development/python-modules/zope-event/default.nix
+++ pkgs/development/python-modules/zope-event/default.nix
@@ -1,11 +1,14 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, setuptools
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "zope-event";
   version = "4.6";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "zope.event";
@@ -13,11 +16,31 @@ buildPythonPackage rec {
     hash = "sha256-gdmIEwRvyGzEE242mP7mKKMoL5wyDbGGWMIXSSNfzoA=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pytestFlagsArray = [
+    "src/zope/event/tests.py"
+  ];
+
+  pythonImportsCheck = [
+    "zope.event"
+  ];
+
+  pythonNamespaces = [
+    "zope"
+  ];
+
   meta = with lib; {
     description = "An event publishing system";
     homepage = "https://pypi.org/project/zope.event/";
-    license = licenses.zpl20;
+    changelog = "https://github.com/zopefoundation/zope.event/blob/${src}/CHANGES.rst";
+    license = licenses.zpl21;
     maintainers = with maintainers; [ goibhniu ];
   };
-
 }
