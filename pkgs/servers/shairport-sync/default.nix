11e49554f816844079548e70b0119bb7a7f84389
Adam Stephens <adam@valkor.net>
2023-11-04 12:29:09 -0400

    shairport-sync: add adamcstephens as maintainer

--- pkgs/servers/shairport-sync/default.nix
+++ pkgs/servers/shairport-sync/default.nix
@@ -117,11 +117,11 @@ stdenv.mkDerivation rec {
 
   strictDeps = true;
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/mikebrady/shairport-sync";
     description = "Airtunes server and emulator with multi-room capabilities";
-    license = licenses.mit;
-    maintainers = with maintainers; [ lnl7 jordanisaacs ];
-    platforms = platforms.unix;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ lnl7 jordanisaacs ];
+    platforms = lib.platforms.unix;
   };
 }
