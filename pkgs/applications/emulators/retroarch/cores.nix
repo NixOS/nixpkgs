NixOS/nixpkgs

commit fe330c57da8bdd986571071fc606c937163f9f25

retroarchFull: fix pcsx2 core installation

pkgs/applications/emulators/retroarch/cores.nix
@@ -768,7 +768,7 @@ in
     # causes redefinition of _FORTIFY_SOURCE
     hardeningDisable = [ "fortify3" ];
 
-    postBuild = "cd /build/source/build/pcsx2";
+    postBuild = "cd $NIX_BUILD_TOP/source/build/pcsx2";
     meta = {
       description = "Port of PCSX2 to libretro";
       license = lib.licenses.gpl3Plus;

commit 05a15bfcfb58e53e57e1fe64a9ea60fc99c5b460

libretro.pcsx2: avoid $NIX_BUILD_TOP
Using $NIX_BUILD_TOP in a Nix expression break under nix-shell (or when
using nativeBuildInputs = [ keepBuildTree ]), so let's avoid it.

Ref. https://github.com/NixOS/nixpkgs/issues/189691

Fixes: fe330c57da8bdd98 ("retroarchFull: fix pcsx2 core installation")

pkgs/applications/emulators/retroarch/cores.nix
@@ -760,7 +760,7 @@ in
     # causes redefinition of _FORTIFY_SOURCE
     hardeningDisable = [ "fortify3" ];
 
-    postBuild = "cd $NIX_BUILD_TOP/source/build/pcsx2";
+    postBuild = "cd pcsx2";
     meta = {
       description = "Port of PCSX2 to libretro";
       license = lib.licenses.gpl3Plus;