309903802adf7742884aeb3ce6ec1b49c6c72bb1
Vincenzo Mantova <1962985+xworld21@users.noreply.github.com>
2023-11-04 20:02:28 +0000

    nuweb: replace texlive.combined.scheme-medium with texliveMedium

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19205,9 +19205,7 @@ with pkgs;
   # NOTE: Override and set useIcon = false to use Awk instead of Icon.
   noweb = callPackage ../development/tools/literate-programming/noweb { };
 
-  nuweb = callPackage ../development/tools/literate-programming/nuweb {
-    tex = texlive.combined.scheme-medium;
-  };
+  nuweb = callPackage ../development/tools/literate-programming/nuweb { };
 
   eztrace = callPackage ../development/tools/profiling/EZTrace { };
 
