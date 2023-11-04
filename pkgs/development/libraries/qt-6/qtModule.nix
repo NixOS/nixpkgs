437baa2688d1009057bf7ad4d6953d55d0c6a2da
Adam Joseph <adam@westernsemico.com>
2023-11-04 14:31:01 -0700

    qt6.qtModule: fix cross

--- pkgs/development/libraries/qt-6/qtModule.nix
+++ pkgs/development/libraries/qt-6/qtModule.nix
@@ -6,6 +6,7 @@
 , moveBuildTree
 , srcs
 , patches ? [ ]
+, pkgsBuildHost
 }:
 
 args:
@@ -15,7 +16,7 @@ let
   version = args.version or srcs.${pname}.version;
   src = args.src or srcs.${pname}.src;
 in
-stdenv.mkDerivation (args // {
+(stdenv.mkDerivation ((builtins.removeAttrs args [ "dontIgnorePath" "nativeQtBuildInputs" ]) // {
   inherit pname version src;
   patches = args.patches or patches.${pname} or [ ];
 
@@ -39,4 +40,32 @@ stdenv.mkDerivation (args // {
     maintainers = with maintainers; [ milahu nickcao ];
     platforms = platforms.unix;
   } // (args.meta or { });
-})
+}))
+  .overrideAttrs(previousAttrs: lib.optionalAttrs (stdenv.buildPlatform != stdenv.hostPlatform) {
+    strictDeps = true;
+    env = (previousAttrs.env or {}) // { QT_HOST_PATH = "${pkgsBuildHost.qt6Packages.qtbase}"; };
+    cmakeFlags = [
+      "-DQT_FORCE_BUILD_TOOLS=true"
+    ] ++ lib.optionals (!(args.dontIgnorePath or false)) [
+      # When cross-compiling, QT's build process will ignore the
+      # $PATH, assuming a conventional FHS distro, all QT packages
+      # smashed together into a single installation directory, and
+      # poor hygiene on the part of the developer.  Since we carefully
+      # manage our $PATH and install each package to a separate prefix
+      # it's okay for QT to trust the $PATH we expose to it.
+      #
+      # The bizzarrely-named
+      # `QT_DISABLE_NO_DEFAULT_PATH_IN_QT_PACKAGES` option is how you
+      # tell QT that we are smarter than it is.
+      #
+      # See: https://codereview.qt-project.org/gitweb?p=qt%2Fqtbase.git;a=commit;h=7bb91398f25cb2016c0558fd397b376f413e3e96
+      # TODO: set this globally, everywhere.
+      "-DQT_DISABLE_NO_DEFAULT_PATH_IN_QT_PACKAGES=true"
+    ] ++ lib.optional (args?nativeQtBuildInputs)
+      ("-DQT_ADDITIONAL_HOST_PACKAGES_PREFIX_PATH=${
+        lib.concatMapStringsSep ":"
+          (pname: pkgsBuildHost.qt6Packages.${pname})
+          args.nativeQtBuildInputs}")
+    ++ (previousAttrs.cmakeFlags or []);
+  })
+
