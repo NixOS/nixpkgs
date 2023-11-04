167d320d07dd16c15fae8d8bf5111dc46946001b
Yuriy Taraday <yorik.sar@gmail.com>
2023-11-04 08:35:02 +0100

    dark-mode-notify: migrate to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3713,8 +3713,6 @@ with pkgs;
 
   dcap = callPackage ../tools/networking/dcap { };
 
-  dark-mode-notify = callPackage ../os-specific/darwin/dark-mode-notify { };
-
   deltachat-cursed = callPackage ../applications/networking/instant-messengers/deltachat-cursed { };
 
   delayarchitect = callPackage ../applications/audio/delayarchitect { };
