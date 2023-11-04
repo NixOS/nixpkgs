8e7947fa0604dedddc4a2d38837f3b4c47310b3f
natsukium <tomoya.otabi@gmail.com>
2023-11-04 13:34:17 +0900

    python311Packages.torrent-parser: refactor

--- pkgs/development/python-modules/torrent-parser/default.nix
+++ pkgs/development/python-modules/torrent-parser/default.nix
@@ -1,20 +1,38 @@
-{ buildPythonPackage, lib, fetchFromGitHub }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "torrent-parser";
   version = "0.4.1";
+  pyproject = true;
 
-  # No tarballs on Pypi
   src = fetchFromGitHub {
     owner  = "7sDream";
     repo   = "torrent_parser";
     rev    = "v${version}";
-    sha256 = "sha256-zM738r3o9dGZYoWLN7fM4E06m6YPcAODEkgDS6wU/Sc=";
+    hash = "sha256-zM738r3o9dGZYoWLN7fM4E06m6YPcAODEkgDS6wU/Sc=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "torrent_parser"
+  ];
 
   meta = {
     description = "A .torrent file parser and creator for both Python 2 and 3";
     homepage    = "https://github.com/7sDream/torrent_parser";
+    changelog = "https://github.com/7sDream/torrent_parser/blob/${src.rev}/CHANGELOG.md";
     license     = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ];
   };
