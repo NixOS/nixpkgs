097a352b7083fdd7207efbe295ac830b059b3dff
natsukium <tomoya.otabi@gmail.com>
2023-11-04 13:03:25 +0900

    python211Packages.radio-beam: refactor
    - use SRI hash
    - adopt pyproject instead of format
    - add pythonImportsCheck
    - remove unnecessary meta.platforms

--- pkgs/development/python-modules/radio-beam/default.nix
+++ pkgs/development/python-modules/radio-beam/default.nix
@@ -14,11 +14,11 @@
 buildPythonPackage rec {
   pname = "radio-beam";
   version = "0.3.4";
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "e032257f1501303873f251c00c74b1188180785c79677fb4443098d517852309";
+    hash = "sha256-4DIlfxUBMDhz8lHADHSxGIGAeFx5Z3+0RDCY1ReFIwk=";
   };
 
   nativeBuildInputs = [
@@ -38,11 +38,14 @@ buildPythonPackage rec {
     pytest-astropy
   ];
 
+  pythonImportsCheck = [
+    "radio_beam"
+  ];
+
   meta = {
     description = "Tools for Beam IO and Manipulation";
     homepage = "http://radio-astro-tools.github.io";
     license = lib.licenses.bsd3;
-    platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ smaret ];
   };
 }
