ca7c2d02b1839009070efa800672352d3e3130a1
Robert Schütz <nix@dotlambda.de>
2023-11-03 22:41:08 -0700

    deltachat-repl: init at 1.128.0

--- pkgs/development/libraries/libdeltachat/default.nix
+++ pkgs/development/libraries/libdeltachat/default.nix
@@ -3,6 +3,7 @@
 , fetchFromGitHub
 , cargo
 , cmake
+, deltachat-repl
 , openssl
 , perl
 , pkg-config
@@ -17,7 +18,17 @@
 , libiconv
 }:
 
-stdenv.mkDerivation rec {
+let
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "email-0.0.21" = "sha256-u4CsK/JqFgq5z3iJGxxGtb7QbSkOAqmOvrmagsqfXIU=";
+      "encoded-words-0.2.0" = "sha256-KK9st0hLFh4dsrnLd6D8lC6pRFFs8W+WpZSGMGJcosk=";
+      "iroh-0.4.1" = "sha256-oLvka1nV2yQPzlcaq5CXqXRRu7GkbMocV6GoIlxQKlo=";
+      "lettre-0.9.2" = "sha256-+hU1cFacyyeC9UGVBpS14BWlJjHy90i/3ynMkKAzclk=";
+    };
+  };
+in stdenv.mkDerivation rec {
   pname = "libdeltachat";
   version = "1.128.0";
 
@@ -32,15 +43,7 @@ stdenv.mkDerivation rec {
     ./no-static-lib.patch
   ];
 
-  cargoDeps = rustPlatform.importCargoLock {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "email-0.0.21" = "sha256-u4CsK/JqFgq5z3iJGxxGtb7QbSkOAqmOvrmagsqfXIU=";
-      "encoded-words-0.2.0" = "sha256-KK9st0hLFh4dsrnLd6D8lC6pRFFs8W+WpZSGMGJcosk=";
-      "iroh-0.4.1" = "sha256-oLvka1nV2yQPzlcaq5CXqXRRu7GkbMocV6GoIlxQKlo=";
-      "lettre-0.9.2" = "sha256-+hU1cFacyyeC9UGVBpS14BWlJjHy90i/3ynMkKAzclk=";
-    };
-  };
+  cargoDeps = rustPlatform.importCargoLock cargoLock;
 
   nativeBuildInputs = [
     cmake
@@ -67,8 +70,12 @@ stdenv.mkDerivation rec {
     cargoCheckHook
   ];
 
-  passthru.tests = {
-    python = python3.pkgs.deltachat;
+  passthru = {
+    inherit cargoLock;
+    tests = {
+      inherit deltachat-repl;
+      python = python3.pkgs.deltachat;
+    };
   };
 
   meta = with lib; {
