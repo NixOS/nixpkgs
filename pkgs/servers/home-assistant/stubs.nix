5624c7fe381500dd2f35fddbe2d9ef254b200521
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-11-05 09:44:41 +0100

    python311Packages.homeassistant-stubs: 2023.11.0 -> 2023.11.1
    https://github.com/KapJI/homeassistant-stubs/releases/tag/2023.11.1

--- pkgs/servers/home-assistant/stubs.nix
+++ pkgs/servers/home-assistant/stubs.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "homeassistant-stubs";
-  version = "2023.11.0";
+  version = "2023.11.1";
   format = "pyproject";
 
   disabled = python.version != home-assistant.python.version;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "KapJI";
     repo = "homeassistant-stubs";
     rev = "refs/tags/${version}";
-    hash = "sha256-/yiTNsr22X8orQ8sni50/FeWWZL9OyVZvTngeaGv5SI=";
+    hash = "sha256-eLmWOMKLzhZ7M/gdUHhlDZ3T+N4h5aHxMwOI8ZUepps=";
   };
 
   nativeBuildInputs = [
