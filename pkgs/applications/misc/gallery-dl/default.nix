10ac60e75c6ee0716f3ec21fc434c7295845f898
Mario Rodas <marsam@users.noreply.github.com>
2023-11-05 04:20:00 +0000

    gallery-dl: 1.26.1 -> 1.26.2
    Changelog: https://github.com/mikf/gallery-dl/blob/v1.26.2/CHANGELOG.md

--- pkgs/applications/misc/gallery-dl/default.nix
+++ pkgs/applications/misc/gallery-dl/default.nix
@@ -2,13 +2,13 @@
 
 buildPythonApplication rec {
   pname = "gallery-dl";
-  version = "1.26.1";
+  version = "1.26.2";
   format = "setuptools";
 
   src = fetchPypi {
     inherit version;
     pname = "gallery_dl";
-    sha256 = "sha256-SJshEdvmPDQZ5mqiQfJpWcQ43WGXUxPvMMJiY/4Cxsc=";
+    sha256 = "sha256-Agccsz0TlzCDnhR5Vy7Tt3jrqz9+hwaclQgXJBhGY9w=";
   };
 
   propagatedBuildInputs = [
