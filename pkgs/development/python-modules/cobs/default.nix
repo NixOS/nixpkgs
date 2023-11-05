06f8b5fe65356f2b30cdd2ffdb03e5296ae5bdda
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-05 05:47:45 +0000

    python310Packages.cobs: 1.2.0 -> 1.2.1

--- pkgs/development/python-modules/cobs/default.nix
+++ pkgs/development/python-modules/cobs/default.nix
@@ -6,13 +6,13 @@
 
 buildPythonPackage rec {
   pname = "cobs";
-  version = "1.2.0";
+  version = "1.2.1";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-2TsQtTcNyIaJYK77cK2x9zpOYQexaRgwekru79PtuPY=";
+    hash = "sha256-Kvf4eRzeGufGuTb10MNf4p/rEN4l95wVsK8NZyl4PMA=";
   };
 
   checkPhase = ''
