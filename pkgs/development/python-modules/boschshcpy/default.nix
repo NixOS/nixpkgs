20467cc9cd6255782420dc3572d5f66ce1bd3773
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-06 08:31:00 +0100

    python311Packages.boschshcpy: 0.2.73 -> 0.2.75
    Diff: https://github.com/tschamm/boschshcpy/compare/0.2.73...0.2.75

--- pkgs/development/python-modules/boschshcpy/default.nix
+++ pkgs/development/python-modules/boschshcpy/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "boschshcpy";
-  version = "0.2.73";
+  version = "0.2.75";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "tschamm";
     repo = pname;
     rev = version;
-    hash = "sha256-DHH9VZWnQaLWEiZSrU4y2/jlqhvUvoKRjWpBTz01m8k=";
+    hash = "sha256-T3QTNnnkquv0IurwNtblX9CF/gLeMONEFfbJV/n/Wj4=";
   };
 
   propagatedBuildInputs = [
