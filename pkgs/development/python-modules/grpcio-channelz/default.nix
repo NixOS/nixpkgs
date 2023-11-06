ed515e0b04bd7fa845cdb44414cccdbdf1eb20dd
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-06 10:15:09 +0000

    python310Packages.grpcio-channelz: 1.59.0 -> 1.59.2

--- pkgs/development/python-modules/grpcio-channelz/default.nix
+++ pkgs/development/python-modules/grpcio-channelz/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "grpcio-channelz";
-  version = "1.59.0";
+  version = "1.59.2";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-H25CEs6JW+gnpuGJe88vSh0h5ETKMT8lxEgfViExhMk=";
+    hash = "sha256-apOnMd8EDU+ocj6fL/dkl3tqh6en/Q8H9K/lgvkqFN8=";
   };
 
   nativeBuildInputs = [
