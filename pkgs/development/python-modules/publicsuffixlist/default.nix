def9f36c899044ea5c5164dc56030a7f67975676
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-06 08:50:33 +0100

    python311Packages.publicsuffixlist: 0.10.0.20231104 -> 0.10.0.20231105

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "publicsuffixlist";
-  version = "0.10.0.20231104";
+  version = "0.10.0.20231105";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-p+sOaRfp2IDdXL6eotCrSHu91i+mAwavmqELa5nDB7c=";
+    hash = "sha256-+/r2gVtA0j+aY827B0wZCOlogil7921V94/bay8/dVg=";
   };
 
   nativeBuildInputs = [
