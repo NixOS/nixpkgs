NixOS/nixpkgs

commit 5cebbd4be4a818386d2d24861a3c0386c3c242c0

python311Packages.django-q: mark as broken

pkgs/development/python-modules/django-q/default.nix
@@ -1,34 +1,45 @@
-{ arrow
+{ lib
+, stdenv
+, arrow
 , blessed
 , buildPythonPackage
 , croniter
 , django
-, django-redis
 , django-picklefield
+, django-redis
 , fetchFromGitHub
 , future
-, lib
+, pkgs
 , poetry-core
 , pytest-django
 , pytest-mock
 , pytestCheckHook
-, pkgs
-, stdenv
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "django-q";
   version = "1.3.9";
-  format = "pyproject";
+  pyproject = true;
+
+  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "Koed00";
     repo = "django-q";
+    rev = "refs/tags/v${version}";
     hash = "sha256-gFSrAl3QGoJEJfvTTvLQgViPPjeJ6BfvgEwgLLo+uAA=";
-    rev = "v${version}";
   };
 
-  nativeBuildInputs = [ poetry-core ];
+  # fixes empty version string
+  # analog to https://github.com/NixOS/nixpkgs/pull/171200
+  patches = [
+    ./pep-621.patch
+  ];
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
 
   propagatedBuildInputs = [
     django-picklefield
@@ -38,10 +49,12 @@ buildPythonPackage rec {
     future
   ];
 
-  # fixes empty version string
-  # analog to https://github.com/NixOS/nixpkgs/pull/171200
-  patches = [
-    ./pep-621.patch
+  nativeCheckInputs = [
+    croniter
+    django-redis
+    pytest-django
+    pytest-mock
+    pytestCheckHook
   ];
 
   pythonImportsCheck = [
@@ -57,14 +70,6 @@ buildPythonPackage rec {
     kill $REDIS_PID
   '';
 
-  nativeCheckInputs = [
-    croniter
-    django-redis
-    pytest-django
-    pytest-mock
-    pytestCheckHook
-  ];
-
   # don't bother with two more servers to test
   disabledTests = [
     "test_disque"
@@ -76,7 +81,11 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "A multiprocessing distributed task queue for Django";
     homepage = "https://django-q.readthedocs.org";
+    changelog = "https://github.com/Koed00/django-q/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ gador ];
+    # django-q is unmaintained at the moment
+    # https://github.com/Koed00/django-q/issues/733
+    broken = true;
   };
 }

pkgs/development/python-modules/django-redis/default.nix
@@ -18,12 +18,9 @@
 , pytestCheckHook
 }:
 
-let
+buildPythonPackage rec {
   pname = "django-redis";
   version = "5.4.0";
-in
-buildPythonPackage {
-  inherit pname version;
   pyproject = true;
 
   disabled = pythonOlder "3.6";
@@ -45,11 +42,16 @@ buildPythonPackage {
 
   propagatedBuildInputs = [
     django
-    hiredis
     lz4
     msgpack
     redis
   ];
+
+  passthru.optional-dependencies = {
+    hiredis = [
+      redis
+    ] ++ redis.optional-dependencies.hiredis;
+  };
 
   pythonImportsCheck = [
     "django_redis"
@@ -70,7 +72,7 @@ buildPythonPackage {
     pytest-django
     pytest-mock
     pytestCheckHook
-  ];
+  ] ++ lib.flatten (builtins.attrValues passthru.optional-dependencies);
 
   pytestFlagsArray = [
     "-W"

commit ec10e4e9961d9895c4f73468fbf39f99a5038646

python3.pkgs.django-q: fix for Hyperkitty
mailmanPackages.hyperkitty was marked transiently broken, since it
depends on django-q, even though its Python package set overrides
redis to a verison compatible with django-q.

At the same time, hiredis was moved to being an optional dependency of
django-redis.  It's required by django-q, so use it here.

Fixes: 5cebbd4be4a8 ("python311Packages.django-q: mark as broken")

pkgs/development/python-modules/django-q/default.nix
@@ -15,6 +15,7 @@
 , pytest-mock
 , pytestCheckHook
 , pythonOlder
+, redis
 }:
 
 buildPythonPackage rec {
@@ -55,7 +56,7 @@ buildPythonPackage rec {
     pytest-django
     pytest-mock
     pytestCheckHook
-  ];
+  ] ++ django-redis.optional-dependencies.hiredis;
 
   pythonImportsCheck = [
     "django_q"
@@ -86,6 +87,6 @@ buildPythonPackage rec {
     maintainers = with maintainers; [ gador ];
     # django-q is unmaintained at the moment
     # https://github.com/Koed00/django-q/issues/733
-    broken = true;
+    broken = lib.versionAtLeast redis.version "5";
   };
 }