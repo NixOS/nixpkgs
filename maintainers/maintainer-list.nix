1bce16d06e9ec8f3ff69ae50247c33f8b7ec249e
Swastik <swstkbaranwal@gmail.com>
2023-11-06 21:29:43 +0530

    maintainers: add delta231

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4209,6 +4209,12 @@
     githubId = 12224254;
     name = "Delta";
   };
+  delta231 = {
+    email = "swstkbaranwal@gmail.com";
+    github = "Delta456";
+    githubId = 28479139;
+    name = "Swastik Baranwal";
+  };
   deltadelta = {
     email = "contact@libellules.eu";
     name = "Dara Ly";
