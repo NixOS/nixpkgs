af6823703bf5bde50039ee35f7565082f9bab79e
ckie <git-525ff67@ckie.dev>
2023-11-07 18:33:23 +0200

    mommy: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19704,8 +19704,6 @@ with pkgs;
     '';
   };
 
-  mommy = callPackage ../tools/misc/mommy { };
-
   moon = callPackage ../development/tools/build-managers/moon/default.nix { };
 
   msgpack-tools = callPackage ../development/tools/msgpack-tools { };
