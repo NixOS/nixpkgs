e76693422f2a6e693afd40a2cc0c5e979ae6f144
Anthony Roussel <anthony@roussel.dev>
2023-11-07 11:38:34 +0100

    axis2: add anthonyroussel to maintainers

--- pkgs/servers/http/tomcat/axis2/default.nix
+++ pkgs/servers/http/tomcat/axis2/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     description = "Web Services / SOAP / WSDL engine, the successor to the widely used Apache Axis SOAP stack";
     homepage = "https://axis.apache.org/axis2/java/core/";
     changelog = "https://axis.apache.org/axis2/java/core/release-notes/${version}.html";
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.anthonyroussel ];
     platforms = lib.platforms.unix;
     sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
     license = lib.licenses.asl20;
