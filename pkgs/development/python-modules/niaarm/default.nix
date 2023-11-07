82fbe6827d2be0e1270371d4b769df9f605d0b4d
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-07 02:18:19 +0000

    python310Packages.niaarm: 0.3.3 -> 0.3.5

--- pkgs/development/python-modules/niaarm/default.nix
+++ pkgs/development/python-modules/niaarm/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "niaarm";
-  version = "0.3.3";
+  version = "0.3.5";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "firefly-cpp";
     repo = "NiaARM";
     rev = "refs/tags/${version}";
-    hash = "sha256-kWOJfADqtC8YdZUlifKeiaS2a2cgcsMgCf0IHJt4NKY=";
+    hash = "sha256-E5G1uVDSErqwxTBNQ7qselemW9A3W8sr3ExPEh+1les=";
   };
 
   nativeBuildInputs = [
