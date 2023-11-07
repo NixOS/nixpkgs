1183bcf60435fd6b51d8b7f1a079e87762e0b3f2
ckie <git-525ff67@ckie.dev>
2023-11-07 18:32:19 +0200

    mommy: 1.2.3 -> 1.2.4

--- pkgs/tools/misc/mommy/default.nix
+++ pkgs/tools/misc/mommy/default.nix
@@ -23,27 +23,14 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "mommy";
-  version = "1.2.3";
+  version = "1.2.4";
 
   src = fetchFromGitHub {
     owner = "FWDekker";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-LT21MJg2rF84o2rWKguEP4UUOOu27nNGls95sBYgICw=";
+    hash = "sha256-SqWhbhQeRUO0cD9Fv/nwKjTI3F0Sg2VhFZtrbyA9Wb4=";
   };
-
-  patches = [
-    # 🫣 mommy finds your config with an environment variable now~
-    (fetchpatch {
-      url = "https://github.com/FWDekker/mommy/commit/d5785521fe2ce9ec832dbfe20abc483545b1df97.patch";
-      hash = "sha256-GacUjQyvvwRRYKIHHTTAL0mQSPMZbbxacqilBaw807Y=";
-    })
-    # 💜 mommy follows the XDG base directory specification now~
-    (fetchpatch {
-      url = "https://github.com/FWDekker/mommy/commit/71806bc0ace2822ac32dc8dd5bb0b0881d849982.patch";
-      hash = "sha256-Znq3VOgYI7a13Fqyz9nevtrvn7S5Jb2uBc78D0BG8rY=";
-    })
-  ];
 
   nativeBuildInputs = [ makeWrapper ];
   nativeCheckInputs = [ shellspec ];
