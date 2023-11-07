NixOS/nixpkgs

commit 1916d387bdb02bf0cba668bd5d526722f0c2c2c7

python3Packages.pyparted: 3.12.0 -> 3.13.0

pkgs/development/python-modules/pyparted/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "pyparted";
-  version = "3.12.0";
+  version = "3.13.0";
   disabled = isPyPy;
 
   src = fetchFromGitHub {
     repo = pname;
     owner = "dcantrell";
-    rev = "v${version}";
-    hash = "sha256-LfBLR0A/wnfBtXISAAY6Nl4vnk1rtY03F+PT8UIMrEs=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-AiUCCrEbDD0OxrvXs1YN3/1IE7SuVasC2YCirIG58iU=";
   };
 
   postPatch = ''

commit 18597c8aa70b439faff09d66d656b3f12f874209

python311Packages.pyparted: fix build for version 3.13.0
The update to version 3.13.0 in
1916d387bdb02bf0cba668bd5d526722f0c2c2c7
broke the build (for all python versions and architectures).

The following changes are required to fix the package:

* the two cherry-picked patches from upstream are dropped
  as they are already included in version 3.13.0
* "our" patch is adapted to chanes in the Makefile, see
  https://github.com/dcantrell/pyparted/commit/9131fcf6088cbad120c4fbb1d96c429e38cee543

pkgs/development/python-modules/pyparted/default.nix
@@ -1,6 +1,5 @@
 { lib, stdenv
 , fetchFromGitHub
-, fetchpatch
 , buildPythonPackage
 , isPyPy
 , pkgs
@@ -33,14 +32,6 @@ buildPythonPackage rec {
 
   patches = [
     ./fix-test-pythonpath.patch
-    (fetchpatch {
-      url = "https://github.com/dcantrell/pyparted/commit/07ba882d04fa2099b53d41370416b97957d2abcb.patch";
-      hash = "sha256-yYfLdy+TOKfN3gtTMgOWPebPTRYyaOYh/yFTowCbdjg=";
-    })
-    (fetchpatch {
-      url = "https://github.com/dcantrell/pyparted/commit/a01b4eeecf63b0580c192c7c2db7a5c406a7ad6d.patch";
-      hash = "sha256-M/8hYiKUBzaTOxPYDFK5BAvCm6WJGx+693qwj3HzdRA=";
-    })
   ];
 
   preConfigure = ''

pkgs/development/python-modules/pyparted/fix-test-pythonpath.patch
@@ -18,9 +18,9 @@ diff -ur a/Makefile b/Makefile
  	$(COVERAGE) report --include="build/lib.*/parted/*" > coverage-report.log
  
  check: clean
- 	env PYTHON=python3 $(MAKE) ; \
--	env PYTHON=python3 PYTHONPATH=$$(find $$(pwd) -name "*.so" | head -n 1 | xargs dirname):src/parted:src \
-+	env PYTHON=python3 PYTHONPATH=$$(find $$(pwd) -name "*.so" | head -n 1 | xargs dirname):src/parted:src:$$PYTHONPATH \
+ 	$(MAKE) ; \
+-	env PYTHONPATH=$$(find $$(pwd) -name "*.so" | head -n 1 | xargs dirname):src/parted:src \
++	env PYTHONPATH=$$(find $$(pwd) -name "*.so" | head -n 1 | xargs dirname):src/parted:src:$$PYTHONPATH \
  	tests/pylint/runpylint.py
  
  dist: