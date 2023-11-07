a74d08264d69a69f48d08f684a6a6349a718a88d
Cole Mickens <cole.mickens@gmail.com>
2023-11-07 17:32:16 -0500

    libtypec: init at 0.6.1

---
+++ pkgs/by-name/li/libtypec/package.nix
@@ -0,0 +1,57 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, meson
+, ninja
+, pkg-config
+, libusb1
+, systemd
+, libudev0-shim
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libtypec";
+  version = "0.6.1";
+
+  src = fetchFromGitHub {
+    owner = "libtypec";
+    repo = pname;
+    rev = "${pname}-${version}";
+    hash = "sha256-XkT0bgBjoJTAFa9NLZdzbJSpchiXxKjeu88PeT/AlPY=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    libusb1
+    libudev0-shim
+    systemd
+  ];
+
+  # postInstall = ''
+  #   cd $NIX_BUILD_TOP/source/utils
+  #   cmake CMakeLists.txt
+  #   cmake -P cmake_install.cmake
+  #   make
+  #   ls -al $NIX_BUILD_TOP/source/utils
+
+
+  #   # # TODO: use ins
+  #   # mkdir $out/bin
+  #   # cp $NIX_BUILD_TOP/source/utils/lstypec ${placeholder "bin"}/bin/lstypec
+  #   # cp $NIX_BUILD_TOP/source/utils/typecstatus ${placeholder "bin"}/bin/typecstatus
+  #   # # patchelf --print-rpath "$out/bin/lstypec"
+  # '';
+
+  meta = with lib; {
+    homepage = "https://github.com/Rajaram-Regupathy/libtypec";
+    description = "libtypec is aimed to provide a generic interface abstracting all platform complexity for user space to develop tools for efficient USB-C port management. The library can also enable development of diagnostic and debug tools to debug system issues around USB-C/USB PD topology.";
+    platforms = platforms.linux;
+    license = licenses.mit; # TODO: or gpl2
+    maintainers = with maintainers; [ ];
+  };
+}
