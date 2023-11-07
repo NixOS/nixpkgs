3441e3d32eab0e50a4fd3ea1bc7cb8a0190f21dc
Connor Baker <connor.baker@tweag.io>
2023-11-07 20:03:14 +0000

    cudaPackages_12_2.cuda-samples: init at 12.2

--- pkgs/test/cuda/cuda-samples/extension.nix
+++ pkgs/test/cuda/cuda-samples/extension.nix
@@ -15,6 +15,7 @@ final: prev: let
     "11.8" = "sha256-7+1P8+wqTKUGbCUBXGMDO9PkxYr2+PLDx9W2hXtXbuc=";
     "12.0" = "sha256-Lj2kbdVFrJo5xPYPMiE4BS7Z8gpU5JLKXVJhZABUe/g=";
     "12.1" = "sha256-xE0luOMq46zVsIEWwK4xjLs7NorcTIi9gbfZPVjIlqo=";
+    "12.2" = "sha256-pOy0qfDjA/Nr0T9PNKKefK/63gQnJV2MQsN2g3S2yng=";
   }.${prev.cudaVersion};
 
 in {
