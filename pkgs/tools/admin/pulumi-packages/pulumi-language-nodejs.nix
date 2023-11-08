ec3ae8ddf699f31aced16ec9e0ef74bef689a9c3
Andreas Stührk <andy@yaxi.tech>
2023-11-08 15:24:31 +0100

    pulumi: 3.90.1 -> 3.93.0

--- pkgs/tools/admin/pulumi-packages/pulumi-language-nodejs.nix
+++ pkgs/tools/admin/pulumi-packages/pulumi-language-nodejs.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
 
   sourceRoot = "${src.name}/sdk/nodejs/cmd/pulumi-language-nodejs";
 
-  vendorHash = "sha256-gbZoDUJCKb5wcLhJ7DRSGHT3Q0xUlptUeS6Wen4dle0";
+  vendorHash = "sha256-gEOVtAyn7v8tsRU11NgrD3swMFFBxOTIjMWCqSSvHlI=";
 
   postPatch = ''
     # Gives github.com/pulumi/pulumi/pkg/v3: is replaced in go.mod, but not marked as replaced in vendor/modules.txt etc
