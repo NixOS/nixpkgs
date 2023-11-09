da348739ab4cb29e3bec08400416ee02396d9132
Raphael Robatsch <raphael-git@tapesoftware.net>
2023-11-09 08:37:26 +0100

    mbedtls: 3.5.0 -> 3.5.1, mbedtls_2: 2.28.5 -> 2.28.6
    Upstream license was updated from Apache-2.0 to Apache-2.0 OR
    GPL-2.0-or-later.
    
    Changelog: https://github.com/Mbed-TLS/mbedtls/blob/v3.5.1/ChangeLog
    Changelog: https://github.com/Mbed-TLS/mbedtls/blob/v2.28.6/ChangeLog

--- pkgs/development/libraries/mbedtls/3.nix
+++ pkgs/development/libraries/mbedtls/3.nix
@@ -1,6 +1,6 @@
 { callPackage }:
 
 callPackage ./generic.nix {
-  version = "3.5.0";
-  hash = "sha256-uHHQmaAmFS8Vd7PrAfRpK+aNi3pJ76XBC7rFWcd16NU=";
+  version = "3.5.1";
+  hash = "sha256-HxsHcGbSExp1aG5yMR/J3kPL4zqnmNoN5T5wfV3APaw=";
 }
