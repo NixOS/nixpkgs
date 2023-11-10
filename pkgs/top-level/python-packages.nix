c4da593b47d2ad7a7917b0f36bcdda94794ecb45
Cody Hiar <cody@hiar.ca>
2023-11-10 10:45:44 -0700

    python3Packages.python-jwt: rename to python3Packages.jwt

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5857,6 +5857,8 @@ self: super: with self; {
 
   jwcrypto = callPackage ../development/python-modules/jwcrypto { };
 
+  jwt = callPackage ../development/python-modules/jwt { };
+
   jxmlease = callPackage ../development/python-modules/jxmlease { };
 
   k-diffusion = callPackage ../development/python-modules/k-diffusion { };
@@ -8280,8 +8282,6 @@ self: super: with self; {
   nsz = callPackage ../development/python-modules/nsz { };
 
   nxt-python = callPackage ../development/python-modules/nxt-python { };
-
-  python-jwt = callPackage ../development/python-modules/python-jwt { };
 
   python-ndn = callPackage ../development/python-modules/python-ndn { };
 
