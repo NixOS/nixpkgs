d247c57a748824e2691f003b3e295bc906a97645
Fabian Affolter <fabian@affolter-engineering.ch>
2023-11-10 16:38:18 +0100

    python311Packages.tailscale: 0.3.0 -> 0.6.0 (#265627)
    Diff: frenck/python-tailscale@refs/tags/v0.3.0...v0.6.0
    
    Changelog:
    - https://github.com/frenck/python-tailscale/releases/tag/v0.4.0
    - https://github.com/frenck/python-tailscale/releases/tag/v0.5.0
    - https://github.com/frenck/python-tailscale/releases/tag/v0.6.0

--- pkgs/development/python-modules/tailscale/default.nix
+++ pkgs/development/python-modules/tailscale/default.nix
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   version = "0.6.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "frenck";
