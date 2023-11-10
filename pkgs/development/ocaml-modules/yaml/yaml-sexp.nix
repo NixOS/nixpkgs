099511157c1580994cd3ecef9e8f6c22d730c7ae
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-11-10 05:44:41 +0100

    ocamlPackages.yaml: 3.1.0 → 3.2.0

--- pkgs/development/ocaml-modules/yaml/yaml-sexp.nix
+++ pkgs/development/ocaml-modules/yaml/yaml-sexp.nix
@@ -1,13 +1,16 @@
-{ lib, fetchurl, buildDunePackage, yaml, dune-configurator, ppx_sexp_conv, sexplib }:
+{ lib, fetchurl, buildDunePackage, yaml, dune-configurator, ppx_sexp_conv, sexplib
+, junit_alcotest
+}:
 
 buildDunePackage rec {
   pname = "yaml-sexp";
 
   inherit (yaml) version src;
 
-  duneVersion = "3";
-
   propagatedBuildInputs = [ yaml ppx_sexp_conv sexplib ];
+
+  doCheck = true;
+  checkInputs = [ junit_alcotest ];
 
   meta = yaml.meta // {
     description = "ocaml-yaml with sexp support";
