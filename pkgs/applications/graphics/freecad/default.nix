7876c91d5b80be9832897f55f58c10f0864f5bc8
Lily Foster <lily@lily.flowers>
2023-11-10 16:04:02 -0500

    freecad: add passthru.tests.python-path to test patch

--- pkgs/applications/graphics/freecad/default.nix
+++ pkgs/applications/graphics/freecad/default.nix
@@ -9,6 +9,7 @@
 , boost
 , coin3d
 , eigen
+, freecad  # for passthru.tests
 , gfortran
 , gts
 , hdf5
@@ -35,6 +36,7 @@
 , qtwebengine
 , qtx11extras
 , qtxmlpatterns
+, runCommand  # for passthru.tests
 , scipy
 , shiboken2
 , soqt
@@ -146,6 +148,21 @@ stdenv.mkDerivation (finalAttrs: {
     ln -s $out/bin/FreeCAD $out/bin/freecad
     ln -s $out/bin/FreeCADCmd $out/bin/freecadcmd
   '';
+
+  passthru.tests = {
+    # Check that things such as argument parsing still work correctly with
+    # the above PYTHONPATH patch. Previously the patch used above changed
+    # the `PyConfig_InitIsolatedConfig` to `PyConfig_InitPythonConfig`,
+    # which caused the built-in interpreter to attempt (and fail) to doubly
+    # parse argv. This should catch if that ever regresses and also ensures
+    # that PYTHONPATH is still respected enough for the FreeCAD console to
+    # successfully run and check that it was included in `sys.path`.
+    python-path = runCommand "freecad-test-console" {
+      nativeBuildInputs = [ freecad ];
+    } ''
+      HOME="$(mktemp -d)" PYTHONPATH="$(pwd)/test" FreeCADCmd --log-file $out -c "if not '$(pwd)/test' in sys.path: sys.exit(1)" </dev/null
+    '';
+  };
 
   meta = {
     homepage = "https://www.freecad.org";
