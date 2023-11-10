7aa1e441a5a0b451ab0ac9bbfbb563062676c2a5
Ed <contact@oberi.co.uk>
2023-11-10 21:10:18 +0000

    maintainers: add ekimber

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5034,6 +5034,12 @@
     name = "Edvin Källström";
     githubId = 84442052;
   };
+  ekimber  = {
+    email = "ekimber@protonmail.com";
+    github = "ekimber";
+    name = "Edward Kimber";
+    githubId = 99987;
+  };
   ekleog = {
     email = "leo@gaspard.io";
     matrix = "@leo:gaspard.ninja";
