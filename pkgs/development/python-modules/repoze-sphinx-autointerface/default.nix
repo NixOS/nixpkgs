b0de0f30d06cb197d9a0b84b68cc7bab09a1fa45
natsukium <tomoya.otabi@gmail.com>
2023-11-11 17:54:26 +0900

    python311Packages.repoze-sphinx-autointerface: refactor

--- pkgs/development/python-modules/repoze-sphinx-autointerface/default.nix
+++ pkgs/development/python-modules/repoze-sphinx-autointerface/default.nix
@@ -1,6 +1,8 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
+, setuptools
 , pytestCheckHook
 , zope_interface
 , zope_testrunner
@@ -10,13 +12,19 @@
 buildPythonPackage rec {
   pname = "repoze-sphinx-autointerface";
   version = "1.0.0";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     pname = "repoze.sphinx.autointerface";
     inherit version;
     hash = "sha256-SGvxQjpGlrkVPkiM750ybElv/Bbd6xSwyYh7RsYOKKE=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     zope_interface
@@ -28,9 +36,19 @@ buildPythonPackage rec {
     zope_testrunner
   ];
 
+  pythonImportsCheck = [
+    "repoze.sphinx.autointerface"
+  ];
+
+  pythonNamespaces = [
+    "repoze"
+    "repoze.sphinx"
+  ];
+
   meta = with lib; {
     homepage = "https://github.com/repoze/repoze.sphinx.autointerface";
     description = "Auto-generate Sphinx API docs from Zope interfaces";
+    changelog = "https://github.com/repoze/repoze.sphinx.autointerface/blob/${version}/CHANGES.rst";
     license = licenses.bsd0;
     maintainers = with maintainers; [ domenkozar ];
   };
