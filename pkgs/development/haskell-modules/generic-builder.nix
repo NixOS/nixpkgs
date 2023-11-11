b974b808a02737b698496434dea05f44a1990001
Randy Eckenrode <randy@largeandhighquality.com>
2023-11-11 10:43:15 -0500

    haskell-modules/generic-builder.nix: set `NIX_LDFLAGS` at the top level
    This is a follow-up PR to #266172 to address the feedback left by
    @sternenseemann regarding `env`.
    
    Rebuilds on staging-next #263535 should be limited if there are any.

--- pkgs/development/haskell-modules/generic-builder.nix
+++ pkgs/development/haskell-modules/generic-builder.nix
@@ -816,7 +816,7 @@ stdenv.mkDerivation ({
 # Works around https://github.com/NixOS/nixpkgs/issues/166205.
 # This can be dropped once a fix has been committed to cc-wrapper.
 // lib.optionalAttrs (stdenv.cc.isClang && stdenv.cc.libcxx != null) {
-  env.NIX_LDFLAGS = "-l${stdenv.cc.libcxx.cxxabi.libName}";
+  NIX_LDFLAGS = "-l${stdenv.cc.libcxx.cxxabi.libName}";
 }
 )
 )
