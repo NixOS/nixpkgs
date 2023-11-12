7f63ad2bdb8408419995784bb30ffde33bf7fef4
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-12 02:20:02 +0000

    python310Packages.types-pillow: 10.0.0.3 -> 10.1.0.2

--- pkgs/development/python-modules/types-pillow/default.nix
+++ pkgs/development/python-modules/types-pillow/default.nix
@@ -5,13 +5,13 @@
 
 buildPythonPackage rec {
   pname = "types-pillow";
-  version = "10.0.0.3";
+  version = "10.1.0.2";
   format = "setuptools";
 
   src = fetchPypi {
     inherit version;
     pname = "types-Pillow";
-    hash = "sha256-rgyHfTY9o0m7uCxUY8nngDcpDMB9NxTLDOr10vf1yCU=";
+    hash = "sha256-UlwcXuZ7CsFyHEDSvGGCJu8hI8NH5SfhTgW5IHIaE7k=";
   };
 
   # Modules doesn't have tests
