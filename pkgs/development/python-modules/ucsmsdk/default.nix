10641700467e929a8f2ab4e5d068730d0d0d6409
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-12 03:18:16 +0000

    python310Packages.ucsmsdk: 0.9.15 -> 0.9.16

--- pkgs/development/python-modules/ucsmsdk/default.nix
+++ pkgs/development/python-modules/ucsmsdk/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "ucsmsdk";
-  version = "0.9.15";
+  version = "0.9.16";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "CiscoUcs";
     repo = "ucsmsdk";
     rev = "refs/tags/v${version}";
-    hash = "sha256-xNRfsIfhoVI5ORWn6NmLCuMMJregIZWQ20QBiBsA1Pc=";
+    hash = "sha256-9ksHA8uvBv370/6Umt5iz/4F8VsDDI9X8kVc5Lv0RVk=";
   };
 
   propagatedBuildInputs = [
