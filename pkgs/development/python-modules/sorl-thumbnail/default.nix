eeb58d0b8fb43209a5518375bd341645b3bf93cd
natsukium <tomoya.otabi@gmail.com>
2023-11-12 15:11:01 +0900

    python311Packages.sorl-thumbnail: refactor

--- pkgs/development/python-modules/sorl-thumbnail/default.nix
+++ pkgs/development/python-modules/sorl-thumbnail/default.nix
@@ -1,28 +1,46 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, isPy27
+, pythonOlder
+, setuptools
 , setuptools-scm
+, django
 }:
 
 buildPythonPackage rec {
   pname = "sorl-thumbnail";
   version = "12.9.0";
-  disabled = isPy27;
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-DLwvUhUufyJm48LLSuXYOv0ulv1eHELlZnNiuqo9LbM=";
   };
 
-  nativeBuildInputs = [ setuptools-scm ];
+  nativeBuildInputs = [
+    setuptools
+    setuptools-scm
+  ];
+
+  buildInputs = [
+    django
+  ];
+
+  env.DJANGO_SETTINGS_MODULE = "sorl.thumbnail.conf.defaults";
+
   # Disabled due to an improper configuration error when tested against django. This looks like something broken in the test cases for sorl.
   doCheck = false;
+
+  pythonImportsCheck = [
+    "sorl.thumbnail"
+  ];
 
   meta = with lib; {
     homepage = "https://sorl-thumbnail.readthedocs.org/en/latest/";
     description = "Thumbnails for Django";
+    changelog = "https://github.com/jazzband/sorl-thumbnail/blob/${version}/CHANGES.rst";
     license = licenses.bsd3;
   };
-
 }
