36f326b4080e216ef8e0537d7d846f5b5c104a8c
Duncan Fairbanks <duncanfairbanks6@gmail.com>
2023-11-12 13:53:51 -0800

    maintainers: add bonsairobo
    Co-authored-by: Jussi Kuokkanen <44469719+Lurkki14@users.noreply.github.com>

--- pkgs/by-name/kt/ktx-tools/package.nix
+++ pkgs/by-name/kt/ktx-tools/package.nix
@@ -60,7 +60,7 @@ stdenv.mkDerivation rec {
     '';
     homepage = "https://github.com/KhronosGroup/KTX-Software";
     license = licenses.asl20;
-    maintainers = [ ];
+    maintainers = with maintainers; [ bonsairobo ];
     platforms = platforms.linux;
   };
 }
