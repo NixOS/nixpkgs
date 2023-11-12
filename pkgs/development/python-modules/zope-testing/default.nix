15e9135dd7ca912ee014344af13da1e7711cfe57
natsukium <tomoya.otabi@gmail.com>
2023-11-12 12:59:38 +0900

    python311Packages.zope-testing: refactor

--- pkgs/development/python-modules/zope-testing/default.nix
+++ pkgs/development/python-modules/zope-testing/default.nix
@@ -2,14 +2,14 @@
 , buildPythonPackage
 , fetchPypi
 , isPyPy
-, zope_interface
-, zope_exceptions
-, zope_location
+, setuptools
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
   pname = "zope-testing";
   version = "5.0.1";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "zope.testing";
@@ -17,15 +17,33 @@ buildPythonPackage rec {
     hash = "sha256-6HzQ2NZmVzza8TOBare5vuyAGmSoZZXBnLX+mS7z1kk=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   doCheck = !isPyPy;
 
-  propagatedBuildInputs = [ zope_interface zope_exceptions zope_location ];
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pytestFlagsArray = [
+    "src/zope/testing/tests.py"
+  ];
+
+  pythonImportsCheck = [
+    "zope.testing"
+  ];
+
+  pythonNamespaces = [
+    "zope"
+  ];
 
   meta = with lib; {
     description = "Zope testing helpers";
-    homepage =  "http://pypi.python.org/pypi/zope.testing";
-    license = licenses.zpl20;
+    homepage =  "https://github.com/zopefoundation/zope.testing";
+    changelog = "https://github.com/zopefoundation/zope.testing/blob/${version}/CHANGES.rst";
+    license = licenses.zpl21;
     maintainers = with maintainers; [ goibhniu ];
   };
-
 }
