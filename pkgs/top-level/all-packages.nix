656ad4af936f9c1a9aeb025ba4eec7ccc047b855
natsukium <tomoya.otabi@gmail.com>
2023-11-12 16:57:17 +0900

    grab-site: remove
    grab-site has been removed because it's unmaintained and broken

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21751,8 +21751,6 @@ with pkgs;
 
   gperftools = callPackage ../development/libraries/gperftools { };
 
-  grab-site = callPackage ../tools/backup/grab-site { };
-
   grilo = callPackage ../development/libraries/grilo { };
 
   grilo-plugins = callPackage ../development/libraries/grilo-plugins { };
