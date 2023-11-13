6bee2d0ac69d598dfe6614214fb4e69122513ee3
Elijah M. Immer <me@eimmer.me>
2023-11-13 08:47:06 +0000

    nixos/hyprland: move to programs/wayland
    Just moved to hyprland module to programs/wayland.
    This has no effect on the module side (still accessed the same way in
    the module `programs.hyprland`) just moved to be inline with other
    wayland compositors.
    
    Also edit the module list to reflect where the file is located.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -194,7 +194,6 @@
   ./programs/haguichi.nix
   ./programs/hamster.nix
   ./programs/htop.nix
-  ./programs/hyprland.nix
   ./programs/iay.nix
   ./programs/iftop.nix
   ./programs/i3lock.nix
@@ -271,6 +270,7 @@
   ./programs/virt-manager.nix
   ./programs/wavemon.nix
   ./programs/wayland/cardboard.nix
+  ./programs/wayland/hyprland.nix
   ./programs/wayland/river.nix
   ./programs/wayland/sway.nix
   ./programs/wayland/waybar.nix
