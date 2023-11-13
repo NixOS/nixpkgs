77ad3f4615fae8eadf8e9270e47c9a6670fda71b
toastal <toastal@posteo.net>
2023-11-14 02:08:41 +0700

    lunarml: 0.0.20230924 → 0.0.20231113

--- pkgs/development/compilers/lunarml/default.nix
+++ pkgs/development/compilers/lunarml/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "0.0.20230924";
+  version = "0.0.20231113";
 in
 stdenvNoCC.mkDerivation {
   inherit version;
@@ -17,7 +17,7 @@ stdenvNoCC.mkDerivation {
     owner = "minoki";
     repo = "LunarML";
     rev = "refs/tags/v${version}";
-    sha256 = "QN5iJEpJJZZuUfY/z57bpOQHDU31ecmJPWQtkXsLmDg=";
+    hash = "sha256-LmS+pkdCraY8sor+lsq/vCODFyVneKkZqjZqbJ1trb4=";
   };
 
   outputs = [ "out" "doc" ];
