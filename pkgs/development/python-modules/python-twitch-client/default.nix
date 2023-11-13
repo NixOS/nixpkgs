95b40d0ffdb292be563c3abc54cba4c68376abf3
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-13 12:02:09 +0100

    python311Packages.python-twitch-client: fix build

--- pkgs/development/python-modules/python-twitch-client/default.nix
+++ pkgs/development/python-modules/python-twitch-client/default.nix
@@ -5,32 +5,50 @@
 , pythonOlder
 , requests
 , responses
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "python-twitch-client";
   version = "0.7.1";
+  pyproject = true;
+
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "tsifrer";
-    repo = pname;
-    rev = version;
-    sha256 = "10wwkam3dw0nqr3v9xzigx1zjlrnrhzr7jvihddvzi84vjb6j443";
+    repo = "python-twitch-client";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-gxBpltwExb9bg3HLkz/MNlP5Q3/x97RHxhbwNqqanIM=";
   };
 
-  propagatedBuildInputs = [ requests ];
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    requests
+  ];
 
   nativeCheckInputs = [
     pytestCheckHook
     responses
   ];
 
-  pythonImportsCheck = [ "twitch" ];
+  pythonImportsCheck = [
+    "twitch"
+  ];
+
+  disabledTests = [
+    # Tests require network access
+    "test_delete_from_community"
+    "test_update"
+  ];
 
   meta = with lib; {
     description = "Python wrapper for the Twitch API";
     homepage = "https://github.com/tsifrer/python-twitch-client";
+    changelog = "https://github.com/tsifrer/python-twitch-client/blob/${version}/CHANGELOG.md";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
