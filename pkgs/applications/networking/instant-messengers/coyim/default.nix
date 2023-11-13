543c6a78abbaf974319ecbcd2c1af29052010516
Aaron Jheng <wentworth@outlook.com>
2023-11-13 04:15:45 +0000

    coyim: 0.4.1 -> 0.5.0

--- pkgs/applications/networking/instant-messengers/coyim/default.nix
+++ pkgs/applications/networking/instant-messengers/coyim/default.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, buildGoPackage
+, buildGoModule
 , fetchFromGitHub
 , pkg-config
 , cairo
@@ -9,30 +9,40 @@
 , gnome
 , wrapGAppsHook
 , gtk3
+, gtk-mac-integration
 }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "coyim";
-  version = "0.4.1";
-
-  goPackagePath = "github.com/coyim/coyim";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "coyim";
     repo = "coyim";
-    rev = "v${version}";
-    sha256 = "sha256-PmB6POaHKEXzIAaz3lAbUOhtVOzrj5oXRk90giYo6SI=";
+    # rev = "v${version}";
+    rev = "3f84daa8c27277543b1b4ad4536dde5100d9df12";
+    hash = "sha256-lzhcUSBuAgYwcmdwnqNxKG0P6ZSjWeLS/g/gaF171D4=";
   };
+
+  vendorHash = "sha256-zG7r/Db6XiwKoHRduGj3tEh/KT1hsuBoSGLYaZ+qO0Y=";
+
+  subPackages = [ "." ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.BuildTag=${version}"
+  ];
 
   nativeBuildInputs = [ pkg-config wrapGAppsHook ];
 
-  buildInputs = [ glib cairo gdk-pixbuf gtk3 gnome.adwaita-icon-theme ];
+  buildInputs = [ glib cairo gdk-pixbuf gtk3 gnome.adwaita-icon-theme ]
+    ++ lib.optionals stdenv.isDarwin [ gtk-mac-integration ];
 
   meta = with lib; {
     description = "a safe and secure chat client";
     homepage = "https://coy.im/";
     license = licenses.gpl3;
     platforms = [ "x86_64-linux" "x86_64-darwin" ];
-    broken = stdenv.isDarwin;
   };
 }
