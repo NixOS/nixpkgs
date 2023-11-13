bae7c1bd265f6d4d56f39553edac1d175e8c2121
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-13 08:59:29 +0100

    python311Packages.bc-python-hcl2: update disabled

--- pkgs/development/python-modules/bc-python-hcl2/default.nix
+++ pkgs/development/python-modules/bc-python-hcl2/default.nix
@@ -11,7 +11,7 @@ buildPythonPackage rec {
   version = "0.4.1";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
