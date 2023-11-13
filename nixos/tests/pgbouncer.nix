73198870cd8afe6f763b1d2e801b143d3015363e
Raito Bezarius <masterancpp@gmail.com>
2023-11-13 17:14:26 +0100

    nixos/tests/pgbouncer: do not use `ensureDBOwnership`
    pgbouncer test is special in the sense where it actually tries
    to connect via SCRAM SHA, let's avoid `ensureDBOwnership` here
    otherwise for some reason pgbouncer will try to look in pg_shadow
    for the authuser…

--- nixos/tests/pgbouncer.nix
+++ nixos/tests/pgbouncer.nix
@@ -17,18 +17,18 @@ in
 
       systemd.services.postgresql = {
         postStart = ''
-            ${pkgs.postgresql}/bin/psql -U postgres -c "ALTER ROLE testuser WITH LOGIN PASSWORD 'testpass'";
+          ${pkgs.postgresql}/bin/psql -U postgres -c "ALTER ROLE testuser WITH LOGIN PASSWORD 'testpass'";
+          ${pkgs.postgresql}/bin/psql -U postgres -c "ALTER DATABASE testdb OWNER TO testuser;";
         '';
       };
 
       services = {
         postgresql = {
           enable = true;
-          ensureDatabases = [ "test" ];
+          ensureDatabases = [ "testdb" ];
           ensureUsers = [
           {
-            name = "test";
-            ensureDBOwnership = true;
+            name = "testuser";
           }];
           authentication = ''
             local testdb testuser scram-sha-256
@@ -38,7 +38,7 @@ in
         pgbouncer = {
           enable = true;
           listenAddress = "localhost";
-          databases = { test = "host=/run/postgresql/ port=5432 auth_user=testuser dbname=test"; };
+          databases = { test = "host=/run/postgresql/ port=5432 auth_user=testuser dbname=testdb"; };
           authType = "scram-sha-256";
           authFile = testAuthFile;
         };
