cba69cc17dec46596c8de8e350d6839904b5802b
Anna Lee <150648636+a-n-n-a-l-e-e@users.noreply.github.com>
2023-11-13 23:15:59 -0800

    python311Packages.rmrl: add patch to fix xdg import failure
    import xdg -> import xdg_base_dirs
    https://github.com/naturale0/rmrl/pull/3/commits/6ec915514303d631257a8f9e13fe02a6a8d943a8

--- pkgs/development/python-modules/rmrl/default.nix
+++ pkgs/development/python-modules/rmrl/default.nix
@@ -2,6 +2,7 @@
 , buildPythonPackage
 , pythonOlder
 , fetchFromGitHub
+, fetchpatch
 , poetry-core
 , pdfrw
 , reportlab
@@ -13,16 +14,21 @@
 buildPythonPackage rec {
   pname = "rmrl";
   version = "unstable-2022-12-11";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
-
-  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "naturale0";
     repo = "rmrl";
     rev = "3c908536f11dfa92f81e7127ae76f18f0b2cc3e3";
     hash = "sha256-13pMfRe2McWDpBTlJy/TBT0W5wyd0EXDoocxeIzmqCo=";
+  };
+
+  patches = fetchpatch {
+    name = "use_xdg_base_dirs.patch";
+    url = "https://github.com/naturale0/rmrl/commit/6ec915514303d631257a8f9e13fe02a6a8d943a8.patch";
+    hash = "sha256-SYUbzYMwFcgDEOHYvPdHrbWSErUoxg4DSiMOunorGB4=";
   };
 
   nativeBuildInputs = [
