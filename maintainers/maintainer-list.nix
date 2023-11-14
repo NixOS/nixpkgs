f18e7c8f323f88e169734181e411555a9e29b880
Schweber <64630479+Schweber@users.noreply.github.com>
2023-11-14 04:10:50 +0000

    Update maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16113,6 +16113,11 @@
     githubId = 5104601;
     name = "schnusch";
   };
+  Schweber = {
+    github = "Schweber";
+    githubId = 64630479;
+    name = "Schweber";
+  };
   sciencentistguy = {
     email = "jamie@quigley.xyz";
     name = "Jamie Quigley";
