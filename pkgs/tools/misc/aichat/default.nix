97c970ac19410a4e1ccfc3b23480a31181f3101f
Pizmovc <pizmovc@kalu.blue>
2023-11-14 07:57:21 +0100

    aichat: Fix build on MacOS
    Build inputs were missing the AppKit AppleSDK Framework.
    
    For reference, the reported error when building the package was
    'note: ld: framework not found AppKit'.

--- pkgs/tools/misc/aichat/default.nix
+++ pkgs/tools/misc/aichat/default.nix
@@ -26,6 +26,7 @@ rustPlatform.buildRustPackage rec {
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreFoundation
     darwin.apple_sdk.frameworks.Security
+    darwin.apple_sdk.frameworks.AppKit
   ];
 
   meta = with lib; {
