76652adb6d520ec4257fb29935a7cc277fa9ffa3
James ‘Twey’ Kay <twey@twey.co.uk>
2023-11-14 17:18:34 +0000

    Apply review suggestions from @OPNA2608.

--- pkgs/applications/networking/ouroboros/default.nix
+++ pkgs/applications/networking/ouroboros/default.nix
@@ -19,24 +19,27 @@ stdenv.mkDerivation rec {
     rev = version;
     hash = "sha256-r5IyWEdNDfrJnW8Th+pWvGVFHsxEp91IQomesLNADJI=";
   };
-  buildInputs = [
+  nativeBuildInputs = [
     cmake
+    pkg-config
+  ];
+  buildInputs = [
+    protobufc
     dnsutils
     fuse
     libgcrypt
     openssl
-    pkg-config
     protobuf
-    protobufc
     systemd
   ];
+  strictDeps = true;
   cmakeFlags = [ "-DSYSTEMD_UNITDIR=${placeholder "out"}/etc/systemd/system" ];
   meta = {
     description = "A future network prototype descended from RINA";
     homepage = "https://ouroboros.rocks/";
     license = with lib.licenses; [
-      lgpl21 # library
-      gpl2 # daemons
+      lgpl21Only # library
+      gpl2Only # daemons
       bsd3 # tools
     ];
     maintainers = with lib.maintainers; [ twey ];
