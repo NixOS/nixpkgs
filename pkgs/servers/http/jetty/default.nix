4d77d2a63f18b622df8d15ba529761a3c92d62d5
Anthony Roussel <anthony@roussel.dev>
2023-11-14 23:10:27 +0100

    jetty: 12.0.2 -> 12.0.3

--- pkgs/servers/http/jetty/default.nix
+++ pkgs/servers/http/jetty/default.nix
@@ -2,10 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "jetty";
-  version = "12.0.2";
+  version = "12.0.3";
+
   src = fetchurl {
     url = "mirror://maven/org/eclipse/jetty/jetty-home/${version}/jetty-home-${version}.tar.gz";
-    hash = "sha256-DtlHTXjbr31RmK6ycDdiWOL7jIpbWNh0la90OnOhzvM=";
+    hash = "sha256-Z/jJKKzoqTPZnoFOMwbpSd/Kd1w+rXloKH+aw6aNrKs=";
   };
 
   dontBuild = true;
