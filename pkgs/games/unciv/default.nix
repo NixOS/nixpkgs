5322c8c4373359705f3651e8c914dfc4bc672090
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-15 05:21:58 +0000

    unciv: 4.8.13 -> 4.8.16

--- pkgs/games/unciv/default.nix
+++ pkgs/games/unciv/default.nix
@@ -25,11 +25,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "unciv";
-  version = "4.8.13";
+  version = "4.8.16";
 
   src = fetchurl {
     url = "https://github.com/yairm210/Unciv/releases/download/${version}/Unciv.jar";
-    hash = "sha256-16TpsKNLcm6lbi4exYxDZWfmRsvfAhT1ktP36zC9Psg=";
+    hash = "sha256-hALssS+RzWoBJCtli5A2gS00zt0ABgphoLOC7sfE9tQ=";
   };
 
   dontUnpack = true;
