549c51d541b78e0ed87f7f12e75114b6fe3554bc
Bobby Rong <rjl931189261@126.com>
2023-11-15 12:59:47 +0000

    gnome.ghex: 45.0 → 45.1
    https://gitlab.gnome.org/GNOME/ghex/-/compare/45.0...45.1
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
    Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/desktops/gnome/apps/ghex/default.nix
+++ pkgs/desktops/gnome/apps/ghex/default.nix
@@ -22,13 +22,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ghex";
-  version = "45.0";
+  version = "45.1";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/ghex/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "Bc7MRWHKQLJXxdsx2p9o1pYTPvwK5CftgvuYWphuhA4=";
+    sha256 = "+ysII80WJJ7b6u6DAvm9UAXgFQNos18eR8JmgMrKwvo=";
   };
 
   nativeBuildInputs = [
