7e47efc67d5a6f19fdb3cccdd6fb7db49f487432
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-15 18:13:15 +0000

    ruler: 2.4.1 -> 2.5.0

--- pkgs/tools/security/ruler/default.nix
+++ pkgs/tools/security/ruler/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "ruler";
-  version = "2.4.1";
+  version = "2.5.0";
 
   src = fetchFromGitHub {
     owner = "sensepost";
     repo = pname;
     rev = version;
-    hash = "sha256-cEYpK1LB9b65xr6MCMax1vUtSWefjJdXNs4sPgx65d0=";
+    hash = "sha256-m41U24OoUV1ubxvZBfaANGs58o+4q7XveR+Jq38/swQ=";
   };
 
-  vendorHash = "sha256-ITd3cvZmRBWK3922dDRvNHNH8KzHoVfIQI6S318ibxA=";
+  vendorHash = "sha256-z9SKDpkDFsDVMvyaTAtB8tTcXsgVYU+Ql4QlyfeRhZM=";
 
   meta = with lib; {
     description = "Tool to abuse Exchange services";
