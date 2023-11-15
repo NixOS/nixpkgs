bd07ae09b08124dad13c2eef6d400834df83ffca
Chen <i@cuichen.cc>
2023-11-16 01:52:16 +0800

    certbot: 2.6.0 -> 2.7.4

--- pkgs/development/python-modules/certbot/default.nix
+++ pkgs/development/python-modules/certbot/default.nix
@@ -27,13 +27,13 @@
 
 buildPythonPackage rec {
   pname = "certbot";
-  version = "2.6.0";
+  version = "2.7.4";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-CbYamk1+NvcyS6w7pOjpD5lqWHCk+VJqF9R3xYxzpQk=";
+    hash = "sha256-BZ7JqAciwbmkpbzR/qZHAraLJWWXNRN3Er4XvfU5kYs=";
   };
 
   sourceRoot = "${src.name}/${pname}";
