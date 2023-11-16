7602c7e47a176541fe7352a914a4b7fdba4b4a99
figsoda <figsoda@pm.me>
2023-11-15 22:47:36 -0500

    stylua: 0.19.0 -> 0.19.1
    Diff: https://github.com/johnnymorganz/stylua/compare/v0.19.0...v0.19.1
    
    Changelog: https://github.com/johnnymorganz/stylua/blob/v0.19.1/CHANGELOG.md

--- pkgs/development/tools/stylua/default.nix
+++ pkgs/development/tools/stylua/default.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "stylua";
-  version = "0.19.0";
+  version = "0.19.1";
 
   src = fetchFromGitHub {
     owner = "johnnymorganz";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-Lfd6jULV64vP5GQtOQ/wqyu4zZNpU83HSVGXQ8AtnvQ=";
+    sha256 = "sha256-Bw9+BUvZC4sRLFyU5K6FHsWVZ3PTlMAonRmTSwV5AwU=";
   };
 
-  cargoSha256 = "sha256-rojb15M58TnGZfptTMVCC6XaI9RBlpVL7s/Mb18CaSM=";
+  cargoSha256 = "sha256-bainEM4eRMNyecSBi5k1ifjDULWdXed6GP6i10IVA+w=";
 
   # remove cargo config so it can find the linker on aarch64-unknown-linux-gnu
   postPatch = ''
