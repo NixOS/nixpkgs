b9d223743176531c738b1b33755816b90f7d0028
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-17 20:46:20 +0100

    python311Packages.msal: 1.24.1 -> 1.25.0
    Changelog: https://github.com/AzureAD/microsoft-authentication-library-for-python/releases/tag/1.25.0

--- pkgs/development/python-modules/msal/default.nix
+++ pkgs/development/python-modules/msal/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "msal";
-  version = "1.24.1";
+  version = "1.25.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-qglyiEs8b97FPZoL0VwS5b17cawbZtdG9U0Shwnz+Pg=";
+    hash = "sha256-9EMp/bWfTwRMd5Fko0R0uKRK2eSUCvvEw6Oiu+kDJNk=";
   };
 
   propagatedBuildInputs = [
