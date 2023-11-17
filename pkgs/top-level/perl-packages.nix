02fe1eba10f2a468c158509d8e04fe4fd54a668c
Stig Palmquist <git@stig.io>
2023-11-17 20:17:49 +0100

    perlPackages.GetoptLong: 2.54 -> 2.57

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -10703,10 +10703,10 @@ with self; {
 
   GetoptLong = buildPerlPackage {
     pname = "Getopt-Long";
-    version = "2.54";
+    version = "2.57";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/J/JV/JV/Getopt-Long-2.54.tar.gz";
-      hash = "sha256-WEujyZuy1rNBN1IS+bh0YT9wbPsBzuIbiiZ2qYq5hf4=";
+      url = "mirror://cpan/authors/id/J/JV/JV/Getopt-Long-2.57.tar.gz";
+      hash = "sha256-03kea/FncINk6lrTvleNyRc6AHYWcWCkNBwFoel5eV4=";
     };
     meta = {
       description = "Extended processing of command line options";
