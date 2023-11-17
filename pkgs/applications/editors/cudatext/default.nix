20c5140b2dd16a25b0fd9d9c9eed0ac0322e6ad3
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-17 08:25:30 +0000

    cudatext-gtk: 1.201.0 -> 1.201.0.2

--- pkgs/applications/editors/cudatext/default.nix
+++ pkgs/applications/editors/cudatext/default.nix
@@ -38,13 +38,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "cudatext";
-  version = "1.201.0";
+  version = "1.201.0.2";
 
   src = fetchFromGitHub {
     owner = "Alexey-T";
     repo = "CudaText";
     rev = version;
-    hash = "sha256-Do2JPNZtoi7zbUnJomQAZ8zR/WPB6+G051xZWmeUBP4=";
+    hash = "sha256-ke7i0eeZHEsVhtzaS0AeLQOrYE1F+ppCwjR2TWeJQPA=";
   };
 
   postPatch = ''
