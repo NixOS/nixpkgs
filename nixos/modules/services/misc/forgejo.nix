b3e8dae766a3db1f0845a598f71e1267292a470a
emilylange <git@emilylange.de>
2023-11-17 19:52:31 +0100

    nixos/forgejo: remove `postgresql_15` permission work-around
    This is no longer necessary as of
    ccfe07c3168109567b98462533f7ddf14c7ba18d.
    
    Previously: b8585a119ce5c28754267f349107304117041083.

--- nixos/modules/services/misc/forgejo.nix
+++ nixos/modules/services/misc/forgejo.nix
@@ -436,17 +436,6 @@ in
       ];
     };
 
-    # Work around 'pq: permission denied for schema public' with postgres v15, until a
-    # solution for `services.postgresql.ensureUsers` is found.
-    # See https://github.com/NixOS/nixpkgs/issues/216989
-    systemd.services.postgresql.postStart = lib.mkIf (
-      usePostgresql
-      && cfg.database.createDatabase
-      && lib.strings.versionAtLeast config.services.postgresql.package.version "15.0"
-    ) (lib.mkAfter ''
-      $PSQL -tAc 'ALTER DATABASE "${cfg.database.name}" OWNER TO "${cfg.database.user}";'
-    '');
-
     services.mysql = optionalAttrs (useMysql && cfg.database.createDatabase) {
       enable = mkDefault true;
       package = mkDefault pkgs.mariadb;
