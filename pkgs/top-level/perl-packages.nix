111837fd4fdd1f075a344153c5184f21091058ac
Stig Palmquist <git@stig.io>
2023-11-17 20:17:49 +0100

    perlPackages.FutureIO: 0.14 -> 0.15

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -10543,10 +10543,10 @@ with self; {
 
   FutureIO = buildPerlModule {
     pname = "Future-IO";
-    version = "0.14";
+    version = "0.15";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/P/PE/PEVANS/Future-IO-0.14.tar.gz";
-      hash = "sha256-a1j++vwwlMJwHwp7mMsUCwmItRaKfV3069Hu6OhyBgo=";
+      url = "mirror://cpan/authors/id/P/PE/PEVANS/Future-IO-0.15.tar.gz";
+      hash = "sha256-Sk4clkrRw99gI3aHxRcKR5v+9bAweuAsY5EJb7Rp378=";
     };
     buildInputs = [ TestFutureIOImpl ];
     propagatedBuildInputs = [ Future StructDumb ];
