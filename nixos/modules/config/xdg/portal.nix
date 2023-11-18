9b4a8464096fde24a37c3e2aa4373961d87a45fd
Bobby Rong <rjl931189261@126.com>
2023-11-18 20:28:48 +0800

    nixos/xdg/portal: Fix link to portals.conf documentation
    The original url is now 404.
    Let's keep this link in sync with the one in doc/manpage-urls.json.

--- nixos/modules/config/xdg/portal.nix
+++ nixos/modules/config/xdg/portal.nix
@@ -139,7 +139,7 @@ in
         should either set `xdg.portal.config` or `xdg.portal.configPackages`
         to specify which portal backend to use for the requested interface.
 
-        https://github.com/flatpak/xdg-desktop-portal/blob/main/doc/portals-conf.rst
+        https://github.com/flatpak/xdg-desktop-portal/blob/1.18.1/doc/portals.conf.rst.in
 
         If you simply want to keep the behaviour in < 1.17, which uses the first
         portal implementation found in lexicographical order, use the following:
