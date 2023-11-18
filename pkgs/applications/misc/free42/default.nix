02539e5c865c80ec9457c973bcadb2c8a677bcfe
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-18 03:19:49 +0000

    free42: 3.0.21 -> 3.1

--- pkgs/applications/misc/free42/default.nix
+++ pkgs/applications/misc/free42/default.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "free42";
-  version = "3.0.21";
+  version = "3.1";
 
   src = fetchFromGitHub {
     owner = "thomasokken";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-zRO0buYfKtybUisWZJRkvLJVLJYZwLcDnT04rnQWy+s=";
+    hash = "sha256-v3nZMjV9KnoTefeu2jl3k1B7efnJnNVOAfDVLyce6QI=";
   };
 
   nativeBuildInputs = [
