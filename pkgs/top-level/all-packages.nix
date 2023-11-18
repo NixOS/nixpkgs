9a5c5f6b9dd6eebad76f1652197a24a7c2eba1c6
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-11-19 10:00:31 +1100

    norouter: unbreak, pin go

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33926,7 +33926,11 @@ with pkgs;
 
   normalize = callPackage ../applications/audio/normalize { };
 
-  norouter = callPackage ../tools/networking/norouter { };
+  norouter = callPackage ../tools/networking/norouter {
+    # doesn't build with go 1.21
+    # https://github.com/norouter/norouter/issues/165
+    buildGoModule = buildGo120Module;
+  };
 
   nqptp = callPackage ../tools/networking/nqptp { };
 
