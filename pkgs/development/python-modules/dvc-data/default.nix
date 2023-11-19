29555caa8b3bf672dadd206672c9f84edc06f8a3
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-19 08:54:24 +0100

    python311Packages.dvc-data: 2.21.2 -> 2.22.0
    Diff: https://github.com/iterative/dvc-data/compare/refs/tags/2.21.2...2.22.0
    
    Changelog: https://github.com/iterative/dvc-data/releases/tag/2.22.0

--- pkgs/development/python-modules/dvc-data/default.nix
+++ pkgs/development/python-modules/dvc-data/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "dvc-data";
-  version = "2.21.2";
+  version = "2.22.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "iterative";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-2RLf2HBkb/vwbr9JecQQfO68ifVgcBIwmRQkXGJh0Fs=";
+    hash = "sha256-7T6aR1qCJm7m0AmASyJZpZtc+yXqULKc9mMcY2WYT6I=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
