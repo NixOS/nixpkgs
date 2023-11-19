3ce9db808b5bf6cf7e7c9c64d20c17620916c7e7
Robert Hensing <robert@roberthensing.nl>
2023-11-19 12:24:10 +0100

    changelog-d: Move out tests.basic

--- pkgs/by-name/ch/changelog-d/package.nix
+++ pkgs/by-name/ch/changelog-d/package.nix
@@ -1,9 +1,9 @@
 {
   cabal2nix,
+  callPackage,
   lib,
   haskell,
   haskellPackages,
-  runCommand,
   writeShellApplication,
 }:
 
@@ -27,29 +27,7 @@ let
       '';
     });
     passthru.tests = {
-      basic = runCommand "changelog-d-basic-test" {
-          nativeBuildInputs = [ finalAttrs.finalPackage ];
-        } ''
-          mkdir changelogs
-          cat > changelogs/config <<EOF
-          organization: NixOS
-          repository: boondoggle
-          EOF
-          cat > changelogs/a <<EOF
-          synopsis: Support numbers with incrementing base-10 digits
-          issues: #1234
-          description: {
-          This didn't work before.
-          }
-          EOF
-          changelog-d changelogs >$out
-          cat -n $out
-          echo Checking the generated output
-          set -x
-          grep -F 'Support numbers with incrementing base-10 digits' $out >/dev/null
-          grep -F 'https://github.com/NixOS/boondoggle/issues/1234' $out >/dev/null
-          set +x
-        '';
+      basic = callPackage ./tests/basic.nix { changelog-d = finalAttrs.finalPackage; };
     };
 
     meta = oldAttrs.meta // {
