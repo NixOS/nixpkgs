7b1d46017ddd3698cbddb8cfff769e53a54b2792
Otavio Salvador <otavio@ossystems.com.br>
2023-11-19 16:26:22 -0300

    python3Packages.lpc-checksum: init at 3.0.0
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6473,6 +6473,8 @@ self: super: with self; {
 
   losant-rest = callPackage ../development/python-modules/losant-rest { };
 
+  lpc-checksum = callPackage ../development/python-modules/lpc-checksum { };
+
   lrcalc-python = callPackage ../development/python-modules/lrcalc-python { };
 
   lru-dict = callPackage ../development/python-modules/lru-dict { };
