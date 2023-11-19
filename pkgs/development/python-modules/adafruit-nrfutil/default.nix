25b859ae0eaaf1b8a36a2511713ffae4ae511fb8
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-11-19 20:35:42 +0100

    python311Packages.adafruit-nrfutil: 1 -> 0.5.3.post17
    Fixes the build and applies various updates:
    
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post17
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post16
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post15
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post14
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post13
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/%24(APPVEYOR_REPO_TAG_NAME)
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post10
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post9
    https://github.com/adafruit/Adafruit_nRF52_nrfutil/releases/tag/0.5.3.post8

--- pkgs/development/python-modules/adafruit-nrfutil/default.nix
+++ pkgs/development/python-modules/adafruit-nrfutil/default.nix
@@ -1,43 +1,40 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, fetchpatch
-, pythonOlder
+
+# build-system
+, setuptools
+
+# dependencies
 , pyserial
 , click
 , ecdsa
+
+# tests
 , behave
 , nose
 }:
 
 buildPythonPackage rec {
   pname = "adafruit-nrfutil";
-  version = "1";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.7";
+  version = "0.5.3.post17";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "adafruit";
     repo = "Adafruit_nRF52_nrfutil";
-    rev = "refs/tags/appveyor-test-release-${version}";
-    hash = "sha256-wsspDg8XwEtJwJye6Z3TXaIN1TcfI7gYDah3L/xiiLo=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-mHHKOQE9AGBX8RAyaPOy+JS3fTs98+AFdq9qsVy7go4=";
   };
 
-  patches = [
-    # Pull a patch which fixes the tests, but is not yet released in a new version:
-    # https://github.com/adafruit/Adafruit_nRF52_nrfutil/pull/38
-    (fetchpatch {
-      name = "fix-tests.patch";
-      url = "https://github.com/adafruit/Adafruit_nRF52_nrfutil/commit/e5fbcc8ee5958041db38c04139ba686bf7d1b845.patch";
-      hash = "sha256-0tbJldGtYcDdUzA3wZRv0lenXVn6dqV016U9nMpQ6/w=";
-    })
+  nativeBuildInputs = [
+    setuptools
   ];
 
   propagatedBuildInputs = [
-    pyserial
     click
     ecdsa
+    pyserial
   ];
 
   nativeCheckInputs = [
