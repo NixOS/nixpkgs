d94bc90fe444707b0640104d481c2ea264e2ea20
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-20 19:52:08 +0100

    python311Packages.strawberry-graphql: 0.214.0 -> 0.215.1
    Diff: https://github.com/strawberry-graphql/strawberry/compare/refs/tags/0.214.0...0.215.1
    
    Changelog: https://github.com/strawberry-graphql/strawberry/blob/0.215.1/CHANGELOG.md

--- pkgs/development/python-modules/strawberry-graphql/default.nix
+++ pkgs/development/python-modules/strawberry-graphql/default.nix
@@ -42,7 +42,7 @@
 
 buildPythonPackage rec {
   pname = "strawberry-graphql";
-  version = "0.214.0";
+  version = "0.215.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -51,7 +51,7 @@ buildPythonPackage rec {
     owner = "strawberry-graphql";
     repo = "strawberry";
     rev = "refs/tags/${version}";
-    hash = "sha256-5mPVZz/KmrRytGoW5h1vG8pt8+N+jFk2Q22ih+sjcjM=";
+    hash = "sha256-7jWG9bk7NN3BhpzS2fi7OkAsxL0446hnqiNqhwiBGHc=";
   };
 
   patches = [
