0e7469d16ee2e2e494bca516ac25a2d4f68970d5
Thomas Gerbet <thomas@gerbet.me>
2023-11-21 19:48:36 +0100

    sng: use up-to-date libpng
    sng 1.1.0 is compatible with more recent version of libpng.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13349,9 +13349,7 @@ with pkgs;
 
   snet = callPackage ../tools/networking/snet { };
 
-  sng = callPackage ../tools/graphics/sng {
-    libpng = libpng12;
-  };
+  sng = callPackage ../tools/graphics/sng { };
 
   snmpcheck = callPackage ../tools/networking/snmpcheck { };
 
