2378029bb3c4c98fa00255e18920fdded7d1fb3e
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-21 09:18:47 +0100

    python311Packages.bimmer-connected: 0.14.2 -> 0.14.3
    Diff: https://github.com/bimmerconnected/bimmer_connected/compare/refs/tags/0.14.2...0.14.3
    
    Changelog: https://github.com/bimmerconnected/bimmer_connected/releases/tag/0.14.3

--- pkgs/development/python-modules/bimmer-connected/default.nix
+++ pkgs/development/python-modules/bimmer-connected/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "bimmer-connected";
-  version = "0.14.2";
+  version = "0.14.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "bimmerconnected";
     repo = "bimmer_connected";
     rev = "refs/tags/${version}";
-    hash = "sha256-69H0hB+yVmyzJ5A2Cb7ZcaaoRzMt618U+TUHYQ03/cY=";
+    hash = "sha256-I/MKjdEu69uis5f/Xuk4H6ynC71IQmfASLr07PdEBXE=";
   };
 
   nativeBuildInputs = [
