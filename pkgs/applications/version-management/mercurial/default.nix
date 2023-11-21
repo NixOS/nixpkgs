5a8471da3ab9c08e8950714939f6b84cdd5e6b87
pacien <pacien.trangirard@pacien.net>
2023-11-21 19:19:57 +0100

    mercurial: 6.5.3 -> 6.6
    Release notes: https://wiki.mercurial-scm.org/Release6.6

--- pkgs/applications/version-management/mercurial/default.nix
+++ pkgs/applications/version-management/mercurial/default.nix
@@ -21,11 +21,11 @@ let
 
   self = python3Packages.buildPythonApplication rec {
     pname = "mercurial${lib.optionalString fullBuild "-full"}";
-    version = "6.5.3";
+    version = "6.6";
 
     src = fetchurl {
       url = "https://mercurial-scm.org/release/mercurial-${version}.tar.gz";
-      sha256 = "sha256-LNyB+t4SnPVrEoQXUn8ZC6cv13ZWc5TOVO7XZOZn59U=";
+      sha256 = "sha256-a/1xy6DfOxjeQkIWsw4qVBzKbgEEhT0zNL6AoqsJpK0=";
     };
 
     format = "other";
@@ -35,7 +35,7 @@ let
     cargoDeps = if rustSupport then rustPlatform.fetchCargoTarball {
       inherit src;
       name = "mercurial-${version}";
-      sha256 = "sha256-ob81zMUY4AVNIbkFKyImnj7QhHTh7LVOCcGeZDtTAXc=";
+      sha256 = "sha256-mqWb0X44rhZscyN9mmLMqUjc0wpJt42MHtQRHtYDhBs=";
       sourceRoot = "mercurial-${version}/rust";
     } else null;
     cargoRoot = if rustSupport then "rust" else null;
