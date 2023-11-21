10dad1503839fe57b52fed08ad8e326370378ccc
chayleaf <chayleaf-git@pavluk.org>
2023-11-21 12:26:15 +0700

    cutter: fix build; switch from qt5 to qt6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19853,7 +19853,7 @@ with pkgs;
 
   rizinPlugins = recurseIntoAttrs rizin.plugins;
 
-  cutter = libsForQt5.callPackage ../development/tools/analysis/rizin/cutter.nix { };
+  cutter = qt6.callPackage ../development/tools/analysis/rizin/cutter.nix { };
 
   cutterPlugins = recurseIntoAttrs cutter.plugins;
 
