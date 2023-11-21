78079efb8d5f2185fafb181b3180fb6a92bd1dcb
x0ba <dax@omg.lol>
2023-11-21 10:21:15 -0800

    maintainers: add x0ba

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -19506,6 +19506,12 @@
     githubId = 3992240;
     name = "Elijah Rum";
   };
+  x0ba = {
+    name = "x0ba";
+    email = "dax@omg.lol";
+    github = "x0ba";
+    githubId = 64868985;
+  };
   x3ro = {
     name = "^x3ro";
     email = "nix@x3ro.dev";
