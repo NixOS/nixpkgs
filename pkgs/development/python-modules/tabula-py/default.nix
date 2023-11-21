c6a800cab78b42f6c56afb9c56c6d9f45f7d1717
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-21 12:03:13 +0000

    python311Packages.tabula-py: 2.8.2 -> 2.9.0

--- pkgs/development/python-modules/tabula-py/default.nix
+++ pkgs/development/python-modules/tabula-py/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "tabula-py";
-  version = "2.8.2";
+  version = "2.9.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "chezou";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-Zrq1i+HYXXNulyZ/fv00AgVd7ODj3rP9orLq5rT3ERU=";
+    hash = "sha256-MGv2n8DoSjumD3lRcqwI0sEsaEDgs1n+st8DwZuZauo=";
   };
 
   postPatch = ''
