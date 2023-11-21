b5aefaee8bb61f110b3aea2b34a3f19181db93b1
musjj <72612857+musjj@users.noreply.github.com>
2023-11-21 19:51:31 +0700

    gb-studio: init at 3.1.0
    closes: #185564

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3846,6 +3846,8 @@ with pkgs;
 
   gams = callPackage ../tools/misc/gams (config.gams or {});
 
+  gb-studio = callPackage ../development/tools/gb-studio { };
+
   gem = callPackage ../applications/audio/pd-plugins/gem { };
 
   github-changelog-generator = callPackage ../development/tools/github-changelog-generator { };
