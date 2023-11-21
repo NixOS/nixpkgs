12f6da6fa8b6f4d54fd4ffd8e67e4f29027e9468
Brian McGillion <bmg.avoin@gmail.com>
2023-11-21 08:36:00 +0400

    mdbook-footnote: init at 0.1.1
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>
    (cherry picked from commit b16e02b405d635bbb15629b874a57549f2eeeb94)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9446,6 +9446,10 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) CoreServices;
   };
 
+  mdbook-footnote = callPackage ../tools/text/mdbook-footnote {
+    inherit (darwin.apple_sdk.frameworks) CoreServices;
+  };
+
   mdcat = callPackage ../tools/text/mdcat {
     inherit (darwin.apple_sdk.frameworks) Security;
     inherit (python3Packages) ansi2html;
