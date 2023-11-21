5d94a9c49a48d8d937dc44dc779100eb6568360f
Michael Raitza <spacefrogg-devel@meterriblecrew.net>
2023-11-21 14:44:40 +0100

    goredo: 2.3.0 -> 2.4.0

--- pkgs/by-name/go/goredo/package.nix
+++ pkgs/by-name/go/goredo/package.nix
@@ -9,11 +9,11 @@
 
 buildGoModule rec {
   pname = "goredo";
-  version = "2.3.0";
+  version = "2.4.0";
 
   src = fetchurl {
     url = "http://www.goredo.cypherpunks.ru/download/${pname}-${version}.tar.zst";
-    hash = "sha256-i43ivJ2DOSebh7qRpYWTfTyQo/PFtzSXJciYbOy8e4s=";
+    hash = "sha256-oUC/N6NLEVBrFC3tSEsWEXUBl5oyZNmqRTFWFbgL+zg=";
   };
 
   patches = [ ./fix-tests.diff ];
