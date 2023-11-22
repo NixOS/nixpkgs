9b8808af31202c33ed35349ebdef08ed65ad84d9
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-22 16:24:44 +0100

    python311Packages.pyatag: 3.5.1 -> 0.3.7.1
    Diff: https://github.com/MatsNl/pyatag/compare/refs/tags/3.5.1...0.3.7.1
    
    Changelog: https://github.com/MatsNl/pyatag/releases/tag/0.3.7.1

--- pkgs/development/python-modules/pyatag/default.nix
+++ pkgs/development/python-modules/pyatag/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "pyatag";
-  version = "3.5.1";
+  version = "0.3.7.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     owner = "MatsNl";
     repo = "pyatag";
     rev = "refs/tags/${version}";
-    hash = "sha256-hyGos0LFVKv63jf1ODPFfk+R47oyHea+8MGvxeKpop8=";
+    hash = "sha256-3h9mpopTbEULCx7rcEt/I/ZnUA0L/fJ7Y3L5h/6EuC4=";
   };
 
   propagatedBuildInputs = [
