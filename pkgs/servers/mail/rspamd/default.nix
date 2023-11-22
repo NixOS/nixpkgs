3cf5bb87d69b209685ff42dbaccccdb822042b98
chayleaf <chayleaf-git@pavluk.org>
2023-11-22 13:44:23 +0700

    rspamd: fix build on non-x86_64 platforms
    (cherry picked from commit c6e4eb5097f9b9a699a66e86e4d6c75d8d83ed35)

--- pkgs/servers/mail/rspamd/default.nix
+++ pkgs/servers/mail/rspamd/default.nix
@@ -37,6 +37,14 @@ stdenv.mkDerivation rec {
     hash = "sha256-Bg0EFgxk/sRwE8/7a/m8J4cTgooR4fobQil8pbWtkoc=";
   };
 
+  patches = [
+    (fetchpatch2 {
+      name = "no-hyperscan-fix.patch";
+      url = "https://github.com/rspamd/rspamd/commit/d907a95ac2e2cad6f7f65c4323f031f7931ae18b.patch";
+      hash = "sha256-bMmgiJSy0QrzvBAComzT0aM8UF8OKeV0VgMr0wwrM6w=";
+    })
+  ];
+
   hardeningEnable = [ "pie" ];
 
   nativeBuildInputs = [ cmake pkg-config perl ];
