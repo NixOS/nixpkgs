c6437f51efefdd12eccadb9985d5024a91452df5
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-22 06:13:02 +0000

    python311Packages.odp-amsterdam: 5.3.1 -> 6.0.0

--- pkgs/development/python-modules/odp-amsterdam/default.nix
+++ pkgs/development/python-modules/odp-amsterdam/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "odp-amsterdam";
-  version = "5.3.1";
+  version = "6.0.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "klaasnicolaas";
     repo = "python-odp-amsterdam";
     rev = "refs/tags/v${version}";
-    hash = "sha256-HesAg6hJ8Al/ZZRBTXZM0EVv1kjYmmA66W+crwtWhf4=";
+    hash = "sha256-dAyKypc8bMWkXhYa7BlGGAGqPaPJHFHwXd/UK80BGoE=";
   };
 
   postPatch = ''
