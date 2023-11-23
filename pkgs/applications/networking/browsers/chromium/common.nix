043dc7990f236350f50ca4883dfac17f0adf2c27
Lily Foster <lily@lily.flowers>
2023-11-22 19:52:30 -0500

    chromium: add rpath to libGLESv2.so from libANGLE
    (cherry picked from commit 1a7856976554fcdc27027c02bfd4b2c626d563be)

--- pkgs/applications/networking/browsers/chromium/common.nix
+++ pkgs/applications/networking/browsers/chromium/common.nix
@@ -489,11 +489,11 @@ let
     '';
 
     postFixup = ''
-      # Make sure that libGLESv2 and libvulkan are found by dlopen.
+      # Make sure that libGLESv2 and libvulkan are found by dlopen in both chromium binary and ANGLE libGLESv2.so.
       # libpci (from pciutils) is needed by dlopen in angle/src/gpu_info_util/SystemInfo_libpci.cpp
-      chromiumBinary="$libExecPath/$packageName"
-      origRpath="$(patchelf --print-rpath "$chromiumBinary")"
-      patchelf --set-rpath "${lib.makeLibraryPath [ libGL vulkan-loader pciutils ]}:$origRpath" "$chromiumBinary"
+      for chromiumBinary in "$libExecPath/$packageName" "$libExecPath/libGLESv2.so"; do
+        patchelf --set-rpath "${lib.makeLibraryPath [ libGL vulkan-loader pciutils ]}:$(patchelf --print-rpath "$chromiumBinary")" "$chromiumBinary"
+      done
     '';
 
     passthru = {
