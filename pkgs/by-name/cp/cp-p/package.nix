d85a01103e574d0e3b3f860a58080d731577363a
deadendpl <123810408+deadendpl@users.noreply.github.com>
2023-11-23 16:45:49 +0100

    Fixing whitespace in cp-p

--- pkgs/by-name/cp/cp-p/package.nix
+++ pkgs/by-name/cp/cp-p/package.nix
@@ -10,10 +10,10 @@ stdenv.mkDerivation {
     owner = "Naheel-Azawy";
     repo = "cp-p";
     rev = "2e97ba534a5892c47a0317a038b19bcda221e5e6";
-    sha256 = "1ki8bp33d9xf4jinr0isvkxq3i86xi1kkv2f5x54as6i0yz9w7iq";
+    hash = "sha256-OB6evgfRaEVKL07sOUPsBsWB+9w6gmyjJK6nNsZdKM4=";
   };
-  
-  makeFlags = [ "PREFIX=$(out)" "BINPREFIX=$(PREFIX)/bin" ]; 
+
+  makeFlags = [ "PREFIX=$(out)" "BINPREFIX=$(PREFIX)/bin" ];
 
   meta = {
     homepage = "https://github.com/Naheel-Azawy/cp-p";
@@ -21,5 +21,5 @@ stdenv.mkDerivation {
     license = with lib.licenses; [ gpl3Only ];
     maintainers = with lib.maintainers; [ deadendpl ];
     platforms = lib.platforms.all;
-  }; 
+  };
 }
