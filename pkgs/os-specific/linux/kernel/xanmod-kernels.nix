047d3be8dd5bd45e8d976b517b1f04c626d0fc3a
zzzsyyy <me@zzzsy.top>
2023-11-23 18:09:53 +0800

    linux_xanmod: 6.1.62 -> 6.1.63
    (cherry picked from commit 0a825f0bdffd5f7e67cfc320c86801d39fbbe12d)

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -3,8 +3,8 @@
 let
   # These names are how they are designated in https://xanmod.org.
   ltsVariant = {
-    version = "6.1.62";
-    hash = "sha256-fo5OQ/MZ+QVdCmLzX0OgFUBedfqrkqp+Ev081RVdtWw=";
+    version = "6.1.63";
+    hash = "sha256-WBMKJCLYexWJuTpli8vjvdms2ZYPXIS0yUxTgAL00io=";
     variant = "lts";
   };
 
