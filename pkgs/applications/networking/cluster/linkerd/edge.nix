d05c29bc39f83959d3d920fdf4e9b66b4238c3c6
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-23 12:04:45 +0000

    linkerd_edge: 23.10.4 -> 23.11.4

--- pkgs/applications/networking/cluster/linkerd/edge.nix
+++ pkgs/applications/networking/cluster/linkerd/edge.nix
@@ -2,7 +2,7 @@
 
 (callPackage ./generic.nix { }) {
   channel = "edge";
-  version = "23.10.4";
-  sha256 = "1fbzxkfc957kdhk60x3ywwpn54zq8njqk313cgfygnrmmj3m67j9";
-  vendorHash = "sha256-WEnopX/tIRoA5wiiCMue1T3wottxv744Mp7XJl63j4k=";
+  version = "23.11.4";
+  sha256 = "0j6yzjd2rnm6vzn2fky83pw3v943n3chhnr7a302rnafprlbmmp4";
+  vendorHash = "sha256-1s2vj9GSNe4j9TtIo69uakrg8PnBHNchlApryBeHmKs=";
 }
