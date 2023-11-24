33d2a40d6751477472a2eeb0531ef7ddbf1cc695
Anderson Torres <torres.anderson.85@protonmail.com>
2023-11-24 16:39:57 -0300

    systemd: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28740,7 +28740,7 @@ with pkgs;
 
   sysstat = callPackage ../os-specific/linux/sysstat { };
 
-  systemd = callPackage ../os-specific/linux/systemd {
+  systemd = callPackage ../by-name/sy/systemd/package.nix {
     # break some cyclic dependencies
     util-linux = util-linuxMinimal;
     # provide a super minimal gnupg used for systemd-machined
