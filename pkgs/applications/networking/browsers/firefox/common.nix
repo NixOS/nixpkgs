b9969d49acfcd78b6708fe9155432037d5c9f696
Nick Cao <nickcao@nichi.co>
2023-11-23 22:06:45 -0500

    firefox: fix cross compilation by replacing hardcoded pkg-config with $PKG_CONFIG

--- pkgs/applications/networking/browsers/firefox/common.nix
+++ pkgs/applications/networking/browsers/firefox/common.nix
@@ -511,7 +511,7 @@ buildStdenv.mkDerivation {
     cd mozobj
   '' + lib.optionalString (lib.versionAtLeast version "120") ''
     # https://bugzilla.mozilla.org/show_bug.cgi?id=1864083
-    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE $(pkg-config dbus-1 --cflags)"
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE $($PKG_CONFIG dbus-1 --cflags)"
   '';
 
   postBuild = ''
