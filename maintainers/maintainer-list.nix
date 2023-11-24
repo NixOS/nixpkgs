997edd981bd089aca0f915736118f6f5283a59e5
Nathaniel Brough <nathaniel.brough@gmail.com>
2023-11-24 11:23:48 -0800

    maintainers: Add Nathaniel Brough

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12644,6 +12644,12 @@
     githubId = 330943;
     name = "Nathan Bijnens";
   };
+  nathanielbrough = {
+    github = "silvergasp";
+    githubId = 7277663;
+    email = "nathaniel.brough@gmail.com";
+    name = "Nathaniel Brough";
+  };
   nathanruiz = {
     email = "nathanruiz@protonmail.com";
     github = "nathanruiz";
