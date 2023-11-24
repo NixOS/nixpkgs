add725d43629651aa54322e829f3f707ef0f9958
natsukium <tomoya.otabi@gmail.com>
2023-11-25 00:57:55 +0900

    python311Packages.demetriek: apply a pydantic>=2 compatible patch

--- pkgs/development/python-modules/demetriek/default.nix
+++ pkgs/development/python-modules/demetriek/default.nix
@@ -5,6 +5,7 @@
 , buildPythonPackage
 , pydantic
 , fetchFromGitHub
+, fetchpatch
 , poetry-core
 , yarl
 , aresponses
@@ -26,6 +27,16 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-LCHHBcZgO9gw5jyaJiiS4lKyb0ut+PJvKTylIvIKHhc=";
   };
+
+  patches = [
+    # https://github.com/frenck/python-demetriek/pull/531
+    (fetchpatch {
+      name = "pydantic_2-compatibility.patch";
+      url = "https://github.com/frenck/python-demetriek/commit/e677fe5b735b6b28572e3e5fd6aab56fc056f5e6.patch";
+      excludes = [ "pyproject.toml" "poetry.lock" ];
+      hash = "sha256-oMVR45KHDhcPId/0X9obJXCPE8s1gk5IgsGsgZesdZw=";
+    })
+  ];
 
   postPatch = ''
     # Upstream doesn't set a version for the pyproject.toml
