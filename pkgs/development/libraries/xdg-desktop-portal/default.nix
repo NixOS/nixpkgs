NixOS/nixpkgs

commit c585543ccd5065f0d5e36cdcbb51ada63c3a4370

xdg-desktop-portal: 1.16.0 → 1.17.0
https://github.com/flatpak/xdg-desktop-portal/compare/1.16.0...1.17.0
https://github.com/flatpak/xdg-desktop-portal/releases/tag/1.17.0

Ported to meson. Some of the changes are highlighted in the
"Changes that might be relevant for distributors" session in
https://github.com/flatpak/xdg-desktop-portal/releases/tag/1.18.0

Notably, desktop environments are now responsible for declaring preferred portal backends:
https://github.com/flatpak/xdg-desktop-portal/commit/939f0b0fcec6a42c1acdc397986547e9805359ff

Specify sysconfdir so that the configs are loaded from `/etc` rather than `$out/etc`.

Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>
Changelog-reviewed-by: Jan Tojnar <jtojnar@gmail.com>

pkgs/development/libraries/xdg-desktop-portal/default.nix
@@ -1,11 +1,11 @@
 { lib
-, acl
-, autoreconfHook
-, dbus
 , fetchFromGitHub
 , flatpak
 , fuse3
 , bubblewrap
+, docbook_xml_dtd_412
+, docbook_xml_dtd_43
+, docbook_xsl
 , systemdMinimal
 , geoclue2
 , glib
@@ -13,6 +13,8 @@
 , json-glib
 , libportal
 , libxml2
+, meson
+, ninja
 , nixosTests
 , pipewire
 , gdk-pixbuf
@@ -22,12 +24,13 @@
 , stdenv
 , runCommand
 , wrapGAppsHook
+, xmlto
 , enableGeoLocation ? true
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xdg-desktop-portal";
-  version = "1.16.0";
+  version = "1.17.0";
 
   outputs = [ "out" "installedTests" ];
 
@@ -35,7 +38,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "flatpak";
     repo = "xdg-desktop-portal";
     rev = finalAttrs.version;
-    sha256 = "sha256-5VNauinTvZrSaQzyP/quL/3p2RPcTJUDLscEQMJpvYA=";
+    sha256 = "sha256-OGf2ohP/Qd+ff3KfJYoHu9wbCAUXCP3wm/utjqkgccE=";
   };
 
   patches = [
@@ -46,18 +49,24 @@ stdenv.mkDerivation (finalAttrs: {
       substitute "${flatpak.icon-validator-patch}" "$out" \
         --replace "/icon-validator/validate-icon.c" "/src/validate-icon.c"
     '')
+
+    # Allow installing installed tests to a separate output.
+    ./installed-tests-path.patch
   ];
 
   nativeBuildInputs = [
-    autoreconfHook
+    docbook_xml_dtd_412
+    docbook_xml_dtd_43
+    docbook_xsl
     libxml2
+    meson
+    ninja
     pkg-config
     wrapGAppsHook
+    xmlto
   ];
 
   buildInputs = [
-    acl
-    dbus
     flatpak
     fuse3
     bubblewrap
@@ -80,16 +89,27 @@ stdenv.mkDerivation (finalAttrs: {
     geoclue2
   ];
 
-  configureFlags = [
-    "--enable-installed-tests"
-  ] ++ lib.optionals (!enableGeoLocation) [
-    "--disable-geoclue"
+  nativeCheckInputs = [
+    python3.pkgs.pytest
+    python3.pkgs.python-dbusmock
+    python3.pkgs.pygobject3
+    python3.pkgs.dbus-python
   ];
 
-  makeFlags = [
-    "installed_testdir=${placeholder "installedTests"}/libexec/installed-tests/xdg-desktop-portal"
-    "installed_test_metadir=${placeholder "installedTests"}/share/installed-tests/xdg-desktop-portal"
+  mesonFlags = [
+    "--sysconfdir=/etc"
+    "-Dinstalled-tests=true"
+    "-Dinstalled_test_prefix=${placeholder "installedTests"}"
+  ] ++ lib.optionals (!enableGeoLocation) [
+    "-Dgeoclue=false"
   ];
+
+  doCheck = true;
+
+  preCheck = ''
+    # For test_trash_file
+    export HOME=$(mktemp -d)
+  '';
 
   passthru = {
     tests = {

pkgs/development/libraries/xdg-desktop-portal/installed-tests-path.patch
@@ -1,0 +1,30 @@
+diff --git a/meson.build b/meson.build
+index b25f9ef..7975f85 100644
+--- a/meson.build
++++ b/meson.build
+@@ -40,8 +40,8 @@ if dataroot_dir == ''
+     dataroot_dir = datadir
+ endif
+ 
+-installed_tests_dir = prefix / libexecdir / 'installed-tests' / meson.project_name()
+-installed_tests_data_dir = prefix / datadir / 'installed-tests' / meson.project_name()
++installed_tests_dir = get_option('installed_test_prefix') / 'libexec' / 'installed-tests' / meson.project_name()
++installed_tests_data_dir = get_option('installed_test_prefix') / 'share' / 'installed-tests' / meson.project_name()
+ docs_dir = datadir / 'doc' / meson.project_name()
+ 
+ summary({
+diff --git a/meson_options.txt b/meson_options.txt
+index fccada3..ca87600 100644
+--- a/meson_options.txt
++++ b/meson_options.txt
+@@ -38,6 +38,10 @@ option('installed-tests',
+         type: 'boolean',
+         value: false,
+         description: 'Enable installation of some test cases')
++option('installed_test_prefix',
++        type: 'string',
++        value: '',
++        description: 'Prefix for installed tests')
+ option('pytest',
+         type: 'feature',
+         value: 'auto',

commit f8dd4b46ccc97a15a623c7a3833239d12b1e6e98

xdg-desktop-portal: fix disabling Geo location
The "geoclue" Meson option is defined to have type "feature", which
means that the set of possible options is "enabled", "disabled" or
"auto". We pass "false" though when the user has disabled Geo location
support, which is wrong and causes the build to fail:

> meson.build:1:0: ERROR: Value "false" (of type "string") for combo option "Enable Geoclue support. Needed for location portal" is not one of the choices. Possible choices are (as string): "enabled", "disabled", "auto".

This broke in c585543ccd50 (xdg-desktop-portal: 1.16.0 → 1.17.0,
2023-08-08) where we started using the new Meson-based build system
instead of the autotools-based one. Fix it by using "disabled" instead.

pkgs/development/libraries/xdg-desktop-portal/default.nix
@@ -109,7 +109,7 @@ stdenv.mkDerivation (finalAttrs: {
     "-Dinstalled-tests=true"
     "-Dinstalled_test_prefix=${placeholder "installedTests"}"
   ] ++ lib.optionals (!enableGeoLocation) [
-    "-Dgeoclue=false"
+    "-Dgeoclue=disabled"
   ];
 
   doCheck = true;