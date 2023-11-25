f4b9f2156dede08bda9ae3c9440725724a4d7a5c
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-25 09:48:13 +0000

    postgresql12JitPackages.pg_auto_failover: 2.0 -> 2.1

--- pkgs/servers/sql/postgresql/ext/pg_auto_failover.nix
+++ pkgs/servers/sql/postgresql/ext/pg_auto_failover.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "pg_auto_failover";
-  version = "2.0";
+  version = "2.1";
 
   src = fetchFromGitHub {
     owner = "citusdata";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-CLtLOzKRB9p6+SytMvWCYo7m7s/d+clAGOa2sWi6uZ0=";
+    sha256 = "sha256-OIWykfFbVskrkPG/zSmZtZjc+W956KSfIzK7f5QOqpI=";
   };
 
   buildInputs = [ postgresql openssl zlib readline libkrb5 ]
