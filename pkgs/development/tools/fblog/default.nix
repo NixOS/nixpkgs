748a0aaa627dcc58114f88d9cda9aa3341d16803
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-25 16:13:23 +0000

    fblog: 4.5.0 -> 4.6.0

--- pkgs/development/tools/fblog/default.nix
+++ pkgs/development/tools/fblog/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "fblog";
-  version = "4.5.0";
+  version = "4.6.0";
 
   src = fetchFromGitHub {
     owner = "brocode";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-T0NvcNg2UeUpEf1hjSdoaUkIzCAP29vo6edfeno/oyo=";
+    hash = "sha256-yNl+A0nwzCO4vJMNJTSTIpXK1zgfaK+ROMQ41gB+H1Y=";
   };
 
-  cargoHash = "sha256-3/j/TjsQjXFe+rTfCncjoownXzaUiUBUkCXbFc5RM2o=";
+  cargoHash = "sha256-r+6kTNUn97SVCgZj7vCjksS2s/74OzeZgXOx7XOgMdY=";
 
   meta = with lib; {
     description = "A small command-line JSON log viewer";
