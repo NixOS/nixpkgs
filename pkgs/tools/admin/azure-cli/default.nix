c796a7f933c26414ca43a72a3bda792dcd0c830e
Samy Lahfa <14914796+AkechiShiro@users.noreply.github.com>
2023-11-25 04:12:05 +0100

    azure-cli: 2.53.1 -> 2.54.0
    BIG CHANGES (improve stability):
    - Removed patches for python311 test as they got fixed.
    - Fix az self-test, upgrade azure-mgmt-signalr version to 2.0.0b2
    - Pin all release dependencies and add msrest to azure-mgmt-maps
    - Removed override antlr4-python3-runtime of antlr4 to antlr4_9 was causing ord 'monitor' errors during self-test.
    - Pinned azure-mgmt-* dependencies to https://github.com/Azure/azure-cli/blob/f74a18bc269b41b1b9d2712113ac54cba16b7e02/src/azure-cli/requirements.py3.Linux.txt
    - Pin antlr to 4.12

--- pkgs/tools/admin/azure-cli/default.nix
+++ pkgs/tools/admin/azure-cli/default.nix
@@ -5,14 +5,14 @@
 }:
 
 let
-  version = "2.53.1";
+  version = "2.54.0";
 
   src = fetchFromGitHub {
     name = "azure-cli-${version}-src";
     owner = "Azure";
     repo = "azure-cli";
     rev = "azure-cli-${version}";
-    hash = "sha256-++JquLva3ij4NDhXnY7tSQMH4HCtJ8DhZIzE0XguInM=";
+    hash = "sha256-LbGDPZe4AWl6wnZ5GtiiM4O5EsCHEHX420lBIJJ0+Kk=";
   };
 
   # put packages that needs to be overridden in the py package scope
