ecddf906dc52161749cdafa5c9605c0812bd7019
Louis Bettens <louis@bettens.info>
2023-11-25 11:59:04 +0100

    teck-udev-rules: correct meta.license attribute

--- pkgs/development/node-packages/overrides.nix
+++ pkgs/development/node-packages/overrides.nix
@@ -311,10 +311,11 @@ final: prev: {
     };
   };
 
-  teck-programmer = prev.teck-programmer.override {
+  teck-programmer = prev.teck-programmer.override ({ meta, ... }: {
     nativeBuildInputs = [ final.node-gyp-build ];
     buildInputs = [ pkgs.libusb1 ];
-  };
+    meta = meta // { license = lib.licenses.gpl3Plus; };
+  });
 
   thelounge-plugin-closepms = prev.thelounge-plugin-closepms.override {
     nativeBuildInputs = [ final.node-pre-gyp ];
