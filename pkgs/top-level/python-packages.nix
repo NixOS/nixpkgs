307a8e05570a2c27d8d98c95faebc7c5a55a7f15
Joshua Trees <me@jtrees.io>
2023-11-26 16:51:59 +0100

    python311Packages.durationpy: init at 0.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3384,6 +3384,8 @@ self: super: with self; {
 
   duo-client = callPackage ../development/python-modules/duo-client { };
 
+  durationpy = callPackage ../development/python-modules/durationpy {  };
+
   durus = callPackage ../development/python-modules/durus {  };
 
   dvc = callPackage ../development/python-modules/dvc {  };
