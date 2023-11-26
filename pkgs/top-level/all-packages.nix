47b2da5c196ce8611ec179f476dc19b2bd851890
Felix Buehler <account@buehler.rocks>
2023-11-26 16:46:24 +0100

    extremetuxracer: remove override libpng

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37756,9 +37756,7 @@ with pkgs;
 
   endless-sky = callPackage ../games/endless-sky { };
 
-  extremetuxracer = callPackage ../games/extremetuxracer {
-    libpng = libpng12;
-  };
+  extremetuxracer = callPackage ../games/extremetuxracer { };
 
   exult = callPackage ../games/exult { };
 
