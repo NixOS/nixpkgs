8053d43c6434fc9fffb57ed65fd99b8d03cedcce
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2023-11-26 17:52:44 +0800

    debian-archive-keyring: init at 2023.4

---
+++ pkgs/by-name/de/debian-archive-keyring/package.nix
@@ -0,0 +1,37 @@
+{ lib
+, stdenv
+, fetchFromGitLab
+, gnupg
+, jetring
+, debian-archive-keyring
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "debian-archive-keyring";
+  version = "2023.4";
+
+  src = fetchFromGitLab {
+    domain = "salsa.debian.org";
+    owner = "release-team";
+    repo = "${finalAttrs.pname}";
+    rev = "${finalAttrs.version}";
+    hash = "sha256-RjHC4AJ0xyVmELVjX/WBmZYsKWFqcg8F5j+9+18jwj4=";
+  };
+
+  nativeBuildInputs = [
+    gnupg
+    jetring
+  ];
+
+  makeFlags = [
+    "DESTDIR=${placeholder "out"}"
+  ];
+
+  meta = {
+    homepage = "https://packages.debian.org/sid/debian-archive-keyring";
+    description = "GnuPG archive keys of the Debian archive";
+    license = with lib.licenses; [ publicDomain ];
+    maintainers = with lib.maintainers; [ MakiseKurisu ];
+    platforms = lib.platforms.all;
+  };
+})
