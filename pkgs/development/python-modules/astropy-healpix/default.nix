99b40d7ef8a5083fb5f9c379893a6b8febda49bb
annalee <150648636+a-n-n-a-l-e-e@users.noreply.github.com>
2023-11-26 02:26:50 +0000

    python311Packages.astropy-healpix: upstream patch to fix darwin build
    upstream patch adds const to arguments so the function definition
    matches the function pointer prototype.
    
    remove patch on next version update

--- pkgs/development/python-modules/astropy-healpix/default.nix
+++ pkgs/development/python-modules/astropy-healpix/default.nix
@@ -2,6 +2,7 @@
 , stdenv
 , buildPythonPackage
 , fetchPypi
+, fetchpatch
 , numpy
 , astropy
 , astropy-extension-helpers
@@ -20,6 +21,16 @@ buildPythonPackage rec {
     pname = lib.replaceStrings ["-"] ["_"] pname;
     hash = "sha256-9ILvYqEOaGMD84xm8I3xe53e5a2CIZwjVx7oDXar7qM=";
   };
+
+  patches = [
+    # remove on next udpate. make Numpy loop function args const correct.
+    # https://github.com/astropy/astropy-healpix/pull/199
+    (fetchpatch {
+      name = "numpy-const-args-match.patch";
+      url = "https://github.com/astropy/astropy-healpix/commit/ccf6d9ea4be131f56646adbd7bc482bfcd84f21c.patch";
+      hash = "sha256-fpDxTbs3sHJSb4mnveorM+wlseXbZu1biGyBTNC9ZUo=";
+    })
+  ];
 
   nativeBuildInputs = [
     astropy-extension-helpers
