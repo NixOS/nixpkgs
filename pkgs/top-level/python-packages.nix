cdc49f207292c2f555e9bf8786cee3e71b345b15
Anton Mosich <anton@mosich.at>
2023-11-27 00:46:19 +0100

    vegafusion: init at 1.6.9
    The tests for vegafusion fail horrendously when activating them, for
    reasons I have not investigated. The package works as intended for
    geo-activity-playground, though.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18358,6 +18358,8 @@ self: super: with self; {
 
   vega-datasets = callPackage ../development/python-modules/vega-datasets { };
 
+  vegafusion = callPackage ../development/python-modules/vegafusion { };
+
   vegafusion-embed = callPackage ../development/python-modules/vegafusion-embed {
     inherit (pkgs) protobuf;
   };
