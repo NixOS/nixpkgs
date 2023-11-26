5e90b32c299a8bf2ac2147fe3f7ecd4ba16cf708
Oliver Koss <oliver.koss06@gmail.com>
2023-11-26 02:24:41 +0100

    maintainers: add oliver-koss

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13462,6 +13462,12 @@
     githubId = 72201;
     name = "Ole Jørgen Brønner";
   };
+  oliver-koss = {
+    email = "oliver.koss06@gmail.com";
+    github = "oliver-koss";
+    githubId = 39134647;
+    name = "Oliver Koss";
+  };
   ollieB = {
     github = "oliverbunting";
     githubId = 1237862;
