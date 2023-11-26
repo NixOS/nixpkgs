8e7a666d43966f2fd077b1f0ace0fe757a565d23
Anton Mosich <anton@mosich.at>
2023-11-27 00:46:47 +0100

    vl-convert-python: init at 1.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17104,6 +17104,10 @@ self: super: with self; {
 
   vllm = callPackage ../development/python-modules/vllm { };
 
+  vl-convert-python = callPackage ../development/python-modules/vl-convert {
+    inherit (pkgs) protobuf;
+  };
+
   vmprof = callPackage ../development/python-modules/vmprof { };
 
   vncdo = callPackage ../development/python-modules/vncdo { };
