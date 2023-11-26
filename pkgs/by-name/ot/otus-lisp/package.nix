a73f29ddcfcfd6cdf63774d4f6456d9e117fb05b
Daniel Nagy <danielnagy@posteo.de>
2023-11-26 15:38:05 +0100

    otus-lisp: 2.4 -> 2.5
    Diff: https://github.com/yuriy-chumak/ol/compare/2.4...2.5

--- pkgs/by-name/ot/otus-lisp/package.nix
+++ pkgs/by-name/ot/otus-lisp/package.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "otus-lisp";
-  version = "2.4";
+  version = "2.5";
 
   src = fetchFromGitHub {
     owner = "yuriy-chumak";
     repo = "ol";
     rev = version;
-    sha256 = "sha256-+6qH1BhvMkuG2rUOfo9qMjMjhCib9KONQTBWS27c3Ts=";
+    sha256 = "sha256-xwn2cvtw3co7MJ4J0FraEtZhKWVaaaoJYMrohyFF+us=";
   };
 
   nativeBuildInputs = [ xxd ];
@@ -21,5 +21,6 @@ stdenv.mkDerivation rec {
     license = with lib.licenses; [ mit lgpl3Only ]; # dual licensed
     platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ nagy ];
+    mainProgram = "ol";
   };
 }
