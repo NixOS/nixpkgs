bf92006f47ad996ab1261a9408ec7d14e432f61a
Mario Rodas <marsam@users.noreply.github.com>
2023-11-26 12:38:00 +0000

    postgresqlPackages.pgrouting: fix build darwin

--- pkgs/servers/sql/postgresql/ext/pgrouting.nix
+++ pkgs/servers/sql/postgresql/ext/pgrouting.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, postgresql, perl, cmake, boost }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, postgresql, perl, cmake, boost }:
 
 stdenv.mkDerivation rec {
   pname = "pgrouting";
@@ -13,6 +13,14 @@ stdenv.mkDerivation rec {
     rev    = "v${version}";
     sha256 = "sha256-FOHIQzL8tmSWllCTzQkuGOiqk47S+HTB8aEpAC30YNk=";
   };
+
+  patches = [
+    # Fix issues with size_t vs uint64_ on Darwin. Remove with the next release.
+    (fetchpatch {
+      url = "https://github.com/pgRouting/pgrouting/commit/b16e9da748e9d78c8b19d2b1db3baeb19c33c6aa.patch";
+      hash = "sha256-CJmuVxZ3zIJTa6KXhM2cvynAE6Vmff7XBDfSGg4W9dE=";
+    })
+  ];
 
   installPhase = ''
     install -D lib/*.so                        -t $out/lib
