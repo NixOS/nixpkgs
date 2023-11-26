441c18d7e82a3f90e04c6361316fa0c9fc4564a1
Guillaume Girol <symphorien+git@xlumurb.eu>
2023-11-26 12:00:00 +0000

    dovecot_fts_xapian: 1.5.5 -> 1.5.7

--- pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
+++ pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, autoconf, automake, sqlite, pkg-config, dovecot, libtool, xapian, icu64 }:
 stdenv.mkDerivation rec {
   pname = "dovecot-fts-xapian";
-  version = "1.5.5";
+  version = "1.5.7";
 
   src = fetchFromGitHub {
     owner = "grosjo";
     repo = "fts-xapian";
     rev = version;
-    sha256 = "sha256-KAZno4N/4dFH3QHFTs0wkY4PtGF+j4ZEjZzn9ljCjrM=";
+    sha256 = "sha256-Kxc5mmnp4HBuaPeQUwDLYj3ga7PeF0WJFJFRwhNBZA4=";
   };
 
   buildInputs = [ dovecot xapian icu64 sqlite ];
