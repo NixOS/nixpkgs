31fa2e5f9de265b32ce5ae48ccebdb4f9ccdb39f
Nick Cao <nickcao@nichi.co>
2023-11-26 10:31:44 -0500

    python311Packages.tcxreader: modernize

--- pkgs/development/python-modules/tcxreader/default.nix
+++ pkgs/development/python-modules/tcxreader/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, poetry-core
 , pytestCheckHook
 , pythonOlder
 }:
@@ -8,9 +9,9 @@
 buildPythonPackage rec {
   pname = "tcxreader";
   version = "0.4.5";
-  format = "setuptools";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "alenrajsp";
@@ -18,6 +19,10 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-CiOLcev9fo2BPgnPZZ2borU25f/gKISqRAapAHgLN3w=";
   };
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
 
   nativeCheckInputs = [
     pytestCheckHook
