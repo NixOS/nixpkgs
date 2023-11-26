5f88256717e9db7bcbbaacdc878a6bf61924a3ff
Anton Mosich <anton@mosich.at>
2023-11-26 21:24:16 +0100

    fitdecode: init at 0.10.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4099,6 +4099,8 @@ self: super: with self; {
 
   fitbit = callPackage ../development/python-modules/fitbit { };
 
+  fitdecode = callPackage ../development/python-modules/fitdecode { };
+
   fivem-api = callPackage ../development/python-modules/fivem-api { };
 
   fixerio = callPackage ../development/python-modules/fixerio { };
