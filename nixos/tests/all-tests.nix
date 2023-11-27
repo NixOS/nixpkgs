070bcf9da438a76495f624fc31b2be8036ee7a30
Arthur Gautier <arthur.gautier@arista.com>
2023-11-27 09:45:10 -0800

    nixos/dublin-traceroute: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -253,6 +253,7 @@ in {
   domination = handleTest ./domination.nix {};
   dovecot = handleTest ./dovecot.nix {};
   drbd = handleTest ./drbd.nix {};
+  dublin-traceroute = handleTest ./dublin-traceroute.nix {};
   earlyoom = handleTestOn ["x86_64-linux"] ./earlyoom.nix {};
   early-mount-options = handleTest ./early-mount-options.nix {};
   ec2-config = (handleTestOn ["x86_64-linux"] ./ec2.nix {}).boot-ec2-config or {};
