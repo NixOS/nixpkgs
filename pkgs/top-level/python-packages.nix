8856d14f0c672ade3b9f00cd7bb52c6d7e428bec
Maciej Krüger <mkg20001@gmail.com>
2023-11-27 06:35:44 +0100

    pyipv8: init at 2.12.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10333,6 +10333,8 @@ self: super: with self; {
 
   pyipp = callPackage ../development/python-modules/pyipp { };
 
+  pyipv8 = callPackage ../development/python-modules/pyipv8 { };
+
   pyiqvia = callPackage ../development/python-modules/pyiqvia { };
 
   pyisbn = callPackage ../development/python-modules/pyisbn { };
