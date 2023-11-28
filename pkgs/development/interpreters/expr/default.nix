0acfddb0eea2a5ad1d90e9c6b1ca4ea282d4ddc5
figsoda <figsoda@pm.me>
2023-11-28 14:18:37 -0500

    expr: 1.15.4 -> 1.15.5
    Diff: https://github.com/antonmedv/expr/compare/v1.15.4...v1.15.5
    
    Changelog: https://github.com/antonmedv/expr/releases/tag/v1.15.5
    (cherry picked from commit 96727dd4e1467072a8ce41eae5834f036b18f59c)

--- pkgs/development/interpreters/expr/default.nix
+++ pkgs/development/interpreters/expr/default.nix
@@ -5,18 +5,18 @@
 
 buildGoModule rec {
   pname = "expr";
-  version = "1.15.4";
+  version = "1.15.5";
 
   src = fetchFromGitHub {
     owner = "antonmedv";
     repo = "expr";
     rev = "v${version}";
-    hash = "sha256-x96I6HHhm3RIrlg1/KVCIbFkelazGt0H2nk8juUWjKg=";
+    hash = "sha256-DIJBVL1HAZyD+K+vKnB1O2cZJkBu0R2xueDO1OUHq3I=";
   };
 
   sourceRoot = "${src.name}/repl";
 
-  vendorHash = "sha256-ZVB6P0WdjyDK9OlEgKjR3D3IVBkDbDx9bOpLC/H2JLs=";
+  vendorHash = "sha256-D4JdFY9OweTigT0NIKmBbxrHn5FoVsj/EU6c0y9aaDo=";
 
   ldflags = [ "-s" "-w" ];
 
