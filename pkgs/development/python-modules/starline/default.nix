4bdb4b80496cf5f987db637ff56851fd5ea6703a
Robert Schütz <nix@dotlambda.de>
2023-11-28 08:25:03 -0800

    python311Packages.starline: modernize expression

--- pkgs/development/python-modules/starline/default.nix
+++ pkgs/development/python-modules/starline/default.nix
@@ -2,7 +2,8 @@
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
-, aiohttp
+, fetchpatch
+, setuptools
 , requests
 }:
 
@@ -12,13 +13,26 @@ buildPythonPackage rec {
 
   disabled = pythonOlder "3.5";
 
+  pyproject = true;
+
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1753f5fcd2a6976aed775afb03f8392159f040c673917cc0c634510d95c13cb9";
+    hash = "sha256-F1P1/NKml2rtd1r7A/g5IVnwQMZzkXzAxjRRDZXBPLk=";
   };
 
+  patches = [
+    # https://github.com/Anonym-tsk/starline/pull/5
+    (fetchpatch {
+      url = "https://github.com/Anonym-tsk/starline/commit/4e6cdf8e05c5fb8509ee384e77b39a2495587160.patch";
+      hash = "sha256-y9b6ePH3IEgmt3ALHQGwH102rlm4KfmH4oIoIC93cWU=";
+    })
+  ];
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [
-    aiohttp
     requests
   ];
 
