f524921fa90777fd99f8daf902058522f0394a6c
natsukium <tomoya.otabi@gmail.com>
2023-11-28 09:29:49 +0900

    python311Packages.pydantic_1: init at 1.10.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10159,6 +10159,8 @@ self: super: with self; {
 
   pydantic = callPackage ../development/python-modules/pydantic { };
 
+  pydantic_1 = callPackage ../development/python-modules/pydantic/1.nix { };
+
   pydantic-core = callPackage ../development/python-modules/pydantic-core { };
 
   pydantic-extra-types = callPackage ../development/python-modules/pydantic-extra-types { };
