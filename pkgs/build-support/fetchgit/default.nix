07f930f5e25367a3892705b1fa3233e1b041694f
Tim Cuthbertson <tim@gfxmonk.net>
2023-11-28 22:15:40 +1100

    treewide: support NIX_SSL_CERT_FILE as an impureEnvVar
    This envvar is also added to lib.proxyImpureEnvVars since it's
    typically required for https proxies.
    
    This change also updates fetchgit and go module fetching to use this
    envvar. NIX_GIT_SSL_CAINFO is still supported for backwards
    compatibility in fetchgit.

--- pkgs/build-support/fetchgit/default.nix
+++ pkgs/build-support/fetchgit/default.nix
@@ -67,7 +67,7 @@ stdenvNoCC.mkDerivation {
   builder = ./builder.sh;
   fetcher = ./nix-prefetch-git;
 
-  nativeBuildInputs = [ git ]
+  nativeBuildInputs = [ git cacert ]
     ++ lib.optionals fetchLFS [ git-lfs ];
 
   outputHashAlgo = if hash != "" then null else "sha256";
@@ -93,8 +93,6 @@ stdenvNoCC.mkDerivation {
     export NETRC=$PWD/.netrc
     export HOME=$PWD
   '';
-
-  GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
 
   impureEnvVars = lib.fetchers.proxyImpureEnvVars ++ netrcImpureEnvVars ++ [
     "GIT_PROXY_COMMAND" "NIX_GIT_SSL_CAINFO" "SOCKS_SERVER"
