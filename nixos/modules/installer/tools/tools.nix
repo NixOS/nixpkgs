eb8678e241a39a131fc0a9eb8e29b7c78e316215
Evils <evils.devils@protonmail.com>
2023-11-28 18:24:21 +0100

    nixos-generate-config: correct search command
    this line dates from 2018-04-27...
     (e8d5070e23203163afb6450a6d82fc59e0012580)

--- nixos/modules/installer/tools/tools.nix
+++ nixos/modules/installer/tools/tools.nix
@@ -140,7 +140,7 @@ let
       # programs.firefox.enable = true;
 
       # List packages installed in system profile. To search, run:
-      # \$ nix search wget
+      # \$ nix search --extra-experimental-features "nix-command flakes" nixpkgs wget
       # environment.systemPackages = with pkgs; [
       #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       #   wget
