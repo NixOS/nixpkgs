2a5388a54e6dad4828647c53c180d69df7683605
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-28 10:04:15 +0100

    python311Packages.gentools: add changelog to meta

--- pkgs/development/python-modules/gentools/default.nix
+++ pkgs/development/python-modules/gentools/default.nix
@@ -13,12 +13,11 @@ buildPythonPackage rec {
   version = "1.2.1";
   pyproject = true;
 
-  # Pypi doesn't ship the tests, so we fetch directly from GitHub
   src = fetchFromGitHub {
     owner = "ariebovenberg";
-    repo = pname;
+    repo = "gentools";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-RBUIji3FOIRjfp4t7zBAVSeiWaYufz4ID8nTWmhDkf8=";
+    hash = "sha256-RBUIji3FOIRjfp4t7zBAVSeiWaYufz4ID8nTWmhDkf8=";
   };
 
   nativeBuildInputs = [ poetry-core ];
@@ -33,8 +32,9 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Tools for generators, generator functions, and generator-based coroutines";
-    license = licenses.mit;
     homepage = "https://gentools.readthedocs.io/";
+    changelog = "https://github.com/ariebovenberg/gentools/blob/v${version}/CHANGELOG.rst";
+    license = licenses.mit;
     maintainers = with maintainers; [ mredaelli ];
   };
 }
