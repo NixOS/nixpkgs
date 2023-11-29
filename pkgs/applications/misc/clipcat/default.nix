17d63a4bf54ba1435eb808bdc853ec0ee143c65e
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-11-29 17:56:10 +0800

    clipcat: 0.7.0 -> 0.7.1
    Diff: https://github.com/xrelkd/clipcat/compare/v0.7.0...v0.7.1
    (cherry picked from commit a2c73b7543bfa86855161657879aecd3889b3e0a)

--- pkgs/applications/misc/clipcat/default.nix
+++ pkgs/applications/misc/clipcat/default.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "clipcat";
-  version = "0.7.0";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "xrelkd";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-MEyKUSEGVELk00TtKdfEgfzc3zwBllYrIjtC4NVbmRo=";
+    hash = "sha256-74OUXvB6DrnWTSox3J9rakP+gOYpWF+O/pA6ucn5+J8=";
   };
 
-  cargoHash = "sha256-LiTZfkp0uuKgfl4uaxNEJAn7UlrHHaWh/ivaJxYhULY=";
+  cargoHash = "sha256-SOiCwFRKzBm/ClnNb4hha0RPHJ227hCyDhKv5lciasw=";
 
   nativeBuildInputs = [
     protobuf
