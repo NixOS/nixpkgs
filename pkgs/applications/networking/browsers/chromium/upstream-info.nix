47ce8a69bf970f271b2397cdc5820740d3fd8255
networkException <git@nwex.de>
2023-11-29 22:14:08 +0100

    chromium: 119.0.6045.159 -> 119.0.6045.199
    https://chromereleases.googleblog.com/2023/11/stable-channel-update-for-desktop_28.html
    
    This update includes 7 security fixes. Google is aware that an exploit
    for CVE-2023-6345 exists in the wild.
    
    CVEs:
    CVE-2023-6348 CVE-2023-6347 CVE-2023-6346 CVE-2023-6350 CVE-2023-6351
    CVE-2023-6345

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -15,9 +15,9 @@
         version = "2023-09-12";
       };
     };
-    hash = "sha256-8xPm3vNF0HjfL7ewTz7iz7GMfiJi6mhMK1YSC7VeoSM=";
-    hash_deb_amd64 = "sha256-xAm7bPsnnJD7UWNTtHKMv5enHo3rM9w0M81QPqZVlP4=";
-    version = "119.0.6045.159";
+    hash = "sha256-LqAORwZRyS9ASo0U+iVi9srEKYoSBG5upjqi5F65ITg=";
+    hash_deb_amd64 = "sha256-9nZjyJnXvOO1iZea3mdsj5FYkylrWnhColZ+q+X/xcU=";
+    version = "119.0.6045.199";
   };
   ungoogled-chromium = {
     deps = {
