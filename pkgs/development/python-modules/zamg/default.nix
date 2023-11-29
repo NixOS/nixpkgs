872f816e61e57505073ecfa9538dc524f2a060b3
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-29 23:27:41 +0100

    python311Packages.zamg: 0.3.1 -> 0.3.3
    Diff: https://github.com/killer0071234/python-zamg/compare/refs/tags/v0.3.1...v0.3.3
    
    Changelog: https://github.com/killer0071234/python-zamg/releases/tag/v0.3.3

--- pkgs/development/python-modules/zamg/default.nix
+++ pkgs/development/python-modules/zamg/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "zamg";
-  version = "0.3.1";
+  version = "0.3.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "killer0071234";
     repo = "python-zamg";
     rev = "refs/tags/v${version}";
-    hash = "sha256-snYRYwJc9H6H+dxaQM4beGvu5AuaXv7W8w5xjR6RPyw=";
+    hash = "sha256-hgJtM208xsDh9RbxOFu5DOiKonLMj8aWyu9+EhpE6TA=";
   };
 
   postPatch = ''
