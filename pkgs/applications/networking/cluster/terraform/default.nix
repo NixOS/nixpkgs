577d5de6f42fbd1302a9effd98f49d6cf5a74d82
Mario Rodas <marsam@users.noreply.github.com>
2023-11-29 09:53:58 -0500

    terraform: 1.6.4 -> 1.6.5 (#270915)
    Diff: https://github.com/hashicorp/terraform/compare/v1.6.4...v1.6.5
    
    Changelog: https://github.com/hashicorp/terraform/blob/v1.6.5/CHANGELOG.md

--- pkgs/applications/networking/cluster/terraform/default.nix
+++ pkgs/applications/networking/cluster/terraform/default.nix
@@ -167,9 +167,9 @@ rec {
   mkTerraform = attrs: pluggable (generic attrs);
 
   terraform_1 = mkTerraform {
-    version = "1.6.4";
-    hash = "sha256-kA0H+JxyMV6RKRr20enTOzfwj2Lk2IP4vivfHv02+w8=";
-    vendorHash = "sha256-cxnvEwtZLXYZzCITJgYk8hDRndLLC8YTD+RvgcNska0=";
+    version = "1.6.5";
+    hash = "sha256-TJKs7pWoLFIeov/ERgPqZxPtbjSAHrHI2wrSEXUAS1A=";
+    vendorHash = "sha256-QHfCGlgOv4v3MzUs4JxIHytcyymUYmnk4Z0smgak1Mg=";
     patches = [ ./provider-path-0_15.patch ];
     passthru = {
       inherit plugins;
