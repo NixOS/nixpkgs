d426be111f88a1ced8a171a5b7b0d09446bc28bf
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-11-29 12:48:08 +0100

    python311Packages.bellows: 0.36.8 -> 0.37.1
    https://github.com/zigpy/bellows/releases/tag/0.37.1

--- pkgs/development/python-modules/bellows/default.nix
+++ pkgs/development/python-modules/bellows/default.nix
@@ -9,14 +9,15 @@
 , pytest-timeout
 , pytestCheckHook
 , pythonOlder
+, setuptools
 , voluptuous
 , zigpy
 }:
 
 buildPythonPackage rec {
   pname = "bellows";
-  version = "0.36.8";
-  format = "setuptools";
+  version = "0.37.1";
+  pyproject = true;
 
   disabled = pythonOlder "3.8";
 
@@ -24,8 +25,18 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "bellows";
     rev = "refs/tags/${version}";
-    hash = "sha256-+p3As+fi6mw9i5q2klFTM9QQ2JoQarwrphc6tB6C94M=";
+    hash = "sha256-JkaCM9XOdGZE9/C2BUV3g9QL5+k/HlVYd4WsAMS2mgk=";
   };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace '"setuptools-git-versioning<2"' "" \
+      --replace 'dynamic = ["version"]' 'version = "${version}"'
+  '';
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     click
