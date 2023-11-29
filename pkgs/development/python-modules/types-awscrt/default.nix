3388c6de50ffda6ff6ab6d025e133e5db0e81f92
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-29 10:15:46 +0100

    python311Packages.types-awscrt: 0.19.13 -> 0.19.18
    Changelog: https://github.com/youtype/types-awscrt/releases/tag/0.19.18

--- pkgs/development/python-modules/types-awscrt/default.nix
+++ pkgs/development/python-modules/types-awscrt/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "types-awscrt";
-  version = "0.19.13";
+  version = "0.19.18";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "types_awscrt";
     inherit version;
-    hash = "sha256-N0erJxk0FN47IClSt0YiSYH58j9ysQFscSShN7rAD3s=";
+    hash = "sha256-T1pZyHwFgraev5lNYU4Pt2zC0KXsPzLk0dscCRhnzdM=";
   };
 
   nativeBuildInputs = [
