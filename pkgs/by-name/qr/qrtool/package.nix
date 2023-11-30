5f949409857cb5670dda29a3dcefa075dfea131d
Philip Taron <philip.taron@gmail.com>
2023-11-29 22:46:08 -0800

    qrtool: 0.8.4 -> 0.8.5
    I added `asciidoctor` to the nativeBuildInputs as I saw warnings that it
    wasn't found as I built the tool.
    
    I also stopped using `rec` syntax as the nixpkgs manual recommends
    [against it](https://nixos.org/manual/nixpkgs/unstable/#mkderivation-recursive-attributes).
    
    However, `rustPlatform.buildRustPackage` doesn't allow the use of
    `mkDerivation (finalAttrs: {` form, so it's just a simple `let` binding
    here.

--- pkgs/by-name/qr/qrtool/package.nix
+++ pkgs/by-name/qr/qrtool/package.nix
@@ -2,27 +2,31 @@
 , stdenv
 , fetchFromGitHub
 , rustPlatform
-}:
-
-rustPlatform.buildRustPackage rec {
-  pname = "qrtool";
-  version = "0.8.4";
+, asciidoctor
+}: let
+  name = "qrtool";
+  version = "0.8.5";
+in rustPlatform.buildRustPackage {
+  pname = name;
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "sorairolake";
-    repo = "qrtool";
+    repo = name;
     rev = "v${version}";
-    sha256 = "sha256-FoWUGhNfVILpYxmsnSzRIM1+R9/xFxCF7W1sdiHaAiA=";
+    sha256 = "sha256-jrvNZGO1VIDo6Mz3NKda1C7qZUtF9T00CAFK8yoGWjc=";
   };
 
-  cargoSha256 = "sha256-mtejnHCkN2krgFAneyyBpvbv5PZO3GigM2DJqrbHim4=";
+  cargoSha256 = "sha256-JOnvlabCr3fZsIIRc2qTjf50Ga83zL8Aoo2sqzMBs7g=";
+
+  nativeBuildInputs = [ asciidoctor ];
 
   meta = with lib; {
     maintainers = with maintainers; [ philiptaron ];
     description = "An utility for encoding or decoding QR code";
     license = licenses.asl20;
-    homepage = "https://sorairolake.github.io/qrtool/book/index.html";
-    changelog = "https://sorairolake.github.io/qrtool/book/changelog.html";
-    mainProgram = "qrtool";
+    homepage = "https://sorairolake.github.io/${name}/book/index.html";
+    changelog = "https://sorairolake.github.io/${name}/book/changelog.html";
+    mainProgram = name;
   };
 }
