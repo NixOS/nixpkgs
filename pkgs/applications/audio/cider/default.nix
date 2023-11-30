013ac7b26c12611b53fbc98f9d147d59fd30d72c
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-11-30 17:43:09 +0100

    cider: fix build

--- pkgs/applications/audio/cider/default.nix
+++ pkgs/applications/audio/cider/default.nix
@@ -12,8 +12,6 @@ appimageTools.wrapType2 rec {
   extraInstallCommands =
     let contents = appimageTools.extract { inherit pname version src; };
     in ''
-      mv $out/bin/${pname}-${version} $out/bin/${pname}
-
       install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
       substituteInPlace $out/share/applications/${pname}.desktop \
         --replace 'Exec=AppRun' 'Exec=${pname}'
