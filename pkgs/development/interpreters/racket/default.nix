b9032a99059db4a7027fe3ef16e898f6209f1007
Mario Rodas <marsam@users.noreply.github.com>
2023-11-30 04:20:00 +0000

    racket,racket-minimal: 8.11 -> 8.11.1
    Diff: https://github.com/racket/racket/compare/v8.11...v8.11.1
    
    Changelog: https://blog.racket-lang.org/2023/11/racket-v8-11-1.html

--- pkgs/development/interpreters/racket/default.nix
+++ pkgs/development/interpreters/racket/default.nix
@@ -50,7 +50,7 @@ in
 
 stdenv.mkDerivation rec {
   pname = "racket";
-  version = "8.11"; # always change at once with ./minimal.nix
+  version = "8.11.1"; # always change at once with ./minimal.nix
 
   src = (lib.makeOverridable ({ name, sha256 }:
     fetchurl {
@@ -59,7 +59,7 @@ stdenv.mkDerivation rec {
     }
   )) {
     name = "${pname}-${version}";
-    sha256 = "sha256-lIrXmTyTnk7n54zOvl7qN8CkbiKgYj66k3fgEolk93w=";
+    sha256 = "sha256-5ZqwMLkqeONYnsQFxdJfpRdojCCZAjO9aMs0Vo1lTAU=";
   };
 
   FONTCONFIG_FILE = fontsConf;
