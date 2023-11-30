e556de2b625e192ba769c8fbfb6aaf21b69b5ece
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-11-30 17:41:23 +0100

    anytype: fix build

--- pkgs/applications/misc/anytype/default.nix
+++ pkgs/applications/misc/anytype/default.nix
@@ -3,23 +3,20 @@
 let
   pname = "anytype";
   version = "0.35.25-beta";
-  name = "Anytype-${version}";
-  nameExecutable = pname;
   src = fetchurl {
-    url = "https://github.com/anyproto/anytype-ts/releases/download/v${version}/${name}.AppImage";
+    url = "https://github.com/anyproto/anytype-ts/releases/download/v${version}/Anytype-${version}.AppImage";
     name = "Anytype-${version}.AppImage";
     sha256 = "sha256-55VT4t/SNl13Gdh7LG4SqLN9uo0cYDu0LnPFIuZ7Sm0=";
   };
-  appimageContents = appimageTools.extractType2 { inherit name src; };
+  appimageContents = appimageTools.extractType2 { inherit pname version src; };
 in
 appimageTools.wrapType2 {
-  inherit name src;
+  inherit pname version src;
 
   extraPkgs = pkgs: (appimageTools.defaultFhsEnvArgs.multiPkgs pkgs)
     ++ [ pkgs.libsecret ];
 
   extraInstallCommands = ''
-    mv $out/bin/${name} $out/bin/${pname}
     source "${makeWrapper}/nix-support/setup-hook"
     wrapProgram $out/bin/${pname} \
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
