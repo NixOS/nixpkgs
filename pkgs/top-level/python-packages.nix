27ef46821068e80b600cb8e4962346ac6e639578
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-30 21:30:09 +0100

    python311Packages.sigstore-rekor-types: init 0.0.11
    Python models for Rekor's API types
    
    https://github.com/trailofbits/sigstore-rekor-types

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12931,6 +12931,8 @@ self: super: with self; {
 
   sigrok = callPackage ../development/python-modules/sigrok { };
 
+  sigstore-rekor-types = callPackage ../development/python-modules/sigstore-rekor-types { };
+
   sigtools = callPackage ../development/python-modules/sigtools { };
 
   simanneal = callPackage ../development/python-modules/simanneal { };
