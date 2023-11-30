c7475ce01c634f884993e51fc34ee409b8008aa7
Fabian Affolter <mail@fabian-affolter.ch>
2023-11-30 21:44:05 +0100

    python311Packages.sigstore-protobuf-specs: init at 0.2.2
    Library for serializing and deserializing Sigstore messages
    
    https://pypi.org/project/sigstore-protobuf-specs/

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12911,6 +12911,8 @@ self: super: with self; {
 
   sigrok = callPackage ../development/python-modules/sigrok { };
 
+  sigstore-protobuf-specs = callPackage ../development/python-modules/sigstore-protobuf-specs { };
+
   sigstore-rekor-types = callPackage ../development/python-modules/sigstore-rekor-types { };
 
   sigtools = callPackage ../development/python-modules/sigtools { };
