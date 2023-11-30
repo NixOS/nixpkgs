44638da128c56a0bc7d5185b2b7d6a06c4e3c9ef
Sergei Trofimovich <slyich@gmail.com>
2023-11-30 08:34:26 +0000

    moreutils: 0.67 -> 0.68
    Citing changes from https://joeyh.name/code/moreutils/:
    
        popen: Use pclose, fixing compile warning. Thanks, Mikel Olasagasti Uranga
        vidir: Zero pad line numbers to work better when used with a small tab size such as 2. Thanks, Johan Grande
    
    Co-authored-by: kirillrdy <kirillrdy@gmail.com>

--- pkgs/tools/misc/moreutils/default.nix
+++ pkgs/tools/misc/moreutils/default.nix
@@ -9,16 +9,17 @@
 , makeWrapper
 , perl # for pod2man
 , darwin
+, gitUpdater
 }:
 
 stdenv.mkDerivation rec {
   pname = "moreutils";
-  version = "0.67";
+  version = "0.68";
 
   src = fetchgit {
     url = "git://git.joeyh.name/moreutils";
     rev = "refs/tags/${version}";
-    sha256 = "sha256-8Mu7L3KqOsW9OmidMkWB+q9TofHd1P1sbsNrtE4MUoA=";
+    hash = "sha256-kOY12oejH0xKaaPrKem+l0PACqyPqD4P1jEjOYfNntM=";
   };
 
   preBuild = ''
@@ -38,6 +39,11 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/chronic --prefix PERL5LIB : $PERL5LIB
     wrapProgram $out/bin/ts --prefix PERL5LIB : $PERL5LIB
   '';
+
+  passthru.updateScript = gitUpdater {
+    # No nicer place to find latest release.
+    url = "git://git.joeyh.name/moreutils";
+  };
 
   meta = with lib; {
     description = "Growing collection of the unix tools that nobody thought to write long ago when unix was young";
