c9988c22651bd7fb4b80fb1ab34ab56c9b83a241
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-11-30 17:43:17 +0100

    immersed-vr: fix build

--- pkgs/by-name/im/immersed-vr/package.nix
+++ pkgs/by-name/im/immersed-vr/package.nix
@@ -5,16 +5,11 @@
 appimageTools.wrapType2 rec {
   pname = "immersed-vr";
   version = "9.6";
-  name = "${pname}-${version}";
 
   src = fetchurl {
     url = "http://web.archive.org/web/20231011083250/https://static.immersed.com/dl/Immersed-x86_64.AppImage";
     hash = "sha256-iA0SQlPktETFXEqCbSoWV9NaWVahkPa6qO4Cfju0aBQ=";
   };
-
-  extraInstallCommands = ''
-    mv $out/bin/{${name},${pname}}
-  '';
 
   extraPkgs = pkgs: with pkgs; [
     libthai
