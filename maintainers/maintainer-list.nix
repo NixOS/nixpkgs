a4e8a2a6018dbf3cbab6a4d30a521fc4b9b768ed
RGBCube <RGBCube@users.noreply.github.com>
2023-11-30 22:44:42 +0300

    maintainers: add RGBCube

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15329,6 +15329,15 @@
     githubId = 44014925;
     name = "Rexx Larsson";
   };
+  RGBCube = {
+    name = "RGBCube";
+    email = "rgbsphere+nixpkgs@gmail.com";
+    github = "RGBCube";
+    githubId = 78925721;
+    keys = [{
+      fingerprint = "jPaU3Vpm/GN0tUuDg72n2+bd9dDMWe4bnKM325eP7eI";
+    }];
+  };
   rgnns = {
     email = "jglievano@gmail.com";
     github = "rgnns";
