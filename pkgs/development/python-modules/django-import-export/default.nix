712365d460697e95e316cb0f6c40b0a04afa882e
R. Ryantm <ryantm-bot@ryantm.com>
2023-11-30 21:52:27 +0000

    python310Packages.django-import-export: 3.3.1 -> 3.3.3

--- pkgs/development/python-modules/django-import-export/default.nix
+++ pkgs/development/python-modules/django-import-export/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "django-import-export";
-  version = "3.3.1";
+  version = "3.3.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "django-import-export";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-9eFBFTx+M28tbIKflldvDJW1po5ht/5FBX5ImfHGFuk=";
+    hash = "sha256-pV8esZBK1ne2Cl0NBejEi2wLu7HbqTnKG16MU/gl4Ig=";
   };
 
   propagatedBuildInputs = [
