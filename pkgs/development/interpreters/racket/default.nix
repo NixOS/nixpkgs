cda93e08b20fb49366798041cfdb8eb0dd72d38e
Mario Rodas <marsam@users.noreply.github.com>
2023-11-30 04:20:00 +0000

    racket: use hash instead of sha256 in fetchurl

--- pkgs/development/interpreters/racket/default.nix
+++ pkgs/development/interpreters/racket/default.nix
@@ -52,14 +52,14 @@ stdenv.mkDerivation rec {
   pname = "racket";
   version = "8.11.1"; # always change at once with ./minimal.nix
 
-  src = (lib.makeOverridable ({ name, sha256 }:
+  src = (lib.makeOverridable ({ name, hash }:
     fetchurl {
       url = "https://mirror.racket-lang.org/installers/${version}/${name}-src.tgz";
-      inherit sha256;
+      inherit hash;
     }
   )) {
     name = "${pname}-${version}";
-    sha256 = "sha256-5ZqwMLkqeONYnsQFxdJfpRdojCCZAjO9aMs0Vo1lTAU=";
+    hash = "sha256-5ZqwMLkqeONYnsQFxdJfpRdojCCZAjO9aMs0Vo1lTAU=";
   };
 
   FONTCONFIG_FILE = fontsConf;
