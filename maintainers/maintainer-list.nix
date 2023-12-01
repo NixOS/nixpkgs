642e05439052539ed9053775ae5044da4d481912
fortuneteller2k <lythe1107@gmail.com>
2023-12-02 02:07:44 +0800

    maintainers.moni: fix github id

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12162,7 +12162,7 @@
   moni = {
     email = "lythe1107@gmail.com";
     matrix = "@fortuneteller2k:matrix.org";
-    github = "moni";
+    github = "moni-dz";
     githubId = 20619776;
     name = "moni";
   };
