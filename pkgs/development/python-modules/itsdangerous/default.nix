8a92abdf3a2470d21dac2dffc73a0de46850b701
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-01 17:01:26 +0100

    python311Packages.itsdangerous: ignore deprecation warnings
    during check phase.

--- pkgs/development/python-modules/itsdangerous/default.nix
+++ pkgs/development/python-modules/itsdangerous/default.nix
@@ -21,6 +21,10 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
+  pytestFlagsArray = [
+    "-W" "ignore::DeprecationWarning"
+  ];
+
   meta = with lib; {
     description = "Safely pass data to untrusted environments and back";
     homepage = "https://itsdangerous.palletsprojects.com";
