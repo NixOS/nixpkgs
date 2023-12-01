27adb1a85f6a8ee581f47bab37a9c520a613eba7
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-01 21:03:24 +0100

    python311Packages.gehomesdk: 0.5.23 -> 0.5.25
    Changelog: https://github.com/simbaja/gehome/blob/master/CHANGELOG.md

--- pkgs/development/python-modules/gehomesdk/default.nix
+++ pkgs/development/python-modules/gehomesdk/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "gehomesdk";
-  version = "0.5.23";
+  version = "0.5.25";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-6Xk7wAF0bZrHriSyDMnPfaPRBiVinHawj3nEqpwbUmo=";
+    hash = "sha256-VQSefwzw4zA9ycO8723kBlMbtrOJxmKgZ8tfXZmtyQc=";
   };
 
   propagatedBuildInputs = [
