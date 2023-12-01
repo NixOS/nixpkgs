e3339debd3f47b19956c88d5df5b231f7d028c54
happysalada <raphael@megzari.com>
2023-12-02 08:48:41 +0900

    python311Packages.html2image: init at 2.0.4.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5122,6 +5122,8 @@ self: super: with self; {
 
   hstspreload = callPackage ../development/python-modules/hstspreload { };
 
+  html2image = callPackage ../development/python-modules/html2image { };
+
   html2text = callPackage ../development/python-modules/html2text { };
 
   html5lib = callPackage ../development/python-modules/html5lib { };
