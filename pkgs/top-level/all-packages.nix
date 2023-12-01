4562c6b3ebb2210f9af3ec60127824bbc44f90bf
Nick Cao <nickcao@nichi.co>
2023-12-01 10:28:47 -0500

    hydra_unstable: 2023-11-17 -> 2023-12-01
    Diff: https://github.com/NixOS/hydra/compare/8f48e4ddecbf403be35f8243b97d73cb39dd61bb...4d1c8505120961f10897b8fe9a070d4e193c9a13
    (cherry picked from commit b81808f053e5825fd88c931ca8ba2f553a1eaa42)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22145,7 +22145,7 @@ with pkgs;
 
   hwloc = callPackage ../development/libraries/hwloc { };
 
-  hydra_unstable = callPackage ../development/tools/misc/hydra/unstable.nix { nix = nixVersions.nix_2_17; };
+  hydra_unstable = callPackage ../development/tools/misc/hydra/unstable.nix { nix = nixVersions.nix_2_18; };
 
   hydra-cli = callPackage ../development/tools/misc/hydra-cli { };
 
