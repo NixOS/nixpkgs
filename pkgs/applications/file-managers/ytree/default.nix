bb595d0a8a9058e4140e7cc11254a13197f2f543
Anderson Torres <torres.anderson.85@protonmail.com>
2023-12-01 12:38:49 -0300

    ytree: use finalAttrs desin pattern
    Ans also a file patch instead of postPatch.

--- pkgs/applications/file-managers/ytree/default.nix
+++ pkgs/applications/file-managers/ytree/default.nix
@@ -1,40 +1,46 @@
-{ lib, stdenv
+{ lib
+, stdenv
 , fetchurl
 , ncurses
 , readline
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "ytree";
   version = "2.06";
 
   src = fetchurl {
-    url = "https://han.de/~werner/${pname}-${version}.tar.gz";
-    sha256 = "sha256-QRqI779ZnnytVUC7A7Zt0zyWexRwBnp+CVQcNvnvWeY=";
+    url = "https://han.de/~werner/ytree-${finalAttrs.version}.tar.gz";
+    hash = "sha256-QRqI779ZnnytVUC7A7Zt0zyWexRwBnp+CVQcNvnvWeY=";
   };
+
+  patches = [
+    # Two fixups (because diff files can't be smaller):
+    # - Create PREFIX instead of using DESTDIR
+    # - use gzip without timestamp, to improve reproducibility
+    ./0001-use-prefix-and-gzip-n.diff
+  ];
 
   buildInputs = [
     ncurses
     readline
   ];
 
-  # don't save timestamp, in order to improve reproducibility
-  postPatch = ''
-    substituteInPlace Makefile --replace 'gzip' 'gzip -n'
-  '';
+  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
 
-  installFlags = [ "DESTDIR=${placeholder "out"}" ];
+  installFlags = [ "PREFIX=${placeholder "out"}" ];
 
   preInstall = ''
     mkdir -p $out/bin $out/share/man/man1
   '';
 
-  meta = with lib; {
-    description = "A curses-based file manager similar to DOS Xtree(TM)";
+  meta = {
     homepage = "https://www.han.de/~werner/ytree.html";
-    license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ AndersonTorres ];
-    platforms = with platforms; unix;
+    description = "A curses-based file manager similar to DOS Xtree(TM)";
+    license = with lib.licenses; [ gpl2Plus ];
+    mainProgram = "ytree";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.unix;
   };
-}
+})
 # TODO: X11 support
