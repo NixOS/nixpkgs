44d6d9ff6a2d7ec65d0bd5174ee7bd09d94352f1
NinjaFB <oscar@oronberg.com>
2023-12-01 22:01:11 +1100

    kalgebra: init at 23.08.3

--- pkgs/applications/kde/default.nix
+++ pkgs/applications/kde/default.nix
@@ -101,6 +101,7 @@ let
       kaccounts-providers = callPackage ./kaccounts-providers.nix {};
       kaddressbook = callPackage ./kaddressbook.nix {};
       kalarm = callPackage ./kalarm.nix {};
+      kalgebra = callPackage ./kalgebra.nix {};
       merkuro = callPackage ./merkuro.nix {};
       kalzium = callPackage ./kalzium.nix {};
       kamoso = callPackage ./kamoso.nix {};
