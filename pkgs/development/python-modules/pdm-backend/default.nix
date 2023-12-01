e86b7137c47ac722cde25b0b8df9bc192edc4035
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-02 00:56:48 +0100

    python311Packages.pdm-backend: 2.1.6 -> 2.1.7
    https://github.com/pdm-project/pdm-backend/releases/tag/2.1.7

--- pkgs/development/python-modules/pdm-backend/default.nix
+++ pkgs/development/python-modules/pdm-backend/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "pdm-backend";
-  version = "2.1.6";
+  version = "2.1.7";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "pdm-project";
     repo = "pdm-backend";
     rev = "refs/tags/${version}";
-    hash = "sha256-ZEci8VeKYuORs9iAzaEqrtVBh9fMWHlLsFH1e5PRLwA=";
+    hash = "sha256-1YM/vba+8+2wKcWzPKzkpaWVmHqbFsYdhQSY/VBBAfo=";
   };
 
   env.PDM_BUILD_SCM_VERSION = version;
