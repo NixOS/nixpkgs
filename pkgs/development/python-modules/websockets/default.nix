6d59c51e880105824db7a95913268585daa44cf8
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:03:26 +0100

    python3Packages.websockets: 11.0.3 -> 12.0
    https://github.com/aaugustin/websockets/blob/12.0/docs/project/changelog.rst

--- pkgs/development/python-modules/websockets/default.nix
+++ pkgs/development/python-modules/websockets/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "websockets";
-  version = "11.0.3";
+  version = "12.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "aaugustin";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-RdkbIiZI/UYsWdnnl5gJPsnJ/6adfFtkiXC7MO/HwcI=";
+    hash = "sha256-sOL3VI9Ib/PncZs5KN4dAIHOrBc7LfXqT15LO4M6qKg=";
   };
 
   patchPhase = ''
