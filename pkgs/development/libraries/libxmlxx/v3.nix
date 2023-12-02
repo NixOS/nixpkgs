89119df973827a8e0ba5e39c10f400819e300611
Jan Tojnar <jtojnar@gmail.com>
2023-12-02 16:28:24 +0100

    libxmlxx3: 3.0.1 → 3.2.4
    https://github.com/libxmlplusplus/libxmlplusplus/blob/3.2.4/NEWS
    
    - Uses Meson by default now, Perl not needed.
    - Docs not installed since those are not part of the Meson-built tarball and building them would be pain.
    
    Also clean up the expression:
    
    - Format with nixpkgs-fmt.
    - Use finalAttrs pattern.
    - Update meta.website.
    - Propagate glibmm, since it is in Requires section of the pkg-config. The dependency will be dropped completely in libxml++ 5.x.

--- pkgs/development/libraries/libxmlxx/v3.nix
+++ pkgs/development/libraries/libxmlxx/v3.nix
@@ -1,41 +1,50 @@
-{ lib, stdenv, fetchurl, pkg-config, libxml2, glibmm, perl, gnome }:
+{ lib
+, stdenv
+, fetchurl
+, pkg-config
+, libxml2
+, glibmm
+, meson
+, ninja
+, gnome
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libxml++";
-  version = "3.0.1";
+  version = "3.2.4";
+
+  outputs = [ "out" "dev" ];
 
   src = fetchurl {
-    url = "mirror://gnome/sources/libxml++/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "19kik79fmg61nv0by0a5f9wchrcfjwzvih4v2waw01hqflhqvp0r";
+    url = "mirror://gnome/sources/libxml++/${lib.versions.majorMinor finalAttrs.version}/libxml++-${finalAttrs.version}.tar.xz";
+    hash = "sha256-ulP16spFt59OwbOyi8gTb84mhzzTjy44HZNVKJ5DJAU=";
   };
 
-  outputs = [ "out" "dev" "doc" "devdoc" ];
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    ninja
+  ];
 
-  nativeBuildInputs = [ pkg-config perl ];
-
-  buildInputs = [ glibmm ];
-
-  propagatedBuildInputs = [ libxml2 ];
-
-  postFixup = ''
-    substituteInPlace $dev/lib/pkgconfig/libxml++-3.0.pc \
-      --replace 'docdir=''${datarootdir}' "docdir=$doc/share"
-  '';
+  propagatedBuildInputs = [
+    libxml2
+    glibmm
+  ];
 
   passthru = {
     updateScript = gnome.updateScript {
       attrPath = "libxmlxx3";
-      packageName = pname;
+      packageName = "libxml++";
       versionPolicy = "odd-unstable";
       freeze = true;
     };
   };
 
   meta = with lib; {
-    homepage = "https://libxmlplusplus.sourceforge.net/";
+    homepage = "https://libxmlplusplus.github.io/libxmlplusplus/";
     description = "C++ wrapper for the libxml2 XML parser library, version 3";
     license = licenses.lgpl2Plus;
     platforms = platforms.unix;
     maintainers = with maintainers; [ loskutov ];
   };
-}
+})
