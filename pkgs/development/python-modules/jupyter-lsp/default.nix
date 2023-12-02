bed6037deaf209380e5527b19ac38f40010952a2
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:01:13 +0100

    python3Packages.jupyter-lsp: 2.2.0 -> 2.2.1

--- pkgs/development/python-modules/jupyter-lsp/default.nix
+++ pkgs/development/python-modules/jupyter-lsp/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "jupyter-lsp";
-  version = "2.2.0";
+  version = "2.2.1";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-jrvLUzrbQeXWNeuP6ClWsKr78P1EO2xL+pBu3uuGNaE=";
+    hash = "sha256-sX+rbXD+g8iJawz/WSN2QAOCR8GWBWtDaEoJArap4Ps=";
   };
 
   propagatedBuildInputs = [
