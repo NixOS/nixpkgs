d0295a191abd4c586b94e04c07a2a34549aa716b
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:02:30 +0100

    python3Packages.python-gitlab: 3.15.0 -> 4.2.0
    https://github.com/python-gitlab/python-gitlab/blob/v4.2.0/CHANGELOG.md

--- pkgs/development/python-modules/python-gitlab/default.nix
+++ pkgs/development/python-modules/python-gitlab/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "python-gitlab";
-  version = "3.15.0";
+  version = "4.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-yeZet2Eqn7uKvwM5ly7Kf9enPU2mbJtEb/5SiTCv9TQ=";
+    hash = "sha256-+HDXb5jJXQXEM5nXOx6qtZxnGzJ5ODDNL53fMsyTVB4=";
   };
 
   propagatedBuildInputs = [
