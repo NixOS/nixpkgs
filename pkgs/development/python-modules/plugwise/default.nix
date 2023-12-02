171c2d4389140dcea41e48cf5f1193141a1ede33
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-02 12:12:40 +0100

    python311Packages.plugwise: 0.35.2 -> 0.35.3
    Diff: https://github.com/plugwise/python-plugwise/compare/refs/tags/v0.35.2...v0.35.3
    
    Changelog: https://github.com/plugwise/python-plugwise/releases/tag/v0.35.3

--- pkgs/development/python-modules/plugwise/default.nix
+++ pkgs/development/python-modules/plugwise/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "plugwise";
-  version = "0.35.2";
+  version = "0.35.3";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = "python-plugwise";
     rev = "refs/tags/v${version}";
-    hash = "sha256-HgccU+wTl6ZhFEz+pUXgdYMWhsm+ZXpLkaz4ybaLZQc=";
+    hash = "sha256-DCG1sKpUUV2/2mVJ2ltCkzCxQxAkDtxzNX6uMSpJhi4=";
   };
 
   propagatedBuildInputs = [
