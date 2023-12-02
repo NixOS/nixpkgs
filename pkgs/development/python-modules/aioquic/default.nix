e9416149f5c3cfa49b7e1077cc7c5290e6119a4e
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-02 23:59:40 +0100

    python3Packages.aioquic: 0.9.21 -> 0.9.23

--- pkgs/development/python-modules/aioquic/default.nix
+++ pkgs/development/python-modules/aioquic/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "aioquic";
-  version = "0.9.21";
+  version = "0.9.23";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ecfsBjGOeFYnZlyk6HI63zR7ciW30AbjMtJXWh9RbvU=";
+    hash = "sha256-UsnaYO0IN/6LimoNfc8N++vsjpoCfhDr9yBPBWnFj6g=";
   };
 
   propagatedBuildInputs = [
