57af488098fefef4283b8115a904e022303f3aaa
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:02:03 +0100

    python3Packages.pybids: 0.16.3 -> 0.16.4

--- pkgs/development/python-modules/pybids/default.nix
+++ pkgs/development/python-modules/pybids/default.nix
@@ -18,12 +18,12 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.16.3";
+  version = "0.16.4";
   pname = "pybids";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-EOJ5NQyNFMpgLA1EaaXkv3/zk+hkPIMaVGrnNba4LMM=";
+    hash = "sha256-pahl8wi6Sf8AuVqkvi7H90ViHr+9utb14ZVmKK3rFm4=";
   };
 
   nativeBuildInputs = [ pythonRelaxDepsHook ];
