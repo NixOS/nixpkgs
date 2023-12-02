3ce3c44e1209b997930f931efb0f9ade1cbce197
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:01:01 +0100

    python3Packages.huggingface-hub: 0.18.0 -> 0.19.4
    https://github.com/huggingface/huggingface_hub/releases/tag/v0.19.4

--- pkgs/development/python-modules/huggingface-hub/default.nix
+++ pkgs/development/python-modules/huggingface-hub/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "huggingface-hub";
-  version = "0.18.0";
+  version = "0.19.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "huggingface";
     repo = "huggingface_hub";
     rev = "refs/tags/v${version}";
-    hash = "sha256-/KbD3TNSbQ9ueXYFLoXnIRIoi/y3l0w72GZ1+JC8ULk=";
+    hash = "sha256-bK/Cg+ZFhf9TrTVlDU35cLMDuTmdH4bN/QuPVeUVDsI=";
   };
 
   propagatedBuildInputs = [
