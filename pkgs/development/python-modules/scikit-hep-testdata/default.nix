106744a9e1e2a618239ec65a178ee59bc869d694
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:02:51 +0100

    python3Packages.scikit-hep-testdata: 0.4.34 -> 0.4.35
    https://github.com/scikit-hep/scikit-hep-testdata/releases/tag/v0.4.35

--- pkgs/development/python-modules/scikit-hep-testdata/default.nix
+++ pkgs/development/python-modules/scikit-hep-testdata/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "scikit-hep-testdata";
-  version = "0.4.34";
+  version = "0.4.35";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "scikit-hep";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-kHpJXqFQI3vtDJIcH2ebzbaReHecwItDh73/NcoPk9A=";
+    hash = "sha256-1SROsrl7zBaZRDju1M6wlKLZypk9OswA8kromiJGeqw=";
   };
 
   nativeBuildInputs = [
