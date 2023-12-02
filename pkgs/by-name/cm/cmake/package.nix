791b761c6181525b897e6f0f6f6e80d3fe462eca
Sergei Trofimovich <slyich@gmail.com>
2023-12-02 11:08:39 +0000

    cmake: 3.27.8 -> 3.27.9
    Changes: https://github.com/Kitware/CMake/compare/v3.27.8...v3.27.9

--- pkgs/by-name/cm/cmake/package.nix
+++ pkgs/by-name/cm/cmake/package.nix
@@ -46,11 +46,11 @@ stdenv.mkDerivation (finalAttrs: {
     + lib.optionalString isMinimalBuild "-minimal"
     + lib.optionalString cursesUI "-cursesUI"
     + lib.optionalString qt5UI "-qt5UI";
-  version = "3.27.8";
+  version = "3.27.9";
 
   src = fetchurl {
     url = "https://cmake.org/files/v${lib.versions.majorMinor finalAttrs.version}/cmake-${finalAttrs.version}.tar.gz";
-    hash = "sha256-/s4kVj9peHD7uYLqi/F0gsnV+FXYyb8LgkY9dsno0Mw=";
+    hash = "sha256-YJqbmFcqal6kd/kSz/uXMQntTQpqaz+eI1PSzcBIcI4=";
   };
 
   patches = [
