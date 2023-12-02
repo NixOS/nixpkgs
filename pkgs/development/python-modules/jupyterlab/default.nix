1c3fcc0661d70e295562cdb597f519ad9b112d20
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:01:12 +0100

    python3Packages.jupyterlab: 4.0.6 -> 4.0.9
    https://github.com/jupyterlab/jupyterlab/blob/v4.0.9/CHANGELOG.md

--- pkgs/development/python-modules/jupyterlab/default.nix
+++ pkgs/development/python-modules/jupyterlab/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "jupyterlab";
-  version = "4.0.6";
+  version = "4.0.9";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-bEOuWmof0v36/LNFQASVi95tp2Mxq7RM/8b55Daxm6E=";
+    hash = "sha256-nrraQdUmUfYjwMnwad24oh1oSOTIh9jl3cBhMWbtXAs=";
   };
 
   nativeBuildInputs = [
