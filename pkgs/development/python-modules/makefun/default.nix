c2b76120283ffcc5f85129cd840b6094902f6ce5
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:01:23 +0100

    python3Packages.makefun: 1.15.1 -> 1.15.2

--- pkgs/development/python-modules/makefun/default.nix
+++ pkgs/development/python-modules/makefun/default.nix
@@ -7,11 +7,11 @@
 
 buildPythonPackage rec {
   pname = "makefun";
-  version = "1.15.1";
+  version = "1.15.2";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-QLDxGLbe0NjXjHjx62ebi2skYuPBs+BfsbLajNRrSKU=";
+    hash = "sha256-FvKis02e4MK1eMlgoYCMl04oIs959um5xFWqzhCILUU=";
   };
 
   postPatch = ''
