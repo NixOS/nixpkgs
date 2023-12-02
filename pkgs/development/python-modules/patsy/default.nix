e52c52ae2b7ecf5af57a29c5b524f4723cf2cf2f
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:01:49 +0100

    python3Packages.patsy: 0.5.3 -> 0.5.4

--- pkgs/development/python-modules/patsy/default.nix
+++ pkgs/development/python-modules/patsy/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "patsy";
-  version = "0.5.3";
+  version = "0.5.4";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-vcGAAYdeMZvJHIEsHrahC+S7E8uB63Y/RmF53KO2cnc=";
+    hash = "sha256-favFJ1lzCN4OjxiPqiCvfgaom9qjBnVt/HeDaT6havQ=";
   };
 
   propagatedBuildInputs = [
