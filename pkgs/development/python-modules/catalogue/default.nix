1c6aad537e209a70aed36b21ceb137a2c72331be
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-02 23:59:59 +0100

    python3Packages.catalogue: 2.0.8 -> 2.1.0
    https://github.com/explosion/catalogue/releases/tag/v2.1.0

--- pkgs/development/python-modules/catalogue/default.nix
+++ pkgs/development/python-modules/catalogue/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "catalogue";
-  version = "2.0.8";
+  version = "2.1.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-syXHdlkgi/tq8bDZOxoapBEuG7KaTFztgWdYpyLw44g=";
+    hash = "sha256-oczi8XOKMd6yqiSz5Kieb7PoUlVcGDsOzaiivMHq7y4=";
   };
 
   propagatedBuildInputs = lib.optionals (pythonOlder "3.8") [
