449761cacae284e7e529f637d45135fddb37c23b
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:01:36 +0100

    python3Packages.numexpr: 2.8.6 -> 2.8.7

--- pkgs/development/python-modules/numexpr/default.nix
+++ pkgs/development/python-modules/numexpr/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "numexpr";
-  version = "2.8.6";
+  version = "2.8.7";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Yzb426P0VuQaT/w8l+tj2JxzWJ/24XBxQSJLkwJjJg0=";
+    hash = "sha256-WW7rO7/ryRL0tuqvhCthunIs69uLxC3++mV9OnSVOEk=";
   };
 
   nativeBuildInputs = [
