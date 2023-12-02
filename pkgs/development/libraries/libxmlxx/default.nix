e088a22ffd257b8f280c63becbaf17c7b8d2fc09
Jan Tojnar <jtojnar@gmail.com>
2023-12-02 17:26:13 +0100

    libxmlxx: Fix build with latest libxml2

--- pkgs/development/libraries/libxmlxx/default.nix
+++ pkgs/development/libraries/libxmlxx/default.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchurl
+, fetchpatch
 , pkg-config
 , libxml2
 , glibmm
@@ -19,6 +20,14 @@ stdenv.mkDerivation (finalAttrs: {
     url = "mirror://gnome/sources/libxml++/${lib.versions.majorMinor finalAttrs.version}/libxml++-${finalAttrs.version}.tar.xz";
     hash = "sha256-pDOYf1TMHsqoSvJq8EemLfnohFdODWhuXdxvcEQcFSs=";
   };
+
+  patches = [
+    # Fix compilation with libxml 2.12.
+    (fetchpatch {
+      url = "https://github.com/libxmlplusplus/libxmlplusplus/commit/1a5ec5cacf515bda4d655b2f20a65f4ca7e9b886.patch";
+      hash = "sha256-voGKykSjTUdNRNvMct0EI4ArANnyDXgyAgUM46sGbnk=";
+    })
+  ];
 
   nativeBuildInputs = [
     pkg-config
