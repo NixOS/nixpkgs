c9be599a34be43167bbf5d98d53ba6125071e8e9
Yueh-Shun Li <shamrocklee@posteo.net>
2023-12-02 20:42:30 +0800

    buildPython*: allow stdenv customization through <function>.override

--- pkgs/development/interpreters/python/python-packages-base.nix
+++ pkgs/development/interpreters/python/python-packages-base.nix
@@ -57,6 +57,13 @@ let
         (fdrv: decorate (f.override fdrv));
     };
 
+  compatCustomStdenv = f: origArgs: (
+    if origArgs?stdenv then
+      f.override { inherit (origArgs) stdenv; }
+    else
+      f
+  ) origArgs;
+
   mkPythonDerivation = if python.isPy3k then
     ./mk-python-derivation.nix
   else
@@ -69,6 +76,10 @@ let
     # This will most likely be shadowed by the typical <pkg>.override
     # added by another callPackage onto package definitions.
     lib.makeOverridable
+    # Take the obsolete input argument `stdenv`
+    # and pass as `buildPython*.override { stdenv = stdenv; }`
+    # TODO: Remove this after Nixpkgs 24.11 branch-off
+    compatCustomStdenv
     # Adds <pkg>.overridePythonAttrs
     makeOverridablePythonPackage
   ]);
@@ -76,11 +87,13 @@ let
   buildPythonPackage = decorateBuildPyhon (callPackage mkPythonDerivation {
     inherit namePrefix;     # We want Python libraries to be named like e.g. "python3.6-${name}"
     inherit toPythonModule; # Libraries provide modules
+    stdenv = python.stdenv; # Customizing stdenv through buildPythonPackage.override without rebuilding python
   });
 
   buildPythonApplication = decorateBuildPyhon (callPackage mkPythonDerivation {
     namePrefix = "";        # Python applications should not have any prefix
     toPythonModule = x: x;  # Application does not provide modules.
+    stdenv = python.stdenv; # Customizing stdenv through buildPythonApplication.override without rebuilding python
   });
 
   # See build-setupcfg/default.nix for documentation.
