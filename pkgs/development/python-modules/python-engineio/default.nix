b136d5d0063357589cf8b8cfbaef5613163f2926
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:02:30 +0100

    python3Packages.python-engineio: 4.7.1 -> 4.8.0
    https://github.com/miguelgrinberg/python-engineio/blob/v4.8.0/CHANGES.md

--- pkgs/development/python-modules/python-engineio/default.nix
+++ pkgs/development/python-modules/python-engineio/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "python-engineio";
-  version = "4.7.1";
+  version = "4.8.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "miguelgrinberg";
     repo = "python-engineio";
     rev = "refs/tags/v${version}";
-    hash = "sha256-jHXpPnrQlIpmQ2sY4y6AUx/6W8Pf+683s4NmmlwZO58=";
+    hash = "sha256-btXwx9GRLBcjtcGdgckb2Y/MxC0E/rKTWKgkP8olezo=";
   };
 
   propagatedBuildInputs = [
