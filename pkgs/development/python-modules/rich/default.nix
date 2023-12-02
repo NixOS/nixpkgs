484daf4220301aaf2adffe5a192fc8d50c4838dd
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:02:45 +0100

    python3Packages.rich: 13.5.2 -> 13.7.0
    https://github.com/Textualize/rich/blob/v13.7.0/CHANGELOG.md

--- pkgs/development/python-modules/rich/default.nix
+++ pkgs/development/python-modules/rich/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "rich";
-  version = "13.5.2";
+  version = "13.7.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "Textualize";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-ycDmFJa68OOrNqIy/hGKxbjoaIbiniiO4UAPNSyZvDk=";
+    hash = "sha256-L72an7vHC+aBj8NlLOjofDrQGvmFxJpdbfiEubfg0GM=";
   };
 
   nativeBuildInputs = [
