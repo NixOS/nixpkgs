265e94f488f64a5f0326758f68fe54e7c0e1a005
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:00:02 +0100

    python3Packages.clarifai-grpc: 9.10.6 -> 9.10.8
    https://github.com/Clarifai/clarifai-python-grpc/releases/tag/9.10.8

--- pkgs/development/python-modules/clarifai-grpc/default.nix
+++ pkgs/development/python-modules/clarifai-grpc/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "clarifai-grpc";
-  version = "9.10.6";
+  version = "9.10.8";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "Clarifai";
     repo = "clarifai-python-grpc";
     rev = "refs/tags/${version}";
-    hash = "sha256-OlSbeMBINN4gyFUklLh9zrQNv0VkRZxRwml4jbMjumE=";
+    hash = "sha256-jKmMS8WDia0lyFxSz06tv1H8S2AdCw842o4d5mOjS0M=";
   };
 
   nativeBuildInputs = [
