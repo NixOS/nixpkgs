a7e7a9ae1662710d6388609abafad1b7cccdaf71
Gustavo Coutinho de Souza <dev@onemoresuza.mailer.me>
2023-12-02 15:14:08 -0300

    hareThirdParty.hare-toml: init at 0.1.0

--- pkgs/top-level/hare-third-party.nix
+++ pkgs/top-level/hare-third-party.nix
@@ -5,8 +5,8 @@ let
   inherit (self) callPackage;
 in
 {
-
   hare-compress = callPackage ../development/hare-third-party/hare-compress { };
   hare-ev = callPackage ../development/hare-third-party/hare-ev { };
   hare-json = callPackage ../development/hare-third-party/hare-json { };
+  hare-toml = callPackage ../development/hare-third-party/hare-toml { };
 })
