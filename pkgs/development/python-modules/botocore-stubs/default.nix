95a9c8cdc6c12e659983dd707c727227968d4ae2
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-02 23:59:58 +0100

    python3Packages.botocore-stubs: 1.33.0 -> 1.33.6

--- pkgs/development/python-modules/botocore-stubs/default.nix
+++ pkgs/development/python-modules/botocore-stubs/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "botocore-stubs";
-  version = "1.33.0";
+  version = "1.33.6";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "botocore_stubs";
     inherit version;
-    hash = "sha256-9f8WY8hvxlPbFk6zYTH+80sOxa51hPWVZNpuqBA9IPQ=";
+    hash = "sha256-AuAy6jHxOLLQ8GVUOqXrOJcrgn+HGBBd4A9pPzRmLgQ=";
   };
 
   nativeBuildInputs = [
