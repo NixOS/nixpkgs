38ac493e24f3da091ea38a9b0c8bab524dcaefa9
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:03:24 +0100

    python3Packages.wandb: 0.15.11 -> 0.16.0
    https://github.com/wandb/wandb/raw/v0.16.0/CHANGELOG.md

--- pkgs/development/python-modules/wandb/default.nix
+++ pkgs/development/python-modules/wandb/default.nix
@@ -52,7 +52,7 @@
 
 buildPythonPackage rec {
   pname = "wandb";
-  version = "0.15.11";
+  version = "0.16.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.6";
@@ -61,7 +61,7 @@ buildPythonPackage rec {
     owner = pname;
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-WaVgyF+pQgFCqIsi5Tcu+btyUKU2e3/qJi4Ma8dnx8M=";
+    hash = "sha256-XXs9KjiAPzZ932r4UJ87RpM+qhg/bNDWEYsq2Ua6SRw=";
   };
 
   patches = [
