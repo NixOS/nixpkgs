32a84ba185a31033dc54a73fdf15fc7f7593198f
Jan Tojnar <jtojnar@gmail.com>
2023-12-02 19:29:58 +0100

    libcmis: Fix build with libxml2 2.12

--- pkgs/development/libraries/libcmis/default.nix
+++ pkgs/development/libraries/libcmis/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, boost, libxml2, pkg-config, docbook2x, curl, autoreconfHook, cppunit }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, boost, libxml2, pkg-config, docbook2x, curl, autoreconfHook, cppunit }:
 
 stdenv.mkDerivation rec {
   pname = "libcmis";
@@ -10,6 +10,14 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "sha256-HXiyQKjOlQXWABY10XrOiYxPqfpmUJC3a6xD98LIHDw=";
   };
+
+  patches = [
+    # Fix build with libxml 2.12
+    (fetchpatch {
+      url = "https://github.com/tdf/libcmis/commit/72618e63433c7243e4d9e79a638f19a09402eecc.patch";
+      hash = "sha256-pRXoNxlVC8Uw8/2+Y+994mBNyRq9zzjSJhkpiZur+Xw=";
+    })
+  ];
 
   nativeBuildInputs = [ autoreconfHook pkg-config docbook2x ];
   buildInputs = [ boost libxml2 curl cppunit ];
