f71e0a117118b44214a3450ad4024d55c7a864d0
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:02:29 +0100

    python3Packages.python-dbusmock: 0.29.1 -> 0.30.0

--- pkgs/development/python-modules/python-dbusmock/default.nix
+++ pkgs/development/python-modules/python-dbusmock/default.nix
@@ -20,13 +20,13 @@ let
   '';
 in buildPythonPackage rec {
   pname = "python-dbusmock";
-  version = "0.29.1";
+  version = "0.30.0";
 
   src = fetchFromGitHub {
     owner = "martinpitt";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-sfvVLPTSTXjwyB0a2NyDIONv01FXZ40nHZwwo3oqI90=";
+    hash = "sha256-jYlVbxeBXEmKiVvkFGdmChETCKUHllQG53VW4tTa0Tc=";
   };
 
   nativeBuildInputs = [
