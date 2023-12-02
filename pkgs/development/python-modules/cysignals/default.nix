453398a89a31e96a0228b6395dab1c7a7d2fba8c
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:00:12 +0100

    python3Packages.cysignals: 1.11.2 -> 1.11.4

--- pkgs/development/python-modules/cysignals/default.nix
+++ pkgs/development/python-modules/cysignals/default.nix
@@ -10,11 +10,11 @@ assert pariSupport -> pari != null;
 
 buildPythonPackage rec {
   pname = "cysignals";
-  version = "1.11.2";
+  version = "1.11.4";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "5858b1760fbe21848121b826b2463a67ac5a45caf3d73105497a68618c5a6fa6";
+    sha256 = "sha256-Dx4yHlWgf5AchqNqHkSX9v+d/nAGgdATCjjDbk6yOMM=";
   };
 
   # explicit check:
