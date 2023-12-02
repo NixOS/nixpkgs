ec934e0c647c207766d849d8c6b3285bcf8d66c8
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-03 00:02:25 +0100

    python3Packages.pytest-httpx: 0.25.0 -> 0.27.0
    https://github.com/Colin-b/pytest_httpx/blob/v0.27.0/CHANGELOG.md

--- pkgs/development/python-modules/pytest-httpx/default.nix
+++ pkgs/development/python-modules/pytest-httpx/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "pytest-httpx";
-  version = "0.25.0";
+  version = "0.27.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "Colin-b";
     repo = "pytest_httpx";
     rev = "refs/tags/v${version}";
-    hash = "sha256-8cuXsHyS9OaYXzKXgwan+mBFocyr39B9G8wr8I1pmg4=";
+    hash = "sha256-5CDmIjehW9/aBxoFVbo8W2fAwgIrPPxEqHQjxsTPlpY=";
   };
 
   nativeBuildInputs = [
