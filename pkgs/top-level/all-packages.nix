646116bf6efdf4582d7dd883be0d902086fddff4
Adam Joseph <adam@westernsemico.com>
2023-12-02 17:28:00 -0800

    curaengine: disable warnings if gcc13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35424,12 +35424,12 @@ with pkgs;
 
   slic3r = callPackage ../applications/misc/slic3r { };
 
-  curaengine_stable = callPackage ../applications/misc/curaengine/stable.nix { };
+  curaengine_stable = disable-warnings-if-gcc13 (callPackage ../applications/misc/curaengine/stable.nix { });
 
-  curaengine = callPackage ../applications/misc/curaengine {
+  curaengine = disable-warnings-if-gcc13 (callPackage ../applications/misc/curaengine {
     inherit (python3.pkgs) libarcus;
     protobuf = protobuf_21;
-  };
+  });
 
   cura = libsForQt5.callPackage ../applications/misc/cura { };
 
