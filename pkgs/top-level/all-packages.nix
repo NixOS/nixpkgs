b899bb1bd15450152f6585ecbcb5b010b6736734
Adam Joseph <adam@westernsemico.com>
2023-12-03 12:36:21 -0800

    reproc: disable-warnings-if-gcc13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34322,7 +34322,7 @@ with pkgs;
 
   rep-gtk = callPackage ../development/libraries/rep-gtk { };
 
-  reproc = callPackage ../development/libraries/reproc { };
+  reproc = disable-warnings-if-gcc13 (callPackage ../development/libraries/reproc { });
 
   sawfish = callPackage ../applications/window-managers/sawfish { };
 
