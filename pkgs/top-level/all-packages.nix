5c037da467d1af4250381bf772126fa836dd644b
Adam Joseph <adam@westernsemico.com>
2023-12-03 12:35:29 -0800

    opencollada: disable-warnings-if-gcc13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24321,7 +24321,7 @@ with pkgs;
 
   opencl-clhpp = callPackage ../development/libraries/opencl-clhpp { };
 
-  opencollada = callPackage ../development/libraries/opencollada { };
+  opencollada = disable-warnings-if-gcc13 (callPackage ../development/libraries/opencollada { });
 
   opencore-amr = callPackage ../development/libraries/opencore-amr { };
 
