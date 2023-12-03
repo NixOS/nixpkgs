24c0a68e39fef69cd3d52169d9d38fb9cad1ff0b
Adam Joseph <adam@westernsemico.com>
2023-12-03 12:39:18 -0800

    python3Packages.fasttext: disable-warnings-if-gcc13

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3891,7 +3891,7 @@ self: super: with self; {
 
   fastrlock = callPackage ../development/python-modules/fastrlock { };
 
-  fasttext = callPackage ../development/python-modules/fasttext { };
+  fasttext = pkgs.disable-warnings-if-gcc13 (callPackage ../development/python-modules/fasttext { });
 
   fasttext-predict = callPackage ../development/python-modules/fasttext-predict { };
 
