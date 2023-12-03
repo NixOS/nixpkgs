3c5e88f539969670634cc48fea63c5633a2d7769
Sergei Trofimovich <slyich@gmail.com>
2023-12-03 16:54:24 +0000

    gjs: 1.78.0 -> 1.78.1
    Changes: https://gitlab.gnome.org/GNOME/gjs/-/compare/1.78.0...1.78.1?from_project_id=106&straight=false

--- pkgs/development/libraries/gjs/default.nix
+++ pkgs/development/libraries/gjs/default.nix
@@ -31,13 +31,13 @@ let
   ];
 in stdenv.mkDerivation rec {
   pname = "gjs";
-  version = "1.78.0";
+  version = "1.78.1";
 
   outputs = [ "out" "dev" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gjs/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-+6og4JF2aIMIAPkpUWiPn8CPASlq/9XNtLNfdQvifck=";
+    hash = "sha256-fpBRHEKRJ8OerABoxKyaNT335vu8ZG9fGOiWKILBhkE=";
   };
 
   patches = [
