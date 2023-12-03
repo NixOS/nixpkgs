e0952b38b3c1c5ab026339bd1eb7456704416cc2
Adam Joseph <adam@westernsemico.com>
2023-12-03 02:32:48 -0800

    waylandpp: pin back to gcc12Stdenv

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25617,9 +25617,10 @@ with pkgs;
 
   wayland-protocols = callPackage ../development/libraries/wayland/protocols.nix { };
 
-  waylandpp = disable-warnings-if-gcc13 (callPackage ../development/libraries/waylandpp {
+  waylandpp = callPackage ../development/libraries/waylandpp {
     graphviz = graphviz-nox;
-  });
+    stdenv = gcc12Stdenv;
+  };
 
   wcslib = callPackage ../development/libraries/science/astronomy/wcslib { };
 
