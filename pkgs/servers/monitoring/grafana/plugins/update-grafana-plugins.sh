c510ed9904a9c38c1450fe3888214418926eb28b
Bouke van der Bijl <i@bou.ke>
2023-12-04 15:24:47 +0100

    Convert to Nix

--- pkgs/servers/monitoring/grafana/plugins/update-grafana-plugins.sh
+++ pkgs/servers/monitoring/grafana/plugins/update-grafana-plugins.sh
@@ -3,34 +3,4 @@
 
 set -eu -o pipefail
 
-curl "https://grafana.com/api/plugins" | jq -r '
-{
-  "linux-amd64": "x86_64-linux",
-  "linux-arm64": "aarch64-linux",
-  "darwin-amd64": "x86_64-darwin",
-  "darwin-arm64": "aarch64-darwin"
-} as $arch_mapping | "{ callPackage }:
-rec {
-  inherit callPackage;
-
-  grafanaPlugin = callPackage ./grafana-plugin.nix { };
-
-  "
-+ (
-.items | sort_by(.slug) | map(.slug + " = grafanaPlugin {
-    pname = \"" + .slug + "\";
-    version = \"" + .version + "\";
-    zipHash = "+ (if .packages | has("any") then
-      "\"" + .packages.any.sha256 + "\""
-    else
-      "{" + (.packages | to_entries | map(.key as $key | if $arch_mapping | has($key) then
-        "\n      " + $arch_mapping[.key] + " = \"" + .value.sha256 + "\";"
-      else
-        ""
-      end) | join("")) + "\n    }"
-    end) +";
-    meta.description = \"" + .description + "\";
-  };") | join("\n  ")
-) + "
-}"
-' > plugins.nix
+curl "https://grafana.com/api/plugins" | jq .items > plugins.json
