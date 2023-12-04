76f9e71ce51ad3eaa4946dd3ca9c6ea00a41813a
Alyssa Ross <hi@alyssa.is>
2023-12-04 12:55:18 +0100

    rustc-wasm32: fix build
    rust.config was never intended to exist, and has been removed.
    
    Link: https://github.com/NixOS/nixpkgs/pull/271707#issuecomment-1838471170
    Fixes: 973120823b58 ("lib.systems.elaborate: fix passing `rust` (more) (#271707)")
    (cherry picked from commit a449e06d5fb1aa174ff5e6f7c429d2f02e1b5548)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16949,7 +16949,7 @@ with pkgs;
       targetPlatform = lib.systems.elaborate {
         # lib.systems.elaborate won't recognize "unknown" as the last component.
         config = "wasm32-unknown-wasi";
-        rust.config = "wasm32-unknown-unknown";
+        rust.rustcTarget = "wasm32-unknown-unknown";
       };
     };
   }).overrideAttrs (old: {
