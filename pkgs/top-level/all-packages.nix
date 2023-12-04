2b48fcf2a3c1d5b777e38664658f8341a36894ac
Adam Joseph <adam@westernsemico.com>
2023-12-04 13:07:25 -0800

    claws-mail: disable-warnings-if-gcc13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30929,7 +30929,7 @@ with pkgs;
 
   clapper = callPackage ../applications/video/clapper { };
 
-  claws-mail = callPackage ../applications/networking/mailreaders/claws-mail { };
+  claws-mail = disable-warnings-if-gcc13 (callPackage ../applications/networking/mailreaders/claws-mail { });
 
   cligh = python3Packages.callPackage ../development/tools/github/cligh { };
 
