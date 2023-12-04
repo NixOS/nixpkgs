db4ce465edd23222973fff494b0cc3a522d7a3dc
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2023-12-03 20:09:31 -0500

    handlr-regex: migrate to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33515,8 +33515,6 @@ with pkgs;
 
   handlr = callPackage ../tools/misc/handlr { };
 
-  handlr-regex = callPackage ../tools/misc/handlr-regex { };
-
   jftui = callPackage ../applications/video/jftui { };
 
   lime = callPackage ../development/libraries/lime { };
