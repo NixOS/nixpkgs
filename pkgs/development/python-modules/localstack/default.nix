40733bbe457736612a40107cadb6dae01adb0db4
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-04 01:48:51 +0000

    python310Packages.localstack: 3.0.0 -> 3.0.2

--- pkgs/development/python-modules/localstack/default.nix
+++ pkgs/development/python-modules/localstack/default.nix
@@ -20,13 +20,13 @@
 
 buildPythonPackage rec {
   pname = "localstack";
-  version = "3.0.0";
+  version = "3.0.2";
 
   src = fetchFromGitHub {
     owner = "localstack";
     repo = "localstack";
     rev = "refs/tags/v${version}";
-    hash = "sha256-N/Mc1bubCcq38VxUqkO9LGG25pEetEyJ+VJMdg/7hrU=";
+    hash = "sha256-HncD/lhYfBrqtXF8F1Gz7JqwrASoHbsXvp1HXM5rldw=";
   };
 
   postPatch = ''
