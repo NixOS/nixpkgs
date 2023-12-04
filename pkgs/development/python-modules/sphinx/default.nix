9ec4f90d9c63f6df78bada4296af915415918068
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-04 01:37:27 +0100

    python311Packages.sphinx: disable racy tests

--- pkgs/development/python-modules/sphinx/default.nix
+++ pkgs/development/python-modules/sphinx/default.nix
@@ -100,6 +100,11 @@ buildPythonPackage rec {
   disabledTests = [
     # requires network access
     "test_latex_images"
+    # racy
+    "test_defaults"
+    "test_check_link_response_only"
+    "test_anchors_ignored_for_url"
+    "test_autodoc_default_options"
   ] ++ lib.optionals isPyPy [
     # PyPy has not __builtins__ which get asserted
     # https://doc.pypy.org/en/latest/cpython_differences.html#miscellaneous
