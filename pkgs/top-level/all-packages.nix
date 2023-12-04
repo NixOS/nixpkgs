e962c0b593ce46eef5e374d31a2de14c779e11fe
Adam Joseph <adam@westernsemico.com>
2023-12-03 23:55:22 -0800

    qgrep: pin-to-gcc12-if-gcc13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12526,9 +12526,9 @@ with pkgs;
 
   qdigidoc = libsForQt5.callPackage ../tools/security/qdigidoc { } ;
 
-  qgrep = callPackage ../tools/text/qgrep {
+  qgrep = pin-to-gcc12-if-gcc13 (callPackage ../tools/text/qgrep {
     inherit (darwin.apple_sdk.frameworks) CoreServices CoreFoundation;
-  };
+  });
 
   qhull = callPackage ../development/libraries/qhull { };
 
