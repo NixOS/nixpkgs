a8f6fd870503839efaf2c2141478edab01159d39
Taeer Bar-Yam <taeer.bar-yam@tweag.io>
2023-12-06 11:43:36 -0500

    attempt to fix redpanda-server (WIP)

--- pkgs/servers/redpanda/seastar.nix
+++ pkgs/servers/redpanda/seastar.nix
@@ -12,7 +12,7 @@
 , liburing
 , libxfs
 , lksctp-tools
-, llvmPackages_14
+, llvmPackages_15
 , lz4
 , ninja
 , numactl
@@ -25,16 +25,18 @@
 }:
 let
   pname = "seastar";
-  version = "22.11.0";
+  # see redpanda/cmake/dependencies.cmake
+  version = "1e2ad26ac57c1130190f3f41237af0907aab17d8";
 in
-llvmPackages_14.stdenv.mkDerivation {
+llvmPackages_15.stdenv.mkDerivation {
   inherit pname version;
   strictDeps = true;
   src = fetchFromGitHub {
     owner = "redpanda-data";
     repo = "seastar";
-    rev = "30d3a28bde08d2228b4e560c173b89fdd94c3f05";
-    sha256 = "sha256-Xzu7AJMkvE++BGEqluod3fwMEIpDnbCczmlEad0/4v4=";
+    # 23.3.x is a branch; in nix we have to pin to a particular commit
+    rev = version; # "7ca1eaebf1fffed2858cec92b74aea1415c73913";
+    sha256 = "sha256-nGDw9FwasVfHc1RuBH29SR17x5uNS0CbBsDwOdUvH0s=";
   };
   nativeBuildInputs = [
     cmake
@@ -63,15 +65,27 @@ llvmPackages_14.stdenv.mkDerivation {
     valgrind
     yaml-cpp
   ];
-  patches = [
-    ./seastar-fixes.patch
-  ];
+  # patches = [
+  #   ./seastar-fixes.patch
+  # ];
   postPatch = ''
     patchShebangs ./scripts/seastar-json2code.py
   '';
   cmakeFlags = [
     "-DSeastar_EXCLUDE_DEMOS_FROM_ALL=ON"
     "-DSeastar_EXCLUDE_TESTS_FROM_ALL=ON"
+
+    # from redpanda/cmake/oss.cmake.in
+    # NOTE: redpanda expects to be building seastar itself, whereas we build it in a separate package.
+    "-DSeastar_CXX_FLAGS=-Wno-error"
+    "-DSeastar_DPDK=OFF"
+    "-DSeastar_APPS=OFF"
+    "-DSeastar_DEMOS=OFF"
+    "-DSeastar_DOCS=OFF"
+    "-DSeastar_TESTING=OFF"
+    "-DSeastar_API_LEVEL=6"
+    "-DSeastar_CXX_DIALECT=c++20"
+    # "-DSeastar_UNUSED_RESULT_ERROR=ON"
   ];
   doCheck = false;
   meta = with lib; {
