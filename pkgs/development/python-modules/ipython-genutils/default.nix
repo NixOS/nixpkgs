3381af65d3258bb02bf5787c8db8e8f61a1a4154
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-06 19:42:54 +0100

    python312Packages.ipython-genutils: disable
    > Vestigial IPython utilities: DO NOT USE
    
    https://github.com/ipython/ipython_genutils

--- pkgs/development/python-modules/ipython-genutils/default.nix
+++ pkgs/development/python-modules/ipython-genutils/default.nix
@@ -4,12 +4,16 @@
 , setuptools
 , nose
 , pytestCheckHook
+, pythonAtLeast
 }:
 
 buildPythonPackage rec {
   pname = "ipython-genutils";
   version = "0.2.0";
   pyproject = true;
+
+  # uses the imp module, upstream says "DO NOT USE"
+  disabled = pythonAtLeast "3.12";
 
   src = fetchPypi {
     pname = "ipython_genutils";
