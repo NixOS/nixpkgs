75cbf2f031338c8930bee795f5ef7a8bfabeab95
Martin Madsen <mj@blackbird.online>
2023-12-06 18:58:24 +0100

    napi-rs-cli: 2.12.0 -> 2.16.5
    Versions above 2.12 adds a number of new features and bugfixes, particularly
    https://github.com/napi-rs/napi-rs/commit/3d48d4464bd3b5c7866183bad007fcc9269d8721
    that ensures re-exported types are written to the type definition file.
    
    Changelog: https://github.com/napi-rs/napi-rs/compare/@napi-rs/cli@2.12.0...@napi-rs/cli@2.16.5

--- pkgs/development/tools/napi-rs-cli/default.nix
+++ pkgs/development/tools/napi-rs-cli/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "napi-rs-cli";
-  version = "2.12.0";
+  version = "2.16.5";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@napi-rs/cli/-/cli-${version}.tgz";
-    hash = "sha256-TGhPPv73tb3tr1cY9mUuN4FaVql5tGh436uJeTkbnJs=";
+    hash = "sha256-axNzUp25LD0mSMtMwp1U9VpNfgZlyvpOkFJm05of0nU=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
