546c660ff673bb4e95af205fcbdade43a6e9abbe
jopejoe1 <johannes@joens.email>
2023-12-06 19:26:04 +0100

    noto-fonts-emoji-blob-bin: move out of noto-fonts file

--- pkgs/data/fonts/noto-fonts/default.nix
+++ pkgs/data/fonts/noto-fonts/default.nix
@@ -270,36 +270,4 @@ rec {
         sourceProvenance = [ sourceTypes.binaryBytecode ];
       };
     };
-
-  noto-fonts-emoji-blob-bin =
-    let
-      pname = "noto-fonts-emoji-blob-bin";
-      version = "15.0";
-    in
-    stdenvNoCC.mkDerivation {
-      inherit pname version;
-
-      src = fetchurl {
-        url = "https://github.com/C1710/blobmoji/releases/download/v${version}/Blobmoji.ttf";
-        hash = "sha256-3MPWZ1A2ups171dNIiFTJ3C1vZiGy6I8ZF70aUfrePk=";
-      };
-
-      dontUnpack = true;
-
-      installPhase = ''
-        runHook preInstall
-
-        install -Dm 444 $src $out/share/fonts/blobmoji/Blobmoji.ttf
-
-        runHook postInstall
-      '';
-
-      meta = with lib; {
-        description = "Noto Emoji with extended Blob support";
-        homepage = "https://github.com/C1710/blobmoji";
-        license = with licenses; [ ofl asl20 ];
-        platforms = platforms.all;
-        maintainers = with maintainers; [ rileyinman jk ];
-      };
-    };
 }
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29754,7 +29754,6 @@ with pkgs;
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     noto-fonts-color-emoji
-    noto-fonts-emoji-blob-bin
     noto-fonts-monochrome-emoji
     ;
 
