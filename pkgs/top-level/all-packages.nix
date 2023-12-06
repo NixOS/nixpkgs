1f8d62a69936dd899d0e7c955537b2f4ca5bc623
Jörg Thalheim <joerg@thalheim.io>
2023-12-06 09:46:58 +0100

    nix-eval-jobs: 2.18.0 -> 2.19.0
    Diff: https://github.com/nix-community/nix-eval-jobs/compare/v2.18.0...v2.19.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -40732,7 +40732,7 @@ with pkgs;
   dnadd = callPackage ../tools/nix/dnadd { };
 
   nix-eval-jobs = callPackage ../tools/package-management/nix-eval-jobs {
-    nix = nixVersions.nix_2_18;
+    nix = nixVersions.nix_2_19;
   };
 
   nix-doc = callPackage ../tools/package-management/nix-doc { };
