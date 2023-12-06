f593020c9865a92dc1d5347ec51295aca825869a
jopejoe1 <johannes@joens.email>
2023-12-06 19:37:56 +0100

    noto-fonts-monochrome-emoji: move out of noto-fonts file

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29742,7 +29742,6 @@ with pkgs;
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     noto-fonts-color-emoji
-    noto-fonts-monochrome-emoji
     ;
 
   nuclear = callPackage ../applications/audio/nuclear { };
