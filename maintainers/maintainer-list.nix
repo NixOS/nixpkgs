5f6ee819ca183e5fbd3ffa8aeaf4220e4b8bf78e
TornaxO7 <tornax@proton.me>
2023-12-06 17:16:43 +0100

    maintainers: add tornax

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18526,6 +18526,12 @@
     githubId = 1486805;
     name = "Toon Nolten";
   };
+  tornax = {
+    email = "tornax@pm.me";
+    github = "TornaxO7";
+    githubId = 50843046;
+    name = "tornax";
+  };
   toschmidt = {
     email = "tobias.schmidt@in.tum.de";
     github = "toschmidt";
