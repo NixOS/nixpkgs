30008fd748ea919310cc02818da4523046bc6d83
Anthony Roussel <anthony@roussel.dev>
2023-12-07 20:17:37 +0100

    pcsctools: add meta.{changelog,mainProgram}

--- pkgs/tools/security/pcsctools/default.nix
+++ pkgs/tools/security/pcsctools/default.nix
@@ -85,7 +85,9 @@ stdenv.mkDerivation (finalAttrs: {
   meta = with lib; {
     description = "Tools used to test a PC/SC driver, card or reader";
     homepage = "https://pcsc-tools.apdu.fr/";
+    changelog = "https://github.com/LudovicRousseau/pcsc-tools/releases/tag/${finalAttrs.version}";
     license = licenses.gpl2Plus;
+    mainProgram = "pcsc_scan";
     maintainers = with maintainers; [ peterhoeg ];
     platforms = platforms.linux;
   };
