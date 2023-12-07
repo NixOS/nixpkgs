db2e0448ba717c64a55a9d49c4d3bc2596a7e0ec
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-07 21:22:46 +0100

    python312Packages.zope_event: refactor

--- pkgs/development/python-modules/zope_event/default.nix
+++ pkgs/development/python-modules/zope_event/default.nix
@@ -1,22 +1,36 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
-  pname = "zope.event";
+  pname = "zope-event";
   version = "4.6";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "zope.event";
+    inherit version;
     hash = "sha256-gdmIEwRvyGzEE242mP7mKKMoL5wyDbGGWMIXSSNfzoA=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  pythonImportsCheck = [
+    "zope.event"
+  ];
+
   meta = with lib; {
     description = "An event publishing system";
-    homepage = "https://pypi.org/project/zope.event/";
+    homepage = "https://github.com/zopefoundation/zope.event";
+    changelog = "https://github.com/zopefoundation/zope.event/blob/${version}/CHANGES.rst";
     license = licenses.zpl20;
     maintainers = with maintainers; [ goibhniu ];
   };
-
 }
