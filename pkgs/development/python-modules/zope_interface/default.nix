6ecf767aa1bea3ccf29ca83c7e7e5ff66bbf2b43
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-07 21:49:49 +0100

    python311Packages.zope_interface: refactor

--- pkgs/development/python-modules/zope_interface/default.nix
+++ pkgs/development/python-modules/zope_interface/default.nix
@@ -1,26 +1,43 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
+, setuptools
 , zope_event
 }:
 
 buildPythonPackage rec {
   pname = "zope.interface";
   version = "5.5.2";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-v+4fP/YhQ4GUmeNI9bin86oCWfmspeDdrnOR0Fnc5nE=";
   };
 
-  propagatedBuildInputs = [ zope_event ];
+  nativeBuildInputs = [
+    setuptools
+  ];
 
-  doCheck = false; # Circular deps.
+  propagatedBuildInputs = [
+    zope_event
+  ];
+
+  # Circular dependency
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "zope.interface"
+  ];
 
   meta = with lib; {
     description = "Zope.Interface";
-    homepage = "https://zope.org/Products/ZopeInterface";
+    homepage = "https://github.com/zopefoundation/zope.interface";
+    changelog = "https://github.com/zopefoundation/zope.interface/blob/${version}/CHANGES.rst";
     license = licenses.zpl20;
-    maintainers = [ maintainers.goibhniu ];
+    maintainers = with maintainers; [ goibhniu ];
   };
 }
