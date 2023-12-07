0eb84808b9d0b12df65a548d8bb9bc2ea5799660
P. <d.ol.rod@tutanota.com>
2023-12-07 06:49:42 -0600

    indiepass-desktop: init at unstable-2023-05-19
    indigenous-desktop is now indiepass-desktop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32503,7 +32503,9 @@ with pkgs;
 
   hypnotix = callPackage ../applications/video/hypnotix { };
 
-  indigenous-desktop = callPackage ../applications/networking/feedreaders/indigenous-desktop { };
+  indiepass-desktop = callPackage ../by-name/in/indiepass-desktop/package.nix {
+    electron = electron_19;
+  };
 
   jackline = callPackage ../applications/networking/instant-messengers/jackline { };
 
