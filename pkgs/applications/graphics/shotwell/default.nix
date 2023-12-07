50fc7079fac05c00d2eb4c366784ef613109cf4a
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-07 15:38:07 +0000

    shotwell: 0.32.3 -> 0.32.4

--- pkgs/applications/graphics/shotwell/default.nix
+++ pkgs/applications/graphics/shotwell/default.nix
@@ -40,11 +40,11 @@
 
 stdenv.mkDerivation rec {
   pname = "shotwell";
-  version = "0.32.3";
+  version = "0.32.4";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-4AD+5bPzYseRFPDs/44is0yaKGW1nkGi2j5NxdLKLDw=";
+    sha256 = "sha256-3iqUUIRtHOwUxqEDA3X9SeGvJNySCtZIA0QST5zLhW8=";
   };
 
   nativeBuildInputs = [
