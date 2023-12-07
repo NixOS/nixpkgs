19ae7c4638f4ba7fde8431642046d5760c498c75
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-07 21:54:48 +0100

    python311Packages.zope_schema: refactor

--- pkgs/development/python-modules/zope_schema/default.nix
+++ pkgs/development/python-modules/zope_schema/default.nix
@@ -1,31 +1,50 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, zope_location
+, pythonOlder
+, setuptools
 , zope_event
 , zope_interface
+, zope_location
 , zope-testing
 }:
 
 buildPythonPackage rec {
   pname = "zope.schema";
   version = "7.0.1";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-6tTbywM1TU5BDJo7kERR60TZAlR1Gxy97fSmGu3p+7k=";
   };
 
-  propagatedBuildInputs = [ zope_location zope_event zope_interface zope-testing ];
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    zope_event
+    zope_interface
+    zope_location
+    zope-testing
+  ];
 
   # ImportError: No module named 'zope.event'
   # even though zope_event has been included.
   # Package seems to work fine.
   doCheck = false;
 
+  pythonImportsCheck = [
+    "zope.schema"
+  ];
+
   meta = with lib; {
-    homepage = "https://github.com/zopefoundation/zope.schema";
     description = "zope.interface extension for defining data schemas";
+    homepage = "https://github.com/zopefoundation/zope.schema";
+    changelog = "https://github.com/zopefoundation/zope.schema/blob/${version}/CHANGES.rst";
     license = licenses.zpl20;
     maintainers = with maintainers; [ goibhniu ];
   };
