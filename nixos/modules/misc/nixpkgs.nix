338f451606c807faf1e39d78217268aeb5a88ca2
Janne Heß <janne@hess.ooo>
2023-12-07 14:41:58 +0100

    nixos/nixpkgs: Validate nixpkgs config
    This uses nixpkgs config as a submodule rather than a custom merging
    function. This has the benefit of proper merge functions (like listOf)
    in configuration options rather than the `recursiveUpdate` with the
    hacked-in two lists of packages.

--- nixos/modules/misc/nixpkgs.nix
+++ nixos/modules/misc/nixpkgs.nix
@@ -1,46 +1,10 @@
-{ config, options, lib, pkgs, ... }:
+{ config, options, lib, ... }:
 
 with lib;
 
 let
   cfg = config.nixpkgs;
   opt = options.nixpkgs;
-
-  isConfig = x:
-    builtins.isAttrs x || lib.isFunction x;
-
-  optCall = f: x:
-    if lib.isFunction f
-    then f x
-    else f;
-
-  mergeConfig = lhs_: rhs_:
-    let
-      lhs = optCall lhs_ { inherit pkgs; };
-      rhs = optCall rhs_ { inherit pkgs; };
-    in
-    recursiveUpdate lhs rhs //
-    optionalAttrs (lhs ? packageOverrides) {
-      packageOverrides = pkgs:
-        optCall lhs.packageOverrides pkgs //
-        optCall (attrByPath [ "packageOverrides" ] { } rhs) pkgs;
-    } //
-    optionalAttrs (lhs ? perlPackageOverrides) {
-      perlPackageOverrides = pkgs:
-        optCall lhs.perlPackageOverrides pkgs //
-        optCall (attrByPath [ "perlPackageOverrides" ] { } rhs) pkgs;
-    };
-
-  configType = mkOptionType {
-    name = "nixpkgs-config";
-    description = "nixpkgs config";
-    check = x:
-      let traceXIfNot = c:
-            if c x then true
-            else lib.traceSeqN 1 x false;
-      in traceXIfNot isConfig;
-    merge = args: foldr (def: mergeConfig def.value) {};
-  };
 
   overlayType = mkOptionType {
     name = "nixpkgs-overlay";
@@ -151,7 +115,7 @@ in
         ''
           { allowBroken = true; allowUnfree = true; }
         '';
-      type = configType;
+      type = types.submodule ../../../pkgs/top-level/config.nix;
       description = lib.mdDoc ''
         The configuration of the Nix Packages collection.  (For
         details, see the Nixpkgs documentation.)  It allows you to set
