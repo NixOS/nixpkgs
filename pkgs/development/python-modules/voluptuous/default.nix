98ae8b62257c8d93ecfc7a7078bf224142ee3c39
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-07 07:14:31 +0100

    python311Packages.voluptuous: refactor
    - add changelog to meta
    - add format
    - disable on unsupported Python releases

--- pkgs/development/python-modules/voluptuous/default.nix
+++ pkgs/development/python-modules/voluptuous/default.nix
@@ -2,15 +2,19 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pytestCheckHook
+, pythonOlder
 }:
 
 buildPythonPackage rec {
   pname = "voluptuous";
   version = "0.14.1";
+  format = "setuptools";
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "alecthomas";
-    repo = pname;
+    repo = "voluptuous";
     rev = "refs/tags/${version}";
     hash = "sha256-7KXuypcKoqZboHTzoNKK5sYUR57wWGJu6y9zkLecep0=";
   };
@@ -30,6 +34,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python data validation library";
     homepage = "http://alecthomas.github.io/voluptuous/";
+    changelog = "https://github.com/alecthomas/voluptuous/blob/${version}/CHANGELOG.md";
     license = licenses.bsd3;
     maintainers = with maintainers; [ fab ];
   };
