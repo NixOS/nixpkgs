d62ee2774db62b321c35d5dc5d517b2d0161cf89
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-12-08 17:55:31 +0100

    python311Packages.xattr: drop support for Python 3.7

--- pkgs/development/python-modules/xattr/default.nix
+++ pkgs/development/python-modules/xattr/default.nix
@@ -11,7 +11,7 @@ buildPythonPackage rec {
   version = "1.0.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
