b9ae9e8afaa987d75a2105d30f9f7de1978b0b91
Julia DeMille <me@jdemille.com>
2023-12-08 10:41:48 -0600

    tmuxPlugins.sensible: unstable-2017-09-05 -> unstable-2022-08-14
    Signed-off-by: Julia DeMille <me@jdemille.com>

--- pkgs/misc/tmux-plugins/default.nix
+++ pkgs/misc/tmux-plugins/default.nix
@@ -533,12 +533,12 @@ in rec {
 
   sensible = mkTmuxPlugin {
     pluginName = "sensible";
-    version = "unstable-2017-09-05";
+    version = "unstable-2022-08-14";
     src = fetchFromGitHub {
       owner = "tmux-plugins";
       repo = "tmux-sensible";
-      rev = "e91b178ff832b7bcbbf4d99d9f467f63fd1b76b5";
-      sha256 = "1z8dfbwblrbmb8sgb0k8h1q0dvfdz7gw57las8nwd5gj6ss1jyvx";
+      rev = "25cb91f42d020f675bb0a2ce3fbd3a5d96119efa";
+      sha256 = "0y747qnryvlfch6kd37dwi1svmnnf5l4ijajckfnggz6ikan03xk";
     };
     postInstall = lib.optionalString stdenv.isDarwin ''
       sed -e 's:reattach-to-user-namespace:${pkgs.reattach-to-user-namespace}/bin/reattach-to-user-namespace:g' -i $target/sensible.tmux
