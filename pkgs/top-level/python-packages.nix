b64bda660592159bd6a053fdb1ba96b9d67f204c
Jann Marc Villablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-12-08 15:54:41 +0800

    python311Packages.fpdf2: init at 2.7.6 (#271076)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4261,6 +4261,8 @@ self: super: with self; {
 
   fpdf = callPackage ../development/python-modules/fpdf { };
 
+  fpdf2 = callPackage ../development/python-modules/fpdf2 { };
+
   fpylll = callPackage ../development/python-modules/fpylll { };
 
   fpyutils = callPackage ../development/python-modules/fpyutils { };
