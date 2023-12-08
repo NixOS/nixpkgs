8bb3431638026e47b32b9feaaada6c461a6bba4b
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-12-08 18:11:40 +0100

    python310Packages.flask-mailman: drop Python 3.6 support

--- pkgs/development/python-modules/flask-mailman/default.nix
+++ pkgs/development/python-modules/flask-mailman/default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   version = "1.0.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "waynerv";
