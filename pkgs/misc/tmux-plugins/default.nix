d098db74a731dc9b00ac4d648b1850e568d0c827
Julia DeMille <me@jdemille.com>
2023-12-08 10:41:01 -0600

    tmuxPlugins.resurrect: unstable-2022-05-01 -> unstable-2023-03-06
    Signed-off-by: Julia DeMille <me@jdemille.com>

--- pkgs/misc/tmux-plugins/default.nix
+++ pkgs/misc/tmux-plugins/default.nix
@@ -496,12 +496,12 @@ in rec {
 
   resurrect = mkTmuxPlugin {
     pluginName = "resurrect";
-    version = "unstable-2022-05-01";
+    version = "unstable-2023-03-06";
     src = fetchFromGitHub {
       owner = "tmux-plugins";
       repo = "tmux-resurrect";
-      rev = "ca6468e2deef11efadfe3a62832ae67742505432";
-      sha256 = "0d7jg5dy4jq64679rf2zqmqbvgiqvpcj5jxfljk7d7y86dnqhj3n";
+      rev = "cff343cf9e81983d3da0c8562b01616f12e8d548";
+      hash = "sha256-FcSjYyWjXM1B+WmiK2bqUNJYtH7sJBUsY2IjSur5TjY=";
     };
     meta = {
       homepage = "https://github.com/tmux-plugins/tmux-resurrect";
