551d523b3f5057e1b8538766448aea0d4c67ff92
networkException <git@nwex.de>
2023-12-09 00:24:55 +0100

    chromium: 119.0.6045.199 -> 120.0.6099.71
    https://chromereleases.googleblog.com/2023/12/stable-channel-update-for-desktop.html
    https://chromereleases.googleblog.com/2023/12/stable-channel-update-for-desktop_6.html
    
    This update includes 10 security fixes.
    
    CVEs:
    CVE-2023-6508 CVE-2023-6509 CVE-2023-6510 CVE-2023-6511 CVE-2023-6512
    
    Co-authored-by: emilylange <git@emilylange.de>
    (cherry picked from commit ca726d0a8a74bd04568357eee27e6d4ccbdd5733)

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -9,15 +9,15 @@
     };
     deps = {
       gn = {
-        hash = "sha256-4jWqtsOBh96xbYk1m06G9hj2eQwW6buUXsxWsa5W6/4=";
-        rev = "991530ce394efb58fcd848195469022fa17ae126";
+        hash = "sha256-dwluGOfq05swtBM5gg4a6gY3IpFHaKKkD0TV1XW7c7k=";
+        rev = "e4702d7409069c4f12d45ea7b7f0890717ca3f4b";
         url = "https://gn.googlesource.com/gn";
-        version = "2023-09-12";
+        version = "2023-10-23";
       };
     };
-    hash = "sha256-LqAORwZRyS9ASo0U+iVi9srEKYoSBG5upjqi5F65ITg=";
-    hash_deb_amd64 = "sha256-9nZjyJnXvOO1iZea3mdsj5FYkylrWnhColZ+q+X/xcU=";
-    version = "119.0.6045.199";
+    hash = "sha256-2IYdIhe299Fn5gtmLKxqIPqTYYEpCJqbXh3Vx8zN9Uo=";
+    hash_deb_amd64 = "sha256-xHwBLIU1QoDM0swG2DzRJ7BY9ESiqOrm4SwvK0mfIZc=";
+    version = "120.0.6099.71";
   };
   ungoogled-chromium = {
     deps = {
