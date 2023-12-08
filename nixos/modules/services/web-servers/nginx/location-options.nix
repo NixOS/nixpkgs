78541e68ebd88b29990296e9073e4b66f07e973d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2023-12-08 15:04:11 +0100

    nixos/nginx: allow return to be an int

--- nixos/modules/services/web-servers/nginx/location-options.nix
+++ nixos/modules/services/web-servers/nginx/location-options.nix
@@ -93,7 +93,7 @@ with lib;
     };
 
     return = mkOption {
-      type = types.nullOr types.str;
+      type = with types; nullOr (oneOf [ str int ]);
       default = null;
       example = "301 http://example.com$request_uri";
       description = lib.mdDoc ''
