61ce21686d0647a568b756a7819c241ac7d5f757
Davis Davalos-DeLosh <davis.davalos.delosh@gmail.com>
2023-12-08 13:44:59 -0700

    obsidian-export: init at 23.12.0

---
+++ pkgs/by-name/ob/obsidian-export/package.nix
@@ -0,0 +1,23 @@
+{ lib, rustPlatform, fetchFromGitHub }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "obsidian-export";
+  version = "23.12.0";
+
+  src = fetchFromGitHub {
+    owner = "zoni";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-r5G2XVV2F/Bt29gxuTZKX+KxH6RFa1hJNH3gSTi7yCU=";
+  };
+
+  cargoSha256 = "sha256-lkqoMFasHpfhmVd3dlYd/TKIBIDzqMbsxfigpeJq0w8=";
+
+  meta = with lib; {
+    description = "Rust library and CLI to export an Obsidian vault to regular Markdown";
+    homepage = "https://github.com/zoni/${pname}";
+    license = licenses.bsd2Patent;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ programmerino ];
+  };
+}
