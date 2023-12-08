cf73fb86712c2b68d261875c9fe306fbe409a08e
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-08 09:16:35 +0000

    xc: 0.5.0 -> 0.6.0

--- pkgs/development/tools/xc/default.nix
+++ pkgs/development/tools/xc/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "xc";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "joerdav";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-cVTa2ot95Hcm+1V1QXnlxSL9OjmoQNR9nVUgW/rZhl0=";
+    sha256 = "sha256-0Er8MqAqKCyz928bdbYRO3D9sGZ/JJBrCXhlq9M2dEA=";
   };
 
   vendorHash = "sha256-J4/a4ujM7A6bDwRlLCYt/PmJf6HZUmdYcJMux/3KyUI=";
