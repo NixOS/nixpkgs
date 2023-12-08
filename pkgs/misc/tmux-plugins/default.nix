27eb51fedc9ff3a1f20955d9874fec9c06cf1811
Julia DeMille <me@jdemille.com>
2023-12-08 09:46:29 -0600

    tmuxPlugins.battery: unstable-2019-07-04 -> unstable-2023-12-01
    Signed-off-by: Julia DeMille <me@jdemille.com>

--- pkgs/misc/tmux-plugins/default.nix
+++ pkgs/misc/tmux-plugins/default.nix
@@ -54,12 +54,12 @@ in rec {
 
   battery = mkTmuxPlugin {
     pluginName = "battery";
-    version = "unstable-2019-07-04";
+    version = "unstable-2023-12-01";
     src = fetchFromGitHub {
       owner = "tmux-plugins";
       repo = "tmux-battery";
-      rev = "f8b8e8451990365e0c98c38c184962e4f83b793b";
-      sha256 = "1bhdzsx3kdjqjmm1q4j8937lrpkzf71irr3fqhdbddsghwrrmwim";
+      rev = "48fae59ba4503cf345d25e4e66d79685aa3ceb75";
+      hash = "sha256-yKMTuRiNnGC3tAbZeWzMEudzhBSJhji8sZh96rFxpb8=";
     };
   };
 
