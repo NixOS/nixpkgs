7fd14af3865d391ef5e9a4be9832462308c4c969
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-08 03:22:01 +0000

    vivaldi: 6.4.3160.42 -> 6.4.3160.47

--- pkgs/applications/networking/browsers/vivaldi/default.nix
+++ pkgs/applications/networking/browsers/vivaldi/default.nix
@@ -24,7 +24,7 @@ let
   vivaldiName = if isSnapshot then "vivaldi-snapshot" else "vivaldi";
 in stdenv.mkDerivation rec {
   pname = "vivaldi";
-  version = "6.4.3160.42";
+  version = "6.4.3160.47";
 
   suffix = {
     aarch64-linux = "arm64";
@@ -34,8 +34,8 @@ in stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://downloads.vivaldi.com/${branch}/vivaldi-${branch}_${version}-1_${suffix}.deb";
     hash = {
-      aarch64-linux = "sha256-DQXlINbgZmYdmrp/VeWloWFk2REPMyWUaJkMVl0wDho=";
-      x86_64-linux = "sha256-udzdWNG0B9SidaOPsAOzkoviB3kwjaNCPZkSSIpLXNI=";
+      aarch64-linux = "sha256-hSc5ve5uPw7NseLzyDD+g6jocLDDHCY9pw1eJxOdlm8=";
+      x86_64-linux = "sha256-ELqpgGdsMSbGBGG/5mxHJM/9ASIoOKqo6PCIiERueqg=";
     }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
   };
 
