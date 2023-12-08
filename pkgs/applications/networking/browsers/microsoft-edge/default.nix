194e8b9d4c7ff41862faa93abd5131c26a3c0109
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-08 15:27:22 +0000

    microsoft-edge-beta: 120.0.2210.22 -> 120.0.2210.61

--- pkgs/applications/networking/browsers/microsoft-edge/default.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/default.nix
@@ -1,20 +1,20 @@
 {
+  dev = import ./browser.nix {
+    channel = "dev";
+    version = "121.0.2256.2";
+    revision = "1";
+    hash = "sha256-3QCyZVuXAUhaMOzD/JQ9ctKesilwyAn6NK6a1mxu3x4=";
+  };
   stable = import ./browser.nix {
     channel = "stable";
-    version = "119.0.2151.72";
+    version = "120.0.2210.61";
     revision = "1";
-    hash = "sha256-thBx/+6thNXXKppA11IOG5EiMx7pA9FA3vSkwa9+F0o=";
+    hash = "sha256-206Yk5CU3nL8cva0ZlK6aLraU8PURyeNL7vw90o3u0E=";
   };
   beta = import ./browser.nix {
     channel = "beta";
-    version = "120.0.2210.22";
+    version = "120.0.2210.61";
     revision = "1";
-    hash = "sha256-GayVVZbtGLQmmXu+k4wdsD+rPwGiSPHnQwzVYyKWhHM=";
-  };
-  dev = import ./browser.nix {
-    channel = "dev";
-    version = "121.0.2220.3";
-    revision = "1";
-    hash = "sha256-M3r+SLp3lQ7oWDYoM7aNZDC5wbMxFZggsu0Iuyyw/cw=";
+    hash = "sha256-XRO/Ry8zKFtBrDqbJsczmhi13XEmTdYMx4z/m/wWX8s=";
   };
 }
