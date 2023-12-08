92605d2155a8eda40f781dd65733a09957ea9b69
Sporesirius <sporesirius+git+public@pm.me>
2023-12-08 21:56:47 +0100

    alsa-ucm-conf: apply patch to fix SplitPCM: Device argument may not be set

--- pkgs/by-name/al/alsa-ucm-conf/package.nix
+++ pkgs/by-name/al/alsa-ucm-conf/package.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl }:
+{ lib, stdenv, fetchurl, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "alsa-ucm-conf";
@@ -8,6 +8,16 @@ stdenv.mkDerivation rec {
     url = "mirror://alsa/lib/${pname}-${version}.tar.bz2";
     hash = "sha256-nCHj8B/wC6p1jfF+hnzTbiTrtBpr7ElzfpkQXhbyrpc=";
   };
+
+  patches = [
+    (fetchpatch {
+      # ToDo: Remove this patch in the next package upgrade
+      # Fixes SplitPCM to make some audio devices work with alsa-ucm-conf v1.2.10 again
+      name = "alsa-ucm-conf-splitpcm-device-argument-fix.patch";
+      url = "https://github.com/alsa-project/alsa-ucm-conf/commit/b68aa52acdd2763fedad5eec0f435fbf43e5ccc6.patch";
+      hash = "sha256-8WE4+uhi4W7cCSZYmL7uFpcHJ9muX09UkGXyZIpEd9I=";
+    })
+  ];
 
   dontBuild = true;
 
