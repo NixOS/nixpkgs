1b39bfc9e3dabc1af75168f419e04df72f3b3d7d
Leon Barrett <Leon.Barrett@sony.com>
2023-12-09 14:33:53 -0800

    web-ext: fix build by setting NODE_ENV
    The [package docs
    say](https://github.com/mozilla/web-ext/blob/7.6.2/CONTRIBUTING.md#build-web-ext)
    to set `NODE_ENV=production` when running `npm build`.
    
    Without this change, when I run web-ext, it hits a branch that tries to
    import a dev dependency and fails with this error:
    
    ```
    Error [ERR_MODULE_NOT_FOUND]: Cannot find package 'git-rev-sync' ...
    ```
    
    With this change, web-ext runs as expected.

--- pkgs/development/tools/web-ext/default.nix
+++ pkgs/development/tools/web-ext/default.nix
@@ -16,6 +16,14 @@ buildNpmPackage rec {
 
   npmDepsHash = "sha256-KPBKUjCxva11w/E+Qhlx+1vikpCL7Hr9MiKenYHEVSU=";
 
+  # Set `NODE_ENV=production` as described in the docs at
+  # https://github.com/mozilla/web-ext/blob/7.6.2/CONTRIBUTING.md#build-web-ext
+  # (Otherwise, this branch
+  # https://github.com/mozilla/web-ext/blob/7.6.2/src/program.js#L396 will be
+  # reached without git-rev-sync being available, as it's just a dev
+  # dependency.)
+  preBuild = "export NODE_ENV=production";
+
   meta = {
     description = "A command line tool to help build, run, and test web extensions";
     homepage = "https://github.com/mozilla/web-ext";
