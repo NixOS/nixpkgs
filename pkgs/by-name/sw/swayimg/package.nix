31f223904d9aa30f0c052b301557775243a03278
Daniel Nagy <danielnagy@posteo.de>
2023-12-09 19:00:00 +0100

    swayimg: 1.11 -> 1.12

--- pkgs/by-name/sw/swayimg/package.nix
+++ pkgs/by-name/sw/swayimg/package.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, fetchpatch
 , meson
 , ninja
 , pkg-config
@@ -20,26 +19,19 @@
 , libpng
 , libjxl
 , libexif
+, openexr_3
 , bash-completion
 }:
 stdenv.mkDerivation rec {
   pname = "swayimg";
-  version = "1.11";
+  version = "1.12";
 
   src = fetchFromGitHub {
     owner = "artemsen";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-UwIufR3EwbpNVHD1GypV3qNgiqDRllwtxAM0CZPodn0=";
+    sha256 = "sha256-aKDt4lPh4w0AOucN7VrA7mo8SHI9eJqdrpJF+hG93gI=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "link-libwebp-1.3.1.patch";
-      url = "https://github.com/artemsen/swayimg/commit/bd3d6c838c699b876fd8c19b408c475eb47e17b6.patch";
-      hash = "sha256-2aMq/GTqyKw+CQr8o8ij4P4yNjBXYKXShQUknStUb5c=";
-    })
-  ];
 
   strictDeps = true;
 
@@ -65,6 +57,7 @@ stdenv.mkDerivation rec {
     libpng
     libjxl
     libexif
+    openexr_3
   ];
 
   meta = with lib; {
