dbc60b414172904b88478926e85abbac6aa6d439
notohh <github@notohh.dev>
2023-12-09 04:52:30 -0500

    spotify-player: 0.15.2 -> 0.16.1

--- pkgs/applications/audio/spotify-player/default.nix
+++ pkgs/applications/audio/spotify-player/default.nix
@@ -33,16 +33,16 @@ assert lib.assertOneOf "withAudioBackend" withAudioBackend [ "" "alsa" "pulseaud
 
 rustPlatform.buildRustPackage rec {
   pname = "spotify-player";
-  version = "0.15.2";
+  version = "0.16.1";
 
   src = fetchFromGitHub {
     owner = "aome510";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-yYn8xuJE0mILF7poiTbHCmFswP/xG+BbL+AASrLpbAs=";
+    hash = "sha256-wetodRX3bKU7DxDrBR5ordtIsO90u0W25UFV05gN7eQ=";
   };
 
-  cargoHash = "sha256-/q7xrsuRym5oDCGJRpBTdBach2CAbhCCC3cPFzCT4PU=";
+  cargoHash = "sha256-4J10XG+L/2CrHT2lzrwx/8jltFoLV7AXSFntRVNV/aQ=";
 
   nativeBuildInputs = [
     pkg-config
