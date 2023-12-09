78342bd72b56e97a7fdc62159ed1deb5e96b6b0c
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-09 20:56:22 +0100

    python311Packages.dynalite-devices: remove asynctest usage in testsuite

--- pkgs/development/python-modules/dynalite-devices/default.nix
+++ pkgs/development/python-modules/dynalite-devices/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, fetchpatch
 , pytest-asyncio
 , pytestCheckHook
 , pythonOlder
@@ -19,6 +20,14 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-kJo4e5vhgWzijLUhQd9VBVk1URpg9SXhOA60dJYashM=";
   };
+
+  patches = [
+    (fetchpatch {
+      # remove asynctest from tests
+      url = "https://github.com/ziv1234/python-dynalite-devices/commit/1729035f2b435b345b4e694aeae5d1823d732208.patch";
+      hash = "sha256-LqFXrJrZTPPjPnWT4+blHFYS3W1fD9T+iwaLUauxjNE=";
+    })
+  ];
 
   postPatch = ''
     sed -i '/^addopts/d' setup.cfg
