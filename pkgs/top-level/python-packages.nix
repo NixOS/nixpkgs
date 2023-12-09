7274a35eaa370e87451f7276747a6850aeb218dd
Isidor Zeuner <nix@quidecco.pl>
2023-12-09 20:03:14 +0100

    python.pkgs.trec-car-tools: init at 2.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14405,6 +14405,8 @@ self: super: with self; {
 
   transmissionrpc = callPackage ../development/python-modules/transmissionrpc { };
 
+  trec-car-tools = callPackage ../development/python-modules/trec-car-tools { };
+
   trectools = callPackage ../development/python-modules/trectools { };
 
   tree-sitter = callPackage ../development/python-modules/tree-sitter { };
