329abb1a5dde2a4b28025e835697c981904bee06
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-12-10 02:04:17 +0100

    houdini: 19.5.569 -> 20.0.506

--- pkgs/applications/misc/houdini/default.nix
+++ pkgs/applications/misc/houdini/default.nix
@@ -31,6 +31,7 @@ buildFHSEnv rec {
     ocl-icd  # needed for opencl
     numactl  # needed by hfs ocl backend
     ncurses5  # needed by hfs ocl backend
+    zstd  # needed from 20.0
   ] ++ (with xorg; [
     libICE
     libSM
