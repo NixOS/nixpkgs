51cc8fab4f6eb612ed577a66d1d6d4c673b1dbfe
Jan Tojnar <jtojnar@gmail.com>
2023-12-10 21:34:30 +0100

    openconnect: Fix build with libxml2 2.12

--- pkgs/tools/networking/openconnect/common.nix
+++ pkgs/tools/networking/openconnect/common.nix
@@ -4,6 +4,7 @@
 
 { lib
 , stdenv
+, fetchpatch
 , pkg-config
 , gnutls
 , p11-kit
@@ -23,6 +24,15 @@
 stdenv.mkDerivation rec {
   pname = "openconnect";
   inherit version src;
+
+  patches = [
+    # Fix build with libxml 2.12.
+    # https://gitlab.com/openconnect/openconnect/-/merge_requests/505
+    (fetchpatch {
+      url = "https://gitlab.com/openconnect/openconnect/-/commit/06af9454c29b3aff53aede30cfb15071be4ddc14.patch";
+      hash = "sha256-lPTgfj6b9Jv1P/Cs88PbDd0m0kSqG5Uce+5ea9vyDzQ=";
+    })
+  ];
 
   outputs = [ "out" "dev" ];
 
