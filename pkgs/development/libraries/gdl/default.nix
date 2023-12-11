f490b8dcc6789462550cd9e746289efe1866eb22
Jan Tojnar <jtojnar@gmail.com>
2023-12-11 02:14:50 +0100

    gdl: Fix build with libxml2 2.12

--- pkgs/development/libraries/gdl/default.nix
+++ pkgs/development/libraries/gdl/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, pkg-config, libxml2, gtk3, gnome, intltool }:
+{ lib, stdenv, fetchurl, fetchpatch, pkg-config, libxml2, gtk3, gnome, intltool }:
 
 stdenv.mkDerivation rec {
   pname = "gdl";
@@ -8,6 +8,15 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/gdl/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "NkHU/WadHhgYrv88+f+3iH/Fw2eFC3jCjHdeukq2pVU=";
   };
+
+  patches = [
+    # Fix build with libxml 2.12
+    # https://gitlab.gnome.org/GNOME/gdl/-/merge_requests/4
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gdl/-/commit/414f83eb4ad9e5576ee3d089594bf1301ff24091.patch";
+      hash = "sha256-Uy+WlTCxkq45Al8XqW8/gGl/PiVc7AOik1phmMBj4Vo=";
+    })
+  ];
 
   nativeBuildInputs = [ pkg-config intltool ];
   buildInputs = [ libxml2 gtk3 ];
