c83c4b57d57951a17f0def98f89a97ed4b6e7556
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-11 20:58:16 +0000

    filebot: 5.1.1 -> 5.1.2
    (cherry picked from commit 3145307809f845b3d1c940afc14b45cb61587818)

--- pkgs/applications/video/filebot/default.nix
+++ pkgs/applications/video/filebot/default.nix
@@ -9,11 +9,11 @@ let
   };
 in stdenv.mkDerivation (finalAttrs: {
   pname = "filebot";
-  version = "5.1.1";
+  version = "5.1.2";
 
   src = fetchurl {
     url = "https://web.archive.org/web/20230917142929/https://get.filebot.net/filebot/FileBot_${finalAttrs.version}/FileBot_${finalAttrs.version}-portable.tar.xz";
-    hash = "sha256-BCsZBRtT2Ka7WZw7WFnagwoJwIO1L3qpFk/6nlGdpmQ=";
+    hash = "sha256-+5I0t67asbCwaMCuqI/ixRHNAdcLTziuYOfepVThoPk=";
   };
 
   unpackPhase = "tar xvf $src";
