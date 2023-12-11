445c3d6857e2d7e17004dde19ee392da7dc5ddd8
Yuriy Taraday <yuriy.taraday@tweag.io>
2023-12-11 15:38:00 +0100

    discord-canary: 0.0.184 -> 0.0.213
    (cherry picked from commit 91976d3a92a6806a54b7e9779b52d40d76c83619)

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -4,7 +4,7 @@ let
     if stdenv.isLinux then {
       stable = "0.0.37";
       ptb = "0.0.59";
-      canary = "0.0.184";
+      canary = "0.0.213";
       development = "0.0.0";
     } else {
       stable = "0.0.284";
@@ -25,7 +25,7 @@ let
       };
       canary = fetchurl {
         url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-        hash = "sha256-Pu0kei/ls9yrDEpRQcgDAaEkRbYkFmp/jTwOkljoy18=";
+        hash = "sha256-DGRq58Xj5p/7BunY/vFds9LVmxYOl9LcF8ESHrCLly4=";
       };
       development = fetchurl {
         url = "https://dl-development.discordapp.net/apps/linux/${version}/discord-development-${version}.tar.gz";
