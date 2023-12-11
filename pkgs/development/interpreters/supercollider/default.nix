87b92d1962dcd788644cef733de3e277b2ba6554
mitchmindtree <mail@mitchellnordine.com>
2023-12-11 14:54:07 +1100

    supercollider: Enable darwin platform support
    This enables the `darwin` platform for the `supercollider` package,
    making the main `scsynth` and `sclang` CLI binaries available to macOS
    users. My main motivation is enabling darwin support in tidalcycles.nix
    
    https://github.com/mitchmindtree/tidalcycles.nix
    
    Currently IDE support is disabled as I've not yet been able to get
    it building and installing succesfully due to two issues.
    
    1. The build appears to inexplicably segfault at the
       `qt5_add_translation` step in `editors/sc-ide/CMakeLists.txt`. I
       haven't attempted to dig into this, but commenting this line out
       appears to get the `buildPhase` completing "successfully".
    2. The `installPhase` segfaults during the "Deploying additional Qt
       resources and libraries" step at the `DEPLOY_CMD` in the same
       file. This command invokes `macdeployqt` with the created
       `SuperCollider.app`, and immediately segfaults without explanation.
    
    I've no serious experience with Qt, let alone macOS Qt, and wasn't able
    to find any documentation on either of these issues, so for now I
    thought I'd just omit the IDE.
    
    My main motivation for enabling at least `sclang` and `scsynth` is to
    enable darwin support in `tidalcycles.nix`.

--- pkgs/development/interpreters/supercollider/default.nix
+++ pkgs/development/interpreters/supercollider/default.nix
@@ -1,6 +1,6 @@
-{ lib, stdenv, mkDerivation, fetchurl, cmake, runtimeShell
+{ lib, stdenv, mkDerivation, boost, fetchurl, cmake, runtimeShell
 , pkg-config, alsa-lib, libjack2, libsndfile, fftw
-, curl, gcc, libXt, qtbase, qttools, qtwebengine
+, curl, gcc, libXt, libyamlcpp, qtbase, qtmacextras, qttools, qtwebengine
 , readline, qtwebsockets, useSCEL ? false, emacs
 , gitUpdater, supercollider-with-plugins
 , supercolliderPlugins, writeText, runCommand
@@ -30,13 +30,21 @@ mkDerivation rec {
     ++ lib.optionals useSCEL [ emacs ];
 
   buildInputs = [ gcc libjack2 libsndfile fftw curl libXt qtbase qtwebengine qtwebsockets readline ]
-    ++ lib.optional (!stdenv.hostPlatform.isDarwin) alsa-lib;
+    ++ lib.optional (!stdenv.hostPlatform.isDarwin) alsa-lib
+    ++ lib.optionals (stdenv.hostPlatform.isDarwin) [ boost libyamlcpp qtmacextras ];
 
   hardeningDisable = [ "stackprotector" ];
 
   cmakeFlags = [
     "-DSC_WII=OFF"
     "-DSC_EL=${if useSCEL then "ON" else "OFF"}"
+  ] ++ lib.optionals stdenv.isDarwin [
+    "-DSYSTEM_BOOST=ON"
+    "-DSYSTEM_YAMLCPP=ON"
+    # FIXME: Enabling darwin support but with the IDE disabled due to
+    # `macdeployqt` segfault in `installPhase`.
+    # Remove this when attempting to get the IDE building on darwin.
+    "-DSC_IDE=OFF"
   ];
 
   passthru = {
@@ -67,12 +75,38 @@ mkDerivation rec {
     };
   };
 
+  # The darwin target doesn't install headers by default.
+  # We copy them during `preConfigure` as configure phase appears to rm them.
+  preConfigure = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    mkdir -p $out/include
+    cp -r include $out/include/SuperCollider
+    cp SCVersion.txt $out/include/SuperCollider
+  '';
+
+  postInstall = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    # Move the `.app` into the conventional `Applications` directory.
+    mkdir -p $out/Applications
+    mkdir -p $out/share/SuperCollider
+    mv $out/SuperCollider/SuperCollider.app $out/Applications
+    mv $out/SuperCollider/examples $out/share/SuperCollider
+    rm -r $out/SuperCollider
+
+    # Make the plugins available under `lib` (where `SC_PLUGIN_DIR` expects them).
+    mkdir -p $out/lib/SuperCollider/plugins
+    ln -s $out/Applications/SuperCollider.app/Contents/Resources/plugins/* $out/lib/SuperCollider/plugins/
+
+    # Expose the `sclang` and `scsynth` bins from the `.app` on darwin.
+    mkdir -p $out/bin
+    ln -s $out/Applications/SuperCollider.app/Contents/MacOS/sclang $out/bin/sclang
+    ln -s $out/Applications/SuperCollider.app/Contents/Resources/scsynth $out/bin/scsynth
+  '';
+
   meta = with lib; {
     description = "Programming language for real time audio synthesis";
     homepage = "https://supercollider.github.io";
     changelog = "https://github.com/supercollider/supercollider/blob/Version-${version}/CHANGELOG.md";
-    maintainers = [ ];
+    maintainers = with maintainers; [ mitchmindtree ];
     license = licenses.gpl3Plus;
-    platforms = platforms.linux;
+    platforms = with platforms; darwin ++ linux;
   };
 }
