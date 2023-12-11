a81c0c887bb921d81cbbd4916c3e226a654f964d
Kerstin Humm <kerstin@erictapen.name>
2023-12-11 02:33:20 +0100

    mobilizon: 4.0.0 -> 4.0.2

--- pkgs/servers/mobilizon/common.nix
+++ pkgs/servers/mobilizon/common.nix
@@ -1,19 +1,13 @@
-{ fetchFromGitLab, applyPatches }: rec {
+{ fetchFromGitLab }: rec {
 
   pname = "mobilizon";
-  version = "4.0.0";
+  version = "4.0.2";
 
-  src = applyPatches {
-    src = fetchFromGitLab {
-      domain = "framagit.org";
-      owner = "framasoft";
-      repo = pname;
-      rev = version;
-      sha256 = "sha256-PslcIS+HjGTx8UYhb7BG2OgLXfIWHDouuiogA/rq/7M=";
-    };
-    patches = [
-      # See https://framagit.org/framasoft/mobilizon/-/merge_requests/1452
-      ./cacerts_get.patch
-    ];
+  src = fetchFromGitLab {
+    domain = "framagit.org";
+    owner = "framasoft";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-Ri1qCiQaKlSTSSGWHzFqYBCoTEMtOtwe0Kli466dv4M=";
   };
 }
