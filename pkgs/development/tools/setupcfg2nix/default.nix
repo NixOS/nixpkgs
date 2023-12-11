6b9f45ae37450d224a89d072df672551ead5453a
adisbladis <adisbladis@gmail.com>
2023-12-12 12:05:34 +1300

    python3.pkgs.buildsetupcfg: Remove function from python-packages-base.nix
    This function is not, and never have been, used anywhere inside nixpkgs, outside of bootstrapping setupcfg2nix itself.
    It was added in https://github.com/NixOS/nixpkgs/pull/38778 by @shlevy.
    
    It has no out-of-tree users on Github either. External breakage is not expected.

--- pkgs/development/tools/setupcfg2nix/default.nix
+++ pkgs/development/tools/setupcfg2nix/default.nix
@@ -1,14 +1,17 @@
-{ buildSetupcfg, fetchFromGitHub, lib }:
+{ buildPythonApplication, fetchFromGitHub, lib, setuptools }:
 
-buildSetupcfg rec {
-  info = import ./info.nix;
+buildPythonApplication rec {
+  pname = "setupcfg2nix";
+  version = "2.0.1";
+  format = "setuptools";
+
   src = fetchFromGitHub {
     owner = "target";
     repo = "setupcfg2nix";
-    rev = info.version;
+    rev = version;
     sha256 = "1rj227vxybwp9acwnpwg9np964b1qcw2av3qmx00isnrw5vcps8m";
   };
-  application = true;
+
   meta = {
     description = "Generate nix expressions from setup.cfg for a python package";
     homepage = "https://github.com/target/setupcfg2nix";
