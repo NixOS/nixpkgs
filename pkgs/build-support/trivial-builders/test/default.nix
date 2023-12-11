507db5e1958bbebdb4106e2c8419f2090ad4f08d
Yueh-Shun Li <shamrocklee@posteo.net>
2023-12-11 04:19:44 +0000

    test.trivial-builders.references: Unify interface across systems
    Forward override interface and essential passthru attributes of
    `tests.trivial-builders.references` to non-Linux platforms.
    
    Enable `pkgs`-wide `samples` overriding via the override interface
    of `tests.trivial-builders.references` in Nixpkgs overlays.

--- pkgs/build-support/trivial-builders/test/default.nix
+++ pkgs/build-support/trivial-builders/test/default.nix
@@ -11,27 +11,47 @@
 
 */
 
-{ callPackage, lib, stdenv }:
+{ callPackage, lib, stdenv, tests }:
 let
   inherit (lib) recurseIntoAttrs;
   references = callPackage ./references {};
+  # To override the `samples` for all other test cases, pass the new
+  # `samples` to `references.override` in Nixpkgs overlays.
+  final-references = tests.trivial-builders.references;
 in
 recurseIntoAttrs {
   concat = callPackage ./concat-test.nix {};
   linkFarm = callPackage ./link-farm.nix {};
   overriding = callPackage ../test-overriding.nix {};
-  # VM test not supported beyond linux yet
+  # VM test not supported beyond linux yet, while non-Linux users can
+  # still run the test by executing `references.testScriptBin`, and
+  # access passthru attributes like `samples`, the way Linux users do.
+  # Use `references.override` to override these attributes.
   references =
-    if stdenv.hostPlatform.isLinux
-    then references
-    else {};
+    if stdenv.hostPlatform.isLinux then
+      references
+    else
+      lib.makeOverridable (lib.mirrorFunctionArgs references.override (referenceArgs:
+      let
+        references-overridden = references.override referenceArgs;
+      in
+      {
+        inherit (references-overridden)
+          samples
+          references
+          directReferences
+          testScriptBin
+        ;
+        meta = { inherit (references-overridden.meta) maintainers; };
+      }
+      )) { };
   writeCBin = callPackage ./writeCBin.nix {};
   writeShellApplication = callPackage ./writeShellApplication.nix {};
   writeScriptBin = callPackage ./writeScriptBin.nix {};
   writeShellScript = callPackage ./write-shell-script.nix {};
   writeShellScriptBin = callPackage ./writeShellScriptBin.nix {};
   writeStringReferencesToFile = callPackage ./writeStringReferencesToFile.nix {
-    inherit (references) samples;
+    inherit (final-references) samples;
   };
   writeTextFile = callPackage ./write-text-file.nix {};
 }
