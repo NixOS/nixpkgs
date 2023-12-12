969511fba74e62e0fc01719ce848b265eb6642ac
Arthur Noel <arthur@ischium.net>
2023-12-12 15:14:46 +0000

    tmuxPlugins.tmux-thumbs: 0.7.1 -> 0.8.0

--- pkgs/misc/tmux-plugins/tmux-thumbs/default.nix
+++ pkgs/misc/tmux-plugins/tmux-thumbs/default.nix
@@ -1,16 +1,11 @@
-{ lib, mkTmuxPlugin, fetchFromGitHub, thumbs, substituteAll }:
+{ mkTmuxPlugin, thumbs, substituteAll }:
 
-mkTmuxPlugin rec {
-  pluginName = "tmux-thumbs";
-  version = "0.7.1";
+mkTmuxPlugin {
+
+  inherit (thumbs) version src meta;
+
+  pluginName = thumbs.src.repo;
   rtpFilePath = "tmux-thumbs.tmux";
-
-  src = fetchFromGitHub {
-    owner = "fcsonline";
-    repo = pluginName;
-    rev = version;
-    sha256 = "sha256-PH1nscmVhxJFupS7dlbOb+qEwG/Pa/2P6XFIbR/cfaQ=";
-  };
 
   patches = [
     (substituteAll {
@@ -19,11 +14,4 @@ mkTmuxPlugin rec {
     })
   ];
 
-  meta = with lib; {
-    homepage = "https://github.com/fcsonline/tmux-thumbs";
-    description = "A lightning fast version of tmux-fingers written in Rust for copy pasting with vimium/vimperator like hints.";
-    license = licenses.mit;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ ghostbuster91 ];
-  };
 }
