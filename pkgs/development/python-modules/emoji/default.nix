4a5dbb0b8cf15c9090715e1ee9ba56c567cffa16
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-12 16:07:51 +0100

    python311Packages.emoji: 2.8.0 -> 2.9.0
    Diff: https://github.com/carpedm20/emoji/compare/refs/tags/v2.8.0...v2.9.0
    
    Changelog: https://github.com/carpedm20/emoji/blob/v2.9.0/CHANGES.md

--- pkgs/development/python-modules/emoji/default.nix
+++ pkgs/development/python-modules/emoji/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "emoji";
-  version = "2.8.0";
+  version = "2.9.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     owner = "carpedm20";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-fnVY4KwiqvSVYijlDckLq6qDrBJj/rJGMwaQ1mMygek=";
+    hash = "sha256-unPd0Z9hKN1mNsHleytYaKsnnMhpUn5K0ByH8vHc0AU=";
   };
 
   nativeCheckInputs = [
