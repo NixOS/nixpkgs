48aa4ec63d532b5f5f29c7fd440a9f125f487755
Jan Tojnar <jtojnar@gmail.com>
2023-12-13 07:59:38 +0100

    gtranslator: Fix build with libxml2 2.12

--- pkgs/tools/text/gtranslator/default.nix
+++ pkgs/tools/text/gtranslator/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchurl
+, fetchpatch
 , meson
 , ninja
 , pkg-config
@@ -28,6 +29,14 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
     sha256 = "MBAgTfXHpa4Cf1owsVRNaXfUF/Dku53il/CtGoAzGHM=";
   };
+
+  patches = [
+    # Fix build with libxml2 2.12
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/gtranslator/-/commit/db991f4c897f8a13bfb2db6f1b8bc60f39a98e4d.patch";
+      hash = "sha256-AFhxMXoJM/x6U92HzPP721vNIUQZPRQ0eEReZREmKiQ=";
+    })
+  ];
 
   nativeBuildInputs = [
     meson
