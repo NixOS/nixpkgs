2a8d453a2567184a07f3e7639c94d4e4b5eaae97
Arjun31415 <arjunp0710@gmail.com>
2023-12-13 08:56:18 +0530

    signal-desktop-beta: fix missing `callPackage`

--- pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/default.nix
@@ -2,5 +2,5 @@
   signal-desktop = if hostPlatform.system == "aarch64-linux"
     then callPackage ./signal-desktop-aarch64.nix { }
     else callPackage ./signal-desktop.nix { };
-  signal-desktop-beta = ./signal-desktop-beta.nix;
+  signal-desktop-beta = callPackage ./signal-desktop-beta.nix{ };
 }
