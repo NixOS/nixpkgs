3f8405670b19dc0b1feb305ed46bbf01f81e97e4
Rebecca Turner <rbt@sent.as>
2023-12-13 12:59:02 -0800

    lib: Update `modules.sh` test for Nix #9606
    https://github.com/NixOS/nix/pull/9606 changes how values are printed in
    some cases. This updates the lib tests to work with the new output
    format, which should be familiar to `nix repl` users.

--- lib/tests/modules.sh
+++ lib/tests/modules.sh
@@ -371,7 +371,7 @@ checkConfigOutput '^null$' config.value.null ./types-anything/equal-atoms.nix
 checkConfigOutput '^0.1$' config.value.float ./types-anything/equal-atoms.nix
 # Functions can't be merged together
 checkConfigError "The option .value.multiple-lambdas.<function body>. has conflicting option types" config.applied.multiple-lambdas ./types-anything/functions.nix
-checkConfigOutput '^<LAMBDA>$' config.value.single-lambda ./types-anything/functions.nix
+checkConfigOutput '^(<LAMBDA>|«lambda @ [^ ]+»)$' config.value.single-lambda ./types-anything/functions.nix
 checkConfigOutput '^null$' config.applied.merging-lambdas.x ./types-anything/functions.nix
 checkConfigOutput '^null$' config.applied.merging-lambdas.y ./types-anything/functions.nix
 # Check that all mk* modifiers are applied
@@ -407,7 +407,7 @@ checkConfigError 'The option .int.a. is used but not defined' config.int.a ./emp
 checkConfigError 'The option .nonEmptyList.a. is used but not defined' config.nonEmptyList.a ./emptyValues.nix
 
 ## types.raw
-checkConfigOutput "{ foo = <CODE>; }" config.unprocessedNesting ./raw.nix
+checkConfigOutput "{ foo = (<CODE>|«thunk»); }" config.unprocessedNesting ./raw.nix
 checkConfigOutput "10" config.processedToplevel ./raw.nix
 checkConfigError "The option .multiple. is defined multiple times" config.multiple ./raw.nix
 checkConfigOutput "bar" config.priorities ./raw.nix
