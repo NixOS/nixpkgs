8e4f8df6012c39fd0d4ecfd14c9fdf9d034db05a
Weijia Wang <9713184+wegank@users.noreply.github.com>
2023-12-13 14:41:53 +0100

    jetbrains.datagrip: fix build

--- pkgs/applications/editors/jetbrains/default.nix
+++ pkgs/applications/editors/jetbrains/default.nix
@@ -131,7 +131,7 @@ rec {
     '';
   });
 
-  datagrip = mkJetBrainsProduct { pname = "datagrip"; };
+  datagrip = mkJetBrainsProduct { pname = "datagrip"; extraBuildInputs = [ stdenv.cc.cc ]; };
 
   dataspell = let
     libr = runCommand "libR" {} ''
