82253e9862585899e564a58ce6e9a80f03127271
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-13 08:40:12 +0100

    python311Packages.google-cloud-iam: 2.12.2 -> 2.13.0
    Changelog: https://github.com/googleapis/python-iam/releases/tag/v2.13.0

--- pkgs/development/python-modules/google-cloud-iam/default.nix
+++ pkgs/development/python-modules/google-cloud-iam/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-iam";
-  version = "2.12.2";
+  version = "2.13.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-YDHQwZEfx5zguLuPb+FkUoO5wakYi0g9rmI7U7TYGBw=";
+    hash = "sha256-YCuUm9JJB0kjJ+aZpg3oaodA7qcKVrPrxiekZFoNfNI=";
   };
 
   propagatedBuildInputs = [
