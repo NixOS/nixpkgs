0d6c4e0b314d7a9297d56fcc3348b6613d110b9a
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-13 08:18:51 +0000

    boogie: 3.0.6 -> 3.0.9

--- pkgs/by-name/bo/boogie/package.nix
+++ pkgs/by-name/bo/boogie/package.nix
@@ -2,13 +2,13 @@
 
 buildDotnetModule rec {
   pname = "Boogie";
-  version = "3.0.6";
+  version = "3.0.9";
 
   src = fetchFromGitHub {
     owner = "boogie-org";
     repo = "boogie";
     rev = "v${version}";
-    sha256 = "sha256-A/nshihI1DxV0mvYYDLPWTNQkuduppxNC7OyWuGNCD8=";
+    sha256 = "sha256-8hujTiUO62yfv3T/Fu5unTuvJXLGgn9BXoCA2oag3jQ=";
   };
 
   projectFile = [ "Source/Boogie.sln" ];
