da2c7589a808cfa3cdf3bbb81f7d9c08591b2856
K900 <me@0upti.me>
2023-12-13 17:24:17 +0300

    path-of-building.data: 2.36.1 -> 2.37.0
    Diff: https://github.com/PathOfBuildingCommunity/PathOfBuilding/compare/v2.36.1...v2.37.0

--- pkgs/games/path-of-building/default.nix
+++ pkgs/games/path-of-building/default.nix
@@ -2,13 +2,13 @@
 let
   data = stdenv.mkDerivation(finalAttrs: {
     pname = "path-of-building-data";
-    version = "2.36.1";
+    version = "2.37.0";
 
     src = fetchFromGitHub {
       owner = "PathOfBuildingCommunity";
       repo = "PathOfBuilding";
       rev = "v${finalAttrs.version}";
-      hash = "sha256-lz0Nxn+HOFHcz/uKJZ3Jd7NDtkXmL7wLsD/W7Y3dKxQ=";
+      hash = "sha256-XoDnTmlONloBP60WNY1c6Mfscu2bG5dFh5qUtgqrfrg=";
     };
 
     nativeBuildInputs = [ unzip ];
