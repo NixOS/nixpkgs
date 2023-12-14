2d302087143feef97fb27fba96e2132d29d3082f
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2023-12-14 21:16:38 +0100

    linuxKernel.kernels.linux_zen: 6.6.6-zen1 -> 6.6.7-zen1

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -4,9 +4,9 @@ let
   # comments with variant added for update script
   # ./update-zen.py zen
   zenVariant = {
-    version = "6.6.6"; #zen
+    version = "6.6.7"; #zen
     suffix = "zen1"; #zen
-    sha256 = "13lxj1841mykfmbd8pwshr8jjxpxw1d8dyzkzq4ks6nviivnqfsn"; #zen
+    sha256 = "1fl8kfrxp9gnj7ngva1fq6sxmbpih6arl3ql0av775zi46kw0z7m"; #zen
     isLqx = false;
   };
   # ./update-zen.py lqx
