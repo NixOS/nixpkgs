NixOS/nixpkgs

commit 0a37316d6cfea44280f4470b6867a711a24606bd

treewide: use `mkPackageOption`
This commit replaces a lot of usages of `mkOption` with the package
type, to be `mkPackageOption`, in order to reduce the amount of code.

nixos/modules/services/web-apps/node-red.nix
@@ -19,12 +19,7 @@ in
   options.services.node-red = {
     enable = mkEnableOption (lib.mdDoc "the Node-RED service");
 
-    package = mkOption {
-      default = pkgs.nodePackages.node-red;
-      defaultText = literalExpression "pkgs.nodePackages.node-red";
-      type = types.package;
-      description = lib.mdDoc "Node-RED package to use.";
-    };
+    package = mkPackageOption pkgs "node-red" { };
 
     openFirewall = mkOption {
       type = types.bool;

commit 22bbd1834e2aed9c917c67d2181fe7cf225cd7a5

nixos/node-red: fix `cfg.package` default value
This fixes the following eval error:

```
error: nodePackages.node-red cannot be found in pkgs
```

when having `services.node-red.enable = true;` without specifying
`services.node-red.package`, just like the nixos VM test.

Follow-up of f509382c11ed445b52f249efa6e2fe66d1b56fa7, which attempted
to fix this as well.

Breakage introduced in 0a37316d6cfea44280f4470b6867a711a24606bd.

nixos/modules/services/web-apps/node-red.nix
@@ -19,7 +19,7 @@ in
   options.services.node-red = {
     enable = mkEnableOption (lib.mdDoc "the Node-RED service");
 
-    package = mkPackageOption pkgs "nodePackages.node-red" { };
+    package = mkPackageOption pkgs.nodePackages "node-red" { };
 
     openFirewall = mkOption {
       type = types.bool;