c6234da6986ff31c58c7dd86d7741a1cf2fa44d3
Philip Taron <philip.taron@gmail.com>
2023-12-13 22:59:42 -0800

    clasp-common-lisp: fetch dependencies in Nix
    I'm doing this as I wasn't able to reproduce the tarball that the
    previous derivation produced, and some of the repositories that were
    pinned no longer exist.

--- pkgs/development/compilers/clasp/default.nix
+++ pkgs/development/compilers/clasp/default.nix
@@ -16,6 +16,23 @@
 let
   inherit (llvmPackages_15) stdenv llvm libclang;
 
+  # Gathered from https://github.com/clasp-developers/clasp/raw/2.2.0/repos.sexp
+  dependencies = import ./dependencies.nix {
+    inherit fetchFromGitHub;
+  };
+
+  # Shortened version of `_defaultUnpack`
+  unpackDependency = elem: ''
+    mkdir -p "source/${elem.directory}"
+    cp -pr --reflink=auto -- ${elem.src}/* "source/${elem.directory}"
+    chmod -R u+w -- "source/${elem.directory}"
+  '';
+in
+
+stdenv.mkDerivation {
+  pname = "clasp";
+  version = "2.2.0";
+
   src = fetchFromGitHub {
     owner = "clasp-developers";
     repo = "clasp";
@@ -23,56 +40,9 @@ let
     hash = "sha256-gvUqUb0dftW1miiBcAPJur0wOunox4y2SUYeeJpR9R4=";
   };
 
-  reposDirs = [
-    "dependencies"
-    "src/lisp/kernel/contrib"
-    "src/lisp/modules/asdf"
-    "src/mps"
-    "src/bdwgc"
-    "src/libatomic_ops"
+  patches = [
+    ./clasp-pin-repos-commits.patch
   ];
-
-  reposTarball = stdenv.mkDerivation {
-    pname = "clasp-repos";
-    version = "tarball";
-
-    inherit src;
-
-    patches = [ ./clasp-pin-repos-commits.patch ];
-
-    nativeBuildInputs = [
-      sbcl
-      git
-      cacert
-    ];
-
-    buildPhase = ''
-      export SOURCE_DATE_EPOCH=1
-      export ASDF_OUTPUT_TRANSLATIONS=$(pwd):$(pwd)/__fasls
-      sbcl --script koga --help
-      for x in {${lib.concatStringsSep "," reposDirs}}; do
-        find $x -type d -name .git -exec rm -rvf {} \; || true
-      done
-    '';
-
-    installPhase = ''
-      tar --owner=0 --group=0 --numeric-owner --format=gnu \
-        --sort=name --mtime="@$SOURCE_DATE_EPOCH" \
-        -czf $out ${lib.concatStringsSep " " reposDirs}
-    '';
-
-    outputHashMode = "flat";
-    outputHashAlgo = "sha256";
-    outputHash = "sha256-vgwThjn2h3nKnShtKoHgaPdH/FDHv28fLMQvKFEwG6o=";
-  };
-
-in
-
-stdenv.mkDerivation {
-  pname = "clasp";
-  version = "2.2.0";
-
-  inherit src;
 
   nativeBuildInputs = [
     sbcl
@@ -88,10 +58,11 @@ stdenv.mkDerivation {
     libclang
   ];
 
+  postUnpack = lib.concatStringsSep "\n" (builtins.map unpackDependency dependencies);
+
   configurePhase = ''
     export SOURCE_DATE_EPOCH=1
     export ASDF_OUTPUT_TRANSLATIONS=$(pwd):$(pwd)/__fasls
-    tar xf ${reposTarball}
     sbcl --script koga \
       --skip-sync \
       --cc=$NIX_CC/bin/cc \
