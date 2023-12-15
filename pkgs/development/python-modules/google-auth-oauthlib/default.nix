a496f31a713f1a8b0ec0a7ff2c289c879ba1ae50
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-15 13:07:19 +0100

    python311Packages.google-auth-oauthlib: refactor

--- pkgs/development/python-modules/google-auth-oauthlib/default.nix
+++ pkgs/development/python-modules/google-auth-oauthlib/default.nix
@@ -8,12 +8,13 @@
 , google-auth
 , requests-oauthlib
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "google-auth-oauthlib";
   version = "1.2.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
@@ -22,16 +23,25 @@ buildPythonPackage rec {
     hash = "sha256-KS0tN4M0nysHNKCgIHseHjIqwZPCwJ2PfGE/t8xQHqg=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [
     google-auth
     requests-oauthlib
   ];
 
+  passthru.optional-dependencies = {
+    tool = [
+      click
+    ];
+  };
+
   nativeCheckInputs = [
-    click
     mock
     pytestCheckHook
-  ];
+  ] ++ lib.flatten (builtins.attrValues passthru.optional-dependencies);
 
   disabledTests = lib.optionals stdenv.isDarwin [
     # This test fails if the hostname is not associated with an IP (e.g., in `/etc/hosts`).
@@ -44,7 +54,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Google Authentication Library: oauthlib integration";
-    homepage = "https://github.com/GoogleCloudPlatform/google-auth-library-python-oauthlib";
+    homepage = "https://github.com/googleapis/google-auth-library-python-oauthlib";
+    changelog = "https://github.com/googleapis/google-auth-library-python-oauthlib/blob/v${version}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ terlar ];
   };
