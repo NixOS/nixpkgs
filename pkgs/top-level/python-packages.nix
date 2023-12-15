44e54cf37cb4af643ba37bd146217647874383d9
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-12-15 20:24:59 +0100

    python3Packages.pdoc-pyo3-sample-library: init at 1.0.11

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9028,6 +9028,8 @@ self: super: with self; {
 
   pdoc = callPackage ../development/python-modules/pdoc { };
 
+  pdoc-pyo3-sample-library = callPackage ../development/python-modules/pdoc-pyo3-sample-library { };
+
   pdoc3 = callPackage ../development/python-modules/pdoc3 { };
 
   peaqevcore = callPackage ../development/python-modules/peaqevcore { };
