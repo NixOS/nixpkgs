ba139707c9be53a0edc4647d26a739d8ceccf86a
Eden Krasnopolsky <35879078+edenkras@users.noreply.github.com>
2023-12-15 23:09:35 +0200

    Update pkgs/applications/misc/rofi-blocks/default.nix
    Co-authored-by: Anderson Torres <torres.anderson.85@protonmail.com>

--- pkgs/applications/misc/rofi-blocks/default.nix
+++ pkgs/applications/misc/rofi-blocks/default.nix
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
     owner = "OmarCastro";
     repo = "rofi-blocks";
     rev = "0a2ba561aa9a31586c0bc8203f8836a18a1f664e";
-    sha256 = "sha256-U955hzd55xiV5XdQ18iUIwNLn2JrvuHsItgUSf6ww58=";
+    hash = "sha256-U955hzd55xiV5XdQ18iUIwNLn2JrvuHsItgUSf6ww58=";
   };
 
   patches = [
