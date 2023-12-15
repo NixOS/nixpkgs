27d0568f4201cd356ed3009b5fa60b83298f8b54
teutat3s <10206665+teutat3s@users.noreply.github.com>
2023-12-15 15:39:56 +0100

    keycloak: 23.0.1 -> 23.0.3
    https://github.com/keycloak/keycloak/releases/tag/23.0.2
    https://github.com/keycloak/keycloak/compare/23.0.2...23.0.3

--- pkgs/servers/keycloak/default.nix
+++ pkgs/servers/keycloak/default.nix
@@ -18,11 +18,11 @@ let
   '';
 in stdenv.mkDerivation rec {
   pname = "keycloak";
-  version = "23.0.1";
+  version = "23.0.3";
 
   src = fetchzip {
     url = "https://github.com/keycloak/keycloak/releases/download/${version}/keycloak-${version}.zip";
-    hash = "sha256-N3lkJgy0DrHxiKCIYyAI9VOrwnd5au9mXIdZt46mT0s=";
+    hash = "sha256-5K8+pfn1zoXzBWJevZBx+9kZmefs1AvPoshOKP/dkNY=";
   };
 
   nativeBuildInputs = [ makeWrapper jre ];
