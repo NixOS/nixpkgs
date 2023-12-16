09cae608053e8509b39cbb90b4b84bac563babe6
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-16 04:06:17 +0000

    kotlin: 1.9.20 -> 1.9.21

--- pkgs/development/compilers/kotlin/default.nix
+++ pkgs/development/compilers/kotlin/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "kotlin";
-  version = "1.9.20";
+  version = "1.9.21";
 
   src = fetchurl {
     url = "https://github.com/JetBrains/kotlin/releases/download/v${version}/kotlin-compiler-${version}.zip";
-    sha256 = "sha256-Faiiglt0zPbETgTpdnLbgC0t91zi+7Y+8FOb865QBvA=";
+    sha256 = "sha256-zxfgJyvAZdSeZKhpU7c68GBlNwYp8JDVt8L+NTzPnBo=";
   };
 
   propagatedBuildInputs = [ jre ] ;
