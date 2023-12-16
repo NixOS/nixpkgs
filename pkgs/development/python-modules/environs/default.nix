2e6cb2571652ad9574a7513176682b16b5db97b5
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-16 14:22:55 +0100

    python311Packages.environs: refactor

--- pkgs/development/python-modules/environs/default.nix
+++ pkgs/development/python-modules/environs/default.nix
@@ -8,21 +8,26 @@
 , pytestCheckHook
 , python-dotenv
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "environs";
   version = "10.0.0";
-  format = "setuptools";
+  pyproject = true;
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "sloria";
-    repo = pname;
-    rev = version;
+    repo = "environs";
+    rev = "refs/tags/${version}";
     hash = "sha256-B/ICzopFuOAWMlDn950LXmi/XQaQxh5jFVmLdmt7Dlg=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     marshmallow
@@ -43,6 +48,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python modle for environment variable parsing";
     homepage = "https://github.com/sloria/environs";
+    changelog = "https://github.com/sloria/environs/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
