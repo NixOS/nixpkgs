d3ecf9002efc5bc2a2bd85edcce18107a3c5d1f2
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-12-16 19:33:47 +0100

    dnf4: wrap with plugins

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -619,7 +619,7 @@ with pkgs;
 
   dnf-plugins-core = with python3Packages; toPythonApplication dnf-plugins-core;
 
-  dnf4 =  with python3Packages; toPythonApplication dnf4;
+  dnf4 = python3Packages.callPackage ../development/python-modules/dnf4/wrapper.nix { };
 
   dnf5 = callPackage ../tools/package-management/dnf5 { };
 
