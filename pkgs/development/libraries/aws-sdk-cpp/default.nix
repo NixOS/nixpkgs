dbb7b67b569cf5149ca38347b6457806cfdb7c76
Nick Cao <nickcao@nichi.co>
2023-12-16 16:26:40 -0500

    aws-sdk-cpp: drop libatomic workaround for riscv

--- pkgs/development/libraries/aws-sdk-cpp/default.nix
+++ pkgs/development/libraries/aws-sdk-cpp/default.nix
@@ -78,8 +78,6 @@ stdenv.mkDerivation rec {
 
   # propagation is needed for Security.framework to be available when linking
   propagatedBuildInputs = [ aws-crt-cpp ];
-  # Ensure the linker is using atomic when compiling for RISC-V, otherwise fails
-  LDFLAGS = lib.optionalString stdenv.hostPlatform.isRiscV "-latomic";
 
   cmakeFlags = [
     "-DBUILD_DEPS=OFF"
