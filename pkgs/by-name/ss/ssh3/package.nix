d6b48c9529594ca23d836a76ae0aa1399f132643
Raito Bezarius <masterancpp@gmail.com>
2023-12-16 12:32:48 +0100

    ssh3: init at 0.1.7
    https://github.com/francoismichel/ssh3

---
+++ pkgs/by-name/ss/ssh3/package.nix
@@ -0,0 +1,33 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, libxcrypt
+}:
+
+buildGoModule rec {
+  pname = "ssh3";
+  version = "0.1.7";
+
+  src = fetchFromGitHub {
+    owner = "francoismichel";
+    repo = "ssh3";
+    rev = "v${version}";
+    hash = "sha256-ZtQAJwGvNlJWUoDa6bS3AEdM3zbNMPQGdaIhR+yIonw=";
+  };
+
+  buildInputs = [
+    libxcrypt
+  ];
+
+  vendorHash = "sha256-VUNvb7m1nnH+mXUsnIKyPKJEVSMXBAaS4ihi5DZeFiI=";
+
+  ldflags = [ "-s" "-w" ];
+
+  meta = with lib; {
+    description = "Revisit of the SSH protocol using QUIC + TLS 1.3 and HTTP authorization";
+    homepage = "https://github.com/francoismichel/ssh3";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ raitobezarius ];
+    mainProgram = "ssh3";
+  };
+}
