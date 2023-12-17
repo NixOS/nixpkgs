8f48be20e9aff8c7c46c35d8b741efa1c440037f
kashw2 <supra4keanu@hotmail.com>
2023-12-17 13:00:31 +1000

    enigma: clean up derivation
    # Conflicts:
    #	pkgs/by-name/en/enigma/package.nix

--- pkgs/by-name/en/enigma/package.nix
+++ pkgs/by-name/en/enigma/package.nix
@@ -1,5 +1,22 @@
-{ lib, stdenv, fetchurl, fetchpatch, makeWrapper, pkg-config, gettext, imagemagick, curl, libpng, SDL2, SDL2_image, SDL2_mixer, SDL2_ttf, xercesc, xdg-utils, hicolor-icon-theme }:
-stdenv.mkDerivation rec {
+{
+  lib,
+  stdenv,
+  fetchurl,
+  fetchpatch,
+  makeWrapper,
+  pkg-config,
+  gettext,
+  imagemagick,
+  curl,
+  libpng,
+  SDL2,
+  SDL2_image,
+  SDL2_mixer,
+  SDL2_ttf,
+  xercesc,
+  xdg-utils,
+}:
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "enigma";
   version = "1.30";
@@ -13,12 +30,26 @@ stdenv.mkDerivation (finalAttrs: {
     # fix format string security warning (turned into error)
     (fetchpatch {
       url = "https://github.com/Enigma-Game/Enigma/pull/70/commits/d25051eb6228c885e779a9674f8ee3979da30663.patch";
-      sha256 = "L5C4NCZDDUKji9Tg4geKaiw3CkSY6rCoawqGKqR4dFM=";
+      hash = "sha256-L5C4NCZDDUKji9Tg4geKaiw3CkSY6rCoawqGKqR4dFM=";
     })
   ];
 
-  nativeBuildInputs = [ pkg-config gettext makeWrapper imagemagick ];
-  buildInputs = [ SDL2 SDL2_image SDL2_mixer SDL2_ttf libpng xercesc curl xdg-utils ];
+  nativeBuildInputs = [
+    pkg-config
+    gettext
+    makeWrapper
+    imagemagick
+  ];
+  buildInputs = [
+    SDL2
+    SDL2_image
+    SDL2_mixer
+    SDL2_ttf
+    libpng
+    xercesc
+    curl
+    xdg-utils
+  ];
 
   # The configure script of enigma uses pkg-config to determine the header
   # directories of SDL2. However, pkg-config only returns the path to the core
@@ -39,7 +70,10 @@ stdenv.mkDerivation (finalAttrs: {
   meta = with lib; {
     description = "Puzzle game inspired by Oxyd on the Atari ST and Rock'n'Roll on the Amiga";
     mainProgram = "enigma";
-    license = with licenses; [ gpl2Plus free ]; # source + bundles libs + art
+    license = with licenses; [
+      gpl2
+      free
+    ]; # source + bundles libs + art
     platforms = platforms.unix;
     broken = stdenv.hostPlatform.isDarwin;
     maintainers = with maintainers; [ iblech ];
