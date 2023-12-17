e3d5e66ffe35367905cc8d0c945118d87a7b6630
Arjun31415 <arjunp0710@gmail.com>
2023-12-17 21:09:57 +0530

    signal-desktop (aarch64) : 6.40.0 -> 6.42.0 signal-desktop-beta: 6.40.0-beta.2 -> 6.53.0-beta.1
    (cherry picked from commit 102d9fe5fd58bb3b2d920079e9f20d8da01be28f)

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-aarch64.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-aarch64.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "6.40.0";
+  version = "6.42.0";
   url = "https://github.com/0mniteck/Signal-Desktop-Mobian/raw/${version}/builds/release/signal-desktop_${version}_arm64.deb";
-  hash = "sha256-3Pi0c+CGcJR1M4ll51m+B5PmGIcIjjlc0qa9b8rkMeU=";
+  hash = "sha256-kr8FM+EAtL/7TrgJlI33oDd4CPGMJ+F2PpQCR4OL6j4=";
 }
