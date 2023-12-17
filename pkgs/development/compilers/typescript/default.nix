5137849ea28a20ee5354832e874fa7cbaf0ff338
Mario Rodas <marsam@users.noreply.github.com>
2023-12-17 04:20:00 +0000

    typescript: 5.3.2 -> 5.3.3
    Diff: https://github.com/microsoft/TypeScript/compare/v5.3.2...v5.3.3
    
    Changelog: https://github.com/microsoft/TypeScript/releases/tag/v5.3.3

--- pkgs/development/compilers/typescript/default.nix
+++ pkgs/development/compilers/typescript/default.nix
@@ -1,21 +1,27 @@
-{ lib, buildNpmPackage, fetchFromGitHub}:
+{ lib, buildNpmPackage, fetchFromGitHub, testers, typescript }:
 
 buildNpmPackage rec {
   pname = "typescript";
-  version = "5.3.2";
+  version = "5.3.3";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "TypeScript";
     rev = "v${version}";
-    hash = "sha256-lwc2bYC2f8x3Np/LxbN+5x6Apuekp7LmHXNutqL9Z2E=";
+    hash = "sha256-gZdS4TGbafaOdNc1ZB24uAjMu9g0hef6mEsOr/dPqvY=";
   };
 
   patches = [
     ./disable-dprint-dstBundler.patch
   ];
 
-  npmDepsHash = "sha256-vD/tax5RjREdsdte3ONahVf9GPOkxPqeP9jmsxjCYkY=";
+  npmDepsHash = "sha256-gj59jjko13UBPqqy/3z1KgVMFUQPUAIg47UTTaseF+w=";
+
+  passthru.tests = {
+    version = testers.testVersion {
+      package = typescript;
+    };
+  };
 
   meta = with lib; {
     description = "A superset of JavaScript that compiles to clean JavaScript output";
