fde92bf596a6f02e1cddd8232dcb3594373f23d8
ppenguin <hieronymusv@gmail.com>
2023-12-17 20:51:15 +0100

    platformio: bump to 6.1.11; fix patches
    typo
    
    reformat (after rebase)

--- pkgs/development/python-modules/platformio/default.nix
+++ pkgs/development/python-modules/platformio/default.nix
@@ -1,11 +1,16 @@
-{ stdenv, lib, python3
-, fetchFromGitHub
-, fetchPypi
-, git
-, spdx-license-list-data
+{
+  stdenv,
+  lib,
+  python3,
+  fetchFromGitHub,
+  fetchPypi,
+  git,
+  spdx-license-list-data,
+  substituteAll,
 }:
 
-with python3.pkgs; buildPythonPackage rec {
+with python3.pkgs;
+buildPythonPackage rec {
   pname = "platformio";
 
   version = "6.1.11";
@@ -15,21 +20,21 @@ with python3.pkgs; buildPythonPackage rec {
     owner = "platformio";
     repo = "platformio-core";
     rev = "v${version}";
-    sha256 = "sha256-nrJwyoiBbOgRC8ag1V7EdhWxf5mrRaPqZuvMfETYxwc=";
+    sha256 = "sha256-NR4UyAt8q5sUGtz1Sy6E8Of7y9WrH9xpcAWzLBeDQmo=";
   };
 
   # outputs = [ "out" ];
 
   patches = [
-    ../../embedded/platformio/fix-searchpath.patch
-    ./use-local-spdx-license-list.patch
+    # ../../embedded/platformio/fix-searchpath.patch
+    (substituteAll {
+      src = ../../embedded/platformio/use-local-spdx-license-list.patch;
+      spdx_license_list_data = spdx-license-list-data.json;
+    })
     ../../embedded/platformio/missing-udev-rules-nixos.patch
   ];
 
   postPatch = ''
-    substitute platformio/package/manifest/schema.py platformio/package/manifest/schema.py \
-      --subst-var-by SPDX_LICENSE_LIST_DATA '${spdx-license-list-data.json}'
-
     substituteInPlace setup.py \
       --replace 'aiofiles==%s" % ("0.8.0" if PY36 else "22.1.*")' 'aiofiles"' \
       --replace 'starlette==%s" % ("0.19.1" if PY36 else "0.23.*")' 'starlette"' \
@@ -94,71 +99,76 @@ with python3.pkgs; buildPythonPackage rec {
     "test_ping_internet_ips"
   ];
 
-  pytestFlagsArray = [
-    "tests"
-  ] ++ (map (e: "--deselect tests/${e}") [
-    "commands/pkg/test_exec.py::test_pkg_specified"
-    "commands/pkg/test_exec.py::test_unrecognized_options"
-    "commands/test_ci.py::test_ci_boards"
-    "commands/test_ci.py::test_ci_build_dir"
-    "commands/test_ci.py::test_ci_keep_build_dir"
-    "commands/test_ci.py::test_ci_lib_and_board"
-    "commands/test_ci.py::test_ci_project_conf"
-    "commands/test_init.py::test_init_custom_framework"
-    "commands/test_init.py::test_init_duplicated_boards"
-    "commands/test_init.py::test_init_enable_auto_uploading"
-    "commands/test_init.py::test_init_ide_atom"
-    "commands/test_init.py::test_init_ide_clion"
-    "commands/test_init.py::test_init_ide_eclipse"
-    "commands/test_init.py::test_init_ide_vscode"
-    "commands/test_init.py::test_init_incorrect_board"
-    "commands/test_init.py::test_init_special_board"
-    "commands/test_lib.py::test_global_install_archive"
-    "commands/test_lib.py::test_global_install_registry"
-    "commands/test_lib.py::test_global_install_repository"
-    "commands/test_lib.py::test_global_lib_list"
-    "commands/test_lib.py::test_global_lib_uninstall"
-    "commands/test_lib.py::test_global_lib_update"
-    "commands/test_lib.py::test_global_lib_update_check"
-    "commands/test_lib.py::test_install_duplicates"
-    "commands/test_lib.py::test_lib_show"
-    "commands/test_lib.py::test_lib_stats"
-    "commands/test_lib.py::test_saving_deps"
-    "commands/test_lib.py::test_search"
-    "commands/test_lib.py::test_update"
-    "commands/test_lib_complex.py::test_global_install_archive"
-    "commands/test_lib_complex.py::test_global_install_registry"
-    "commands/test_lib_complex.py::test_global_install_repository"
-    "commands/test_lib_complex.py::test_global_lib_list"
-    "commands/test_lib_complex.py::test_global_lib_uninstall"
-    "commands/test_lib_complex.py::test_global_lib_update"
-    "commands/test_lib_complex.py::test_global_lib_update_check"
-    "commands/test_lib_complex.py::test_install_duplicates"
-    "commands/test_lib_complex.py::test_lib_show"
-    "commands/test_lib_complex.py::test_lib_stats"
-    "commands/test_lib_complex.py::test_search"
-    "package/test_manager.py::test_download"
-    "package/test_manager.py::test_install_force"
-    "package/test_manager.py::test_install_from_registry"
-    "package/test_manager.py::test_install_lib_depndencies"
-    "package/test_manager.py::test_registry"
-    "package/test_manager.py::test_uninstall"
-    "package/test_manager.py::test_update_with_metadata"
-    "package/test_manager.py::test_update_without_metadata"
-    "test_builder.py::test_build_flags"
-    "test_builder.py::test_build_unflags"
-    "test_builder.py::test_debug_custom_build_flags"
-    "test_builder.py::test_debug_default_build_flags"
-    "test_misc.py::test_api_cache"
-    "test_misc.py::test_ping_internet_ips"
-    "test_misc.py::test_platformio_cli"
-    "test_pkgmanifest.py::test_packages"
-  ]);
+  pytestFlagsArray =
+    [
+      "tests"
+    ]
+    ++ (map (e: "--deselect tests/${e}") [
+      "commands/pkg/test_exec.py::test_pkg_specified"
+      "commands/pkg/test_exec.py::test_unrecognized_options"
+      "commands/test_ci.py::test_ci_boards"
+      "commands/test_ci.py::test_ci_build_dir"
+      "commands/test_ci.py::test_ci_keep_build_dir"
+      "commands/test_ci.py::test_ci_lib_and_board"
+      "commands/test_ci.py::test_ci_project_conf"
+      "commands/test_init.py::test_init_custom_framework"
+      "commands/test_init.py::test_init_duplicated_boards"
+      "commands/test_init.py::test_init_enable_auto_uploading"
+      "commands/test_init.py::test_init_ide_atom"
+      "commands/test_init.py::test_init_ide_clion"
+      "commands/test_init.py::test_init_ide_eclipse"
+      "commands/test_init.py::test_init_ide_vscode"
+      "commands/test_init.py::test_init_incorrect_board"
+      "commands/test_init.py::test_init_special_board"
+      "commands/test_lib.py::test_global_install_archive"
+      "commands/test_lib.py::test_global_install_registry"
+      "commands/test_lib.py::test_global_install_repository"
+      "commands/test_lib.py::test_global_lib_list"
+      "commands/test_lib.py::test_global_lib_uninstall"
+      "commands/test_lib.py::test_global_lib_update"
+      "commands/test_lib.py::test_global_lib_update_check"
+      "commands/test_lib.py::test_install_duplicates"
+      "commands/test_lib.py::test_lib_show"
+      "commands/test_lib.py::test_lib_stats"
+      "commands/test_lib.py::test_saving_deps"
+      "commands/test_lib.py::test_search"
+      "commands/test_lib.py::test_update"
+      "commands/test_lib_complex.py::test_global_install_archive"
+      "commands/test_lib_complex.py::test_global_install_registry"
+      "commands/test_lib_complex.py::test_global_install_repository"
+      "commands/test_lib_complex.py::test_global_lib_list"
+      "commands/test_lib_complex.py::test_global_lib_uninstall"
+      "commands/test_lib_complex.py::test_global_lib_update"
+      "commands/test_lib_complex.py::test_global_lib_update_check"
+      "commands/test_lib_complex.py::test_install_duplicates"
+      "commands/test_lib_complex.py::test_lib_show"
+      "commands/test_lib_complex.py::test_lib_stats"
+      "commands/test_lib_complex.py::test_search"
+      "package/test_manager.py::test_download"
+      "package/test_manager.py::test_install_force"
+      "package/test_manager.py::test_install_from_registry"
+      "package/test_manager.py::test_install_lib_depndencies"
+      "package/test_manager.py::test_registry"
+      "package/test_manager.py::test_uninstall"
+      "package/test_manager.py::test_update_with_metadata"
+      "package/test_manager.py::test_update_without_metadata"
+      "test_builder.py::test_build_flags"
+      "test_builder.py::test_build_unflags"
+      "test_builder.py::test_debug_custom_build_flags"
+      "test_builder.py::test_debug_default_build_flags"
+      "test_misc.py::test_api_cache"
+      "test_misc.py::test_ping_internet_ips"
+      "test_misc.py::test_platformio_cli"
+      "test_pkgmanifest.py::test_packages"
+    ]);
 
   meta = with lib; {
     description = "An open source ecosystem for IoT development";
     homepage = "https://platformio.org";
     license = licenses.asl20;
-    maintainers = with maintainers; [ mog makefu ];
+    maintainers = with maintainers; [
+      mog
+      makefu
+    ];
   };
 }
