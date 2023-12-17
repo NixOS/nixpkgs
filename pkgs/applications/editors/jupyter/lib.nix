0d302d188bbc2b2f24e1c6906a0bc5629397997b
Matthieu Coudron <886074+teto@users.noreply.github.com>
2023-12-18 00:26:34 +0100

    jupyter.lib: init
    Introduce 2 functions to help with creating a jupyter environment:
    - mkKernelFromHaskellEnv
    - mkKernelFromPythonEnv

---
+++ pkgs/applications/editors/jupyter/lib.nix
@@ -0,0 +1,65 @@
+{ jupyter-kernel, python3, lib, ...}:
+{
+  /* Generates a kernel definition from a haskell environment
+
+      Example:
+        let ghcEnv = pkgs.haskellPackages.ghcWithPackages (p: [ p.aeson ]);
+        in mkKernelFromHaskellEnv ghcEnv;
+
+      Type:
+        mkKernelFromHaskellEnv :: Derivation -> AttrSet
+  */
+  mkKernelFromHaskellEnv = ghcEnv:
+    let
+      ghcEnv' = ghcEnv.withPackages (p: [ p.ihaskell ]);
+    in
+
+    {
+      displayName = "Haskell";
+      argv = [
+        "${ghcEnv'}/bin/ihaskell"
+        "-l"
+        # the ihaskell flake does `-l $(${env}/bin/ghc --print-libdir`
+        # we guess the path via hardcoded
+        # we can't use name else we get the 'with-packages' suffix
+        "${ghcEnv}/lib/ghc-${ghcEnv.version}"
+        "kernel"
+        "{connection_file}"
+      ];
+      language = "haskell";
+      logo32 = null;
+      logo64 = null;
+    };
+
+
+    /* Generates a kernel definition from a python environment
+    Example:
+      let pyEnv = pkgs.python3.withPackages (p: [ p.numpy ]);
+      in mkKernelFromPythonEnv pyEnv;
+
+    Type:
+      mkKernelFromPythonEnv :: Derivation -> AttrSet
+     */
+    mkKernelFromPythonEnv = env: let
+      env' = env.override(prev: {
+        # fastai
+        extraLibs = prev.extraLibs ++ [ env.python.pkgs.ipykernel ];
+      });
+
+    in
+     {
+      displayName = "Python 3";
+      argv = [
+        env'.interpreter
+        "-m"
+        "ipykernel_launcher"
+        "-f"
+        "{connection_file}"
+      ];
+      language = "python";
+      logo32 = "${env}/${env.sitePackages}/ipykernel/resources/logo-32x32.png";
+      logo64 = "${env}/${env.sitePackages}/ipykernel/resources/logo-64x64.png";
+    };
+
+
+}
