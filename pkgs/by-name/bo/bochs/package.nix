87f581380e8bfc7b076cf8a3d68bed960e02e803
Anderson Torres <torres.anderson.85@protonmail.com>
2023-12-17 16:26:48 -0300

    bochs: 2.7 -> 2.7-unstable-2023-12-16
    - `enableWx` false by default, since it pulls heavy dependencies
    - fetch from GitHub instead of Sourceforge
    - split outputs
    - strictDeps
    - use lib.* functions
    - set `meta.mainProgram`

--- pkgs/by-name/bo/bochs/package.nix
+++ pkgs/by-name/bo/bochs/package.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, fetchurl
+, fetchFromGitHub
 , SDL2
 , curl
 , darwin
@@ -19,24 +19,31 @@
 , wxGTK32
 , enableSDL2 ? true
 , enableTerm ? true
-, enableWx ? !stdenv.isDarwin
+, enableWx ? false # Too heavy
 , enableX11 ? !stdenv.isDarwin
 }:
 
+assert enableWx -> !stdenv.isDarwin;
 stdenv.mkDerivation (finalAttrs: {
   pname = "bochs";
-  version = "2.7";
+  version = "2.7-unstable-2023-12-16";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/project/bochs/bochs/${finalAttrs.version}/bochs-${finalAttrs.version}.tar.gz";
-    hash = "sha256-oBCrG/3HKsWgjS4kEs1HHA/r1mrx2TSbwNeWh53lsXo=";
+  src = fetchFromGitHub {
+    owner = "bochs-emu";
+    repo = "Bochs";
+    rev = "54831068df334989405f16f85117f7f46fef944d";
+    hash = "sha256-iZ3HN1Ysi8VAAL4rfDqS3YppdgkJ3soKy7ZAjesoyKE=";
   };
+
+  sourceRoot = "${finalAttrs.src.name}/bochs";
 
   nativeBuildInputs = [
     docbook_xml_dtd_45
     docbook_xsl
     libtool
     pkg-config
+  ] ++ lib.optionals enableSDL2 [
+    SDL2
   ];
 
   buildInputs = [
@@ -59,77 +66,81 @@ stdenv.mkDerivation (finalAttrs: {
     darwin.libobjc
   ];
 
+  outputs = [ "out" "lib" "man" ];
+
+  strictDeps = true;
+
   configureFlags = [
-    "--with-rfb=no"
-    "--with-vncsrv=no"
-    "--with-nogui"
+    (lib.withFeature false "rfb")
+    (lib.withFeature false "vncsrv")
+    (lib.withFeature false "nogui")
 
-    # These will always be "yes" on NixOS
-    "--enable-ltdl-install=yes"
-    "--enable-readline=yes"
-    "--enable-all-optimizations=yes"
-    "--enable-logging=yes"
-    "--enable-xpm=yes"
+    # These will always be "true" on NixOS
+    (lib.enableFeature true "ltdl-install")
+    (lib.enableFeature true "readline")
+    (lib.enableFeature true "all-optimizations")
+    (lib.enableFeature true "logging")
+    (lib.enableFeature true "xpm")
 
-    # ... whereas these, always "no"!
-    "--enable-cpp=no"
-    "--enable-instrumentation=no"
+    # ... whereas these, always "false"
+    (lib.enableFeature false "cpp")
+    (lib.enableFeature false "instrumentation")
 
-    "--enable-docbook=no" # Broken - it requires docbook2html
+    # Broken - it requires docbook2html
+    (lib.enableFeature false "docbook")
 
-    # Dangerous options - they are marked as "incomplete/experimental" on Bochs documentation
-    "--enable-3dnow=no"
-    "--enable-monitor-mwait=no"
-    "--enable-raw-serial=no"
+    # Dangerous options - they are marked as "incomplete/experimental" on Bochs
+    # documentation
+    (lib.enableFeature false "3dnow")
+    (lib.enableFeature false "monitor-mwait")
+    (lib.enableFeature false "raw-serial")
 
     # These are completely configurable, and they don't depend of external tools
-    "--enable-a20-pin"
-    "--enable-avx"
-    "--enable-busmouse"
-    "--enable-cdrom"
-    "--enable-clgd54xx"
-    "--enable-configurable-msrs"
-    "--enable-cpu-level=6" # from 3 to 6
-    "--enable-debugger" #conflicts with gdb-stub option
-    "--enable-debugger-gui"
-    "--enable-evex"
-    "--enable-fpu"
-    "--enable-gdb-stub=no" # conflicts with debugger option
-    "--enable-handlers-chaining"
-    "--enable-idle-hack"
-    "--enable-iodebug"
-    "--enable-large-ramfile"
-    "--enable-largefile"
-    "--enable-pci"
-    "--enable-repeat-speedups"
-    "--enable-show-ips"
-    "--enable-smp"
-    "--enable-vmx=2"
-    "--enable-svm"
-    "--enable-trace-linking"
-    "--enable-usb"
-    "--enable-usb-ehci"
-    "--enable-usb-ohci"
-    "--enable-usb-xhci"
-    "--enable-voodoo"
-    "--enable-x86-64"
-    "--enable-x86-debugger"
-  ] ++ lib.optionals enableSDL2 [
-    "--with-sdl2"
-  ] ++ lib.optionals enableTerm [
-    "--with-term"
-  ] ++ lib.optionals enableWx [
-    "--with-wx"
-  ] ++ lib.optionals enableX11 [
-    "--with-x"
-    "--with-x11"
-  ] ++ lib.optionals (!stdenv.isDarwin) [
-    "--enable-e1000"
-    "--enable-es1370"
-    "--enable-ne2000"
-    "--enable-plugins"
-    "--enable-pnic"
-    "--enable-sb16"
+    (lib.enableFeature true "a20-pin")
+    (lib.enableFeature true "avx")
+    (lib.enableFeature true "busmouse")
+    (lib.enableFeature true "cdrom")
+    (lib.enableFeature true "clgd54xx")
+    (lib.enableFeature true "configurable-msrs")
+    (lib.enableFeature true "debugger") # conflicts with gdb-stub option
+    (lib.enableFeature true "debugger-gui")
+    (lib.enableFeature true "evex")
+    (lib.enableFeature true "fpu")
+    (lib.enableFeature false "gdb-stub") # conflicts with debugger option
+    (lib.enableFeature true "handlers-chaining")
+    (lib.enableFeature true "idle-hack")
+    (lib.enableFeature true "iodebug")
+    (lib.enableFeature true "large-ramfile")
+    (lib.enableFeature true "largefile")
+    (lib.enableFeature true "pci")
+    (lib.enableFeature true "repeat-speedups")
+    (lib.enableFeature true "show-ips")
+    (lib.enableFeature true "smp")
+    (lib.enableFeature true "svm")
+    (lib.enableFeature true "trace-linking")
+    (lib.enableFeature true "usb")
+    (lib.enableFeature true "usb-ehci")
+    (lib.enableFeature true "usb-ohci")
+    (lib.enableFeature true "usb-xhci")
+    (lib.enableFeature true "voodoo")
+    (lib.enableFeature true "x86-64")
+    (lib.enableFeature true "x86-debugger")
+
+    (lib.enableFeature (!stdenv.isDarwin) "e1000")
+    (lib.enableFeature (!stdenv.isDarwin) "es1370")
+    (lib.enableFeature (!stdenv.isDarwin) "ne2000")
+    (lib.enableFeature (!stdenv.isDarwin) "plugins")
+    (lib.enableFeature (!stdenv.isDarwin) "pnic")
+    (lib.enableFeature (!stdenv.isDarwin) "sb16")
+
+    (lib.enableFeatureAs true "cpu-level" "6")
+    (lib.enableFeatureAs true "vmx" "2")
+
+    (lib.withFeature enableSDL2 "sdl2")
+    (lib.withFeature enableTerm "term")
+    (lib.withFeature enableWx "wx")
+    (lib.withFeature enableX11 "x")
+    (lib.withFeature enableX11 "x11")
   ];
 
   enableParallelBuilding = true;
@@ -142,10 +153,10 @@ stdenv.mkDerivation (finalAttrs: {
       in C++, that runs on most popular platforms. It includes emulation of the
       Intel x86 CPU, common I/O devices, and a custom BIOS.
     '';
-    license = lib.licenses.lgpl2Plus;
+    license = with lib.licenses; [ lgpl2Plus ];
+    mainProgram = "bochs";
     maintainers = with lib.maintainers; [ AndersonTorres ];
     platforms = lib.platforms.unix;
   };
 })
-# TODO: a better way to organize the options
 # TODO: docbook (docbook-tools from RedHat mirrors should help)
