a3e6db8c34992df4c8eea7085ec6397d74618e9c
Stanisław Pitucha <git@viraptor.info>
2023-12-17 15:21:33 +1100

    caqe: init at 4.0.2

---
+++ pkgs/by-name/ca/caqe/package.nix
@@ -0,0 +1,45 @@
+{ stdenv
+, lib
+, rustPlatform
+, fetchFromGitHub
+, nix-update-script
+, cmake
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "caqe";
+  version = "4.0.2";
+
+  src = fetchFromGitHub {
+    owner = "ltentrup";
+    repo = "caqe";
+    rev = version;
+    sha256 = "sha256-TVh2bsVX318Ma/ATnv9QJtzsJtGyoBQsTXtTF6mWsJQ=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  env = lib.optionalAttrs stdenv.cc.isClang {
+    NIX_LDFLAGS = "-l${stdenv.cc.libcxx.cxxabi.libName}";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "cryptominisat-5.6.6" = "sha256-KBRcnfUYg4iUHhufb93iHFk9n4c27XdONCKGycj7vmw=";
+    };
+  };
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
+
+  meta = with lib; {
+    description = "A solver for quantified Boolean formulas";
+    homepage = "https://github.com/ltentrup/caqe";
+    license = with licenses; [ mit ];
+    maintainers = with maintainers; [ viraptor ];
+  };
+}
