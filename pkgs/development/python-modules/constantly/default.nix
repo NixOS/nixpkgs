5f1690353b625202d3c889f26a020d8911e3646c
Dennis Wuitz <dennish@wuitz.de>
2023-12-17 23:44:01 +0100

    python311Packages.constantly: 15.1.0 -> 23.10.4

--- pkgs/development/python-modules/constantly/default.nix
+++ pkgs/development/python-modules/constantly/default.nix
@@ -1,20 +1,22 @@
-{ lib, buildPythonPackage, fetchFromGitHub, twisted }:
+{ lib, buildPythonPackage, fetchFromGitHub, twisted, versioneer }:
 
 let
   self = buildPythonPackage rec {
     pname = "constantly";
-    version = "15.1.0";
+    version = "23.10.4";
     format = "setuptools";
 
     src = fetchFromGitHub {
       owner = "twisted";
       repo = "constantly";
       rev = version;
-      hash = "sha256-0RPK5Vy0b6V4ubvm+vfNOAua7Qpa6j+G+QNExFuHgUU=";
+      hash = "sha256-HTj6zbrCrxvh0PeSkeCSOCloTrVGUX6+o57snrKf6PA=";
     };
 
     # would create dependency loop with twisted
     doCheck = false;
+
+    propagatedBuildInputs = [ versioneer ];
 
     nativeCheckInputs = [ twisted ];
 
