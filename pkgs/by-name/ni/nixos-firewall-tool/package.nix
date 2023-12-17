ff0ce0571855c7aed0f93ef2bcefa5aaed785c01
Rvfg <i@rvf6.com>
2023-12-18 00:23:01 +0800

    nixos-firewall-tool: add nftables support

--- pkgs/by-name/ni/nixos-firewall-tool/package.nix
+++ pkgs/by-name/ni/nixos-firewall-tool/package.nix
@@ -1,15 +1,24 @@
-{ writeShellApplication, iptables, lib }:
+{ writeShellApplication, iptables, lib
+, nftables, backend ? "iptables"
+}:
 
-writeShellApplication {
+writeShellApplication ({
   name = "nixos-firewall-tool";
-  text = builtins.readFile ./nixos-firewall-tool.sh;
-  runtimeInputs = [
-    iptables
-  ];
 
   meta = with lib; {
     description = "Temporarily manipulate the NixOS firewall";
     license = licenses.mit;
-    maintainers = with maintainers; [ clerie ];
+    maintainers = with maintainers; [ clerie rvfg ];
   };
-}
+} // (
+  if backend == "iptables" then {
+    text = builtins.readFile ./nixos-firewall-tool.sh;
+    runtimeInputs = [ iptables ];
+
+  } else if backend == "nftables" then {
+    text = builtins.readFile ./nixos-firewall-tool-nftables.sh;
+    runtimeInputs = [ nftables ];
+
+  } else
+    throw "Unknown firewall backend."
+))
