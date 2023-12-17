b94c806d9380959d79069552abaeda9f20561a61
Philip Taron <philip.taron@gmail.com>
2023-12-17 15:16:01 -0800

    qrtool: 0.8.5 -> 0.10.0
    Also, update `sha256` to `hash` and `cargoSha256` to `cargoHash` per feedback from @kirillrdy.
    
    Co-authored-by: kirillrdy <kirillrdy@gmail.com>

--- pkgs/by-name/qr/qrtool/package.nix
+++ pkgs/by-name/qr/qrtool/package.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "qrtool";
-  version = "0.8.5";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "sorairolake";
     repo = "qrtool";
     rev = "v${version}";
-    sha256 = "sha256-jrvNZGO1VIDo6Mz3NKda1C7qZUtF9T00CAFK8yoGWjc=";
+    hash = "sha256-PXjggeDofrrS32XdqnOgAMlAiw+/7eMQpELw8EmiwNI=";
   };
 
-  cargoSha256 = "sha256-JOnvlabCr3fZsIIRc2qTjf50Ga83zL8Aoo2sqzMBs7g=";
+  cargoHash = "sha256-0Vwx80XxrJX8C293+4qgRr0pTMV6Jy3IBNQISzFxyr0=";
 
   nativeBuildInputs = [ asciidoctor installShellFiles ];
 
