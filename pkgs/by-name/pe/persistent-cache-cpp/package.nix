5079ef293082fd890601c05e602e1420a7555032
OPNA2608 <christoph.neidahl@gmail.com>
2023-12-18 02:17:04 +0100

    persistent-cache-cpp: 1.0.5 -> 1.0.6

--- pkgs/by-name/pe/persistent-cache-cpp/package.nix
+++ pkgs/by-name/pe/persistent-cache-cpp/package.nix
@@ -1,7 +1,6 @@
 { stdenv
 , lib
 , fetchFromGitLab
-, fetchpatch
 , gitUpdater
 , testers
 , boost
@@ -16,30 +15,22 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "persistent-cache-cpp";
-  version = "1.0.5";
+  version = "1.0.6";
 
   src = fetchFromGitLab {
     owner = "ubports";
     repo = "development/core/lib-cpp/persistent-cache-cpp";
     rev = finalAttrs.version;
-    hash = "sha256-MOH8ardiTliTKrtRQ5eFRhbRtnC0Yq20WJYoo1tAn3E=";
+    hash = "sha256-RLZiYY0Y9LT+ajM4Va4MpVVDBlu2yvCpn8bNGMB8ydo=";
   };
 
+  outputs = [
+    "out"
+    "dev"
+    "doc"
+  ];
+
   patches = [
-    # Fix build on GCC 12
-    # Remove when version > 1.0.5
-    (fetchpatch {
-      url = "https://gitlab.com/ubports/development/core/lib-cpp/persistent-cache-cpp/-/commit/3ed84ee1d32a27d183de2cb5f9feffc3f48fd9a1.patch";
-      hash = "sha256-aNZ6KVHAsLVYlAcPNNkjUlOPRDZxzN5tzk4/1KuVaSY=";
-    })
-
-    # Fix build on current Boost
-    # Remove when version > 1.0.5
-    (fetchpatch {
-      url = "https://gitlab.com/ubports/development/core/lib-cpp/persistent-cache-cpp/-/commit/a590ffcccec252caa7b19a2922c678502069b057.patch";
-      hash = "sha256-OhlsnUVhclt17brkncYkJ0+lXrJO671mtwopaAGPrtY=";
-    })
-
     # PersistentStringCacheImpl.exceptions test fails on LLVM's libcxx, it depends on std::system_error producing a very specific exception text
     # Expects "Unknown error 666", gets "unspecified generic_category error"
     # https://gitlab.com/ubports/development/core/lib-cpp/persistent-cache-cpp/-/blob/1.0.5/tests/core/internal/persistent_string_cache_impl/persistent_string_cache_impl_test.cpp?ref_type=tags#L1298
@@ -54,7 +45,7 @@ stdenv.mkDerivation (finalAttrs: {
     # Runs in parallel to other tests, limit to 1 thread
     substituteInPlace tests/headers/compile_headers.py \
       --replace 'multiprocessing.cpu_count()' '1'
-  '' + (if finalAttrs.doCheck then ''
+  '' + (if finalAttrs.finalPackage.doCheck then ''
     patchShebangs tests/{headers,whitespace}/*.py
   '' else ''
     sed -i -e '/add_subdirectory(tests)/d' CMakeLists.txt
