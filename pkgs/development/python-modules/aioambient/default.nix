1ede548b027748858593e288636c89671e9de502
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-18 07:39:08 +0000

    python310Packages.aioambient: 2023.10.1 -> 2023.12.0

--- pkgs/development/python-modules/aioambient/default.nix
+++ pkgs/development/python-modules/aioambient/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "aioambient";
-  version = "2023.10.1";
+  version = "2023.12.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "bachya";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-Q7jb0tJsbVM2vEqKgjXOWJN2OwR9qLchU/4ShOUGPT4=";
+    hash = "sha256-O9MlXtX7UzFN1w/vxpcZ/nRPDFPK5wFKBl42rhaAu94=";
   };
 
   postPatch = ''
