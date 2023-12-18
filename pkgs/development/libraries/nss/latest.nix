ae2527e1e2c9136f6df8221f9851d89a12d9eb17
ajs124 <git@ajs124.de>
2023-12-18 21:28:00 +0100

    nss_latest: 3.96 -> 3.96.1
    https://groups.google.com/a/mozilla.org/g/dev-tech-crypto/c/WjaCRG7B8tY

--- pkgs/development/libraries/nss/latest.nix
+++ pkgs/development/libraries/nss/latest.nix
@@ -5,6 +5,6 @@
 #       Example: nix-shell ./maintainers/scripts/update.nix --argstr package cacert
 
 import ./generic.nix {
-  version = "3.96";
-  hash = "sha256-OdvJ7OLL3CDvJkgtnMsuE1CVaaKpmgRmzEzW0WmD8Jo=";
+  version = "3.96.1";
+  hash = "sha256-HhN3wZEdi9R/KD0nl3+et+94LBJjGLDVqDX8v5qGrqQ=";
 }
