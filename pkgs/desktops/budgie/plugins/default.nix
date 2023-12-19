c749d441a1f95b041490944ddbd54b5d2170d818
Adam Erickson <adam.michael.erickson@gmail.com>
2023-12-18 19:10:51 -0800

    trim fat

--- pkgs/desktops/budgie/plugins/default.nix
+++
@@ -1,4 +0,0 @@
-{ callPackage }: {
-  budgie-analogue-clock-applet = callPackage ./budgie-analogue-clock-applet { };
-  budgie-user-indicator-redux = callPackage ./budgie-user-indicator-redux { };
-}
--- pkgs/games/freeorion/fix-paths.sh
+++
@@ -1,6 +0,0 @@
-#!@shell@
-
-if [ -e ~/.config/freeorion/config.xml ]; then
-  @libxsltBin@/bin/xsltproc -o ~/.config/freeorion/config.xml @out@/share/freeorion/fix-paths.xslt ~/.config/freeorion/config.xml
-fi
-exit 0
