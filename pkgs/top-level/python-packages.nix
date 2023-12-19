782978b7fe13c7d66e0980d2d192de2527c55557
Robert Schütz <nix@dotlambda.de>
2023-12-19 10:53:09 -0800

    python311Packages.dynalite-panel: init at 0.0.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3461,6 +3461,8 @@ self: super: with self; {
 
   dynalite-devices = callPackage ../development/python-modules/dynalite-devices { };
 
+  dynalite-panel = callPackage ../development/python-modules/dynalite-panel { };
+
   dynd = callPackage ../development/python-modules/dynd { };
 
   e3-core = callPackage ../development/python-modules/e3-core { };
