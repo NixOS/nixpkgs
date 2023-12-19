a1767458e30cb969914350a2110fa521b80928da
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-19 15:29:40 +0000

    python310Packages.glean-parser: 10.0.3 -> 11.0.0

--- pkgs/development/python-modules/glean-parser/default.nix
+++ pkgs/development/python-modules/glean-parser/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "glean-parser";
-  version = "10.0.3";
+  version = "11.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "glean_parser";
     inherit version;
-    hash = "sha256-1XNZYp0pX57lcAaKKEaWaJLj/ttyIlnsvNjMN20pm1E=";
+    hash = "sha256-MHy51x6TJ2cFUZQ4XaZWLTgws/40DwxIqBSc81zCTlY=";
   };
 
   postPatch = ''
