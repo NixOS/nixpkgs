0b311f4328f63b77096e3e4e87a4dbe191ad29e8
Adrian Pistol <vifino@posteo.net>
2023-12-19 16:35:52 +0100

    libnvme: 1.6 -> 1.7.1

--- pkgs/os-specific/linux/libnvme/default.nix
+++ pkgs/os-specific/linux/libnvme/default.nix
@@ -11,14 +11,13 @@
 , stdenv
 , swig
 , systemd
-, fetchpatch
 # ImportError: cannot import name 'mlog' from 'mesonbuild'
 , withDocs ? stdenv.hostPlatform.canExecute stdenv.buildPlatform
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libnvme";
-  version = "1.6";
+  version = "1.7.1";
 
   outputs = [ "out" ] ++ lib.optionals withDocs [ "man" ];
 
@@ -26,26 +25,8 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "linux-nvme";
     repo = "libnvme";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-7bvjsmt16/6RycSDKIECtJ4ES7NTaspU6IMpUw0sViA=";
+    hash = "sha256-hCR/K8bPXj8HthayrnwwGfI+wxpUwcWkcx3S/8h+3m8=";
   };
-
-  patches = [
-    # included in next release
-    (fetchpatch {
-      url = "https://github.com/linux-nvme/libnvme/commit/ff742e792725c316ba6de0800188bf36751bd1d1.patch";
-      hash = "sha256-IUjPUBmGQC4oAKFFlBrjonqD2YdyNPC9siK4t/t2slE=";
-    })
-    # included in next release
-    (fetchpatch {
-      url = "https://github.com/linux-nvme/libnvme/commit/a2b8e52e46cfd888ac5a48d8ce632bd70a5caa93.patch";
-      hash = "sha256-AVSWraFriErfz7dA2CjU8+ehJtAmuLxBZyBALygmrf0=";
-    })
-    # included in next release
-    (fetchpatch {
-      url = "https://github.com/linux-nvme/libnvme/commit/68c6ffb11d40a427fc1fd70ac2ac97fd01952913.patch";
-      hash = "sha256-dvc1sjgCFU31/LornvJ/aRVYtPOsewkas0jS+/AwFuU=";
-    })
-  ];
 
   postPatch = ''
     patchShebangs scripts
