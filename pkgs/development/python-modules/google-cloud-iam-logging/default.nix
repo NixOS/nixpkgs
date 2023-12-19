1148691cfa2f239a4bf4b193545963dad2e8b480
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-19 19:53:40 +0000

    python310Packages.google-cloud-iam-logging: 1.2.2 -> 1.3.0

--- pkgs/development/python-modules/google-cloud-iam-logging/default.nix
+++ pkgs/development/python-modules/google-cloud-iam-logging/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-iam-logging";
-  version = "1.2.2";
+  version = "1.3.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-6IBjA2WwP11d/vQJSIY3WhbqYvAgFRtZFFSffUqKM38=";
+    hash = "sha256-oLqRmxNPbb+nUMN70kGlAtBCji4wXrbRv2DhNMcZV5c=";
   };
 
   propagatedBuildInputs = [
