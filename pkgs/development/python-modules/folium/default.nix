c8c68e50a5ea392c3ae3f59994275f7f08cfa7d9
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-19 13:54:53 +0000

    python310Packages.folium: 0.15.0 -> 0.15.1

--- pkgs/development/python-modules/folium/default.nix
+++ pkgs/development/python-modules/folium/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "folium";
-  version = "0.15.0";
+  version = "0.15.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "python-visualization";
     repo = "folium";
     rev = "refs/tags/v${version}";
-    hash = "sha256-xaz9oelkyS8lWECCmKs8P3mHB3Usv0KMUoh/K7rBnAs=";
+    hash = "sha256-CHPHxp8xEZhEEMLvhs/xAiOr2Hw6B+5svFNY+QvQa+U=";
   };
 
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
@@ -41,6 +41,7 @@ buildPythonPackage rec {
     jinja2
     numpy
     requests
+    xyzservices
   ];
 
   nativeCheckInputs = [
@@ -50,7 +51,6 @@ buildPythonPackage rec {
     pillow
     pytestCheckHook
     selenium
-    xyzservices
   ];
 
   disabledTests = [
