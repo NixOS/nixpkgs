a76f70c38045bb9d66cfd7b839e08194ac9797ae
Thomas Gerbet <thomas@gerbet.me>
2023-12-19 21:47:04 +0100

    putty: 0.76 -> 0.80
    Includes the mitigation for CVE-2023-48795 (Terrapin attack).
    
    https://www.chiark.greenend.org.uk/~sgtatham/putty/changes.html
    (cherry picked from commit b6a6bca17cb482c832dcf339a0be3b8da357d8e6)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41009,7 +41009,7 @@ with pkgs;
   pwntools = with python3Packages; toPythonApplication pwntools;
 
   putty = callPackage ../applications/networking/remote/putty {
-    gtk2 = gtk2-x11;
+    gtk3 = if stdenv.isDarwin then gtk3-x11 else gtk3;
   };
 
   qMasterPassword = libsForQt5.callPackage ../applications/misc/qMasterPassword { };
