a13a4a04eaf0a3b8cc74c75de29496405ea83aef
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-19 10:33:47 +0000

    pluto: 5.18.6 -> 5.19.0

--- pkgs/applications/networking/cluster/pluto/default.nix
+++ pkgs/applications/networking/cluster/pluto/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "pluto";
-  version = "5.18.6";
+  version = "5.19.0";
 
   src = fetchFromGitHub {
     owner = "FairwindsOps";
     repo = "pluto";
     rev = "v${version}";
-    hash = "sha256-PQf1gEFlZ6Y9KMQjKeXAZy/OfxCbiKfST3pr9xp0/vg=";
+    hash = "sha256-sudq7HILNZSGTwQgggSovrXAaY7VSTw6IJn+mIYwjv8=";
   };
 
   vendorHash = "sha256-8ZOYp/vM16PugmE+3QK7ZRDwIwRCMEwD0NRyiOBlh14=";
