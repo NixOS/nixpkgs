0b338a0b5a9454d637fd0fa465653baaeb10d319
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-12-21 00:36:43 +0100

    ugrep: 4.3.6 -> 4.4.1

--- pkgs/tools/text/ugrep/default.nix
+++ pkgs/tools/text/ugrep/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ugrep";
-  version = "4.3.6";
+  version = "4.4.1";
 
   src = fetchFromGitHub {
     owner = "Genivia";
     repo = "ugrep";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-eCOSUtSPIRaoc7pIyQAftcwG3P8321qk6GPbeDNNevI=";
+    hash = "sha256-Bbt20XE+PNIxl2qDzxpIh4yjU93JgXF4gn1kb4bvdBw=";
   };
 
   buildInputs = [
