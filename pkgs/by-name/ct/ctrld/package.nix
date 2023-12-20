b7e63cc4cb46a825029e086af4db666d77535d0c
arthsmn <arth.sh@proton.me>
2023-12-20 15:52:04 -0300

    ctrld: init at 1.3.3

---
+++ pkgs/by-name/ct/ctrld/package.nix
@@ -0,0 +1,36 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+}:
+
+buildGoModule rec {
+  pname = "ctrld";
+  version = "1.3.3";
+
+  src = fetchFromGitHub {
+    owner = "Control-D-Inc";
+    repo = "ctrld";
+    rev = "v${version}";
+    hash = "sha256-vFXCBonJCZWFy+sKXDQ55ykFb67Sj1GdfuMtKIf4+5I=";
+  };
+
+  vendorHash = "sha256-il3jA4Dtfs1GMyL3IEgmzRDOIsKViovBqhtsvzwgafQ=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.version=${version}"
+    "-X=main.commit=${src.rev}"
+  ];
+
+  # Tests need internet
+  doCheck = false;
+
+  meta = with lib; {
+    description = "A highly configurable, multi-protocol DNS forwarding proxy";
+    homepage = "https://github.com/Control-D-Inc/ctrld";
+    license = licenses.mit;
+    maintainers = with maintainers; [ arthsmn ];
+    mainProgram = "ctrld";
+  };
+}
