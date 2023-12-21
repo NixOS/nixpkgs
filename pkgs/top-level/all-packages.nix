fd7ed6d2a858e6973c4aa4ef8fe9fc0e693fc0c4
eymeric <eymericdechelette@gmail.com>
2023-12-21 12:32:38 +0100

    melos: init at 3.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41533,6 +41533,8 @@ with pkgs;
 
   dartHooks = callPackage ../build-support/dart/build-dart-application/hooks { };
 
+  melos = callPackage ../development/tools/misc/melos { };
+
   httrack = callPackage ../tools/backup/httrack { };
 
   httraqt = libsForQt5.callPackage ../tools/backup/httrack/qt.nix { };
