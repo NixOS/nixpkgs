940cb3b70cf60fbdf0797b9c968f3928c01a5248
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-21 10:49:53 +0100

    python310Packages.google-cloud-iam-logging: update URLs

--- pkgs/development/python-modules/google-cloud-iam-logging/default.nix
+++ pkgs/development/python-modules/google-cloud-iam-logging/default.nix
@@ -43,8 +43,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "IAM Service Logging client library";
-    homepage = "https://github.com/googleapis/python-iam-logging";
-    changelog = "https://github.com/googleapis/python-iam-logging/blob/v${version}/CHANGELOG.md";
+    homepage = "https://github.com/googleapis/google-cloud-python/tree/main/packages/google-cloud-iam-logging";
+    changelog = "https://github.com/googleapis/google-cloud-python/blob/google-cloud-iam-logging-v${version}/packages/google-cloud-iam-logging/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
