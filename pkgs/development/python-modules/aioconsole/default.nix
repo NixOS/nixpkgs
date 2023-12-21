1ec9282f87e38aa4320f71c97a280f37c505c4b4
Sergei Trofimovich <slyich@gmail.com>
2023-12-21 10:25:28 +0000

    python3Packages.aioconsole: drop duplicate `setuptools` and `meta.changelog` enties

--- pkgs/development/python-modules/aioconsole/default.nix
+++ pkgs/development/python-modules/aioconsole/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, setuptools
 , pytest-asyncio
 , pytestCheckHook
 , pythonOlder
@@ -60,7 +59,6 @@ buildPythonPackage rec {
     changelog = "https://github.com/vxgmichel/aioconsole/releases/tag/v${version}";
     description = "Asynchronous console and interfaces for asyncio";
     homepage = "https://github.com/vxgmichel/aioconsole";
-    changelog = "https://github.com/vxgmichel/aioconsole/releases/tag/v${version}";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ catern ];
   };
