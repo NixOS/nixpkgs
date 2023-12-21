684c2fd90b07634c25202963147df63caecbc435
Aaron Jheng <wentworth@outlook.com>
2023-12-21 15:25:22 +0000

    mbox: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10836,8 +10836,6 @@ with pkgs;
 
   mb2md = callPackage ../tools/text/mb2md { };
 
-  mbox = callPackage ../tools/security/mbox { };
-
   mbuffer = callPackage ../tools/misc/mbuffer { };
 
   mdsh = callPackage ../development/tools/documentation/mdsh { };
