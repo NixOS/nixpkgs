4ecc674e46bb7bf93eed4ca89fbb4ab2dd036c89
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-12-22 00:31:12 +0100

    tailscale-nginx-auth: Remove unneeded definition in all-packages.nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27431,8 +27431,6 @@ with pkgs;
 
   tailscale = callPackage ../servers/tailscale { };
 
-  tailscale-nginx-auth = callPackage ../by-name/ta/tailscale-nginx-auth/package.nix { };
-
   tailscale-systray = callPackage ../applications/misc/tailscale-systray { };
 
   tailspin = callPackage ../tools/misc/tailspin { };
