4728bb8e6dbd3fbcbfbe893956ee1ded5a3c892c
Robert Medeiros <robert@crimeminister.org>
2023-12-22 13:19:56 -0500

    iroh: 0.11 -> 0.12

--- pkgs/applications/networking/iroh/default.nix
+++ pkgs/applications/networking/iroh/default.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "iroh";
-  version = "0.11.0";
+  version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "n0-computer";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-b3XpKAV/K+69tQmjM1CGzoOTcaQHB6q3gpoSa/YFwak=";
+    hash = "sha256-a5Yd//ihJe7Li5JySoOops4UbO+978BRnZk/nBy07Aw=";
   };
 
-  cargoHash = "sha256-dnEEque40qi7vuUxY/UDZ5Kz8LTuz0GvYVjTxl8eMvI=";
+  cargoHash = "sha256-X8SJQLqV9CAkIre04ykyAxSI93PbiTFGO1Pk/vvyMJU=";
 
   buildInputs = lib.optionals stdenv.isDarwin (
     with darwin.apple_sdk.frameworks; [
