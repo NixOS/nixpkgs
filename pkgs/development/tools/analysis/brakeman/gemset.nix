baf9072d9dd88f3399526fbd6cec78a21c37b2bd
Mario Rodas <marsam@users.noreply.github.com>
2023-12-23 04:20:00 +0000

    brakeman: 6.0.0 -> 6.1.0
    Changelog: https://github.com/presidentbeef/brakeman/releases/tag/v6.1.0

--- pkgs/development/tools/analysis/brakeman/gemset.nix
+++ pkgs/development/tools/analysis/brakeman/gemset.nix
@@ -4,9 +4,9 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1l2584f7cm7lmwihm1l449rk6vl4wlx3s7x317cm2inapzjhiybg";
+      sha256 = "00vlip5z1gc1npj1nxvcy2gvwya4fk01xzyhazkhz3ymdn9nch0d";
       type = "gem";
     };
-    version = "6.0.0";
+    version = "6.1.0";
   };
 }
