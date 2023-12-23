9011ba40f78d47cb47f28be4f2ef13ec54eacf27
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-23 05:25:10 +0000

    rocmPackages.rocm-docs-core: 0.26.0 -> 0.30.3

--- pkgs/development/rocm-modules/5/rocm-docs-core/default.nix
+++ pkgs/development/rocm-modules/5/rocm-docs-core/default.nix
@@ -23,14 +23,14 @@
 # FIXME: Move to rocmPackages_common
 buildPythonPackage rec {
   pname = "rocm-docs-core";
-  version = "0.26.0";
+  version = "0.30.3";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "RadeonOpenCompute";
     repo = "rocm-docs-core";
     rev = "v${version}";
-    hash = "sha256-Mr6/Ne6P+TapoCqN7xkKMNse3fTaIAvvLmMl0kVg7Vs=";
+    hash = "sha256-D4OTLrFhxUcUee99EB0EovmsulGBFuQRHFdtzj7OawA=";
   };
 
   buildInputs = [ setuptools ];
