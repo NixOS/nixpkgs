4cbf461ffd3f493ce4e6b0f0c663df087c385c4a
jopejoe1 <johannes@joens.email>
2023-12-23 13:49:40 +0100

    vdrsymbols: change license to bitstreamVera and publicDomain

--- pkgs/data/fonts/vdrsymbols/default.nix
+++ pkgs/data/fonts/vdrsymbols/default.nix
@@ -27,6 +27,6 @@ stdenvNoCC.mkDerivation rec {
     # Copyright (c) 2006 by Tavmjong Bah. All Rights Reserved.
     # DejaVu changes are in public domain
     # See https://dejavu-fonts.github.io/License.html for details
-    license = licenses.free;
+    license = with licenses; [ bitstreamVera publicDomain ];
   };
 }
