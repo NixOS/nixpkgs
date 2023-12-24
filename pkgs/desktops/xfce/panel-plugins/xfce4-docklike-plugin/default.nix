c9516f97dfd435449dcca3083bcc27acdb100fab
Bobby Rong <rjl931189261@126.com>
2023-12-24 13:45:50 +0000

    xfce.xfce4-docklike-plugin: 0.4.1 -> 0.4.2
    https://gitlab.xfce.org/panel-plugins/xfce4-docklike-plugin/-/compare/xfce4-docklike-plugin-0.4.1...xfce4-docklike-plugin-0.4.2

--- pkgs/desktops/xfce/panel-plugins/xfce4-docklike-plugin/default.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-docklike-plugin/default.nix
@@ -1,30 +1,30 @@
 { lib
 , mkXfceDerivation
+, cairo
 , glib
 , gtk3
+, libX11
 , libxfce4ui
 , libxfce4util
 , xfce4-panel
-, xfconf
 , libwnck
-, exo
 }:
 
 mkXfceDerivation {
   category = "panel-plugins";
   pname = "xfce4-docklike-plugin";
-  version = "0.4.1";
-  sha256 = "sha256-BKxd2TFEbRHeFy/dC2Wx5ppErsi7d2m7JicFCcZbjTo=";
+  version = "0.4.2";
+  sha256 = "sha256-M/V8cnEU/nSEDjQ3k8fWiklF5OuNg3uzzJMHBSZBiLU=";
 
   buildInputs = [
+    cairo
     glib
     gtk3
+    libX11
     libxfce4ui
     libxfce4util
     xfce4-panel
-    xfconf
     libwnck
-    exo
   ];
 
   NIX_CFLAGS_COMPILE = "-I${glib.dev}/include/gio-unix-2.0";
