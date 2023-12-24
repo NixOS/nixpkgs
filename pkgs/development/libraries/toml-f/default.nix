e19f1b9d5b47054f4f7bcfdd718687473061da49
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-24 13:58:14 +0000

    toml-f: 0.4.1 -> 0.4.2

--- pkgs/development/libraries/toml-f/default.nix
+++ pkgs/development/libraries/toml-f/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "toml-f";
-  version = "0.4.1";
+  version = "0.4.2";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-sCU0uMdcXIA5O964hlK37cOrLTlk1CJeTcWD9FhevOs=";
+    hash = "sha256-+cac4rUNpd2w3yBdH1XoCKdJ9IgOHZioZg8AhzGY0FE=";
   };
 
   nativeBuildInputs = [ gfortran cmake ];
