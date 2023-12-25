0301e234c975d84f2fea5e3cbcd2aca99f8bfab1
Pavel Anpin <pavel@anpin.fyi>
2023-12-25 18:47:28 +0100

    maintainers: added new pgp key to anpin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1281,9 +1281,11 @@
     githubId = 6060545;
     matrix = "@anpin:matrix.org";
     name = "Pavel Anpin";
-    keys = [{
-      fingerprint = "06E8 4FF6 0CCF 7AFD 5101  76C9 0FBC D3EE 6310 7407";
-    }];
+    keys = [
+      { fingerprint = "06E8 4FF6 0CCF 7AFD 5101  76C9 0FBC D3EE 6310 7407"; }
+      # compare with https://keybase.io/anpin/pgp_keys.asc
+      { fingerprint = "DADF F3EA 06DC 8C1B 100A  24DB 312E 8F17 91C5 DA8C"; }
+    ];
   };
   anpryl = {
     email = "anpryl@gmail.com";
