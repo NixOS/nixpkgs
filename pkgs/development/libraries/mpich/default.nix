947614897ecf2d726f76c3fc5933ab64bbdb5419
Someone Serge <sergei.kozlukov@aalto.fi>
2023-12-25 12:39:38 +0000

    mpich: extend the warning about pmix

--- pkgs/development/libraries/mpich/default.nix
+++ pkgs/development/libraries/mpich/default.nix
@@ -3,10 +3,12 @@
 # either libfabric or ucx work for ch4backend on linux. On darwin, neither of
 # these libraries currently build so this argument is ignored on Darwin.
 , ch4backend
-# Process manager to build,
+# Process managers to build (`--with-pm`),
 # cf. https://github.com/pmodels/mpich/blob/b80a6d7c24defe7cdf6c57c52430f8075a0a41d6/README.vin#L562-L586
 , withPm ? [ "hydra" "gforker" ]
 , pmix
+# PMIX support is likely incompatible with process managers (`--with-pm`)
+# https://github.com/NixOS/nixpkgs/pull/274804#discussion_r1432601476
 , pmixSupport ? false
 } :
 
@@ -57,8 +59,8 @@ stdenv.mkDerivation  rec {
   '';
 
   meta = with lib; {
-    # --with-pmix silently disables gforker
-    broken = ((builtins.elem "gforker" processManagers) && pmixSupport);
+    # As far as we know, --with-pmix silently disables all of `--with-pm`
+    broken = pmixSupport && processManagers != [ ];
 
     description = "Implementation of the Message Passing Interface (MPI) standard";
 
