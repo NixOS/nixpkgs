ad736b8ecd5a59a98dba0a20e62edd9a475415e7
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-26 12:50:13 +0100

    python311Packages.aiopurpleair: 2023.10.0 -> 2023.12.0
    https://github.com/bachya/aiopurpleair/releases/tag/2023.12.0

--- pkgs/development/python-modules/aiopurpleair/default.nix
+++ pkgs/development/python-modules/aiopurpleair/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aiopurpleair";
-  version = "2023.10.0";
+  version = "2023.12.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "bachya";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-SNgdz3pnPyEH9qIi+aTBbEG6YBIXpwPYZuNJ4dEPLW8=";
+    hash = "sha256-2Ngo2pvzwcgQvpyW5Q97VQN/tGSVhVJwRj0DMaPn+O4=";
   };
 
   nativeBuildInputs = [
