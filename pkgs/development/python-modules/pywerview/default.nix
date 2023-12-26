ea3b769d47538b79a7c30d336b95fb9de0cfee5a
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-26 09:52:44 +0100

    python311Packages.pywerview: refactor

--- pkgs/development/python-modules/pywerview/default.nix
+++ pkgs/development/python-modules/pywerview/default.nix
@@ -9,21 +9,26 @@
 , pyasn1
 , pycryptodome
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "pywerview";
   version = "0.6";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "the-useless-one";
-    repo = pname;
+    repo ="pywerview";
     rev = "refs/tags/v${version}";
     hash = "sha256-WZE6qWq9v4A78YELMEcbgyufBRrVFRTqlhGmknpKn1Y=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     beautifulsoup4
