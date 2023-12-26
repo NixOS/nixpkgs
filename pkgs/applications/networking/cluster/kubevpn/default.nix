6dd3ebb98e2f01d26ebd582a4f9f45c0a7ac28a9
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-26 14:55:46 +0000

    kubevpn: 2.1.3 -> 2.2.0

--- pkgs/applications/networking/cluster/kubevpn/default.nix
+++ pkgs/applications/networking/cluster/kubevpn/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "kubevpn";
-  version = "2.1.3";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     owner  = "KubeNetworks";
     repo   = "kubevpn";
     rev    = "v${version}";
-    sha256 = "sha256-KHOqVVB1QjhhPUopeZd/A/htanmIoLCNhtnWrqyEN2A=";
+    sha256 = "sha256-n65z7L82qQE4Xao5W99zIkXGEx2BFM4n/6C1cuTJXsk=";
   };
 
   vendorHash = null;
