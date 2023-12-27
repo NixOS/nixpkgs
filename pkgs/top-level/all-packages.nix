58f6aba98bf51ad194a4d4c34fdcfcbad3d84272
Nick Cao <nickcao@nichi.co>
2023-12-26 19:29:11 -0500

    wasmer: 4.2.1 -> 4.2.5
    Diff: https://github.com/wasmerio/wasmer/compare/refs/tags/v4.2.1...v4.2.5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41375,7 +41375,7 @@ with pkgs;
   wamr = darwin.apple_sdk_11_0.callPackage ../development/interpreters/wamr { };
 
   wasmer = callPackage ../development/interpreters/wasmer {
-    llvmPackages = llvmPackages_14;
+    llvmPackages = llvmPackages_15;
     inherit (darwin.apple_sdk.frameworks) CoreFoundation SystemConfiguration Security;
   };
 
