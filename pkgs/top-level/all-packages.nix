a786eee28474f306e9b0eb883f5631c187c7be0c
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2023-12-27 14:17:09 +0100

    docker-machine-xhyve: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31118,10 +31118,6 @@ with pkgs;
   docker-machine-hyperkit = callPackage ../applications/networking/cluster/docker-machine/hyperkit.nix { };
   docker-machine-kvm = callPackage ../applications/networking/cluster/docker-machine/kvm.nix { };
   docker-machine-kvm2 = callPackage ../applications/networking/cluster/docker-machine/kvm2.nix { };
-  docker-machine-xhyve = callPackage ../applications/networking/cluster/docker-machine/xhyve.nix {
-    inherit (darwin.apple_sdk.frameworks) Hypervisor vmnet;
-    inherit (darwin) cctools;
-  };
 
   docker-distribution = callPackage ../applications/virtualization/docker/distribution.nix { };
 
