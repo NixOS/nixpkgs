071d3a6b5372024d3238b53c0333becb0f3bc569
Stig Palmquist <git@stig.io>
2023-12-27 13:30:07 +0100

    perlPackages.GnuPGInterface: 1.03 -> 1.04

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -10947,10 +10947,10 @@ with self; {
 
   GnuPGInterface = buildPerlPackage {
     pname = "GnuPG-Interface";
-    version = "1.03";
+    version = "1.04";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/B/BP/BPS/GnuPG-Interface-1.03.tar.gz";
-      hash = "sha256-WvVmMPD6wpDXJCGD9kSaoOAoKfRhHcYrxunps4CPGHo=";
+      url = "mirror://cpan/authors/id/B/BP/BPS/GnuPG-Interface-1.04.tar.gz";
+      hash = "sha256-1frwQkRtuyd4jp2kiyDYMD730Sxi0rajukFvYEv6xnM=";
     };
     buildInputs = [ pkgs.which pkgs.gnupg1compat ];
     propagatedBuildInputs = [ MooXHandlesVia MooXlate ];
