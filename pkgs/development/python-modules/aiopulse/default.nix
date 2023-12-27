c3fb7e04a03e8f3a8adf50c0cfba5382415b1810
Fabian Affolter <mail@fabian-affolter.ch>
2023-12-27 14:36:04 +0100

    python311Packages.aiopulse: refactor

--- pkgs/development/python-modules/aiopulse/default.nix
+++ pkgs/development/python-modules/aiopulse/default.nix
@@ -1,14 +1,15 @@
 { lib
+, async-timeout
 , buildPythonPackage
 , fetchPypi
-, async-timeout
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "aiopulse";
   version = "0.4.5";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
@@ -17,11 +18,15 @@ buildPythonPackage rec {
     hash = "sha256-yrltqnePNnNwsVGkfXgtrtL/FuazRvmoVL4L0H4j4Bg=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [
     async-timeout
   ];
 
-  # tests are not present
+  # Tests are not present
   doCheck = false;
 
   pythonImportsCheck = [
