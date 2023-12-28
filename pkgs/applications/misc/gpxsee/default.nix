e3e3eff41cc3c97ca2d6029b1a8da668b1e65a5a
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-28 05:58:14 +0000

    gpxsee-qt6: 13.12 -> 13.13

--- pkgs/applications/misc/gpxsee/default.nix
+++ pkgs/applications/misc/gpxsee/default.nix
@@ -18,13 +18,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "gpxsee";
-  version = "13.12";
+  version = "13.13";
 
   src = fetchFromGitHub {
     owner = "tumic0";
     repo = "GPXSee";
     rev = finalAttrs.version;
-    hash = "sha256-jHqxCOxkM7RJmJYq+nKJfSfd0LGQ7jZnUhuAZLFEG58=";
+    hash = "sha256-EUBExf2fpa4Y3T6pKnDoaZYhwE4XOJDMEn+LT1XxIYc=";
   };
 
   buildInputs = [
