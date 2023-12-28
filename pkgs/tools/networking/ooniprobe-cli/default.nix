c3e2e79d6e029b29618a427e42a6b9e6f9850751
Robert Schütz <nix@dotlambda.de>
2023-12-28 04:11:05 -0800

    ooniprobe-cli: 3.20.0 -> 3.20.1
    Diff: https://github.com/ooni/probe-cli/compare/v3.20.0...v3.20.1
    
    Changelog: https://github.com/ooni/probe-cli/releases/tag/v3.20.1

--- pkgs/tools/networking/ooniprobe-cli/default.nix
+++ pkgs/tools/networking/ooniprobe-cli/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "ooniprobe-cli";
-  version = "3.20.0";
+  version = "3.20.1";
 
   src = fetchFromGitHub {
     owner = "ooni";
     repo = "probe-cli";
     rev = "v${version}";
-    hash = "sha256-kOARV3tnRyOAScc3Vn96TFQFgqvTgi6NFHWM7ZbpciU=";
+    hash = "sha256-XjIrae4HPFB1Rv8yIAUh6Xj9UVU55Wx7SuyKJ0BvmXY=";
   };
 
-  vendorHash = "sha256-c922VpxtpNfua3Sx3vXKz4x1FsLMwbaSvRH4dyFrr9s=";
+  vendorHash = "sha256-HYU+oS+iqdl2jQJc3h9T+MSc/Hq2W6UqP+oPSEyfcOU=";
 
   subPackages = [ "cmd/ooniprobe" ];
 
