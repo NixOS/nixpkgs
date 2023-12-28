34d2421d7196d7382dda2b76fe3c28c9e606c06e
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-12-29 00:10:09 +0100

    satty: 0.8.2 -> 0.8.3
    Diff: https://github.com/gabm/Satty/compare/v0.8.2...v0.8.3

--- pkgs/by-name/sa/satty/package.nix
+++ pkgs/by-name/sa/satty/package.nix
@@ -15,16 +15,16 @@
 rustPlatform.buildRustPackage rec {
 
   pname = "satty";
-  version = "0.8.2";
+  version = "0.8.3";
 
   src = fetchFromGitHub {
     owner = "gabm";
     repo = "Satty";
     rev = "v${version}";
-    hash = "sha256-5FKEQUH43qx8w7s7lW8EDOWtWCUJTbWlXcMQazR8Thk=";
+    hash = "sha256-KCHKR6DP8scd9xdWi0bLw3wObrEi0tOsflXHa9f4Z5k=";
   };
 
-  cargoHash = "sha256-FpCmzU2C+5+5eSB5Mno+lOFd4trHXmfp6e5oV+CvU1c=";
+  cargoHash = "sha256-pUBtUC+WOuiypLUpXCPR1pu0fRrMVTxg7FE2JSaszNw=";
 
   nativeBuildInputs = [
     copyDesktopItems
