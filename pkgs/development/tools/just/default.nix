3ec22524f3788ae5fcdc0b79bcab4ff777018b36
Muhammad Falak R Wani <falakreyaz@gmail.com>
2023-12-29 13:13:03 +0530

    just: 1.18.1 -> 1.20.0
    Diff: https://github.com/casey/just/compare/refs/tags/1.18.1...1.20.0
    Changelog: https://github.com/casey/just/blob/1.20.0/CHANGELOG.md
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>
    (cherry picked from commit da6707b432093b24fedafc384ca27ac7892363b7)

--- pkgs/development/tools/just/default.nix
+++ pkgs/development/tools/just/default.nix
@@ -11,17 +11,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "just";
-  version = "1.18.1";
+  version = "1.20.0";
   outputs = [ "out" "man" "doc" ];
 
   src = fetchFromGitHub {
     owner = "casey";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-jmTSTx2WSLOtxy0gPCTonjcoy4o9FKA5aiQW3+wPrZQ=";
+    hash = "sha256-MTfxVUr5rQpu5AXJmP/7rOjeHSsX+iQqfBdYb8YWfiU=";
   };
 
-  cargoHash = "sha256-4kbvtmXkU5bhuC079K5NOGKVdqYvTileVNXSNLIV0ok=";
+  cargoHash = "sha256-lvqtt6RCy/SqzZXWRR5u2P9UOlHC5Hjg6UhYjxpS3as=";
 
   nativeBuildInputs = [ installShellFiles mdbook ];
   buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
