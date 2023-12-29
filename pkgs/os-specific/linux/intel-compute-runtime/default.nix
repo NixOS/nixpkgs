8f5f83dae1fc286dd99d332bb95f04edfa60f110
R. Ryantm <ryantm-bot@ryantm.com>
2023-12-29 08:26:18 +0000

    intel-compute-runtime: 23.35.27191.9 -> 23.48.27912.9

--- pkgs/os-specific/linux/intel-compute-runtime/default.nix
+++ pkgs/os-specific/linux/intel-compute-runtime/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-compute-runtime";
-  version = "23.35.27191.9";
+  version = "23.48.27912.9";
 
   src = fetchFromGitHub {
     owner = "intel";
     repo = "compute-runtime";
     rev = version;
-    hash = "sha256-yyuP9erK3bQ5oegLXPM+of6x7fslUXHPlX2q71lnGWA=";
+    hash = "sha256-aiELbwQiyERiDhNr3/43c3mYAuaMszwlpJi7sUFUL4U=";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
