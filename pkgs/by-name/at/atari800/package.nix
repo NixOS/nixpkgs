704823fe3c9ec66bb058161fd76cfcd4ce87ccca
Anderson Torres <torres.anderson.85@protonmail.com>
2023-12-29 14:55:48 -0300

    atari800: cleanup

--- pkgs/by-name/at/atari800/package.nix
+++ pkgs/by-name/at/atari800/package.nix
@@ -1,43 +1,60 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook
-, zlib, SDL, readline, libGLU, libGL, libX11 }:
+{ lib
+, stdenv
+, SDL
+, autoreconfHook
+, fetchFromGitHub
+, libGL
+, libGLU
+, libX11
+, readline
+, zlib
+}:
 
-with lib;
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "atari800";
   version = "5.0.0";
 
   src = fetchFromGitHub {
     owner = "atari800";
     repo = "atari800";
-    rev = "ATARI800_${replaceStrings ["."] ["_"] version}";
-    sha256 = "sha256-+eJXhqPyU0GhmzF7DbteTXzEnn5klCor9Io/UgXQfQg=";
+    hash = "sha256-+eJXhqPyU0GhmzF7DbteTXzEnn5klCor9Io/UgXQfQg=";
+    rev = "ATARI800_${lib.replaceStrings ["."] ["_"] finalAttrs.version}";
   };
 
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [
+    autoreconfHook
+  ];
 
-  buildInputs = [ zlib SDL readline libGLU libGL libX11 ];
+  buildInputs = [
+    SDL
+    libGL
+    libGLU
+    libX11
+    readline
+    zlib
+  ];
 
   configureFlags = [
     "--target=default"
-    "--with-video=sdl"
-    "--with-sound=sdl"
-    "--with-readline"
-    "--with-opengl"
-    "--with-x"
-    "--enable-riodevice"
+    (lib.enableFeature true "riodevice")
+    (lib.withFeature true "opengl")
+    (lib.withFeature true "readline")
+    (lib.withFeature true "x")
+    (lib.withFeatureAs true "sound" "sdl")
+    (lib.withFeatureAs true "video" "sdl")
   ];
 
   meta = {
     homepage = "https://atari800.github.io/";
     description = "An Atari 8-bit emulator";
     longDescription = ''
-      Atari800 is the emulator of Atari 8-bit computer systems and
-      5200 game console for Unix, Linux, Amiga, MS-DOS, Atari
-      TT/Falcon, MS-Windows, MS WinCE, Sega Dreamcast, Android and
-      other systems supported by the SDL library.
+      Atari800 is the emulator of Atari 8-bit computer systems and 5200 game
+      console for Unix, Linux, Amiga, MS-DOS, Atari TT/Falcon, MS-Windows, MS
+      WinCE, Sega Dreamcast, Android and other systems supported by the SDL
+      library.
     '';
-    maintainers = [ maintainers.AndersonTorres ];
-    license = licenses.gpl2Plus;
+    license = with lib.licenses; [ gpl2Plus ];
+    maintainers = with lib.maintainers; [ AndersonTorres ];
     platforms = lib.platforms.linux;
   };
-}
+})
