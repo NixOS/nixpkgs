ebba98b1192c57f735d71c4988b0f0266c2ccc8c
Ben Darwin <bcdarwin@gmail.com>
2023-12-29 13:08:18 -0500

    python311Packages.pylibjpeg-openjpeg: init at 2.2.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11308,6 +11308,8 @@ self: super: with self; {
 
   pylibjpeg-libjpeg = callPackage ../development/python-modules/pylibjpeg-libjpeg { };
 
+  pylibjpeg-openjpeg = callPackage ../development/python-modules/pylibjpeg-openjpeg { };
+
   pyliblo = callPackage ../development/python-modules/pyliblo { };
 
   pylibmc = callPackage ../development/python-modules/pylibmc { };
