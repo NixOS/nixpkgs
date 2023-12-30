4711e8868124ad860cea902b8ab6e3223e78a9e5
jopejoe1 <johannes@joens.email>
2023-12-30 01:00:48 +0100

    tty-clock: change license to bsd3
    found at https://github.com/xorg62/tty-clock/blob/master/LICENSE

--- pkgs/tools/misc/tty-clock/default.nix
+++ pkgs/tools/misc/tty-clock/default.nix
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     broken = stdenv.isDarwin;
     homepage = "https://github.com/xorg62/tty-clock";
-    license = licenses.free;
+    license = licenses.bsd3;
     description = "Digital clock in ncurses";
     platforms = platforms.all;
     maintainers = [ maintainers.koral ];
