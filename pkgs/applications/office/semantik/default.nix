303576b74cca43b00ee4d7b3c4825d48101fc709
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-12-30 17:36:59 +0900

    semantik: unmark broken on aarch64-linux

--- pkgs/applications/office/semantik/default.nix
+++ pkgs/applications/office/semantik/default.nix
@@ -84,7 +84,6 @@ mkDerivation rec {
   ];
 
   meta = with lib; {
-    broken = (stdenv.isLinux && stdenv.isAarch64);
     description = "A mind-mapping application for KDE";
     license = licenses.mit;
     homepage = "https://waf.io/semantik.html";
