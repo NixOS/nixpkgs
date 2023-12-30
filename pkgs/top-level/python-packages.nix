9f4fad502eb3109ca2f902a34ce07268d9d2fb80
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-30 16:59:07 +0100

    python311Packages.pysilero-vad: init at 1.0.0
    Pre-packaged voice activity detector using silero-vad

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9856,6 +9856,8 @@ self: super: with self; {
 
   pysiaalarm = callPackage ../development/python-modules/pysiaalarm { };
 
+  pysilero-vad = callPackage ../development/python-modules/pysilero-vad { };
+
   pysimplesoap = callPackage ../development/python-modules/pysimplesoap { };
 
   pyskyqhub = callPackage ../development/python-modules/pyskyqhub { };
