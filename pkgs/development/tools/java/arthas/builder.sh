5fb4229d97718d077e5065f88dc8d25d439e43b4
hitsmaxft <mfthits@gmail.com>
2023-12-31 04:14:20 -0800

    add arthas

---
+++ pkgs/development/tools/java/arthas/builder.sh
@@ -0,0 +1,34 @@
+
+unset PATH
+
+for p in $baseInputs $buildInputs; do
+  if [ -d $p/bin ]; then
+    export PATH="$p/bin${PATH:+:}$PATH"
+  fi
+  if [ -d $p/lib/pkgconfig ]; then
+    export PKG_CONFIG_PATH="$p/lib/pkgconfig${PKG_CONFIG_PATH:+:}$PKG_CONFIG_PATH"
+  fi
+done
+
+if [ -e .attrs.sh ]; then source .attrs.sh; fi
+source $stdenv/setup
+
+unpackPhase
+
+mkdir -p $out/arthas
+
+
+echo "binPath: ${binPATH}"
+
+echo $src
+ls -alh $src
+
+cp -r $src/* $out/arthas
+
+export baseDir="$out/arthas"
+
+makeWrapper $baseDir/as.sh $out/bin/as --set-default JAVA_HOME "$jdk" 
+
+makeWrapper $baseDir/as.sh $out/bin/arthas --set-default JAVA_HOME "$jdk"
+
+runHook postInstall
---
+++ pkgs/development/tools/java/arthas/default.nix
@@ -0,0 +1,57 @@
+
+{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }
+, lib ? pkgs.lib
+, stdenv ? pkgs.stdenv
+, fetchurl? pkgs.fetchurl
+, fetchzip? pkgs.fetchzip
+, jdk? pkgs.jdk
+, makeWrapper ? pkgs.makeWrapper
+}:
+stdenv.mkDerivation rec {
+  pname = "arthas";
+  version = "3.7.1";
+
+  inherit jdk;
+
+  inherit (pkgs)  gawk gnused gnugrep inetutils curl;
+
+  baseInputs = with pkgs; [gawk  inetutils curl];
+
+  src = fetchzip {
+      url = "https://github.com/alibaba/arthas/releases/download/arthas-all-${version}/arthas-bin.zip";
+      hash = "sha256-dN50xjQqfo17QwaQSSPcaHo3p6AfsGRQjRJ3MSwAbLg=";
+      # curlOptsList = ["-x" "http://127.0.0.1:7890"];
+      stripRoot=false;
+  };
+
+  builder = "${pkgs.bash}/bin/bash";
+  args = [./builder.sh ];
+
+
+  postInstall = ''
+        wrapProgram $out/arthas/as.sh \
+    --set PATH ${lib.makeBinPath [
+      pkgs.bash
+      pkgs.gawk
+      pkgs.gnugrep
+      pkgs.curl
+      pkgs.unzip
+      pkgs.inetutils
+      jdk
+      pkgs.coreutils
+    ]}
+'';
+
+  nativeBuildInputs = [ makeWrapper ];
+
+
+  system = builtins.currentSystem;
+
+  meta = with lib; {
+      description = "Java Diagnostic Tool,Arthas allows developers to troubleshoot production issues for Java applications without modifying code or restarting servers.";
+      homepage = "https://arthas.aliyun.com/";
+      license = licenses.apache;
+      platforms = platforms.mesaPlatforms;
+      maintainers = with maintainers; [ hitsmaxft ];
+  };
+  }
