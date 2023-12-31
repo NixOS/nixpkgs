aecca2ea3330f15c423a87e331c2fd38734bdb5c
Artemis Tosini <me@artem.ist>
2023-12-31 05:34:48 +0000

    freebsd.locale: init

--- pkgs/os-specific/bsd/freebsd/default.nix
+++ pkgs/os-specific/bsd/freebsd/default.nix
@@ -90,6 +90,7 @@ lib.makeScope newScope (self: with self; { inherit stdenv;
   libprocstat = callPackage ./libprocstat.nix {};
   libexecinfo = callPackage ./libexecinfo.nix {};
   localedef = callPackage ./localedef.nix {};
+  locale = callPackage ./locale.nix {};
   config = callPackage ./config.nix {};
   libsbuf = callPackage ./libsbuf.nix {};
   file2c = callPackage ./file2c.nix {};
---
+++ pkgs/os-specific/bsd/freebsd/locale.nix
@@ -0,0 +1,10 @@
+{ mkDerivation, libsbuf, ... }:
+mkDerivation {
+  path = "usr.bin/locale";
+
+  buildInputs = [ libsbuf ];
+
+  extraPaths = [ "lib/libc/locale" ];
+
+  MK_TESTS = "no";
+}
