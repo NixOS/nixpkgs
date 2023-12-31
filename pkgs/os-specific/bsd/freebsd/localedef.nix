3b55fa3d00cad474001245aad8cadfc33e209234
Audrey Dutcher <rhelmot@emotionlabs.io>
2023-12-31 15:12:48 -0800

    freebsd.localedef: fix for linux compilation

--- pkgs/os-specific/bsd/freebsd/localedef.nix
+++ pkgs/os-specific/bsd/freebsd/localedef.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, buildPackages, buildFreebsd, lib, stdenv, ... }:
-mkDerivation {
+{ patchesRoot, mkDerivation, buildPackages, buildFreebsd, lib, stdenv, compat, ... }:
+mkDerivation ({
   path = "usr.bin/localedef";
 
-  extraPaths = [ "lib/libc/locale" "lib/libc/stdtime" "include" ];
+  extraPaths = [ "lib/libc/locale" "lib/libc/stdtime" "include" "." ];
 
   nativeBuildInputs = (with buildPackages; [
     bsdSetupHook
@@ -12,11 +12,16 @@ mkDerivation {
     bmakeMinimal install
   ]);
 
-  preBuild = lib.optionalString stdenv.cc.isClang ''
-    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -D_VA_LIST -D_VA_LIST_DECLARED -Dva_list=__builtin_va_list -D_SIZE_T -D_WCHAR_T"
-  '' + lib.optionalString (!stdenv.hostPlatform.isFreeBSD) ''
-    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I $src/usr.bin/localedef/bootstrap"
+  buildInputs = [];
+
+  preBuild = lib.optionalString (!stdenv.hostPlatform.isFreeBSD) ''
+    export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I${compat}/include -D__unused= -D__pure= -D_WCHAR_T -Dwchar_t=short -Wno-strict-aliasing"
+    export NIX_LDFLAGS="$NIX_LDFLAGS -L${compat}/lib"
   '';
 
+  patches = lib.optionals (!stdenv.hostPlatform.isFreeBSD) [ /${patchesRoot}/localedef.patch ];
+
   MK_TESTS = "no";
-}
+} // lib.optionalAttrs (!stdenv.hostPlatform.isFreeBSD) {
+  BOOTSTRAPPING = 1;
+})
