a279d74f99d44f3a20748b39cdb7a3a55cc1c062
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-01 06:16:32 +0000

    plocate: 1.1.19 -> 1.1.20

--- pkgs/tools/misc/plocate/default.nix
+++ pkgs/tools/misc/plocate/default.nix
@@ -10,12 +10,12 @@
 }:
 stdenv.mkDerivation rec {
   pname = "plocate";
-  version = "1.1.19";
+  version = "1.1.20";
 
   src = fetchgit {
     url = "https://git.sesse.net/plocate";
     rev = version;
-    sha256 = "sha256-Vf/NgUPDL3KWMpjnyB2QR2sU6rQfPIADNU6OlpN+O0M=";
+    sha256 = "sha256-Nc39wPVW+GpmT8X8q/VbrPhPxO/PgFBPTOCWAkkUfDY=";
   };
 
   postPatch = ''
