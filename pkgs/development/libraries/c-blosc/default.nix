18ee31152a86519ecf045c9bf4580a0b5c8ac245
Robert Scott <code@humanleg.org.uk>
2024-01-01 16:50:21 +0000

    c-blosc: 1.21.1 -> 1.21.5

--- pkgs/development/libraries/c-blosc/default.nix
+++ pkgs/development/libraries/c-blosc/default.nix
@@ -1,15 +1,27 @@
-{ lib, stdenv, fetchFromGitHub, cmake }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+}:
 
 stdenv.mkDerivation rec {
   pname = "c-blosc";
-  version = "1.21.1";
+  version = "1.21.5";
 
   src = fetchFromGitHub {
     owner = "Blosc";
     repo = "c-blosc";
     rev = "v${version}";
-    sha256 = "sha256-6SKEyciwDOxcbO8chvmxrLCxLkc93zxo6eH0c/lRyT8=";
+    sha256 = "sha256-bz922lWiap3vMy8qS9dmXa8zUg5NJlg0bx3+/xz7QAk=";
   };
+
+  # https://github.com/NixOS/nixpkgs/issues/144170
+  postPatch = ''
+    sed -i -E \
+      -e '/^libdir[=]/clibdir=@CMAKE_INSTALL_FULL_LIBDIR@' \
+      -e '/^includedir[=]/cincludedir=@CMAKE_INSTALL_FULL_INCLUDEDIR@' \
+      blosc.pc.in
+  '';
 
   nativeBuildInputs = [ cmake ];
 
@@ -18,5 +30,6 @@ stdenv.mkDerivation rec {
     homepage = "https://www.blosc.org";
     license = licenses.bsd3;
     platforms = platforms.all;
+    maintainers = with maintainers; [ ris ];
   };
 }
