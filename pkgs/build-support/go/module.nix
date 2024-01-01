7b4e7be13be07e2733474de9eed4736a602f427c
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-01 20:09:03 +0100

    buildGoModule: announce removal of `buildFlags` and `buildFlagsArray`

--- pkgs/build-support/go/module.nix
+++ pkgs/build-support/go/module.nix
@@ -305,6 +305,6 @@ let
 in
 lib.warnIf (args' ? vendorSha256) "`vendorSha256` is deprecated. Use `vendorHash` instead"
 lib.warnIf (buildFlags != "" || buildFlagsArray != "")
-  "Use the `ldflags` and/or `tags` attributes instead of `buildFlags`/`buildFlagsArray`"
+  "`buildFlags`/`buildFlagsArray` are deprecated and will be removed in the 24.11 release. Use the `ldflags` and/or `tags` attributes instead"
 lib.warnIf (builtins.elem "-buildid=" ldflags) "`-buildid=` is set by default as ldflag by buildGoModule"
   package
