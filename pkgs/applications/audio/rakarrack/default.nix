6293f614787c863a88a6f0ba8d38534383dbaeb1
Isidor Zeuner <nix@quidecco.pl>
2024-01-02 15:53:57 +0100

    rakarrack: fix ALSA plugins load path

--- pkgs/applications/audio/rakarrack/default.nix
+++ pkgs/applications/audio/rakarrack/default.nix
@@ -1,7 +1,21 @@
-{ lib, stdenv, fetchurl, alsa-lib, alsa-utils, fltk, libjack2, libXft,
-libXpm, libjpeg, libpng, libsamplerate, libsndfile, zlib }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  alsa-lib-with-plugins,
+  alsa-utils,
+  fltk,
+  libjack2,
+  libXft,
+  libXpm,
+  libjpeg,
+  libpng,
+  libsamplerate,
+  libsndfile,
+  zlib,
+}:
 
-stdenv.mkDerivation  rec {
+stdenv.mkDerivation rec {
   pname = "rakarrack";
   version = "0.6.1";
 
@@ -14,8 +28,19 @@ stdenv.mkDerivation  rec {
 
   patches = [ ./fltk-path.patch ];
 
-  buildInputs = [ alsa-lib alsa-utils fltk libjack2 libXft libXpm libjpeg
-    libpng libsamplerate libsndfile zlib ];
+  buildInputs = [
+    alsa-lib-with-plugins
+    alsa-utils
+    fltk
+    libjack2
+    libXft
+    libXpm
+    libjpeg
+    libpng
+    libsamplerate
+    libsndfile
+    zlib
+  ];
 
   meta = with lib; {
     description = "Multi-effects processor emulating a guitar effects pedalboard";
