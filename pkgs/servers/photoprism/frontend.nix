1e5f255c115bd2bc63c76f9775cbc1fa1b8de655
Pascal Bach <pascal.bach@nextrem.ch>
2024-01-03 00:41:48 +0100

    photoprism: 231011-63f708417 -> 231128-f48ff16ef

--- pkgs/servers/photoprism/frontend.nix
+++ pkgs/servers/photoprism/frontend.nix
@@ -8,7 +8,7 @@ buildNpmPackage {
     cd frontend
   '';
 
-  npmDepsHash = "sha256-v7G06x/6MAFlOPbmkdh9Yt9/0BcMSYXI5EUmIHKiVFo=";
+  npmDepsHash = "sha256-4bAWSSy7kvG1ChzEb2iuxgWxnkjtJlr7Lm7USbgsmAw=";
 
   installPhase = ''
     runHook preInstall
