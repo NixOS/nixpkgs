528610c6891831c96bd6613309a65270eec2b9c2
Claudio Bley <claudio.bley@gmail.com>
2024-01-02 21:06:44 +0100

    mediathekview: Pass expected JVM arguments
    This prevents the warning dialog about incorrect JVM parameters and disables the update check.

--- pkgs/applications/video/mediathekview/default.nix
+++ pkgs/applications/video/mediathekview/default.nix
@@ -14,6 +14,18 @@ stdenv.mkDerivation rec {
   installPhase =
   let
     libraryPath = lib.strings.makeLibraryPath [ libglvnd libnotify ];
+    # add JVM args from
+    # https://github.com/mediathekview/MediathekView/blob/9105485f50ec10d863727b4817c8c4ffcbb02643/pom.xml#L136-L139
+    jvmArgList = [
+      "-XX:+UseShenandoahGC"
+      "-XX:ShenandoahGCHeuristics=compact"
+      "-XX:MaxRAMPercentage=50.0"
+      "-XX:+UseStringDeduplication"
+      "--add-opens=java.desktop/sun.awt.X11=ALL-UNNAMED"
+      "-Dfile.encoding=UTF-8"
+      "-DexternalUpdateCheck" # disable checking for updates
+    ];
+    jvmArgs = lib.strings.concatStringsSep " " jvmArgList;
   in
   ''
     runHook preInstall
@@ -24,15 +36,15 @@ stdenv.mkDerivation rec {
     install -m644 MediathekView.svg $out/share/pixmaps
 
     makeWrapper ${jre}/bin/java $out/bin/mediathek \
-      --add-flags "-jar $out/lib/MediathekView.jar" \
+      --add-flags "${jvmArgs} -jar $out/lib/MediathekView.jar" \
       --suffix LD_LIBRARY_PATH : "${libraryPath}"
 
     makeWrapper ${jre}/bin/java $out/bin/MediathekView \
-      --add-flags "-jar $out/lib/MediathekView.jar" \
+      --add-flags "${jvmArgs} -jar $out/lib/MediathekView.jar" \
       --suffix LD_LIBRARY_PATH : "${libraryPath}"
 
     makeWrapper ${jre}/bin/java $out/bin/MediathekView_ipv4 \
-      --add-flags "-Djava.net.preferIPv4Stack=true -jar $out/lib/MediathekView.jar" \
+      --add-flags "${jvmArgs} -Djava.net.preferIPv4Stack=true -jar $out/lib/MediathekView.jar" \
       --suffix LD_LIBRARY_PATH : "${libraryPath}"
 
     runHook postInstall
