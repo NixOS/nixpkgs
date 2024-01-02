2c764ccdaf76df54c42432a02d706a72c96a1c84
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-02 14:05:16 +0100

    python311Packages.boschshcpy: refactor

--- pkgs/development/python-modules/boschshcpy/default.nix
+++ pkgs/development/python-modules/boschshcpy/default.nix
@@ -5,22 +5,27 @@
 , getmac
 , pythonOlder
 , requests
+, setuptools
 , zeroconf
 }:
 
 buildPythonPackage rec {
   pname = "boschshcpy";
   version = "0.2.88";
-  format = "setuptools";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.10";
 
   src = fetchFromGitHub {
     owner = "tschamm";
-    repo = pname;
-    rev = version;
+    repo = "boschshcpy";
+    rev = "refs/tags/${version}";
     hash = "sha256-tyx7VJGsU9YYNJQy1mly0AgwKULZ1BWeRzz1BDgXrUU=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     cryptography
