662d10fc8ce9f08580e7ad64c2368a9be8090e55
Robert Schütz <nix@dotlambda.de>
2024-01-02 01:57:48 -0800

    imagemagick: 7.1.1-24 -> 7.1.1-25
    Diff: https://github.com/ImageMagick/ImageMagick/compare/7.1.1-24...7.1.1-25
    
    Changelog: https://github.com/ImageMagick/Website/blob/main/ChangeLog.md

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -49,13 +49,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "imagemagick";
-  version = "7.1.1-24";
+  version = "7.1.1-25";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     rev = finalAttrs.version;
-    hash = "sha256-oQ/g2/OhZWKvh//QevYsyi8uNwgo1yuihT5728eVKF8=";
+    hash = "sha256-HKDeeh8DNj0y7wS4DqctXhmNaOqZ02JeBXRFrEpH0M4=";
   };
 
   outputs = [ "out" "dev" "doc" ]; # bin/ isn't really big
