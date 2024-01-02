d8f20e7c7bb922d695876cc8040c8515167c8989
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-02 05:57:35 +0000

    besu: 23.10.2 -> 23.10.3

--- pkgs/applications/blockchains/besu/default.nix
+++ pkgs/applications/blockchains/besu/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "besu";
-  version = "23.10.2";
+  version = "23.10.3";
 
   src = fetchurl {
     url = "https://hyperledger.jfrog.io/artifactory/${pname}-binaries/${pname}/${version}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-JVgYpcYGejiqi1ZdjzKkmhcqdTah03BnO7t19UgmPCw=";
+    sha256 = "sha256-c8g0zzLHu+JV19jMfKXR6w34QwuRFJNcjc86Z1sqy8I=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
