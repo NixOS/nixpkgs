eb97e9f924943c968545b64e3df77a73fc62b917
GetPsyched <priyanshu@getpsyched.dev>
2024-01-04 00:06:58 +0530

    atlauncher: make binary name static
    Align this name with meta.mainProgram as that points to the main binary of a
    package. Overriding the pname should not break this.

--- pkgs/by-name/at/atlauncher/package.nix
+++ pkgs/by-name/at/atlauncher/package.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir -p $out/bin $out/share/java
     cp $src $out/share/java/ATLauncher.jar
 
-    makeWrapper ${jre}/bin/java $out/bin/${finalAttrs.pname} \
+    makeWrapper ${jre}/bin/java $out/bin/atlauncher \
       --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ xorg.libXxf86vm udev ]}" \
       --add-flags "-jar $out/share/java/ATLauncher.jar" \
       --add-flags "--working-dir \"\''${XDG_DATA_HOME:-\$HOME/.local/share}/ATLauncher\"" \
