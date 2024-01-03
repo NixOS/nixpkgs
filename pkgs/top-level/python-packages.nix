d0c684e8f8f5f965c722ef14c43308ad4af1dc85
Tom Hubrecht <tom@hubrecht.ovh>
2024-01-03 12:54:15 +0100

    pythonPackages.flask-multipass: init at 0.5.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4137,6 +4137,8 @@ self: super: with self; {
 
   flask-mongoengine = callPackage ../development/python-modules/flask-mongoengine { };
 
+  flask-multipass = callPackage ../development/python-modules/flask-multipass { };
+
   flask-mysqldb = callPackage ../development/python-modules/flask-mysqldb { };
 
   flask-openid = callPackage ../development/python-modules/flask-openid { };
