386d36f062aad4d986fe416eaf49aedb665cf03a
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2024-01-03 23:50:00 +0300

    Add truebad0ur to maintainers

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18907,6 +18907,12 @@
     githubId = 483735;
     name = "Dmitry Geurkov";
   };
+  truebad0ur = {
+    email = "pieceofcakecupofcoffee@gmail.com";
+    github = "TrueBad0ur";
+    githubId = 34392986;
+    name = "Andrey Orekhov";
+  };
   truh = {
     email = "jakob-nixos@truh.in";
     github = "truh";
