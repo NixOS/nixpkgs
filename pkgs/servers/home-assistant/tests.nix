6e3207ae07f0dd9bc5e48fd9251b1544672108e7
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-01-04 00:13:34 +0100

    home-assistant: mark telegram test broken

--- pkgs/servers/home-assistant/tests.nix
+++ pkgs/servers/home-assistant/tests.nix
@@ -138,6 +138,8 @@ in lib.listToAttrs (map (component: lib.nameValuePair component (
       broken = lib.elem component [
         # pinned version incompatible with urllib3>=2.0
         "telegram_bot"
+        # depends on telegram_bot
+        "telegram"
       ];
       # upstream only tests on Linux, so do we.
       platforms = lib.platforms.linux;
