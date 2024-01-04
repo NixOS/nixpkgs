9163222bd0df47e5e5a435d4d8d1debe72508dfd
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-04 21:25:31 +0100

    python311Packages.habluetooth: 2.0.1 -> 2.0.2
    Diff: https://github.com/Bluetooth-Devices/habluetooth/compare/refs/tags/v2.0.1...v2.0.2
    
    Changelog: https://github.com/Bluetooth-Devices/habluetooth/blob/v2.0.2/CHANGELOG.md

--- pkgs/development/python-modules/habluetooth/default.nix
+++ pkgs/development/python-modules/habluetooth/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "habluetooth";
-  version = "2.0.1";
+  version = "2.0.2";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = "habluetooth";
     rev = "refs/tags/v${version}";
-    hash = "sha256-vUq3WVo9dAt4Q/oid9ixLwI6v6jCavRU9FZYR6t6TzI=";
+    hash = "sha256-3HyFKg+JR48MQrWmOjOQV2qhVHRHLnJHvtvBajXPDMg=";
   };
 
   postPatch = ''
