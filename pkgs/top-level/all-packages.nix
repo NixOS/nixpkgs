ceca46a62cd9b7de74ce2171522c2027fd75376f
Anderson Torres <torres.anderson.85@protonmail.com>
2024-01-04 11:05:43 -0300

    loksh: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15426,8 +15426,6 @@ with pkgs;
 
   oksh = callPackage ../shells/oksh { };
 
-  loksh = callPackage ../shells/loksh { };
-
   scponly = callPackage ../shells/scponly { };
 
   rush = callPackage ../shells/rush { };
