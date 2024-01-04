948199a97c52c8954643fe78bd0545e2c430f853
Nicolas Benes <nbenes.gh@xandea.de>
2024-01-04 22:56:19 +0100

    box2d: 2.3.1 -> 2.4.1
    * https://github.com/erincatto/box2d/releases/tag/v2.4.0
    * https://github.com/erincatto/box2d/releases/tag/v2.4.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20583,7 +20583,9 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) CoreServices Security;
   };
 
-  box2d = callPackage ../development/libraries/box2d { };
+  box2d = callPackage ../development/libraries/box2d {
+    inherit (pkgs.darwin.apple_sdk.frameworks) Carbon Cocoa IOKit;
+  };
 
   boxfort = callPackage ../development/libraries/boxfort { };
 
