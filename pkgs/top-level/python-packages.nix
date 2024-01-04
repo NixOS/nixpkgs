0740d47320a775c677cd01a8dc02d9f7fc09a796
Preston Hunt <me@prestonhunt.com>
2024-01-04 13:20:50 -0800

    python3Packages.pyemvue: init at 0.18.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10326,6 +10326,8 @@ self: super: with self; {
 
   pyemd = callPackage ../development/python-modules/pyemd { };
 
+  pyemvue = callPackage ../development/python-modules/pyemvue { };
+
   pyenchant = callPackage ../development/python-modules/pyenchant {
     inherit (pkgs) enchant2;
   };
