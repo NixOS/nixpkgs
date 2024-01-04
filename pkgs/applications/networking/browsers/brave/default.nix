a2ea57bbd0b798a3e0e45e763ebeaf2e71bf708d
Sean Buckley <sean.bck@gmail.com>
2024-01-04 17:08:19 -0500

    brave: 1.61.109 -> 1.61.114
    https://community.brave.com/t/release-channel-1-61-114/524569
    (cherry picked from commit 26c23f6c74f423da7ff9e55924e2db1a4d073c86)

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -92,11 +92,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "brave";
-  version = "1.61.109";
+  version = "1.61.114";
 
   src = fetchurl {
     url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-    hash = "sha256-vIi205FqgoQEZCV4iWCFxjH2hJNWH9HjRU94jt7Ee8A=";
+    hash = "sha256-AVL08Npg1nuvFJrd3rC2rCZeoLnPuQsgpvf2R623c6Y=";
   };
 
   dontConfigure = true;
