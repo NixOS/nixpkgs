919956e5ed65b418a4ae27375734a3510797486e
Andrey <34392986+TrueBad0ur@users.noreply.github.com>
2024-01-04 15:44:59 +0300

    Update pkgs/tools/misc/rogauracore/default.nix
    Co-authored-by: Robert Schütz <github@dotlambda.de>

--- pkgs/tools/misc/rogauracore/default.nix
+++ pkgs/tools/misc/rogauracore/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   pname = "rogauracore";
   version = "1.6.0";
 
-  src = fetchgit {
+  src = fetchFromGitHub {
     url = "https://github.com/wroberts/rogauracore";
     rev = "a872431a59e47c1ab0b2a523e413723bdcd93a6e";
     sha256 = "sha256-SeG6B9ksWH4/UjLq5yPncVMTYjqMOxOh2R3N0q29fQ0=";
