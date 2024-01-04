604025b852c4adbdd09b50c045cf76e9cf2acc44
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-01-04 09:45:36 -0500

    signal-desktop-beta: 6.43.0-beta.1 -> 6.44.0-beta.1
    (cherry picked from commit 6ee8bf84b4fc495dd0bfb84d9c80d00c93dd01ee)

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix {} rec {
   pname = "signal-desktop-beta";
   dir = "Signal Beta";
-  version = "6.43.0-beta.1";
+  version = "6.44.0-beta.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-7UlfpOWAalJjZjAJLa2CL3HdR2LFlE1/5sdec5Sj/tg=";
+  hash = "sha256-SW/br1k7lO0hQngST0qV9Qol1hA9f1NZe86A5uyYhcI=";
 }
