661ca76879facd12c5f3b804962d806427c55f5a
Bobby Rong <rjl931189261@126.com>
2024-01-05 21:12:04 +0800

    cinnamon.cinnamon-translations: 6.0.1 -> 6.0.2
    https://github.com/linuxmint/cinnamon-translations/compare/6.0.1...6.0.2

--- pkgs/desktops/cinnamon/cinnamon-translations/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-translations/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-translations";
-  version = "6.0.1";
+  version = "6.0.2";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-nzPveo48rLu5CFEXj1GV3cJG6DepAFosWBibxoiYvIs=";
+    hash = "sha256-kLZ0niamPV5Kaq6ZBTp1SMAl6dKMkcC+rodtAoH5+Go=";
   };
 
   nativeBuildInputs = [
