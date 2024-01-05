d163ea4133312f04ebf8eb4875ea4b61400b4b5e
networkException <git@nwex.de>
2024-01-05 17:29:51 +0100

    ungoogled-chromium: 120.0.6099.129-1 -> 120.0.6099.199-1
    https://chromereleases.googleblog.com/2024/01/stable-channel-update-for-desktop.html
    
    This update includes 6 security fixes.
    
    CVEs:
    CVE-2024-0222 CVE-2024-0223 CVE-2024-0224 CVE-2024-0225

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -28,12 +28,12 @@
         version = "2023-10-23";
       };
       ungoogled-patches = {
-        hash = "sha256-kVhAa/+RnYEGy7McysqHsb3ysPIILnxGXe6BTLbioQk=";
-        rev = "120.0.6099.129-1";
+        hash = "sha256-B1MNo8BdjMOmTvIr4uu3kg/MO1t+YLQz2S23L4Cye3E=";
+        rev = "120.0.6099.199-1";
       };
     };
-    hash = "sha256-6RURdPU1k3GaQAgA1LMQ0NhSGBEpOEJBPvk2QjLdoHo=";
-    hash_deb_amd64 = "sha256-0FB1gTbsjqFRy0ocE0w5ACtD9kSJ5AMnxg+qBxqCulc=";
-    version = "120.0.6099.129";
+    hash = "sha256-lT1CCwYj0hT4tCJb689mZwNecUsEwcfn2Ot8r9LBT+M=";
+    hash_deb_amd64 = "sha256-4BWLn0+gYNWG4DsolbY6WlTvXWl7tZIZrnqXlrGUGjQ=";
+    version = "120.0.6099.199";
   };
 }
