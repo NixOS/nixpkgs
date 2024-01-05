cc86f3c38be573dbb00959781b3bf2b17139bbab
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-05 07:43:49 +0000

    signal-desktop-beta: 6.43.0-beta.1 -> 6.44.0-beta.2

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix {} rec {
   pname = "signal-desktop-beta";
   dir = "Signal Beta";
-  version = "6.43.0-beta.1";
+  version = "6.44.0-beta.2";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-7UlfpOWAalJjZjAJLa2CL3HdR2LFlE1/5sdec5Sj/tg=";
+  hash = "sha256-hIU774dUaZTqa+nj/uNt7e2Ns84xlmheh+0WtzmoJas=";
 }
