932ac8f0fb333adc5c749883b6503618a485361a
Yureka <yuka@yuka.dev>
2024-01-06 02:01:19 +0100

    blender: build with openexr 3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30649,6 +30649,7 @@ with pkgs;
   blender = callPackage  ../applications/misc/blender {
     # LLVM 11 crashes when compiling GHOST_SystemCocoa.mm
     stdenv = if stdenv.isDarwin then llvmPackages_10.stdenv else stdenv;
+    openexr = openexr_3;
     inherit (darwin.apple_sdk.frameworks) Cocoa CoreGraphics ForceFeedback OpenAL OpenGL;
   };
 
