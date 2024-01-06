8708b24912063d3a021d7899cb0f97f6efc46e11
jopejoe1 <johannes@joens.email>
2024-01-06 13:29:24 +0100

    firefox-devedition-bin-unwrapped: update channel references
    the channel for firefox-devedition-bin-unwrapped got changed in #256292 updating some stuff related to the update script that relied on the channel

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31922,7 +31922,7 @@ with pkgs;
   firefox-devedition-bin-unwrapped = callPackage ../applications/networking/browsers/firefox-bin {
     inherit (gnome) adwaita-icon-theme;
     channel = "developer-edition";
-    generated = import ../applications/networking/browsers/firefox-bin/devedition_sources.nix;
+    generated = import ../applications/networking/browsers/firefox-bin/developer-edition_sources.nix;
   };
 
   firefox-devedition-bin = res.wrapFirefox firefox-devedition-bin-unwrapped {
