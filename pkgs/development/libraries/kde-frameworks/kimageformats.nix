9d6a688fa13618110201a2cc984b459f3cdb63c8
Yureka <yuka@yuka.dev>
2024-01-06 03:04:14 +0100

    plasma5Packages.kimageformats: fix build with openexr 3

--- pkgs/development/libraries/kde-frameworks/kimageformats.nix
+++ pkgs/development/libraries/kde-frameworks/kimageformats.nix
@@ -1,7 +1,7 @@
 {
   mkDerivation, lib,
   extra-cmake-modules,
-  ilmbase, karchive, openexr, libavif, libheif, libjxl, libraw, qtbase
+  karchive, openexr, libavif, libheif, libjxl, libraw, qtbase
 }:
 
 let inherit (lib) getDev; in
@@ -12,7 +12,6 @@ mkDerivation {
   nativeBuildInputs = [ extra-cmake-modules ];
   buildInputs = [ karchive openexr libavif libheif libjxl libraw qtbase ];
   outputs = [ "out" ]; # plugins only
-  CXXFLAGS = "-I${getDev ilmbase}/include/OpenEXR";
   cmakeFlags = [
     "-DKIMAGEFORMATS_HEIF=ON"
   ];
