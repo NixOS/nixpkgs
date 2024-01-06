92a401fb19086b2ffdd55741c5c67be533173719
amesgen <amesgen@amesgen.de>
2024-01-06 18:47:31 +0100

    cddl: 0.8.9 -> 0.10.3

--- pkgs/development/tools/cddl/default.nix
+++ pkgs/development/tools/cddl/default.nix
@@ -1,10 +1,33 @@
-{ lib, bundlerApp, bundlerUpdateScript }:
+{ lib
+, bundlerApp
+, bundlerUpdateScript
+, makeBinaryWrapper
+}:
 
-bundlerApp {
+bundlerApp rec {
   pname = "cddl";
 
-  gemdir = ./.;
+  gemfile = ./Gemfile;
+  lockfile = ./Gemfile.lock;
+
+  gemset = lib.recursiveUpdate (import ./gemset.nix) ({
+    "cddl" = {
+      dontBuild = false;
+      # setting env vars is not supported by patchShebangs
+      postPatch = ''
+        sed -i 's\#!/usr/bin/env RUBY_THREAD_VM_STACK_SIZE=5000000\#!/usr/bin/env\' bin/cddl
+      '';
+    };
+  });
+
   exes = [ "cddl" ];
+
+  nativeBuildInputs = [ makeBinaryWrapper ];
+
+  postBuild = ''
+    wrapProgram $out/bin/cddl \
+      --set RUBY_THREAD_VM_STACK_SIZE 5000000
+  '';
 
   passthru.updateScript = bundlerUpdateScript "cddl";
 
