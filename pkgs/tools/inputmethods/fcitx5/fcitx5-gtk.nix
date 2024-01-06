1f24f02f50a1281e210eea30ccd13f400a8c1c69
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-06 05:25:10 +0000

    fcitx5-gtk: 5.1.0 -> 5.1.1

--- pkgs/tools/inputmethods/fcitx5/fcitx5-gtk.nix
+++ pkgs/tools/inputmethods/fcitx5/fcitx5-gtk.nix
@@ -26,13 +26,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fcitx5-gtk";
-  version = "5.1.0";
+  version = "5.1.1";
 
   src = fetchFromGitHub {
     owner = "fcitx";
     repo = pname;
     rev = version;
-    sha256 = "sha256-xVBmFFUnlWqviht/KGFTHCd3xCln/6hyBG72tIHqopc=";
+    sha256 = "sha256-Ex24cHTsYsZjP8o+vrCdgGogk1UotWpd8xaLZAqzgaQ=";
   };
 
   outputs = [ "out" "dev" ];
