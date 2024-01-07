ee967c77a2cea3caf4981a6aec84b33133165a59
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-01-07 20:36:08 +0100

    fira-mono: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29322,8 +29322,6 @@ with pkgs;
 
   fira-go = callPackage ../data/fonts/fira-go { };
 
-  fira-mono = callPackage ../data/fonts/fira-mono { };
-
   flat-remix-icon-theme = callPackage ../data/icons/flat-remix-icon-theme {
     inherit (plasma5Packages) breeze-icons;
   };
