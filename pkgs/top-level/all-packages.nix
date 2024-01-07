6ad7e6fb312a15d05766e4be5723bd1c25d20bf2
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-07 10:24:10 +0100

    Revert "gocode: remove"

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28378,6 +28378,8 @@ with pkgs;
 
   go-outline = callPackage ../development/tools/go-outline { };
 
+  gocode = callPackage ../development/tools/gocode { };
+
   gocode-gomod = callPackage ../development/tools/gocode-gomod { };
 
   goconst = callPackage ../development/tools/goconst { };
