27fe24d5a70190f79f9cab36ff30325895e827dd
Bobby Rong <rjl931189261@126.com>
2024-01-07 07:47:38 +0000

    gnome.gnome-software: 45.2 → 45.3
    https://gitlab.gnome.org/GNOME/gnome-software/-/compare/45.2...45.3

--- pkgs/desktops/gnome/core/gnome-software/default.nix
+++ pkgs/desktops/gnome/core/gnome-software/default.nix
@@ -46,11 +46,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "gnome-software";
-  version = "45.2";
+  version = "45.3";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-software/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "C92PwMrs1usBPGAQ28qTB3OXEYpu9eryZOKoIKKS9bc=";
+    sha256 = "1rkkWyIjfae9FzndKMI8yPODX5n6EMEDfZ3XY1M1JRw=";
   };
 
   patches = [
