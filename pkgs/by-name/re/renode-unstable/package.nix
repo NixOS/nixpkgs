fac4ee51af07a8b0ca7326b61e22b179d220032b
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-07 05:41:30 +0000

    renode-unstable: 1.14.0+20231229gita76dac0ae -> 1.14.0+20240106git1b3952c2c

--- pkgs/by-name/re/renode-unstable/package.nix
+++ pkgs/by-name/re/renode-unstable/package.nix
@@ -7,10 +7,10 @@
   inherit buildUnstable;
 }).overrideAttrs (finalAttrs: _: {
   pname = "renode-unstable";
-  version = "1.14.0+20231229gita76dac0ae";
+  version = "1.14.0+20240106git1b3952c2c";
 
   src = fetchurl {
     url = "https://builds.renode.io/renode-${finalAttrs.version}.linux-portable.tar.gz";
-    hash = "sha256-fvwNN3sT8VZ7XJPrfpAbjSiuAB274QhuPeekwz0AU3c=";
+    hash = "sha256-tDo/01jYoq1Qg8h0BS4BQSPh3rsINpe72eMk9UBWgR0=";
   };
 })
