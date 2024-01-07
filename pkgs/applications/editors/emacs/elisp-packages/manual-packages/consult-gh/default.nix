5f0330e33b5a4195bd9b220fd0242fd5255ec3e9
Anderson Torres <torres.anderson.85@protonmail.com>
2024-01-07 20:48:32 -0300

    emacs.pkgs.consult-gh: convert to a new rec-less format

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/consult-gh/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/consult-gh/default.nix
@@ -4,49 +4,44 @@
 , consult
 , embark
 , forge
-, gh
 , markdown-mode
 , writeText
-, unstableGitUpdater
 }:
 
 let
-  commit = "1fe876d9552b6ec6af257a4299a34eca99b40539";
+  argset = {
+    pname = "consult-gh";
+    ename = "consult-gh";
+    version = "20231206.1823";
+
+    src = fetchFromGitHub {
+      owner = "armindarvish";
+      repo = "consult-gh";
+      rev = "a035eac54a3be270168e86f32075e5f6f426c103";
+      hash = "sha256-qZ7ra8Q8kcBDfR832rquKn8fy0UrNhonHZcX1oCz3dI=";
+    };
+
+    commit = argset.src.rev;
+
+    packageRequires = [
+      consult
+      embark
+      forge
+      markdown-mode
+    ];
+
+    recipe = writeText "recipe" ''
+      (consult-gh
+       :repo "armindarvish/consult-gh"
+       :fetcher github)
+    '';
+
+    meta = {
+      homepage = "https://github.com/armindarvish/consult-gh";
+      description = "A GitHub CLI client inside GNU Emacs using Consult";
+      license = lib.licenses.gpl3Only;
+      maintainers = with lib.maintainers; [ AndersonTorres ];
+    };
+  };
 in
-melpaBuild {
-  pname = "consult-gh";
-  version = "20230706.438";
-
-  inherit commit;
-
-  src = fetchFromGitHub {
-    owner = "armindarvish";
-    repo = "consult-gh";
-    rev = commit;
-    hash = "sha256-bi+qlNvNMXbS4cXbXt01txwD2NAyAqJGNKeOtdtj7tg=";
-  };
-
-  packageRequires = [
-    consult
-    embark
-    forge
-    gh
-    markdown-mode
-  ];
-
-  recipe = writeText "recipe" ''
-    (consult-gh
-      :repo "armindarvish/consult-gh"
-      :fetcher github
-      :files ("consult-gh-embark.el" "consult-gh-forge.el" "consult-gh.el"))
-  '';
-
-  passthru.updateScript = unstableGitUpdater { };
-
-  meta = {
-    homepage = "https://github.com/armindarvish/consult-gh";
-    description = "A GitHub CLI client inside GNU Emacs using Consult";
-    license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [ AndersonTorres ];
-  };
-}
+melpaBuild argset
