b41993177d55732014fa19e128063d433dced358
TheDuckWithAMonocle <theduckwithamonocle@riseup.net>
2024-01-07 20:13:27 +0530

    apple-fonts: init at 1.0.3
    apple-fonts: init at 1.0.3
    
    Update pkgs/data/fonts/apple-fonts/default.nix
    
    Co-authored-by: Weijia Wang <9713184+wegank@users.noreply.github.com>
    
    Update pkgs/data/fonts/apple-fonts/default.nix
    
    Co-authored-by: Weijia Wang <9713184+wegank@users.noreply.github.com>
    
    apple-fonts: init at 1.0.3
    
    apple-fonts: init at 1.0.3
    
    apple-fonts: init at 1.0.3
    
    apple-fonts: init at 1.0.3

---
+++ pkgs/by-name/ap/apple-fonts/package.nix
@@ -0,0 +1,74 @@
+{ lib, stdenvNoCC, fetchurl, unzip, p7zip }:
+
+let
+  pro = fetchurl {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
+    hash = "sha256-nkuHge3/Vy8lwYx9z+pvsQZfzrNIP4K0OutpPl4yXn0=";
+  };
+
+  compact = fetchurl {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
+    hash = "sha256-+Q4HInJBl3FLb29/x9utf7A55uh5r79eh/7hdQDdbSI=";
+  };
+
+  mono = fetchurl {
+    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
+    hash = "sha256-pqkYgJZttKKHqTYobBUjud0fW79dS5tdzYJ23we9TW4=";
+  };
+
+  ny = fetchurl {
+    url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
+    hash = "sha256-XOiWc4c7Yah+mM7axk8g1gY12vXamQF78Keqd3/0/cE=";
+  };
+in
+stdenvNoCC.mkDerivation rec {
+  pname = "apple-fonts";
+  version = "1.0.3";
+
+  nativeBuildInputs = [ p7zip ];
+
+  sourceRoot = ".";
+
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+    7z x ${pro}
+    cd SFProFonts
+    7z x 'SF Pro Fonts.pkg'
+    7z x 'Payload~'
+    mkdir -p $out/fontfiles
+    mv Library/Fonts/* $out/fontfiles
+    cd ..
+    7z x ${mono}
+    cd SFMonoFonts
+    7z x 'SF Mono Fonts.pkg'
+    7z x 'Payload~'
+    mv Library/Fonts/* $out/fontfiles
+    cd ..
+    7z x ${compact}
+    cd SFCompactFonts
+    7z x 'SF Compact Fonts.pkg'
+    7z x 'Payload~'
+    mv Library/Fonts/* $out/fontfiles
+    cd ..
+    7z x ${ny}
+    cd NYFonts
+    7z x 'NY Fonts.pkg'
+    7z x 'Payload~'
+    mv Library/Fonts/* $out/fontfiles
+    mkdir -p $out/usr/share/fonts/OTF $out/usr/share/fonts/TTF
+    mv $out/fontfiles/*.otf $out/usr/share/fonts/OTF
+    mv $out/fontfiles/*.ttf $out/usr/share/fonts/TTF
+    rm -rf $out/fontfiles
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Apple San Francisco, New York fonts";
+    homepage = "https://developer.apple.com/fonts/";
+    license = lib.licenses.unfree;
+    platforms = platforms.all;
+
+  };
+}
