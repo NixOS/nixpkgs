NixOS/nixpkgs

commit 9b2d4d2faa875d91f76d77a9193ff7a840f263e7

snakemake-interface-common: init at 1.15.0

pkgs/development/python-modules/snakemake-interface-common/default.nix
@@ -1,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, poetry-core
+, argparse-dataclass
+, ConfigArgParse
+}:
+
+buildPythonPackage rec {
+  pname = "snakemake-interface-common";
+  version = "1.15.0";
+  format = "pyproject";
+
+  src = fetchFromGitHub {
+    owner = "snakemake";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "sha256-Rf2eMkRvkTCR2swB53ekjv8U8DzTPgjhIkBVrn6gTTI=";
+  };
+
+  nativeBuildInputs = [
+    poetry-core
+  ];
+
+  propagatedBuildInputs = [
+    argparse-dataclass
+    ConfigArgParse
+  ];
+
+  pythonImportsCheck = [ "snakemake_interface_common" ];
+
+  meta = with lib; {
+    description = "Common functions and classes for Snakemake and its plugins";
+    homepage = "https://github.com/snakemake/snakemake-interface-common";
+    license = licenses.mit;
+    maintainers = with maintainers; [ veprbl ];
+  };
+}

pkgs/top-level/python-packages.nix
@@ -13276,6 +13276,8 @@ self: super: with self; {
     inherit (self) python;
   });
 
+  snakemake-interface-common = callPackage ../development/python-modules/snakemake-interface-common { };
+
   snakebite = callPackage ../development/python-modules/snakebite { };
 
   snakeviz = callPackage ../development/python-modules/snakeviz { };

commit 3e929146cf01b532ec68a2f73af22271effdc75d

pythonPackages.snakemake-interface-common: Fix eval with allowAliases false
Was broken since initialised in 9b2d4d2faa875d91f76d77a9193ff7a840f263e7
but not noticed due to https://github.com/NixOS/nixpkgs/pull/271123#discussion_r1442134594

pkgs/development/python-modules/snakemake-interface-common/default.nix
@@ -3,7 +3,7 @@
 , fetchFromGitHub
 , poetry-core
 , argparse-dataclass
-, ConfigArgParse
+, configargparse
 }:
 
 buildPythonPackage rec {
@@ -24,7 +24,7 @@ buildPythonPackage rec {
 
   propagatedBuildInputs = [
     argparse-dataclass
-    ConfigArgParse
+    configargparse
   ];
 
   pythonImportsCheck = [ "snakemake_interface_common" ];