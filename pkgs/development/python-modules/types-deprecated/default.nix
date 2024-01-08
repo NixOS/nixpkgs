40bd27698be94626bb74fcf03a371b6e43b1638f
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-08 09:04:42 +0100

    python311Packages.types-deprecated: refactor

--- pkgs/development/python-modules/types-deprecated/default.nix
+++ pkgs/development/python-modules/types-deprecated/default.nix
@@ -1,18 +1,23 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "types-deprecated";
   version = "1.2.9.20240106";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "types-Deprecated";
     inherit version;
     hash = "sha256-r+uBnpoD0KV5XxjIj+YgfEjtE8Y56TKBvZ2be7bTQxA=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   # Modules has no tests
   doCheck = false;
