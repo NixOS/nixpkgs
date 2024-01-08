365bff86c68fbb017b75ad87a50a5520f92759bb
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-08 12:27:44 +0100

    python311Packages.db-dtypes: 1.1.1 -> 1.2.0
    Diff: https://github.com/googleapis/python-db-dtypes-pandas/compare/refs/tags/v1.1.1...v1.2.0
    
    Changelog: https://github.com/googleapis/python-db-dtypes-pandas/blob/v1.2.0/CHANGELOG.md

--- pkgs/development/python-modules/db-dtypes/default.nix
+++ pkgs/development/python-modules/db-dtypes/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, fetchpatch
 , numpy
 , packaging
 , pandas
@@ -12,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "db-dtypes";
-  version = "1.1.1";
+  version = "1.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -21,23 +20,8 @@ buildPythonPackage rec {
     owner = "googleapis";
     repo = "python-db-dtypes-pandas";
     rev = "refs/tags/v${version}";
-    hash = "sha256-OAVHx/a4uupVGXSWN2/3uem9/4i+TUkzTX4kp0uLY44=";
+    hash = "sha256-FVRqh30mYVfC8zuhPteuvqGYGTp3PW+pi1bquUjYFAg=";
   };
-
-  patches = [
-    # on master branch, to be released as 1.1.2
-    (fetchpatch {
-      name = "xfail-tests-that-are-known-to-fail.patch";
-      url = "https://github.com/googleapis/python-db-dtypes-pandas/commit/4a56b766b0ccba900a555167863f1081a76c4c0d.patch";
-      hash = "sha256-ra1d8Vewvwhkr7PBHc3KM6IUCWsHxE+B7UP2duTgjew=";
-    })
-    # on master branch, to be released as 1.1.2
-    (fetchpatch {
-      name = "add-import-and-object-reference-due-to-upstream-changes.patch";
-      url = "https://github.com/googleapis/python-db-dtypes-pandas/commit/8a7b25f3e708df5cd32afcb702fe16130846b165.patch";
-      hash = "sha256-JVbhiOIr5gKMSQpIQ+DgIRqq8V5x5ClQhkQzAmIYqEU=";
-    })
-  ];
 
   propagatedBuildInputs = [
     numpy
