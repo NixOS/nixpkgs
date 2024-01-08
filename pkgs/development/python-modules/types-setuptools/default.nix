8a9c45f9feb533bbeab5a1f78e42219040d7595c
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-08 09:15:58 +0100

    python311Packages.types-setuptools: 68.2.0.2 -> 69.0.0.20240106

--- pkgs/development/python-modules/types-setuptools/default.nix
+++ pkgs/development/python-modules/types-setuptools/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "types-setuptools";
-  version = "68.2.0.2";
+  version = "69.0.0.20240106";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Ce/DgK1cf3jjC8oVRvcGRpVozyYITPq3Ps+D3qHShEY=";
+    hash = "sha256-4Hf5CJV43zyZOPbkqhYz8YK6Z0Cm/bEzPxYrrl38utw=";
   };
 
   # Module doesn't have tests
