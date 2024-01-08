fcfec2be67eac9bdc7d5153c91d72a4a1eccff7b
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-08 04:56:01 +0000

    btop: 1.2.13 -> 1.3.0

--- pkgs/tools/system/btop/default.nix
+++ pkgs/tools/system/btop/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "btop";
-  version = "1.2.13";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "aristocratos";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-F/muCjhcnM+VqAn6FlD4lv23OLITrmtnHkFc5zv97yk=";
+    hash = "sha256-QQM2/LO/EHovhj+S+4x3ro/aOVrtuxteVVvYAd6feTk=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin [
