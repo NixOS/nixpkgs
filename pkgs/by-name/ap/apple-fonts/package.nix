d8566acf2a37ad2b92a346f65433edd571e120b2
TheDuckWithAMonocle <theduckwithamonocle@riseup.net>
2024-01-08 20:03:33 +0530

    apple-fonts: init at 1.0.3

--- pkgs/by-name/ap/apple-fonts/package.nix
+++ pkgs/by-name/ap/apple-fonts/package.nix
@@ -2,22 +2,22 @@
 
 let
   pro = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
+    url = "";
     hash = "sha256-nkuHge3/Vy8lwYx9z+pvsQZfzrNIP4K0OutpPl4yXn0=";
   };
 
   compact = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
+    url = "https://archive.org/download/sf-pro_202401/SF-Compact.dmg";
     hash = "sha256-+Q4HInJBl3FLb29/x9utf7A55uh5r79eh/7hdQDdbSI=";
   };
 
   mono = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
+    url = "https://archive.org/download/sf-pro_202401/SF-Mono.dmg";
     hash = "sha256-pqkYgJZttKKHqTYobBUjud0fW79dS5tdzYJ23we9TW4=";
   };
 
   ny = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
+    url = "https://archive.org/download/sf-pro_202401/NY.dmg";
     hash = "sha256-XOiWc4c7Yah+mM7axk8g1gY12vXamQF78Keqd3/0/cE=";
   };
 in
