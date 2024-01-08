98d6d707e899bf8f176d2c54f1ca532fbbd7fec9
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-01-08 11:54:54 +0000

    cpuset: 1.6 -> 1.6.2

--- pkgs/by-name/cp/cpuset/package.nix
+++ pkgs/by-name/cp/cpuset/package.nix
@@ -1,43 +1,32 @@
 { lib
 , fetchFromGitHub
-, fetchpatch
 , python3
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "cpuset";
-  version = "1.6";
-
-  propagatedBuildInputs = with python3.pkgs; [
-    configparser
-    future
-  ];
-
-  # https://github.com/lpechacek/cpuset/pull/36
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/MawKKe/cpuset/commit/a4b6b275d0a43d2794ab9e82922d3431aeea9903.patch";
-      sha256 = "1mi1xrql81iczl67s4dk2rm9r1mk36qhsa19wn7zgryf95krsix2";
-    })
-  ];
-
-  makeFlags = [ "prefix=$(out)" ];
+  version = "1.6.2";
 
   src = fetchFromGitHub {
     owner = "lpechacek";
     repo = "cpuset";
     rev = "v${version}";
-    sha256 = "0ig0ml2zd5542d0989872vmy7cs3qg7nxwa93k42bdkm50amhar4";
+    hash = "sha256-fW0SXNI10pb6FTn/2TOqxP9qlys0KL/H9m//NjslUaY=";
   };
 
+  makeFlags = [ "prefix=$(out)" ];
+
   checkPhase = ''
-    cd t
-    make
+    runHook preCheck
+
+    make -C t
+
+    runHook postCheck
   '';
 
   meta = with lib; {
-    description = "Python application that forms a wrapper around the standard Linux filesystem calls to make using the cpusets facilities in the Linux kernel easier";
-    homepage = "https://github.com/lpechacek/cpuset";
+    description = "Python application that forms a wrapper around the standard Linux filesystem calls, to make using the cpusets facilities in the Linux kernel easier";
+    homepage = "https://github.com/SUSE/cpuset";
     license = licenses.gpl2;
     mainProgram = "cset";
     maintainers = with maintainers; [ thiagokokada wykurz ];
