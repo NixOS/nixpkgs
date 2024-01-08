1e99cf2c07606c7fc874227450f2248b99846177
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-08 22:44:12 +0100

    python311Packages.pymeteoclimatic: refactor

--- pkgs/development/python-modules/pymeteoclimatic/default.nix
+++ pkgs/development/python-modules/pymeteoclimatic/default.nix
@@ -5,20 +5,26 @@
 , lxml
 , pythonOlder
 , pytestCheckHook
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "pymeteoclimatic";
   version = "0.1.0";
-  format = "setuptools";
-  disabled = pythonOlder "3.6";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "adrianmo";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-rP0+OYDnQ4GuoV7DzR6jtgH6ilTMLjdaEFJcz3L0GYQ=";
+    repo = "pymeteoclimatic";
+    rev = "refs/tags/${version}";
+    hash = "sha256-rP0+OYDnQ4GuoV7DzR6jtgH6ilTMLjdaEFJcz3L0GYQ=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     beautifulsoup4
@@ -29,11 +35,14 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "meteoclimatic" ];
+  pythonImportsCheck = [
+    "meteoclimatic"
+  ];
 
   meta = with lib; {
     description = "Python wrapper around the Meteoclimatic service";
     homepage = "https://github.com/adrianmo/pymeteoclimatic";
+    changelog = "https://github.com/adrianmo/pymeteoclimatic/releases/tag/${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
