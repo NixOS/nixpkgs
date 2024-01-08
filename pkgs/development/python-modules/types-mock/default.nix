9dddc733ffa06b96953652de20c7d0ed2aa06a2f
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-08 18:33:01 +0100

    python311Packages.types-mock: 5.1.0.3 -> 5.1.0.20240106

--- pkgs/development/python-modules/types-mock/default.nix
+++ pkgs/development/python-modules/types-mock/default.nix
@@ -5,12 +5,12 @@
 
 buildPythonPackage rec {
   pname = "types-mock";
-  version = "5.1.0.3";
+  version = "5.1.0.20240106";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-7urCVIAoe7ch+xf82i6XaNcKR23MFKLB0DiVNcVPgYQ=";
+    hash = "sha256-E8o3nVcQzLPxj2mt5bCIgYdMuDOD2PtJsdTaydXF0JA=";
   };
 
   # Module has no tests
