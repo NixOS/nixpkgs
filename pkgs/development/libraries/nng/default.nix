e70ba4b5fa1a9f5a5ab574d6c1e97f969730d69a
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-08 11:50:48 +0000

    nng: 1.7.0 -> 1.7.1

--- pkgs/development/libraries/nng/default.nix
+++ pkgs/development/libraries/nng/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nng";
-  version = "1.7.0";
+  version = "1.7.1";
 
   src = fetchFromGitHub {
     owner = "nanomsg";
     repo = "nng";
     rev = "v${version}";
-    hash = "sha256-QnT27Xej8bu2wj2v1uwAAJt4DrQlgMsGOvj8ZLpx57A=";
+    hash = "sha256-6JFmoCELDkvDvTNy2ET4igFCc/J9wraN6Cl1lq9So1Q=";
   };
 
   nativeBuildInputs = [ cmake ninja ]
