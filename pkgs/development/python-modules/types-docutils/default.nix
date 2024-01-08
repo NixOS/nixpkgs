15d18d8b40f1cca44937b799ba81b0a3719dc6dd
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-08 09:07:18 +0100

    python311Packages.types-docutils: refactor

--- pkgs/development/python-modules/types-docutils/default.nix
+++ pkgs/development/python-modules/types-docutils/default.nix
@@ -1,17 +1,22 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "types-docutils";
   version = "0.20.0.20240106";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-A5kuyXb74IDbWI4eVqg8Xkq6XHMwIrJbsmy4Q5e5YEk=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   # Module doesn't have tests
   doCheck = false;
