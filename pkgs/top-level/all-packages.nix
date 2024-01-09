b7c782f8a41410867e5d34aafb3d8a6d6d6b81d4
Ivan Trubach <mr.trubach@icloud.com>
2024-01-09 13:42:44 +0300

    sonarr: build from source
    This change refactors sonarr package to build from source instead of
    downloading pre-built releases.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13037,8 +13037,6 @@ with pkgs;
 
   solvespace = callPackage ../applications/graphics/solvespace { };
 
-  sonarr = callPackage ../servers/sonarr { };
-
   sonata = callPackage ../applications/audio/sonata { };
 
   sony-headphones-client = callPackage ../applications/audio/sony-headphones-client { };
