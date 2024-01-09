7322c44adad445b786d45fd9f1e148ca9f6f987e
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-09 00:01:36 +0000

    python310Packages.google-cloud-datacatalog: 3.17.0 -> 3.17.1

--- pkgs/development/python-modules/google-cloud-datacatalog/default.nix
+++ pkgs/development/python-modules/google-cloud-datacatalog/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-datacatalog";
-  version = "3.17.0";
+  version = "3.17.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-xaKBfkgmhB7MH1qFWu9hjHIIVG1BjBgzjfnyD14V9Z0=";
+    hash = "sha256-lZdiwowrtBbP7LKdplRDbAEybGi/Pimq2XkCtzGFjZY=";
   };
 
   nativeBuildInputs = [
