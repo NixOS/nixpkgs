5223d1f86aab688bf05c6069b87be5f662d84eac
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-01-09 08:40:19 +0100

    msat: init at 0.9.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39550,6 +39550,8 @@ with pkgs;
 
   monosat = callPackage ../applications/science/logic/monosat { };
 
+  msat = callPackage ../applications/science/logic/msat { };
+
   nusmv = callPackage ../applications/science/logic/nusmv { };
 
   nuXmv = callPackage ../applications/science/logic/nuXmv { };
