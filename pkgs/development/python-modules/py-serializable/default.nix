759d213f898d943d0cdd401c506e325161eee056
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-09 08:52:37 +0000

    python310Packages.py-serializable: 0.16.0 -> 0.17.1

--- pkgs/development/python-modules/py-serializable/default.nix
+++ pkgs/development/python-modules/py-serializable/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "py-serializable";
-  version = "0.16.0";
+  version = "0.17.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "madpah";
     repo = "serializable";
     rev = "refs/tags/v${version}";
-    hash = "sha256-javjmdFQBxg/yqa6lsxKK18DgvVu/YmqvaWo2Upgzqs=";
+    hash = "sha256-G7bIsvWdL4qVg4akJ2KtXVS10DiJSFUYEzyQSp9ry9o=";
   };
 
   nativeBuildInputs = [
