1bc446be2c394c91ab577aecfb6b93ff868be52b
Valter Nazianzeno <manipuladordedados@gmail.com>
2024-01-09 19:23:19 +0000

    emilua: 0.5.1 -> 0.6.0

--- pkgs/development/interpreters/emilua/default.nix
+++ pkgs/development/interpreters/emilua/default.nix
@@ -21,7 +21,7 @@
 , cmake
 , asciidoctor
 }:
- 
+
 let
   trial-protocol-wrap = fetchFromGitHub {
       owner = "breese";
@@ -34,14 +34,14 @@ in
 stdenv.mkDerivation rec {
   pname = "emilua";
   version = "0.6.0";
- 
+
   src = fetchFromGitLab {
       owner = "emilua";
       repo = "emilua";
       rev = "v${version}";
       hash = "sha256-cW2b+jUQT60hCCirBzxZltzA7KvBihnzWNPkKDID6kU=";
   };
- 
+
   buildInputs = [
     luajit_openresty
     boost182
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
     openssl
     cereal
   ];
- 
+
   nativeBuildInputs = [
     re2c
     gperf
@@ -65,34 +65,34 @@ stdenv.mkDerivation rec {
     cmake
     ninja
   ];
- 
+
   dontUseCmakeConfigure = true;
- 
+
   # Meson is no longer able to pick up Boost automatically.
   # https://github.com/NixOS/nixpkgs/issues/86131
   env = {
     BOOST_INCLUDEDIR = "${lib.getDev boost182}/include";
     BOOST_LIBRARYDIR = "${lib.getLib boost182}/lib";
   };
- 
+
   mesonFlags = [
-	(lib.mesonBool "enable_file_io" true)
-	(lib.mesonBool "enable_io_uring" true)
-	(lib.mesonBool "enable_tests" true)
-	(lib.mesonBool "enable_manpages" true)
+	  (lib.mesonBool "enable_file_io" true)
+	  (lib.mesonBool "enable_io_uring" true)
+	  (lib.mesonBool "enable_tests" true)
+    (lib.mesonBool "enable_manpages" true)
     (lib.mesonOption "version_suffix" "-nixpkgs1")
   ];
- 
+
   postPatch = ''
     pushd subprojects
     cp -r ${trial-protocol-wrap} trial-protocol
     chmod +w trial-protocol
     cp "packagefiles/trial.protocol/meson.build" "trial-protocol/"
     popd
- 
+
     substituteInPlace src/emilua_gperf.awk  --replace '#!/usr/bin/env -S gawk --file' '#!${gawk}/bin/gawk -f'
   '';
- 
+
   meta = with lib; {
     description = "Lua execution engine";
     homepage = "https://emilua.org/";
