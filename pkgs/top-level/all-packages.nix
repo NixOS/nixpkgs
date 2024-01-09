b597c38884d483995710c61164ae42a42bda55c1
Yureka <yuka@yuka.dev>
2024-01-10 00:32:18 +0100

    openimageio: build with openexr 3
    ilmbase is replaced by Imath (propagated from openexr_3)
    
    See the following upstream change from 2020 on OpenEXR 3.x support: https://github.com/AcademySoftwareFoundation/OpenImageIO/commit/6313484e35765df1adf8f40c217cebc4c9d9d954#diff-40f1f520160b8fecd67891ee24b54add91856f733ece4bae0c3896adb00452af

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34385,7 +34385,9 @@ with pkgs;
 
   openfx = callPackage ../development/libraries/openfx { };
 
-  openimageio = darwin.apple_sdk_11_0.callPackage ../development/libraries/openimageio { };
+  openimageio = darwin.apple_sdk_11_0.callPackage ../development/libraries/openimageio {
+    openexr = openexr_3;
+  };
 
   openjump = callPackage ../applications/misc/openjump { };
 
