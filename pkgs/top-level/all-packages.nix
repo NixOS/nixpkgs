27147978611cc70144076b2bb1724b9480c13d43
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-01-10 06:35:57 +0100

    jackline: use OCaml 4.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32461,7 +32461,9 @@ with pkgs;
     electron = electron_19;
   };
 
-  jackline = callPackage ../applications/networking/instant-messengers/jackline { };
+  jackline = callPackage ../applications/networking/instant-messengers/jackline {
+    ocamlPackages = ocaml-ng.ocamlPackages_4_14;
+  };
 
   jay = callPackage ../applications/window-managers/jay { };
 
