52a88fbeb65d03cd6e4c82540730c5989c21b1be
eryngion <eryngion@protonmail.com>
2024-01-10 03:38:14 +0300

    Revert "nixos/no-x-libs: build qtbase without qt translation"
    Not sure what was the problem here, but as of next commit
    qtbase successfully builds with `environment.noXlibs = true`
    and qttranslations.
    
    This reverts commit 2e14d8ba72b07047d1dda0ed8161956fba34ee0a.

--- pkgs/development/libraries/qt-5/modules/qtbase.nix
+++ pkgs/development/libraries/qt-5/modules/qtbase.nix
@@ -15,7 +15,7 @@
   # optional dependencies
 , cups ? null, postgresql ? null
 , withGtk3 ? false, dconf, gtk3
-, withQttranslation ? true, qttranslations ? null
+, qttranslations ? null
 
   # options
 , libGLSupported ? !stdenv.isDarwin
@@ -351,8 +351,7 @@ stdenv.mkDerivation (finalAttrs: ({
     ] ++ lib.optionals (mysqlSupport) [
       "-L" "${libmysqlclient}/lib"
       "-I" "${libmysqlclient}/include"
-    ] ++ lib.optional (withQttranslation && (qttranslations != null)) [
-      # depends on x11
+    ] ++ lib.optional (qttranslations != null) [
       "-translationdir" "${qttranslations}/translations"
     ]
   );
