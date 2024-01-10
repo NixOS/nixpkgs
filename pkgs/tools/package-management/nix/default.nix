c27bc49c43ba354eea4187f4c7be086d6a55eb1b
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-01-10 14:04:30 -0500

    nixVersions.stable: 2.18.1 -> 2.19.2

--- pkgs/tools/package-management/nix/default.nix
+++ pkgs/tools/package-management/nix/default.nix
@@ -259,7 +259,7 @@ in lib.makeExtensible (self: ({
     else
       nix;
 
-  stable = addFallbackPathsCheck self.nix_2_18;
+  stable = addFallbackPathsCheck self.nix_2_19;
 
   unstable = self.nix_2_19;
 } // lib.optionalAttrs config.allowAliases {
