ec33394a85b51a21c0762fa4e6723399751c3fe9
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-11 16:26:39 +0100

    trivy: 0.48.2 -> 0.48.3
    Diff: https://github.com/aquasecurity/trivy/compare/refs/tags/v0.48.2...v0.48.3
    
    Changelog: https://github.com/aquasecurity/trivy/releases/tag/v0.48.3

--- pkgs/tools/admin/trivy/default.nix
+++ pkgs/tools/admin/trivy/default.nix
@@ -10,19 +10,19 @@
 
 buildGoModule rec {
   pname = "trivy";
-  version = "0.48.2";
+  version = "0.48.3";
 
   src = fetchFromGitHub {
     owner = "aquasecurity";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-b7lwy1erESjSg+pPZelYNBfIcbdcfDdM3klefQ6+L00=";
+    hash = "sha256-zWv/4dDzWfR9qbbBaMaHFMId1OVhcOja7lTy3gcm77w=";
   };
 
   # Hash mismatch on across Linux and Darwin
   proxyVendor = true;
 
-  vendorHash = "sha256-1jznsC6VFUph7AKk86iGAV7GKFoAcA87Ltt4n0EaX4c=";
+  vendorHash = "sha256-EOu4VHfrQbIP1vSWF3UkZDMyEIcbjQKjzdch9c6cVg4=";
 
   subPackages = [ "cmd/trivy" ];
 
