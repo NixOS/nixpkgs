65949313b456c1630c14a118ebb8287aa0d63035
Mario Rodas <marsam@users.noreply.github.com>
2024-01-12 04:20:00 +0000

    vale: 3.0.0 -> 3.0.3
    Diff: https://github.com/errata-ai/vale/compare/v3.0.0...v3.0.3
    
    Changelog: https://github.com/errata-ai/vale/releases/tag/v3.0.3

--- pkgs/tools/text/vale/default.nix
+++ pkgs/tools/text/vale/default.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "vale";
-  version = "3.0.0";
+  version = "3.0.3";
 
   subPackages = [ "cmd/vale" ];
   outputs = [ "out" "data" ];
@@ -11,7 +11,7 @@ buildGoModule rec {
     owner = "errata-ai";
     repo = "vale";
     rev = "v${version}";
-    hash = "sha256-qov9DWpHzxgDhgF9LwPGxjuOdmnYt4YcFeCO6Xb+QAw=";
+    hash = "sha256-KBqs8hSotVt7+DOpBoDyBTTVhtkk1v5DyhflaPmcWS8=";
   };
 
   vendorHash = "sha256-AsBbJJQs+pU2UNfEFvNnPwaaabTrXvFBQLcriIA2ST4=";
