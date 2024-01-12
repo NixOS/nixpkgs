e2514012631946c9e6723dae89dfcb3699af928f
ppenguin <hieronymusv@gmail.com>
2024-01-12 11:51:14 +0100

    freecad: add freecad-wayland that runs on wayland natively
    freecad: add freecad-wayland that runs on wayland natively (no xwayland)
    
    reformat
    
    all-packages
    
    fix freecad

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36176,6 +36176,8 @@ with pkgs;
 
   forge-mtg = callPackage ../games/forge-mtg { };
 
+  freecad-wayland = freecad.override { withWayland = true; };
+
   freeciv = callPackage ../games/freeciv {
     sdl2Client = false;
     gtkClient = true;
