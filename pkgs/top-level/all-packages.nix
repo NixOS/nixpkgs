c88c272938c473601078ec2ef8f9230da6c111e6
Sameer Rahmani <lxsameer@gnu.org>
2024-01-12 20:11:09 +0000

    llvm: Add the mlir package to llvm 17
    (cherry picked from commit 262ed9bd904310a33653904a56546a15f88e971a)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16643,6 +16643,8 @@ with pkgs;
   llvm_16 = llvmPackages_16.llvm;
   llvm_17 = llvmPackages_17.llvm;
 
+  mlir_17 = llvmPackages_17.mlir;
+
   libllvm = llvmPackages.libllvm;
   llvm-manpages = llvmPackages.llvm-manpages;
 
