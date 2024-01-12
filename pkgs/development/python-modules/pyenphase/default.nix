ad8c1af79d8e1834b37b8be773d220ddc43c1e16
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-12 21:48:50 +0100

    python311Packages.pyenphase: 1.16.0 -> 1.17.0
    Diff: https://github.com/pyenphase/pyenphase/compare/refs/tags/v1.16.0...v1.17.0
    
    Changelog: https://github.com/pyenphase/pyenphase/blob/v1.17.0/CHANGELOG.md

--- pkgs/development/python-modules/pyenphase/default.nix
+++ pkgs/development/python-modules/pyenphase/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pyenphase";
-  version = "1.16.0";
+  version = "1.17.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "pyenphase";
     repo = "pyenphase";
     rev = "refs/tags/v${version}";
-    hash = "sha256-4oxYa05TSmhqv/f9gzq+zRsCNU0KSyHt0nZSEmfpx7k=";
+    hash = "sha256-GNyhUk2/CSSdigrAJ0T2F1+49PkyPxMaec3nO9/cmNo=";
   };
 
   postPatch = ''
