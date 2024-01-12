193b18f446d1b7d463b3dad9c4f4045fbfb68968
ppenguin <hieronymusv@gmail.com>
2024-01-12 11:51:14 +0100

    freecad: add freecad-wayland that runs on wayland natively

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31967,6 +31967,8 @@ with pkgs;
       shiboken2;
   };
 
+  freecad-wayland = freecad.override { withWayland = true; };
+
   freedv = callPackage ../applications/radio/freedv {
     inherit (darwin.apple_sdk.frameworks) AppKit AVFoundation Cocoa CoreMedia;
     codec2 = codec2.override {
