47cd2c62f4707d1bbb37c2bf045ffdb4eecf3c83
Peter Retzlaff <pe.retzlaff@gmail.com>
2024-01-13 10:58:56 +0100

    add mainProgram to meta

--- pkgs/by-name/pa/parallel-disk-usage/package.nix
+++ pkgs/by-name/pa/parallel-disk-usage/package.nix
@@ -21,5 +21,6 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/KSXGitHub/parallel-disk-usage";
     license = licenses.asl20;
     maintainers = [maintainers.peret];
+    mainProgram = "pdu";
   };
 }
