b4a3c010f1206fd0c0b2dfc4ddb6ddb4d8a61cca
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-13 21:19:07 +0100

    python311Packages.argparse-dataclass: 1.0.0 -> 2.0.0
    Diff: https://github.com/mivade/argparse_dataclass/compare/refs/tags/1.0.0...2.0.0
    
    Changelog: https://github.com/mivade/argparse_dataclass/blob/2.0.0/CHANGELOG.md

--- pkgs/development/python-modules/argparse-dataclass/default.nix
+++ pkgs/development/python-modules/argparse-dataclass/default.nix
@@ -1,24 +1,33 @@
-{ lib, buildPythonPackage, fetchFromGitHub, pytestCheckHook }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pytestCheckHook
+}:
 
 buildPythonPackage rec {
   pname = "argparse-dataclass";
-  version = "1.0.0";
+  version = "2.0.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "mivade";
     repo = "argparse_dataclass";
-    rev = version;
-    sha256 = "6//XQKUnCH3ZtOL6M/EstMJ537nEmbuGQNqfelTluOs=";
+    rev = "refs/tags/${version}";
+    sha256 = "sha256-ASdP6LOEeTszyppYV6vRQX8BKOHYUimI36tMSZTQfTk=";
   };
 
-  checkInputs = [ pytestCheckHook ];
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 
-  pythonImportsCheck = [ "argparse_dataclass" ];
+  pythonImportsCheck = [
+    "argparse_dataclass"
+  ];
 
   meta = with lib; {
     description = "Declarative CLIs with argparse and dataclasses";
     homepage = "https://github.com/mivade/argparse_dataclass";
+    changelog = "https://github.com/mivade/argparse_dataclass/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ tm-drtina ];
   };
