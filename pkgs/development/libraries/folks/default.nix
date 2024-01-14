f5dfc9716403cb950f3bdb50de77cf4249b045b4
Maxine Aubrey <max@ine.dev>
2024-01-14 12:10:15 +0100

    folks: disable checks
    checks were re-enabled in #279843 / 10dad205777ae16ca27c336142e649295ec14978
    but inconsistent timeouts still occur

--- pkgs/development/libraries/folks/default.nix
+++ pkgs/development/libraries/folks/default.nix
@@ -86,7 +86,9 @@ stdenv.mkDerivation rec {
   # ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]
   env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-error=implicit-function-declaration";
 
-  doCheck = stdenv.isLinux;
+  # Checks last re-enabled in https://github.com/NixOS/nixpkgs/pull/279843, but timeouts in tests still
+  # occur inconsistently
+  doCheck = false;
 
   # Prevents e-d-s add-contacts-stress-test from timing out
   checkPhase = ''
