07535c9fda9a0666dff8dc927db16c6256a74ad5
aikooo7 <prozinhopro1973@gmail.com>
2024-01-14 00:43:40 +0000

    betterdiscord-installer: 1.0.0-beta → 1.3.0

--- pkgs/tools/misc/betterdiscord-installer/default.nix
+++ pkgs/tools/misc/betterdiscord-installer/default.nix
@@ -1,23 +1,24 @@
 { appimageTools, lib, fetchurl }:
 let
   pname = "betterdiscord-installer";
-  version = "1.0.0-beta";
+  version = "1.3.0";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/BetterDiscord/Installer/releases/download/v${version}/Betterdiscord-Linux.AppImage";
-    sha256 = "103acb11qmvjmf6g9lgsfm5jyahfwfdqw0x9w6lmv1hzwbs26dsr";
+    sha256 = "sha256-In5J6TWoJsFODDwMXd1lMg3341IZJD2OJebVtgISxP0=";
   };
 
   appimageContents = appimageTools.extract { inherit name src; };
-in appimageTools.wrapType2 {
+in
+appimageTools.wrapType2 {
   inherit name src;
 
   extraInstallCommands = ''
     mv $out/bin/${name} $out/bin/${pname}
 
-    install -m 444 -D ${appimageContents}/betterdiscord.desktop -t $out/share/applications
-    substituteInPlace $out/share/applications/betterdiscord.desktop \
+    install -m 444 -D ${appimageContents}/betterdiscord-installer.desktop -t $out/share/applications
+    substituteInPlace $out/share/applications/betterdiscord-installer.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
     cp -r ${appimageContents}/usr/share/icons $out/share
   '';
@@ -26,7 +27,7 @@ in appimageTools.wrapType2 {
     description = "Installer for BetterDiscord";
     homepage = "https://betterdiscord.app";
     license = licenses.mit;
-    maintainers = [ maintainers.ivar ];
+    maintainers = with maintainers; [ ivar aikooo7 ];
     platforms = [ "x86_64-linux" ];
     mainProgram = "betterdiscord-installer";
   };
