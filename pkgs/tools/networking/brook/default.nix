9ecb8b53f9bccd67c75d4bd36df64fc07c1ad66e
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-14 22:09:04 +0000

    brook: 20230606 -> 20240214

--- pkgs/tools/networking/brook/default.nix
+++ pkgs/tools/networking/brook/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "brook";
-  version = "20230606";
+  version = "20240214";
 
   src = fetchFromGitHub {
     owner = "txthinking";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-F4muuU696YbKcPkpD1LAeyD8ghQAe65UdqV5wS1fATI=";
+    sha256 = "sha256-5+AqlmDa11PrB24XkelOFHK4sBi4j78WMLQrzDuP1/M=";
   };
 
-  vendorHash = "sha256-uKlO1x5sGM8B1htmvRt9kND7tuH36iLN/Mev77vwZ6M=";
+  vendorHash = "sha256-cTw9k4AqS4NOJ0vX0InR0xxOfCXIgA3FxgL6oXryOnA=";
 
   meta = with lib; {
     homepage = "https://github.com/txthinking/brook";
