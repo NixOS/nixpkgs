5184b0d55e200b1ec38cc55959736ea1f2849fb0
Robert Helgesson <robert@rycee.net>
2024-01-15 12:34:43 +0100

    nmd: turn into a fixed-output derivation
    This should avoid the need for IFD. Also switch source fetch to
    Sourcehut since it is back online.
    
    (cherry picked from commit 06c2143eded61ab33a8102b2efff4790458d451d)

--- pkgs/by-name/ni/nix-lib-nmd/package.nix
+++ pkgs/by-name/ni/nix-lib-nmd/package.nix
@@ -1,22 +1,19 @@
-{ lib, stdenv, fetchurl }:
+{ lib, stdenv, fetchFromSourcehut }:
 
 let version = "0.5.0";
 in stdenv.mkDerivation {
   pname = "nix-lib-nmd";
   inherit version;
 
-  # TODO: Restore when Sourcehut once its back from DDoS attack.
-  # src = fetchFromSourcehut {
-  #   owner = "~rycee";
-  #   repo = "nmd";
-  #   rev = "v${version}";
-  #   hash = "sha256-1glxIg/b+8qr+ZsSsBqZIqGpsYWzRuMyz74/sy765Uk=";
-  # };
-
-  src = fetchurl {
-    url = "https://rycee.net/tarballs/nmd-${version}.tar.gz";
-    hash = "sha256-+65+VYFgnbFGzCyyQytyxVStSZwEP989qi/6EDOdA8A=";
+  src = fetchFromSourcehut {
+    owner = "~rycee";
+    repo = "nmd";
+    rev = "v${version}";
+    hash = "sha256-x3zzcdvhJpodsmdjqB4t5mkVW22V3wqHLOun0KRBzUI=";
   };
+
+  outputHashMode = "recursive";
+  outputHash = "sha256-7BQmDJBo7rzv0rgfRiUAR3HvKkUHQ6x0umhBRhAAyzM=";
 
   installPhase = ''
     mkdir -v "$out"
