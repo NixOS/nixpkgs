4aa7a80930f87eeae76a16d0ff29f4f38b01efce
annalee <150648636+a-n-n-a-l-e-e@users.noreply.github.com>
2024-01-15 21:27:01 +0000

    avidemux: make install path match rpath; fix runtime
    (cherry picked from commit 33b76fae22c03ce8e80e238d85a489de9050b153)

--- pkgs/applications/video/avidemux/default.nix
+++ pkgs/applications/video/avidemux/default.nix
@@ -83,6 +83,11 @@ stdenv.mkDerivation rec {
 
     ln -s "$out/bin/avidemux3_${default}" "$out/bin/avidemux"
 
+    # make the install path match the rpath
+    if [[ -d ''${!outputLib}/lib64 ]]; then
+      mv ''${!outputLib}/lib64 ''${!outputLib}/lib
+      ln -s lib ''${!outputLib}/lib64
+    fi
     fixupPhase
   '';
 
@@ -93,13 +98,5 @@ stdenv.mkDerivation rec {
     # "CPU not supported" errors on AArch64
     platforms = [ "i686-linux" "x86_64-linux" ];
     license = licenses.gpl2;
-    # Downstream we experience:
-    #
-    # https://github.com/NixOS/nixpkgs/issues/239424
-    #
-    # Upstream doesn't have a contact page / Bug tracker, so it's not easy to
-    # notify them about it. Using firejail might help, as some commented
-    # downstream.
-    broken = true;
   };
 }
