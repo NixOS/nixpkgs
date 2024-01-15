f462bea34eb7b82f7c4a21e66bb62264fd51304c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-01-15 15:32:05 +0100

    nginxModules.echo: 0.62 -> 0.63
    Diff: https://github.com/openresty/echo-nginx-module/compare/v0.62...v0.63

--- pkgs/servers/http/nginx/modules.nix
+++ pkgs/servers/http/nginx/modules.nix
@@ -233,14 +233,14 @@ let self = {
 
   echo = rec {
     name = "echo";
-    version = "0.62";
+    version = "0.63";
 
     src = fetchFromGitHub {
       name = "echo";
       owner = "openresty";
       repo = "echo-nginx-module";
       rev = "v${version}";
-      sha256 = "0kr1y094yw1a9fyrf4w73ikq18w5ys463wza9n7yfl77xdwirnvl";
+      hash = "sha256-K7oOE0yxPYLf+3YMVbBsncpHRpGHXjs/8B5QPO3MQC4=";
     };
 
     meta = with lib; {
