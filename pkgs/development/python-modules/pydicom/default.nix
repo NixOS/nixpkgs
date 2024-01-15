59738de84bdaa0b3c9975475bd85d30d7f42d542
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-15 13:10:43 +0100

    python311Packages.pydicom: refactor

--- pkgs/development/python-modules/pydicom/default.nix
+++ pkgs/development/python-modules/pydicom/default.nix
@@ -2,10 +2,11 @@
 , stdenv
 , buildPythonPackage
 , fetchFromGitHub
-, pythonOlder
-, pytestCheckHook
+, flit-core
 , numpy
 , pillow
+, pytestCheckHook
+, pythonOlder
 , setuptools
 }:
 
@@ -32,13 +33,17 @@ let
 in
 buildPythonPackage {
   inherit pname version src;
-  disabled = pythonOlder "3.6";
+  pyproject = true;
 
-  format = "setuptools";
+  disabled = pythonOlder "3.10";
 
   patches = [
     # backport of https://github.com/pydicom/pydicom/commit/2513a20cc41743a42bdb86f4cbb4873899b7823c
     ./pillow-10.1.0-compat.patch
+  ];
+
+  nativeBuildInputs = [
+    flit-core
   ];
 
   propagatedBuildInputs = [
@@ -78,6 +83,7 @@ buildPythonPackage {
   meta = with lib; {
     description = "Python package for working with DICOM files";
     homepage = "https://pydicom.github.io";
+    changelog = "https://github.com/pydicom/pydicom/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ bcdarwin ];
   };
