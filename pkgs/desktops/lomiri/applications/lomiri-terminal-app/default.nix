6d8373b052878c617dc85f3c03e4ab459a9b6957
OPNA2608 <opna2608@protonmail.com>
2024-01-15 18:43:23 +0100

    lomiri.lomiri-terminal-app: Fetch upstream-submitted patches

--- pkgs/desktops/lomiri/applications/lomiri-terminal-app/default.nix
+++ pkgs/desktops/lomiri/applications/lomiri-terminal-app/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitLab
+, fetchpatch
 , gitUpdater
 , nixosTests
 , cmake
@@ -27,15 +28,34 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   patches = [
-    # Fix CMake code not using the (intended? correct? working?) mechanism for depending on Qt modules
-    ./0001-Drop-deprecated-qt5_use_modules.patch
+    # Stop usage of private qt5_use_modules function, seemingly unavailable in this package
+    # Remove when https://gitlab.com/ubports/development/apps/lomiri-terminal-app/-/merge_requests/103 merged & in release
+    (fetchpatch {
+      name = "0001-lomiri-terminal-app-Stop-using-qt5_use_modules.patch";
+      url = "https://gitlab.com/ubports/development/apps/lomiri-terminal-app/-/commit/db210c74e771a427066aebdc3a99cab6e782d326.patch";
+      hash = "sha256-op4+/eo8rBRMcW6MZ0rOEFReM7JBCck1B+AsgAPyqAI=";
+    })
+
+    # Explicitly bind textdomain, don't rely on hacky workaround in LUITK
+    # Remove when https://gitlab.com/ubports/development/apps/lomiri-terminal-app/-/merge_requests/104 merged & in release
+    (fetchpatch {
+      name = "0002-lomiri-terminal-app-Call-i18n.bindtextdomain.patch";
+      url = "https://gitlab.com/ubports/development/apps/lomiri-terminal-app/-/commit/7f9d419e29043f0d0922d2ac1dce5673e2723a01.patch";
+      hash = "sha256-HfIvGVbIdTasoHAfHysnzFLufQQ4lskym5HTekH+mjk=";
+    })
+
+    # Add more & correct existing usage of GNUInstallDirs variables
+    # Remove when https://gitlab.com/ubports/development/apps/lomiri-terminal-app/-/merge_requests/105 merged & in release
+    (fetchpatch {
+      name = "0003-lomiri-terminal-app-GNUInstallDirs-usage.patch";
+      url = "https://gitlab.com/ubports/development/apps/lomiri-terminal-app/-/commit/fcde1f05bb442c74b1dff95917fd7594f26e97a7.patch";
+      hash = "sha256-umxCMGNjyz0TVmwH0Gl0MpgjLQtkW9cHkUfpNJcoasE=";
+    })
   ];
 
   postPatch = ''
     substituteInPlace CMakeLists.txt \
-      --replace "\''${CMAKE_INSTALL_LIBDIR}/qt5/qml" '${qtbase.qtQmlPrefix}' \
-      --replace "\''${CMAKE_INSTALL_PREFIX}/\''${DATA_DIR}" "\''${CMAKE_INSTALL_FULL_DATADIR}/lomiri-terminal-app" \
-      --replace 'EXEC "''${APP_NAME}"' 'EXEC "${placeholder "out"}/bin/''${APP_NAME}"'
+      --replace "\''${CMAKE_INSTALL_LIBDIR}/qt5/qml" "\''${CMAKE_INSTALL_PREFIX}/${qtbase.qtQmlPrefix}"
 
     # Tests look abandoned - add_test in CMake code is commented out, refers to old repo structure in import paths
     sed -i -e '/add_subdirectory(tests)/d' CMakeLists.txt
@@ -77,6 +97,7 @@ stdenv.mkDerivation (finalAttrs: {
     description = "A terminal app for desktop and mobile devices";
     homepage = "https://gitlab.com/ubports/development/apps/lomiri-terminal-app";
     license = licenses.gpl3Only;
+    mainProgram = "lomiri-terminal-app";
     maintainers = teams.lomiri.members;
     platforms = platforms.linux;
   };
