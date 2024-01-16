6425843adacfa1f8a61f3e0fc3e903f333cc1f94
Sylvain Fankhauser <sephi@fhtagn.top>
2024-01-16 10:56:11 +0100

    python3Packages.wagtail: relax willow dependency

--- pkgs/development/python-modules/wagtail/default.nix
+++ pkgs/development/python-modules/wagtail/default.nix
@@ -38,7 +38,8 @@ buildPythonPackage rec {
     substituteInPlace setup.py \
       --replace "beautifulsoup4>=4.8,<4.12" "beautifulsoup4>=4.8" \
       --replace "draftjs_exporter>=2.1.5,<3.0" "draftjs_exporter>=2.1.5,<6.0" \
-      --replace "django-taggit>=2.0,<5.0" "django-taggit>=2.0,<6.0"
+      --replace "django-taggit>=2.0,<5.0" "django-taggit>=2.0,<6.0" \
+      --replace "Willow[heif]>=1.6.2,<1.7" "Willow[heif]>=1.6.2,<2"
   '';
 
   propagatedBuildInputs = [
