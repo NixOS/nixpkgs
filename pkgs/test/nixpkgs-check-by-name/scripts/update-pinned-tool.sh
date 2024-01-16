9da57b5bd370a90e979453694e715975a38eb966
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-01-16 23:04:26 +0100

    check-by-name/run-local.sh: Make it usable for non-CI platforms
    Since https://github.com/NixOS/nixpkgs/pull/281374, the
    nixpkgs-check-by-name tooling is pinned to a specific /nix/store path to
    avoid having to evaluate Nixpkgs in CI.
    
    The same path is used for local runs, but that doesn't actually work
    when you're trying to run it on a platform different from CI.
    
    This commit makes it work by being clearer about platforms and making
    local runs check out the correct Nixpkgs to evaluate the tool from.

--- pkgs/test/nixpkgs-check-by-name/scripts/update-pinned-tool.sh
+++ pkgs/test/nixpkgs-check-by-name/scripts/update-pinned-tool.sh
@@ -7,6 +7,9 @@ trace() { echo >&2 "$@"; }
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Determined by `runs-on: ubuntu-latest` in .github/workflows/check-by-name.yml
+CI_SYSTEM=x86_64-linux
+
 channel=nixos-unstable
 pin_file=$SCRIPT_DIR/pinned-tool.json
 
@@ -19,13 +22,19 @@ trace "$nixpkgs"
 rev=$(<"$nixpkgs/.git-revision")
 trace -e "Git revision of channel $channel is \e[34m$rev\e[0m"
 
-
-trace -n "Fetching the prebuilt version of nixpkgs-check-by-name.. "
-path=$(nix-build --no-out-link "$nixpkgs" -A tests.nixpkgs-check-by-name -j 0 | tee /dev/stderr)
+trace -n "Fetching the prebuilt version of nixpkgs-check-by-name for $CI_SYSTEM.. "
+# This is the architecture used by CI, we want to prefetch the exact path to avoid having to evaluate Nixpkgs
+ci_path=$(nix-build --no-out-link "$nixpkgs" \
+    -A tests.nixpkgs-check-by-name \
+    --arg config '{}' \
+    --argstr system "$CI_SYSTEM" \
+    --arg overlays '[]' \
+    -j 0 \
+    | tee /dev/stderr)
 
 trace "Updating $pin_file"
 jq -n \
     --arg rev "$rev" \
-    --arg path "$path" \
+    --arg ci-path "$ci_path" \
     '$ARGS.named' \
     > "$pin_file"
