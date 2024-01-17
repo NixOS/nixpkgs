e6b207f345eb92a913169ed95484116fc9796109
Adam Stephens <adam@valkor.net>
2024-01-17 10:21:47 -0500

    garage_0_9: 0.9.0 -> 0.9.1
    https://git.deuxfleurs.fr/Deuxfleurs/garage/releases/tag/v0.9.1
    
    Due to a recent change in the build system, we have to remove actively a
    file (`.cargo/config.toml`) to avoid the mold linker to leak during the linkage phase.
    
    Not sure if we should have more sophisticated ways to ignore this.
    
    (cherry picked from commit e8f1c9b2fb4ee502871157c4fcae4e2700fdbb46)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8485,7 +8485,7 @@ with pkgs;
   })
     garage
       garage_0_8 garage_0_9
-      garage_0_8_5 garage_0_9_0;
+      garage_0_8_5 garage_0_9_1;
 
   garmin-plugin = callPackage ../applications/misc/garmin-plugin { };
 
