34afff7c8023556bd25d37f5326613a7880bbf01
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-01-17 02:08:31 +0100

    feh: 3.10.1 -> 3.10.2

--- pkgs/applications/graphics/feh/default.nix
+++ pkgs/applications/graphics/feh/default.nix
@@ -1,27 +1,18 @@
-{ lib, stdenv, fetchFromGitHub, makeWrapper, fetchpatch
+{ lib, stdenv, fetchFromGitHub, makeWrapper
 , xorg, imlib2, libjpeg, libpng
 , curl, libexif, jpegexiforient, perl
 , enableAutoreload ? !stdenv.hostPlatform.isDarwin }:
 
 stdenv.mkDerivation rec {
   pname = "feh";
-  version = "3.10.1";
+  version = "3.10.2";
 
   src = fetchFromGitHub {
     owner = "derf";
     repo = pname;
     rev = version;
-    hash = "sha256-1dz04RcaoP79EoE+SsatXm2wMRCbNnmAzMECYk3y3jg=";
+    hash = "sha256-378rhZhpcua3UbsY0OcGKGXdMIQCuG84YjJ9vfJhZVs=";
   };
-
-  patches = [
-    # upstream PR: https://github.com/derf/feh/pull/723
-    (fetchpatch {
-      name = "fix-right-click-buffer-overflow.patch";
-      url = "https://github.com/derf/feh/commit/2c31f8863b80030e772a529ade519fc2fee4a991.patch";
-      sha256 = "sha256-sUWS06qt1d1AyGfqKb+1BzZslYxOzur4q0ePEHcTz1g=";
-    })
-  ];
 
   outputs = [ "out" "man" "doc" ];
 
