37dfcd71f4efa1a1e1884d1c9b5975853bfb4e47
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-17 01:18:39 +0100

    python311Packages.ailment: 9.2.84 -> 9.2.85
    Diff: https://github.com/angr/ailment/compare/refs/tags/v9.2.84...v9.2.85

--- pkgs/development/python-modules/ailment/default.nix
+++ pkgs/development/python-modules/ailment/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "ailment";
-  version = "9.2.84";
+  version = "9.2.85";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-I4lZrp4coJOBB8gREmeQsCiNhMC0MqhYxd5BmYXq9BA=";
+    hash = "sha256-5tO+eVDVumbzg3OaMDa9SqlBzHfpvUtgif4d2+wShQc=";
   };
 
   nativeBuildInputs = [
