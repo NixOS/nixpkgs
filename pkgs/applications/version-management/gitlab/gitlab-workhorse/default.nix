dd65d0c67edff6b26e00084f6dbc909a60c266bc
Yaya <github@uwu.is>
2024-01-17 14:31:00 +0100

    gitlab: 16.7.4 -> 16.8.1
    https://gitlab.com/gitlab-org/gitlab/-/blob/v16.8.1-ee/CHANGELOG.md
    
    Co-Authored-By: ajs124 <git@ajs124.de>

--- pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
@@ -5,7 +5,7 @@ in
 buildGoModule rec {
   pname = "gitlab-workhorse";
 
-  version = "16.7.4";
+  version = "16.8.1";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitLab {
@@ -17,7 +17,7 @@ buildGoModule rec {
 
   sourceRoot = "${src.name}/workhorse";
 
-  vendorHash = "sha256-1oeToeqGXSj5CdL5dgWsnN/VpwALlus93P0Ed7G8TIw=";
+  vendorHash = "sha256-OcdfanSlH1ZUmKlhzMOD3oNySx1o92Is7EDf1Z3953A=";
   buildInputs = [ git ];
   ldflags = [ "-X main.Version=${version}" ];
   doCheck = false;
