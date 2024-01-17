NixOS/nixpkgs

commit d0c27c8077d1f32bc7b9138aec70f7c650e29409

python3Packages.django-mailman3: 1.3.9 -> 1.3.11

pkgs/development/python-modules/django-mailman3/default.nix
@@ -17,12 +17,12 @@
 
 buildPythonPackage rec {
   pname = "django-mailman3";
-  version = "1.3.9";
+  version = "1.3.11";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-GpI1W0O9aJpLF/mcS23ktJDZsP69S2zQy7drOiWBnTM=";
+    hash = "sha256-uIjJaZHWL2evj+oISLprvKWT5Sm5f2EKgUD1twL1VbQ=";
   };
 
   patches = [

commit 40b68099580b7f42080273fea08ef373bb02fd6c

python3.pkgs.django-mailman3: drop applied patches
Fixes: d0c27c8077d1 ("python3Packages.django-mailman3: 1.3.9 -> 1.3.11")

pkgs/development/python-modules/django-mailman3/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, fetchpatch
 
 # propagates
 , django-gravatar2
@@ -24,17 +23,6 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-uIjJaZHWL2evj+oISLprvKWT5Sm5f2EKgUD1twL1VbQ=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://gitlab.com/mailman/django-mailman3/-/commit/840d0d531a0813de9a30e72427e202aea21b40fe.patch";
-      hash = "sha256-vltvsIP/SWpQZeXDUB+GWlTu+ghFMUqIT8i6CrYcmGo=";
-    })
-    (fetchpatch {
-      url = "https://gitlab.com/mailman/django-mailman3/-/commit/25c55e31d28f2fa8eb23f0e83c12f9b0a05bfbf0.patch";
-      hash = "sha256-ug5tBmnVfJTn5ufDDVg/cEtsZM59jQYJpQZV51T3qIc=";
-    })
-  ];
 
   postPatch = ''
     substituteInPlace setup.py \