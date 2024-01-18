f5675fe077f67fda94d3b0a53473f04c8a93d62b
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-18 13:55:08 +0000

    python311Packages.zigpy: 0.60.2 -> 0.60.6

--- pkgs/development/python-modules/zigpy/default.nix
+++ pkgs/development/python-modules/zigpy/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "zigpy";
-  version = "0.60.2";
+  version = "0.60.6";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zigpy";
     rev = "refs/tags/${version}";
-    hash = "sha256-3hYgb2uvyFQmtfdVKBorGhTgVt/Dq1roXTu7xvE7SHY=";
+    hash = "sha256-hoDjDoRrft3CjproNJ9VROFZFaVZNBAJmDMtEQUc//8=";
   };
 
   postPatch = ''
