294433bb4d9c061a664ef8fa334c1f45023046e7
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-18 13:14:01 +0000

    python311Packages.wyoming: 1.4.0 -> 1.5.0

--- pkgs/development/python-modules/wyoming/default.nix
+++ pkgs/development/python-modules/wyoming/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "wyoming";
-  version = "1.4.0";
+  version = "1.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "rhasspy";
     repo = "wyoming";
     rev = "refs/tags/${version}";
-    hash = "sha256-59/6tRHHAu31VFuKhj2LCEUqkdVi81fu5POuGJmw9bw=";
+    hash = "sha256-sXnBwX1FJp4/HruzI7lDkLULxoalaGZhzCPIzxrXTxM=";
   };
 
   nativeBuildInputs = [
