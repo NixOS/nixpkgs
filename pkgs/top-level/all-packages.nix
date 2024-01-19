016a436059667c68a886639fa6b77e27e9a2bd85
Adrian Pistol <vifino@posteo.net>
2024-01-19 18:16:29 +0100

    libyang: move to by-name; 2.1.128 -> 2.1.148

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23765,8 +23765,6 @@ with pkgs;
 
   yas = callPackage ../development/libraries/yas { };
 
-  libyang = callPackage ../development/libraries/libyang { };
-
   libcyaml = callPackage ../development/libraries/libcyaml { };
 
   rang = callPackage ../development/libraries/rang { };
