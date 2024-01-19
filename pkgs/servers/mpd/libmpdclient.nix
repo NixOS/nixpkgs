c4d49ff02f9c8653f3db1820a25a48fdaf5c1f5c
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-19 10:14:22 +0000

    libmpdclient: 2.20 -> 2.22

--- pkgs/servers/mpd/libmpdclient.nix
+++ pkgs/servers/mpd/libmpdclient.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libmpdclient";
-  version = "2.20";
+  version = "2.22";
 
   src = fetchFromGitHub {
     owner  = "MusicPlayerDaemon";
     repo   = pname;
     rev    = "v${version}";
-    sha256 = "sha256-qEgdwG7ygVblIa3uRf1tddxHg7T1yvY17nbhZ7NRNvg=";
+    sha256 = "sha256-KF8IR9YV6b9ro+L9m6nHs1IggakEZddfcBKm/oKCVZY=";
   };
 
   nativeBuildInputs = [ meson ninja ]
