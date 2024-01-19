120419fd374b1ec297d0c0fa42be77bcdf1b8a8d
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-19 18:51:08 +0000

    python311Packages.django-import-export: 3.3.5 -> 3.3.6

--- pkgs/development/python-modules/django-import-export/default.nix
+++ pkgs/development/python-modules/django-import-export/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "django-import-export";
-  version = "3.3.5";
+  version = "3.3.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "django-import-export";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-bYb000KRnvuMSMTTicqrJ+0zU/XguQFcLATqxUvc5V0=";
+    hash = "sha256-1c+ZGCVrHqqT9aUua+7fI8fYZYBq4I/qq1yIjdVLJPY=";
   };
 
   propagatedBuildInputs = [
