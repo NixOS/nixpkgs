5f3f093c013877f5f85ebb1a1d53aaeeb93f6f63
Anderson Torres <torres.anderson.85@protonmail.com>
2024-01-19 11:22:14 -0300

    live555: 2023.11.08 -> 2023.11.30

--- pkgs/by-name/li/live555/package.nix
+++ pkgs/by-name/li/live555/package.nix
@@ -9,7 +9,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "live555";
-  version = "2023.11.08";
+  version = "2023.11.30";
 
   src = fetchurl {
     urls = [
@@ -18,7 +18,7 @@ stdenv.mkDerivation (finalAttrs: {
       "https://download.videolan.org/contrib/live555/live.${finalAttrs.version}.tar.gz"
       "mirror://sourceforge/slackbuildsdirectlinks/live.${finalAttrs.version}.tar.gz"
     ];
-    hash = "sha256-erRHznHhUaEELDf4rsjQrGvs9IZnAkGz5krnxSDklIY=";
+    hash = "sha256-xue+9YtdAM2XkzAY6dU2PZ3n6bvPwlULIHqBqc8wuSU=";
   };
 
   patches = [
