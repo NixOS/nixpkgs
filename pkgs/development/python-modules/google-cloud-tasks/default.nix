4a75beb11794888d18cec9b27e707d2225c317f9
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-19 15:42:32 +0100

    python311Packages.google-cloud-tasks: 2.15.0 -> 2.15.1
    Changelog: https://github.com/googleapis/google-cloud-python/blob/google-cloud-tasks-v2.15.1/packages/google-cloud-tasks/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-tasks/default.nix
+++ pkgs/development/python-modules/google-cloud-tasks/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-tasks";
-  version = "2.15.0";
+  version = "2.15.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-SpmTjbARHVU3hkG1I1uY5r12S8jip+JN9wb4uGO98nw=";
+    hash = "sha256-wkk3hefFhfDvL7iSFbOZqzTLaVBJPydSItjt5P4/zxY=";
   };
 
   propagatedBuildInputs = [
