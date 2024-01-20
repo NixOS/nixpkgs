b386499bc5e2fa63aeaab177e94f722e407b17fd
tsandrini <tomas.sandrini@seznam.cz>
2024-01-20 21:11:24 +0100

    pywalfox-native: fixup manifest installation

--- pkgs/by-name/py/pywalfox-native/package.nix
+++ pkgs/by-name/py/pywalfox-native/package.nix
@@ -1,4 +1,4 @@
-{ lib, python3, fetchPypi }:
+{ lib, stdenv, python3, fetchPypi }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "pywalfox-native";
@@ -9,6 +9,21 @@ python3.pkgs.buildPythonApplication rec {
     pname = "pywalfox";
     hash = "sha256-Wec9fic4lXT7gBY04D2EcfCb/gYoZcrYA/aMRWaA7WY=";
   };
+
+  postInstall = ''
+    # Overwrite the original wrapper script with a new one that has the
+    # appropriate executable paths.
+    cat > $out/lib/${python3.libPrefix}/site-packages/pywalfox/bin/main.sh <<'EOF'
+    #!${stdenv.shell}
+      ${placeholder "out"}/bin/pywalfox start
+    EOF
+
+    # The install.py script forcefully sets executable privileged no matter
+    # what during the installation. This is undesired due to read-only nix store
+    substituteInPlace $out/lib/${python3.libPrefix}/site-packages/pywalfox/install.py  \
+      --replace "set_executable_permissions(BIN_PATH_UNIX)" \
+      "#set_executable_permissions(BIN_PATH_UNIX) # Note: read-only /nix/store"
+  '';
 
   pythonImportsCheck = [ "pywalfox" ];
 
