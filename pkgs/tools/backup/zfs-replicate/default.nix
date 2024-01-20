7eea7beafd27a90eec82f31f7f8799dd168d4d67
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-20 04:10:53 +0000

    zfs-replicate: 3.1.4 -> 3.2.3

--- pkgs/tools/backup/zfs-replicate/default.nix
+++ pkgs/tools/backup/zfs-replicate/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonApplication rec {
   pname = "zfs_replicate";
-  version = "3.1.4";
+  version = "3.2.3";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-mRINo20/uFlWtP5W7w+D2E9o89hlAsqZmBjuv0qWP9k=";
+    hash = "sha256-uzA8cQTUWAGLEhISZhaU6hNe9xcsF9jPZ1ETOi1XIFc=";
   };
 
   postPatch = ''
