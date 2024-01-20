b114486bae60576da10807fd58dd2a937ddb4901
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-20 13:37:43 +0000

    dpkg: 1.22.1 -> 1.22.3

--- pkgs/tools/package-management/dpkg/default.nix
+++ pkgs/tools/package-management/dpkg/default.nix
@@ -18,12 +18,12 @@
 
 stdenv.mkDerivation rec {
   pname = "dpkg";
-  version = "1.22.1";
+  version = "1.22.3";
 
   src = fetchgit {
     url = "https://git.launchpad.net/ubuntu/+source/dpkg";
     rev = "applied/${version}";
-    hash = "sha256-63XRO3Img+XS2F5Krb5DAw0LMhtxB+eJi754O03Lx8Q=";
+    hash = "sha256-UlAG7MrzaDKGhrGLG+FDHt+d/E/ULiyogYokdLrM04c=";
   };
 
   configureFlags = [
