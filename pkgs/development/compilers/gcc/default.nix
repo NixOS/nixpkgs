43aeeaf1c4da794b10c915c130c4516b246a7497
Adam Joseph <adam@westernsemico.com>
2024-01-19 20:01:58 -0800

    gcc: link $lib/lib -> $lib/$targetConfig correctly and consistently
    When native-compiling, gcc will install libraries into:
    
      /nix/store/...-$targetConfig-gcc-$version-lib/lib
    
    When cross-compiling, gcc will install libraries into:
    
      /nix/store/...-$targetConfig-gcc-$version-lib/$targetConfig
    
    When cross-compiling, we intended to create a link from $lib/lib to
    $lib/$targetConfig, so that downstream users can always safely
    assume that "${lib.getLib stdenv.cc.cc}/lib" is where the gcc
    libraries are, regardless of whether `stdenv.cc.cc` is a cross
    compiler or a native compiler.
    
    Unfortunately, there were two problems with how we were trying to
    create these links:
    
    1. The link would be created only when `enableLibGccOutput==true`
    
    2. The link was being created from the incorrect source
       `$lib/lib/lib` instead of `$lib/lib`.
    
    Both of these mistakes are my fault.  This commit corrects them by
    creating the link using `ln -Ts` (which is more predictable) and by
    creating the link from `gcc/common/builder.nix` rather than from
    `gcc/common/libgcc.nix`.

--- pkgs/development/compilers/gcc/default.nix
+++ pkgs/development/compilers/gcc/default.nix
@@ -103,6 +103,7 @@ let inherit version;
     disableBootstrap = atLeast11 && !stdenv.hostPlatform.isDarwin && (atLeast12 -> !profiledCompiler);
 
     inherit (stdenv) buildPlatform hostPlatform targetPlatform;
+    targetConfig = if targetPlatform != hostPlatform then targetPlatform.config else null;
 
     patches = callFile ./patches {};
 
@@ -124,6 +125,7 @@ let inherit version;
         buildPlatform
         hostPlatform
         targetPlatform
+        targetConfig
         patches
         crossMingw
         stageNameAddon
@@ -329,7 +331,7 @@ lib.pipe ((callFile ./common/builder.nix {}) ({
     ++ optional (is7 && targetPlatform.isAarch64) "--enable-fix-cortex-a53-843419"
     ++ optional (is7 && targetPlatform.isNetBSD) "--disable-libcilkrts";
 
-  targetConfig = if targetPlatform != hostPlatform then targetPlatform.config else null;
+  inherit targetConfig;
 
   buildFlags =
     # we do not yet have Nix-driven profiling
