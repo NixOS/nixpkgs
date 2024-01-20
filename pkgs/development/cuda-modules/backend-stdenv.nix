780b25323791ba0dd4cb02c3515a0be8ece0e9f3
Adam Joseph <adam@westernsemico.com>
2024-01-19 20:40:59 -0800

    cc-wrapper: relocate proprietary-compiler-specific material
    PR #275947, which was self-merged without approvals, inserted
    functionality specific to a propriteary closed-source compiler
    (CUDA) into cc-wrapper.
    
    This commit relocates this CUDA-specific functionality into the
    appropritate place: `cuda-modules`.
    
    It is unclear to me exactly what this function is supposed to be
    doing; much of it (like the `.kind` attributes) do not appear to be
    used *anywhere* in nixpkgs.  Making sure we don't insert unexplained
    deadcode like this is one of the important functions of the review
    process.

--- pkgs/development/cuda-modules/backend-stdenv.nix
+++ pkgs/development/cuda-modules/backend-stdenv.nix
@@ -23,6 +23,26 @@ in
 assert let
   cxxStdlibCuda = cudaStdenv.cc.cxxStdlib.package;
   cxxStdlibNixpkgs = stdenv.cc.cxxStdlib.package;
+
+  # Expose the C++ standard library we're using. See the comments on "General
+  # libc++ support". This is also relevant when using older gcc than the
+  # stdenv's, as may be required e.g. by CUDAToolkit's nvcc.
+  cxxStdlib = libcxx:
+    let
+      givenLibcxx = libcxx != null && (libcxx.isLLVM or false);
+      givenGccForLibs = libcxx != null && !(libcxx.isLLVM or false) && (libcxx.isGNU or false);
+      libcxx_solib = "${lib.getLib libcxx}/lib";
+    in
+      if (!givenLibcxx) && givenGccForLibs then
+        { kind = "libstdc++"; package = libcxx; solib = libcxx_solib; }
+      else if givenLibcxx then
+        { kind = "libc++"; package = libcxx;  solib = libcxx_solib;}
+      else
+        # We're probably using the `libstdc++` that came with our `gcc`.
+        # TODO: this is maybe not always correct?
+        # TODO: what happens when `nativeTools = true`?
+        { kind = "libstdc++"; package = cc; solib = cc_solib; }
+  ;
 in
 ((stdenv.cc.cxxStdlib.kind or null) == "libstdc++")
 -> lib.versionAtLeast cxxStdlibCuda.version cxxStdlibNixpkgs.version;
