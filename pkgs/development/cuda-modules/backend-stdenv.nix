f565379baf885a9898b097364c745035c64d70da
Adam Joseph <adam@westernsemico.com>
2024-01-19 20:54:15 -0800

    cuda-modules: reenable functionality

--- pkgs/development/cuda-modules/backend-stdenv.nix
+++ pkgs/development/cuda-modules/backend-stdenv.nix
@@ -19,17 +19,18 @@ let
   assertCondition = true;
 in
 
-/*
+
 # We should use libstdc++ at least as new as nixpkgs' stdenv's one.
 assert let
-  cxxStdlibCuda = cudaStdenv.cc.cxxStdlib.package;
-  cxxStdlibNixpkgs = stdenv.cc.cxxStdlib.package;
+  cxxStdlibCuda = (cxxStdlib cudaStdenv).package;
+  cxxStdlibNixpkgs = (cxxStdlib stdenv).package;
 
   # Expose the C++ standard library we're using. See the comments on "General
   # libc++ support". This is also relevant when using older gcc than the
   # stdenv's, as may be required e.g. by CUDAToolkit's nvcc.
-  cxxStdlib = libcxx:
+  cxxStdlib = stdenv:
     let
+      libcxx = stdenv.cc.libcxx;
       givenLibcxx = libcxx != null && (libcxx.isLLVM or false);
       givenGccForLibs = libcxx != null && !(libcxx.isLLVM or false) && (libcxx.isGNU or false);
       libcxx_solib = "${lib.getLib libcxx}/lib";
@@ -39,14 +40,14 @@ assert let
       else if givenLibcxx then
         { kind = "libc++"; package = libcxx;  solib = libcxx_solib;}
       else
-        # We're probably using the `libstdc++` that came with our `gcc`.
-        # TODO: this is maybe not always correct?
-        # TODO: what happens when `nativeTools = true`?
-        { kind = "libstdc++"; package = cc; solib = cc_solib; }
-  ;
+        { kind = "libstdc++"; package = stdenv.cc;
+          solib = throw ''
+            # We're probably using the `libstdc++` that came with our `gcc`.
+            # TODO: this is maybe not always correct?
+            # TODO: what happens when `nativeTools = true`?
+          ''; };
 in
-((stdenv.cc.cxxStdlib.kind or null) == "libstdc++")
+(((cxxStdlib stdenv).kind or null) == "libstdc++")
 -> lib.versionAtLeast cxxStdlibCuda.version cxxStdlibNixpkgs.version;
-*/
 
 lib.extendDerivation assertCondition passthruExtra cudaStdenv
