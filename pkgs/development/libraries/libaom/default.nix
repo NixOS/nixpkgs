59fe697100e0bc5046c20c314c2d5689831b4b30
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-20 00:07:51 +0000

    libaom: 3.8.0 -> 3.8.1

--- pkgs/development/libraries/libaom/default.nix
+++ pkgs/development/libraries/libaom/default.nix
@@ -9,11 +9,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "libaom";
-  version = "3.8.0";
+  version = "3.8.1";
 
   src = fetchzip {
     url = "https://aomedia.googlesource.com/aom/+archive/v${version}.tar.gz";
-    hash = "sha256-JxMz+XnjmUvk8TlTqdU2HP1Gq3bXfcLkXp5AEv9+7hM=";
+    hash = "sha256-qng9fEbm71HqPnPzfgqswSium9egIgpB6ZLesOQVg6c=";
     stripRoot = false;
   };
 
