b0c02928501e9620da1c6abd25ca7b3ec6df73e2
Aaron Jheng <wentworth@outlook.com>
2024-01-21 09:25:15 +0800

    cue: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3728,8 +3728,6 @@ with pkgs;
 
   cudd = callPackage ../development/libraries/cudd { };
 
-  cue = callPackage ../development/tools/cue { };
-
   writeCueValidator = callPackage ../development/tools/cue/validator.nix { };
 
   cuelsp = callPackage ../development/tools/cuelsp { };
