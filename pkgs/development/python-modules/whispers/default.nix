f07c4b14532514b6c1f8f63f8e943ed9b9435b6a
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-21 16:23:45 +0100

    python311Packages.whispers: refactor

--- pkgs/development/python-modules/whispers/default.nix
+++ pkgs/development/python-modules/whispers/default.nix
@@ -12,21 +12,31 @@
 , pytestCheckHook
 , pythonOlder
 , pyyaml
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "whispers";
   version = "2.2.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "adeptex";
-    repo = pname;
+    repo = "whispers";
     rev = "refs/tags/${version}";
     hash = "sha256-9vXku8BWJtlf+lmAcQ8a7qTisRNc+xVw0T0Eunc4lt4=";
   };
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace '"pytest-runner"' ""
+  '';
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     astroid
@@ -44,11 +54,6 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace '"pytest-runner"' ""
-  '';
-
   preCheck = ''
     # Some tests need the binary available in PATH
     export PATH=$out/bin:$PATH
@@ -60,7 +65,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Tool to identify hardcoded secrets in static structured text";
-    homepage = "https://github.com/Skyscanner/whispers";
+    homepage = "https://github.com/adeptex/whispers";
+    changelog = "https://github.com/adeptex/whispers/releases/tag/${version}";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ fab ];
   };
