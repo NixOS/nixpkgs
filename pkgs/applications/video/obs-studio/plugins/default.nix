887365a6887b65c8f1312b85dc2b14ecd662ae9e
Gaetan Lepage <gaetan@glepage.com>
2024-01-21 16:50:25 +0100

    obs-studio-plugins.obs-composite-blur: init at 1.1.0

--- pkgs/applications/video/obs-studio/plugins/default.nix
+++ pkgs/applications/video/obs-studio/plugins/default.nix
@@ -20,6 +20,8 @@
 
   obs-command-source = callPackage ./obs-command-source.nix { };
 
+  obs-composite-blur = callPackage ./obs-composite-blur.nix { };
+
   obs-freeze-filter = qt6Packages.callPackage ./obs-freeze-filter.nix { };
 
   obs-gradient-source = callPackage ./obs-gradient-source.nix { };
