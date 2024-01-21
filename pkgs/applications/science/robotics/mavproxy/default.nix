4339631288aa89b55ff78b6e476431dc51fed7f7
Ben Wolsieffer <benwolsieffer@gmail.com>
2024-01-21 12:23:51 -0500

    mavproxy: 1.8.66 -> 1.8.69

--- pkgs/applications/science/robotics/mavproxy/default.nix
+++ pkgs/applications/science/robotics/mavproxy/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonApplication rec {
   pname = "MAVProxy";
-  version = "1.8.66";
+  version = "1.8.69";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-tIwXiDHEmFHF5Jdv25hPkzEqAdig+i5h4fW6SGIrZDM=";
+    hash = "sha256-VNwaOvMx4sKW/0+Q6FeB8XYv0uk+T/TiZeXMWzSgXKU=";
   };
 
   postPatch = ''
