f69f394511fa16f27a69831c9e19669d1a028b05
Tom Wieczorek <tom@bibbu.net>
2024-01-21 15:15:24 +0100

    yx: patch glibc out-of-bounds access
    Added a small test script to try to verify correctness.
    
    See: https://gitlab.com/tomalok/yx/-/issues/1

--- pkgs/tools/text/yx/default.nix
+++ pkgs/tools/text/yx/default.nix
@@ -1,8 +1,10 @@
 { lib
 , stdenv
 , fetchFromGitLab
+, fetchpatch
 , libyaml
 , testers
+, runCommand
 , yx
 }:
 stdenv.mkDerivation rec {
@@ -24,6 +26,14 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ libyaml ];
 
+  patches = [
+    # https://gitlab.com/tomalok/yx/-/issues/1
+    (fetchpatch {
+      url = "https://gitlab.com/tomalok/yx/-/commit/ef93d581552cf69d9396773fc8ceb849e588980c.diff";
+      hash = "sha256-Au4s/AKKKiCB9JMl27boRth69KlPsSp/Gd7qwGfRYo4=";
+    })
+  ];
+
   doCheck = true;
 
   passthru.tests.version = testers.testVersion {
@@ -31,6 +41,17 @@ stdenv.mkDerivation rec {
     command = "${meta.mainProgram} -v";
     version = "v${yx.version}";
   };
+
+  passthru.tests.isssue-1-glibc-oob-read = runCommand "${pname}-isssue-1-test" { } ''
+    for i in $(seq 1 100); do
+      actual=$(printf 'a:\n- b:' | ${yx}/bin/${meta.mainProgram} a 1)
+      if [ "$actual" != b ]; then
+        echo Affected by https://gitlab.com/tomalok/yx/-/issues/1
+        exit 1
+      fi
+    done
+    touch $out
+  '';
 
   meta = with lib; {
     description = "YAML Data Extraction Tool";
