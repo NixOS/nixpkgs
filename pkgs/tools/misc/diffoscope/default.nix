10714a23f655541f4cd0cac7f7a0036ad529d2c1
Sergei Trofimovich <slyich@gmail.com>
2024-01-21 09:08:27 +0000

    diffoscope: 253 -> 254
    Changes: https://diffoscope.org/news/diffoscope-254-released/
    (cherry picked from commit 9c13e10574582c0f2e8f256751fb0c76fb6a2510)

--- pkgs/tools/misc/diffoscope/default.nix
+++ pkgs/tools/misc/diffoscope/default.nix
@@ -79,11 +79,11 @@
 # Note: when upgrading this package, please run the list-missing-tools.sh script as described below!
 python3.pkgs.buildPythonApplication rec {
   pname = "diffoscope";
-  version = "253";
+  version = "254";
 
   src = fetchurl {
     url = "https://diffoscope.org/archive/diffoscope-${version}.tar.bz2";
-    hash = "sha256-xI+SIEUPKFxz7sk9qqE1ibSJX0WRPnJEpco0Mqv7Wp8=";
+    hash = "sha256-LsRJUFrWMo9OiKL0/yBokAxY0b4gMGVRHyWMS1zhDjE=";
   };
 
   outputs = [
