0730e1d7fbf9208e7afff09d42b92b94ed569a83
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-21 13:24:14 +0100

    python311Packages.find-libpython: refactor

--- pkgs/development/python-modules/find-libpython/default.nix
+++ pkgs/development/python-modules/find-libpython/default.nix
@@ -3,24 +3,35 @@
 , fetchPypi
 , pytestCheckHook
 , pythonOlder
+, setuptools
+, setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "find-libpython";
   version = "0.3.0";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit version;
     pname = "find_libpython";
-    sha256 = "sha256-bn/l2a9/rW3AZstVFaDpyQpx8f6yuy+OTNu0+DJ26eU=";
+    hash = "sha256-bn/l2a9/rW3AZstVFaDpyQpx8f6yuy+OTNu0+DJ26eU=";
   };
 
-  disabled = pythonOlder "3.7";
+  nativeBuildInputs = [
+    setuptools
+    setuptools-scm
+  ];
 
-  pythonImportsCheck = [ "find_libpython" ];
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  pythonImportsCheck = [ 
+    "find_libpython"
+  ];
 
   meta = with lib; {
     description = "Finds the libpython associated with your environment, wherever it may be hiding";
