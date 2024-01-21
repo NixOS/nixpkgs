abf2920481c3563cd2299b8aad7d548e8653ee87
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-21 16:28:37 +0100

    python311Packages.whispers: add patch to support astroid > 3

--- pkgs/development/python-modules/whispers/default.nix
+++ pkgs/development/python-modules/whispers/default.nix
@@ -4,6 +4,7 @@
 , buildPythonPackage
 , crossplane
 , fetchFromGitHub
+, fetchpatch
 , jellyfish
 , jproperties
 , luhn
@@ -28,6 +29,26 @@ buildPythonPackage rec {
     rev = "refs/tags/${version}";
     hash = "sha256-9vXku8BWJtlf+lmAcQ8a7qTisRNc+xVw0T0Eunc4lt4=";
   };
+
+  patches = [
+    # Support astroid > 3, https://github.com/adeptex/whispers/pull/117
+    (fetchpatch {
+      url = "https://github.com/adeptex/whispers/commit/ff25e81cb3d775e5fb186c2d135b77c27d9ed43a.patch";
+      hash = "sha256-jKm7fs04mGUD7MZYAA/3xt01e9knuLun3c3u8PlLebg=";
+    })
+    (fetchpatch {
+      url = "https://github.com/adeptex/whispers/commit/ba6a56dddb12d1cb62f94dd7659ba24fdc4363ee.patch";
+      hash = "sha256-eHWnXHT0lzS7BqneMqfvV3w6GfrCiTJ5i+av82J+fpk=";
+    })
+    (fetchpatch {
+      url = "https://github.com/adeptex/whispers/commit/8b7b1593eb86abfc09b3581d463fc7d0e06309dc.patch";
+      hash = "sha256-JcRdv5eIyXKWaVqbJZlYqiSieE4z0MKF4dvO/hRBBMs=";
+    })
+    (fetchpatch {
+      url = "https://github.com/adeptex/whispers/commit/71dcb614e4d9e0247afc50cd4214659739f8844e.patch";
+      hash = "sha256-7XIFuc8Rf2ValN3BoAJOjSqjgmiOauxCFonMgGljFg0=";
+    })
+  ];
 
   postPatch = ''
     substituteInPlace setup.py \
