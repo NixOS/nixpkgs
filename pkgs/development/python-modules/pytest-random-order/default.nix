85c1d08cb5358a5567abacb83fc7aa238b6e977b
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-21 13:07:02 +0100

    python311Packages.pytest-random-order: refactor

--- pkgs/development/python-modules/pytest-random-order/default.nix
+++ pkgs/development/python-modules/pytest-random-order/default.nix
@@ -1,28 +1,49 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, pythonOlder
+, py
 , pytest
+, pytest-xdist
+, pytestCheckHook
+, pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
-  version = "1.1.1";
-  format = "setuptools";
   pname = "pytest-random-order";
+  version = "1.1.1";
+  pyproject = true;
+
+  disabled = pythonOlder "3.5";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-RHLX008fHF86NZxP/FwT7QZSMvMeyhnIhEwatAbnkIA=";
   };
 
-  disabled = pythonOlder "3.5";
+  nativeBuildInputs = [
+    setuptools
+  ];
 
-  buildInputs = [ pytest ];
+  buildInputs = [
+    pytest
+  ];
+
+  nativeCheckInputs = [
+    py
+    pytest-xdist
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "random_order"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/jbasko/pytest-random-order";
     description = "Randomise the order of tests with some control over the randomness";
+    changelog = "https://github.com/jbasko/pytest-random-order/releases/tag/v${version}";
     license = licenses.mit;
-    maintainers = [ maintainers.prusnak ];
+    maintainers = with maintainers; [ prusnak ];
   };
 }
