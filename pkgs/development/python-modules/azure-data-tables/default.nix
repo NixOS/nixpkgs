8337f7b8989cdeaf7d920561deb22748890b6434
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-01-22 23:54:10 +0100

    python311Packages.azure-data-tables: drop support for python 3.7

--- pkgs/development/python-modules/azure-data-tables/default.nix
+++ pkgs/development/python-modules/azure-data-tables/default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   version = "12.5.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
