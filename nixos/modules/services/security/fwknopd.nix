2851bd6258e01f342e913916063960fbeb40c490
Kosyrev Serge <kosyrev.serge@protonmail.com>
2024-01-22 21:34:02 +0400

    fwknopd service:  dictionary-based configuration processing & review fixes

--- nixos/modules/services/security/fwknopd.nix
+++ nixos/modules/services/security/fwknopd.nix
@@ -1,93 +1,68 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
   cfg = config.services.fwknopd;
 
-  configFile = pkgs.writeText "fwknopd.conf"
-    (with cfg;
-      (''
-         ENABLE_DIGEST_PERSISTENCE Y
-         ENABLE_SPA_PACKET_AGING Y
-         ENABLE_CMD_SUDO_EXEC Y
-         MAX_SPA_PACKET_AGE ${toString maxSpaPacketAge}
-       ''
-       + optionalString (pcapFilter != null)
-       ''
-         PCAP_FILTER ${pcapFilter}
-       ''
-       + optionalString (tcpServerPort == true)
-       ''
-         ENABLE_TCP_SERVER Y
-         TCP_SERVER_PORT ${toString tcpServerPort}
-       ''
-       + optionalString (udpServerPort != null)
-       ''
-         ENABLE_UDP_SERVER Y
-         UDP_SERVER_PORT ${toString udpServerPort}
-       ''
-       + optionalString (disablePcapPromisc == true)
-       ''
-         ENABLE_PCAP_PROMISC N
-       ''));
+  ## Transform an attrset to a fwknopd config file.
+  toFwknopd = kv:
+    let renderKeyValue = k: v:
+          if v != null && v != [] then k + " " + renderValue v + "\n"
+          else "";
+        renderValue = v:
+          let err = t: v: abort
+            ("toFwknopd: " +
+             "${t} not supported: ${lib.toPretty {} v}");
+          in   if lib.isInt      v then toString v
+          else if lib.isString   v then v
+          else if true  ==       v then "Y"
+          else if false ==       v then "N"
+          else if lib.isList     v then lib.concatStringsSep "," (map toString v)
+          else if lib.isAttrs    v then err "attrsets" v
+          else if lib.isFunction v then err "functions" v
+          else err "this value of type '${__typeOf v}' is" (toString v);
+    in lib.concatStrings (lib.mapAttrsToList renderKeyValue kv) + "\n";
 
-  accessFile = pkgs.writeText "fwknopd-access.conf"
-    ''
-      ${
-        let f = name: cfg:
-          with cfg;
-          ''
-            SOURCE         ${name}
-            DESTINATION    ${destination}
-          ''
-          + optionalString (openPorts != [])
-          ''
-            OPEN_PORTS     ${concatStringsSep "," (map toString openPorts)}
-          ''
-          + optionalString (restrictPorts != [])
-          ''
-            RESTRICT_PORTS ${concatStringsSep "," (map toString restrictPorts)}
-          ''
-          + optionalString (fwAccessTimeout != null)
-          ''
-            FW_ACCESS_TIMEOUT ${toString fwAccessTimeout}
-          ''
-          + optionalString (accessExpire != null)
-          ''
-            ACCESS_EXPIRE ${accessExpire}
-          ''
-          + optionalString (accessExpireEpoch != null)
-          ''
-            ACCESS_EXPIRE_EPOCH ${toString accessExpireEpoch}
-          ''
-          + optionalString (cmdCycleOpen != null)
-          ''
-            CMD_CYCLE_OPEN ${cmdCycleOpen}
-          ''
-          + optionalString (cmdCycleClose != null)
-          ''
-            CMD_CYCLE_CLOSE ${cmdCycleClose}
-          ''
-          + optionalString (cmdCycleTimer != null)
-          ''
-            CMD_CYCLE_TIMER ${toString cmdCycleTimer}
-          ''
-          + optionalString (requireUsername != null)
-          ''
-            REQUIRE_USERNAME ${requireUsername}
-          ''
-          + optionalString (requireSourceAddress == true)
-          ''
-            REQUIRE_SOURCE_ADDRESS Y
-          '';
-        in concatStringsSep "\n"
-          (mapAttrsToList f cfg.access
-           ++
-           map (f: "%include ${f}") cfg.accessInclude)
-      }
-    '';
+  configAttrs =
+    {
+      ENABLE_DIGEST_PERSISTENCE = true;
+      ENABLE_SPA_PACKET_AGING   = true;
+      ENABLE_CMD_SUDO_EXEC      = true;
+      MAX_SPA_PACKET_AGE        = cfg.maxSpaPacketAge;
+      PCAP_FILTER               = cfg.pcapFilter;
+      ENABLE_PCAP_PROMISC       = !cfg.disablePcapPromisc;
+    } // lib.optionalAttrs (cfg.tcpServerPort != null) {
+      ENABLE_TCP_SERVER         = true;
+      TCP_SERVER_PORT           = cfg.tcpServerPort;
+    } // lib.optionalAttrs (cfg.udpServerPort != null) {
+      ENABLE_UDP_SERVER         = true;
+      UDP_SERVER_PORT           = cfg.udpServerPort;
+    } // cfg.extraRawConfig;
 
+  configRendered = toFwknopd configAttrs;
+
+  accessSectionAttrs =
+    name: section:
+    with section;
+    {
+      SOURCE                 = name;
+      DESTINATION            = destination;
+      OPEN_PORTS             = openPorts;
+      RESTRICT_PORTS         = restrictPorts;
+      FW_ACCESS_TIMEOUT      = fwAccessTimeout;
+      ACCESS_EXPIRE          = accessExpire;
+      ACCESS_EXPIRE_EPOCH    = accessExpireEpoch;
+      CMD_CYCLE_OPEN         = cmdCycleOpen;
+      CMD_CYCLE_CLOSE        = cmdCycleClose;
+      CMD_CYCLE_TIMER        = cmdCycleTimer;
+      REQUIRE_USERNAME       = requireUsername;
+      REQUIRE_SOURCE_ADDRESS = requireSourceAddress;
+    };
+
+  accessRendered' = lib.mapAttrsToList (name: sectn: toFwknopd (accessSectionAttrs name sectn) + "\n\n") cfg.access
+                    ++ [("\n" + lib.concatStringsSep "\n" (map (f: "%include ${f}") cfg.accessInclude))];
+  accessRendered = lib.concatStringsSep "\n" accessRendered';
+
+  inherit (lib) types;
 in
 
 {
@@ -96,119 +71,131 @@ in
 
   options = {
 
-    services.fwknopd = with types; {
+    services.fwknopd = {
 
-      enable = mkEnableOption "the fwknopd daemon";
+      enable = lib.mkEnableOption "the fwknopd daemon";
 
-      interface = mkOption {
-        type = str;
+      interface = lib.mkOption {
+        type = types.str;
         default = null;
         description = "Network interface to listen on.";
       };
 
-      disablePcapPromisc = mkOption {
-        type = nullOr bool;
+      disablePcapPromisc = lib.mkOption {
+        type = types.nullOr types.bool;
         default = true;
         description = "By default fwknopd puts the pcap interface into promiscuous mode. This disables the promiscuity.";
       };
 
-      pcapFilter = mkOption {
-        type = nullOr str;
+      pcapFilter = lib.mkOption {
+        type = types.nullOr types.str;
         default = null;
         description = "For promiscuous mode:  override the default PCAP filter.";
       };
 
-      udpServerPort = mkOption {
-        type = nullOr int;
+      udpServerPort = lib.mkOption {
+        type = types.nullOr types.int;
         default = null;
         description = "Enable UDP server on this port.";
       };
 
-      tcpServerPort = mkOption {
-        type = nullOr int;
+      tcpServerPort = lib.mkOption {
+        type = types.nullOr types.int;
         default = null;
         description = "Enable TCP server on this port.";
       };
 
-      test = mkOption {
-        type = bool;
+      test = lib.mkOption {
+        type = types.bool;
         default = false;
         description = "Test mode: no firewall changes.";
       };
 
-      verbose = mkOption {
-        type = bool;
+      verbose = lib.mkOption {
+        type = types.bool;
         default = false;
         description = "Verbose logging.";
       };
 
-      maxSpaPacketAge = mkOption {
-        type = int;
+      maxSpaPacketAge = lib.mkOption {
+        type = types.int;
         default = 120;
         description = "Defines the maximum age (in seconds) that an SPA packet will be accepted. This requires that the client system is in relatively close time synchronization with the fwknopd server system (NTP is good). The default age is 120 seconds (two minutes)";
       };
 
-      accessInclude = mkOption {
-        type = listOf str;
+      extraRawConfig = lib.mkOption {
+        type = types.attrs;
+        default = {};
+        description = "A verbatim, dictionary-based passthrough for the options.";
+      };
+
+      accessInclude = lib.mkOption {
+        type = types.listOf types.str;
         default = [];
         description = "List of file paths to include in access.conf.";
       };
 
-      access = mkOption {
+      extraRawAccess = lib.mkOption {
+        type = types.attrs;
+        default = {};
+        description = "A verbatim, dictionary-based passthrough for the access file.";
+      };
+
+      access = lib.mkOption {
         type = types.attrsOf (types.submodule {
           options = {
-            destination = mkOption {
-              type = str;
+            destination = lib.mkOption {
+              type = types.str;
               default = "ANY";
               description = "Address of arrival of the knock.";
             };
-            openPorts = mkOption {
-              type = listOf int;
+            openPorts = lib.mkOption {
+              type = types.listOf types.int;
               default = [];
               description = "Ports to open on successful knock.";
             };
-            restrictPorts = mkOption {
-              type = listOf int;
+            restrictPorts = lib.mkOption {
+              type = types.listOf types.int;
               default = [];
               description = "Ports to close on successful knock.";
             };
-            fwAccessTimeout = mkOption {
-              type = int;
+            fwAccessTimeout = lib.mkOption {
+              type = types.int;
               default = null;
               description = "How many seconds to keep the access open.";
             };
-            accessExpire = mkOption {
-              type = nullOr str;
+            accessExpire = lib.mkOption {
+              type = types.nullOr types.str;
               default = null;
               description = "Ignore knocks after MM/DD/YYYY.";
             };
-            accessExpireEpoch = mkOption {
-              type = nullOr int;
+            accessExpireEpoch = lib.mkOption {
+              type = types.nullOr types.int;
               default = null;
               description = "Ignore knocks after Unix epoch time.";
             };
-            cmdCycleOpen = mkOption {
-              type = nullOr str;
+            cmdCycleOpen = lib.mkOption {
+              type = types.nullOr types.str;
               default = null;
               description = "Command to execute on a successful knock.";
             };
-            cmdCycleClose = mkOption {
-              type = nullOr str;
+            cmdCycleClose = lib.mkOption {
+              type = types.nullOr types.str;
               default = null;
               description = "Command to execute on a successful knock, after cmdCycleTimer expires.";
             };
-            cmdCycleTimer = mkOption {
-              type = nullOr int;
+            cmdCycleTimer = lib.mkOption {
+              type = types.nullOr types.int;
               default = null;
               description = "How many seconds to wait, before invoking cmdCycleClose.";
             };
-            requireUsername = mkOption {
-              type = nullOr str;
+            requireUsername = lib.mkOption {
+              type = types.nullOr types.str;
               default = null;
               description = "Require a user name to be specified in the knock packet.";
             };
-            requireSourceAddress = mkOption {
-              type = nullOr bool;
+            requireSourceAddress = lib.mkOption {
+              type = types.nullOr types.bool;
               default = true;
               description = "Require the real source address to be included in the packet (recommended).";
             };
@@ -229,12 +216,11 @@ in
     };
   };
 
-
   ###### implementation
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
 
-    warnings = mkIf (cfg.test == true) [
+    warnings = lib.mkIf (cfg.test == true) [
       "fwknopd operating in test mode!"
     ];
 
@@ -247,9 +233,10 @@ in
     users.groups.fwknopd = {};
 
     systemd.targets.fwknop = {
-          wantedBy = [ "multi-user.target" ];
-          wants = [ "fwknopd.service" ];
-        };
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "fwknopd.service" ];
+    };
+
     systemd.services.fwknopd = {
       description = "Single Packet Authorization server: fwknopd";
       documentation = [ "man:fwknopd(8) http://www.cipherdyne.org/fwknop/" ];
@@ -266,21 +253,15 @@ in
         StateDirectoryMode = "700";
         AmbientCapabilities = "CAP_NET_RAW CAP_NET_ADMIN";
         Restart = "on-failure";
-        ExecStart = pkgs.writeShellScript "fwknopd-start.sh"
-          ''
-          ${escapeShellArgs
-            ([ "${pkgs.fwknop}/bin/fwknopd"
-             "--sudo-exe"    "/run/wrappers/bin/sudo"
-             "--foreground"
-             "--interface"    cfg.interface
-             "--run-dir"     "/var/lib/fwknopd"
-             "--config-file" "${configFile}"
-             "--access-file" "${accessFile}"
-           ]
-          ++ optional cfg.test      "--test"
-          ++ optional cfg.verbose   "--verbose"
-          )}
-          '';
+        ExecStart = "${pkgs.fwknop}/bin/fwknopd " +
+          (lib.cli.toGNUCommandLineShell { } {
+            inherit (cfg) interface test verbose;
+            sudo-exe = "/run/wrappers/bin/sudo";
+            foreground = true;
+            run-dir = "/var/lib/fwknopd";
+            config-file = pkgs.writeText "fwknopd.conf" configRendered;
+            access-file = pkgs.writeText "fwknopd-access.conf" accessRendered;
+          });
       };
     };
   };
