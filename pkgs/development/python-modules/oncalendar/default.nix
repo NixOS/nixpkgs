689c9db91678758d35b50f76e1caee04a1d94114
phaer <hello@phaer.org>
2024-01-22 15:00:12 +0100

    pythonPackages.oncalendar: init at 1.0

---
+++ pkgs/development/python-modules/oncalendar/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, python
+, buildPythonPackage
+, fetchFromGitHub
+}:
+
+buildPythonPackage rec {
+  pname = "oncalendar";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "cuu508";
+    repo = "oncalendar";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-eQYxOnL4/TJbY/nijcPl8TqK2MrwcdISKGfZQoI7828=";
+  };
+
+  checkPhase = ''
+    ${python.interpreter} setup.py test
+  '';
+
+  pythonImportsCheck = [ "oncalendar" ];
+
+  meta = with lib; {
+    description = "A systemd OnCalendar expression parser and evaluator";
+    homepage = "https://github.com/cuu508/oncalendar";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ phaer ];
+  };
+}
