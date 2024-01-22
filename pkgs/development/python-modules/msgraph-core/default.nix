73575b596211608367ffad66c958929dfcf705f7
Robert Schütz <nix@dotlambda.de>
2024-01-22 14:20:54 -0800

    python311Packages.msgraph-core: 0.2.2 -> 1.0.0
    Diff: https://github.com/microsoftgraph/msgraph-sdk-python-core/compare/v0.2.2...v1.0.0
    
    Changelog: https://github.com/microsoftgraph/msgraph-sdk-python-core/blob/v1.0.0/CHANGELOG.md

--- pkgs/development/python-modules/msgraph-core/default.nix
+++ pkgs/development/python-modules/msgraph-core/default.nix
@@ -2,47 +2,50 @@
 , buildPythonPackage
 , pythonOlder
 , fetchFromGitHub
-, flit-core
-, requests
+, setuptools
+, httpx
+, microsoft-kiota-abstractions
+, microsoft-kiota-authentication-azure
+, microsoft-kiota-http
+, azure-identity
 , pytestCheckHook
-, responses
 }:
 
 buildPythonPackage rec {
   pname = "msgraph-core";
-  version = "0.2.2";
+  version = "1.0.0";
 
-  disabled = pythonOlder "3.5";
+  disabled = pythonOlder "3.8";
 
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "microsoftgraph";
     repo = "msgraph-sdk-python-core";
     rev = "v${version}";
-    hash = "sha256-eRRlG3GJX3WeKTNJVWgNTTHY56qiUGOlxtvEZ2xObLA=";
+    hash = "sha256-VizjN7sXqPvo9VOSaaUnogTlUDJ1OA2COYNTcVRqhJA=";
   };
 
   nativeBuildInputs = [
-    flit-core
+    setuptools
   ];
 
   propagatedBuildInputs = [
-    requests
-  ];
+    httpx
+    microsoft-kiota-abstractions
+    microsoft-kiota-authentication-azure
+    microsoft-kiota-http
+  ] ++ httpx.optional-dependencies.http2;
 
   nativeCheckInputs = [
+    azure-identity
     pytestCheckHook
-    responses
   ];
 
-  disabledTestPaths = [
-    "tests/integration"
-  ];
-
-  pythonImportsCheck = [ "msgraph.core" ];
+  pythonImportsCheck = [ "msgraph_core" ];
 
   meta = {
+    changelog = "https://github.com/microsoftgraph/msgraph-sdk-python-core/blob/${src.rev}/CHANGELOG.md";
     description = "Core component of the Microsoft Graph Python SDK";
     homepage = "https://github.com/microsoftgraph/msgraph-sdk-python-core";
     license = lib.licenses.mit;
