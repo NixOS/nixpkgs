49a9858dadcaa088dd594e4e08cb05cddd6b6047
Minijackson <minijackson@riseup.net>
2024-01-22 16:17:57 +0100

    netbox: 3.6.9 -> 3.7.1
    Or another way to see it:
    
    netbox_3_7: init at 3.7.1
    
    Make NetBox 3.7 the default version if stateVersion >= 24.05,
    switch upgrade test to test upgrade from 3.6 to 3.7,
    remove clearcache command for >=3.7.0,
    make reindex command mandatory

--- pkgs/servers/web-apps/netbox/default.nix
+++ pkgs/servers/web-apps/netbox/default.nix
@@ -3,7 +3,7 @@ let
   generic = import ./generic.nix;
 in
 lib.fix (self: {
-  netbox = self.netbox_3_6;
+  netbox = self.netbox_3_7;
 
   netbox_3_5 = callPackage generic {
     version = "3.5.9";
@@ -30,6 +30,22 @@ lib.fix (self: {
     ];
     tests = {
       netbox = nixosTests.netbox_3_6;
+      inherit (nixosTests) netbox-upgrade;
+    };
+
+    maintainers = with lib.maintainers; [ minijackson n0emis raitobezarius ];
+  };
+
+  netbox_3_7 = callPackage generic {
+    version = "3.7.1";
+    hash = "sha256-hAwkrrjrV+XVIYe3C8f/342SPlllXUhiFuaAp+TLMUw=";
+    extraPatches = [
+      # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
+      ./config.patch
+      ./fix-doc-link.patch
+    ];
+    tests = {
+      netbox = nixosTests.netbox_3_7;
       inherit (nixosTests) netbox-upgrade;
     };
 
