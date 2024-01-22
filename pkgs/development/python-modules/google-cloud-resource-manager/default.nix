8a9b4203e4107f6f63ababc2345eeb51cd2d3418
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-01-23 00:50:59 +0100

    python310Packages.google-cloud-resource-manager: drop support for python 3.6

--- pkgs/development/python-modules/google-cloud-resource-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-resource-manager/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "1.11.0";
   format = "setuptools";
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
