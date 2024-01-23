be76e22a6ff365f3727e239ec88d3eadffd76b54
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2024-01-23 13:45:47 +0100

    badlion-client: 3.15.0 -> 4.0.0

--- pkgs/games/badlion-client/default.nix
+++ pkgs/games/badlion-client/default.nix
@@ -2,11 +2,11 @@
 
 let
   pname = "badlion-client";
-  version = "3.15.0";
+  version = "4.0.0";
 
   src = fetchurl {
     url = "https://client-updates-cdn77.badlion.net/BadlionClient";
-    hash = "sha256-HqMgY9+Xnp4uSTWr//REZGv3p7ivwLX97vxGD5wqu9E=";
+    hash = "sha256-qI/5eNgaDgyLC9NOrHaDkITZsYsBLTOyqzGwobUDKh8=";
   };
 
   appimageContents = appimageTools.extract { inherit pname version src; };
