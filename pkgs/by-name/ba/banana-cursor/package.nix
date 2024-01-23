1eaac5791b383754f65ac848c9eebe5e10996b46
GetPsyched <priyanshu@getpsyched.dev>
2024-01-23 18:33:46 +0530

    banana-cursor: refactor to build from source and update meta

--- pkgs/by-name/ba/banana-cursor/package.nix
+++ pkgs/by-name/ba/banana-cursor/package.nix
@@ -1,31 +1,50 @@
-{
-  lib,
-  stdenvNoCC,
-  fetchFromGitHub,
+{ fetchFromGitHub
+, lib
+, stdenvNoCC
+
+, # build deps
+  clickgen
+, python3Packages
 }:
-stdenvNoCC.mkDerivation rec {
+
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "banana-cursor";
   version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "ful1e5";
     repo = "banana-cursor";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-PI7381xf/GctQTnfcE0W3M3z2kqbX4VexMf17C61hT8=";
   };
 
-  dontBuild = true;
+  nativeBuildInputs = [
+    clickgen
+    python3Packages.attrs
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    ctgen build.toml -p x11 -o $out
+
+    runHook postBuild
+  '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/share/icons
-    mv themes/Banana $out/share/icons
+    mv $out/Banana $out/share/icons
+
+    runHook postInstall
   '';
 
   meta = with lib; {
+    description = "The Banana Cursor";
     homepage = "https://github.com/ful1e5/banana-cursor";
-    description = "The banana cursor theme";
-    license = licenses.gpl3;
-    platforms = platforms.linux;
+    license = licenses.gpl3Plus;
     maintainers = with maintainers; [ yrd ];
+    platforms = platforms.linux;
   };
-}
+})
