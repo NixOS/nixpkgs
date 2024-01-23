a9680e363f166802b02a4c22fefd0979990493d9
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-23 18:50:26 +0100

    python311Packages.msgspec: 0.18.5 -> 0.18.6
    Diff: https://github.com/jcrist/msgspec/compare/refs/tags/0.18.5...0.18.6
    
    Changelog: https://github.com/jcrist/msgspec/releases/tag/0.18.6

--- pkgs/development/python-modules/msgspec/default.nix
+++ pkgs/development/python-modules/msgspec/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "msgspec";
-  version = "0.18.5";
+  version = "0.18.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "jcrist";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-BcENL1vPCspzYdAHicC5AHs/7xZPWf+Yys37vKgbris=";
+    hash = "sha256-xqtV60saQNINPMpOnZRSDnicedPSPBUQwPSE5zJGrTo=";
   };
 
   # Requires libasan to be accessible
