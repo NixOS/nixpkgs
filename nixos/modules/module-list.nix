c1d74696a9f2cbb396b39644b05e87af38b85760
Mihai Fufezan <fufexan@protonmail.com>
2024-01-23 21:26:45 +0200

    nixos/howdy: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1212,6 +1212,7 @@
   ./services/security/hockeypuck.nix
   ./services/security/hologram-agent.nix
   ./services/security/hologram-server.nix
+  ./services/security/howdy
   ./services/security/infnoise.nix
   ./services/security/intune.nix
   ./services/security/jitterentropy-rngd.nix
