b60c5f00ad54fa71b363c559c5af12e09e40b8d7
Mario Rodas <marsam@users.noreply.github.com>
2024-01-23 04:20:00 +0000

    esbuild: 0.19.11 -> 0.19.12
    Diff: https://github.com/evanw/esbuild/compare/v0.19.11...v0.19.12
    
    Changelog: https://github.com/evanw/esbuild/blob/v0.19.12/CHANGELOG.md

--- pkgs/development/tools/esbuild/default.nix
+++ pkgs/development/tools/esbuild/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "esbuild";
-  version = "0.19.11";
+  version = "0.19.12";
 
   src = fetchFromGitHub {
     owner = "evanw";
     repo = "esbuild";
     rev = "v${version}";
-    hash = "sha256-NUwjzOpHA0Ijuh0E69KXx8YVS5GTnKmob9HepqugbIU=";
+    hash = "sha256-NQ06esCSU6YPvQ4cMsi3DEFGIQGl8Ff6fhdTxUAyGvo=";
   };
 
   vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
