9c565e0e69f468be6f453235fb8f19089930a8f5
Maciej Krüger <mkg20001@gmail.com>
2024-01-23 20:58:21 +0100

    rustdesk-server: use DynamicUser
    this was a suggestion on #272501

--- nixos/modules/services/monitoring/rustdesk-server.nix
+++ nixos/modules/services/monitoring/rustdesk-server.nix
@@ -35,15 +35,14 @@ in {
         Slice = "system-rustdesk.slice";
         User  = "rustdesk";
         Group = "rustdesk";
+        DynamicUser = "yes";
         Environment = [];
         WorkingDirectory = "/var/lib/rustdesk";
         StateDirectory   = "rustdesk";
         StateDirectoryMode = "0750";
         LockPersonality = true;
-        NoNewPrivileges = true;
         PrivateDevices = true;
         PrivateMounts = true;
-        PrivateTmp = true;
         PrivateUsers = true;
         ProtectClock = true;
         ProtectControlGroups = true;
@@ -53,10 +52,7 @@ in {
         ProtectKernelModules = true;
         ProtectKernelTunables = true;
         ProtectProc = "invisible";
-        ProtectSystem = "strict";
-        RemoveIPC = true;
         RestrictNamespaces = true;
-        RestrictSUIDSGID = true;
       };
     };
   in lib.mkIf cfg.enable {
