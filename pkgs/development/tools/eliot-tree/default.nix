390a681eacc25fc1a3248a86c034800c51e23e31
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-24 00:49:28 +0000

    eliot-tree: 19.0.1 -> 21.0.0

--- pkgs/development/tools/eliot-tree/default.nix
+++ pkgs/development/tools/eliot-tree/default.nix
@@ -2,11 +2,11 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "eliot-tree";
-  version = "19.0.1";
+  version = "21.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "18gvijsm0vh3x83mv8dd80c3mpm80r7i111qsg4y7rj4i590phma";
+    sha256 = "sha256-hTl+r+QJPPQ7ss73lty3Wm7DLy2SKGmmgIuJx38ilO8=";
   };
 
   nativeCheckInputs = with python3Packages; [
