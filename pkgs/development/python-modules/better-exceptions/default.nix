84f42ae04790d655faa269bae497940fe97e55d0
P. <d.ol.rod@tutanota.com>
2024-01-24 10:05:25 -0600

    python3Packages.better-exceptions: init at 0.3.3-unstable-2023-01-04

--- pkgs/development/python-modules/better-exceptions/default.nix
+++ pkgs/development/python-modules/better-exceptions/default.nix
@@ -1,24 +1,53 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, setuptools  
+, setuptools
+, pytestCheckHook
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "better-exceptions";
-  version = "0.2.1";
+  version = "0.3.3-unstable-2023-01-04";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Qix-";
     repo = "better-exceptions";
-    rev = version;
-    hash = "";
+    rev = "f7f1476e57129dc74d241b4377b0df39c37bc8a7";
+    hash = "sha256-c8m4VLD+PDayzhdIURXUHVwf4HrfjXYNUjftThGzm9g=";
   };
 
   nativeBuildInputs = [
     setuptools
   ];
+
+  checkInputs = [
+    pytestCheckHook
+  ];
+
+  disabledTestPaths = [
+    # ValueError: Cause error
+    "test/test_chaining.py"
+    # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    "test/test_encoding.py"
+    # IndentationError: unexpected indent
+    "test/test_indentation_error.py"
+    # SyntaxError: invalid syntax
+    "test/test_syntax_error.py"
+    # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    "test/test_truncating.py"
+    # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    "test/test_truncating_disabled.py"
+  ];
+
+  disabledTests = [
+    # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    "test_add"
+  ];
+
+  # Some check files need to be disabled because of various errors, same with some tests.
+  # After disabling and running the build, no tests are collected
+  doCheck = false;
 
   pythonImportsCheck = [
     "better_exceptions"
@@ -27,7 +56,6 @@ buildPythonPackage rec {
   meta = {
     description = "Pretty and useful exceptions in Python, automatically";
     homepage = "https://github.com/Qix-/better-exceptions";
-    changelog = "https://github.com/Qix-/better-exceptions/releases/tag/${version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ wolfangaukang ];
   };
