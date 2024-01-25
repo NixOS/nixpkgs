f5e44d2969fc93134bfb71d0e3dd2fe8ba5310b2
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-25 21:53:36 +0100

    python311Packages.s3transfer: refactor

--- pkgs/development/python-modules/s3transfer/default.nix
+++ pkgs/development/python-modules/s3transfer/default.nix
@@ -1,44 +1,57 @@
 { lib
+, stdenv
 , botocore
 , buildPythonPackage
 , fetchFromGitHub
 , pytestCheckHook
 , pythonOlder
-, stdenv
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "s3transfer";
   version = "0.8.2";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "boto";
-    repo = pname;
-    rev = version;
+    repo = "s3transfer";
+    rev = "refs/tags/${version}";
     hash = "sha256-sdoPjkZHN5wVCK9V6V+fkGvQvEQo2ABy2lqefEKfg6o=";
   };
 
-  propagatedBuildInputs = [ botocore ];
+  nativeBuildInputs = [
+    setuptools
+  ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  propagatedBuildInputs = [
+    botocore
+  ];
+
+  passthru.optional-dependencies = {
+    crt = [
+      botocore
+    ] ++ botocore.optional-dependencies.crt;
+  };
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 
   disabledTestPaths = [
     # Requires network access
     "tests/integration"
   ] ++
-  # There was a change in python 3.8 that defaults multiprocessing to spawn instead of fork on macOS
+  # There was a change in Python 3.8 that defaults multiprocessing to spawn instead of fork on macOS
   # See https://bugs.python.org/issue33725 and https://github.com/python/cpython/pull/13603.
   # I suspect the underlying issue here is that upstream tests aren't compatible with spawn multiprocessing, and pass on linux where the default is still fork
   lib.optionals stdenv.isDarwin [ "tests/unit/test_compat.py" ];
 
-  pythonImportsCheck = [ "s3transfer" ];
-
-  passthru.optional-dependencies = {
-    crt = [ botocore.optional-dependencies.crt ];
-  };
+  pythonImportsCheck = [
+    "s3transfer"
+  ];
 
   meta = with lib; {
     description = "Library for managing Amazon S3 transfers";
