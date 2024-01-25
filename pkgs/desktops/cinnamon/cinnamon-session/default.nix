524e1e399df26ec855ac9062bbb04f879fee740a
Bobby Rong <rjl931189261@126.com>
2024-01-25 21:54:50 +0800

    cinnamon.cinnamon-session: 6.0.1 -> 6.0.2
    https://github.com/linuxmint/cinnamon-session/compare/6.0.1...6.0.2

--- pkgs/desktops/cinnamon/cinnamon-session/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-session/default.nix
@@ -32,13 +32,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "cinnamon-session";
-  version = "6.0.1";
+  version = "6.0.2";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-9wdakMCW0RnsYdf9OmK/Q9o8m0g+5EfHVbjqvFY3d/w=";
+    hash = "sha256-AO4/JUysQyGDsQDbP9X7sqmcxyRSkNGjjTEu4fFzDZA=";
   };
 
   patches = [
