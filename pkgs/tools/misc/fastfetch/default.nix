0c53194c74dd08695240b139a29e64029a724edf
Lin Xianyi <iynaix@gmail.com>
2024-01-26 15:14:14 +0800

    fastfetch: 2.6.3 -> 2.7.0

--- pkgs/tools/misc/fastfetch/default.nix
+++ pkgs/tools/misc/fastfetch/default.nix
@@ -32,13 +32,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "2.6.3";
+  version = "2.7.0";
 
   src = fetchFromGitHub {
     owner = "fastfetch-cli";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-pHDlMeFsC99RuTCSbQT+2LbQ7SACeTWfwP56D/AUb3g=";
+    hash = "sha256-+p85f2cH+syRpnSozc++jPT4Hbn/kGkVsUv3L5Na8+s=";
   };
 
   nativeBuildInputs = [
