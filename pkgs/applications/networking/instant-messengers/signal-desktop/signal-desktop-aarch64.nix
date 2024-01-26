d57b972a43a2828c5676892e79a0813be0521f84
teutat3s <10206665+teutat3s@users.noreply.github.com>
2024-01-26 17:59:41 +0100

    signal-desktop (aarch64): 6.42.0 -> 6.44.0
    (cherry picked from commit 21924e31bbaf8ba68b3437aa61b2ac2ae88f869e)

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-aarch64.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-aarch64.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "6.42.0";
+  version = "6.44.0";
   url = "https://github.com/0mniteck/Signal-Desktop-Mobian/raw/${version}/builds/release/signal-desktop_${version}_arm64.deb";
-  hash = "sha256-kr8FM+EAtL/7TrgJlI33oDd4CPGMJ+F2PpQCR4OL6j4=";
+  hash = "sha256-M4Xiy8cDQciMzgGl1/eeKZjEaelVtkk6JXJYBP4ua2s=";
 }
