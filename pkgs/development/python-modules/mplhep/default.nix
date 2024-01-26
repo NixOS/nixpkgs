a7a7fef7455c8a5405433824772ec8e2d8313d6b
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-26 10:24:29 +0000

    python311Packages.mplhep: 0.3.31 -> 0.3.32

--- pkgs/development/python-modules/mplhep/default.nix
+++ pkgs/development/python-modules/mplhep/default.nix
@@ -16,12 +16,12 @@
 
 buildPythonPackage rec {
   pname = "mplhep";
-  version = "0.3.31";
+  version = "0.3.32";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-aZwazbDljRncB2p7qD95Cms0uQBU8tciUrXcKpsyVTM=";
+    hash = "sha256-xwlrMZLJKRW6iqMsDKS5BDqejADSk/qGOAnQDZ2fTPc=";
   };
 
   nativeBuildInputs = [
