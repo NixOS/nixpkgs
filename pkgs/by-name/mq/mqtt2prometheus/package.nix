83fec9e25fcc064aaec366fbacc2cf67dea869f9
Thomas Gerbet <thomas@gerbet.me>
2024-01-27 12:12:39 +0100

    mqtt2prometheus: init at 0.1.7
    https://github.com/hikhvar/mqtt2prometheus

---
+++ pkgs/by-name/mq/mqtt2prometheus/package.nix
@@ -0,0 +1,42 @@
+{ fetchFromGitHub
+, buildGoModule
+, lib
+, testers
+, mqtt2prometheus
+}:
+buildGoModule rec {
+  pname = "mqtt2prometheus";
+  version = "0.1.7";
+
+  src = fetchFromGitHub {
+    owner = "hikhvar";
+    repo = "mqtt2prometheus";
+    rev = "v${version}";
+    hash = "sha256-D5AO6Qsz44ssmRu80PDiRjKSxkOUe4OSm+xtvyGkdUQ=";
+  };
+
+  vendorHash = "sha256-5P5J1HwlOFMaGj77k4jU8uJtm0XUIqdPT9abRcvHt2s=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
+  ];
+
+  postInstall = ''
+    mv $out/bin/cmd $out/bin/mqtt2prometheus
+  '';
+
+  passthru = {
+    tests.version = testers.testVersion { package = mqtt2prometheus; };
+  };
+
+  meta = {
+    description = "MQTT to Prometheus gateway";
+    homepage = "https://github.com/hikhvar/mqtt2prometheus";
+    changelog = "https://github.com/hikhvar/mqtt2prometheus/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    mainProgram = "mqtt2prometheus";
+    maintainers = with lib.maintainers; [ lesuisse ];
+  };
+}
