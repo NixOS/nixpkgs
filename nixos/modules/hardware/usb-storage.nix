ea2e18416d11a03c7c80608e09889f28557d4cd8
bb2020 <bb2020@users.noreply.github.com>
2024-01-27 12:25:21 +0300

    nixos/usbStorage: upstream changes

--- nixos/modules/hardware/usb-storage.nix
+++ nixos/modules/hardware/usb-storage.nix
@@ -8,13 +8,14 @@ with lib;
     description = lib.mdDoc ''
       Enable this option to gracefully spin-down external storage during shutdown.
       If you suspect improper head parking after poweroff, install `smartmontools` and check
-      for the `Power-Off_Retract_Count` field for an increment.
+      for the `Power-Off_Retract_Count` field for an increment. You can also see:
+      https://bugzilla.kernel.org/show_bug.cgi?id=205107
     '';
   };
 
   config = mkIf config.hardware.usbStorage.manageStartStop {
     services.udev.extraRules = ''
-      ACTION=="add|change", SUBSYSTEM=="scsi_disk", DRIVERS=="usb-storage", ATTR{manage_system_start_stop}="1"
+      ACTION=="add|change", SUBSYSTEM=="scsi_disk", DRIVERS=="usb-storage|uas", ATTR{manage_shutdown}="1"
     '';
   };
 }
