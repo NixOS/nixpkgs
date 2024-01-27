6c1f8181b823359fea74a4a06bee13c8bd41845d
Sergei Trofimovich <slyich@gmail.com>
2024-01-27 15:43:50 +0000

    libidn2: 2.3.4 -> 2.3.7
    Changes: https://lists.gnu.org/archive/html/help-libidn/2024-01/msg00005.html

--- pkgs/development/libraries/libidn2/default.nix
+++ pkgs/development/libraries/libidn2/default.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libidn2";
-  version = "2.3.4";
+  version = "2.3.7";
 
   src = fetchurl {
     url = "https://ftp.gnu.org/gnu/libidn/${pname}-${version}.tar.gz";
-    sha256 = "sha256-k8q6crTgUdH41PWgdqtjyZt3+u4Bm3K5eDsmeYbbtF8=";
+    hash = "sha256-TCGnkbYQuVGbnQ4SuAl78vNZsS+N2SZHYRqSnmv9fWQ=";
   };
 
   strictDeps = true;
