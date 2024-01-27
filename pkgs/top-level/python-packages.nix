f1791e44bb1f865a4977d2226e1beda6d170fac0
Edmund Miller <git@edmundmiller.dev>
2024-01-27 16:54:51 -0600

    python31{1,2}Packages.spectra: init at 0.0.11

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14626,6 +14626,8 @@ self: super: with self; {
 
   speaklater3 = callPackage ../development/python-modules/speaklater3 { };
 
+  spectra = callPackage ../development/python-modules/spectra { };
+
   spectral-cube = callPackage ../development/python-modules/spectral-cube { };
 
   speechbrain = callPackage ../development/python-modules/speechbrain { };
