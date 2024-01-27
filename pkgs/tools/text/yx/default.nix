15a135a391d68ca9c397af0112c36291c9d4d90d
Tom Wieczorek <tom@bibbu.net>
2024-01-27 22:50:08 +0100

    yx: 1.0.0 -> 1.0.1
    * Fix wrong version number
    * Fix man page installation

--- pkgs/tools/text/yx/default.nix
+++ pkgs/tools/text/yx/default.nix
@@ -1,19 +1,21 @@
 { lib
 , stdenv
 , fetchFromGitLab
+, fetchpatch
 , libyaml
 , testers
+, installShellFiles
 , yx
 }:
 stdenv.mkDerivation rec {
   pname = "yx";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitLab {
     owner = "tomalok";
     repo = "yx";
     rev = version;
-    sha256 = "sha256-oY61V9xP0DwRooabzi0XtaFsQa2GwYbuvxfERXQtYcA=";
+    sha256 = "sha256-SGbvmwm2xxRTULzmfloOnMqoSO3TghWDlMMFaEE5v7E=";
   };
 
   makeFlags = [
@@ -22,9 +24,25 @@ stdenv.mkDerivation rec {
 
   strictDeps = true;
 
+  nativeBuildInputs = [ installShellFiles ];
+
   buildInputs = [ libyaml ];
 
+  patches = [
+    # https://gitlab.com/tomalok/yx/-/merge_requests/8
+    (fetchpatch {
+      url = "https://gitlab.com/tomalok/yx/-/commit/6ed4358f6930f4f5e44480c0ec7d3d7ea085d372.diff";
+      hash = "sha256-4OeYv1l+YkDTc1ZI+gH7I9wGlnXn5vGFqgpPwBz1HyI=";
+    })
+  ];
+
   doCheck = true;
+
+  postInstall = ''
+    installManPage $out/usr/share/man/man1/yx.1
+    rm $out/usr/share/man/man1/yx.1
+    find $out/usr -type d -empty -delete
+  '';
 
   passthru.tests.version = testers.testVersion {
     package = yx;
