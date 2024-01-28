86ce0733d2cabad56958ffb68b4e1006d4b71248
ocfox <i@ocfox.me>
2024-01-28 12:55:05 +0800

    nixos/realm: init
    Co-authored-by: Nick Cao <nickcao@nichi.co>
    Co-authored-by: oluceps <i@oluceps.uk>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1150,6 +1150,7 @@
   ./services/networking/radicale.nix
   ./services/networking/radvd.nix
   ./services/networking/rdnssd.nix
+  ./services/networking/realm.nix
   ./services/networking/redsocks.nix
   ./services/networking/resilio.nix
   ./services/networking/robustirc-bridge.nix
