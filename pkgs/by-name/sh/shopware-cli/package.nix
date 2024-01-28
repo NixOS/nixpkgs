fb3251ad1a1eb42a5467175143a8eb4408478ff3
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-28 07:44:23 +0000

    shopware-cli: 0.4.13 -> 0.4.18

--- pkgs/by-name/sh/shopware-cli/package.nix
+++ pkgs/by-name/sh/shopware-cli/package.nix
@@ -9,18 +9,18 @@
 
 buildGoModule rec {
   pname = "shopware-cli";
-  version = "0.4.13";
+  version = "0.4.18";
   src = fetchFromGitHub {
     repo = "shopware-cli";
     owner = "FriendsOfShopware";
     rev = version;
-    hash = "sha256-EetMOCqyzpNzxzCaaFHnSQEkjf9lZyYBWdRKMuxObNM=";
+    hash = "sha256-LOmGxH/czICSii8AkoXi1cQPL+MErV92iUZtJc2eg64=";
   };
 
   nativeBuildInputs = [ installShellFiles makeWrapper ];
   nativeCheckInputs = [ git dart-sass ];
 
-  vendorHash = "sha256-TRc3VK1Sgpmi/ToMaqkIMIrxHoyYHNUgrtQewTuQ6ts=";
+  vendorHash = "sha256-KMNPw2B4fLaOdSIFHBIAKXUtnu0sMwksJg3RUZKLDsE=";
 
   postInstall = ''
     export HOME="$(mktemp -d)"
