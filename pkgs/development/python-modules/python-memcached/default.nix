08d7dab2bed188732ac7275661290a48d72e3f95
Robert Schütz <nix@dotlambda.de>
2024-01-27 18:18:50 -0800

    python311Packages.python-memcached: 1.61 -> 1.62
    Diff: https://github.com/linsomniac/python-memcached/compare/1.61...1.62
    
    Changelog: https://github.com/linsomniac/python-memcached/releases/tag/1.62

--- pkgs/development/python-modules/python-memcached/default.nix
+++ pkgs/development/python-modules/python-memcached/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "python-memcached";
-  version = "1.61";
+  version = "1.62";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "linsomniac";
     repo = "python-memcached";
     rev = version;
-    hash = "sha256-7bUCVAmOJ6znVmTZg9AJokOuym07NHL12gZgQ2uhfNo=";
+    hash = "sha256-Qko4Qr9WofeklU0uRRrSPrT8YaBYMCy0GP+TF7YZHLI=";
   };
 
   nativeBuildInputs = [
