595407fe800c43ac3f2d537a966bd8d789a4e56f
Artemis Tosini <me@artem.ist>
2024-01-28 09:38:57 +0000

    freebsd.mount: use PATH variable

--- pkgs/os-specific/bsd/freebsd/mount.nix
+++ pkgs/os-specific/bsd/freebsd/mount.nix
@@ -1,7 +1,9 @@
-{ lib, stdenv, mkDerivation, libutil, libxo, ... }:
+{ lib, stdenv, mkDerivation, libutil, libxo, patchesRoot, ... }:
 mkDerivation {
   path = "sbin/mount";
-  buildInputs = [libutil libxo];
+  buildInputs = [ libutil libxo ];
+
+  patches = [ /${patchesRoot}/mount-use-path.patch ];
 
   preBuild = lib.optionalString stdenv.cc.isClang ''
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -D_VA_LIST -D_VA_LIST_DECLARED -Dva_list=__builtin_va_list -D_SIZE_T -D_WCHAR_T"
