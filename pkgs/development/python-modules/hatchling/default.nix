7f1305d3d2666813654c14e0b7bb97d65d5602e0
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-01-28 14:33:46 +0100

    python311Packages.hatchling: 1.21.0 -> 1.21.1
    https://github.com/pypa/hatch/releases/tag/hatchling-v1.21.1

--- pkgs/development/python-modules/hatchling/default.nix
+++ pkgs/development/python-modules/hatchling/default.nix
@@ -20,13 +20,13 @@
 
 buildPythonPackage rec {
   pname = "hatchling";
-  version = "1.21.0";
+  version = "1.21.1";
   format = "pyproject";
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-XAhncjV6UHI7gl/V2lJ4rH42l833eX0HVBpskLb/dUw=";
+    hash = "sha256-u6RARToiTn1EeEV/oujYw2M3Zbr6Apdaa1O5v5F5gLw=";
   };
 
   # listed in backend/pyproject.toml
