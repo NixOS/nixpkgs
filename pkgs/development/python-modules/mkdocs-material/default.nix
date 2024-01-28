f38a46f78c92550a30a526e93623ebf3592c8445
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-28 07:10:05 +0000

    python311Packages.mkdocs-material: 9.4.14 -> 9.5.5

--- pkgs/development/python-modules/mkdocs-material/default.nix
+++ pkgs/development/python-modules/mkdocs-material/default.nix
@@ -32,7 +32,7 @@
 
 buildPythonPackage rec {
   pname = "mkdocs-material";
-  version = "9.4.14";
+  version = "9.5.5";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -41,7 +41,7 @@ buildPythonPackage rec {
     owner = "squidfunk";
     repo = "mkdocs-material";
     rev = "refs/tags/${version}";
-    hash = "sha256-oP0DeSRgoLx6boEOa3if5BitGXmJ11DoUVZK16Sjlwg=";
+    hash = "sha256-ou3mBkfid8egtetVoosh/vmCc1cpkRyF3fbyZI6SAWo=";
   };
 
   nativeBuildInputs = [
