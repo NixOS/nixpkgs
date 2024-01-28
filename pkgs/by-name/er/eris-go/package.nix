d07ba447895a9469bd4fc5f289163325fd87249a
Emery Hemingway <ehmry@posteo.net>
2024-01-28 19:36:34 +0100

    eris-go: 20231219 -> 20240128

--- pkgs/by-name/er/eris-go/package.nix
+++ pkgs/by-name/er/eris-go/package.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "eris-go";
-  version = "20231219";
+  version = "20240128";
   outputs = [ "out" "man" ];
 
   src = fetchFromGitea {
@@ -10,7 +10,7 @@ buildGoModule rec {
     owner = "eris";
     repo = "eris-go";
     rev = version;
-    hash = "sha256-eXLfBkJgG51ZjR1qXRE2BgTrIpQsPW5SKeMlGd3J1NE=";
+    hash = "sha256-mS5PMrp6rBR8ehlpDAZaTQL8vhFSpcztMaQF5zjozxc=";
   };
 
   vendorHash = "sha256-pA/fz7JpDwdTRFfLDY0M6p9TeBOK68byhy/0Cw53p4M=";
@@ -23,10 +23,10 @@ buildGoModule rec {
   '';
   postBuild = "tupBuild";
   postInstall = ''
-    install -D *.1.man -t $man/share/man/man1
+    install -D *.1.gz -t $man/share/man/man1
   '';
 
-  skipNetworkTests = true;
+  env.skipNetworkTests = true;
 
   passthru.tests = { inherit (nixosTests) eris-server; };
 
