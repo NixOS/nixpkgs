ad828be1ca61bce3ee2c3cd453983280850387b8
Krzysztof Nazarewski <gpg@kdn.im>
2024-01-29 09:58:15 +0100

    netclient: 0.22.0 -> unstable-2024-01-26
    Uses the fork because the SSO feature wouldn't work without it.
    Can be switched to upstream after following PR lands in a release:
     https://github.com/gravitl/netclient/pull/682

--- pkgs/by-name/ne/netclient/package.nix
+++ pkgs/by-name/ne/netclient/package.nix
@@ -1,34 +1,61 @@
-{ buildGoModule
+{ lib
+, buildGoModule
 , fetchFromGitHub
-, lib
-, libX11
+, installShellFiles
+, makeWrapper
+, nix-update-script
 , stdenv
-, darwin
+, systemd
 }:
-
 buildGoModule rec {
   pname = "netclient";
-  version = "0.22.0";
+  version = "unstable-2024-01-26";
 
   src = fetchFromGitHub {
-    owner = "gravitl";
+    # TODO: switch to upstream after https://github.com/gravitl/netclient/pull/682
+    #owner = "gravitl";
+    owner = "nazarewk";
     repo = "netclient";
-    rev = "v${version}";
-    hash = "sha256-7raWk4Y/ZrSaGKPLrrnD49aDALkZ+Nxycd+px8Eks10=";
+    rev = "7262296d65f1e129f1475782c453bcf10f5f60d7";
+    hash = "sha256-veh+k2WxfOqVFrUgyDOTuxI7Q87m8EH6LhrF0ZQr/tA=";
   };
+  vendorHash = "sha256-w7D79uUnfKg3rK9s9XSHWCtA/RbN9X9jc1eaOzCvVJk=";
 
-  vendorHash = "sha256-lRXZ9iSWQEKWmeQV1ei/G4+HvqhW9U8yUv1Qb/d2jvY=";
-
-  buildInputs = lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Cocoa
-    ++ lib.optional stdenv.isLinux libX11;
-
+  subPackages = [
+    "."
+    /* TODO: to package "gui":
+        1. create a separate package because it pulls in graphical dependencies unnecessary on servers
+           old package is a good starting point https://github.com/NixOS/nixpkgs/blob/1c4593f3519b83840087729589abf6a45b6803f6/pkgs/by-name/ne/netclient/package.nix
+        2. be sure to rename `gui` binary to `netclient-gui` or something else more appropriate
+    */
+    # "gui"
+  ];
+  CGO_ENABLED = false;
   hardeningEnabled = [ "pie" ];
 
-  meta = with lib; {
+  nativeBuildInputs = [
+    makeWrapper
+    installShellFiles
+  ];
+
+  postInstall = ''
+    wrapProgram "$out/bin/netclient" \
+      --set NETCLIENT_AUTO_UPDATE disabled \
+      --set NETCLIENT_INIT_TYPE '${lib.optionalString (lib.meta.availableOn stdenv.hostPlatform systemd) "systemd"}'
+
+    installShellCompletion --cmd netclient \
+      --bash <($out/bin/netclient completion bash) \
+      --fish <($out/bin/netclient completion fish) \
+      --zsh <($out/bin/netclient completion zsh)
+  '';
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch=fix-sso" ]; };
+  meta = {
     description = "Automated WireGuard® Management Client";
     homepage = "https://netmaker.io";
     changelog = "https://github.com/gravitl/netclient/releases/tag/v${version}";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ wexder ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ wexder nazarewk ];
+    mainProgram = "netclient";
   };
 }
