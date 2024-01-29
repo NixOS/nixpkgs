37dc580a1a3a533022782bb536fdee136dbb362d
Anderson Torres <torres.anderson.85@protonmail.com>
2024-01-29 08:21:13 -0300

    j: use finalAttrs design pattern

--- pkgs/by-name/j/j/package.nix
+++ pkgs/by-name/j/j/package.nix
@@ -6,14 +6,14 @@
 , avx2Support ? stdenv.hostPlatform.avx2Support
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "j";
   version = "9.5.1";
 
   src = fetchFromGitHub {
     owner = "jsoftware";
     repo = "jsource";
-    rev = "${version}";
+    rev = "${finalAttrs.version}";
     hash = "sha256-QRQhE8138+zaGQOdq9xUOrifkVIprzbJWbmMK+WhEOU=";
   };
 
@@ -33,27 +33,27 @@ stdenv.mkDerivation rec {
 
   dontConfigure = true;
 
-  # Emulate jplatform64.sh configuration variables
-  jplatform =
-    if stdenv.isDarwin then "darwin"
-    else if stdenv.hostPlatform.isAarch then "raspberry"
-    else if stdenv.isLinux then "linux"
-    else "unsupported";
-
-  j64x =
-    if stdenv.is32bit then "j32"
-    else if stdenv.isx86_64 then
-      if stdenv.isLinux && avx2Support then "j64avx2" else "j64"
-    else if stdenv.isAarch64 then
-      if stdenv.isDarwin then "j64arm" else "j64"
-    else "unsupported";
-
   env.NIX_LDFLAGS = "-lgmp";
 
-  buildPhase = ''
+  # Emulate jplatform64.sh
+  buildPhase = let
+    jplatform =
+      if stdenv.isDarwin then "darwin"
+      else if stdenv.hostPlatform.isAarch then "raspberry"
+      else if stdenv.isLinux then "linux"
+      else "unsupported";
+
+    j64x =
+      if stdenv.is32bit then "j32"
+      else if stdenv.isx86_64 then
+        if stdenv.isLinux && avx2Support then "j64avx2" else "j64"
+      else if stdenv.isAarch64 then
+        if stdenv.isDarwin then "j64arm" else "j64"
+      else "unsupported";
+  in ''
     runHook preBuild
     MAKEFLAGS+=" ''${enableParallelBuilding:+-j$NIX_BUILD_CORES}" \
-      jplatform=${jplatform} j64x=${j64x} make2/build_all.sh
+      CC=${stdenv.cc.targetPrefix}cc jplatform=${jplatform} j64x=${j64x} make2/build_all.sh
     cp -v bin/${jplatform}/${j64x}/* jlibrary/bin/
     runHook postBuild
   '';
@@ -80,4 +80,4 @@ stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ raskin synthetica AndersonTorres ];
     platforms = lib.platforms.unix;
   };
-}
+})
