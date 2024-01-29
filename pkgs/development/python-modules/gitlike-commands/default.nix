7371b0a3bb54cb056b549834e243043593fe4a35
Fabian Affolter <mail@fabian-affolter.ch>
2024-01-29 13:47:41 +0100

    python311Packages.gitlike-commands: remove patch

--- pkgs/development/python-modules/gitlike-commands/default.nix
+++ pkgs/development/python-modules/gitlike-commands/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
-, fetchpatch
 , poetry-core
 , pythonOlder
 }:
@@ -19,15 +18,6 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-Z0l8nCKov1iMJvI3YTHvg0ey+oPju3rgaKtmk6OX44g=";
   };
-
-  patches = [
-    # Replace distutils, https://github.com/unixorn/gitlike-commands/pull/8
-    (fetchpatch {
-      name = "replace-distutils.patch";
-      url = "https://github.com/unixorn/gitlike-commands/commit/8a97206aff50a25ac6860032aa03925899c3d0aa.patch";
-      hash = "sha256-a2utKbf9vrQlYlPcdj/+OAqWf7VkuC5kvbJ53SK52IA=";
-    })
-  ];
 
   nativeBuildInputs = [
     poetry-core
