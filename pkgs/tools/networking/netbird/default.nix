ac1e28e8c1995d58c8c80902665b5a5749a37541
Tom Hubrecht <tom@hubrecht.ovh>
2024-01-29 17:38:04 +0100

    netbird: Add meta.mainProgram

--- pkgs/tools/networking/netbird/default.nix
+++ pkgs/tools/networking/netbird/default.nix
@@ -111,5 +111,6 @@ buildGoModule rec {
     description = "Connect your devices into a single secure private WireGuard®-based mesh network with SSO/MFA and simple access controls";
     license = licenses.bsd3;
     maintainers = with maintainers; [ misuzu ];
+    mainProgram = "netbird";
   };
 }
