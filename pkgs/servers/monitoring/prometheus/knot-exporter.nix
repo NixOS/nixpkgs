08883e1129534e4e3f21fd86286417a58ae07314
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-29 02:08:35 +0000

    prometheus-knot-exporter: 3.3.3 -> 3.3.4
    (cherry picked from commit 6e1cbcd3c228eae9b7d4c94e425b685159295557)

--- pkgs/servers/monitoring/prometheus/knot-exporter.nix
+++ pkgs/servers/monitoring/prometheus/knot-exporter.nix
@@ -6,13 +6,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "knot-exporter";
-  version = "3.3.3";
+  version = "3.3.4";
   pyproject = true;
 
   src = fetchPypi {
     pname = "knot_exporter";
     inherit version;
-    hash = "sha256-A7WRwWwcfgrkR5ixEaDCcCdJ1J8/zeG5aFCBnFbD3Qo=";
+    hash = "sha256-jZNNJiJxq3pNSZFwu2UEHhrA0odIDY0UO+d4PJII1ZI=";
   };
 
   nativeBuildInputs = [
