30bc84ac2ee7a40b75dbb292ba8464ab2ed9cca4
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-01-30 22:16:34 +0100

    home-assistant-custom-lovelace-modules.mushroom: 3.2.4 -> 3.3.0
    https://github.com/piitaya/lovelace-mushroom/releases/tag/v3.3.0

--- pkgs/servers/home-assistant/custom-lovelace-modules/mushroom/default.nix
+++ pkgs/servers/home-assistant/custom-lovelace-modules/mushroom/default.nix
@@ -5,16 +5,16 @@
 
 buildNpmPackage rec {
   pname = "mushroom";
-  version = "3.2.4";
+  version = "3.3.0";
 
   src = fetchFromGitHub {
     owner = "piitaya";
     repo = "lovelace-mushroom";
     rev = "v${version}";
-    hash = "sha256-UV/kqeVslB1lc1a9uvj8ocQKeAVINwo3aH8oHC9GLBQ=";
+    hash = "sha256-RL1+KPeKrooBi1AIYz7K7aS9dIc1DlUafyb3DgguhLU=";
   };
 
-  npmDepsHash = "sha256-p43ks6HM23LK3LOBOZA0uV+DTOj89/boBpjcXpbcw24=";
+  npmDepsHash = "sha256-DaseqpE2Kn/LUD9kC5kmnpTYm46FpoRccM+ovWdZn/Y=";
 
   installPhase = ''
     runHook preInstall
