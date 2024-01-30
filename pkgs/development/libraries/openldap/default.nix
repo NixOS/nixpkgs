efc7c1f5e8e56d75e2edd1ba67194ac14ca4799c
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-30 06:44:43 +0000

    openldap: 2.6.6 -> 2.6.7

--- pkgs/development/libraries/openldap/default.nix
+++ pkgs/development/libraries/openldap/default.nix
@@ -17,11 +17,11 @@
 
 stdenv.mkDerivation rec {
   pname = "openldap";
-  version = "2.6.6";
+  version = "2.6.7";
 
   src = fetchurl {
     url = "https://www.openldap.org/software/download/OpenLDAP/openldap-release/${pname}-${version}.tgz";
-    hash = "sha256-CC6ZjPVCmE1DY0RC2+EdqGB1nlEJBxUupXm9xC/jnqA=";
+    hash = "sha256-zXdfYlyUTteKPaGKA7A7CO6nPIqryXtBuzNumhCVSTA=";
   };
 
   # TODO: separate "out" and "bin"
