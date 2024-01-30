65ed0944fc11550f83990a63006146b4d02bd54c
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-30 04:28:32 +0000

    python312Packages.types-protobuf: 4.24.0.20240106 -> 4.24.0.20240129

--- pkgs/development/python-modules/types-protobuf/default.nix
+++ pkgs/development/python-modules/types-protobuf/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "types-protobuf";
-  version = "4.24.0.20240106";
+  version = "4.24.0.20240129";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Ak8DTzteK7K7/1XrxNWR7Q0igNkPrO7csUi55xSj8+4=";
+    hash = "sha256-ioPdO5t2oz4I2GNsXaohKs4TlkGO2Rg3Y1/NVkpiSJE=";
   };
 
   propagatedBuildInputs = [
