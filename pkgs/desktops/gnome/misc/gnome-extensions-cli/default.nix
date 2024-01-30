78afe0b7ef9e1828199920a9877c3b9baf6851aa
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2024-01-30 18:23:16 +0000

    gnome-extensions-cli: 0.9.5 -> 0.10.1
    The `gnome-extensions-cli` uses other packaged Python dependencies
    and failing to build because the packaged versions of `more-itertools`
    and `pydantic` are newer than what the project's dependency constraints
    allow for.
    
    Upgrading the package to the newest version fixes the build.
    
    https://github.com/essembeh/gnome-extensions-cli/compare/0.9.5...0.10.1

--- pkgs/desktops/gnome/misc/gnome-extensions-cli/default.nix
+++ pkgs/desktops/gnome/misc/gnome-extensions-cli/default.nix
@@ -3,24 +3,24 @@
 , buildPythonApplication
 , poetry-core
 , colorama
-, more-itertools
 , packaging
 , pydantic
 , requests
 , pygobject3
+, tqdm
 , gobject-introspection
 , wrapGAppsNoGuiHook
 }:
 
 buildPythonApplication rec {
   pname = "gnome-extensions-cli";
-  version = "0.9.5";
+  version = "0.10.1";
   format = "pyproject";
 
   src = fetchPypi {
     pname = "gnome_extensions_cli";
     inherit version;
-    hash = "sha256-4eRVmG5lqK8ql9WpvXsf18znOt7kDSnpQnLfy73doy4=";
+    hash = "sha256-yAoo3NjNtTZSHmbLKzW2X7Cy2smLNp8/9vo+OPGxlVY=";
   };
 
   nativeBuildInputs = [
@@ -31,11 +31,11 @@ buildPythonApplication rec {
 
   propagatedBuildInputs = [
     colorama
-    more-itertools
     packaging
     pydantic
     requests
     pygobject3
+    tqdm
   ];
 
   pythonImportsCheck = [
