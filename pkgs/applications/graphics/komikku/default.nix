2320b8d0eef4db968b865c6f0c7106761498eef4
R. Ryantm <ryantm-bot@ryantm.com>
2024-01-31 19:05:09 +0000

    komikku: 1.36.0 -> 1.37.0

--- pkgs/applications/graphics/komikku/default.nix
+++ pkgs/applications/graphics/komikku/default.nix
@@ -19,7 +19,7 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "komikku";
-  version = "1.36.0";
+  version = "1.37.0";
 
   format = "other";
 
@@ -28,7 +28,7 @@ python3.pkgs.buildPythonApplication rec {
     owner = "valos";
     repo = "Komikku";
     rev = "v${version}";
-    hash = "sha256-sX/4Sez6gKSgaUJC7iW2BeVp0y9h64YqQi7FL5fzZ4o=";
+    hash = "sha256-TQT9AE8KQRPnEFfJOwc1nLWMdMiP+tF/qEGqvsGaX4Y=";
   };
 
   nativeBuildInputs = [
