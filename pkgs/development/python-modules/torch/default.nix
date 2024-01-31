6bcf3b890f49bb3b6c0ad1aeb2a8c4118c9a4bbc
Gaetan Lepage <gaetan@glepage.com>
2024-01-31 08:37:40 +0100

    python311Packages.torch: 2.1.2 -> 2.2.0
    Changelog: https://github.com/pytorch/pytorch/releases/tag/v2.2.0
    
    Co-authored-by: Daniel Fahey <daniel.fahey@pm.me>

--- pkgs/development/python-modules/torch/default.nix
+++ pkgs/development/python-modules/torch/default.nix
@@ -21,6 +21,7 @@
   Accelerate, CoreServices, libobjc,
 
   # Propagated build inputs
+  astunparse,
   fsspec,
   filelock,
   jinja2,
@@ -126,8 +127,8 @@ let
 in buildPythonPackage rec {
   pname = "torch";
   # Don't forget to update torch-bin to the same version.
-  version = "2.1.2";
-  format = "setuptools";
+  version = "2.2.0";
+  pyproject = true;
 
   disabled = pythonOlder "3.8.0";
 
@@ -144,7 +145,7 @@ in buildPythonPackage rec {
     repo = "pytorch";
     rev = "refs/tags/v${version}";
     fetchSubmodules = true;
-    hash = "sha256-E/GQCRWBf3hYsDCCk0twaL9gkVOCEQeCvO3Va+jgIdE=";
+    hash = "sha256-FfFjgx6yrPEQlF8CLglsWq+zWGr6MD4z0F+lYoy6grc=";
   };
 
   patches = lib.optionals cudaSupport [
@@ -246,6 +247,9 @@ in buildPythonPackage rec {
   # Avoid using pybind11 from git submodule
   # Also avoids pytorch exporting the headers of pybind11
   USE_SYSTEM_PYBIND11 = true;
+
+  # NB technical debt: building without NNPACK as workaround for missing `six`
+  USE_NNPACK = 0;
 
   preBuild = ''
     export MAX_JOBS=$NIX_BUILD_CORES
@@ -383,6 +387,7 @@ in buildPythonPackage rec {
     ++ lib.optionals rocmSupport [ rocmtoolkit_joined ];
 
   propagatedBuildInputs = [
+    astunparse
     cffi
     click
     numpy
