NixOS/nixpkgs

commit 9a33f8ce5ba7329600b296996d944ec9ddaa7d38

cudaPackagesGoogle.cudnn_8_6: fix eval
Now:
❯ nix eval -f . --arg config '{ allowUnfree = true; }' cudaPackagesGoogle.cudnn_8_6.outPath
"/nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-cudnn-8.6.0.163"

Previously:
meta.broken = true

pkgs/development/cuda-modules/generic-builders/multiplex.nix
@@ -81,7 +81,14 @@ let
 
   # All the supported packages we can build for our platform.
   # perSystemReleases :: List Package
-  perSystemReleases = releaseSets.${redistArch} or [ ];
+  perSystemReleases = lib.pipe (releaseSets.${redistArch} or [ ])
+    [
+      (builtins.groupBy (p: lib.versions.majorMinor p.version))
+      (builtins.mapAttrs (_: builtins.sort preferable))
+      (builtins.mapAttrs (_: lib.take 1))
+      (builtins.attrValues)
+      (builtins.concatMap lib.trivial.id)
+    ];
 
   preferable =
     p1: p2: (isSupported p2 -> isSupported p1) && (strings.versionAtLeast p1.version p2.version);

commit 83d131fd30a873123880b493b61b23c149f40e31

cudaPackage: don't rely on non-portable builtins
builtins.groupBy was added in Nix 2.5 (https://nixos.org/manual/nix/stable/release-notes/rl-2.5),
but Nixpkgs has to support Nix >= 2.3.

Problem introduced in 9a33f8ce5ba7329600b296996d944ec9ddaa7d38.

pkgs/development/cuda-modules/generic-builders/multiplex.nix
@@ -83,13 +83,13 @@ let
   # perSystemReleases :: List Package
   allReleases = lib.pipe releaseSets
     [
-      (builtins.attrValues)
+      (lib.attrValues)
       (lists.flatten)
-      (builtins.groupBy (p: lib.versions.majorMinor p.version))
-      (builtins.mapAttrs (_: builtins.sort preferable))
-      (builtins.mapAttrs (_: lib.take 1))
-      (builtins.attrValues)
-      (builtins.concatMap lib.trivial.id)
+      (lib.groupBy (p: lib.versions.majorMinor p.version))
+      (lib.mapAttrs (_: builtins.sort preferable))
+      (lib.mapAttrs (_: lib.take 1))
+      (lib.attrValues)
+      (lib.concatMap lib.trivial.id)
     ];
 
   newest = builtins.head (builtins.sort preferable allReleases);