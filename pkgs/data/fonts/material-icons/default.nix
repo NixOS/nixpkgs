955bc676eb3557429ab4c4e67091b439d012cd4c
Guanran Wang <guanran928@outlook.com>
2024-02-01 14:22:23 +0800

    material-icons: 3.0.1 -> 4.0.0

--- pkgs/data/fonts/material-icons/default.nix
+++ pkgs/data/fonts/material-icons/default.nix
@@ -2,20 +2,23 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "material-icons";
-  version = "3.0.1";
+  version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "material-design-icons";
     rev = version;
-    hash = "sha256-4FphNJCsaLWzlVR4TmXnDBid0EVj39fkeoh5j1leBZ8=";
+    hash = "sha256-wX7UejIYUxXOnrH2WZYku9ljv4ZAlvgk8EEJJHOCCjE=";
   };
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/share/fonts/truetype
-    cp iconfont/*.ttf $out/share/fonts/truetype
+    cp font/*.ttf $out/share/fonts/truetype
+
+    mkdir -p $out/share/fonts/opentype
+    cp font/*.otf $out/share/fonts/opentype
 
     runHook postInstall
   '';
