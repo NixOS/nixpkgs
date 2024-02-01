fe23bb90abf25e4baef370fa07792eb18e0bcd06
RatCornu <balthazar.patiachvili@crans.org>
2024-02-01 15:25:05 +0100

    nixos/pingvin-share: init at 0.29.0

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1467,6 +1467,7 @@
   ./services/web-apps/phylactery.nix
   ./services/web-apps/photoprism.nix
   ./services/web-apps/pict-rs.nix
+  ./services/web-apps/pingvin-share.nix
   ./services/web-apps/plantuml-server.nix
   ./services/web-apps/plausible.nix
   ./services/web-apps/powerdns-admin.nix
