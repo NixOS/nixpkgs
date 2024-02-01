5efd33ebde78604aed9101304aab6aa38c39530a
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-01 10:56:22 +0100

    python311Packages.dvc: 3.42.0 -> 3.43.0
    Diff: https://github.com/iterative/dvc/compare/refs/tags/3.42.0...3.43.0
    
    Changelog: https://github.com/iterative/dvc/releases/tag/3.43.0

--- pkgs/development/python-modules/dvc/default.nix
+++ pkgs/development/python-modules/dvc/default.nix
@@ -58,7 +58,7 @@
 
 buildPythonPackage rec {
   pname = "dvc";
-  version = "3.42.0";
+  version = "3.43.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -67,7 +67,7 @@ buildPythonPackage rec {
     owner = "iterative";
     repo = "dvc";
     rev = "refs/tags/${version}";
-    hash = "sha256-NTviaNhGe3hisP32Ccp1wHTrKXHZZP7gJFwDy7BlI/M=";
+    hash = "sha256-Be68we8JP+XCv+aUrm/JIbeA5wXL+QSu7IpFxWWHsbA=";
   };
 
   pythonRelaxDeps = [
