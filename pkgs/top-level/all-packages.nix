19ab78a01032e196096ae91bc13b46486c1c5e59
Jonas Heinrich <onny@project-insanity.org>
2024-02-01 11:12:28 +0100

    maddy: 0.7.1 -> unstable-2024-02-14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28173,8 +28173,6 @@ with pkgs;
     enableVDO = true;
   };
 
-  maddy = callPackage ../servers/maddy { };
-
   mbelib = callPackage ../development/libraries/audio/mbelib { };
 
   mbpfan = callPackage ../os-specific/linux/mbpfan { };
