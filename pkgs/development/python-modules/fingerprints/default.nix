b337d5873e4765211c5b541be31a9a27d66e3fc5
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-01 21:26:07 +0100

    python311Packages.fingerprints: 1.1.0 -> 1.2.3
    Diff: https://github.com/alephdata/fingerprints/compare/1.1.0...1.2.3

--- pkgs/development/python-modules/fingerprints/default.nix
+++ pkgs/development/python-modules/fingerprints/default.nix
@@ -6,14 +6,14 @@
 }:
 buildPythonPackage rec {
   pname = "fingerprints";
-  version = "1.1.0";
+  version = "1.2.3";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "alephdata";
     repo = "fingerprints";
     rev = version;
-    hash = "sha256-rptBM08dvivfglPvl3PZd9V/7u2SHbJ/BxfVHNGMt3A=";
+    hash = "sha256-U2UslCy1OagVTtllCKsEBX4zI/qIczbxs2Cxzy+/Xys=";
   };
 
   propagatedBuildInputs = [
