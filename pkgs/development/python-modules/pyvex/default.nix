f6624ffda833df51bc6b3fa918c91d6fa6c72233
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-02 08:31:21 +0000

    python312Packages.pyvex: 9.2.84 -> 9.2.88

--- pkgs/development/python-modules/pyvex/default.nix
+++ pkgs/development/python-modules/pyvex/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "pyvex";
-  version = "9.2.84";
+  version = "9.2.88";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-asTvaSwoT1yD6nqHTr6vICeukynMq1WRRn3gEvvnoVA=";
+    hash = "sha256-djuixaFYnPsJY+0knA4DvsVjZVHnJFW24tmfQINPweg=";
   };
 
   nativeBuildInputs = [
