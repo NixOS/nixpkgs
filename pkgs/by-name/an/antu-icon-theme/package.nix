6dd3dfba80269c3edd01ec2a303f911f97ea64c3
Angel J <78835633+Iamanaws@users.noreply.github.com>
2024-02-01 17:00:55 -0800

    add antu-icon-theme to pkgs/by-name

---
+++ pkgs/by-name/an/antu-icon-theme/package.nix
@@ -0,0 +1,33 @@
+{ lib, stdenvNoCC, fetchFromGitLab }:
+
+stdenvNoCC.mkDerivation {
+  pname = "antu-icon-theme";
+  version = "0-unstable-2024-01-23";
+
+  src = fetchFromGitLab {
+    owner = "froodo_alexis";
+    repo = "antu-icons";
+    rev = "279be277";
+    hash = "sha256-CLcr+X/b0moVEBV0O/dzCDq4w5G2+KRLUBdqKm0eAKA=";
+  };
+
+  dontDropIconThemeCache = true;
+
+  installPhase = ''
+    runHook preInstall
+    
+    mkdir -p $out/share/icons/antu-icons
+    cp -r * $out/share/icons/antu-icons
+    
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "A smooth icon theme designed for Plasma Desktop";
+    homepage = "https://gitlab.com/froodo_alexis/Antu-icons";
+    changelog = "https://gitlab.com/froodo_alexis/Antu-icons/-/releases";
+    license = licenses.cc-by-nc-sa-30;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ iamanaws ];
+  };
+}
