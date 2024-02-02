9c4a65790a0001872ccf6787471818a9fe5c72a3
DavHau <hsngrmpf+github@gmail.com>
2024-02-02 15:35:19 +0700

    qemu-python-utils: init at "0.6.1.0a1"

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34760,6 +34760,8 @@ with pkgs;
     inherit (darwin) sigtool;
   };
 
+  qemu-python-utils = python3Packages.toPythonApplication python3Packages.qemu;
+
   qemu-utils = qemu.override {
     toolsOnly = true;
   };
