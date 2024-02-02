3fafcf0683f662f4197598533979f12e6cd0a3c4
Nikolay Korotkiy <sikmir@disroot.org>
2024-02-02 22:34:46 +0400

    reproxy: 1.0.0 → 1.1.1

--- pkgs/servers/reproxy/default.nix
+++ pkgs/servers/reproxy/default.nix
@@ -2,28 +2,14 @@
 
 buildGoModule rec {
   pname = "reproxy";
-  version = "1.0.0";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "umputun";
-    repo = pname;
+    repo = "reproxy";
     rev = "v${version}";
-    hash = "sha256-ac4fOOMht2WGlrXLN95NEIA8ivqghhVuxHnBumvajx0=";
+    hash = "sha256-/ydpqi7O4z41YxYb/RngPWk/79h3MIxAopzqIDMgw1g=";
   };
-
-  postPatch = ''
-    # Requires network access
-    substituteInPlace app/main_test.go \
-      --replace "Test_Main" "Skip_Main"
-    substituteInPlace app/proxy/proxy_test.go \
-      --replace "TestHttp_matchHandler" "SkipHttp_matchHandler"
-  '' + lib.optionalString stdenv.isDarwin ''
-    # Fails on Darwin.
-    # https://github.com/umputun/reproxy/issues/77
-    substituteInPlace app/discovery/provider/file_test.go \
-      --replace "TestFile_Events" "SkipFile_Events" \
-      --replace "TestFile_Events_BusyListener" "SkipFile_Events_BusyListener"
-  '';
 
   vendorHash = null;
 
@@ -31,13 +17,19 @@ buildGoModule rec {
     "-s" "-w" "-X main.revision=${version}"
   ];
 
-  installPhase = ''
-    install -Dm755 $GOPATH/bin/app $out/bin/reproxy
+  checkFlags = [
+    # Requires network access or fluky
+    "-skip=^Test(_MainWithPlugin|_MainWithSSL|_Main|Http_matchHandler|Http_withBasicAuth|File_Events|File_Events_BusyListener)$"
+  ];
+
+  postInstall = ''
+    mv $out/bin/{app,reproxy}
   '';
 
   meta = with lib; {
     description = "Simple edge server / reverse proxy";
     homepage = "https://reproxy.io/";
+    changelog = "https://github.com/umputun/reproxy/releases/tag/${src.rev}";
     license = licenses.mit;
     maintainers = with maintainers; [ sikmir ];
     mainProgram = "reproxy";
