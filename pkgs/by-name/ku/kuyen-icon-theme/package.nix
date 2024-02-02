2d034f7bf2c4b64b2cf827663e3bde478e987868
Angel J <78835633+Iamanaws@users.noreply.github.com>
2024-02-01 16:58:33 -0800

    add kuyen-icon-theme to pkgs/by-name

---
+++ pkgs/by-name/ku/kuyen-icon-theme/package.nix
@@ -0,0 +1,33 @@
+{ lib, stdenvNoCC, fetchFromGitLab }:
+
+stdenvNoCC.mkDerivation {
+  pname = "kuyen-icon-theme";
+  version = "0-unstable-2024-01-27";
+
+  src = fetchFromGitLab {
+    owner = "froodo_alexis";
+    repo = "kuyen-icons";
+    rev = "9fc6c0e4";
+    hash = "sha256-6BK6+6CZhcPgDNuBIF9cYXGwS414NlL+KvRdsDzziaQ=";
+  };
+
+  dontDropIconThemeCache = true;
+
+  installPhase = ''
+    runHook preInstall
+  
+    mkdir -p $out/share/icons/kuyen-icons
+    cp -r * $out/share/icons/kuyen-icons
+    
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "A colourful flat theme designed for Plasma Desktop";
+    homepage = "https://gitlab.com/froodo_alexis/kuyen-icons";
+    changelog = "https://gitlab.com/froodo_alexis/kuyen-icons/-/releases";
+    license = licenses.cc-by-nc-sa-30;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ iamanaws ];
+  };
+}
