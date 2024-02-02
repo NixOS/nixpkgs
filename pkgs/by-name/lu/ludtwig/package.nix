3074b2c4f9b798dedb896d2515b601a8da0d3e34
Malte Janz <service.malte.j@protonmail.com>
2024-02-02 14:15:32 +0100

    ludtwig: 0.8.0 -> 0.8.1

--- pkgs/by-name/lu/ludtwig/package.nix
+++ pkgs/by-name/lu/ludtwig/package.nix
@@ -2,23 +2,23 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ludtwig";
-  version = "0.8.0";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "MalteJanz";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-WF3tEf3SuXiH35Ny4RGLzvEW7yMsFcnVTX52e5qvK5g=";
+    hash = "sha256-iDJvPgBmlca0hJnLrQcGJwlBeX8U1AX4D56gz13ieGg=";
   };
 
   checkType = "debug";
 
-  cargoHash = "sha256-AbT8Jv6v7EVPX5mIplKaBkGrVonA8YWlMvo46coFMzk=";
+  cargoHash = "sha256-wlIDqT+uoWgDaBOdrHwJU2AEwUXOJPQRF5RsCq/26m0=";
 
   meta = with lib; {
     description = "Linter / Formatter for Twig template files which respects HTML and your time.";
     homepage = "https://github.com/MalteJanz/ludtwig";
     license = licenses.mit;
-    maintainers = with maintainers; [ shyim ];
+    maintainers = with maintainers; [ shyim MalteJanz ];
   };
 }
