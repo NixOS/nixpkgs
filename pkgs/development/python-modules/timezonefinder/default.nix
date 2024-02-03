c73afae89bac84114dd729592f79c7a98f20f678
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-03 16:05:42 +0100

    python311Packages.timezonefinder: 6.2.0 -> 6.4.0
    Diff: https://github.com/jannikmi/timezonefinder/compare/refs/tags/6.2.0...6.4.0
    
    Changelog: https://github.com/jannikmi/timezonefinder/blob/6.4.0/CHANGELOG.rst

--- pkgs/development/python-modules/timezonefinder/default.nix
+++ pkgs/development/python-modules/timezonefinder/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "timezonefinder";
-  version = "6.2.0";
+  version = "6.4.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "jannikmi";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-n6TcTezu5seKy34KDlzGikAVaqAud00gxywwJA3MaWM=";
+    hash = "sha256-6hcReAzqTp4Od/PJN/W1uz4VS129yMFqQYznbsC/TRY=";
   };
 
   nativeBuildInputs = [
