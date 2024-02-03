6e897e4a4abccf44c7f9d7694d747c576c05b4d1
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-03 14:46:37 +0000

    k3s_1_29: 1.29.0+k3s1 -> 1.29.1+k3s1

--- pkgs/applications/networking/cluster/k3s/1_29/versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_29/versions.nix
@@ -1,8 +1,8 @@
 {
-  k3sVersion = "1.29.0+k3s1";
-  k3sCommit = "3190a5faa28d7a0d428c756d67adcab7eb11e6a5";
-  k3sRepoSha256 = "1g75a7kz9nnv0vagzhggkw0zqigykimdwsmibgssa8vyjpg7idda";
-  k3sVendorHash = "sha256-iHmPVjYR/ZLH9UZ5yNEApyuGQsEwtxVbQw7Pu7WrpaQ=";
+  k3sVersion = "1.29.1+k3s1";
+  k3sCommit = "6d77b7a9204ebe40c53425ce4bc82c1df456e911";
+  k3sRepoSha256 = "0x9ahb2b8l1gskrw6nwwylblsi6rv11ivxlrplmcjjg4jxv9xc6m";
+  k3sVendorHash = "sha256-jKNew4vSqR2kO6LtG0dn+8bdXg8vOnQ56HJMLsmOKSE=";
   chartVersions = import ./chart-versions.nix;
   k3sRootVersion = "0.12.2";
   k3sRootSha256 = "1gjynvr350qni5mskgm7pcc7alss4gms4jmkiv453vs8mmma9c9k";
