f6376745deb50a25fbf39474cd4477f7516319ac
Infinidoge <infinidoge@inx.moe>
2024-02-04 06:44:43 -0500

    mysql-workbench: use latest jre

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39840,7 +39840,6 @@ with pkgs;
     };
     mysql = mysql;
     pcre = pcre-cpp;
-    jre = openjdk19; # TODO: remove override https://github.com/NixOS/nixpkgs/pull/89731
   });
 
   r128gain = callPackage ../applications/audio/r128gain { };
