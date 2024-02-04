fef3145c8dd75ef4a49c74cdc16603b9fb4ab200
Infinidoge <infinidoge@inx.moe>
2024-02-04 13:55:51 -0500

    semeru-bin: mark EOL releases as vulnerable

--- pkgs/development/compilers/semeru-bin/jdk-linux.nix
+++ pkgs/development/compilers/semeru-bin/jdk-linux.nix
@@ -3,14 +3,16 @@
 let
   sources = (lib.importJSON ./sources.json).openj9.linux;
   common = opts: callPackage (import ./jdk-linux-base.nix opts) {};
+
+  EOL = [ "This JDK/JRE version has reached End of Life." ];
 in
 {
   jdk-8 = common { sourcePerArch = sources.jdk.openjdk8; };
   jre-8 = common { sourcePerArch = sources.jre.openjdk8; };
   jdk-11 = common { sourcePerArch = sources.jdk.openjdk11; };
   jre-11 = common { sourcePerArch = sources.jre.openjdk11; };
-  jdk-16 = common { sourcePerArch = sources.jdk.openjdk16; };
-  jre-16 = common { sourcePerArch = sources.jre.openjdk16; };
+  jdk-16 = common { sourcePerArch = sources.jdk.openjdk16; knownVulnerabilities = EOL; };
+  jre-16 = common { sourcePerArch = sources.jre.openjdk16; knownVulnerabilities = EOL; };
   jdk-17 = common { sourcePerArch = sources.jdk.openjdk17; };
   jre-17 = common { sourcePerArch = sources.jre.openjdk17; };
   jdk-21 = common { sourcePerArch = sources.jdk.openjdk21; };
