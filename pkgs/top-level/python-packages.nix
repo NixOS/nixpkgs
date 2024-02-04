8dde3a1788f1d4a280dfe097178d4fb09c42d7df
Stanisław Pitucha <git@viraptor.info>
2024-02-04 21:28:05 +1100

    python3Packages.mlx: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7164,6 +7164,8 @@ self: super: with self; {
 
   mlrose = callPackage ../development/python-modules/mlrose { };
 
+  mlx = callPackage ../development/python-modules/mlx { };
+
   mlxtend = callPackage ../development/python-modules/mlxtend { };
 
   mlt = toPythonModule (pkgs.mlt.override {
