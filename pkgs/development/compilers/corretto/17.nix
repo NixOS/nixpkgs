3317d3533fd17250c9e6c349ffd0444776fbad64
Infinidoge <infinidoge@inx.moe>
2024-02-04 07:10:23 -0500

    corretto: 17.0.8.8.1 -> 17.0.11.9.1
    Removes the FixNullPtrCast patch, as it failed to apply.
    Need to investigate whether the patch needs to be updated for this
    version, or if the patch was backported into corretto's JDK source.

--- pkgs/development/compilers/corretto/17.nix
+++ pkgs/development/compilers/corretto/17.nix
@@ -1,4 +1,5 @@
 { fetchFromGitHub
+, fetchurl
 , gradle_7
 , jdk17
 , lib
@@ -9,17 +10,25 @@
 }:
 
 let
-  corretto = import ./mk-corretto.nix {
+  corretto = import ./mk-corretto.nix rec {
     inherit lib stdenv rsync runCommand testers;
     jdk = jdk17;
     gradle = gradle_7;
-    version = "17.0.8.8.1";
+    version = "17.0.11.9.1";
     src = fetchFromGitHub {
       owner = "corretto";
       repo = "corretto-17";
-      rev = "9a3cc984f76cb5f90598bdb43215bad20e0f7319";
-      sha256 = "sha256-/VuB3ocD5VvDqCU7BoTG+fQ0aKvK1TejegRYmswInqQ=";
+      rev = version;
+      sha256 = "sha256-LxZSFILFfyh8oBiYEnuBQ0Og2i713qdK2jIiCBnrlj0=";
     };
   };
 in
-corretto
+corretto.overrideAttrs (final: prev: {
+  # HACK: Removes the FixNullPtrCast patch, as it fails to apply. Need to figure out what causes it to fail to apply.
+  patches = lib.remove
+    (fetchurl {
+      url = "https://git.alpinelinux.org/aports/plain/community/openjdk17/FixNullPtrCast.patch?id=41e78a067953e0b13d062d632bae6c4f8028d91c";
+      sha256 = "sha256-LzmSew51+DyqqGyyMw2fbXeBluCiCYsS1nCjt9hX6zo=";
+    })
+    prev.patches;
+})
