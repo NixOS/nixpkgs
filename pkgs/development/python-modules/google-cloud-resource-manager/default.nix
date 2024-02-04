0a81f921caf4d10c873327dfd1be89736419461e
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-04 22:39:39 +0100

    python311Packages.google-cloud-resource-manager: refactor

--- pkgs/development/python-modules/google-cloud-resource-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-resource-manager/default.nix
@@ -9,12 +9,13 @@
 , pytest-asyncio
 , pytestCheckHook
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "google-cloud-resource-manager";
   version = "1.12.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
@@ -22,6 +23,10 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-wsSuU+X5/2EuTPlh8brD+RAK5UpRIsGxzUI26ITREsU=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     google-api-core
