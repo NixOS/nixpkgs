6b6c0c112371ca79283cf8975a6814671b92dfe8
Infinidoge <infinidoge@inx.moe>
2024-02-04 04:54:35 -0500

    openjdk: add infinidoge as maintainer

--- pkgs/development/compilers/openjdk/meta.nix
+++ pkgs/development/compilers/openjdk/meta.nix
@@ -2,7 +2,7 @@ lib: version: with lib; {
   homepage = "https://openjdk.java.net/";
   license = licenses.gpl2Only;
   description = "The open-source Java Development Kit";
-  maintainers = with maintainers; [ edwtjo ];
+  maintainers = with maintainers; [ edwtjo infinidoge ];
   platforms = [ "i686-linux" "x86_64-linux" "aarch64-linux" "armv7l-linux" "armv6l-linux" "powerpc64le-linux" ];
   mainProgram = "java";
   knownVulnerabilities = optionals (builtins.elem (versions.major version) [ "12" "13" "14" "15" "16" "18" "19" "20" ]) [
