1d91d1ca4b318737fd16d0e64c87af087ff00466
netthier <admin@netthier.net>
2024-02-04 22:16:33 +0100

    wip
    Signed-off-by: netthier <admin@netthier.net>

--- pkgs/applications/networking/cluster/kluctl/default.nix
+++ pkgs/applications/networking/cluster/kluctl/default.nix
@@ -1,29 +1,49 @@
-{ lib, stdenv, buildGoModule, fetchFromGitHub, testers, kluctl }:
+{ lib, pkgs, stdenv, buildGoModule, fetchFromGitHub, testers, autoPatchelfHook, kluctl }:
 
 buildGoModule rec {
   pname = "kluctl";
   version = "2.23.1";
 
   src = fetchFromGitHub {
-    owner = "kluctl";
+    owner = "netthier";
     repo = "kluctl";
-    rev = "v${version}";
-    hash = "sha256-wT40MgvXl32jHq+k0nYaPnAcfLBMxRMiN5CYVXM2PYM=";
+    #rev = "v${version}";
+    rev = "go-embed-py-patch";
+    hash = "sha256-V6qZqWvMXOcRW0vKUgcg1jIdt/W2HAlpfOxfEMhP3e0=";
   };
 
   subPackages = [ "cmd" ];
 
-  vendorHash = "sha256-dGuB68y/JsmfBZLGPgGbGgdRPzVWzveOLjFXOKMp7GY=";
+  vendorHash = "sha256-E8RDDRWJmzVxmVA1ssRJd+tscjf7bJLiB/OhzDpRJ5o=";
 
   ldflags = [ "-s" "-w" "-X main.version=v${version}" ];
 
   # Depends on docker
   doCheck = false;
 
+  nativeBuildInputs = [
+    autoPatchelfHook
+    pkgs.libxcrypt-legacy
+  ];
+
+  buildInputs = with pkgs; [
+    libxcrypt-legacy
+  ];
+
   passthru.tests.version = testers.testVersion {
     package = kluctl;
     version = "v${version}";
   };
+
+  modPostBuild = ''
+    PREPARE_PATH=$(mktemp -d)
+    cd vendor/github.com/kluctl/go-embed-python
+    go run -mod=mod github.com/kluctl/go-embed-python/python/generate --python-version=3.11.6 --python-standalone-version=20231002 --pack=false --prepare-path=$PREPARE_PATH
+    autoPatchelf $PREPARE_PATH
+    go run -mod=mod github.com/kluctl/go-embed-python/python/generate --python-version=3.11.6 --python-standalone-version=20231002 --prepare=false --prepare-path=$PREPARE_PATH
+    cd ../../../..
+  '';
+
 
   postInstall = ''
     mv $out/bin/{cmd,kluctl}
