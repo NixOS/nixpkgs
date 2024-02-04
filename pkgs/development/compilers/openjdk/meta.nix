4484cc91ac0ff8e41739eeeaecbd62e6b4dc88d8
Infinidoge <infinidoge@inx.moe>
2024-02-04 05:57:16 -0500

    openjdk: mark openjdk19 and openjdk20 as EOL

--- pkgs/development/compilers/openjdk/meta.nix
+++ pkgs/development/compilers/openjdk/meta.nix
@@ -5,7 +5,7 @@ lib: version: with lib; {
   maintainers = with maintainers; [ edwtjo ];
   platforms = [ "i686-linux" "x86_64-linux" "aarch64-linux" "armv7l-linux" "armv6l-linux" "powerpc64le-linux" ];
   mainProgram = "java";
-  knownVulnerabilities = optionals (builtins.elem (versions.major version) [ "12" "13" "14" "15" "16" "18" ]) [
+  knownVulnerabilities = optionals (builtins.elem (versions.major version) [ "12" "13" "14" "15" "16" "18" "19" "20" ]) [
     "This OpenJDK version has reached its end of life."
   ];
 }
