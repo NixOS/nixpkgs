2b43efb8ae720fff9528d4bbafa06a7758dfa53f
Jan Tojnar <jtojnar@gmail.com>
2024-02-04 15:45:22 +0100

    libxml2: 2.12.4 → 2.12.5
    https://discourse.gnome.org/t/libxml2-2-12-5-released/19337
    https://gitlab.gnome.org/GNOME/libxml2/-/compare/v2.12.4...v2.12.5
    
    CVE-2024-25062

--- pkgs/development/libraries/libxml2/default.nix
+++ pkgs/development/libraries/libxml2/default.nix
@@ -34,7 +34,7 @@ in
 let
 libxml = stdenv.mkDerivation rec {
   pname = "libxml2";
-  version = "2.12.4";
+  version = "2.12.5";
 
   outputs = [ "bin" "dev" "out" "doc" ]
     ++ lib.optional pythonSupport "py"
@@ -43,7 +43,7 @@ libxml = stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/libxml2/${lib.versions.majorMinor version}/libxml2-${version}.tar.xz";
-    hash = "sha256-SXNg5CPPC9merNt8YhXeqS5tbonulAOTwrrg53y5t9A=";
+    hash = "sha256-qXJ5Zpav04Bz4PWcKDw6L1pWC1JotLq8ORsoYWZSayE=";
   };
 
   strictDeps = true;
