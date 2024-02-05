834a68f047d9c1140441dec6145bc8147d4e1e83
Motiejus Jakštys <motiejusja@wix.com>
2024-02-05 14:40:26 +0200

    maintainers: add motiejus

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12794,6 +12794,16 @@
     githubId = 10601196;
     name = "Jérémie Ferry";
   };
+  motiejus = {
+    email = "motiejus@jakstys.lt";
+    matrix = "@motiejus:jakstys.lt";
+    github = "motiejus";
+    githubId = 107720;
+    name = "Motiejus Jakštys";
+    keys = [{
+      fingerprint = "5F6B 7A8A 92A2 60A4 3704  9BEB 6F13 3A0C 1C28 48D7";
+    }];
+  };
   mounium = {
     email = "muoniurn@gmail.com";
     github = "Mounium";
