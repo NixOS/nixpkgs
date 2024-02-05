78631ced4ffce8fa23049bf0be01cb2a8cf2033a
Jacob Hrbek <kreyren@fsfe.org>
2024-02-05 04:19:02 +0000

    ubootOlimexA64Teres1: Use Crust
    SCP Firmware needed for power management of the device

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -21,7 +21,9 @@
 , armTrustedFirmwareRK3328
 , armTrustedFirmwareRK3399
 , armTrustedFirmwareS905
+, crustOlimexA64Teres1
 , buildPackages
+, pkgsCross
 }:
 
 let
@@ -357,8 +359,7 @@ in {
     defconfig = "teres_i_defconfig";
     extraMeta.platforms = ["aarch64-linux"];
     BL31 = "${armTrustedFirmwareAllwinner}/bl31.bin";
-    # Using /dev/null here is upstream-specified way that disables the inclusion of crust-firmware as it's not yet packaged and without which the build will fail -- https://docs.u-boot.org/en/latest/board/allwinner/sunxi.html#building-the-crust-management-processor-firmware
-    SCP = "/dev/null";
+    SCP = "${pkgsCross.or1k.crustOlimexA64Teres1}/scp.bin";
     filesToInstall = ["u-boot-sunxi-with-spl.bin"];
   };
 
