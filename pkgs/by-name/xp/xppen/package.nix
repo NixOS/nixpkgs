e4a9c0f0c8a810e97d623861e93e7d225d4e8fbd
yakrobat <yakrobat@protonmail.com>
2024-02-05 15:13:09 +0300

    Create wrapper for xppen-unwrapped

--- pkgs/by-name/xp/xppen/package.nix
+++ pkgs/by-name/xp/xppen/package.nix
@@ -1,53 +1,29 @@
-{ stdenv
-, lib
-, fetchzip
+{ lib
+, buildFHSEnv
+, xppen-unwrapped
 , libusb1
-, autoPatchelfHook
 , qt5
 , libGL
 }:
-stdenv.mkDerivation {
-  pname = "xppen";
-  version = "3.4.9";
+buildFHSEnv {
+  name = "xppen";
 
-  src = fetchzip {
-    extension = "tar.gz";
-    url = "https://www.xp-pen.com/download/file.html?id=1936&pid=990&ext=gz";
-    hash = "sha256-A/dv6DpelH0NHjlGj32tKv37S+9q3F8cYByiYlMuqLg=";
-  };
+  runScript = "PenTablet";
 
-  nativeBuildInputs = [ autoPatchelfHook qt5.wrapQtAppsHook ];
+  targetPkgs = pkgs: [
+    xppen-unwrapped
+    libusb1
+    qt5.full
+    libGL
+  ];
 
-  buildInputs = [ qt5.full libusb1 libGL ];
+  extraBwrapArgs = [
+    "--ro-bind ${xppen-unwrapped}/usr/lib/pentablet /var/lib/pentablet"
+  ];
 
-  dontConfigure = true;
-  dontBuild = true;
-  dontCheck = true;
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin $out/share
-    cp -r App/usr/lib/pentablet/. $out/bin
-    rm $out/bin/PenTablet.sh
-    cp -r App/usr/share/. $out/share
-    rm -r $out/bin/lib
-    cp -r App/lib $out/lib
-    runHook postInstall
+  extraBuildCommands = ''
+    mkdir -p $out/var/lib/pentablet
   '';
 
-  postInstall = ''
-    substituteInPlace $out/share/applications/xppentablet.desktop \
-      --replace "/usr/lib/pentablet/PenTablet.sh" "PenTablet" \
-      --replace "/usr/share/icons/hicolor/256x256/apps/xppentablet.png" "xppentablet"
-  '';
-
-  meta = with lib; {
-    description = "XPPen driver";
-    homepage = "https://www.xp-pen.com/";
-    downloadPage = "https://www.xp-pen.com/download/";
-    license = licenses.unfree;
-    maintainers = with maintainers; [ nasrally ];
-    mainProgram = "PenTablet";
-    platforms = platforms.linux;
-  };
+  meta = xppen-unwrapped.meta;
 }
