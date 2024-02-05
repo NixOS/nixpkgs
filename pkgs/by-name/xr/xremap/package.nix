93c0e9cd511e96d23b69cfba88cc71b59760ff1e
0x4A6F <0x4A6F@users.noreply.github.com>
2024-02-05 14:22:20 +0100

    xremap: make configurable

--- pkgs/by-name/xr/xremap/package.nix
+++ pkgs/by-name/xr/xremap/package.nix
@@ -4,8 +4,10 @@
   fetchFromGitHub,
   pkg-config,
   stdenv,
-  wayland,
-  kdeSupport ? true,
+  gnomeSupport ? false,
+  kdeSupport ? false,
+  wlrootsSupport ? false,
+  x11Support ? false,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -21,14 +23,14 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-ABzt8PMsas9+NRvpgtZlsoYjjvwpU8f6lqhceHxq91M=";
 
-  cargoBuildFlags = lib.optional kdeSupport "--features kde";
+  cargoBuildFlags =
+    lib.optional gnomeSupport "--features gnome"
+    ++ lib.optional kdeSupport "--features kde"
+    ++ lib.optional wlrootsSupport "--features wlroots"
+    ++ lib.optional x11Support "--features x11";
 
   nativeBuildInputs = [
     pkg-config
-  ];
-
-  buildInputs = lib.optionals stdenv.isLinux [
-    wayland
   ];
 
   meta = with lib; {
