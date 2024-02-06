439f6752c27e077ce0f28fd4178e8e2a47eaa2d6
Jonas Heinrich <onny@project-insanity.org>
2024-02-06 16:41:33 +0100

    posterazor: minor improvements

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34666,8 +34666,6 @@ with pkgs;
 
   potrace = callPackage ../applications/graphics/potrace { };
 
-  posterazor = callPackage ../applications/misc/posterazor { };
-
   pqiv = callPackage ../applications/graphics/pqiv { };
 
   qiv = callPackage ../applications/graphics/qiv {
