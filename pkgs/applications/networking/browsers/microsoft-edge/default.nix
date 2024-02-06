01c35392582cbf3596bf44c9781b13d09572c753
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-06 18:48:05 +0000

    microsoft-edge: 121.0.2277.98 -> 121.0.2277.106

--- pkgs/applications/networking/browsers/microsoft-edge/default.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/default.nix
@@ -1,20 +1,20 @@
 {
   stable = import ./browser.nix {
     channel = "stable";
-    version = "121.0.2277.98";
+    version = "121.0.2277.106";
     revision = "1";
-    hash = "sha256-vm0aBWiGtjdSu55nCNuhbqn4XVI6l/BxwmpTlTHWt/M=";
+    hash = "sha256-D0bHpz85J6R6LNWr8zaMt9vyolHYkmo9Bi4VaXCkH1U=";
   };
   beta = import ./browser.nix {
     channel = "beta";
-    version = "122.0.2365.8";
+    version = "122.0.2365.16";
     revision = "1";
-    hash = "sha256-1qM61lO7LyX7CuLrKsEuciud7BuDxRKNyQahdFJhq+g=";
+    hash = "sha256-SeLX7UibXd1nOhxWwMuUTCKK4GkN2TmJPesWhLwCD6A=";
   };
   dev = import ./browser.nix {
     channel = "dev";
-    version = "122.0.2365.3";
+    version = "123.0.2380.1";
     revision = "1";
-    hash = "sha256-O2SxGzcvNloxLbexDjA0C28w7EJi1Fl9IUnI1zc1S6Y=";
+    hash = "sha256-SBlHXURiPoC5Q7wi67tgnuV2PUw4ffniGq6kmOZtIf0=";
   };
 }
