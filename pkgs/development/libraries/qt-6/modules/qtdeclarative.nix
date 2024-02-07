a56b2b2d225feb73c743bb61d8de712b1db350c7
Nick Cao <nickcao@nichi.co>
2024-02-07 11:52:44 -0500

    qt6.qtdeclarative: refresh patches

--- pkgs/development/libraries/qt-6/modules/qtdeclarative.nix
+++ pkgs/development/libraries/qt-6/modules/qtdeclarative.nix
@@ -11,8 +11,8 @@ qtModule {
   propagatedBuildInputs = [ qtbase qtlanguageserver qtshadertools openssl python3 ];
   patches = [
     # prevent headaches from stale qmlcache data
-    ../patches/qtdeclarative-default-disable-qmlcache.patch
+    ../patches/0001-qtdeclarative-disable-qml-disk-cache.patch
     # add version specific QML import path
-    ../patches/qtdeclarative-qml-paths.patch
+    ../patches/0002-qtdeclarative-also-use-versioned-qml-paths.patch
   ];
 }
