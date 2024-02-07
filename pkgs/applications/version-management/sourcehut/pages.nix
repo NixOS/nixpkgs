16c4501f965f1d0abc128dbc2885239a19d0ed5b
Christoph Heiss <christoph@c8h4.io>
2024-02-07 10:11:51 +0100

    sourcehut.pagessrht: 0.13.0 -> 0.15.4
    Signed-off-by: Christoph Heiss <christoph@c8h4.io>

--- pkgs/applications/version-management/sourcehut/pages.nix
+++ pkgs/applications/version-management/sourcehut/pages.nix
@@ -6,13 +6,13 @@
 
 buildGoModule (rec {
   pname = "pagessrht";
-  version = "0.13.0";
+  version = "0.15.4";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = "pages.sr.ht";
     rev = version;
-    sha256 = "sha256-vUN6c6cyhcLI8bKrFYKoxlBQ29VS/bowpSfBRmi47wg=";
+    hash = "sha256-3kdQVIL7xaIPu2elxj1k+4/y75bd+OKP5+VPSniF7w8=";
   };
 
   postPatch = ''
@@ -20,7 +20,7 @@ buildGoModule (rec {
       --replace "all: server" ""
   '';
 
-  vendorHash = "sha256-GKuHkUqSVBLN3k8YsFtxdmdHFkqKo9YZqDk2GBmbfWo=";
+  vendorHash = "sha256-DP+6rxjiXzs0RbSuMD20XwO/+v7QXCNgXj2LxZ96lWE=";
 
   postInstall = ''
     mkdir -p $out/share/sql/
