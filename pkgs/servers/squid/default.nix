d3d877b08ff64214a5464c97af57cf8e867104cf
Sarah Brofeldt <sarah@qtr.dk>
2024-02-07 14:41:48 +0100

    Revert "squid: add patches for several CVEs"
    This reverts commit 32f201ba1e6f93ef1354274d083f3935906323f0.

--- pkgs/servers/squid/default.nix
+++ pkgs/servers/squid/default.nix
@@ -1,7 +1,6 @@
 { lib, stdenv, fetchurl, perl, openldap, pam, db, cyrus_sasl, libcap
 , expat, libxml2, openssl, pkg-config, systemd
 , cppunit
-, fetchpatch
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -12,39 +11,6 @@ stdenv.mkDerivation (finalAttrs: {
     url = "http://www.squid-cache.org/Versions/v6/${finalAttrs.pname}-${finalAttrs.version}.tar.xz";
     hash = "sha256-Vb1/n0iYFTFh6hIomYrLVRv4QIMrnluQ/I7NKUJCAxg=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "CVE-2023-46847.patch";
-      url = "http://www.squid-cache.org/Versions/v5/SQUID-2023_3.patch";
-      hash = "sha256-ofY9snWOsfHTCZcK7HbsLD0j5nP+O0eitWU4fK/mSqA=";
-    })
-    (fetchpatch {
-      name = "CVE-2023-50269.patch";
-      url = "http://www.squid-cache.org/Versions/v5/SQUID-2023_10.patch";
-      hash = "sha256-xa81wd2WAUboUhMpDcsj33sKoIzPF00tZDY/pw76XYQ=";
-    })
-    (fetchpatch {
-      name = "CVE-2023-49285.patch";
-      url = "http://www.squid-cache.org/Versions/v5/SQUID-2023_7.patch";
-      hash = "sha256-G6DZhHCfzoIb+q+sw0a5cMF6rSvrLhT7ntp0KiiCVSA=";
-    })
-    (fetchpatch {
-      name = "CVE-2023-46848.patch";
-      url = "http://www.squid-cache.org/Versions/v5/SQUID-2023_5.patch";
-      hash = "sha256-aduHoEqU/XKuh57y7tUDr7zIRprfC24Ifw5Ep0aboCg=";
-    })
-    (fetchpatch {
-      name = "CVE-2023-46724.patch";
-      url = "http://www.squid-cache.org/Versions/v5/SQUID-2023_4.patch";
-      hash = "sha256-Ptt/yweUmB3lfg/o3mdBIjzpntCkRR9wDruQNYJifmE=";
-    })
-    (fetchpatch {
-      name = "CVE-2023-46846.patch";
-      url = "http://www.squid-cache.org/Versions/v5/SQUID-2023_1.patch";
-      hash = "sha256-Tu8oJTlYleKlZPfD3rslNfkLVcB8sjp25+N/9S423+8=";
-    })
-  ];
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [
@@ -89,12 +55,6 @@ stdenv.mkDerivation (finalAttrs: {
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
     maintainers = with maintainers; [ raskin ];
-    knownVulnerabilities = [
-      "GHSA-rj5h-46j6-q2g5"
-      "CVE-2023-5824"
-      "CVE-2023-46728"
-      "CVE-2023-49286"
-      "Several outstanding, unnumbered issues from https://megamansec.github.io/Squid-Security-Audit/ with unclear status"
-    ];
+    knownVulnerabilities = [ "Squid has multiple unresolved security vulnerabilities, for more information see https://megamansec.github.io/Squid-Security-Audit/" ];
   };
 })
