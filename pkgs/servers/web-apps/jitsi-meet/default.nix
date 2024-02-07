239986078b35942d022a72abb21214dc3fb33f64
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-07 19:12:43 +0000

    jitsi-meet: 1.0.7762 -> 1.0.7790

--- pkgs/servers/web-apps/jitsi-meet/default.nix
+++ pkgs/servers/web-apps/jitsi-meet/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "jitsi-meet";
-  version = "1.0.7762";
+  version = "1.0.7790";
 
   src = fetchurl {
     url = "https://download.jitsi.org/jitsi-meet/src/jitsi-meet-${version}.tar.bz2";
-    sha256 = "SsmMQdR6JczL63/o/LHbg5sjnBA+TuYPu4WR+tlGaC4=";
+    sha256 = "qW3Zcrq+a1I5LABUc4uhr58E7Ig8SmrJVNdjLs0l0io=";
   };
 
   dontBuild = true;
