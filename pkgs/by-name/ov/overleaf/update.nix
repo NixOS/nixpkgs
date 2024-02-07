5f6f055e699051b7f882149ca4d698d309287ad9
Camille M. (genesis) <camillemondon@free.fr>
2024-02-07 12:46:09 +0100

    overleaf: move to pkgs/by-name

--- pkgs/by-name/ov/overleaf/update.nix
+++ pkgs/by-name/ov/overleaf/update.nix
@@ -19,10 +19,10 @@ writeScript "update-overleaf" ''
   rev=$(curl -s "https://hub.docker.com/v2/repositories/sharelatex/sharelatex/tags/$tag/images" | jq .[0].layers | grep -Po "MONOREPO_REVISION=\K[a-z0-9]*" -m 1)
 
   (
-    cd ../../../
+    cd ../../../../
     nix-update overleaf --version branch=$rev
   )
 
-  sed -i "s/version = \"[0-9a-z-]*\";/version = \"$tag\";/g" default.nix
+  sed -i "s/version = \"[0-9a-z-]*\";/version = \"$tag\";/g" package.nix
 ''
 
