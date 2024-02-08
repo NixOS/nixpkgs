bac15ec63085add6c6ebc1fe7d268ed5d53bb112
Luflosi <luflosi@luflosi.de>
2024-02-08 10:09:01 +0100

    python3Packages.furo: 2023.9.10 -> 2024.1.29
    https://github.com/pradyunsg/furo/releases/tag/2024.01.29

--- pkgs/development/python-modules/furo/default.nix
+++ pkgs/development/python-modules/furo/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "furo";
-  version = "2023.9.10";
+  version = "2024.1.29";
   format = "wheel";
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     inherit pname version format;
     dist = "py3";
     python = "py3";
-    hash = "sha256-UTCSU4U33FxZZpHaBuPDcHFOyZvEOGgO3B3r/7c+W/w=";
+    hash = "sha256-NUi+LO9Foy+M3AJy1BX8s+X6ag603f4h3z7PH+RaE88=";
   };
 
   nativeBuildInputs = [
