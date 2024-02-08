93af9701dd3b298e6bd1b6aaff4e86611e57e88c
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-08 05:55:54 +0000

    eccodes: 2.33.0 -> 2.34.0

--- pkgs/development/libraries/eccodes/default.nix
+++ pkgs/development/libraries/eccodes/default.nix
@@ -16,11 +16,11 @@
 
 stdenv.mkDerivation rec {
   pname = "eccodes";
-  version = "2.33.0";
+  version = "2.34.0";
 
   src = fetchurl {
     url = "https://confluence.ecmwf.int/download/attachments/45757960/eccodes-${version}-Source.tar.gz";
-    sha256 = "sha256-vc7IzmNlTsaANADFB/ASIKmqQDpF+mtb3/f9zET9fa8=";
+    sha256 = "sha256-PNIIyN2tEyeJZiz49nqUBVFL/vysrEA8DYyEUH8wOro=";
   };
 
   postPatch = ''
