d2f7dcc6446376550aefb147ce51fb1bb291fd86
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-08 22:16:37 +0000

    quark-engine: 23.12.1 -> 24.2.1

--- pkgs/tools/security/quark-engine/default.nix
+++ pkgs/tools/security/quark-engine/default.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "quark-engine";
-  version = "23.12.1";
+  version = "24.2.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-NeJGkqGpg2uOsz10gEYz/nzh21DhXSU4RgQtI1brASQ=";
+    sha256 = "sha256-77yfysmFEneVOiejoCooi1buqEM/Ljv5xqjKv17DFWE=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
