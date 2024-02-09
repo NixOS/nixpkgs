0460e74a345a8f341c5dfd4761402ed7ce1373e1
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-09 11:07:46 +0000

    dovecot_fts_xapian: 1.5.9 -> 1.6.0

--- pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
+++ pkgs/servers/mail/dovecot/plugins/fts_xapian/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchFromGitHub, autoconf, automake, sqlite, pkg-config, dovecot, libtool, xapian, icu64 }:
 stdenv.mkDerivation rec {
   pname = "dovecot-fts-xapian";
-  version = "1.5.9";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "grosjo";
     repo = "fts-xapian";
     rev = version;
-    sha256 = "sha256-RH272gWmGQH/+V0JHUO9c4PsWY89Cd2TeeHVAnkJcws=";
+    sha256 = "sha256-UAH6IF6iEzzXY2Zl/1aeRnFwb73K5Fgp0WWEgo7ZdFM=";
   };
 
   buildInputs = [ dovecot xapian icu64 sqlite ];
