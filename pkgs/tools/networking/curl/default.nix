NixOS/nixpkgs

commit 996b4ebc08e567e51af2116d8aa86c4069256fe6

curl: build with public suffix list support
And make sure we pass `--without-libpsl` when it is disabled.

https://daniel.haxx.se/blog/2024/01/10/psl-in-curl/

pkgs/tools/networking/curl/default.nix
@@ -120,6 +120,7 @@ stdenv.mkDerivation (finalAttrs: {
       (lib.withFeature rtmpSupport "librtmp")
       (lib.withFeature rustlsSupport "rustls")
       (lib.withFeature zstdSupport "zstd")
+      (lib.withFeature pslSupport "libpsl")
       (lib.withFeatureAs brotliSupport "brotli" (lib.getDev brotli))
       (lib.withFeatureAs gnutlsSupport "gnutls" (lib.getDev gnutls))
       (lib.withFeatureAs idnSupport "libidn2" (lib.getDev libidn2))

pkgs/top-level/all-packages.nix
@@ -7346,6 +7346,7 @@ with pkgs;
 
   curl = curlMinimal.override ({
     idnSupport = true;
+    pslSupport = true;
     zstdSupport = true;
   } // lib.optionalAttrs (!stdenv.hostPlatform.isStatic) {
     brotliSupport = true;

commit 817d20170dc6b7b2c195a6c3271d88789f206b0b

curl.tests.static: init
We need static curl for nixStatic, so we should test that it builds
when changing curl.

This would have caught a regression introduced by
996b4ebc08e5 ("curl: build with public suffix list support").

pkgs/tools/networking/curl/default.nix
@@ -33,6 +33,7 @@
 , haskellPackages
 , ocamlPackages
 , phpExtensions
+, pkgsStatic
 , python3
 , tests
 , testers
@@ -195,6 +196,7 @@ stdenv.mkDerivation (finalAttrs: {
       # nginx-http3 = useThisCurl nixosTests.nginx-http3;
       nginx-http3 = nixosTests.nginx-http3;
       pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+      static = pkgsStatic.curl;
     } // lib.optionalAttrs (!stdenv.isDarwin) {
       fetchpatch = tests.fetchpatch.simple.override { fetchpatch = (fetchpatch.override { fetchurl = useThisCurl fetchurl; }) // { version = 1; }; };
     };