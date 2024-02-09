c25c4d4933652d2bb279eb79dcd25870fbb423b4
Gaetan Lepage <gaetan@glepage.com>
2024-02-09 23:27:41 +0100

    fishPlugins.plugin-git: 0.1 -> 0.2
    Diff: https://github.com/jhillyerd/plugin-git/compare/refs/tags/v0.1...v0.2
    
    Changelog: https://github.com/jhillyerd/plugin-git/releases/tag/v0.2

--- pkgs/shells/fish/plugins/plugin-git.nix
+++ pkgs/shells/fish/plugins/plugin-git.nix
@@ -2,18 +2,19 @@
 
 buildFishPlugin rec {
   pname = "plugin-git";
-  version = "0.1";
+  version = "0.2";
 
   src = fetchFromGitHub {
     owner = "jhillyerd";
     repo = "plugin-git";
-    rev = "v0.1";
-    sha256 = "sha256-MfrRQdcj7UtIUgtqKjt4lqFLpA6YZgKjE03VaaypNzE";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-MfrRQdcj7UtIUgtqKjt4lqFLpA6YZgKjE03VaaypNzE";
   };
 
   meta = with lib; {
     description = "Git plugin for fish (similar to oh-my-zsh git)";
     homepage = "https://github.com/jhillyerd/plugin-git";
+    changelog = "https://github.com/jhillyerd/plugin-git/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ unsolvedcypher ];
   };
