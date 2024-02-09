NixOS/nixpkgs

commit 996b4ebc08e567e51af2116d8aa86c4069256fe6

curl: build with public suffix list support
And make sure we pass `--without-libpsl` when it is disabled.

https://daniel.haxx.se/blog/2024/01/10/psl-in-curl/

pkgs/tools/networking/curl/default.nix
@@ -120,6 +120,7 @@ stdenv.mkDerivation (finalAttrs: {
       (lib.withFeature rtmpSupport "librtmp")
       (lib.withFeature rustlsSupport "rustls")
       (lib.withFeature zstdSupport "zstd")
+      (lib.withFeature pslSupport "libpsl")
       (lib.withFeatureAs brotliSupport "brotli" (lib.getDev brotli))
       (lib.withFeatureAs gnutlsSupport "gnutls" (lib.getDev gnutls))
       (lib.withFeatureAs idnSupport "libidn2" (lib.getDev libidn2))

pkgs/top-level/all-packages.nix
@@ -7346,6 +7346,7 @@ with pkgs;
 
   curl = curlMinimal.override ({
     idnSupport = true;
+    pslSupport = true;
     zstdSupport = true;
   } // lib.optionalAttrs (!stdenv.hostPlatform.isStatic) {
     brotliSupport = true;

commit 4ae0827699ae0b3b412b5683bddb23d251b529c9

pkgsStatic.curl: fix build
Link: https://github.com/NixOS/nixpkgs/pull/285295#issuecomment-1935911944
Fixes: 996b4ebc08e5 ("curl: build with public suffix list support")

pkgs/tools/networking/curl/default.nix
@@ -102,6 +102,11 @@ stdenv.mkDerivation (finalAttrs: {
   preConfigure = ''
     sed -e 's|/usr/bin|/no-such-path|g' -i.bak configure
     rm src/tool_hugehelp.c
+  '' + lib.optionalString (pslSupport && stdenv.hostPlatform.isStatic) ''
+    # curl doesn't understand that libpsl2 has deps because it doesn't use
+    # pkg-config.
+    # https://github.com/curl/curl/pull/12919
+    configureFlagsArray+=("LIBS=-lidn2 -lunistring")
   '';
 
   configureFlags = [