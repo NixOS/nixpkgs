39eeea8ccc0df01839567485fcbf795535594b65
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-02-09 15:11:49 +0100

    python311Packages.habluetooth: 2.1.0 -> 2.4.0
    https://github.com/Bluetooth-Devices/habluetooth/blob/v2.4.0/CHANGELOG.md

--- pkgs/development/python-modules/habluetooth/default.nix
+++ pkgs/development/python-modules/habluetooth/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "habluetooth";
-  version = "2.1.0";
+  version = "2.4.0";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = "habluetooth";
     rev = "refs/tags/v${version}";
-    hash = "sha256-oPdKmaj2wKgOQw7QYwOQc8efcNtQiGryZgNJ+bbB6L8=";
+    hash = "sha256-bZtcvidjUhlb9ML1UIP00yqJ+KnJig5i0j/tAZSK7+Y=";
   };
 
   postPatch = ''
