NixOS/nixpkgs

commit ad78ce77647ebccfef86f44202efe7be9a671a24

libvirt: fix hardcoded modules path

pkgs/development/libraries/libvirt/default.nix
@@ -250,6 +250,9 @@ stdenv.mkDerivation rec {
       substituteInPlace src/util/virpolkit.h \
         --replace '"/usr/bin/pkttyagent"' '"${if isLinux then polkit.bin else "/usr"}/bin/pkttyagent"'
 
+      substituteInPlace src/util/virpci.c \
+         --replace '/lib/modules' '${if isLinux then "/run/current-system/kernel-modules" else ""}/lib/modules'
+
       patchShebangs .
     ''
     + (lib.concatStringsSep "\n" (lib.mapAttrsToList patchBuilder overrides));

commit df966328e0ee9183c57d6f1a409a418a4704b583

libvirt: Fix broken when Kernel differs b/w current- and booted-system
Libvirt checks this path + $(uname -r), which is the running Kernel.
current-system does not necessarily contain the running Kernel, but
booted-system does.

Broken in ad78ce77647ebccfef86f44202efe7be9a671a24, which ignored the
discussion in https://github.com/NixOS/nixpkgs/pull/286330. It is still
broken on non-NixOS Linux.

pkgs/development/libraries/libvirt/default.nix
@@ -251,7 +251,7 @@ stdenv.mkDerivation rec {
         --replace '"/usr/bin/pkttyagent"' '"${if isLinux then polkit.bin else "/usr"}/bin/pkttyagent"'
 
       substituteInPlace src/util/virpci.c \
-         --replace '/lib/modules' '${if isLinux then "/run/current-system/kernel-modules" else ""}/lib/modules'
+         --replace '/lib/modules' '${if isLinux then "/run/booted-system/kernel-modules" else ""}/lib/modules'
 
       patchShebangs .
     ''