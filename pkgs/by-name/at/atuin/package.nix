ae216c9ccdad3e496b9213bc40ad016cea963942
0x4A6F <0x4A6F@users.noreply.github.com>
2024-02-10 18:06:49 +0100

    atuin: apply atuinsh/atuin#1694
    (cherry picked from commit 371e1506ed143ce1fe2e1851abd561de3db3b615)
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/by-name/at/atuin/package.nix
+++ pkgs/by-name/at/atuin/package.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , installShellFiles
 , rustPlatform
 , libiconv
@@ -18,6 +19,15 @@ rustPlatform.buildRustPackage rec {
     rev = "v${version}";
     hash = "sha256-2nBaGoaTd1TGm8aZnrNA66HkW7+OrD6gOmj+uSFz020=";
   };
+
+  patches = [
+    # https://github.com/atuinsh/atuin/pull/1694
+    (fetchpatch {
+      name = "0001-atuin_src_command_client_search_interactive.rs.patch";
+      url = "https://github.com/atuinsh/atuin/commit/6bc38f4cf3c8d2b6fbd135998a4e64e6abfb2566.patch";
+      hash = "sha256-pUiuECiAmq7nmKO/cOHZ1V5Iy3zDzZyBNNCH7Czo/NA=";
+    })
+  ];
 
   # TODO: unify this to one hash because updater do not support this
   cargoHash =
