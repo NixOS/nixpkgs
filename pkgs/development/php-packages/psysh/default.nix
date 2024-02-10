1509d1807dfd5d39911dca9e6184210229afc27c
Patka <patka@patka.dev>
2024-02-10 17:29:34 +0100

    phpPackages.psysh: 0.11.21 -> 0.12.0
    Release notes: https://github.com/bobthecow/psysh/releases/tag/v0.12.0

--- pkgs/development/php-packages/psysh/default.nix
+++ pkgs/development/php-packages/psysh/default.nix
@@ -2,23 +2,24 @@
 
 php.buildComposerProject (finalAttrs: {
   pname = "psysh";
-  version = "0.11.21";
+  version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "bobthecow";
     repo = "psysh";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-YuBn4mrgOzGeMGfGcyZySAISmQdv3WRGn91PRozyxdI=";
+    hash = "sha256-FoSqVnTEilKFqGxLZMMh2KNYWT8rLDUEmB4oekN7xIo=";
   };
 
   composerLock = ./composer.lock;
-  vendorHash = "sha256-FZFeO7UiVssxTf0JX6wdjrAE+jucYnfQJA1eOng39lQ=";
+  vendorHash = "sha256-e/ltORFvIsYmY0xU6cti8tqUgb+y+l8hhTJNIMAAP8I=";
 
   meta = {
     changelog = "https://github.com/bobthecow/psysh/releases/tag/v${finalAttrs.version}";
     description = "PsySH is a runtime developer console, interactive debugger and REPL for PHP.";
     license = lib.licenses.mit;
     homepage = "https://psysh.org/";
+    mainProgram = "psysh";
     maintainers = lib.teams.php.members;
   };
 })
