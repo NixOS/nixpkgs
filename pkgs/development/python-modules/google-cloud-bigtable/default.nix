af48d7949256b3b4f94f6c3338f78489d3b84017
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-10 02:13:34 +0000

    python311Packages.google-cloud-bigtable: 2.22.0 -> 2.23.0

--- pkgs/development/python-modules/google-cloud-bigtable/default.nix
+++ pkgs/development/python-modules/google-cloud-bigtable/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-bigtable";
-  version = "2.22.0";
+  version = "2.23.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Y6HO3Q5JZC4lSBJDbJxMlAHWnF3LzIDLY2ZpK1Abv6E=";
+    hash = "sha256-w8q7Zhn18u6hGz23JzHoTSixaAVcnMF8LS2v8Vy4I3k=";
   };
 
   propagatedBuildInputs = [
