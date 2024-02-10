40cdf4d54c27a0918ec3ded15112bebebd080431
Anderson Torres <torres.anderson.85@protonmail.com>
2024-02-10 10:10:22 -0300

    lightning: 2.2.2 -> 2.2.3
    - Enable disassembler

--- pkgs/by-name/li/lightning/package.nix
+++ pkgs/by-name/li/lightning/package.nix
@@ -1,23 +1,35 @@
 { lib
 , fetchurl
 , libopcodes
+, libiberty
 , stdenv
+, libbfd
+, zlib
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lightning";
-  version = "2.2.2";
+  version = "2.2.3";
 
   src = fetchurl {
     url = "mirror://gnu/lightning/lightning-${finalAttrs.version}.tar.gz";
-    hash = "sha256-CsqCQt6tF9YhF7z8sHjmqeqFbMgXQoE8noOUvM5zs+I=";
+    hash = "sha256-wEXHozoAr/v+sRBm+lAsA5kuR0piupWXeq0G28FMaCk=";
   };
 
   outputs = [ "out" "dev" "info" ];
 
-  nativeCheckInputs = [ libopcodes ];
+  buildInputs = [
+    libopcodes
+    libbfd
+    libiberty
+    zlib
+  ];
 
   strictDeps = true;
+
+  configureFlags = [
+    (lib.enableFeature true "disassembler")
+  ];
 
   doCheck = true;
 
