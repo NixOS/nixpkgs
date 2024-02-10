f8239b2b90d2cdf1323c715d6ac4b4b4aae43111
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2024-02-10 19:05:13 +0100

    rocmPackages: update source for rocm-docs-core

--- pkgs/development/rocm-modules/6/rocm-docs-core/default.nix
+++ pkgs/development/rocm-modules/6/rocm-docs-core/default.nix
@@ -23,14 +23,14 @@
 # FIXME: Move to rocmPackages_common
 buildPythonPackage rec {
   pname = "rocm-docs-core";
-  version = "0.26.0";
+  version = "0.34.0";
   format = "pyproject";
 
   src = fetchFromGitHub {
-    owner = "RadeonOpenCompute";
+    owner = "ROCm";
     repo = "rocm-docs-core";
     rev = "v${version}";
-    hash = "sha256-Mr6/Ne6P+TapoCqN7xkKMNse3fTaIAvvLmMl0kVg7Vs=";
+    hash = "sha256-p75g68Dn0RrjX9vYY+AWNu0qOKTLsBCnOZekMO0Usho=";
   };
 
   buildInputs = [ setuptools ];
@@ -58,7 +58,7 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "ROCm Documentation Python package for ReadTheDocs build standardization";
-    homepage = "https://github.com/RadeonOpenCompute/rocm-docs-core";
+    homepage = "https://github.com/ROCm/rocm-docs-core";
     license = with licenses; [ mit cc-by-40 ];
     maintainers = teams.rocm.members;
     platforms = platforms.linux;
