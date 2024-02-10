2feaab4a240b23f8c9de0c5c836bb3bea68a8410
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-02-10 02:39:37 +0100

    python311Packages.home-assistant-chip-clusters: 2023.12.0 -> 2024.1.0
    https://github.com/home-assistant-libs/chip-wheels/releases/tag/2024.1.0

--- pkgs/development/python-modules/home-assistant-chip-clusters/default.nix
+++ pkgs/development/python-modules/home-assistant-chip-clusters/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "home-assistant-chip-clusters";
-  version = "2023.12.0";
+  version = "2024.1.0";
   format = "wheel";
 
   src = fetchPypi {
@@ -15,7 +15,7 @@ buildPythonPackage rec {
     pname = "home_assistant_chip_clusters";
     dist = "py3";
     python = "py3";
-    hash = "sha256-4yAfbQBqHMEXWMwJ0kSDs0We/AsHweJ+Tc8aZiWi90w=";
+    hash = "sha256-4btkqAHbwAsyGV1LjngEoeTT5qyI8dtqFVTp0lIFwmg=";
   };
 
   propagatedBuildInputs = [
