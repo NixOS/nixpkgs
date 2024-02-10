066702962aa0683bf984e1273c90ae5975c3930b
superherointj <5861043+superherointj@users.noreply.github.com>
2024-02-10 11:11:26 -0300

    k3s: k3s_1_27 -> k3s_1_28
    Changelog & upgrade notes: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.28.md#changelog-since-v1270
    
    Tracking issue: https://github.com/NixOS/nixpkgs/issues/287344

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32797,7 +32797,7 @@ with pkgs;
   }) k3s_1_26 k3s_1_27 k3s_1_28;
   inherit (callPackage ../applications/networking/cluster/k3s { }) k3s_1_29;
 
-  k3s = k3s_1_27;
+  k3s = k3s_1_28;
 
   k3sup = callPackage ../applications/networking/cluster/k3sup { };
 
