01a39263ffc37b9d49f715de76b6248e93c2cb06
Tobias Mayer <tobim@fastmail.fm>
2024-02-11 17:44:54 +0100

    arrow-cpp: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20459,8 +20459,6 @@ with pkgs;
     cudaPackages = cudaPackages_12;
   };
 
-  arrow-cpp = callPackage ../development/libraries/arrow-cpp { };
-
   arrow-glib = callPackage ../development/libraries/arrow-glib { };
 
   arsenal = callPackage ../tools/security/arsenal { };
