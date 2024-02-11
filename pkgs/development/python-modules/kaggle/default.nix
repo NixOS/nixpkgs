bf28d854f618a463e6445b062a34cd32ec361ffc
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-11 10:38:47 +0000

    python312Packages.kaggle: 1.6.3 -> 1.6.5

--- pkgs/development/python-modules/kaggle/default.nix
+++ pkgs/development/python-modules/kaggle/default.nix
@@ -13,12 +13,12 @@
 
 buildPythonPackage rec {
   pname = "kaggle";
-  version = "1.6.3";
+  version = "1.6.5";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-J2FOzXJhO59Ya4cjE68WOK2MChfQx4rZ1KcYeb7NcuQ=";
+    sha256 = "sha256-m/i212ojvLwyfxXMIfxni6pzOVPc4BLseVwnTPrnRdA=";
   };
 
   propagatedBuildInputs = [
