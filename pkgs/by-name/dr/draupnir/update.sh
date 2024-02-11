24f76ff128c5e3d4d4ddffe4716e9fc38fcffb76
Rory& <root@rory.gay>
2024-02-11 01:55:33 +0100

    fixup! draupnir: init at 1.86.2 - updated

--- pkgs/by-name/dr/draupnir/update.sh
+++ pkgs/by-name/dr/draupnir/update.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p curl common-updater-scripts coreutils jq
+#!nix-shell -i bash -p curl common-updater-scripts coreutils jq prefetch-yarn-deps
 
 set -euo pipefail
 set -x
+
+TMPDIR=$(mktemp -d)
 
 echo "Getting versions..."
 latestVersion="$(curl -sL "https://api.github.com/repos/the-draupnir-project/Draupnir/releases?per_page=1" | jq -r '.[0].tag_name | ltrimstr("v")')"
@@ -17,13 +19,19 @@ else
 fi
 
 curl https://raw.githubusercontent.com/the-draupnir-project/Draupnir/v$latestVersion/package.json -o pkgs/by-name/dr/draupnir/package.json
-OLD_HASH=`nix-hash --type sha256 --flat --base32 --sri <(curl https://raw.githubusercontent.com/the-draupnir-project/Draupnir/v$currentVersion/yarn.lock)`
 
 update-source-version draupnir "$latestVersion"
 
-NEW_HASH=`nix-hash --type sha256 --flat --base32 --sri <(curl https://raw.githubusercontent.com/the-draupnir-project/Draupnir/v$latestVersion/yarn.lock)`
+# Update yarn offline cache hash
+cd $TMPDIR
 
-echo Got old yarn.lock hash: $OLD_HASH
-echo Got new yarn.lock hash: $NEW_HASH
+curl https://raw.githubusercontent.com/the-draupnir-project/Draupnir/v$latestVersion/yarn.lock -o yarn.lock
+TMP_PREFETCH_HASH=`prefetch-yarn-deps yarn.lock`
+NEW_YARN_OFFLINE_HASH=`nix hash to-sri --type sha256 $TMP_PREFETCH_HASH`
 
-sed -i 's/'${OLD_HASH/\//\\/}'/'${NEW_HASH/\//\\/}'/' pkgs/by-name/dr/draupnir/default.nix
+cd -
+echo "New yarn offline hash: $NEW_YARN_OFFLINE_HASH"
+
+TMPFILE=$(mktemp)
+jq '.yarn_offline_cache_hash = "'$NEW_YARN_OFFLINE_HASH'"' pkgs/by-name/dr/draupnir/hashes.json > $TMPFILE
+mv -- "$TMPFILE" pkgs/by-name/dr/draupnir/hashes.json
