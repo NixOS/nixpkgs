e38d3d6c37abdc5ddc1cde1c53a419fb461caa84
Maxine Aubrey <max@ine.dev>
2024-02-11 12:00:03 +0100

    vscode: drop self from maintainers

--- pkgs/applications/editors/vscode/vscode.nix
+++ pkgs/applications/editors/vscode/vscode.nix
@@ -97,7 +97,7 @@ in
       homepage = "https://code.visualstudio.com/";
       downloadPage = "https://code.visualstudio.com/Updates";
       license = licenses.unfree;
-      maintainers = with maintainers; [ eadwu synthetica amaxine bobby285271 Enzime ];
+      maintainers = with maintainers; [ eadwu synthetica bobby285271 Enzime ];
       platforms = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" "armv7l-linux" ];
     };
   }
