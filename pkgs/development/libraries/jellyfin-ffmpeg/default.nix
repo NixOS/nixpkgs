8f28deae45cc3e481382cac1ff1a329bc226f49c
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-12 21:36:50 +0000

    jellyfin-ffmpeg: 6.0.1-2 -> 6.0.1-3

--- pkgs/development/libraries/jellyfin-ffmpeg/default.nix
+++ pkgs/development/libraries/jellyfin-ffmpeg/default.nix
@@ -5,13 +5,13 @@
 
 ffmpeg_6-full.overrideAttrs (old: rec {
   pname = "jellyfin-ffmpeg";
-  version = "6.0.1-2";
+  version = "6.0.1-3";
 
   src = fetchFromGitHub {
     owner = "jellyfin";
     repo = "jellyfin-ffmpeg";
     rev = "v${version}";
-    hash = "sha256-wc9OGwjcRDTDxlHYVTlbLe1B/F11z0Xcz6WRrO42zn4=";
+    hash = "sha256-UINiXO61nB/AL0HJJy7G7emujakk/mQv81aUioyJz0Y=";
   };
 
   # Clobber upstream patches as they don't apply to the Jellyfin fork
