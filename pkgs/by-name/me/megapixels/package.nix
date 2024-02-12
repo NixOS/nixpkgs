a74abfa17d8164cae3d4ed0bed80ad69e86782a1
Luflosi <luflosi@luflosi.de>
2024-02-12 13:12:55 +0100

    megapixels: 1.7.0 -> 1.8.0
    https://gitlab.com/megapixels-org/Megapixels/-/tags/1.8.0

--- pkgs/by-name/me/megapixels/package.nix
+++ pkgs/by-name/me/megapixels/package.nix
@@ -29,13 +29,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "megapixels";
-  version = "1.7.0";
+  version = "1.8.0";
 
   src = fetchFromGitLab {
     owner = "megapixels-org";
     repo = "Megapixels";
     rev = finalAttrs.version;
-    hash = "sha256-ejTCYZMDkqz8P3vroq8XAl+pUGgcS56cm3tzOTE3rfc=";
+    hash = "sha256-J94kDSqQkfdltbYbzvJofd5o/Dhivtld475CaQs7+Ok=";
   };
 
   nativeBuildInputs = [
