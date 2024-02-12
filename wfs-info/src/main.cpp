01295a5c9667714e5f1f834f485edc9d23ab4986
koolkdev <koolkdev@gmail.com>
2024-02-13 01:35:50 +0200

    wfs-info: Dump the header of the allocator too

--- wfs-info/src/main.cpp
+++ wfs-info/src/main.cpp
@@ -16,7 +16,8 @@
 #include <vector>
 
 #include <wfslib/wfslib.h>
-#include "../../wfslib/src/area.h"  // TOOD: Public header?
+#include "../../wfslib/src/area.h"                   // TOOD: Public header?
+#include "../../wfslib/src/free_blocks_allocator.h"  // TOOD: Public header?
 
 std::string inline pretify_path(const std::filesystem::path& path) {
   return "/" + path.generic_string();
@@ -24,14 +25,26 @@ std::string inline pretify_path(const std::filesystem::path& path) {
 
 constexpr int blocks_counts[] = {0, 3, 6, 10, 14, 18, 22, 26};
 
-void dumpArea(int depth, const std::filesystem::path& path, const std::shared_ptr<Area>& area) {
+void dumpArea(int depth, const std::filesystem::path& path, const std::shared_ptr<const Area>& area) {
   std::cout << std::setw(depth) << std::setfill('\t') << ""
             << "Area " << pretify_path(path) << " [0x" << std::hex << std::setw(8) << std::setfill('0')
             << area->BlockNumber() << "-0x" << std::hex << std::setw(8) << std::setfill('0')
             << area->AbsoluteBlockNumber(area->BlocksCount()) << "]:" << std::endl;
-  const auto& tree = area->GetFreeBlocksAllocatorTree();
+  auto allocator = area->GetFreeBlocksAllocator();
+  std::cout << std::setw(depth + 1) << std::setfill('\t') << ""
+            << "Free blocks: " << std::hex << std::setw(8) << std::setfill('0')
+            << allocator->header()->free_blocks_count.value() << std::endl;
+  std::cout << std::setw(depth + 1) << std::setfill('\t') << ""
+            << "Free metadata blocks: " << std::hex << std::setw(8) << std::setfill('0')
+            << allocator->header()->free_metadata_blocks_count.value() << std::endl;
+  std::cout << std::setw(depth + 1) << std::setfill('\t') << ""
+            << "Free metadata block: " << std::hex << std::setw(8) << std::setfill('0')
+            << area->AbsoluteBlockNumber(allocator->header()->free_metadata_block.value()) << std::endl;
+  std::cout << std::setw(depth + 1) << std::setfill('\t') << ""
+            << "Unknown: " << std::hex << std::setw(8) << std::setfill('0') << allocator->header()->unknown.value()
+            << std::endl;
   std::map<uint32_t, uint32_t> free_ranges;
-  for (const auto& [tree_block_number, free_tree] : tree) {
+  for (const auto& [tree_block_number, free_tree] : allocator->tree()) {
     int size = 0;
     for (const auto& free_tree_per_size : free_tree) {
       for (const auto& [block_number, blocks_count] : free_tree_per_size) {
@@ -55,9 +68,11 @@ void dumpArea(int depth, const std::filesystem::path& path, const std::shared_pt
       ++current;
     }
   }
+  std::cout << std::setw(depth + 1) << std::setfill('\t') << ""
+            << "Free ranges:" << std::endl;
   for (const auto& [start_block, end_block] : free_ranges) {
-    std::cout << std::setw(depth + 1) << std::setfill('\t') << ""
-              << "Free range [0x" << std::hex << std::setw(8) << std::setfill('0') << start_block << "-0x" << std::hex
+    std::cout << std::setw(depth + 2) << std::setfill('\t') << ""
+              << "[0x" << std::hex << std::setw(8) << std::setfill('0') << start_block << "-0x" << std::hex
               << std::setw(8) << std::setfill('0') << end_block << "]" << std::endl;
   }
 }
@@ -143,7 +158,7 @@ int main(int argc, char* argv[]) {
     }
     auto device = std::make_shared<FileDevice>(vm["input"].as<std::string>(), 9);
     Wfs::DetectDeviceSectorSizeAndCount(device, key);
-    std::cout << "Free blocks:" << std::endl;
+    std::cout << "Allocator state:" << std::endl;
     dumpdir(0, Wfs(device, key).GetRootArea()->GetRootDirectory(), {});
     std::cout << "Done!" << std::endl;
   } catch (std::exception& e) {
