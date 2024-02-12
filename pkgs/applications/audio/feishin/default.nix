6ebb5c26d055d33331f0368eea6102a58def1fb9
Robert Schütz <nix@dotlambda.de>
2024-02-11 20:35:34 -0800

    feishin: build from source

--- pkgs/applications/audio/feishin/default.nix
+++ pkgs/applications/audio/feishin/default.nix
@@ -1,27 +1,67 @@
 { lib
-, stdenv
-, callPackage
-, ...
-}@args:
+, buildNpmPackage
+, fetchFromGitHub
+, electron_25
+}:
 
 let
-  extraArgs = removeAttrs args [ "callPackage" ];
-
+  electron = electron_25;
+in buildNpmPackage rec {
   pname = "feishin";
   version = "0.5.1";
-  appname = "Feishin";
+
+  src = fetchFromGitHub {
+    owner = "jeffvli";
+    repo = "feishin";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-7L1KufMiwqWgTvv7E1bDNL+epvNb5iLXI4Gee8w17qs=";
+  };
+
+  npmDepsHash = "sha256-TuNkVhNNOB23QnMXiGBWhDI0JXWnWdfI9MLvMq5xzJ8=";
+
+  postPatch = ''
+    substituteInPlace package.json \
+      --replace-fail "electron-builder install-app-deps &&" ""
+  '';
+
+  npmFlags = [ "--legacy-peer-deps" ];
+
+  makeCacheWritable = true;
+
+  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+
+  postBuild = ''
+    npm exec electron-builder -- \
+      --dir \
+      -c.electronDist=${electron}/libexec/electron \
+      -c.electronVersion=${electron.version} \
+      -c.npmRebuild=false
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    pushd release/build/*/
+    mkdir -p $out/opt/feishin
+    cp -r locales resources{,.pak} $out/opt/feishin
+    popd
+
+    makeWrapper ${lib.getExe electron} $out/bin/feishin \
+      --add-flags $out/opt/feishin/resources/app.asar \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+      --inherit-argv0
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "Full-featured Subsonic/Jellyfin compatible desktop music player";
     homepage = "https://github.com/jeffvli/feishin";
     changelog = "https://github.com/jeffvli/feishin/releases/tag/v${version}";
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    sourceProvenance = with sourceTypes; [ fromSource ];
     license = licenses.mit;
-    platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    platforms = platforms.unix;
+    mainProgram = "feishin";
     maintainers = with maintainers; [ onny ];
   };
-
-in
-if stdenv.isDarwin
-then callPackage ./darwin.nix (extraArgs // { inherit pname appname version meta; })
-else callPackage ./linux.nix (extraArgs // { inherit pname appname version meta; })
+}
