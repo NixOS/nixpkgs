5c8b72fd75ef6db82de00a1f6e53c84ba8ab9271
Luflosi <luflosi@luflosi.de>
2024-02-12 12:51:48 +0100

    megapixels: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33404,8 +33404,6 @@ with pkgs;
 
   mediathekview = callPackage ../applications/video/mediathekview { jre = temurin-bin-20; };
 
-  megapixels = callPackage ../applications/graphics/megapixels { };
-
   meteo = callPackage ../applications/networking/weather/meteo { };
 
   meld = callPackage ../applications/version-management/meld { };
