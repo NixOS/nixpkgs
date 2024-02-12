4fad76301b822a00c6cfa77ca0e1489e42ce06d2
Dawid Dziurla <dawidd0811@gmail.com>
2024-02-12 11:35:21 +0100

    molecule: add version test

--- pkgs/development/python-modules/molecule/default.nix
+++ pkgs/development/python-modules/molecule/default.nix
@@ -1,11 +1,13 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, testers
 , ansible-compat
 , ansible-core
 , click-help-colors
 , enrich
 , jsonschema
+, molecule
 , withPlugins ? true, molecule-plugins
 , packaging
 , pluggy
@@ -50,6 +52,14 @@ buildPythonPackage rec {
 
   # tests can't be easily run without installing things from ansible-galaxy
   doCheck = false;
+
+  passthru.tests.version = (testers.testVersion {
+    package = molecule;
+    command = "PY_COLORS=0 ${pname} --version";
+  }).overrideAttrs (old: {
+    # workaround the error: Permission denied: '/homeless-shelter'
+    HOME = "$(mktemp -d)";
+  });
 
   meta = with lib; {
     description = "Molecule aids in the development and testing of Ansible roles";
