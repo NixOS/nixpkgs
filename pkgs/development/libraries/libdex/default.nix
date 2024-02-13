b8c57f5318b8d7ff46f67680a018568cde95071c
Bobby Rong <rjl931189261@126.com>
2024-02-13 11:27:34 +0000

    libdex: 0.4.3 → 0.5.1
    https://gitlab.gnome.org/GNOME/libdex/-/compare/0.4.3...0.5.1

--- pkgs/development/libraries/libdex/default.nix
+++ pkgs/development/libraries/libdex/default.nix
@@ -14,7 +14,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libdex";
-  version = "0.4.3";
+  version = "0.5.1";
 
   outputs = [ "out" "dev" "devdoc" ];
 
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
     owner = "GNOME";
     repo = "libdex";
     rev = version;
-    sha256 = "0GNlgJgAOE3JGwu/6Zsh4sjFapA7nUcGD3lgZZJ0BfQ=";
+    sha256 = "sha256-lencXKLUlhs6LnVb5C9q67oVhfyhfUBWAH/Rhmw5btw=";
   };
 
   nativeBuildInputs = [
