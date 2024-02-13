84a514ad67ef4ef1f057575c77ae319068edfbbb
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-13 12:59:27 +0000

    qownnotes: 24.2.0 -> 24.2.1

--- pkgs/applications/office/qownnotes/default.nix
+++ pkgs/applications/office/qownnotes/default.nix
@@ -19,14 +19,14 @@
 let
   pname = "qownnotes";
   appname = "QOwnNotes";
-  version = "24.2.0";
+  version = "24.2.1";
 in
 stdenv.mkDerivation {
   inherit pname version;
 
   src = fetchurl {
     url = "https://github.com/pbek/QOwnNotes/releases/download/v${version}/qownnotes-${version}.tar.xz";
-    hash = "sha256-mk7yFlL+NiTZ0JtSY3y/Y1NrN1QYcBxveMImv1zB1l8=";
+    hash = "sha256-aDpWXfUIfLjDhEyw6xyuYrR4ZQockFv+JbC1tvMF6ZQ=";
   };
 
   nativeBuildInputs = [
