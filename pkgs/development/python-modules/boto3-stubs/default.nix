99cc53834557463654cfad3eab64e0b245e229c5
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-13 14:19:46 +0100

    python311Packages.boto3-stubs: 1.34.39 -> 1.34.40

--- pkgs/development/python-modules/boto3-stubs/default.nix
+++ pkgs/development/python-modules/boto3-stubs/default.nix
@@ -365,14 +365,14 @@
 
 buildPythonPackage rec {
   pname = "boto3-stubs";
-  version = "1.34.39";
+  version = "1.34.40";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ywCmGzid3oVaJllil8xm99ahgHUpi7iyA4GLOWOx0VM=";
+    hash = "sha256-rQpnx4/mR80rYR8/gtryVp4WQ3gdU8vIafGAYuF3rig=";
   };
 
   nativeBuildInputs = [
