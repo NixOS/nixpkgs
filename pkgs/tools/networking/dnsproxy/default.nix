3591e6a801a74200c7b96d04dbc8c84c031dd850
Marton Csutora <marton@csutora.com>
2024-02-13 23:44:48 +0100

    dnsproxy: 0.54.0 -> 0.65.0

--- pkgs/tools/networking/dnsproxy/default.nix
+++ pkgs/tools/networking/dnsproxy/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "dnsproxy";
-  version = "0.54.0";
+  version = "0.65.0";
 
   src = fetchFromGitHub {
     owner = "AdguardTeam";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-wA88v8zF5sq3NQwKh8g4k/COviuaegGn6bBTzBMcdGM=";
+    sha256 = "sha256-TE33gwEfOLWa32lHM7gWsrtUsVX0B5pWtxMfyD36AyQ=";
   };
 
   vendorHash = null;
@@ -18,7 +18,7 @@ buildGoModule rec {
   doCheck = false;
 
   meta = with lib; {
-    description = "Simple DNS proxy with DoH, DoT, and DNSCrypt support";
+    description = "Simple DNS proxy with DoH, DoT, DoQ and DNSCrypt support";
     homepage = "https://github.com/AdguardTeam/dnsproxy";
     license = licenses.asl20;
     maintainers = with maintainers; [ contrun ];
