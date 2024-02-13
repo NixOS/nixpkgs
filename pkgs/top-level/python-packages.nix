731ef3d4c489d5bafd6a5de35e2e305e9ba42240
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-13 11:52:50 +0100

    python311Packages.pytest-textual-snapshot: init at 0.4.0
    Snapshot testing for Textual applications
    
    https://github.com/Textualize/pytest-textual-snapshot

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11717,6 +11717,8 @@ self: super: with self; {
 
   pytest-testmon = callPackage ../development/python-modules/pytest-testmon { };
 
+  pytest-textual-snapshot = callPackage ../development/python-modules/pytest-textual-snapshot { };
+
   pytest-timeout = callPackage ../development/python-modules/pytest-timeout { };
 
   pytest-tornado = callPackage ../development/python-modules/pytest-tornado { };
