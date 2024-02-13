282f0f561ceb29621dd4f769c88cfe276b76b245
Bobby Rong <rjl931189261@126.com>
2024-02-13 11:25:25 +0000

    epiphany: 45.2 → 46.rc
    https://gitlab.gnome.org/GNOME/epiphany/-/compare/45.2...46.rc
    
    Remove Safe Browsing support
    https://github.com/GNOME/epiphany/commit/4cfb37b4a6690503cf4f2264ed602a582b73668a
    
    Add PKCS11 (smartcard) support
    https://github.com/GNOME/epiphany/commit/d1f428e9bdedea625903e4a83ca039d976a96d99

--- pkgs/desktops/gnome/core/epiphany/default.nix
+++ pkgs/desktops/gnome/core/epiphany/default.nix
@@ -36,11 +36,11 @@
 
 stdenv.mkDerivation rec {
   pname = "epiphany";
-  version = "45.2";
+  version = "46.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "eccUYL/+/M715nvj+1/KZXhT6CFstiY5nSuVDOAyDdw=";
+    sha256 = "sha256-V8EitGHPDOSPrFkOpQ2T5bY9ZbWAzTuPgS/h/XXTCls=";
   };
 
   nativeBuildInputs = [
