e63302016ffe2d77db473482facdf6ca15315eef
Mario Rodas <marsam@users.noreply.github.com>
2024-02-13 04:20:00 +0000

    ocamlPackages.luv: 0.5.12 -> 0.5.13
    Diff: https://github.com/aantron/luv/compare/0.5.12...0.5.13
    
    Changelog: https://github.com/aantron/luv/releases/tag/0.5.13

--- pkgs/development/ocaml-modules/luv/default.nix
+++ pkgs/development/ocaml-modules/luv/default.nix
@@ -6,13 +6,13 @@
 
 buildDunePackage rec {
   pname = "luv";
-  version = "0.5.12";
+  version = "0.5.13";
 
   minimalOCamlVersion = "4.03";
 
   src = fetchurl {
     url = "https://github.com/aantron/luv/releases/download/${version}/luv-${version}.tar.gz";
-    sha256 = "sha256-dp9qCIYqSdROIAQ+Jw73F3vMe7hnkDe8BgZWImNMVsA=";
+    hash = "sha256-WaYWvOZYG9ftQn3WpkD/NZ8qVO7bXetf7rNUMXVsIlI=";
   };
 
   postConfigure = ''
@@ -30,6 +30,7 @@ buildDunePackage rec {
   meta = with lib; {
     homepage = "https://github.com/aantron/luv";
     description = "Binding to libuv: cross-platform asynchronous I/O";
+    changelog = "https://github.com/aantron/luv/releases/tag/${version}";
     # MIT-licensed, extra licenses apply partially to libuv vendor
     license = with licenses; [ mit bsd2 bsd3 cc-by-sa-40 ];
     maintainers = with maintainers; [ locallycompact sternenseemann ];
