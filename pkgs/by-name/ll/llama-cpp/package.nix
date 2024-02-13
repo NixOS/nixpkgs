aee29286145f3ea703707d997e30d7b9d5e1b159
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-13 10:34:27 +0000

    llama-cpp: 2105 -> 2135

--- pkgs/by-name/ll/llama-cpp/package.nix
+++ pkgs/by-name/ll/llama-cpp/package.nix
@@ -31,13 +31,13 @@ let
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "llama-cpp";
-  version = "2105";
+  version = "2135";
 
   src = fetchFromGitHub {
     owner = "ggerganov";
     repo = "llama.cpp";
     rev = "refs/tags/b${finalAttrs.version}";
-    hash = "sha256-Xq/P7EN6dz2oW++bXhIMY7AhWgVk6hmuf4PmEaoVgMM=";
+    hash = "sha256-rYK5YaqzJXlAaSNXz37cDMhAr+OEGprfzTdtpd4/6jY=";
   };
 
   postPatch = ''
