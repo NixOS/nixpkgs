1e4ce2c9695a002300ea62ab3e0a0af28a274548
ocfox <i@ocfox.me>
2024-02-14 23:17:34 +0800

    speedtest-rs: fix darwin build

--- pkgs/tools/networking/speedtest-rs/default.nix
+++ pkgs/tools/networking/speedtest-rs/default.nix
@@ -19,7 +19,10 @@ rustPlatform.buildRustPackage rec {
   };
 
   buildInputs = [ openssl ] ++
-    lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+    lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
 
   nativeBuildInputs = [ pkg-config ];
 
