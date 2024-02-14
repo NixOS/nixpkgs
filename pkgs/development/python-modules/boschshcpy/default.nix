51e0677d8bd313b0359d6be9353100292cc70c2b
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-14 22:39:19 +0100

    python311Packages.boschshcpy: 0.2.89 -> 0.2.90
    Diff: https://github.com/tschamm/boschshcpy/compare/refs/tags/0.2.89...0.2.90

--- pkgs/development/python-modules/boschshcpy/default.nix
+++ pkgs/development/python-modules/boschshcpy/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "boschshcpy";
-  version = "0.2.89";
+  version = "0.2.90";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "tschamm";
     repo = "boschshcpy";
     rev = "refs/tags/${version}";
-    hash = "sha256-/BZz666b2qZ6Dzkw+l1OpoMP+MIsFzhohNutzZMooNQ=";
+    hash = "sha256-qI8fpQJ7fyZ6CX010cyPuoFj9UQM+jHOJ201GCjIwBU=";
   };
 
   nativeBuildInputs = [
