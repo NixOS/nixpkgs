56b61a15e66a2c986ea88a3b0b173f203b6516da
Sergei Trofimovich <slyich@gmail.com>
2024-02-14 22:22:48 +0000

    btrfs-progs: 6.7 -> 6.7.1
    Changes: https://github.com/kdave/btrfs-progs/releases/tag/v6.7.1

--- pkgs/tools/filesystems/btrfs-progs/default.nix
+++ pkgs/tools/filesystems/btrfs-progs/default.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation rec {
   pname = "btrfs-progs";
-  version = "6.7";
+  version = "6.7.1";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    hash = "sha256-wn91UYW58tqzH0LoowPTa+0qPzNBzG117migplCiR2c=";
+    hash = "sha256-JNx7l08KV7oOyoD5dEC4QN+oWw8cssAb39l2WaSAsgA=";
   };
 
   nativeBuildInputs = [
