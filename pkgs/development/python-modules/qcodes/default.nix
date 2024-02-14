5a98ac4bf055cb08d8771dffd5b4ee484431ef7c
annalee <150648636+a-n-n-a-l-e-e@users.noreply.github.com>
2024-02-14 01:11:00 +0000

    python311Packages.qcodes: disable flaky tests
    disable flaky tests
    use flags similar to the upstream workflow
    -m "not serial" --hypothesis-profile ci

--- pkgs/development/python-modules/qcodes/default.nix
+++ pkgs/development/python-modules/qcodes/default.nix
@@ -119,6 +119,8 @@ buildPythonPackage rec {
     "-n"
     "$NIX_BUILD_CORES"
     # Follow upstream with settings
+    "-m 'not serial'"
+    "--hypothesis-profile ci"
     "--durations=20"
   ];
 
@@ -141,6 +143,11 @@ buildPythonPackage rec {
     "test_get_array_in_scalar_param_data"
     "test_get_parameter_data"
     "test_ramp_safely"
+
+    # more flaky tests
+    # https://github.com/microsoft/Qcodes/issues/5551
+    "test_query_close_once_at_init"
+    "test_step_ramp"
   ];
 
   pythonImportsCheck = [
