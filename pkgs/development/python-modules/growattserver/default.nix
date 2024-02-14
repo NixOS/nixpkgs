71a06d6afb938071b9d8b979c7d14d019d6e348a
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-14 22:42:44 +0100

    python311Packages.growattserver: 1.4.0 -> 1.5.0
    Diff: https://github.com/indykoning/PyPi_GrowattServer/compare/refs/tags/1.4.0...1.5.0
    
    Changelog: https://github.com/indykoning/PyPi_GrowattServer/releases/tag/1.5.0

--- pkgs/development/python-modules/growattserver/default.nix
+++ pkgs/development/python-modules/growattserver/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "growattserver";
-  version = "1.4.0";
+  version = "1.5.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "indykoning";
     repo = "PyPi_GrowattServer";
     rev = "refs/tags/${version}";
-    hash = "sha256-V0EW3I0FIDx9urbxX/zh3A51B/BiDqUfsrKbKU9FKiE=";
+    hash = "sha256-ATxXjIF5QRsdLuXZCOWMwvbBzawrhlYZ+wodITz36sE=";
   };
 
   nativeBuildInputs = [
