3dd8ef3a2cb6997a5e58f5314306a871dd4b1839
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-15 14:42:34 +0100

    python311Packages.google-cloud-vision: 3.6.0 -> 3.7.0
    Changelog: https://github.com/googleapis/python-vision/blob/v3.7.0/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-vision/default.nix
+++ pkgs/development/python-modules/google-cloud-vision/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-vision";
-  version = "3.6.0";
+  version = "3.7.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-uaaL/CbR4RM4YFU0xd/naGn3xSiLuRqI0/qyZNLKJ3Q=";
+    hash = "sha256-uHIhWpihTxcV28zv3OZrGf4mj26aBaR2isycRMtMeNM=";
   };
 
   nativeBuildInputs = [
