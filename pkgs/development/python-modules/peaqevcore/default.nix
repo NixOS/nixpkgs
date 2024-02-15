4aa83a8892abd41219164ca4d2005e3c6f35a8b2
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-15 16:11:13 +0000

    python312Packages.peaqevcore: 19.6.6 -> 19.6.9

--- pkgs/development/python-modules/peaqevcore/default.nix
+++ pkgs/development/python-modules/peaqevcore/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "peaqevcore";
-  version = "19.6.6";
+  version = "19.6.9";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-4T30jiMRAbPDgGW8Zcolj1k3UpKN1/juXtYSHrjO804=";
+    hash = "sha256-Wu8eNNVUF73WXt1oc/nZ3DnojmMJkIqlULY3oZ/K45o=";
   };
 
   postPatch = ''
