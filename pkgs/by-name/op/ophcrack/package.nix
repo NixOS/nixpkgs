dfec011babcf91b77a0105486b0110c68b48fb39
Tochiaha <tochipreciousahan@gmail.com>
2024-02-15 07:55:50 +0000

    ophcrack: init at 3.8.0

--- pkgs/by-name/op/ophcrack/package.nix
+++ pkgs/by-name/op/ophcrack/package.nix
@@ -35,7 +35,6 @@ stdenv.mkDerivation rec {
  buildInputs = [ pkg-config zlib openssl freetype fontconfig libpthreadstubs gcc expat qtcharts qtbase ];
 
  configPhase = ''
-   CFLAGS="-I/usr/include/libxml2/libxml/"
     ./config
  '';
 
