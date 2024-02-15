427d84591b5e90e6225b559ff663df4827e25d67
Phil Dyer <phildyer@protonmail.com>
2024-02-15 15:10:41 +1000

    ryzen_smu: remove `with lib;` and `lib.mdDoc`

--- nixos/modules/hardware/cpu/amd-ryzen-smu.nix
+++ nixos/modules/hardware/cpu/amd-ryzen-smu.nix
@@ -1,18 +1,20 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
-
+{ config
+, lib
+, pkgs
+, ...
+}:
 let
+  inherit (lib) mkEnableOption mkIf;
   cfg = config.hardware.cpu.amd.ryzen_smu;
   ryzen_smu = config.boot.kernelPackages.ryzen_smu;
 in
 {
   options.hardware.cpu.amd.ryzen_smu = {
-    enable = mkEnableOption (lib.mdDoc ''
+    enable = mkEnableOption ''
         A linux kernel driver that exposes access to the SMU (System Management Unit) for certain AMD Ryzen Processors.
 
         WARNING: Damage cause by use of your AMD processor outside of official AMD specifications or outside of factory settings are not covered under any AMD product warranty and may not be covered by your board or system manufacturer's warranty.
-      '');
+      '';
   };
 
   config = mkIf cfg.enable {
@@ -21,6 +23,6 @@ in
     environment.systemPackages = [ ryzen_smu ];
   };
 
-  meta.maintainers = with maintainers; [ Cryolitia phdyellow ];
+  meta.maintainers = with lib.maintainers; [ Cryolitia phdyellow ];
 
 }
