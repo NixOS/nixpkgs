8aac2900868755b11f210550aa25da1ae8cd23b8
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-15 16:57:01 +0100

    python311Packages.google-cloud-speech: 2.24.0 -> 2.24.1
    Changelog: https://github.com/googleapis/google-cloud-python/blob/google-cloud-speech-v2.24.1/packages/google-cloud-speech/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-speech/default.nix
+++ pkgs/development/python-modules/google-cloud-speech/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-speech";
-  version = "2.24.0";
+  version = "2.24.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Z7x4xfCbeRPoCbinCrTwIAp9JIvfavFvA1c4/a5UbHQ=";
+    hash = "sha256-aPW5m1cZNXpbiWCUQs104QH+/rVueanW0yE5TCdgWoQ=";
   };
 
   nativeBuildInputs = [
