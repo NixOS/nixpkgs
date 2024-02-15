ce7adb389e53bf4654e841335d7066bc2b4cf51c
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-15 13:02:41 +0100

    python311Packages.google-cloud-videointelligence: 2.13.0 -> 2.13.1
    Changelog: https://github.com/googleapis/google-cloud-python/blob/google-cloud-videointelligence-v2.13.1/packages/google-cloud-videointelligence/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-videointelligence/default.nix
+++ pkgs/development/python-modules/google-cloud-videointelligence/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-videointelligence";
-  version = "2.13.0";
+  version = "2.13.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ODRF08xxjUrj6PMzfTlcYLqGrgV71OA0VHT44ktQZ4k=";
+    hash = "sha256-nZQVoV6ciXHZfbr26U4PKFU/sHJPoZNB5j7covnE9aw=";
   };
 
   propagatedBuildInputs = [
