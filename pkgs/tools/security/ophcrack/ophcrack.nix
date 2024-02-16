1f01bb1ae5fc05dd7fe225d3f228d64dc970c2b1
Tochiaha <tochipreciousahan@gmail.com>
2024-02-16 05:42:50 +0000

    ophcrack: init at 3.8.0

---
+++ pkgs/tools/security/ophcrack/ophcrack.nix
@@ -0,0 +1,58 @@
+{ lib
+, stdenv
+, fetchFromGitLab
+, autoreconfHook
+, libtool
+, zlib
+, openssl
+, freetype
+, fontconfig
+, libpthreadstubs
+, gcc
+, makeWrapper
+, pkg-config
+, expat
+, qtcharts
+, wrapQtAppsHook
+, cmake
+}:
+
+stdenv.mkDerivation rec {
+  pname = "ophcrack";
+  version = "3.8.0-3";
+
+  src = fetchFromGitLab {
+    domain = "salsa.debian.org";
+    owner = "pkg-security-team";
+    repo = "ophcrack";
+    rev = "debian/${version}";
+    hash = "sha256-94zEr7aBeqMjy5Ma50W3qv1S5yx090bYuTieoZaXFcc=";
+  };
+
+ buildInputs = [ pkg-config zlib openssl freetype fontconfig libpthreadstubs gcc expat qtcharts ];
+ 
+ nativeBuildInputs = [ autoreconfHook libtool wrapQtAppsHook];
+
+ configPhase = ''
+   CFLAGS="-I/usr/include/libxml2/libxml"
+    ./configure
+ '';
+
+ buildPhase = ''
+   make
+ '';
+
+ installPhase = ''
+   make install
+ '';
+
+  meta = with lib; {
+    description = "Ophcrack packaging";
+    homepage = "https://salsa.debian.org/pkg-security-team/ophcrack";
+    changelog = "https://salsa.debian.org/pkg-security-team/ophcrack/-/blob/${src.rev}/ChangeLog";
+    license = with licenses; [ openssl gpl2Only ];
+    maintainers = with maintainers; [ tochiaha ];
+    mainProgram = "ophcrack";
+    platforms = platforms.all;
+  };
+}
