fc342ced463361607dce02644ca65a321ea8a64e
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-16 10:54:00 +0100

    python311Packages.msgraph-sdk: init at 1.1.0
    Microsoft Graph SDK for Python
    
    https://github.com/microsoftgraph/msgraph-sdk-python

---
+++ pkgs/development/python-modules/msgraph-sdk/default.nix
@@ -0,0 +1,57 @@
+{ lib
+, azure-identity
+, buildPythonPackage
+, fetchFromGitHub
+, microsoft-kiota-abstractions
+, microsoft-kiota-authentication-azure
+, microsoft-kiota-http
+, microsoft-kiota-serialization-json
+, microsoft-kiota-serialization-text
+, msgraph-core
+, pythonOlder
+, setuptools
+}:
+
+buildPythonPackage rec {
+  pname = "msgraph-sdk";
+  version = "1.1.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
+
+  src = fetchFromGitHub {
+    owner = "microsoftgraph";
+    repo = "msgraph-sdk-python";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-fAchReqVhkVhT48UrTnBUQerHmgB7qxpey0xrgxIVDs=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    azure-identity
+    microsoft-kiota-abstractions
+    microsoft-kiota-authentication-azure
+    microsoft-kiota-http
+    microsoft-kiota-serialization-json
+    microsoft-kiota-serialization-text
+    msgraph-core
+  ];
+
+  # Module doesn't have tests
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "msgraph"
+  ];
+
+  meta = with lib; {
+    description = "Microsoft Graph SDK for Python";
+    homepage = "https://github.com/microsoftgraph/msgraph-sdk-python";
+    changelog = "https://github.com/microsoftgraph/msgraph-sdk-python/blob/${version}/CHANGELOG.md";
+    license = licenses.mit;
+    maintainers = with maintainers; [ fab ];
+  };
+}
