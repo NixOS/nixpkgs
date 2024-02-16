8831cd4b052a95bb3aee7017234d1f7b274e0b55
hacker1024 <hacker1024@users.sourceforge.net>
2024-02-16 12:33:31 +1100

    python3Packages.babeltrace, python3Packages.babeltrace2: Explain use of PYTHON environment variable

--- pkgs/development/python-modules/babeltrace/default.nix
+++ pkgs/development/python-modules/babeltrace/default.nix
@@ -9,6 +9,10 @@ toPythonModule (babeltrace.overrideAttrs ({ nativeBuildInputs ? [ ], configureFl
 
   configureFlags = configureFlags ++ [ "--enable-python-bindings" ];
 
+  # Nix treats nativeBuildInputs specially for cross-compilation, but in this
+  # case, cross-compilation is accounted for explicitly. Using the variables
+  # ensures that the platform setup isn't messed with further. It also allows
+  # regular Python to be added in the future if it is ever needed.
   PYTHON = "${python.pythonOnBuildForHost}/bin/python";
   PYTHON_CONFIG = "${python.pythonOnBuildForHost}/bin/python-config";
 }))
--- pkgs/development/python-modules/babeltrace2/default.nix
+++ pkgs/development/python-modules/babeltrace2/default.nix
@@ -12,6 +12,10 @@ toPythonModule (babeltrace2.overrideAttrs ({ nativeBuildInputs ? [ ], configureF
     "--enable-python-plugins"
   ];
 
+  # Nix treats nativeBuildInputs specially for cross-compilation, but in this
+  # case, cross-compilation is accounted for explicitly. Using the variables
+  # ensures that the platform setup isn't messed with further. It also allows
+  # regular Python to be added in the future if it is ever needed.
   PYTHON = "${python.pythonOnBuildForHost}/bin/python";
   PYTHON_CONFIG = "${python.pythonOnBuildForHost}/bin/python-config";
 }))
