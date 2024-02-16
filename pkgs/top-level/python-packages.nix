07538cfddf00780802f0780f1fae6e805deeccfe
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-16 08:52:22 +0100

    python311Packages.pyprobables: init at 0.6.0
    Probabilistic data structures
    
    https://github.com/barrust/pyprobables

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9327,6 +9327,8 @@ self: super: with self; {
 
   pyprecice = callPackage ../development/python-modules/pyprecice { };
 
+  pyprobables = callPackage ../development/python-modules/pyprobables { };
+
   pyproject-api = callPackage ../development/python-modules/pyproject-api { };
 
   pyproject-hooks = callPackage ../development/python-modules/pyproject-hooks { };
