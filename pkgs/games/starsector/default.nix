b26028e5c383fdaa4f079667f79a271c2d305ac9
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-16 13:47:51 +0000

    starsector: 0.97a-RC9 -> 0.97a-RC10

--- pkgs/games/starsector/default.nix
+++ pkgs/games/starsector/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "starsector";
-  version = "0.97a-RC9";
+  version = "0.97a-RC10";
 
   src = fetchzip {
     url = "https://f005.backblazeb2.com/file/fractalsoftworks/release/starsector_linux-${version}.zip";
-    sha256 = "sha256-xX4QVr7vmVX+/qvKALYZOE/Wy+d+zpNWCnpQE1kBd7M=";
+    sha256 = "sha256-GTzFY7iUB0xtUk9di8LKAgZwCOjqvv2j5QFSUKAfNDU=";
   };
 
   nativeBuildInputs = [ copyDesktopItems makeWrapper ];
