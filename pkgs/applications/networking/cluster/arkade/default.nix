21c812a03545fb41e46d8aa749d47887d7dbb70f
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-16 01:30:53 +0000

    arkade: 0.10.20 -> 0.11.0

--- pkgs/applications/networking/cluster/arkade/default.nix
+++ pkgs/applications/networking/cluster/arkade/default.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "arkade";
-  version = "0.10.20";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "alexellis";
     repo = "arkade";
     rev = version;
-    hash = "sha256-aOJ07Ks+J7cDvF3InKGYx4dJAql9ltJQqa8P+4cKhNk=";
+    hash = "sha256-SNYaUbWj8t73Aaamg2SOu5EBiYjDMHCiXlaERqGmr2A=";
   };
 
   CGO_ENABLED = 0;
