db80fe791ca8a2ca8a39af9ca590dad459ca492c
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-16 05:08:26 +0000

    atmos: 1.60.0 -> 1.63.0

--- pkgs/applications/networking/cluster/atmos/default.nix
+++ pkgs/applications/networking/cluster/atmos/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "atmos";
-  version = "1.60.0";
+  version = "1.63.0";
 
   src = fetchFromGitHub {
     owner = "cloudposse";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-sc0tKOv5J4lF00Zzs2X6ff1EC1xrTRbDvVopq+1Wn6Y=";
+    sha256 = "sha256-3XmRTboq8DSIV4w6EFsbgl21eY3QA5DicE9nM5sAZOQ=";
   };
 
-  vendorHash = "sha256-wmpFWySQ9MaRGT3yw4gUQEWsF4MNbSDUu68/LHjE28w=";
+  vendorHash = "sha256-i7m9YXPlWqHtvC4Df7v5bLWt2tqeT933t2+Xit5RQxg=";
 
   ldflags = [ "-s" "-w" "-X github.com/cloudposse/atmos/cmd.Version=v${version}" ];
 
