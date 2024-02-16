4625072bb82f3df2e755c491d51beda33f7e8355
Rafael Fernández López <ereslibre@ereslibre.es>
2024-02-16 19:29:48 +0100

    wasm-tools: 1.0.60 -> 1.200.0

--- pkgs/tools/misc/wasm-tools/default.nix
+++ pkgs/tools/misc/wasm-tools/default.nix
@@ -5,19 +5,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wasm-tools";
-  version = "1.0.60";
+  version = "1.200.0";
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = pname;
-    rev = "${pname}-${version}";
-    hash = "sha256-+cOx1ad2IvBLFMo83NAvyDSHCZC9aAGPmQBISaiMSaY=";
+    rev = "v${version}";
+    hash = "sha256-GuN70HiCmqBRwcosXqzT8sl5SRCTttOPIRl6pxaQiec=";
     fetchSubmodules = true;
   };
 
   # Disable cargo-auditable until https://github.com/rust-secure-code/cargo-auditable/issues/124 is solved.
   auditable = false;
-  cargoHash = "sha256-ek89mtJpRH/WR9mekw0gJyd64L/bRGvF7624byHWKPQ=";
+  cargoHash = "sha256-T9p1PvgiAZrj82ABx7KX2InZACQ/ff7N0zPKGTCTBPk=";
   cargoBuildFlags = [ "--package" "wasm-tools" ];
   cargoTestFlags = [ "--all" ];
 
