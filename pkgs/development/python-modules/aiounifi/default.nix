f6b1483c274993869ad0ab01f0ea587e3907db48
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-16 13:57:41 +0000

    python311Packages.aiounifi: 70 -> 71

--- pkgs/development/python-modules/aiounifi/default.nix
+++ pkgs/development/python-modules/aiounifi/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "aiounifi";
-  version = "70";
+  version = "71";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "Kane610";
     repo = "aiounifi";
     rev = "refs/tags/v${version}";
-    hash = "sha256-yLqGqRWzuMqymGqGR1mA4oQb+tWt58lA7C/kXC5bYz8=";
+    hash = "sha256-KmxwCjmvDByCtsSQ+fQtdLS4ZDxtUaqc5zoOF9dsSq8=";
   };
 
   postPatch = ''
