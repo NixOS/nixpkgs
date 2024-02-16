0a020a72a79b674236a15c832d6624eeb68ddbbb
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-16 15:40:25 +0000

    moar: 1.23.5 -> 1.23.6

--- pkgs/tools/misc/moar/default.nix
+++ pkgs/tools/misc/moar/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "moar";
-  version = "1.23.5";
+  version = "1.23.6";
 
   src = fetchFromGitHub {
     owner = "walles";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-DOscmjLjQnHUGrVPO/rwICPDWVzfE3yu3u84qCmwfgs=";
+    hash = "sha256-WWMFyDLNi5XUEWi33Sav69i41icbp7NTzFy+Y+ImGNU=";
   };
 
   vendorHash = "sha256-1u/2OlMX2FuZaxWnpU4n5r/4xKe+rK++GoCJiSq/BdE=";
