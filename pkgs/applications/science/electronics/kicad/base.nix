60a97f8c9b48d2e80bbe3ee6b5713aa029382923
Evils <evils.devils@protonmail.com>
2024-02-17 00:29:14 +0100

    kicad: use ninja
    brings a ~33m build down to ~30m
      on a ryzen 3600 with 32G RAM

--- pkgs/applications/science/electronics/kicad/base.nix
+++ pkgs/applications/science/electronics/kicad/base.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , cmake
+, ninja
 , libGLU
 , libGL
 , zlib
@@ -131,6 +132,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     cmake
+    ninja
     doxygen
     graphviz
     pkg-config
