9615b00dd8a82c2fb3521a5873b0aad6958db47e
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-17 00:33:35 +0000

    python311Packages.boto3-stubs: 1.34.42 -> 1.34.44

--- pkgs/development/python-modules/boto3-stubs/default.nix
+++ pkgs/development/python-modules/boto3-stubs/default.nix
@@ -365,14 +365,14 @@
 
 buildPythonPackage rec {
   pname = "boto3-stubs";
-  version = "1.34.42";
+  version = "1.34.44";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-wg/LCxaAvonBhZUaLIhAbn3NSKkYFCMbWfN9rWJJEIo=";
+    hash = "sha256-FXLl1Ro/kpZE6qRQqTKYDPmgDjKbJjzgpTAtdEolVMM=";
   };
 
   nativeBuildInputs = [
