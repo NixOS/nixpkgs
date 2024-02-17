c7378b2f5019217576bc5dfe0f495e21e98a4a04
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-17 06:12:22 +0000

    obs-studio-plugins.obs-vertical-canvas: 1.3.1 -> 1.4.0

--- pkgs/applications/video/obs-studio/plugins/obs-vertical-canvas.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-vertical-canvas.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "obs-vertical-canvas";
-  version = "1.3.1";
+  version = "1.4.0";
 
   src = fetchFromGitHub {
     owner = "Aitum";
     repo = "obs-vertical-canvas";
     rev = version;
-    sha256 = "sha256-kJJepKUH/tc6iV/zFDtrAa4bM9Gaqc9M7IItytknkGA=";
+    sha256 = "sha256-80yqyfbSFBDLgGzSC8ktJRHFhbD5REWH0Rwrghcu6KY=";
   };
 
   nativeBuildInputs = [ cmake ];
