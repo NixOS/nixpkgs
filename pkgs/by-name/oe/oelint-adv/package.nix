4a5a18e3ee16f4c131110b74c8c003caacb59c0e
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-17 12:41:30 +0000

    oelint-adv: 4.2.0 -> 4.3.0

--- pkgs/by-name/oe/oelint-adv/package.nix
+++ pkgs/by-name/oe/oelint-adv/package.nix
@@ -6,13 +6,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "oelint-adv";
-  version = "4.2.0";
+  version = "4.3.0";
   format = "setuptools";
 
   src = fetchPypi {
     inherit version;
     pname = "oelint_adv";
-    hash = "sha256-Yq69pZLtOdUP+ZkKA6F7KgRlmXJQiS17+ETMVjpt9iY=";
+    hash = "sha256-G2wBy1nx05WiAtnNXp7Kvio4dA3rYJRVfdKm3a2ZF/g=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
