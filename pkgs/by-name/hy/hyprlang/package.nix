45fa2708731c929173f01878ce9483bd5b151d60
IogaMaster <iogamastercode@gmail.com>
2024-02-17 14:25:53 -0700

    hyprlang: 0.3.2 -> 0.4.0
    https://github.com/hyprwm/hyprlang

--- pkgs/by-name/hy/hyprlang/package.nix
+++ pkgs/by-name/hy/hyprlang/package.nix
@@ -4,15 +4,16 @@
   fetchFromGitHub,
   cmake,
 }:
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "hyprlang";
-  version = "0.3.2";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "hyprwm";
     repo = "hyprlang";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-9TT3xk++LI5/SPYgjYX34xZ4ebR93c1uerIq+SE/ues=";
+    hash = "sha256-nW3Zrhh9RJcMTvOcXAaKADnJM/g6tDf3121lJtTHnYo=";
   };
 
   nativeBuildInputs = [cmake];
