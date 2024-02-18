b9214ecf65ab9b89667a1a234715bd43d30d4acd
Bruno Rodrigues <bruno@brodrigues.co>
2024-02-18 20:39:05 +0100

    rPackages.SemiCompRisks: fixed build

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -430,6 +430,7 @@ let
     strawr = with pkgs; [ curl.dev ];
     terra = with pkgs; [ gdal proj geos ];
     apcf = with pkgs; [ geos ];
+    SemiCompRisks = [ pkgs.gsl ];
     showtext = with pkgs; [ zlib libpng icu freetype.dev ];
     simplexreg = [ pkgs.gsl ];
     spate = [ pkgs.fftw.dev ];
