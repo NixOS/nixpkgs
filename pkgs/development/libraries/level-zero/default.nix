d86b861c617eea21bc7c3b3aeba833638d2ac2dc
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-02-18 23:04:23 +0100

    level-zero: 1.16.0 -> 1.16.1
    Diff: https://github.com/oneapi-src/level-zero/compare/refs/tags/v1.16.0...v1.16.1
    
    Changelog: https://github.com/oneapi-src/level-zero/blob/v1.16.1/CHANGELOG.md
    (cherry picked from commit 46dbad85d818aee27040e983fa7c15173724f111)

--- pkgs/development/libraries/level-zero/default.nix
+++ pkgs/development/libraries/level-zero/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "level-zero";
-  version = "1.16.0";
+  version = "1.16.1";
 
   src = fetchFromGitHub {
     owner = "oneapi-src";
     repo = "level-zero";
     rev = "refs/tags/v${version}";
-    hash = "sha256-YEYCbMj5oK9S117MgnC0O5SYM1jRGEIhFcLaOjC8bac=";
+    hash = "sha256-iPWEZ9aJ3uI4cAKRgur78zdVwGtD6q1TqwNpK+mg5hw=";
   };
 
   nativeBuildInputs = [ cmake addOpenGLRunpath ];
