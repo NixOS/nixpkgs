4194b60f05d0251c7ce6ae693ad2b02af753da94
Nick Cao <nickcao@nichi.co>
2024-02-18 13:23:53 -0500

    telegram-desktop: 4.14.15 -> 4.15.0
    Diff: https://github.com/telegramdesktop/tdesktop/compare/v4.14.15...v4.15.0
    
    Changelog: https://github.com/telegramdesktop/tdesktop/releases/tag/v4.15.0
    (cherry picked from commit 656ea76b1b21c0bd6df6abdb6fb96ba3c1040fa6)

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/default.nix
@@ -64,14 +64,14 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "telegram-desktop";
-  version = "4.14.15";
+  version = "4.15.0";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-706FAtXS541D7H/Qc86eC1FLUWu1/tZuCq3GgJ0L/Ds=";
+    hash = "sha256-kLF5vVt7JKHtNIEJ4dp/HSOOCgRdjFzn0aQpFpiw9MM=";
   };
 
   patches = [
