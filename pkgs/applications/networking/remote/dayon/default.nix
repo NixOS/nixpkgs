171812ea70daf7636b2f3e25d5e6d3f5451e0496
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-18 23:54:50 +0000

    dayon: 13.0.1 -> 13.0.2

--- pkgs/applications/networking/remote/dayon/default.nix
+++ pkgs/applications/networking/remote/dayon/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "dayon";
-  version = "13.0.1";
+  version = "13.0.2";
 
   src = fetchFromGitHub {
     owner = "RetGal";
     repo = "dayon";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-nevDC4kfVSgfmJZiCj82mc+/yZcIgub3CP9qi9ISF3o=";
+    hash = "sha256-sKA50D+VYjfKzdZAppIGfU5uJqrCrZPEsk9EEMBxu3I=";
   };
 
   nativeBuildInputs = [
