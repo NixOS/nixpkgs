51704de0c6c5985d11c7cd2d9cb3b38493410316
Aidan Gauland <aidalgol@fastmail.net>
2024-02-19 14:38:44 +1300

    gogdl: 0.7.3 -> unstable-2024-02-15
    Upstream has some bug fixes on the main branch without a version number
    bump.  Also switch the update script to unstableGitUpdater, because this
    has happened before (upstream fixes without a version bump).

--- pkgs/games/gogdl/default.nix
+++ pkgs/games/gogdl/default.nix
@@ -7,18 +7,19 @@
 , setuptools
 , requests
 , cacert
+, unstableGitUpdater
 }:
 
 buildPythonApplication rec {
   pname = "gogdl";
-  version = "1.0.0";
+  version = "unstable-2024-02-15";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "Heroic-Games-Launcher";
     repo = "heroic-gogdl";
-    rev = "e1c3e5b98feda53ea88f54f9aa5c614ae9424ef0";
-    hash = "sha256-MGxpWfDGchZruVmSiea5s1JQV23F9QvQLWrxPmIsBEo=";
+    rev = "18f651931d4098cba66f4c2a2f9dc6e1351f9460";
+    hash = "sha256-wGytOzMencWZIku5lM6gEFaFyqUtfurjZVSHAY5mI7g=";
   };
 
   disabled = pythonOlder "3.8";
@@ -39,28 +40,5 @@ buildPythonApplication rec {
 
   # Upstream no longer create git tags when bumping the version, so we have to
   # extract it from the source code on the main branch.
-  passthru.updateScript = writeScript "gogdl-update-script" ''
-    #!/usr/bin/env nix-shell
-    #!nix-shell -i bash -p curl gnused jq common-updater-scripts
-    set -eou pipefail;
-
-    owner=Heroic-Games-Launcher
-    repo=heroic-gogdl
-    path='gogdl/__init__.py'
-
-    version=$(
-      curl --cacert "${cacert}/etc/ssl/certs/ca-bundle.crt" \
-      https://raw.githubusercontent.com/$owner/$repo/main/$path |
-      sed -n 's/^\s*version\s*=\s*"\([0-9]\.[0-9]\.[0-9]\)"\s*$/\1/p')
-
-    commit=$(curl --cacert "${cacert}/etc/ssl/certs/ca-bundle.crt" \
-      https://api.github.com/repos/$owner/$repo/commits?path=$path |
-      jq -r '.[0].sha')
-
-    update-source-version \
-      ${pname} \
-      "$version" \
-      --file=./pkgs/games/gogdl/default.nix \
-      --rev=$commit
-  '';
+  passthru.updateScript = unstableGitUpdater { };
 }
