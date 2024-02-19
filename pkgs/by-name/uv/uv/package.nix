9b0e483241c1e53e288c22f4c70ac27b590550ec
Gaetan Lepage <gaetan@glepage.com>
2024-02-19 14:04:18 +0100

    uv: 0.1.4 -> 0.1.5
    Diff: https://github.com/astral-sh/uv/compare/0.1.4...0.1.5

--- pkgs/by-name/uv/uv/package.nix
+++ pkgs/by-name/uv/uv/package.nix
@@ -15,14 +15,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "uv";
-  version = "0.1.4";
+  version = "0.1.5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "astral-sh";
     repo = "uv";
     rev = version;
-    hash = "sha256-0bG1nReTaNqjHXCeW8gg3AWfXpsm8KulX6jsZQMVLlg=";
+    hash = "sha256-aiTec1uWcLcCA03qki5EE7Yy4Ne2+kXu9YtIVIAyd+o=";
   };
 
   cargoDeps = rustPlatform.importCargoLock {
