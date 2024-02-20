0b871380c15c065cd621822d178a942e8233419f
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-20 23:48:12 +0000

    trufflehog: 3.67.6 -> 3.67.7

--- pkgs/tools/security/trufflehog/default.nix
+++ pkgs/tools/security/trufflehog/default.nix
@@ -7,16 +7,16 @@
 
 buildGoModule rec {
   pname = "trufflehog";
-  version = "3.67.6";
+  version = "3.67.7";
 
   src = fetchFromGitHub {
     owner = "trufflesecurity";
     repo = "trufflehog";
     rev = "refs/tags/v${version}";
-    hash = "sha256-LKnFlgMbgp47mNkER+gE4PwEpqBY1txmhDpmcPCXH24=";
+    hash = "sha256-EgAZVFWhAmC24kDYIsvyHcjT7DbmQ3PRO9dSAT3ZdXU=";
   };
 
-  vendorHash = "sha256-/DKly5ZFrySYrjGywjsyQd5Ky1bQ+ZIJll0io6XC5+s=";
+  vendorHash = "sha256-KkP8NY35Uo0RM14VSGqAJ3yhw8dXDi5JDPpkBYPmpfQ=";
 
   ldflags = [
     "-s"
