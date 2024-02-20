65ac553a62323237c709d7b10ed853b8885741d5
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-20 20:59:04 +0100

    ldeep: refactor

--- pkgs/tools/security/ldeep/default.nix
+++ pkgs/tools/security/ldeep/default.nix
@@ -6,7 +6,7 @@
 python3.pkgs.buildPythonApplication rec {
   pname = "ldeep";
   version = "1.0.52";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "franc-pentest";
@@ -15,15 +15,24 @@ python3.pkgs.buildPythonApplication rec {
     hash = "sha256-I51vz3zF1J3223hcO3cdfsNBfpq/UolDxUEXyqx3dLI=";
   };
 
+  pythonRelaxDeps = [
+    "cryptography"
+  ];
+
   nativeBuildInputs = with python3.pkgs; [
     cython
+    pythonRelaxDepsHook
+    setuptools
   ];
 
   propagatedBuildInputs = with python3.pkgs; [
     commandparse
     cryptography
     dnspython
+    gssapi
     ldap3
+    oscrypto
+    pycryptodome
     pycryptodomex
     six
     termcolor
