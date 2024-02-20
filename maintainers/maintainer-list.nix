17875636a48c9a8bb199c6f03ddb5a2af20a6f62
Edgar Lee <122112154+elpdt852@users.noreply.github.com>
2024-02-20 08:09:48 +0800

    maintainers: add elpdt852

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5612,6 +5612,12 @@
     githubId = 2536303;
     name = "Enno Lohmeier";
   };
+  elpdt852 = {
+    email = "nix@pdtpartners.com";
+    github = "elpdt852";
+    githubId = 122112154;
+    name = "Edgar Lee";
+  };
   elvishjerricco = {
     email = "elvishjerricco@gmail.com";
     matrix = "@elvishjerricco:matrix.org";
