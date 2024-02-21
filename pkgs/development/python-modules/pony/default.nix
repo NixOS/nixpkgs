d41d3a27ab8d91ef97e5f6c6947817955cc66197
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-21 09:07:11 +0100

    python311Packages.pony: refactor

--- pkgs/development/python-modules/pony/default.nix
+++ pkgs/development/python-modules/pony/default.nix
@@ -4,21 +4,26 @@
 , pytestCheckHook
 , pythonOlder
 , pythonAtLeast
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "pony";
   version = "0.7.17";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.8" || pythonAtLeast "3.12";
 
   src = fetchFromGitHub {
     owner = "ponyorm";
-    repo = pname;
+    repo = "pony";
     rev = "refs/tags/v${version}";
     hash = "sha256-wBqw+YHKlxYplgsYL1pbkusHyPfCaVPcH/Yku6WDYbE=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   nativeCheckInputs = [
     pytestCheckHook
@@ -36,6 +41,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library for advanced object-relational mapping";
     homepage = "https://ponyorm.org/";
+    changelog = "https://github.com/ponyorm/pony/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ d-goldin xvapx ];
   };
