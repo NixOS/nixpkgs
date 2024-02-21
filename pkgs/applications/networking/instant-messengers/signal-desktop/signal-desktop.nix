8151209120614235766a8b57da5638d29ea8ac41
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-21 00:12:17 +0000

    signal-desktop: 6.47.1 -> 6.48.0
    (cherry picked from commit 36b4f14d6d673d83fba54ec015c85570480f5511)

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix {} rec {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "6.47.1";
+  version = "6.48.0";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-WRdn3T18xhWvlELtwlOs/ZoPuEt/yQgs7JP/1MGN5Ps=";
+  hash = "sha256-hQhMl3ArIZK+0u90uIw0mERv9z4aL1Nny9Qx/Cvsfl4=";
 }
