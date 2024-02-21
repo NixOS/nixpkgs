beeb6a6fbd891472cf2261567e2d430d131c27e9
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-21 20:26:20 +0100

    python312Packages.aiocsv: refactor

--- pkgs/development/python-modules/aiocsv/default.nix
+++ pkgs/development/python-modules/aiocsv/default.nix
@@ -6,24 +6,26 @@
 , pytest-asyncio
 , pytestCheckHook
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "aiocsv";
   version = "1.3.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "MKuranowski";
-    repo = pname;
+    repo = "aiocsv";
     rev = "refs/tags/v${version}";
     hash = "sha256-5jMmT7XY+1VNbDNciZS6B/oQJFj4XmGvhDITKWHCuOQ=";
   };
 
   nativeBuildInputs = [
     cython
+    setuptools
   ];
 
   nativeCheckInputs = [
