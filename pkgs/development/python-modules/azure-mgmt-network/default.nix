d250b7a6c8367c134c2c00ed46ec7ce9bfb770bf
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-22 09:43:00 +0100

    python311Packages.azure-mgmt-network: 25.2.0 -> 25.3.0

--- pkgs/development/python-modules/azure-mgmt-network/default.nix
+++ pkgs/development/python-modules/azure-mgmt-network/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-network";
-  version = "25.2.0";
+  version = "25.3.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-EUxCktIjsdHiR7Qa9luNWjeTVn5q2/ojB3IFUiP0GCo=";
+    hash = "sha256-3OLK+xrg5WPgte/FN9yYp8Ctgk1CYeZL7XX3iBlt1cY=";
   };
 
   propagatedBuildInputs = [
