40c0cfa30b2e65649fa76eccccce25d638d6db0c
modderme123 <modderme123@gmail.com>
2024-02-22 10:11:35 -0800

    maintainers: add modderme123

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12575,6 +12575,12 @@
     githubId = 7831184;
     name = "John Mercier";
   };
+  modderme123 = {
+    email = "modderme123@gmail.com";
+    github = "modderme123";
+    githubId = 14153763;
+    name = "modderme123";
+  };
   modulistic = {
     email = "modulistic@gmail.com";
     github = "modulistic";
