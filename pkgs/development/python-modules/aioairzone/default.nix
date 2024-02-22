82befbfaafd6f4a2585002b592ac84a4c85adb21
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-22 09:38:33 +0100

    python311Packages.aioairzone: refactor

--- pkgs/development/python-modules/aioairzone/default.nix
+++ pkgs/development/python-modules/aioairzone/default.nix
@@ -9,13 +9,13 @@
 buildPythonPackage rec {
   pname = "aioairzone";
   version = "0.7.4";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "Noltari";
-    repo = pname;
+    repo = "aioairzone";
     rev = "refs/tags/${version}";
     hash = "sha256-yIkP0eWyEJAZc2tJbwTvlU5b2pDta4SatmsTMPsSDt8=";
   };
