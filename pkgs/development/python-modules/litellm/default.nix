436107469fc4d30742ce0bf8cd73a49df96411d9
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-22 13:14:27 +0000

    python311Packages.litellm: 1.23.9 -> 1.26.8

--- pkgs/development/python-modules/litellm/default.nix
+++ pkgs/development/python-modules/litellm/default.nix
@@ -33,7 +33,7 @@
 
 buildPythonPackage rec {
   pname = "litellm";
-  version = "1.23.9";
+  version = "1.26.8";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -42,7 +42,7 @@ buildPythonPackage rec {
     owner = "BerriAI";
     repo = "litellm";
     rev = "refs/tags/v${version}";
-    hash = "sha256-5VqYo9JhRwtPnk0z7w7jFKN8/E2JhZ50Zi4HgbFiyhE=";
+    hash = "sha256-w2d33mYNgQ+S+000ZswyqXE8RxOUQaH89R7M6PHe020=";
   };
 
   postPatch = ''
