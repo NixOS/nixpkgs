cb8c1e4120b5552391805135044c0a3faf3f8682
Robert Schütz <nix@dotlambda.de>
2024-02-23 15:19:45 -0800

    python311Packages.py-ecc: 6.0.0 -> 7.0.0
    Diff: https://github.com/ethereum/py_ecc/compare/v6.0.0...v7.0.0
    
    Changelog: https://github.com/ethereum/py_ecc/blob/v7.0.0/CHANGELOG.rst

--- pkgs/development/python-modules/py-ecc/default.nix
+++ pkgs/development/python-modules/py-ecc/default.nix
@@ -4,29 +4,32 @@
 , cached-property
 , eth-typing
 , eth-utils
-, mypy-extensions
 , pytestCheckHook
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "py-ecc";
-  version = "6.0.0";
-  format = "setuptools";
-  disabled = pythonOlder "3.6";
+  version = "7.0.0";
+  pyproject = true;
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "py_ecc";
     rev = "v${version}";
-    hash = "sha256-638otYA3e/Ld4mcM69yrqHQnGoK/Sfl/UA9FWnjgO/U=";
+    hash = "sha256-DKe+bI1GEzXg4Y4n5OA1/hWYz9L3X1AvaOFPEnCaAfs=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     cached-property
     eth-typing
     eth-utils
-    mypy-extensions
   ];
 
   nativeCheckInputs = [
@@ -36,6 +39,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "py_ecc" ];
 
   meta = with lib; {
+    changelog = "https://github.com/ethereum/py_ecc/blob/${src.rev}/CHANGELOG.rst";
     description = "ECC pairing and bn_128 and bls12_381 curve operations";
     homepage = "https://github.com/ethereum/py_ecc";
     license = licenses.mit;
