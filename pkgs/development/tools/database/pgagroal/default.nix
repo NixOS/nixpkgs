d5845a9cb894be56309ab184ae56d272092a3f61
Mario Rodas <marsam@users.noreply.github.com>
2024-02-23 04:20:00 +0000

    pgagroal: 1.5.1 -> 1.6.0
    Diff: https://github.com/agroal/pgagroal/compare/1.5.1...1.6.0
    
    Changelog: https://github.com/agroal/pgagroal/releases/tag/1.6.0

--- pkgs/development/tools/database/pgagroal/default.nix
+++ pkgs/development/tools/database/pgagroal/default.nix
@@ -1,27 +1,29 @@
-{ lib, stdenv, fetchFromGitHub, cmake, docutils, libev, openssl, systemd }:
+{ lib, stdenv, fetchFromGitHub, cjson, cmake, docutils, libev, openssl, systemd }:
 
 stdenv.mkDerivation rec {
   pname = "pgagroal";
-  version = "1.5.1";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "agroal";
     repo = "pgagroal";
     rev = version;
-    hash = "sha256-d6icEYlk0qnzmoP/mvSmTw16YfIYWc2WbY7sKguX7Ug=";
+    hash = "sha256-bgJvGJ35RdFopW88o+H1DLpG70anP197y6xrpRRrxUA=";
   };
 
   patches = [ ./do-not-search-libatomic.patch ];
 
   nativeBuildInputs = [ cmake docutils ];
 
-  buildInputs = [ libev openssl systemd ];
+  buildInputs = [ cjson libev openssl ]
+    ++ lib.optionals stdenv.isLinux [ systemd ];
 
   meta = with lib; {
     description = "High-performance connection pool for PostgreSQL";
     homepage = "https://agroal.github.io/pgagroal/";
+    changelog = "https://github.com/agroal/pgagroal/releases/tag/${version}";
     license = licenses.bsd3;
     maintainers = [ maintainers.marsam ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }
