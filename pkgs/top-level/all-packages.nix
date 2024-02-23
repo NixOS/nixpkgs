a118964872418b3ce092174dbe2ddb0cbb29837d
Nikolay Korotkiy <sikmir@disroot.org>
2024-02-23 16:00:54 +0400

    orbiton: 2.65.8 → 2.65.10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31083,7 +31083,9 @@ with pkgs;
 
   edlin = callPackage ../applications/editors/edlin { };
 
-  orbiton = callPackage ../applications/editors/orbiton { };
+  orbiton = callPackage ../applications/editors/orbiton {
+    buildGoModule = buildGo122Module;
+  };
 
   oed = callPackage ../applications/editors/oed { };
 
