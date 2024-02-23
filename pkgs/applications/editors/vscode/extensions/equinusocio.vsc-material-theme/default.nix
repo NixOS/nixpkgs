a00e878cb349df2837f4c746f61aeb5ddf4905ca
Felix Buehler <account@buehler.rocks>
2024-02-23 22:09:08 +0100

    vscode-extensions.equinusocio.vsc-material-theme: fix user-settings

--- pkgs/applications/editors/vscode/extensions/equinusocio.vsc-material-theme/default.nix
+++ pkgs/applications/editors/vscode/extensions/equinusocio.vsc-material-theme/default.nix
@@ -10,6 +10,12 @@ vscode-utils.buildVscodeMarketplaceExtension {
     sha256 = "sha256-3yxFTMtjJR1b4EzBDfm55HF9chrya5OUF5wN+KHEduE=";
   };
 
+  # extensions wants to write at the /nix/store path, so we patch it to use the globalStorageUri instead.
+  prePatch = ''
+    substituteInPlace ./build/core/extension-manager.js \
+      --replace-fail "path_1.posix.join(extensionFolderUri.path, env_1.USER_CONFIG_FILE_NAME)" "path_1.posix.join(ExtensionContext.globalStorageUri.fsPath, env_1.USER_CONFIG_FILE_NAME)"
+  '';
+
   meta = with lib; {
     changelog = "https://marketplace.visualstudio.com/items/Equinusocio.vsc-material-theme/changelog";
     description = "The most epic theme now for Visual Studio Code";
