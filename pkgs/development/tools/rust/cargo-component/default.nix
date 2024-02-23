9ae2cc6eb0979a03b1c65e751ffbf6db335c6915
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-23 07:43:13 +0000

    cargo-component: 0.7.1 -> 0.8.0
    (cherry picked from commit 5bd5143989bd1d24c7cd1ec5c15c9fe51ba3176c)
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/development/tools/rust/cargo-component/default.nix
+++ pkgs/development/tools/rust/cargo-component/default.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-component";
-  version = "0.7.1";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = "cargo-component";
     rev = "v${version}";
-    hash = "sha256-FmaJUdi3hJEd0Fo4tgzne47aP8U/SK+S0UDVEG/P6e8=";
+    hash = "sha256-MYJxwJuiBbZQzCpFoJ/iKv6ShkCYJIMu+QQEURHRBAU=";
   };
 
-  cargoHash = "sha256-8Shw3YSHBFHkw57ZVNjJo/eg2fXvyTVKM9kPX17pfkM=";
+  cargoHash = "sha256-qh6/e4ya0+ZPYIJRYe87F65tWO3ZllZpIw9B5jE+YbU=";
 
   nativeBuildInputs = [
     pkg-config
