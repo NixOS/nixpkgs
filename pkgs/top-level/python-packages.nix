6171ba45393fdac3479340ae7799424c22e1d861
Yueh-Shun Li <shamrocklee@posteo.net>
2024-02-23 23:59:31 +0800

    lastversion, python3Packages.lastversion: init at 3.5.0
    Co-authored-by: Anderson Torres <torres.anderson.85@protonmail.com>
    Co-authored-by: Arne Keller <2012gdwu+github@posteo.de>
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7235,6 +7235,8 @@ self: super: with self; {
 
   laspy = callPackage ../development/python-modules/laspy { };
 
+  lastversion = callPackage ../development/python-modules/lastversion { };
+
   laszip = callPackage ../development/python-modules/laszip {
     inherit (pkgs) cmake ninja;
     inherit (pkgs.__splicedPackages) laszip;
