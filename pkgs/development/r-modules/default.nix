2b94db858191c68346923e47a63783b0feaa1cc6
László Kupcsik <koopac@gmail.com>
2024-02-24 21:50:51 +0100

    rPackages.pkgdepends: fix compile error
    Locate patchShebangs correctly
    
    Co-authored-by: Justin Bedő <cu@cua0.org>

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -1507,6 +1507,12 @@ let
         patchShebangs src/library/ps/configure
       '';
     });
+
+    pkgdepends = old.pkgdepends.overrideAttrs (attrs: {
+      postPatch = ''
+        patchShebangs configure
+      '';
+    });
   };
 in
   self
