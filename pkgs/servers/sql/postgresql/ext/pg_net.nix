3bb3711ded9942b2ba152e34a72aedb1e42345fd
Mario Rodas <marsam@users.noreply.github.com>
2024-02-24 08:00:00 +0000

    postgresqlPackages.pg_net: fix build with gcc

--- pkgs/servers/sql/postgresql/ext/pg_net.nix
+++ pkgs/servers/sql/postgresql/ext/pg_net.nix
@@ -13,6 +13,8 @@ stdenv.mkDerivation rec {
     hash   = "sha256-ZPsRPWV1G3lMM2mT+H139Wvgoy8QnmeUbzEnGeDJmZA=";
   };
 
+  env.NIX_CFLAGS_COMPILE = "-Wno-error";
+
   installPhase = ''
     mkdir -p $out/{lib,share/postgresql/extension}
 
