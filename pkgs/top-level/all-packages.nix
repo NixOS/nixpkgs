086388fc15419bd9000378056a4369f63948ab1a
stuebinm <stuebinm@disroot.org>
2024-02-25 18:49:15 +0100

    mattermost: 8.1.10 → 9.5.1
    This jumps Mattermost ESR Versions (see [1] for their release cycle). The
    new version makes use of Go's workspace feature, which unfortunately the
    buildGoModule function does not (yet?) support [2], which breaks the
    previous build process for mattermost.
    
    Further, the new release also makes use of private modules only included
    in the (non-free) enterprise version of mattermost which makes it impossible
    to build in the usual way even outside of nixpkgs's build abstractions [3].
    
    Both issues can be solved by using Go 1.22, which has added support for
    vendoring when using workspaces, and instructing it to ignore errors with
    the -e flag. This requires overriding the go-modules derivation's
    buildPhase, and still produces a functional binary.
    
    [1] https://docs.mattermost.com/upgrade/extended-support-release.html
    [2] https://github.com/NixOS/nixpkgs/issues/203039
    [3] https://github.com/mattermost/mattermost/issues/26221

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26455,7 +26455,7 @@ with pkgs;
 
   matrix-alertmanager = callPackage ../servers/monitoring/matrix-alertmanager { };
 
-  mattermost = callPackage ../servers/mattermost { };
+  mattermost = callPackage ../servers/mattermost { buildGoModule = buildGo122Module; };
   matterircd = callPackage ../servers/mattermost/matterircd.nix { };
   matterbridge = callPackage ../servers/matterbridge { };
 
