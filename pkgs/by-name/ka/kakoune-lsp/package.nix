a31431034a3cae87d3f07925c2a68dd1eb028e84
V <v@anomalous.eu>
2024-02-26 10:55:56 +0100

    kakoune-lsp: 15.0.1 -> 16.0.0
    The project renamed from "kak-lsp" in this version. Command and config
    file paths remain the same, at least for now.
    
    Change-Id: I7cbce56096bac5d4c68c2e9d4effd11f51b7bbf5

--- pkgs/by-name/ka/kakoune-lsp/package.nix
+++ pkgs/by-name/ka/kakoune-lsp/package.nix
@@ -1,21 +1,23 @@
 { stdenv, lib, fetchFromGitHub, rustPlatform, perl, CoreServices, Security, SystemConfiguration }:
 
 rustPlatform.buildRustPackage rec {
-  pname = "kak-lsp";
-  version = "15.0.1";
+  pname = "kakoune-lsp";
+  version = "16.0.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-W4z2YtOEBCTM+NsL1HBHSYCXJXN459chE4RW0CPMjD4=";
+    sha256 = "sha256-d4Tc6iYp20uOKMd+T2LhWgXWZzvzq1E+VWqjhhiIiHE=";
   };
 
-  cargoHash = "sha256-tAA9eu4y1h6huNmEgY3L6v29itP5I4a8UZgoA+ANoq0=";
+  cargoHash = "sha256-kV8d0PwIWS6gyfCtv70iv8MrL91ZOZbwYznhc3lUw0U=";
 
   buildInputs = [ perl ] ++ lib.optionals stdenv.isDarwin [ CoreServices Security SystemConfiguration ];
 
-  patches = [ ./0001-Use-full-Perl-path.patch ];
+  patches = [
+    ./Use-full-Perl-path.patch
+  ];
 
   postPatch = ''
     substituteInPlace rc/lsp.kak \
@@ -24,7 +26,7 @@ rustPlatform.buildRustPackage rec {
 
   meta = with lib; {
     description = "Kakoune Language Server Protocol Client";
-    homepage = "https://github.com/kak-lsp/kak-lsp";
+    homepage = "https://github.com/kakoune-lsp/kakoune-lsp";
     license = with licenses; [ unlicense /* or */ mit ];
     maintainers = with maintainers; [ spacekookie poweredbypie ];
     mainProgram = "kak-lsp";
