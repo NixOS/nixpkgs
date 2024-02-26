18477ad3b3d13ddc5015baa6b0e28b378ebc99ff
SaltyKitkat <56984513+SaltyKitkat@users.noreply.github.com>
2024-02-26 14:24:43 +0800

    nixos/plasma5: dont set "plasma" to `services.xserver.displayManager.defaultSession`

--- nixos/modules/services/x11/desktop-managers/plasma5.nix
+++ nixos/modules/services/x11/desktop-managers/plasma5.nix
@@ -408,10 +408,6 @@ in
       ];
 
       services.xserver.displayManager.sessionPackages = [ pkgs.plasma5Packages.plasma-workspace ];
-      # Default to be `plasma` (X11) instead of `plasmawayland`, since plasma wayland currently has
-      # many tiny bugs.
-      # See: https://github.com/NixOS/nixpkgs/issues/143272
-      services.xserver.displayManager.defaultSession = mkDefault "plasma";
 
       environment.systemPackages =
         with pkgs.plasma5Packages;
