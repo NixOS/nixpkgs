0aca8bdd4a5f10838fc5e96d24abb9eddf8706f4
Michael Dormann <michael.c.dormann@gmail.com>
2024-02-25 22:25:44 -0600

    maintainers: add MichaelCDormann

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12458,6 +12458,12 @@
     github = "MichaelBrunn3r";
     githubId = 19626539;
   };
+  MichaelCDormann = {
+    email = "michael.c.dormann@gmail.com";
+    name = "Michael Dormann";
+    github = "MichaelCDormann";
+    githubId = 12633081;
+  };
   michaelCTS = {
     email = "michael.vogel@cts.co";
     name = "Michael Vogel";
