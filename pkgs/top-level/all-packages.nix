1d2ae60bc8e39b51adcb975c04feda6010401b7a
Jim Fowler <fowler@math.osu.edu>
2024-02-26 12:18:12 -0500

    update version of gargoyle which now depends on qt5

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37486,9 +37486,7 @@ with pkgs;
 
   garden-of-coloured-lights = callPackage ../games/garden-of-coloured-lights { allegro = allegro4; };
 
-  gargoyle = callPackage ../games/gargoyle {
-    inherit (darwin) cctools;
-  };
+  gargoyle = libsForQt5.callPackage ../games/gargoyle { };
 
   gav = callPackage ../games/gav { };
 
