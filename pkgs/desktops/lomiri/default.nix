c5292bdd6d1c0b55634d066a102dfd1332cd4610
OPNA2608 <opna2608@protonmail.com>
2024-02-27 12:08:26 +0100

    lomiri.lomiri-session: init at 0.2

--- pkgs/desktops/lomiri/default.nix
+++ pkgs/desktops/lomiri/default.nix
@@ -17,6 +17,7 @@ let
 
     #### Data
     lomiri-schemas = callPackage ./data/lomiri-schemas { };
+    lomiri-session = callPackage ./data/lomiri-session { };
     lomiri-sounds = callPackage ./data/lomiri-sounds { };
     lomiri-wallpapers = callPackage ./data/lomiri-wallpapers { };
     suru-icon-theme = callPackage ./data/suru-icon-theme { };
