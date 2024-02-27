542bc56dd69e74518a9e8a52588dbf8212334964
László Kupcsik <koopac@gmail.com>
2024-02-27 23:27:14 +0100

    quantlib: 1.29 -> 1.33
    Upgrade library and enable intraday (=high resolution) dates

--- pkgs/development/libraries/quantlib/default.nix
+++ pkgs/development/libraries/quantlib/default.nix
@@ -7,19 +7,31 @@
 
 stdenv.mkDerivation rec {
   pname = "quantlib";
-  version = "1.29";
+  version = "1.33";
 
   outputs = [ "out" "dev" ];
 
   src = fetchFromGitHub {
     owner = "lballabio";
     repo = "QuantLib";
-    rev = "QuantLib-v${version}";
-    sha256 = "sha256-TpVn3zPru/GtdNqDH45YdOkm7fkJzv/qay9SY3J6Jiw=";
+    rev = "v${version}";
+    sha256 = "sha256-j2nRm6ebf5OU6mqmcC7wQf/qlf/K9RmmCAnfT+Au8ZM=";
   };
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [ boost ];
+
+  # Required by RQuantLib, may be beneficial for others too
+  postPatch = ''
+    substituteInPlace CMakeLists.txt --replace-fail \
+    "QL_HIGH_RESOLUTION_DATE \"Enable date resolution down to microseconds\" OFF" \
+    "QL_HIGH_RESOLUTION_DATE \"Enable date resolution down to microseconds\" ON"
+  '';
+
+  # Needed for RQuantLib and possible others
+  postInstall = ''
+    cp ./quantlib-config $out/bin/
+  '';
 
   meta = with lib; {
     description = "A free/open-source library for quantitative finance";
