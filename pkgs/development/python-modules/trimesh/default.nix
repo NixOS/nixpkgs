29520980f211184e73113febe3d27b8b04df7daa
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-27 08:50:11 +0000

    python311Packages.trimesh: 4.1.5 -> 4.1.6

--- pkgs/development/python-modules/trimesh/default.nix
+++ pkgs/development/python-modules/trimesh/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "trimesh";
-  version = "4.1.5";
+  version = "4.1.6";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-m7Qhx8vdQAtvBtJS+4nQQSh0qnp/TRXoch/WJgk/7EI=";
+    hash = "sha256-ps99+3JDyKuB25G1hho6MClDFp48N/dBarHZUpcZK30=";
   };
 
   nativeBuildInputs = [ setuptools ];
