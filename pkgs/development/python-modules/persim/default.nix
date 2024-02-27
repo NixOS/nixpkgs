ffe7b92464b0466c9c39be3abd5948b16a336645
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-27 10:38:50 +0000

    python311Packages.persim: 0.3.2 -> 0.3.4

--- pkgs/development/python-modules/persim/default.nix
+++ pkgs/development/python-modules/persim/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "persim";
-  version = "0.3.2";
+  version = "0.3.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-p6Vumfr+vRDr0D9PnEZItp9vNlCLIb59HpBg1KdyHGE=";
+    hash = "sha256-LCpD1E+Dc+vBPC3Z739F2BfV3IKD47mkGLQyMVyxdZM=";
   };
 
   propagatedBuildInputs = [
