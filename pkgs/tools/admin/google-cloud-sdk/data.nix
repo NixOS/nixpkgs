5b9402d2bd16657c6dcca396dbeeccda94a2f3d2
Moe K <me@mkhalil.org>
2024-02-27 18:49:54 +0000

    google-cloud-sdk: 452.0.1 -> 466.0.0
    Updating the google-cloud-sdk pkg from version 452.0.1 to 466.0.0

--- pkgs/tools/admin/google-cloud-sdk/data.nix
+++ pkgs/tools/admin/google-cloud-sdk/data.nix
@@ -1,32 +1,32 @@
 # DO NOT EDIT! This file is generated automatically by update.sh
 { }:
 {
-  version = "452.0.1";
+  version = "466.0.0";
   googleCloudSdkPkgs = {
     x86_64-linux =
       {
-        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-452.0.1-linux-x86_64.tar.gz";
-        sha256 = "0dj0hzhfvw1p74ja079jmhrhzrbsivgfm0509rxf9xvh1a0kb8zx";
+        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-466.0.0-linux-x86_64.tar.gz";
+        sha256 = "06rzwqyqaam7g46db0x50zyd7l4x15hrnijrmnn4nhximklmwf84";
       };
     x86_64-darwin =
       {
-        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-452.0.1-darwin-x86_64.tar.gz";
-        sha256 = "0gxz12sblc3slw9rhy0i7x8k61sakr0gwipx75h3cghf72hsx0cn";
+        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-466.0.0-darwin-x86_64.tar.gz";
+        sha256 = "0ca5jrh1w522cp9f9qywqzqhb85fa2qgk5w2dhrcgb02p1w3czh8";
       };
     aarch64-linux =
       {
-        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-452.0.1-linux-arm.tar.gz";
-        sha256 = "1jcacwqxa5ylffpfp5mr509imay24ddsv43l04fygzahv2sv8a2d";
+        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-466.0.0-linux-arm.tar.gz";
+        sha256 = "1lnvhl74m4rg0d8474sdr0aq457fibxmf1ica1yhkqbc53gc0439";
       };
     aarch64-darwin =
       {
-        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-452.0.1-darwin-arm.tar.gz";
-        sha256 = "1mpydxqkmfrnqiifgplvc5ghn6rs4981p4pw5zgxip7khw64dfap";
+        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-466.0.0-darwin-arm.tar.gz";
+        sha256 = "0g28dj2cxjp65zvxdd3xqhsb6vckvfm4ba2dp6lwgxcy8ki96njj";
       };
     i686-linux =
       {
-        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-452.0.1-linux-x86.tar.gz";
-        sha256 = "1whijjp2bn24j78rlfpp01y832fnlgj557v6fim9l78zcavgnahz";
+        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-466.0.0-linux-x86.tar.gz";
+        sha256 = "1r7ky2hbhgsqjk2ih3dnl1pngfrpxkv8n355xa6rka4d4xbslbrq";
       };
   };
 }
