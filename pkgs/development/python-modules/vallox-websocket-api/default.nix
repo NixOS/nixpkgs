6fb545d037f43b2fc44dfc6e5da4e7b227be019f
Robert Schütz <nix@dotlambda.de>
2024-02-27 16:49:59 -0800

    python311Packages.vallox-websocket-api: 4.2.0 -> 5.1.0
    Diff: https://github.com/yozik04/vallox_websocket_api/compare/refs/tags/4.2.0...5.1.0
    
    Changelog: https://github.com/yozik04/vallox_websocket_api/releases/tag/5.0.1
               https://github.com/yozik04/vallox_websocket_api/releases/tag/5.0.2
               https://github.com/yozik04/vallox_websocket_api/releases/tag/5.1.0

--- pkgs/development/python-modules/vallox-websocket-api/default.nix
+++ pkgs/development/python-modules/vallox-websocket-api/default.nix
@@ -13,16 +13,16 @@
 
 buildPythonPackage rec {
   pname = "vallox-websocket-api";
-  version = "4.2.0";
-  format = "pyproject";
+  version = "5.1.0";
+  pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "yozik04";
     repo = "vallox_websocket_api";
     rev = "refs/tags/${version}";
-    hash = "sha256-e05MP130okj8j20yMn8a7P6PYZ4PKwCOlAf0ZlUR5aI=";
+    hash = "sha256-ZYcLoOYwQK1+txiBuCEIDp+tYM3eXFtOSR0iNIrIP0w=";
   };
 
   nativeBuildInputs = [
