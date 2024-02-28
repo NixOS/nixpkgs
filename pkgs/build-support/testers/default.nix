NixOS/nixpkgs

commit 90679ea06c7f5ad3538d8c3940aba576865609bf

Specify guest *system* instead of guest *packages*
… based on feedback from @roberth

This plays nicer with other changes the user might want to make and
also works in conjunction with the `pkgsReadOnly`.

Note that this required deleting the `nixpkgs.pkgs` default because
that's not compatible with setting `nixpkgs.system`.  If `nixpkgs.pkgs`
is defined (even as a default-valued option) then `nixpkgs.system` is
ignored.

pkgs/build-support/testers/default.nix
@@ -121,11 +121,6 @@
         (import ../../../nixos/lib/testing-python.nix {
           inherit (stdenv.hostPlatform) system;
           inherit pkgs;
-          extraConfigurations = [(
-            { lib, ... }: {
-              config.nixpkgs.pkgs = lib.mkDefault pkgs;
-            }
-          )];
         });
     in
       test:

nixos/lib/testing/nodes.nix
@@ -16,14 +16,14 @@ let
 
   inherit (hostPkgs) hostPlatform pkgsNative;
 
-  guestPkgs =
+  guestSystem =
     if hostPlatform.isLinux
-    then hostPkgs
+    then hostPkgs.system
     else
       let
         hostToGuest = {
-          "x86_64-darwin" = pkgsNative.gnu64;
-          "aarch64-darwin" = pkgsNative.aarch64-multiplatform;
+          "x86_64-darwin" = "x86_64-linux";
+          "aarch64-darwin" = "aarch64-linux";
         };
 
         supportedHosts = lib.concatStringsSep ", " (lib.attrNames hostToGuest);
@@ -47,14 +47,13 @@ let
             {
               virtualisation.qemu.package = testModuleArgs.config.qemu.package;
               virtualisation.host.pkgs = hostPkgs;
-              nixpkgs.pkgs = guestPkgs;
             })
           ({ options, ... }: {
             key = "nodes.nix-pkgs";
             config = optionalAttrs (!config.node.pkgsReadOnly) (
               mkIf (!options.nixpkgs.pkgs.isDefined) {
                 # TODO: switch to nixpkgs.hostPlatform and make sure containers-imperative test still evaluates.
-                nixpkgs.system = hostPlatform.system;
+                nixpkgs.system = guestSystem;
               }
             );
           })

commit ab45d3118634ad3cd0c319e5bdb42e31524ecb0d

Fix test failure
Specifically, the `nixosTest-example` test correctly flagged that the
change from 90679ea06c7f5ad3538d8c3940aba576865609bf broke the user's
expectation that the `pkgs` in `pkgs.nixosTest` would be reused as the
package set for the NixOS test system.

However, if we restore the original code removed by that commit:

```nix
nixpkgs.pkgs = lib.mkDefault pkgs;
```

… then that won't work because then we can't override `nixpkgs.system`
and reinstantiate that package set.  The whole *point* of this set of
changes is that we DON'T want to use the user's `pkgs` set as is
because then the wrong system is used for the NixOS test (on macOS).
Specifically, on Apple Silicon the `pkgs` in `pkgs.nixosTest` is going
to be instantiated with `system = aarch64-darwin;` but we want the
`nixpkgs.pkgs` that the NixOS machine uses to be that same package set
reinstantiated with `system = aarch64-linux;`.

Then I realized we can strike a balance by doing this instead:

```nix
nixpkgs.overlays = lib.mkDefault pkgs.overlays;
nixpkgs.config = lib.mkDefault pkgs.config;
```

… then we can get something which splits the difference: we preserve
the user's original package overrides but now we can reinstantiate
`nixpkgs.pkgs` with the new (correct) `nixpkgs.system`.

This isn't a foolproof solution, though, because `pkgs.overlays` and
`pkgs.config` do not necessarily contain all the information to
recreate the original `pkgs` that the user supplied.  As a concrete
example, if the user were to extend the package set "artificially" like
this:

```nix
(pkgs // { foo = …; }).nixosTest …
```

… then that `pkgs.foo` attribute would no longer be passed along to
`nixpgs.pkgs`.  However, in practice I believe most users are going to
be overriding `pkgs` using either the `config.packageOverrides` or
`overlays` arguments to the Nixpkgs function, so I think this is a
reasonable compromise.  Moreover, the benefit of this compromise is

pkgs/build-support/testers/default.nix
@@ -121,6 +121,22 @@
         (import ../../../nixos/lib/testing-python.nix {
           inherit (stdenv.hostPlatform) system;
           inherit pkgs;
+          extraConfigurations = [(
+            { lib, ... }: {
+              # Carefully note that we intentionally don't do this:
+              #
+              # config.nixpkgs.pkgs = lib.mkDefault pkgs;
+              #
+              # … because we want to to be able to reinstantiate `nixpkgs.pkgs`
+              # with the (possibly different) `nixpkgs.system` specified in
+              # `./nixos/lib/testing/nodes.nix`.  This means that only package
+              # overrides specified in the `overlays` or
+              # `config.packageOverrides` arguments to Nixpkgs will be passed
+              # through to `nixpkgs.pkgs`.
+              config.nixpkgs.overlays = lib.mkDefault pkgs.overlays;
+              config.nixpkgs.config = lib.mkDefault pkgs.config;
+            }
+          )];
         });
     in
       test: