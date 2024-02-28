54ccb06d85bfa9eff174fba958ff46e96c7fe96b
Bryan Lai <bryanlais@gmail.com>
2024-02-28 12:40:39 +0800

    python2Packages.wcwidth: fix build
    As of version 0.2.13 upstream still supports python2. In the future, this
    package should be dropped or pinned to the last working version after the
    final release for python2.

--- pkgs/development/python2-modules/wcwidth/default.nix
+++ pkgs/development/python2-modules/wcwidth/default.nix
@@ -6,5 +6,12 @@ wcwidth.overridePythonAttrs(oldAttrs: {
   propagatedBuildInputs = oldAttrs.propagatedBuildInputs or [] ++ [
     backports-functools-lru-cache
   ];
-})
 
+  /**
+    As of version 0.2.13 upstream still supports python2. In the future, this
+    package should be dropped or pinned to the last working version after the
+    final release for python2. See:
+      https://github.com/jquast/wcwidth/pull/117#issuecomment-1946609638
+  */
+  disabled = false;
+})
