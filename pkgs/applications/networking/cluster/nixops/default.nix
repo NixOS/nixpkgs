c5180c311eb26a913815eed888b51932235ab03c
Robert Hensing <robert@roberthensing.nl>
2024-02-28 16:32:52 +0100

    nixops_unstable_*: Add selectedPlugins attribute

--- pkgs/applications/networking/cluster/nixops/default.nix
+++ pkgs/applications/networking/cluster/nixops/default.nix
@@ -41,7 +41,7 @@ let
     '';
 
     passthru = old.passthru // {
-      inherit availablePlugins;
+      inherit availablePlugins selectedPlugins;
       inherit withPlugins python;
       tests = old.passthru.tests // {
         nixos = old.passthru.tests.nixos.passthru.override {
