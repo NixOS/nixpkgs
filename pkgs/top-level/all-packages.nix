e850b10ec9998d93747359b54676f8752baf4864
Gepardgame <thomas.degg@gmail.com>
2024-02-28 19:43:25 +0100

    liboprf: init at 0.2.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22651,6 +22651,8 @@ with pkgs;
 
   libnats-c = callPackage ../development/libraries/libnats-c { };
 
+  liboprf = callPackage ../development/libraries/liboprf { };
+
   liburing = callPackage ../development/libraries/liburing { };
 
   librseq = callPackage ../development/libraries/librseq { };
