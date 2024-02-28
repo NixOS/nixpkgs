b2092220f528c17354b71507cdaa8b88be2ac38b
Anderson Torres <torres.anderson.85@protonmail.com>
2024-02-27 22:22:49 -0300

    cask: refactor
    - makeWrapper instead of symlink
    - get rid of nested with
    - set meta.mainProgram

--- pkgs/by-name/ca/cask/package.nix
+++ pkgs/by-name/ca/cask/package.nix
@@ -1,9 +1,10 @@
 { lib
-, stdenv
-, fetchFromGitHub
 , bash
 , emacs
+, fetchFromGitHub
+, makeWrapper
 , python3
+, stdenv
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -17,9 +18,11 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-TlReq5sLVJj+pXmJSnepKQkNEWVhnh30iq4egM1HJMU=";
   };
 
-  doCheck = true;
+  nativeBuildInputs = [
+    emacs
+    makeWrapper
+  ];
 
-  nativeBuildInputs = [ emacs ];
   buildInputs = [
     bash
     python3
@@ -42,6 +45,8 @@ stdenv.mkDerivation (finalAttrs: {
 
   strictDeps = true;
 
+  doCheck = true;
+
   buildPhase = ''
     runHook preBuild
 
@@ -56,21 +61,22 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir -p $out/bin
     install -Dm444 -t $out/share/emacs/site-lisp/cask *.el *.elc
     install -Dm555 -t $out/share/emacs/site-lisp/cask/bin bin/cask
-    ln -s $out/share/emacs/site-lisp/cask/bin/cask $out/bin/cask
+    makeWrapper $out/share/emacs/site-lisp/cask/bin/cask $out/bin/cask
 
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
+    homepage = "https://github.com/cask/cask";
     description = "Project management for Emacs";
     longDescription = ''
       Cask is a project management tool for Emacs that helps automate the
       package development cycle; development, dependencies, testing, building,
       packaging and more.
     '';
-    homepage = "https://cask.readthedocs.io/en/latest/index.html";
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ AndersonTorres ];
+    license = with lib.licenses; [ gpl3Plus ];
+    mainProgram = "cask";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
     inherit (emacs.meta) platforms;
   };
 })
