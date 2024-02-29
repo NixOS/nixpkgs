bf82777340bcb9b01ebfc8e18994a67724ce78bb
Sandro <sandro.jaeckel@gmail.com>
2024-02-29 18:32:48 +0100

    gopls: 0.15.0 -> 0.15.1, set meta.changelog (#292354)
    Diff: https://github.com/golang/tools/compare/gopls/v0.15.0...gopls/v0.15.1
    Changelog: https://github.com/golang/tools/releases/tag/gopls/v0.15.1

--- pkgs/development/tools/language-servers/gopls/default.nix
+++ pkgs/development/tools/language-servers/gopls/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "gopls";
-  version = "0.15.0";
+  version = "0.15.1";
 
   src = fetchFromGitHub {
     owner = "golang";
     repo = "tools";
     rev = "gopls/v${version}";
-    hash = "sha256-Ii3c7zqMC/CeSv6X7wSgUdCkVbP+bxDuUcqPKIeE3Is=";
+    hash = "sha256-GJ2zc5OgZXwEq12f0PyvgOOUd7cctUbFvdp095VQb9E=";
   };
 
   modRoot = "gopls";
-  vendorHash = "sha256-i6Pa2cMxf97LKVy6ZVyPvjAVbQHaF84RAO0dM/dgk/Y=";
+  vendorHash = "sha256-Xxik0t1BHQPqzrE3Oh3VhODn+IqIVa+TCNqQSnmbBM0=";
 
   doCheck = false;
 
@@ -22,6 +22,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "Official language server for the Go language";
     homepage = "https://github.com/golang/tools/tree/master/gopls";
+    changelog = "https://github.com/golang/tools/releases/tag/${src.rev}";
     license = licenses.bsd3;
     maintainers = with maintainers; [ mic92 rski SuperSandro2000 zimbatm ];
     mainProgram = "gopls";
