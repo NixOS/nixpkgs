fd825615991fe71912a0c5437fc60cde78c23733
R. Ryantm <ryantm-bot@ryantm.com>
2024-02-29 05:46:45 +0000

    python311Packages.reptor: 0.9 -> 0.11

--- pkgs/development/python-modules/reptor/default.nix
+++ pkgs/development/python-modules/reptor/default.nix
@@ -32,7 +32,7 @@
 
 buildPythonPackage rec {
   pname = "reptor";
-  version = "0.9";
+  version = "0.11";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -41,7 +41,7 @@ buildPythonPackage rec {
     owner = "Syslifters";
     repo = "reptor";
     rev = "refs/tags/${version}";
-    hash = "sha256-CjTedIl7ZkSFfKKhHmCadTd7zdz3gP7Q4uCscv5An38=";
+    hash = "sha256-OJcg/e+ZC5Hf4dkP1dhsR9A+5lWvuFeuNLXyW8Hw6ko=";
   };
 
   pythonRelaxDeps = true;
