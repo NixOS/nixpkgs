4232ae387f934408d6b1eaa099b022da0e4bc08e
6543 <6543@obermui.de>
2024-02-29 01:43:42 +0100

    maintainers: add 6543
    (cherry picked from commit 5c74739ab5d5e4745bdd4095fdc8e37766ba443b)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -183,6 +183,15 @@
       fingerprint = "D292 365E 3C46 A5AA 75EE  B30B 78DB 7EDE 3540 794B";
     }];
   };
+  _6543 = {
+    email = "6543@obermui.de";
+    github = "6543";
+    githubId = 24977596;
+    name = "6543";
+    keys = [{
+      fingerprint = "8722 B61D 7234 1082 553B  201C B8BE 6D61 0E61 C862";
+    }];
+  };
   _6AA4FD = {
     email = "f6442954@gmail.com";
     github = "6AA4FD";
