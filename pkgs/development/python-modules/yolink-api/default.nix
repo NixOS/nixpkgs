4a0d046dba50b718c8610128e09939ad63a8a30f
Fabian Affolter <mail@fabian-affolter.ch>
2024-02-29 09:07:11 +0100

    python311Packages.yolink-api: 0.3.8 -> 0.3.9
    Diff: https://github.com/YoSmart-Inc/yolink-api/compare/refs/tags/v0.3.8...v0.3.9
    
    Changelog: https://github.com/YoSmart-Inc/yolink-api/releases/tag/v0.3.9

--- pkgs/development/python-modules/yolink-api/default.nix
+++ pkgs/development/python-modules/yolink-api/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "yolink-api";
-  version = "0.3.8";
+  version = "0.3.9";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "YoSmart-Inc";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-QkN6gF2Gq/gWkQxm8ahXBEsG/ClydHaaKnT3t+r5sNo=";
+    hash = "sha256-RXO++8Dh0hLkjXev/WDhPixLHKXlqRabbrPW6Hs/xoM=";
   };
 
   nativeBuildInputs = [
