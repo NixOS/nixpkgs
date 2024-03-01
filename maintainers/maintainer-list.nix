f1c8e6d5edc1ae2f4f7455fbd6c5bd655707fd15
RoseHobgoblin <earlofmagadan@Protonmail.com>
2024-03-01 15:43:53 +1300

    maintainers: add rosehobgoblin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16582,6 +16582,11 @@
       fingerprint = "1401 1B63 393D 16C1 AA9C  C521 8526 B757 4A53 6236";
     }];
   };
+  RoseHobgoblin = {
+    name = "J. L. Bowden";
+    github = "rosehobgoblin";
+    githubId = 84164410;
+  };
   rossabaker = {
     name = "Ross A. Baker";
     email = "ross@rossabaker.com";
