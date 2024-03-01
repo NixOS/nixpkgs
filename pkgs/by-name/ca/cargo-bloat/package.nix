4c9371a9956b06b333a8642fc35650f65ae97938
xrelkd <46590321+xrelkd@users.noreply.github.com>
2024-03-02 00:38:05 +0800

    cargo-bloat: add `meta.mainProgram`
    (cherry picked from commit 89304e3b2bea15be5fff830b8191f9be4570272c)
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/by-name/ca/cargo-bloat/package.nix
+++ pkgs/by-name/ca/cargo-bloat/package.nix
@@ -8,10 +8,10 @@ rustPlatform.buildRustPackage rec {
     owner = "RazrFalcon";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-lCA7C1G2xu65jn3/wzj6prWSrjQz3EqqJyMlPR/HRFs=";
+    hash = "sha256-lCA7C1G2xu65jn3/wzj6prWSrjQz3EqqJyMlPR/HRFs=";
   };
 
-  cargoSha256 = "sha256-fOenXn5gagFss9DRDXXsGxQlDqVXZ5LZcdM4WsXAyUU=";
+  cargoHash = "sha256-fOenXn5gagFss9DRDXXsGxQlDqVXZ5LZcdM4WsXAyUU=";
 
   meta = with lib; {
     description = "A tool and Cargo subcommand that helps you find out what takes most of the space in your executable";
@@ -19,6 +19,6 @@ rustPlatform.buildRustPackage rec {
     license = licenses.mit;
     platforms = platforms.unix;
     maintainers = with maintainers; [ xrelkd matthiasbeyer ];
+    mainProgram = "cargo-bloat";
   };
 }
-
