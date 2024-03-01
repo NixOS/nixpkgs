66a244df2b26788b3509362673acff68301afc4c
RoseHobgoblin <earlofmagadan@Protonmail.com>
2024-03-01 15:48:07 +1300

    Followed instructions from PR

--- pkgs/by-name/fc/fcitx5-rose-pine/package.nix
+++ pkgs/by-name/fc/fcitx5-rose-pine/package.nix
@@ -3,15 +3,15 @@
 , lib
 }:
 
-stdenvNoCC.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation {
   pname = "fcitx5-rose-pine";
-  version = "unstable-2024-03-01";
-
+  version = "0-unstable-2024-03-01";
+  
   src = fetchFromGitHub {
     owner = "rose-pine";
     repo = "fcitx5";
     rev = "148de09929c2e2f948376bb23bc25d72006403bc";
-    sha256 = "0gywb8ykb2j3zyr20w1mc3f5xfw2aw4v9b821d55w36ja753k52a";
+    hash = "0gywb8ykb2j3zyr20w1mc3f5xfw2aw4v9b821d55w36ja753k52a";
   };
 
   installPhase = ''
@@ -21,13 +21,13 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     cp -rv rose-pine* $out/share/fcitx5/themes/
 
     runHook postInstall
-
   '';
 
-  meta = with lib; {
+
+  meta = {
     description = "Fcitx5 themes based on Rosé Pine";
     homepage = "https://github.com/rose-pine/fcitx5";
-    maintainers = with maintainers; [ RoseHobgoblin ];
-    platforms = platforms.all;
+    maintainers = with lib.maintainers; [ RoseHobgoblin ];
+    platforms = lib.platforms.all;
   };
-})
+}
