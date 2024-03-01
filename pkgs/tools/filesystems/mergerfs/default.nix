75a1ff3f94d7d214f039eb3ec1a400a46836ea19
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-01 14:05:57 +0000

    mergerfs: 2.40.1 -> 2.40.2

--- pkgs/tools/filesystems/mergerfs/default.nix
+++ pkgs/tools/filesystems/mergerfs/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "mergerfs";
-  version = "2.40.1";
+  version = "2.40.2";
 
   src = fetchFromGitHub {
     owner = "trapexit";
     repo = pname;
     rev = version;
-    sha256 = "sha256-EeAgDkm8WyD9OCM8/tHydp/slDGPwCAljeOrUCIWAqQ=";
+    sha256 = "sha256-3DfSGuTtM+h0IdtsIhLVXQxX5/Tj9G5Qcha3DWmyyq4=";
   };
 
   nativeBuildInputs = [
