cf161bf1d0b00408b6a67311869b36376faf8171
Robert Schütz <nix@dotlambda.de>
2024-03-01 08:48:09 -0800

    nextcloud26: 26.0.11 -> 26.0.12
    Changelog: https://nextcloud.com/changelog/#26-0-12
    (cherry picked from commit 51f28cf7f2be2f76c09eecebded1ec432a191af9)

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -55,8 +55,8 @@ in {
   '';
 
   nextcloud26 = generic {
-    version = "26.0.11";
-    hash = "sha256-Vc0QCCy495oYeRnpWaYwc4Nf4D/ko4VsODoKcS7YADA=";
+    version = "26.0.12";
+    hash = "sha256-fuTAIAJB9pRfMd0Ewh19FmY0Vj4MuH1iMkkS1BiTI0w=";
     packages = nextcloud26Packages;
   };
 
