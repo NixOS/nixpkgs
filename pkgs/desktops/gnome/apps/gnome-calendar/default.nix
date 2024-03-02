470270be8e2e469d1ca67fec4eb30893da4b27bd
Maxine Aubrey <max@ine.dev>
2024-03-02 15:42:07 +0100

    gnome.gnome-calendar: 45.1 → 46.0
    https://gitlab.gnome.org/GNOME/gnome-calendar/-/compare/45.1...46.0
    
    Changelog-Reviewed-By: Maxine Aubrey <max@ine.dev>

--- pkgs/desktops/gnome/apps/gnome-calendar/default.nix
+++ pkgs/desktops/gnome/apps/gnome-calendar/default.nix
@@ -21,11 +21,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-calendar";
-  version = "45.1";
+  version = "46.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "f6hQdUOGWqdDK7UxmDDIcVi1RHygnMpFtgfcZ5bHEAg=";
+    hash = "sha256-XiGWDBdL2GBtkIm/eccPMQcKtIN5GYeLANsvFK+f5xg=";
   };
 
   nativeBuildInputs = [
