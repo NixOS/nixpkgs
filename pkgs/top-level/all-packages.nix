960274c9685804739e9a8fa6d3bc77ddca572010
Wolfgang Walther <walther@technowledgy.de>
2024-03-02 12:19:25 +0100

    postgresql: refactor to split up default.nix (1/2)
    This just renames default.nix to generic.nix, because the biggest chunk
    of code should move that way in the next commit. This gives us a much
    better diff for the next commit and makes rebasing **much** easier in
    case of changes. This commit does not stand on its own and needs to go
    in with the next commit (2/2).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26726,7 +26726,7 @@ with pkgs;
 
   timescaledb-tune = callPackage ../development/tools/database/timescaledb-tune { };
 
-  inherit (import ../servers/sql/postgresql pkgs)
+  inherit (import ../servers/sql/postgresql/generic.nix pkgs)
     postgresql_12
     postgresql_13
     postgresql_14
