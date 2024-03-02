6e2ec958ec8d598ef1454711d9b4a8c8787b0b1c
Gerg-L <GregLeyda@proton.me>
2024-03-01 20:17:44 -0500

    spotify-tray: 1.3.2 -> 1.3.2-unstable-2024-02-10

--- pkgs/by-name/sp/spotify-tray/package.nix
+++ pkgs/by-name/sp/spotify-tray/package.nix
@@ -1,34 +1,28 @@
-{ lib, stdenv, fetchFromGitHub, fetchpatch, autoreconfHook, pkg-config, gtk3 }:
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config, gtk3 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "spotify-tray";
-  version = "1.3.2";
+  version = "1.3.2-unstable-2024-02-10";
 
   src = fetchFromGitHub {
     owner = "tsmetana";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-E86rA8cBjy/bI7sZHlT40o7i23PcONXT5GTHEfcaDf0=";
+    repo = "spotify-tray";
+    rev = "faf3a72a695c5271361af13f44a65ac3ee481697";
+    hash = "sha256-OX8RUmJedPMJFOGhnfOKkHDU9P34XCQCvzd/MPwi52M=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "fix-building-with-automake-1.16.5.patch";
-      url = "https://github.com/tsmetana/spotify-tray/commit/1305f473ba4a406e907b98c8255f23154f349613.patch";
-      sha256 = "sha256-u2IopfMzNCu2F06RZoJw3OAsRxxZYdIMnKnyb7/KBgk=";
-    })
-  ];
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
 
   buildInputs = [ gtk3 ];
 
-  meta = with lib; {
+  strictDeps = true;
+
+  meta = {
     homepage = "https://github.com/tsmetana/spotify-tray";
     description = "Adds a tray icon to the Spotify Linux client application.";
-    license = licenses.gpl3Only;
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ Enzime ];
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ Enzime ];
     mainProgram = "spotify-tray";
   };
 }
