72f9fbafb48ec0f39e05141c400af4639a1070c6
Maxine Aubrey <max@ine.dev>
2024-03-02 16:49:14 +0100

    pango: 1.51.2 → 1.52.2
    https://gitlab.gnome.org/GNOME/pango/-/compare/1.51.2...1.52.2
    
    Changelog-Reviewed-By: Maxine Aubrey <max@ine.dev>

--- pkgs/development/libraries/pango/default.nix
+++ pkgs/development/libraries/pango/default.nix
@@ -24,13 +24,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pango";
-  version = "1.51.2";
+  version = "1.52.2";
 
   outputs = [ "bin" "out" "dev" ] ++ lib.optional withIntrospection "devdoc";
 
   src = fetchurl {
     url = with finalAttrs; "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-PbpAfytfwRfhkvMCXwocyO3B/ZuTSxxXiyuXNCE5QVo=";
+    hash = "sha256-0Adq/gEIKBS4U97smfk0ns5fLOg5CLjlj/c2tB94qWs=";
   };
 
   depsBuildBuild = [
