7ca2767f5a53f2a26ce4be588b7f8a3cd8b62da9
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-02 09:34:38 +0100

    python312Packages.pybrowsers: update disabled

--- pkgs/development/python-modules/pybrowsers/default.nix
+++ pkgs/development/python-modules/pybrowsers/default.nix
@@ -11,7 +11,7 @@ buildPythonPackage rec {
   version = "0.6.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "roniemartinez";
