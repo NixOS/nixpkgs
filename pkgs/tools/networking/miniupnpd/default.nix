ca2c7e00e8f486a80542e3e9d89dd71d85b140ba
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-02 23:24:12 +0000

    miniupnpd: 2.3.4 -> 2.3.5

--- pkgs/tools/networking/miniupnpd/default.nix
+++ pkgs/tools/networking/miniupnpd/default.nix
@@ -28,11 +28,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "miniupnpd";
-  version = "2.3.4";
+  version = "2.3.5";
 
   src = fetchurl {
     url = "https://miniupnp.tuxfamily.org/files/miniupnpd-${version}.tar.gz";
-    sha256 = "sha256-5zAzSPyKxCfefwTw7rdX1J3Mg2cxHuJYJVLDj11toIo=";
+    sha256 = "sha256-ljekEsM9J3jNrlYQWJM7TgtnRl/xNOlnYaexl1Gy+g8=";
   };
 
   buildInputs = [ iptables-legacy libuuid openssl ]
