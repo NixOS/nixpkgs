6d36a0c107511dcfde2a6d2d0fb7d1da5b25e66c
Maxine Aubrey <max@ine.dev>
2024-03-02 14:14:04 +0100

    tracker-miners: 3.6.0 → 3.7.rc
    https://gitlab.gnome.org/GNOME/tracker-miners/-/compare/3.6.0...3.7.rc

--- pkgs/development/libraries/tracker-miners/default.nix
+++ pkgs/development/libraries/tracker-miners/default.nix
@@ -47,11 +47,11 @@
 
 stdenv.mkDerivation rec {
   pname = "tracker-miners";
-  version = "3.6.2";
+  version = "3.7.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "Ctci89Uywh11fPSI+UKWBnnqj0V5Je+pdlbtTJ6bpP8=";
+    sha256 = "sha256-lsi2yLy03PrDUEgO0kHeqg5q+gP/Y7Tu1KmRQhy98Z4=";
   };
 
   nativeBuildInputs = [
