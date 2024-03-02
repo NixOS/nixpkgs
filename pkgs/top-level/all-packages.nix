6d101c0b376e8e12200dc7c8b165f1f367415bcc
Gerg-L <GregLeyda@proton.me>
2024-03-01 19:28:58 -0500

    spotify-tray: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34977,8 +34977,6 @@ with pkgs;
 
   spotify-qt = libsForQt5.callPackage ../applications/audio/spotify-qt { };
 
-  spotify-tray = callPackage ../applications/misc/spotify-tray { };
-
   spotify-tui = callPackage ../applications/audio/spotify-tui {
     inherit (darwin.apple_sdk.frameworks) AppKit Security;
   };
