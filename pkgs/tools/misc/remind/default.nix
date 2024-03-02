37bc2e4aa29f6107b99cff31cac2afd2b44daf4d
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-02 04:11:04 +0000

    remind: 04.02.09 -> 04.03.01

--- pkgs/tools/misc/remind/default.nix
+++ pkgs/tools/misc/remind/default.nix
@@ -15,11 +15,11 @@ let
 in
 tcl.mkTclDerivation rec {
   pname = "remind";
-  version = "04.02.09";
+  version = "04.03.01";
 
   src = fetchurl {
     url = "https://dianne.skoll.ca/projects/remind/download/remind-${version}.tar.gz";
-    sha256 = "sha256-ZEaEYLQFpwXfp7ONsUt4I/ENqPADruooFRfdtJhrhyo=";
+    sha256 = "sha256-thmjcNWa1t4sHQTUu6NotbThTJ6scqrd/UWBqELe1NM=";
   };
 
   propagatedBuildInputs = tclLibraries;
