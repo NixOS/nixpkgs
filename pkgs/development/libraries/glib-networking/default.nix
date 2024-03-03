b50916a1335f3cb449ab2a01512506519c844b71
Maxine Aubrey <max@ine.dev>
2024-03-03 01:01:32 +0100

    glib-networking: 2.78.0 → 2.78.1
    https://gitlab.gnome.org/GNOME/glib-networking/-/compare/2.78.0...2.78.1

--- pkgs/development/libraries/glib-networking/default.nix
+++ pkgs/development/libraries/glib-networking/default.nix
@@ -19,13 +19,13 @@
 
 stdenv.mkDerivation rec {
   pname = "glib-networking";
-  version = "2.78.0";
+  version = "2.78.1";
 
   outputs = [ "out" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "Uv5M6T99xRM0sQKJRZmFjSPIplrEoRELMJIFZdaNOro=";
+    sha256 = "5I8t27BJgyy7CSMFKcXkXayp8N8O2jJfgy9zeYWb8J8=";
   };
 
   patches = [
