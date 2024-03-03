430f004c085416befa0e89e4b21b0be089cec99c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-03-03 23:46:15 +0100

    osu-lazer: 2024.221.0 -> 2024.302.1

--- pkgs/games/osu-lazer/default.nix
+++ pkgs/games/osu-lazer/default.nix
@@ -17,13 +17,13 @@
 
 buildDotnetModule rec {
   pname = "osu-lazer";
-  version = "2024.221.0";
+  version = "2024.302.1";
 
   src = fetchFromGitHub {
     owner = "ppy";
     repo = "osu";
     rev = version;
-    hash = "sha256-isRGruIMU05/4WDI0jkgycmA0sxkBL4pu9fclTr8VtY=";
+    hash = "sha256-4+ACEWYgXvr8eoRJZHmTq74Bh159V+p2K+Pn1IPi2l4=";
   };
 
   projectFile = "osu.Desktop/osu.Desktop.csproj";
