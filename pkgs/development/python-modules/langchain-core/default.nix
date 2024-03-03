e878ba5ff7dfbdd56096bbc8bc05199c4f4fccdb
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-03 02:07:14 +0000

    python311Packages.langchain-core: 0.1.27 -> 0.1.28

--- pkgs/development/python-modules/langchain-core/default.nix
+++ pkgs/development/python-modules/langchain-core/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "langchain-core";
-  version = "0.1.27";
+  version = "0.1.28";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "langchain_core";
     inherit version;
-    hash = "sha256-aYQUIjUlwLwTDYWmFOFJOQXViKty/gya07U3sdxiBn8=";
+    hash = "sha256-BOdhpRMgC25bWBhhOCGUV5nAe8U0kIfXaS5QgjEHydY=";
   };
 
   pythonRelaxDeps = [
