ff23881f23dcd6595efd0bf388851845dac570b5
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2024-03-04 02:32:17 +0100

    ansilove: init at 4.2.0

---
+++ pkgs/by-name/an/ansilove/package.nix
@@ -0,0 +1,29 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, libansilove
+}:
+
+stdenv.mkDerivation rec {
+  pname = "ansilove";
+  version = "4.2.0";
+
+  src = fetchFromGitHub {
+    owner = "ansilove";
+    repo = pname;
+    rev = version;
+    hash = "sha256-cIJBerIbVY/V2LVupBLapmeHDWlBd49M5IjKPFM5OcE=";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ libansilove ];
+
+  meta = with lib; {
+    mainProgram = "ansilove";
+    description = "ANSI and ASCII art to PNG converter in C";
+    homepage = "https://www.ansilove.org/";
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ kip93 ];
+  };
+}
