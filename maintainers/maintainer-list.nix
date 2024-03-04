0cd750792444b10fc7420efb5ad7b17c09005143
Nuno Alves <nuno.alves02@gmail.com>
2024-03-04 10:37:07 +0000

    maintainers: add nalves599

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13459,6 +13459,12 @@
     githubId = 1009523;
     name = "Ashijit Pramanik";
   };
+  nalves599 = {
+    name = "Nuno Alves";
+    email = "me@nalves.pt";
+    github = "nalves599";
+    githubId = 50085568;
+  };
   name-snrl = {
     github = "name-snrl";
     githubId = 72071763;
