90a60d7a79b318868d61e594dd63da80fd96b896
Leandro Emmanuel Reina Kiperman <leandro@kip93.net>
2024-03-04 02:31:24 +0100

    libansilove: init at 1.4.1

---
+++ pkgs/by-name/li/libansilove/package.nix
@@ -0,0 +1,27 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, gd
+}:
+
+stdenv.mkDerivation rec {
+  pname = "libansilove";
+  version = "1.4.1";
+
+  src = fetchFromGitHub {
+    owner = "ansilove";
+    repo = pname;
+    rev = version;
+    hash = "sha256-5ieahoxxT+7O47ZNP0hRzUOSCg9ayTqDq0soMhmVNpk=";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ gd ];
+  meta = with lib; {
+    description = "Library for converting ANSI, ASCII, and other formats to PNG";
+    homepage = "https://www.ansilove.org/";
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ kip93 ];
+  };
+}
