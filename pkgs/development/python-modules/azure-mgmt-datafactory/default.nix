c3b626a3aa723d697cdd0baa3b508e208fd7b240
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-04 20:09:31 +0100

    python311Packages.azure-mgmt-datafactory: refactor

--- pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
+++ pkgs/development/python-modules/azure-mgmt-datafactory/default.nix
@@ -5,12 +5,13 @@
 , fetchPypi
 , isodate
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "azure-mgmt-datafactory";
   version = "6.0.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.8";
 
@@ -19,10 +20,14 @@ buildPythonPackage rec {
     hash = "sha256-0B+K8u9M/z3edwlc8LNR8DeqV9y6vIVCmevoUIWN0YM=";
   };
 
+  nativeBuildInputs = [
+    setuptools
+  ];
+
   propagatedBuildInputs = [
-    isodate
     azure-common
     azure-mgmt-core
+    isodate
   ];
 
   # has no tests
