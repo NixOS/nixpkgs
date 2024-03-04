c39fd61be9f5c3aa3d8eb48d6a3a971fbe948bac
Sergei Trofimovich <slyich@gmail.com>
2024-03-04 22:09:19 +0000

    xorg.xauth: 1.1.2 -> 1.1.3
    Changes: https://lists.x.org/archives/xorg-announce/2024-March/003472.html

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -2158,11 +2158,11 @@ self: with self; {
   # THIS IS A GENERATED FILE.  DO NOT EDIT!
   xauth = callPackage ({ stdenv, pkg-config, fetchurl, libX11, libXau, libXext, libXmu, xorgproto, testers }: stdenv.mkDerivation (finalAttrs: {
     pname = "xauth";
-    version = "1.1.2";
+    version = "1.1.3";
     builder = ./builder.sh;
     src = fetchurl {
-      url = "mirror://xorg/individual/app/xauth-1.1.2.tar.xz";
-      sha256 = "0072ivzn4z59ysanz838nh8s4mcmdsx6q9xkvlfysv2k37ynmfkq";
+      url = "mirror://xorg/individual/app/xauth-1.1.3.tar.xz";
+      sha256 = "0cwxzq7lv932rcfad3baanr541qcjjdhyrhxy0bzjcp3pac581z7";
     };
     hardeningDisable = [ "bindnow" "relro" ];
     strictDeps = true;
