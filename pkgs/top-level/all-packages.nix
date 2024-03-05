5ffadd504aa886db3b911239830b0ba3dab89272
Yaya <github@uwu.is>
2024-03-05 06:07:29 +0100

    snipe-it: Move to pkgs/by-name/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27039,7 +27039,7 @@ with pkgs;
 
   smcroute = callPackage ../servers/smcroute { };
 
-  snipe-it = callPackage ../servers/web-apps/snipe-it {
+  snipe-it = callPackage ../by-name/sn/snipe-it/package.nix {
     php = php81;
   };
 
