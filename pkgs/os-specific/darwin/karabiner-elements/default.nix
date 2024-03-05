3c51197c4cacfacd63cdd6da6bd553b3c3d195f9
Alexis Hildebrandt <afh@surryhill.net>
2024-03-05 11:59:37 +0100

    treewide: refactor to use darwin.installBinaryPackage (karabiner-elements)

--- pkgs/os-specific/darwin/karabiner-elements/default.nix
+++ pkgs/os-specific/darwin/karabiner-elements/default.nix
@@ -1,43 +1,39 @@
-{ lib, stdenv, fetchurl, cpio, xar, undmg }:
+{ lib, stdenvNoCC, fetchurl, darwin }:
 
-stdenv.mkDerivation rec {
+darwin.installBinaryPackage rec {
   pname = "karabiner-elements";
   version = "14.13.0";
 
   src = fetchurl {
     url = "https://github.com/pqrs-org/Karabiner-Elements/releases/download/v${version}/Karabiner-Elements-${version}.dmg";
-    sha256 = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
+    hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
   };
 
   outputs = [ "out" "driver" ];
 
-  nativeBuildInputs = [ cpio xar undmg ];
-
-  unpackPhase = ''
-    undmg $src
-    xar -xf Karabiner-Elements.pkg
-    cd Installer.pkg
-    zcat Payload | cpio -i
-    cd ../Karabiner-DriverKit-VirtualHIDDevice.pkg
-    zcat Payload | cpio -i
-    cd ..
+  postUnpack = ''
+    pushd ''${sourceRoot}
+    unpackPkg Karabiner-Elements-${version}/Karabiner-Elements.pkg .
+    bsdtar -xf Installer.pkg/Payload -C Installer.pkg
+    bsdtar -xf Karabiner-DriverKit-VirtualHIDDevice.pkg/Payload -C Karabiner-DriverKit-VirtualHIDDevice.pkg
+    popd
   '';
 
-  sourceRoot = ".";
-
+  dontPatch = false;
   postPatch = ''
     for f in *.pkg/Library/Launch{Agents,Daemons}/*.plist; do
       substituteInPlace $f \
-        --replace "/Library/" "$out/Library/"
+        --replace-fail "/Library/" "$out/Library/"
     done
   '';
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out $driver
     cp -R Installer.pkg/Applications Installer.pkg/Library $out
     cp -R Karabiner-DriverKit-VirtualHIDDevice.pkg/Applications Karabiner-DriverKit-VirtualHIDDevice.pkg/Library $driver
-
     cp "$out/Library/Application Support/org.pqrs/Karabiner-Elements/package-version" "$out/Library/Application Support/org.pqrs/Karabiner-Elements/version"
+    runHook postInstall
   '';
 
   passthru.updateScript = ./updater.sh;
@@ -45,7 +41,6 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Karabiner-Elements is a powerful utility for keyboard customization on macOS Sierra (10.12) or later.";
     homepage = "https://karabiner-elements.pqrs.org/";
-    platforms = platforms.darwin;
     maintainers = with maintainers; [ Enzime ];
     license = licenses.unlicense;
   };
