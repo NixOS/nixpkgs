5611a3cf1b45873f23cbf10730d2bd961f2a1a96
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-05 10:31:25 +0100

    python311Packages.google-api-python-client: 2.109.0 -> 2.120.0
    Changelog: https://github.com/googleapis/google-api-python-client/releases/tag/v2.120.0

--- pkgs/development/python-modules/google-api-python-client/default.nix
+++ pkgs/development/python-modules/google-api-python-client/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "google-api-python-client";
-  version = "2.109.0";
+  version = "2.120.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0GOQwlR3w2HVJjn+AO+RLD+rja/H+/KVgMEUTpJSOnk=";
+    hash = "sha256-oMh2nK2VdnaLyzGRyx9VD2qzKQy6BCutsPsXu6A/cMw=";
   };
 
   propagatedBuildInputs = [
