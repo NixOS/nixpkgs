3b4f7005b66e4065a1359b07720cffb094797841
0x4A6F <0x4A6F@users.noreply.github.com>
2024-03-05 08:29:57 +0100

    gortr: convert to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41544,8 +41544,6 @@ with pkgs;
 
   sieveshell = with python3.pkgs; toPythonApplication managesieve;
 
-  gortr = callPackage ../servers/gortr { };
-
   stayrtr = callPackage ../servers/stayrtr { };
 
   sunshine = callPackage ../servers/sunshine { };
