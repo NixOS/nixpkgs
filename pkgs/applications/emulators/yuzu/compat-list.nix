57c96a521abd7de22af9b5f9c8053265ecf80783
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-05 16:31:06 +0000

    yuzuPackages.compat-list: unstable-2024-02-26 -> unstable-2024-03-04

--- pkgs/applications/emulators/yuzu/compat-list.nix
+++ pkgs/applications/emulators/yuzu/compat-list.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub, unstableGitUpdater }:
 stdenv.mkDerivation {
   pname = "yuzu-compatibility-list";
-  version = "unstable-2024-02-26";
+  version = "unstable-2024-03-04";
 
   src = fetchFromGitHub {
     owner = "flathub";
     repo = "org.yuzu_emu.yuzu";
-    rev = "9c2032a3c7e64772a8112b77ed8b660242172068";
-    hash = "sha256-ITh/W4vfC9w9t+TJnPeTZwWifnhTNKX54JSSdpgaoBk=";
+    rev = "4abf1d239aba843180abfed58fa8541432fece5b";
+    hash = "sha256-rBnsW8ijDEPCXzIkbfQnyhSFhIv91+jIkNPosVHJ2UY=";
   };
 
   buildCommand = ''
