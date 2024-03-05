849dd17e00fd704568c6c93904929450772c5da2
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-05 09:25:53 +0100

    checkov: 3.2.29 -> 3.2.31
    Diff: https://github.com/bridgecrewio/checkov/compare/refs/tags/3.2.29...3.2.31
    
    Changelog: https://github.com/bridgecrewio/checkov/releases/tag/3.2.31

--- pkgs/development/tools/analysis/checkov/default.nix
+++ pkgs/development/tools/analysis/checkov/default.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "checkov";
-  version = "3.2.29";
+  version = "3.2.31";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bridgecrewio";
     repo = "checkov";
     rev = "refs/tags/${version}";
-    hash = "sha256-Y3lT2udrVzQZlZY/R27wFkIiI52PyJac6STnAz6Zvsk=";
+    hash = "sha256-GJh58fTBtjhOsSlwu9687qVdceGF9iMkZ2VViH2Wmp4=";
   };
 
   patches = [
