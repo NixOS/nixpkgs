72c126e34ed83349032e386b721b6a46630c8a7c
Felix Uhl <github@mail.felix-uhl.de>
2024-03-05 16:59:59 +0100

    persepolis: fix incorrect meta fields

--- pkgs/tools/networking/persepolis/default.nix
+++ pkgs/tools/networking/persepolis/default.nix
@@ -86,10 +86,10 @@ buildPythonApplication rec {
   ];
 
   meta = with lib; {
-    description = "Persepolis Download Manager is a GUI for aria2";
+    description = "A GUI for aria2";
     mainProgram = "persepolis";
     homepage = "https://persepolisdm.github.io/";
-    license = licenses.gpl3;
+    license = licenses.gpl3Plus;
     maintainers = with maintainers; [ iFreilicht ];
   };
 }
