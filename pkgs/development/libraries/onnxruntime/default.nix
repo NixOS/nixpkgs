b581658f77d88ea3bb5649ba843e0f2f3ead01cd
Niklas Gollenstede <niklas.gollenstede@web.de>
2024-03-06 13:55:43 +0100

    python3Packages.onnxruntime: fix build with cudaSupport

--- pkgs/development/libraries/onnxruntime/default.nix
+++ pkgs/development/libraries/onnxruntime/default.nix
@@ -224,6 +224,7 @@ effectiveStdenv.mkDerivation rec {
   '';
 
   passthru = {
+    inherit cudaSupport cudaPackages; # for the python module
     protobuf = protobuf_21;
     tests = lib.optionalAttrs pythonSupport {
       python = python3Packages.onnxruntime;
