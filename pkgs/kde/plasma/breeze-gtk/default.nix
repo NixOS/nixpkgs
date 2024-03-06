35f59cc7a5022d33fc23e14ca29ae9684db8ee3e
K900 <me@0upti.me>
2024-03-06 10:43:17 +0300

    kde/plasma: apply hacks to fix build

--- pkgs/kde/plasma/breeze-gtk/default.nix
+++ pkgs/kde/plasma/breeze-gtk/default.nix
@@ -10,5 +10,10 @@ mkKdeDerivation {
   # FIXME(later): upstream
   patches = [./0001-fix-add-executable-bit.patch];
 
+  # FIXME: hack to fix build, remove for 6.0.2
+  postPatch = ''
+    substituteInPlace CMakeLists.txt --replace-fail "ECM 6.0.1" "ECM 6.0.0"
+  '';
+
   extraNativeBuildInputs = [sass python3 python3Packages.pycairo];
 }
