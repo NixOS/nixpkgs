f8941c07083130e7bc02f9eff132f35573f1ef38
Erin Yuki Schlarb <erin-dev@ninetailed.ninja>
2024-03-06 22:38:42 +0100

    nixos/dovecot: Move suitable options from `services.dovecot2.sieve.*` to `services.dovecot2.pluginSettings`
    No legacy handlers (`mkRenamedOptionModule`/`mkChangedOptionModule`) were added
    since these options were only available in NixOS unstable.

--- nixos/modules/services/mail/dovecot.nix
+++ nixos/modules/services/mail/dovecot.nix
@@ -470,11 +470,49 @@ in
 
     pluginSettings = mkOption {
       # types.str does not coerce from packages, like `sievePipeBinScriptDirectory`.
-      type =
-        let
-          singleValueType = types.oneOf [ types.bool types.int types.str types.package ];
-        in
-        types.attrsOf (types.oneOf [ singleValueType (types.listOf singleValueType) ]);
+      type = types.submodule {
+        freeformType =
+          let
+            singleValueType = types.oneOf [ types.bool types.int types.str types.package ];
+          in
+          types.attrsOf (types.oneOf [ singleValueType (types.listOf singleValueType) ]);
+        options = {
+          sieve_plugins = mkOption {
+            default = [];
+            example = [ "sieve_extprograms" ];
+            description = "Sieve plugins to load";
+            type = types.listOf types.str;
+          };
+
+          sieve_extensions = mkOption {
+            default = [];
+            description = "Sieve extensions to enable in user scripts";
+            example = [ "+notify" "+imapflags" "+vnd.dovecot.filter" ];
+            type = types.listOf types.str;
+          };
+
+          sieve_global_extensions = mkOption {
+            default = [];
+            example = [ "+vnd.dovecot.environment" ];
+            description = "Sieve extensions to enable in global scripts";
+            type = types.listOf types.str;
+          };
+
+          sieve_pipe_bin_dir = mkOption {
+            default = sievePipeBinScriptDirectory;
+            defaultText = "Directory containing the scripts defined in {option}`services.dovecot2.sieve.pipeBins`.";
+            example = "/etc/dovecot/sieve-pipe";
+            description = ''
+              Directory containing programs available for use by the vnd.dovecot.pipe extension
+
+              Unless you want to manage your own pipe-bins directory, it is
+              recommended to add items to the
+              {option}`services.dovecot2.sieve.pipeBins` list instead.
+            '';
+            type = types.either types.path types.package;
+          };
+        };
+      };
       default = {};
       example = literalExpression ''
         {
@@ -488,6 +526,8 @@ in
         can still override any plugin settings.
 
         If you override a plugin setting, its value is cleared and you have to copy over the defaults.
+
+        See https://doc.dovecot.org/settings/plugin/ for an incomplete list of all allowed settings.
       '';
     };
 
@@ -556,27 +596,6 @@ in
     };
 
     sieve = {
-      plugins = mkOption {
-        default = [];
-        example = [ "sieve_extprograms" ];
-        description = "Sieve plugins to load";
-        type = types.listOf types.str;
-      };
-
-      extensions = mkOption {
-        default = [];
-        description = "Sieve extensions for use in user scripts";
-        example = [ "notify" "imapflags" "vnd.dovecot.filter" ];
-        type = types.listOf types.str;
-      };
-
-      globalExtensions = mkOption {
-        default = [];
-        example = [ "vnd.dovecot.environment" ];
-        description = "Sieve extensions for use in global scripts";
-        type = types.listOf types.str;
-      };
-
       scripts = mkOption {
         type = types.attrsOf types.path;
         default = {};
@@ -616,18 +635,17 @@ in
         perProtocol.imap.enable = [ "imap_quota" ];
       };
 
-      sieve.plugins =
-        optional (cfg.imapsieve.mailbox != []) "sieve_imapsieve"
-        ++ optional (cfg.sieve.pipeBins != []) "sieve_extprograms";
-
-      sieve.globalExtensions = optional (cfg.sieve.pipeBins != []) "vnd.dovecot.pipe";
-
-      pluginSettings = lib.mapAttrs (n: lib.mkDefault) ({
-        sieve_plugins = concatStringsSep " " cfg.sieve.plugins;
-        sieve_extensions = concatStringsSep " " (map (el: "+${el}") cfg.sieve.extensions);
-        sieve_global_extensions = concatStringsSep " " (map (el: "+${el}") cfg.sieve.globalExtensions);
-        sieve_pipe_bin_dir = sievePipeBinScriptDirectory;
-      } // sieveScriptSettings // imapSieveMailboxSettings);
+      pluginSettings = (lib.mapAttrs (n: lib.mkDefault) (
+        sieveScriptSettings // imapSieveMailboxSettings
+      )) // {
+        sieve_global_extensions = lib.mkBefore (
+          optional (cfg.sieve.pipeBins != []) "+vnd.dovecot.pipe"
+        );
+        sieve_plugins = lib.mkBefore (
+          optional (cfg.imapsieve.mailbox != []) "sieve_imapsieve"
+          ++ optional (cfg.sieve.pipeBins != []) "sieve_extprograms"
+        );
+      };
     };
 
     users.users = {
