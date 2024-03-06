57df47d9186ee07915cbfb7f6e33af6715740ace
Yaya <github@uwu.is>
2024-03-06 13:49:28 +0100

    snipe-it: 6.3.1 -> 6.3.3
    https://github.com/snipe/snipe-it/releases/tag/v6.3.3

--- pkgs/by-name/sn/snipe-it/package.nix
+++ pkgs/by-name/sn/snipe-it/package.nix
@@ -8,16 +8,16 @@
 
 php.buildComposerProject (finalAttrs: {
   pname = "snipe-it";
-  version = "6.3.1";
+  version = "6.3.3";
 
   src = fetchFromGitHub {
     owner = "snipe";
     repo = "snipe-it";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-/IyQeSnD3lgNpxvPG11qgyL66UhvO7acZOLzk3BQI7U=";
+    hash = "sha256-ePE55mK8woopNuRXox51I0sJGBmjF6XDfjE+k+ncoJ0=";
   };
 
-  vendorHash = "sha256-V1jiJnSe7F/4bMj/gG4cfRerfIl+eAZBARm5FgErFoE=";
+  vendorHash = "sha256-wO+hKttiI7T7C+4oSl8G0I4pQEfZpXjYspUhoaaLrAQ=";
 
   postInstall = ''
     snipe_it_out="$out/share/php/snipe-it"
