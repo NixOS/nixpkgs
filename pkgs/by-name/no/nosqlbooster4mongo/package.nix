ee4cbb965ed474c95673785131c2f73c646c150a
Erik Villarreal Gallardo <evillareal@wattwin.com>
2024-03-06 23:17:21 +0100

    nosqlbooster4mongo: init at 8.1.7

---
+++ pkgs/by-name/no/nosqlbooster4mongo/package.nix
@@ -0,0 +1,53 @@
+{
+  appimageTools,
+  lib,
+  fetchurl,
+  jdk21,
+  stdenv,
+  _7zz
+}:
+let
+  pname = "nosqlbooster4mongo";
+  version = "8.1.7";
+  src = fetchurl {
+    url = "https://s3.nosqlbooster.com/download/releasesv8/nosqlbooster4mongo-${version}.AppImage";
+    hash = "sha256-W1kODb52jOmjwW4sam1Uf284Z/OBClRMybVL/B8LEkw=";
+  };
+  appimageContents = appimageTools.extractType2 { inherit pname version src; };
+
+in appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraPkgs = pkgs: (appimageTools.defaultFhsEnvArgs.multiPkgs pkgs) ++ [
+    # Required to run DynamoDB locally
+    pkgs.xorg.libXtst
+    pkgs.libnotify
+    pkgs.gnome2.GConf
+  ];
+
+  extraInstallCommands = let
+    appimageContents = appimageTools.extract {
+      inherit pname version src;
+    };
+  in ''
+    mkdir -p $out/share/applications $out/share/pixmaps
+    mv $out/bin/${pname}-${version} $out/bin/${pname}
+
+    # Install XDG Desktop file and its icon
+    install -Dm444 ${appimageContents}/${pname}.desktop -t $out/share/applications
+    install -Dm444 ${appimageContents}/${pname}.png -t $out/share/pixmaps
+
+    # Replace wrong exec statement in XDG Desktop file
+    substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname}'
+  '';
+
+  meta = {
+    description = "Shell-centric GUI tool for MongoDB";
+    homepage = "https://nosqlbooster.com/home";
+    changelog = "https://nosqlbooster.com/blog/announcing-nosqlbooster-81/";
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ lynxeko ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
