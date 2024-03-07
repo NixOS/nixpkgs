2e767754309deebb84c29a4193c97b62098ee4b2
Robert Hensing <robert@roberthensing.nl>
2024-03-07 15:17:50 +0100

    haskellPackages.hercules-ci-cnix-*: nix_2_16 -> nix_2_19
    (cherry picked from commit 773cc494131802cc93e59a19f009f8aa22789e84)

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -1118,7 +1118,7 @@ self: super: builtins.intersectAttrs super {
   hercules-ci-cnix-store = overrideCabal
     (old: {
       passthru = old.passthru or { } // {
-        nixPackage = pkgs.nixVersions.nix_2_16;
+        nixPackage = pkgs.nixVersions.nix_2_19;
       };
     })
     (super.hercules-ci-cnix-store.override {
