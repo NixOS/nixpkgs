0b8ba08f4a7f65db27a1b93e21f4dd6daba43b7a
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-03-07 17:44:47 +0100

    python311Packages.nilearn: drop support for python 3.7

--- pkgs/development/python-modules/nilearn/default.nix
+++ pkgs/development/python-modules/nilearn/default.nix
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   version = "0.10.3";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
