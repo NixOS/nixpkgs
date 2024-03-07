ae09161a5bf79c4addc6a224d03a849d40cb86f3
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-03-07 22:47:58 +0100

    python312Packages.rope: drop support for python 3.7

--- pkgs/development/python-modules/rope/default.nix
+++ pkgs/development/python-modules/rope/default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   version = "1.12.0";
   format = "pyproject";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "python-rope";
