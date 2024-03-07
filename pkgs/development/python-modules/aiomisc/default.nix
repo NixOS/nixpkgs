a569f455d1e3e8f46780c1c30da6cd0493e9b1c1
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-07 16:56:41 +0100

    python311Packages.aiomisc: 17.3.41 -> 17.3.48
    Changelog: https://github.com/aiokitchen/aiomisc/blob/master/CHANGELOG.md

--- pkgs/development/python-modules/aiomisc/default.nix
+++ pkgs/development/python-modules/aiomisc/default.nix
@@ -22,14 +22,14 @@
 
 buildPythonPackage rec {
   pname = "aiomisc";
-  version = "17.3.41";
+  version = "17.3.48";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-thJBptiwH3jLQIYw7ucCv4xwaGPXMpE+dUwRvsyURtw=";
+    hash = "sha256-AVavnUsx/hUrT1gspfMNxtmyDLUty+ocPqRZAun036I=";
   };
 
   nativeBuildInputs = [
