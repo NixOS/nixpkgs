f4a362ed2589b8fa866b8d0e57da170a41980f3e
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-07 17:49:19 +0000

    eigenmath: unstable-2024-02-25 -> unstable-2024-03-06

--- pkgs/applications/science/math/eigenmath/default.nix
+++ pkgs/applications/science/math/eigenmath/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "eigenmath";
-  version = "unstable-2024-02-25";
+  version = "unstable-2024-03-06";
 
   src = fetchFromGitHub {
     owner = "georgeweigt";
     repo = pname;
-    rev = "4391a5bfe22d095cdf9fc12f376f64a8ffccccd9";
-    hash = "sha256-p+dnu35HGX8SgVpq5NczoZVehzfcuN+uucGurT7lWYM=";
+    rev = "ff2a5f89969e106f57ad624ac3897e06f26692d7";
+    hash = "sha256-54nw734EjICaac8PvdgiGeDWdJTCXnWVUJL2uE937E4=";
   };
 
   checkPhase = let emulator = stdenv.hostPlatform.emulator buildPackages; in ''
