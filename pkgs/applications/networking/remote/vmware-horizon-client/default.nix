d44080dded77d67d1b409a5af43cd60e422649eb
Sean Buckley <sean.bck@gmail.com>
2024-03-07 13:27:20 -0500

    vmware-horizon-client: replace buildFHSEnvChroot with buildFHSEnv

--- pkgs/applications/networking/remote/vmware-horizon-client/default.nix
+++ pkgs/applications/networking/remote/vmware-horizon-client/default.nix
@@ -1,6 +1,6 @@
 { stdenv
 , lib
-, buildFHSEnvChroot
+, buildFHSEnv
 , copyDesktopItems
 , fetchurl
 , gsettings-desktop-schemas
@@ -67,7 +67,7 @@ let
     '';
   };
 
-  vmwareFHSUserEnv = name: buildFHSEnvChroot {
+  vmwareFHSUserEnv = name: buildFHSEnv {
     inherit name;
 
     runScript = "${vmwareHorizonClientFiles}/bin/${name}_wrapper";
