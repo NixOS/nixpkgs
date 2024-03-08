519f99861f4f355734c6903a8df6e26b04ae24c3
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:43:15 +0100

    python3Packages.youseedee: 0.4.1 -> 0.5.3

--- pkgs/development/python-modules/youseedee/default.nix
+++ pkgs/development/python-modules/youseedee/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "youseedee";
-  version = "0.4.1";
+  version = "0.5.3";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-fdI4CBUSMbdKW0qg4y22wjWI6bhotgDkQdFMc9X00as=";
+    hash = "sha256-hQkI8kdropLiO86LXDy6eQma3FEg48gLldU7bFg9dzI=";
   };
 
   patches = [
