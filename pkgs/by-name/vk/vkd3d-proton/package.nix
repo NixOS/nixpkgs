c1bc800e65eb9622b408e3ec5d68df2c46338353
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-08 11:21:12 -0300

    vkd3d-proton: refactor
    - finalAttrs
    - split outputs
    - strictDeps
    - no nested with

--- pkgs/by-name/vk/vkd3d-proton/package.nix
+++ pkgs/by-name/vk/vkd3d-proton/package.nix
@@ -1,28 +1,41 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, wine, glslang }:
+{ lib
+, fetchFromGitHub
+, glslang
+, meson
+, ninja
+, stdenv
+, wine
+}:
 
-let
-  # these are both embedded in the output files
-  rev = "83308675078e9ea263fa8c37af95afdd15b3ab71";
-  # git describe --tags
-  shortRev = builtins.substring 0 8 rev;
-  realVersion = "v2.8-302-g${shortRev}";
-in
-
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "vkd3d-proton";
-  version = "unstable-2023-04-21";
-
-  nativeBuildInputs = [ meson ninja wine glslang ];
+  version = "2.8-unstable-2023-04-21";
 
   src = fetchFromGitHub {
     owner = "HansKristian-Work";
-    repo = pname;
-    inherit rev;
-    sha256 = "sha256-iLpVvYmWhqy0rbbyJoT+kxzIqp68Vsb/TkihGtQQucU=";
+    repo = "vkd3d-proton";
+    rev = "83308675078e9ea263fa8c37af95afdd15b3ab71";
     fetchSubmodules = true;
+    hash = "sha256-iLpVvYmWhqy0rbbyJoT+kxzIqp68Vsb/TkihGtQQucU=";
   };
 
-  prePatch = ''
+  outputs = [ "out" "dev" ];
+
+  nativeBuildInputs = [
+    glslang
+    meson
+    ninja
+    wine
+  ];
+
+  strictDeps = true;
+
+  prePatch = let
+    # these are both embedded in the output files
+    # git describe --tags
+    shortRev = builtins.substring 0 8 finalAttrs.src.rev;
+    realVersion = "v2.8-302-g${shortRev}";
+  in ''
     substituteInPlace meson.build \
       --replace "vkd3d_build = vcs_tag(" \
                 "vkd3d_build = vcs_tag( fallback : '${shortRev}'", \
@@ -30,12 +43,11 @@ stdenv.mkDerivation rec {
                 "vkd3d_version = vcs_tag( fallback : '${realVersion}'",
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/HansKristian-Work/vkd3d-proton";
-    description =
-      "A fork of VKD3D, which aims to implement the full Direct3D 12 API on top of Vulkan";
-    license = licenses.lgpl21;
-    maintainers = with maintainers; [ expipiplus1 ];
-    platforms = platforms.all;
+    description = "A fork of VKD3D, which aims to implement the full Direct3D 12 API on top of Vulkan";
+    license = with lib.licenses; [ lgpl21Plus ];
+    maintainers = with lib.maintainers; [ expipiplus1 ];
+    inherit (wine.meta) platforms;
   };
-}
+})
