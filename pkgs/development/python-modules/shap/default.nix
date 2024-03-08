466c708cc830ea7dabdb251b0b55a9a2ee076631
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:42:29 +0100

    python3Packages.shap: 0.43.0 -> 0.44.1
    https://github.com/slundberg/shap/releases/tag/v0.44.1

--- pkgs/development/python-modules/shap/default.nix
+++ pkgs/development/python-modules/shap/default.nix
@@ -31,7 +31,7 @@
 
 buildPythonPackage rec {
   pname = "shap";
-  version = "0.43.0";
+  version = "0.44.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -40,7 +40,7 @@ buildPythonPackage rec {
     owner = "slundberg";
     repo = "shap";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ylkpXhaLXsQiu6YMC3pUtlicptQmtjITzW+ydinB4ls=";
+    hash = "sha256-pC201Q/i3UAuJPZw0H+giebhJKVTmmKfxhFdonmkxtI=";
   };
 
   nativeBuildInputs = [
