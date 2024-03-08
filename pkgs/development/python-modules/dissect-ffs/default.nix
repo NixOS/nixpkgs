afa7596f64c21bc735ab096c8db9b49aeaf73386
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-08 09:41:46 +0100

    python311Packages.dissect-ffs: 3.6 -> 3.7
    Diff: https://github.com/fox-it/dissect.ffs/compare/refs/tags/3.6...3.7
    
    Changelog: https://github.com/fox-it/dissect.ffs/releases/tag/3.7

--- pkgs/development/python-modules/dissect-ffs/default.nix
+++ pkgs/development/python-modules/dissect-ffs/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "dissect-ffs";
-  version = "3.6";
+  version = "3.7";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "fox-it";
     repo = "dissect.ffs";
     rev = "refs/tags/${version}";
-    hash = "sha256-eyqqsOKcRmzOY+fj1FYudh9FUanD0Z59zZPtsNz6I0s=";
+    hash = "sha256-f7lXYJ2U8M+rZXrlbrAXAjizCVz6rAAZbeSg+iOmKdo=";
   };
 
   nativeBuildInputs = [
