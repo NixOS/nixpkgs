7c7d6ef459713f6f578c792e28b5b1844bb3c171
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:41:35 +0100

    python3Packages.progressbar2: 4.2.0 -> 4.4.2

--- pkgs/development/python-modules/progressbar2/default.nix
+++ pkgs/development/python-modules/progressbar2/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "progressbar2";
-  version = "4.2.0";
+  version = "4.4.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-E5OSL8tkWYlErUV1afvrSzrBie9Qta25zvMoTofjlM4=";
+    hash = "sha256-P9ouDGBpNgCmWFp4TJ07xOHaxX6Z4TP4wPXIzz3zdKI=";
   };
 
   postPatch = ''
