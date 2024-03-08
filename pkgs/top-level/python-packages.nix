75ab200389361e86b6587b43d22fb01d09d9aba4
Radik Islamov <vizid1337@gmail.com>
2024-03-08 07:24:19 +0500

    python3Packages.xdxf2html: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16447,6 +16447,8 @@ self: super: with self; {
 
   xxhash = callPackage ../development/python-modules/xxhash { };
 
+  xdxf2html = callPackage ../development/python-modules/xdxf2html { };
+
   xyzservices = callPackage ../development/python-modules/xyzservices { };
 
   y-py = callPackage ../development/python-modules/y-py { };
