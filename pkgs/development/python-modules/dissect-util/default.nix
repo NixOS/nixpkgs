38c47313be6ccd31e5676c667a55cd643de3d64e
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-08 09:42:10 +0100

    python311Packages.dissect-util: 3.14 -> 3.15
    Diff: https://github.com/fox-it/dissect.util/compare/refs/tags/3.14...3.15
    
    Changelog: https://github.com/fox-it/dissect.util/releases/tag/3.15

--- pkgs/development/python-modules/dissect-util/default.nix
+++ pkgs/development/python-modules/dissect-util/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "dissect-util";
-  version = "3.14";
+  version = "3.15";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "fox-it";
     repo = "dissect.util";
     rev = "refs/tags/${version}";
-    hash = "sha256-NzqQW6OFv+CtAxa5J2X/XGS9UCFfhMp4elvgcTrA1Wg=";
+    hash = "sha256-JIrk6YRuW5B30d9fNaRFesO01ajcSy8ErkpFtM1Coaw=";
   };
 
   nativeBuildInputs = [
