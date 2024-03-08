be8ce7e1614dc7d229bb6e5696a0fd333697ad02
Maxine Aubrey <max@ine.dev>
2024-03-08 15:40:41 +0100

    gnome.gnome-terminal: 3.50.1 → 3.51.91
    https://gitlab.gnome.org/GNOME/gnome-terminal/-/compare/3.50.1...3.51.91

--- pkgs/desktops/gnome/core/gnome-terminal/default.nix
+++ pkgs/desktops/gnome/core/gnome-terminal/default.nix
@@ -30,14 +30,14 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-terminal";
-  version = "3.50.1";
+  version = "3.51.91";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "GNOME";
     repo = "gnome-terminal";
     rev = version;
-    sha256 = "sha256-lJAzmz8tvEbr371VtYjlV4+z3cSy4QrmP0vmD5WiJD4=";
+    sha256 = "sha256-wYCbXqVBgXvYr5TBxxu5oqnta+bCrGqxMOu88JOfHI4=";
   };
 
   nativeBuildInputs = [
