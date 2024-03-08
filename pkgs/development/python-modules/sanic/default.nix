ea0ba507189fa1349bb0e79e5c29d3c1b4d0e01d
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:42:24 +0100

    python3Packages.sanic: 23.6.0 -> 23.12.1
    https://github.com/sanic-org/sanic/releases/tag/v23.12.1

--- pkgs/development/python-modules/sanic/default.nix
+++ pkgs/development/python-modules/sanic/default.nix
@@ -38,7 +38,7 @@
 
 buildPythonPackage rec {
   pname = "sanic";
-  version = "23.6.0";
+  version = "23.12.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -47,7 +47,7 @@ buildPythonPackage rec {
     owner = "sanic-org";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-Ffw92mlYNV+ikb6299uw24EI1XPpl3Ju2st1Yt/YHKw=";
+    hash = "sha256-TizjibqoLNMX0m5oPyncKgFnltXOLZUIPSzVIeKU25w=";
   };
 
   patches = [
