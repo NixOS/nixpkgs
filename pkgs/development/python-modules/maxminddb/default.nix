e827f0144af74d373c70e11c88f3e8661c30fbdf
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:40:58 +0100

    python3Packages.maxminddb: 2.5.1 -> 2.5.2
    https://github.com/maxmind/MaxMind-DB-Reader-python/blob/v2.5.2/HISTORY.rst

--- pkgs/development/python-modules/maxminddb/default.nix
+++ pkgs/development/python-modules/maxminddb/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "maxminddb";
-  version = "2.5.1";
+  version = "2.5.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-SAfTdOZFvWgzTk9Ie6haJxidvBJnqY5kSqaGp5J+BVk=";
+    hash = "sha256-s8M+T8eCHubJ9Ag3EW4Wq2F1hj1KZO7gJMW+xoZpCoc=";
   };
 
   buildInputs = [
