e7e3d60f02dae4ee6b0a3ea0116ddc51aeafc6bc
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:41:00 +0100

    python3Packages.mistletoe: 1.2.1 -> 1.3.0
    https://github.com/miyuchina/mistletoe/releases/tag/v1.3.0

--- pkgs/development/python-modules/mistletoe/default.nix
+++ pkgs/development/python-modules/mistletoe/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "mistletoe";
-  version = "1.2.1";
+  version = "1.3.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-fQwas3RwR9Fp+fxLkl0cuj9cE+rwuQw2W3LkflnQCgI=";
+    hash = "sha256-vQ54it2bRHjRnseS9tX7kIog5wq/5hGT8pF4xV062+k=";
   };
 
   pythonImportsCheck = [
