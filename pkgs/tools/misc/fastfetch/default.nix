5a83e4a22c342ac013a0303f2c2dce28b920a548
Lin Xianyi <iynaix@gmail.com>
2024-03-08 12:41:41 +0800

    fastfetch: 2.8.7 -> 2.8.8
    Changelog: https://github.com/fastfetch-cli/fastfetch/releases/tag/2.8.8
    Diff: https://github.com/fastfetch-cli/fastfetch/compare/2.8.7...2.8.8

--- pkgs/tools/misc/fastfetch/default.nix
+++ pkgs/tools/misc/fastfetch/default.nix
@@ -43,13 +43,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "2.8.7";
+  version = "2.8.8";
 
   src = fetchFromGitHub {
     owner = "fastfetch-cli";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-lJRTw8Z//x6tMpwfwSodTz7aVbnJPt3rac7AudqF+DA=";
+    hash = "sha256-IvAUlCDtrtBiaKZbhAiXqQXbpKiqIaKwMVC3NxaAqtw=";
   };
 
   outputs = [ "out" "man" ];
