9e6ae14b6165722ea6720c903918c81415a0ff50
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-08 11:47:14 -0300

    vkd3d-proton: 2.8-unstable-2023-04-21 -> 2.11.1
    Also, set AndersonTorres as maintainer.

--- pkgs/by-name/vk/vkd3d-proton/package.nix
+++ pkgs/by-name/vk/vkd3d-proton/package.nix
@@ -9,14 +9,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vkd3d-proton";
-  version = "2.8-unstable-2023-04-21";
+  version = "2.11.1";
 
   src = fetchFromGitHub {
     owner = "HansKristian-Work";
     repo = "vkd3d-proton";
-    rev = "83308675078e9ea263fa8c37af95afdd15b3ab71";
+    rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-iLpVvYmWhqy0rbbyJoT+kxzIqp68Vsb/TkihGtQQucU=";
+    hash = "sha256-NhHtuXdoccrvpUSplho1FZd47URTBWhzycXhi7Il0Mc=";
   };
 
   outputs = [ "out" "dev" ];
@@ -31,10 +31,12 @@ stdenv.mkDerivation (finalAttrs: {
   strictDeps = true;
 
   prePatch = let
-    # these are both embedded in the output files
-    # git describe --tags
-    shortRev = builtins.substring 0 8 finalAttrs.src.rev;
-    realVersion = "v2.8-302-g${shortRev}";
+    # these are both embedded in the output files; they are obtained via
+    # git describe --always --exclude='*' --abbrev=15 --dirty=0
+    # git describe --always --tags --dirty=+
+    rev = "105b5b77c9a34fd336b5c604e3c7a6cc48f39c3a";
+    shortRev = lib.substring 0 8 rev;
+    realVersion = finalAttrs.src.rev;
   in ''
     substituteInPlace meson.build \
       --replace "vkd3d_build = vcs_tag(" \
@@ -47,7 +49,7 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://github.com/HansKristian-Work/vkd3d-proton";
     description = "A fork of VKD3D, which aims to implement the full Direct3D 12 API on top of Vulkan";
     license = with lib.licenses; [ lgpl21Plus ];
-    maintainers = with lib.maintainers; [ expipiplus1 ];
+    maintainers = with lib.maintainers; [ AndersonTorres ];
     inherit (wine.meta) platforms;
   };
 })
