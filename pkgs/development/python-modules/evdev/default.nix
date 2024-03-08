fa7320dd8d2ba0be296c2356fc0837832e5327f7
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:40:05 +0100

    python3Packages.evdev: 1.6.1 -> 1.7.0
    https://github.com/gvalkov/python-evdev/blob/v1.7.0/docs/changelog.rst

--- pkgs/development/python-modules/evdev/default.nix
+++ pkgs/development/python-modules/evdev/default.nix
@@ -3,28 +3,33 @@
 , fetchPypi
 , linuxHeaders
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "evdev";
-  version = "1.6.1";
-  format = "setuptools";
+  version = "1.7.0";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-KZ24YozHOyN/wcxX08KUj6oHVuKli2GUtb+B3CCB8eM=";
+    hash = "sha256-lb0qHgxs4s16LsxubNlzb/eUs61ctU2B2MvC5BTQuHA=";
   };
+
+  patchPhase = ''
+    substituteInPlace setup.py \
+      --replace-fail /usr/include ${linuxHeaders}/include
+  '';
+
+  build-system = [
+    setuptools
+  ];
 
   buildInputs = [
     linuxHeaders
   ];
-
-  patchPhase = ''
-    substituteInPlace setup.py \
-      --replace /usr/include/linux ${linuxHeaders}/include/linux
-  '';
 
   doCheck = false;
 
