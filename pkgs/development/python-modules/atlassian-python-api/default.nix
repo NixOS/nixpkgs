4303c230e306e9d86386fcdd9344da599cb3d972
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:39:13 +0100

    python3Packages.atlassian-python-api: 3.41.3 -> 3.41.11
    https://github.com/atlassian-api/atlassian-python-api/releases/tag/3.41.11

--- pkgs/development/python-modules/atlassian-python-api/default.nix
+++ pkgs/development/python-modules/atlassian-python-api/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "atlassian-python-api";
-  version = "3.41.3";
+  version = "3.41.11";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "atlassian-api";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-MIzBuIeQfAVlllaU02htx1eufRlpe+V9Kzo+EFF45ME=";
+    hash = "sha256-yQqy+pFl5TK4CEUM+vpwl+pkUjBuc0xvqC9o1ZC5m7c=";
   };
 
   propagatedBuildInputs = [
