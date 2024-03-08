49653ab9dd63d30d1d80caba68c3c0a86e36328b
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-08 08:43:45 +0000

    aerospike: 7.0.0.4 -> 7.0.0.5

--- pkgs/servers/nosql/aerospike/default.nix
+++ pkgs/servers/nosql/aerospike/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "aerospike-server";
-  version = "7.0.0.4";
+  version = "7.0.0.5";
 
   src = fetchFromGitHub {
     owner = "aerospike";
     repo = "aerospike-server";
     rev = version;
-    hash = "sha256-CyDGJ0fM9mDNOG1CV/noaSDIh8x/duM3NhgLTnANNKA=";
+    hash = "sha256-NTZW/pBCrwhmqMNXBS34HUKENy+TJKmoFWS7LhcLM4k=";
     fetchSubmodules = true;
   };
 
