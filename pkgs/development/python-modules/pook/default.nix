6ca5de40044068ff266175461462b19d3d4e9fed
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:41:33 +0100

    python3Packages.pook: 1.3.0 -> 1.4.3
    https://github.com/h2non/pook/blob/v1.4.3/History.rst

--- pkgs/development/python-modules/pook/default.nix
+++ pkgs/development/python-modules/pook/default.nix
@@ -6,6 +6,7 @@
 , hatchling
 , jsonschema
 , pytest-asyncio
+, pytest-httpbin
 , pytestCheckHook
 , pythonOlder
 , requests
@@ -14,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "pook";
-  version = "1.3.0";
+  version = "1.4.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -23,7 +24,7 @@ buildPythonPackage rec {
     owner = "h2non";
     repo = "pook";
     rev = "refs/tags/v${version}";
-    hash = "sha256-0sS2QJcshMuxxCGlrcVHeIQnVMZbBoJfLsRIxpvl7pM=";
+    hash = "sha256-sdfkMvPSlVK7EoDUEuJbiuocOjGJygqiCiftrsjnDhU=";
   };
 
   nativeBuildInputs = [
@@ -40,6 +41,7 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [
     pytest-asyncio
+    pytest-httpbin
     pytestCheckHook
   ];
 
