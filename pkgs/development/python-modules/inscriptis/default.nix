a0c15a81b42b83b2a84fc6ad3765ecaee275d9d7
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:40:35 +0100

    python3Packages.inscriptis: 2.3.2 -> 2.5.0
    https://github.com/weblyzard/inscriptis/releases/tag/2.5.0

--- pkgs/development/python-modules/inscriptis/default.nix
+++ pkgs/development/python-modules/inscriptis/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "inscriptis";
-  version = "2.3.2";
+  version = "2.5.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "weblyzard";
     repo = "inscriptis";
     rev = "refs/tags/${version}";
-    hash = "sha256-grsyHqt7ahiNsYKcZN/c5cJaag/nTWTBcaHaXnW1SpU=";
+    hash = "sha256-9KEkXcdZ7USXfyIXGDrp4p4kJTzF2q30fvOccxF1hBU=";
   };
 
   propagatedBuildInputs = [
