20743e64b9b47d0210019b9e249730bf913a4294
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:39:02 +0100

    python3Packages.aioaladdinconnect: 0.1.58 -> 0.2.0
    https://github.com/mkmer/AIOAladdinConnect/releases/tag/0.2.0

--- pkgs/development/python-modules/aioaladdinconnect/default.nix
+++ pkgs/development/python-modules/aioaladdinconnect/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "aioaladdinconnect";
-  version = "0.1.58";
+  version = "0.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "AIOAladdinConnect";
     inherit version;
-    hash = "sha256-ymynaOKvnqqHIEuQc+5CagsaH5cHnQit8ileoUO6G+I=";
+    hash = "sha256-5vfw8SU3lWlPoMAR+byf8jpZrGmXTPoeO+DvPByjZnw=";
   };
 
   propagatedBuildInputs = [
