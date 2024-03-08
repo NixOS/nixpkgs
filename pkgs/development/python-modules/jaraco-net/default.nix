fa4baae5c8867702c8488e5b03d16a4e439cb9da
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:40:40 +0100

    python3Packages.jaraco-net: 9.3.1 -> 10.2.0
    https://github.com/jaraco/jaraco.net/blob/refs/tags/v10.2.0/CHANGES.rst

--- pkgs/development/python-modules/jaraco-net/default.nix
+++ pkgs/development/python-modules/jaraco-net/default.nix
@@ -31,7 +31,7 @@
 
 buildPythonPackage rec {
   pname = "jaraco-net";
-  version = "9.3.1";
+  version = "10.2.0";
 
   disabled = pythonOlder "3.7";
 
@@ -41,7 +41,7 @@ buildPythonPackage rec {
     owner = "jaraco";
     repo = "jaraco.net";
     rev = "refs/tags/v${version}";
-    hash = "sha256-aq5v4QlapmMTrqwNA0GtRi/xZCcyoR1giZECBsYwymw=";
+    hash = "sha256-z9+gz6Sos0uluU5icXJN9OMmWFErVrJXBvoBcKv6Wwg=";
   };
 
   nativeBuildInputs = [
