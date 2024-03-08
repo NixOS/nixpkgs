edf5b79b5cece6110e1e395622978c05969192f3
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-08 02:41:33 +0100

    python3Packages.pook: 1.3.0 -> 1.4.3
    https://github.com/h2non/pook/blob/v1.4.3/History.rst

--- pkgs/development/python-modules/pook/default.nix
+++ pkgs/development/python-modules/pook/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "pook";
-  version = "1.3.0";
+  version = "1.4.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "h2non";
     repo = "pook";
     rev = "refs/tags/v${version}";
-    hash = "sha256-0sS2QJcshMuxxCGlrcVHeIQnVMZbBoJfLsRIxpvl7pM=";
+    hash = "sha256-sdfkMvPSlVK7EoDUEuJbiuocOjGJygqiCiftrsjnDhU=";
   };
 
   nativeBuildInputs = [
