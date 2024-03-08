e44c8bc7e9f567d7dd1f8f86a69e582b6dc6974e
mjarosie <mjarosie@users.noreply.github.com>
2024-03-08 17:14:00 -0500

    openfga: init at 1.5.0

---
+++ pkgs/by-name/op/openfga/package.nix
@@ -0,0 +1,27 @@
+{ lib, buildGoModule, fetchFromGitHub, go }:
+
+buildGoModule rec {
+  pname = "openfga";
+  version = "1.5.0";
+
+  src = fetchFromGitHub {
+    owner = "openfga";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-lqAoIOnizfvZm/NjGeinXzoci+ykmzdFtLDGeNoi97c=";
+  };
+
+  vendorHash = "sha256-vUfyo7/mapAzNs6RV+GVj5gOPEpMypNNIkoUgnBYTjA=";
+
+  meta = with lib; {
+    description = "A high performance and flexible authorization/permission engine built for developers and inspired by Google Zanzibar";
+    homepage = "https://openfga.dev/";
+    license = licenses.asl20;
+    mainProgram = "openfga";
+    maintainers = with maintainers; [ mjarosie ];
+  };
+
+  buildPhase = ''
+    go build -o $out/openfga ./cmd/openfga
+  '';
+}
