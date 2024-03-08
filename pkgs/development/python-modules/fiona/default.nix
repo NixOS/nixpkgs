a939e05d3c9474ee53736997b552611d777fc005
Gaetan Lepage <gaetan@glepage.com>
2024-03-08 17:45:25 +0100

    python311Packages.fiona: 1.9.5 -> 1.9.6
    Diff: https://github.com/Toblerity/Fiona/compare/refs/tags/1.9.5...1.9.6
    
    Changelog: https://github.com/Toblerity/Fiona/blob/refs/tags/1.9.6/CHANGES.txt

--- pkgs/development/python-modules/fiona/default.nix
+++ pkgs/development/python-modules/fiona/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "fiona";
-  version = "1.9.5";
+  version = "1.9.6";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = "Toblerity";
     repo = "Fiona";
     rev = "refs/tags/${version}";
-    hash = "sha256-fq/BuyzuK4iOxdpE4h+KRH0CxOEk/wdmbb9KgCfJ1cw=";
+    hash = "sha256-MboM3IwGF8cuz+jMQ3QVZFAHjpspQ6kVJincq7OEkCM=";
   };
 
   nativeBuildInputs = [
