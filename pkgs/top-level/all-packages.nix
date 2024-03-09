f88b0e47f05428a5cdccd5dfc2ac461604aca92e
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-09 13:38:32 -0300

    freecad: prepare migration to by-name
    - change libsForQt5.callPackage to callPackage
    - bring the libsForQt5 and python3Packages into the scope of expression

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31641,21 +31641,7 @@ with pkgs;
 
   fragments = callPackage ../applications/networking/p2p/fragments { };
 
-  freecad = libsForQt5.callPackage ../applications/graphics/freecad {
-    boost = python3Packages.boost;
-    inherit (python3Packages)
-      gitpython
-      matplotlib
-      pivy
-      ply
-      pycollada
-      pyside2
-      pyside2-tools
-      python
-      pyyaml
-      scipy
-      shiboken2;
-  };
+  freecad = callPackage ../applications/graphics/freecad { };
 
   freedv = callPackage ../applications/radio/freedv {
     inherit (darwin.apple_sdk.frameworks) AppKit AVFoundation Cocoa CoreMedia;
