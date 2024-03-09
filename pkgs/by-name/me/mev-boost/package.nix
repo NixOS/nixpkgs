cec1ea1272ebb7aa07cd2cdc132c6444c050b5a0
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-09 14:00:18 +0000

    mev-boost: 1.7 -> 1.7.1

--- pkgs/by-name/me/mev-boost/package.nix
+++ pkgs/by-name/me/mev-boost/package.nix
@@ -6,12 +6,12 @@
 
 buildGoModule rec {
   pname = "mev-boost";
-  version = "1.7";
+  version = "1.7.1";
   src = fetchFromGitHub {
       owner = "flashbots";
       repo = "mev-boost";
       rev = "v${version}";
-      hash = "sha256-Z5B+PRYb6eWssgyaXpXoHOVRoMZoSAwun7s6Fh1DrfM=";
+      hash = "sha256-4Vxs1Jo7rkw9l0pXfi+J7YmzQawt7tc19I1MdHQgjBA=";
   };
 
   vendorHash = "sha256-yfWDGVfgCfsmzI5oxEmhHXKCUAHe6wWTkaMkBN5kQMw=";
