cfb85b75c6999771ab72b0ad857685c7d2af21da
Sergei Trofimovich <slyich@gmail.com>
2024-03-09 10:15:57 +0000

    s2n-tls: 1.4.5 -> 1.4.6
    Changes: https://github.com/aws/s2n-tls/releases/tag/v1.4.6

--- pkgs/development/libraries/s2n-tls/default.nix
+++ pkgs/development/libraries/s2n-tls/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "s2n-tls";
-  version = "1.4.5";
+  version = "1.4.6";
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-ZpnKMZtn9G5icH/QscP/6/fJlnb3FmKq4BTRs8P8pU0=";
+    hash = "sha256-x4/AkmkmuTKxzlk8AxbydA4GctpShsKiFTTJ8m7B4TY=";
   };
 
   nativeBuildInputs = [ cmake ];
