52afd5d25f30c6ab0a4c25597a391619656eba8a
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-09 01:52:23 +0000

    diffoscope: 259 -> 260

--- pkgs/tools/misc/diffoscope/default.nix
+++ pkgs/tools/misc/diffoscope/default.nix
@@ -79,11 +79,11 @@
 # Note: when upgrading this package, please run the list-missing-tools.sh script as described below!
 python3.pkgs.buildPythonApplication rec {
   pname = "diffoscope";
-  version = "259";
+  version = "260";
 
   src = fetchurl {
     url = "https://diffoscope.org/archive/diffoscope-${version}.tar.bz2";
-    hash = "sha256-WYgFWM6HKFt3xVcRNytQPWOf3ZpH1cG7Cghhu/AES80=";
+    hash = "sha256-jZXBX6aIArm3eFmJpr60HxlcSlVNCK/wSL1yeIl/MjQ=";
   };
 
   outputs = [
