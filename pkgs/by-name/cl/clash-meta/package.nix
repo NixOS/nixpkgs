3f9f279cefbb8e8161266eb04b3cdbc41a7a31e7
oluceps <i@oluceps.uk>
2024-03-11 02:46:10 +0800

    clash-meta: rename to mihomo (#271658)
    * mihomo: rename from clash-meta
    
    Co-authored-by: Anderson Torres <torres.anderson.85@protonmail.com>
    
    * clash-meta: reinit at 1.8.1
    
    ---------
    
    Co-authored-by: Anderson Torres <torres.anderson.85@protonmail.com>
    Co-authored-by: Weijia Wang <9713184+wegank@users.noreply.github.com>

--- pkgs/by-name/cl/clash-meta/package.nix
+++ pkgs/by-name/cl/clash-meta/package.nix
@@ -1,45 +1,13 @@
-{ lib
-, fetchFromGitHub
-, buildGoModule
-}:
+{ mihomo }:
 
-buildGoModule rec {
+mihomo.overrideAttrs (finalAttrs: previousAttrs: {
   pname = "clash-meta";
-  version = "1.18.1";
-
-  src = fetchFromGitHub {
-    owner = "MetaCubeX";
-    repo = "mihomo";
-    rev = "v${version}";
-    hash = "sha256-ezOkDrpytZQdc+Txe4eUyuWY6oipn9jIrmu7aO8lNlQ=";
-  };
-
-  vendorHash = "sha256-tvPR5kAta4MlMTwjfxwVOacRr2nVpfalbN08mfxml64=";
-
-  excludedPackages = [ "./test" ];
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X github.com/metacubex/mihomo/constant.Version=${version}"
-  ];
-
-  tags = [
-    "with_gvisor"
-  ];
-
-  # network required
-  doCheck = false;
 
   postInstall = ''
-    mv $out/bin/mihomo $out/bin/clash-meta
+    mv $out/bin/${previousAttrs.meta.mainProgram} $out/bin/${finalAttrs.meta.mainProgram}
   '';
 
-  meta = with lib; {
-    description = "A rule-based tunnel in Go. Present named mihomo";
-    homepage = "https://github.com/MetaCubeX/mihomo";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ oluceps ];
+  meta = previousAttrs.meta // {
     mainProgram = "clash-meta";
   };
-}
+})
