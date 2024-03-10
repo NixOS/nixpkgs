b7cd0d3c885afc3e087ea54ed568baaee0bee7ce
rewine <luhongxu@deepin.org>
2024-03-11 00:37:09 +0800

    qt6.wrapQtAppsNoGuiHook: init
    Unlike wrapQtAppsHook, wrapQtAppsNoGuiHook does not propagate qtwayland,
    to reduce closure size for CLI or other non-GUI applications.
    
    Co-authored-by: eclairevoyant <contactmeongithubinstead@proton.me>

--- pkgs/development/libraries/qt-6/default.nix
+++ pkgs/development/libraries/qt-6/default.nix
@@ -172,6 +172,14 @@ let
           } ./hooks/wrap-qt-apps-hook.sh)
         { };
 
+      wrapQtAppsNoGuiHook = callPackage
+        ({ makeBinaryWrapper }: makeSetupHook
+          {
+            name = "wrap-qt6-apps-no-gui-hook";
+            propagatedBuildInputs = [ makeBinaryWrapper ];
+          } ./hooks/wrap-qt-apps-hook.sh)
+        { };
+
       qmake = callPackage
         ({ qtbase }: makeSetupHook
           {
