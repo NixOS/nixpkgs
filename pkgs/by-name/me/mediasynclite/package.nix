2f43a630ec32ffd173f724d787bbccbdac42d44f
Tobi Oloke <tobzdaman619@gmail.com>
2024-03-10 12:07:41 +0000

    mediasynclite: init at 0.4.2
    Final package.nix
    
    Strict deps + wrapGAppsHook3

---
+++ pkgs/by-name/me/mediasynclite/package.nix
@@ -0,0 +1,54 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, gtk3
+, glib
+, gsettings-desktop-schemas
+, pkg-config
+, curl
+, openssl
+, jansson
+, wrapGAppsHook3
+}:
+
+stdenv.mkDerivation rec {
+  pname = "mediasynclite";
+  version = "0.4.2";
+
+  src = fetchFromGitHub {
+    owner = "iBroadcastMediaServices";
+    repo = "MediaSyncLiteLinux";
+    rev = version;
+    hash = "sha256-mS2e6zZutSEAr5gCS58t0Nzg8dCJgn/yoFMmbgnIM2I=";
+    };
+
+  buildInputs = [
+   curl
+   openssl.dev
+   jansson
+  ];
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+   gtk3
+   glib
+   gsettings-desktop-schemas
+   pkg-config
+   wrapGAppsHook3
+  ];
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  postPatch = ''
+    substitute ./src/ibmsl.c ./src/ibmsl.c --subst-var out
+  '';
+
+  meta = with lib; {
+    description = "A Linux-native graphical uploader for iBroadcast";
+    downloadPage = "https://github.com/tobz619/MediaSyncLiteLinuxNix";
+    homepage = "https://github.com/iBroadcastMediaServices/MediaSyncLiteLinux";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ tobz619 ];
+  };
+}
