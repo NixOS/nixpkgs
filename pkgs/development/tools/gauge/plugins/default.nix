46cd150135e1ecd5d4ed6549d9489fb71554b752
Marie Ramlow <me@nycode.dev>
2024-03-11 13:32:55 +0100

    gaugePlugins.dotnet: init at 0.5.7

--- pkgs/development/tools/gauge/plugins/default.nix
+++ pkgs/development/tools/gauge/plugins/default.nix
@@ -3,6 +3,7 @@ lib.makeScope pkgs.newScope (final: let
   inherit (final) callPackage;
 in {
   makeGaugePlugin = callPackage ./make-gauge-plugin.nix { };
+  dotnet = callPackage ./dotnet { };
   html-report = callPackage ./html-report { };
   java = callPackage ./java { };
 })
