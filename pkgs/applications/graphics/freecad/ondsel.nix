cd9ec71ea791628a827fddc6179dc20405a073a0
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-03-11 16:37:48 +0100

    Add ondsel branding

--- pkgs/applications/graphics/freecad/ondsel.nix
+++ pkgs/applications/graphics/freecad/ondsel.nix
@@ -36,6 +36,25 @@ let
         --replace-fail "\''${prefix}/@CMAKE_INSTALL_INCLUDEDIR@" "@CMAKE_INSTALL_FULL_INCLUDEDIR@"
     '';
 
+    postFixup =
+      let
+        feedstock = fetchFromGitHub {
+          owner = "Ondsel-Development";
+          repo = "freecad-feedstock";
+          rev = "386fab80c8aaa3bb1b9454aee8695c959d642bac";
+          hash = "sha256-H5LbcK5iXS+QFhMayvCDSfXk2ulc6BJXWVwv97nPH9o=";
+          fetchSubmodules = true;
+        };
+      in
+      ''
+        cp ${feedstock}/recipe/branding/branding.xml $out/bin
+        cp -r ${feedstock}/recipe/branding $out/share/Gui/Ondsel
+
+        mv $out/share/doc $out
+        ln -s $out/bin/FreeCAD $out/bin/freecad
+        ln -s $out/bin/FreeCADCmd $out/bin/freecadcmd
+      '';
+
     meta = with lib; {
       # mainProgram = "freecad";
       homepage = "https://ondsel.com/";
