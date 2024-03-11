6c0f9679de1fd9582add8ccd5bb66a9e31d4428b
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-11 11:02:17 -0300

    hydra-check: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -270,6 +270,10 @@ with pkgs;
     python3Packages = python310Packages;
   };
 
+  hydra-check = callPackage ../by-name/hy/hydra-check/package.nix {
+    python3Packages = python310Packages;
+  };
+
   aocd = with python3Packages; toPythonApplication aocd;
 
   aesfix = callPackage ../tools/security/aesfix { };
@@ -21800,8 +21804,6 @@ with pkgs;
   hydra-cli = callPackage ../development/tools/misc/hydra-cli { };
 
   hydraAntLogger = callPackage ../development/libraries/java/hydra-ant-logger { };
-
-  hydra-check = with python310.pkgs; toPythonApplication hydra-check;
 
   hyena = callPackage ../development/libraries/hyena { };
 
