3925fa19434f2d3677e579d334b829be8aeaa630
Jens Getreu <getreu@web.de>
2024-03-11 12:47:51 +0200

    maintainer: add getreu

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7048,6 +7048,12 @@
     github = "getpsyched";
     githubId = 43472218;
   };
+  getreu = {
+    email = "getreu@web.de";
+    github = "getreu";
+    githubId = 579082;
+    name = "Jens Getreu";
+  };
   gfrascadorio = {
     email = "gfrascadorio@tutanota.com";
     github = "gfrascadorio";
