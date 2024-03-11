dd3920c2ea1e566a5e6467601242f2f5f85ad115
Marie Ramlow <me@nycode.dev>
2024-03-11 09:47:28 +0100

    gaugePlugins.html-report: init at 4.3.1

--- pkgs/development/tools/gauge/plugins/default.nix
+++ pkgs/development/tools/gauge/plugins/default.nix
@@ -3,5 +3,6 @@ lib.makeScope pkgs.newScope (final: let
   inherit (final) callPackage;
 in {
   makeGaugePlugin = callPackage ./make-gauge-plugin.nix { };
+  html-report = callPackage ./html-report { };
   java = callPackage ./java { };
 })
