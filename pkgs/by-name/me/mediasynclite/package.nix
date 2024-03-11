1dff65912c43e16f66fc92e7e0e9804e6e724e8b
Tobi Oloke <93312805+tobz619@users.noreply.github.com>
2024-03-11 09:38:37 +0000

    url literal to string literal in package.nix

--- pkgs/by-name/me/mediasynclite/package.nix
+++ pkgs/by-name/me/mediasynclite/package.nix
@@ -30,8 +30,8 @@ pkgs.stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "A Linux-native graphical uploader for iBroadcast";
-    downloadPage = https://github.com/tobz619/MediaSyncLiteLinuxNix;
-    homepage = https://github.com/iBroadcastMediaServices/MediaSyncLiteLinux;
+    downloadPage = "https://github.com/tobz619/MediaSyncLiteLinuxNix";
+    homepage = "https://github.com/iBroadcastMediaServices/MediaSyncLiteLinux";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ tobz619 ];
   };
