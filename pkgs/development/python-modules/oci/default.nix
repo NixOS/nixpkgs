26ab1be2a9de303a7257c20e958fb67ee08c8e8d
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-12 14:23:10 +0000

    python312Packages.oci: 2.124.0 -> 2.124.1

--- pkgs/development/python-modules/oci/default.nix
+++ pkgs/development/python-modules/oci/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "oci";
-  version = "2.124.0";
+  version = "2.124.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "oracle";
     repo = "oci-python-sdk";
     rev = "refs/tags/v${version}";
-    hash = "sha256-/I86zjhQsDYljgde7L2lPFHiMykRmOgNOaqk5SxNMlg=";
+    hash = "sha256-tiTtBJIohUW80PLMGkdgsC8m4QF0lzmfqErNNb0Ede4=";
   };
 
   pythonRelaxDeps = [
