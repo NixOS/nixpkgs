b824e341f515972659d30700ca249ed777db261e
Fea <feathecutie@disroot.org>
2024-03-12 03:02:23 +0100

    maintainers: add feathecutie
    (cherry picked from commit 70cf12a6d7a7eb86a0ba05f2ad703cfe76e19c28)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6350,6 +6350,11 @@
     githubId = 541748;
     name = "Felipe Espinoza";
   };
+  feathecutie = {
+    name = "feathecutie";
+    github = "feathecutie";
+    githubId = 53912746;
+  };
   fedx-sudo = {
     email = "fedx-sudo@pm.me";
     github = "FedX-sudo";
