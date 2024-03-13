7617d6363f1b95ff3407d315e3bdb3f901cd15a5
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-12 23:35:34 -0300

    biliass: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4345,8 +4345,6 @@ with pkgs;
 
   bicon = callPackage ../applications/misc/bicon { };
 
-  biliass = with python3.pkgs; toPythonApplication biliass;
-
   bilibili = callPackage ../applications/video/bilibili { };
 
   bindfs = callPackage ../tools/filesystems/bindfs { };
