cb836e01ceed1defeafa050bd05710ff90d40b07
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-13 01:43:08 +0000

    python311Packages.llama-index-question-gen-openai: 0.10.18 -> 0.10.19

--- pkgs/development/python-modules/llama-index-core/default.nix
+++ pkgs/development/python-modules/llama-index-core/default.nix
@@ -30,7 +30,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-core";
-  version = "0.10.18";
+  version = "0.10.19";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -39,7 +39,7 @@ buildPythonPackage rec {
     owner = "run-llama";
     repo = "llama_index";
     rev = "refs/tags/v${version}";
-    hash = "sha256-xNPvaXODY159x8Fl3HRdYCdYeFNIieX5TsLTfup8Dtg=";
+    hash = "sha256-2giVN2zXvk+fqRXZB8jrzL5PYGDcL3s/vRwnaC1ZIvE=";
   };
 
   sourceRoot = "${src.name}/${pname}";
