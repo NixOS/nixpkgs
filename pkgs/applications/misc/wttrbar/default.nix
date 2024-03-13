129532901f15822be37d7214ad5a2c4fd4b18697
Austin Horstman <khaneliman12@gmail.com>
2024-03-13 14:11:31 -0500

    wttrbar: 0.9.2 -> 0.9.3

--- pkgs/applications/misc/wttrbar/default.nix
+++ pkgs/applications/misc/wttrbar/default.nix
@@ -7,18 +7,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wttrbar";
-  version = "0.9.2";
+  version = "0.9.3";
 
   src = fetchFromGitHub {
     owner = "bjesus";
     repo = "wttrbar";
     rev = version;
-    hash = "sha256-2oUj9G82+aGXU+qB37f+lRz5rctZNnb3bK8IETrt/4g=";
+    hash = "sha256-9qAluu9W6OG/G1SmAEOe97mUS83PZL/oLYUsIJNunwY=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin (with darwin.apple_sdk_11_0.frameworks; [ Security SystemConfiguration ]);
 
-  cargoHash = "sha256-yvgqvcOxl/AmvUg6jTFtYh13sgqAWKPt2uMFHaX5OMM=";
+  cargoHash = "sha256-AVlI2Yi3Gx9jCgP2O5NfaTvUFHdw6HPRmsMqbPFvxf8=";
 
   meta = {
     description = "A simple but detailed weather indicator for Waybar using wttr.in";
