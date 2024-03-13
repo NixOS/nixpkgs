d8545300ef41072615cf010d51fcfdbbe3d09a55
Yureka <yuka@yuka.dev>
2024-03-13 19:15:18 +0100

    electron_29: init at 29.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18297,6 +18297,7 @@ with pkgs;
   electron_26 = if lib.meta.availableOn stdenv.hostPlatform electron-source.electron_26 then electron-source.electron_26 else electron_26-bin;
   electron_27 = if lib.meta.availableOn stdenv.hostPlatform electron-source.electron_27 then electron-source.electron_27 else electron_27-bin;
   electron_28 = if lib.meta.availableOn stdenv.hostPlatform electron-source.electron_28 then electron-source.electron_28 else electron_28-bin;
+  electron_29 = electron-source.electron_29;
   electron = electron_28;
 
   autobuild = callPackage ../development/tools/misc/autobuild { };
