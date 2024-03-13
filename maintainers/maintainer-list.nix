f2d82a2cfd9057c424f52f455ef5b357a419b501
Henning Kiel <henning.kiel@gmail.com>
2024-03-13 18:12:28 +0100

    maintainers: add hennk

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7720,6 +7720,12 @@
     matrix = "@chris:netsoj.nl";
     name = "Chris Josten";
   };
+  hennk = {
+    email = "henning.kiel@gmail.com";
+    github = "hennk";
+    githubId = 328259;
+    name = "Henning Kiel";
+  };
   henrikolsson = {
     email = "henrik@fixme.se";
     github = "henrikolsson";
