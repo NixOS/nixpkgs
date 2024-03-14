28897450f4786eb6fec0a6fcc708fc91a9c250fd
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-14 17:19:24 +0100

    python311Packages.pykaleidescape: refactor

--- pkgs/development/python-modules/pykaleidescape/default.nix
+++ pkgs/development/python-modules/pykaleidescape/default.nix
@@ -7,21 +7,26 @@
 , pytestCheckHook
 , pythonAtLeast
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "pykaleidescape";
   version = "1.0.1";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "SteveEasley";
-    repo = pname;
+    repo = "pykaleidescape";
     rev = "refs/tags/v${version}";
     hash = "sha256-KM/gtpsQ27QZz2uI1t/yVN5no0zp9LZag1duAJzK55g=";
   };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
 
   propagatedBuildInputs = [
     aiohttp
