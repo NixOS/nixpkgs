3bea2b1f5eb27d8fbc0e256b9dff5869494dde6e
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-14 04:56:38 +0000

    svt-av1: 1.8.0 -> 2.0.0

--- pkgs/tools/video/svt-av1/default.nix
+++ pkgs/tools/video/svt-av1/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "svt-av1";
-  version = "1.8.0";
+  version = "2.0.0";
 
   src = fetchFromGitLab {
     owner = "AOMediaCodec";
     repo = "SVT-AV1";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-JV65VuEPJBrADsGviBnE6EgZmbqJ4Z4qli6cAfUMmkw=";
+    hash = "sha256-yfKnkO8GPmMpTWTVYDliERouSFgQPe3CfJmVussxfHY=";
   };
 
   nativeBuildInputs = [
