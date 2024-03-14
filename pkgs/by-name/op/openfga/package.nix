2302a3b07e66cb9aea4be093fcd3f55607c9d67d
Maciej J <mjarosie@users.noreply.github.com>
2024-03-14 02:09:14 +0000

    add go linker flags and specify bin path in subpackages instead of buildPhase
    Co-authored-by: Nikolay Korotkiy <sikmir@disroot.org>

--- pkgs/by-name/op/openfga/package.nix
+++ pkgs/by-name/op/openfga/package.nix
@@ -12,7 +12,9 @@ buildGoModule rec {
   };
 
   vendorHash = "sha256-vUfyo7/mapAzNs6RV+GVj5gOPEpMypNNIkoUgnBYTjA=";
+  subPackages = [ "./cmd/openfga" ];
 
+  ldflags = [ "-s" "-w" "-X github.com/openfga/openfga/internal/build.Version=${version}" ];
   meta = with lib; {
     description = "A high performance and flexible authorization/permission engine built for developers and inspired by Google Zanzibar";
     homepage = "https://openfga.dev/";
@@ -21,7 +23,4 @@ buildGoModule rec {
     maintainers = with maintainers; [ mjarosie ];
   };
 
-  buildPhase = ''
-    go build -o $out/openfga ./cmd/openfga
-  '';
 }
