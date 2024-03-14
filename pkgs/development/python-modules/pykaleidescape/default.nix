c18d322dc6f2020b20ac1b552a396f9ea01a0d9c
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-14 17:15:37 +0100

    python311Packages.pykaleidescape: 2022.2.3 -> 1.0.1
    Diff: https://github.com/SteveEasley/pykaleidescape/compare/refs/tags/v2022.2.3...v1.0.1
    
    Changelog: https://github.com/SteveEasley/pykaleidescape/releases/tag/v1.0.1

--- pkgs/development/python-modules/pykaleidescape/default.nix
+++ pkgs/development/python-modules/pykaleidescape/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "pykaleidescape";
-  version = "2022.2.3";
+  version = "1.0.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "SteveEasley";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-h5G7wV4Z+sf8Qq4GNFsp8DVDSgQgS0dLGf+DzK/egYM=";
+    hash = "sha256-KM/gtpsQ27QZz2uI1t/yVN5no0zp9LZag1duAJzK55g=";
   };
 
   propagatedBuildInputs = [
