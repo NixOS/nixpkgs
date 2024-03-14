ca35281160192bf524fb541b48fdade5cb152ae2
phaer <hello@phaer.org>
2024-03-14 12:32:23 +0100

    playwright: add browsers.json to driver

--- pkgs/development/python-modules/playwright/update.sh
+++ pkgs/development/python-modules/playwright/update.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p curl gnused nix-prefetch common-updater-scripts node2nix jq
+#!nix-shell -i bash -p curl gnused nix-prefetch common-updater-scripts node2nix jq unzip
 set -euo pipefail
 
 root="$(dirname "$(readlink -f "$0")")"
-driver_file="$root/../../web/playwright/driver.nix"
+playwright_dir="$root/../../web/playwright"
+driver_file="$playwright_dir/driver.nix"
 playwright_test="$root/../../web/playwright-test"
 
 version=$(curl ${GITHUB_TOKEN:+" -u \":$GITHUB_TOKEN\""} -s https://api.github.com/repos/microsoft/playwright-python/releases/latest | jq -r '.tag_name | sub("^v"; "")')
@@ -15,18 +16,34 @@ setup_py_url="https://github.com/microsoft/playwright-python/raw/v${version}/set
 driver_version=$(curl -Ls "$setup_py_url" | grep '^driver_version =' | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 
 fetch_driver_arch() {
-  nix-prefetch-url "https://playwright.azureedge.net/builds/driver/playwright-${driver_version}-${1}.zip"
+  nix-prefetch-url --print-path "https://playwright.azureedge.net/builds/driver/playwright-${driver_version}-${1}.zip"
 }
 
 replace_sha() {
   sed -i "s|$1 = \".\{44,52\}\"|$1 = \"$2\"|" "$driver_file"
 }
 
+linux_driver="$(fetch_driver_arch "linux")"
+linux_driver_hash="$(echo "$linux_driver" | head -1)"
+linux_driver_path="$(echo "$linux_driver" | tail -1)"
+# browsers.json is expected to include data for all platforms
+unzip -p "$linux_driver_path" package/browsers.json \
+    | jq '
+        .comment = "This file is kept up to date via update.sh"
+        | .browsers |= (
+            [.[]
+                | select(.installByDefault) | del(.installByDefault)]
+                | map({(.name): . | del(.name)})
+                |add
+        )
+    ' \
+    > "$playwright_dir/browsers.json"
+
 # Replace SHAs for the driver downloads
-replace_sha "x86_64-linux" "$(fetch_driver_arch "linux")"
-replace_sha "x86_64-darwin" "$(fetch_driver_arch "mac")"
-replace_sha "aarch64-linux" "$(fetch_driver_arch "linux-arm64")"
-replace_sha "aarch64-darwin" "$(fetch_driver_arch "mac-arm64")"
+replace_sha "x86_64-linux" "$linux_driver_hash"
+replace_sha "x86_64-darwin" "$(fetch_driver_arch "mac" | head -1)"
+replace_sha "aarch64-linux" "$(fetch_driver_arch "linux-arm64" | head -1)"
+replace_sha "aarch64-darwin" "$(fetch_driver_arch "mac-arm64" | head -1)"
 
 # Update the version stamps
 sed -i "s/version =\s*\"[^\$]*\"/version = \"$driver_version\"/" "$driver_file"
