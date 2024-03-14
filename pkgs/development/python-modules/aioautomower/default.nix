cadb7966e6f4697b4cd5d03a02004bd9ac675ec5
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-14 16:59:47 +0100

    python311Packages.aioautomower: 2024.2.10 -> 2024.3.3
    Diff: https://github.com/Thomas55555/aioautomower/compare/refs/tags/2024.2.10...2024.3.3
    
    Changelog: https://github.com/Thomas55555/aioautomower/releases/tag/2024.3.3

--- pkgs/development/python-modules/aioautomower/default.nix
+++ pkgs/development/python-modules/aioautomower/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aioautomower";
-  version = "2024.2.10";
+  version = "2024.3.3";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "Thomas55555";
     repo = "aioautomower";
     rev = "refs/tags/${version}";
-    hash = "sha256-NRcLyuU5FFIKJALUrx5iVSihzgO6ljqaqlhbs+y2E4Q=";
+    hash = "sha256-ZJRc5nzp1P3X93PxGVB55VgN1HZ2kvn43LT2wYEPxSo=";
   };
 
   postPatch = ''
