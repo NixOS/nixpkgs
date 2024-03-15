a9e897c9474e7e3016c11067588857fe4791cd8b
Elliot Cameron <nixpkgs-commits@deshaw.com>
2024-03-15 14:57:20 -0400

    libevent: add static build option

--- pkgs/development/libraries/libevent/default.nix
+++ pkgs/development/libraries/libevent/default.nix
@@ -1,6 +1,8 @@
 { lib, stdenv, fetchurl, findutils, fixDarwinDylibNames
 , sslSupport ? true, openssl
 , fetchpatch
+
+, static ? stdenv.hostPlatform.isStatic
 }:
 
 stdenv.mkDerivation rec {
@@ -20,7 +22,10 @@ stdenv.mkDerivation rec {
     })
   ];
 
-  configureFlags = lib.optional (!sslSupport) "--disable-openssl";
+  configureFlags = lib.flatten [
+    (lib.optional (!sslSupport) "--disable-openssl")
+    (lib.optionals static ["--disable-shared" "--with-pic"])
+  ];
 
   preConfigure = lib.optionalString (lib.versionAtLeast stdenv.hostPlatform.darwinMinVersion "11") ''
     MACOSX_DEPLOYMENT_TARGET=10.16
