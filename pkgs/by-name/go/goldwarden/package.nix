a5404e6de94a11b9cd4fc439dd38b6afd1d40831
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-03-15 16:32:07 +0100

    goldwarden: 0.2.13 -> 0.2.13-unstable-2024-03-14, install browser native extension files

--- pkgs/by-name/go/goldwarden/package.nix
+++ pkgs/by-name/go/goldwarden/package.nix
@@ -1,7 +1,7 @@
 { lib
-, bash
 , buildGoModule
 , fetchFromGitHub
+, fetchpatch
 , gobject-introspection
 , gtk4
 , libadwaita
@@ -13,18 +13,30 @@
 
 buildGoModule rec {
   pname = "goldwarden";
-  version = "0.2.13";
+  version = "0.2.13-unstable-2024-03-14";
 
   src = fetchFromGitHub {
     owner = "quexten";
     repo = "goldwarden";
-    rev = "v${version}";
-    hash = "sha256-4KxPtsIEW46p+cFx6yeSdNlsffy9U31k+ZSkE6V0AFc=";
+    rev = "d6e1cd263365611e520a2ef6c7847c9da19362f1";
+    hash = "sha256-IItKOmE0xHKO2u5jp7R20/T2eSvQ3QCxlzp6R4oiqf8=";
   };
 
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/quexten/goldwarden/pull/140/commits/c134a0e61d51079c44865f68ab65cfb3aea6f8f2.patch";
+      hash = "sha256-nClC/FYq3muXMeYXln+VVGUhanqElEgJRosWeSTNlmM=";
+    })
+    (fetchpatch {
+      url = "https://github.com/quexten/goldwarden/pull/140/commits/86d4f907fba241fd66d0fb3c109c0281a9766bb4.patch";
+      hash = "sha256-A8PBzfyd2blFIjCeO4xOVJMQjnEPwtK4wTcRcfsjyDk=";
+    })
+  ];
+
   postPatch = ''
-    substituteInPlace browserbiometrics/manifests.go \
-      --replace-fail "/bin/bash" "${lib.getExe bash}"
+    substituteInPlace browserbiometrics/chrome-com.8bit.bitwarden.json browserbiometrics/mozilla-com.8bit.bitwarden.json \
+      --replace-fail "@PATH@" "$out/bin/goldwarden"
+
     substituteInPlace gui/com.quexten.Goldwarden.desktop \
       --replace-fail "Exec=goldwarden_ui_main.py" "Exec=$out/bin/goldwarden-gui"
     substituteInPlace gui/src/gui/browserbiometrics.py \
@@ -70,6 +82,11 @@ buildGoModule rec {
     install -D gui/goldwarden.svg -t $out/share/icons/hicolor/scalable/apps
     install -Dm644 gui/com.quexten.Goldwarden.metainfo.xml -t $out/share/metainfo
     install -Dm644 resources/com.quexten.goldwarden.policy -t $out/share/polkit-1/actions
+
+    install -D browserbiometrics/chrome-com.8bit.bitwarden.json $out/etc/chrome/native-messaging-hosts/com.8bit.bitwarden.json
+    install -D browserbiometrics/chrome-com.8bit.bitwarden.json $out/etc/chromium/native-messaging-hosts/com.8bit.bitwarden.json
+    install -D browserbiometrics/chrome-com.8bit.bitwarden.json $out/etc/edge/native-messaging-hosts/com.8bit.bitwarden.json
+    install -D browserbiometrics/mozilla-com.8bit.bitwarden.json $out/lib/mozilla/native-messaging-hosts/com.8bit.bitwarden.json
   '';
 
   dontWrapGApps = true;
