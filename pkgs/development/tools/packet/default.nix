df69a86a837eb34c7a6540a3be02ea18fd5c41fb
Aaron Jheng <wentworth@outlook.com>
2024-03-15 12:55:43 +0800

    packet: use buildGoModule

--- pkgs/development/tools/packet/default.nix
+++ pkgs/development/tools/packet/default.nix
@@ -1,19 +1,24 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   pname = "packet";
   version = "2.2.2";
-
-  goPackagePath = "github.com/ebsarr/packet";
 
   src = fetchFromGitHub {
     owner = "ebsarr";
     repo = "packet";
     rev = "v${version}";
-    sha256 = "sha256-jm9u+LQE48aqO6CLdLZAw38woH1phYnEYpEsRbNwyKI=";
+    hash = "sha256-jm9u+LQE48aqO6CLdLZAw38woH1phYnEYpEsRbNwyKI=";
   };
 
-  goDeps = ./deps.nix;
+  vendorHash = "sha256-K+gLn6EM6tvgpBwnrlDoBW76iQE8CsIuUP/eoNm5PCs=";
+
+  preBuild = ''
+    cp ${./go.mod} go.mod
+    cp ${./go.sum} go.sum
+  '';
+
+  ldflags = [ "-s" "-w" ];
 
   meta = with lib; {
     description = "a CLI tool to manage packet.net services";
