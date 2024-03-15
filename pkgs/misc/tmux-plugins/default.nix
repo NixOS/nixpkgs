6dd60655e7b0e3373bf993d22ccd6abd82b4cc6f
Nico Domino <yo@ndo.dev>
2024-03-15 16:18:59 +0100

    feat: add tmux-window-name tmuxPlugin

--- pkgs/misc/tmux-plugins/default.nix
+++ pkgs/misc/tmux-plugins/default.nix
@@ -797,4 +797,43 @@ in rec {
       sha256 = "hRvkBf+YrWycecnDixAsD4CAHg3KsioomfJ/nLl5Zgs=";
     };
   };
+
+  tmux-window-name = mkTmuxPlugin {
+    pluginName = "tmux-window-name";
+    version = "2024-03-08";
+    src = fetchFromGitHub {
+      owner = "ofirgall";
+      repo = "tmux-window-name";
+      rev = "34026b6f442ceb07628bf25ae1b04a0cd475e9ae";
+      sha256 = "sha256-BNgxLk/BkaQkGlB4g2WKVs39y4VHL1Y2TdTEoBy7yo0=";
+    };
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    rtpFilePath = "tmux_window_name.tmux";
+    postInstall = ''
+      # Update USR_BIN_REMOVER with .nix-profile PATH
+      sed -i "s|^USR_BIN_REMOVER.*|USR_BIN_REMOVER = (r\'^/home/${config.home.username}/.nix-profile/bin/(.+)( --.*)?\', r\'\\\g<1>\')|" $target/scripts/rename_session_windows.py
+
+      # Update substitute_sets with .nix-profile PATHs
+      sed -i "s|^\ssubstitute_sets: List.*|    substitute_sets: List[Tuple] = field(default_factory=lambda: [(\'/home/${config.home.username}/.nix-profile/bin/(.+) --.*\', \'\\\g<1>\'), (r\'.+ipython([32])\', r\'ipython\\\g<1>\'), USR_BIN_REMOVER, (r\'(bash) (.+)/(.+[ $])(.+)\', \'\\\g<3>\\\g<4>\')])|" $target/scripts/rename_session_windows.py
+
+      # Update dir_programs with .nix-profile PATH for applications
+      sed -i "s|^\sdir_programs: List.*|    dir_programs: List[str] = field(default_factory=lambda: [['/home/${config.home.username}/.nix-profile/bin/vim', '/home/${config.home.username}/.nix-profile/bin/vi', '/home/${config.home.username}/.nix-profile/bin/git', '/home/${config.home.username}/.nix-profile/bin/nvim']])|" $target/scripts/rename_session_windows.py
+
+      for f in tmux_window_name.tmux scripts/rename_session_windows.py; do
+        wrapProgram $target/$f \
+          --prefix PATH : ${lib.makeBinPath [(pkgs.python3.withPackages (p: with p; [
+              libtmux
+              pip
+            ]))
+          ]}
+      done
+    '';
+    meta = with lib; {
+      homepage = "https://github.com/ofirgall/tmux-window-name";
+      description = "A plugin to name your tmux windows smartly, like IDE's";
+      license = licenses.mit;
+      platforms = platforms.unix;
+      maintainers = with maintainers; [ ndom91 ];
+    };
+  };
 }
