3b3abeba5d9726c1dae1af0175c98af3fd4974e7
peterparser <massimo.schembri18@gmail.com>
2024-03-15 17:05:08 +0100

    (kwokctl): init at v0.5.1

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15055,6 +15055,12 @@
     githubId = 722550;
     name = "Peter Hoeg";
   };
+  peterparser = {
+    email = "massimo.schembri18@gmail.com";
+    github = "peterparser";
+    githubId = 32139342;
+    name = "peterparser";
+  };
   peterromfeldhk = {
     email = "peter.romfeld.hk@gmail.com";
     github = "peterromfeldhk";
