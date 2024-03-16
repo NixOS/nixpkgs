d5fb11eb623dd9580a3ffe14e66bf8c786e84aeb
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-16 19:06:55 +0000

    iosevka-bin: 29.0.1 -> 29.0.2

--- pkgs/data/fonts/iosevka/bin.nix
+++ pkgs/data/fonts/iosevka/bin.nix
@@ -17,7 +17,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "${name}-bin";
-  version = "29.0.1";
+  version = "29.0.2";
 
   src = fetchurl {
     url = "https://github.com/be5invis/Iosevka/releases/download/v${version}/PkgTTC-${name}-${version}.zip";
