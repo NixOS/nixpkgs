7307d1b54d6ced90bc94496b1de9abc419deab94
Ilya Epifanov <mail@ilya.network>
2024-03-16 20:47:48 +0100

    onagre 1.0.0

--- pkgs/applications/misc/onagre/default.nix
+++ pkgs/applications/misc/onagre/default.nix
@@ -5,34 +5,36 @@
 , pkgconf
 , freetype
 , expat
+, wayland
+, xorg
+, libxkbcommon
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "onagre";
-  version = "1.0.0-alpha.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
-    owner = "oknozor";
+    owner = "onagre-launcher";
     repo = pname;
     rev = version;
-    hash = "sha256-hP+slfCWgsTgR2ZUjAmqx9f7+DBu3MpSLvaiZhqNK1Q=";
+    hash = "sha256-FqmOcmq0yNxTXZRNPA5MpsTAm4cxXpvU99yPPhihayI=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "pop-launcher-1.2.1" = "sha256-LeKaJIvooD2aUlY113P0mzxOcj63sGkrA0SIccNqCLY=";
-    };
-  };
-
-  cargoSha256 = "sha256-IOhAGrAiT2mnScNP7k7XK9CETUr6BjGdQVdEUvTYQT4=";
+  cargoSha256 = "sha256-xC5nf7DJCMBR2gUoB5NWZJGuZ8brs8WklCzg/osCsho=";
 
   nativeBuildInputs = [ cmake pkgconf ];
-  buildInputs = [ freetype expat ];
+  buildInputs = [ freetype expat wayland libxkbcommon xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi ];
+
+  postFixup = let
+    rpath = lib.makeLibraryPath buildInputs;
+  in ''
+    patchelf --set-rpath ${rpath} $out/bin/onagre
+  '';
 
   meta = with lib; {
     description = "A general purpose application launcher for X and wayland inspired by rofi/wofi and alfred";
-    homepage = "https://github.com/oknozor/onagre";
+    homepage = "https://github.com/onagre-launcher/onagre";
     license = licenses.mit;
     maintainers = [ maintainers.jfvillablanca ];
     platforms = platforms.linux;
