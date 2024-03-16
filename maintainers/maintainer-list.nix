3ad0d0fb9dad8d5760685e77ec3e0588307708cd
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-03-16 17:11:07 +0100

    maintainers: add aorith

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1425,6 +1425,12 @@
     githubId = 4194320;
     name = "Anton Schirg";
   };
+  aorith = {
+    email = "aomanu+nixpkgs@gmail.com";
+    github = "aorith";
+    githubId = 5411704;
+    name = "Manuel Sanchez Pinar";
+  };
   apeschar = {
     email = "albert@peschar.net";
     github = "apeschar";
