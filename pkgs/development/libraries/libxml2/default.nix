1839eb1a177ff3fb682aa65c5ace1d47976fb67c
Jan Tojnar <jtojnar@gmail.com>
2024-03-16 14:21:12 +0100

    libxml2: 2.12.5 → 2.12.6
    https://discourse.gnome.org/t/libxml2-2-12-6-released/19989
    https://gitlab.gnome.org/GNOME/libxml2/-/compare/v2.12.5...v2.12.6

--- pkgs/development/libraries/libxml2/default.nix
+++ pkgs/development/libraries/libxml2/default.nix
@@ -35,7 +35,7 @@ in
 let
 libxml = stdenv.mkDerivation (finalAttrs: rec {
   pname = "libxml2";
-  version = "2.12.5";
+  version = "2.12.6";
 
   outputs = [ "bin" "dev" "out" "doc" ]
     ++ lib.optional pythonSupport "py"
@@ -44,7 +44,7 @@ libxml = stdenv.mkDerivation (finalAttrs: rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/libxml2/${lib.versions.majorMinor version}/libxml2-${version}.tar.xz";
-    hash = "sha256-qXJ5Zpav04Bz4PWcKDw6L1pWC1JotLq8ORsoYWZSayE=";
+    hash = "sha256-iJxZOogaPbX92WzJMYyH3zTrZI7fxFgnKtRv1gc1P7s=";
   };
 
   strictDeps = true;
