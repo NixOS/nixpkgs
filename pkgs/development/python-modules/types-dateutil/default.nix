06c4378990a48998f9f34baa318e62a422d67c5b
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-16 20:09:59 +0000

    python311Packages.types-dateutil: 2.8.19.20240311 -> 2.9.0.20240316

--- pkgs/development/python-modules/types-dateutil/default.nix
+++ pkgs/development/python-modules/types-dateutil/default.nix
@@ -5,13 +5,13 @@
 
 buildPythonPackage rec {
   pname = "types-dateutil";
-  version = "2.8.19.20240311";
+  version = "2.9.0.20240316";
   format = "setuptools";
 
   src = fetchPypi {
     pname = "types-python-dateutil";
     inherit version;
-    hash = "sha256-UReCJ7vUy+w13Jrf+/Wdgy8g4JhC19y4xzsWm4eAt8s=";
+    hash = "sha256-XS8uJAuGkF5AlE3Xh9ttqSY/Deq+8Qdt2u15c1HsAgI=";
   };
 
   pythonImportsCheck = [
