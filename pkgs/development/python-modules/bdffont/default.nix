4c0c5ef79de61453c09286ab61792e15a02eb79f
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-17 06:50:22 +0000

    python311Packages.bdffont: 0.0.16 -> 0.0.17

--- pkgs/development/python-modules/bdffont/default.nix
+++ pkgs/development/python-modules/bdffont/default.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "bdffont";
-  version = "0.0.16";
+  version = "0.0.17";
 
   disabled = pythonOlder "3.11";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-2qR9uKQk9zrKpyekpZJht8uZOp8PK01sv2CYyP+BqcA=";
+    hash = "sha256-JBPo5tmwnXRzPpZbBrcW2wEC/XNd8M+mi58CRIpOVL0=";
   };
 
   format = "pyproject";
