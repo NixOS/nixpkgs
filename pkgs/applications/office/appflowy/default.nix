6f7990dde56e7378c5e4851b57cb48527fde4a25
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-17 19:33:47 +0000

    appflowy: 0.4.9 -> 0.5.2
    (cherry picked from commit 92a285029b38399978747b22dac123ac3c9c6ecf)

--- pkgs/applications/office/appflowy/default.nix
+++ pkgs/applications/office/appflowy/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "appflowy";
-  version = "0.4.9";
+  version = "0.5.2";
 
   src = fetchzip {
     url = "https://github.com/AppFlowy-IO/appflowy/releases/download/${version}/AppFlowy-${version}-linux-x86_64.tar.gz";
-    hash = "sha256-+Olmp2z5cLDgZikY2n9LI2A9W03pYdCtUE9hdr9Tp2Q=";
+    hash = "sha256-yXrdV/m6Ss9DyYleA5K7Wz1RUa8fznDJl5Yvco+jaiA=";
     stripRoot = false;
   };
 
