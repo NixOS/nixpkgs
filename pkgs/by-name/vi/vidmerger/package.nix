a64fe1f98749d858c9decfc8930cddd1c49f28ee
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-03-17 13:42:38 +0100

    vidmerger : init at 0.3.2

---
+++ pkgs/by-name/vi/vidmerger/package.nix
@@ -0,0 +1,40 @@
+{ lib
+, ffmpeg
+, rustPlatform
+, fetchFromGitHub
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "vidmerger";
+  version = "0.3.2";
+
+  src = fetchFromGitHub {
+    owner = "TGotwig";
+    repo = "vidmerger";
+    rev = version;
+    hash = "sha256-TnVDbhMPBByuhXNuKiyhq6wD3XZgp8nGtIf+7XkVmw8=";
+  };
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  postPatch = ''
+    cp ${./Cargo.lock} Cargo.lock
+  '';
+
+  # Running cargo test -- . fails
+  doCheck = false;
+
+  buildInputs = [
+    # Example Run-time Additional Dependencies
+    ffmpeg
+  ];
+  meta = with lib; {
+    description = "Merge video & audio files via CLI ";
+    homepage = "https://github.com/TGotwig/vidmerger";
+    license = licenses.free;
+    maintainers = with maintainers; [ ByteSudoer ];
+    mainProgram = "vidmerger";
+  };
+
+}
