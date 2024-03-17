7d5165e789a09a0e5fe7ba2e4e1d0574bb235142
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-17 20:32:19 +0100

    python311Packages.roadtx: init at 1.7.0
    ROADtools Token eXchange
    
    https://pypi.org/project/roadtx/

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13008,6 +13008,8 @@ self: super: with self; {
 
   roadlib = callPackage ../development/python-modules/roadlib { };
 
+  roadtx = callPackage ../development/python-modules/roadtx { };
+
   robomachine = callPackage ../development/python-modules/robomachine { };
 
   robot-detection = callPackage ../development/python-modules/robot-detection { };
