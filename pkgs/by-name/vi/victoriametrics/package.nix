NixOS/nixpkgs

commit ee411f2c1ba132a5d96199b6e3132611b7443293

VictoriaMetrics: 1.97.1 -> 1.99.0 (#293328)
Package upgrade and move to pkgs/by-name.

* https://github.com/VictoriaMetrics/VictoriaMetrics/releases/tag/v1.98.0
* https://github.com/VictoriaMetrics/VictoriaMetrics/releases/tag/v1.99.0

pkgs/by-name/vi/victoriametrics/package.nix
@@ -2,16 +2,34 @@
 
 buildGoModule rec {
   pname = "VictoriaMetrics";
-  version = "1.97.1";
+  version = "1.99.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-zaRXvktHqsM/pZd7DsCAXCSI2jaPZ3iKHLQqDILZ9pc=";
+    hash = "sha256-IHUmxdCOzvA2JL06k/ei6/OTVWHTL1TiKKYZB1hgqyA=";
   };
 
   vendorHash = null;
+
+  subPackages = [
+    "app/victoria-logs"
+    "app/victoria-metrics"
+    "app/vlinsert"
+    "app/vlselect"
+    "app/vlstorage"
+    "app/vmagent"
+    "app/vmalert-tool"
+    "app/vmauth"
+    "app/vmctl"
+    "app/vminsert"
+    "app/vmselect"
+    "app/vmstorage"
+    "app/vmbackup"
+    "app/vmrestore"
+    "app/vmui"
+  ];
 
   postPatch = ''
     # main module (github.com/VictoriaMetrics/VictoriaMetrics) does not contain package

pkgs/top-level/all-packages.nix
@@ -27162,8 +27162,6 @@ with pkgs;
 
   vouch-proxy = callPackage ../servers/vouch-proxy { };
 
-  victoriametrics = callPackage ../servers/nosql/victoriametrics { };
-
   virtiofsd = callPackage ../servers/misc/virtiofsd { };
 
   virtualenv = with python3Packages; toPythonApplication virtualenv;

commit 5a459174ce15e6f846edef049177b751d13d62f8

VictoriaMetrics: re-include vmalert
This was broken by https://github.com/NixOS/nixpkgs/pull/293328 / ee411f2c1ba132a5d96199b6e3132611b7443293

pkgs/by-name/vi/victoriametrics/package.nix
@@ -21,6 +21,7 @@ buildGoModule rec {
     "app/vlstorage"
     "app/vmagent"
     "app/vmalert-tool"
+    "app/vmalert"
     "app/vmauth"
     "app/vmctl"
     "app/vminsert"