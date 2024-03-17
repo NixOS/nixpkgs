0b15549eb297589681441cf4422997e8808f856f
Mario Rodas <marsam@users.noreply.github.com>
2024-03-17 04:20:00 +0000

    atlas: 0.19.2 -> 0.20.0
    Diff: https://github.com/ariga/atlas/compare/v0.19.2...v0.20.0
    
    Changelog: https://github.com/ariga/atlas/releases/tag/v0.20.0

--- pkgs/development/tools/database/atlas/default.nix
+++ pkgs/development/tools/database/atlas/default.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "atlas";
-  version = "0.19.2";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "ariga";
     repo = "atlas";
     rev = "v${version}";
-    hash = "sha256-P3KatCF9zoqGRh2N1Rc89wJmsE0bRM5Uoq3zTSdAhTY=";
+    hash = "sha256-PQPf3cHlKP+0PIOkR/SZ4f9sBv+gJ+bJuJl9/OLRpb0=";
   };
 
   modRoot = "cmd/atlas";
 
   proxyVendor = true;
-  vendorHash = "sha256-roMEd/R+WmsWdCT9eye0ijqCcYzSSqVFU9mRN1Sw0jk=";
+  vendorHash = "sha256-48UmLwp93SjwPPrdySFVApjVf1Rsj3oNhi1lha4ZvyE=";
 
   nativeBuildInputs = [ installShellFiles ];
 
