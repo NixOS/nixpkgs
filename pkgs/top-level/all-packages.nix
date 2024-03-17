1f57f24114a26bb2c1f4338f853318823b680a64
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-03-17 16:46:58 +0100

    hydra-check: unpin python version
    We don't support Python 3.10 anymore, and it has incurred breakages that
    blocked the build of this package.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21741,7 +21741,7 @@ with pkgs;
 
   hydraAntLogger = callPackage ../development/libraries/java/hydra-ant-logger { };
 
-  hydra-check = with python310.pkgs; toPythonApplication hydra-check;
+  hydra-check = with python3.pkgs; toPythonApplication hydra-check;
 
   hyena = callPackage ../development/libraries/hyena { };
 
