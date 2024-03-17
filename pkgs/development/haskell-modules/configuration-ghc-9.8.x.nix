73c02043c1cc59e84eafd52682c127c9e6cdf1bc
maralorn <mail@maralorn.de>
2024-03-17 13:23:16 +0100

    haskell.packages.ghc98: Drop some overrides because they might be obsolete

--- pkgs/development/haskell-modules/configuration-ghc-9.8.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.8.x.nix
@@ -59,10 +59,7 @@ self: super: {
   megaparsec = doDistribute self.megaparsec_9_6_1;
   aeson = doDistribute self.aeson_2_2_1_0;
   attoparsec-aeson = doDistribute self.attoparsec-aeson_2_2_0_1;
-  ormolu = doDistribute self.ormolu_0_7_3_0;
-  fourmolu = doDistribute (dontCheck self.fourmolu_0_14_1_0);
   xmonad = doDistribute self.xmonad_0_18_0;
-  hlint = doDistribute self.hlint_3_8;
 
   #
   # Jailbreaks
