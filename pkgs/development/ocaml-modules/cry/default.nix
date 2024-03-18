8362f8da098ac0075bd8c18ce7d2adf9c4a62aaf
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-18 17:43:33 +0000

    ocamlPackages.cry: 1.0.2 -> 1.0.3

--- pkgs/development/ocaml-modules/cry/default.nix
+++ pkgs/development/ocaml-modules/cry/default.nix
@@ -2,13 +2,13 @@
 
 buildDunePackage rec {
   pname = "cry";
-  version = "1.0.2";
+  version = "1.0.3";
 
   src = fetchFromGitHub {
     owner = "savonet";
     repo = "ocaml-cry";
     rev = "v${version}";
-    hash = "sha256-wtilYOUOHElW8ZVxolMNomvT//ho2tACmoubEvU2bpQ=";
+    hash = "sha256-ea6f2xTVmYekPmzAKasA9mNG4Voxw2MCkfZ9LB9gwbo=";
   };
 
   postPatch = ''
