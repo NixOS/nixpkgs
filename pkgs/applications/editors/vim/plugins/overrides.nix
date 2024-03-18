d007e76ff390a1a7dd3923b507acbf45d3af8e89
Gaetan Lepage <gaetan@glepage.com>
2024-03-18 13:28:17 +0100

    vimPlugins.neotest-gradle: add plenary-nvim dependency

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -947,6 +947,10 @@
     dependencies = with self; [ plenary-nvim ];
   };
 
+  neotest-gradle = super.neotest-gradle.overrideAttrs {
+    dependencies = with self; [ plenary-nvim ];
+  };
+
   neo-tree-nvim = super.neo-tree-nvim.overrideAttrs {
     dependencies = with self; [ plenary-nvim nui-nvim ];
   };
