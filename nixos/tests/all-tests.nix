20defc53beef4a0e220795cda1bc357e1e0987d8
oluceps <i@oluceps.uk>
2024-03-19 01:43:22 +0800

    nixos/juicity: add test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -443,6 +443,7 @@ in {
   jirafeau = handleTest ./jirafeau.nix {};
   jitsi-meet = handleTest ./jitsi-meet.nix {};
   jool = import ./jool.nix { inherit pkgs runTest; };
+  juicity = handleTest ./juicity.nix {};
   k3s = handleTest ./k3s {};
   kafka = handleTest ./kafka.nix {};
   kanidm = handleTest ./kanidm.nix {};
