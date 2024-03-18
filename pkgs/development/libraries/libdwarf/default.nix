642ad65e474358a59f876862d0eeab0459d1e114
Thomas Gerbet <thomas@gerbet.me>
2024-03-18 23:15:24 +0100

    libdwarf: 0.9.0 -> 0.9.1
    Changes:
    https://github.com/davea42/libdwarf-code/releases/tag/v0.9.1

--- pkgs/development/libraries/libdwarf/default.nix
+++ pkgs/development/libraries/libdwarf/default.nix
@@ -4,9 +4,9 @@
 }:
 
 callPackage ./common.nix rec {
-  version = "0.9.0";
+  version = "0.9.1";
   url = "https://www.prevanders.net/libdwarf-${version}.tar.xz";
-  hash = "sha512-KC2Q38nacE62SkuhFB8q5mD+6xS78acjdzhmmOMSSSi0SmkU2OiOYUGrCINc5yOtCQqFOtV9vLQ527pXJV+1iQ==";
+  hash = "sha512-7Gbw28ct891omi04664CnggeDsMAjdUQNy4MLDh/AJLC+f6NmSje2HucLsMPHD3GO/rKfMShThyMVX08OzfSJw==";
   buildInputs = [ zlib zstd ];
   knownVulnerabilities = [];
 }
