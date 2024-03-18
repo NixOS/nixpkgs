f833ab824dca0752a296f785e923ec3c5b379259
Victor Engmark <victor@engmark.name>
2024-03-18 20:03:31 +1300

    gphoto2: Add option for group name
    Enables referencing it in configuration dynamically, rather than with a
    magic string.
    
    Closes #294871.

--- nixos/modules/programs/gphoto2.nix
+++ nixos/modules/programs/gphoto2.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 
+let
+  defaultGroup = "camera";
+in
 {
   meta.maintainers = [ lib.maintainers.league ];
 
@@ -9,12 +12,20 @@
       enable = lib.mkOption {
         default = false;
         type = lib.types.bool;
-        description = ''
-          Whether to configure system to use gphoto2.
-          To grant digital camera access to a user, the user must
-          be part of the camera group:
-          `users.users.alice.extraGroups = ["camera"];`
+        description = lib.mdDoc "Whether to configure system to use gphoto2.";
+      };
+      group = lib.mkOption {
+        description = lib.mdDoc ''
+          Group created when gphoto2 is enabled.
+
+          To grant digital camera access to a user, the user must be part of this group:
+          `users.users.alice.extraGroups = [config.programs.gphoto2.group];`.
+
+          By default, a group named `${defaultGroup}` will be created.
         '';
+        type = lib.types.str;
+        default = defaultGroup;
+        example = "gphoto2";
       };
     };
   };
@@ -23,6 +34,6 @@
   config = lib.mkIf config.programs.gphoto2.enable {
     services.udev.packages = [ pkgs.libgphoto2 ];
     environment.systemPackages = [ pkgs.gphoto2 ];
-    users.groups.camera = {};
+    users.groups."${config.programs.gphoto2.group}" = {};
   };
 }
