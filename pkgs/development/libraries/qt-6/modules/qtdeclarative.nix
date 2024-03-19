75dd5faddc36442214bb4ec88c6c58819091e09e
Sean Link <sean.link@lightdeckdx.com>
2024-03-19 10:59:53 -0600

    qt6.qtdeclarative: add mingw support Part of a larger effort to add mingw support for qtmultimedia.

--- pkgs/development/libraries/qt-6/modules/qtdeclarative.nix
+++ pkgs/development/libraries/qt-6/modules/qtdeclarative.nix
@@ -3,16 +3,24 @@
 , qtlanguageserver
 , qtshadertools
 , openssl
+, stdenv
 , python3
+, lib
+, pkgsBuildBuild
 }:
 
 qtModule {
   pname = "qtdeclarative";
-  propagatedBuildInputs = [ qtbase qtlanguageserver qtshadertools openssl python3 ];
+  propagatedBuildInputs = [ qtbase qtlanguageserver qtshadertools openssl ];
+  nativeBuildInputs = [ python3 ];
   patches = [
     # prevent headaches from stale qmlcache data
     ../patches/qtdeclarative-default-disable-qmlcache.patch
     # add version specific QML import path
     ../patches/qtdeclarative-qml-paths.patch
+  ];
+  cmakeFlags = lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderTools"
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
   ];
 }
