a9336230ce09df0c066d550f97e57bc87a3226bf
Naïm Favier <n@monade.li>
2024-03-19 22:21:45 +0100

    darcs: build with GHC 9.6
    Undoes 49c1247b0e548b5b92bf5c83014c1efdc8534688

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30536,7 +30536,7 @@ with pkgs;
   daktilo = callPackage ../tools/misc/daktilo { };
 
   darcs = haskell.lib.compose.disableCabalFlag "library"
-    (haskell.lib.compose.justStaticExecutables haskell.packages.ghc94.darcs);
+    (haskell.lib.compose.justStaticExecutables haskellPackages.darcs);
 
   darkman = callPackage ../applications/misc/darkman { };
 
