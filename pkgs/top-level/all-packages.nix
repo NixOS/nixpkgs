25c4d96ee4b139a8e0823e1c623e48a9760ce8c1
Eric Yen <eric@ericyen.com>
2024-03-18 23:01:06 -0700

    conda: replace FHS conda with pure nix package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7171,7 +7171,7 @@ with pkgs;
     llvmPackages = llvmPackages_13;
   };
 
-  conda = callPackage ../tools/package-management/conda { };
+  conda = with python3Packages; toPythonApplication conda;
 
   conduktor = callPackage ../applications/misc/conduktor { };
 
