e91045851f2ce0cf9d1d72276f3b089afc80a8a2
Oliver Kopp <kopp.dev@gmail.com>
2024-03-19 22:37:33 +0100

    Update maintainer-list.nix
    Co-authored-by:  Anderson Torres <torres.anderson.85@protonmail.com>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -60,7 +60,8 @@
     See `./scripts/check-maintainer-github-handles.sh` for an example on how to work with this data.
 
     When creating a commit:
-      * When you add a maintainer: Use `maintainers: handle` as commit message.
+      * When adding a new maintainer, be aware of the current commit conventions:
+        https://github.com/NixOS/nixpkgs/blob/master/CONTRIBUTING.md#commit-conventions.
 */
 {
   _0qq = {
