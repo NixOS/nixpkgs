d9745239170677aa52b332f83fcb4174061eb411
Quantenzitrone <nix@dev.quantenzitrone.eu>
2024-03-19 17:13:01 +0100

    fido2luks: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8093,8 +8093,6 @@ with pkgs;
 
   flux = callPackage ../development/compilers/flux { };
 
-  fido2luks = callPackage ../tools/security/fido2luks { };
-
   fierce = callPackage ../tools/security/fierce { };
 
   figlet = callPackage ../tools/misc/figlet { };
