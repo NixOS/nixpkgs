30baa5a96a5481480b4d86733274862ced0bf947
Jake Levi <jakedevquestions@gmail.com>
2024-03-18 20:41:24 -0400

    andromeda-gtk-theme: init at version 0-unstable-2024-03-04

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28718,6 +28718,8 @@ with pkgs;
 
   android-udev-rules = callPackage ../os-specific/linux/android-udev-rules { };
 
+  andromeda-gtk-theme = libsForQt5.callPackage ../data/themes/andromeda-gtk-theme { };
+
   ankacoder = callPackage ../data/fonts/ankacoder { };
   ankacoder-condensed = callPackage ../data/fonts/ankacoder/condensed.nix { };
 
