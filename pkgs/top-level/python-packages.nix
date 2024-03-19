fcf8ed38d648b8552bb3867972a3923ae908b457
Rodrigo Gryzinski <contact@rogryza.me>
2024-03-19 14:50:02 +0000

    python3Packages.mozilla-django-oidc: init at 4.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7487,6 +7487,8 @@ self: super: with self; {
 
   mouseinfo = callPackage ../development/python-modules/mouseinfo { };
 
+  mozilla-django-oidc = callPackage ../development/python-modules/mozilla-django-oidc { };
+
   moviepy = callPackage ../development/python-modules/moviepy { };
 
   mox3 = callPackage ../development/python-modules/mox3 { };
