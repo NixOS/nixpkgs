a1215f89803e95088858ff6ccb4183aefb2dcae4
Philip Taron <philip.taron@gmail.com>
2024-03-19 14:12:24 -0700

    Avoid top-level `with ...;` in pkgs/development/tools/clpm/default.nix

--- pkgs/development/tools/clpm/default.nix
+++ pkgs/development/tools/clpm/default.nix
@@ -9,12 +9,11 @@
 # Broken on newer versions:
 # "https://gitlab.common-lisp.net/clpm/clpm/-/issues/51". Once that bug is
 # fixed, remove this, and all 2.1.9 references from the SBCL build file.
-with rec {
+let
   sbcl_2_1_9 = sbcl.override (_: {
     version = "2.1.9";
   });
-};
-
+in
 
 stdenv.mkDerivation rec {
   pname = "clpm";
