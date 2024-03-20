9a59dfccd6f50413e849277ff5bb843080fe3b99
annalee <150648636+a-n-n-a-l-e-e@users.noreply.github.com>
2024-03-20 16:28:21 +0000

    stdenv.darwin: bootstrap darwin using updated tools
    - update the hashes and tools needed to extract the bootstrap-tools archive
    - unify the x64 and aarch64 unpack process

--- pkgs/stdenv/darwin/make-bootstrap-tools.nix
+++ pkgs/stdenv/darwin/make-bootstrap-tools.nix
@@ -401,8 +401,7 @@ rec {
   };
 
   # The ultimate test: bootstrap a whole stdenv from the tools specified above and get a package set out of it
-  # TODO: uncomment once https://github.com/NixOS/nixpkgs/issues/222717 is resolved
-  /*
+  # eg: nix-build -A freshBootstrapTools.test-pkgs.stdenv
   test-pkgs = import test-pkgspath {
     # if the bootstrap tools are for another platform, we should be testing
     # that platform.
@@ -412,5 +411,4 @@ rec {
         args' = args // { inherit bootstrapFiles; };
       in (import (test-pkgspath + "/pkgs/stdenv/darwin") args');
   };
-  */
 }
