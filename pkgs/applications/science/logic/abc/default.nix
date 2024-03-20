00f9f17616ae808ea5a9bfba4ebacdf2e6e987e1
Luflosi <luflosi@luflosi.de>
2024-03-20 17:25:38 +0100

    abc-verifier: unstable-2023-10-13 -> unstable-2024-03-04

--- pkgs/applications/science/logic/abc/default.nix
+++ pkgs/applications/science/logic/abc/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname   = "abc-verifier";
-  version = "unstable-2023-10-13";
+  version = "unstable-2024-03-04";
 
   src = fetchFromGitHub {
     owner = "yosyshq";
     repo  = "abc";
-    rev   = "896e5e7dedf9b9b1459fa019f1fa8aa8101fdf43";
-    hash  = "sha256-ou+E2lvDEOxXRXNygE/TyVi7quqk+CJHRI+HDI0xljE=";
+    rev   = "0cd90d0d2c5338277d832a1d890bed286486bcf5";
+    hash  = "sha256-1v/HOYF/ZdfR75eC3uYySKs2k6ZLCTUI0rtzPQs0hKQ=";
   };
 
   nativeBuildInputs = [ cmake ];
