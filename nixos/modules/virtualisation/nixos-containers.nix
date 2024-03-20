72a9d45d7f9b4c447e717e5ec4a0648e4fccd680
Kira Bruneau <kira.bruneau@pm.me>
2024-03-20 14:22:44 -0400

    nixos/nixos-containers: require mounts for bind mount host paths
    Fixes starting containers before bind mount host paths are mounted

--- nixos/modules/virtualisation/nixos-containers.nix
+++ nixos/modules/virtualisation/nixos-containers.nix
@@ -834,7 +834,10 @@ in
               script = startScript containerConfig;
               postStart = postStartScript containerConfig;
               serviceConfig = serviceDirectives containerConfig;
-              unitConfig.RequiresMountsFor = lib.optional (!containerConfig.ephemeral) "${stateDirectory}/%i";
+              unitConfig.RequiresMountsFor = lib.optional (!containerConfig.ephemeral) "${stateDirectory}/%i"
+                ++ builtins.map
+                  (d: if d.hostPath != null then d.hostPath else d.mountPoint)
+                  (builtins.attrValues cfg.bindMounts);
               environment.root = if containerConfig.ephemeral then "/run/nixos-containers/%i" else "${stateDirectory}/%i";
             } // (
             optionalAttrs containerConfig.autoStart
