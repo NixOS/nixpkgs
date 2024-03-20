47ebba221532cbaa9071c035fd5d2d868a1409cb
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-20 06:57:10 +0100

    python311Packages.linkify-it-py: refactor

--- pkgs/development/python-modules/linkify-it-py/default.nix
+++ pkgs/development/python-modules/linkify-it-py/default.nix
@@ -10,13 +10,13 @@
 buildPythonPackage rec {
   pname = "linkify-it-py";
   version = "2.0.2";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "tsutsu3";
-    repo = pname;
+    repo = "linkify-it-py";
     rev = "refs/tags/v${version}";
     hash = "sha256-e3k2RblqvzWSSIrJL6ib2dxFw7T5vXlN8DrCV1qEr6w=";
   };
@@ -29,17 +29,18 @@ buildPythonPackage rec {
     uc-micro-py
   ];
 
-  pythonImportsCheck = [
-    "linkify_it"
-  ];
-
   nativeCheckInputs = [
     pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "linkify_it"
   ];
 
   meta = with lib; {
     description = "Links recognition library with full unicode support";
     homepage = "https://github.com/tsutsu3/linkify-it-py";
+    changelog = "https://github.com/tsutsu3/linkify-it-py/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ AluisioASG ];
   };
