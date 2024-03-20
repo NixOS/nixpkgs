1f3a7e4192264ed273f4ea07f61761f4c0d69dfc
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-20 08:27:41 +0100

    python312Packages.toml-adapt: refactor

--- pkgs/development/python-modules/toml-adapt/default.nix
+++ pkgs/development/python-modules/toml-adapt/default.nix
@@ -11,13 +11,13 @@
 buildPythonPackage rec {
   pname = "toml-adapt";
   version = "0.3.3";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "firefly-cpp";
-    repo = pname;
+    repo = "toml-adapt";
     rev = "refs/tags/${version}";
     hash = "sha256-KD5dTr/wxFbDg3AbfE0jUbgNjvxqDmbHwjY5Dmp6JFI=";
   };
@@ -41,9 +41,10 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "A simple Command-line interface for manipulating toml files";
-    mainProgram = "toml-adapt";
     homepage = "https://github.com/firefly-cpp/toml-adapt";
+    changelog = "https://github.com/firefly-cpp/toml-adapt/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ firefly-cpp ];
+    mainProgram = "toml-adapt";
   };
 }
