2ce1f48431ce1ec08c72330632df1a7c248caffc
Felix Schröter <dev@felschr.com>
2024-03-20 01:43:40 +0100

    mullvad-browser: 13.0.10 -> 13.0.12
    https://github.com/mullvad/mullvad-browser/releases/tag/13.0.12
    (cherry picked from commit d68fe668ba8993192f00bea6a76977121b9c16d5)

--- pkgs/applications/networking/browsers/mullvad-browser/default.nix
+++ pkgs/applications/networking/browsers/mullvad-browser/default.nix
@@ -90,7 +90,7 @@ let
       ++ lib.optionals mediaSupport [ ffmpeg ]
   );
 
-  version = "13.0.10";
+  version = "13.0.12";
 
   sources = {
     x86_64-linux = fetchurl {
@@ -102,7 +102,7 @@ let
         "https://tor.eff.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
         "https://tor.calyxinstitute.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
       ];
-      hash = "sha256-+8b3K3XLSPlndR12KNUH0lsPquhTupxQrLBuSEGac7Y=";
+      hash = "sha256-qqz3tdic2muBQI6JXc0/uxBeenWFYHN43K7qJee9q78=";
     };
   };
 
