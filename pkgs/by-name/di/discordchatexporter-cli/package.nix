ec80106f9ab4e15c2dc4f7c4622e6779fbcff430
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-20 04:10:45 +0000

    discordchatexporter-cli: 2.42.8 -> 2.42.9

--- pkgs/by-name/di/discordchatexporter-cli/package.nix
+++ pkgs/by-name/di/discordchatexporter-cli/package.nix
@@ -8,13 +8,13 @@
 
 buildDotnetModule rec {
   pname = "discordchatexporter-cli";
-  version = "2.42.8";
+  version = "2.42.9";
 
   src = fetchFromGitHub {
     owner = "tyrrrz";
     repo = "discordchatexporter";
     rev = version;
-    hash = "sha256-54NTeIs0a8hd2xKQkAxwfyGwEPUlSSXXvDamGLfa9ls=";
+    hash = "sha256-7XVNcg6ibwXJveYa5gGVhVXNrto+JMZViyBWe0NdSbA=";
   };
 
   projectFile = "DiscordChatExporter.Cli/DiscordChatExporter.Cli.csproj";
