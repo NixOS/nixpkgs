f4bc70c45726e67d26b46501bd05e6ab4750ade0
Maxine Aubrey <max@ine.dev>
2024-03-20 20:03:29 +0100

    gnome.gnome-panel: 3.50.0 → 3.52.0
    https://gitlab.gnome.org/GNOME/gnome-panel/-/compare/3.50.0...3.52.0
    
    Changelog-Reviewed-By: Maxine Aubrey <max@ine.dev>

--- pkgs/desktops/gnome/misc/gnome-panel/default.nix
+++ pkgs/desktops/gnome/misc/gnome-panel/default.nix
@@ -24,13 +24,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-panel";
-  version = "3.50.0";
+  version = "3.52.0";
 
   outputs = [ "out" "dev" "man" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    hash = "sha256-Al7IidBCYtQclb7oW+ZCG6OmBQ4mm5zpn2ksrUmi6xM=";
+    hash = "sha256-nim6iHPN5A1AwpNKRk+PQ7ousbUisZFEfKon3XhTxdQ=";
   };
 
   patches = [
