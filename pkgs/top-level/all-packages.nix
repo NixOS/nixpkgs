472c35cbac5a16a080ca24d7d111c2550c5ad84c
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-03-21 07:38:00 +0100

    stog: 0.20.0 → 1.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34895,7 +34895,7 @@ with pkgs;
 
   stalonetray = callPackage ../applications/window-managers/stalonetray { };
 
-  inherit (ocaml-ng.ocamlPackages_4_12) stog;
+  inherit (ocaml-ng.ocamlPackages) stog;
 
   stp = callPackage ../applications/science/logic/stp { };
 
