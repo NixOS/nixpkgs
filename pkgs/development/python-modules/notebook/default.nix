59e5aea080b09bcc12d9e0b6905a528afbea033c
natsukium <tomoya.otabi@gmail.com>
2024-03-21 22:05:58 +0900

    python311Packages.notebook: 7.1.1 -> 7.1.2
    Changelog: https://github.com/jupyter/notebook/blob/v7.1.2/CHANGELOG.md

--- pkgs/development/python-modules/notebook/default.nix
+++ pkgs/development/python-modules/notebook/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "notebook";
-  version = "7.1.1";
+  version = "7.1.2";
   disabled = pythonOlder "3.8";
 
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-gY50IPoh9ALnJq+58C3388EPKUwC44PtGYUoZsMWEIs=";
+    hash = "sha256-78LIAEOQng+qF/zp6bN8BZwDrw7Jmk1NuEyyHZ0uk2o=";
   };
 
   postPatch = ''
