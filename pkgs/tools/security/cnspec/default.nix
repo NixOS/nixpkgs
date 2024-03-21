1e7255a550602ce38c68f24ba3702d9d2a4cd2e6
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-21 21:53:07 +0100

    cnspec: 10.8.2 -> 10.8.4
    Diff: https://github.com/mondoohq/cnspec/compare/refs/tags/v10.8.2...v10.8.4
    
    Changelog: https://github.com/mondoohq/cnspec/releases/tag/v10.8.4

--- pkgs/tools/security/cnspec/default.nix
+++ pkgs/tools/security/cnspec/default.nix
@@ -5,18 +5,18 @@
 
 buildGoModule rec {
   pname = "cnspec";
-  version = "10.8.2";
+  version = "10.8.4";
 
   src = fetchFromGitHub {
     owner = "mondoohq";
     repo = "cnspec";
     rev = "refs/tags/v${version}";
-    hash = "sha256-F38qymDYAV2hc1jKrnyot6rk/vDPAvH+DP/JhucmZkE=";
+    hash = "sha256-emY8y+OKk052o5LGhHMra7CTpbmS/U+5sjWIYR7DRsA=";
   };
 
   proxyVendor = true;
 
-  vendorHash = "sha256-w8iGRPnYbyNeHZ+cOA6K4GJdsIyES5zC3A70r9BEFuY=";
+  vendorHash = "sha256-W39Ccc5UU+K+W/h/R1vrB1lcJXKZs8gbfxmb99vGEfM=";
 
   subPackages = [
     "apps/cnspec"
