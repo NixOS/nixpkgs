0c8faaf6c535fe015b92912763d3162683364293
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-03-21 13:11:59 +0100

    python312Packages.aplpy: fix build
     - Fix compatiblity issue with Astropy 6
    
     - Replace distutils with setuptool to build with Python 3.12

--- pkgs/development/python-modules/aplpy/default.nix
+++ pkgs/development/python-modules/aplpy/default.nix
@@ -32,6 +32,20 @@ buildPythonPackage rec {
     hash = "sha256-KCdmBwQWt7IfHsjq7pWlbSISEpfQZDyt+SQSTDaUCV4=";
   };
 
+  # Fix build with Astropy 6 and Python 3.12.
+  patches = [
+    # https://github.com/aplpy/aplpy/pull/496
+    (fetchpatch {
+      url = "https://github.com/aplpy/aplpy/commit/d232a4984bc6a83ec86dfdc3ab3bc1b05de44c48.patch";
+      hash = "sha256-jGUTzIrVdGNPy0BV8w46jzz045fDXBisiwIn90bn7oY=";
+    })
+    # https://github.com/aplpy/aplpy/pull/497
+    (fetchpatch {
+      url = "https://github.com/aplpy/aplpy/commit/468be394970b39f1aaa6debef51eb674e2dd86d8.patch";
+      hash = "sha256-/ovLrFOKb3RQ8TZSviuOV6EYNgz0gtrhVWZLFJBrzFg=";
+    })
+  ];
+
   nativeBuildInputs = [
     setuptools
     setuptools-scm
