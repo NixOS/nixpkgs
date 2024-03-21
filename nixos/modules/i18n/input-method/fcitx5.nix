e66ede958e3db470627978959810534185b75209
Nick Cao <nickcao@nichi.co>
2024-03-21 00:42:26 -0400

    nixos/fcitx5: stop using renamed option services.xserver.desktopManager.plasma6.enable

--- nixos/modules/i18n/input-method/fcitx5.nix
+++ nixos/modules/i18n/input-method/fcitx5.nix
@@ -32,8 +32,8 @@ in
       };
       plasma6Support = mkOption {
         type = types.bool;
-        default = config.services.xserver.desktopManager.plasma6.enable;
-        defaultText = literalExpression "config.services.xserver.desktopManager.plasma6.enable";
+        default = config.services.desktopManager.plasma6.enable;
+        defaultText = literalExpression "config.services.desktopManager.plasma6.enable";
         description = lib.mdDoc ''
           Use qt6 versions of fcitx5 packages.
           Required for configuring fcitx5 in KDE System Settings.
