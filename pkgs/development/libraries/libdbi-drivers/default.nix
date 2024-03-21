367912953c382ed11b5e4d94145e8206d9a20274
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-03-21 10:00:46 +0100

    libdbiDrivers: fix build with clang

--- pkgs/development/libraries/libdbi-drivers/default.nix
+++ pkgs/development/libraries/libdbi-drivers/default.nix
@@ -46,6 +46,11 @@ stdenv.mkDerivation rec {
     "--with-pgsql_libdir=${postgresql.lib}/lib"
   ];
 
+  env.NIX_CFLAGS_COMPILE = toString (lib.optionals stdenv.cc.isClang [
+    "-Wno-error=incompatible-function-pointer-types"
+    "-Wno-error=int-conversion"
+  ]);
+
   installFlags = [ "DESTDIR=\${out}" ];
 
   postInstall = ''
