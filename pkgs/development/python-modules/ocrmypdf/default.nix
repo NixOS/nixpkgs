34336ae7bad91f96acc2543ceee1782d2e30aa14
Robert Schütz <nix@dotlambda.de>
2024-03-21 09:26:47 -0700

    python311Packages.ocrmypdf: 16.1.1 -> 16.1.2
    Diff: https://github.com/ocrmypdf/OCRmyPDF/compare/v16.1.1...v16.1.2
    
    Changelog: https://github.com/ocrmypdf/OCRmyPDF/blob/v16.1.2/docs/release_notes.rst

--- pkgs/development/python-modules/ocrmypdf/default.nix
+++ pkgs/development/python-modules/ocrmypdf/default.nix
@@ -30,7 +30,7 @@
 
 buildPythonPackage rec {
   pname = "ocrmypdf";
-  version = "16.1.1";
+  version = "16.1.2";
 
   disabled = pythonOlder "3.10";
 
@@ -46,7 +46,7 @@ buildPythonPackage rec {
     postFetch = ''
       rm "$out/.git_archival.txt"
     '';
-    hash = "sha256-XCYNz1QQodUEidz1+A79yleqOnOCK3zJ8mBIPU5JEQg=";
+    hash = "sha256-nZvfkfO5u3iuN0g/KITWbhYCRAJngEOKCW48z6IEPMI=";
   };
 
   patches = [
