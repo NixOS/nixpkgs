74a290caee7636de83dd2642fccb16708f70e88a
Matthias Queitsch <matthias.queitsch@mailbox.org>
2024-03-22 23:46:20 +0100

    bdt: improve package style

--- pkgs/applications/misc/bdt/default.nix
+++ pkgs/applications/misc/bdt/default.nix
@@ -1,21 +1,24 @@
-{ pkgs ? import <nixpkgs> { }, lib ? pkgs.lib }:
-pkgs.rustPlatform.buildRustPackage rec {
+{ lib
+, rustPlatform
+, fetchFromGitHub
+}:
+rustPlatform.buildRustPackage rec {
   pname = "bdt";
   version = "0.18.0";
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "datafusion-contrib";
-    repo = pname;
+    repo = "bdt";
     rev = "5c6730a8e3cd43c7847aef76b499197730cded58";
     hash = "sha256-gUKsJwbpMPSO+KPqyJRodrRLjUpTh/y6C2xhrgvJFKk=";
   };
 
   cargoHash = "sha256-4KrFhchoIB2N89m7HrL0xj2Z+u/6/6Onxa2wIAX18Io=";
 
-  meta = with lib; {
+  meta = {
     description = "boring data tool. A CLI tool to query parquet, json and avro files.";
     homepage = "https://github.com/datafusion-contrib/bdt";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
     maintainers = [];
   };
 }
