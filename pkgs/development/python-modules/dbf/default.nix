NixOS/nixpkgs

commit 63155af44bade56465b69493dcdd2a4e65921ffc

python3Packages.dbf: 0.99.3 -> 0.99.9

pkgs/development/python-modules/dbf/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "dbf";
-  version = "0.99.3";
+  version = "0.99.9";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-lAJypyrCfRah22mq/vggaEASzDVT/+mHXVzS46nLadw=";
+    hash = "sha256-MFEi1U0RNvrfDtV4HpvPgKTCibAh76z7Gnmj32IubYw=";
   };
 
   propagatedBuildInputs = [

commit 198d32f50b7c3740bec72c43dcf4f17f48dd5d14

python3Packages.dbf: fix build
Broken in 63155af44bade56465b69493dcdd2a4e65921ffc. Failed with:

> Traceback (most recent call last):
>   File "/build/dbf-0.99.9/dbf/./test.py", line 15, in <module>
>     from . import *
> ImportError: attempted relative import with no known parent package

Relative imports only work when running as a module, not directly as a
file.

pkgs/development/python-modules/dbf/default.nix
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   ];
 
   checkPhase = ''
-    ${python.interpreter} dbf/test.py
+    ${python.interpreter} -m dbf.test
   '';
 
   pythonImportsCheck = [