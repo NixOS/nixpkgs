NixOS/nixpkgs

commit 2f4ea0c6dfb067551bff9f04c10d7c9e8eb70dcf

python3Packages.leather: 0.3.4 -> 0.4.0

pkgs/development/python-modules/leather/default.nix
@@ -9,12 +9,12 @@
 
 buildPythonPackage rec {
   pname = "leather";
-  version = "0.3.4";
+  version = "0.4.0";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "b43e21c8fa46b2679de8449f4d953c06418666dc058ce41055ee8a8d3bb40918";
+    sha256 = "sha256-+WS+wghvMVOmwW5wfyDLcY+BH1evEWB19MD0gFxgi5U=";
   };
 
   propagatedBuildInputs = [ six ];

commit 956cf5f6124c0ee518562b547d92e7b498a87a9c

python3Packages.leather: fix build
Broken in 2f4ea0c6dfb067551bff9f04c10d7c9e8eb70dcf.

Upstream changed from nose to pytest. Previously, the
setuptoolsCheckHook would run and execute the `./test.py` file, but that
is not part of upstream’s build process and doesn’t work. Including
pytestCheckHook means that does not run—just pytest.

pkgs/development/python-modules/leather/default.nix
@@ -4,7 +4,7 @@
 , six
 , cssselect
 , lxml
-, nose
+, pytestCheckHook
 }:
 
 buildPythonPackage rec {
@@ -22,14 +22,8 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     cssselect
     lxml
-    nose
+    pytestCheckHook
   ];
-
-  checkPhase = ''
-    runHook preCheck
-    nosetests
-    runHook postCheck
-  '';
 
   meta = with lib; {
     homepage = "http://leather.rtfd.io";