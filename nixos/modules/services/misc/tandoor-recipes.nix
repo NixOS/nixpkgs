af6e25787827e25a87bb874362ad380a2cfab199
Bruno BELANYI <bruno@belanyi.fr>
2024-03-22 20:48:15 +0000

    nixos/tandoor-recipes: improve manage script
    This is shamelessly stolen from Photoprism's module, and should allow
    executing the manage script without resorting to various tricks due to
    relying on transient users.

--- nixos/modules/services/misc/tandoor-recipes.nix
+++ nixos/modules/services/misc/tandoor-recipes.nix
@@ -20,7 +20,10 @@ let
   manage = pkgs.writeShellScript "manage" ''
     set -o allexport # Export the following env vars
     ${lib.toShellVars env}
-    exec ${pkg}/bin/tandoor-recipes "$@"
+    eval "$(${config.systemd.package}/bin/systemctl show -pUID,GID,MainPID tandoor-recipes.service)"
+    exec ${pkgs.util-linux}/bin/nsenter \
+      -t $MainPID -m -S $UID -G $GID \
+      ${pkg}/bin/tandoor-recipes "$@"
   '';
 in
 {
