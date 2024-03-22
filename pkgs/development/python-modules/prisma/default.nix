46decb5d230b5b322bcddcc66a0dd9b0cbc9a03f
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-22 14:22:40 +0000

    python312Packages.prisma: 0.12.0 -> 0.13.0

--- pkgs/development/python-modules/prisma/default.nix
+++ pkgs/development/python-modules/prisma/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "prisma";
-  version = "0.12.0";
+  version = "0.13.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "RobertCraigie";
     repo = "prisma-client-py";
     rev = "refs/tags/v${version}";
-    hash = "sha256-vmcYBUPDhFbxgWyrF+AjoXwAAH2R/tJYttFD+41bPbA=";
+    hash = "sha256-j9HJZTt4VTq29Q+nynYmRWKx02GVdyA+iZzxZwspXn8=";
   };
 
   nativeBuildInputs = [
