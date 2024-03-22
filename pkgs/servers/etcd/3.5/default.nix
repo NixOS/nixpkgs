dd7b21c96732eb68ce1c7dfc65ac02936b0e5a52
superherointj <5861043+superherointj@users.noreply.github.com>
2024-03-22 12:34:09 -0300

    etcd_3_5: add superherointj as maintainer

--- pkgs/servers/etcd/3.5/default.nix
+++ pkgs/servers/etcd/3.5/default.nix
@@ -20,7 +20,7 @@ let
     description = "Distributed reliable key-value store for the most critical data of a distributed system";
     license = licenses.asl20;
     homepage = "https://etcd.io/";
-    maintainers = with maintainers; [ offline endocrimes ];
+    maintainers = with maintainers; [ endocrimes offline superherointj ];
     platforms = platforms.darwin ++ platforms.linux;
   };
 
