724c8f5ea5d6d21c0ef10296c1e1b77ce81e7f35
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-23 10:59:50 +0100

    osgqt: init at 3.5.7

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11644,6 +11644,8 @@ with pkgs;
 
   os-prober = callPackage ../tools/misc/os-prober { };
 
+  osgqt = libsForQt5.callPackage ../development/libraries/osgqt/package.nix { };
+
   oshka = callPackage ../development/tools/oshka { };
 
   osl = libsForQt5.callPackage ../development/compilers/osl {
