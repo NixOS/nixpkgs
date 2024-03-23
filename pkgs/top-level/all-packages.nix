7ede0c842de8274d0372a9b26f073fb6a62671fc
Matthias Beyer <mail@beyermatthias.de>
2024-03-23 09:44:48 +0100

    cargo-public-api: Build with rustPackages_1_76
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17204,7 +17204,9 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
-  cargo-public-api = callPackage ../development/tools/rust/cargo-public-api { };
+  cargo-public-api = callPackage ../development/tools/rust/cargo-public-api {
+    inherit (rustPackages_1_76) rustPlatform;
+  };
 
   cargo-zigbuild = callPackage ../development/tools/rust/cargo-zigbuild { };
 
