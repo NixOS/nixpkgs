9f9bf2cfbe01999ad6e2855448620f7425b651ed
Randy Eckenrode <randy@largeandhighquality.com>
2024-03-22 20:18:55 -0400

    darwin.moltenvk: 1.2.7 -> 1.2.8
    https://github.com/KhronosGroup/MoltenVK/releases/tag/v1.2.8

--- pkgs/os-specific/darwin/moltenvk/default.nix
+++ pkgs/os-specific/darwin/moltenvk/default.nix
@@ -28,7 +28,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "MoltenVK";
-  version = "1.2.7";
+  version = "1.2.8";
 
   buildInputs = [
     AppKit
@@ -56,7 +56,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "KhronosGroup";
     repo = "MoltenVK";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-0+S/kueV+AEVt+oFnh4cgcDRVtEbUH1QiHFPhGhimCA=";
+    hash = "sha256-nsNN2iCSjXzfarSdaNmeNbfDliUBMQMsb0rXEENWJqw=";
   };
 
   postPatch = ''
