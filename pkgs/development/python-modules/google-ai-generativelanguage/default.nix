07689ed8d7eb0ed77d7cfbed8623c1eb6e319361
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-23 00:52:41 +0000

    python312Packages.google-ai-generativelanguage: 0.5.4 -> 0.6.0

--- pkgs/development/python-modules/google-ai-generativelanguage/default.nix
+++ pkgs/development/python-modules/google-ai-generativelanguage/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "google-ai-generativelanguage";
-  version = "0.5.4";
+  version = "0.6.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-XBhXXrzbIiKoFPew/UdUD673AUPb96rm9LudyVcY3H8=";
+    hash = "sha256-vA/JVTaj3+NuA91LJo+Utn1hxogihr/OaBV4ujOFm7o=";
   };
 
   propagatedBuildInputs = [
