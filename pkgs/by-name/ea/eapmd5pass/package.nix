86e7b60adf025e3b3b6fe558001157153ac81f89
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-03-23 20:02:10 +0100

    eapmd5pass: init at 1.5

---
+++ pkgs/by-name/ea/eapmd5pass/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  openssl_1_1,
+  libpcap,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "eapmd5pass";
+  version = "1.5";
+
+  src = fetchFromGitHub {
+    owner = "joswr1ght";
+    repo = "eapmd5pass";
+    rev = "v${version}";
+    hash = "sha256-DYoily2dX/mbDFpQivGZZ/JMTIblooMzeoMw19w1Ky4=";
+  };
+
+  nativeBuildInputs = [
+    openssl_1_1
+    libpcap
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp eapmd5pass $out/bin
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Brute force password selection for EAP-MD5 authentication exchanges";
+    homepage = "https://github.com/joswr1ght/eapmd5pass";
+    license = licenses.gpl2Only;
+    maintainers = with maintainers; [ pinpox ];
+    mainProgram = "eapmd5pass";
+    platforms = platforms.all;
+  };
+}
