a56ece4ee151c3eb620961c6e9ef0b9eaa57cd4a
Soispha <soispha@vhack.eu>
2024-03-23 11:35:37 +0100

    fixup! fixup! manim-slides: init at v5.1.3

--- pkgs/development/python-modules/manim-slides/default.nix
+++ pkgs/development/python-modules/manim-slides/default.nix
@@ -22,6 +22,7 @@
 , rich
 , rtoml
 , tqdm
+, pyqt6
 
   # Optional dependencies
 , ipython
@@ -47,43 +48,37 @@ buildPythonPackage rec {
     hash = "sha256-WZR95swapT2Fbu6mbuHLjMu3Okq/wKFabzN7xpZw0/g=";
   };
 
-  nativeBuildInputs = [
-    pdm-backend
-    pythonRelaxDepsHook
-  ];
+  nativeBuildInputs = [ pdm-backend pythonRelaxDepsHook ];
 
   pythonRemoveDeps = [ "opencv-python" ];
 
-  pythonRelaxDeps = [
-    "rtoml"
-    "qtpy"
-  ];
+  pythonRelaxDeps = [ "rtoml" "qtpy" ];
 
-  propagatedBuildInputs =
-    [
-      av
-      click
-      click-default-group
-      jinja2
-      lxml
-      numpy
-      opencv4
-      pillow
-      pydantic
-      pydantic-extra-types
-      python-pptx
-      qtpy
-      requests
-      rich
-      rtoml
-      tqdm
+  propagatedBuildInputs = [
+    av
+    click
+    click-default-group
+    jinja2
+    lxml
+    numpy
+    opencv4
+    pillow
+    pydantic
+    pydantic-extra-types
+    python-pptx
+    qtpy
+    requests
+    rich
+    rtoml
+    tqdm
 
-      # avconv is a potential alternative
-      ffmpeg
-      # This could also be manimgl, but that is not (yet) packaged
-      manim
-    ]
-    ++ lib.lists.optional withGui ipython;
+    # dependency of qtpy (could also be pyqt5)
+    pyqt6
+    # avconv is a potential alternative
+    ffmpeg
+    # This could also be manimgl, but that is not (yet) packaged
+    manim
+  ] ++ lib.lists.optional withGui ipython;
 
   pythonImportsCheck = [ "manim_slides" ];
 
