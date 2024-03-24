6efd3efcb147e2cae621aad1e6aa6993f63c454c
Anthony Roussel <anthony@roussel.dev>
2024-03-24 16:42:33 +0100

    pdk: add passthru.tests.version, meta.changelog, meta.mainProgram

--- pkgs/by-name/pd/pdk/package.nix
+++ pkgs/by-name/pd/pdk/package.nix
@@ -4,6 +4,8 @@
   gnumake,
   lib,
   makeWrapper,
+  pdk,
+  testers,
 }:
 
 bundlerApp {
@@ -17,12 +19,20 @@ bundlerApp {
     wrapProgram $out/bin/pdk --prefix PATH : ${lib.makeBinPath [ gnumake ]}
   '';
 
-  passthru.updateScript = bundlerUpdateScript "pdk";
+  passthru = {
+    tests.version = testers.testVersion {
+      package = pdk;
+      version = (import ./gemset.nix).pdk.version;
+    };
+    updateScript = bundlerUpdateScript "pdk";
+  };
 
   meta = {
+    changelog = "https://github.com/puppetlabs/pdk/blob/main/CHANGELOG.md";
     description = "Puppet Development Kit";
     homepage = "https://github.com/puppetlabs/pdk";
     license = lib.licenses.asl20;
+    mainProgram = "pdk";
     maintainers = with lib.maintainers; [ netali anthonyroussel ];
   };
 }
