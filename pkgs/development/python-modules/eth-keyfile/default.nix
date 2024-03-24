8cbc7128ab09113f5e227094d16acc0ee852f0b5
hellwolf <hellwolf@freehell.org>
2024-03-24 18:30:59 +0200

    python311Packages.eth-keyfile: 0.6.0 -> 0.8.0
    - use pyproject & setuptools-scm
    - remove the setuptools-markdown post patch

--- pkgs/development/python-modules/eth-keyfile/default.nix
+++ pkgs/development/python-modules/eth-keyfile/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, setuptools
 , eth-keys
 , eth-utils
 , pycryptodome
@@ -10,22 +11,19 @@
 
 buildPythonPackage rec {
   pname = "eth-keyfile";
-  version = "0.6.0";
-  format = "setuptools";
-  disabled = pythonOlder "3.7";
+  version = "0.8.0";
+  pyproject = true;
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "eth-keyfile";
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-JD4bRoD9L0JXcd+bTZrq/BkWw5QGzOi1RvoyLJC77kk=";
+    hash = "sha256-797yhHuU9/lm96YKxl3SZ5IQAwDxDSYkLkiBdAHh0Uk=";
   };
 
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace "'setuptools-markdown'" ""
-  '';
+  build-system = [ setuptools];
 
   propagatedBuildInputs = [
     eth-keys
