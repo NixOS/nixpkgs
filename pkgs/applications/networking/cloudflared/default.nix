d67666c432699ae892a7df072bfecc8bbd0e3186
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-24 09:09:55 +0000

    cloudflared: 2024.2.1 -> 2024.3.0

--- pkgs/applications/networking/cloudflared/default.nix
+++ pkgs/applications/networking/cloudflared/default.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "cloudflared";
-  version = "2024.2.1";
+  version = "2024.3.0";
 
   src = fetchFromGitHub {
     owner = "cloudflare";
     repo = "cloudflared";
     rev = "refs/tags/${version}";
-    hash = "sha256-aSAwDz7QSYbHfDA+/usGh7xCxSq+kBTB3eqMBf5XEa8=";
+    hash = "sha256-Fzi5g8bHBC5xao0iZ4I/SXLpEVaoUB+7UuQZhbfHw60=";
   };
 
   vendorHash = null;
