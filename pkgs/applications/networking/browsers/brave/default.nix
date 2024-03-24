00fc1256e0de33bf7090968e7e68c1535ef5ea3d
itsOnly1Jah <40274157+itsOnly1Jah@users.noreply.github.com>
2024-03-24 08:26:14 -0400

    brave: refactor
    create exception for unsupported platforms
    
    Co-authored-by: Anderson Torres <torres.anderson.85@protonmail.com>

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -10,5 +10,5 @@
       version = "1.64.109";
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
       hash = "sha256-36igba0U3p8i7t91RxeG6PqlKYyHDDlj295ICcYmCNc=";
-    };
+    } else throw "Unsupported platform."; 
 }
