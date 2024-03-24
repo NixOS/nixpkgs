560da4a49de5d2189607397852f610e6cf058ce2
itsOnly1Jah <kingjah23@gmail.com>
2024-03-24 09:21:47 -0400

    brave: refactor
    update references of `generic.nix` to `make-brave.nix`

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -1,12 +1,12 @@
 { hostPlatform, callPackage }: {
   brave = if hostPlatform.isAarch64
-    then callPackage ./generic.nix { } rec {
+    then callPackage ./make-brave.nix { } rec {
       pname = "brave";
       version = "1.64.109";
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_arm64.deb";
       hash = "sha256-5/tFuPuvk5fr7/ZKhFLLlikh8GnZaSHF9l0XU+0x2DA=";
       platform = "aarch64-linux";
-    } else if hostPlatform.isx86_64 then callPackage /generic.nix { } rec {
+    } else if hostPlatform.isx86_64 then callPackage /make-brave.nix { } rec {
       pname = "brave";
       version = "1.64.109";
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
