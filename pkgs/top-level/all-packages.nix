187a5f88dc3f2cc04610a19a6780b33134197a90
Felix Singer <felixsinger@posteo.net>
2024-03-24 22:13:04 +0100

    redmine: Move package files into pkgs/by-name directory
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12529,8 +12529,6 @@ with pkgs;
 
   redir = callPackage ../tools/networking/redir { };
 
-  redmine = callPackage ../applications/version-management/redmine { };
-
   redpanda-client = callPackage ../servers/redpanda { };
 
   redpanda-server = redpanda-client.server;
