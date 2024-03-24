c9cf985126d6d74164a90bcac89fef9ed4a74c66
Anthony Roussel <anthony@roussel.dev>
2024-03-25 00:22:42 +0100

    nagiosPlugins.check_uptime: 20161112 -> unstable-2016-11-12

--- pkgs/servers/monitoring/nagios-plugins/check_uptime/default.nix
+++ pkgs/servers/monitoring/nagios-plugins/check_uptime/default.nix
@@ -6,26 +6,28 @@
 }:
 
 stdenv.mkDerivation {
-  pname = "check_uptime";
-  version = "20161112";
+  pname = "check-uptime";
+  version = "unstable-2016-11-12";
 
   src = fetchFromGitHub {
     owner = "madrisan";
     repo = "nagios-plugins-uptime";
     rev = "51822dacd1d404b3eabf3b4984c64b2475ed6f3b";
-    sha256 = "18q9ibzqn97dsyr9xs3w9mqk80nmmfw3kcjidrdsj542amlsycyk";
+    hash = "sha256-0zOvaVWCFKlbblGyObir1QI0cU186J6y1+0ki/+KCaM=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
 
   enableParallelBuilding = true;
 
-  postInstall = "ln -sr $out/libexec $out/bin";
+  postInstall = ''
+    ln -sr $out/libexec $out/bin
+  '';
 
   meta = {
     description = "Uptime check plugin for Sensu/Nagios/others";
     homepage = "https://github.com/madrisan/nagios-plugins-uptime";
-    license = lib.licenses.gpl3;
+    license = lib.licenses.gpl3Plus;
     mainProgram = "check_uptime";
     maintainers = with lib.maintainers; [ peterhoeg ];
   };
