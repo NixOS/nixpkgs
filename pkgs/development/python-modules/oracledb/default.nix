7ce968a38e90a9dbddf584985e03d57d60f5753a
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-24 20:38:07 +0100

    python312Packages.oracledb: 2.1.0 -> 2.1.1
    Changelog: https://github.com/oracle/python-oracledb/blob/v2.1.1/doc/src/release_notes.rst

--- pkgs/development/python-modules/oracledb/default.nix
+++ pkgs/development/python-modules/oracledb/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "oracledb";
-  version = "2.1.0";
+  version = "2.1.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-HJpEjJhD2zPxC3d9aSD7k5XqsLD9wX8WIPrHw+7NtXo=";
+    hash = "sha256-4ugXz6bf82xxMXNvNOKq7HXXJv040ZENgxgGGieCKPo=";
   };
 
   nativeBuildInputs = [
