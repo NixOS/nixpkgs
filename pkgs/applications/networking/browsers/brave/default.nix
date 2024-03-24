ff06329971c2fc9e1de30bc29e2f063140d0d0b7
itsOnly1Jah <40274157+itsOnly1Jah@users.noreply.github.com>
2024-03-24 08:10:54 -0400

    brave: refactor
    checks for specific host platforms
    
    Co-authored-by: Anderson Torres <torres.anderson.85@protonmail.com>

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -5,7 +5,7 @@
       version = "1.64.109";
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_arm64.deb";
       hash = "sha256-5/tFuPuvk5fr7/ZKhFLLlikh8GnZaSHF9l0XU+0x2DA=";
-    } else callPackage /generic.nix { } rec {
+    } else if hostPlatform.isx86_64 then callPackage /generic.nix { } rec {
       pname = "brave";
       version = "1.64.109";
       url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
