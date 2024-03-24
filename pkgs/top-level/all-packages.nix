e13810a0d862467da04e90a51b118f29dd780803
Anderson Torres <torres.anderson.85@protonmail.com>
2024-03-24 19:18:14 -0300

    graphicsmagick: migrate to by-name
    (cherry picked from commit cdf0640f87de7c94d172bcc6923e0e53bfb6e61b)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32301,7 +32301,6 @@ with pkgs;
         pythonPackages = python3Packages;
   };
 
-  graphicsmagick = callPackage ../applications/graphics/graphicsmagick { };
   graphicsmagick_q16 = graphicsmagick.override { quantumdepth = 16; };
   graphicsmagick-imagemagick-compat = graphicsmagick.imagemagick-compat;
 
