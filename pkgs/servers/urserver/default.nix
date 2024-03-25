ad594b450386bbc658196638cc778382c4d0a665
Stefan Frijters <sfrijters@gmail.com>
2024-03-25 11:35:25 +0100

    urserver: 3.10.0.2467 -> 3.13.0.2505

--- pkgs/servers/urserver/default.nix
+++ pkgs/servers/urserver/default.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "urserver";
-  version = "3.10.0.2467";
+  version = "3.13.0.2505";
 
   src = fetchurl {
     url = "https://www.unifiedremote.com/static/builds/server/linux-x64/${builtins.elemAt (builtins.splitVersion version) 3}/urserver-${version}.tar.gz";
-    sha256 = "sha256-IaLRhia6mb4h7x5MbBRtPJxJ3uTlkfOzmoTwYzwfbWA=";
+    hash = "sha256-rklv6Ppha1HhEPunbL8ELYdQ9Z1FN4FrVsNwny3/gA4=";
   };
 
   nativeBuildInputs = [
