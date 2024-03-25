6d182087eb27609323768fce70f36722f2497f39
ByteSudoer <ByteSudoer@users.noreply.github.com>
2024-03-25 22:27:46 +0100

    gpg-tui: 0.10.0 -> 0.11.0

--- pkgs/tools/security/gpg-tui/default.nix
+++ pkgs/tools/security/gpg-tui/default.nix
@@ -11,21 +11,23 @@
 , libiconv
 , libobjc
 , libresolv
-, x11Support ? true, libxcb, libxkbcommon
+, x11Support ? true
+, libxcb
+, libxkbcommon
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "gpg-tui";
-  version = "0.10.0";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "orhun";
     repo = "gpg-tui";
     rev = "v${version}";
-    hash = "sha256-zTFWIIqIDMI77lg2CB1ug+GeKPVIT1OQ1p80x6tLgGg=";
+    hash = "sha256-aHmLcWiDy5GMbcKi285tfBggNmGkpVAoZMm4dt8LKak=";
   };
 
-  cargoHash = "sha256-5qLrmU/SfUfiQOOpECTEn8K142STnbhqE3XbJFxKPZg=";
+  cargoHash = "sha256-rtBvo2nX4A6K/TBl6xhW8huLXdR6xDUhzMB3KRXRYMs=";
 
   nativeBuildInputs = [
     gpgme # for gpgme-config
@@ -53,6 +55,7 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/orhun/gpg-tui";
     changelog = "https://github.com/orhun/gpg-tui/blob/${src.rev}/CHANGELOG.md";
     license = licenses.mit;
+    mainProgram = "gpg-tui";
     maintainers = with maintainers; [ dotlambda matthiasbeyer ];
   };
 }
