77ea14bb7e1ef4bb2a001b3387a314d65498981a
Connor Baker <connor.baker@tweag.io>
2024-03-25 17:39:47 +0000

    cuda-modules/setup-hooks/setup-cuda-hook: factor out cc access

--- pkgs/development/cuda-modules/setup-hooks/setup-cuda-hook/default.nix
+++ pkgs/development/cuda-modules/setup-hooks/setup-cuda-hook/default.nix
@@ -1,15 +1,17 @@
 # Currently propagated by cuda_nvcc or cudatoolkit, rather than used directly
 { backendStdenv, makeSetupHook }:
+let
+  inherit (backendStdenv) cc;
+in
 makeSetupHook
   {
     name = "setup-cuda-hook";
-
     substitutions = {
       # Required in addition to ccRoot as otherwise bin/gcc is looked up
       # when building CMakeCUDACompilerId.cu
-      ccFullPath = "${backendStdenv.cc}/bin/${backendStdenv.cc.targetPrefix}c++";
+      ccFullPath = "${cc}/bin/${cc.targetPrefix}c++";
       # Point NVCC at a compatible compiler
-      ccRoot = "${backendStdenv.cc}";
+      ccRoot = "${cc}";
       setupCudaHook = placeholder "out";
     };
   }
