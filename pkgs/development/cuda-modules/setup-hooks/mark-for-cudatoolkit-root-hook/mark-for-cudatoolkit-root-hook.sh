de0bb6a996d08beec718e8c54249d4172a9cc71c
Connor Baker <connor.baker@tweag.io>
2024-03-25 18:21:35 +0000

    cuda-modules/setup-hooks: wip rewrite and set NIX_DEBUG=1

--- pkgs/development/cuda-modules/setup-hooks/mark-for-cudatoolkit-root-hook/mark-for-cudatoolkit-root-hook.sh
+++ pkgs/development/cuda-modules/setup-hooks/mark-for-cudatoolkit-root-hook/mark-for-cudatoolkit-root-hook.sh
@@ -1,14 +1,43 @@
 # shellcheck shell=bash
 
-# Should we mimick cc-wrapper's "hygiene"?
-[[ -z ${strictDeps-} ]] || (( "$hostOffset" < 0 )) || return 0
+guard=Sourcing
+reason=
 
-echo "Sourcing mark-for-cudatoolkit-root-hook" >&2
+export NIX_DEBUG=1
+
+# Only run the hook from nativeBuildInputs.
+# See the table under https://nixos.org/manual/nixpkgs/unstable/#dependency-propagation for information
+# about the different target combinations and their offsets.
+if (( "${hostOffset:?}" != -1 && "${targetOffset:?}" != 0 )); then
+    guard=Skipping
+    reason=" because the hook is not in nativeBuildInputs"
+fi
+
+if (( "${NIX_DEBUG:-0}" >= 1 )); then
+    echo "$guard hostOffset=$hostOffset targetOffset=$targetOffset mark-for-cudatoolkit-root-hook$reason" >&2
+else
+    echo "$guard mark-for-cudatoolkit-root-hook$reason" >&2
+fi
+
+[[ "$guard" = Sourcing ]] || return 0
 
 markForCUDAToolkit_ROOT() {
-    mkdir -p "${prefix}/nix-support"
-    [[ -f "${prefix}/nix-support/include-in-cudatoolkit-root" ]] && return
-    echo "$pname-$output" > "${prefix}/nix-support/include-in-cudatoolkit-root"
-}
+    local fnName=mark-for-cudatoolkit-root-hook::markForCUDAToolkit_ROOT
+    echo "$fnName: Running" >&2
 
+    mkdir -p "${prefix:?}/nix-support"
+    local markerPath="$prefix/nix-support/include-in-cudatoolkit-root"
+    if [[ -f "$markerPath" ]]; then
+        (( "${NIX_DEBUG:-0}" >= 1 )) && echo "$fnName: $markerPath exists, skipping" >&2
+        return
+    fi
+
+    # Always create the file, even if it's empty, since setup-cuda-hook relies on its existence.
+    # However, only populate it if strictDeps is not set.
+    touch "$markerPath"
+    if [[ -z ${strictDeps-} ]]; then
+        (( "${NIX_DEBUG:-0}" >= 1 )) || echo "$fnName: populating $markerPath" >&2
+        echo "${pname:?}-${output:?}" > "$markerPath"
+    fi
+}
 fixupOutputHooks+=(markForCUDAToolkit_ROOT)
--- pkgs/development/cuda-modules/setup-hooks/setup-cuda-hook/setup-cuda-hook.sh
+++ pkgs/development/cuda-modules/setup-hooks/setup-cuda-hook/setup-cuda-hook.sh
@@ -1,15 +1,26 @@
 # shellcheck shell=bash
 
-# Only run the hook from nativeBuildInputs
-(( "$hostOffset" == -1 && "$targetOffset" == 0)) || return 0
-
 guard=Sourcing
 reason=
 
-[[ -n ${cudaSetupHookOnce-} ]] && guard=Skipping && reason=" because the hook has been propagated more than once"
+export NIX_DEBUG=1
 
-if (( "${NIX_DEBUG:-0}" >= 1 )) ; then
-    echo "$guard hostOffset=$hostOffset targetOffset=$targetOffset setupCudaHook$reason" >&2
+# Only run the hook from buildInputs: outside executables like cuda_nvcc, most
+# CUDA dependencies are needed at runtime, not build-time.
+# See the table under https://nixos.org/manual/nixpkgs/unstable/#dependency-propagation for information
+# about the different target combinations and their offsets.
+if (( "${hostOffset:?}" != -1 && "${targetOffset:?}" != 0 )); then
+    guard=Skipping
+    reason=" because the hook is not in nativeBuildInputs"
+fi
+
+if [[ -n ${cudaSetupHookOnce-} ]]; then
+    guard=Skipping
+    reason=" because the hook has been propagated more than once"
+fi
+
+if (( "${NIX_DEBUG:-0}" >= 1 )); then
+    echo "$guard hostOffset=$hostOffset targetOffset=$targetOffset setup-cuda-hook$reason" >&2
 else
     echo "$guard setup-cuda-hook$reason" >&2
 fi
@@ -20,13 +31,22 @@ declare -g cudaSetupHookOnce=1
 declare -Ag cudaHostPathsSeen=()
 declare -Ag cudaOutputToPath=()
 
-extendcudaHostPathsSeen() {
-    (( "${NIX_DEBUG:-0}" >= 1 )) && echo "extendcudaHostPathsSeen $1" >&2
+extendCudaHostPathsSeen() {
+    local fnName=setup-cuda-hook::extendCudaHostPathsSeen
+    (( "${NIX_DEBUG:-0}" >= 1 )) && echo "$fnName: $1" >&2
 
     local markerPath="$1/nix-support/include-in-cudatoolkit-root"
-    [[ ! -f "${markerPath}" ]] && return
-    [[ -v cudaHostPathsSeen[$1] ]] && return
+    if [[ ! -f "$markerPath" ]]; then
+        (( "${NIX_DEBUG:-0}" >= 1 )) && echo "$fnName: skipping since $markerPath exists" >&2
+        return
+    fi
 
+    if [[ -v cudaHostPathsSeen[$1] ]]; then
+        (( "${NIX_DEBUG:-0}" >= 1 )) && echo "$fnName: skipping since $1 has already been seen" >&2
+        return
+    fi
+
+    # Add the path to the list of CUDA host paths.
     cudaHostPathsSeen["$1"]=1
 
     # E.g. cuda_cudart-lib
@@ -36,31 +56,32 @@ extendcudaHostPathsSeen() {
     [[ -z "$cudaOutputName" ]] && return
 
     local oldPath="${cudaOutputToPath[$cudaOutputName]-}"
-    [[ -n "$oldPath" ]] && echo "extendcudaHostPathsSeen: warning: overwriting $cudaOutputName from $oldPath to $1" >&2
+    [[ -n "$oldPath" ]] && echo "$fnName: warning: overwriting $cudaOutputName from $oldPath to $1" >&2
     cudaOutputToPath["$cudaOutputName"]="$1"
 }
-addEnvHooks "$targetOffset" extendcudaHostPathsSeen
+addEnvHooks "$targetOffset" extendCudaHostPathsSeen
 
 setupCUDAToolkit_ROOT() {
-    (( "${NIX_DEBUG:-0}" >= 1 )) && echo "setupCUDAToolkit_ROOT: cudaHostPathsSeen=${!cudaHostPathsSeen[*]}" >&2
+    local fnName=setup-cuda-hook::setupCUDAToolkit_ROOT
+    (( "${NIX_DEBUG:-0}" >= 1 )) && echo "$fnName: cudaHostPathsSeen=${!cudaHostPathsSeen[*]}" >&2
 
-    for path in "${!cudaHostPathsSeen[@]}" ; do
+    for path in "${!cudaHostPathsSeen[@]}"; do
         addToSearchPathWithCustomDelimiter ";" CUDAToolkit_ROOT "$path"
-        if [[ -d "$path/include" ]] ; then
-            addToSearchPathWithCustomDelimiter ";" CUDAToolkit_INCLUDE_DIR "$path/include"
-        fi
+        [[ -d "$path/include" ]] && addToSearchPathWithCustomDelimiter ";" CUDAToolkit_INCLUDE_DIR "$path/include"
     done
 
-    export cmakeFlags+=" -DCUDAToolkit_INCLUDE_DIR=$CUDAToolkit_INCLUDE_DIR -DCUDAToolkit_ROOT=$CUDAToolkit_ROOT"
+    export cmakeFlagsArray+=(
+        -DCUDAToolkit_INCLUDE_DIR="${CUDAToolkit_INCLUDE_DIR:-}"
+        -DCUDAToolkit_ROOT="${CUDAToolkit_ROOT:-}"
+    )
 }
 preConfigureHooks+=(setupCUDAToolkit_ROOT)
 
 setupCUDAToolkitCompilers() {
-    echo Executing setupCUDAToolkitCompilers >&2
+    local fnName=setup-cuda-hook::setupCUDAToolkitCompilers
+    echo "$fnName: Running" >&2
 
-    if [[ -n "${dontSetupCUDAToolkitCompilers-}" ]] ; then
-        return
-    fi
+    [[ -n "${dontSetupCUDAToolkitCompilers-}" ]] && return
 
     # Point NVCC at a compatible compiler
 
@@ -69,8 +90,10 @@ setupCUDAToolkitCompilers() {
     # https://cmake.org/cmake/help/latest/envvar/CUDAHOSTCXX.html
     # https://cmake.org/cmake/help/latest/variable/CMAKE_CUDA_HOST_COMPILER.html
 
-    export cmakeFlags+=" -DCUDA_HOST_COMPILER=@ccFullPath@"
-    export cmakeFlags+=" -DCMAKE_CUDA_HOST_COMPILER=@ccFullPath@"
+    export cmakeFlagsArray+=(
+        -DCUDA_HOST_COMPILER="@ccFullPath@"
+        -DCMAKE_CUDA_HOST_COMPILER="@ccFullPath@"
+    )
 
     # For non-CMake projects:
     # We prepend --compiler-bindir to nvcc flags.
@@ -78,26 +101,23 @@ setupCUDAToolkitCompilers() {
     # uses the last --compiler-bindir it gets on the command line.
     # FIXME: this results in "incompatible redefinition" warnings.
     # https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html#compiler-bindir-directory-ccbin
-    if [ -z "${CUDAHOSTCXX-}" ]; then
-      export CUDAHOSTCXX="@ccFullPath@";
-    fi
+    [[ -z "${CUDAHOSTCXX-}" ]] && export CUDAHOSTCXX="@ccFullPath@"
 
     export NVCC_PREPEND_FLAGS+=" --compiler-bindir=@ccRoot@/bin"
 
     # NOTE: We set -Xfatbin=-compress-all, which reduces the size of the compiled
-    #   binaries. If binaries grow over 2GB, they will fail to link. This is a problem for us, as
-    #   the default set of CUDA capabilities we build can regularly cause this to occur (for
-    #   example, with Magma).
+    # binaries. If binaries grow over 2GB, they will fail to link. This is a problem for us, as
+    # the default set of CUDA capabilities we build can regularly cause this to occur (for
+    # example, with Magma).
     #
     # @SomeoneSerge: original comment was made by @ConnorBaker in .../cudatoolkit/common.nix
-    if [[ -z "${dontCompressFatbin-}" ]]; then
-        export NVCC_PREPEND_FLAGS+=" -Xfatbin=-compress-all"
-    fi
+    [[ -z "${dontCompressFatbin-}" ]] && export NVCC_PREPEND_FLAGS+=" -Xfatbin=-compress-all"
 }
 preConfigureHooks+=(setupCUDAToolkitCompilers)
 
 propagateCudaLibraries() {
-    (( "${NIX_DEBUG:-0}" >= 1 )) && echo "propagateCudaLibraries: cudaPropagateToOutput=$cudaPropagateToOutput cudaHostPathsSeen=${!cudaHostPathsSeen[*]}" >&2
+    local fnName=setup-cuda-hook::propagateCudaLibraries
+    (( "${NIX_DEBUG:-0}" >= 1 )) && echo "$fnName: cudaPropagateToOutput=$cudaPropagateToOutput cudaHostPathsSeen=${!cudaHostPathsSeen[*]}" >&2
 
     [[ -z "${cudaPropagateToOutput-}" ]] && return
 
@@ -106,11 +126,8 @@ propagateCudaLibraries() {
     echo "@setupCudaHook@" >> "${!cudaPropagateToOutput}/nix-support/propagated-native-build-inputs"
 
     local propagatedBuildInputs=( "${!cudaHostPathsSeen[@]}" )
-    for output in $(getAllOutputNames) ; do
-        if [[ ! "$output" = "$cudaPropagateToOutput" ]] ; then
-            propagatedBuildInputs+=( "${!output}" )
-        fi
-        break
+    for output in $(getAllOutputNames); do
+        [[ ! "$output" = "$cudaPropagateToOutput" ]] && propagatedBuildInputs+=( "${!output}" ) && break
     done
 
     # One'd expect this should be propagated-host-host-deps, but that doesn't seem to work
