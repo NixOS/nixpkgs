0ef0d1674353136fafb27d87b4261c24eb286162
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-03-25 21:56:02 +0100

    python311Packages.trimesh: 4.2.0 -> 4.2.1
    Changelog: https://github.com/mikedh/trimesh/releases/tag/4.2.1

--- pkgs/development/python-modules/trimesh/default.nix
+++ pkgs/development/python-modules/trimesh/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "trimesh";
-  version = "4.2.0";
+  version = "4.2.1";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-uS6oVNS+JBN61SEVDwLQDoCf60SwqXV7831E2J+hb7I=";
+    hash = "sha256-CAn67M1T48J9y18QL1sjD3R57qfm9FYtK93Tq60WElE=";
   };
 
   nativeBuildInputs = [ setuptools ];
