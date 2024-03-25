NixOS/nixpkgs

commit 88e8d87fedcd17d7125d970fc2e6ff03c9884d6a

dotnet: strip debug info
This reduces the sdk closure size from 739.2M/2.5G to 494.5M/584.0M.

pkgs/development/compilers/dotnet/vmr.nix
@@ -311,6 +311,13 @@ in stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
+  # dotnet cli is in the root, so we need to strip from there
+  # TODO: should we install in $out/share/dotnet?
+  stripDebugList = [ "." ];
+  # stripping dlls results in:
+  # Failed to load System.Private.CoreLib.dll (error code 0x8007000B)
+  stripExclude = [ "*.dll" ];
+
   passthru = {
     inherit releaseManifest buildRid targetRid;
     icu = _icu;

commit 12fe0da11b6b7bc3ade204b4949839efd90335f4

dotnet: fix stripping on darwin
Fixes: 88e8d87fedcd17d7125d970fc2e6ff03c9884d6a

pkgs/development/compilers/dotnet/vmr.nix
@@ -251,6 +251,11 @@ in stdenv.mkDerivation rec {
     substituteInPlace \
       src/runtime/src/native/libs/System.Security.Cryptography.Native.Apple/CMakeLists.txt \
       --replace-fail 'xcrun swiftc' 'xcrun swiftc -module-cache-path "$ENV{HOME}/.cache/module-cache"'
+
+    # fix: strip: error: unknown argument '-n'
+    substituteInPlace \
+      src/runtime/eng/native/functions.cmake \
+      --replace-fail ' -no_code_signature_warning' ""
   '';
 
   prepFlags = [