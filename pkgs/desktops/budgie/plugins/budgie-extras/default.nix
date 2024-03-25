057ae4e89bb728743af36e21617cc3279883ffa2
Federico Damián Schonborn <federicoschonborn@disroot.org>
2024-03-24 23:53:14 -0300

    budgiePlugins.budgie-app-launcher: init at 1.8.0
    Signed-off-by: Federico Damián Schonborn <federicoschonborn@disroot.org>

--- pkgs/desktops/budgie/plugins/budgie-extras/default.nix
+++ pkgs/desktops/budgie/plugins/budgie-extras/default.nix
@@ -1,5 +1,21 @@
-{ callPackage }:
+{ callPackage
+, gnome-menus
+}:
+
+let
+  mkBudgieExtrasPlugin = callPackage ./mkBudgieExtrasPlugin.nix { };
+in
 
 {
-  mkBudgieExtrasPlugin = callPackage ./mkBudgieExtrasPlugin.nix { };
+  inherit mkBudgieExtrasPlugin;
+
+  budgie-app-launcher = mkBudgieExtrasPlugin {
+    pluginName = "app-launcher";
+    moduleName = "AppLauncher";
+    isPython = true;
+    buildInputs = [
+      gnome-menus
+    ];
+    meta.description = "This applet lists your favourite apps";
+  };
 }
