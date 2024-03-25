d493ad99f621a179b00ed84ba7b4f817b16f26f4
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-03-25 16:28:55 +0000

    mupdf: 1.23.6 -> 1.24.0

--- pkgs/applications/misc/mupdf/default.nix
+++ pkgs/applications/misc/mupdf/default.nix
@@ -1,7 +1,6 @@
 { stdenv
 , lib
 , fetchurl
-, fetchFromGitHub
 , copyDesktopItems
 , makeDesktopItem
 , desktopToDarwinBundle
@@ -37,7 +36,7 @@
 , xcbuild
 , gitUpdater
 
-# for passthru.tests
+  # for passthru.tests
 , cups-filters
 , zathura
 , mupdf
@@ -45,33 +44,20 @@
 
 assert enablePython -> enableCxx;
 
-let
-
-  freeglut-mupdf = freeglut.overrideAttrs (old: rec {
-    pname = "freeglut-mupdf";
-    version = "3.0.0-r${src.rev}";
-    src = fetchFromGitHub {
-      owner = "ArtifexSoftware";
-      repo = "thirdparty-freeglut";
-      rev = "13ae6aa2c2f9a7b4266fc2e6116c876237f40477";
-      hash = "sha256-0fuE0lm9rlAaok2Qe0V1uUrgP4AjMWgp3eTbw8G6PMM=";
-    };
-  });
-
-in
 stdenv.mkDerivation rec {
-  version = "1.23.6";
+  version = "1.24.0";
   pname = "mupdf";
 
   src = fetchurl {
     url = "https://mupdf.com/downloads/archive/${pname}-${version}-source.tar.gz";
-    sha256 = "sha256-rBHrhZ3UBEiOUVPNyWUbtDQeW6r007Pyfir8gvmq3Ck=";
+    hash = "sha256-UvYwA6b02J8jTp7ftLTIPSFrg6rrMjz9pgR8t1RZmuA=";
   };
 
-  patches = [ ./0001-Use-command-v-in-favor-of-which.patch
-              ./0002-Add-Darwin-deps.patch
-              ./0003-Fix-cpp-build.patch
-            ];
+  patches = [
+    ./0001-Use-command-v-in-favor-of-which.patch
+    ./0002-Add-Darwin-deps.patch
+    ./0003-Fix-cpp-build.patch
+  ];
 
   postPatch = ''
     substituteInPlace Makerules --replace "(shell pkg-config" "(shell $PKG_CONFIG"
@@ -93,8 +79,8 @@ stdenv.mkDerivation rec {
     "USE_SYSTEM_LIBS=yes"
     "PKG_CONFIG=${buildPackages.pkg-config}/bin/${buildPackages.pkg-config.targetPrefix}pkg-config"
   ] ++ lib.optionals (!enableX11) [ "HAVE_X11=no" ]
-    ++ lib.optionals (!enableGL) [ "HAVE_GLUT=no" ]
-    ++ lib.optionals (enableOcr) [ "USE_TESSERACT=yes" ];
+  ++ lib.optionals (!enableGL) [ "HAVE_GLUT=no" ]
+  ++ lib.optionals (enableOcr) [ "USE_TESSERACT=yes" ];
 
   nativeBuildInputs = [ pkg-config ]
     ++ lib.optional (enableGL || enableX11) copyDesktopItems
@@ -107,11 +93,11 @@ stdenv.mkDerivation rec {
     ++ lib.optionals enableX11 [ libX11 libXext libXi libXrandr ]
     ++ lib.optionals enableCurl [ curl openssl ]
     ++ lib.optionals enableGL (
-      if stdenv.isDarwin then
-        with darwin.apple_sdk.frameworks; [ GLUT OpenGL ]
-      else
-        [ freeglut-mupdf libGLU ]
-    )
+    if stdenv.isDarwin then
+      with darwin.apple_sdk.frameworks; [ GLUT OpenGL ]
+    else
+      [ freeglut libGLU ]
+  )
     ++ lib.optionals enableOcr [ leptonica tesseract ]
   ;
   outputs = [ "bin" "dev" "out" "man" "doc" ];
@@ -145,8 +131,16 @@ stdenv.mkDerivation rec {
       ];
       categories = [ "Graphics" "Viewer" ];
       keywords = [
-        "mupdf" "comic" "document" "ebook" "viewer"
-        "cbz" "epub" "fb2" "pdf" "xps"
+        "mupdf"
+        "comic"
+        "document"
+        "ebook"
+        "viewer"
+        "cbz"
+        "epub"
+        "fb2"
+        "pdf"
+        "xps"
       ];
     })
   ];
@@ -175,9 +169,10 @@ stdenv.mkDerivation rec {
     cp docs/logo/mupdf.png $bin/share/icons/hicolor/48x48/apps
   '') + (if enableGL then ''
     ln -s "$bin/bin/mupdf-gl" "$bin/bin/mupdf"
-  '' else lib.optionalString (enableX11) ''
-    ln -s "$bin/bin/mupdf-x11" "$bin/bin/mupdf"
-  '') + (lib.optionalString (enableCxx) ''
+  '' else
+    lib.optionalString (enableX11) ''
+      ln -s "$bin/bin/mupdf-x11" "$bin/bin/mupdf"
+    '') + (lib.optionalString (enableCxx) ''
     cp platform/c++/include/mupdf/*.h $out/include/mupdf
     cp build/*/libmupdfcpp.so $out/lib
   '') + (lib.optionalString (enablePython) (''
