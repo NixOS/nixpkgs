82a64b9c6c8805601369189164f58ac55020fd19
t-monaghan <tom.monaghan@cultureamp.com>
2024-03-26 15:55:11 +1100

    maintainers: add t-monaghan

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -19778,6 +19778,11 @@
     githubId = 26902865;
     name = "Tomas Drtina";
   };
+  t-monaghan = {
+    github = "t-monaghan";
+    githubId = 62273348;
+    name = "Thomas Monaghan";
+  };
   tmountain = {
     email = "tinymountain@gmail.com";
     github = "tmountain";
