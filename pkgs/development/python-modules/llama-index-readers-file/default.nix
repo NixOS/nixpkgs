f2cb5422ac949e2d4187d4f8944a6d843a839463
Robert Schütz <github@dotlambda.de>
2024-03-25 17:19:39 -0700

    Revert "python311Packages.llama-index-readers-file: 0.1.7 -> 0.1.12"

--- pkgs/development/python-modules/llama-index-readers-file/default.nix
+++ pkgs/development/python-modules/llama-index-readers-file/default.nix
@@ -1,30 +1,27 @@
 { lib
 , beautifulsoup4
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , llama-index-core
 , poetry-core
 , pymupdf
 , pypdf
-, pythonOlder
+, pytestCheckHook
 , pythonRelaxDepsHook
-, striprtf
 }:
 
 buildPythonPackage rec {
   pname = "llama-index-readers-file";
-  version = "0.1.12";
+  version = "0.1.7";
+
+  inherit (llama-index-core) src meta;
+
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
-
-  src = fetchPypi {
-    pname = "llama_index_readers_file";
-    inherit version;
-    hash = "sha256-YGXL+AsPtdGJVYuLkK273JKsuGFH/KGS2I/MJwStKvM=";
-  };
+  sourceRoot = "${src.name}/llama-index-integrations/readers/${pname}";
 
   pythonRelaxDeps = [
+    "beautifulsoup4"
     "pymupdf"
     "pypdf"
   ];
@@ -33,30 +30,23 @@ buildPythonPackage rec {
     "bs4"
   ];
 
-  build-system = [
+  nativeBuildInputs = [
     poetry-core
     pythonRelaxDepsHook
   ];
 
-  dependencies = [
+  propagatedBuildInputs = [
     beautifulsoup4
     llama-index-core
     pymupdf
     pypdf
-    striprtf
   ];
 
-  # Tests are only available in the mono repo
-  doCheck = false;
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 
   pythonImportsCheck = [
     "llama_index.readers.file"
   ];
-
-  meta = with lib; {
-    description = "LlamaIndex Readers Integration for files";
-    homepage = "https://github.com/run-llama/llama_index/tree/main/llama-index-integrations/readers/llama-index-readers-file";
-    license = licenses.mit;
-    maintainers = with maintainers; [ fab ];
-  };
 }
