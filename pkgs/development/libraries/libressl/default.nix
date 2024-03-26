NixOS/nixpkgs

commit 449d50ab360688cb8ef471392bbe18f4a5dbc2b7

libressl: 3.8.2 -> 3.8.3
Changes: https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.8.3-relnotes.txt

pkgs/development/libraries/libressl/default.nix
@@ -111,7 +111,7 @@ in {
   };
 
   libressl_3_8 = generic {
-    version = "3.8.2";
-    hash = "sha256-bUuNW7slofgzZjnlbsUIgFLUOpUlZpeoXEzpEyPCWVQ=";
+    version = "3.8.3";
+    hash = "sha256-pl9A4+9uPJRRyDGObyxFTDZ+Z/CcDN4YSXMaTW7McnI=";
   };
 }

commit 2f006a72d0c38655dc94edc36cdf0401ace64cab

libressl: backport fix for linking static libtls
This fixes e.g. pkgsStatic.catgirl.

Fixes: 449d50ab3606 ("libressl: 3.8.2 -> 3.8.3")

pkgs/development/libraries/libressl/default.nix
@@ -113,5 +113,13 @@ in {
   libressl_3_8 = generic {
     version = "3.8.3";
     hash = "sha256-pl9A4+9uPJRRyDGObyxFTDZ+Z/CcDN4YSXMaTW7McnI=";
+
+    patches = [
+      (fetchpatch {
+        name = "libtls-pkg-config-static.patch";
+        url = "https://github.com/libressl/portable/commit/f7a0f40d52b994d0bca0eacd88b39f71e447c5d9.patch";
+        hash = "sha256-2ly6lsIdoV/riVqDViFXDP7nkZ/RUatEdiaSudQKtz0=";
+      })
+    ];
   };
 }