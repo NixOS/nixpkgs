ff77d83327c280931d42562a18d3d4639d1d400b
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-03-26 16:30:25 -0400

    nixos/wireplumber: provide example for `services.pipewire.wireplumber.configPackages`

--- nixos/modules/services/desktops/pipewire/wireplumber.nix
+++ nixos/modules/services/desktops/pipewire/wireplumber.nix
@@ -36,6 +36,16 @@ in
       configPackages = mkOption {
         type = listOf package;
         default = [ ];
+        example = literalExpression ''[
+          (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez.conf" '''
+            monitor.bluez.properties = {
+              bluez5.roles = [ a2dp_sink a2dp_source bap_sink bap_source hsp_hs hsp_ag hfp_hf hfp_ag ]
+              bluez5.codecs = [ sbc sbc_xq aac ]
+              bluez5.enable-sbc-xq = true
+              bluez5.hfphsp-backend = "native"
+            }
+          ''')
+        ]'';
         description = ''
           List of packages that provide WirePlumber configuration, in the form of
           `share/wireplumber/*/*.conf` files.
