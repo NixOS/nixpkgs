9d24ddfc1b16a72f654dd9554a9930842003ed9d
Anthony Roussel <anthony@roussel.dev>
2024-03-26 17:13:50 +0100

    apvlv: 0.1.5 -> 0.5.0
    https://github.com/naihe2010/apvlv/compare/v0.1.5...v0.5.0-final

--- pkgs/applications/misc/apvlv/default.nix
+++ pkgs/applications/misc/apvlv/default.nix
@@ -1,50 +1,73 @@
-{ lib, stdenv, fetchFromGitHub, fetchpatch, cmake, pkg-config, pcre, libxkbcommon, libepoxy
-, gtk3, poppler, freetype, libpthreadstubs, libXdmcp, libxshmfence, wrapGAppsHook
+{
+  cmake,
+  copyDesktopItems,
+  ebook_tools,
+  fetchFromGitHub,
+  freetype,
+  ghostscript,
+  gtk3,
+  installShellFiles,
+  lib,
+  libepoxy,
+  libpthreadstubs,
+  libXdmcp,
+  libxkbcommon,
+  libxml2,
+  libxshmfence,
+  man,
+  nix-update-script,
+  pcre,
+  pkg-config,
+  poppler,
+  stdenv,
+  testers,
+  webkitgtk,
+  wrapGAppsHook,
 }:
 
-stdenv.mkDerivation rec {
-  version = "0.1.5";
+stdenv.mkDerivation (finalAttrs: {
   pname = "apvlv";
+
+  # If you change the version, please also update src.rev accordingly
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "naihe2010";
     repo = "apvlv";
-    rev = "v${version}";
-    sha256 = "1n4xiic8lqnv3mqi7wpdv866gyyakax71gffv3n9427rmcld465i";
+    rev = "refs/tags/v0.5.0-final";
+    hash = "sha256-5Wbv3dXieymhhPmEKQu8X/38WsDA1T/IBPoMXdpzcaA=";
   };
 
   env.NIX_CFLAGS_COMPILE = "-I${poppler.dev}/include/poppler";
 
   nativeBuildInputs = [
     cmake
+    copyDesktopItems
+    ghostscript
+    installShellFiles
+    man
     pkg-config
     wrapGAppsHook
   ];
 
   buildInputs = [
-    poppler pcre libxkbcommon libepoxy
-    freetype gtk3
-    libpthreadstubs libXdmcp libxshmfence # otherwise warnings in compilation
-  ];
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/naihe2010/apvlv/commit/d432635b9c5ea6c052a2ae1fb71aedec5c4ad57a.patch";
-      sha256 = "1am8dgv2kkpqmm2vaysa61czx8ppdx94zb3c59sx88np50jpy70w";
-    })
-    (fetchpatch {
-      url = "https://github.com/naihe2010/apvlv/commit/4c7a583e8431964def482e5471f02e6de8e62a7b.patch";
-      sha256 = "1dszm120lwm90hcg5zmd4vr6pjyaxc84qmb7k0fr59mmb3qif62j";
-    })
-    # fix build with gcc7
-    (fetchpatch {
-      url = "https://github.com/naihe2010/apvlv/commit/a3a895772a27d76dab0c37643f0f4c73f9970e62.patch";
-      sha256 = "1fpc7wr1ajilvwi5gjsy5g9jcx4bl03gp5dmajg90ljqbhwz2bfi";
-    })
-    ./fix-build-with-poppler-0.73.0.patch
+    ebook_tools
+    freetype
+    gtk3
+    libepoxy
+    libpthreadstubs
+    libXdmcp
+    libxkbcommon
+    libxml2
+    libxshmfence # otherwise warnings in compilation
+    pcre
+    poppler
+    webkitgtk
   ];
 
   installPhase = ''
+    runHook preInstall
+
     # binary
     mkdir -p $out/bin
     cp src/apvlv $out/bin/apvlv
@@ -53,23 +76,38 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/doc/apvlv/
     cp ../Startup.pdf $out/share/doc/apvlv/Startup.pdf
     cp ../main_menubar.glade $out/share/doc/apvlv/main_menubar.glade
-  ''
-  + lib.optionalString (!stdenv.isDarwin) ''
-    install -D ../apvlv.desktop $out/share/applications/apvlv.desktop
+
+    mkdir -p $out/etc
+    cp ../apvlvrc.example $out/etc/apvlvrc
+
+    installManPage ../apvlv.1
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
-    homepage = "http://naihe2010.github.io/apvlv/";
+  desktopItems = [
+    "../apvlv.desktop"
+  ];
+
+  passthru = {
+    tests.version = testers.testVersion {
+      command = "${lib.getExe finalAttrs.finalPackage} -v";
+      package = finalAttrs.finalPackage;
+      version = "${finalAttrs.version}-rel";
+    };
+  };
+
+  meta = {
+    changelog = "https://github.com/naihe2010/apvlv/blob/v${finalAttrs.version}/NEWS";
     description = "PDF viewer with Vim-like behaviour";
-    mainProgram = "apvlv";
+    homepage = "https://naihe2010.github.io/apvlv/";
+    license = lib.licenses.lgpl2;
     longDescription = ''
       apvlv is a PDF/DJVU/UMD/TXT Viewer Under Linux/WIN32
       with Vim-like behaviour.
     '';
-
-    license = licenses.lgpl2;
-    platforms = platforms.linux;
-    maintainers = [ maintainers.ardumont ];
+    mainProgram = "apvlv";
+    maintainers = with lib.maintainers; [ ardumont ];
+    platforms = lib.platforms.linux;
   };
-
-}
+})
