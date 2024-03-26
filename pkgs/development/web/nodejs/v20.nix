caa516d5c341d5bcc7644e6a17b921d45104fb7c
Mario Rodas <marsam@users.noreply.github.com>
2024-03-26 04:20:00 +0000

    nodejs_20: 20.11.1 -> 20.12.0
    Changelog: https://github.com/nodejs/node/releases/tag/v20.12.0

--- pkgs/development/web/nodejs/v20.nix
+++ pkgs/development/web/nodejs/v20.nix
@@ -8,8 +8,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "20.11.1";
-  sha256 = "sha256-d4E+2/P38W0tNdM1NEPe5OYdXuhNnjE4x1OKPAylIJ4=";
+  version = "20.12.0";
+  sha256 = "sha256-duU0bOv9WBUo9pn3ZPTRpuh8uBi2lnCPI13ctiWg940=";
   patches = [
     ./revert-arm64-pointer-auth.patch
     ./disable-darwin-v8-system-instrumentation-node19.patch
