ddb5111771fcbb4c56cf377b927dc8f7d0cdd580
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-27 23:36:41 +0000

    python311Packages.teslajsonpy: 3.10.1 -> 3.10.2

--- pkgs/development/python-modules/teslajsonpy/default.nix
+++ pkgs/development/python-modules/teslajsonpy/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "teslajsonpy";
-  version = "3.10.1";
+  version = "3.10.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "zabuldon";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-Eez8ktPhbWauFNsIuwoxMkGpI7hnYsBwStmU80IDFw4=";
+    hash = "sha256-M3/1J73C3gUqyZYsu10O6FnACKYVbGuMH+8Ns0s2Rwc=";
   };
 
   nativeBuildInputs = [
