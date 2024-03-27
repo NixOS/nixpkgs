62d03326248f58de57fb4efd611351689e61bf76
V <v@anomalous.eu>
2024-03-27 08:24:27 +0100

    libfprint-2-tod1-vfs0090: fix compilation with libfprint-tod 1.92.0+
    Some APIs were changed in this version, in an incompatible fashion.
    
    Change-Id: I9f312a54329c1f1877ddf451587ddf4fca9aabaf

--- pkgs/development/libraries/libfprint-2-tod1-vfs0090/default.nix
+++ pkgs/development/libraries/libfprint-2-tod1-vfs0090/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchFromGitLab, pkg-config, libfprint-tod, gusb, udev, nss, openssl, meson, pixman, ninja, glib }:
+{ stdenv, lib, fetchFromGitLab, fetchpatch2, pkg-config, libfprint-tod, gusb, udev, nss, openssl, meson, pixman, ninja, glib }:
 stdenv.mkDerivation {
   pname = "libfprint-2-tod1-vfs0090";
   version = "0.8.5";
@@ -16,6 +16,12 @@ stdenv.mkDerivation {
     ./0001-vfs0090-add-missing-explicit-dependencies-in-meson.b.patch
     # TODO remove once https://gitlab.freedesktop.org/3v1n0/libfprint-tod-vfs0090/-/merge_requests/2 is merged
     ./0002-vfs0090-add-missing-linux-limits.h-include.patch
+
+    # Fix compilation with libfprint-tod 1.92.0+
+    (fetchpatch2 {
+      url = "https://gitlab.com/bingch/libfprint-tod-vfs0090/-/commit/f3d21720592bd0b8b233cf6e8a1adc668d5a769e.patch";
+      hash = "sha256-CjBucbuat6v3HGTarZOCP11GnvRiItZtEakdI63ZX5Y=";
+    })
   ];
 
   nativeBuildInputs = [ pkg-config meson ninja ];
