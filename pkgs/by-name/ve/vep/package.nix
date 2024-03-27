79b82938a65d8d51f9d3fc72ca4df3462a65c4fc
Alexis Praga <alexis.praga@proton.me>
2024-03-28 00:26:17 +0100

    Adressing reviewing changes. Use a revision instead of commits. Use nixfmt-rfc-style

--- pkgs/by-name/ve/vep/package.nix
+++ pkgs/by-name/ve/vep/package.nix
@@ -1,99 +1,96 @@
 # Instructions:http://www.ensembl.org/info/docs/api/api_installation.html
 # Do not use https://github.com/Ensembl/ensembl-vep/archive/release/${version}.zip
 # We cannot use INSTALL.pl but it’s not that bad:install the dependencies and copies the .pm files should be ok
-{ config
-, curl
-, lib
-, perlPackages
-, pkgs
-, stdenv
-, fetchFromGitHub
-, git
-, perl
-, unzip
-, makeWrapper
-, which
-, zlib
+{
+  lib,
+  perlPackages,
+  pkgs,
+  stdenv,
+  fetchFromGitHub,
+  fetchzip,
+  git,
+  perl,
+  unzip,
+  makeWrapper,
+  which,
+  zlib,
 }:
 
-with pkgs;
-
 let
+  version = "110";
   customInstallPhase = ''
     mkdir -p $out/${perl.libPrefix}/${perl.version}/
     tests=$(find modules/ -mindepth 1 -maxdepth 1 -type d | grep -v t)
     cp -r $tests $out/${perl.libPrefix}/${perl.version}/
   '';
 
-  ensemblGit = n: r: s:
+  ensemblGit =
+    name: sha256:
     # Copy modules directly
-    stdenv.mkDerivation rec {
-      name = n;
-      version = "110";
-      # Neither tag nor release, so we need the commit...
-      src = fetchgit {
-        url = "https://github.com/Ensembl/${name}";
-        rev = r;
-        sha256 = s;
+    stdenv.mkDerivation {
+      inherit name version;
+      # Not available through fetchFromGitHub
+      src = fetchzip {
+        inherit sha256 version;
+        url = "https://github.com/Ensembl/${name}/archive/release/${version}.zip";
       };
       installPhase = customInstallPhase;
     };
 
-  vepPlugins = fetchgit {
-    url = "https://github.com/Ensembl/VEP_plugins";
+  vepPlugins = fetchFromGitHub {
+    owner = "Ensembl";
+    repo = "VEP_plugins";
     rev = "8f271c4848338dc7d504881ff71fdf2892c3d096";
     sha256 = "sha256-LbaXwLFDP3m1QhRHwO9uh36BEFHE2NzL4xdxTb7/S5Q=";
   };
 
   # Install ensembl-xs, faster run using re-implementation in C of some of the Perl subroutines
-  ensembl-xs = perlPackages.buildPerlModule rec {
+  ensembl-xs = perlPackages.buildPerlPackage rec {
     pname = "ensembl-xs";
     version = "2.3.2";
-    src = fetchzip {
-      # No longer maintained
-      url = "https://github.com/Ensembl/ensembl-xs/archive/2.3.2.zip";
+    src = fetchFromGitHub {
+      inherit version;
+      owner = "Ensembl";
+      repo = "ensembl-xs";
+      rev = version;
       sha256 = "1qqnski532f4bz32wxbqd9w1sz40rjh81ipp9p02k3rlaf1gp1fa";
     };
     # PREFIX is important
     configurePhase = ''
       perl Makefile.PL PREFIX=$out INSTALLDIRS=site
     '';
-    buildPhase = "make";
     # Test do not work -- wrong include path
-    checkPhase = "";
-    installPhase = "make install";
+    doCheck = false;
   };
 
-
   # it contains compiled versions of certain key subroutines used in VEP
-  ensembl = ensemblGit "ensembl"
-    "39154d005fc22b90b57ae2912c7c2838c0801568"
-    "sha256-QMRtfAhNdUX3eqo1dva34NSCTmU0X1IrlNmBy4GHgNU=";
-  # No release, no tag for these 4 repos
-  ensembl-io = ensemblGit "ensembl-io"
-    "b1a0d57104ed2532e958194c38159a6092ba8b7b"
-    "sha256-IFjEmHXCN1TIaFmcvIPru6vRaz99WtvC2EyBarjw+Vw=";
-  ensembl-variation = ensemblGit "ensembl-variation"
-    "d34d25e481f22c11705b23a1aded34feb8434010"
-    "sha256-UjrLHF9EqI+Mp+SZR4sLNZUCGiA/UYhoFWtpwiKF8tM=";
-  ensembl-funcgen = ensemblGit "ensembl-funcgen"
-    "24e6da6efd80043cfba810accc4424c812f6a0b0"
-    "sha256-a9hxLBoXJsF5JWuRdpyOac1u033M8ivEjEQecuncghs=";
-
+  ensembl = ensemblGit "ensembl" "sha256-labyyqp+FRLIN9y4koePkX5fUls8HbCJlgYHUA0JR/0=";
+  ensembl-io = ensemblGit "ensembl-io" "sha256-r3RvN5U2kcyghoKM0XuiBRe54t1U4FaZ0QEeYIFiG0w=";
+  ensembl-variation = ensemblGit "ensembl-variation" "sha256-UjrLHF9EqI+Mp+SZR4sLNZUCGiA/UYhoFWtpwiKF8tM=";
+  ensembl-funcgen = ensemblGit "ensembl-funcgen" "sha256-a9hxLBoXJsF5JWuRdpyOac1u033M8ivEjEQecuncghs=";
 in
 perlPackages.buildPerlModule rec {
-  inherit (ensembl ensembl-io ensembl-variation ensembl-funcgen ensembl-xs vepPlugins);
-
   pname = "ensembl-vep";
   version = "110";
-  buildInputs = (with perlPackages; [ ArchiveZip BioBigFile BioDBHTS BioExtAlign BioPerl DBI DBDmysql LWP JSON ])
+  buildInputs =
+    (with perlPackages; [
+      ArchiveZip
+      BioBigFile
+      BioDBHTS
+      BioExtAlign
+      BioPerl
+      DBI
+      DBDmysql
+      LWP
+      JSON
+    ])
     ++ [
-    ensembl-xs
-    ensembl
-    ensembl-funcgen
-    ensembl-io
-    ensembl-variation
-  ];
+      ensembl-xs
+      ensembl
+      ensembl-funcgen
+      ensembl-io
+      ensembl-variation
+    ];
   propagatedBuildInputs = [ pkgs.htslib ];
   src = fetchFromGitHub {
     owner = "Ensembl";
@@ -103,12 +100,9 @@ perlPackages.buildPerlModule rec {
   };
 
   nativeBuildInputs = [ makeWrapper ];
-  buildPhase = "";
+  dontBuild = true;
   doCheck = false;
 
-  # Cached data.
-  # We have tabix, so we only use the indexed version
-  cacheURL = "ftp://ftp.ensembl.org/pub/release-${version}/variation/indexed_vep_cache/";
   outputs = [ "out" ];
   postFixup = ''
     wrapProgram $out/bin/vep \
@@ -119,14 +113,14 @@ perlPackages.buildPerlModule rec {
     cp vep filter_vep haplo variant_recoder $out/bin/
 
     mkdir -p $out/${perl.libPrefix}/${perl.version}/
-    cp -r modules/Bio  $out/${perl.libPrefix}/${perl.version}/
-    '';
+    cp -r modules/Bio $out/${perl.libPrefix}/${perl.version}/
+  '';
 
-    meta = with lib; {
-      homepage = "https://www.ensembl.org/info/docs/tools/vep/index.html";
-      description = "Annotate genetics variants based on genes, transcripts, and protein sequence, as well as regulatory regions";
-      license = lib.licenses.asl20 ;
-      mainProgram = "vep";
-      maintainers = with maintainers; [ apraga ];
-    };
+  meta = {
+    homepage = "https://www.ensembl.org/info/docs/tools/vep/index.html";
+    description = "Annotate genetics variants based on genes, transcripts, and protein sequence, as well as regulatory regions";
+    license = lib.licenses.asl20;
+    mainProgram = "vep";
+    maintainers = with lib.maintainers; [ apraga ];
+  };
 }
