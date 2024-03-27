bc1e589ccc6ab64c242e9127def1ac8d4d05d35d
Gaetan Lepage <gaetan@glepage.com>
2024-03-27 23:56:43 +0100

    python311Packages.torch-bin: 2.2.1 -> 2.2.2
    Changelog: https://github.com/pytorch/pytorch/releases/tag/v2.2.2

--- pkgs/development/python-modules/torch/bin.nix
+++ pkgs/development/python-modules/torch/bin.nix
@@ -25,7 +25,7 @@ let
   pyVerNoDot = builtins.replaceStrings [ "." ] [ "" ] python.pythonVersion;
   srcs = import ./binary-hashes.nix version;
   unsupported = throw "Unsupported system";
-  version = "2.2.1";
+  version = "2.2.2";
 in buildPythonPackage {
   inherit version;
 
