56bd37f57b5ca6f48580fc89326014e0bcea1557
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-27 12:35:36 +0000

    abaddon: 0.1.14 -> 0.2.0

--- pkgs/applications/networking/instant-messengers/abaddon/default.nix
+++ pkgs/applications/networking/instant-messengers/abaddon/default.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "abaddon";
-  version = "0.1.14";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "uowuo";
     repo = "abaddon";
     rev = "v${version}";
-    hash = "sha256-Amp6PkQWd4PnwUL29fzGETLuQXVEaARr+jIRlfrxTKc=";
+    hash = "sha256-Gl4BI+bkYuc5RtClfTth+WQ4EVYCWn0xnFOaQpS7yq0=";
     fetchSubmodules = true;
   };
 
