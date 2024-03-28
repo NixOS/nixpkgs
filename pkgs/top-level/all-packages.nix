69a2818f8353541da6dc82d2d510d9a30cf737fd
imrying <philiprying@gmail.com>
2024-03-28 17:15:27 +0100

    dstep init at version 1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7555,6 +7555,8 @@ with pkgs;
 
   dropbear = callPackage ../tools/networking/dropbear { };
 
+  dstep = callPackage ../tools/misc/dstep { };
+
   dsview = qt6Packages.callPackage ../applications/science/electronics/dsview { };
 
   dtach = callPackage ../tools/misc/dtach { };
