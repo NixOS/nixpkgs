5cb8cc5721071094c2f61d66427125e54dbb5617
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2024-03-28 10:27:57 -0600

    maintainers: add katanallama

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10113,6 +10113,11 @@
     githubId = 15855440;
     name = "Keanu Ashwell";
   };
+  katanallama = {
+    github = "katanallama";
+    githubId = 70604257;
+    name = "katanallama";
+  };
   katexochen = {
     github = "katexochen";
     githubId = 49727155;
