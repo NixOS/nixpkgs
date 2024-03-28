28f589dcd1b7cc189280c439fecd9c4e697f0701
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-28 22:10:40 +0100

    python312Packages.boto3: 1.34.58 -> 1.34.73
    Diff: https://github.com/boto/boto3/compare/refs/tags/1.34.58...1.34.73
    
    Changelog: https://github.com/boto/boto3/blob/1.34.73/CHANGELOG.rst

--- pkgs/development/python-modules/boto3/default.nix
+++ pkgs/development/python-modules/boto3/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "boto3";
-  version = "1.34.58"; # N.B: if you change this, change botocore and awscli to a matching version
+  version = "1.34.73"; # N.B: if you change this, change botocore and awscli to a matching version
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "boto";
     repo = "boto3";
     rev = "refs/tags/${version}";
-    hash = "sha256-2L4pHjrDoy7dPZm0nx+NXZV/K3ZVx7FrNGYZTMrwAs4=";
+    hash = "sha256-v/pk9j7fschNJvDI7oXXyfU4jjDKDoh/jsH1MdfMFjs=";
   };
 
   pythonRelaxDeps = [
