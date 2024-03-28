47c3b5e3a4f1b07fe9beba9cc9ca44ea6c3f04bb
Vladimír Čunát <v@cunat.cz>
2024-03-28 11:45:21 +0100

    python3Packages.apsw: 3.45.1.0 -> 3.45.2.0
    https://github.com/rogerbinns/apsw/releases/tag/3.45.2.0
    Fixes build (tests) after sqlite update in PR #295538

--- pkgs/development/python-modules/apsw/default.nix
+++ pkgs/development/python-modules/apsw/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "apsw";
-  version = "3.45.1.0";
+  version = "3.45.2.0";
   format = "setuptools";
 
   disabled = isPyPy;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "rogerbinns";
     repo = "apsw";
     rev = "refs/tags/${version}";
-    hash = "sha256-NkpkciLR2TgfK/7UQHEzvQu8qRj4UJyOlGQbiV23qrc=";
+    hash = "sha256-tTi3/10W4OoGH6PQVhvPWc5o09on5BZrWoAvrfh4C/E=";
   };
 
   buildInputs = [
