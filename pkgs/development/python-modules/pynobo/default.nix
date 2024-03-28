63bd7f0d714702404aa957abcc21c957e2743563
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-28 10:56:34 +0100

    python312Packages.pynobo: refactor

--- pkgs/development/python-modules/pynobo/default.nix
+++ pkgs/development/python-modules/pynobo/default.nix
@@ -2,21 +2,26 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "pynobo";
   version = "1.8.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "echoromeo";
-    repo = pname;
+    repo = "pynobo";
     rev = "refs/tags/v${version}";
     hash = "sha256-Hfyf7XGleDWTKKWNlItcBFuiS3UEwsYed7v5FPRdC0w=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   # Project has no tests
   doCheck = false;
