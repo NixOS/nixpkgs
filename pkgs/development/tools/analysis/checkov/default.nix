c24a08353f4c7c05df2e3946ba0f62a05598691a
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-28 08:39:38 +0100

    checkov: 3.2.47 -> 3.2.48
    Diff: https://github.com/bridgecrewio/checkov/compare/refs/tags/3.2.47...3.2.48
    
    Changelog: https://github.com/bridgecrewio/checkov/releases/tag/3.2.48

--- pkgs/development/tools/analysis/checkov/default.nix
+++ pkgs/development/tools/analysis/checkov/default.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "checkov";
-  version = "3.2.47";
+  version = "3.2.48";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bridgecrewio";
     repo = "checkov";
     rev = "refs/tags/${version}";
-    hash = "sha256-vwkTbHhgXaGeHrAkOM8gRDJ2VgbSmqt9Ia+qdOMxkko=";
+    hash = "sha256-B0wvUd94J+Q+ZuWitYMKYgOTSNGC4njPrUAhs1QPCuQ=";
   };
 
   patches = [
