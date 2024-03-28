e9fc5be0c783e9b6c2a87f095773fd2b42851503
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-28 13:56:35 +0100

    python312Packages.asgi-lifespan: init at 2.1.0
    Programmatic startup/shutdown of ASGI apps
    
    https://github.com/florimondmanca/asgi-lifespan

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -783,6 +783,8 @@ self: super: with self; {
 
   asgi-csrf = callPackage ../development/python-modules/asgi-csrf { };
 
+  asgi-lifespan = callPackage ../development/python-modules/asgi-lifespan { };
+
   asgi-logger = callPackage ../development/python-modules/asgi-logger { };
 
   asgineer = callPackage ../development/python-modules/asgineer { };
