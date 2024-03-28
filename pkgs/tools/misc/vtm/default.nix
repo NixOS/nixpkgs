8ac04a82db13a12d13ddab98c4c6a72c3bbec726
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-28 10:45:31 +0000

    vtm: 0.9.74 -> 0.9.76

--- pkgs/tools/misc/vtm/default.nix
+++ pkgs/tools/misc/vtm/default.nix
@@ -6,20 +6,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vtm";
-  version = "0.9.74";
+  version = "0.9.76";
 
   src = fetchFromGitHub {
     owner = "netxs-group";
     repo = "vtm";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-O8fnh8I3KbiOD40bU0eO7tbvpMoSCVonKPVFx5pynR4=";
+    hash = "sha256-F1ia1xyg2/Jnm/lsSn7bKNJ7cXIJuvEhNTEZWiYxGAo=";
   };
 
   nativeBuildInputs = [
     cmake
   ];
-
-  cmakeFlags = [ "../src" ];
 
   meta = {
     description = "Terminal multiplexer with window manager and session sharing";
