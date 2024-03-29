a46c9d4cdf9f12d85bc8e653101fe82ccf9c4abf
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-03-29 15:32:40 +0100

    nixos/partition-manager: remove with with lib, cleanup

--- nixos/modules/programs/partition-manager.nix
+++ nixos/modules/programs/partition-manager.nix
@@ -1,17 +1,13 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 {
-  meta.maintainers = [ maintainers.oxalica ];
+  meta.maintainers = [ lib.maintainers.oxalica ];
 
-  ###### interface
   options = {
-    programs.partition-manager.enable = mkEnableOption (lib.mdDoc "KDE Partition Manager");
+    programs.partition-manager.enable = lib.mkEnableOption (lib.mdDoc "KDE Partition Manager");
   };
 
-  ###### implementation
-  config = mkIf config.programs.partition-manager.enable {
+  config = lib.mkIf config.programs.partition-manager.enable {
     services.dbus.packages = [ pkgs.libsForQt5.kpmcore ];
     # `kpmcore` need to be installed to pull in polkit actions.
     environment.systemPackages = [ pkgs.libsForQt5.kpmcore pkgs.libsForQt5.partitionmanager ];
