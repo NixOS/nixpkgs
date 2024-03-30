2736530265e14ec3a7d898006c2a2b12b6a5db90
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-03-30 12:22:26 +0100

    buildDubPackage, dub-to-nix: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7553,6 +7553,9 @@ with pkgs;
 
   dub = callPackage ../development/tools/build-managers/dub { };
 
+  inherit (import ../build-support/dlang/dub-support.nix { inherit callPackage; })
+    buildDubPackage dub-to-nix;
+
   duc = callPackage ../tools/misc/duc { };
 
   duff = callPackage ../tools/filesystems/duff {
