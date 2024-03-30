39c135b1455115a94eca6072244c9072cfd18fa9
twitchy0 <code@nitinpassa.com>
2024-03-29 21:56:29 -0400

    imgkit: init at 1.2.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5592,6 +5592,8 @@ self: super: with self; {
 
   imgdiff = callPackage ../development/python-modules/imgdiff { };
 
+  imgkit = callPackage ../development/python-modules/imgkit { };
+
   imgsize = callPackage ../development/python-modules/imgsize { };
 
   imgtool = callPackage ../development/python-modules/imgtool { };
