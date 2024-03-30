9eaba2176c99b8857795475dd44b1cb403dd98c2
Joaquim Simão <osmano807@gmail.com>
2024-03-30 09:50:50 -0300

    maintainers: add osmano807

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14827,6 +14827,11 @@
     githubId = 111265;
     name = "Ozan Sener";
   };
+  osmano807 = {
+    github = "osmano807";
+    githubId = 321379;
+    name = "Joaquim Pedro";
+  };
   osnyx = {
     email = "os@flyingcircus.io";
     github = "osnyx";
