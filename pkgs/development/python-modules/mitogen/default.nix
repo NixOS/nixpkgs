6586a5bcb89049bd886ca2c49b1288ea19ccd63c
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-30 10:13:29 +0100

    python312Packages.mitogen: refactor

--- pkgs/development/python-modules/mitogen/default.nix
+++ pkgs/development/python-modules/mitogen/default.nix
@@ -2,21 +2,26 @@
 , buildPythonPackage
 , fetchFromGitHub
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "mitogen";
   version = "0.3.5";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "mitogen-hq";
-    repo = pname;
+    repo = "mitogen";
     rev = "refs/tags/v${version}";
     hash = "sha256-EDybx25eqJiMeoyB/lkuuofGTS8Q0ZjtB8HaKuGaKhk=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   # Tests require network access and Docker support
   doCheck = false;
@@ -28,6 +33,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python Library for writing distributed self-replicating programs";
     homepage = "https://github.com/mitogen-hq/mitogen";
+    changelog = "https://github.com/mitogen-hq/mitogen/blob/v${version}/docs/changelog.rst";
     license = licenses.bsd3;
     maintainers = with maintainers; [ fab ];
   };
