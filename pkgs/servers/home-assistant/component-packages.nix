72a4b62e36aa8e50e2f4d16faa4b58aeb4003064
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-30 12:02:26 +0100

    home-assistant: update component-packages

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -4193,7 +4193,8 @@
       rokuecp
     ];
     "romy" = ps: with ps; [
-    ]; # missing inputs: romy
+      romy
+    ];
     "roomba" = ps: with ps; [
       roombapy
     ];
@@ -6424,6 +6425,7 @@
     "rmvtransport"
     "roborock"
     "roku"
+    "romy"
     "roomba"
     "roon"
     "rpi_power"
