3c353aef95a7e780fcebd052f64c7e3e3dedf48b
Moraxyc <i@qaq.li>
2024-03-31 18:51:12 +0800

    maintainers: add moraxyc

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13385,6 +13385,12 @@
     githubId = 830082;
     name = "Nathan Moos";
   };
+  moraxyc = {
+    name = "Moraxyc Xu";
+    email = "nix@qaq.li";
+    github = "Moraxyc";
+    githubId = 69713071;
+  };
   moredread = {
     email = "code@apb.name";
     github = "Moredread";
