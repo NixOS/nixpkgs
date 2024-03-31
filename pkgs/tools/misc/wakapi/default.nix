55bc642bde4452142b499324581b8467dda2d4ec
R. Ryantm <ryantm-bot@ryantm.com>
2024-03-31 01:48:35 +0000

    wakapi: 2.10.5 -> 2.11.0

--- pkgs/tools/misc/wakapi/default.nix
+++ pkgs/tools/misc/wakapi/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "wakapi";
-  version = "2.10.5";
+  version = "2.11.0";
 
   src = fetchFromGitHub {
     owner = "muety";
     repo = pname;
     rev = version;
-    sha256 = "sha256-u+br+C5tWSCc8LPGS3/CMkxpqaO30BsWPRFGOkloMlA=";
+    sha256 = "sha256-ZSnbbxSRgdAHNAFw9gUl02gw8H9/5UmgNphNpAYw2hY=";
   };
 
-  vendorHash = "sha256-TeKVhG1V9inyDWfILwtpU9QknJ9bt3Dja5GVHrK9PkA=";
+  vendorHash = "sha256-0IB9k/NY8Gcl2qLABMQJQ7BLimeDi3s9GvkmyCjxtfg=";
 
   # Not a go module required by the project, contains development utilities
   excludedPackages = [ "scripts" ];
