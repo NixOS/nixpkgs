35f198602ef36915bb03ece8870a0ced8c8bb046
Yueh-Shun Li <shamrocklee@posteo.net>
2024-04-01 02:51:04 +0800

    cmake: setup-hook.sh: unify indentation

--- pkgs/by-name/cm/cmake/setup-hook.sh
+++ pkgs/by-name/cm/cmake/setup-hook.sh
@@ -38,10 +38,10 @@ cmakeConfigurePhase() {
     declare -ga cmakeFlagsArray
 
     if [[ -n "${__structuredAttrs-}" ]]; then
-      cmakeFlagsArray=("${cmakeFlags[@]}" "${cmakeFlagsArray[@]}")
+        cmakeFlagsArray=("${cmakeFlags[@]}" "${cmakeFlagsArray[@]}")
     else
-      # shellcheck disable=SC2206
-      cmakeFlagsArray=($cmakeFlags "${cmakeFlagsArray[@]}")
+        # shellcheck disable=SC2206
+        cmakeFlagsArray=($cmakeFlags "${cmakeFlagsArray[@]}")
     fi
 
     if [ -z "${dontAddPrefix-}" ]; then
@@ -159,37 +159,37 @@ fi
 addEnvHooks "$targetOffset" addCMakeParams
 
 makeCmakeFindLibs(){
-  isystem_seen=
-  iframework_seen=
-  for flag in ${NIX_CFLAGS_COMPILE-} ${NIX_LDFLAGS-}; do
-    if test -n "$isystem_seen" && test -d "$flag"; then
-      isystem_seen=
-      export CMAKE_INCLUDE_PATH="${CMAKE_INCLUDE_PATH-}${CMAKE_INCLUDE_PATH:+:}${flag}"
-    elif test -n "$iframework_seen" && test -d "$flag"; then
-      iframework_seen=
-      export CMAKE_FRAMEWORK_PATH="${CMAKE_FRAMEWORK_PATH-}${CMAKE_FRAMEWORK_PATH:+:}${flag}"
-    else
-      isystem_seen=
-      iframework_seen=
-      case $flag in
-        -I*)
-          export CMAKE_INCLUDE_PATH="${CMAKE_INCLUDE_PATH-}${CMAKE_INCLUDE_PATH:+:}${flag:2}"
-          ;;
-        -L*)
-          export CMAKE_LIBRARY_PATH="${CMAKE_LIBRARY_PATH-}${CMAKE_LIBRARY_PATH:+:}${flag:2}"
-          ;;
-        -F*)
-          export CMAKE_FRAMEWORK_PATH="${CMAKE_FRAMEWORK_PATH-}${CMAKE_FRAMEWORK_PATH:+:}${flag:2}"
-          ;;
-        -isystem)
-          isystem_seen=1
-          ;;
-        -iframework)
-          iframework_seen=1
-          ;;
-      esac
-    fi
-  done
+    isystem_seen=
+    iframework_seen=
+    for flag in ${NIX_CFLAGS_COMPILE-} ${NIX_LDFLAGS-}; do
+        if test -n "$isystem_seen" && test -d "$flag"; then
+            isystem_seen=
+            export CMAKE_INCLUDE_PATH="${CMAKE_INCLUDE_PATH-}${CMAKE_INCLUDE_PATH:+:}${flag}"
+        elif test -n "$iframework_seen" && test -d "$flag"; then
+            iframework_seen=
+            export CMAKE_FRAMEWORK_PATH="${CMAKE_FRAMEWORK_PATH-}${CMAKE_FRAMEWORK_PATH:+:}${flag}"
+        else
+            isystem_seen=
+            iframework_seen=
+            case $flag in
+            -I*)
+                export CMAKE_INCLUDE_PATH="${CMAKE_INCLUDE_PATH-}${CMAKE_INCLUDE_PATH:+:}${flag:2}"
+                ;;
+            -L*)
+                export CMAKE_LIBRARY_PATH="${CMAKE_LIBRARY_PATH-}${CMAKE_LIBRARY_PATH:+:}${flag:2}"
+                ;;
+            -F*)
+                export CMAKE_FRAMEWORK_PATH="${CMAKE_FRAMEWORK_PATH-}${CMAKE_FRAMEWORK_PATH:+:}${flag:2}"
+                ;;
+            -isystem)
+                isystem_seen=1
+                ;;
+            -iframework)
+                iframework_seen=1
+                ;;
+            esac
+        fi
+    done
 }
 
 # not using setupHook, because it could be a setupHook adding additional
