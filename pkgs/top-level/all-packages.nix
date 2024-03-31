d9f0c7947673bd10f12598f37abf4229ee90e670
seth <getchoo@tuta.io>
2024-03-31 18:12:47 -0400

    vkdt-wayland: drop
    this follows `glfw-wayland` also being dropped

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25211,8 +25211,6 @@ with pkgs;
 
   vkdt = callPackage ../applications/graphics/vkdt { };
 
-  vkdt-wayland = callPackage ../applications/graphics/vkdt { glfw = glfw-wayland; };
-
   vlock = callPackage ../misc/screensavers/vlock { };
 
   virtualpg = callPackage ../development/libraries/virtualpg { };
