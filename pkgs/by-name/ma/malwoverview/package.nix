e70d52bacb61d2124021203e1be8d1e93363c157
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-31 17:49:01 +0200

    malwoverview: init at 5.4.2
    Tool for threat hunting and gathering intel information from various sources
    
    https://github.com/alexandreborges/malwoverview

---
+++ pkgs/by-name/ma/malwoverview/package.nix
@@ -0,0 +1,57 @@
+{ lib
+, fetchFromGitHub
+, python3
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "malwoverview";
+  version = "5.4.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "alexandreborges";
+    repo = "malwoverview";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-WAlVEEukPOynCGpRdQu3wP+JZ1UKuSR6pH5ek81L73E=";
+  };
+
+  pythonRemoveDeps = [
+    "pathlib"
+  ];
+
+  nativeBuildInputs = with python3.pkgs; [
+    pythonRelaxDepsHook
+  ];
+
+  build-system  = with python3.pkgs; [
+    setuptools
+  ];
+
+  dependencies = with python3.pkgs; [
+    colorama
+    configparser
+    geocoder
+    pefile
+    polyswarm-api
+    python-magic
+    requests
+    simplejson
+    validators
+  ];
+
+  # Project has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "malwoverview"
+  ];
+
+  meta = with lib; {
+    description = "Tool for threat hunting and gathering intel information from various sources";
+    homepage = "https://github.com/alexandreborges/malwoverview";
+    changelog = "https://github.com/alexandreborges/malwoverview/releases/tag/v${version}";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ fab ];
+    mainProgram = "malwoverview.py";
+  };
+}
