48347342edcf9adea58561f511d807450c53dad3
Philip Taron <philip.taron@gmail.com>
2024-03-31 13:57:57 -0700

    Avoid top-level `with ...;` in pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix

--- pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
@@ -1,8 +1,19 @@
 { pkgs, haskellLib }:
 
-with haskellLib;
-
 let
+  inherit (haskellLib)
+    addBuildDepend
+    addBuildDepends
+    disableOptimization
+    doDistribute
+    doJailbreak
+    dontCheck
+    generateOptparseApplicativeCompletions
+    markUnbroken
+    overrideCabal
+    unmarkBroken
+    ;
+
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
   inherit (pkgs) lib;
 in
