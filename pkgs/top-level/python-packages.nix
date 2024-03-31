a659454c92f595a5fd9d49725cd2fbe324bd0f46
Fabian Affolter <mail@fabian-affolter.ch>
2024-03-31 17:45:45 +0200

    python312Packages.polyswarm-api: init at 3.5.2
    Library to interface with the PolySwarm consumer APIs
    
    https://github.com/polyswarm/polyswarm-api

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10087,6 +10087,8 @@ self: super: with self; {
 
   polygon3 = callPackage ../development/python-modules/polygon3 { };
 
+  polyswarm-api = callPackage ../development/python-modules/polyswarm-api { };
+
   pomegranate = callPackage ../development/python-modules/pomegranate { };
 
   pontos = callPackage ../development/python-modules/pontos { };
