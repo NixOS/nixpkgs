6c48b2eb9cf83ffb3b6259bc62490bf66dc208ba
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-01 13:49:55 +0000

    jbang: 0.115.0 -> 0.116.0

--- pkgs/development/tools/jbang/default.nix
+++ pkgs/development/tools/jbang/default.nix
@@ -1,12 +1,12 @@
 { stdenv, lib, fetchzip, jdk, makeWrapper, coreutils, curl }:
 
 stdenv.mkDerivation rec {
-  version = "0.115.0";
+  version = "0.116.0";
   pname = "jbang";
 
   src = fetchzip {
     url = "https://github.com/jbangdev/jbang/releases/download/v${version}/${pname}-${version}.tar";
-    sha256 = "sha256-JTyMX3120h+xtXnkTW313zc7OkWMB4DayeRW6hNc4/A=";
+    sha256 = "sha256-EyyTokVvhIMWOq4dLCi2wynoX++5DoJP20irPdfD58A=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
