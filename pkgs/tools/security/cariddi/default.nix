9f0338a909c90f9363822bf3a3e24d8bff17beb8
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-02 11:05:33 +0200

    cariddi: refactor

--- pkgs/tools/security/cariddi/default.nix
+++ pkgs/tools/security/cariddi/default.nix
@@ -9,19 +9,24 @@ buildGoModule rec {
 
   src = fetchFromGitHub {
     owner = "edoardottt";
-    repo = pname;
+    repo = "cariddi";
     rev = "refs/tags/v${version}";
     hash = "sha256-nApgsvHSMWmgJWyvdtBdrGt9v8YSwWiGnmrDS8vVvDw=";
   };
 
   vendorHash = "sha256-GgJyYDnlaFybf3Gu1gVcA12HkA0yUIjYEFj0G83GVGQ=";
 
+  ldflags = [
+    "-w"
+    "-s"
+  ];
+
   meta = with lib; {
     description = "Crawler for URLs and endpoints";
-    mainProgram = "cariddi";
     homepage = "https://github.com/edoardottt/cariddi";
     changelog = "https://github.com/edoardottt/cariddi/releases/tag/v${version}";
     license = with licenses; [ gpl3Plus ];
     maintainers = with maintainers; [ fab ];
+    mainProgram = "cariddi";
   };
 }
