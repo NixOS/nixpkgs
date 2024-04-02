ef5e2e5c5e8d879f205b5f200aae657b0c52d530
Sumner Evans <me@sumnerevans.com>
2024-04-02 12:52:32 -0600

    matrix-synapse-unwrapped: 1.103.0 -> 1.104.0
    https://github.com/element-hq/synapse/releases/tag/v1.104.0
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- pkgs/servers/matrix-synapse/default.nix
+++ pkgs/servers/matrix-synapse/default.nix
@@ -32,20 +32,20 @@ let
 in
 python.pkgs.buildPythonApplication rec {
   pname = "matrix-synapse";
-  version = "1.103.0";
+  version = "1.104.0";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "element-hq";
     repo = "synapse";
     rev = "v${version}";
-    hash = "sha256-NwHX4pOM2PUf2MldaPTOzP9gOcTmILxM1Sx2HPkLBcw=";
+    hash = "sha256-/P7EBtXSYygUrqKQ4niI8J5zkBPZDgHCW/j2rFxRlsY=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-AyV0JPPJkJ4jdaw0FUXPqGF3Qkce1+RK70FkXAw+bLA=";
+    hash = "sha256-0lCbIlEM4wIG7W5BXWIZWkS6c/BkEG13xtcnPm3LjgY=";
   };
 
   postPatch = ''
