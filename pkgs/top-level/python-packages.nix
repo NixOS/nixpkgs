43118ee972e824f28cd243140c337459dcb8c3d1
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-03 09:33:11 +0200

    python311Packages.zha: init at 0.0.5
    Zigbee Home Automation
    
    https://github.com/zigpy/zha

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17100,6 +17100,8 @@ self: super: with self; {
 
   zfec = callPackage ../development/python-modules/zfec { };
 
+  zha = callPackage ../development/python-modules/zha { };
+
   zha-quirks = callPackage ../development/python-modules/zha-quirks { };
 
   ziafont = callPackage ../development/python-modules/ziafont { };
