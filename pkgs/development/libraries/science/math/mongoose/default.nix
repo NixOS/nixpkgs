11681cf289c8c0bc13eb407331db662e21d89d51
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-04-03 14:27:05 +0200

    mongoose: 3.3.2 -> 3.3.3

--- pkgs/development/libraries/science/math/mongoose/default.nix
+++ pkgs/development/libraries/science/math/mongoose/default.nix
@@ -7,11 +7,11 @@
 }:
 
 let
-  suitesparseVersion = "7.6.0";
+  suitesparseVersion = "7.7.0";
 in
 stdenv.mkDerivation {
   pname = "mongoose";
-  version = "3.3.2";
+  version = "3.3.3";
 
   outputs = [ "bin" "out" "dev" ];
 
@@ -19,7 +19,7 @@ stdenv.mkDerivation {
     owner = "DrTimothyAldenDavis";
     repo = "SuiteSparse";
     rev = "v${suitesparseVersion}";
-    hash = "sha256-zZXbUNXxVi4mpI4j4GjyVYraoPFWcuep9q5jl1XdqEo=";
+    hash = "sha256-wE1DKC0Hn3Q9X1FzKH7Kev26ICNWH0LEKQIQP98AGuo=";
   };
 
   nativeBuildInputs = [
