abae504e2aaed4fcf99c401beb4f56811b302082
Mario Rodas <marsam@users.noreply.github.com>
2024-04-03 04:20:00 +0000

    nodejs_21: 21.7.1 -> 21.7.2
    Changelog: https://github.com/nodejs/node/releases/tag/v21.7.2
    (cherry picked from commit 17064f0793ebe80fcc645d32f753ed5a1ce07d5f)

--- pkgs/development/web/nodejs/v21.nix
+++ pkgs/development/web/nodejs/v21.nix
@@ -8,8 +8,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "21.7.1";
-  sha256 = "sha256-EnK24SnVZNveF1J7hEIQuXHCCnCucpJoGGt8udmQpks=";
+  version = "21.7.2";
+  sha256 = "sha256-tLHioH6W+F9s40ovv+o0hpGu/lyyGappUeI8zJkfni8=";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
