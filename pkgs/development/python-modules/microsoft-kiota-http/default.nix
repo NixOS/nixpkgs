e5b7ca7b56810955bc614396f0c55e7830e84ae6
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-03 22:59:06 +0200

    python312Packages.microsoft-kiota-http: use nixfmt

--- pkgs/development/python-modules/microsoft-kiota-http/default.nix
+++ pkgs/development/python-modules/microsoft-kiota-http/default.nix
@@ -1,16 +1,17 @@
-{ lib
-, buildPythonPackage
-, fetchFromGitHub
-, flit-core
-, httpx
-, microsoft-kiota-abstractions
-, opentelemetry-api
-, opentelemetry-sdk
-, pytest-asyncio
-, pytest-mock
-, pytestCheckHook
-, pythonOlder
-, urllib3
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  flit-core,
+  httpx,
+  microsoft-kiota-abstractions,
+  opentelemetry-api,
+  opentelemetry-sdk,
+  pytest-asyncio,
+  pytest-mock,
+  pytestCheckHook,
+  pythonOlder,
+  urllib3,
 }:
 
 buildPythonPackage rec {
@@ -27,9 +28,7 @@ buildPythonPackage rec {
     hash = "sha256-I16WARk6YBr8KgE9MtHcA5VdsnLXBKcZOaqRL/eqwKE=";
   };
 
-  nativeBuildInputs = [
-    flit-core
-  ];
+  nativeBuildInputs = [ flit-core ];
 
   propagatedBuildInputs = [
     httpx
@@ -45,9 +44,7 @@ buildPythonPackage rec {
     urllib3
   ];
 
-  pythonImportsCheck = [
-    "kiota_http"
-  ];
+  pythonImportsCheck = [ "kiota_http" ];
 
   meta = with lib; {
     description = "HTTP request adapter implementation for Kiota clients for Python";
