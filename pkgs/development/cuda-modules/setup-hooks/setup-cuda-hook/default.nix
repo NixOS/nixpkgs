0e4b5c5a3ea10d44059dbcab8d3578109ac72c31
Connor Baker <connor.baker@tweag.io>
2024-04-03 16:56:52 +0000

    cuda-modules/setup-hooks: refactor to use sub-directories

---
+++ pkgs/development/cuda-modules/setup-hooks/setup-cuda-hook/default.nix
@@ -0,0 +1,16 @@
+# Currently propagated by cuda_nvcc or cudatoolkit, rather than used directly
+{ backendStdenv, makeSetupHook }:
+let
+  inherit (backendStdenv) cc;
+in
+makeSetupHook {
+  name = "setup-cuda-hook";
+  substitutions = {
+    # Required in addition to ccRoot as otherwise bin/gcc is looked up
+    # when building CMakeCUDACompilerId.cu
+    ccFullPath = "${cc}/bin/${cc.targetPrefix}c++";
+    # Point NVCC at a compatible compiler
+    ccRoot = "${cc}";
+    setupCudaHook = placeholder "out";
+  };
+} ./setup-cuda-hook.sh
