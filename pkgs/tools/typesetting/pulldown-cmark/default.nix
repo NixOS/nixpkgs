9d2926b9fb8b9c61d928029a1c14f14e746ec2e5
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-03 01:11:18 +0000

    pulldown-cmark: 0.10.0 -> 0.10.2

--- pkgs/tools/typesetting/pulldown-cmark/default.nix
+++ pkgs/tools/typesetting/pulldown-cmark/default.nix
@@ -5,14 +5,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pulldown-cmark";
-  version = "0.10.0";
+  version = "0.10.2";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-7ZO3MdQBNgltrd4Anu19g0Gkye6Bc2WHDuSng6mB9pM=";
+    hash = "sha256-jRQ2BnaElhciyBPQiHw680uFC6FXs+rP8rJGWH5ZbCc=";
   };
 
-  cargoHash = "sha256-4UUdsS3dK5a6phwYZqjNwX52UMLLe/LHxOiBanTRMZM=";
+  cargoHash = "sha256-UsdssFfy7cTM0wrfLDLrzbKudB5vqFINInJAteH5OTk=";
 
   meta = {
     description = "A pull parser for CommonMark written in Rust";
