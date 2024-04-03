ca1a1953dddd5629da83bf2f138ba996319b0f9d
Mario Rodas <marsam@users.noreply.github.com>
2024-04-03 04:20:00 +0000

    git-absorb: 0.6.12 -> 0.6.13
    Diff: https://github.com/tummychow/git-absorb/compare/refs/tags/0.6.12...0.6.13

--- pkgs/applications/version-management/git-absorb/default.nix
+++ pkgs/applications/version-management/git-absorb/default.nix
@@ -2,20 +2,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "git-absorb";
-  version = "0.6.12";
+  version = "0.6.13";
 
   src = fetchFromGitHub {
     owner = "tummychow";
     repo = "git-absorb";
     rev = "refs/tags/${version}";
-    hash = "sha256-yHCO1v1d0MUakae16fFVvtKG3rVxU/Cii/G6IKzyebA=";
+    hash = "sha256-k0smjIpy/+y6M5p24Ju4CVJkThzWOgp5kBJuVnCrXiE=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
 
   buildInputs = lib.optionals stdenv.isDarwin [ Security ];
 
-  cargoHash = "sha256-Bx7gH7jSLizG95JyBtziPBby9mF1Nj3CQexIg6gaiM0=";
+  cargoHash = "sha256-bRPdtiC9Dwi21g4WtjawQ2AUdizUEX2zPHAnG08D3ac=";
 
   postInstall = ''
     installManPage Documentation/git-absorb.1
