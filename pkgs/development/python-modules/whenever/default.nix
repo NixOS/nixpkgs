22558083a10bf9aba88ab772f5daf73e64c601e4
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-03 03:24:31 +0000

    python312Packages.whenever: 0.5.0 -> 0.5.1

--- pkgs/development/python-modules/whenever/default.nix
+++ pkgs/development/python-modules/whenever/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "whenever";
-  version = "0.5.0";
+  version = "0.5.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "ariebovenberg";
     repo = "whenever";
     rev = "refs/tags/${version}";
-    hash = "sha256-5Ik9+i5T5ztb+2zqFZ+SBmrZFLDxji66e3lK0z2w92c=";
+    hash = "sha256-RH2614M91zYULNTQsr6JoKfxlnGyAJsCkB7oeiz7urs=";
   };
 
   postPatch = ''
