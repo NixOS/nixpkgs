b506ac750e6f25c632ba214ae073e9225e60a398
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-03 21:47:52 +0200

    python312Packages.referencing: use nixfmt

--- pkgs/development/python-modules/referencing/default.nix
+++ pkgs/development/python-modules/referencing/default.nix
@@ -1,14 +1,15 @@
-{ lib
-, attrs
-, buildPythonPackage
-, fetchFromGitHub
-, hatch-vcs
-, hatchling
-, jsonschema
-, pytest-subtests
-, pytestCheckHook
-, pythonOlder
-, rpds-py
+{
+  lib,
+  attrs,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatch-vcs,
+  hatchling,
+  jsonschema,
+  pytest-subtests,
+  pytestCheckHook,
+  pythonOlder,
+  rpds-py,
 }:
 
 let
@@ -48,9 +49,7 @@ let
 
     passthru.tests.referencing = self.overridePythonAttrs { doCheck = true; };
 
-    pythonImportsCheck = [
-      "referencing"
-    ];
+    pythonImportsCheck = [ "referencing" ];
 
     meta = with lib; {
       description = "Cross-specification JSON referencing";
@@ -61,4 +60,4 @@ let
     };
   };
 in
-  self
+self
