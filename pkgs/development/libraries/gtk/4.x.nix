6173115fc7d234af170c9172bb1d3768ad602abb
Maxine Aubrey <max@ine.dev>
2024-04-03 18:21:11 +0200

    gtk4: 4.14.1 → 4.14.2
    https://gitlab.gnome.org/GNOME/gtk/-/compare/4.14.1...4.14.2

--- pkgs/development/libraries/gtk/4.x.nix
+++ pkgs/development/libraries/gtk/4.x.nix
@@ -70,7 +70,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gtk4";
-  version = "4.14.1";
+  version = "4.14.2";
 
   outputs = [ "out" "dev" ] ++ lib.optionals x11Support [ "devdoc" ];
   outputBin = "dev";
@@ -82,7 +82,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = with finalAttrs; "mirror://gnome/sources/gtk/${lib.versions.majorMinor version}/gtk-${version}.tar.xz";
-    hash = "sha256-/O+z8TL4zEcRqe+ls1PJrpu17v8CRvp028Ly+Dm54wg=";
+    hash = "sha256-ImBM7yiYp55fIUO7eu4rfR+i65RpianRM47PnIrg4HI=";
   };
 
   depsBuildBuild = [
