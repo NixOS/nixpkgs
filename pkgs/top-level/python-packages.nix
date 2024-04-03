089ccb554a3d147900730f5a56ecedd7f05aa4d1
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-03 21:36:50 +0200

    python311Packages.fyta-cli: init at 0.3.4
    Module to access the FYTA API
    
    https://github.com/dontinelli/fyta_cli

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4554,6 +4554,8 @@ self: super: with self; {
 
   fx2 = callPackage ../development/python-modules/fx2 { };
 
+  fyta-cli = callPackage ../development/python-modules/fyta-cli { };
+
   g2pkk = callPackage ../development/python-modules/g2pkk { };
 
   galario = toPythonModule (pkgs.galario.override {
