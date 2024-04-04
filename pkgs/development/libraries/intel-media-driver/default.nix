615b7edb5bb22ee0081309fff99f9144de524c42
Todd Brown <todd.brown@hey.com>
2024-04-04 04:06:45 -0700

    intel-media-driver 23.3.5 -> 23.4.3

--- pkgs/development/libraries/intel-media-driver/default.nix
+++ pkgs/development/libraries/intel-media-driver/default.nix
@@ -16,7 +16,7 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-media-driver";
-  version = "23.3.5";
+  version = "23.4.3";
 
   outputs = [ "out" "dev" ];
 
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
     owner = "intel";
     repo = "media-driver";
     rev = "intel-media-${version}";
-    hash = "sha256-7OdLpqO2evNeyxceOtHEI7sJCVybqvrcM1ZZx8bI4xw=";
+    hash = "sha256-KVdnCl+jModdDNBtssTnVnq82ZbWovHlaMZ/9/xU8mU=";
   };
 
   patches = [
