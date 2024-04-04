cd6ddb3a82a664c2543bb6f8b9d3ff9e65983fe9
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-04-04 11:48:04 +0200

    home-assistant: 2024.3.3 -> 2024.4.0
    https://www.home-assistant.io/blog/2024/04/03/release-20244/

--- pkgs/servers/home-assistant/stubs.nix
+++ pkgs/servers/home-assistant/stubs.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "homeassistant-stubs";
-  version = "2024.3.3";
+  version = "2024.4.0";
   format = "pyproject";
 
   disabled = python.version != home-assistant.python.version;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "KapJI";
     repo = "homeassistant-stubs";
     rev = "refs/tags/${version}";
-    hash = "sha256-Qc+pwG87R57wGyCujMQ6qygDOhRh/FoDf1HTRZS3M6s=";
+    hash = "sha256-5igsxs0/1S0sYlaNxFSsTAo9HkIAqn3XZ031VjAkEQg=";
   };
 
   nativeBuildInputs = [
