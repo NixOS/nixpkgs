19a38a11c68d4137216792aeba179f0158d53086
Olli Helenius <liff@iki.fi>
2024-04-04 23:53:40 +0300

    jetbrains.jdk: add updateScript
    The update script uses the `jbr/release` file in IDEA Ultimate’s
    current release tarball to resolve the latest JBR version JetBrains
    is currently using in their IDEs.

--- pkgs/development/compilers/jetbrains-jdk/default.nix
+++ pkgs/development/compilers/jetbrains-jdk/default.nix
@@ -36,29 +36,26 @@ let
     "x86_64-linux" = "x64";
   }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
   cpu = stdenv.hostPlatform.parsed.cpu.name;
+
+  versionInfo = lib.importJSON ./version-info.json;
+
 in
 openjdk17.overrideAttrs (oldAttrs: rec {
   pname = "jetbrains-jdk" + lib.optionalString withJcef "-jcef";
-  javaVersion = "17.0.8";
-  build = "1000.8";
-  # To get the new tag:
-  # git clone https://github.com/jetbrains/jetbrainsruntime
-  # cd jetbrainsruntime
-  # git reset --hard [revision]
-  # git log --simplify-by-decoration --decorate=short --pretty=short | grep "jbr-" --color=never | cut -d "(" -f2 | cut -d ")" -f1 | awk '{print $2}' | sort -t "-" -k 2 -g | tail -n 1 | tr -d ","
-  openjdkTag = "jbr-17.0.7+7";
+  inherit (versionInfo)
+    javaVersion
+    build
+    openjdkTag
+    SOURCE_DATE_EPOCH;
   version = "${javaVersion}-b${build}";
 
   src = fetchFromGitHub {
     owner = "JetBrains";
     repo = "JetBrainsRuntime";
-    rev = "jb${version}";
-    hash = "sha256-PXS8wRF37D9vzeC4CvmB3szFMbt+NRqhQqtPZcbeAO8=";
+    inherit (versionInfo) rev hash;
   };
 
   BOOT_JDK = openjdk17-bootstrap.home;
-  # run `git log -1 --pretty=%ct` in jdk repo for new value on update
-  SOURCE_DATE_EPOCH = 1691119859;
 
   patches = [ ];
 
@@ -152,5 +149,6 @@ openjdk17.overrideAttrs (oldAttrs: rec {
 
   passthru = oldAttrs.passthru // {
     home = "${jetbrains.jdk}/lib/openjdk";
+    updateScript = [ ./update.py jetbrains.idea-ultimate.src jetbrains.idea-ultimate.version ];
   };
 })
