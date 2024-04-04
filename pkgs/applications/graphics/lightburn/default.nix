00ae1d67cac4a4833e106b3828e0b68465ab3aa2
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-04 14:50:17 +0000

    lightburn: 1.5.05 -> 1.5.06

--- pkgs/applications/graphics/lightburn/default.nix
+++ pkgs/applications/graphics/lightburn/default.nix
@@ -6,7 +6,7 @@
 
 stdenv.mkDerivation rec {
   pname = "lightburn";
-  version = "1.5.05";
+  version = "1.5.06";
 
   nativeBuildInputs = [
     p7zip
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/LightBurnSoftware/deployment/releases/download/${version}/LightBurn-Linux64-v${version}.7z";
-    sha256 = "sha256-rvskm0Br+3HqPo2DW/ZUIl8n3+UJKPlmkEtQofw05x4=";
+    sha256 = "sha256-VJMfvZHqdgUQXLyBBZLEV3nyP0qzibuXLnZyBhBBg9E=";
   };
 
   buildInputs = [
