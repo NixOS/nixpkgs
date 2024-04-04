2c10be2751fec16d0c64cf1c8ac29afab11d62bc
Nikolay Korotkiy <sikmir@disroot.org>
2024-04-05 00:00:18 +0400

    gpscorrelate: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31717,8 +31717,6 @@ with pkgs;
     withDoc = true;
   };
 
-  gpscorrelate = callPackage ../applications/misc/gpscorrelate { };
-
   gpsd = callPackage ../servers/gpsd { };
 
   gpsprune = callPackage ../applications/misc/gpsprune { };
