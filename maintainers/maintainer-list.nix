d6d7ed64850ce1afd8b518f46dcae41de5407d7d
Harry Maclean <harry@hmac.dev>
2024-04-04 17:21:12 +0100

    maintainers: add hmac

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8011,6 +8011,11 @@
     githubId = 6074754;
     name = "Hlodver Sigurdsson";
   };
+  hmac = {
+    github = "hmac";
+    githubId = 237543;
+    name = "Harry Maclean";
+  };
   hmajid2301 = {
     name = "Haseeb Majid";
     email = "hello@haseebmajid.dev";
