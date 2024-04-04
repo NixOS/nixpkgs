4f95acbbf3f62652f503edb0df7676ff238d75ea
Steffen Vogel <post@steffenvogel.de>
2024-04-04 21:25:49 +0200

    libiec61850: extend to all Linux platforms

--- pkgs/by-name/li/libiec61850/package.nix
+++ pkgs/by-name/li/libiec61850/package.nix
@@ -28,6 +28,6 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://libiec61850.com/";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ stv0g ];
-    platforms = [ "x86_64-linux" ];
+    platforms = platforms.linux;
   };
 })
