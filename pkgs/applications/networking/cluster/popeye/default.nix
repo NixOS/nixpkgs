71827415f8fd4b4fb7790f24fd1f416b5ac28086
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-04 11:45:01 +0000

    popeye: 0.21.2 -> 0.21.3

--- pkgs/applications/networking/cluster/popeye/default.nix
+++ pkgs/applications/networking/cluster/popeye/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "popeye";
-  version = "0.21.2";
+  version = "0.21.3";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "derailed";
     repo = "popeye";
-    sha256 = "sha256-NhQER6XeicpQY0rYisGvkUCHYsURJqt6xVKc9F0CmtE=";
+    sha256 = "sha256-2+AV2gt67zLa7bRIfsxQIeB82iJba8Y4P0fq9273r7Q=";
   };
 
   ldflags = [
