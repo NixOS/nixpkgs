d70a827335f91fc21661d67e4988bdd72a6fd45a
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-04 12:32:28 +0000

    python311Packages.jsonschema-spec: 0.2.4 -> 0.3.2

--- pkgs/development/python-modules/jsonschema-spec/default.nix
+++ pkgs/development/python-modules/jsonschema-spec/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "jsonschema-spec";
-  version = "0.2.4";
+  version = "0.3.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "p1c2u";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-1Flb3XQCGhrAYzTvriSVhHDb/Z/uvCyZdbav2u7f3sg=";
+    hash = "sha256-HC0yfACKFIQEQoIa8/FUKyV8YS8TQ0BY7i3n9xCdKz8=";
   };
 
   postPatch = ''
