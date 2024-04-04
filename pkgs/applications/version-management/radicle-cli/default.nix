04a5396341eb08ed62681e06c9f1f2a815279a86
roblabla <unfiltered@roblab.la>
2024-04-04 12:49:56 +0200

    radicle-cli: 0.6.1 -> 1.0.0-rc8

--- pkgs/applications/version-management/radicle-cli/default.nix
+++ pkgs/applications/version-management/radicle-cli/default.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, fetchFromGitHub
+, fetchgit
 , rustPlatform
 , pkg-config
 , cmake
@@ -18,28 +18,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "radicle-cli";
-  version = "0.6.1";
+  version = "1.0.0-rc8";
 
-  src = fetchFromGitHub {
-    owner = "radicle-dev";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-LS6zYpMg0LanRL2M8ioGG8Ys07TPT/3hP7geEGehwxg=";
+  src = fetchgit {
+    url = "https://seed.radicle.xyz/z3gqcJUoA1n9HaHKufZs5FCSGazv5.git";
+    rev = "0d880e12e151979ab676083f779599b8f14b69fb";
+    hash = "sha256-F2n7ui0EgXK8fT76M14RVhXXGeRYub+VpH+puDUJ1pQ=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "automerge-0.0.2" = "sha256-MZ1/rca8ZsEUhd3bhd502PHlBbvqAOtnWFEdp7XWmYE=";
-      "automerge-0.1.0" = "sha256-dwbmx3W13oZ1O0Uw3/D5Z0ht1BO1PmVVoWc/tLCm0/4=";
-      "cob-0.1.0" = "sha256-ewPJEx7OSr8X6e5QJ4dh2SbzZ2TDa8G4zBR5euBbABo=";
-      "libusb1-sys-0.6.2" = "sha256-577ld1xqJkHp2bqALNq5IuZivD8y+VO8vNy9Y+hfq6c=";
-      "walletconnect-0.1.0" = "sha256-fdgdhotTYBmWbR4r0OMplOwhYq1C7jkuOdhKASjH+Fs=";
-    };
-  };
-
-  # Otherwise, there are errors due to the `abigen` macro from `ethers`.
-  auditable = false;
+  cargoSha256 = "sha256-Ej9cxuHxiQDwCXOwcIyfCau9Thhz+hf3IevN6F4rDjM=";
 
   nativeBuildInputs = [
     pkg-config
@@ -69,6 +56,7 @@ rustPlatform.buildRustPackage rec {
     git
     openssh
   ];
+
   preCheck = ''
     eval $(ssh-agent)
   '';
