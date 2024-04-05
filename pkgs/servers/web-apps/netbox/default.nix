18dfbfa76808b59aec0e602f26cbbf2f3cfb164e
Jonathan Davies <jpds@protonmail.com>
2024-04-05 15:43:41 +0100

    netbox: 3.7.4 → 3.7.6

--- pkgs/servers/web-apps/netbox/default.nix
+++ pkgs/servers/web-apps/netbox/default.nix
@@ -22,8 +22,8 @@ lib.fix (self: {
   };
 
   netbox_3_7 = callPackage generic {
-    version = "3.7.4";
-    hash = "sha256-jQJYtu1K13AsFXx4GTgtMOdppK1Tgoaf2/fKKhS1sQo=";
+    version = "3.7.6";
+    hash = "sha256-tC9x4Oy5ZSD3iuoWwmV/qnRgQHDuCbFO7nttbIT+p3U=";
     extraPatches = [
       # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
       ./config.patch
