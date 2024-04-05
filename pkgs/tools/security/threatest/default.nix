a4784d78f0dea0b2a99104f15b77407321bb242b
Yongun Seong <nevivurn@nevi.dev>
2024-04-06 06:07:19 +0900

    threatest: fix vendorHash for go1.22

--- pkgs/tools/security/threatest/default.nix
+++ pkgs/tools/security/threatest/default.nix
@@ -16,7 +16,7 @@ buildGoModule rec {
   };
 
   proxyVendor = true;
-  vendorHash = "sha256-zwHcGy7wjy2yx7nMi88R+z+Is+YcqGRMK0czeBNlcdA=";
+  vendorHash = "sha256-EvVazz51sW8z+8XfZB0Xo42KuUT6Q9n2Y/0HvlF1bV4=";
 
   nativeBuildInputs = [
     installShellFiles
