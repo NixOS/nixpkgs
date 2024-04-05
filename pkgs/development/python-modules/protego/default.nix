ee4e254e140d56fd19c28b9ad1ddf9552e42f9b1
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-05 16:38:09 +0200

    python312Packages.protego: refactor

--- pkgs/development/python-modules/protego/default.nix
+++ pkgs/development/python-modules/protego/default.nix
@@ -3,12 +3,13 @@
 , fetchPypi
 , pytestCheckHook
 , pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "protego";
   version = "0.3.1";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
@@ -18,15 +19,23 @@ buildPythonPackage rec {
     hash = "sha256-6UQw0NJcu/I5vISdhsXlRPveUx/Mz6BZlTx9o0ShcSw=";
   };
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  nativeBuildInputs = [
+    setuptools
+  ];
 
-  pythonImportsCheck = [ "protego" ];
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "protego"
+  ];
 
   meta = with lib; {
-    description = "A pure-Python robots.txt parser with support for modern conventions";
+    description = "Module to parse robots.txt files with support for modern conventions";
     homepage = "https://github.com/scrapy/protego";
     changelog = "https://github.com/scrapy/protego/blob/${version}/CHANGELOG.rst";
     license = licenses.bsd3;
-    maintainers = [ maintainers.marsam ];
+    maintainers = with maintainers; [ marsam ];
   };
 }
