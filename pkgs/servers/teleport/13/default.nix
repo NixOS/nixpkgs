2f701ec9e82af862a672737d13ebea7e84f25956
Yongun Seong <nevivurn@nevi.dev>
2024-04-06 06:05:59 +0900

    teleport_13: fix vendorHash for go1.22

--- pkgs/servers/teleport/13/default.nix
+++ pkgs/servers/teleport/13/default.nix
@@ -2,7 +2,7 @@
 callPackage ../generic.nix ({
   version = "13.4.14";
   hash = "sha256-g11D5lekI3pUpKf5CLUuNjejs0gN/bEemHkCj3akha0=";
-  vendorHash = "sha256-wQywm41qnv/ryZwwyIg+La1Z7qAw2I/fUI3kLgHlq9Q=";
+  vendorHash = "sha256-kiDhlR/P81u/yNq72JuskES/UzMrTFzJT0H3xldGk8I=";
   yarnHash = "sha256-E9T+7aXVoERdUnVEL4va2fcMnv1jsL9Js/R2LZo4hu4=";
   cargoLock = {
     lockFile = ./Cargo.lock;
