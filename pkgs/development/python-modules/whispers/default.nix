ab15a7f31860572b2fcc067f41e8a8ebccf4376d
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-06 15:39:50 +0200

    python312Packages.whispers: 2.2.0 -> 2.2.1
    Changelog: https://github.com/adeptex/whispers/releases/tag/2.2.1

--- pkgs/development/python-modules/whispers/default.nix
+++ pkgs/development/python-modules/whispers/default.nix
@@ -4,7 +4,6 @@
 , buildPythonPackage
 , crossplane
 , fetchFromGitHub
-, fetchpatch
 , jellyfish
 , jproperties
 , luhn
@@ -18,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "whispers";
-  version = "2.2.0";
+  version = "2.2.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -27,39 +26,19 @@ buildPythonPackage rec {
     owner = "adeptex";
     repo = "whispers";
     rev = "refs/tags/${version}";
-    hash = "sha256-9vXku8BWJtlf+lmAcQ8a7qTisRNc+xVw0T0Eunc4lt4=";
+    hash = "sha256-YPfTe2txQY/sVEIGSMTA1b3Ye3j8qT+YdWp3AVqpdLI=";
   };
-
-  patches = [
-    # Support astroid > 3, https://github.com/adeptex/whispers/pull/117
-    (fetchpatch {
-      url = "https://github.com/adeptex/whispers/commit/ff25e81cb3d775e5fb186c2d135b77c27d9ed43a.patch";
-      hash = "sha256-jKm7fs04mGUD7MZYAA/3xt01e9knuLun3c3u8PlLebg=";
-    })
-    (fetchpatch {
-      url = "https://github.com/adeptex/whispers/commit/ba6a56dddb12d1cb62f94dd7659ba24fdc4363ee.patch";
-      hash = "sha256-eHWnXHT0lzS7BqneMqfvV3w6GfrCiTJ5i+av82J+fpk=";
-    })
-    (fetchpatch {
-      url = "https://github.com/adeptex/whispers/commit/8b7b1593eb86abfc09b3581d463fc7d0e06309dc.patch";
-      hash = "sha256-JcRdv5eIyXKWaVqbJZlYqiSieE4z0MKF4dvO/hRBBMs=";
-    })
-    (fetchpatch {
-      url = "https://github.com/adeptex/whispers/commit/71dcb614e4d9e0247afc50cd4214659739f8844e.patch";
-      hash = "sha256-7XIFuc8Rf2ValN3BoAJOjSqjgmiOauxCFonMgGljFg0=";
-    })
-  ];
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace '"pytest-runner"' ""
+      --replace-fail '"pytest-runner"' ""
   '';
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     astroid
     beautifulsoup4
     crossplane
@@ -86,10 +65,10 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Tool to identify hardcoded secrets in static structured text";
-    mainProgram = "whispers";
     homepage = "https://github.com/adeptex/whispers";
     changelog = "https://github.com/adeptex/whispers/releases/tag/${version}";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ fab ];
+    mainProgram = "whispers";
   };
 }
