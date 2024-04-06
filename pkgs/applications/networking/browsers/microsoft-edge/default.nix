e3ef180e6d2fdd89f18e026f5adbdfa67d497c44
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-06 15:21:27 +0000

    microsoft-edge: 123.0.2420.53 -> 123.0.2420.81

--- pkgs/applications/networking/browsers/microsoft-edge/default.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/default.nix
@@ -1,20 +1,20 @@
 {
   beta = import ./browser.nix {
     channel = "beta";
-    version = "123.0.2420.53";
+    version = "124.0.2478.19";
     revision = "1";
-    hash = "sha256-6mE/zxVvGYrI7Emk5RBW+GC5W1FbVPFUeKMjev1yeFQ=";
+    hash = "sha256-+CanF7AadFQJj3t8OnZyoxPG2f2KO2e+EVBofKG3slg=";
   };
   dev = import ./browser.nix {
     channel = "dev";
-    version = "124.0.2464.2";
+    version = "125.0.2492.1";
     revision = "1";
-    hash = "sha256-vNvSzoVSVewTbKrnE6f+0Hx/1N5gOvRcdRGsmunBJHA=";
+    hash = "sha256-S6DfXJfxR8FsHyRtCcvUialaVYP/1rPivjRVSm9XAtg=";
   };
   stable = import ./browser.nix {
     channel = "stable";
-    version = "123.0.2420.53";
+    version = "123.0.2420.81";
     revision = "1";
-    hash = "sha256-7C6wZCIRodqWKimbnUl32TOhizsiE3U/be3tlpSNtt0=";
+    hash = "sha256-3c4DHs0p2YDW17nzCXB+O6PR9wTMb9h98EvN11imvsM=";
   };
 }
