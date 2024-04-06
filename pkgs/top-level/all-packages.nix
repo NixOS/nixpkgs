3b9574b5573ed4fc0648256e183d5be9ad23080f
Jan Tojnar <jtojnar@gmail.com>
2024-04-07 00:52:31 +0200

    bustle: remove
    The Haskell version is abandoned and no longer compiles.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4464,8 +4464,6 @@ with pkgs;
 
   bundletool = callPackage ../development/tools/bundletool { };
 
-  bustle = haskellPackages.bustle;
-
   bwm_ng = callPackage ../tools/networking/bwm-ng { };
 
   bwbasic = callPackage ../development/interpreters/bwbasic { };
