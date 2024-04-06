529fea12fd3bcc97e0fe4f163e89e2d6c32598a7
Nicolas Mémeint <git@nicomem.com>
2024-04-06 13:28:03 +0200

    nixos/authelia: Remove options incompatible with new settings
    - Remove settings.server.{host,port} options
      - Replaced by settings.server.address
    
    - Change what secrets.oidcIssuerPrivateKeyFile maps to
      - Previously: AUTHELIA_IDENTITY_PROVIDERS_OIDC_ISSUER_PRIVATE_KEY_FILE
      - Now: identity_providers.oidc.jwks[0].key
        - Not done directly in the NixOS settings config but as a separate
          YAML config file
        - Done that way because Go templates are not correctly handled by
          the YAML generator (#319716)
    
    - Change secrets.jwtSecretFile env variable mapping
      - Previously: AUTHELIA_JWT_SECRET_FILE
      - Now: AUTHELIA_IDENTITY_VALIDATION_RESET_PASSWORD_JWT_SECRET_FILE

--- nixos/modules/services/security/authelia.nix
+++ nixos/modules/services/security/authelia.nix
@@ -8,7 +8,6 @@ let
   cfg = config.services.authelia;
 
   format = pkgs.formats.yaml { };
-  configFile = format.generate "config.yml" cfg.settings;
 
   autheliaOpts = with lib; { name, ... }: {
     options = {
@@ -156,17 +155,11 @@ let
             };
 
             server = {
-              host = mkOption {
+              address = mkOption {
                 type = types.str;
-                default = "localhost";
-                example = "0.0.0.0";
+                default = "tcp://:9091/";
+                example = "unix:///var/run/authelia.sock";
                 description = "The address to listen on.";
-              };
-
-              port = mkOption {
-                type = types.port;
-                default = 9091;
-                description = "The port to listen on.";
               };
             };
 
@@ -233,6 +226,13 @@ let
       };
     };
   };
+
+  writeOidcJwksConfigFile = oidcIssuerPrivateKeyFile: pkgs.writeText "oidc-jwks.yaml" ''
+    identity_providers:
+      oidc:
+        jwks:
+          - key: {{ secret "${oidcIssuerPrivateKeyFile}" | mindent 10 "|" | msquote }}
+  '';
 in
 {
   options.services.authelia.instances = with lib; mkOption {
@@ -283,7 +283,8 @@ in
         let
           execCommand = "${instance.package}/bin/authelia";
           configFile = format.generate "config.yml" instance.settings;
-          configArg = "--config ${builtins.concatStringsSep "," (lib.concatLists [[configFile] instance.settingsFiles])}";
+          oidcJwksConfigFile = lib.optional (instance.secrets.oidcIssuerPrivateKeyFile != null) (writeOidcJwksConfigFile instance.secrets.oidcIssuerPrivateKeyFile);
+          configArg = "--config ${builtins.concatStringsSep "," (lib.concatLists [[configFile] instance.settingsFiles oidcJwksConfigFile])}";
         in
         {
           description = "Authelia authentication and authorization server";
@@ -291,10 +292,10 @@ in
           after = [ "network.target" ];
           environment =
             (lib.filterAttrs (_: v: v != null) {
-              AUTHELIA_JWT_SECRET_FILE = instance.secrets.jwtSecretFile;
+              X_AUTHELIA_CONFIG_FILTERS = lib.mkIf (oidcJwksConfigFile != [ ]) "template";
+              AUTHELIA_IDENTITY_VALIDATION_RESET_PASSWORD_JWT_SECRET_FILE = instance.secrets.jwtSecretFile;
               AUTHELIA_STORAGE_ENCRYPTION_KEY_FILE = instance.secrets.storageEncryptionKeyFile;
               AUTHELIA_SESSION_SECRET_FILE = instance.secrets.sessionSecretFile;
-              AUTHELIA_IDENTITY_PROVIDERS_OIDC_ISSUER_PRIVATE_KEY_FILE = instance.secrets.oidcIssuerPrivateKeyFile;
               AUTHELIA_IDENTITY_PROVIDERS_OIDC_HMAC_SECRET_FILE = instance.secrets.oidcHmacSecretFile;
             })
             // instance.environmentVariables;
