0f27c8a4ac5ce085fb1989dd0ea421b758ff8b9b
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-06 10:25:24 +0200

    python312Packages.semver: refactor

--- pkgs/development/python-modules/semver/default.nix
+++ pkgs/development/python-modules/semver/default.nix
@@ -10,7 +10,7 @@
 buildPythonPackage rec {
   pname = "semver";
   version = "3.0.2";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
@@ -21,7 +21,12 @@ buildPythonPackage rec {
     hash = "sha256-772PSUq1dqtn9aOol+Bo0S0OItBmoiCNP8q/YCBvKU4=";
   };
 
-  nativeBuildInputs = [
+  postPatch = ''
+    sed -i "/--cov/d" setup.cfg
+    sed -i "/--no-cov-on-fail/d" setup.cfg
+  '';
+
+  build-system = [
     setuptools
     setuptools-scm
   ];
@@ -29,11 +34,6 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     pytestCheckHook
   ];
-
-  postPatch = ''
-    sed -i "/--cov/d" setup.cfg
-    sed -i "/--no-cov-on-fail/d" setup.cfg
-  '';
 
   disabledTestPaths = [
     # Don't test the documentation
@@ -46,10 +46,10 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Python package to work with Semantic Versioning (http://semver.org/)";
-    mainProgram = "pysemver";
     homepage = "https://python-semver.readthedocs.io/";
     changelog = "https://github.com/python-semver/python-semver/releases/tag/3.0.0";
     license = licenses.bsd3;
     maintainers = with maintainers; [ np ];
+    mainProgram = "pysemver";
   };
 }
