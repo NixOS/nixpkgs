d41fd0203b463fadca1319aa137260020a52d4e8
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-06 23:09:13 +0000

    check-jsonschema: 0.28.0 -> 0.28.1

--- pkgs/development/tools/check-jsonschema/default.nix
+++ pkgs/development/tools/check-jsonschema/default.nix
@@ -4,7 +4,7 @@ with python3.pkgs;
 
 buildPythonApplication rec {
   pname = "check-jsonschema";
-  version = "0.28.0";
+  version = "0.28.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -13,7 +13,7 @@ buildPythonApplication rec {
     owner = "python-jsonschema";
     repo = "check-jsonschema";
     rev = "refs/tags/${version}";
-    hash = "sha256-qcY846y8xLEsPfdtzoOfxo5gdggH6Dn3QkQOY7kMwm0=";
+    hash = "sha256-JUb6jrbO1dylqOiDUDgYpMsXtMw7vErw6NJNyvfJIoM=";
   };
 
   propagatedBuildInputs = [
