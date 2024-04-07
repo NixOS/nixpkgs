c5071f55de3f290af797bea98216d8be12e1934f
Lukas Lihotzki <lukas@lihotzki.de>
2024-04-08 01:17:17 +0200

    asahi-nvram: init at 0.2.1

---
+++ pkgs/by-name/as/asahi-nvram/package.nix
@@ -0,0 +1,26 @@
+{ lib
+, fetchCrate
+, rustPlatform
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "asahi-nvram";
+  version = "0.2.1";
+
+  src = fetchCrate {
+    inherit pname version;
+    hash = "sha256-bFUFjHVTYj0eUmhijraOdeCvAt2UGX8+yyvooYN1Uo0=";
+  };
+
+  cargoHash = "sha256-WhySIQew8xxdwXLWkpvTYQZFiqCEPjEAjr7NVxfjDkU=";
+  cargoDepsName = pname;
+
+  meta = with lib; {
+    description = "A tool to read and write nvram variables on ARM Macs";
+    homepage = "https://crates.io/crates/asahi-nvram";
+    license = licenses.mit;
+    maintainers = with maintainers; [ lukaslihotzki ];
+    mainProgram = "asahi-nvram";
+    platforms = platforms.linux;
+  };
+}
