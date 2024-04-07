b8898ff837e84efe1752239399cedc3a18269bbf
Patka <patka@patka.dev>
2024-04-07 19:30:56 +0200

    phpunit: 11.1.0 -> 11.1.1
    diff: https://github.com/sebastianbergmann/phpunit/compare/11.1.0...11.1.1

--- pkgs/by-name/ph/phpunit/package.nix
+++ pkgs/by-name/ph/phpunit/package.nix
@@ -5,13 +5,13 @@
 
 php.buildComposerProject (finalAttrs: {
   pname = "phpunit";
-  version = "11.1.0";
+  version = "11.1.1";
 
   src = fetchFromGitHub {
     owner = "sebastianbergmann";
     repo = "phpunit";
     rev = finalAttrs.version;
-    hash = "sha256-shmKMsfAm64EJr882lP5P4Uy1I97W/Anawj+dSaxnDo=";
+    hash = "sha256-aS2mQeE8WnEaTexl8qhhfGyi1MP48s6fjrTXkVLq1LU=";
   };
 
   vendorHash = "sha256-kjMJCrMG08AXX662GAR5+V6w1+WOv8F9r6ONIOowP8Q=";
