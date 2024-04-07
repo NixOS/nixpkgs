9d27034ebaef4dcca1cff0d966703cfbe38d65d7
Mario Rodas <marsam@users.noreply.github.com>
2024-04-07 04:20:00 +0000

    nixos/fzf: fix fish shell commmand

--- nixos/modules/programs/fzf.nix
+++ nixos/modules/programs/fzf.nix
@@ -9,11 +9,11 @@ in
 {
   imports = [
     (lib.mkRemovedOptionModule [ "programs" "fzf" "keybindings" ] ''
-      Use "programs.fzf.enabled" instead, due to fzf upstream-change it's not possible to load shell-completion and keybindings seperatly.
+      Use "programs.fzf.enabled" instead, due to fzf upstream-change it's not possible to load shell-completion and keybindings separately.
       If you want to change/disable certain keybindings please check the fzf-documentation.
     '')
     (lib.mkRemovedOptionModule [ "programs" "fzf" "fuzzyCompletion" ] ''
-      Use "programs.fzf.enabled" instead, due to fzf upstream-change it's not possible to load shell-completion and keybindings seperatly.
+      Use "programs.fzf.enabled" instead, due to fzf upstream-change it's not possible to load shell-completion and keybindings separately.
       If you want to change/disable certain keybindings please check the fzf-documentation.
     '')
   ];
@@ -30,7 +30,7 @@ in
     '';
 
     programs.fish.interactiveShellInit = ''
-      eval "$(${getExe pkgs.fzf} --fish)"
+      ${getExe pkgs.fzf} --fish | source
     '';
 
     programs.zsh = {
