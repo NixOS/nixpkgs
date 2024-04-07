2942a15fdc034939db09fd72a23a0a024c276d74
Robert Schütz <nix@dotlambda.de>
2024-04-07 16:06:27 -0700

    rapidfuzz-cpp: 3.0.3 -> 3.0.4
    Diff: https://github.com/rapidfuzz/rapidfuzz-cpp/compare/v3.0.3...v3.0.4
    
    Changelog: https://github.com/rapidfuzz/rapidfuzz-cpp/blob/v3.0.4/CHANGELOG.md

--- pkgs/development/libraries/rapidfuzz-cpp/default.nix
+++ pkgs/development/libraries/rapidfuzz-cpp/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "rapidfuzz-cpp";
-  version = "3.0.3";
+  version = "3.0.4";
 
   src = fetchFromGitHub {
     owner = "rapidfuzz";
     repo = "rapidfuzz-cpp";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-6Df0X/gkS0i1RWv1uh0Hcn/lrLlIHeRp/+vfUmQ2EmI=";
+    hash = "sha256-urMdK+6ORHRgisppb700jaQpxLXAvXVjd8WDN7Zky3A=";
   };
 
   nativeBuildInputs = [
