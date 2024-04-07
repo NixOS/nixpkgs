b39dc0999f51ae8b65329225bf9040e7c1eb0d19
Mario Rodas <marsam@users.noreply.github.com>
2024-04-07 04:20:00 +0000

    passt: 0.2023_11_10.5ec3634 -> 2024_04_05.954589b

--- pkgs/by-name/pa/passt/package.nix
+++ pkgs/by-name/pa/passt/package.nix
@@ -1,27 +1,37 @@
-{ stdenv, lib, fetchgit }:
+{ lib
+, stdenv
+, fetchurl
+, gitUpdater
+, testers
+}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "passt";
-  version = "0.2023_11_10.5ec3634";
-  src = fetchgit {
-    url = "git://passt.top/passt";
-    rev = "5ec3634b07215337c2e69d88f9b1d74711897d7d";
-    hash = "sha256-76CD9PYD/NcBkmRYFSZaYl381QJjuWo0VsNdh31d6/M=";
+  version = "2024_04_05.954589b";
+
+  src = fetchurl {
+    url = "https://passt.top/passt/snapshot/passt-${finalAttrs.version}.tar.gz";
+    hash = "sha256-lwiHjr7rv1xBwXktKAsYIjDzNDkHGjZsW/FsQyFbLng=";
   };
-  nativeBuildInputs = [ ];
-  buildInputs = [];
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin $out/share/man/man1
-    cp passt pasta qrap $out/bin/
-    cp passt.1 pasta.1 qrap.1 $out/share/man/man1/
-  '' + (lib.optionalString stdenv.hostPlatform.avx2Support ''
-    cp passt.avx2 pasta.avx2 $out/bin/
-    runHook postInstall
-  '');
+
+  makeFlags = [
+    "prefix=${placeholder "out"}"
+    "VERSION=${finalAttrs.version}"
+  ];
+
+  passthru = {
+    tests.version = testers.testVersion {
+      package = finalAttrs.finalPackage;
+    };
+
+    updateScript = gitUpdater {
+      url = "https://passt.top/passt";
+    };
+  };
+
   meta = with lib; {
     homepage = "https://passt.top/passt/about/";
-    description = "Translation layer between a Layer-2 network interface and native Layer-4 sockets";
+    description = "Plug A Simple Socket Transport";
     longDescription = ''
       passt implements a translation layer between a Layer-2 network interface
       and native Layer-4 sockets (TCP, UDP, ICMP/ICMPv6 echo) on a host.
@@ -34,8 +44,9 @@ stdenv.mkDerivation {
       interfaces on the host, hence not requiring any capabilities or
       privileges.
     '';
-    license = lib.licenses.gpl2Plus;
+    license = [ licenses.bsd3 /* and */ licenses.gpl2Plus ];
     platforms = platforms.linux;
     maintainers = with maintainers; [ _8aed ];
+    mainProgram = "passt";
   };
-}
+})
