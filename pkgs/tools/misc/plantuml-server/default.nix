fe7b45bedf903f1ad2e843c400e3b8033775f682
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-07 01:44:15 +0000

    plantuml-server: 1.2024.3 -> 1.2024.4

--- pkgs/tools/misc/plantuml-server/default.nix
+++ pkgs/tools/misc/plantuml-server/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, fetchurl, nixosTests }:
 
 let
-  version = "1.2024.3";
+  version = "1.2024.4";
 in
 stdenv.mkDerivation rec {
   pname = "plantuml-server";
   inherit version;
   src = fetchurl {
     url = "https://github.com/plantuml/plantuml-server/releases/download/v${version}/plantuml-v${version}.war";
-    sha256 = "sha256-tuRtQl6tbjrgew6s6OV4EeY+GUo3DPVJJyuoD2vfnoo=";
+    sha256 = "sha256-7m0MOP6AN7V7mlrVfwxGy1AfCQx2ufp5GU2WQoSTIBc=";
   };
 
   dontUnpack = true;
