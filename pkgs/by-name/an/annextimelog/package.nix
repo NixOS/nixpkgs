6a289419cc3bf68dd05a4b99aa8371d7182899ae
Matthias Beyer <mail@beyermatthias.de>
2024-04-07 09:19:40 +0200

    annextimelog: 0.5.0 -> 0.13.1
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/by-name/an/annextimelog/package.nix
+++ pkgs/by-name/an/annextimelog/package.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "annextimelog";
-  version = "0.5.0";
+  version = "0.13.1";
   format = "pyproject";
 
   src = fetchFromGitLab {
     owner = "nobodyinperson";
     repo = "annextimelog";
     rev = "v${version}";
-    hash = "sha256-Q87SmzGaZ2rx+tsOgJIdxHaXK3Kda3jCkgH5y4wvLKE=";
+    hash = "sha256-VgeILw8WfqVrmsU/kBw+jHTOt2a6sVT7YgP2pKRp2AY=";
   };
 
   nativeBuildInputs = with python3.pkgs; [
