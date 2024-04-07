9d79a8d7b04573ff0a07201ff130a38b31f387b8
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-07 10:51:52 +0000

    python311Packages.ipyparallel: 8.7.0 -> 8.8.0

--- pkgs/development/python-modules/ipyparallel/default.nix
+++ pkgs/development/python-modules/ipyparallel/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "ipyparallel";
-  version = "8.7.0";
+  version = "8.8.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-QDwJca5Wwrajn/6PNfMcf9FwzhJ5JGKUWc3X96C058M=";
+    hash = "sha256-JATVn4ajqqO9J79rV993e/9cE2PBxuYEA3WdFu1C3Hs=";
   };
 
   # We do not need the jupyterlab build dependency, because we do not need to
