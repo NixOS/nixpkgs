89db988ed2399c9970dffae03e50fa9f13e9690a
linsui <linsui@inbox.lv>
2024-04-08 20:41:09 +0800

    nixos/lazygit: init
    Update nixos/modules/programs/lazygit.nix
    
    Co-authored-by: Aleksana <alexander.huang.y@gmail.com>
    
    Update nixos/modules/programs/lazygit.nix
    
    Co-authored-by: Aleksana <alexander.huang.y@gmail.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -212,6 +212,7 @@
   ./programs/kbdlight.nix
   ./programs/kclock.nix
   ./programs/kdeconnect.nix
+  ./programs/lazygit.nix
   ./programs/less.nix
   ./programs/liboping.nix
   ./programs/light.nix
