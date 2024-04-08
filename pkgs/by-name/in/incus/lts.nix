c60f185ea0594b01219ebfe7cd0f8328113b3586
Adam Stephens <adam@valkor.net>
2024-04-08 14:18:02 -0400

    incus-lts: init at 6.0.0 and make default

--- pkgs/by-name/in/incus/lts.nix
+++ pkgs/by-name/in/incus/lts.nix
@@ -1,3 +1,8 @@
-# this release doesn't exist yet, but satisfay the by-name checks
-# will be added as incus-lts in all-packages.nix once ready
-import ./generic.nix { }
+import ./generic.nix {
+  hash = "sha256-+q5qP7w2RdtuwvxPThCryYYEJ7s5WDnWHRvjo4TuajA=";
+  version = "6.0.0";
+  vendorHash = "sha256-wcauzIbBcYpSWttZCVVE9m49AEQGolGYSsv9eEkhb7Y=";
+  patches = [ ];
+  lts = true;
+  updateScriptArgs = "--lts=true --regex '6.0.*'";
+}
