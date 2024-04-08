2ad0f8ca0f32b4f8e32eb37d6f444fce6d5c54f7
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-08 08:33:11 +0000

    eigenlayer: 0.6.3 -> 0.7.0

--- pkgs/by-name/ei/eigenlayer/package.nix
+++ pkgs/by-name/ei/eigenlayer/package.nix
@@ -6,16 +6,16 @@
 }:
 buildGoModule rec {
   pname = "eigenlayer";
-  version = "0.6.3";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "Layr-Labs";
     repo = "eigenlayer-cli";
     rev = "v${version}";
-    hash = "sha256-v2d6+jxEYXgJjZOFRc+teQWq161v4FLO+KjYq96XwTs=";
+    hash = "sha256-4KN5AletaQ/Xpr/mCLgB/ceVbqMzWoi7bKoWRDePnuc=";
   };
 
-  vendorHash = "sha256-pJYs9jD3EmmXlkPO6gkuB2FYsgX9Yn6S6QYb2dQ1fY4=";
+  vendorHash = "sha256-6RF32eWU7HS9C3T7hDVHjWxX/3Ch/ixOGHml30Q9PaU=";
 
   ldflags = ["-s" "-w"];
   subPackages = ["cmd/eigenlayer"];
