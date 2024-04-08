NixOS/nixpkgs

commit fc060cc3cb1ceb1825cc015abd16dc0fce326836

nixos/resolvconf: add `::1` as local dns resolver

nixos/modules/config/resolvconf.nix
@@ -28,6 +28,8 @@ let
     '' + optionalString cfg.useLocalResolver ''
       # This hosts runs a full-blown DNS resolver.
       name_servers='127.0.0.1'
+    '' + optionalString (cfg.useLocalResolver && config.networking.enableIPv6) ''
+      name_servers='::1'
     '' + cfg.extraConfig;
 
 in

commit eb91bc05bd9ded4807a42255820a03b61d1f3f21

nixos/resolvconf: fix useLocalResolver when IPv6 is enabled
resolvconf.conf is a shell script and unlike resolv.conf, multiple
nameservers are specified by space separating them instead of adding
multiple lines.

Fixes: fc060cc3cb1ceb1825cc015abd16dc0fce326836

nixos/modules/config/resolvconf.nix
@@ -27,9 +27,7 @@ let
       resolv_conf_options='${concatStringsSep " " resolvconfOptions}'
     '' + optionalString cfg.useLocalResolver ''
       # This hosts runs a full-blown DNS resolver.
-      name_servers='127.0.0.1'
-    '' + optionalString (cfg.useLocalResolver && config.networking.enableIPv6) ''
-      name_servers='::1'
+      name_servers='127.0.0.1${optionalString config.networking.enableIPv6 " ::1"}'
     '' + cfg.extraConfig;
 
 in