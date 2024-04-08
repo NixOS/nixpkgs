b4e0f97395fc69624e014f29f9a7c33e7756d47a
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-08 14:57:36 +0000

    sngrep: 1.8.0 -> 1.8.1

--- pkgs/applications/networking/sniffers/sngrep/default.nix
+++ pkgs/applications/networking/sniffers/sngrep/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "sngrep";
-  version = "1.8.0";
+  version = "1.8.1";
 
   src = fetchFromGitHub {
     owner = "irontec";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-9ccp5Pxhs7jOQuWHCmU9yvzLKeOAN8lEaieCIvnXJRA=";
+    sha256 = "sha256-EbB5Ok/8RUoNzkgoWIhHTRvlq9Vv2KVx6Hu8ejrLkMc=";
   };
 
   nativeBuildInputs = [
