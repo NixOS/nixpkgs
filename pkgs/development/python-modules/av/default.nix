dd10886d520e2f0067d4d99001dad94699b536c2
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-08 20:33:46 +0200

    python312Packages.av: format with nixfmt

--- pkgs/development/python-modules/av/default.nix
+++ pkgs/development/python-modules/av/default.nix
@@ -1,15 +1,16 @@
-{ lib
-, stdenv
-, buildPythonPackage
-, cython
-, fetchFromGitHub
-, ffmpeg_5-headless
-, numpy
-, pillow
-, pkg-config
-, pytestCheckHook
-, pythonOlder
-, setuptools
+{
+  lib,
+  stdenv,
+  buildPythonPackage,
+  cython,
+  fetchFromGitHub,
+  ffmpeg_5-headless,
+  numpy,
+  pillow,
+  pkg-config,
+  pytestCheckHook,
+  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
@@ -26,18 +27,14 @@ buildPythonPackage rec {
     hash = "sha256-+xbVkNyFg5VKIf+G6AbAAYVqTSwxFE0Hyc52XSmibAw=";
   };
 
-  build-system = [
-    setuptools
-  ];
+  build-system = [ setuptools ];
 
   nativeBuildInputs = [
     cython
     pkg-config
   ];
 
-  buildInputs = [
-    ffmpeg_5-headless
-  ];
+  buildInputs = [ ffmpeg_5-headless ];
 
   preCheck = ''
     # ensure we import the built version
@@ -50,71 +47,73 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  disabledTests = [
-    # urlopen fails during DNS resolution
-    "test_writing_to_custom_io"
-    "test_decode_close_then_use"
-    "test_bits_per_coded_sample"
-    "test_codec_delay"
-    "test_frame_index"
-    "test_penguin_joke"
-    "test_sky_timelapse"
-    "test_flush_decoded_video_frame_count"
-    "test_path_input"
-    "test_path_output"
-    "test_str_input"
-    "test_str_output"
-    "test_is_corrupt"
-    "test_is_discard"
-    "test_is_disposable"
-    "test_is_keyframe"
-    "test_noside_data"
-    "test_side_data"
-    # Tests that want to download FATE data, https://github.com/PyAV-Org/PyAV/issues/955
-    "test_vobsub"
-    "test_transcode"
-    "test_stream_tuples"
-    "test_stream_seek"
-    "test_stream_probing"
-    "test_seek_start"
-    "test_seek_middle"
-    "test_seek_int64"
-    "test_seek_float"
-    "test_seek_end"
-    "test_roundtrip"
-    "test_reading_from_write_readonl"
-    "test_reading_from_pipe_readonly"
-    "test_reading_from_file"
-    "test_reading_from_buffer"
-    "test_reading_from_buffer_no_see"
-    "test_parse"
-    "test_movtext"
-    "test_encoding_xvid"
-    "test_encoding_tiff"
-    "test_encoding_png"
-    "test_encoding_pcm_s24le"
-    "test_encoding_mpeg4"
-    "test_encoding_mpeg1video"
-    "test_encoding_mp2"
-    "test_encoding_mjpeg"
-    "test_encoding_h264"
-    "test_encoding_dvvideo"
-    "test_encoding_dnxhd"
-    "test_encoding_aac"
-    "test_decoded_video_frame_count"
-    "test_decoded_time_base"
-    "test_decoded_motion_vectors"
-    "test_decode_half"
-    "test_decode_audio_sample_count"
-    "test_data"
-    "test_container_probing"
-    "test_codec_tag"
-    "test_selection"
-  ] ++ lib.optionals (stdenv.isDarwin) [
-    # Segmentation Faults
-    "test_encoding_with_pts"
-    "test_bayer_write"
-  ];
+  disabledTests =
+    [
+      # urlopen fails during DNS resolution
+      "test_writing_to_custom_io"
+      "test_decode_close_then_use"
+      "test_bits_per_coded_sample"
+      "test_codec_delay"
+      "test_frame_index"
+      "test_penguin_joke"
+      "test_sky_timelapse"
+      "test_flush_decoded_video_frame_count"
+      "test_path_input"
+      "test_path_output"
+      "test_str_input"
+      "test_str_output"
+      "test_is_corrupt"
+      "test_is_discard"
+      "test_is_disposable"
+      "test_is_keyframe"
+      "test_noside_data"
+      "test_side_data"
+      # Tests that want to download FATE data, https://github.com/PyAV-Org/PyAV/issues/955
+      "test_vobsub"
+      "test_transcode"
+      "test_stream_tuples"
+      "test_stream_seek"
+      "test_stream_probing"
+      "test_seek_start"
+      "test_seek_middle"
+      "test_seek_int64"
+      "test_seek_float"
+      "test_seek_end"
+      "test_roundtrip"
+      "test_reading_from_write_readonl"
+      "test_reading_from_pipe_readonly"
+      "test_reading_from_file"
+      "test_reading_from_buffer"
+      "test_reading_from_buffer_no_see"
+      "test_parse"
+      "test_movtext"
+      "test_encoding_xvid"
+      "test_encoding_tiff"
+      "test_encoding_png"
+      "test_encoding_pcm_s24le"
+      "test_encoding_mpeg4"
+      "test_encoding_mpeg1video"
+      "test_encoding_mp2"
+      "test_encoding_mjpeg"
+      "test_encoding_h264"
+      "test_encoding_dvvideo"
+      "test_encoding_dnxhd"
+      "test_encoding_aac"
+      "test_decoded_video_frame_count"
+      "test_decoded_time_base"
+      "test_decoded_motion_vectors"
+      "test_decode_half"
+      "test_decode_audio_sample_count"
+      "test_data"
+      "test_container_probing"
+      "test_codec_tag"
+      "test_selection"
+    ]
+    ++ lib.optionals (stdenv.isDarwin) [
+      # Segmentation Faults
+      "test_encoding_with_pts"
+      "test_bayer_write"
+    ];
 
   disabledTestPaths = [
     # urlopen fails during DNS resolution
