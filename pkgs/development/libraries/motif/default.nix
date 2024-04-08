NixOS/nixpkgs

commit 68c003b256ad6a5abff2e36ebcf0e96320bca416

motif: fixup build after automake update #28232
It seemed easiest to avoid the need to autoreconf.

pkgs/development/libraries/motif/default.nix
@@ -3,7 +3,7 @@
 , expat, libjpeg, libpng, libiconv
 , flex
 , libXp, libXau
-, demoSupport ? false, autoconf, automake
+, demoSupport ? false
 }:
 # refer to the gentoo package
 
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
     expat libjpeg libpng libiconv
   ];
 
-  nativeBuildInputs = [ pkgconfig flex ] ++ stdenv.lib.optionals (!demoSupport) [ autoconf automake ];
+  nativeBuildInputs = [ pkgconfig flex ];
 
   propagatedBuildInputs = [ libXp libXau ];
 
@@ -30,13 +30,16 @@ stdenv.mkDerivation rec {
 
   makeFlags = [ "CFLAGS=-fno-strict-aliasing" ];
 
-  prePatch = ''rm lib/Xm/Xm.h'';
+  prePatch = ''
+    rm lib/Xm/Xm.h
+  '' + stdenv.lib.optionalString (!demoSupport) ''
+    sed '/^SUBDIRS =,^$/s/\<demos\>//' -i Makefile.{am,in}
+  '';
 
   patches = [ ./Remove-unsupported-weak-refs-on-darwin.patch
               ./Use-correct-header-for-malloc.patch
               ./Add-X.Org-to-bindings-file.patch
-            ]
-            ++ stdenv.lib.optional (!demoSupport) ./Do-not-compile-demos.patch;
+            ];
 
   meta = with stdenv.lib; {
     homepage = http://motif.ics.com;

pkgs/development/libraries/motif/Do-not-compile-demos.patch
@@ -1,11 +1,0 @@
---- a/Makefile.am
-+++ b/Makefile.am
-@@ -27,7 +27,6 @@ SUBDIRS   = bindings bitmaps \
-              include \
-              tools \
-              clients \
--             doc \
--             demos
-+             doc
- AUTOMAKE_OPTIONS = 1.4
- ACLOCAL_AMFLAGS = -I .

commit 9cfffcd54a0a34889b2d2cc5d9263fb70c1b10ef

motif: fix disabling demos
The previous sed expression didn't work, even in the commit that added
it.

Fixes: 68c003b256ad ("motif: fixup build after automake update #28232")

pkgs/development/libraries/motif/default.nix
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   propagatedBuildInputs = [ libXp libXau ];
 
   prePatch = lib.optionalString (!demoSupport) ''
-    sed '/^SUBDIRS =,^$/s/\<demos\>//' -i Makefile.{am,in}
+    sed 's/\<demos\>//' -i Makefile.{am,in}
   '';
 
   patches = [