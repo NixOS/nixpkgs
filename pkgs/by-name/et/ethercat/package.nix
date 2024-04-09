7131e294e36e9cf17f2032eb1e4113b8568d6adc
Steffen Vogel <post@steffenvogel.de>
2024-04-10 00:39:36 +0200

    ethercat: package documentation

--- pkgs/by-name/et/ethercat/package.nix
+++ pkgs/by-name/et/ethercat/package.nix
@@ -7,6 +7,13 @@
   gitUpdater,
   pkg-config,
   systemdMinimal,
+  doxygen,
+  fig2dev,
+  python3,
+  inkscape,
+  graphviz,
+  texlive,
+  git,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "ethercat";
@@ -17,6 +24,8 @@ stdenv.mkDerivation (finalAttrs: {
     repo = "ethercat";
     rev = "refs/tags/${finalAttrs.version}";
     hash = "sha256-NgRyvNvHy04jr7ieOscBYULRdWJ7YuHbuYbRrSfXYRU=";
+    fetchSubmodules = true;
+    leaveDotGit = true;
   };
 
   separateDebugInfo = true;
@@ -31,6 +40,7 @@ stdenv.mkDerivation (finalAttrs: {
   outputs = [
     "bin"
     "dev"
+    "doc"
   ];
 
   configureFlags = [
@@ -57,6 +67,38 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   passthru.updateScript = gitUpdater { };
+
+  # See: https://gitlab.com/etherlab.org/ethercat/-/blob/6e8119b95563ba955954a68e5e2f4f3f861ac72e/.gitlab-ci.yml#L117
+  postPatch = ''
+    git show -s --format="\def\revision{%h}\def\gitversion{%(describe)}\def\gittag{%(describe:abbrev=0)}\def\gitauthor{%an}\def\isodate#1-#2-#3x{\day=#3 \month=#2 \year=#1}\isodate %csx" HEAD > documentation/git.tex
+  '';
+
+  postInstall = ''
+    mkdir -p $doc
+
+    echo "Build Doxygen docs"
+    make doc
+    cp -r doxygen-output/html $doc/html
+
+    echo "Build Doxygen LaTeX docs"
+    make -C doxygen-output/latex
+    cp -r doxygen-output/latex/refman.pdf $doc/ethercat_ref.pdf
+
+    echo "Build LateX manual"
+    mkdir -p documentation/external
+    make -C documentation
+    make -C documentation index
+    make -C documentation
+
+    cp documentation/*.pdf $doc
+  '';
+
+  postFixup = ''
+    mv $out/{share,lib,etc} $bin
+    find $out
+    echo
+    find $bin
+  '';
 
   meta = with lib; {
     description = "IgH EtherCAT Master for Linux";
