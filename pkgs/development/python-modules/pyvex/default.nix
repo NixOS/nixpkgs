5d8c2e7418ca0ace198157b6385773c2e380fb2d
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-09 22:57:37 +0200

    python311Packages.pyvex: 9.2.97 -> 9.2.98

--- pkgs/development/python-modules/pyvex/default.nix
+++ pkgs/development/python-modules/pyvex/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "pyvex";
-  version = "9.2.97";
+  version = "9.2.98";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-GIBsWtj8nLuzGX/C6Ioe8mSfKg3QXtEJf3+d5h9M02c=";
+    hash = "sha256-+X4G5jLG7c75XIItNlQLF5YV0XUQwdA6JyF1IGBfiE0=";
   };
 
   build-system = [
