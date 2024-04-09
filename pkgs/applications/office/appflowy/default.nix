2c70198b7593d3aee7ccfae84f4c45575513c3bc
DarkOnion0 <darkonion0@clapot.org>
2024-04-09 09:44:44 +0200

    appflowy: 0.5.3 -> 0.5.4
    https://github.com/AppFlowy-IO/AppFlowy/releases/tag/0.5.4

--- pkgs/applications/office/appflowy/default.nix
+++ pkgs/applications/office/appflowy/default.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "appflowy";
-  version = "0.5.3";
+  version = "0.5.4";
 
   src = fetchzip {
     url = "https://github.com/AppFlowy-IO/appflowy/releases/download/${version}/AppFlowy-${version}-linux-x86_64.tar.gz";
-    hash = "sha256-BFPtT8/DvSsZY1ckrXRZn6F0+pSRRZLoqc638JKUpjQ=";
+    hash = "sha256-gIM4CQvPkQIWvHXYPwr4yiDY1mUuOmubdqkOmHNssVo=";
     stripRoot = false;
   };
 
