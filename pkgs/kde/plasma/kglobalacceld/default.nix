1a3d1c6dad8386817b5972d412e3ef19d3d8d013
K900 <me@0upti.me>
2024-04-09 08:28:52 +0300

    Revert "kdePackages.kglobalacceld: hack around ksycoca breakage"

--- pkgs/kde/plasma/kglobalacceld/default.nix
+++ pkgs/kde/plasma/kglobalacceld/default.nix
@@ -1,9 +1,4 @@
 {mkKdeDerivation}:
 mkKdeDerivation {
   pname = "kglobalacceld";
-
-  # Don't delete shortcuts when applications disappear from sycoca,
-  # because for us they sometimes do.
-  # FIXME: make them actually not do that instead.
-  patches = [./dont-delete-shortcuts.patch];
 }
