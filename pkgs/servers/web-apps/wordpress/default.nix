b65b517c55f36820a9e0e89eb621199617f4652b
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-04-09 23:34:19 +0200

    wordpress: 6.4.3 -> 6.5

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -1,5 +1,5 @@
 { callPackage }: builtins.mapAttrs (_: callPackage ./generic.nix) rec {
-  wordpress = wordpress6_4;
+  wordpress = wordpress6_5;
   wordpress6_3 = {
     version = "6.3.2";
     hash = "sha256-Jo2/Vlm4Ml24ucPI6ZHs2mkbpY2rZB1dofmGXNPweA8=";
@@ -7,5 +7,9 @@
   wordpress6_4 = {
     version = "6.4.3";
     hash = "sha256-PwjHHRlwhEH9q94bPq34NnQv3uhm1kOpjRAu0/ECaYY=";
+  };
+  wordpress6_5 = {
+    version = "6.5";
+    hash = "sha256-NCRWKIrhSMC6itw2y/WJUd2LSRtnVWkmVO/b2nEJK58=";
   };
 }
