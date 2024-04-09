daaa9c3181764bda246a7d1d8a9e82cade0884a9
iko <mail@iko.soy>
2024-04-09 15:36:41 +0300

    pgroll: init at 0.5.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19425,6 +19425,8 @@ with pkgs;
 
   pgcli = with pkgs.python3Packages; toPythonApplication pgcli;
 
+  pgroll = callPackage ../development/tools/pgroll{ } ;
+
   picotool = callPackage ../development/tools/picotool { };
 
   planus = callPackage ../development/tools/misc/planus { };
