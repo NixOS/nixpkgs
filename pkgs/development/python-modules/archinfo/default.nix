3da2cf5201821196f1fa1ebfeb18794b9db80ea0
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-09 22:57:32 +0200

    python311Packages.archinfo: 9.2.97 -> 9.2.98
    Diff: https://github.com/angr/archinfo/compare/refs/tags/v9.2.97...v9.2.98

--- pkgs/development/python-modules/archinfo/default.nix
+++ pkgs/development/python-modules/archinfo/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "archinfo";
-  version = "9.2.97";
+  version = "9.2.98";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "archinfo";
     rev = "refs/tags/v${version}";
-    hash = "sha256-X8rMTQvNolYjSPyXbP2i5MYTPEvQlwoUQmXeEW56wQs=";
+    hash = "sha256-EdVGm4453XxGrq2D1v5JzSZyroRNzluZ5/r74YAt5zU=";
   };
 
   build-system = [
