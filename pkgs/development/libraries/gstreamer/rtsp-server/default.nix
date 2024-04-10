d39d5238f24efe0fb1fa988f8a51587492b7b9ee
Maxine Aubrey <max@ine.dev>
2024-04-10 23:27:25 +0200

    gst_all_1.gst-rtsp-server: 1.24.1 → 1.24.2

--- pkgs/development/libraries/gstreamer/rtsp-server/default.nix
+++ pkgs/development/libraries/gstreamer/rtsp-server/default.nix
@@ -15,11 +15,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gst-rtsp-server";
-  version = "1.24.1";
+  version = "1.24.2";
 
   src = fetchurl {
     url = "https://gstreamer.freedesktop.org/src/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-q0NqLsnDxY+/08Wxc2QZiRu1MV0TbXxyxYR58oBwCOY=";
+    hash = "sha256-5MhKeGdefv7zsm6cueLkJzGEIbStnuS1E2767rtrugw=";
   };
 
   outputs = [
