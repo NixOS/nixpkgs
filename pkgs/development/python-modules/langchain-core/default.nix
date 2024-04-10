a05aa9839e73ea6d9ce976b275755c170a48504a
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-10 23:35:17 +0200

    python312Packages.langchain-core: 0.1.40 -> 0.1.41

--- pkgs/development/python-modules/langchain-core/default.nix
+++ pkgs/development/python-modules/langchain-core/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "langchain-core";
-  version = "0.1.40";
+  version = "0.1.41";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "langchain_core";
     inherit version;
-    hash = "sha256-NMBvwObTU0tzjGP4VANEa0vnEWFmW34JH5uxnJFOwQA=";
+    hash = "sha256-QAIwSIOyUa+Mt4HwHFn1au7li+Qsll273soKO/uPlq8=";
   };
 
   pythonRelaxDeps = [
