b89e9c437e4715d1157cf988adfa35536e58b42e
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-10 13:15:34 +0000

    minder: 1.16.3 -> 1.16.4

--- pkgs/applications/misc/minder/default.nix
+++ pkgs/applications/misc/minder/default.nix
@@ -25,13 +25,13 @@
 
 stdenv.mkDerivation rec {
   pname = "minder";
-  version = "1.16.3";
+  version = "1.16.4";
 
   src = fetchFromGitHub {
     owner = "phase1geo";
     repo = pname;
     rev = version;
-    sha256 = "sha256-YDsWWC4exh+9V87WyjdMdCH1arjBct3oEXbvaFyAaMY=";
+    sha256 = "sha256-1r2PbBYw4mmiScKbX9BGqe4i+emSvismJfPm3opLlOg=";
   };
 
   nativeBuildInputs = [
