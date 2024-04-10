f73b2e3976296b7e1018df1210aaab83d6103266
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-10 12:29:31 +0200

    python312Packages.apsw: 3.45.1.0 -> 3.45.2.0
    Diff: rogerbinns/apsw@refs/tags/3.45.1.0...3.45.2.0
    
    Changelog: https://github.com/rogerbinns/apsw/releases/tag/3.45.2.0

--- pkgs/development/python-modules/apsw/default.nix
+++ pkgs/development/python-modules/apsw/default.nix
@@ -1,17 +1,19 @@
-{ lib
-, buildPythonPackage
-, fetchFromGitHub
-, sqlite
-, isPyPy
-, python
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  python,
+  pythonOlder,
+  setuptools,
+  sqlite,
 }:
 
 buildPythonPackage rec {
   pname = "apsw";
   version = "3.45.2.0";
-  format = "setuptools";
+  pyproject = true;
 
-  disabled = isPyPy;
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "rogerbinns";
@@ -20,9 +22,9 @@ buildPythonPackage rec {
     hash = "sha256-tTi3/10W4OoGH6PQVhvPWc5o09on5BZrWoAvrfh4C/E=";
   };
 
-  buildInputs = [
-    sqlite
-  ];
+  build-system = [ setuptools ];
+
+  buildInputs = [ sqlite ];
 
   # Project uses custom test setup to exclude some tests by default, so using pytest
   # requires more maintenance
@@ -31,13 +33,12 @@ buildPythonPackage rec {
     ${python.interpreter} setup.py test
   '';
 
-  pythonImportsCheck = [
-    "apsw"
-  ];
+  pythonImportsCheck = [ "apsw" ];
 
   meta = with lib; {
     description = "A Python wrapper for the SQLite embedded relational database engine";
     homepage = "https://github.com/rogerbinns/apsw";
+    changelog = "https://github.com/rogerbinns/apsw/releases/tag/${version}";
     license = licenses.zlib;
     maintainers = with maintainers; [ gador ];
   };
