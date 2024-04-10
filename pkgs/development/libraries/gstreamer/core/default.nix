2d474af3017952d6cc51e60fd7064ba495389d75
Maxine Aubrey <max@ine.dev>
2024-04-10 23:25:21 +0200

    gst_all_1.gstreamer: 1.24.1 → 1.24.2
    https://gstreamer.freedesktop.org/releases/1.24/#1.24.2
    https://gitlab.freedesktop.org/gstreamer/gstreamer/-/compare/1.24.1...1.24.2

--- pkgs/development/libraries/gstreamer/core/default.nix
+++ pkgs/development/libraries/gstreamer/core/default.nix
@@ -25,7 +25,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gstreamer";
-  version = "1.24.1";
+  version = "1.24.2";
 
   outputs = [
     "bin"
@@ -37,7 +37,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) pname version;
   in fetchurl {
     url = "https://gstreamer.freedesktop.org/src/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-iPw2KajKz3lV2GcbKuhn1KUqB7aUjaoltn/u0i+AVe8=";
+    hash = "sha256-nK/dI70YDxaBxWzTpoeahJfM8k2m9CKmtvNW+gdKhIE=";
   };
 
   depsBuildBuild = [
