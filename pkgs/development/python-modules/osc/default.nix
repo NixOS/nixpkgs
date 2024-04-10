6e7c708085871937991a481fe3dfe899e1063f2d
Sascha Grunert <sgrunert@redhat.com>
2024-04-10 13:00:09 +0200

    osc: 1.3.1 -> 1.6.1
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- pkgs/development/python-modules/osc/default.nix
+++ pkgs/development/python-modules/osc/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "osc";
-  version = "1.3.1";
+  version = "1.6.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "openSUSE";
     repo = "osc";
     rev = version;
-    sha256 = "sha256-pywSXGM3IX3cTr1uJIP7pNGIYE/skMIoJeoaMU75zwc=";
+    hash = "sha256-U76nAE7NdLIdrKzRC0sP8hy6G6A8Tr4Qe2PGZI2xvyk=";
   };
 
   buildInputs = [ bashInteractive ]; # needed for bash-completion helper
@@ -45,7 +45,7 @@ buildPythonPackage rec {
     homepage = "https://github.com/openSUSE/osc";
     description = "opensuse-commander with svn like handling";
     mainProgram = "osc";
-    maintainers = [ maintainers.peti ];
+    maintainers = with maintainers; [ peti saschagrunert ];
     license = licenses.gpl2;
   };
 
