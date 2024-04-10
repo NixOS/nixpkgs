fc0cccdbd5792d38b19c7ea191e7c3440def586d
dflores1 <david.flores@ucm.es>
2024-04-10 14:04:50 +0200

    python3Packages.spectra: init at 0.0.11
    python3Packages.spectra: removed wrong info from meta

---
+++ pkgs/development/python-modules/spectra/default.nix
@@ -0,0 +1,50 @@
+{ lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  colormath,
+  nose
+}:
+
+buildPythonPackage rec {
+  pname = "spectra";
+  version = "0.0.11";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "jsvine";
+    repo  = "spectra";
+    rev = "v${version}";
+    hash = "sha256-4A2TWTxYqckJ3DX5cd2KN3KXcmO/lQdXmOEnGi76RsA=";
+  };
+
+  dependencies = [ setuptools ];
+
+  buildInputs = [ colormath ];
+
+  nativeCheckInputs = [ nose ];
+  checkPhase = "nosetests";
+
+  meta = {
+    changelog = "https://github.com/jsvine/spectra/releases/tag/v${version}";
+    description = "Spectra is a Python library that makes color math, color scales, and color-space conversion easy";
+    longDescription = ''
+    Spectra has support for:
+
+    * Color scales
+    * Color ranges
+    * Color blending
+    * Brightening/darkening colors
+    * Saturating/desaturating colors
+    * Conversion to/from multiple color spaces
+
+    Spectra is built on colormath and grapefruit. Spectra is enormously inspired by chroma.js and d3's scales.
+    '';
+    homepage = "https://github.com/jsvine/spectra";
+    sourceProvenance = [ lib.sourceTypes.fromSource ];
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.dflores ];
+    platforms = lib.platforms.unix;
+  };
+
+}
