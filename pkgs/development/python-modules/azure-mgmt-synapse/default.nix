0a0a7a12b9d13dd6201f74d6559094debeeb8afe
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-10 09:59:48 +0200

    python312Packages.azure-mgmt-synapse: format with nixfmt

--- pkgs/development/python-modules/azure-mgmt-synapse/default.nix
+++ pkgs/development/python-modules/azure-mgmt-synapse/default.nix
@@ -1,12 +1,13 @@
-{ lib
-, azure-common
-, azure-mgmt-core
-, buildPythonPackage
-, fetchPypi
-, msrest
-, msrestazure
-, pythonOlder
-, setuptools
+{
+  lib,
+  azure-common,
+  azure-mgmt-core,
+  buildPythonPackage,
+  fetchPypi,
+  msrest,
+  msrestazure,
+  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
@@ -22,9 +23,7 @@ buildPythonPackage rec {
     extension = "zip";
   };
 
-  build-system = [
-    setuptools
-  ];
+  build-system = [ setuptools ];
 
   dependencies = [
     azure-common
