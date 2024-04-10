2281dbff390bdc44e35856d17c5f3c7f7ca67e78
Maxine Aubrey <max@ine.dev>
2024-04-10 23:27:25 +0200

    gst_all_1.gst-vaapi: 1.24.1 → 1.24.2

--- pkgs/development/libraries/gstreamer/vaapi/default.nix
+++ pkgs/development/libraries/gstreamer/vaapi/default.nix
@@ -24,11 +24,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gstreamer-vaapi";
-  version = "1.24.1";
+  version = "1.24.2";
 
   src = fetchurl {
     url = "https://gstreamer.freedesktop.org/src/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-yRSg9uu06W5rMyfwXYs/2bVFt2T7OV1GuP8zxW4ApHI=";
+    hash = "sha256-zFq4yIRD4PW/I9YRC0qsM99Z5K5ib1NtlosWBcx7li8=";
   };
 
   outputs = [
