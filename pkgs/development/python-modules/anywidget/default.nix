bd874a7d8d67be37f3c8ef223c2ebb7a5390b795
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-10 03:40:08 +0000

    python312Packages.anywidget: 0.9.4 -> 0.9.7

--- pkgs/development/python-modules/anywidget/default.nix
+++ pkgs/development/python-modules/anywidget/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "anywidget";
-  version = "0.9.4";
+  version = "0.9.7";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-97nCw1PohHKW2DtY6RARk1/RlMsc1s5wajuhbY3pQxo=";
+    hash = "sha256-mepiQbJVtIHgzEhnjy8MjBOEMYLvlpLJ/wzMSm3+2bE=";
   };
 
   # We do not need the jupyterlab build dependency, because we do not need to
