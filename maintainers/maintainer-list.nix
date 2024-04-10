35389ae987bf215b0f11005fe4536f9ae04c30ea
nyadiia <nyadiia@pm.me>
2024-04-10 16:15:06 -0500

    maintainers: add nyadiia

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14661,6 +14661,12 @@
     githubId = 16027994;
     name = "Nathan Viets";
   };
+  nyadiia = {
+    email = "nyadiia@pm.me";
+    github = "nyadiia";
+    githubId = 43252360;
+    name = "Nadia";
+  };
   nyanbinary = {
     email = "nyanbinary@keemail.me";
     matrix = "@niko:conduit.rs";
