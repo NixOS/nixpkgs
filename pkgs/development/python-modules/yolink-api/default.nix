8a1d41f226255e9f0316925d3791e3de5f644a32
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-11 16:12:28 +0000

    python312Packages.yolink-api: 0.4.2 -> 0.4.3

--- pkgs/development/python-modules/yolink-api/default.nix
+++ pkgs/development/python-modules/yolink-api/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "yolink-api";
-  version = "0.4.2";
+  version = "0.4.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "YoSmart-Inc";
     repo = "yolink-api";
     rev = "refs/tags/v${version}";
-    hash = "sha256-De3uMrLAz8LdX2pU5pn+6W6UvB+M9fSv80RSjgleo3M=";
+    hash = "sha256-cLuto2V5i3au1MRYYbgR2plw9YBpgIAxsG2fu4t37jk=";
   };
 
   build-system = [ setuptools ];
