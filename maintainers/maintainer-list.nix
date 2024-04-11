b76950871a2f313a8ba81ea8445190e0fc30cae7
uncenter <47499684+uncenter@users.noreply.github.com>
2024-04-11 12:05:44 -0400

    maintainers: add uncenter

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20600,6 +20600,12 @@
     githubId = 20206121;
     name = "umlx5h";
   };
+  uncenter = {
+    name = "uncenter";
+    email = "uncenter@uncenter.dev";
+    github = "uncenter";
+    githubId = 47499684;
+  };
   unclamped = {
     name = "Maru";
     email = "clear6860@tutanota.com";
