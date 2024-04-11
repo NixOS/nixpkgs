59a8558857d1035a98988fd23ab190e82ba332c3
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-11 20:36:51 +0000

    python312Packages.sievelib: 1.3.0 -> 1.4.0

--- pkgs/development/python-modules/sievelib/default.nix
+++ pkgs/development/python-modules/sievelib/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "sievelib";
-  version = "1.3.0";
+  version = "1.4.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-MxPX8fP4Mkq2qOISnknXbuCN8NQ+L1UOaBuPEuP0TNE=";
+    hash = "sha256-eM8es/WZENFBASjOk1KVbbwkmzxTr7NirOiSLt7F3N8=";
   };
 
   build-system = [
