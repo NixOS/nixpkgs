d987d9ef1ddcef785a56e4c7e86d741ed69b7f74
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-11 23:41:26 +0000

    renode-dts2repl: unstable-2024-03-24 -> unstable-2024-04-08

--- pkgs/by-name/re/renode-dts2repl/package.nix
+++ pkgs/by-name/re/renode-dts2repl/package.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication {
   pname = "renode-dts2repl";
-  version = "unstable-2024-03-24";
+  version = "unstable-2024-04-08";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "antmicro";
     repo = "dts2repl";
-    rev = "b581d3b7e68f7822110bc4895f7826f3505be21c";
-    hash = "sha256-Syu5DC8FP9Lyc7CVmUp38Vr3K7FK2Pv/jodzgnSB3cE=";
+    rev = "fa6c132a394e3503c1ba4466e546f98e33b098cf";
+    hash = "sha256-s1KOchSttAxDri4pLubjNCE2UXIqfnNtvpd6tfGmqao=";
   };
 
   nativeBuildInputs = [
