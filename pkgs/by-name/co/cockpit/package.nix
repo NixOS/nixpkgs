f46a73dbcd6e5781ab36c2a6835b8f96bd1683bf
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-12 03:48:23 +0000

    cockpit: 314 -> 315

--- pkgs/by-name/co/cockpit/package.nix
+++ pkgs/by-name/co/cockpit/package.nix
@@ -44,13 +44,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "cockpit";
-  version = "314";
+  version = "315";
 
   src = fetchFromGitHub {
     owner = "cockpit-project";
     repo = "cockpit";
     rev = "refs/tags/${version}";
-    hash = "sha256-6iLfUqzBG/pvGNnzMJzMQqnNjPvKJtTHHCSBWtSwwFM=";
+    hash = "sha256-N6MDqhYQky80r9wSiDIPusg6iOpDh4S52pf337nojyY=";
     fetchSubmodules = true;
   };
 
