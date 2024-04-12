a228d5f3d2508db02fb42e94bb3217089b2c472b
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-12 09:32:16 +0200

    audiness: 0.3.1 -> 0.3.2
    Diff: https://github.com/audiusGmbH/audiness/compare/refs/tags/0.3.1...0.3.2
    
    Changelog: https://github.com/audiusGmbH/audiness/releases/tag/0.3.2

--- pkgs/by-name/au/audiness/package.nix
+++ pkgs/by-name/au/audiness/package.nix
@@ -5,18 +5,26 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "audiness";
-  version = "0.3.1";
+  version = "0.3.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "audiusGmbH";
     repo = "audiness";
     rev = "refs/tags/${version}";
-    hash = "sha256-r+xWwXRKuTp5ifUUlF1K6BIVWh67hNLMBKBB7wnLLAM=";
+    hash = "sha256-vc2k3oEMTgzm/C6z6BieRrT3cSP0IkY+D3RXkNGaZTE=";
   };
+
+  pythonRelaxDeps = [
+    "validators"
+  ];
 
   build-system = with python3.pkgs; [
     poetry-core
+  ];
+
+  nativeBuildInputs = with python3.pkgs; [
+    pythonRelaxDepsHook
   ];
 
   dependencies = with python3.pkgs; [
