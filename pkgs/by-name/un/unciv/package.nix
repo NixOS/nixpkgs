7bea871ff2d584d5eb60d716701cc769bf1a7442
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-13 18:35:05 +0000

    unciv: 4.11.2 -> 4.11.4

--- pkgs/by-name/un/unciv/package.nix
+++ pkgs/by-name/un/unciv/package.nix
@@ -27,11 +27,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "unciv";
-  version = "4.11.2";
+  version = "4.11.4";
 
   src = fetchurl {
     url = "https://github.com/yairm210/Unciv/releases/download/${version}/Unciv.jar";
-    hash = "sha256-ZUDfJf+FgIX8gRoZYlYAJhpLwNWp3MCKEQOSQGQ88T8=";
+    hash = "sha256-QqnM1kTXHHpf8l99J8ydIjTkl/LetijzIJgLYt6OaRA=";
   };
 
   dontUnpack = true;
