77164110f2d62f3ce1f3b19e8d888bf9c410ba9e
Austin Horstman <khaneliman12@gmail.com>
2024-04-13 22:31:16 -0500

    sketchybar-app-font: pkgs/data/fonts -> pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13091,8 +13091,6 @@ with pkgs;
     inherit (darwin.apple_sdk_11_0.frameworks) AppKit Carbon CoreAudio CoreWLAN CoreVideo DisplayServices IOKit MediaRemote SkyLight;
   };
 
-  sketchybar-app-font = callPackage ../data/fonts/sketchybar-app-font { };
-
   skippy-xd = callPackage ../tools/X11/skippy-xd { };
 
   sks = callPackage ../servers/sks {
