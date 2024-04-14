aca15021d66f2591b5e43f039373fbac45729a69
paki23 <pk93@posteo.net>
2024-04-14 15:15:51 +0200

    firefoxpwa: add fhs wrapper to run downloaded binaries

--- pkgs/by-name/fi/firefoxpwa/package.nix
+++ pkgs/by-name/fi/firefoxpwa/package.nix
@@ -16,84 +16,105 @@
 , pciutils
 , libcanberra-gtk3
 , extraLibs ? [ ]
+, symlinkJoin
 , nixosTests
+, buildFHSUserEnv
 }:
-
-rustPlatform.buildRustPackage rec {
-  pname = "firefoxpwa";
+let
+  name = "firefoxpwa";
   version = "2.11.1";
-
-  src = fetchFromGitHub {
-    owner = "filips123";
-    repo = "PWAsForFirefox";
-    rev = "v${version}";
-    hash = "sha256-ZD/bTziVmHtQVKejzj+fUXVazCm2PaulS2NZjTribSk=";
-  };
-
-  sourceRoot = "${src.name}/native";
-  buildFeatures = [ "immutable-runtime" ];
-
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "mime-0.4.0-a.0" = "sha256-LjM7LH6rL3moCKxVsA+RUL9lfnvY31IrqHa9pDIAZNE=";
-      "web_app_manifest-0.0.0" = "sha256-G+kRN8AEmAY1TxykhLmgoX8TG8y2lrv7SCRJlNy0QzA=";
+  unwrapped = rustPlatform.buildRustPackage rec {
+    inherit version;
+    pname = "${name}-unwrapped";
+    src = fetchFromGitHub {
+      owner = "filips123";
+      repo = "PWAsForFirefox";
+      rev = "v${version}";
+      hash = "sha256-ZD/bTziVmHtQVKejzj+fUXVazCm2PaulS2NZjTribSk=";
     };
+
+    sourceRoot = "${src.name}/native";
+    buildFeatures = [ "immutable-runtime" ];
+
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      outputHashes = {
+        "mime-0.4.0-a.0" = "sha256-LjM7LH6rL3moCKxVsA+RUL9lfnvY31IrqHa9pDIAZNE=";
+        "web_app_manifest-0.0.0" = "sha256-G+kRN8AEmAY1TxykhLmgoX8TG8y2lrv7SCRJlNy0QzA=";
+      };
+    };
+
+    preConfigure = ''
+      sed -i 's;version = "0.0.0";version = "${version}";' Cargo.toml
+      sed -zi 's;name = "firefoxpwa"\nversion = "0.0.0";name = "firefoxpwa"\nversion = "${version}";' Cargo.lock
+      sed -i $'s;DISTRIBUTION_VERSION = \'0.0.0\';DISTRIBUTION_VERSION = \'${version}\';' userchrome/profile/chrome/pwa/chrome.jsm
+    '';
+
+    nativeBuildInputs = [ makeWrapper pkg-config installShellFiles ];
+    buildInputs = [ openssl ];
+
+    FFPWA_EXECUTABLES = ""; # .desktop entries generated without any store path references
+    FFPWA_SYSDATA = "${placeholder "out"}/share/firefoxpwa";
+    completions = "target/${stdenv.targetPlatform.config}/release/completions";
+
+    gtk_modules = map (x: x + x.gtkModule) [ libcanberra-gtk3 ];
+    libs = let libs = lib.optionals stdenv.isLinux [ udev libva mesa libnotify xorg.libXScrnSaver cups pciutils ] ++ gtk_modules ++ extraLibs; in lib.makeLibraryPath libs + ":" + lib.makeSearchPathOutput "lib" "lib64" libs;
+
+    postInstall = ''
+      # Runtime
+      mkdir -p $out/share/firefoxpwa
+      cp -Lr ${firefox-unwrapped}/lib/firefox $out/share/firefoxpwa/runtime
+      chmod -R +w $out/share/firefoxpwa
+
+      # UserChrome
+      cp -r userchrome $out/share/firefoxpwa
+
+      # Runtime patching
+      FFPWA_USERDATA=$out/share/firefoxpwa $out/bin/firefoxpwa runtime patch
+
+      # Manifest
+      sed -i "s!/usr/libexec!$out/bin!" manifests/linux.json
+      install -Dm644 manifests/linux.json $out/lib/mozilla/native-messaging-hosts/firefoxpwa.json
+
+      installShellCompletion --cmd firefoxpwa \
+        --bash $completions/firefoxpwa.bash \
+        --fish $completions/firefoxpwa.fish \
+        --zsh $completions/_firefoxpwa
+
+      # AppStream Metadata
+      install -Dm644 packages/appstream/si.filips.FirefoxPWA.metainfo.xml $out/share/metainfo/si.filips.FirefoxPWA.metainfo.xml
+      install -Dm644 packages/appstream/si.filips.FirefoxPWA.svg $out/share/icons/hicolor/scalable/apps/si.filips.FirefoxPWA.svg
+
+      wrapProgram $out/bin/firefoxpwa \
+        --prefix FFPWA_SYSDATA : "$out/share/firefoxpwa" \
+        --prefix LD_LIBRARY_PATH : "$libs" \
+        --suffix-each GTK_PATH : "$gtk_modules"
+
+      wrapProgram $out/bin/firefoxpwa-connector \
+        --prefix FFPWA_SYSDATA : "$out/share/firefoxpwa" \
+        --prefix LD_LIBRARY_PATH : "$libs" \
+        --suffix-each GTK_PATH : "$gtk_modules"
+    '';
+
+    postInstallCheck = ''
+      mv $out/bin/firefoxpwa $out/bin/.firefoxpwa-wrapped
+    '';
   };
 
-  preConfigure = ''
-    sed -i 's;version = "0.0.0";version = "${version}";' Cargo.toml
-    sed -zi 's;name = "firefoxpwa"\nversion = "0.0.0";name = "firefoxpwa"\nversion = "${version}";' Cargo.lock
-    sed -i $'s;DISTRIBUTION_VERSION = \'0.0.0\';DISTRIBUTION_VERSION = \'${version}\';' userchrome/profile/chrome/pwa/chrome.jsm
-  '';
-
-  nativeBuildInputs = [ makeWrapper pkg-config installShellFiles ];
-  buildInputs = [ openssl ];
-
-  FFPWA_EXECUTABLES = ""; # .desktop entries generated without any store path references
-  FFPWA_SYSDATA = "${placeholder "out"}/share/firefoxpwa";
-  completions = "target/${stdenv.targetPlatform.config}/release/completions";
-
-  gtk_modules = map (x: x + x.gtkModule) [ libcanberra-gtk3 ];
-  libs = let libs = lib.optionals stdenv.isLinux [ udev libva mesa libnotify xorg.libXScrnSaver cups pciutils ] ++ gtk_modules ++ extraLibs; in lib.makeLibraryPath libs + ":" + lib.makeSearchPathOutput "lib" "lib64" libs;
-
-  postInstall = ''
-    # Runtime
-    mkdir -p $out/share/firefoxpwa
-    cp -Lr ${firefox-unwrapped}/lib/firefox $out/share/firefoxpwa/runtime
-    chmod -R +w $out/share/firefoxpwa
-
-    # UserChrome
-    cp -r userchrome $out/share/firefoxpwa
-
-    # Runtime patching
-    FFPWA_USERDATA=$out/share/firefoxpwa $out/bin/firefoxpwa runtime patch
-
-    # Manifest
-    sed -i "s!/usr/libexec!$out/bin!" manifests/linux.json
-    install -Dm644 manifests/linux.json $out/lib/mozilla/native-messaging-hosts/firefoxpwa.json
-
-    installShellCompletion --cmd firefoxpwa \
-      --bash $completions/firefoxpwa.bash \
-      --fish $completions/firefoxpwa.fish \
-      --zsh $completions/_firefoxpwa
-
-    # AppStream Metadata
-    install -Dm644 packages/appstream/si.filips.FirefoxPWA.metainfo.xml $out/share/metainfo/si.filips.FirefoxPWA.metainfo.xml
-    install -Dm644 packages/appstream/si.filips.FirefoxPWA.svg $out/share/icons/hicolor/scalable/apps/si.filips.FirefoxPWA.svg
-
-    wrapProgram $out/bin/firefoxpwa \
-      --prefix FFPWA_SYSDATA : "$out/share/firefoxpwa" \
-      --prefix LD_LIBRARY_PATH : "$libs" \
-      --suffix-each GTK_PATH : "$gtk_modules"
-
-    wrapProgram $out/bin/firefoxpwa-connector \
-      --prefix FFPWA_SYSDATA : "$out/share/firefoxpwa" \
-      --prefix LD_LIBRARY_PATH : "$libs" \
-      --suffix-each GTK_PATH : "$gtk_modules"
-  '';
-
-  passthru.tests.firefoxpwa = nixosTests.firefoxpwa;
+  # firefoxpwa wants to run binaries downloaded into users' home dir
+  fhs = buildFHSUserEnv {
+    name = "${name}-wrapper-${version}";
+    runScript = "${unwrapped}/bin/.firefoxpwa-wrapped";
+    targetPkgs = _: firefox-unwrapped.buildInputs;
+  };
+in
+(symlinkJoin {
+  name = "${name}-${version}";
+  paths = [ fhs unwrapped ];
+}) // {
+  inherit unwrapped fhs version;
+  pname = name;
+  tests.firefoxpwa = nixosTests.firefoxpwa;
 
   meta = with lib; {
     description = "A tool to install, manage and use Progressive Web Apps (PWAs) in Mozilla Firefox (native component)";
