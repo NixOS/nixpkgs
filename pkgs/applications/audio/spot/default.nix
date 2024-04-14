cf1752d253c2ac1cf17b1d4aa551d5cb14782f17
Harsh Shandilya <me@msfjarvis.dev>
2024-04-14 23:41:21 +0530

    spot: make msfjarvis the maintainer

--- pkgs/applications/audio/spot/default.nix
+++ pkgs/applications/audio/spot/default.nix
@@ -78,7 +78,7 @@ stdenv.mkDerivation rec {
     mainProgram = "spot";
     homepage = "https://github.com/xou816/spot";
     license = licenses.mit;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ msfjarvis ];
     platforms = platforms.linux;
   };
 }
