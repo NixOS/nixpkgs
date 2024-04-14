41b9b176ca1ced41dc679bc4063ba5a434f151f4
xrelkd <46590321+xrelkd@users.noreply.github.com>
2024-04-14 19:43:20 +0800

    solana-validator: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36070,8 +36070,6 @@ with pkgs;
 
   sumokoin = callPackage ../applications/blockchains/sumokoin { };
 
-  solana-validator = callPackage ../applications/blockchains/solana-validator { };
-
   snarkos = callPackage ../applications/blockchains/snarkos {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
