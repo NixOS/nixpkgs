80c133de66cd6ea21c913d1c357e1b76ead87855
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-14 23:17:05 +0200

    python312Packages.manifestoo: refactor

--- pkgs/development/python-modules/manifestoo/default.nix
+++ pkgs/development/python-modules/manifestoo/default.nix
@@ -1,48 +1,48 @@
-{ buildPythonPackage
-, fetchPypi
-, hatch-vcs
-, importlib-metadata
-, lib
-, manifestoo-core
-, nix-update-script
-, pytestCheckHook
-, pythonOlder
-, textual
-, typer
-, typing-extensions
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  hatch-vcs,
+  importlib-metadata,
+  manifestoo-core,
+  nix-update-script,
+  pytestCheckHook,
+  pythonOlder,
+  textual,
+  typer,
+  typing-extensions,
 }:
 
 buildPythonPackage rec {
   pname = "manifestoo";
   version = "0.7";
-  format = "pyproject";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-gCGchc+fShBgt6fVJAx80+QnH+vxWo3jsIyePkFwhYE=";
   };
 
-  nativeBuildInputs = [
-    hatch-vcs
-  ];
+  build-system = [ hatch-vcs ];
 
-  nativeCheckInputs = [
-    pytestCheckHook
-  ];
+  propagatedBuildInputs =
+    [
+      manifestoo-core
+      textual
+      typer
+    ]
+    ++ typer.optional-dependencies.standard ++ lib.optionals (pythonOlder "3.8") [ importlib-metadata ];
 
-  propagatedBuildInputs = [
-    manifestoo-core
-    textual
-    typer
-  ]
-  ++ typer.passthru.optional-dependencies.standard
-  ++ lib.optionals (pythonOlder "3.8") [ importlib-metadata ];
+  nativeCheckInputs = [ pytestCheckHook ];
 
-  passthru.updateScript = nix-update-script { };
+  pythonImportsCheck = [ "manifestoo" ];
 
   meta = with lib; {
     description = "A tool to reason about Odoo addons manifests";
     homepage = "https://github.com/acsone/manifestoo";
+    changelog = "https://github.com/acsone/manifestoo/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ yajo ];
   };
