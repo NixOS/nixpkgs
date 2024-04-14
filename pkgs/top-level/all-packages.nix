16e24f645b60e0057936c350929d821aecdeb745
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-04-14 10:17:43 +0200

    python3Packages.sev-snp-measure: add executable output

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24471,6 +24471,8 @@ with pkgs;
 
   serf = callPackage ../development/libraries/serf { };
 
+  sev-snp-measure = with python3Packages; toPythonApplication sev-snp-measure;
+
   sfsexp = callPackage ../development/libraries/sfsexp { };
 
   shhmsg = callPackage ../development/libraries/shhmsg { };
