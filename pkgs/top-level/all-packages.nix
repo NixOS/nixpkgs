ccf30c2d08e9f39e6f2dab17cdcdbd0c881e5f27
Sefa Eyeoglu <contact@scrumplex.net>
2024-04-15 14:59:15 +0200

    vibrantlinux: 2.1.10 -> 2.2.0
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13862,6 +13862,8 @@ with pkgs;
 
   vhdl-ls = callPackage ../development/tools/language-servers/vhdl-ls { };
 
+  vibrantlinux = qt6Packages.callPackage ../tools/X11/vibrantlinux {};
+
   video2midi = callPackage ../tools/audio/video2midi {
     pythonPackages = python3Packages;
   };
