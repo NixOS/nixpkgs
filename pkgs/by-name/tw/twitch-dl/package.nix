5c7621cc673f1825aa54cfa2843435dfa2a288b4
Mario Rodas <marsam@users.noreply.github.com>
2024-04-15 04:20:00 +0000

    twitch-dl: 2.1.4 -> 2.2.0
    Diff: https://github.com/ihabunek/twitch-dl/compare/refs/tags/2.1.4...2.2.0
    
    Changelog: https://github.com/ihabunek/twitch-dl/blob/refs/tags/2.2.0/CHANGELOG.md

--- pkgs/by-name/tw/twitch-dl/package.nix
+++ pkgs/by-name/tw/twitch-dl/package.nix
@@ -7,26 +7,36 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "twitch-dl";
-  version = "2.1.4";
-
-  format = "setuptools";
+  version = "2.2.0";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ihabunek";
     repo = "twitch-dl";
     rev = "refs/tags/${version}";
-    hash = "sha256-0mITsNQQWG6lVQSxnDdU4ta548AR9q+zs/E96uwtG/U=";
+    hash = "sha256-H2SxZgEjVdj/GRguJ2v/WWUrh0VTrwFV9mZVn/EYyPg=";
   };
 
-  nativeCheckInputs = [
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'm3u8>=1.0.0,<4.0.0' 'm3u8>=1.0.0'
+  '';
+
+  nativeBuildInputs = [
+    python3Packages.setuptools
+    python3Packages.setuptools-scm
     installShellFiles
-    python3Packages.pytestCheckHook
     scdoc
   ];
 
   propagatedBuildInputs = with python3Packages; [
+    click
     httpx
     m3u8
+  ];
+
+  nativeCheckInputs = [
+    python3Packages.pytestCheckHook
   ];
 
   disabledTestPaths = [
@@ -46,7 +56,7 @@ python3Packages.buildPythonApplication rec {
   meta = with lib; {
     description = "CLI tool for downloading videos from Twitch";
     homepage = "https://github.com/ihabunek/twitch-dl";
-    changelog = "https://github.com/ihabunek/twitch-dl/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/ihabunek/twitch-dl/blob/${src.rev}/CHANGELOG.md";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ marsam ];
     mainProgram = "twitch-dl";
