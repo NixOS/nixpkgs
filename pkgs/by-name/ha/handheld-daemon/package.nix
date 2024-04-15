c9e8b7cf0260618682c15f7ba601ec9827396ceb
Toast <39011842+toast003@users.noreply.github.com>
2024-04-15 20:07:46 +0200

    handheld-daemon: move udev rule comment

--- pkgs/by-name/ha/handheld-daemon/package.nix
+++ pkgs/by-name/ha/handheld-daemon/package.nix
@@ -34,12 +34,12 @@ python3.pkgs.buildPythonApplication rec {
 
   # handheld-daemon contains a fork of the python module `hid`, so this hook
   # is borrowed from the `hid` derivation.
+  # The generated udev rules point to /bin/chroot, which does not exist in NixOS
   postPatch = ''
     hidapi=${ hidapi }/lib/
     test -d $hidapi || { echo "ERROR: $hidapi doesn't exist, please update/fix this build expression."; exit 1; }
     sed -i -e "s|libhidapi|$hidapi/libhidapi|" src/hhd/controller/lib/hid.py
 
-    # The generated udev rules point to /bin/chroot, which does not exist in NixOS
     chmod=${ toybox }/bin/chmod
     sed -i -e "s|/bin/chmod|$chmod|" src/hhd/controller/lib/hide.py
   '';
