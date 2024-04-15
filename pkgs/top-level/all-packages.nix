f2d00fba3a090e16e8c59ef6673731447ec1a007
Anderson Torres <torres.anderson.85@protonmail.com>
2024-04-15 12:59:18 -0300

    aegisub: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1577,7 +1577,7 @@ with pkgs;
 
   aefs = callPackage ../tools/filesystems/aefs { };
 
-  aegisub = callPackage ../applications/video/aegisub ({
+  aegisub = callPackage ../by-name/ae/aegisub/package.nix ({
     wxGTK = wxGTK32;
   } // (config.aegisub or {}));
 
