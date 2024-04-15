53f7c1d7b9899baa79da8bd017f401f2ba935474
Iggy Ziggy <iggy.ziggy@gmail.com>
2024-04-15 11:43:46 +0200

    bump to version 10.2.1

--- pkgs/by-name/im/immersed-vr/package.nix
+++ pkgs/by-name/im/immersed-vr/package.nix
@@ -6,15 +6,15 @@
 }:
 let
   pname = "immersed-vr";
-  version = "10.2.0";
+  version = "10.2.1";
 
   sources = rec {
     x86_64-linux = {
-      url = "https://web.archive.org/web/20240411143649/https://static.immersed.com/dl/Immersed-x86_64.AppImage";
-      hash = "sha256-ZY9pwOryADkpbMLx1w5Lymx6iQ0BhIrFAAvy47wLnQ8=";
+      url = "http://web.archive.org/web/20240415082836/https://static.immersed.com/dl/Immersed-x86_64.AppImage";
+      hash = "sha256-hKaF1XzZa0qAh7r1CtK0j2JFqE16hGNBGph7aOnYJCQ=";
     };
     x86_64-darwin = {
-      url = "https://web.archive.org/web/20240411144415/https://static.immersed.com/dl/Immersed.dmg";
+      url = "http://web.archive.org/web/20240415083533/https://static.immersed.com/dl/Immersed.dmg";
       hash = "sha256-MdRJAo85xAB8OEgdQf0BjfORwSLIwtpOVwJ5qWfpXJY=";
     };
     aarch64-darwin = x86_64-darwin;
