228621e42dc43f936b66e0ed042c90c511aa0535
Gabriel Volpe <volpegabriel@gmail.com>
2024-04-15 19:03:54 +0200

    lib/attrsets: rename cartesianProductOfSets to cartesianProduct

--- lib/default.nix
+++ lib/default.nix
@@ -86,7 +86,7 @@ let
       zipAttrsWithNames zipAttrsWith zipAttrs recursiveUpdateUntil
       recursiveUpdate matchAttrs mergeAttrsList overrideExisting showAttrPath getOutput
       getBin getLib getDev getMan chooseDevOutputs zipWithNames zip
-      recurseIntoAttrs dontRecurseIntoAttrs cartesianProductOfSets
+      recurseIntoAttrs dontRecurseIntoAttrs cartesianProduct cartesianProductOfSets
       updateManyAttrsByPath;
     inherit (self.lists) singleton forEach foldr fold foldl foldl' imap0 imap1
       concatMap flatten remove findSingle findFirst any all count
