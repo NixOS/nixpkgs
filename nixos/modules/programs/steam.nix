7c40604725945aa2185212df63fad5e3724bb507
qubitnano <146656568+qubitnano@users.noreply.github.com>
2024-04-15 18:22:48 -0400

    nixos/steam: enable hashed certs

--- nixos/modules/programs/steam.nix
+++ nixos/modules/programs/steam.nix
@@ -168,6 +168,9 @@ in {
 
     hardware.steam-hardware.enable = true;
 
+    # Some games require hashed certificates
+    security.pki.useHashedCertificates = true;
+
     environment.systemPackages = [
       cfg.package
       cfg.package.run
