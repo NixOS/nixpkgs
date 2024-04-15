d469a69d5b63e4258391fe0032220f34ef5a8c98
Jacek Galowicz <jacek@galowicz.de>
2024-04-15 12:23:21 +0200

    NixOS integration tests: Fix setup.py formatting for latest versions of ruff and black

---
+++ nixos/lib/test-driver/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="nixos-test-driver",
+    version="1.1",
+    packages=find_packages(),
+    package_data={"test_driver": ["py.typed"]},
+    entry_points={
+        "console_scripts": [
+            "nixos-test-driver=test_driver:main",
+            "generate-driver-symbols=test_driver:generate_driver_symbols",
+        ]
+    },
+)
