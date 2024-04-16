37945e7f0853fd0966e1123137e5c142fd644226
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-04-16 12:44:55 +0200

    wordpress6_5: 6.5 -> 6.5.2

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -9,7 +9,7 @@
     hash = "sha256-aLOO/XgjI3d/+1BpHDT2pGR697oceghjzOId1MjC+wQ=";
   };
   wordpress6_5 = {
-    version = "6.5";
-    hash = "sha256-NCRWKIrhSMC6itw2y/WJUd2LSRtnVWkmVO/b2nEJK58=";
+    version = "6.5.2";
+    hash = "sha256-APBO7hO9iuDGOP/IvB0dLAwby0PU7LPFth4IUXNQe9I=";
   };
 }
