d947abe783a7307ed7faca7f9c91c7907ac77ffc
rster2002 <rster2002dev@gmail.com>
2024-04-16 17:24:21 +0200

    maintainers: add rster2002

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17331,6 +17331,11 @@
     githubId = 19433256;
     name = "Radoslaw Sniezek";
   };
+  rster2002 = {
+    name = "Bjørn";
+    github = "rster2002";
+    githubId = 26026518;
+  };
   rsynnest = {
     email = "contact@rsynnest.com";
     github = "rsynnest";
