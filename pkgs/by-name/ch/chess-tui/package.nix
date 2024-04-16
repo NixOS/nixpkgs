8f656700e8ea4c370bdd2bf9ed951a8e7db82daa
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-16 16:03:21 +0000

    chess-tui: 1.2.0 -> 1.2.1

--- pkgs/by-name/ch/chess-tui/package.nix
+++ pkgs/by-name/ch/chess-tui/package.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "chess-tui";
-  version = "1.2.0";
+  version = "1.2.1";
 
   src = fetchFromGitHub {
     owner = "thomas-mauran";
     repo = "chess-tui";
     rev = "${version}";
-    hash = "sha256-If2xShHuqdCeasP12ZzwvGJSIKFmqJs0Hv1fBhJKoU4=";
+    hash = "sha256-LtxaZ/7p/lqStoUmckVVaegQp02Ci3L46fMFEgledj4=";
   };
 
-  cargoHash = "sha256-TT4lVAttlT3knufMslSVxInn9QM0QJ9jyklzwhRikWA=";
+  cargoHash = "sha256-RUnT5b9pBcopTPT/1J48xZ4pfn3C0mIuYTDvgf3zvn0=";
 
   meta = with lib;{
     description = "A chess TUI implementation in rust";
