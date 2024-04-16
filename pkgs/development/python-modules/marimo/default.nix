a4de95ac536b2cc731c395ce0c03a929df9271b6
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-16 05:59:17 +0000

    python312Packages.marimo: 0.3.12 -> 0.4.0

--- pkgs/development/python-modules/marimo/default.nix
+++ pkgs/development/python-modules/marimo/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "marimo";
-  version = "0.3.12";
+  version = "0.4.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-4OwTY4OFkTQcQqHntjcfwPTx4w+2GPEUBy/XgW/nATU=";
+    hash = "sha256-IiM7iJs3CIl6WjstgvtJzIylVL49jRG246GU5G5GEG0=";
   };
 
   build-system = [
