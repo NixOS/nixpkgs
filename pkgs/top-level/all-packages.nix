70a9ac30edc1ec8f855675955ca3d6af26b9ffb9
Anderson Torres <torres.anderson.85@protonmail.com>
2024-04-15 22:30:09 -0300

    bsd-finger: adopt and refactor
    - migrate to by-name
    - convert to finalAttrs design pattern
    - get rid of fetchDebianPatch
    - set mainProgram according to buildProduct
    - deprecate buildClient
    - nixfmt-rfc-style
    - adopted by AndersonTorres

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7837,8 +7837,9 @@ with pkgs;
 
   fingerprintx = callPackage ../tools/security/fingerprintx { };
 
-  bsd-finger = callPackage ../tools/networking/bsd-finger { };
-  bsd-fingerd = bsd-finger.override({ buildClient = false; });
+  bsd-fingerd = bsd-finger.override({
+    buildProduct = "daemon";
+  });
 
   iprange = callPackage ../applications/networking/firehol/iprange.nix { };
 
