5cc869f235b8784775b4484fd835036eb35da9da
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-16 11:06:46 +0200

    python312Packages.aliyun-python-sdk-core: 2.15.0 -> 2.15.1
    Changelog: https://github.com/aliyun/aliyun-openapi-python-sdk/blob/master/aliyun-python-sdk-core/ChangeLog.txt

--- pkgs/development/python-modules/aliyun-python-sdk-core/default.nix
+++ pkgs/development/python-modules/aliyun-python-sdk-core/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "aliyun-python-sdk-core";
-  version = "2.15.0";
+  version = "2.15.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-7cRVVIjYqfHGG9QZx74nsjl0sqBSlxtGFPzSKerus4I=";
+    hash = "sha256-UYVQ0H9TfNOvrDtsk7XJl840QOTQwFTjrL2qgmHpCt8=";
   };
 
   nativeBuildInputs = [
