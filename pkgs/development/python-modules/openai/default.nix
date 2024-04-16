9398b91572d92a9a4832c7d14e2616283c7f3ef3
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-16 09:14:21 +0000

    openai: 1.17.0 -> 1.19.0

--- pkgs/development/python-modules/openai/default.nix
+++ pkgs/development/python-modules/openai/default.nix
@@ -25,7 +25,7 @@
 
 buildPythonPackage rec {
   pname = "openai";
-  version = "1.17.0";
+  version = "1.19.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7.1";
@@ -34,7 +34,7 @@ buildPythonPackage rec {
     owner = "openai";
     repo = "openai-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-NAfG8edQB9L72MlESqMXw6fo/s2F/BjAf5yRAmx+J/s=";
+    hash = "sha256-VlBRKJNGD/pdS/BMHwyckhjMgbK7H0j0v08TZHvxxWY=";
   };
 
   build-system = [
