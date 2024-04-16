e696d45c4d9bb059b8823c4d6c0a1ae22f0e60dd
Hobr <mail@hobr.site>
2024-04-16 11:46:45 +0800

    fit PR standard

--- pkgs/applications/video/aegisub/default.nix
+++ pkgs/applications/video/aegisub/default.nix
@@ -3,6 +3,8 @@
 , stdenv
 , fetchFromGitHub
 , fetchurl
+, fetchpatch
+
 , meson
 , ninja
 , cmake
@@ -11,6 +13,7 @@
 , gettext
 , python3
 , wrapGAppsHook
+
 , fontconfig
 , libass
 , boost
@@ -22,72 +25,81 @@
 , ffmpeg_4
 , jansson
 , libGL
+, libGLU
 , zlib
 , libX11
+, libiconv
+, darwin
+
 , spellcheckSupport ? true
 , hunspell ? null
+
 , openalSupport ? false
 , openal ? null
+
 , alsaSupport ? stdenv.isLinux
 , alsa-lib ? null
+
 , pulseaudioSupport ? config.pulseaudio or stdenv.isLinux
 , libpulseaudio ? null
+
 , portaudioSupport ? false
 , portaudio ? null
-,
 }:
 assert spellcheckSupport -> (hunspell != null);
 assert openalSupport -> (openal != null);
 assert alsaSupport -> (alsa-lib != null);
 assert pulseaudioSupport -> (libpulseaudio != null);
-assert portaudioSupport -> (portaudio != null); let
+assert portaudioSupport -> (portaudio != null);
+let
   inherit (lib) optional;
-
+  inherit (darwin.apple_sdk.frameworks) CoreText CoreFoundation AppKit Carbon IOKit QuartzCore Cocoa;
   luajit52 = luajit.override { enable52Compat = true; };
-
-  full_version = "feature_11";
 
   bestsource = fetchFromGitHub {
     owner = "vapoursynth";
     repo = "bestsource";
+    # https://github.com/arch1t3cht/Aegisub/blob/feature_11/subprojects/bestsource.wrap#L3
     rev = "ba1249c1f5443be6d0ec2be32490af5bbc96bf99";
     hash = "sha256-9BnyRzF33otju3W503O18JuTyvp+hFxk6JMwrozKoZY=";
-    fetchSubmodules = true;
   };
 
   vapoursynth = fetchFromGitHub {
     owner = "vapoursynth";
     repo = "vapoursynth";
-    rev = "R65";
+    # https://github.com/arch1t3cht/Aegisub/blob/feature_11/subprojects/vapoursynth.wrap#L3
+    rev = "R59";
     hash = "sha256-6w7GSC5ZNIhLpulni4sKq0OvuxHlTJRilBFGH5PQW8U=";
   };
 
   AviSynthPlus = fetchFromGitHub {
     owner = "AviSynth";
     repo = "AviSynthPlus";
-    rev = "v3.7.3";
-    hash = "sha256-sBxZ2J5sS/2wrL+tSxVAFPKNbg3c1iMSHRueRQAs5J4=";
+    # https://github.com/arch1t3cht/Aegisub/blob/feature_11/subprojects/avisynth.wrap#L3
+    rev = "v3.7.2";
+    hash = "sha256-PNIrDRJNKWEBPEKlCq0nE6UW0prVswE6mW+Fi4ROTAc=";
     fetchSubmodules = true;
   };
 
   gtest = fetchurl {
+    # https://github.com/arch1t3cht/Aegisub/blob/feature_11/subprojects/gtest.wrap#L4
     url = "https://github.com/google/googletest/archive/release-1.8.1.zip";
     hash = "sha256-kngnwYPQFzTMXP74Xg/z9akv/mGI4NGOkJxe/r8ooMc=";
   };
 
   gtest_patch = fetchurl {
+    # https://github.com/arch1t3cht/Aegisub/blob/feature_11/subprojects/gtest.wrap#L8
     url = "https://wrapdb.mesonbuild.com/v1/projects/gtest/1.8.1/1/get_zip";
     hash = "sha256-959f1G4JUHs/LgmlHqbrIAIO/+VDM19a7lnzDMjRWAU=";
   };
-in
-stdenv.mkDerivation rec {
+in stdenv.mkDerivation rec {
   pname = "aegisub";
-  version = full_version;
+  version = "feature_11";
 
   src = fetchFromGitHub {
     owner = "arch1t3cht";
     repo = "Aegisub";
-    rev = full_version;
+    rev = version;
     hash = "sha256-yEXDwne+wros0WjOwQbvMIXk0UXV5TOoV/72K12vi/c=";
   };
 
@@ -99,94 +111,81 @@ stdenv.mkDerivation rec {
     luajit52
     gettext
     python3
+    wrapGAppsHook
   ];
 
-  buildInputs =
-    [
-      fontconfig
-      libass
-      boost
-      wxGTK32
-      icu
-      ffms
-      fftw
-      libuchardet
-      ffmpeg_4
-      jansson
-      libGL
-      zlib
-      libX11
-    ]
-    ++ optional alsaSupport alsa-lib
-    ++ optional openalSupport openal
-    ++ optional portaudioSupport portaudio
-    ++ optional pulseaudioSupport libpulseaudio
-    ++ optional spellcheckSupport hunspell;
+  buildInputs = [
+    fontconfig
+    libass
+    boost
+    wxGTK32
+    icu
+    ffms
+    fftw
+    libuchardet
+    ffmpeg_4
+    jansson
+    libGL
+    libGLU
+    zlib
+    libX11
+    libiconv
+  ]
+  ++ optional alsaSupport alsa-lib
+  ++ optional openalSupport openal
+  ++ optional portaudioSupport portaudio
+  ++ optional pulseaudioSupport libpulseaudio
+  ++ optional spellcheckSupport hunspell
+  ++ lib.optionals stdenv.isDarwin [
+    CoreText
+    CoreFoundation
+    AppKit
+    Carbon
+    IOKit
+    QuartzCore
+    Cocoa
+  ];
 
-  env = {
-    BOOST_INCLUDEDIR = "${lib.getDev boost}/include";
-    BOOST_LIBRARYDIR = "${lib.getLib boost}/lib";
-    NIX_CFLAGS_COMPILE = "-I${luajit52}/include";
-    NIX_CFLAGS_LINK = "-L${luajit52}/lib";
-  };
+  BOOST_INCLUDEDIR = "${lib.getDev boost}/include";
+  BOOST_LIBRARYDIR = "${lib.getLib boost}/lib";
 
   patches = [
-    # https://aur.archlinux.org/cgit/aur.git/tree/0001-bas-to-bs.patch?h=aegisub-arch1t3cht
-    ./0001-bas-to-bs.patch
+    (fetchpatch {
+      name = "0001-bas-to-bs.patch";
+      url =
+        "https://aur.archlinux.org/cgit/aur.git/plain/0001-bas-to-bs.patch?h=aegisub-arch1t3cht&id=bbbea73953858fc7bf2775a0fb92cec49afb586c";
+      hash = "sha256-T0Msa8rpE3Qo++Tq6J/xdsDX9f1vVIj/b9rR/iuIGK4=";
+    })
     # Fix git_version.h unable to generate
     ./0002-remove-git-version.patch
     # Fix meson unable exec python respack
-    # https://github.com/arch1t3cht/Aegisub/pull/121
     ./0003-respack-unable-run.patch
   ];
 
-  mesonFlags =
-    [
-      "--buildtype=release"
-      (lib.mesonBool "b_lto" false)
-      (lib.mesonOption "default_audio_output" "auto")
-      (lib.mesonEnable "directsound" false)
-      (lib.mesonEnable "xaudio2" false)
+  mesonFlags = [
+    "--buildtype=release"
+    (lib.mesonBool "b_lto" false)
+    (lib.mesonOption "default_audio_output" "auto")
+    (lib.mesonEnable "alsa" alsaSupport)
+    (lib.mesonEnable "openal" openalSupport)
+    (lib.mesonEnable "libpulse" pulseaudioSupport)
+    (lib.mesonEnable "portaudio" portaudioSupport)
+    (lib.mesonEnable "directsound" false)
+    (lib.mesonEnable "xaudio2" false)
 
-      (lib.mesonEnable "ffms2" true)
-      (lib.mesonEnable "avisynth" true)
-      (lib.mesonEnable "bestsource" true)
-      (lib.mesonEnable "vapoursynth" true)
+    (lib.mesonEnable "ffms2" true)
+    (lib.mesonEnable "avisynth" true)
+    (lib.mesonEnable "bestsource" true)
+    (lib.mesonEnable "vapoursynth" true)
 
-      (lib.mesonEnable "fftw3" true)
-      (lib.mesonEnable "uchardet" true)
-      (lib.mesonEnable "csri" true)
-    ]
-    ++ (
-      if alsaSupport
-      then [ (lib.mesonEnable "alsa" true) ]
-      else [ (lib.mesonEnable "alsa" false) ]
-    )
-    ++ (
-      if openalSupport
-      then [ (lib.mesonEnable "openal" true) ]
-      else [ (lib.mesonEnable "openal" false) ]
-    )
-    ++ (
-      if pulseaudioSupport
-      then [ (lib.mesonEnable "libpulse" true) (lib.mesonOption "default_audio_output" "PulseAudio") ]
-      else [ (lib.mesonEnable "libpulse" false) ]
-    )
-    ++ (
-      if portaudioSupport
-      then [ (lib.mesonEnable "portaudio" true) ]
-      else [ (lib.mesonEnable "portaudio" false) ]
-    )
-    ++ (
-      if spellcheckSupport
-      then [ (lib.mesonEnable "hunspell" true) ]
-      else [ (lib.mesonEnable "hunspell" false) ]
-    );
+    (lib.mesonEnable "fftw3" true)
+    (lib.mesonEnable "uchardet" true)
+    (lib.mesonEnable "csri" true)
+    (lib.mesonEnable "hunspell" spellcheckSupport)
+  ];
 
   enableParallelBuilding = true;
 
-  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/video/aegisub/default.nix
-  # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=aegisub-arch1t3cht
   preConfigure = ''
     cp -r --no-preserve=mode ${bestsource} subprojects/bestsource
     cp -r --no-preserve=mode ${AviSynthPlus} subprojects/avisynth
@@ -199,17 +198,21 @@ stdenv.mkDerivation rec {
     meson subprojects packagefiles --apply bestsource
     meson subprojects packagefiles --apply avisynth
     meson subprojects packagefiles --apply vapoursynth
-
-    mkdir -p build
-    echo "#define BUILD_GIT_VERSION_NUMBER 0" > build/git_version.h
-    echo """#define BUILD_GIT_VERSION_STRING \"${full_version}\"""" >> build/git_version.h
   '';
 
   meta = with lib; {
-    description = "Cross-platform advanced subtitle editor, with new feature branches.";
+    description =
+      "Cross-platform advanced subtitle editor, with new feature branches.";
+    longDescription = ''
+      Aegisub is a free, cross-platform open source tool for creating and
+      modifying subtitles. Aegisub makes it quick and easy to time subtitles to
+      audio, and features many powerful tools for styling them, including a
+      built-in real-time video preview.
+    '';
     homepage = "https://github.com/arch1t3cht/Aegisub";
     license = licenses.bsd3;
+    maintainers = with maintainers; [ AndersonTorres wegank ];
+    platforms = platforms.unix;
     mainProgram = "aegisub";
-    platforms = platforms.all;
   };
 }
