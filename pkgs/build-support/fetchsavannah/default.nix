5b4c6f063ee3479af3b399b1da3d55794a04b357
Anderson Torres <torres.anderson.85@protonmail.com>
2024-04-17 20:07:26 -0300

    fetchFromSavannah: set meta.repository attribute
    So that it can be "inherited" by the caller.
    
    Set it to a list so that it can be "expanded".

--- pkgs/build-support/fetchsavannah/default.nix
+++ pkgs/build-support/fetchsavannah/default.nix
@@ -8,5 +8,6 @@ lib.makeOverridable (
   inherit name;
   url = "https://git.savannah.gnu.org/cgit/${repo}.git/snapshot/${repo}-${rev}.tar.gz";
   meta.homepage = "https://git.savannah.gnu.org/cgit/${repo}.git/";
+  meta.repository = [ "https://git.savannah.gnu.org/cgit/${repo}.git/" ];
 } // removeAttrs args [ "repo" "rev" ]) // { inherit rev; }
 )
