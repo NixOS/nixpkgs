f5d6dcb4b9cacefac1bc3679c77af114fa2e24b7
Anderson Torres <torres.anderson.85@protonmail.com>
2024-04-17 20:41:32 -0300

    mednafen: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2748,8 +2748,6 @@ with pkgs;
     description = mame.meta.description + " (tools only)";
   } (lib.getOutput "tools" mame);
 
-  mednafen = callPackage ../applications/emulators/mednafen { };
-
   mednaffe = callPackage ../applications/emulators/mednaffe { };
 
   melonDS = libsForQt5.callPackage ../applications/emulators/melonDS { };
