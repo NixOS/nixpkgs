a9b250db6010e52b96b1dc8a1fc5e6003e937fb6
Fionn Kelleher <fionn@daytona.io>
2024-04-18 18:16:22 +0100

    maintainers: add osslate

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15083,6 +15083,13 @@
     githubId = 104593071;
     name = "Oliver Schmidt";
   };
+  osslate = {
+    email = "fionn@kelleher.email";
+    github = "osslate";
+    githubId = 773673;
+    matrix = "@osslate:fsfe.org";
+    name = "Fionn Kelleher";
+  };
   ostrolucky = {
     email = "gabriel.ostrolucky@gmail.com";
     github = "ostrolucky";
