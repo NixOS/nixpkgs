e7175331939e1c50ff00747e7749c29dcd68f87d
Daniel Baker <daniel.n.baker@gmail.com>
2024-04-17 22:25:24 -0700

    Bumped version to 0.12.0 and disabled broken tests.

--- pkgs/development/python-modules/certomancer/default.nix
+++ pkgs/development/python-modules/certomancer/default.nix
@@ -1,31 +1,32 @@
-{ lib
-, asn1crypto
-, buildPythonPackage
-, click
-, cryptography
-, fetchFromGitHub
-, freezegun
-, jinja2
-, oscrypto
-, pyhanko-certvalidator
-, pytest-aiohttp
-, pytestCheckHook
-, python-dateutil
-, python-pkcs11
-, pythonOlder
-, pytz
-, pyyaml
-, requests
-, requests-mock
-, setuptools
-, tzlocal
-, werkzeug
-, wheel
+{
+  lib,
+  asn1crypto,
+  buildPythonPackage,
+  click,
+  cryptography,
+  fetchFromGitHub,
+  freezegun,
+  jinja2,
+  oscrypto,
+  pyhanko-certvalidator,
+  pytest-aiohttp,
+  pytestCheckHook,
+  python-dateutil,
+  python-pkcs11,
+  pythonOlder,
+  pytz,
+  pyyaml,
+  requests,
+  requests-mock,
+  setuptools,
+  tzlocal,
+  werkzeug,
+  wheel,
 }:
 
 buildPythonPackage rec {
   pname = "certomancer";
-  version = "0.11.0";
+  version = "0.12.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -34,7 +35,7 @@ buildPythonPackage rec {
     owner = "MatthiasValvekens";
     repo = "certomancer";
     rev = "refs/tags/v${version}";
-    hash = "sha256-UQV0Tk4C5b5iBZ34Je59gK2dLTaJusnpxdyNicIh2Q8=";
+    hash = "sha256-c2Fq4YTHQvhxuZrpKQYZvqHIMfubbkeKV4rctELLeJU=";
   };
 
   postPatch = ''
@@ -57,19 +58,13 @@ buildPythonPackage rec {
   ];
 
   passthru.optional-dependencies = {
-    requests-mocker = [
-      requests-mock
-    ];
+    requests-mocker = [ requests-mock ];
     web-api = [
       jinja2
       werkzeug
     ];
-    pkcs12 = [
-      cryptography
-    ];
-    pkcs11 = [
-      python-pkcs11
-    ];
+    pkcs12 = [ cryptography ];
+    pkcs11 = [ python-pkcs11 ];
   };
 
   nativeCheckInputs = [
@@ -84,11 +79,14 @@ buildPythonPackage rec {
   disabledTests = [
     # pyhanko_certvalidator.errors.DisallowedAlgorithmError
     "test_validate"
+    # https://github.com/MatthiasValvekens/certomancer/issues/12
+    "test_keyset_templates_in_arch"
+    "test_crl"
+    "test_aia_ca_issuers"
+    "test_timestamp"
   ];
 
-  pythonImportsCheck = [
-    "certomancer"
-  ];
+  pythonImportsCheck = [ "certomancer" ];
 
   meta = with lib; {
     description = "Quickly construct, mock & deploy PKI test configurations using simple declarative configuration";
