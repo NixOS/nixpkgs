8ef2b87e00c837b21a44144b1479240e871b1d1e
Anderson Torres <torres.anderson.85@protonmail.com>
2024-04-17 21:40:22 -0300

    mednafen-server: refactor
    - finalAttrs
    - strictDeps

--- pkgs/by-name/me/mednafen-server/package.nix
+++ pkgs/by-name/me/mednafen-server/package.nix
@@ -1,22 +1,30 @@
-{ lib, stdenv, fetchurl }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mednafen-server";
   version = "0.5.2";
 
   src = fetchurl {
-    url = "https://mednafen.github.io/releases/files/mednafen-server-${version}.tar.xz";
-    sha256 = "0xm7dj5nwnrsv69r72rcnlw03jm0l8rmrg3s05gjfvxyqmlb36dq";
+    url = "https://mednafen.github.io/releases/files/mednafen-server-${finalAttrs.version}.tar.xz";
+    hash = "sha256-uJmxaMW+bydfAXq8XDOioMoBOLUsi5OT2Tpbbotsp3Y=";
   };
 
-  postInstall = "install -m 644 -Dt $out/share/mednafen-server standard.conf";
+  strictDeps = true;
 
-  meta = with lib; {
+  postInstall = ''
+    install -m 644 -Dt $out/share/mednafen-server standard.conf
+  '';
+
+  meta = {
     description = "Netplay server for Mednafen";
     mainProgram = "mednafen-server";
     homepage = "https://mednafen.github.io/";
-    license = licenses.gpl2;
-    maintainers = with maintainers; [ AndersonTorres ];
-    platforms = platforms.unix;
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.unix;
   };
-}
+})
