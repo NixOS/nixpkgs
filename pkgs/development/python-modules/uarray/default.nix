32fbc63e4bab2748592970d64e08c416d81eff28
Daniel Baker <daniel.n.baker@gmail.com>
2024-04-17 21:57:31 -0700

    Disable uarray for python version 3.12 and above as it is broken and no longer maintained.

--- pkgs/development/python-modules/uarray/default.nix
+++ pkgs/development/python-modules/uarray/default.nix
@@ -1,19 +1,24 @@
-{ lib
-, buildPythonPackage
-, fetchFromGitHub
-, fetchpatch
-, matchpy
-, numpy
-, astunparse
-, typing-extensions
-, pytestCheckHook
-, pytest-cov
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  fetchpatch,
+  matchpy,
+  numpy,
+  astunparse,
+  typing-extensions,
+  pytestCheckHook,
+  pytest-cov,
+  pythonAtLeast,
 }:
 
 buildPythonPackage rec {
   pname = "uarray";
   version = "0.8.2";
   format = "setuptools";
+
+  # broken with Python 3.12; source not updated since Aug 2022
+  disabled = pythonAtLeast "3.12";
 
   src = fetchFromGitHub {
     owner = "Quansight-Labs";
@@ -22,23 +27,38 @@ buildPythonPackage rec {
     sha256 = "1x2jp7w2wmn2awyv05xs0frpq0fa0rprwcxyg72wgiss0bnzxnhm";
   };
 
-  patches = [(
-    # Fixes a compile error with newer versions of GCC -- should be included
-    # in the next release after 0.8.2
-    fetchpatch {
-      url = "https://github.com/Quansight-Labs/uarray/commit/a2012fc7bb94b3773eb402c6fe1ba1a894ea3d18.patch";
-      sha256 = "1qqh407qg5dz6x766mya2bxrk0ffw5h17k478f5kcs53g4dyfc3s";
-    }
-  )];
+  patches = [
+    (
+      # Fixes a compile error with newer versions of GCC -- should be included
+      # in the next release after 0.8.2
+      fetchpatch {
+        url = "https://github.com/Quansight-Labs/uarray/commit/a2012fc7bb94b3773eb402c6fe1ba1a894ea3d18.patch";
+        sha256 = "1qqh407qg5dz6x766mya2bxrk0ffw5h17k478f5kcs53g4dyfc3s";
+      }
+    )
+  ];
 
-  nativeCheckInputs = [ pytestCheckHook pytest-cov ];
-  propagatedBuildInputs = [ matchpy numpy astunparse typing-extensions ];
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-cov
+  ];
+  propagatedBuildInputs = [
+    matchpy
+    numpy
+    astunparse
+    typing-extensions
+  ];
 
   # Tests must be run from outside the source directory
   preCheck = ''
     cd $TMP
   '';
-  pytestFlagsArray = ["--pyargs" "uarray" "-W" "ignore::pytest.PytestRemovedIn8Warning" ];
+  pytestFlagsArray = [
+    "--pyargs"
+    "uarray"
+    "-W"
+    "ignore::pytest.PytestRemovedIn8Warning"
+  ];
   pythonImportsCheck = [ "uarray" ];
 
   meta = with lib; {
