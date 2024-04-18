1a2e7b5dbbc3dd51da696985eed48063c95372f9
Sergei Trofimovich <slyich@gmail.com>
2024-04-18 21:55:31 +0100

    tracker: pull upstream fix for sqlite-3.45.3 compatibility

--- pkgs/development/libraries/tracker/default.nix
+++ pkgs/development/libraries/tracker/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchurl
+, fetchpatch
 , gettext
 , meson
 , mesonEmulatorHook
@@ -41,6 +42,17 @@ stdenv.mkDerivation (finalAttrs: {
     url = with finalAttrs; "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
     sha256 = "Ulks/hm6/9FtvkdHW+fadQ29C2Mz/XrLYPqp2lvEDfI=";
   };
+
+  patches = [
+    # sqlite-3.45.3 compatibility upstream fix:
+    #   https://gitlab.gnome.org/GNOME/tracker/-/merge_requests/665
+    # TODO: remove with a next release
+    (fetchpatch {
+      name = "sqlite-3.45.3.patch";
+      url = "https://gitlab.gnome.org/GNOME/tracker/-/commit/0c576af6df5af2f1b8df9841fbb566fa52a4e382.patch";
+      hash = "sha256-NYZ6xPbmkO283eboRl205OQ0C7NpL/zd2RcWGsMZfrc=";
+    })
+  ];
 
   strictDeps = true;
 
