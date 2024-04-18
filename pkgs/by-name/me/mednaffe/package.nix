8a065e47ddf2119b1beaeab81982eac949f1008c
Anderson Torres <torres.anderson.85@protonmail.com>
2024-04-17 21:55:46 -0300

    mednaffe: refactor
    - finalAttrs
    - strictDeps

--- pkgs/by-name/me/mednaffe/package.nix
+++ pkgs/by-name/me/mednaffe/package.nix
@@ -1,29 +1,39 @@
-{ stdenv
-, lib
-, fetchFromGitHub
-, autoreconfHook
-, pkg-config
-, mednafen
-, gtk3
-, wrapGAppsHook
+{
+  lib,
+  autoreconfHook,
+  fetchFromGitHub,
+  gtk3,
+  mednafen,
+  pkg-config,
+  stdenv,
+  wrapGAppsHook,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mednaffe";
   version = "0.9.2";
 
   src = fetchFromGitHub {
     owner = "AmatCoder";
     repo = "mednaffe";
-    rev = version;
-    sha256 = "sha256-zvSAt6CMcgdoPpTTA5sPlQaWUw9LUMsR2Xg9jM2UaWY=";
+    rev = finalAttrs.version;
+    hash = "sha256-zvSAt6CMcgdoPpTTA5sPlQaWUw9LUMsR2Xg9jM2UaWY=";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkg-config wrapGAppsHook ];
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    wrapGAppsHook
+  ];
 
-  buildInputs = [ gtk3 mednafen ];
+  buildInputs = [
+    gtk3
+    mednafen
+  ];
 
   enableParallelBuilding = true;
+
+  strictDeps = true;
 
   preFixup = ''
     gappsWrapperArgs+=(
@@ -31,12 +41,12 @@ stdenv.mkDerivation rec {
     )
    '';
 
-  meta = with lib; {
+  meta = {
     description = "GTK-based frontend for mednafen emulator";
     mainProgram = "mednaffe";
     homepage = "https://github.com/AmatCoder/mednaffe";
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ sheenobu yana AndersonTorres ];
-    platforms = platforms.unix;
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.unix;
   };
-}
+})
