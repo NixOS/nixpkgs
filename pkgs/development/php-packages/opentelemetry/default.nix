NixOS/nixpkgs

commit a399db43527b481d2af275ba058a4dc1d36ed373

php81Extensions.opentelemetry: 1.0.1 -> 1.0.2

pkgs/development/php-packages/opentelemetry/default.nix
@@ -1,7 +1,7 @@
 { lib, buildPecl, fetchFromGitHub }:
 
 let
-  version = "1.0.1";
+  version = "1.0.2";
 in buildPecl rec {
   inherit version;
   pname = "opentelemetry";
@@ -10,7 +10,7 @@ in buildPecl rec {
     owner = "open-telemetry";
     repo = "opentelemetry-php-instrumentation";
     rev = version;
-    hash = "sha256-VHUzRhTtHygHoW+poItaphV+mxe4rmmSfGgesUgPz8Q=";
+    hash = "sha256-w6Gkjh9+I6KlQyztv4o1XZ6nJ+Yn4wXXN6nma8/dLTU=";
   };
 
   sourceRoot = "${src.name}/ext";

commit 93a10e1b2d0282ae9b2c2203ce38b9fb2845010a

phpPackages.opentelemetry: fix builds on Darwin
This should fix https://github.com/NixOS/nixpkgs/issues/304809
Issue introduced in https://github.com/NixOS/nixpkgs/pull/303437/commits/a399db43527b481d2af275ba058a4dc1d36ed373

pkgs/development/php-packages/opentelemetry/default.nix
@@ -15,6 +15,8 @@ in buildPecl rec {
 
   sourceRoot = "${src.name}/ext";
 
+  env.NIX_CFLAGS_COMPILE = "-Wno-parentheses-equality";
+
   doCheck = true;
 
   meta = with lib; {