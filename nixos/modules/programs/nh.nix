d3b627a443f43f77d5e2165c70818c12be5031af
PerchunPak <git@perchun.it>
2024-04-18 09:38:23 +0200

    nixos/nh: coerce flake option to string

--- nixos/modules/programs/nh.nix
+++ nixos/modules/programs/nh.nix
@@ -17,6 +17,7 @@ in
     flake = lib.mkOption {
       type = lib.types.nullOr lib.types.path;
       default = null;
+      apply = toString;
       description = ''
         The path that will be used for the `FLAKE` environment variable.
 
