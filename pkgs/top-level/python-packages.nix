9db2f4f7c8bbcfeec1440fd3b4310c216d239924
Laurent Arnoud <laurent.arnoud@platform.sh>
2024-04-19 16:39:13 +0200

    python313Packages.blackfire: init at 1.20.26

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1880,6 +1880,8 @@ self: super: with self; {
 
   blacken-docs = callPackage ../development/python-modules/blacken-docs { };
 
+  blackfire = callPackage ../development/python-modules/blackfire { };
+
   blackjax = callPackage ../development/python-modules/blackjax { };
 
   blackrenderer = callPackage ../development/python-modules/blackrenderer { };
