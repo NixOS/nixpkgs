4d207ab9b8bf74cc3edecdb611e7f0a686a9d43c
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-19 03:08:52 +0000

    python312Packages.green: 4.0.1 -> 4.0.2

--- pkgs/development/python-modules/green/default.nix
+++ pkgs/development/python-modules/green/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "green";
-  version = "4.0.1";
+  version = "4.0.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-O178HRtyKg/2fYD9jHzfbUfNGPlRpGuEvbx7H7yr0/w=";
+    hash = "sha256-pAZ8P5/CpkTtNfU2ZJUGQzROxGLm0uu1vXS3YpcVprE=";
   };
 
   patches = [
