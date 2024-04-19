1d91b59670d5a9785c87a4e63a19695727166598
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-04-19 21:47:02 +0530

    rnnoise: 2021-01-22 -> 0.2 new external dependency added - model model_version can be found from https://gitlab.xiph.org/xiph/rnnoise/-/raw/v${finalAttrs.version}/model_version

--- pkgs/development/libraries/rnnoise/default.nix
+++ pkgs/development/libraries/rnnoise/default.nix
@@ -1,15 +1,27 @@
-{ stdenv, lib, fetchFromGitHub, autoreconfHook }:
+{ stdenv, lib, fetchFromGitLab, fetchurl, autoreconfHook }:
 
-stdenv.mkDerivation (rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "rnnoise";
-  version = "2021-01-22";
+  version = "0.2";
 
-  src = fetchFromGitHub {
+  src = fetchFromGitLab {
+    domain = "gitlab.xiph.org";
     owner = "xiph";
     repo = "rnnoise";
-    rev = "1cbdbcf1283499bbb2230a6b0f126eb9b236defd";
-    sha256 = "1y0rzgmvy8bf9a431garpm2w177s6ajgf79y5ymw4yb0pik57rwb";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-Qaf+0iOprq7ILRWNRkBjsniByctRa/lFVqiU5ZInF/Q=";
   };
+
+  # Copy from https://gitlab.xiph.org/xiph/rnnoise/-/raw/v${finalAttrs.version}/model_version
+  model_version = "0b50c45";
+  model = fetchurl {
+    url = "https://media.xiph.org/rnnoise/models/rnnoise_data-${finalAttrs.model_version}.tar.gz";
+    hash = "sha256-SsgcXAiE7EvVkHAmqq4WIJt7ds2df3GvWCCUovmPS0M=";
+  };
+
+  patchPhase = ''
+    tar xvomf ${finalAttrs.model}
+  '';
 
   nativeBuildInputs = [ autoreconfHook ];
 
@@ -17,12 +29,12 @@ stdenv.mkDerivation (rec {
     install -Dt $out/bin examples/.libs/rnnoise_demo
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Recurrent neural network for audio noise reduction";
     homepage = "https://people.xiph.org/~jm/demo/rnnoise/";
-    license = licenses.bsd3;
-    maintainers = [ maintainers.nh2 ];
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ nh2 ];
     mainProgram = "rnnoise_demo";
-    platforms = platforms.all;
+    platforms = lib.platforms.all;
   };
 })
