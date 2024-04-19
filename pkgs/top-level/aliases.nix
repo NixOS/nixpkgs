74e7dfcd0bc29ab45fe77a7ae42ec140eee0e4d1
Mrinal Purohit <github@mrinalpurohit.in>
2024-04-19 10:48:44 +0200

    authy: remove
    desktop apps reached end of life on March 19, 2024
    https://web.archive.org/web/20240314234911/https://help.twilio.com/articles/19753631228315

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -92,6 +92,7 @@ mapAliases ({
   atomEnv = throw "'atomEnv' has been removed because 'atom' is discontinued and deprecated. Consider using 'pulsar', a maintained fork"; # Added 2023-10-01
   atomPackages = throw "'atomPackages' has been removed because 'atom' is discontinued and deprecated. Consider using 'pulsar', a maintained fork"; # Added 2023-10-01
   auditBlasHook = throw "'auditBlasHook' has been removed since it never worked"; # Added 2024-04-02
+  authy = throw "'authy' has been removed since it reached end of life"; # Added 2024-04-19
   avldrums-lv2 = x42-avldrums; # Added 2020-03-29
   awesome-4-0 = awesome; # Added 2022-05-05
 
