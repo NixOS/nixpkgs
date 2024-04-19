5efa13b4a40d236a3b39d82b60c250b82112c062
shivaraj-bh <sbh69840@gmail.com>
2024-04-19 09:11:57 +0530

    nixci: 0.2.0 -> 0.4.0; fix Darwin build
    https://github.com/srid/nixci/releases/tag/0.4.0
    
    Add SystemConfiguration and IOKit from apple_sdk to fix the darwin build failure

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -40055,7 +40055,7 @@ with pkgs;
   alejandra = callPackage ../tools/nix/alejandra { };
 
   nixci = callPackage ../tools/nix/nixci {
-    inherit (darwin.apple_sdk.frameworks) Security;
+    inherit (darwin.apple_sdk.frameworks) Security SystemConfiguration IOKit;
   };
 
   nixfmt-classic = haskellPackages.nixfmt.bin;
