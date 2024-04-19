a67771aa1a503884a16775d7526c368f286f8188
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-19 16:18:41 +0000

    pwdsafety: 0.3 -> 0.4.0

--- pkgs/tools/security/pwdsafety/default.nix
+++ pkgs/tools/security/pwdsafety/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "pwdsafety";
-  version = "0.3";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "edoardottt";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-ryMLiehJVZhQ3ZQf4/g7ILeJri78A6z5jfell0pD9E8=";
+    hash = "sha256-cKxTcfNjvwcDEw0Z1b50A4u0DUYXlGMMfGWJLPaSkcw=";
   };
 
-  vendorHash = "sha256-b+tWTQUyYDzY2O28hwy5vI6b6S889TCiVh7hQhw/KAc=";
+  vendorHash = "sha256-RoRq9JZ8lOMtAluz8TB2RRuDEWFOBtWVhz21aTkXXy4=";
 
   meta = with lib; {
     description = "Command line tool checking password safety";
