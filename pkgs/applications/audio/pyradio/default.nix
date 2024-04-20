ab9bb53418734a49c2298ab85fa145819f1efd32
Yaya <github@uwu.is>
2024-04-20 13:27:47 +0200

    pyradio: 0.9.3.2 -> 0.9.3.3
    Diff: https://github.com/coderholic/pyradio/compare/refs/tags/0.9.3.2...0.9.3.3
    
    Changelog: https://github.com/coderholic/pyradio/releases/tag/0.9.3.3

--- pkgs/applications/audio/pyradio/default.nix
+++ pkgs/applications/audio/pyradio/default.nix
@@ -6,13 +6,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "pyradio";
-  version = "0.9.3.2";
+  version = "0.9.3.3";
 
   src = fetchFromGitHub {
     owner = "coderholic";
     repo = "pyradio";
     rev = "refs/tags/${version}";
-    hash = "sha256-aIWU68bdPUsIh8QRNnF0NcK7FemmYyUHbJg9KcUALBk=";
+    hash = "sha256-JKgUy0Dtp0DGPu9NbWzuoJ7U0JeqQ/PxocMVF0nFze0=";
   };
 
   nativeBuildInputs = [
