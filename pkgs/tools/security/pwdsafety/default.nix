4fb51d082bfd75068231f8881219c9e6f23ad246
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-20 11:35:32 +0200

    pwdsafety: refactor

--- pkgs/tools/security/pwdsafety/default.nix
+++ pkgs/tools/security/pwdsafety/default.nix
@@ -9,18 +9,24 @@ buildGoModule rec {
 
   src = fetchFromGitHub {
     owner = "edoardottt";
-    repo = pname;
-    rev = "v${version}";
+    repo = "pwdsafety";
+    rev = "refs/tags/v${version}";
     hash = "sha256-cKxTcfNjvwcDEw0Z1b50A4u0DUYXlGMMfGWJLPaSkcw=";
   };
 
   vendorHash = "sha256-RoRq9JZ8lOMtAluz8TB2RRuDEWFOBtWVhz21aTkXXy4=";
 
+  ldflags = [
+    "-w"
+    "-s"
+  ];
+
   meta = with lib; {
     description = "Command line tool checking password safety";
-    mainProgram = "cmd";
     homepage = "https://github.com/edoardottt/pwdsafety";
+    changelog = "https://github.com/edoardottt/pwdsafety/releases/tag/v${version}";
     license = with licenses; [ gpl3Plus ];
     maintainers = with maintainers; [ fab ];
+    mainProgram = "pwdsafety";
   };
 }
