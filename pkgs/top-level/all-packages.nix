c00d9adbaf767943d41b7b1471fbf1f1962b283b
Yueh-Shun Li <shamrocklee@posteo.net>
2024-04-21 20:36:56 +0800

    argc: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4183,8 +4183,6 @@ with pkgs;
 
   aptdec = callPackage ../development/libraries/aptdec { };
 
-  argc = callPackage ../development/tools/argc { };
-
   aria2 = callPackage ../tools/networking/aria2 {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
