e3e0865670f36ee0e7803ee783a390d19649b80f
Naxdy <naxdy@naxdy.org>
2024-04-21 10:11:39 +0200

    kdePackages/kweather: format

--- pkgs/kde/gear/kweather/default.nix
+++ pkgs/kde/gear/kweather/default.nix
@@ -1,14 +1,14 @@
-{
-  mkKdeDerivation,
-  qtsvg,
-  qtcharts,
-  qqc2-desktop-style,
-  kholidays,
-  python3,
+{ mkKdeDerivation
+, qtsvg
+, qtcharts
+, qqc2-desktop-style
+, kholidays
+, python3
+,
 }:
 mkKdeDerivation {
   pname = "kweather";
 
-  extraBuildInputs = [qtsvg qtcharts qqc2-desktop-style kholidays python3];
+  extraBuildInputs = [ qtsvg qtcharts qqc2-desktop-style kholidays python3 ];
   meta.mainProgram = "kweather";
 }
