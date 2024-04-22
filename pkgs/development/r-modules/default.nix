c15ac8a50b9a8566356aac7c59a0719965cb785d
Bruno Rodrigues <bruno@brodrigues.co>
2024-04-22 20:47:47 +0200

    rPackages.zoomerjoin: fixed build

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -1116,6 +1116,11 @@ let
       postPatch = "patchShebangs configure";
     });
 
+    zoomerjoin = old.zoomerjoin.overrideAttrs (attrs: {
+      nativeBuildInputs = [ pkgs.cargo ] ++ attrs.nativeBuildInputs;
+      postPatch = "patchShebangs configure";
+    });
+
     data_table = old.data_table.overrideAttrs (attrs: {
       env = (attrs.env or { }) // {
         NIX_CFLAGS_COMPILE = attrs.env.NIX_CFLAGS_COMPILE + " -fopenmp";
