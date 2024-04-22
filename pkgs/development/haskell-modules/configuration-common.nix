c4192037f7febee4b23b6aefc7a81f9f35189cb9
maralorn <mail@maralorn.de>
2024-04-23 00:24:08 +0200

    haskellPackages.jsaddle-dom: Fix build

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1594,6 +1594,7 @@ self: super: {
   jsaddle-dom = overrideCabal (old: {
     postPatch = old.postPatch or "" + ''
       sed -i 's/lens.*4.20/lens/' jsaddle-dom.cabal
+      rm Setup.hs
     '';
   }) (doJailbreak super.jsaddle-dom);
 
