7aa7920fb0439c3b9c13a2d39f449beee9c7c9c8
Anders Johan Jamtli <anders@jamtli.no>
2024-04-22 05:49:11 +0200

    Revert "nixos/qemu-vm: set security model for 'xchg' directory to 'none'"
    This reverts commit c16ff7f9f3f15723f77d4db13ef6ad6bb7619933.

--- nixos/modules/virtualisation/qemu-vm.nix
+++ nixos/modules/virtualisation/qemu-vm.nix
@@ -1108,7 +1108,6 @@ in
       xchg = {
         source = ''"$TMPDIR"/xchg'';
         securityModel = "none";
-        securityModel = "none";
         target = "/tmp/xchg";
       };
       shared = {
