3e5645a2a1082b36e57c4688a7436c383c751456
Jamie Magee <jamie.magee@gmail.com>
2024-04-21 21:19:04 -0700

    gptscript: 0.1.1 -> 0.5.0

--- pkgs/by-name/gp/gptscript/package.nix
+++ pkgs/by-name/gp/gptscript/package.nix
@@ -5,22 +5,21 @@
 }:
 buildGo122Module rec {
   pname = "gptscript";
-  version = "0.1.1";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "gptscript-ai";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-zG75L10WvfkmjwW3ifBHaTkHNXqXvNO0PaXejCc2tls=";
+    hash = "sha256-teZQhGYKJ5Ma5by3Wug5B1hAV1tox94MF586ZeEXp6o=";
   };
 
-  vendorHash = "sha256-LV9uLLwdtLJTIxaBB1Jew92S0QjQsceyLEfSrDeDnR4=";
+  vendorHash = "sha256-0irUcEomQzo9+vFJEk28apLNuJdsX1RHEqB7T88X7Ks=";
 
   ldflags = [
     "-s"
     "-w"
-    "-X main.Version=${version}"
-    "-X main.Commit=${version}"
+    "-X github.com/gptscript-ai/gptscript/pkg/version.Tag=v${version}"
   ];
 
   # Requires network access
@@ -30,8 +29,8 @@ buildGo122Module rec {
     homepage = "https://gptscript.ai";
     changelog = "https://github.com/gptscript-ai/gptscript/releases/tag/v{version}";
     description = "Natural Language Programming";
-    license = with licenses; [asl20];
-    maintainers = with maintainers; [jamiemagee];
+    license = with licenses; [ asl20 ];
+    maintainers = with maintainers; [ jamiemagee ];
     mainProgram = "gptscript";
   };
 }
