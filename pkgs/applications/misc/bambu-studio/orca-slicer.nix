2715a8ad3a6d3237e1658340d4fb5758e76d0544
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-04-24 06:46:15 +1000

    orca-slicer: move LICENSE to prevent collision

--- pkgs/applications/misc/bambu-studio/orca-slicer.nix
+++ pkgs/applications/misc/bambu-studio/orca-slicer.nix
@@ -19,6 +19,12 @@ bambu-studio.overrideAttrs (
       hash = "sha256-YlLDUH3ODIfax5QwnsVJi1JjZ9WtxP3ssqRP1C4d4bw=";
     };
 
+    # needed to prevent collisions between the LICENSE.txt files of
+    # bambu-studio and orca-slicer.
+    postInstall = ''
+      mv $out/LICENSE.txt $out/share/OrcaSlicer/LICENSE.txt
+    '';
+
     meta = with lib; {
       description = "G-code generator for 3D printers (Bambu, Prusa, Voron, VzBot, RatRig, Creality, etc";
       homepage = "https://github.com/SoftFever/OrcaSlicer";
