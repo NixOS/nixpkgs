c2cd826e236d7a25864e616844667e39e915146f
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-23 08:25:16 +0200

    python312Packages.llama-index-vector-stores-qdrant: 0.2.1 -> 0.2.3

--- pkgs/development/python-modules/llama-index-vector-stores-qdrant/default.nix
+++ pkgs/development/python-modules/llama-index-vector-stores-qdrant/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-vector-stores-qdrant";
-  version = "0.2.1";
+  version = "0.2.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_index_vector_stores_qdrant";
     inherit version;
-    hash = "sha256-begHJBxdu+19LIoNgAd3Gnei2TQqpEU3gd6cVrv0zGw=";
+    hash = "sha256-udOdZZZXLBDYwumcBeWuBT8Kd03KpBCwyEekmD0Ul/g=";
   };
 
   build-system = [ poetry-core ];
