3a36b4556e688f22e26d3d85f80f3ab63bdc6371
Mario Rodas <marsam@users.noreply.github.com>
2024-04-23 04:20:00 +0000

    dune_3: 3.15.1 -> 3.15.2
    Diff: https://github.com/ocaml/dune/compare/3.15.1...3.15.2
    
    Changelog: https://github.com/ocaml/dune/raw/3.15.2/CHANGES.md

--- pkgs/development/tools/ocaml/dune/3.nix
+++ pkgs/development/tools/ocaml/dune/3.nix
@@ -6,11 +6,11 @@ else
 
 stdenv.mkDerivation rec {
   pname = "dune";
-  version = "3.15.1";
+  version = "3.15.2";
 
   src = fetchurl {
     url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-    hash = "sha256-tbeKSgLU3QiUI02/cYv/R3/Nheff3/6OzgC5oM89VHs=";
+    hash = "sha256-+VmYBULKhZCbPz+Om+ZcK4o3XzpOO9g8etegfy4HeTM=";
   };
 
   nativeBuildInputs = [ ocaml findlib ];
