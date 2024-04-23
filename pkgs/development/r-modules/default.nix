ef043217799321bf50dd72c3f34e0055e5a82614
Bruno Rodrigues <bruno@brodrigues.co>
2024-04-23 11:38:00 +0200

    rPackages.registr: fixed build

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -556,6 +556,7 @@ let
     RMark = [ pkgs.which ];
     RPushbullet = [ pkgs.which ];
     stpphawkes = [ pkgs.gsl ];
+    registr = with pkgs; [ icu.dev zlib.dev bzip2.dev xz.dev ];
     RCurl = [ pkgs.curl.dev ];
     R2SWF = [ pkgs.pkg-config ];
     rDEA = [ pkgs.glpk ];
