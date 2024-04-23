f104d62b274da7e5f9296b439fa50a012ecfc3f2
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2024-04-24 00:17:31 +0200

    azure-cli: Add extension azure-devops

--- pkgs/tools/admin/azure-cli/extensions-generated.nix
+++ pkgs/tools/admin/azure-cli/extensions-generated.nix
@@ -132,6 +132,13 @@
     sha256 = "1337a8bf90739df646231533801cce9050bad641c627382468c86af5f2f99d89";
     description = "Microsoft Azure Command-Line Tools AutomationClient Extension";
   };
+  azure-devops = mkAzExtension rec {
+    pname = "azure-devops";
+    version = "1.0.0";
+    url = "https://github.com/Azure/azure-devops-cli-extension/releases/download/20240206.1/azure_devops-${version}-py2.py3-none-any.whl";
+    sha256 = "658a2854d8c80f874f9382d421fa45abf6a38d00334737dda006f8dec64cf70a";
+    description = "Tools for managing Azure DevOps";
+  };
   azure-firewall = mkAzExtension rec {
     pname = "azure-firewall";
     version = "1.0.1";
