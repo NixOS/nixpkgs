0d9236cfcbfc3ed62e7398525a5b3cd27fa421c6
Carl Hjerpe <git@hjerpe.xyz>
2024-04-23 15:21:30 +0200

    franz+derivatives: Implement NIXOS_OZONE_WL
    Reverts back to using wrapProgramShell rather than wrapProgramBinary
    Adds "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations" when NIXOS_OZONE_WL is set

--- pkgs/applications/networking/instant-messengers/franz/generic.nix
+++ pkgs/applications/networking/instant-messengers/franz/generic.nix
@@ -87,10 +87,11 @@ in stdenv.mkDerivation (rec {
   dontWrapGApps = true;
 
   postFixup = ''
-    # make xdg-open overrideable at runtime
-    wrapProgram $out/opt/${name}/${pname} \
+    # make xdg-open overridable at runtime
+    wrapProgramShell $out/opt/${name}/${pname} \
       --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeDependencies}" \
       --suffix PATH : ${xdg-utils}/bin \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
       "''${gappsWrapperArgs[@]}"
   '';
 } // cleanedArgs)
