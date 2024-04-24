a221e6c225dc69824a5e9021af38a3ddb272f360
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-24 01:12:14 +0000

    git-extras: 7.1.0 -> 7.2.0

--- pkgs/applications/version-management/git-extras/default.nix
+++ pkgs/applications/version-management/git-extras/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "git-extras";
-  version = "7.1.0";
+  version = "7.2.0";
 
   src = fetchFromGitHub {
     owner = "tj";
     repo = "git-extras";
     rev = version;
-    sha256 = "sha256-e1UUAHXTRNgNWrYZuLGdrQIAD8WADmA2B9bVnDNidf8=";
+    sha256 = "sha256-Zzk133ZJv91zbFz0U9tiv8pb0pAIBt9Y/8PYIaABMGc=";
   };
 
   postPatch = ''
