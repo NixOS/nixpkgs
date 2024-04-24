fba6fde650c1147c6147a625d544338c525a2193
Karel Kočí <cynerd@email.cz>
2024-04-24 21:46:33 +0200

    pkgsForCross: add a way to create custom cross system package set
    Package set pkgsCross provides a lot of platforms but still that is not
    enough. My use case is for armv7e-m but instead of adding that this
    might be more generic approach. The less used platforms can be just
    simply declared on demand.

--- pkgs/top-level/stage.nix
+++ pkgs/top-level/stage.nix
@@ -190,6 +190,9 @@ let
                               nixpkgsFun { inherit crossSystem; })
                               lib.systems.examples;
 
+    # Like pkgsCross but allows caller to specify custom crossSystem.
+    pkgsForCross = crossSystem: nixpkgsFun { inherit crossSystem; };
+
     pkgsLLVM = nixpkgsFun {
       overlays = [
         (self': super': {
