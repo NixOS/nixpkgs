a8a03314f7cb56c248989b3bbe8836e2054dc3bd
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-04-24 12:36:21 +0200

    nixos/evcc: restart on failure
    (cherry picked from commit cfa475854a7c4aa1b7b5e4d90990c81583186c99)

--- nixos/modules/services/home-automation/evcc.nix
+++ nixos/modules/services/home-automation/evcc.nix
@@ -62,6 +62,7 @@ in
         DynamicUser = true;
         LockPersonality = true;
         MemoryDenyWriteExecute = true;
+        Restart = "on-failure";
         RestrictAddressFamilies = [
           "AF_INET"
           "AF_INET6"
