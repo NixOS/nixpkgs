d5f0a46b0c8b85ae1e28088e9e885aac1f33ff41
Guanpeng Xu <herberteuler@gmail.com>
2024-04-24 22:33:14 -0700

    jetbrains: respect boot java runtime settings

--- pkgs/applications/editors/jetbrains/bin/linux.nix
+++ pkgs/applications/editors/jetbrains/bin/linux.nix
@@ -102,6 +102,13 @@ with stdenv; lib.makeOverridable mkDerivation (rec {
     jdk=${jdk.home}
     item=${desktopItem}
 
+    BOOT_JDK_BASE_NAME=$(grep '\.jdk' "$out/$pname/bin/${loName}.sh" \
+                         | grep '^if' \
+                         | head -n 1 \
+                         | sed -e 's|\(.*\)".*|\1|' \
+                               -e 's|.*"||' \
+                               -e 's|.*/\(.*/.*\)|\1|')
+    BOOT_JDK_NAME="\$HOME/.config/JetBrains/$BOOT_JDK_BASE_NAME"
     wrapProgram  "$out/$pname/bin/${loName}.sh" \
       --prefix PATH : "${lib.makeBinPath [ jdk coreutils gnugrep which git ]}" \
       --suffix PATH : "${lib.makeBinPath [ python3 ]}" \
@@ -113,6 +120,18 @@ with stdenv; lib.makeOverridable mkDerivation (rec {
       --set-default JETBRAINSCLIENT_JDK "$jdk" \
       --set-default ${hiName}_JDK "$jdk" \
       --set-default ${hiName}_VM_OPTIONS ${vmoptsFile}
+    sed -i -e '/^export ${hiName}_JDK=/afi' \
+        -e "/^export ${hiName}_JDK=/iif [[ ! -s \"$BOOT_JDK_NAME\" ]]; then" \
+        -e 's|^export ${hiName}_JDK\(.*\)|    export ${hiName}_JDK\1|' \
+        "$out/$pname/bin/${loName}.sh"
+    sed -i -e '/^export ${hiName}_VM_OPTIONS=/afi' \
+        -e "/^export ${hiName}_VM_OPTIONS=/iVMOPTS_STR=\"\$(dirname $BOOT_JDK_NAME)/*.vmoptions\"\\
+    VMOPTS=\"\$(echo \$VMOPTS_STR)\"\\
+    if [[ -s \"\$VMOPTS\" ]]; then\\
+        export ${hiName}_VM_OPTIONS=\$(cat \"\$VMOPTS\")\\
+    else" \
+        -e 's|^export ${hiName}_VM_OPTIONS\(.*\)|    export ${hiName}_VM_OPTIONS\1|' \
+        "$out/$pname/bin/${loName}.sh"
 
     ln -s "$out/$pname/bin/${loName}.sh" $out/bin/$pname
     rm -rf $out/$pname/plugins/remote-dev-server/selfcontained/
