2416df82e4419ab146b45b695dd71793e0d1877d
elcuervo <elcuervo@elcuervo.net>
2024-04-25 14:39:12 -0300

    ruby_3_2: 3.2.3 -> 3.2.4
    (cherry picked from commit 6663dbfa1b55f8773f2ae4147e14b1e872d723b2)

--- pkgs/development/interpreters/ruby/default.nix
+++ pkgs/development/interpreters/ruby/default.nix
@@ -327,8 +327,8 @@ in {
   };
 
   ruby_3_2 = generic {
-    version = rubyVersion "3" "2" "3" "";
-    sha256 = "sha256-r38XV9ndtjA0WYgTkhHx/VcP9bqDDe8cx8Rorptlybo=";
+    version = rubyVersion "3" "2" "4" "";
+    sha256 = "sha256-xys8XDBILcoYsPhoyQdfP0fYFo6vYm1OaCzltZyFhpI=";
     cargoSha256 = "sha256-6du7RJo0DH+eYMOoh3L31F3aqfR5+iG1iKauSV1uNcQ=";
   };
 
