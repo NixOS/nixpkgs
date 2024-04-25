4793074c8e6f1e8aed2cbc4c7f872c80b0349042
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-25 02:54:00 +0000

    python311Packages.flake8-bugbear: 24.2.6 -> 24.4.21

--- pkgs/development/python-modules/flake8-bugbear/default.nix
+++ pkgs/development/python-modules/flake8-bugbear/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "flake8-bugbear";
-  version = "24.2.6";
+  version = "24.4.21";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "PyCQA";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-9GuHgRCwHD7YP0XdoFip9rWyPtZtVme+c+nHjvBrB8k=";
+    hash = "sha256-6jKVKVJuNpdKLzl2dTkr1cvArGWCWvuhyjww05r9W/c=";
   };
 
   propagatedBuildInputs = [
