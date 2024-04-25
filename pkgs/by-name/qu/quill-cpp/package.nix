6891e9b0348673a160242e821f5f5feeb413aef0
Vaci <vaci@vaci.org>
2024-04-25 20:03:45 +0100

    quill-cpp: init at 3.8.0

---
+++ pkgs/by-name/qu/quill-cpp/package.nix
@@ -0,0 +1,33 @@
+{
+  cmake,
+  fetchFromGitHub,
+  lib,
+  stdenv
+}:
+
+stdenv.mkDerivation rec {
+  pname = "quill-cpp";
+  version = "3.8.0";
+
+  src = fetchFromGitHub {
+    owner = "odygrd";
+    repo = "quill";
+    rev = "v${version}";
+    sha256 = "sha256-8AS/1Yb8sHO/0wpjNZk/b18QtDM2tR7+DFnClFeMMPI=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  meta = with lib; {
+    description = "A high-performance, cross-platform logging library";
+    homepage = "https://github.com/odygrd/quill";
+    license = licenses.mit;
+    maintainers = [ maintainers.vaci ];
+    sourceProvenance = [
+      sourceTypes.fromSource
+    ];
+  };
+}
+
