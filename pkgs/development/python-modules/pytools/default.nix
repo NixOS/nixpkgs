cb27658a8c23b5d962898c00b5b2010b31f39783
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-25 18:33:56 +0000

    python311Packages.pytools: 2024.1.1 -> 2024.1.2

--- pkgs/development/python-modules/pytools/default.nix
+++ pkgs/development/python-modules/pytools/default.nix
@@ -11,13 +11,13 @@
 
 buildPythonPackage rec {
   pname = "pytools";
-  version = "2024.1.1";
+  version = "2024.1.2";
   format = "setuptools";
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-LIjt+pkMjjJRZ8N2WfseEKPBEz3691K719hFZAK43P8=";
+    hash = "sha256-CBhx5FFQXEuYbrr6aK7qv9x76z+qG6pQ9yauviHh0Fc=";
   };
 
   propagatedBuildInputs = [
