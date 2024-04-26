aebfdb30b437453f6cee8acdd55f04e4b349d193
Albert Safin <xzfcpw@gmail.com>
2024-04-26 20:07:19 +0000

    nixos/vtune: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -286,6 +286,7 @@
   ./programs/usbtop.nix
   ./programs/vim.nix
   ./programs/virt-manager.nix
+  ./programs/vtune.nix
   ./programs/wavemon.nix
   ./programs/wayland/cardboard.nix
   ./programs/wayland/hyprland.nix
