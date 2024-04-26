26dc08626bcef7802e58d4e1d50b99ab71f1a60d
Albert Safin <xzfcpw@gmail.com>
2024-04-26 20:05:57 +0000

    intel-vtune-sepdk: init

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -392,6 +392,8 @@ in {
 
     intel-speed-select = if lib.versionAtLeast kernel.version "5.3" then callPackage ../os-specific/linux/intel-speed-select { } else null;
 
+    intel-vtune-sepdk = callPackage ../os-specific/linux/intel-vtune-sepdk { };
+
     ipu6-drivers = callPackage ../os-specific/linux/ipu6-drivers {};
 
     ivsc-driver = callPackage ../os-specific/linux/ivsc-driver {};
