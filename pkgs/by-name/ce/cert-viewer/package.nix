NixOS/nixpkgs

commit 97faa99d9df74709182f35bb4c86c5ad21eb23c6

cert-viewer: install manual page

pkgs/by-name/ce/cert-viewer/package.nix
@@ -1,6 +1,7 @@
 { buildGoModule
 , fetchFromGitHub
 , lib
+, installShellFiles
 }:
 
 buildGoModule rec {
@@ -15,6 +16,15 @@ buildGoModule rec {
   };
 
   vendorHash = "sha256-jNT04bYH5L/Zcfvel673zr2UJLayCO443tvBGZjrBZk=";
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  postInstall = ''
+    $out/bin/cert-viewer --help-man > cert-viewer.1
+    installManPage cert-viewer.1
+  '';
 
   meta = {
     description = "Admin tool to view and inspect multiple x509 Certificates";

commit 2066dc98960d798ba5089376636085d9c4c660da

cert-viewer: fix cross-compilation
Tested with `nix-build -A pkgsCross.armv7l-hf-multiplatform.cert-viewer`.

Fixes: 97faa99d9df74709 ("cert-viewer: install manual page")

pkgs/by-name/ce/cert-viewer/package.nix
@@ -1,7 +1,9 @@
 { buildGoModule
+, buildPackages
 , fetchFromGitHub
 , lib
 , installShellFiles
+, stdenv
 }:
 
 buildGoModule rec {
@@ -21,10 +23,17 @@ buildGoModule rec {
     installShellFiles
   ];
 
-  postInstall = ''
-    $out/bin/cert-viewer --help-man > cert-viewer.1
-    installManPage cert-viewer.1
-  '';
+  postInstall =
+    let
+      prog =
+        if stdenv.buildPlatform.canExecute stdenv.hostPlatform
+        then "$out/bin/cert-viewer"
+        else lib.getExe buildPackages.cert-viewer;
+    in
+      ''
+        ${prog} --help-man > cert-viewer.1
+        installManPage cert-viewer.1
+      '';
 
   meta = {
     description = "Admin tool to view and inspect multiple x509 Certificates";