fdbde7adb8e27575d449d5447cf8648f4898b9aa
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-26 03:37:21 +0000

    python311Packages.optimum: 1.18.1 -> 1.19.1

--- pkgs/development/python-modules/optimum/default.nix
+++ pkgs/development/python-modules/optimum/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "optimum";
-  version = "1.18.1";
+  version = "1.19.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     owner = "huggingface";
     repo = "optimum";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Y+KWvpd/ULthCOr18hQjP0REQPcs2Ql2aUi3DIKIOpQ=";
+    hash = "sha256-EHu3p3M7xBjMnf3tKsneYOZtd9YWsC0qok1fnk4TMAg=";
   };
 
   propagatedBuildInputs = [
