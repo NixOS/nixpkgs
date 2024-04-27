2234d13cb51fcefd14d76f1ead0c19f944debffb
John Chadwick <john@jchw.io>
2024-04-27 11:25:50 -0400

    flips: unstable-2023-03-15 -> unstable-2024-04-17, add jchw as maintainer

--- pkgs/tools/compression/flips/default.nix
+++ pkgs/tools/compression/flips/default.nix
@@ -1,18 +1,32 @@
-{ lib, stdenv, fetchFromGitHub, gtk3, libdivsufsort, pkg-config, wrapGAppsHook }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  gtk3,
+  libdivsufsort,
+  pkg-config,
+  wrapGAppsHook,
+}:
 
 stdenv.mkDerivation {
   pname = "flips";
-  version = "unstable-2023-03-15";
+  version = "unstable-2024-04-17";
 
   src = fetchFromGitHub {
     owner = "Alcaro";
     repo = "Flips";
-    rev = "fdd5c6e34285beef5b9be759c9b91390df486c66";
-    hash = "sha256-uuHgpt7aWqiMTUILm5tAEGGeZrls3g/DdylYQgsfpTw=";
+    rev = "20b0da9ab95d23da89f821bbddedb11b8e0e6531";
+    hash = "sha256-/i/0FvZqMvG4FFIKaOtUe0A5BfG4NXjpBfLYIQOFky8=";
   };
 
-  nativeBuildInputs = [ pkg-config wrapGAppsHook ];
-  buildInputs = [ gtk3 libdivsufsort ];
+  nativeBuildInputs = [
+    pkg-config
+    wrapGAppsHook
+  ];
+  buildInputs = [
+    gtk3
+    libdivsufsort
+  ];
   patches = [ ./use-system-libdivsufsort.patch ];
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
   buildPhase = ''
@@ -25,7 +39,7 @@ stdenv.mkDerivation {
     description = "A patcher for IPS and BPS files";
     homepage = "https://github.com/Alcaro/Flips";
     license = licenses.gpl3Plus;
-    maintainers = [ ];
+    maintainers = [ maintainers.jchw ];
     platforms = platforms.linux;
     mainProgram = "flips";
   };
