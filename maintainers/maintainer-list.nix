71eb26ef20ad2d6acc4579ce67983e991901ae9a
Torben Schweren <torben@schweren.dev>
2024-04-27 02:08:48 +0200

    maintainers: add Kitt3120

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11860,6 +11860,12 @@
     githubId = 148352;
     name = "Jim Fowler";
   };
+  Kitt3120 = {
+    email = "nixpkgs@schweren.dev";
+    github = "Kitt3120";
+    githubId = 10689811;
+    name = "Torben Schweren";
+  };
   kittywitch = {
     email = "kat@inskip.me";
     github = "kittywitch";
