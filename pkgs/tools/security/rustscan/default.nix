faf0f4fa13c969c9de71642acf56133ad9937107
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-27 23:03:46 +0200

    rustscan: refactor

--- pkgs/tools/security/rustscan/default.nix
+++ pkgs/tools/security/rustscan/default.nix
@@ -1,4 +1,12 @@
-{ lib, rustPlatform, fetchCrate, nmap, stdenv, Security, perl, python3 }:
+{ lib
+, stdenv
+, rustPlatform
+, fetchCrate
+, nmap
+, Security
+, perl
+, python3
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "rustscan";
@@ -6,14 +14,14 @@ rustPlatform.buildRustPackage rec {
 
   src = fetchCrate {
     inherit pname version;
-    sha256 = "sha256-yGVhbI1LivTIQEgqOK59T1+8SiTJBPIdftiXkwE4lZM=";
+    hash = "sha256-yGVhbI1LivTIQEgqOK59T1+8SiTJBPIdftiXkwE4lZM=";
   };
 
-  cargoSha256 = "sha256-UR3ktV80QU0N3f7qmqdhYpc5uwoPq4UvN40zEuMbp+Q=";
+  cargoHash = "sha256-UR3ktV80QU0N3f7qmqdhYpc5uwoPq4UvN40zEuMbp+Q=";
 
   postPatch = ''
     substituteInPlace src/scripts/mod.rs \
-      --replace 'call_format = "nmap' 'call_format = "${nmap}/bin/nmap'
+      --replace-fail 'call_format = "nmap' 'call_format = "${nmap}/bin/nmap'
     patchShebangs fixtures/.rustscan_scripts/*
   '';
 
@@ -29,9 +37,10 @@ rustPlatform.buildRustPackage rec {
 
   meta = with lib; {
     description = "Faster Nmap Scanning with Rust";
-    mainProgram = "rustscan";
     homepage = "https://github.com/RustScan/RustScan";
+    changelog = "https://github.com/RustScan/RustScan/releases/tag/${version}";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [ figsoda ];
+    mainProgram = "rustscan";
   };
 }
