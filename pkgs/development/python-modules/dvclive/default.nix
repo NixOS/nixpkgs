ab6032f43a4fa031b81e9d7c10d4128feb6a6da5
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-27 15:26:51 +0200

    python311Packages.dvclive: format with nixfmt

--- pkgs/development/python-modules/dvclive/default.nix
+++ pkgs/development/python-modules/dvclive/default.nix
@@ -1,33 +1,34 @@
-{ lib
-, buildPythonPackage
-, datasets
-, dvc
-, dvc-render
-, dvc-studio-client
-, fastai
-, fetchFromGitHub
-, funcy
-, gto
-, jsonargparse
-, lightgbm
-, lightning
-, matplotlib
-, mmcv
-, numpy
-, optuna
-, pandas
-, pillow
-, psutil
-, pynvml
-, pythonOlder
-, ruamel-yaml
-, scikit-learn
-, scmrepo
-, setuptools-scm
-, tensorflow
-, torch
-, transformers
-, xgboost
+{
+  lib,
+  buildPythonPackage,
+  datasets,
+  dvc,
+  dvc-render,
+  dvc-studio-client,
+  fastai,
+  fetchFromGitHub,
+  funcy,
+  gto,
+  jsonargparse,
+  lightgbm,
+  lightning,
+  matplotlib,
+  mmcv,
+  numpy,
+  optuna,
+  pandas,
+  pillow,
+  psutil,
+  pynvml,
+  pythonOlder,
+  ruamel-yaml,
+  scikit-learn,
+  scmrepo,
+  setuptools-scm,
+  tensorflow,
+  torch,
+  transformers,
+  xgboost,
 }:
 
 buildPythonPackage rec {
@@ -44,9 +45,7 @@ buildPythonPackage rec {
     hash = "sha256-yIViKlkCdoG2vSZdScL38fZd9musLRKzBd9wSR6lJdk=";
   };
 
-  build-system = [
-    setuptools-scm
-  ];
+  build-system = [ setuptools-scm ];
 
   dependencies = [
     dvc
@@ -81,29 +80,17 @@ buildPythonPackage rec {
       numpy
       pillow
     ];
-    sklearn = [
-      scikit-learn
-    ];
+    sklearn = [ scikit-learn ];
     plots = [
       pandas
       scikit-learn
       numpy
     ];
-    markdown = [
-      matplotlib
-    ];
-    mmcv = [
-      mmcv
-    ];
-    tf = [
-      tensorflow
-    ];
-    xgb = [
-      xgboost
-    ];
-    lgbm = [
-      lightgbm
-    ];
+    markdown = [ matplotlib ];
+    mmcv = [ mmcv ];
+    tf = [ tensorflow ];
+    xgb = [ xgboost ];
+    lgbm = [ lightgbm ];
     huggingface = [
       datasets
       transformers
@@ -111,25 +98,19 @@ buildPythonPackage rec {
     # catalyst = [
     #   catalyst
     # ];
-    fastai = [
-      fastai
-    ];
+    fastai = [ fastai ];
     lightning = [
       lightning
       torch
       jsonargparse
     ] ++ jsonargparse.optional-dependencies.signatures;
-    optuna = [
-      optuna
-    ];
+    optuna = [ optuna ];
   };
 
   # Circular dependency with dvc
   doCheck = false;
 
-  pythonImportsCheck = [
-    "dvclive"
-  ];
+  pythonImportsCheck = [ "dvclive" ];
 
   meta = with lib; {
     description = "Library for logging machine learning metrics and other metadata in simple file formats";
