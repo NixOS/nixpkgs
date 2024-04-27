6a20d4c13780c2aa718f90986b9bdf0bc0af58bb
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-27 19:01:54 +0200

    python312Packages.peaqevcore: 19.9.0 -> 19.9.2
    Changelog: https://github.com/elden1337/peaqev-core/releases/tag/19.9.2

--- pkgs/development/python-modules/peaqevcore/default.nix
+++ pkgs/development/python-modules/peaqevcore/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "peaqevcore";
-  version = "19.9.0";
+  version = "19.9.2";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-tFbk33SQqCNcUnJ0X+OtJ8XDEpMXWnuxA2L30Prhssc=";
+    hash = "sha256-X5JKfIE6rISXb3F2liuyLrVHm5xw3DoFeMhqhEsdEIY=";
   };
 
   postPatch = ''
