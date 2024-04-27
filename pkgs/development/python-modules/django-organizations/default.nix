3a0ddfcf0f0f3cf38f598b0268535b73bcaca027
soyouzpanda <soyouzpanda@soyouzpanda.fr>
2024-04-27 16:35:20 +0200

    python3Packages.django-organizations: init at 2.4.1
    Co-authored-by: Defelo <mail@defelo.de>

---
+++ pkgs/development/python-modules/django-organizations/default.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  django,
+  django-extensions,
+}:
+
+buildPythonPackage rec {
+  pname = "django-organizations";
+  version = "2.5.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "bennylope";
+    repo = "django-organizations";
+    tag = version;
+    hash = "sha256-lgri6CCITp1oYCwpH8vrUglphXOmwZ3KX5G/L29akrs=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    django
+    django-extensions
+  ];
+
+  pythonImportsCheck = [ "organizations" ];
+
+  meta = {
+    description = "Multi-user accounts for Django projects";
+    homepage = "https://github.com/bennylope/django-organizations";
+    changelog = "https://github.com/bennylope/django-organizations/blob/${version}/HISTORY.rst";
+    license = lib.licenses.bsd2;
+    maintainers = with lib.maintainers; [ defelo ];
+  };
+}
