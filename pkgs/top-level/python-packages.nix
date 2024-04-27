121e9cb884b4758a02713cc4311478ca253748f8
Radik Islamov <vizid1337@gmail.com>
2024-04-27 06:06:11 +0500

    python312Packages.epitran: init at 1.24

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3849,6 +3849,8 @@ self: super: with self; {
 
   epion = callPackage ../development/python-modules/epion { };
 
+  epitran = callPackage ../development/python-modules/epitran { };
+
   epson-projector = callPackage ../development/python-modules/epson-projector { };
 
   equinox = callPackage ../development/python-modules/equinox { };
