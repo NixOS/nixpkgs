3c91948a7a99f8ccc9fddc06aba82005ec42ea35
duvetfall <git@myxa.me>
2024-04-28 22:42:03 +0300

    maintainers: add duvetfall

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5508,6 +5508,10 @@
       fingerprint = "9484 44FC E03B 05BA 5AB0  591E C37B 1C1D 44C7 86EE";
     }];
   };
+  duvetfall = {
+    name = "Darya Mukhina";
+    email = "nix@myxa.me";
+  };
   dvn0 = {
     email = "git@dvn.me";
     github = "dvn0";
