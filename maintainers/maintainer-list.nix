d5ae6e1a5725d2a5cb98b8410c2b0c6cb8378eb7
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-04-28 13:49:35 -0400

    maintainers: add wgunderwood

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21466,6 +21466,12 @@
     githubId = 24979302;
     name = "Vladimír Zahradník";
   };
+  wgunderwood = {
+    email = "wg.underwood13@gmail.com";
+    github = "WGUNDERWOOD";
+    githubId = 42812654;
+    name = "William Underwood";
+  };
   wheelsandmetal = {
     email = "jakob@schmutz.co.uk";
     github = "wheelsandmetal";
