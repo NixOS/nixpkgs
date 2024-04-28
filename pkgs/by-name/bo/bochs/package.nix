28474838f33319adf07e94399fe660a678486b0f
Anderson Torres <torres.anderson.85@protonmail.com>
2024-04-28 09:31:44 -0300

    bochs: refactor
    - fetch from GitHub instead of SourceForge
    - use lib.* functions
    - mainProgram
    - split outputs
    - strictDeps

--- pkgs/by-name/bo/bochs/package.nix
+++ pkgs/by-name/bo/bochs/package.nix
@@ -1,35 +1,39 @@
-{ lib
-, stdenv
-, fetchurl
-, SDL2
-, curl
-, darwin
-, docbook_xml_dtd
-, docbook_xsl
-, gtk3
-, libGL
-, libGLU
-, libX11
-, libXpm
-, libtool
-, ncurses
-, pkg-config
-, readline
-, wget
-, wxGTK
-, enableSDL2 ? true
-, enableTerm ? true
-, enableWx ? !stdenv.isDarwin
-, enableX11 ? !stdenv.isDarwin
+{
+  lib,
+  SDL2,
+  curl,
+  darwin,
+  docbook_xml_dtd,
+  docbook_xsl,
+  fetchFromGitHub,
+  gtk3,
+  libGL,
+  libGLU,
+  libX11,
+  libXpm,
+  libtool,
+  ncurses,
+  pkg-config,
+  readline,
+  stdenv,
+  wget,
+  wxGTK,
+  # Boolean flags
+  enableSDL2 ? true,
+  enableTerm ? true,
+  enableWx ? !stdenv.isDarwin,
+  enableX11 ? !stdenv.isDarwin,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bochs";
   version = "2.8";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/project/bochs/bochs/${finalAttrs.version}/bochs-${finalAttrs.version}.tar.gz";
-    hash = "sha256-qFsTr/fYQR96nzVrpsM7X13B+7EH61AYzCOmJjnaAFk=";
+  src = fetchFromGitHub {
+    owner = "bochs-emu";
+    repo = "Bochs";
+    rev = "REL_${lib.replaceStrings ["."] [ "_" ] finalAttrs.version}_FINAL";
+    hash = "sha256-Xnw5h8/pxzAXmGxPUmlUB6iUulTsPJcXsdGA1KN9FaQ=";
   };
 
   nativeBuildInputs = [
@@ -37,6 +41,9 @@ stdenv.mkDerivation (finalAttrs: {
     docbook_xsl
     libtool
     pkg-config
+  ]
+  ++ lib.optionals enableSDL2 [
+    SDL2
   ];
 
   buildInputs = [
@@ -60,79 +67,85 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   configureFlags = [
-    "--with-rfb=no"
-    "--with-vncsrv=no"
-    "--with-nogui"
+    (lib.withFeature false "rfb")
+    (lib.withFeature false "vncsrv")
+
+    (lib.withFeature true "nogui")
 
     # These will always be "yes" on NixOS
-    "--enable-ltdl-install=yes"
-    "--enable-readline=yes"
-    "--enable-all-optimizations=yes"
-    "--enable-logging=yes"
-    "--enable-xpm=yes"
+    (lib.enableFeature true "ltdl-install")
+    (lib.enableFeature true "readline")
+    (lib.enableFeature true "all-optimizations")
+    (lib.enableFeature true "logging")
+    (lib.enableFeature true "xpm")
 
     # ... whereas these, always "no"!
-    "--enable-cpp=no"
-    "--enable-instrumentation=no"
+    (lib.enableFeature false "cpp")
+    (lib.enableFeature false "instrumentation")
 
-    "--enable-docbook=no" # Broken - it requires docbook2html
+    (lib.enableFeature false "docbook") # Broken - it requires docbook2html
 
     # Dangerous options - they are marked as "incomplete/experimental" on Bochs documentation
-    "--enable-3dnow=no"
-    "--enable-monitor-mwait=no"
-    "--enable-raw-serial=no"
+    (lib.enableFeature false "3dnow")
+    (lib.enableFeature false "monitor-mwait")
+    (lib.enableFeature false "raw-serial")
 
     # These are completely configurable, and they don't depend of external tools
-    "--enable-a20-pin"
-    "--enable-avx"
-    "--enable-busmouse"
-    "--enable-cdrom"
-    "--enable-clgd54xx"
-    "--enable-configurable-msrs"
-    "--enable-cpu-level=6" # from 3 to 6
-    "--enable-debugger" #conflicts with gdb-stub option
-    "--enable-debugger-gui"
-    "--enable-evex"
-    "--enable-fpu"
-    "--enable-gdb-stub=no" # conflicts with debugger option
-    "--enable-handlers-chaining"
-    "--enable-idle-hack"
-    "--enable-iodebug"
-    "--enable-large-ramfile"
-    "--enable-largefile"
-    "--enable-pci"
-    "--enable-repeat-speedups"
-    "--enable-show-ips"
-    "--enable-smp"
-    "--enable-vmx=2"
-    "--enable-svm"
-    "--enable-trace-linking"
-    "--enable-usb"
-    "--enable-usb-ehci"
-    "--enable-usb-ohci"
-    "--enable-usb-xhci"
-    "--enable-voodoo"
-    "--enable-x86-64"
-    "--enable-x86-debugger"
-  ] ++ lib.optionals enableSDL2 [
-    "--with-sdl2"
-  ] ++ lib.optionals enableTerm [
-    "--with-term"
-  ] ++ lib.optionals enableWx [
-    "--with-wx"
-  ] ++ lib.optionals enableX11 [
-    "--with-x"
-    "--with-x11"
-  ] ++ lib.optionals (!stdenv.isDarwin) [
-    "--enable-e1000"
-    "--enable-es1370"
-    "--enable-ne2000"
-    "--enable-plugins"
-    "--enable-pnic"
-    "--enable-sb16"
+    (lib.enableFeatureAs true "cpu-level" "6") # from 3 to 6
+    (lib.enableFeatureAs true "vmx" "2")
+
+    (lib.enableFeature false "gdb-stub") # conflicts with debugger option
+    (lib.enableFeature true "debugger") #conflicts with gdb-stub option
+
+    (lib.enableFeature true "a20-pin")
+    (lib.enableFeature true "avx")
+    (lib.enableFeature true "busmouse")
+    (lib.enableFeature true "cdrom")
+    (lib.enableFeature true "clgd54xx")
+    (lib.enableFeature true "configurable-msrs")
+    (lib.enableFeature true "debugger-gui")
+    (lib.enableFeature true "evex")
+    (lib.enableFeature true "fpu")
+    (lib.enableFeature true "handlers-chaining")
+    (lib.enableFeature true "idle-hack")
+    (lib.enableFeature true "iodebug")
+    (lib.enableFeature true "large-ramfile")
+    (lib.enableFeature true "largefile")
+    (lib.enableFeature true "pci")
+    (lib.enableFeature true "repeat-speedups")
+    (lib.enableFeature true "show-ips")
+    (lib.enableFeature true "smp")
+    (lib.enableFeature true "svm")
+    (lib.enableFeature true "trace-linking")
+    (lib.enableFeature true "usb")
+    (lib.enableFeature true "usb-ehci")
+    (lib.enableFeature true "usb-ohci")
+    (lib.enableFeature true "usb-xhci")
+    (lib.enableFeature true "voodoo")
+    (lib.enableFeature true "x86-64")
+    (lib.enableFeature true "x86-debugger")
+
+    (lib.withFeature enableSDL2 "sdl2")
+    (lib.withFeature enableTerm "term")
+    (lib.withFeature enableWx "wx")
+    (lib.withFeature enableX11 "x")
+    (lib.withFeature enableX11 "x11")
+
+    (lib.enableFeature (!stdenv.isDarwin) "e1000")
+    (lib.enableFeature (!stdenv.isDarwin) "es1370")
+    (lib.enableFeature (!stdenv.isDarwin) "ne2000")
+    (lib.enableFeature (!stdenv.isDarwin) "plugins")
+    (lib.enableFeature (!stdenv.isDarwin) "pnic")
+    (lib.enableFeature (!stdenv.isDarwin) "sb16")
   ];
 
+  outputs = [ "out" "lib" "doc" "man" ];
+
   enableParallelBuilding = true;
+
+  strictDeps = true;
+
+  sourceRoot = "${finalAttrs.src.name}/bochs";
 
   meta = {
     homepage = "https://bochs.sourceforge.io/";
@@ -143,9 +156,10 @@ stdenv.mkDerivation (finalAttrs: {
       Intel x86 CPU, common I/O devices, and a custom BIOS.
     '';
     license = lib.licenses.lgpl2Plus;
+    mainProgram = "bochs";
     maintainers = with lib.maintainers; [ AndersonTorres ];
     platforms = lib.platforms.unix;
   };
 })
-# TODO: a better way to organize the options
+# TODO: investigate autoreconfHook errors
 # TODO: docbook (docbook-tools from RedHat mirrors should help)
