81f92b2ae3c191bcc959989a7857960e44073443
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-28 05:44:45 +0000

    oelint-adv: 5.3.0 -> 5.3.1

--- pkgs/by-name/oe/oelint-adv/package.nix
+++ pkgs/by-name/oe/oelint-adv/package.nix
@@ -6,13 +6,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "oelint-adv";
-  version = "5.3.0";
+  version = "5.3.1";
   format = "setuptools";
 
   src = fetchPypi {
     inherit version;
     pname = "oelint_adv";
-    hash = "sha256-EAYHxp7pXXYNYRGsQ3XckRxDM1pFHgiZr+gj8fJLqa0=";
+    hash = "sha256-8fftHQpv2GZhi3ZDXYUG7uAiWuuX79dntGAbKIvv4Kc=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
