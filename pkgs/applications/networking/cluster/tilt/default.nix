4d12a2d7177f9126b8d1be9ec494b6c6c47f03c7
Simon Žlender <pub.git@zlender.si>
2024-04-28 12:24:01 +0200

    tilt: 0.33.10 -> 0.33.13

--- pkgs/applications/networking/cluster/tilt/default.nix
+++ pkgs/applications/networking/cluster/tilt/default.nix
@@ -5,13 +5,13 @@ let args = rec {
       /* Do not use "dev" as a version. If you do, Tilt will consider itself
         running in development environment and try to serve assets from the
         source tree, which is not there once build completes.  */
-      version = "0.33.10";
+      version = "0.33.13";
 
       src = fetchFromGitHub {
         owner = "tilt-dev";
         repo = "tilt";
         rev = "v${version}";
-        hash = "sha256-LPb2tC3xIGhjiLYkTU+NBIUoqiicO2ORM6Nt1eTnwQs=";
+        hash = "sha256-B1kau6G56Iz6Yso2KpJCPE18yznhKCmq+Pabi2sxSmI=";
       };
     };
 
