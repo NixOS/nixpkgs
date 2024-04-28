04a00e5a84e041664438ae7b01b07ada79083d2a
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-28 02:53:09 +0000

    vhdl-ls: 0.79.0 -> 0.80.0

--- pkgs/development/tools/language-servers/vhdl-ls/default.nix
+++ pkgs/development/tools/language-servers/vhdl-ls/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "vhdl-ls";
-  version = "0.79.0";
+  version = "0.80.0";
 
   src = fetchFromGitHub {
     owner = "VHDL-LS";
     repo = "rust_hdl";
     rev = "v${version}";
-    hash = "sha256-9AcMMT1d4FrTcCN6c2uMa1Xh21e79sM+YsPs+RT892U=";
+    hash = "sha256-3p6SmmNnuPoHbA0QmfgvO342AhRx5dI9q4YXypi/2k0=";
   };
 
-  cargoHash = "sha256-Cb4WvOdWtwaC5GCQcnIorAFbkDXliU9w1xDU8g3YlSI=";
+  cargoHash = "sha256-zAz33P5k6wmQej8u45+lXKHSRVaZhrB5L0jUPjs44W8=";
 
   postPatch = ''
     substituteInPlace vhdl_lang/src/config.rs \
