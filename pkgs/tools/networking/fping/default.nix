c646edb98dc4c95bfdee82b4121b43155d1536c9
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-28 10:54:29 +0200

    fping: refactor
    - add myself as maintainer

--- pkgs/tools/networking/fping/default.nix
+++ pkgs/tools/networking/fping/default.nix
@@ -1,4 +1,7 @@
-{ lib, stdenv, fetchurl }:
+{ lib
+, stdenv
+, fetchurl
+}:
 
 stdenv.mkDerivation rec {
   pname = "fping";
@@ -6,16 +9,21 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://www.fping.org/dist/fping-${version}.tar.gz";
-    sha256 = "sha256-p2ktENc/sLt24fdFmqfxm7zb/Frb7e8C9GiXSxiw5C8=";
+    hash = "sha256-p2ktENc/sLt24fdFmqfxm7zb/Frb7e8C9GiXSxiw5C8=";
   };
 
-  configureFlags = [ "--enable-ipv6" "--enable-ipv4" ];
+  configureFlags = [
+    "--enable-ipv6"
+    "--enable-ipv4"
+  ];
 
   meta = with lib; {
-    homepage = "http://fping.org/";
     description = "Send ICMP echo probes to network hosts";
+    homepage = "http://fping.org/";
+    changelog = "https://github.com/schweikert/fping/releases/tag/v${version}";
     license = licenses.bsd0;
-    platforms = platforms.all;
     mainProgram = "fping";
+    maintainers = with maintainers; [ fab ];
+    platforms = platforms.all;
   };
 }
