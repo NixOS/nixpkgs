f6f4e58924d4f29e96b6b43cdef46c973a97f792
natsukium <tomoya.otabi@gmail.com>
2024-04-28 13:57:47 +0900

    mafft: enable simple test

--- pkgs/applications/science/biology/mafft/default.nix
+++ pkgs/applications/science/biology/mafft/default.nix
@@ -2,6 +2,8 @@
   lib,
   stdenv,
   fetchFromGitLab,
+  runCommand,
+  mafft,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -24,6 +26,24 @@ stdenv.mkDerivation (finalAttrs: {
     "CC=${stdenv.cc.targetPrefix}cc"
     "PREFIX=$(out)"
   ];
+
+  passthru.tests = {
+    simple = runCommand "${finalAttrs.pname}-test" { } ''
+      mkdir $out
+      cd ${finalAttrs.src}/test
+      ${lib.getExe mafft} sample > $out/test.fftns2
+      ${lib.getExe mafft} --maxiterate 100  sample > $out/test.fftnsi
+      ${lib.getExe mafft} --globalpair sample > $out/test.gins1
+      ${lib.getExe mafft} --globalpair --maxiterate 100  sample > $out/test.ginsi
+      ${lib.getExe mafft} --localpair sample > $out/test.lins1
+      ${lib.getExe mafft} --localpair --maxiterate 100  sample > $out/test.linsi
+      diff $out/test.fftns2 sample.fftns2
+      diff $out/test.fftnsi sample.fftnsi
+      diff $out/test.gins1 sample.gins1
+      diff $out/test.ginsi sample.ginsi
+      diff $out/test.lins1 sample.lins1
+    '';
+  };
 
   meta = with lib; {
     description = "Multiple alignment program for amino acid or nucleotide sequences";
