21192a7561a55273b4e1f4a0d4aa6bc5b89f5231
nicoo <nicoo@mur.at>
2024-04-28 02:13:04 +0000

    miniz: add pkgconf metadata, test, and validation

--- pkgs/development/libraries/miniz/default.nix
+++ pkgs/development/libraries/miniz/default.nix
@@ -2,22 +2,24 @@
 , fetchFromGitHub
 , nix-update-script
 , stdenv
+, testers
+, validatePkgConfig
 , cmake
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "miniz";
   version = "3.0.2";
 
   src = fetchFromGitHub {
     owner = "richgel999";
-    repo = pname;
-    rev = version;
+    repo = "miniz";
+    rev = finalAttrs.version;
     hash = "sha256-3J0bkr2Yk+MJXilUqOCHsWzuykySv5B1nepmucvA4hg=";
   };
   passthru.updateScript = nix-update-script {};
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake validatePkgConfig ];
 
   postFixup = ''
     substituteInPlace "$out"/lib/pkgconfig/miniz.pc \
@@ -25,11 +27,14 @@ stdenv.mkDerivation rec {
       --replace-fail '=''${exec_prefix}//' '=/'
   '';
 
+  passthru.tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+
   meta = with lib; {
     description = "Single C source file zlib-replacement library";
     homepage = "https://github.com/richgel999/miniz";
     license = licenses.mit;
     maintainers = with maintainers; [ astro ];
     platforms = platforms.unix;
+    pkgConfigModules = [ "miniz" ];
   };
-}
+})
