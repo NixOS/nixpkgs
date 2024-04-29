a9e7d41ce0b8cc1b3d01e1b8522dccbcf5e87e46
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-29 04:29:23 +0000

    bindfs: 1.17.6 -> 1.17.7

--- pkgs/tools/filesystems/bindfs/default.nix
+++ pkgs/tools/filesystems/bindfs/default.nix
@@ -7,12 +7,12 @@
 }:
 
 stdenv.mkDerivation (finalAttrs: {
-  version = "1.17.6";
+  version = "1.17.7";
   pname = "bindfs";
 
   src = fetchurl {
     url = "https://bindfs.org/downloads/bindfs-${finalAttrs.version}.tar.gz";
-    hash = "sha256-076zzGm7K2gCzFOViOkh/qlz7WGRsTPyAkcZMR0cwYs=";
+    hash = "sha256-wLBg6Uw6IxodSqC88mb/GJmBpO845C++IylqfYFxm3o=";
   };
 
   nativeBuildInputs = [
