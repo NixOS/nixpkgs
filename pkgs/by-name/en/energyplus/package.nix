fbea80013635b8b7783824e64ee60aa04f59481c
RobWalt <robwalter96@gmail.com>
2024-04-29 07:38:54 +0200

    fix: place resulting files in right directories

--- pkgs/by-name/en/energyplus/package.nix
+++ pkgs/by-name/en/energyplus/package.nix
@@ -22,9 +22,22 @@ stdenv.mkDerivation rec {
     python3
   ];
 
-  # this removes references to libenergyplus which are needed.
-  # At the time of writing this I don't know how else to solve this
-  dontFixup = true;
+  postInstall = ''
+    # binaries
+    mkdir -p $out/bin
+    # main binary that ships with the repo
+    cp $out/${pname}-${version} $out/bin/${pname}
+    # secondary utility binary shipping with the repo
+    cp $out/ConvertInputFormat-${version} $out/bin/ConvertInputFormat
+
+    # libraries
+    mkdir -p $out/lib
+    # put lib into the right spot so it can actually be found
+    cp $out/lib${pname}api.so.${version} $out/lib
+  '';
+
+  # this would remove references to libenergyplus from the binaries
+  dontPatchELF = true;
 
   meta = with lib; {
     description = "A whole building energy simulation program to model both energy consumption and water use";
