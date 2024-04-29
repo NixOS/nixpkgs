64073966b4a9b1bd08ecccb00ede17a9abee8935
Infinidoge <infinidoge@inx.moe>
2024-04-29 13:48:02 -0400

    temurin-bin: init at 22

--- pkgs/development/compilers/temurin-bin/jdk-linux.nix
+++ pkgs/development/compilers/temurin-bin/jdk-linux.nix
@@ -26,4 +26,7 @@ in
 
   jdk-21 = common { sourcePerArch = sources.jdk.openjdk21; };
   jre-21 = common { sourcePerArch = sources.jre.openjdk21; };
+
+  jdk-22 = common { sourcePerArch = sources.jdk.openjdk22; };
+  jre-22 = common { sourcePerArch = sources.jre.openjdk22; };
 }
