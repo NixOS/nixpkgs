9acd7964879f1d5d066963b5712fa8ab5c30037f
R. Ryantm <ryantm-bot@ryantm.com>
2024-04-29 09:23:56 +0000

    fldigi: 4.2.04 -> 4.2.05

--- pkgs/applications/radio/fldigi/default.nix
+++ pkgs/applications/radio/fldigi/default.nix
@@ -18,11 +18,11 @@
 
 stdenv.mkDerivation rec {
   pname = "fldigi";
-  version = "4.2.04";
+  version = "4.2.05";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pname}/${pname}-${version}.tar.gz";
-    hash = "sha256-crVeX9vtvn1O5ah1dc74425qUKcozKlDMVeIefMpktY=";
+    hash = "sha256-rBGJ+63Szhy37LQw0LpE2/lLyP5lwK7hsz/uq453iHY=";
   };
 
   nativeBuildInputs = [ pkg-config ];
