5f4be41af724f157315e6f5840a7076eef28d8f0
Sizhe Zhao <prc.zhao@outlook.com>
2024-04-29 23:37:00 +0800

    nixos/sd-card: enable rpi's mini uart console

--- nixos/modules/installer/sd-card/sd-image-aarch64.nix
+++ nixos/modules/installer/sd-card/sd-image-aarch64.nix
@@ -15,8 +15,14 @@
 
   # The serial ports listed here are:
   # - ttyS0: for Tegra (Jetson TX1)
+  # - ttyS1: for Raspberry Pi's mini UART
   # - ttyAMA0: for QEMU's -machine virt
-  boot.kernelParams = ["console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0"];
+  boot.kernelParams = [
+    "console=ttyS1,115200n8"
+    "console=ttyS0,115200n8"
+    "console=ttyAMA0,115200n8"
+    "console=tty0"
+  ];
 
   sdImage = {
     populateFirmwareCommands = let
