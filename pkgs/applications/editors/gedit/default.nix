094802616669a2d50c612ccc8d185cb7c625bbd0
Bobby Rong <rjl931189261@126.com>
2024-04-29 13:45:42 +0000

    gedit: 46.2 → 47.0
    https://gitlab.gnome.org/GNOME/gedit/-/compare/46.2...47.0

--- pkgs/applications/editors/gedit/default.nix
+++ pkgs/applications/editors/gedit/default.nix
@@ -30,13 +30,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gedit";
-  version = "46.2";
+  version = "47.0";
 
   outputs = [ "out" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gedit/${lib.versions.major version}/gedit-${version}.tar.xz";
-    sha256 = "wIZkErrRR+us4tKC/8u1oOmjBLIP1VZAvuIcgebVAe8=";
+    sha256 = "+kpZfjTHbUrJFDG1rm4ZHJbGsK8XAuCJmrNRme36G/o=";
   };
 
   patches = [
