7018f2066b47a4c9c8ffc1ee1848b6118d3657a7
oluceps <i@oluceps.uk>
2024-04-29 06:53:33 +0000

    mihomo: 1.18.3 -> 1.18.4
    Diff: https://github.com/MetaCubeX/mihomo/compare/v1.18.3...v1.18.4

--- pkgs/by-name/mi/mihomo/package.nix
+++ pkgs/by-name/mi/mihomo/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "mihomo";
-  version = "1.18.3";
+  version = "1.18.4";
 
   src = fetchFromGitHub {
     owner = "MetaCubeX";
     repo = "mihomo";
     rev = "v${version}";
-    hash = "sha256-/fFVUI++OhOer5DrvcXO+R+4whtb5+3Qg3e4+ikJr1Y=";
+    hash = "sha256-mqNcMB2nb8+e9XEGKqwaBE/nC4+jRaEFxE17e0JTo8Y=";
   };
 
-  vendorHash = "sha256-k4xB/jO78VGD+n9HtuoWXoXB+kZCEyPKJTTwj32nGIw=";
+  vendorHash = "sha256-9Ey4roUm/Y6GfFmLD6ij/A+YIgetStawWmhD+Iq3qc8=";
 
   excludedPackages = [ "./test" ];
 
