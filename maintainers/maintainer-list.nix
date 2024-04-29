adc9566b3274444f7924bc1debf9eac0a3e5775b
Sebastian Zivota <loewenheim@mailbox.org>
2024-04-29 20:03:09 +0200

    maintainers: Remove loewenheim

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11596,12 +11596,6 @@
     githubId = 918448;
     name = "Anthony Lodi";
   };
-  loewenheim = {
-    email = "loewenheim@mailbox.org";
-    github = "loewenheim";
-    githubId = 7622248;
-    name = "Sebastian Zivota";
-  };
   logo = {
     email = "logo4poop@protonmail.com";
     matrix = "@logo4poop:matrix.org";
