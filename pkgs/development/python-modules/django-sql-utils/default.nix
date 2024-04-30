09de42ef935abb361b806bc9220dd74476c98be4
soyouzpanda <soyouzpanda@soyouzpanda.fr>
2024-04-30 12:41:03 +0200

    python3Packages.django-sql-utils: init at 0.7.0
    Co-authored-by: Defelo <mail@defelo.de>

---
+++ pkgs/development/python-modules/django-sql-utils/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  django,
+  sqlparse,
+}:
+
+buildPythonPackage rec {
+  pname = "django-sql-utils";
+  version = "0.7.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "martsberger";
+    repo = "django-sql-utils";
+    tag = version;
+    hash = "sha256-OjKPxoWYheu8UQ14KvyiQyHISAQjJep+N4HRc4Msa1w=";
+  };
+
+  postPatch = ''
+    echo -e "\n[tool.hatch.build.targets.wheel]\npackages = [ \"sql_util\" ]" >> pyproject.toml
+  '';
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    django
+    sqlparse
+  ];
+
+  checkPhase = ''
+    runHook preCheck
+
+    python runtests.py
+
+    runHook postCheck
+  '';
+
+  pythonImportsCheck = [ "sql_util" ];
+
+  meta = {
+    description = "SQL utilities for Django";
+    homepage = "https://github.com/martsberger/django-sql-utils";
+    changelog = "https://github.com/martsberger/django-sql-utils/releases/tag/${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ defelo ];
+  };
+}
