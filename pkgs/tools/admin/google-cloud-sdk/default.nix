6ce4620d2d780b014bf9a9a3bfed3add2b9803a7
Marcus Ramberg <marcus@means.no>
2024-04-30 08:37:14 +0200

    google-cloud-sdk: Add marcusramberg to maintainers

--- pkgs/tools/admin/google-cloud-sdk/default.nix
+++ pkgs/tools/admin/google-cloud-sdk/default.nix
@@ -131,7 +131,7 @@ in stdenv.mkDerivation rec {
     license = licenses.free;
     homepage = "https://cloud.google.com/sdk/";
     changelog = "https://cloud.google.com/sdk/docs/release-notes";
-    maintainers = with maintainers; [ iammrinal0 pradyuman stephenmw zimbatm ];
+    maintainers = with maintainers; [ iammrinal0 marcusramberg pradyuman stephenmw zimbatm ];
     platforms = builtins.attrNames data.googleCloudSdkPkgs;
     mainProgram = "gcloud";
   };
