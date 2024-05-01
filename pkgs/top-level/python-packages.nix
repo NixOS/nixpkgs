ad40b65fd5b5c1bd8553f36c5947933765b5a27e
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-01 23:38:34 +0200

    python312Packages.sanix: init at 1.0.6
    Module to get measurements data from Sanix devices
    
    https://github.com/tomaszsluszniak/sanix_py

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13490,6 +13490,8 @@ self: super: with self; {
 
   sanic-testing = callPackage ../development/python-modules/sanic-testing { };
 
+  sanix = callPackage ../development/python-modules/sanix { };
+
   sansio-multipart = callPackage ../development/python-modules/sansio-multipart { };
 
   sarif-om = callPackage ../development/python-modules/sarif-om { };
