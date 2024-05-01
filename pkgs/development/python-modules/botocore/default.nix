7e245875273c0c4dcadcd2b350d27b33b1d8a26f
Danielle Lancashire <dani@builds.terrible.systems>
2024-05-01 20:13:24 +0000

    python311Packages.botocore: 1.34.58 -> 1.34.96

--- pkgs/development/python-modules/botocore/default.nix
+++ pkgs/development/python-modules/botocore/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "botocore";
-  version = "1.34.58"; # N.B: if you change this, change boto3 and awscli to a matching version
+  version = "1.34.96"; # N.B: if you change this, change boto3 and awscli to a matching version
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-11IWlSiG3FE+obXil5pq8I/u0vU34/wQLkoKLq1WOjU=";
+    hash = "sha256-AOkXzYFS2QKkdxueHk0M8e4JbJACfuNfKna205TiraU=";
   };
 
   pythonRelaxDeps = [
