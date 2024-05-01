7cad4c77dd6b6988e3d7be6eb7e522c7aee6c1a4
R. RyanTM <ryantm-bot@ryantm.com>
2024-05-01 20:30:37 +1000

    mkvtoolnix: 83.0 -> 84.0 (#308170)

--- pkgs/applications/video/mkvtoolnix/default.nix
+++ pkgs/applications/video/mkvtoolnix/default.nix
@@ -49,13 +49,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "mkvtoolnix";
-  version = "83.0";
+  version = "84.0";
 
   src = fetchFromGitLab {
     owner = "mbunkus";
     repo = "mkvtoolnix";
     rev = "release-${version}";
-    hash = "sha256-MHi3ewxCn560vpVfOucV34CNj/95U2OFd6bxAjtMBoc=";
+    hash = "sha256-//I++WWnSHnkpTZ0TzS3lhH5+eDD5mazTQ1HVMQS4Ug=";
   };
 
   nativeBuildInputs = [
