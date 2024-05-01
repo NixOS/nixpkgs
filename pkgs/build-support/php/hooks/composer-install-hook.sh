382220e679c962b65206b25a4abcb983ab04da6e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-01 11:15:16 +0200

    build-support/php: remove `makeBinaryWrapper`

--- pkgs/build-support/php/hooks/composer-install-hook.sh
+++ pkgs/build-support/php/hooks/composer-install-hook.sh
@@ -117,8 +117,9 @@ composerInstallInstallHook() {
 
     # Create symlinks for the binaries.
     jq -r -c 'try (.bin[] | select(test(".bat$")? | not) )' composer.json | while read -r bin; do
-        mkdir -p "$out"/share/php/"${pname}" "$out"/bin
-        makeWrapper "$out"/share/php/"${pname}"/"$bin" "$out"/bin/"$(basename "$bin")"
+        echo -e "\e[32mCreating symlink ${bin}...\e[0m"
+        mkdir -p "$out"/bin
+        ln -s "$out"/share/php/"${pname}"/"$bin" "$out"/bin/"$(basename "$bin")"
     done
 
     echo "Finished composerInstallInstallHook"
