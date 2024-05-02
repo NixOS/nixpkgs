eed14146a5731c81808b61f856fe986ed24f1218
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-02 12:13:34 +0200

    python312Packages.drawsvg: init at 2.3.0
    Programmatically generate SVG (vector) images, animations, and interactive Jupyter widgets
    
    https://github.com/cduck/drawsvg

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3454,6 +3454,8 @@ self: super: with self; {
 
   drawilleplot = callPackage ../development/python-modules/drawilleplot { };
 
+  drawsvg = callPackage ../development/python-modules/drawsvg { };
+
   dremel3dpy = callPackage ../development/python-modules/dremel3dpy { };
 
   drf-jwt = callPackage ../development/python-modules/drf-jwt { };
