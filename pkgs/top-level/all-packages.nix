20259f9904516f1d166811f7e61ded747fea920c
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-01 21:30:07 -0300

    SDL_audiolib: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24293,8 +24293,6 @@ with pkgs;
 
   SDL = SDL_classic;
 
-  SDL_audiolib = callPackage ../development/libraries/SDL_audiolib { };
-
   SDL_sixel = callPackage ../development/libraries/SDL_sixel { };
 
   SDL_gfx = callPackage ../development/libraries/SDL_gfx { };
