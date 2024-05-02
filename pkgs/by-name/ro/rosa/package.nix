b664c95febb804de87fd300020f6d1a20a682a75
Jean-Francois Chevrette <jfchevrette@gmail.com>
2024-05-02 09:47:47 -0400

    disable tests requiring network access
    - e2e tests require network access
    - TestRhRegionCommand require network access

--- pkgs/by-name/ro/rosa/package.nix
+++ pkgs/by-name/ro/rosa/package.nix
@@ -16,6 +16,18 @@ buildGoModule rec {
 
   __darwinAllowLocalNetworking = true;
 
+  postPatch = ''
+    # e2e tests require network access
+    rm -r tests/e2e
+  '';
+
+  preCheck = ''
+    # Workaround for cmd/list/rhRegion/cmd_test.go:39
+    #   Failed to get OCM regions: Can't retrieve shards: Get "https://api.stage.openshift.com/static/ocm-shards.json": dial tcp: lookup api.stage.openshift.com on [::1]:53: read udp [::1]:55793->[::1]:53: read: connection refused
+    substituteInPlace "cmd/list/rhRegion/cmd_test.go" \
+      --replace-fail "TestRhRegionCommand" "SkipRhRegionCommand"
+  '';
+
   nativeBuildInputs = [ installShellFiles ];
   postInstall = ''
     installShellCompletion --cmd rosa \
