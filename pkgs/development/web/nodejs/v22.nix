8a9a1e6369d83265d9a21dc62ced894b3e08edb4
Cobalt <cobalt@cobalt.rocks>
2024-05-02 20:27:36 +0200

    nodejs_22: 22.0.0 -> 22.1.0
    Update to current nodejs 22.1.0 release.
    This release notably include a node compile cache and multiple bug fixes.
    Release note: https://github.com/nodejs/node/releases/tag/v22.1.0

--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -8,8 +8,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "22.0.0";
-  sha256 = "sha256-IuKPv/MfaQc7gCTLQnReUQX4QEHzR1smC5fVoUEDnRo=";
+  version = "22.1.0";
+  sha256 = "sha256-nX1fQNnb1iYMmbXklLX5vHVejw/6xw4SGtzl+0QvI8s=";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
