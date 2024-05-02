7ad57dac8040632c53fb093584d36627c9f82e09
aviac <aviac@mailbox.org>
2024-05-02 19:27:06 +0200

    codeberg-cli: 0.3.5 -> 0.4.0
    Also reformat and update to `finalAttrs` style

--- pkgs/applications/version-management/codeberg-cli/default.nix
+++ pkgs/applications/version-management/codeberg-cli/default.nix
@@ -1,43 +1,53 @@
-{ lib
-, CoreServices
-, Security
-, fetchFromGitea
-, installShellFiles
-, openssl
-, pkg-config
-, rustPlatform
-, stdenv
+{
+  lib,
+  CoreServices,
+  Security,
+  fetchFromGitea,
+  installShellFiles,
+  openssl,
+  pkg-config,
+  rustPlatform,
+  stdenv,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "codeberg-cli";
-  version = "0.3.5";
+rustPlatform.buildRustPackage (
+  finalAttrs: {
+    pname = "codeberg-cli";
+    version = "0.4.0";
 
-  src = fetchFromGitea {
-    domain = "codeberg.org";
-    owner = "RobWalt";
-    repo = "codeberg-cli";
-    rev = "v${version}";
-    hash = "sha256-KjH78yqfZoN24TBYyFZuxf7z9poRov0uFYQ8+eq9p/o=";
-  };
+    src = fetchFromGitea {
+      domain = "codeberg.org";
+      owner = "RobWalt";
+      repo = "codeberg-cli";
+      rev = "v${finalAttrs.version}";
+      hash = "sha256-g5V3Noqh7Y9v/t/dt7n45/NblqNtpZCKELPc9DOkb8A=";
+    };
 
-  cargoHash = "sha256-RE4Zwa5vUWPc42w5GaaYkS6fLIbges1fAsOUuwqR2ag=";
-  nativeBuildInputs = [ pkg-config installShellFiles ];
+    cargoHash = "sha256-zTg/3PcFWzBmKZA7lRIpM3P03d1qpNVBczqWFbnxpic=";
+    nativeBuildInputs = [
+      pkg-config
+      installShellFiles
+    ];
 
-  buildInputs = [ openssl ]
-    ++ lib.optionals stdenv.isDarwin [ CoreServices Security ];
+    buildInputs =
+      [ openssl ]
+      ++ lib.optionals stdenv.isDarwin [
+        CoreServices
+        Security
+      ];
 
-  postInstall = ''
-    installShellCompletion --cmd berg \
-      --bash <($out/bin/berg completion bash) \
-      --fish <($out/bin/berg completion fish) \
-      --zsh <($out/bin/berg completion zsh)
-  '';
+    postInstall = ''
+      installShellCompletion --cmd berg \
+        --bash <($out/bin/berg completion bash) \
+        --fish <($out/bin/berg completion fish) \
+        --zsh <($out/bin/berg completion zsh)
+    '';
 
-  meta = with lib; {
-    description = "CLI Tool for Codeberg similar to gh and glab";
-    homepage = "https://codeberg.org/RobWalt/codeberg-cli";
-    license = with licenses; [ agpl3Plus ];
-    maintainers = with maintainers; [ robwalt ];
-    mainProgram = "berg";
-  };
-}
+    meta = with lib; {
+      description = "CLI Tool for Codeberg similar to gh and glab";
+      homepage = "https://codeberg.org/RobWalt/codeberg-cli";
+      license = with licenses; [ agpl3Plus ];
+      maintainers = with maintainers; [ robwalt ];
+      mainProgram = "berg";
+    };
+  }
+)
