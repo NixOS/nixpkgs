f83c48b2c768c93de94adfeeb5f8c26df73d3e1f
nicoo <nicoo@mur.at>
2024-05-02 17:20:52 +0000

    jikken: Reformat to RFC166 style

--- pkgs/by-name/ji/jikken/package.nix
+++ pkgs/by-name/ji/jikken/package.nix
@@ -21,27 +21,23 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-FxsI2ku52MlSGUph3/ovmn6HIwW+cUwVXuwzcd/1DV4=";
 
-  nativeBuildInputs = [
-    pkg-config
-  ];
+  nativeBuildInputs = [ pkg-config ];
 
   buildInputs =
-    [
-      openssl
-    ]
+    [ openssl ]
     ++ lib.optionals stdenv.isDarwin [
       darwin.apple_sdk.frameworks.IOKit
       darwin.apple_sdk.frameworks.Security
     ];
 
-  passthru.updateScript = nix-update-script {};
+  passthru.updateScript = nix-update-script { };
 
   meta = with lib; {
     description = "A powerful, source control friendly REST API testing toolkit";
     homepage = "https://jikken.io/";
     changelog = "https://github.com/jikkenio/jikken/blob/${src.rev}/CHANGELOG.md";
     license = licenses.mit;
-    maintainers = with maintainers; [vinnymeller];
+    maintainers = with maintainers; [ vinnymeller ];
     mainProgram = "jk";
   };
 }
