e44a9cc1b61d43c3b054ba3ad4c8bc11f8e66e54
toastal <toastal@posteo.net>
2024-05-02 16:11:05 +0700

    meli: mv to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32824,8 +32824,6 @@ with pkgs;
 
   meld = callPackage ../applications/version-management/meld { };
 
-  meli = callPackage ../applications/networking/mailreaders/meli { };
-
   melmatcheq.lv2 = callPackage ../applications/audio/melmatcheq.lv2 { };
 
   melody = callPackage ../tools/misc/melody { };
