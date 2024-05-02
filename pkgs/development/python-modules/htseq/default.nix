455be5c8bcf0da7033b8d1c09ee2b97c88213aef
natsukium <tomoya.otabi@gmail.com>
2024-05-02 17:11:46 +0900

    python311Packages.htseq: format with nixfmt

--- pkgs/development/python-modules/htseq/default.nix
+++ pkgs/development/python-modules/htseq/default.nix
@@ -1,4 +1,15 @@
-{ lib, buildPythonPackage, fetchFromGitHub, cython, numpy, pysam, matplotlib, python, isPy27, isPy3k }:
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  cython,
+  numpy,
+  pysam,
+  matplotlib,
+  python,
+  isPy27,
+  isPy3k,
+}:
 buildPythonPackage rec {
   version = "0.12.4";
   format = "setuptools";
@@ -12,13 +23,19 @@ buildPythonPackage rec {
   };
 
   nativeBuildInputs = [ cython ];
-  propagatedBuildInputs = [ numpy pysam matplotlib ];
+  propagatedBuildInputs = [
+    numpy
+    pysam
+    matplotlib
+  ];
 
-  checkPhase = lib.optionalString isPy27 ''
-    ${python.interpreter} python2/test/test_general.py
-  '' + lib.optionalString isPy3k ''
-    ${python.interpreter} python3/test/test_general.py
-  '';
+  checkPhase =
+    lib.optionalString isPy27 ''
+      ${python.interpreter} python2/test/test_general.py
+    ''
+    + lib.optionalString isPy3k ''
+      ${python.interpreter} python3/test/test_general.py
+    '';
 
   meta = with lib; {
     homepage = "https://htseq.readthedocs.io/";
