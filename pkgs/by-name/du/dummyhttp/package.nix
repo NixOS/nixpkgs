8d285d87723d9e962377dd707c7ec2ca915ac26b
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-05-02 18:43:51 +0200

    dummyhttp: init at 1.0.3

---
+++ pkgs/by-name/du/dummyhttp/package.nix
@@ -0,0 +1,33 @@
+{ lib
+, fetchFromGitHub
+, rustPlatform
+, darwin
+, stdenv
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "dummyhttp";
+  version = "1.0.3";
+
+  src = fetchFromGitHub {
+    owner = "svenstaro";
+    repo = "dummyhttp";
+    rev = "v${version}";
+    hash = "sha256-MDkyJAVNpCaAomAEweYrQeQWtil8bYo34ko9rAu+VBU=";
+  };
+
+  cargoHash = "sha256-JkA0qW/MQH+XmiD9eiT0s70HxNNYyk9ecBo4k5nUF10=";
+
+  buildInputs = lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Security
+  ];
+
+  meta = with lib; {
+    description = "Super simple HTTP server that replies a fixed body with a fixed response code";
+    homepage = "https://github.com/svenstaro/dummyhttp";
+    license = with licenses; [ mit ];
+    maintainers = with maintainers; [ GuillaumeDesforges ];
+    mainProgram = "dummyhttp";
+  };
+}
+
