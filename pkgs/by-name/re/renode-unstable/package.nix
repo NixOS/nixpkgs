9e3f6c3af518a3fa9c9f49b1d8d554be3a3075e2
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-03 06:40:21 +0000

    renode-unstable: 1.15.0+20240426git72f3c383b -> 1.15.0+20240502gita79411a5d

--- pkgs/by-name/re/renode-unstable/package.nix
+++ pkgs/by-name/re/renode-unstable/package.nix
@@ -5,11 +5,11 @@
 
 renode.overrideAttrs (finalAttrs: _: {
   pname = "renode-unstable";
-  version = "1.15.0+20240426git72f3c383b";
+  version = "1.15.0+20240502gita79411a5d";
 
   src = fetchurl {
     url = "https://builds.renode.io/renode-${finalAttrs.version}.linux-portable.tar.gz";
-    hash = "sha256-BOQT5nG6Vu12RyAkCwnh6PYfNQTcYGdV4XHDpi/8kPE=";
+    hash = "sha256-JgmoVTFq5z/4YFnmqn/nct7M50JOevQHWfMsaVtBybo=";
   };
 
   passthru.updateScript =
