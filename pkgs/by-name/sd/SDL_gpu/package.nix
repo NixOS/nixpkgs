232ab4014bf43a510da96c0567b3c75a5e652332
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-03 18:28:35 -0300

    SDL_gpu: 2019-01-24 -> 0-unstable-2022-06-24
    - finalAttrs design pattern
    - remove @piddend (inactive, https://github.com/NixOS/nixpkgs/issues/290642)

--- pkgs/by-name/sd/SDL_gpu/package.nix
+++ pkgs/by-name/sd/SDL_gpu/package.nix
@@ -1,36 +1,52 @@
-{ lib, stdenv, fetchFromGitHub, cmake, SDL2, libGLU }:
+{
+  lib,
+  SDL2,
+  cmake,
+  fetchFromGitHub,
+  libGLU,
+  pkg-config,
+  stdenv,
+}:
 
-stdenv.mkDerivation {
-  pname = "SDL_gpu-unstable";
-  version = "2019-01-24";
+stdenv.mkDerivation (finalAttrs: {
+  pname = "SDL_gpu";
+  version = "0-unstable-2022-06-24";
 
   src = fetchFromGitHub {
     owner = "grimfang4";
     repo = "sdl-gpu";
-    rev = "e3d350b325a0e0d0b3007f69ede62313df46c6ef";
-    sha256 = "0kibcaim01inb6xxn4mr6affn4hm50vz9kahb5k9iz8dmdsrhxy1";
+    rev = "e8ee3522ba0dbe72ca387d978e5f49a9f31e7ba0";
+    hash = "sha256-z1ZuHh9hvno2h+kCKfe+uWa/S6/OLZWWgLZ0zs9HetQ=";
   };
 
-  nativeBuildInputs = [ cmake ];
-  buildInputs = [ SDL2 libGLU ];
-
-  cmakeFlags = [
-    "-DSDL_gpu_BUILD_DEMOS=OFF"
-    "-DSDL_gpu_BUILD_TOOLS=OFF"
-    "-DSDL_gpu_BUILD_VIDEO_TEST=OFF"
-    "-DSDL_gpu_BUILD_TESTS=OFF"
+  nativeBuildInputs = [
+    SDL2
+    cmake
+    pkg-config
   ];
 
-  patchPhase = ''
-    sed -ie '210s#''${OUTPUT_DIR}/lib#''${CMAKE_INSTALL_LIBDIR}#' src/CMakeLists.txt
-    sed -ie '213s#''${OUTPUT_DIR}/lib#''${CMAKE_INSTALL_LIBDIR}#' src/CMakeLists.txt
-  '';
+  buildInputs = [
+    SDL2
+    libGLU
+  ];
 
-  meta = with lib; {
+  cmakeFlags = [
+    (lib.cmakeBool "SDL_gpu_BUILD_DEMOS" false)
+    (lib.cmakeBool "SDL_gpu_BUILD_TOOLS" false)
+    (lib.cmakeBool "SDL_gpu_BUILD_VIDEO_TEST" false)
+    (lib.cmakeBool "SDL_gpu_BUILD_TESTS" false)
+  ];
+
+  outputs = [ "out" "dev" ];
+
+  strictDeps = true;
+
+  meta = {
     description = "A library for high-performance, modern 2D graphics with SDL written in C";
-    homepage = "https://github.com/grimfang4/sdl-gpu";
-    license = licenses.mit;
-    maintainers = with maintainers; [ pmiddend ];
-    platforms = platforms.linux;
+    homepage = "https://grimfang4.github.io/sdl-gpu";
+    license = lib.licenses.mit;
+    maintainers = lib.teams.sdl.members
+                  ++ (with lib.maintainers; [ ]);
+    inherit (SDL2.meta) platforms;
   };
-}
+})
