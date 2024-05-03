c0ad46e480df160c0c51dcf2cc02ba40b730e99c
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-02 21:21:51 -0300

    SDL2_mixer: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24340,8 +24340,6 @@ with pkgs;
     };
   });
 
-  SDL2_mixer = callPackage ../development/libraries/SDL2_mixer { };
-
   SDL2_gfx = callPackage ../development/libraries/SDL2_gfx { };
 
   SDL2_sound = callPackage ../development/libraries/SDL2_sound {
