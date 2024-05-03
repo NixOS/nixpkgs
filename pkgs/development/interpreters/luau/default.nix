f3dc051dc1408cb9a472289da1341575dffe0e8a
Thomas Croft <thomas.croft@proton.me>
2024-05-03 16:55:26 -0600

    Luau: installs libraries + headers

--- pkgs/development/interpreters/luau/default.nix
+++ pkgs/development/interpreters/luau/default.nix
@@ -15,12 +15,61 @@ stdenv.mkDerivation rec {
 
   buildInputs = lib.optionals stdenv.cc.isClang [ llvmPackages.libunwind ];
 
+  patches = [
+    ./add-pkgconfig.patch
+  ];
+
   installPhase = ''
     runHook preInstall
 
-    install -Dm755 -t $out/bin luau
-    install -Dm755 -t $out/bin luau-analyze
-    install -Dm755 -t $out/bin luau-compile
+    mkdir -pv "$out/bin" "$dev/lib/pkgconfig" "$dev/include/Luau"
+
+    local execs=(
+      luau
+      luau-analyze
+      luau-ast
+      luau-bytecode
+      luau-compile
+      luau-reduce
+    )
+    for exec in "''${execs[@]}"; do
+      install -Dm755 -t "$out/bin" "$exec"
+    done
+
+    local libs=(
+      libLuau.Analysis.a
+      libLuau.Ast.a
+      libLuau.CodeGen.a
+      libLuau.Compiler.a
+      libLuau.Config.a
+      libLuau.VM.a
+      libisocline.a
+    )
+    for lib in "''${libs[@]}"; do
+      install -Dm644 -t "$dev/lib" "$lib"
+    done
+
+    local headers=(
+      $TEMPDIR/source/Analysis/include/Luau/*.h
+      $TEMPDIR/source/Ast/include/Luau/*.h
+      $TEMPDIR/source/CodeGen/include/*.h
+      $TEMPDIR/source/CodeGen/include/Luau/*.h
+      $TEMPDIR/source/Common/include/Luau/*.h
+      $TEMPDIR/source/Compiler/include/*.h
+      $TEMPDIR/source/Compiler/include/Luau/*.h
+      $TEMPDIR/source/Config/include/Luau/*.h
+      $TEMPDIR/source/VM/include/*.h
+      $TEMPDIR/source/extern/isocline/include/*.h
+    )
+
+    for header in "''${headers[@]}"; do
+      install -Dm644 -t "$dev/include/Luau" "$header"
+    done
+
+    cp -v "$TEMPDIR/source/Luau.pc" "Luau.pc"
+    substituteInPlace Luau.pc --replace-fail "@out@" "$dev"
+    substituteInPlace Luau.pc --replace-fail "@version@" "${version}"
+    install -Dm644 -t "$dev/lib/pkgconfig" Luau.pc
 
     runHook postInstall
   '';
@@ -37,6 +86,7 @@ stdenv.mkDerivation rec {
   '';
 
   passthru.updateScript = gitUpdater { };
+  outputs = [ "out" "dev" ];
 
   meta = with lib; {
     description = "A fast, small, safe, gradually typed embeddable scripting language derived from Lua";
@@ -46,5 +96,6 @@ stdenv.mkDerivation rec {
     platforms = platforms.all;
     maintainers = [ ];
     mainProgram = "luau";
+    pkgConfigModules = [ "Luau" ];
   };
 }
