89822cc130c8928490caa37e3ab547f41841cd7e
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-03 04:04:37 +0000

    python311Packages.roadlib: 0.23.0 -> 0.24.0

--- pkgs/development/python-modules/roadlib/default.nix
+++ pkgs/development/python-modules/roadlib/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "roadlib";
-  version = "0.23.0";
+  version = "0.24.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0hDiuF0dBRyR2B9dp4c7/jsC6li8uOduQBbhs6fFLfU=";
+    hash = "sha256-+5vR2iTFu50PJIsj4sW6McH1sg3yyEIwk15W8Jk0GKA=";
   };
 
   nativeBuildInputs = [
