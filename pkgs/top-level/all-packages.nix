c610840150c23cced954bba1ada967bcb6aacff0
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-03 08:54:35 -0300

    SDL2_gfx: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24318,8 +24318,6 @@ with pkgs;
     };
   });
 
-  SDL2_gfx = callPackage ../development/libraries/SDL2_gfx { };
-
   SDL2_sound = callPackage ../development/libraries/SDL2_sound {
     inherit (darwin.apple_sdk.frameworks) AudioToolbox CoreAudio;
   };
