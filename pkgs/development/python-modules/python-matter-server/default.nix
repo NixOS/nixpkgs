b2818747ab7cb003f0e7069e50723888e88d8f41
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-05-03 17:51:44 +0200

    python312Packages.python-matter-server: 5.9.0 -> 5.10.0
    https://github.com/home-assistant-libs/python-matter-server/releases/tag/5.10.0

--- pkgs/development/python-modules/python-matter-server/default.nix
+++ pkgs/development/python-modules/python-matter-server/default.nix
@@ -55,7 +55,7 @@ in
 
 buildPythonPackage rec {
   pname = "python-matter-server";
-  version = "5.9.0";
+  version = "5.10.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.10";
@@ -64,7 +64,7 @@ buildPythonPackage rec {
     owner = "home-assistant-libs";
     repo = "python-matter-server";
     rev = "refs/tags/${version}";
-    hash = "sha256-O3AJ7vBjuwRGa4AMwWIdxn5m2F45rLCjCHeff18b/5E=";
+    hash = "sha256-rfpGclSgCBTxlTgVqgNz3ixoldB9M+6mLmogkNDDdWs=";
   };
 
   patches = [
