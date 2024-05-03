1762864450e38314dbe247242ac256835957272f
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-03 23:17:00 +0200

    python312Packages.mypy-boto3-payment-cryptography: 1.34.20 -> 1.34.89

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -540,7 +540,7 @@ rec {
 
   mypy-boto3-panorama = buildMypyBoto3Package "panorama" "1.34.0" "sha256-Pw0yRgawY5TC0OIgcYmzK3KEQusyRf3coThpeHL4X30=";
 
-  mypy-boto3-payment-cryptography = buildMypyBoto3Package "payment-cryptography" "1.34.20" "sha256-WdyhWl00Khf3gA6OeWeKrlgFnTvWhk+AFoS2UhM5Haw=";
+  mypy-boto3-payment-cryptography = buildMypyBoto3Package "payment-cryptography" "1.34.89" "sha256-8zKT6hbI0bc7sZePlS+xYZO2JhsZ2jSmXz1SI0T/1L8=";
 
   mypy-boto3-payment-cryptography-data = buildMypyBoto3Package "payment-cryptography-data" "1.34.58" "sha256-mc4NO3yjdLlXc9TBkmIsGFqNfW2RT7/jVMC9uhug4tc=";
 
