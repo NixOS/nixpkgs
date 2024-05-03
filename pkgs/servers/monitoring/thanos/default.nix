eb0a31c2598cf149153b394d0939b03af14ef944
Jonathan Davies <jpds@protonmail.com>
2024-05-03 16:58:40 +0100

    thanos: 0.34.1 → 0.35.0

--- pkgs/servers/monitoring/thanos/default.nix
+++ pkgs/servers/monitoring/thanos/default.nix
@@ -10,16 +10,16 @@
 
 buildGoModule rec {
   pname = "thanos";
-  version = "0.34.1";
+  version = "0.35.0";
 
   src = fetchFromGitHub {
     owner = "thanos-io";
     repo = "thanos";
     rev = "refs/tags/v${version}";
-    hash = "sha256-AynKxORgO8fY8KAhzyIeHCw2MI2se5eHSPn0kt1RJq4=";
+    hash = "sha256-optV6g3R0W4SP5BJgUOyya+VHHRWrsz5XS7zGuKcN6k=";
   };
 
-  vendorHash = "sha256-4IT3YeGXSEzC0z+rC2yQCY9QQZeXxpMtJM/4/kINn3s=";
+  vendorHash = "sha256-i8EGUxNbxfyPQ3BVa7yBR1ygHIC64v6m/aDGFzWWfIE=";
 
   doCheck = true;
 
