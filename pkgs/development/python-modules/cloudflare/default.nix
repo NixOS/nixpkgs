b830943b3c63f491364238706df923d7dfd685b3
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-04 06:09:24 +0000

    python311Packages.cloudflare: 2.19.2 -> 2.19.4

--- pkgs/development/python-modules/cloudflare/default.nix
+++ pkgs/development/python-modules/cloudflare/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "cloudflare";
-  version = "2.19.2";
+  version = "2.19.4";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ENS5ayrd7gffo2meChZ9930qjVq3+G5lkOqm6ofW3Bg=";
+    hash = "sha256-O2AAoBojfCO8z99tICVupREex0qCaunnT58OW7WyOD8=";
   };
 
   nativeBuildInputs = [
