c5a0d5f10e14af74bf8c3a6affa204a48c1b79c6
Bruno Rodrigues <bruno@brodrigues.co>
2024-05-04 15:24:34 +0200

    rPackages.luajr: fixed build

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -1165,6 +1165,11 @@ let
       patchPhase = "patchShebangs configure";
     });
 
+    luajr = old.luajr.overrideAttrs (attrs: {
+      hardeningDisable = [ "format" ];
+      postPatch = "patchShebangs configure";
+    });
+
     RcppArmadillo = old.RcppArmadillo.overrideAttrs (attrs: {
       patchPhase = "patchShebangs configure";
     });
