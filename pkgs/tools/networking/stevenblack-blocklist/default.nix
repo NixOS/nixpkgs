30498fc5ff3e77e3b514b539acc090a4cbc84810
Guanran Wang <guanran928@outlook.com>
2024-05-04 16:30:01 +0800

    stevenblack-blocklist: 3.14.44 -> 3.14.71

--- pkgs/tools/networking/stevenblack-blocklist/default.nix
+++ pkgs/tools/networking/stevenblack-blocklist/default.nix
@@ -1,7 +1,7 @@
 { lib, fetchFromGitHub }:
 
 let
-  version = "3.14.44";
+  version = "3.14.71";
 in
 fetchFromGitHub {
   name = "stevenblack-blocklist-${version}";
@@ -9,7 +9,7 @@ fetchFromGitHub {
   owner = "StevenBlack";
   repo = "hosts";
   rev = version;
-  sha256 = "sha256-LlTyhtx3DbtsQdkl6J7ktj/zLJULFqQWq5sCqKPX71g=";
+  hash = "sha256-33aDL+nJ+BOwOOSfiaAX3r8BdDM2rWCaeiz55nUXrd8=";
 
   meta = with lib; {
     description = "Unified hosts file with base extensions";
