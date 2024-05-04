13702a189931d562bdfacb3dd8125ffde368998d
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-04 00:04:35 +0000

    intune-portal: 1.2402.12-jammy -> 1.2404.23-jammy

--- pkgs/by-name/in/intune-portal/package.nix
+++ pkgs/by-name/in/intune-portal/package.nix
@@ -23,11 +23,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "intune-portal";
-  version = "1.2402.12-jammy";
+  version = "1.2404.23-jammy";
 
   src = fetchurl {
     url = "https://packages.microsoft.com/ubuntu/22.04/prod/pool/main/i/${pname}/${pname}_${version}_amd64.deb";
-    hash = "sha256-S5+Ddm13OJ4eQTQ9Q8OgsCqnpM6GxuZ1hEycZCy6//g=";
+    hash = "sha256-zAH35iF+3YpDNGo3UhmzL4pRJXlEhG1PaT71qnRtpAg=";
   };
 
   nativeBuildInputs = [ dpkg ];
