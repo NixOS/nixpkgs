562e8b6883227b92aacef3da29340a73dfb2aebd
Inex Code <gitea@inex.rocks>
2024-05-04 22:26:20 +0300

    maintainers: add inexcode

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9366,6 +9366,13 @@
     githubId = 37965;
     name = "Léo Stefanesco";
   };
+  inexcode = {
+    name = "Inex Code";
+    email = "nixpkgs@inex.dev";
+    matrix = "@inexcode:inex.rocks";
+    github = "inexcode";
+    githubId = 13254627;
+  };
   infinidoge = {
     name = "Infinidoge";
     email = "infinidoge@inx.moe";
