f137c814c84cdf0dd452ff3aeed4dbc60f18b1f1
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-05 01:00:11 -0300

    dillo: refactor
    - fix version string
    - strictDeps
    - split outputs
    - no nested with

--- pkgs/by-name/di/dillo/package.nix
+++ pkgs/by-name/di/dillo/package.nix
@@ -1,33 +1,35 @@
-{ lib
-, stdenv
-, fetchhg
-, autoreconfHook
-, fltk
-, libXcursor
-, libXi
-, libXinerama
-, libjpeg
-, libpng
-, mbedtls_2
-, openssl
-, perl
-, pkg-config
-, which
+{
+  lib,
+  autoreconfHook,
+  fetchhg,
+  fltk,
+  libXcursor,
+  libXi,
+  libXinerama,
+  libjpeg,
+  libpng,
+  mbedtls_2,
+  openssl,
+  perl,
+  pkg-config,
+  stdenv,
+  which,
 }:
 
 stdenv.mkDerivation {
   pname = "dillo";
-  version = "unstable-2021-02-09";
+  version = "3.0.5-unstable-2021-02-09";
 
   src = fetchhg {
     url = "https://hg.sr.ht/~seirdy/dillo-mirror";
     rev = "67b70f024568b505633524be61fcfbde5337849f";
-    sha256 = "sha256-lbn5u9oEL0zt9yBhznBS9Dz9/6kSwRDJeNXKEojty1g=";
+    hash = "sha256-lbn5u9oEL0zt9yBhznBS9Dz9/6kSwRDJeNXKEojty1g=";
   };
 
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
+    fltk
     which
   ];
 
@@ -43,21 +45,28 @@ stdenv.mkDerivation {
     perl
   ];
 
+  outputs = [ "out" "doc" "man" ];
+
+  configureFlags = [
+    (lib.enableFeature true "ssl")
+  ];
+
   # Workaround build failure on -fno-common toolchains:
   #   ld: main.o:/build/dillo-3.0.5/dpid/dpid.h:64: multiple definition of `sock_set';
   #     dpid.o:/build/dillo-3.0.5/dpid/dpid.h:64: first defined here
   env.NIX_CFLAGS_COMPILE = "-fcommon";
 
-  configureFlags = [ "--enable-ssl=yes" ];
+  strictDeps = true;
 
-  meta = with lib; {
+  meta = {
     homepage = "https://hg.sr.ht/~seirdy/dillo-mirror";
     description = "A fast graphical web browser with a small footprint";
     longDescription = ''
       Dillo is a small, fast web browser, tailored for older machines.
     '';
-    maintainers = [ maintainers.AndersonTorres ];
-    platforms = platforms.linux;
-    license = licenses.gpl3Plus;
+    mainProgram = "dillo";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
   };
 }
