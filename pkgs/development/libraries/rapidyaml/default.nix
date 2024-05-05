4aa1c01b5c080f4a2bf7268d3d2ed10867e1ed43
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-05-05 17:53:46 +0200

    rapidyaml: set meta.platforms

--- pkgs/development/libraries/rapidyaml/default.nix
+++ pkgs/development/libraries/rapidyaml/default.nix
@@ -24,5 +24,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/biojppm/rapidyaml";
     license = licenses.mit;
     maintainers = with maintainers; [ martfont ];
+    platforms = platforms.all;
   };
 }
