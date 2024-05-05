3fdb75bce34486b2f25ff0208c6498ce85a8d41d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-05 18:45:52 +0200

    build-support/php: move environment variables under `env` attribute
    (cherry picked from commit b5855e4e666231507bdde8982190d5e0486f4c47)

--- pkgs/build-support/php/pkgs/composer-local-repo-plugin.nix
+++ pkgs/build-support/php/pkgs/composer-local-repo-plugin.nix
@@ -38,10 +38,12 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     hash = "sha256-edbn07r/Uc1g0qOuVBZBs6N1bMN5kIfA1b4FCufdw5M=";
   };
 
-  COMPOSER_CACHE_DIR = "/dev/null";
-  COMPOSER_MIRROR_PATH_REPOS = "1";
-  COMPOSER_HTACCESS_PROTECT = "0";
-  COMPOSER_DISABLE_NETWORK = "1";
+  env = {
+    COMPOSER_CACHE_DIR = "/dev/null";
+    COMPOSER_MIRROR_PATH_REPOS = "1";
+    COMPOSER_HTACCESS_PROTECT = "0";
+    COMPOSER_DISABLE_NETWORK = "1";
+  };
 
   nativeBuildInputs = [
     makeBinaryWrapper
