197d6261ebf862d7375d61b9973dfa4e10606103
Roland Conybeare <rconybeare@gmail.com>
2024-05-05 01:26:40 -0400

    python311Packages.sphinxcontrib-ditaa: init at 1.0.2
    Co-authored-by: OTABI Tomoya <tomoya.otabi@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14283,6 +14283,8 @@ self: super: with self; {
 
   sphinxcontrib-devhelp = callPackage ../development/python-modules/sphinxcontrib-devhelp { };
 
+  sphinxcontrib-ditaa = callPackage ../development/python-modules/sphinxcontrib-ditaa { };
+
   sphinxcontrib-excel-table = callPackage ../development/python-modules/sphinxcontrib-excel-table { };
 
   sphinxcontrib-fulltoc = callPackage ../development/python-modules/sphinxcontrib-fulltoc { };
