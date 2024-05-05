329c7a097c8a559be12ec7daf0d5ae150d138ab0
Petingoso <petingavasco@protonmail.com>
2024-05-05 18:32:16 +0100

    maintainers: add petingoso
    maintainers: add ulysseszhan

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17521,6 +17521,12 @@
     githubId = 28323;
     name = "Peter Simons";
   };
+  petingoso = {
+    email = "petingavasco@protonmail.com";
+    github = "Petingoso";
+    githubId = 92183955;
+    name = "Vasco Petinga";
+  };
   petrkozorezov = {
     email = "petr.kozorezov@gmail.com";
     github = "petrkozorezov";
