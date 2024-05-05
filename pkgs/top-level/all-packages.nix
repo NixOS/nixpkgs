e58393e0aa97bb284ba5fe606b04419875d6e923
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-04 23:46:30 -0300

    dillong: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20596,8 +20596,6 @@ with pkgs;
     fltk = fltk13;
   };
 
-  dillong = callPackage ../applications/networking/browsers/dillong { };
-
   directfb = callPackage ../development/libraries/directfb { };
 
   discord-gamesdk = callPackage ../development/libraries/discord-gamesdk { };
