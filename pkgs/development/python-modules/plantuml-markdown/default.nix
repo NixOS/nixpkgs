f544fe041c7f50f9c99e6158b8b0fe8a4508da5a
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-05 03:57:16 +0000

    python311Packages.plantuml-markdown: 3.9.5 -> 3.9.6

--- pkgs/development/python-modules/plantuml-markdown/default.nix
+++ pkgs/development/python-modules/plantuml-markdown/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "plantuml-markdown";
-  version = "3.9.5";
+  version = "3.9.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "mikitex70";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-bfbji517y0/PXMfLg2gGWlogg7IPhXvQ5vekLzc4/ow=";
+    hash = "sha256-LbAQt7cbSUHSU5QfqQWQWuolPxCoXBjmVaw5Tz96fK8=";
   };
 
   propagatedBuildInputs = [
