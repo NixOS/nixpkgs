a0d748ee9cfd8746b58f4a287b3a8ed1abf6d69c
aviac <aviac@mailbox.org>
2024-05-06 16:50:30 +0200

    lib/licenses: add radiance

--- lib/licenses.nix
+++ lib/licenses.nix
@@ -1060,6 +1060,11 @@ in mkLicense lset) ({
     url = "https://qwt.sourceforge.io/qwtlicense.html";
   };
 
+  radiance = {
+    fullName = "The Radiance Software License, Version 2.0";
+    url = "https://github.com/LBNL-ETA/Radiance/blob/master/License.txt";
+  };
+
   ruby = {
     spdxId = "Ruby";
     fullName = "Ruby License";
