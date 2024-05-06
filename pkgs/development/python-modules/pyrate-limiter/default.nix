435ef13d28d62a0375060bf5bc9d94e666c23df8
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-06 12:26:56 +0000

    python311Packages.pyrate-limiter: 2.10.0 -> 3.6.1

--- pkgs/development/python-modules/pyrate-limiter/default.nix
+++ pkgs/development/python-modules/pyrate-limiter/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "pyrate-limiter";
-  version = "2.10.0";
+  version = "3.6.1";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "vutran1710";
     repo = "PyrateLimiter";
-    rev = "v${version}";
-    hash = "sha256-CPusPeyTS+QyWiMHsU0ii9ZxPuizsqv0wQy3uicrDw0=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-uOUvKC+rXzaZfV6lsbsK1WdDGnPjr4HeFDCjNXDWHpw=";
   };
 
   nativeBuildInputs = [
