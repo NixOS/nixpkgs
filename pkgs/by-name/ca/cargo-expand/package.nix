cab5f0bc2652243ffbc1807380621d1691b79b41
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-07 00:29:20 +0000

    cargo-expand: 1.0.85 -> 1.0.86

--- pkgs/by-name/ca/cargo-expand/package.nix
+++ pkgs/by-name/ca/cargo-expand/package.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-expand";
-  version = "1.0.85";
+  version = "1.0.86";
 
   src = fetchFromGitHub {
     owner = "dtolnay";
     repo = pname;
     rev = version;
-    hash = "sha256-2i9FAWF9b1tNdDbTwCzQY8Mh/h85uigR5IT9kzPft00=";
+    hash = "sha256-wp4JYuAHtZrHer4db0vBCchf8mQIt6jXWqjqpGKws4o=";
   };
 
-  cargoHash = "sha256-Vl0zC9TPhiFv2SiZtzIUV7GftB1y9K1gLy1ajisP8Y0=";
+  cargoHash = "sha256-QNH3G34yTEecZyVgw2a1RJnd1CMfV4aw1OJqes/cK2s=";
 
   meta = with lib; {
     description = "Cargo subcommand to show result of macro expansion";
