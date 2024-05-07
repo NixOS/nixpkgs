3605676da26e447b2b75308b737ff35f0985573d
Alyssa Ross <hi@alyssa.is>
2024-05-07 19:53:51 +0200

    modemmanager: remove unused udev input
    This is only used to determine the default value of udevdir, which we
    set explicitly.

--- pkgs/tools/networking/modemmanager/default.nix
+++ pkgs/tools/networking/modemmanager/default.nix
@@ -3,7 +3,6 @@
 , fetchFromGitLab
 , fetchpatch
 , glib
-, udev
 , libgudev
 , polkit
 , ppp
@@ -62,7 +61,6 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     glib
-    udev
     libgudev
     polkit
     ppp
