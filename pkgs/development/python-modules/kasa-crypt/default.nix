bb5ed2b5945242d6571376fd2290c410a2d79470
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-08 00:07:54 +0200

    python312Packages.kasa-crypt: 0.4.1 -> 0.4.2
    Diff: https://github.com/bdraco/kasa-crypt/compare/refs/tags/v0.4.1...v0.4.2
    
    Changelog: https://github.com/bdraco/kasa-crypt/blob/v0.4.2/CHANGELOG.md

--- pkgs/development/python-modules/kasa-crypt/default.nix
+++ pkgs/development/python-modules/kasa-crypt/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "kasa-crypt";
-  version = "0.4.1";
+  version = "0.4.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "bdraco";
     repo = "kasa-crypt";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ZAynSL6tIQoe9veYGusel9GQEffeLQ8dBA9HfA6TMzI=";
+    hash = "sha256-7PLOuWxA5ziOuysSu+nLWNmNA3/sbuUCuZcMRBflf6U=";
   };
 
   postPatch = ''
