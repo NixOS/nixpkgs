bf65333707722da33ce3260a1b918302c0144d0b
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-07 23:06:47 +0000

    python311Packages.dissect-thumbcache: 1.7 -> 1.8

--- pkgs/development/python-modules/dissect-thumbcache/default.nix
+++ pkgs/development/python-modules/dissect-thumbcache/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "dissect-thumbcache";
-  version = "1.7";
+  version = "1.8";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "fox-it";
     repo = "dissect.thumbcache";
     rev = "refs/tags/${version}";
-    hash = "sha256-t5LovLS+YMWQWMCFP6jh3SNiLb/Jy9we4FX6MmsIJmY=";
+    hash = "sha256-LcuuRVSnZVFtht03MD8qVNNPljVUzGa8vXu9GQPOgjs=";
   };
 
   nativeBuildInputs = [
