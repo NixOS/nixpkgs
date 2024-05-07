NixOS/nixpkgs

commit 234502f34803c2630d97a55c2d5b98b1108192fc

libmbim: remove unused systemd dependency
This was originally a dependency on udev, from back when libmbim used
udev rather than directly interacting with sysfs.  It was converted
from udev to systemd in 005c3cffc15c ("libmbim: 1.16.2 -> 1.18.0"),
presumably just because at the time udev was considered to be a
deprecated alias for systemd, because the libmbim source code from
that time doesn't mention systemd.  Nowadays, neither udev or systemd
is mentioned in the libmbim source code, except that it installs some
udev rules of its own.

pkgs/development/libraries/libmbim/default.nix
@@ -52,7 +52,6 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     glib
-    systemd
     bash-completion
     bash
   ];

commit 8ea6b8dcacecd3b6565978c552c8148f8ce19d77

libmbim: remove unused systemd input
Oops.

Fixes: 234502f34803 ("libmbim: remove unused systemd dependency")

pkgs/development/libraries/libmbim/default.nix
@@ -7,7 +7,6 @@
 , glib
 , python3
 , help2man
-, systemd
 , bash-completion
 , bash
 , buildPackages