69f65113c0a39aa4b03b4cf9c14873338c88d5a2
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-08 00:12:38 +0200

    python311Packages.dissect-cstruct: 3.13 -> 3.14
    Diff: https://github.com/fox-it/dissect.cstruct/compare/refs/tags/3.13...3.14
    
    Changelog: https://github.com/fox-it/dissect.cstruct/releases/tag/3.14

--- pkgs/development/python-modules/dissect-cstruct/default.nix
+++ pkgs/development/python-modules/dissect-cstruct/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "dissect-cstruct";
-  version = "3.13";
+  version = "3.14";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "fox-it";
     repo = "dissect.cstruct";
     rev = "refs/tags/${version}";
-    hash = "sha256-QfnvWCI67y8GdCfXIRaehzb26IVZ8N0muUnNpr1NGJo=";
+    hash = "sha256-Et6Pb7iRVlxcrR4gZf+5h7mVikf7CyDd7N+q4TH18vg=";
   };
 
   nativeBuildInputs = [
