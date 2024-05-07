aff8d11104fc569f8f93a5d1cc1d5a2e6219ab3a
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-07 01:05:05 +0000

    i3bar-river: 0.1.7 -> 0.1.8

--- pkgs/by-name/i3/i3bar-river/package.nix
+++ pkgs/by-name/i3/i3bar-river/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "i3bar-river";
-  version = "0.1.7";
+  version = "0.1.8";
 
   src = fetchFromGitHub {
     owner = "MaxVerevkin";
     repo = "i3bar-river";
     rev = "v${version}";
-    hash = "sha256-mLRB4o8FR/R9QUpRkcNppiE2XcWFWE05wPxuKdxG18M=";
+    hash = "sha256-Rw4jildX3t853hIwEem/KzTBUyO3a/kour3dvSw8DVA=";
   };
 
-  cargoHash = "sha256-INjuI3XTSzAjLqk/P+cd7rMhXsOBDSqMaZZN9kFyreg=";
+  cargoHash = "sha256-uGzXEeQ2yzk8HEdgY/gTxqaCoMO25kbiD1XrpJwmVp4=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ pango ];
