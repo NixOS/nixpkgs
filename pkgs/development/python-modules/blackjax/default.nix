4d1a4f546a1914a4a321e183174ef02c9df2ba35
Gaetan Lepage <gaetan@glepage.com>
2024-05-08 20:04:17 +0200

    python311Packages.blackjax: 1.2.0 -> 1.2.1
    Diff: https://github.com/blackjax-devs/blackjax/compare/refs/tags/1.2.0...1.2.1
    
    Changelog: https://github.com/blackjax-devs/blackjax/releases/tag/1.2.1

--- pkgs/development/python-modules/blackjax/default.nix
+++ pkgs/development/python-modules/blackjax/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "blackjax";
-  version = "1.2.0";
+  version = "1.2.1";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "blackjax-devs";
     repo = "blackjax";
     rev = "refs/tags/${version}";
-    hash = "sha256-vXyxK3xALKG61YGK7fmoqQNGfOiagHFrvnU02WKZThw=";
+    hash = "sha256-VoWBCjFMyE5LVJyf7du/pKlnvDHj22lguiP6ZUzH9ak=";
   };
 
   build-system = [
