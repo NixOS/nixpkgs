43396e552aee214419b3d29b5abe134456de46c8
Florian Brandes <florian.brandes@posteo.de>
2024-05-08 14:02:56 +0200

    python311Packages.libgravatar: init at 1.0.4
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6638,6 +6638,8 @@ self: super: with self; {
     inherit (pkgs.config) cudaSupport;
   };
 
+  libgravatar = callPackage ../development/python-modules/libgravatar { };
+
   libiio = (toPythonModule (pkgs.libiio.override {
     pythonSupport = true;
     inherit python;
