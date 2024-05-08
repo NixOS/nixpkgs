f22cbdca656529ec1543eb5cb148a98b66ac6acf
Joachim Ernst <joachim.ernst@helsinki-systems.de>
2024-05-08 22:04:45 +0200

    unbound: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14449,8 +14449,6 @@ with pkgs;
 
   unclutter-xfixes = callPackage ../tools/misc/unclutter-xfixes { };
 
-  unbound = callPackage ../tools/networking/unbound { };
-
   unbound-with-systemd = unbound.override {
     withSystemd = true;
   };
