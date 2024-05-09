5510aae4c83dda1992d2b6af2870d078123b4894
Sefa Eyeoglu <contact@scrumplex.net>
2024-05-09 15:30:03 +0200

    openvr: add update script
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/development/libraries/openvr/default.nix
+++ pkgs/development/libraries/openvr/default.nix
@@ -7,6 +7,7 @@
 , lib
 , libGL
 , stdenv
+, nix-update-script
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -55,6 +56,8 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   cmakeFlags = [ "-DUSE_SYSTEM_JSONCPP=ON" "-DBUILD_SHARED=1" ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     broken = stdenv.isDarwin;
