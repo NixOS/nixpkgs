f4dc31f58b78d5a945623222f8e2e0aa975fc7f1
Nick Cao <nickcao@nichi.co>
2024-05-09 17:01:10 -0400

    hugo: 0.125.6 -> 0.125.7
    Diff: https://github.com/gohugoio/hugo/compare/refs/tags/v0.125.6...v0.125.7
    
    Changelog: https://github.com/gohugoio/hugo/releases/tag/v0.125.7

--- pkgs/by-name/hu/hugo/package.nix
+++ pkgs/by-name/hu/hugo/package.nix
@@ -10,16 +10,16 @@
 
 buildGoModule rec {
   pname = "hugo";
-  version = "0.125.6";
+  version = "0.125.7";
 
   src = fetchFromGitHub {
     owner = "gohugoio";
     repo = "hugo";
     rev = "refs/tags/v${version}";
-    hash = "sha256-rOkvt+U8iju+oIb/BfPMHSqhZYM6XSUS2B8Oxd46cF8=";
+    hash = "sha256-aONA9qfoilte73wBTZFu8rrVz+O8xtnRk/rOxJLANS8=";
   };
 
-  vendorHash = "sha256-UJoK73oQ1gH4Y1hxjE66Ou8o9jAeJpA4njgP3VHu68s=";
+  vendorHash = "sha256-ZEaByHlJIfzGbmdadDpCJGCybj9SOHRzrrzR/S/zRnc=";
 
   doCheck = false;
 
