5ceb5e9964d8fe77daaf640af9893ac8da0d2a17
Winston R. Milling <Winston@Milli.ng>
2024-05-10 15:36:01 -0500

    k3s: Add wrmilling as maintainer

--- pkgs/applications/networking/cluster/k3s/builder.nix
+++ pkgs/applications/networking/cluster/k3s/builder.nix
@@ -91,7 +91,7 @@ let
     description = "A lightweight Kubernetes distribution";
     license = licenses.asl20;
     homepage = "https://k3s.io";
-    maintainers = with maintainers; [ euank mic92 superherointj yajo ];
+    maintainers = with maintainers; [ euank mic92 superherointj yajo wrmilling ];
     platforms = platforms.linux;
 
     # resolves collisions with other installations of kubectl, crictl, ctr
