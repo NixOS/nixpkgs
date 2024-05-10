b29fd82b11964588948547833bf19d77efb2cdfd
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-10 04:01:47 +0000

    beanhub-extract: init at 0.0.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1463,6 +1463,8 @@ self: super: with self; {
 
   beancount-docverif = callPackage ../development/python-modules/beancount-docverif { };
 
+  beanhub-extract = callPackage ../development/python-modules/beanhub-extract { };
+
   beanstalkc = callPackage ../development/python-modules/beanstalkc { };
 
   beartype = callPackage ../development/python-modules/beartype { };
