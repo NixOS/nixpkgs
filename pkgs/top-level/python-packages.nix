3ed78d592d7c7db0f1ed484dc1a27af078a7d056
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-05-10 02:44:37 -0400

    python3Packages.pyjet: remove

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11153,8 +11153,6 @@ self: super: with self; {
 
   pyisbn = callPackage ../development/python-modules/pyisbn { };
 
-  pyjet = callPackage ../development/python-modules/pyjet { };
-
   pyjks = callPackage ../development/python-modules/pyjks { };
 
   pyjnius = callPackage ../development/python-modules/pyjnius { };
