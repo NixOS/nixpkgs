906a5b5809f6a4678a2515d500c2fbb597bf4a88
Coutinho de Souza <dev@onemoresuza.com>
2024-05-10 17:01:33 -0300

    hare: refactor cross-compilation tests
    Now it does test for cross-compilation.

--- pkgs/by-name/ha/hare/cross-compilation-tests.nix
+++ pkgs/by-name/ha/hare/cross-compilation-tests.nix
@@ -1,31 +1,32 @@
-{ lib
-, buildPackages
-, hare
-, runCommandNoCC
-, stdenv
-, writeText
+{
+  lib,
+  hare,
+  runCommandNoCC,
+  writeText,
 }:
 let
-  inherit (stdenv.hostPlatform.uname) processor;
-  inherit (stdenv.hostPlatform) emulator;
+  archs = lib.concatStringsSep " " (
+    builtins.map (lib.removeSuffix "-linux") (
+      builtins.filter (lib.hasSuffix "-linux") hare.meta.platforms
+    )
+  );
   mainDotHare = writeText "main.ha" ''
-    use fmt;
-    use os;
-    export fn main() void = {
-        const machine = os::machine();
-        if (machine == "${processor}") {
-            fmt::println("os::machine() matches ${processor}")!;
-        } else {
-            fmt::fatalf("os::machine() does not match ${processor}: {}", machine);
-        };
-    };
+    export fn main() void = void;
   '';
 in
-runCommandNoCC "${hare.pname}-cross-compilation-test" { meta.timeout = 60; } ''
-  HARECACHE="$(mktemp -d --tmpdir harecache.XXXXXXXX)"
+runCommandNoCC "${hare.pname}-cross-compilation-test" { nativeBuildInputs = [ hare ]; } ''
+  HARECACHE="$(mktemp -d)"
   export HARECACHE
-  outbin="test-${processor}"
-  ${lib.getExe hare} build -q -a "${processor}" -o "$outbin" ${mainDotHare}
-  ${emulator buildPackages} "./$outbin"
+  readonly binprefix="bin"
+  for a in ${archs}; do
+    outbin="$binprefix-$a"
+    set -x
+    hare build -o "$outbin" -q -R -a "$a" ${mainDotHare}
+    set +x
+    printf -- 'Built "%s" target\n' "$a"
+  done
+
+  printf -- 'Generated binary file: %s\n' "$binprefix-"*
+
   : 1>$out
 ''
