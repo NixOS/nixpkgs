c6b17b3071c0a4cfa8802134a64a900a58376f61
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-10 20:11:42 -0300

    SDL2_ttf: refactor
    - strictDeps
    - use system harfbuzz and freetype instead of vendored
    - sdl team

--- pkgs/by-name/sd/SDL2_ttf/package.nix
+++ pkgs/by-name/sd/SDL2_ttf/package.nix
@@ -1,32 +1,65 @@
-{ lib, stdenv, pkg-config, darwin, fetchurl, SDL2, freetype, harfbuzz, libGL, testers }:
+{
+  lib,
+  SDL2,
+  darwin,
+  fetchurl,
+  freetype,
+  harfbuzz,
+  libGL,
+  pkg-config,
+  stdenv,
+  testers,
+  # Boolean flags
+  enableSdltest ? (!stdenv.isDarwin),
+}:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "SDL2_ttf";
   version = "2.22.0";
 
   src = fetchurl {
-    url = "https://www.libsdl.org/projects/SDL_ttf/release/${finalAttrs.pname}-${finalAttrs.version}.tar.gz";
+    url = "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-${finalAttrs.version}.tar.gz";
     sha256 = "sha256-1Iy9HOR1ueF4IGvzty1Wtm2E1E9krAWAMyg5YjTWdyM=";
   };
 
-  configureFlags = [ "--disable-harfbuzz-builtin" ]
-    ++ lib.optionals stdenv.isDarwin [ "--disable-sdltest" ];
+  nativeBuildInputs = [
+    SDL2
+    pkg-config
+  ];
 
-  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [
+    SDL2
+    freetype
+    harfbuzz
+  ]
+  ++ lib.optionals (!stdenv.isDarwin) [
+    libGL
+  ]
+  ++ lib.optionals stdenv.isDarwin [
+    darwin.libobjc
+  ];
 
-  buildInputs = [ SDL2 freetype harfbuzz ]
-    ++ lib.optional (!stdenv.isDarwin) libGL
-    ++ lib.optional stdenv.isDarwin darwin.libobjc;
+  configureFlags = [
+    (lib.enableFeature false "harfbuzz-builtin")
+    (lib.enableFeature false "freetype-builtin")
+    (lib.enableFeature enableSdltest "sdltest")
+  ];
 
-  passthru.tests.pkg-config = testers.hasPkgConfigModules {
-    package = finalAttrs.finalPackage;
+  strictDeps = true;
+
+  passthru = {
+    tests.pkg-config = testers.hasPkgConfigModules {
+      package = finalAttrs.finalPackage;
+    };
   };
 
-  meta = with lib; {
-    description = "Support for TrueType (.ttf) font files with Simple Directmedia Layer";
-    platforms = platforms.unix;
-    license = licenses.zlib;
+  meta = {
     homepage = "https://github.com/libsdl-org/SDL_ttf";
+    description = "Support for TrueType (.ttf) font files with Simple Directmedia Layer";
+    license = lib.licenses.zlib;
+    maintainers = lib.teams.sdl.members
+                  ++ (with lib.maintainers; [ ]);
+    inherit (SDL2.meta) platforms;
     pkgConfigModules = [ "SDL2_ttf" ];
   };
 })
