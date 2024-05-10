9b790335f116ef075b6db741acd7608f29469f26
Moraxyc <i@qaq.li>
2024-05-10 10:29:44 +0800

    nixos/cyrus-imap: init module
    Co-authored-by: jtbx <jeremy@baxters.nz>
    Co-authored-by: pluiedev <hi@pluie.me>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -661,6 +661,7 @@
   ./services/logging/ulogd.nix
   ./services/mail/automx2.nix
   ./services/mail/clamsmtp.nix
+  ./services/mail/cyrus-imap.nix
   ./services/mail/davmail.nix
   ./services/mail/dkimproxy-out.nix
   ./services/mail/dovecot.nix
