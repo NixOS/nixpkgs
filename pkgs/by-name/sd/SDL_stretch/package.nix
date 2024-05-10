af8e579938097727c8064ef01c86236e5dd1c73a
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-10 20:05:26 -0300

    SDL_stretch: refactor
    - finalAttrs
    - strictDeps
    - sdl team

--- pkgs/by-name/sd/SDL_stretch/package.nix
+++ pkgs/by-name/sd/SDL_stretch/package.nix
@@ -1,20 +1,31 @@
-{ lib, stdenv, fetchurl, SDL }:
+{
+  lib,
+  SDL,
+  fetchurl,
+  stdenv,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "SDL_stretch";
   version = "0.3.1";
 
   src = fetchurl {
-    url = "mirror://sourceforge/sdl-stretch/${version}/${pname}-${version}.tar.bz2";
-    sha256 = "1mzw68sn4yxbp8429jg2h23h8xw2qjid51z1f5pdsghcn3x0pgvw";
+    url = "mirror://sourceforge/sdl-stretch/${finalAttrs.version}/SDL_stretch-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-fL8L+rAMPt1uceGH0qLEgncEh4DiySQIuqt7YjUy/Nc=";
   };
+
+  nativeBuildInputs = [ SDL ];
 
   buildInputs = [ SDL ];
 
-  meta = with lib; {
-     description = "Stretch Functions For SDL";
+  strictDeps = true;
+
+  meta = {
      homepage = "https://sdl-stretch.sourceforge.net/";
-     license = licenses.lgpl2;
-     platforms = platforms.linux;
+     description = "Stretch Functions For SDL";
+     license = lib.licenses.lgpl2;
+     maintainers = lib.teams.sdl.members
+                  ++ (with lib.maintainers; [ ]);
+     inherit (SDL.meta) platforms;
   };
-}
+})
