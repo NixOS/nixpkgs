7acf24e0932b2320234ef932658b06f5df6c0e0e
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-10 20:18:10 -0300

    guile-sdl: refactor and fixup
    - finalAttrs
    - Call getDev for all SDL-related build inputs

--- pkgs/by-name/gu/guile-sdl/package.nix
+++ pkgs/by-name/gu/guile-sdl/package.nix
@@ -1,60 +1,62 @@
-{ lib
-, stdenv
-, fetchurl
-, guile
-, lzip
-, pkg-config
-, SDL
-, SDL_image
-, SDL_mixer
-, SDL_ttf
-, buildEnv
+{
+  lib,
+  SDL,
+  SDL_image,
+  SDL_mixer,
+  SDL_ttf,
+  buildEnv,
+  fetchurl,
+  guile,
+  lzip,
+  pkg-config,
+  stdenv,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "guile-sdl";
   version = "0.6.1";
 
   src = fetchurl {
-    url = "mirror://gnu/${pname}/${pname}-${version}.tar.lz";
+    url = "mirror://gnu/guile-sdl/guile-sdl-${finalAttrs.version}.tar.lz";
     hash = "sha256-/9sTTvntkRXck3FoRalROjqUQC8hkePtLTnHNZotKOE=";
   };
 
-  strictDeps = true;
-
   nativeBuildInputs = [
+    SDL
     guile
     lzip
     pkg-config
-    SDL
   ];
 
   buildInputs = [
-    guile
     (lib.getDev SDL)
-    SDL_image
-    SDL_mixer
-    SDL_ttf
+    (lib.getDev SDL_image)
+    (lib.getDev SDL_mixer)
+    (lib.getDev SDL_ttf)
+    guile
   ];
 
   makeFlags =
     let
       sdl-env = buildEnv {
         name = "sdl-env";
-        paths = buildInputs;
+        paths = finalAttrs.buildInputs;
       };
     in
     [
       "SDLMINUSI=-I${sdl-env}/include/SDL"
     ];
 
-  meta = with lib; {
+  strictDeps = true;
+
+  meta = {
     homepage = "https://www.gnu.org/software/guile-sdl/";
     description = "Guile bindings for SDL";
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ vyp ];
-    platforms = guile.meta.platforms;
     # configure: error: *** SDL version  not found!
     broken = stdenv.isDarwin;
+    license = lib.licenses.gpl3Plus;
+    maintainers = lib.teams.sdl.members
+                  ++ (with lib.maintainers; [ vyp ]);
+    inherit (guile.meta) platforms;
   };
-}
+})
