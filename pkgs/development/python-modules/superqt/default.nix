9a6840a19c7eb2fca7061c9c2d4f3d1c4df20663
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-10 15:01:55 +0200

    python311Packages.superqt: 0.3.8 -> 0.6.5
    Diff: https://github.com/pyapp-kit/superqt/compare/refs/tags/v0.3.8...v0.6.5
    
    Changelog: https://github.com/pyapp-kit/superqt/releases/tag/v0.6.5

--- pkgs/development/python-modules/superqt/default.nix
+++ pkgs/development/python-modules/superqt/default.nix
@@ -2,33 +2,40 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  setuptools-scm,
+  hatch-vcs,
+  hatchling,
   pyqt5,
   qtpy,
   typing-extensions,
   pytestCheckHook,
   pygments,
+  pythonOlder,
 }:
 
 buildPythonPackage rec {
   pname = "superqt";
   version = "0.6.5";
-  format = "pyproject";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
-    owner = "napari";
-    repo = pname;
+    owner = "pyapp-kit";
+    repo = "superqt";
     rev = "refs/tags/v${version}";
     hash = "sha256-zEMG2zscGDlRxtLn/lUTEjZBPabcwzMcj/kMcy3yOs8=";
   };
 
-  nativeBuildInputs = [ setuptools-scm ];
+  build-system = [
+    hatch-vcs
+    hatchling
+  ];
 
-  propagatedBuildInputs = [
+  dependencies = [
+    pygments
     pyqt5
     qtpy
     typing-extensions
-    pygments
   ];
 
   nativeCheckInputs = [ pytestCheckHook ];
@@ -40,6 +47,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Missing widgets and components for Qt-python (napari/superqt)";
     homepage = "https://github.com/napari/superqt";
+    changelog = "https://github.com/pyapp-kit/superqt/releases/tag/v${version}";
     license = licenses.bsd3;
     maintainers = with maintainers; [ SomeoneSerge ];
   };
