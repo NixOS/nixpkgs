1bcc0baeaa8b89afbd30551a67cd1154a0555078
Sigmanificient <edhyjox@gmail.com>
2024-05-10 05:43:23 +0200

    zz: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25147,8 +25147,6 @@ with pkgs;
 
   zix = callPackage ../development/libraries/audio/zix { };
 
-  zz = callPackage ../development/compilers/zz { };
-
   zziplib = callPackage ../development/libraries/zziplib { };
 
   glpng = callPackage ../development/libraries/glpng { };
