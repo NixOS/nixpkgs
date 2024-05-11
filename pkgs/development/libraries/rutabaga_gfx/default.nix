NixOS/nixpkgs

commit 28257606653cef6913d4ccd76ead276f050507e1

rutabaga_gfx: init at 0.1.2
(cherry picked from commit a8c49d6e2801627f97e0b4ceda1ccd4bf4ec8698)

pkgs/development/libraries/rutabaga_gfx/default.nix
@@ -1,0 +1,41 @@
+{ lib, stdenv, fetchgit, cargo, pkg-config, rustPlatform
+, aemu, gfxstream, libcap, libdrm, minijail
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "rutabaga_gfx";
+  version = "0.1.2";
+
+  src = fetchgit {
+    url = "https://chromium.googlesource.com/crosvm/crosvm";
+    rev = "v${finalAttrs.version}-rutabaga-release";
+    fetchSubmodules = true;
+    hash = "sha256-0RJDKzeU7U6hc6CLKks8QcRs3fxN+/LYUbB0t6W790M=";
+  };
+
+  nativeBuildInputs = [ cargo pkg-config rustPlatform.cargoSetupHook ];
+  buildInputs = [ aemu gfxstream ]
+    ++ lib.optional (lib.meta.availableOn stdenv.hostPlatform libdrm) libdrm;
+
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    inherit (finalAttrs) src;
+    hash = "sha256-wuF3Isrp+u5J8jPQoPsIOWYGNKLSNa2pLfvladAWkLs=";
+  };
+
+  # make install always rebuilds
+  dontBuild = true;
+
+  makeFlags = [ "prefix=$(out)" ];
+
+  preInstall = ''
+    cd rutabaga_gfx/ffi
+  '';
+
+  meta = with lib; {
+    homepage = "https://crosvm.dev/book/appendix/rutabaga_gfx.html";
+    description = "cross-platform abstraction for GPU and display virtualization";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ qyliss ];
+    platforms = platforms.darwin ++ platforms.linux;
+  };
+})

pkgs/top-level/all-packages.nix
@@ -24852,6 +24852,8 @@ with pkgs;
 
   rubberband = callPackage ../development/libraries/rubberband { };
 
+  rutabaga_gfx = callPackage ../development/libraries/rutabaga_gfx { };
+
   rure = callPackage ../development/libraries/rure { };
 
   rustc-demangle = callPackage ../development/libraries/rustc-demangle { };

commit 19bb0a96f41e639ea0b33dc92f9caa94d27e5a38

rutabaga_gfx: build for the right architecture
Previously, cross compiling rutabaga_gfx looked like it worked, but it
produced binaries for the build machine.  This prevented
cross-compiling QEMU with rutabaga_gfx support enabled.

Fixes: 28257606653c ("rutabaga_gfx: init at 0.1.2")

pkgs/development/libraries/rutabaga_gfx/default.nix
@@ -54,14 +54,22 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-wuF3Isrp+u5J8jPQoPsIOWYGNKLSNa2pLfvladAWkLs=";
   };
 
+  CARGO_BUILD_TARGET = stdenv.hostPlatform.rust.rustcTargetSpec;
+  "CARGO_TARGET_${stdenv.hostPlatform.rust.cargoEnvVarTarget}_LINKER" =
+    "${stdenv.cc.targetPrefix}cc";
+
+  postConfigure = ''
+    cd rutabaga_gfx/ffi
+    substituteInPlace Makefile --replace-fail pkg-config "$PKG_CONFIG"
+  '';
+
   # make install always rebuilds
   dontBuild = true;
 
-  makeFlags = [ "prefix=$(out)" ];
-
-  preInstall = ''
-    cd rutabaga_gfx/ffi
-  '';
+  makeFlags = [
+    "prefix=$(out)"
+    "OUT=target/${stdenv.hostPlatform.rust.cargoShortTarget}/release"
+  ];
 
   meta = with lib; {
     homepage = "https://crosvm.dev/book/appendix/rutabaga_gfx.html";