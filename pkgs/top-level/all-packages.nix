aa0026163f87d37fd36a081094657dbd32a3fce8
Kris Budde <kris@budd.ee>
2024-05-11 10:56:43 +0200

    move myks to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19193,8 +19193,6 @@ with pkgs;
 
   msitools = callPackage ../development/tools/misc/msitools { };
 
-  myks = callPackage ../development/tools/myks { };
-
   haskell-ci = haskell.lib.compose.justStaticExecutables haskellPackages.haskell-ci;
 
   nailgun = callPackage ../development/tools/nailgun { };
