ba53e70800050586be06c2e1ff37074de2a6e108
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-11 22:10:19 +0200

    python311Packages.html-text: 0.5.2 -> 0.6.2
    Diff: https://github.com/zytedata/html-text/compare/refs/tags/0.5.2...0.6.2
    
    Changelog: https://github.com/zytedata/html-text/blob/0.6.2/CHANGES.rst

--- pkgs/development/python-modules/html-text/default.nix
+++ pkgs/development/python-modules/html-text/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "html-text";
-  version = "0.5.2";
+  version = "0.6.2";
   format = "setuptools";
 
   src = fetchFromGitHub {
-    owner = "TeamHG-Memex";
+    owner = "zytedata";
     repo = "html-text";
-    rev = version;
-    hash = "sha256-jw/hpz0QfcgP5OEJcmre0h1OzOfpPtaROxHm+YUqces=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-e9gkibQv8mn1Jbt77UmpauOeTqhJQhY9R5Sge/iYi+U=";
   };
 
   propagatedBuildInputs = [
@@ -31,7 +31,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Extract text from HTML";
-    homepage = "https://github.com/TeamHG-Memex/html-text";
+    homepage = "https://github.com/zytedata/html-text";
+    changelog = "https://github.com/zytedata/html-text/blob/${version}/CHANGES.rst";
     license = licenses.mit;
     maintainers = with maintainers; [ ambroisie ];
   };
