1086e16770713117b2b450f2bf8dc487a92ec637
Dominique Martinet <asmadeus@codewreck.org>
2024-05-11 17:18:09 +0900

    nixos/tests/cryptpad: fix test

--- nixos/tests/cryptpad.nix
+++ nixos/tests/cryptpad.nix
@@ -3,8 +3,11 @@ let
   certs = pkgs.runCommand "cryptpadSelfSignedCerts" { buildInputs = [ pkgs.openssl ]; } ''
     mkdir -p $out
     cd $out
-    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes -subj '/CN=cryptpad.localhost' -days 36500
-    openssl req -x509 -newkey rsa:4096 -keyout sandbox-key.pem -out sandbox-cert.pem -nodes -subj '/CN=cryptpad-sandbox.localhost' -days 36500
+    openssl req -x509 -newkey rsa:4096 \
+      -keyout key.pem -out cert.pem -nodes -days 3650 \
+      -subj '/CN=cryptpad.localhost' \
+      -addext 'subjectAltName = DNS.1:cryptpad.localhost, DNS.2:cryptpad-sandbox.localhost'
+    #openssl req -x509 -newkey rsa:4096 -keyout sandbox-key.pem -out sandbox-cert.pem -nodes -subj '/CN=cryptpad-sandbox.localhost' -days 36500
   '';
   # data sniffed from cryptpad's /checkup network trace, seems to be re-usable
   test_write_data = pkgs.writeText "cryptpadTestData" ''
@@ -13,30 +16,21 @@ let
   machine_config = {
     services.cryptpad = {
       enable = true;
+      configureNginx = true;
       settings = {
         httpUnsafeOrigin = "https://cryptpad.localhost";
         httpSafeOrigin = "https://cryptpad-sandbox.localhost";
       };
     };
     services.nginx = {
-      enable = true;
-      recommendedTlsSettings = true;
-      recommendedOptimisation = true;
-      recommendedGzipSettings = true;
-      recommendedProxySettings = true;
       virtualHosts."cryptpad.localhost" = {
         enableACME = false;
         sslCertificate = "${certs}/cert.pem";
         sslCertificateKey = "${certs}/key.pem";
       };
-      virtualHosts."cryptpad-sandbox.localhost" = {
-        enableACME = false;
-        sslCertificate = "${certs}/sandbox-cert.pem";
-        sslCertificateKey = "${certs}/sandbox-key.pem";
-      };
     };
     security = {
-      pki.certificateFiles = [ "${certs}/cert.pem" "${certs}/sandbox-cert.pem" ];
+      pki.certificateFiles = [ "${certs}/cert.pem" ];
     };
   };
   test_script = machine: ''
@@ -47,14 +41,15 @@ let
     # test home page
     ${machine}.succeed("curl --fail https://cryptpad.localhost -o /tmp/cryptpad_home.html")
     ${machine}.succeed("grep -F 'CryptPad: Collaboration suite' /tmp/cryptpad_home.html")
-    # test npm run build actually did what it should (part of checkup)
-    ${machine}.succeed("grep -F 'og:url' /tmp/cryptpad_home.html")
+
+    # test scripts/build.js actually generated customize content from config
+    ${machine}.succeed("grep -F 'meta property=\"og:url\" content=\"https://cryptpad.localhost/index.html' /tmp/cryptpad_home.html")
 
     # make sure child pages are accessible (e.g. check nginx try_files paths)
     ${machine}.succeed(
         "grep -oE '/(customize|components)[^\"]*' /tmp/cryptpad_home.html"
-        "  | while read page; do"
-        "        curl -O --fail https://cryptpad.localhost/$page || exit;"
+        "  | while read -r page; do"
+        "        curl -O --fail https://cryptpad.localhost$page || exit;"
         "    done")
 
     # test some API (e.g. check cryptpad main process)
