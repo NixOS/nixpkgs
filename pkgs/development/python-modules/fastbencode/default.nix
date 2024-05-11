eb945009a2c38bce25b04016c5992ef9a0c2b5f9
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-11 20:24:10 +0200

    python312Packages.fastbencode: refactor

--- pkgs/development/python-modules/fastbencode/default.nix
+++ pkgs/development/python-modules/fastbencode/default.nix
@@ -1,22 +1,27 @@
 { lib
 , buildPythonPackage
+, cython
 , fetchPypi
 , python
 , pythonOlder
-, cython
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "fastbencode";
   version = "0.3.1";
-  format = "setuptools";
+  pyproject  =true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-X+DLfRc2iRr2HSreQM6UiUHUbpCLFvU4P1XxJ4SNoZc=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   nativeBuildInputs = [
     cython
@@ -33,6 +38,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Fast implementation of bencode";
     homepage = "https://github.com/breezy-team/fastbencode";
+    changelog = "https://github.com/breezy-team/fastbencode/releases/tag/v${version}";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ ];
   };
