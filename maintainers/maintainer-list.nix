bb74f2396ebe1c952176cfa8a8ae44cbc48b5fa6
bwkam <userdev987@gmail.com>
2024-05-12 00:50:57 +0300

    maintainers: add bwkam

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3031,6 +3031,12 @@
     githubId = 25644444;
     name = "Ben C";
   };
+  bwkam = {
+    email = "beshoykamel391@gmail.com";
+    github = "bwkam";
+    githubId = 91009118;
+    name = "Beshoy Kamel";
+  };
   bwlang = {
     email = "brad@langhorst.com";
     github = "bwlang";
