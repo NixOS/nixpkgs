608b9d19894bf6f1280aa176ef7681b6fcbe0914
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-11 21:54:00 +0200

    python311Packages.mechanize: 0.4.9 -> 0.4.10
    Changelog: https://github.com/python-mechanize/mechanize/blob/v0.4.10/ChangeLog

--- pkgs/development/python-modules/mechanize/default.nix
+++ pkgs/development/python-modules/mechanize/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, fetchpatch2
 , html5lib
 , pytestCheckHook
 , pythonOlder
@@ -10,23 +9,15 @@
 
 buildPythonPackage rec {
   pname = "mechanize";
-  version = "0.4.9";
+  version = "0.4.10";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-aaXtsJYvkh6LEINzaMIkLYrQSfC5H/aZzn9gG/xDFSE=";
+    hash = "sha256-HeqUf5vn6gq2EPe7xKTja0XWv9/O6imtPTiaiKGVfd8=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      # python 3.11+ compat
-      url = "https://github.com/python-mechanize/mechanize/commit/1324b09b661aaac7d4cdc7e1e9d49e1c3851db2c.patch";
-      hash = "sha256-d0Zuz6m2Uv8pnR8TC0L+AStS82rPPTpQrrjkCZnJliE=";
-    })
-  ];
 
   nativeBuildInputs = [
     setuptools
