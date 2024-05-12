5ec7f4afad2c6ae8ea3f392fc1208ac2efcbcf23
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-12 16:27:12 +0000

    git-cola: 4.7.0 -> 4.7.1

--- pkgs/applications/version-management/git-cola/default.nix
+++ pkgs/applications/version-management/git-cola/default.nix
@@ -10,14 +10,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "git-cola";
-  version = "4.7.0";
+  version = "4.7.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "git-cola";
     repo = "git-cola";
     rev = "v${version}";
-    hash = "sha256-BiSs3vWJacCtGthHi/nFJm4Hqt0uO6XXZi/Zqvjb928=";
+    hash = "sha256-93aayGGMgkSghTpx8M5Cfbxf2szAwrSzuoWK6GCTqZ8=";
   };
 
   buildInputs = lib.optionals stdenv.isLinux [
