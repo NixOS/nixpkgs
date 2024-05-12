86669c5217b9e8e1a742514e3f386b9fa0ff4e90
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-12 17:49:59 +0000

    python311Packages.pytorch-lightning: 2.2.3 -> 2.2.4

--- pkgs/development/python-modules/pytorch-lightning/default.nix
+++ pkgs/development/python-modules/pytorch-lightning/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "pytorch-lightning";
-  version = "2.2.3";
+  version = "2.2.4";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "Lightning-AI";
     repo = "pytorch-lightning";
     rev = "refs/tags/${version}";
-    hash = "sha256-5dz4NQZC0tKIYA9omRZAsSlvH5hpyCSmV0yvoRICcqg=";
+    hash = "sha256-IkoSID7nEPbKrhEMlo/UaMcF80HYldvndFA54DoHT+M=";
   };
 
   preConfigure = ''
