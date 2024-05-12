b9fa94ab9162c3e58f085f12ec86a216c33c5824
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-05-12 13:36:06 +0200

    python312Packages.ecdsa: use pyproject=true, enable testing

--- pkgs/development/python-modules/ecdsa/default.nix
+++ pkgs/development/python-modules/ecdsa/default.nix
@@ -1,28 +1,40 @@
-{ lib
-, buildPythonPackage
-, fetchPypi
-, pkgs
-, six
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  six,
+  pytestCheckHook,
+  hypothesis,
+  pkgs,
 }:
 
 buildPythonPackage rec {
   pname = "ecdsa";
   version = "0.19.0";
-  format = "setuptools";
+  pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-YOqtEZllmQDdCvUh7UYreTu9+GdDKzlI6HQWrkyva/g=";
+  src = fetchFromGitHub {
+    owner = "tlsfuzzer";
+    repo = "python-ecdsa";
+    rev = "python-ecdsa-${version}";
+    hash = "sha256-yDhTlc0adNo+RzVpP+L4D73Av+NbcvJ1uNB2hUxXsBA=";
   };
 
-  propagatedBuildInputs = [ six ];
-  # Only needed for tests
-  nativeCheckInputs = [ pkgs.openssl ];
+  build-system = [ setuptools ];
+
+  dependencies = [ six ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    hypothesis
+    pkgs.openssl # Only needed for tests
+  ];
 
   meta = with lib; {
     description = "ECDSA cryptographic signature library";
-    homepage = "https://github.com/warner/python-ecdsa";
+    homepage = "https://github.com/tlsfuzzer/python-ecdsa";
     license = licenses.mit;
+    maintainers = with maintainers; [ ];
   };
-
 }
