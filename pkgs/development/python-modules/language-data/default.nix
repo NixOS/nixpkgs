8b567c6689001f2a68ad5617d9b76742d1a9ea2d
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-12 15:53:49 +0200

    python312Packages.language-data: refactor

--- pkgs/development/python-modules/language-data/default.nix
+++ pkgs/development/python-modules/language-data/default.nix
@@ -10,23 +10,23 @@
 buildPythonPackage rec {
   pname = "language-data";
   version = "1.0.1";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "rspeer";
     repo = "language_data";
-    rev = "v${version}";
-    sha256 = "51TUVHXPHG6ofbnxI6+o5lrtr+QCIpGKu+OjDK3l7Mc=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-51TUVHXPHG6ofbnxI6+o5lrtr+QCIpGKu+OjDK3l7Mc=";
   };
 
-  nativeBuildInputs = [
+  build-system = [
     poetry-core
     setuptools
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     marisa-trie
   ];
 
@@ -40,6 +40,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Supplement module for langcodes";
     homepage = "https://github.com/rspeer/language_data";
+    changelog = "https://github.com/rspeer/language_data/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
