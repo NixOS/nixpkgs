e526629d759949faea584911171a4e1c71f9cc60
K900 <me@0upti.me>
2024-05-13 08:26:18 +0300

    kdePackages.kdeconnect-kde: hardcode sshfs path
    Fixes #311236

--- pkgs/kde/gear/kdeconnect-kde/default.nix
+++ pkgs/kde/gear/kdeconnect-kde/default.nix
@@ -1,5 +1,8 @@
 {
+  lib,
   mkKdeDerivation,
+  substituteAll,
+  sshfs,
   qtconnectivity,
   qtmultimedia,
   qtwayland,
@@ -10,6 +13,19 @@
 }:
 mkKdeDerivation {
   pname = "kdeconnect-kde";
+
+  patches = [
+    (substituteAll {
+      src = ./hardcode-sshfs-path.patch;
+      sshfs = lib.getExe sshfs;
+    })
+  ];
+
+  # Hardcoded as a QString, which is UTF-16 so Nix can't pick it up automatically
+  postFixup = ''
+    mkdir -p $out/nix-support
+    echo "${sshfs}" > $out/nix-support/depends
+  '';
 
   extraNativeBuildInputs = [pkg-config];
   extraBuildInputs = [qtconnectivity qtmultimedia qtwayland wayland wayland-protocols libfakekey];
