NixOS/nixpkgs

commit 199efdb1dead77efa41fbed267cdb1285f907000

luaPackages.vusted: fix build by setting bounds

pkgs/development/lua-modules/overrides.nix
@@ -711,9 +711,9 @@ in
     propagatedBuildInputs = oa.propagatedBuildInputs ++ [ sol2 ];
 
     postPatch = ''
-      substituteInPlace CMakeLists.txt \
-        --replace "TOML_PLUS_PLUS_SRC" "${tomlplusplus.src}" \
-        --replace "MAGIC_ENUM_SRC" "${magic-enum.src}"
+      substituteInPlace CMakeLists.txt --replace-fail \
+        "TOML_PLUS_PLUS_SRC" \
+        "${tomlplusplus.src}"
     '';
   });
 
@@ -738,9 +738,11 @@ in
 
   vusted = prev.vusted.overrideAttrs (_: {
     postConfigure = ''
+      cat ''${rockspecFilename}
       substituteInPlace ''${rockspecFilename} \
-        --replace '"luasystem = 0.2.1",' '"luasystem",'
+        --replace-fail '"luasystem = 0.2.1",' "'luasystem >= 0.2',"
     '';
+
     # make sure vusted_entry.vim doesn't get wrapped
     postInstall = ''
       chmod -x $out/bin/vusted_entry.vim

commit 900dacfb7887cac71264ea0df44cbd51870e6b8e

luaPackages.toml: unbreak
Regression introduced by commit 199efdb1dead77efa41fbed267cdb1285f907000

pkgs/development/lua-modules/overrides.nix
@@ -709,9 +709,11 @@ in
     propagatedBuildInputs = oa.propagatedBuildInputs ++ [ sol2 ];
 
     postPatch = ''
-      substituteInPlace CMakeLists.txt --replace-fail \
-        "TOML_PLUS_PLUS_SRC" \
-        "${tomlplusplus.src}"
+      substituteInPlace CMakeLists.txt \
+        --replace-fail "TOML_PLUS_PLUS_SRC" "${tomlplusplus.src}/include/toml++" \
+        --replace-fail "MAGIC_ENUM_SRC" "${magic-enum.src}/include/magic_enum"
+
+      cat CMakeLists.txt
     '';
   });
 

pkgs/development/lua-modules/toml.patch
@@ -1,16 +1,16 @@
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index faae37a..6990d4a 100644
+index ab3884c..c0fd356 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -38,26 +38,17 @@ include(FetchContent)
+@@ -38,32 +38,23 @@ include(FetchContent)
  
  FetchContent_Declare(
  	${TOML++}
 -	GIT_REPOSITORY "https://github.com/marzer/tomlplusplus.git"
 -	GIT_SHALLOW ON
 -    GIT_SUBMODULES ""
--	GIT_TAG "v3.4.0"
-+      DOWNLOAD_COMMAND true
+-	GIT_TAG "v3.3.0"
++  DOWNLOAD_COMMAND true
  )
  
  FetchContent_Declare(
@@ -19,7 +19,7 @@ index faae37a..6990d4a 100644
 -	GIT_SHALLOW ON
 -    GIT_SUBMODULES ""
 -	GIT_TAG "v3.3.0"
-+      DOWNLOAD_COMMAND true
++  DOWNLOAD_COMMAND true
  )
  
  FetchContent_Declare(
@@ -27,26 +27,35 @@ index faae37a..6990d4a 100644
 -	GIT_REPOSITORY "https://github.com/Neargye/magic_enum.git"
 -	GIT_SHALLOW ON
 -    GIT_SUBMODULES ""
--	GIT_TAG "v0.9.5"
-+      DOWNLOAD_COMMAND true
+-	GIT_TAG "v0.8.2"
++  DOWNLOAD_COMMAND true
  )
  
  FetchContent_GetProperties(${TOML++})
-@@ -112,7 +103,7 @@ if(NOT LUA_INCLUDE_DIR OR (WIN32 AND NOT LUA_LIBRARIES))
+ if(NOT ${TOML++}_POPULATED)
+     message(STATUS "Cloning ${TOML++}")
+-	FetchContent_Populate(${TOML++}) 
++	FetchContent_Populate(${TOML++})
+     FetchContent_MakeAvailable(${TOML++})
+ endif()
+ 
+@@ -113,7 +104,7 @@ if(NOT LUA_INCLUDE_DIR OR (WIN32 AND NOT LUA_LIBRARIES))
      find_package(Lua)
  endif()
  
 -include_directories(${LUA_INCLUDE_DIR} src src/include ${${TOML++}_SOURCE_DIR} ${${SOL2}_SOURCE_DIR}/include ${${MAGIC_ENUM}_SOURCE_DIR}/include)
-+include_directories(${LUA_INCLUDE_DIR} src src/include TOML_PLUS_PLUS_SRC ${${SOL2}_SOURCE_DIR}/include MAGIC_ENUM_SRC/include)
++include_directories(${LUA_INCLUDE_DIR} src src/include TOML_PLUS_PLUS_SRC ${${SOL2}_SOURCE_DIR}/include MAGIC_ENUM_SRC)
  
  set(SOURCES
      src/toml.cpp
-@@ -129,7 +120,7 @@ source_group(src FILES ${SOURCES})
- if(WIN32 AND "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang" OR MSVC)
+@@ -129,8 +120,8 @@ source_group(src FILES ${SOURCES})
+ 
+ if(WIN32 AND "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
  	target_link_options(toml.lua PUBLIC ${PROJECT_SOURCE_DIR}\\libs\\lua51.lib)
- else() 
--    target_link_libraries(toml.lua ${LUA_LIBRARIES} tomlplusplus::tomlplusplus)    
-+    target_link_libraries(toml.lua ${LUA_LIBRARIES} tomlplusplus)
+-else() 
+-    target_link_libraries(toml.lua ${LUA_LIBRARIES})    
++else()
++    target_link_libraries(toml.lua ${LUA_LIBRARIES})
  endif()
  
  if (LINK_FLAGS)