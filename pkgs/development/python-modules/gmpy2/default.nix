141a41dd14b9e193f0c1f3128fe5fd9ad4a300af
Clément <clement2104.boillot@gmail.com>
2024-05-13 10:48:30 +0200

    python312Packages.gmpy2: fix build failure for python312
    a

--- pkgs/development/python-modules/gmpy2/default.nix
+++ pkgs/development/python-modules/gmpy2/default.nix
@@ -8,6 +8,7 @@
 
 # Reverse dependency
 , sage
+, fetchpatch
 }:
 
 let
@@ -27,6 +28,16 @@ buildPythonPackage {
     rev = "gmpy2-${version}";
     hash = "sha256-ARCttNzRA+Ji2j2NYaSCDXgvoEg01T9BnYadyqON2o0=";
   };
+
+  patches = [
+    # Broken for Python3.12
+    # Fix from Arch Linux
+    # https://gitlab.archlinux.org/archlinux/packaging/packages/python-gmpy2/-/commit/1c0d2d08d15b7b3bea012df2e91dbe0166be0afc
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/python-gmpy2/-/raw/1c0d2d08d15b7b3bea012df2e91dbe0166be0afc/python-3.12.patch";
+      hash = "sha256-vHh66E2o/Tw/sCXC4XgA1GHWhQLG3iWhPw+4NN2GuJQ=";
+    })
+  ];
 
   buildInputs = [ gmp mpfr libmpc ];
 
