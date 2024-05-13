6024fafb6c486986add3dad0c0a70983e63430ac
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-13 11:42:25 +0200

    python312Packages.pynws: add changelog to meta
    - format with nixfmt

--- pkgs/development/python-modules/pynws/default.nix
+++ pkgs/development/python-modules/pynws/default.nix
@@ -1,17 +1,18 @@
-{ lib
-, aiohttp
-, buildPythonPackage
-, fetchFromGitHub
-, freezegun
-, metar
-, pytest-aiohttp
-, pytest-asyncio
-, pytest-cov
-, pytestCheckHook
-, pythonOlder
-, setuptools
-, setuptools-scm
-, tenacity
+{
+  lib,
+  aiohttp,
+  buildPythonPackage,
+  fetchFromGitHub,
+  freezegun,
+  metar,
+  pytest-aiohttp,
+  pytest-asyncio,
+  pytest-cov,
+  pytestCheckHook,
+  pythonOlder,
+  setuptools,
+  setuptools-scm,
+  tenacity,
 }:
 
 buildPythonPackage rec {
@@ -19,11 +20,11 @@ buildPythonPackage rec {
   version = "1.8.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "MatthewFlamm";
-    repo = pname;
+    repo = "pynws";
     rev = "refs/tags/v${version}";
     hash = "sha256-KUCylHYng6mn2TWKf8C7k0IoerM22OIQ7pJMKi5SF3A=";
   };
@@ -38,9 +39,7 @@ buildPythonPackage rec {
     metar
   ];
 
-  optional-dependencies.retry = [
-    tenacity
-  ];
+  optional-dependencies.retry = [ tenacity ];
 
   nativeCheckInputs = [
     freezegun
@@ -55,6 +54,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library to retrieve data from NWS/NOAA";
     homepage = "https://github.com/MatthewFlamm/pynws";
+    changelog = "https://github.com/MatthewFlamm/pynws/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
