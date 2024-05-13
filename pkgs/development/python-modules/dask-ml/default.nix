3ad32d9319f73ba9fed9ef6b9b9b367587cbb5f3
Gaetan Lepage <gaetan@glepage.com>
2024-05-13 12:10:01 +0200

    python311Packages.dask-ml: add GaetanLepage as maintainer

--- pkgs/development/python-modules/dask-ml/default.nix
+++ pkgs/development/python-modules/dask-ml/default.nix
@@ -72,6 +72,6 @@ buildPythonPackage rec {
     description = "Scalable Machine Learn with Dask";
     homepage = "https://github.com/dask/dask-ml";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ GaetanLepage ];
   };
 }
