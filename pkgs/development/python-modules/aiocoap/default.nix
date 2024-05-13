2c4139d45ef00a7e174eeec9d0d1d0acd271affb
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-13 13:57:14 +0200

    python312Packages.aiocoap: format with nixfmt

--- pkgs/development/python-modules/aiocoap/default.nix
+++ pkgs/development/python-modules/aiocoap/default.nix
@@ -1,19 +1,20 @@
-{ lib
-, buildPythonPackage
-, cbor-diag
-, cbor2
-, cryptography
-, dtlssocket
-, fetchFromGitHub
-, filelock
-, ge25519
-, pygments
-, pytestCheckHook
-, pythonAtLeast
-, pythonOlder
-, setuptools
-, termcolor
-, websockets
+{
+  lib,
+  buildPythonPackage,
+  cbor-diag,
+  cbor2,
+  cryptography,
+  dtlssocket,
+  fetchFromGitHub,
+  filelock,
+  ge25519,
+  pygments,
+  pytestCheckHook,
+  pythonAtLeast,
+  pythonOlder,
+  setuptools,
+  termcolor,
+  websockets,
 }:
 
 buildPythonPackage rec {
@@ -30,9 +31,7 @@ buildPythonPackage rec {
     hash = "sha256-jBRxorHr5/CgAR6WVXBUycZpJ6n1DYVFQk6kqVv8D1Q=";
   };
 
-  build-system = [
-    setuptools
-  ];
+  build-system = [ setuptools ];
 
   passthru.optional-dependencies = {
     oscore = [
@@ -41,12 +40,8 @@ buildPythonPackage rec {
       filelock
       ge25519
     ];
-    tinydtls = [
-      dtlssocket
-    ];
-    ws = [
-      websockets
-    ];
+    tinydtls = [ dtlssocket ];
+    ws = [ websockets ];
     prettyprint = [
       termcolor
       cbor2
@@ -55,9 +50,7 @@ buildPythonPackage rec {
     ];
   };
 
-  nativeCheckInputs = [
-    pytestCheckHook
-  ];
+  nativeCheckInputs = [ pytestCheckHook ];
 
   disabledTestPaths = [
     # Don't test the plugins
@@ -66,29 +59,29 @@ buildPythonPackage rec {
     "tests/test_oscore_plugtest.py"
   ];
 
-  disabledTests = [
-    # Communication is not properly mocked
-    "test_uri_parser"
-  ] ++ lib.optionals (pythonAtLeast "3.12") [
-    # https://github.com/chrysn/aiocoap/issues/339
-    "TestServerTCP::test_big_resource"
-    "TestServerTCP::test_empty_accept"
-    "TestServerTCP::test_error_resources"
-    "TestServerTCP::test_fast_resource"
-    "TestServerTCP::test_js_accept"
-    "TestServerTCP::test_manualbig_resource"
-    "TestServerTCP::test_nonexisting_resource"
-    "TestServerTCP::test_replacing_resource"
-    "TestServerTCP::test_root_resource"
-    "TestServerTCP::test_slow_resource"
-    "TestServerTCP::test_slowbig_resource"
-    "TestServerTCP::test_spurious_resource"
-    "TestServerTCP::test_unacceptable_accept"
-  ];
+  disabledTests =
+    [
+      # Communication is not properly mocked
+      "test_uri_parser"
+    ]
+    ++ lib.optionals (pythonAtLeast "3.12") [
+      # https://github.com/chrysn/aiocoap/issues/339
+      "TestServerTCP::test_big_resource"
+      "TestServerTCP::test_empty_accept"
+      "TestServerTCP::test_error_resources"
+      "TestServerTCP::test_fast_resource"
+      "TestServerTCP::test_js_accept"
+      "TestServerTCP::test_manualbig_resource"
+      "TestServerTCP::test_nonexisting_resource"
+      "TestServerTCP::test_replacing_resource"
+      "TestServerTCP::test_root_resource"
+      "TestServerTCP::test_slow_resource"
+      "TestServerTCP::test_slowbig_resource"
+      "TestServerTCP::test_spurious_resource"
+      "TestServerTCP::test_unacceptable_accept"
+    ];
 
-  pythonImportsCheck = [
-    "aiocoap"
-  ];
+  pythonImportsCheck = [ "aiocoap" ];
 
   meta = with lib; {
     description = "Python CoAP library";
