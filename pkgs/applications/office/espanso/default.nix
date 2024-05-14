68f2c9be0bcebcdb32520013b1d0f74307e8db80
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-05-14 21:29:33 +0200

    espanso: add n8henrie as maintainer

--- pkgs/applications/office/espanso/default.nix
+++ pkgs/applications/office/espanso/default.nix
@@ -146,7 +146,7 @@ rustPlatform.buildRustPackage rec {
     mainProgram = "espanso";
     homepage = "https://espanso.org";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ kimat pyrox0 ];
+    maintainers = with maintainers; [ kimat pyrox0 n8henrie ];
     platforms = platforms.unix;
     # With apple_sdk_10_12,
     # kCFURLVolumeAvailableCapacityForImportantUsageKey
