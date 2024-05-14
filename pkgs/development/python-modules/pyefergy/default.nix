abebab9b4919921e77842e438dd97508722f6d03
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-14 08:51:53 +0200

    python312Packages.pyefergy: 22.1.1 -> 22.5.0
    Diff: https://github.com/tkdrob/pyefergy/compare/refs/tags/v22.1.1...v22.5.0
    
    Changelog: https://github.com/tkdrob/pyefergy/releases/tag/v22.5.0

--- pkgs/development/python-modules/pyefergy/default.nix
+++ pkgs/development/python-modules/pyefergy/default.nix
@@ -1,37 +1,42 @@
 { lib
 , aiohttp
 , buildPythonPackage
+, codecov
 , fetchFromGitHub
 , iso4217
+, poetry-core
+, poetry-dynamic-versioning
 , pytest-asyncio
 , pythonOlder
 , pytz
+, types-pytz
 }:
 
 buildPythonPackage rec {
   pname = "pyefergy";
-  version = "22.1.1";
-  format = "setuptools";
+  version = "22.5.0";
+  pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "tkdrob";
-    repo = pname;
-    rev = version;
-    hash = "sha256-AdoM+PcVoajxhnEfkyN9UuNufChu8XGmZDLNC3mjrps=";
+    repo = "pyefergy";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-4M3r/+C42X95/7BGZAJbkXKKFEkGzLlvX0Ynv+eL8qc=";
   };
 
-  postPatch = ''
-    # setuptools.extern.packaging.version.InvalidVersion: Invalid version: 'master'
-    substituteInPlace setup.py \
-      --replace 'version="master",' 'version="${version}",'
-  '';
+  build-system = [
+    poetry-core
+    poetry-dynamic-versioning
+  ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     aiohttp
+    codecov
     iso4217
     pytz
+    types-pytz
   ];
 
   # Tests require network access
@@ -44,6 +49,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python API library for Efergy energy meters";
     homepage = "https://github.com/tkdrob/pyefergy";
+    changelog = "https://github.com/tkdrob/pyefergy/releases/tag/v${version}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
