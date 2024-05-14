baccd57dd2c2656291372c6ad57581eae0fb600f
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-14 16:44:02 +0200

    python312Packages.requirements-detector: refactor

--- pkgs/development/python-modules/requirements-detector/default.nix
+++ pkgs/development/python-modules/requirements-detector/default.nix
@@ -29,12 +29,15 @@ buildPythonPackage rec {
     "astroid"
   ];
 
-  nativeBuildInputs = [
+  build-system = [
     poetry-core
+  ];
+
+  nativeBuildInputs = [
     pythonRelaxDepsHook
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     astroid
     packaging
     toml
@@ -51,10 +54,10 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Python tool to find and list requirements of a Python project";
-    mainProgram = "detect-requirements";
     homepage = "https://github.com/landscapeio/requirements-detector";
     changelog = "https://github.com/landscapeio/requirements-detector/releases/tag/${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ kamadorueda ];
+    mainProgram = "detect-requirements";
   };
 }
