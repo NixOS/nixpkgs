147760c141c4eaf02ab42b783d35de1032c3cdbe
Herwig Hochleitner <herwig@bendlas.net>
2024-05-15 12:34:31 +0200

    cdemu: avoid double-wrapper and nested callPackage

--- pkgs/applications/emulators/cdemu/analyzer.nix
+++ pkgs/applications/emulators/cdemu/analyzer.nix
@@ -1,14 +1,21 @@
-{ callPackage, makeWrapper, gobject-introspection, cmake, wrapGAppsHook3
+{ stdenv, cmake, pkg-config, callPackage, makeWrapper, gobject-introspection, wrapGAppsHook3
 , python3Packages, gtk3, glib, libxml2, gnuplot, gnome, gdk-pixbuf, librsvg, intltool, libmirage }:
-callPackage ./base.nix {
-  version = "3.2.6";
-  pname = "image-analyzer";
-  hash = "sha256-7I8RUgd+k3cEzskJGbziv1f0/eo5QQXn62wGh/Y5ozc=";
-  buildInputs = [ glib gtk3 libxml2 gnuplot libmirage gnome.adwaita-icon-theme gdk-pixbuf librsvg
+stdenv.mkDerivation {
+
+  inherit (callPackage ./common-drv-attrs.nix {
+    version = "3.2.6";
+    pname = "image-analyzer";
+    hash = "sha256-7I8RUgd+k3cEzskJGbziv1f0/eo5QQXn62wGh/Y5ozc=";
+  }) pname version src meta;
+
+  buildInputs = [ libxml2 gnuplot libmirage gnome.adwaita-icon-theme gdk-pixbuf librsvg
                   python3Packages.pygobject3 python3Packages.matplotlib ];
-  nativeBuildInputs = [ gobject-introspection cmake wrapGAppsHook3 intltool ];
+  nativeBuildInputs = [ cmake pkg-config wrapGAppsHook3 intltool ];
+
+  dontWrapGApps = true;
   postFixup = ''
     wrapProgram $out/bin/image-analyzer \
+      ''${gappsWrapperArgs[@]} \
       --set PYTHONPATH "$PYTHONPATH"
   '';
 }
