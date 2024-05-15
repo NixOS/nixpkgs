d1800d1437537de7da1303aa2c10f907a2c607b6
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-15 14:19:58 +0200

    python312Packages.libgravatar: refactor

--- pkgs/development/python-modules/libgravatar/default.nix
+++ pkgs/development/python-modules/libgravatar/default.nix
@@ -4,11 +4,15 @@
   fetchFromGitHub,
   setuptools,
   pytestCheckHook,
+  pythonOlder,
 }:
 
 buildPythonPackage rec {
   pname = "libgravatar";
   version = "1.0.4";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "pabluk";
@@ -17,15 +21,16 @@ buildPythonPackage rec {
     hash = "sha256-rJv/jfdT+JldxR0kKtXQLOI5wXQYSQRWJnqwExwWjTA=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "libgravatar" ];
 
   meta = with lib; {
-    homepage = "https://github.com/pabluk/libgravatar";
     description = "A library that provides a Python 3 interface for the Gravatar API";
+    homepage = "https://github.com/pabluk/libgravatar";
+    changelog = "https://github.com/pabluk/libgravatar/releases/tag/${version}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ gador ];
   };
