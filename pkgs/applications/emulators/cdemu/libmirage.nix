304f2767fab5ab26c0e7b3d17106d9a2cda73c65
Herwig Hochleitner <herwig@bendlas.net>
2024-05-15 15:12:49 +0200

    cdemu: use buildPythonApplication

--- pkgs/applications/emulators/cdemu/libmirage.nix
+++ pkgs/applications/emulators/cdemu/libmirage.nix
@@ -1,4 +1,4 @@
-{ stdenv, callPackage, cmake, pkg-config
+{ stdenv, callPackage, cmake, pkg-config, gobject-introspection
 , glib, libsndfile, zlib, bzip2, xz, libsamplerate, intltool
 , pcre, util-linux, libselinux, libsepol }:
 
@@ -13,6 +13,7 @@ stdenv.mkDerivation {
   PKG_CONFIG_GOBJECT_INTROSPECTION_1_0_GIRDIR = "${placeholder "out"}/share/gir-1.0";
   PKG_CONFIG_GOBJECT_INTROSPECTION_1_0_TYPELIBDIR = "${placeholder "out"}/lib/girepository-1.0";
   buildInputs = [ glib libsndfile zlib bzip2 xz libsamplerate ];
-  nativeBuildInputs = [ cmake pkg-config intltool ];
+  nativeBuildInputs = [ cmake pkg-config intltool gobject-introspection ];
   propagatedBuildInputs = [ pcre util-linux libselinux libsepol ];
+
 }
