2a9ad59445f459fe1bfc222e53cabae3f64edfff
Sigmanificient <edhyjox@gmail.com>
2024-05-15 04:15:24 +0200

    gbsplay: add sigmanificient in maintainers

--- pkgs/applications/audio/gbsplay/default.nix
+++ pkgs/applications/audio/gbsplay/default.nix
@@ -27,6 +27,6 @@ stdenv.mkDerivation rec {
     description = "Gameboy sound player";
     license = licenses.gpl1Plus;
     platforms = [ "i686-linux" "x86_64-linux" ];
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ sigmanificient ];
   };
 }
