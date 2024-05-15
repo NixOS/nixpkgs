c22e92dfe18b7cbe716741dfe9d24d1d0f59d1ab
Sigmanificient <edhyjox@gmail.com>
2024-05-15 05:35:01 +0200

    unipicker: add sigmanificient to maintainers

--- pkgs/applications/misc/unipicker/default.nix
+++ pkgs/applications/misc/unipicker/default.nix
@@ -34,7 +34,7 @@ stdenv.mkDerivation rec {
     description = "A CLI utility for searching unicode characters by description and optionally copying them to clipboard";
     homepage = "https://github.com/jeremija/unipicker";
     license = licenses.mit;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ sigmanificient ];
     platforms = platforms.unix;
     mainProgram = "unipicker";
    };
