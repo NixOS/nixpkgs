6932ffc34dade1a62ad799ed9e24ca752a349a99
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-15 11:59:37 +0200

    python312Packages.schema: refactor

--- pkgs/development/python-modules/schema/default.nix
+++ pkgs/development/python-modules/schema/default.nix
@@ -5,12 +5,13 @@
 , pytestCheckHook
 , pythonOlder
 , pythonRelaxDepsHook
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "schema";
   version = "0.7.7";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
@@ -19,12 +20,16 @@ buildPythonPackage rec {
     hash = "sha256-faVTq9KVihncJUfDiM3lM5izkZYXWpvlnqHK9asKGAc=";
   };
 
-  nativeBuildInputs = [
-    pythonRelaxDepsHook
-  ];
-
   pythonRemoveDeps = [
     "contextlib2"
+  ];
+
+  build-system = [
+    setuptools
+  ];
+
+  nativeBuildInputs = [
+    pythonRelaxDepsHook
   ];
 
   nativeCheckInputs = [
@@ -39,6 +44,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library for validating Python data structures";
     homepage = "https://github.com/keleshev/schema";
+    changelog = "https://github.com/keleshev/schema/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ tobim ];
   };
