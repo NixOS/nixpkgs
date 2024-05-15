3d759aa78d7c2bac8db189acc9352aeb2bd08855
Audrey Dutcher <audrey@rhelmot.io>
2024-05-14 23:12:02 -0700

    freebsd.boot-install: filter the arguments that are not applicable to coreutils install

--- pkgs/os-specific/bsd/freebsd/pkgs/boot-install.nix
+++ pkgs/os-specific/bsd/freebsd/pkgs/boot-install.nix
@@ -1,10 +1,20 @@
 { buildPackages, freebsd-lib }:
 
-# Wrap NetBSD's install
+# Wrap GNU coreutils' install
 buildPackages.writeShellScriptBin "boot-install" (
   freebsd-lib.install-wrapper
   + ''
+    fixed_args=()
+    while [[ ''${#args[0]} > 0 ]]; do
+      case "''${args[0]}" in
+        -l)
+          args=("''${args[@]:2}")
+          continue
+      esac
+      fixed_args+=("''${args[0]}")
+      args=("''${args[@]:1}")
+    done
 
-    ${buildPackages.coreutils}/bin/install "''${args[@]}"
+    ${buildPackages.coreutils}/bin/install "''${fixed_args[@]}"
   ''
 )
