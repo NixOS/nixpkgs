af67744619263226c42c41907932c3b2402e0239
ajs124 <git@ajs124.de>
2024-05-16 23:24:04 +0200

    lvm2: 2.03.23 -> 2.03.24
    https://gitlab.com/lvmteam/lvm2/-/blob/v2_03_24/WHATS_NEW

--- pkgs/os-specific/linux/lvm2/2_03.nix
+++ pkgs/os-specific/linux/lvm2/2_03.nix
@@ -1,4 +1,4 @@
 import ./common.nix {
-  version = "2.03.23";
-  hash = "sha256-dOeUqene4bz4ogZfZbkZbET98yHiLWO5jtfejJqhel0=";
+  version = "2.03.24";
+  hash = "sha256-WTxVA7oA+qscbgtKWXuWBex7WIGEXAS/QS6/nRu/oTw=";
 }
