c4cb8c842171c6016332ad71499d2eeea4df8b6f
Mario Rodas <marsam@users.noreply.github.com>
2024-05-16 04:20:00 +0000

    nodejs_22: 22.1.0 -> 22.2.0
    Changelog: https://github.com/nodejs/node/releases/tag/v22.2.0

--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -8,8 +8,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "22.1.0";
-  sha256 = "sha256-nX1fQNnb1iYMmbXklLX5vHVejw/6xw4SGtzl+0QvI8s=";
+  version = "22.2.0";
+  sha256 = "sha256-iJkIqIKNFISRDX5lm2qlet6NUo/w45Dpp372WadihHQ=";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
