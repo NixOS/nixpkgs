a01a572fc649e968afb66e13eebf9e7abaabb7d8
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-16 18:47:10 +0200

    python312Packages.flaky: refactor
    - migrate to pytestCheckHook
    - add pythonImportsCheck

--- pkgs/development/python-modules/flaky/default.nix
+++ pkgs/development/python-modules/flaky/default.nix
@@ -1,15 +1,18 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, pytest-xdist
+, pytestCheckHook
+, pythonOlder
 , setuptools
-, mock
-, pytest
 }:
 
 buildPythonPackage rec {
   pname = "flaky";
   version = "3.8.1";
   pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
@@ -21,23 +24,28 @@ buildPythonPackage rec {
   ];
 
   nativeCheckInputs = [
-    mock
-    pytest
+    pytest-xdist
+    pytestCheckHook
   ];
 
-  checkPhase = ''
-    # based on tox.ini
-    pytest -k 'example and not options' --doctest-modules test/test_pytest/
-    pytest -k 'example and not options' test/test_pytest/
-    pytest -p no:flaky test/test_pytest/test_flaky_pytest_plugin.py
-    pytest --force-flaky --max-runs 2  test/test_pytest/test_pytest_options_example.py
-  '';
+  pythonImportsCheck = [
+    "flaky"
+  ];
+
+  disabledTests = [
+    # AssertionError and ValueError
+    "test_flaky_plugin_handles_non_ascii_byte_string_in_exception"
+    "test_flaky_plugin_identifies_failure"
+    "test_something_flaky"
+    "test_write_then_read"
+    "test_writelines_then_read"
+  ];
 
   meta = with lib; {
-    changelog = "https://github.com/box/flaky/blob/v${version}/HISTORY.rst";
+    description = "Plugin for pytest that automatically reruns flaky tests";
     homepage = "https://github.com/box/flaky";
-    description = "Plugin for nose or py.test that automatically reruns flaky tests";
+    changelog = "https://github.com/box/flaky/blob/v${version}/HISTORY.rst";
     license = licenses.asl20;
+    maintainers = with maintainers; [ ];
   };
-
 }
