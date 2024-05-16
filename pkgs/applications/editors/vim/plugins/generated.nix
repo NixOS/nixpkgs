041cec907b989ac1395335f513e6ccc53fcaf226
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-05-17 00:07:50 +0200

    vimPlugins.middleclass: init at 2022-09-20

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -6023,6 +6023,18 @@ final: prev:
     meta.homepage = "https://github.com/xero/miasma.nvim/";
   };
 
+  middleclass = buildNeovimPlugin {
+    pname = "middleclass";
+    version = "2022-09-20";
+    src = fetchFromGitHub {
+      owner = "anuvyklack";
+      repo = "middleclass";
+      rev = "9fab4d5bca67262614960960ca35c4740eb2be2c";
+      sha256 = "1r68dh0vz08i37g9jmdgr799j35kr7z4r9cx8q0scrymigp5ghmi";
+    };
+    meta.homepage = "https://github.com/anuvyklack/middleclass/";
+  };
+
   midnight-nvim = buildVimPlugin {
     pname = "midnight.nvim";
     version = "2024-02-24";
