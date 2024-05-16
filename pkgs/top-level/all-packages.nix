f8d5af455807586848f5863b97cc7c57270fba42
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-05-16 20:31:51 +0530

    tracker-miners: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7630,8 +7630,6 @@ with pkgs;
 
   tracker = callPackage ../development/libraries/tracker { };
 
-  tracker-miners = callPackage ../development/libraries/tracker-miners { };
-
   tracy = callPackage ../development/tools/tracy { };
 
   trivy = callPackage ../tools/admin/trivy { };
