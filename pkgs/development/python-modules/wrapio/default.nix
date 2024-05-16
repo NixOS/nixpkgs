a4caad4eaf2ebae9dca8830fdba90d1148f5a5ed
Stefan Frijters <sfrijters@gmail.com>
2024-05-16 19:41:51 +0200

    python3Packages.wrapio: refactor

--- pkgs/development/python-modules/wrapio/default.nix
+++ pkgs/development/python-modules/wrapio/default.nix
@@ -2,12 +2,13 @@
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "wrapio";
   version = "2.0.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.5";
 
@@ -15,6 +16,10 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-CUocIbdZ/tJQCxAHzhFpB267ynlXf8Mu+thcRRc0yeg=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   doCheck = false;
   pythonImportsCheck = [ "wrapio" ];
