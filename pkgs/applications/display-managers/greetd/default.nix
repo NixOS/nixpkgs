0e700c934c5c866b9ca435606e51d78f9805dc28
Craft <djcraft@gmail.com>
2024-05-17 12:52:21 -0400

    greetd: 0.9.0 -> 0.10.0
    Updating greetd from 0.9.0 -> 0.10.0.
    Updating cargoHash, in addition to version and sha256.

--- pkgs/applications/display-managers/greetd/default.nix
+++ pkgs/applications/display-managers/greetd/default.nix
@@ -17,7 +17,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-QFxE5c4XDOBa3rKpUFur6xOG+5m0NSCOWo7960ZWEow=";
   };
 
-  cargoHash = "sha256-vn1ih4YXsltR1zdy7BzS3CSJO9Uewx/654lRTSkWV0k=";
+  cargoHash = "sha256-w7V5ZBFy5MIEQKOPPgsc7XnrXsF5rEDNzUKeLPPy4P4=";
 
   nativeBuildInputs = [
     scdoc
