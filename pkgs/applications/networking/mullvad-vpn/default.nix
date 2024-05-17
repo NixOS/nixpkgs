063e7c6647947252ec352978f3ccdef0fa481672
John Napoli <jnapoli@itsporkroll.com>
2024-05-17 13:31:21 -0400

    mullvad-vpn: 2024.1 -> 2024.3

--- pkgs/applications/networking/mullvad-vpn/default.nix
+++ pkgs/applications/networking/mullvad-vpn/default.nix
@@ -64,7 +64,11 @@ let
     systemd
   ];
 
+<<<<<<< HEAD
   version = "2024.1";
+=======
+  version = "2024.3";
+>>>>>>> 5c584ea4b0ee (mullvad-vpn: 2024.1 -> 2024.3)
 
   selectSystem = attrs: attrs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 
@@ -74,8 +78,13 @@ let
   };
 
   hash = selectSystem {
+<<<<<<< HEAD
     x86_64-linux = "sha256-io6ROUHoSBij1ah6yi1Gbni6yWVVoYZKUd7BR+GXKLg=";
     aarch64-linux = "sha256-bzKTASfqjmjyKZecr8MGaChd6g48aQhfpuc+gUqwoPI=";
+=======
+    x86_64-linux = "sha256-LfuLBYGHlVEcVpHSdRSAEf9D7QChRd/fhx8EoCBZbSc=";
+    aarch64-linux = "sha256-7uUgewZ9KVLyMUax6u0R6ZN1YS3L4c43meVqJQD77lA=";
+>>>>>>> 5c584ea4b0ee (mullvad-vpn: 2024.1 -> 2024.3)
   };
 in
 
