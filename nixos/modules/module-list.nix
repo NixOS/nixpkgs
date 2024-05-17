e8653ac7cc7828549d6afeed489e455df43a0e7f
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-05-17 18:04:47 +0200

    nixos/music-assistant: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -365,6 +365,7 @@
   ./services/audio/mopidy.nix
   ./services/audio/mpd.nix
   ./services/audio/mpdscribble.nix
+  ./services/audio/music-assistant.nix
   ./services/audio/mympd.nix
   ./services/audio/navidrome.nix
   ./services/audio/networkaudiod.nix
