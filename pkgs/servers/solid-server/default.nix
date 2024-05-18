c0c86bc0406801e2658bf23bce2fe9c0f30d7084
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-05-18 20:40:45 +0300

    add meta info to package

--- pkgs/servers/solid-server/default.nix
+++ pkgs/servers/solid-server/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgs }:
+{ stdenv, pkgs, lib }:
 
 let
   nodePackages = import ./node-packages.nix {
@@ -8,4 +8,12 @@ let
 in
 nodePackages."@solid/community-server".override {
   pname = "community-solid-server";
+  meta = {
+    description = "Community Solid Server: an open and modular implementation of the Solid specifications";
+    homepage = "https://github.com/CommunitySolidServer/CommunitySolidServer";
+    changelog = "https://github.com/CommunitySolidServer/CommunitySolidServer/blob/main/CHANGELOG.md";
+    license = lib.licenses.mit;
+    mainProgram = "community-solid-server";
+    maintainers = with lib.maintainers; [ ivandimitrov8080 ];
+  };
 }
