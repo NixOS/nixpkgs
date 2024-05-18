c09e7c4629d262bce0aa1625b1a4d1ffcb1a0393
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-18 20:33:12 +0200

    python312Packages.pynmea2: refactor

--- pkgs/development/python-modules/pynmea2/default.nix
+++ pkgs/development/python-modules/pynmea2/default.nix
@@ -1,22 +1,39 @@
-{ lib, buildPythonPackage, fetchPypi, pytestCheckHook }:
+{ lib
+, buildPythonPackage
+, fetchPypi
+, pytestCheckHook
+, setuptools
+, pythonOlder
+}:
 
 buildPythonPackage rec {
   pname = "pynmea2";
   version = "1.19.0";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-Hap5uTJ5+IfRwjXlzFx54yZEVkE4zkaYmrD0ovyXDXw=";
   };
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  build-system = [
+    setuptools
+  ];
 
-  pythonImportsCheck = [ "pynmea2" ];
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "pynmea2"
+  ];
 
   meta = {
-    homepage = "https://github.com/Knio/pynmea2";
     description = "Python library for the NMEA 0183 protcol";
+    homepage = "https://github.com/Knio/pynmea2";
+    changelog = "https://github.com/Knio/pynmea2/releases/tag/${version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ oxzi ];
   };
