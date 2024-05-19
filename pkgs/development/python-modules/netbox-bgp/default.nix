737db0850b698552a8e7f488f410b741fa4a8fe0
Nico Felbinger <nico@felbinger.eu>
2024-05-19 13:15:09 +0200

    python311Packages.netbox-bgp: init at 0.11.1
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

---
+++ pkgs/development/python-modules/netbox-bgp/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  netbox,
+  pytestCheckHook,
+  python,
+}:
+
+buildPythonPackage rec {
+  pname = "netbox-bgp";
+  version = "0.11.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "netbox-community";
+    repo = "netbox-bgp";
+    rev = "v${version}";
+    hash = "sha256-+F7Y+bmxVz/P1C8zG9Uev646zBKvlYOx/pENN1+4Dig=";
+  };
+
+  build-system = [ setuptools ];
+
+  nativeCheckInputs = [ netbox ];
+
+  preFixup = ''
+    export PYTHONPATH=${netbox}/opt/netbox/netbox:$PYTHONPATH
+  '';
+
+  dontUsePythonImportsCheck = python.pythonVersion != netbox.python.pythonVersion;
+
+  pythonImportsCheck = [ "netbox_bgp" ];
+
+  meta = {
+    description = "NetBox plugin for BGP related objects documentation";
+    homepage = "https://github.com/netbox-community/netbox-bgp";
+    changelog = "https://github.com/netbox-community/netbox-bgp/releases/tag/${src.rev}";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ felbinger ];
+  };
+}
