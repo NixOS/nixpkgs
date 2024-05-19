8b06c4793b62b48a110ede4bff8c92ce2e6c1a95
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-19 17:54:00 -0300

    trealla: 2.40.0 -> 2.50.0

--- pkgs/by-name/tr/trealla/package.nix
+++ pkgs/by-name/tr/trealla/package.nix
@@ -23,13 +23,13 @@ assert lib.elem lineEditingLibrary [
 ];
 stdenv.mkDerivation (finalAttrs: {
   pname = "trealla";
-  version = "2.40.0";
+  version = "2.50.0";
 
   src = fetchFromGitHub {
     owner = "trealla-prolog";
     repo = "trealla";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-gFFgmvahHHONNTSELMTiPRHo9YXGHw+CjQUfrah4NzQ=";
+    hash = "sha256-DRYD5itgwGxojJMt1Pz1dUzvQTlm3OohQqgbdoA2ZJI=";
   };
 
   postPatch = ''
