802e9a1051c3a53c82e12309015ee2b88107b678
Aidan Gauland <aidalgol@fastmail.net>
2024-05-19 18:00:51 +1200

    nixos/nvidia: make libXNVCtrl available on nvidia systems
    (cherry picked from commit 4e353b67f667766b1c6489f23c20b920ab0c82f5)

--- nixos/modules/hardware/video/nvidia.nix
+++ nixos/modules/hardware/video/nvidia.nix
@@ -289,6 +289,7 @@ in {
         hardware.opengl = {
           extraPackages = [
             nvidia_x11.out
+            nvidia_x11.settings.libXNVCtrl
           ];
           extraPackages32 = [
             nvidia_x11.lib32
