8c8c688678c7d0d7516f55a8f038d0155db9139b
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-19 19:50:51 +0000

    apkleaks: 2.6.1 -> 2.6.2

--- pkgs/tools/security/apkleaks/default.nix
+++ pkgs/tools/security/apkleaks/default.nix
@@ -6,15 +6,15 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "apkleaks";
-  version = "2.6.1";
+  version = "2.6.2";
 
   disabled = python3.pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "dwisiswant0";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "0ysciv643p8gkqw2wp7zy4n07hihdcyil8d20lj86cpgga71rd64";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-a7zOowvhV9H91RwNDImN2+ecixY8g3WUotlBQVdmLgA=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
