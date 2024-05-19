022ba5c2c49d7be5de18b508d5b8eb85a67d4165
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-19 17:40:33 +0000

    python311Packages.fakeredis: 2.23.1 -> 2.23.2

--- pkgs/development/python-modules/fakeredis/default.nix
+++ pkgs/development/python-modules/fakeredis/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "fakeredis";
-  version = "2.23.1";
+  version = "2.23.2";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "dsoftwareinc";
     repo = "fakeredis-py";
     rev = "refs/tags/v${version}";
-    hash = "sha256-IMYxzlvPgwJNpb976jYXNW+6veQlUxQph43ZClyC4Eg=";
+    hash = "sha256-/nuBj9h5MMz1YvJozhl4Fq/OkLckesnQHHTUWfz06sA=";
   };
 
   build-system = [ poetry-core ];
