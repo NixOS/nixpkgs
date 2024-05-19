03e2c8609d44114e35e970f58a2a7f48c4453358
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-19 10:51:35 +0000

    robo: 4.0.6 -> 5.0.0

--- pkgs/by-name/ro/robo/package.nix
+++ pkgs/by-name/ro/robo/package.nix
@@ -6,16 +6,16 @@
 
 php.buildComposerProject (finalAttrs: {
   pname = "robo";
-  version = "4.0.6";
+  version = "5.0.0";
 
   src = fetchFromGitHub {
     owner = "consolidation";
     repo = "robo";
     rev = finalAttrs.version;
-    hash = "sha256-rpCs24Q15XM4BdW1+IfysFR/8/ZU4o5b4MyJL48uDaU=";
+    hash = "sha256-tibG2sR5CsRnUjZEvOewX/fyMuAS1kgKjYbrkk+f0BI=";
   };
 
-  vendorHash = "sha256-Ul8XjH0Nav37UVpNQslOkF2bkiyqUAEZiIbcSW2tGkQ=";
+  vendorHash = "sha256-RRnHv6sOYm8fYhY3Q6m5sFDflFXd9b9LPcAqk/D1jdE=";
 
   meta = {
     changelog = "https://github.com/consolidation/robo/blob/${finalAttrs.version}/CHANGELOG.md";
