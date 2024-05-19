9d9702de2b25450d32f237fdaaad7964cd2c1597
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-19 20:44:16 +0000

    python311Packages.azure-storage-file-share: 12.15.0 -> 12.16.0

--- pkgs/development/python-modules/azure-storage-file-share/default.nix
+++ pkgs/development/python-modules/azure-storage-file-share/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "azure-storage-file-share";
-  version = "12.15.0";
+  version = "12.16.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-AJjxz6k0TE94HODNUE/zo1JVdRVTwB5yDczQyqjYqio=";
+    hash = "sha256-QS+35sPCj29yKvmBlapZQHqqMjI6+hOkoB9i0Lh3TrM=";
   };
 
   propagatedBuildInputs = [
