78189e06e1e8a73c9458c7f922a5f766f5f11b71
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-19 10:48:42 +0200

    racket: reformat using `nixfmt-rfc-style`

--- pkgs/development/interpreters/racket/minimal.nix
+++ pkgs/development/interpreters/racket/minimal.nix
@@ -1,5 +1,4 @@
-{ racket
-}:
+{ racket }:
 
 racket.overrideAttrs (oldAttrs: rec {
   pname = "racket-minimal";
@@ -11,10 +10,16 @@ racket.overrideAttrs (oldAttrs: rec {
 
   meta = oldAttrs.meta // {
     description = "Racket without bundled packages, such as Dr. Racket";
-    longDescription = ''The essential package racket-libs is included,
-      as well as libraries that live in collections. In particular, raco
-      and the pkg library are still bundled.
+    longDescription = ''
+      The essential package racket-libs is included,
+            as well as libraries that live in collections. In particular, raco
+            and the pkg library are still bundled.
     '';
-    platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
   };
 })
