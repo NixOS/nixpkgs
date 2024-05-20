992735db2203b32b2eaf516ca46b353c8dc9d4fa
Alexander Tomokhov <alexoundos@gmail.com>
2024-05-21 00:26:33 +0400

    nixosTests.castopod: fix mp3 generation
    Fix `--cbr` encoding mode.

--- nixos/tests/castopod.nix
+++ nixos/tests/castopod.nix
@@ -208,7 +208,7 @@ import ./make-test-python.nix ({ pkgs, lib, ... }:
             text = ''
               out=/tmp/podcast.mp3
               sox -n -r 48000 -t wav - synth ${targetPodcastDuration} sine 440 `
-              `| lame --noreplaygain -cbr -q 9 -b 320 - $out
+              `| lame --noreplaygain --cbr -q 9 -b 320 - $out
               FILESIZE="$(stat -c%s $out)"
               [ "$FILESIZE" -gt 0 ]
               [ "$FILESIZE" -le "${toString targetPodcastSize}" ]
