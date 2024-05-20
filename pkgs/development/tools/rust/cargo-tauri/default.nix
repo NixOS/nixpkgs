c91e7877c1cf0a468b7898bdc76546ae9da0d8ed
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-20 00:08:53 +0000

    cargo-tauri: 1.6.5 -> 1.6.6
    (cherry picked from commit cdc080bbd5fd54b9c062ae41a8bd70539f07cefb)
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/development/tools/rust/cargo-tauri/default.nix
+++ pkgs/development/tools/rust/cargo-tauri/default.nix
@@ -17,20 +17,20 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "tauri";
-  version = "1.6.5";
+  version = "1.6.6";
 
   src = fetchFromGitHub {
     owner = "tauri-apps";
     repo = pname;
     rev = "tauri-v${version}";
-    hash = "sha256-5Hz6vJaClZ6/6KVN11dSinb4xerf/3Qiq230H8+j5q8=";
+    hash = "sha256-RttKRAjkD12E8gvOum/ynHecsvrZWw3br3OzQuvutcA=";
   };
 
   # Manually specify the sourceRoot since this crate depends on other crates in the workspace. Relevant info at
   # https://discourse.nixos.org/t/difficulty-using-buildrustpackage-with-a-src-containing-multiple-cargo-workspaces/10202
   sourceRoot = "${src.name}/tooling/cli";
 
-  cargoHash = "sha256-eb33aBG3qcMqBVY9/gzEvodS2w5rqioPAmiEKBEJIEw=";
+  cargoHash = "sha256-vxLFw+tD6+IdV58A6nmFwuhcgbCBpcMfpej8VsBmJ1w=";
 
   buildInputs = [ openssl ] ++ lib.optionals stdenv.isLinux [ glibc libsoup cairo gtk3 webkitgtk ]
     ++ lib.optionals stdenv.isDarwin [ CoreServices Security SystemConfiguration ];
