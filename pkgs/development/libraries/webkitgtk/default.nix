a3f47b1bba8922e9b609db2fa1fc6b822202ea72
Bobby Rong <rjl931189261@126.com>
2024-05-20 11:25:33 +0800

    webkitgtk: 2.44.1 → 2.44.2
    https://webkitgtk.org/2024/05/16/webkitgtk2.44.2-released.html
    https://webkitgtk.org/security/WSA-2024-0003.html
    https://github.com/WebKit/WebKit/compare/webkitgtk-2.44.1...webkitgtk-2.44.2
    
    CVE-2024-27834
    
    (cherry picked from commit 5405010c02f7ee81cc73540ce1a82ac1323beda8)

--- pkgs/development/libraries/webkitgtk/default.nix
+++ pkgs/development/libraries/webkitgtk/default.nix
@@ -68,7 +68,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "webkitgtk";
-  version = "2.44.1";
+  version = "2.44.2";
   name = "${finalAttrs.pname}-${finalAttrs.version}+abi=${if lib.versionAtLeast gtk3.version "4.0" then "6.0" else "4.${if lib.versions.major libsoup.version == "2" then "0" else "1"}"}";
 
   outputs = [ "out" "dev" "devdoc" ];
@@ -79,7 +79,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://webkitgtk.org/releases/webkitgtk-${finalAttrs.version}.tar.xz";
-    hash = "sha256-QlsUWbDwTQYAx40au15+36PAYKQg+LIx6aai1dKcVWE=";
+    hash = "sha256-Uj9CyP8kgyrdF2Mfbqr+j5MDr+MW7xp+GES5Uqf3Uhs=";
   };
 
   patches = lib.optionals stdenv.isLinux [
