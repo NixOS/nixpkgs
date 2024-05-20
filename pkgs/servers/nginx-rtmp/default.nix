8f053f68ac3e855e788d4f8cd39c760600c05938
Emre YILMAZ <z@emre.xyz>
2024-05-20 19:42:26 -0400

    Add Nginx with RTMP module

---
+++ pkgs/servers/nginx-rtmp/default.nix
@@ -0,0 +1,44 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "nginx-rtmp";
+  version = "1.21.4";  # Set the desired Nginx version
+
+  src = pkgs.fetchurl {
+    url = "http://nginx.org/download/nginx-${version}.tar.gz";
+    sha256 = "1ziv3xargxhxycd5hp6r3r5mww54nvvydiywcpsamg3i9r3jzxyi";
+  };
+
+  rtmpModule = pkgs.fetchFromGitHub {
+    owner = "arut";
+    repo = "nginx-rtmp-module";
+    rev = "refs/tags/v1.2.2";
+    sha256 = "sha256-0Gz8Hut7hxP3Tb9/ZTbqMHdts1Cshy3YhH4EMblehXg=";  # Verify and update the sha256
+  };
+
+  buildInputs = [ pkgs.openssl pkgs.pcre pkgs.zlib pkgs.libxcrypt ];
+
+  configureFlags = [
+    "--with-http_ssl_module "
+    "--add-module=${rtmpModule}"
+  ];
+  cf = pkgs.lib.concatStrings configureFlags;
+
+  buildPhase = ''
+    ./configure ${cf}
+    make
+  '';
+
+  installPhase = ''
+    make install DESTDIR=$out
+  '';
+
+  meta = with pkgs.lib; {
+    description = "NGINX with nginx-rtmp-module";
+    homepage = "https://github.com/arut/nginx-rtmp-module";
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ workouse ];
+    platforms = platforms.unix;
+  };
+}
+
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -41069,4 +41069,5 @@ with pkgs;
   dillo = callPackage ../by-name/di/dillo/package.nix {
     fltk = fltk13;
   };
+  nginx-rtmp = callPackage ../servers/nginx-rtmp { };
 }
