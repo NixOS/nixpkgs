c4c15be8568f565120a33b46c584b271ee3ad94b
Alexis Praga <alexis.praga@proton.me>
2024-05-20 13:42:15 +0200

    init: ropebwt2 0-unstable-2021-02-01
    Tiddit dependency.

---
+++ pkgs/by-name/ro/ropebwt2/package.nix
@@ -0,0 +1,28 @@
+{ config, lib, stdenv, fetchFromGitHub, zlib  }:
+
+stdenv.mkDerivation {
+  name = "ropebwt2";
+  version = "0-unstable-2021-02-01";
+
+  src = fetchFromGitHub {
+    owner = "lh3";
+    repo = "ropebwt2";
+    rev = "bd8dbd3db2e9e3cff74acc2907c0742c9ebbf033";
+    hash = "sha256-R/VvbprwcfXF2TBZOYmc1MU3AzCcXFfWCHoYYumXtI8=";
+  };
+
+  buildInputs = [ zlib ];
+  installPhase = ''
+      mkdir -p $out/bin
+      cp ropebwt2 $out/bin
+    '';
+
+  meta = with lib; {
+    homepage = "https://github.com/lh3/ropebwt2";
+    description = "Incremental construction of FM-index for DNA sequences";
+    mainProgram = "ropebwt2";
+    license = licenses.mit;
+    maintainers = with maintainers; [ apraga ];
+    platforms = platforms.unix;
+  };
+}
