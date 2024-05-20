1e3397fd4b28f85909f7d861db9996c5eccf82a6
alina sireneva <alina@tei.su>
2024-05-20 23:10:17 +0300

    innoextract: fixed darwin build

--- pkgs/tools/archivers/innoextract/default.nix
+++ pkgs/tools/archivers/innoextract/default.nix
@@ -1,6 +1,14 @@
-{ lib, stdenv, fetchurl, cmake, makeWrapper
-, boost, xz
-, withGog ? false, unar ? null }:
+{ lib
+, stdenv
+, fetchurl
+, cmake
+, makeWrapper
+, boost
+, xz
+, libiconv
+, withGog ? false
+, unar ? null
+}:
 
 stdenv.mkDerivation rec {
   pname = "innoextract";
@@ -11,7 +19,8 @@ stdenv.mkDerivation rec {
     sha256 = "09l1z1nbl6ijqqwszdwch9mqr54qb7df0wp2sd77v17dq6gsci33";
   };
 
-  buildInputs = [ xz boost ];
+  buildInputs = [ xz boost ]
+    ++ lib.optionals stdenv.isDarwin [ libiconv ];
 
   # Python is reported as missing during the build, however
   # including Python does not change the output.
