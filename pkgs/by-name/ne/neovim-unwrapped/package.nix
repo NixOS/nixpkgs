722d1b0e05810288129ed28bd3cb9a374bd1ce6e
natsukium <tomoya.otabi@gmail.com>
2024-05-22 00:31:45 +0900

    Revert "neovim: Update bundled tree-sitter parsers"
    This reverts commit 7f4c36c982a798a954a2f8643060de3a10a3bac9.
    
    neovim was accidentally bumped during the freeze period before release
    with breaking changes.
    This commit is related to neovim 0.10, so it is reverted for now.

--- pkgs/by-name/ne/neovim-unwrapped/package.nix
+++ pkgs/by-name/ne/neovim-unwrapped/package.nix
@@ -86,11 +86,7 @@ in {
 
     dontFixCmake = true;
 
-    inherit lua;
-    treesitter-parsers = treesitter-parsers //
-      { markdown = treesitter-parsers.markdown // { location = "tree-sitter-markdown"; }; } //
-      { markdown-inline = treesitter-parsers.markdown // { language = "markdown_inline"; location = "tree-sitter-markdown-inline"; }; }
-      ;
+    inherit lua treesitter-parsers;
 
     buildInputs = [
       gperf
@@ -173,13 +169,11 @@ in {
     '' + ''
       mkdir -p $out/lib/nvim/parser
     '' + lib.concatStrings (lib.mapAttrsToList
-      (language: grammar: ''
+      (language: src: ''
         ln -s \
           ${tree-sitter.buildGrammar {
-            inherit (grammar) src;
+            inherit language src;
             version = "neovim-${finalAttrs.version}";
-            language = grammar.language or language;
-            location = grammar.location or null;
           }}/parser \
           $out/lib/nvim/parser/${language}.so
       '')
