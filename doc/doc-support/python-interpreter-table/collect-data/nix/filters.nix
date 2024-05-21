e8a751939ca41690f5be8884c7176e129764bca1
Valentin Gagarin <valentin.gagarin@tweag.io>
2024-05-21 22:06:22 +0200

    simplify name matching

--- doc/doc-support/python-interpreter-table/collect-data/nix/filters.nix
+++ doc/doc-support/python-interpreter-table/collect-data/nix/filters.nix
@@ -1,20 +1,10 @@
-# `attrname` must follow the regex `r"(pypy|python)(\d*)"` (Python flavor)
 { lib }:
-let
-  aliasFilterWithExcludes = excludeList: attrname:
-    let
-      inherit (lib.strings) removePrefix toIntBase10;
-      # It's faster to prepend a 0 and call `toIntBase10` than to check if the suffix is ""
-      nameSuffix = "0" + (removePrefix "python" (removePrefix "pypy" attrname));
-    in
-      ((lib.strings.hasPrefix "python" attrname) || (lib.strings.hasPrefix "pypy" attrname)) &&
-      # If the suffix contains more than an int number, then it isn't an interpreter alias
-      (builtins.tryEval(toIntBase10 nameSuffix)).success &&
-      # Aliases are not part of the `pkgs.pythonInterpreters` attrs
-      ! builtins.elem attrname excludeList;
+rec {
+  isPythonInterpreterName = name: (lib.strings.match "(pypy|python)([[:digit:]]*)" name) != null;
 
-in {
-  inherit aliasFilterWithExcludes;
+  aliasFilterWithExcludes = excludeList: name:
+    isPythonInterpreterName name &&
+    ! builtins.elem name excludeList;
 
   /* NB: About pkNames that don't follow the regular expression:
     - `python-cosmopolitan` is not part of `pkgs.pythonInterpreters`.
