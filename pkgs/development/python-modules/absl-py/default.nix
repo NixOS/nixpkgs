a9e99d0af6a44ca96fdace66c06701984f36aacf
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-21 09:30:39 +0200

    python312Packages.absl-py: refactor

--- pkgs/development/python-modules/absl-py/default.nix
+++ pkgs/development/python-modules/absl-py/default.nix
@@ -1,9 +1,9 @@
 {
-  buildPythonPackage,
   lib,
+  buildPythonPackage,
   fetchPypi,
   setuptools,
-  six,
+  pythonOlder,
 }:
 
 buildPythonPackage rec {
@@ -11,21 +11,24 @@ buildPythonPackage rec {
   version = "2.1.0";
   pyproject = true;
 
+  disabled = pythonOlder "3.7";
+
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-eCB5DvuzFnOc3otOGTVyQ/w2CKFSAkKIUT3ZaNfZWf8=";
   };
 
-  nativeBuildInputs = [ setuptools ];
-
-  propagatedBuildInputs = [ six ];
+  build-system = [ setuptools ];
 
   # checks use bazel; should be revisited
   doCheck = false;
 
+  pythonImportsCheck = [ "absl" ];
+
   meta = {
     description = "Abseil Python Common Libraries";
     homepage = "https://github.com/abseil/abseil-py";
+    changelog = "https://github.com/abseil/abseil-py/blob/v${version}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
