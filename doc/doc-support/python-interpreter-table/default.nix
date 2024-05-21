bcc750e3463e3d8073d42d50f384a42bba126059
Valentin Gagarin <valentin.gagarin@tweag.io>
2024-05-21 21:22:21 +0200

    use makeBinPath

--- doc/doc-support/python-interpreter-table/default.nix
+++ doc/doc-support/python-interpreter-table/default.nix
@@ -6,18 +6,14 @@ let
   inherit (pkgs.lib.lists) map;
   inherit (pkgs.lib.strings) concatStringsSep toJSON;
 
-  paths = let
-    inputs = [ pkgs.jq pkgs.unixtools.column ];
-  in concatStringsSep ":" (map (x: "${(getBin x).outPath}/bin") inputs);
-
   data = pkgs.writeText "python-interpreter-table.md"
     (toJSON (import ./collect-data { inherit pkgs; inherit (pkgs) lib; }));
 
   headers = ''["Package","Aliases", "Interpreter"]'';
   keys = ''[.pkgKey, (if .aliases == null then "" else .aliases|join(", ") end) , .interpreter]'';
 
-in pkgs.runCommand "python-table-md" { inherit paths data; } ''
-  export PATH=$PATH:$paths
+in pkgs.runCommand "python-table-md" { inherit data; } ''
+  export PATH=$PATH:${with pkgs; lib.makeBinPath [ jq unixtools.column ]}
   cat $data \
     | jq -r '(${headers} | (., map(length*"-"))), (.[] | ${keys}) | @tsv' -- \
     | column -t -s$'\t' -o ' | ' \
