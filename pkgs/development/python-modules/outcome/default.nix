992009876f150963b38c8e828b84714745d2c55b
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-21 20:33:09 +0200

    python312Packages.outcome: refactor

--- pkgs/development/python-modules/outcome/default.nix
+++ pkgs/development/python-modules/outcome/default.nix
@@ -1,32 +1,42 @@
 { lib
+, attrs
 , buildPythonPackage
 , fetchPypi
+, pythonOlder
 , setuptools
-, attrs
-, pytest
 }:
 
 buildPythonPackage rec {
   pname = "outcome";
   version = "1.3.0.post0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-nc8C5l8pcbgAR7N3Ro5yomjhXArzzxI45v8U9/kRQ7g=";
   };
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
   ];
 
-  nativeCheckInputs = [ pytest ];
-  propagatedBuildInputs = [ attrs ];
+  dependencies = [
+    attrs
+  ];
+
   # Has a test dependency on trio, which depends on outcome.
   doCheck = false;
 
+  pythonImportsCheck = [
+    "outcome"
+  ];
+
   meta = {
-    description = "Capture the outcome of Python function calls.";
+    description = "Capture the outcome of Python function calls";
     homepage = "https://github.com/python-trio/outcome";
+    changelog = "https://github.com/python-trio/outcome/releases/tag/v${version}";
     license = with lib.licenses; [ mit asl20 ];
     maintainers = with lib.maintainers; [ catern ];
   };
