f2d064da039e99f5c8e74a5e38add057492c88ea
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-21 20:12:51 +0000

    polypane: 19.0.0 -> 19.0.1

--- pkgs/applications/networking/browsers/polypane/default.nix
+++ pkgs/applications/networking/browsers/polypane/default.nix
@@ -2,12 +2,12 @@
 
 let
   pname = "polypane";
-  version = "19.0.0";
+  version = "19.0.1";
 
   src = fetchurl {
     url = "https://github.com/firstversionist/${pname}/releases/download/v${version}/${pname}-${version}.AppImage";
     name = "${pname}-${version}.AppImage";
-    sha256 = "sha256-yRJsyJ4dhPAbqVmUmVnFlGGQ5CUJZ0VG19TMnSW0tJ4=";
+    sha256 = "sha256-wy+MzuGQ1+Ayj6SgyItKdy6nldTBy3p24xWlgFwcW+0=";
   };
 
   appimageContents = appimageTools.extractType2 {
