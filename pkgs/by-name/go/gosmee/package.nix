1780e07b764e24668bc9ca606d935be4dd6d069c
Vincent Demeester <vincent@sbr.pm>
2024-05-21 13:11:50 +0200

    gosmee: new package at 0.21.0
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ pkgs/by-name/go/gosmee/package.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildGoModule
+, fetchFromGitHub
+, installShellFiles
+}:
+
+buildGoModule rec {
+  pname = "gosmee";
+  version = "0.21.0";
+
+  src = fetchFromGitHub {
+    owner = "chmouel";
+    repo = "gosmee";
+    rev = "v${version}";
+    sha256 = "sha256-VNBz6roula/YlsYMM1kjQT3TLuQ86/MESzNNkEf/92M=";
+  };
+  vendorHash = null;
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postPatch = ''
+    printf ${version} > gosmee/templates/version
+  '';
+
+  postInstall = ''
+    installShellCompletion --cmd gosmee \
+      --bash <($out/bin/gosmee completion bash) \
+      --fish <($out/bin/gosmee completion fish) \
+      --zsh <($out/bin/gosmee completion zsh)
+  '';
+
+  meta = {
+    description = "Command line server and client for webhooks deliveries (and https://smee.io)";
+    homepage = "https://github.com/chmouel/gosmee";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ vdemeester chmouel ];
+  };
+}
