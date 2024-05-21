74dbfced093b5249883fc603d604e61132902c0a
Cameron Dart <8763518+SkamDart@users.noreply.github.com>
2024-05-21 16:25:29 -0700

    maintainers: add skamdart

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18880,6 +18880,12 @@
     githubId = 158321;
     name = "Stewart Mackenzie";
   };
+  skamdart = {
+    email = "sigskam@proton.me";
+    github = "SkamDart";
+    githubId = 8763518;
+    name = "skamdart";
+  };
   skovati = {
     github = "skovati";
     githubId = 49844593;
