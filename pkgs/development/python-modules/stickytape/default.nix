376309fc14fed804c295638991b24c65ff735534
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-21 09:41:33 +0200

    python312Packages.stickytape: refactor

--- pkgs/development/python-modules/stickytape/default.nix
+++ pkgs/development/python-modules/stickytape/default.nix
@@ -1,19 +1,27 @@
 { lib
 , buildPythonPackage
 , fetchFromGitHub
+, pythonOlder
+, setuptools
 }:
 
 buildPythonPackage rec {
   pname = "stickytape";
   version = "0.2.1";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "mwilliamson";
-    repo = pname;
-    rev = version;
+    repo = "stickytape";
+    rev = "refs/tags/${version}";
     hash = "sha256-KOZN9oxPb91l8QVU07I49UMNXqox8j+oekA1fMtj6l8=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   # Tests have additional requirements
   doCheck = false;
@@ -24,9 +32,9 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Python module to convert Python packages into a single script";
-    mainProgram = "stickytape";
     homepage = "https://github.com/mwilliamson/stickytape";
-    license = with licenses; [ bsd2 ];
+    license = licenses.bsd2;
     maintainers = with maintainers; [ fab ];
+    mainProgram = "stickytape";
   };
 }
