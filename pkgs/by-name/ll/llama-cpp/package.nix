f26e8f439c4a335989a00542b479fecc223ff4d0
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-21 14:41:37 +0000

    llama-cpp: 2901 -> 2953

--- pkgs/by-name/ll/llama-cpp/package.nix
+++ pkgs/by-name/ll/llama-cpp/package.nix
@@ -72,13 +72,13 @@ let
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "llama-cpp";
-  version = "2901";
+  version = "2953";
 
   src = fetchFromGitHub {
     owner = "ggerganov";
     repo = "llama.cpp";
     rev = "refs/tags/b${finalAttrs.version}";
-    hash = "sha256-bsPqSvJ9/iXE/wu+8kHikt1YPxX5XC+WsPbXTuSlsAo=";
+    hash = "sha256-IqR0tdTdrydrMCgOfNbRnVESN3pEzti3bAuTH9i3wQQ=";
     leaveDotGit = true;
     postFetch = ''
       git -C "$out" rev-parse --short HEAD > $out/COMMIT
