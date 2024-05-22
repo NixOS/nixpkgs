6777931fb8091bf2409e5aa4dabd9ff6fa697fe5
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-22 02:37:19 +0000

    python311Packages.biliass: 1.3.7 -> 1.3.11

--- pkgs/development/python-modules/biliass/default.nix
+++ pkgs/development/python-modules/biliass/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "biliass";
-  version = "1.3.7";
+  version = "1.3.11";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "yutto-dev";
     repo = "biliass";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Opb4rlGe+LDJZs3F7e/NZYfuMtHEWUZeMm8VZQfEzKI=";
+    hash = "sha256-hBorYAqtxTZ4LElxxJOGxC2g7sBRhRKVv6HOVHZn9FA=";
   };
 
   nativeBuildInputs = [
