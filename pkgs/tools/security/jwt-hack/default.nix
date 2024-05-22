32e624428bda5e04e11d5dd01cd13a63c10530be
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-22 01:19:42 +0000

    jwt-hack: 1.1.2 -> 1.2.0

--- pkgs/tools/security/jwt-hack/default.nix
+++ pkgs/tools/security/jwt-hack/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "jwt-hack";
-  version = "1.1.2";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "hahwul";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-K0ZtEi0zAKRlIGvorrXmtmkcMvyLIXWPnVMQANZbClk=";
+    hash = "sha256-IHR+ItI4ToINLpkVc7yrgpNTS17nD02G6x3pNMEfIW4=";
   };
 
-  vendorHash = "sha256-VYh3oRy8bmtXf6AnLNi/M2kA6t+crW3AXBiGovpdt8U=";
+  vendorHash = "sha256-YEH+epSvyy1j0s8AIJ5+BdF47H7KqgBRC4t81noOkjo=";
 
   meta = with lib; {
     description = "Tool for attacking JWT";
