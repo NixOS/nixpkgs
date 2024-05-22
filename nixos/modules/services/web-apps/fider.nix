76569ebe3ac08021a048d683eb13a5561b74eb6a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-22 21:40:19 +0200

    !fixup nixos/fider: init

--- nixos/modules/services/web-apps/fider.nix
+++ nixos/modules/services/web-apps/fider.nix
@@ -1,41 +1,85 @@
-{ config
-, lib
-, pkgs
-, ...
+{
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 let
   cfg = config.services.fider;
+
+  postgresqlPackage =
+    if config.services.postgresql.enable then config.services.postgresql.package else pkgs.postgresql;
 in
 {
   options = {
+
     services.fider = {
       enable = lib.mkEnableOption "the Fider server";
       package = lib.mkPackageOption pkgs "fider" { };
+      dataDir = lib.mkOption {
+        type = lib.types.str;
+        default = "/var/lib/fider";
+        description = "Default data folder for Fider.";
+        example = "/mnt/fider";
+      };
+      database = {
+        url = lib.mkOption {
+          type = lib.types.str;
+          default = "local";
+          description = ''
+            URI to use for the main PostgreSQL database. If this needs to include
+            credentials that shouldn't be world-readable in the Nix store, set an
+            environment file on the systemd service and override the
+            `DATABASE_URL` entry. Pass the string
+            `local` to setup a database on the local server.
+          '';
+        };
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
+    services.postgresql = lib.mkIf (cfg.database.url == "local") {
+      enable = true;
+      ensureUsers = [
+        {
+          name = "fider";
+          ensureDBOwnership = true;
+        }
+      ];
+      ensureDatabases = [ "fider" ];
+    };
+
     systemd.services.fider = {
       description = "Fider server";
       wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      preStart = ''
-        ${lib.getExe cfg.package} migrate
-      '';
-      environment = {
-        DATABASE_URL = "postgres://fider_ci:fider_ci_pw@postgres:5432/fider_ci?sslmode=disable";
-        JWT_SECRET = "not_so_secret";
-        BASE_URL = "/";
-        EMAIL_NOREPLY = "noreply@fider.io";
-        EMAIL_SMTP_HOST = "mailhog";
-        EMAIL_SMTP_PORT = "1025";
-      };
+      after = [
+        "network.target"
+      ] ++ lib.optionals (cfg.database.url == "local") [ "postgresql.service" ];
+      requires = lib.optionals (cfg.database.url == "local") [ "postgresql.service" ];
+      environment =
+        let
+          localPostgresqlUrl = "postgres://localhost/fider?host=/run/postgresql";
+        in
+        {
+          DATABASE_URL = if (cfg.database.url == "local") then localPostgresqlUrl else cfg.database.url;
+          JWT_SECRET = "not_so_secret";
+          BASE_URL = "/";
+          EMAIL_NOREPLY = "noreply@fider.io";
+          EMAIL_SMTP_HOST = "mailhog";
+          EMAIL_SMTP_PORT = "1025";
+          BLOB_STORAGE_FS_PATH = "${cfg.dataDir}";
+        };
       serviceConfig = {
-        ExecStart = ''${lib.getExe cfg.package}'';
+        ExecStart = lib.getExe cfg.package;
         StateDirectory = "fider";
         DynamicUser = true;
         PrivateTmp = "yes";
         Restart = "on-failure";
+        RuntimeDirectory = "fider";
+        RuntimeDirectoryPreserve = true;
+        CacheDirectory = "fider";
+        WorkingDirectory = "${cfg.package}";
       };
     };
   };
