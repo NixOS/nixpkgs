0bd1bacb94e89e1f4516e620af7f147cc70ff5e0
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-22 20:59:02 -0300

    iw: adopt and refactor
    - finalAttrs
    - split outputs
    - strictDeps

--- pkgs/by-name/iw/iw/package.nix
+++ pkgs/by-name/iw/iw/package.nix
@@ -1,31 +1,45 @@
-{ lib, stdenv, fetchurl, pkg-config, libnl }:
+{
+  lib,
+  fetchurl,
+  libnl,
+  pkg-config,
+  stdenv,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "iw";
   version = "5.19";
 
   src = fetchurl {
-    url = "https://www.kernel.org/pub/software/network/${pname}/${pname}-${version}.tar.xz";
-    sha256 = "sha256-8We76UfdU7uevAwdzvXbatc6wdYITyxvk3bFw2DMTU4=";
+    url = "https://www.kernel.org/pub/software/network/iw/iw-${finalAttrs.version}.tar.xz";
+    hash = "sha256-8We76UfdU7uevAwdzvXbatc6wdYITyxvk3bFw2DMTU4=";
   };
 
   nativeBuildInputs = [ pkg-config ];
+
   buildInputs = [ libnl ];
+
+  outputs = [
+    "out"
+    "man"
+  ];
 
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
 
+  strictDeps = true;
+
   meta = {
+    homepage = "https://wireless.wiki.kernel.org/en/users/Documentation/iw";
     description = "Tool to use nl80211";
-    mainProgram = "iw";
     longDescription = ''
       iw is a new nl80211 based CLI configuration utility for wireless devices.
       It supports all new drivers that have been added to the kernel recently.
       The old tool iwconfig, which uses Wireless Extensions interface, is
       deprecated and it's strongly recommended to switch to iw and nl80211.
     '';
-    homepage = "https://wireless.wiki.kernel.org/en/users/Documentation/iw";
     license = lib.licenses.isc;
-    maintainers = with lib.maintainers; [ viric primeos ];
-    platforms = with lib.platforms; linux;
+    mainProgram = "iw";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.linux;
   };
-}
+})
