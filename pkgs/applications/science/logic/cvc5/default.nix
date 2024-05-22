0d9a4a2b01b70b8970614e5420dbe5555fcd3a37
Thea Heinen <theinen@protonmail.com>
2024-05-22 17:23:45 -0400

    cvc5: reformat cvc5/default.nix using nixfmt-rfc-style

--- pkgs/applications/science/logic/cvc5/default.nix
+++ pkgs/applications/science/logic/cvc5/default.nix
@@ -1,4 +1,21 @@
-{ lib, stdenv, fetchFromGitHub, pkg-config, cmake, flex, cadical, symfpu, gmp, python3, gtest, libantlr3c, antlr3_4, boost, jdk, pythonBindings ? false }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pkg-config,
+  cmake,
+  flex,
+  cadical,
+  symfpu,
+  gmp,
+  python3,
+  gtest,
+  libantlr3c,
+  antlr3_4,
+  boost,
+  jdk,
+  pythonBindings ? false,
+}:
 
 stdenv.mkDerivation rec {
   pname = "cvc5";
@@ -12,16 +29,38 @@ stdenv.mkDerivation rec {
   };
 
   pythonic = fetchFromGitHub {
-    owner  = "cvc5";
-    repo   = "cvc5_pythonic_api";
-    rev    = "0bb7f092113e3b933dcd61b0959f3e9be9638109";
-    hash  = "sha256-FfZa18v1iPN9HvKtzadbJ9N7BIDPvHDSeWevCJIVNWI=";
+    owner = "cvc5";
+    repo = "cvc5_pythonic_api";
+    rev = "0bb7f092113e3b933dcd61b0959f3e9be9638109";
+    hash = "sha256-FfZa18v1iPN9HvKtzadbJ9N7BIDPvHDSeWevCJIVNWI=";
   };
 
-  nativeBuildInputs = [ pkg-config cmake flex ];
+  nativeBuildInputs = [
+    pkg-config
+    cmake
+    flex
+  ];
   buildInputs = [
-    cadical.dev symfpu gmp gtest libantlr3c antlr3_4 boost jdk
-    (python3.withPackages (ps: with ps; [ pyparsing tomli ] ++ lib.optionals pythonBindings [ scikit-build cython ]))
+    cadical.dev
+    symfpu
+    gmp
+    gtest
+    libantlr3c
+    antlr3_4
+    boost
+    jdk
+    (python3.withPackages (
+      ps:
+      with ps;
+      [
+        pyparsing
+        tomli
+      ]
+      ++ lib.optionals pythonBindings [
+        scikit-build
+        cython
+      ]
+    ))
   ];
 
   preConfigure = ''
@@ -33,23 +72,32 @@ stdenv.mkDerivation rec {
     mv $out/lib/python3.11/ $python/lib/.
   '';
 
-  outputs = [ "out" "lib" "dev" ]  ++ lib.optional pythonBindings "python";
+  outputs = [
+    "out"
+    "lib"
+    "dev"
+  ] ++ lib.optional pythonBindings "python";
   cmakeBuildType = "Production";
 
-  cmakeFlags = [
-    "-DBUILD_SHARED_LIBS=1"
-    "-DCMAKE_SKIP_BUILD_RPATH=ON"
-    "-DANTLR3_JAR=${antlr3_4}/lib/antlr/antlr-3.4-complete.jar"
-  ] ++ lib.optionals pythonBindings [ "-DBUILD_BINDINGS_PYTHON=1" "-DPYTHONIC_PATH=${pythonic}" ];
+  cmakeFlags =
+    [
+      "-DBUILD_SHARED_LIBS=1"
+      "-DCMAKE_SKIP_BUILD_RPATH=ON"
+      "-DANTLR3_JAR=${antlr3_4}/lib/antlr/antlr-3.4-complete.jar"
+    ]
+    ++ lib.optionals pythonBindings [
+      "-DBUILD_BINDINGS_PYTHON=1"
+      "-DPYTHONIC_PATH=${pythonic}"
+    ];
 
   doCheck = true;
 
   meta = with lib; {
     description = "High-performance theorem prover and SMT solver";
     mainProgram = "cvc5";
-    homepage    = "https://cvc5.github.io";
-    license     = licenses.gpl3Only;
-    platforms   = platforms.unix;
+    homepage = "https://cvc5.github.io";
+    license = licenses.gpl3Only;
+    platforms = platforms.unix;
     maintainers = with maintainers; [ shadaj ];
   };
 }
