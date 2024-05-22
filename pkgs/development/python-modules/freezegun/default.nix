1fef11c4ec11a32e53ea31e63d76db1ff1005484
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-23 00:35:51 +0200

    python312Packages.freezegun: 1.4.0 -> 1.5.1
    Changelog: https://github.com/spulec/freezegun/blob/1.5.1/CHANGELOG

--- pkgs/development/python-modules/freezegun/default.nix
+++ pkgs/development/python-modules/freezegun/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "freezegun";
-  version = "1.4.0";
+  version = "1.5.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-EJObC6D/Wtrs87BqXC9zBx2WeOUHxertsjx2HVasd0s=";
+    hash = "sha256-sp3t/NptXo4IPOcbK1QnU61Iz+xEA3s/x5cC4pgKiek=";
   };
 
   nativeBuildInputs = [ setuptools ];
