67518f18ca9c400b166efd3eed83839bc80a78b0
Arjan Schrijver <github@anymore.nl>
2024-05-22 10:40:54 +0200

    python3Packages.pywlroots: 0.16.8 -> 0.17.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12788,7 +12788,7 @@ self: super: with self; {
   pywizlight = callPackage ../development/python-modules/pywizlight { };
 
   pywlroots = callPackage ../development/python-modules/pywlroots {
-    wlroots = pkgs.wlroots_0_16;
+    wlroots = pkgs.wlroots_0_17;
   };
 
   pyws66i = callPackage ../development/python-modules/pyws66i { };
