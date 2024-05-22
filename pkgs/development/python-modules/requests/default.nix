8f68f09d3b189b4e073b9766a1577f9cfdc61f6c
Fabian Affolter <mail@fabian-affolter.ch>
2024-05-22 10:20:26 +0200

    python312Packages.requests: 2.31.0 -> 2.32.2
    Changelog: https://github.com/psf/requests/blob/v2.32.2/HISTORY.md
    
    Fixes CVE-2024-35195

--- pkgs/development/python-modules/requests/default.nix
+++ pkgs/development/python-modules/requests/default.nix
@@ -13,22 +13,25 @@
   pytest-xdist,
   pytestCheckHook,
   pythonOlder,
+  setuptools,
   urllib3,
 }:
 
 buildPythonPackage rec {
   pname = "requests";
-  version = "2.31.0";
-  format = "setuptools";
+  version = "2.32.2";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   __darwinAllowLocalNetworking = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-lCxadY+Y15Dq7Ropy27vx/+w0c968Fw9J5Flbb1q0eE=";
+    hash = "sha256-3ZUf9ezz47OqJrQHA7p3SV2rQdqDmucu88jl2OJDMok=";
   };
+
+  build-system = [ setuptools ];
 
   dependencies = [
     brotlicffi
