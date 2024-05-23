f633fcf7b83b55fccc0e72f7d1710ee64b1135c9
Enric Morales <me@enric.me>
2024-05-23 11:18:43 +0200

    linuxPackages.ena: reformat, use upstream patch, remove problematic flag

--- pkgs/os-specific/linux/ena/default.nix
+++ pkgs/os-specific/linux/ena/default.nix
@@ -1,4 +1,9 @@
-{ lib, stdenv, fetchFromGitHub, kernel }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  kernel,
+}:
 
 stdenv.mkDerivation rec {
   version = "2.12.0";
@@ -16,20 +21,18 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = kernel.moduleBuildDependencies;
   makeFlags = kernel.makeFlags;
 
-  # linux 3.12
-  env.NIX_CFLAGS_COMPILE = "-Wno-error=implicit-function-declaration";
+  env.KERNEL_BUILD_DIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
 
   patches = [
-    # Use kernel version checks instead of API feature detection
-    # See https://github.com/NixOS/nixpkgs/pull/310680
-    ./override-features-api-detection.patch
+    # The eth_hw_addr_set function was backported to kernel 4.19.291 but support for it wasn't added to ENA. It will be added in a future release.
+    # See https://github.com/amzn/amzn-drivers/issues/302#issuecomment-2126587626
+    ./0001-Temp-fix-Allow-4.19.291-to-compile-eth_hw_addr_set.patch
   ];
 
   configurePhase = ''
     runHook preConfigure
     cd kernel/linux/ena
     export ENA_PHC_INCLUDE=1
-    substituteInPlace Makefile --replace '/lib/modules/$(BUILD_KERNEL)' ${kernel.dev}/lib/modules/${kernel.modDirVersion}
     runHook postConfigure
   '';
 
@@ -47,7 +50,10 @@ stdenv.mkDerivation rec {
     description = "Amazon Elastic Network Adapter (ENA) driver for Linux";
     homepage = "https://github.com/amzn/amzn-drivers";
     license = licenses.gpl2Only;
-    maintainers = with maintainers; [ eelco sielicki ];
+    maintainers = with maintainers; [
+      eelco
+      sielicki
+    ];
     platforms = platforms.linux;
   };
 }
