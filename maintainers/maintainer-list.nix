517a3aef28ff8772a317224049757b5e81f837d0
pcboy <david.hagege@gmail.com>
2024-05-23 20:02:48 +0900

    maintainers: add @pcboy

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15752,6 +15752,12 @@
     githubId = 817039;
     name = "Paulo Casaretto";
   };
+  pcboy = {
+    email = "david@joynetiks.com";
+    github = "pcboy";
+    githubId = 943430;
+    name = "David Hagege";
+  };
   pedrohlc = {
     email = "root@pedrohlc.com";
     github = "PedroHLC";
