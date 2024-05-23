018dc39372764424ea962bd5d85cbc724e7a8827
Dawid Dziurla <dawidd0811@gmail.com>
2024-05-23 20:21:24 +0200

    hieroglyphic: 1.0.1 -> 1.1.0

--- pkgs/by-name/hi/hieroglyphic/package.nix
+++ pkgs/by-name/hi/hieroglyphic/package.nix
@@ -15,6 +15,7 @@
   libadwaita,
   darwin,
   gettext,
+  appstream,
 }:
 
 let
@@ -22,20 +23,22 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "hieroglyphic";
-  version = "1.0.1";
-  # Note: 1.1.0 requires a higher gtk4 version. This requirement could be patched out.
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "FineFindus";
     repo = "Hieroglyphic";
     rev = "refs/tags/v${finalAttrs.version}";
-    hash = "sha256-Twx3yM71xn2FT3CbiFGbo2knGvb4MBl6VwjWlbjfks0=";
+    hash = "sha256-8UUFatJwtxqumhHd0aiPk6nKsaaF/jIIqMFxXye0X8U=";
   };
 
-  cargoDeps = rustPlatform.fetchCargoTarball {
-    inherit (finalAttrs) src;
-    name = "${finalAttrs.pname}-${finalAttrs.version}";
-    hash = "sha256-Se/YCi0e+Uoh5guDteLRXZYyk7et0NA8cv+vNpLxzx4=";
+  # We have to use importCargoLock here because `cargo vendor` currently doesn't support workspace
+  # inheritance within Git dependencies, but importCargoLock does.
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "detexify-0.4.0" = "sha256-BPOHNr3pwu2il3/ERko+WHAWby4rPR49i62tXDlDRu0=";
+    };
   };
 
   nativeBuildInputs = [
@@ -47,6 +50,7 @@ stdenv.mkDerivation (finalAttrs: {
     pkg-config
     wrapGAppsHook4
     desktop-file-utils
+    appstream
   ];
 
   buildInputs =
