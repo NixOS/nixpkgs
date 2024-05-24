f5a764de27bea0f3fe8f90992221c66a8cc30e02
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-05-24 12:53:45 +0500

    firefox-gnome-theme: init at 126

---
+++ pkgs/by-name/fi/firefox-gnome-theme/package.nix
@@ -0,0 +1,41 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+}:
+
+stdenv.mkDerivation (finalAttr: {
+  pname = "firefox-gnome-theme";
+  version = "126";
+
+  src = fetchFromGitHub {
+    owner = "rafaelmardojai";
+    repo = "firefox-gnome-theme";
+    rev = "v${finalAttr.version}";
+    hash = "sha256-jVbj2JD5GRkP8s3vKBtd9PDpftf3kjLR0ZA/ND/c2+Q=";
+  };
+
+  strictDeps = true;
+
+  # Only copy necessary files
+  installPhase = ''
+    runHook preInstall
+    
+    mkdir -p $out
+    cp -r ./theme ./icon.svg ./userChrome.css ./userContent.css -t $out
+    
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "A GNOME theme for Firefox";
+    longDescription = ''
+      A GNOME theme for Firefox.
+      This theme follows latest GNOME Adwaita style.
+    '';
+    homepage = "https://github.com/rafaelmardojai/firefox-gnome-theme";
+    downloadPage = "https://github.com/rafaelmardojai/firefox-gnome-theme/releases";
+    license = lib.licenses.unlicense;
+    maintainers = with lib.maintainers; [];
+    platforms = lib.platforms.all;
+  };
+})
