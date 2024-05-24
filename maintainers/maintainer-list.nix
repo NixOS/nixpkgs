2159c33e45d5aa3322392e42dc63557b1f34d1b8
aware70 <7832566+aware70@users.noreply.github.com>
2024-05-23 21:39:14 -0500

    maintainers: add aware70

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2458,6 +2458,11 @@
     githubId = 206242;
     name = "Andreas Wiese";
   };
+  aware70 = {
+    name = "aware70";
+    github = "aware70";
+    githubId = 7832566;
+  };
   awwpotato = {
     email = "awwpotato@voidq.com";
     github = "awwpotato";
