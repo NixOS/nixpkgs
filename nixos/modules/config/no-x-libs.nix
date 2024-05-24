cb58275dc676d0ef90671feb260957098de8e47b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-05-25 01:22:51 +0200

    nixos/no-x-libs: build gst-plugins-rs without gtk

--- nixos/modules/config/no-x-libs.nix
+++ nixos/modules/config/no-x-libs.nix
@@ -48,6 +48,7 @@ with lib;
         gst-plugins-bad = super.gst_all_1.gst-plugins-bad.override { guiSupport = false; };
         gst-plugins-base = super.gst_all_1.gst-plugins-base.override { enableWayland = false; enableX11 = false; };
         gst-plugins-good = super.gst_all_1.gst-plugins-good.override { enableWayland = false; enableX11 = false; gtkSupport = false; qt5Support = false; qt6Support = false; };
+        gst-plugins-rs = super.gst_all_1.gst-plugins-rs.override { withGtkPlugins = false; };
       };
       imagemagick = super.imagemagick.override { libX11Support = false; libXtSupport = false; };
       imagemagickBig = super.imagemagickBig.override { libX11Support = false; libXtSupport = false; };
