12e943374718f6d11ed12b36cb8490c610fbcf7b
Jan van Brügge <supermanitu@gmail.com>
2024-05-24 15:05:14 +0100

    naproche: unstable-2024-01-18 -> unstable-2024-05-19

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5857,8 +5857,6 @@ with pkgs;
 
   mrkd = callPackage ../tools/text/mrkd { };
 
-  naproche = callPackage ../applications/science/logic/naproche { };
-
   nautilus-open-any-terminal = callPackage ../tools/misc/nautilus-open-any-terminal { };
 
   n2n = callPackage ../tools/networking/n2n { };
