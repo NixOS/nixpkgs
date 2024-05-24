c066d5a16918399a87d29abdc7beb8245fe86d87
natsukium <tomoya.otabi@gmail.com>
2024-05-24 01:33:13 +0000

    python3Packages.langchain-community: 0.0.38 -> 0.2.1
    Diff: https://github.com/langchain-ai/langchain/compare/langchain-community==0.0.38...langchain-community==0.2.1

--- pkgs/development/python-modules/langchain-community/default.nix
+++ pkgs/development/python-modules/langchain-community/default.nix
@@ -1,33 +1,47 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   poetry-core,
   pythonOlder,
   aiohttp,
   dataclasses-json,
+  duckdb-engine,
+  langchain,
   langchain-core,
   langsmith,
+  lark,
   numpy,
+  pandas,
+  pytest-asyncio,
+  pytest-mock,
+  pytestCheckHook,
   pyyaml,
   requests,
+  requests-mock,
+  responses,
   sqlalchemy,
+  syrupy,
   tenacity,
+  toml,
   typer,
 }:
 
 buildPythonPackage rec {
   pname = "langchain-community";
-  version = "0.0.38";
+  version = "0.2.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
-  src = fetchPypi {
-    pname = "langchain_community";
-    inherit version;
-    hash = "sha256-En/Et1vGe2L+gnxmwC5xWnMP74/mm9ICPUZrqwa1gQ0=";
+  src = fetchFromGitHub {
+    owner = "langchain-ai";
+    repo = "langchain";
+    rev = "langchain-community==${version}";
+    hash = "sha256-h8ZJiQYmyvzaRrEVNS7SamJTq4zY7J1IgYdQiVBFh4I=";
   };
+
+  sourceRoot = "${src.name}/libs/community";
 
   build-system = [ poetry-core ];
 
@@ -35,6 +49,7 @@ buildPythonPackage rec {
     aiohttp
     dataclasses-json
     langchain-core
+    langchain
     langsmith
     numpy
     pyyaml
@@ -49,8 +64,20 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "langchain_community" ];
 
-  # PyPI source does not have tests
-  doCheck = false;
+  nativeCheckInputs = [
+    duckdb-engine
+    lark
+    pandas
+    pytest-asyncio
+    pytest-mock
+    pytestCheckHook
+    requests-mock
+    responses
+    syrupy
+    toml
+  ];
+
+  pytestFlagsArray = [ "tests/unit_tests" ];
 
   passthru = {
     updateScript = langchain-core.updateScript;
@@ -59,6 +86,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Community contributed LangChain integrations";
     homepage = "https://github.com/langchain-ai/langchain/tree/master/libs/community";
+    changelog = "https://github.com/langchain-ai/langchain/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ natsukium ];
   };
