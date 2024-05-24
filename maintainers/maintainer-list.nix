146f08b87ad2fa039d0be5e8299dd80ab4ceb745
Peïo Thibault <peio.thibault@gmail.com>
2024-05-24 16:01:53 +0200

    maintainers: add potb

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16325,6 +16325,11 @@
     githubId = 146413;
     name = "Tobias Poschwatta";
   };
+  potb = {
+    name = "Peïo Thibault";
+    github = "potb";
+    githubId = 10779093;
+  };
   poweredbypie = {
     name = "poweredbypie";
     github = "poweredbypie";
