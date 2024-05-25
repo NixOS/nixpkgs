fb6123bedfb921623c30538b8241fe381522b659
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-25 14:42:57 -0300

    cbqn: 0.6.0 -> 0.7.0
    Also other sources were updated:
    - cbqn-bytecode:  0-unstable-2023-12-04 -> 0-unstable-2024-05-22
    - replxx: didn't change
    - singeli: 0-unstable-2023-12-29 -> 0-unstable-2024-02-26

--- pkgs/development/interpreters/bqn/cbqn/sources.nix
+++ pkgs/development/interpreters/bqn/cbqn/sources.nix
@@ -10,13 +10,13 @@
   cbqn = let
     self = {
       pname = "cbqn";
-      version = "0.6.0";
+      version = "0.7.0";
 
       src = fetchFromGitHub {
         owner = "dzaima";
         repo = "CBQN";
         rev = "v${self.version}";
-        hash = "sha256-hd8Z9x5dy7+KjYBMqLC31QblcLS8aqSsVKXIZc6GBeI=";
+        hash = "sha256-TUK0HrJ1IyiVi9Y3S1IrK/d4/EZxdRdWyxsAwj79KEc=";
       };
     };
   in
@@ -24,13 +24,13 @@
 
   cbqn-bytecode = {
     pname = "cbqn-bytecode";
-    version = "0-unstable-2023-12-04";
+    version = "0-unstable-2024-05-22";
 
     src = fetchFromGitHub {
       owner = "dzaima";
       repo = "cbqnBytecode";
-      rev = "fd68e7436976d6106349109a2f8955d8d82d5c6a";
-      hash = "sha256-K2bGS3/ALZIL/HQ4bOKJCzeDU+B8UFWwaz8SKN847/0=";
+      rev = "c5674783c11d7569e5a4d166600ffcde2409331d";
+      hash = "sha256-y7gqHzUxVUUVryutlq3Upuso8r3ZRSyF7ydMg1OVlwA=";
     };
   };
 
@@ -48,13 +48,13 @@
 
   singeli = {
     pname = "singeli";
-    version = "0-unstable-2023-12-29";
+    version = "0-unstable-2024-02-26";
 
     src = fetchFromGitHub {
       owner = "mlochbaum";
       repo = "Singeli";
-      rev = "5f9cbd46c265491ff167a5d9377d1462539dbdd8";
-      hash = "sha256-q8csizarLZhTQ7TaAew4Bz1WB2nM8djxhZfYJNbsro4=";
+      rev = "ce6ef5d06d35777f0016bbfe0c6c1cf6a9c1b48e";
+      hash = "sha256-dDoWoq4LYMD2CKyPxXDAwoeH2E+f0FDyvngtWPEr67w=";
     };
   };
 }
