1323cb792a10da63de35e12c5cf251b4835dc88c
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-25 04:28:27 +0000

    seafile-shared: 9.0.5 -> 9.0.6

--- pkgs/misc/seafile-shared/default.nix
+++ pkgs/misc/seafile-shared/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "seafile-shared";
-  version = "9.0.5";
+  version = "9.0.6";
 
   src = fetchFromGitHub {
     owner = "haiwen";
     repo = "seafile";
     rev = "v${version}";
-    sha256 = "sha256-ENxmRnnQVwRm/3OXouM5Oj0fLVRSj0aOHJeVT627UdY=";
+    sha256 = "sha256-ig22Rw9VWPqOsJS1Wxy69OjdMRcxh2fOyqMHBEky/Uo=";
   };
 
   nativeBuildInputs = [
