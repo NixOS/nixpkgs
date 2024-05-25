ea8341fb3e8802327b2b1055c6fafa63e8f8b9b0
Matthias Beyer <mail@beyermatthias.de>
2024-05-25 10:10:26 +0200

    difftastic: 0.56.1 -> 0.58.0
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/tools/text/difftastic/default.nix
+++ pkgs/tools/text/difftastic/default.nix
@@ -1,47 +1,27 @@
 { lib
-, fetchpatch
 , rustPlatform
 , fetchFromGitHub
 , testers
 , difftastic
-, stdenv
 }:
-
-let
-  mimallocPatch = fetchpatch {
-    name = "fix-build-on-older-macos-releases.patch";
-    url = "https://github.com/microsoft/mimalloc/commit/40e0507a5959ee218f308d33aec212c3ebeef3bb.patch";
-    sha256 = "sha256-DK0LqsVXXiEVQSQCxZ5jyZMg0UJJx9a/WxzCroYSHZc=";
-  };
-in
 
 rustPlatform.buildRustPackage rec {
   pname = "difftastic";
-  version = "0.56.1";
+  version = "0.58.0";
 
   src = fetchFromGitHub {
     owner = "wilfred";
     repo = pname;
     rev = version;
-    hash = "sha256-XQzsLowHtgXIKfUWx1Sj1D0F8scb7fNp33Cwfh5XvJI=";
+    hash = "sha256-PTc8/NhWsLcKJj+9ebV/YaWEmyOWKJCYUjmVbr4z2SY=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "tree_magic_mini-3.0.2" = "sha256-iIX/DeDbquObDPOx/pctVFN4R8GSkD9bPNkNgOLdUJs=";
-    };
-  };
+  cargoSha256 = "sha256-wtWJ32bBoCmx+eGkrOk8o0OQFMhGYZEBEDI1oHn3Zuw=";
 
   # skip flaky tests
   checkFlags = [
     "--skip=options::tests::test_detect_display_width"
   ];
-
-  postPatch = ''
-    patch -d $cargoDepsCopy/libmimalloc-sys-0.1.24/c_src/mimalloc \
-      -p1 < ${mimallocPatch}
-  '';
 
   passthru.tests.version = testers.testVersion { package = difftastic; };
 
