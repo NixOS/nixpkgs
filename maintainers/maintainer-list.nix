7f366f9fcf2085fb91c6195a1e818b5ea8b21063
Daniel Mita <966706+m-dango@users.noreply.github.com>
2024-05-25 14:59:20 +0100

    Maintainter ordering

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13022,6 +13022,15 @@
     githubId = 1377571;
     name = "Matthew S. Daiter";
   };
+  m-dango = {
+    github = "m-dango";
+    githubId = 966706;
+    matrix = "@m-dango:beeper.com";
+    name = "Daniel Mita";
+    keys = [{
+      fingerprint = "69F7 7191 46B0 74E0 0F43 056C 6479 B966 8E88 BAFE";
+    }];
+  };
   mdarocha = {
     email = "marek@mdarocha.pl";
     github = "mdarocha";
@@ -14176,15 +14185,6 @@
     github = "Myrl";
     githubId = 9636071;
     name = "Myrl Hex";
-  };
-  m-dango = {
-    github = "m-dango";
-    githubId = 966706;
-    matrix = "@m-dango:beeper.com";
-    name = "Daniel Mita";
-    keys = [{
-      fingerprint = "69F7 7191 46B0 74E0 0F43 056C 6479 B966 8E88 BAFE";
-    }];
   };
   n00b0ss = {
     email = "nixpkgs@n00b0ss.de";
