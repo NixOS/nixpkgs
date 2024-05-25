88fb6d37e393a8d7f4214953940a678541a80892
Julien Moutinho <julm+nixpkgs@sourcephile.fr>
2024-05-25 02:23:48 +0200

    nixos/radicle: init services

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -804,6 +804,7 @@ in {
   rabbitmq = handleTest ./rabbitmq.nix {};
   radarr = handleTest ./radarr.nix {};
   radicale = handleTest ./radicale.nix {};
+  radicle = runTest ./radicle.nix;
   ragnarwm = handleTest ./ragnarwm.nix {};
   rasdaemon = handleTest ./rasdaemon.nix {};
   readarr = handleTest ./readarr.nix {};
