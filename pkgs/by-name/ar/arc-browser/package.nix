cd46bba5ed9155443f68306bd4e558dc1a25da35
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-05-25 20:20:29 +0300

    arc-browser: 1.43.1-50198 -> 1.44.2-50412
    Changelog: https://arc.net/e/C56904FA-1C75-4D77-9A87-E7F1A52529CD

--- pkgs/by-name/ar/arc-browser/package.nix
+++ pkgs/by-name/ar/arc-browser/package.nix
@@ -9,11 +9,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "arc-browser";
-  version = "1.43.1-50198";
+  version = "1.44.2-50412";
 
   src = fetchurl {
     url = "https://releases.arc.net/release/Arc-${finalAttrs.version}.dmg";
-    hash = "sha256-++C3COEs0ycfj+vV4PdlWloc4M0hGZG5fdJ+rbyBo7w=";
+    hash = "sha256-0UrvCTGnbt+jQ4UpEt5vsgKZ/UDJz3I1obK4GPshNjg=";
   };
 
   nativeBuildInputs = [ undmg ];
