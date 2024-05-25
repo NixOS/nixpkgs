f5a4551619a872f9acb3282fbe6c376ecbecf346
Pavel Sobolev <contact@paveloom.dev>
2024-05-25 22:32:17 +0300

    nixos/whisparr: initial commit

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1058,6 +1058,7 @@ in {
   wastebin = handleTest ./wastebin.nix {};
   watchdogd = handleTest ./watchdogd.nix {};
   webhook = runTest ./webhook.nix;
+  whisparr = handleTest ./whisparr.nix {};
   wiki-js = handleTest ./wiki-js.nix {};
   wine = handleTest ./wine.nix {};
   wireguard = handleTest ./wireguard {};
