8a08782e4dfc5b8d31063e2eb5d047f030bf9c79
Jan Tojnar <jtojnar@gmail.com>
2024-05-25 17:54:35 +0000

    libmsgraph: 0.2.1 → 0.2.2
    https://gitlab.gnome.org/GNOME/msgraph/-/compare/0.2.1...0.2.2

--- pkgs/by-name/li/libmsgraph/package.nix
+++ pkgs/by-name/li/libmsgraph/package.nix
@@ -7,23 +7,23 @@
 , ninja
 , pkg-config
 , uhttpmock_1_0
+, libxml2
 , glib
 , gnome-online-accounts
 , json-glib
-, librest_1_0
 , libsoup_3
 , gnome
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libmsgraph";
-  version = "0.2.1";
+  version = "0.2.2";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/msgraph/${lib.versions.majorMinor finalAttrs.version}/msgraph-${finalAttrs.version}.tar.xz";
-    hash = "sha256-4OWeqorj4KSOwKbC/tBHCFanCSSOkhK2odA33leS7Ls=";
+    hash = "sha256-EsyVT3ULsTes6FAP6TVdJubgDvbMqrKVDp02DpJ1gzU=";
   };
 
   nativeBuildInputs = [
@@ -36,19 +36,14 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = [
     uhttpmock_1_0
+    libxml2
   ];
 
   propagatedBuildInputs = [
     glib
     gnome-online-accounts
     json-glib
-    librest_1_0
     libsoup_3
-  ];
-
-  mesonFlags = [
-    # https://gitlab.gnome.org/GNOME/msgraph/-/merge_requests/9
-    "-Dc_args=-Wno-error=format-security"
   ];
 
   postFixup = ''
