bbe9f3a1dacdfd33f768f182a7dafd8961aa4fda
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-25 19:13:25 +0000

    python311Packages.herepy: 3.6.2 -> 3.6.3

--- pkgs/development/python-modules/herepy/default.nix
+++ pkgs/development/python-modules/herepy/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "herepy";
-  version = "3.6.2";
+  version = "3.6.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "abdullahselek";
     repo = "HerePy";
     rev = "refs/tags/${version}";
-    hash = "sha256-+DI9wjYVAUKdGEXWhbV9Xp0oOazcB+sS89ugIZuZM2w=";
+    hash = "sha256-ht4EZBfREU7tDNo6tCPyECjm0H+yuhjsfJ60M4ss0jE=";
   };
 
   build-system = [ setuptools ];
