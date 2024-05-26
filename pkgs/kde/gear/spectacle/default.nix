aa9b0ef26a0be224847b6f4fe3d9d1c52ee44147
Coca <coca16622@gmail.com>
2024-05-26 20:16:29 +0300

    spectacle: fix cuda support

--- pkgs/kde/gear/spectacle/default.nix
+++ pkgs/kde/gear/spectacle/default.nix
@@ -1,12 +1,24 @@
 {
+  lib,
+  config,
   mkKdeDerivation,
   qtwayland,
   qtmultimedia,
   opencv,
+  cudaSupport ? config.cudaSupport,
+  cudaPackages,
 }:
 mkKdeDerivation {
   pname = "spectacle";
 
-  extraBuildInputs = [qtwayland qtmultimedia opencv];
+  extraBuildInputs = [
+    qtwayland
+    qtmultimedia
+    opencv
+  ] ++ lib.optionals cudaSupport [
+    cudaPackages.cuda_cudart
+    cudaPackages.cuda_nvcc
+  ];
+
   meta.mainProgram = "spectacle";
 }
