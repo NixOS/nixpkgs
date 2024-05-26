9da429394f81056e5cae74d1f6e1d8d180cdce7a
Artemis Tosini <me@artem.ist>
2024-05-26 04:12:12 +0000

    freebsd.libncurses: Fix typo

--- pkgs/os-specific/bsd/freebsd/pkgs/libncurses.nix
+++ pkgs/os-specific/bsd/freebsd/pkgs/libncurses.nix
@@ -1,12 +1,12 @@
-{ lib, versionInfo, mkDerivation, libncurses-tinfo, ...}:
+{ lib, versionData, mkDerivation, libncurses-tinfo, ...}:
 mkDerivation {
   path = "lib/ncurses/ncurses";
   extraPaths = ["lib/ncurses" "contrib/ncurses" "lib/Makefile.inc"];
   MK_TESTS = "no";
-  preBuild = lib.optionalString (versionInfo.major == 14) ''
+  preBuild = lib.optionalString (versionData.major == 14) ''
     make -C ../tinfo $makeFlags curses.h ncurses_dll.h ncurses_def.h
   '';
-  buildInputs = lib.optionals (versionInfo.major == 14) [libncurses-tinfo];
+  buildInputs = lib.optionals (versionData.major == 14) [libncurses-tinfo];
 
   # some packages depend on libncursesw.so.8
   postInstall = ''
