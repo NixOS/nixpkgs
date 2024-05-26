0d8fe4b47638fcaf77a0c908c834388e6aa8783f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-05-27 00:16:20 +0200

    python311Packages.openrazer: fix indentation

--- pkgs/development/python-modules/openrazer/daemon.nix
+++ pkgs/development/python-modules/openrazer/daemon.nix
@@ -18,46 +18,43 @@
 let
   common = import ./common.nix { inherit lib fetchFromGitHub; };
 in
-buildPythonPackage (
-  common
-  // {
-    pname = "openrazer-daemon";
+buildPythonPackage (common // {
+  pname = "openrazer-daemon";
 
-    outputs = [
-      "out"
-      "man"
-    ];
+  outputs = [
+    "out"
+    "man"
+  ];
 
-    sourceRoot = "${common.src.name}/daemon";
+  sourceRoot = "${common.src.name}/daemon";
 
-    postPatch = ''
-      substituteInPlace openrazer_daemon/daemon.py \
-        --replace-fail "plugdev" "openrazer"
-    '';
+  postPatch = ''
+    substituteInPlace openrazer_daemon/daemon.py \
+      --replace-fail "plugdev" "openrazer"
+  '';
 
-    nativeBuildInputs = [ setuptools ];
+  nativeBuildInputs = [ setuptools ];
 
-    propagatedBuildInputs = [
-      daemonize
-      dbus-python
-      gobject-introspection
-      gtk3
-      pygobject3
-      pyudev
-      setproctitle
-      notify2
-    ];
+  propagatedBuildInputs = [
+    daemonize
+    dbus-python
+    gobject-introspection
+    gtk3
+    pygobject3
+    pyudev
+    setproctitle
+    notify2
+  ];
 
-    postInstall = ''
-      DESTDIR="$out" PREFIX="" make manpages install-resources install-systemd
-    '';
+  postInstall = ''
+    DESTDIR="$out" PREFIX="" make manpages install-resources install-systemd
+  '';
 
-    # no tests run
-    doCheck = false;
+  # no tests run
+  doCheck = false;
 
-    meta = common.meta // {
-      description = "An entirely open source user-space daemon that allows you to manage your Razer peripherals on GNU/Linux";
-      mainProgram = "openrazer-daemon";
-    };
-  }
-)
+  meta = common.meta // {
+    description = "An entirely open source user-space daemon that allows you to manage your Razer peripherals on GNU/Linux";
+    mainProgram = "openrazer-daemon";
+  };
+})
