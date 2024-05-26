5d9de240e36e961d38bd703cc3140be38aac3dc6
Anderson Torres <torres.anderson.85@protonmail.com>
2024-05-25 21:30:40 -0300

    vms-empire: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37452,8 +37452,6 @@ with pkgs;
 
   vitetris = callPackage ../games/vitetris { };
 
-  vms-empire = callPackage ../games/vms-empire { };
-
   voxelands = callPackage ../games/voxelands { };
 
   vvvvvv = callPackage ../by-name/vv/vvvvvv/package.nix {
