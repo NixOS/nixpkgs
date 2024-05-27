1ace0e78fa04433a86079706e1f796ee7b8c9ce1
Erin van der Veen <erin.vanderveen@tweag.io>
2024-05-27 14:34:54 +0200

    nixos/genealogos: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -509,6 +509,7 @@
   ./services/development/bloop.nix
   ./services/development/distccd.nix
   ./services/development/gemstash.nix
+  ./services/development/genealogos.nix
   ./services/development/hoogle.nix
   ./services/development/jupyter/default.nix
   ./services/development/jupyterhub/default.nix
