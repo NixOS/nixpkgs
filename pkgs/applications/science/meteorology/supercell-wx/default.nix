f1d70b1c48f0019f4f4583d87c626de6be469dc8
aware70 <7832566+aware70@users.noreply.github.com>
2024-05-27 18:09:48 -0500

    supercell-wx: run nixfmt-rfc-style

--- pkgs/applications/science/meteorology/supercell-wx/default.nix
+++ pkgs/applications/science/meteorology/supercell-wx/default.nix
@@ -35,7 +35,8 @@
   pkg-config,
   python3,
   wrapQtAppsHook,
-}: let
+}:
+let
   version = "0.4.4";
   src = fetchFromGitHub {
     owner = "dpaulat";
@@ -45,83 +46,83 @@
     fetchSubmodules = true;
   };
 in
-  stdenv.mkDerivation {
-    name = "supercell-wx";
-    inherit version;
-    inherit src;
+stdenv.mkDerivation {
+  name = "supercell-wx";
+  inherit version;
+  inherit src;
 
-    meta = {
-      homepage = "https://supercell-wx.rtfd.io";
-      downloadPage = "https://github.com/dpaulat/supercell-wx/releases";
-      description = ''
-        Supercell Wx is a free, open source application to visualize live and
-        archive NEXRAD Level 2 and Level 3 data, and severe weather alerts.
-        It displays continuously updating weather data on top of a responsive
-        map, providing the capability to monitor weather events using
-        reflectivity, velocity, and other products.
-      '';
-      license = lib.licenses.mit;
-      mainProgram = "supercell-wx";
-      platforms = ["x86_64-linux"];
-      maintainers = with lib.maintainers; [ aware70 ];
-    };
-
-    env.NIX_CFLAGS_COMPILE = "-Wno-error=restrict";
-
-    patches = [
-      ./patches/use-find-package.patch
-      ./patches/add-cstdint.patch
-      ./patches/fix-zoned-time.patch
-      (substituteAll {
-        src = ./patches/skip-git-versioning.patch;
-        rev = src.rev;
-      })
-      ./patches/fix-cmake-install.patch
-      ./patches/fix-audio-codec-setup.patch
-    ];
-
-    postPatch = ''
-      substituteInPlace external/maplibre-native-qt/src/core/CMakeLists.txt \
-        --replace-fail "CMAKE_SOURCE_DIR" "PROJECT_SOURCE_DIR"
+  meta = {
+    homepage = "https://supercell-wx.rtfd.io";
+    downloadPage = "https://github.com/dpaulat/supercell-wx/releases";
+    description = ''
+      Supercell Wx is a free, open source application to visualize live and
+      archive NEXRAD Level 2 and Level 3 data, and severe weather alerts.
+      It displays continuously updating weather data on top of a responsive
+      map, providing the capability to monitor weather events using
+      reflectivity, velocity, and other products.
     '';
+    license = lib.licenses.mit;
+    mainProgram = "supercell-wx";
+    platforms = [ "x86_64-linux" ];
+    maintainers = with lib.maintainers; [ aware70 ];
+  };
 
-    nativeBuildInputs = [
-      cmake
-      ninja
-      wrapQtAppsHook
-      pkg-config
-      git
-    ];
+  env.NIX_CFLAGS_COMPILE = "-Wno-error=restrict";
 
-    buildInputs = [
-      zlib
-      openssl
-      curl
-      qtbase
-      qttools
-      qtmultimedia
-      qtpositioning
-      qtimageformats
-      aws-sdk-cpp
-      howard-hinnant-date
-      boost185
-      tbb_2021_11
-      glew
-      geos
-      spdlog
-      stb
-      libcpr
-      libpng
-      libSM
-      re2
-      openssl
-      geographiclib
-      gtest
-      glm
-      bzip2
-      (python3.withPackages (ps: [
-        ps.geopandas
-        ps.gitpython
-      ]))
-    ];
-  }
+  patches = [
+    ./patches/use-find-package.patch
+    ./patches/add-cstdint.patch
+    ./patches/fix-zoned-time.patch
+    (substituteAll {
+      src = ./patches/skip-git-versioning.patch;
+      rev = src.rev;
+    })
+    ./patches/fix-cmake-install.patch
+    ./patches/fix-audio-codec-setup.patch
+  ];
+
+  postPatch = ''
+    substituteInPlace external/maplibre-native-qt/src/core/CMakeLists.txt \
+      --replace-fail "CMAKE_SOURCE_DIR" "PROJECT_SOURCE_DIR"
+  '';
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    wrapQtAppsHook
+    pkg-config
+    git
+  ];
+
+  buildInputs = [
+    zlib
+    openssl
+    curl
+    qtbase
+    qttools
+    qtmultimedia
+    qtpositioning
+    qtimageformats
+    aws-sdk-cpp
+    howard-hinnant-date
+    boost185
+    tbb_2021_11
+    glew
+    geos
+    spdlog
+    stb
+    libcpr
+    libpng
+    libSM
+    re2
+    openssl
+    geographiclib
+    gtest
+    glm
+    bzip2
+    (python3.withPackages (ps: [
+      ps.geopandas
+      ps.gitpython
+    ]))
+  ];
+}
