89b216d36fa3b4d6b33ffbf20adcc62c2cf07e54
Renzo Carbonara <gnuk0001@gmail.com>
2024-05-27 11:39:33 +0300

    pgx_ulid: 0.1.4 > 0.1.5

--- pkgs/servers/sql/postgresql/ext/pgx_ulid.nix
+++ pkgs/servers/sql/postgresql/ext/pgx_ulid.nix
@@ -4,16 +4,15 @@ buildPgrxExtension rec {
   inherit postgresql;
 
   pname = "pgx_ulid";
-  version = "0.1.4";
+  version = "0.1.5";
 
   src = fetchFromGitHub {
     owner = "pksunkara";
     repo = "pgx_ulid";
-    # rev: This is one commit past v0.1.4. It adds the Cargo.lock file.
-    rev = "566bc1b21fa88276d7d7c27dd3cd4b74458dbc10";
-    hash = "sha256-o6BnoaXIg4zbg86fTTcgydrl1JeykZxwDcvWz8+h31g=";
+    rev = "29a037e7e2dd8b18e4bb5e5ec4f9a6fe270e84bc";
+    hash = "sha256-zql7wtZQ+GDEpM0kld7vHCbWNHSpPKjYZgVWhx1GtvU=";
   };
-  cargoHash = "sha256-nUI+QXhNgcmqRU/dY3Sk8bwdndJE8TT00LEFoauZ97s=";
+  cargoHash = "sha256-kMo5wnQA8FakTEYebbpoKxMGi1SrFJQd45ZYHGUqVrY=";
 
   passthru.updateScript = gitUpdater { rev-prefix = "v"; };
 
