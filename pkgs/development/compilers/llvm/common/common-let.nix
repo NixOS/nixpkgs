65e2d797a234feb71c787ad3ef0cafc52299bb37
John Ericson <John.Ericson@Obsidian.Systems>
2024-05-27 13:49:18 -0400

    llvmPackages: Format common files
    This will help with further refactors

--- pkgs/development/compilers/llvm/common/common-let.nix
+++ pkgs/development/compilers/llvm/common/common-let.nix
@@ -1,9 +1,10 @@
-{ lib
-, fetchFromGitHub ? null
-, release_version ? null
-, gitRelease ? null
-, officialRelease ? null
-, monorepoSrc' ? null
+{
+  lib,
+  fetchFromGitHub ? null,
+  release_version ? null,
+  gitRelease ? null,
+  officialRelease ? null,
+  monorepoSrc' ? null,
 }:
 
 rec {
@@ -13,31 +14,31 @@ rec {
 
     # See llvm/cmake/config-ix.cmake.
     platforms =
-      lib.platforms.aarch64 ++
-      lib.platforms.arm ++
-      lib.platforms.mips ++
-      lib.platforms.power ++
-      lib.platforms.s390x ++
-      lib.platforms.wasi ++
-      lib.platforms.x86 ++
-      lib.optionals (lib.versionAtLeast release_version "7") lib.platforms.riscv ++
-      lib.optionals (lib.versionAtLeast release_version "14") lib.platforms.m68k;
+      lib.platforms.aarch64
+      ++ lib.platforms.arm
+      ++ lib.platforms.mips
+      ++ lib.platforms.power
+      ++ lib.platforms.s390x
+      ++ lib.platforms.wasi
+      ++ lib.platforms.x86
+      ++ lib.optionals (lib.versionAtLeast release_version "7") lib.platforms.riscv
+      ++ lib.optionals (lib.versionAtLeast release_version "14") lib.platforms.m68k;
   };
 
   releaseInfo =
-    if gitRelease != null then rec {
-      original = gitRelease;
-      release_version = original.version;
-      version = gitRelease.rev-version;
-    } else rec {
-      original = officialRelease;
-      release_version = original.version;
-      version =
-        if original ? candidate then
-          "${release_version}-${original.candidate}"
-        else
-          release_version;
-    };
+    if gitRelease != null then
+      rec {
+        original = gitRelease;
+        release_version = original.version;
+        version = gitRelease.rev-version;
+      }
+    else
+      rec {
+        original = officialRelease;
+        release_version = original.version;
+        version =
+          if original ? candidate then "${release_version}-${original.candidate}" else release_version;
+      };
 
   monorepoSrc =
     if monorepoSrc' != null then
@@ -45,16 +46,11 @@ rec {
     else
       let
         sha256 = releaseInfo.original.sha256;
-        rev =
-          if gitRelease != null then
-            gitRelease.rev
-          else
-            "llvmorg-${releaseInfo.version}";
+        rev = if gitRelease != null then gitRelease.rev else "llvmorg-${releaseInfo.version}";
       in
       fetchFromGitHub {
         owner = "llvm";
         repo = "llvm-project";
         inherit rev sha256;
       };
-
 }
