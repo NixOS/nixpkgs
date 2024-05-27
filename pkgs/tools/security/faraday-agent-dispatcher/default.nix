2ced1d73b211c978188f4fcc48c480f2e6ff2aa0
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-27 04:15:10 +0000

    faraday-agent-dispatcher: 3.3.0 -> 3.4.1

--- pkgs/tools/security/faraday-agent-dispatcher/default.nix
+++ pkgs/tools/security/faraday-agent-dispatcher/default.nix
@@ -5,14 +5,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "faraday-agent-dispatcher";
-  version = "3.3.0";
+  version = "3.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "infobyte";
     repo = "faraday_agent_dispatcher";
     rev = "refs/tags/${version}";
-    hash = "sha256-rpi8S6pmei8MaACnfmOKFK+nNpljoMglu2uk75zvs9I=";
+    hash = "sha256-b62WO1+5EWzsTCzeZPX9T+ho8Sig46lH/9dPmGGhPWA=";
   };
 
   postPatch = ''
