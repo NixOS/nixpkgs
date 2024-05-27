9180836151e927eda47ba422283af8a59dec1db0
Francesco Gazzetta <fgaz@fgaz.me>
2024-05-27 19:44:49 +0200

    domination: 1.2.9 -> 1.3.0

--- pkgs/games/domination/default.nix
+++ pkgs/games/domination/default.nix
@@ -26,7 +26,7 @@ let
 
 in stdenv.mkDerivation {
   pname = "domination";
-  version = "1.2.9";
+  version = "1.3.0";
 
   # The .zip releases do not contain the build.xml file
   src = fetchsvn {
@@ -34,8 +34,8 @@ in stdenv.mkDerivation {
     # There are no tags in the repository.
     # Look for commits like "new version x.y.z info on website"
     # or "website update for x.y.z".
-    rev = "2470";
-    hash = "sha256-ghq7EGg++mTOzA3ASzXhk97fzy5/n9vyaRzxp12X3/4=";
+    rev = "2530";
+    hash = "sha256-LR4s8JmRs61IhQB2ASSM6MGt+ev+ENFPa5iIeE1tOyI=";
   };
 
   nativeBuildInputs = [
