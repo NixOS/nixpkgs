a3fc3f0b59c43c5a556a367c1755bc77da346739
Matthias Queitsch <matthias.queitsch@mailbox.org>
2024-05-28 15:34:02 +0200

    maintainers: add matthiasq

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12190,6 +12190,12 @@
     githubId = 427866;
     name = "Matthias Beyer";
   };
+  matthiasq = {
+    email = "matthias.queitsch@mailbox.org";
+    github = "matthias-Q";
+    githubId = 35303817;
+    name = "Matthias Queitsch";
+  };
   MatthieuBarthel = {
     email = "matthieu@imatt.ch";
     name = "Matthieu Barthel";
