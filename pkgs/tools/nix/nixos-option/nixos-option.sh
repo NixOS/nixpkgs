1bd0fb37624cc63976e8421a7d2102807f15b366
Zhong Jianxin <azuwis@gmail.com>
2024-05-28 20:47:21 +0800

    nixos-option: Support flake

--- pkgs/tools/nix/nixos-option/nixos-option.sh
+++ pkgs/tools/nix/nixos-option/nixos-option.sh
@@ -4,6 +4,7 @@ set -eou pipefail
 recursive=false
 positional_args=()
 nix_args=()
+flake=""
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -18,6 +19,11 @@ while [[ $# -gt 0 ]]; do
 
     -I)
       nix_args+=("$1" "$2")
+      shift 2
+      ;;
+
+    -F|--flake)
+      flake=$2
       shift 2
       ;;
 
@@ -37,6 +43,26 @@ while [[ $# -gt 0 ]]; do
       ;;
   esac
 done
+
+if [ -z "$flake" ] && [ -e /etc/nixos/flake.nix ]; then
+  flake="$(dirname "$(readlink -f /etc/nixos/flake.nix)")"
+fi
+if [ -n "$flake" ]; then
+  if [[ $flake =~ ^(.*)\#([^\#\"]*)$ ]]; then
+    flake="${BASH_REMATCH[1]}"
+    flakeAttr="${BASH_REMATCH[2]}"
+  fi
+  if [ -z "${flakeAttr:-}" ]; then
+    hostname=$(hostname)
+    if [ -z "${hostname:-}" ]; then
+      hostname=default
+    fi
+    flakeAttr="nixosConfigurations.\"$hostname\""
+  else
+    flakeAttr="nixosConfigurations.\"$flakeAttr\""
+  fi
+  nix_args+=(--arg nixos "(builtins.getFlake \"$flake\").$flakeAttr")
+fi
 
 case ${#positional_args[@]} in
   0) path= ;;
