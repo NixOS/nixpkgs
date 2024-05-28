490b77108a02a769fc9bdb7531bc500a0fcc8e95
Audrey Dutcher <audrey@rhelmot.io>
2024-05-28 00:38:32 -0700

    llvmPackages.libcxx: Remove cxxabi parameter and option to use libcxxrt
    This option was added for FreeBSD and is no longer necessary since
    FreeBSD is now using libcxxabi.
    
    Co-authored-by: Rahul Butani <rrbutani@users.noreply.github.com>

--- pkgs/development/compilers/llvm/common/libcxx/default.nix
+++ pkgs/development/compilers/llvm/common/libcxx/default.nix
@@ -8,31 +8,17 @@
 , runCommand
 , substitute
 , cmake
-, lndir
 , ninja
 , python3
 , fixDarwinDylibNames
 , version
-, cxxabi ? null
-, libcxxrt
 , libunwind
 , enableShared ? !stdenv.hostPlatform.isStatic
 }:
 
-# note: our setup using libcxxabi instead of libcxxrt on FreeBSD diverges from
-# normal FreeBSD. This may cause issues with binary patching down the line.
-# If this becomes an issue, try adding as symlink libcxxrt.so -> libc++abi.so
-
-# external cxxabi is not supported on Darwin as the build will not link libcxx
-# properly and not re-export the cxxabi symbols into libcxx
-# https://github.com/NixOS/nixpkgs/issues/166205
-# https://github.com/NixOS/nixpkgs/issues/269548
-assert cxxabi == null || !stdenv.hostPlatform.isDarwin;
 let
   basename = "libcxx";
   pname = basename;
-  cxxabiName = "lib${if cxxabi == null then "cxxabi" else cxxabi.libName}";
-  runtimes = [ "libcxx" ] ++ lib.optional (cxxabi == null) "libcxxabi";
 
   # Note: useLLVM is likely false for Darwin but true under pkgsLLVM
   useLLVM = stdenv.hostPlatform.useLLVM or false;
@@ -40,19 +26,15 @@ let
   src' = if monorepoSrc != null then
     runCommand "${pname}-src-${version}" {} (''
       mkdir -p "$out/llvm"
-    '' + (lib.optionalString (lib.versionAtLeast release_version "14") ''
-      cp -r ${monorepoSrc}/cmake "$out"
-    '') + ''
       cp -r ${monorepoSrc}/libcxx "$out"
       cp -r ${monorepoSrc}/llvm/cmake "$out/llvm"
       cp -r ${monorepoSrc}/llvm/utils "$out/llvm"
-    '' + (lib.optionalString (lib.versionAtLeast release_version "14") ''
-      cp -r ${monorepoSrc}/third-party "$out"
-    '') + ''
       cp -r ${monorepoSrc}/runtimes "$out"
-    '' + (lib.optionalString (cxxabi == null) ''
       cp -r ${monorepoSrc}/libcxxabi "$out"
-    '')) else src;
+    '' + lib.optionalString (lib.versionAtLeast release_version "14") ''
+      cp -r ${monorepoSrc}/third-party "$out"
+      cp -r ${monorepoSrc}/cmake "$out"
+    '') else src;
 
   cxxabiCMakeFlags = lib.optionals (lib.versionAtLeast release_version "18") [
     "-DLIBCXXABI_USE_LLVM_UNWINDER=OFF"
@@ -70,15 +52,13 @@ let
   ];
 
   cxxCMakeFlags = [
-    "-DLIBCXX_CXX_ABI=${cxxabiName}"
-  ] ++ lib.optionals (cxxabi == null && lib.versionAtLeast release_version "16") [
+    "-DLIBCXX_CXX_ABI=libcxxabi"
+  ] ++ lib.optionals (lib.versionAtLeast release_version "16") [
     # Note: llvm < 16 doesn't support this flag (or it's broken); handled in postInstall instead.
     # Include libc++abi symbols within libc++.a for static linking libc++;
     # dynamic linking includes them through libc++.so being a linker script
     # which includes both shared objects.
     "-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON"
-  ] ++ lib.optionals (cxxabi != null) [
-    "-DLIBCXX_CXX_ABI_INCLUDE_PATHS=${lib.getDev cxxabi}/include"
   ] ++ lib.optionals (stdenv.hostPlatform.isMusl || stdenv.hostPlatform.isWasi) [
     "-DLIBCXX_HAS_MUSL_LIBC=1"
   ] ++ lib.optionals (lib.versionAtLeast release_version "18" && !useLLVM && stdenv.hostPlatform.libc == "glibc" && !stdenv.hostPlatform.isStatic) [
@@ -96,7 +76,7 @@ let
   ];
 
   cmakeFlags = [
-    "-DLLVM_ENABLE_RUNTIMES=${lib.concatStringsSep ";" runtimes}"
+    "-DLLVM_ENABLE_RUNTIMES=libcxx;libcxxabi"
   ] ++ lib.optionals (useLLVM && !stdenv.hostPlatform.isWasm) [
     # libcxxabi's CMake looks as though it treats -nostdlib++ as implying -nostdlib,
     # but that does not appear to be the case for example when building
@@ -108,7 +88,7 @@ let
     "-DCMAKE_CXX_COMPILER_WORKS=ON"
     "-DUNIX=ON" # Required otherwise libc++ fails to detect the correct linker
   ] ++ cxxCMakeFlags
-    ++ lib.optionals (cxxabi == null) cxxabiCMakeFlags;
+    ++ cxxabiCMakeFlags;
 
 in
 
@@ -124,33 +104,22 @@ stdenv.mkDerivation (rec {
   '';
 
   nativeBuildInputs = [ cmake ninja python3 ]
-    ++ lib.optional stdenv.isDarwin fixDarwinDylibNames
-    ++ lib.optional (cxxabi != null) lndir;
+    ++ lib.optional stdenv.isDarwin fixDarwinDylibNames;
 
-  buildInputs = [ cxxabi ]
-    ++ lib.optionals (useLLVM && !stdenv.hostPlatform.isWasm) [ libunwind ];
+  buildInputs = lib.optionals (useLLVM && !stdenv.hostPlatform.isWasm) [ libunwind ];
 
-  # libc++.so is a linker script which expands to multiple libraries,
-  # libc++.so.1 and libc++abi.so or the external cxxabi. ld-wrapper doesn't
-  # support linker scripts so the external cxxabi needs to be symlinked in
-  postInstall = lib.optionalString (cxxabi != null) ''
-    lndir ${lib.getDev cxxabi}/include $dev/include/c++/v1
-    lndir ${lib.getLib cxxabi}/lib $out/lib
-    libcxxabi=$out/lib/lib${cxxabi.libName}.a
-  ''
-  # LIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON doesn't work for LLVM < 16 or
-  # external cxxabi libraries so merge libc++abi.a into libc++.a ourselves.
-
+  # LIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON doesn't work for LLVM < 16 so
+  # merge libc++abi.a into libc++.a ourselves.
+  #
   # GNU binutils emits objects in LIFO order in MRI scripts so after the merge
   # the objects are in reversed order so a second MRI script is required so the
   # objects in the archive are listed in proper order (libc++.a, libc++abi.a)
-  + lib.optionalString (cxxabi != null || lib.versionOlder release_version "16") ''
-    libcxxabi=''${libcxxabi-$out/lib/libc++abi.a}
-    if [[ -f $out/lib/libc++.a && -e $libcxxabi ]]; then
+  postInstall = lib.optionalString (lib.versionOlder release_version "16") ''
+    if [[ -f $out/lib/libc++.a && -f $out/lib/libc++abi.a ]]; then
       $AR -M <<MRI
         create $out/lib/libc++.a
         addlib $out/lib/libc++.a
-        addlib $libcxxabi
+        addlib $out/lib/libc++abi.a
         save
         end
     MRI
