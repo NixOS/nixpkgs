cf27cbd0de4cab1158f52e7909393f34bef53aa1
OulipianSummer <benbow.drew@gmail.com>
2024-05-28 10:22:31 -0400

    everforest-gtk-theme: revise package metadata according to feedback

--- pkgs/data/themes/everforest-gtk-theme/default.nix
+++ pkgs/data/themes/everforest-gtk-theme/default.nix
@@ -1,25 +1,31 @@
-{ lib, stdenv, fetchurl}:
+{ lib, stdenvNoCC, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
-  pname = "Everforest-GTK-Theme";
-  version = "1.0";
+stdenvNoCC.mkDerivation rec {
+  pname = "everforest-gtk-theme";
+  version = "0-unstable-2023-03-20";
 
-  src = fetchurl {
-    url = "https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme/archive/refs/heads/master.tar.gz";
-    sha256 = "2gmSMW/yUdinvZ/Jaw6GyfEL/CBqYKtw0QrYWy9KrL0=";
+  src = fetchFromGitHub {
+    owner = "Fausto-Korpsvart";
+    repo = "Everforest-GTK-Theme";
+    rev = "8481714cf9ed5148694f1916ceba8fe21e14937b";
+    sha256 = "NO12ku8wnW/qMHKxi5TL/dqBxH0+cZbe+fU0iicb9JU=";
   };
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/share/themes
     rm -rf README.md LICENSE icons extra
     cp -r themes/* $out/share/themes/
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "Everforest theme for GTK based desktop environments";
     homepage = "https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme";
-    license = licenses.gpl3Only;
-    platforms = platforms.unix;
+    license = licenses.gpl3Plus;
+    platforms = platforms.all;
     maintainers = [ maintainers.OulipianSummer ];
   };
 }
