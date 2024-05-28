1b8f8b1b8312d36fbce757979b43f5b115274de4
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-28 11:59:17 +0000

    simplex-chat-desktop: 5.7.0 -> 5.7.4

--- pkgs/by-name/si/simplex-chat-desktop/package.nix
+++ pkgs/by-name/si/simplex-chat-desktop/package.nix
@@ -6,11 +6,11 @@
 
 let
   pname = "simplex-chat-desktop";
-  version = "5.7.0";
+  version = "5.7.4";
 
   src = fetchurl {
     url = "https://github.com/simplex-chat/simplex-chat/releases/download/v${version}/simplex-desktop-x86_64.AppImage";
-    hash = "sha256-T8ojnay/FCa9Q4PObqlfy2MC4pKTF73taNW8elNDjIg=";
+    hash = "sha256-byns0F3i1YiYekO5KoAek5LXCU8bvPK/tzl+xExQM4g=";
   };
 
   appimageContents = appimageTools.extract {
