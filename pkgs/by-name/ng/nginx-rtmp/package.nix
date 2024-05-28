f06fd4264a7ca25dbebb3e88482ef35b928ceb20
Emre YILMAZ <z@emre.xyz>
2024-05-28 09:03:21 -0400

    nginx-rtmp: file name changed as package.nix, nixpkgs import removed
    Signed-off-by: Emre YILMAZ <z@emre.xyz>

--- pkgs/by-name/ng/nginx-rtmp/package.nix
+++ pkgs/by-name/ng/nginx-rtmp/package.nix
@@ -1,28 +1,35 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ lib,
+fetchFromGitHub ,
+fetchurl,
+openssl,
+pcre,
+zlib,
+libxcrypt
+}:
 
 pkgs.stdenv.mkDerivation rec {
   pname = "nginx-rtmp";
   version = "1.21.4";  # Set the desired Nginx version
 
-  src = pkgs.fetchurl {
+  src = fetchurl {
     url = "http://nginx.org/download/nginx-${version}.tar.gz";
     sha256 = "1ziv3xargxhxycd5hp6r3r5mww54nvvydiywcpsamg3i9r3jzxyi";
   };
 
-  rtmpModule = pkgs.fetchFromGitHub {
+  rtmpModule = fetchFromGitHub {
     owner = "arut";
     repo = "nginx-rtmp-module";
     rev = "refs/tags/v1.2.2";
     sha256 = "sha256-0Gz8Hut7hxP3Tb9/ZTbqMHdts1Cshy3YhH4EMblehXg=";  # Verify and update the sha256
   };
 
-  buildInputs = [ pkgs.openssl pkgs.pcre pkgs.zlib pkgs.libxcrypt ];
+  buildInputs = [ openssl pcre zlib libxcrypt ];
 
   configureFlags = [
     "--with-http_ssl_module "
     "--add-module=${rtmpModule}"
   ];
-  cf = pkgs.lib.concatStrings configureFlags;
+  cf = lib.concatStrings configureFlags;
 
   buildPhase = ''
     ./configure ${cf}
@@ -33,7 +40,7 @@ pkgs.stdenv.mkDerivation rec {
     make install DESTDIR=$out
   '';
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "NGINX with nginx-rtmp-module";
     homepage = "https://github.com/arut/nginx-rtmp-module";
     license = licenses.bsd2;
