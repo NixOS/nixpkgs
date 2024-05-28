41f367aaecb4954badfea06c72ef0c69539a155c
Arnout Engelen <arnout@bzzt.net>
2024-05-28 16:40:17 +0200

    python3Packages/rpi-gpio2: remove package
    It's been broken for a while and doesn't appear to be used anywhere.
    Components that use rpi-gpio2 should probably be rewritten to use
    libgpiod directly.
    
    Fixes #263759

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13517,8 +13517,6 @@ self: super: with self; {
 
   rpi-gpio = callPackage ../development/python-modules/rpi-gpio { };
 
-  rpi-gpio2 = callPackage ../development/python-modules/rpi-gpio2 { };
-
   rplcd = callPackage ../development/python-modules/rplcd { };
 
   rply = callPackage ../development/python-modules/rply { };
