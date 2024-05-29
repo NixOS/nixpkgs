cd2d05a39917b939270261b043303492eb3d8a6b
Anthony Roussel <anthony@roussel.dev>
2024-05-29 11:09:09 +0200

    bundler: 2.5.10 -> 2.5.11

--- pkgs/development/ruby-modules/bundler/default.nix
+++ pkgs/development/ruby-modules/bundler/default.nix
@@ -4,8 +4,8 @@ buildRubyGem rec {
   inherit ruby;
   name = "${gemName}-${version}";
   gemName = "bundler";
-  version = "2.5.10";
-  source.sha256 = "sha256-eta+7Y+kmnmlkgKfdz8kkrn075FtinLO5OLRBOW2+q0=";
+  version = "2.5.11";
+  source.sha256 = "sha256-3XhL/lODSzmlbmQtvG4eyhmi5kVOTVOZTLcpgAWsTC4=";
   dontPatchShebangs = true;
 
   postFixup = ''
