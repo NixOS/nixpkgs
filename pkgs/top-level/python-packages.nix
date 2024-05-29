8aa99497c85ca91d524782f7af9ec6e4fc038555
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-29 11:03:36 +0200

    python312Packages.swh-auth: init at 0.10.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17349,6 +17349,8 @@ self: super: with self; {
 
   swagger-ui-bundle = callPackage ../development/python-modules/swagger-ui-bundle { };
 
+  swh-auth = callPackage ../development/python-modules/swh-auth { };
+
   swh-core = callPackage ../development/python-modules/swh-core { };
 
   swh-model = callPackage ../development/python-modules/swh-model { };
