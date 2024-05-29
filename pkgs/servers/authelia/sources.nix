dd065bd66b03f391a3ce29afc28498b0a7071832
Herwig Hochleitner <herwig@bendlas.net>
2024-05-29 20:28:56 +0200

    authelia: 4.37.5 -> 4.38.8

--- pkgs/servers/authelia/sources.nix
+++ pkgs/servers/authelia/sources.nix
@@ -1,14 +1,14 @@
 { fetchFromGitHub }:
 rec {
   pname = "authelia";
-  version = "4.37.5";
+  version = "4.38.8";
 
   src = fetchFromGitHub {
     owner = "authelia";
     repo = "authelia";
     rev = "v${version}";
-    hash = "sha256-xsdBnyPHFIimhp2rcudWqvVR36WN4vBXbxRmvgqMcDw=";
+    hash = "sha256-wuGA3nxzMhpaJKPQeMgVv27zmLyUL5o3MVY+BM6SbAI=";
   };
-  vendorHash = "sha256-mzGE/T/2TT4+7uc2axTqG3aeLMnt1r9Ya7Zj2jIkw/w=";
-  npmDepsHash = "sha256-MGs6UAxT5QZd8S3AO75mxuCb6U0UdRkGEjenOVj+Oqs=";
+  vendorHash = "sha256-k+VzAxV9ctvOMxAM6j9qFNOMERxm5Rgcni18dhh3Rfs=";
+  npmDepsHash = "sha256-9ffl/VP3ztiZzXer86LKhV54sr8b53yfi8FmP2ioqRI=";
 }
