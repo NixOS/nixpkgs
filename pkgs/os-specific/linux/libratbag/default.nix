6b0333d4453373b03a48e575ab9bbbf0ce2e85a8
Jörg Thalheim <joerg@thalheim.io>
2024-05-29 16:52:11 +0200

    libratbag: fix ratbagctl dependencies

--- pkgs/os-specific/linux/libratbag/default.nix
+++ pkgs/os-specific/linux/libratbag/default.nix
@@ -1,6 +1,6 @@
 { lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config
 , glib, systemd, udev, libevdev, gitMinimal, check, valgrind, swig, python3
-, json-glib, libunistring }:
+, json-glib, libunistring, wrapGAppsNoGuiHook, gobject-introspection }:
 
 stdenv.mkDerivation rec {
   pname = "libratbag";
@@ -15,12 +15,19 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     meson ninja pkg-config gitMinimal swig check valgrind
+    wrapGAppsNoGuiHook
+    gobject-introspection
   ];
 
   buildInputs = [
     glib systemd udev libevdev json-glib libunistring
-    (python3.withPackages (ps: with ps; [ evdev pygobject3 ]))
   ];
+
+  preFixup = ''
+    gappsWrapperArgs+=(
+      --prefix PYTHONPATH : "${python3.pkgs.makePythonPath [ python3.pkgs.evdev python3.pkgs.pygobject3 ]}"
+    )
+  '';
 
   mesonFlags = [
     "-Dsystemd-unit-dir=./lib/systemd/system/"
