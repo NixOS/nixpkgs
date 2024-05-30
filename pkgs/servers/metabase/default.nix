1b84dafe228e7c2afddc7624573ff743e19e5366
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-30 08:54:45 +0000

    metabase: 0.49.11 -> 0.49.12

--- pkgs/servers/metabase/default.nix
+++ pkgs/servers/metabase/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "metabase";
-  version = "0.49.11";
+  version = "0.49.12";
 
   src = fetchurl {
     url = "https://downloads.metabase.com/v${version}/metabase.jar";
-    hash = "sha256-sLQdIjj2bB4Kebwn2fi6mwz2zym/Sv5l42dWAz5DrAg=";
+    hash = "sha256-+THQ4i7wMO/76MNjdvDoqstFcXknZ5/F82TNXn07gnQ=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
