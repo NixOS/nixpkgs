2ae85d37e25c52ea2fe94b2ec21ca06a38b0929d
R. Ryantm <ryantm-bot@ryantm.com>
2024-05-30 13:23:29 +0000

    redpanda-client: 24.1.3 -> 24.1.4

--- pkgs/servers/redpanda/default.nix
+++ pkgs/servers/redpanda/default.nix
@@ -7,12 +7,12 @@
 , stdenv
 }:
 let
-  version = "24.1.3";
+  version = "24.1.4";
   src = fetchFromGitHub {
     owner = "redpanda-data";
     repo = "redpanda";
     rev = "v${version}";
-    sha256 = "sha256-LuaUm8FToGJ4//tDFvdHbTCHzhul4ympSznbp1wrLM8=";
+    sha256 = "sha256-8N4Ig1IorKYe2ehKCeRNxVWmxLajaaDNvEgH/pSSVJE=";
   };
   server = callPackage ./server.nix { inherit src version; };
 in
