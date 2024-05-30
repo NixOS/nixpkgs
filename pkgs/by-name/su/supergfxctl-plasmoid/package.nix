a590fd9cfbad0cc5f6fcb1f2edc2e15b15fcdd25
JohnyLPM <johnyparreirinhamarques@gmail.com>
2024-05-30 23:04:30 +0100

    shave off unnecessary portions and implemented better practices

--- pkgs/by-name/su/supergfxctl-plasmoid/package.nix
+++ pkgs/by-name/su/supergfxctl-plasmoid/package.nix
@@ -2,8 +2,6 @@
 , stdenv
 , fetchFromGitLab
 , cmake
-, gcc
-, hicolor-icon-theme
 , kdePackages
 }:
 
@@ -13,43 +11,28 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitLab {
     owner = "jhyub";
-    repo = pname;
-    rev = "v2.0.0";
-    sha256 = "1c9s3mab6p8bbanc1b5ypb5rjl5n9ma32814ra1amdpxa1n6cwwv";
+    repo = "supergfxctl-plasmoid";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-m3NmbFD9tqqCyiQgMVRNtlCZy7q+rMCsWgtds1QdOrE=";
   };
 
   nativeBuildInputs = [
     cmake
-    kdePackages.extra-cmake-modules
-    gcc
   ];
 
   buildInputs = [
-    hicolor-icon-theme
     kdePackages.wrapQtAppsHook
-    kdePackages.kcoreaddons
-    kdePackages.kconfig
-    kdePackages.ki18n
-    kdePackages.kirigami
-    kdePackages.ksvg
     kdePackages.libplasma
-    kdePackages.full
-    kdePackages.frameworkintegration
   ];
 
-  buildPhase = ''
-    cmake -DBUILD_WITH_QT6=ON ..
-    make
-  '';
-
-  meta = with lib; {
+  meta = {
     description = "KDE Plasma plasmoid for supergfxctl";
     longDescription = ''
       KDE Plasma plasmoid for supergfxctl
       Built as a C++/QML Plasmoid
     '';
-    license = licenses.mpl20;
+    license = lib.licenses.mpl20;
     homepage = "https://gitlab.com/Jhyub/supergfxctl-plasmoid";
-    maintainers = with maintainers; [ johnylpm ];
+    maintainers = with lib.maintainers; [ johnylpm ];
   };
 }
