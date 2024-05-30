61f781fb43b01971af7ad3c7f7098823de5a1e76
euxane <euxane.trangirard@pacien.net>
2024-05-30 16:20:48 +0200

    mercurial: 6.6.3 -> 6.7.3
    Changelog: https://wiki.mercurial-scm.org/Release6.7

--- pkgs/applications/version-management/mercurial/default.nix
+++ pkgs/applications/version-management/mercurial/default.nix
@@ -23,11 +23,11 @@ let
 
   self = python3Packages.buildPythonApplication rec {
     pname = "mercurial${lib.optionalString fullBuild "-full"}";
-    version = "6.6.3";
+    version = "6.7.3";
 
     src = fetchurl {
       url = "https://mercurial-scm.org/release/mercurial-${version}.tar.gz";
-      hash = "sha256-911qSnWCOht9cTpJZ+yi9Zb0ZuWPxrwG1yZCky/X4wc=";
+      hash = "sha256-ABlpROqSc4gJMX3HqO18shKHygoAqFJG5mFwlV3NkDE=";
     };
 
     format = "other";
@@ -37,7 +37,7 @@ let
     cargoDeps = if rustSupport then rustPlatform.fetchCargoTarball {
       inherit src;
       name = "mercurial-${version}";
-      sha256 = "sha256-G5tzwoIGOgpVI35rYXDeelnBgTbAiq7BDcXCHQzqSrs=";
+      sha256 = "sha256-gVBjUePod7iIY2g1fO+V361i7iEcSdtK8iSjRLczvjE=";
       sourceRoot = "mercurial-${version}/rust";
     } else null;
     cargoRoot = if rustSupport then "rust" else null;
