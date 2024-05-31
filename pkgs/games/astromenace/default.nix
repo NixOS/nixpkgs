0ea76875846d3d90dcaa611bfaa8dc95c4a7bb9d
Francesco Gazzetta <fgaz@fgaz.me>
2024-05-31 19:18:06 +0200

    astromenace: add fgaz to maintainers

--- pkgs/games/astromenace/default.nix
+++ pkgs/games/astromenace/default.nix
@@ -72,5 +72,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
     mainProgram = "astromenace";
+    maintainers = with maintainers; [ fgaz ];
   };
 }
