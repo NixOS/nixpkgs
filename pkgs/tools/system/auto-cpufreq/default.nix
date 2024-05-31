34b9d656bdf1fd0a8edf9b08ba2313f3ad84ee7d
isabel <isabel@isabelroses.com>
2024-05-31 19:47:49 +0100

    auto-cpufreq: 1.9.9 -> 2.3.0

--- pkgs/tools/system/auto-cpufreq/default.nix
+++ pkgs/tools/system/auto-cpufreq/default.nix
@@ -1,17 +1,43 @@
-{ lib, python3Packages, fetchFromGitHub, substituteAll }:
-
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+  substituteAll,
+  gobject-introspection,
+  wrapGAppsHook3,
+  gtk3,
+}:
 python3Packages.buildPythonPackage rec {
   pname = "auto-cpufreq";
-  version = "1.9.9";
+  version = "2.3.0";
+  format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "AdnanHodzic";
-    repo = pname;
+    repo = "auto-cpufreq";
     rev = "v${version}";
-    sha256 = "sha256-D/5pwE2V+yXj92ECOUcl/dajMDbvVdz9YNJrl2Pzvts=";
+    hash = "sha256-Bet/WOVveLIA+0Mvly4AsielR+r/AJXIgHdWrtc7i/U=";
   };
 
-  propagatedBuildInputs = with python3Packages; [ setuptools-git-versioning click distro psutil ];
+  nativeBuildInputs = [
+    gobject-introspection
+    wrapGAppsHook3
+  ];
+
+  buildInputs = [
+    gtk3
+    python3Packages.poetry-core
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    click
+    distro
+    psutil
+    pygobject3
+    poetry-dynamic-versioning
+    setuptools
+    pyinotify
+  ];
 
   doCheck = false;
   pythonImportsCheck = [ "auto_cpufreq" ];
@@ -29,21 +55,46 @@ python3Packages.buildPythonPackage rec {
     ./prevent-update.patch
   ];
 
+  postPatch = ''
+    substituteInPlace auto_cpufreq/core.py \
+      --replace-fail '/opt/auto-cpufreq/override.pickle' /var/run/override.pickle
+    substituteInPlace scripts/org.auto-cpufreq.pkexec.policy \
+      --replace-fail "/opt/auto-cpufreq/venv/bin/auto-cpufreq" $out/bin/auto-cpufreq
+    substituteInPlace auto_cpufreq/gui/app.py auto_cpufreq/gui/objects.py \
+      --replace-fail "/usr/local/share/auto-cpufreq/images/icon.png" $out/share/pixmaps/auto-cpufreq.png
+    substituteInPlace auto_cpufreq/gui/app.py \
+      --replace-fail "/usr/local/share/auto-cpufreq/scripts/style.css" $out/share/auto-cpufreq/scripts/style.css
+  '';
+
   postInstall = ''
     # copy script manually
     cp ${src}/scripts/cpufreqctl.sh $out/bin/cpufreqctl.auto-cpufreq
 
+    # copy css file
+    mkdir -p $out/share/auto-cpufreq/scripts
+    cp scripts/style.css $out/share/auto-cpufreq/scripts/style.css
+
     # systemd service
     mkdir -p $out/lib/systemd/system
     cp ${src}/scripts/auto-cpufreq.service $out/lib/systemd/system
+
+    # desktop icon
+    mkdir -p $out/share/applications
+    mkdir $out/share/pixmaps
+    cp scripts/auto-cpufreq-gtk.desktop $out/share/applications
+    cp images/icon.png $out/share/pixmaps/auto-cpufreq.python3Packages
+
+    # polkit policy
+    mkdir -p $out/share/polkit-1/actions
+    cp scripts/org.auto-cpufreq.pkexec.policy $out/share/polkit-1/actions
   '';
 
-  meta = with lib; {
-    mainProgram = "${pname}";
+  meta = {
+    mainProgram = "auto-cpufreq";
     homepage = "https://github.com/AdnanHodzic/auto-cpufreq";
     description = "Automatic CPU speed & power optimizer for Linux";
-    license = licenses.lgpl3Plus;
-    platforms = platforms.linux;
-    maintainers = [ maintainers.Technical27 ];
+    license = lib.licenses.lgpl3Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ Technical27 ];
   };
 }
