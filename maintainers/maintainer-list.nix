ebc77db2cc49d47caf4a3de69820c9e6996454e2
yomaq <yomaq@bsjm.xyz>
2024-05-30 20:46:36 -0500

    maintainers: add yomaq
    (cherry picked from commit ba6025ce4150d66954ce443a0564a8b72017245a)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22567,6 +22567,11 @@
     githubId = 40352765;
     name = "Yoctocell";
   };
+  yomaq = {
+    name = "yomaq";
+    github = "yomaq";
+    githubId = 112864332;
+  };
   yorickvp = {
     email = "yorickvanpelt@gmail.com";
     matrix = "@yorickvp:matrix.org";
