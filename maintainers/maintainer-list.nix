d35cdfc5cc8c32d35428e4fe890c705e8410b2e0
Matteo Pacini <matteo@codecraft.it>
2024-06-01 13:32:26 +0200

    maintainers: add matteopacini

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12804,6 +12804,12 @@
     githubId = 952712;
     name = "Matt Christ";
   };
+  matteopacini = {
+    email = "m@matteopacini.me";
+    github = "matteo-pacini";
+    githubId = 3139724;
+    name = "Matteo Pacini";
+  };
   matthewbauer = {
     email = "mjbauer95@gmail.com";
     github = "matthewbauer";
