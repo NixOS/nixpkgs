8e79c665fec3d65c9f95e0edf44ac54ec4464856
Luis David Licea Torres <luislicea@hotmail.com>
2024-05-31 22:15:44 -0500

    Address review feedback

--- nixos/modules/programs/localsend.nix
+++ nixos/modules/programs/localsend.nix
@@ -9,19 +9,19 @@ with lib; let
   firewallPort = 53317;
 in {
   options.programs.localsend = {
-    enable = mkEnableOption (mdDoc "localsend");
+    enable = mkEnableOption "localsend, an open source cross-platform alternative to AirDrop";
 
     openFirewall = mkOption {
       description = mdDoc ''
         Whether to open the firewall port ${firewallPort} for receiving files.
       '';
-      default = false;
+      default = true;
       type = types.bool;
     };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [localsend];
+    environment.systemPackages = with pkgs; [ localsend ];
     networking.firewall.allowedTCPPorts = optional cfg.openFirewall firewallPort;
   };
 }
