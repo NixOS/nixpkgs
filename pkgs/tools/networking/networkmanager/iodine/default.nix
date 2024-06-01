9e5f97e405541170499f137dbf2473dc9a2d1a9d
Jan Tojnar <jtojnar@gmail.com>
2024-06-01 16:35:22 +0000

    networkmanager-iodine: unstable-2019-11-05 → 1.2.0-unstable-2024-05-12
    https://gitlab.gnome.org/GNOME/network-manager-iodine/-/compare/2ef0abf089b00a0546f214dde0d45e63f2990b79...8ec0a35e12047ccf256b3951897c701661ddb8af
    
    - Removes nm-glib https://gitlab.gnome.org/GNOME/network-manager-iodine/-/commit/dca561224903ea0dc68fdd77011fc47e59dae961

--- pkgs/tools/networking/networkmanager/iodine/default.nix
+++ pkgs/tools/networking/networkmanager/iodine/default.nix
@@ -12,32 +12,26 @@
   gtk3,
   withGnome ? true,
   unstableGitUpdater,
-  fetchpatch,
   libnma,
   glib,
 }:
 
 stdenv.mkDerivation {
   pname = "NetworkManager-iodine${lib.optionalString withGnome "-gnome"}";
-  version = "unstable-2019-11-05";
+  version = "1.2.0-unstable-2024-05-12";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "GNOME";
     repo = "network-manager-iodine";
-    rev = "2ef0abf089b00a0546f214dde0d45e63f2990b79";
-    sha256 = "1ps26fr9b1yyafj7lrzf2kmaxb0ipl0mhagch5kzrjdsc5xkajz7";
+    rev = "8ec0a35e12047ccf256b3951897c701661ddb8af";
+    sha256 = "cNjznry8wi1UmE5khf0JCEYjs9nDU/u8lFLte53MLTM=";
   };
 
   patches = [
     (substituteAll {
       src = ./fix-paths.patch;
       inherit iodine;
-    })
-    # Don't use etc/dbus-1/system.d
-    (fetchpatch {
-      url = "https://gitlab.gnome.org/GNOME/network-manager-iodine/merge_requests/2.patch";
-      sha256 = "108pkf0mddj32s46k7jkmpwcaq2ylci4dqpp7wck3zm9q2jffff2";
     })
   ];
 
@@ -60,7 +54,6 @@ stdenv.mkDerivation {
     ];
 
   configureFlags = [
-    "--without-libnm-glib"
     "--with-gnome=${if withGnome then "yes" else "no"}"
     "--localstatedir=/" # needed for the management socket under /run/NetworkManager
     "--enable-absolute-paths"
@@ -69,11 +62,6 @@ stdenv.mkDerivation {
   preConfigure = ''
     intltoolize
   '';
-
-  env = {
-    # glib-2.62 deprecations
-    NIX_CFLAGS_COMPILE = "-DGLIB_DISABLE_DEPRECATION_WARNINGS";
-  };
 
   passthru = {
     updateScript = unstableGitUpdater {
