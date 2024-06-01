4ccd7df5fda5e1203ddf5da433eae0113d1b843e
Rahul Rameshbabu <rrameshbabu@nvidia.com>
2024-06-01 10:49:32 -0700

    globalprotect-openconnect: remove deprecated 1.x package
    The 1.x iteration of globalprotect-openconnect is no longer being
    developed. Remove related components from nixpkgs.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1016,7 +1016,6 @@
   ./services/networking/gdomap.nix
   ./services/networking/ghostunnel.nix
   ./services/networking/git-daemon.nix
-  ./services/networking/globalprotect-vpn.nix
   ./services/networking/gns3-server.nix
   ./services/networking/gnunet.nix
   ./services/networking/go-autoconfig.nix
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14026,8 +14026,6 @@ with pkgs;
 
   inherit (openconnectPackages) openconnect openconnect_openssl;
 
-  globalprotect-openconnect = libsForQt5.callPackage ../tools/networking/globalprotect-openconnect { };
-
   ding-libs = callPackage ../tools/misc/ding-libs { };
 
   sssd = callPackage ../os-specific/linux/sssd {
