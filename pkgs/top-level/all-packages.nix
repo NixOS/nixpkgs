361faf65e9a6411b5b3b1f7d203f16b6c7c47cf0
aleksana <me@aleksana.moe>
2024-06-01 19:13:32 +0800

    nixd: 2.1.2 -> 2.2.0; {nixf,nixt}: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17943,10 +17943,10 @@ with pkgs;
 
   nil = callPackage ../development/tools/language-servers/nil { };
 
-  nixd = callPackage ../development/tools/language-servers/nixd {
+  inherit (callPackages ../development/tools/language-servers/nixd {
     llvmPackages = llvmPackages_16;
     nix = nixVersions.nix_2_19;
-  };
+  }) nixf nixt nixd;
 
   openscad-lsp = callPackage ../development/tools/language-servers/openscad-lsp { };
 
