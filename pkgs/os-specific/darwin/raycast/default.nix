504995307c78439ad0e5ab0e05051d6f2e2972ea
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-06-01 12:58:48 +0300

    raycast: 1.75.1 -> 1.75.2

--- pkgs/os-specific/darwin/raycast/default.nix
+++ pkgs/os-specific/darwin/raycast/default.nix
@@ -11,12 +11,12 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "raycast";
-  version = "1.75.1";
+  version = "1.75.2";
 
   src = fetchurl {
     name = "Raycast.dmg";
     url = "https://releases.raycast.com/releases/${finalAttrs.version}/download?build=universal";
-    hash = "sha256-lHGKWj4nn0GsviV83MKgCaQ6HW/CfeP8gg4VXlVXaXg=";
+    hash = "sha256-P9lbIU8IBdowy8vkv+PHITBUpRNTI9t0j8Vm1DjYXnQ=";
   };
 
   dontPatch = true;
