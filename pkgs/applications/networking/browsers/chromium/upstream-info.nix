5e47aeadecd8fdfac0ff6943a995eb0c76d04b42
networkException <git@nwex.de>
2024-06-01 03:07:23 +0200

    ungoogled-chromium: 125.0.6422.112-1 -> 125.0.6422.141-1
    https://chromereleases.googleblog.com/2024/05/stable-channel-update-for-desktop_30.html
    
    This update includes 11 security fixes.
    
    CVEs:
    CVE-2024-5493 CVE-2024-5494 CVE-2024-5495 CVE-2024-5496 CVE-2024-5497
    CVE-2024-5498 CVE-2024-5499
    
    (cherry picked from commit ffaf07420040a4413df8165cd4f39ee48fef35d1)

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -27,11 +27,11 @@
         version = "2024-04-10";
       };
       ungoogled-patches = {
-        hash = "sha256-vHnXIrDdHGIe8byb41CiEWq3FPTecKg006dU7+iESKA=";
-        rev = "125.0.6422.112-1";
+        hash = "sha256-ZYYizL3hFSEQUdDDZIvsEzidq5td+UoaWdertY/pqOc=";
+        rev = "125.0.6422.141-1";
       };
     };
-    hash = "sha256-EA8TzemtndFb8qAp4XWNjwWmNRz/P4Keh3k1Cn9qLEU=";
-    version = "125.0.6422.112";
+    hash = "sha256-8Qe1hgDEjvdAf2ao4CIieC7l2pTSIPLTZb+vdctUEo0=";
+    version = "125.0.6422.141";
   };
 }
