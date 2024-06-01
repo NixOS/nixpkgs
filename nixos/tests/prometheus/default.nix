38495e1ec0399ab6b8873eee45d40cb0e525ed69
Jonathan Davies <jpds@protonmail.com>
2024-06-02 00:12:11 +0100

    nixos/prometheus: Added federation test case

--- nixos/tests/prometheus/default.nix
+++ nixos/tests/prometheus/default.nix
@@ -4,6 +4,7 @@
 }:
 
 {
+  federation = import ./federation.nix { inherit system pkgs; };
   prometheus-pair = import ./prometheus-pair.nix { inherit system pkgs; };
   pushgateway = import ./pushgateway.nix { inherit system pkgs; };
   remote-write = import ./remote-write.nix { inherit system pkgs; };
