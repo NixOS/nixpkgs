87cb2d582e32fa75a07be128674b8aa0f1eeb234
Jonathan Davies <jpds@protonmail.com>
2024-06-02 00:10:13 +0100

    nixos/prometheus: Added pushgateway test case

--- nixos/tests/prometheus/default.nix
+++ nixos/tests/prometheus/default.nix
@@ -5,5 +5,6 @@
 
 {
   prometheus-pair = import ./prometheus-pair.nix { inherit system pkgs; };
+  pushgateway = import ./pushgateway.nix { inherit system pkgs; };
   remote-write = import ./remote-write.nix { inherit system pkgs; };
 }
