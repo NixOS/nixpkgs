a5e6faa0991e165aee51d41417aae109add2adf1
Bruno Rodrigues <bruno@brodrigues.co>
2024-06-01 11:17:39 +0200

    rPackages.RcppGetconf: fixed build

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -1190,6 +1190,10 @@ let
       patchPhase = "patchShebangs configure";
     });
 
+    RcppGetconf = old.RcppGetconf.overrideAttrs (attrs: {
+      postPatch = "patchShebangs configure";
+    });
+
     SpliceWiz = old.SpliceWiz.overrideAttrs (attrs: {
       postPatch = "patchShebangs configure";
     });
