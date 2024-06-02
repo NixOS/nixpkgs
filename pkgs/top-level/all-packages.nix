192b17b56937c985ad619ccb43ac4da7f4316519
Sergei Trofimovich <slyich@gmail.com>
2024-06-02 23:27:30 +0100

    ensureNewerSourcesHook: handle $sourceRoot that starts with dash
    Without the change `ensureNewerSourcesHook` fails on tarballs like
    `diffoscope-269` that contain single top-level `-269` root as:
    
        diffoscope> unpacking source archive ...-diffoscope-269.tar.bz2
        diffoscope> source root is -269
        diffoscope> find: unknown predicate `-269'
    
    Currently `diffoscope-269` has a `sourceRoot = "./-269";` workaround to
    bypass the failure.
    
    Co-authored-by: Philip Taron <philip.taron@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -217,7 +217,11 @@ with pkgs;
   } (writeScript "ensure-newer-sources-hook.sh" ''
       postUnpackHooks+=(_ensureNewerSources)
       _ensureNewerSources() {
-        '${findutils}/bin/find' "$sourceRoot" \
+        local r=$sourceRoot
+        # Avoid passing option-looking directory to find. The example is diffoscope-269:
+        #   https://salsa.debian.org/reproducible-builds/diffoscope/-/issues/378
+        [[ $r == -* ]] && r="./$r"
+        '${findutils}/bin/find' "$r" \
           '!' -newermt '${year}-01-01' -exec touch -h -d '${year}-01-02' '{}' '+'
       }
     '');
