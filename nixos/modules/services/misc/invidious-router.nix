039e6e75bd7bb2cd786ee0c7dd6eef8ac283edc3
Silas Schöffel <sils@sils.li>
2024-06-02 13:27:22 +0200

    nixos/invidious-router: remove redundant "Enables" in description
    (cherry picked from commit 37afbbb6022dd80f759e05c05c29f947d357f4ba)

--- nixos/modules/services/misc/invidious-router.nix
+++ nixos/modules/services/misc/invidious-router.nix
@@ -11,7 +11,7 @@ in {
   meta.maintainers = [lib.maintainers.s1ls];
 
   options.services.invidious-router = {
-    enable = lib.mkEnableOption "Enables the invidious-router service";
+    enable = lib.mkEnableOption "the invidious-router service";
     port = lib.mkOption {
       type = lib.types.port;
       default = 8050;
