a7389a7866f4c537bf71f7fab0d51209923d8722
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-02 11:55:28 +0000

    groonga: 14.0.2 -> 14.0.4

--- pkgs/servers/search/groonga/default.nix
+++ pkgs/servers/search/groonga/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "groonga";
-  version = "14.0.2";
+  version = "14.0.4";
 
   src = fetchurl {
     url = "https://packages.groonga.org/source/groonga/groonga-${finalAttrs.version}.tar.gz";
-    hash = "sha256-o9C6lPOPkb2KCbF4CqLyKtXHfOdB3jAIv5P6SjTJAJc=";
+    hash = "sha256-8ppuiEAc25BDcWSiL28ehGfdarnBc4vnGTO+nLl6RsY=";
   };
 
   patches = [
