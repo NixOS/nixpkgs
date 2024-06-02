f40cb8ed714e0d2856ed30a709bbc46285e54a01
linsui <linsui@inbox.lv>
2024-06-02 15:56:01 +0800

    yutto: 2.0.0-beta.37 -> 2.0.0-beta.40

--- pkgs/by-name/yu/yutto/package.nix
+++ pkgs/by-name/yu/yutto/package.nix
@@ -7,7 +7,7 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "yutto";
-  version = "2.0.0-beta.37";
+  version = "2.0.0-beta.40";
   format = "pyproject";
 
   disabled = python3Packages.pythonOlder "3.9";
@@ -16,7 +16,7 @@ python3Packages.buildPythonApplication rec {
     owner = "yutto-dev";
     repo = "yutto";
     rev = "v${version}";
-    hash = "sha256-daRuFYfR3FjvhVsQM1FXI19iOH+bukh6WxfH5O+CFk4=";
+    hash = "sha256-gopCQ8tEhwtDFs/w+jafD3ZW/4MIrYxPcMh8SbOCwww=";
   };
 
   nativeBuildInputs = with python3Packages; [
