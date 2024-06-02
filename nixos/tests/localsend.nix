4d4b3b69ad27e73f91448604ab654802e82f2d91
Luis David Licea Torres <luislicea@hotmail.com>
2024-06-02 15:09:39 -0500

    Simplify localsend test
    Enabling `xserver` explicitly is not needed because that is taken care
    of by importing x11.nix.
    
    Also, set the timeout to 10 seconds since we do not want to wait for
    minutes and minutes for localsend to start, if it ever starts.

--- nixos/tests/localsend.nix
+++ nixos/tests/localsend.nix
@@ -2,21 +2,14 @@ import ./make-test-python.nix ({ lib, pkgs, ... }: {
   name = "localsend";
 
   nodes.machine = { config, pkgs, ... }: {
-    imports = [
-      ./common/x11.nix
-    ];
-    services.xserver.enable = true;
-
-    programs.localsend = {
-      enable = true;
-      openFirewall = true;
-    };
+    imports = [ ./common/x11.nix ];
+    programs.localsend.enable = true;
   };
 
   testScript = ''
     machine.wait_for_x()
-    machine.execute("localsend >&2 &")
-    machine.wait_for_window("LocalSend")
+    machine.execute("localsend")
+    machine.wait_for_window("LocalSend", 10)
     machine.wait_for_open_port(53317)
     machine.succeed("netstat --listening --program --tcp | grep -P 'tcp.*53317.*localsend'")
   '';
