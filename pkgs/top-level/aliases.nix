b8873a8a8038db832d5c8620bac1a7f7e773cce6
Jan Tojnar <jtojnar@gmail.com>
2024-06-02 15:20:23 +0200

    top-level/aliases: Clean up outdated Nix aliases
    - Nix 2.6 was removed on 2022-12-28: 172347745ffb457e47cf5386f2ae3ff6678c2b02
    - Nix 2.4 and 2.5 were removed on 2022-07-14: b2eea05b06baa2886039d61485d37c338ad3d578
    - `nixVersions.unstable` throws but it does not mention the alias: 2b4e18f3d4a7b80af21b640c0970f83b34efceff

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -887,11 +887,8 @@ mapAliases ({
   nix-template-rpm = throw "'nix-template-rpm' has been removed as it is broken and unmaintained"; # Added 2023-11-20
   nixFlakes = nixVersions.stable; # Added 2021-05-21
   nixStable = nixVersions.stable; # Added 2022-01-24
-  nixUnstable = nixVersions.unstable; # Added 2022-01-26
+  nixUnstable = throw "nixUnstable has been removed. For bleeding edge (Nix master, roughly weekly updated) use nixVersions.git, otherwise use nixVersions.latest."; # Converted to throw 2024-04-22
   nix_2_3 = nixVersions.nix_2_3;
-  nix_2_4 = nixVersions.nix_2_4;
-  nix_2_5 = nixVersions.nix_2_5;
-  nix_2_6 = nixVersions.nix_2_6;
   nixfmt = lib.warn "nixfmt was renamed to nixfmt-classic. The nixfmt attribute may be used for the new RFC 166-style formatter in the future, which is currently available as nixfmt-rfc-style" nixfmt-classic; # Added 2024-03-31
   nixops = throw "'nixops' has been removed. Please use 'nixops_unstable_minimal' for the time being. E.g. nixops_unstable_minimal.withPlugins (ps: [ ps.nixops-gce ])"; # Added 2023-10-26
   nixopsUnstable = nixops_unstable; # Added 2022-03-03
