b23bf066664571655a7aa1aba36f5c46339e5919
Doron Behar <doron.behar@gmail.com>
2024-06-03 12:47:26 +0300

    python311Packages.tree-sitter-languages: init at 1.10.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15414,6 +15414,8 @@ self: super: with self; {
 
   tree-sitter-json = callPackage ../development/python-modules/tree-sitter-json { };
 
+  tree-sitter-languages = callPackage ../development/python-modules/tree-sitter-languages { };
+
   treelib = callPackage ../development/python-modules/treelib { };
 
   treelog = callPackage ../development/python-modules/treelog { };
