73dee9023bda28cac7d0f5077c055abcc787b471
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-03 08:28:09 +0200

    python311Packages.llama-index-graph-stores-neo4j: 0.2.0 -> 0.2.2

--- pkgs/development/python-modules/llama-index-graph-stores-neo4j/default.nix
+++ pkgs/development/python-modules/llama-index-graph-stores-neo4j/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-graph-stores-neo4j";
-  version = "0.2.0";
+  version = "0.2.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_index_graph_stores_neo4j";
     inherit version;
-    hash = "sha256-+FkhevhrjEmJk9WXHJ0oR9T6DF5ZHWzDtrIWMjVa2vY=";
+    hash = "sha256-z2XLZ5tt2uTzTxx5F3jxSLLva2PwDMterKH17P46qjU=";
   };
 
   build-system = [ poetry-core ];
