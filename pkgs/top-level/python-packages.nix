1a9df0630934ff5d56dd6177d469fd82241c18af
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-03 11:32:11 +0200

    python312Packages.typer-shell: init at 0.1.9
    Library for making beautiful shells/REPLs with Typer
    
    https://github.com/FergusFettes/typer-shell

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15562,6 +15562,8 @@ self: super: with self; {
 
   typer = callPackage ../development/python-modules/typer { };
 
+  typer-shell = callPackage ../development/python-modules/typer-shell { };
+
   type-infer = callPackage ../development/python-modules/type-infer { };
 
   types-aiobotocore = callPackage ../development/python-modules/types-aiobotocore { };
