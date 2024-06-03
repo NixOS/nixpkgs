a6616c70aaabcead1224c859c9cffe4c73b9727b
Doron Behar <doron.behar@gmail.com>
2024-06-03 14:24:30 +0300

    python311Packages.tree-sitter-json: init at 0.21.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15489,6 +15489,8 @@ self: super: with self; {
 
   tree-sitter-javascript = callPackage ../development/python-modules/tree-sitter-javascript { };
 
+  tree-sitter-json = callPackage ../development/python-modules/tree-sitter-json { };
+
   treelib = callPackage ../development/python-modules/treelib { };
 
   treelog = callPackage ../development/python-modules/treelog { };
