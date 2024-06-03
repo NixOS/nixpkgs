ed12d8f5e0d6e88570ae8a024c19bc7f77fceb16
Anderson Torres <torres.anderson.85@protonmail.com>
2024-06-02 21:21:10 -0300

    arcan: declare upstream tracy alongside letoram fork
    The fork is somewhat broken.

--- pkgs/by-name/ar/arcan/sources.nix
+++ pkgs/by-name/ar/arcan/sources.nix
@@ -56,13 +56,25 @@
 
   tracy = {
     pname = "tracy";
-    version = "0.10-unstable-2024-05-08";
+    version = "0.9.1-unstable-2023-10-09";
 
     src = fetchFromGitHub {
       owner = "wolfpld";
       repo = "tracy";
-      rev = "11eee619fbb2ca97d8b7f7f6e0d6b20e37afbe61";
-      hash = "sha256-VRRNL7trX9Q6x/ujByidlJQvHxtDe7NZ7JomLFzXDE0=";
+      rev = "93537dff336e0796b01262e8271e4d63bf39f195";
+      hash = "sha256-FNB2zTbwk8hMNmhofz9GMts7dvH9phBRVIdgVjRcyQM=";
+    };
+  };
+
+  letoram-tracy = {
+    pname = "letoram-tracy";
+    version = "0-unstable-2024-04-12";
+
+    src = fetchFromGitHub {
+      owner = "letoram";
+      repo = "tracy";
+      rev = "5b3513d9838317bfc0e72344b94aa4443943c2fd";
+      hash = "sha256-hUdYC4ziQ7V7T7k99MERp81F5mPHzFtPFrqReWsTjOQ=";
     };
   };
 }
