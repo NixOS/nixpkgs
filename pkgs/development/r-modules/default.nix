69b21aa8778dd0c7f4a1ad0a6e807d0b7ad7580d
Justin Bedo <cu@cua0.org>
2024-06-03 10:02:11 +1000

    rPackages.arcgisplaces: fix build

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -466,6 +466,7 @@ let
     tok = [ pkgs.cargo ];
     arcgisutils = with pkgs; [ cargo rustc ];
     arcgisgeocode = with pkgs; [ cargo rustc ];
+    arcgisplaces = with pkgs; [ pkg-config openssl.dev cargo rustc ];
     apcf = with pkgs; [ geos ];
     SemiCompRisks = [ pkgs.gsl ];
     showtext = with pkgs; [ zlib libpng icu freetype.dev ];
