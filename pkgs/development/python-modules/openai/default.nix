b69b00a2316b34c6508bf45089ae1d36b0844a20
Malo Bourgon <mbourgon@gmail.com>
2024-06-03 16:11:59 -0700

    python311Packages.openai: 1.30.4 -> 1.31.0
    Diff: https://github.com/openai/openai-python/compare/refs/tags/v1.30.4...v1.31.0
    
    Changelog: https://github.com/openai/openai-python/releases/tag/v1.31.0

--- pkgs/development/python-modules/openai/default.nix
+++ pkgs/development/python-modules/openai/default.nix
@@ -25,7 +25,7 @@
 
 buildPythonPackage rec {
   pname = "openai";
-  version = "1.30.4";
+  version = "1.31.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7.1";
@@ -34,7 +34,7 @@ buildPythonPackage rec {
     owner = "openai";
     repo = "openai-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-tzHU5yO7o7wxdqYnp7tBctvWGY7SYq5u6VnU3iPGPuk=";
+    hash = "sha256-En7lqi9dF3DLX+LkwgCeagt2//0JZgANZPVlFM0fHZs=";
   };
 
   build-system = [
