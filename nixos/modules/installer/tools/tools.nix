c3ed7a69e9441c18d213894060ce2e59e52db992
Sandro <sandro.jaeckel@gmail.com>
2024-06-03 21:06:06 +0200

    nixos-generate-config: drop console.keyMap
    This is already set by console.useXkbConfig and causes a conflict

--- nixos/modules/installer/tools/tools.nix
+++ nixos/modules/installer/tools/tools.nix
@@ -162,7 +162,6 @@ in
         # i18n.defaultLocale = "en_US.UTF-8";
         # console = {
         #   font = "Lat2-Terminus16";
-        #   keyMap = "us";
         #   useXkbConfig = true; # use xkb.options in tty.
         # };
 
