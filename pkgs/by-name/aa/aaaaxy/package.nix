84b5b4cfa7ac2c32c12f899f1eddb3e40e043102
Luflosi <luflosi@luflosi.de>
2024-06-03 23:18:43 +0200

    aaaaxy: 1.5.129 -> 1.5.139
    https://github.com/divVerent/aaaaxy/releases/tag/v1.5.139

--- pkgs/by-name/aa/aaaaxy/package.nix
+++ pkgs/by-name/aa/aaaaxy/package.nix
@@ -20,17 +20,17 @@
 
 buildGoModule rec {
   pname = "aaaaxy";
-  version = "1.5.129";
+  version = "1.5.139";
 
   src = fetchFromGitHub {
     owner = "divVerent";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-kH2eFFxohvuuEP2p7bt8zOYbk3gF86X9y3sNdLYl/Qo=";
+    hash = "sha256-j8BpLN0PhMqUQXhF1L7uEW2KLaVq5cfckXnWrkltCgM=";
     fetchSubmodules = true;
   };
 
-  vendorHash = "sha256-VEayNWztJYeQoJHVJfAlmHD65PEho1TCttTfT0cbgUQ=";
+  vendorHash = "sha256-a6nfGP6Lwjwr4wXXRUrCyV9+BCyHOSKczk5rjZdFnlA=";
 
   buildInputs = [
     alsa-lib
