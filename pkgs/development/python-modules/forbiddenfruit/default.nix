13794c0dfb2e3768da6fc529e667b81d6f3bfbfc
natsukium <tomoya.otabi@gmail.com>
2024-06-04 20:41:53 +0900

    python311Packages.forbiddenfruit: refactor and remove nose

--- pkgs/development/python-modules/forbiddenfruit/default.nix
+++ pkgs/development/python-modules/forbiddenfruit/default.nix
@@ -2,31 +2,38 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  nose,
+  pynose,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   version = "0.1.4";
-  format = "setuptools";
   pname = "forbiddenfruit";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "clarete";
     repo = "forbiddenfruit";
     rev = version;
-    sha256 = "16chhrxbbmg6lfbzm532fq0v00z8qihcsj0kg2b5jlgnb6qijwn8";
+    hash = "sha256-yHIZsVn2UVmWeBNIzWDE6AOwAXZilPqXo+bVtXqGkJk=";
   };
 
-  nativeCheckInputs = [ nose ];
+  build-system = [ setuptools ];
 
-  preBuild = ''
-    export FFRUIT_EXTENSION="true";
-  '';
+  env.FFRUIT_EXTENSION = "true";
+
+  pythonImportsCheck = [ "forbiddenfruit" ];
+
+  nativeCheckInputs = [ pynose ];
 
   # https://github.com/clarete/forbiddenfruit/pull/47 required to switch to pytest
   checkPhase = ''
+    runHook preCheck
+
     find ./build -name '*.so' -exec mv {} tests/unit \;
     nosetests
+
+    runHook postCheck
   '';
 
   meta = with lib; {
