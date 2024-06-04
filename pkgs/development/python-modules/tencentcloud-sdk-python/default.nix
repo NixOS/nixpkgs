4fa521e8c119f182b705961693edd9830d757be6
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-04 09:24:32 +0200

    python312Packages.tencentcloud-sdk-python: 3.0.1160 -> 3.0.1161
    Diff: https://github.com/TencentCloud/tencentcloud-sdk-python/compare/refs/tags/3.0.1160...3.0.1161
    
    Changelog: https://github.com/TencentCloud/tencentcloud-sdk-python/blob/3.0.1161/CHANGELOG.md

--- pkgs/development/python-modules/tencentcloud-sdk-python/default.nix
+++ pkgs/development/python-modules/tencentcloud-sdk-python/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "tencentcloud-sdk-python";
-  version = "3.0.1160";
+  version = "3.0.1161";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "TencentCloud";
     repo = "tencentcloud-sdk-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-G0qft2hz9LfN0QKarANtJavxKVTx3rMNG6i8rJmlGnU=";
+    hash = "sha256-dev6qDLT6rMG6hiWdx+HQDXkvd2wLzlZaZp3dbh3A5c=";
   };
 
   build-system = [ setuptools ];
