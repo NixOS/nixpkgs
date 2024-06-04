a062b23ad1c276bfd86312146df92919a05d66c3
Nikolay Korotkiy <sikmir@disroot.org>
2024-06-04 21:24:57 +0400

    tile38: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26765,8 +26765,6 @@ with pkgs;
 
   pg_tileserv = callPackage ../servers/geospatial/pg_tileserv { };
 
-  tile38 = callPackage ../servers/geospatial/tile38 { };
-
   ### OS-SPECIFIC
 
   airbuddy = callPackage ../os-specific/darwin/airbuddy { };
