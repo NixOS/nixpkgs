e0399f66a7f215a18393ccfeea6b5f9413230f81
Nick Cao <nickcao@nichi.co>
2024-06-04 18:57:40 -0400

    julia_110: 1.10.3 -> 1.10.4

--- pkgs/development/compilers/julia/default.nix
+++ pkgs/development/compilers/julia/default.nix
@@ -50,10 +50,9 @@ in
     { });
   julia_110 = wrapJulia (callPackage
     (import ./generic.nix {
-      version = "1.10.3";
-      hash = "sha256-2JKyEjvmTaz50F5My61/F5f2v4fDl6dIBLARyHUPbI8=";
+      version = "1.10.4";
+      hash = "sha256-8y5Sd/XYKmOCSILN6/rBWBmbuEgUw8AZo/7MNgFYYZE=";
       patches = [
-        ./patches/1.10/0001-skip-building-docs-as-it-requires-network-access.patch
         ./patches/1.10/0002-skip-failing-and-flaky-tests.patch
       ];
     })
