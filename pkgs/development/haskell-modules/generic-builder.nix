5dbf6650897fb95767fd6b1c4c04d7080de4ed5e
Wolfgang Walther <walther@technowledgy.de>
2024-06-04 14:34:03 +0200

    haskellPackages: Pass ghc-options in generic-builder when cross-compiling
    The following sequence of operations loses ghc-options:
      Setup.hs configure --ghc-options
      Setup.hs build --with-ghc=...
    
    This is described in [1].
    
    The fix is simple: Don't pass --with-ghc in the build phase. The values
    are taken from the configure step anyway. This seems to have been
    introduced all the way back in 64ec4dd87bf7b211773541fa350ef2f56b9c658f
    about 8 years ago for unknown reasons.
    
    Resolves #286285
    
    [1]: https://github.com/haskell/cabal/issues/10069

--- pkgs/development/haskell-modules/generic-builder.nix
+++ pkgs/development/haskell-modules/generic-builder.nix
@@ -215,9 +215,6 @@ let
 
   parallelBuildingFlags = "-j$NIX_BUILD_CORES" + optionalString stdenv.isLinux " +RTS -A64M -RTS";
 
-  crossCabalFlagsString =
-    lib.optionalString isCross (" " + lib.concatStringsSep " " crossCabalFlags);
-
   buildFlagsString = optionalString (buildFlags != []) (" " + concatStringsSep " " buildFlags);
 
   defaultConfigureFlags = [
@@ -556,7 +553,7 @@ stdenv.mkDerivation ({
         find dist/build -exec touch -d '1970-01-01T00:00:00Z' {} +
         ''
     + ''
-      ${setupCommand} build ${buildTarget}${crossCabalFlagsString}${buildFlagsString}
+      ${setupCommand} build ${buildTarget}${buildFlagsString}
       runHook postBuild
       '';
 
