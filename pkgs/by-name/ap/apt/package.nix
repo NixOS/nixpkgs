c25484219c81b4cd76a9ca336f4a77bfa857f32c
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-04 05:58:46 +0000

    apt: 2.9.3 -> 2.9.4

--- pkgs/by-name/ap/apt/package.nix
+++ pkgs/by-name/ap/apt/package.nix
@@ -33,11 +33,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "apt";
-  version = "2.9.3";
+  version = "2.9.4";
 
   src = fetchurl {
     url = "mirror://debian/pool/main/a/apt/apt_${finalAttrs.version}.tar.xz";
-    hash = "sha256-7R0BlcpujXjbulOJ7Eb8/gQiut77vce1GkbJCfa6LXE=";
+    hash = "sha256-Ra6jeJM7nkaPZWBR7Sv8+TurArs1D5TodUcn6xe0B6Q=";
   };
 
   # cycle detection; lib can't be split
