b969d28996c02d41c4b14a6367e425cd44be4c60
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-06-05 13:44:22 +0200

    mousai: move to pkgs/by-name
    (cherry picked from commit a8e5c604cccfa65423bea577879abfe15443f5d4)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32889,8 +32889,6 @@ with pkgs;
 
   motif = callPackage ../development/libraries/motif { };
 
-  mousai = callPackage ../applications/audio/mousai { };
-
   mozjpeg = callPackage ../applications/graphics/mozjpeg { };
 
   edgetx = libsForQt5.callPackage ../applications/misc/edgetx { };
