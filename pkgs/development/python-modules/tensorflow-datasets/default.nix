d19210a20c5e9f899ff9c6b242cec1f903965978
Gaetan Lepage <gaetan@glepage.com>
2024-06-05 11:01:09 +0200

    python311Packages.tensorflow-datasets: 4.9.4 -> 4.9.7
    Changelog: https://github.com/tensorflow/datasets/releases/tag/v4.9.7
    
    Diff: https://github.com/tensorflow/datasets/compare/v4.9.4...v4.9.7

--- pkgs/development/python-modules/tensorflow-datasets/default.nix
+++ pkgs/development/python-modules/tensorflow-datasets/default.nix
@@ -1,81 +1,82 @@
 {
-  apache-beam,
-  array-record,
-  attrs,
-  beautifulsoup4,
+  lib,
   buildPythonPackage,
-  click,
-  datasets,
+  fetchFromGitHub,
+
+  # dependencies
+  array-record,
   dill,
   dm-tree,
-  fetchFromGitHub,
-  ffmpeg,
   future,
-  imagemagick,
+  immutabledict,
   importlib-resources,
+  numpy,
+  promise,
+  protobuf,
+  psutil,
+  requests,
+  simple-parsing,
+  six,
+  tensorflow-metadata,
+  termcolor,
+  tqdm,
+
+  # tests
+  apache-beam,
+  beautifulsoup4,
+  click,
+  datasets,
+  ffmpeg,
+  imagemagick,
   jax,
   jaxlib,
   jinja2,
   langdetect,
-  lib,
   lxml,
   matplotlib,
   mwparserfromhell,
   mwxml,
   networkx,
   nltk,
-  numpy,
   opencv4,
   pandas,
   pillow,
-  promise,
-  protobuf,
-  psutil,
   pycocotools,
   pydub,
   pytest-xdist,
   pytestCheckHook,
-  requests,
   scikit-image,
   scipy,
-  six,
   tensorflow,
-  tensorflow-metadata,
-  termcolor,
   tifffile,
-  tqdm,
   zarr,
 }:
 
 buildPythonPackage rec {
   pname = "tensorflow-datasets";
-  version = "4.9.4";
-  format = "setuptools";
+  version = "4.9.7";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tensorflow";
     repo = "datasets";
     rev = "refs/tags/v${version}";
-    hash = "sha256-HY/atBEWeEJgBNxEapq9jPFoZbFof2AHEDAiJa/lYAE=";
+    hash = "sha256-pnut5z3rEYIOOhsQT6uVjPdy+SqftKptSQMwxbMXoKA=";
   };
 
-  patches = [
-    # addresses https://github.com/tensorflow/datasets/issues/3673
-    ./corruptions.patch
-  ];
-
-  propagatedBuildInputs = [
+  dependencies = [
     array-record
-    attrs
     dill
     dm-tree
     future
+    immutabledict
     importlib-resources
     numpy
     promise
     protobuf
     psutil
     requests
+    simple-parsing
     six
     tensorflow-metadata
     termcolor
@@ -85,7 +86,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "tensorflow_datasets" ];
 
   nativeCheckInputs = [
-    apache-beam
+    # apache-beam
     beautifulsoup4
     click
     datasets
@@ -147,10 +148,6 @@ buildPythonPackage rec {
     "tensorflow_datasets/core/features/audio_feature_test.py"
     "tensorflow_datasets/image/lsun_test.py"
 
-    # Requires `envlogger` which is not packaged in `nixpkgs`.
-    "tensorflow_datasets/rlds/locomotion/locomotion_test.py"
-    "tensorflow_datasets/rlds/robosuite_panda_pick_place_can/robosuite_panda_pick_place_can_test.py"
-
     # Fails with `TypeError: Constant constructor takes either 0 or 2 positional arguments`
     # deep in TF AutoGraph. Doesn't reproduce in Docker with Ubuntu 22.04 => might be related
     # to the differences in some of the dependencies?
@@ -171,10 +168,11 @@ buildPythonPackage rec {
     "tensorflow_datasets/text/c4_utils_test.py"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Library of datasets ready to use with TensorFlow";
     homepage = "https://www.tensorflow.org/datasets/overview";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ ndl ];
+    changelog = "https://github.com/tensorflow/datasets/releases/tag/v${version}";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ ndl ];
   };
 }
