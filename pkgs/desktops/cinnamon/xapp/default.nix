e0d69e6ba699dd527c8c870035722537b23e814e
Bobby Rong <rjl931189261@126.com>
2024-06-05 21:31:25 +0800

    cinnamon.xapp: 2.8.2 -> 2.8.3
    https://github.com/linuxmint/xapp/compare/2.8.2...2.8.3

--- pkgs/desktops/cinnamon/xapp/default.nix
+++ pkgs/desktops/cinnamon/xapp/default.nix
@@ -22,7 +22,7 @@
 
 stdenv.mkDerivation rec {
   pname = "xapp";
-  version = "2.8.2";
+  version = "2.8.3";
 
   outputs = [ "out" "dev" ];
 
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-n600mc8/4+bYUtYaHUnmr90ThVkngcu8Ft02iuSrWWQ=";
+    hash = "sha256-uWBHCsKcW0vP2HYHQWXPrCSr5NsN2nYWAzBByxJBVrY=";
   };
 
   # Recommended by upstream, which enables the build of xapp-debug.
