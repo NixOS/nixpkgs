211bf3746046062fb7071aa097d8b4b7671f9926
teutat3s <10206665+teutat3s@users.noreply.github.com>
2024-06-06 00:42:19 +0200

    element-desktop: 1.11.67 -> 1.11.68
    https://github.com/element-hq/element-desktop/releases/tag/v1.11.68

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5063,7 +5063,7 @@ with pkgs;
 
   element-desktop = callPackage ../applications/networking/instant-messengers/element/element-desktop.nix {
     inherit (darwin.apple_sdk.frameworks) Security AppKit CoreServices;
-    electron = electron_29;
+    electron = electron_30;
   };
   element-desktop-wayland = writeScriptBin "element-desktop" ''
     #!/bin/sh
