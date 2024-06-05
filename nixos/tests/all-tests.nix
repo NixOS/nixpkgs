1cfd74809b8342733e7b1ab453bb13d1dda49c87
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-06-05 15:14:56 -0300

    nixos/flood: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -328,6 +328,7 @@ in {
   firewall-nftables = handleTest ./firewall.nix { nftables = true; };
   fish = handleTest ./fish.nix {};
   flannel = handleTestOn ["x86_64-linux"] ./flannel.nix {};
+  flood = handleTest ./flood.nix {};
   floorp = handleTest ./firefox.nix { firefoxPackage = pkgs.floorp; };
   fluentd = handleTest ./fluentd.nix {};
   fluidd = handleTest ./fluidd.nix {};
