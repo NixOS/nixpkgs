4ced8cde6bfa443e1ee10c756d051a586ab6d3dd
eyjhb <eyjhbb@gmail.com>
2024-06-05 10:40:42 +0200

    arduino-mk: 1.6.0 -> 1.6.0-unstable-2021-08-02

--- pkgs/development/embedded/arduino/arduino-mk/default.nix
+++ pkgs/development/embedded/arduino/arduino-mk/default.nix
@@ -1,21 +1,29 @@
-{ stdenv, lib, fetchFromGitHub, python3Packages, installShellFiles }:
+{ stdenv, lib, fetchFromGitHub, fetchpatch, python3Packages, installShellFiles }:
 
 stdenv.mkDerivation rec {
-  version = "1.6.0";
+  version = "1.6.0-unstable-2021-08-02";
   pname = "arduino-mk";
 
   src = fetchFromGitHub {
     owner  = "sudar";
     repo   = "Arduino-Makefile";
-    rev    = version;
-    sha256 = "0flpl97d2231gp51n3y4qvf3y1l8xzafi1sgpwc305vwc2h4dl2x";
+    rev    = "a1fbda0c53a75862bd7ac6285b70103ed04f70a6";
+    sha256 = "sha256-sXFTkPv15RRzBna8eXeDjTYxLCDBiqBYqS2luwXN4kI=";
   };
+
+  patches = [
+    # removes grep warnings - https://github.com/sudar/Arduino-Makefile/pull/678
+    (fetchpatch {
+      url = "https://github.com/sudar/Arduino-Makefile/pull/678/commits/b03ad3402f556296bfddbbe3fb5ae0db6a886dce.patch";
+      sha256 = "sha256-CzXbvoZAS4DRqrgQU59zgrIw6I0bda72S0N9f1MDmq8=";
+    })
+  ];
 
   nativeBuildInputs = [ python3Packages.wrapPython installShellFiles ];
   propagatedBuildInputs = with python3Packages; [ pyserial ];
   installPhase = ''
     mkdir $out
-    cp -rT $src $out
+    cp -rT . $out
     installManPage *.1
   '';
   postFixupPhase = ''
