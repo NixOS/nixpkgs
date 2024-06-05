fb29eac5a8c4871cc9013301087e98e4d8b6d425
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-05 23:27:14 +0000

    wttrbar: 0.10.1 -> 0.10.2

--- pkgs/by-name/wt/wttrbar/package.nix
+++ pkgs/by-name/wt/wttrbar/package.nix
@@ -8,18 +8,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wttrbar";
-  version = "0.10.1";
+  version = "0.10.2";
 
   src = fetchFromGitHub {
     owner = "bjesus";
     repo = "wttrbar";
     rev = version;
-    hash = "sha256-rUiLB0M/dzNxzBPAqlGy5m/gOTGYw4GRzb+ud0l/1+8=";
+    hash = "sha256-lwlfarnu2PC5toAf6FAefnpxDlzzwphrP+zXRQ2DQeQ=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin (with darwin.apple_sdk_11_0.frameworks; [ Security SystemConfiguration ]);
 
-  cargoHash = "sha256-v415OJ6dmWSLUDeFUtd27mBaQlB3x1vC37sjpMhKyYY=";
+  cargoHash = "sha256-H3UpBRf97JsHudq6naZxMnpIzda0v7teDjgDdgluul0=";
 
   passthru.updateScript = nix-update-script { };
 
