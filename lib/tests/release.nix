013e398b0fbd51ef36930a91284156241300d38c
Tristan Ross <tristan.ross@midstall.com>
2024-06-06 00:07:09 -0700

    lib/tests/release.nix: make pure

--- lib/tests/release.nix
+++ lib/tests/release.nix
@@ -24,7 +24,10 @@ in
       #
       #   https://github.com/NixOS/nixpkgs/issues/272591
       #
-      [(import ../../pkgs/test/release {})]
+      [(import ../../pkgs/test/release {
+        inherit pkgs;
+        pkgs-path = pkgs.path;
+      })]
     ;
 
   }
