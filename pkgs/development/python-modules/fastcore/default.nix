cc4f1b24b9e1ece437dc211ce0adf6bb27280f59
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-06 08:11:05 +0200

    python312Packages.fastcore: 1.5.43 -> 1.5.44
    Diff: https://github.com/fastai/fastcore/compare/refs/tags/1.5.43...1.5.44
    
    Changelog: https://github.com/fastai/fastcore/blob/1.5.44/CHANGELOG.md

--- pkgs/development/python-modules/fastcore/default.nix
+++ pkgs/development/python-modules/fastcore/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "fastcore";
-  version = "1.5.43";
+  version = "1.5.44";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "fastai";
     repo = "fastcore";
     rev = "refs/tags/${version}";
-    hash = "sha256-81U5LEhReoNLtPqDe9WyX5BPc0H88cYUcx3kI7ssjr8=";
+    hash = "sha256-78PFSLqPmFgdKKYHGID7pHczCXvfFeyBG1vbZSoq7ts=";
   };
 
   build-system = [ setuptools ];
