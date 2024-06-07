0e50d6a1e25af2cd5e0d5acfa6797d0976f113a0
Yueh-Shun Li <shamrocklee@posteo.net>
2024-06-07 13:06:53 +0800

    losslesscut-bin: *-dmg: refactor
    Run pre- and post- hooks.
    
    Utilize $sourceRoot.

--- pkgs/applications/video/losslesscut-bin/build-from-dmg.nix
+++ pkgs/applications/video/losslesscut-bin/build-from-dmg.nix
@@ -24,10 +24,13 @@ stdenvNoCC.mkDerivation {
   sourceRoot = "LosslessCut.app";
 
   installPhase = ''
-    mkdir -p "$out/Applications/LosslessCut.app"
-    cp -R . "$out/Applications/LosslessCut.app"
+    runHook preInstall
+    mkdir -p "$out/Applications"
+    cd ..
+    mv "$sourceRoot" "$out/Applications"
     mkdir -p "$out/bin"
-    ln -s "$out/Applications/LosslessCut.app/Contents/MacOS/LosslessCut" "$out/bin/losslesscut"
+    ln -s "$out/Applications/$(basename "$sourceRoot")/Contents/MacOS/LosslessCut" "$out/bin/losslesscut"
+    runHook postInstall
   '';
 
   meta = metaCommon // (with lib; {
