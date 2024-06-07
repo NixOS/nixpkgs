3996a2be67b57fde121327dba3bcb802719f0147
MilesCranmer <miles.cranmer@gmail.com>
2024-06-07 04:17:12 +0100

    leave on warning

--- pkgs/development/compilers/julia/generic.nix
+++ pkgs/development/compilers/julia/generic.nix
@@ -71,7 +71,7 @@ stdenv.mkDerivation rec {
 
   NIX_CFLAGS_COMPILE = [] ++ lib.optionals stdenv.isDarwin [
     "-Wno-error=implicit-function-declaration"
-    "-Wno-elaborated-enum-base"
+    "-Wno-error=elaborated-enum-base"
   ];
   # TODO: figure out how to re-enable these as errors?
 
