2be5bbe055bed0e89de821fe9b879b49b0dbf805
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-07 08:43:26 +0200

    python312Packages.adafruit-platformdetect: 3.68.0 -> 3.69.0
    Changelog: https://github.com/adafruit/Adafruit_Python_PlatformDetect/releases/tag/3.69.0

--- pkgs/development/python-modules/adafruit-platformdetect/default.nix
+++ pkgs/development/python-modules/adafruit-platformdetect/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "adafruit-platformdetect";
-  version = "3.68.0";
+  version = "3.69.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "adafruit_platformdetect";
     inherit version;
-    hash = "sha256-wJgnwe8szhLMQWABiWC8nluuJICezuhy//OMeMIjftk=";
+    hash = "sha256-J+4VSA+2xZCNoLrICNXpmIG8tU6cnOn4EjEnN1VFGYU=";
   };
 
   build-system = [ setuptools-scm ];
