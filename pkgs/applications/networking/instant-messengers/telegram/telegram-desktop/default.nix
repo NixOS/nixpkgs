bebf27e522546756fce284903f8038c6ac5f0f23
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-07 11:05:25 +0000

    telegram-desktop: 5.1.2 -> 5.1.5

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/default.nix
@@ -64,14 +64,14 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "telegram-desktop";
-  version = "5.1.2";
+  version = "5.1.5";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-KTgID7pd0QSFi5t9cdIVEi4/oQirDgsf7tTcEEtRdY0=";
+    hash = "sha256-dlPt87SKP17v9QFiRZSSUtSLVGTGbeo05G/p05l2zVc=";
   };
 
   patches = [
