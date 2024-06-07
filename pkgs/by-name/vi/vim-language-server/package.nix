5f72217808e050941b044a23fb58b221e2fb7b05
Doron Behar <doron.behar@gmail.com>
2024-06-07 16:22:57 +0300

    vim-language-server: use to yarn{Build,Config}Hook

--- pkgs/by-name/vi/vim-language-server/package.nix
+++ pkgs/by-name/vi/vim-language-server/package.nix
@@ -1,37 +1,40 @@
 { lib
-, mkYarnPackage
+, stdenv
 , fetchFromGitHub
 , fetchYarnDeps
+, yarnConfigHook
+, yarnBuildHook
+, nodejs
+, npmHooks
 }:
 
-mkYarnPackage rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "vim-language-server";
   version = "2.3.1";
 
   src = fetchFromGitHub {
     owner = "iamcco";
     repo = "vim-language-server";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-NfBKNCTvCMIJrSiTlCG+LtVoMBMdCc3rzpDb9Vp2CGM=";
   };
 
-  packageJSON = ./package.json;
   offlineCache = fetchYarnDeps {
-    yarnLock = src + "/yarn.lock";
+    yarnLock = finalAttrs.src + "/yarn.lock";
     hash = "sha256-mo8urQaWIHu33+r0Y7mL9mJ/aSe/5CihuIetTeDHEUQ=";
   };
 
-  buildPhase = ''
-    runHook preBuild
-
-    export HOME=$(mktemp -d)
-    # https://stackoverflow.com/a/69699772/4935114
-    env NODE_OPTIONS=--openssl-legacy-provider yarn --offline build
-
-    runHook postBuild
+  nativeBuildInputs = [
+    yarnConfigHook
+    yarnBuildHook
+    # Needed for executing package.json scripts
+    nodejs
+    npmHooks.npmInstallHook
+  ];
+  # https://stackoverflow.com/a/69699772/4935114
+  preBuild = ''
+    export NODE_OPTIONS=--openssl-legacy-provider
   '';
-
-  doDist = false;
 
   meta = with lib; {
     description = "VImScript language server, LSP for vim script";
@@ -40,4 +43,4 @@ mkYarnPackage rec {
     maintainers = with maintainers; [ doronbehar ];
     mainProgram = "vim-language-server";
   };
-}
+})
