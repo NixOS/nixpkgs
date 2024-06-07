771a1b06b930fa178cdc2ce9a3fefeed6f3b8e8a
Daru <mica.tui@proton.me>
2024-06-07 05:07:31 +0200

    lyssa: init at unstable-2024-06-01

---
+++ pkgs/by-name/ly/lyssa/package.nix
@@ -0,0 +1,60 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  pkg-config,
+  taglib,
+  cglm,
+  libclipboard,
+  libxcb,
+  makeWrapper,
+  python3Packages,
+  ffmpeg,
+  yt-dlp,
+  miniaudio,
+  jq,
+  glfw,
+  exiftool,
+  stb,
+  libGL
+}:
+
+stdenv.mkDerivation {
+  name = "lyssa";
+  version = "unstable-2024-06-01";
+
+  src = fetchFromGitHub {
+    owner = "cococry";
+    repo = "lyssa";
+    rev = "a7ebb786b005d9f716dfa99869e29660ca14c749";
+    hash = "sha256-jJx7V/ru6quvZhqkVK3OGguH81882fQXNdpLVp+g5sk=";
+  };
+
+  buildInputs = [ cglm libclipboard libxcb glfw ];
+  nativeBuildInputs = [ pkg-config taglib makeWrapper ];
+
+  postFixup = ''
+      wrapProgram $out/bin/lyssa \
+      --prefix PATH : ${
+      lib.makeBinPath [ jq exiftool stb miniaudio yt-dlp ffmpeg python3Packages.glad ]
+    }
+'';
+
+  installPhase = ''
+    mkdir -p $out/{bin,share/{applications,icons}}
+
+
+    install -Dm755 bin/lyssa -t $out/bin
+    install -D Lyssa.desktop -t $out/share/applications
+    mkdir -p $out/share/icons
+    cp -r logo $out/share/icons/lyssa
+  '';
+
+  meta = {
+    description = "Music player designed to be an aesthetic addition for every desktop";
+    homepage = "https://github.com/cococry/lyssa";
+    maintainers = with lib.maintainers; [ daru-san ];
+    mainProgram = "lyssa";
+    platforms = lib.platforms.linux;
+  };
+}
