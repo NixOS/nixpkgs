49531ab4b11afac753216d959e0b2ca903d187f6
kilimnik <mail@kilimnik.de>
2024-06-08 21:36:19 +0200

    python3Packages.scienceplots: init at 2.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14107,6 +14107,8 @@ self: super: with self; {
 
   schwifty = callPackage ../development/python-modules/schwifty { };
 
+  scienceplots = callPackage ../development/python-modules/scienceplots { };
+
   scim2-filter-parser = callPackage ../development/python-modules/scim2-filter-parser { };
 
   scikit-bio = callPackage ../development/python-modules/scikit-bio { };
