96f4c83a7170dc924c2ba2d4432ceff040d94a3c
Nick Cao <nickcao@nichi.co>
2024-06-08 09:24:08 -0400

    Revert "ffado: fix hash, see https://ffado.org/posts/ffado-2.4.8-tarball_fix/"

--- pkgs/os-specific/linux/ffado/default.nix
+++ pkgs/os-specific/linux/ffado/default.nix
@@ -29,8 +29,10 @@ mkDerivation rec {
 
   src = fetchurl {
     url = "http://www.ffado.org/files/libffado-${version}.tgz";
-    hash = "sha256-0iFXYyGctOoHCdc232Ud80/wV81tiS7ItiS0uLKyq2Y=";
+    hash = "sha256-f0x561ehKw6uMSri0RZip+v1JHZuhixtywl0PVU/N44=";
   };
+
+  sourceRoot = "libffado-${version}/libffado";
 
   prePatch = ''
     substituteInPlace ./support/tools/ffado-diag.in \
