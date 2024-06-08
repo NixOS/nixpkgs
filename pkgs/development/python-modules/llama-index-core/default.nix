6ee2101a0db602ec7a3dd5b3752b2cb5068db2cc
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-08 09:33:07 +0000

    python311Packages.llama-index: 0.10.42 -> 0.10.43

--- pkgs/development/python-modules/llama-index-core/default.nix
+++ pkgs/development/python-modules/llama-index-core/default.nix
@@ -46,7 +46,7 @@ in
 
 buildPythonPackage rec {
   pname = "llama-index-core";
-  version = "0.10.42";
+  version = "0.10.43";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -55,7 +55,7 @@ buildPythonPackage rec {
     owner = "run-llama";
     repo = "llama_index";
     rev = "refs/tags/v${version}";
-    hash = "sha256-DdD0gllACmwtXDQutjaKQmB8OpCBu+jgQ963g0dNW2c=";
+    hash = "sha256-eqT3PR1yOrMU/lWGOr70OcekTeHveDfM9C8VOJEx1ps=";
   };
 
   sourceRoot = "${src.name}/${pname}";
