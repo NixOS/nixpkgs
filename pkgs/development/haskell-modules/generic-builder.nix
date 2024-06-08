a2f60bb52e86be09e79e7f2078f3c8972d7d8cfc
Niklas Hambüchen <mail@nh2.me>
2024-06-08 19:58:32 +0200

    haskell: Don't use package-qualified and non-package-qualified grep

--- pkgs/development/haskell-modules/generic-builder.nix
+++ pkgs/development/haskell-modules/generic-builder.nix
@@ -24,7 +24,7 @@ let
 
   inherit (buildPackages)
     fetchurl removeReferencesTo
-    pkg-config coreutils gnugrep glibcLocales
+    pkg-config coreutils glibcLocales
     emscripten;
 
 in
@@ -569,7 +569,7 @@ stdenv.mkDerivation ({
     echo configureFlags: $configureFlags
     ${setupCommand} configure $configureFlags 2>&1 | ${coreutils}/bin/tee "$NIX_BUILD_TOP/cabal-configure.log"
     ${lib.optionalString (!allowInconsistentDependencies) ''
-      if ${gnugrep}/bin/egrep -q -z 'Warning:.*depends on multiple versions' "$NIX_BUILD_TOP/cabal-configure.log"; then
+      if grep -E -q -z 'Warning:.*depends on multiple versions' "$NIX_BUILD_TOP/cabal-configure.log"; then
         echo >&2 "*** abort because of serious configure-time warning from Cabal"
         exit 1
       fi
