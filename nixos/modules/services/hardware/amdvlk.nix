8847c3669e1a18592c80c6f4fe2b88655e847e3c
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-06-08 09:49:19 +0530

    nixos/amdvlk: add settings option

--- nixos/modules/services/hardware/amdvlk.nix
+++ nixos/modules/services/hardware/amdvlk.nix
@@ -12,6 +12,22 @@ in {
 
     support32Bit.enable = lib.mkEnableOption "32-bit driver support";
     support32Bit.package = lib.mkPackageOption pkgs.driversi686Linux "amdvlk" { };
+
+    settings = lib.mkOption {
+      type = lib.types.attrs;
+      default = { };
+      example = {
+        AllowVkPipelineCachingToDisk = 1;
+        ShaderCacheMode = 1;
+        IFH = 0;
+        EnableVmAlwaysValid = 1;
+        IdleAfterSubmitGpuMask = 1;
+      };
+      description = ''
+        Runtime settings for AMDVLK to be configured {file}`/etc/amd/amdVulkanSettings.cfg`.
+        See [AMDVLK GitHub page](https://github.com/GPUOpen-Drivers/AMDVLK?tab=readme-ov-file#runtime-settings).
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -31,6 +47,15 @@ in {
 
     environment.sessionVariables = lib.mkIf cfg.supportExperimental.enable {
       AMDVLK_ENABLE_DEVELOPING_EXT = "all";
+    };
+
+    environment.etc = lib.mkIf (cfg.settings != { }) {
+      "amd/amdVulkanSettings.cfg".text = lib.concatStrings
+        (lib.mapAttrsToList
+          (n: v: ''
+            ${n},${builtins.toString v}
+          '')
+          cfg.settings);
     };
   };
 
