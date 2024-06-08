685f79a6814c49cd45a95f6ea290fb94916f2a08
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-08 18:00:50 +0000

    python311Packages.azure-synapse-artifacts: 0.18.0 -> 0.19.0

--- pkgs/development/python-modules/azure-synapse-artifacts/default.nix
+++ pkgs/development/python-modules/azure-synapse-artifacts/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "azure-synapse-artifacts";
-  version = "0.18.0";
+  version = "0.19.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-qgiHMzW7T5NuQl0jwZheMXp7wHhNOHyH24J6wfc4VqQ=";
+    hash = "sha256-UvCSsiZ315IoDwvMI02JLJ9zjpPI4Ut0wZUQG5uicYQ=";
   };
 
   propagatedBuildInputs = [
