c856eeeae63f287a26dce2b6040c5869859d4de8
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-08 01:20:15 +0000

    iroh: 0.17.0 -> 0.18.0

--- pkgs/applications/networking/iroh/default.nix
+++ pkgs/applications/networking/iroh/default.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "iroh";
-  version = "0.17.0";
+  version = "0.18.0";
 
   src = fetchFromGitHub {
     owner = "n0-computer";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-CemVlw0S4UmKFopWSh1Hv2znMRN3guIBx303esK/gLw=";
+    hash = "sha256-feH+H/TjZEYVyw4JnIdOkptQ/J0vASouyE7Ctsg0BvE=";
   };
 
-  cargoHash = "sha256-LgA6gdBRLsow98nv9A5rYb/v/FGzu/LPQwftyeuKHk4=";
+  cargoHash = "sha256-tyFuzNkorFvFJ7BMNo2Z+M3EZNniDLKa7cK3UmlEjxg=";
 
   buildInputs = lib.optionals stdenv.isDarwin (
     with darwin.apple_sdk.frameworks; [
