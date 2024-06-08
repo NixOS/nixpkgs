277c8578ac2cfcee3b7c063839d4d9309d3806c3
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-08 02:57:21 +0000

    upbound: 0.30.0 -> 0.31.0

--- pkgs/by-name/up/upbound/package.nix
+++ pkgs/by-name/up/upbound/package.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "upbound";
-  version = "0.30.0";
+  version = "0.31.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = "up";
     rev = "v${version}";
-    sha256 = "sha256-KAWyHlqgj4xz3abSSa1cezpeDTmJbGSZAMffq3p/CyI=";
+    sha256 = "sha256-nXP97J0DdcXPAD/gB3py5UXhe48Kaec7CRTHmalo+RQ=";
   };
 
-  vendorHash = "sha256-4o1WJHGHiO9r8RC8hr4KWZBiC8h52la1gKdYaYxEjbk=";
+  vendorHash = "sha256-9HnKpofJPxCieG5thNcLC7diA51ohzy7xgtniL7FEgo=";
 
   subPackages = [ "cmd/docker-credential-up" "cmd/up" ];
 
