8acd6f32b72981bace44297f8ec43be19a518609
ppom <ppom@ppom.me>
2024-06-08 12:00:00 +0200

    reaction: 1.3.1 → 1.4.1

--- pkgs/by-name/re/reaction/package.nix
+++ pkgs/by-name/re/reaction/package.nix
@@ -5,7 +5,7 @@
   fetchFromGitLab,
 }:
 let
-  version = "1.3.1";
+  version = "1.4.1";
 in buildGoModule {
   inherit version;
   pname = "reaction";
@@ -15,7 +15,7 @@ in buildGoModule {
     owner = "ppom";
     repo = "reaction";
     rev = "v${version}";
-    sha256 = "sha256-hBEtXaTpubb5sKSrA8bhw3MW6YLszuESWrFZYf/+RvM=";
+    sha256 = "sha256-UL3ck+gejZAu/mZS3ZiZ78a2/I+OesaSRZUhHirgu9o=";
   };
 
   vendorHash = "sha256-THUIoWFzkqaTofwH4clBgsmtUlLS9WIB2xjqW7vkhpg=";
