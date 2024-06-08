164f03d97108aec32ffd1988f636351f61239e5c
natsukium <tomoya.otabi@gmail.com>
2024-06-08 19:52:35 +0900

    python311Packages.mediapy: 1.2.1 -> 1.2.2
    Changelog: https://github.com/google/mediapy/releases/tag/v1.2.2

--- pkgs/development/python-modules/mediapy/default.nix
+++ pkgs/development/python-modules/mediapy/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "mediapy";
-  version = "1.2.1";
+  version = "1.2.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-jINA5J69gPywaVGcOOIrgVwlss4nF4I6qk2W34blxK4=";
+    hash = "sha256-QtmhqpPBg1ULgk27Tw3l2mGqXITbjwHwY6zR8juQ7wo=";
   };
 
   nativeBuildInputs = [ flit-core ];
