57411e2649a33719d991b647eed34b63e1d76c3c
Nikolay Korotkiy <sikmir@disroot.org>
2024-06-08 20:28:19 +0400

    josm: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32019,8 +32019,6 @@ with pkgs;
 
   johnny = callPackage ../applications/misc/johnny { };
 
-  josm = callPackage ../applications/misc/josm { };
-
   js8call = qt5.callPackage ../applications/radio/js8call { };
 
   jwm = callPackage ../applications/window-managers/jwm { };
