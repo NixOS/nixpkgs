f768f3f2bae317d4b502bf4aa937a69de9138234
Luis David Licea Torres <luislicea@hotmail.com>
2024-06-09 14:07:46 -0500

    List modules starting with l in alphabetical order

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -219,9 +219,8 @@
   ./programs/kbdlight.nix
   ./programs/kclock.nix
   ./programs/kdeconnect.nix
-  ./programs/ladybird.nix
-  ./programs/lazygit.nix
   ./programs/kubeswitch.nix
+  ./programs/ladybird.nix
   ./programs/lazygit.nix
   ./programs/less.nix
   ./programs/liboping.nix
