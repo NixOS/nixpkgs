f6c8f4adf6765ade77e2bd90d5b17f8e0d5235b6
Raito Bezarius <masterancpp@gmail.com>
2024-06-09 18:53:21 +0200

    lix-doc: support path `src` instead of attribute `src`
    If you pass `src = ./.;`, this should work.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- pkgs/tools/package-management/lix/doc/default.nix
+++ pkgs/tools/package-management/lix/doc/default.nix
@@ -8,6 +8,6 @@
 
 rustPlatform.buildRustPackage {
   pname = "lix-doc";
-  sourceRoot = "${src.name}/lix-doc";
+  sourceRoot = "${src.name or src}/lix-doc";
   inherit version src cargoHash cargoLock;
 }
