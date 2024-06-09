0085639befa85ba8f8c713e732a6fdc29fb62130
Luis David Licea Torres <luislicea@hotmail.com>
2024-06-09 13:42:19 -0500

    Spawn localsend_app instead of localsend

--- nixos/tests/localsend.nix
+++ nixos/tests/localsend.nix
@@ -1,16 +1,16 @@
-import ./make-test-python.nix ({ lib, pkgs, ... }: {
+import ./make-test-python.nix ({ ... }: {
   name = "localsend";
 
-  nodes.machine = { config, pkgs, ... }: {
+  nodes.machine = { ... }: {
     imports = [ ./common/x11.nix ];
     programs.localsend.enable = true;
   };
 
   testScript = ''
     machine.wait_for_x()
-    machine.execute("localsend")
-    machine.wait_for_window("LocalSend", 10)
+    machine.succeed("localsend_app >&2 &")
     machine.wait_for_open_port(53317)
+    machine.wait_for_window("LocalSend", 10)
     machine.succeed("netstat --listening --program --tcp | grep -P 'tcp.*53317.*localsend'")
   '';
 })
