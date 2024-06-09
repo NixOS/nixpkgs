c839d808573bc5cf007c03a9e437ff90233bd1e8
Anderson Torres <torres.anderson.85@protonmail.com>
2024-06-09 19:53:17 -0300

    python3Packages.mpv: get rid of rec

--- pkgs/development/python-modules/mpv/default.nix
+++ pkgs/development/python-modules/mpv/default.nix
@@ -7,39 +7,42 @@
   stdenv,
 }:
 
-buildPythonPackage rec {
-  pname = "mpv";
-  version = "1.0.6";
-  format = "pyproject";
+let
+  argset = {
+    pname = "mpv";
+    version = "1.0.6";
+    format = "pyproject";
 
-  src = fetchFromGitHub {
-    owner = "jaseg";
-    repo = "python-mpv";
-    rev = "v${version}";
-    hash = "sha256-1axVJ8XXs0ZPgsVux3+6YUm1KttLceZyyHOuUEHIFl4=";
+    src = fetchFromGitHub {
+      owner = "jaseg";
+      repo = "python-mpv";
+      rev = "v${argset.version}";
+      hash = "sha256-1axVJ8XXs0ZPgsVux3+6YUm1KttLceZyyHOuUEHIFl4=";
+    };
+
+    nativeBuildInputs = [ setuptools ];
+
+    buildInputs = [ mpv ];
+
+    postPatch = ''
+      substituteInPlace mpv.py \
+        --replace "sofile = ctypes.util.find_library('mpv')" \
+                  'sofile = "${mpv}/lib/libmpv${stdenv.hostPlatform.extensions.sharedLibrary}"'
+    '';
+
+    # tests impure, will error if it can't load libmpv.so
+    doCheck = false;
+    pythonImportsCheck = [ "mpv" ];
+
+    meta = {
+      homepage = "https://github.com/jaseg/python-mpv";
+      description = "A python interface to the mpv media player";
+      license = lib.licenses.agpl3Plus;
+      maintainers = with lib.maintainers; [
+        onny
+        AndersonTorres
+      ];
+    };
   };
-
-  nativeBuildInputs = [ setuptools ];
-
-  buildInputs = [ mpv ];
-
-  postPatch = ''
-    substituteInPlace mpv.py \
-      --replace "sofile = ctypes.util.find_library('mpv')" \
-                'sofile = "${mpv}/lib/libmpv${stdenv.hostPlatform.extensions.sharedLibrary}"'
-  '';
-
-  # tests impure, will error if it can't load libmpv.so
-  doCheck = false;
-  pythonImportsCheck = [ "mpv" ];
-
-  meta = {
-    homepage = "https://github.com/jaseg/python-mpv";
-    description = "A python interface to the mpv media player";
-    license = lib.licenses.agpl3Plus;
-    maintainers = with lib.maintainers; [
-      onny
-      AndersonTorres
-    ];
-  };
-}
+in
+buildPythonPackage argset
