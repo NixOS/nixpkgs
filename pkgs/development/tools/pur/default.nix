67a3e1953b06d3aaef4acba7817357f3f0f2a122
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-09 11:09:51 +0200

    pur: refactor

--- pkgs/development/tools/pur/default.nix
+++ pkgs/development/tools/pur/default.nix
@@ -6,7 +6,7 @@
 python3.pkgs.buildPythonApplication rec {
   pname = "pur";
   version = "7.3.2";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "alanhamlett";
@@ -15,7 +15,11 @@ python3.pkgs.buildPythonApplication rec {
     hash = "sha256-XLI9U9ej3+tS0zzmCDGwZ0pAb3mKnrqBtm90f5N6rMw=";
   };
 
-  propagatedBuildInputs = with python3.pkgs; [
+  build-system = with python3.pkgs; [
+    setuptools
+  ];
+
+  dependencies = with python3.pkgs; [
     click
   ];
 
@@ -29,10 +33,10 @@ python3.pkgs.buildPythonApplication rec {
 
   meta = with lib; {
     description = "Python library for update and track the requirements";
-    mainProgram = "pur";
     homepage = "https://github.com/alanhamlett/pip-update-requirements";
     changelog = "https://github.com/alanhamlett/pip-update-requirements/blob/${version}/HISTORY.rst";
-    license = with licenses; [ bsd2 ];
+    license = licenses.bsd2;
     maintainers = with maintainers; [ fab ];
+    mainProgram = "pur";
   };
 }
