1055046ddd3b9aed51fda477bac586994184d874
zendo <linzway@qq.com>
2024-06-09 15:01:05 +0800

    clash-nyanpasu: don't override other package

--- pkgs/by-name/cl/clash-nyanpasu/package.nix
+++ pkgs/by-name/cl/clash-nyanpasu/package.nix
@@ -1,12 +1,18 @@
-{ lib
-, clash-verge
-, mihomo
-, fetchurl
+{
+  lib,
+  stdenv,
+  fetchurl,
+  dpkg,
+  wrapGAppsHook3,
+  autoPatchelfHook,
+  clash-meta,
+  openssl,
+  webkitgtk,
+  udev,
+  libayatana-appindicator,
 }:
 
-(clash-verge.override {
-  clash-meta = mihomo;
-}).overrideAttrs (old: rec {
+stdenv.mkDerivation rec {
   pname = "clash-nyanpasu";
   version = "1.4.5";
 
@@ -15,9 +21,44 @@
     hash = "sha256-cxaq7Rndf0ytEaqc7CGQix5SOAdsTOoTj1Jlhjr5wEA=";
   };
 
-  meta = old.meta // (with lib; {
-    homepage = "https://github.com/keiko233/clash-nyanpasu";
-    maintainers = with maintainers; [ Guanran928 ];
+  nativeBuildInputs = [
+    dpkg
+    wrapGAppsHook3
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    openssl
+    webkitgtk
+    stdenv.cc.cc
+  ];
+
+  runtimeDependencies = [
+    (lib.getLib udev)
+    libayatana-appindicator
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    mv usr/* $out
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    rm -f $out/bin/clash
+    ln -sf ${lib.getExe clash-meta} $out/bin/${clash-meta.meta.mainProgram}
+  '';
+
+  meta = with lib; {
+    description = "Clash GUI based on tauri";
+    homepage = "https://github.com/LibNyanpasu/clash-nyanpasu";
+    license = licenses.gpl3Plus;
     mainProgram = "clash-nyanpasu";
-  });
-})
+    platforms = [ "x86_64-linux" ];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    maintainers = with maintainers; [ Guanran928 ];
+  };
+}
