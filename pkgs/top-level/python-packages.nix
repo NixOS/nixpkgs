77927b3fe3c591e73e856bd1eaf7a10ab4e1bffd
Nikolay Korotkiy <sikmir@disroot.org>
2024-06-09 10:12:48 +0400

    python3Packages.inequality: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5787,6 +5787,8 @@ self: super: with self; {
 
   indexed-zstd = callPackage ../development/python-modules/indexed-zstd { inherit (pkgs) zstd; };
 
+  inequality = callPackage ../development/python-modules/inequality { };
+
   infinity = callPackage ../development/python-modules/infinity { };
 
   inflect = callPackage ../development/python-modules/inflect { };
