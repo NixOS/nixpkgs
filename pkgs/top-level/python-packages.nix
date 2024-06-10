d9ceff13d81e7d0bf81be24bcddaabb862b18cbf
Andrei Mahalean <andrei@maha.nz>
2024-06-11 01:52:38 +1200

    azure-ai-textanalytics: init at 5.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1074,6 +1074,8 @@ self: super: with self; {
 
   axisregistry = callPackage ../development/python-modules/axisregistry { };
 
+  azure-ai-textanalytics = callPackage ../development/python-modules/azure-ai-textanalytics { };
+
   azure-appconfiguration = callPackage ../development/python-modules/azure-appconfiguration { };
 
   azure-applicationinsights = callPackage ../development/python-modules/azure-applicationinsights { };
