2f3952d525088d798e5ac75d4f4b361ba6be673b
Nick Cao <nickcao@nichi.co>
2024-06-10 07:32:38 -0400

    keycloak: 24.0.5 -> 25.0.1
    Diff: https://github.com/keycloak/keycloak/compare/24.0.5...25.0.1
    (cherry picked from commit 134a223f3fda26847e51cbe92db9b06a8142e322)

--- pkgs/servers/keycloak/default.nix
+++ pkgs/servers/keycloak/default.nix
@@ -18,11 +18,11 @@ let
   '';
 in stdenv.mkDerivation rec {
   pname = "keycloak";
-  version = "24.0.5";
+  version = "25.0.1";
 
   src = fetchzip {
     url = "https://github.com/keycloak/keycloak/releases/download/${version}/keycloak-${version}.zip";
-    hash = "sha256-lf1miVEGQvPbmlOZMCXUyX/pKE+JoJFawhjVEPJDJ6s=";
+    hash = "sha256-gwnBCH65s8KGca2FNBxfBfNKox9OFTN3oEAPcYhSx9o=";
   };
 
   nativeBuildInputs = [ makeWrapper jre ];
