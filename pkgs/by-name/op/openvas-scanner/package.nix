bbb81381a8fff1f1fc22199edaac26b3f3259ea7
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-10 12:05:56 +0000

    openvas-scanner: 23.3.2 -> 23.4.0

--- pkgs/by-name/op/openvas-scanner/package.nix
+++ pkgs/by-name/op/openvas-scanner/package.nix
@@ -31,13 +31,13 @@
 
 stdenv.mkDerivation rec {
   pname = "openvas-scanner";
-  version = "23.3.2";
+  version = "23.4.0";
 
   src = fetchFromGitHub {
     owner = "greenbone";
     repo = "openvas-scanner";
     rev = "refs/tags/v${version}";
-    hash = "sha256-KlWO5Cik380pHWC4Lo7eE47z0tNcnbmHIO0J07UrYlg=";
+    hash = "sha256-8/a8OkaulT0uZb3CllLo0CWqloVbYF/6CY7a/Xn48T8=";
   };
 
   nativeBuildInputs = [
