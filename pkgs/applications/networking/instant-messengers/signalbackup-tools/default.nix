6437209e2a964ddb1e13f13a1a9fa6bf39c804e6
Malo Bourgon <mbourgon@gmail.com>
2024-06-11 10:55:38 -0700

    signalbackup-tools: 20240604 -> 20240610
    Diff: https://github.com/bepaald/signalbackup-tools/compare/20240604...20240610

--- pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
+++ pkgs/applications/networking/instant-messengers/signalbackup-tools/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "signalbackup-tools";
-  version = "20240604";
+  version = "20240610";
 
   src = fetchFromGitHub {
     owner = "bepaald";
     repo = pname;
     rev = version;
-    hash = "sha256-s7nldRrwi17ByuUyBMka39HCL4PWVM8qD7y6GfGhOqk=";
+    hash = "sha256-txtOCw7fakQavzpTJrVbelZdEw2mYNe9lb8p1zH6dyw=";
   };
 
   postPatch = ''
