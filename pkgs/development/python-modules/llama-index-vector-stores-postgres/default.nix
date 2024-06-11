5cf25475aee4675df9a89bd133d7b9e6556f80c6
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-11 16:20:20 +0000

    python311Packages.llama-index-vector-stores-postgres: 0.1.9 -> 0.1.10

--- pkgs/development/python-modules/llama-index-vector-stores-postgres/default.nix
+++ pkgs/development/python-modules/llama-index-vector-stores-postgres/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-vector-stores-postgres";
-  version = "0.1.9";
+  version = "0.1.10";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_index_vector_stores_postgres";
     inherit version;
-    hash = "sha256-gBa5Z6FaPTFw75CtzY0o/kaXr+3xbOaXQ62qbvue1Ko=";
+    hash = "sha256-mtEAl4e3NAx5rILQu7BAv/4+kxth5IybHeKUAwFZQbs=";
   };
 
   pythonRemoveDeps = [ "psycopg2-binary" ];
