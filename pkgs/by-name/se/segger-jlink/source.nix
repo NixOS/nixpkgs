39dbb38aa7bbfdd75303b11d95101dea06a405d7
h7x4 <h7x4@nani.wtf>
2024-06-12 17:39:39 +0200

    segger-jlink: 796b -> 796k

--- pkgs/by-name/se/segger-jlink/source.nix
+++ pkgs/by-name/se/segger-jlink/source.nix
@@ -1,19 +1,19 @@
 {
-  version = "796b";
+  version = "796k";
   x86_64-linux = {
     name = "x86_64";
-    hash = "sha256-UsDP+wMS7ZeWMQBObwv5RxbwuWU8nLnHes7LEXK6imE=";
+    hash = "sha256-GDmdKjMD9nJLPl4Qaxgrb5+b9CsUdyNqNak1JG4ERjo=";
   };
   i686-linux = {
     name = "i386";
-    hash = "sha256-InNHXWAc6QZEWyEcTTUCRMDsKd0RtR8d7O0clWKuFo8=";
+    hash = "sha256-8j2UkPtDyIA+mfXl4jIVp89CpGA+T4eU5IQ0GwROgwU=";
   };
   aarch64-linux = {
     name = "arm64";
-    hash = "sha256-ueIGdqfuIRCuEwaPkgZMgghO9DU11IboLLMryg/mxQ8=";
+    hash = "sha256-FIzg7vAPlhnjuKEm5uGa7a37srp1U0e4eqlG9C6q26s=";
   };
   armv7l-linux = {
     name = "arm";
-    hash = "sha256-6nTQGQpkbqQntheQqiUAdVS4rp30nl2KRUn5Adsfeoo=";
+    hash = "sha256-NceqkV54QVXEJr4pJ3nvY3zxSYb9Er0uQWQ4vaojkv8=";
   };
 }
