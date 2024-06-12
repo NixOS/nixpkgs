4c6953ca52c3440b77912ee3cb9995c5906a51ed
Colin <colin@uninsane.org>
2024-06-12 07:30:00 +0000

    whalebird: remove uninsane from maintainers

--- pkgs/applications/misc/whalebird/default.nix
+++ pkgs/applications/misc/whalebird/default.nix
@@ -107,7 +107,7 @@ stdenv.mkDerivation rec {
     homepage = "https://whalebird.social";
     changelog = "https://github.com/h3poteto/whalebird-desktop/releases/tag/v${version}";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ wolfangaukang colinsane weathercold ];
+    maintainers = with maintainers; [ wolfangaukang weathercold ];
     platforms = [ "x86_64-linux" "aarch64-linux" ];
   };
 }
