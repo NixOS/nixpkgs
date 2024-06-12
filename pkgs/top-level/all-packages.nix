3c4eb11a2972dd173d783f1e6c6a73b80eb88356
Zitrone <nix@dev.quantenzitrone.eu>
2024-06-12 07:56:29 +0200

    DisnixWebService: rename to disnix-web-service

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39718,10 +39718,6 @@ with pkgs;
 
   disnixos = callPackage ../tools/package-management/disnix/disnixos { };
 
-  DisnixWebService = callPackage ../tools/package-management/disnix/DisnixWebService {
-    jdk = jdk8;
-  };
-
   i3a = callPackage ../misc/i3a { };
 
   lice = python3Packages.callPackage ../tools/misc/lice { };
