6e302492e7f665ad4a6617118dd9059f86b4b5d3
h7x4 <h7x4@nani.wtf>
2024-06-12 17:38:33 +0200

    segger-jlink: add `headless` variant

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12680,6 +12680,8 @@ with pkgs;
 
   seehecht = callPackage ../tools/text/seehecht { };
 
+  segger-jlink-headless = callPackage ../by-name/se/segger-jlink/package.nix { headless = true; };
+
   selectdefaultapplication = libsForQt5.callPackage ../applications/misc/selectdefaultapplication { };
 
   semantic-release = callPackage ../development/tools/semantic-release {
