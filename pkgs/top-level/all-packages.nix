16d1664b1fca2d6669efee61204a43dc9d1abe86
Zitrone <nix@dev.quantenzitrone.eu>
2024-06-12 08:13:50 +0200

    SkypeExport: rename to skypeexport

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34221,8 +34221,6 @@ with pkgs;
 
   skypeforlinux = callPackage ../applications/networking/instant-messengers/skypeforlinux { };
 
-  SkypeExport = callPackage ../applications/networking/instant-messengers/SkypeExport { };
-
   slingshot = callPackage ../tools/misc/slingshot { };
 
   slop = callPackage ../tools/misc/slop { };
