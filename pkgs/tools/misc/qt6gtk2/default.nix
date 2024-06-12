639edea59fde14186fe911e5be29e34dd9e22978
José Romildo <malaquias@gmail.com>
2024-06-12 07:25:51 -0300

    kdePackages.qt6gtk2: 0.2 -> 0.2-unstable-2024-05-06
    Diff: https://github.com/trialuser02/qt6gtk2/compare/0.2...d29ba6c1fb4ac933ed7b91f0480cbd0c5a975ab8

--- pkgs/tools/misc/qt6gtk2/default.nix
+++ pkgs/tools/misc/qt6gtk2/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "qt6gtk2";
-  version = "0.2";
+  version = "0.2-unstable-2024-05-06";
 
   src = fetchFromGitHub {
     owner = "trialuser02";
     repo = finalAttrs.pname;
-    rev = finalAttrs.version;
-    hash = "sha256-g5ZCwTnNEJJ57zEwNqMxrl0EWYJMt3PquZ2IsmxQYqk=";
+    rev = "d29ba6c1fb4ac933ed7b91f0480cbd0c5a975ab8";
+    hash = "sha256-lIUCdfsmvuzDQaOi2U/CHch1re6Jn6yDfcX26Gu0eUo=";
   };
 
   buildInputs = [ gtk2 qtbase ];
