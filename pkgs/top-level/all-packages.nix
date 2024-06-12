a6ec7da7e2a34572dadee2b34262c3469b202cd7
Jonas Heinrich <onny@project-insanity.org>
2024-06-12 12:32:17 +0200

    sublime-music: move to pkgs/by-name
    (cherry picked from commit 14f5ac461d0842489bacbbd6cb813bdc0a64f4c1)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34577,8 +34577,6 @@ with pkgs;
     pythonBindings = true;
   };
 
-  sublime-music = callPackage ../applications/audio/sublime-music { };
-
   subtitlr = callPackage ../applications/audio/subtitlr { };
 
   subunit = callPackage ../development/libraries/subunit { };
