89ce6bd66993fabe7997a4236372cbd14f7ba3cd
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-13 18:59:52 +0000

    glog: 0.6.0 -> 0.7.1
    Release: https://github.com/google/glog/releases/tag/v0.7.1
    
    - split development output (which reduces closure size for downstream)
    - enable strictDeps (which aids in cross-compiling)
    - convert sha256 into hash
    - nixfmt format
    - order entries by phases and alphabet
    - meta: add changelog and remove nested with

--- pkgs/development/libraries/glog/default.nix
+++ pkgs/development/libraries/glog/default.nix
@@ -1,21 +1,28 @@
-{ stdenv, lib, fetchFromGitHub, cmake, gflags, gtest, perl }:
+{
+  cmake,
+  fetchFromGitHub,
+  gflags,
+  gtest,
+  lib,
+  perl,
+  stdenv,
+}:
 
 stdenv.mkDerivation rec {
   pname = "glog";
-  version = "0.6.0";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "glog";
     rev = "v${version}";
-    sha256 = "sha256-xqRp9vaauBkKz2CXbh/Z4TWqhaUtqfbsSlbYZR/kW9s=";
+    hash = "sha256-+nwWP6VBmhgU7GCPSEGUzvUSCc48wXME181WpJ5ABP4=";
   };
 
-  nativeBuildInputs = [ cmake ];
-
-  buildInputs = [ gtest ];
-
-  propagatedBuildInputs = [ gflags ];
+  outputs = [
+    "dev"
+    "out"
+  ];
 
   cmakeFlags = [
     "-DBUILD_SHARED_LIBS=ON"
@@ -26,37 +33,54 @@ stdenv.mkDerivation rec {
     "-DWITH_UNWIND=OFF"
   ];
 
-  doCheck = true;
-
-  # There are some non-thread safe tests that can fail
-  enableParallelChecking = false;
-  nativeCheckInputs = [ perl ];
-
   env.GTEST_FILTER =
     let
-      filteredTests = lib.optionals stdenv.hostPlatform.isMusl [
-        "Symbolize.SymbolizeStackConsumption"
-        "Symbolize.SymbolizeWithDemanglingStackConsumption"
-      ] ++ lib.optionals stdenv.hostPlatform.isStatic [
-        "LogBacktraceAt.DoesBacktraceAtRightLineWhenEnabled"
-      ] ++ lib.optionals stdenv.cc.isClang [
-        # Clang optimizes an expected allocation away.
-        # See https://github.com/google/glog/issues/937
-        "DeathNoAllocNewHook.logging"
-      ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-        "LogBacktraceAt.DoesBacktraceAtRightLineWhenEnabled"
-      ];
+      filteredTests =
+        lib.optionals stdenv.hostPlatform.isMusl [
+          "Symbolize.SymbolizeStackConsumption"
+          "Symbolize.SymbolizeWithDemanglingStackConsumption"
+        ]
+        ++ lib.optionals stdenv.hostPlatform.isStatic [
+          "LogBacktraceAt.DoesBacktraceAtRightLineWhenEnabled"
+        ]
+        ++ lib.optionals stdenv.cc.isClang [
+          # Clang optimizes an expected allocation away.
+          # See https://github.com/google/glog/issues/937
+          "DeathNoAllocNewHook.logging"
+        ]
+        ++ lib.optionals stdenv.hostPlatform.isDarwin [
+          "LogBacktraceAt.DoesBacktraceAtRightLineWhenEnabled"
+        ];
     in
     "-${builtins.concatStringsSep ":" filteredTests}";
 
+  # Non-thread safe tests can fail
+  enableParallelChecking = false;
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ cmake ];
+
+  buildInputs = [ gtest ];
+
+  propagatedBuildInputs = [ gflags ];
+
+  doCheck = true;
+
+  nativeCheckInputs = [ perl ];
+
   checkPhase =
     let
-      excludedTests = lib.optionals stdenv.hostPlatform.isDarwin [
-        "mock-log"
-      ] ++ lib.optionals (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isAarch64) [
-        "logging"   # works around segfaults on aarch64-darwin for now
-      ];
-      excludedTestsRegex = lib.optionalString (excludedTests != [ ]) "(${lib.concatStringsSep "|" excludedTests})";
+      excludedTests =
+        lib.optionals stdenv.hostPlatform.isDarwin [
+          "mock-log"
+        ]
+        ++ lib.optionals (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isAarch64) [
+          "logging" # works around segfaults on aarch64-darwin for now
+        ];
+      excludedTestsRegex = lib.optionalString (
+        excludedTests != [ ]
+      ) "(${lib.concatStringsSep "|" excludedTests})";
     in
     ''
       runHook preCheck
@@ -64,11 +88,15 @@ stdenv.mkDerivation rec {
       runHook postCheck
     '';
 
-  meta = with lib; {
-    homepage = "https://github.com/google/glog";
-    license = licenses.bsd3;
+  meta = {
     description = "Library for application-level logging";
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ nh2 r-burns ];
+    changelog = "https://github.com/google/glog/releases/tag/v${version}";
+    homepage = "https://github.com/google/glog";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [
+      nh2
+      r-burns
+    ];
   };
 }
