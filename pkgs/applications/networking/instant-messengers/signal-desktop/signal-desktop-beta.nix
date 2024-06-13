f33e8719b5fcc3d2722fb627452bbf91e16ff436
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-13 11:18:35 +0000

    signal-desktop-beta: 7.12.0-beta.2 -> 7.13.0-beta.1
    (cherry picked from commit 15f7be54278e6e5b22d39b762869a2e24abe7e21)

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop-beta";
   dir = "Signal Beta";
-  version = "7.12.0-beta.2";
+  version = "7.13.0-beta.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-Hpg9pkRXbwF5uKhLzn1cfHTzlYmsZd5tndtwVFcL7iU=";
+  hash = "sha256-DvYRvIA+rg4RKXbqWjWj7oFnfLboEiMeP7HgGYkRBDM=";
 }
