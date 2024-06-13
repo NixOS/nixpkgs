b896cba908d42ff847a739926dcf9862e1459a74
Jan Tojnar <jtojnar@gmail.com>
2024-06-13 11:04:40 +0200

    nautilus: Expect extensions passed through wrapper
    No more environment variables pointing to global locations.
    This will allow easy creation of self-contained Nautilus installations
    with extensions (e.g. for nix-shell or Home manager).
    
    The environment variable will be set by a wrapper configured via `nautilusExtensions`
    argument of the derivation, that can be changed using `.override` mechanism.
    This is also what to Thunar does. I decided to not create a separate entrypoint
    like `gimp-with-plugins` since we Nautilus extension are a more open set
    and we can just not wrap when the building with the default empty list of extensions.
    
    For convenience, users should set `programs.nautilus.extensions` NixOS option,
    which will do the overriding for them.

--- nixos/modules/programs/nautilus.nix
+++ nixos/modules/programs/nautilus.nix
@@ -7,23 +7,26 @@
 
 let
   cfg = config.programs.nautilus;
-  package = pkgs.nautilus;
+  package = pkgs.nautilus.override { nautilusExtensions = cfg.extensions; };
 in
 
 {
   options = {
     programs.nautilus = {
       enable = lib.mkEnableOption "Nautilus file manager";
+
+      extensions = lib.mkOption {
+        default = [ ];
+        type = lib.types.listOf lib.types.package;
+        description = "List of Nautilus extensions to install.";
+        example = lib.literalExpression "[ pkgs.nautilus-python ]";
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ package ];
     services.dbus.packages = [ package ];
-
-    # Let nautilus find extensions
-    # TODO: Create nautilus-with-extensions package
-    environment.sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
 
     environment.pathsToLink = [
       "/share/nautilus-python/extensions"
