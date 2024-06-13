8abfba296e920428b0441120537903aa0d5624b9
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-13 17:11:08 +0000

    python311Packages.ubelt: 1.3.5 -> 1.3.6

--- pkgs/development/python-modules/ubelt/default.nix
+++ pkgs/development/python-modules/ubelt/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "ubelt";
-  version = "1.3.5";
+  version = "1.3.6";
   pyproject = true;
 
   disabled = pythonOlder "3.6";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "Erotemic";
     repo = "ubelt";
     rev = "refs/tags/v${version}";
-    hash = "sha256-pwqqt5Syag4cO6a93+7ZE3eI61yTZGc+NEu/Y0i1U0k=";
+    hash = "sha256-ygoJqyTscfuZVDGVbrZobZFvlcuF4HavTU65tmhyU+w=";
   };
 
   nativeBuildInputs = [
