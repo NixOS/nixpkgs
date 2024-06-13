06e8d5fca7216e274be4f72987c865f2778eeed1
Jack Leightcap <jack@leightcap.com>
2024-06-13 13:01:32 -0400

    init: lib25519 at 20240321
    Signed-off-by: Jack Leightcap <jack@leightcap.com>

---
+++ pkgs/by-name/li/lib25519/package.nix
@@ -0,0 +1,55 @@
+{ stdenv
+, lib
+, python3
+, openssl
+, fetchzip
+, librandombytes
+, libcpucycles
+}:
+stdenv.mkDerivation (prev: {
+  pname = "lib25519";
+  version = "20240321";
+
+  src = fetchzip {
+    url = "https://lib25519.cr.yp.to/lib25519-${prev.version}.tar.gz";
+    hash = "sha256-R10Q803vCjIZCS4Z/uErsx547RaXfAELGQm9NuNhw+I=";
+  };
+
+  preConfigure = ''
+    patchShebangs configure
+    patchShebangs scripts-build
+  '';
+
+  # NOTE: librandombytes uses a custom Python `./configure`: it does not expect standard
+  # autoconfig --build --host etc. arguments: disable
+  configurePlatforms = [ ];
+
+  # NOTE: the librandombytes library has required specific CFLAGS defined:
+  # https://randombytes.cr.yp.to/librandombytes-20240318/compilers/default.html
+  # - `-Qunused-arguments` suppress clang warning
+  env.NIX_CFLAGS_COMPILE = toString
+    (lib.optionals stdenv.cc.isClang [ "-Qunused-arguments" ]);
+
+  nativeBuildInputs = [ python3 ];
+  buildInputs = [ librandombytes libcpucycles ];
+
+  meta = {
+    homepage = "https://randombytes.cr.yp.to/";
+    description = "A simple API for applications generating fresh randomness";
+    changelog = "https://randombytes.cr.yp.to/download.html";
+    license = with lib.licenses; [
+      # Upstream specifies the public domain licenses with the terms here https://cr.yp.to/spdx.html
+      publicDomain
+      cc0
+      bsd0
+      mit
+      mit0
+    ];
+    maintainers = with lib.maintainers; [
+      kiike
+      imadnyc
+      jleightcap
+    ];
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
+  };
+})
