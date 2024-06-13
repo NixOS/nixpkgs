cdd31bb7265194b7ee2dbed5ab8fce45c079bb17
Tristan Ross <tristan.ross@midstall.com>
2024-06-13 14:42:37 -0700

    flutterPackages: support beta releases

--- pkgs/build-support/flutter/default.nix
+++ pkgs/build-support/flutter/default.nix
@@ -67,6 +67,20 @@ let
             exit 1
           fi
         '';
+        # https://github.com/dart-lang/pub/blob/e1fbda73d1ac597474b82882ee0bf6ecea5df108/lib/src/sdk/dart.dart#L80
+        "dart" = name: runCommand "dart-sdk-${name}" { passthru.packageRoot = "."; } ''
+          for path in '${flutter.dart}/pkg/${name}'; do
+            if [ -d "$path" ]; then
+              ln -s "$path" "$out"
+              break
+            fi
+          done
+
+          if [ ! -e "$out" ]; then
+            echo 1>&2 'The Dart SDK does not contain the requested package: ${name}!'
+            exit 1
+          fi
+        '';
       };
 
       extraPackageConfigSetup = ''
