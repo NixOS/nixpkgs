fb9f0380dbc5ec63a94fae965f7a8d008df32d96
Yaya <github@uwu.is>
2024-06-13 07:18:57 +0200

    gitlab: 16.10.6 -> 16.11.4
    https://gitlab.com/gitlab-org/gitlab/-/blob/v16.11.4-ee/CHANGELOG.md
    
    Fixes CVE-2024-1495
    Fixes CVE-2024-1963
    Fixes CVE-2024-4201
    Fixes CVE-2024-5469

--- pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-workhorse/default.nix
@@ -5,7 +5,7 @@ in
 buildGoModule rec {
   pname = "gitlab-workhorse";
 
-  version = "16.10.6";
+  version = "16.11.4";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitLab {
@@ -17,10 +17,11 @@ buildGoModule rec {
 
   sourceRoot = "${src.name}/workhorse";
 
-  vendorHash = "sha256-bbKX22Tb2pM+Wnyl1ojdA1nmT40Z5R99mDP1hLD+lco=";
+  vendorHash = "sha256-44EtpjYsxYqDH035/ruPfshfejiO011HybKD2inp8bU=";
   buildInputs = [ git ];
   ldflags = [ "-X main.Version=${version}" ];
   doCheck = false;
+  prodyVendor = true;
 
   meta = with lib; {
     homepage = "http://www.gitlab.com/";
