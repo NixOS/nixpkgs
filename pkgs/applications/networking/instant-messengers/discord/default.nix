5314cd7b2acebe818758c40dffae8ce986ff2f7b
jopejoe1 <johannes@joens.email>
2024-06-14 20:15:35 +0000

    discord-canary: 0.0.402 -> 0.0.422
    (cherry picked from commit 35b6295019c7abca9b7ea6f7a5188e73d0a2d264)

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -4,7 +4,7 @@ let
     if stdenv.isLinux then {
       stable = "0.0.56";
       ptb = "0.0.90";
-      canary = "0.0.402";
+      canary = "0.0.422";
       development = "0.0.19";
     } else {
       stable = "0.0.305";
@@ -25,7 +25,7 @@ let
       };
       canary = fetchurl {
         url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-        hash = "sha256-YE+sHmEkUCHLflSUgf4aotWWdzhbFyzozc3vLcJ96yA=";
+        hash = "sha256-MTC079WTN0Vw57aLro1WkHRL09IKYjnUsfBAMiiHjI0=";
       };
       development = fetchurl {
         url = "https://dl-development.discordapp.net/apps/linux/${version}/discord-development-${version}.tar.gz";
