9e51a3564f606253a6ac57d2252f730acd24bc79
superherointj <5861043+superherointj@users.noreply.github.com>
2024-06-14 17:47:39 -0300

    ocamlPackages.ppx_yojson_conv: minimalOCamlVersion

--- pkgs/development/ocaml-modules/ppx_yojson_conv/default.nix
+++ pkgs/development/ocaml-modules/ppx_yojson_conv/default.nix
@@ -10,7 +10,7 @@ buildDunePackage rec {
   pname = "ppx_yojson_conv";
   version = "0.15.1";
   duneVersion = "3";
-  minimumOCamlVersion = "4.08.0";
+  minimalOCamlVersion = "4.08.0";
 
   src = fetchFromGitHub {
     owner = "janestreet";
