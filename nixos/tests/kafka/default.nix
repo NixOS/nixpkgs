e5ae1e29ec387479da1f067e5397b5977c5a0fd5
Jonathan Davies <jpds@protonmail.com>
2024-06-14 19:42:22 +0100

    nixos/kafka: Added mirrormaker testcase

--- nixos/tests/kafka/default.nix
+++ nixos/tests/kafka/default.nix
@@ -6,4 +6,5 @@
 {
   base = import ./base.nix { inherit system pkgs; };
   cluster = import ./cluster.nix { inherit system pkgs; };
+  mirrormaker = import ./mirrormaker.nix { inherit system pkgs; };
 }
