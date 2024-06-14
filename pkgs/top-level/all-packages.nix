1d0f8a067b7d063adb0647954befcd47da3d3330
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-06-14 01:03:41 -0400

    citrix_workspace: 24.2.0.65 -> 24.5.0.76

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6638,8 +6638,9 @@ with pkgs;
     citrix_workspace_23_09_0
     citrix_workspace_23_11_0
     citrix_workspace_24_02_0
+    citrix_workspace_24_05_0
   ;
-  citrix_workspace = citrix_workspace_24_02_0;
+  citrix_workspace = citrix_workspace_24_05_0;
 
   clima = callPackage ../tools/text/clima { };
 
