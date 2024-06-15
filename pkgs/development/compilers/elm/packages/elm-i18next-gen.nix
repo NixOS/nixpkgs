d6ba455abefc179829e6da36dce31c6170c1fe98
iko <ilyakooo0@gmail.com>
2024-06-15 18:40:35 +0000

    Apply suggestions from code review
    Co-authored-by: Robert Schütz <github@dotlambda.de>

--- pkgs/development/compilers/elm/packages/elm-i18next-gen.nix
+++ pkgs/development/compilers/elm/packages/elm-i18next-gen.nix
@@ -6,7 +6,7 @@ buildNpmPackage rec {
 
   src = fetchFromGitHub {
     owner = "yonigibbs";
-    repo = pname;
+    repo = "elm-i18next-gen";
     rev = "v${version}";
     hash = "sha256-RBBTJA3sEpI5nIqhifDZGldUnVvaGK2ZeYqgvccODQ0=";
   };
@@ -14,12 +14,15 @@ buildNpmPackage rec {
   npmDepsHash = "sha256-eXh73Wb5x/7DLJk5bcpn3EkCAj8of76DrRoCHNHdvxE=";
 
   dontNpmBuild = true;
+
+  # we can't download Elm during npm install
   npmInstallFlags = "--omit=dev";
 
   meta = with lib; {
     description = "Code generation for elm-i18next";
     homepage = "https://github.com/yonigibbs/elm-i18next-gen";
     license = licenses.mit;
+    mainProgram = "elm-i18next-gen";
     maintainers = with maintainers; [ ilyakooo0 ];
   };
 }
