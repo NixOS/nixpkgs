554abf3f51b5d7f4c8a1c06021acb5befc390e0e
lelgenio <lelgenio@lelgenio.com>
2024-06-15 20:14:28 -0300

    lessc: repackage with buildNpmPackage

--- pkgs/development/node-packages/aliases.nix
+++ pkgs/development/node-packages/aliases.nix
@@ -132,6 +132,7 @@ mapAliases {
   inherit (pkgs) kaput-cli; # added 2024-12-03
   karma = pkgs.karma-runner; # added 2023-07-29
   leetcode-cli = self.vsc-leetcode-cli; # added 2023-08-31
+  less = pkgs.lessc; # added 2024-06-15
   inherit (pkgs) lv_font_conv; # added 2024-06-28
   manta = pkgs.node-manta; # Added 2023-05-06
   inherit (pkgs) markdown-link-check; # added 2024-06-28
