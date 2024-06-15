7a7060c5bfae66ea06439f139ae8361f46e2bc35
Tim Häring <tim.haering@gmail.com>
2024-06-15 12:55:06 +0200

    nixos/aria2: fix missing default

--- nixos/modules/services/networking/aria2.nix
+++ nixos/modules/services/networking/aria2.nix
@@ -71,6 +71,7 @@ in
 
           [0]: https://aria2.github.io/manual/en/html/aria2c.html#synopsis
         '';
+        default = { };
         type = lib.types.submodule {
           freeformType = with lib.types; attrsOf (oneOf [ bool int float singleLineStr ]);
           options = {
