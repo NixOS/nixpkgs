68c577f15b0280a903c0c3b36b195e279092f082
Hayden Gray <35206453+A1029384756@users.noreply.github.com>
2024-06-15 01:38:33 -0400

    odin: pin llvmPackages_18

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -27600,6 +27600,7 @@ with pkgs;
   odin = callPackage ../development/compilers/odin {
     inherit (pkgs.darwin.apple_sdk_11_0) MacOSX-SDK;
     inherit (pkgs.darwin.apple_sdk_11_0.frameworks) Security;
+    llvmPackages = llvmPackages_18;
   };
 
   odp-dpdk = callPackage ../os-specific/linux/odp-dpdk { };
