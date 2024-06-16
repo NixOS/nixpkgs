ecf529e88fea9eed50e0b8f4a6dd7309cf5de52f
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-16 11:48:32 +0000

    nodejs-slim_22: 22.2.0 -> 22.3.0
    (cherry picked from commit dd1ea8f3077098b09d0f57deb8b04b5a8f6e0838)

--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -8,8 +8,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "22.2.0";
-  sha256 = "sha256-iJkIqIKNFISRDX5lm2qlet6NUo/w45Dpp372WadihHQ=";
+  version = "22.3.0";
+  sha256 = "0k0h4s9s2y0ms3g6xhynsqsrkl9hz001dmj6j0gpc5x5vk8mpf5z";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
