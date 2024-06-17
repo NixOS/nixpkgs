fa547edc660719b6593514ea551a49c341aa877f
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-06-17 08:29:37 +0200

    xmonad-log: move to by-name
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35655,8 +35655,6 @@ with pkgs;
 
   xmobar = haskellPackages.xmobar.bin;
 
-  xmonad-log = callPackage ../tools/misc/xmonad-log { };
-
   xmonad-with-packages = callPackage ../applications/window-managers/xmonad/wrapper.nix {
     inherit (haskellPackages) ghcWithPackages;
     packages = _: [ haskellPackages.xmonad-contrib ];
