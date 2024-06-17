f38018a09b8972b1905fccdc9832f6b8313c23bf
Jörg Thalheim <joerg@thalheim.io>
2024-06-17 17:02:59 +0200

    python311Packages.deltachat2: init at 0.6.2
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>
    Co-authored-by: Robert Schütz <nix@dotlambda.de>
    (cherry picked from commit 935eb15442967e297e439f022af091c2b9fbdb61)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2924,6 +2924,8 @@ self: super: with self; {
 
   deltachat = callPackage ../development/python-modules/deltachat { };
 
+  deltachat2 = callPackage ../development/python-modules/deltachat2 { };
+
   deluge-client = callPackage ../development/python-modules/deluge-client { };
 
   demes = callPackage ../development/python-modules/demes { };
