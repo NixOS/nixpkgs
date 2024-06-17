31591e093a37b07f3a9583b877cb6b0e638799ea
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-17 08:47:54 +0200

    python312Packages.home-assistant-bluetooth: 1.12.0 -> 1.12.1
    Diff: https://github.com/home-assistant-libs/home-assistant-bluetooth/compare/refs/tags/v1.12.0...v1.12.1
    
    Changelog: https://github.com/home-assistant-libs/home-assistant-bluetooth/blob/v1.12.1/CHANGELOG.md

--- pkgs/development/python-modules/home-assistant-bluetooth/default.nix
+++ pkgs/development/python-modules/home-assistant-bluetooth/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "home-assistant-bluetooth";
-  version = "1.12.0";
+  version = "1.12.1";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -28,33 +28,29 @@ buildPythonPackage rec {
     owner = "home-assistant-libs";
     repo = "home-assistant-bluetooth";
     rev = "refs/tags/v${version}";
-    hash = "sha256-KTaZ3xbZpBIN5zP73YdJW6QeCQThGdqejnfWwvL+0R8=";
+    hash = "sha256-3gjx1XA5GJXmSs9SDTXvZlBrKgVkiqw1ikxGrrX43FM=";
   };
-
-  patches = [
-    # https://github.com/home-assistant-libs/home-assistant-bluetooth/issues/38
-    ./habluetooth-3.0-compat.patch
-  ];
 
   postPatch = ''
     # drop pytest parametrization (coverage, etc.)
     sed -i '/addopts/d' pyproject.toml
   '';
 
-  nativeBuildInputs = [
-    cython
+  build-system = [
     poetry-core
     setuptools
   ];
 
-  propagatedBuildInputs = [ habluetooth ];
+  nativeBuildInputs = [ cython ];
 
-  pythonImportsCheck = [ "home_assistant_bluetooth" ];
+  dependencies = [ habluetooth ];
 
   nativeCheckInputs = [
     bleak
     pytestCheckHook
   ];
+
+  pythonImportsCheck = [ "home_assistant_bluetooth" ];
 
   meta = with lib; {
     description = "Basic bluetooth models used by Home Assistant";
