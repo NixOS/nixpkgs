59131cc40d999d9b5567a543baa685b258e14962
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-17 13:35:14 +0000

    prometheus-junos-czerwonk-exporter: 0.12.2 -> 0.12.3

--- pkgs/servers/monitoring/prometheus/junos-czerwonk-exporter.nix
+++ pkgs/servers/monitoring/prometheus/junos-czerwonk-exporter.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "junos-czerwonk-exporter";
-  version = "0.12.2";
+  version = "0.12.3";
 
   src = fetchFromGitHub {
     owner = "czerwonk";
     repo = "junos_exporter";
     rev = version;
-    sha256 = "sha256-KdVyRddAr2gqiFyIGBfWbi4DHAaiey4p4OBFND/2u7U=";
+    sha256 = "sha256-5haMQlwGJO0WdT75AEfs41k1oWUqrFk9nj+DOcKAom0=";
   };
 
-  vendorHash = "sha256-fytDr56ZhhO5u6u9CRIEKXGqgnzntSVqEVItibpLyPM=";
+  vendorHash = "sha256-qHs6KuBmJmmkmR23Ae7COadb2F7N8CMUmScx8JFt98Q=";
 
   meta = with lib; {
     description = "Exporter for metrics from devices running JunOS";
