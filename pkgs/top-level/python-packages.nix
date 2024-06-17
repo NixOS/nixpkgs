2c9f6929847cdee2056270db53d5258fe0417649
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-06-17 13:30:32 +0700

    python311Packages.rfeed, python312Packages.rfeed: init at 1.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13118,6 +13118,8 @@ self: super: with self; {
 
   rfcat = callPackage ../development/python-modules/rfcat { };
 
+  rfeed = callPackage ../development/python-modules/rfeed { };
+
   rflink = callPackage ../development/python-modules/rflink { };
 
   rich = callPackage ../development/python-modules/rich { };
