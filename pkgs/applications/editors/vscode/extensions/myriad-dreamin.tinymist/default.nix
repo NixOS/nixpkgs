81ff3e6019ec6104b05ce05e4a03520167924c0e
Gaetan Lepage <gaetan@glepage.com>
2024-06-17 08:13:42 +0200

    tinymist: 0.11.10 -> 0.11.11
    Diff: https://github.com/Myriad-Dreamin/tinymist/compare/refs/tags/v0.11.10...v0.11.11
    
    Changelog: https://github.com/Myriad-Dreamin/tinymist/blob/refs/tags/v0.11.11/CHANGELOG.md

--- pkgs/applications/editors/vscode/extensions/myriad-dreamin.tinymist/default.nix
+++ pkgs/applications/editors/vscode/extensions/myriad-dreamin.tinymist/default.nix
@@ -11,7 +11,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
     name = "tinymist";
     publisher = "myriad-dreamin";
     inherit (tinymist) version;
-    hash = "sha256-etPjbmcBhS1dgq5wEoRIekZlRxYoC6KrsV/+owjHu4I=";
+    hash = "sha256-Oama8FkRhEY4yza77RpWHg0Aeo9WFhveucJqJvCYMJQ=";
   };
 
   nativeBuildInputs = [
