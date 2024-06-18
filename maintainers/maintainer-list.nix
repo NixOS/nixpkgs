d83566ed20e19571d5a87f2e930d7d339ebcf76c
Aria <me@aria.rip>
2024-06-18 21:33:38 +0100

    akkoma: remove maintainer tcmal (#320858)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -19621,12 +19621,6 @@
     githubId = 2389333;
     name = "Andy Tockman";
   };
-  tcmal = {
-    email = "me@aria.rip";
-    github = "tcmal";
-    githubId = 4183876;
-    name = "Aria Shrimpton";
-  };
   teatwig = {
     email = "nix@teatwig.net";
     name = "tea";
