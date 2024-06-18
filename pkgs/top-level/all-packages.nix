4bd76beac0eee70381663d2ef0e84aa2ae2ac29d
John Ericson <John.Ericson@Obsidian.Systems>
2024-06-18 15:50:48 -0400

    windows.mingw_w64*: Fetch common attrs from the headers package
    This is the most upstream one, and so to avoid infinite recursion we
    should get the things from it. This isn't needed per-se now, but will be
    after the next commit.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15464,7 +15464,7 @@ with pkgs;
     dontStrip = true;
   }));
 
-  gccCrossLibcStdenv = overrideCC stdenv buildPackages.gccWithoutTargetLibc;
+  gccCrossLibcStdenv = overrideCC stdenvNoCC buildPackages.gccWithoutTargetLibc;
 
   crossLibcStdenv =
     if stdenv.hostPlatform.useLLVM or false || stdenv.hostPlatform.isDarwin
