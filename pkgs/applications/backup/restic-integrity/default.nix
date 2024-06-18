ba7027b7b9e9cebb85146fd79ef58f688dfc9976
networkException <git@nwex.de>
2024-06-18 21:33:46 +0200

    restic-integrity: 1.2.1 -> 1.2.2
    (cherry picked from commit e8e57d101461b8880a33fa1fc75c6613a27cb2ce)

--- pkgs/applications/backup/restic-integrity/default.nix
+++ pkgs/applications/backup/restic-integrity/default.nix
@@ -5,17 +5,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "restic-integrity";
-  version = "1.2.1";
+  version = "1.2.2";
 
   src = fetchFromGitea {
     domain = "git.nwex.de";
     owner = "networkException";
     repo = "restic-integrity";
     rev = version;
-    hash = "sha256-/n8muqW9ol0AY9RM3N4nqLDw0U1h0308M1uRCMS2kOM=";
+    hash = "sha256-QiISJCxxJH8wQeH/klB48POn6W9juQmIMCLGzGSyl6w=";
   };
 
-  cargoHash = "sha256-TYDPzjWxTK9hQhzSknkCao9lq9UjZN3yQX3wtkMmP6E=";
+  cargoHash = "sha256-GxehJjDd0AHbEc8kPWyLXAOPbrPCT59LddAL1ydnT5g=";
 
   meta = with lib; {
     description = "A CLI tool to check the integrity of a restic repository without unlocking it";
