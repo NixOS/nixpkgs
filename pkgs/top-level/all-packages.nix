b7fd868c2e8e5d23fde0b65631b7d54dcb7be689
Robert Schütz <mail@dotlambda.de>
2024-06-18 02:34:46 -0700

    deltachat-desktop: 1.44.1 -> 1.46.1
    Diff: https://github.com/deltachat/deltachat-desktop/compare/v1.44.1...v1.46.1
    
    Changelog: https://github.com/deltachat/deltachat-desktop/blob/v1.46.1/CHANGELOG.md
    (cherry picked from commit 4ee62639cdfa344a02c90108fc8a106bcd76f757)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3647,6 +3647,7 @@ with pkgs;
   dell-command-configure = callPackage ../tools/system/dell-command-configure { };
 
   deltachat-desktop = callPackage ../applications/networking/instant-messengers/deltachat-desktop {
+    electron = electron_30;
     inherit (darwin.apple_sdk.frameworks) CoreServices;
   };
 
