9b0b6ee4210c61032ef5838c70d0d8f8fbedba63
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-18 11:05:54 +0000

    python311Packages.aioautomower: 2024.5.1 -> 2024.6.1

--- pkgs/development/python-modules/aioautomower/default.nix
+++ pkgs/development/python-modules/aioautomower/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "aioautomower";
-  version = "2024.5.1";
+  version = "2024.6.1";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "Thomas55555";
     repo = "aioautomower";
     rev = "refs/tags/${version}";
-    hash = "sha256-SwRr8BJOchQ7qObgVkNGTkaFEx7wiB/EPedRG2csH+A=";
+    hash = "sha256-9mIGhSw8FP5+hAs9It8oTCFYSSRgTGAkMNBbCVImkMQ=";
   };
 
   postPatch = ''
