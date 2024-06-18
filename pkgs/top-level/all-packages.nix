57f5d2e150201d523600c10e038f90f8820253a3
Aaron Jheng <wentworth@outlook.com>
2024-06-18 16:23:09 +0800

    unconvert: unstable-2022-09-18 -> 0-unstable-2023-09-07
    https://github.com/mdempsky/unconvert/compare/3f84926d692329767c21c2aef3dfb7889c956832...415706980c061b6f71ea923bd206aec88785638f

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24629,9 +24629,7 @@ with pkgs;
 
   umockdev = callPackage ../development/libraries/umockdev { };
 
-  unconvert = callPackage ../development/tools/unconvert {
-    buildGoModule = buildGo121Module;
-  };
+  unconvert = callPackage ../development/tools/unconvert { };
 
   uncover = callPackage ../tools/security/uncover { };
 
