6920e5a7905b67b9e9a7d067a6cc511e2b8b22ed
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:06:32 +0200

    python3Packages.niaaml: 2.0.0 -> 2.1.0
    https://github.com/lukapecnik/NiaAML/releases/tag/2.1.0

--- pkgs/development/python-modules/niaaml/default.nix
+++ pkgs/development/python-modules/niaaml/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "niaaml";
-  version = "2.0.0";
+  version = "2.1.0";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "lukapecnik";
     repo = "NiaAML";
     rev = "refs/tags/${version}";
-    hash = "sha256-d6UlpMrT0GBZlcD1BCZxInXTdyFMBVltxnSyUFbSy0g=";
+    hash = "sha256-VMZLEirE01Q9eyQIhV18PepGWmBcxLIwNeuVf7EuSWE=";
   };
 
   pythonRelaxDeps = [ "pandas" ];
