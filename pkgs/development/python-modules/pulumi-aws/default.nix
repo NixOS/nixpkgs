92648b7c3ca128c468d60a2b9fda259a75567467
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:06:56 +0200

    python3Packages.pulumi-aws: 6.38.0 -> 6.41.0
    https://github.com/pulumi/pulumi-aws/releases/tag/v6.41.0

--- pkgs/development/python-modules/pulumi-aws/default.nix
+++ pkgs/development/python-modules/pulumi-aws/default.nix
@@ -12,7 +12,7 @@
 buildPythonPackage rec {
   pname = "pulumi-aws";
   # Version is independant of pulumi's.
-  version = "6.38.0";
+  version = "6.41.0";
 
   pyproject = true;
   build-system = [ setuptools ];
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "pulumi";
     repo = "pulumi-aws";
     rev = "refs/tags/v${version}";
-    hash = "sha256-sV8Gt8EZ1LPzRbnFoVIWjykiFK04UWQAjuF7hAmJBPk=";
+    hash = "sha256-7l+m4ULDGa15QNu6l2kURQznKP2Ut9Y1P9njV7s9nzc=";
   };
 
   sourceRoot = "${src.name}/sdk/python";
