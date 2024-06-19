decdda42114265b1db569db11e265345329fff31
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:07:23 +0200

    python3Packages.stripe: 9.9.0 -> 9.12.0
    https://github.com/stripe/stripe-python/blob/v9.12.0/CHANGELOG.md

--- pkgs/development/python-modules/stripe/default.nix
+++ pkgs/development/python-modules/stripe/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "stripe";
-  version = "9.9.0";
+  version = "9.12.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-xC2Pa0RjpU86AlWBgQ9OYy4tWnHeYQD8WV11WB9ppJI=";
+    hash = "sha256-y8Umq9DwAckgwyO6fEDM497hZH2SDp2+yuNIjzc2dSQ=";
   };
 
   build-system = [ setuptools ];
