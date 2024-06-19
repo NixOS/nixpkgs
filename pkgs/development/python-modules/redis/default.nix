49cbed27c9b2b297c13731e9b62ff672c4a348e7
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:07:12 +0200

    python3Packages.redis: 5.0.3 -> 5.0.6
    https://github.com/redis/redis-py/releases/tag/v5.0.6

--- pkgs/development/python-modules/redis/default.nix
+++ pkgs/development/python-modules/redis/default.nix
@@ -22,14 +22,14 @@
 
 buildPythonPackage rec {
   pname = "redis";
-  version = "5.0.3";
+  version = "5.0.6";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-SXO650RMD77WSga4dEb3k2HLfk7BU4wCLWlu16UBVYA=";
+    hash = "sha256-OEc818Y4mtPkSpH0w+r2vLip90YAfym/T7IIJP8LIZc=";
   };
 
   propagatedBuildInputs = [
