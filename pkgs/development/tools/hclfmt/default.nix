282a8256970deb911b35a51f4ad1fa817a538cc5
R. RyanTM <ryantm-bot@ryantm.com>
2024-06-20 04:55:55 +1000

    hclfmt: 2.20.1 -> 2.21.0 (#321035)

--- pkgs/development/tools/hclfmt/default.nix
+++ pkgs/development/tools/hclfmt/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "hclfmt";
-  version = "2.20.1";
+  version = "2.21.0";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = "hcl";
     rev = "v${version}";
-    hash = "sha256-+4K6k32azx/66eag9c6lUN8TUJ1ICx4Q8zpnTJWqgQ0=";
+    hash = "sha256-izbctn5OqaDunQLovgCGTgqMzC9i227zXlXUNCEExvA=";
   };
 
-  vendorHash = "sha256-L5OabeDUXbrwFOgWRhi9FPTWK+xbL54ZM7cYhS15Jis=";
+  vendorHash = "sha256-F2i7ph9GL9Xo43da6jHPn9P9FdWC6eSgqCyHPBxdFJY=";
 
   # The code repository includes other tools which are not useful. Only build
   # hclfmt.
