ab926cb3b6293200b52533678b4638072a97177d
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-19 11:51:36 +0200

    python311Packages.angr: 9.2.106 -> 9.2.107
    Diff: https://github.com/angr/angr/compare/refs/tags/v9.2.106...v9.2.107

--- pkgs/development/python-modules/angr/default.nix
+++ pkgs/development/python-modules/angr/default.nix
@@ -37,7 +37,7 @@
 
 buildPythonPackage rec {
   pname = "angr";
-  version = "9.2.106";
+  version = "9.2.107";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -46,7 +46,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "angr";
     rev = "refs/tags/v${version}";
-    hash = "sha256-TM6Jvb260lQWDrqYASdpRqgS9PmkmPUBcncDtTwWKAU=";
+    hash = "sha256-f4RiLXEp4q3V2SiX6OSLHbAVCaZ5GLOvqm1qmBAYIZ8=";
   };
 
   pythonRelaxDeps = [ "capstone" ];
