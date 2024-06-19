49abc7d10ecd28eb5e4e028eefc3fcbe0646181d
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:05:07 +0200

    python3Packages.conda: 24.4.0 -> 24.5.0

--- pkgs/development/python-modules/conda/default.nix
+++ pkgs/development/python-modules/conda/default.nix
@@ -26,7 +26,7 @@
 }:
 buildPythonPackage rec {
   pname = "conda";
-  version = "24.4.0";
+  version = "24.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
@@ -34,7 +34,7 @@ buildPythonPackage rec {
     owner = "conda";
     repo = "conda";
     rev = "refs/tags/${version}";
-    hash = "sha256-LdoBlR5EFYd2mQIjOgp1MH3w6osfRfurPq+N5Y1iaFw=";
+    hash = "sha256-DbgdTaCMWf0d3MLEMGoWxN3x37tAtoW8T7mm5279yqk=";
   };
 
 
