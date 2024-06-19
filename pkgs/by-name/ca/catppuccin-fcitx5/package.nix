6f52adb1fe16f20a85166fe0b7217ceb5b2cb33e
jopejoe1 <johannes@joens.email>
2024-06-19 20:27:23 +0200

    catppuccin-fcitx5: merge with fcitx5-catppuccin
    they are the same package

--- pkgs/by-name/ca/catppuccin-fcitx5/package.nix
+++ pkgs/by-name/ca/catppuccin-fcitx5/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
+  unstableGitUpdater,
 }:
 stdenvNoCC.mkDerivation {
   pname = "catppuccin-fcitx5";
@@ -14,6 +15,9 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-uFaCbyrEjv4oiKUzLVFzw+UY54/h7wh2cntqeyYwGps=";
   };
 
+  dontConfigure = true;
+  dontBuild = true;
+
   installPhase = ''
     runHook preInstall
     mkdir -p $out/share/fcitx5
@@ -21,11 +25,13 @@ stdenvNoCC.mkDerivation {
     runHook postInstall
   '';
 
+  passthru.updateScript = unstableGitUpdater { };
+
   meta = {
     description = "Soothing pastel theme for Fcitx5";
     homepage = "https://github.com/catppuccin/fcitx5";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ pluiedev ];
+    maintainers = with lib.maintainers; [ pluiedev Guanran928 ];
     platforms = lib.platforms.all;
   };
 }
