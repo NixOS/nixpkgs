56dcd86785b70086d84447ca7b7824157774de5b
Jakub Okoński <jakub@okonski.org>
2024-06-19 14:43:40 +0200

    Update graalvm-oracle to 22

--- pkgs/development/compilers/graalvm/oracle/hashes.nix
+++ pkgs/development/compilers/graalvm/oracle/hashes.nix
@@ -1,21 +1,21 @@
 {
-  "version" = "21";
+  "version" = "22";
   "hashes" = {
     "aarch64-linux" = {
-      sha256 = "eb1eaf9d1e1e01263b0acd552552686084903ae11a9a7698a144ef8c3ee02dec";
-      url = "https://download.oracle.com/graalvm/21/archive/graalvm-jdk-21_linux-aarch64_bin.tar.gz";
+      sha256 = "d303d30c10764feaab076efe79019df3c981b0487fcac3df7a5313f76f61040e";
+      url = "https://download.oracle.com/graalvm/22/archive/graalvm-jdk-22_linux-aarch64_bin.tar.gz";
     };
     "x86_64-linux" = {
-      sha256 = "be1ab3c9b08b5747b7cd577c991d25b52157cff1c8c5f290f8556c593b57a6f4";
-      url = "https://download.oracle.com/graalvm/21/archive/graalvm-jdk-21_linux-x64_bin.tar.gz";
+      sha256 = "d1860b5b7588310e70b259c891156f6d0cbc34d0d1feec3b37169ed2a415f3c3";
+      url = "https://download.oracle.com/graalvm/22/archive/graalvm-jdk-22_linux-x64_bin.tar.gz";
     };
     "x86_64-darwin" = {
-      sha256 = "0744ab104998f8f45d9ae582134963f5d273286dff9aff586aed24f5f8434660";
-      url = "https://download.oracle.com/graalvm/21/archive/graalvm-jdk-21_macos-x64_bin.tar.gz";
+      sha256 = "5b83f20dbc4c636ed41f19c3309f09839d4f5c6442dba986f460589c494a476c";
+      url = "https://download.oracle.com/graalvm/22/archive/graalvm-jdk-22_macos-x64_bin.tar.gz";
     };
     "aarch64-darwin" = {
-      sha256 = "c2ca434adef1e497c8a4d942ae1dbf6bbd1c8174a6fcdafc65cde0e853285300";
-      url = "https://download.oracle.com/graalvm/21/archive/graalvm-jdk-21_macos-aarch64_bin.tar.gz";
+      sha256 = "61632065cfcdc4e121362f1fd25a543955836bbacd6c1aadbcbe0d469d5ab8a3";
+      url = "https://download.oracle.com/graalvm/22/archive/graalvm-jdk-22_macos-aarch64_bin.tar.gz";
     };
   };
 }
