b8a6b7ac76301dca72113bd9acc95da1258bd313
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:07:15 +0200

    python3Packages.schema: 0.7.5 -> 0.7.7

--- pkgs/development/python-modules/schema/default.nix
+++ pkgs/development/python-modules/schema/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "schema";
-  version = "0.7.5";
+  version = "0.7.7";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-8GcXESxhiVyrxHB3UriHFuhCCogZ1xQEUB4RT5EEMZc=";
+    hash = "sha256-faVTq9KVihncJUfDiM3lM5izkZYXWpvlnqHK9asKGAc=";
   };
 
 
