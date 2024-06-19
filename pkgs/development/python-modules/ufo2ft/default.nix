04afded47e7e7fc1d55aeb382d565dbe1ce4636e
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:07:34 +0200

    python3Packages.ufo2ft: 3.2.4 -> 3.2.5
    https://github.com/googlefonts/ufo2ft/releases/tag/v3.2.5

--- pkgs/development/python-modules/ufo2ft/default.nix
+++ pkgs/development/python-modules/ufo2ft/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "ufo2ft";
-  version = "3.2.4";
+  version = "3.2.5";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-LkrYKERPJrKsWAYnlJJlybNy93J+uStaHv35jcZpTrU=";
+    hash = "sha256-PUPk92wugtIZWXP8vq8bJNxqTDhDENKdNhW1kNEcL3E=";
   };
 
   nativeBuildInputs = [
