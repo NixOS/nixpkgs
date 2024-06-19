829f27eb0c7ec5548eb507492c3cbfa12aa9afbc
Emily <vcs@emily.moe>
2024-06-19 14:14:33 +0100

    jasper: use the GLUT framework on macOS
    There are probably more cases like this; it might be good to have a
    top‐level glut package that resolves to either GLUT or freeglut
    depending on platform?

--- pkgs/by-name/ja/jasper/package.nix
+++ pkgs/by-name/ja/jasper/package.nix
@@ -5,6 +5,7 @@
 , libGL
 , libheif
 , libjpeg
+, darwin
 , pkg-config
 , stdenv
 , enableHEIFCodec ? true
@@ -35,10 +36,16 @@ stdenv.mkDerivation (finalAttrs: {
     libheif
   ] ++ lib.optionals enableJPGCodec [
     libjpeg
-  ] ++ lib.optionals enableOpenGL [
-    freeglut
-    libGL
-  ];
+  ] ++ lib.optionals enableOpenGL (
+    if stdenv.hostPlatform.isDarwin then [
+      darwin.apple_sdk.frameworks.OpenGL
+      darwin.apple_sdk.frameworks.GLUT
+      darwin.apple_sdk.frameworks.Cocoa
+    ] else [
+      libGL
+      freeglut
+    ]
+  );
 
   # Since "build" already exists and is populated, cmake tries to use it,
   # throwing uncomprehensible error messages...
