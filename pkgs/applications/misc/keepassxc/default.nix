826a15d066e234ba95f425fa70dff7b225e42314
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-20 06:55:05 +0000

    keepassxc: 2.7.7 -> 2.7.9
    (cherry picked from commit e6b0f860eb7214a41c84e59b24066751894d1e26)

--- pkgs/applications/misc/keepassxc/default.nix
+++ pkgs/applications/misc/keepassxc/default.nix
@@ -41,13 +41,13 @@
 
 stdenv.mkDerivation rec {
   pname = "keepassxc";
-  version = "2.7.7";
+  version = "2.7.9";
 
   src = fetchFromGitHub {
     owner = "keepassxreboot";
     repo = "keepassxc";
     rev = version;
-    hash = "sha256-HjDzb1H3eMSraKbfHgg9S+w4TXNt40lQkDz+EChb5Ks=";
+    hash = "sha256-rnietdc8eDNTag0GaZ8VJb28JsKKD/qrQ0Gg6FMWpr0=";
   };
 
   env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang (toString [
