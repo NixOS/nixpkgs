8300f8c1ebce3e85f91a915fb76c25707eb85822
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-20 11:13:23 +0200

    python311Packages.pyswitchbot: 0.47.2 -> 0.48.0
    Diff: https://github.com/Danielhiversen/pySwitchbot/compare/refs/tags/0.47.2...0.48.0
    
    Changelog: https://github.com/Danielhiversen/pySwitchbot/releases/tag/0.48.0

--- pkgs/development/python-modules/pyswitchbot/default.nix
+++ pkgs/development/python-modules/pyswitchbot/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "pyswitchbot";
-  version = "0.47.2";
+  version = "0.48.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "Danielhiversen";
     repo = "pySwitchbot";
     rev = "refs/tags/${version}";
-    hash = "sha256-89bFP/7ovsPj7htFGXrYR4jhm1E/ph47f4qZCKf1hfY=";
+    hash = "sha256-UVLB1SdtYiMsXR3od2gIxNxiQ6wyW2ItAJQrxv0w1tk=";
   };
 
   build-system = [ setuptools ];
