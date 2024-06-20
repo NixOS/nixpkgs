d01fe5dad192d03745672ec0b2d1b0054610c5ca
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-06-20 18:52:46 +0200

    lib.isInt: inherit from lib.trivial
    (cherry picked from commit fa82e813f10165305b4a2387e6e54080c9e5c486)

--- lib/default.nix
+++ lib/default.nix
@@ -66,7 +66,7 @@ let
 
     inherit (builtins) addErrorContext
       genericClosure getAttr
-      isInt isList isPath isString
+      isList isPath isString
       pathExists readFile replaceStrings
       trace;
     inherit (self.trivial) id const pipe concat or and xor bitAnd bitOr bitXor
@@ -75,7 +75,7 @@ let
       info showWarnings nixpkgsVersion version isInOldestRelease
       mod compare splitByAndCompare seq deepSeq lessThan add sub
       functionArgs setFunctionArgs isFunction toFunction mirrorFunctionArgs
-      toHexString toBaseDigits inPureEvalMode isBool;
+      toHexString toBaseDigits inPureEvalMode isBool isInt;
     inherit (self.fixedPoints) fix fix' converge extends composeExtensions
       composeManyExtensions makeExtensible makeExtensibleWithCustomName;
     inherit (self.attrsets) attrByPath hasAttrByPath setAttrByPath
