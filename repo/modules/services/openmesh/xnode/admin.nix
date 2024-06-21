049589a6710a060dc4c5215fcaf019dbe4281a78
harrys522 <hsackman@protonmail.com>
2024-06-21 15:32:37 +1000

    clone dev branch locally instead of feature branch

--- repo/modules/services/openmesh/xnode/admin.nix
+++ repo/modules/services/openmesh/xnode/admin.nix
@@ -54,8 +54,8 @@ in
 
       serviceConfig = {
         ExecStart = ''${lib.getExe cfg.package} -p ${cfg.stateDir} ${cfg.remoteDir} ${toString cfg.searchInterval}''; 
-        ExecStartPre = ''-${lib.getExe pkgs.git} clone --branch feature/import-agent-config-dynamically-testing https://github.com/openmesh-network/xnodeos ${cfg.stateDir}/xnodeos'';
-        ExecCondition = ''-${lib.getExe pkgs.git} pull --branch feature/import-agent-config-dynamically-testing https://github.com/openmesh-network/xnodeos ${cfg.stateDir}/xnodeos'';
+        ExecStartPre = ''-${lib.getExe pkgs.git} clone --branch dev https://github.com/openmesh-network/xnodeos ${cfg.stateDir}/xnodeos'';
+        ExecCondition = ''-${lib.getExe pkgs.git} pull --branch dev https://github.com/openmesh-network/xnodeos ${cfg.stateDir}/xnodeos'';
         Restart = "always";
         WorkingDirectory = cfg.stateDir;
         StateDirectory = "openmesh-xnode-admin";
