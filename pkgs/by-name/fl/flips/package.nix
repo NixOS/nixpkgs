3ebaf59838f03c3eea5a2bd1d4a7f8e016ab9820
Anderson Torres <torres.anderson.85@protonmail.com>
2024-06-21 01:57:41 -0300

    flips: 0-unstable-2023-03-15 -> 0-unstable-2024-04-17
    - add AndersonTorres as maintainer

--- pkgs/by-name/fl/flips/package.nix
+++ pkgs/by-name/fl/flips/package.nix
@@ -1,32 +1,48 @@
-{ lib, stdenv, fetchFromGitHub, gtk3, libdivsufsort, pkg-config, wrapGAppsHook3 }:
+{
+  lib,
+  fetchFromGitHub,
+  gtk3,
+  libdivsufsort,
+  pkg-config,
+  stdenv,
+  wrapGAppsHook3
+}:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "flips";
-  version = "unstable-2023-03-15";
+  version = "0-unstable-2024-04-17";
 
   src = fetchFromGitHub {
     owner = "Alcaro";
     repo = "Flips";
-    rev = "fdd5c6e34285beef5b9be759c9b91390df486c66";
-    hash = "sha256-uuHgpt7aWqiMTUILm5tAEGGeZrls3g/DdylYQgsfpTw=";
+    rev = "20b0da9ab95d23da89f821bbddedb11b8e0e6531";
+    hash = "sha256-/i/0FvZqMvG4FFIKaOtUe0A5BfG4NXjpBfLYIQOFky8=";
   };
 
-  nativeBuildInputs = [ pkg-config wrapGAppsHook3 ];
-  buildInputs = [ gtk3 libdivsufsort ];
-  patches = [ ./use-system-libdivsufsort.patch ];
-  makeFlags = [ "PREFIX=${placeholder "out"}" ];
-  buildPhase = ''
-    runHook preBuild
-    ./make.sh
-    runHook postBuild
-  '';
+  patches = [ ./000-use-system-libdivsufsort.patch ];
 
-  meta = with lib; {
-    description = "Patcher for IPS and BPS files";
+  nativeBuildInputs = [
+    pkg-config
+    wrapGAppsHook3
+  ];
+
+  buildInputs = [
+    gtk3
+    libdivsufsort
+  ];
+
+  strictDeps = true;
+
+  makeFlags = [
+    "PREFIX=${placeholder "out"}"
+  ];
+
+  meta = {
     homepage = "https://github.com/Alcaro/Flips";
-    license = licenses.gpl3Plus;
-    maintainers = [ ];
-    platforms = platforms.linux;
+    description = "A patcher for IPS and BPS files";
+    license = lib.licenses.gpl3Plus;
     mainProgram = "flips";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.unix;
   };
-}
+})
