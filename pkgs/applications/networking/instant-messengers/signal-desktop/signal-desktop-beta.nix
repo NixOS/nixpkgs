feb0349c7b53979e348a7488e04a93e4481f2da5
Pyrox <pyrox@pyrox.dev>
2024-06-20 22:55:50 -0400

    signal-desktop-beta: 7.13.0-beta.1 -> 7.14.0-beta.1
    (cherry picked from commit fc5e4c49e6d582ef00e171045ff338fe1f527fe7)

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop-beta";
   dir = "Signal Beta";
-  version = "7.13.0-beta.1";
+  version = "7.14.0-beta.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-DvYRvIA+rg4RKXbqWjWj7oFnfLboEiMeP7HgGYkRBDM=";
+  hash = "sha256-SC7CCqylPkc/qmlSYlXJcVWGi1+hvRQ9qBGR6wqo6sk=";
 }
