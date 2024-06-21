5f3fa89d82ad95c5f3ebc90ca562bdcb54a8a9bc
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-21 02:56:48 +0000

    python311Packages.openai: 1.34.0 -> 1.35.3

--- pkgs/development/python-modules/openai/default.nix
+++ pkgs/development/python-modules/openai/default.nix
@@ -25,7 +25,7 @@
 
 buildPythonPackage rec {
   pname = "openai";
-  version = "1.34.0";
+  version = "1.35.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7.1";
@@ -34,7 +34,7 @@ buildPythonPackage rec {
     owner = "openai";
     repo = "openai-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ES0lA/eMll35MhL1evegyp5VOIdHPTRF1Jckl+n250E=";
+    hash = "sha256-xdOfJfoGOSDTPkUhv/dK+AY/1b0WCK3lXWGMoeCiHpk=";
   };
 
   build-system = [
