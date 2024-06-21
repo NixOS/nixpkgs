4826f3aa23ee14a44deb8464a06aa4ae4d88a5dc
Robert Schütz <mail@dotlambda.de>
2024-06-21 03:46:11 -0700

    bitwarden-cli: 2024.4.1 -> 2024.6.0
    Diff: https://github.com/bitwarden/clients/compare/cli-v2024.4.1...cli-v2024.6.0
    
    Changelog: https://github.com/bitwarden/clients/releases/tag/cli-v2024.6.0

--- pkgs/by-name/bi/bitwarden-cli/package.nix
+++ pkgs/by-name/bi/bitwarden-cli/package.nix
@@ -1,7 +1,7 @@
 { lib
 , stdenv
 , buildNpmPackage
-, nodejs_18
+, nodejs_20
 , fetchFromGitHub
 , python3
 , darwin
@@ -10,18 +10,18 @@
 
 buildNpmPackage rec {
   pname = "bitwarden-cli";
-  version = "2024.4.1";
+  version = "2024.6.0";
 
   src = fetchFromGitHub {
     owner = "bitwarden";
     repo = "clients";
     rev = "cli-v${version}";
-    hash = "sha256-Dz7EActqXd97kNxEaNINj2O6TLZWEgHHg1lOIa2+Lt4=";
+    hash = "sha256-qiUUrs23WHE3+KFsWDknuDSA6M3Zwjz9Jdjq6mn5XkE=";
   };
 
-  nodejs = nodejs_18;
+  nodejs = nodejs_20;
 
-  npmDepsHash = "sha256-fjYez3nSDsG5kYtrun3CkDCz1GNAjNlwPzEL+/9qQRU=";
+  npmDepsHash = "sha256-Mgd15eFJtWoBqFFCsjmsnlNbcg5NDs1U7DlMkE0hIb8=";
 
   nativeBuildInputs = [
     python3
@@ -33,7 +33,7 @@ buildNpmPackage rec {
 
   env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
-  npmBuildScript = "build:prod";
+  npmBuildScript = "build:oss:prod";
 
   npmWorkspace = "apps/cli";
 
