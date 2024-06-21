da5ae1bd60b4c4a9398f38b457b2b15774f1a0d3
Diogo Correia <me@diogotc.com>
2024-06-21 23:34:56 +0100

    postgresqlPackages.pgvecto-rs: 0.2.1 -> 0.3.0

--- pkgs/development/tools/rust/cargo-pgrx/buildPgrxExtension.nix
+++ pkgs/development/tools/rust/cargo-pgrx/buildPgrxExtension.nix
@@ -56,6 +56,7 @@
 # dependency here. Set to false and provide rustfmt in nativeBuildInputs, if you need it, e.g.
 # if you include the generated code in the output via postInstall.
 , useFakeRustfmt ? true
+, usePgTestCheckFeature ? true
 , ...
 } @ args:
 let
@@ -96,7 +97,7 @@ let
     pg_ctl stop
   '';
 
-  argsForBuildRustPackage = builtins.removeAttrs args [ "postgresql" "useFakeRustfmt" ];
+  argsForBuildRustPackage = builtins.removeAttrs args [ "postgresql" "useFakeRustfmt" "usePgTestCheckFeature" ];
 
   # so we don't accidentally `(rustPlatform.buildRustPackage argsForBuildRustPackage) // { ... }` because
   # we forgot parentheses
@@ -154,7 +155,7 @@ let
     RUST_BACKTRACE = "full";
 
     checkNoDefaultFeatures = true;
-    checkFeatures = (args.checkFeatures or [ ]) ++ [ "pg_test pg${pgrxPostgresMajor}" ];
+    checkFeatures = (args.checkFeatures or [ ]) ++ (lib.optionals usePgTestCheckFeature [ "pg_test" ]) ++ [ "pg${pgrxPostgresMajor}" ];
   };
 in
 rustPlatform.buildRustPackage finalArgs
