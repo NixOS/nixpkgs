0095aaa5e63a07a83648a7737276b170fb42906d
Stanisław Pitucha <git@viraptor.info>
2024-06-21 20:57:31 +1000

    isa-l: fix darwin aarch64 build
    Apply the upstream patch. The releases don't happen too often.
    Enable tests.

--- pkgs/development/libraries/isa-l/default.nix
+++ pkgs/development/libraries/isa-l/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, nasm }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, autoreconfHook, nasm }:
 
 stdenv.mkDerivation rec {
   pname = "isa-l";
@@ -11,10 +11,24 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-xBBtpjtWyba0DogdLobtuWmiiAHTXMK4oRnjYuTUCNk=";
   };
 
+  patches = [
+    # From https://github.com/intel/isa-l/pull/226
+    (fetchpatch {
+      name = "darwin-aarch64-fold-constant.patch";
+      url = "https://github.com/intel/isa-l/commit/3c784749380f7f167ff7777fd0414f138f9debd5.patch";
+      hash = "sha256-Fctw7xpdfbQySQpjBB8gLe4m1jF6OYmp179dIue2v7c=";
+    })
+  ];
+
   nativeBuildInputs = [ nasm autoreconfHook ];
 
   preConfigure = ''
     export AS=nasm
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    make test
   '';
 
   meta = with lib; {
@@ -24,6 +38,5 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/intel/isa-l";
     maintainers = with maintainers; [ jbedo ];
     platforms = platforms.all;
-    broken = stdenv.isDarwin && stdenv.isAarch64; # does not build on M1 mac (asm/hwcap.h file not found) maybe needs gcc not clang?
   };
 }
