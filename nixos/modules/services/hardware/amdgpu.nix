9ca663df059dbd3ef1919ff862b3b02a9db6e6e7
Atemu <atemu.main@gmail.com>
2024-06-22 03:50:27 +0200

    amdgpu: add kernelModule.patches option

--- nixos/modules/services/hardware/amdgpu.nix
+++ nixos/modules/services/hardware/amdgpu.nix
@@ -42,6 +42,36 @@ in
         This allows for an earlier modeset to apply the preferred resolution in
         the beginning of the initramfs phase rather than after it.
       '';
+
+      patches = lib.mkOption {
+        type = with lib.types; listOf path;
+        default = [ ];
+        description = ''
+          Patches to apply to the kernel for the `amdgpu` kernel module build.
+
+          This is intended for applying small patches concerning only the
+          `amdgpu` module's internals without needing to rebuild the entire
+          kernel.
+
+          The patches are applied to the entire kernel tree but only the
+          `amdgpu` module will actually be built and used. You should therefore
+          not touch anything outside of `drivers/gpu/drm/amd/amdgpu` using the
+          patches as those modifications will not be present in the actual
+          kernel you will be running which might cause undefined and likely
+          erroneous behaviour.
+          Use {option}`boot.kernelPatches` instead for such cases.
+
+          A reboot is required for the patched module to be loaded.
+        '';
+        example = lib.literalExpression ''
+          [
+            (pkgs.fetchpatch2 {
+              url = "https://lore.kernel.org/lkml/20240610-amdgpu-min-backlight-quirk-v1-1-8459895a5b2a@weissschuh.net/raw";
+              hash = "";
+            })
+          ]
+        '';
+      };
     };
 
     opencl.enable = lib.mkEnableOption ''OpenCL support using ROCM runtime library'';
@@ -69,6 +99,13 @@ in
       ];
 
     boot.initrd.kernelModules = lib.optionals cfg.kernelModule.inInitrd [ "amdgpu" ];
+
+    boot.extraModulePackages = lib.mkIf (cfg.kernelModule.patches != [ ]) [
+      (pkgs.callPackage ./amdgpu-kernel-module.nix {
+        inherit (config.boot.kernelPackages) kernel;
+        inherit (cfg.kernelModule) patches;
+      })
+    ];
 
     hardware.graphics = lib.mkIf cfg.opencl.enable {
       enable = lib.mkDefault true;
