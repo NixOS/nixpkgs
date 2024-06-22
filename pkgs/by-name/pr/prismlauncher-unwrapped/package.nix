fc3e31dabe8a8882741e4135f700fa800366bedc
seth <getchoo@tuta.io>
2024-06-22 18:28:11 -0400

    prismlauncher: 8.3 -> 8.4
    Diff: https://github.com/PrismLauncher/PrismLauncher/compare/8.3...8.4
    
    Changelog: https://github.com/PrismLauncher/PrismLauncher/releases/tag/8.4
    (cherry picked from commit 88039bb875585904972c9598a681e63ed2a18263)

--- pkgs/by-name/pr/prismlauncher-unwrapped/package.nix
+++ pkgs/by-name/pr/prismlauncher-unwrapped/package.nix
@@ -32,13 +32,13 @@ assert lib.assertMsg (
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "prismlauncher-unwrapped";
-  version = "8.3";
+  version = "8.4";
 
   src = fetchFromGitHub {
     owner = "PrismLauncher";
     repo = "PrismLauncher";
     rev = finalAttrs.version;
-    hash = "sha256-1YGzCgNdzscnOVeNlHMFJa0RbMo6C2qQjtBOeDxHakI=";
+    hash = "sha256-460hB91M2hZm+uU1tywJEj20oRd5cz/NDvya8/vJdSA=";
   };
 
   nativeBuildInputs = [
