1b78c75b0cdaca2a708bef6af74306649361e4f9
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2024-06-22 12:59:13 +0200

    ear2ctl: init at 0.1.0

---
+++ pkgs/by-name/ea/ear2ctl/package.nix
@@ -0,0 +1,27 @@
+{ lib, rustPlatform, fetchFromGitLab, pkg-config, dbus }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "ear2ctl";
+  version = "0.1.0";
+
+  src = fetchFromGitLab {
+    owner = "bharadwaj-raju";
+    repo = pname;
+    rev = version;
+    hash = "sha256-xaxl4opLMw9KEDpmNcgR1fBGUqO4BP5a/U52Kz+GAvc=";
+  };
+
+  cargoHash = "sha256-ax+/lvdEOjLnwE3Gvji7aaeF9KXjoOXdlTvxYDo8wGI=";
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ dbus ];
+
+  meta = with lib; {
+    description = "Controls for the Nothing Ear (2)";
+    homepage = "https://gitlab.com/bharadwaj-raju/ear2ctl";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+    mainProgram = "ear2ctl";
+  };
+}
