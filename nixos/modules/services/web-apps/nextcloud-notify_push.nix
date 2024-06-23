5845a5d62aeca81118daf33a9a0258d1160ff00b
Jonas Heinrich <onny@project-insanity.org>
2024-06-23 14:30:27 +0200

    nixos/nextcloud-notify_push: Duplicate database settings

--- nixos/modules/services/web-apps/nextcloud-notify_push.nix
+++ nixos/modules/services/web-apps/nextcloud-notify_push.nix
@@ -8,6 +8,7 @@
 
 let
   cfg = config.services.nextcloud.notify_push;
+  cfgD = config.services.nextcloud.notify_push.database;
   cfgN = config.services.nextcloud;
 in
 {
@@ -56,97 +57,118 @@ in
           This is useful when nextcloud's domain is not a static IP address and when the reverse proxy cannot be bypassed because the backend connection is done via unix socket.
         '';
       };
-    }
-    // (lib.genAttrs
-      [
-        "dbtype"
-        "dbname"
-        "dbuser"
-        "dbpassFile"
-        "dbhost"
-        "dbport"
-        "dbtableprefix"
-      ]
-      (
-        opt:
-        options.services.nextcloud.config.${opt}
-        // {
-          default = config.services.nextcloud.config.${opt};
-          defaultText = lib.literalExpression "config.services.nextcloud.config.${opt}";
-        }
-      )
-    );
 
-  config = lib.mkIf cfg.enable {
-    systemd.services = {
-      nextcloud-notify_push = {
-        description = "Push daemon for Nextcloud clients";
-        documentation = [ "https://github.com/nextcloud/notify_push" ];
-        after = [
-          "phpfpm-nextcloud.service"
-          "redis-nextcloud.service"
-        ];
-        wantedBy = [ "multi-user.target" ];
-        environment = {
-          NEXTCLOUD_URL = cfg.nextcloudUrl;
-          SOCKET_PATH = cfg.socketPath;
-          DATABASE_PREFIX = cfg.dbtableprefix;
-          LOG = cfg.logLevel;
-        };
-        script =
-          let
-            dbType = if cfg.dbtype == "pgsql" then "postgresql" else cfg.dbtype;
-            dbUser = lib.optionalString (cfg.dbuser != null) cfg.dbuser;
-            dbPass = lib.optionalString (cfg.dbpassFile != null) ":$DATABASE_PASSWORD";
-            dbHostHasPrefix = prefix: lib.hasPrefix prefix (toString cfg.dbhost);
-            isPostgresql = dbType == "postgresql";
-            isMysql = dbType == "mysql";
-            isSocket = (isPostgresql && dbHostHasPrefix "/") || (isMysql && dbHostHasPrefix "localhost:/");
-            dbHost = lib.optionalString (cfg.dbhost != null) (
-              if isSocket then lib.optionalString isMysql "@localhost" else "@${cfg.dbhost}"
-            );
-            dbOpts = lib.optionalString (cfg.dbhost != null && isSocket) (
-              if isPostgresql then
-                "?host=${cfg.dbhost}"
-              else if isMysql then
-                "?socket=${lib.removePrefix "localhost:" cfg.dbhost}"
-              else
-                throw "unsupported dbtype"
-            );
-            dbName = lib.optionalString (cfg.dbname != null) "/${cfg.dbname}";
-            dbUrl = "${dbType}://${dbUser}${dbPass}${dbHost}${dbName}${dbOpts}";
-          in
-          lib.optionalString (cfg.dbpassFile != null) ''
-            export DATABASE_PASSWORD="$(<"$CREDENTIALS_DIRECTORY/dbpass")"
-          ''
-          + ''
-            export DATABASE_URL="${dbUrl}"
-            exec ${cfg.package}/bin/notify_push '${cfgN.datadir}/config/config.php'
+      database = {
+        dbtype = lib.mkOption {
+          type = lib.types.enum [ "sqlite" "pgsql" "mysql" ];
+          default = cfgN.settings.dbtype;
+          defaultText = lib.literalExpression ''
+            config.services.nextcloud.settings.dbtype
           '';
-        serviceConfig = {
-          User = "nextcloud";
-          Group = "nextcloud";
-          RuntimeDirectory = [ "nextcloud-notify_push" ];
-          Restart = "on-failure";
-          RestartSec = "5s";
-          Type = "notify";
-          LoadCredential = lib.optional (cfg.dbpassFile != null) "dbpass:${cfg.dbpassFile}";
+          description = "Database type.";
+        };
+        dbname = lib.mkOption {
+          type = lib.types.nullOr lib.types.str;
+          default = cfgN.settings.dbname;
+          defaultText = lib.literalExpression ''
+            config.services.nextcloud.settings.dbname
+          '';
+          description = "Database name.";
+        };
+        dbuser = lib.mkOption {
+          type = lib.types.nullOr lib.types.str;
+          default = cfgN.settings.dbuser;
+          defaultText = lib.literalExpression ''
+            config.services.nextcloud.settings.dbuser
+          '';
+          description = "Database user.";
+        };
+        dbhost = lib.mkOption {
+          type = lib.types.nullOr lib.types.str;
+          default = cfgN.settings.dbhost;
+          defaultText = lib.literalExpression ''
+            config.services.nextcloud.settings.dbhost
+          '';
+          description = ''
+            Database host (+port) or socket path. Defaults to the correct unix socket
+            instead if [](#opt-services.nextcloud.database.createLocally) is true and
+            [](#opt-services.nextcloud.settings.dbtype) is either `pgsql` or
+            `mysql`.
+          '';
+        };
+        dbtableprefix = lib.mkOption {
+          type = lib.types.str;
+          default = cfgN.settings.dbtableprefix;
+          defaultText = lib.literalExpression ''
+            config.services.nextcloud.settings.dbtableprefix
+          '';
+          description = "Table prefix in Nextcloud database.";
+        };
+        dbpassFile = lib.mkOption {
+          type = lib.types.nullOr lib.types.str;
+          default = null;
+          description = ''
+            The full path to a file that contains the database password.
+          '';
         };
       };
+    };
 
-      nextcloud-notify_push_setup = {
-        wantedBy = [ "multi-user.target" ];
-        requiredBy = [ "nextcloud-notify_push.service" ];
-        after = [ "nextcloud-notify_push.service" ];
-        serviceConfig = {
-          Type = "oneshot";
-          User = "nextcloud";
-          Group = "nextcloud";
-          ExecStart = "${lib.getExe cfgN.occ} notify_push:setup ${cfg.nextcloudUrl}/push";
-          LoadCredential = config.systemd.services.nextcloud-cron.serviceConfig.LoadCredential;
-          RestartMode = "direct";
-          Restart = "on-failure";
-        };
+  config = lib.mkIf cfg.enable {
+    systemd.services.nextcloud-notify_push = {
+      description = "Push daemon for Nextcloud clients";
+      documentation = [ "https://github.com/nextcloud/notify_push" ];
+      after = [
+        "phpfpm-nextcloud.service"
+        "redis-nextcloud.service"
+      ];
+      wantedBy = [ "multi-user.target" ];
+      environment = {
+        NEXTCLOUD_URL = cfg.nextcloudUrl;
+        SOCKET_PATH = cfg.socketPath;
+        DATABASE_PREFIX = cfgD.dbtableprefix;
+        LOG = cfg.logLevel;
+      };
+      postStart = ''
+        ${cfgN.occ}/bin/nextcloud-occ notify_push:setup ${cfg.nextcloudUrl}/push
+      '';
+      script =
+        let
+          dbType = if cfgD.dbtype == "pgsql" then "postgresql" else cfgD.dbtype;
+          dbUser = lib.optionalString (cfgD.dbuser != null) cfgD.dbuser;
+          dbPass = lib.optionalString (cfgD.dbpassFile != null) ":$DATABASE_PASSWORD";
+          dbHostHasPrefix = prefix: lib.hasPrefix prefix (toString cfgD.dbhost);
+          isPostgresql = dbType == "postgresql";
+          isMysql = dbType == "mysql";
+          isSocket = (isPostgresql && dbHostHasPrefix "/") || (isMysql && dbHostHasPrefix "localhost:/");
+          dbHost = lib.optionalString (cfgD.dbhost != null) (
+            if isSocket then lib.optionalString isMysql "@localhost" else "@${cfgD.dbhost}"
+          );
+          dbOpts = lib.optionalString (cfgD.dbhost != null && isSocket) (
+            if isPostgresql then
+              "?host=${cfgD.dbhost}"
+            else if isMysql then
+              "?socket=${lib.removePrefix "localhost:" cfgD.dbhost}"
+            else
+              throw "unsupported dbtype"
+          );
+          dbName = lib.optionalString (cfgD.dbname != null) "/${cfgD.dbname}";
+          dbUrl = "${dbType}://${dbUser}${dbPass}${dbHost}${dbName}${dbOpts}";
+        in
+        lib.optionalString (dbPass != "") ''
+          export DATABASE_PASSWORD="$(<"${cfgD.dbpassFile}")"
+        ''
+        + ''
+          export DATABASE_URL="${dbUrl}"
+          exec ${cfg.package}/bin/notify_push '${cfgN.datadir}/config/config.php'
+        '';
+      serviceConfig = {
+        User = "nextcloud";
+        Group = "nextcloud";
+        RuntimeDirectory = [ "nextcloud-notify_push" ];
+        Restart = "on-failure";
+        RestartSec = "5s";
+        Type = "notify";
       };
     };
 
