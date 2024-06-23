799243cebf7342c6b27f3de29ffeac5f65755a9b
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-23 14:14:21 +0000

    planify: 4.8.2 -> 4.8.4

--- pkgs/applications/office/planify/default.nix
+++ pkgs/applications/office/planify/default.nix
@@ -28,13 +28,13 @@
 
 stdenv.mkDerivation rec {
   pname = "planify";
-  version = "4.8.2";
+  version = "4.8.4";
 
   src = fetchFromGitHub {
     owner = "alainm23";
     repo = "planify";
     rev = version;
-    hash = "sha256-IwFA/4QGfkMYbuUxbg3/jkrud0MUlEjtkYtCLy+3emc=";
+    hash = "sha256-iQo7ETz5j/Uy5a96XFRkZ0U67dTHWEYLsr/qUi79Y4E=";
   };
 
   nativeBuildInputs = [
