1cb6a3b6cc75318f347907e8b5ddf24d0ea12aec
Francis Gagné <fragag1@gmail.com>
2024-06-22 21:58:37 -0400

    Remove the wxGTK patch for slade
    This patch fails to apply on top of wxWidgets 3.2.5. SLADE 3.2.5 already
    contains a fix for the breaking change that was introduced in wxWidgets
    3.2.4, so the patch is no longer necessary.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36148,16 +36148,7 @@ with pkgs;
   rbdoom-3-bfg = callPackage ../games/doom-ports/rbdoom-3-bfg { };
 
   slade = callPackage ../games/doom-ports/slade {
-    wxGTK = (wxGTK32.overrideAttrs {
-      patches = [
-       (fetchpatch { # required to run slade 3.2.4 on wxGTK 3.2.4, see PR #266945
-         url = "https://github.com/wxWidgets/wxWidgets/commit/425d9455e8307c1267a79d47d77e3dafeb4d86de.patch";
-         excludes = [ "docs/changes.txt" ];
-         revert = true;
-         hash = "sha256-6LOYLDLtVCHxNdHAWv3zhlCsljIpi//RJb9XVLGD5hM=";
-       })
-     ];
-    }).override {
+    wxGTK = wxGTK32.override {
       withWebKit = true;
     };
   };
