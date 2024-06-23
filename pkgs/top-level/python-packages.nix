f446ca6c6496ebe230c58532ee40ea067c1589cc
Daniel Baker <daniel.n.baker@gmail.com>
2024-06-22 21:35:14 -0700

    python3Packages.bdsim: init at 1.1.1
    Co-authored-by: Anthony Camarillo <anthony.camarillo.96@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1470,6 +1470,8 @@ self: super: with self; {
 
   bdffont = callPackage ../development/python-modules/bdffont { };
 
+  bdsim = callPackage ../development/python-modules/bdsim { };
+
   beaker = callPackage ../development/python-modules/beaker { };
 
   before-after = callPackage ../development/python-modules/before-after { };
