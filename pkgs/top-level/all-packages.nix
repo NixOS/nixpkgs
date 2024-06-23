c7dd09e34294f6a7479c116ff1b12b9ec6fbaaec
Francis Gagné <fragag1@gmail.com>
2024-06-23 00:39:40 -0400

    sladeUnstable: 3.2.4-unstable-2023-09-30 -> 3.2.6-unstable-2024-11-26
    Additionally:
    
    - Remove obsolete postPatch.
    - Add wrapGAppsHook3 to nativeBuildInputs. This is taken from the slade
      package. This fixes the application crashing with "No GSettings
      schemas are installed on the system" when using File > Open or File >
      Open Directory.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16524,16 +16524,7 @@ with pkgs;
   };
 
   sladeUnstable = callPackage ../games/doom-ports/slade/git.nix {
-    wxGTK = (wxGTK32.overrideAttrs {
-      patches = [
-       (fetchpatch { # required to run sladeUnstable unstable-2023-09-30 on wxGTK 3.2.4, see PR #266945
-         url = "https://github.com/wxWidgets/wxWidgets/commit/425d9455e8307c1267a79d47d77e3dafeb4d86de.patch";
-         excludes = [ "docs/changes.txt" ];
-         revert = true;
-         hash = "sha256-6LOYLDLtVCHxNdHAWv3zhlCsljIpi//RJb9XVLGD5hM=";
-       })
-     ];
-    }).override {
+    wxGTK = wxGTK32.override {
       withWebKit = true;
     };
   };
