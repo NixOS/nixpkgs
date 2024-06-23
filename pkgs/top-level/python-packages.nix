a50463dbcb14ed23c21bbb84b02082111c5d8c48
Daniel Baker <daniel.n.baker@gmail.com>
2024-06-22 20:59:41 -0700

    python3Packages.ansitable: init at 0.10.0
    Co-authored-by: Anthony Camarillo <anthony.camarillo.96@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -621,6 +621,8 @@ self: super: with self; {
 
   ansimarkup = callPackage ../development/python-modules/ansimarkup { };
 
+  ansitable = callPackage ../development/python-modules/ansitable { };
+
   ansiwrap = callPackage ../development/python-modules/ansiwrap { };
 
   anthemav = callPackage ../development/python-modules/anthemav { };
