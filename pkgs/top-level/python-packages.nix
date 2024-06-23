41a7b510e07802a580347a063466c270e2ac31c1
Daniel Baker <daniel.n.baker@gmail.com>
2024-06-22 21:31:37 -0700

    python3Packages.pgraph-python: init at 0.6.2
    Co-authored-by: Anthony Camarillo <anthony.camarillo.96@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9998,6 +9998,8 @@ self: super: with self; {
 
   pypsrp = callPackage ../development/python-modules/pypsrp { };
 
+  pgraph-python = callPackage ../development/python-modules/pgraph-python { };
+
   phpserialize = callPackage ../development/python-modules/phpserialize { };
 
   phunspell = callPackage ../development/python-modules/phunspell { };
