10962c7ca2f5003de1109330316337d22b782f15
Ryze <50497128+ryze312@users.noreply.github.com>
2024-06-23 18:10:59 +0300

    nixos/zerotierone: fix default value for localConf

--- nixos/modules/services/networking/zerotierone.nix
+++ nixos/modules/services/networking/zerotierone.nix
@@ -34,7 +34,7 @@ in
   options.services.zerotierone.package = mkPackageOption pkgs "zerotierone" { };
 
   options.services.zerotierone.localConf = mkOption {
-    default = null;
+    default = {};
     description = ''
       Optional configuration to be written to the Zerotier JSON-based local.conf.
       If set, the configuration will be symlinked to `/var/lib/zerotier-one/local.conf` at build time.
