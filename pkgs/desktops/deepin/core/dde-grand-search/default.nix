4f1aacd85893252150cb4be157c108bedc1dc503
rewine <luhongxu@deepin.org>
2024-06-24 22:33:41 +0800

    deepin.dde-grand-search: fix build on aarch64-linux

--- pkgs/desktops/deepin/core/dde-grand-search/default.nix
+++ pkgs/desktops/deepin/core/dde-grand-search/default.nix
@@ -14,6 +14,7 @@
 , taglib
 , ffmpeg
 , ffmpegthumbnailer
+, pcre
 , dbus
 }:
 
@@ -45,6 +46,7 @@ stdenv.mkDerivation rec {
     taglib
     ffmpeg
     ffmpegthumbnailer
+    pcre
   ];
 
   patches = [
