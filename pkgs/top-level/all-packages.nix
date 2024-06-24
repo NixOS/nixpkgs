e53d176f272fb8351799ee922b0930cc27dcbf0e
zxexz <eric@genhealth.ai>
2024-06-24 08:16:19 +0000

    fix json formatting. revert cudaPackages_12 version (current cudnn in nixpkgs, supports up to 12.3, need to upgrade to latest)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7060,7 +7060,7 @@ with pkgs;
   cudaPackages_12_3 = callPackage ./cuda-packages.nix { cudaVersion = "12.3"; };
   cudaPackages_12_4 = callPackage ./cuda-packages.nix { cudaVersion = "12.4"; };
   cudaPackages_12_5 = callPackage ./cuda-packages.nix { cudaVersion = "12.5"; };
-  cudaPackages_12 = cudaPackages_12_5; # Latest supported by cudnn
+  cudaPackages_12 = cudaPackages_12_3; # Latest supported by cudnn
 
   cudaPackages = recurseIntoAttrs cudaPackages_12;
 
