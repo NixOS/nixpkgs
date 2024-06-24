10680f39b762d00685cb8d516f1253632e46878e
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-24 19:07:15 +0000

    bilibili: 1.13.5-2 -> 1.13.6-2

--- pkgs/by-name/bi/bilibili/package.nix
+++ pkgs/by-name/bi/bilibili/package.nix
@@ -8,10 +8,10 @@
 
 stdenv.mkDerivation rec {
   pname = "bilibili";
-  version = "1.13.5-2";
+  version = "1.13.6-2";
   src = fetchurl {
     url = "https://github.com/msojocs/bilibili-linux/releases/download/v${version}/io.github.msojocs.bilibili_${version}_amd64.deb";
-    hash = "sha256-4p5izaQsax4i2/AnjivDVoY205ITmEx48GhblPOZDjk=";
+    hash = "sha256-SVN3iw3o3QGLY0XWd48+kpNPYoK5V8oPOA10VyDgDMo=";
   };
 
   unpackPhase = ''
