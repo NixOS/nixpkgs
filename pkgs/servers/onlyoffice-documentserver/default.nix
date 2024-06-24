d97954a2c6a54d85e80e835eeb7ea5650c034a05
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-24 15:58:54 +0000

    onlyoffice-documentserver: 7.5.1 -> 8.1.0

--- pkgs/servers/onlyoffice-documentserver/default.nix
+++ pkgs/servers/onlyoffice-documentserver/default.nix
@@ -15,11 +15,11 @@ let
   # var/www/onlyoffice/documentserver/server/DocService/docservice
   onlyoffice-documentserver = stdenv.mkDerivation rec {
     pname = "onlyoffice-documentserver";
-    version = "7.5.1";
+    version = "8.1.0";
 
     src = fetchurl {
       url = "https://github.com/ONLYOFFICE/DocumentServer/releases/download/v${lib.concatStringsSep "." (lib.take 3 (lib.splitVersion version))}/onlyoffice-documentserver_amd64.deb";
-      sha256 = "sha256-191PYpxs/TbVXoBPHvuyTp81ZMtw1YaFznY1hUSbh+0=";
+      sha256 = "sha256-vDMgLNts7CJ8Un2WMLUnWdq6PucTh8wlsEEBP32lHgQ=";
     };
 
     preferLocalBuild = true;
