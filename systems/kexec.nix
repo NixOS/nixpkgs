a421dfa10b55502ecca431a01ee22227e727799c
dowdt <tomas.dowd@openmesh.network>
2024-06-25 14:06:41 +1000

    Made kexec match netboot options

--- systems/kexec.nix
+++ systems/kexec.nix
@@ -5,6 +5,8 @@ let
       ../repo/modules/services/openmesh/xnode/admin.nix
     ];
     config = {
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      nix.settings.trusted-users = [ "root" "xnode" "openmesh-xnode-admin" ];
       documentation = {
         nixos = {
           enable = false;
@@ -18,10 +20,12 @@ let
           xnode = {
             admin = {
               enable = true;
+              remoteDir = "https://dpl-staging.openmesh.network/xnodes/functions";
             };
           };
         };
         getty = {
+          autologinUser = lib.mkForce "xnode";
           greetingLine = ''<<< Welcome to Openmesh XnodeOS ${config.system.nixos.label} (\m) - \l >>>'';
         };
       };
@@ -32,6 +36,46 @@ let
       };
       netboot = {
         squashfsCompression = "gzip -Xcompression-level 1";
+      };
+      boot = {
+        postBootCommands = ''
+                            cat > /etc/nixos/configuration.nix <<ENDFILE
+                            {config, lib, pkgs, ...}:{
+                              imports=[./hardware-configuration.nix] ++ lib.optional (builtins.pathExists /var/lib/openmesh-xnode-admin/config.nix) /var/lib/openmesh-xnode-admin/config.nix ++ lib.optional (builtins.pathExists /var/lib/openmesh-xnode-admin/xnodeos) /var/lib/openmesh-xnode-admin/xnodeos/repo/modules/services/openmesh/xnode/admin.nix ;
+                              config = {
+                                nix.settings.experimental-features = [ "nix-command" "flakes" ];
+                                nix.settings.trusted-users = [ "root" "xnode" "openmesh-xnode-admin" ];
+                                boot.loader.grub.enable=false;
+                                nixpkgs.config.allowUnfree = true;
+                                services.openmesh.xnode.admin = {
+                                  enable = true;
+                                  remoteDir = "https://dpl-staging.openmesh.network/xnodes/functions";
+                                };
+                                users.users = {
+                                  "xnode" = {
+                                    isNormalUser = true;
+                                    password = "xnode";
+                                    extraGroups = [ "wheel" ];
+                                  };
+                                };
+                              };
+                            }
+                            ENDFILE
+
+                            cat > /etc/nixos/hardware-configuration.nix <<ENDFILE
+                            {config, lib, pkgs, modulesPath, ...}: {
+                              fileSystems = {
+                                "/" = {
+                                  device = "none";
+                                  fsType = "tmpfs";
+                                };
+                              };
+
+                              networking.useDHCP = lib.mkForce true;
+                              nixpkgs.hostPlatform=lib.mkDefault "x86_64-linux";
+                            }
+                            ENDFILE
+                          '';
       };
       networking = {
         hostName = "xnode";
