5469ec02dda8b14eee20c90070e13a9707f9501a
harrys522 <hsackman@protonmail.com>
2024-06-25 14:25:17 +1000

    abstracted common nix configuration

--- systems/kexec.nix
+++ systems/kexec.nix
@@ -1,52 +1,6 @@
 { inputs, ... }@flakeContext:
 let
-  kexec = { config, lib, pkgs, ... }: {
-    imports = [
-      ../repo/modules/services/openmesh/xnode/admin.nix
-    ];
-    config = {
-      documentation = {
-        nixos = {
-          enable = false;
-        };
-        doc = {
-          enable = false;
-        };
-      };
-      services = {
-        openmesh = {
-          xnode = {
-            admin = {
-              enable = true;
-            };
-          };
-        };
-        getty = {
-          greetingLine = ''<<< Welcome to Openmesh XnodeOS ${config.system.nixos.label} (\m) - \l >>>'';
-        };
-      };
-      environment = {
-        systemPackages = with pkgs; [
-          nyancat
-        ];
-      };
-      netboot = {
-        squashfsCompression = "gzip -Xcompression-level 1";
-      };
-      networking = {
-        hostName = "xnode";
-      };
-      users = {
-        users = {
-          xnode = {
-            isNormalUser = true;
-            password = "xnode";
-            extraGroups = [ "wheel" ];
-          };
-        };
-      };
-    };
-  };
+  kexec = import ./xnode-common.nix;
 in
 inputs.nixos-generators.nixosGenerate {
   system = "x86_64-linux";
