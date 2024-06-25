4299680f26e25cb524d6e4ee2d388e2ace540a6c
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-06-25 16:59:37 -0400

    stevenblack-blocklist: move to `pkgs/by-name/st`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13207,8 +13207,6 @@ with pkgs;
 
   staticjinja = with python3.pkgs; toPythonApplication staticjinja;
 
-  stevenblack-blocklist  = callPackage ../tools/networking/stevenblack-blocklist { };
-
   stress = callPackage ../tools/system/stress { };
 
   stress-ng = callPackage ../tools/system/stress-ng { };
