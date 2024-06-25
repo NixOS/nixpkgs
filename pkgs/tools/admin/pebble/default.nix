38572248116762d34783858425d0947153753651
Alexis Hildebrandt <afh@surryhill.net>
2024-06-25 07:50:17 +0200

    pebble: 2.4.0 -> 2.6.0

--- pkgs/tools/admin/pebble/default.nix
+++ pkgs/tools/admin/pebble/default.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "pebble";
-  version = "2.4.0";
+  version = "2.6.0";
 
   src = fetchFromGitHub {
     owner = "letsencrypt";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0sh67bzq3hlagk73w2kp45viq15g2rcxm760jk9fqshamq784m6m";
+    hash = "sha256-YPU/bl7h6rOWg+5ut0Thn2UupeKpJ7u4KXc2svIeZEM=";
   };
 
   vendorHash = null;
@@ -28,8 +28,6 @@ buildGoModule rec {
   };
 
   meta = {
-    # ca/ca.go:374:67: 9223372038 (untyped int constant) overflows uint
-    broken = stdenv.hostPlatform.is32bit;
     homepage = "https://github.com/letsencrypt/pebble";
     description = "Small RFC 8555 ACME test server";
     longDescription = "Miniature version of Boulder, Pebble is a small RFC 8555 ACME test server not suited for a production CA";
