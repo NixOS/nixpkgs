f6d220c971f836ae74ac6cc587af3986dc7c6480
Alexis Hildebrandt <afh@surryhill.net>
2024-06-25 07:50:03 +0200

    pebble: fix version info

--- pkgs/tools/admin/pebble/default.nix
+++ pkgs/tools/admin/pebble/default.nix
@@ -1,5 +1,4 @@
 { lib
-, stdenv
 , buildGoModule
 , fetchFromGitHub
 , nixosTests
@@ -18,6 +17,11 @@ buildGoModule rec {
 
   vendorHash = null;
 
+  ldflags = [
+    "-s" "-w"
+    "-X main.version=${version}"
+  ];
+
   passthru.tests = {
     smoke-test = nixosTests.acme;
   };
@@ -26,8 +30,10 @@ buildGoModule rec {
     # ca/ca.go:374:67: 9223372038 (untyped int constant) overflows uint
     broken = stdenv.hostPlatform.is32bit;
     homepage = "https://github.com/letsencrypt/pebble";
-    description = "Miniature version of Boulder, Pebble is a small RFC 8555 ACME test server not suited for a production CA";
+    description = "Small RFC 8555 ACME test server";
+    longDescription = "Miniature version of Boulder, Pebble is a small RFC 8555 ACME test server not suited for a production CA";
     license = [ lib.licenses.mpl20 ];
+    mainProgram = "pebble";
     maintainers = lib.teams.acme.members;
   };
 }
