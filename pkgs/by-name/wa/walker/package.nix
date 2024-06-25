64c7cf7c3e5e71813f2ff34eb46bd47b6e019632
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-25 23:34:55 +0000

    walker: 0.0.71 -> 0.0.72

--- pkgs/by-name/wa/walker/package.nix
+++ pkgs/by-name/wa/walker/package.nix
@@ -11,13 +11,13 @@
 
 buildGoModule rec {
   pname = "walker";
-  version = "0.0.71";
+  version = "0.0.72";
 
   src = fetchFromGitHub {
     owner = "abenz1267";
     repo = "walker";
     rev = "v${version}";
-    hash = "sha256-ShLwQBWhaoeHijiQCMO3n0DnhNaEd8GOrYMcli5EIZs=";
+    hash = "sha256-F100XVtWtY+yxGrMVCrkR/ayM2/aoko1ec0FFgNMhww=";
   };
 
   vendorHash = "sha256-zDntJ695k8dbwyFXbg9PapWD335MHrWbep1xxzXNIL4=";
