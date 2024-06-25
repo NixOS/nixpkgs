62d9e7b6fdee1774769d013fa786f35d604ac64c
Pyrox <pyrox@pyrox.dev>
2024-06-25 17:37:35 -0400

    nodePackages.autoprefixer: Remove from node-packages
    Moves to pkgs/by-name.

--- pkgs/development/node-packages/aliases.nix
+++ pkgs/development/node-packages/aliases.nix
@@ -56,6 +56,7 @@ mapAliases {
   "@zwave-js/server" = pkgs.zwave-js-server; # Added 2023-09-09
   alloy = pkgs.titanium-alloy; # added 2023-08-17
   antennas = pkgs.antennas; # added 2023-07-30
+  inherit (pkgs) autoprefixer; # added 2024-06-25
   inherit (pkgs) asar; # added 2023-08-26
   inherit (pkgs) auto-changelog; # added 2024-06-25
   inherit (pkgs) aws-azure-login; # added 2023-09-30
