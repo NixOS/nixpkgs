NixOS/nixpkgs

commit c01ada8c551ff651df8ab95f30f1dabc43143c34

audit: 3.1.2 -> 4.0
https://github.com/linux-audit/audit-userspace/blob/v4.0/ChangeLog

pkgs/os-specific/linux/audit/default.nix
@@ -16,11 +16,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "audit";
-  version = "3.1.2";
+  version = "4.0";
 
   src = fetchurl {
     url = "https://people.redhat.com/sgrubb/audit/audit-${finalAttrs.version}.tar.gz";
-    hash = "sha256-wLF5LR8KiMbxgocQUJy7mHBZ/GhxLJdmnKkOrhA9KH0=";
+    hash = "sha256-v0ItQSard6kqTDrDneVHPyeNw941ck0lGKSMe+FdVNg=";
   };
 
   postPatch = ''

commit 6fc7779cf3e5f538902f6238c54a50270c015751

audit.tests.musl: init
This would have caught the build regression introduced by
c01ada8c551f ("audit: 3.1.2 -> 4.0") and fixed by
8fbb95ca8dea ("pkgsMusl.audit: fix build").

pkgs/os-specific/linux/audit/default.nix
@@ -8,6 +8,7 @@
 , linuxHeaders
 , python3
 , swig
+, pkgsCross
 
 # Enabling python support while cross compiling would be possible, but the
 # configure script tries executing python to gather info instead of relying on
@@ -73,6 +74,10 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   enableParallelBuilding = true;
+
+  passthru.tests = {
+    musl = pkgsCross.musl64.audit;
+  };
 
   meta = {
     homepage = "https://people.redhat.com/sgrubb/audit/";