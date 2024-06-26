9b78c3760a564141e9f8d836748db9db3975cad2
Victor Engmark <victor@engmark.name>
2024-06-26 18:09:13 +0200

    jekyll: Quote variable dereferencing
    As per `shellcheck`:
    
    > SC2086 (info): Double quote to prevent globbing and word splitting.

--- pkgs/applications/misc/jekyll/update.sh
+++ pkgs/applications/misc/jekyll/update.sh
@@ -3,7 +3,7 @@
 
 set -o errexit -o nounset
 
-readonly BASEDIR="$(dirname $(readlink -f $0))"
+readonly BASEDIR="$(dirname "$(readlink -f "$0")")"
 
 for directory in "basic" "full"; do
   pushd "$BASEDIR/$directory"
