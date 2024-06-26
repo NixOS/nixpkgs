a7d82e802363df539cb7fdd9fe1df7f902eb6aa2
seth <getchoo@tuta.io>
2024-06-25 20:51:44 -0400

    identity: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31646,8 +31646,6 @@ with pkgs;
 
   id3v2 = callPackage ../applications/audio/id3v2 { };
 
-  identity = callPackage ../applications/graphics/identity { };
-
   ifenslave = callPackage ../os-specific/linux/ifenslave { };
 
   ii = callPackage ../applications/networking/irc/ii {
