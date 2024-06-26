df7a3621699f3c29c658d0a9fc098b115b6b6d4f
Victor Engmark <victor@engmark.name>
2024-06-26 18:14:32 +0200

    jekyll: Use idiomatic lowercase un-exported variable

--- pkgs/applications/misc/jekyll/update.sh
+++ pkgs/applications/misc/jekyll/update.sh
@@ -3,10 +3,10 @@
 
 set -o errexit -o nounset
 
-BASEDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 for directory in "basic" "full"; do
-  pushd "$BASEDIR/$directory"
+  pushd "$script_dir/$directory"
   rm -f Gemfile.lock gemset.nix
   BUNDLE_FORCE_RUBY_PLATFORM=true bundix --magic
   rm -rf .bundle vendor
