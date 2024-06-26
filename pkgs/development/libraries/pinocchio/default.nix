54f0f3829c4efaaa4fbd518ee0e412ff78d63b40
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-26 10:48:15 +0200

    pinocchio: deactivate one failing test on aarch64

--- pkgs/development/libraries/pinocchio/default.nix
+++ pkgs/development/libraries/pinocchio/default.nix
@@ -30,6 +30,9 @@ stdenv.mkDerivation (finalAttrs: {
     substituteInPlace unittest/CMakeLists.txt \
       --replace-fail "add_pinocchio_unit_test(contact-cholesky)" ""
   '' + lib.optionalString (stdenv.isLinux && stdenv.isAarch64) ''
+    # test failure, ref https://github.com/stack-of-tasks/pinocchio/issues/2304
+    substituteInPlace unittest/CMakeLists.txt \
+      --replace-fail "add_pinocchio_unit_test(contact-models)" ""
     # test failure, ref https://github.com/stack-of-tasks/pinocchio/issues/2277
     substituteInPlace unittest/algorithm/utils/CMakeLists.txt \
       --replace-fail "add_pinocchio_unit_test(force)" ""
