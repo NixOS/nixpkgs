01ba8ea798944eeb02cb97296f6948ff0f00e3ac
iivusly <iivusly@icloud.com>
2024-06-27 16:21:23 -0700

    gtk-mac-bundler: fix typo & use python2

--- pkgs/development/tools/gtk-mac-bundler/default.nix
+++ pkgs/development/tools/gtk-mac-bundler/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchFromGitHub }:
+{ stdenv, lib, python2, makeWrapper, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "gtk-mac-bundler";
@@ -11,19 +11,26 @@ stdenv.mkDerivation rec {
     sha256 = "1kyyq2hc217i5vhbfff0ldgv0r3aziwryd1xlck5cw3s6hgskbza";
   };
 
+  buildInputs = [ python2 ];
+  nativeBuildInputs = [ makeWrapper ];
+
   installPhase = ''
     mkdir -p $out/bin
     substitute gtk-mac-bundler.in $out/bin/gtk-mac-bundler \
       --subst-var-by PATH $out/share
     chmod a+x $out/bin/gtk-mac-bundler
+    wrapProgram $out/bin/gtk-mac-bundler \
+      --set PATH ${lib.makeBinPath [
+        python2
+      ]}
 
     mkdir -p $out/share
     cp -r bundler $out/share
   '';
 
   meta = with lib; {
-    description = "Helper script that creates application bundles form GTK executables for macOS";
-    maintainers = [ maintainers.matthewbauer ];
+    description = "Helper script that creates application bundles from GTK executables for macOS";
+    maintainers = [ maintainers.matthewbauer maintainers.iivusly ];
     platforms = platforms.darwin;
     homepage = "https://gitlab.gnome.org/GNOME/gtk-mac-bundler";
     license = licenses.gpl2;
