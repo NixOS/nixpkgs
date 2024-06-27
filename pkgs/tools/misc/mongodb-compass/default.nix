4f89a5f1ff73bb6cb195f8806393b4d08c1f690f
Ludovico Piero <lewdovico@gnuweeb.org>
2024-06-27 16:32:04 +0900

    mongodb-compass: 1.43.0 -> 1.43.2
    Release Notes: https://www.mongodb.com/docs/compass/current/release-notes/
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pkgs/tools/misc/mongodb-compass/default.nix
+++ pkgs/tools/misc/mongodb-compass/default.nix
@@ -33,7 +33,7 @@ xorg,
 }:
 
 let
-  version = "1.43.0";
+  version = "1.43.2";
 
   rpath = lib.makeLibraryPath [
     alsa-lib
@@ -82,7 +82,7 @@ let
     if stdenv.hostPlatform.system == "x86_64-linux" then
       fetchurl {
         url = "https://downloads.mongodb.com/compass/mongodb-compass_${version}_amd64.deb";
-        sha256 = "sha256-hzPhF0NGwv+Lm+q5SoS8qv10UmuKf4RarGMkEeCxp9w=";
+        hash = "sha256-idOFt60MlspB8Bm9HbLAhd/F1zhkHSVmdTxvCsgjGvk=";
       }
     else
       throw "MongoDB compass is not supported on ${stdenv.hostPlatform.system}";
