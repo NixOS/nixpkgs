fe312ba6e413f5f48fbbefefd830c18a649eb157
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-27 10:41:42 +0000

    cloudlog: 2.6.14 -> 2.6.15

--- pkgs/applications/radio/cloudlog/default.nix
+++ pkgs/applications/radio/cloudlog/default.nix
@@ -8,13 +8,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "cloudlog";
-  version = "2.6.14";
+  version = "2.6.15";
 
   src = fetchFromGitHub {
     owner = "magicbug";
     repo = "Cloudlog";
     rev = version;
-    hash = "sha256-nsn/pvlFRDGUnm/X5pyzlKWgP6OlfVn3Mdj6vOJZMWQ=";
+    hash = "sha256-G+PnzyOG/HZ8I66BHdtK0GOUF7ATrTYpzM9sVaSjMDQ=";
   };
 
   postPatch = ''
