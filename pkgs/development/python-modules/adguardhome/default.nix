7bdd2ba1367d4ebe4a1379e64140e8a48b58b293
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-27 10:28:11 +0200

    python312Packages.adguardhome: 0.6.3 -> 0.7.0
    Diff: https://github.com/frenck/python-adguardhome/compare/refs/tags/v0.6.3...v0.7.0
    
    Changelog: https://github.com/frenck/python-adguardhome/releases/tag/v0.7.0

--- pkgs/development/python-modules/adguardhome/default.nix
+++ pkgs/development/python-modules/adguardhome/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "adguardhome";
-  version = "0.6.3";
+  version = "0.7.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.11";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "frenck";
     repo = "python-${pname}";
     rev = "refs/tags/v${version}";
-    hash = "sha256-V8SsWsGYmUhR9/yV6BZBK1UjYGHlDrXrF8nt0eZbTnI=";
+    hash = "sha256-n55G6ulKcgSSrgPk70D52OO9fp3WURlcRhJQUKrZ1Nk=";
   };
 
   postPatch = ''
