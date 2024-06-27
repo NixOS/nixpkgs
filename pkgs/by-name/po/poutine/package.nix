3cec93f1900c64d7587498308afe388b1ad6e005
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-27 10:10:55 +0200

    poutine: 0.12.0 -> 0.13.0
    Diff: https://github.com/boostsecurityio/poutine/compare/refs/tags/v0.12.0...v0.13.0
    
    Changelog: https://github.com/boostsecurityio/poutine/releases/tag/v0.13.0

--- pkgs/by-name/po/poutine/package.nix
+++ pkgs/by-name/po/poutine/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "poutine";
-  version = "0.12.0";
+  version = "0.13.0";
 
   src = fetchFromGitHub {
     owner = "boostsecurityio";
     repo = "poutine";
     rev = "refs/tags/v${version}";
-    hash = "sha256-mB/d3EWBWHx7zrMGRPGOJc2uD3MlhXV8H2WfNiKlzdE=";
+    hash = "sha256-9vbK2tc57e/YNfhSVbCMxnzOmmahr9T3x5Tt7GQjVnc=";
   };
 
   vendorHash = "sha256-HYuyGSatUOch73IKc7/9imhwz0Oz6Mrccs2HKVQtaVE=";
