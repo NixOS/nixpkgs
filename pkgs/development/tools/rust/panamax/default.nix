2a0beb52294a22f28a2eb0c2a1d41aba088d6dfd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-06-28 01:34:57 +0200

    panamax: add missing darwin frameworks

--- pkgs/development/tools/rust/panamax/default.nix
+++ pkgs/development/tools/rust/panamax/default.nix
@@ -28,6 +28,7 @@ rustPlatform.buildRustPackage rec {
     zlib
   ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
+    darwin.apple_sdk.frameworks.SystemConfiguration
   ];
 
   meta = with lib; {
