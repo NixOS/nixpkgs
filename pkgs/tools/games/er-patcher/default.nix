03af74117ef69f1deffd04e7a8cf54b62732906e
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-27 05:27:54 +0000

    er-patcher: 1.12-1 -> 1.12-2

--- pkgs/tools/games/er-patcher/default.nix
+++ pkgs/tools/games/er-patcher/default.nix
@@ -6,13 +6,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "er-patcher";
-  version = "1.12-1";
+  version = "1.12-2";
 
   src = fetchFromGitHub {
     owner = "gurrgur";
     repo = "er-patcher";
     rev = "v${version}";
-    sha256 = "sha256-IN7JKaPexc6XLOymBWKbwtjhIlSEy3NhXoPpLiIc7qY=";
+    sha256 = "sha256-C+QtPpvEuYAYxYGs2lbYLaYkDge6r0RNCISglkQpjEQ=";
   };
 
   buildInputs = [
