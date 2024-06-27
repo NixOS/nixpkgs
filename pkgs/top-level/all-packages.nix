58417cac9520d721b79fbe4524da19d0c6a36c12
Tomodachi94 <tomodachi94@protonmail.com>
2024-06-26 17:10:11 -0700

    batik: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29618,8 +29618,6 @@ with pkgs;
 
   bashSnippets = callPackage ../applications/misc/bashSnippets { };
 
-  batik = callPackage ../applications/graphics/batik { };
-
   batsignal = callPackage ../applications/misc/batsignal { };
 
   baudline = callPackage ../applications/audio/baudline { };
