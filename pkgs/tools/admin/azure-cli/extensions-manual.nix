0023491a0970cd577582f856d13d8485439d9e42
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-06-28 16:56:07 +0200

    azure-cli-extensions.azure-devops: add katexochen as maintainer
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/tools/admin/azure-cli/extensions-manual.nix
+++ pkgs/tools/admin/azure-cli/extensions-manual.nix
@@ -16,6 +16,7 @@
     sha256 = "658a2854d8c80f874f9382d421fa45abf6a38d00334737dda006f8dec64cf70a";
     description = "Tools for managing Azure DevOps";
     propagatedBuildInputs = with python3Packages; [ distro ];
+    meta.maintainers = with lib.maintainers; [ katexochen ];
   };
 
   rdbms-connect = mkAzExtension rec {
