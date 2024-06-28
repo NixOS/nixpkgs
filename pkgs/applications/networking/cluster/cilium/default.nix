6911de5be9906a2ad57ee3b0eebca9b017551f33
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-28 00:31:06 +0000

    cilium-cli: 0.16.10 -> 0.16.11

--- pkgs/applications/networking/cluster/cilium/default.nix
+++ pkgs/applications/networking/cluster/cilium/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "cilium-cli";
-  version = "0.16.10";
+  version = "0.16.11";
 
   src = fetchFromGitHub {
     owner = "cilium";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-SgAqq9tT4Rtg1AvoUsDvR5cCLIOuHwNUFN2NOheciYw=";
+    hash = "sha256-muIVjT6ssNejp5O8ju06h9e7ZVcfwXyHgQIa0IEP4Xg=";
   };
 
   vendorHash = null;
