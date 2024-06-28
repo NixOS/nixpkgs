d1cbcec1e6a3e6b30a5e1caf3a8ac2a0343fa458
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-28 12:23:40 +0000

    gcsfuse: 2.2.0 -> 2.3.0

--- pkgs/tools/filesystems/gcsfuse/default.nix
+++ pkgs/tools/filesystems/gcsfuse/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "gcsfuse";
-  version = "2.2.0";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "googlecloudplatform";
     repo = "gcsfuse";
     rev = "v${version}";
-    hash = "sha256-ecTwC0FHg0MHBkrsydTam4UfylldZQp06t/1BYUwgUI=";
+    hash = "sha256-6Gn8+C7M2iCSq4bL4NfqUa8O/1OyP1GBv2gpxmRrRSM=";
   };
 
-  vendorHash = "sha256-GmLyYtgiUBd09mUktjNhbPN5QCUb9VNUzA0xT5mzQ0Q=";
+  vendorHash = "sha256-9eUUjcRQ/H3iPO2F+KE0nd5+b8slc6xacXSZt3jytgU=";
 
   subPackages = [ "." "tools/mount_gcsfuse" ];
 
