9ba2820ace1c796fa10fc6a9a1589d8e65edfd9f
Afnan <github@afnanarise.com>
2024-06-29 16:07:51 +0600

    github-desktop: 3.3.12 -> 3.3.18

--- pkgs/by-name/gi/github-desktop/package.nix
+++ pkgs/by-name/gi/github-desktop/package.nix
@@ -20,22 +20,22 @@
 }:
 
 let
-  rcversion = "2";
+  rcversion = "1";
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "github-desktop";
-  version = "3.3.12";
+  version = "3.3.18";
 
   src =
     let
       urls = {
         "x86_64-linux" = {
           url = "https://github.com/shiftkey/desktop/releases/download/release-${finalAttrs.version}-linux${rcversion}/GitHubDesktop-linux-amd64-${finalAttrs.version}-linux${rcversion}.deb";
-          hash = "sha256-iflKD7NPuZvhxviNW8xmtCOYgdRz1rXiG42ycWCjXiY=";
+          hash = "sha256-0fxn8bkj0hi68986y1ms5r06xffhahyxb656k4xkyglyqa2mfx85=";
         };
         "aarch64-linux" = {
           url = "https://github.com/shiftkey/desktop/releases/download/release-${finalAttrs.version}-linux${rcversion}/GitHubDesktop-linux-arm64-${finalAttrs.version}-linux${rcversion}.deb";
-          hash = "sha256-C9eCvuf/TwXQiYjZ88xSiyaqi8+cppmrLiSYTyQCkmg=";
+          hash = "sha256-1i5kha334ilgp2c0lc4bfrpmcjr7z5wki0b6vqc826s9ar4vcngi=";
         };
       };
     in
