1ef0020f1486f734abbb6a47b553b65d53c23473
Simon Gardling <titaniumtown@proton.me>
2024-06-28 21:39:39 -0700

    add armTrustedFirmwareJ784S4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26651,6 +26651,7 @@ with pkgs;
     armTrustedFirmwareAllwinner
     armTrustedFirmwareAllwinnerH616
     armTrustedFirmwareAllwinnerH6
+    armTrustedFirmwareJ784S4
     armTrustedFirmwareQemu
     armTrustedFirmwareRK3328
     armTrustedFirmwareRK3399
