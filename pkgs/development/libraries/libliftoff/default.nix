d15c90e4868d26c167f5bcf234cc788e66787a99
Sergei Trofimovich <slyich@gmail.com>
2024-06-29 16:52:47 +0100

    libliftoff_0_4: backport gcc-14 build fix
    Without the change `gcc-14` build fails as:
    
        $ nix build --impure --expr 'with import ./. {}; libliftoff_0_4.override { stdenv = gcc14Stdenv; }'
        ...
        libliftoff> ../layer.c: In function 'liftoff_layer_create':
        libliftoff> ../layer.c:20:48: error: 'calloc' sizes specified with 'sizeof' in the earlier argument and not in the later argument [-Werror=calloc-transposed-args]
        libliftoff>    20 |         layer->candidate_planes = calloc(sizeof(layer->candidate_planes[0]),
        libliftoff>       |                                                ^

--- pkgs/development/libraries/libliftoff/default.nix
+++ pkgs/development/libraries/libliftoff/default.nix
@@ -1,9 +1,9 @@
-{ callPackage, fetchFromGitLab, ... }:
+{ callPackage, fetchFromGitLab, fetchpatch }:
 let
   mkVariant =
-    version: hash:
+    version: hash: patches:
     callPackage ./generic.nix {
-      inherit version;
+      inherit version patches;
       src = fetchFromGitLab {
         domain = "gitlab.freedesktop.org";
         owner = "emersion";
@@ -14,6 +14,14 @@ let
     };
 in
 {
-  libliftoff_0_4 = mkVariant "0.4.1" "sha256-NPwhsd6IOQ0XxNQQNdaaM4kmwoLftokV86WYhoa5csY=";
-  libliftoff_0_5 = mkVariant "0.5.0" "sha256-PcQY8OXPqfn8C30+GAYh0Z916ba5pik8U0fVpZtFb5g=";
+  libliftoff_0_4 = mkVariant "0.4.1" "sha256-NPwhsd6IOQ0XxNQQNdaaM4kmwoLftokV86WYhoa5csY=" [
+    # Pull gcc-14 fix:
+    #   https://gitlab.freedesktop.org/emersion/libliftoff/-/merge_requests/78
+    (fetchpatch {
+      name = "gcc-14-calloc.patch";
+      url = "https://gitlab.freedesktop.org/emersion/libliftoff/-/commit/29a06add8ef184f85e37ff8abdc34fbaa2f4ee1e.patch";
+      hash = "sha256-Y8x1RK3o/I9bs/ZOLeC4t9AIK78l0QnlBWHhiVC+sz8=";
+    })
+  ];
+  libliftoff_0_5 = mkVariant "0.5.0" "sha256-PcQY8OXPqfn8C30+GAYh0Z916ba5pik8U0fVpZtFb5g=" [];
 }
