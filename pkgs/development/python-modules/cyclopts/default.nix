5d6705fd309ec36123f41133d95c12e3471e5b3d
Fabian Affolter <mail@fabian-affolter.ch>
2024-06-29 22:35:33 +0200

    python312Packages.cyclopts: 2.7.1 -> 2.8.0
    Diff: https://github.com/BrianPugh/cyclopts/compare/refs/tags/v2.7.1...v2.8.0
    
    Changelog: https://github.com/BrianPugh/cyclopts/releases/tag/v2.8.0

--- pkgs/development/python-modules/cyclopts/default.nix
+++ pkgs/development/python-modules/cyclopts/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "cyclopts";
-  version = "2.7.1";
+  version = "2.8.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "BrianPugh";
     repo = "cyclopts";
     rev = "refs/tags/v${version}";
-    hash = "sha256-JnSWKQFPrD8wadgz/4Ulisy6Mjc264JHX3PMOX+msac=";
+    hash = "sha256-g16BqbL2WOsMO7PlzTfVR8Ftf32VR8QtvBw1ZNTIOZQ=";
   };
 
   build-system = [
