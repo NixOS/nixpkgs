e0a83b9d5f2f0df486f4fe586b3c104b51c539e6
Pyrox <pyrox@pyrox.dev>
2024-06-28 21:22:06 -0400

    nodePackages.parsoid: drop
    It's been deprecated by upstream and should be removed.

--- pkgs/development/node-packages/main-programs.nix
+++ pkgs/development/node-packages/main-programs.nix
@@ -40,7 +40,6 @@
   localtunnel = "lt";
   lua-fmt = "luafmt";
   neovim = "neovim-node-host";
-  parsoid = "parse.js";
   poor-mans-t-sql-formatter-cli = "sqlformat";
   postcss-cli = "postcss";
   prettier = "prettier";
