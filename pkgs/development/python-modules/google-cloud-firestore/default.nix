be99c5153be65f77a26b52de5914da44a1e95b4c
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-29 02:17:45 +0000

    python311Packages.google-cloud-firestore: 2.16.0 -> 2.16.1

--- pkgs/development/python-modules/google-cloud-firestore/default.nix
+++ pkgs/development/python-modules/google-cloud-firestore/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-firestore";
-  version = "2.16.0";
+  version = "2.16.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-5hrnAimm5TLkOcjRZEejICREfy7kojA/xNBUwljWh38=";
+    hash = "sha256-M4HrgpbtECjZtCGqQrkQDxmMWH+OM8AF0xplVnALda4=";
   };
 
   build-system = [ setuptools ];
