c261102571931f83c93271caefe11c66463f59a8
R. Ryantm <ryantm-bot@ryantm.com>
2024-06-29 09:37:07 +0000

    octavePackages.statistics: 1.6.0 -> 1.6.7

--- pkgs/development/octave-modules/statistics/default.nix
+++ pkgs/development/octave-modules/statistics/default.nix
@@ -6,13 +6,13 @@
 
 buildOctavePackage rec {
   pname = "statistics";
-  version = "1.6.0";
+  version = "1.6.7";
 
   src = fetchFromGitHub {
     owner = "gnu-octave";
     repo = "statistics";
     rev = "refs/tags/release-${version}";
-    sha256 = "sha256-XJXDiVDg3Nw7a/ih49jtkYRmyvAhTfs3LbBQmw+87oc=";
+    sha256 = "sha256-cXAjUiv4xWPrWf7HQg9Y+JkR7ON9iefKFUGbEr9FGNA=";
   };
 
   requiredOctavePackages = [
