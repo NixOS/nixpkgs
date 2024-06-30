42a398fd3454731610e12e53e3607631474fe96b
Thomas Gerbet <thomas@gerbet.me>
2024-06-30 18:48:49 +0200

    procps: remove typeteris from the maintainers
    https://github.com/NixOS/nixpkgs/pull/298505#issuecomment-2198112591

--- pkgs/os-specific/linux/procps-ng/default.nix
+++ pkgs/os-specific/linux/procps-ng/default.nix
@@ -56,6 +56,6 @@ stdenv.mkDerivation rec {
     priority = 11; # less than coreutils, which also provides "kill" and "uptime"
     license = licenses.gpl2;
     platforms = platforms.unix;
-    maintainers = [ maintainers.typetetris ];
+    maintainers = [ ];
   };
 }
