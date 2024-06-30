e6f8a3babaadc3e1560e5143610cf1098fee4a77
chayleaf <chayleaf-git@pavluk.org>
2024-06-30 13:07:05 +0700

    openjfx: convert to gradle setup hook

--- pkgs/development/compilers/openjdk/openjfx/20/default.nix
+++ pkgs/development/compilers/openjdk/openjfx/20/default.nix
@@ -1,107 +1,67 @@
-{ stdenv, lib, fetchFromGitHub, writeText, openjdk17_headless
+{ stdenv, lib, pkgs, fetchFromGitHub, writeText
 , openjdk20_headless, gradle_7, pkg-config, perl, cmake, gperf, gtk2, gtk3, libXtst
-, libXxf86vm, glib, alsa-lib, ffmpeg_4, python3, ruby, fetchurl, runCommand
+, libXxf86vm, glib, alsa-lib, ffmpeg_4, python3, ruby
 , withMedia ? true
 , withWebKit ? false
 }:
 
 let
+  pname = "openjfx-modular-sdk";
   major = "20";
   update = ".0.2";
   build = "-ga";
   repover = "${major}${update}${build}";
-  gradle_ = (gradle_7.override {
-    # note: gradle does not yet support running on 19
-    java = openjdk17_headless;
-  });
+  jdk = openjdk20_headless;
+  gradle = gradle_7;
 
-  icuVersionWithSep = s: "71${s}1";
-  icuPath = "download/release-${icuVersionWithSep "-"}/icu4c-${icuVersionWithSep "_"}-data-bin-l.zip";
-  icuData = fetchurl {
-    url = "https://github.com/unicode-org/icu/releases/${icuPath}";
-    hash = "sha256-pVWIy0BkICsthA5mxhR9SJQHleMNnaEcGl/AaLi5qZM=";
-  };
-  icuFakeRepository = runCommand "icu-data-repository" {} ''
-    install -Dm644 ${icuData} $out/${icuPath}
-  '';
+in stdenv.mkDerivation {
+  inherit pname;
+  version = "${major}${update}${build}";
 
-  makePackage = args: stdenv.mkDerivation ({
-    version = "${major}${update}${build}";
-
-    src = fetchFromGitHub {
-      owner = "openjdk";
-      repo = "jfx20u";
-      rev = repover;
-      hash = "sha256-3Hhz4i8fPU2yowb4roylCXzuO9HkW7ZWF9TMA3HIH9o=";
-    };
-
-    buildInputs = [ gtk2 gtk3 libXtst libXxf86vm glib alsa-lib ffmpeg_4 ];
-    nativeBuildInputs = [ gradle_ perl pkg-config cmake gperf python3 ruby ];
-
-    dontUseCmakeConfigure = true;
-
-    postPatch = ''
-      # Add missing includes for gcc-13 for webkit build:
-      sed -e '1i #include <cstdio>' \
-        -i modules/javafx.web/src/main/native/Source/bmalloc/bmalloc/Heap.cpp \
-           modules/javafx.web/src/main/native/Source/bmalloc/bmalloc/IsoSharedPageInlines.h
-    '';
-
-    config = writeText "gradle.properties" (''
-      CONF = Release
-      JDK_HOME = ${openjdk20_headless.home}
-    '' + args.gradleProperties or "");
-
-    buildPhase = ''
-      runHook preBuild
-
-      export NUMBER_OF_PROCESSORS=$NIX_BUILD_CORES
-      export GRADLE_USER_HOME=$(mktemp -d)
-      ln -s $config gradle.properties
-      export NIX_CFLAGS_COMPILE="$(pkg-config --cflags glib-2.0) $NIX_CFLAGS_COMPILE"
-      gradle --no-daemon --console=plain $gradleFlags sdk
-
-      runHook postBuild
-    '';
-  } // args);
-
-  # Fake build to pre-download deps into fixed-output derivation.
-  # We run nearly full build because I see no other way to download everything that's needed.
-  # Anyone who knows a better way?
-  deps = makePackage {
-    pname = "openjfx-deps";
-
-    # perl code mavenizes pathes (com.squareup.okio/okio/1.13.0/a9283170b7305c8d92d25aff02a6ab7e45d06cbe/okio-1.13.0.jar -> com/squareup/okio/okio/1.13.0/okio-1.13.0.jar)
-    installPhase = ''
-      find $GRADLE_USER_HOME -type f -regex '.*/modules.*\.\(jar\|pom\)' \
-        | perl -pe 's#(.*/([^/]+)/([^/]+)/([^/]+)/[0-9a-f]{30,40}/([^/\s]+))$# ($x = $2) =~ tr|\.|/|; "install -Dm444 $1 \$out/$x/$3/$4/$5" #e' \
-        | sh
-      rm -rf $out/tmp
-    '';
-
-    outputHashAlgo = "sha256";
-    outputHashMode = "recursive";
-    # suspiciously the same as for openjfx 17 ...
-    # could they really not have changed any of their dependencies?
-    # or did we miss changing another upstream hash when copy-pasting?
-    outputHash = "sha256-dV7/U5GpFxhI13smZ587C6cVE4FRNPY0zexZkYK4Yqo=";
+  src = fetchFromGitHub {
+    owner = "openjdk";
+    repo = "jfx20u";
+    rev = repover;
+    hash = "sha256-3Hhz4i8fPU2yowb4roylCXzuO9HkW7ZWF9TMA3HIH9o=";
   };
 
-in makePackage {
-  pname = "openjfx-modular-sdk";
+  buildInputs = [ gtk2 gtk3 libXtst libXxf86vm glib alsa-lib ffmpeg_4 ];
+  nativeBuildInputs = [ gradle perl pkg-config cmake gperf python3 ruby ];
 
-  gradleProperties = ''
+  dontUseCmakeConfigure = true;
+
+  config = writeText "gradle.properties" ''
+    CONF = Release
+    JDK_HOME = ${jdk.home}
     COMPILE_MEDIA = ${lib.boolToString withMedia}
     COMPILE_WEBKIT = ${lib.boolToString withWebKit}
-    ${lib.optionalString withWebKit "icuRepositoryURL = file://${icuFakeRepository}"}
   '';
 
-  preBuild = ''
-    swtJar="$(find ${deps} -name org.eclipse.swt\*.jar)"
-    substituteInPlace build.gradle \
-      --replace 'mavenCentral()' 'mavenLocal(); maven { url uri("${deps}") }' \
-      --replace 'name: SWT_FILE_NAME' "files('$swtJar')"
+  postPatch = ''
+    # Add missing includes for gcc-13 for webkit build:
+    sed -e '1i #include <cstdio>' \
+      -i modules/javafx.web/src/main/native/Source/bmalloc/bmalloc/Heap.cpp \
+         modules/javafx.web/src/main/native/Source/bmalloc/bmalloc/IsoSharedPageInlines.h
+
+    ln -s $config gradle.properties
   '';
+
+  mitmCache = gradle.fetchDeps {
+    attrPath = "openjfx${major}";
+    pkg = pkgs."openjfx${major}".override { withWebKit = true; };
+    data = ./deps.json;
+  };
+
+  __darwinAllowLocalNetworking = true;
+
+  preBuild = ''
+    export NUMBER_OF_PROCESSORS=$NIX_BUILD_CORES
+    export NIX_CFLAGS_COMPILE="$(pkg-config --cflags glib-2.0) $NIX_CFLAGS_COMPILE"
+  '';
+
+  enableParallelBuilding = false;
+
+  gradleBuildTask = "sdk";
 
   installPhase = ''
     cp -r build/modular-sdk $out
@@ -111,16 +71,14 @@ in makePackage {
 
   postFixup = ''
     # Remove references to bootstrap.
-    export openjdkOutPath='${openjdk20_headless.outPath}'
+    export openjdkOutPath='${jdk.outPath}'
     find "$out" -name \*.so | while read lib; do
       new_refs="$(patchelf --print-rpath "$lib" | perl -pe 's,:?\Q$ENV{openjdkOutPath}\E[^:]*,,')"
       patchelf --set-rpath "$new_refs" "$lib"
     done
   '';
 
-  disallowedReferences = [ openjdk17_headless openjdk20_headless ];
-
-  passthru.deps = deps;
+  disallowedReferences = [ jdk gradle.jdk ];
 
   meta = with lib; {
     homepage = "https://openjdk.org/projects/openjfx/";
