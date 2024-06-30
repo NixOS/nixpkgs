40af21d0b33da7161c5276e27a22424465780ad9
Zhong Jianxin <azuwis@gmail.com>
2024-06-30 19:36:58 +0800

    moonlight-qt: 6.0.0 -> 6.0.1

--- pkgs/by-name/mo/moonlight-qt/package.nix
+++ pkgs/by-name/mo/moonlight-qt/package.nix
@@ -2,7 +2,6 @@
   stdenv,
   lib,
   fetchFromGitHub,
-  fetchpatch,
   darwin,
   overrideSDK,
   libsForQt5,
@@ -35,29 +34,15 @@ in
 
 stdenv'.mkDerivation rec {
   pname = "moonlight-qt";
-  version = "6.0.0";
+  version = "6.0.1";
 
   src = fetchFromGitHub {
     owner = "moonlight-stream";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-tXlcHQhXnGdZqUlOHPN1f4vaGlF/kGkTybVW/+FMGX8=";
+    sha256 = "sha256-zrl8WPXvQ/7FTqFnpwoXEJ85prtgJWoWNsdckw5+JHI=";
     fetchSubmodules = true;
   };
-
-  patches = [
-    # Add 'CONFIG+=disable-prebuilts' qmake option
-    # When specified, qmake uses pkg-config for libraries on macOS instead of the prebuilts in the libs submodule.
-    (fetchpatch {
-      url = "https://github.com/moonlight-stream/moonlight-qt/commit/83811e2a077b78409cf79ed77b8437041159ad88.patch";
-      hash = "sha256-JlYtUiY0jJMNjF4KWpo58MOGufbl+od+UC+uxmu3ZF4=";
-    })
-    # Don't bundle libs into the final app package if disable-prebuilts is set
-    (fetchpatch {
-      url = "https://github.com/moonlight-stream/moonlight-qt/commit/640ac3f9fe90197c826c9694942b9cd7be53f757.patch";
-      hash = "sha256-l7bpkR9f3+Xwx5e7p9IGddjHrV77BrCHkTQ9cwDyYeY=";
-    })
-  ];
 
   nativeBuildInputs = [
     libsForQt5.qmake
