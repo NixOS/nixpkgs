5e30f75d13f713ff608599db66c406a7dbd03488
NoRePercussions <tucker@tuckershea.com>
2024-06-30 00:06:30 -0400

    salt-lint: init at 0.9.2

---
+++ pkgs/by-name/sa/salt-lint/package.nix
@@ -0,0 +1,31 @@
+{ lib, testers, python3Packages, fetchPypi, salt-lint }:
+
+python3Packages.buildPythonApplication rec {
+  pname = "salt-lint";
+  version = "0.9.2";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-f3Tmguf9eHIqbTkeqO3J/HlRE+z9QGV9aAV9QE7nvo4=";
+  };
+
+  dependencies = with python3Packages; [
+    pipBuildHook
+    pathspec
+    pyyaml
+  ];
+
+  pythonImportsCheck = [ "saltlint" ];
+
+  passthru.tests = {
+    version = testers.testVersion { package = salt-lint; };
+  };
+
+  meta = with lib; {
+    description = "Command-line utility that checks for best practices in SaltStack";
+    homepage = "https://salt-lint.readthedocs.io";
+    license = licenses.mit;
+    maintainers = with maintainers; [ norepercussions ];
+    mainProgram = "salt-lint";
+  };
+}
