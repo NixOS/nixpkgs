46210ae98d5b077c3a7036b6f434b64bcdebdfea
plebhash <plebhash@proton.me>
2024-06-30 22:22:03 -0300

    add lianad + liana-cli to liana package

--- pkgs/by-name/li/liana/package.nix
+++ pkgs/by-name/li/liana/package.nix
@@ -14,6 +14,7 @@
 , systemd
 , vulkan-loader
 , xorg
+, withGui ? true
 }:
 
 let
@@ -31,7 +32,7 @@ let
   ];
 in
 rustPlatform.buildRustPackage rec {
-  pname = "liana";
+  pname = if withGui then "liana" else "lianad";
   version = "5.0";
 
   src = fetchFromGitHub {
@@ -41,13 +42,14 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-RkZ2HSN7IjwN3tD0UhpMeQeqkb+Y79kSWnjJZ5KPbQk=";
   };
 
-  cargoLock = {
+  cargoLock = if withGui then {
     lockFile = ./Cargo.lock;
     outputHashes = {
       "liana-5.0.0" = "sha256-wePqsVpMBRP2eJZd8W05CaeesqY5g/rnr4OonmRzeeM=";
       "iced_futures-0.6.0" = "sha256-ejkAxU6DwiX1/119eA0GRapSmz7dqwx9M0uMwyDHATQ=";
     };
-  };
+  } else null;
+  cargoSha256 = if withGui then null else "sha256-6r8G4sO/aDO5VyJbmbdFz3W2b8ZmulXJZtnz8IAwcaU=";
 
   nativeBuildInputs = [
     pkg-config
@@ -61,14 +63,16 @@ rustPlatform.buildRustPackage rec {
     systemd
   ];
 
-  sourceRoot = "${src.name}/gui";
+  sourceRoot = if withGui then "${src.name}/gui" else null;
 
-  postInstall = ''
+  postInstall = if withGui then ''
     install -Dm0644 ./ui/static/logos/liana-app-icon.svg $out/share/icons/hicolor/scalable/apps/liana.svg
     wrapProgram $out/bin/liana-gui --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeLibs}"
+  '' else ''
+    install -Dm0644 ./contrib/lianad_config_example.toml $out/etc/liana/config.toml
   '';
 
-  desktopItems = [
+  desktopItems = if withGui then [
     (makeDesktopItem {
       name = "Liana";
       exec = "liana-gui";
@@ -76,12 +80,12 @@ rustPlatform.buildRustPackage rec {
       desktopName = "Liana";
       comment = meta.description;
     })
-  ];
+  ] else null;
 
-  doCheck = true;
+  doCheck = if withGui then true else false;
 
   meta = with lib; {
-    mainProgram = "liana-gui";
+    mainProgram = if withGui then "liana-gui" else "lianad";
     description = "A Bitcoin wallet leveraging on-chain timelocks for safety and recovery";
     homepage = "https://wizardsardine.com/liana";
     license = licenses.bsd3;
