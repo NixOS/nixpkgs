06dc828cb6a412a52d2c0110b67db84da6b6faf4
Drewry Pope <drewrypope@gmail.com>
2024-07-01 04:11:25 -0500

    feat: use gitHub

--- pkgs/by-name/wr/wrangler_3/update.sh
+++ pkgs/by-name/wr/wrangler_3/update.sh
@@ -5,9 +5,8 @@ set -euo pipefail
 pushd "$(dirname "${BASH_SOURCE[0]}")"
 
 version=$(npm view wrangler version)
-tarball="wrangler-$version.tgz"
-url="https://registry.npmjs.org/wrangler/-/$tarball"
-
+tarball="wrangler@$version.tar.gz"
+url="https://github.com/cloudflare/workers-sdk/archive/refs/tags/$tarball"
 if [[ "$UPDATE_NIX_OLD_VERSION" == "$version" ]]; then
     echo "Already up to date!"
     exit 0
@@ -15,16 +14,27 @@ fi
 
 sed -i 's#version = "[^"]*"#version = "'"$version"'"#' package.nix
 
-sha256=$(nix-prefetch-url "$url")
+store_name="wrangler_$version.tar.gz"
+sha256=$(nix-prefetch-url --name "$store_name" "$url")
 src_hash=$(nix-hash --to-sri --type sha256 "$sha256")
 sed -i 's#hash = "[^"]*"#hash = "'"$src_hash"'"#' package.nix
 
-rm -f package-lock.json package.json *.tgz
+rm -f *.tar.gz
+rm -rf "${tarball%.tar.gz}"
+
 wget "$url"
-tar xf "$tarball" --strip-components=1 package/package.json
-npm i --save-dev --legacy-peer-deps --package-lock-only --ignore-scripts
-npm_hash=$(prefetch-npm-deps package-lock.json)
+
+mkdir -p "${tarball%.tar.gz}"
+tar xf "$tarball" -C "${tarball%.tar.gz}"
+
+tar xf "$tarball" -C "${tarball%.tar.gz}"
+
+target_dir="${tarball%.tar.gz}/workers-sdk-wrangler-${version}"
+npm i --save-dev --legacy-peer-deps --package-lock-only --ignore-scripts --prefix "$target_dir"
+npm_hash=$(prefetch-npm-deps "${target_dir}/package-lock.json")
+rm -f *.tar.gz
+rm -rf "${tarball%.tar.gz}"
+
 sed -i 's#npmDepsHash = "[^"]*"#npmDepsHash = "'"$npm_hash"'"#' package.nix
-rm -f package.json *.tgz
 
 popd
