559c0872852f95ab3e69204099fb2aecd84cf99a
Philip Taron <philip.taron@gmail.com>
2024-07-01 11:14:42 -0700

    pidginPackages: move __attrsFailEvaluation to allow deeper evaluation
    The test (`nix-build pkgs/test/release/default.nix`) test continues to pass.
    
    No new derivations are reported from `nix-instantiate --eval --strict --json pkgs/top-level/release-attrpaths-superset.nix -A names` since there are aliases for all the derivations in pidginPackages.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33497,10 +33497,7 @@ with pkgs;
 
   picosnitch = callPackage ../tools/networking/picosnitch { };
 
-  pidginPackages =
-    let pidgin-plugins =
-          recurseIntoAttrs (callPackage ../applications/networking/instant-messengers/pidgin/pidgin-plugins { });
-    in pidgin-plugins // { pidginPackages = pidgin-plugins.pidginPackages // { __attrsFailEvaluation = true; }; };
+  pidginPackages = callPackage ../applications/networking/instant-messengers/pidgin/pidgin-plugins { };
 
   inherit (pidginPackages) pidgin;
 
