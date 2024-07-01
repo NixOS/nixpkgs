9af00c401223e7b7b8307fc51ea1fcd35befbb9b
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-01 19:18:41 -0300

    toml11: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24493,8 +24493,6 @@ with pkgs;
 
   tomlcpp = callPackage ../development/libraries/tomlcpp { };
 
-  toml11 = callPackage ../development/libraries/toml11 { };
-
   tomlplusplus = callPackage ../development/libraries/tomlplusplus { };
 
   tokyocabinet = callPackage ../development/libraries/tokyo-cabinet { };
