fe4c2f44022fb02acdbce5ca2a1e0ca4c94aea44
hexchen <hexchen@lilwit.ch>
2024-07-02 15:41:47 +0000

    maintainers: remove hexchen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8032,12 +8032,6 @@
     githubId = 222664;
     name = "Matthew Leach";
   };
-  hexchen = {
-    email = "nix@lilwit.ch";
-    github = "hexchen";
-    githubId = 41522204;
-    name = "hexchen";
-  };
   hexclover = {
     email = "hexclover@outlook.com";
     github = "hexclover";
