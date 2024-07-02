59b88794222ea3b1792d7cafe5c9c7486e055aaa
AMS21 <AMS21.github@gmail.com>
2024-07-02 18:48:29 +0200

    cmake: 3.29.6 -> 3.30.0

--- pkgs/by-name/cm/cmake/package.nix
+++ pkgs/by-name/cm/cmake/package.nix
@@ -48,11 +48,11 @@ stdenv.mkDerivation (finalAttrs: {
     + lib.optionalString isMinimalBuild "-minimal"
     + lib.optionalString cursesUI "-cursesUI"
     + lib.optionalString qt5UI "-qt5UI";
-  version = "3.29.6";
+  version = "3.30.0";
 
   src = fetchurl {
     url = "https://cmake.org/files/v${lib.versions.majorMinor finalAttrs.version}/cmake-${finalAttrs.version}.tar.gz";
-    hash = "sha256-E5ExMAO4PUjiqxFai1JaVX942MFURhi0jR2QGEoQ8K8=";
+    hash = "sha256-FX5b5gVcFUw09YB5X+WDLyYCRlBtMpVKlxMA7XiZ9Xk=";
   };
 
   patches = [
