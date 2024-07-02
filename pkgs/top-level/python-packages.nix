63707fe688af4da55a26b4a12e383dc1d3e8e414
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-02 15:46:48 +0200

    hpp-centroidal-dynamics: init at 6.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6565,6 +6565,13 @@ self: super: with self; {
 
   hpccm = callPackage ../development/python-modules/hpccm { };
 
+  hpp-centroidal-dynamics = toPythonModule (
+    pkgs.hpp-centroidal-dynamics.override {
+      pythonSupport = true;
+      python3Packages = self;
+    }
+  );
+
   hs-dbus-signature = callPackage ../development/python-modules/hs-dbus-signature { };
 
   hsaudiotag3k = callPackage ../development/python-modules/hsaudiotag3k { };
