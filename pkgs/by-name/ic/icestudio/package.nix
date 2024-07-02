aae14d3c4562ec20716f39849e7d819231a9e90b
Enric Morales <me@enric.me>
2024-07-02 16:39:33 +0200

    icestudio: init at 0.12
    Co-authored-by: Abdullah Imad <me@imad.nyc>
    Co-authored-by: Alberto Merino <amerinor01@gmail.com>
    Co-authored-by: Enric Morales <me@enric.me>
    Co-authored-by: Jack Leightcap <jack@leightcap.com>
    Co-authored-by: Roland Coeurjoly <rolandcoeurjoly@gmail.com>

---
+++ pkgs/by-name/ic/icestudio/package.nix
@@ -0,0 +1,29 @@
+{ lib, appimageTools, fetchurl }:
+let
+  pname = "icestudio";
+  version = "0.12";
+
+  src = fetchurl {
+    url = "https://github.com/FPGAwars/icestudio/releases/download/v${version}/${pname}-${version}-linux64.AppImage";
+    hash = "sha256-s+hvD+S2f30u8GrYadFOIjexmyicw6bOlFOvyYyjHeE=";
+  };
+  extraPkgs = pkgs: [ pkgs.python3 ];
+
+  meta = with lib; {
+    description = "Visual editor for open FPGA boards";
+    homepage = "https://github.com/FPGAwars/icestudio";
+    license = licenses.gpl2;
+    maintainers = with maintainers; [ imadnyc jleightcap ];
+    mainProgram = "icestudio";
+    platforms = ["x86_64-linux"];
+  };
+in
+appimageTools.wrapType2 {
+  inherit
+    pname
+    version
+    src
+    extraPkgs
+    meta
+    ;
+}
