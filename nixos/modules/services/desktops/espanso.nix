e7ace4f4010a828326adad31d9efe2f17f4cd37b
David Flanagan <david@rawkode.dev>
2024-07-02 10:57:10 +0100

    chore: use mkEnableOption

--- nixos/modules/services/desktops/espanso.nix
+++ nixos/modules/services/desktops/espanso.nix
@@ -8,11 +8,7 @@ in {
   options = {
     services.espanso = {
       enable = mkEnableOption "Espanso";
-      wayland = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Use the Wayland compatible espanso package";
-      };
+      wayland = mkEnableOption "Use the Wayland compatible espanso package";
       package = mkPackageOption pkgs "espanso" {
         example = "pkgs.espanso-wayland";
       };
