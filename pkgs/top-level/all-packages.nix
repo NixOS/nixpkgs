5977c53117556ab0ed6aa0040ffcc42b3e1513f7
Martin Joerg <martin.joerg@gmail.com>
2024-07-02 10:45:52 +0000

    skypeforlinux: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34012,8 +34012,6 @@ with pkgs;
     libjpeg_turbo = libjpeg8;
   };
 
-  skypeforlinux = callPackage ../applications/networking/instant-messengers/skypeforlinux { };
-
   slingshot = callPackage ../tools/misc/slingshot { };
 
   slop = callPackage ../tools/misc/slop { };
