407a7cd0d019f056198174901cc7b4e1f70aff15
Enric Morales <me@enric.me>
2024-07-02 21:29:13 +0200

    lib25519: replace compiler command to fix cross- and native compiling

--- pkgs/by-name/li/lib25519/package.nix
+++ pkgs/by-name/li/lib25519/package.nix
@@ -5,7 +5,7 @@
   fetchzip,
   librandombytes,
   libcpucycles,
-  system ? builtins.currentSystem 
+  system ? builtins.currentSystem,
 }:
 stdenv.mkDerivation (prev: {
   pname = "lib25519";
@@ -20,29 +20,21 @@ stdenv.mkDerivation (prev: {
     patchShebangs configure
     patchShebangs scripts-build
   '';
-  #doInstallCheck = true;
+  doInstallCheck = true;
   installCheckPhase = ''
     $out/bin/lib25519-test
   '';
 
   # NOTE: lib25519 uses a custom Python `./configure`: it does not expect standard
   # autoconfig --build --host etc. arguments: disable
-  configurePlatforms = [ ];
+  # Pass the hostPlatform string
+  configurePhase = ''
+    runHook preConfigure
+    ./configure --host=${system} --prefix=$out
+    runHook postConfigure
+  '';
 
-   # configurePhase = ''
-   #  runHook preConfigure
-   #  pwd; ./configure --host=${system} --prefix=$out 
-   #  # pwd; ./configure --host=arm64 --prefix=$out 
-   #  # echo "ISTHISBEINGCALLED"; ./configure --prefix=$out 
-   #  runHook postConfigure
-   # '';
-
-  # NOTE: the librandombytes library has required specific CFLAGS defined:
-  # https://randombytes.cr.yp.to/librandombytes-20240318/compilers/default.html
-  # - `-Qunused-arguments` suppress clang warning
-  env.NIX_CFLAGS_COMPILE = toString (lib.optionals stdenv.cc.isClang [ "-Qunused-arguments" ]);
-
-  # patches = [ ./environment-variable-tools.patch ];
+  patches = [ ./environment-variable-tools.patch ];
 
   nativeBuildInputs = [ python3 ];
   buildInputs = [
@@ -67,21 +59,7 @@ stdenv.mkDerivation (prev: {
       imadnyc
       jleightcap
     ];
-    platforms = [
-      "i686-linux"
-      "x86_64-linux"
-      "armv7a-linux"
-      "aarch64-linux"
-      # Cannot support 32 bit MIPS because options in libcpucycles only supports mips64: https://cpucycles.cr.yp.to/libcpucycles-20240318/cpucycles/options.html
-      "mips64-linux"
-      "mips64el-linux"
-      # powerpc-linux (32 bits) is supported by upstream project but not by nix
-      "powerpc64-linux"
-      "powerpc64le-linux"
-      "riscv32-linux"
-      "riscv64-linux"
-      "s390x-linux"
-      # Upstream package supports sparc, but nix does not
-    ] ++ lib.platforms.darwin; # Work on MacOS X mentioned: https://randombytes.cr.yp.to/download.html
+    # This supports whatever platforms libcpucycles supports
+    inherit (libcpucycles.meta) platforms;
   };
 })
