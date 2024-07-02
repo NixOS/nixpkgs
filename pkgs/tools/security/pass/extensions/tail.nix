07fb2b249902d7dddd4d8cf8f4c693c7489d1c9b
Pablo Fraile Alonso <pablo@pablofraile.net>
2024-07-02 15:59:30 +0200

    refactor: improve comments from #322735

--- pkgs/tools/security/pass/extensions/tail.nix
+++ pkgs/tools/security/pass/extensions/tail.nix
@@ -1,30 +1,26 @@
 { lib, stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "pass-tail";
   version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "palortoff";
     repo = "pass-extension-tail";
-    rev = "v${version}";
-    sha256 = "RyPXW4Lgx6GdF9j+zEJqKRGxinDwPCmF9WXJ5yW4Pcc=";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-RyPXW4Lgx6GdF9j+zEJqKRGxinDwPCmF9WXJ5yW4Pcc=";
   };
-
-  buildInputs = [ ];
-
-  dontBuild = true;
 
   installFlags = [
     "PREFIX=$(out)"
     "BASHCOMPDIR=$(out)/share/bash-completion/completions"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "A pass extension to avoid printing the password to the console";
     homepage = "https://github.com/palortoff/pass-extension-tail";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ pablito2020 ];
-    platforms = platforms.unix;
+    license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [ pablito2020 ];
+    platforms = lib.platforms.unix;
   };
-}
+})
