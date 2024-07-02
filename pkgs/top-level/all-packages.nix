6d70f0c32830742ace451f248133c85eb28d4686
Martin Messer <martin.messer@cyberus-technology.de>
2024-07-02 15:45:13 +0200

    pkgs.elasticsearch8: add elasticsearch 8.14.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7654,11 +7654,17 @@ with pkgs;
   # The latest version used by elasticsearch, logstash, kibana and the the beats from elastic.
   # When updating make sure to update all plugins or they will break!
   elk7Version = "7.17.16";
+  elk8Version = "8.14.1";
 
   elasticsearch7 = callPackage ../servers/search/elasticsearch/7.x.nix {
     util-linux = util-linuxMinimal;
   };
+
   elasticsearch = elasticsearch7;
+
+  elasticsearch8Plugins = recurseIntoAttrs (
+    callPackage ../servers/search/elasticsearch/plugins.nix { elasticsearch = elasticsearch8; }
+  );
 
   elasticsearch7Plugins = recurseIntoAttrs (
     callPackage ../servers/search/elasticsearch/plugins.nix { elasticsearch = elasticsearch7; }
