631add00abcf312bbbf3892ecbd8eab38dec9aa4
aleksana <me@aleksana.moe>
2024-07-02 18:30:36 +0800

    python3Packages.mariadb: 1.1.4 -> 1.1.10

--- pkgs/development/python-modules/mariadb/default.nix
+++ pkgs/development/python-modules/mariadb/default.nix
@@ -3,23 +3,23 @@
   fetchPypi,
   libmysqlclient,
   lib,
-  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "mariadb";
-  version = "1.1.4";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.6";
+  version = "1.1.10";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-c6CsvSrOOB7BvPxhztenmlGeZsAsJOEq5tJ7qgNxeHY=";
-    extension = "zip";
+    hash = "sha256-ozKJPj73zreXCrS9fIRLy0vWigUcpRMTVm+YCNdBHy0=";
   };
 
+  # Should be in buildInputs, but was found by its executables
   nativeBuildInputs = [ libmysqlclient ];
+
+  dependencies = [ setuptools ];
 
   # Requires a running MariaDB instance
   doCheck = false;
@@ -29,7 +29,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "MariaDB Connector/Python";
     homepage = "https://github.com/mariadb-corporation/mariadb-connector-python";
-    license = licenses.lgpl21Only;
-    maintainers = with maintainers; [ vanilla ];
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [ ];
   };
 }
