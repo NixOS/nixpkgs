5f7524844ee7790250a22254f6e038686dc6c168
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-07-04 01:17:35 +0200

    nss_latest: 3.101.1 -> 3.102
    https://groups.google.com/a/mozilla.org/g/dev-tech-crypto/c/M1CvveRmbGU

--- pkgs/development/libraries/nss/latest.nix
+++ pkgs/development/libraries/nss/latest.nix
@@ -5,6 +5,6 @@
 #       Example: nix-shell ./maintainers/scripts/update.nix --argstr package cacert
 
 import ./generic.nix {
-  version = "3.101.1";
-  hash = "sha256-KcRiOUbdFnH618MFM6uxmRn+/Jn4QMHtv1BELXrCAX4=";
+  version = "3.102";
+  hash = "sha256-Dl04bPMH7/IFyKktD6Ql9XZZRCnIoq7P+30Qo8j0eBQ=";
 }
