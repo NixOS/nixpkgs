606d9ef3eacf4bf7c2829ac49829f2210d17fbed
savedram <michael.savedra@ovo.com>
2024-07-03 18:32:55 +0100

    clipse: v1.0.0 -> v1.0.2

--- pkgs/by-name/cl/clipse/package.nix
+++ pkgs/by-name/cl/clipse/package.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "clipse";
-  version = "1.0.0";
+  version = "1.0.2";
 
   src = fetchFromGitHub {
     owner = "savedra1";
     repo = "clipse";
     rev = "v${version}";
-    hash = "sha256-9r/Ih73eYb45LYOu8HMXqdme/rUwLBI6+gctF603C2w=";
+    hash = "sha256-RMdR9d9HdczWstZjDQqJzuWeKv2fAZHfRoy7fJjsJUg=";
   };
 
   vendorHash = "sha256-QEBRlwNS8K44chB3fMOJZxYnIaWMnuDySIhKfF7XtxM=";
