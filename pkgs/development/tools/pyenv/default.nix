a09152bf2fe4b395d9d26f96fda345cf2f015b71
Muhammad Falak R Wani <falakreyaz@gmail.com>
2024-07-03 15:04:34 +0530

    pyenv: 2.4.4 -> 2.4.5
    Diff: https://github.com/pyenv/pyenv/compare/refs/tags/v2.4.4...v2.4.5
    Changelog: https://github.com/pyenv/pyenv/blob/refs/tags/v2.4.5/CHANGELOG.md
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- pkgs/development/tools/pyenv/default.nix
+++ pkgs/development/tools/pyenv/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "pyenv";
-  version = "2.4.4";
+  version = "2.4.5";
 
   src = fetchFromGitHub {
     owner = "pyenv";
     repo = "pyenv";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ujbJrF7QDd2DIsZGEoJrM3VA2WNpkJNe4BZVcPokXRg=";
+    hash = "sha256-0MMPbn6BaIn623nHYuNYsiMIFF0y2fAYmjxZ/XESyTg=";
   };
 
   nativeBuildInputs = [
