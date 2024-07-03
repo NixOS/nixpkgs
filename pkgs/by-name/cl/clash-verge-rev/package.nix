b225d56c4ba88165b9467e6c3e0fb2f649b51f34
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-03 05:16:52 +0000

    clash-verge-rev: 1.6.6 -> 1.7.1

--- pkgs/by-name/cl/clash-verge-rev/package.nix
+++ pkgs/by-name/cl/clash-verge-rev/package.nix
@@ -5,11 +5,11 @@
 
 clash-verge.overrideAttrs (old: rec {
   pname = "clash-verge-rev";
-  version = "1.6.6";
+  version = "1.7.1";
 
   src = fetchurl {
     url = "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v${version}/clash-verge_${version}_amd64.deb";
-    hash = "sha256-x+Xkasg6Yzft5CLg2YFCRkgpDeiVvvdmcLjrg+oIOT8=";
+    hash = "sha256-b7H3olZ8PtPObOjmuAtiBHm1U/iMhUCsR5nFaVmHIU4=";
   };
 
   meta = old.meta // (with lib; {
