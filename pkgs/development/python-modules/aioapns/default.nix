cd177843a9fab7debd35482f8513afffa41cf951
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-03 07:48:42 +0000

    python311Packages.aioapns: 3.2 -> 3.3

--- pkgs/development/python-modules/aioapns/default.nix
+++ pkgs/development/python-modules/aioapns/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "aioapns";
-  version = "3.2";
+  version = "3.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-QPayQogW97saMmaPUP5x0CiXd6Qptg/OROigi5ASNQg=";
+    hash = "sha256-lLlfrv9yHBHKqmSrs4y9NKMgGSGQQe+zVFWMht+MvGk=";
   };
 
   nativeBuildInputs = [ setuptools ];
