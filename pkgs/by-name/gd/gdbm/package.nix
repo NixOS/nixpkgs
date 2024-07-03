3fff5aaaaef02c6a62134fa57a23fd73740b7f91
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-03 19:52:01 -0300

    gdbm: refactor
    - finalAttrs
    - get rid of ${pname}
    - strictDeps
    - testVersion
    - get rid of nested with

--- pkgs/by-name/gd/gdbm/package.nix
+++ pkgs/by-name/gd/gdbm/package.nix
@@ -1,69 +1,87 @@
-{ stdenv, lib, fetchurl, updateAutotoolsGnuConfigScriptsHook }:
+{
+  lib,
+  fetchurl,
+  stdenv,
+  testers,
+  updateAutotoolsGnuConfigScriptsHook,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gdbm";
   version = "1.23";
 
   src = fetchurl {
-    url = "mirror://gnu/gdbm/${pname}-${version}.tar.gz";
-    sha256 = "sha256-dLEIHSH/8TrkvXwW5dblBKTCb3zeHcoNljpIQXS7ys0=";
+    url = "mirror://gnu/gdbm/gdbm-${finalAttrs.version}.tar.gz";
+    hash = "sha256-dLEIHSH/8TrkvXwW5dblBKTCb3zeHcoNljpIQXS7ys0=";
   };
 
-  nativeBuildInputs = [ updateAutotoolsGnuConfigScriptsHook ];
+  nativeBuildInputs = [
+    updateAutotoolsGnuConfigScriptsHook
+  ];
 
-  doCheck = true; # not cross;
+  configureFlags = [
+    (lib.enableFeature true "libgdbm-compat")
+  ];
 
-  # Linking static stubs on cygwin requires correct ordering.
-  # Consider upstreaming this.
+  outputs = [ "out" "dev" "info" "lib" "man" ];
 
-  # Disable dbmfetch03.at test because it depends on unlink()
-  # failing on a link in a chmod -w directory, which cygwin
-  # apparently allows.
-  postPatch = lib.optionalString stdenv.buildPlatform.isCygwin ''
-      substituteInPlace tests/Makefile.in --replace \
-        '_LDADD = ../src/libgdbm.la ../compat/libgdbm_compat.la' \
-        '_LDADD = ../compat/libgdbm_compat.la ../src/libgdbm.la'
-      substituteInPlace tests/testsuite.at --replace \
-        'm4_include([dbmfetch03.at])' ""
-  '';
+  doCheck = true;
 
   enableParallelBuilding = true;
-  configureFlags = [ "--enable-libgdbm-compat" ];
+
+  # 1. Linking static stubs on cygwin requires correct ordering. Consider
+  #    upstreaming this.
+  #
+  # 2. Disable dbmfetch03.at test because it depends on unlink() failing on a
+  #    link in a chmod -w directory, which cygwin apparently allows.
+  postPatch = lib.optionalString stdenv.buildPlatform.isCygwin ''
+      substituteInPlace tests/Makefile.in \
+        --replace-fail '_LDADD = ../src/libgdbm.la ../compat/libgdbm_compat.la' \
+                       '_LDADD = ../compat/libgdbm_compat.la ../src/libgdbm.la'
+      substituteInPlace tests/testsuite.at
+        --replace-fail 'm4_include([dbmfetch03.at])' ""
+  '';
 
   # create symlinks for compatibility
   postInstall = ''
-    install -dm755 $out/include/gdbm
-    (
-      cd $out/include/gdbm
-      ln -s ../gdbm.h gdbm.h
-      ln -s ../ndbm.h ndbm.h
-      ln -s ../dbm.h  dbm.h
-    )
+    install -dm755 ''${!outputDev}/include/gdbm
+    pushd ''${!outputDev}/include/gdbm
+    ln -s ../dbm.h dbm.h
+    ln -s ../gdbm.h gdbm.h
+    ln -s ../ndbm.h ndbm.h
+    popd
   '';
 
-  meta = with lib; {
+  passthru = {
+    tests.version = testers.testVersion {
+      package = finalAttrs.finalPackage;
+      command = "gdbmtool --version";
+    };
+  };
+
+  meta = {
+    homepage = "https://www.gnu.org/software/gdbm/";
     description = "GNU dbm key/value database library";
     longDescription = ''
-       GNU dbm (or GDBM, for short) is a library of database functions that
-       use extensible hashing and work similar to the standard UNIX dbm.
-       These routines are provided to a programmer needing to create and
-       manipulate a hashed database.
+       GNU dbm (or GDBM, for short) is a library of database functions that use
+       extensible hashing and work similar to the standard UNIX dbm. These
+       routines are provided to a programmer needing to create and manipulate a
+       hashed database.
 
-       The basic use of GDBM is to store key/data pairs in a data file.
-       Each key must be unique and each key is paired with only one data
-       item.
+       The basic use of GDBM is to store key/data pairs in a data file. Each
+       key must be unique and each key is paired with only one data item.
 
-       The library provides primitives for storing key/data pairs,
-       searching and retrieving the data by its key and deleting a key
-       along with its data.  It also support sequential iteration over all
-       key/data pairs in a database.
+       The library provides primitives for storing key/data pairs, searching and
+       retrieving the data by its key and deleting a key along with its data.
+       It also support sequential iteration over all key/data pairs in a
+       database.
 
-       For compatibility with programs using old UNIX dbm function, the
-       package also provides traditional dbm and ndbm interfaces.
-      '';
-    homepage = "https://www.gnu.org/software/gdbm/";
-    license = licenses.gpl3Plus;
-    platforms = platforms.all;
-    maintainers = [ maintainers.vrthra ];
+       For compatibility with programs using old UNIX dbm function, the package
+       also provides traditional dbm and ndbm interfaces.
+    '';
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "gdbmtool";
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.all;
   };
-}
+})
