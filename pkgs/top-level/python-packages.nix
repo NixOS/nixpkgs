b50049c069b4cf1d3d959c26da1c49f1c4fb893b
GetPsyched <priyanshu@getpsyched.dev>
2024-07-03 10:33:45 +0400

    python3Packages.fluent-syntax: init at 0.19.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4510,6 +4510,8 @@ self: super: with self; {
 
   fluent-logger = callPackage ../development/python-modules/fluent-logger { };
 
+  fluent-syntax = callPackage ../development/python-modules/python-fluent/fluent-syntax.nix { };
+
   flufl-bounce = callPackage ../development/python-modules/flufl/bounce.nix { };
 
   flufl-i18n = callPackage ../development/python-modules/flufl/i18n.nix { };
