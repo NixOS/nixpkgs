66f3e97521380504cc21f49f587f8160d2f21652
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-03 04:06:27 +0000

    ktlint: 1.3.0 -> 1.3.1

--- pkgs/development/tools/ktlint/default.nix
+++ pkgs/development/tools/ktlint/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ktlint";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchurl {
     url = "https://github.com/pinterest/ktlint/releases/download/${version}/ktlint";
-    sha256 = "sha256-AbLg74kzg6UNvrE5cP5/o742yj6DJZ4BZJlFsJ1zaYU=";
+    sha256 = "sha256-qfkjvlj70yZwoX8Lcpsd+ASviC+ldAIWV0HLJuVEDKE=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
