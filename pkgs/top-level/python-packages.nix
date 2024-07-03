a87b988a17f6eb7848b0200dede3bbe373402144
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-03 14:00:38 +0200

    hpp-baxter: init at 6.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6565,6 +6565,13 @@ self: super: with self; {
 
   hpccm = callPackage ../development/python-modules/hpccm { };
 
+  hpp-baxter = toPythonModule (
+    pkgs.hpp-baxter.override {
+      pythonSupport = true;
+      python3Packages = self;
+    }
+  );
+
   hpp-bezier-com-traj = toPythonModule (
     pkgs.hpp-bezier-com-traj.override {
       pythonSupport = true;
