0564643c278bb95c4073dbff9c01cdff109d5d4b
Drewry Pope <drewrypope@gmail.com>
2024-07-02 22:35:44 -0500

    ???

--- pkgs/by-name/wr/wrangler/package.nix
+++ pkgs/by-name/wr/wrangler/package.nix
@@ -68,7 +68,7 @@ stdenv.mkDerivation (finalAttrs: {
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin $out/lib $out/lib/packages/wrangler
-    rm -rf node_modules/typescript
+    rm -rf node_modules/typescript node_modules/eslint
     cp -r node_modules $out/lib
     cp -r packages/wrangler/bin $out/lib/packages/wrangler
     cp -r packages/wrangler/wrangler-dist $out/lib/packages/wrangler
