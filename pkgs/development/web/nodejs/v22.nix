05c5ffbbf8bbcc0aa6a4e4943552e8c977e1ebad
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-07-04 15:37:37 +0200

    nodejs_22: 22.3.0 -> 22.4.0 (#323982)
    (cherry picked from commit 87327df1061ee068d2a1d1b2d8bd72a68e010cc5)

--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -8,19 +8,13 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "22.3.0";
-  sha256 = "0k0h4s9s2y0ms3g6xhynsqsrkl9hz001dmj6j0gpc5x5vk8mpf5z";
+  version = "22.4.0";
+  sha256 = "sha256-KStDAITy8ykT3H2k6y+8iWklJ3Kp0b/ormxLSpjKOtM=";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
     ./node-npm-build-npm-package-logic.patch
     ./use-correct-env-in-tests.patch
     ./bin-sh-node-run-v22.patch
-    (fetchpatch2 {
-      # Fixes OpenSSL 3.0.14 compatibility in tests.
-      # See https://github.com/nodejs/node/pull/53373
-      url = "https://github.com/nodejs/node/commit/14863e80584e579fd48c55f6373878c821c7ff7e.patch";
-      hash = "sha256-I7Wjc7DE059a/ZyXAvAqEGvDudPjxQqtkBafckHCFzo=";
-    })
   ];
 }
