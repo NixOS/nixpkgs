55cdd65163c34fa899557305ffc68fad02ed7d39
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-07-04 12:36:42 -0400

    pdfarranger: migrate to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11601,8 +11601,6 @@ with pkgs;
 
   pdf-quench = callPackage ../applications/misc/pdf-quench { };
 
-  pdfarranger = callPackage ../applications/misc/pdfarranger { };
-
   briss = callPackage ../tools/graphics/briss { };
 
   brickd = callPackage ../servers/brickd { };
