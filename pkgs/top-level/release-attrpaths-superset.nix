bb7a5dd5791401c434d83ceb81cf1eba4c1683f1
Philip Taron <philip.taron@gmail.com>
2024-07-04 15:44:51 -0700

    Remove buildPackages and test-pkgs from excluded-attrnames-at-any-depth

--- pkgs/top-level/release-attrpaths-superset.nix
+++ pkgs/top-level/release-attrpaths-superset.nix
@@ -75,11 +75,7 @@ let
     newScope = true;
     scope = true;
     pkgs = true;
-    test-pkgs = true;
-
-    buildPackages = true;
     buildFreebsd = true;
-
     callPackage = true;
     mkDerivation = true;
     overrideDerivation = true;
