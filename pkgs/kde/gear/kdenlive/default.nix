9be77b47b6485abf071fd367ee621b3f2d446de3
Tobias Bora <tobias.bora@gmail.com>
2024-07-04 14:43:36 +0200

    kdenlive: minor refactoring

--- pkgs/kde/gear/kdenlive/default.nix
+++ pkgs/kde/gear/kdenlive/default.nix
@@ -14,7 +14,6 @@
   frei0r,
   fetchpatch,
   glaxnimate,
-  enableGlaxnimate ? true,
 }:
 mkKdeDerivation {
   pname = "kdenlive";
@@ -34,12 +33,7 @@ mkKdeDerivation {
       url = "https://invent.kde.org/multimedia/kdenlive/-/commit/8be0e826471332bb739344ebe1859298c46e9e0f.patch";
       hash = "sha256-5hLePH5NlO4Lx8lg9kjBPI4jTmP666RGplaVCmS/9TA=";
     })
-  ] ++ lib.optional enableGlaxnimate (
-    substituteAll {
-      src = ./dependency-paths-glaxnimate.patch;
-      inherit glaxnimate;
-    }
-  );
+  ];
 
   extraBuildInputs = [
     qtsvg
@@ -51,7 +45,8 @@ mkKdeDerivation {
     mlt
     shared-mime-info
     libv4l
-  ] ++ lib.optional enableGlaxnimate glaxnimate;
+    glaxnimate
+  ];
 
   qtWrapperArgs = [
     "--set FREI0R_PATH ${frei0r}/lib/frei0r-1"
