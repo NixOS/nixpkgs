3052f83ada58b634154f75b3f285f776615f9d9f
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-05 07:41:50 +0000

    python311Packages.pytapo: 3.3.21 -> 3.3.23

--- pkgs/development/python-modules/pytapo/default.nix
+++ pkgs/development/python-modules/pytapo/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pytapo";
-  version = "3.3.21";
+  version = "3.3.23";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-rc9XhV99vzgoUF5ERFmJHHB9GMwq5Y44CJKg+g5tjOo=";
+    hash = "sha256-fjZ3DK95d0keYaMbBaRJTncnaxpWea+kIGzhh/fYDxw=";
   };
 
   build-system = [ setuptools ];
