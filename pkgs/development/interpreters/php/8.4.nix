dd3bfda229f111ebe2d0621b5fec9131c596e95c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-07-05 16:36:46 +0200

    php84: init at 8.4.0alpha1

---
+++ pkgs/development/interpreters/php/8.4.nix
@@ -0,0 +1,63 @@
+{ callPackage, fetchurl, ... }@_args:
+
+let
+  base = (
+    callPackage ./generic.nix (
+      _args
+      // {
+        version = "8.4.0alpha1";
+        phpSrc = fetchurl {
+          url = "https://downloads.php.net/~saki/php-8.4.0alpha1.tar.xz";
+          hash = "sha256-ZZA6et1RNQVAtWf4zS2WSsETZr8z4bKHSJdl/qxFJ44=";
+        };
+      }
+    )
+  );
+in
+base.withExtensions (
+  { all, ... }:
+  with all;
+  ([
+    bcmath
+    calendar
+    curl
+    ctype
+    dom
+    exif
+    fileinfo
+    filter
+    ftp
+    gd
+    gettext
+    gmp
+    iconv
+    intl
+    ldap
+    mbstring
+    mysqli
+    mysqlnd
+    opcache
+    openssl
+    pcntl
+    pdo
+    pdo_mysql
+    pdo_odbc
+    pdo_pgsql
+    pdo_sqlite
+    pgsql
+    posix
+    readline
+    session
+    simplexml
+    sockets
+    soap
+    sodium
+    sysvsem
+    sqlite3
+    tokenizer
+    xmlreader
+    xmlwriter
+    zip
+    zlib
+  ])
+)
