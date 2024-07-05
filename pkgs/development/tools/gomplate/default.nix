121127d295eee0acdb99e7fe9c8d4f6ec63ed9e3
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-07-05 14:28:25 +0200

    gomplate: 3.11.8 -> 4.0.1
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/development/tools/gomplate/default.nix
+++ pkgs/development/tools/gomplate/default.nix
@@ -5,37 +5,36 @@
 
 buildGoModule rec {
   pname = "gomplate";
-  version = "3.11.8";
+  version = "4.0.1";
 
   src = fetchFromGitHub {
     owner = "hairyhenderson";
-    repo = pname;
+    repo = "gomplate";
     rev = "refs/tags/v${version}";
-    hash = "sha256-pE9TLEBY1KQvgMFwVJfn5kojESqZURcbCwRt4jrexhk=";
+    hash = "sha256-+oc7/CFqLA1xvA1c5opKBUa+6AJ2SK7SwFeaNqw3x08=";
   };
 
-  vendorHash = "sha256-09QUEudbWnO11iwJafF9zoYqbTr7SVBUiPWTHGnZ06Q=";
+  vendorHash = "sha256-xifJNyw8BCRoQVyBvyRRA1S03e3x5/ORwjpmGlKcA0g=";
 
-  postPatch = ''
+  ldflags = [
+    "-s"
+    "-X github.com/${src.owner}/${pname}/v4/version.Version=${version}"
+  ];
+
+  preCheck = ''
     # some tests require network access
     rm net/net_test.go \
       internal/tests/integration/datasources_blob_test.go \
-      internal/tests/integration/datasources_git_test.go
+      internal/tests/integration/datasources_git_test.go \
+      render_test.go
     # some tests rely on external tools we'd rather not depend on
     rm internal/tests/integration/datasources_consul_test.go \
       internal/tests/integration/datasources_vault*_test.go
   '';
 
-  # TestInputDir_RespectsUlimit
-  preCheck = ''
-    ulimit -n 1024
+  postInstall = ''
+    rm $out/bin/gen
   '';
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X github.com/${src.owner}/${pname}/v3/version.Version=${version}"
-  ];
 
   meta = with lib; {
     description = "Flexible commandline tool for template rendering";
