cea85f2b1eb58d95bf7b42c2bd793cc4f117f191
Robert Schütz <mail@dotlambda.de>
2024-07-05 13:10:59 -0700

    python312Packages.safety: 3.2.3 -> 3.2.4
    Changelog: https://github.com/pyupio/safety/blob/3.2.4/CHANGELOG.md

--- pkgs/development/python-modules/safety/default.nix
+++ pkgs/development/python-modules/safety/default.nix
@@ -19,12 +19,13 @@
   pydantic,
   safety-schemas,
   typing-extensions,
+  filelock,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "safety";
-  version = "3.2.3";
+  version = "3.2.4";
 
   disabled = pythonOlder "3.7";
 
@@ -32,7 +33,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-QUFUk08XJ9r4pkc0k5RP7LOAVAw/AIddwa43c4L32D8=";
+    hash = "sha256-usAgIBbXNqIRgFeWSg45g/og/yVj/RA8rD86we0/6hE=";
   };
 
   postPatch = ''
@@ -47,18 +48,14 @@ buildPythonPackage rec {
       --replace-fail "telemetry=True" "telemetry=False"
   '';
 
-  nativeBuildInputs = [
-    setuptools
-  ];
+  build-system = [ setuptools ];
 
   pythonRelaxDeps = [
-    "packaging"
     "dparse"
-    "authlib"
-    "pydantic"
+    "filelock"
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     setuptools
     click
     urllib3
@@ -75,6 +72,7 @@ buildPythonPackage rec {
     pydantic
     safety-schemas
     typing-extensions
+    filelock
   ];
 
   nativeCheckInputs = [ pytestCheckHook ];
