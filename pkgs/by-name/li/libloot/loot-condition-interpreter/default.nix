5978d88a310c6f024d9206a9eb7579d0ec276ae5
schnusch <schnusch@users.noreply.github.com>
2024-07-05 14:13:50 +0200

    libloot: enable more tests

--- pkgs/by-name/li/libloot/loot-condition-interpreter/default.nix
+++ pkgs/by-name/li/libloot/loot-condition-interpreter/default.nix
@@ -52,14 +52,17 @@ rustPlatform.buildRustPackage rec {
       hash = "sha256-gRkRQ5XmzPPYm3QZQY45CebX5koNWU7eVuB8Ojls3nM=";
     }}
     (cd tests && ln -s "../$tmp"/* libloot_win64)
-  '';
 
-  checkFlags = [
-    # We don't want to download, extract, and then fix-up another archive
-    # for those two tests.
-    "--skip=function::version::tests::constructors::version_read_file_version_should_return_none_if_there_is_no_version_info"
-    "--skip=function::version::tests::constructors::version_read_product_version_should_return_none_if_there_is_no_version_info"
-  ];
+    # needed by:
+    # function::version::tests::constructors::version_read_file_version_should_return_none_if_there_is_no_version_info
+    # function::version::tests::constructors::version_read_product_version_should_return_none_if_there_is_no_version_info
+    tmp=$(mktemp -dp .)
+    ${p7zip}/bin/7z x -o"$tmp" ${fetchurl {
+      url = "https://github.com/loot/loot-api-python/releases/download/4.0.2/loot_api_python-4.0.2-0-gd356ac2_master-python2.7-win32.7z";
+      hash = "sha256-g6iYRGsT63PfACNHAnwPXdit+aQEyE7KZdtXEDCP+vw=";
+    }}
+    (cd tests && ln -s "../$tmp"/* loot_api_python)
+  '';
 
   # FIXME
   postInstall = ''
