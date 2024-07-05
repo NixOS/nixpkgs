c39f1864ba03dd7c4f986b66a0c60b0527a4899d
Simon Menke <simon.menke@gmail.com>
2024-07-05 14:56:27 +0200

    shopify-cli: v2.34.0 -> v3.63.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12729,8 +12729,6 @@ with pkgs;
 
   shocco = callPackage ../tools/text/shocco { };
 
-  shopify-cli = callPackage ../development/web/shopify-cli { };
-
   shopify-themekit = callPackage ../development/web/shopify-themekit { };
 
   shorewall = callPackage ../tools/networking/shorewall { };
