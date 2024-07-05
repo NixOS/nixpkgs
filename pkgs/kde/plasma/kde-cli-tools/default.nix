7ffa44e7b2cfe51a896b10cfd83636c1d912d3ca
José Romildo <malaquias@gmail.com>
2024-07-05 06:59:28 -0300

    kdePackages.kde-cli-tools: install a symlink to kdesu in bin

--- pkgs/kde/plasma/kde-cli-tools/default.nix
+++ pkgs/kde/plasma/kde-cli-tools/default.nix
@@ -6,4 +6,10 @@ mkKdeDerivation {
   pname = "kde-cli-tools";
 
   extraBuildInputs = [qtsvg];
+
+  postInstall = ''
+    # install a symlink in bin so that kdesu can eventually be found in PATH
+    mkdir -p $out/bin
+    ln -s $out/libexec/kf6/kdesu $out/bin/
+  '';
 }
