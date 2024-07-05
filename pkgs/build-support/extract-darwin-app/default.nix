8fdeba1f9da8850025a8f3e2d18de41d0a1abcd1
Luke Worth <luke.worth@bellroy.com>
2024-07-05 10:04:04 +1000

    refactor(extract-darwin-app): remove unused srcHasManySubfolders,sourceRoot

--- pkgs/build-support/extract-darwin-app/default.nix
+++ pkgs/build-support/extract-darwin-app/default.nix
@@ -28,15 +28,6 @@
 , version
 , /** a nix path or derivation to be used as mkDerivation.src */
   src
-, /** The unpackPhase fails with many subfolders!
-    * Also set this if `src` is a .dmg or another archive
-    *  that extracts to a folder containing multiple subfolders.
-    * The assumption is made a folder named "$appName.app" exists.
-    * Should you know the true name, use the `sourceRoot` option instead.
-    */
-  srcHasManySubfolders ? false
-, /** See `srcHasManySubfolders` */
-  sourceRoot ? null
 , /** Whether to use makeWrapper for the binary.
     * For some packages symbolic links just don't work
     */
@@ -46,52 +37,42 @@
 , /** Can use a custom function e.g stdenvNoCC.mkDerivation */
   derivationFunction ? stdenv.mkDerivation
 }:
-let
-  # Prefer sourceRoot over srcHasManySubfolders (if both are set)
-  _sourceRoot = (lib.findFirst (el: el.condition) { value = null; } [
-    { condition = sourceRoot != null; value = sourceRoot; }
-    { condition = srcHasManySubfolders; value = "${appName}.app"; }
-  ]).value;
-in
-derivationFunction (
+derivationFunction
   # .dmg extracts have multiple folders
   # the unpack phase requires only one folder
-  (lib.attrsets.optionalAttrs (lib.isString _sourceRoot) {
-    sourceRoot = _sourceRoot;
-  }) // {
-    inherit pname version src;
-    nativeBuildInputs = (lib.optional wrapBinary makeWrapper) ++ [ undmg ];
+{
+  inherit pname version src;
+  nativeBuildInputs = (lib.optional wrapBinary makeWrapper) ++ [ undmg ];
 
-    installPhase = ''
-      appDir="$out/Applications/${appName}.app"
-      binDir="$appDir"/Contents/MacOS
+  installPhase = ''
+    appDir="$out/Applications/${appName}.app"
+    binDir="$appDir"/Contents/MacOS
 
-      mkdir -p $out/bin
-      if [[ -d Contents/ ]] ; then
-        # .dmg uses sourceRoot, which means PWD is already in the sourceRoot
-        # Passing a .app folder also means we've already cd'ed into the folder
-        mkdir -p $appDir
-        cp -r Contents $appDir
-      else
-        # expects a .app folder in the source folder
-        mkdir $out/Applications
-        cp -r ${appName}.app/ $appDir
-      fi
+    mkdir -p $out/bin
+    if [[ -d Contents/ ]] ; then
+      # .dmg uses sourceRoot, which means PWD is already in the sourceRoot
+      # Passing a .app folder also means we've already cd'ed into the folder
+      mkdir -p $appDir
+      cp -r Contents $appDir
+    else
+      # expects a .app folder in the source folder
+      mkdir $out/Applications
+      cp -r ${appName}.app/ $appDir
+    fi
 
-      # Make application available in bin/
-      if [[ "${toString wrapBinary}" = "1" ]] ; then
-        makeWrapper $binDir/${binaryName} $out/bin/${pname}
-      else
-        ln -s $binDir/${binaryName} $out/bin
-      fi
-    '';
+    # Make application available in bin/
+    if [[ "${toString wrapBinary}" = "1" ]] ; then
+      makeWrapper $binDir/${binaryName} $out/bin/${pname}
+    else
+      ln -s $binDir/${binaryName} $out/bin
+    fi
+  '';
 
-    meta = {
-      platforms = lib.platforms.darwin;
-      mainProgram = binaryName;
-      name = pname;
-      hydraPlatforms = [ ];
-      sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
-    } // packageMeta;
-  }
-)
+  meta = {
+    platforms = lib.platforms.darwin;
+    mainProgram = binaryName;
+    name = pname;
+    hydraPlatforms = [ ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+  } // packageMeta;
+}
