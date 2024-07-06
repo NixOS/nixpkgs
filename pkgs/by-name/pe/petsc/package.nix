7fa522446b3b6189e71a5bce38d8e41398e0bcb6
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-06 23:18:57 +0000

    petsc: 3.21.0 -> 3.21.3

--- pkgs/by-name/pe/petsc/package.nix
+++ pkgs/by-name/pe/petsc/package.nix
@@ -28,11 +28,11 @@ assert petsc-withp4est -> p4est.mpiSupport;
 
 stdenv.mkDerivation rec {
   pname = "petsc";
-  version = "3.21.0";
+  version = "3.21.3";
 
   src = fetchzip {
     url = "https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-${version}.tar.gz";
-    hash = "sha256-2J6jtIKz1ZT9qwN8tuYQNBIeBJdE4Gt9cE3b5rTIeF4=";
+    hash = "sha256-dxHa8JUJCN4zRIXMCx7gcvbzFH2SPtkJ377ssIevjgU=";
   };
 
   inherit mpiSupport;
