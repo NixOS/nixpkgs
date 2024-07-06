01d721bc86531b72d60e2deb42fd1cbfd15ea457
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-07-06 22:23:23 +0200

    python311Packages.notifications-android-tv: drop support for python 3.8 and 3.9

--- pkgs/development/python-modules/notifications-android-tv/default.nix
+++ pkgs/development/python-modules/notifications-android-tv/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   pname = "notifications-android-tv";
   version = "1.2.2";
   format = "pyproject";
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.10";
 
   src = fetchFromGitHub {
     owner = "engrbm87";
