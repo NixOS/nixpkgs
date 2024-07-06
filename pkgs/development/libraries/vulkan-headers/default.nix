959a4b1440d3d6fc20b4b08ce43c0f77a0373fd2
Francesco Gazzetta <fgaz@fgaz.me>
2024-07-06 15:05:19 +0200

    vulkan-headers: 1.3.283.0 -> 1.3.289

--- pkgs/development/libraries/vulkan-headers/default.nix
+++ pkgs/development/libraries/vulkan-headers/default.nix
@@ -1,15 +1,15 @@
 { lib, stdenv, fetchFromGitHub, cmake }:
 stdenv.mkDerivation rec {
   pname = "vulkan-headers";
-  version = "1.3.283.0";
+  version = "1.3.289";
 
   nativeBuildInputs = [ cmake ];
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "Vulkan-Headers";
-    rev = "vulkan-sdk-${version}";
-    hash = "sha256-DpbTYlEJPtyf/m9QEI8fdAm1Hw8MpFd+iCd7WB2gp/M=";
+    rev = "v${version}";
+    hash = "sha256-pcsTzdSTMwGZ4daEIS24s5COim4C6edIBcrUTbXIEBw=";
   };
 
   passthru.updateScript = ./update.sh;
