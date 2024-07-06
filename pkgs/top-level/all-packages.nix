b7442e877062473e5a7e6ed3076b2df28763c7c2
NotAShelf <raf@notashelf.dev>
2024-07-06 19:51:21 +0300

    prettierd: move to by-name; nixfmt-rfc-style

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11870,8 +11870,6 @@ with pkgs;
 
   pretender = callPackage ../tools/security/pretender { };
 
-  prettierd = callPackage ../development/tools/prettierd { };
-
   pretty-simple = callPackage ../development/tools/pretty-simple { };
 
   prettyping = callPackage ../tools/networking/prettyping { };
