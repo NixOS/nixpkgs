993a12e41329ef548608be1d1db237c7492eb05b
qubitnano <146656568+qubitnano@users.noreply.github.com>
2024-07-06 16:12:14 -0400

    z64decompress: init at 1.0.3-unstable-2023-12-21

---
+++ pkgs/by-name/z6/z64decompress/package.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  unstableGitUpdater,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "z64decompress";
+  version = "1.0.3-unstable-2023-12-21";
+
+  src = fetchFromGitHub {
+    owner = "z64tools";
+    repo = "z64decompress";
+    rev = "e2b3707271994a2a1b3afc6c3997a7cf6b479765";
+    hash = "sha256-PHiOeEB9njJPsl6ScdoDVwJXGqOdIIJCZRbIXSieBIY=";
+  };
+
+  makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 -t $out/bin z64decompress
+    install -Dm644 -t $out/share/licenses/${finalAttrs.pname} LICENSE
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = unstableGitUpdater { };
+
+  meta = {
+    description = "Zelda 64 rom decompressor";
+    homepage = "https://github.com/z64tools/z64decompress";
+    license = with lib.licenses; [
+      gpl3Only
+
+      # Reverse engineering
+      unfree
+    ];
+    maintainers = with lib.maintainers; [ qubitnano ];
+    mainProgram = "z64decompress";
+    platforms = lib.platforms.linux;
+    hydraPlatforms = [ ];
+  };
+})
