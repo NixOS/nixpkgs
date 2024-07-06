c7b88b1002ed13ad9c279930dfa6bdea94c1a2ee
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-07-06 16:49:34 +0530

    elmPackages.elm-test-rs: move to fetchFromGitHub
    nixfmt-rfc-style

--- pkgs/development/compilers/elm/packages/elm-test-rs/default.nix
+++ pkgs/development/compilers/elm/packages/elm-test-rs/default.nix
@@ -1,35 +1,50 @@
-{ lib, rustPlatform, fetchurl, openssl, stdenv, darwin }:
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  openssl,
+  stdenv,
+  darwin,
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "elm-test-rs";
   version = "3.0";
 
-  src = fetchurl {
-    url = "https://github.com/mpizenberg/elm-test-rs/archive/v${version}.tar.gz";
-    sha256 = "sha256-nrX+jb/fKwoQSmQTim3sew0uGg5+nscL22YP5lkFyls=";
+  src = fetchFromGitHub {
+    owner = "mpizenberg";
+    repo = "elm-test-rs";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-l3RV+j3wAQ88QGNXLILp7YiUpdk7bkN25Y723pDZw48=";
   };
 
-  buildInputs = lib.optionals (!stdenv.isDarwin) [
-    openssl
-  ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-    Security
-    CoreServices
-  ]);
+  buildInputs =
+    lib.optionals (!stdenv.isDarwin) [ openssl ]
+    ++ lib.optionals stdenv.isDarwin (
+      with darwin.apple_sdk.frameworks;
+      [
+        Security
+        CoreServices
+      ]
+    );
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-         "pubgrub-dependency-provider-elm-0.1.0" = "sha256-00J5XZfmuB4/fgB06aaXrRjdmOpOsSwA3dC3Li1m2Cc=";
-       };
+      "pubgrub-dependency-provider-elm-0.1.0" = "sha256-00J5XZfmuB4/fgB06aaXrRjdmOpOsSwA3dC3Li1m2Cc=";
+    };
   };
   # Tests perform networking and therefore can't work in sandbox
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     description = "Fast and portable executable to run your Elm tests";
     mainProgram = "elm-test-rs";
     homepage = "https://github.com/mpizenberg/elm-test-rs";
-    license = licenses.bsd3;
-    maintainers = [ maintainers.jpagex maintainers.zupo ];
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [
+      jpagex
+      zupo
+    ];
   };
 }
