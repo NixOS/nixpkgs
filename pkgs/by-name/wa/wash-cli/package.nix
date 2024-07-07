358d137b5c742f1fe765f088a968f2ea2ac13f34
Daniel Schaefer <dhs@frame.work>
2024-07-07 16:44:43 +0800

    wash-cli: 0.28.1 -> 0.29.2
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- pkgs/by-name/wa/wash-cli/package.nix
+++ pkgs/by-name/wa/wash-cli/package.nix
@@ -2,24 +2,24 @@
 
 let
   wasiPreviewCommandComponentAdapter = fetchurl {
-    url = "https://github.com/bytecodealliance/wasmtime/releases/download/v21.0.1/wasi_snapshot_preview1.command.wasm";
-    hash = "sha256-Dr/vQlMF8h79vAMtwuAtq6vII1h94kcUdaSspFIsbKY=";
+    url = "https://github.com/bytecodealliance/wasmtime/releases/download/v22.0.0/wasi_snapshot_preview1.command.wasm";
+    hash = "sha256-UVBFddlI0Yh1ZNs0b2jSnKsHvGGAS5U09yuwm8Q6lxw=";
   };
   wasiPreviewReactorComponentAdapter = fetchurl {
-    url = "https://github.com/bytecodealliance/wasmtime/releases/download/v21.0.1/wasi_snapshot_preview1.reactor.wasm";
-    hash = "sha256-NWumxeKoUt8HlwVJSVAbnlH9XGyOZGoNo6gJbRi+xRo=";
+    url = "https://github.com/bytecodealliance/wasmtime/releases/download/v22.0.0/wasi_snapshot_preview1.reactor.wasm";
+    hash = "sha256-oE53IRMZgysSWT7RhrpZJjdaIyzCRf0h4d1yjqj/PSk=";
   };
 
 in rustPlatform.buildRustPackage rec {
   pname = "wash-cli";
-  version = "0.28.1";
+  version = "0.29.2";
 
   src = fetchCrate {
     inherit version pname;
-      hash = "sha256-sEMOINkoYBR2E3QO8Ri28TshlqZSzSVDVKavv+jCiZg=";
+      hash = "sha256-A66KSDYFbByguhnlzzU5nf8pE3lhnYQjI3h73SKB2Zo=";
   };
 
-  cargoHash = "sha256-8ftRSi2kWexD4Ltr8WejFuHdrcrgYPs0ion9/0PC0A0=";
+  cargoHash = "sha256-2mo30xHQ3aCExdI0ITDY9g/C5peN48PdUNFVVxM//+c=";
 
   nativeBuildInputs = [ pkg-config ];
 
