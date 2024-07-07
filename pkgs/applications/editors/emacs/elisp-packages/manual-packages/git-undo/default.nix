51cddfc4a557fc5ebf54a6f9b459ad6baae99c9d
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-07 11:09:22 -0300

    emacsPackages.git-undo: use melpaBuild
    - get rid of nested with

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/git-undo/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/git-undo/default.nix
@@ -1,10 +1,10 @@
-{ lib
-, fetchFromGitHub
-, emacs
-, trivialBuild
+{
+  lib,
+  fetchFromGitHub,
+  melpaBuild,
 }:
 
-trivialBuild {
+melpaBuild {
   pname = "git-undo";
   version = "0-unstable-2019-12-21";
 
@@ -12,14 +12,13 @@ trivialBuild {
     owner = "jwiegley";
     repo = "git-undo-el";
     rev = "cf31e38e7889e6ade7d2d2b9f8719fd44f52feb5";
-    sha256 = "sha256-cVkK9EF6qQyVV3uVqnBEjF8e9nEx/8ixnM8PvxqCyYE=";
+    hash = "sha256-cVkK9EF6qQyVV3uVqnBEjF8e9nEx/8ixnM8PvxqCyYE=";
   };
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/jwiegley/git-undo-el";
     description = "Revert region to most recent Git-historical version";
-    license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ leungbk ];
-    inherit (emacs.meta) platforms;
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ leungbk ];
   };
 }
