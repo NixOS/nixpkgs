1d6bfd9756935e557009058397e2d34eb5e08e60
Drewry Pope <drewrypope@gmail.com>
2024-07-07 01:54:08 -0500

    spacing

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7542,7 +7542,6 @@ with pkgs;
   timeline = callPackage ../applications/office/timeline { };
 
   tsm-client = callPackage ../tools/backup/tsm-client { };
-
   tsm-client-withGui = callPackage ../tools/backup/tsm-client { enableGui = true; };
 
   tracker = callPackage ../development/libraries/tracker { };
