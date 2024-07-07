6f80b637fb6ca24b46a5a28e1ddf4dcc80b8121d
Fabian Affolter <mail@fabian-affolter.ch>
2024-07-07 11:07:15 +0200

    python312Packages.cpe: enable tests

--- pkgs/development/python-modules/cpe/default.nix
+++ pkgs/development/python-modules/cpe/default.nix
@@ -1,9 +1,18 @@
-{ lib, fetchFromGitHub, buildPythonPackage, sphinx, setuptools }:
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  pytestCheckHook,
+  pythonOlder,
+  setuptools,
+}:
 
 buildPythonPackage rec {
   pname = "cpe";
   version = "1.2.1";
   pyproject = true;
+
+  disabled = pythonOlder "3.10";
 
   src = fetchFromGitHub {
     owner = "nilp0inter";
@@ -14,16 +23,21 @@ buildPythonPackage rec {
 
   build-system = [ setuptools ];
 
-  dependencies = [ sphinx ];
+  nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "cpe" ];
 
-  doCheck = false;
+  disabledTests = [
+    # Tests are outdated
+    "testfile_cpelang2"
+    "test_incompatible_versions"
+    "test_equals"
+  ];
 
   meta = {
     description = "Common platform enumeration for python";
     homepage = "https://github.com/nilp0inter/cpe";
-    license = lib.licenses.lgpl3Only;
+    license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [ tochiaha ];
   };
 }
