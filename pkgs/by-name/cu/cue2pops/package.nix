139e900b88e9302fcef6f5cddd6377b10382720a
NotAShelf <raf@notashelf.dev>
2024-07-07 13:44:18 +0300

    cue2pops: modernize derivation

--- pkgs/by-name/cu/cue2pops/package.nix
+++ pkgs/by-name/cu/cue2pops/package.nix
@@ -3,16 +3,15 @@
   stdenv,
   fetchFromGitHub,
 }:
-
 stdenv.mkDerivation {
   pname = "cue2pops";
-  version = "unstable-2018-01-04";
+  version = "0-unstable-2018-01-04";
 
   src = fetchFromGitHub {
     owner = "makefu";
     repo = "cue2pops-linux";
     rev = "541863adf23fdecde92eba5899f8d58586ca4551";
-    sha256 = "05w84726g3k33rz0wwb9v77g7xh4cnhy9sxlpilf775nli9bynrk";
+    hash = "sha256-M1u/UqS2nONovLTr5KFlBPbzztlpcQ5+HmOOZ8QhiBc=";
   };
 
   dontConfigure = true;
@@ -20,15 +19,17 @@ stdenv.mkDerivation {
   makeFlags = [ "CC=${stdenv.cc.targetPrefix}cc" ];
 
   installPhase = ''
+    runHook preInstall
     install --directory --mode=755 $out/bin
     install --mode=755 cue2pops $out/bin
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Convert CUE to ISO suitable to POPStarter";
     homepage = "https://github.com/makefu/cue2pops-linux";
-    maintainers = with maintainers; [ AndersonTorres ];
-    platforms = platforms.all;
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.all;
     mainProgram = "cue2pops";
   };
 }
