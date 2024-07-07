21ce0d2d77a91d757103b691b51c5e932bfede47
Alexis Praga <alexis.praga@proton.me>
2024-07-07 11:51:15 +0200

    python312Packages.tensorflow: remove distutils
    Remove distutils reference using an archlinux patches.
    Fixes #305827 while waiting for upstream fix.

--- pkgs/development/python-modules/tensorflow/default.nix
+++ pkgs/development/python-modules/tensorflow/default.nix
@@ -233,6 +233,13 @@ let
         url = "https://github.com/bazelbuild/rules_cc/commit/156497dc89100db8a3f57b23c63724759d431d05.diff";
         hash = "sha256-NES1KeQmMiUJQVoV6dS4YGRxxkZEjOpFSCyOq9HZYO0=";
       })
+
+      # https://github.com/tensorflow/tensorflow/issues/58073
+      (fetchpatch {
+        name = "tensorflow-python3.12-no-distutil";
+        url = "https://gitlab.archlinux.org/archlinux/packaging/packages/tensorflow/-/raw/fff04b07ca2bc6758f81505cc396c91f7466c08a/tensorflow-2.16.1-python-distutils-removal.patch";
+        hash = "sha256-t+vP8pwCn3D8W2s33QiPm8SjgKVjIxD6cg4kESh1dL4=";
+      })
     ];
     postPatch = "popd";
 
@@ -612,7 +619,6 @@ let
 in
 buildPythonPackage {
   inherit version pname;
-  disabled = pythonAtLeast "3.12";
 
   src = bazel-build.python;
 
