3dc8993b21cf1be360547f24e580ccbc351b7e0f
novenary <novenary@kwak.zip>
2024-07-07 12:48:45 +0300

    zathuraPkgs: move default plugins into wrapper

--- pkgs/applications/misc/zathura/default.nix
+++ pkgs/applications/misc/zathura/default.nix
@@ -10,7 +10,9 @@ lib.makeScope pkgs.newScope (
   let
     callPackage = self.callPackage;
   in
-  rec {
+  {
+    inherit useMupdf;
+
     gtk = pkgs.gtk3;
 
     zathura_core = callPackage ./core { };
@@ -25,13 +27,6 @@ lib.makeScope pkgs.newScope (
 
     zathura_cb = callPackage ./cb { };
 
-    zathuraWrapper = callPackage ./wrapper.nix {
-      plugins = [
-        zathura_djvu
-        zathura_ps
-        zathura_cb
-        (if useMupdf then zathura_pdf_mupdf else zathura_pdf_poppler)
-      ];
-    };
+    zathuraWrapper = callPackage ./wrapper.nix { };
   }
 )
