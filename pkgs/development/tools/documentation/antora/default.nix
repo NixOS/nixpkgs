bbb9e8d185d950d1b2faecbfe0c179916c3ad13c
Daniel Schaefer <dhs@frame.work>
2024-07-07 16:23:59 +0800

    antora: 3.1.8 -> 3.1.9
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- pkgs/development/tools/documentation/antora/default.nix
+++ pkgs/development/tools/documentation/antora/default.nix
@@ -2,16 +2,16 @@
 
 buildNpmPackage rec {
   pname = "antora";
-  version = "3.1.8";
+  version = "3.1.9";
 
   src = fetchFromGitLab {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-d3PkMiOY4fRuwK5UYULY7qY0dnBDWchy1L6fpXaRans=";
+    hash = "sha256-hkavYC2LO8NRIRwHNWIJLRDkVnhAB4Di3IqL8uGt+U8=";
   };
 
-  npmDepsHash = "sha256-sZMMIHVMY9usBDyGNFXpUcL7owEk6DvvIGk6U+E9jL4=";
+  npmDepsHash = "sha256-ngreuitwUcIDVF6vW7fZA1OaVxr9fv7s0IjCErXlcxg=";
 
   # This is to stop tests from being ran, as some of them fail due to trying to query remote repositories
   postPatch = ''
