06ecf990bce92d63fa44deb2fb3a8362656b22e8
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-07 19:13:47 +0000

    v2ray-domain-list-community: 20240624143214 -> 20240707162925

--- pkgs/data/misc/v2ray-domain-list-community/default.nix
+++ pkgs/data/misc/v2ray-domain-list-community/default.nix
@@ -3,12 +3,12 @@
 let
   generator = pkgsBuildBuild.buildGoModule rec {
     pname = "v2ray-domain-list-community";
-    version = "20240624143214";
+    version = "20240707162925";
     src = fetchFromGitHub {
       owner = "v2fly";
       repo = "domain-list-community";
       rev = version;
-      hash = "sha256-VaETDDfDrnniMLgm2Z2/SUdIeWfQlJYygpF6Kx++whw=";
+      hash = "sha256-Cpd66RSbKcEcLeL/ibS6cWnZABJuYCthaaMTkCsbij0=";
     };
     vendorHash = "sha256-NLh14rXRci4hgDkBJVJDIDvobndB7KYRKAX7UjyqSsg=";
     meta = with lib; {
