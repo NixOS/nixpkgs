46c18d0b2ab899bebc9afaf5696e048503b58000
Fabian Affolter <mail@fabian-affolter.ch>
2024-07-07 10:45:29 +0200

    python312Packages.pyloadapi: 1.2.0 -> 1.3.1
    Diff: https://github.com/tr4nt0r/pyloadapi/compare/refs/tags/v1.2.0...v1.3.1
    
    Changelog: https://github.com/tr4nt0r/pyloadapi/blob/1.3.1/CHANGELOG.md

--- pkgs/development/python-modules/pyloadapi/default.nix
+++ pkgs/development/python-modules/pyloadapi/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "pyloadapi";
-  version = "1.2.0";
+  version = "1.3.1";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "tr4nt0r";
     repo = "pyloadapi";
     rev = "refs/tags/v${version}";
-    hash = "sha256-uOgqc1RqmEk0Lqz/ixlChKTZva7+0v4V8KutLSgPKEE=";
+    hash = "sha256-tgK2zxgd0v2JEWgFwJLcNngDobuttM7FHKuHfTeFo14=";
   };
 
   postPatch = ''
@@ -46,6 +46,11 @@ buildPythonPackage rec {
   ];
 
   pythonImportsCheck = [ "pyloadapi" ];
+
+  disabledTestPaths = [
+    # Tests require network access
+    "tests/test_cli.py"
+  ];
 
   meta = with lib; {
     description = "Simple wrapper for pyLoad's API";
