dfd941f9a2c57b53ccc09e221dba6f8a24ba0e48
euxane <euxane.trangirard@pacien.net>
2024-07-07 03:09:46 +0200

    python3Packages.jupysql-plugin: init at 0.4.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6323,6 +6323,8 @@ self: super: with self; {
 
   junos-eznc = callPackage ../development/python-modules/junos-eznc { };
 
+  jupysql-plugin = callPackage ../development/python-modules/jupysql-plugin { };
+
   jupyter = callPackage ../development/python-modules/jupyter { };
 
   jupyter-book = callPackage ../development/python-modules/jupyter-book { };
