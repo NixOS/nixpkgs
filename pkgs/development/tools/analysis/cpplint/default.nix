89dcfa2ee95411d34dce9bd112289d154ca58cad
Gaetan Lepage <gaetan@glepage.com>
2024-07-08 22:28:28 +0200

    cpplint: 1.5.5 -> 1.6.1
    Diff: https://github.com/cpplint/cpplint/compare/1.5.5...1.6.1

--- pkgs/development/tools/analysis/cpplint/default.nix
+++ pkgs/development/tools/analysis/cpplint/default.nix
@@ -1,49 +1,46 @@
-{ lib, python3Packages, fetchFromGitHub, fetchpatch }:
+{ lib, python3Packages, fetchFromGitHub }:
 
 python3Packages.buildPythonApplication rec {
   pname = "cpplint";
-  version = "1.5.5";
-  format = "setuptools";
+  version = "1.6.1";
+  pyproject = true;
 
   # Fetch from github instead of pypi, since the test cases are not in the pypi archive
   src = fetchFromGitHub {
-    owner = pname;
-    repo = pname;
-    rev = version;
-    hash = "sha256-JXz2Ufo7JSceZVqYwCRkuAsOR08znZlIUk8GCLAyiI4=";
+    owner = "cpplint";
+    repo = "cpplint";
+    rev = "refs/tags/${version}";
+    hash = "sha256-N5YrlhEXQGYxhsJ4M5dGYZUzA81GKRSI83goaqbtCkI=";
   };
 
-  patches = [
-    ./0001-Remove-pytest-runner-version-pin.patch
-
-    # The patch below stops using the sre_compile module, which was deprecated
-    # in Python 3.11 and replaces it with re.compile. Upstream is unsure if it
-    # should use re.compile or re._compiler.compile, so we should monitor the
-    # thread for updates.
-    #
-    #   https://github.com/cpplint/cpplint/pull/214
-    #
-    (fetchpatch {
-      name = "python-3.11-compatibility.patch";
-      url = "https://github.com/cpplint/cpplint/commit/e84e84f53915ae2a9214e756cf89c573a73bbcd3.patch";
-      hash = "sha256-u57AFWaVmGFSsvSGq1x9gZmTsuZPqXvTC7mTfyb2164=";
-    })
-  ];
-
   postPatch = ''
+    substituteInPlace setup.py \
+      --replace-fail '"pytest-runner==5.2"' ""
+
     patchShebangs cpplint_unittest.py
+
+    substituteInPlace cpplint_unittest.py \
+      --replace-fail "assertEquals" "assertEqual"
   '';
 
-  nativeCheckInputs = with python3Packages; [ pytest pytest-runner ];
+  build-system = with python3Packages; [
+    setuptools
+  ];
+
+  nativeCheckInputs = with python3Packages; [
+    pytest
+    pytest-runner
+  ];
+
   checkPhase = ''
     ./cpplint_unittest.py
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/cpplint/cpplint";
     description = "Static code checker for C++";
     mainProgram = "cpplint";
-    maintainers = [ maintainers.bhipple ];
-    license = [ licenses.bsd3 ];
+    maintainers = [ lib.maintainers.bhipple ];
+    license = [ lib.licenses.bsd3 ];
   };
 }
