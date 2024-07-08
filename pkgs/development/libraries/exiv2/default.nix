dfeac33400eb9d273e130bff9299cc6af97ac868
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-08 13:22:13 +0000

    exiv2: 0.28.2 -> 0.28.3
    (cherry picked from commit 67c485796cb2152e029ba01e855391e91476b6ed)

--- pkgs/development/libraries/exiv2/default.nix
+++ pkgs/development/libraries/exiv2/default.nix
@@ -20,7 +20,7 @@
 
 stdenv.mkDerivation rec {
   pname = "exiv2";
-  version = "0.28.2";
+  version = "0.28.3";
 
   outputs = [ "out" "lib" "dev" "doc" "man" ];
 
@@ -28,7 +28,7 @@ stdenv.mkDerivation rec {
     owner = "exiv2";
     repo = "exiv2";
     rev = "v${version}";
-    hash = "sha256-0TgvIiuHMeohStIwmHOq4yvTj2H07wyx4w3iIdkrLTc=";
+    hash = "sha256-KD3kkIQXPcjCVZZrmbn93xYbJ8ryG0mLFbkO5AVqnTw=";
   };
 
   nativeBuildInputs = [
