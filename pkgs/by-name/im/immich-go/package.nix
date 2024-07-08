2d06c51e57970cd08e4213e6e158c91827a5ec4b
Kai Norman Clasen <k.clasen@protonmail.com>
2024-07-08 08:46:56 +0200

    immich-go: 0.18.2 -> 0.19.1
    Diff: https://github.com/simulot/immich-go/compare/0.18.2...0.19.1
    
    Changelog: https://github.com/simulot/immich-go/releases/tag/0.19.1

--- pkgs/by-name/im/immich-go/package.nix
+++ pkgs/by-name/im/immich-go/package.nix
@@ -1,13 +1,13 @@
 { lib, buildGoModule, fetchFromGitHub, nix-update-script, testers, immich-go }:
 buildGoModule rec {
   pname = "immich-go";
-  version = "0.18.2";
+  version = "0.19.1";
 
   src = fetchFromGitHub {
     owner = "simulot";
     repo = "immich-go";
     rev = "${version}";
-    hash = "sha256-cP31y/MfqsbJlzshISfvwT9m7N4sBU0/b8kYt4vwIU8=";
+    hash = "sha256-IFNl6qlhXfrdNcHhgoqHZxcWPp8TSpSplirPxSqvjPA=";
 
     # Inspired by: https://github.com/NixOS/nixpkgs/blob/f2d7a289c5a5ece8521dd082b81ac7e4a57c2c5c/pkgs/applications/graphics/pdfcpu/default.nix#L20-L32
     # The intention here is to write the information into files in the `src`'s
@@ -44,7 +44,7 @@ buildGoModule rec {
     updateScript = nix-update-script { };
     tests.versionTest = testers.testVersion {
       package = immich-go;
-      command = "immich-go -h";
+      command = "immich-go -version";
       version = version;
     };
   };
