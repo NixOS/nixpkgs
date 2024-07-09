1da011c7862b2515bda36cb89e29b9c0905c14dd
John Chadwick <john@jchw.io>
2024-07-09 08:03:27 -0400

    pythonPackages.pyexcel-ods: migrate to pynose, pep517 builder

--- pkgs/development/python-modules/pyexcel-ods/default.nix
+++ pkgs/development/python-modules/pyexcel-ods/default.nix
@@ -4,21 +4,24 @@
   fetchPypi,
   pyexcel-io,
   odfpy,
-  nose,
+  pynose,
   pyexcel,
   pyexcel-xls,
   psutil,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "pyexcel-ods";
   version = "0.6.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "f61b56515fd4ccd4687f0a112422f74ce8535247ad2da49db90038d7e3ed397c";
   };
+
+  build-system = [ setuptools ];
 
   propagatedBuildInputs = [
     pyexcel-io
@@ -26,13 +29,17 @@ buildPythonPackage rec {
   ];
 
   nativeCheckInputs = [
-    nose
+    pynose
     pyexcel
     pyexcel-xls
     psutil
   ];
 
-  checkPhase = "nosetests";
+  checkPhase = ''
+    runHook preCheck
+    nosetests
+    runHook postCheck
+  '';
 
   meta = {
     description = "Plug-in to pyexcel providing the capbility to read, manipulate and write data in ods formats using odfpy";
