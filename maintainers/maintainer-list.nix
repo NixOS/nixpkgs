fab7ede032cbd03b20ae54cf32e9724d4e6ce235
kylelovestoad <kylelovestoad@protonmail.com>
2024-07-10 04:01:00 -0400

    maintainers: add kylelovestoad
    maintainers: add email alias

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11052,6 +11052,12 @@
     github = "kylehendricks";
     githubId = 981958;
   };
+  kylelovestoad = {
+    name = "kylelovestoad";
+    github = "kylelovestoad";
+    githubId = 20408518;
+    email = "kylelovestoad+nixpkgs@protonmail.com";
+  };
   kyleondy = {
     email = "kyle@ondy.org";
     github = "KyleOndy";
