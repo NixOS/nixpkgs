71448bf2147464c004ed1d38181ab6edd2ad5019
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-10 01:16:23 +0000

    nomacs: 3.17.2295 -> 3.19.0

--- pkgs/by-name/no/nomacs/package.nix
+++ pkgs/by-name/no/nomacs/package.nix
@@ -12,14 +12,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "nomacs";
-  version = "3.17.2295";
+  version = "3.19.0";
 
   src = fetchFromGitHub {
     owner = "nomacs";
     repo = "nomacs";
     rev = finalAttrs.version;
     fetchSubmodules = false; # We'll use our own
-    hash = "sha256-jHr7J0X1v2n/ZK0y3b/XPDISk7e08VWS6nicJU4fKKY=";
+    hash = "sha256-lpmM2GfMDlIp1vnbHMaOdicFcKH6LwEoKSETMt7C1NY=";
   };
 
   outputs = [ "out" ]
