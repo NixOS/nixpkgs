3a8792223f6527b26cdb068ed29e0adbdb24859b
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-07-10 10:28:20 +1000

    vcluster: unpin buildGoModule

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -39750,9 +39750,7 @@ with pkgs;
 
   ib-controller = callPackage ../applications/office/ib/controller { jdk=oraclejdk8; };
 
-  vcluster = callPackage ../applications/networking/cluster/vcluster {
-    buildGoModule = buildGo122Module;
-  };
+  vcluster = callPackage ../applications/networking/cluster/vcluster { };
 
   sshportal = callPackage ../servers/sshportal { };
 
