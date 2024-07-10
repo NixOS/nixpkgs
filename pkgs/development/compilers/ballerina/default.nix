256a0b89d919873981f0132bc8e2d7976b6164d2
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-10 02:33:48 +0000

    ballerina: 2201.9.1 -> 2201.9.2

--- pkgs/development/compilers/ballerina/default.nix
+++ pkgs/development/compilers/ballerina/default.nix
@@ -1,6 +1,6 @@
 { ballerina, lib, writeText, runCommand, makeWrapper, fetchzip, stdenv, openjdk }:
 let
-  version = "2201.9.1";
+  version = "2201.9.2";
   codeName = "swan-lake";
 in stdenv.mkDerivation {
   pname = "ballerina";
@@ -8,7 +8,7 @@ in stdenv.mkDerivation {
 
   src = fetchzip {
     url = "https://dist.ballerina.io/downloads/${version}/ballerina-${version}-${codeName}.zip";
-    hash = "sha256-5ATrR2TZHu4xr+XuGaiSZpIKTDQt6MTxg2DdU1qVjMI=";
+    hash = "sha256-J73fai5mPPNHk/wux4nhX3KsalxWER8nMqWovWgm6WA=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
