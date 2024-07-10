a2e1d485815fdf6de689d1533af714f2ed0a69f6
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-07-09 20:53:48 -0400

    python311Packages.tabcmd: mark broken

--- pkgs/development/python-modules/tabcmd/default.nix
+++ pkgs/development/python-modules/tabcmd/default.nix
@@ -91,6 +91,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "tabcmd" ];
 
   meta = with lib; {
+    broken = true;
     description = "Command line client for working with Tableau Server";
     homepage = "https://github.com/tableau/tabcmd";
     changelog = "https://github.com/tableau/tabcmd/releases/tag/v${version}";
