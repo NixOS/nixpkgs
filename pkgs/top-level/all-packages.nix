ebda20cc6bdc3a90a3d8fa26703209d7da601792
Maximilian Güntner <code@mguentner.de>
2024-07-10 18:43:45 +0200

    ma1sd: remove package
    unmaintained package, see repository
    
    https://github.com/ma1uta/ma1sd

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9662,8 +9662,6 @@ with pkgs;
 
   kzipmix = pkgsi686Linux.callPackage ../tools/compression/kzipmix { };
 
-  ma1sd = callPackage ../servers/ma1sd { };
-
   mailcatcher = callPackage ../development/web/mailcatcher { };
 
   makebootfat = callPackage ../tools/misc/makebootfat { };
