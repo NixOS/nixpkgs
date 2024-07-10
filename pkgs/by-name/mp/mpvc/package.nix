a5875f7a7bc6e924b9488308827cc90105323f50
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-10 19:48:46 -0300

    mpvc: rework
    - align input set
    - finalAttrs
    - strictDeps
    - remove nested with

--- pkgs/by-name/mp/mpvc/package.nix
+++ pkgs/by-name/mp/mpvc/package.nix
@@ -1,32 +1,43 @@
-{ lib, stdenv, socat, fetchFromGitHub, makeWrapper }:
+{
+  lib,
+  fetchFromGitHub,
+  makeWrapper,
+  socat,
+  stdenv,
+  testers,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mpvc";
   version = "1.3";
 
   src = fetchFromGitHub {
     owner = "lwilletts";
     repo = "mpvc";
-    rev = version;
-    sha256 = "sha256-wPETEG0BtNBEj3ZyP70byLzIP+NMUKbnjQ+kdvrvK3s=";
+    rev = finalAttrs.version;
+    hash = "sha256-wPETEG0BtNBEj3ZyP70byLzIP+NMUKbnjQ+kdvrvK3s=";
   };
-
-  makeFlags = [ "PREFIX=$(out)" ];
-  installFlags = [ "PREFIX=$(out)" ];
-
-  postInstall = ''
-    wrapProgram $out/bin/mpvc --prefix PATH : "${socat}/bin/"
-  '';
 
   nativeBuildInputs = [ makeWrapper ];
+
   buildInputs = [ socat ];
 
-  meta = with lib; {
-    description = "Mpc-like control interface for mpv";
-    mainProgram = "mpvc";
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  installFlags = [ "PREFIX=$(out)" ];
+
+  strictDeps = true;
+
+  postInstall = ''
+    wrapProgram $out/bin/mpvc --prefix PATH : "${lib.getBin socat}/"
+  '';
+
+  meta = {
     homepage = "https://github.com/lwilletts/mpvc";
-    license = licenses.mit;
-    maintainers = [ maintainers.neeasade ];
-    platforms = platforms.linux;
+    description = "Mpc-like control interface for mpv";
+    license = lib.licenses.mit;
+    mainProgram = "mpvc";
+    maintainers = with lib.maintainers; [ neeasade ];
+    platforms = lib.platforms.linux;
   };
-}
+})
