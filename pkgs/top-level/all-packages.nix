06cbcd0501fbaf28a60dd00d15a66abbcbd5de05
Sam Schweigel <s.schweigel@gmail.com>
2024-07-10 12:54:07 -0700

    uftrace: add optional dependencies
    Build uftrace with all optional dependencies, save for libunwind (for debugging
    uftrace only).  Link with Python and LuaJIT only in the "uftraceFull" version to
    keep the closure size down.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13786,6 +13786,11 @@ with pkgs;
 
   uftrace = callPackage ../development/tools/uftrace { };
 
+  uftraceFull = uftrace.override {
+    withLuaJIT = true;
+    withPython = true;
+  };
+
   uftpd = callPackage ../servers/ftp/uftpd {};
 
   uget = callPackage ../tools/networking/uget { };
