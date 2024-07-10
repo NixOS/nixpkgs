3ce1e066be9d2d273c654dd2f8823230c94fb11a
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-10 20:53:01 -0300

    mpvc: 1.3 -> 1.4-unstable-2024-07-09
    - Using a new repo because it has more updates;
    - Using master tip instead of tagged release because, well, 1.5 does not exist
    and the latest versions are `1.5-<some audio-related word>`.
    - Adding myself as maintainer

--- pkgs/by-name/mp/mpvc/package.nix
+++ pkgs/by-name/mp/mpvc/package.nix
@@ -9,18 +9,20 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mpvc";
-  version = "1.3";
+  version = "1.4-unstable-2024-07-09";
 
   src = fetchFromGitHub {
-    owner = "lwilletts";
+    owner = "gmt4";
     repo = "mpvc";
-    rev = finalAttrs.version;
-    hash = "sha256-wPETEG0BtNBEj3ZyP70byLzIP+NMUKbnjQ+kdvrvK3s=";
+    rev = "966156dacd026cde220951d41c4ac5915cd6ad64";
+    hash = "sha256-/M3xOb0trUaxJGXmV2+sOCbrHGyP4jpyo+S/oBoDkO0=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   buildInputs = [ socat ];
+
+  outputs = [ "out" "doc" ];
 
   makeFlags = [ "PREFIX=$(out)" ];
 
@@ -32,12 +34,18 @@ stdenv.mkDerivation (finalAttrs: {
     wrapProgram $out/bin/mpvc --prefix PATH : "${lib.getBin socat}/"
   '';
 
+  # This is not Archlinux :)
+  postFixup = ''
+    rm -r $out/share/licenses
+    rmdir $out/share || true
+  '';
+
   meta = {
-    homepage = "https://github.com/lwilletts/mpvc";
+    homepage = "https://github.com/gmt4/mpvc";
     description = "Mpc-like control interface for mpv";
     license = lib.licenses.mit;
     mainProgram = "mpvc";
-    maintainers = with lib.maintainers; [ neeasade ];
+    maintainers = with lib.maintainers; [ AndersonTorres neeasade ];
     platforms = lib.platforms.linux;
   };
 })
