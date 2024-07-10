0c2009376bcd994a498fbe09e282769df9bf8dad
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-10 01:32:14 +0000

    spglib: 2.4.0 -> 2.5.0

--- pkgs/development/libraries/spglib/default.nix
+++ pkgs/development/libraries/spglib/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "spglib";
-  version = "2.4.0"; # N.B: if you change this, please update: pythonPackages.spglib
+  version = "2.5.0"; # N.B: if you change this, please update: pythonPackages.spglib
 
   src = fetchFromGitHub {
     owner = "spglib";
     repo = "spglib";
     rev = "v${version}";
-    hash = "sha256-Y6WkN3Q1q4h2TqrlKSKFmFxQZQWKmleRHV74PmYyw2A=";
+    hash = "sha256-nooN4skbhEoUD+YuBtdI7TJq7PIdH9EN5dYAheILp5w=";
   };
 
   nativeBuildInputs = [ cmake gfortran gtest ];
