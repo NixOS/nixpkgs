3caea02c0ab740f995a15ab6161488d7f5f665c6
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-07-10 07:45:53 +0200

    azure-cli-extensions.aks-preview: 5.0.0b1 -> 5.0.0b3
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/tools/admin/azure-cli/extensions-generated.nix
+++ pkgs/tools/admin/azure-cli/extensions-generated.nix
@@ -57,9 +57,9 @@
   };
   aks-preview = mkAzExtension rec {
     pname = "aks-preview";
-    version = "5.0.0b1";
+    version = "5.0.0b3";
     url = "https://azcliprod.blob.core.windows.net/cli-extensions/aks_preview-${version}-py2.py3-none-any.whl";
-    sha256 = "2c0a6b1a952b30f10f36f744e214a03774016fe7834038b917b8bc0ef03ce0c9";
+    sha256 = "ff3f4cb237c87040662306e3b46bfaabde9ea4e7d3c23be55f3a829baf8404d0";
     description = "Provides a preview for upcoming AKS features";
   };
   akshybrid = mkAzExtension rec {
