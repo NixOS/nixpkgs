fbb80bc6b38f75c36e1fe30708c5920469ddf991
Aleksandr Sterkhov <github@so.likeon.dev>
2024-07-11 19:48:56 +0200

    tilt: 0.33.13 -> 0.33.17

--- pkgs/applications/networking/cluster/tilt/default.nix
+++ pkgs/applications/networking/cluster/tilt/default.nix
@@ -5,13 +5,13 @@ let args = rec {
       /* Do not use "dev" as a version. If you do, Tilt will consider itself
         running in development environment and try to serve assets from the
         source tree, which is not there once build completes.  */
-      version = "0.33.13";
+      version = "0.33.17";
 
       src = fetchFromGitHub {
         owner = "tilt-dev";
         repo = "tilt";
         rev = "v${version}";
-        hash = "sha256-B1kau6G56Iz6Yso2KpJCPE18yznhKCmq+Pabi2sxSmI=";
+        hash = "sha256-GzWnTq3X615A/jRjYhBriRYaH4tjv+yg2/zHIJuKXPE=";
       };
     };
 
