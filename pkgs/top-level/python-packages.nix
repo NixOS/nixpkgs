4f5c6cc337b3f186ec221f0c7ceca89ae9602c51
Winston R. Milling <Winston@Milli.ng>
2024-07-11 08:21:55 -0500

    python311Packages.suds: init at 1.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14870,6 +14870,8 @@ self: super: with self; {
 
   sudachipy = callPackage ../development/python-modules/sudachipy { };
 
+  suds = callPackage ../development/python-modules/suds { };
+
   sumo = callPackage ../development/python-modules/sumo { };
 
   sumtypes = callPackage ../development/python-modules/sumtypes { };
