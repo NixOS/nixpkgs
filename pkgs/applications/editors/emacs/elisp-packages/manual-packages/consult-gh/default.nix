6af5cd05bb2e166feb29d1dca8aaf2b0f8b88481
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-10 22:54:14 -0300

    emacs.pkgs.consult-gh: modernize according to new melpaBuild

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/consult-gh/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/consult-gh/default.nix
@@ -1,28 +1,24 @@
-{ lib
-, melpaBuild
-, fetchFromGitHub
-, consult
-, embark
-, forge
-, gh
-, markdown-mode
-, writeText
-, unstableGitUpdater
+{
+  lib,
+  consult,
+  embark,
+  fetchFromGitHub,
+  forge,
+  gh,
+  markdown-mode,
+  melpaBuild,
+  unstableGitUpdater,
+  writeText,
 }:
 
-let
-  commit = "3a07139a1f7e38b959ce177a122c8f47c401d7fa";
-in
 melpaBuild {
   pname = "consult-gh";
   version = "0.12-unstable-2024-04-23";
 
-  inherit commit;
-
   src = fetchFromGitHub {
     owner = "armindarvish";
     repo = "consult-gh";
-    rev = commit;
+    rev = "3a07139a1f7e38b959ce177a122c8f47c401d7fa";
     hash = "sha256-BZloG5RuQzC2RwCfvqPPhGcbsCabQWBnRHdU62rwNdo=";
   };
 
@@ -30,16 +26,10 @@ melpaBuild {
     consult
     embark
     forge
-    gh
     markdown-mode
   ];
 
-  recipe = writeText "recipe" ''
-    (consult-gh
-      :repo "armindarvish/consult-gh"
-      :fetcher github
-      :files ("consult-gh-embark.el" "consult-gh-forge.el" "consult-gh.el"))
-  '';
+  propagatedUserEnvPkgs = [ gh ];
 
   passthru.updateScript = unstableGitUpdater { };
 
