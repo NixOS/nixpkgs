206f09a25658ec271c32b6a845c698a1d3f68f5d
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2024-07-11 11:34:30 +0200

    python12Packages.osprofiler: init at 4.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9609,6 +9609,8 @@ self: super: with self; {
 
   ospd = callPackage ../development/python-modules/ospd { };
 
+  osprofiler = callPackage ../development/python-modules/osprofiler { };
+
   osqp = callPackage ../development/python-modules/osqp { };
 
   oss2 = callPackage ../development/python-modules/oss2 { };
