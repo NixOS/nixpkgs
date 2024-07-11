687502080f5f5a1cd4c6aa302c15a750848c557b
Emily <vcs@emily.moe>
2024-07-11 17:38:09 +0100

    qodem: add patches
    As it doesn’t look like there’ll be another upstream release,
    add bug fix patches from the upstream bug tracker, plus one I wrote
    just now for miniupnpc 2.2.8 support.

--- pkgs/by-name/qo/qodem/package.nix
+++ pkgs/by-name/qo/qodem/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitLab,
+  fetchpatch2,
   autoreconfHook,
   ncurses,
   SDL,
@@ -19,6 +20,32 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "69cc7458ef23243f790348a4cc503a8173008e55";
     hash = "sha256-Ocb2inuxeDOfqge+h7pHL9I9Kn72Mgi8Eq179/58alk=";
   };
+
+  patches = [
+    # Fix ICH with count>1
+    # https://gitlab.com/AutumnMeowMeow/qodem/-/issues/77
+    (fetchpatch2 {
+      url = "https://gitlab.com/-/project/6684464/uploads/c2ceaef82d483c13ff9ec64424f3c40a/0001-Fix-ICH-with-count-1.patch";
+      hash = "sha256-lCqj4p8onUS4pehQMXS6lbC7JH5dP6sOjDALpasgd2M=";
+    })
+
+    # Don't clear line rendition on partial ED
+    # https://gitlab.com/AutumnMeowMeow/qodem/-/issues/78
+    (fetchpatch2 {
+      url = "https://gitlab.com/-/project/6684464/uploads/462c0b1cf05c3fc2857ce982e62fefcc/0001-Don-t-clear-line-rendition-on-partial-ED.patch";
+      hash = "sha256-lSuxP0tUfGa3BjK3ehpdMi16XaGZrdVvAcM2vnjAme8=";
+    })
+
+    # DECCOLM should clear line rendition attributes
+    # https://gitlab.com/AutumnMeowMeow/qodem/-/issues/78
+    (fetchpatch2 {
+      url = "https://gitlab.com/-/project/6684464/uploads/812bdfdfaee44eed346fcff85f53efbe/0002-DECCOLM-should-clear-line-rendition-attributes.patch";
+      hash = "sha256-XO+h5fpBTLLYC3t4FRCy1uFiMkmSXbre4T2NB/FC3uQ=";
+    })
+
+    # Fix build with miniupnpc 2.2.8
+    ./qodem-fix-miniupnpc-2.2.8.patch
+  ];
 
   nativeBuildInputs = [
     autoreconfHook
