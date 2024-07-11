82816bac618021fcad28b954f21184de84bc9ea1
Gavin John <>
2024-07-11 16:59:00 -0400

    immersed-vr: Add pandapip1 as maintainer

--- pkgs/by-name/im/immersed-vr/package.nix
+++ pkgs/by-name/im/immersed-vr/package.nix
@@ -26,7 +26,7 @@ let
     description = "VR coworking platform";
     homepage = "https://immersed.com";
     license = licenses.unfree;
-    maintainers = with maintainers; [ haruki7049 ];
+    maintainers = with maintainers; [ haruki7049 pandapip1 ];
     platforms = builtins.attrNames sources;
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
   };
