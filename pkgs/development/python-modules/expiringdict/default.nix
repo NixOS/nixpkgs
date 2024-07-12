34d1affd6e1bbc74715e5b064268d3b336cbed9e
Andrew Kvalheim <andrew@kvalhe.im>
2024-07-12 14:42:25 -0700

    python3Packages.expiringdict: patch for Python 3.12
    Applies mailgun/expiringdict#56 for compatibility with Python 3.12

--- pkgs/development/python-modules/expiringdict/default.nix
+++ pkgs/development/python-modules/expiringdict/default.nix
@@ -28,6 +28,12 @@ buildPythonPackage rec {
       url = "https://github.com/mailgun/expiringdict/commit/1c0f82232d20f8b3b31c9269a4d0e9510c1721a6.patch";
       hash = "sha256-IeeJVb2tOwRhEPNGqM30fNZyz3jFcnZNWC3I6K1+hSY=";
     })
+
+    # apply mailgun/expiringdict#56 for compatibility with Python 3.12
+    (fetchpatch2 {
+      url = "https://github.com/mailgun/expiringdict/commit/976faf3664d54049e443aca054f5819db834577b.patch";
+      hash = "sha256-FNdnU6iUMyED5j8oAjhmJTR7zQeEc/Z5s64pdeT4F8w=";
+    })
   ];
 
   nativeCheckInputs = [
