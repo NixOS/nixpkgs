5c1ee085cd750f9fb125420f38098b6532d057ad
Philipp David <pd@3b.pm>
2024-07-12 08:08:06 +0200

    python312Packages.pyecotrend-ista: init at 3.3.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11135,6 +11135,8 @@ self: super: with self; {
 
   pyeconet = callPackage ../development/python-modules/pyeconet { };
 
+  pyecotrend-ista = callPackage ../development/python-modules/pyecotrend-ista { };
+
   pyecowitt = callPackage ../development/python-modules/pyecowitt { };
 
   pyedimax = callPackage ../development/python-modules/pyedimax { };
