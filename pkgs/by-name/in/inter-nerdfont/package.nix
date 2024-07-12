45d14085614f80a08ab594733e629c15754b8543
midirhee12 <midirhee@proton.me>
2024-07-11 19:08:57 -0500

    inter-nerdfont: init at 4.0

---
+++ pkgs/by-name/in/inter-nerdfont/package.nix
@@ -0,0 +1,39 @@
+{ lib, fontforge, nerd-font-patcher, stdenvNoCC, fetchzip }:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "inter-nerdfont";
+  version = "4.0";
+
+  src = fetchzip {
+    url = "https://github.com/rsms/inter/releases/download/v${finalAttrs.version}/Inter-${finalAttrs.version}.zip";
+    stripRoot = false;
+    hash = "sha256-hFK7xFJt69n+98+juWgMvt+zeB9nDkc8nsR8vohrFIc=";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    nerd-font-patcher Inter.ttc
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm444 'Inter Nerd Font.ttc' $out/share/fonts/truetype/InterNerdFont.ttc
+    cp *.ttf $out/share/fonts/truetype
+
+    runHook postInstall
+  '';
+
+  buildInputs = [ fontforge nerd-font-patcher ];
+
+  meta = {
+    homepage = "https://gitlab.com/mid_os/inter-nerdfont";
+    description = "NerdFont patch of the Inter font";
+    license = lib.licenses.ofl;
+    platforms = lib.platforms.all;
+    maintainers = [ lib.maintainers.cvoges12 ];
+  };
+})
