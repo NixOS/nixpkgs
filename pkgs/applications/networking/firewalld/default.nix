9bb024aa1f864f77112299d7ac170a797d15d480
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-12 04:27:23 +0000

    firewalld-gui: 2.1.2 -> 2.2.0

--- pkgs/applications/networking/firewalld/default.nix
+++ pkgs/applications/networking/firewalld/default.nix
@@ -31,13 +31,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "firewalld";
-  version = "2.1.2";
+  version = "2.2.0";
 
   src = fetchFromGitHub {
     owner = "firewalld";
     repo = "firewalld";
     rev = "v${version}";
-    sha256 = "sha256-bCLMTWtgEay3fLKOFWUoWpOL1iGDHRR5hu+v06bLxLs=";
+    sha256 = "sha256-MaBYJLNVlDEsMlnyNtf8G8D1Tnvcfv0/bXQ/8f7kBao=";
   };
 
   patches = [
