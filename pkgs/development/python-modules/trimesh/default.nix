1294d2d69fb5acd90af71be5c489986b22b38b4c
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-07-12 22:45:15 +0200

    python312Packages.trimesh: update lower python version bound

--- pkgs/development/python-modules/trimesh/default.nix
+++ pkgs/development/python-modules/trimesh/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   version = "4.4.3";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
