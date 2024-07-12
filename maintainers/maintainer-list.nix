2a27662845330f07c481baf066053b6b53f5afd8
Joel Holdsworth <joel@airwebreathe.org.uk>
2024-07-12 16:26:49 -0700

    maintainers: add jhol

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10253,6 +10253,13 @@
     githubId = 2502736;
     name = "James Hillyerd";
   };
+  jhol = {
+    name = "Joel Holdsworth";
+    email = "joel@airwebreathe.org.uk";
+    github = "jhol";
+    githubId = 1449493;
+    keys = [ { fingerprint = "08F7 2546 95DE EAEF 03DE  B0E4 D874 562D DC99 D889"; } ];
+  };
   jhollowe = {
     email = "jhollowe@johnhollowell.com";
     github = "jhollowe";
