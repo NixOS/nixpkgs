0de21e247826a89628352788b4f77b5b75edf423
Azat Bahawi <azat@bahawi.net>
2024-07-13 23:23:29 +0300

    werf: 2.6.7 -> 2.7.1

--- pkgs/by-name/we/werf/package.nix
+++ pkgs/by-name/we/werf/package.nix
@@ -11,13 +11,13 @@
 
 buildGoModule rec {
   pname = "werf";
-  version = "2.6.7";
+  version = "2.7.1";
 
   src = fetchFromGitHub {
     owner = "werf";
     repo = "werf";
     rev = "v${version}";
-    hash = "sha256-nR/fEUoRWOeZih54emRAOLywhvymSp562tHLQAH2mc0=";
+    hash = "sha256-krMisFNo74ZVHTe/0vkxWS8HxqgAa8pWPQdkYOLBlgw=";
   };
 
   vendorHash = "sha256-Ta2owh1Z7mB4e85ZHK1maL5GoBMzM0mf3wsU6ho+4Z0=";
