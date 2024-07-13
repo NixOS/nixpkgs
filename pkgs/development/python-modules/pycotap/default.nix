5ec12531cb3c3eec460fae067f08f00203294941
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-13 07:40:54 -0300

    pycotap: fixup
    - convert to new pyproject system
    - get rid of rec
    - get rid of nested with
    - meta
      - longDescription
      - description
    - add AndersonTorres as maintainer

--- pkgs/development/python-modules/pycotap/default.nix
+++ pkgs/development/python-modules/pycotap/default.nix
@@ -2,22 +2,57 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
-buildPythonPackage rec {
-  pname = "pycotap";
-  version = "1.3.1";
-  format = "setuptools";
+let
+  self = buildPythonPackage {
+    pname = "pycotap";
+    version = "1.3.1";
+    pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-Z0NV8BMAvgPff4cXhOSYZSwtiawZzXfujmFlJjSi+Do=";
-  };
+    src = fetchPypi {
+      inherit (self) pname version;
+      hash = "sha256-Z0NV8BMAvgPff4cXhOSYZSwtiawZzXfujmFlJjSi+Do=";
+    };
 
-  meta = with lib; {
-    description = "Test runner for unittest that outputs TAP results to stdout";
-    homepage = "https://el-tramo.be/pycotap";
-    license = licenses.mit;
-    maintainers = with maintainers; [ mwolfe ];
+    build-system = [ setuptools ];
+
+    postFixup = ''
+      rm $out/COPYING
+    '';
+
+    meta = {
+      homepage = "https://github.com/remko/pycotap";
+      description = "Tiny Python TAP Test Runner";
+      longDescription = ''
+        pycotap is a simple Python test runner for unittest that outputs Test
+        Anything Protocol results to standard output (similar to what tape does
+        for JavaScript).
+
+        Contrary to other TAP runners for Python, pycotap
+
+        - prints TAP (and only TAP) to standard output instead of to a separate
+          file, allowing you to pipe it directly to TAP pretty printers and
+          processors (such as the ones listed on the tape page). By piping it to
+          other consumers, you can avoid the need to add specific test runners
+          to your test code. Since the TAP results are printed as they come in,
+          the consumers can directly display results while the tests are run.
+
+        - only contains a TAP reporter, so no parsers, no frameworks, no
+          dependencies ...
+        - is configurable: you can choose how you want the test output and test
+          result diagnostics to end up in your TAP output (as TAP diagnostics,
+          YAML blocks, or attachments). The defaults are optimized for a Jenkins
+          based flow.
+
+        > Nice work with pycotap! I took a "kitchen sink" approach with tappy so
+        > I'm glad someone made a no dependency TAP unittest runner. :) -- Matt
+        > Layman, author of tappy
+      '';
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ mwolfe AndersonTorres ];
+    };
   };
-}
+in
+self
