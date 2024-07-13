45cd7d5ab6cc6b4c5a678c4297f62d43dba9a6d9
K900 <me@0upti.me>
2024-07-13 11:10:07 +0300

    kdePackages: Frameworks 6.3.0 -> 6.4.0

--- pkgs/kde/frameworks/kio/default.nix
+++ pkgs/kde/frameworks/kio/default.nix
@@ -11,9 +11,6 @@ mkKdeDerivation {
   patches = [
     # Remove hardcoded smbd search path
     ./0001-Remove-impure-smbd-search-path.patch
-    # When running a process through systemd, resolve the full path ourselves
-    ./early-resolve-executables.diff
-    # FIXME(later): discuss with upstream?
   ];
 
   extraBuildInputs = [qt5compat qttools acl attr];
