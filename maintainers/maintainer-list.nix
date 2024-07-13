25a0df8f0aa3a4d0b752faaa33a6b7d31f32e1f8
d-brasher <175485311+d-brasher@users.noreply.github.com>
2024-07-13 14:41:10 +0200

    maintainers: add d-brasher

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4686,6 +4686,11 @@
     githubId = 3179832;
     name = "D. Bohdan";
   };
+  d-brasher = {
+    github = "d-brasher";
+    githubId = 175485311;
+    name = "D. Brasher";
+  };
   dbrgn = {
     email = "nix@dbrgn.ch";
     github = "dbrgn";
