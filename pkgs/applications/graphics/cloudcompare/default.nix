3c201d1aebd1cfa0dfa14c6ff14de0f5aee72bc5
Ivan Mincik <ivan.mincik@gmail.com>
2024-07-13 13:57:30 +0200

    cloudcompare: fix source hash

--- pkgs/applications/graphics/cloudcompare/default.nix
+++ pkgs/applications/graphics/cloudcompare/default.nix
@@ -31,7 +31,7 @@ mkDerivation rec {
     owner = "CloudCompare";
     repo = "CloudCompare";
     rev = "v${version}";
-    hash = "sha256-KVbFGim2Nqhs5PAQiBNTmJStmnEINTeopiqqEBBniHc=";
+    hash = "sha256-a/0lf3Mt5ZpLFRM8jAoqZer8pY1ROgPRY4dPt34Bk3E=";
     fetchSubmodules = true;
   };
 
