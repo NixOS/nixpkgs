0e0ffd4b728540c5d0ddd0cb949b49ffac2f07a8
Stefan Frijters <sfrijters@gmail.com>
2024-07-14 13:53:53 +0200

    obconf: use finalAttrs, hash instead of sha256

--- pkgs/tools/X11/obconf/default.nix
+++ pkgs/tools/X11/obconf/default.nix
@@ -12,14 +12,14 @@
 , openbox
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "obconf";
   version = "unstable-2015-02-13";
 
   src = fetchgit {
     url = "git://git.openbox.org/dana/obconf";
     rev = "63ec47c5e295ad4f09d1df6d92afb7e10c3fec39";
-    sha256 = "sha256-qwm66VA/ueRMFtSUcrmuObNkz+KYgWRnmR7TnQwpxiE=";
+    hash = "sha256-qwm66VA/ueRMFtSUcrmuObNkz+KYgWRnmR7TnQwpxiE=";
   };
 
   nativeBuildInputs = [
@@ -38,7 +38,7 @@ stdenv.mkDerivation rec {
   ];
 
   postPatch = ''
-    substituteInPlace configure.ac --replace 2.0.4 ${version}
+    substituteInPlace configure.ac --replace 2.0.4 ${finalAttrs.version}
   '';
 
   meta = {
@@ -50,4 +50,4 @@ stdenv.mkDerivation rec {
     platforms = lib.platforms.linux;
     mainProgram = "obconf";
   };
-}
+})
