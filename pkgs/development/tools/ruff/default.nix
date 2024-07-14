4b72d38641bb75ee020da3e1222b6c592ecea891
Gaetan Lepage <gaetan@glepage.com>
2024-07-14 17:13:25 +0200

    ruff: 0.5.1 -> 0.5.2
    Diff: https://github.com/astral-sh/ruff/compare/refs/tags/0.5.1...0.5.2
    
    Changelog: https://github.com/astral-sh/ruff/releases/tag/0.5.2

--- pkgs/development/tools/ruff/default.nix
+++ pkgs/development/tools/ruff/default.nix
@@ -14,13 +14,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ruff";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchFromGitHub {
     owner = "astral-sh";
     repo = "ruff";
     rev = "refs/tags/${version}";
-    hash = "sha256-2tW/p9A7jpQg8ZmSF7KRuN6kBNKK1cfjnS9KlvnCpQA=";
+    hash = "sha256-g71RqbEoCpmCjd0CKkc++yv00ohoORDeMYAwYEHKhW4=";
   };
 
   cargoLock = {
@@ -31,17 +31,6 @@ rustPlatform.buildRustPackage rec {
     };
   };
 
-  # Fix compatibility with cargo-auditable
-  # https://github.com/astral-sh/ruff/pull/12275
-  # TODO: this will be included in the next release
-  patches = [
-    (fetchpatch {
-      name = "fix-compatibility-with-cargo-auditable";
-      url = "https://github.com/astral-sh/ruff/commit/d0298dc26d471666acc01dacdb603e3e95aca06f.patch";
-      hash = "sha256-Shf1Gw1pY98ZE+h9OhlpkJwq/S52EAJqUUk/uHix2fg=";
-    })
-  ];
-
   nativeBuildInputs = [
     installShellFiles
   ];
@@ -50,31 +39,6 @@ rustPlatform.buildRustPackage rec {
     rust-jemalloc-sys
   ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreServices
-  ];
-
-  # tests expect no colors
-  preCheck = ''
-    export NO_COLOR=1
-  '';
-
-  # Failing for an unclear reason.
-  # According to the maintainers, those tests are from an experimental crate that isn't actually
-  # used by ruff currently and can thus be safely skipped.
-  checkFlags = [
-    "--skip=semantic::tests::expression_scope"
-    "--skip=semantic::tests::reachability_trivial"
-    "--skip=semantic::types::infer::tests::follow_import_to_class"
-    "--skip=semantic::types::infer::tests::if_elif"
-    "--skip=semantic::types::infer::tests::if_elif_else"
-    "--skip=semantic::types::infer::tests::ifexpr_walrus"
-    "--skip=semantic::types::infer::tests::ifexpr_walrus_2"
-    "--skip=semantic::types::infer::tests::join_paths"
-    "--skip=semantic::types::infer::tests::literal_int_arithmetic"
-    "--skip=semantic::types::infer::tests::maybe_unbound"
-    "--skip=semantic::types::infer::tests::narrow_none"
-    "--skip=semantic::types::infer::tests::resolve_base_class_by_name"
-    "--skip=semantic::types::infer::tests::resolve_module_member"
-    "--skip=semantic::types::infer::tests::resolve_visible_def"
   ];
 
   postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
