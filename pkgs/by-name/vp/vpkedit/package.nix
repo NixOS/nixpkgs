729d5b08fd3ca3065ecf76e93a2a776bfe0562b5
HurricanePootis <53066639+HurricanePootis@users.noreply.github.com.>
2024-07-13 22:48:04 -0500

    vpkedit: format with nixfmt

--- pkgs/by-name/vp/vpkedit/package.nix
+++ pkgs/by-name/vp/vpkedit/package.nix
@@ -1,15 +1,5 @@
-{
-  lib,
-  stdenv,
-  cmake,
-  fetchFromGitHub,
-  makeWrapper,
-  qt6,
-  cryptopp,
-  minizip-ng,
-  pkg-config,
-  rapidjson,
-}:
+{ lib, stdenv, cmake, fetchFromGitHub, makeWrapper, qt6, cryptopp, minizip-ng
+, pkg-config, rapidjson, }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "vpkedit";
   version = "4.2.2";
@@ -25,38 +15,23 @@ stdenv.mkDerivation (finalAttrs: {
   cmakeFlags = [ "-DVPKEDIT_BUILD_LIBC=OFF" ];
   patches = [ ./pkg-config.diff ./install.diff ./desktop.diff ];
 
-  buildInputs =
-    with qt6;
-    [
-      qtbase
-      qttools
-    ]
-    ++ [
-      cryptopp
-      minizip-ng
-      pkg-config
-      rapidjson
-    ];
+  buildInputs = with qt6;
+    [ qtbase qttools ] ++ [ cryptopp minizip-ng pkg-config rapidjson ];
 
-  nativeBuildInputs = [
-    makeWrapper
-    qt6.wrapQtAppsHook
-    cmake
-  ];
+  nativeBuildInputs = [ makeWrapper qt6.wrapQtAppsHook cmake ];
 
   postInstall = ''
-  mkdir "$out/bin"
-  ln -s "$out/lib/vpkedit/vpkedit" "$out/bin/vpkedit"
-  ln -s "$out/lib/vpkedit/vpkeditcli" "$out/bin/vpkeditcli"
+    mkdir "$out/bin"
+    ln -s "$out/lib/vpkedit/vpkedit" "$out/bin/vpkedit"
+    ln -s "$out/lib/vpkedit/vpkeditcli" "$out/bin/vpkeditcli"
   '';
 
   meta = {
-    description = "A CLI/GUI tool to create, read, and write several pack file formats.";
+    description =
+      "A CLI/GUI tool to create, read, and write several pack file formats.";
     license = lib.licenses.mit;
     homepage = "https://github.com/craftablescience/VPKEdit/";
     mainProgram = "vpkedit";
-    maintainers = with lib.maintainers; [
-      hurricanepootis
-    ];
+    maintainers = with lib.maintainers; [ hurricanepootis ];
   };
 })
