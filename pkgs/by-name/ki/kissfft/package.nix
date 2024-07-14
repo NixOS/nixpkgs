1f81efc8a0cc20b131572e5e0e2f5cae3f993c55
Emily <vcs@emily.moe>
2024-07-15 00:35:15 +0100

    kissfft: build with CMake
    Fix the library install names on macOS and replace a patch with a
    smaller one.

--- pkgs/by-name/ki/kissfft/package.nix
+++ pkgs/by-name/ki/kissfft/package.nix
@@ -2,87 +2,85 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  cmake,
+  ninja,
+  pkg-config,
   fftw,
   fftwFloat,
   python3,
   datatype ? "double",
-  withTools ? false,
   libpng,
   enableStatic ? stdenv.hostPlatform.isStatic,
   enableOpenmp ? false,
   llvmPackages,
 }:
-let
-  py = python3.withPackages (ps: with ps; [ numpy ]);
-  option = cond: if cond then "1" else "0";
-in
-stdenv.mkDerivation rec {
+
+stdenv.mkDerivation (finalAttrs: {
   pname = "kissfft-${datatype}${lib.optionalString enableOpenmp "-openmp"}";
   version = "131.1.0";
+
+  outputs = [
+    "bin"
+    "dev"
+    "out"
+  ];
 
   src = fetchFromGitHub {
     owner = "mborgerding";
     repo = "kissfft";
-    rev = version;
-    sha256 = "1yfws5bn4kh62yk6hdyp9h9775l6iz7wsfisbn58jap6b56s8j5s";
+    rev = finalAttrs.version;
+    hash = "sha256-ukikTVnmKomKXTo6zc+PhpZzEkzXN2imFwZOYlfR3Pk=";
   };
 
-  patches = [ ./0001-pkgconfig-darwin.patch ];
+  patches = [
+    # Fix FFTW dependency check
+    # https://github.com/mborgerding/kissfft/pull/95
+    ./fix-fftw-dependency-check.patch
+  ];
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    pkg-config
+  ];
+
+  buildInputs =
+    lib.optionals (datatype != "simd") [ libpng ]
+    # TODO: This may mismatch the LLVM version in the stdenv, see #79818.
+    ++ lib.optional (enableOpenmp && stdenv.cc.isClang) llvmPackages.openmp;
+
+  nativeCheckInputs = [ (python3.withPackages (ps: [ ps.numpy ])) ];
+
+  checkInputs = [ (if datatype == "float" then fftwFloat else fftw) ];
+
+  cmakeFlags = [
+    (lib.cmakeFeature "KISSFFT_DATATYPE" datatype)
+    (lib.cmakeBool "KISSFFT_STATIC" enableStatic)
+    # `test/testkiss.py` expects this…
+    (lib.cmakeFeature "KISSFFT_OPENMP" (if enableOpenmp then "ON" else "OFF"))
+  ];
+
+  # Required for `test/testcpp.c`.
+  env = {
+    NIX_CFLAGS_COMPILE = lib.optionalString stdenv.isDarwin "-D__MATH_LONG_DOUBLE_CONSTANTS=1";
+  };
+
+  doCheck = true;
 
   # https://bugs.llvm.org/show_bug.cgi?id=45034
   postPatch =
     lib.optionalString
       (stdenv.hostPlatform.isLinux && stdenv.cc.isClang && lib.versionOlder stdenv.cc.version "10")
       ''
-        substituteInPlace test/Makefile \
+        substituteInPlace CMakeLists.txt \
           --replace "-ffast-math" ""
-      ''
-    + lib.optionalString (stdenv.hostPlatform.isDarwin) ''
-      substituteInPlace test/Makefile \
-        --replace "LD_LIBRARY_PATH" "DYLD_LIBRARY_PATH"
-      # Don't know how to make math.h's double long constants available
-      substituteInPlace test/testcpp.cc \
-        --replace "M_PIl" "M_PI"
-    '';
+      '';
 
-  makeFlags = [
-    "PREFIX=${placeholder "out"}"
-    "KISSFFT_DATATYPE=${datatype}"
-    "KISSFFT_TOOLS=${option withTools}"
-    "KISSFFT_STATIC=${option enableStatic}"
-    "KISSFFT_OPENMP=${option enableOpenmp}"
-  ];
-
-  buildInputs =
-    lib.optionals (withTools && datatype != "simd") [ libpng ]
-    # TODO: This may mismatch the LLVM version in the stdenv, see #79818.
-    ++ lib.optional (enableOpenmp && stdenv.cc.isClang) llvmPackages.openmp;
-
-  doCheck = true;
-
-  nativeCheckInputs = [
-    py
-    (if datatype == "float" then fftwFloat else fftw)
-  ];
-
-  checkFlags = [ "testsingle" ];
-
-  postInstall = ''
-    ln -s ${pname}.pc $out/lib/pkgconfig/kissfft.pc
-  '';
-
-  # Tools can't find kissfft libs on Darwin
-  postFixup = lib.optionalString (withTools && stdenv.hostPlatform.isDarwin) ''
-    for bin in $out/bin/*; do
-      install_name_tool -change lib${pname}.dylib $out/lib/lib${pname}.dylib $bin
-    done
-  '';
-
-  meta = with lib; {
+  meta = {
     description = "Mixed-radix Fast Fourier Transform based up on the KISS principle";
     homepage = "https://github.com/mborgerding/kissfft";
-    license = licenses.bsd3;
+    license = lib.licenses.bsd3;
     maintainers = [ ];
-    platforms = platforms.all;
+    platforms = lib.platforms.all;
   };
-}
+})
