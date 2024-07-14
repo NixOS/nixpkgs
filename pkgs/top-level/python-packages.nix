75e249cecbe250d1ef3e922ef8342e6c3d7c4c89
natsukium <tomoya.otabi@gmail.com>
2024-07-14 15:43:33 +0900

    python3Packages.twitter-common-lang: remove
    twitter-common-lang has been removed because it is abandoned and
    unmaintained
    
    https://github.com/twitter-archive/commons

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15740,8 +15740,6 @@ self: super: with self; {
 
   twitter = callPackage ../development/python-modules/twitter { };
 
-  twitter-common-lang = callPackage ../development/python-modules/twitter-common-lang { };
-
   twitter-common-log = callPackage ../development/python-modules/twitter-common-log { };
 
   twitter-common-options = callPackage ../development/python-modules/twitter-common-options { };
