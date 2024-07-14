bfde109276a94f5aef32abcc2276f586de05cc70
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2024-07-14 21:20:40 +0200

    _1password-gui: remove lib-binding for meta, mark as broken for darwin

--- pkgs/applications/misc/1password-gui/default.nix
+++ pkgs/applications/misc/1password-gui/default.nix
@@ -7,7 +7,6 @@
 , polkitPolicyOwners ? [ ] }:
 
 let
-
   pname = "1password";
   version = if channel == "stable" then "8.10.36" else "8.10.38-13.BETA";
 
@@ -54,12 +53,14 @@ let
     inherit (sources.${channel}.${stdenv.system} or (throw "unsupported system ${stdenv.hostPlatform.system}")) url hash;
   };
 
-  meta = with lib; {
+  meta = {
+    # Requires to be installed in "/Application" which is not possible for now (https://github.com/NixOS/nixpkgs/issues/254944)
+    broken = stdenv.isDarwin;
     description = "Multi-platform password manager";
     homepage = "https://1password.com/";
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-    license = licenses.unfree;
-    maintainers = with maintainers; [ timstott savannidgerinel sebtm ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ timstott savannidgerinel sebtm ];
     platforms = builtins.attrNames sources.${channel};
     mainProgram = "1password";
   };
