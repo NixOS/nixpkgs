b3b493b7e8dc7ab57391dc4d37b93ac1cb9c4d79
Jörg Thalheim <joerg@thalheim.io>
2024-07-15 09:07:30 +0200

    tabula: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34264,8 +34264,6 @@ with pkgs;
 
   taskjuggler = callPackage ../applications/misc/taskjuggler { };
 
-  tabula = callPackage ../applications/misc/tabula { };
-
   tabula-java = callPackage ../applications/misc/tabula-java { };
 
   tasknc = callPackage ../applications/misc/tasknc { };
