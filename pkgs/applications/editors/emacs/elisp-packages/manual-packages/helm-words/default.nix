676868e8122ded94acdd19c450edc1ba521e9d2e
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-15 13:41:22 -0300

    emacsPackages.helm-words: use melpaBuild

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/helm-words/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/helm-words/default.nix
@@ -1,28 +1,30 @@
-{ lib
-, trivialBuild
-, fetchFromGitHub
-, dictionary
-, emacs
-, helm
+{
+  lib,
+  dictionary,
+  fetchFromGitHub,
+  helm,
+  melpaBuild,
 }:
 
-trivialBuild rec {
+melpaBuild {
   pname = "helm-words";
   version = "0-unstable-2019-03-12";
 
   src = fetchFromGitHub {
     owner = "emacsmirror";
-    repo = pname;
+    repo = "helm-words";
     rev = "e6387ece1940a06695b9d910de3d90252efb8d29";
     hash = "sha256-rh8YKDLZZCUE6JnnRnFyDDyUjK+35+M2dkawR/+qwNM=";
   };
 
-  packageRequires = [ helm dictionary ];
+  packageRequires = [
+    dictionary
+    helm
+  ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/emacsmirror/helm-words";
     description = "Helm extension for looking up words in dictionaries and thesauri";
-    license = licenses.gpl3Plus;
-    inherit (emacs.meta) platforms;
+    license = lib.licenses.gpl3Plus;
   };
 }
