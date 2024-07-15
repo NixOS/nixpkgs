153e3febf63643bf873badc5362b468fd8dae51b
Doron Behar <doron.behar@gmail.com>
2024-07-15 11:48:18 +0300

    python312Packages.guidata: init at 3.5.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5321,6 +5321,8 @@ self: super: with self; {
 
   guidance = callPackage ../development/python-modules/guidance { };
 
+  guidata = callPackage ../development/python-modules/guidata { };
+
   gumath = callPackage ../development/python-modules/gumath { };
 
   gunicorn = callPackage ../development/python-modules/gunicorn { };
