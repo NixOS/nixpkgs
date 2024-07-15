2bad3487c6c3fc69d8aeb308cc22d8d8fc6ddafd
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-07-14 20:07:44 -0400

    flameshot: 12.1.0 -> 12.1.0-unstable-2024-07-02

--- pkgs/by-name/fl/flameshot/package.nix
+++ pkgs/by-name/fl/flameshot/package.nix
@@ -7,23 +7,24 @@
 , fetchpatch
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "flameshot";
-  version = "12.1.0";
+  version = "12.1.0-unstable-2024-07-02";
 
   src = fetchFromGitHub {
     owner = "flameshot-org";
     repo = "flameshot";
-    rev = "v12.1.0";
-    sha256 = "sha256-omyMN8d+g1uYsEw41KmpJCwOmVWLokEfbW19vIvG79w=";
+    rev = "ccb5a27b136a633911b3b1006185530d9beeea5d";
+    hash = "sha256-JIXsdVUR/4183aJ0gvNGYPTyCzX7tCrk8vRtR8bcdhE=";
   };
 
   patches = [
     # https://github.com/flameshot-org/flameshot/pull/3166
+    # fixes fractional scaling calculations on wayland
     (fetchpatch {
       name = "10-fix-wayland.patch";
       url = "https://github.com/flameshot-org/flameshot/commit/5fea9144501f7024344d6f29c480b000b2dcd5a6.patch";
-      sha256 = "sha256-SnjVbFMDKD070vR4vGYrwLw6scZAFaQA4b+MbI+0W9E=";
+      hash = "sha256-SnjVbFMDKD070vR4vGYrwLw6scZAFaQA4b+MbI+0W9E=";
     })
   ];
 
