NixOS/nixpkgs

commit 3bf2356a5343019347b8639300870dcd5fca38a4

xterm: 390 -> 392 (#311472)
Changes:
- https://invisible-island.net/xterm/xterm.log.html#xterm_391
- https://invisible-island.net/xterm/xterm.log.html#xterm_392

pkgs/applications/terminal-emulators/xterm/default.nix
@@ -4,14 +4,14 @@
 
 stdenv.mkDerivation rec {
   pname = "xterm";
-  version = "390";
+  version = "392";
 
   src = fetchurl {
     urls = [
       "ftp://ftp.invisible-island.net/xterm/${pname}-${version}.tgz"
       "https://invisible-mirror.net/archives/xterm/${pname}-${version}.tgz"
     ];
-    hash = "sha256-dRF8PMUXSgnEJe8QbmlATXL17wXgOl2gCq8VeS1vnA8=";
+    hash = "sha256-TVc3LvCOr6n7doLbjQe+D+BRPljoR4wuyOm2JIbn/l4=";
   };
 
   strictDeps = true;

commit 9cd5101c6a4efcb0fc6f0db8be1349353082bc34

xterm.tests.musl: init
This would have caught the build regression introduced by
3bf2356a5343 ("xterm: 390 -> 392 (#311472)"), and fixed by
5b2bbd8be584 ("Revert "xterm: patch from alpine to fix pty on musl"").

pkgs/applications/terminal-emulators/xterm/default.nix
@@ -1,5 +1,5 @@
 { lib, stdenv, fetchurl, xorg, ncurses, freetype, fontconfig
-, pkg-config, makeWrapper, nixosTests, gitUpdater
+, pkg-config, makeWrapper, nixosTests, pkgsCross, gitUpdater
 , nix, gnused, coreutils, enableDecLocator ? true }:
 
 stdenv.mkDerivation rec {
@@ -78,6 +78,7 @@ stdenv.mkDerivation rec {
     tests = {
       customTest = nixosTests.xterm;
       standardTest = nixosTests.terminal-emulators.xterm;
+      musl = pkgsCross.musl64.xterm;
     };
 
     updateScript = gitUpdater {