c277040c24f3d465e5354bf26b2458966b495035
Ivan Mincik <ivan.mincik@gmail.com>
2024-07-15 11:47:07 +0200

    gdal: re-enable some tests

--- pkgs/development/libraries/gdal/default.nix
+++ pkgs/development/libraries/gdal/default.nix
@@ -250,12 +250,6 @@ stdenv.mkDerivation (finalAttrs: {
     "test_sentinel2_zipped"
     # tries to call unwrapped executable
     "test_SetPROJAuxDbPaths"
-    # fixed and renamed in 3.8.0RC1
-    # https://github.com/OSGeo/gdal/commit/c8b471ca1e6318866ff668d2b57bb6f076e3ae29
-    "test_visoss_6"
-    # failing with PROJ 9.3.1
-    # https://github.com/OSGeo/gdal/issues/8908
-    "test_osr_esri_28"
   ] ++ lib.optionals (!stdenv.isx86_64) [
     # likely precision-related expecting x87 behaviour
     "test_jp2openjpeg_22"
