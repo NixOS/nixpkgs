NixOS/nixpkgs

commit 04952ac5c3b810ab89d4e332b4bc62997b161da0

mailmanPackages.python3: fix markdown support
Fixes the issue reported at
https://gitlab.com/mailman/mailman/-/issues/1137. The readme-renderer
package needs cmarkgfm for markdown support. Mailman's postorious needs
this to render the info field of mailing lists.

pkgs/servers/mail/mailman/python.nix
@@ -28,6 +28,10 @@ python3.override {
           hash = "sha256-WF3FFrnrBCphnvCjnD19Vf6BvbTfCaUsnN3g0Hvxqn0=";
         };
       });
+
+      readme-renderer = super.readme-renderer.overridePythonAttrs (_: {
+        propagatedBuildInputs = [ self.cmarkgfm ];
+      });
     })
 
     overlay;

commit fd6b57e93b2ff6d7c96de18c81bfe5050761a064

mailmanPackages.postorius: use optional-dependencies
We have a mechanism for this — we don't need an overlay hack.

Fixes: 04952ac5c3b8 ("mailmanPackages.python3: fix markdown support")

pkgs/servers/mail/mailman/postorius.nix
@@ -11,7 +11,9 @@ buildPythonPackage rec {
     hash = "sha256-GmbIqO+03LgbUxJ1nTStXrYN3t2MfvzbeYRAipfTW1o=";
   };
 
-  propagatedBuildInputs = [ django-mailman3 readme-renderer ];
+  propagatedBuildInputs = [
+    django-mailman3 readme-renderer
+  ] ++ readme-renderer.optional-dependencies.md;
   nativeCheckInputs = [ beautifulsoup4 vcrpy mock ];
 
   # Tries to connect to database.

pkgs/servers/mail/mailman/python.nix
@@ -28,10 +28,6 @@ python3.override {
           hash = "sha256-WF3FFrnrBCphnvCjnD19Vf6BvbTfCaUsnN3g0Hvxqn0=";
         };
       });
-
-      readme-renderer = super.readme-renderer.overridePythonAttrs (_: {
-        propagatedBuildInputs = [ self.cmarkgfm ];
-      });
     })
 
     overlay;