312843d0a2e6617cf5cdd697ceffd210c59c98a3
Michael Glass <me@mike.is>
2024-07-16 12:03:50 +0200

    mydumper: add meta.homepage

--- pkgs/tools/backup/mydumper/default.nix
+++ pkgs/tools/backup/mydumper/default.nix
@@ -1,15 +1,17 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, cmake
-, pkg-config
-, sphinx
-, glib
-, pcre
-, libmysqlclient
-, libressl
-, zlib
-, zstd
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  sphinx,
+  glib,
+  pcre,
+  libmysqlclient,
+  libressl,
+  zlib,
+  zstd,
+  nix-update-script,
 }:
 
 stdenv.mkDerivation rec {
@@ -25,9 +27,17 @@ stdenv.mkDerivation rec {
     hash = "sha256-iMhrwKRAorufmkkNU4h0BvLV6c3/cwYPdsGm41jTkZ4=";
   };
 
-  outputs = [ "out" "doc" "man" ];
+  outputs = [
+    "out"
+    "doc"
+    "man"
+  ];
 
-  nativeBuildInputs = [ cmake pkg-config sphinx ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    sphinx
+  ];
 
   buildInputs = [
     glib
@@ -44,16 +54,15 @@ stdenv.mkDerivation rec {
     "-DWITH_ZSTD=ON"
   ];
 
-
-  env.NIX_CFLAGS_COMPILE =
-    (if stdenv.isDarwin then
+  env.NIX_CFLAGS_COMPILE = (
+    if stdenv.isDarwin then
       toString [
         "-Wno-error=deprecated-non-prototype"
         "-Wno-error=format"
-      ] else
+      ]
+    else
       "-Wno-unused-result"
-    );
-
+  );
 
   postPatch = ''
     substituteInPlace CMakeLists.txt\
@@ -63,10 +72,13 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "High-performance MySQL backup tool";
-    homepage = "https://github.com/maxbube/mydumper";
+    homepage = "https://github.com/mydumper/mydumper";
     changelog = "https://github.com/mydumper/mydumper/releases/tag/v${version}";
     license = licenses.gpl3Plus;
     platforms = lib.platforms.unix;
-    maintainers = with maintainers; [ izorkin michaelglass ];
+    maintainers = with maintainers; [
+      izorkin
+      michaelglass
+    ];
   };
 }
