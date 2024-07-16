569d5e544acb22499debe1b6d2481b4d831a8cc8
zimbatm <zimbatm@zimbatm.com>
2024-07-16 14:24:24 +0200

    wkhtmltopdf: use the -bin version
    wkhtmltopdf is a bit cursed.
    
    1. It needs a specially patched version of QT to work.
    2. It doesn't build currently.
    3. Upstream repos are archived.
    4. It depends on insecure dependencies.
    
    In the mid term this dependency should be removed, but it's still a hard
    dependency for:
    
    * bookstack
    * monica
    * odoo (ref https://github.com/odoo/odoo/issues/86501)
    * snipe-it
    
    Instead of spending time to fix the build issues, use the -bin version
    in the meantime.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14432,8 +14432,6 @@ with pkgs;
 
   wireguard-go = callPackage ../tools/networking/wireguard-go { };
 
-  wkhtmltopdf = libsForQt5.callPackage ../tools/graphics/wkhtmltopdf { };
-
   wkhtmltopdf-bin = callPackage ../tools/graphics/wkhtmltopdf-bin {
     openssl = openssl_1_1;
   };
