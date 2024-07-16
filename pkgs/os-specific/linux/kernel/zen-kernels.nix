86832307ec2d2e7ca9af7496e42c9b0ef8ac96fd
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-07-16 21:03:46 +0100

    linuxKernel.kernels.linux_lqx: fix build
    - Override RCU_LAZY since this is defined in the kernel/common-config.nix
    - Make RCU_BOOST_DELAY optional
    
    Fixes issue #327719.

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -62,7 +62,7 @@ let
       RCU_FANOUT = freeform "64";
       RCU_FANOUT_LEAF = freeform "16";
       RCU_BOOST = yes;
-      RCU_BOOST_DELAY = freeform "500";
+      RCU_BOOST_DELAY = option (freeform "500");
       RCU_NOCB_CPU = yes;
       RCU_LAZY = yes;
       RCU_DOUBLE_CHECK_CB_TIME = yes;
@@ -94,7 +94,7 @@ let
 
       # https://github.com/damentz/liquorix-package/commit/a7055b936c0f4edb8f6afd5263fe1d2f8a5cd877
       RCU_BOOST = no;
-      RCU_LAZY = no;
+      RCU_LAZY = lib.mkOverride 60 no;
 
       # Swap storage is compressed with LZ4 using zswap
       ZSWAP_COMPRESSOR_DEFAULT_LZ4  = lib.mkOptionDefault yes;
