50873c5f82716a29feeb1997c81034a8fb4028cd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-07-16 10:15:44 +0200

    crosstest.sh: drop

--- crosstest.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -i bash -p bash
-set -eux
-
-nix-build -A "librandombytes"
-nix-build -I nixpkgs=$PWD -E 'with import <nixpkgs> {}; librandombytes.override({stdenv = clangStdenv;})'
-nix-build -A "pkgsCross.aarch64-multiplatform.librandombytes"
-nix-build -I nixpkgs=$PWD -E 'with import <nixpkgs> {}; pkgsCross.aarch64-multiplatform.librandombytes.override({stdenv = clangStdenv;})'
