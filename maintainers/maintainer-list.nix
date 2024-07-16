805cdc576a8a30f648d211c2de7ec49a8ab9c299
schnow265 <thesnowbox@icloud.com>
2024-07-16 18:14:38 +0200

    goread: init at 1.6.5

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18050,6 +18050,12 @@
     githubId = 70169773;
     name = "Tarun Chinmai Sekar";
   };
+  schnow265 = {
+    email = "thesnowbox@icloud.com";
+    github = "schnow265";
+    githubId = 57457177;
+    name = "Luca Scalet";
+  };
   schmitthenner = {
     email = "development@schmitthenner.eu";
     github = "fkz";
