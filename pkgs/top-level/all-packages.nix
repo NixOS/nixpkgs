7e390c813e4a4937f53ecbfdbaf7ddd58cd2efe2
aleksana <me@aleksana.moe>
2024-07-16 15:08:04 +0800

    ethabi: remove
    Upstream released several versions after 13.0.0, but none of them were
    updated accordingly in Nixpkgs. Upstream made it clear about a year ago
    that this project was not actively maintained and recommended other
    related projects.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35664,8 +35664,6 @@ with pkgs;
 
   nbxplorer = callPackage ../applications/blockchains/nbxplorer { };
 
-  ethabi = callPackage ../applications/blockchains/ethabi { };
-
   pycoin = with python3Packages; toPythonApplication pycoin;
 
   stellar-core = callPackage ../applications/blockchains/stellar-core { };
