NixOS/nixpkgs

commit 6abc2f4548c00da99ab59da5960825bac2d935f2

python3Packages.django-haystack: 3.2.1 -> 3.3.0

pkgs/development/python-modules/django-haystack/default.nix
@@ -14,7 +14,6 @@
   # tests
   elasticsearch,
   geopy,
-  nose,
   pysolr,
   python-dateutil,
   requests,
@@ -23,42 +22,36 @@
 
 buildPythonPackage rec {
   pname = "django-haystack";
-  version = "3.2.1";
+  version = "3.3.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.5";
 
   src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-l+MZeu/CJf5AW28XYAolNL+CfLTWdDEwwgvBoG9yk6Q=";
+    pname = "django_haystack";
+    inherit version;
+    hash = "sha256-487ta4AAYl2hTUCetNrGmJSQXirIrBj5v9tZMjygLqs=";
   };
 
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace "geopy==" "geopy>="
-  '';
-
-  nativeBuildInputs = [
+  build-system = [
     setuptools
     setuptools-scm
   ];
 
   buildInputs = [ django ];
 
-  passthru.optional-dependencies = {
+  optional-dependencies = {
     elasticsearch = [ elasticsearch ];
   };
 
-  doCheck = lib.versionOlder django.version "4";
-
   nativeCheckInputs = [
     geopy
-    nose
     pysolr
     python-dateutil
     requests
     whoosh
-  ] ++ passthru.optional-dependencies.elasticsearch;
+  ] ++ optional-dependencies.elasticsearch;
+
 
   checkPhase = ''
     runHook preCheck

commit 123686fe6ef5d440df6a227419111f312426c0bc

python3.pkgs.django-haystack: propagate packaging
	  File "/nix/store/155049nzby4i67hh6k8s4m72d3s4zb3j-python3-3.11.9-env/lib/python3.11/site-packages/haystack/__init__.py", line 5, in <module>
	    from packaging.version import Version
	ModuleNotFoundError: No module named 'packaging'

Fixes: 6abc2f4548c0 ("python3Packages.django-haystack: 3.2.1 -> 3.3.0")

pkgs/development/python-modules/django-haystack/default.nix
@@ -10,6 +10,7 @@
 
   # dependencies
   django,
+  packaging,
 
   # tests
   elasticsearch,
@@ -39,6 +40,7 @@ buildPythonPackage rec {
   ];
 
   buildInputs = [ django ];
+  propagatedBuildInputs = [ packaging ];
 
   optional-dependencies = {
     elasticsearch = [ elasticsearch ];