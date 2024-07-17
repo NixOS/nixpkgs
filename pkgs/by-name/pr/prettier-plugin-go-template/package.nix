2dd7649ec44b62879fd71321a4240a7ad5abdb05
Jan Kremer <mail@jankremer.eu>
2024-07-17 10:56:13 +0200

    prettier-plugin-go-template: fix package

--- pkgs/by-name/pr/prettier-plugin-go-template/package.nix
+++ pkgs/by-name/pr/prettier-plugin-go-template/package.nix
@@ -4,23 +4,24 @@
   fetchFromGitHub,
 }:
 
-buildNpmPackage {
+buildNpmPackage rec {
   pname = "prettier-plugin-go-template";
 
   src = fetchFromGitHub {
     owner = "NiklasPor";
-    repo = "prettier-plugin-go-template";
+    repo = pname;
     rev = "d91c82e1377b89592ea3365e7e5569688fbc7954";
     hash = "sha256-3Tvh+OzqDTtzoaTp5dZpgEQiNA2Y2dbyq4SV9Od499A=";
   };
 
   npmDepsHash = "sha256-PpJnVZFRxpUHux2jIBDtyBS4qNo6IJY4kwTAq6stEVQ=";
 
+  dontNpmBuild = true;
+
   meta = {
     description = "Fixes prettier formatting for go templates";
     homepage = "https://github.com/NiklasPor/prettier-plugin-go-template";
     license = lib.licenses.mit;
-    mainProgram = "prettier-plugin-go-template";
     maintainers = with lib.maintainers; [ jukremer ];
   };
 }
