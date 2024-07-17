158362fe87abd94300b0ce6dbdf14e3fc04c4224
Lin Jian <me@linj.tech>
2024-07-18 03:31:11 +0800

    emacsPackages.notdeft: trivialBuild -> melpaBuild

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/notdeft/default.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/notdeft/default.nix
@@ -1,8 +1,7 @@
 { lib
 , stdenv
-, trivialBuild
+, melpaBuild
 , fetchFromGitHub
-, emacs
 , hydra
 , ivy
 , pkg-config
@@ -16,7 +15,7 @@
 
 let
   pname = "notdeft";
-  version = "20211204.0846";
+  version = "0-unstable-2021-12-04";
 
   src = fetchFromGitHub {
     owner = "hasu";
@@ -44,11 +43,10 @@ let
     '';
   };
 in
-trivialBuild {
+melpaBuild {
   inherit pname version src;
   packageRequires = lib.optional withHydra hydra
     ++ lib.optional withIvy ivy;
-  buildInputs = [ xapian ];
 
   postPatch = ''
     substituteInPlace notdeft-xapian.el \
@@ -56,20 +54,17 @@ trivialBuild {
                 "defcustom notdeft-xapian-program \"${notdeft-xapian}/bin/notdeft-xapian\""
   '';
 
-  # Extra modules are contained in the extras/ directory
-  preBuild = lib.optionalString withHydra ''
-    mv extras/notdeft-{mode-hydra,global-hydra}.el ./
-  '' +
-  lib.optionalString withIvy ''
-    mv extras/notdeft-ivy.el ./
-  '' + ''
-    rm -r extras/
+  files = ''
+    (:defaults
+     ${lib.optionalString withHydra ''"extras/notdeft-global-hydra.el"''}
+     ${lib.optionalString withHydra ''"extras/notdeft-mode-hydra.el"''}
+     ${lib.optionalString withIvy ''"extras/notdeft-ivy.el"''})
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://tero.hasu.is/notdeft/";
     description = "Fork of Deft that uses Xapian as a search engine";
-    maintainers = [ maintainers.nessdoor ];
-    platforms = platforms.linux;
+    maintainers = [ lib.maintainers.nessdoor ];
+    platforms = lib.platforms.linux;
   };
 }
