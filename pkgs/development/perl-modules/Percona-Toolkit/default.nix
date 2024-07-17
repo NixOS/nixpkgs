05ca727e6a614da21d5eb53306ea081c63783fd6
Michael Glass <me@mike.is>
2024-07-17 12:13:07 +0200

    perlModules.PerconaToolkit: 3.2.0 -> 3.6.0

--- pkgs/development/perl-modules/Percona-Toolkit/default.nix
+++ pkgs/development/perl-modules/Percona-Toolkit/default.nix
@@ -1,23 +1,24 @@
-{
-  lib,
-  fetchFromGitHub,
-  buildPerlPackage,
-  shortenPerlShebang,
-  DBDmysql,
-  DBI,
-  IOSocketSSL,
-  TermReadKey,
+{ lib
+, fetchFromGitHub
+, buildPerlPackage
+, shortenPerlShebang
+, DBDmysql
+, DBI
+, IOSocketSSL
+, TermReadKey
+, go
+,
 }:
 
 buildPerlPackage rec {
   pname = "Percona-Toolkit";
-  version = "3.2.0";
+  version = "3.6.0";
 
   src = fetchFromGitHub {
     owner = "percona";
     repo = "percona-toolkit";
     rev = "v${version}";
-    sha256 = "084ldpskvlfm32lfss5qqzm5y9b8hf029aa4i5pcnzgb53xaxkqx";
+    sha256 = "sha256-NtyUm6swKru2VkBD+WzRN99oC3dGVguZWgFZxcOUv7E";
   };
 
   outputs = [ "out" ];
@@ -25,11 +26,18 @@ buildPerlPackage rec {
   nativeBuildInputs = [ shortenPerlShebang ];
 
   buildInputs = [
+    go
     DBDmysql
     DBI
     IOSocketSSL
     TermReadKey
   ];
+
+  # workaround for
+  # failed to initialize build cache at /homeless-shelter/Library/Caches/go-build: mkdir /homeless-shelter: read-only file system
+  preBuild = ''
+    export HOME=$TMPDIR
+  '';
 
   postInstall = ''
     shortenPerlShebang $(grep -l "/bin/env perl" $out/bin/*)
@@ -40,6 +48,9 @@ buildPerlPackage rec {
     homepage = "https://www.percona.com/software/database-tools/percona-toolkit";
     changelog = "https://docs.percona.com/percona-toolkit/release_notes.html";
     license = with lib.licenses; [ gpl2Only ];
-    maintainers = with lib.maintainers; [ izorkin ];
+    maintainers = with lib.maintainers; [
+      izorkin
+      michaelglass
+    ];
   };
 }
