162eea10e4484bee03bdcbcbe4ba5fac501444cb
Michael Glass <me@mike.is>
2024-07-17 14:22:22 +0200

    wip move towards upgrading

--- pkgs/development/perl-modules/Percona-Toolkit/default.nix
+++ pkgs/development/perl-modules/Percona-Toolkit/default.nix
@@ -7,46 +7,86 @@
 , IOSocketSSL
 , TermReadKey
 , go
-,
+, buildGoModule
+, git
 }:
 
-buildPerlPackage rec {
-  pname = "Percona-Toolkit";
-  version = "3.6.0";
+let
+  version = "3.6.0"; # Adjust the version as needed
 
   src = fetchFromGitHub {
     owner = "percona";
     repo = "percona-toolkit";
     rev = "v${version}";
     sha256 = "sha256-NtyUm6swKru2VkBD+WzRN99oC3dGVguZWgFZxcOUv7E";
+
+    # needed for build script
+    leaveDotGit = true;
   };
+
+  goDeps = (buildGoModule rec {
+    pname = "Percona-Toolkit go-bindings";
+    inherit src version;
+
+
+    vendorHash = "sha256-uOKccpRuVjtbBW1stTZtUdD1DsUpD9i87wZ4iJrFfUw"; # Nix will automatically fetch and verify dependencies
+
+    buildPhase = ''
+      # skip me
+    '';
+  }).goModules;
+in
+buildPerlPackage rec {
+  pname = "Percona-Toolkit";
+
+  inherit src version;
 
   outputs = [ "out" ];
 
   nativeBuildInputs = [ shortenPerlShebang ];
 
   buildInputs = [
-    go
     DBDmysql
+    go
     DBI
     IOSocketSSL
     TermReadKey
+    git
   ];
 
+
   # workaround for
-  # failed to initialize build cache at /homeless-shelter/Library/Caches/go-build: mkdir /homeless-shelter: read-only file system
+  postPatch = ''
+    cp -r --reflink=auto ${goDeps} vendor
+  '';
+
   preBuild = ''
     export HOME=$TMPDIR
   '';
 
   postInstall = ''
-    shortenPerlShebang $(grep -l "/bin/env perl" $out/bin/*)
+      shortenPerlShebang $(grep -l " /bin/env
+    perl " $out/bin/*)
   '';
 
   meta = {
-    description = "Collection of advanced command-line tools to perform a variety of MySQL and system tasks";
-    homepage = "https://www.percona.com/software/database-tools/percona-toolkit";
-    changelog = "https://docs.percona.com/percona-toolkit/release_notes.html";
+    description = "
+    Collection
+    of
+    advanced
+    command-line
+    tools
+    to
+    perform
+    a
+    variety
+    of
+    MySQL
+    and
+    system
+    tasks ";
+    homepage = " https://www.percona.com/software/database-tools/percona-toolkit ";
+    changelog = " https://docs.percona.com/percona-toolkit/release_notes.html ";
     license = with lib.licenses; [ gpl2Only ];
     maintainers = with lib.maintainers; [
       izorkin
