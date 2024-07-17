ee092285e676502e094d6b4c4c34a0c8d1464369
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-07-18 00:42:08 +0200

    python312Packages.pysmi: 0.3.4 -> 1.4.4
    https://github.com/lextudio/pysmi/blob/v1.4.4/CHANGES.rst
    
    Absorbs pysmi-lextudio, since the author of pysmi has passed away and
    the lextudio fork has now become pysmi.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12423,8 +12423,6 @@ self: super: with self; {
 
   pysmi = callPackage ../development/python-modules/pysmi { };
 
-  pysmi-lextudio = callPackage ../development/python-modules/pysmi-lextudio { };
-
   pysml = callPackage ../development/python-modules/pysml { };
 
   pysmlight = callPackage ../development/python-modules/pysmlight { };
