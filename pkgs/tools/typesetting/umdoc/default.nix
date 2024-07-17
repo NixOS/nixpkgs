c5dafc98d471bf15d363d9bd86ac52a00e20c9a1
Lutz Berger <lutz.berger@airbus.com>
2024-07-17 14:41:34 +0200

    umdoc: init at 0.3.0

---
+++ pkgs/tools/typesetting/umdoc/default.nix
@@ -0,0 +1,26 @@
+{ 
+  stdenv,
+  cmake,
+  texliveFull
+  }:
+
+stdenv.mkDerivation {
+  name = "umdoc";
+  version = "0.3.0";
+  src = fetchGit {
+	url = "https://github.com/craflin/umdoc.git";
+   	ref = "master";
+        submodules = true;
+  };
+
+  postPatch = ''
+     sed -i -e 's$include(CDeploy)$$' CMakeLists.txt
+     head -n 42 CMakeLists.txt > tmp.txt
+     mv tmp.txt CMakeLists.txt
+  '';
+  buildPhase =''
+  '';
+
+  buildInputs = [ cmake texliveFull ];
+
+}
