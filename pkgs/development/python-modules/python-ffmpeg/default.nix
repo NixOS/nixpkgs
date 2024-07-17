6e98dfa9f8774fb8de099dd10d2cbe368c8bd5d9
Roshan Kumar <roshaen09@gmail.com>
2024-07-17 09:44:40 +0530

    python311Packages.python-ffmpeg, python312Packages.python-ffmpeg: init at 2.0.12

--- pkgs/development/python-modules/python-ffmpeg/default.nix
+++ pkgs/development/python-modules/python-ffmpeg/default.nix
@@ -9,7 +9,7 @@
 buildPythonPackage rec {
   pname = "python_ffmpeg";
   version = "2.0.12";
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
@@ -22,8 +22,8 @@ buildPythonPackage rec {
   pythonImportCheck = [ "ffmpeg" ];
 
   meta = {
-    homepage = " https://github.com/jonghwanhyeon/python-ffmpeg";
-    description = "A python binding for FFmpeg which provides sync and async APIs";
+    homepage = "https://github.com/jonghwanhyeon/python-ffmpeg";
+    description = "Python binding for FFmpeg which provides sync and async APIs";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ roshaen ];
   };
