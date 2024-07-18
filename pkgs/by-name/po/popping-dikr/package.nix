923dadfb6587b808a592a11e5fdbca9277eefd69
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-07-18 19:17:28 +0100

    changes 'popping-dikr/package.nix' removed comments, added license and maintainer

--- pkgs/by-name/po/popping-dikr/package.nix
+++ pkgs/by-name/po/popping-dikr/package.nix
@@ -1,5 +1,3 @@
-# بسم الله الرحمن الرحيم
-# la ilaha illa Allah Mohammed Rassoul Allah
 { lib
 , stdenv
 , fetchgit
@@ -15,7 +13,6 @@ stdenv.mkDerivation rec {
 
   src = fetchgit {
     url = "https://github.com/muslimDevCommunity/PoppingDikr.git";
-    #url = "https://github.com/IbrahimOuhamou/popping-dikr.git";
     rev = "78239ee4aef80ab3d4aa0cea0602ee5a7b78136c";
     hash = "sha256-6uxvMTmRcGncVletEX2+GiIrZCX1gnG9lEp3ciwfL+A=";
     fetchSubmodules = true;
@@ -31,8 +28,9 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Dikr app that shows itself every now and then";
     homepage = "https://github.com/muslimDevCommunity/PoppingDikr";
-    # license = licenses.mit;
+    license = licenses.mit;
     mainProgram = "popping-dikr";
     inherit (zig.meta) platforms;
+    maintainers = with maintainers; [ IbrahimOuhamou ];
   };
 }
