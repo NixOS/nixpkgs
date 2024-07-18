671148ade1ceaf96d34657b6cd6376a6ff41e9ab
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-18 19:35:30 +0000

    google-guest-oslogin: 20230831.00 -> 20240701.00

--- pkgs/tools/virtualization/google-guest-oslogin/default.nix
+++ pkgs/tools/virtualization/google-guest-oslogin/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "google-guest-oslogin";
-  version = "20230831.00";
+  version = "20240701.00";
 
   src = fetchFromGitHub {
     owner = "GoogleCloudPlatform";
     repo = "guest-oslogin";
     rev = version;
-    sha256 = "sha256-9QCB94HVbeLjioJuSN1Aa+EqFncojPoWFxw5mS9bDGw=";
+    sha256 = "sha256-YYyUSenixzJAYrtUYMYEbl+GSme++KSX08LnpS4564k=";
   };
 
   postPatch = ''
