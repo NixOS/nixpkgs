0fb7268b9bc3d4e682c18e0716f52a54cb4affe8
Litchi Pi <litchi.pi@proton.me>
2024-07-18 17:17:36 +0200

    Init IFM service at version 4.0.2
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1403,6 +1403,7 @@
   ./services/web-apps/honk.nix
   ./services/web-apps/icingaweb2/icingaweb2.nix
   ./services/web-apps/icingaweb2/module-monitoring.nix
+  ./services/web-apps/ifm.nix
   ./services/web-apps/invidious.nix
   ./services/web-apps/invoiceplane.nix
   ./services/web-apps/isso.nix
