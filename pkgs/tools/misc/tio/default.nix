cde663f7e56506ff6433356aa6bd8a0ac73d00e5
Alexis Hildebrandt <afh@surryhill.net>
2024-07-19 01:39:09 +0200

    tio: 2.8 -> 3.5

--- pkgs/tools/misc/tio/default.nix
+++ pkgs/tools/misc/tio/default.nix
@@ -1,19 +1,19 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, inih, lua, bash-completion, darwin }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, glib, inih, lua, bash-completion, darwin }:
 
 stdenv.mkDerivation rec {
   pname = "tio";
-  version = "2.8";
+  version = "3.5";
 
   src = fetchFromGitHub {
     owner = "tio";
     repo = "tio";
     rev = "v${version}";
-    hash = "sha256-BBfLmRhbDeymXXlYp71XTwbAab7h7gJ842fzZJNb6kU=";
+    hash = "sha256-3d3TYHSERIQdw+Iw6qCydGpWRpWrhZwb4SnwV1nVtIk=";
   };
 
   strictDeps = true;
 
-  buildInputs = [ inih lua ]
+  buildInputs = [ inih lua glib ]
     ++ lib.optionals (stdenv.hostPlatform.isDarwin) [ darwin.apple_sdk.frameworks.IOKit ];
 
   nativeBuildInputs = [ meson ninja pkg-config bash-completion ];
