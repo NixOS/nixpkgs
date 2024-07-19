bbb49863d9b76d414a1da1d5ba5fa5b4aa4a64e8
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-07-19 20:11:34 +0200

    ssm-session-manager-plugin: migrate to buildGoModule
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/by-name/ss/ssm-session-manager-plugin/package.nix
+++ pkgs/by-name/ss/ssm-session-manager-plugin/package.nix
@@ -1,13 +1,11 @@
 { lib
 , fetchFromGitHub
-, buildGoPackage
+, buildGoModule
+, fetchpatch
 }:
-
-buildGoPackage rec {
+buildGoModule rec {
   pname = "ssm-session-manager-plugin";
   version = "1.2.650.0";
-
-  goPackagePath = "github.com/aws/session-manager-plugin";
 
   src = fetchFromGitHub {
     owner = "aws";
@@ -16,40 +14,57 @@ buildGoPackage rec {
     hash = "sha256-IcDVt/iE/EYV9Blvl7Gj0UFJcdsUCFdaSQkIto0CKRI=";
   };
 
+  patches = [
+    # Add support for Go modules.
+    #
+    # This patch doesn't belong to any upstream PR, it is specially crafted for
+    # nixpkgs. Deleting the vendor dir is left out from the patch and done in
+    # postPatch instead, as otherwise the patch would be to big and GitHub returns
+    # an error.
+    #
+    # With https://github.com/aws/session-manager-plugin/pull/74 there is an
+    # upstream PR with the same goal. It isn't pulled here as patch for the same
+    # reason.
+    #
+    # Notice that the dependencies are pinned with the patch, and upstream dependency
+    # updates won't take effect. Patch should be recreated from time to time.
+    # - `rm -rf vendor`
+    # - `go mod init github.com/aws/session-manager-plugin`
+    # - `go mod tidy`
+    # - `go get github.com/twinj/uuid@v0.0.0-20151029044442-89173bcdda19`
+    # - `go mod tidy`
+    # - `git checkout HEAD vendor`
+    ./0001-module-support.patch
+  ];
+
   postPatch = ''
-    mv vendor{,-old}
-    mv vendor-old/src vendor
-    rm -r vendor-old
+    rm -rf vendor
   '';
 
+  vendorHash = "sha256-wK+aWRC5yrPtdihXAj6RlYC9ZTTPuGUg9wLY33skzeE=";
+
+  subPackages = [ "src/sessionmanagerplugin-main" ];
+
   preBuild = ''
-    pushd go/src/${lib.escapeShellArg goPackagePath}
     echo -n ${lib.escapeShellArg version} > VERSION
     go run src/version/versiongenerator/version-gen.go
-    popd
   '';
 
   doCheck = true;
   checkFlags = [ "-skip=TestSetSessionHandlers" ];
 
   preCheck = ''
-    if ! [[ $(go/bin/sessionmanagerplugin-main --version) = ${lib.escapeShellArg version} ]]; then
+    if ! [[ $($GOPATH/bin/sessionmanagerplugin-main --version) = ${lib.escapeShellArg version} ]]; then
       echo 'wrong version'
       exit 1
     fi
   '';
 
-  installPhase = ''
-    runHook preInstall
-    install -Dm555 go/bin/sessionmanagerplugin-main "$out/bin/session-manager-plugin"
-    runHook postInstall
-  '';
-
-  meta = with lib; {
+  meta = {
     homepage = "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html";
     description = "Amazon SSM Session Manager Plugin";
     mainProgram = "session-manager-plugin";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ amarshall mbaillie ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ amarshall mbaillie ];
   };
 }
