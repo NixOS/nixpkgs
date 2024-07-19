b79ed3c78d2c645b83481d5ee66c16f5853ee644
Robert Schütz <mail@dotlambda.de>
2024-07-19 08:59:51 -0700

    python312Packages.pytest-flake8: 1.1.1 -> 1.2.0
    Diff: https://github.com/coherent-oss/pytest-flake8/compare/refs/tags/v1.1.1...v1.2.0
    
    Changelog: https://github.com/coherent-oss/pytest-flake8/blob/refs/tags/v1.2.0/NEWS.rst

--- pkgs/development/python-modules/pytest-flake8/default.nix
+++ pkgs/development/python-modules/pytest-flake8/default.nix
@@ -1,44 +1,52 @@
 {
   lib,
-  fetchpatch,
   buildPythonPackage,
   pythonOlder,
-  fetchPypi,
+  fetchFromGitHub,
+  setuptools-scm,
   flake8,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "pytest-flake8";
-  version = "1.1.1";
+  version = "1.2.0";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
-  format = "setuptools";
-
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "ba4f243de3cb4c2486ed9e70752c80dd4b636f7ccb27d4eba763c35ed0cd316e";
+  src = fetchFromGitHub {
+    owner = "coherent-oss";
+    repo = "pytest-flake8";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-VNefGRB++FZFIGOS8Pyxbfe0zAXqwy+p6uERE70+CT4=";
   };
 
-  patches = [
-    # https://github.com/tholo/pytest-flake8/issues/87
-    # https://github.com/tholo/pytest-flake8/pull/88
-    (fetchpatch {
-      url = "https://github.com/tholo/pytest-flake8/commit/976e6180201f7808a3007c8c5903a1637b18c0c8.patch";
-      hash = "sha256-Hbcpz4fTXtXRnIWuKuDhOVpGx9H1sdQRKqxadk2s+uE=";
-    })
-  ];
+  build-system = [ setuptools-scm ];
 
-  propagatedBuildInputs = [ flake8 ];
+  dependencies = [ flake8 ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
+  disabledTests = [
+    # https://github.com/coherent-oss/pytest-flake8/issues/2
+    "test_version"
+    "test_default_flake8_ignores"
+    "test_ignores_all"
+    "test_w293w292"
+    "test_mtime_caching"
+    "test_ok_verbose"
+    "test_keyword_match"
+    "test_run_on_init_file"
+    "test_unicode_error"
+    "test_junit_classname"
+  ];
+
   meta = {
+    changelog = "https://github.com/coherent-oss/pytest-flake8/blob/${src.rev}/NEWS.rst";
     description = "py.test plugin for efficiently checking PEP8 compliance";
-    homepage = "https://github.com/tholo/pytest-flake8";
+    homepage = "https://github.com/coherent-oss/pytest-flake8";
     maintainers = with lib.maintainers; [ jluttine ];
     license = lib.licenses.bsd2;
-    broken = lib.versionAtLeast flake8.version "6.0.0";
   };
 }
