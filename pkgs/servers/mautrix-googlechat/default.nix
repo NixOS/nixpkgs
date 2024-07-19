ea52831349761ed5cbec2df679bb16c6d8f1e209
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-19 11:24:45 +0000

    mautrix-googlechat: 0.5.1 -> 0.5.2

--- pkgs/servers/mautrix-googlechat/default.nix
+++ pkgs/servers/mautrix-googlechat/default.nix
@@ -7,13 +7,13 @@
 , enableSqlite ? true
 }: python3.pkgs.buildPythonApplication rec {
   pname = "mautrix-googlechat";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "googlechat";
     rev = "refs/tags/v${version}";
-    hash = "sha256-a/EWz/aCkBE6XdDpmZcx2Q7/xKNwGCiZUhZc9YIIDhU=";
+    hash = "sha256-4H+zUH0GEQ5e/9Bv0BVdf1/pXulx2ihZrhJ+jl/db+U=";
   };
 
   patches = [
