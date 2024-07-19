15ebd6c89502de8652e244fcffffe5788814f2f6
summersamara <126388106+summersamara@users.noreply.github.com>
2024-07-19 15:25:45 +0200

    Fixup
    Co-authored-by: Kerstin <kerstin@erictapen.name>

--- pkgs/by-name/vi/visual-hexdiff/package.nix
+++ pkgs/by-name/vi/visual-hexdiff/package.nix
@@ -20,6 +20,7 @@ stdenv.mkDerivation {
   '';
 
   patches = [
+    # Some changes the debian/ubuntu developers made over the original source code
     (fetchpatch {
       url = "mirror://ubuntu/pool/universe/h/hexdiff/hexdiff_0.0.53-0ubuntu4.diff.gz";
       sha256 = "sha256-X5ONNp9jeACxsulyowDQJ6REX6bty6L4in0/+rq8Wz4=";
