fbefaa4d6d00274d24f9f971e2fc91d5e48c74bc
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-19 01:18:01 +0000

    python312Packages.bring-api: 0.7.2 -> 0.7.3

--- pkgs/development/python-modules/bring-api/default.nix
+++ pkgs/development/python-modules/bring-api/default.nix
@@ -13,16 +13,16 @@
 
 buildPythonPackage rec {
   pname = "bring-api";
-  version = "0.7.2";
+  version = "0.7.3";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "miaucl";
     repo = "bring-api";
     rev = "refs/tags/${version}";
-    hash = "sha256-941IAVlLwfHCyqUu0AhdIfBjuT3pZpk98ZUssBVjEUA=";
+    hash = "sha256-9asmGm2RwiP2BIygIkLLU30E0zJ/05kvoAfEPlGFW5U=";
   };
 
   build-system = [ setuptools ];
