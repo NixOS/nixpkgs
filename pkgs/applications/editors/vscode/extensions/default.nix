243cd1b556785ae00f081ff38c1a9b8a682a5daa
Benedikt Hiemer <ben.email@posteo.de>
2024-07-20 12:14:32 +0200

    vscode-extensions.42crunch.vscode-openapi: 4.25.3 -> 4.27.0
    - Changelog: https://github.com/42Crunch/vscode-openapi/blob/master/CHANGELOG.md#version-4270-june-27-2024
    - Comparing changes: https://github.com/42Crunch/vscode-openapi/compare/v4.25.3...v4.27.0

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -90,8 +90,8 @@ let
         mktplcRef = {
           publisher = "42Crunch";
           name = "vscode-openapi";
-          version = "4.25.3";
-          hash = "sha256-1kz/M2od2gLSFgqW6LsPHgtm+BwXA+0+7z3HyqNmsOg=";
+          version = "4.27.0";
+          hash = "sha256-urXGyHpIDWQ0Bc+8LODC0DcEo6jQ5tA/QptyxCej9yU=";
         };
         meta = {
           changelog = "https://marketplace.visualstudio.com/items/42Crunch.vscode-openapi/changelog";
