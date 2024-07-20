c774c7bffe0d5fbf3da5c61e9b8bc3a8d8b11cf7
Someone Serge <sergei.kozlukov@aalto.fi>
2024-07-20 12:02:30 +0000

    cudaPackages.writeGpuTestPython: allow a selector for `libraries` to accommodate different python versions

--- pkgs/development/cuda-modules/write-gpu-python-test.nix
+++ pkgs/development/cuda-modules/write-gpu-python-test.nix
@@ -2,16 +2,40 @@
   lib,
   writers,
   runCommand,
+  python3Packages,
 }:
 {
   feature ? "cuda",
   name ? feature,
-  libraries ? [ ],
+  libraries ? [ ], # [PythonPackage] | (PackageSet -> [PythonPackage])
 }:
+
+let
+  inherit (builtins) isFunction all;
+  librariesFun = if isFunction libraries then libraries else (_: libraries);
+in
+
+assert lib.assertMsg (
+  isFunction libraries || all (python3Packages.hasPythonModule) libraries
+) "writeGpuTestPython was passed `libraries` from the wrong python release";
+
 content:
 
 let
-  tester = writers.writePython3Bin "tester-${name}" { inherit libraries; } content;
+  interpreter = python3Packages.python.withPackages librariesFun;
+  tester =
+    runCommand "tester-${name}"
+      {
+        inherit content;
+        passAsFile = [ "content" ];
+      }
+      ''
+        mkdir -p "$out"/bin
+        cat << EOF >"$out"/bin/"tester-${name}"
+        #!${lib.getExe interpreter}
+        EOF
+        cat "$contentPath" >>"$out"/bin/"tester-${name}"
+      '';
   tester' = tester.overrideAttrs (oldAttrs: {
     passthru.gpuCheck =
       runCommand "test-${name}"
