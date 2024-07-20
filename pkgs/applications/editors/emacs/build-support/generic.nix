eabd7cf0fc1e760c5069f43c5dbe325eb48decfd
Lin Jian <me@linj.tech>
2024-07-20 22:34:45 +0800

    emacs: log native compilation commands in genericBuild

--- pkgs/applications/editors/emacs/build-support/generic.nix
+++ pkgs/applications/editors/emacs/build-support/generic.nix
@@ -82,7 +82,7 @@ stdenv.mkDerivation (finalAttrs: ({
     addEmacsVars "$out"
 
     find $out/share/emacs -type f -name '*.el' -print0 \
-      | xargs -0 -I {} -n 1 -P $NIX_BUILD_CORES sh -c \
+      | xargs --verbose -0 -I {} -n 1 -P $NIX_BUILD_CORES sh -c \
           "emacs \
              --batch \
              --eval '(setq large-file-warning-threshold nil)' \
