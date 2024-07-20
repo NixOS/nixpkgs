c60070110e1ae87df1ea4ca9f381e5846d50726f
Audrey Dutcher <audrey@rhelmot.io>
2024-07-20 01:00:27 -0700

    python3Packages.cffi: disable tests on FreeBSD
    This test suite cannot work on FreeBSD under nix. It only works on linux
    because of hardcoded exceptions in the library loading logic which are
    not present for FreeBSD, requiring it to fall back to /sbin/ldconfig
    (does not work obviously).

--- pkgs/development/python-modules/cffi/default.nix
+++ pkgs/development/python-modules/cffi/default.nix
@@ -84,7 +84,7 @@ else
     # The tests use -Werror but with python3.6 clang detects some unreachable code.
     env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-unused-command-line-argument -Wno-unreachable-code -Wno-c++11-narrowing";
 
-    doCheck = !stdenv.hostPlatform.isMusl;
+    doCheck = !stdenv.hostPlatform.isMusl && !stdenv.hostPlatform.isFreeBSD;
 
     nativeCheckInputs = [ pytestCheckHook ];
 
