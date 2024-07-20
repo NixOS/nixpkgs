38ad1f17e739da4afce65718dea33b493fe0d42f
Franz Pletz <fpletz@fnordicwalking.de>
2024-07-20 16:16:21 +0200

    nixos/proxmox-lxc: fix nixos-rebuild
    Same is being done in lxc-container module.
    
    (cherry picked from commit ceafec213ff2514791848b1999e01ebf17dd6ceb)

--- nixos/modules/virtualisation/proxmox-lxc.nix
+++ nixos/modules/virtualisation/proxmox-lxc.nix
@@ -64,6 +64,18 @@ with lib;
         extraCommands = "mkdir -p root etc/systemd/network";
       };
 
+      boot.postBootCommands = ''
+        # After booting, register the contents of the Nix store in the Nix
+        # database.
+        if [ -f /nix-path-registration ]; then
+          ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration &&
+          rm /nix-path-registration
+        fi
+
+        # nixos-rebuild also requires a "system" profile
+        ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+      '';
+
       boot = {
         isContainer = true;
         loader.initScript.enable = true;
