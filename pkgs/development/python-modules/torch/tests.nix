2cc58226b0205fae68ff9266aa6edef09df06f6d
Someone Serge <sergei.kozlukov@aalto.fi>
2024-07-20 12:05:52 +0000

    python3Packages.torch.tests.*compile*: init

--- pkgs/development/python-modules/torch/tests.nix
+++ pkgs/development/python-modules/torch/tests.nix
@@ -1,6 +1,6 @@
 { callPackage }:
 
-{
+rec {
   # To perform the runtime check use either
   # `nix run .#python3Packages.torch.tests.tester-cudaAvailable` (outside the sandbox), or
   # `nix build .#python3Packages.torch.tests.tester-cudaAvailable.gpuCheck` (in a relaxed sandbox)
@@ -12,6 +12,20 @@
   tester-rocmAvailable = callPackage ./mk-runtime-check.nix {
     feature = "rocm";
     versionAttr = "hip";
+    libraries = ps: [ ps.torchWithRocm ];
+  };
+
+  compileCpu = tester-compileCpu.gpuCheck;
+  tester-compileCpu = callPackage ./mk-torch-compile-check.nix {
+    feature = null;
+    libraries = ps: [ ps.torch ];
+  };
+  tester-compileCuda = callPackage ./mk-torch-compile-check.nix {
+    feature = "cuda";
+    libraries = ps: [ ps.torchWithCuda ];
+  };
+  tester-compileRocm = callPackage ./mk-torch-compile-check.nix {
+    feature = "rocm";
     libraries = ps: [ ps.torchWithRocm ];
   };
 }
