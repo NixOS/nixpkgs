c71063129466bb582f539fcbe2b255dbaba5622b
eymeric <eymericdechelette@gmail.com>
2024-07-20 16:28:41 +0200

    onlyoffice-documentserver: enable aarch64-linux support

--- pkgs/servers/onlyoffice-documentserver/default.nix
+++ pkgs/servers/onlyoffice-documentserver/default.nix
@@ -17,10 +17,16 @@ let
     pname = "onlyoffice-documentserver";
     version = "7.5.1";
 
-    src = fetchurl {
-      url = "https://github.com/ONLYOFFICE/DocumentServer/releases/download/v${lib.concatStringsSep "." (lib.take 3 (lib.splitVersion version))}/onlyoffice-documentserver_amd64.deb";
-      sha256 = "sha256-191PYpxs/TbVXoBPHvuyTp81ZMtw1YaFznY1hUSbh+0=";
-    };
+    src = fetchurl ({
+      "aarch64-linux" = {
+        url = "https://github.com/ONLYOFFICE/DocumentServer/releases/download/v${version}/onlyoffice-documentserver_arm64.deb";
+        sha256 = "sha256-j16LrYDq/7kuJWAmCb8BTJ6S9LyJ092cwRemDic7EOs=";
+      };
+      "x86_64-linux" = {
+        url = "https://github.com/ONLYOFFICE/DocumentServer/releases/download/v${version}/onlyoffice-documentserver_amd64.deb";
+        sha256 = "sha256-191PYpxs/TbVXoBPHvuyTp81ZMtw1YaFznY1hUSbh+0=";
+      };
+    }.${stdenv.hostPlatform.system} or (throw "unsupported platform"));
 
     preferLocalBuild = true;
 
@@ -144,7 +150,7 @@ let
       '';
       homepage = "https://github.com/ONLYOFFICE/DocumentServer";
       license = licenses.agpl3Plus;
-      platforms = [ "x86_64-linux" ];
+      platforms = [ "x86_64-linux" "aarch64-linux" ];
       sourceProvenance = [ sourceTypes.binaryNativeCode ];
       maintainers = with maintainers; [ SuperSandro2000 ];
     };
