d67000f0aacd4339255a4806cda9fc359f2acc6a
Franz Pletz <fpletz@fnordicwalking.de>
2024-07-21 21:40:41 +0200

    font-awesome: reformat

--- pkgs/data/fonts/font-awesome/default.nix
+++ pkgs/data/fonts/font-awesome/default.nix
@@ -1,35 +1,48 @@
-{ lib, stdenvNoCC, fetchFromGitHub }:
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+}:
 let
-  font-awesome = { version, hash, rev ? version }: stdenvNoCC.mkDerivation {
-    pname = "font-awesome";
-    inherit version;
+  font-awesome =
+    {
+      version,
+      hash,
+      rev ? version,
+    }:
+    stdenvNoCC.mkDerivation {
+      pname = "font-awesome";
+      inherit version;
 
-    src = fetchFromGitHub {
-      owner = "FortAwesome";
-      repo = "Font-Awesome";
-      inherit rev hash;
-    };
+      src = fetchFromGitHub {
+        owner = "FortAwesome";
+        repo = "Font-Awesome";
+        inherit rev hash;
+      };
 
-    installPhase = ''
-      runHook preInstall
+      installPhase = ''
+        runHook preInstall
 
-      install -m444 -Dt $out/share/fonts/opentype {fonts,otfs}/*.otf
+        install -m444 -Dt $out/share/fonts/opentype {fonts,otfs}/*.otf
 
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "Font Awesome - OTF font";
-      longDescription = ''
-        Font Awesome gives you scalable vector icons that can instantly be customized.
-        This package includes only the OTF font. For full CSS etc. see the project website.
+        runHook postInstall
       '';
-      homepage = "https://fontawesome.com/";
-      license = licenses.ofl;
-      platforms = platforms.all;
-      maintainers = with maintainers; [ abaldeau johnazoidberg ];
+
+      meta = with lib; {
+        description = "Font Awesome - OTF font";
+        longDescription = ''
+          Font Awesome gives you scalable vector icons that can instantly be customized.
+          This package includes only the OTF font. For full CSS etc. see the project website.
+        '';
+        homepage = "https://fontawesome.com/";
+        license = licenses.ofl;
+        platforms = platforms.all;
+        maintainers = with maintainers; [
+          abaldeau
+          johnazoidberg
+        ];
+      };
     };
-  };
 in
 {
   # Keeping version 4 and 5 because version 6 is incompatible for some icons. That
