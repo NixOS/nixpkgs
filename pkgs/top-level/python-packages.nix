7b4e457e05dbadff22654e933a1d092bc5e298ae
Sigmanificient <edhyjox@gmail.com>
2024-07-21 19:14:07 +0200

    python312Packages.topojson: init at 1.9

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15582,6 +15582,8 @@ self: super: with self; {
 
   toonapi = callPackage ../development/python-modules/toonapi { };
 
+  topojson =  callPackage ../development/python-modules/topojson { };
+
   toposort = callPackage ../development/python-modules/toposort { };
 
   torch = callPackage ../development/python-modules/torch {
