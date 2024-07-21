a9ea12dba3dc7bf5b31898979b8597d90b2671ee
Sigmanificient <edhyjox@gmail.com>
2024-07-21 07:28:51 +0200

    python312Packages.quackosm: init at 0.8.2
    remove srai

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13212,6 +13212,8 @@ self: super: with self; {
 
   qtpy = callPackage ../development/python-modules/qtpy { };
 
+  quackosm = callPackage ../development/python-modules/quackosm { };
+
   quadprog = callPackage ../development/python-modules/quadprog { };
 
   qualysclient = callPackage ../development/python-modules/qualysclient { };
