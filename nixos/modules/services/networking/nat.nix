6cb4e7d591c65f17c891faa135d267ee8c54b307
TNE <tne@garudalinux.org>
2024-07-21 06:58:20 +0200

    nixos/nat: Only connections made to the nat.externalIP will be port forwarded.

--- nixos/modules/services/networking/nat.nix
+++ nixos/modules/services/networking/nat.nix
@@ -82,7 +82,8 @@ in
         The public IP address to which packets from the local
         network are to be rewritten.  If this is left empty, the
         IP address associated with the external interface will be
-        used.
+        used.  Only connections made to this IP address will be
+        forwarded to the internal network when using forwardPorts.
       '';
     };
 
@@ -94,7 +95,8 @@ in
         The public IPv6 address to which packets from the local
         network are to be rewritten.  If this is left empty, the
         IP address associated with the external interface will be
-        used.
+        used.  Only connections made to this IP address will be
+        forwarded to the internal network when using forwardPorts.
       '';
     };
 
