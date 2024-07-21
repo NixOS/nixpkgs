e9dced891dd4f39b855b67b63ef1b2878c85a77e
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-07-21 19:08:16 -0400

    yt-dlp: move to by-name

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17903,13 +17903,13 @@ self: super: with self; {
 
   yoyo-migrations = callPackage ../development/python-modules/yoyo-migrations { };
 
-  yt-dlp = callPackage ../tools/misc/yt-dlp { ffmpeg = pkgs.ffmpeg-headless; };
+  yt-dlp = toPythonModule (pkgs.yt-dlp.override {
+    python3Packages = self;
+  });
 
-  yt-dlp-light = callPackage ../tools/misc/yt-dlp {
-    atomicparsleySupport = false;
-    ffmpegSupport = false;
-    rtmpSupport = false;
-  };
+  yt-dlp-light = toPythonModule (pkgs.yt-dlp-light.override {
+    python3Packages = self;
+  });
 
   youtube-search = callPackage ../development/python-modules/youtube-search { };
 
