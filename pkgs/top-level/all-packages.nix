0ece69c750f2b38f3bdb56e1eeb7c10f4588d2a9
Franz Pletz <fpletz@fnordicwalking.de>
2024-07-21 20:26:57 +0200

    nmap: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10925,8 +10925,6 @@ with pkgs;
 
   npth = callPackage ../development/libraries/npth { };
 
-  nmap = callPackage ../tools/security/nmap { };
-
   nmap-formatter = callPackage ../tools/security/nmap-formatter { };
 
   nmapsi4 = libsForQt5.callPackage ../tools/security/nmap/qt.nix { };
