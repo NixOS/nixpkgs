c761fba97464efcd7bcff02020a71f2893938f55
Bob van der Linden <bobvanderlinden@gmail.com>
2024-07-22 09:17:35 +0200

    ngrok: format update.sh

--- pkgs/by-name/ng/ngrok/update.sh
+++ pkgs/by-name/ng/ngrok/update.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p xh
-#!nix-shell -p jq
-#!nix-shell -i bash
+#!nix-shell -i bash -p jq xh
 
 set -eu -o pipefail
 
 get_download_info() {
     xh --json \
-         https://update.equinox.io/check \
-         'Accept:application/json; q=1; version=1; charset=utf-8' \
-         'Content-Type:application/json; charset=utf-8' \
-         app_id=app_c3U4eZcDbjV \
-         channel=stable \
-         os="$1" \
-         goarm= \
-         arch="$2" \
-    | jq --arg sys "$1-$2" '{
+        https://update.equinox.io/check \
+        'Accept:application/json; q=1; version=1; charset=utf-8' \
+        'Content-Type:application/json; charset=utf-8' \
+        app_id=app_c3U4eZcDbjV \
+        channel=stable \
+        os="$1" \
+        goarm= \
+        arch="$2" |
+        jq --arg sys "$1-$2" '{
         sys: $sys,
         url: .download_url,
         sha256: .checksum,
@@ -31,4 +29,4 @@ get_download_info() {
     get_download_info darwin amd64
     get_download_info darwin arm64
 ) | jq --slurp 'map ({ (.sys): . }) | add' \
-    > pkgs/by-name/ng/ngrok/versions.json
+    >pkgs/by-name/ng/ngrok/versions.json
