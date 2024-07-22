50dcf3dd501c34cc1a1488635a7693886fbea5b2
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-22 02:37:08 +0000

    python312Packages.renault-api: 0.2.4 -> 0.2.5

--- pkgs/development/python-modules/renault-api/default.nix
+++ pkgs/development/python-modules/renault-api/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "renault-api";
-  version = "0.2.4";
+  version = "0.2.5";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "hacf-fr";
     repo = "renault-api";
     rev = "refs/tags/v${version}";
-    hash = "sha256-dr830PDGiTpiWCHsgxDspVyk6isOVsdGhB/jL96CDic=";
+    hash = "sha256-PBNiYfWcPx3AorpYmNiPGC8MAE8efQOwdtaY8dH1wTs=";
   };
 
   build-system = [ poetry-core ];
