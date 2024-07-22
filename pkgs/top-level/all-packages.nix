db2396c7f9af99da25daf2a8e6baa05d75f6501a
Max Hausch <github@hausch.xyz>
2024-07-22 10:55:07 +0200

    parmetis: Move to pkgs/by-name/pa/parmetis

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37702,8 +37702,6 @@ with pkgs;
     p4est-sc = p4est-sc-dbg;
   };
 
-  parmetis = callPackage ../development/libraries/science/math/parmetis { };
-
   QuadProgpp = callPackage ../development/libraries/science/math/QuadProgpp { };
 
   scs = callPackage ../development/libraries/science/math/scs { };
