8aa88958e060dd77bd0b19872cb33dca2bde35fb
e-renna <ann3r@pm.me>
2024-07-22 18:43:34 +0100

    owncloud-client-nautilus: init at 5.0.0

---
+++ pkgs/by-name/ow/owncloud-client-nautilus/package.nix
@@ -0,0 +1,31 @@
+{ python3, fetchFromGitHub, gnome, stdenv, lib }:
+stdenv.mkDerivation rec {
+  pname = "owncloud-client-nautilus";
+  version = "5.0.0";
+
+  src = fetchFromGitHub {
+      owner = "owncloud";
+      repo = "client-desktop-shell-integration-nautilus";
+      rev = "refs/tags/v${version}";
+      hash = "sha256-BhluDyMBrv6K85VjHWFO48y8hp36FmMs1U342yuQdi8=";
+  };
+
+  buildInputs = [
+    gnome.nautilus-python
+    python3.pkgs.pygobject3
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm555 ./src/syncstate.py -t $out/share/nautilus-python/extensions
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "ownCloud client extension for Nautilus.";
+    license = licenses.gpl2;
+    maintainers = with maintainers; [ e-renna ];
+    homepage = "https://github.com/owncloud/client-desktop-shell-integration-nautilus";
+    platforms = platforms.linux;
+  };
+}
