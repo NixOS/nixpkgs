5622ff23c9e6aef821249fa053d62e0bb0b3ba1d
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-07-23 15:50:02 +0200

    mopsa: init at 1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18810,6 +18810,8 @@ with pkgs;
 
   moon = callPackage ../development/tools/build-managers/moon/default.nix { };
 
+  mopsa = ocamlPackages.mopsa.bin;
+
   msgpack-tools = callPackage ../development/tools/msgpack-tools { };
 
   msgpuck = callPackage ../development/libraries/msgpuck { };
