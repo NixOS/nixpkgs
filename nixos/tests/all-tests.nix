ed2396bde2512139e95ca79e0edad6ddbdb6d996
eljamm <fedi.jamoussi@protonmail.ch>
2024-07-23 18:20:41 +0100

    tests/taler: init basic test
    Test build and services start, but libeufin-bank fails when trying to
    access the database to change the admin's password.
    
    We need to execute the command as the libeufin-bank user.
    
    tests/taler: add master private key
    
    tests/taler: rewrite `register_bank_account` to Nix
    
    tests/taler: rename libeufin node to bank
    
    tests/taler: use xtaler wire_type instead of iban
    
    tests/taler: remove redundant data from conf files
    
    tests/taler: enable exchange account
    
    tests/taler: remove unused talerConfig
    
    tests/taler: add client node and attempt a withdrawal
    
    tests/taler: systemd_run optional user and group args
    
    tests/taler: refactor and make a withdrawal
    
    tests/taler: refactor tasks into subtests
    
    tests/taler: properly read and test balance
    
    tests/taler: refactor commands and add comments
    
    nixos/taler: rename private key
    
    tests/taler: enable nexus service in bank node
    
    tests/taler: nexus fake incoming payment test
    
    tests/taler: use correct path for nexus client keys
    
    tests/taler: add merchant node
    
    tests/taler: merchant register instance
    
    tests/taler: init pay for order merchant
    
    tests/taler: fix payto uri
    
    tests/taler: withdraw smaller amount
    
    This makes the test faster
    
    tests/taler: verify balance
    
    tests/nixos: debugging merchant payment, cleanup
    
    tests/taler: fix libeufin command, use curl to register accounts
    
    tests/taler: add basic online test
    
    tests/taler: move nodes into separate directory
    
    tests/taler: fix insufficient balance error
    
    Turns out that the exchange wire fees need to be set up (even if they're
    0) in order for the CLI wallet to deposit coins into the merchant's bank
    account.
    
    tests/taler: improve node importing, port forwarding
    
    tests/taler: import scripts from a separate file
    
    tests/taler: move tests into a sub-directory
    
    tests/taler: manually start services, cleanup
    
    This results in less overhead and conflict since components will not try
    to prematurely connect to the ones that haven't finished their set up.
    
    tests/taler: remove online test
    
    This was used to debug the insufficient balance problem, but it's not
    really that useful by itself.
    
    tests/taler: add nexus keys
    
    tests/taler: use bank initalAccounts option
    
    taler/tests: use initialAccount
    
    tests/taler: make nexus work
    
    tests/taler: don't run nexus test if there is no internet
    
    tests/taler: use openFirewall, remove manual package install

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1003,6 +1003,7 @@ in {
   systemd-userdbd = handleTest ./systemd-userdbd.nix {};
   systemd-homed = handleTest ./systemd-homed.nix {};
   systemtap = handleTest ./systemtap.nix {};
+  taler = handleTest ./taler {};
   tandoor-recipes = handleTest ./tandoor-recipes.nix {};
   tandoor-recipes-script-name = handleTest ./tandoor-recipes-script-name.nix {};
   tang = handleTest ./tang.nix {};
