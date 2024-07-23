c31bf925295243609e3ac6aa6d3ce43bebcd0760
Franz Pletz <fpletz@fnordicwalking.de>
2024-07-23 21:55:00 +0200

    httm: reformat, move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1851,8 +1851,6 @@ with pkgs;
 
   hsd = callPackage ../tools/misc/hsd { };
 
-  httm = darwin.apple_sdk_11_0.callPackage ../tools/filesystems/httm { };
-
   hyperpotamus = callPackage ../tools/misc/hyperpotamus { };
 
   immich-cli = callPackage ../tools/misc/immich-cli { };
