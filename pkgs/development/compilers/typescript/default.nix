0ad5a4c2f3419b0aac6337ba9007678b4894a7da
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-23 05:15:50 +0000

    typescript: 5.5.3 -> 5.5.4

--- pkgs/development/compilers/typescript/default.nix
+++ pkgs/development/compilers/typescript/default.nix
@@ -2,20 +2,20 @@
 
 buildNpmPackage rec {
   pname = "typescript";
-  version = "5.5.3";
+  version = "5.5.4";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "TypeScript";
     rev = "v${version}";
-    hash = "sha256-2yfF14hR/avl67cOwo+Gr95w2jf+7KYlsMrlBTAKswM=";
+    hash = "sha256-U4lFR2SJQYqJYptVf3/QMD7ufa3r7Zm704gcwFKM/7s=";
   };
 
   patches = [
     ./disable-dprint-dstBundler.patch
   ];
 
-  npmDepsHash = "sha256-YihnUZgM+74EmImalyTFtp9xyiOOfjZVohDfpcvlSEg=";
+  npmDepsHash = "sha256-JYNFAOTN0WvYkr20J6tFM1KSIDTm1LlFw7w1bLkdg6k=";
 
   passthru.tests = {
     version = testers.testVersion {
