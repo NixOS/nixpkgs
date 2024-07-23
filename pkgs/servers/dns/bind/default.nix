2faa9a190282cb7012e4abf443906f2b80c5b817
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-07-23 18:28:28 +0200

    bind: 9.18.27 -> 9.18.28
    https://downloads.isc.org/isc/bind9/cur/9.18/CHANGES
    https://www.openwall.com/lists/oss-security/2024/07/23/1
    
    Fixes: CVE-2024-1975, CVE-2024-4076, CVE-2024-1737, CVE-2024-0760
    (cherry picked from commit bbb4e2f9e28148bc4d00f7f6e05bc54dfabe0264)

--- pkgs/servers/dns/bind/default.nix
+++ pkgs/servers/dns/bind/default.nix
@@ -24,11 +24,11 @@
 
 stdenv.mkDerivation rec {
   pname = "bind";
-  version = "9.18.27";
+  version = "9.18.28";
 
   src = fetchurl {
     url = "https://downloads.isc.org/isc/bind9/${version}/${pname}-${version}.tar.xz";
-    hash = "sha256-6j89jPovaueMhyJ1HQCPVLwXo67Svj9zmet79fTNqPE=";
+    hash = "sha256-58zpoWX3thnu/Egy8KjcFrAF0p44kK7WAIxQbqKGpec=";
   };
 
   outputs = [ "out" "lib" "dev" "man" "dnsutils" "host" ];
