c3dacd86f095e325888d6dc91b1beef5bc5eafe6
Robert Schütz <mail@dotlambda.de>
2024-07-23 08:47:00 -0700

    python312Packages.anyio: 4.3.0 -> 4.4.0
    Diff: https://github.com/agronholm/anyio/compare/refs/tags/4.3.0...4.4.0
    
    Changelog: https://github.com/agronholm/anyio/blob/refs/tags/4.4.0/docs/versionhistory.rst

--- pkgs/development/python-modules/anyio/default.nix
+++ pkgs/development/python-modules/anyio/default.nix
@@ -6,7 +6,6 @@
   pythonOlder,
 
   # build-system
-  setuptools,
   setuptools-scm,
 
   # dependencies
@@ -33,24 +32,21 @@
 
 buildPythonPackage rec {
   pname = "anyio";
-  version = "4.3.0";
+  version = "4.4.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "agronholm";
-    repo = pname;
+    repo = "anyio";
     rev = "refs/tags/${version}";
-    hash = "sha256-y58DQiTD0ZKaBNf0cA3MFE+7F68Svrl+Idz6BZY7HWQ=";
+    hash = "sha256-Sz/wWOT59T7LOAq68fBujgkTaY9ydMsIoSxeP3fBaoY=";
   };
 
-  nativeBuildInputs = [
-    setuptools
-    setuptools-scm
-  ];
+  build-system = [ setuptools-scm ];
 
-  propagatedBuildInputs =
+  dependencies =
     [
       idna
       sniffio
@@ -63,9 +59,6 @@ buildPythonPackage rec {
   passthru.optional-dependencies = {
     trio = [ trio ];
   };
-
-  # trustme uses pyopenssl
-  doCheck = !(stdenv.isDarwin && stdenv.isAarch64);
 
   nativeCheckInputs = [
     exceptiongroup
@@ -85,7 +78,7 @@ buildPythonPackage rec {
     "'not network'"
   ];
 
-  disabledTests = lib.optionals (stdenv.isx86_64 && stdenv.isDarwin) [
+  disabledTests = lib.optionals stdenv.isDarwin [
     # PermissionError: [Errno 1] Operation not permitted: '/dev/console'
     "test_is_block_device"
   ];
