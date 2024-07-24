185a57d2b5776031719e0a6af3d071a01c69383c
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-07-24 08:01:17 +0200

    ocamlPackages.swhid_core: init at 0.1

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1746,6 +1746,8 @@ let
 
     stringext = callPackage ../development/ocaml-modules/stringext { };
 
+    swhid_core = callPackage ../development/ocaml-modules/swhid_core { };
+
     syslog = callPackage ../development/ocaml-modules/syslog { };
 
     syslog-message = callPackage ../development/ocaml-modules/syslog-message { };
