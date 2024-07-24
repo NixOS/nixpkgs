07e304dac74dcd3aa52c5b199300116b80bc4539
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-07-24 08:01:17 +0200

    ocamlPackages.opam-format: clean

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1384,9 +1384,7 @@ let
 
     opam-file-format = callPackage ../development/ocaml-modules/opam-file-format { };
 
-    opam-format = callPackage ../development/ocaml-modules/opam-format {
-      inherit (pkgs) unzip;
-    };
+    opam-format = callPackage ../development/ocaml-modules/opam-format { };
 
     opam-repository = callPackage ../development/ocaml-modules/opam-repository { };
 
