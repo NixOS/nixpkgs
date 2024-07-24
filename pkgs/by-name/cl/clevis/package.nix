6c8a2301776eaa2dc9e569158dbdc8d72d160867
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-23 23:35:08 -0300

    clevis: rework - input set

--- pkgs/by-name/cl/clevis/package.nix
+++ pkgs/by-name/cl/clevis/package.nix
@@ -1,22 +1,23 @@
-{ lib
-, stdenv
-, asciidoc
-, coreutils
-, cryptsetup
-, curl
-, fetchFromGitHub
-, gnugrep
-, gnused
-, jansson
-, jose
-, libpwquality
-, luksmeta
-, makeWrapper
-, meson
-, ninja
-, pkg-config
-, tpm2-tools
-, nixosTests
+{
+  lib,
+  asciidoc,
+  coreutils,
+  cryptsetup,
+  curl,
+  fetchFromGitHub,
+  gnugrep,
+  gnused,
+  jansson,
+  jose,
+  libpwquality,
+  luksmeta,
+  makeWrapper,
+  meson,
+  ninja,
+  nixosTests,
+  pkg-config,
+  stdenv,
+  tpm2-tools,
 }:
 
 stdenv.mkDerivation rec {
