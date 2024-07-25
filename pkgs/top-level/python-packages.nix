945f293fe1a34ed1bdb0f1984446381c60bc81fa
jopejoe1 <johannes@joens.email>
2024-07-25 19:38:36 +0200

    python3Packages.diffenator2: init at 0.4.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3081,6 +3081,8 @@ self: super: with self; {
 
   diff-match-patch = callPackage ../development/python-modules/diff-match-patch { };
 
+  diffenator2 = callPackage ../development/python-modules/diffenator2 { };
+
   diffimg = callPackage ../development/python-modules/diffimg { };
 
   diffsync = callPackage ../development/python-modules/diffsync { };
