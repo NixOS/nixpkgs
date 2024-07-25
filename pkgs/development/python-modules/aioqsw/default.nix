d83d4b8934c10570324b14a743b4e2d151ae0af3
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-25 14:38:07 +0000

    python312Packages.aioqsw: 0.3.5 -> 0.4.0

--- pkgs/development/python-modules/aioqsw/default.nix
+++ pkgs/development/python-modules/aioqsw/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "aioqsw";
-  version = "0.3.5";
+  version = "0.4.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.11";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "Noltari";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-kSwTHwVSMGyXA0SF74f9gyBN82XhdjdN6DWrQV0PiaU=";
+    hash = "sha256-xXbOIG9gw1rO+iWdXNWWmweM83FpTLMuaDTcRZoMvL8=";
   };
 
   nativeBuildInputs = [
