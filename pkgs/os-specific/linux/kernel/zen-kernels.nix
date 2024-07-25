086f74d77acaa02565095b3f4fbe2e6af5eebc59
JerrySM64 <42114389+JerrySM64@users.noreply.github.com>
2024-07-25 16:09:55 +0200

    linuxKernel.kernels.linux_zen: 6.9.8-zen1 -> 6.10.1-zen1
    (cherry picked from commit 83431a5831471e975c4db5a3b30b530a7191795c)

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -4,9 +4,9 @@ let
   # comments with variant added for update script
   # ./update-zen.py zen
   zenVariant = {
-    version = "6.9.8"; #zen
+    version = "6.10.1"; #zen
     suffix = "zen1"; #zen
-    sha256 = "1ixrdx2a6jp1x8kryjmxnrgl0fsrjg6fngg5c48vbl2574nizlbz"; #zen
+    sha256 = "0lr9qjz4hlvx3yc0lj65fnmbciyh6symycbi9ass761l1niswbk5"; #zen
     isLqx = false;
   };
   # ./update-zen.py lqx
