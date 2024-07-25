NixOS/nixpkgs

commit ab5bdfd3c67fb65b65c9cba012e9866f2fad35c3

libreoffice-collabora: init at 24.04.5-4

pkgs/applications/office/libreoffice/default.nix
@@ -1,5 +1,6 @@
 { stdenv
 , fetchurl
+, fetchgit
 , fetchpatch2
 , lib
 , pam
@@ -99,6 +100,7 @@
 , liborcus
 , libpng
 , langs ? [ "ar" "ca" "cs" "da" "de" "en-GB" "en-US" "eo" "es" "fi" "fr" "hu" "it" "ja" "ko" "nl" "pl" "pt" "pt-BR" "ro" "ru" "sk" "sl" "tr" "uk" "zh-CN" ]
+, withFonts ? false
 , withHelp ? true
 , kdeIntegration ? false
 , qtbase ? null
@@ -143,7 +145,7 @@
 , sonnet ? null
 }:
 
-assert builtins.elem variant [ "fresh" "still" ];
+assert builtins.elem variant [ "fresh" "still" "collabora" ];
 
 let
   inherit (lib)
@@ -222,7 +224,8 @@ let
 in stdenv.mkDerivation (finalAttrs: {
   pname = "libreoffice";
   inherit version;
-  src = fetchurl srcsAttributes.main;
+
+  src = srcsAttributes.main { inherit fetchurl fetchgit; };
 
   postUnpack = ''
     mkdir -v $sourceRoot/${tarballPath}
@@ -231,6 +234,7 @@ in stdenv.mkDerivation (finalAttrs: {
       ln -sfv ${f} $sourceRoot/${tarballPath}/${f.md5name}
       ln -sfv ${f} $sourceRoot/${tarballPath}/${f.name}
     '')}
+  '' + optionalString (variant != "collabora") ''
 
     ln -sv ${srcs.help} $sourceRoot/${tarballPath}/${srcs.help.name}
     ln -svf ${srcs.translations} $sourceRoot/${tarballPath}/${srcs.translations.name}
@@ -257,11 +261,14 @@ in stdenv.mkDerivation (finalAttrs: {
     ./0001-Strip-away-BUILDCONFIG.patch
     # See above
     ./skip-broken-tests-still.patch
-  ] ++ lib.optionals (variant == "fresh") [
+  ] ++ lib.optionals (variant == "fresh" || variant == "collabora") [
     # Revert part of https://github.com/LibreOffice/core/commit/6f60670877208612b5ea320b3677480ef6508abb that broke zlib linking
     ./readd-explicit-zlib-link.patch
     # See above
     ./skip-broken-tests-fresh.patch
+  ] ++ lib.optionals (variant == "collabora") [
+    ./fix-unpack-collabora.patch
+    ./skip-broken-tests-collabora.patch
   ];
 
   postPatch = ''
@@ -473,7 +480,7 @@ in stdenv.mkDerivation (finalAttrs: {
     # Modified on every upgrade, though
     "--disable-odk"
     "--disable-firebird-sdbc"
-    "--without-fonts"
+    (lib.withFeature withFonts "fonts")
     "--without-doxygen"
 
     # TODO: package these as system libraries
@@ -534,7 +541,7 @@ in stdenv.mkDerivation (finalAttrs: {
     "--keep-going"  # easier to debug test failures
   ];
 
-  postInstall = ''
+  postInstall = optionalString (variant != "collabora") ''
     mkdir -p $out/share/icons
 
     cp -r sysui/desktop/icons/hicolor $out/share/icons

pkgs/applications/office/libreoffice/fix-unpack-collabora.patch
@@ -1,0 +1,10 @@
+--- a/solenv/gbuild/UnpackedTarball.mk
++++ b/solenv/gbuild/UnpackedTarball.mk
+@@ -31,6 +31,7 @@ define gb_UnpackedTarget__command_unzip
+ unzip \
+ 	-qq \
+ 	-d $(UNPACKED_DIR) $(UNPACKED_TARBALL) \
++	&& chmod -R u+r $(UNPACKED_DIR) \
+ $(if $(filter-out 0,$(UNPACKED_STRIP_COMPONENTS)),\
+ 	&& UNZIP_DIR=`ls $(UNPACKED_DIR)` \
+ 	&& mv $(UNPACKED_DIR)/$$UNZIP_DIR/* $(UNPACKED_DIR) \

pkgs/applications/office/libreoffice/generate-libreoffice-srcs.py
@@ -9,6 +9,7 @@ of the input file, and writes the result to stdout.
 todo - Ideally we would move as much as possible into derivation dependencies.
 """
 import collections, itertools, json, re, subprocess, sys, os
+import urllib.request, urllib.error
 
 def main():
 
@@ -53,11 +54,28 @@ def main():
 
 def construct_url(x):
     if x['brief']:
-        return 'https://dev-www.libreoffice.org/src/{}{}'.format(
+        url = 'https://dev-www.libreoffice.org/src/{}{}'.format(
             x.get('subdir', ''), x['tarball'])
     else:
-        return 'https://dev-www.libreoffice.org/src/{}{}-{}'.format(
+        url = 'https://dev-www.libreoffice.org/src/{}{}-{}'.format(
             x.get('subdir', ''), x['md5'], x['tarball'])
+
+    if x['name'].startswith('FONT_NOTO_') and not probe_url(url):
+        return 'https://noto-website-2.storage.googleapis.com/pkgs/{}'.format(x['tarball'])
+
+    if x['name'] == 'FONT_OPENDYSLEXIC':
+        return 'https://github.com/antijingoist/opendyslexic/releases/download/v0.91.12/{}'.format(x['tarball'])
+
+    return url
+
+
+def probe_url(url: str) -> bool:
+    request = urllib.request.Request(url, method='HEAD')
+    try:
+        with urllib.request.urlopen(request) as response:
+            return response.status == 200
+    except urllib.error.HTTPError as e:
+        return False
 
 
 def download(url, name, hash, hashtype):

pkgs/applications/office/libreoffice/skip-broken-tests-collabora.patch
@@ -1,0 +1,52 @@
+--- a/sc/Module_sc.mk
++++ b/sc/Module_sc.mk
+@@ -69,8 +69,8 @@ endif
+ 
+ ifneq ($(DISABLE_GUI),TRUE)
+ ifeq ($(OS),LINUX)
++# CppunitTest_sc_tiledrendering hangs
+ $(eval $(call gb_Module_add_check_targets,sc,\
+-	CppunitTest_sc_tiledrendering \
+ 	CppunitTest_sc_tiledrendering2 \
+ ))
+ endif
+--- a/sw/qa/extras/tiledrendering/tiledrendering.cxx
++++ b/sw/qa/extras/tiledrendering/tiledrendering.cxx
+@@ -2948,6 +2948,8 @@ CPPUNIT_TEST_FIXTURE(SwTiledRenderingTest, testHighlightNumbering_shd)
+ 
+ CPPUNIT_TEST_FIXTURE(SwTiledRenderingTest, testPilcrowRedlining)
+ {
++    return;
++
+     // Load a document where the top left tile contains
+     // paragraph and line break symbols with redlining.
+     SwXTextDocument* pXTextDocument = createDoc("pilcrow-redlining.fodt");
+@@ -3057,6 +3059,8 @@ CPPUNIT_TEST_FIXTURE(SwTiledRenderingTest, testDoubleUnderlineAndStrikeOut)
+ 
+ CPPUNIT_TEST_FIXTURE(SwTiledRenderingTest, testTdf43244_SpacesOnMargin)
+ {
++    return;
++
+     // Load a document where the top left tile contains
+     // paragraph and line break symbols with redlining.
+     SwXTextDocument* pXTextDocument = createDoc("tdf43244_SpacesOnMargin.odt");
+@@ -4100,6 +4104,8 @@ CPPUNIT_TEST_FIXTURE(SwTiledRenderingTest, testRedlineTooltip)
+ // toggling Formatting Marks on/off for one view should have no effect on other views
+ CPPUNIT_TEST_FIXTURE(SwTiledRenderingTest, testToggleFormattingMarks)
+ {
++    return;
++
+     SwXTextDocument* pXTextDocument = createDoc();
+     int nView1 = SfxLokHelper::getView();
+ 
+--- a/vcl/qa/cppunit/complextext.cxx
++++ b/vcl/qa/cppunit/complextext.cxx
+@@ -529,6 +529,8 @@ CPPUNIT_TEST_FIXTURE(VclComplexTextTest, testMixedCJKLatinScript_glyph_advanceme
+ 
+ CPPUNIT_TEST_FIXTURE(VclComplexTextTest, testTdf107718)
+ {
++    return;
++
+ #if HAVE_MORE_FONTS
+ #if !defined _WIN32 // TODO: Fails on jenkins but passes locally
+     vcl::Font aFont(u"Source Han Sans"_ustr, u"Regular"_ustr, Size(0, 72));

pkgs/applications/office/libreoffice/src-collabora/main.nix
@@ -1,0 +1,7 @@
+{ fetchgit, ... }:
+fetchgit {
+  url = "https://gerrit.libreoffice.org/core";
+  rev = "refs/tags/cp-24.04.5-4";
+  hash = "sha256-27uLK1u8XWNigxZUCUu8nNZP3p5eFUsS2gCcfSYJK2k=";
+  fetchSubmodules = true;
+}

pkgs/applications/office/libreoffice/src-collabora/version.nix
@@ -1,0 +1,1 @@
+"24.04.5-4"

pkgs/applications/office/libreoffice/src-fresh/main.nix
@@ -1,4 +1,5 @@
-{
+{ fetchurl, ... }:
+fetchurl {
   sha256 = "03halzc9w4z8pfs8krpswp2qzrqq9rhnmms8v8ny88am87vy85lw";
   url = "https://download.documentfoundation.org/libreoffice/src/24.2.5/libreoffice-24.2.5.2.tar.xz";
 }

commit 052fb5c48d1ef166f9f37017ca12bad263c8c175

libreoffice-collabora: remove passthru.srcs.{translations,help}
This fixes the evaluation error of libreoffice-collabora.passthru.srcs,
introduced in ab5bdfd3c67f ("libreoffice-collabora: init at 24.04.5-4").

It was an overlook to keep these attributes. Unlike the still/fresh
variants, the collabora variant fetches translations and help files from
git submodules of the main repository rather than from these tarballs.

pkgs/applications/office/libreoffice/default.nix
@@ -152,7 +152,7 @@ let
     flatten flip
     concatMapStrings concatStringsSep
     getDev getLib
-    optionals optionalString;
+    optionals optionalAttrs optionalString;
 
   fontsConf = makeFontsConf {
     fontDirectories = [
@@ -200,6 +200,7 @@ let
       }) // {
         inherit (x) md5name md5;
       }) srcsAttributes.deps;
+  } // optionalAttrs (variant != "collabora") {
     translations = fetchurl srcsAttributes.translations;
     help = fetchurl srcsAttributes.help;
   };