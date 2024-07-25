81a1e0392be0d374675207a29fe0ae7ef5f415f8
Sigmanificient <edhyjox@gmail.com>
2024-07-25 18:54:01 +0200

    python3Packages.geoparquet: init at 0.7.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4851,6 +4851,8 @@ self: super: with self; {
 
   geopandas = callPackage ../development/python-modules/geopandas { };
 
+  geoparquet = callPackage  ../development/python-modules/geoparquet { };
+
   geopy = callPackage ../development/python-modules/geopy { };
 
   georss-client = callPackage ../development/python-modules/georss-client { };
