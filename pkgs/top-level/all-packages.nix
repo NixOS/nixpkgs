f3edb08a2880a070401df9f9285ec03de6f4f1a7
zimbatm <zimbatm@zimbatm.com>
2024-07-25 22:05:04 +0200

    aws-vault: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3304,8 +3304,6 @@ with pkgs;
 
   aws-sso-creds = callPackage ../tools/admin/aws-sso-creds { };
 
-  aws-vault = callPackage ../tools/admin/aws-vault { };
-
   iamy = callPackage ../tools/admin/iamy { };
 
   iam-policy-json-to-terraform = callPackage ../tools/misc/iam-policy-json-to-terraform { };
