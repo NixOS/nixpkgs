eed29b0b0d03004e6a355d5e326d524fcd4e3431
Ben Darwin <bcdarwin@gmail.com>
2024-07-25 12:20:55 -0400

    python311Packages.fslpy: init at 3.21.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4663,6 +4663,8 @@ self: super: with self; {
 
   fschat = callPackage ../development/python-modules/fschat { };
 
+  fslpy = callPackage ../development/python-modules/fslpy { };
+
   fsspec-xrootd = callPackage ../development/python-modules/fsspec-xrootd { };
 
   fsspec = callPackage ../development/python-modules/fsspec { };
