4baaec994f63fc29b7c18543bb39b09bdd241cd4
21CSM <81891917+21CSM@users.noreply.github.com>
2024-07-25 03:27:27 -0400

    tesseract: 5.3.4 -> 5.4.1
    Diff: https://github.com/tesseract-ocr/tesseract/compare/5.3.4...5.4.1
    
    Changelog: https://github.com/tesseract-ocr/tesseract/blob/5.4.1/ChangeLog

--- pkgs/applications/graphics/tesseract/tesseract5.nix
+++ pkgs/applications/graphics/tesseract/tesseract5.nix
@@ -1,17 +1,29 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook, pkg-config
-, curl, leptonica, libarchive, libpng, libtiff, icu, pango, opencl-headers
-, Accelerate, CoreGraphics, CoreVideo
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoreconfHook
+, pkg-config
+, curl
+, leptonica
+, libarchive
+, libpng
+, libtiff
+, icu
+, pango
+, Accelerate
+, CoreGraphics
+, CoreVideo
 }:
 
 stdenv.mkDerivation rec {
   pname = "tesseract";
-  version = "5.3.4";
+  version = "5.4.1";
 
   src = fetchFromGitHub {
     owner = "tesseract-ocr";
     repo = "tesseract";
     rev = version;
-    sha256 = "sha256-IKxzDhSM+BPsKyQP3mADAkpRSGHs4OmdFIA+Txt084M=";
+    hash = "sha256-Yce9DVt1RJZkwN7ZlUE57eHm+cB9z7MbdFv8uCiGapo=";
   };
 
   enableParallelBuilding = true;
@@ -29,7 +41,6 @@ stdenv.mkDerivation rec {
     libtiff
     icu
     pango
-    opencl-headers
   ] ++ lib.optionals stdenv.isDarwin [
     Accelerate
     CoreGraphics
@@ -40,7 +51,7 @@ stdenv.mkDerivation rec {
     description = "OCR engine";
     homepage = "https://github.com/tesseract-ocr/tesseract";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ anselmschueler ];
+    maintainers = with lib.maintainers; [ anselmschueler _21CSM ];
     platforms = lib.platforms.unix;
     mainProgram = "tesseract";
   };
