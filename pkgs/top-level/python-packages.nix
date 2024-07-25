fada29bff667770f2fbf64018c73e54d58cd09a1
Sigmanificient <edhyjox@gmail.com>
2024-07-25 22:17:29 +0200

    python311Packages.pygogo: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11388,8 +11388,6 @@ self: super: with self; {
 
   pygobject-stubs = callPackage ../development/python-modules/pygobject-stubs { };
 
-  pygogo = callPackage ../development/python-modules/pygogo { };
-
   pygpgme = callPackage ../development/python-modules/pygpgme { };
 
   pygraphviz = callPackage ../development/python-modules/pygraphviz {
