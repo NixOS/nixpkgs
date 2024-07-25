27bffc8cc2a116755172ebe32ecaba1f1f67586a
Sigmanificient <edhyjox@gmail.com>
2024-07-25 23:52:56 +0200

    python312Packages.selectors2: drop

--- pkgs/top-level/python-aliases.nix
+++ pkgs/top-level/python-aliases.nix
@@ -521,6 +521,7 @@ mapAliases ({
   scikitimage = scikit-image; # added 2023-05-14
   scikitlearn = scikit-learn; # added 2021-07-21
   scikits-samplerate = throw "scikits-samplerate has been removed, it was unsed and unmaintained since 2015"; # added 2024-05-23
+  selectors2 = throw "selectors2 has been removed: archived by upstream."; # added 2024-07-27
   selectors34 = throw "selectors34 has been removed: functionality provided by Python itself; archived by upstream."; # added 2021-06-10
   sequoia = throw "python3Packages.sequoia was replaced by pysequoia - built from a dedicated repository, with a new API."; # added 2023-06-24
   setuptools_dso = setuptools-dso; # added 2024-03-03
