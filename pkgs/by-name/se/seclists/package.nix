34e70823e1fd8f04b62f4149a065f1df8dd9184e
D3vil0p3r <vozaanthony@gmail.com>
2024-07-25 16:45:17 +0200

    seclists: add archiveExtract option

--- pkgs/by-name/se/seclists/package.nix
+++ pkgs/by-name/se/seclists/package.nix
@@ -1,34 +1,103 @@
-{ lib
-, fetchFromGitHub
-, stdenvNoCC
+{
+  lib,
+  fetchFromGitHub,
+  stdenvNoCC,
+  archiveExtract ? [ ],
+  unzip,
 }:
 
-stdenvNoCC.mkDerivation {
+let
   pname = "seclists";
-  version = "2024.2";
+  archiveExtractList = [
+    "phpinfo"
+    "rockyou"
+    "rockyou-withcount"
+    "top-domains-alexa"
+    "top-domains-majestic"
+    "all"
+  ];
+in
+lib.checkListOfEnum "${pname}: archiveExtract" archiveExtractList archiveExtract
 
-  src = fetchFromGitHub {
-    owner = "danielmiessler";
-    repo = "SecLists";
-    rev = "2024.2";
-    hash = "sha256-qqXOLuZOj+mF7kXrdO62HZTrGsyepOSWr5v6j4WFGcc=";
-  };
+  stdenvNoCC.mkDerivation (finalAttrs: {
+    inherit pname;
+    version = "2024.3";
 
-  installPhase = ''
-    runHook preInstall
+    src = fetchFromGitHub {
+      owner = "danielmiessler";
+      repo = "SecLists";
+      rev = "${finalAttrs.version}";
+      hash = "sha256-Ffd4jpV8F6rtMQVqsu+8X/QU5rwbKXv0zkOCmUuhP8I=";
+    };
 
-    mkdir -p $out/share/wordlists/seclists
-    find . -maxdepth 1 -type d -regextype posix-extended -regex '^./[A-Z].*' -exec cp -R {} $out/share/wordlists/seclists \;
-    find $out/share/wordlists/seclists -name "*.md" -delete
+    nativeBuildInputs = [ unzip ];
 
-    runHook postInstall
-  '';
+    installPhase = ''
+      runHook preInstall
 
-  meta = with lib; {
-    description = "Collection of multiple types of lists used during security assessments, collected in one place";
-    homepage = "https://github.com/danielmiessler/seclists";
-    license = licenses.mit;
-    maintainers = with maintainers; [ tochiaha pamplemousse ];
-  };
-}
+      mkdir -p $out/share/wordlists/seclists
+      ${lib.optionalString (lib.elem "phpinfo" archiveExtract) ''
+        unzip "Payloads/PHPInfo.zip" -d "./Payloads"
+        rm "./Payloads/PHPInfo.zip"
+      ''}
 
+      ${lib.optionalString (lib.elem "rockyou" archiveExtract) ''
+        tar -zxvf "Passwords/Leaked-Databases/rockyou.txt.tar.gz" -C "./Passwords/Leaked-Databases"
+        rm "./Passwords/Leaked-Databases/rockyou.txt.tar.gz"
+      ''}
+
+      ${lib.optionalString (lib.elem "rockyou-withcount" archiveExtract) ''
+        tar -zxvf "Passwords/Leaked-Databases/rockyou-withcount.txt.tar.gz" -C "./Passwords/Leaked-Databases"
+        rm "./Passwords/Leaked-Databases/rockyou-withcount.txt.tar.gz"
+      ''}
+
+      ${lib.optionalString (lib.elem "top-domains-alexa" archiveExtract) ''
+        unzip "Miscellaneous/top-domains-alexa.csv.zip" -d "./Miscellaneous"
+        rm "Miscellaneous/top-domains-alexa.csv.zip"
+      ''}
+
+      ${lib.optionalString (lib.elem "top-domains-majestic" archiveExtract) ''
+        unzip "Miscellaneous/top-domains-majestic.csv.zip" -d "./Miscellaneous"
+        rm "Miscellaneous/top-domains-majestic.csv.zip"
+      ''}
+
+      ${lib.optionalString (archiveExtract == [ "all" ]) ''
+        DIRS=(
+            "./Passwords/Leaked-Databases"
+            "./Payloads"
+            "./Miscellaneous"
+        )
+        for DIR in "''${DIRS[@]}"; do
+          for file in "$DIR"/*.{tar.gz,zip}; do
+            echo "File $file"
+            if [[ -f "$file" ]]; then
+              case "$file" in
+                *.tar.gz)
+                  tar -zxvf "$file" -C "$DIR"
+                  ;;
+                *.zip)
+                  unzip "$file" -d "$DIR"
+                  ;;
+              esac
+              rm "$file"
+            fi
+          done
+        done
+      ''}
+      find . -maxdepth 1 -type d -regextype posix-extended -regex '^./[A-Z].*' -exec cp -R {} $out/share/wordlists/seclists \;
+      find $out/share/wordlists/seclists -name "*.md" -delete
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Collection of multiple types of lists used during security assessments, collected in one place";
+      homepage = "https://github.com/danielmiessler/seclists";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [
+        tochiaha
+        pamplemousse
+        d3vil0p3r
+      ];
+    };
+  })
