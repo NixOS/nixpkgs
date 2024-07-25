caa12efaf425a4b5fd083daf96401e002e10869a
zimbatm <zimbatm@zimbatm.com>
2024-07-25 21:19:35 +0200

    keycard-cli: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -40132,8 +40132,6 @@ with pkgs;
 
   coz = callPackage ../development/tools/analysis/coz { };
 
-  keycard-cli = callPackage ../tools/security/keycard-cli { };
-
   sieveshell = with python3.pkgs; toPythonApplication managesieve;
 
   gortr = callPackage ../servers/gortr { };
