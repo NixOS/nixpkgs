9264afd8f0f96c69d4479cf136c81aa39066a356
jopejoe1 <johannes@joens.email>
2024-07-26 08:09:24 +0200

    python3Packages.gftools: init at 0.9.68

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4979,6 +4979,8 @@ self: super: with self; {
 
   gfsubsets = callPackage ../development/python-modules/gfsubsets { };
 
+  gftools = callPackage ../development/python-modules/gftools { };
+
   gguf = callPackage ../development/python-modules/gguf { };
 
   ghapi = callPackage ../development/python-modules/ghapi { };
