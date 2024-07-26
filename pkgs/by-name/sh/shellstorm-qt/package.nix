283b20e6ec16fcb2d86cda84917e7e0e57285904
Malzoro <bitbunny@skiff.com>
2024-07-26 05:15:08 -0400

    fixed package structure

--- pkgs/by-name/sh/shellstorm-qt/package.nix
+++ pkgs/by-name/sh/shellstorm-qt/package.nix
@@ -1,4 +1,9 @@
-{ stdenv, fetchFromGitHub, cmake, qt6 }:
+{
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  qt6,
+}:
 
 stdenv.mkDerivation {
   pname = "shellstorm-qt";
@@ -7,19 +12,24 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "algoatson";
     repo = "shellstorm-qt";
-    rev =
-      "b8bc420773fa08caa7846b75fb388b9fae6cd8ec"; # or another commit, tag, or branch
+    rev = "b8bc420773fa08caa7846b75fb388b9fae6cd8ec"; # or another commit, tag, or branch
     sha256 = "sha256-KbdqGVqOz8CLPBOGiC5qsOpLW4Rzoalnsl0l55afJ7s";
   };
 
-  buildInputs = [ qt6.qtbase qt6.full ];
+  buildInputs = [
+    qt6.qtbase
+    qt6.full
+  ];
 
   configurePhase = ''
     cd src
     cmake .
   '';
 
-  nativeBuildInputs = [ cmake qt6.wrapQtAppsHook ];
+  nativeBuildInputs = [
+    cmake
+    qt6.wrapQtAppsHook
+  ];
 
   buildPhase = ''
     cmake --build . --config Release
