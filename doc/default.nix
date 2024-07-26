87b8931d74d9e744a16a00503003a46584f03b4a
Philip Taron <philip.taron@gmail.com>
2024-07-26 10:50:26 -0700

    doc: make ./doc-support/lib-function-docs.nix callPackage style
    Move `libsets` over to it, since it's the only user.
    
    Format with `nixfmt` since we're changing it so dramatically.

--- doc/default.nix
+++ doc/default.nix
@@ -1,34 +1,13 @@
 { pkgs ? (import ./.. { }), nixpkgs ? { }}:
 let
-  inherit (pkgs) lib;
+  inherit (pkgs) lib callPackage;
   inherit (lib) hasPrefix removePrefix;
   fs = lib.fileset;
 
   common = import ./common.nix;
 
-  lib-docs = import ./doc-support/lib-function-docs.nix {
-    inherit pkgs nixpkgs;
-    libsets = [
-      { name = "asserts"; description = "assertion functions"; }
-      { name = "attrsets"; description = "attribute set functions"; }
-      { name = "strings"; description = "string manipulation functions"; }
-      { name = "versions"; description = "version string functions"; }
-      { name = "trivial"; description = "miscellaneous functions"; }
-      { name = "fixedPoints"; baseName = "fixed-points"; description = "explicit recursion functions"; }
-      { name = "lists"; description = "list manipulation functions"; }
-      { name = "debug"; description = "debugging functions"; }
-      { name = "options"; description = "NixOS / nixpkgs option handling"; }
-      { name = "path"; description = "path functions"; }
-      { name = "filesystem"; description = "filesystem functions"; }
-      { name = "fileset"; description = "file set functions"; }
-      { name = "sources"; description = "source filtering functions"; }
-      { name = "cli"; description = "command-line serialization functions"; }
-      { name = "generators"; description = "functions that create file formats from nix data structures"; }
-      { name = "gvariant"; description = "GVariant formatted string serialization functions"; }
-      { name = "customisation"; description = "Functions to customise (derivation-related) functions, derivatons, or attribute sets"; }
-      { name = "meta"; description = "functions for derivation metadata"; }
-      { name = "derivations"; description = "miscellaneous derivation-specific functions"; }
-    ];
+  lib-docs = callPackage ./doc-support/lib-function-docs.nix {
+    inherit nixpkgs;
   };
 
   epub = pkgs.runCommand "manual.epub" {
