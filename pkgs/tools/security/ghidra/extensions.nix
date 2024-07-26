57a7c134de9012f5ad8841227285c19d3a106d77
vringar <git@zabka.it>
2024-07-26 19:52:35 +0200

    ghidra: chore run nixfmt-rfc-style on ghidra dir

--- pkgs/tools/security/ghidra/extensions.nix
+++ pkgs/tools/security/ghidra/extensions.nix
@@ -1,7 +1,15 @@
-{ lib, newScope, callPackage, ghidra }:
+{
+  lib,
+  newScope,
+  callPackage,
+  ghidra,
+}:
 
 lib.makeScope newScope (self: {
-  inherit (callPackage ./build-extension.nix { inherit ghidra; }) buildGhidraExtension buildGhidraScripts;
+  inherit (callPackage ./build-extension.nix { inherit ghidra; })
+    buildGhidraExtension
+    buildGhidraScripts
+    ;
 
   ghidraninja-ghidra-scripts = self.callPackage ./extensions/ghidraninja-ghidra-scripts { };
 
