0a81914bcba4c45a035792221446575db1149fba
rewine <luhongxu@deepin.org>
2024-07-26 21:57:32 +0800

    deepin.util-dfm: 1.2.24 -> 1.3.2

--- pkgs/desktops/deepin/library/util-dfm/default.nix
+++ pkgs/desktops/deepin/library/util-dfm/default.nix
@@ -3,7 +3,7 @@
 , fetchFromGitHub
 , cmake
 , pkg-config
-, qtbase
+, libsForQt5
 , libmediainfo
 , libsecret
 , libisoburn
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "util-dfm";
-  version = "1.2.24";
+  version = "1.3.2";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    hash = "sha256-7MuQVkb+xRMtcbB5PQBv6Au/i3ONA0c1whnZ9bMrA/8=";
+    hash = "sha256-ngDjjdwuYqvyhaUcMNV5PRmGKC3lmY/nJQGOQgRMIQE=";
   };
 
   nativeBuildInputs = [
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
   dontWrapQtApps = true;
 
   buildInputs = [
-    qtbase
+    libsForQt5.qtbase
     libmediainfo
     libsecret
     libuuid
