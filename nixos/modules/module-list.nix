04d010a9af295302194c4e26b6612a78e8a5dd6a
K900 <me@0upti.me>
2024-07-26 16:06:48 +0300

    nixos/speechd: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -356,6 +356,7 @@
   ./security/systemd-confinement.nix
   ./security/tpm2.nix
   ./security/wrappers/default.nix
+  ./services/accessibility/speechd.nix
   ./services/admin/docuum.nix
   ./services/admin/meshcentral.nix
   ./services/admin/oxidized.nix
