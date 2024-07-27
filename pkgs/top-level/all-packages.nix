43b1a6e12cfe2fc4b4e165f740f5254c685abd25
Tungsten842 <886724vf@anonaddy.me>
2024-07-27 12:40:47 +0200

    btop: add rocm version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25564,6 +25564,7 @@ with pkgs;
   };
 
   btop = darwin.apple_sdk_11_0.callPackage ../tools/system/btop { };
+  btop-rocm = btop.override { rocmSupport = true; };
 
   nmon = callPackage ../os-specific/linux/nmon { };
 
