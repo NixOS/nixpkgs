aea895e0581f1232401dc73959548cd3054cba68
melvyn <melvyn2@dnsense.pub>
2024-07-27 13:56:33 -0700

    slskd: 0.21.1 -> 0.21.3
    (cherry picked from commit 90de5096f6024e4825ce69365d617a9d1e0eda53)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26602,8 +26602,6 @@ with pkgs;
 
   shiori = callPackage ../servers/web-apps/shiori { };
 
-  slskd = callPackage ../servers/web-apps/slskd { };
-
   inherit (callPackages ../servers/web-apps/matomo {})
     matomo
     matomo_5
