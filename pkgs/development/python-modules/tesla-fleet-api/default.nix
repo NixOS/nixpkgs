0963d010469f35c3d8f4cb7003fda2b04707ed12
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-27 08:25:03 +0000

    python312Packages.tesla-fleet-api: 0.7.2 -> 0.7.3

--- pkgs/development/python-modules/tesla-fleet-api/default.nix
+++ pkgs/development/python-modules/tesla-fleet-api/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "tesla-fleet-api";
-  version = "0.7.2";
+  version = "0.7.3";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "Teslemetry";
     repo = "python-tesla-fleet-api";
     rev = "refs/tags/v${version}";
-    hash = "sha256-BvKFRJS/0xvk++HP5Ovpzb1+phGAyTHgByqwTNwS3nQ=";
+    hash = "sha256-P5cU5TZ2zWVIkFp4USjofA6mOshG9IdjhYLXnY2z9fY=";
   };
 
   build-system = [ setuptools ];
