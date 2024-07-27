28b1d46b3d048122108132e237e7d7a0fbd62d61
Christina Sørensen <christina@cafkafk.com>
2024-07-27 14:41:29 +0200

    discord-stable: 0.0.60 -> 0.0.61
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>
    (cherry picked from commit 1606915e33a56adc3358b15ea5c4183c2f760984)

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -2,7 +2,7 @@
 let
   versions =
     if stdenv.isLinux then {
-      stable = "0.0.60";
+      stable = "0.0.61";
       ptb = "0.0.95";
       canary = "0.0.455";
       development = "0.0.24";
@@ -17,7 +17,7 @@ let
     x86_64-linux = {
       stable = fetchurl {
         url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-        hash = "sha256-hu1+/z/ZtHoobjHF+pgNm040r4LQJUTnpZ06RNERFr8=";
+        hash = "sha256-jJapVZ67nqLSNhti7LkfsGNV3JzglZjQkHhTwJvPO98=";
       };
       ptb = fetchurl {
         url = "https://dl-ptb.discordapp.net/apps/linux/${version}/discord-ptb-${version}.tar.gz";
