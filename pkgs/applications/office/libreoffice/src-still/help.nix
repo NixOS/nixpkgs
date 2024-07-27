f31b6e87b767ad8297b172271c10e83b622f23ae
Albert Safin <xzfcpw@gmail.com>
2024-07-27 13:27:00 +0000

    libreoffice-collabora: fix Hydra failure by splitting src derivation
    The Hydra build of the libreoffice-collabora derivation introduced in
    ab5bdfd3c67f ("libreoffice-collabora: init at 24.04.5-4") fails with the
    status "Output limit exceeded" [1], particularly at fetchgit of its
    sources.  Indeed, the full checkout of the git repo along with its
    submodules takes 4.2 GB which is past the 3.4 GB Hydra's max_output_size
    [2].  Unfortunately, the Collabora branch lacks the tarball releases
    like the upstream fresh/still variants, so we still have to fetch these
    submodules.  This commit gives up on the usage of fetchSubmodules and
    instead fetches the submodules using separate fetchgit calls.  This
    makes the structure of the nix expression for the collabora variant
    closer to the fresh/still variants than the initial version of the
    collabora variant.
    
    [1]: https://hydra.nixos.org/build/267358376
    [2]: https://github.com/NixOS/infra/blob/4b5dd4f974d3f707b64ad60793b8182e645631ed/build/hydra.nix#L51

--- pkgs/applications/office/libreoffice/src-still/help.nix
+++ pkgs/applications/office/libreoffice/src-still/help.nix
@@ -1,4 +1,5 @@
-{
+{ fetchurl, ... }:
+fetchurl {
   sha256 = "1l543k603mbr3rnwlnv9j52mblmvkgj9y49w4v7w3xm8b15331rs";
   url = "https://download.documentfoundation.org/libreoffice/src/7.6.7/libreoffice-help-7.6.7.2.tar.xz";
 }
