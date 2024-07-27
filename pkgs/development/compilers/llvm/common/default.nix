019d0919383f5b28f0339020bb85fb291000bf54
Tristan Ross <tristan.ross@midstall.com>
2024-07-26 21:11:17 -0700

    llvmPackages.clangUseLLVM: add --undefined-version by default
    Majority of packages with a version script have broken version scripts,
    this will likely result in us adding this flag to many packages. To keep
    things simple, just add it as a default.

--- pkgs/development/compilers/llvm/common/default.nix
+++ pkgs/development/compilers/llvm/common/default.nix
@@ -439,7 +439,8 @@ let
             ++ lib.optional stdenv.targetPlatform.isWasm "-fno-exceptions";
           nixSupport.cc-ldflags = lib.optionals (
             !stdenv.targetPlatform.isWasm && !stdenv.targetPlatform.isFreeBSD
-          ) [ "-L${targetLlvmLibraries.libunwind}/lib" ];
+          ) [ "-L${targetLlvmLibraries.libunwind}/lib" ]
+            ++ lib.optional (lib.versionAtLeast metadata.release_version "17") "--undefined-version";
         }
       );
 
@@ -619,6 +620,7 @@ let
 
       compiler-rt-no-libc = callPackage ./compiler-rt {
         patches = compiler-rtPatches;
+        doFakeLibgcc = stdenv.hostPlatform.useLLVM or false;
         stdenv =
           if stdenv.hostPlatform.useLLVM or false then
             overrideCC stdenv buildLlvmTools.clangNoCompilerRt
