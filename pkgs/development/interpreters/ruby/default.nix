3040c1ebd00c1895cea71ceea3b1e3926b3cd1b9
Peter Hoeg <peter@hoeg.com>
2024-07-27 10:31:11 +0200

    ruby: add mainProgram

--- pkgs/development/interpreters/ruby/default.nix
+++ pkgs/development/interpreters/ruby/default.nix
@@ -269,6 +269,7 @@ let
           license     = licenses.ruby;
           maintainers = with maintainers; [ manveru ];
           platforms   = platforms.all;
+          mainProgram = "ruby";
           knownVulnerabilities = op (lib.versionOlder ver.majMin "3.0") "This Ruby release has reached its end of life. See https://www.ruby-lang.org/en/downloads/branches/.";
         };
 
