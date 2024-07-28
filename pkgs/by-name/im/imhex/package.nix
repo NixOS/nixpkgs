86882f7c1c6662067efd99f9165996b8673915e0
pcasaretto <pcasaretto@gmail.com>
2024-07-28 18:17:53 -0300

    imhex: add support for darwin

--- pkgs/by-name/im/imhex/package.nix
+++ pkgs/by-name/im/imhex/package.nix
@@ -1,32 +1,43 @@
-{
-  lib,
-  stdenv,
-  cmake,
-  llvm,
-  fetchFromGitHub,
-  mbedtls,
-  gtk3,
-  pkg-config,
-  capstone,
-  dbus,
-  libGLU,
-  libGL,
-  glfw3,
-  file,
-  perl,
-  python3,
-  jansson,
-  curl,
-  fmt_8,
-  nlohmann_json,
-  yara,
-  rsync,
-  autoPatchelfHook,
+{ lib
+, stdenv
+, cmake
+, darwin
+, llvmPackages_17
+, fetchFromGitHub
+, mbedtls
+, gtk3
+, pkg-config
+, capstone
+, dbus
+, libGLU
+, libGL
+, glfw3
+, file
+, perl
+, python3
+, jansson
+, curl
+, fmt_8
+, nlohmann_json
+, yara
+, rsync
+, nix-update-script
+, autoPatchelfHook
+, makeWrapper
+, overrideSDK
 }:
 
 let
   version = "1.35.3";
   patterns_version = "1.35.3";
+
+  llvmPackages = llvmPackages_17;
+
+  stdenv' =
+    let
+      baseStdenv = if stdenv.cc.isClang then llvmPackages.stdenv else stdenv;
+    in
+    if stdenv.isDarwin then overrideSDK baseStdenv "11.0" else baseStdenv;
 
   patterns_src = fetchFromGitHub {
     name = "ImHex-Patterns-source-${patterns_version}";
@@ -37,7 +48,7 @@ let
   };
 
 in
-stdenv.mkDerivation rec {
+stdenv'.mkDerivation(finalAttrs: {
   pname = "imhex";
   inherit version;
 
@@ -46,18 +57,29 @@ stdenv.mkDerivation rec {
     fetchSubmodules = true;
     owner = "WerWolv";
     repo = "ImHex";
-    rev = "refs/tags/v${version}";
+    rev = "refs/tags/v${finalAttrs.version}";
     hash = "sha256-8vhOOHfg4D9B9yYgnGZBpcjAjuL4M4oHHax9ad5PJtA=";
   };
 
+   postPatch = lib.optionalString stdenv.isDarwin ''
+    substituteInPlace cmake/modules/PostprocessBundle.cmake \
+      --replace-fail "fixup_bundle" "#fixup_bundle"
+    substituteInPlace plugins/builtin/source/content/providers/process_memory_provider.cpp \
+      --replace-fail "mach/arm/kern_return.h" "mach/machine/kern_return.h" \
+      --replace-fail "mach/arm/vm_types.h" "mach/machine/vm_types.h"
+  '';
+
   nativeBuildInputs = [
-    autoPatchelfHook
     cmake
-    llvm
+    llvmPackages.llvm
     python3
     perl
     pkg-config
     rsync
+  ] ++ lib.optionals stdenv.isLinux [
+    autoPatchelfHook
+  ] ++ lib.optionals stdenv.isDarwin [
+    makeWrapper
   ];
 
   buildInputs = [
@@ -73,31 +95,48 @@ stdenv.mkDerivation rec {
     mbedtls
     nlohmann_json
     yara
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk_11_0.frameworks.UniformTypeIdentifiers
   ];
 
   # autoPatchelfHook only searches for *.so and *.so.*, and won't find *.hexpluglib
   # however, we will append to RUNPATH ourselves
-  autoPatchelfIgnoreMissingDeps = [ "*.hexpluglib" ];
-  appendRunpaths = [
+  autoPatchelfIgnoreMissingDeps = lib.optionals stdenv.isLinux [ "*.hexpluglib" ];
+  appendRunpaths = lib.optionals stdenv.isLinux [
     (lib.makeLibraryPath [ libGL ])
     "${placeholder "out"}/lib/imhex/plugins"
   ];
 
   cmakeFlags = [
-    "-DIMHEX_OFFLINE_BUILD=ON"
-    "-DUSE_SYSTEM_CAPSTONE=ON"
-    "-DUSE_SYSTEM_CURL=ON"
-    "-DUSE_SYSTEM_FMT=ON"
-    "-DUSE_SYSTEM_LLVM=ON"
-    "-DUSE_SYSTEM_NLOHMANN_JSON=ON"
-    "-DUSE_SYSTEM_YARA=ON"
+    (lib.cmakeBool "IMHEX_OFFLINE_BUILD" true)
+    (lib.cmakeBool "IMHEX_COMPRESS_DEBUG_INFO" false) # avoids error: cannot compress debug sections (zstd not enabled)
+    (lib.cmakeBool "IMHEX_GENERATE_PACKAGE" stdenv.isDarwin)
+    (lib.cmakeBool "USE_SYSTEM_CAPSTONE" true)
+    (lib.cmakeBool "USE_SYSTEM_CURL" true)
+    (lib.cmakeBool "USE_SYSTEM_FMT" true)
+    (lib.cmakeBool "USE_SYSTEM_LLVM" true)
+    (lib.cmakeBool "USE_SYSTEM_NLOHMANN_JSON" true)
+    (lib.cmakeBool "USE_SYSTEM_YARA" true)
   ];
 
   # rsync is used here so we can not copy the _schema.json files
-  postInstall = ''
+  postInstall = if stdenv.isLinux then ''
     mkdir -p $out/share/imhex
-    rsync -av --exclude="*_schema.json" ${patterns_src}/{constants,encodings,includes,magic,patterns} $out/share/imhex
-  '';
+    rsync -av --exclude="*_schema.json" ${patterns_src}/{constants,encodings,includes,magic,nodes,patterns} $out/share/imhex
+''
+    else if stdenv.isDarwin then ''
+    mkdir -p $out/Applications
+    mv $out/imhex.app $out/Applications
+    rsync -av --exclude="*_schema.json" ${patterns_src}/{constants,encodings,includes,magic,nodes,patterns} "$out/Applications/imhex.app/Contents/MacOS"
+    install_name_tool \
+      -change "$out/lib/libimhex.${finalAttrs.version}${stdenv.hostPlatform.extensions.sharedLibrary}" \
+      "@executable_path/../Frameworks/libimhex.${finalAttrs.version}${stdenv.hostPlatform.extensions.sharedLibrary}" \
+      "$out/Applications/imhex.app/Contents/MacOS/imhex"
+    makeWrapper "$out/Applications/imhex.app/Contents/MacOS/imhex" "$out/bin/imhex"
+  ''
+    else throw "Unsupported system";
+
+  passthru.updateScript = nix-update-script { };
 
   meta = with lib; {
     description = "Hex Editor for Reverse Engineers, Programmers and people who value their retinas when working at 3 AM";
@@ -107,6 +146,6 @@ stdenv.mkDerivation rec {
       kashw2
       cafkafk
     ];
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
-}
+})
