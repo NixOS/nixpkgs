402699d00e98907f7922986fdcd50c875a6a48fc
Nick Cao <nickcao@nichi.co>
2024-07-28 10:22:34 -0400

    nixos/networkd: add IPv6RetransmissionTimeSec option to [Network] section

--- nixos/modules/system/boot/networkd.nix
+++ nixos/modules/system/boot/networkd.nix
@@ -663,6 +663,7 @@ let
           "IPv6AcceptRA"
           "IPv6DuplicateAddressDetection"
           "IPv6HopLimit"
+          "IPv6RetransmissionTimeSec"
           "IPv4ReversePathFilter"
           "IPv4AcceptLocal"
           "IPv4RouteLocalnet"
@@ -716,6 +717,7 @@ let
         (assertMinimum "IPv6DuplicateAddressDetection" 0)
         (assertInt "IPv6HopLimit")
         (assertMinimum "IPv6HopLimit" 0)
+        (assertInt "IPv6RetransmissionTimeSec")
         (assertValueOneOf "IPv4ReversePathFilter" ["no" "strict" "loose"])
         (assertValueOneOf "IPv4AcceptLocal" boolValues)
         (assertValueOneOf "IPv4RouteLocalnet" boolValues)
