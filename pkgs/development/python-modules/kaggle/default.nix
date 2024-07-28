027037fd724728f7190d83b2265ba3afdbecec03
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-28 05:01:21 +0000

    kaggle: 1.6.14 -> 1.6.17

--- pkgs/development/python-modules/kaggle/default.nix
+++ pkgs/development/python-modules/kaggle/default.nix
@@ -14,12 +14,12 @@
 
 buildPythonPackage rec {
   pname = "kaggle";
-  version = "1.6.14";
+  version = "1.6.17";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-WDUyveyjyeDK/EkxESxnN7Xmjxh6tZ7nff/fCf3529k=";
+    hash = "sha256-Q5p96h1QOfMg/WrV7CG2iNz6cNQFy0IJW4H0HtxAG4E=";
   };
 
   propagatedBuildInputs = [
