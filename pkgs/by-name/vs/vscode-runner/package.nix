5e99dbef56a09b8eb90307751ac6f390ef8aa38e
pinage404 <pinage404@gmail.com>
2024-07-28 15:40:38 +0200

    vscode-runner: init at 1.6.1

---
+++ pkgs/by-name/vs/vscode-runner/package.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildDartApplication,
+  sqlite,
+  kdePackages,
+}:
+
+let
+  pname = "vscode-runner";
+  version = "1.6.1";
+
+  src = fetchFromGitHub {
+    owner = "Merrit";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-mDhwydAFlDcpbpmh+I2zjjuC+/5hmygFkpHSZGEpuLs=";
+  };
+in
+buildDartApplication {
+  inherit pname version src;
+
+  vendorHash = "sha256-jS4jH00uxZIX81sZQIi+s42ofmXpD4/tPMRV2heaM2U=";
+
+  pubspecLock = lib.importJSON ./pubspec.lock.json;
+
+  dartEntryPoints = {
+    "bin/vscode_runner" = "bin/vscode_runner.dart";
+  };
+
+  preFixup = ''
+    wrapProgram $out/bin/vscode_runner \
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ sqlite ]}
+  '';
+
+  postInstall = ''
+    substituteInPlace ./package/codes.merritt.vscode_runner.service \
+      --replace "Exec=" "Exec=$out/bin/vscode_runner"
+    install -D \
+      ./package/codes.merritt.vscode_runner.service \
+      $out/share/dbus-1/services/codes.merritt.vscode_runner.service
+
+    install -D \
+      ./package/plasma-runner-vscode_runner.desktop \
+      $out/share/krunner/dbusplugins/plasma-runner-vscode_runner.desktop
+  '';
+
+  meta = {
+    description = "KRunner plugin for quickly opening recent VSCode workspaces";
+    homepage = "https://github.com/Merrit/vscode-runner";
+    changelog = "https://github.com/Merrit/vscode-runner/blob/${src.rev}/CHANGELOG.md";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ lib.maintainers.pinage404 ];
+    mainProgram = "vscode_runner";
+    inherit (kdePackages.krunner.meta) platforms;
+  };
+}
