3e21c6092bb9bee76588c87b8b7225406b52ec40
Tristan Ross <tristan.ross@midstall.com>
2024-07-27 20:34:48 -0700

    pkgs/top-level/release{,-lib}.nix: enable pkgsLLVM

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -271,6 +271,7 @@ let
   #   https://github.com/NixOS/nixpkgs/pull/182058
   jobs = let
     packagePlatforms = if attrNamesOnly then id else release-lib.packagePlatforms;
+    packagePlatformsWithBadPlatforms = if attrNamesOnly then id else release-lib.packagePlatformsWithBadPlatforms;
     packageJobs = {
       haskell.compiler = packagePlatforms pkgs.haskell.compiler;
       haskellPackages = packagePlatforms pkgs.haskellPackages;
@@ -290,9 +291,12 @@ let
       idrisPackages = packagePlatforms pkgs.idrisPackages;
       agdaPackages = packagePlatforms pkgs.agdaPackages;
 
-      pkgsLLVM.stdenv = [ "x86_64-linux" "aarch64-linux" ];
       pkgsArocc.stdenv = [ "x86_64-linux" "aarch64-linux" ];
       pkgsZig.stdenv = [ "x86_64-linux" "aarch64-linux" ];
+
+      # Currently, pkgsLLVM.stdenv is a bit borked under x86_64-darwin but not aarch64-darwin.
+      pkgsLLVM = packagePlatformsWithBadPlatforms [ "aarch64-darwin" "x86_64-darwin" ] pkgs.pkgsLLVM;
+
       pkgsMusl.stdenv = [ "x86_64-linux" "aarch64-linux" ];
       pkgsStatic.stdenv = [ "x86_64-linux" "aarch64-linux" ];
 
