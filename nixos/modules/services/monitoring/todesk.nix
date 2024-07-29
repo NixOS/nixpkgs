bd2994da1b3ac9d79bc2f1de1dccd6f94e209497
wxt <3264117476@qq.com>
2024-07-29 21:53:16 +0800

    nixos/todesk: init

---
+++ nixos/modules/services/monitoring/todesk.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.services.todesk;
+in
+{
+  options = {
+    services.todesk.enable = lib.mkEnableOption "ToDesk daemon";
+    services.todesk.package = lib.mkPackageOption pkgs "todesk" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = [ cfg.package ];
+
+    systemd.services.todeskd = {
+      description = "ToDesk Daemon Service";
+
+      wantedBy = [ "multi-user.target" ];
+      wants = [
+        "network-online.target"
+        "display-manager.service"
+        "nss-lookup.target"
+      ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${cfg.package}/bin/todesk service";
+        ExecReload = "${pkgs.coreutils}/bin/kill -SIGINT $MAINPID";
+        Restart = "on-failure";
+        User = "root";
+        Group = "root";
+        WorkingDirectory = "/var/lib/todesk";
+        PrivateTmp = true;
+        StateDirectory = "todesk";
+        StateDirectoryMode = "0777"; # Desktop application read /opt/todesk/config/config.ini. Such a pain!
+        ProtectSystem = "strict";
+        ProtectHome = "read-only";
+        RemoveIPC = "yes";
+      };
+    };
+  };
+}
