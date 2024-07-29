b7fb6c386036417a48269316fc08b17745d17651
Doron Behar <doron.behar@gmail.com>
2024-07-29 23:16:58 +0300

    python312Packages.migen: unstable-2022-09-02 -> 0-unstable-2024-07-21

--- pkgs/development/python-modules/migen/default.nix
+++ pkgs/development/python-modules/migen/default.nix
@@ -2,22 +2,28 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   colorama,
+  pytestCheckHook,
 }:
 
 buildPythonPackage {
   pname = "migen";
-  version = "unstable-2022-09-02";
-  format = "setuptools";
+  version = "unstable-2024-07-21";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "m-labs";
     repo = "migen";
-    rev = "639e66f4f453438e83d86dc13491b9403bbd8ec6";
-    hash = "sha256-IPyhoFZLhY8d3jHB8jyvGdbey7V+X5eCzBZYSrJ18ec=";
+    rev = "9279e8623f8433bc4f23ac51e5e2331bfe544417";
+    hash = "sha256-z3LRhNmKZrjr6rFD0yxtccSa/SWvFIYmb+G/D5d2Jd8=";
   };
 
+  nativeBuildInputs = [ setuptools ];
+
   propagatedBuildInputs = [ colorama ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "migen" ];
 
