96eb0d2cba97a76c482bc3937ec9871eb80f395d
Pyrox <pyrox@pyrox.dev>
2024-07-29 00:25:26 -0400

    python312Packages.inotify: modernize

--- pkgs/development/python-modules/inotify/default.nix
+++ pkgs/development/python-modules/inotify/default.nix
@@ -3,19 +3,19 @@
   buildPythonPackage,
   fetchFromGitHub,
   pytestCheckHook,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "inotify";
   version = "unstable-2020-08-27";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dsoprea";
     repo = "PyInotify";
     rev = "f77596ae965e47124f38d7bd6587365924dcd8f7";
-    sha256 = "X0gu4s1R/Kg+tmf6s8SdZBab2HisJl4FxfdwKktubVc=";
-    fetchSubmodules = false;
+    hash = "sha256-X0gu4s1R/Kg+tmf6s8SdZBab2HisJl4FxfdwKktubVc=";
   };
 
   postPatch = ''
@@ -23,6 +23,8 @@ buildPythonPackage rec {
     substituteInPlace tests/test_inotify.py \
       --replace-fail "assertEquals" "assertEqual" \
   '';
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
@@ -34,10 +36,10 @@ buildPythonPackage rec {
     "test__renames"
   ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/dsoprea/PyInotify";
     description = "Monitor filesystems events on Linux platforms with inotify";
-    license = licenses.gpl2;
-    platforms = platforms.linux;
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.linux;
   };
 }
