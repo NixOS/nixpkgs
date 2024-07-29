39dacb36324ee121a53bea833fa65c136413d99c
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-29 19:38:12 +0200

    hpp-romeo: init at 6.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6300,6 +6300,13 @@ self: super: with self; {
     }
   );
 
+  hpp-romeo = toPythonModule (
+    pkgs.hpp-romeo.override {
+      pythonSupport = true;
+      python3Packages = self;
+    }
+  );
+
   hpp-universal-robot = toPythonModule (
     pkgs.hpp-universal-robot.override {
       pythonSupport = true;
