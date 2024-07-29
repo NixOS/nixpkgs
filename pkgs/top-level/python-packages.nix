e45bce4ecf9f8df8a51ab6b9d35b6df8d2537e86
Pyrox <pyrox@pyrox.dev>
2024-07-29 16:53:08 -0400

    python312Packages.html-table-parser-python3: init at 0.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5642,6 +5642,8 @@ self: super: with self; {
 
   html-sanitizer = callPackage ../development/python-modules/html-sanitizer { };
 
+  html-table-parser-python3 = callPackage ../development/python-modules/html-table-parser-python3 { };
+
   html-tag-names = callPackage ../development/python-modules/html-tag-names { };
 
   html-text = callPackage ../development/python-modules/html-text { };
