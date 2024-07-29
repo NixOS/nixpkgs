8b26db9b889bacbc51dbffa9a2e2ec81bb266c7b
adamoudad <adam.oudad@gmail.com>
2024-07-29 23:28:38 +0900

    Add hmac-sha2-512 to default Macs for OpenSSH

--- nixos/modules/services/networking/ssh/sshd.nix
+++ nixos/modules/services/networking/ssh/sshd.nix
@@ -438,6 +438,7 @@ in
                 "hmac-sha2-512-etm@openssh.com"
                 "hmac-sha2-256-etm@openssh.com"
                 "umac-128-etm@openssh.com"
+                "hmac-sha2-512"
               ];
               description = ''
                 Allowed MACs
