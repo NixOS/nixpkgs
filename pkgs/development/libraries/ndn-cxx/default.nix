5e77a9a0c3642d0623f318f41206911f255ccb77
Filippo Berto <berto.f@protonmail.com>
2024-07-29 16:07:14 +0200

    ndn-cxx: format with nixfmt-rfc-style

--- pkgs/development/libraries/ndn-cxx/default.nix
+++ pkgs/development/libraries/ndn-cxx/default.nix
@@ -1,14 +1,15 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, doxygen
-, pkg-config
-, python3
-, python3Packages
-, wafHook
-, boost
-, openssl
-, sqlite
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  doxygen,
+  pkg-config,
+  python3,
+  python3Packages,
+  wafHook,
+  boost,
+  openssl,
+  sqlite,
 }:
 
 stdenv.mkDerivation rec {
@@ -22,9 +23,19 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-u9+QxqdCET1f5B54HF+Jk/YuQvhcYWsPNIVHi5l0XTM=";
   };
 
-  nativeBuildInputs = [ doxygen pkg-config python3 python3Packages.sphinx wafHook ];
+  nativeBuildInputs = [
+    doxygen
+    pkg-config
+    python3
+    python3Packages.sphinx
+    wafHook
+  ];
 
-  buildInputs = [ boost openssl sqlite ];
+  buildInputs = [
+    boost
+    openssl
+    sqlite
+  ];
 
   wafConfigureFlags = [
     "--with-openssl=${openssl.dev}"
@@ -58,6 +69,9 @@ stdenv.mkDerivation rec {
     '';
     license = licenses.lgpl3;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ sjmackenzie bertof ];
+    maintainers = with maintainers; [
+      sjmackenzie
+      bertof
+    ];
   };
 }
