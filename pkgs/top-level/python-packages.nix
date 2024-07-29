55af7a0cdeb100391b6d4d1d2a808f9c21fb06d6
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-29 19:34:07 +0200

    hpp-corbaserver: init at 6.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7287,6 +7287,8 @@ self: super: with self; {
     }
   );
 
+  hpp-corbaserver = toPythonModule (pkgs.hpp-corbaserver.override { python3Packages = self; });
+
   hpp-environments = toPythonModule (
     pkgs.hpp-environments.override {
       pythonSupport = true;
