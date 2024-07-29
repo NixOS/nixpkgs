2a4710f92d2b416242ae45037afe770e695bcaa6
Filippo Berto <berto.f@protonmail.com>
2024-07-29 16:06:49 +0200

    ndn-cxx: 0.8.1 -> 0.9.0

--- pkgs/development/libraries/ndn-cxx/default.nix
+++ pkgs/development/libraries/ndn-cxx/default.nix
@@ -6,30 +6,30 @@
 , python3
 , python3Packages
 , wafHook
-, boost179
+, boost
 , openssl
 , sqlite
 }:
 
 stdenv.mkDerivation rec {
   pname = "ndn-cxx";
-  version = "0.8.1";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "named-data";
     repo = "ndn-cxx";
-    rev = "${pname}-${version}";
-    sha256 = "sha256-nnnxlkYVTSRB6ZcuIUDFol999+amGtqegHXK+06ITK8=";
+    rev = "ndn-cxx-${version}";
+    sha256 = "sha256-u9+QxqdCET1f5B54HF+Jk/YuQvhcYWsPNIVHi5l0XTM=";
   };
 
   nativeBuildInputs = [ doxygen pkg-config python3 python3Packages.sphinx wafHook ];
 
-  buildInputs = [ boost179 openssl sqlite ];
+  buildInputs = [ boost openssl sqlite ];
 
   wafConfigureFlags = [
     "--with-openssl=${openssl.dev}"
-    "--boost-includes=${boost179.dev}/include"
-    "--boost-libs=${boost179.out}/lib"
+    "--boost-includes=${boost.dev}/include"
+    "--boost-libs=${boost.out}/lib"
     "--with-tests"
   ];
 
