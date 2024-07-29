28e14b32a89accfba6d5314f152f0d68cd96a61a
Rafael Kraut <14234815+RafaelKr@users.noreply.github.com>
2024-07-29 18:58:01 +0200

    mailpit: 1.15.1 -> 1.19.3

--- pkgs/servers/mail/mailpit/source.nix
+++ pkgs/servers/mail/mailpit/source.nix
@@ -1,6 +1,6 @@
 {
-  version = "1.15.1";
-  hash = "sha256-5QEn4sEZgtoFrVonZsAtvzhEkxYGDEWwhJOxqwWQJTk=";
-  npmDepsHash = "sha256-5F68ia2V8mw4iPAjSoz0b8z1lplWtAg98BgDXYOmMKs=";
-  vendorHash = "sha256-e2mlOwGDU5NlKZSstHMdTidSfhNeeY6cBgtW+W9nwV8=";
+  version = "1.19.3";
+  hash = "sha256-LgKQOkkstmvbzkWtr53pNebZx2TSxJBArcchDCT7rMI=";
+  npmDepsHash = "sha256-cNLejqYjeR4GL9xXnCHe5HdONg8tYlpgJPAg3K9U+Xw=";
+  vendorHash = "sha256-kDNbYB1KzCUUn3EXt3CkfUXTIsg7VOLVtF7anzZOvYs=";
 }
