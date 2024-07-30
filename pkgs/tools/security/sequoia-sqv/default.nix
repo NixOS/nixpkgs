39ecfa62730a4e4bc1253e2c6f2b1b9f0346238d
Doron Behar <doron.behar@gmail.com>
2024-07-30 18:05:10 +0300

    sequoia-sqv: small 'with lib;' removal

--- pkgs/tools/security/sequoia-sqv/default.nix
+++ pkgs/tools/security/sequoia-sqv/default.nix
@@ -50,11 +50,11 @@ rustPlatform.buildRustPackage rec {
 
   passthru.updateScript = nix-update-script { };
 
-  meta = with lib; {
+  meta = {
     description = "Command-line OpenPGP signature verification tool";
     homepage = "https://docs.sequoia-pgp.org/sqv/";
-    license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ doronbehar ];
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ doronbehar ];
     mainProgram = "sqv";
   };
 }
