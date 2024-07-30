07db828f9643680d2ee541e51f9db1094d9bc187
Matt Sturgeon <matt@sturgeon.me.uk>
2024-07-30 18:19:05 +0100

    nexusmods-app: add long description
    Add a long description that documents whether or not RAR archive mods
    are supported and how to enable support.

--- pkgs/by-name/ne/nexusmods-app/package.nix
+++ pkgs/by-name/ne/nexusmods-app/package.nix
@@ -121,7 +121,6 @@ buildDotnetModule rec {
   };
 
   meta = {
-    description = "Game mod installer, creator and manager";
     mainProgram = "NexusMods.App";
     homepage = "https://github.com/Nexus-Mods/NexusMods.App";
     changelog = "https://github.com/Nexus-Mods/NexusMods.App/releases/tag/${src.rev}";
@@ -131,5 +130,32 @@ buildDotnetModule rec {
       MattSturgeon
     ];
     platforms = lib.platforms.linux;
+    description = "Game mod installer, creator and manager";
+    longDescription = ''
+      A mod installer, creator and manager for all your popular games.
+
+      Currently experimental and undergoing active development,
+      new releases may include breaking changes!
+
+      ${
+        if _7zz.meta.unfree then
+          ''
+            This "unfree" variant includes support for mods packaged as RAR archives.
+          ''
+        else
+          ''
+            It is strongly recommended that you use the "unfree" variant of this package,
+            which provides support for mods packaged as RAR archives.
+
+            You can also enable unrar support manually, by overriding the `_7zz` used:
+
+            ```nix
+            pkgs.nexusmods-app.override {
+              _7zz = pkgs._7zz-rar;
+            }
+            ```
+          ''
+      }
+    '';
   };
 }
