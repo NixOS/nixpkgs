7fe8d70a09306ea793e4e35516891b11bf1efd68
Taeer Bar-Yam <taeer.bar-yam@moduscreate.com>
2024-07-30 10:59:49 -0400

    add --rebuild-cmd flag

--- nixos/lib/test-driver/test_driver/driver.py
+++ nixos/lib/test-driver/test_driver/driver.py
@@ -2,6 +2,7 @@ import os
 import re
 import signal
 import subprocess
+import sys
 import tempfile
 import threading
 from contextlib import contextmanager
@@ -51,6 +52,7 @@ class Driver:
     global_timeout: int
     race_timer: threading.Timer
     logger: AbstractLogger
+    rebuild_cmd: Optional[str]
 
     def __init__(
         self,
@@ -61,12 +63,14 @@ class Driver:
         logger: AbstractLogger,
         keep_vm_state: bool = False,
         global_timeout: int = 24 * 60 * 60 * 7,
+        rebuild_cmd: Optional[str] = None,
     ):
         self.tests = tests
         self.out_dir = out_dir
         self.global_timeout = global_timeout
         self.race_timer = threading.Timer(global_timeout, self.terminate_test)
         self.logger = logger
+        self.rebuild_cmd = rebuild_cmd
 
         tmp_dir = get_tmp_dir()
 
@@ -111,17 +115,20 @@ class Driver:
                 except Exception as e:
                     self.logger.error(f"Error during cleanup of vlan{vlan.nr}: {e}")
 
-    # TODO: default to the path of this executable (need to make sure it's the wrapper and also via the symlink)
-    # TODO: handle the case of the result/ directory not just the executable
-    # TODO: check we're in interactive mode?
-    # TODO: consider renaming ()
-    # TODO: __init__ and reinit should share a bunch of code
-    def reinit(self, new_driver_cmd: str) -> None:
+    def rebuild(self, cmd: Optional[str] = None, exe: str = sys.argv[0]) -> None:
+        """
+        Only makes sense when running interactively. Rebuilds the test driver by running `cmd`, or a globally defined `driver.rebuild_cmd`. This should be the same command that built this test driver to begin with. Then uses the new driver at path `exe` to reconfigure this one and redeploy changed machines.
+        """
+
+        if cmd is None:
+            cmd = self.rebuild_cmd
+        if cmd is not None:
+            subprocess.run(cmd, shell=True, check=True)
 
         tmp_dir = get_tmp_dir()
 
         new_driver_info = subprocess.check_output(
-            [new_driver_cmd, "--internal-print-update-driver-info-and-exit"],
+            [exe, "--internal-print-update-driver-info-and-exit"],
             text=True,
         )
         (
@@ -132,11 +139,11 @@ class Driver:
         ) = new_driver_info.rstrip().split("\n")
 
         with self.logger.nested("updating machines"):
-            cmds = [
+            start_cmds = [
                 NixStartScript(start_script)
                 for start_script in start_scripts.split(" ")
             ]
-            names = [cmd.machine_name for cmd in cmds]
+            names = [start_cmd.machine_name for start_cmd in start_cmds]
 
             # delete machines that are no longer part of the test
             to_del = []
@@ -144,7 +151,7 @@ class Driver:
                 if machine.name not in names:
                     if machine.is_up():
                         self.logger.warning(
-                            f"{machine.name} removed from the test, but it's running so we're not going to shut it down automatically. Call driver.machines[\"{machine.name}\"].stop() and re-run this function to remove."
+                            f"{machine.name} removed from the test, but it's running so we're not going to shut it down automatically. Call driver.machines[\"{machine.name}\"].stop() and re-run rebuild() to remove."
                         )
                     else:
                         to_del.append(idx)
@@ -156,40 +163,42 @@ class Driver:
                 del self.machines[idx]
 
             # add and change new machines
-            for cmd in cmds:
-                existing = [m for m in self.machines if m.name == cmd.machine_name]
+            for start_cmd in start_cmds:
+                existing = [
+                    m for m in self.machines if m.name == start_cmd.machine_name
+                ]
                 if len(existing) == 0:
-                    machine = self.create_machine(cmd._cmd)
+                    machine = self.create_machine(start_cmd._cmd)
                     name = pythonize_name(machine.name)
                     globals()[name] = machine
                     self.machines.append(machine)
 
                     self.logger.info(
-                        f'new machine created {cmd.machine_name}. start it with driver.machines["{cmd.machine_name}"].start()'
+                        f'new machine created {start_cmd.machine_name}. start it with driver.machines["{start_cmd.machine_name}"].start()'
                     )
                 elif len(existing) == 1:
                     machine = existing[0]
-                    if machine.start_command._cmd == cmd._cmd:
-                        self.logger.info(f"{cmd.machine_name} unchanged.")
+                    if machine.start_command._cmd == start_cmd._cmd:
+                        self.logger.info(f"{start_cmd.machine_name} unchanged.")
                     elif not machine.booted:
-                        self.logger.info(f"{cmd.machine_name} changed.")
-                        machine.start_command = cmd
+                        self.logger.info(f"{start_cmd.machine_name} changed.")
+                        machine.start_command = start_cmd
                     else:
                         with self.logger.nested(
-                            f"{cmd.machine_name} updated. switching to new configuration"
+                            f"{start_cmd.machine_name} updated. switching to new configuration"
                         ):
                             # TODO: assuming things are at well-known paths isn't great
                             switch_cmd = (
-                                Path(cmd._cmd).parent.parent
+                                Path(start_cmd._cmd).parent.parent
                                 / "system"
                                 / "bin"
                                 / "switch-to-configuration"
                             )
                             machine.succeed(f"{switch_cmd} test")
-                            machine.start_command = cmd
+                            machine.start_command = start_cmd
                 else:
                     self.logger.warning(
-                        f"{cmd.machine_name} has multiple instances. This shouldn't be possible, but either way it's now ambiguous which to update."
+                        f"{start_cmd.machine_name} has multiple instances. This shouldn't be possible, but either way it's now ambiguous which to update."
                     )
 
         # XXX: something's going wrong here
@@ -219,7 +228,9 @@ class Driver:
         # we can't copy the existing outputs over to the new directory because there might be other files mixed in (and usually is)
         new_out_dir = Path(output_directory)
         if new_out_dir != self.out_dir:
-            self.logger.info("new output directory: `{new_out_dir}`. Already transferred files will remain in old directory `{self.out_dir}`.")
+            self.logger.info(
+                "new output directory: `{new_out_dir}`. Already transferred files will remain in old directory `{self.out_dir}`."
+            )
             self.out_dir = new_out_dir
 
     def subtest(self, name: str) -> Iterator[None]:
@@ -252,6 +263,7 @@ class Driver:
             serial_stdout_off=self.serial_stdout_off,
             serial_stdout_on=self.serial_stdout_on,
             polling_condition=self.polling_condition,
+            rebuild=self.rebuild,
             Machine=Machine,  # for typing
         )
         machine_symbols = {pythonize_name(m.name): m for m in self.machines}
