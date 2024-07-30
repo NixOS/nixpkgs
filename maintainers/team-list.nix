02c04305d56439b943aa01725f544cf7e9fe0115
Tristan Ross <tristan.ross@midstall.com>
2024-07-29 21:37:20 -0700

    maintainers/teams: add RossComputerGuy to zig

--- maintainers/team-list.nix
+++ maintainers/team-list.nix
@@ -1019,7 +1019,10 @@ with lib.maintainers;
   };
 
   zig = {
-    members = [ figsoda ];
+    members = [
+      figsoda
+      RossComputerGuy
+    ];
     scope = "Maintain the Zig compiler toolchain and nixpkgs integration.";
     shortName = "Zig";
     enableFeatureFreezePing = true;
