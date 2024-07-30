66e7d5d079a522e2777ec22c8762ea072aef5c1f
R. Ryantm <ryantm-bot@ryantm.com>
2024-07-30 00:13:26 +0000

    nextcloud-notify_push: 0.6.12 -> 0.7.0

--- pkgs/servers/nextcloud/notify_push.nix
+++ pkgs/servers/nextcloud/notify_push.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "notify_push";
-  version = "0.6.12";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "nextcloud";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-Wbrkr9DWOQpOKAp9X/PzU8alDDrDapX/1hE+ObbD/nc=";
+    hash = "sha256-QHVWiH6qkwQay0wffoyDUyVxgxzNp10ieYIsdqoEdCM=";
   };
 
-  cargoHash = "sha256-4bgbhtqdb1IVsf0yIcZOXZCVdRHjdvhZe/VCab0kuMk=";
+  cargoHash = "sha256-HwE/ql8rJcGIINy+hNnpaTFEJqbmuUDrIvVI8kCpfQ8=";
 
   passthru = rec {
     test_client = rustPlatform.buildRustPackage {
