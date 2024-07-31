c882d9b673e8d6eb6a29b0954519c72871b91e08
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-07-31 21:31:37 +0200

    nixos/screego: init module
    Add module for screego

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1464,6 +1464,7 @@
   ./services/web-apps/pretix.nix
   ./services/web-apps/prosody-filer.nix
   ./services/web-apps/rimgo.nix
+  ./services/web-apps/screego.nix
   ./services/web-apps/sftpgo.nix
   ./services/web-apps/suwayomi-server.nix
   ./services/web-apps/rss-bridge.nix
