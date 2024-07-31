49b2c51134957cdf6b8357846cd78a2ce6a928d3
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-07-31 22:58:44 +0200

    removed jre from input and ran formatter

--- pkgs/by-name/bl/bleep/package.nix
+++ pkgs/by-name/bl/bleep/package.nix
@@ -1,30 +1,30 @@
-{ stdenv
-, fetchurl
-, autoPatchelfHook
-, installShellFiles
-, makeWrapper
-, jre
-, lib
-, zlib
+{ stdenv, fetchurl, autoPatchelfHook, installShellFiles, makeWrapper, lib, zlib
 }:
 
 stdenv.mkDerivation rec {
   pname = "bleep";
   version = "0.0.5";
 
-  platform =
-    if stdenv.isLinux && stdenv.isx86_64 then "x86_64-pc-linux"
-    else if stdenv.isDarwin && stdenv.isx86_64 then "x86_64-apple-darwin"
-    else if stdenv.isDarwin && stdenv.isAarch64 then "arm64-apple-darwin"
-    else throw "unsupported platform";
+  platform = if stdenv.isLinux && stdenv.isx86_64 then
+    "x86_64-pc-linux"
+  else if stdenv.isDarwin && stdenv.isx86_64 then
+    "x86_64-apple-darwin"
+  else if stdenv.isDarwin && stdenv.isAarch64 then
+    "arm64-apple-darwin"
+  else
+    throw "unsupported platform";
 
   src = fetchurl {
-    url = "https://github.com/oyvindberg/bleep/releases/download/v${version}/bleep-${platform}.tar.gz";
-    sha256 =
-      if stdenv.isLinux && stdenv.isx86_64 then "sha256-oNowRH92i76Ubbn07fLBzzX2X9Xez1ByZ/va4+vMdP0="
-      else if stdenv.isDarwin && stdenv.isx86_64 then "sha256-XvgD7Y02hWUEJyTscQ3orOREv5pB78tTIJq0EEGVeN4="
-      else if stdenv.isDarwin && stdenv.isAarch64 then "sha256-aGXNKUcbuaz1GJIJfVU8B3YJKC24EtX7hgsEOoXKBwo="
-    else throw "unsupported platform";
+    url =
+      "https://github.com/oyvindberg/bleep/releases/download/v${version}/bleep-${platform}.tar.gz";
+    sha256 = if stdenv.isLinux && stdenv.isx86_64 then
+      "sha256-oNowRH92i76Ubbn07fLBzzX2X9Xez1ByZ/va4+vMdP0="
+    else if stdenv.isDarwin && stdenv.isx86_64 then
+      "sha256-XvgD7Y02hWUEJyTscQ3orOREv5pB78tTIJq0EEGVeN4="
+    else if stdenv.isDarwin && stdenv.isAarch64 then
+      "sha256-aGXNKUcbuaz1GJIJfVU8B3YJKC24EtX7hgsEOoXKBwo="
+    else
+      throw "unsupported platform";
   };
 
   nativeBuildInputs = [ installShellFiles makeWrapper ]
