cca6533bdff90f65946b7f64fdab4eba0f39dc8d
Cyberczy <czysheep@qq.com>
2024-07-31 21:10:55 +0800

    maintainers: add Cyberczy

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4269,6 +4269,12 @@
     githubId = 16950437;
     name = "cwyc";
   };
+  Cyberczy = {
+    email = "czysheep@gmail.com";
+    github = "Cyberczy";
+    githubId = 109653523;
+    name = "Cyberczy";
+  };
   cybershadow = {
     name = "Vladimir Panteleev";
     email = "nixpkgs@cy.md";
