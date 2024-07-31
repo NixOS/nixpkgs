6507c6d20b92268e69e29e382893ccd0d88299e9
Tristan Ross <tristan.ross@midstall.com>
2024-07-30 19:45:34 -0700

    zig.cc: add bintools

--- pkgs/development/compilers/zig/cc.nix
+++ pkgs/development/compilers/zig/cc.nix
@@ -1,7 +1,9 @@
 {
   lib,
   wrapCCWith,
+  wrapBintoolsWith,
   makeWrapper,
+  stdenv,
   runCommand,
   targetPlatform,
   targetPackages,
@@ -16,12 +18,17 @@ wrapCCWith {
 
         nativeBuildInputs = [ makeWrapper ];
 
+        outputs = [
+          "out"
+          "lib"
+        ];
+
         passthru.isZig = true;
         inherit zig;
       }
       ''
         mkdir -p $out/bin
-        for tool in ar cc c++ objcopy; do
+        for tool in cc c++; do
           makeWrapper "$zig/bin/zig" "$out/bin/$tool" \
             --add-flags "$tool" \
             --run "export ZIG_GLOBAL_CACHE_DIR=\$(mktemp -d)"
@@ -29,7 +36,42 @@ wrapCCWith {
 
         mv $out/bin/c++ $out/bin/clang++
         mv $out/bin/cc $out/bin/clang
+
+        # Fake lib output needed
+        mkdir -p $lib
       '';
+
+  bintools =
+    let
+      targetPrefix = lib.optionalString (
+        stdenv.hostPlatform != stdenv.targetPlatform
+      ) "${stdenv.targetPlatform.config}-";
+    in
+    wrapBintoolsWith {
+      bintools =
+        runCommand "zig-bintools-${zig.version}"
+          {
+            pname = "zig-bintools";
+            inherit (zig) version meta;
+
+            nativeBuildInputs = [ makeWrapper ];
+
+            passthru = {
+              isZig = true;
+              inherit targetPrefix;
+            };
+
+            inherit zig;
+          }
+          ''
+            mkdir -p $out/bin
+            for tool in ar objcopy; do
+              makeWrapper "$zig/bin/zig" "$out/bin/${targetPrefix}-$tool" \
+                --add-flags "$tool" \
+                --run "export ZIG_GLOBAL_CACHE_DIR=\$(mktemp -d)"
+            done
+          '';
+    };
 
   nixSupport.cc-cflags =
     [
