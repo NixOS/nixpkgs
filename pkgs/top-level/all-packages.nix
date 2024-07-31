9ca37768272983b28ee60cd4815b3a6fe55c3825
amesgen <amesgen@amesgen.de>
2024-07-31 22:29:18 +0200

    gh-actions-cache: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1783,8 +1783,6 @@ with pkgs;
 
   gfshare = callPackage ../tools/security/gfshare { };
 
-  gh-actions-cache = callPackage ../tools/misc/gh-actions-cache { };
-
   gh-cal = callPackage ../tools/misc/gh-cal {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
