e6552eb60f9e54cd131dc22612761889b661f727
rewine <luhongxu@deepin.org>
2024-07-31 20:40:03 +0800

    deepin.dde-appearance: 1.1.25 -> 1.1.29

--- pkgs/desktops/deepin/core/dde-appearance/default.nix
+++ pkgs/desktops/deepin/core/dde-appearance/default.nix
@@ -17,23 +17,20 @@
 
 stdenv.mkDerivation rec {
   pname = "dde-appearance";
-  version = "1.1.25";
+  version = "1.1.29";
 
   src = fetchFromGitHub {
     owner = "linuxdeepin";
     repo = pname;
     rev = version;
-    hash = "sha256-H9TvWF6Q0lX4GF4mQ71E3SUqWbhH7dzHIlScovbN7lM=";
+    hash = "sha256-M39EugV0uGCIaXK4isTQpHd6Rh2Vl6sg3Jp8JIEFEE4=";
   };
-
-  patches = [
-    ./fix-custom-wallpapers-path.diff
-  ];
 
   postPatch = ''
     substituteInPlace src/service/impl/appearancemanager.cpp \
-      src/service/modules/api/compatibleengine.cpp \
-      src/service/modules/subthemes/customtheme.cpp \
+      src/service/modules/{api/compatibleengine.cpp,subthemes/customtheme.cpp,background/backgrounds.cpp} \
+      misc/dconfig/org.deepin.dde.appearance.json \
+      fakewm/dbus/deepinwmfaker.cpp \
       --replace "/usr/share" "/run/current-system/sw/share"
 
     for file in $(grep -rl "/usr/bin/dde-appearance"); do
