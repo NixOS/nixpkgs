abb8db9c3d37372a0fed6e41f530baeb6d94c4bf
Pyrox <pyrox@pyrox.dev>
2024-07-31 17:16:43 -0400

    python312Packages.mwxml: 0.3.3 -> 0.3.4

--- pkgs/development/python-modules/mwxml/default.nix
+++ pkgs/development/python-modules/mwxml/default.nix
@@ -2,35 +2,40 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  fetchpatch2,
   jsonschema,
   mwcli,
   mwtypes,
-  nose,
   pytestCheckHook,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "mwxml";
-  version = "0.3.3";
   format = "setuptools";
+  version = "0.3.4";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-CEjfDPLik3GPVUMRrPRxW9Z59jn05Sy+R9ggZYnbHTE=";
+    hash = "sha256-ejf3RfdwcEp0Ge+96dORuHS5Bx28GSs7H4HD1LUnde4=";
   };
 
-  propagatedBuildInputs = [
+  patches = [
+    # https://github.com/mediawiki-utilities/python-mwxml/pull/21
+    (fetchpatch2 {
+      name = "nose-to-pytest.patch";
+      url = "https://github.com/mediawiki-utilities/python-mwxml/compare/2b477be6aa9794064d03b5be38c7759d1570488b...71bbfd2b309e0720a34a4e783b71169aebc571ef.patch";
+      hash = "sha256-4XxNvda1Dj+kFbD9t9gzucrMjdfXcoqYlvecXO2B2R0=";
+    })
+  ];
+
+  dependencies = [
     jsonschema
     mwcli
     mwtypes
   ];
 
-  nativeCheckInputs = [
-    nose
-    pytestCheckHook
-  ];
-
-  disabledTests = [ "test_page_with_discussion" ];
+  nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "mwxml" ];
 
