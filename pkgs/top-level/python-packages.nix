54d0da017b1d065ed6f0ff4ebf3a371a5ba718d5
Sigmanificient <edhyjox@gmail.com>
2024-07-31 22:12:49 +0200

    python312Packages.tinytag: init at 1.10.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15553,6 +15553,8 @@ self: super: with self; {
 
   tinysegmenter = callPackage ../development/python-modules/tinysegmenter { };
 
+  tinytag = callPackage ../development/python-modules/tinytag { };
+
   tinytuya = callPackage ../development/python-modules/tinytuya { };
 
   titlecase = callPackage ../development/python-modules/titlecase { };
