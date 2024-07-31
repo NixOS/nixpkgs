8c98fdc5a085003f03a755351b149faea2a8b284
Anderson Torres <torres.anderson.85@protonmail.com>
2024-07-31 11:59:09 -0300

    micro: nixfmt-rfc-style

--- pkgs/by-name/mi/micro/package.nix
+++ pkgs/by-name/mi/micro/package.nix
@@ -1,25 +1,27 @@
-{ lib
-, stdenv
-, buildGoModule
-, fetchFromGitHub
-, installShellFiles
-, callPackage
-, wl-clipboard
-, xclip
-, makeWrapper
-# Boolean flags
-, withXclip ? stdenv.isLinux
-, withWlClipboard ?
-  if withWlclip != null then
-    lib.warn ''
-      withWlclip is deprecated and will be removed;
-      use withWlClipboard instead.
-    '' withWlclip
-  else stdenv.isLinux
-# Deprecated options
-# Remove them before or right after next version update from Nixpkgs or this
-# package itself
-, withWlclip ? null
+{
+  lib,
+  stdenv,
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  callPackage,
+  wl-clipboard,
+  xclip,
+  makeWrapper,
+  # Boolean flags
+  withXclip ? stdenv.isLinux,
+  withWlClipboard ?
+    if withWlclip != null then
+      lib.warn ''
+        withWlclip is deprecated and will be removed;
+        use withWlClipboard instead.
+      '' withWlclip
+    else
+      stdenv.isLinux,
+  # Deprecated options
+  # Remove them before or right after next version update from Nixpkgs or this
+  # package itself
+  withWlclip ? null,
 }:
 
 let
@@ -36,39 +38,51 @@ let
 
     vendorHash = "sha256-ePhObvm3m/nT+7IyT0W6K+y+9UNkfd2kYjle2ffAd9Y=";
 
-    nativeBuildInputs = [ installShellFiles makeWrapper ];
+    nativeBuildInputs = [
+      installShellFiles
+      makeWrapper
+    ];
 
-    outputs = [ "out" "man" ];
+    outputs = [
+      "out"
+      "man"
+    ];
 
     subPackages = [ "cmd/micro" ];
 
-    ldflags = let t = "github.com/zyedidia/micro/v2/internal"; in [
-      "-s"
-      "-w"
-      "-X ${t}/util.Version=${self.version}"
-      "-X ${t}/util.CommitHash=${self.src.rev}"
-    ];
+    ldflags =
+      let
+        t = "github.com/zyedidia/micro/v2/internal";
+      in
+      [
+        "-s"
+        "-w"
+        "-X ${t}/util.Version=${self.version}"
+        "-X ${t}/util.CommitHash=${self.src.rev}"
+      ];
 
     strictDeps = true;
 
     preBuild = ''
       GOOS= GOARCH= go generate ./runtime
-  '';
+    '';
 
     postInstall = ''
       installManPage assets/packaging/micro.1
       install -Dm444 assets/packaging/micro.desktop $out/share/applications/micro.desktop
       install -Dm644 assets/micro-logo-mark.svg $out/share/icons/hicolor/scalable/apps/micro.svg
-  '';
+    '';
 
-    postFixup = let
-      clipboardPackages =
-        lib.optionals withXclip [ xclip ] ++ lib.optionals withWlClipboard [ wl-clipboard ];
-    in
-      lib.optionalString (withXclip || withWlClipboard)
-        ''wrapProgram "$out/bin/micro" \
-          --prefix PATH : "${lib.makeBinPath clipboardPackages}"
-        '';
+    postFixup =
+      let
+        clipboardPackages =
+          lib.optionals withXclip [ xclip ]
+          ++ lib.optionals withWlClipboard [ wl-clipboard ];
+      in
+      lib.optionalString (withXclip || withWlClipboard) ''
+        wrapProgram "$out/bin/micro" \
+                  --prefix PATH : "${lib.makeBinPath clipboardPackages}"
+      '';
 
     passthru = {
       tests = lib.packagesFromDirectoryRecursive {
@@ -89,7 +103,7 @@ let
         nano editor by being easy to install and use. It strives to be enjoyable
         as a full-time editor for people who prefer to work in a terminal, or
         those who regularly edit files over SSH.
-    '';
+      '';
       license = lib.licenses.mit;
       mainProgram = "micro";
       maintainers = with lib.maintainers; [ AndersonTorres ];
