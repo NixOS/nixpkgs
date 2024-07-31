e4f405d002ba79e048d5bc582608d54629cd01c1
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-07-31 15:44:46 +0200

    python3Packages.motor: 3.4.0 -> 3.5.1

--- pkgs/development/python-modules/motor/default.nix
+++ pkgs/development/python-modules/motor/default.nix
@@ -2,6 +2,8 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  hatchling,
+  hatch-requirements-txt,
   mockupdb,
   pymongo,
   pythonOlder,
@@ -9,19 +11,24 @@
 
 buildPythonPackage rec {
   pname = "motor";
-  version = "3.4.0";
-  format = "setuptools";
+  version = "3.5.1";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "mongodb";
-    repo = pname;
+    repo = "motor";
     rev = "refs/tags/${version}";
-    hash = "sha256-Rj8eYZxmw/cn/vkhLunmHgxMMHIQe80Zhc2p0q3b/AY=";
+    hash = "sha256-mg31FzMF0xEEhfLKAdF2pzEkULESFFGaXnE0uospXqE=";
   };
 
-  propagatedBuildInputs = [ pymongo ];
+  build-system = [
+    hatchling
+    hatch-requirements-txt
+  ];
+
+  dependencies = [ pymongo ];
 
   nativeCheckInputs = [ mockupdb ];
 
@@ -30,10 +37,10 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "motor" ];
 
-  meta = with lib; {
+  meta = {
     description = "Non-blocking MongoDB driver for Tornado or asyncio";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
     homepage = "https://github.com/mongodb/motor";
-    maintainers = with maintainers; [ globin ];
+    maintainers = with lib.maintainers; [ globin ];
   };
 }
