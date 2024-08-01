0cb1143443bdadeef1cd62f90219c2db5d898e1e
euxane <euxane.trangirard@pacien.net>
2024-08-01 07:24:53 +0200

    nixos/fcgiwrap: add deprecation notice and security warning
    This deprecates the use of the global shared instance of fcgiwrap,
    due to its security issues (running as root by default, actually
    insecure control socket, allowing local remote escalation privileges,
    with no fix due to the multiple consumers).
    
    A warning is added to encourage users to migrate to properly isolated
    instances (`services.fcgiwrap.instances.*`).

--- nixos/modules/services/web-servers/fcgiwrap.nix
+++ nixos/modules/services/web-servers/fcgiwrap.nix
@@ -4,6 +4,12 @@ with lib;
 
 let
   cfg = config.services.fcgiwrap;
+  deprecationNote = ''
+
+    This global instance option is deprecated in favour of per-instance
+    options configured through `services.fcgiwrap.instances.*`.
+  '';
+
 in {
 
   options = {
@@ -11,43 +17,52 @@ in {
       enable = mkOption {
         type = types.bool;
         default = false;
-        description = "Whether to enable fcgiwrap, a server for running CGI applications over FastCGI.";
+        description = "Whether to enable fcgiwrap, a server for running CGI applications over FastCGI." + deprecationNote;
       };
 
       preforkProcesses = mkOption {
         type = types.int;
         default = 1;
-        description = "Number of processes to prefork.";
+        description = "Number of processes to prefork." + deprecationNote;
       };
 
       socketType = mkOption {
         type = types.enum [ "unix" "tcp" "tcp6" ];
         default = "unix";
-        description = "Socket type: 'unix', 'tcp' or 'tcp6'.";
+        description = "Socket type: 'unix', 'tcp' or 'tcp6'." + deprecationNote;
       };
 
       socketAddress = mkOption {
         type = types.str;
         default = "/run/fcgiwrap.sock";
         example = "1.2.3.4:5678";
-        description = "Socket address. In case of a UNIX socket, this should be its filesystem path.";
+        description = "Socket address. In case of a UNIX socket, this should be its filesystem path." + deprecationNote;
       };
 
       user = mkOption {
         type = types.nullOr types.str;
         default = null;
-        description = "User permissions for the socket.";
+        description = "User permissions for the socket." + deprecationNote;
       };
 
       group = mkOption {
         type = types.nullOr types.str;
         default = null;
-        description = "Group permissions for the socket.";
+        description = "Group permissions for the socket." + deprecationNote;
       };
     };
   };
 
   config = mkIf cfg.enable {
+    warnings = [
+      ''
+        The fcgiwrap module is configured with a global shared instance.
+        This has security implications: <TODO: advisory link>.
+        Isolated instances should instead be configured through `services.fcgiwrap.instances.*'.
+        The global options at `services.fcgiwrap.*` will be removed in NixOS 24.11.
+      ''
+    ];
+
     systemd.services.fcgiwrap = {
       after = [ "nss-user-lookup.target" ];
       wantedBy = optional (cfg.socketType != "unix") "multi-user.target";
