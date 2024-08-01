2b2db13058b1b2eaa1cec202213ec753103766d5
dowdt <tomas.dowd@openmesh.network>
2024-08-01 10:09:32 +1000

    Updated admin service with base64 decode update, this needs to be applied for the admin service to be able to read messages from the dpl.

--- pkgs/openmesh/xnode/admin/default.nix
+++ pkgs/openmesh/xnode/admin/default.nix
@@ -2,14 +2,14 @@
 
 pkgs.python3Packages.buildPythonPackage rec {
   pname = "xnode-admin";
-  version = "0.0.2";
+  version = "0.0.3";
   format = "pyproject";
 
   src = pkgs.fetchFromGitHub {
     owner = "Openmesh-Network";
     repo = pname;
-    rev = "5017fc1dc3c046b1d6558a86a450e275e1c7641b";
-    sha256 = "1vm7lzj7k9cq5nlraqdribrihghgmj1yrvnnyj77kq73081kxlgc";
+    rev = "665debd5246b995314082330cfe16afb59d3579a";
+    sha256 = "1qmjswhs95894i6fnw5qj336w3254lhivgdqvg0anwhr3g5p7zqb";
   };
 
   nativeBuildInputs = [
