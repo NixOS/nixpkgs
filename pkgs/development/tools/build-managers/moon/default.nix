43d78ceb8783f0661cf9b1df30ccf700241f249a
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-01 11:35:46 +0000

    moon: 1.26.4 -> 1.27.5

--- pkgs/development/tools/build-managers/moon/default.nix
+++ pkgs/development/tools/build-managers/moon/default.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "moon";
-  version = "1.26.4";
+  version = "1.27.5";
 
   src = fetchFromGitHub {
     owner = "moonrepo";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-+vgP8Nu/t7pxU/YGIIi8zd0zuAzZFdpcZLGnjZgqYGM=";
+    hash = "sha256-FckWn8tHSuowhVSq1B/KqzXB4KlWMOWfmuVfKr/y7Hk=";
   };
 
-  cargoHash = "sha256-WDBAuzUCjZxayXeEdxvWAHZyYRQLDMz3QkNO9QT/DyI=";
+  cargoHash = "sha256-rES9cHwOH/Zw0fYhYplQb8FJ/iOndFgskg5zRTMeMDI=";
 
   env = {
     RUSTFLAGS = "-C strip=symbols";
