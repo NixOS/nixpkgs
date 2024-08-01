7f56ff9834f33d67e0cb9949ac28a0aa533887aa
Litchi Pi <litchi.pi@proton.me>
2024-08-01 15:15:14 +0200

    fixup format nixostest
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- nixos/tests/firefly-iii-data-importer.nix
+++ nixos/tests/firefly-iii-data-importer.nix
@@ -1,24 +1,22 @@
-import ./make-test-python.nix ({ pkgs, ...} :
+import ./make-test-python.nix ({ pkgs, ... }:
 
-{
-  name = "firefly-iii-data-importer";
-  meta = with pkgs.lib.maintainers; {
-    maintainers = [ litchipi ];
-  };
+  {
+    name = "firefly-iii-data-importer";
+    meta = with pkgs.lib.maintainers; { maintainers = [ litchipi ]; };
 
-  nodes = {
-    server = {
-      services.firefly-iii.data-importer = {
-        enable = true;
-        port = 9001;
+    nodes = {
+      server = {
+        services.firefly-iii.data-importer = {
+          enable = true;
+          port = 9001;
+        };
       };
     };
-  };
 
-  testScript = ''
-    start_all()
-    server.wait_for_unit("firefly-iii-data-importer.service")
-    server.wait_for_open_port(9001)
-    server.succeed("curl --fail http://localhost:9001")
-  '';
-})
+    testScript = ''
+      start_all()
+      server.wait_for_unit("firefly-iii-data-importer.service")
+      server.wait_for_open_port(9001)
+      server.succeed("curl --fail http://localhost:9001")
+    '';
+  })
