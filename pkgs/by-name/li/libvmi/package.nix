5ac7daf320d3a7ec2780a7df19636c3c851876db
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-08-01 19:02:19 +0000

    libvmi: 0.12.0 -> 0.14.0-unstable-2024-07-25
    Also splits the package output and adds more configuration options.
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/by-name/li/libvmi/package.nix
+++ pkgs/by-name/li/libvmi/package.nix
@@ -3,63 +3,89 @@
   stdenv,
   fetchFromGitHub,
   autoreconfHook,
+  autoconf-archive,
   bison,
   flex,
   glib,
   pkg-config,
   json_c,
-  xen,
   libvirt,
+
+  withVMIFS ? true,
+  fuse,
+
+  legacyKVM ? false,
+  libkvmi,
+
   xenSupport ? true,
+  xen-slim,
 }:
 
 stdenv.mkDerivation rec {
   pname = "libvmi";
-  version = "0.12.0";
-  libVersion = "0.0.12";
+  version = "0.14.0-unstable-2024-07-25";
+  libVersion = "0.0.15";
+
+  outputs = [
+    "out" # Contains binaries helpful for testing the libraries. Must be run on the host system.
+    "lib" # Contains the LibVMI libraries. This is most likely what you want.
+    "dev" # Development headers and the pkg-config spec.
+  ];
 
   src = fetchFromGitHub {
     owner = "libvmi";
     repo = "libvmi";
-    rev = "v${version}";
-    sha256 = "0wbi2nasb1gbci6cq23g6kq7i10rwi1y7r44rl03icr5prqjpdyv";
+    rev = "2d7258b5ab2cddea8d0889303d5f2f6f07b125b9";
+    hash = "sha256-gTNZEESHtC5K1pM9cznI3OHfq7Fg0LRrSBHybM+DTqs=";
   };
 
-  buildInputs = [
-    glib
-    libvirt
-    json_c
-  ] ++ (lib.optional xenSupport xen);
   nativeBuildInputs = [
     autoreconfHook
+    autoconf-archive
     bison
     flex
     pkg-config
   ];
 
-  configureFlags = lib.optional (!xenSupport) "--disable-xen";
+  buildInputs =
+    [
+      glib
+      json_c
+      libvirt
+    ]
+    ++ (lib.lists.optional xenSupport xen-slim)
+    ++ (lib.lists.optional (!legacyKVM) libkvmi)
+    ++ (lib.lists.optional withVMIFS fuse);
+
+  configureFlags =
+    lib.lists.optional (!xenSupport) "--disable-xen"
+    ++ (lib.lists.optional legacyKVM "--enable-kvm-legacy")
+    ++ (lib.lists.optional withVMIFS "--enable-vmifs");
 
   # libvmi uses dlopen() for the xen libraries, however autoPatchelfHook doesn't work here
-  postFixup = lib.optionalString xenSupport ''
-    libvmi="$out/lib/libvmi.so.${libVersion}"
+  postFixup = lib.strings.optionalString xenSupport ''
+    libvmi="$lib/lib/libvmi.so.${libVersion}"
     oldrpath=$(patchelf --print-rpath "$libvmi")
-    patchelf --set-rpath "$oldrpath:${lib.makeLibraryPath [ xen ]}" "$libvmi"
+    patchelf --set-rpath "$oldrpath:${lib.makeLibraryPath [ xen-slim ]}" "$libvmi"
   '';
 
-  meta = with lib; {
-    homepage = "https://libvmi.com/";
+  meta = {
     description = "C library for virtual machine introspection";
     longDescription = ''
       LibVMI is a C library with Python bindings that makes it easy to monitor the low-level
       details of a running virtual machine by viewing its memory, trapping on hardware events,
       and accessing the vCPU registers.
     '';
-    license = with licenses; [
-      gpl3
-      lgpl3
+    homepage = "https://libvmi.com/";
+    license = with lib.licenses; [
+      gpl3Only
+      lgpl3Only
     ];
-    platforms = platforms.linux;
-    maintainers = [ ];
-    broken = true;
+    platforms = lib.lists.intersectLists lib.platforms.linux lib.platforms.x86_64;
+    maintainers = [ lib.maintainers.sigmasquadron ];
+    outputsToInstall = [
+      "out"
+      "lib"
+    ];
   };
 }
