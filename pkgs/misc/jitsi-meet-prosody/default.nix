1085c876681629f08939513ccab7b7f060d79a57
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-01 19:01:04 +0000

    jitsi-meet-prosody: 1.0.8043 -> 1.0.8091

--- pkgs/misc/jitsi-meet-prosody/default.nix
+++ pkgs/misc/jitsi-meet-prosody/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   pname = "jitsi-meet-prosody";
-  version = "1.0.8043";
+  version = "1.0.8091";
   src = fetchurl {
     url = "https://download.jitsi.org/stable/${pname}_${version}-1_all.deb";
-    sha256 = "aCrcsmEH3t294G2f5VzhdagzAFnWb5x1jn3htxlSXdE=";
+    sha256 = "LsZItRkFvpYaj5DwEv4C6tRpmypHadzSVv8/Wto2/68=";
   };
 
   dontBuild = true;
