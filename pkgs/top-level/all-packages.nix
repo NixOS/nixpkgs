aa4aae33c51735f558819b9d8d20188edf997768
Emily <vcs@emily.moe>
2024-08-02 21:53:03 +0100

    febio-studio: move to `pkgs/by-name`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37181,8 +37181,6 @@ with pkgs;
 
   fastp = callPackage ../applications/science/biology/fastp { };
 
-  febio-studio = callPackage ../applications/science/biology/febio-studio { };
-
   flywheel-cli = callPackage ../applications/science/biology/flywheel-cli { };
 
   hh-suite = callPackage ../applications/science/biology/hh-suite {
