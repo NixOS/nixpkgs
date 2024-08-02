93fd130c66fb276ec316eadd2f142c4231a75189
Minijackson <minijackson@riseup.net>
2024-08-02 11:37:03 +0200

    netbox_3_6: remove

--- pkgs/servers/web-apps/netbox/default.nix
+++ pkgs/servers/web-apps/netbox/default.nix
@@ -1,21 +1,12 @@
-{ lib, nixosTests, callPackage, }:
+{
+  lib,
+  nixosTests,
+  callPackage,
+}:
 let
   generic = import ./generic.nix;
 in
-lib.fix (self: {
-  netbox_3_6 = callPackage generic {
-    version = "3.6.9";
-    hash = "sha256-R/hcBKrylW3GnEy10DkrLVr8YJtsSCvCP9H9LhafO9I=";
-    extraPatches = [
-      # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
-      ./config.patch
-    ];
-    tests.netbox = nixosTests.netbox_3_6;
-
-    maintainers = with lib.maintainers; [ minijackson n0emis raitobezarius ];
-    eol = true;
-  };
-
+{
   netbox_3_7 = callPackage generic {
     version = "3.7.8";
     hash = "sha256-61pJbMWXNFnvWI0z9yWvsutdCAP4VydeceANNw0nKsk=";
@@ -25,7 +16,11 @@ lib.fix (self: {
     ];
     tests.netbox = nixosTests.netbox_3_7;
 
-    maintainers = with lib.maintainers; [ minijackson n0emis raitobezarius ];
+    maintainers = with lib.maintainers; [
+      minijackson
+      n0emis
+      raitobezarius
+    ];
     eol = true;
   };
-})
+}
