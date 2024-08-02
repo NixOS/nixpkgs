bdb56f408a8767b9f098c98f2809db9916a75a34
MikeHorn-git <MikeHornProton@proton.me>
2024-08-02 12:49:15 +0200

    maintainers: add MikeHorn-git

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13325,6 +13325,12 @@
     githubId = 978196;
     name = "Michaël Faille";
   };
+  mikehorn = {
+    email = "mikehornproton@proton.me";
+    github = "MikeHorn-git"
+    githubId = 123373126;
+    name = "Mike Horn";
+  };
   mikesperber = {
     email = "sperber@deinprogramm.de";
     github = "mikesperber";
