576a6918391f9daea10ef1ed5c27bfa200803c49
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-03 05:29:06 +0000

    openfga: 1.5.6 -> 1.5.7

--- pkgs/by-name/op/openfga/package.nix
+++ pkgs/by-name/op/openfga/package.nix
@@ -7,7 +7,7 @@
 
 let
   pname = "openfga";
-  version = "1.5.6";
+  version = "1.5.7";
 in
 
 buildGoModule {
@@ -17,10 +17,10 @@ buildGoModule {
     owner = "openfga";
     repo = "openfga";
     rev = "v${version}";
-    hash = "sha256-R5BqaKiW2jhglJ6zPPQF2Ld97YCttocjhfbo0dDGvCI=";
+    hash = "sha256-zvZjKtJNcawfbomulY0Z8h1Nf1re2faONiEpOB6swtU=";
   };
 
-  vendorHash = "sha256-zrhLG5msji6t4VPKG85jD8c2XQCRdQkF9UfRMqnzSFM=";
+  vendorHash = "sha256-MdhTvz7EY5xjpJmb+ww9YXD4U+EI7gFPSnQppccF9mQ=";
 
   nativeBuildInputs = [ installShellFiles ];
 
