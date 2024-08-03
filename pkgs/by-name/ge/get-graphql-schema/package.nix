0502f67fa6f46db44b0f7e9c08bf31b9eb3eb223
Pyrox <pyrox@pyrox.dev>
2024-08-02 23:59:55 -0400

    get-graphql-schema: remove usage of mkYarnPackage

--- pkgs/by-name/ge/get-graphql-schema/package.nix
+++ pkgs/by-name/ge/get-graphql-schema/package.nix
@@ -1,37 +1,36 @@
 {
   lib,
-  mkYarnPackage,
+  stdenv,
   fetchYarnDeps,
   fetchFromGitHub,
-  nodejs_22
-}: mkYarnPackage rec {
+  nodejs,
+  yarnConfigHook,
+  yarnBuildHook,
+  npmHooks,
+}:
+stdenv.mkDerivation (finalAttrs: {
   pname = "get-graphql-schema";
   version = "2.1.1";
 
   src = fetchFromGitHub {
     owner = "prisma-labs";
     repo = "get-graphql-schema";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-ujc0LGAqmo4SmItm4VcbBOtmUvL6aV1ppMm4fMmuSRs=";
   };
 
-  packageJSON = ./package.json;
-  offlineCache = fetchYarnDeps {
-    yarnLock = "${src}/yarn.lock";
+  yarnOfflineCache = fetchYarnDeps {
+    yarnLock = "${finalAttrs.src}/yarn.lock";
     hash = "sha256-TZGNX8UHbolLyBmQNGTnFjgx3/3f2HNVQf/h9rIVJKs=";
   };
 
-  buildPhase = ''
-    runHook preBuild
-    yarn --offline build
-    runHook postBuild
-  '';
+  nativeBuildInputs = [
+    yarnConfigHook
+    yarnBuildHook
+    npmHooks.npmInstallHook
+    nodejs
+  ];
 
-  postFixup = ''
-    substituteInPlace $out/libexec/get-graphql-schema/deps/get-graphql-schema/dist/index.js \
-      --replace-fail "#!/usr/bin/env node" "#!${lib.getExe nodejs_22}"
-    chmod +x $out/bin/get-graphql-schema
-  '';
   meta = {
     description = "Command line tool for generating a changelog from git tags and commit history";
     homepage = "https://github.com/cookpete/auto-changelog";
@@ -40,4 +39,4 @@
     mainProgram = "get-graphql-schema";
     maintainers = with lib.maintainers; [ pyrox0 ];
   };
-}
+})
