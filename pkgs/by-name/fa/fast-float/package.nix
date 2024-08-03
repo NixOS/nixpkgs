38ccc358f46257b6db0e937c8a5c91229487290b
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-08-04 00:01:37 +0200

    fast-float: format with nixfmt

--- pkgs/by-name/fa/fast-float/package.nix
+++ pkgs/by-name/fa/fast-float/package.nix
@@ -1,7 +1,8 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, cmake
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -15,14 +16,16 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-1QH9XvY981nSKCjb2nK3tDFHkJy9N1zGNX0dACRjTxE=";
   };
 
-  nativeBuildInputs = [
-    cmake
-  ];
+  nativeBuildInputs = [ cmake ];
 
   meta = {
     description = "Fast and exact implementation of the C++ from_chars functions for number types";
     homepage = "https://github.com/fastfloat/fast_float";
-    license = with lib.licenses; [ asl20 boost mit ];
+    license = with lib.licenses; [
+      asl20
+      boost
+      mit
+    ];
     maintainers = with lib.maintainers; [ wegank ];
     platforms = lib.platforms.all;
   };
