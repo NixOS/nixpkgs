ca80d70436a87dfc4e5d04c5d9446fab6a6ec15e
Emily <vcs@emily.moe>
2024-08-04 00:18:44 +0100

    python3Packages.ffmpeg-python: use `pyproject`

--- pkgs/development/python-modules/ffmpeg-python/default.nix
+++ pkgs/development/python-modules/ffmpeg-python/default.nix
@@ -2,21 +2,19 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  ffmpeg_4,
-  future,
-  pytest-mock,
-  pytestCheckHook,
-  pythonAtLeast,
-  pythonOlder,
   substituteAll,
+  setuptools,
+  future,
+  pytestCheckHook,
+  pytest-mock,
+  ffmpeg_4,
+  pythonAtLeast,
 }:
 
 buildPythonPackage rec {
   pname = "ffmpeg-python";
   version = "0.2.0";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.7";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "kkroening";
@@ -25,18 +23,20 @@ buildPythonPackage rec {
     hash = "sha256-Dk3nHuYVlIiFF6nORZ5TVFkBXdoZUxLfoiz68V1tvlY=";
   };
 
-  propagatedBuildInputs = [ future ];
-
-  nativeCheckInputs = [
-    pytestCheckHook
-    pytest-mock
-  ];
-
   patches = [
     (substituteAll {
       src = ./ffmpeg-location.patch;
       ffmpeg = ffmpeg_4;
     })
+  ];
+
+  build-system = [ setuptools ];
+
+  dependencies = [ future ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-mock
   ];
 
   postPatch = ''
@@ -48,10 +48,10 @@ buildPythonPackage rec {
 
   disabledTests = lib.optionals (pythonAtLeast "3.10") [ "test__output__video_size" ];
 
-  meta = with lib; {
+  meta = {
     description = "Python bindings for FFmpeg - with complex filtering support";
     homepage = "https://github.com/kkroening/ffmpeg-python";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
     maintainers = [ ];
   };
 }
