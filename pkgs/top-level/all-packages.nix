2c6e98e51c84049684c38e8b9e12d379f71d34d9
Emily <vcs@emily.moe>
2024-08-03 16:10:45 +0100

    libresample: move to `pkgs/by-name`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22031,8 +22031,6 @@ with pkgs;
     python = python3;
   };
 
-  libresample = callPackage ../development/libraries/libresample { };
-
   librevenge = callPackage ../development/libraries/librevenge { };
 
   librime = callPackage ../development/libraries/librime { };
