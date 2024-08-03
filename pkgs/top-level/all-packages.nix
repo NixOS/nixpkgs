37e3f3e85d429fb213513656569f4ac8bb300b19
Emily <vcs@emily.moe>
2024-08-03 16:32:14 +0100

    musly: move to `pkgs/by-name`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38777,8 +38777,6 @@ with pkgs;
 
   museeks = callPackage ../applications/audio/museeks { };
 
-  musly = callPackage ../applications/audio/musly { };
-
   mynewt-newt = callPackage ../tools/package-management/mynewt-newt { };
 
   mynewt-newtmgr = callPackage ../tools/misc/mynewt-newtmgr { };
