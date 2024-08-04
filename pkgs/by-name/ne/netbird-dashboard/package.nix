3796b4949821045b351e958e6d538f0c5331cf3d
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-04 07:50:45 +0000

    netbird-dashboard: 2.3.0 -> 2.4.2

--- pkgs/by-name/ne/netbird-dashboard/package.nix
+++ pkgs/by-name/ne/netbird-dashboard/package.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "netbird-dashboard";
-  version = "2.3.0";
+  version = "2.4.2";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "dashboard";
     rev = "v${version}";
-    hash = "sha256-4aZ7WGLhjpTHOggJ+sRdln0YOG3jf7TKT9/bH/n3LmQ=";
+    hash = "sha256-ObrwK8afQ5TVtIDEkI53x82gyU/TygY8bMBYo+pgxTw=";
   };
 
-  npmDepsHash = "sha256-ts3UuThIMf+wwSr3DpZ+k1i9RnHi/ltvhD/7lomVxQk=";
+  npmDepsHash = "sha256-l5n4ChqkwZNEJgCMs78c6IwBDkk6UCxfLQgQ4FN1/tI=";
   npmFlags = [ "--legacy-peer-deps" ];
 
   installPhase = ''
