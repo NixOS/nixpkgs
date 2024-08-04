6c9d023f984a8dc4be87ea7752ef700f09daf1f6
aprilthepink <aprl@acab.dev>
2024-08-04 12:12:47 +0200

    nixos/tuxedo-keyboard: rename module to tuxedo-drivers

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -98,7 +98,7 @@
   ./hardware/sensor/iio.nix
   ./hardware/steam-hardware.nix
   ./hardware/system-76.nix
-  ./hardware/tuxedo-keyboard.nix
+  ./hardware/tuxedo-drivers.nix
   ./hardware/ubertooth.nix
   ./hardware/uinput.nix
   ./hardware/uni-sync.nix
