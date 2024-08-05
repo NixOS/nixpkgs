a52bcc8536f114be246ae47293ad55a9d89002f8
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-05 12:25:39 +0000

    libasn1c: 0.9.36 -> 0.9.37

--- pkgs/servers/osmocom/libasn1c/default.nix
+++ pkgs/servers/osmocom/libasn1c/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libasn1c";
-  version = "0.9.36";
+  version = "0.9.37";
 
   src = fetchFromGitHub {
     owner = "osmocom";
     repo = "libasn1c";
     rev = version;
-    hash = "sha256-Qh4QVssHS6XDfHJBR+y8J5tUhT/6tDg+aF9nX6UAGV8=";
+    hash = "sha256-st5KbAUhNFSJ0DmPFYOnNvDQ8xtTNi4t8DNYvEjt9Ns=";
   };
 
   postPatch = ''
