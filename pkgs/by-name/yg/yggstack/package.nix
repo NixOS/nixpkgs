e1d92cda6fd1bcec60e4938ce92fcee619eea793
peigongdsd <peigong2013@outlook.com>
2024-08-06 07:48:11 +0800

    yggstack: add peigongdsd as maintainer

--- pkgs/by-name/yg/yggstack/package.nix
+++ pkgs/by-name/yg/yggstack/package.nix
@@ -26,6 +26,6 @@ buildGoModule rec {
     description = "Yggdrasil as SOCKS proxy / port forwarder";
     homepage = "https://yggdrasil-network.github.io/";
     license = licenses.lgpl3;
-    maintainers = with maintainers; [ ehmry ];
+    maintainers = with maintainers; [ ehmry peigongdsd ];
   };
 }
