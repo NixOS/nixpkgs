293d83c226a87e961f0f1b0d47b58f7759db779c
Fabian Affolter <mail@fabian-affolter.ch>
2024-08-05 08:07:20 +0200

    python312Packages.aiofile: refactor

--- pkgs/development/python-modules/aiofile/default.nix
+++ pkgs/development/python-modules/aiofile/default.nix
@@ -7,23 +7,26 @@
   fetchFromGitHub,
   pytestCheckHook,
   pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "aiofile";
   version = "3.8.6";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "mosquito";
-    repo = pname;
+    repo = "aiofile";
     rev = "refs/tags/${version}";
     hash = "sha256-KBly/aeHHZh7mL8MJ9gmxbqS7PmR4sedtBY/2HCXt54=";
   };
 
-  propagatedBuildInputs = [ caio ];
+  build-system = [ setuptools ];
+
+  dependencies = [ caio ];
 
   nativeCheckInputs = [
     aiomisc
