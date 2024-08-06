9581a2bdcaa97a17f1147deab25e12c3e6d6ccf5
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-08-06 16:46:49 +0200

    compress-drv: misc cleanup

--- pkgs/build-support/compress-drv/web.nix
+++ pkgs/build-support/compress-drv/web.nix
@@ -1,7 +1,8 @@
 {
-  zopfli,
   brotli,
   compressDrv,
+  lib,
+  zopfli,
 }:
 /**
   compressDrvWeb compresses a derivation for common web server use.
@@ -123,8 +124,8 @@ drv:
   ],
   extraFormats ? [ ],
   compressors ? {
-    "gz" = "${zopfli}/bin/zopfli --keep {}";
-    "br" = "${brotli}/bin/brotli --keep --no-copy-stat {}";
+    br = "${lib.getExe brotli} --keep --no-copy-stat {}";
+    gz = "${lib.getExe zopfli} --keep {}";
   },
 }:
 compressDrv drv {
