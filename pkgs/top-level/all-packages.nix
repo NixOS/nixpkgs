4a4605c2831d0eb09626e59c0bdb15d971f8d1d5
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-08-06 11:11:10 +0530

    wallust: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35044,8 +35044,6 @@ with pkgs;
 
   yuview = libsForQt5.yuview;
 
-  wallust = callPackage ../applications/misc/wallust { };
-
   zam-plugins = callPackage ../applications/audio/zam-plugins { };
 
   zammad = callPackage ../applications/networking/misc/zammad { };
