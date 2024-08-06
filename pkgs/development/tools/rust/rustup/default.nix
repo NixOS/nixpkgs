NixOS/nixpkgs

commit 26f5d6fbec4e0be97fe42fe9642153444fd28699

rustup: 1.25.2 -> 1.26.0

pkgs/development/tools/rust/rustup/default.nix
@@ -23,20 +23,17 @@ in
 
 rustPlatform.buildRustPackage rec {
   pname = "rustup";
-  version = "1.25.2";
+  version = "1.26.0";
 
   src = fetchFromGitHub {
     owner = "rust-lang";
     repo = "rustup";
     rev = version;
-    sha256 = "sha256-zFdw6P4yrLDshtF9A5MbkxFcUE8KvlZGx5qkW4LSPzw=";
+    sha256 = "sha256-rdhG9MdjWyvoaMGdjgFyCfQaoV48QtAZE7buA5TkDKg=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
-    outputHashes = {
-      "home-0.5.3" = "sha256-lb+FGJmGnBm64pddhQWclErwFLFnd7scFcuA+auw1Rk=";
-    };
   };
 
   nativeBuildInputs = [ makeBinaryWrapper pkg-config ];
@@ -62,12 +59,22 @@ rustPlatform.buildRustPackage rec {
 
   doCheck = !stdenv.isAarch64 && !stdenv.isDarwin;
 
+  # skip failing tests
+  checkFlags = [
+    # auto-self-update mode is set to 'disable' for nix rustup
+    "--skip=suite::cli_exact::check_updates_none"
+    "--skip=suite::cli_exact::check_updates_some"
+    "--skip=suite::cli_exact::check_updates_with_update"
+    # rustup-init is not used in nix rustup
+    "--skip=suite::cli_ui::rustup_init_ui_doc_text_tests"
+  ];
+
   postInstall = ''
     pushd $out/bin
     mv rustup-init rustup
     binlinks=(
       cargo rustc rustdoc rust-gdb rust-lldb rls rustfmt cargo-fmt
-      cargo-clippy clippy-driver cargo-miri rust-gdbgui
+      cargo-clippy clippy-driver cargo-miri rust-gdbgui rust-analyzer
     )
     for link in ''${binlinks[@]}; do
       ln -s rustup $link

commit 3fe9664e2ab39752a300be06d2f283da7ab407d2

rustup: convert to cargoHash
This used to have git dependencies, but no longer does.

Fixes: 26f5d6fbec4e ("rustup: 1.25.2 -> 1.26.0")

pkgs/development/tools/rust/rustup/default.nix
@@ -32,9 +32,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-rdhG9MdjWyvoaMGdjgFyCfQaoV48QtAZE7buA5TkDKg=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-  };
+  cargoHash = "sha256-CRqRSsDNWfVHOgmZGMOD7z44tzEEFqGxHo7z7IExVn8=";
 
   nativeBuildInputs = [ makeBinaryWrapper pkg-config ];
 