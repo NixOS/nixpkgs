5d29a4e478a18929a19edf6e1a4b56dea32b194a
Kerstin Humm <kerstin@erictapen.name>
2024-08-06 16:21:46 +0200

    python3Packages.flask-themer: init at 2.0.0
    Co-Authored-By: Sofi <sofi+git@mailbox.org>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4494,6 +4494,8 @@ self: super: with self; {
 
   flask-testing = callPackage ../development/python-modules/flask-testing { };
 
+  flask-themer = callPackage ../development/python-modules/flask-themer { };
+
   flask-themes2 = callPackage ../development/python-modules/flask-themes2 { };
 
   flask-versioned = callPackage ../development/python-modules/flask-versioned { };
