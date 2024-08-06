65038ab556c46f24250b18f1ebef36a1ec0c8df5
Picnoir <picnoir@alternativebit.fr>
2024-08-06 12:30:36 +0200

    nsncd: unstable-2024-03-18 -> 1.4.1-unstable-2024-04-10
    Fixes nsncd for rustc > 1.80.
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/os-specific/linux/nsncd/default.nix
+++ pkgs/os-specific/linux/nsncd/default.nix
@@ -2,21 +2,29 @@
 , stdenv
 , fetchFromGitHub
 , rustPlatform
-, nix-gitignore
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   pname = "nsncd";
-  version = "unstable-2024-03-18";
+  version = "1.4.1-unstable-2024-04-10";
 
   src = fetchFromGitHub {
     owner = "twosigma";
     repo = "nsncd";
-    rev =  "7605e330d5a313a8656e6fcaf1c10cd6b5cdd427";
+    rev = "7605e330d5a313a8656e6fcaf1c10cd6b5cdd427";
     hash = "sha256-Bd7qE9MP5coBCkr70TdoJfwYhQpdrn/zmN4KoARcaMI=";
   };
 
-  cargoHash = "sha256-i1rmc5wxtc631hZy2oM4d6r7od0w8GrG7+/pdM6Gqco=";
+  cargoHash = "sha256-N7U9YsyGh8+fLT973GGZTmVXcdnWhpqkeYTxzJ0rzdo=";
+
+  # TOREMOVE when https://github.com/twosigma/nsncd/pull/119 gets merged.
+  cargoPatches = [
+    ./0001-cargo-bump.patch
+  ];
+
+  # TOREMOVE when https://github.com/twosigma/nsncd/pull/119 gets merged.
+  RUSTFLAGS = "-A dead_code";
+
   checkFlags = [
     # Relies on the test environment to be able to resolve "localhost"
     # on IPv4. That's not the case in the Nix sandbox somehow. Works
