f8cfb66c88adecccd6738c711ac931886ef2663e
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-06 20:03:09 +0000

    suitesparse-graphblas: 9.2.0 -> 9.3.0

--- pkgs/development/libraries/science/math/suitesparse-graphblas/default.nix
+++ pkgs/development/libraries/science/math/suitesparse-graphblas/default.nix
@@ -7,7 +7,7 @@
 
 stdenv.mkDerivation rec {
   pname = "suitesparse-graphblas";
-  version = "9.2.0";
+  version = "9.3.0";
 
   outputs = [ "out" "dev" ];
 
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     owner = "DrTimothyAldenDavis";
     repo = "GraphBLAS";
     rev = "v${version}";
-    hash = "sha256-UtJ5AXbmoUA1NokgXDUDnhCZzOT1bTen6C89bsCWEIo=";
+    hash = "sha256-/ubvds/mg5SW8kEfnEuPyHCM/P8+ETT1Uus2ESj50Og=";
   };
 
   nativeBuildInputs = [
