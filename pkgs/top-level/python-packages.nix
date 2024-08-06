e7c47c701bdd2a1ff057abf9c0c29b790c69a2f5
Geraldo Nascimento <geraldogabriel@gmail.com>
2024-08-06 00:10:06 -0300

    aiosonic: init at 0.20.1
    Co-authored-by: Florian <florian.brandes@posteo.de>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -403,6 +403,8 @@ self: super: with self; {
 
   aiosasl = callPackage ../development/python-modules/aiosasl { };
 
+  aiosonic = callPackage ../development/python-modules/aiosonic { };
+
   aiosql = callPackage ../development/python-modules/aiosql { };
 
   aiosenz = callPackage ../development/python-modules/aiosenz { };
