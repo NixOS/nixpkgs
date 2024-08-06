ac573bcf2e12fe2534891192bfa12f0cf013aebb
toastal <toastal@posteo.net>
2024-08-06 21:49:04 +0700

    topiary: 0.3.0 → 0.4.0
    A messy update to say the least. Topiary 0.4.0 was released a while
    back, but the update was non-trivial. Not only did a bunch of Git
    outputHashes SHAs need to get updated & a new, revendored Cargo.lock,
    but a Git submodule in tree-sitter-bash, which is by Topiary referenced
    at an arbitrary point in 0.19.0’s history, is now no longer up & needed
    patching to remove it. To circumvent needing to learn how patch to a
    Cargo dependency for this, I have forked the tree-sitter-bash project @
    Topiary 0.4.0’s commit + cherry-picked the
    b2959f8be16fa1e2ee88a560074527fdef05b328 patch which removes the
    submodule dependencies (to check that this is indeed the case, you may
    check the commit graph
    <https://codeberg.org/toastal/tree-sitter-bash/graph>). I also opened an
    upstream issue with Topiary
    <https://github.com/tweag/topiary/issues/734> to hopefully in the future
    start keeping these grammar queries updated more often than once every 3
    years.
    
    Co-Authored-By: Avi Dessauer <avi.the.coder@gmail.com>

--- pkgs/development/tools/misc/topiary/default.nix
+++ pkgs/development/tools/misc/topiary/default.nix
@@ -6,28 +6,37 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "topiary";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "tweag";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-zt4uXkO6Y0Yc1Wt8l5O79oKbgNLrgip40ftD7UfUPwo=";
+    hash = "sha256-5o5DvwUYAmODYlFCuDJwRvEkitgq2SFD9Wy0DRMjKsY=";
   };
+
+  # lib.maintainers.toastal forked tree-sitter-bash to Codeberg @ 0.19.0 +
+  # Topiary’s random extra post-0.19.0 commits to cherry-pick a
+  # tree-sitter-bash upstream patch, b2959f8be16fa1e2ee88a560074527fdef05b328,
+  # that removes the now-failing, previous submodule dependency. This is
+  # reflected in the patch, ./Cargo.lock, & self.cargoLock.
+  # See: https://github.com/tweag/topiary/issues/734
+  cargoPatches = [
+    ./tree-sitter-bash-update.patch
+  ];
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "tree-sitter-bash-0.19.0" = "sha256-a0/KyHV2jUpytsZSHI7tVw8GfYnuqVjfs5KScLZkB0I=";
-      "tree-sitter-facade-0.9.3" = "sha256-M/npshnHJkU70pP3I4WMXp3onlCSWM5mMIqXP45zcUs=";
-      "tree-sitter-json-0.20.0" = "sha256-cyrea0Y13OVGkXbYE0Cwc7nUsDGEZyoQmPAS9wVuHw0=";
-      "tree-sitter-nickel-0.0.1" = "sha256-aYsEx1Y5oDEqSPCUbf1G3J5Y45ULT9OkD+fn6stzrOU=";
-      "tree-sitter-ocaml-0.20.4" = "sha256-j3Hv2qOMxeBNOW+WIgIYzG3zMIFWPQpoHe94b2rT+A8=";
+      "tree-sitter-bash-0.19.0" = "sha256-dBO5Sg4fSUesGgQqI1W3JtjGriqkh6ZyxuzwTmEWCQI=";
+      "tree-sitter-css-0.20.0" = "sha256-IniaiBBx2pDD5nwJKfr5i9qvfvG+z8H21v14qk14M0g=";
+      "tree-sitter-json-0.20.2" = "sha256-dVErHgsUDEN42wc/Gd68vQfVc8+/r/8No9KZk2GFzmY=";
+      "tree-sitter-nickel-0.1.0" = "sha256-WuY6X1mnXdjiy4joIcY8voK2sqICFf0GvudulZ9lwqg=";
+      "tree-sitter-ocaml-0.20.4" = "sha256-9Y/eZNsKkz8RKjMn5RIAPITkDQTWdSc/fBXzxMg1ViQ=";
       "tree-sitter-ocamllex-0.20.2" = "sha256-YhmEE7I7UF83qMuldHqc/fD/no/7YuZd6CaAIaZ1now=";
-      "tree-sitter-query-0.1.0" = "sha256-5N7FT0HTK3xzzhAlk3wBOB9xlEpKSNIfakgFnsxEi18=";
-      "tree-sitter-rust-0.20.4" = "sha256-seWoMuA87ZWCq3mUXopVeDCcTxX/Bh+B4PFLVa0CBQA=";
+      "tree-sitter-query-0.2.0" = "sha256-H2QLsjl3/Kh0ojCf2Df38tb9KrM2InphEmtGd0J6+hM=";
+      "tree-sitter-rust-0.20.4" = "sha256-egTxBuliboYbl+5N6Jdt960EMLByVmLqSmQLps3rEok=";
       "tree-sitter-toml-0.5.1" = "sha256-5nLNBxFeOGE+gzbwpcrTVnuL1jLUA0ZLBVw2QrOLsDQ=";
-      "web-tree-sitter-sys-1.3.0" = "sha256-9rKB0rt0y9TD/HLRoB9LjEP9nO4kSWR9ylbbOXo2+2M=";
     };
   };
 
@@ -37,7 +46,7 @@ rustPlatform.buildRustPackage rec {
   env.TOPIARY_LANGUAGE_DIR = "${placeholder "out"}/share/queries";
 
   postInstall = ''
-    install -Dm444 queries/* -t $out/share/queries
+    install -Dm444 topiary-queries/queries/* -t $out/share/queries
   '';
 
   passthru.updateScript = nix-update-script { };
