4c6f5fcf565237bc27faf538e85cea0e0ef7b011
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-05 23:41:58 -0300

    emacsPackages.lspce-module: 1.1.0-unstable-2024-07-14 -> 1.1.0-unstable-2024-08-04

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/lspce-module/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/lspce-module/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage {
   pname = "lspce-module";
-  version = "1.1.0-unstable-2024-07-14";
+  version = "1.1.0-unstable-2024-08-04";
 
   src = fetchFromGitHub {
     owner = "zbelial";
     repo = "lspce";
-    rev = "fd320476df89cfd5d10f1b70303c891d3b1e3c81";
-    hash = "sha256-KnERYq/CvJhJIdQkpH/m82t9KFMapPl+CyZkYyujslU=";
+    rev = "59b9c0551ee34440d3cc754313ac8076f7076f54";
+    hash = "sha256-VpIsAteBEoENhejS0QqAZRoEYgaDpVCdPqrgrj4OcIU=";
   };
 
-  cargoHash = "sha256-I2OobRu1hc6xc4bRrIO1FImPYBbFy1jXPcTsivbbskk=";
+  cargoHash = "sha256-q0xUOzDRpiAdvtH3rzCOZRYthGR6MvbiXB4a7cDgf9A=";
 
   checkFlags = [
     # flaky test
