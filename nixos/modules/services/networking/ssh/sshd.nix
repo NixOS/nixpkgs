b64eb21a86e1ab19d2ee910c3cdd8221a6995a49
adamoudad <adam.oudad@gmail.com>
2024-08-07 02:11:36 +0900

    Add all recommended Macs for OpenSSH
    Co-authored-by: Mirko Lenz <mirko@lenz.am>

--- nixos/modules/services/networking/ssh/sshd.nix
+++ nixos/modules/services/networking/ssh/sshd.nix
@@ -439,6 +439,8 @@ in
                 "hmac-sha2-256-etm@openssh.com"
                 "umac-128-etm@openssh.com"
                 "hmac-sha2-512"
+                "hmac-sha2-256"
+                "umac-128@openssh.com"
               ];
               description = ''
                 Allowed MACs
