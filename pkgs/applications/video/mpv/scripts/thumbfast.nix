5f2b6c65b77c2f6c993ba1ce27c7721c58a690f8
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-06 09:08:51 +0000

    mpvScripts.thumbfast: 0-unstable-2023-12-08 -> 0-unstable-2024-08-02

--- pkgs/applications/video/mpv/scripts/thumbfast.nix
+++ pkgs/applications/video/mpv/scripts/thumbfast.nix
@@ -8,13 +8,13 @@
 
 buildLua {
   pname = "mpv-thumbfast";
-  version = "0-unstable-2023-12-08";
+  version = "0-unstable-2024-08-02";
 
   src = fetchFromGitHub {
     owner = "po5";
     repo = "thumbfast";
-    rev = "03e93feee5a85bf7c65db953ada41b4826e9f905";
-    hash = "sha256-5u5WBvWOEydJrnr/vilEgW4+fxkxM6wNjb9Fyyxx/1c=";
+    rev = "f1fdf10b17f394f2d42520d0e9bf22feaa20a9f4";
+    hash = "sha256-cygLf+0PMH7cVXBcY12PdcxBHmy38DNoXQubKAlerHM=";
   };
   passthru.updateScript = unstableGitUpdater { };
 
