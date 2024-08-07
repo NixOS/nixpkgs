a2d28faa8672c34ec22285db410872c69f5933f0
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-08-07 09:38:03 +0300

    mindustry: add zenity to $PATH
    Co-authored-by: Francesco Gazzetta <fgaz@fgaz.me>

--- pkgs/games/mindustry/default.nix
+++ pkgs/games/mindustry/default.nix
@@ -7,6 +7,7 @@
 , fetchFromGitHub
 , gradle
 , jdk17
+, zenity
 
 # for arc
 , SDL2
@@ -189,6 +190,7 @@ stdenv.mkDerivation {
       mkdir -p $out/bin
       makeWrapper ${jdk}/bin/java $out/bin/mindustry \
         --add-flags "-jar $out/share/mindustry.jar" \
+        ${lib.optionalString stdenv.isLinux "--suffix PATH : ${lib.makeBinPath [zenity]}"} \
         --suffix LD_LIBRARY_PATH : ${lib.makeLibraryPath [libpulseaudio alsa-lib libjack2]} \
         --set ALSA_PLUGIN_DIR ${alsa-plugins}/lib/alsa-lib/'' + optionalString enableWayland '' \
         --set SDL_VIDEODRIVER wayland \
