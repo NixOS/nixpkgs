ab8cb51b02b7c28893892d84c399ee4e891ba354
Dennis Wuitz <dennish@wuitz.de>
2024-08-07 12:27:56 +0200

    python3Packages.parselmouth: init at 0.4.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9659,6 +9659,8 @@ self: super: with self; {
 
   parsel = callPackage ../development/python-modules/parsel { };
 
+  parselmouth = callPackage ../development/python-modules/parselmouth { };
+
   parse-type = callPackage ../development/python-modules/parse-type { };
 
   parsimonious = callPackage ../development/python-modules/parsimonious { };
