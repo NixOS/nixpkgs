1ab6f87a54587d3d18c432e5016f428660d80294
Tomodachi94 <tomodachi94@protonmail.com>
2024-08-07 12:36:26 -0700

    python3Packages.pywikibot: init at 9.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13132,6 +13132,8 @@ self: super: with self; {
 
   pywfa = callPackage ../development/python-modules/pywfa { };
 
+  pywikibot = callPackage ../development/python-modules/pywikibot { };
+
   pywilight = callPackage ../development/python-modules/pywilight { };
 
   pywinrm = callPackage ../development/python-modules/pywinrm { };
