a53befd1c9f56d12caf26fa67a1b957c081f8efa
Bobby Rong <rjl931189261@126.com>
2024-08-07 13:36:32 +0000

    gtksourceview5: 5.12.1 → 5.13.1
    https://gitlab.gnome.org/GNOME/gtksourceview/-/compare/5.12.1...5.13.1
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/gtksourceview/5.x.nix
+++ pkgs/development/libraries/gtksourceview/5.x.nix
@@ -24,13 +24,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gtksourceview";
-  version = "5.12.1";
+  version = "5.13.1";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gtksourceview/${lib.versions.majorMinor finalAttrs.version}/gtksourceview-${finalAttrs.version}.tar.xz";
-    hash = "sha256-hMgqrZhcWq2ufOp4BJBKdjQeyCsmjUZZTBpHjzm0LB8=";
+    hash = "sha256-olsElgdCT3xjQS67828mNbyfLidHcD8/B2D9DwcSBhY=";
   };
 
   patches = [
