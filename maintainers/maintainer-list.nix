272a2ceff217f754d85f3c628d09bed4085f2c1a
Gavin John <gavinnjohn@gmail.com>
2024-08-07 19:32:48 +0100

    maintainers: Add @technobaboo to maintainers list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20126,6 +20126,11 @@
     githubId = 38222826;
     name = "Aamaruvi Yogamani";
   };
+  technobaboo = {
+    github = "technobaboo";
+    githubId = 4541968;
+    name = "Nova King";
+  };
   teh = {
     email = "tehunger@gmail.com";
     github = "teh";
