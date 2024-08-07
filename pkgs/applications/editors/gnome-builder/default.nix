7e474c7ca191755c48d698b31bb5ceebad31be85
Bobby Rong <rjl931189261@126.com>
2024-08-07 13:35:59 +0000

    gnome-builder: 46.3 → 47.beta
    https://gitlab.gnome.org/GNOME/gnome-builder/-/compare/46.3...47.beta

--- pkgs/applications/editors/gnome-builder/default.nix
+++ pkgs/applications/editors/gnome-builder/default.nix
@@ -12,6 +12,7 @@
 , gobject-introspection
 , enchant
 , icu
+, gom
 , gtk4
 , gtksourceview5
 , json-glib
@@ -42,13 +43,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-builder";
-  version = "46.3";
+  version = "47.beta";
 
   outputs = [ "out" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-builder/${lib.versions.major finalAttrs.version}/gnome-builder-${finalAttrs.version}.tar.xz";
-    hash = "sha256-EvJ8DipKcxb59+IYWd9r8bd1cNKWb5V6QzBvaZ7FRhI=";
+    hash = "sha256-pkpH54aaJGcnPzo1HERDpXF5lpoM5KJhaTwtjZXVaZU=";
   };
 
   patches = [
@@ -87,6 +88,7 @@ stdenv.mkDerivation (finalAttrs: {
     vte-gtk4
     enchant
     icu
+    gom
     gtk4
     gtksourceview5
     json-glib
