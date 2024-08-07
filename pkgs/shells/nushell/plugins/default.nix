195fde55320b38f4d0f439ff337a26624c415800
DarkKronicle <darkkronicle@gmail.com>
2024-08-07 12:56:33 -0600

    nushellPlugins.dbus: init at 0.9.0

--- pkgs/shells/nushell/plugins/default.nix
+++ pkgs/shells/nushell/plugins/default.nix
@@ -1,10 +1,21 @@
-{ lib, newScope, IOKit, CoreFoundation, Foundation, Security }:
-
-lib.makeScope newScope (self: with self; {
-  gstat = callPackage ./gstat.nix { inherit Security; };
-  formats = callPackage ./formats.nix { inherit IOKit Foundation; };
-  polars = callPackage ./polars.nix { inherit IOKit Foundation; };
-  query = callPackage ./query.nix { inherit IOKit CoreFoundation; };
-  regex = throw "`nu_plugin_regex` is no longer compatible with the current Nushell release.";
-  net = callPackage ./net.nix { inherit IOKit CoreFoundation; };
-})
+{
+  lib,
+  newScope,
+  IOKit,
+  CoreFoundation,
+  Foundation,
+  Security,
+  dbus,
+}:
+lib.makeScope newScope (
+  self: with self; {
+    gstat = callPackage ./gstat.nix { inherit Security; };
+    formats = callPackage ./formats.nix { inherit IOKit Foundation; };
+    polars = callPackage ./polars.nix { inherit IOKit Foundation; };
+    query = callPackage ./query.nix { inherit IOKit CoreFoundation; };
+    regex = throw "`nu_plugin_regex` is no longer compatible with the current Nushell release.";
+    net = callPackage ./net.nix { inherit IOKit CoreFoundation; };
+    # Include dbus argument or else infinite recursion
+    dbus = callPackage ./dbus.nix { inherit dbus; };
+  }
+)
