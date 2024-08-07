0f75aa443fcf9fd049e979bee078675897b18455
Bobby Rong <rjl931189261@126.com>
2024-08-07 13:36:45 +0000

    libdex: 0.6.1 → 0.7.1
    https://gitlab.gnome.org/GNOME/libdex/-/compare/0.6.1...0.7.1
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/libdex/default.nix
+++ pkgs/development/libraries/libdex/default.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libdex";
-  version = "0.6.1";
+  version = "0.7.1";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/libdex/${lib.versions.majorMinor finalAttrs.version}/libdex-${finalAttrs.version}.tar.xz";
-    hash = "sha256-0XbeZXhXHjKowLYDtqWhP6X4f7a1RCV1s47FrxaxepI=";
+    hash = "sha256-wLE+l7NABX3E58U/F+XX7NMQofoqvJpylnDwImGkCps=";
   };
 
   nativeBuildInputs = [
