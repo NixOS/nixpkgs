15a028796d96d5076ad1b6f70c9e9762edfd192e
Bobby Rong <rjl931189261@126.com>
2024-08-07 13:36:22 +0000

    gspell: 1.12.2 → 1.13.1
    https://gitlab.gnome.org/GNOME/gspell/-/compare/1.12.2...1.13.1
    
    Ported to meson.
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/gspell/default.nix
+++ pkgs/development/libraries/gspell/default.nix
@@ -1,65 +1,65 @@
-{ stdenv
-, lib
-, buildPackages
-, fetchurl
-, pkg-config
-, libxml2
-, autoreconfHook
-, gtk-doc
-, glib
-, gtk3
-, enchant2
-, icu
-, vala
-, gobject-introspection
-, gnome
+{
+  stdenv,
+  lib,
+  fetchurl,
+  docbook-xsl-nons,
+  glib,
+  gobject-introspection,
+  gtk-doc,
+  meson,
+  ninja,
+  pkg-config,
+  vala,
+  mesonEmulatorHook,
+  gtk3,
+  icu,
+  enchant2,
+  gnome,
 }:
 
 stdenv.mkDerivation rec {
   pname = "gspell";
-  version = "1.12.2";
+  version = "1.13.1";
 
-  outputs = [ "out" "dev" ];
+  outputs = [
+    "out"
+    "dev"
+    "devdoc"
+  ];
+
   outputBin = "dev";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "tOmTvYJ+TOtqdwsbXolQ/OO+nIsrDL6yL9+ZKAjdITk=";
+    sha256 = "zO1F6Ykro1wxuaEdYqnhVC5d/Wj3+zfDNbp0SfIM1hU=";
   };
 
   patches = [
-    # Extracted from: https://github.com/Homebrew/homebrew-core/blob/2a27fb86b08afc7ae6dff79cf64aafb8ecc93275/Formula/gspell.rb#L125-L149
-    # Dropped the GTK_MAC_* changes since gtk-mac-integration is not needed since 1.12.1
+    # https://gitlab.gnome.org/GNOME/gspell/-/merge_requests/10
     ./0001-Darwin-build-fix.patch
   ];
 
   nativeBuildInputs = [
+    docbook-xsl-nons
+    glib # glib-mkenums
+    gobject-introspection
+    gtk-doc
+    meson
+    ninja
     pkg-config
     vala
-    gobject-introspection
-    libxml2
-    autoreconfHook
-    gtk-doc
-    glib
+  ] ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    mesonEmulatorHook
   ];
 
   buildInputs = [
     gtk3
     icu
-    vala # for share/vala/Makefile.vapigen (PKG_CONFIG_VAPIGEN_VAPIGEN)
   ];
 
   propagatedBuildInputs = [
     # required for pkg-config
     enchant2
-  ];
-
-  configureFlags = [
-    "GLIB_COMPILE_RESOURCES=${lib.getDev buildPackages.glib}/bin/glib-compile-resources"
-    "GLIB_MKENUMS=${lib.getDev buildPackages.glib}/bin/glib-mkenums"
-    "PKG_CONFIG_VAPIGEN_VAPIGEN=${lib.getBin buildPackages.vala}/bin/vapigen"
-    "--enable-introspection=yes"
-    "--enable-vala=yes"
   ];
 
   passthru = {
