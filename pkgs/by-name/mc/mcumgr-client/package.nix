8c2c5176de89572170698d40cbf715ddba4a0adb
Alyssa Ross <hi@alyssa.is>
2024-08-08 20:44:07 +0200

    Revert "mcumgr-client.updateScript: drop"
    This reverts commit eecd125eeab7a1ce44277e6b3020aa39cd818f1b.
    
    The update script should work now, because upstream includes a
    lockfile.

--- pkgs/by-name/mc/mcumgr-client/package.nix
+++ pkgs/by-name/mc/mcumgr-client/package.nix
@@ -2,6 +2,7 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  nix-update-script,
   pkg-config,
   udev,
   stdenv,
@@ -20,6 +21,8 @@ rustPlatform.buildRustPackage rec {
   };
 
   cargoHash = "sha256-9jlthe7YQJogcjGv+TOk+O2YW3Xrq6h9tTjXdKHG99k=";
+
+  passthru.updateScript = nix-update-script { };
 
   nativeBuildInputs = [ pkg-config ];
 
