380c8262a27d1e19fb12ef50f05c586717d5361b
Fabian Affolter <mail@fabian-affolter.ch>
2024-08-08 23:12:22 +0200

    python312Packages.container-inspector: refactor

--- pkgs/development/python-modules/container-inspector/default.nix
+++ pkgs/development/python-modules/container-inspector/default.nix
@@ -14,26 +14,20 @@
 buildPythonPackage rec {
   pname = "container-inspector";
   version = "33.0.1";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "nexB";
-    repo = pname;
+    repo = "container-inspector";
     rev = "refs/tags/v${version}";
     hash = "sha256-bXJ4UIDVhiU0DurEeRiyLlSUrNRgwoMqAxXxGb/CcJs=";
   };
 
   dontConfigure = true;
 
-  postPatch = ''
-    # PEP440 support was removed in newer setuptools, https://github.com/nexB/container-inspector/pull/51
-    substituteInPlace setup.cfg \
-      --replace ">=3.7.*" ">=3.7"
-  '';
-
-  nativeBuildInputs = [ setuptools-scm ];
+  build-system = [ setuptools-scm ];
 
   propagatedBuildInputs = [
     attrs
