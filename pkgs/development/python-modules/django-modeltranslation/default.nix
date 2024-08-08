5a51773c34ce1a116830126e2f25a8f4fbe4562c
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-08 02:49:11 +0000

    python312Packages.django-modeltranslation: 0.19.5 -> 0.19.6

--- pkgs/development/python-modules/django-modeltranslation/default.nix
+++ pkgs/development/python-modules/django-modeltranslation/default.nix
@@ -12,7 +12,7 @@
 let
   # 0.18.12 was yanked from PyPI, it refers to this issue:
   # https://github.com/deschler/django-modeltranslation/issues/701
-  version = "0.19.5";
+  version = "0.19.6";
 in
 buildPythonPackage {
   pname = "django-modeltranslation";
@@ -22,7 +22,7 @@ buildPythonPackage {
     owner = "deschler";
     repo = "django-modeltranslation";
     rev = "refs/tags/v${version}";
-    hash = "sha256-wIvX9m3AGa6rLAWI56B7sEyj04g8cPIF37JWgVzbtig=";
+    hash = "sha256-zFY8YsM1Qp6rdcb+Upfk0/6mTv0o1zwxGqKzTupZ1zI=";
   };
 
   # Remove all references to pytest-cov
