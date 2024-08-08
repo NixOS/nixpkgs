4f44434611cdc48eb2022f05116844815ea7156e
Robert Schütz <mail@dotlambda.de>
2024-08-08 12:56:25 -0700

    python312Packages.pyegps: init at 0.2.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11312,6 +11312,8 @@ self: super: with self; {
 
   pyefergy = callPackage ../development/python-modules/pyefergy { };
 
+  pyegps = callPackage ../development/python-modules/pyegps { };
+
   pyeight = callPackage ../development/python-modules/pyeight { };
 
   pyelftools = callPackage ../development/python-modules/pyelftools { };
