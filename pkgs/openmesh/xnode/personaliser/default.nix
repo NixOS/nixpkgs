e3f2c3fda7e1b2a15d65fa99876e597d8a9faaa3
J Lynn <j@jaesharp.com>
2024-08-08 16:57:32 +1000

    xnode-personaliser: flake-compat update

--- pkgs/openmesh/xnode/personaliser/default.nix
+++ pkgs/openmesh/xnode/personaliser/default.nix
@@ -7,8 +7,8 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "Openmesh-Network";
     repo = "xnode-personaliser";
-    rev = "e2b09aeea33f4b32d1ed109c0ea1d71667e92305";
-    sha256 = "sha256-JrPgnhGlKT16BBqrhcHszNtXGahf/hHkd9eGTavQsC8=";
+    rev = "cbdaf4fe15f6a0efbe19fb41782f11798de4b66a";
+    sha256 = lib.fakeHash;
   };
 in
 import src
