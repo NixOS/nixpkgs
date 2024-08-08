285168dc7308152d57add18caba681ec419bb0b3
Robert Schütz <mail@dotlambda.de>
2024-08-08 15:02:39 -0700

    python312Packages.pyblu: init at 0.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11064,6 +11064,8 @@ self: super: with self; {
 
   pybloom-live = callPackage ../development/python-modules/pybloom-live { };
 
+  pyblu = callPackage ../development/python-modules/pyblu { };
+
   pybluez = callPackage ../development/python-modules/pybluez {
     inherit (pkgs) bluez;
   };
