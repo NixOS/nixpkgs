f20eeca6dfa51fcb6c9a9f9f351f92b739ed32ba
Ashish SHUKLA <ashish.is@lostca.se>
2024-08-08 00:15:28 +0000

    ugrep: 6.3.0 -> 6.4.0

--- pkgs/tools/text/ugrep/default.nix
+++ pkgs/tools/text/ugrep/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ugrep";
-  version = "6.3.0";
+  version = "6.4.0";
 
   src = fetchFromGitHub {
     owner = "Genivia";
     repo = "ugrep";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-v9Bk+RfzoS1/rgnJMo0UQWJn23MPEAVH/phaMQSLjGg=";
+    hash = "sha256-oD55EEThyetByEaO2ZZxAadouZf/Z+p10lvnESep8gg=";
   };
 
   buildInputs = [
