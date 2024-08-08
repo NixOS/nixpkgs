7faa8c73b7bf65a1d7b765ecb2bdcb16bf5e4003
Robert Schütz <mail@dotlambda.de>
2024-08-08 05:52:04 -0700

    python312Packages.doorbirdpy: fix tests with aiohttp 3.10

--- pkgs/development/python-modules/doorbirdpy/default.nix
+++ pkgs/development/python-modules/doorbirdpy/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitLab,
+  fetchpatch2,
   setuptools,
   aiohttp,
   aioresponses,
@@ -20,6 +21,15 @@ buildPythonPackage rec {
     rev = "refs/tags/${version}";
     hash = "sha256-6B4EMK41vEpmLoQLD+XN9yStLdxyHHk/Mym9J0o7Qvc=";
   };
+
+  patches = [
+    # https://gitlab.com/klikini/doorbirdpy/-/merge_requests/15
+    (fetchpatch2 {
+      name = "aiohttp-3.10-compat.patch";
+      url = "https://gitlab.com/klikini/doorbirdpy/-/commit/91f417433be36a0c9d2baaf0d6ff1a45042f94eb.patch";
+      hash = "sha256-b/ORH6ygkiBreWYTH7rP8b68HlFUEyLQCzVo1KLffPQ=";
+    })
+  ];
 
   build-system = [ setuptools ];
 
