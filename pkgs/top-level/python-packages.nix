0aa20c22cb6c2a35d9138ec70dfc1ded158f3257
Cabia Rangris <cab@cab.moe>
2024-08-08 07:30:36 +0400

    pythonPackages.senf: init at 1.5.0
    Co-authored-by: OTABI Tomoya <tomoya.otabi@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14120,6 +14120,8 @@ self: super: with self; {
 
   sendgrid = callPackage ../development/python-modules/sendgrid { };
 
+  senf = callPackage ../development/python-modules/senf { };
+
   sense-energy = callPackage ../development/python-modules/sense-energy { };
 
   sensirion-ble = callPackage ../development/python-modules/sensirion-ble { };
