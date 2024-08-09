9b82f8625b87f4485a4df8d404c7844bc655a86f
David Cox <david@integrated-reasoning.com>
2024-08-09 16:19:53 -0700

    cargo-typify: 0.0.14 -> 0.1.0

--- pkgs/by-name/ca/cargo-typify/package.nix
+++ pkgs/by-name/ca/cargo-typify/package.nix
@@ -1,35 +1,58 @@
-{ lib, rustfmt, rustPlatform, fetchFromGitHub, gitUpdater }:
+{
+  lib,
+  rustfmt,
+  rustPlatform,
+  fetchFromGitHub,
+  gitUpdater,
+  makeWrapper,
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-typify";
-  version = "0.0.14";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "oxidecomputer";
     repo = "typify";
     rev = "v${version}";
-    hash = "sha256-Clwm5hRjPPPRB6xpO8YOGpqnyNFtsSMkPbWBY3etPCI=";
+    hash = "sha256-vokhWIY5iikTyADrqxp6DIq+tJ+xdFPebDFTddJnstA=";
   };
 
-  cargoHash = "sha256-balx5xOtrWwMOFeGQkYQ2f+lcGMCJvdPqE8rH9adkfU=";
+  cargoHash = "sha256-etlZqhtRCcCBeMC4lq6BjTD4TQyWEwJf1bLKjoIDR70=";
 
-  cargoBuildFlags = [ "-p" "cargo-typify" ];
+  nativeBuildInputs = [
+    rustfmt
+    makeWrapper
+  ];
 
-  nativeCheckInputs = [ rustfmt ];
+  cargoBuildFlags = [
+    "--package"
+    "cargo-typify"
+  ];
+  cargoTestFlags = [
+    "--package"
+    "cargo-typify"
+  ];
+
+  strictDeps = true;
 
   preCheck = ''
     # cargo-typify depends on rustfmt-wrapper, which requires RUSTFMT:
     export RUSTFMT="${lib.getExe rustfmt}"
   '';
 
+  postInstall = ''
+    wrapProgram $out/bin/cargo-typify \
+      --set RUSTFMT "${lib.getExe rustfmt}"
+  '';
+
   passthru.updateScript = gitUpdater { rev-prefix = "v"; };
 
-  meta = with lib; {
+  meta = {
     description = "JSON Schema to Rust type converter";
     mainProgram = "cargo-typify";
     homepage = "https://github.com/oxidecomputer/typify";
-    license = with licenses; [ asl20 ];
-    maintainers = with maintainers; [ david-r-cox ];
-    broken = true;
+    license = with lib.licenses; [ asl20 ];
+    maintainers = with lib.maintainers; [ david-r-cox ];
   };
 }
