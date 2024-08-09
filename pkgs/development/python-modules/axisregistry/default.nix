6c29094a4dd243ae9a6fa5c57c92ddce027d953c
Fabian Affolter <mail@fabian-affolter.ch>
2024-08-09 08:15:31 +0200

    python312Packages.axisregistry: refactor
    - disable on unsupported Python releases
    - add pythonImportsCheck
    - update ordering

--- pkgs/development/python-modules/axisregistry/default.nix
+++ pkgs/development/python-modules/axisregistry/default.nix
@@ -6,34 +6,37 @@
   protobuf,
   pytestCheckHook,
   setuptools-scm,
+  pythonOlder,
 }:
 
 buildPythonPackage rec {
   pname = "axisregistry";
   version = "0.4.11";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-p1/ocmWqrCJ4CylRgen/DR0LeqcwIxB1jAauJbw8ygY=";
   };
 
-  propagatedBuildInputs = [
-    fonttools
-    protobuf
-  ];
-  nativeBuildInputs = [
-    setuptools-scm
-  ];
-
-  doCheck = true;
-  nativeCheckInputs = [ pytestCheckHook ];
-
   # Relax the dependency on protobuf 3. Other packages in the Google Fonts
   # ecosystem have begun upgrading from protobuf 3 to protobuf 4,
   # so we need to use protobuf 4 here as well to avoid a conflict
   # in the closure of fontbakery. It seems to be compatible enough.
   pythonRelaxDeps = [ "protobuf" ];
+
+  build-system = [ setuptools-scm ];
+
+  dependencies = [
+    fonttools
+    protobuf
+  ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "axisregistry" ];
 
   meta = with lib; {
     description = "Google Fonts registry of OpenType variation axis tags";
