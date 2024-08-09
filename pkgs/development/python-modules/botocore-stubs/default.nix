509cebe97d1f884ad48595c47f81226548958c2a
Fabian Affolter <mail@fabian-affolter.ch>
2024-08-09 14:33:33 +0200

    python312Packages.botocore-stubs: 1.34.156 -> 1.34.157

--- pkgs/development/python-modules/botocore-stubs/default.nix
+++ pkgs/development/python-modules/botocore-stubs/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "botocore-stubs";
-  version = "1.34.156";
+  version = "1.34.157";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "botocore_stubs";
     inherit version;
-    hash = "sha256-u1rRaHzTYwNcPwAIVFQkdgQEVWKUY9Tk2x6/QQSKTAY=";
+    hash = "sha256-uKG3fQlui+6Jxj+HGcc8vSdOAn5WaaqHNQOrnw2AQ+U=";
   };
 
   nativeBuildInputs = [ poetry-core ];
