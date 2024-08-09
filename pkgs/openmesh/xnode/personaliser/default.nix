31650779ced0acb85ce859ad564a2933d4a2d7b8
J Lynn <j@jaesharp.com>
2024-08-09 11:35:30 +1000

    xnode-personaliser: remove package definition duplication by importing package.nix from src directly

--- pkgs/openmesh/xnode/personaliser/default.nix
+++ pkgs/openmesh/xnode/personaliser/default.nix
@@ -6,27 +6,15 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "Openmesh-Network";
     repo = name;
-    rev = "16ac1d5862fffd18a4da185b6e6ef3ac0a3904cb";
-    sha256 = "sha256-U+9oWxqqbAtXiaEQ/7li078LG130kOQ6KcbnEY9OAIs=";
+    rev = "655cff7c0a8869fbc72323c3c76daec930a8f605";
+    sha256 = "sha256-Dd678TQMkCS1kYKFeqXjk9nc2nKsE9ndPVcoc25k/xc=";
   };
-  inputs = with pkgs; [ jq curl gnugrep ];
-  script = (pkgs.writeScriptBin name (builtins.readFile "${src}/src/xnode-personaliser.sh")).overrideAttrs(old: {
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-        });
 in
-pkgs.symlinkJoin {
-  name = name;
-  version = version;
-
-  buildInputs = [ pkgs.makeWrapper ];
-  paths = [ script ] ++ inputs;
-  postBuild = "wrapProgram $out/bin/${name} --prefix PATH : $out/bin";
-
-  meta = with lib; {
-    homepage = "https://openmesh.network/";
-    description = "Installs a simple script that takes a script from any applicable personalisation interface (kernel cmdline base64/etc) and provides a well defined runtime environment.";
-    mainProgram = "xnode-personaliser";
-    license = with licenses; [ mit ];
-    maintainers = with maintainers; [ j-openmesh ];
-  };
+(import "${src}/package.nix") {
+  inherit stdenv;
+  inherit pkgs;
+  inherit lib;
+  package-name = name;
+  package-version = version;
+  package-src = src;
 }
