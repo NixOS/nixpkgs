c1aafecb92d23f0ea76116d4d169d661f9aafd74
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-10 13:38:00 +0000

    gh-i: 0.0.7 -> 0.0.8

--- pkgs/by-name/gh/gh-i/package.nix
+++ pkgs/by-name/gh/gh-i/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "gh-i";
-  version = "0.0.7";
+  version = "0.0.8";
 
   src = fetchFromGitHub {
     owner = "gennaro-tedesco";
     repo = "gh-i";
     rev = "v${version}";
-    hash = "sha256-nVMWeXssSpfWsD20+qLvQp6Wlrp/DiVNLBR6qnvuD2M=";
+    hash = "sha256-fW9T7B/97kI8zjaTvJHOkEUGVVM+ysOxZzqVNeOfVkc=";
   };
 
   vendorHash = "sha256-TSl+7N3W3BeW8UWxUdTv3cob2P7eLvO+80BLqcbhanQ=";
