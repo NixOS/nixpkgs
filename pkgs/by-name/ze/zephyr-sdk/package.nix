35ef2d8fb68551abcf2462ec916119904c411bfe
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-08-10 16:07:47 -0300

    Update package.nix
    applied nixfmt-rfc-style

--- pkgs/by-name/ze/zephyr-sdk/package.nix
+++ pkgs/by-name/ze/zephyr-sdk/package.nix
@@ -1,5 +1,13 @@
-{ stdenvNoCC, fetchurl, cacert, which, wget, cmake, lib
-, system ? builtins.currentSystem }:
+{
+  stdenvNoCC,
+  fetchurl,
+  cacert,
+  which,
+  wget,
+  cmake,
+  lib,
+  system ? builtins.currentSystem,
+}:
 let
   pname = "zephyr-sdk";
   version = "0.16.8";
@@ -9,18 +17,21 @@ let
     x86_64-darwin = "macos-x86_64";
     aarch64-darwin = "macos-aarch64";
   };
-  url =
-    "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/${pname}-${version}_${
-      system_fixup.${system}
-    }.tar.xz";
-in stdenvNoCC.mkDerivation {
+  url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/${pname}-${version}_${system_fixup.${system}}.tar.xz";
+in
+stdenvNoCC.mkDerivation {
   inherit pname version;
   src = fetchurl {
     inherit url;
     sha256 = "0smwgpzikdd1yxj1nw815qg6imfynjwql7pcy6m2ci8yfl940knb";
   };
   sourceRoot = ".";
-  nativeBuildInputs = [ cacert which wget cmake ];
+  nativeBuildInputs = [
+    cacert
+    which
+    wget
+    cmake
+  ];
   dontUseCmakeConfigure = true;
 
   # Zephyr SDK contains multiple patched packages
@@ -37,18 +48,22 @@ in stdenvNoCC.mkDerivation {
 
   meta = with lib; {
     description = "Software Develpment Kit for Zephyr RTOS";
-    longDescription =
-      "The Zephyr Software Development Kit (SDK) includes the toolchains for all supported target architectures as well as the host tools, such as QEMU and OpenOCD, for testing and debugging the Zephyr RTOS.";
+    longDescription = "The Zephyr Software Development Kit (SDK) includes the toolchains for all supported target architectures as well as the host tools, such as QEMU and OpenOCD, for testing and debugging the Zephyr RTOS.";
     homepage = "https://github.com/zephyrproject-rtos/sdk-ng";
     licence = licences.asl20;
-    platforms =
-      [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    maintainers = [{
-      email = "giancarlo.bonvenuto@gmail.com";
-      github = "gBonvenuto";
-      githubId = 25495557;
-      name = "Giancarlo Bonvenuto";
-    }];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    maintainers = [
+      {
+        email = "giancarlo.bonvenuto@gmail.com";
+        github = "gBonvenuto";
+        githubId = 25495557;
+        name = "Giancarlo Bonvenuto";
+      }
+    ];
   };
-
 }
