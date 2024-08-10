b8f7c30f68e5bb6b80c1be10d2934b621ead8209
Thibault Duplessis <t@lichess.org>
2024-08-10 10:34:53 +0200

    maintainers: add thibaultd

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20464,6 +20464,12 @@
     name = "Thiago K. Okada";
     matrix = "@k0kada:matrix.org";
   };
+  thibaultd = {
+    email = "t@lichess.org";
+    github = "ornicar";
+    githubId = 140370;
+    name = "Thibault D";
+  };
   thibaultlemaire = {
     email = "thibault.lemaire@protonmail.com";
     github = "ThibaultLemaire";
