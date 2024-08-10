aff9ad07733431f2402cfcfc702aa708c67f5cf8
Pavel Sobolev <contact@paveloom.dev>
2024-08-10 17:25:59 +0300

    subtitleedit: 4.0.6 -> 4.0.7

--- pkgs/by-name/su/subtitleedit/package.nix
+++ pkgs/by-name/su/subtitleedit/package.nix
@@ -19,13 +19,13 @@
 
 stdenv.mkDerivation rec {
   pname = "subtitleedit";
-  version = "4.0.6";
+  version = "4.0.7";
 
   src = fetchzip {
     url = "https://github.com/SubtitleEdit/subtitleedit/releases/download/${version}/SE${
       lib.replaceStrings [ "." ] [ "" ] version
     }.zip";
-    hash = "sha256-ipAqnF7rpSXccWkyTysUBrD0/mnv5AEA5GuxMJjW9Dg=";
+    hash = "sha256-7BTW0wqlxyxtXu4aBVQXMQ1mUdSINn+S6W7XPL5pcSI=";
     stripRoot = false;
   };
 
@@ -77,7 +77,7 @@ stdenv.mkDerivation rec {
       exec = "subtitleedit";
       icon = "subtitleedit";
       comment = meta.description;
-      categories = [ "Video" ];
+      categories = [ "AudioVideo" ];
     })
   ];
 
