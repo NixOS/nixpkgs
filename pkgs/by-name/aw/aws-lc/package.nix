426740c0a970ced712683f8140e1a989f7442f93
Theo Paris <theo@tinted.dev>
2024-08-10 01:04:27 +0000

    maintainers: add theoparis
    Signed-off-by: Theo Paris <theo@tinted.dev>

---
+++ pkgs/by-name/aw/aws-lc/package.nix
@@ -0,0 +1,80 @@
+{
+  lib,
+  stdenv,
+  cmakeMinimal,
+  fetchFromGitHub,
+  ninja,
+  testers,
+  aws-lc,
+  ...
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "aws-lc";
+  version = "1.33.0";
+
+  src = fetchFromGitHub {
+    owner = "aws";
+    repo = "aws-lc";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-YTvKpTaZnmYlEiKS/W5FlmiEMY7MDxgplXIENxRjI3Q=";
+  };
+
+  outputs = [
+    "out"
+    "bin"
+    "dev"
+  ];
+
+  nativeBuildInputs = [
+    cmakeMinimal
+    ninja
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "BUILD_SHARED_LIBS" (!stdenv.targetPlatform.isStatic))
+    "-GNinja"
+    "-DDISABLE_GO=ON"
+  ];
+
+  env.NIX_CFLAGS_COMPILE = toString (
+    lib.optionals stdenv.cc.isGNU [
+      # Needed with GCC 12 but breaks on darwin (with clang)
+      "-Wno-error=stringop-overflow"
+    ]
+  );
+
+  postFixup = ''
+    for f in $out/lib/crypto/cmake/*/crypto-targets.cmake; do
+      substituteInPlace "$f" \
+        --replace-fail 'INTERFACE_INCLUDE_DIRECTORIES "''${_IMPORT_PREFIX}/include"' 'INTERFACE_INCLUDE_DIRECTORIES ""'
+    done
+  '';
+
+  passthru.tests = {
+    version = testers.testVersion {
+      package = aws-lc;
+      command = "bssl version";
+    };
+    pkg-config = testers.hasPkgConfigModules {
+      package = aws-lc;
+      moduleNames = [
+        "libcrypto"
+        "libssl"
+        "openssl"
+      ];
+    };
+  };
+
+  meta = {
+    description = "General-purpose cryptographic library maintained by the AWS Cryptography team for AWS and their customers";
+    homepage = "https://github.com/aws/aws-lc";
+    license = [
+      lib.licenses.asl20 # or
+      lib.licenses.isc
+    ];
+    maintainers = [ lib.maintainers.theoparis ];
+    platforms = lib.platforms.all;
+    mainProgram = "bssl";
+  };
+})
