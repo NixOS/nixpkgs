b02d6884e25886698c18662adcdc3f8d77148bf9
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-10 05:14:37 +0000

    mill: 0.11.10 -> 0.11.11

--- pkgs/development/tools/build-managers/mill/default.nix
+++ pkgs/development/tools/build-managers/mill/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mill";
-  version = "0.11.10";
+  version = "0.11.11";
 
   src = fetchurl {
     url = "https://github.com/com-lihaoyi/mill/releases/download/${finalAttrs.version}/${finalAttrs.version}-assembly";
-    hash = "sha256-/IVWeTUu3piV80SfbqeSG6tnRUDTpAyoBcbeCVelUpc=";
+    hash = "sha256-tTLLY/4rTnV/Mj6/InK7Pfl/49feeWivEWURusRT6Bk=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
