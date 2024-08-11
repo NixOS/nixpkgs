57662bfbd2685e0771f061280ff2dadf753bbd99
Jan Tojnar <jtojnar@gmail.com>
2024-08-11 18:33:43 +0200

    gnome.gnome-nettool: Format with nixfmt

--- pkgs/desktops/gnome/apps/gnome-nettool/default.nix
+++ pkgs/desktops/gnome/apps/gnome-nettool/default.nix
@@ -1,22 +1,23 @@
-{ stdenv
-, lib
-, fetchurl
-, fetchpatch
-, desktop-file-utils
-, itstool
-, meson
-, ninja
-, pkg-config
-, python3
-, wrapGAppsHook3
-, glib
-, gtk3
-, libgtop
-, dnsutils
-, iputils
-, nmap
-, inetutils
-, gnome
+{
+  stdenv,
+  lib,
+  fetchurl,
+  fetchpatch,
+  desktop-file-utils,
+  itstool,
+  meson,
+  ninja,
+  pkg-config,
+  python3,
+  wrapGAppsHook3,
+  glib,
+  gtk3,
+  libgtop,
+  dnsutils,
+  iputils,
+  nmap,
+  inetutils,
+  gnome,
 }:
 
 stdenv.mkDerivation rec {
@@ -24,7 +25,7 @@ stdenv.mkDerivation rec {
   version = "42.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
+    url = "mirror://gnome/sources/gnome-nettool/${lib.versions.major version}/gnome-nettool-${version}.tar.xz";
     sha256 = "pU8p7vIDiu5pVRyLGcpPdY5eueIJCkvGtWM9/wGIdR8=";
   };
 
@@ -60,19 +61,21 @@ stdenv.mkDerivation rec {
 
   preFixup = ''
     gappsWrapperArgs+=(
-      --prefix PATH : "${lib.makeBinPath [
-        dnsutils # for dig
-        iputils # for ping
-        nmap # for nmap
-        inetutils # for ping6, traceroute, whois
-      ]}"
+      --prefix PATH : "${
+        lib.makeBinPath [
+          dnsutils # for dig
+          iputils # for ping
+          nmap # for nmap
+          inetutils # for ping6, traceroute, whois
+        ]
+      }"
     )
   '';
 
   passthru = {
     updateScript = gnome.updateScript {
-      packageName = pname;
-      attrPath = "gnome.${pname}";
+      packageName = "gnome-nettool";
+      attrPath = "gnome.gnome-nettool";
     };
   };
 
