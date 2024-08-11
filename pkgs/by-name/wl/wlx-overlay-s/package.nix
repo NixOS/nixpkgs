41908b4abb51b73a7f306dd885dd09871574d9c7
Sefa Eyeoglu <contact@scrumplex.net>
2024-08-11 16:28:30 +0200

    wlx-overlay-s: format using nixfmt
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/by-name/wl/wlx-overlay-s/package.nix
+++ pkgs/by-name/wl/wlx-overlay-s/package.nix
@@ -1,23 +1,26 @@
-{ lib
-, stdenv
-, rustPlatform
-, fetchFromGitHub
-, alsa-lib
-, dbus
-, fontconfig
-, libxkbcommon
-, makeWrapper
-, nix-update-script
-, openvr
-, openxr-loader
-, pipewire
-, pkg-config
-, pulseaudio
-, shaderc
-, testers
-, wayland
-, wlx-overlay-s
-, xorg
+{
+  alsa-lib,
+  dbus,
+  fetchFromGitHub,
+  fontconfig,
+  lib,
+  libX11,
+  libXext,
+  libXrandr,
+  libxkbcommon,
+  makeWrapper,
+  nix-update-script,
+  openvr,
+  openxr-loader,
+  pipewire,
+  pkg-config,
+  pulseaudio,
+  rustPlatform,
+  shaderc,
+  stdenv,
+  testers,
+  wayland,
+  wlx-overlay-s,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -56,9 +59,9 @@ rustPlatform.buildRustPackage rec {
     openvr
     openxr-loader
     pipewire
-    xorg.libX11
-    xorg.libXext
-    xorg.libXrandr
+    libX11
+    libXext
+    libXrandr
   ];
 
   env.SHADERC_LIB_DIR = "${lib.getLib shaderc}/lib";
