cf621377cc2ae5d2a78b066fbcb8c751cc3d0ba5
Jan Tojnar <jtojnar@gmail.com>
2024-08-11 18:34:05 +0200

    gnome.gnome-mines: Format with nixfmt

--- pkgs/desktops/gnome/games/gnome-mines/default.nix
+++ pkgs/desktops/gnome/games/gnome-mines/default.nix
@@ -1,20 +1,54 @@
-{ lib, stdenv, fetchurl, meson, ninja, vala, pkg-config, gnome, adwaita-icon-theme, gtk3, wrapGAppsHook3
-, librsvg, gettext, itstool, python3, libxml2, libgnome-games-support, libgee, desktop-file-utils }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  meson,
+  ninja,
+  vala,
+  pkg-config,
+  gnome,
+  adwaita-icon-theme,
+  gtk3,
+  wrapGAppsHook3,
+  librsvg,
+  gettext,
+  itstool,
+  python3,
+  libxml2,
+  libgnome-games-support,
+  libgee,
+  desktop-file-utils,
+}:
 
 stdenv.mkDerivation rec {
   pname = "gnome-mines";
   version = "40.1";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
+    url = "mirror://gnome/sources/gnome-mines/${lib.versions.major version}/gnome-mines-${version}.tar.xz";
     sha256 = "NQLps/ccs7LnEcDmAZGH/rzCvKh349RW3KtwD3vjEnI=";
   };
 
   nativeBuildInputs = [
-    meson ninja vala pkg-config gettext itstool python3
-    libxml2 wrapGAppsHook3 desktop-file-utils
+    meson
+    ninja
+    vala
+    pkg-config
+    gettext
+    itstool
+    python3
+    libxml2
+    wrapGAppsHook3
+    desktop-file-utils
   ];
-  buildInputs = [ gtk3 librsvg adwaita-icon-theme libgnome-games-support libgee ];
+
+  buildInputs = [
+    gtk3
+    librsvg
+    adwaita-icon-theme
+    libgnome-games-support
+    libgee
+  ];
 
   postPatch = ''
     chmod +x build-aux/meson_post_install.py
