2ce7f875993e3ddcb3249fdbe483175f726115ce
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-12 16:21:39 +0200

    python3Packages.doxmlparser: init at 1.12.0

---
+++ pkgs/development/tools/documentation/doxygen/doxmlparser.nix
@@ -0,0 +1,32 @@
+{
+  buildPythonPackage,
+  doxygen,
+  lxml,
+  setuptools,
+  six,
+}:
+buildPythonPackage rec {
+  inherit (doxygen) version src;
+  pname = "doxmlparser";
+
+  sourceRoot = "${src.name}/addon/doxmlparser";
+
+  build-system = [ setuptools ];
+
+  propagatedBuildInputs = [
+    lxml
+    six
+  ];
+
+  pythonImportsCheck = [ "doxmlparser" ];
+
+  meta = {
+    inherit (doxygen.meta)
+      license
+      homepage
+      changelog
+      platforms
+      ;
+    description = "easier to parse the XML output produced by doxygen";
+  };
+}
