826006153a2098c5b3e0fd0557ca6a63f578148a
Pratham Patel <prathampatel@thefossguy.com>
2024-08-12 13:40:25 +0530

    ubootNanoPCT6: include the SPI binary

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -303,7 +303,7 @@ in {
     extraMeta.platforms = ["aarch64-linux"];
     BL31 = "${armTrustedFirmwareRK3588}/bl31.elf";
     ROCKCHIP_TPL = rkbin.TPL_RK3588;
-    filesToInstall = [ "u-boot.itb" "idbloader.img" "u-boot-rockchip.bin" ];
+    filesToInstall = [ "u-boot.itb" "idbloader.img" "u-boot-rockchip.bin" "u-boot-rockchip-spi.bin" ];
   };
 
   ubootNovena = buildUBoot {
