03d2bb13bfa913f48acc4fbce452a65c829712ad
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-12 05:58:50 +0000

    rosa: 1.2.39 -> 1.2.43

--- pkgs/by-name/ro/rosa/package.nix
+++ pkgs/by-name/ro/rosa/package.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "rosa";
-  version = "1.2.39";
+  version = "1.2.43";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "rosa";
     rev = "v${version}";
-    hash = "sha256-K1FGiUNXSyCTmF//dculpnkTyn3hfqWrOiMUGk9kHrA=";
+    hash = "sha256-1/go7mL2uRzzV/iiTXsgZHGNW8EIONwEnb4jcMiKkv4=";
   };
   vendorHash = null;
 
