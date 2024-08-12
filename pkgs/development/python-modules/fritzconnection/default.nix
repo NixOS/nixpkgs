c5fffea35a385f5efe114cab090b566393537719
Robert Schütz <mail@dotlambda.de>
2024-08-12 12:57:29 -0700

    python312Packages.fritzconnection: 1.13.2 -> 1.14.0
    Diff: https://github.com/kbr/fritzconnection/compare/refs/tags/1.13.2...1.14.0
    
    Changelog: https://fritzconnection.readthedocs.io/en/1.14.0/sources/version_history.html

--- pkgs/development/python-modules/fritzconnection/default.nix
+++ pkgs/development/python-modules/fritzconnection/default.nix
@@ -6,23 +6,26 @@
   pytestCheckHook,
   requests,
   segno,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "fritzconnection";
-  version = "1.13.2";
-  format = "setuptools";
+  version = "1.14.0";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "kbr";
-    repo = pname;
+    repo = "fritzconnection";
     rev = "refs/tags/${version}";
-    hash = "sha256-nWXtXhF2pUBxHdrivi4DA7+bFiZPyxb2nqsiN3j4HdI=";
+    hash = "sha256-1LLfSEOKqUIhWIR/RQEG0Bp41d908hAKDlslJlWCHys=";
   };
 
-  propagatedBuildInputs = [ requests ];
+  build-system = [ setuptools ];
+
+  dependencies = [ requests ];
 
   optional-dependencies = {
     qr = [ segno ];
