4ba28ed023b34c7e3a661ae4b359445f1aa5ae93
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-12 19:39:41 -0300

    emacsPackages.lspce-module: combine both files
    It is ugly as hell, but at least it is overridable.

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/lspce/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/lspce/package.nix
@@ -1,26 +1,67 @@
 {
   lib,
   f,
-  lspce-module,
+  fetchFromGitHub,
   markdown-mode,
   melpaBuild,
+  nix-update-script,
+  rustPlatform,
   yasnippet,
 }:
 
-melpaBuild {
-  pname = "lspce";
-  inherit (lspce-module) version src meta;
+let
+  self = melpaBuild {
+    pname = "lspce";
+    version = "1.1.0-unstable-2024-08-04";
 
-  packageRequires = [
-    f
-    markdown-mode
-    yasnippet
-  ];
+    src = fetchFromGitHub {
+      owner = "zbelial";
+      repo = "lspce";
+      rev = "59b9c0551ee34440d3cc754313ac8076f7076f54";
+      hash = "sha256-VpIsAteBEoENhejS0QqAZRoEYgaDpVCdPqrgrj4OcIU=";
+    };
 
-  # lspce-module.so is needed to compile lspce.el
-  files = ''(:defaults "${lib.getLib lspce-module}/lib/lspce-module.*")'';
+    lspce-module = rustPlatform.buildRustPackage {
+      pname = "lspce-module";
+      inherit (self) version src;
 
-  passthru = {
-    inherit lspce-module;
+      cargoHash = "sha256-q0xUOzDRpiAdvtH3rzCOZRYthGR6MvbiXB4a7cDgf9A=";
+
+      checkFlags = [
+        # flaky test
+        "--skip=msg::tests::serialize_request_with_null_params"
+      ];
+
+      # rename module without changing either suffix or location
+      # use for loop because there seems to be two modules on darwin systems
+      # https://github.com/zbelial/lspce/issues/7#issue-1783708570
+      postInstall = ''
+      for f in $out/lib/*; do
+        mv --verbose $f $out/lib/lspce-module.''${f##*.}
+      done
+    '';
+    };
+
+    packageRequires = [
+      f
+      markdown-mode
+      yasnippet
+    ];
+
+    # lspce-module.so is needed to compile lspce.el
+    files = ''(:defaults "${lib.getLib self.lspce-module}/lib/lspce-module.*")'';
+
+    passthru = {
+      updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+    };
+
+    meta = {
+      homepage = "https://github.com/zbelial/lspce";
+      description = "LSP Client for Emacs implemented as a module using Rust";
+      license = lib.licenses.gpl3Only;
+      maintainers = with lib.maintainers; [ AndersonTorres ];
+    };
   };
-}
+in
+self
+
