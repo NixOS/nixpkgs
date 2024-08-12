edd2b5e185a5eb5dbcf0faeca491a771b0f35bc8
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-08-13 00:27:41 +0200

    python312Packages.leb128: 1.0.7 -> 1.0.8
    https://github.com/mohanson/leb128/releases/tag/v1.0.8

--- pkgs/development/python-modules/leb128/default.nix
+++ pkgs/development/python-modules/leb128/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "leb128";
-  version = "1.0.7";
+  version = "1.0.8";
   pyproject = true;
 
   # fetchPypi doesn't include files required for tests
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     owner = "mohanson";
     repo = "leb128";
     rev = "refs/tags/v${version}";
-    hash = "sha256-17C0Eic8T2PFkuIGExcrfb3b1HldaSBAPSh5TtRBUuU=";
+    hash = "sha256-7ZjDqxGUANk3FfB3HPTc5CB5YcIi2ee0igXWAYXaZ88=";
   };
 
   build-system = [ setuptools ];
@@ -26,6 +26,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "leb128" ];
 
   meta = with lib; {
+    changelog = "https://github.com/mohanson/leb128/releases/tag/v${version}";
     description = "Utility to encode and decode Little Endian Base 128";
     homepage = "https://github.com/mohanson/leb128";
     license = licenses.mit;
