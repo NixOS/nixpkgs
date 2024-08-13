34317afe70113a4c9409ff6c608f715f6f46f8f2
Pavel Sobolev <contact@paveloom.dev>
2024-08-13 20:42:35 +0300

    picard: reformat

--- pkgs/by-name/pi/picard/package.nix
+++ pkgs/by-name/pi/picard/package.nix
@@ -1,24 +1,21 @@
-{ lib
-# Python 3.12 demonstrates a peculiar segmentation fault with pyqt5. Using
-# pyqt6 with Python 3.12 should work, but this is not released yet.
-, python311Packages
-, fetchFromGitHub
+{
+  lib,
+  # Python 3.12 demonstrates a peculiar segmentation fault with pyqt5. Using
+  # pyqt6 with Python 3.12 should work, but this is not released yet.
+  python311Packages,
+  fetchFromGitHub,
 
-, chromaprint
-, gettext
-, qt5
+  chromaprint,
+  gettext,
+  qt5,
 
-, enablePlayback ? true
-, gst_all_1
+  enablePlayback ? true,
+  gst_all_1,
 }:
 
 let
   pythonPackages = python311Packages;
-  pyqt5 =
-    if enablePlayback then
-      pythonPackages.pyqt5-multimedia
-    else
-      pythonPackages.pyqt5;
+  pyqt5 = if enablePlayback then pythonPackages.pyqt5-multimedia else pythonPackages.pyqt5;
 in
 pythonPackages.buildPythonApplication rec {
   pname = "picard";
@@ -33,23 +30,23 @@ pythonPackages.buildPythonApplication rec {
     hash = "sha256-+++NDJzXw4tA5eQd24r+l3UK3YS8Jy1t9WNiEU9sH0Q=";
   };
 
-  nativeBuildInputs = [
-    gettext
-    qt5.wrapQtAppsHook
-  ] ++ lib.optionals (pyqt5.multimediaEnabled) [
-    gst_all_1.gst-libav
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-vaapi
-    gst_all_1.gstreamer
-  ];
+  nativeBuildInputs =
+    [
+      gettext
+      qt5.wrapQtAppsHook
+    ]
+    ++ lib.optionals (pyqt5.multimediaEnabled) [
+      gst_all_1.gst-libav
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-vaapi
+      gst_all_1.gstreamer
+    ];
 
   buildInputs = [
     qt5.qtbase
     qt5.qtwayland
-  ] ++ lib.optionals (pyqt5.multimediaEnabled) [
-    qt5.qtmultimedia.bin
-  ];
+  ] ++ lib.optionals (pyqt5.multimediaEnabled) [ qt5.qtmultimedia.bin ];
 
   propagatedBuildInputs = with pythonPackages; [
     chromaprint
@@ -63,18 +60,24 @@ pythonPackages.buildPythonApplication rec {
     pyyaml
   ];
 
-  setupPyGlobalFlags = [ "build" "--disable-autoupdate" "--localedir=$out/share/locale" ];
+  setupPyGlobalFlags = [
+    "build"
+    "--disable-autoupdate"
+    "--localedir=$out/share/locale"
+  ];
 
   preCheck = ''
     export HOME=$(mktemp -d)
   '';
 
   # In order to spare double wrapping, we use:
-  preFixup = ''
-    makeWrapperArgs+=("''${qtWrapperArgs[@]}")
-  '' + lib.optionalString (pyqt5.multimediaEnabled) ''
-    makeWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
-  '';
+  preFixup =
+    ''
+      makeWrapperArgs+=("''${qtWrapperArgs[@]}")
+    ''
+    + lib.optionalString (pyqt5.multimediaEnabled) ''
+      makeWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
+    '';
 
   meta = with lib; {
     homepage = "https://picard.musicbrainz.org";
