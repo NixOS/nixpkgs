892daf95d5dbde24bbed61b216a8205a7d830059
Abhishek Adhikari <abhiayush23@gmail.com>
2024-08-13 13:09:44 +0530

    pymssql: format

--- pkgs/development/python-modules/pymssql/default.nix
+++ pkgs/development/python-modules/pymssql/default.nix
@@ -1,15 +1,5 @@
-{
-  lib,
-  buildPythonPackage,
-  fetchPypi,
-  freetds,
-  krb5,
-  openssl,
-  cython,
-  setuptools-scm,
-  sqlparse,
-  tomli
-}:
+{ lib, buildPythonPackage, fetchPypi, freetds, krb5, openssl, cython
+, setuptools-scm, sqlparse, tomli }:
 
 buildPythonPackage rec {
   pname = "pymssql";
@@ -21,28 +11,17 @@ buildPythonPackage rec {
     hash = "sha256-8DTka1aAYdFxSPHe3qZI+dX2tzQOjP/g7bqhcTy0q6s=";
   };
 
-  build-system = [
-    setuptools-scm
-    tomli
-    cython
-  ];
+  build-system = [ setuptools-scm tomli cython ];
 
-  buildInputs = [
-    krb5
-    openssl
-    freetds
-  ];
+  buildInputs = [ krb5 openssl freetds ];
 
-  propagatedBuildInputs = [
-    sqlparse
-  ];
+  propagatedBuildInputs = [ sqlparse ];
 
-  pythonImportsCheck = [
-    "pymssql"
-  ];
+  pythonImportsCheck = [ "pymssql" ];
 
   meta = with lib; {
-    description = "A simple database interface for Python that builds on top of FreeTDS to provide a Python DB-API (PEP-249) interface to Microsoft SQL Server.";
+    description =
+      "A simple database interface for Python that builds on top of FreeTDS to provide a Python DB-API (PEP-249) interface to Microsoft SQL Server.";
     homepage = "https://github.com/pymssql/pymssql";
     license = licenses.lgpl21;
     maintainers = [ maintainers.sith-lord-vader ];
