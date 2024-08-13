129eace28d9ffdba9280242c2582044853fbe4bb
Doron Behar <doron.behar@gmail.com>
2024-08-13 20:47:07 +0300

    python312Packages.gast: modernize

--- pkgs/development/python-modules/gast/default.nix
+++ pkgs/development/python-modules/gast/default.nix
@@ -16,8 +16,8 @@ buildPythonPackage rec {
 
   src = fetchFromGitHub {
     owner = "serge-sans-paille";
-    repo = pname;
-    rev = version;
+    repo = "gast";
+    rev = "refs/tags/${version}";
     hash = "sha256-0y2bHT7YEfTvDxTm6yLl3GmnPUYEieoGEnwkzfA6mOg=";
   };
 
@@ -28,11 +28,11 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "gast" ];
 
-  meta = with lib; {
+  meta = {
     description = "Compatibility layer between the AST of various Python versions";
     homepage = "https://github.com/serge-sans-paille/gast/";
-    license = licenses.bsd3;
-    maintainers = with maintainers; [
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [
       jyp
       cpcloud
     ];
