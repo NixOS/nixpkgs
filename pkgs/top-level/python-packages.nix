cd8b61df94ae575b6daec28517230c18b4273c4c
Ben Darwin <bcdarwin@gmail.com>
2024-08-13 15:42:53 -0400

    python312Packages.medvol: init at 0.0.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7614,6 +7614,8 @@ self: super: with self; {
 
   medpy = callPackage ../development/python-modules/medpy { };
 
+  medvol = callPackage ../development/python-modules/medvol { };
+
   meeko = callPackage ../development/python-modules/meeko { };
 
   meep = callPackage ../development/python-modules/meep { };
