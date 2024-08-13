a123c649342f591a283750559d4cb0b6f9cf7bca
matt1432 <matt@nelim.org>
2024-08-13 18:21:32 -0400

    maintainers: add matt1432

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13035,6 +13035,12 @@
     github = "matrss";
     githubId = 9308656;
   };
+  matt1432 = {
+    email = "matt@nelim.org";
+    github = "matt1432";
+    githubId = 98562002;
+    name = "Mathis H.";
+  };
   mattchrist = {
     email = "nixpkgs-matt@christ.systems";
     github = "mattchrist";
