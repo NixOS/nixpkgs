654bd512db66d549e778ac534491b5b2fb553536
Matt Sturgeon <matt@sturgeon.me.uk>
2024-08-14 18:44:37 +0100

    test ideas

--- lib/tests/modules.sh
+++ lib/tests/modules.sh
@@ -135,6 +135,11 @@ checkConfigError "attribute 'age' in selection path 'config.people.alice.age' no
 # record optional field with definition
 checkConfigOutput '^27$' config.people.mike.age ./declare-record-optional-field.nix ./define-record-mike.nix
 
+#TODO:
+# - test empty definitions
+# - test neseted records
+# - test nested optional records
+# - etc?
 
 
 if false; then
