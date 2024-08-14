d656924c439c03fb7649590f4a2411079b75c21d
Alexandru Scvortov <code@scvalex.net>
2024-08-14 11:43:51 +0100

    labplot: 2.10.1 -> 2.11.1

--- pkgs/applications/science/math/labplot/default.nix
+++ pkgs/applications/science/math/labplot/default.nix
@@ -44,24 +44,16 @@
 
 stdenv.mkDerivation rec {
   pname = "labplot";
-  version = "2.10.1";
+  version = "2.11.1";
 
   src = fetchurl {
     url = "mirror://kde/stable/labplot/labplot-${version}.tar.xz";
-    sha256 = "sha256-K24YFRfPtuDf/3uJXz6yDHzjWeZzLThUXgdXya6i2u8=";
+    sha256 = "sha256-U6pqyN85Mk2ZRj5g2I3iU0azko2luw8hCwVjSJBGZ50=";
   };
 
   cmakeFlags = [
     # Disable Vector BLF since it depends on DBC parser which fails to be detected
     "-DENABLE_VECTOR_BLF=OFF"
-  ];
-
-  patches = [
-    (fetchpatch {
-      name = "matio-fix-compilation-for-latest-version-1.5.27.patch";
-      url = "https://github.com/KDE/labplot/commit/d6142308ffa492d9f7cea00fad3b4cd1babfd00c.patch";
-      hash = "sha256-qD5jj6GxBKbQezKJb1Z8HnwFO84WJBGQDawS/6o/wHE=";
-    })
   ];
 
   nativeBuildInputs = [
