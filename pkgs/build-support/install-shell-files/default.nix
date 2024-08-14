8b674370c42fd5312d53edd2002af0978ff3f2b9
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-14 11:59:49 -0300

    installShellFiles: nixfmt-rfc-style

--- pkgs/build-support/install-shell-files/default.nix
+++ pkgs/build-support/install-shell-files/default.nix
@@ -4,9 +4,8 @@
 let
   setupHook = makeSetupHook { name = "install-shell-files"; } ./setup-hook.sh;
 in
-
 setupHook.overrideAttrs (oldAttrs: {
-  passthru = (oldAttrs.passthru or {}) // {
+  passthru = (oldAttrs.passthru or { }) // {
     tests = callPackage ./tests { };
   };
 })
