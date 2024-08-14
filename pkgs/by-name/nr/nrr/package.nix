1460e067737106a50a16597dcb9181b306c4d567
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-08-14 17:22:57 +0800

    nrr: add meta.homepage

--- pkgs/by-name/nr/nrr/package.nix
+++ pkgs/by-name/nr/nrr/package.nix
@@ -43,6 +43,7 @@ rustPlatform.buildRustPackage rec {
 
   meta = with lib; {
     description = "Minimal, blazing fast npm scripts runner";
+    homepage = "https://github.com/ryanccn/nrr";
     maintainers = with maintainers; [ ryanccn ];
     license = licenses.gpl3Only;
     mainProgram = "nrr";
