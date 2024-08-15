ca18f1c1912635c6e6f06c2e5dc598198ab33434
Kerstin Humm <kerstin@erictapen.name>
2024-08-15 14:09:11 +0200

    weblate: 5.6.2 -> 5.7
    Changelog: https://github.com/WeblateOrg/weblate/releases/tag/weblate-5.7
    
    Also had to rework django-compressor settings and aligned them more
    closely with the official docs:
    https://cheat.readthedocs.io/en/latest/django/compressor.html

--- pkgs/by-name/we/weblate/package.nix
+++ pkgs/by-name/we/weblate/package.nix
@@ -34,7 +34,7 @@ let
 in
 python.pkgs.buildPythonApplication rec {
   pname = "weblate";
-  version = "5.6.2";
+  version = "5.7";
 
   pyproject = true;
 
@@ -47,7 +47,7 @@ python.pkgs.buildPythonApplication rec {
     owner = "WeblateOrg";
     repo = "weblate";
     rev = "weblate-${version}";
-    sha256 = "sha256-t/hnigsKjdWCkUd8acNWhYVFmZ7oGn74+12347MkFgM=";
+    sha256 = "sha256-h5+0lOMD+H0ehtZ0bngA9bI5va1I5KjZH9boaEtXJPo=";
   };
 
   patches = [
@@ -55,27 +55,15 @@ python.pkgs.buildPythonApplication rec {
     ./cache.lock.patch
   ];
 
-  # Relax dependency constraints
-  # mistletoe: https://github.com/WeblateOrg/weblate/commit/50df46a25dda2b7b39de86d4c65ecd7a685f62e6
-  # borgbackup: https://github.com/WeblateOrg/weblate/commit/355c81c977c59948535a98a35a5c05d7e6909703
-  # django-crispy-forms: https://github.com/WeblateOrg/weblate/commit/7b341c523ed9b3b41ecfbc5c92dd6156992e4f32
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace '"mistletoe>=1.3.0,<1.4"' '"mistletoe>=1.3.0,<1.5"' \
-      --replace '"borgbackup>=1.2.5,<1.3"' '"borgbackup>=1.2.5,<1.5"' \
-      --replace '"django-crispy-forms>=2.1,<2.3"' '"django-crispy-forms>=2.1,<2.4"'
-  '';
-
   build-system = with python.pkgs; [ setuptools ];
 
   # Build static files into a separate output
   postBuild =
     let
       staticSettings = writeText "static_settings.py" ''
-        STATIC_ROOT = os.environ["static"] + "/static"
-        COMPRESS_ENABLED = True
+        DEBUG = False
+        STATIC_ROOT = os.environ["static"]
         COMPRESS_OFFLINE = True
-        COMPRESS_ROOT = os.environ["static"] + "/compressor-cache"
         # So we don't need postgres dependencies
         DATABASES = {}
       '';
@@ -99,6 +87,7 @@ python.pkgs.buildPythonApplication rec {
     cryptography
     cssselect
     cython
+    cyrtranslit
     diff-match-patch
     django-appconf
     django-celery-beat
@@ -107,6 +96,8 @@ python.pkgs.buildPythonApplication rec {
     django-crispy-forms
     django-filter
     django-redis
+    django-otp
+    django-otp-webauthn
     django
     djangorestframework
     filelock
@@ -117,7 +108,6 @@ python.pkgs.buildPythonApplication rec {
     iniparse
     jsonschema
     lxml
-    misaka
     mistletoe
     nh3
     openpyxl
@@ -131,6 +121,7 @@ python.pkgs.buildPythonApplication rec {
     pyparsing
     python-dateutil
     python-redis-lock
+    qrcode
     rapidfuzz
     redis
     requests
