e45e3313af1c07fc1c482566592b8c87d5d7f312
Kerstin Humm <kerstin@erictapen.name>
2024-08-15 16:32:15 +0200

    pythonPackages.sqlalchemy-json: init at 0.7.0
    Co-Authored-By: Auguste Baum <auguste.apple@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15154,6 +15154,8 @@ self: super: with self; {
 
   sqlalchemy-i18n = callPackage ../development/python-modules/sqlalchemy-i18n { };
 
+  sqlalchemy-json = callPackage ../development/python-modules/sqlalchemy-json { };
+
   sqlalchemy-jsonfield = callPackage ../development/python-modules/sqlalchemy-jsonfield { };
 
   sqlalchemy-mixins = callPackage ../development/python-modules/sqlalchemy-mixins { };
