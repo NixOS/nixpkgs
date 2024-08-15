f75ba5aaa4b488bacf6228d52136792713cbc411
Ivan Mincik <ivan.mincik@gmail.com>
2024-08-15 05:48:27 +0200

    shapelib: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23689,8 +23689,6 @@ with pkgs;
   };
   csfml = callPackage ../development/libraries/csfml { };
 
-  shapelib = callPackage ../development/libraries/shapelib { };
-
   sharness = callPackage ../development/libraries/sharness { };
 
   shibboleth-sp = callPackage ../development/libraries/shibboleth-sp { };
