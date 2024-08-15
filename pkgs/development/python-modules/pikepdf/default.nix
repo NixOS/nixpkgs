9fa6d793dd061878784be4c3fa2ad7cb12c5cdab
Robert Schütz <mail@dotlambda.de>
2024-08-15 08:17:44 -0700

    python312Packages.pikepdf: 9.1.0 -> 9.1.1
    Diff: https://github.com/pikepdf/pikepdf/compare/v9.1.0...v9.1.1
    
    Changelog: https://github.com/pikepdf/pikepdf/blob/v9.1.1/docs/releasenotes/version9.rst

--- pkgs/development/python-modules/pikepdf/default.nix
+++ pkgs/development/python-modules/pikepdf/default.nix
@@ -25,7 +25,7 @@
 
 buildPythonPackage rec {
   pname = "pikepdf";
-  version = "9.1.0";
+  version = "9.1.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -40,7 +40,7 @@ buildPythonPackage rec {
     postFetch = ''
       rm "$out/.git_archival.txt"
     '';
-    hash = "sha256-rcL4kwF8aBHy/sxZiQsQGvN3i1geiPFyrv8nRSXUZSA=";
+    hash = "sha256-++je4tKnXr504Yl9VBH9dc9fycDrMstRN5hu4s/oe9w=";
   };
 
   patches = [
