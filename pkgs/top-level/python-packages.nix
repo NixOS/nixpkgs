8ed70a5da57249ad6ec8bb3853b49f99b3061528
Kerstin Humm <kerstin@erictapen.name>
2024-08-15 16:30:53 +0200

    python3Packages.slapd: init at 0.1.5
    (cherry picked from commit ea8280c3a479734bd0c1023c15431e9a3b252d85)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14659,6 +14659,8 @@ self: super: with self; {
 
   slack-sdk = callPackage ../development/python-modules/slack-sdk { };
 
+  slapd = callPackage ../development/python-modules/slapd { };
+
   sleekxmpp = callPackage ../development/python-modules/sleekxmpp { };
 
   sleekxmppfs = callPackage ../development/python-modules/sleekxmppfs { };
