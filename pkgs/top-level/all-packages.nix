eac298f680b16bde9b283ef2af0b9679f2bae961
Simon Hauser <simon.hauser@helsinki-systems.de>
2024-08-15 16:44:40 +0200

    mariadb: 10.5.26, 10.6.19, 10.11.9, 11.4.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25536,7 +25536,7 @@ with pkgs;
     mariadb_105
     mariadb_106
     mariadb_1011
-    mariadb_110
+    mariadb_114
   ;
   mariadb = mariadb_1011;
   mariadb-embedded = mariadb.override { withEmbedded = true; };
