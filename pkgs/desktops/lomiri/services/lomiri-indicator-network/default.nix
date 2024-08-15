363e6d2d986cbb27b91a6855d3bfccc8c21c68c1
OPNA2608 <opna2608@protonmail.com>
2024-08-15 21:23:17 +0200

    lomiri.lomiri-indicator-network: nixfmt, modernise

--- pkgs/desktops/lomiri/services/lomiri-indicator-network/default.nix
+++ pkgs/desktops/lomiri/services/lomiri-indicator-network/default.nix
@@ -1,32 +1,33 @@
-{ stdenv
-, lib
-, fetchFromGitLab
-, gitUpdater
-, nixosTests
-, testers
-, cmake
-, cmake-extras
-, coreutils
-, dbus
-, doxygen
-, gettext
-, glib
-, gmenuharness
-, gtest
-, intltool
-, libsecret
-, libqofono
-, libqtdbusmock
-, libqtdbustest
-, lomiri-api
-, lomiri-url-dispatcher
-, networkmanager
-, ofono
-, pkg-config
-, python3
-, qtdeclarative
-, qtbase
-, validatePkgConfig
+{
+  stdenv,
+  lib,
+  fetchFromGitLab,
+  gitUpdater,
+  nixosTests,
+  testers,
+  cmake,
+  cmake-extras,
+  coreutils,
+  dbus,
+  doxygen,
+  gettext,
+  glib,
+  gmenuharness,
+  gtest,
+  intltool,
+  libsecret,
+  libqofono,
+  libqtdbusmock,
+  libqtdbustest,
+  lomiri-api,
+  lomiri-url-dispatcher,
+  networkmanager,
+  ofono,
+  pkg-config,
+  python3,
+  qtdeclarative,
+  qtbase,
+  validatePkgConfig,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -78,11 +79,7 @@ stdenv.mkDerivation (finalAttrs: {
     qtbase
   ];
 
-  nativeCheckInputs = [
-    (python3.withPackages (ps: with ps; [
-      python-dbusmock
-    ]))
-  ];
+  nativeCheckInputs = [ (python3.withPackages (ps: with ps; [ python-dbusmock ])) ];
 
   checkInputs = [
     gmenuharness
@@ -109,9 +106,7 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   passthru = {
-    ayatana-indicators = [
-      "lomiri-indicator-network"
-    ];
+    ayatana-indicators = [ "lomiri-indicator-network" ];
     tests = {
       pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
       vm = nixosTests.ayatana-indicators;
@@ -119,15 +114,13 @@ stdenv.mkDerivation (finalAttrs: {
     updateScript = gitUpdater { };
   };
 
-  meta = with lib; {
+  meta = {
     description = "Ayatana indiator exporting the network settings menu through D-Bus";
     homepage = "https://gitlab.com/ubports/development/core/lomiri-indicator-network";
     changelog = "https://gitlab.com/ubports/development/core/lomiri-indicator-network/-/blob/${finalAttrs.version}/ChangeLog";
-    license = licenses.gpl3Only;
-    maintainers = teams.lomiri.members;
-    platforms = platforms.linux;
-    pkgConfigModules = [
-      "lomiri-connectivity-qt1"
-    ];
+    license = lib.licenses.gpl3Only;
+    maintainers = lib.teams.lomiri.members;
+    platforms = lib.platforms.linux;
+    pkgConfigModules = [ "lomiri-connectivity-qt1" ];
   };
 })
