cc01673c2306b109f775b455d04dd1c91217748c
OPNA2608 <opna2608@protonmail.com>
2024-08-15 22:06:15 +0200

    tests/ayatana-indicators: nixfmt, modernise

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -141,7 +141,7 @@ in {
   authelia = handleTest ./authelia.nix {};
   avahi = handleTest ./avahi.nix {};
   avahi-with-resolved = handleTest ./avahi.nix { networkd = true; };
-  ayatana-indicators = handleTest ./ayatana-indicators.nix {};
+  ayatana-indicators = runTest ./ayatana-indicators.nix;
   babeld = handleTest ./babeld.nix {};
   bazarr = handleTest ./bazarr.nix {};
   bcachefs = handleTestOn ["x86_64-linux" "aarch64-linux"] ./bcachefs.nix {};
