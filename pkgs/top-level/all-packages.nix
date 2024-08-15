bb926b3af94737085b54bec8f2610d46c832b5ab
Zitrone <general@dev.quantenzitrone.eu>
2024-08-15 06:07:11 +0200

    steam-run-free: add alias from steamPackages.steam-fhsenv-without-steam.run

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36542,6 +36542,8 @@ with pkgs;
 
   steam-run = steam.run;
 
+  steam-run-free = steamPackages.steam-fhsenv-without-steam.run;
+
   steam-tui = callPackage ../games/steam-tui { };
 
   steamcmd = steamPackages.steamcmd;
