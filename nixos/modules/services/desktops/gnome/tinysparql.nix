e3b25a17e4e1d63e4e6ab2762bc8253e5fdfe310
Bobby Rong <rjl931189261@126.com>
2024-08-16 00:27:56 +0800

    nixos/tinysparql: Format with nixfmt
    As requested by CI.

--- nixos/modules/services/desktops/gnome/tinysparql.nix
+++ nixos/modules/services/desktops/gnome/tinysparql.nix
@@ -1,6 +1,11 @@
 # TinySPARQL daemon.
 
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   cfg = config.services.gnome.tinysparql;
@@ -12,12 +17,33 @@ in
   };
 
   imports = [
-    (lib.mkRemovedOptionModule [ "services" "gnome" "tracker" "subcommandPackages" ] ''
-      This option is broken since 3.7 and since 3.8 tracker (tinysparql) no longer expect
-      CLI to be extended by external projects, note that tracker-miners (localsearch) now
-      provides its own CLI tool.
-    '')
-    (lib.mkRenamedOptionModule [ "services" "gnome" "tracker" "enable" ] [ "services" "gnome" "tinysparql" "enable" ])
+    (lib.mkRemovedOptionModule
+      [
+        "services"
+        "gnome"
+        "tracker"
+        "subcommandPackages"
+      ]
+      ''
+        This option is broken since 3.7 and since 3.8 tracker (tinysparql) no longer expect
+        CLI to be extended by external projects, note that tracker-miners (localsearch) now
+        provides its own CLI tool.
+      ''
+    )
+    (lib.mkRenamedOptionModule
+      [
+        "services"
+        "gnome"
+        "tracker"
+        "enable"
+      ]
+      [
+        "services"
+        "gnome"
+        "tinysparql"
+        "enable"
+      ]
+    )
   ];
 
   ###### interface
@@ -38,7 +64,6 @@ in
     };
 
   };
-
 
   ###### implementation
 
