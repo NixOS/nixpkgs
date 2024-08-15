c78d3b84652b24b0094becef1b0f3aae4edd2553
damhiya <damhiya@gmail.com>
2024-08-16 01:34:59 +0900

    coqPackages.Ordinal: init at 0.5.3

--- pkgs/top-level/coq-packages.nix
+++ pkgs/top-level/coq-packages.nix
@@ -132,6 +132,7 @@ let
       mtac2 = callPackage ../development/coq-modules/mtac2 {};
       multinomials = callPackage ../development/coq-modules/multinomials {};
       odd-order = callPackage ../development/coq-modules/odd-order { };
+      Ordinal = callPackage ../development/coq-modules/Ordinal {};
       paco = callPackage ../development/coq-modules/paco {};
       paramcoq = callPackage ../development/coq-modules/paramcoq {};
       parsec = callPackage ../development/coq-modules/parsec {};
