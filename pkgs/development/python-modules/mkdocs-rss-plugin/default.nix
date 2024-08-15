5feace32774a000032167eafb3371cb6314d66c5
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-08-15 09:58:29 -0300

    Apply suggestions from code review
    Co-authored-by: Fabian Affolter <mail@fabian-affolter.ch>

--- pkgs/development/python-modules/mkdocs-rss-plugin/default.nix
+++ pkgs/development/python-modules/mkdocs-rss-plugin/default.nix
@@ -6,20 +6,34 @@
 buildPythonPackage {
   pname = "mkdocs-rss-plugin";
   version = "1.8.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "Guts";
     repo = "mkdocs-rss-plugin";
-    rev = "1.8.0";
-    sha256 = "sha256-rCz1Uk5uqIsnIWw0b1oBsjAO6aK/tpVgqAX/8dVnAGw=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-rCz1Uk5uqIsnIWw0b1oBsjAO6aK/tpVgqAX/8dVnAGw=";
   };
+  nativeBuildInputs = [
+    setuptools
+  ];
 
+  propagatedBuildInputs = [
+    ...
+  ];
 
   doCheck = false;
 
   meta = with lib; {
-    description = "MkDocs plugin to generate a RSS feeds for created and updated pages, using git log and YAML frontmatter (page.meta).";
+    description = "Plugin to generate a RSS feeds for created and updated pages";
+    longDescription = ''
+      MkDocs plugin to generate a RSS feeds for created and updated pages, using
+      git log and YAML frontmatter (page.meta).
+    '';
     homepage = "https://github.com/Guts/mkdocs-rss-plugin";
+    changelog = "https://github.com/Guts/mkdocs-rss-plugin/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ caarlos0 ];
   };
