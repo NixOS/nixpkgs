670003530c1b6b990ca06d373396a5a40f6a0901
Kerstin Humm <kerstin@erictapen.name>
2024-08-15 16:29:55 +0200

    python3Packages.pytest-smtpd: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12794,6 +12794,8 @@ self: super: with self; {
 
   pytest-shutil = callPackage ../development/python-modules/pytest-shutil { };
 
+  pytest-smtpd = callPackage ../development/python-modules/pytest-smtpd { };
+
   pytest-spec = callPackage ../development/python-modules/pytest-spec { };
 
   python-status = callPackage ../development/python-modules/python-status { };
