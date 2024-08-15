162176068159676918e82d05e2b7ae4a795c8ebf
OPNA2608 <opna2608@protonmail.com>
2024-08-15 21:12:16 +0200

    ayatana-indicator-sound: Update to new passthru.ayatana-indicators format

--- pkgs/by-name/ay/ayatana-indicator-sound/package.nix
+++ pkgs/by-name/ay/ayatana-indicator-sound/package.nix
@@ -106,7 +106,12 @@ stdenv.mkDerivation (finalAttrs: {
   doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
 
   passthru = {
-    ayatana-indicators = [ "ayatana-indicator-sound" ];
+    ayatana-indicators = {
+      ayatana-indicator-sound = [
+        "ayatana"
+        "lomiri"
+      ];
+    };
     tests.vm = nixosTests.ayatana-indicators;
     updateScript = gitUpdater { };
   };
