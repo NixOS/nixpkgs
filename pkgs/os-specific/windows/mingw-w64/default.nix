99d344b4c3f3892c475c5a9e30442367b07b7598
seth <getchoo@tuta.io>
2024-08-16 04:35:01 -0400

    windows.*: format with nixfmt

--- pkgs/os-specific/windows/mingw-w64/default.nix
+++ pkgs/os-specific/windows/mingw-w64/default.nix
@@ -1,26 +1,31 @@
-{ lib
-, stdenv
-, windows
-, autoreconfHook
-, mingw_w64_headers
+{
+  lib,
+  stdenv,
+  windows,
+  autoreconfHook,
+  mingw_w64_headers,
 }:
 
 stdenv.mkDerivation {
   pname = "mingw-w64";
   inherit (mingw_w64_headers) version src meta;
 
-  outputs = [ "out" "dev" ];
+  outputs = [
+    "out"
+    "dev"
+  ];
 
   configureFlags = [
     "--enable-idl"
     "--enable-secure-api"
-  ] ++ lib.optionals (stdenv.targetPlatform.libc == "ucrt") [
-    "--with-default-msvcrt=ucrt"
-  ];
+  ] ++ lib.optionals (stdenv.targetPlatform.libc == "ucrt") [ "--with-default-msvcrt=ucrt" ];
 
   enableParallelBuilding = true;
 
   nativeBuildInputs = [ autoreconfHook ];
   buildInputs = [ windows.mingw_w64_headers ];
-  hardeningDisable = [ "stackprotector" "fortify" ];
+  hardeningDisable = [
+    "stackprotector"
+    "fortify"
+  ];
 }
