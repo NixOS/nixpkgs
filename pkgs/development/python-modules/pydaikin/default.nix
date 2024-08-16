ca25d96201101b49af46667997be44d1bbd28b02
Otto Sabart <seberm@seberm.com>
2024-08-16 21:00:00 +0200

    python312Packages.pydaikin: 2.13.2 -> 2.13.4

--- pkgs/development/python-modules/pydaikin/default.nix
+++ pkgs/development/python-modules/pydaikin/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "pydaikin";
-  version = "2.13.2";
+  version = "2.13.4";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "fredrike";
     repo = "pydaikin";
     rev = "refs/tags/v${version}";
-    hash = "sha256-eq6QE5yYPb9y7bNJwk3sLQp7Pw0PtWn2T/6J+ICfPsY=";
+    hash = "sha256-zttn1AlKeLtDBxoJd15uYfi1V8FYIwbADm864jnpSQs=";
   };
 
   build-system = [ setuptools ];
