34d9244a4a1bb3c4cd599a178ab7c6794176da4e
Robert Schütz <mail@dotlambda.de>
2024-08-16 05:32:05 -0700

    python312Packages.pycoolmasternet-async: 0.2.1 -> 0.2.2
    Diff: https://github.com/OnFreund/pycoolmasternet-async/compare/v0.2.1...0.2.2

--- pkgs/development/python-modules/pycoolmasternet-async/default.nix
+++ pkgs/development/python-modules/pycoolmasternet-async/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "pycoolmasternet-async";
-  version = "0.2.1";
+  version = "0.2.2";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -16,8 +16,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "OnFreund";
     repo = "pycoolmasternet-async";
-    rev = "v${version}";
-    hash = "sha256-q8hOT9Dx7JcEDCpPL6AFgF4cqHBrZGq9kjEaq5wuwJY=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-MfWWy4C/G2w0Zb4C6iYbcfKciFtWctZ63K8lWaHuSnQ=";
   };
 
   build-system = [ setuptools ];
