d2938c024c66730c5ef6b80bf15a690313b27afe
Colin <colin@uninsane.org>
2024-08-16 08:36:05 +0000

    baresip: packaging cleanups
    the `LIBRE*` and `USE_*` make flags were removed during the cmake
    transition in v2.11.0: <https://github.com/baresip/baresip/pull/2354/files>
    
    upstream now detects features on a best-effort basis, with pkg-config.

--- pkgs/by-name/ba/baresip/package.nix
+++ pkgs/by-name/ba/baresip/package.nix
@@ -1,30 +1,31 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, zlib
-, openssl
-, libre
-, librem
-, pkg-config
-, gst_all_1
-, cairo
-, gtk3
-, mpg123
-, alsa-lib
-, SDL2
-, libv4l
-, celt
-, libsndfile
-, srtp
-, ffmpeg
-, gsm
-, speex
-, portaudio
-, spandsp3
-, libuuid
-, libvpx
-, cmake
-, dbusSupport ? true
+{
+  SDL2,
+  alsa-lib,
+  cairo,
+  celt,
+  cmake,
+  fetchFromGitHub,
+  ffmpeg,
+  gsm,
+  gst_all_1,
+  gtk3,
+  lib,
+  libre,
+  librem,
+  libsndfile,
+  libuuid,
+  libv4l,
+  libvpx,
+  mpg123,
+  openssl,
+  pkg-config,
+  portaudio,
+  spandsp3,
+  speex,
+  srtp,
+  stdenv,
+  zlib,
+  dbusSupport ? true,
 }:
 stdenv.mkDerivation rec {
   version = "3.10.1";
@@ -38,28 +39,28 @@ stdenv.mkDerivation rec {
   prePatch = lib.optionalString (!dbusSupport) ''
     substituteInPlace cmake/modules.cmake --replace 'list(APPEND MODULES ctrl_dbus)' ""
   '';
-  nativeBuildInputs = [ pkg-config cmake ];
+  nativeBuildInputs = [ cmake pkg-config ];
   buildInputs = [
-    zlib
-    openssl
-    libre
-    librem
-    cairo
-    gtk3
-    mpg123
-    alsa-lib
     SDL2
-    libv4l
+    alsa-lib
+    cairo
     celt
-    libsndfile
-    srtp
     ffmpeg
     gsm
-    speex
+    gtk3
+    libre
+    librem
+    libsndfile
+    libuuid
+    libv4l
+    libvpx
+    mpg123
+    openssl
     portaudio
     spandsp3
-    libuuid
-    libvpx
+    speex
+    srtp
+    zlib
   ] ++ (with gst_all_1; [ gstreamer gst-libav gst-plugins-base gst-plugins-bad gst-plugins-good ]);
 
   cmakeFlags = [
@@ -68,50 +69,8 @@ stdenv.mkDerivation rec {
   ];
 
   makeFlags = [
-    "LIBRE_MK=${libre}/share/re/re.mk"
-    "LIBRE_SO=${libre}/lib"
-    "LIBREM_PATH=${librem}"
     "PREFIX=$(out)"
-    "USE_VIDEO=1"
     "CCACHE_DISABLE=1"
-
-    "USE_ALSA=1"
-    "USE_AMR=1"
-    "USE_CAIRO=1"
-    "USE_CELT=1"
-    "USE_CONS=1"
-    "USE_EVDEV=1"
-    "USE_FFMPEG=1"
-    "USE_GSM=1"
-    "USE_GST1=1"
-    "USE_GTK=1"
-    "USE_L16=1"
-    "USE_MPG123=1"
-    "USE_OSS=1"
-    "USE_PLC=1"
-    "USE_VPX=1"
-    "USE_PORTAUDIO=1"
-    "USE_SDL=1"
-    "USE_SNDFILE=1"
-    "USE_SPEEX=1"
-    "USE_SPEEX_AEC=1"
-    "USE_SPEEX_PP=1"
-    "USE_SPEEX_RESAMP=1"
-    "USE_SRTP=1"
-    "USE_STDIO=1"
-    "USE_SYSLOG=1"
-    "USE_UUID=1"
-    "USE_V4L2=1"
-    "USE_X11=1"
-
-    "USE_BV32="
-    "USE_COREAUDIO="
-    "USE_G711=1"
-    "USE_G722=1"
-    "USE_G722_1="
-    "USE_ILBC="
-    "USE_OPUS="
-    "USE_SILK="
   ]
   ++ lib.optional (stdenv.cc.cc != null) "SYSROOT_ALT=${stdenv.cc.cc}"
   ++ lib.optional (stdenv.cc.libc != null) "SYSROOT=${stdenv.cc.libc}"
