b77e53f5523018d46a7ba80471c6852781967315
Philip Taron <philip.taron@gmail.com>
2024-08-16 13:58:09 -0700

    getdns: remove with statements

--- pkgs/development/libraries/getdns/default.nix
+++ pkgs/development/libraries/getdns/default.nix
@@ -3,10 +3,10 @@
 { lib, stdenv, fetchurl, cmake, darwin, doxygen, libidn2, libyaml, openssl
 , systemd, unbound, yq }:
 let
-  metaCommon = with lib; {
-    maintainers = with maintainers; [ leenaars ehmry ];
-    license = licenses.bsd3;
-    platforms = platforms.all;
+  metaCommon = {
+    maintainers = with lib.maintainers; [ leenaars ehmry ];
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.all;
   };
 in rec {
 
@@ -16,7 +16,7 @@ in rec {
     outputs = [ "out" "dev" "lib" "man" ];
 
     src = fetchurl {
-      url = with lib; "https://getdnsapi.net/releases/${pname}-${concatStringsSep "-" (splitVersion version)}/${pname}-${version}.tar.gz";
+      url = "https://getdnsapi.net/releases/${pname}-${lib.concatStringsSep "-" (lib.splitVersion version)}/${pname}-${version}.tar.gz";
       # upstream publishes hashes in hex format
       sha256 = "f1404ca250f02e37a118aa00cf0ec2cbe11896e060c6d369c6761baea7d55a2c";
     };
@@ -34,22 +34,21 @@ in rec {
 
     postInstall = "rm -r $out/share/doc";
 
-    meta = with lib;
-      metaCommon // {
-        description = "Modern asynchronous DNS API";
-        longDescription = ''
-          getdns is an implementation of a modern asynchronous DNS API; the
-          specification was originally edited by Paul Hoffman. It is intended to make all
-          types of DNS information easily available to application developers and non-DNS
-          experts. DNSSEC offers a unique global infrastructure for establishing and
-          enhancing cryptographic trust relations. With the development of this API the
-          developers intend to offer application developers a modern and flexible
-          interface that enables end-to-end trust in the DNS architecture, and which will
-          inspire application developers to implement innovative security solutions in
-          their applications.
-        '';
-        homepage = "https://getdnsapi.net";
-      };
+    meta = metaCommon // {
+      description = "Modern asynchronous DNS API";
+      longDescription = ''
+        getdns is an implementation of a modern asynchronous DNS API; the
+        specification was originally edited by Paul Hoffman. It is intended to make all
+        types of DNS information easily available to application developers and non-DNS
+        experts. DNSSEC offers a unique global infrastructure for establishing and
+        enhancing cryptographic trust relations. With the development of this API the
+        developers intend to offer application developers a modern and flexible
+        interface that enables end-to-end trust in the DNS architecture, and which will
+        inspire application developers to implement innovative security solutions in
+        their applications.
+      '';
+      homepage = "https://getdnsapi.net";
+    };
   };
 
   stubby = stdenv.mkDerivation rec {
@@ -72,22 +71,20 @@ in rec {
         > $stubbyExampleJson
     '';
 
-    passthru.settingsExample = with builtins;
-      fromJSON (readFile stubby.stubbyExampleJson);
+    passthru.settingsExample = builtins.fromJSON (builtins.readFile stubby.stubbyExampleJson);
 
-    meta = with lib;
-      metaCommon // {
-        description = "Local DNS Privacy stub resolver (using DNS-over-TLS)";
-        mainProgram = "stubby";
-        longDescription = ''
-          Stubby is an application that acts as a local DNS Privacy stub
-          resolver (using RFC 7858, aka DNS-over-TLS). Stubby encrypts DNS
-          queries sent from a client machine (desktop or laptop) to a DNS
-          Privacy resolver increasing end user privacy. Stubby is developed by
-          the getdns team.
-        '';
-        homepage = "https://dnsprivacy.org/dns_privacy_daemon_-_stubby/";
-      };
+    meta = metaCommon // {
+      description = "Local DNS Privacy stub resolver (using DNS-over-TLS)";
+      mainProgram = "stubby";
+      longDescription = ''
+        Stubby is an application that acts as a local DNS Privacy stub
+        resolver (using RFC 7858, aka DNS-over-TLS). Stubby encrypts DNS
+        queries sent from a client machine (desktop or laptop) to a DNS
+        Privacy resolver increasing end user privacy. Stubby is developed by
+        the getdns team.
+      '';
+      homepage = "https://dnsprivacy.org/dns_privacy_daemon_-_stubby/";
+    };
   };
 
 }
