555abab375aa1b6ff762150b9e778b76881c8109
seth <getchoo@tuta.io>
2024-08-16 04:36:42 -0400

    windows.mingw_w64*: 11.0.1 -> 12.0.0
    Changelog: https://www.mingw-w64.org/changelog/#v1200-2024-05-29
    
    Only notable change for our purposes is UCRT now being the default, so
    either it or MSVCRT must be explicitly passed

--- pkgs/os-specific/windows/mingw-w64/default.nix
+++ pkgs/os-specific/windows/mingw-w64/default.nix
@@ -4,7 +4,13 @@
   windows,
   autoreconfHook,
   mingw_w64_headers,
+  crt ? stdenv.hostPlatform.libc,
 }:
+
+assert lib.assertOneOf "crt" crt [
+  "msvcrt"
+  "ucrt"
+];
 
 stdenv.mkDerivation {
   pname = "mingw-w64";
@@ -18,7 +24,8 @@ stdenv.mkDerivation {
   configureFlags = [
     "--enable-idl"
     "--enable-secure-api"
-  ] ++ lib.optionals (stdenv.targetPlatform.libc == "ucrt") [ "--with-default-msvcrt=ucrt" ];
+    "--with-default-msvcrt=${crt}"
+  ];
 
   enableParallelBuilding = true;
 
