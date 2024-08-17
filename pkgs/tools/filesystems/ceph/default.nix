2f67c13015d03382927e69a0b0b293d1d1b3e6f6
Niklas Hambüchen <mail@nh2.me>
2024-08-17 16:00:06 +0000

    ceph: Add nh2 to maintainers

--- pkgs/tools/filesystems/ceph/default.nix
+++ pkgs/tools/filesystems/ceph/default.nix
@@ -142,7 +142,7 @@ let
      homepage = "https://ceph.io/en/";
      inherit description;
      license = with lib.licenses; [ lgpl21 gpl2Only bsd3 mit publicDomain ];
-     maintainers = with lib.maintainers; [ adev ak johanot krav ];
+     maintainers = with lib.maintainers; [ adev ak johanot krav nh2 ];
      platforms = [ "x86_64-linux" "aarch64-linux" ];
    };
 
