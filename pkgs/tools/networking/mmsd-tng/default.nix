1c8a87ee18b458f11cee246933c8d0a791cc7d97
Donovan Glover <donovan@dglover.co>
2024-08-16 23:51:40 -0400

    mmsd-tng: cleanup

--- pkgs/tools/networking/mmsd-tng/default.nix
+++ pkgs/tools/networking/mmsd-tng/default.nix
@@ -1,26 +1,28 @@
-{ lib, stdenv
-, fetchFromGitLab
-, c-ares
-, dbus
-, glib
-, libphonenumber
-, libsoup
-, meson
-, mobile-broadband-provider-info
-, modemmanager
-, ninja
-, pkg-config
-, protobuf
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  c-ares,
+  dbus,
+  glib,
+  libphonenumber,
+  libsoup,
+  meson,
+  mobile-broadband-provider-info,
+  modemmanager,
+  ninja,
+  pkg-config,
+  protobuf,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mmsd-tng";
   version = "1.12.1";
 
   src = fetchFromGitLab {
     owner = "kop316";
     repo = "mmsd";
-    rev = version;
+    rev = finalAttrs.version;
     hash = "sha256-fhbiTJWmQwJpuMaVX2qWyWwJ/2Y/Vczo//+0T0b6jhA=";
   };
 
@@ -43,12 +45,12 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
-  meta = with lib; {
+  meta = {
     description = "Multimedia Messaging Service Daemon - The Next Generation";
     homepage = "https://gitlab.com/kop316/mmsd";
-    license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ julm ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ julm ];
+    platforms = lib.platforms.linux;
     mainProgram = "mmsdtng";
   };
-}
+})
