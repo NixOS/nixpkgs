3f2b634b998a3eb2e27aaf9d0f2d60215bd319b0
Daniël de Kok <me@danieldk.eu>
2024-08-17 16:42:23 +0000

    cudaPackages_12_4: init at 12.4.0

--- pkgs/development/cuda-modules/cudatoolkit/releases.nix
+++ pkgs/development/cuda-modules/cudatoolkit/releases.nix
@@ -105,4 +105,10 @@
     url = "https://developer.download.nvidia.com/compute/cuda/12.3.0/local_installers/cuda_12.3.0_545.23.06_linux.run";
     sha256 = "sha256-fBP6zjr2TW4WSNbjEB0xyBEedHFDrLAHfZc8FpCCBCI=";
   };
+
+  "12.4" = {
+    version = "12.4.0";
+    url = "https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda_12.4.0_550.54.14_linux.run";
+    sha256 = "sha256-5qhC9OypSQV1zbaLaxu3jUe5Wol95I3uKSxDGJLlfRc=";
+  };
 }
