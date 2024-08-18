ab83a4ede140a03a363de3bc2840395da12faf14
Bobby Rong <rjl931189261@126.com>
2024-08-18 08:29:37 +0000

    libpeas2: 2.0.3 → 2.0.4
    https://gitlab.gnome.org/GNOME/libpeas/-/compare/2.0.3...2.0.4
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/libpeas/2.x.nix
+++ pkgs/development/libraries/libpeas/2.x.nix
@@ -11,19 +11,19 @@
 , glib
 , lua5_1
 , python3
-, spidermonkey_115
+, spidermonkey_128
 , gnome
 }:
 
 stdenv.mkDerivation rec {
   pname = "libpeas";
-  version = "2.0.3";
+  version = "2.0.4";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    hash = "sha256-OeO1B8KdLQHfE0XpszgP16nQrrWy5lfTjmwr6lAj5fA=";
+    hash = "sha256-16Wd5ejQSAcxBNAqhtGzw7VYuF8/KWxz0Qx99LmnYk4=";
   };
 
   patches = [
@@ -55,7 +55,7 @@ stdenv.mkDerivation rec {
     lua5_1.pkgs.lgi
     python3
     python3.pkgs.pygobject3
-    spidermonkey_115
+    spidermonkey_128
   ];
 
   propagatedBuildInputs = [
