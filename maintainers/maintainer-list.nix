3680e49de49c83a610ca9e33241fb343271ae70d
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2024-08-18 15:36:02 +0200

    maintainers: add jovandeginste
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10097,6 +10097,12 @@
     githubId = 15893072;
     name = "Josh van Leeuwen";
   };
+  jovandeginste = {
+    email = "jo.vandeginste@gmail.com";
+    github = "jovandeginste";
+    githubId = 3170771;
+    name = "Jo Vandeginste";
+  };
   jpagex = {
     name = "Jérémy Pagé";
     email = "contact@jeremypage.me";
