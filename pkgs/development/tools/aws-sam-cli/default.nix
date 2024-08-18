80952447612760cd203eaaf656839bdd3dc22535
Fabian Affolter <mail@fabian-affolter.ch>
2024-08-18 09:42:03 +0200

    aws-sam-cli: 1.120.0 -> 1.122.0
    Diff: https://github.com/aws/aws-sam-cli/compare/refs/tags/v1.120.0...v1.122.0
    
    Changelog: https://github.com/aws/aws-sam-cli/releases/tag/v1.122.0

--- pkgs/development/tools/aws-sam-cli/default.nix
+++ pkgs/development/tools/aws-sam-cli/default.nix
@@ -10,7 +10,7 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "aws-sam-cli";
-  version = "1.120.0";
+  version = "1.122.0";
   pyproject = true;
 
   disabled = python3.pythonOlder "3.8";
@@ -19,7 +19,7 @@ python3.pkgs.buildPythonApplication rec {
     owner = "aws";
     repo = "aws-sam-cli";
     rev = "refs/tags/v${version}";
-    hash = "sha256-XFVh7e9ooIBhWRkVovPdrSuyosBMQ84PDRjV2o0x9ns=";
+    hash = "sha256-kGU0f8rW6ZS4/Cszb/DshfJKeRaqeFwNHQz4WqDg5JI=";
   };
 
   build-system = with python3.pkgs; [
