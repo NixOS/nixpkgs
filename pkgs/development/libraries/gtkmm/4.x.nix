76e6919d500dc26dc29da5592fb95447724da63f
Bobby Rong <rjl931189261@126.com>
2024-08-18 08:29:10 +0000

    gtkmm4: 4.14.0 → 4.15.0
    https://gitlab.gnome.org/GNOME/gtkmm/-/compare/4.14.0...4.15.0
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/gtkmm/4.x.nix
+++ pkgs/development/libraries/gtkmm/4.x.nix
@@ -18,13 +18,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gtkmm";
-  version = "4.14.0";
+  version = "4.15.0";
 
   outputs = [ "out" "dev" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    hash = "sha256-k1CgREt0TKPcaVhuvRtnB1IJIrbZ9PIyEDzmA6Jx7No=";
+    hash = "sha256-IVuCDr1ZvzfJ1UkRV6kk2XuPDGNQj3MfL4g20rv/4xU=";
   };
 
   nativeBuildInputs = [
