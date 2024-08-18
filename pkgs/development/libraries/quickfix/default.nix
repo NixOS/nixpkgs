8152fcd22a31949429e4f3d91b8c57687bbdf2db
Sebastián Mancilla <smancill@smancill.dev>
2024-08-18 18:15:23 -0400

    quickfix: format

--- pkgs/development/libraries/quickfix/default.nix
+++ pkgs/development/libraries/quickfix/default.nix
@@ -1,13 +1,21 @@
-{ lib, stdenv, fetchFromGitHub, fetchpatch, autoconf, automake, libtool }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  fetchpatch,
+  autoconf,
+  automake,
+  libtool,
+}:
 
 stdenv.mkDerivation rec {
   pname = "quickfix";
   version = "1.15.1";
 
   src = fetchFromGitHub {
-    owner = pname;
-    repo = pname;
-    rev =  "v${version}";
+    owner = "quickfix";
+    repo = "quickfix";
+    rev = "v${version}";
     sha256 = "1fgpwgvyw992mbiawgza34427aakn5zrik3sjld0i924a9d17qwg";
   };
 
@@ -21,7 +29,11 @@ stdenv.mkDerivation rec {
   ];
 
   # autoreconfHook does not work
-  nativeBuildInputs = [ autoconf automake libtool ];
+  nativeBuildInputs = [
+    autoconf
+    automake
+    libtool
+  ];
 
   enableParallelBuilding = true;
 
