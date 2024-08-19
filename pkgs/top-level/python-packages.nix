aa83e8ec4a5f8a56d746e3b2df4ce31ee68c6012
tahanonu <tochipreciousahan@gmail.com>
2024-08-19 14:17:11 +0100

    python312Packages.autojinja: init at 1.14.1
    Release: https://github.com/ldflo/autojinja/releases/tag/v1.14.1
    
    Co-authored-by: OTABI Tomoya <tomoya.otabi@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1019,6 +1019,8 @@ self: super: with self; {
 
   autoit-ripper = callPackage ../development/python-modules/autoit-ripper { };
 
+  autojinja = callPackage ../development/python-modules/autojinja { };
+
   autologging = callPackage ../development/python-modules/autologging { };
 
   automat = callPackage ../development/python-modules/automat { };
