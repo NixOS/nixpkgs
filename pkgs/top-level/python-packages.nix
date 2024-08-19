d45e32544ae74efc92f9592feb08118ee865f942
Sigmanificient <edhyjox@gmail.com>
2024-08-19 19:22:01 +0200

    python312Packages.winbox: init at 0.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13854,6 +13854,8 @@ self: super: with self; {
 
   pywilight = callPackage ../development/python-modules/pywilight { };
 
+  pywinbox = callPackage ../development/python-modules/pywinbox { };
+
   pywinrm = callPackage ../development/python-modules/pywinrm { };
 
   pywizlight = callPackage ../development/python-modules/pywizlight { };
