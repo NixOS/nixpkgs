2b139d3b1e99c2c5640f289345728acada409a35
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2024-08-19 02:57:48 +0200

    nixos/frr_exporter: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1030,6 +1030,7 @@
   ./services/networking/freeradius.nix
   ./services/networking/frp.nix
   ./services/networking/frr.nix
+  ./services/networking/frr_exporter.nix
   ./services/networking/gateone.nix
   ./services/networking/gdomap.nix
   ./services/networking/ghostunnel.nix
