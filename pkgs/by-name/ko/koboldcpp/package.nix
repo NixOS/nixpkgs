15a2bb6d13896304598c87f99a392f3cee093807
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-08-19 13:41:20 +0300

    koboldcpp: 1.72 -> 1.73
    Changelog: https://github.com/LostRuins/koboldcpp/releases/tag/v1.73
    Diff: https://github.com/LostRuins/koboldcpp/compare/v1.72...v1.73
    (cherry picked from commit 4e8b1b96ef6f8207517316b80e75dccc17c65594)

--- pkgs/by-name/ko/koboldcpp/package.nix
+++ pkgs/by-name/ko/koboldcpp/package.nix
@@ -53,13 +53,13 @@ let
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "koboldcpp";
-  version = "1.72";
+  version = "1.73";
 
   src = fetchFromGitHub {
     owner = "LostRuins";
     repo = "koboldcpp";
     rev = "refs/tags/v${finalAttrs.version}";
-    hash = "sha256-DePcBVNXIWIlOXyNeNMUFFIQxQuwqG8KBUh2/X7R6X8=";
+    hash = "sha256-S0MonY2rIAkbJnTaDVc2YiGPjOaevgp82mt6JwWdN1U=";
   };
 
   enableParallelBuilding = true;
