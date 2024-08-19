3c294406012ad507199bcfce0770098d82d4e2bf
nicoo <nicoo@mur.at>
2024-08-19 20:52:04 +0000

    fixup! nixos/openrct2: init

--- nixos/modules/services/games/openrct2.nix
+++ nixos/modules/services/games/openrct2.nix
@@ -1,4 +1,10 @@
-{ config, options, lib, pkgs, ... }:
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 
@@ -6,138 +12,141 @@ let
   cfg = config.services.openrct2;
 
   parkOptions =
-  { name, ... }:
-  {
-    options = {
-      listenAddress = mkOption {
-        type = types.str;
-        default = "";
-        description = "Address to listen on.";
-        example = "0.0.0.0";
-      };
+    { name, ... }:
+    {
+      options = {
+        listenAddress = mkOption {
+          type = types.str;
+          default = "";
+          description = "Address to listen on.";
+          example = "0.0.0.0";
+        };
 
-      port = mkOption {
-        type = types.port;
-        default = 11753;
-        description = "Port to listen on.";
-      };
+        port = mkOption {
+          type = types.port;
+          default = 11753;
+          description = "Port to listen on.";
+        };
 
-      advertise = mkOption {
-        type = types.bool;
-        default = false;
-        description = mdDoc ''
-          Wether to advertise our server to the master server, making it
-          visible in the public listing.
-        '';
-      };
+        advertise = mkOption {
+          type = types.bool;
+          default = false;
+          description = mdDoc ''
+            Wether to advertise our server to the master server, making it
+            visible in the public listing.
+          '';
+        };
 
-      advertiseAddress = mkOption {
-        type = types.str;
-        default = "";
-        description = "Address to advertise to the master server.";
-        example = "128.66.43.85";
-      };
+        advertiseAddress = mkOption {
+          type = types.str;
+          default = "";
+          description = "Address to advertise to the master server.";
+          example = "128.66.43.85";
+        };
 
-      password = mkOption {
-        type = types.str;
-        default = null;
-        description = "Password needed to join the server.";
-      };
+        password = mkOption {
+          type = types.str;
+          default = null;
+          description = "Password needed to join the server.";
+        };
 
-      autosave = mkOption {
-        type = types.int;
-        default = 0;
-        description = mdDoc ''
-          Autosave frequency.
+        autosave = mkOption {
+          type = types.int;
+          default = 0;
+          description = mdDoc ''
+            Autosave frequency.
 
-          We rely on the autosave mechanism to keep the game state through
-          restarts or other misfortunes, so it's recommended to set this to `0`
-          or `1` to enable autosaves every 1 or 5 minute(s) respectively.
+            We rely on the autosave mechanism to keep the game state through
+            restarts or other misfortunes, so it's recommended to set this to `0`
+            or `1` to enable autosaves every 1 or 5 minute(s) respectively.
 
-          Autosaves are saved to the `${cfg.dataDirRoot}/${name}/save/autosave` directory.
+            Autosaves are saved to the `${cfg.dataDirRoot}/${name}/save/autosave` directory.
 
-          | value | frequency         |
-          | ----- | ----------------- |
-          |   0   | (every) 1 minute  |
-          |   1   | 5 minutes         |
-          |   2   | 10 minutes        |
-          |   3   | 15 minutes        |
-          |   4   | 20 minutes        |
-          |   5   | disable           |
-        '';
-      };
+            | value | frequency         |
+            | ----- | ----------------- |
+            |   0   | (every) 1 minute  |
+            |   1   | 5 minutes         |
+            |   2   | 10 minutes        |
+            |   3   | 15 minutes        |
+            |   4   | 20 minutes        |
+            |   5   | disable           |
+          '';
+        };
 
-      autosaveAmount = mkOption {
-        type = types.int;
-        default = 60;
-        description = "Amount of autosaves to keep.";
-      };
+        autosaveAmount = mkOption {
+          type = types.int;
+          default = 60;
+          description = "Amount of autosaves to keep.";
+        };
 
-      autoRestoreAutosave = mkOption {
-        type = types.bool;
-        default = true;
-        description = mdDoc ''
-          Whether to automatically restore the latest autosave on startup.
+        autoRestoreAutosave = mkOption {
+          type = types.bool;
+          default = true;
+          description = mdDoc ''
+            Whether to automatically restore the latest autosave on startup.
 
-          If set to `true`, the latest autosave will automatically be restored
-          on startup. Use this in combination with a high frequency `autosave`
-          value (like `0` to autosave every 1 minute), to limit lost in-game
-          time on server restarts.
-        '';
-      };
+            If set to `true`, the latest autosave will automatically be restored
+            on startup. Use this in combination with a high frequency `autosave`
+            value (like `0` to autosave every 1 minute), to limit lost in-game
+            time on server restarts.
+          '';
+        };
 
-      playerName = mkOption {
-        type = types.str;
-        default = "${name} Server";
-        description = "Name of the player for the server that hosts the game.";
-      };
+        playerName = mkOption {
+          type = types.str;
+          default = "${name} Server";
+          description = "Name of the player for the server that hosts the game.";
+        };
 
-      maxPlayers = mkOption {
-        type = types.int;
-        default = 16;
-        description = "Maximum amount of online players to allow.";
-      };
+        maxPlayers = mkOption {
+          type = types.int;
+          default = 16;
+          description = "Maximum amount of online players to allow.";
+        };
 
-      dataDir = mkOption {
-        type = types.path;
-        default = "${cfg.dataDirRoot}/${name}";
-        description = ''
-          Path to the park data directory.
+        dataDir = mkOption {
+          type = types.path;
+          default = "${cfg.dataDirRoot}/${name}";
+          description = ''
+            Path to the park data directory.
 
-          Most importantly, this directory contains the savegames under
-          `save/autosave` and the current park file at `${name}.park`.
-        '';
-      };
+            Most importantly, this directory contains the savegames under
+            `save/autosave` and the current park file at `${name}.park`.
+          '';
+        };
 
-      settings = mkOption {
-        type = format.type;
-        default = {};
-        description = "OpenRCT2 config.ini";
-        example = literalExpression ''{
-          general = {
-            currency_format = "EUR";
-            date_format = "DD/MM/YY";
-          };
-          network = {
-            pause_server_if_no_clients = true;
-          };
-          plugin = {
-            enable_hot_reloading = true;
-          };
-        }'';
+        settings = mkOption {
+          type = format.type;
+          default = { };
+          description = "OpenRCT2 config.ini";
+          example = literalExpression ''
+            {
+                      general = {
+                        currency_format = "EUR";
+                        date_format = "DD/MM/YY";
+                      };
+                      network = {
+                        pause_server_if_no_clients = true;
+                      };
+                      plugin = {
+                        enable_hot_reloading = true;
+                      };
+                    }'';
+        };
       };
     };
-  };
 
-
-  mkService = name: park:
-  {
+  mkService = name: park: {
     description = "OpenRCT2 Server - ${name}";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
 
     serviceConfig = {
-      ExecStart = ''${cfg.package}/bin/openrct2-cli host "${park.dataDir}/${name}.park" --headless "--user-data-path=${park.dataDir}" ${if cfg.gamePath == "" then "" else  ''"--rct2-data-path=${cfg.gamePath}"''} ${if cfg.rct1Path == "" then "" else ''"--rct1-data-path=${cfg.rct1Path}"''} --port ${toString park.port}'';
+      ExecStart = ''${cfg.package}/bin/openrct2-cli host "${park.dataDir}/${name}.park" --headless "--user-data-path=${park.dataDir}" ${
+        if cfg.gamePath == "" then "" else ''"--rct2-data-path=${cfg.gamePath}"''
+      } ${
+        if cfg.rct1Path == "" then "" else ''"--rct1-data-path=${cfg.rct1Path}"''
+      } --port ${toString park.port}'';
       Restart = "always";
       User = cfg.user;
       WorkingDirectory = park.dataDir;
@@ -185,12 +194,13 @@ let
     '';
   };
 
-  mkParkConfig = name: park:
+  mkParkConfig =
+    name: park:
     recursiveUpdate {
       general = {
         autosave = park.autosave;
         autosave_amount = park.autosaveAmount;
-        rct1_path = "${ if isNull cfg.rct1Path then "" else cfg.rct1Path }";
+        rct1_path = "${if isNull cfg.rct1Path then "" else cfg.rct1Path}";
         game_path = cfg.gamePath;
       };
 
@@ -204,13 +214,14 @@ let
       };
     } park.settings;
 
-  format = pkgs.formats.ini {};
+  format = pkgs.formats.ini { };
 
   parkConfigs = mapAttrs (name: value: mkParkConfig name value) cfg.parks;
 
   parkConfigFiles = mapAttrs (name: value: format.generate "${name}-config.ini" value) parkConfigs;
 
-in {
+in
+{
   options.services.openrct2 = {
     enable = mkEnableOption (lib.mdDoc ''OpenRCT2 headless game server.'');
 
@@ -266,23 +277,23 @@ in {
   };
 
   config = mkIf config.services.openrct2.enable {
-      users.users.openrct2 = mkIf (cfg.user == "openrct2") {
-        description = "OpenRCT2";
-        home = cfg.dataDirRoot;
-        createHome = true;
-        isSystemUser = true;
-        uid = config.ids.uids.openrct2;
-        group = cfg.group;
-      };
+    users.users.openrct2 = mkIf (cfg.user == "openrct2") {
+      description = "OpenRCT2";
+      home = cfg.dataDirRoot;
+      createHome = true;
+      isSystemUser = true;
+      uid = config.ids.uids.openrct2;
+      group = cfg.group;
+    };
 
-      users.groups.openrct2 = mkIf (cfg.group == "openrct2") {
-        gid = config.ids.gids.openrct2;
-      };
+    users.groups.openrct2 = mkIf (cfg.group == "openrct2") { gid = config.ids.gids.openrct2; };
 
-      systemd.services = mapAttrs' (name: value: nameValuePair "openrct2-${name}" (mkService name value)) cfg.parks;
+    systemd.services = mapAttrs' (
+      name: value: nameValuePair "openrct2-${name}" (mkService name value)
+    ) cfg.parks;
 
-      networking.firewall = mkIf cfg.openFirewall {
-        allowedTCPPorts = mapAttrsToList (name: value: value.port) cfg.parks;
-      };
+    networking.firewall = mkIf cfg.openFirewall {
+      allowedTCPPorts = mapAttrsToList (name: value: value.port) cfg.parks;
+    };
   };
 }
