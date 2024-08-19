1910f6e07242cbdb2d0fe33b40b1faa9cdb79814
Sigmanificient <edhyjox@gmail.com>
2024-08-19 19:14:13 +0200

    python312Packages.pymonctl: init at 0.92

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12383,6 +12383,8 @@ self: super: with self; {
 
   pymodbus = callPackage ../development/python-modules/pymodbus { };
 
+  pymonctl = callPackage ../development/python-modules/pymonctl { };
+
   pymongo = callPackage ../development/python-modules/pymongo { };
 
   pymongo-inmemory = callPackage ../development/python-modules/pymongo-inmemory { };
