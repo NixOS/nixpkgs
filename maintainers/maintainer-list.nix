9ce6da76e70c5a08a1f5ef4ec18de29cda2df2c0
Hadi <hadilq.dev@gmail.com>
2024-08-20 13:39:07 -0400

    androidenv: add maintainers

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7950,6 +7950,13 @@
     github = "hacker1024";
     githubId = 20849728;
   };
+  hadilq = {
+    name = "Hadi Lashkari Ghouchani";
+    email = "hadilq.dev@gmail.com";
+    github = "hadilq";
+    githubId = 5190539;
+    keys = [ { fingerprint = "AD3D 53CB A68A FEC0 8065  BCBB 416A D9E8 E372 C075"; } ];
+  };
   hagl = {
     email = "harald@glie.be";
     github = "hagl";
