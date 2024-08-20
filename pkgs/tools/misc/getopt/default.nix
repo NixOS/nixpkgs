79ecc945cd3b381293db9c006f64502fc65c6b20
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-08-21 01:22:36 +0200

    getopt: change `${pname}` to string literal

--- pkgs/tools/misc/getopt/default.nix
+++ pkgs/tools/misc/getopt/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   pname = "getopt";
   version = "1.1.6";
   src = fetchurl {
-    url = "http://frodo.looijaard.name/system/files/software/${pname}/${pname}-${version}.tar.gz";
+    url = "http://frodo.looijaard.name/system/files/software/getopt/getopt-${version}.tar.gz";
     sha256 = "1zn5kp8ar853rin0ay2j3p17blxy16agpp8wi8wfg4x98b31vgyh";
   };
 
