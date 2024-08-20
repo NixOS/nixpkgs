e64ca20dd9a9a369dfa86e4ce045d2f6c870c943
Fabian Affolter <mail@fabian-affolter.ch>
2024-08-20 14:04:52 +0200

    python312Packages.pyeapi: update changelog URL

--- pkgs/development/python-modules/pyeapi/default.nix
+++ pkgs/development/python-modules/pyeapi/default.nix
@@ -49,7 +49,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Client for Arista eAPI";
     homepage = "https://github.com/arista-eosplus/pyeapi";
-    changelog = "https://github.com/arista-eosplus/pyeapi/releases/tag/v${version}";
+    changelog = "https://github.com/arista-eosplus/pyeapi/releases/tag/v.${version}";
     license = licenses.bsd3;
     maintainers = with maintainers; [ astro ];
   };
