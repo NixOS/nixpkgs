d931a1bebc8128781495afb51672d35b647aad57
Aaron Jheng <wentworth@outlook.com>
2024-08-20 01:50:02 +0000

    notation: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10779,8 +10779,6 @@ with pkgs;
 
   noisetorch = callPackage ../applications/audio/noisetorch { };
 
-  notation = callPackage ../tools/security/notation { };
-
   notify-osd = callPackage ../applications/misc/notify-osd { };
 
   notes = qt6Packages.callPackage ../applications/office/notes {
