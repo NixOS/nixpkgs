62f3497182dff336dd1bdfb0da0efb876ac8b151
r-vdp <ramses@well-founded.dev>
2024-08-20 19:00:17 +0200

    mycelium: add rvdp as maintainer

--- pkgs/by-name/my/mycelium/package.nix
+++ pkgs/by-name/my/mycelium/package.nix
@@ -49,7 +49,7 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/threefoldtech/mycelium";
     changelog = "https://github.com/threefoldtech/mycelium/blob/${src.rev}/CHANGELOG.md";
     license = licenses.asl20;
-    maintainers = with maintainers; [ flokli matthewcroughan ];
+    maintainers = with maintainers; [ flokli matthewcroughan rvdp ];
     mainProgram = "mycelium";
   };
 }
