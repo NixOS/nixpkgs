f184c8ff2b73d0056f9eec4f8afbb03af1d7342d
Ashish SHUKLA <ashish.is@lostca.se>
2024-08-20 08:55:39 +0530

    weechat: 4.4.0 -> 4.4.1
    Changes: https://github.com/weechat/weechat/releases/tag/v4.4.1

--- pkgs/applications/networking/irc/weechat/default.nix
+++ pkgs/applications/networking/irc/weechat/default.nix
@@ -36,14 +36,14 @@ let
   in
     assert lib.all (p: p.enabled -> ! (builtins.elem null p.buildInputs)) plugins;
     stdenv.mkDerivation rec {
-      version = "4.4.0";
+      version = "4.4.1";
       pname = "weechat";
 
       hardeningEnable = [ "pie" ];
 
       src = fetchurl {
         url = "https://weechat.org/files/src/weechat-${version}.tar.xz";
-        hash = "sha256-3IYBi+mJ12Q9p2ucEPotWqwnBzvyVNGuqXsLv21VfWU=";
+        hash = "sha256-5d4L0UwqV6UFgTqDw9NyZI0tlXPccoNoV78ocXMmk2w=";
       };
 
       # Why is this needed? https://github.com/weechat/weechat/issues/2031
