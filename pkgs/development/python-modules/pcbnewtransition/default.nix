2a05f4200c193c259269117766e0cc0b6d180436
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-08-20 10:35:33 -0700

    python3Packages.pcbnewtransition: fix Python 3.12 build
    Before this change, pcbnewtransition couldn't build. It would fail with
    this issue already reported (and fixed!) upstream: https://github.com/yaqwsx/pcbnewTransition/issues/6
    
    Unfortunately, there hasn't been a release of pcbnewtransition with the
    fix yet, so I've opted to pull the latest commit from GitHub. I filed an
    issue upstream asking for a proper pypi release:
    https://github.com/yaqwsx/pcbnewTransition/issues/7.
    
    This is a blocker to get kikit building on Python 3.12:
    https://github.com/NixOS/nixpkgs/issues/325220

--- pkgs/development/python-modules/pcbnewtransition/default.nix
+++ pkgs/development/python-modules/pcbnewtransition/default.nix
@@ -1,22 +1,23 @@
 {
   pythonOlder,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   lib,
   kicad,
   versioneer,
 }:
 buildPythonPackage rec {
   pname = "pcbnewtransition";
-  version = "0.4.1";
+  version = "0.4.1-unstable-2024-06-05"; # There hasn't been a release yet with Python 3.12 support: https://github.com/yaqwsx/pcbnewTransition/issues/7
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
-  src = fetchPypi {
-    pname = "pcbnewTransition";
-    inherit version;
-    hash = "sha256-+mRExuDuEYxSSlrkEjSyPK+RRJZo+YJH7WnUVfjblRQ=";
+  src = fetchFromGitHub {
+    owner = "yaqwsx";
+    repo = "pcbnewTransition";
+    rev = "c01829807de18cddc6d735d04bab5fafa67dd95f";
+    hash = "sha256-1iub9t5YILtaSQdmUmVNYzPdvfjPlt7LQNxM7wRqas4=";
   };
 
   propagatedBuildInputs = [ kicad ];
