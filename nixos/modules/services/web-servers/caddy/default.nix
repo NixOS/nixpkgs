1b084bb0c7c03075af99bed8d5bccb4ec05cc717
ThinkChaos <ThinkChaos@users.noreply.github.com>
2024-08-21 17:46:39 -0400

    nixos/caddy: not "before" ACME certs using DNS validation
    Relax dependency with certs that are validated via DNS challenge since
    we know the HTTP server is not required for that validation.
    This allows marking the server's service as depending on the cert.

--- nixos/modules/services/web-servers/caddy/default.nix
+++ nixos/modules/services/web-servers/caddy/default.nix
@@ -5,8 +5,12 @@ with lib;
 let
   cfg = config.services.caddy;
 
+  certs = config.security.acme.certs;
   virtualHosts = attrValues cfg.virtualHosts;
-  acmeVHosts = filter (hostOpts: hostOpts.useACMEHost != null) virtualHosts;
+  acmeEnabledVhosts = filter (hostOpts: hostOpts.useACMEHost != null) virtualHosts;
+  vhostCertNames = unique (map (hostOpts: hostOpts.useACMEHost) acmeEnabledVhosts);
+  dependentCertNames = filter (cert: certs.${cert}.dnsProvider == null) vhostCertNames; # those that might depend on the HTTP server
+  independentCertNames = filter (cert: certs.${cert}.dnsProvider != null) vhostCertNames; # those that don't depend on the HTTP server
 
   mkVHostConf = hostOpts:
     let
@@ -50,8 +54,6 @@ let
   etcConfigFile = "caddy/caddy_config";
 
   configPath = "/etc/${etcConfigFile}";
-
-  acmeHosts = unique (catAttrs "useACMEHost" acmeVHosts);
 
   mkCertOwnershipAssertion = import ../../../security/acme/mk-cert-ownership-assertion.nix;
 in
@@ -332,7 +334,7 @@ in
       inherit (cfg) group user;
       cert = config.security.acme.certs.${name};
       groups = config.users.groups;
-    }) acmeHosts;
+    }) vhostCertNames;
 
     services.caddy.globalConfig = ''
       ${optionalString (cfg.email != null) "email ${cfg.email}"}
@@ -348,9 +350,10 @@ in
 
     systemd.packages = [ cfg.package ];
     systemd.services.caddy = {
-      wants = map (hostOpts: "acme-finished-${hostOpts.useACMEHost}.target") acmeVHosts;
-      after = map (hostOpts: "acme-selfsigned-${hostOpts.useACMEHost}.service") acmeVHosts;
-      before = map (hostOpts: "acme-${hostOpts.useACMEHost}.service") acmeVHosts;
+      wants = map (certName: "acme-finished-${certName}.target") vhostCertNames;
+      after = map (certName: "acme-selfsigned-${certName}.service") vhostCertNames
+        ++ map (certName: "acme-${certName}.service") independentCertNames; # avoid loading self-signed key w/ real cert, or vice-versa
+      before = map (certName: "acme-${certName}.service") dependentCertNames;
 
       wantedBy = [ "multi-user.target" ];
       startLimitIntervalSec = 14400;
@@ -397,10 +400,10 @@ in
 
     security.acme.certs =
       let
-        certCfg = map (useACMEHost: nameValuePair useACMEHost {
+        certCfg = map (certName: nameValuePair certName {
           group = mkDefault cfg.group;
           reloadServices = [ "caddy.service" ];
-        }) acmeHosts;
+        }) vhostCertNames;
       in
         listToAttrs certCfg;
 
