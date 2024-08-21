b71d4b2e1f61ad8ee8bc56bab46ea3429dc3e2d5
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-21 09:28:00 +0000

    shotcut: 24.06.26 -> 24.08.17

--- pkgs/by-name/sh/shotcut/package.nix
+++ pkgs/by-name/sh/shotcut/package.nix
@@ -18,13 +18,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "shotcut";
-  version = "24.06.26";
+  version = "24.08.17";
 
   src = fetchFromGitHub {
     owner = "mltframework";
     repo = "shotcut";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-9eQF3s4BAUK81/94z7cMkd2NWdNLVMraP08qsDmuAI8=";
+    hash = "sha256-PGLbcLX7r8IXwunAEDQfurxJQKkE3yInXdiFCoyS0s4=";
   };
 
   nativeBuildInputs = [
