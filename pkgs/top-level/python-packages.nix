72a4e51b59e82db222c1cff90e9f54628e1ee9bf
Jack Leightcap <jack@leightcap.com>
2024-08-20 23:02:00 -0400

    python312Packages.vunit-hdl: init at 4.7.0
    Signed-off-by: Jack Leightcap <jack@leightcap.com>
    Co-authored-by: OTABI Tomoya <tomoya.otabi@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17377,6 +17377,8 @@ self: super: with self; {
 
   vulture = callPackage ../development/python-modules/vulture { };
 
+  vunit-hdl = callPackage ../development/python-modules/vunit-hdl { };
+
   vxi11 = callPackage ../development/python-modules/vxi11 { };
 
   vyper = callPackage ../development/compilers/vyper { };
