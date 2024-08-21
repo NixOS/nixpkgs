04db3f04485c5e99e103bed2116b8cb0f4e66791
Sigmanificient <edhyjox@gmail.com>
2024-08-21 04:30:01 +0200

    python312Packages.rx: enable pyproject

--- pkgs/development/python-modules/rx/default.nix
+++ pkgs/development/python-modules/rx/default.nix
@@ -3,12 +3,14 @@
   fetchPypi,
   buildPythonPackage,
   pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "rx";
   version = "3.2.0";
-  format = "setuptools";
+  pyproject = true;
+
   disabled = pythonOlder "3.6";
 
   # Use fetchPypi to avoid the updater script to migrate it to `reactivex` which
@@ -18,6 +20,8 @@ buildPythonPackage rec {
     pname = "Rx";
     sha256 = "b657ca2b45aa485da2f7dcfd09fac2e554f7ac51ff3c2f8f2ff962ecd963d91c";
   };
+
+  build-system = [ setuptools ];
 
   doCheck = false; # PyPI tarball does not provides tests
 
