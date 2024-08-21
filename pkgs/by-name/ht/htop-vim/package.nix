890fec4fbee56ecbc92c3e4e8b619635c27d7c6c
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-21 10:08:57 -0300

    htop-nvim: rewrite
    - nixfmt-rfc-style
    - remove rec
    - fix version
    - remove references to by-name
    - remove nested with

--- pkgs/by-name/ht/htop-vim/package.nix
+++ pkgs/by-name/ht/htop-vim/package.nix
@@ -1,21 +1,25 @@
-{ lib, htop, fetchFromGitHub }:
+{
+  lib,
+  fetchFromGitHub,
+  htop,
+}:
 
-htop.overrideAttrs (oldAttrs: rec {
+htop.overrideAttrs (oldAttrs: {
   pname = "htop-vim";
-  version = "unstable-2023-02-16";
+  version = "0-unstable-2023-02-16";
 
   src = fetchFromGitHub {
     owner = "KoffeinFlummi";
-    repo = pname;
+    repo = "htop-vim";
     rev = "b2b58f8f152343b70c33b79ba51a298024278621";
     hash = "sha256-ZfdBAlnjoy8g6xwrR/i2+dGldMOfLlX6DRlNqB8pkGM=";
   };
 
-  meta = with lib; {
+  meta = {
     inherit (oldAttrs.meta) platforms license;
     description = "Interactive process viewer for Linux, with vim-style keybindings";
     homepage = "https://github.com/KoffeinFlummi/htop-vim";
-    maintainers = with maintainers; [ thiagokokada ];
+    maintainers = with lib.maintainers; [ thiagokokada ];
     mainProgram = "htop";
   };
 })
