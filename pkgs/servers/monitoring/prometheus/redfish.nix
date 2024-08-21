29ae0282935c7c2e02b66ae7db9970f9c7287de7
Underknowledge <Wagner.beccard+github@gmail.com>
2024-08-21 14:58:52 +0200

    Update editorConfig redfish.nix

--- pkgs/servers/monitoring/prometheus/redfish.nix
+++ pkgs/servers/monitoring/prometheus/redfish.nix
@@ -2,17 +2,14 @@
 
 buildGoModule rec {
   pname = "redfish-exporter";
-  version = "0.11.0"; 
-
+  version = "0.11.0";
   src = fetchFromGitHub {
     owner = "jenningsloy318";
     repo = "redfish_exporter";
     rev = "d388474c4024d09df0f3ab0290068e9072a7cf21";
     hash = "sha256-TkbME8rdmDvxzpK3KKk4XKS35oVGWaDVBzqiAjx+6sA==";
   };
-
   vendorHash = "sha256-M/5n6LFX8T0BCbM/TdA1qJN/3lNXLi/ZVlWucenp0yk=";
-  
   ldflags = [
     "-X 'main.Version=${version}'"
     "-X 'main.BuildRevision=${src.rev}'"
