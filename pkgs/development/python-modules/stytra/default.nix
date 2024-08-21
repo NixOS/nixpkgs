f8b3679a58788f2f37a1eee1ea2c89d0ea4c95fd
Sigmanificient <edhyjox@gmail.com>
2024-08-21 04:05:58 +0200

    python312Packages.stytra: remove outdated broken notice

--- pkgs/development/python-modules/stytra/default.nix
+++ pkgs/development/python-modules/stytra/default.nix
@@ -87,6 +87,5 @@ buildPythonPackage rec {
     homepage = "https://github.com/portugueslab/stytra";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ tbenst ];
-    broken = true; # incompatible with pyqtgraph>0.13.0: https://github.com/portugueslab/stytra/issues/87
   };
 }
