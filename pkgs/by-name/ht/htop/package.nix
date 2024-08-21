62fc9490951565e3ad4c25ec9740a632c5e33e6c
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-21 10:28:11 -0300

    htop: rewrite
    - nixfmt-rfc-style
    - remove references to pname
    - remove nested with
    - use static changelog link

--- pkgs/by-name/ht/htop/package.nix
+++ pkgs/by-name/ht/htop/package.nix
@@ -1,10 +1,17 @@
-{ lib, fetchFromGitHub, stdenv, autoreconfHook, pkg-config
-, ncurses
-, darwin
-, libcap
-, libnl
-, sensorsSupport ? stdenv.isLinux, lm_sensors
-, systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemd, systemd
+{
+  lib,
+  autoreconfHook,
+  darwin,
+  fetchFromGitHub,
+  libcap,
+  libnl,
+  lm_sensors,
+  ncurses,
+  pkg-config,
+  stdenv,
+  systemd,
+  sensorsSupport ? stdenv.isLinux,
+  systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemd,
 }:
 
 let
@@ -17,46 +24,54 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitHub {
     owner = "htop-dev";
-    repo = pname;
+    repo = "htop";
     rev = version;
     hash = "sha256-qDhQkzY2zj2yxbgFUXwE0MGEgAFOsAhnapUuetO9WTw=";
   };
 
-  nativeBuildInputs = [ autoreconfHook ]
-    ++ lib.optional stdenv.isLinux pkg-config
-  ;
+  nativeBuildInputs = [ autoreconfHook ] ++ lib.optional stdenv.isLinux pkg-config;
 
-  buildInputs = [ ncurses ]
+  buildInputs =
+    [ ncurses ]
     ++ lib.optional stdenv.isDarwin IOKit
-    ++ lib.optionals stdenv.isLinux [ libcap libnl ]
+    ++ lib.optionals stdenv.isLinux [
+      libcap
+      libnl
+    ]
     ++ lib.optional sensorsSupport lm_sensors
-    ++ lib.optional systemdSupport systemd
-  ;
+    ++ lib.optional systemdSupport systemd;
 
-  configureFlags = [ "--enable-unicode" "--sysconfdir=/etc" ]
+  configureFlags =
+    [
+      "--enable-unicode"
+      "--sysconfdir=/etc"
+    ]
     ++ lib.optionals stdenv.isLinux [
       "--enable-affinity"
       "--enable-capabilities"
       "--enable-delayacct"
     ]
-    ++ lib.optional sensorsSupport "--enable-sensors"
-  ;
+    ++ lib.optional sensorsSupport "--enable-sensors";
 
   postFixup =
     let
       optionalPatch = pred: so: lib.optionalString pred "patchelf --add-needed ${so} $out/bin/htop";
-    in lib.optionalString (!stdenv.hostPlatform.isStatic) ''
+    in
+    lib.optionalString (!stdenv.hostPlatform.isStatic) ''
       ${optionalPatch sensorsSupport "${lm_sensors}/lib/libsensors.so"}
       ${optionalPatch systemdSupport "${systemd}/lib/libsystemd.so"}
     '';
 
-  meta = with lib; {
+  meta = {
     description = "Interactive process viewer";
     homepage = "https://htop.dev";
-    license = licenses.gpl2Only;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ relrod SuperSandro2000 ];
-    changelog = "https://github.com/htop-dev/htop/blob/${version}/ChangeLog";
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [
+      relrod
+      SuperSandro2000
+    ];
+    changelog = "https://github.com/htop-dev/htop/blob/main/ChangeLog";
     mainProgram = "htop";
   };
 }
