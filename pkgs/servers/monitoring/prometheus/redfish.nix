6b223d15c2ad87280ad5148d8d0507121830d6ac
Underknowledge <Wagner.beccard+github@gmail.com>
2024-08-21 14:35:10 +0200

    Create redfish.nix

---
+++ pkgs/servers/monitoring/prometheus/redfish.nix
@@ -0,0 +1,34 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "redfish-exporter";
+  version = "0.11.0"; 
+
+  src = fetchFromGitHub {
+    owner = "jenningsloy318";
+    repo = "redfish_exporter";
+    rev = "d388474c4024d09df0f3ab0290068e9072a7cf21";
+    hash = "sha256-TkbME8rdmDvxzpK3KKk4XKS35oVGWaDVBzqiAjx+6sA==";
+  };
+
+  vendorHash = "sha256-M/5n6LFX8T0BCbM/TdA1qJN/3lNXLi/ZVlWucenp0yk=";
+  
+  ldflags = [
+    "-X 'main.Version=${version}'"
+    "-X 'main.BuildRevision=${src.rev}'"
+    "-X 'main.BuildBranch=master'"
+  ];
+
+  # Todo?
+  #   passthru.tests = { inherit (nixosTests.prometheus-exporters) redfish; };
+  # https://github.com/NixOS/nixpkgs/blob/master/nixos/tests/prometheus-exporters.nix
+
+  meta = with lib; {
+    description = "Prometheus exporter for Redfish metrics";
+    mainProgram = "redfish_exporter";
+    homepage = "https://github.com/jenningsloy318/redfish_exporter";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ Underknowledge ];
+    platforms = platforms.linux;
+  };
+}
