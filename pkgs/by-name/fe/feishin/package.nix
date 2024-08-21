21a75e0916edc7268ac0b7aad178a800304e4ab1
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-08-21 02:23:37 +0200

    feishin: change `${pname}` to string literal
    (cherry picked from commit 3a831099452985732c8725c6b693bb0f93522294)

--- pkgs/by-name/fe/feishin/package.nix
+++ pkgs/by-name/fe/feishin/package.nix
@@ -15,7 +15,7 @@ let
 
   src = fetchFromGitHub {
     owner = "jeffvli";
-    repo = pname;
+    repo = "feishin";
     rev = "v${version}";
     hash = "sha256-UOY0wjWGK7sal/qQbbkHjFUIA49QtbO+Ei6hSTOyHWk=";
   };
@@ -123,7 +123,7 @@ buildNpmPackage {
         mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
         ln -s \
           $out/share/feishin/resources/assets/icons/"$size"x"$size".png \
-          $out/share/icons/hicolor/"$size"x"$size"/apps/${pname}.png
+          $out/share/icons/hicolor/"$size"x"$size"/apps/feishin.png
       done
     ''
     + ''
@@ -135,7 +135,7 @@ buildNpmPackage {
       name = "feishin";
       desktopName = "Feishin";
       comment = "Full-featured Subsonic/Jellyfin compatible desktop music player";
-      icon = pname;
+      icon = "feishin";
       exec = "feishin %u";
       categories = [
         "Audio"
