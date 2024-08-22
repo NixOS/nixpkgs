0c8cc820de9bd025031000b01f6f1a6c99721472
Chris Dombroski <cdombroski@gmail.com>
2024-08-21 22:04:28 -0400

    zwave-js-ui: init at 9.27.8
    Closes #230686

---
+++ pkgs/by-name/zw/zwave-js-ui/package.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+  nixosTests,
+}:
+
+buildNpmPackage rec {
+  pname = "zwave-js-ui";
+  version = "9.27.8";
+
+  src = fetchFromGitHub {
+    owner = "zwave-js";
+    repo = "zwave-js-ui";
+    tag = "v${version}";
+    hash = "sha256-6Twyzt2BMozph39GMG3JghA54LYi40c1+WfSbAAO2oM=";
+  };
+  npmDepsHash = "sha256-2cDuRCxyn/wNgZks2Qg+iwqRX8CVRz8qVYh4ZlCR55I=";
+
+  passthru.tests.zwave-js-ui = nixosTests.zwave-js-ui;
+
+  meta = {
+    description = "Full featured Z-Wave Control Panel and MQTT Gateway";
+    homepage = "https://zwave-js.github.io/zwave-js-ui/";
+    license = lib.licenses.mit;
+    downloadPage = "https://github.com/zwave-js/zwave-js-ui/releases";
+    changelog = "https://github.com/zwave-js/zwave-js-ui/blob/v${version}/CHANGELOG.md";
+    mainProgram = "zwave-js-ui";
+    maintainers = with lib.maintainers; [ cdombroski ];
+  };
+}
