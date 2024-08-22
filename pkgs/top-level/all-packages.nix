94b71765fc45f726163720c3d7261348082c986a
Yaya <github@uwu.is>
2024-08-22 11:01:44 +0200

    pocket-casts: 0.7.0 -> 0.8.0
    https://github.com/FelicianoTech/pocket-casts-desktop-app/releases/tag/v0.8.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5925,7 +5925,7 @@ with pkgs;
   pn = callPackage ../tools/text/pn { };
 
   pocket-casts = callPackage ../applications/audio/pocket-casts {
-    electron = electron_27;
+    electron = electron_31;
   };
 
   portal = callPackage ../tools/misc/portal { };
