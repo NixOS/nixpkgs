b04252595536dd2c3c600fd33439e17097a4b382
Chatnoir Miki <cmiki@amono.me>
2024-08-22 16:50:25 +0800

    maintainers: add asterismono

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1879,6 +1879,13 @@
     githubId = 7745457;
     name = "Astavie";
   };
+  asterismono = {
+    email = "cmiki@amono.me";
+    github = "AsterisMono";
+    githubId = 54884471;
+    name = "Chatnoir Miki";
+    matrix = "@mikiiki:matrix.org";
+  };
   astro = {
     email = "astro@spaceboyz.net";
     github = "astro";
