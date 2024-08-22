65f5cbb5f29450dc32a2835aeb8cabfca2fedc09
K900 <me@0upti.me>
2024-08-22 13:42:07 +0300

    kdePackages: Gear 24.05.2 -> 24.08.0
    - kweathercore to gear
    - kipi removed
    - knotes removed
    - kgraphviewer and massif-visualizer added
    - kdevelop and friends to qt6

--- pkgs/kde/default.nix
+++ pkgs/kde/default.nix
@@ -55,13 +55,10 @@
       plasma-wayland-protocols = libsForQt5.plasma-wayland-protocols;
 
       selenium-webdriver-at-spi = null; # Used for integration tests that we don't run, stub
-      # Not ported to Qt6 yet
-      kdevelop-pg-qt = null;
-      okteta = null;
-      libmediawiki = null;
 
       alpaka = self.callPackage ./misc/alpaka {};
       kdiagram = self.callPackage ./misc/kdiagram {};
+      kdevelop-pg-qt = self.callPackage ./misc/kdevelop-pg-qt {};
       kdsoap-ws-discovery-client = self.callPackage ./misc/kdsoap-ws-discovery-client {};
       kirigami-addons = self.callPackage ./misc/kirigami-addons {};
       kio-extras-kf5 = self.callPackage ./misc/kio-extras-kf5 {};
@@ -69,7 +66,6 @@
       ktextaddons = self.callPackage ./misc/ktextaddons {};
       kunifiedpush = self.callPackage ./misc/kunifiedpush {};
       kup = self.callPackage ./misc/kup {};
-      kweathercore = self.callPackage ./misc/kweathercore {};
       marknote = self.callPackage ./misc/marknote {};
       mpvqt = self.callPackage ./misc/mpvqt {};
       oxygen-icons = self.callPackage ./misc/oxygen-icons {};
