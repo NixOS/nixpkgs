175de87d1c9da7d3fdac63a71e8235e5c05b9310
networkException <git@nwex.de>
2024-08-23 15:57:52 +0200

    ungoogled-chromium: 127.0.6533.119-2 -> 128.0.6613.84-1
    https://chromereleases.googleblog.com/2024/08/stable-channel-update-for-desktop_21.html
    
    This update includes 38 security fixes. Google is aware that an exploit
    for CVE-2024-7971 exists in the wild.
    
    CVEs:
    CVE-2024-7964 CVE-2024-7965 CVE-2024-7966 CVE-2024-7967 CVE-2024-7968
    CVE-2024-7969 CVE-2024-7971 CVE-2024-7972 CVE-2024-7973 CVE-2024-7974
    CVE-2024-7975 CVE-2024-7976 CVE-2024-7977 CVE-2024-7978 CVE-2024-7979
    CVE-2024-7980 CVE-2024-7981 CVE-2024-8033 CVE-2024-8034 CVE-2024-8035

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -21,17 +21,17 @@
   ungoogled-chromium = {
     deps = {
       gn = {
-        hash = "sha256-vzZu/Mo4/xATSD9KgKcRuBKVg9CoRZC9i0PEajYr4UM=";
-        rev = "b3a0bff47dd81073bfe67a402971bad92e4f2423";
+        hash = "sha256-BiMGbML5aNUt4JzzVqSszBj+8BMlEc92csNugo5qjUk=";
+        rev = "b2afae122eeb6ce09c52d63f67dc53fc517dbdc8";
         url = "https://gn.googlesource.com/gn";
-        version = "2024-06-06";
+        version = "2024-06-11";
       };
       ungoogled-patches = {
-        hash = "sha256-m7x7tPrJfddPER9uiSp983xGn3YSH+Bq01l14vOlwrU=";
-        rev = "127.0.6533.119-2";
+        hash = "sha256-UneOTEWgAbWCuzqtJgw+t5LJXBYmjpXBTxjhMMqzd/A=";
+        rev = "128.0.6613.84-1";
       };
     };
-    hash = "sha256-LuUDEpYCJLR/X+zjsF26aum4/Wfu2MNowofPu8iRVxI=";
-    version = "127.0.6533.119";
+    hash = "sha256-kUHJtJ4X8UDMP2TgHdFd6gEPzU28mBgxtGceVZCl5xM=";
+    version = "128.0.6613.84";
   };
 }
