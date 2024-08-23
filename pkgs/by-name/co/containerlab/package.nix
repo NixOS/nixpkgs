3c9d0c6cbfb811d9bc868b7ee36b61f2377a1a08
Aaron Jheng <wentworth@outlook.com>
2024-08-23 03:26:54 +0000

    containerlab: 0.56.0 -> 0.57.0

--- pkgs/by-name/co/containerlab/package.nix
+++ pkgs/by-name/co/containerlab/package.nix
@@ -6,26 +6,31 @@
 
 buildGoModule rec {
   pname = "containerlab";
-  version = "0.56.0";
+  version = "0.57.0";
 
   src = fetchFromGitHub {
     owner = "srl-labs";
     repo = "containerlab";
     rev = "v${version}";
-    hash = "sha256-7jQvfEplGIwTtIAOBc6R5OfcxPQuEbBnzBoAI9mE+7E=";
+    hash = "sha256-dqREbTDRhSEQg8swZ5ljhAtu3Yv421/QNmpkWopPWbk=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
 
-  vendorHash = "sha256-SPxpi9jG3Xtit9JOAv0xWd89qWJZXeIjMwoNTkVeDgk=";
+  vendorHash = "sha256-3ESz1wnbm6KfXSO5Fw4c7uUxL3K8Lsib5KAYoRD6vrw=";
 
   ldflags = [
     "-s"
     "-w"
-    "-X" "github.com/srl-labs/containerlab/cmd.version=${version}"
-    "-X" "github.com/srl-labs/containerlab/cmd.commit=${src.rev}"
-    "-X" "github.com/srl-labs/containerlab/cmd.date=1970-01-01T00:00:00Z"
+    "-X github.com/srl-labs/containerlab/cmd.version=${version}"
+    "-X github.com/srl-labs/containerlab/cmd.commit=${src.rev}"
+    "-X github.com/srl-labs/containerlab/cmd.date=1970-01-01T00:00:00Z"
   ];
+
+  preCheck = ''
+    # Fix failed TestLabelsInit test
+    export USER="runner"
+  '';
 
   postInstall = ''
     local INSTALL="$out/bin/containerlab"
