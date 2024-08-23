e41451febf8662e36e70180a78708558e4d1e438
jopejoe1 <johannes@joens.email>
2024-08-23 19:11:39 +0200

    qdiskinfo: remove CMAKE_BUILD_TYPE from cmakeFlags

--- pkgs/by-name/qd/qdiskinfo/package.nix
+++ pkgs/by-name/qd/qdiskinfo/package.nix
@@ -29,8 +29,9 @@ stdenv.mkDerivation (finalAttrs: {
     smartmontools
   ];
 
+  cmakeBuildType = "MinSizeRel";
+
   cmakeFlags = [
-    "-DCMAKE_BUILD_TYPE:STRING=MinSizeRel"
     "-DQT_VERSION_MAJOR=6"
   ];
 
