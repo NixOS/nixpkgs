caef6dd355ad6cc584a5e316c545808e7636b185
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-08-23 09:04:05 +0200

    curl: enable configure flag `--enable-versioned-symbols`
    This flag enable versioned symbols in CURL shared library.

--- pkgs/tools/networking/curl/default.nix
+++ pkgs/tools/networking/curl/default.nix
@@ -120,6 +120,7 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   configureFlags = [
+      "--enable-versioned-symbols"
       # Build without manual
       "--disable-manual"
       (lib.enableFeature c-aresSupport "ares")
