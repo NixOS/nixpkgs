59f7d71c0a3d84f16727e317597a60304431f7e4
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-23 18:25:00 -0300

    so: use self pattern
    So that we do not need to call itself as input parameter to testVersion.

--- pkgs/by-name/so/so/package.nix
+++ pkgs/by-name/so/so/package.nix
@@ -7,42 +7,55 @@
   pkg-config,
   rustPlatform,
   stdenv,
+  testers,
 }:
 
 let
   inherit (darwin.apple_sdk.frameworks) Security;
-  version = "0.4.9";
+  self = rustPlatform.buildRustPackage {
+    pname = "so";
+    version = "0.4.9";
+
+    src = fetchFromGitHub {
+      owner = "samtay";
+      repo = "so";
+      rev = "v${self.version}";
+      hash = "sha256-4IZNOclQj3ZLE6WRddn99CrV8OoyfkRBXnA4oEyMxv8=";
+    };
+
+    cargoHash = "sha256-hHXA/n/HQeBaD4QZ2b6Okw66poBRwNTpQWF0qBhLp/o=";
+
+    nativeBuildInputs = [ pkg-config ];
+
+    buildInputs =
+      [ openssl ]
+      ++ lib.optionals stdenv.isDarwin [
+        libiconv
+        Security
+      ];
+
+    strictDeps = true;
+
+    passthru = {
+      tests = {
+        version = testers.testVersion {
+          package = self;
+          command = ''
+            export HOME=$TMP
+            so --version
+          '';
+        };
+      };
+    };
+
+    meta = {
+      homepage = "https://github.com/samtay/so";
+      description = "TUI to StackExchange network";
+      changelog = "https://github.com/samtay/so/blob/main/CHANGELOG.md";
+      mainProgram = "so";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ AndersonTorres ];
+    };
+  };
 in
-rustPlatform.buildRustPackage {
-  pname = "so";
-  inherit version;
-
-  src = fetchFromGitHub {
-    owner = "samtay";
-    repo = "so";
-    rev = "v${version}";
-    hash = "sha256-4IZNOclQj3ZLE6WRddn99CrV8OoyfkRBXnA4oEyMxv8=";
-  };
-
-  cargoHash = "sha256-hHXA/n/HQeBaD4QZ2b6Okw66poBRwNTpQWF0qBhLp/o=";
-
-  nativeBuildInputs = [ pkg-config ];
-
-  buildInputs =
-    [ openssl ]
-    ++ lib.optionals stdenv.isDarwin [
-      libiconv
-      Security
-    ];
-
-  strictDeps = true;
-
-  meta = {
-    homepage = "https://github.com/samtay/so";
-    description = "TUI to StackExchange network";
-    changelog = "https://github.com/samtay/so/blob/main/CHANGELOG.md";
-    mainProgram = "so";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ AndersonTorres ];
-  };
-}
+self
