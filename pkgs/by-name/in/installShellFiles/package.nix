56ff39832db97b06ab3abc658a4569a30c6449f6
Anderson Torres <torres.anderson.85@protonmail.com>
2024-08-23 23:11:50 -0300

    installShellFiles: set passthru directly instead of overriding it

--- pkgs/by-name/in/installShellFiles/package.nix
+++ pkgs/by-name/in/installShellFiles/package.nix
@@ -5,14 +5,12 @@
 }:
 
 # See the header comment in ./setup-hook.sh for example usage.
-let
-  setupHook = makeSetupHook { name = "install-shell-files"; } ./setup-hook.sh;
-in
-setupHook.overrideAttrs (oldAttrs: {
-  passthru = (oldAttrs.passthru or { }) // {
+makeSetupHook {
+  name = "install-shell-files";
+  passthru = {
     tests = lib.packagesFromDirectoryRecursive {
       inherit callPackage;
       directory = ./tests;
     };
   };
-})
+} ./setup-hook.sh
