0f4c4c8baef2aab5da2ba23c9646d67bab82923d
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-24 19:36:18 +0000

    simdutf: 5.3.7 -> 5.4.15

--- pkgs/by-name/si/simdutf/package.nix
+++ pkgs/by-name/si/simdutf/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "simdutf";
-  version = "5.3.7";
+  version = "5.4.15";
 
   src = fetchFromGitHub {
     owner = "simdutf";
     repo = "simdutf";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-n99lt0OYK6OyLArdgPRabQ80ohQgHklpt9saDzKhvJw=";
+    hash = "sha256-oIrrI0Z5x1AvT9y0Ldg8zrkFJj1PZtebhJaL2UtEoB8=";
   };
 
   # Fix build on darwin
