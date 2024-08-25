6f479830a2611b68672aba475779c9832eb09dc7
6543 <6543@obermui.de>
2024-08-25 19:52:56 +0200

    i2p: 2.6.0 -> 2.6.1

--- pkgs/by-name/i2/i2p/package.nix
+++ pkgs/by-name/i2/i2p/package.nix
@@ -13,7 +13,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "i2p";
-  version = "2.6.0";
+  version = "2.6.1";
 
   src = fetchzip {
     urls = [
@@ -23,7 +23,7 @@ stdenv.mkDerivation (finalAttrs: {
       "https://files.i2p-projekt.de/"
       "https://download.i2p2.no/releases/"
     ]);
-    hash = "sha256-9f4L1JRDTT+sBCEI/GIM9q2u1VJuiZi7DI9qgJMftd0=";
+    hash = "sha256-ntjTXdpgcTReEVxzDEoq9r3NAqS7q4m+rlJXp7is1k0=";
   };
 
   strictDeps = true;
