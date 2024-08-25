93fc93def0f78f772cab176825579a448ed7a641
zendo <linzway@qq.com>
2024-08-25 11:25:36 +0800

    deploy-rs: unstable-2024-02-16 -> 0-unstable-2024-06-12

--- pkgs/by-name/de/deploy-rs/package.nix
+++ pkgs/by-name/de/deploy-rs/package.nix
@@ -1,33 +1,34 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, rustPlatform
-, darwin
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  rustPlatform,
+  darwin,
 }:
 
 rustPlatform.buildRustPackage {
   pname = "deploy-rs";
-  version = "unstable-2024-02-16";
+  version = "0-unstable-2024-06-12";
 
   src = fetchFromGitHub {
     owner = "serokell";
     repo = "deploy-rs";
-    rev = "0a0187794ac7f7a1e62cda3dabf8dc041f868790";
-    hash = "sha256-dTGGw2y8wvfjr+J9CjQbfdulOq72hUG17HXVNxpH1yE=";
+    rev = "3867348fa92bc892eba5d9ddb2d7a97b9e127a8a";
+    hash = "sha256-FaGrf7qwZ99ehPJCAwgvNY5sLCqQ3GDiE/6uLhxxwSY=";
   };
 
-  cargoHash = "sha256-Vo/45cZM/sBAaoikhEwCvduhMQjurwSZwCjwrIQn7IA=";
+  cargoHash = "sha256-lsW810bktMzHZIbuN3pz9N+IUcjNtE5J2AuB/G6pGWI=";
 
   buildInputs = lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.CoreServices
     darwin.apple_sdk.frameworks.SystemConfiguration
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Multi-profile Nix-flake deploy tool";
     homepage = "https://github.com/serokell/deploy-rs";
-    license = licenses.mpl20;
-    maintainers = with maintainers; [ teutat3s ];
+    license = lib.licenses.mpl20;
+    maintainers = with lib.maintainers; [ teutat3s ];
     mainProgram = "deploy";
   };
 }
