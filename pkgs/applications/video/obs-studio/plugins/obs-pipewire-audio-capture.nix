3b37c4eabbeb1e83b932fd0e68d38524b0604f43
Franz Pletz <fpletz@fnordicwalking.de>
2024-08-25 15:31:44 +0200

    obs-studio-plugins.obs-pipewire-audio-capture: 1.1.3 -> 1.1.5

--- pkgs/applications/video/obs-studio/plugins/obs-pipewire-audio-capture.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-pipewire-audio-capture.nix
@@ -10,21 +10,20 @@
 
 stdenv.mkDerivation rec {
   pname = "obs-pipewire-audio-capture";
-  version = "1.1.3";
+  version = "1.1.5";
 
   src = fetchFromGitHub {
     owner = "dimtpap";
     repo = pname;
     rev = version;
-    sha256 = "sha256-dL/+Y1uaD+7EY0UNWbxvh1TTLYfgk07qCqLLGvfzWZk=";
+    sha256 = "sha256-qYHU0m+jz/mQmjleITnzxNkTio5ir8dFkHKfmY4l0Es=";
   };
 
   nativeBuildInputs = [ cmake ninja pkg-config ];
   buildInputs = [ obs-studio pipewire ];
 
   cmakeFlags = [
-    "-DLIBOBS_INCLUDE_DIR=${obs-studio.src}/libobs"
-    "-Wno-dev"
+    "-DCMAKE_INSTALL_LIBDIR=/lib"
   ];
 
   meta = with lib; {
