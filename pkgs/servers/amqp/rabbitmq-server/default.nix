cf99b99fe673c2861da33adfc6598de46ec01a67
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-26 15:07:51 +0000

    rabbitmq-server: 3.13.6 -> 3.13.7

--- pkgs/servers/amqp/rabbitmq-server/default.nix
+++ pkgs/servers/amqp/rabbitmq-server/default.nix
@@ -38,12 +38,12 @@ in
 
 stdenv.mkDerivation rec {
   pname = "rabbitmq-server";
-  version = "3.13.6";
+  version = "3.13.7";
 
   # when updating, consider bumping elixir version in all-packages.nix
   src = fetchurl {
     url = "https://github.com/rabbitmq/rabbitmq-server/releases/download/v${version}/${pname}-${version}.tar.xz";
-    hash = "sha256-y6rHZpAhXVuO6sn18ZRvbG8qajTCVdE7iWkn+KXUhwI=";
+    hash = "sha256-GDUyYudwhQSLrFXO21W3fwmH2tl2STF9gSuZsb3GZh0=";
   };
 
   nativeBuildInputs = [ unzip xmlto docbook_xml_dtd_45 docbook_xsl zip rsync python3 ];
