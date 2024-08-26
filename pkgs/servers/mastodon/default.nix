c32d1c98dfc2a3cff7db2d15e985860b80c212ad
Izorkin <izorkin@elven.pw>
2024-08-26 23:05:56 +0300

    mastodon: 4.2.13 -> 4.3.0
    Changelog:
    https://github.com/mastodon/mastodon/releases/tag/v4.3.0

--- pkgs/servers/mastodon/default.nix
+++ pkgs/servers/mastodon/default.nix
@@ -1,7 +1,6 @@
 { lib, stdenv, nodejs-slim, bundlerEnv, nixosTests
-, yarn, callPackage, ruby, writeShellScript
-, fetchYarnDeps, fixup-yarn-lock
-, brotli
+, yarn-berry, callPackage, ruby, writeShellScript
+, brotli, python3
 
   # Allow building a fork or custom version of Mastodon:
 , pname ? "mastodon"
@@ -28,12 +27,12 @@ stdenv.mkDerivation rec {
     pname = "${pname}-modules";
     inherit src version;
 
-    yarnOfflineCache = fetchYarnDeps {
-      yarnLock = "${src}/yarn.lock";
+    yarnOfflineCache = callPackage ./yarn.nix {
+      inherit version src;
       hash = yarnHash;
     };
 
-    nativeBuildInputs = [ fixup-yarn-lock nodejs-slim yarn mastodonGems mastodonGems.wrappedRuby brotli ];
+    nativeBuildInputs = [ nodejs-slim yarn-berry mastodonGems mastodonGems.wrappedRuby brotli python3 ];
 
     RAILS_ENV = "production";
     NODE_ENV = "production";
@@ -42,29 +41,33 @@ stdenv.mkDerivation rec {
       runHook preBuild
 
       export HOME=$PWD
-      fixup-yarn-lock ~/yarn.lock
-      yarn config --offline set yarn-offline-mirror $yarnOfflineCache
-      yarn install --offline --frozen-lockfile --ignore-engines --ignore-scripts --no-progress
+      export YARN_ENABLE_TELEMETRY=0
+      export npm_config_nodedir=${nodejs-slim}
+      export SECRET_KEY_BASE_DUMMY=1
+
+      mkdir -p ~/.yarn/berry
+      ln -s $yarnOfflineCache ~/.yarn/berry/cache
+
+      yarn install --immutable --immutable-cache
 
       patchShebangs ~/bin
       patchShebangs ~/node_modules
 
-      # skip running yarn install
-      rm -rf ~/bin/yarn
+      bundle exec rails assets:precompile
 
-      OTP_SECRET=precompile_placeholder SECRET_KEY_BASE=precompile_placeholder \
-        rails assets:precompile
-      yarn cache clean --offline
+      yarn cache clean --all
       rm -rf ~/node_modules/.cache
 
+      # Remove execute permissions
+      find ~/public/assets -type f ! -perm 0555 \
+        -exec chmod 0444 {} ';'
+
       # Create missing static gzip and brotli files
-      gzip --best --keep ~/public/assets/500.html
-      gzip --best --keep ~/public/packs/report.html
-      find ~/public/assets -maxdepth 1 -type f -name '.*.json' \
-        -exec gzip --best --keep --force {} ';'
-      brotli --best --keep ~/public/packs/report.html
-      find ~/public/assets -type f -regextype posix-extended -iregex '.*\.(css|js|json|html)' \
+      find ~/public/assets -type f -regextype posix-extended -iregex '.*\.(css|html|js|json|svg)' \
+        -exec gzip --best --keep --force {} ';' \
         -exec brotli --best --keep {} ';'
+      gzip --best --keep ~/public/packs/report.html
+      brotli --best --keep ~/public/packs/report.html
 
       runHook postBuild
     '';
@@ -101,13 +104,14 @@ stdenv.mkDerivation rec {
     done
 
     # Remove execute permissions
-    chmod 0444 public/emoji/*.svg
+    find public/emoji -type f ! -perm 0555 \
+      -exec chmod 0444 {} ';'
 
     # Create missing static gzip and brotli files
-    find public -maxdepth 1 -type f -regextype posix-extended -iregex '.*\.(css|js|svg|txt|xml)' \
+    find public -maxdepth 1 -type f -regextype posix-extended -iregex '.*\.(js|txt)' \
       -exec gzip --best --keep --force {} ';' \
       -exec brotli --best --keep {} ';'
-    find public/emoji -type f -name '.*.svg' \
+    find public/emoji -type f -name '*.svg' \
       -exec gzip --best --keep --force {} ';' \
       -exec brotli --best --keep {} ';'
     ln -s assets/500.html.gz public/500.html.gz
@@ -133,7 +137,8 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     mkdir -p $out
-    cp -r * $out/
+    mv .{env*,ruby*} $out/
+    mv * $out/
     ln -s ${run-streaming} $out/run-streaming.sh
 
     runHook postInstall
