accd0c74031f5fe0c9acf5750da12c46bfd2fd49
Harbiinger <=>
2024-08-26 12:12:46 +0200

    maintainers: add harbiinger

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8657,6 +8657,13 @@
     githubId = 5317234;
     name = "Raphael Megzari";
   };
+  harbiinger = {
+    email = "theo.godin@protonmail.com";
+    matrix = "@hrbgr:matrix.org";
+    github = "harbiinger";
+    githubId = 55398594;
+    name = "Theo Godin";
+  };
   hardselius = {
     email = "martin@hardselius.dev";
     github = "hardselius";
