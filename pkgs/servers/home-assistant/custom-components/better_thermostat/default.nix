77d055b7b32ddb73e29e6f744edcebe4ea3bca47
Robert Schütz <mail@dotlambda.de>
2024-08-25 20:09:34 -0700

    home-assistant-custom-components.better_thermostat: format with nixfmt-rfc-style

--- pkgs/servers/home-assistant/custom-components/better_thermostat/default.nix
+++ pkgs/servers/home-assistant/custom-components/better_thermostat/default.nix
@@ -1,4 +1,8 @@
-{ lib, fetchFromGitHub, buildHomeAssistantComponent }:
+{
+  buildHomeAssistantComponent,
+  fetchFromGitHub,
+  lib,
+}:
 
 buildHomeAssistantComponent rec {
   owner = "KartoffelToby";
@@ -12,13 +16,11 @@ buildHomeAssistantComponent rec {
     hash = "sha256-noo96Uks+MHtTbwIoU5gnekp/IwMry0U18X4I1qe4LQ=";
   };
 
-  meta = with lib; {
-    changelog =
-      "https://github.com/KartoffelToby/better_thermostat/releases/tag/${version}";
-    description =
-      "Smart TRV control integrates room-temp sensors, window/door sensors, weather forecasts, and ambient probes for efficient heating and calibration, enhancing energy savings and comfort.";
+  meta = {
+    changelog = "https://github.com/KartoffelToby/better_thermostat/releases/tag/${version}";
+    description = "Smart TRV control integrates room-temp sensors, window/door sensors, weather forecasts, and ambient probes for efficient heating and calibration, enhancing energy savings and comfort";
     homepage = "https://better-thermostat.org/";
-    maintainers = with maintainers; [ mguentner ];
-    license = licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ mguentner ];
+    license = lib.licenses.agpl3Only;
   };
 }
