0a9774970be2429ea86c9c778230ebacf3a3f597
linsui <36977733+linsui@users.noreply.github.com>
2024-08-26 22:06:18 +0800

    fragments: 3.0.0 -> 3.0.1

--- pkgs/by-name/fr/fragments/package.nix
+++ pkgs/by-name/fr/fragments/package.nix
@@ -1,41 +1,42 @@
-{ lib
-, stdenv
-, fetchFromGitLab
-, appstream-glib
-, cargo
-, dbus
-, desktop-file-utils
-, git
-, glib
-, gtk4
-, libadwaita
-, meson
-, ninja
-, openssl
-, pkg-config
-, rustPlatform
-, rustc
-, sqlite
-, transmission_4
-, wrapGAppsHook4
+{
+  lib,
+  stdenv,
+  fetchFromGitLab,
+  appstream-glib,
+  cargo,
+  dbus,
+  desktop-file-utils,
+  git,
+  glib,
+  gtk4,
+  libadwaita,
+  meson,
+  ninja,
+  openssl,
+  pkg-config,
+  rustPlatform,
+  rustc,
+  sqlite,
+  transmission_4,
+  wrapGAppsHook4,
 }:
 
 stdenv.mkDerivation rec {
   pname = "fragments";
-  version = "3.0.0";
+  version = "3.0.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "World";
     repo = "Fragments";
     rev = version;
-    hash = "sha256-HtulyB1XYBsA595ghJN0EmyJT7DjGUbtJKaMGM3f0I8=";
+    hash = "sha256-lTOO6ZQWImaFqYZ3qerYYHWj/eOLYU/2k2Wh/ju9Njw=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-EUE+Qc+MqsKPqHMYJflZQ6zm3ErW+KLuJq/7HEBf8VM=";
+    hash = "sha256-7TIjd1ewazJGY8uG6f1B97ol+7+uLjnZVGC2/2DlUdQ=";
   };
 
   nativeBuildInputs = [
