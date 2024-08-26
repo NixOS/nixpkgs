2a30e8cdba3e763115cbd4a343d215e1bd413922
Ben Darwin <bcdarwin@gmail.com>
2024-08-26 18:30:30 -0400

    nnpdf: apply nixfmt

--- pkgs/applications/science/physics/nnpdf/default.nix
+++ pkgs/applications/science/physics/nnpdf/default.nix
@@ -1,16 +1,17 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, cmake
-, pkg-config
-, apfel
-, gsl
-, lhapdf
-, libarchive
-, yaml-cpp
-, python3
-, sqlite
-, swig4
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  apfel,
+  gsl,
+  lhapdf,
+  libarchive,
+  yaml-cpp,
+  python3,
+  sqlite,
+  swig4,
 }:
 
 stdenv.mkDerivation rec {
