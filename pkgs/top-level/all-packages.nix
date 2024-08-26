4caafb45e0626a1a759e141626d1b5b8d4dbfa5a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-26 19:23:15 +0200

    hpp-fcl: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20771,8 +20771,6 @@ with pkgs;
 
   hound = callPackage ../development/tools/misc/hound { };
 
-  hpp-fcl = callPackage ../development/libraries/hpp-fcl { };
-
   hpx = callPackage ../development/libraries/hpx {
     boost = boost179;
     asio = asio.override { boost = boost179; };
