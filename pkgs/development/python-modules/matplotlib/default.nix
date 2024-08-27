NixOS/nixpkgs

commit 513074bb9b4d8f5c1f025300a9cbfaeba79efa92

python312Packages.matplotlib: 3.9.0 -> 3.9.1
Changelog: https://github.com/matplotlib/matplotlib/releases/tag/v3.9.1

pkgs/development/python-modules/matplotlib/default.nix
@@ -89,7 +89,7 @@ let
 in
 
 buildPythonPackage rec {
-  version = "3.9.0";
+  version = "3.9.1";
   pname = "matplotlib";
   pyproject = true;
 
@@ -97,7 +97,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-5tKepsGeNLMPt9iLcIH4aaAwFPZv4G1izHfVpuqI7Xo=";
+    hash = "sha256-3gaxm425XdM9DcF8kmx8nr7Z9XIHS2+sT2UGimgU0BA=";
   };
 
   patches = lib.optionals stdenv.isDarwin [

commit 8888333686f3da12e2e65ea02512ae37667e8681

python3Packages.matplotlib: remove upstreamed patch
Fixes the build on Darwin.

Fixes: 513074bb9b4d8f5c1f025300a9cbfaeba79efa92

pkgs/development/python-modules/matplotlib/darwin-sandbox-crash.patch
@@ -1,14 +1,0 @@
---- a/lib/matplotlib/font_manager.py
-+++ b/lib/matplotlib/font_manager.py
-@@ -266,8 +266,11 @@ def _get_fontconfig_fonts():
- @lru_cache
- def _get_macos_fonts():
-     """Cache and list the font paths known to ``system_profiler SPFontsDataType``."""
--    d, = plistlib.loads(
--        subprocess.check_output(["system_profiler", "-xml", "SPFontsDataType"]))
-+    try:
-+        d, = plistlib.loads(
-+            subprocess.check_output(["system_profiler", "-xml", "SPFontsDataType"]))
-+    except (OSError, subprocess.CalledProcessError, plistlib.InvalidFileException):
-+        return []
-     return [Path(entry["path"]) for entry in d["_items"]]

pkgs/development/python-modules/matplotlib/default.nix
@@ -100,12 +100,6 @@ buildPythonPackage rec {
     hash = "sha256-3gaxm425XdM9DcF8kmx8nr7Z9XIHS2+sT2UGimgU0BA=";
   };
 
-  patches = lib.optionals stdenv.isDarwin [
-    # Don't crash when running in Darwin sandbox
-    # Submitted upstream: https://github.com/matplotlib/matplotlib/pull/28498
-    ./darwin-sandbox-crash.patch
-  ];
-
   env.XDG_RUNTIME_DIR = "/tmp";
 
   # Matplotlib tries to find Tcl/Tk by opening a Tk window and asking the