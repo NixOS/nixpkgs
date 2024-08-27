1feaea91067bd93947cba889f2479c1733908880
Ben Darwin <bcdarwin@gmail.com>
2024-08-27 00:13:11 -0400

    swig: swig3 -> swig4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19018,7 +19018,7 @@ with pkgs;
 
   swig3 = callPackage ../development/tools/misc/swig/3.x.nix { };
   swig4 = callPackage ../development/tools/misc/swig/4.nix { };
-  swig = swig3;
+  swig = swig4;
   swigWithJava = swig;
 
   c2ffi = callPackage ../development/tools/misc/c2ffi { };
