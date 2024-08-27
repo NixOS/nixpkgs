6c702884e9b7e9944f5111038183d17fe90b51b3
Felix Buehler <account@buehler.rocks>
2024-08-27 20:43:05 +0200

    nixos/services.neard: remove `with lib;`

--- nixos/modules/services/desktops/neard.nix
+++ nixos/modules/services/desktops/neard.nix
@@ -1,19 +1,16 @@
 # neard service.
 { config, lib, pkgs, ... }:
-
-with lib;
-
 {
   ###### interface
   options = {
     services.neard = {
-      enable = mkEnableOption "neard, an NFC daemon";
+      enable = lib.mkEnableOption "neard, an NFC daemon";
     };
   };
 
 
   ###### implementation
-  config = mkIf config.services.neard.enable {
+  config = lib.mkIf config.services.neard.enable {
     environment.systemPackages = [ pkgs.neard ];
 
     services.dbus.packages = [ pkgs.neard ];
