ad318f1ff4d7f004a66d2c4e835c913e2d45a36c
Martin Joerg <martin.joerg@gmail.com>
2024-08-27 15:14:51 +0000

    edk2: remove patch obsoleted by OpenSSL 3.0.14

--- pkgs/by-name/ed/edk2/package.nix
+++ pkgs/by-name/ed/edk2/package.nix
@@ -69,11 +69,6 @@ edk2 = stdenv.mkDerivation {
       cd CryptoPkg/Library/OpensslLib/openssl
       tar --strip-components=1 -xf ${buildPackages.openssl.src}
 
-      # Fix missing INT64_MAX include that edk2 explicitly does not provide
-      # via it's own <stdint.h>. Let's pull in openssl's definition instead:
-      sed -i crypto/property/property_parse.c \
-          -e '1i #include "internal/numbers.h"'
-
       # Apply OpenSSL patches.
       ${lib.pipe buildPackages.openssl.patches [
         (builtins.filter (
