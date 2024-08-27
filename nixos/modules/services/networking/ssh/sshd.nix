07d609e1e260b72c32cdf9f01be83aef5e859261
nora <48135649+Noratrieb@users.noreply.github.com>
2024-08-27 14:25:34 +0200

    sshd: Don't use rounds for ed25519 in example
    ed25519 has no configuration parameters, -a is ignored by ssh-keygen for it.

--- nixos/modules/services/networking/ssh/sshd.nix
+++ nixos/modules/services/networking/ssh/sshd.nix
@@ -269,7 +269,7 @@ in
           ];
         example =
           [ { type = "rsa"; bits = 4096; path = "/etc/ssh/ssh_host_rsa_key"; rounds = 100; openSSHFormat = true; }
-            { type = "ed25519"; path = "/etc/ssh/ssh_host_ed25519_key"; rounds = 100; comment = "key comment"; }
+            { type = "ed25519"; path = "/etc/ssh/ssh_host_ed25519_key"; comment = "key comment"; }
           ];
         description = ''
           NixOS can automatically generate SSH host keys.  This option
