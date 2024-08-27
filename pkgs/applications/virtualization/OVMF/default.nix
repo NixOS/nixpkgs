ab9a643c0db2fb62b1bbb10b66d59e654364cf7f
Martin Joerg <martin.joerg@gmail.com>
2024-08-27 15:14:51 +0000

    OVMFFull: update debian source to 2024.05-1

--- pkgs/applications/virtualization/OVMF/default.nix
+++ pkgs/applications/virtualization/OVMF/default.nix
@@ -76,8 +76,8 @@ let
       "debian/python"
       "debian/PkKek-1-*.pem"
     ];
-    rev = "refs/tags/debian/2023.11-5";
-    hash = "sha256-4vDOoZbWQg7yKXiQprK8CRzKGkbKQYlAgQzTqmNxxjU=";
+    rev = "refs/tags/debian/2024.05-1";
+    hash = "sha256-uAjXJaHOVh944ZxcA2IgCsrsncxuhc0JKlsXs0E03s0=";
   };
 
   buildPrefix = "Build/*/*";
