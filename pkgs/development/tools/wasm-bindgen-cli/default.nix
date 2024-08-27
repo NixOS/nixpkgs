884fbccb56c3bccdbedb3343e782cbde121ce9d2
xvrqt <git@xvrqt.com>
2024-08-27 02:30:54 -0700

    wasm-bindgen-cli: 0.2.92 -> 0.2.93

--- pkgs/development/tools/wasm-bindgen-cli/default.nix
+++ pkgs/development/tools/wasm-bindgen-cli/default.nix
@@ -1,18 +1,18 @@
-{ lib
-, rustPlatform
-, fetchCrate
-, nix-update-script
-, nodejs
-, pkg-config
-, openssl
-, stdenv
-, curl
-, Security
-, version ? "0.2.92"
-, hash ? "sha256-1VwY8vQy7soKEgbki4LD+v259751kKxSxmo/gqE6yV0="
-, cargoHash ? "sha256-aACJ+lYNEU8FFBs158G1/JG8sc6Rq080PeKCMnwdpH0="
+{
+  lib,
+  rustPlatform,
+  fetchCrate,
+  nix-update-script,
+  nodejs,
+  pkg-config,
+  openssl,
+  stdenv,
+  curl,
+  Security,
+  version ? "0.2.93",
+  hash ? "sha256-DDdu5mM3gneraM85pAepBXWn3TMofarVR4NbjMdz3r0=",
+  cargoHash ? "sha256-birrg+XABBHHKJxfTKAMSlmTVYLmnmqMDfRnmG6g/YQ=",
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "wasm-bindgen-cli";
   inherit version hash cargoHash;
@@ -21,22 +21,28 @@ rustPlatform.buildRustPackage rec {
     inherit pname version hash;
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [pkg-config];
 
-  buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [ curl Security ];
+  buildInputs = [openssl] ++ lib.optionals stdenv.isDarwin [curl Security];
 
-  nativeCheckInputs = [ nodejs ];
+  nativeCheckInputs = [nodejs];
 
   # tests require it to be ran in the wasm-bindgen monorepo
   doCheck = false;
 
   meta = with lib; {
     homepage = "https://rustwasm.github.io/docs/wasm-bindgen/";
-    license = with licenses; [ asl20 /* or */ mit ];
+    license = with licenses; [
+      asl20
+      /*
+      or
+      */
+      mit
+    ];
     description = "Facilitating high-level interactions between wasm modules and JavaScript";
-    maintainers = with maintainers; [ rizary ];
+    maintainers = with maintainers; [rizary];
     mainProgram = "wasm-bindgen";
   };
 
-  passthru.updateScript = nix-update-script { };
+  passthru.updateScript = nix-update-script {};
 }
