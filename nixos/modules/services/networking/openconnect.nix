add721a30e44d34b1a1653ef4d54b2eb968f4e46
Felix Buehler <account@buehler.rocks>
2024-08-28 20:33:20 +0200

    nixos/modules/services/networking/openconnect.nix: remove `with lib;`

--- nixos/modules/services/networking/openconnect.nix
+++ nixos/modules/services/networking/openconnect.nix
@@ -1,44 +1,43 @@
 { config, lib, options, pkgs, ... }:
-with lib;
 let
   cfg = config.networking.openconnect;
   openconnect = cfg.package;
-  pkcs11 = types.strMatching "pkcs11:.+" // {
+  pkcs11 = lib.types.strMatching "pkcs11:.+" // {
     name = "pkcs11";
     description = "PKCS#11 URI";
   };
   interfaceOptions = {
     options = {
-      autoStart = mkOption {
+      autoStart = lib.mkOption {
         default = true;
         description = "Whether this VPN connection should be started automatically.";
-        type = types.bool;
+        type = lib.types.bool;
       };
 
-      gateway = mkOption {
+      gateway = lib.mkOption {
         description = "Gateway server to connect to.";
         example = "gateway.example.com";
-        type = types.str;
+        type = lib.types.str;
       };
 
-      protocol = mkOption {
+      protocol = lib.mkOption {
         description = "Protocol to use.";
         example = "anyconnect";
         type =
-          types.enum [ "anyconnect" "array" "nc" "pulse" "gp" "f5" "fortinet" ];
+          lib.types.enum [ "anyconnect" "array" "nc" "pulse" "gp" "f5" "fortinet" ];
       };
 
-      user = mkOption {
+      user = lib.mkOption {
         description = "Username to authenticate with.";
         example = "example-user";
-        type = types.nullOr types.str;
+        type = lib.types.nullOr lib.types.str;
         default = null;
       };
 
       # Note: It does not make sense to provide a way to declaratively
       # set an authentication cookie, because they have to be requested
       # for every new connection and would only work once.
-      passwordFile = mkOption {
+      passwordFile = lib.mkOption {
         description = ''
           File containing the password to authenticate with. This
           is passed to `openconnect` via the
@@ -46,24 +45,24 @@ let
         '';
         default = null;
         example = "/var/lib/secrets/openconnect-passwd";
-        type = types.nullOr types.path;
+        type = lib.types.nullOr lib.types.path;
       };
 
-      certificate = mkOption {
+      certificate = lib.mkOption {
         description = "Certificate to authenticate with.";
         default = null;
         example = "/var/lib/secrets/openconnect_certificate.pem";
-        type = with types; nullOr (either path pkcs11);
+        type = with lib.types; nullOr (either path pkcs11);
       };
 
-      privateKey = mkOption {
+      privateKey = lib.mkOption {
         description = "Private key to authenticate with.";
         example = "/var/lib/secrets/openconnect_private_key.pem";
         default = null;
-        type = with types; nullOr (either path pkcs11);
+        type = with lib.types; nullOr (either path pkcs11);
       };
 
-      extraOptions = mkOption {
+      extraOptions = lib.mkOption {
         description = ''
           Extra config to be appended to the interface config. It should
           contain long-format options as would be accepted on the command
@@ -79,23 +78,23 @@ let
           no-http-keepalive = true;
           no-dtls = true;
         };
-        type = with types; attrsOf (either str bool);
+        type = with lib.types; attrsOf (either str bool);
       };
     };
   };
   generateExtraConfig = extra_cfg:
-    strings.concatStringsSep "\n" (attrsets.mapAttrsToList
+    lib.concatStringsSep "\n" (lib.mapAttrsToList
       (name: value: if (value == true) then name else "${name}=${value}")
-      (attrsets.filterAttrs (_: value: value != false) extra_cfg));
+      (lib.filterAttrs (_: value: value != false) extra_cfg));
   generateConfig = name: icfg:
     pkgs.writeText "config" ''
       interface=${name}
-      ${optionalString (icfg.protocol != null) "protocol=${icfg.protocol}"}
-      ${optionalString (icfg.user != null) "user=${icfg.user}"}
-      ${optionalString (icfg.passwordFile != null) "passwd-on-stdin"}
-      ${optionalString (icfg.certificate != null)
+      ${lib.optionalString (icfg.protocol != null) "protocol=${icfg.protocol}"}
+      ${lib.optionalString (icfg.user != null) "user=${icfg.user}"}
+      ${lib.optionalString (icfg.passwordFile != null) "passwd-on-stdin"}
+      ${lib.optionalString (icfg.certificate != null)
       "certificate=${icfg.certificate}"}
-      ${optionalString (icfg.privateKey != null) "sslkey=${icfg.privateKey}"}
+      ${lib.optionalString (icfg.privateKey != null) "sslkey=${icfg.privateKey}"}
 
       ${generateExtraConfig icfg.extraOptions}
     '';
@@ -103,7 +102,7 @@ let
     description = "OpenConnect Interface - ${name}";
     requires = [ "network-online.target" ];
     after = [ "network.target" "network-online.target" ];
-    wantedBy = optional icfg.autoStart "multi-user.target";
+    wantedBy = lib.optional icfg.autoStart "multi-user.target";
 
     serviceConfig = {
       Type = "simple";
@@ -117,9 +116,9 @@ let
   };
 in {
   options.networking.openconnect = {
-    package = mkPackageOption pkgs "openconnect" { };
+    package = lib.mkPackageOption pkgs "openconnect" { };
 
-    interfaces = mkOption {
+    interfaces = lib.mkOption {
       description = "OpenConnect interfaces.";
       default = { };
       example = {
@@ -130,16 +129,16 @@ in {
           passwordFile = "/var/lib/secrets/openconnect-passwd";
         };
       };
-      type = with types; attrsOf (submodule interfaceOptions);
+      type = with lib.types; attrsOf (submodule interfaceOptions);
     };
   };
 
   config = {
-    systemd.services = mapAttrs' (name: value: {
+    systemd.services = lib.mapAttrs' (name: value: {
       name = "openconnect-${name}";
       value = generateUnit name value;
     }) cfg.interfaces;
   };
 
-  meta.maintainers = with maintainers; [ alyaeanyx ];
+  meta.maintainers = with lib.maintainers; [ alyaeanyx ];
 }
