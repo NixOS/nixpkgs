15a69cfd1a36c8e3e3e7b5ca2c9e0ac6347a8d51
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-28 18:03:48 +0000

    qovery-cli: 1.2.4 -> 1.2.5

--- pkgs/tools/admin/qovery-cli/default.nix
+++ pkgs/tools/admin/qovery-cli/default.nix
@@ -9,13 +9,13 @@
 
 buildGoModule rec {
   pname = "qovery-cli";
-  version = "1.2.4";
+  version = "1.2.5";
 
   src = fetchFromGitHub {
     owner = "Qovery";
     repo = "qovery-cli";
     rev = "refs/tags/v${version}";
-    hash = "sha256-bTlbrL2pP6KB2g3bMsvyT24/7Sc4I707KL3hJktsWpA=";
+    hash = "sha256-RoHQXlfdVJVumgr/Ruph0O79vSf162fwj8R+MrRnmXY=";
   };
 
   vendorHash = "sha256-z7O0IAXGCXV63WjaRG+7c7q/rlqkV12XWNLhsduvk6s=";
