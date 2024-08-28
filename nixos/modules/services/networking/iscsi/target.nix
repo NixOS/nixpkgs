e7675398a5497e88320b4fd8ffec803e2b61aedf
Felix Buehler <account@buehler.rocks>
2024-08-28 22:58:56 +0200

    nixos/modules/services/networking/iscsi/target.nix: remove `with lib;`

--- nixos/modules/services/networking/iscsi/target.nix
+++ nixos/modules/services/networking/iscsi/target.nix
@@ -1,17 +1,14 @@
 { config, lib, pkgs, ... }:
-
-with lib;
-
 let
   cfg = config.services.target;
 in
 {
   ###### interface
   options = {
-    services.target = with types; {
-      enable = mkEnableOption "the kernel's LIO iscsi target";
+    services.target = with lib.types; {
+      enable = lib.mkEnableOption "the kernel's LIO iscsi target";
 
-      config = mkOption {
+      config = lib.mkOption {
         type = attrs;
         default = {};
         description = ''
@@ -23,7 +20,7 @@ in
   };
 
   ###### implementation
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     environment.etc."target/saveconfig.json" = {
       text = builtins.toJSON cfg.config;
       mode = "0600";
