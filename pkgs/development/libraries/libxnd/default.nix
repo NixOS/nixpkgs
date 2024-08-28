a28e50c46a170a777d168a889d1f4bd132db8c0b
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-08-28 12:10:39 +0200

    libxnd: unstable-2019-08-01 -> 0.2.0-unstable-2023-11-17

--- pkgs/development/libraries/libxnd/default.nix
+++ pkgs/development/libraries/libxnd/default.nix
@@ -1,18 +1,19 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, libndtypes
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  libndtypes,
 }:
 
 stdenv.mkDerivation {
   pname = "libxnd";
-  version = "unstable-2019-08-01";
+  version = "0.2.0-unstable-2023-11-17";
 
   src = fetchFromGitHub {
     owner = "xnd-project";
-    repo = "xnd";
-    rev = "6f305cd40d90b4f3fc2fe51ae144b433d186a6cc";
-    sha256 = "1n31d64qwlc7m3qkzbafhp0dgrvgvkdx89ykj63kll7r1n3yk59y";
+    repo = "libxnd";
+    rev = "e1a06d9f6175f4f4e1da369b7e907ad6b2952c00";
+    hash = "sha256-RWt2Nx0tfMghQES2SM+0jbAU7IunuuTORhBe2tvqVTY=";
   };
 
   buildInputs = [ libndtypes ];
@@ -20,12 +21,12 @@ stdenv.mkDerivation {
   # Override linker with cc (symlink to either gcc or clang)
   # Library expects to use cc for linking
   configureFlags = [
-      # Override linker with cc (symlink to either gcc or clang)
-      # Library expects to use cc for linking
-      "LD=${stdenv.cc.targetPrefix}cc"
-      # needed for tests
-      "--with-includes=${libndtypes}/include"
-      "--with-libs=${libndtypes}/lib"
+    # Override linker with cc (symlink to either gcc or clang)
+    # Library expects to use cc for linking
+    "LD=${stdenv.cc.targetPrefix}cc"
+    # needed for tests
+    "--with-includes=${libndtypes}/include"
+    "--with-libs=${libndtypes}/lib"
   ];
 
   # other packages which depend on libxnd seem to expect overflow.h, but
