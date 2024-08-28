5fada979f50c6634e303b01ff272e4c353f932e4
Coosis <1159727122@qq.com>
2024-08-28 21:24:24 +0800

    codelldb: init at 1.10.0

--- pkgs/by-name/co/codelldb/package.nix
+++ pkgs/by-name/co/codelldb/package.nix
@@ -1,62 +1,35 @@
 {
-  pkgs ? import <nixpkgs> { },
+  stdenv, lib, fetchurl, unzip
 }:
 
 let
-  stdenv = pkgs.stdenv;
-  lib = pkgs.lib;
-  binary =
-    with stdenv;
-    if stdenv.isLinux then
-      if stdenv.system == "x86_64-linux" then
-        {
-          url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-x86_64-linux.vsix";
-          sha256 = "1jhjs2na8cjhd4yqib1vij33094sgf6lsxg9i13f2x0rh4czyayi";
-        }
-      else if stdenv.system == "aarch64-linux" then
-        {
-          url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-aarch64-linux.vsix";
-          sha256 = "01kxnbj9kq6sczcqp31g6xmx4l5dm4pawyqba8vlax9lg1hzd08a";
-        }
-      else if stdenv.system == "arm-linux" then
-        {
-          url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-arm-linux.vsix";
-          sha256 = "1jb8mx5l06zg418qnw9v53yn4ni4yhkcg3yglfdrqdflagkmxz2b";
-        }
-      else
-        {
-          url = "";
-          sha256 = "";
-        }
-    else if stdenv.isDarwin then
-      if stdenv.system == "x86_64-darwin" then
-        {
-          url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-x86_64-darwin.vsix";
-          sha256 = "1kzm0hrg6ji2wjxhnsj45g49dq5kll8vb760131k8154f1b0vcci";
-        }
-      else if stdenv.system == "aarch64-darwin" then
-        {
-          url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-aarch64-darwin.vsix";
-          sha256 = "0hbs2rr4r8zlii3srbc9xbmn5wm3p88cdsx85xp2vibbf9d7kc2a";
-        }
-      else
-        {
-          url = "";
-          sha256 = "";
-        }
-    else
-      {
-        url = "";
-        sha256 = "";
-      };
+  binary = if stdenv.system == "x86_64-linux" then {
+    url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-x86_64-linux.vsix";
+    sha256 = "1jhjs2na8cjhd4yqib1vij33094sgf6lsxg9i13f2x0rh4czyayi";
+  } else if stdenv.system == "aarch64-linux" then {
+    url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-aarch64-linux.vsix";
+    sha256 = "01kxnbj9kq6sczcqp31g6xmx4l5dm4pawyqba8vlax9lg1hzd08a";
+  } else if stdenv.system == "arm-linux" then {
+    url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-arm-linux.vsix";
+    sha256 = "1jb8mx5l06zg418qnw9v53yn4ni4yhkcg3yglfdrqdflagkmxz2b";
+  } else if stdenv.system == "x86_64-darwin" then {
+    url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-x86_64-darwin.vsix";
+    sha256 = "1kzm0hrg6ji2wjxhnsj45g49dq5kll8vb760131k8154f1b0vcci";
+  } else if stdenv.system == "aarch64-darwin" then {
+    url = "https://github.com/vadimcn/codelldb/releases/download/v1.10.0/codelldb-aarch64-darwin.vsix";
+    sha256 = "0hbs2rr4r8zlii3srbc9xbmn5wm3p88cdsx85xp2vibbf9d7kc2a";
+  } else {
+    url = "";
+    sha256 = "";
+  };
 in
-pkgs.stdenv.mkDerivation {
+stdenv.mkDerivation {
   pname = "codelldb";
   version = "1.10.0";
 
-  nativeBuildInputs = [ pkgs.unzip ];
+  nativeBuildInputs = [ unzip ];
 
-  src = pkgs.fetchurl {
+  src = fetchurl {
     url = binary.url;
     sha256 = binary.sha256;
   };
