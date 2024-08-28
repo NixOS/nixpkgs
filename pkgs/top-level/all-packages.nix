1b067f5e31f1cf5ce428f08f68170a0beea9c473
Jan Tojnar <jtojnar@gmail.com>
2024-08-28 23:38:27 +0200

    gnome-session-ctl: Move to top-level

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36515,6 +36515,8 @@ with pkgs;
 
   gnome-browser-connector = callPackage ../desktops/gnome/extensions/gnome-browser-connector { };
 
+  gnome-session-ctl = callPackage ../desktops/gnome/core/gnome-session/ctl.nix { };
+
   gnustep = recurseIntoAttrs (callPackage ../desktops/gnustep { });
 
   lomiri = recurseIntoAttrs (callPackage ../desktops/lomiri { });
