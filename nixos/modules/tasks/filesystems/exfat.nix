0d33ca368252cd71d0659bb7415aaec5b043aa8d
Felix Buehler <account@buehler.rocks>
2024-08-28 22:59:20 +0200

    nixos/modules/tasks/filesystems/exfat.nix: remove `with lib;`

--- nixos/modules/tasks/filesystems/exfat.nix
+++ nixos/modules/tasks/filesystems/exfat.nix
@@ -1,9 +1,6 @@
 { config, lib, pkgs, ... }:
-
-with lib;
-
 {
-  config = mkIf (config.boot.supportedFilesystems.exfat or false) {
+  config = lib.mkIf (config.boot.supportedFilesystems.exfat or false) {
     system.fsPackages = if config.boot.kernelPackages.kernelOlder "5.7" then [
       pkgs.exfat # FUSE
     ] else [
