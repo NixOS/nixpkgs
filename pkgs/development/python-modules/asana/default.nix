58408ce69996bb00999893dd3361834bae1dfc9b
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-28 20:37:35 +0000

    python312Packages.asana: 5.0.9 -> 5.0.10

--- pkgs/development/python-modules/asana/default.nix
+++ pkgs/development/python-modules/asana/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "asana";
-  version = "5.0.9";
+  version = "5.0.10";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "asana";
     repo = "python-asana";
     rev = "refs/tags/v${version}";
-    hash = "sha256-7qJdPIk2xSF909chvjwT9zdSQ6NA6cgwefJGaBw1BQY=";
+    hash = "sha256-nuBvRqrs00OIY3UzN7bF5dB15TZqeE43o1BIpBaJZcQ=";
   };
 
   build-system = [ setuptools ];
