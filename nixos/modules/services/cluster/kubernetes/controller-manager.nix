b6eef1d104b36e38c5c9e2df40d6b2481327dbcc
Felix Buehler <account@buehler.rocks>
2024-08-28 20:33:11 +0200

    nixos/modules/services/cluster/kubernetes/controller-manager.nix: remove `with lib;`

--- nixos/modules/services/cluster/kubernetes/controller-manager.nix
+++ nixos/modules/services/cluster/kubernetes/controller-manager.nix
@@ -1,7 +1,4 @@
 { config, lib, options, pkgs, ... }:
-
-with lib;
-
 let
   top = config.services.kubernetes;
   otop = options.services.kubernetes;
@@ -9,72 +6,72 @@ let
 in
 {
   imports = [
-    (mkRenamedOptionModule [ "services" "kubernetes" "controllerManager" "address" ] ["services" "kubernetes" "controllerManager" "bindAddress"])
-    (mkRemovedOptionModule [ "services" "kubernetes" "controllerManager" "insecurePort" ] "")
+    (lib.mkRenamedOptionModule [ "services" "kubernetes" "controllerManager" "address" ] ["services" "kubernetes" "controllerManager" "bindAddress"])
+    (lib.mkRemovedOptionModule [ "services" "kubernetes" "controllerManager" "insecurePort" ] "")
   ];
 
   ###### interface
   options.services.kubernetes.controllerManager = with lib.types; {
 
-    allocateNodeCIDRs = mkOption {
+    allocateNodeCIDRs = lib.mkOption {
       description = "Whether to automatically allocate CIDR ranges for cluster nodes.";
       default = true;
       type = bool;
     };
 
-    bindAddress = mkOption {
+    bindAddress = lib.mkOption {
       description = "Kubernetes controller manager listening address.";
       default = "127.0.0.1";
       type = str;
     };
 
-    clusterCidr = mkOption {
+    clusterCidr = lib.mkOption {
       description = "Kubernetes CIDR Range for Pods in cluster.";
       default = top.clusterCidr;
-      defaultText = literalExpression "config.${otop.clusterCidr}";
+      defaultText = lib.literalExpression "config.${otop.clusterCidr}";
       type = str;
     };
 
-    enable = mkEnableOption "Kubernetes controller manager";
+    enable = lib.mkEnableOption "Kubernetes controller manager";
 
-    extraOpts = mkOption {
+    extraOpts = lib.mkOption {
       description = "Kubernetes controller manager extra command line options.";
       default = "";
       type = separatedString " ";
     };
 
-    featureGates = mkOption {
+    featureGates = lib.mkOption {
       description = "Attribute set of feature gates.";
       default = top.featureGates;
-      defaultText = literalExpression "config.${otop.featureGates}";
+      defaultText = lib.literalExpression "config.${otop.featureGates}";
       type = attrsOf bool;
     };
 
     kubeconfig = top.lib.mkKubeConfigOptions "Kubernetes controller manager";
 
-    leaderElect = mkOption {
+    leaderElect = lib.mkOption {
       description = "Whether to start leader election before executing main loop.";
       type = bool;
       default = true;
     };
 
-    rootCaFile = mkOption {
+    rootCaFile = lib.mkOption {
       description = ''
         Kubernetes controller manager certificate authority file included in
         service account's token secret.
       '';
       default = top.caFile;
-      defaultText = literalExpression "config.${otop.caFile}";
+      defaultText = lib.literalExpression "config.${otop.caFile}";
       type = nullOr path;
     };
 
-    securePort = mkOption {
+    securePort = lib.mkOption {
       description = "Kubernetes controller manager secure listening port.";
       default = 10252;
       type = int;
     };
 
-    serviceAccountKeyFile = mkOption {
+    serviceAccountKeyFile = lib.mkOption {
       description = ''
         Kubernetes controller manager PEM-encoded private RSA key file used to
         sign service account tokens
@@ -83,19 +80,19 @@ in
       type = nullOr path;
     };
 
-    tlsCertFile = mkOption {
+    tlsCertFile = lib.mkOption {
       description = "Kubernetes controller-manager certificate file.";
       default = null;
       type = nullOr path;
     };
 
-    tlsKeyFile = mkOption {
+    tlsKeyFile = lib.mkOption {
       description = "Kubernetes controller-manager private key file.";
       default = null;
       type = nullOr path;
     };
 
-    verbosity = mkOption {
+    verbosity = lib.mkOption {
       description = ''
         Optional glog verbosity level for logging statements. See
         <https://github.com/kubernetes/community/blob/master/contributors/devel/logging.md>
@@ -107,7 +104,7 @@ in
   };
 
   ###### implementation
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     systemd.services.kube-controller-manager = {
       description = "Kubernetes Controller Manager Service";
       wantedBy = [ "kubernetes.target" ];
@@ -117,26 +114,26 @@ in
         Restart = "on-failure";
         Slice = "kubernetes.slice";
         ExecStart = ''${top.package}/bin/kube-controller-manager \
-          --allocate-node-cidrs=${boolToString cfg.allocateNodeCIDRs} \
+          --allocate-node-cidrs=${lib.boolToString cfg.allocateNodeCIDRs} \
           --bind-address=${cfg.bindAddress} \
-          ${optionalString (cfg.clusterCidr!=null)
+          ${lib.optionalString (cfg.clusterCidr!=null)
             "--cluster-cidr=${cfg.clusterCidr}"} \
-          ${optionalString (cfg.featureGates != {})
-            "--feature-gates=${concatStringsSep "," (builtins.attrValues (mapAttrs (n: v: "${n}=${trivial.boolToString v}") cfg.featureGates))}"} \
+          ${lib.optionalString (cfg.featureGates != {})
+            "--feature-gates=${lib.concatStringsSep "," (builtins.attrValues (lib.mapAttrs (n: v: "${n}=${lib.trivial.boolToString v}") cfg.featureGates))}"} \
           --kubeconfig=${top.lib.mkKubeConfig "kube-controller-manager" cfg.kubeconfig} \
-          --leader-elect=${boolToString cfg.leaderElect} \
-          ${optionalString (cfg.rootCaFile!=null)
+          --leader-elect=${lib.boolToString cfg.leaderElect} \
+          ${lib.optionalString (cfg.rootCaFile!=null)
             "--root-ca-file=${cfg.rootCaFile}"} \
           --secure-port=${toString cfg.securePort} \
-          ${optionalString (cfg.serviceAccountKeyFile!=null)
+          ${lib.optionalString (cfg.serviceAccountKeyFile!=null)
             "--service-account-private-key-file=${cfg.serviceAccountKeyFile}"} \
-          ${optionalString (cfg.tlsCertFile!=null)
+          ${lib.optionalString (cfg.tlsCertFile!=null)
             "--tls-cert-file=${cfg.tlsCertFile}"} \
-          ${optionalString (cfg.tlsKeyFile!=null)
+          ${lib.optionalString (cfg.tlsKeyFile!=null)
             "--tls-private-key-file=${cfg.tlsKeyFile}"} \
-          ${optionalString (elem "RBAC" top.apiserver.authorizationMode)
+          ${lib.optionalString (lib.elem "RBAC" top.apiserver.authorizationMode)
             "--use-service-account-credentials"} \
-          ${optionalString (cfg.verbosity != null) "--v=${toString cfg.verbosity}"} \
+          ${lib.optionalString (cfg.verbosity != null) "--v=${toString cfg.verbosity}"} \
           ${cfg.extraOpts}
         '';
         WorkingDirectory = top.dataDir;
@@ -162,7 +159,7 @@ in
       };
     };
 
-    services.kubernetes.controllerManager.kubeconfig.server = mkDefault top.apiserverAddress;
+    services.kubernetes.controllerManager.kubeconfig.server = lib.mkDefault top.apiserverAddress;
   };
 
   meta.buildDocsInSandbox = false;
