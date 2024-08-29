99265c7248ee21918ffd45b573ec431c5c0c4e0d
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-08-29 10:46:18 +0200

    uplosi: add passthru.update-script
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/by-name/up/uplosi/package.nix
+++ pkgs/by-name/up/uplosi/package.nix
@@ -1,8 +1,11 @@
-{ lib
-, fetchFromGitHub
-, buildGoModule
-, installShellFiles
+{
+  lib,
+  fetchFromGitHub,
+  buildGoModule,
+  installShellFiles,
+  nix-update-script,
 }:
+
 buildGoModule rec {
   pname = "uplosi";
   version = "0.2.1";
@@ -17,7 +20,10 @@ buildGoModule rec {
   vendorHash = "sha256-f8Yz99qlN0S0Ybewifc0VQanYXKinb1togBkUwDPSvw=";
 
   CGO_ENABLED = "0";
-  ldflags = [ "-s" "-w" "-X main.version=${version}" ];
+  ldflags = [
+    "-s"
+    "-X main.version=${version}"
+  ];
 
   nativeBuildInputs = [ installShellFiles ];
 
@@ -28,13 +34,18 @@ buildGoModule rec {
       --zsh <($out/bin/uplosi completion zsh)
   '';
 
-  meta = with lib; {
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
     description = "Upload OS images to cloud provider";
     homepage = "https://github.com/edgelesssys/uplosi";
     changelog = "https://github.com/edgelesssys/uplosi/releases/tag/v${version}";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
     mainProgram = "uplosi";
-    maintainers = with maintainers; [ katexochen malt3 ];
-    platforms = platforms.unix;
+    maintainers = with lib.maintainers; [
+      katexochen
+      malt3
+    ];
+    platforms = lib.platforms.unix;
   };
 }
