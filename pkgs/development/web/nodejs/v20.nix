82d9747f3921ea4def9974b31deba19d8081d05e
Fabian Schmitthenner <fabian.schmitthenner@sap.com>
2024-08-29 13:42:25 +0000

    nodejs_20: 20.15.1 -> 20.17.0

--- pkgs/development/web/nodejs/v20.nix
+++ pkgs/development/web/nodejs/v20.nix
@@ -12,28 +12,16 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "20.15.1";
-  sha256 = "sha256-/dU6VynZNmkaKhFRBG+0iXchy4sPyir5V4I6m0D+DDQ=";
+  version = "20.17.0";
+  sha256 = "sha256-mr8DrCM2LGA4frtjOlFjA2NxRcs8F3vjNIsWiA/Ysow=";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
     ./node-npm-build-npm-package-logic.patch
     ./use-correct-env-in-tests.patch
     (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/534c122de166cb6464b489f3e6a9a544ceb1c913.patch";
-      hash = "sha256-4q4LFsq4yU1xRwNsM1sJoNVphJCnxaVe2IyL6AeHJ/I=";
-    })
-    (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/14863e80584e579fd48c55f6373878c821c7ff7e.patch";
-      hash = "sha256-I7Wjc7DE059a/ZyXAvAqEGvDudPjxQqtkBafckHCFzo=";
-    })
-    (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/87598d4b63ef2c827a2bebdfa0f1540c35718519.patch";
-      hash = "sha256-efRJ2nN9QXaT91SQTB+ESkHvXtBq30Cb9BEDEZU9M/8=";
-    })
-    (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/d0a6b605fba6cd69a82e6f12ff0363eef8fe1ee9.patch";
-      hash = "sha256-TfYal/PikRZHL6zpAlC3SmkYXCe+/8Gs83dLX/X/P/k=";
+      url = "https://github.com/nodejs/node/commit/02b30954a8a5b1f93b5e50d005c7a01b122a86ef.patch";
+      hash = "sha256-3hbxVa5zRM2M3rc/YiLcdudWt32Z+LWkcHdhkUcjz24=";
     })
   ] ++ gypPatches;
 }
