13419f08762836e22952968c82a10ff59176b72c
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-08-29 11:25:08 +0200

    prow: move to by-name
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -38917,8 +38917,6 @@ with pkgs;
 
   jx = callPackage ../applications/networking/cluster/jx { };
 
-  prow = callPackage ../applications/networking/cluster/prow { };
-
   pv-migrate = callPackage ../applications/networking/cluster/pv-migrate { };
 
   tagref = callPackage ../tools/misc/tagref { };
