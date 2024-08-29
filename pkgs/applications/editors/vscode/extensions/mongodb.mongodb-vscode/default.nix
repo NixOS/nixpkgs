99e3c5a1d6b69fa9b45b6647370dadf9b5ed2bfb
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-29 16:18:39 +0000

    vscode-extensions.mongodb.mongodb-vscode: 1.7.0 -> 1.8.0

--- pkgs/applications/editors/vscode/extensions/mongodb.mongodb-vscode/default.nix
+++ pkgs/applications/editors/vscode/extensions/mongodb.mongodb-vscode/default.nix
@@ -4,8 +4,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "mongodb-vscode";
     publisher = "mongodb";
-    version = "1.7.0";
-    hash = "sha256-EDU8kQLTQIe5D905ZVskFt/28Mzv1Zr7auqG4tksQ/o=";
+    version = "1.8.0";
+    hash = "sha256-KtpXhDRf9vFS0iSQCJzywmIlRMhWLOlSuK7DPc0ddnw=";
   };
 
   meta = {
