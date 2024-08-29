ccd8cd8bd85ec5fab368a0f1d87eae73230ff973
Florens Pauwels <github@florens.be>
2024-08-29 20:00:23 +0200

    maintainers: add florensie

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6852,6 +6852,11 @@
     githubId = 183879;
     name = "Florian Klink";
   };
+  florensie = {
+    github = "florensie";
+    githubId = 13403842;
+    name = "Florens Pauwels";
+  };
   florentc = {
     github = "florentc";
     githubId = 1149048;
