83a2a5e63eca3fb9e33b59b95a15c4162b07ba69
Matthias Beyer <mail@beyermatthias.de>
2024-08-29 19:55:42 +0200

    home-assistant-custom-lovelace-modules.weather-card: init at 1.5.0
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
    Co-authored-by: Arne Keller <2012gdwu+github@posteo.de>

--- pkgs/servers/home-assistant/custom-lovelace-modules/default.nix
+++ pkgs/servers/home-assistant/custom-lovelace-modules/default.nix
@@ -41,6 +41,8 @@
 
   valetudo-map-card = callPackage ./valetudo-map-card { };
 
+  weather-card = callPackage ./weather-card { };
+
   zigbee2mqtt-networkmap = callPackage ./zigbee2mqtt-networkmap { };
 }
 // lib.optionalAttrs pkgs.config.allowAliases {
