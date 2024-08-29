577e5200aefb592134fa119859e28e0ce864ca7a
Doron Behar <doron.behar@gmail.com>
2024-08-29 09:39:27 +0300

    libogg: modernize

--- pkgs/development/libraries/libogg/default.nix
+++ pkgs/development/libraries/libogg/default.nix
@@ -1,17 +1,25 @@
-{ lib, stdenv, fetchurl }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libogg";
   version = "1.3.5";
 
   src = fetchurl {
-    url = "http://downloads.xiph.org/releases/ogg/${pname}-${version}.tar.xz";
+    url = "http://downloads.xiph.org/releases/ogg/libogg-${finalAttrs.version}.tar.xz";
     sha256 = "01b7050bghdvbxvw0gzv588fn4a27zh42ljpwzm4vrf8dziipnf4";
   };
 
-  outputs = [ "out" "dev" "doc" ];
+  outputs = [
+    "out"
+    "dev"
+    "doc"
+  ];
 
-  meta = with lib; {
+  meta = {
     description = "Media container library to manipulate Ogg files";
     longDescription = ''
       Library to work with Ogg multimedia container format.
@@ -19,8 +27,8 @@ stdenv.mkDerivation rec {
       plethora of codecs. Open format free for anyone to use.
     '';
     homepage = "https://xiph.org/ogg/";
-    license = licenses.bsd3;
-    maintainers = [ maintainers.ehmry ];
-    platforms = platforms.all;
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ ehmry ];
+    platforms = lib.platforms.all;
   };
-}
+})
