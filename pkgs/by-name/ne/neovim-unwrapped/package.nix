deec4c2213f8bbe95c517403d96f0f8864c22d5c
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-08-29 16:36:56 -0400

    neovim-unwrapped: fix disallowedRequisites check
    see https://nix.dev/manual/nix/2.18/release-notes/rl-2.2 or https://nix.dev/manual/nix/2.18/language/advanced-attributes#adv-attr-outputChecks

--- pkgs/by-name/ne/neovim-unwrapped/package.nix
+++ pkgs/by-name/ne/neovim-unwrapped/package.nix
@@ -147,7 +147,7 @@ in {
       find "$out" -type f -exec remove-references-to -t ${stdenv.cc} '{}' +
     '';
     # check that the above patching actually works
-    disallowedRequisites = [ stdenv.cc ] ++ lib.optional (lua != codegenLua) codegenLua;
+    outputChecks.out.disallowedRequisites = [ stdenv.cc ] ++ lib.optional (lua != codegenLua) codegenLua;
 
     cmakeFlagsArray = [
       # Don't use downloaded dependencies. At the end of the configurePhase one
