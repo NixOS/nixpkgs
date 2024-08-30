5815d63425dc4798c60603307ca546f5056fcbdf
Randy Eckenrode <randy@largeandhighquality.com>
2024-08-29 21:50:47 -0400

    libcaca: fix build on Darwin
    libcaca tries to access a private API that is no longer available. It
    worked before the SDK changes because no Darwin frameworks were
    available in the build environment, but there is now always an SDK
    available, causing it to try to build Cocoa code that no longer works.

--- pkgs/by-name/li/libcaca/package.nix
+++ pkgs/by-name/li/libcaca/package.nix
@@ -39,6 +39,10 @@ stdenv.mkDerivation rec {
 
   configureFlags = [
     (if x11Support then "--enable-x11" else "--disable-x11")
+  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    # Suppresses a build failure building Cocoa support due to accessing private ivar `_running`,
+    # which no longer available.
+    (lib.enableFeature false "cocoa")
   ];
 
   env.NIX_CFLAGS_COMPILE = lib.optionalString (!x11Support) "-DX_DISPLAY_MISSING";
