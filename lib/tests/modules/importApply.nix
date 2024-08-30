cdd69f959a69ff30d813c1df827a28db25a07aeb
Robert Hensing <robert@roberthensing.nl>
2024-08-30 10:02:29 +0200

    Format

--- lib/tests/modules/importApply.nix
+++ lib/tests/modules/importApply.nix
@@ -1,8 +1,5 @@
-{ lib, ... }: {
-  options.value = lib.mkOption {
-    default = 1;
-  };
-  imports = [
-    (lib.modules.importApply ./importApply-function.nix { foo = "abc"; })
-  ];
+{ lib, ... }:
+{
+  options.value = lib.mkOption { default = 1; };
+  imports = [ (lib.modules.importApply ./importApply-function.nix { foo = "abc"; }) ];
 }
