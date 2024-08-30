a4aabf88668a363631452188ee3c658df4e7be38
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-30 02:17:48 +0000

    neocmakelsp: 0.8.1 -> 0.8.2

--- pkgs/development/tools/language-servers/neocmakelsp/default.nix
+++ pkgs/development/tools/language-servers/neocmakelsp/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "neocmakelsp";
-  version = "0.8.1";
+  version = "0.8.2";
 
   src = fetchFromGitHub {
     owner = "Decodetalkers";
     repo = "neocmakelsp";
     rev = "v${version}";
-    hash = "sha256-SDSqYfnHI6nFJrLUDApYR1nfGdcPdPihrb54gNIRkLU=";
+    hash = "sha256-t9cFECz4olFQ3VOuZzqHRMuvC8df1qaF7etb9ThJiok=";
   };
 
-  cargoHash = "sha256-Rlu2m+pbaU+EunQ7pthYPSRZo1yVF/+L114WxCv3l9c=";
+  cargoHash = "sha256-m+eO2y6TNRYc9Nau5ma9qcZcj7xUdxjo34eBmuXANkU=";
 
   meta = with lib; {
     description = "CMake lsp based on tower-lsp and treesitter";
