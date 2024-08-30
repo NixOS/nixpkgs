337bd8c31c26c152b1cb88b10cf4b600ef158a99
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-08-30 09:18:44 +0200

    fscryptctl: 1.0.0 -> 1.2.0
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/os-specific/linux/fscryptctl/default.nix
+++ pkgs/os-specific/linux/fscryptctl/default.nix
@@ -1,21 +1,28 @@
-{ lib, stdenv, fetchFromGitHub }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  pandoc,
+}:
 
 stdenv.mkDerivation rec {
   pname = "fscryptctl";
-  version = "1.0.0";
-
-  goPackagePath = "github.com/google/fscrypt";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "fscryptctl";
     rev = "v${version}";
-    sha256 = "1hwj726mm0yhlcf6523n07h0yq1rvkv4km64h3ydpjcrcxklhw6l";
+    hash = "sha256-5suEdSpX8alDkSnSnyiIjUmZq98eK0ZPVAtDKhOs65c=";
   };
+
+  nativeBuildInputs = [ pandoc ];
+
+  strictDeps = true;
 
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
 
-  meta = with lib; {
+  meta = {
     description = "Small C tool for Linux filesystem encryption";
     mainProgram = "fscryptctl";
     longDescription = ''
@@ -33,9 +40,9 @@ stdenv.mkDerivation rec {
       documentation for filesystem encryption before using fscryptctl.
     '';
     inherit (src.meta) homepage;
-    changelog = "https://github.com/google/fscryptctl/releases/tag/v${version}";
-    license = licenses.asl20;
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ primeos ];
+    changelog = "https://github.com/google/fscryptctl/blob/master/NEWS.md";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ primeos ];
   };
 }
