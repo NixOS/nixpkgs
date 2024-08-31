5fc67c2ab7b68e9bc58e6feee6a941163471e2e2
Felix Singer <felixsinger@posteo.net>
2024-08-31 21:43:40 +0200

    libjaylink: Move package definition into by-name directory
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22227,8 +22227,6 @@ with pkgs;
 
   liburcu = callPackage ../development/libraries/liburcu { };
 
-  libjaylink = callPackage ../development/libraries/libjaylink { };
-
   libusb-compat-0_1 = callPackage ../development/libraries/libusb-compat/0.1.nix { };
 
   libusb1 = callPackage ../development/libraries/libusb1 {
