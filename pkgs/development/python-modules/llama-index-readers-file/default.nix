b2101fd600ce3ddf944dc1a26894681be14c4ec4
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-01 01:38:39 +0200

    python311Packages.llama-index-readers-file: 0.1.27 -> 0.2.0

--- pkgs/development/python-modules/llama-index-readers-file/default.nix
+++ pkgs/development/python-modules/llama-index-readers-file/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-readers-file";
-  version = "0.1.27";
+  version = "0.2.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_index_readers_file";
     inherit version;
-    hash = "sha256-BMm8MGOpQJ/Nh3932bKr/VAuBydjqm3XiODGN0GLhmQ=";
+    hash = "sha256-Vdt8MWZrqyst0vdi1iLy3I5zkzlDyS+IOIaKkB5QVwg=";
   };
 
   pythonRelaxDeps = [
