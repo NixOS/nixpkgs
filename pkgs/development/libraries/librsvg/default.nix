38d39e6d6e94294cca62bbfc05d5bab36c36a795
Randy Eckenrode <randy@largeandhighquality.com>
2024-08-30 21:48:43 -0400

    librsvg: link libobjc on Darwin independent of CPU architecture
    aarch64-darwin now also needs to link libobjc explicitly when using the
    new Darwin SDKs.

--- pkgs/development/libraries/librsvg/default.nix
+++ pkgs/development/libraries/librsvg/default.nix
@@ -113,11 +113,8 @@ stdenv.mkDerivation (finalAttrs: {
     ${lib.optionalString (stdenv.hostPlatform.emulatorAvailable buildPackages) (stdenv.hostPlatform.emulator buildPackages)} ${lib.getDev gdk-pixbuf}/bin/gdk-pixbuf-query-loaders
   '';
 
-  # librsvg only links Foundation, but it also requiers libobjc. The Framework.tbd in the 11.0 SDK
-  # reexports libobjc, but the one in the 10.12 SDK does not, so link it manually.
-  env = lib.optionalAttrs (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64) {
-    NIX_LDFLAGS = "-lobjc";
-  };
+  # librsvg only links Foundation, but it also requiers libobjc.
+  env = lib.optionalAttrs stdenv.hostPlatform.isDarwin { NIX_LDFLAGS = "-lobjc"; };
 
   preConfigure = ''
     PKG_CONFIG_VAPIGEN_VAPIGEN="$(type -p vapigen)"
