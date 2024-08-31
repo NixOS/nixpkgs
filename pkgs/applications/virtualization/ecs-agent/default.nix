f51c8db4727a3cac066988e1dcdff5868947e0e3
R. Ryantm <ryantm-bot@ryantm.com>
2024-08-31 19:43:34 +0000

    ecs-agent: 1.85.1 -> 1.86.3

--- pkgs/applications/virtualization/ecs-agent/default.nix
+++ pkgs/applications/virtualization/ecs-agent/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "amazon-ecs-agent";
-  version = "1.85.1";
+  version = "1.86.3";
 
   src = fetchFromGitHub {
     rev = "v${version}";
     owner = "aws";
     repo = pname;
-    hash = "sha256-TrfFJ6N1DreO3NcznXBcNZziESAMxWa4FR+KzDjRDmM=";
+    hash = "sha256-YygvAYoqbWVmtBgHNrP0Xp1zSgCv2PsnWARWyo3K8rM=";
   };
 
   vendorHash = null;
