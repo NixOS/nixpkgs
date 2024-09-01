b1fb30f511c9a77afac16bd4aabf27794f709cc4
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-01 01:23:31 +0000

    go-minimock: 3.3.14 -> 3.4.0

--- pkgs/development/tools/go-minimock/default.nix
+++ pkgs/development/tools/go-minimock/default.nix
@@ -2,20 +2,20 @@
 
 buildGoModule rec {
   pname = "go-minimock";
-  version = "3.3.14";
+  version = "3.4.0";
 
   src = fetchFromGitHub {
     owner = "gojuno";
     repo = "minimock";
     rev = "v${version}";
-    hash = "sha256-J4clMn65l7L+qSHbJBMDDRoEfgGHKllRu8nvTGlbgaw=";
+    hash = "sha256-IfowEauUoMybElxThsAZxtdZu3LCqwgpJM5wLQI/yAc=";
   };
 
   ldflags = [
     "-s" "-w" "-X main.version=${version}"
   ];
 
-  vendorHash = "sha256-60mOX0N8q9Q4Cb1G3hxQgykkDgvrVLYaZElptsW1PPI=";
+  vendorHash = "sha256-ObyIiHidGioSelX2qplFMkC0KX44h0rT6ePa3QKpNfU=";
 
   doCheck = true;
 
