75000c10a64a8cb4d641fd8e3bf4981606ef355e
oxalica <oxalicc@pm.me>
2024-09-01 14:39:15 -0400

    jq: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8936,8 +8936,6 @@ with pkgs;
 
   jpylyzer = with python3Packages; toPythonApplication jpylyzer;
 
-  jq = callPackage ../development/tools/jq { };
-
   jiq = callPackage ../development/tools/misc/jiq { };
 
   jql = callPackage ../development/tools/jql { };
