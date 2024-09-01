9654c12c66d9a0c07278d713f7793fb8b2e4814d
Robert Scott <code@humanleg.org.uk>
2024-09-01 22:25:35 +0100

    grpc: 1.62.1 -> 1.62.3
    addressing CVE-2024-7246

--- pkgs/development/libraries/grpc/default.nix
+++ pkgs/development/libraries/grpc/default.nix
@@ -21,7 +21,7 @@
 
 stdenv.mkDerivation rec {
   pname = "grpc";
-  version = "1.62.1"; # N.B: if you change this, please update:
+  version = "1.62.3"; # N.B: if you change this, please update:
     # pythonPackages.grpcio-tools
     # pythonPackages.grpcio-status
 
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
     owner = "grpc";
     repo = "grpc";
     rev = "v${version}";
-    hash = "sha256-L0bn6Bg36UKIRxznH9o4T7WXUqMwFjr8ybeQfbUi8xM=";
+    hash = "sha256-ShcC90WSMFVjpuM1up+0JShqK3RGW2edxlp9wMt6dPg=";
     fetchSubmodules = true;
   };
 
