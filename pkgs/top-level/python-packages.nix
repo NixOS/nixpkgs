8430312facb9e7913c2a9ddfe92cf5b276ef7422
Robert Schütz <mail@dotlambda.de>
2024-08-31 20:59:14 -0700

    python312Packages.pyvicare-neo: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12942,6 +12942,8 @@ self: super: with self; {
 
   pyvicare = callPackage ../development/python-modules/pyvicare { };
 
+  pyvicare-neo = callPackage ../development/python-modules/pyvicare-neo { };
+
   pyvirtualdisplay = callPackage ../development/python-modules/pyvirtualdisplay { };
 
   pyvis = callPackage ../development/python-modules/pyvis { };
