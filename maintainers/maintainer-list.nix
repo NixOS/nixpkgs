cc2ad05f411f36e61071776ec2751a1f01baaa09
cheeseecake <chaneln95@gmail.com>
2024-09-01 20:25:47 +0800

    maintainers: add cheesecake

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3536,6 +3536,12 @@
     matrix = "@chayleaf:matrix.pavluk.org";
     name = "Anna Pavlyuk";
   };
+  cheeseecake = {
+    email = "chanelnjw@gmail.com";
+    github = "cheeseecake";
+    githubId = 34784816;
+    name = "Chanel";
+  };
   chekoopa = {
     email = "chekoopa@mail.ru";
     github = "chekoopa";
