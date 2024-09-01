5fb520389bcf859e9a73d30653acd4364c701ea7
eyjhb <eyjhbb@gmail.com>
2024-09-01 12:14:02 +0200

    mautrix-meta: fixed typo in description

--- pkgs/by-name/ma/mautrix-meta/package.nix
+++ pkgs/by-name/ma/mautrix-meta/package.nix
@@ -37,7 +37,7 @@ buildGoModule rec {
 
   meta = {
     homepage = "https://github.com/mautrix/meta";
-    description = "Matrix <-> Facebook and Mautrix <-> Instagram hybrid puppeting/relaybot bridge";
+    description = "Matrix <-> Facebook and Matrix <-> Instagram hybrid puppeting/relaybot bridge";
     license = lib.licenses.agpl3Plus;
     maintainers = with lib.maintainers; [ rutherther eyjhb ];
     mainProgram = "mautrix-meta";
