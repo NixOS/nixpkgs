d786b908176a2489a3c0446a4dd61ff81505d7fc
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-01 02:39:13 +0000

    systemfd: 0.4.2 -> 0.4.3

--- pkgs/development/tools/systemfd/default.nix
+++ pkgs/development/tools/systemfd/default.nix
@@ -4,7 +4,7 @@
 }:
 
 let
-  version = "0.4.2";
+  version = "0.4.3";
 
 in
 rustPlatform.buildRustPackage {
@@ -16,10 +16,10 @@ rustPlatform.buildRustPackage {
     repo = "systemfd";
     owner = "mitsuhiko";
     rev = version;
-    sha256 = "sha256-MASpQJkqmKpHZzMxHqAsuVO50dHHTv74Rnbv1gLapTU=";
+    sha256 = "sha256-Ypt9/dqDrurhiEhahVk8gG3QxP2ZKTeL7F0IVUGE8Kw=";
   };
 
-  cargoHash = "sha256-zgRbaZchdqzr+E6gqltSte9dGMnjbrM7/7t0BiNn4kA=";
+  cargoHash = "sha256-1t+yYqPDMEI39kieGkm+EUVzDBsTlDWQ7iGyjepjc7s=";
 
   meta = {
     description = "Convenient helper for passing sockets into another process";
