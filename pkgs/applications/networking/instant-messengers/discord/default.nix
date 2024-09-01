686d14280cf2ed4954cf73d878b4a18056325f60
jopejoe1 <johannes@joens.email>
2024-09-01 11:51:13 +0000

    discord-canary: 0.0.475 -> 0.0.479

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -4,7 +4,7 @@ let
     if stdenv.isLinux then {
       stable = "0.0.66";
       ptb = "0.0.101";
-      canary = "0.0.475";
+      canary = "0.0.479";
       development = "0.0.25";
     } else {
       stable = "0.0.316";
@@ -25,7 +25,7 @@ let
       };
       canary = fetchurl {
         url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-        hash = "sha256-suVIDCZhI6DwUVizBmTm104Clr6pH/olAIbOMNzFNss=";
+        hash = "sha256-mirioiRpwQB07sL1avGYk2+Cnmdw7PW808AAA6p2Y0U=";
       };
       development = fetchurl {
         url = "https://dl-development.discordapp.net/apps/linux/${version}/discord-development-${version}.tar.gz";
