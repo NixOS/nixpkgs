2b557be33648580fc54024f5498437839c4710ae
Sefa Eyeoglu <contact@scrumplex.net>
2024-09-01 16:21:35 +0200

    tomlplusplus: move to by-name
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23930,8 +23930,6 @@ with pkgs;
 
   toml11 = callPackage ../development/libraries/toml11 { };
 
-  tomlplusplus = callPackage ../development/libraries/tomlplusplus { };
-
   tokyocabinet = callPackage ../development/libraries/tokyo-cabinet { };
 
   tokyotyrant = callPackage ../development/libraries/tokyo-tyrant { };
