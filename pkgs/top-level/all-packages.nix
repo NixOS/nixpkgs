c3bda7ebcb79fabc637461755654b3451878c39c
Anthony ROUSSEL <anthony@roussel.dev>
2024-09-01 21:33:56 +0200

    liberasurecode: migrate to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19925,8 +19925,6 @@ with pkgs;
 
   libesmtp = callPackage ../development/libraries/libesmtp { };
 
-  liberasurecode = callPackage ../applications/misc/liberasurecode { };
-
   example-robot-data = callPackage ../development/libraries/example-robot-data { };
 
   exiv2 = callPackage ../development/libraries/exiv2 { };
