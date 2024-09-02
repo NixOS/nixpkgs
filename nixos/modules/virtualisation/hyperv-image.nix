3a505620ec4cf3cf153230a1ca0cc62f92935197
phaer <hello@phaer.org>
2024-09-02 13:46:16 +0200

    hyperv-image: use virtualisation.diskSize

--- nixos/modules/virtualisation/hyperv-image.nix
+++ nixos/modules/virtualisation/hyperv-image.nix
@@ -9,19 +9,29 @@ with lib;
 
 let
   cfg = config.hyperv;
+  virtualisationOptions = import ./virtualisation-options.nix;
 
 in
 {
+
+  imports = [
+    virtualisationOptions.diskSize
+    (lib.mkRenamedOptionModuleWith {
+      sinceRelease = 2411;
+      from = [
+        "virtualisation"
+        "hyperv"
+        "baseImageSize"
+      ];
+      to = [
+        "virtualisation"
+        "diskSize"
+      ];
+    })
+  ];
+
   options = {
     hyperv = {
-      baseImageSize = mkOption {
-        type = with types; either (enum [ "auto" ]) int;
-        default = "auto";
-        example = 2048;
-        description = ''
-          The size of the hyper-v base image in MiB.
-        '';
-      };
       vmDerivationName = mkOption {
         type = types.str;
         default = "nixos-hyperv-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}";
@@ -40,6 +50,8 @@ in
   };
 
   config = {
+    virtualisation.diskSize = lib.mkDefault (2 * 1024);
+
     system.build.hypervImage = import ../../lib/make-disk-image.nix {
       name = cfg.vmDerivationName;
       postVM = ''
@@ -47,7 +59,7 @@ in
         rm $diskImage
       '';
       format = "raw";
-      diskSize = cfg.baseImageSize;
+      inherit (config.virtualisation) diskSize;
       partitionTableType = "efi";
       inherit config lib pkgs;
     };
