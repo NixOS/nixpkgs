e6532e6bdc83cdb6c3ec46ea39fb59014535f9f8
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-02 20:41:49 +0200

    mim-solvers: init at 0.0.5
    Co-authored-by: Arne Keller <2012gdwu+github@posteo.de>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8013,6 +8013,11 @@ self: super: with self; {
 
   millheater = callPackage ../development/python-modules/millheater { };
 
+  mim-solvers = toPythonModule (pkgs.mim-solvers.override {
+    python3Packages = self;
+    pythonSupport = true;
+  });
+
   minari = callPackage ../development/python-modules/minari { };
 
   mindsdb-evaluator = callPackage ../development/python-modules/mindsdb-evaluator { };
