a8fd5e26011debfff88171dcd91579a7a5fbae0f
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-02 23:31:22 +0400

    pyradio: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16908,8 +16908,6 @@ with pkgs;
 
   pw-volume = callPackage ../tools/audio/pw-volume { };
 
-  pyradio = callPackage ../applications/audio/pyradio { };
-
   racket = callPackage ../development/interpreters/racket {
     inherit (darwin.apple_sdk.frameworks) CoreFoundation;
   };
