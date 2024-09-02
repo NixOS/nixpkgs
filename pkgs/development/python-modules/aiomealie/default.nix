22b942a8078f5b685493cdbcface56f86c35109c
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-02 08:10:31 +0200

    python312Packages.aiomealie: 0.8.1 -> 0.9.1
    Diff: https://github.com/joostlek/python-mealie/compare/refs/tags/v0.8.1...v0.9.1
    
    Changelog: https://github.com/joostlek/python-mealie/releases/tag/v0.9.1

--- pkgs/development/python-modules/aiomealie/default.nix
+++ pkgs/development/python-modules/aiomealie/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "aiomealie";
-  version = "0.8.1";
+  version = "0.9.1";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "joostlek";
     repo = "python-mealie";
     rev = "refs/tags/v${version}";
-    hash = "sha256-1n/AMXEoJJ/jftYzYHvo+jTSasNEqnFVAYqlipHFmGc=";
+    hash = "sha256-qQ9mOyXiDyiPn4NZLHnt21h1Cnt7iSY63tM1MyQunow=";
   };
 
   postPatch = ''
