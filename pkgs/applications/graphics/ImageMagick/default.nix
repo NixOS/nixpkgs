286d80604976392cddba6bf4221a141148d56b06
Robert Schütz <mail@dotlambda.de>
2024-09-02 10:07:46 -0700

    imagemagick: 7.1.1-37 -> 7.1.1-38
    Diff: https://github.com/ImageMagick/ImageMagick/compare/7.1.1-37...7.1.1-38
    
    Changelog: https://github.com/ImageMagick/Website/blob/main/ChangeLog.md

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -49,13 +49,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "imagemagick";
-  version = "7.1.1-37";
+  version = "7.1.1-38";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     rev = finalAttrs.version;
-    hash = "sha256-dlcyCJw/tytb6z5VaogblUlzBRYBtijUJbkX3vZdeEU=";
+    hash = "sha256-dyk9kCH1w76Jhy/yBhVFLthTKYaMgXLBn7QGWAFS0XU=";
   };
 
   outputs = [ "out" "dev" "doc" ]; # bin/ isn't really big
