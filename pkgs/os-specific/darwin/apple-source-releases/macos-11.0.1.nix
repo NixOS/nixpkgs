b3626c6f844cd386722b9a9d19265867f6ca3ea7
Randy Eckenrode <randy@largeandhighquality.com>
2024-09-02 19:27:03 -0400

    darwin.Libc: remove and replace with stubs

--- pkgs/os-specific/darwin/apple-source-releases/macos-11.0.1.nix
+++ pkgs/os-specific/darwin/apple-source-releases/macos-11.0.1.nix
@@ -5,7 +5,6 @@
 {
 Csu = applePackage' "Csu" "88" "macos-11.0.1" "1lzp9x8iv60c2h12q2s89nf49b5hvpqq4a9li44zr2fxszn8lqxh" {};
 ICU = callPackage ./ICU/package.nix { };
-Libc = applePackage' "Libc" "1439.40.11" "macos-11.0.1" "12k5sbz2k1pl839w2lk9iw414zzl50zdjzgq2x6bm20yjbfj69qm" {};
 Libinfo = applePackage' "Libinfo" "542.40.3" "macos-11.0.1" "18jvl7cdg64x6clhsfv5pbzxis2aldddpca5r81xqakrmi9mck80" {};
 Libnotify = applePackage' "Libnotify" "279.40.4" "macos-11.0.1" "1vr11s0c42ssjs29shy1m8rj008np7aswdzjpimsfzyav47jb6y7" {};
 Librpcsvc = applePackage' "Librpcsvc" "26" "macos-11.0.1" "0wf6srbw28664wa0dckldbhrl9ydg70fms06rj6i7mvlrz1ccxk0" {};
