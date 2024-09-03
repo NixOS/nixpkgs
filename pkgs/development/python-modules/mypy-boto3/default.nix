842f06e256898e282db1c2720244781f16e50e36
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-04 00:17:14 +0200

    python312Packages.mypy-boto3-m2: 1.34.0 -> 1.35.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -843,8 +843,8 @@ rec {
       "sha256-0Tz/X8RIuLvbDRXmZ+g0aEOcO7Qyg7ZKDLW1bN4yfJA=";
 
   mypy-boto3-m2 =
-    buildMypyBoto3Package "m2" "1.34.0"
-      "sha256-ZF3ZViV0pN64OEc8aHuCRR0lnVyHOiTRfqVQGCmMMKE=";
+    buildMypyBoto3Package "m2" "1.35.0"
+      "sha256-rn9xCU8qtkR/zRzi5MM9dNInJOa30VrYRj2hBLN9Zao=";
 
   mypy-boto3-machinelearning =
     buildMypyBoto3Package "machinelearning" "1.34.0"
