800c043c04f3b6da6d065164129bd506a9564709
Robert Scott <code@humanleg.org.uk>
2024-09-03 20:18:31 +0100

    python312Packages.webob: 1.8.7 -> 1.8.8
    addresses CVE-2024-42353
    
    switch to github source as pypi isn't up to date

--- pkgs/development/python-modules/webob/default.nix
+++ pkgs/development/python-modules/webob/default.nix
@@ -1,7 +1,7 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   setuptools,
   legacy-cgi,
   pytestCheckHook,
@@ -11,15 +11,16 @@
 
 buildPythonPackage rec {
   pname = "webob";
-  version = "1.8.7";
+  version = "1.8.8";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
-  src = fetchPypi {
-    pname = "WebOb";
-    inherit version;
-    hash = "sha256-tk71FBvlWc+t5EjwRPpFwiYDUe3Lao72t+AMfc7wwyM=";
+  src = fetchFromGitHub {
+    owner = "Pylons";
+    repo = "webob";
+    rev = "refs/tags/${version}";
+    hash = "sha256-QN0UMLzO0g8Oalnn5GlOulXUxtXOx89jeeEvJV53rVs=";
   };
 
   build-system = [ setuptools ];
