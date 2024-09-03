acca668affff8f07222856dc759d96cc98359eaf
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-04 00:15:52 +0200

    python312Packages.mypy-boto3-inspector2: 1.34.29 -> 1.35.4

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -639,8 +639,8 @@ rec {
       "sha256-4QXRWahJ0y9Svi/WRIiRFfo36tkKM25bXCTMrZjE41g=";
 
   mypy-boto3-inspector2 =
-    buildMypyBoto3Package "inspector2" "1.34.29"
-      "sha256-ZMdNVgKXQnEHyK4tV/XegvFX7xdk5A1AiSfpTKWCtcY=";
+    buildMypyBoto3Package "inspector2" "1.35.4"
+      "sha256-NslpiIBwxTvcEe/Lh8wM2PJE31JNmu4qyCZMbB+9noE=";
 
   mypy-boto3-internetmonitor =
     buildMypyBoto3Package "internetmonitor" "1.34.74"
