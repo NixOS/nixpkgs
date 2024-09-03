ac18d254fb6a15c3a628a5e04a4aaf57021c8d1b
Jan B. <git@keksgesicht.net>
2024-09-03 20:55:34 +0200

    tuxedo-keyboard: 3.2.14 -> 4.6.2

--- pkgs/os-specific/linux/tuxedo-keyboard/default.nix
+++ pkgs/os-specific/linux/tuxedo-keyboard/default.nix
@@ -1,14 +1,15 @@
-{ lib, stdenv, fetchFromGitHub, kernel, linuxHeaders, pahole }:
+{ lib, stdenv, fetchFromGitLab, kernel, linuxHeaders, pahole }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "tuxedo-keyboard-${kernel.version}";
-  version = "3.2.14";
+  version = "4.6.2";
 
-  src = fetchFromGitHub {
-    owner = "tuxedocomputers";
-    repo = "tuxedo-keyboard";
+  src = fetchFromGitLab {
+    group = "tuxedocomputers";
+    owner = "development/packages";
+    repo = "tuxedo-drivers";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-L3NsUUKA/snUcRWwlZidsBiTznhfulNldidEDDmNOkw=";
+    hash = "sha256-HS/KGhgFW0vO2SSFYSdseQBhAZC70eAx9JvRgR6e6qs=";
   };
 
   buildInputs = [
@@ -23,9 +24,8 @@ stdenv.mkDerivation (finalAttrs: {
 
     mkdir -p "$out/lib/modules/${kernel.modDirVersion}"
 
-    for module in clevo_acpi.ko clevo_wmi.ko tuxedo_keyboard.ko tuxedo_io/tuxedo_io.ko uniwill_wmi.ko; do
-        mv src/$module $out/lib/modules/${kernel.modDirVersion}
-    done
+    find src/ -type f -name '*.ko' \
+      -exec mv {} $out/lib/modules/${kernel.modDirVersion} \;
 
     runHook postInstall
   '';
@@ -33,7 +33,7 @@ stdenv.mkDerivation (finalAttrs: {
   meta = {
     broken = stdenv.hostPlatform.isAarch64 || (lib.versionOlder kernel.version "5.5");
     description = "Keyboard and hardware I/O driver for TUXEDO Computers laptops";
-    homepage = "https://github.com/tuxedocomputers/tuxedo-keyboard/";
+    homepage = "https://gitlab.com/tuxedocomputers/development/packages/tuxedo-drivers";
     license = lib.licenses.gpl3Plus;
     longDescription = ''
       This driver provides support for Fn keys, brightness/color/mode for most TUXEDO
