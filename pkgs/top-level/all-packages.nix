e416cfd21c8682b45769d5e3eb362b191f4d1a48
t4ccer <t4ccer@gmail.com>
2024-09-02 22:20:22 -0600

    measureme: move to by-name, format

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5629,8 +5629,6 @@ with pkgs;
 
   maxcso = callPackage ../tools/archivers/maxcso { };
 
-  measureme = callPackage ../development/tools/rust/measureme { };
-
   medusa = callPackage ../tools/security/medusa { };
 
   megasync = libsForQt5.callPackage ../applications/misc/megasync {
