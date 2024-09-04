57808a38ec26c734a62b81a1066b6891d62219a9
Alyssa Ross <hi@alyssa.is>
2024-09-04 15:00:48 +0200

    weston: 13.0.3 -> 14.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34530,7 +34530,6 @@ with pkgs;
 
   westonLite = weston.override {
     demoSupport = false;
-    hdrSupport = false;
     jpegSupport = false;
     lcmsSupport = false;
     pangoSupport = false;
