b30f23ae5488c738d467640803f59c0b5d117dd1
Yarek <yarekt@gmail.com>
2024-09-04 13:38:56 +0100

    cato: init at 5.2.1.1

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -988,6 +988,7 @@
   ./services/networking/bitlbee.nix
   ./services/networking/blockbook-frontend.nix
   ./services/networking/blocky.nix
+  ./services/networking/cato-client.nix
   ./services/networking/centrifugo.nix
   ./services/networking/cgit.nix
   ./services/networking/charybdis.nix
