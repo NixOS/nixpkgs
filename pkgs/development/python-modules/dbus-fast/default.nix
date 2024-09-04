7894402c49b464f598b192061f20654c387fe535
Robert Schütz <mail@dotlambda.de>
2024-09-04 12:26:32 -0700

    python312Packages.dbus-fast: run (almost) all tests

--- pkgs/development/python-modules/dbus-fast/default.nix
+++ pkgs/development/python-modules/dbus-fast/default.nix
@@ -3,11 +3,13 @@
   async-timeout,
   buildPythonPackage,
   cython,
+  dbus,
   fetchFromGitHub,
   poetry-core,
+  pytest,
   pytest-asyncio,
   pytest-cov-stub,
-  pytestCheckHook,
+  python,
   pythonOlder,
   setuptools,
 }:
@@ -39,9 +41,10 @@ buildPythonPackage rec {
   dependencies = [ async-timeout ];
 
   nativeCheckInputs = [
+    dbus
+    pytest
     pytest-asyncio
     pytest-cov-stub
-    pytestCheckHook
   ];
 
   pythonImportsCheck = [
@@ -51,47 +54,16 @@ buildPythonPackage rec {
     "dbus_fast.message"
   ];
 
-  disabledTests = [
-    # Test require a running Dbus instance
-    "test_aio_big_message"
-    "test_aio_properties"
-    "test_aio_proxy_object"
-    "test_bus_disconnect_before_reply"
-    "test_error_handling"
-    "test_export_alias"
-    "test_export_introspection"
-    "test_export_unexport"
-    "test_fast_disconnect"
-    "test_glib_big_message"
-    "test_high_level_service_fd_passing"
-    "test_interface_add_remove_signal"
-    "test_introspectable_interface"
-    "test_methods"
-    "test_multiple_flags_in_message"
-    "test_name_requests"
-    "test_object_manager"
-    "test_peer_interface"
-    "test_property_changed_signal"
-    "test_property_changed_signal"
-    "test_property_methods"
-    "test_sending_file_descriptor_low_level"
-    "test_sending_file_descriptor_with_proxy"
-    "test_sending_messages_between_buses"
-    "test_sending_signals_between_buses"
-    "test_signals"
-    "test_standard_interface_properties"
-    "test_standard_interfaces"
-    "test_tcp_connection_with_forwarding"
-    "test_unexpected_disconnect"
-    # NameError: name '_cast_uint32_native' is not defined
-    "test_unmarshall_bluez_interfaces_added_message"
-    "test_unmarshall_bluez_interfaces_removed_message"
-    "test_unmarshall_bluez_message"
-    "test_unmarshall_bluez_properties_changed_with_service_data"
-    "test_unmarshall_can_resume"
-    "test_unmarshalling_with_table"
-    "test_ay_buffer"
-  ];
+  checkPhase = ''
+    runHook preCheck
+
+    # test_peer_interface times out
+    dbus-run-session \
+      --config-file=${dbus}/share/dbus-1/session.conf \
+      ${python.interpreter} -m pytest -k "not test_peer_interface"
+
+    runHook postCheck
+  '';
 
   meta = with lib; {
     description = "Faster version of dbus-next";
