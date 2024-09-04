c8204eda767df9eba559e64036b46fb436f7a17c
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-09-04 10:47:34 -0400

    pkgs: fix eval

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14793,7 +14793,7 @@ with pkgs;
     ocamlPackages = ocaml-ng.ocamlPackages_4_14;
   };
 
-  inherit (coqPackages) compcert;
+  inherit (coqPackages_8_19) compcert;
 
   computecpp-unwrapped = callPackage ../development/compilers/computecpp { };
   computecpp = wrapCCWith rec {
