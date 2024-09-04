cbe5bb299ba2bfd7388df1304fa0d4a211e58eb5
Robert Schütz <mail@dotlambda.de>
2024-09-04 11:37:49 -0700

    python312Packages.lmcloud: fix repo and version

--- pkgs/development/python-modules/lmcloud/default.nix
+++ pkgs/development/python-modules/lmcloud/default.nix
@@ -12,15 +12,15 @@
 
 buildPythonPackage rec {
   pname = "lmcloud";
-  version = ".1.2.2";
+  version = "1.2.2";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "zweckj";
-    repo = "lmcloud";
-    rev = "refs/tags/v${version}";
+    repo = "pylamarzocco";
+    rev = "refs/tags/v.${version}";
     hash = "sha256-uYd52T9dAvFP6in1fHiCXFVUdbRXDE7crjfelbasW4Q=";
   };
 
@@ -40,8 +40,8 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "Library to interface with La Marzocco's cloud";
-    homepage = "https://github.com/zweckj/lmcloud";
-    changelog = "https://github.com/zweckj/lmcloud/releases/tag/v${version}";
+    homepage = "https://github.com/zweckj/pylamarzocco";
+    changelog = "https://github.com/zweckj/pylamarzocco/releases/tag/v.${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
