NixOS/nixpkgs

commit 0b68b152e270d45df844edbc3b1e2d99f0cd19de

go: set meta attribute for bootstrap binary derivation (#276857)
* go: set sourceProvenance for bootstrap binary

* Update pkgs/development/compilers/go/binary.nix

---------

Co-authored-by: zowoq <59103226+zowoq@users.noreply.github.com>
Co-authored-by: Aleksana <me@aleksana.moe>

pkgs/development/compilers/go/binary.nix
@@ -18,7 +18,7 @@ let
 
   platform = toGoPlatform stdenv.hostPlatform;
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   name = "go-${version}-${platform}-bootstrap";
 
   src = fetchurl {
@@ -36,4 +36,14 @@ stdenv.mkDerivation rec {
     ln -s $out/share/go/bin/go $out/bin/go
     runHook postInstall
   '';
+
+  meta = with lib; {
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    changelog = "https://go.dev/doc/devel/release#go${lib.versions.majorMinor version}";
+    description = "The Go Programming language";
+    homepage = "https://go.dev/";
+    license = licenses.bsd3;
+    maintainers = teams.golang.members;
+    platforms = platforms.darwin ++ platforms.linux;
+  };
 }

commit cc15cb63e6ea888642b66d61262574c433be166a

go: fix eval for binary derivation
This was broken in https://github.com/NixOS/nixpkgs/pull/276857 commit
69aa113e45f448d12a2648986ab3694a7ad2b84f

pkgs/development/compilers/go/binary.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, version, hashes }:
+{ lib, stdenv, fetchurl, version, hashes }:
 let
   toGoKernel = platform:
     if platform.isDarwin then "darwin"
@@ -37,13 +37,13 @@ stdenv.mkDerivation {
     runHook postInstall
   '';
 
-  meta = with lib; {
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+  meta = {
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     changelog = "https://go.dev/doc/devel/release#go${lib.versions.majorMinor version}";
     description = "The Go Programming language";
     homepage = "https://go.dev/";
-    license = licenses.bsd3;
-    maintainers = teams.golang.members;
-    platforms = platforms.darwin ++ platforms.linux;
+    license = lib.licenses.bsd3;
+    maintainers = lib.teams.golang.members;
+    platforms = lib.platforms.darwin ++ lib.platforms.linux;
   };
 }