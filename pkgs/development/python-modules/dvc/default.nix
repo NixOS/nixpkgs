ad595e7b96f9cbe0526b176154f07483c8d5cd97
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-04 11:04:57 +0000

    dvc-with-remotes: 3.55.1 -> 3.55.2

--- pkgs/development/python-modules/dvc/default.nix
+++ pkgs/development/python-modules/dvc/default.nix
@@ -57,7 +57,7 @@
 
 buildPythonPackage rec {
   pname = "dvc";
-  version = "3.55.1";
+  version = "3.55.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -66,7 +66,7 @@ buildPythonPackage rec {
     owner = "iterative";
     repo = "dvc";
     rev = "refs/tags/${version}";
-    hash = "sha256-DrhjmVHCX1lXJUrklVgO/eguL1uRQ33kJjSgCAEIyew=";
+    hash = "sha256-yNnOSYh4lCefTnIgNstsKaRbrPCgSiWEgKeF66KD66k=";
   };
 
   pythonRelaxDeps = [
