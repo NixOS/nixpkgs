186aa12f703233b2908c23d4e05235ff8ea6f0a0
Robert Schütz <mail@dotlambda.de>
2024-09-05 15:17:21 -0700

    python312Packages.moderngl-window: support NumPy 2

--- pkgs/development/python-modules/moderngl-window/default.nix
+++ pkgs/development/python-modules/moderngl-window/default.nix
@@ -34,13 +34,15 @@ buildPythonPackage rec {
     hash = "sha256-zTygSXU/vQZaFCuHbRBpO9/BYYA2UOid+wvhyc2bWMI=";
   };
 
-  pythonRelaxDeps = [ "pillow" ];
+  pythonRelaxDeps = [
+    "numpy" # https://github.com/moderngl/moderngl-window/issues/193
+  ];
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     numpy
     moderngl
     pyglet
@@ -48,7 +50,7 @@ buildPythonPackage rec {
     pyrr
   ];
 
-  passthru.optional-dependencies = {
+  optional-dependencies = {
     trimesh = [
       trimesh
       scipy
