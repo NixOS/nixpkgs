c1beba12cb9c09df4113447d043b301dc3551d39
RatCornu <balthazar.patiachvili@crans.org>
2024-09-05 11:33:25 +0200

    pingvin-share: 0.29.0 -> 1.1.1

--- pkgs/servers/pingvin-share/default.nix
+++ pkgs/servers/pingvin-share/default.nix
@@ -5,12 +5,12 @@
 }:
 
 let
-  version = "0.29.0";
+  version = "1.1.1";
   src = fetchFromGitHub {
     owner = "stonith404";
     repo = "pingvin-share";
     rev = "v${version}";
-    hash = "sha256-ETsIGb6IxGruApUP05cuMtTDNAE23CI1Q2MmjxX3aPo=";
+    hash = "sha256-nBNz4xEMEnOJGxVIbpukGzlAEgG+uGntvTJoc+GHmIw=";
   };
 in
 
