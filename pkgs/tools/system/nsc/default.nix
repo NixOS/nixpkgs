c585a85e49e44f5d5d1dcb48752cc41916a36804
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-05 01:59:13 +0000

    nsc: 2.8.7 -> 2.8.8

--- pkgs/tools/system/nsc/default.nix
+++ pkgs/tools/system/nsc/default.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "nsc";
-  version = "2.8.7";
+  version = "2.8.8";
 
   src = fetchFromGitHub {
     owner = "nats-io";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-uJR4AdXGSL3vKUABpBBteND7EUocKz+mLRqt5XPdREk=";
+    hash = "sha256-ZaizxiNGiyV3Z18U4W2LcqZXDLfUB7NhuURNVbx6M4s=";
   };
 
   ldflags = [
@@ -27,24 +27,26 @@ buildGoModule rec {
 
   nativeBuildInputs = [ installShellFiles ];
 
-  postInstall = ''
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
     installShellCompletion --cmd nsc \
       --bash <($out/bin/nsc completion bash) \
       --fish <($out/bin/nsc completion fish) \
       --zsh <($out/bin/nsc completion zsh)
   '';
 
-  preCheck = ''
-    # Tests attempt to write to the home directory.
+  preInstall = ''
+    # asc attempt to write to the home directory.
     export HOME=$(mktemp -d)
   '';
+
+  preCheck = preInstall;
 
   # Tests currently fail on darwin because of a test in nsc which
   # expects command output to contain a specific path. However
   # the test strips table formatting from the command output in a naive way
   # that removes all the table characters, including '-'.
   # The nix build directory looks something like:
-  # /private/tmp/nix-build-nsc-2.8.7.drv-0/nsc_test2000598938/keys
+  # /private/tmp/nix-build-nsc-2.8.8.drv-0/nsc_test2000598938/keys
   # Then the `-` are removed from the path unintentionally and the test fails.
   # This should be fixed upstream to avoid mangling the path when
   # removing the table decorations from the command output.
