59c844b41771d32c0e341ccfc74c397f240d9329
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-05 11:44:20 +0000

    patroni: 4.0.0 -> 4.0.1

--- pkgs/servers/sql/patroni/default.nix
+++ pkgs/servers/sql/patroni/default.nix
@@ -6,13 +6,13 @@
 
 pythonPackages.buildPythonApplication rec {
   pname = "patroni";
-  version = "4.0.0";
+  version = "4.0.1";
 
   src = fetchFromGitHub {
     owner = "zalando";
     repo = pname;
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-C0YjLak/m8ZMCzR5x22zjCATvzs72l56GRccN0Gur+4=";
+    sha256 = "sha256-0Eqk9X/qO9MkETaVIOCS6vyvywAK175/CDCNHH+YatQ=";
   };
 
   propagatedBuildInputs = with pythonPackages; [
