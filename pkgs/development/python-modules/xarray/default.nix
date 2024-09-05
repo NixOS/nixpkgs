0aa5a025908680f2e25071afef74d6bf03dcc3ab
Robert Schütz <mail@dotlambda.de>
2024-09-05 15:21:37 -0700

    python312Packages.xarray: 2024.6.0 -> 2024.07.0
    Changelog: https://github.com/pydata/xarray/blob/v2024.07.0/doc/whats-new.rst

--- pkgs/development/python-modules/xarray/default.nix
+++ pkgs/development/python-modules/xarray/default.nix
@@ -1,8 +1,7 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
-  flaky,
+  fetchFromGitHub,
   numpy,
   packaging,
   pandas,
@@ -14,40 +13,37 @@
 
 buildPythonPackage rec {
   pname = "xarray";
-  version = "2024.6.0";
+  version = "2024.07.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.9";
+  disabled = pythonOlder "3.10";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-C5HgvE3AKWlHlHZA/jHsboZ84ljS98vBC+30ptaDQMc=";
+  src = fetchFromGitHub {
+    owner = "pydata";
+    repo = "xarray";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-pt0qnkgf3E/QQHQAaZLommakhqEJ4NuTyjx5tdk2N1U=";
   };
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
     setuptools-scm
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     numpy
     packaging
     pandas
   ];
 
   nativeCheckInputs = [
-    flaky
     pytestCheckHook
-  ];
-
-  pytestFlagsArray = [
-    # ModuleNotFoundError: No module named 'xarray.datatree_'
-    "--ignore xarray/tests/datatree"
   ];
 
   pythonImportsCheck = [ "xarray" ];
 
   meta = with lib; {
+    changelog = "https://github.com/pydata/xarray/blob/${src.rev}/doc/whats-new.rst";
     description = "N-D labeled arrays and datasets in Python";
     homepage = "https://github.com/pydata/xarray";
     license = licenses.asl20;
