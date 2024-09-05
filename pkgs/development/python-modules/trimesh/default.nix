59dbee2d85c227f980ccc4d3fd990bfa545c9c85
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-09-05 09:27:53 +0200

    python312Packages.trimesh: 4.4.8 -> 4.4.9
    Changelog: https://github.com/mikedh/trimesh/releases/tag/4.4.9

--- pkgs/development/python-modules/trimesh/default.nix
+++ pkgs/development/python-modules/trimesh/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "trimesh";
-  version = "4.4.8";
+  version = "4.4.9";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-6MHTYDLH3pozGpAUfTsZtpTd28RMgAL0ESlAGjnsH84=";
+    hash = "sha256-6fVMtO9w+dtJRGytOEW3qAQ/x9YtkZKyQXQfP7DYE6w=";
   };
 
   build-system = [ setuptools ];
