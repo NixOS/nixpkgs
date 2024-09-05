27c4af822bfcf7f82b6ea9387a1ba37c9d7f205f
Sebastian Sellmeier <mail@sebastian-sellmeier.de>
2024-09-05 19:39:13 +0200

    protonvpn-gui: 4.3.0 -> 4.4.4, add sebtm as maintainer

--- pkgs/applications/networking/protonvpn-gui/default.nix
+++ pkgs/applications/networking/protonvpn-gui/default.nix
@@ -1,47 +1,46 @@
-{ lib
-, buildPythonApplication
-, fetchFromGitHub
-, gobject-introspection
-, setuptools
-, wrapGAppsHook3
-, dbus-python
-, packaging
-, proton-core
-, proton-keyring-linux
-, proton-keyring-linux-secretservice
-, proton-vpn-api-core
-, proton-vpn-connection
-, proton-vpn-killswitch
-, proton-vpn-killswitch-network-manager
-, proton-vpn-logger
-, proton-vpn-network-manager
-, proton-vpn-network-manager-openvpn
-, proton-vpn-session
-, pycairo
-, pygobject3
-, pytest-cov-stub
-, pytestCheckHook
-, withIndicator ? true
-, libappindicator-gtk3
-, libayatana-appindicator
+{
+  lib,
+  buildPythonApplication,
+  fetchFromGitHub,
+  gobject-introspection,
+  setuptools,
+  wrapGAppsHook3,
+  dbus-python,
+  packaging,
+  proton-core,
+  proton-keyring-linux,
+  proton-keyring-linux-secretservice,
+  proton-vpn-api-core,
+  proton-vpn-connection,
+  proton-vpn-killswitch,
+  proton-vpn-killswitch-network-manager,
+  proton-vpn-logger,
+  proton-vpn-network-manager,
+  proton-vpn-network-manager-openvpn,
+  proton-vpn-network-manager-wireguard,
+  proton-vpn-session,
+  pycairo,
+  pygobject3,
+  withIndicator ? true,
+  libappindicator-gtk3,
+  libayatana-appindicator,
 }:
 
 buildPythonApplication rec {
   pname = "protonvpn-gui";
-  version = "4.3.0";
+  version = "4.4.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ProtonVPN";
     repo = "proton-vpn-gtk-app";
     rev = "refs/tags/v${version}";
-    hash = "sha256-H4m4u9zksab47W5aIsQZPQTPEYiXbmrVCnT67b+A5Tc=";
+    hash = "sha256-e581FgXrk1cfjsl/UaG9M+3VBYXcV0mggeLeEW9s9KM=";
   };
 
   nativeBuildInputs = [
     # Needed for the NM namespace
     gobject-introspection
-    setuptools
     wrapGAppsHook3
   ];
 
@@ -50,6 +49,10 @@ buildPythonApplication rec {
     libappindicator-gtk3
     # Adds AyatanaAppIndicator3 namespace
     libayatana-appindicator
+  ];
+
+  build-system = [
+    setuptools
   ];
 
   propagatedBuildInputs = [
@@ -65,6 +68,7 @@ buildPythonApplication rec {
     proton-vpn-logger
     proton-vpn-network-manager
     proton-vpn-network-manager-openvpn
+    proton-vpn-network-manager-wireguard
     proton-vpn-session
     pycairo
     pygobject3
@@ -76,25 +80,15 @@ buildPythonApplication rec {
     install -Dm 644 ${src}/rpmbuild/SOURCES/proton-vpn-logo.svg $out/share/pixmaps
   '';
 
-  nativeCheckInputs = [
-    pytest-cov-stub
-    pytestCheckHook
-  ];
-
-  preCheck = ''
-    # Needed for Permission denied: '/homeless-shelter'
-    export HOME=$(mktemp -d)
-  '';
-
   # Gets a segmentation fault after the widgets test
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     description = "Proton VPN GTK app for Linux";
     homepage = "https://github.com/ProtonVPN/proton-vpn-gtk-app";
-    license = licenses.gpl3Plus;
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
     mainProgram = "protonvpn-app";
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ sebtm ];
   };
 }
