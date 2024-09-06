7e208d2ce1ee5721c64b19e2aa26ea28b1a27185
aktaboot <akhtaboot@protonmail.com>
2024-09-06 14:11:28 +0200

    watchmate: 0.4.5 -> 0.5.2-unstable-2024-08-13

--- pkgs/applications/misc/watchmate/default.nix
+++ pkgs/applications/misc/watchmate/default.nix
@@ -10,22 +10,24 @@
 , wrapGAppsHook4
 , glib
 }:
-
+let
+  releaseVersion = "0.5.2";
+in
 rustPlatform.buildRustPackage rec {
   pname = "watchmate";
-  version = "0.4.5";
+  version = "${releaseVersion}-unstable-2024-08-13";
 
   src = fetchFromGitHub {
     owner = "azymohliad";
     repo = "watchmate";
-    rev = "v${version}";
-    hash = "sha256-4L9mfZWphwXON8VgRcGrz+k62wyPzd1phtRu9HQnVdE=";
+    rev = "e05edfae94a1973110c6f40f25133d5979f485ab";
+    hash = "sha256-fHWxn7hFx/9cnLlCHIC6hIJaLd1U3Ii9mJgTJ+Hw63M=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "mpris2-zbus-0.1.0" = "sha256-f2hth7TnA14I4UPyp0u4IfMi9WY4G3M1sEc4xNtnbr0=";
+      "mpris2-zbus-0.1.0" = "sha256-a/cvbB0M9cUd8RP5XxgHRbJ/i/UKAEK4DTwwUU69IuY=";
     };
   };
 
@@ -34,6 +36,7 @@ rustPlatform.buildRustPackage rec {
     wrapGAppsHook4
     glib
   ];
+
   buildInputs = [
     gtk4
     libadwaita
@@ -45,6 +48,8 @@ rustPlatform.buildRustPackage rec {
   postInstall = ''
     install -Dm444 assets/io.gitlab.azymohliad.WatchMate.desktop -t $out/share/applications/
     install -Dm444 assets/io.gitlab.azymohliad.WatchMate.metainfo.xml -t $out/share/metainfo/
+    install -Dm444 assets/io.gitlab.azymohliad.WatchMate.gschema.xml -t $out/share/glib-2.0/schemas/
+    glib-compile-schemas $out/share/glib-2.0/schemas/
     install -Dm444 assets/icons/io.gitlab.azymohliad.WatchMate.svg -t $out/share/icons/hicolor/scalable/apps/
     install -Dm444 assets/icons/io.gitlab.azymohliad.WatchMate-symbolic.svg -t $out/share/icons/hicolor/scalable/apps/
   '';
@@ -53,7 +58,7 @@ rustPlatform.buildRustPackage rec {
     description = "PineTime smart watch companion app for Linux phone and desktop";
     mainProgram = "watchmate";
     homepage = "https://github.com/azymohliad/watchmate";
-    changelog = "https://github.com/azymohliad/watchmate/raw/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/azymohliad/watchmate/raw/v${releaseVersion}/CHANGELOG.md";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ chuangzhu ];
     platforms = platforms.linux;
