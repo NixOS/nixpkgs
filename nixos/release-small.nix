NixOS/nixpkgs

commit 862911fd227110e16739fe5cebe710cb29d28942

nixos/release-small: remove minimal installer ISOs
These take up 2 GiB every time anything in the minimal installer
changes, or up to 4 GiB per day. We already stopped building Amazon
images in 9426d90c6784aa19f11a8cfc5171a46f0b21cd94. Meaningful
installer changes are rare enough, and the couple of days it takes
for them to trickle down to the large channel acceptable enough,
that this is mostly a waste of space.

This should buy enough slack to build `stdenv` on `staging` without
contributing to cache size growth.

nixos/release-small.nix
@@ -53,12 +53,8 @@ in rec {
         inherit (nixos'.tests.installer)
           lvm
           separateBoot
-          simple;
-      };
-      boot = {
-        inherit (nixos'.tests.boot)
-          biosCdrom
-          uefiCdrom;
+          simple
+          simpleUefiSystemdBoot;
       };
     };
   };
@@ -107,18 +103,16 @@ in rec {
         "nixpkgs.tarball"
         "nixpkgs.release-checks"
       ]
+      (onSystems [ "aarch64-linux" ] "nixos.tests.installer.simpleUefiSystemdBoot")
       (map (onSystems [ "x86_64-linux" ]) [
-        "nixos.tests.boot.biosCdrom"
         "nixos.tests.installer.lvm"
         "nixos.tests.installer.separateBoot"
         "nixos.tests.installer.simple"
       ])
       (map onSupported [
         "nixos.dummy"
-        "nixos.iso_minimal"
         "nixos.manual"
         "nixos.tests.acme"
-        "nixos.tests.boot.uefiCdrom"
         "nixos.tests.containers-imperative"
         "nixos.tests.containers-ip"
         "nixos.tests.firewall"

nixos/release-combined.nix
@@ -86,6 +86,7 @@ in rec {
         # Upstream issue: https://gitlab.com/qemu-project/qemu/-/issues/2321
         # (onSystems ["x86_64-linux"] "nixos.tests.hibernate")
         (onFullSupported "nixos.tests.i3wm")
+        (onSystems ["aarch64-linux"] "nixos.tests.installer.simpleUefiSystemdBoot")
         (onSystems ["x86_64-linux"] "nixos.tests.installer.btrfsSimple")
         (onSystems ["x86_64-linux"] "nixos.tests.installer.btrfsSubvolDefault")
         (onSystems ["x86_64-linux"] "nixos.tests.installer.btrfsSubvolEscape")

commit 616b34597ae2ebc2bace5b72d59a37015cdaa8f0

nixos/release-small: actually remove minimal installer ISOs
Fixes: 862911fd227110e16739fe5cebe710cb29d28942

nixos/release-small.nix
@@ -32,7 +32,7 @@ let
 in rec {
 
   nixos = {
-    inherit (nixos') channel manual options iso_minimal dummy;
+    inherit (nixos') channel manual options dummy;
     tests = {
       inherit (nixos'.tests)
         acme