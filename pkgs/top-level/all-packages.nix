aec399ee4b4ea7ec1259e37b4c49be82165e0c88
Kristoffer Dalby <kristoffer@tailscale.com>
2024-09-06 13:32:52 +0200

    headscale: 0.22.3 -> 0.23.0
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8498,7 +8498,9 @@ with pkgs;
 
   heimdall-gui = heimdall.override { enableGUI = true; };
 
-  headscale = callPackage ../servers/headscale { };
+  headscale = callPackage ../servers/headscale {
+    buildGoModule = buildGo123Module;
+  };
 
   health = callPackage ../applications/misc/health { };
 
