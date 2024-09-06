7723e827091fdbe5aa4a440bfb9cc59f68bdf556
Elian Doran <contact@eliandoran.me>
2024-09-06 16:41:05 +0300

    mkdocs-print-site-plugin: init at 2.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7968,6 +7968,8 @@ self: super: with self; {
 
   mkdocs-minify-plugin = callPackage ../development/python-modules/mkdocs-minify-plugin { };
 
+  mkdocs-print-site-plugin = callPackage ../development/python-modules/mkdocs-print-site-plugin { };
+
   mkdocs-redirects = callPackage ../development/python-modules/mkdocs-redirects { };
 
   mkdocs-redoc-tag = callPackage ../development/python-modules/mkdocs-redoc-tag { };
