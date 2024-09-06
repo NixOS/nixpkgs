9817e538c262d8434271d9ec935140daf1d0ad79
Valter Nazianzeno <manipuladordedados@gmail.com>
2024-09-06 21:44:30 +0000

    emiluaPlugins.qt5: init at 1.0.1

---
+++ pkgs/development/emilua-plugins/qt5/default.nix
@@ -0,0 +1,36 @@
+{ lib, stdenv, meson, ninja, fetchFromGitLab, gperf, gawk, pkg-config, boost
+, luajit_openresty, asciidoctor, qt5, emilua, liburing, fmt }:
+
+stdenv.mkDerivation rec {
+  pname = "emilua-qt5";
+  version = "1.0.1";
+
+  src = fetchFromGitLab {
+    owner = "emilua";
+    repo = "qt5";
+    rev = "v${version}";
+    hash = "sha256-FkBfzGzUX7dvHjWRBjVwppU4jZBbY02gP+fIta8mjIw=";
+  };
+
+  buildInputs =
+    [ luajit_openresty boost qt5.qtdeclarative emilua liburing fmt qt5.qtbase ];
+
+  nativeBuildInputs = [
+    gperf
+    gawk
+    pkg-config
+    asciidoctor
+    meson
+    ninja
+    qt5.wrapQtAppsHook
+    qt5.qttools
+  ];
+
+  meta = with lib; {
+    description = "Qt5 bindings for Emilua";
+    homepage = "https://emilua.org/";
+    license = licenses.boost;
+    maintainers = with maintainers; [ manipuladordedados ];
+    platforms = platforms.linux;
+  };
+}
--- pkgs/top-level/emilua-plugins.nix
+++ pkgs/top-level/emilua-plugins.nix
@@ -9,4 +9,5 @@ emilua:
 (lib.makeScope newScope (self: {
   inherit emilua;
   beast = self.callPackage ../development/emilua-plugins/beast { };
+  qt5 = self.callPackage ../development/emilua-plugins/qt5 { };
 }))
