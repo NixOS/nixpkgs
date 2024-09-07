1f15da3c69db0b60c74e85e92c2d5157319a682b
Emily <vcs@emily.moe>
2024-09-07 22:18:24 +0100

    xine-ui: 0.99.14 -> 0.99.14-unstable-2024-08-26

--- pkgs/by-name/xi/xine-ui/package.nix
+++ pkgs/by-name/xi/xine-ui/package.nix
@@ -1,7 +1,8 @@
 { lib
 , stdenv
-, fetchurl
+, fetchhg
 , autoreconfHook
+, perl
 , curl
 , libXext
 , libXft
@@ -23,11 +24,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xine-ui";
-  version = "0.99.14";
+  version = "0.99.14-unstable-2024-08-26";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/xine/xine-ui-${finalAttrs.version}.tar.xz";
-    hash = "sha256-1NSQ1c7OcOK7mEnJ5ILyz4evAwLUUbYUR2/cw2Qs2cM=";
+  src = fetchhg {
+    url = "http://hg.code.sf.net/p/xine/xine-ui";
+    rev = "2beaad6bb92e6732585f68af2e346a24e5ad53a5";
+    hash = "sha256-Y08JX9q4w6pSJRCa5mWN11BnA6mZJSO/yn3X8YyZ6E4=";
   };
 
   outputs = [ "out" "dev" "lib" "man" ];
@@ -36,6 +38,7 @@ stdenv.mkDerivation (finalAttrs: {
     autoreconfHook
     pkg-config
     shared-mime-info
+    perl
   ];
 
   buildInputs = [
