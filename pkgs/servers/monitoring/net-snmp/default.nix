NixOS/nixpkgs

commit 5e6e3520df70cf2be576d4fb133cca7db9d9d5ba

net-snmp: darwin support
The autotools files shipped in the source tarball did not want to link, so I added autoreconfHook to just remake them.

pkgs/servers/monitoring/net-snmp/default.nix
@@ -1,6 +1,8 @@
 { lib, stdenv, fetchurl
 , file, openssl, perl, nettools
-, withPerlTools ? false }: let
+, autoreconfHook
+, withPerlTools ? false
+, darwin }: let
 
   perlWithPkgs = perl.withPackages (ps: with ps; [
     JSON
@@ -52,9 +54,15 @@ in stdenv.mkDerivation rec {
       -e "/NETSNMP_CONFIGURE_OPTIONS/ s|$NIX_STORE/[a-z0-9]\{32\}-|$NIX_STORE/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-|g"
   '';
 
-  nativeBuildInputs = [ nettools file ];
+  nativeBuildInputs = [ nettools file autoreconfHook ];
   buildInputs = [ openssl ]
-    ++ lib.optional withPerlTools perlWithPkgs;
+    ++ lib.optional withPerlTools perlWithPkgs
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.ApplicationServices
+      darwin.apple_sdk.frameworks.CoreServices
+      darwin.apple_sdk.frameworks.IOKit
+      darwin.apple_sdk.frameworks.DiskArbitration
+    ];
 
   enableParallelBuilding = true;
   # Missing dependencies during relinking:
@@ -74,6 +82,6 @@ in stdenv.mkDerivation rec {
     description = "Clients and server for the SNMP network monitoring protocol";
     homepage = "http://www.net-snmp.org/";
     license = licenses.bsd3;
-    platforms = platforms.linux;
+    platforms = platforms.unix;
   };
 }

commit b151851760ef3a34de298c33301342ef55908e0c

net-snmp: fix building for musl
Since autoreconfHook was added, fix-fd_mask.patch doesn't do anything,
because it patches the configure script, which is now overwritten.

Replace the Alpine patch with the upstream equivalent, which patches
the m4 source of configure as well.

Fixes: 5e6e3520df70 ("net-snmp: darwin support")

pkgs/servers/monitoring/net-snmp/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl
+{ lib, stdenv, fetchurl, fetchpatch
 , file, openssl, perl, nettools
 , autoreconfHook
 , withPerlTools ? false
@@ -27,7 +27,11 @@ in stdenv.mkDerivation rec {
   in [
     (fetchAlpinePatch "fix-includes.patch" "0zpkbb6k366qpq4dax5wknwprhwnhighcp402mlm7950d39zfa3m")
     (fetchAlpinePatch "netsnmp-swinst-crash.patch" "0gh164wy6zfiwiszh58fsvr25k0ns14r3099664qykgpmickkqid")
-    (fetchAlpinePatch "fix-fd_mask.patch" "/i9ve61HjDzqZt+u1wajNtSQoizl+KePvhcAt24HKd0=")
+    (fetchpatch {
+      name = "configure-musl.patch";
+      url = "https://github.com/net-snmp/net-snmp/commit/a62169f1fa358be8f330ea8519ade0610fac525b.patch";
+      hash = "sha256-+vWH095fFL3wE6XLsTaPXgMDya0LRWdlL6urD5AIBUs=";
+    })
   ];
 
   outputs = [ "bin" "out" "dev" "lib" ];