764f7c1011f1fc6a26b1831bc7b7612562cd53a5
Sefa Eyeoglu <contact@scrumplex.net>
2024-09-07 09:24:01 +0200

    envision-unwrapped: 0-unstable-2024-07-03 -> 0-unstable-2024-09-06
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/by-name/en/envision-unwrapped/package.nix
+++ pkgs/by-name/en/envision-unwrapped/package.nix
@@ -6,6 +6,7 @@
   fetchFromGitLab,
   gdb,
   gdk-pixbuf,
+  git,
   glib,
   gtk4,
   gtksourceview5,
@@ -17,6 +18,7 @@
   ninja,
   nix-update-script,
   openssl,
+  openxr-loader,
   pango,
   pkg-config,
   rustPlatform,
@@ -29,13 +31,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "envision-unwrapped";
-  version = "0-unstable-2024-07-03";
+  version = "0-unstable-2024-09-06";
 
   src = fetchFromGitLab {
     owner = "gabmus";
     repo = "envision";
-    rev = "6cf5e40b96d1cbd99a3cfcef1f03899356e79448";
-    hash = "sha256-a/IUNGoq9OKEC3uCg6PUp2TRHkfm4mTT3QQ8SfA29RU=";
+    rev = "849f47a8533bc3fc673afbdd9b32acac3ff26f7d";
+    hash = "sha256-t1+4MXD1s4NW38r3Ht+1OmCAY44MqEPijXdUVKy0rY4=";
   };
 
   strictDeps = true;
@@ -43,7 +45,8 @@ stdenv.mkDerivation (finalAttrs: {
   cargoDeps = rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "libmonado-rs-0.1.0" = "sha256-PsNgfpgso3HhIMXKky/u6Xw8phk1isRpNXKLhvN1wIE=";
+      "libmonado-rs-0.1.0" = "sha256-xztevBUaYBm5G3A0ZTb+3GV3g1IAU3SzfSS5BBqfp1Y=";
+      "openxr-0.18.0" = "sha256-ktkbhmExstkNJDYM/HYOwAwv3acex7P9SP0KMAOKhQk=";
     };
   };
 
@@ -51,6 +54,7 @@ stdenv.mkDerivation (finalAttrs: {
     appstream-glib
     desktop-file-utils
     cargo
+    git
     meson
     ninja
     pkg-config
@@ -69,6 +73,7 @@ stdenv.mkDerivation (finalAttrs: {
     libgit2
     libusb1
     openssl
+    openxr-loader
     pango
     vte-gtk4
     zlib
