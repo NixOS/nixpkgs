aa5fa5446c533bd468e7f6bc69a44e7535c5d135
GetPsyched <priyanshu@getpsyched.dev>
2024-09-07 14:05:03 +0530

    hoppscotch: add service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1515,6 +1515,7 @@
   ./services/web-servers/fcgiwrap.nix
   ./services/web-servers/garage.nix
   ./services/web-servers/hitch/default.nix
+  ./services/web-servers/hoppscotch/default.nix
   ./services/web-servers/jboss/default.nix
   ./services/web-servers/keter
   ./services/web-servers/lighttpd/cgit.nix
