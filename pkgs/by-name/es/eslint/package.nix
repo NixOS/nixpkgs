964e93cbf5bb9e463f32c52878b3cf1baf1f2b04
Austin Horstman <khaneliman12@gmail.com>
2024-09-07 17:26:44 -0500

    eslint: fix darwin

--- pkgs/by-name/es/eslint/package.nix
+++ pkgs/by-name/es/eslint/package.nix
@@ -2,9 +2,15 @@
   lib,
   buildNpmPackage,
   fetchFromGitHub,
+  stdenv,
+  overrideSDK,
 }:
-
-buildNpmPackage rec {
+let
+  buildNpmPackage' = buildNpmPackage.override {
+    stdenv = if stdenv.isDarwin then overrideSDK stdenv "11.0" else stdenv;
+  };
+in
+buildNpmPackage' rec {
   pname = "eslint";
   version = "9.9.1";
 
@@ -15,11 +21,16 @@ buildNpmPackage rec {
     hash = "sha256-n07a50bigglwr3ItZqbyQxu0mPZawTSVunwIe8goJBQ=";
   };
 
+  # NOTE: Generating lock-file
+  # arch = [ x64 arm64 ]
+  # platform = [ darwin linux]
+  # npm install --package-lock-only --arch=<arch> --platform=<os>
+  # darwin seems to generate a cross platform compatible lockfile
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
   '';
 
-  npmDepsHash = "sha256-/E1JUsbPyHzWJ4kuNRg/blYRaAdATYbk+jnJFJyzHLE=";
+  npmDepsHash = "sha256-sqQ7YeCMMK/9/XOX6QHZjX+2U+dYHkKiAzsLI0ehpAE=";
 
   dontNpmBuild = true;
   dontNpmPrune = true;
