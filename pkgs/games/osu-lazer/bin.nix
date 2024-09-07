9957d3ae64815626bcc37386c02c4afb6e13ca19
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-09-07 13:12:20 +0200

    osu-lazer-bin: disable auto updates

--- pkgs/games/osu-lazer/bin.nix
+++ pkgs/games/osu-lazer/bin.nix
@@ -3,6 +3,7 @@
 , fetchurl
 , fetchzip
 , appimageTools
+, makeWrapper
 }:
 
 let
@@ -64,7 +65,10 @@ else appimageTools.wrapType2 {
       contents = appimageTools.extract { inherit pname version src; };
     in
     ''
+      . ${makeWrapper}/nix-support/setup-hook
       mv -v $out/bin/${pname} $out/bin/osu\!
+      wrapProgram $out/bin/osu\! \
+        --set OSU_EXTERNAL_UPDATE_PROVIDER 1
       install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
       for i in 16 32 48 64 96 128 256 512 1024; do
         install -D ${contents}/osu\!.png $out/share/icons/hicolor/''${i}x$i/apps/osu\!.png
