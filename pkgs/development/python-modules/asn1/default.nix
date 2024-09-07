0514a1b8639bb8e83afbb0d3396916f6dfb336bc
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-07 18:36:46 +0200

    python312Packages.asn1: refactor

--- pkgs/development/python-modules/asn1/default.nix
+++ pkgs/development/python-modules/asn1/default.nix
@@ -5,12 +5,13 @@
   fetchFromGitHub,
   future,
   pytestCheckHook,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "asn1";
   version = "2.7.1";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
@@ -21,7 +22,9 @@ buildPythonPackage rec {
     hash = "sha256-xdkSJIe7qmz0Zu5IZ3Rl/h4v2j3YFrm1gz7lsNQ0ORs=";
   };
 
-  propagatedBuildInputs = [ future ];
+  build-system = [ setuptools ];
+
+  dependencies = [ future ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
