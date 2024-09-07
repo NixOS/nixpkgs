2193887893d81bd02a36aa4e51550fd00ee9b2c3
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-07 06:03:19 +0200

    python312: 3.12.5 -> 3.12.6
    https://docs.python.org/release/3.12.6/whatsnew/changelog.html
    (cherry picked from commit 9550eb8922a4ff28fcdc14b32bd28553d050dd8c)

--- pkgs/development/interpreters/python/default.nix
+++ pkgs/development/interpreters/python/default.nix
@@ -79,10 +79,10 @@ in {
     sourceVersion = {
       major = "3";
       minor = "12";
-      patch = "5";
+      patch = "6";
       suffix = "";
     };
-    hash = "sha256-+oouEsXmILCfU+ZbzYdVDS5aHi4Ev4upkdzFUROHY5c=";
+    hash = "sha256-GZllgpjPL7g33/7Y/zwDPvDJjvIM9zxdX2a+1auJaXw=";
     inherit (darwin) configd;
     inherit passthruFun;
   };
