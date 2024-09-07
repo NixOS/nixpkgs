8e54a4d4a9bdd79aeb428a999b271ed798a63c3a
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-09-07 02:50:49 +0200

    twitch-dl: 2.6.0 -> 2.7.0
    Diff: https://github.com/ihabunek/twitch-dl/compare/refs/tags/2.6.0...2.7.0
    
    Changelog: https://github.com/ihabunek/twitch-dl/blob/refs/tags/2.7.0/CHANGELOG.md

--- pkgs/by-name/tw/twitch-dl/package.nix
+++ pkgs/by-name/tw/twitch-dl/package.nix
@@ -7,14 +7,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "twitch-dl";
-  version = "2.6.0";
+  version = "2.7.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ihabunek";
     repo = "twitch-dl";
     rev = "refs/tags/${version}";
-    hash = "sha256-3cC8eukL3k4C9desqCMak1wAFFYcrtcpCM3ybMqrJ8A=";
+    hash = "sha256-UNxuUE7FJ1euDbGWzX6P4AdYP5SLZuFVYHNaFjuS6ps=";
   };
 
   pythonRelaxDeps = [
