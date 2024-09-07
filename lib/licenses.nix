f9660d9b40790c11114440f4b16e8103898b3aa3
jopejoe1 <johannes@joens.email>
2024-09-07 21:24:31 +0200

    lib/licenses: remove urls from licenses that have a spdxId
    if the licenses have a spdxId the url get auto-generated from it.

--- lib/licenses.nix
+++ lib/licenses.nix
@@ -110,7 +110,6 @@ lib.mapAttrs mkLicense ({
   arphicpl = {
     spdxId = "Arphic-1999";
     fullName = "Arphic Public License";
-    url = "https://www.freedesktop.org/wiki/Arphic_Public_License/";
   };
 
   artistic1 = {
@@ -231,7 +230,6 @@ lib.mapAttrs mkLicense ({
   bsl11 = {
     spdxId = "BUSL-1.1";
     fullName = "Business Source License 1.1";
-    url = "https://mariadb.com/bsl11";
     free = false;
     redistributable = true;
   };
@@ -244,13 +242,11 @@ lib.mapAttrs mkLicense ({
   cal10 = {
     spdxId = "CAL-1.0";
     fullName = "Cryptographic Autonomy License version 1.0 (CAL-1.0)";
-    url = "https://opensource.org/licenses/CAL-1.0";
   };
 
   caldera = {
     spdxId = "Caldera";
     fullName = "Caldera License";
-    url = "http://www.lemis.com/grog/UNIX/ancient-source-all.pdf";
   };
 
   capec = {
@@ -454,7 +450,6 @@ lib.mapAttrs mkLicense ({
 
   ecl20 = {
     fullName = "Educational Community License, Version 2.0";
-    url = "https://opensource.org/licenses/ECL-2.0";
     shortName = "ECL 2.0";
     spdxId = "ECL-2.0";
   };
@@ -472,7 +467,6 @@ lib.mapAttrs mkLicense ({
   elastic20 = {
     spdxId = "Elastic-2.0";
     fullName = "Elastic License 2.0";
-    url = "https://github.com/elastic/elasticsearch/blob/main/licenses/ELASTIC-LICENSE-2.0.txt";
     free = false;
   };
 
@@ -659,7 +653,6 @@ lib.mapAttrs mkLicense ({
   iasl = {
     spdxId = "Intel-ACPI";
     fullName = "iASL";
-    url = "https://old.calculate-linux.org/packages/licenses/iASL";
   };
 
   icu = {
@@ -685,7 +678,6 @@ lib.mapAttrs mkLicense ({
   info-zip = {
     spdxId = "Info-ZIP";
     fullName = "Info-ZIP License";
-    url = "https://infozip.sourceforge.net/license.html";
   };
 
   inria-compcert = {
@@ -870,7 +862,6 @@ lib.mapAttrs mkLicense ({
   miros = {
     spdxId = "MirOS";
     fullName = "MirOS License";
-    url = "https://opensource.org/licenses/MirOS";
   };
 
   mit = {
@@ -927,7 +918,6 @@ lib.mapAttrs mkLicense ({
   mulan-psl2 = {
     spdxId = "MulanPSL-2.0";
     fullName = "Mulan Permissive Software License, Version 2";
-    url = "https://license.coscl.org.cn/MulanPSL2";
   };
 
   nasa13 = {
@@ -957,7 +947,6 @@ lib.mapAttrs mkLicense ({
     fullName = "Netdata Cloud UI License v1.0";
     free = false;
     redistributable = true; # Only if used in Netdata products.
-    url = "https://raw.githubusercontent.com/netdata/netdata/master/web/gui/v2/LICENSE.md";
   };
 
   nistSoftware = {
@@ -1055,7 +1044,6 @@ lib.mapAttrs mkLicense ({
   parity70 = {
     spdxId = "Parity-7.0.0";
     fullName = "Parity Public License 7.0.0";
-    url = "https://paritylicense.com/versions/7.0.0.html";
   };
 
   php301 = {
@@ -1077,7 +1065,6 @@ lib.mapAttrs mkLicense ({
   psfl = {
     spdxId = "Python-2.0";
     fullName = "Python Software Foundation License version 2";
-    url = "https://docs.python.org/license.html";
   };
 
   publicDomain = {
@@ -1203,7 +1190,6 @@ lib.mapAttrs mkLicense ({
   ufl = {
     spdxId = "Ubuntu-font-1.0";
     fullName = "Ubuntu Font License 1.0";
-    url = "https://ubuntu.com/legal/font-licence";
   };
 
   unfree = {
@@ -1247,7 +1233,6 @@ lib.mapAttrs mkLicense ({
   upl = {
     spdxId = "UPL-1.0";
     fullName = "Universal Permissive License";
-    url = "https://oss.oracle.com/licenses/upl/";
   };
 
   vim = {
@@ -1304,7 +1289,6 @@ lib.mapAttrs mkLicense ({
   xfig = {
     spdxId = "Xfig";
     fullName = "xfig";
-    url = "https://mcj.sourceforge.net/authors.html#xfig";
   };
 
   xinetd = {
