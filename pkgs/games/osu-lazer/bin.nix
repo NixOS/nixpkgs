51dc189cf999aec090f720ec2e90b8156d9969a8
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-09-07 13:31:23 +0200

    osu-lazer-bin: don't escape exclamation marks

--- pkgs/games/osu-lazer/bin.nix
+++ pkgs/games/osu-lazer/bin.nix
@@ -66,12 +66,12 @@ else appimageTools.wrapType2 {
     in
     ''
       . ${makeWrapper}/nix-support/setup-hook
-      mv -v $out/bin/${pname} $out/bin/osu\!
-      wrapProgram $out/bin/osu\! \
+      mv -v $out/bin/${pname} $out/bin/osu!
+      wrapProgram $out/bin/osu! \
         --set OSU_EXTERNAL_UPDATE_PROVIDER 1
-      install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
+      install -m 444 -D ${contents}/osu!.desktop -t $out/share/applications
       for i in 16 32 48 64 96 128 256 512 1024; do
-        install -D ${contents}/osu\!.png $out/share/icons/hicolor/''${i}x$i/apps/osu\!.png
+        install -D ${contents}/osu!.png $out/share/icons/hicolor/''${i}x$i/apps/osu!.png
       done
     '';
 }
