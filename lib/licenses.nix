6a1317763d418f3446fd6e0d77924145f0606c0f
jopejoe1 <johannes@joens.email>
2024-09-07 21:16:37 +0200

    lib/licenses: set spdx for Ubuntu font license

--- lib/licenses.nix
+++ lib/licenses.nix
@@ -1201,6 +1201,7 @@ lib.mapAttrs mkLicense ({
   };
 
   ufl = {
+    spdxId = "Ubuntu-font-1.0";
     fullName = "Ubuntu Font License 1.0";
     url = "https://ubuntu.com/legal/font-licence";
   };
