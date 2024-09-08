6a6a191d00ef20a836be34ac5a8ce6fbe1dbb640
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2024-09-08 09:07:10 +0200

    libtiff: migrate to `pkgs/by-name` overlay

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22090,8 +22090,6 @@ with pkgs;
 
   libtifiles2 = callPackage ../development/libraries/libtifiles2 { };
 
-  libtiff = callPackage ../development/libraries/libtiff { };
-
   libtiger = callPackage ../development/libraries/libtiger { };
 
   libtommath = callPackage ../development/libraries/libtommath { };
