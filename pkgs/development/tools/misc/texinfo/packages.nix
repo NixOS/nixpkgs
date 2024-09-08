9bd50dacb431d0188759d2c0b8e046e8610b89b2
Alexis Hildebrandt <afh@surryhill.net>
2024-09-08 16:05:51 +0200

    texinfo7: 7.1 -> 7.1.1

--- pkgs/development/tools/misc/texinfo/packages.nix
+++ pkgs/development/tools/misc/texinfo/packages.nix
@@ -102,7 +102,7 @@ in
     patches = [ ./fix-glibc-2.34.patch ];
   };
   texinfo7 = buildTexinfo {
-    version = "7.1";
-    hash = "sha256-3u7J8Z8VngRv34rSIjGYGAbawzLMNy8cdjUErYKzCVM=";
+    version = "7.1.1";
+    hash = "sha256-Ma435GKDUpQythvuHOAe0AkNWZ5gb8aincofd8dqbII=";
   };
 }
