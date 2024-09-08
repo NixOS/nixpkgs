86d7a802cb310b4aea78a6fbd2f0e9907a0429c3
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2024-09-08 08:39:07 +0200

    gscan2pdf: use newest libtiff instead of libtiff 4.6.0t
    libtiff 4.7.0 restored the programs needed by gscan2pdf,
    cf. https://github.com/NixOS/nixpkgs/commit/ec1a3695b48e649ededd2b25bf421aa9145a9afe

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5281,11 +5281,7 @@ with pkgs;
 
   grobi = callPackage ../tools/X11/grobi { };
 
-  gscan2pdf = callPackage ../applications/graphics/gscan2pdf {
-    # needs this fork of libtiff, because original libtiff
-    # stopped packaging required tools with version 4.6
-    libtiff = libtiff_t;
-  };
+  gscan2pdf = callPackage ../applications/graphics/gscan2pdf { };
 
   gsctl = callPackage ../applications/misc/gsctl { };
 
