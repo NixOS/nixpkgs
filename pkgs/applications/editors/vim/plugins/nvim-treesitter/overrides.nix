a3fe631e5218a66065fc9bef63f7569e8f746f19
PerchunPak <git@perchun.it>
2024-09-08 13:29:41 +0200

    vimPlugins.nvim-treesitter: properly handle duplicate grammars

--- pkgs/applications/editors/vim/plugins/nvim-treesitter/overrides.nix
+++ pkgs/applications/editors/vim/plugins/nvim-treesitter/overrides.nix
@@ -31,27 +31,57 @@ let
   allGrammars = lib.attrValues generatedDerivations;
 
   # Usage:
-  # pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.c p.java ... ])
+  #   pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.c p.java ... ])
   # or for all grammars:
-  # pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+  #   pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+  # you can also filter out plugins (e.g. if there are duplicates):
+  #   pkgs.vimPlugins.nvim-treesitter.withAllGrammars.filterOutGrammars (e: e.meta.name == "c-grammar-0.0.0+rev=deca017")
+  # or use `e.name`, which contains `vimplugin-treesitter-grammar-c` (no version and rev)
+  # function provided to `filterOutGrammars` is passed to `lib.filter`
   withPlugins =
     f: self.nvim-treesitter.overrideAttrs {
-      passthru.dependencies =
-        let
-          grammars = map grammarToPlugin
-            (f (tree-sitter.builtGrammars // builtGrammars));
-          copyGrammar = grammar:
-            let name = lib.last (lib.splitString "-" grammar.name); in
-            "ln -sf ${grammar}/parser/${name}.so $out/parser/${name}.so";
-        in
-        [
-          (runCommand "vimplugin-treesitter-grammars"
-            { meta.platforms = lib.platforms.all; }
-            ''
-              mkdir -p $out/parser
-              ${lib.concatMapStringsSep "\n" copyGrammar grammars}
-            '')
-        ];
+      passthru = let
+        grammars = map grammarToPlugin
+          (f (tree-sitter.builtGrammars // builtGrammars));
+
+        filterDuplicateGrammars = lib.lists.foldl' (
+          res: new-el:
+            if lib.lists.any (old-el:
+              # `meta.name` contains something like `c-grammar-0.0.0+rev=be23d2c`
+              # while `name` contains `vimplugin-treesitter-grammar-c`
+              if new-el.meta.name == old-el.meta.name
+              then true
+              else if new-el.name == old-el.name
+                then builtins.trace ''
+                  error: found duplicate grammars (${new-el.meta.name} and ${old-el.meta.name}). use '.filterOutGrammars' to filter out duplicates
+                  example: 'vimPlugins.nvim-treesitter.withAllGrammars.filterOutGrammars (e: e.meta.name == "${new-el.meta.name}")'
+                '' false
+                else false
+            ) res
+            then res
+            else res ++ [ new-el ]
+        ) [ ];
+
+        copyGrammar = grammar:
+          let name = lib.last (lib.splitString "-" grammar.name); in
+          "ln -s ${grammar}/parser/${name}.so $out/parser/${name}.so";
+
+        collateGrammars = grammars:
+          [
+            (runCommand "vimplugin-treesitter-grammars"
+              { meta.platforms = lib.platforms.all; }
+              ''
+                mkdir -p $out/parser
+                ${lib.concatMapStringsSep "\n" copyGrammar (filterDuplicateGrammars grammars)}
+              '')
+          ];
+      in {
+        dependencies = collateGrammars grammars;
+
+        filterOutPlugins = filter: self.nvim-treesitter.overrideAttrs {
+          passthru.dependencies = collateGrammars (lib.filter filter grammars);
+        };
+      };
     };
 
   withAllGrammars = withPlugins (_: allGrammars);
