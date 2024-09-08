19ba8eaf6ea738bb91798e6d24482636c247b91a
kiri <eduardo@eduardoquiros.com>
2024-09-07 18:42:18 -0600

    signal-desktop: 7.22.0 -> 7.23.0, -beta: 7.24.0-beta.1, aarch64: 7.19.0 -> 7.23.0

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop-beta";
   dir = "Signal Beta";
-  version = "7.23.0-beta.1";
+  version = "7.24.0-beta.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-qVPvzz5dEzff9+qmy92X2VWOhcfYdeoOsV4hX/PcJ+k=";
+  hash = "sha256-Nzutc0H/ESapLFVeu+oOHs+WbtSvgjM53mm1LWZ8weE=";
 }
--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "7.22.0";
+  version = "7.23.0";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-om3hhGYgrBrcNt3tV9QHZOKuhN/ljaR6SG/6S51xqOo=";
+  hash = "sha256-B/lTJNqkFXdjKW7YEH8BFs2TrpyUh2do+6KnA3f9QQQ=";
 }
