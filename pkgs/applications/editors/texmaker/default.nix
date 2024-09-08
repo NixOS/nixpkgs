059cc40db439a1ac8261ee9acf92e67d3ddb9ef3
Markus Kowalewski <markus.kowalewski@gmail.com>
2024-09-08 20:59:49 +0200

    texmaker: format code

--- pkgs/applications/editors/texmaker/default.nix
+++ pkgs/applications/editors/texmaker/default.nix
@@ -1,16 +1,40 @@
-{ lib, stdenv, fetchurl, cmake, qtbase, qttools, qtwebengine, qt5compat, zlib, pkg-config, poppler, wrapQtAppsHook }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  cmake,
+  pkg-config,
+  wrapQtAppsHook,
+  poppler,
+  qtbase,
+  qttools,
+  qtwebengine,
+  qt5compat,
+  zlib
+}:
 
 stdenv.mkDerivation rec {
   pname = "texmaker";
   version = "6.0.0";
 
   src = fetchurl {
-    url = "http://www.xm1math.net/texmaker/${pname}-${version}.tar.bz2";
+    url = "http://www.xm1math.net/texmaker/texmaker-${version}.tar.bz2";
     hash = "sha256-l3zlgOJcGrbgvD2hA74LQ+v2C4zg0nJzEE/df1hhd/w=";
   };
 
-  buildInputs = [ qtbase poppler zlib qtwebengine qt5compat qttools ];
-  nativeBuildInputs = [ pkg-config cmake wrapQtAppsHook ];
+  buildInputs = [
+    poppler
+    qtbase
+    qtwebengine
+    qt5compat
+    qttools
+    zlib
+  ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    wrapQtAppsHook
+  ];
 
   qmakeFlags = [
     "DESKTOPDIR=${placeholder "out"}/share/applications"
@@ -20,7 +44,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "TeX and LaTeX editor";
-    longDescription=''
+    longDescription = ''
       This editor is a full fledged IDE for TeX and
       LaTeX editing with completion, structure viewer, preview,
       spell checking and support of any compilation chain.
@@ -28,7 +52,10 @@ stdenv.mkDerivation rec {
     homepage = "http://www.xm1math.net/texmaker/";
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ cfouche markuskowa ];
+    maintainers = with maintainers; [
+      cfouche
+      markuskowa
+    ];
     mainProgram = "texmaker";
   };
 }
