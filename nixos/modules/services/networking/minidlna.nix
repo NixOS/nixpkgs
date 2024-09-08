547251956b2dad7de893eb09e876a8e79df6f9b9
bb2020 <bb2020@users.noreply.github.com>
2024-09-08 13:34:19 +0300

    nixos/minidlna: remove with lib

--- nixos/modules/services/networking/minidlna.nix
+++ nixos/modules/services/networking/minidlna.nix
@@ -1,6 +1,5 @@
 # Module for MiniDLNA, a simple DLNA server.
 { config, lib, pkgs, ... }:
-with lib;
 
 let
   cfg = config.services.minidlna;
@@ -10,8 +9,8 @@ in
 
 {
   ###### interface
-  options.services.minidlna.enable = mkOption {
-    type = types.bool;
+  options.services.minidlna.enable = lib.mkOption {
+    type = lib.types.bool;
     default = false;
     description = ''
       Whether to enable MiniDLNA, a simple DLNA server.
@@ -23,25 +22,25 @@ in
 
   options.services.minidlna.package = lib.mkPackageOption pkgs "minidlna" { };
 
-  options.services.minidlna.openFirewall = mkOption {
-    type = types.bool;
+  options.services.minidlna.openFirewall = lib.mkOption {
+    type = lib.types.bool;
     default = false;
     description = ''
       Whether to open both HTTP (TCP) and SSDP (UDP) ports in the firewall.
     '';
   };
 
-  options.services.minidlna.settings = mkOption {
+  options.services.minidlna.settings = lib.mkOption {
     default = {};
     description = ''
       The contents of MiniDLNA's configuration file.
       When the service is activated, a basic template is generated from the current options opened here.
     '';
-    type = types.submodule {
+    type = lib.types.submodule {
       freeformType = settingsFormat.type;
 
-      options.media_dir = mkOption {
-        type = types.listOf types.str;
+      options.media_dir = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
         default = [];
         example = [ "/data/media" "V,/home/alice/video" ];
         description = ''
@@ -50,8 +49,8 @@ in
           The directories must be accessible to the `minidlna` user account.
         '';
       };
-      options.notify_interval = mkOption {
-        type = types.int;
+      options.notify_interval = lib.mkOption {
+        type = lib.types.int;
         default = 90000;
         description = ''
           The interval between announces (in seconds).
@@ -61,48 +60,48 @@ in
           https://sourceforge.net/p/minidlna/discussion/879957/thread/1389d197/
         '';
       };
-      options.port = mkOption {
-        type = types.port;
+      options.port = lib.mkOption {
+        type = lib.types.port;
         default = 8200;
         description = "Port number for HTTP traffic (descriptions, SOAP, media transfer).";
       };
-      options.db_dir = mkOption {
-        type = types.path;
+      options.db_dir = lib.mkOption {
+        type = lib.types.path;
         default = "/var/cache/minidlna";
         example = "/tmp/minidlna";
         description = "Specify the directory where you want MiniDLNA to store its database and album art cache.";
       };
-      options.friendly_name = mkOption {
-        type = types.str;
+      options.friendly_name = lib.mkOption {
+        type = lib.types.str;
         default = config.networking.hostName;
-        defaultText = literalExpression "config.networking.hostName";
+        defaultText = lib.literalExpression "config.networking.hostName";
         example = "rpi3";
         description = "Name that the DLNA server presents to clients.";
       };
-      options.root_container = mkOption {
-        type = types.str;
+      options.root_container = lib.mkOption {
+        type = lib.types.str;
         default = "B";
         example = ".";
         description = "Use a different container as the root of the directory tree presented to clients.";
       };
-      options.log_level = mkOption {
-        type = types.str;
+      options.log_level = lib.mkOption {
+        type = lib.types.str;
         default = "warn";
         example = "general,artwork,database,inotify,scanner,metadata,http,ssdp,tivo=warn";
         description = "Defines the type of messages that should be logged and down to which level of importance.";
       };
-      options.inotify = mkOption {
-        type = types.enum [ "yes" "no" ];
+      options.inotify = lib.mkOption {
+        type = lib.types.enum [ "yes" "no" ];
         default = "no";
         description = "Whether to enable inotify monitoring to automatically discover new files.";
       };
-      options.enable_tivo = mkOption {
-        type = types.enum [ "yes" "no" ];
+      options.enable_tivo = lib.mkOption {
+        type = lib.types.enum [ "yes" "no" ];
         default = "no";
         description = "Support for streaming .jpg and .mp3 files to a TiVo supporting HMO.";
       };
-      options.wide_links = mkOption {
-        type = types.enum [ "yes" "no" ];
+      options.wide_links = lib.mkOption {
+        type = lib.types.enum [ "yes" "no" ];
         default = "no";
         description = "Set this to yes to allow symlinks that point outside user-defined `media_dir`.";
       };
@@ -110,19 +109,19 @@ in
   };
 
   imports = [
-    (mkRemovedOptionModule [ "services" "minidlna" "config" ] "")
-    (mkRemovedOptionModule [ "services" "minidlna" "extraConfig" ] "")
-    (mkRenamedOptionModule [ "services" "minidlna" "loglevel"] [ "services" "minidlna" "settings" "log_level" ])
-    (mkRenamedOptionModule [ "services" "minidlna" "rootContainer"] [ "services" "minidlna" "settings" "root_container" ])
-    (mkRenamedOptionModule [ "services" "minidlna" "mediaDirs"] [ "services" "minidlna" "settings" "media_dir" ])
-    (mkRenamedOptionModule [ "services" "minidlna" "friendlyName"] [ "services" "minidlna" "settings" "friendly_name" ])
-    (mkRenamedOptionModule [ "services" "minidlna" "announceInterval"] [ "services" "minidlna" "settings" "notify_interval" ])
+    (lib.mkRemovedOptionModule [ "services" "minidlna" "config" ] "")
+    (lib.mkRemovedOptionModule [ "services" "minidlna" "extraConfig" ] "")
+    (lib.mkRenamedOptionModule [ "services" "minidlna" "loglevel"] [ "services" "minidlna" "settings" "log_level" ])
+    (lib.mkRenamedOptionModule [ "services" "minidlna" "rootContainer"] [ "services" "minidlna" "settings" "root_container" ])
+    (lib.mkRenamedOptionModule [ "services" "minidlna" "mediaDirs"] [ "services" "minidlna" "settings" "media_dir" ])
+    (lib.mkRenamedOptionModule [ "services" "minidlna" "friendlyName"] [ "services" "minidlna" "settings" "friendly_name" ])
+    (lib.mkRenamedOptionModule [ "services" "minidlna" "announceInterval"] [ "services" "minidlna" "settings" "notify_interval" ])
   ];
 
   ###### implementation
-  config = mkIf cfg.enable {
-    networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.settings.port ];
-    networking.firewall.allowedUDPPorts = mkIf cfg.openFirewall [ 1900 ];
+  config = lib.mkIf cfg.enable {
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [ cfg.settings.port ];
+    networking.firewall.allowedUDPPorts = lib.mkIf cfg.openFirewall [ 1900 ];
 
     users.users.minidlna = {
       description = "MiniDLNA daemon user";
