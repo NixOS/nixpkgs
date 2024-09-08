5d65b513e0ffa75e03ecc38d47e4b9e3ed125776
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-09 00:46:09 +0400

    merkaartor: 0.19.0 → 0.20.0

--- pkgs/applications/misc/merkaartor/default.nix
+++ pkgs/applications/misc/merkaartor/default.nix
@@ -1,13 +1,14 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, fetchpatch
 , cmake
 , pkg-config
 , qttools
 , wrapQtAppsHook
 , gdal
 , proj
+, protobuf
+, qtnetworkauth
 , qtsvg
 , qtwebengine
 , withGeoimage ? true, exiv2
@@ -18,31 +19,18 @@
 
 stdenv.mkDerivation rec {
   pname = "merkaartor";
-  version = "0.19.0";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "openstreetmap";
     repo = "merkaartor";
     rev = version;
-    hash = "sha256-I3QNCXzwhEFa8aOdwl3UJV8MLZ9caN9wuaaVrGFRvbQ=";
+    hash = "sha256-oxLGhIE1qJ9+GOztD1HvrLGRGVO3gyy7Rc6CyzKTFec=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "exiv2-0.28.patch";
-      url = "https://github.com/openstreetmap/merkaartor/commit/1e20d2ccd743ea5f8c2358e4ae36fead8b9390fd.patch";
-      hash = "sha256-aHjJLKYvqz7V0QwUIg0SbentBe+DaCJusVqy4xRBVWo=";
-    })
-    # https://github.com/openstreetmap/merkaartor/pull/290
-    (fetchpatch {
-      url = "https://github.com/openstreetmap/merkaartor/commit/7dede77370d89e8e7586f6ed5af225f9b5bde6cf.patch";
-      hash = "sha256-3oDRPysVNvA50t/b9xOcVQgac3U1lDPrencanl4c6Zk=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake pkg-config qttools wrapQtAppsHook ];
 
-  buildInputs = [ gdal proj qtsvg qtwebengine ]
+  buildInputs = [ gdal proj protobuf qtnetworkauth qtsvg qtwebengine ]
     ++ lib.optional withGeoimage exiv2
     ++ lib.optional withGpsdlib gpsd
     ++ lib.optional withLibproxy libproxy
@@ -56,11 +44,7 @@ stdenv.mkDerivation rec {
     (lib.cmakeBool "ZBAR" withZbar)
   ];
 
-  postInstall = ''
-    # Binary is looking for .qm files in share/merkaartor
-    mv $out/share/merkaartor/{translations/*.qm,}
-    rm -r $out/share/merkaartor/translations
-  '' + lib.optionalString stdenv.isDarwin ''
+  postInstall = lib.optionalString stdenv.isDarwin ''
     mkdir -p $out/{Applications,bin}
     mv $out/merkaartor.app $out/Applications
     # Prevent wrapping, otherwise plugins will not be loaded
@@ -68,12 +52,12 @@ stdenv.mkDerivation rec {
     makeWrapper $out/{Applications/merkaartor.app/Contents/MacOS,bin}/merkaartor
   '';
 
-  meta = with lib; {
+  meta = {
     description = "OpenStreetMap editor";
     homepage = "http://merkaartor.be/";
-    license = licenses.gpl2Plus;
+    license = lib.licenses.gpl2Plus;
     mainProgram = "merkaartor";
-    maintainers = with maintainers; [ sikmir ];
-    platforms = platforms.unix;
+    maintainers = with lib.maintainers; [ sikmir ];
+    platforms = lib.platforms.unix;
   };
 }
