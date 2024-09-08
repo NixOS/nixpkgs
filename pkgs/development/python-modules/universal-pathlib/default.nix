6a7bc2048e649a3ad0f918aed3fc396d9a0e3d03
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-08 02:26:28 +0000

    python312Packages.universal-pathlib: 0.2.3 -> 0.2.4

--- pkgs/development/python-modules/universal-pathlib/default.nix
+++ pkgs/development/python-modules/universal-pathlib/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "universal-pathlib";
-  version = "0.2.3";
+  version = "0.2.4";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "universal_pathlib";
     inherit version;
-    hash = "sha256-IvXyif7exLZjlWWWdCZS4hd7yiRmG2yKFz9ZdM/uAFI=";
+    hash = "sha256-VXChH9iMrRu8YiCz5yP92K38XF4AhlIJ6IrwP/SqFUs=";
   };
 
   nativeBuildInputs = [
