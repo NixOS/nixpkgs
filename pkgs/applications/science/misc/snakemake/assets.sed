803e3d25c6e92b899e5839eecda7d573669e59fa
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-09-08 13:37:36 -0400

    snakemake: 8.19.3 -> 8.20.1

--- pkgs/applications/science/misc/snakemake/assets.sed
+++
@@ -1,9 +0,0 @@
-/Asset(.*/ {               # find Asset declaration
-s/: Asset(/ = fetchurl {/; # replace leading
-  :b;                      # a label
-  $!N;                     # if not EOF, include next line
-  s/",/";/;                # provide separators between attributes
-  /)/!bb;                  # repeat until a closing bracket is located
-  s/),/};/;                # provide separators between assets
-  p                        # output what we have
-}
--- pkgs/applications/science/misc/snakemake/default.nix
+++ pkgs/applications/science/misc/snakemake/default.nix
@@ -1,7 +1,6 @@
 { lib
-, fetchFromGitHub
+, fetchPypi
 , fetchpatch
-, fetchurl
 , python3
 , runtimeShell
 , stress
@@ -9,28 +8,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "snakemake";
-  version = "8.19.3";
+  version = "8.20.1";
   format = "setuptools";
 
-  src = fetchFromGitHub {
-    owner = "snakemake";
-    repo = pname;
-    rev = "refs/tags/v${version}";
-    hash = "sha256-DWlMF/aNRCYDfTP41ALaGJFBTBjHxqZtx/0PtD/37KY=";
-    # https://github.com/python-versioneer/python-versioneer/issues/217
-    postFetch = ''
-      sed -i "$out"/snakemake/_version.py -e 's#git_refnames = ".*"#git_refnames = " (tag: v${version})"#'
-    '';
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-adNwIA1z/TwWsa0gQb4hAsUvHInjd30sm1dYKXvvXy8=";
   };
-
-  patches = [
-    # Avoid downloading if files are provided
-    # https://github.com/snakemake/snakemake/pull/3072
-    (fetchpatch {
-      url = "https://github.com/snakemake/snakemake/commit/ea89ec2f20faa0db303fdab0eafa640c9147cc14.diff";
-      hash = "sha256-gzmto3Kusk40kPH29DkvYcJgAx2VAOq3mw5vPSCp5mI=";
-    })
-  ];
 
   postPatch = ''
     patchShebangs --build tests/
@@ -39,14 +23,7 @@ python3.pkgs.buildPythonApplication rec {
       --replace-fail 'del os.environ["PYTHONPATH"]' "pass"
     substituteInPlace snakemake/unit_tests/__init__.py \
       --replace-fail '"unit_tests/templates"' '"'"$PWD"'/snakemake/unit_tests/templates"'
-  '' + (let
-    assets = import ./assets.nix { inherit fetchurl; };
-  in
-    # Use fetchPypi once https://github.com/snakemake/snakemake/pull/3073 is available
-    lib.concatStrings (lib.mapAttrsToList (name: file: ''
-      echo install -D "${file}" snakemake/assets/data/${name}
-      install -D "${file}" snakemake/assets/data/${name}
-    '') assets));
+  '';
 
   propagatedBuildInputs = with python3.pkgs; [
     appdirs
@@ -106,44 +83,6 @@ python3.pkgs.buildPythonApplication rec {
 
   # Some will be disabled via https://github.com/snakemake/snakemake/pull/3074
   disabledTests = [
-    # requires network connection
-    "test_deploy_sources"
-    "test_modules_meta_wrapper"
-    "test_module_complex"
-    "test_module_complex2"
-    "test_module_with_script"
-    "test_module_report"
-    "test_load_metawrapper"
-    # requires conda
-    "test_archive"
-    "test_script"
-    "test_conda"
-    "test_conda_list_envs"
-    "test_upstream_conda"
-    "test_deploy_script"
-    "test_deploy_hashing"
-    "test_conda_custom_prefix"
-    "test_conda_cmd_exe"
-    "test_wrapper"
-    "test_wrapper_local_git_prefix"
-    "test_singularity_conda"
-    "test_issue635"
-    "test_issue1093"
-    "test_jupyter_notebook"
-    "test_jupyter_notebook_draft"
-    "test_converting_path_for_r_script"
-    "test_conda_named"
-    "test_conda_function"
-    "test_conda_pin_file"
-    "test_conda_python_script"
-    "test_conda_python_3_7_script"
-    "test_prebuilt_conda_script"
-    "test_conda_global"
-    "test_script_pre_py39"
-    "test_resource_string_in_cli_or_profile"
-    "test_conda"
-    "test_env_modules"
-    "test_script_pre_py39"
     # requires graphviz
     "test_filegraph"
     # requires s3
@@ -152,28 +91,23 @@ python3.pkgs.buildPythonApplication rec {
     "test_output_file_cache_storage"
     # requires peppy and eido
     "test_pep"
+    "test_modules_peppy"
     # requires perl
     "test_shadow"
     # requires snakemake-storage-plugin-http
     "test_ancient"
     "test_modules_prefix"
+    # requires snakemake-storage-plugin-s3
+    "test_deploy_sources"
+    # requires modules
+    "test_env_modules"
+    # issue with locating template file
+    "test_generate_unit_tests"
     # weird
     "test_strict_mode"
     "test_issue1256"
     "test_issue2574"
-    # requires apptainer
-    "test_singularity"
-    "test_singularity_cluster"
-    "test_singularity_invalid"
-    "test_singularity_module_invalid"
-    "test_singularity_none"
-    "test_singularity_global"
-    "test_cwl_singularity"
-    "test_issue1083"
-    "test_github_issue78"
-    "test_container"
-    "test_shell_exec"
-    "test_containerized"
+    "test_github_issue1384"
     # future-proofing
     "conda"
     "singularity"
@@ -188,8 +122,6 @@ python3.pkgs.buildPythonApplication rec {
   preCheck = ''
     export HOME="$(mktemp -d)"
   '';
-
-  passthru.updateScript = ./update.sh;
 
   meta = with lib; {
     homepage = "https://snakemake.github.io";
