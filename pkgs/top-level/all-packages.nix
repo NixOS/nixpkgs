5d61a1c53e9381830cac968e4af71d90b971ae6a
Randy Eckenrode <randy@largeandhighquality.com>
2024-09-07 22:01:39 -0400

    libxml2: build with the bootstrap stdenv on Darwin
    Using the bootstrap stdenv avoids an infinite recursion from xcbuild
    depending on libxml2 depending on xcrun from xcbuild, which is
    propagated by the non-bootstrap stdenv.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21684,6 +21684,13 @@ with pkgs;
 
   libxml2 = callPackage ../development/libraries/libxml2 {
     python = python3;
+    stdenv =
+      # libxml2 is a dependency of xcbuild. Avoid an infinite recursion by using a bootstrap stdenv
+      # that does not propagate xcrun.
+      if stdenv.hostPlatform.isDarwin then
+        darwin.bootstrapStdenv
+      else
+        stdenv;
   };
 
   libxml2Python = let
