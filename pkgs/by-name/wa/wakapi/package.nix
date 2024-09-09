c2e3956797ac0d2aa9128f36ff722890a29a5880
isabel <isabel@isabelroses.com>
2024-09-09 14:16:14 +0100

    wakapi: 2.11.2 -> 2.12.0

--- pkgs/by-name/wa/wakapi/package.nix
+++ pkgs/by-name/wa/wakapi/package.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "wakapi";
-  version = "2.11.2";
+  version = "2.12.0";
 
   src = fetchFromGitHub {
     owner = "muety";
     repo = pname;
     rev = version;
-    sha256 = "sha256-lBjYtb64blFUH/iW/SmC4A7nX9asokvsNKu6QVYgmZ8=";
+    sha256 = "sha256-/aacT/VLA5S4PeGcxEGaCpgAw++b3VFD7T0CldZWcQI=";
   };
 
-  vendorHash = "sha256-Kt7RzAGZeLFhwvq+V6AK88rivqkoTE1Zep7NMh3BXXQ=";
+  vendorHash = "sha256-Q56Ud0MtkstB/dhn+QyAHTzIqHsmKvHEK+5PAt5lIMM=";
 
   # Not a go module required by the project, contains development utilities
   excludedPackages = [ "scripts" ];
