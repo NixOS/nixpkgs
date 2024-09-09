7bee8c8cd9a6e8fde8a0fac18c571ab7c08d16ec
isabel <isabel@isabelroses.com>
2024-09-09 14:17:09 +0100

    wakapi: modernize

--- pkgs/by-name/wa/wakapi/package.nix
+++ pkgs/by-name/wa/wakapi/package.nix
@@ -1,14 +1,20 @@
-{ lib, buildGoModule, fetchFromGitHub }:
-
-buildGoModule rec {
-  pname = "wakapi";
+{
+  lib,
+  buildGo123Module,
+  fetchFromGitHub,
+}:
+let
   version = "2.12.0";
+in
+buildGo123Module {
+  pname = "wakapi";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "muety";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-/aacT/VLA5S4PeGcxEGaCpgAw++b3VFD7T0CldZWcQI=";
+    repo = "wakapi";
+    rev = "refs/tags/${version}";
+    hash = "sha256-/aacT/VLA5S4PeGcxEGaCpgAw++b3VFD7T0CldZWcQI=";
   };
 
   vendorHash = "sha256-Q56Ud0MtkstB/dhn+QyAHTzIqHsmKvHEK+5PAt5lIMM=";
@@ -24,12 +30,15 @@ buildGoModule rec {
     "-w"
   ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://wakapi.dev/";
     changelog = "https://github.com/muety/wakapi/releases/tag/${version}";
     description = "Minimalist self-hosted WakaTime-compatible backend for coding statistics";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ t4ccer ];
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [
+      t4ccer
+      isabelroses
+    ];
     mainProgram = "wakapi";
   };
 }
