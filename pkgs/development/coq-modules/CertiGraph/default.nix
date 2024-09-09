948f79aed8546ac6c5914175b84b217019cde3ee
definfo <hjsdbb1@gmail.com>
2024-09-10 00:26:44 +0800

    fix: remove compcert from args

--- pkgs/development/coq-modules/CertiGraph/default.nix
+++ pkgs/development/coq-modules/CertiGraph/default.nix
@@ -3,7 +3,6 @@
   mkCoqDerivation,
   stdenv,
   lib,
-  compcert,
   VST,
   version ? null,
 }:
@@ -14,16 +13,16 @@ mkCoqDerivation {
 
   inherit version;
   defaultVersion = with lib.versions;
-    lib.switch [ coq.coq-version compcert.version VST.version ] [
-      { cases = [ "8.19" "3.14" "2.14" ]; out = "live"; }
-      { cases = [ (range "8.14" "8.16") "3.14" (isGe "2.8") ]; out = "CPS-Coq8.16"; }
+    lib.switch [ coq.coq-version VST.version ] [
+      { cases = [ "8.19" "2.14" ]; out = "live"; }
+      { cases = [ (range "8.14" "8.16") (isGe "2.8") ]; out = "CPS-Coq8.16"; }
     ] null;
   release."live" = {
     rev = "a2db4cf823730a104ddbde7f170627a47fc2f1b7";
     sha256 = "sha256-Bg1f8RHwXhaX0Fu1hM++MMGwfUE27pRZEKX2Zs6UPvI=";
   };
 
-  propagatedBuildInputs = [ compcert VST ];
+  propagatedBuildInputs = [ VST ];
 
   makeFlags = lib.optional stdenv.is32bit [ "BITSIZE=32" ];
 
