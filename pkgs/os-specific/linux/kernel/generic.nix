abc759221fc62d75c0441a278b381591af717c7a
Andrew Marshall <andrew@johnandrewmarshall.com>
2024-09-09 09:57:01 -0400

    linux: remove unneeded and misleading passthru.isVanilla
    This was added for use by ZFS, but it turned out that `pname == "linux"`
    is sufficient enough and has better coverage since many of our Linux
    variants do not have an existing `passthru.isX`, and instead are
    identifiable by a different pname.

--- pkgs/os-specific/linux/kernel/generic.nix
+++ pkgs/os-specific/linux/kernel/generic.nix
@@ -230,7 +230,6 @@ kernel.overrideAttrs (finalAttrs: previousAttrs: {
   passthru = previousAttrs.passthru or { } // basicArgs // {
     features = kernelFeatures;
     inherit commonStructuredConfig structuredExtraConfig extraMakeFlags isZen isHardened isLibre;
-    isVanilla = !(isHardened || isLibre || isZen);
     isXen = lib.warn "The isXen attribute is deprecated. All Nixpkgs kernels that support it now have Xen enabled." true;
 
     # Adds dependencies needed to edit the config:
