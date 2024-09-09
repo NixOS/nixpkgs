898af682bb0a71b666916e1c289138f1d3e8df98
cyewashish <wawashish@cyekaivy.dev>
2024-09-08 21:45:04 -0400

    Update maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4386,6 +4386,10 @@
 
     keys = [ { fingerprint = "BBED 1B08 8CED 7F95 8917 FBE8 5004 F0FA D051 576D"; } ];
   };
+  cyewashish = {
+    name = "Cyewashish";
+    email = "wawashish@cyekaivy.dev";
+  };
   cynerd = {
     name = "Karel Kočí";
     email = "cynerd@email.cz";
