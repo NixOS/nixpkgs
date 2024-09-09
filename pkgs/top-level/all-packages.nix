NixOS/nixpkgs

commit 95e172d7c0f4fbdcc117760fe973507e61a6d550

valgrind-light: set meta.description to clarify difference to valgrind

pkgs/top-level/all-packages.nix
@@ -19123,7 +19123,9 @@ with pkgs;
   valgrind = callPackage ../development/tools/analysis/valgrind {
     inherit (buildPackages.darwin) xnu bootstrap_cmds;
   };
-  valgrind-light = res.valgrind.override { gdb = null; };
+  valgrind-light = (res.valgrind.override { gdb = null; }).overrideAttrs (oldAttrs: {
+    meta.description = "${oldAttrs.meta.description} (without GDB)";
+  });
 
   qcachegrind = libsForQt5.callPackage ../development/tools/analysis/qcachegrind { };
 

commit d4766057138817a7bf92ef5bf05791c06e819f9b

valgrind-light: correctly override meta
This is broken due to Nix treating `a = { b.c = d; }` as `a = { b = { c
= d; }; }` and `overrideAttrs` isn't recursively merging attributes with
old ones.

Broken in 95e172d7c0f4fbdcc117760fe973507e61a6d550
https://github.com/NixOS/nixpkgs/pull/340144

pkgs/top-level/all-packages.nix
@@ -19123,7 +19123,7 @@ with pkgs;
     inherit (buildPackages.darwin) xnu bootstrap_cmds;
   };
   valgrind-light = (res.valgrind.override { gdb = null; }).overrideAttrs (oldAttrs: {
-    meta.description = "${oldAttrs.meta.description} (without GDB)";
+    meta = oldAttrs.meta // { description = "${oldAttrs.meta.description} (without GDB)"; };
   });
 
   qcachegrind = libsForQt5.callPackage ../development/tools/analysis/qcachegrind { };