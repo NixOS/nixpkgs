6c2526f831a9c88c3b4844081f055b9633ba8d35
DataHearth <dev@antoine-langlois.net>
2024-09-09 16:19:41 +0200

    insomnia: 9.0.0 -> 9.3.3

--- pkgs/development/web/insomnia/default.nix
+++ pkgs/development/web/insomnia/default.nix
@@ -2,74 +2,97 @@
   lib,
   stdenv,
   fetchurl,
-  appimageTools
+  appimageTools,
 }:
 let
   pname = "insomnia";
-  version = "9.0.0";
+  version = "9.3.3";
 
-  src = fetchurl {
-    x86_64-darwin = {
-      url = "https://github.com/Kong/insomnia/releases/download/core%40${version}/Insomnia.Core-${version}.dmg";
-      hash = "sha256-QIArPdThQcNTUgrXpWP8JHaZfrZ/6ztekIvzFdoWjsY=";
-    };
-    x86_64-linux = {
-      url = "https://github.com/Kong/insomnia/releases/download/core%40${version}/Insomnia.Core-${version}.AppImage";
-      hash = "sha256-2UiqopYmNxnDcIqQMn/H89ugvOtTWkHH4LrmKkQErSs=";
-    };
-  }.${stdenv.system} or (throw "Unsupported system: ${stdenv.system}");
+  src =
+    fetchurl
+      {
+        x86_64-darwin = {
+          url = "https://github.com/Kong/insomnia/releases/download/core%40${version}/Insomnia.Core-${version}.dmg";
+          hash = "sha256-nRrBOHLng/hH2+QhL4JyjKxjYTNXoUk9finfVRbakSg=";
+        };
+        x86_64-linux = {
+          url = "https://github.com/Kong/insomnia/releases/download/core%40${version}/Insomnia.Core-${version}.AppImage";
+          hash = "sha256-VzZCGyu8r27/eD3dIQRIW2YQCdO9IB+r+p/OEIU412g=";
+        };
+      }
+      .${stdenv.system} or (throw "Unsupported system: ${stdenv.system}");
 
-  meta = with lib; {
+  meta = {
     homepage = "https://insomnia.rest";
     description = " The open-source, cross-platform API client for GraphQL, REST, WebSockets, SSE and gRPC. With Cloud, Local and Git storage.";
     mainProgram = "insomnia";
     changelog = "https://github.com/Kong/insomnia/releases/tag/core@${version}";
-    license = licenses.asl20;
-    platforms = [ "x86_64-linux" "x86_64-darwin" ];
-    maintainers = with maintainers; [ markus1189 kashw2 DataHearth ];
+    license = lib.licenses.asl20;
+    platforms = [
+      "x86_64-linux"
+      "x86_64-darwin"
+    ];
+    maintainers = with lib.maintainers; [
+      markus1189
+      kashw2
+      DataHearth
+    ];
   };
 in
-if stdenv.isDarwin then stdenv.mkDerivation {
-  inherit pname version src meta;
+if stdenv.isDarwin then
+  stdenv.mkDerivation {
+    inherit
+      pname
+      version
+      src
+      meta
+      ;
     sourceRoot = ".";
 
     unpackCmd = ''
-    echo "Creating temp directory"
-    mnt=$(TMPDIR=/tmp mktemp -d -t nix-XXXXXXXXXX)
-    function finish {
-      echo "Ejecting temp directory"
-      /usr/bin/hdiutil detach $mnt -force
-      rm -rf $mnt
-    }
-    # Detach volume when receiving SIG "0"
-    trap finish EXIT
-    # Mount DMG file
-    echo "Mounting DMG file into \"$mnt\""
-    /usr/bin/hdiutil attach -nobrowse -mountpoint $mnt $curSrc
-    # Copy content to local dir for later use
-    echo 'Copying extracted content into "sourceRoot"'
-    cp -a $mnt/Insomnia.app $PWD/
-  '';
+      echo "Creating temp directory"
+      mnt=$(TMPDIR=/tmp mktemp -d -t nix-XXXXXXXXXX)
+      function finish {
+        echo "Ejecting temp directory"
+        /usr/bin/hdiutil detach $mnt -force
+        rm -rf $mnt
+      }
+      # Detach volume when receiving SIG "0"
+      trap finish EXIT
+      # Mount DMG file
+      echo "Mounting DMG file into \"$mnt\""
+      /usr/bin/hdiutil attach -nobrowse -mountpoint $mnt $curSrc
+      # Copy content to local dir for later use
+      echo 'Copying extracted content into "sourceRoot"'
+      cp -a $mnt/Insomnia.app $PWD/
+    '';
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p "$out/Applications"
-    mv Insomnia.app $out/Applications/
-    runHook postInstall
-  '';
-} else appimageTools.wrapType2 {
-  inherit pname version src meta;
+    installPhase = ''
+      runHook preInstall
+      mkdir -p "$out/Applications"
+      mv Insomnia.app $out/Applications/
+      runHook postInstall
+    '';
+  }
+else
+  appimageTools.wrapType2 {
+    inherit
+      pname
+      version
+      src
+      meta
+      ;
 
-  extraInstallCommands = let
-    appimageContents = appimageTools.extract {
-      inherit pname version src;
-    };
-  in ''
-    # Install XDG Desktop file and its icon
-    install -Dm444 ${appimageContents}/insomnia.desktop -t $out/share/applications
-    install -Dm444 ${appimageContents}/insomnia.png -t $out/share/pixmaps
-    # Replace wrong exec statement in XDG Desktop file
-    substituteInPlace $out/share/applications/insomnia.desktop \
-        --replace-fail 'Exec=AppRun --no-sandbox %U' 'Exec=insomnia'
-  '';
-}
+    extraInstallCommands =
+      let
+        appimageContents = appimageTools.extract { inherit pname version src; };
+      in
+      ''
+        # Install XDG Desktop file and its icon
+        install -Dm444 ${appimageContents}/insomnia.desktop -t $out/share/applications
+        install -Dm444 ${appimageContents}/insomnia.png -t $out/share/pixmaps
+        # Replace wrong exec statement in XDG Desktop file
+        substituteInPlace $out/share/applications/insomnia.desktop \
+            --replace-fail 'Exec=AppRun --no-sandbox %U' 'Exec=insomnia'
+      '';
+  }
