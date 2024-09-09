8cd10b74ee99008036c0cf54ea9cc39a7e12faba
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-09 13:31:10 -0300

    xine-lib: nixfmt-rfc-style

--- pkgs/by-name/xi/xine-lib/package.nix
+++ pkgs/by-name/xi/xine-lib/package.nix
@@ -1,32 +1,33 @@
-{ lib
-, stdenv
-, fetchhg
-, aalib
-, alsa-lib
-, autoreconfHook
-, ffmpeg
-, flac
-, libGL
-, libGLU
-, libX11
-, libXext
-, libXinerama
-, libXv
-, libcaca
-, libcdio
-, libmng
-, libmpcdec
-, libpulseaudio
-, libtheora
-, libv4l
-, libvorbis
-, libxcb
-, ncurses
-, perl
-, pkg-config
-, speex
-, vcdimager
-, zlib
+{
+  lib,
+  aalib,
+  alsa-lib,
+  autoreconfHook,
+  fetchhg,
+  ffmpeg,
+  flac,
+  libGL,
+  libGLU,
+  libX11,
+  libXext,
+  libXinerama,
+  libXv,
+  libcaca,
+  libcdio,
+  libmng,
+  libmpcdec,
+  libpulseaudio,
+  libtheora,
+  libv4l,
+  libvorbis,
+  libxcb,
+  ncurses,
+  perl,
+  pkg-config,
+  speex,
+  stdenv,
+  vcdimager,
+  zlib,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -39,7 +40,11 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-O5lIYmNC2TpoY4QbPMsDWxNOoxdw61967Q4QG9d/+Bg=";
   };
 
-  outputs = [ "out" "dev" "man" ];
+  outputs = [
+    "out"
+    "dev"
+    "man"
+  ];
 
   nativeBuildInputs = [
     autoreconfHook
@@ -55,8 +60,12 @@ stdenv.mkDerivation (finalAttrs: {
     libGL
     libGLU
     libX11
+    libX11
+    libXext
     libXext
     libXinerama
+    libXinerama
+    libXv
     libXv
     libcaca
     libcdio
@@ -67,16 +76,12 @@ stdenv.mkDerivation (finalAttrs: {
     libv4l
     libvorbis
     libxcb
+    libxcb
     ncurses
     perl
     speex
     vcdimager
     zlib
-    libX11
-    libXext
-    libXinerama
-    libXv
-    libxcb
   ];
 
   env.NIX_LDFLAGS = "-lxcb-shm";
@@ -88,7 +93,11 @@ stdenv.mkDerivation (finalAttrs: {
   meta = {
     homepage = "https://xine.sourceforge.net/";
     description = "High-performance, portable and reusable multimedia playback engine";
-    license = with lib.licenses; [ gpl2Plus lgpl2Plus ];
+    license = with lib.licenses; [
+      gpl2Plus
+      lgpl2Plus
+    ];
+    # No useful mainProgram
     maintainers = with lib.maintainers; [ AndersonTorres ];
     platforms = lib.platforms.linux;
   };
