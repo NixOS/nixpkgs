1882c39f9318d0ce7c1a0bc5147880523f202380
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-10 00:40:47 +0000

    containerd: 1.7.21 -> 1.7.22

--- pkgs/applications/virtualization/containerd/default.nix
+++ pkgs/applications/virtualization/containerd/default.nix
@@ -11,13 +11,13 @@
 
 buildGoModule rec {
   pname = "containerd";
-  version = "1.7.21";
+  version = "1.7.22";
 
   src = fetchFromGitHub {
     owner = "containerd";
     repo = "containerd";
     rev = "v${version}";
-    hash = "sha256-cL1RKFg+B2gTPMg963DKup5BCLLgF9t9VZn2WlmmWPI=";
+    hash = "sha256-8IHBKai4PvvTuHPDTgx9wFEBzz4MM7Mwo8Q/bzFRzfk=";
   };
 
   vendorHash = null;
