2a2b88eb557c655c2b4960a8d8f3e23a05a3472c
Wolfgang Walther <walther@technowledgy.de>
2024-09-11 19:28:11 +0200

    auto-patchelf: fix test for hook
    Fix suggested by Yann Hamdaoui in
    https://github.com/NixOS/nixpkgs/pull/340858#issuecomment-2344155304.

--- pkgs/test/auto-patchelf-hook/package.nix
+++ pkgs/test/auto-patchelf-hook/package.nix
@@ -60,7 +60,6 @@ stdenv.mkDerivation {
   # Additional phase performing the actual test.
   installCheckPhase =
     let allDeps = runtimeDependencies ++ [
-        (lib.getLib stdenv.cc.libc)
         (lib.getLib freetype)
       ];
     in
