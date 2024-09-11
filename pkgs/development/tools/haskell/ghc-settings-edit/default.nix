2beaad63c6a46a9ed4ae7b61f702efa1678cd10d
sternenseemann <sternenseemann@systemli.org>
2024-09-11 13:40:21 +0200

    haskellPackages.ghc-settings-edit: build using Cabal
    This allows us to reuse our existing infrastructure which alreday
    contains workarounds for various issues. Prior to this,
    ghc-settings-edit wouldn't compile with musl for some
    reason.

--- pkgs/development/tools/haskell/ghc-settings-edit/default.nix
+++ pkgs/development/tools/haskell/ghc-settings-edit/default.nix
@@ -1,33 +1,31 @@
 {
-  stdenv,
-  ghc,
+  mkDerivation,
+  base,
+  containers,
   lib,
 }:
 
-stdenv.mkDerivation {
+mkDerivation {
   pname = "ghc-settings-edit";
   version = "0.1.0";
-  # See the source code for an explanation
-  src = ./ghc-settings-edit.lhs;
-  dontUnpack = true;
-  dontBuild = true;
-
-  nativeBuildInputs = [ ghc ];
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p "$out/bin"
-    ${ghc.targetPrefix}ghc --make "$src" -o "$out/bin/ghc-settings-edit"
-
-    runHook postInstall
-  '';
-
-  meta = {
-    license = [
-      lib.licenses.mit
-      lib.licenses.bsd3
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./Setup.hs
+      ./ghc-settings-edit.lhs
+      ./ghc-settings-edit.cabal
     ];
-    platforms = lib.platforms.all;
-    description = "Tool for editing GHC's settings file";
   };
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base
+    containers
+  ];
+  license = [
+    lib.licenses.mit
+    lib.licenses.bsd3
+  ];
+  description = "Tool for editing GHC's settings file";
+  mainProgram = "ghc-settings-edit";
 }
