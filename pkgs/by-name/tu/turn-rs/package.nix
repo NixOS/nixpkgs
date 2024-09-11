15c45598e232e37cc140fa3381bd3412a266e918
wxt <3264117476@qq.com>
2024-09-11 10:51:36 +0800

    turn-rs: init at 3.1.0

---
+++ pkgs/by-name/tu/turn-rs/package.nix
@@ -0,0 +1,32 @@
+{
+  rustPlatform,
+  lib,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "turn-rs";
+  version = "3.1.0";
+
+  src = fetchFromGitHub {
+    owner = "mycrl";
+    repo = "turn-rs";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-uXMRDgSHrwT6+kejWRSE1WjXO8LaOR+fnffIXcL3A4I=";
+  };
+
+  cargoHash = "sha256-gO2vuOQMvl6KYp529k3CYDyma5ECzOr/lcSvP4OpUUo=";
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Pure rust implemented turn server";
+    homepage = "https://github.com/mycrl/turn-rs";
+    changelog = "https://github.com/mycrl/turn-rs/releases/tag/v${version}";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "turn-server";
+    maintainers = with lib.maintainers; [ bot-wxt1221 ];
+    platforms = lib.platforms.linux;
+  };
+}
