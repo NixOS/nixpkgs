1e9f0328783c77480b690fd589872e43c6df20e1
Emily <vcs@emily.moe>
2024-09-12 11:20:59 +0100

    llvmPackages_{14,15,16,17}.lldb: use SWIG 4
    The patches are already included in LLVM 16 and 17.

--- pkgs/development/compilers/llvm/common/lldb.nix
+++ pkgs/development/compilers/llvm/common/lldb.nix
@@ -5,8 +5,7 @@
 , cmake
 , zlib
 , ncurses
-, swig3
-, swig4
+, swig
 , which
 , libedit
 , libxml2
@@ -42,7 +41,6 @@ let
     name = if lib.versionAtLeast release_version "18" then "lldb-dap" else "lldb-vscode";
     version = if lib.versionAtLeast release_version "18" then "0.2.0" else "0.1.0";
   };
-  swig = if lib.versionAtLeast release_version "18" then swig4 else swig3;
 in
 
 stdenv.mkDerivation (rec {
