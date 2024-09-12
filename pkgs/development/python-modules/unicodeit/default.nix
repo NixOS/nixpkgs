bdd81e2bbca75e64f02c7e16e508e98f958e434f
nicoo <nicoo@mur.at>
2024-09-12 12:17:56 +0000

    python3.pkgs.unicodeit: define CLI entrypoint

--- pkgs/development/python-modules/unicodeit/default.nix
+++ pkgs/development/python-modules/unicodeit/default.nix
@@ -2,9 +2,12 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   pytestCheckHook,
+  runCommand,
 
   setuptools,
+  unicodeit,
 }:
 buildPythonPackage rec {
   pname = "unicodeit";
@@ -18,6 +21,14 @@ buildPythonPackage rec {
     hash = "sha256-NeR3fGDbOzwyq85Sep9KuUiARCvefN6l5xcb8D/ntHE=";
   };
 
+  patches = [
+    (fetchpatch {
+      # Defines a CLI entry point, so `setuptools` generates an `unicodeit` executable
+      url = "https://github.com/svenkreiss/unicodeit/pull/79.patch";
+      hash = "sha256-mAhmU17K0adEFFAIf7ZeJ/cNohrzrL+sol7gYfWbPGo=";
+    })
+  ];
+
   build-system = [ setuptools ];
   nativeCheckInputs = [ pytestCheckHook ];
 
@@ -26,8 +37,20 @@ buildPythonPackage rec {
     "unicodeit.cli"
   ];
 
+  passthru.tests.entrypoint =
+    runCommand "python3-unicodeit-test-entrypoint"
+      {
+        nativeBuildInputs = [ unicodeit ];
+        preferLocalBuild = true;
+      }
+      ''
+        [[ "$(unicodeit "\BbbR")" = "ℝ" ]]
+        touch $out
+      '';
+
   meta = {
     description = "Converts LaTeX tags to unicode";
+    mainProgram = "unicodeit";
     homepage = "https://github.com/svenkreiss/unicodeit";
     licenses = with lib.licenses; [
       lppl13c
