89c71a569a3b44cf307e543c55f24520f13c0a86
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-12 08:44:28 +0200

    python312Packages.sfrbox-api: 0.0.10 -> 0.0.11
    Diff: https://github.com/hacf-fr/sfrbox-api/compare/refs/tags/v0.0.10...v0.0.11
    
    Changelog: https://github.com/hacf-fr/sfrbox-api/releases/tag/v0.0.11

--- pkgs/development/python-modules/sfrbox-api/default.nix
+++ pkgs/development/python-modules/sfrbox-api/default.nix
@@ -5,8 +5,8 @@
   defusedxml,
   fetchFromGitHub,
   httpx,
+  mashumaro,
   poetry-core,
-  pydantic,
   pytest-asyncio,
   pytestCheckHook,
   pythonOlder,
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "sfrbox-api";
-  version = "0.0.10";
+  version = "0.0.11";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "hacf-fr";
     repo = "sfrbox-api";
     rev = "refs/tags/v${version}";
-    hash = "sha256-xvtusgqCseXAmiPQBFFZnRS9KmuhzHhZUAj5aaqyFrE=";
+    hash = "sha256-Ec3UOserFijBK6goyM6AMOekfLgjBq8l/9sMKYnj240=";
   };
 
   pythonRelaxDeps = [
@@ -37,7 +37,7 @@ buildPythonPackage rec {
   dependencies = [
     defusedxml
     httpx
-    pydantic
+    mashumaro
   ];
 
   passthru.optional-dependencies = {
