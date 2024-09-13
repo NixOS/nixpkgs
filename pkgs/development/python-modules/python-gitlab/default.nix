35bc9b0e162eaf50935626e6fd9b87fd18b935ee
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-13 22:23:53 +0000

    python312Packages.python-gitlab: 4.10.0 -> 4.11.1

--- pkgs/development/python-modules/python-gitlab/default.nix
+++ pkgs/development/python-modules/python-gitlab/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "python-gitlab";
-  version = "4.10.0";
+  version = "4.11.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "python_gitlab";
     inherit version;
-    hash = "sha256-hvmcGRUIji0lc4F6yov2urXk8S1CsE72st8qu1KNV/0=";
+    hash = "sha256-evovnDBhi8Pa7pXSGGoG4c8Yyi+peJDrVf2N3E4zmBI=";
   };
 
   nativeBuildInputs = [ setuptools ];
