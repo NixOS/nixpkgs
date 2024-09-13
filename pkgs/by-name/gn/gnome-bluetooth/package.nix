7a67152f339d180c55532e453ab2f5584fa1b708
Bobby Rong <rjl931189261@126.com>
2024-09-13 13:23:48 +0000

    gnome-bluetooth: 47.beta → 47.0
    https://gitlab.gnome.org/GNOME/gnome-bluetooth/-/compare/47.beta...47.0
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/by-name/gn/gnome-bluetooth/package.nix
+++ pkgs/by-name/gn/gnome-bluetooth/package.nix
@@ -28,7 +28,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-bluetooth";
-  version = "47.beta";
+  version = "47.0";
 
   # TODO: split out "lib"
   outputs = [
@@ -40,7 +40,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-bluetooth/${lib.versions.major finalAttrs.version}/gnome-bluetooth-${finalAttrs.version}.tar.xz";
-    hash = "sha256-oNKlNsdrYKdR4mugnACMZBEje8sHnqoARw2wObWHHk0=";
+    hash = "sha256-eqQG08txFS9SW9vJeI4RgWIEQIGXtkEcbuJVVLxMtnM=";
   };
 
   nativeBuildInputs = [
