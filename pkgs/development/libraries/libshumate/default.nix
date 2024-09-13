4b5ad0c80d5e1fe2cc0b9543eed8df1da6c22195
Bobby Rong <rjl931189261@126.com>
2024-09-13 13:24:48 +0000

    libshumate: 1.3.beta → 1.3.0
    https://gitlab.gnome.org/GNOME/libshumate/-/compare/1.3.beta...1.3.0
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/libshumate/default.nix
+++ pkgs/development/libraries/libshumate/default.nix
@@ -22,20 +22,15 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libshumate";
-  version = "1.3.beta";
+  version = "1.3.0";
 
   outputs = [ "out" "dev" "devdoc" ];
   outputBin = "devdoc"; # demo app
 
   src = fetchurl {
     url = "mirror://gnome/sources/libshumate/${lib.versions.majorMinor finalAttrs.version}/libshumate-${finalAttrs.version}.tar.xz";
-    hash = "sha256-VMOsrq0FRul8tMY6++U8ydK2irVCT/g/B7HZIWuuKys=";
+    hash = "sha256-giem6Cgc3hIjKJT++Ddg1E+maznvAzxh7ZNKhsbcddQ=";
   };
-
-  patches = [
-    # https://gitlab.gnome.org/GNOME/libshumate/-/issues/81
-    ./json-glib-192-newline-escape.patch
-  ];
 
   depsBuildBuild = [
     # required to find native gi-docgen when cross compiling
