424bbc430e1dbde07878ccf85733f0d7671b2773
David McFarland <corngood@gmail.com>
2024-09-13 16:26:28 -0300

    addNuGetDeps: fix fetch-deps output path handling

--- pkgs/build-support/dotnet/add-nuget-deps/default.nix
+++ pkgs/build-support/dotnet/add-nuget-deps/default.nix
@@ -66,18 +66,19 @@ attrs
             src = ./fetch-deps.sh;
             isExecutable = true;
             inherit cacert;
-            defaultDepsFile =
-              # Wire in the depsFile such that running the script with no args
-              # runs it agains the correct deps file by default.
-              # Note that toString is necessary here as it results in the path at
-              # eval time (i.e. to the file in your local Nixpkgs checkout) rather
-              # than the Nix store path of the path after it's been imported.
-              if lib.isPath nugetDeps && !lib.isStorePath nugetDeps then
-                toString nugetDeps
-              else
-                ''$(mktemp -t "${finalAttrs.pname or finalPackage.name}-deps-XXXXXX.nix")'';
             nugetToNix = nuget-to-nix;
           };
+
+          defaultDepsFile =
+            # Wire in the depsFile such that running the script with no args
+            # runs it agains the correct deps file by default.
+            # Note that toString is necessary here as it results in the path at
+            # eval time (i.e. to the file in your local Nixpkgs checkout) rather
+            # than the Nix store path of the path after it's been imported.
+            if lib.isPath nugetDeps && !lib.isStorePath nugetDeps then
+              toString nugetDeps
+            else
+              ''$(mktemp -t "${finalAttrs.pname or finalPackage.name}-deps-XXXXXX.nix")'';
 
         in
         writeShellScript "${finalPackage.name}-fetch-deps" ''
@@ -85,8 +86,12 @@ attrs
 
           echo 'fetching dependencies for' ${lib.escapeShellArg finalPackage.name} >&2
 
+          # this needs to be before TMPDIR is changed, so the output isn't deleted
+          # if it uses mktemp
+          depsFile=$(realpath "''${1:-${lib.escapeShellArg defaultDepsFile}}")
+
           export TMPDIR
-          TMPDIR=$(mktemp -d -t fetch-deps-${finalPackage.name}.XXXXXX)
+          TMPDIR=$(mktemp -d -t fetch-deps-${lib.escapeShellArg finalPackage.name}.XXXXXX)
           trap 'chmod -R +w "$TMPDIR" && rm -fr "$TMPDIR"' EXIT
 
           export NUGET_HTTP_CACHE_PATH=''${NUGET_HTTP_CACHE_PATH-~/.local/share/NuGet/v3-cache}
@@ -96,8 +101,8 @@ attrs
 
           cd "$TMPDIR"
 
-          NIX_BUILD_SHELL="${runtimeShell}" ${nix}/bin/nix-shell \
-            --pure --keep NUGET_HTTP_CACHE_PATH --run 'source "${innerScript}"' "${drv}"
+          NIX_BUILD_SHELL=${lib.escapeShellArg runtimeShell} ${nix}/bin/nix-shell \
+            --pure --keep NUGET_HTTP_CACHE_PATH --run 'source '${lib.escapeShellArg innerScript}' '"''${depsFile@Q}" "${drv}"
         '';
     };
 }
