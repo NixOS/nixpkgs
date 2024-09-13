de4388c609895d74e6ddd7b70729a65e5c8af9c7
Emily <vcs@emily.moe>
2024-09-13 19:10:19 +0100

    {cc-wrapper,gcc}: drop unused logic and patches

--- pkgs/development/compilers/gcc/all.nix
+++ pkgs/development/compilers/gcc/all.nix
@@ -3,11 +3,10 @@
 , gccStdenv
 , gcc7Stdenv
 , callPackage
-, isl_0_11, isl_0_14, isl_0_17, isl_0_20
+, isl_0_17, isl_0_20
 , libcCross
 , threadsCross
 , noSysDirs
-, cloog_0_18_0, cloog
 , lowPrio
 , wrapCC
 }@args:
@@ -27,14 +26,8 @@ let
         threadsCross = if stdenv.targetPlatform != stdenv.buildPlatform then threadsCross else { };
         isl = if       stdenv.isDarwin then null
               else if    atLeast "9"   then isl_0_20
-              else if    atLeast "7"   then isl_0_17
-              else if    atLeast "6"   then (if stdenv.targetPlatform.isRedox then isl_0_17 else isl_0_14)
-              else            /* "5" */     isl_0_11;
-      } // lib.optionalAttrs (!(atLeast "6")) {
-        cloog = if stdenv.isDarwin
-                then null
-                else          /* 5 */       cloog_0_18_0;
-      } // lib.optionalAttrs (atLeast "6" && !(atLeast "9")) {
+              else    /* atLeast "7" */     isl_0_17;
+      } // lib.optionalAttrs (!(atLeast "9")) {
         # gcc 10 is too strict to cross compile gcc <= 8
         stdenv = if (stdenv.targetPlatform != stdenv.buildPlatform) && stdenv.cc.isGNU then gcc7Stdenv else stdenv;
       })));
