a4620f5b836ae64596298a3437cf89ea6660a2ce
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-13 13:27:06 +0200

    python312Packages.mypy-boto3-cognito-idp: 1.35.0 -> 1.35.18

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -314,8 +314,8 @@ rec {
       "sha256-UVEJn/VNbYEIRPHV9CuDI0Hos5POiMQThiN4OlncQIE=";
 
   mypy-boto3-cognito-idp =
-    buildMypyBoto3Package "cognito-idp" "1.35.0"
-      "sha256-Ym01lZnDAGNqFNBkYKj34zD66CLpWS471cVGCLmp17A=";
+    buildMypyBoto3Package "cognito-idp" "1.35.18"
+      "sha256-StmODomtTdvtjYL54eNQBWWuVLozMB+sowpZKeGsYX0=";
 
   mypy-boto3-cognito-sync =
     buildMypyBoto3Package "cognito-sync" "1.35.0"
