d09dbe2d7f31aa865bd2e6b963d52df0dc231323
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-09-13 19:50:25 +0000

    ipxe: move to by-name
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8948,8 +8948,6 @@ with pkgs;
 
   ipv6calc = callPackage ../tools/networking/ipv6calc { };
 
-  ipxe = callPackage ../tools/misc/ipxe { };
-
   irker = callPackage ../servers/irker { };
 
   iroh = callPackage ../applications/networking/iroh { };
