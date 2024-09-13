39c8fd70023c39b5e138acc9a298f0121ce77a3c
Marie Ramlow <me@nycode.dev>
2024-09-13 15:30:49 +0200

    kdePackages.kaccounts-integration: add signon-ui to buildInputs

--- pkgs/kde/gear/kaccounts-integration/default.nix
+++ pkgs/kde/gear/kaccounts-integration/default.nix
@@ -1,9 +1,11 @@
 {
   mkKdeDerivation,
   intltool,
+  signon-ui,
 }:
 mkKdeDerivation {
   pname = "kaccounts-integration";
 
   propagatedNativeBuildInputs = [intltool];
+  extraBuildInputs = [signon-ui];
 }
