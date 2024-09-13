0ed7e74523d93bf14823f8d097db1e3e35521fc0
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-09-13 19:50:46 +0000

    ipxe: format with nixfmt-rfc-style
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/by-name/ip/ipxe/package.nix
+++ pkgs/by-name/ip/ipxe/package.nix
@@ -1,41 +1,63 @@
-{ stdenv, lib, fetchFromGitHub, unstableGitUpdater, buildPackages
-, gnu-efi, mtools, openssl, perl, xorriso, xz
-, syslinux ? null
-, embedScript ? null
-, additionalTargets ? {}
-, additionalOptions ? []
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  unstableGitUpdater,
+  buildPackages,
+  gnu-efi,
+  mtools,
+  openssl,
+  perl,
+  xorriso,
+  xz,
+  syslinux ? null,
+  embedScript ? null,
+  additionalTargets ? { },
+  additionalOptions ? [ ],
 }:
 
 let
-  targets = additionalTargets // lib.optionalAttrs stdenv.isx86_64 {
-    "bin-x86_64-efi/ipxe.efi" = null;
-    "bin-x86_64-efi/ipxe.efirom" = null;
-    "bin-x86_64-efi/ipxe.usb" = "ipxe-efi.usb";
-    "bin-x86_64-efi/snp.efi" = null;
-  } // lib.optionalAttrs stdenv.hostPlatform.isx86 {
-    "bin/ipxe.dsk" = null;
-    "bin/ipxe.usb" = null;
-    "bin/ipxe.iso" = null;
-    "bin/ipxe.lkrn" = null;
-    "bin/undionly.kpxe" = null;
-  } // lib.optionalAttrs stdenv.isAarch32 {
-    "bin-arm32-efi/ipxe.efi" = null;
-    "bin-arm32-efi/ipxe.efirom" = null;
-    "bin-arm32-efi/ipxe.usb" = "ipxe-efi.usb";
-    "bin-arm32-efi/snp.efi" = null;
-  } // lib.optionalAttrs stdenv.isAarch64 {
-    "bin-arm64-efi/ipxe.efi" = null;
-    "bin-arm64-efi/ipxe.efirom" = null;
-    "bin-arm64-efi/ipxe.usb" = "ipxe-efi.usb";
-    "bin-arm64-efi/snp.efi" = null;
-  };
+  targets =
+    additionalTargets
+    // lib.optionalAttrs stdenv.isx86_64 {
+      "bin-x86_64-efi/ipxe.efi" = null;
+      "bin-x86_64-efi/ipxe.efirom" = null;
+      "bin-x86_64-efi/ipxe.usb" = "ipxe-efi.usb";
+      "bin-x86_64-efi/snp.efi" = null;
+    }
+    // lib.optionalAttrs stdenv.hostPlatform.isx86 {
+      "bin/ipxe.dsk" = null;
+      "bin/ipxe.usb" = null;
+      "bin/ipxe.iso" = null;
+      "bin/ipxe.lkrn" = null;
+      "bin/undionly.kpxe" = null;
+    }
+    // lib.optionalAttrs stdenv.isAarch32 {
+      "bin-arm32-efi/ipxe.efi" = null;
+      "bin-arm32-efi/ipxe.efirom" = null;
+      "bin-arm32-efi/ipxe.usb" = "ipxe-efi.usb";
+      "bin-arm32-efi/snp.efi" = null;
+    }
+    // lib.optionalAttrs stdenv.isAarch64 {
+      "bin-arm64-efi/ipxe.efi" = null;
+      "bin-arm64-efi/ipxe.efirom" = null;
+      "bin-arm64-efi/ipxe.usb" = "ipxe-efi.usb";
+      "bin-arm64-efi/snp.efi" = null;
+    };
 in
 
 stdenv.mkDerivation rec {
   pname = "ipxe";
   version = "1.21.1-unstable-2024-08-15";
 
-  nativeBuildInputs = [ gnu-efi mtools openssl perl xorriso xz ] ++ lib.optional stdenv.hostPlatform.isx86 syslinux;
+  nativeBuildInputs = [
+    gnu-efi
+    mtools
+    openssl
+    perl
+    xorriso
+    xz
+  ] ++ lib.optional stdenv.hostPlatform.isx86 syslinux;
   depsBuildBuild = [ buildPackages.stdenv.cc ];
 
   strictDeps = true;
@@ -52,15 +74,18 @@ stdenv.mkDerivation rec {
   ''; # calling syslinux on a FAT image isn't going to work
 
   # not possible due to assembler code
-  hardeningDisable = [ "pic" "stackprotector" ];
+  hardeningDisable = [
+    "pic"
+    "stackprotector"
+  ];
 
   env.NIX_CFLAGS_COMPILE = "-Wno-error";
 
-  makeFlags =
-    [ "ECHO_E_BIN_ECHO=echo" "ECHO_E_BIN_ECHO_E=echo" # No /bin/echo here.
-      "CROSS=${stdenv.cc.targetPrefix}"
-    ] ++ lib.optional (embedScript != null) "EMBED=${embedScript}";
-
+  makeFlags = [
+    "ECHO_E_BIN_ECHO=echo"
+    "ECHO_E_BIN_ECHO_E=echo" # No /bin/echo here.
+    "CROSS=${stdenv.cc.targetPrefix}"
+  ] ++ lib.optional (embedScript != null) "EMBED=${embedScript}";
 
   enabledOptions = [
     "PING_CMD"
@@ -69,15 +94,18 @@ stdenv.mkDerivation rec {
     "DOWNLOAD_PROTO_HTTPS"
   ] ++ additionalOptions;
 
-  configurePhase = ''
-    runHook preConfigure
-    for opt in ${lib.escapeShellArgs enabledOptions}; do echo "#define $opt" >> src/config/general.h; done
-    substituteInPlace src/Makefile.housekeeping --replace '/bin/echo' echo
-  '' + lib.optionalString stdenv.hostPlatform.isx86 ''
-    substituteInPlace src/util/genfsimg --replace /usr/lib/syslinux ${syslinux}/share/syslinux
-  '' + ''
-    runHook postConfigure
-  '';
+  configurePhase =
+    ''
+      runHook preConfigure
+      for opt in ${lib.escapeShellArgs enabledOptions}; do echo "#define $opt" >> src/config/general.h; done
+      substituteInPlace src/Makefile.housekeeping --replace '/bin/echo' echo
+    ''
+    + lib.optionalString stdenv.hostPlatform.isx86 ''
+      substituteInPlace src/util/genfsimg --replace /usr/lib/syslinux ${syslinux}/share/syslinux
+    ''
+    + ''
+      runHook postConfigure
+    '';
 
   preBuild = "cd src";
 
@@ -87,10 +115,11 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     mkdir -p $out
-    ${lib.concatStringsSep "\n" (lib.mapAttrsToList (from: to:
-      if to == null
-      then "cp -v ${from} $out"
-      else "cp -v ${from} $out/${to}") targets)}
+    ${lib.concatStringsSep "\n" (
+      lib.mapAttrsToList (
+        from: to: if to == null then "cp -v ${from} $out" else "cp -v ${from} $out/${to}"
+      ) targets
+    )}
 
     # Some PXE constellations especially with dnsmasq are looking for the file with .0 ending
     # let's provide it as a symlink to be compatible in this case.
@@ -105,10 +134,10 @@ stdenv.mkDerivation rec {
     tagPrefix = "v";
   };
 
-  meta = with lib;
-    { description = "Network boot firmware";
-      homepage = "https://ipxe.org/";
-      license = licenses.gpl2Only;
-      platforms = platforms.linux;
-    };
+  meta = with lib; {
+    description = "Network boot firmware";
+    homepage = "https://ipxe.org/";
+    license = licenses.gpl2Only;
+    platforms = platforms.linux;
+  };
 }
