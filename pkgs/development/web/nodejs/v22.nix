c23b03c781d650dc70cd5a83cae358b3d2df3abe
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-09-14 01:09:23 +0200

    [24.05 backport] nodejs changes (#336570)
    * nodejs_18: pin to Python 3.11, fix tests with OpenSSL 3.0.14
    
    (cherry picked from commit 7c1177c514661cdd8b9592905a234db6068f0d0c)
    
    * nodejs: use upstream patch instead of custom one
    
    (cherry picked from commit f75d50324faf773abf9c66c04fa1ff1fe2bf0919)
    
    * nodejs_22: 22.4.1 -> 22.5.1
    
    (cherry picked from commit 070791fba6ce4cf0ebb16e490f25397bcc562e59)
    
    * nodejs: use .finalPackage for ease of overriding
    
    Previously, the nodejs `passthru.pkgs` attribute refers to the package
    fixed point derived in the current context, without considering
    potential overrides to the main nodejs package.
    
    This is fixed here with the `.finalPackage` attribute, which refers to
    the final package fixed point, after potential overrides.
    
    For example, previously, after overriding nodejs with `nodejs.override`,
    `nodejs.pkgs` would still refer to the original package, instead of the
    overridden one. After this fix, `nodejs.pkgs` would be correctly based
    on the overridden `nodejs` package.
    
    (cherry picked from commit ee9a623f897b154ddce0178e1c7daa9793fc14ab)
    
    * nodejs: fix tests and disable parallel checking
    
    (cherry picked from commit d7f46fbb5c40e7e9a165adf4f82669ae35c5b055)
    
    * nodejs: disable failing tests on x86_64-darwin
    
    (cherry picked from commit 31d238110c720ab467cbd7adb715ebca76f9e09d)
    
    * nodejs: skip tests failing in sandbox in all platforms
    
    ce685a8422 added list of tests failing on sandbox, but for some reason
    only for Darwin. We should skip those tests on all platforms otherwise
    OfBorg builds are failing.
    
    (cherry picked from commit f5846d25a1335267bc8bad633ffcbdf2109b18dc)
    
    * nodejs_20: 20.15.1 -> 20.16.0
    
    (cherry picked from commit 35b556d314ff99940619f4f16920542fd5b8f972)
    
    * nodejs: 22.5.1 -> 22.6.0 (#333210)
    
    (cherry picked from commit 4c38d8a84621ed8c0a4f133ba167cb37f2ab0c9d)
    
    * nodejs_22: 22.6.0 -> 22.7.0 (#336556)
    
    (cherry picked from commit c455cc3170d68f05b4614c41883a7d417d1c16e0)
    
    * nodejs_20: 20.16.0 -> 20.17.0 (#336388)
    
    (cherry picked from commit 45db1ff7c1aa8219a83e3fd48034fdf58706fd00)
    
    * nodejs_22: 22.7.0 -> 22.8.0
    
    (cherry picked from commit 235ae9c70fbb260384cb6497f45c443002c5fed0)
    
    ---------
    
    Co-authored-by: K900 <me@0upti.me>
    Co-authored-by: Bryan Lai <bryanlais@gmail.com>
    Co-authored-by: Ivan Trubach <mr.trubach@icloud.com>

--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -1,4 +1,4 @@
-{ callPackage, fetchpatch2, openssl, python3, enableNpm ? true }:
+{ callPackage, openssl, python3, enableNpm ? true }:
 
 let
   buildNodejs = callPackage ./nodejs.nix {
@@ -12,8 +12,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "22.4.1";
-  sha256 = "sha256-ZfyFf1qoJWqvyQCzRMARXJrq4loCVB/Vzg29Tf0cX7k=";
+  version = "22.8.0";
+  sha256 = "f130e82176d1ee0702d99afc1995d0061bf8ed357c38834a32a08c9ef74f1ac7";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
