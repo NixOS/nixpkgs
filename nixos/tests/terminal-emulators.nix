d2cf5dda65bbf2cdacbcf652fee527e5987d39d9
OPNA2608 <opna2608@protonmail.com>
2024-09-14 20:21:20 +0200

    nixosTests.terminal-emulators: Disable lomiri-terminal-app colour test
    02a4d9a728489f78a8265d84c6fd6c1057f449d1 made the colour output of the terminal(?) in the icewm environment
    on x86_64 BGR instead of RGB, but not on aarch64. Just disable for now.

--- nixos/tests/terminal-emulators.nix
+++ nixos/tests/terminal-emulators.nix
@@ -62,6 +62,9 @@ let tests = {
       konsole.pkg = p: p.plasma5Packages.konsole;
 
       lomiri-terminal-app.pkg = p: p.lomiri.lomiri-terminal-app;
+      # after recent Mesa change, borked software rendering config under x86_64 icewm?
+      # BGR colour display on x86_64, RGB on aarch64
+      lomiri-terminal-app.colourTest = false;
 
       lxterminal.pkg = p: p.lxterminal;
 
