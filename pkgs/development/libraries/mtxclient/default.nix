bb5ea56c6d6a432dabe852f8ab057d015f93a938
rnhmjoj <rnhmjoj@inventati.org>
2024-09-14 14:51:04 +0200

    mtxclient: add rnhmjoj as maintainer

--- pkgs/development/libraries/mtxclient/default.nix
+++ pkgs/development/libraries/mtxclient/default.nix
@@ -56,7 +56,7 @@ stdenv.mkDerivation rec {
     description = "Client API library for the Matrix protocol";
     homepage = "https://github.com/Nheko-Reborn/mtxclient";
     license = licenses.mit;
-    maintainers = with maintainers; [ fpletz pstn ];
+    maintainers = with maintainers; [ fpletz pstn rnhmjoj ];
     platforms = platforms.all;
     # Should be fixable if a higher clang version is used, see:
     # https://github.com/NixOS/nixpkgs/pull/85922#issuecomment-619287177
