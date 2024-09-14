27ac4893992cb6ae02e96842686281446e0e0785
Lin Jian <me@linj.tech>
2024-09-15 03:44:42 +0800

    emacs.pkgs.elpaPackages.jinx: move to common overrides

--- pkgs/applications/editors/emacs/elisp-packages/elpa-packages.nix
+++ pkgs/applications/editors/emacs/elisp-packages/elpa-packages.nix
@@ -57,33 +57,6 @@ self: let
         else super.org-transclusion;
       rcirc-menu = markBroken super.rcirc-menu; # Missing file header
 
-      jinx = super.jinx.overrideAttrs (old: let
-        libExt = pkgs.stdenv.hostPlatform.extensions.sharedLibrary;
-      in {
-        dontUnpack = false;
-
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
-            pkgs.pkg-config
-        ];
-
-        buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.enchant2 ];
-
-        postBuild = ''
-          NIX_CFLAGS_COMPILE="$($PKG_CONFIG --cflags enchant-2) $NIX_CFLAGS_COMPILE"
-          $CC -shared -o jinx-mod${libExt} jinx-mod.c -lenchant-2
-        '';
-
-        postInstall = (old.postInstall or "") + "\n" + ''
-          outd=$out/share/emacs/site-lisp/elpa/jinx-*
-          install -m444 -t $outd jinx-mod${libExt}
-          rm $outd/jinx-mod.c $outd/emacs-module.h
-        '';
-
-        meta = old.meta // {
-          maintainers = [ lib.maintainers.DamienCassou ];
-        };
-      });
-
       plz = super.plz.overrideAttrs (
         old: {
           dontUnpack = false;
