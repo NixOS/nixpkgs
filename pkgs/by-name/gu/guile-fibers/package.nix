8b172bcb75a79b3de74e6392d990bbb892948b29
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-13 23:07:21 -0300

    guile-fibers: refactor
    - finalAttrs
    - hammer
    - strictDeps
    - guile team

--- pkgs/by-name/gu/guile-fibers/package.nix
+++ pkgs/by-name/gu/guile-fibers/package.nix
@@ -1,25 +1,24 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, autoreconfHook
-, guile
-, libevent
-, pkg-config
-, texinfo
+{
+  lib,
+  autoreconfHook,
+  fetchFromGitHub,
+  guile,
+  libevent,
+  pkg-config,
+  stdenv,
+  texinfo,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "guile-fibers";
   version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "wingo";
     repo = "fibers";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-jJKA5JEHsmqQ/IKb1aNmOtoVaGKNjcgTKyo5VCiJbXM=";
   };
-
-  strictDeps = true;
 
   nativeBuildInputs = [
     autoreconfHook
@@ -33,15 +32,15 @@ stdenv.mkDerivation rec {
     libevent
   ];
 
-  makeFlags = [
-    "GUILE_AUTO_COMPILE=0"
-  ];
+  makeFlags = [ "GUILE_AUTO_COMPILE=0" ];
 
-  meta = with lib; {
+  strictDeps = true;
+
+  meta = {
     homepage = "https://github.com/wingo/fibers";
     description = "Concurrent ML-like concurrency for Guile";
-    license = licenses.lgpl3Plus;
-    maintainers = with maintainers; [ vyp ];
-    platforms = guile.meta.platforms;
+    license = lib.licenses.lgpl3Plus;
+    maintainers = with lib.maintainers; [ vyp ];
+    inherit (guile.meta) platforms;
   };
-}
+})
