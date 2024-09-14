35203d7e9000a89da81c90edf08a64b086ec33b9
Orivej Desh <orivej@gmx.fr>
2024-09-14 13:17:42 +0000

    fmit: add orivej to maintainers

--- pkgs/applications/audio/fmit/default.nix
+++ pkgs/applications/audio/fmit/default.nix
@@ -55,6 +55,7 @@ mkDerivation rec {
     '';
     homepage = "http://gillesdegottex.github.io/fmit/";
     license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ orivej ];
     platforms = platforms.linux;
   };
 }
