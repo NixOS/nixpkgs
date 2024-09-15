be4319b9186a4b4140d9b09f7671f77009d066da
Lin Jian <me@linj.tech>
2024-09-16 05:12:04 +0800

    emacsPackages.mu4e: build package tarball in postBuild instead

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/mu4e/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/mu4e/package.nix
@@ -12,8 +12,7 @@ elpaBuild {
 
   # prepare a multi-file package tar archive according to info
   # "(elisp) Multi-file Packages" for elpaBuild to install
-  postUnpack = ''
-    pushd mu-*-mu4e
+  postBuild = ''
     local content_directory=$pname-$version
     mkdir $content_directory
     cp --verbose share/emacs/site-lisp/mu4e/*.el $content_directory/
@@ -21,7 +20,6 @@ elpaBuild {
     cp --verbose share/info/* $content_directory/
     src=$PWD/$content_directory.tar
     tar --create --verbose --file=$src $content_directory
-    popd
   '';
 
   ignoreCompilationError = false;
