e74dff45e1d2504f8e79c6d9caff76f5e2f0ea75
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-15 14:35:02 +0200

    python311Packages.llama-parse: 0.5.3 -> 0.5.5

--- pkgs/development/python-modules/llama-parse/default.nix
+++ pkgs/development/python-modules/llama-parse/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "llama-parse";
-  version = "0.5.3";
+  version = "0.5.5";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_parse";
     inherit version;
-    hash = "sha256-UjwldGujOM1OGYAGzbtkQtUBzJyiR7wkU3C87UQNgL0=";
+    hash = "sha256-ILAOCbFNG1ehsEDkvWFDbVH0Nza3tRADBypNBbA170Q=";
   };
 
   build-system = [ poetry-core ];
