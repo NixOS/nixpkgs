498bfee8f6befdf52a0ee40dd55c078acb4393ab
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-15 10:57:15 +0200

    python312Packages.aioitertools: refactor

--- pkgs/development/python-modules/aioitertools/default.nix
+++ pkgs/development/python-modules/aioitertools/default.nix
@@ -17,18 +17,18 @@
 buildPythonPackage rec {
   pname = "aioitertools";
   version = "0.12.0";
-  format = "pyproject";
+  pyproject = true;
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-wqkFW0+7dwX1YbnYYFPor10QzIRdIsMgCMQ0kLLY3Ws=";
   };
 
-  nativeBuildInputs = [ flit-core ];
+  build-system = [ flit-core ];
 
-  propagatedBuildInputs = lib.optionals (pythonOlder "3.10") [ typing-extensions ];
+  dependencies = lib.optionals (pythonOlder "3.10") [ typing-extensions ];
 
   nativeCheckInputs = [ unittestCheckHook ];
 
@@ -37,6 +37,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Implementation of itertools, builtins, and more for AsyncIO and mixed-type iterables";
     homepage = "https://aioitertools.omnilib.dev/";
+    changelog = "https://github.com/omnilib/aioitertools/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ teh ];
   };
