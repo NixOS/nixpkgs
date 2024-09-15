75594987c955031e014a9b04bb0997f16bf5b6c5
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-15 05:21:20 -0400

    sublime4-dev: 4175 -> 4178

--- pkgs/applications/editors/sublime/4/packages.nix
+++ pkgs/applications/editors/sublime/4/packages.nix
@@ -11,9 +11,9 @@ in
   } { };
 
   sublime4-dev = common {
-    buildVersion = "4175";
+    buildVersion = "4178";
     dev = true;
-    x64sha256 = "xncyxAaFJLLMko/iF6fhnpkOEHzD3nzWWGQCRK9srq4=";
-    aarch64sha256 = "oqz1HASwmv0B1T3ZQBdIOZD9wYcHbni8tovW7jGp3KM=";
+    x64sha256 = "2eTdb5MzXK3QbAEzl1yxURj4m/PqGHPVnHZV2WzD6Jc=";
+    aarch64sha256 = "NLLOB4WnujMx3+wf6Evi+yBWM6463EZoNL2wEdJA8BA=";
   } { };
 }
