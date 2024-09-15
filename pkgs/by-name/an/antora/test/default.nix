5e833a12b14168290812fca564f4b94ebb808bbc
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-09-15 19:28:51 +0200

    antora: verify core functionality with a minimal working example
    Link: https://github.com/NixOS/nixpkgs/pull/332341

---
+++ pkgs/by-name/an/antora/test/default.nix
@@ -0,0 +1,63 @@
+{
+  antora,
+  antora-ui-default,
+  gitMinimal,
+  lib,
+  stdenvNoCC,
+}:
+stdenvNoCC.mkDerivation {
+  name = "${antora.pname}-test";
+  src = ./minimal_working_example;
+
+  postPatch =
+    let
+      date = lib.escapeShellArg "1/1/1970 00:00:00 +0000";
+    in
+    ''
+      # > In order to use a local content repository with Antora, even when using
+      # > the worktree (HEAD), the repository must have at least one commit.
+      # >
+      # > -- https://docs.antora.org/antora/3.1/playbook/content-source-url
+      git init &&
+        GIT_AUTHOR_DATE=${date} \
+        GIT_AUTHOR_EMAIL= \
+        GIT_AUTHOR_NAME=Nixpkgs \
+        GIT_COMMITTER_DATE=${date} \
+        GIT_COMMITTER_EMAIL= \
+        GIT_COMMITTER_NAME=Nixpkgs \
+        git commit --allow-empty --allow-empty-message --message ""
+    '';
+
+  buildPhase =
+    let
+      playbook = builtins.toFile "antora-playbook.json" (
+        builtins.toJSON {
+          content.sources = [ { url = "~+"; } ];
+          runtime.log.failure_level = "warn";
+        }
+      );
+    in
+    ''
+      # The --to-dir and --ui-bundle-url options are not included in the
+      # playbook due to Antora and Nix limitations.
+      antora ${
+        lib.cli.toGNUCommandLineShell { } {
+          cache-dir = "$(mktemp --directory)";
+          to-dir = placeholder "out";
+          ui-bundle-url = "${antora-ui-default}/ui-bundle.zip";
+        }
+      } "${playbook}"
+    '';
+
+  nativeBuildInputs = [
+    antora
+    gitMinimal
+  ];
+
+  meta = {
+    description = "Reproducible Antora test framework";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.naho ];
+    platforms = lib.platforms.all;
+  };
+}
