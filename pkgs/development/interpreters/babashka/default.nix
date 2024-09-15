ba0125efd1781db9f228248e181e9bc6043918a1
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-15 00:43:13 +0000

    babashka-unwrapped: 1.3.191 -> 1.4.192

--- pkgs/development/interpreters/babashka/default.nix
+++ pkgs/development/interpreters/babashka/default.nix
@@ -9,11 +9,11 @@
 let
   babashka-unwrapped = buildGraalvmNativeImage rec {
     pname = "babashka-unwrapped";
-    version = "1.3.191";
+    version = "1.4.192";
 
     src = fetchurl {
       url = "https://github.com/babashka/babashka/releases/download/v${version}/babashka-${version}-standalone.jar";
-      sha256 = "sha256-xF0u2VO0xdNvRElhUb7atpvy2Y5T9/V1b92ffaOMga8=";
+      sha256 = "sha256-uVRk1/lfOXMSp3qOdeMoAFpwJaEBxF1MYtFOv364H7Y=";
     };
 
     graalvmDrv = graalvmCEPackages.graalvm-ce;
