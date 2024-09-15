21c140025f2af8a7f18d79ed84ff838cf46d7739
Eric Yen <eric@ericyen.com>
2024-09-15 02:06:18 -0700

    simplify runtime logic by moving it to the package

--- pkgs/by-name/li/linkwarden/package.nix
+++ pkgs/by-name/li/linkwarden/package.nix
@@ -1,19 +1,21 @@
-{ lib
-, fetchFromGitHub
-, mkYarnPackage
-, nodePackages
-, openssl
-, prisma-engines
-, python3
-, nodejs
-, ...
+{
+  lib,
+  fetchFromGitHub,
+  mkYarnPackage,
+  nodePackages,
+  openssl,
+  prisma-engines,
+  python3,
+  nodejs,
+  runCommand,
+  yarn,
+  writeShellScript,
 }:
 mkYarnPackage rec {
   pname = "linkwarden";
   version = "2.7.1";
   # yarnFlags = [ "--production=false" ];
 
-  # nativeBuildInputs = [nodePackages.node-gyp]; # needed for bcrypt
   extraBuildInputs = [
     nodePackages.prisma
     prisma-engines
@@ -27,7 +29,13 @@ mkYarnPackage rec {
   };
   pkgConfig = {
     bcrypt = {
-      nativeBuildInputs = [ nodePackages.node-gyp nodePackages.node-gyp-build nodePackages.node-pre-gyp nodejs python3 ];
+      nativeBuildInputs = [
+        nodePackages.node-gyp
+        nodePackages.node-gyp-build
+        nodePackages.node-pre-gyp
+        nodejs
+        python3
+      ];
       postInstall = ''
         yarn --offline run install --nodedir=${nodejs}
       '';
@@ -35,9 +43,15 @@ mkYarnPackage rec {
   };
   doDist = false;
   configurePhase = ''
+    export HOME=$(mktemp -d)
+    cat > .env << EOF
+    NEXTAUTH_SECRET=NIXPKGS_NEXTAUTH_SECRET
+    NEXTAUTH_URL=NIXPKGS_NEXTAUTH_URL
+    DATABASE_URL=postgres://NIXPKGS_DATABASE_URL
+    EOF
     cp -r $node_modules node_modules
-    chmod +w node_modules
-    export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+    chmod +w -R node_modules
+    export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
     export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
     export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
     export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
@@ -57,11 +71,46 @@ mkYarnPackage rec {
     runHook postInstall
   '';
 
+  passthru.withUserConfiguration =
+    {
+      authUrl,
+      secret ? null,
+      secretFile ? null,
+      databaseURI,
+    }:
+    {
+      preStart =
+        let
+          localSecret = if secret != null then secret else "cat ${secretFile}";
+        in
+        runCommand "${pname}-${version}-preStart" ''
+          export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          _SECRET=${localSecret}
+          cat > .env << EOF
+          NEXTAUTH_SECRET=$_SECRET
+          NEXTAUTH_URL=${lib.escapeShellArg authUrl}
+          DATABASE_URL=${lib.escapeShellArg databaseURI}
+          EOF
+          cp -r --no-preserve-mode ${placeholder "out"} $out
+
+          ${yarn}/bin/yarn --offline run prisma migrate deploy
+        '';
+      start = writeShellScript "${pname}-${version}-start" ''
+        cd ${passthru.preStart}
+        ${yarn}/bin/yarn start
+
+      '';
+    };
+
   meta = {
     description = "Self-hosted collaborative bookmark manager to collect, organize, and preserve webpages and articles";
     downloadPage = "https://github.com/linkwarden/linkwarden/releases";
     homepage = "https://linkwarden.app/";
-    license = lib.licenses.agpl3;
+    license = lib.licenses.agpl3Only;
     maintainers = [ lib.maintainers.ericthemagician ];
   };
 }
