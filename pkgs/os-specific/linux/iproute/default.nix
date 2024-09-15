98d8c29972cfa69ab4c095af6b2634ea2bc629b8
Alyssa Ross <hi@alyssa.is>
2024-09-15 22:29:22 +0200

    iproute2: apply patch for musl 1.2.5

--- pkgs/os-specific/linux/iproute/default.nix
+++ pkgs/os-specific/linux/iproute/default.nix
@@ -24,6 +24,11 @@ stdenv.mkDerivation rec {
       url = "https://lore.kernel.org/netdev/20240712191209.31324-2-contact@hacktivis.me/raw";
       hash = "sha256-X5BYSZBxcvdjtX1069a1GfcpdoVd0loSAe4xTpbCipA=";
     })
+    (fetchurl {
+      name = "musl-basename.patch";
+      url = "https://lore.kernel.org/netdev/20240804161054.942439-1-dilfridge@gentoo.org/raw";
+      hash = "sha256-47obv6mIn/HO47lt47slpTAFDxiQ3U/voHKzIiIGCTM=";
+    })
   ];
 
   postPatch = ''
