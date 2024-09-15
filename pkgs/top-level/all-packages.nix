7d0b98b4b5f4568249ac6cbf50b11fcdc7830a21
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-15 13:32:01 +0400

    thonny: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33608,8 +33608,6 @@ with pkgs;
 
   thokr = callPackage ../applications/misc/thokr { };
 
-  thonny = callPackage ../applications/editors/thonny { };
-
   betterbird-unwrapped = callPackage ../applications/networking/mailreaders/betterbird { };
   betterbird = wrapThunderbird betterbird-unwrapped {
     desktopName = "Betterbird";
