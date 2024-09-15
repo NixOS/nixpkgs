5addf7453fcf1b18de9c9e212b0a3d057560e01b
Eric Yen <eric@ericyen.com>
2024-09-15 13:40:43 -0700

    working set of changes. needs some changes for playwright-driver to return a version or something

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1443,6 +1443,7 @@
   ./services/web-apps/lanraragi.nix
   ./services/web-apps/lemmy.nix
   ./services/web-apps/limesurvey.nix
+  ./services/web-apps/linkwarden.nix
   ./services/web-apps/mainsail.nix
   ./services/web-apps/mastodon.nix
   ./services/web-apps/matomo.nix
--- nixos/modules/services/web-apps/linkwarden.nix
+++ nixos/modules/services/web-apps/linkwarden.nix
@@ -3,10 +3,34 @@
   pkgs,
   lib,
   ...
-}: let
+}:
+let
   cfg = config.services.linkwarden;
-  inherit (lib) mkIf mkOption mkEnableOption types literalExpression optional optionals optionalAttrs;
-in {
+  inherit (lib)
+    mkIf
+    mkOption
+    mkEnableOption
+    types
+    literalExpression
+    mkForce
+    mkMerge
+    optional
+    optionals
+    optionalAttrs
+    ;
+  database_information = {
+    DATABASE_URL = "postgres://linkwarden@localhost/linkwarden?host=/run/postgresql";
+  };
+
+  linkwarden = cfg.package.withUserConfiguration {
+    secret = cfg.secret;
+    # secretFile = cfg.secretFile;
+    authUrl = "https://${cfg.domain}/api/v1/auth";
+    databaseURI = database_information.DATABASE_URL;
+    dataStoragePath = cfg.data_path;
+  };
+in
+{
   options.services.linkwarden = {
     enable = mkEnableOption "linkwarden";
     package = mkOption {
@@ -14,6 +38,10 @@ in {
       default = pkgs.linkwarden;
       defaultText = "pkgs.linkwarden";
       description = "The linkwarden package to use.";
+    };
+    domain = mkOption {
+      type = types.str;
+      description = "FQDN for linkwarden";
     };
     host = mkOption {
       type = types.str;
@@ -34,9 +62,19 @@ in {
         description = lib.mdDoc "Create the database and database user locally.";
       };
     };
+    secret = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = "Secret used by linkwarden";
+    };
+    secretFile = mkOption {
+      type = types.nullOr types.path;
+      default = null;
+      description = "Path to file containing secret";
+    };
     settings = mkOption {
       type = types.attrs;
-      default = {};
+      default = { };
       description = lib.mdDoc "Environment variables to pass to linkwarden. See [documentation](https://docs.linkwarden.app/self-hosting/environment-variables)";
     };
     settingsFile = mkOption {
@@ -52,56 +90,100 @@ in {
       default = "/var/lib/linkwarden/data";
       description = "Path to store the application data";
     };
+    nginx = mkOption {
+      type = types.nullOr (
+        types.submodule (import ../web-servers/nginx/vhost-options.nix { inherit config lib; })
+      );
+      default = null;
+      example = literalExpression ''
+        {
+          serverAliases = [
+            "linkwarden.''${config.networking.domain}"
+          ];
+          enableACME = true;
+          forceHttps = true;
+        }
+      '';
+      description = ''
+        With this option, you can customize an nginx virtual host which already has sensible defaults for linkwarden.
+        Set to {} if you do not need any customization to the virtual host.
+        If enabled, then by default, the {option}`serverName` is
+        `''${host}`,
+        If this is set to null (the default), no nginx virtualHost will be configured.
+      '';
+    };
+
   };
   config = mkIf cfg.enable {
     systemd.services.linkwarden = {
-      wantedBy = ["multi-user.target"];
-      wants = optionals cfg.database.createLocally ["postgresql.service"];
-      path = [pkgs.nodejs pkgs.concurrently pkgs.nodePackages.ts-node pkgs.nodePackages.prisma pkgs.prisma-engines pkgs.openssl] ++ cfg.package.workspaceDependencies;
-      environment = let
-        database_information = {DATABASE_URL = "postgresql://linkwarden@localhost/linkwarden?host=/run/postgresql";};
-      in
-        {
-          HOME = "/var/lib/linkwarden";
-          PORT = builtins.toString cfg.port;
-          HOST = cfg.host;
-          STORAGE_FOLDER = cfg.data_path;
-          PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
-          PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-          PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-          PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
-          PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
-        }
-        // cfg.settings
-        // optionalAttrs cfg.database.createLocally database_information;
+      wantedBy = [ "multi-user.target" ];
+      wants = optionals cfg.database.createLocally [ "postgresql.service" ];
+      after = optionals cfg.database.createLocally [ "postgresql.service" ];
+      path = [
+        pkgs.nodejs
+        pkgs.concurrently
+        pkgs.nodePackages.ts-node
+        pkgs.nodePackages.prisma
+        pkgs.prisma-engines
+        pkgs.openssl
+        pkgs.playwright
+        pkgs.playwright-driver
+        pkgs.monolith
+      ] ++ cfg.package.workspaceDependencies;
+      environment = {
+        HOME = "/var/lib/linkwarden";
+        PORT = builtins.toString cfg.port;
+        HOST = cfg.host;
+        STORAGE_FOLDER = "data";
+        PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+        PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+        PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+        PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
+        PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+        PRISMA_ENGINES_CHECKSUM_IGNORE_MISSING = "1";
+        PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
+        PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+        PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
+        PLAYWRIGHT_NODEJS_PATH = "${pkgs.nodejs}/bin/node";
+        NIXPKGS_PLAYRIGHT_EXECUTABLE_PATH = "${pkgs.playwright-driver.browsers}/chromium-1091/chrome-linux/chrome";
+        # CRPATH = "${pkgs.chromium}/bin/chromium";
+      } // cfg.settings // optionalAttrs cfg.database.createLocally database_information;
 
-      preStart = ''
-        rm -rf /var/lib/private/linkwarden/src/
-        cp -r ${cfg.package} /var/lib/private/linkwarden/src
-        chmod u+w -R /var/lib/private/linkwarden/src
-        cd src
-        ${pkgs.yarn}/bin/yarn prisma migrate deploy
-      '';
-      script = ''
-        cd src
-        ${pkgs.yarn}/bin/yarn start
-      '';
+      # preStart = ''
+      #   rm -f /var/lib/linkwarden/src/data
+      #   ln -s "${cfg.data_path}" /var/lib/linkwarden/src/data
+      # '';
+
+      # script = ''
+      #  echo "Starting linkwarden"
+      #   ${pkgs.yarn}/bin/yarn start
+      # '';
+
       serviceConfig = {
         DynamicUser = true;
         StateDirectory = "linkwarden";
         WorkingDirectory = "/var/lib/linkwarden";
         PrivateTmp = true;
         EnvironmentFile = optional (cfg.settingsFile != null) cfg.settingsFile;
+        ExecStartPre = linkwarden.preStartScript;
+        ExecStart = linkwarden.startScript;
       };
     };
     services.postgresql = mkIf cfg.database.createLocally {
       enable = true;
-      ensureDatabases = ["linkwrden"];
+      ensureDatabases = [ "linkwarden" ];
       ensureUsers = [
         {
-          name = "linkwrden";
+          name = "linkwarden";
           ensureDBOwnership = true;
         }
+      ];
+    };
+    services.nginx = mkIf (cfg.nginx != null) {
+      enable = true;
+      virtualHosts."${cfg.domain}" = mkMerge [
+        cfg.nginx
+        { root = mkForce linkwarden.rootFolder; }
       ];
     };
   };
