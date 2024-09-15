bf429e57dc2c169da223548faf23fdbebd965636
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-14 20:17:58 -0400

    lxqt-build-tools: cleanup drv + nixfmt-rfc-style

--- pkgs/desktops/lxqt/lxqt-build-tools/default.nix
+++ pkgs/desktops/lxqt/lxqt-build-tools/default.nix
@@ -1,42 +1,41 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, cmake
-, pkg-config
-, pcre
-, qtbase
-, glib
-, perl
-, wrapQtAppsHook
-, gitUpdater
-, version ? "2.0.0"
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  pkg-config,
+  pcre,
+  qtbase,
+  glib,
+  perl,
+  wrapQtAppsHook,
+  gitUpdater,
+  version ? "2.0.0",
 }:
-
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "lxqt-build-tools";
   inherit version;
 
+  setupHook = ./setup-hook.sh;
+  passthru.updateScript = gitUpdater { };
+
   src = fetchFromGitHub {
     owner = "lxqt";
-    repo = pname;
+    repo = finalAttrs.pname;
     rev = version;
-    hash = {
-      "0.13.0" = "sha256-4/hVlEdqqqd6CNitCRkIzsS1R941vPJdirIklp4acXA=";
-      "2.0.0" = "sha256-ZFvnIumP03Mp+4OHPe1yMVsSYhMmYUY1idJGCAy5IhA=";
-    }."${version}";
+    hash =
+      {
+        "0.13.0" = "sha256-4/hVlEdqqqd6CNitCRkIzsS1R941vPJdirIklp4acXA=";
+        "2.0.0" = "sha256-ZFvnIumP03Mp+4OHPe1yMVsSYhMmYUY1idJGCAy5IhA=";
+      }
+      ."${version}";
   };
 
-  postPatch = ''
-    # Nix clang on darwin identifies as 'Clang', not 'AppleClang'
-    # Without this, dependants fail to link.
-    substituteInPlace cmake/modules/LXQtCompilerSettings.cmake \
-      --replace-fail AppleClang Clang
-  '';
-
   nativeBuildInputs = [
+    finalAttrs.setupHook
+
     cmake
     pkg-config
-    setupHook
     wrapQtAppsHook
   ];
 
@@ -50,15 +49,18 @@ stdenv.mkDerivation rec {
     perl # needed by LXQtTranslateDesktop.cmake
   ];
 
-  setupHook = ./setup-hook.sh;
+  postPatch = ''
+    # Nix clang on darwin identifies as 'Clang', not 'AppleClang'
+    # Without this, dependants fail to link.
+    substituteInPlace cmake/modules/LXQtCompilerSettings.cmake \
+      --replace-fail AppleClang Clang
+  '';
 
   # We're dependent on this macro doing add_definitions in most places
   # But we have the setup-hook to set the values.
   postInstall = ''
     cp ${./LXQtConfigVars.cmake} $out/share/cmake/lxqt${lib.optionalString (lib.versionAtLeast version "2.0.0") "2"}-build-tools/modules/LXQtConfigVars.cmake
   '';
-
-  passthru.updateScript = gitUpdater { };
 
   meta = with lib; {
     homepage = "https://github.com/lxqt/lxqt-build-tools";
@@ -68,4 +70,4 @@ stdenv.mkDerivation rec {
     platforms = with platforms; unix;
     maintainers = teams.lxqt.members;
   };
-}
+})
