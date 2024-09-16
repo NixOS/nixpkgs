897954b8ae5c2cac7d61c9406aded112edc02750
Kerstin Humm <kerstin@erictapen.name>
2024-09-16 16:37:49 +0200

    nixos/open-web-calendar: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1494,6 +1494,7 @@
   ./services/web-apps/onlyoffice.nix
   ./services/web-apps/openvscode-server.nix
   ./services/web-apps/mediagoblin.nix
+  ./services/web-apps/open-web-calendar.nix
   ./services/web-apps/mobilizon.nix
   ./services/web-apps/openwebrx.nix
   ./services/web-apps/outline.nix
