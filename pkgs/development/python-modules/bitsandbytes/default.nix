eee442b86029c36aa74903d0400b98743a804837
Gaetan Lepage <gaetan@glepage.com>
2024-09-17 00:07:38 +0200

    python312Packages.bitsandbytes: 0.43.1 -> 0.44.1
    Diff:
    https://github.com/TimDettmers/bitsandbytes/compare/refs/tags/0.43.1...0.44.1
    
    Changelog:
    https://github.com/TimDettmers/bitsandbytes/releases/tag/0.44.1

--- pkgs/development/python-modules/bitsandbytes/default.nix
+++ pkgs/development/python-modules/bitsandbytes/default.nix
@@ -1,31 +1,31 @@
 {
   lib,
+  torch,
+  symlinkJoin,
   buildPythonPackage,
   fetchFromGitHub,
-  python,
-  pythonOlder,
+  cmake,
   setuptools,
-  wheel,
-  torch,
   scipy,
-  symlinkJoin,
 }:
 
 let
   pname = "bitsandbytes";
-  version = "0.43.1";
+  version = "0.44.1";
 
-  inherit (torch) cudaCapabilities cudaPackages cudaSupport;
-  inherit (cudaPackages) backendStdenv cudaVersion;
+  inherit (torch) cudaPackages cudaSupport;
+  inherit (cudaPackages) cudaVersion;
 
   # NOTE: torchvision doesn't use cudnn; torch does!
   #   For this reason it is not included.
   cuda-common-redist = with cudaPackages; [
-    cuda_cccl # <thrust/*>
-    libcublas # cublas_v2.h
+    cuda_cccl.dev # <thrust/*>
+    libcublas.dev # cublas_v2.h
+    libcublas.lib
     libcurand
     libcusolver # cusolverDn.h
-    libcusparse # cusparse.h
+    libcusparse.dev # cusparse.h
+    libcusparse.lib
   ];
 
   cuda-native-redist = symlinkJoin {
@@ -33,7 +33,9 @@ let
     paths =
       with cudaPackages;
       [
-        cuda_cudart # cuda_runtime.h cuda_runtime_api.h
+        cuda_cudart.dev # cuda_runtime.h cuda_runtime_api.h
+        cuda_cudart.lib
+        cuda_cudart.static
         cuda_nvcc
       ]
       ++ cuda-common-redist;
@@ -48,47 +50,38 @@ buildPythonPackage {
   inherit pname version;
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
-
   src = fetchFromGitHub {
     owner = "TimDettmers";
     repo = "bitsandbytes";
     rev = "refs/tags/${version}";
-    hash = "sha256-GFbFKPdV96DXPA+PZO4h0zdBclN670fb0PGv4QPHWHU=";
+    hash = "sha256-yvxD5ymMK5p4Xg7Csx/90mPV3yxUC6QUuF/8BKO2p0k=";
   };
-
-  postPatch =
-    ''
-      substituteInPlace Makefile --replace "/usr/bin/g++" "g++" --replace "lib64" "lib"
-      substituteInPlace bitsandbytes/cuda_setup/main.py  \
-        --replace "binary_path = package_dir / self.binary_name"  \
-                  "binary_path = Path('$out/${python.sitePackages}/${pname}')/self.binary_name"
-    ''
-    + lib.optionalString torch.cudaSupport ''
-      substituteInPlace bitsandbytes/cuda_setup/main.py  \
-        --replace "/usr/local/cuda/lib64" "${cuda-native-redist}/lib"
-    '';
 
   CUDA_HOME = "${cuda-native-redist}";
 
-  preBuild =
-    if torch.cudaSupport then
-      with torch.cudaPackages;
-      let
-        cudaVersion = lib.concatStrings (lib.splitVersion torch.cudaPackages.cudaMajorMinorVersion);
-      in
-      ''make CUDA_VERSION=${cudaVersion} cuda${cudaMajorVersion}x''
-    else
-      ''make CUDA_VERSION=CPU cpuonly'';
-
-  nativeBuildInputs = [
+  build-system = [
+    cmake
     setuptools
-    wheel
-  ] ++ lib.optionals torch.cudaSupport [ cuda-native-redist ];
+  ] ++ lib.optionals cudaSupport [ cuda-native-redist ];
 
-  buildInputs = lib.optionals torch.cudaSupport [ cuda-redist ];
+  dontUseCmakeConfigure = true;
 
-  propagatedBuildInputs = [
+  buildInputs = lib.optionals cudaSupport [ cuda-redist ];
+
+  preBuild =
+    if cudaSupport then
+      ''
+        export NVCC_APPEND_FLAGS="-I${cuda-native-redist}/include -L${cuda-native-redist}/lib"
+        cmake -DCMAKE_CXX_FLAGS="-I${cuda-native-redist}/include" -DCOMPUTE_BACKEND=cuda -S .
+        make
+      ''
+    else
+      ''
+        cmake -DCOMPUTE_BACKEND=cpu -S .
+        make
+      '';
+
+  dependencies = [
     scipy
     torch
   ];
@@ -97,11 +90,11 @@ buildPythonPackage {
 
   pythonImportsCheck = [ "bitsandbytes" ];
 
-  meta = with lib; {
+  meta = {
     description = "8-bit CUDA functions for PyTorch";
     homepage = "https://github.com/TimDettmers/bitsandbytes";
     changelog = "https://github.com/TimDettmers/bitsandbytes/releases/tag/${version}";
-    license = licenses.mit;
-    maintainers = with maintainers; [ bcdarwin ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ bcdarwin ];
   };
 }
