feb9b32f1c4b1a33a58c86a272abe8a67af25479
Bobby Rong <rjl931189261@126.com>
2024-09-16 15:43:54 +0000

    libpeas2: 2.0.4 → 2.0.5
    https://gitlab.gnome.org/GNOME/libpeas/-/compare/2.0.4...2.0.5
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/libpeas/2.x.nix
+++ pkgs/development/libraries/libpeas/2.x.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libpeas";
-  version = "2.0.4";
+  version = "2.0.5";
 
   outputs = [ "out" "dev" "devdoc" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    hash = "sha256-16Wd5ejQSAcxBNAqhtGzw7VYuF8/KWxz0Qx99LmnYk4=";
+    hash = "sha256-N28vc9cxtU4T3bqx2Rtjgs9qmAUk3vRN9irdFUid5t0=";
   };
 
   patches = [
