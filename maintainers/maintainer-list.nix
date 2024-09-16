f9a0b4cdecfed08ec35d33476150b64ef7fa95c7
Jacek Galowicz <jacek@galowicz.de>
2024-09-16 22:54:47 +0200

    maintainers: Fix tfc's matrix handle

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20739,7 +20739,7 @@
   };
   tfc = {
     email = "jacek@galowicz.de";
-    matrix = "@jonge:ukvly.org";
+    matrix = "@tfc:matrix.org";
     github = "tfc";
     githubId = 29044;
     name = "Jacek Galowicz";
