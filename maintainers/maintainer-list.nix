4885b662cc5cccb449b640820fbf91fb6483851e
PixelSergey <sergey.ichtchenko@gmail.com>
2024-09-16 18:36:45 +0300

    maintainers: add pixelsergey

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16465,6 +16465,12 @@
     githubId = 20472367;
     name = "Peter Hebden";
   };
+  pixelsergey = {
+    email = "sergey.ichtchenko@gmail.com";
+    github = "PixelSergey";
+    githubId = 14542417;
+    name = "Sergey Ichtchenko";
+  };
   pjbarnoy = {
     email = "pjbarnoy@gmail.com";
     github = "waaamb";
