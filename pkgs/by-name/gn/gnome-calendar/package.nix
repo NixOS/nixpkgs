d9a1ea10154b09c2b4ade05673f94f7636c89e3f
Bobby Rong <rjl931189261@126.com>
2024-09-17 13:27:47 +0000

    gnome-calendar: 47.beta → 47.0
    https://gitlab.gnome.org/GNOME/gnome-calendar/-/compare/47.beta...47.0
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/by-name/gn/gnome-calendar/package.nix
+++ pkgs/by-name/gn/gnome-calendar/package.nix
@@ -21,11 +21,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-calendar";
-  version = "47.beta";
+  version = "47.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-calendar/${lib.versions.major version}/gnome-calendar-${version}.tar.xz";
-    hash = "sha256-jC0hC9pYKKbf1IIyNxQxHkscQ0jesfANiY3KEgrucWQ=";
+    hash = "sha256-OwMxPxxNEtyCHgfiCdFZbVPq+iVdSS0s5av5LtG1HnY=";
   };
 
   nativeBuildInputs = [
