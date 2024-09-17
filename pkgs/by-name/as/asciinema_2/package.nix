94ac87fb1f9396f995a6be3c083d23881ba5830b
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-17 11:59:44 -0300

    asciinema_2: hammer
    - get rid of rec
    - strictDeps
    - reorder meta
    - add version test
    - add AndersonTorres as maintainer

--- pkgs/by-name/as/asciinema_2/package.nix
+++ pkgs/by-name/as/asciinema_2/package.nix
@@ -1,36 +1,62 @@
 {
   lib,
-  python3Packages,
   fetchFromGitHub,
+  python3Packages,
+  testers,
 }:
 
-python3Packages.buildPythonApplication rec {
-  pname = "asciinema";
-  version = "2.4.0";
-  pyproject = true;
+let
+  self = python3Packages.buildPythonApplication {
+    pname = "asciinema";
+    version = "2.4.0";
+    pyproject = true;
 
-  src = fetchFromGitHub {
-    owner = "asciinema";
-    repo = "asciinema";
-    rev = "v${version}";
-    hash = "sha256-UegLwpJ+uc9cW3ozLQJsQBjIGD7+vzzwzQFRV5gmDmI=";
+    src = fetchFromGitHub {
+      owner = "asciinema";
+      repo = "asciinema";
+      rev = "v${self.version}";
+      hash = "sha256-UegLwpJ+uc9cW3ozLQJsQBjIGD7+vzzwzQFRV5gmDmI=";
+    };
+
+    build-system = [ python3Packages.setuptools ];
+
+    nativeCheckInputs = [ python3Packages.pytestCheckHook ];
+
+    postPatch = ''
+      substituteInPlace tests/pty_test.py \
+        --replace-fail "python3" "${python3Packages.python.interpreter}"
+    '';
+
+    strictDeps = true;
+
+    passthru = {
+      tests.version = testers.testVersion {
+        package = self;
+        command = ''
+          export HOME=$TMP
+          asciinema --version
+        '';
+      };
+    };
+
+    meta = {
+      homepage = "https://asciinema.org/";
+      description = "Terminal session recorder and the best companion of asciinema.org";
+      longDescription = ''
+        asciinema is a suite of tools for recording, replaying, and sharing
+        terminal sessions. It is free and open-source software (FOSS), created
+        by Marcin Kulik.
+
+        Its typical use cases include creating tutorials, demonstrating
+        command-line tools, and sharing reproducible bug reports. It focuses on
+        simplicity and interoperability, which makes it a popular choice among
+        computer users working with the command-line, such as developers or
+        system administrators.
+      '';
+      license = with lib.licenses; [ gpl3Plus ];
+      mainProgram = "asciinema";
+      maintainers = with lib.maintainers; [ AndersonTorres ];
+    };
   };
-
-  build-system = [ python3Packages.setuptools ];
-
-  postPatch = ''
-    substituteInPlace tests/pty_test.py \
-      --replace-fail "python3" "${python3Packages.python.interpreter}"
-  '';
-
-  nativeCheckInputs = [ python3Packages.pytestCheckHook ];
-
-  meta = {
-    description = "Terminal session recorder and the best companion of asciinema.org";
-    homepage = "https://asciinema.org/";
-    license = with lib.licenses; [ gpl3Plus ];
-    maintainers = with lib.maintainers; [ ];
-    platforms = lib.platforms.all;
-    mainProgram = "asciinema";
-  };
-}
+in
+self
