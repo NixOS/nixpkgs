a3345f70cc8ca63d99097a7b1114dc00a50fb320
Herwig Hochleitner <herwig@bendlas.net>
2024-09-17 15:36:53 +0200

    cdemu: update packages
    cdemu-daemon: 3.2.6 -> 3.2.7
    libmirage: 3.2.7 -> 3.2.9
    linuxPackages.vhba: 20240202 -> 20240917

--- pkgs/applications/emulators/cdemu/daemon.nix
+++ pkgs/applications/emulators/cdemu/daemon.nix
@@ -2,9 +2,9 @@
 stdenv.mkDerivation {
 
   inherit (callPackage ./common-drv-attrs.nix {
-    version = "3.2.6";
+    version = "3.2.7";
     pname = "cdemu-daemon";
-    hash = "sha256-puQE4+91xhRuNjVPZYgN/WO0uO8fVAOdxQWOGQ+FfY8=";
+    hash = "sha256-EKh2G6RA9Yq46BpTAqN2s6TpLJb8gwDuEpGiwdGcelc=";
   }) pname version src meta;
 
   nativeBuildInputs = [ cmake pkg-config intltool ];
