d6e14935fc81ee7b858320930cbae3496ecd5679
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-17 11:27:48 +0000

    alpaca: 1.1.1 -> 2.0.2

--- pkgs/by-name/al/alpaca/package.nix
+++ pkgs/by-name/al/alpaca/package.nix
@@ -17,14 +17,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "alpaca";
-  version = "1.1.1";
+  version = "2.0.2";
   pyproject = false; # Built with meson
 
   src = fetchFromGitHub {
     owner = "Jeffser";
     repo = "Alpaca";
-    rev = version;
-    hash = "sha256-FFMclm+IUEU4cQZ0+uJHDCHytgW8+fygooWw3Nc1jxM=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-NDMkbIfhvfP4QUuf9jisREjqwcDTn/HApyHGrmGLhPQ=";
   };
 
   nativeBuildInputs = [
