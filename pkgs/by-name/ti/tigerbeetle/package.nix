578d317eacdaa479fb0ae4e327396ad26c4cf5ed
Nate Smith <nate@theinternate.com>
2024-09-17 11:04:38 -0400

    tigerbeetle: 0.15.3 -> 0.16.2

--- pkgs/by-name/ti/tigerbeetle/package.nix
+++ pkgs/by-name/ti/tigerbeetle/package.nix
@@ -1,26 +1,26 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, zig_0_11
+, zig_0_13
 , testers
 , tigerbeetle
 , nix-update-script
 }:
 let
-  # Read [these comments](pkgs/development/compilers/zig/hook.nix#L12-L30) on the default Zig flags, and the associated links. tigerbeetle stopped exposing the `-Doptimize` build flag, so we can't use the default Nixpkgs zig hook as-is. tigerbeetle only exposes a boolean `-Drelease` flag which we'll add in the tigerbeetle derivation in this file.
-  custom_zig_hook = zig_0_11.hook.overrideAttrs (previousAttrs: {
-    zig_default_flags = builtins.filter (flag: builtins.match "-Doptimize.*" flag == null) previousAttrs.zig_default_flags;
+  # Read [these comments](pkgs/development/compilers/zig/hook.nix#L12-L30) on the default Zig flags, and the associated links. tigerbeetle stopped exposing the `-Doptimize` and `-Dcpu` build flags, so we can't use the default Nixpkgs zig hook as-is. tigerbeetle only exposes a boolean `-Drelease` flag which we'll add in the tigerbeetle derivation in this file.
+  custom_zig_hook = zig_0_13.hook.overrideAttrs (previousAttrs: {
+    zig_default_flags = builtins.filter (flag: builtins.match "-D(optimize|cpu).*" flag == null) previousAttrs.zig_default_flags;
   });
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "tigerbeetle";
-  version = "0.15.3";
+  version = "0.16.2";
 
   src = fetchFromGitHub {
     owner = "tigerbeetle";
     repo = "tigerbeetle";
     rev = "refs/tags/${finalAttrs.version}";
-    hash = "sha256-3+uCMoOnyvI//ltEaqTIXytUxxgJrfMnFly11WCh66Q=";
+    hash = "sha256-5cGEoUsY9580kHiJP0yvDmHpc6TCgwOvZyIa1vwocJo=";
   };
 
   env.TIGERBEETLE_RELEASE = finalAttrs.version;
@@ -28,8 +28,10 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [ custom_zig_hook ];
 
   zigBuildFlags = [
-    "-Drelease"
-    "-Dgit-commit=0000000000000000000000000000000000000000"
+    "-Drelease=true"
+    "-Dgit-commit=c26a2ee1c401ddf225eaa242b14bab19ecb60fe2"
+    "-Dconfig-release=${finalAttrs.version}"
+    "-Dconfig-release-client-min=0.15.3"
   ];
 
   passthru = {
