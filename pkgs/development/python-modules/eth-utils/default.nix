1f3ff90501313bc5755e90ab88275f1e65ada966
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-17 15:03:06 +0200

    python312Packages.eth-utils: 4.0.0 -> 5.0.0

--- pkgs/development/python-modules/eth-utils/default.nix
+++ pkgs/development/python-modules/eth-utils/default.nix
@@ -1,46 +1,61 @@
 {
   lib,
-  fetchFromGitHub,
   buildPythonPackage,
+  fetchFromGitHub,
+  fetchpatch2,
+  isPyPy,
+  pythonOlder,
+  cytoolz,
   eth-hash,
   eth-typing,
-  cytoolz,
   hypothesis,
-  isPyPy,
+  mypy,
   pytestCheckHook,
-  pythonOlder,
+  pytest-xdist,
   setuptools,
   toolz,
 }:
 
 buildPythonPackage rec {
   pname = "eth-utils";
-  version = "4.0.0";
+  version = "5.0.0";
+
   pyproject = true;
-  disabled = pythonOlder "3.6";
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "eth-utils";
-    rev = "v${version}";
-    hash = "sha256-k2pHM1eKPzoGxZlU6yT7bZMv4CCWGaZaSnFHSbT76Zo=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-bBtNdwvg8fY2aWWmBJV07oSGb5rR29aPbAyG9fnO8ic=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  patches = [
+    (fetchpatch2 {
+      # Merged but unreleased: https://github.com/ethereum/eth-utils/pull/284
+      name = "fix-cyclic-dependency.patch";
+      url = "https://github.com/ethereum/eth-utils/commit/c27072b6caf758f02d2eda8b1ee004d772e491dd.patch";
+      hash = "sha256-7gKxRXDZmiWxQxA+OPCB32wS1hoA0ChjGOlRRD281I4=";
+    })
+  ];
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     eth-hash
     eth-typing
   ] ++ lib.optional (!isPyPy) cytoolz ++ lib.optional isPyPy toolz;
 
   nativeCheckInputs = [
     hypothesis
+    mypy
     pytestCheckHook
+    pytest-xdist
   ] ++ eth-hash.optional-dependencies.pycryptodome;
 
-  # Removing a poorly written test case from test suite.
-  # TODO work with the upstream
-  disabledTestPaths = [ "tests/functional-utils/test_type_inference.py" ];
+  # side-effect: runs pip online check and is blocked by sandbox
+  disabledTests = [ "test_install_local_wheel" ];
 
   pythonImportsCheck = [ "eth_utils" ];
 
@@ -49,6 +64,6 @@ buildPythonPackage rec {
     description = "Common utility functions for codebases which interact with ethereum";
     homepage = "https://github.com/ethereum/eth-utils";
     license = lib.licenses.mit;
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.FlorianFranzen ];
   };
 }
