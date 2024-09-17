50fb52b84540b58c837b7a1ee250a9629896b162
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-09-17 17:15:09 +0200

    ocamlPackages.httpun-types: init at 0.2.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -689,6 +689,8 @@ let
 
     httpaf-lwt-unix = callPackage ../development/ocaml-modules/httpaf/lwt-unix.nix { };
 
+    httpun-types = callPackage ../development/ocaml-modules/httpun/types.nix { };
+
     hxd = callPackage ../development/ocaml-modules/hxd { };
 
     ### I ###
