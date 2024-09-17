bbef1be3121a0a1a49405644c2af1bc55c48f2db
Anthony ROUSSEL <anthony@roussel.dev>
2024-09-17 20:49:19 +0200

    overcommit: add passthru.tests.version, meta.mainProgram

--- pkgs/by-name/ov/overcommit/package.nix
+++ pkgs/by-name/ov/overcommit/package.nix
@@ -4,6 +4,8 @@
   bundlerUpdateScript,
   git,
   makeWrapper,
+  overcommit,
+  testers,
 }:
 
 bundlerApp {
@@ -18,13 +20,19 @@ bundlerApp {
   '';
 
   passthru = {
+    tests.version = testers.testVersion {
+      package = overcommit;
+      version = (import ./gemset.nix).overcommit.version;
+    };
     updateScript = bundlerUpdateScript "overcommit";
   };
 
   meta = with lib; {
     description = "Tool to manage and configure Git hooks";
     homepage = "https://github.com/sds/overcommit";
+    changelog = "https://github.com/sds/overcommit/blob/main/CHANGELOG.md";
     license = licenses.mit;
+    mainProgram = "overcommit";
     maintainers = with maintainers; [
       Br1ght0ne
       anthonyroussel
