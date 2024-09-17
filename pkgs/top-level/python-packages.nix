c4fcf6436b52c7bf5d95f49c7f8d0f24e43dce7d
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-17 22:26:44 +0200

    python312Packages.pyunormalize: init at 16.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10843,6 +10843,8 @@ self: super: with self; {
 
   pyunpack = callPackage ../development/python-modules/pyunpack { };
 
+  pyunormalize = callPackage ../development/python-modules/pyunormalize { };
+
   pyutil = callPackage ../development/python-modules/pyutil { };
 
   pyzbar = callPackage ../development/python-modules/pyzbar { };
