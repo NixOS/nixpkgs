17d7e76bbdd31d6cc2be1d9705b8d8dcfee3de41
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-17 15:54:22 +0200

    crocoddyl: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19663,8 +19663,6 @@ with pkgs;
 
   croaring = callPackage ../development/libraries/croaring { };
 
-  crocoddyl = callPackage ../development/libraries/crocoddyl { };
-
   crossguid = callPackage ../development/libraries/crossguid { };
 
   cryptopp = callPackage ../development/libraries/crypto++ { };
