b585a1d35ee8f3d76a89d819026f292319fe19c2
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-17 15:02:31 -0300

    emacs30: init

--- pkgs/applications/editors/emacs/make-emacs.nix
+++ pkgs/applications/editors/emacs/make-emacs.nix
@@ -157,7 +157,9 @@ mkDerivation (finalAttrs: {
     (substituteAll {
       src = if lib.versionOlder finalAttrs.version "29"
             then ./native-comp-driver-options-28.patch
-            else ./native-comp-driver-options.patch;
+            else if lib.versionOlder finalAttrs.version "30"
+            then ./native-comp-driver-options.patch
+            else ./native-comp-driver-options-30.patch;
       backendPath = (lib.concatStringsSep " "
         (builtins.map (x: ''"-B${x}"'') ([
           # Paths necessary so the JIT compiler finds its libraries:
