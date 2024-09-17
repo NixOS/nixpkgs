f19ef08998749070aa94a1819f11adfdabf7025e
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-17 16:48:14 -0300

    nix-update: get rid of rec
    So that nix-update-script can refer to nix-update itself.

--- pkgs/by-name/ni/nix-update/package.nix
+++ pkgs/by-name/ni/nix-update/package.nix
@@ -8,52 +8,55 @@
   python3Packages,
 }:
 
-python3Packages.buildPythonApplication rec {
-  pname = "nix-update";
-  version = "1.5.1";
-  pyproject = true;
+let
+  self = python3Packages.buildPythonApplication {
+    pname = "nix-update";
+    version = "1.5.1";
+    pyproject = true;
 
-  src = fetchFromGitHub {
-    owner = "Mic92";
-    repo = "nix-update";
-    rev = "refs/tags/${version}";
-    hash = "sha256-JXls4EgMDAWtd736nXS2lYTUv9QIjRpkCTimxNtMN7Q=";
-  };
+    src = fetchFromGitHub {
+      owner = "Mic92";
+      repo = "nix-update";
+      rev = "refs/tags/${self.version}";
+      hash = "sha256-JXls4EgMDAWtd736nXS2lYTUv9QIjRpkCTimxNtMN7Q=";
+    };
 
-  nativeBuildInputs = [ python3Packages.setuptools ];
+    nativeBuildInputs = [ python3Packages.setuptools ];
 
-  makeWrapperArgs = [
-    "--prefix"
-    "PATH"
-    ":"
-    (lib.makeBinPath [
-      nix
-      nix-prefetch-git
-      nixpkgs-review
-    ])
-  ];
-
-  checkPhase = ''
-    runHook preCheck
-
-    $out/bin/nix-update --help >/dev/null
-
-    runHook postCheck
-  '';
-
-  passthru = {
-    nix-update-script = callPackage ./nix-update-script.nix { };
-  };
-
-  meta = {
-    homepage = "https://github.com/Mic92/nix-update/";
-    description = "Swiss-knife for updating nix packages";
-    changelog = "https://github.com/Mic92/nix-update/releases/tag/${version}";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [
-      figsoda
-      mic92
+    makeWrapperArgs = [
+      "--prefix"
+      "PATH"
+      ":"
+      (lib.makeBinPath [
+        nix
+        nix-prefetch-git
+        nixpkgs-review
+      ])
     ];
-    mainProgram = "nix-update";
+
+    checkPhase = ''
+      runHook preCheck
+
+      $out/bin/nix-update --help >/dev/null
+
+      runHook postCheck
+    '';
+
+    passthru = {
+      nix-update-script = callPackage ./nix-update-script.nix { nix-update = self; };
+    };
+
+    meta = {
+      homepage = "https://github.com/Mic92/nix-update/";
+      description = "Swiss-knife for updating nix packages";
+      changelog = "https://github.com/Mic92/nix-update/releases/tag/${self.version}";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [
+        figsoda
+        mic92
+      ];
+      mainProgram = "nix-update";
+    };
   };
-}
+in
+self
