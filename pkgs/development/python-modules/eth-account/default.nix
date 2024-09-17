4bb19284a54f10e30b6eda7542c6dba3f262b8d1
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-17 14:43:17 +0200

    python312Packages.eth-account: 0.9.0 -> 13.3.0

--- pkgs/development/python-modules/eth-account/default.nix
+++ pkgs/development/python-modules/eth-account/default.nix
@@ -2,7 +2,9 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  pythonOlder,
   bitarray,
+  ckzg,
   eth-abi,
   eth-keyfile,
   eth-keys,
@@ -10,25 +12,34 @@
   eth-utils,
   websockets,
   hexbytes,
-  pythonOlder,
+  hypothesis,
+  pydantic,
+  pytestCheckHook,
+  pytest-xdist,
   rlp,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "eth-account";
-  version = "0.9.0";
-  format = "setuptools";
-  disabled = pythonOlder "3.7";
+  version = "0.13.3";
+
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "eth-account";
-    rev = "v${version}";
-    hash = "sha256-Ps/vzJv0W1+wy1mSJaqRNNU6CoCMchReHIocB9kPrGs=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-657l7M7euUGXXKTrQyB/kVA4miyePr310enhkaSBJss=";
   };
+
+  build-system = [ setuptools ];
 
   propagatedBuildInputs = [
     bitarray
+    ckzg
     eth-abi
     eth-keyfile
     eth-keys
@@ -39,15 +50,29 @@ buildPythonPackage rec {
     websockets
   ];
 
-  # require buildinga npm project
-  doCheck = false;
+  nativeCheckInputs = [
+    hypothesis
+    pydantic
+    pytestCheckHook
+    pytest-xdist
+  ];
+
+  disabledTests = [
+    # requires local nodejs install
+    "test_messages_where_all_3_sigs_match"
+    "test_messages_where_eth_account_matches_ethers_but_not_metamask"
+    "test_messages_where_eth_account_matches_metamask_but_not_ethers"
+    # disable flaky fuzzing test
+    "test_compatibility"
+  ];
 
   pythonImportsCheck = [ "eth_account" ];
 
-  meta = with lib; {
+  meta = {
+    changelog = "https://github.com/ethereum/eth-account/blob/v${version}/docs/release_notes.rst";
     description = "Account abstraction library for web3.py";
     homepage = "https://github.com/ethereum/eth-account";
-    license = licenses.mit;
-    maintainers = [ ];
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.FlorianFranzen ];
   };
 }
