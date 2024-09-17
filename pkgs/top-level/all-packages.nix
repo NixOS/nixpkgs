dc8c5008eabd814512ef7f4a051c5d5e3bc0e21c
Anthony ROUSSEL <anthony@roussel.dev>
2024-09-17 20:44:11 +0200

    overcommit: migrate to pkgs/by-name, format with nixfmt-rfc-style

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10950,8 +10950,6 @@ with pkgs;
 
   ovftool = callPackage ../tools/virtualization/ovftool { };
 
-  overcommit = callPackage ../development/tools/overcommit { };
-
   overmind = callPackage ../applications/misc/overmind { };
 
   oxker = callPackage ../applications/misc/oxker { };
