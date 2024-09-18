9549cfde07d8dee386ceecca90993bf7df1805c0
Bazyli Cyran <bazyli@cyran.dev>
2024-09-18 19:23:42 +0200

    maintainers: add bcyran

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2458,6 +2458,12 @@
     githubId = 34620799;
     name = "Jacob Bachmann";
   };
+  bcyran = {
+    email = "bazyli@cyran.dev";
+    github = "bcyran";
+    githubId = 8322846;
+    name = "Bazyli Cyran";
+  };
   bdd = {
     email = "bdd@mindcast.org";
     github = "bdd";
