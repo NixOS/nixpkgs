757f4e6755ba1bccea2146423461e4ef47caa3c6
Florian Franzen <Florian.Franzen@gmail.com>
2024-09-18 03:06:01 +0200

    python312Packages.eth-keys: 0.5.0 -> 0.6.0

--- pkgs/development/python-modules/eth-keys/default.nix
+++ pkgs/development/python-modules/eth-keys/default.nix
@@ -1,37 +1,37 @@
 {
   lib,
-  buildPythonPackage,
   fetchFromGitHub,
+  buildPythonPackage,
   setuptools,
-  asn1tools,
-  coincurve,
-  eth-hash,
+  # dependencies
   eth-typing,
   eth-utils,
+  # nativeCheckInputs
+  asn1tools,
   factory-boy,
   hypothesis,
-  isPyPy,
   pyasn1,
   pytestCheckHook,
-  pythonOlder,
+  coincurve,
+  eth-hash,
+  isPyPy,
 }:
 
 buildPythonPackage rec {
   pname = "eth-keys";
-  version = "0.5.0";
+  version = "0.6.0";
   pyproject = true;
-  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "eth-keys";
-    rev = "v${version}";
-    hash = "sha256-vyyaLCG2uIHXX0t93DmFq8/u0rZL+nsBsH2gfgjziyo=";
+    tag = "v${version}";
+    hash = "sha256-HyOfuzwldtqjjowW7HGdZ8RNMXNK3y2NrXUoeMlWJjs=";
   };
 
   build-system = [ setuptools ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     eth-typing
     eth-utils
   ];
@@ -48,29 +48,17 @@ buildPythonPackage rec {
     ++ lib.optional (!isPyPy) eth-hash.optional-dependencies.pysha3
     ++ lib.optional isPyPy eth-hash.optional-dependencies.pycryptodome;
 
-  disabledTests = [
-    # tests are broken
-    "test_compress_decompress_inversion"
-    "test_public_key_generation_is_equal"
-    "test_signing_is_equal"
-    "test_native_to_coincurve_recover"
-    "test_public_key_compression_is_equal"
-    "test_public_key_decompression_is_equal"
-    "test_signatures_with_high_s"
-    # timing sensitive
-    "test_encode_decode_pairings"
-  ];
-
   pythonImportsCheck = [ "eth_keys" ];
 
   optional-dependencies = {
     coincurve = [ coincurve ];
   };
 
-  meta = with lib; {
+  meta = {
     description = "Common API for Ethereum key operations";
     homepage = "https://github.com/ethereum/eth-keys";
-    license = licenses.mit;
-    maintainers = [ ];
+    changelog = "https://github.com/ethereum/eth-keys/blob/v${version}/CHANGELOG.rst";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ hellwolf ];
   };
 }
