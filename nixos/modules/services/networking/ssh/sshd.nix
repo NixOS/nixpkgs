c598e6bc5953cbe37b0eff151a8e63030326d771
nicoo <nicoo@mur.at>
2024-09-18 11:17:25 +0000

    fixup! nixos/sshd: Disable `authorizedKeysInHomedir` if `stateVersion` >= 24.11

--- nixos/modules/services/networking/ssh/sshd.nix
+++ nixos/modules/services/networking/ssh/sshd.nix
@@ -304,7 +304,7 @@ in
       authorizedKeysInHomedir = lib.mkOption {
         type = lib.types.bool;
         default = lib.versionOlder config.system.stateVersion "24.11";
-        defaultText = "`false` unless [](#opt-system.stateVersion) is 24.05 or older";
+        defaultText = lib.literalMD "`false` unless [](#opt-system.stateVersion) is 24.05 or older";
         description = ''
           Enables the use of the `~/.ssh/authorized_keys` file.
 
