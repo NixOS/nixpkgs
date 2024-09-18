40f8cbea1cc5d3e289dcc6831ee67d8b12b4da76
sarahec <sarahec@nextquestion.net>
2024-09-18 10:46:49 -0700

    mupdf: 1.24.8 -> 1.24.9

--- pkgs/applications/misc/mupdf/default.nix
+++ pkgs/applications/misc/mupdf/default.nix
@@ -1,46 +1,47 @@
-{ stdenv
-, lib
-, fetchurl
-, fetchFromGitHub
-, copyDesktopItems
-, makeDesktopItem
-, desktopToDarwinBundle
-, buildPackages
-, pkg-config
-, fixDarwinDylibNames
-, freetype
-, harfbuzz
-, openjpeg
-, jbig2dec
-, libjpeg
-, darwin
-, gumbo
-, enableX11 ? (!stdenv.isDarwin)
-, libX11
-, libXext
-, libXi
-, libXrandr
-, enableCurl ? true
-, curl
-, openssl
-, enableGL ? true
-, freeglut
-, libGLU
-, enableOcr ? false
-, leptonica
-, tesseract
-, enableCxx ? false
-, python3
-, enablePython ? false
-, which
-, swig
-, xcbuild
-, gitUpdater
+{
+  stdenv,
+  lib,
+  fetchurl,
+  fetchFromGitHub,
+  copyDesktopItems,
+  makeDesktopItem,
+  desktopToDarwinBundle,
+  buildPackages,
+  pkg-config,
+  fixDarwinDylibNames,
+  freetype,
+  harfbuzz,
+  openjpeg,
+  jbig2dec,
+  libjpeg,
+  darwin,
+  gumbo,
+  enableX11 ? (!stdenv.isDarwin),
+  libX11,
+  libXext,
+  libXi,
+  libXrandr,
+  enableCurl ? true,
+  curl,
+  openssl,
+  enableGL ? true,
+  freeglut,
+  libGLU,
+  enableOcr ? false,
+  leptonica,
+  tesseract,
+  enableCxx ? false,
+  python3,
+  enablePython ? false,
+  which,
+  swig,
+  xcbuild,
+  gitUpdater,
 
-# for passthru.tests
-, cups-filters
-, zathura
-, mupdf
+  # for passthru.tests
+  cups-filters,
+  zathura,
+  mupdf,
 }:
 
 assert enablePython -> enableCxx;
@@ -60,17 +61,16 @@ let
 
 in
 stdenv.mkDerivation rec {
-  version = "1.24.8";
+  version = "1.24.9";
   pname = "mupdf";
 
   src = fetchurl {
     url = "https://mupdf.com/downloads/archive/${pname}-${version}-source.tar.gz";
-    hash = "sha256-pRjZvpds2yAG1FOC1/+xubjWS8P9PLc8picNdS+n9Eg=";
+    hash = "sha256-C0RqoO7MEU6ZadzNcMl4k1j8y2WJqB1HDclBoIdNqYo=";
   };
 
   patches = [
     ./0002-Add-Darwin-deps.patch
-    ./0003-Fix-cpp-build.patch
   ];
 
   postPatch = ''
@@ -87,34 +87,78 @@ stdenv.mkDerivation rec {
     done
   '';
 
-  makeFlags = [
-    "prefix=$(out)"
-    "shared=yes"
-    "USE_SYSTEM_LIBS=yes"
-    "PKG_CONFIG=${buildPackages.pkg-config}/bin/${buildPackages.pkg-config.targetPrefix}pkg-config"
-  ] ++ lib.optionals (!enableX11) [ "HAVE_X11=no" ]
+  makeFlags =
+    [
+      "prefix=$(out)"
+      "shared=yes"
+      "USE_SYSTEM_LIBS=yes"
+      "PKG_CONFIG=${buildPackages.pkg-config}/bin/${buildPackages.pkg-config.targetPrefix}pkg-config"
+    ]
+    ++ lib.optionals (!enableX11) [ "HAVE_X11=no" ]
     ++ lib.optionals (!enableGL) [ "HAVE_GLUT=no" ]
     ++ lib.optionals (enableOcr) [ "USE_TESSERACT=yes" ];
 
-  nativeBuildInputs = [ pkg-config ]
+  nativeBuildInputs =
+    [ pkg-config ]
     ++ lib.optional (enableGL || enableX11) copyDesktopItems
     ++ lib.optional (stdenv.isDarwin && (enableGL || enableX11)) desktopToDarwinBundle
-    ++ lib.optionals (enableCxx || enablePython) [ python3 python3.pkgs.setuptools python3.pkgs.libclang ]
-    ++ lib.optionals (enablePython) [ which swig ]
-    ++ lib.optionals stdenv.isDarwin [ fixDarwinDylibNames xcbuild ];
+    ++ lib.optionals (enableCxx || enablePython) [
+      python3
+      python3.pkgs.setuptools
+      python3.pkgs.libclang
+    ]
+    ++ lib.optionals (enablePython) [
+      which
+      swig
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      fixDarwinDylibNames
+      xcbuild
+    ];
 
-  buildInputs = [ freetype harfbuzz openjpeg jbig2dec libjpeg gumbo ]
-    ++ lib.optionals enableX11 [ libX11 libXext libXi libXrandr ]
-    ++ lib.optionals enableCurl [ curl openssl ]
+  buildInputs =
+    [
+      freetype
+      harfbuzz
+      openjpeg
+      jbig2dec
+      libjpeg
+      gumbo
+    ]
+    ++ lib.optionals enableX11 [
+      libX11
+      libXext
+      libXi
+      libXrandr
+    ]
+    ++ lib.optionals enableCurl [
+      curl
+      openssl
+    ]
     ++ lib.optionals enableGL (
       if stdenv.isDarwin then
-        with darwin.apple_sdk.frameworks; [ GLUT OpenGL ]
+        with darwin.apple_sdk.frameworks;
+        [
+          GLUT
+          OpenGL
+        ]
       else
-        [ freeglut-mupdf libGLU ]
+        [
+          freeglut-mupdf
+          libGLU
+        ]
     )
-    ++ lib.optionals enableOcr [ leptonica tesseract ]
-  ;
-  outputs = [ "bin" "dev" "out" "man" "doc" ];
+    ++ lib.optionals enableOcr [
+      leptonica
+      tesseract
+    ];
+  outputs = [
+    "bin"
+    "dev"
+    "out"
+    "man"
+    "doc"
+  ];
 
   preConfigure = ''
     # Don't remove mujs because upstream version is incompatible
@@ -143,51 +187,76 @@ stdenv.mkDerivation rec {
         "application/x-cbz"
         "application/x-pdf"
       ];
-      categories = [ "Graphics" "Viewer" ];
+      categories = [
+        "Graphics"
+        "Viewer"
+      ];
       keywords = [
-        "mupdf" "comic" "document" "ebook" "viewer"
-        "cbz" "epub" "fb2" "pdf" "xps"
+        "mupdf"
+        "comic"
+        "document"
+        "ebook"
+        "viewer"
+        "cbz"
+        "epub"
+        "fb2"
+        "pdf"
+        "xps"
       ];
     })
   ];
 
-  postInstall = ''
-    mkdir -p "$out/lib/pkgconfig"
-    cat >"$out/lib/pkgconfig/mupdf.pc" <<EOF
-    prefix=$out
-    libdir=\''${prefix}/lib
-    includedir=\''${prefix}/include
+  postInstall =
+    ''
+      mkdir -p "$out/lib/pkgconfig"
+      cat >"$out/lib/pkgconfig/mupdf.pc" <<EOF
+      prefix=$out
+      libdir=\''${prefix}/lib
+      includedir=\''${prefix}/include
 
-    Name: mupdf
-    Description: Library for rendering PDF documents
-    Version: ${version}
-    Libs: -L\''${libdir} -lmupdf
-    Cflags: -I\''${includedir}
-    EOF
+      Name: mupdf
+      Description: Library for rendering PDF documents
+      Version: ${version}
+      Libs: -L\''${libdir} -lmupdf
+      Cflags: -I\''${includedir}
+      EOF
 
-    moveToOutput "bin" "$bin"
-    cp ./build/shared-release/libmupdf${stdenv.hostPlatform.extensions.sharedLibrary}* $out/lib
-  '' + (lib.optionalString (stdenv.isDarwin) ''
-    for exe in $bin/bin/*; do
-      install_name_tool -change build/shared-release/libmupdf.dylib $out/lib/libmupdf.dylib "$exe"
-    done
-  '') + (lib.optionalString (enableX11 || enableGL) ''
-    mkdir -p $bin/share/icons/hicolor/48x48/apps
-    cp docs/logo/mupdf-icon-48.png $bin/share/icons/hicolor/48x48/apps
-  '') + (if enableGL then ''
-    ln -s "$bin/bin/mupdf-gl" "$bin/bin/mupdf"
-  '' else lib.optionalString (enableX11) ''
-    ln -s "$bin/bin/mupdf-x11" "$bin/bin/mupdf"
-  '') + (lib.optionalString (enableCxx) ''
-    cp platform/c++/include/mupdf/*.h $out/include/mupdf
-    cp build/*/libmupdfcpp.so* $out/lib
-  '') + (lib.optionalString (enablePython) (''
-    mkdir -p $out/${python3.sitePackages}/mupdf
-    cp build/*/_mupdf.so $out/${python3.sitePackages}
-    cp build/*/mupdf.py $out/${python3.sitePackages}/mupdf/__init__.py
-  '' + lib.optionalString (stdenv.isDarwin) ''
-    install_name_tool -add_rpath $out/lib $out/${python3.sitePackages}/_mupdf.so
-  ''));
+      moveToOutput "bin" "$bin"
+      cp ./build/shared-release/libmupdf${stdenv.hostPlatform.extensions.sharedLibrary}* $out/lib
+    ''
+    + (lib.optionalString (stdenv.isDarwin) ''
+      for exe in $bin/bin/*; do
+        install_name_tool -change build/shared-release/libmupdf.dylib $out/lib/libmupdf.dylib "$exe"
+      done
+    '')
+    + (lib.optionalString (enableX11 || enableGL) ''
+      mkdir -p $bin/share/icons/hicolor/48x48/apps
+      cp docs/logo/mupdf-icon-48.png $bin/share/icons/hicolor/48x48/apps
+    '')
+    + (
+      if enableGL then
+        ''
+          ln -s "$bin/bin/mupdf-gl" "$bin/bin/mupdf"
+        ''
+      else
+        lib.optionalString (enableX11) ''
+          ln -s "$bin/bin/mupdf-x11" "$bin/bin/mupdf"
+        ''
+    )
+    + (lib.optionalString (enableCxx) ''
+      cp platform/c++/include/mupdf/*.h $out/include/mupdf
+      cp build/*/libmupdfcpp.so* $out/lib
+    '')
+    + (lib.optionalString (enablePython) (
+      ''
+        mkdir -p $out/${python3.sitePackages}/mupdf
+        cp build/*/_mupdf.so $out/${python3.sitePackages}
+        cp build/*/mupdf.py $out/${python3.sitePackages}/mupdf/__init__.py
+      ''
+      + lib.optionalString (stdenv.isDarwin) ''
+        install_name_tool -add_rpath $out/lib $out/${python3.sitePackages}/_mupdf.so
+      ''
+    ));
 
   enableParallelBuilding = true;
 
@@ -195,7 +264,13 @@ stdenv.mkDerivation rec {
     tests = {
       inherit cups-filters zathura;
       inherit (python3.pkgs) pikepdf pymupdf;
-      mupdf-all = mupdf.override { enableCurl = true; enableGL = true; enableOcr = true; enableCxx = true; enablePython = true; };
+      mupdf-all = mupdf.override {
+        enableCurl = true;
+        enableGL = true;
+        enableOcr = true;
+        enableCxx = true;
+        enablePython = true;
+      };
     };
 
     updateScript = gitUpdater {
@@ -212,8 +287,6 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ fpletz ];
     platforms = platforms.unix;
     mainProgram = "mupdf";
-    # ImportError: cannot import name '_mupdf' from partially initialized module 'mupdf'
-    # (most likely due to a circular import)
-    broken = enablePython;
+    broken = enableCxx; # see https://bugs.ghostscript.com/show_bug.cgi?id=708041
   };
 }
