59070b8044d202849b42d4af1632feaded2f58ef
codgician <15964984+codgician@users.noreply.github.com>
2024-09-20 02:12:04 +0800

    linuxPackages_latest.prl-tools: 20.0.0-55653 -> 20.0.1-55659

--- pkgs/os-specific/linux/prl-tools/default.nix
+++ pkgs/os-specific/linux/prl-tools/default.nix
@@ -37,13 +37,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "prl-tools";
-  version = "20.0.0-55653";
+  version = "20.0.1-55659";
 
   # We download the full distribution to extract prl-tools-lin.iso from
   # => ${dmg}/Parallels\ Desktop.app/Contents/Resources/Tools/prl-tools-lin.iso
   src = fetchurl {
     url = "https://download.parallels.com/desktop/v${lib.versions.major finalAttrs.version}/${finalAttrs.version}/ParallelsDesktop-${finalAttrs.version}.dmg";
-    hash = "sha256-ohGhaLVzXuR/mQ6ToeGbTixKy01F14JSgTs128vGZXM=";
+    hash = "sha256-5h8WZB7L6D9KOgIPSstN1sNcf3FZQiOQFB5MUC4YzvA=";
   };
 
   hardeningDisable = [ "pic" "format" ];
