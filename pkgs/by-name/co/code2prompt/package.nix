97c03659ef05816d9d5c0d64939c5efc5525e355
Hernan <hernan@hernancosi.com>
2024-09-19 00:51:49 -0700

    formatting from nix-shell

--- pkgs/by-name/co/code2prompt/package.nix
+++ pkgs/by-name/co/code2prompt/package.nix
@@ -1,12 +1,12 @@
-{ lib
-, fetchFromGitHub
-, rustPlatform
-, pkg-config
-, openssl
-, perl
-, stdenv
-, darwin
-,
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  pkg-config,
+  openssl,
+  perl,
+  stdenv,
+  darwin,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -20,23 +20,29 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-ESgSMkid92ojTcT/iPskz7S4dJQnigtxdj5IlS4xvhc=";
   };
 
-  cargoLock = { lockFile = ./Cargo.lock; };
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
 
   postPatch = ''
     # src is missing Cargo.lock
     ln -s ${./Cargo.lock} Cargo.lock
   '';
 
-  nativeBuildInputs = [ pkg-config perl ];
-
-  buildInputs = [ openssl ] ++ lib.optionals stdenv.isDarwin [
-    darwin.apple_sdk.frameworks.Security
-    darwin.apple_sdk.frameworks.AppKit
+  nativeBuildInputs = [
+    pkg-config
+    perl
   ];
 
+  buildInputs =
+    [ openssl ]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.AppKit
+    ];
+
   meta = {
-    description =
-      "A CLI tool that converts your codebase into a single LLM prompt with a source tree, prompt templating, and token counting";
+    description = "A CLI tool that converts your codebase into a single LLM prompt with a source tree, prompt templating, and token counting";
     homepage = "https://github.com/mufeedvh/code2prompt";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ heisfer ];
