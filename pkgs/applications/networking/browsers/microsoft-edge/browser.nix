5410f08a32dfbc86d4671be7ba5e5302de942f13
Marley <marleypowell9@gmail.com>
2024-09-19 09:25:54 +0100

    microsoft-edge: Add alsa-plugins path to ALSA_PLUGIN_DIR

--- pkgs/applications/networking/browsers/microsoft-edge/browser.nix
+++ pkgs/applications/networking/browsers/microsoft-edge/browser.nix
@@ -178,6 +178,7 @@ stdenv.mkDerivation rec {
   postFixup = ''
     wrapProgram "$out/bin/${longName}" \
       --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.pname}-${gtk3.version}" \
+      --set ALSA_PLUGIN_DIR ${prev.alsa-plugins}/lib/alsa-lib/ \
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
       --add-flags ${lib.escapeShellArg commandLineArgs}
   '';
