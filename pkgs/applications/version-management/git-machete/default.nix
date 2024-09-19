d4ae0166e59538b67b69527bd964881d570e0af3
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-19 12:45:56 +0000

    git-machete: 3.29.2 -> 3.29.3

--- pkgs/applications/version-management/git-machete/default.nix
+++ pkgs/applications/version-management/git-machete/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonApplication rec {
   pname = "git-machete";
-  version = "3.29.2";
+  version = "3.29.3";
 
   src = fetchFromGitHub {
     owner = "virtuslab";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-C3AQ5HHsD2JfF/BO+t3Rbx1DQwRXMG41i2wsk/9BkF8=";
+    hash = "sha256-3GXTdIXITZeDqe6gxwOCaFXwITYYfXTy57H2AHA5Zyc=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
