3fc5f627496b21c1cc4dac34a676337962fef4bd
Gaetan Lepage <gaetan@glepage.com>
2024-09-19 08:49:25 +0200

    python312Packages.treescope: init at 0.1.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15773,6 +15773,8 @@ self: super: with self; {
 
   treeo = callPackage ../development/python-modules/treeo { };
 
+  treescope = callPackage ../development/python-modules/treescope { };
+
   treex = callPackage ../development/python-modules/treex { };
 
   treq = callPackage ../development/python-modules/treq { };
