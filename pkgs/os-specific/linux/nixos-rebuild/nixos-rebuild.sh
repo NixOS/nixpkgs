1bf0ffb22ea32cc83001483e16215bf1f9461f2f
K900 <me@0upti.me>
2024-09-19 20:05:58 +0300

    nixos-rebuild: inline get-version-suffix script
    It has no other consumers anyway. Also saves some work.

--- pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
+++ pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
@@ -548,6 +548,26 @@ getNixDrv() {
     fi
 }
 
+getVersion() {
+    local dir="$1"
+    local rev=
+    local gitDir="$dir/.git"
+    if [ -e "$gitDir" ]; then
+        if [ -z "$(type -P git)" ]; then
+            echo "warning: Git not found; cannot figure out revision of $dir" >&2
+            return
+        fi
+        cd "$dir"
+        rev=$(git --git-dir="$gitDir" rev-parse --short HEAD)
+        if git --git-dir="$gitDir" describe --always --dirty | grep -q dirty; then
+            rev+=M
+        fi
+    fi
+
+    echo ".git.$rev"
+}
+
+
 if [[ -n $buildNix && -z $flake ]]; then
     log "building Nix..."
     getNixDrv
@@ -569,7 +589,7 @@ fi
 # nixos-version shows something useful).
 if [[ -n $canRun && -z $flake ]]; then
     if nixpkgs=$(runCmd nix-instantiate --find-file nixpkgs "${extraBuildFlags[@]}"); then
-        suffix=$(runCmd $SHELL "$nixpkgs/nixos/modules/installer/tools/get-version-suffix" "${extraBuildFlags[@]}" || true)
+        suffix=$(getVersion "$nixpkgs" || true)
         if [ -n "$suffix" ]; then
             echo -n "$suffix" > "$nixpkgs/.version-suffix" || true
         fi
