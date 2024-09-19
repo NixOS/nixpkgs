abbef2d9e89e6bbcf17f13c0a4bd7ef79598a6fb
scrufulufugus <smonson@irbash.net>
2024-09-18 21:58:12 -0400

    system76-scheduler: migrate to pkgs/by-name format

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26063,8 +26063,6 @@ with pkgs;
 
   system76-power = callPackage ../os-specific/linux/system76-power { };
 
-  system76-scheduler = callPackage ../os-specific/linux/system76-scheduler { };
-
   ocf-resource-agents = callPackage ../os-specific/linux/ocf-resource-agents { };
 
   open-vm-tools-headless = open-vm-tools.override { withX = false; };
