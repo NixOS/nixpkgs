51a47282bad6227f30a3da32fcc6fd7a3c6f49b3
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-19 23:25:44 +0200

    python312Packages.reolink-aio: 0.9.8 -> 0.9.9
    Diff: starkillerOG/reolink_aio@refs/tags/0.9.8...0.9.9
    
    Changelog: https://github.com/starkillerOG/reolink_aio/releases/tag/0.9.9

--- pkgs/development/python-modules/reolink-aio/default.nix
+++ pkgs/development/python-modules/reolink-aio/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   version = "0.9.9";
   pyproject = true;
 
-  disabled = pythonOlder "3.9";
+  disabled = pythonOlder "3.10";
 
   src = fetchFromGitHub {
     owner = "starkillerOG";
@@ -42,7 +42,7 @@ buildPythonPackage rec {
     description = "Module to interact with the Reolink IP camera API";
     homepage = "https://github.com/starkillerOG/reolink_aio";
     changelog = "https://github.com/starkillerOG/reolink_aio/releases/tag/${version}";
-    license = with licenses; [ mit ];
+    license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
 }
