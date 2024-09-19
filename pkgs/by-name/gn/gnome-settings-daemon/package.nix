c73a0e3e2f46349b3157413cb9a7a0e5e176a795
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2024-09-19 23:59:27 +0000

    gnome-settings-daemon: 47.beta → 47.1
    https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/compare/47.beta...47.1
    
    Applied from pull request 343127.
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/by-name/gn/gnome-settings-daemon/package.nix
+++ pkgs/by-name/gn/gnome-settings-daemon/package.nix
@@ -42,11 +42,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-settings-daemon";
-  version = "47.beta";
+  version = "47.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-settings-daemon/${lib.versions.major finalAttrs.version}/gnome-settings-daemon-${finalAttrs.version}.tar.xz";
-    hash = "sha256-uY7RXPmWYNhIsIxZeth2n4w8JVnB4wvF5Burza/QOCo=";
+    hash = "sha256-8qrL5V+jjocIWD7sCmZRBJ5TfrUFo+0s4Lqk6bZCRtE=";
   };
 
   patches = [
@@ -113,7 +113,7 @@ stdenv.mkDerivation (finalAttrs: {
   env.NIX_CFLAGS_COMPILE = "-DG_DISABLE_CAST_CHECKS";
 
   postPatch = ''
-    for f in gnome-settings-daemon/codegen.py plugins/power/gsd-power-constants-update.pl; do
+    for f in plugins/power/gsd-power-constants-update.pl; do
       chmod +x $f
       patchShebangs $f
     done
