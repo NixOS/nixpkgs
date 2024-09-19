bb1dc69cf5e59ce8ac7da16868c17a171dad534c
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2024-09-19 15:45:49 -0400

    Ballerina 2201.9.2 -> 2201.10.0
    Update Ballerina release to version 2201.10.0

--- pkgs/development/compilers/ballerina/default.nix
+++ pkgs/development/compilers/ballerina/default.nix
@@ -1,6 +1,6 @@
 { ballerina, lib, writeText, runCommand, makeWrapper, fetchzip, stdenv, openjdk }:
 let
-  version = "2201.9.2";
+  version = "2201.10.0";
   codeName = "swan-lake";
 in stdenv.mkDerivation {
   pname = "ballerina";
@@ -8,7 +8,7 @@ in stdenv.mkDerivation {
 
   src = fetchzip {
     url = "https://dist.ballerina.io/downloads/${version}/ballerina-${version}-${codeName}.zip";
-    hash = "sha256-J73fai5mPPNHk/wux4nhX3KsalxWER8nMqWovWgm6WA=";
+    hash = "sha256-S4GyxqrMa8JTBKYFyf3K3Ba88vcsFXWqpPtYP1O/dw0=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
