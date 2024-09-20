ade0a3395523bf8f3e280f9675e0c2275fc4071e
Cyril Cohen <cohen@crans.org>
2024-09-20 15:58:25 +0200

    Adding a github artifact option

--- pkgs/development/ocaml-modules/elpi/default.nix
+++ pkgs/development/ocaml-modules/elpi/default.nix
@@ -1,5 +1,4 @@
 { lib
-, fetchurl
 , buildDunePackage, camlp5
 , ocaml
 , menhir, menhirLib
@@ -8,6 +7,7 @@
 , re, perl, ncurses
 , ppxlib, ppx_deriving
 , ppxlib_0_15, ppx_deriving_0_15
+, coqPackages
 , version ? if lib.versionAtLeast ocaml.version "4.08" then "1.18.1"
     else "1.15.2"
 }:
@@ -15,29 +15,25 @@
 let p5 = camlp5; in
 let camlp5 = p5.override { legacy = true; }; in
 
-let tarball =
-  if lib.versionAtLeast version "1.13.8"
-  then "elpi-${version}.tbz"
-  else "elpi-v${version}.tbz"
-; in
-
+let fetched = coqPackages.metaFetch ({
+    release."1.19.2".sha256 = "sha256-dBj5Ek7PWq/8Btq/dggJUqa8cUtfvbi6EWo/lJEDOU4=";
+    release."1.18.1".sha256 = "sha256-rrIv/mVC0Ez3nU7fpnzwduIC3tI6l73DjgAbv1gd2v0=";
+    release."1.17.0".sha256 = "sha256-J8FJBeaB+2HtHjrkgNzOZJngZ2AcYU+npL9Y1HNPnzo=";
+    release."1.15.2".sha256 = "sha256-+sQYQiN3n+dlzXzi5opOjhkJZqpkNwlHZcUjaUM6+xQ=";
+    release."1.15.0".sha256 = "sha256-vpQzbkDqJPCmaBmXcBnzlWGS7djW9wWv8xslkIlXgP0=";
+    release."1.13.7".sha256 = "sha256-0QbOEnrRCYA2mXDGRKe+QYCXSESLJvLzRW0Iq+/3P9Y=";
+    release."1.12.0".sha256 = "sha256-w4JzLZB8jcxw7nA7AfgU9jTZTr6IYUxPU5E2vNIFC4Q=";
+    release."1.11.4".sha256 = "sha256-dyzEpzokgffsF9lt+FZgUlcZEuAb70vGuHfGUtjZYIM=";
+    releaseRev = v: "v${v}";
+    releaseArtifact = v: if lib.versionAtLeast v "1.13.8"
+      then "elpi-${v}.tbz"
+      else "elpi-v${v}.tbz";
+    location = { domain = "github.com"; owner = "LPCIC"; repo = "elpi"; };
+  }) version;
+in
 buildDunePackage {
   pname = "elpi";
-  inherit version;
-
-  src = fetchurl {
-    url = "https://github.com/LPCIC/elpi/releases/download/v${version}/${tarball}";
-    hash = {
-      "1.19.2" = "sha256-dBj5Ek7PWq/8Btq/dggJUqa8cUtfvbi6EWo/lJEDOU4=";
-      "1.18.1" = "sha256-rrIv/mVC0Ez3nU7fpnzwduIC3tI6l73DjgAbv1gd2v0=";
-      "1.17.0" = "sha256-J8FJBeaB+2HtHjrkgNzOZJngZ2AcYU+npL9Y1HNPnzo=";
-      "1.15.2" = "sha256-+sQYQiN3n+dlzXzi5opOjhkJZqpkNwlHZcUjaUM6+xQ=";
-      "1.15.0" = "sha256-vpQzbkDqJPCmaBmXcBnzlWGS7djW9wWv8xslkIlXgP0=";
-      "1.13.7" = "sha256-0QbOEnrRCYA2mXDGRKe+QYCXSESLJvLzRW0Iq+/3P9Y=";
-      "1.12.0" = "sha256-w4JzLZB8jcxw7nA7AfgU9jTZTr6IYUxPU5E2vNIFC4Q=";
-      "1.11.4" = "sha256-dyzEpzokgffsF9lt+FZgUlcZEuAb70vGuHfGUtjZYIM=";
-    }."${version}";
-  };
+  inherit (fetched) version src;
 
   patches = lib.optional (version == "1.16.5")
     ./atd_2_10.patch;
