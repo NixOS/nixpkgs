1bdcb1bacc0abde25b93e786f083469cbdff490a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-09-20 15:31:07 +0200

    fatrop: 0.0.3 -> 0.0.4

--- pkgs/by-name/fa/fatrop/package.nix
+++ pkgs/by-name/fa/fatrop/package.nix
@@ -2,7 +2,6 @@
   blasfeo,
   cmake,
   fetchFromGitHub,
-  fetchpatch,
   lib,
   llvmPackages,
   python3Packages,
@@ -12,28 +11,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fatrop";
-  version = "0.0.3";
+  version = "0.0.4";
 
   src = fetchFromGitHub {
     owner = "meco-group";
     repo = "fatrop";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-vCGix3qYQR9bY9GoIyBMrTNvsMgt0h7TZUye6wlH9H8=";
+    hash = "sha256-XVOS9L2vQeFkPXZieX1ZJiVagR0f2BtiRmSDPB9LQeI=";
   };
-
-  patches = lib.optionals pythonSupport [
-    # fix python packaging
-    # ref. https://github.com/meco-group/fatrop/pull/17
-    # this was merged upstream and can be removed on next release
-    (fetchpatch {
-      url = "https://github.com/meco-group/fatrop/pull/17/commits/22e33c216e47df90dc060686d7d1806233642249.patch";
-      hash = "sha256-0/uSHAXVzXVyR+kklQGvraLA6sJbHzUcAp3eEHQK068=";
-    })
-    (fetchpatch {
-      url = "https://github.com/meco-group/fatrop/pull/17/commits/0c03fd9fec95de42976fed1770a15081d0874ee2.patch";
-      hash = "sha256-FenQ05rqn9EbU0wDVQQ1OFxSXj1fL/rOFKOcP8t1NwY=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake ];
   buildInputs =
