19635fef8f61273320a5f1528c4a4006b120a88e
h7x4 <h7x4@nani.wtf>
2024-09-20 10:50:13 +0200

    nrfutil: 7.11.1 -> 7.13.0

--- pkgs/by-name/nr/nrfutil/source.nix
+++ pkgs/by-name/nr/nrfutil/source.nix
@@ -1,15 +1,15 @@
 {
-  version = "7.11.1";
+  version = "7.13.0";
   x86_64-linux = {
     name = "x86_64-unknown-linux-gnu";
-    hash = "sha256-faF/iA07wWiAuxeqEZciIYlnoWe4LKCtDxD0BwIyeYw=";
+    hash = "sha256-R3OF/340xEab+0zamfwvejY16fjy/3TrzMvQaBlVxHw=";
   };
   x86_64-darwin = {
     name = "x86_64-apple-darwin";
-    hash = "sha256-EImYXMIvxPgzaGuAOWi4O6mG5S1awdGSX0HQgT1iHaI=";
+    hash = "sha256-cnZkVkTbQ/+ciITPEx2vxxZchCC54T0JOApB4HKp8e0=";
   };
   aarch64-darwin = {
     name = "aarch64-apple-darwin";
-    hash = "sha256-jgigeJRHH/c761wYGMHhtRHE59ms7obZPxiH5pKeoeQ=";
+    hash = "sha256-5VxDQ25tW+qTXHwkltpaAm4AnQvA18qGMaflYQzE2pQ=";
   };
 }
