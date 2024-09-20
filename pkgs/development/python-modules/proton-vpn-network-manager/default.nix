edd749df0292f1efbcd9e907ab1e69446c1c2653
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2024-09-20 14:55:00 +0300

    python3Packages.proton-vpn-network-manager: 0.5.2 -> 0.6.3
    Update to the latest versions.
    
    The proton-vpn-network-manager-wireguard and proton-vpn-network-manager-openvpn
    modules are now legacy: The same functionality is now in proton-vpn-network-manager module,
    version 0.6.3 and upwards.

--- pkgs/development/python-modules/proton-vpn-network-manager/default.nix
+++ pkgs/development/python-modules/proton-vpn-network-manager/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "proton-vpn-network-manager";
-  version = "0.5.2";
+  version = "0.6.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ProtonVPN";
     repo = "python-proton-vpn-network-manager";
     rev = "refs/tags/v${version}";
-    hash = "sha256-hTJE9sUjPMsE9d0fIA/OhoasumtfsWuFwn0aTm10PN4=";
+    hash = "sha256-fbA3kvhU3l20+7irThiTk/fDe60yR4aWxhE3Ol2K7ow=";
   };
 
   nativeBuildInputs = [
@@ -53,6 +53,11 @@ buildPythonPackage rec {
     pytest-cov-stub
     pytest-asyncio
   ];
+
+  preCheck = ''
+    # Needed for Permission denied: '/homeless-shelter'
+    export HOME=$(mktemp -d)
+  '';
 
   meta = {
     description = "Provides the necessary functionality for other ProtonVPN components to interact with NetworkManager";
