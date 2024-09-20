a3492a8f1185cc1a2a6145ff4c9bf5d36c1d212f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-09-20 19:41:00 +1000

    awscli2: unpin python

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3171,11 +3171,6 @@ with pkgs;
 
   awscli = callPackage ../tools/admin/awscli { };
 
-  awscli2 = callPackage ../by-name/aw/awscli2/package.nix {
-    # change when https://github.com/aws/aws-cli/issues/8342 resolved
-    python3 = python311;
-  };
-
   okta-aws-cli = callPackage ../tools/admin/okta-aws-cli { };
 
   awsebcli = callPackage ../tools/virtualization/awsebcli { };
