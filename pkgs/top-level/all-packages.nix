4ad4b46c7142f0b24b9a6e6de4bc0f597e57ed3d
Martin Joerg <martin.joerg@gmail.com>
2024-09-20 08:41:30 +0000

    inxi: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8766,8 +8766,6 @@ with pkgs;
 
   invoice2data  = callPackage ../tools/text/invoice2data  { };
 
-  inxi = callPackage ../tools/system/inxi { };
-
   iodine = callPackage ../tools/networking/iodine { };
 
   ioc-scan = callPackage ../tools/security/ioc-scan { };
