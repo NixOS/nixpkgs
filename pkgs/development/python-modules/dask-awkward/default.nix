297f1bc1727a6c8ce490df633d4e03862c381b85
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-20 03:50:55 +0000

    python312Packages.dask-awkward: 2024.7.0 -> 2024.9.0

--- pkgs/development/python-modules/dask-awkward/default.nix
+++ pkgs/development/python-modules/dask-awkward/default.nix
@@ -28,7 +28,7 @@
 
 buildPythonPackage rec {
   pname = "dask-awkward";
-  version = "2024.7.0";
+  version = "2024.9.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -37,10 +37,8 @@ buildPythonPackage rec {
     owner = "dask-contrib";
     repo = "dask-awkward";
     rev = "refs/tags/${version}";
-    hash = "sha256-xy3rq/gXQUtquvyWSCcBjZ+gUYT3RzxMgXThyT6Fwec=";
+    hash = "sha256-4CwixPj0bJHVjnwZ7fPkRdiDHs8/IzvNlwSPynXvcAo=";
   };
-
-  pythonRelaxDeps = [ "awkward" ];
 
   build-system = [
     hatch-vcs
