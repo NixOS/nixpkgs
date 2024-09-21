8ecc78e6462ab136a3c42801b84e8591b2a3093e
Daniel Nagy <danielnagy@posteo.de>
2024-09-21 13:15:00 +0200

    onioncircuits: 0.7 -> 0.8.1

--- pkgs/tools/security/onioncircuits/default.nix
+++ pkgs/tools/security/onioncircuits/default.nix
@@ -1,7 +1,6 @@
 { stdenv
 , lib
 , fetchFromGitLab
-, fetchpatch
 , gobject-introspection
 , intltool
 , python3
@@ -10,14 +9,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "onioncircuits";
-  version = "0.7";
+  version = "0.8.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.tails.boum.org";
     owner = "tails";
     repo = "onioncircuits";
     rev = version;
-    sha256 = "sha256-O4tSbKBTmve4u8bXVg128RLyuxvTbU224JV8tQ+aDAQ=";
+    sha256 = "sha256-5VGOuvngZvUFQ+bubdt4YV3/IflOhBB1i+oEQaV4kr0=";
   };
 
   nativeBuildInputs = [
@@ -32,15 +31,6 @@ python3.pkgs.buildPythonApplication rec {
     stem
   ];
 
-  patches = [
-    # Fix https://gitlab.tails.boum.org/tails/onioncircuits/-/merge_requests/4
-    (fetchpatch {
-      name = "fix-setuptool-package-discovery.patch";
-      url = "https://gitlab.tails.boum.org/tails/onioncircuits/-/commit/4c620c77f36f540fa27041fcbdeaf05c9f57826c.patch";
-      sha256 = "sha256-WXqyDa2meRMMHkHLO5Xl7x43KUGtlsai+eOVzUGUPpo=";
-    })
-  ];
-
   postInstall = ''
     mkdir -p $out/etc/apparmor.d
 
@@ -48,7 +38,6 @@ python3.pkgs.buildPythonApplication rec {
   '';
 
   meta = with lib; {
-    broken = stdenv.hostPlatform.isDarwin;
     homepage = "https://tails.boum.org";
     description = "GTK application to display Tor circuits and streams";
     mainProgram = "onioncircuits";
