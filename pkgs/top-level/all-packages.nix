efee20930924bc17417d726977aacb9ca0b79e69
Randy Eckenrode <randy@largeandhighquality.com>
2024-09-20 20:41:00 -0400

    apple-sdk_10_13: init at 10.13.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23603,6 +23603,7 @@ with pkgs;
   ### DEVELOPMENT / LIBRARIES / DARWIN SDKS
 
   apple-sdk_10_12 = callPackage ../by-name/ap/apple-sdk/package.nix { darwinSdkMajorVersion = "10.12"; };
+  apple-sdk_10_13 = callPackage ../by-name/ap/apple-sdk/package.nix { darwinSdkMajorVersion = "10.13"; };
   apple-sdk_11 = callPackage ../by-name/ap/apple-sdk/package.nix { darwinSdkMajorVersion = "11"; };
 
   darwinMinVersionHook =
