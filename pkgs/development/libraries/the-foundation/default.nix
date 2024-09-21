780a49292920669e4afff1a029e23b8d3bc205c8
Nikolay Korotkiy <sikmir@disroot.org>
2024-09-21 15:12:10 +0400

    the-foundation: 1.8.1 → 1.9.0

--- pkgs/development/libraries/the-foundation/default.nix
+++ pkgs/development/libraries/the-foundation/default.nix
@@ -1,41 +1,55 @@
-{ lib
-, stdenv
-, fetchFromGitea
-, cmake
-, pkg-config
-, curl
-, libunistring
-, openssl
-, pcre
-, zlib
+{
+  lib,
+  stdenv,
+  fetchFromGitea,
+  cmake,
+  pkg-config,
+  curl,
+  libunistring,
+  openssl,
+  pcre,
+  zlib,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "the-foundation";
-  version = "1.8.1";
+  version = "1.9.0";
 
   src = fetchFromGitea {
     domain = "git.skyjake.fi";
     owner = "skyjake";
     repo = "the_Foundation";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-gyDBK/bF+QxXMnthUfMjeUuRBZk0Xcahm7wOtLGs5kY=";
+    hash = "sha256-hrwnY8m4xW8Sr2RunwD5VB+gnYUtZxXyGoiO3N23qGM=";
   };
 
-  nativeBuildInputs = [ cmake pkg-config ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
 
-  buildInputs = [ curl libunistring openssl pcre zlib ];
+  buildInputs = [
+    curl
+    libunistring
+    openssl
+    pcre
+    zlib
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeFeature "UNISTRING_DIR" "${libunistring}")
+  ];
 
   postFixup = ''
     substituteInPlace "$out"/lib/pkgconfig/the_Foundation.pc \
       --replace '="''${prefix}//' '="/'
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Opinionated C11 library for low-level functionality";
     homepage = "https://git.skyjake.fi/skyjake/the_Foundation";
-    license = licenses.bsd2;
-    maintainers = with maintainers; [ sikmir ];
-    platforms = platforms.unix;
+    license = lib.licenses.bsd2;
+    maintainers = with lib.maintainers; [ sikmir ];
+    platforms = lib.platforms.unix;
   };
 })
