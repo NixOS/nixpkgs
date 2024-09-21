253688541aa3bd6033d68e83c22cfc3b353fe53e
Valter Nazianzeno <manipuladordedados@gmail.com>
2024-09-21 20:54:21 +0000

    emiluaPlugins.botan: init at 1.1.0

--- pkgs/top-level/emilua-plugins.nix
+++ pkgs/top-level/emilua-plugins.nix
@@ -9,6 +9,9 @@ emilua:
 (lib.makeScope newScope (self: {
   inherit emilua;
   beast = self.callPackage ../development/emilua-plugins/beast { };
+  botan = self.callPackage ../development/emilua-plugins/botan {
+    inherit (pkgs) botan3;
+  };
   qt5 = self.callPackage ../development/emilua-plugins/qt5 { };
   qt6 = self.callPackage ../development/emilua-plugins/qt6 { };
 }))
