14b02d33a5cd82166d2f9c71194759fc54ab4ad2
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-21 10:48:53 +0200

    python312Packages.aiovlc: update disabled

--- pkgs/development/python-modules/aiovlc/default.nix
+++ pkgs/development/python-modules/aiovlc/default.nix
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   version = "0.5.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.9";
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "MartinHjelmare";
