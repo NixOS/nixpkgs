a4a742b6b6b65c570be2f723cbad0d467f5acc69
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2024-09-21 11:28:57 +0200

    maintainers: add fnune

--- pkgs/kde/third-party/koi/default.nix
+++ pkgs/kde/third-party/koi/default.nix
@@ -50,6 +50,6 @@ stdenv.mkDerivation rec {
     license = licenses.lgpl3;
     platforms = platforms.linux;
     homepage = "https://github.com/baduhai/Koi";
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ fnune ];
   };
 }
