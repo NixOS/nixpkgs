8818274937d216d911b8e79a98b3b31c642bc96e
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2024-09-21 02:12:53 -0600

    gle: format

--- pkgs/development/libraries/gle/default.nix
+++ pkgs/development/libraries/gle/default.nix
@@ -1,8 +1,32 @@
-{lib, stdenv, fetchurl, libglut, libX11, libXt, libXmu, libXi, libXext, libGL, libGLU}:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  libglut,
+  libX11,
+  libXt,
+  libXmu,
+  libXi,
+  libXext,
+  libGL,
+  libGLU,
+}:
+
 stdenv.mkDerivation rec {
   pname = "gle";
   version = "3.1.0";
-  buildInputs = [libGLU libGL libglut libX11 libXt libXmu libXi libXext];
+
+  buildInputs = [
+    libGLU
+    libGL
+    libglut
+    libX11
+    libXt
+    libXmu
+    libXi
+    libXext
+  ];
+
   src = fetchurl {
     urls = [
       "mirror://sourceforge/project/gle/gle/gle-${version}/gle-${version}.tar.gz"
@@ -10,10 +34,11 @@ stdenv.mkDerivation rec {
       ];
     sha256 = "09zs1di4dsssl9k322nzildvf41jwipbzhik9p43yb1bcfsp92nw";
   };
+
   meta = {
     description = "Tubing and extrusion library";
-    license = lib.licenses.gpl2 ;
-    maintainers = [lib.maintainers.raskin];
+    license = lib.licenses.gpl2;
+    maintainers = [ lib.maintainers.raskin ];
     platforms = lib.platforms.linux;
   };
 }
