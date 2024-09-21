da018db1441dd743283b504579a3a076ff437931
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-09-21 15:44:51 +0000

    nixos/xen: fix spurious ShellCheck warning
    It thinks we want to expand the `*` regex expressions inside the `sed`
    commands. We do not.
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- nixos/modules/virtualisation/xen-dom0.nix
+++ nixos/modules/virtualisation/xen-dom0.nix
@@ -32,6 +32,10 @@ let
     runtimeEnv = {
       efiMountPoint = config.boot.loader.efi.efiSysMountPoint;
     };
+
+    # We disable SC2016 because we don't want to expand the regexes in the sed commands.
+    excludeShellChecks = [ "SC2016" ];
+
     text = builtins.readFile ./xen-boot-builder.sh;
   };
 in
