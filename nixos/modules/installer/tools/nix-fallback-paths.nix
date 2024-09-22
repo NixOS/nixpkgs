af7bcfae7844484d50ea54dbee8b42dab6b5200e
Jörg Thalheim <joerg@thalheim.io>
2024-09-22 09:11:14 +0200

    nix: update fallback-paths

--- nixos/modules/installer/tools/nix-fallback-paths.nix
+++ nixos/modules/installer/tools/nix-fallback-paths.nix
@@ -1,8 +1,8 @@
 {
-  x86_64-linux = "/nix/store/b9kk9p6ankg080wh70smhg44dyan78kn-nix-2.24.2";
-  i686-linux = "/nix/store/nin0kajw977j8hhbyamgv5mkclssr03x-nix-2.24.2";
-  aarch64-linux = "/nix/store/pghw4m535hnrwkdlm7bamxca6lbx6n7z-nix-2.24.2";
-  riscv64-linux = "/nix/store/jcva3mcba7qn4waxkmv77w2r44q7wmq0-nix-riscv64-unknown-linux-gnu-2.24.2";
-  x86_64-darwin = "/nix/store/fkvinbdryqr854hk9i9hpdcqqaqln39m-nix-2.24.2";
-  aarch64-darwin = "/nix/store/z9j3va2vs4ycl9nxxph335c3jf47l27h-nix-2.24.2";
+  x86_64-linux = "/nix/store/s429drnj3jp5dbxib74r3q0rypknvw48-nix-2.24.7";
+  i686-linux = "/nix/store/1ysk63fwjvx4whxqpka1x4lyjb4m5576-nix-2.24.7";
+  aarch64-linux = "/nix/store/8av0avdm2anwmvf9qka02cz093599rw7-nix-2.24.7";
+  riscv64-linux = "/nix/store/b70zcmr3zrd7q569zzpz8dln93d3sx39-nix-riscv64-unknown-linux-gnu-2.24.7";
+  x86_64-darwin = "/nix/store/gba97bmnyrp6x936kr8zqc2mkhcrz1vf-nix-2.24.7";
+  aarch64-darwin = "/nix/store/7mb1mcpmjphg5bgc8m7m0gxrsshrzsk7-nix-2.24.7";
 }
