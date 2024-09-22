3fd9ea67f6259fe1a7d23f035e4b3e5e1c269fc0
Jared Baur <jaredbaur@fastmail.com>
2024-09-22 12:00:48 -0700

    nixos/tmpfiles: only setup nix tmpfiles when nix is used
    If nix is not installed on the end system, the tmpfiles settings related
    to it do not need to be setup.

--- nixos/modules/system/boot/systemd/tmpfiles.nix
+++ nixos/modules/system/boot/systemd/tmpfiles.nix
@@ -281,15 +281,19 @@ in
     ) cfg.settings);
 
     systemd.tmpfiles.rules = [
-      "d  /nix/var                           0755 root root - -"
-      "L+ /nix/var/nix/gcroots/booted-system 0755 root root - /run/booted-system"
       "d  /run/lock                          0755 root root - -"
       "d  /var/db                            0755 root root - -"
       "L  /var/lock                          -    -    -    - ../run/lock"
-      # Boot-time cleanup
+    ] ++ lib.optionals config.nix.enable [
+      "d  /nix/var                           0755 root root - -"
+      "L+ /nix/var/nix/gcroots/booted-system 0755 root root - /run/booted-system"
+    ]
+    # Boot-time cleanup
+    ++ [
       "R! /etc/group.lock                    -    -    -    - -"
       "R! /etc/passwd.lock                   -    -    -    - -"
       "R! /etc/shadow.lock                   -    -    -    - -"
+    ] ++ lib.optionals config.nix.enable [
       "R! /nix/var/nix/gcroots/tmp           -    -    -    - -"
       "R! /nix/var/nix/temproots             -    -    -    - -"
     ];
