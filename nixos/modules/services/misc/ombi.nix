9883191029f49058ebaa97698400521472e024f5
h7x4 <h7x4@nani.wtf>
2024-09-22 03:05:08 +0200

    nixos/ombi: tmpfiles.rules -> tmpfiles.settings

--- nixos/modules/services/misc/ombi.nix
+++ nixos/modules/services/misc/ombi.nix
@@ -45,9 +45,10 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.tmpfiles.rules = [
-      "d '${cfg.dataDir}' 0700 ${cfg.user} ${cfg.group} - -"
-    ];
+    systemd.tmpfiles.settings."10-ombi".${cfg.dataDir}.d = {
+      inherit (cfg) user group;
+      mode = "0700";
+    };
 
     systemd.services.ombi = {
       description = "Ombi";
