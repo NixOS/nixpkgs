4a815265c4d4e2dc95fbf12df68c6053ff6792e0
Stig Palmquist <git@stig.io>
2024-09-22 18:45:56 +0200

    perl540: fix build in stage1, add C locale patch
    - Add patch from Perl upstream fixing build if only C locale is
      available
    
    - Change back from `perl538` to `perl` (perl540) in bootstrap-stage-1

--- pkgs/development/interpreters/perl/interpreter.nix
+++ pkgs/development/interpreters/perl/interpreter.nix
@@ -70,6 +70,9 @@ stdenv.mkDerivation (rec {
     ++ lib.optional (lib.versionOlder version "5.38.0") ./no-sys-dirs-5.31.patch
     ++ lib.optional (lib.versionAtLeast version "5.38.0") ./no-sys-dirs-5.38.0.patch
 
+    # Fix compilation on platforms with only a C locale: https://github.com/Perl/perl5/pull/22569
+    ++ lib.optional (version == "5.40.0") ./fix-build-with-only-C-locale-5.40.0.patch
+
     ++ lib.optional stdenv.hostPlatform.isSunOS ./ld-shared.patch
     ++ lib.optionals stdenv.hostPlatform.isDarwin [ ./cpp-precomp.patch ./sw_vers.patch ]
     ++ lib.optional crossCompiling ./cross.patch;
