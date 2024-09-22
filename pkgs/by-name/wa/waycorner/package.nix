bf643f094dbaf701af451d08072bf81253882234
NotAShelf <raf@notashelf.dev>
2024-09-22 09:14:20 +0300

    waycorner: modernize derivation
    - Use `refs/tags/${version}` over ${version}
    - Get rid of nested `with lib;`
    - Remove redundant build input (tested build & functionality)
    - Update changelog link to use current ref

--- pkgs/by-name/wa/waycorner/package.nix
+++ pkgs/by-name/wa/waycorner/package.nix
@@ -9,33 +9,34 @@
 rustPlatform.buildRustPackage rec {
   pname = "waycorner";
   version = "0.2.3";
+
   src = fetchFromGitHub {
     owner = "AndreasBackx";
     repo = "waycorner";
-    rev = version;
+    rev = "refs/tags/${version}";
     hash = "sha256-b8juIhJ3kh+NJc8RUVVoatqjWISSW0ir/vk2Dz/428Y=";
   };
+
   cargoHash = "sha256-LGxFRGzQ8jOfxT5di3+YGqfS5KM4+Br6KlTFpPbkJyU=";
-  buildInputs = [
-    wayland
-  ];
+
   nativeBuildInputs = [
     pkg-config
     makeWrapper
   ];
+
   postFixup = ''
     # the program looks for libwayland-client.so at runtime
     wrapProgram $out/bin/waycorner \
       --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ wayland ]}
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Hot corners for Wayland";
     mainProgram = "waycorner";
-    changelog = "https://github.com/AndreasBackx/waycorner/blob/main/CHANGELOG.md";
+    changelog = "https://github.com/AndreasBackx/waycorner/blob/${version}/CHANGELOG.md";
     homepage = "https://github.com/AndreasBackx/waycorner";
-    platforms = platforms.linux;
-    license = licenses.mit;
-    maintainers = with maintainers; [ NotAShelf ];
+    platforms = lib.platforms.linux;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ NotAShelf ];
   };
 }
