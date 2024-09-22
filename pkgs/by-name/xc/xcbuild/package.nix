6007749d13ad6f26caeb37fa55a0a979e8057119
Randy Eckenrode <randy@largeandhighquality.com>
2024-09-22 18:44:26 -0400

    xcbuild: avoid `xcrun` invoking itself via `/usr/bin` stubs
    macOS ships with several stubs in `/usr/bin` that invoke `xcrun` to run
    the tools from the active SDK. When `/usr/bin` is in `PATH`, this will
    cause `xcrun` from xcbuild to invoke itself over and over. Filtering
    `/usr/bin` from `xcrun`’s search path prevents this from happening.

--- pkgs/by-name/xc/xcbuild/package.nix
+++ pkgs/by-name/xc/xcbuild/package.nix
@@ -84,6 +84,8 @@ stdenv.mkDerivation (finalAttrs: {
   patches = [
     # Add missing header for `abort`
     ./patches/includes.patch
+    # Prevent xcrun from recusively invoking itself
+    ./patches/Avoid-fork-bomb-when-searching-system-paths.patch
   ];
 
   prePatch = ''
