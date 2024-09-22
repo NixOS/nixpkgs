479a1a6c167a070400057e20a87d8af302d1cda1
Robert Schütz <mail@dotlambda.de>
2024-09-21 18:36:36 -0700

    nbqa: add passthru.withTools
    You can now do fancy things like
    
        (nbqa.override { python3 = python311; }).withTools (ps: [ ps.black ])

--- pkgs/by-name/nb/nbqa/package.nix
+++ pkgs/by-name/nb/nbqa/package.nix
@@ -9,91 +9,109 @@
   # tests
   versionCheckHook,
 }:
-python3.pkgs.buildPythonApplication rec {
-  pname = "nbqa";
-  version = "1.9.0";
-  pyproject = true;
 
-  src = fetchFromGitHub {
-    owner = "nbQA-dev";
-    repo = "nbQA";
-    rev = "refs/tags/${version}";
-    hash = "sha256-9s+q2unh+jezU0Er7ZH0tvgntmPFts9OmsgAMeQXRrY=";
-  };
+let
+  nbqa = python3.pkgs.buildPythonApplication rec {
+    pname = "nbqa";
+    version = "1.9.0";
+    pyproject = true;
 
-  build-system = with python3.pkgs; [
-    setuptools
-  ];
+    src = fetchFromGitHub {
+      owner = "nbQA-dev";
+      repo = "nbQA";
+      rev = "refs/tags/${version}";
+      hash = "sha256-9s+q2unh+jezU0Er7ZH0tvgntmPFts9OmsgAMeQXRrY=";
+    };
 
-  optional-dependencies.toolchain =
-    (with python3.pkgs; [
-      black
-      blacken-docs
-      flake8
-      isort
-      jupytext
-      mypy
-      pylint
-      pyupgrade
-    ])
-    ++ [
-      ruff
+    build-system = with python3.pkgs; [
+      setuptools
     ];
 
-  dependencies = with python3.pkgs; [
-    autopep8
-    ipython
-    tokenize-rt
-    tomli
-  ];
+    optional-dependencies.toolchain =
+      (with python3.pkgs; [
+        black
+        blacken-docs
+        flake8
+        isort
+        jupytext
+        mypy
+        pylint
+        pyupgrade
+      ])
+      ++ [
+        ruff
+      ];
 
-  postPatch = ''
-    # Force using the Ruff executable rather than the Python package
-    substituteInPlace nbqa/__main__.py --replace 'if shell:' 'if shell or main_command == "ruff":'
-  '';
+    dependencies = with python3.pkgs; [
+      autopep8
+      ipython
+      tokenize-rt
+      tomli
+    ];
 
-  preCheck = ''
-    # Allow the tests to run `nbqa` itself from the path
-    export PATH="$out/bin":"$PATH"
-  '';
+    postPatch = ''
+      # Force using the Ruff executable rather than the Python package
+      substituteInPlace nbqa/__main__.py --replace 'if shell:' 'if shell or main_command == "ruff":'
+    '';
 
-  nativeCheckInputs =
-    (with python3.pkgs; [
-      autoflake
-      distutils
-      mdformat
-      pre-commit-hooks
-      pydocstyle
-      pytestCheckHook
-      yapf
-    ])
-    ++ lib.flatten (lib.attrValues optional-dependencies)
-    ++ [ versionCheckHook ];
+    preCheck = ''
+      # Allow the tests to run `nbqa` itself from the path
+      export PATH="$out/bin":"$PATH"
+    '';
 
-  disabledTests = [
-    # Test data not found
-    "test_black_multiple_files"
-    "test_black_return_code"
-    "test_grep"
-    "test_jupytext_on_folder"
-    "test_mypy_works"
-    "test_running_in_different_dir_works"
-    "test_unable_to_reconstruct_message_pythonpath"
-    "test_with_subcommand"
-    "test_pylint_works"
-  ];
+    nativeCheckInputs =
+      (with python3.pkgs; [
+        autoflake
+        distutils
+        mdformat
+        pre-commit-hooks
+        pydocstyle
+        pytestCheckHook
+        yapf
+      ])
+      ++ lib.flatten (lib.attrValues optional-dependencies)
+      ++ [ versionCheckHook ];
 
-  disabledTestPaths = [
-    # Test data not found
-    "tests/test_include_exclude.py"
-  ];
+    disabledTests = [
+      # Test data not found
+      "test_black_multiple_files"
+      "test_black_return_code"
+      "test_grep"
+      "test_jupytext_on_folder"
+      "test_mypy_works"
+      "test_running_in_different_dir_works"
+      "test_unable_to_reconstruct_message_pythonpath"
+      "test_with_subcommand"
+      "test_pylint_works"
+    ];
 
-  meta = {
-    homepage = "https://github.com/nbQA-dev/nbQA";
-    changelog = "https://nbqa.readthedocs.io/en/latest/history.html";
-    description = "Run ruff, isort, pyupgrade, mypy, pylint, flake8, black, blacken-docs, and more on Jupyter Notebooks";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ l0b0 ];
-    mainProgram = "nbqa";
+    disabledTestPaths = [
+      # Test data not found
+      "tests/test_include_exclude.py"
+    ];
+
+    passthru = {
+      # selector is a function mapping pythonPackages to a list of code quality
+      # tools, e.g. nbqa.withTools (ps: [ ps.black ])
+      withTools =
+        selector:
+        nbqa.overridePythonAttrs (
+          { dependencies, ... }:
+          {
+            dependencies = dependencies ++ selector python3.pkgs;
+            doCheck = false;
+          }
+        );
+    };
+
+    meta = {
+      homepage = "https://github.com/nbQA-dev/nbQA";
+      changelog = "https://nbqa.readthedocs.io/en/latest/history.html";
+      description = "Run ruff, isort, pyupgrade, mypy, pylint, flake8, black, blacken-docs, and more on Jupyter Notebooks";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ l0b0 ];
+      mainProgram = "nbqa";
+    };
   };
-}
+in
+nbqa
