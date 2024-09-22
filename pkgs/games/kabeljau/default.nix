5227050fbe5eb998ec94f100ec290780db9c492e
Sarah Clark <seclark@nextquestion.net>
2024-09-21 17:08:55 -0700

    kabeljau: replace inkscape with cairosvg to fix build issues

--- pkgs/games/kabeljau/default.nix
+++ pkgs/games/kabeljau/default.nix
@@ -1,4 +1,12 @@
-{ stdenvNoCC, lib, fetchFromGitea, just, inkscape, makeWrapper, bash, dialog }:
+{
+  dialog,
+  fetchFromGitea,
+  just,
+  lib,
+  makeWrapper,
+  python3Packages,
+  stdenvNoCC,
+}:
 
 stdenvNoCC.mkDerivation rec {
   pname = "kabeljau";
@@ -12,21 +20,29 @@ stdenvNoCC.mkDerivation rec {
     hash = "sha256-RedVItgfr6vgqXHA3bOiHXDpfGuHI+sX4jCHL9G5jYk=";
   };
 
-  # Inkscape is needed in a just recipe where it is used to export the SVG icon to several different sized PNGs.
-  nativeBuildInputs = [ just inkscape makeWrapper ];
+  # `cairosvg` is needed in a `just` recipe where it is used to export the SVG icon to several different sized PNGs.
+  nativeBuildInputs = [
+    just
+    makeWrapper
+    python3Packages.cairosvg
+  ];
+
   postPatch = ''
     patchShebangs --host ${pname}
     substituteInPlace ./justfile \
-      --replace " /bin" " $out/bin" \
-      --replace " /usr" " $out"
+      --replace-quiet " /bin" " $out/bin" \
+      --replace-quiet " /usr" " $out" \
+      --replace-fail "inkscape" "cairosvg" \
+      --replace-quiet "-C -w $icon_width" "-W $icon_width" \
+      --replace-quiet "-h $icon_height" "-H $icon_height" \
+      --replace-quiet "--export-png-color-mode=RGBA_8" "-f png"
   '';
+
   installPhase = ''
     runHook preInstall
 
     just install
-    wrapProgram $out/bin/${pname} --suffix PATH : ${
-      lib.makeBinPath [ dialog ]
-    }
+    wrapProgram $out/bin/${pname} --suffix PATH : ${lib.makeBinPath [ dialog ]}
 
     runHook postInstall
   '';
