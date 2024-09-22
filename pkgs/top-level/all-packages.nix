1b89990e784d5723b3e6ec54b6f24c932d16751c
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-09-21 23:35:37 -0400

    podman-desktop: 0.12.0 -> 1.12.0
    This also fixes a build failure on darwin due to read-only permissions
    on some Electron.app files, which apparently breaks electron-builder.
    
    Closes: #343328

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11418,7 +11418,9 @@ with pkgs;
 
   podman-compose = python3Packages.callPackage ../applications/virtualization/podman-compose { };
 
-  podman-desktop = callPackage ../applications/virtualization/podman-desktop {};
+  podman-desktop = callPackage ../applications/virtualization/podman-desktop {
+    inherit (darwin) autoSignDarwinBinariesHook;
+  };
 
   pods = callPackage ../applications/virtualization/pods { };
 
