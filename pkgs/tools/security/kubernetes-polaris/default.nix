ca1d3ef5db64bf590b527751f286c4dec72fbc32
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-22 10:03:17 +0000

    kubernetes-polaris: 9.3.0 -> 9.4.0

--- pkgs/tools/security/kubernetes-polaris/default.nix
+++ pkgs/tools/security/kubernetes-polaris/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "kubernetes-polaris";
-  version = "9.3.0";
+  version = "9.4.0";
 
   src = fetchFromGitHub {
     owner = "FairwindsOps";
     repo = "polaris";
     rev = version;
-    sha256 = "sha256-qJAhxwVM/tYdCWLL1snUYjXGfgdcHkBFrI9xBg1/EXU=";
+    sha256 = "sha256-Vw+4/GVuTM4KioZnFk2RH6rmXD6zQO29NepAr2TwHjI=";
   };
 
-  vendorHash = "sha256-6sxzRI22xiZOQds20iU5OsU+JqcNB2wOUrOZrOH1Sa4=";
+  vendorHash = "sha256-X0GPKR6l1v5312hOmelrcqp4KT3LwsGkvKoqUfEM0uU=";
 
   nativeBuildInputs = [ installShellFiles ];
 
