0e6f5d2b9b35fc5775369bb271bd2428f22b2a53
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-23 09:25:27 -0300

    httplib: refactor
    - finalAttrs
    - strictDeps
    - no nested with
    - add AndersonTorres as maintainer

--- pkgs/by-name/ht/httplib/package.nix
+++ pkgs/by-name/ht/httplib/package.nix
@@ -1,30 +1,37 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, cmake
-, openssl
+{
+  lib,
+  cmake,
+  fetchFromGitHub,
+  openssl,
+  stdenv,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "httplib";
   version = "0.17.3";
 
   src = fetchFromGitHub {
     owner = "yhirose";
     repo = "cpp-httplib";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-yvaPIbRqJGkiob3Nrv3H1ieFAC5b+h1tTncJWTy4dmk=";
   };
 
   nativeBuildInputs = [ cmake ];
+
   buildInputs = [ openssl ];
 
-  meta = with lib; {
-    description = "C++ header-only HTTP/HTTPS server and client library";
+  strictDeps = true;
+
+  meta = {
     homepage = "https://github.com/yhirose/cpp-httplib";
-    changelog = "https://github.com/yhirose/cpp-httplib/releases/tag/v${version}";
-    maintainers = with maintainers; [ aidalgol ];
-    license = licenses.mit;
-    platforms = platforms.all;
+    description = "C++ header-only HTTP/HTTPS server and client library";
+    changelog = "https://github.com/yhirose/cpp-httplib/releases/tag/${finalAttrs.src.rev}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      AndersonTorres
+      aidalgol
+    ];
+    platforms = lib.platforms.all;
   };
-}
+})
