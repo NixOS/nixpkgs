7a3290b85e5cb13adfc56679ed5b9b1e82003c89
Mutsuha Asada <me@momee.mt>
2024-09-23 13:57:43 +0900

    ejs: 3.1.9 -> 3.1.10 - Removed \`rec\` - Removed \`with lib;\` - Added momeemt to maintainers - Formatted via nixfmt-rfc-style

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1674,8 +1674,6 @@ with pkgs;
 
   crypto-tracker = callPackage ../tools/misc/crypto-tracker { };
 
-  ejs = callPackage ../development/tools/ejs { };
-
   ejson2env = callPackage ../tools/admin/ejson2env { };
 
   davinci-resolve = callPackage ../applications/video/davinci-resolve { };
