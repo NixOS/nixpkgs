28379f642cf11e7a8c3b99285fd57472d834d63d
Tristan Ross <tristan.ross@midstall.com>
2024-09-22 21:09:18 -0700

    widevine-cdm: fix formatting

--- pkgs/applications/networking/browsers/misc/widevine-cdm.nix
+++ pkgs/applications/networking/browsers/misc/widevine-cdm.nix
@@ -1,11 +1,12 @@
-{ lib
-, stdenv
-, fetchurl
-, fetchzip
-, fetchFromGitHub
-, squashfsTools
-, curl
-, python3
+{
+  lib,
+  stdenv,
+  fetchurl,
+  fetchzip,
+  fetchFromGitHub,
+  squashfsTools,
+  curl,
+  python3,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -13,32 +14,46 @@ stdenv.mkDerivation (finalAttrs: {
   version = "4.10.2710.0";
   lacrosVersion = "120.0.6098.0";
 
-  widevineInstaller = if stdenv.targetPlatform.isAarch64 then fetchFromGitHub {
-      owner = "AsahiLinux";
-      repo = "widevine-installer";
-      rev = "7a3928fe1342fb07d96f61c2b094e3287588958b";
-      sha256 = "sha256-XI1y4pVNpXS+jqFs0KyVMrxcULOJ5rADsgvwfLF6e0Y=";
-    } else null;
+  widevineInstaller =
+    if stdenv.targetPlatform.isAarch64 then
+      fetchFromGitHub {
+        owner = "AsahiLinux";
+        repo = "widevine-installer";
+        rev = "7a3928fe1342fb07d96f61c2b094e3287588958b";
+        sha256 = "sha256-XI1y4pVNpXS+jqFs0KyVMrxcULOJ5rADsgvwfLF6e0Y=";
+      }
+    else
+      null;
 
-  src = if stdenv.targetPlatform.isAarch64 then fetchurl {
-    url = "https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles/chromeos-lacros-arm64-squash-zstd-${finalAttrs.lacrosVersion}";
-    hash = "sha256-OKV8w5da9oZ1oSGbADVPCIkP9Y0MVLaQ3PXS3ZBLFXY=";
-  } else fetchzip {
-    url = "https://dl.google.com/widevine-cdm/${finalAttrs.version}-linux-x64.zip";
-    hash = "sha256-lGTrSzUk5FluH1o4E/9atLIabEpco3C3gZw+y6H6LJo=";
-    stripRoot = false;
-  };
+  src =
+    if stdenv.targetPlatform.isAarch64 then
+      fetchurl {
+        url = "https://commondatastorage.googleapis.com/chromeos-localmirror/distfiles/chromeos-lacros-arm64-squash-zstd-${finalAttrs.lacrosVersion}";
+        hash = "sha256-OKV8w5da9oZ1oSGbADVPCIkP9Y0MVLaQ3PXS3ZBLFXY=";
+      }
+    else
+      fetchzip {
+        url = "https://dl.google.com/widevine-cdm/${finalAttrs.version}-linux-x64.zip";
+        hash = "sha256-lGTrSzUk5FluH1o4E/9atLIabEpco3C3gZw+y6H6LJo=";
+        stripRoot = false;
+      };
 
   nativeBuildInputs = lib.optionals stdenv.targetPlatform.isAarch64 [
-    squashfsTools curl python3
+    squashfsTools
+    curl
+    python3
   ];
 
-  unpackPhase = if stdenv.targetPlatform.isAarch64 then ''
-    unsquashfs -q $src 'WidevineCdm/*'
-    python3 ${finalAttrs.widevineInstaller}/widevine_fixup.py squashfs-root/WidevineCdm/_platform_specific/cros_arm64/libwidevinecdm.so libwidevinecdm.so
-    cp squashfs-root/WidevineCdm/manifest.json .
-    cp squashfs-root/WidevineCdm/LICENSE LICENSE.txt
-  '' else null;
+  unpackPhase =
+    if stdenv.targetPlatform.isAarch64 then
+      ''
+        unsquashfs -q $src 'WidevineCdm/*'
+        python3 ${finalAttrs.widevineInstaller}/widevine_fixup.py squashfs-root/WidevineCdm/_platform_specific/cros_arm64/libwidevinecdm.so libwidevinecdm.so
+        cp squashfs-root/WidevineCdm/manifest.json .
+        cp squashfs-root/WidevineCdm/LICENSE LICENSE.txt
+      ''
+    else
+      null;
 
   installPhase = ''
     runHook preInstall
@@ -46,15 +61,18 @@ stdenv.mkDerivation (finalAttrs: {
     install -vD manifest.json $out/share/google/chrome/WidevineCdm/manifest.json
     install -vD LICENSE.txt $out/share/google/chrome/WidevineCdm/LICENSE.txt
     install -vD libwidevinecdm.so $out/share/google/chrome/WidevineCdm/_platform_specific/linux_${
-    if stdenv.targetPlatform.isAarch64 then "arm64"
-    else if stdenv.targetPlatform.isx86_64 then "x64"
-    else throw "Unsupported platform ${stdenv.targetPlatform.config}"
+      if stdenv.targetPlatform.isAarch64 then
+        "arm64"
+      else if stdenv.targetPlatform.isx86_64 then
+        "x64"
+      else
+        throw "Unsupported platform ${stdenv.targetPlatform.config}"
     }/libwidevinecdm.so
 
     runHook postInstall
   '';
 
-# Accoring to widevine-installer: "Hack because Chromium hardcodes a check for this right now..."
+  # Accoring to widevine-installer: "Hack because Chromium hardcodes a check for this right now..."
   postInstall = lib.optionalString stdenv.targetPlatform.isAarch64 ''
     mkdir -p "$out/share/google/chrome/WidevineCdm/_platform_specific/linux_x64"
     touch "$out/share/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so"
@@ -66,6 +84,9 @@ stdenv.mkDerivation (finalAttrs: {
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
     maintainers = with maintainers; [ jlamur ];
-    platforms = [ "x86_64-linux" "aarch64-linux" ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
   };
 })
