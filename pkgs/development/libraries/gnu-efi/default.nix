b4f6bfc66f4e25a3b02e8ca4d04ea8f9f67e0b74
K900 <me@0upti.me>
2024-09-23 11:15:38 +0300

    gnu-efi: backport patch to fix memory corruption in rEFInd

--- pkgs/development/libraries/gnu-efi/default.nix
+++ pkgs/development/libraries/gnu-efi/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, buildPackages, fetchFromGitHub, pciutils
+{ lib, stdenv, buildPackages, fetchFromGitHub, fetchpatch, pciutils
 , gitUpdater, fwupd-efi, ipxe, refind, syslinux }:
 
 stdenv.mkDerivation rec {
@@ -11,6 +11,18 @@ stdenv.mkDerivation rec {
     rev = version;
     hash = "sha256-xtiKglLXm9m4li/8tqbOsyM6ThwGhyu/g4kw5sC4URY=";
   };
+
+  patches = [
+    # Backport patch fixing memory corruption in rEFInd
+    (fetchpatch {
+      url = "https://github.com/ncroxon/gnu-efi/commit/f5bb548df572c992fe3389a586bec3a19b092c18.diff";
+      hash = "sha256-lImlcFUWKwm3u5srFkZa0QOQzUcpNfqPXOpVlh7d390=";
+    })
+    (fetchpatch {
+      url = "https://github.com/ncroxon/gnu-efi/commit/6b9dae0bef0fab82230a6672eaadd38d739e3e1e.diff";
+      hash = "sha256-1CRD8EY/RGdi7zEQ8NZ+OjA74xZrvNnAkUG8JkJ8/Y4=";
+    })
+  ];
 
   buildInputs = [ pciutils ];
 
