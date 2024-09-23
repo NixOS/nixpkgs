ada79bfff2aad6f4c09e14efeecfdbd1a4e767e4
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-23 03:39:13 +0000

    plasticity: 24.2.0 -> 24.2.3

--- pkgs/by-name/pl/plasticity/package.nix
+++ pkgs/by-name/pl/plasticity/package.nix
@@ -33,11 +33,11 @@
 }:
 stdenv.mkDerivation rec  {
   pname = "plasticity";
-  version = "24.2.0";
+  version = "24.2.3";
 
   src = fetchurl {
     url = "https://github.com/nkallen/plasticity/releases/download/v${version}/Plasticity-${version}-1.x86_64.rpm";
-    hash = "sha256-3dHS7chTgoD35AV/q8DHIYl43KbCsoFYEqSQHXm05tg=";
+    hash = "sha256-iiVh4k5r5PXN1/VJZcropTMu36N2B/ECq2L5e59QxJY=";
   };
 
   passthru.updateScript = ./update.sh;
