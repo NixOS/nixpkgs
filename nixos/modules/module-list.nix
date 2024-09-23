6af67500fe5d31de58d1ba33362dd548c554036e
emily <ek@kyouma.net>
2024-09-23 22:18:02 +0200

    nixos/librespeed: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1441,6 +1441,7 @@
   ./services/web-apps/komga.nix
   ./services/web-apps/lanraragi.nix
   ./services/web-apps/lemmy.nix
+  ./services/web-apps/librespeed.nix
   ./services/web-apps/limesurvey.nix
   ./services/web-apps/mainsail.nix
   ./services/web-apps/mastodon.nix
