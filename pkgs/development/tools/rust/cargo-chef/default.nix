b4162d91763c90b61fd7904a16abeb1ddad794ba
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-23 13:11:08 +0000

    cargo-chef: 0.1.67 -> 0.1.68

--- pkgs/development/tools/rust/cargo-chef/default.nix
+++ pkgs/development/tools/rust/cargo-chef/default.nix
@@ -2,14 +2,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-chef";
-  version = "0.1.67";
+  version = "0.1.68";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-5bvA3lss+F2Wx0SSx5KRCmpERdIXUkUhFP+zRn8aZH0=";
+    hash = "sha256-NTFrNSEIzHppwOOjI3VadjhdV6EgGUmJzyX5JmTsJoI=";
   };
 
-  cargoHash = "sha256-EIpi1k5GffGCk+fzHSW32T+ZLkRfswnEGZdER95TyBk=";
+  cargoHash = "sha256-DoIkK/tj3AYt0vm7u7f4SmgOKbdQZv3ZunMFT68+37E=";
 
   meta = with lib; {
     description = "Cargo-subcommand to speed up Rust Docker builds using Docker layer caching";
