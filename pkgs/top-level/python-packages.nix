857db009bc4a0c13e0ece3ff8d515926f42b0e3f
Théo Bori <theo1.bori@epitech.eu>
2024-09-23 20:37:01 +0200

    python3Packages.loompy: init at 3.0.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7407,6 +7407,8 @@ self: super: with self; {
 
   lomond = callPackage ../development/python-modules/lomond { };
 
+  loompy = callPackage ../development/python-modules/loompy { };
+
   loopy = callPackage ../development/python-modules/loopy { };
 
   looseversion = callPackage ../development/python-modules/looseversion { };
