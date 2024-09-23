b17ca19e8fc1f900407eb3f0ac1cc0127416665b
emily <ek@kyouma.net>
2024-09-23 22:18:02 +0200

    nixos/librespeed: init
    Co-Authored-By: Sandro Jäckel <sandro.jaeckel@gmail.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1627,6 +1627,7 @@
   ./services/web-apps/lasuite-docs.nix
   ./services/web-apps/lasuite-meet.nix
   ./services/web-apps/lemmy.nix
+  ./services/web-apps/librespeed.nix
   ./services/web-apps/libretranslate.nix
   ./services/web-apps/limesurvey.nix
   ./services/web-apps/mainsail.nix
