557846c520706940dc520fe5d5ef7bb25beafb32
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-23 06:59:04 +0000

    elasticmq-server-bin: 1.6.7 -> 1.6.8

--- pkgs/servers/elasticmq-server-bin/default.nix
+++ pkgs/servers/elasticmq-server-bin/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "elasticmq-server";
-  version = "1.6.7";
+  version = "1.6.8";
 
   src = fetchurl {
     url = "https://s3-eu-west-1.amazonaws.com/softwaremill-public/elasticmq-server-${finalAttrs.version}.jar";
-    sha256 = "sha256-JSMxJBdYpYcBo/9wLY7QuQMEGlRF3hdVzVH2VMNT5b4=";
+    sha256 = "sha256-t0aLUJYuKi19N4iPMvw72OLgIc1steqnUQbi0xF6rv4=";
   };
 
   # don't do anything?
