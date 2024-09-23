e8f6b8225ab3308a0584471125abb9857af3e95a
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-23 09:22:25 -0300

    httplib: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19806,8 +19806,6 @@ with pkgs;
 
   hnswlib = callPackage ../development/libraries/hnswlib { };
 
-  httplib = callPackage ../development/libraries/httplib { };
-
   icon-lang = callPackage ../development/interpreters/icon-lang { };
 
   libgit2 = callPackage ../development/libraries/libgit2 {
