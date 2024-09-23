6da8eeb636c7e905d7cb03c5522a076999cf4ec7
Lin Jian <me@linj.tech>
2024-09-23 14:18:10 +0800

    emacsPackages: fix build for nongnu packages

--- pkgs/applications/editors/emacs/elisp-packages/nongnu-devel-packages.nix
+++ pkgs/applications/editors/emacs/elisp-packages/nongnu-devel-packages.nix
@@ -19,6 +19,10 @@
 self:
 let
 
+  inherit (import ./lib-override-helper.nix pkgs lib)
+    addPackageRequires
+    ;
+
   generateNongnu = lib.makeOverridable (
     {
       generated ? ./nongnu-devel-generated.nix,
@@ -39,9 +43,15 @@ let
 
       super = imported;
 
-      commonOverrides = import ./nongnu-common-overrides.nix pkgs;
+      commonOverrides = import ./nongnu-common-overrides.nix pkgs lib;
 
-      overrides = self: super: { };
+      overrides = self: super: {
+        # missing optional dependencies
+        haskell-tng-mode = addPackageRequires super.haskell-tng-mode [
+          self.shut-up
+          self.lsp-mode
+        ];
+      };
 
     in
     let
