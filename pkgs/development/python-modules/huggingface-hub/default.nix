f6a980e7c1f1f8508c053afd512541022c567475
Gaetan Lepage <gaetan@glepage.com>
2024-09-23 15:43:52 +0200

    python312Packages.huggingface-hub: 0.25.0 -> 0.25.1
    Diff: https://github.com/huggingface/huggingface_hub/compare/refs/tags/v0.25.0...v0.25.1
    
    Changelog: https://github.com/huggingface/huggingface_hub/releases/tag/v0.25.1

--- pkgs/development/python-modules/huggingface-hub/default.nix
+++ pkgs/development/python-modules/huggingface-hub/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "huggingface-hub";
-  version = "0.25.0";
+  version = "0.25.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "huggingface";
     repo = "huggingface_hub";
     rev = "refs/tags/v${version}";
-    hash = "sha256-N/c/aTUWHolQ1TWVOoyfQ3eCLOSX3/6qtXk1T918/wg=";
+    hash = "sha256-MloCUtvJ3A7t6NbCCPp4kcR+7apTrIjbvm6Ppe0SgdA=";
   };
 
   build-system = [ setuptools ];
