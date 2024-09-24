a086700eac4578da02dfd7dd592366ee5ff9e644
Mathew Polzin <matt.polzin@gmail.com>
2024-09-23 21:50:50 -0500

    idris2Packages.pack: init at 2024-02-07

--- pkgs/development/compilers/idris2/default.nix
+++ pkgs/development/compilers/idris2/default.nix
@@ -4,5 +4,7 @@
   idris2Api = callPackage ./idris2-api.nix { };
   idris2Lsp = callPackage ./idris2-lsp.nix { };
 
+  pack = callPackage ./pack.nix { };
+
   buildIdris = callPackage ./build-idris.nix { };
 }
