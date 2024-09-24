48a314372cd075f0b7c337c846be7c6bdc4f39cf
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-24 20:07:22 +0000

    python312Packages.python-linkplay: 0.0.10 -> 0.0.11

--- pkgs/development/python-modules/python-linkplay/default.nix
+++ pkgs/development/python-modules/python-linkplay/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "python-linkplay";
-  version = "0.0.10";
+  version = "0.0.11";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Velleman";
     repo = "python-linkplay";
     rev = "refs/tags/v${version}";
-    hash = "sha256-uenFr86WXSFzo3PlDz/KyMgG06QDzm69z0TM59UP6pg=";
+    hash = "sha256-IFDVIUBB/8FZMn6CKmZBbOEB3ghzer7Fe6YLhwtjJSU=";
   };
 
   build-system = [ setuptools ];
