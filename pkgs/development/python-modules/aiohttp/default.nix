204d149502d49343d29a2bfc1d9c56a5ee877457
Robert Schütz <mail@dotlambda.de>
2024-09-24 16:44:02 -0700

    python312Packages.aiohttp: 3.10.5 -> 3.10.7
    Diff: https://github.com/aio-libs/aiohttp/compare/refs/tags/v3.10.5...v3.10.7
    
    Changelog: https://github.com/aio-libs/aiohttp/blob/v3.10.7/CHANGES.rst

--- pkgs/development/python-modules/aiohttp/default.nix
+++ pkgs/development/python-modules/aiohttp/default.nix
@@ -40,7 +40,7 @@
 
 buildPythonPackage rec {
   pname = "aiohttp";
-  version = "3.10.5";
+  version = "3.10.7";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -49,7 +49,7 @@ buildPythonPackage rec {
     owner = "aio-libs";
     repo = "aiohttp";
     rev = "refs/tags/v${version}";
-    hash = "sha256-HN2TJ8hVbClakV3ldTOn3wbrhCuf2Qn9EjWCSlSyJpw=";
+    hash = "sha256-UOsWC5P/KT5YMzkAM6WuoOum30b8s4sgSUgR6EskHy0=";
   };
 
   patches = [
