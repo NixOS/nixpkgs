f11b5681f9bd5ec34e99318cedb34f927387c718
techknowlogick <techknowlogick@gitea.com>
2024-09-24 14:04:43 -0400

    nomad-pack: 0.1.2 -> nightly-2024-09-23

--- pkgs/by-name/no/nomad-pack/package.nix
+++ pkgs/by-name/no/nomad-pack/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "nomad-pack";
-  version = "0.1.2";
+  version = "nightly-2024-09-23";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-5OTGBO2EMGPJ4J2Eu3pIlZmYGXYxgL/6uGQgdB+8nPk=";
+    repo = "nomad-pack";
+    rev = "3c0178a561b360906c591718edb25ae25ae9d964";  # nightly tag as of 2024-09-23
+    hash = "sha256-8erw+8ZTpf8Dc9m6t5NeRgwOETkjXN/wVhoZ4g0uWhg=";
   };
 
-  vendorHash = "sha256-8hWiVtya/TlVLUlYVJSjOzZUI8vsPm355ksIRPhB2rA=";
+  vendorHash = "sha256-Rt0T6cCMzO4YBFF6/9xeCZcsqziDmxPMNirHLqepwek=";
 
   # skip running go tests as they require network access
   doCheck = false;
