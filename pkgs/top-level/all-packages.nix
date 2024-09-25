b4a9038c84c66c33b48db1dff4c6ee75c0018864
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-09-25 21:41:46 +0200

    mysql-workbench: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -37709,7 +37709,7 @@ with pkgs;
 
   moonfire-nvr = callPackage ../misc/moonfire-nvr { };
 
-  mysql-workbench = callPackage ../applications/misc/mysql-workbench (let mysql = mysql80; in {
+  mysql-workbench = callPackage ../by-name/my/mysql-workbench/package.nix (let mysql = mysql80; in {
     gdal = gdal.override {
       libmysqlclient = mysql;
     };
