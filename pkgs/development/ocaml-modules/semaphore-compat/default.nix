8dcd5b47818c65d813a42479eebee0938b1f5b00
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-25 14:29:52 +0000

    ocamlPackages.semaphore-compat: 1.0.1 -> 1.0.2

--- pkgs/development/ocaml-modules/semaphore-compat/default.nix
+++ pkgs/development/ocaml-modules/semaphore-compat/default.nix
@@ -2,11 +2,11 @@
 
 buildDunePackage rec {
   pname = "semaphore-compat";
-  version = "1.0.1";
+  version = "1.0.2";
 
   src = fetchurl {
     url = "https://github.com/mirage/semaphore-compat/releases/download/${version}/semaphore-compat-${version}.tbz";
-    sha256 = "139c5rxdp4dg1jcwyyxvhxr8213l1xdl2ab0mc288rfcppsiyxrb";
+    sha256 = "sha256-4CnZ2vX17IPpnlA7CNeuxZEKfA5HFoeQvwH0tCKNRnY=";
   };
 
   useDune2 = true;
