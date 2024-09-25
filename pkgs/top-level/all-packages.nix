e507e0a7f09230a9e5d4db1a600d015a48a5d7ca
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-24 22:12:34 -0300

    ciano: remove arguments in top-level

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4189,11 +4189,7 @@ with pkgs;
 
   burpsuite = callPackage ../tools/networking/burpsuite { };
 
-  ciano = callPackage ../applications/graphics/ciano {
-    inherit (pantheon) granite;
-    python = python3;
-    gtk = gtk3;
-  };
+  ciano = callPackage ../applications/graphics/ciano { };
 
   c3d = callPackage ../applications/graphics/c3d {
     inherit (darwin.apple_sdk.frameworks) Cocoa;
