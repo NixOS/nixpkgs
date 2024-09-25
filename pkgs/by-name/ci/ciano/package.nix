90124c48d5ab76e3fbe1c0b30eee6bd69f7d2fb8
Anderson Torres <torres.anderson.85@protonmail.com>
2024-09-25 09:12:06 -0300

    ciano: refactor
    - finalAttrs
    - strictDeps
    - no nested with

--- pkgs/by-name/ci/ciano/package.nix
+++ pkgs/by-name/ci/ciano/package.nix
@@ -1,30 +1,31 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, desktop-file-utils
-, ffmpeg
-, pantheon
-, gtk3
-, imagemagick
-, meson
-, ninja
-, pkg-config
-, python3
-, vala
-, wrapGAppsHook3
+{
+  lib,
+  desktop-file-utils,
+  fetchFromGitHub,
+  ffmpeg,
+  gtk3,
+  imagemagick,
+  meson,
+  ninja,
+  pantheon,
+  pkg-config,
+  python3,
+  stdenv,
+  vala,
+  wrapGAppsHook3,
 }:
 
 let
   inherit (pantheon) granite;
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "ciano";
   version = "0.2.4";
 
   src = fetchFromGitHub {
     owner = "robertsanseries";
-    repo = pname;
-    rev = version;
+    repo = "ciano";
+    rev = finalAttrs.version;
     hash = "sha256-nubm6vBWwsHrrmvFAL/cIzYPxg9B1EhnpC79IJMNuFY=";
   };
 
@@ -45,30 +46,33 @@ stdenv.mkDerivation rec {
     gtk3
   ];
 
+  dontWrapGApps = true;
+
+  strictDeps = true;
+
   postPatch = ''
     chmod +x meson/post_install.py
     patchShebangs meson/post_install.py
   '';
 
-  dontWrapGApps = true;
-
-  postFixup = let
-    binPath = lib.makeBinPath [
-      ffmpeg
-      imagemagick
-    ];
-  in
+  postFixup =
+    let
+      binPath = lib.makeBinPath [
+        ffmpeg
+        imagemagick
+      ];
+    in
     ''
       wrapProgram $out/bin/com.github.robertsanseries.ciano \
          --prefix PATH : ${binPath} "''${gappsWrapperArgs[@]}"
       ln -s $out/bin/com.github.robertsanseries.ciano $out/bin/ciano
     '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/robertsanseries/ciano";
     description = "Multimedia file converter focused on simplicity";
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ AndersonTorres ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = lib.platforms.linux;
   };
-}
+})
