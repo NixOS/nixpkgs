2d57f5251abcc9b3b4829006e6a036bb5f8c967e
Pyrox <pyrox@pyrox.dev>
2024-09-25 15:46:33 -0400

    uv: 0.4.11 -> 0.4.16

--- pkgs/by-name/uv/uv/package.nix
+++ pkgs/by-name/uv/uv/package.nix
@@ -16,14 +16,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "uv";
-  version = "0.4.11";
+  version = "0.4.16";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "astral-sh";
     repo = "uv";
     rev = "refs/tags/${version}";
-    hash = "sha256-a8mN2wag26BSL+2b5i4P1XN34J8jt+lZm2poZQdsAzM=";
+    hash = "sha256-pd7jwhhEFmIIMnt5dVnBaejxB812LYVRzFYsN+ZbwUQ=";
   };
 
   cargoDeps = rustPlatform.importCargoLock {
