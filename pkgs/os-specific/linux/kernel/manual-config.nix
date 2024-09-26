5d5b4734ff24f101e374cd4e7e6568a68f57ba51
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-09-26 14:57:51 +0200

    linux: allow building with ld.lld
    Even when building with pkgsLLVM.stdenv where ld is ld.lld and not
    binutils ld, the build picks up binutils ld for linking. This prevents
    features from working that require ld.lld.
    
    The reason is that when the LD environment variable is not set, Linux
    defaults to `ld` as a linker and ld is:
    
    /nix/store/zznja5f8v3jafffyah1rk46vpfcn38dv-gcc-wrapper-13.3.0/bin/ld
    GNU ld (GNU Binutils) 2.42
    
    This ld comes from pkgsLLVM.buildPackages.stdenv.cc.
    
    Fix by being more specific about which tools we want to build with.
    This just extends what manual-config.nix has already done for quite a
    while to avoid similar problems for other tools.
    
    It's important to pass LD and other toolchain environment variables to
    `make config` in generate-config.pl, because otherwise `make config`
    will also make decisions based on the wrong toolchain.

--- pkgs/os-specific/linux/kernel/manual-config.nix
+++ pkgs/os-specific/linux/kernel/manual-config.nix
@@ -427,6 +427,13 @@ stdenv.mkDerivation ((drvAttrs config stdenv.hostPlatform.linux-kernel kernelPat
   makeFlags = [
     "O=$(buildRoot)"
     "CC=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc"
+    "LD=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}ld"
+    "AR=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}ar"
+    "NM=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}nm"
+    "STRIP=${stdenv.cc.bintools.bintools}/bin/${stdenv.cc.targetPrefix}strip"
+    "OBJCOPY=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}objcopy"
+    "OBJDUMP=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}objdump"
+    "READELF=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}readelf"
     "HOSTCC=${buildPackages.stdenv.cc}/bin/${buildPackages.stdenv.cc.targetPrefix}cc"
     "HOSTLD=${buildPackages.stdenv.cc.bintools}/bin/${buildPackages.stdenv.cc.targetPrefix}ld"
     "ARCH=${stdenv.hostPlatform.linuxArch}"
