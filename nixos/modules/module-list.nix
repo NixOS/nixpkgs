4c49e3c3a2388ea1df199498b2611f84f4d8b79c
Jonas Heinrich <onny@project-insanity.org>
2024-09-26 12:35:40 +0000

    nixos/nextcloud-whiteboard-server: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1456,6 +1456,7 @@
   ./services/web-apps/netbox.nix
   ./services/web-apps/nextcloud.nix
   ./services/web-apps/nextcloud-notify_push.nix
+  ./services/web-apps/nextcloud-whiteboard-server.nix
   ./services/web-apps/nextjs-ollama-llm-ui.nix
   ./services/web-apps/nexus.nix
   ./services/web-apps/nifi.nix
