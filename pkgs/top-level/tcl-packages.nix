dbaeb3d0945332de49eba60997023cae9df0107c
Francesco Gazzetta <fgaz@fgaz.me>
2024-09-26 17:18:31 +0200

    tclPackages: init

---
+++ pkgs/top-level/tcl-packages.nix
@@ -0,0 +1,18 @@
+let
+  autoCalledPackages = import ./by-name-overlay.nix ../development/tcl-modules/by-name;
+in
+
+{
+  lib,
+  newScope,
+  tcl,
+  tk,
+}:
+
+lib.makeScope newScope (
+  lib.extends autoCalledPackages (self: {
+    inherit tcl tk;
+    inherit (tcl) mkTclDerivation tclPackageHook;
+
+  })
+)
