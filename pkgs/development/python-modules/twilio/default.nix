df548c681e6567671e576f507db4890e3de75d21
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-26 20:27:41 +0200

    python312Packages.twilio: 9.3.1 -> 9.3.2
    Diff: https://github.com/twilio/twilio-python/compare/refs/tags/9.3.1...9.3.2
    
    Changelog: https://github.com/twilio/twilio-python/blob/9.3.2/CHANGES.md

--- pkgs/development/python-modules/twilio/default.nix
+++ pkgs/development/python-modules/twilio/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "twilio";
-  version = "9.3.1";
+  version = "9.3.2";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "twilio";
     repo = "twilio-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-yQrWMRLH8GZYmpmhjW2Lomk6qyKydWsteCvFR0uDPRQ=";
+    hash = "sha256-XAJmAdwBF6HM0E89gAP4AOg1vepumJ8O39V0lKr9iDw=";
   };
 
   build-system = [ setuptools ];
