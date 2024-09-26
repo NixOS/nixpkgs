23970016579f52e54f83f7ab3bf890db318c47c3
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-09-26 11:51:14 +0200

    datadog-agent: unpin Go version
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/tools/networking/dd-agent/datadog-agent.nix
+++ pkgs/tools/networking/dd-agent/datadog-agent.nix
@@ -1,7 +1,7 @@
 { lib
 , stdenv
 , cmake
-, buildGo121Module
+, buildGoModule
 , makeWrapper
 , fetchFromGitHub
 , pythonPackages
@@ -37,7 +37,7 @@ let
     cmakeFlags = ["-DBUILD_DEMO=OFF" "-DDISABLE_PYTHON2=ON"];
   };
 
-in buildGo121Module rec {
+in buildGoModule rec {
   pname = "datadog-agent";
   inherit src version;
 
