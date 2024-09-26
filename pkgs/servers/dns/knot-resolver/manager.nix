2600b9a4ebf86c8f56c0e52444be7d66e7d1e9df
Vladimír Čunát <v@cunat.cz>
2024-09-26 10:07:20 +0200

    fixup! WIP

--- pkgs/servers/dns/knot-resolver/manager.nix
+++ pkgs/servers/dns/knot-resolver/manager.nix
@@ -1,24 +1,27 @@
-{ lib, knot-resolver, writeText, python3 }:
+{
+  lib,
+  knot-resolver,
+  writeText,
+  python3Packages,
+}:
 
-with python3.pkgs;
-
-buildPythonPackage rec {
+python3Packages.buildPythonPackage {
   pname = "knot-resolver";
   inherit (knot-resolver) version src;
 
   patches = [
-      # Rewrap the two supervisor's binaries, so that they obtain access to python modules
-      # defined in the manager.  Those are then used as extensions of supervisord.
-      # Manager needs this fixed bin/supervisord on its $PATH.
-      (writeText "rewrap-supervisor.patch" ''
-        --- a/setup.py
-        +++ b/setup.py
-        @@ -30,2 +30,4 @@
-         {'console_scripts': ['knot-resolver = knot_resolver.manager.main:main',
-        +                     'supervisord = supervisor.supervisord:main',
-        +                     'supervisorctl = supervisor.supervisorctl:main',
-                              'kresctl = knot_resolver.client.main:main']}
-      '')
+    # Rewrap the two supervisor's binaries, so that they obtain access to python modules
+    # defined in the manager.  Those are then used as extensions of supervisord.
+    # Manager needs this fixed bin/supervisord on its $PATH.
+    (writeText "rewrap-supervisor.patch" ''
+      --- a/setup.py
+      +++ b/setup.py
+      @@ -30,2 +30,4 @@
+       {'console_scripts': ['knot-resolver = knot_resolver.manager.main:main',
+      +                     'supervisord = supervisor.supervisord:main',
+      +                     'supervisorctl = supervisor.supervisorctl:main',
+                            'kresctl = knot_resolver.client.main:main']}
+    '')
   ];
 
   # Propagate meson config from the C part to the python part.
@@ -26,8 +29,8 @@ buildPythonPackage rec {
     cp '${knot-resolver.config_py}'/knot_resolver/constants.py ./python/knot_resolver/constants.py
   '';
 
-  # Deps can be seen in ./manager/pyproject.toml
-  propagatedBuildInputs = [
+  # Deps can be seen in ${src}/pyproject.toml
+  propagatedBuildInputs = with python3Packages; [
     aiohttp
     jinja2
     pyyaml
@@ -36,9 +39,9 @@ buildPythonPackage rec {
   ];
 
   doCheck = false; # FIXME
-  checkInputs = [ pytestCheckHook pytest-asyncio pyparsing ];
+  checkInputs = with python3Packages; [ pytestCheckHook pytest-asyncio pyparsing ];
 
-  meta = with lib; { #FIXME
-    maintainers = [ maintainers.vcunat /* upstream developer */ ];
+  meta = knot-resolver.meta // {
+    mainProgram = "knot-resolver";
   };
 }
