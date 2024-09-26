d157cfca61536ae360ea0ea00274829778c8a79f
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-26 08:14:15 +0000

    httping: 4.1.0 -> 4.2.0

--- pkgs/by-name/ht/httping/package.nix
+++ pkgs/by-name/ht/httping/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "httping";
-  version = "4.1.0";
+  version = "4.2.0";
 
   src = fetchFromGitHub {
     owner = "folkertvanheusden";
     repo = "HTTPing";
     rev = "refs/tags/v${finalAttrs.version}";
-    hash = "sha256-JtsPtrj/ZineBokEytbkZdAfjVLTkiaNcGtKVJlKm38=";
+    hash = "sha256-6PXus8lg+2L9RoRe4nPI1+2iYDKKEhVfJJZMoKWSCb0=";
   };
 
   nativeBuildInputs = [
