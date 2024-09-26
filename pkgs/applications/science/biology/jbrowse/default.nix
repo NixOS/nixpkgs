1a133b175d6e62df40f8d2582a79cfdab79f48ba
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-26 07:44:23 +0000

    jbrowse: 2.15.1 -> 2.15.4

--- pkgs/applications/science/biology/jbrowse/default.nix
+++ pkgs/applications/science/biology/jbrowse/default.nix
@@ -2,11 +2,11 @@
 
 let
   pname = "jbrowse";
-  version = "2.15.1";
+  version = "2.15.4";
 
   src = fetchurl {
     url = "https://github.com/GMOD/jbrowse-components/releases/download/v${version}/jbrowse-desktop-v${version}-linux.AppImage";
-    sha256 = "sha256-/dpHUrigO0wnWay0jIkg6eG75ynrVXd7zcIAss6DhSs=";
+    sha256 = "sha256-ojIvC93dx9BSnUw7zBpDMBD0xPCMG4GUH1e5ZRGd9pk=";
   };
 
   appimageContents = appimageTools.extractType2 {
