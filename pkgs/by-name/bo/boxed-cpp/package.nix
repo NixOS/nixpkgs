648fd81e3e4834328d5d79f6fa22981f4d75ac89
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-27 03:20:31 +0000

    boxed-cpp: 1.4.2 -> 1.4.3

--- pkgs/by-name/bo/boxed-cpp/package.nix
+++ pkgs/by-name/bo/boxed-cpp/package.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation (final: {
   pname = "boxed-cpp";
-  version = "1.4.2";
+  version = "1.4.3";
 
   src = fetchFromGitHub {
     owner = "contour-terminal";
     repo = "boxed-cpp";
     rev = "v${final.version}";
-    hash = "sha256-Z/dfSa/6SnzLWnFCXjJUbTBNa5dFZna099Crbcya/Dw=";
+    hash = "sha256-uZ/wT159UuEcTUtoQyt0D59z2wnLT5KpeeCpjyij198=";
   };
 
   nativeBuildInputs = [ cmake ];
