1dc039e9afcc5e676bdbbfea9466c0ebf27aec25
kraanzu <kraanzu@gmail.com>
2024-09-27 09:36:13 +0530

    add kraanzu to maintainers list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11349,6 +11349,12 @@
     githubId = 787421;
     name = "Kevin Quick";
   };
+  kraanzu = {
+    name = "Murli Tawari";
+    email = "kraanzu@gmail.com";
+    github = "kraanzu";
+    githubId = 97718086;
+  };
   kradalby = {
     name = "Kristoffer Dalby";
     email = "kristoffer@dalby.cc";
