81ecc353064cdb4f71e676571e8f2d0a95ab34e0
eklairs <eklairs@proton.me>
2024-09-27 19:50:12 +0530

    maintainers: add eklairs

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6040,6 +6040,12 @@
     name = "Edward Kimber";
     githubId = 99987;
   };
+  eklairs = {
+    name = "Eklairs";
+    email = "eklairs@proton.me";
+    github = "eklairs";
+    githubId = 142717667;
+  };
   ekleog = {
     email = "leo@gaspard.io";
     matrix = "@leo:gaspard.ninja";
