c433856475476ef9f010222686a27443ecbd3735
Jeff Huffman <tejing@tejing.com>
2024-09-27 16:30:48 -0400

    hred: 1.5.0 -> 1.5.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2417,8 +2417,6 @@ with pkgs;
 
   gut = callPackage ../applications/version-management/gut { };
 
-  hred = callPackage ../development/tools/hred { };
-
   hub = callPackage ../applications/version-management/hub { };
 
   hut = callPackage ../applications/version-management/hut { };
