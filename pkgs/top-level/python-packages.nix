fc958cc14689617075e777abaae0a87085dd07af
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-09-28 01:08:06 +0200

    python312Packages.cgal: init at 5.6.1.post202403291426

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2156,6 +2156,10 @@ self: super: with self; {
 
   cftime = callPackage ../development/python-modules/cftime { };
 
+  cgal = callPackage ../development/python-modules/cgal {
+    inherit (pkgs) cgal;
+  };
+
   cgen = callPackage ../development/python-modules/cgen { };
 
   cgroup-utils = callPackage ../development/python-modules/cgroup-utils { };
