de34497562b2c4de0eeac48196f5be7cc1fcfb2c
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-09-27 21:16:48 -0400

    libimobiledevice-glue: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21055,8 +21055,6 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) SystemConfiguration CoreFoundation;
   };
 
-  libimobiledevice-glue = callPackage ../development/libraries/libimobiledevice-glue { };
-
   libindicator-gtk2 = libindicator.override { gtkVersion = "2"; };
   libindicator-gtk3 = libindicator.override { gtkVersion = "3"; };
   libindicator = callPackage ../development/libraries/libindicator { };
