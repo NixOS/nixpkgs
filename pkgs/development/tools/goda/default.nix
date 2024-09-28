31ebc7ef07ad3ccdbf1209133d63145661fafa10
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-28 00:37:40 +0000

    goda: 0.5.9 -> 0.5.11

--- pkgs/development/tools/goda/default.nix
+++ pkgs/development/tools/goda/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "goda";
-  version = "0.5.9";
+  version = "0.5.11";
 
   src = fetchFromGitHub {
     owner = "loov";
     repo = "goda";
     rev = "v${version}";
-    hash = "sha256-tkGIo4FWIFFMtp4rP0GJaF7B6lrmtjaAVx45G4wAPQg=";
+    hash = "sha256-UeXn+JAR4TExZahwFozjbwXHF3QEcJvh5SzU/8VAmLg=";
   };
 
   vendorHash = "sha256-FYjlOYB0L4l6gF8hYtJroV1qMQD0ZmKWXBarjyConRs=";
