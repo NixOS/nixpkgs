aaa894cf3583bfcb91b6ddd225e02d53f752a991
qubitnano <146656568+qubitnano@users.noreply.github.com>
2024-09-28 12:06:39 -0400

    sdl_gamecontrollerdb: init at 0-unstable-2025-04-02

---
+++ pkgs/by-name/sd/sdl_gamecontrollerdb/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  unstableGitUpdater,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "sdl_gamecontrollerdb";
+  version = "0-unstable-2025-04-02";
+
+  src = fetchFromGitHub {
+    owner = "mdqinc";
+    repo = "SDL_GameControllerDB";
+    rev = "62d3999407b6cd4b88b3db28aa9ac173be8aaea6";
+    hash = "sha256-MnfyNiqIvN43efQrITbfiL2JDq2HqC8CWZP6N1trRYo=";
+  };
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm644 gamecontrollerdb.txt -t $out/share
+    install -Dm644 LICENSE -t $out/share/licenses/${finalAttrs.pname}
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = unstableGitUpdater { };
+
+  meta = {
+    description = "Community sourced database of game controller mappings to be used with SDL2 and SDL3 Game Controller functionality";
+    homepage = "https://github.com/mdqinc/SDL_GameControllerDB";
+    license = lib.licenses.zlib;
+    maintainers = with lib.maintainers; [ qubitnano ];
+    platforms = lib.platforms.all;
+  };
+})
