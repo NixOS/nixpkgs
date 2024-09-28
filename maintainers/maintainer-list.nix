ffedbf4df7fb5e8cbd6368d800b920c49ecf466e
charB66 <nix.disparate221@passinbox.com>
2024-09-28 15:08:33 +0200

    maintainers: add charB66

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3641,6 +3641,12 @@
     name = "ChaosAttractor";
     keys = [ { fingerprint = "A137 4415 DB7C 6439 10EA  5BF1 0FEE 4E47 5940 E125"; } ];
   };
+  charB66 = {
+    email = "nix.disparate221@passinbox.com";
+    github = "charB66";
+    githubId = 59340663;
+    name = "Bryan F.";
+  };
   charlesbaynham = {
     email = "charlesbaynham@gmail.com";
     github = "charlesbaynham";
