c781c8eeaab54e50c2c58a76d5de6cc4255f7248
Yaroslav Bolyukin <iam@lach.pw>
2024-09-28 20:37:00 +0200

    qubes-core-vchan-xen: 4.1.4 -> 4.2.4

--- pkgs/applications/qubes/qubes-core-vchan-xen/default.nix
+++ pkgs/applications/qubes/qubes-core-vchan-xen/default.nix
@@ -1,36 +1,39 @@
 { lib, stdenv
 , fetchFromGitHub
-, xen
+, qubes-vmm-xen
 }:
-
-stdenv.mkDerivation rec {
-  pname = "qubes-core-vchan-xen";
-  version = "4.1.4";
-
+let
+  version = "4.2.4";
   src = fetchFromGitHub {
     owner = "QubesOS";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256:02l1vs5c2jfw22gxvl2fb66m0d99n8ya1i7rphsb5cxsljvxary0";
+    repo = "qubes-core-vchan-xen";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-O7i5zK7S+d/O8oPMvm6szNR1Xq6qSBNE2+uFI/1mDEg=";
   };
+in
+stdenv.mkDerivation {
+  inherit version src;
+  pname = "qubes-core-vchan-xen";
 
-  buildInputs = [ xen ];
+  buildInputs = [ qubes-vmm-xen.dev ];
 
-  buildPhase = ''
-    make all PREFIX=/ LIBDIR="$out/lib" INCLUDEDIR="$out/include"
-  '';
+  buildFlags = [ "all" ];
 
-  installPhase = ''
-    make install DESTDIR=$out PREFIX=/
-  '';
+  makeFlags = [
+    "DESTDIR=/"
+    "PREFIX=/"
+    "LIBDIR=$(out)/lib"
+    "INCLUDEDIR=$(out)/include"
+  ];
+
+  # This flag needs to be enabled for Xen > 1.18.
+  env.CFLAGS = "-DHAVE_XC_DOMAIN_GETINFO_SINGLE";
 
   meta = with lib; {
     description = "Libraries required for the higher-level Qubes daemons and tools";
     homepage = "https://qubes-os.org";
     license = licenses.gpl2Plus;
-    maintainers = [ ];
+    maintainers = with maintainers; [ lach sigmasquadron ];
     platforms = platforms.linux;
-    broken = true;
   };
-
 }
