88a0de19432ed1dd5ac372e761801f73079cfa47
wxt <3264117476@qq.com>
2024-09-28 10:14:31 +0800

    fcp: fix build

--- pkgs/by-name/fc/fcp/package.nix
+++ pkgs/by-name/fc/fcp/package.nix
@@ -4,6 +4,7 @@
   lib,
   rustPlatform,
   stdenv,
+  fetchpatch,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -17,7 +18,19 @@ rustPlatform.buildRustPackage rec {
     sha256 = "0f242n8w88rikg1srimdifadhggrb2r1z0g65id60ahb4bjm8a0x";
   };
 
-  cargoHash = "sha256-iS3H87kTS7Iq0FhHE47zBOozPKC1mG9eHiATxp18hz8=";
+  cargoPatches = [
+    (fetchpatch {
+      url = "https://github.com/Svetlitski/fcp/commit/1988f88be54a507b804b037cb3887fecf11bb571.patch";
+      hash = "sha256-fafpy1tviT1rV+jv1Yxg6xEsFZ9qXWQi5LykagDA5xI=";
+    })
+    (fetchpatch {
+      url = "https://github.com/Svetlitski/fcp/commit/89bcfc9aa1055dcf541da7a6477ffd3107023f48.patch";
+      hash = "sha256-NJ9MMeWf6Ywu+p5uDSWWpAcb01PoMbuSAZ3Qxl9jEaY=";
+    })
+    ./0001-update-Cargo.lock.patch
+  ];
+
+  cargoHash = "sha256-wFXvaARflHsT1xz3muC/BPRmhnXEEIk2pBsu87E7Wo8=";
 
   nativeBuildInputs = [ expect ];
 
