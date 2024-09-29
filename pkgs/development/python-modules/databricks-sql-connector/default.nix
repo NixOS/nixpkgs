bce22b1b0f1867a7583bbb132d227019d8898c82
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:04:10 +0200

    python3Packages.databricks-sql-connector: 3.3.0 -> 3.4.0
    https://github.com/databricks/databricks-sql-python/blob/v3.4.0/CHANGELOG.md

--- pkgs/development/python-modules/databricks-sql-connector/default.nix
+++ pkgs/development/python-modules/databricks-sql-connector/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "databricks-sql-connector";
-  version = "3.3.0";
+  version = "3.4.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = "databricks";
     repo = "databricks-sql-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-a3OeKJ3c2UCClsPMah7iJY2YvIVLfHmmBuHAx8vdXZs=";
+    hash = "sha256-8q5qWN+i2mGbzXvkop/G6mjZegzZ/6kr1Fl7FaHwLYA=";
   };
 
   patches = [
