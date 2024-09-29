bac38c0d7794f18252b0ea6bf8ae15d875b47098
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:05:26 +0200

    python3Packages.llama-index-readers-file: 0.2.1 -> 0.2.2

--- pkgs/development/python-modules/llama-index-readers-file/default.nix
+++ pkgs/development/python-modules/llama-index-readers-file/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-readers-file";
-  version = "0.2.1";
+  version = "0.2.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_index_readers_file";
     inherit version;
-    hash = "sha256-x/kgdISfxZsQBJ1JakrlJmmr/LFZoZnZoROFKi/tcLg=";
+    hash = "sha256-SEWfkJYLhjc3FHtm7YOv7JzomE+O2iVhttJQAhQ2XbI=";
   };
 
   pythonRelaxDeps = [
