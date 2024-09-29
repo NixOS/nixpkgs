a714fcd0ea99d5f05ce41bac3e397914368ec0e9
R. Ryantm <ryantm-bot@ryantm.com>
2024-09-29 00:19:28 +0000

    ssdfs-utils: 4.43 -> 4.45

--- pkgs/tools/filesystems/ssdfs-utils/default.nix
+++ pkgs/tools/filesystems/ssdfs-utils/default.nix
@@ -12,13 +12,13 @@ stdenv.mkDerivation {
   # as ssdfs-utils, not ssdfs-tools.
   pname = "ssdfs-utils";
   # The version is taken from `configure.ac`, there are no tags.
-  version = "4.43";
+  version = "4.45";
 
   src = fetchFromGitHub {
     owner = "dubeyko";
     repo = "ssdfs-tools";
-    rev = "cb0b3d8a01eb2c27d3bf59de6313d14fc7d3b6ac";
-    hash = "sha256-LDxL0+IkQ6sgkFjbiUwB416ZjQGA3JV05o6D6ibAHJE=";
+    rev = "f78ebf2a19eb338ae6858aa4f28133b117507df7";
+    hash = "sha256-RKVcXZakYDd7Vfd1w0pNs9/oMRzvzRr8VabpUBRcKc8=";
   };
 
   strictDeps = true;
