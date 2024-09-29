9b9e3d17ff50c5e8a06a0ca062dba683b8bc1833
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:04:15 +0200

    python3Packages.dirty-equals: 0.7.1-post0 -> 0.8.0
    https://github.com/samuelcolvin/dirty-equals/releases/tag/v0.8.0

--- pkgs/development/python-modules/dirty-equals/default.nix
+++ pkgs/development/python-modules/dirty-equals/default.nix
@@ -13,7 +13,7 @@
 let
   dirty-equals = buildPythonPackage rec {
     pname = "dirty-equals";
-    version = "0.7.1-post0";
+    version = "0.8.0";
     format = "pyproject";
 
     disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ let
       owner = "samuelcolvin";
       repo = pname;
       rev = "refs/tags/v${version}";
-      hash = "sha256-U6DNluthDgxzh6IOaKrN/JhX4u+ztY/jVp9IKh0iP34=";
+      hash = "sha256-DZuzZ8cLYpVdivMh+zNJKpHe+0fpxM3ulKiCpN2S6co=";
     };
 
     nativeBuildInputs = [ hatchling ];
