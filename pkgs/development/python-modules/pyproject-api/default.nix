9b560c76c2d63f554a8a1d24ea43e7d75df770bb
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:06:25 +0200

    python3Packages.pyproject-api: 1.7.1 -> 1.8.0
    https://github.com/tox-dev/pyproject-api/releases/tag/1.8.0

--- pkgs/development/python-modules/pyproject-api/default.nix
+++ pkgs/development/python-modules/pyproject-api/default.nix
@@ -27,7 +27,7 @@
 
 buildPythonPackage rec {
   pname = "pyproject-api";
-  version = "1.7.1";
+  version = "1.8.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -36,7 +36,7 @@ buildPythonPackage rec {
     owner = "tox-dev";
     repo = "pyproject-api";
     rev = "refs/tags/${version}";
-    hash = "sha256-TQ6cRkGnZpNchu+eL8gyUYbzp/7NWhG+LWeI8Hq52ok=";
+    hash = "sha256-zFV44xqbwe1QsIVurvsjqY+zxC+X2hCljZ5u70kNTSU=";
   };
 
   outputs = [
@@ -67,6 +67,8 @@ buildPythonPackage rec {
   disabledTests = [
     # requires eol python2 interpreter
     "test_can_build_on_python_2"
+    # different formatting for version specifier
+    "test_setuptools_prepare_metadata_for_build_wheel"
   ];
 
   pythonImportsCheck = [ "pyproject_api" ];
