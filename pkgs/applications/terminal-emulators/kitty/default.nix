bb89119ec78ac7db5e2577e4b5591da140bcc789
leiserfg <leiserfg@gmail.com>
2024-09-29 15:21:03 +0200

    kitty: 0.36.1 -> 0.36.4

--- pkgs/applications/terminal-emulators/kitty/default.nix
+++ pkgs/applications/terminal-emulators/kitty/default.nix
@@ -25,28 +25,28 @@
 , zsh
 , fish
 , nixosTests
-, go
-, buildGoModule
+, go_1_23
+, buildGo123Module
 , nix-update-script
 }:
 
 with python3Packages;
 buildPythonApplication rec {
   pname = "kitty";
-  version = "0.36.1";
+  version = "0.36.4";
   format = "other";
 
   src = fetchFromGitHub {
     owner = "kovidgoyal";
     repo = "kitty";
     rev = "refs/tags/v${version}";
-    hash = "sha256-7+MxxgQQlAje7klfJvvEWe8CfxyN0oTGQJ/QOORFUsY=";
+    hash = "sha256-nN0y2VKK5UNaozpHQNPN7AYkto9z6rJNpYRJhvLPtVQ=";
   };
 
-  goModules = (buildGoModule {
+  goModules = (buildGo123Module {
     pname = "kitty-go-modules";
     inherit src version;
-    vendorHash = "sha256-YN4sSdDNDIVgtcykg60H0bZEryRHJJfZ5rXWUMYXGr4=";
+    vendorHash = "sha256-8hsQH7OdsxeVG6pomuxdmTXNmQYBROoUUxoC10LeLFo=";
   }).goModules;
 
   buildInputs = [
@@ -82,7 +82,7 @@ buildPythonApplication rec {
     sphinx-copybutton
     sphinxext-opengraph
     sphinx-inline-tabs
-    go
+    go_1_23
     fontconfig
   ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
     imagemagick
