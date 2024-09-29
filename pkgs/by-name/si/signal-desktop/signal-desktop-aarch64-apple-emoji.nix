30d0006b6bbed389354066a71a881eeb89801c90
Stephen Huan <stephen.huan@cgdct.moe>
2024-09-29 05:41:07 -0400

    signal-desktop: add apple emojis

---
+++ pkgs/by-name/si/signal-desktop/signal-desktop-aarch64-apple-emoji.nix
@@ -0,0 +1,16 @@
+{ callPackage }:
+callPackage ./generic.nix { } {
+  pname = "signal-desktop";
+  version = "7.47.0-1";
+
+  libdir = "usr/lib64/signal-desktop";
+  bindir = "usr/bin";
+  extractPkg = ''
+    mkdir -p $out
+    bsdtar -xf $downloadedFile -C "$out"
+  '';
+
+  url = "https://download.copr.fedorainfracloud.org/results/useidel/signal-desktop/fedora-42-aarch64/08795262-signal-desktop/signal-desktop-7.47.0-1.fc42.aarch64.rpm";
+  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  withUnfree = true;
+}
