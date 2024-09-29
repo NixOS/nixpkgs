e581dd45104660e5d0baa9656240bd6991bf5913
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:07:38 +0200

    python3Packages.typical: 2.8.1 -> 2.9.0
    https://github.com/seandstewart/typical/releases/tag/v2.9.0

--- pkgs/development/python-modules/typical/default.nix
+++ pkgs/development/python-modules/typical/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "typical";
-  version = "2.8.1";
+  version = "2.9.0";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "seandstewart";
     repo = "typical";
     rev = "refs/tags/v${version}";
-    hash = "sha256-2t9Jhdy9NmYBNzdtjjgUnoK2RDEUsAvDkYMcBRzEcmI=";
+    hash = "sha256-RS4hJ7NufClroRPRO3EyHwDaMgg0s0F7D/mqcBr8O18=";
   };
 
   pythonRelaxDeps = [ "pendulum" ];
