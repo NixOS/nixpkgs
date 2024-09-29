2a5132e9926129f24ec3b375f1347a0329abc78c
Colin <colin@uninsane.org>
2024-09-29 15:03:30 +0000

    syshud: 0-unstable-2024-08-27 -> 0-unstable-2024-09-26

--- pkgs/by-name/sy/syshud/package.nix
+++ pkgs/by-name/sy/syshud/package.nix
@@ -2,8 +2,10 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  glibmm,
   gtk4-layer-shell,
   gtkmm4,
+  libevdev,
   nix-update-script,
   pkg-config,
   wireplumber,
@@ -12,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "syshud";
-  version = "0-unstable-2024-08-27";
+  version = "0-unstable-2024-09-26";
 
   src = fetchFromGitHub {
     owner = "System64fumo";
     repo = "syshud";
-    rev = "aa2c153f6aa15962c6b97a77dbe8c45708155fe0";
-    hash = "sha256-SBpufr37K6LC4yc9ircUEBrzuRCKmJzD3C17N34qNGk=";
+    rev = "0b6e4958d8ea66b54bc67f0b5aa005fa3eaa4f6f";
+    hash = "sha256-cyE7ZxesAmnepW7kI6b9Gp7R/v+yG2//EQAj4/X6c+c=";
   };
 
   postPatch = ''
@@ -32,8 +34,10 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   buildInputs = [
+    glibmm
     gtk4-layer-shell
     gtkmm4
+    libevdev
     wireplumber
   ];
 
