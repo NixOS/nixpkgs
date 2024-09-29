841334f22e6f69037f82ac1d9b21f8850a27f0b7
Wolfgang Walther <walther@technowledgy.de>
2024-09-29 10:19:39 +0200

    postgresql: 16.4 -> 17.0
    As discussed in [1] this change is decoupled from the major version
    update for the NixOS postgresql module.
    
    [1]: https://github.com/NixOS/nixpkgs/pull/329611/files#r1701393693

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12165,8 +12165,8 @@ with pkgs;
     postgresql_16_jit
     postgresql_17_jit
   ;
-  postgresql = postgresql_16;
-  postgresql_jit = postgresql_16_jit;
+  postgresql = postgresql_17;
+  postgresql_jit = postgresql_17_jit;
   postgresqlPackages = recurseIntoAttrs postgresql.pkgs;
   postgresqlJitPackages = recurseIntoAttrs postgresql_jit.pkgs;
   postgresql12Packages = recurseIntoAttrs postgresql_12.pkgs;
