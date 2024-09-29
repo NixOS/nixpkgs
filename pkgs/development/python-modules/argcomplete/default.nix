f6c974103e438cdeb59f048cad936e4fccbde0a1
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:03:40 +0200

    python3Packages.argcomplete: 3.4.0 -> 3.5.0
    https://github.com/kislyuk/argcomplete/blob/v3.5.0/Changes.rst

--- pkgs/development/python-modules/argcomplete/default.nix
+++ pkgs/development/python-modules/argcomplete/default.nix
@@ -9,19 +9,19 @@
 
 buildPythonPackage rec {
   pname = "argcomplete";
-  version = "3.4.0";
+  version = "3.5.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "kislyuk";
-    repo = pname;
+    repo = "argcomplete";
     rev = "refs/tags/v${version}";
-    hash = "sha256-4JMyBixc6LcSdpvvLmK4nyyqZMK2kuFcPU7OXhJLpoc=";
+    hash = "sha256-8dqX0h3m3qNJUp/ESEy8grVL+mwESqDnMpI+Dal0r08=";
   };
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
     setuptools-scm
   ];
@@ -32,10 +32,10 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "argcomplete" ];
 
   meta = with lib; {
-    changelog = "https://github.com/kislyuk/argcomplete/blob/v${version}/Changes.rst";
     description = "Bash tab completion for argparse";
-    downloadPage = "https://github.com/kislyuk/argcomplete";
     homepage = "https://kislyuk.github.io/argcomplete/";
+    changelog = "https://github.com/kislyuk/argcomplete/blob/v${version}/Changes.rst";
+    downloadPage = "https://github.com/kislyuk/argcomplete";
     license = licenses.asl20;
     maintainers = with maintainers; [ womfoo ];
   };
