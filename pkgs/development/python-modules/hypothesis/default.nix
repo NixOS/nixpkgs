3a248df1278e8dbdf71a9f42c3390ec9803abf30
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 14:35:50 +0200

    python312Packages.hypothesis: 6.108.5 -> 6.112.2
    https://hypothesis.readthedocs.io/en/latest/changes.html#v6-112-2

--- pkgs/development/python-modules/hypothesis/default.nix
+++ pkgs/development/python-modules/hypothesis/default.nix
@@ -23,7 +23,7 @@
 
 buildPythonPackage rec {
   pname = "hypothesis";
-  version = "6.108.5";
+  version = "6.112.2";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -32,7 +32,7 @@ buildPythonPackage rec {
     owner = "HypothesisWorks";
     repo = "hypothesis";
     rev = "hypothesis-python-${version}";
-    hash = "sha256-APZTI7AxbtLT+2mkxUapr1sPcq4ViMXy1J6g0I30s5A=";
+    hash = "sha256-KVi4BAgrswvhZxArYVnYQ6ksvCf84aSEZbJcRSlwsZE=";
   };
 
   # I tried to package sphinx-selective-exclude, but it throws
