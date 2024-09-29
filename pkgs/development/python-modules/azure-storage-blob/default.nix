38419a3e38af46cf91d096d2d887d6821538d447
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:03:47 +0200

    python3Packages.azure-storage-blob: 12.20.0 -> 12.23.1
    https://github.com/Azure/azure-sdk-for-python/blob/azure-storage-blob_12.23.1/sdk/storage/azure-storage-blob/CHANGELOG.md

--- pkgs/development/python-modules/azure-storage-blob/default.nix
+++ pkgs/development/python-modules/azure-storage-blob/default.nix
@@ -11,14 +11,15 @@
 
 buildPythonPackage rec {
   pname = "azure-storage-blob";
-  version = "12.20.0";
+  version = "12.23.1";
   format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-7rkSVuQdS1ubrWqH/QqK3gfdWKpSNE4sjSdG4noBfTs=";
+    pname = "azure_storage_blob";
+    inherit version;
+    hash = "sha256-pYflTU450qJ711EJ2xZP+iBY/hlAYeVEbFqJvKkYJy8=";
   };
 
   propagatedBuildInputs = [
