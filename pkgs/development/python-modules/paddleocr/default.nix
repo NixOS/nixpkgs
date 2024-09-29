32585dca1faa35c2e7ae82e39bfef7b6aa460cbd
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 17:05:50 +0200

    python3Packages.paddleocr: 2.7.5 -> 2.8.1
    https://github.com/PaddlePaddle/PaddleOCR/releases/tag/v2.8.1

--- pkgs/development/python-modules/paddleocr/default.nix
+++ pkgs/development/python-modules/paddleocr/default.nix
@@ -27,7 +27,7 @@
 }:
 
 let
-  version = "2.7.5";
+  version = "2.8.1";
 in
 buildPythonPackage {
   pname = "paddleocr";
@@ -38,7 +38,7 @@ buildPythonPackage {
     owner = "PaddlePaddle";
     repo = "PaddleOCR";
     rev = "refs/tags/v${version}";
-    hash = "sha256-8mnSV4ga6G2cbYCX84XJRFiLCoXstTAtqvg9QqVN6GI=";
+    hash = "sha256-TLNpb+CwLKvtmPppDuUbGyJorhmkVVW01J61+XUICYk=";
   };
 
   patches = [
