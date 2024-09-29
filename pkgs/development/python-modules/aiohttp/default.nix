7810555fda4dbe687df28cf495c318f6ad82eca5
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-09-29 14:09:22 +0200

    python312Packages.aiohttp: 3.10.7 -> 3.10.8
    https://github.com/aio-libs/aiohttp/blob/v3.10.8/CHANGES.rst

--- pkgs/development/python-modules/aiohttp/default.nix
+++ pkgs/development/python-modules/aiohttp/default.nix
@@ -39,7 +39,7 @@
 
 buildPythonPackage rec {
   pname = "aiohttp";
-  version = "3.10.7";
+  version = "3.10.8";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -48,7 +48,7 @@ buildPythonPackage rec {
     owner = "aio-libs";
     repo = "aiohttp";
     rev = "refs/tags/v${version}";
-    hash = "sha256-UOsWC5P/KT5YMzkAM6WuoOum30b8s4sgSUgR6EskHy0=";
+    hash = "sha256-ksvGRzar1Gp+86WrRFwyHoEdthyzvaAbyubdIhON/sk=";
   };
 
   patches = [
