df424faa4addf75d23c241d05d7612601ea2a7fc
Yaroslav Bolyukin <iam@lach.pw>
2024-09-30 23:13:26 +0200

    qubes-manager: add meta

--- pkgs/by-name/qu/qubes-manager/package.nix
+++ pkgs/by-name/qu/qubes-manager/package.nix
@@ -1,10 +1,19 @@
 {
-  stdenv,
+  lib,
   fetchFromGitHub,
-  python3,
+  python3Packages,
   qt5,
-}: let
-  inherit (python3.pkgs) buildPythonApplication pyqt5 setuptools qubes-core-admin-client qasync pyaml lxml;
+}:
+let
+  inherit (python3Packages)
+    buildPythonApplication
+    pyqt5
+    setuptools
+    qubes-core-admin-client
+    qasync
+    pyaml
+    lxml
+    ;
 
   version = "4.3.4-1";
 
@@ -15,38 +24,52 @@
     hash = "sha256-XcegcVjyb1JoXFFIX5Sj/QlZAn1H4bONwPpX/Yd+Njc=";
   };
 in
+buildPythonApplication {
+  inherit version src;
+  pname = "qubes-manager";
 
-  buildPythonApplication {
-    inherit version src;
-    pname = "qubes-manager";
+  nativeBuildInputs = [
+    setuptools
+    pyqt5
+    qt5.qttools
+    qt5.wrapQtAppsHook
+  ];
 
-    nativeBuildInputs = [
-      setuptools
-      pyqt5
-      qt5.qttools
-      qt5.wrapQtAppsHook
+  dependencies = [
+    pyqt5
+    qubes-core-admin-client
+    qasync
+    pyaml
+    lxml
+    qt5.qtwayland
+  ];
+
+  preBuild = ''
+    make ui res translations python $makeFlags
+  '';
+
+  installTargets = [ "python_install" ];
+
+  postInstall = ''
+    make install $makeFlags
+    mv $out/usr/* $out
+    rm -d $out/usr
+  '';
+
+  makeFlags = [
+    "DESTDIR=$(out)"
+    "LRELEASE_QT5=lrelease"
+  ];
+  pythonImportsCheck = [ "qubesmanager" ];
+
+  meta = {
+    description = "Qubes management UI";
+    homepage = "https://qubes-os.org";
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [
+      lach
+      sigmasquadron
     ];
-
-    dependencies = [
-      pyqt5
-      qubes-core-admin-client
-      qasync
-      pyaml
-      lxml
-    ];
-
-    preBuild = ''
-      make ui res translations python $makeFlags
-    '';
-
-    installTargets = ["python_install"];
-
-    postInstall = ''
-      make install $makeFlags
-      mv $out/usr/* $out
-      rm -d $out/usr
-    '';
-
-    makeFlags = ["DESTDIR=$(out)" "LRELEASE_QT5=lrelease"];
-    pythonImportsCheck = ["qubesmanager"];
-  }
+    platforms = lib.platforms.linux;
+  };
+}
