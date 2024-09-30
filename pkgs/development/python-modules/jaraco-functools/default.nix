23af88e8fdd70a2c8752292a31c6a0741cc9a4e4
Fabian Affolter <mail@fabian-affolter.ch>
2024-09-30 18:46:43 +0200

    python312Packages.jaraco-functools: refactor
    - enable tests
    - add changelog to meta

--- pkgs/development/python-modules/jaraco-functools/default.nix
+++ pkgs/development/python-modules/jaraco-functools/default.nix
@@ -2,9 +2,12 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  jaraco-classes,
   more-itertools,
-  setuptools,
+  pytestCheckHook,
   setuptools-scm,
+  setuptools,
+  pythonOlder,
 }:
 
 buildPythonPackage rec {
@@ -12,20 +15,25 @@ buildPythonPackage rec {
   version = "4.1.0";
   pyproject = true;
 
+  disabled = pythonOlder "3.8";
+
   src = fetchPypi {
     pname = "jaraco_functools";
     inherit version;
     hash = "sha256-cPfg4q4HZJjiElYjJegFIE/Akte0wX4OhslZ4klwGp0=";
   };
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
     setuptools-scm
   ];
 
-  propagatedBuildInputs = [ more-itertools ];
+  dependencies = [ more-itertools ];
 
-  doCheck = false;
+  nativeCheckInputs = [
+    jaraco-classes
+    pytestCheckHook
+  ];
 
   pythonNamespaces = [ "jaraco" ];
 
@@ -34,6 +42,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Additional functools in the spirit of stdlib's functools";
     homepage = "https://github.com/jaraco/jaraco.functools";
+    changelog = "https://github.com/jaraco/jaraco.functools/blob/v${version}/NEWS.rst";
     license = licenses.mit;
     maintainers = [ ];
   };
