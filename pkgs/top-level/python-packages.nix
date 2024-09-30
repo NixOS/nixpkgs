869c766a2577d8aaa88ff3ca4b1bb35fd0d1822a
Gaetan Lepage <gaetan@glepage.com>
2024-09-30 09:30:00 +0200

    python312Packages.nutpie: init at 0.13.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9143,6 +9143,8 @@ self: super: with self; {
 
   nutils-poly = callPackage ../development/python-modules/nutils-poly { };
 
+  nutpie = callPackage ../development/python-modules/nutpie { };
+
   nvchecker = callPackage ../development/python-modules/nvchecker { };
 
   nvdlib = callPackage ../development/python-modules/nvdlib { };
