1e7dfe55143e3a74e5e257dd5395c8d116716260
Alexis Hildebrandt <afh@surryhill.net>
2024-10-01 23:10:49 +0200

    icu76: init at 76.1rc

--- pkgs/development/libraries/icu/default.nix
+++ pkgs/development/libraries/icu/default.nix
@@ -6,6 +6,10 @@ let
   };
 in
 {
+  icu76 = make-icu {
+    version = "76.1rc";
+    hash = "sha256-HMY4hRYbF4Dsf1EGx+m0NRsCUA+Q/JGRpdGIgym/A5I=";
+  };
   icu75 = make-icu {
     version = "75.1";
     hash = "sha256-y5aN8+TS6H6LEcSaXQHHh70TuVRSgPxmQvgmUnYYyu8=";
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20219,6 +20219,7 @@ with pkgs;
     icu73
     icu74
     icu75
+    icu76
   ;
 
   icu = icu74;
