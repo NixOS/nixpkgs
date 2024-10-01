c44a2b7a5576ab87769982d4b329c3cad7e3fba4
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-01 11:11:20 +1000

    bun: init configHook and fetchDeps; bun: switch to finalAttrs from rec
    bun: switch to finalAttrs from rec
    
    Could speed up hydra?
    
    bun: added hook tests and improve passthru for hooks
    
    Co-authored-by: Winter <winter@winter.cafe>
    
    fixup: bun: cleaned up bun hook; bun: fix arguments; bun: remove
    lockfile version specifier; bun: moved fetch-dir hooks to hooks dir;
    bun: fix test to include unzip in nativebuildinputs for benDeps
    
    Lockfile version is lock unless for bugfixes read here for context:
    https://github.com/oven-sh/bun/issues/7295#issuecomment-2613695984
    
    bun: add build test subdir to bundeps
    
    Apply suggestions from code review
    
    Co-authored-by: Seth Flynn <getchoo@tuta.io>

--- pkgs/by-name/bu/bun/package.nix
+++ pkgs/by-name/bu/bun/package.nix
@@ -14,14 +14,16 @@
   cctools,
   darwin,
   rcodesign,
+  callPackage,
+  callPackages,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   version = "1.3.4";
   pname = "bun";
 
   src =
-    passthru.sources.${stdenvNoCC.hostPlatform.system}
+    finalAttrs.passthru.sources.${stdenvNoCC.hostPlatform.system}
       or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
 
   sourceRoot =
@@ -86,19 +88,19 @@ stdenvNoCC.mkDerivation rec {
   passthru = {
     sources = {
       "aarch64-darwin" = fetchurl {
-        url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-darwin-aarch64.zip";
+        url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-darwin-aarch64.zip";
         hash = "sha256-iAN3TkxsVcilF0ZMUI8Cgh5ttX+Uyhu1zCo59NIyalE=";
       };
       "aarch64-linux" = fetchurl {
-        url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-aarch64.zip";
+        url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-linux-aarch64.zip";
         hash = "sha256-xG6EH+2FNHUhkVsbOQTW0XXY4v2RXhjgHBETGCGRFaQ=";
       };
       "x86_64-darwin" = fetchurl {
-        url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-darwin-x64-baseline.zip";
+        url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-darwin-x64-baseline.zip";
         hash = "sha256-D3lW1Rr+1BTIJaOk+OzJvGE/9k5IkBZ3XoUU21E3s18=";
       };
       "x86_64-linux" = fetchurl {
-        url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
+        url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-linux-x64.zip";
         hash = "sha256-M8aZYEno036LgVlZsUsF5bb0lhITUr8RuufQRxk8KL8=";
       };
     };
@@ -112,18 +114,20 @@ stdenvNoCC.mkDerivation rec {
         ]
       }"
       NEW_VERSION=$(curl --silent https://api.github.com/repos/oven-sh/bun/releases/latest | jq '.tag_name | ltrimstr("bun-v")' --raw-output)
-      if [[ "${version}" = "$NEW_VERSION" ]]; then
+      if [[ "${finalAttrs.version}" = "$NEW_VERSION" ]]; then
           echo "The new version same as the old version."
           exit 0
       fi
-      for platform in ${lib.escapeShellArgs meta.platforms}; do
+      for platform in ${lib.escapeShellArgs finalAttrs.meta.platforms}; do
         update-source-version "bun" "$NEW_VERSION" --ignore-same-version --source-key="sources.$platform"
       done
     '';
+
+    inherit (callPackages ./hooks { bun = finalAttrs.finalPackage; }) fetchDeps configHook;
   };
   meta = {
     homepage = "https://bun.sh";
-    changelog = "https://bun.sh/blog/bun-v${version}";
+    changelog = "https://bun.sh/blog/bun-v${finalAttrs.version}";
     description = "Incredibly fast JavaScript runtime, bundler, transpiler and package manager – all in one";
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     longDescription = ''
@@ -141,8 +145,9 @@ stdenvNoCC.mkDerivation rec {
       cdmistman
       coffeeispower
       diogomdp
+      eveeifyeve
     ];
-    platforms = builtins.attrNames passthru.sources;
+    platforms = builtins.attrNames finalAttrs.passthru.sources;
     # Broken for Musl at 2024-01-13, tracking issue:
     # https://github.com/NixOS/nixpkgs/issues/280716
     broken = stdenvNoCC.hostPlatform.isMusl;
@@ -150,4 +155,4 @@ stdenvNoCC.mkDerivation rec {
     # Hangs when run via Rosetta 2 on Apple Silicon
     hydraPlatforms = lib.lists.remove "x86_64-darwin" lib.platforms.all;
   };
-}
+})
