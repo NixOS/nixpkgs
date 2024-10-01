2a42c191d437b0a78dd0cee334ccb32d3703af61
Morgan Helton <mhelton@gmail.com>
2024-09-30 20:48:41 -0500

    pomerium: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24898,7 +24898,6 @@ with pkgs;
 
   pflogsumm = callPackage ../servers/mail/postfix/pflogsumm.nix { };
 
-  pomerium = callPackage ../servers/http/pomerium { };
   pomerium-cli = callPackage ../tools/security/pomerium-cli { };
 
   postgrey = callPackage ../servers/mail/postgrey { };
