1b6755659a271d5f261228d853a7405b984af9d0
Philipp Riegger <philipp@riegger.name>
2024-10-01 19:09:25 +0200

    teamspeak_{client,server}: move distribution note
    Move the distribution permit to a separate file to prevent mangling by
    formatters.
    
    (cherry picked from commit 8b13497049e13643c06d8106c4ce07a8e1af2663)

--- pkgs/applications/networking/instant-messengers/teamspeak/client.nix
+++ pkgs/applications/networking/instant-messengers/teamspeak/client.nix
@@ -110,6 +110,7 @@ stdenv.mkDerivation rec {
     homepage = "https://teamspeak.com/";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = {
+      # See distribution-permit.txt for a confirmation that nixpkgs is allowed to distribute TeamSpeak.
       fullName = "Teamspeak client license";
       url = "https://www.teamspeak.com/en/privacy-and-terms/";
       free = false;
@@ -119,38 +120,3 @@ stdenv.mkDerivation rec {
     platforms = [ "x86_64-linux" ];
   };
 }
-
-/*
-License issues:
-Date: Mon, 10 Dec 2007 19:55:16 -0500
-From: TeamSpeak Sales <sales@tritoncia.com>
-To: 'Marc Weber' <marco-oweber@gmx.de>
-Subject: RE: teamspeak on nix?
-
-Yes, that would be fine.  As long as you are not renting servers or selling
-TeamSpeak then you are more than welcome to distribute it.
-
-Thank you,
-
-TeamSpeak Sales Team
-________________________________
-e-Mail: sales@tritoncia.com
-TeamSpeak: http://www.TeamSpeak.com
-Account Login: https://sales.TritonCIA.com/users
-
-
-
------Original Message-----
-From: Marc Weber [mailto:marco-oweber@gmx.de]
-Sent: Monday, December 10, 2007 5:03 PM
-To: sales@tritoncia.com
-Subject: teamspeak on nix?
-
-Hello,
-
-nix is very young software distribution system (http://nix.cs.uu.nl/)
-I'd like to ask wether you permit us to add teamspeak (server/ client?)
-
-Sincerly
-Marc Weber (small nix contributor)
-*/
