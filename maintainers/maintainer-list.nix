6c5287eb04e3a7eeca944e75afd7d66c8086eeb7
Vladimir Timofeenko <id@vtimofeenko.com>
2024-10-01 10:00:25 -0700

    maintainers: add vtimofeenko to maintainer list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23120,6 +23120,12 @@
     githubId = 7677567;
     name = "Victor SENE";
   };
+  vtimofeenko = {
+    email = "nixpkgs.maintain@vtimofeenko.com";
+    github = "VTimofeenko";
+    githubId = 9886026;
+    name = "Vladimir Timofeenko";
+  };
   vtuan10 = {
     email = "mail@tuan-vo.de";
     github = "vtuan10";
