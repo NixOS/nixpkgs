254a29ad41ab3dd33c21c69518a95d74822c6462
a-kenji <aks.kenji@protonmail.com>
2024-10-01 21:55:13 +0200

    gimoji: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16779,8 +16779,6 @@ with pkgs;
 
   gimme-aws-creds = callPackage ../tools/admin/gimme-aws-creds { };
 
-  gimoji = callPackage ../applications/misc/gimoji { };
-
   guile_1_8 = callPackage ../development/interpreters/guile/1.8.nix { };
 
   # Needed for autogen
