9504993d139b1b093744da3a08d2eab004fdd1f8
natsukium <tomoya.otabi@gmail.com>
2024-10-01 18:04:45 +0900

    python312Packages.cattrs: fix build with attrs>=24.2

--- pkgs/development/python-modules/cattrs/default.nix
+++ pkgs/development/python-modules/cattrs/default.nix
@@ -4,6 +4,7 @@
   buildPythonPackage,
   cbor2,
   fetchFromGitHub,
+  fetchpatch2,
   exceptiongroup,
   hatchling,
   hatch-vcs,
@@ -35,6 +36,24 @@ buildPythonPackage rec {
     rev = "refs/tags/v${version}";
     hash = "sha256-LSP8a/JduK0h9GytfbN7/CjFlnGGChaa3VbbCHQ3AFE=";
   };
+
+  patches = [
+    # https://github.com/python-attrs/cattrs/pull/576
+    (fetchpatch2 {
+      name = "attrs-24_2-compatibility1.patch";
+      url = "https://github.com/python-attrs/cattrs/commit/2d37226ff19506e23bbc291125a29ce514575819.patch";
+      excludes = [
+        "pyproject.toml"
+        "pdm.lock"
+      ];
+      hash = "sha256-nbk7rmOFk42DXYdOgw4Oe3gl3HbxNEtaJ7ZiVSBb3YA=";
+    })
+    (fetchpatch2 {
+      name = "attrs-24_2-compatibility2.patch";
+      url = "https://github.com/python-attrs/cattrs/commit/4bd6dde556042241c6381e1993cedd6514921f58.patch";
+      hash = "sha256-H1xSAYjvVUI8/jON3LWg2F2TlSxejf6TU1jpCeqly6I=";
+    })
+  ];
 
   nativeBuildInputs = [
     hatchling
