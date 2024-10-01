aaf8dc615e4d2d9aefcc6395117a9f34a0985a81
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-10-01 09:19:35 +0200

    jbigkit: add more Archlinux patches, set broken on Darwin

--- pkgs/by-name/jb/jbigkit/package.nix
+++ pkgs/by-name/jb/jbigkit/package.nix
@@ -15,6 +15,25 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   patches = [
+    # Archlinux patch: build shared object
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-shared_lib.patch";
+      hash = "sha256-+efeeKg3FJ/TjSOj58kD+DwnaCm3zhGzKLfUes/d5rg=";
+    })
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-ldflags.patch";
+      hash = "sha256-ik3NifyuhDHnIMTrNLAKInPgu2F5u6Gvk9daqrn8ZhY=";
+    })
+    # Archlinux patch: update coverity
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-coverity.patch";
+      hash = "sha256-APm9A2f4sMufuY3cnL9HOcSCa9ov3pyzgQTTKLd49/E=";
+    })
+    # Archlinux patch: fix build warnings
+    (fetchpatch {
+      url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/jbigkit-2.1-build_warnings.patch";
+      hash = "sha256-lDEJ1bvZ+zR7K4CiTq+aXJ8PGjILE3W13kznLLlGOOg=";
+    })
     # Archlinux patch: this helps users to reduce denial-of-service risks, as in CVE-2017-9937
     (fetchpatch {
       url = "https://gitlab.archlinux.org/archlinux/packaging/packages/jbigkit/-/raw/main/0013-new-jbig.c-limit-s-maxmem-maximum-decoded-image-size.patch";
@@ -30,6 +49,7 @@ stdenv.mkDerivation (finalAttrs: {
   makeFlags = [
     "AR=${lib.getBin stdenv.cc.bintools.bintools}/bin/${stdenv.cc.targetPrefix}ar"
     "CC=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc"
+    "DESTDIR=${placeholder "out"}"
     "RANLIB=${lib.getBin stdenv.cc.bintools.bintools}/bin/${stdenv.cc.targetPrefix}ranlib"
   ];
 
@@ -44,24 +64,23 @@ stdenv.mkDerivation (finalAttrs: {
   installPhase = ''
     runHook preInstall
 
-    install -D -m644 libjbig/libjbig.a $out/lib/libjbig.a
-    install -D -m644 libjbig/libjbig85.a $out/lib/libjbig85.a
-    install -D -m644 libjbig/jbig.h $out/include/jbig.h
-    install -D -m644 libjbig/jbig_ar.h $out/include/jbig_ar.h
-    install -D -m644 libjbig/jbig85.h $out/include/jbig85.h
+    install -vDm 644 libjbig/*.h -t "$out/include/"
+    install -vDm 755 pbmtools/{jbgtopbm{,85},pbmtojbg{,85}} -t "$out/bin/"
+    install -vDm 644 pbmtools/*.1* -t "$out/share/man/man1/"
 
-    install -d -m755 $out/share/man/man1
-    install -m644 pbmtools/*.1* $out/share/man/man1
-
-    install -D -m755 pbmtools/jbgtopbm $out/bin/jbgtopbm
-    install -D -m755 pbmtools/pbmtojbg $out/bin/pbmtojbg
-    install -D -m755 pbmtools/jbgtopbm85 $out/bin/jbgtopbm85
-    install -D -m755 pbmtools/pbmtojbg85 $out/bin/pbmtojbg85
+    install -vDm 755 libjbig/*.so.* -t "$out/lib/"
+    for lib in libjbig.so libjbig85.so; do
+      ln -sv "$lib.${finalAttrs.version}" "$out/lib/$lib"
+      ln -sv "$out/lib/$lib.${finalAttrs.version}" "$out/lib/$lib.0"
+    done
 
     runHook postInstall
   '';
 
+  doCheck = true;
+
   meta = {
+    broken = stdenv.hostPlatform.isDarwin;
     description = "Software implementation of the JBIG1 data compression standard";
     homepage = "http://www.cl.cam.ac.uk/~mgk25/jbigkit/";
     license = lib.licenses.gpl2Plus;
