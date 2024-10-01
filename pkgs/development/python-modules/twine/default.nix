a157d02d6994904f12ce69b45759e373eee696b7
natsukium <tomoya.otabi@gmail.com>
2024-10-01 16:23:08 +0900

    python312Packages.twine: fix build with pkginfo>=1.11

--- pkgs/development/python-modules/twine/default.nix
+++ pkgs/development/python-modules/twine/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   buildPythonPackage,
+  fetchpatch2,
   fetchPypi,
   pythonOlder,
   importlib-metadata,
@@ -30,7 +31,24 @@ buildPythonPackage rec {
     hash = "sha256-mqCCUTnAKzQ02RNUXHuEeiHINeEVl/UlWELUV9ojIts=";
   };
 
+  patches = [
+    # pkginfo>=1.11 compatibility patches
+    # https://github.com/pypa/twine/pull/1123
+    (fetchpatch2 {
+      name = "pkginfo-1_11-compatibility-test.patch";
+      url = "https://github.com/pypa/twine/commit/a3206073b87a8e939cf699777882ebfaced689a0.patch";
+      hash = "sha256-gLN7gJsVng/LFfsrAHjJlqFZTu0wSdeBfnUN+UnLSFk=";
+    })
+    (fetchpatch2 {
+      name = "pkginfo-1_11-compatibility-source.patch";
+      url = "https://github.com/pypa/twine/commit/03e3795659b44f263f527b0467680b238c8fbacc.patch";
+      hash = "sha256-Ne9+G8hMVbklKtcZLiBw29Skz5VO5x2F7yu/KozKgN8=";
+    })
+  ];
+
   nativeBuildInputs = [ setuptools-scm ];
+
+  pythonRelaxDeps = [ "pkginfo" ];
 
   propagatedBuildInputs = [
     importlib-metadata
