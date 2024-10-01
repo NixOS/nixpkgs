0bc21b8a804c90542ee70455beb2c1a1a3c734ea
Patrick Hobusch <patrick@hobusch.net>
2024-10-01 14:19:55 +0800

    omada-software-controller: init at 5.15.8.2

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1209,6 +1209,7 @@
   ./services/networking/ofono.nix
   ./services/networking/oidentd.nix
   ./services/networking/oink.nix
+  ./services/networking/omada.nix
   ./services/networking/onedrive.nix
   ./services/networking/openconnect.nix
   ./services/networking/opengfw.nix
