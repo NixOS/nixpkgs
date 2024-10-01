6634e41b3a859308cb396eba21e6f5e333764339
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-01 18:05:32 +0000

    goa: 3.19.0 -> 3.19.1

--- pkgs/development/tools/goa/default.nix
+++ pkgs/development/tools/goa/default.nix
@@ -5,15 +5,15 @@
 
 buildGoModule rec {
   pname = "goa";
-  version = "3.19.0";
+  version = "3.19.1";
 
   src = fetchFromGitHub {
     owner = "goadesign";
     repo = "goa";
     rev = "v${version}";
-    hash = "sha256-NacLcVYQZTHqhTyX5It+BHD9v/ikUEMZCbfjhNYU98M=";
+    hash = "sha256-UOw0bAUvaKpMmFmAAlheALhtgXU2+Df6b/nSRH7bWHc=";
   };
-  vendorHash = "sha256-9h8+OthG161dHA//6R8oRwNhtpx0HpofJeZ1okEfbaE=";
+  vendorHash = "sha256-IqzW5fOLLBbpPFTE5PiOISdmp3Gq6b8SUbG4CbbU01s=";
 
   subPackages = [ "cmd/goa" ];
 
