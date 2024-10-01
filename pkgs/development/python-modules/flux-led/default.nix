8aa6f22d614fe872a3a011dc3fb65e0d7d508ab7
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-01 21:39:44 +0200

    python312Packages.flux-led: disable failing test

--- pkgs/development/python-modules/flux-led/default.nix
+++ pkgs/development/python-modules/flux-led/default.nix
@@ -6,14 +6,15 @@
   webcolors,
   pythonOlder,
   pytestCheckHook,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "flux-led";
   version = "1.0.4";
-  format = "setuptools";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "Danielhiversen";
@@ -22,29 +23,36 @@ buildPythonPackage rec {
     hash = "sha256-enYo2hZ1C8jqO+8xZhSmIOJQAyrtVUJ9S/e2Bxzhv0I=";
   };
 
-  propagatedBuildInputs = [
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace-fail '"pytest-runner>=5.2",' ""
+  '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [
     async-timeout
     webcolors
   ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace '"pytest-runner>=5.2",' ""
-  '';
-
   pytestFlagsArray = [ "tests.py" ];
 
   pythonImportsCheck = [ "flux_led" ];
 
+  disabledTests = [
+    # AttributeError: module 'webcolors' has no attribute 'CSS2_HEX_TO_NAMES'
+    "test_get_color_names_list"
+  ];
+
   meta = with lib; {
     description = "Python library to communicate with the flux_led smart bulbs";
-    mainProgram = "flux_led";
     homepage = "https://github.com/Danielhiversen/flux_led";
     changelog = "https://github.com/Danielhiversen/flux_led/releases/tag/${version}";
     license = licenses.lgpl3Plus;
     maintainers = with maintainers; [ colemickens ];
     platforms = platforms.linux;
+    mainProgram = "flux_led";
   };
 }
