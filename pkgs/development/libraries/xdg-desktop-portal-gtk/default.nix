b65150618aa7d0a6243db42680d7573263e2102e
Jan Tojnar <jtojnar@gmail.com>
2024-10-02 18:56:37 +0200

    xdg-desktop-portal-gtk: Enable all default portals unconditionally
    In 80ea989ac96cc420089326b2e0d725b1e634499f we updated to x-d-p-gtk 1.10.0, in which some portals were extracted to x-d-p-gnome and were disabled by default in x-d-p-gtk. This was continued with x-d-p-gtk 1.15 bump in 6d646062c8636178e03a4eeea8b75ab7110bcf0f, which default-enabled all portals because of port to Meson’s auto features.
    
    But I do not think the conflict between portal implementations from x-d-p-gtk and x-d-p-gnome has ever been an issue at runtime – since `gnome` comes before `gtk` alphabetically, the `gnome` portal should have been always preferred when both were available.
    And since x-d-p 1.17.0, the portal implementation conflicts are a complete non-issue, as desktop environments can configure which implementation to use for each portal.
    
    Since the benefits of disabling the extra portals are minimal and can lead to conflicts when multiple DEs are enable, we have stopped DEs from doing that in parent commits. This makes the argument unused so let’s remove it.

--- pkgs/development/libraries/xdg-desktop-portal-gtk/default.nix
+++ pkgs/development/libraries/xdg-desktop-portal-gtk/default.nix
@@ -11,7 +11,6 @@
 , glib
 , wrapGAppsHook3
 , gsettings-desktop-schemas
-, buildPortalsInGnome ? true
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -36,17 +35,9 @@ stdenv.mkDerivation (finalAttrs: {
     glib
     gtk3
     xdg-desktop-portal
-  ] ++ lib.optionals buildPortalsInGnome [
     gsettings-desktop-schemas # settings exposed by settings portal
     gnome-desktop
     gnome-settings-daemon # schemas needed for settings api (mostly useless now that fonts were moved to g-d-s, just mouse and xsettings)
-  ];
-
-  mesonFlags = lib.optionals (!buildPortalsInGnome) [
-    "-Dwallpaper=disabled"
-    "-Dsettings=disabled"
-    "-Dappchooser=disabled"
-    "-Dlockdown=disabled"
   ];
 
   meta = with lib; {
