188b5df7cd035edc1e77f5d9c108ce61d679f213
Oliver Schmidt <os@flyingcircus.io>
2024-10-02 18:13:17 +0200

    percona-server, percona-xtrabackup: rework naming
    When reworking the percona package naming towards the two release
    streams "innovation" and "lts", I did not anticipate the case that two
    releases are long-term supported in parallel, and that the latest
    Innovation release is end-of-life and replaced by a more recent lts.
    
    Hence, the first-level attribute names now reflect the package
    major-minor versions again, augmented by attribute names pointing to
    respective lts and innovation releases.

--- pkgs/servers/sql/percona-server/default.nix
+++ pkgs/servers/sql/percona-server/default.nix
@@ -1,12 +1,14 @@
 pkgs: {
-  percona-server_lts = pkgs.callPackage ./lts.nix {
+  # old lts
+  percona-server_8_0 = pkgs.callPackage ./8_0.nix {
     inherit (pkgs.darwin) developer_cmds DarwinTools;
     inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices;
     boost = pkgs.boost177; # Configure checks for specific version.
     icu = pkgs.icu69;
+    # newer versions cause linking failures against `libabsl_spinlock_wait`
     protobuf = pkgs.protobuf_21;
   };
-  percona-server_innovation = pkgs.callPackage ./innovation.nix {
+  percona-server_8_3 = pkgs.callPackage ./8_3.nix {
     inherit (pkgs.darwin) developer_cmds DarwinTools;
     inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices;
     # newer versions cause linking failures against `libabsl_spinlock_wait`
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11089,9 +11089,13 @@ with pkgs;
 
   perceptualdiff = callPackage ../tools/graphics/perceptualdiff { };
 
-  inherit (import ../servers/sql/percona-server pkgs) percona-server_lts percona-server_innovation;
+  inherit (import ../servers/sql/percona-server pkgs) percona-server_8_0 percona-server_8_3;
+  percona-server_lts = percona-server_8_0;
+  percona-server_innovation = percona-server_8_3;
   percona-server = percona-server_lts;
-  inherit (import ../tools/backup/percona-xtrabackup pkgs) percona-xtrabackup_lts percona-xtrabackup_innovation;
+  inherit (import ../tools/backup/percona-xtrabackup pkgs) percona-xtrabackup_8_0 percona-xtrabackup_8_3;
+  percona-xtrabackup_lts = percona-xtrabackup_8_0;
+  percona-xtrabackup_innovation = percona-xtrabackup_8_3;
   percona-xtrabackup = percona-xtrabackup_lts;
 
   pick = callPackage ../tools/misc/pick { };
