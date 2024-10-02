ae2edcfd9fc829bff50d0eb71a70354af8e281b0
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-02 02:51:07 +0200

    python312: 3.12.6 -> 3.12.7
    https://docs.python.org/release/3.12.7/whatsnew/changelog.html

--- pkgs/development/interpreters/python/default.nix
+++ pkgs/development/interpreters/python/default.nix
@@ -20,10 +20,10 @@
       sourceVersion = {
         major = "3";
         minor = "12";
-        patch = "6";
+        patch = "7";
         suffix = "";
       };
-      hash = "sha256-GZllgpjPL7g33/7Y/zwDPvDJjvIM9zxdX2a+1auJaXw=";
+      hash = "sha256-JIh7kuKv1KKsYCQZrUtZY3L2esmwdxkPRZq6OQ+vVVA=";
     };
   };
 
