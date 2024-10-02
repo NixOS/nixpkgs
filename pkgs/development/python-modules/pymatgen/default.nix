611a81b5f916340d70f949b976876750524b3ad9
Jonas <30345682+Jonas-Finkler@users.noreply.github.com>
2024-10-02 13:15:25 +0200

    use --replace-fail
    Co-authored-by: kirillrdy <kirillrdy@gmail.com>

--- pkgs/development/python-modules/pymatgen/default.nix
+++ pkgs/development/python-modules/pymatgen/default.nix
@@ -45,7 +45,7 @@ buildPythonPackage rec {
 
   prePatch = ''
     # Upstream switched to building against numpy2 but should still be compatible with numpy1
-    substituteInPlace pyproject.toml --replace "numpy>=2.1.0" "numpy>=1.26.0"
+    substituteInPlace pyproject.toml --replace-fail "numpy>=2.1.0" "numpy>=1.26.0"
   '';
 
   build-system = [ setuptools ];
