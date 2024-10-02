NixOS/nixpkgs

commit 5ff5ea3bfb10459c4b5bd540da51be981b8cf966

python312Packages.imageio: fix tests with new ffmpeg

pkgs/development/python-modules/imageio/default.nix
@@ -4,6 +4,7 @@
   buildPythonPackage,
   pythonOlder,
   fetchFromGitHub,
+  fetchpatch,
   isPyPy,
   substituteAll,
 
@@ -44,7 +45,15 @@ buildPythonPackage rec {
     hash = "sha256-WeoZE2TPBAhzBBcZNQqoiqvribMCLSZWk/XpdMydvCQ=";
   };
 
-  patches = lib.optionals (!stdenv.hostPlatform.isDarwin) [
+  patches = [
+    # Fix tests failing with new enough ffmpeg
+    # Upstream PR: https://github.com/imageio/imageio/pull/1101
+    # FIXME: remove when merged
+    (fetchpatch {
+      url = "https://github.com/imageio/imageio/commit/8d1bea4b560f3aa10ed2d250e483173f488f50fe.patch";
+      hash = "sha256-68CzSoJzbr21N97gWu5qVYh6QeBS9zon8XmytcVK89c=";
+    })
+  ] ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
     (substituteAll {
       src = ./libgl-path.patch;
       libgl = "${libGL.out}/lib/libGL${stdenv.hostPlatform.extensions.sharedLibrary}";

commit 5a9e0e3c299ecad569eaf1bdcede3addfb83426f

python3{11,12}Packages.imageio: format
Fixes: 5ff5ea3bfb10459c4b5bd540da51be981b8cf966

pkgs/development/python-modules/imageio/default.nix
@@ -45,20 +45,22 @@ buildPythonPackage rec {
     hash = "sha256-WeoZE2TPBAhzBBcZNQqoiqvribMCLSZWk/XpdMydvCQ=";
   };
 
-  patches = [
-    # Fix tests failing with new enough ffmpeg
-    # Upstream PR: https://github.com/imageio/imageio/pull/1101
-    # FIXME: remove when merged
-    (fetchpatch {
-      url = "https://github.com/imageio/imageio/commit/8d1bea4b560f3aa10ed2d250e483173f488f50fe.patch";
-      hash = "sha256-68CzSoJzbr21N97gWu5qVYh6QeBS9zon8XmytcVK89c=";
-    })
-  ] ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
-    (substituteAll {
-      src = ./libgl-path.patch;
-      libgl = "${libGL.out}/lib/libGL${stdenv.hostPlatform.extensions.sharedLibrary}";
-    })
-  ];
+  patches =
+    [
+      # Fix tests failing with new enough ffmpeg
+      # Upstream PR: https://github.com/imageio/imageio/pull/1101
+      # FIXME: remove when merged
+      (fetchpatch {
+        url = "https://github.com/imageio/imageio/commit/8d1bea4b560f3aa10ed2d250e483173f488f50fe.patch";
+        hash = "sha256-68CzSoJzbr21N97gWu5qVYh6QeBS9zon8XmytcVK89c=";
+      })
+    ]
+    ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
+      (substituteAll {
+        src = ./libgl-path.patch;
+        libgl = "${libGL.out}/lib/libGL${stdenv.hostPlatform.extensions.sharedLibrary}";
+      })
+    ];
 
   build-system = [ setuptools ];
 