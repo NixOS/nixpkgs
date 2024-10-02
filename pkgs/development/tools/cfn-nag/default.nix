1998c11b2d4e2f0bea57b19ca0a7212c281c6d2b
Mutsuha Asada <me@momee.mt>
2024-10-02 15:04:22 +0900

    cfn-nag: 0.8.9 -> 0.8.10

--- pkgs/development/tools/cfn-nag/default.nix
+++ pkgs/development/tools/cfn-nag/default.nix
@@ -1,19 +1,24 @@
-{ lib, bundlerEnv, bundlerUpdateScript, ruby }:
+{
+  lib,
+  bundlerEnv,
+  bundlerUpdateScript,
+  ruby,
+}:
 
 bundlerEnv {
   pname = "cfn-nag";
-  version = "0.8.9";
+  version = "0.8.10";
 
   inherit ruby;
   gemdir = ./.;
 
   passthru.updateScript = bundlerUpdateScript "cfn-nag";
 
-  meta = with lib; {
+  meta = {
     description = "Linting tool for CloudFormation templates";
     homepage = "https://github.com/stelligent/cfn_nag";
-    license = licenses.mit;
-    maintainers = [ ];
-    platforms = platforms.unix;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ momeemt ];
+    platforms = lib.platforms.unix;
   };
 }
