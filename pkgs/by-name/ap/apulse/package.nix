a3bbe6aaee76b58b56555ca058c69658b687b3d6
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-02 12:00:50 -0300

    apulse: refactor
    - finalAttrs
    - split outputs
    - strictDeps

--- pkgs/by-name/ap/apulse/package.nix
+++ pkgs/by-name/ap/apulse/package.nix
@@ -1,28 +1,33 @@
 {
   lib,
-  stdenv,
-  fetchFromGitHub,
   alsa-lib,
   cmake,
-  pkg-config,
+  fetchFromGitHub,
   glib,
-  tracingSupport ? true,
+  pkg-config,
+  pkgsMusl,
+  stdenv,
+  # Boolean flags
   logToStderr ? true,
+  tracingSupport ? true,
 }:
 
-let
-  oz = x: if x then "1" else "0";
-in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "apulse";
   version = "0.1.13";
 
   src = fetchFromGitHub {
+    name = "apulse-source-${finalAttrs.version}";
     owner = "i-rinat";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "1p6fh6ah5v3qz7dxhcsixx38bxg44ypbim4m03bxk3ls5i9xslmn";
+    repo = "apulse";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-tlLdUyyajtnXAJXUuK4n5PWFRu9RM9jb+XjsApWBztw=";
   };
+
+  outputs = [
+    "out"
+    "man"
+  ];
 
   nativeBuildInputs = [
     cmake
@@ -35,16 +40,50 @@ stdenv.mkDerivation rec {
   ];
 
   cmakeFlags = [
-    "-DWITH_TRACE=${oz tracingSupport}"
-    "-DLOG_TO_STDERR=${oz logToStderr}"
+    (lib.cmakeBool "LOG_TO_STDERR" logToStderr)
+    (lib.cmakeBool "WITH_TRACE" tracingSupport)
   ];
 
-  meta = with lib; {
-    description = "PulseAudio emulation for ALSA";
-    homepage = "https://github.com/i-rinat/apulse";
-    license = licenses.mit;
-    platforms = platforms.linux;
-    maintainers = [ maintainers.jagajaga ];
-    mainProgram = "apulse";
+  strictDeps = true;
+
+  passthru = {
+    tests = {
+      apulse-musl = pkgsMusl.apulse;
+    };
   };
-}
+
+  meta = {
+    homepage = "https://github.com/i-rinat/apulse";
+    description = "PulseAudio emulation for ALSA";
+    longDescription = ''
+      The program provides an alternative partial implementation of the
+      PulseAudio API. It consists of a loader script and a number of shared
+      libraries with the same names as from original PulseAudio, so applications
+      could dynamically load them and think they are talking to PulseAudio.
+      Internally, no separate sound mixing daemon is used. Instead, apulse
+      relies on ALSA's dmix, dsnoop, and plug plugins to handle multiple sound
+      sources and capture streams running at the same time. dmix plugin muxes
+      multiple playback streams; dsnoop plugin allow multiple applications to
+      capture from a single microphone; and plug plugin transparently converts
+      audio between various sample formats, sample rates and channel
+      numbers. For more than a decade now, ALSA comes with these plugins enabled
+      and configured by default.
+
+      apulse wasn't designed to be a drop-in replacement of PulseAudio. It's
+      pointless, since that will be just reimplementation of original
+      PulseAudio, with the same client-daemon architecture, required by the
+      complete feature set. Instead, only parts of the API that are crucial to
+      specific applications are implemented. That's why there is a loader
+      script, named apulse. It updates value of LD_LIBRARY_PATH environment
+      variable to point also to the directory where apulse's libraries are
+      installed, making them available to the application.
+    '';
+    license = lib.licenses.mit;
+    mainProgram = "apulse";
+    maintainers = with lib.maintainers; [
+      AndersonTorres
+      jagajaga
+    ];
+    platforms = lib.platforms.linux;
+  };
+})
