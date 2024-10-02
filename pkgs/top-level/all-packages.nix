e5ddbb1974a398c47278bc343c173bb87ed365cf
Michael Adler <therisen06@gmail.com>
2024-10-02 07:51:15 +0200

    goda: move to pkgs/by-name, format

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -642,8 +642,6 @@ with pkgs;
 
   gobble = callPackage ../tools/X11/gobble { };
 
-  goda = callPackage ../development/tools/goda { };
-
   gokrazy = callPackage ../development/misc/gokrazy { };
 
   gojq = callPackage ../development/tools/gojq { };
