fff1b626712cdc781358717a21f579af654e9883
Bobby Rong <rjl931189261@126.com>
2024-10-02 14:59:34 +0000

    gtk4: 4.16.1 → 4.16.2
    https://gitlab.gnome.org/GNOME/gtk/-/compare/4.16.1...4.16.2
    
    Changelog-reviewed-by: Bobby Rong <rjl931189261@126.com>

--- pkgs/development/libraries/gtk/4.x.nix
+++ pkgs/development/libraries/gtk/4.x.nix
@@ -69,7 +69,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gtk4";
-  version = "4.16.1";
+  version = "4.16.2";
 
   outputs = [ "out" "dev" ] ++ lib.optionals x11Support [ "devdoc" ];
   outputBin = "dev";
@@ -81,7 +81,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = with finalAttrs; "mirror://gnome/sources/gtk/${lib.versions.majorMinor version}/gtk-${version}.tar.xz";
-    hash = "sha256-kmoeq9SB+b+iVTj5UBb9/Pr5Y7L5C0CuaekN71SZIVw=";
+    hash = "sha256-NLYkhI5d4ioTi2da1vOcDHudZ5B8EOH8flsDBg6NVDc=";
   };
 
   depsBuildBuild = [
