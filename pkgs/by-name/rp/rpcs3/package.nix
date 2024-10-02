19cfb0b3709974404ae0484ec34445afd4974a44
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-02 16:27:56 +0000

    rpcs3: 0.0.33-16959-60b5adab6 -> 0.0.33-16999-938306a7b

--- pkgs/by-name/rp/rpcs3/package.nix
+++ pkgs/by-name/rp/rpcs3/package.nix
@@ -33,10 +33,10 @@
 
 let
   # Keep these separate so the update script can regex them
-  rpcs3GitVersion = "16959-60b5adab6";
-  rpcs3Version = "0.0.33-16959-60b5adab6";
-  rpcs3Revision = "60b5adab636f0cef6987165b38b42cbd7b2a9768";
-  rpcs3Hash = "sha256-0/cNiPUnjuik6YyiNeZ5ZAZBExex5WVW56PCzm80Xx8=";
+  rpcs3GitVersion = "16999-938306a7b";
+  rpcs3Version = "0.0.33-16999-938306a7b";
+  rpcs3Revision = "938306a7bc86edd0dfdd995d28e818f36240bf8f";
+  rpcs3Hash = "sha256-5XufzJ3LmhBpLiIscIW3KBgj/R9aSDD5zFiBo/D+p34=";
 
   inherit (qt6Packages) qtbase qtmultimedia wrapQtAppsHook qtwayland;
 in
