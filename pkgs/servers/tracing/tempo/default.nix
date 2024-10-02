aec335fc3ed123867fd6122e95ba0d896e8d90bf
K900 <me@0upti.me>
2024-10-02 10:00:47 +0300

    tempo: pin to Go 1.22 for now

--- pkgs/servers/tracing/tempo/default.nix
+++ pkgs/servers/tracing/tempo/default.nix
@@ -1,6 +1,8 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGo122Module, fetchFromGitHub }:
 
-buildGoModule rec {
+# Does not build with Go 1.23
+# FIXME: check again for next release
+buildGo122Module rec {
   pname = "tempo";
   version = "2.6.0";
 
