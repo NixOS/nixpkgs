8d14fa2886fec877690c6d28cfcdba4503dbbcea
Oliver Schmidt <os@flyingcircus.io>
2024-10-02 18:46:14 +0200

    percona: apply nixfmt

--- pkgs/tools/backup/percona-xtrabackup/generic.nix
+++ pkgs/tools/backup/percona-xtrabackup/generic.nix
@@ -1,8 +1,36 @@
-{ lib, stdenv, fetchFromGitHub, bison, boost, cmake, makeWrapper, pkg-config
-, curl, cyrus_sasl, libaio, libedit, libev, libevent, libgcrypt, libgpg-error, lz4
-, ncurses, numactl, openssl, procps, protobuf, valgrind, xxd, zlib
-, perlPackages
-, version, hash, fetchSubmodules ? false, extraPatches ? [], extraPostInstall ? "", ...
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  bison,
+  boost,
+  cmake,
+  makeWrapper,
+  pkg-config,
+  curl,
+  cyrus_sasl,
+  libaio,
+  libedit,
+  libev,
+  libevent,
+  libgcrypt,
+  libgpg-error,
+  lz4,
+  ncurses,
+  numactl,
+  openssl,
+  procps,
+  protobuf,
+  valgrind,
+  xxd,
+  zlib,
+  perlPackages,
+  version,
+  hash,
+  fetchSubmodules ? false,
+  extraPatches ? [ ],
+  extraPostInstall ? "",
+  ...
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -16,12 +44,39 @@ stdenv.mkDerivation (finalAttrs: {
     inherit hash fetchSubmodules;
   };
 
-  nativeBuildInputs = [ bison boost cmake makeWrapper pkg-config ];
+  nativeBuildInputs = [
+    bison
+    boost
+    cmake
+    makeWrapper
+    pkg-config
+  ];
 
-  buildInputs = [
-    (curl.override { inherit openssl; }) cyrus_sasl libaio libedit libevent libev libgcrypt libgpg-error lz4
-    ncurses numactl openssl procps protobuf valgrind xxd zlib
-  ] ++ (with perlPackages; [ perl DBI DBDmysql ]);
+  buildInputs =
+    [
+      (curl.override { inherit openssl; })
+      cyrus_sasl
+      libaio
+      libedit
+      libevent
+      libev
+      libgcrypt
+      libgpg-error
+      lz4
+      ncurses
+      numactl
+      openssl
+      procps
+      protobuf
+      valgrind
+      xxd
+      zlib
+    ]
+    ++ (with perlPackages; [
+      perl
+      DBI
+      DBDmysql
+    ]);
 
   patches = extraPatches;
 
@@ -42,10 +97,12 @@ stdenv.mkDerivation (finalAttrs: {
     "-DWITH_MAN_PAGES=OFF"
   ];
 
-  postInstall = ''
-    wrapProgram "$out"/bin/xtrabackup --prefix PERL5LIB : $PERL5LIB
-    rm -r "$out"/lib/plugin/debug
-  '' + extraPostInstall;
+  postInstall =
+    ''
+      wrapProgram "$out"/bin/xtrabackup --prefix PERL5LIB : $PERL5LIB
+      rm -r "$out"/lib/plugin/debug
+    ''
+    + extraPostInstall;
 
   passthru.mysqlVersion = lib.versions.majorMinor finalAttrs.version;
 
