eca9ed5701106f89322901b1a218423b6242ac92
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-03 03:03:28 +0000

    mbtileserver: 0.10.0 -> 0.11.0

--- pkgs/servers/geospatial/mbtileserver/default.nix
+++ pkgs/servers/geospatial/mbtileserver/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "mbtileserver";
-  version = "0.10.0";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "consbio";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-hKDgKiy3tmZ7gxmxZlflJHcxatrSqE1d1uhSLJh8XLo=";
+    sha256 = "sha256-RLaAhc24zdCFvpSN2LZXNyS1ygg9zCi4jEj8owdreWU=";
   };
 
-  vendorHash = "sha256-QcyFnzRdGdrVqgKEMbhaD7C7dkGKKhTesMMZKrrLx70=";
+  vendorHash = "sha256-yn7LcR/DvHDSRicUnWLrFZKqZti+YQoLSk3mZkDIj10=";
 
   meta = with lib; {
     description = "Simple Go-based server for map tiles stored in mbtiles format";
