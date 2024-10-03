9309dc73e725b1ffc1c7f04e197d15e12afb1fed
Jan van Brügge <supermanitu@gmail.com>
2024-10-03 15:15:50 +0100

    authentik.outposts.proxy: init at 2024.8.3

--- pkgs/by-name/au/authentik/outposts.nix
+++ pkgs/by-name/au/authentik/outposts.nix
@@ -1,4 +1,5 @@
 { callPackage, authentik }: {
   ldap = callPackage ./ldap.nix { authentik = authentik; };
+  proxy = callPackage ./proxy.nix { authentik = authentik; };
   radius = callPackage ./radius.nix { authentik = authentik; };
 }
