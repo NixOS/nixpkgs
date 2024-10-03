8717d3b50c76864682308dce81526034e050fede
Yongun Seong <nevivurn@nevi.dev>
2024-10-04 00:44:57 +0900

    kubelogin-oidc: 1.30.0 -> 1.30.1 (#346100)
    - Diff: https://github.com/int128/kubelogin/compare/v1.30.0...v1.30.1
    - Pinned buildGo123Module to fix build
    - Added version in ldflags for correct version reporting
    - Run all package tests during build
    - Moved to by-name
    - Added myself as maintainer

--- pkgs/by-name/ku/kubelogin-oidc/package.nix
+++ pkgs/by-name/ku/kubelogin-oidc/package.nix
@@ -1,19 +1,33 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{
+  lib,
+  buildGo123Module,
+  fetchFromGitHub,
+}:
 
-buildGoModule rec {
+buildGo123Module rec {
   pname = "kubelogin";
-  version = "1.30.0";
+  version = "1.30.1";
 
   src = fetchFromGitHub {
     owner = "int128";
-    repo = pname;
+    repo = "kubelogin";
     rev = "v${version}";
-    sha256 = "sha256-DHg46t0gMypK6Nj428gpOMtPuA+XcW4IJU39CHTVGPw=";
+    hash = "sha256-aoLPT3lX+q426QlxAPsjeQyTZMnmqMGh85jJPU7lQVU=";
   };
 
-  subPackages = ["."];
+  subPackages = [ "." ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=v${version}"
+  ];
 
   vendorHash = "sha256-gr+SsC7MiLj/MZ8kca5Hcfge+7Pm4y963TfwyKHEhBY=";
+
+  # test all packages
+  preCheck = ''
+    unset subPackages
+  '';
 
   # Rename the binary instead of symlinking to avoid conflict with the
   # Azure version of kubelogin
@@ -21,11 +35,14 @@ buildGoModule rec {
     mv $out/bin/kubelogin $out/bin/kubectl-oidc_login
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Kubernetes credential plugin implementing OpenID Connect (OIDC) authentication";
     mainProgram = "kubectl-oidc_login";
     inherit (src.meta) homepage;
-    license = licenses.asl20;
-    maintainers = with maintainers; [ benley ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
+      benley
+      nevivurn
+    ];
   };
 }
