1eec2d3b0c429714eb05ad847a5fd60576fc6eb5
Mutsuha Asada <me@momee.mt>
2024-10-03 12:28:16 +0000

    bundletool: moved to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4217,8 +4217,6 @@ with pkgs;
 
   buildtorrent = callPackage ../tools/misc/buildtorrent { };
 
-  bundletool = callPackage ../development/tools/bundletool { };
-
   bwm_ng = callPackage ../tools/networking/bwm-ng { };
 
   bwbasic = callPackage ../development/interpreters/bwbasic { };
