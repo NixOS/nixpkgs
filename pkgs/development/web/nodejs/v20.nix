58afec363052b2565d8d7651919ec1e402290f2a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2024-10-03 21:53:17 +0200

    nodejs_20: 20.17.0 -> 20.18.0
    (cherry picked from commit 0789f53499110776ec56bd980c429b88d332dc7e)

--- pkgs/development/web/nodejs/v20.nix
+++ pkgs/development/web/nodejs/v20.nix
@@ -12,8 +12,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "20.17.0";
-  sha256 = "9abf03ac23362c60387ebb633a516303637145cb3c177be3348b16880fd8b28c";
+  version = "20.18.0";
+  sha256 = "7d9433e91fd88d82ba8de86e711ec41907638e227993d22e95126b02f6cd714a";
   patches = [
     ./disable-darwin-v8-system-instrumentation-node19.patch
     ./bypass-darwin-xcrun-node16.patch
