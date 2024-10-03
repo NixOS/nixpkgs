009ed37d5a2d463f73069d5502dd42a38809c76f
seth <getchoo@tuta.io>
2024-10-03 19:00:41 -0400

    cargo-tauri: add `passthru.updateScript`

--- pkgs/by-name/ca/cargo-tauri/package.nix
+++ pkgs/by-name/ca/cargo-tauri/package.nix
@@ -6,6 +6,7 @@
   fetchFromGitHub,
   darwin,
   gtk4,
+  nix-update-script,
   openssl,
   pkg-config,
   webkitgtk_4_1,
@@ -56,6 +57,8 @@ rustPlatform.buildRustPackage rec {
     tests = {
       setupHooks = callPackage ./test-app.nix { };
     };
+
+    updateScript = nix-update-script { };
   };
 
   meta = {
