28fe3d044ab2b16705a1404723a08e2dc66e9060
Sergei Trofimovich <slyich@gmail.com>
2024-10-03 06:42:59 +0100

    xwayland: 24.1.2 -> 24.1.3
    Changes: https://lists.x.org/archives/xorg/2024-October/061730.html

--- pkgs/servers/x11/xorg/xwayland.nix
+++ pkgs/servers/x11/xorg/xwayland.nix
@@ -49,11 +49,11 @@
 
 stdenv.mkDerivation rec {
   pname = "xwayland";
-  version = "24.1.2";
+  version = "24.1.3";
 
   src = fetchurl {
     url = "mirror://xorg/individual/xserver/${pname}-${version}.tar.xz";
-    hash = "sha256-FB63bn5CKjZhwIeCxwvkCTEIR1UELARQbg2X3UY+99I=";
+    hash = "sha256-3NtXpmzJsSTI+TZ2BZJiisTnRKfXsxeaqGGJrX6kyxA=";
   };
 
   postPatch = ''
