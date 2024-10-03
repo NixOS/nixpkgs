a19fb6e96892180334779ec145a110eb6fdb8fe0
Austin Horstman <khaneliman12@gmail.com>
2024-10-03 10:48:42 -0500

    p3x-onenote: format

--- pkgs/applications/office/p3x-onenote/default.nix
+++ pkgs/applications/office/p3x-onenote/default.nix
@@ -1,20 +1,30 @@
-{ lib, stdenv, appimageTools, desktop-file-utils, fetchurl }:
+{
+  lib,
+  stdenv,
+  appimageTools,
+  desktop-file-utils,
+  fetchurl,
+}:
 
 let
   pname = "p3x-onenote";
   version = "2023.4.117";
 
-  plat = {
-    aarch64-linux = "-arm64";
-    armv7l-linux = "-armv7l";
-    x86_64-linux = "";
-  }.${stdenv.hostPlatform.system};
+  plat =
+    {
+      aarch64-linux = "-arm64";
+      armv7l-linux = "-armv7l";
+      x86_64-linux = "";
+    }
+    .${stdenv.hostPlatform.system};
 
-  sha256 = {
-    aarch64-linux = "sha256-HFuxmMo0m4UOxEQVd32LGvbFsOS8jwCCCS6K/YJIIBE=";
-    armv7l-linux = "sha256-JMgYvqkaRw5sfjbKybAkk28KT12+c19dMir2DUN7Ub0=";
-    x86_64-linux = "sha256-hr/mPOrliP8Dej3DVE2+wYkb1J789WCkkY3xe9EcM44=";
-  }.${stdenv.hostPlatform.system};
+  sha256 =
+    {
+      aarch64-linux = "sha256-HFuxmMo0m4UOxEQVd32LGvbFsOS8jwCCCS6K/YJIIBE=";
+      armv7l-linux = "sha256-JMgYvqkaRw5sfjbKybAkk28KT12+c19dMir2DUN7Ub0=";
+      x86_64-linux = "sha256-hr/mPOrliP8Dej3DVE2+wYkb1J789WCkkY3xe9EcM44=";
+    }
+    .${stdenv.hostPlatform.system};
 
   src = fetchurl {
     url = "https://github.com/patrikx3/onenote/releases/download/v${version}/P3X-OneNote-${version}${plat}.AppImage";
@@ -45,7 +55,11 @@ appimageTools.wrapType2 rec {
     description = "Linux Electron Onenote - A Linux compatible version of OneNote";
     license = licenses.mit;
     maintainers = with maintainers; [ tiagolobocastro ];
-    platforms = [ "x86_64-linux" "aarch64-linux" "armv7l-linux" ];
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "armv7l-linux"
+    ];
     mainProgram = "p3x-onenote";
   };
 }
