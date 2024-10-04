d0567a434378f43989f069ab3aeb4c218472d286
Nikolay Korotkiy <sikmir@disroot.org>
2024-10-05 02:07:30 +0400

    python312Packages.django-split-settings: init at 1.3.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3470,6 +3470,8 @@ self: super: with self; {
 
   django-scopes = callPackage ../development/python-modules/django-scopes { };
 
+  django-split-settings = callPackage ../development/python-modules/django-split-settings { };
+
   djangoql = callPackage ../development/python-modules/djangoql { };
 
   django-ranged-response = callPackage ../development/python-modules/django-ranged-response { };
