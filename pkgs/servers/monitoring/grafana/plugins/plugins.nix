f8d3ae98e62c581c3515f100861966c87e09e919
Simonas Kazlauskas <git@kazlauskas.me>
2024-10-04 21:50:51 +0300

    grafanaPlugins.marcusolsson-calendar-panel: init at 3.7.0

--- pkgs/servers/monitoring/grafana/plugins/plugins.nix
+++ pkgs/servers/monitoring/grafana/plugins/plugins.nix
@@ -21,6 +21,7 @@
   grafana-polystat-panel = callPackage ./grafana-polystat-panel { };
   grafana-worldmap-panel = callPackage ./grafana-worldmap-panel { };
   marcusolsson-dynamictext-panel = callPackage ./marcusolsson-dynamictext-panel { };
+  marcusolsson-calendar-panel = callPackage ./marcusolsson-calendar-panel { };
   redis-app = callPackage ./redis-app { };
   redis-datasource = callPackage ./redis-datasource { };
   redis-explorer-app = callPackage ./redis-explorer-app { };
