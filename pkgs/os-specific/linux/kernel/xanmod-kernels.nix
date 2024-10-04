e10d6394100457f2236045c446dd5ea79ff85399
Shawn8901 <shawn8901@googlemail.com>
2024-10-04 23:42:13 +0200

    linux_xanmod: 6.6.52 -> 6.6.54

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -7,8 +7,8 @@ let
   # kernel config in the xanmod version commit
   variants = {
     lts = {
-      version = "6.6.52";
-      hash = "sha256-bTsxESXY0uhx9/UjUg9cATC1ErfkxF1gH8TXimYVzX4=";
+      version = "6.6.54";
+      hash = "sha256-i6Rm0Qasgxpw9XcK938IJ+qZ+V2z82zpuly2Au2J18Q=";
     };
     main = {
       version = "6.10.11";
