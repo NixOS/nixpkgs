c2a020b857eb6abca54e1fb0e64c36fcd47eea71
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-04 19:30:39 -0300

    libremidi: refactor
    - finalAttrs
    - nixfmt-rfc-style
    - adopted by AndersonTorres

--- pkgs/by-name/li/libremidi/package.nix
+++ pkgs/by-name/li/libremidi/package.nix
@@ -1,9 +1,10 @@
-{ alsa-lib
-, cmake
-, darwin
-, fetchFromGitHub
-, lib
-, stdenv
+{
+  lib,
+  alsa-lib,
+  cmake,
+  darwin,
+  fetchFromGitHub,
+  stdenv,
 }:
 
 let
@@ -11,38 +12,44 @@ let
     CoreAudio
     CoreFoundation
     CoreMIDI
-    CoreServices;
+    CoreServices
+    ;
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libremidi";
   version = "4.4.0";
 
   src = fetchFromGitHub {
     owner = "jcelerier";
     repo = "libremidi";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-raVBJ75/UmM3P69s8VNUXRE/2jV4WqPIfI4eXaf6UEg=";
   };
 
-  nativeBuildInputs = [ cmake ];
-
-  buildInputs = lib.optional stdenv.hostPlatform.isLinux alsa-lib
-    ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    CoreAudio
-    CoreFoundation
-    CoreMIDI
-    CoreServices
+  nativeBuildInputs = [
+    cmake
   ];
+
+  buildInputs =
+    lib.optionals stdenv.hostPlatform.isLinux [
+      alsa-lib
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      CoreAudio
+      CoreFoundation
+      CoreMIDI
+      CoreServices
+    ];
 
   postInstall = ''
     cp -r $src/include $out
   '';
 
   meta = {
-    description = "Modern C++ MIDI real-time & file I/O library";
     homepage = "https://github.com/jcelerier/libremidi";
-    maintainers = [ ];
+    description = "Modern C++ MIDI real-time & file I/O library";
     license = lib.licenses.bsd2;
+    maintainers = with lib.maintainers; [ AndersonTorres ];
     platforms = lib.platforms.all;
   };
-}
+})
