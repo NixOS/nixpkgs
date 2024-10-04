b6e041cb94115f4c4e04e6c8b15890d36fb5eeb9
Robert Schütz <mail@dotlambda.de>
2024-10-04 14:17:04 -0700

    python312Packages.keyutils: drop
    The upstream repository has been archived.

--- pkgs/top-level/python-aliases.nix
+++ pkgs/top-level/python-aliases.nix
@@ -292,6 +292,7 @@ mapAliases ({
   Kajiki = kajiki; # added 2023-02-19
   keepkey_agent = keepkey-agent; # added 2024-01-06
   Keras = keras; # added 2021-11-25
+  keyutils = throw "keyutils has been removed because the upstream repository was archived in 2022"; # added 2024-10-04
   ldap = python-ldap; # added 2022-09-16
   lammps-cython = throw "lammps-cython no longer builds and is unmaintained"; # added 2021-07-04
   langchainplus-sdk = langsmith; # added 2023-08-01
