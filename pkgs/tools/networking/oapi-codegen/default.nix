d75f8839dde4dd58254bcb4d3c9ffb000f74c2f3
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-04 22:20:33 +0000

    oapi-codegen: 2.4.0 -> 2.4.1

--- pkgs/tools/networking/oapi-codegen/default.nix
+++ pkgs/tools/networking/oapi-codegen/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "oapi-codegen";
-  version = "2.4.0";
+  version = "2.4.1";
 
   src = fetchFromGitHub {
     owner = "deepmap";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-Byb4bTtdn2Xi5hZXsAtcXA868VGQO6RORj1M2x8EAzg=";
+    hash = "sha256-21VhHSyfF+NHkXlr2svjwBNZmfS1O448POBP9XUQxak=";
   } ;
 
   vendorHash = "sha256-bp5sFZNJFQonwfF1RjCnOMKZQkofHuqG0bXdG5Hf3jU=";
