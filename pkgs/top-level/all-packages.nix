7be3d140aabc12a01e7c99ac45f82c83c23d0b2e
Austin Horstman <khaneliman12@gmail.com>
2024-10-04 13:26:06 -0500

    p3x-onenote: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10978,8 +10978,6 @@ with pkgs;
 
   payload-dumper-go = callPackage ../tools/archivers/payload-dumper-go { };
 
-  p3x-onenote = callPackage ../applications/office/p3x-onenote { };
-
   p4c = callPackage ../development/compilers/p4c {
     protobuf = protobuf_21;
   };
