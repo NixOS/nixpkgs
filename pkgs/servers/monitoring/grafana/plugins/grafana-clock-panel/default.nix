3bcf288ce5ff8079c54a9768719b39339f5695d5
Simonas Kazlauskas <git@kazlauskas.me>
2024-10-04 21:44:30 +0300

    grafanaPlugins.grafana-clock-panel: 2.1.3 -> 2.1.8

--- pkgs/servers/monitoring/grafana/plugins/grafana-clock-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-clock-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin rec {
   pname = "grafana-clock-panel";
-  version = "2.1.3";
-  zipHash = "sha256-ZedeV/SQsBu55jAxFyyXQefir4hEl1/TQDmaTJN9bag=";
+  version = "2.1.8";
+  zipHash = "sha256-QLvq2CSlJuEaYAazn8MoY3XCiXeRILj4dTp/aqrHL/k=";
   meta = with lib; {
     description = "Clock panel for Grafana";
     license = licenses.mit;
