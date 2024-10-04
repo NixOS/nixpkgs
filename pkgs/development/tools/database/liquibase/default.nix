NixOS/nixpkgs

commit f373abe60b11bf83c48a7fa84fc9f2c5c8d5151e

liquibase: Use `finalAttrs` for recursive fixpoint

pkgs/development/tools/database/liquibase/default.nix
@@ -23,21 +23,17 @@ let
     ];
 in
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "liquibase";
   version = "4.29.2";
 
   src = fetchurl {
-    url = "https://github.com/liquibase/liquibase/releases/download/v${version}/${pname}-${version}.tar.gz";
+    url = "https://github.com/liquibase/liquibase/releases/download/v${finalAttrs.version}/liquibase-${finalAttrs.version}.tar.gz";
     hash = "sha256-HQF6IGqVqzB2pS9mBnnC2AufIXSULLBxXjXVOTHiDuk=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ jre ];
-
-  unpackPhase = ''
-    tar xfz ${src}
-  '';
 
   installPhase =
     let addJars = dir: ''
@@ -53,12 +49,12 @@ stdenv.mkDerivation rec {
       mkdir -p $out/internal/lib
       mv ./internal/lib/*.jar $out/internal/lib/
 
-      mkdir -p $out/share/doc/${pname}-${version}
+      mkdir -p $out/share/doc/liquibase-${finalAttrs.version}
       mv LICENSE.txt \
          README.txt \
          ABOUT.txt \
          changelog.txt \
-         $out/share/doc/${pname}-${version}
+         $out/share/doc/liquibase-${finalAttrs.version}
 
       mkdir -p $out/bin
       # there’s a lot of escaping, but I’m not sure how to improve that
@@ -89,10 +85,10 @@ stdenv.mkDerivation rec {
     description = "Version Control for your database";
     mainProgram = "liquibase";
     homepage = "https://www.liquibase.org/";
-    changelog = "https://raw.githubusercontent.com/liquibase/liquibase/v${version}/changelog.txt";
+    changelog = "https://raw.githubusercontent.com/liquibase/liquibase/v${finalAttrs.version}/changelog.txt";
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     license = licenses.asl20;
     maintainers = with maintainers; [ jsoo1 ];
     platforms = with platforms; unix;
   };
-}
+})

commit 655ef48dbe7b9303b1aa1a9bfde097a124855c24

liquibase: Add `sourceRoot`
To fix the build issue that was introduced in f373abe60b11bf83c48a7fa84fc9f2c5c8d5151e
when the custom `unpackPhase` was removed without correctly validating the build,
which results in:
```
error: builder for '/nix/store/5qax6r6kxi4spn5bc7qjpgglxb9kvhxp-liquibase-4.29.2.drv' failed with exit code 1;
       last 3 log lines:
       > Running phase: unpackPhase
       > unpacking source archive /nix/store/v12lcsjxsq3n4lgi8za3k9mqc8pak59b-liquibase-4.29.2.tar.gz
       > unpacker produced multiple directories
       For full logs, run 'nix log /nix/store/5qax6r6kxi4spn5bc7qjpgglxb9kvhxp-liquibase-4.29.2.drv'.
```

pkgs/development/tools/database/liquibase/default.nix
@@ -35,6 +35,8 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ jre ];
 
+  sourceRoot = ".";
+
   installPhase =
     let addJars = dir: ''
       for jar in ${dir}/*.jar; do