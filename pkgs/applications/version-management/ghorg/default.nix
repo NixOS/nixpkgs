3344825da0a2b309ce041c589f167ee3412dee8c
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-04 23:36:44 +0000

    ghorg: 1.10.0 -> 1.11.0

--- pkgs/applications/version-management/ghorg/default.nix
+++ pkgs/applications/version-management/ghorg/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "ghorg";
-  version = "1.10.0";
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "gabrie30";
     repo = "ghorg";
     rev = "v${version}";
-    sha256 = "sha256-MZGpH8fvSERSMUxGa1JX4mb8d/p3aDztgRH1FQA8f/Y=";
+    sha256 = "sha256-tsR7cBpDa311rLI1CTXD2LLl3j2PxkBeQsjkUP99iIo=";
   };
 
   doCheck = false;
