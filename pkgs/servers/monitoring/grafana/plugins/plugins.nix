3e87b09d6cb32c8c68d42e7b8fe44d8728b9a602
Simonas Kazlauskas <git@kazlauskas.me>
2024-10-04 21:50:20 +0300

    grafanaPlugins.grafana-opensearch-datasource: init at 2.19.0

--- pkgs/servers/monitoring/grafana/plugins/plugins.nix
+++ pkgs/servers/monitoring/grafana/plugins/plugins.nix
@@ -16,6 +16,7 @@
   grafana-googlesheets-datasource = callPackage ./grafana-googlesheets-datasource { };
   grafana-mqtt-datasource = callPackage ./grafana-mqtt-datasource { };
   grafana-oncall-app = callPackage ./grafana-oncall-app { };
+  grafana-opensearch-datasource = callPackage ./grafana-opensearch-datasource { };
   grafana-piechart-panel = callPackage ./grafana-piechart-panel { };
   grafana-polystat-panel = callPackage ./grafana-polystat-panel { };
   grafana-worldmap-panel = callPackage ./grafana-worldmap-panel { };
