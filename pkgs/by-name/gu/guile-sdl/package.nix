ed18b8c474eed237eb3c97df41e725bc0eafae79
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-04 20:31:45 -0300

    guile-sdl: refactor

--- pkgs/by-name/gu/guile-sdl/package.nix
+++ pkgs/by-name/gu/guile-sdl/package.nix
@@ -47,9 +47,7 @@ stdenv.mkDerivation (finalAttrs: {
         paths = finalAttrs.buildInputs;
       };
     in
-    [
-      "SDLMINUSI=-I${sdl-env}/include/SDL"
-    ];
+    [ "SDLMINUSI=-I${sdl-env}/include/SDL" ];
 
   strictDeps = true;
 
@@ -59,8 +57,7 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://www.gnu.org/software/guile-sdl/";
     description = "Guile bindings for SDL";
     license = lib.licenses.gpl3Plus;
-    maintainers = lib.teams.sdl.members
-                  ++ (with lib.maintainers; [ ]);
+    maintainers = lib.teams.sdl.members ++ (with lib.maintainers; [ ]);
     inherit (guile.meta) platforms;
   };
 })
