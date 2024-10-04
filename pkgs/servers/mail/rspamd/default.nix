015006b2558e3a4753ca8a04bdbcbc53ed187601
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-04 01:39:29 +0000

    rspamd: 3.9.1 -> 3.10.0

--- pkgs/servers/mail/rspamd/default.nix
+++ pkgs/servers/mail/rspamd/default.nix
@@ -33,13 +33,13 @@ assert withHyperscan -> stdenv.hostPlatform.isx86_64;
 
 stdenv.mkDerivation rec {
   pname = "rspamd";
-  version = "3.9.1";
+  version = "3.10.0";
 
   src = fetchFromGitHub {
     owner = "rspamd";
     repo = "rspamd";
     rev = version;
-    hash = "sha256-qT94DqX5Dt9pLgqed/j+dtiykwRFY3Fsbvcub7IzAZE=";
+    hash = "sha256-fsxeTgciZb0HqvRWoUjwe/mrRZPW23hOwx7PJBSlBSo=";
   };
 
   hardeningEnable = [ "pie" ];
