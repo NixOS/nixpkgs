40733640c8ca8f0ed92812bc7fd71ba23beac768
Winston R. Milling <Winston@Milli.ng>
2024-10-04 12:13:07 -0500

    k3s_1_29: 1.29.8 -> 1.29.9
    https://github.com/k3s-io/k3s/releases/tag/v1.29.9%2Bk3s1

--- pkgs/applications/networking/cluster/k3s/1_29/versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_29/versions.nix
@@ -1,15 +1,15 @@
 {
-  k3sVersion = "1.29.8+k3s1";
-  k3sCommit = "33fdc35dd67cf6c07989327e992fd26ed89b2449";
-  k3sRepoSha256 = "0ky5f039nkhdj6y5v9yr6lk875l29c67j6kqc2dzdb3iqbwskcbr";
-  k3sVendorHash = "sha256-VxVGBvpeKf/nuw09Llf85d4P8oCD2GvD1f0Mxt6fMj8=";
+  k3sVersion = "1.29.9+k3s1";
+  k3sCommit = "e92d3b3ba7a4810e82e38a3d5c6091a3e18caad5";
+  k3sRepoSha256 = "1i4881nv41dpvxmh20qy121d45431xbhkzlywx1rfwqla8wq6bwh";
+  k3sVendorHash = "sha256-JeX9SJw6U1/FMbv9fVQeQvAZKq+Z99ZrLC2bAy1vUkA=";
   chartVersions = import ./chart-versions.nix;
   imagesVersions = builtins.fromJSON (builtins.readFile ./images-versions.json);
   k3sRootVersion = "0.14.0";
   k3sRootSha256 = "15cs9faw3jishsb5nhgmb5ldjc47hkwf7hz2126fp8ahf80m0fcl";
-  k3sCNIVersion = "1.4.0-k3s2";
-  k3sCNISha256 = "17dg6jgjx18nrlyfmkv14dhzxsljz4774zgwz5dchxcf38bvarqa";
-  containerdVersion = "1.7.20-k3s1";
-  containerdSha256 = "12ihr3z8vcglv5b0v9ris29zkkkdvjbcp3bf7ym71a0xdbg83s8i";
+  k3sCNIVersion = "1.5.1-k3s1";
+  k3sCNISha256 = "1bkz78p77aiw64hdvmlgc5zir9x8zha8qprfaab48jxbcsj3dfi7";
+  containerdVersion = "1.7.21-k3s2";
+  containerdSha256 = "0kp93fhmw2kiy46hw3ag05z8xwhw7kqp4wcbhsxshsdf0929g539";
   criCtlVersion = "1.29.0-k3s1";
 }
