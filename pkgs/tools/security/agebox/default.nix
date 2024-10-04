09ace9672d2044de13ea8d49a8bd939248c11080
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-10-04 13:54:37 +0200

    agebox: 0.6.1 -> 0.7.1
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/tools/security/agebox/default.nix
+++ pkgs/tools/security/agebox/default.nix
@@ -1,30 +1,26 @@
-{ lib, buildGoModule, fetchFromGitHub, fetchpatch }:
+{
+  lib,
+  # Required based on 'go' directive in go.mod,
+  # remove when Go in nixpkgs defaults to 1.23 or later.
+  buildGo123Module,
+  fetchFromGitHub,
+}:
 
-buildGoModule rec {
+buildGo123Module rec {
   pname = "agebox";
-  version = "0.6.1";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "slok";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-W6/v5BIl+k6tMan/Wdua7mHKMsq23QZN13Cy24akJr4=";
+    hash = "sha256-RtFa7k+tw0hyf7bYm51aIxptaD4uOH6/3WDjeoWEEKA=";
   };
 
-  patches = [
-    # Update gopkg.in/yaml.v2 to v2.2.8 to fix vulnerabilities.
-    # https://github.com/slok/agebox/pull/199
-    (fetchpatch {
-      url = "https://github.com/slok/agebox/commit/40a515d39911f601ebe05cc914e8a02695d85dc7.patch";
-      hash = "sha256-0iBI0nID12OoWqWY/8MPb3vvTUDe0JdSHu2vefix/bM=";
-    })
-  ];
-
-  vendorHash = "sha256-MNAF2ExIOYPzXyGR6H7lfUEhnMDCyD7ecst5MKm7u+A=";
+  vendorHash = "sha256-57YbYDvRYOzQATEFpAuGzQzOYNY8n5LUrcu8jhjSiNI=";
 
   ldflags = [
     "-s"
-    "-w"
     "-X main.Version=${version}"
   ];
 
