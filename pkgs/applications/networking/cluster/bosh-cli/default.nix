eaf33ea63a5bfeda80556b8e01fb29993f3776fc
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-04 09:52:55 +0000

    bosh-cli: 7.7.2 -> 7.8.0

--- pkgs/applications/networking/cluster/bosh-cli/default.nix
+++ pkgs/applications/networking/cluster/bosh-cli/default.nix
@@ -8,13 +8,13 @@
 buildGoModule rec {
   pname = "bosh-cli";
 
-  version = "7.7.2";
+  version = "7.8.0";
 
   src = fetchFromGitHub {
     owner = "cloudfoundry";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-orn1pxHefYCp0vPsZ4TI5Y76tpR8dOcEmxA3cHwfYQU=";
+    sha256 = "sha256-DiW7Vq1rfkOqMM3133mIRmRyFDjFITRkXqY34HMdZtc=";
   };
   vendorHash = null;
 
