e96de561f57f42d7a720455ec0e3c0e2c7e26030
Simonas Kazlauskas <git@kazlauskas.me>
2024-10-04 21:47:01 +0300

    grafanaPlugins.fetzerch-sunandmoon-datasource: init at 0.3.3

--- pkgs/servers/monitoring/grafana/plugins/plugins.nix
+++ pkgs/servers/monitoring/grafana/plugins/plugins.nix
@@ -6,6 +6,7 @@
 
   bsull-console-datasource = callPackage ./bsull-console-datasource { };
   doitintl-bigquery-datasource = callPackage ./doitintl-bigquery-datasource { };
+  fetzerch-sunandmoon-datasource = callPackage ./fetzerch-sunandmoon-datasource { };
   grafadruid-druid-datasource = callPackage ./grafadruid-druid-datasource { };
   grafana-clickhouse-datasource = callPackage ./grafana-clickhouse-datasource { };
   grafana-clock-panel = callPackage ./grafana-clock-panel { };
