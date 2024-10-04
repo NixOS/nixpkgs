324b77da91ed2a6a90d20ebf3e536c454e54b6f3
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-04 03:49:19 +0000

    qtrvsim: 0.9.7 -> 0.9.8

--- pkgs/applications/science/computer-architecture/qtrvsim/default.nix
+++ pkgs/applications/science/computer-architecture/qtrvsim/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "QtRVSim";
-  version = "0.9.7";
+  version = "0.9.8";
 
   src = fetchFromGitHub {
     owner = "cvut";
     repo = "qtrvsim";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-SelmqHauj5Yxg043NZqR4bhqW5clwg1h7UD8mW7j7vE=";
+    sha256 = "sha256-+EpPDA2+mBTdQjq6i9TN11yeXqvJA28JtmdNihM1a/U=";
   };
 
   nativeBuildInputs = [ cmake wrapQtAppsHook ];
