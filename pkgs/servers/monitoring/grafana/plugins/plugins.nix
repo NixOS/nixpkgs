02d544b79ecd9cdf607aac09241098966acde076
Simonas Kazlauskas <git@kazlauskas.me>
2024-10-04 21:52:23 +0300

    grafanaPlugins.marcusolsson-json-datasource: init at 1.3.17

--- pkgs/servers/monitoring/grafana/plugins/plugins.nix
+++ pkgs/servers/monitoring/grafana/plugins/plugins.nix
@@ -23,6 +23,7 @@
   marcusolsson-calendar-panel = callPackage ./marcusolsson-calendar-panel { };
   marcusolsson-csv-datasource = callPackage ./marcusolsson-csv-datasource { };
   marcusolsson-dynamictext-panel = callPackage ./marcusolsson-dynamictext-panel { };
+  marcusolsson-json-datasource = callPackage ./marcusolsson-json-datasource { };
   redis-app = callPackage ./redis-app { };
   redis-datasource = callPackage ./redis-datasource { };
   redis-explorer-app = callPackage ./redis-explorer-app { };
