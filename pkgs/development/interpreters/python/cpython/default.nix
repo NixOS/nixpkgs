2d045bb1721e0d71c842e752fbe8e5080b8bb00e
Emily <vcs@emily.moe>
2024-10-05 19:22:18 +0100

    python3: remove code for macOS < 11

--- pkgs/development/interpreters/python/cpython/default.nix
+++ pkgs/development/interpreters/python/cpython/default.nix
@@ -25,7 +25,6 @@
 
 # platform-specific dependencies
 , bash
-, apple-sdk_11
 , darwin
 , windows
 
@@ -178,9 +177,6 @@ let
     bluez
   ] ++ optionals enableFramework [
     darwin.apple_sdk.frameworks.Cocoa
-  ] ++ optionals stdenv.hostPlatform.isDarwin [
-    # Work around for ld64 crashes on x86_64-darwin. Remove once 11.0 becomes the default.
-    apple-sdk_11
   ] ++ optionals stdenv.hostPlatform.isMinGW [
     windows.dlfcn
     windows.mingw_w64_pthreads
