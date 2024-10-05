dc9c7aed22b81eeed0d0fe4ea3833abfed95f553
Nick Cao <nickcao@nichi.co>
2024-10-05 15:04:39 -0400

    telegram-desktop: 5.5.5 -> 5.6.0
    Diff: https://github.com/telegramdesktop/tdesktop/compare/v5.5.5...v5.6.0
    
    Changelog: https://github.com/telegramdesktop/tdesktop/releases/tag/v5.6.0

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/default.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/default.nix
@@ -63,14 +63,14 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "telegram-desktop";
-  version = "5.5.5";
+  version = "5.6.0";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-LWIOgyHp43bLN4RQtBKH2HitfVI6AKstPK5es2s+wVw=";
+    hash = "sha256-whhVe8n3CM2Z6xIDo5SEM2rEP9TTYrco2SizJeKEXGs=";
   };
 
   patches = [
