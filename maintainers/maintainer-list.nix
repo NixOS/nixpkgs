b62ab468f80a26aa5a56ecaa26ae82f5fcad8105
Jannik <jannik@outlook.com>
2024-10-05 21:05:34 +0200

    maintainers: add jeyemwey

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9879,6 +9879,12 @@
     githubId = 110620;
     name = "Jevin Maltais";
   };
+  jeyemwey = {
+    name = "Jannik";
+    email = "jannik@outlook.com";
+    github = "jeyemwey";
+    githubId = 2796271;
+  };
   jfb = {
     email = "james@yamtime.com";
     github = "tftio";
