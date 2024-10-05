8c31c4e46032f81e57dac8b0df8790f9a971d74b
emilylange <git@emilylange.de>
2024-10-05 15:18:03 +0200

    apko: add emilylange as maintainer

--- pkgs/development/tools/apko/default.nix
+++ pkgs/development/tools/apko/default.nix
@@ -74,6 +74,6 @@ buildGoModule rec {
     description = "Build OCI images using APK directly without Dockerfile";
     mainProgram = "apko";
     license = licenses.asl20;
-    maintainers = with maintainers; [ jk developer-guy ];
+    maintainers = with maintainers; [ jk developer-guy emilylange ];
   };
 }
