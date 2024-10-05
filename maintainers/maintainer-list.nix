70724bf1c266250f547793945d6cc45d83dd1aa3
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-10-05 17:06:39 +0100

    maintainers: add ElliottSullingeFarrall

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6189,6 +6189,12 @@
     github = "elliottslaughter";
     githubId = 3129;
   };
+  ElliottSullingeFarrall = {
+    name = "Elliott Sullinge-Farrall";
+    email = "elliott.chalford@gmail.com";
+    github = "ElliottSullingeFarrall";
+    githubId = 108588212;
+  };
   elliottvillars = {
     email = "elliottvillars@gmail.com";
     github = "elliottvillars";
