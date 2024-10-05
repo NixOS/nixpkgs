6601b9ba3af5b54395e7306658620d21408431c7
Nabile Rahmani <nabile13.pc@gmail.com>
2024-10-05 17:53:29 +0200

    ryujinx: archive.org tarball mirror
    Should fix source builds by relying on our beloved archive.org to host a [SHA256-matching tarball generated from a mirror of the repo](https://archive.org/details/ryujinx-5dbba-07e-33e-83c-9047dcbb-701c-9655edbbe-89086.tar).
    
    I have only `nix repl`'d the fetchTarball expression for its consistency check, as I do not currently have the time nor storage to build the package locally, sorry about that.
    
    We could also generate archives to address https://github.com/NixOS/nixpkgs/issues/345410 and the 24.05 branch.
    
    Supersedes: https://github.com/NixOS/nixpkgs/pull/345881

--- pkgs/by-name/ry/ryujinx/package.nix
+++ pkgs/by-name/ry/ryujinx/package.nix
@@ -1,7 +1,7 @@
 { lib
 , buildDotnetModule
 , dotnetCorePackages
-, fetchFromGitHub
+, fetchzip
 , libX11
 , libgdiplus
 , ffmpeg
@@ -27,10 +27,8 @@ buildDotnetModule rec {
   pname = "ryujinx";
   version = "1.1.1401"; # Based off of the official github actions builds: https://github.com/Ryujinx/Ryujinx/actions/workflows/release.yml
 
-  src = fetchFromGitHub {
-    owner = "Ryujinx";
-    repo = "Ryujinx";
-    rev = "5dbba07e33e83c9047dcbb701c9655edbbe89086";
+  src = fetchzip {
+    url = "https://archive.org/download/ryujinx-5dbba-07e-33e-83c-9047dcbb-701c-9655edbbe-89086.tar/Ryujinx-5dbba07e33e83c9047dcbb701c9655edbbe89086.tar.gz";
     hash = "sha256-UeJ3KE5e5H9crqroAxjmxYTf/Z4cbj41a6/1HW2nLcA=";
   };
 
