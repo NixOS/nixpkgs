2ae6e53d37ea1789352fce3c2d79fa47e18cc164
oluceps <i@oluceps.uk>
2024-10-06 18:29:23 +0800

    hysteria: 2.5.0 -> 2.5.2
    Diff: https://github.com/apernet/hysteria/compare/app/v2.5.0...app/v2.5.2

--- pkgs/tools/networking/hysteria/default.nix
+++ pkgs/tools/networking/hysteria/default.nix
@@ -4,16 +4,16 @@
 }:
 buildGoModule rec {
   pname = "hysteria";
-  version = "2.5.0";
+  version = "2.5.2";
 
   src = fetchFromGitHub {
     owner = "apernet";
     repo = pname;
     rev = "app/v${version}";
-    hash = "sha256-vtGJRPQBOO8Ig794FJ3gTrR0LOZdWH1vAc7IcZSq/SE=";
+    hash = "sha256-ClWbA3cjQXK8tzXfmApBQ+TBnbRc6f36G1iIFcNQi7o=";
   };
 
-  vendorHash = "sha256-1VLws98/iAW8BnxOhbshp01D6+kb4CJOvncC5floN5o=";
+  vendorHash = "sha256-I5SCr45IT8gl8eD9BburxHBodOpP+R5rk9Khczx5z8M=";
   proxyVendor = true;
 
   ldflags =
