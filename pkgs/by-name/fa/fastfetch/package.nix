994b1b5fab470dd45a5e55b7e946abb0107b4471
Austin Horstman <khaneliman12@gmail.com>
2024-10-06 02:22:16 -0500

    fastfetch: 2.26.1 -> 2.27.1 (#346791)
    * fastfetch: 2.26.1 -> 2.27.0
    
    * fastfetch: 2.27.0 -> 2.27.1

--- pkgs/by-name/fa/fastfetch/package.nix
+++ pkgs/by-name/fa/fastfetch/package.nix
@@ -47,13 +47,13 @@ let
 in
 stdenv'.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "2.26.1";
+  version = "2.27.1";
 
   src = fetchFromGitHub {
     owner = "fastfetch-cli";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-0TRhMK45mfCft56R07lUbnxjfQrIAXONy4f6ykpc5X8=";
+    hash = "sha256-N/C6vGNebOFQ5RRpHD2hTqvHbX5mwICqIeFsdSCjaR4=";
   };
 
   outputs = [
