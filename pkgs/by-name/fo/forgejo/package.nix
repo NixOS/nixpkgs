7b95577686f5a06cefb9da191347ee09bcbd4ef9
Marie Ramlow <me@nycode.dev>
2024-10-06 14:20:23 +0200

    forgejo: 8.0.3 -> 9.0.0
    Forgejo v9 is now licensed under GPL-3.0-or-later.
    Release notes: https://codeberg.org/forgejo/forgejo/milestone/7235

--- pkgs/by-name/fo/forgejo/package.nix
+++ pkgs/by-name/fo/forgejo/package.nix
@@ -1,8 +1,9 @@
 import ./generic.nix {
-  version = "8.0.3";
-  hash = "sha256-PvCWUiJIs9ktuJetPYZT0V8S8+OYahCDZiZQpvWWXhY=";
-  npmDepsHash = "sha256-E4eq4OompY8e+722PbSFCmcarpYBpO/n9X6GVU9AhDU=";
-  vendorHash = "sha256-4l4kscwesW/cR8mZjE3G9HcVm0d1ukxbtBY6RXYRi8k=";
+  version = "9.0.0";
+  rev = "0ae05e1000d2dd8354b932fcc19c8eda23647da6";
+  hash = "sha256-BXVPB8DXkMkKv/5CFZsxx7xjBvA62Oogk+zC5brjXGY=";
+  npmDepsHash = "sha256-UFUNOR+ks3hDmT7uVEToX+rMmlFL6gQqigAxl6RP37Q=";
+  vendorHash = "sha256-j3BY6fEXCL82TDna80vjL25FDFLUhyMtmQW8d6GLQdk=";
   lts = false;
   nixUpdateExtraArgs = [
     "--override-filename"
