64fafe6b4894c95727fce8bade41a33e0f6ce51b
mkenkel <35666729+mkenkel@users.noreply.github.com>
2024-10-06 17:01:59 -0400

    Apply suggestions from code review
    Thanks for the quick turnaround!
    
    Co-authored-by: Daniil Nartsissov <daniilnartsissov@gmail.com>

--- pkgs/by-name/sa/sarasa-term-nerdfont/package.nix
+++ pkgs/by-name/sa/sarasa-term-nerdfont/package.nix
@@ -7,10 +7,10 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "sarasa-term-sc-nerd";
-  version = "v1.1.0";
+  version = "1.1.0";
 
   src = fetchurl {
-    url = "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/${version}/sarasa-term-sc-nerd.ttf.tar.gz";
+    url = "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v${version}/sarasa-term-sc-nerd.ttf.tar.gz";
     hash = "sha256-ADS5KTYQMTELd8MjAE+ugEwC5Gr8qDpN5kPirvgogAc=";
   };
 
@@ -27,7 +27,7 @@ stdenvNoCC.mkDerivation rec {
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Font based on Sarasa Term SC font.";
     longDescription = "Sarasa Term SC Nerd font is based on Sarasa Term SC font. Nerd fonts
     patch program is modified, Nerd fonts is merged into Sarasa Term SC by this program,
@@ -35,8 +35,8 @@ stdenvNoCC.mkDerivation rec {
 
     This font is especially suitable for Simplified Chinese users to use in terminal or code editor.";
     homepage = "https://github.com/laishulu/Sarasa-Term-SC-Nerd";
-    license = licenses.ofl;
-    maintainers = with maintainers; [ mkenkel ];
-    platforms = platforms.all;
+    license = lib.licenses.ofl;
+    maintainers = with lib.maintainers; [ mkenkel ];
+    platforms = lib.platforms.all;
   };
 }
