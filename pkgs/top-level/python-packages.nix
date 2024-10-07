0dfaf335480bffbfc4673e10f04ca81646d21252
Dennis Wuitz <dennish@wuitz.de>
2024-10-07 11:15:06 +0200

    python3Packages.mtcnn: init at 0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8250,6 +8250,8 @@ self: super: with self; {
 
   msrest = callPackage ../development/python-modules/msrest { };
 
+  mtcnn = callPackage ../development/python-modules/mtcnn { };
+
   mt-940 = callPackage ../development/python-modules/mt-940 { };
 
   mullvad-api = callPackage ../development/python-modules/mullvad-api { };
