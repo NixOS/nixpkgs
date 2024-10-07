557657ce6834bdeb8a2f7c344aa879c2d34416c9
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-07 10:05:30 +0200

    python312Packages.mypy-boto3-quicksight: 1.35.29 -> 1.35.33

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1074,8 +1074,8 @@ rec {
       "sha256-mtpp+ro3b7tOrN4TrWr8BjLzaPo264ty8Sng6wtciMs=";
 
   mypy-boto3-quicksight =
-    buildMypyBoto3Package "quicksight" "1.35.29"
-      "sha256-CzKgkJToddpBtTnmVJCc+jbAeAxQVC1h++UMrXjiry0=";
+    buildMypyBoto3Package "quicksight" "1.35.33"
+      "sha256-sqrWVvrPBS/Wq+m9QDhrvQ+8prlVmrq1VDrAl4Ro9dg=";
 
   mypy-boto3-ram =
     buildMypyBoto3Package "ram" "1.35.0"
