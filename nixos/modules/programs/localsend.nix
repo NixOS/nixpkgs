027c4df2cb7f2919ea2165440745eb2d20b081dc
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2024-10-07 18:15:45 +0200

    localsend: add programs.localsend.port option
    Even if not exposed or documented yet as configuration option, the code allows
    to configure the port [0]. Therefore, it makes sense to provide a dedicated
    configuration option.
    
    https://github.com/localsend/localsend/blob/88bba52b108541615c9c73943fc53648734c091c/app/lib/provider/network/server/server_provider.dart#L83

--- nixos/modules/programs/localsend.nix
+++ nixos/modules/programs/localsend.nix
@@ -6,24 +6,31 @@
 }:
 let
   cfg = config.programs.localsend;
-  firewallPort = 53317;
 in
 {
   options.programs.localsend = {
     enable = lib.mkEnableOption "localsend, an open source cross-platform alternative to AirDrop";
 
-    openFirewall = lib.mkEnableOption "opening the firewall port ${toString firewallPort} for receiving files" // {
+    openFirewall = lib.mkEnableOption "opening the firewall port for receiving files" // {
       default = true;
     };
 
     package = lib.mkPackageOption pkgs "localsend" { };
+
+    # Note that as of version v1.15 (October 2024), the port can not be
+    # changed. However, this may change in the future.
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 53317;
+      description = "TCP and UDP port used in receive mode for incoming traffic";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ cfg.package ];
     networking.firewall = {
-      allowedTCPPorts = lib.optional cfg.openFirewall firewallPort;
-      allowedUDPPorts = lib.optional cfg.openFirewall firewallPort;
+      allowedTCPPorts = lib.optional cfg.openFirewall cfg.port;
+      allowedUDPPorts = lib.optional cfg.openFirewall cfg.port;
     };
   };
 
