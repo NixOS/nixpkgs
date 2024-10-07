f1162a168a5ade7cf71c521856cf65d610d0d1a3
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-07 13:40:01 +0200

    python312Packages.vsure: 2.6.7 -> 2.6.8
    Changelog: https://github.com/persandstrom/python-verisure#version-history

--- pkgs/development/python-modules/vsure/default.nix
+++ pkgs/development/python-modules/vsure/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "vsure";
-  version = "2.6.7";
+  version = "2.6.8";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-/eVFa1BTFbvFTAt48Bv+bjsV7f2eVSuKARJQVxDqU9s=";
+    hash = "sha256-dz7Ud8sOIz/w9IiRgDZWDln65efgf6skNmECwg+MRw0=";
   };
 
   propagatedBuildInputs = [
