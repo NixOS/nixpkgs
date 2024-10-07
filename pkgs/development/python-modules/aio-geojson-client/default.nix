26db1e1fa5a408ea1380f19493a7a2bea488eefb
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-07 09:21:02 +0200

    python312Packages.aio-geojson-client: 0.20 -> 0.21
    Diff: https://github.com/exxamalte/python-aio-geojson-client/compare/refs/tags/v0.20...v0.21
    
    Changelog: https://github.com/exxamalte/python-aio-geojson-client/blob/v0.21/CHANGELOG.md

--- pkgs/development/python-modules/aio-geojson-client/default.nix
+++ pkgs/development/python-modules/aio-geojson-client/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "aio-geojson-client";
-  version = "0.20";
+  version = "0.21";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "exxamalte";
     repo = "python-aio-geojson-client";
     rev = "refs/tags/v${version}";
-    hash = "sha256-GASjsOCZ4lSK0+VtIuVxFNxjMCbHkUGy/KSBtGLSaXw=";
+    hash = "sha256-zHgqsl278XBr2X8oQOsnIQxfyYuB5G8NLcTNy4oerUI=";
   };
 
   __darwinAllowLocalNetworking = true;
