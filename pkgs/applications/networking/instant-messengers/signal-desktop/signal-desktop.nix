06a98b940817bc7c8d0543a7348ca2f7efe457d6
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-07 10:23:35 +0000

    signal-desktop: 7.26.0 -> 7.27.0

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "7.26.0";
+  version = "7.27.0";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-A9gTphQ7FQm1iCutrDxAD2wazihn+4ZafJrqdZ8Nahk=";
+  hash = "sha256-Ff/hRoanAVlaAu5ofZ6oy4ULifk3O0HiQK0Z8Wd1H1g=";
 }
