ed2525e1787b578ad93256c0cf2d616471fd3d31
Minijackson <minijackson@riseup.net>
2024-10-07 16:14:33 +0200

    netbox_4_1: init at 4.1.3

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -659,6 +659,7 @@ in {
   netbox_3_6 = handleTest ./web-apps/netbox.nix { netbox = pkgs.netbox_3_6; };
   netbox_3_7 = handleTest ./web-apps/netbox.nix { netbox = pkgs.netbox_3_7; };
   netbox_4_0 = handleTest ./web-apps/netbox.nix { netbox = pkgs.netbox_4_0; };
+  netbox_4_1 = handleTest ./web-apps/netbox.nix { netbox = pkgs.netbox_4_1; };
   netbox-upgrade = handleTest ./web-apps/netbox-upgrade.nix {};
   # TODO: put in networking.nix after the test becomes more complete
   networkingProxy = handleTest ./networking-proxy.nix {};
