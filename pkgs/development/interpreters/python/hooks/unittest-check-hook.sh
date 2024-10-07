779255f777724e4ac31b881b09734312ed305b97
Yueh-Shun Li <shamrocklee@posteo.net>
2024-10-07 08:39:26 +0800

    unittestCheckHook: lint with ShellCheck

--- pkgs/development/interpreters/python/hooks/unittest-check-hook.sh
+++ pkgs/development/interpreters/python/hooks/unittest-check-hook.sh
@@ -1,22 +1,25 @@
 # Setup hook for unittest.
+# shellcheck shell=bash
+
 echo "Sourcing unittest-check-hook"
 
 # shellcheck source=pkgs/development/interpreters/python/compat-helpers.sh
 source @compatHelpers@
 
-canonicalizeFlagsArrayExpandString unittestFlagsArray
+expandStringAndConvertToArray unittestFlagsArray
 
 unittestCheckPhase() {
     echo "Executing unittestCheckPhase"
     runHook preCheck
 
+    # shellcheck disable=SC2154
     @pythonCheckInterpreter@ -m unittest discover "${unittestFlagsArray[@]}"
 
     runHook postCheck
     echo "Finished executing unittestCheckPhase"
 }
 
-if [ -z "${dontUseUnittestCheck-}" ] && [ -z "${installCheckPhase-}" ]; then
+if [[ -z "${dontUseUnittestCheck-}" ]] && [[ -z "${installCheckPhase-}" ]]; then
     echo "Using unittestCheckPhase"
     appendToVar preDistPhases unittestCheckPhase
 fi
