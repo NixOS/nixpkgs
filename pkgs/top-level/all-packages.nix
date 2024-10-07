320f3e8b09426ca1c81e3a8190d98b558eafbded
Emily <vcs@emily.moe>
2024-10-07 19:20:56 +0100

    soapui: remove dead code
    The derivation doesn’t support Darwin anyway, and OpenJFX 11 is EOL.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12372,9 +12372,7 @@ with pkgs;
   snort = callPackage ../applications/networking/ids/snort { };
 
   soapui = callPackage ../applications/networking/soapui {
-    jdk = if stdenv.hostPlatform.isDarwin
-      then (jdk11.override { enableJavaFX = true; })
-      else jdk11;
+    jdk = jdk11;
   };
 
   sockdump = callPackage ../tools/networking/sockdump { };
