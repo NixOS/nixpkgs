4751325991dfdd9691f3fa418772854fc6e26e74
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-07 18:56:13 -0300

    yodl: refactor
    - nixfmt-rfc-style
    - finalAttrs
    - strictDeps
    - hammer
    - no nested with
    - more meta attrs

--- pkgs/by-name/yo/yodl/package.nix
+++ pkgs/by-name/yo/yodl/package.nix
@@ -1,52 +1,83 @@
-{ lib, stdenv, fetchFromGitLab, perl, icmake, util-linux }:
+{
+  lib,
+  fetchFromGitLab,
+  icmake,
+  perl,
+  stdenv,
+  util-linux,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "yodl";
   version = "4.03.03";
 
-  nativeBuildInputs = [ icmake ];
-
-  buildInputs = [ perl ];
-
   src = fetchFromGitLab {
-    hash = "sha256-MeD/jjhwoiWTb/G8pHrnEEX22h+entPr9MhJ6WHO3DM=";
-    rev = version;
-    repo = "yodl";
     owner = "fbb-git";
+    repo = "yodl";
+    rev = finalAttrs.version;
+    hash = "sha256-MeD/jjhwoiWTb/G8pHrnEEX22h+entPr9MhJ6WHO3DM=";
   };
 
-  setSourceRoot = ''
-    sourceRoot=$(echo */yodl)
-  '';
+  nativeBuildInputs = [
+    icmake
+    perl
+  ];
 
-  preConfigure = ''
-    patchShebangs ./build
-    patchShebangs scripts/
-    substituteInPlace INSTALL.im --replace /usr $out
-    substituteInPlace macros/rawmacros/startdoc.pl --replace /usr/bin/perl ${perl}/bin/perl
-    substituteInPlace scripts/yodl2whatever.in --replace getopt ${util-linux}/bin/getopt
-  '';
+  sourceRoot = "${finalAttrs.src.name}/yodl";
 
   # Set TERM because icmbuild calls tput.
-  TERM = "xterm";
+  env.TERM = "xterm";
+
+  strictDeps = true;
+
+  postPatch = ''
+    patchShebangs ./build ./scripts/
+    substituteInPlace INSTALL.im \
+      --replace "/usr" "$out"
+    substituteInPlace macros/rawmacros/startdoc.pl \
+      --replace "/usr/bin/perl" "${lib.getExe perl}"
+    substituteInPlace scripts/yodl2whatever.in \
+      --replace "getopt" "${lib.getExe' util-linux "getopt"}"
+  '';
 
   buildPhase = ''
+    runHook preBuild
+
     ./build programs
     ./build macros
     ./build man
+
+    runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
+
     ./build install programs
     ./build install macros
     ./build install man
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
-    description = "Package that implements a pre-document language and tools to process it";
+  meta = {
     homepage = "https://fbb-git.gitlab.io/yodl/";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ pSub ];
-    platforms = platforms.linux;
+    description = "Pre-document language and tools to process it";
+    longDescription = ''
+      Yodl implements a pre-document language and tools to process it. The idea
+      behind Yodl is that you write up a document in a pre-language, then use
+      the tools (e.g. yodl2html(1)) to convert it to some final document
+      language. Current converters are for HTML, man, LaTeX, text and a (very)
+      experimental xml converter. Main document types are "article", "report",
+      "book" "manpage" and "letter". The Yodl document language was designed to
+      be easy to use as well as extensible.
+    '';
+    license = lib.licenses.agpl3Plus;
+    mainProgram = "yodl";
+    maintainers = with lib.maintainers; [
+      pSub
+      AndersonTorres
+    ];
+    platforms = lib.platforms.all;
   };
-}
+})
