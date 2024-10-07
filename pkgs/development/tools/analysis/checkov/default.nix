f3a9e99544aeacfa21b0be0bef22b376363a7638
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-07 09:18:05 +0200

    checkov: 3.2.256 -> 3.2.257
    Diff: https://github.com/bridgecrewio/checkov/compare/refs/tags/3.2.256...3.2.257
    
    Changelog: https://github.com/bridgecrewio/checkov/releases/tag/3.2.257

--- pkgs/development/tools/analysis/checkov/default.nix
+++ pkgs/development/tools/analysis/checkov/default.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "checkov";
-  version = "3.2.256";
+  version = "3.2.257";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bridgecrewio";
     repo = "checkov";
     rev = "refs/tags/${version}";
-    hash = "sha256-y6BhNpbXwna8UVDBaAmxEyjIsCDslpi5sF/gAyX65Xg=";
+    hash = "sha256-1uUKOIxv9p++p6t0O6RXn/cw3Py06mFxoCae0Bj75bU=";
   };
 
   patches = [ ./flake8-compat-5.x.patch ];
