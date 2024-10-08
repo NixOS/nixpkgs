028c25a2aed7536ce4ae664f371cdbb8223d3a2b
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-08 23:22:47 +0200

    python312Packages.archinfo: 9.2.119 -> 9.2.122
    Diff: https://github.com/angr/archinfo/compare/refs/tags/v9.2.119...v9.2.122

--- pkgs/development/python-modules/archinfo/default.nix
+++ pkgs/development/python-modules/archinfo/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "archinfo";
-  version = "9.2.119";
+  version = "9.2.122";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "archinfo";
     rev = "refs/tags/v${version}";
-    hash = "sha256-81vaHTzRCOw8C/KHwFSEMGOWEwpHVK0HicGVi9j4MqU=";
+    hash = "sha256-WmwKpd1c53o/wanvyVPIkXwdwEmKtXvaCa2YIcIsGIU=";
   };
 
   build-system = [ setuptools ];
