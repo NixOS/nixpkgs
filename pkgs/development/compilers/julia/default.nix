ef8b82b3bc409e52e3e10196c6c31c99afcf6c22
Nick Cao <nickcao@nichi.co>
2024-10-08 12:07:04 -0400

    julia_111-bin: init at 1.11.0

--- pkgs/development/compilers/julia/default.nix
+++ pkgs/development/compilers/julia/default.nix
@@ -1,4 +1,4 @@
-{ callPackage }:
+{ callPackage, fetchpatch }:
 
 let
   juliaWithPackages = callPackage ../../julia-modules { };
@@ -39,6 +39,17 @@ in
       };
     })
     { });
+  julia_111-bin = wrapJulia (callPackage
+    (import ./generic-bin.nix {
+      version = "1.11.0";
+      sha256 = {
+        x86_64-linux = "bcf815553fda2ed7910524c8caa189c8e8191a40a799dd8b5fbed0d9dd6b882c";
+        aarch64-linux = "66b9195b4c6b85403834dca9ef4fcae75f15be906bb3bb2c48eccb780ab810a1";
+        x86_64-darwin = "e001d80b53cb9637402633f6cf50cbf2d83d4de03c4093783b8878265996e33d";
+        aarch64-darwin = "c25ae0c87a418555e40167e07524824659656cb60bac9d1e4a5d50d887abe560";
+      };
+    })
+    { });
   julia_19 = wrapJulia (callPackage
     (import ./generic.nix {
       version = "1.9.4";
@@ -54,6 +65,21 @@ in
       hash = "sha256-8y5Sd/XYKmOCSILN6/rBWBmbuEgUw8AZo/7MNgFYYZE=";
       patches = [
         ./patches/1.10/0002-skip-failing-and-flaky-tests.patch
+      ];
+    })
+    { });
+  julia_111 = wrapJulia (callPackage
+    (import ./generic.nix {
+      version = "1.11.0";
+      hash = "sha256-jXd4DNBEhOIfnDgFvmsb1WppvL5srt8kheiZIF2FyHQ=";
+      patches = [
+        ./patches/1.11/0002-skip-failing-and-flaky-tests.patch
+        # [build] avoid libedit linkage and align libccalllazy* SONAMEs
+        # https://github.com/JuliaLang/julia/pull/55968
+        (fetchpatch {
+          url = "https://github.com/JuliaLang/julia/commit/77c5875b3cbe85e7fb0bb5a7e796809c901ede95.patch";
+          hash = "sha256-wDAB3VVI6JfOyyFbAktdAe2Im/HbAciq9yFI4HIpWvY=";
+        })
       ];
     })
     { });
