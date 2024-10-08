5b3eeb1405503f8e11c68a200ccd22880a8cca96
David McFarland <corngood@gmail.com>
2024-10-08 20:01:20 -0300

    dotnet: fix updateScript

--- pkgs/development/compilers/dotnet/build-dotnet.nix
+++ pkgs/development/compilers/dotnet/build-dotnet.nix
@@ -194,10 +194,10 @@ mkCommon type rec {
         let
           majorVersion = lib.concatStringsSep "." (lib.take 2 (lib.splitVersion version));
         in
-        writeShellScript "update-dotnet-${majorVersion}" ''
-          pushd pkgs/development/compilers/dotnet
-          exec ${./update.sh} "${majorVersion}"
-        '';
+        [
+          ./update.sh
+          majorVersion
+        ];
     };
 
   meta = with lib; {
