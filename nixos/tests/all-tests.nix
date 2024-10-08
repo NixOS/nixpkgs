ce3879440d40aedb96a54e5d7517b06d96697a64
Jan van Brügge <supermanitu@gmail.com>
2024-10-08 22:26:49 +0100

    nixos/tests/linkwarden: init tests

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -621,6 +621,7 @@ in {
   lighttpd = handleTest ./lighttpd.nix {};
   limesurvey = handleTest ./limesurvey.nix {};
   limine = import ./limine { inherit runTest; };
+  linkwarden = handleTest ./web-apps/linkwarden.nix {};
   listmonk = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./listmonk.nix {};
   litestream = handleTest ./litestream.nix {};
   lldap = handleTest ./lldap.nix {};
