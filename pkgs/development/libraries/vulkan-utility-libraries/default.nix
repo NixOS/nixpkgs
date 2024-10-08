d08074cd8f7d339883c5a105daf92b4fe79e5560
K900 <me@0upti.me>
2024-10-08 20:06:13 +0300

    vulkan-utility-libraries: 1.3.290.0 -> 1.3.296.0
    Diff: https://github.com/KhronosGroup/Vulkan-Utility-Libraries/compare/vulkan-sdk-1.3.290.0...vulkan-sdk-1.3.296.0

--- pkgs/development/libraries/vulkan-utility-libraries/default.nix
+++ pkgs/development/libraries/vulkan-utility-libraries/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vulkan-utility-libraries";
-  version = "1.3.290.0";
+  version = "1.3.296.0";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "Vulkan-Utility-Libraries";
     rev = "vulkan-sdk-${finalAttrs.version}";
-    hash = "sha256-dzX2xePUkjL4G+IMwPCUgFs1iKsqwZScQQBDt5toUzc=";
+    hash = "sha256-WDRDpUOZN/akUA6gsJMlC2GKolVt3g1NerKqe7aLhek=";
   };
 
   nativeBuildInputs = [ cmake python3 ];
