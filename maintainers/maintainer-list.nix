ad24e1ca55b34851f7e8cb312163e23e2367dc37
Térence Chateigné <terence.chateigne@posteo.net>
2024-10-09 20:36:47 +0200

    maintainers: add cterence

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4606,6 +4606,12 @@
     githubId = 490965;
     name = "Craig Swank";
   };
+  cterence = {
+    email = "terence.chateigne@posteo.net";
+    github = "cterence";
+    githubId = 25285508;
+    name = "Térence Chateigné";
+  };
   ctron = {
     email = "ctron@dentrassi.de";
     github = "ctron";
