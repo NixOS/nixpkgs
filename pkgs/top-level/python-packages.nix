e5e7f97bf813ab6cb7dded2cc4d4c22588e5f573
James ‘Twey’ Kay <twey@twey.co.uk>
2024-10-09 12:57:26 +0100

    python3Packages.plover-last-translation: init at 0.0.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9896,6 +9896,8 @@ self: super: with self; {
     inherit (pkgs.libsForQt5) wrapQtAppsHook;
   };
 
+  plover-last-translation = callPackage ../development/python-modules/plover-last-translation { };
+
   plover-stroke = callPackage ../development/python-modules/plover-stroke { };
 
   micloud = callPackage ../development/python-modules/micloud { };
