eb84415a2c066e82049561efc33ceb319cb192f8
techknowlogick <techknowlogick@gitea.com>
2024-10-08 21:24:51 -0400

    edencommon: fix sha256 to hash

--- pkgs/development/libraries/edencommon/default.nix
+++ pkgs/development/libraries/edencommon/default.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
     owner = "facebookexperimental";
     repo = "edencommon";
     rev = "v${version}";
-    sha256 = "sha256-1z4QicS98juv4bUEbHBkCjVJHEhnoJyLYp4zMHmDbMg=";
+    hash = "sha256-1z4QicS98juv4bUEbHBkCjVJHEhnoJyLYp4zMHmDbMg=";
   };
 
   patches = lib.optionals (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64) [
