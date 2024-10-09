3ce7530c5c43dafd6941dcdfd8a11f869157d712
James ‘Twey’ Kay <twey@twey.co.uk>
2024-10-09 12:58:35 +0100

    python3Packages.plover-modal-dictionary: init at 0.0.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9896,6 +9896,8 @@ self: super: with self; {
     inherit (pkgs.libsForQt5) wrapQtAppsHook;
   };
 
+  plover-modal-dictionary = callPackage ../development/python-modules/plover-modal-dictionary { };
+
   plover-stroke = callPackage ../development/python-modules/plover-stroke { };
 
   micloud = callPackage ../development/python-modules/micloud { };
