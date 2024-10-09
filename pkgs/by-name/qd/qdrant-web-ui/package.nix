296e0dcdc4a18cdc47e2ad9cda88ddec90b4fdce
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-09 04:09:11 +0000

    qdrant-web-ui: 0.1.31 -> 0.1.32

--- pkgs/by-name/qd/qdrant-web-ui/package.nix
+++ pkgs/by-name/qd/qdrant-web-ui/package.nix
@@ -5,16 +5,16 @@
 }:
 buildNpmPackage rec {
   pname = "qdrant-web-ui";
-  version = "0.1.31";
+  version = "0.1.32";
 
   src = fetchFromGitHub {
     owner = "qdrant";
     repo = "qdrant-web-ui";
     rev = "refs/tags/v${version}";
-    hash = "sha256-NNX//0ZkXYMaP14TI4BLMQI4jN+Mxc7uZu4rDZTXHG8=";
+    hash = "sha256-iiaWFOqIf5TzJBfp2gINL8kgqbEoYRCjj0lcFP8rPkQ=";
   };
 
-  npmDepsHash = "sha256-ZCHsVIbtnfrksBmXbYkZLv4WH+DBNazHCOtsgi+PKPc=";
+  npmDepsHash = "sha256-y2zTdRPU0nDCi7CcgyrdFec62qLXR8iXn7//Ys+xb8A=";
 
   npmBuildScript = "build-qdrant";
 
