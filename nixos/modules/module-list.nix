e3b22b0e5341d3b1ed8325226dcef877558b8560
Michael Hoang <enzime@users.noreply.github.com>
2024-10-10 03:58:42 +1100

    nixos: add `system.build.toplevelFor.<system>`

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1596,6 +1596,7 @@
   ./services/x11/xserver.nix
   ./system/activation/activatable-system.nix
   ./system/activation/activation-script.nix
+  ./system/activation/alternate-system.nix
   ./system/activation/specialisation.nix
   ./system/activation/switchable-system.nix
   ./system/activation/bootspec.nix
