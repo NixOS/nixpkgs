1bb74f94b1fb49d5cca539b6c3ff8cf207cad36a
Mihai Fufezan <mihai@fufexan.net>
2024-10-09 11:16:33 +0300

    hyprpicker: 0.3.0 -> 0.4.1

--- pkgs/applications/window-managers/hyprwm/hyprpicker/default.nix
+++ pkgs/applications/window-managers/hyprwm/hyprpicker/default.nix
@@ -2,9 +2,12 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  nix-update-script,
   pkg-config,
   cmake,
   cairo,
+  hyprutils,
+  hyprwayland-scanner,
   libGL,
   libjpeg,
   libxkbcommon,
@@ -17,24 +20,26 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "hyprpicker" + lib.optionalString debug "-debug";
-  version = "0.3.0";
+  version = "0.4.1";
 
   src = fetchFromGitHub {
     owner = "hyprwm";
     repo = "hyprpicker";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-BYQF1zM6bJ44ag9FJ0aTSkhOTY9U7uRdp3SmRCs5fJM=";
+    hash = "sha256-gu26MSYbTlRLMUpZ9PeYXtqqhzPDQXxEDkjiJgwzIIc=";
   };
 
   cmakeBuildType = if debug then "Debug" else "Release";
 
   nativeBuildInputs = [
     cmake
+    hyprwayland-scanner
     pkg-config
   ];
 
   buildInputs = [
     cairo
+    hyprutils
     libGL
     libjpeg
     libxkbcommon
@@ -49,6 +54,8 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir -p $out/share/licenses
     install -Dm644 $src/LICENSE -t $out/share/licenses/hyprpicker
   '';
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Wlroots-compatible Wayland color picker that does not suck";
