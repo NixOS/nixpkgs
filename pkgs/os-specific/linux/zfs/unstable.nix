6997079bb19cc15e8c5089cf1f441df865e8530b
Jörg Thalheim <joerg@thalheim.io>
2024-10-09 07:28:04 +0200

    zfsUnstable: 2.2.6 -> 2.3.0-rc2

--- pkgs/os-specific/linux/zfs/unstable.nix
+++ pkgs/os-specific/linux/zfs/unstable.nix
@@ -14,18 +14,18 @@ callPackage ./generic.nix args {
   # this attribute is the correct one for this package.
   kernelModuleAttribute = "zfs_unstable";
   # check the release notes for compatible kernels
-  kernelCompatible = kernel: kernel.kernelOlder "6.11";
+  kernelCompatible = kernel: kernel.kernelOlder "6.12";
 
   # this package should point to a version / git revision compatible with the latest kernel release
   # IMPORTANT: Always use a tagged release candidate or commits from the
   # zfs-<version>-staging branch, because this is tested by the OpenZFS
   # maintainers.
-  version = "2.2.6";
+  version = "2.3.0-rc2";
   # rev = "";
 
   tests = {
     inherit (nixosTests.zfs) unstable;
   };
 
-  hash = "sha256-wkgoYg6uQOHVq8a9sJXzO/QXJ6q28l7JXWkC+BFvOb0=";
+  hash = "sha256-/INlfiPpRm5JprcORzJJKyjpGIMY1LceY3yZ811uf2A=";
 }
