35da5f98b81b1efaef12818f308c8ecd157f6f79
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-09 08:39:38 +0200

    fixup! python3Packages.dbt-common: 1.8.0 -> 1.10.0

--- pkgs/development/python-modules/dbt-common/default.nix
+++ pkgs/development/python-modules/dbt-common/default.nix
@@ -36,7 +36,10 @@ buildPythonPackage rec {
 
   build-system = [ hatchling ];
 
-  pythonRelaxDeps = [ "agate" ];
+  pythonRelaxDeps = [
+    "agate"
+    "protobuf"
+  ];
 
   dependencies = [
     agate
