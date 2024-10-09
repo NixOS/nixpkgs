b1bf8dfc2889bc9a0a2d52e9ecdcc6c78b997c77
Moraxyc <i@qaq.li>
2024-10-10 00:51:03 +0800

    nezha-agent: 0.18.5 -> 0.20.2

--- pkgs/by-name/ne/nezha-agent/package.nix
+++ pkgs/by-name/ne/nezha-agent/package.nix
@@ -1,45 +1,66 @@
 {
   lib,
   buildGoModule,
+  stdenv,
   fetchFromGitHub,
   nezha-agent,
+  versionCheckHook,
   testers,
 }:
 buildGoModule rec {
   pname = "nezha-agent";
-  version = "0.18.5";
+  version = "0.20.2";
 
   src = fetchFromGitHub {
     owner = "nezhahq";
     repo = "agent";
     rev = "refs/tags/v${version}";
-    hash = "sha256-LmWfs3aL+1lsX4ix2FjDP5g+A0wgcfziXdw5SaKlAdk=";
+    hash = "sha256-BM3FhCf9zfccC2xC/Fhz2/andZmPYsJojMRUA3M9NOQ=";
   };
 
-  vendorHash = "sha256-frPAhiexFSt+KobMbf32h8xv7HMcPl5koEgSs8Nz3cs=";
+  vendorHash = "sha256-q6/265vVg6jCnDvs825nni8QFHkJpQz4xxC9MlJH2do=";
 
   ldflags = [
     "-s"
     "-w"
     "-X main.version=${version}"
+    "-X main.arch=${stdenv.hostPlatform.system}"
   ];
 
-  # The test failed due to a geoip request in the sandbox. Remove it to avoid network requirement
-  preCheck = ''
-    rm ./pkg/monitor/myip_test.go
+  checkFlags =
+    let
+      # Skip tests that require network access
+      skippedTests = [
+        "TestLookupIP"
+        "TestGeoIPApi"
+        "TestFetchGeoIP"
+        "TestCloudflareDetection"
+      ];
+    in
+    [ "-skip=^${builtins.concatStringsSep "$|^" skippedTests}$" ];
+
+  postInstall = ''
+    pushd $out/bin
+    mv agent nezha-agent
+
+    # for compatibility
+    ln -sr nezha-agent agent
+    popd
   '';
 
-  passthru.tests = {
-    version = testers.testVersion {
-      package = nezha-agent;
-      command = "${nezha-agent}/bin/agent -v";
-    };
-  };
+  doInstallCheck = true;
+
+  versionCheckProgramArg = "-v";
+
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
 
   meta = {
     description = "Agent of Nezha Monitoring";
     homepage = "https://github.com/nezhahq/agent";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ moraxyc ];
+    mainProgram = "nezha-agent";
   };
 }
