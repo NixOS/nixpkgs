f599c8fb734ec6658173c4e41fd1ca51f19e22f8
Tomo <tomodachi94@protonmail.com>
2024-10-09 18:30:49 +0000

    python3Packages.django-mdeditor: patch out polyfill.io usage
    Part of #323379

--- pkgs/development/python-modules/django-mdeditor/default.nix
+++ pkgs/development/python-modules/django-mdeditor/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   django,
 }:
 let
@@ -17,6 +18,14 @@ buildPythonPackage {
     rev = "v${version}";
     hash = "sha256-t57j1HhjNQtBwlbqe4mAHQ9WiNcIhMKYmrZkiqh+k5k=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "Replace-bootcdn-with-jsdelivr.patch";
+      url = "https://github.com/pylixm/django-mdeditor/pull/184/commits/43c2ae8024ce359e34009faaf813568be8fbbdad.patch";
+      hash = "sha256-9ThEDHOrXqZrupo7CKEYiDILgunqz5fXH37olanJo6A=";
+    })
+  ];
 
   propagatedBuildInputs = [ django ];
 
