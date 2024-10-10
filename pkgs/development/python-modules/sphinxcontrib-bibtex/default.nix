57866aed7b4c3a4a22f7d13b4a1627ef202990bf
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-10 18:26:57 +0200

    fixup! python3Packages.sphinxcontrib-bibtex: 2.6.2 -> 2.6.3

--- pkgs/development/python-modules/sphinxcontrib-bibtex/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-bibtex/default.nix
@@ -1,34 +1,49 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   pythonOlder,
+  setuptools,
+  docutils,
+  importlib-metadata,
   oset,
   pybtex,
   pybtex-docutils,
   sphinx,
+  sphinx-autoapi,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "sphinxcontrib-bibtex";
   version = "2.6.3";
-  format = "setuptools";
+  pyproject = true;
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-fHkDR+8csO3zDeVfwyTZeC0IXonFLCuPqvoILgjiOUY=";
+  src = fetchFromGitHub {
+    owner = "mcmtroffaes";
+    repo = "sphinxcontrib-bibtex";
+    rev = "refs/tags/${version}";
+    hash = "sha256-cqz5Jamtlflo5rFhWPCPlYoymApUtXPG4oTRjfDI+WY=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
+    docutils
     oset
     pybtex
     pybtex-docutils
     sphinx
+  ] ++ lib.optionals (pythonOlder "3.10") [
+    importlib-metadata
   ];
 
-  doCheck = false;
+  nativeCheckInputs = [
+    pytestCheckHook
+    sphinx-autoapi
+  ];
 
   pythonImportsCheck = [ "sphinxcontrib.bibtex" ];
 
