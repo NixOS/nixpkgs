f564a467097b37ef39802763e2b7ae87f3afe5a2
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-10 13:47:11 -0300

    nix-prefetch-scripts: nixfmt-rfc-style

--- pkgs/tools/package-management/nix-prefetch-scripts/default.nix
+++ pkgs/tools/package-management/nix-prefetch-scripts/default.nix
@@ -1,46 +1,84 @@
-{ lib, stdenv, makeWrapper, buildEnv
-, breezy, coreutils, cvs, findutils, gawk, git, git-lfs, gnused, mercurial, nix, subversion
+{
+  lib,
+  breezy,
+  buildEnv,
+  coreutils,
+  cvs,
+  findutils,
+  gawk,
+  git,
+  git-lfs,
+  gnused,
+  makeWrapper,
+  mercurial,
+  nix,
+  stdenv,
+  subversion,
 }:
 
-let mkPrefetchScript = tool: src: deps:
-  stdenv.mkDerivation {
-    name = "nix-prefetch-${tool}";
+let
+  mkPrefetchScript =
+    tool: src: deps:
+    stdenv.mkDerivation {
+      pname = "nix-prefetch-${tool}";
+      version = "0";
 
-    nativeBuildInputs = [ makeWrapper ];
+      nativeBuildInputs = [ makeWrapper ];
 
-    dontUnpack = true;
+      dontUnpack = true;
 
-    installPhase = ''
-      install -vD ${src} $out/bin/$name;
-      wrapProgram $out/bin/$name \
-        --prefix PATH : ${lib.makeBinPath (deps ++ [ gnused nix ])} \
-        --set HOME /homeless-shelter
-    '';
+      preferLocalBuild = true;
 
-    preferLocalBuild = true;
+      installPhase = ''
+        install -vD ${src} $out/bin/$name;
+        wrapProgram $out/bin/$name \
+          --prefix PATH : ${
+            lib.makeBinPath (
+              deps
+              ++ [
+                gnused
+                nix
+              ]
+            )
+          } \
+          --set HOME /homeless-shelter
+      '';
 
-    meta = with lib; {
-      description = "Script used to obtain source hashes for fetch${tool}";
-      maintainers = with maintainers; [ bennofs ];
-      platforms = platforms.unix;
+      meta = {
+        description = "Script used to obtain source hashes for fetch${tool}";
+        maintainers = with lib.maintainers; [ bennofs ];
+        platforms = lib.platforms.unix;
+      };
     };
-  };
-in rec {
+in
+rec {
   nix-prefetch-bzr = mkPrefetchScript "bzr" ./scripts/nix-prefetch-bzr [ breezy ];
   nix-prefetch-cvs = mkPrefetchScript "cvs" ./scripts/nix-prefetch-cvs [ cvs ];
-  nix-prefetch-git = mkPrefetchScript "git" ./scripts/nix-prefetch-git [ coreutils findutils gawk git git-lfs ];
-  nix-prefetch-hg  = mkPrefetchScript "hg"  ./scripts/nix-prefetch-hg [ mercurial ];
+  nix-prefetch-git = mkPrefetchScript "git" ./scripts/nix-prefetch-git [
+    coreutils
+    findutils
+    gawk
+    git
+    git-lfs
+  ];
+  nix-prefetch-hg = mkPrefetchScript "hg" ./scripts/nix-prefetch-hg [ mercurial ];
   nix-prefetch-svn = mkPrefetchScript "svn" ./scripts/nix-prefetch-svn [ subversion ];
 
   nix-prefetch-scripts = buildEnv {
     name = "nix-prefetch-scripts";
 
-    paths = [ nix-prefetch-bzr nix-prefetch-cvs nix-prefetch-git nix-prefetch-hg nix-prefetch-svn ];
+    paths = [
+      nix-prefetch-bzr
+      nix-prefetch-cvs
+      nix-prefetch-git
+      nix-prefetch-hg
+      nix-prefetch-svn
+    ];
 
-    meta = with lib; {
+    meta = {
       description = "Collection of all the nix-prefetch-* scripts which may be used to obtain source hashes";
-      maintainers = with maintainers; [ bennofs ];
-      platforms = platforms.unix;
+      maintainers = with lib.maintainers; [ bennofs ];
+      platforms = lib.platforms.unix;
     };
   };
 }
