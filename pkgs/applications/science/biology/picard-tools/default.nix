97169fdc5c80bbed76003cefc634ccf1211490fe
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-10 04:19:53 +0000

    picard-tools: 3.2.0 -> 3.3.0

--- pkgs/applications/science/biology/picard-tools/default.nix
+++ pkgs/applications/science/biology/picard-tools/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "picard-tools";
-  version = "3.2.0";
+  version = "3.3.0";
 
   src = fetchurl {
     url = "https://github.com/broadinstitute/picard/releases/download/${version}/picard.jar";
-    sha256 = "sha256-4lj6Lj9fphzWeZ7I4HP0mpX73OrxY9imCXGyYZuG+kE=";
+    sha256 = "sha256-WIGadmBka3SzTigvXU0hyNuuoi3e/5bjJYdV2voPhtw=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
