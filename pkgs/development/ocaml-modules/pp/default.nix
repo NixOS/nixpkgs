627472d48925ba6ab181e64ee6ef225521f38a78
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-10-10 21:19:25 +0200

    ocamlPackages.pp: 1.2.0 → 2.0.0

--- pkgs/development/ocaml-modules/pp/default.nix
+++ pkgs/development/ocaml-modules/pp/default.nix
@@ -1,16 +1,23 @@
-{ buildDunePackage, fetchurl, ppx_expect, lib }:
+{ lib
+, buildDunePackage
+, fetchurl
+, ppx_expect
+, version ? "2.0.0"
+}:
 
 buildDunePackage rec {
   pname = "pp";
-  version = "1.2.0";
+  inherit version;
 
   src = fetchurl {
     url =
       "https://github.com/ocaml-dune/pp/releases/download/${version}/pp-${version}.tbz";
-    hash = "sha256-pegiVzxVr7Qtsp7FbqzR8qzY9lzy3yh44pHeN0zmkJw=";
+    hash = {
+      "2.0.0" = "sha256-hlE1FRiwkrSi3vTggXHCdhUvkvtqhKixm2uSnM20RBk=";
+      "1.2.0" = "sha256-pegiVzxVr7Qtsp7FbqzR8qzY9lzy3yh44pHeN0zmkJw=";
+    }."${version}";
   };
 
-  duneVersion = "3";
   minimalOCamlVersion = "4.08";
 
   checkInputs = [ ppx_expect ];
