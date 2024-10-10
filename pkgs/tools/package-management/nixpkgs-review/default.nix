0b5bb8e25392bf3ed07772599d2aa4ae7d8437e2
Gaetan Lepage <gaetan@glepage.com>
2024-10-10 13:31:50 +0200

    nixpkgs-review: 2.11.0 -> 2.12.0
    Diff: https://github.com/Mic92/nixpkgs-review/compare/refs/tags/2.11.0...2.12.0
    
    Changelog: https://github.com/Mic92/nixpkgs-review/releases/tag/2.12.0

--- pkgs/tools/package-management/nixpkgs-review/default.nix
+++ pkgs/tools/package-management/nixpkgs-review/default.nix
@@ -18,14 +18,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "nixpkgs-review";
-  version = "2.11.0";
+  version = "2.12.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = "nixpkgs-review";
     rev = "refs/tags/${version}";
-    hash = "sha256-QK9VTMh31aaY+DXHKVzSETMyzCD4jVw3d4ntBnmlkO8=";
+    hash = "sha256-yNdBqL3tceuoUHx8/j2y5ZTq1zeVDAm37RZtlCbC6rg=";
   };
 
   build-system = [
