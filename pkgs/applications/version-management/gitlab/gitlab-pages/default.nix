deb4dbacfc8510157aef8f321dd9bb10a7892a28
Sarah Brofeldt <sarah@qtr.dk>
2024-10-10 08:31:29 +0200

    gitlab: 17.2.8 -> 17.2.9
    Fixes CVE-2024-9164
    Fixes CVE-2024-8970
    Fixes CVE-2024-8977
    Fixes CVE-2024-9631
    Fixes CVE-2024-6530
    Fixes CVE-2024-9623
    Fixes CVE-2024-5005
    Fixes CVE-2024-9596
    
    (cherry picked from commit 05819f55dad295eaa0ebc6efb3c95a8c48ef3c51)

--- pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   pname = "gitlab-pages";
-  version = "17.2.8";
+  version = "17.2.9";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-pages";
     rev = "v${version}";
-    hash = "sha256-NaLRCLmgDI4ArJyntkmYICVEpwZtAU6XVTmXAh4kOSc=";
+    hash = "sha256-ggfPZ/PMjgfRzzPZdVCVydd/4mdCfh2xh7QWGjp5U/0=";
   };
 
   vendorHash = "sha256-yNHeM8MExcLwv2Ga4vtBmPFBt/Rj7Gd4QQYDlnAIo+c=";
