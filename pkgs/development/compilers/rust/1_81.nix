4d76556fcafec46b134296418cca595abf8c40ef
Julius Michaelis <gitter@liftm.de>
2024-10-10 11:00:30 +0900

    rustc: do not use clang-built llvm on wasi, as it doesn't build and is unnecessary

--- pkgs/development/compilers/rust/1_81.nix
+++ pkgs/development/compilers/rust/1_81.nix
@@ -32,18 +32,21 @@
 let
   llvmSharedFor =
     pkgSet:
-    pkgSet.llvmPackages_18.libllvm.override (
-      {
-        enableSharedLibraries = true;
-      }
-      // lib.optionalAttrs (stdenv.targetPlatform.useLLVM or false) {
-        # Force LLVM to compile using clang + LLVM libs when targeting pkgsLLVM
-        stdenv = pkgSet.stdenv.override {
-          allowedRequisites = null;
-          cc = pkgSet.llvmPackages_18.clangUseLLVM;
-        };
-      }
-    );
+    if !stdenv.targetPlatform.isWasi then
+      pkgSet.llvmPackages_18.libllvm.override (
+        {
+          enableSharedLibraries = true;
+        }
+        // lib.optionalAttrs (stdenv.targetPlatform.useLLVM or false) {
+          # Force LLVM to compile using clang + LLVM libs when targeting pkgsLLVM
+          stdenv = pkgSet.stdenv.override {
+            allowedRequisites = null;
+            cc = pkgSet.llvmPackages_18.clangUseLLVM;
+          };
+        }
+      )
+    else
+      pkgSet.llvmPackages_18.libllvm;
 in
 import ./default.nix
   {
@@ -59,7 +62,7 @@ import ./default.nix
 
     # Expose llvmPackages used for rustc from rustc via passthru for LTO in Firefox
     llvmPackages =
-      if (stdenv.targetPlatform.useLLVM or false) then
+      if ((stdenv.targetPlatform.useLLVM or false) && !stdenv.targetPlatform.isWasi) then
         callPackage (
           {
             pkgs,
