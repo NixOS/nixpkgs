0405917191c0695f0f63246750af5950c8189cce
Alyssa Ross <hi@alyssa.is>
2024-10-10 13:16:50 +0200

    webrtc-audio-processing_1: use upstream's OS list
    This will allow disabling optional webrtc-audio-processing
    functionality on e.g. FreeBSD.

--- pkgs/development/libraries/webrtc-audio-processing/default.nix
+++ pkgs/development/libraries/webrtc-audio-processing/default.nix
@@ -53,8 +53,11 @@ stdenv.mkDerivation rec {
     homepage = "https://www.freedesktop.org/software/pulseaudio/webrtc-audio-processing";
     description = "More Linux packaging friendly copy of the AudioProcessing module from the WebRTC project";
     license = licenses.bsd3;
-    # https://gitlab.freedesktop.org/pulseaudio/webrtc-audio-processing/-/blob/master/webrtc/rtc_base/system/arch.h
-    platforms = intersectLists platforms.unix (platforms.arm ++ platforms.aarch64 ++ platforms.mips ++ platforms.power ++ platforms.riscv ++ platforms.x86);
+    platforms = intersectLists
+      # https://gitlab.freedesktop.org/pulseaudio/webrtc-audio-processing/-/blob/master/meson.build
+      (platforms.darwin ++ platforms.linux ++ platforms.windows)
+      # https://gitlab.freedesktop.org/pulseaudio/webrtc-audio-processing/-/blob/master/webrtc/rtc_base/system/arch.h
+      (platforms.arm ++ platforms.aarch64 ++ platforms.mips ++ platforms.power ++ platforms.riscv ++ platforms.x86);
     # BE platforms are unsupported
     # https://gitlab.freedesktop.org/pulseaudio/webrtc-audio-processing/-/issues/31
     badPlatforms = platforms.bigEndian;
