3c54a259b07ae4f5ad9b39ce669c287ed8010085
Florent Ulvé <florent.ulve@protonmail.com>
2024-10-10 20:50:16 +0200

    nixfmt

--- pkgs/applications/virtualization/podlet/default.nix
+++ pkgs/applications/virtualization/podlet/default.nix
@@ -14,7 +14,8 @@ rustPlatform.buildRustPackage rec {
   cargoHash = "sha256-0V2DTpEjDgo+NQHw1d01g24zi6u1PxS4jB8LPfC6MP0=";
 
   meta = {
-    description = "Podlet generates Podman Quadlet files from a Podman command, compose file, or existing object.";
+    description =
+      "Podlet generates Podman Quadlet files from a Podman command, compose file, or existing object.";
     homepage = "https://github.com/containers/podlet";
     license = lib.licenses.mpl20;
     maintainers = [ ];
