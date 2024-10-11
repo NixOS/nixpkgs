86be0d9b50701053527539a10e1ffaa0fcacbf27
Alice Carroll <git@alice-carroll.pet>
2024-10-11 18:34:13 +0300

    pacparser: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3725,8 +3725,6 @@ with pkgs;
 
   mobilecoin-wallet = callPackage ../applications/misc/mobilecoin-wallet { };
 
-  pacparser = callPackage ../tools/networking/pacparser { };
-
   pairdrop = callPackage ../applications/misc/pairdrop { };
 
   opencbm = callPackage ../tools/misc/opencbm { };
