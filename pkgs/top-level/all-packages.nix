58a132db394d1f54aaa5e99a5bec9388bda2a37c
Wulfsta <wulfstawulfsta@gmail.com>
2024-10-11 17:38:02 -0400

    ao: move to aliases

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28105,8 +28105,6 @@ with pkgs;
 
   anytone-emu = callPackage ../applications/radio/anytone-emu { };
 
-  ao = libfive;
-
   apache-directory-studio = callPackage ../applications/networking/apache-directory-studio { };
 
   apkeep = callPackage ../tools/misc/apkeep {
