d00e2d4ac2b2bcbcf1321550e3dc9cfd907677ea
Gaetan Lepage <gaetan@glepage.com>
2024-10-11 08:55:11 +0200

    nvrh: 0.1.8 -> 0.1.9
    Diff: https://github.com/mikew/nvrh/compare/refs/tags/v0.1.8...v0.1.9
    
    Changelog: https://github.com/mikew/nvrh/blob/refs/tags/v0.1.9/CHANGELOG.md

--- pkgs/by-name/nv/nvrh/package.nix
+++ pkgs/by-name/nv/nvrh/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "nvrh";
-  version = "0.1.8";
+  version = "0.1.9";
 
   src = fetchFromGitHub {
     owner = "mikew";
     repo = "nvrh";
     rev = "refs/tags/v${version}";
-    hash = "sha256-a/TFSS4PeZWEYph4B8qDr4BJPY4CnHafvw07t1ytofo=";
+    hash = "sha256-QM8VVvNTPvu6Yg1G8FW/694RyTbw36AqkvISeP70gpE=";
   };
 
   postPatch = ''
