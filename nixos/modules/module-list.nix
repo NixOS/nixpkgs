93af2d0523f4b9eb77e75208cdde4205110d2997
oddlama <oddlama@oddlama.org>
2024-10-11 03:06:31 +0200

    nixos/actual: init module and tests
    Co-authored-by: PatrickDaG <58092422+PatrickDaG@users.noreply.github.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1413,6 +1413,7 @@
   ./services/video/wivrn.nix
   ./services/wayland/cage.nix
   ./services/wayland/hypridle.nix
+  ./services/web-apps/actual.nix
   ./services/web-apps/akkoma.nix
   ./services/web-apps/agorakit.nix
   ./services/web-apps/alps.nix
