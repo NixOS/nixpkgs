4ccced1cd743a0d5818b30e5e3206823b0e50ee5
Randy Eckenrode <randy@largeandhighquality.com>
2024-10-11 17:52:53 -0400

    rcodesign: 0.22.0 -> 0.27.0
    https://github.com/indygreg/apple-platform-rs/releases/tag/apple-codesign%2F0.23.0
    https://github.com/indygreg/apple-platform-rs/releases/tag/apple-codesign%2F0.24.0
    https://github.com/indygreg/apple-platform-rs/releases/tag/apple-codesign%2F0.25.0
    https://github.com/indygreg/apple-platform-rs/releases/tag/apple-codesign%2F0.26.0
    https://github.com/indygreg/apple-platform-rs/releases/tag/apple-codesign%2F0.27.0

--- pkgs/development/tools/rcodesign/default.nix
+++ pkgs/development/tools/rcodesign/default.nix
@@ -7,20 +7,25 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rcodesign";
-  version = "0.22.0";
+  version = "0.27.0";
 
   src = fetchFromGitHub {
     owner = "indygreg";
     repo = "apple-platform-rs";
     rev = "apple-codesign/${version}";
-    hash = "sha256-ndbDBGtTOfHHUquKrETe4a+hB5Za9samlnXwVGVvWy4=";
+    hash = "sha256-F6Etl3Zbpmh3A/VeCcSXIy3W1WYFg8WUSJBJV/akCxU=";
   };
-
-  cargoHash = "sha256-cpQBdxTw/ge4VtzjdL2a2xgSeCT22fMIjuKu5UEedhI=";
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
     darwin.apple_sdk_11_0.frameworks.Security
   ];
+  cargoPatches = [
+    # Update time to a version that is compatible with Rust 1.80
+    ./update-time-rs-in-cargo-lock.patch
+  ];
+
+  cargoHash = "sha256-VrexypkCW58asvzXo3wj/Rgi72tiGuchA31BkEZoYpI=";
+
 
   cargoBuildFlags = [
     # Only build the binary we want
