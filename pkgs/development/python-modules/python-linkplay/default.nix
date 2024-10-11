f39865b6fc0b145b7d18a6d666fecdc6c27bb0f2
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-11 20:56:23 +0200

    python312Packages.python-linkplay: 0.0.14 -> 0.0.15
    https://github.com/Velleman/python-linkplay/releases/tag/v0.0.15

--- pkgs/development/python-modules/python-linkplay/default.nix
+++ pkgs/development/python-modules/python-linkplay/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "python-linkplay";
-  version = "0.0.14";
+  version = "0.0.15";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Velleman";
     repo = "python-linkplay";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Sj22bbSJoD52WQBWzdFW36WFQcfvPadpd6mv84vN73U=";
+    hash = "sha256-PDkDZchAXxrmjg7G/dbbUyZUS8dNrOppH96mLLdwK1s=";
   };
 
   build-system = [ setuptools ];
