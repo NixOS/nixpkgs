7ac7bd10e8c58a3fb96988c03f56b873222fbd00
Damien Cassou <damien@cassou.me>
2024-10-12 15:51:59 +0200

    eslint_d: 14.0.3 -> 14.1.1

--- pkgs/development/tools/eslint_d/default.nix
+++ pkgs/development/tools/eslint_d/default.nix
@@ -2,16 +2,16 @@
 
 buildNpmPackage rec {
   pname = "eslint_d";
-  version = "14.0.3";
+  version = "14.1.1";
 
   src = fetchFromGitHub {
     owner = "mantoni";
     repo = "eslint_d.js";
     rev = "v${version}";
-    hash = "sha256-r0pb9qbWfyVUHuHrNhiYm+0zlF5WId3dH7QCubzZDts=";
+    hash = "sha256-r+AQFFzB3PhvER6oVHpqQiFuaHuT+2O8gL2zu8aCTbs=";
   };
 
-  npmDepsHash = "sha256-0Db18y7MUnnnr8v+bBOUhGBCsZcZ9OGtGqSVH7/wYQc=";
+  npmDepsHash = "sha256-XOFRzGPrisXE8GyqVO5xms+o9OwA9w0y+uJkcdyX+z0=";
 
   dontNpmBuild = true;
 
