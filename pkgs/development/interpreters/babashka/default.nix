0b07f08a032fe30e7d444bb846a9d531cc250e51
José Luis Lafuente <jl@lafuente.dev>
2024-10-12 12:51:06 +0200

    babashka: 1.4.192 -> 1.12.194

--- pkgs/development/interpreters/babashka/default.nix
+++ pkgs/development/interpreters/babashka/default.nix
@@ -9,11 +9,11 @@
 let
   babashka-unwrapped = buildGraalvmNativeImage rec {
     pname = "babashka-unwrapped";
-    version = "1.4.192";
+    version = "1.12.194";
 
     src = fetchurl {
       url = "https://github.com/babashka/babashka/releases/download/v${version}/babashka-${version}-standalone.jar";
-      sha256 = "sha256-uVRk1/lfOXMSp3qOdeMoAFpwJaEBxF1MYtFOv364H7Y=";
+      sha256 = "sha256-Dx3AANMpmCPH8Zaz8/9FRNRRhcEPnZHXQje47udwwRQ=";
     };
 
     graalvmDrv = graalvmCEPackages.graalvm-ce;
@@ -37,6 +37,7 @@ let
       $out/bin/bb '(vec (dedupe *input*))' <<< '[1 1 1 1 2]' | fgrep '[1 2]'
       $out/bin/bb '(prn "bépo àê")' | fgrep 'bépo àê'
       $out/bin/bb '(:out (babashka.process/sh "echo" "ä"))' | fgrep 'ä'
+      $out/bin/bb '(into-array [:f])'
     '';
 
     postInstall = ''
