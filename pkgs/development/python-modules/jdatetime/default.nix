a9bf4969fc46f4aa3f0af477b34cb75e6dacd360
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-12 10:23:02 +0200

    python312Packages.jdatetime: refactor

--- pkgs/development/python-modules/jdatetime/default.nix
+++ pkgs/development/python-modules/jdatetime/default.nix
@@ -2,29 +2,33 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  six,
+  jalali-core,
   pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "jdatetime";
   version = "5.0.0";
-  format = "setuptools";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-LMYD2RPA2OMokoRU09KVJhywN+mVAif2fJYpq0cQ/fk=";
   };
 
-  propagatedBuildInputs = [ six ];
+  build-system = [ setuptools ];
+
+  dependencies = [ jalali-core ];
 
   pythonImportsCheck = [ "jdatetime" ];
 
   meta = with lib; {
     description = "Jalali datetime binding";
     homepage = "https://github.com/slashmili/python-jalali";
+    changelog = "https://github.com/slashmili/python-jalali/blob/v${version}/CHANGELOG.md";
     license = licenses.psfl;
     maintainers = [ ];
   };
