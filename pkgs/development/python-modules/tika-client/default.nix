1ce2564d2a4504aba953a00bebad49c2ba870401
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-12 09:19:07 +0200

    python312Packages.tika-client: refactor

--- pkgs/development/python-modules/tika-client/default.nix
+++ pkgs/development/python-modules/tika-client/default.nix
@@ -6,10 +6,11 @@
   hatchling,
   httpx,
 }:
+
 buildPythonPackage rec {
   pname = "tika-client";
   version = "0.7.0";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.8";
 
@@ -20,11 +21,12 @@ buildPythonPackage rec {
     hash = "sha256-0cv2HaquIUQOb5CPkCxSYvXDzu3OV7WKIT80jI+pjpY=";
   };
 
-  propagatedBuildInputs = [
-    hatchling
-    httpx
-  ];
+  build-system = [ hatchling ];
+
+  dependencies = [ httpx ];
+
   pythonImportsCheck = [ "tika_client" ];
+
   # Almost all of the tests (all except one in 0.1.0) fail since there
   # is no tika http API endpoint reachable. Since tika is not yet
   # packaged for nixpkgs, it seems like an unreasonable amount of effort
