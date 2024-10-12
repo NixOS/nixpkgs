d4c69733f54b496554331b0e29106cea475b6105
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-10-12 21:20:01 +0200

    satisfactorymodmanager: init at 3.0.0

---
+++ pkgs/by-name/sa/satisfactorymodmanager/generated/i18n/index.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+import de from './de.json';
+import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
+import hu from './hu.json';
+import it from './it.json';
+import ja from './ja.json';
+import ko from './ko.json';
+import mt from './mt.json';
+import pl from './pl.json';
+import pt_BR from './pt-BR.json';
+import ru from './ru.json';
+import tr from './tr.json';
+import zh_Hans from './zh-Hans.json';
+import zh_Hant from './zh-Hant.json';
+
+export const i18n = {
+  "de": de,
+  "en": en,
+  "es": es,
+  "fr": fr,
+  "hu": hu,
+  "it": it,
+  "ja": ja,
+  "ko": ko,
+  "mt": mt,
+  "pl": pl,
+  "pt-BR": pt_BR,
+  "ru": ru,
+  "tr": tr,
+  "zh-Hans": zh_Hans,
+  "zh-Hant": zh_Hant,
+};
