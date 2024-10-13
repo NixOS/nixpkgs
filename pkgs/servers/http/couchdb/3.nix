b254639ae875489aa8e8cd56d8aba4e2fea9e7be
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-13 10:11:20 +0000

    couchdb3: 3.3.3 -> 3.4.1

--- pkgs/servers/http/couchdb/3.nix
+++ pkgs/servers/http/couchdb/3.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation rec {
   pname = "couchdb";
-  version = "3.3.3";
+  version = "3.4.1";
 
   src = fetchurl {
     url = "mirror://apache/couchdb/source/${version}/apache-${pname}-${version}.tar.gz";
-    hash = "sha256-eiAHtfZz1L4iolyaER2QZpGdhy3bkTWn3OwBIimb054=";
+    hash = "sha256-qqzqG9Zs9kH9gZjc5mKjN7NZtp2P1HN+Ow4wa1ScP+U=";
   };
 
   postPatch = ''
