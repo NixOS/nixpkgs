dcfa03a32a853a20282934b8c1ff50c8559635b3
K900 <me@0upti.me>
2024-10-13 21:28:47 +0300

    home-assistant-custom-components.smartthinq-sensors: 0.40.0 -> 0.40.1
    Diff: https://github.com/ollo69/ha-smartthinq-sensors/compare/v0.40.0...v0.40.1
    
    Changelog: https://github.com/ollo69/ha-smartthinq-sensors/releases/tag/v0.40.1

--- pkgs/servers/home-assistant/custom-components/smartthinq-sensors/default.nix
+++ pkgs/servers/home-assistant/custom-components/smartthinq-sensors/default.nix
@@ -9,13 +9,13 @@
 buildHomeAssistantComponent rec {
   owner = "ollo69";
   domain = "smartthinq_sensors";
-  version = "0.40.0";
+  version = "0.40.1";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "ha-smartthinq-sensors";
     rev = "v${version}";
-    hash = "sha256-2uahTfRmhEqcazz6SzG2abR0e15POsIe/gKs62AITB8=";
+    hash = "sha256-15s4xQKNsjrhae8Cp6U0Ay3s/o8L9BjLvc+1Tu+ss1s=";
   };
 
   propagatedBuildInputs = [
