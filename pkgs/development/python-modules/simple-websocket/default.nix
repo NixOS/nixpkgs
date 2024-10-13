f1e8d2b37f3df1be97defdb22aaf31aef43936ea
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-13 23:45:52 +0200

    python312Packages.simple-websocket: 1.0.0 -> 1.1.0
    Diff: https://github.com/miguelgrinberg/simple-websocket/compare/refs/tags/v1.0.0...v1.1.0
    
    Changelog: https://github.com/miguelgrinberg/simple-websocket/blob/1.1.0/CHANGES.md

--- pkgs/development/python-modules/simple-websocket/default.nix
+++ pkgs/development/python-modules/simple-websocket/default.nix
@@ -3,33 +3,37 @@
   buildPythonPackage,
   fetchFromGitHub,
   pytestCheckHook,
+  pythonOlder,
   setuptools,
-  wheel,
   wsproto,
 }:
 
 buildPythonPackage rec {
   pname = "simple-websocket";
-  version = "1.0.0";
-  format = "pyproject";
+  version = "1.1.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "miguelgrinberg";
     repo = "simple-websocket";
     rev = "refs/tags/v${version}";
-    hash = "sha256-5dUZnbjHzH1sQ93CbFdEoW9j2zY4Z+8wNsYfmOrgC8E=";
+    hash = "sha256-dwL6GUyygNGBXqkkTnsHwFFpa1JAaeWc9ycQNRgTN4I=";
   };
 
-  nativeBuildInputs = [
-    setuptools
-    wheel
-  ];
+  build-system = [ setuptools ];
 
-  propagatedBuildInputs = [ wsproto ];
+  dependencies = [ wsproto ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "simple_websocket" ];
+
+  disabledTests = [
+    # Tests require network access
+    "SimpleWebSocketClientTestCase"
+  ];
 
   meta = with lib; {
     description = "Simple WebSocket server and client for Python";
