65018ee3fb925eae1e0f7c16f8416ab71b8c0e88
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-13 09:48:07 +0200

    python312Packages.extract-msg: 0.50.1 -> 0.51.1
    Diff: https://github.com/TeamMsgExtractor/msg-extractor/compare/refs/tags/v0.50.1...v0.51.1
    
    Changelog: https://github.com/TeamMsgExtractor/msg-extractor/blob/0.51.1/CHANGELOG.md

--- pkgs/development/python-modules/extract-msg/default.nix
+++ pkgs/development/python-modules/extract-msg/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "extract-msg";
-  version = "0.50.1";
+  version = "0.51.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "TeamMsgExtractor";
     repo = "msg-extractor";
     rev = "refs/tags/v${version}";
-    hash = "sha256-MWqJYdv+P87n5KtrtSJnyY59uuLtHL9EG8WyYRdfSTw=";
+    hash = "sha256-x+qMhIjB7Z8ceBwCC6wqA0BW9PeUYhY3UFSra67K+8k=";
   };
 
   pythonRelaxDeps = [
