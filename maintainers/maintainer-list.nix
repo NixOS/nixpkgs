b3219e51d8fea3955139a3317403731ba8c78fd5
Scott Bonds <scott@ggr.com>
2024-10-13 10:44:06 -0700

    maintainers: add bonds

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2962,6 +2962,12 @@
     githubId = 150560585;
     name = "Dmitry Ivankov";
   };
+  bonds = {
+    email = "scott@ggr.com";
+    github = "bonds";
+    githubId = 53434;
+    name = "Scott Bonds";
+  };
   bonsairobo = {
     email = "duncanfairbanks6@gmail.com";
     github = "bonsairobo";
