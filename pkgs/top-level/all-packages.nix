faa0c49593922bb7f61068f74db5123388a8b3b5
Oliver Schmidt <os@flyingcircus.io>
2024-10-13 22:59:30 +0200

    percona: don't bump LTS name
    In the stable branch, the `_lts` package names for percona-server and
    percona-xtrabackup remain at the still suported 8_0 packages to avoid
    major version bumps during a release cycle.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11603,12 +11603,12 @@ with pkgs;
   perceptualdiff = callPackage ../tools/graphics/perceptualdiff { };
 
   inherit (import ../servers/sql/percona-server pkgs) percona-server_8_0 percona-server_8_4;
-  percona-server_lts = percona-server_8_4;
+  percona-server_lts = percona-server_8_0;
   # temporarily, latest LTS and Innovation release are equal
   percona-server_innovation = percona-server_8_4;
   percona-server = percona-server_lts;
   inherit (import ../tools/backup/percona-xtrabackup pkgs) percona-xtrabackup_8_0 percona-xtrabackup_8_4;
-  percona-xtrabackup_lts = percona-xtrabackup_8_4;
+  percona-xtrabackup_lts = percona-xtrabackup_8_0;
   # temporarily, latest LTS and Innovation release are equal
   percona-xtrabackup_innovation = percona-xtrabackup_8_4;
   percona-xtrabackup = percona-xtrabackup_lts;
