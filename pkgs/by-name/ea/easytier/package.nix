f8b427b23f38d9484377d1fc80e9728f4f091d4c
Kevin_Chen <108531516+Kevin-2483@users.noreply.github.com>
2024-10-13 20:54:18 +0800

    Update easytier from 2.0.0 to 2.0.3

--- pkgs/by-name/ea/easytier/package.nix
+++ pkgs/by-name/ea/easytier/package.nix
@@ -1,5 +1,12 @@
-{ lib, stdenv, fetchFromGitHub, rustPlatform, protobuf, nix-update-script
-, darwin, withQuic ? false, # with QUIC protocol support
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  rustPlatform,
+  protobuf,
+  nix-update-script,
+  darwin,
+  withQuic ? false, # with QUIC protocol support
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -13,24 +20,17 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-0bS2VzddRZcFGmHugR0yXHjHqz06tpL4+IhQ6ReaU4Y=";
   };
 
-  # 使用占位符 cargoSha256，稍后通过 nix build 获取正确的哈希
   cargoHash = "sha256-AkEgEymgq2asxT4oR+NtGe8bUEPRUskVvwIJYrCD7xs=";
-  # protobuf 是编译时需要的原生依赖
+
   nativeBuildInputs = [ protobuf ];
 
-  # 当在 macOS 上时，链接到 Apple 的安全框架
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin
-    [ darwin.apple_sdk.frameworks.Security ];
+  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 
-  # 如果是 MIPS 平台，不使用默认特性并添加 MIPS 特性
   buildNoDefaultFeatures = stdenv.hostPlatform.isMips;
-  buildFeatures = lib.optional stdenv.hostPlatform.isMips "mips"
-    ++ lib.optional withQuic "quic";
+  buildFeatures = lib.optional stdenv.hostPlatform.isMips "mips" ++ lib.optional withQuic "quic";
 
-  # 测试依赖过多的网络支持，暂时禁用
   doCheck = false;
 
-  # 自动更新脚本
   passthru.updateScript = nix-update-script { };
 
   meta = {
