632414727bf906aed5e4a8595ced4c49ca03a8ce
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-13 15:13:20 +0000

    omegat: 6.0.0 -> 6.0.1

--- pkgs/applications/misc/omegat.nix
+++ pkgs/applications/misc/omegat.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchurl, unzip, jdk, makeWrapper}:
 
 stdenv.mkDerivation {
-  version = "6.0.0";
+  version = "6.0.1";
   pname = "omegat";
 
   src = fetchurl {  # their zip has repeated files or something, so no fetchzip
-    url = "mirror://sourceforge/project/omegat/OmegaT%20-%20Standard/OmegaT%206.0.0/OmegaT_6.0.0_Without_JRE.zip";
-    sha256 = "sha256-dsbT5iuZBudUL4Kw4rdvi4y98mcvnYNj9jRvC5wJoYI=";
+    url = "mirror://sourceforge/project/omegat/OmegaT%20-%20Standard/OmegaT%206.0.1/OmegaT_6.0.1_Without_JRE.zip";
+    sha256 = "sha256-Rj50bzT8k7+GWb0p/ma+zy+PzkF7tB6iV4F4UVAImJg=";
   };
 
   nativeBuildInputs = [ makeWrapper unzip ];
