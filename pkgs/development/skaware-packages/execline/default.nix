31dd45be46c1d5db80e4bbbd1c9292b246b1357a
Sergei Trofimovich <slyich@gmail.com>
2024-10-14 05:38:24 +0100

    execline: 2.9.6.0 -> 2.9.6.1
    Changes: https://git.skarnet.org/cgi-bin/cgit.cgi/execline/log/?h=v2.9.6.1

--- pkgs/development/skaware-packages/execline/default.nix
+++ pkgs/development/skaware-packages/execline/default.nix
@@ -1,14 +1,14 @@
 { lib, skawarePackages, skalibs }:
 
 let
-  version = "2.9.6.0";
+  version = "2.9.6.1";
 
 in skawarePackages.buildPackage {
   inherit version;
 
   pname = "execline";
   # ATTN: also check whether there is a new manpages version
-  sha256 = "uion6Xxetr18pqCYeokl5ERlpb6ZbaoNGPj+yjfXVxo=";
+  sha256 = "sha256-dpGdYvLeTbGsSzpZ7rPg4JtivN2a3ROuPy2tJvjw5co=";
 
   # Maintainer of manpages uses following versioning scheme: for every
   # upstream $version he tags manpages release as ${version}.1, and,
@@ -17,7 +17,7 @@ in skawarePackages.buildPackage {
   manpages = skawarePackages.buildManPages {
     pname = "execline-man-pages";
     version = "2.9.6.0.1";
-    sha256 = "0lyX3rIUZ2JqWioRSm22uDS+q9ONkwIZxfR5E2pSDC4=";
+    sha256 = "sha256-0lyX3rIUZ2JqWioRSm22uDS+q9ONkwIZxfR5E2pSDC4=";
     description = "Port of the documentation for the execline suite to mdoc";
     maintainers = [ lib.maintainers.sternenseemann ];
   };
