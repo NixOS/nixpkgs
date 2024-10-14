afeaa6fa0834ac133ec800ba6ce97c38a9c4ef81
teutat3s <10206665+teutat3s@users.noreply.github.com>
2024-10-14 23:30:06 +0200

    signal-desktop-beta: 7.25.0-beta.2 -> 7.29.0-beta.1
    (cherry picked from commit df0c496ad298860a42617ce8e35ac6085f4c0d04)

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop-beta";
   dir = "Signal Beta";
-  version = "7.25.0-beta.2";
+  version = "7.29.0-beta.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-7L+ldJYJXB9VkC12s8AyUdECVXnyN8PNo7AFIyEHcS8=";
+  hash = "sha256-+IfCdwoDgHgqOKS7QAca0P3ULw8EE40asqJvj2zWWPA=";
 }
