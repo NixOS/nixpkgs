4e51211fdfee11e815e793c043a3b38edcab2666
Anderson Torres <torres.anderson.85@protonmail.com>
2024-10-14 20:58:45 -0300

    setconf: get rid of rec

--- pkgs/tools/misc/setconf/default.nix
+++ pkgs/tools/misc/setconf/default.nix
@@ -1,28 +1,32 @@
-{ lib
-, python3Packages
-, fetchFromGitHub
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
 }:
 
-python3Packages.buildPythonApplication rec {
-  pname = "setconf";
-  version = "0.7.7";
+let
+  self = python3Packages.buildPythonApplication {
+    pname = "setconf";
+    version = "0.7.7";
 
-  src = fetchFromGitHub {
-    owner = "xyproto";
-    repo = "setconf";
-    rev = version;
-    hash = "sha256-HYZdDtDlGrT3zssDdMW3559hhC+cPy8qkmM8d9zEa1A=";
+    src = fetchFromGitHub {
+      owner = "xyproto";
+      repo = "setconf";
+      rev = self.version;
+      hash = "sha256-HYZdDtDlGrT3zssDdMW3559hhC+cPy8qkmM8d9zEa1A=";
+    };
+
+    build-system = with python3Packages; [ setuptools ];
+
+    pyproject = true;
+
+    meta = {
+      homepage = "https://github.com/xyproto/setconf";
+      description = "Small utility for changing settings in configuration textfiles";
+      changelog = "https://github.com/xyproto/setconf/releases/tag/${self.src.rev}";
+      mainProgram = "setconf";
+      maintainers = with lib.maintainers; [ AndersonTorres ];
+    };
   };
-
-  build-system = with python3Packages; [ setuptools ];
-
-  pyproject = true;
-
-  meta = {
-    homepage = "https://github.com/xyproto/setconf";
-    description = "Small utility for changing settings in configuration textfiles";
-    changelog = "https://github.com/xyproto/setconf/releases/tag/${version}";
-    mainProgram = "setconf";
-    maintainers = with lib.maintainers; [ AndersonTorres ];
-  };
-}
+in
+self
