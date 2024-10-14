2df5a7abc90ccc53842db4d98117d200f7b93c81
Erik Arvstedt <erik.arvstedt@gmail.com>
2024-10-14 12:17:18 +0200

    clboss: 0.13.3 -> 0.14.0

--- pkgs/applications/blockchains/clboss/default.nix
+++ pkgs/applications/blockchains/clboss/default.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, fetchFromGitHub
+, fetchzip
 , autoconf-archive
 , autoreconfHook
 , pkg-config
@@ -11,13 +11,12 @@
 
 stdenv.mkDerivation rec {
   pname = "clboss";
-  version = "0.13.3";
+  version = "0.14.0";
 
-  src = fetchFromGitHub {
-    owner = "ZmnSCPxj";
-    repo = "clboss";
-    rev = "v${version}";
-    hash = "sha256-T61rkTEGLCZrEBp1WFhHnQ7DQyhctMf5lgbOs6u9E0o=";
+  # The release tarball includes the pre-generated file `commit_hash.h` that is required for building
+  src = fetchzip {
+    url = "https://github.com/ZmnSCPxj/clboss/releases/download/v${version}/clboss-v${version}.tar.gz";
+    hash = "sha256-Qp8br4ZxiqaxFZ6Tb+wFpqp2APmnU9QdNkM8MyGAtrw=";
   };
 
   nativeBuildInputs = [ autoconf-archive autoreconfHook pkg-config libev curlWithGnuTls sqlite ];
