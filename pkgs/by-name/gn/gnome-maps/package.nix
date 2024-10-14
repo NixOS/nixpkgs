c0583efbe2037ab10381b10a042417b3f6fe29cc
Sefa Eyeoglu <contact@scrumplex.net>
2024-10-14 21:12:27 +0200

    gnome-maps: remove absolute path in desktop entry
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    (cherry picked from commit 8816be7bdaa297e6b4448473f8e23e08b0ed1a0b)

--- pkgs/by-name/gn/gnome-maps/package.nix
+++ pkgs/by-name/gn/gnome-maps/package.nix
@@ -68,16 +68,6 @@ stdenv.mkDerivation (finalAttrs: {
     libsoup_3
   ];
 
-  postPatch = ''
-    # The .service file isn't wrapped with the correct environment
-    # so misses GIR files when started. By re-pointing from the gjs
-    # entry point to the wrapped binary we get back to a wrapped
-    # binary.
-    substituteInPlace "data/org.gnome.Maps.service.in" \
-      --replace-fail "Exec=@pkgdatadir@/@app-id@" \
-                     "Exec=$out/bin/gnome-maps"
-  '';
-
   preCheck = ''
     # “time.js” included by “timeTest” and “translationsTest” depends on “org.gnome.desktop.interface” schema.
     export XDG_DATA_DIRS="${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:$XDG_DATA_DIRS"
