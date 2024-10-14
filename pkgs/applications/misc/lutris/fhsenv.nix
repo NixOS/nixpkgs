ca6b96652354513e59882cdf993a2a02b19893ef
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2024-10-14 13:37:32 +0300

    lutris: fix libjansson and cursor issues
    This fixes the following warning:
    ntlm_auth: /home/reloner/.local/share/lutris/runtime/Ubuntu-18.04-x86_64/libjansson.so.4: no version information available (required by /nix/store/4gcggjvzrbywi92r66p6l15hckxng2q0-samba-4.20.1/lib/samba/libcommon-auth-private-samba.so)
    The reason why this issue is happening is because the Lutris FHS that you are running doesn't install jansson as a dependency. By default the WoW entry tries to prioritize the System's libraries and if it doesn't find something, it will fallback to the Lutris runtime. Since the runtime version is really old, you'll see that error.
    
    This commit also fixes cursor issues caused by xorg in WoW.

--- pkgs/applications/misc/lutris/fhsenv.nix
+++ pkgs/applications/misc/lutris/fhsenv.nix
@@ -11,6 +11,7 @@ let
   xorgDeps = pkgs: with pkgs.xorg; [
     libX11 libXrender libXrandr libxcb libXmu libpthreadstubs libXext libXdmcp
     libXxf86vm libXinerama libSM libXv libXaw libXi libXcursor libXcomposite
+    libXfixes libXtst libXScrnSaver libICE libXt
   ];
   gstreamerDeps = pkgs: with pkgs.gst_all_1; [
     gstreamer
@@ -34,6 +35,9 @@ in buildFHSEnv {
 
     # Adventure Game Studio
     allegro dumb
+
+    # Battle.net
+    jansson
 
     # Curl
     libnghttp2
