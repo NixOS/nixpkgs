14dff391821d925fb014d0c644d2cd86fe1b63bd
Morgan Jones <me@numin.it>
2024-10-13 22:43:37 -0700

    stp: 2.3.3 -> 2.3.4
    Bump the version, enable checkPhase, and create an installCheckPhase.

--- pkgs/applications/science/logic/stp/default.nix
+++ pkgs/applications/science/logic/stp/default.nix
@@ -4,6 +4,7 @@
 , boost
 , bison
 , flex
+, pkg-config
 , fetchFromGitHub
 , fetchpatch
 , perl
@@ -11,17 +12,21 @@
 , zlib
 , minisat
 , cryptominisat
+, gmp
+, gtest
+, lit
+, outputcheck
 }:
 
 stdenv.mkDerivation rec {
   pname = "stp";
-  version = "2.3.3";
+  version = "2.3.4";
 
   src = fetchFromGitHub {
     owner = "stp";
     repo = "stp";
     rev = version;
-    hash = "sha256-B+HQF4TJPkYrpodE4qo4JHvlu+a5HTJf1AFyXTnZ4vk=";
+    hash = "sha256-PtONKgqahT9x+5WHi3zyoXwDTdq/VapzBl3Aod7Cbgc=";
   };
   patches = [
     # Fix missing type declaration
@@ -36,30 +41,62 @@ stdenv.mkDerivation rec {
     })
   ];
 
-  postPatch = ''
-    # Upstream fix for gcc-13 support:
-    #   https://github.com/stp/stp/pull/462
-    # Can't apply it as is as patch context changed in ither patches.
-    # TODO: remove me on 2.4 release
-    sed -e '1i #include <cstdint>' -i include/stp/AST/ASTNode.h
-  '';
-
   buildInputs = [
     boost
     zlib
     minisat
     cryptominisat
     python3
+    gmp
   ];
-  nativeBuildInputs = [ cmake bison flex perl ];
+  nativeBuildInputs = [ cmake bison flex perl pkg-config gtest lit ];
+  cmakeFlags = [ "-DBUILD_SHARED_LIBS=ON" "-DENABLE_TESTING=ON" "-DLIT_ARGS=-v" ];
   preConfigure = ''
     python_install_dir=$out/${python3.sitePackages}
     mkdir -p $python_install_dir
     cmakeFlagsArray=(
       $cmakeFlagsArray
-      "-DBUILD_SHARED_LIBS=ON"
       "-DPYTHON_LIB_INSTALL_DIR=$python_install_dir"
     )
+
+    # Version is wrong in 2.3.4, fix it.
+    substituteInPlace CMakeLists.txt \
+      --replace-fail 2.3.3 ${version} \
+      --replace-fail GIT-hash-notfound ${version}
+
+    # We want to use the Nix wrapper for the output check tool instead of running it through Python
+    substituteInPlace tests/query-files/lit.cfg \
+      --replace-fail "pythonExec + ' ' +OutputCheckTool" "OutputCheckTool"
+
+    # Link in gtest and the output check utility.
+    mkdir -p deps
+    ln -s ${gtest.src} deps/gtest
+    ln -s ${outputcheck} deps/OutputCheck
+  '';
+
+  doCheck = true;
+
+  postInstall = ''
+    # Clean up installed gtest/gmock files that shouldn't be there.
+    find $out/ \( -iname '*gtest*' -o -iname '*gmock*' \) -print | while read -r file; do
+      if [ -e "$file" ]; then
+        echo "Removing $file" >&2
+        rm -rf "$file"
+      fi
+    done
+
+    # Remove empty pkgconfig folders.
+    find $out/ -type d -name pkgconfig -print | while read -r dir; do
+      if [ -z "$(ls -A "$dir")" ]; then
+        echo "Cleaning up empty pkgconfig directory at $dir" >&2
+        rm -rf "$dir"
+      fi
+    done
+  '';
+
+  doInstallCheck = true;
+  postInstallCheck = ''
+    $out/bin/stp --version | grep '^STP version ${version}'
   '';
 
   meta = with lib; {
