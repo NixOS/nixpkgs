3be551c8312667c0e4e542b83994c47bc2b6c532
aleksana <me@aleksana.moe>
2024-10-14 16:38:22 +0800

    qcm: 1.0.5 -> 1.1.1
    Diff: https://github.com/hypengw/Qcm/compare/v1.0.5...v1.1.1
    Changelog:
    https://github.com/hypengw/Qcm/releases/tag/v1.1.0
    https://github.com/hypengw/Qcm/releases/tag/v1.1.1

--- pkgs/by-name/qc/qcm/package.nix
+++ pkgs/by-name/qc/qcm/package.nix
@@ -11,17 +11,23 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "qcm";
-  version = "1.0.5";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "hypengw";
     repo = "Qcm";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-/FOT2xK01JbJbTd5AT5Dk/5EF9qUyLvPTnw8PMtHYoQ=";
+    hash = "sha256-fdqmtS1lKHbsgsDmmlNOV1q2C0u+jCNC0aouImw3/y8=";
   };
 
-  patches = [ ./remove_cubeb_vendor.patch ];
+  patches = [
+    ./remove_cubeb_vendor.patch
+    # fix: 'std::strong_ordering operator<=>(const QString&, const QString&)'
+    # in core/include/core/qstr_helper.h
+    # duplicate with qtbase-6.8.0/include/QtCore/qstring.h
+    ./remove_redefinition_operator.patch
+  ];
 
   nativeBuildInputs = [
     cmake
@@ -32,6 +38,7 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     qt6.qtbase
     qt6.qtdeclarative
+    qt6.qtsvg
     qt6.qtwayland
     curl
     ffmpeg
@@ -40,8 +47,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   # Correct qml import path
   postInstall = ''
-    mkdir $out/lib/qt-6
-    mv $out/lib/qml $out/lib/qt-6/qml
+    mv $out/lib/qt6 $out/lib/qt-6
   '';
 
   qtWrapperArgs = [
