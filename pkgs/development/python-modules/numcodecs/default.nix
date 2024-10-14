6e56a2c071ecd0ce2b3f902a5509c1e6c24fe23c
Doron Behar <doron.behar@gmail.com>
2024-10-14 10:08:59 +0300

    python312Packages.numcodecs: 0.12.1 -> 0.13.1

--- pkgs/development/python-modules/numcodecs/default.nix
+++ pkgs/development/python-modules/numcodecs/default.nix
@@ -3,7 +3,6 @@
   stdenv,
   buildPythonPackage,
   fetchPypi,
-  fetchpatch,
   python,
   pythonOlder,
 
@@ -19,28 +18,21 @@
   # tests
   msgpack,
   pytestCheckHook,
+  importlib-metadata,
 }:
 
 buildPythonPackage rec {
   pname = "numcodecs";
-  version = "0.12.1";
+  version = "0.13.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-BdkaQzcz5+7yaNfoDsImoCMtokQolhSo84JpAa7BCY4=";
+    hash = "sha256-o883iB3wiY86nA1Ed9+IEz/oUYW//le6MbzC+iB3Cbw=";
   };
 
-  patches = [
-    # https://github.com/zarr-developers/numcodecs/pull/487
-    (fetchpatch {
-      name = "fix-tests.patch";
-      url = "https://github.com/zarr-developers/numcodecs/commit/4896680087d3ff1f959401c51cf5aea0fd56554e.patch";
-      hash = "sha256-+lMWK5IsNzJ7H2SmLckgxbSSRIIcC7FtGYSBKQtuo+Y=";
-    })
-  ];
 
   nativeBuildInputs = [
     setuptools
@@ -63,22 +55,11 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     pytestCheckHook
     msgpack
+    importlib-metadata
   ];
 
   # https://github.com/NixOS/nixpkgs/issues/255262
   pytestFlagsArray = [ "$out/${python.sitePackages}/numcodecs" ];
-
-  disabledTests = [
-    "test_backwards_compatibility"
-
-    "test_encode_decode"
-    "test_legacy_codec_broken"
-    "test_bytes"
-
-    # ValueError: setting an array element with a sequence. The requested array has an inhomogeneous shape after 1 dimensions. The detected shape was (3,) + inhomogeneous part.
-    # with numpy 1.24
-    "test_non_numpy_inputs"
-  ];
 
   meta = {
     homepage = "https://github.com/zarr-developers/numcodecs";
