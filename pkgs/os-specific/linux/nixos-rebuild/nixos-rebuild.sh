NixOS/nixpkgs

commit 617a1a6e5778df81efceffa656988e38bd293b1c

nixos-rebuild.sh: pass flags to nix-copy-closure whenever possible

pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
@@ -102,21 +102,32 @@ while [ "$#" -gt 0 ]; do
       --use-substitutes|--substitute-on-destination|-s)
         copyFlags+=("-s")
         ;;
-      -I|--max-jobs|-j|--cores|--builders|--log-format)
+      --builders)
         j="$1"; shift 1
         extraBuildFlags+=("$i" "$j")
         ;;
-      --accept-flake-config|-j*|--quiet|--print-build-logs|-L|--no-build-output|-Q| --show-trace|--keep-going|-k|--keep-failed|-K|--fallback|--refresh|--repair|--impure|--offline|--no-net)
+      --I|--max-jobs|-j|--cores|--log-format)
+        j="$1"; shift 1
+        extraBuildFlags+=("$i" "$j")
+        copyFlags+=("$i" "$j")
+        ;;
+      --accept-flake-config|-j*|--quiet|--print-build-logs|-L|--no-build-output|-Q|--show-trace|--refresh|--impure|--offline|--no-net)
         extraBuildFlags+=("$i")
+        ;;
+      --keep-going|-k|--keep-failed|-K|--fallback|--repair)
+        extraBuildFlags+=("$i")
+        copyFlags+=("$i")
         ;;
       --verbose|-v|-vv|-vvv|-vvvv|-vvvvv)
         verboseScript="true"
         extraBuildFlags+=("$i")
+        copyFlags+=("$i")
         ;;
       --option)
         j="$1"; shift 1
         k="$1"; shift 1
         extraBuildFlags+=("$i" "$j" "$k")
+        copyFlags+=("$i" "$j" "$k")
         ;;
       --fast)
         buildNix=

commit 42a12a0551a59ec1ad09d87d70b1dcabb323f92d

nixos-rebuild: Fix broken -I option
Commit 617a1a6e5778df81efceffa656988e38bd293b1c (#341402) broke this.

Signed-off-by: Anders Kaseorg <andersk@mit.edu>

pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
@@ -102,11 +102,11 @@ while [ "$#" -gt 0 ]; do
       --use-substitutes|--substitute-on-destination|-s)
         copyFlags+=("-s")
         ;;
-      --builders)
+      -I|--builders)
         j="$1"; shift 1
         extraBuildFlags+=("$i" "$j")
         ;;
-      --I|--max-jobs|-j|--cores|--log-format)
+      --max-jobs|-j|--cores|--log-format)
         j="$1"; shift 1
         extraBuildFlags+=("$i" "$j")
         copyFlags+=("$i" "$j")