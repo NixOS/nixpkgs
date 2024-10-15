d5bd8665397d5dd5a0e9fd7640cd0a75e481d5fe
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-15 11:30:55 +0000

    fprintd: 1.94.3 -> 1.94.4

--- pkgs/tools/security/fprintd/default.nix
+++ pkgs/tools/security/fprintd/default.nix
@@ -23,7 +23,7 @@
 
 stdenv.mkDerivation rec {
   pname = "fprintd";
-  version = "1.94.3";
+  version = "1.94.4";
   outputs = [ "out" "devdoc" ];
 
   src = fetchFromGitLab {
@@ -31,7 +31,7 @@ stdenv.mkDerivation rec {
     owner = "libfprint";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-shH+ctQAx4fpTMWTmo3wB45ZS38Jf8RknryPabfZ6QE=";
+    sha256 = "sha256-B2g2d29jSER30OUqCkdk3+Hv5T3DA4SUKoyiqHb8FeU=";
   };
 
   nativeBuildInputs = [
