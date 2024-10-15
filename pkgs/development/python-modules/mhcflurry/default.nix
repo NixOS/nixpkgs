NixOS/nixpkgs

commit 8574a14dd82a7669253dcff86d94c40b1204a8e3

python3{11,12}Packages.mhcflurry: remove nose dependency
This package is currently broken anyway, but I have sent an untested
PR upstream to migrate their tests to pytest in the hopes that it
will be merged by the time we can build this again. If we fix the
package before the PR is merged, hopefully we can use it as a basis
for a vendored patch.

pkgs/development/python-modules/mhcflurry/default.nix
@@ -44,8 +44,9 @@ buildPythonPackage rec {
     tqdm
   ];
 
+  # Tests currently depend on nose; see
+  # <https://github.com/openvax/mhcflurry/pull/244>.
   nativeCheckInputs = [
-    nose
     pytestCheckHook
   ];
 

commit b713071a827ebeeea6df50175ffdd170adb4b1f1

python3{11,12}Packages.mhcflurry: remove unused nose input
Fixes: 8574a14dd82a7669253dcff86d94c40b1204a8e3

pkgs/development/python-modules/mhcflurry/default.nix
@@ -15,7 +15,6 @@
   tqdm,
 
   # tests
-  nose,
   pytestCheckHook,
 }:
 