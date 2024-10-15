f05aba60672463cb938649d9d45bcd9d9605e312
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-15 04:56:27 +0000

    rshim-user-space: 2.0.32 -> 2.0.38

--- pkgs/tools/misc/rshim-user-space/default.nix
+++ pkgs/tools/misc/rshim-user-space/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation rec {
   pname = "rshim-user-space";
-  version = "2.0.32";
+  version = "2.0.38";
 
   src = fetchFromGitHub {
     owner = "Mellanox";
     repo = pname;
     rev = "rshim-${version}";
-    hash = "sha256-GF7cKIf72EYRBRb6d3IWONE7YJwohTLKF1RGdDMTG6c=";
+    hash = "sha256-u7NHtdBjtI81FSb6AizzF0Y+gTdu/4zUw8l5AZQJB4Y=";
   };
 
   nativeBuildInputs = [
