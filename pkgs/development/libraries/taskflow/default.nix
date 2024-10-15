bf3c22db390d65825600556752c825086d2519dc
Robert Schütz <mail@dotlambda.de>
2024-10-14 21:31:07 -0700

    taskflow: 3.7.0 -> 3.8.0
    Diff: https://github.com/taskflow/taskflow/compare/v3.7.0...v3.8.0
    
    Changelog: https://taskflow.github.io/taskflow/release-3-8-0.html

--- pkgs/development/libraries/taskflow/default.nix
+++ pkgs/development/libraries/taskflow/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "taskflow";
-  version = "3.7.0";
+  version = "3.8.0";
 
   src = fetchFromGitHub {
     owner = "taskflow";
     repo = "taskflow";
     rev = "v${version}";
-    hash = "sha256-q2IYhG84hPIZhuogWf6ojDG9S9ZyuJz9s14kQyIc6t0=";
+    hash = "sha256-gim1QQKtzMXz8BmNg5YeN4mcveiid5MrS8IrTaTtZ1Y=";
   };
 
   patches = [
