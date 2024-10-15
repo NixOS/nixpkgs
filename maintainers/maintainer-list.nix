ecb3de9fcec2e46a73c3ff2c1ceb0b56995cc723
TheRealGramdalf <gramdalftech@gmail.com>
2024-10-15 23:26:17 +0000

    maintainers: add therealgramdalf

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21319,6 +21319,12 @@
     githubId = 57180880;
     name = "Ansh Tyagi";
   };
+  therealgramdalf = {
+    email = "gramdalftech@gmail.com";
+    github = "TheRealGramdalf";
+    githubId = 79593869;
+    name = "Gramdalf";
+  };
   therealr5 = {
     email = "rouven@rfive.de";
     github = "therealr5";
