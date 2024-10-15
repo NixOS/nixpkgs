a6def8e0a3febf270e7831a0d24b69216942b183
Robert Schütz <mail@dotlambda.de>
2024-10-14 22:00:56 -0700

    taskflow: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23239,8 +23239,6 @@ with pkgs;
 
   tagparser = callPackage ../development/libraries/tagparser { };
 
-  taskflow = callPackage ../development/libraries/taskflow { };
-
   tclap = tclap_1_2;
 
   tclap_1_2 = callPackage ../development/libraries/tclap/1.2.nix { };
