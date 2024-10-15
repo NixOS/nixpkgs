NixOS/nixpkgs

commit ee5127c6df7e5316a175e0bf9becc6156435a996

iproute2: 6.10.0 -> 6.11.0
Changes: https://lore.kernel.org/netdev/20240915201831.164de47e@hermes.local/T/

pkgs/os-specific/linux/iproute/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   pname = "iproute2";
-  version = "6.10.0";
+  version = "6.11.0";
 
   src = fetchurl {
     url = "mirror://kernel/linux/utils/net/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-kaYvgnN7RJBaAPqAM2nER9VJ6RTpoqQBj911sdVOjc4=";
+    hash = "sha256-H3lTmKBK6qzQao9qziz9kTwz+llTypnaroO7XFNGEcM=";
   };
 
   postPatch = ''

commit 40045faf4a0000dfff781ce817deb7ec5069d48f

iproute2: fix build for musl
Patch is upstream.

Fixes: ee5127c6df7e ("iproute2: 6.10.0 -> 6.11.0")

pkgs/os-specific/linux/iproute/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl
+{ lib, stdenv, fetchurl, fetchpatch
 , buildPackages, bison, flex, pkg-config
 , db, iptables, elfutils, libmnl ,libbpf
 , gitUpdater, pkgsStatic
@@ -28,6 +28,11 @@ stdenv.mkDerivation rec {
       name = "musl-basename.patch";
       url = "https://lore.kernel.org/netdev/20240804161054.942439-1-dilfridge@gentoo.org/raw";
       hash = "sha256-47obv6mIn/HO47lt47slpTAFDxiQ3U/voHKzIiIGCTM=";
+    })
+    (fetchpatch {
+      name = "musl-mst.patch";
+      url = "https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/patch/?id=6a77abab92516e65f07f8657fc4e384c4541ce0e";
+      hash = "sha256-19FzTDvgnmqVFBykVgXl4VIsHs8Cy9NWGOLpxifxVlI=";
     })
   ];
 