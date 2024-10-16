d6a009e615adbdfd9cc7b4bd51115c453036ed70
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-16 16:44:51 +0000

    python312Packages.shiv: 1.0.6 -> 1.0.7

--- pkgs/development/python-modules/shiv/default.nix
+++ pkgs/development/python-modules/shiv/default.nix
@@ -11,12 +11,12 @@
 
 buildPythonPackage rec {
   pname = "shiv";
-  version = "1.0.6";
+  version = "1.0.7";
   format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-4iJ2gTWXe+vftcDRp9/qKVV8VmtY0wDVuMJTXvIj13Y=";
+    hash = "sha256-lHdX/iY4OuntoMV288uiRN+jcV7S9Jk1RLdYJF9xqxU=";
   };
 
   propagatedBuildInputs = [
