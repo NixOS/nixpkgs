9caa70d65e09f682585abed41c5b799890ffe9f7
José Ribeiro <work@jlbribeiro.com>
2024-10-16 01:53:55 +0100

    feishin: 0.11.0 -> 0.11.1
    Diff: https://github.com/jeffvli/feishin/compare/v0.11.0...v0.11.1
    Changelog: https://github.com/jeffvli/feishin/releases/tag/v0.11.1

--- pkgs/by-name/fe/feishin/package.nix
+++ pkgs/by-name/fe/feishin/package.nix
@@ -11,13 +11,13 @@
 }:
 let
   pname = "feishin";
-  version = "0.11.0";
+  version = "0.11.1";
 
   src = fetchFromGitHub {
     owner = "jeffvli";
     repo = "feishin";
     rev = "v${version}";
-    hash = "sha256-6USYOoXN30sFPEVX5lL4HAXSgKgU6DXsM3fCrxaJIPk=";
+    hash = "sha256-fHaNluLes25P/mSTSYFt97pC6uKYuBI/3PUHc84zoWg=";
   };
 
   electron = electron_31;
@@ -26,7 +26,7 @@ buildNpmPackage {
   inherit pname version;
 
   inherit src;
-  npmDepsHash = "sha256-aLey8mxNYZCtI+E3W5T9ajOJfWZ04mvq1IWkXI9s52I=";
+  npmDepsHash = "sha256-8xFB47PJpa+3U+Xy+DEdWoW3/f+naFKtLQsDDVgUccA=";
 
   npmFlags = [ "--legacy-peer-deps" ];
   makeCacheWritable = true;
@@ -60,7 +60,7 @@ buildNpmPackage {
         inherit version;
 
         src = "${src}/release/app";
-        npmDepsHash = "sha256-l88NuPeURhzINxAazQ67XtRUv0C5VedBBuF8Zgy2HCY=";
+        npmDepsHash = "sha256-gufOUBfHTDkIqRTdPqXuuk1ZT0y80y/GyI7ssvHnBYo=";
 
         npmFlags = [ "--ignore-scripts" ];
         dontNpmBuild = true;
