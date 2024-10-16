49110e0c70656270833006e36e10eb80faf15c78
Ryan Omasta <git@ryand.ca>
2024-10-16 12:26:37 -0600

    cinny-unwrapped, cinny-desktop: 4.2.1 -> 4.2.2

--- pkgs/by-name/ci/cinny-desktop/package.nix
+++ pkgs/by-name/ci/cinny-desktop/package.nix
@@ -20,18 +20,18 @@
 rustPlatform.buildRustPackage rec {
   pname = "cinny-desktop";
   # We have to be using the same version as cinny-web or this isn't going to work.
-  version = "4.2.1";
+  version = "4.2.2";
 
   src = fetchFromGitHub {
     owner = "cinnyapp";
     repo = "cinny-desktop";
     rev = "refs/tags/v${version}";
-    hash = "sha256-W73ma8ScF3LGv45yhZCV80zhh7URLuWhbi+JumyTp+4=";
+    hash = "sha256-W8WSnfUqWTtyb6x0Kmej5sAxsi1Kh/uDkIx6SZhgSvw=";
   };
 
   sourceRoot = "${src.name}/src-tauri";
 
-  cargoHash = "sha256-ved2W4+Dt7pN9j9vIaDlAkaY517nBEgPKgu8ArcHXsM=";
+  cargoHash = "sha256-rg4NdxyJfnEPmFjb2wKJcF7ga7t5WNX/LB0haOvGbXU=";
 
   postPatch =
     let
