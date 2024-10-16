4b856fdad728ae88e6f9a50aae9fed7183e30687
Bryan Lai <bryanlais@gmail.com>
2024-10-16 13:08:58 +0800

    rust-cbindgen: fix test failures due to env vars
    ... specifically, it's the `CARGO_BUILD_TARGET` env var introduced in
    commit: 683f97e3781356b3e8251d0ab5e5906c4cdd4ded

--- pkgs/development/tools/rust/cbindgen/default.nix
+++ pkgs/development/tools/rust/cbindgen/default.nix
@@ -23,6 +23,12 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-XTGHHD5Qw3mr+lkPKOXyqb0K3sEENW8Sf0n9mtrFFXI=";
   };
 
+  patches = [
+    # open PR: https://github.com/mozilla/cbindgen/pull/1010
+    # see also: https://github.com/NixOS/nixpkgs/pull/298108
+    ./1010-fix-test-failures-due-to-CARGO_BUILD_TARGET.patch
+  ];
+
   cargoHash = "sha256-l4FgwXdibek4BAnqjWd1rLxpEwuMNjYgvo6X3SS3fRo=";
 
   buildInputs = lib.optional stdenv.hostPlatform.isDarwin Security;
@@ -45,10 +51,6 @@ rustPlatform.buildRustPackage rec {
     # https://github.com/eqrion/cbindgen/issues/628
     "--skip test_body"
   ];
-
-  # tests currently fail, waiting on upstream
-  # Related: https://github.com/NixOS/nixpkgs/pull/348031
-  doCheck = false;
 
   passthru.tests = {
     inherit
