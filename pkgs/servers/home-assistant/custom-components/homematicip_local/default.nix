7fe1aac1e591eb703c766b61ecf4870fc49ebb57
Robert Schütz <mail@dotlambda.de>
2024-10-16 16:56:08 -0700

    home-assistant-custom-components.homematicip_local: 1.65.0 -> 1.67.0
    Diff: https://github.com/danielperna84/custom_homematic/compare/refs/tags/1.65.0...1.67.0
    
    Changelog: https://github.com/danielperna84/custom_homematic/blob/1.67.0/changelog.md

--- pkgs/servers/home-assistant/custom-components/homematicip_local/default.nix
+++ pkgs/servers/home-assistant/custom-components/homematicip_local/default.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent rec {
   owner = "danielperna84";
   domain = "homematicip_local";
-  version = "1.65.0";
+  version = "1.67.0";
 
   src = fetchFromGitHub {
     owner = "danielperna84";
     repo = "custom_homematic";
     rev = "refs/tags/${version}";
-    hash = "sha256-rUiesrVpGZMMo8JSboFTDE2oE0Q9hNYR4AvA/GR5+MY=";
+    hash = "sha256-lKjBt5oMSdGHXUddaGM+BQOhFnsobvgf2k6qMu2uofY=";
   };
 
   dependencies = [
