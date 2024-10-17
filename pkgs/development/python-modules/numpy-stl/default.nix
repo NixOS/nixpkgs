948be822d284e76d955f5293f460d355ea282bb3
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-17 12:42:24 +0200

    python312Packages.numpy-stl: refactor

--- pkgs/development/python-modules/numpy-stl/default.nix
+++ pkgs/development/python-modules/numpy-stl/default.nix
@@ -2,28 +2,31 @@
   lib,
   buildPythonPackage,
   cython,
-  enum34,
   fetchPypi,
-  nine,
   numpy,
   pytestCheckHook,
   python-utils,
+  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "numpy-stl";
   version = "3.1.1";
-  format = "setuptools";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-947qYsgJOL9T6pFPpbbJL0SPDqtWCeDlpzfd4DlAQzQ=";
   };
 
-  propagatedBuildInputs = [
-    cython
-    enum34
-    nine
+  build-system = [ setuptools ];
+
+  nativeBuildInputs = [ cython ];
+
+  dependencies = [
     numpy
     python-utils
   ];
@@ -35,6 +38,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library to make reading, writing and modifying both binary and ascii STL files easy";
     homepage = "https://github.com/WoLpH/numpy-stl/";
+    changelog = "https://github.com/wolph/numpy-stl/releases/tag/v${version}";
     license = licenses.bsd3;
     maintainers = [ ];
   };
