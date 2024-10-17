4f20d42dd1ba01ceb47c7ec11b4e370d4fba2f0e
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-17 23:43:00 +0200

    python312Packages.plugwise: 1.4.1 -> 1.4.2
    Diff: https://github.com/plugwise/python-plugwise/compare/refs/tags/v1.4.1...v1.4.2
    
    Changelog: https://github.com/plugwise/python-plugwise/releases/tag/v1.4.2

--- pkgs/development/python-modules/plugwise/default.nix
+++ pkgs/development/python-modules/plugwise/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "plugwise";
-  version = "1.4.1";
+  version = "1.4.2";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "plugwise";
     repo = "python-plugwise";
     rev = "refs/tags/v${version}";
-    hash = "sha256-lMcehjG1Zc9s02MBsRUXZHQjxcrZetOgOSne0nCGVV0=";
+    hash = "sha256-JzHDmespyQ4+I7+6vbu/jqK70b71FJvoO0wDR6MnIaw=";
   };
 
   postPatch = ''
