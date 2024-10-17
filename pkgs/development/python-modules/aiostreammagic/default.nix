1c7cf80632b3e2568ebc9cfb832f7fbcd8765aaf
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-17 09:16:53 +0200

    python312Packages.aiostreammagic: 2.6.0 -> 2.8.1
    Diff: https://github.com/noahhusby/aiostreammagic/compare/refs/tags/2.6.0...2.8.1
    
    Changelog: https://github.com/noahhusby/aiostreammagic/releases/tag/2.8.1

--- pkgs/development/python-modules/aiostreammagic/default.nix
+++ pkgs/development/python-modules/aiostreammagic/default.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  aiohttp,
   buildPythonPackage,
   fetchFromGitHub,
   mashumaro,
@@ -13,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "aiostreammagic";
-  version = "2.6.0";
+  version = "2.8.1";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -22,7 +21,7 @@ buildPythonPackage rec {
     owner = "noahhusby";
     repo = "aiostreammagic";
     rev = "refs/tags/${version}";
-    hash = "sha256-Clio8JBxlZillqZIYuM8rLcXJeAIS81iXJiopevgpjc=";
+    hash = "sha256-xXKI7qbIAyG957TTZHoA23sQvvom2TNDGSGSTlRcZUQ=";
   };
 
   pythonRelaxDeps = [ "websockets" ];
@@ -30,7 +29,6 @@ buildPythonPackage rec {
   build-system = [ poetry-core ];
 
   dependencies = [
-    aiohttp
     mashumaro
     orjson
     websockets
