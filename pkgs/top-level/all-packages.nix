de2410e6a348d157cd17ac6a44752653d77ed88f
Emily <vcs@emily.moe>
2024-10-17 16:08:29 +0100

    mpv: remove legacy Darwin SDK pattern detritus
    Oh, this is satisfying.
    
    Swift already propagates its corresponding SDK, so we don’t need
    the patch.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31065,7 +31065,7 @@ with pkgs;
     libdvdnav = libdvdnav_4_2_1;
   } // (config.mplayer or {}));
 
-  mpv-unwrapped = darwin.apple_sdk_11_0.callPackage ../applications/video/mpv {
+  mpv-unwrapped = callPackage ../applications/video/mpv {
     stdenv = if stdenv.hostPlatform.isDarwin then swiftPackages.stdenv else stdenv;
   };
 
