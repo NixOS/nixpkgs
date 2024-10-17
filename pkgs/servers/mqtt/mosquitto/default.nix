83b170e5cfab34ea0aed8198fe9a04a12772e274
datafoo <34766150+datafoo@users.noreply.github.com>
2024-10-17 10:14:04 +0200

    mosquitto: 2.0.18 -> 2.0.19
    https://github.com/eclipse/mosquitto/blob/v2.0.19/ChangeLog.txt

--- pkgs/servers/mqtt/mosquitto/default.nix
+++ pkgs/servers/mqtt/mosquitto/default.nix
@@ -30,13 +30,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "mosquitto";
-  version = "2.0.18";
+  version = "2.0.19";
 
   src = fetchFromGitHub {
     owner = "eclipse";
     repo = "mosquitto";
     rev = "v${version}";
-    hash = "sha256-Vs0blV2IhnlEAm0WtOartz+0vLesJfp78FNJCivRxHk=";
+    hash = "sha256-CFfCsKL06NKgiKb1pksBMiSoH3Wz3Ots66uoZiN1y6A=";
   };
 
   patches = lib.optionals stdenv.hostPlatform.isDarwin [
