e85258a781d27540448c3237446b33d03f54bb0c
StepBroBD <ysun@hey.com>
2024-10-17 05:08:52 -0400

    coqPackages.coqutil: init at {0.0.5,0.0.6} (#349174)

--- pkgs/top-level/coq-packages.nix
+++ pkgs/top-level/coq-packages.nix
@@ -57,6 +57,7 @@ let
       coqprime = callPackage ../development/coq-modules/coqprime {};
       coqtail-math = callPackage ../development/coq-modules/coqtail-math {};
       coquelicot = callPackage ../development/coq-modules/coquelicot {};
+      coqutil = callPackage ../development/coq-modules/coqutil {};
       corn = callPackage ../development/coq-modules/corn {};
       deriving = callPackage ../development/coq-modules/deriving {};
       dpdgraph = callPackage ../development/coq-modules/dpdgraph {};
