NixOS/nixpkgs

commit 6f7657be4c73074053d7ba31cd1b05e076190ef9

openexr_3: 3.2.4 -> 3.3.0

pkgs/development/libraries/openexr/3.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "openexr";
-  version = "3.2.4";
+  version = "3.3.0";
 
   src = fetchFromGitHub {
     owner = "AcademySoftwareFoundation";
     repo = "openexr";
     rev = "v${version}";
-    hash = "sha256-mVUxxYe6teiJ18PQ9703/kjBpJ9+a7vcDme+NwtQQQM=";
+    hash = "sha256-/B0IMbPnixLR10KrE2n9n2Ny/pLIsG6DpBJn8Ax79oE=";
   };
 
   outputs = [ "bin" "dev" "out" "doc" ];

commit bb9674228d0f13c1513ab3ca1b13230147085cfd

openexr_3: add libjxl to tests
This would have caught the regression introduced by
6f7657be4c73 ("openexr_3: 3.2.4 -> 3.3.0"), and reverted in the parent
commit.

pkgs/development/libraries/openexr/3.nix
@@ -5,6 +5,7 @@
 , imath
 , libdeflate
 , pkg-config
+, libjxl
 , pkgsCross
 }:
 
@@ -47,6 +48,7 @@ stdenv.mkDerivation rec {
   doCheck = !stdenv.hostPlatform.isAarch32;
 
   passthru.tests = {
+    inherit libjxl;
     musl = pkgsCross.musl64.openexr_3;
   };
 