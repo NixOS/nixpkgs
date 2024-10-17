fd2640ea48659ad2323a3ddee7b5434cd80cb56f
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-17 23:32:54 +0200

    python312Packages.fastcore: 1.7.11 -> 1.7.17
    Diff: https://github.com/fastai/fastcore/compare/refs/tags/1.7.11...1.7.17
    
    Changelog: https://github.com/fastai/fastcore/blob/1.7.17/CHANGELOG.md

--- pkgs/development/python-modules/fastcore/default.nix
+++ pkgs/development/python-modules/fastcore/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "fastcore";
-  version = "1.7.11";
+  version = "1.7.17";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "fastai";
     repo = "fastcore";
     rev = "refs/tags/${version}";
-    hash = "sha256-dRAkhTIH2t6W1Ygda4BRHojKYgwTXaHu4KkKqQIzulw=";
+    hash = "sha256-QBRlJoAOUMNCUQnb4yzU0g/RN0FkX4+swcdxyKROZTE=";
   };
 
   build-system = [ setuptools ];
