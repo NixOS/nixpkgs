f5e43e6c36d1601296a41c28c04c1b27a29f4a32
Emily <vcs@emily.moe>
2024-10-17 16:08:29 +0100

    mpv: don’t override Lua with Lua

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -31067,7 +31067,6 @@ with pkgs;
 
   mpv-unwrapped = darwin.apple_sdk_11_0.callPackage ../applications/video/mpv {
     stdenv = if stdenv.hostPlatform.isDarwin then swiftPackages.stdenv else stdenv;
-    inherit lua;
   };
 
   # Wrap avoiding rebuild
