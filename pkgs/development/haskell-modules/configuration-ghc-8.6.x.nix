3f55059bbd5abd37c1184bf855f33e905cfd681a
sternenseemann <sternenseemann@systemli.org>
2024-10-17 15:46:53 +0200

    haskellPackages.haddock-library_1_7_0: remove at 1.7.0
    stylish-cabal does no longer build for GHC 8.6.5 (binary) for unrelated
    reasons. Doesn't really make sense to invest further into it.

--- pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.6.x.nix
@@ -86,10 +86,6 @@ self: super: {
   # cabal2spec needs a recent version of Cabal
   cabal2spec = super.cabal2spec.overrideScope (self: super: { Cabal = self.Cabal_3_2_1_0; });
 
-  # https://github.com/pikajude/stylish-cabal/issues/12
-  stylish-cabal = doDistribute (markUnbroken (super.stylish-cabal.override { haddock-library = self.haddock-library_1_7_0; }));
-  haddock-library_1_7_0 = dontCheck super.haddock-library_1_7_0;
-
   # ghc versions prior to 8.8.x needs additional dependency to compile successfully.
   ghc-lib-parser-ex = addBuildDepend self.ghc-lib-parser super.ghc-lib-parser-ex;
 
