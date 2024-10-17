749e8452bda064d3bf8d40addcede5104fa281b7
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-10-17 19:40:31 -0300

    application-title-bar: modernize

--- pkgs/by-name/ap/application-title-bar/package.nix
+++ pkgs/by-name/ap/application-title-bar/package.nix
@@ -1,19 +1,19 @@
 {
   lib,
-  stdenv,
+  stdenvNoCC,
   fetchFromGitHub,
   kdePackages,
   nix-update-script,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "application-title-bar";
   version = "0.7.3";
 
   src = fetchFromGitHub {
     owner = "antroids";
     repo = "application-title-bar";
-    rev = "v${finalAttrs.version}";
+    rev = "refs/tags/v${finalAttrs.version}";
     hash = "sha256-kvFUz0m222jTGrkqLyYmnW0o4MXU9lLAsyk6QBAJHr8=";
   };
 
