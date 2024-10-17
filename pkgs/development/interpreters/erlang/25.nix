b53cc70fcd1162b195b42d149ad3561d894eb5f4
chiroptical <chiroptical@gmail.com>
2024-10-17 19:32:42 -0400

    erlang: 25.3.2.13 -> 25.3.2.15

--- pkgs/development/interpreters/erlang/25.nix
+++ pkgs/development/interpreters/erlang/25.nix
@@ -1,6 +1,6 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "25.3.2.13";
-  sha256 = "sha256-ZBk6oOT0vFY66ttyn4G2zy7Ur5bDKkxPzlTsZPaiosc=";
+  version = "25.3.2.15";
+  sha256 = "sha256-y1QZZ+W5jkAygTRtXVu6FyG4I98SGXXourDfPPlEfg8=";
 }
