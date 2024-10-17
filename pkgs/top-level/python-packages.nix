49fe7800e9618261c9ee8d34603652121318c23e
Gaetan Lepage <gaetan@glepage.com>
2024-10-17 14:01:49 +0200

    python312Packages.protobuf: propagate pkgs.protobuf

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10794,7 +10794,9 @@ self: super: with self; {
   };
 
   # Protobuf 5.x
-  protobuf5 = callPackage ../development/python-modules/protobuf/default.nix { };
+  protobuf5 = callPackage ../development/python-modules/protobuf/default.nix {
+    inherit (pkgs.__splicedPackages) protobuf;
+  };
 
   # If a protobuf upgrade causes many Python packages to fail, please pin it here to the previous version.
   protobuf = protobuf5;
