1f5e069694a5abc7f0b954318403dbaa926cba00
Luis David Licea Torres <luisliceatorres@gmail.com>
2024-10-16 22:08:02 -0500

    Fix package.json manual pages not being installed
    Fix incorrect jq data type comparison so that manual pages specified in an array in package.json get installed. The command `echo '[1.23,"abc",true,[],{},null]' | jq 'map(type)'` shows that "list" is not a data type, while "array" is.

--- pkgs/by-name/no/nodejsInstallManuals/hook.sh
+++ pkgs/by-name/no/nodejsInstallManuals/hook.sh
@@ -8,7 +8,7 @@ nodejsInstallManuals() {
     while IFS= read -r man; do
         installManPage "$packageOut/$man"
     done < <(@jq@ --raw-output '(.man | type) as $typ | if $typ == "string" then .man
-        elif $typ == "list" then .man | join("\n")
+        elif $typ == "array" then .man | join("\n")
         elif $typ == "null" then empty
         else "invalid type " + $typ | halt_error end' "$packageJson")
 }
