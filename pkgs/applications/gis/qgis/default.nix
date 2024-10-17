43012a8149bf42b97fdde20f356009e53a209ef1
Ivan Mincik <ivan.mincik@gmail.com>
2024-10-17 11:40:55 +0200

    qgis: re-export unwrapped parameters
    Re-export build configuration parameters from unwrapped package to allow
    standard override using
    
    ```
      qgis.override { ... };
    ```
    
    .

--- pkgs/applications/gis/qgis/default.nix
+++ pkgs/applications/gis/qgis/default.nix
@@ -5,9 +5,16 @@
 , extraPythonPackages ? (ps: [ ])
 
 , libsForQt5
+
+# unwrapped package parameters
+, withGrass ? true
+, withWebKit ? false
 }:
 let
-  qgis-unwrapped = libsForQt5.callPackage ./unwrapped.nix {  };
+  qgis-unwrapped = libsForQt5.callPackage ./unwrapped.nix {
+    withGrass = withGrass;
+    withWebKit = withWebKit;
+  };
 in symlinkJoin rec {
 
   inherit (qgis-unwrapped) version;
