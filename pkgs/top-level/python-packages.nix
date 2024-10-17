478f7544ccd919a7665517a5f5ffae7ab8409118
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-10-17 08:57:42 -0400

    python3.pkgs.geoarrow-pyarrow: init at 0.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4964,6 +4964,8 @@ self: super: with self; {
 
   geoarrow-types = callPackage ../development/python-modules/geoarrow-types { };
 
+  geoarrow-pyarrow = callPackage ../development/python-modules/geoarrow-pyarrow { };
+
   geocachingapi = callPackage ../development/python-modules/geocachingapi { };
 
   geocoder = callPackage ../development/python-modules/geocoder { };
