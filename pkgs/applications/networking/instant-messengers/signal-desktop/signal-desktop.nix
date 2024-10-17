d5baf434dc1f2f48493789729a086db94c0ce43f
teutat3s <10206665+teutat3s@users.noreply.github.com>
2024-10-17 14:04:31 +0200

    signal-desktop(linux): 7.28.0 -> 7.29.0
    https://github.com/signalapp/Signal-Desktop/releases/tag/v7.29.0

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "7.28.0";
+  version = "7.29.0";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-ZBnEIQvx3FLp1aoAKHDpNBptl9jK/z69OyAWLfk2fDw=";
+  hash = "sha256-TftTt9pN4EjgZs3N94TlCzXK64g1g+oGPx1ZLVX02VA=";
 }
