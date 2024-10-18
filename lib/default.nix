016f28ccd0ec052ff34ae26d3786c7dd2fb23cce
Robert Hensing <robert@roberthensing.nl>
2024-10-18 17:19:29 +0200

    lib: Remove lib.evalOptionValue
    lib.modules.evalOptionValue is still available

--- lib/default.nix
+++ lib/default.nix
@@ -143,7 +143,6 @@ let
       mkMergedOptionModule mkChangedOptionModule
       mkAliasOptionModule mkDerivedConfig doRename
       mkAliasOptionModuleMD;
-    evalOptionValue = lib.warn "External use of `lib.evalOptionValue` is deprecated. If your use case isn't covered by non-deprecated functions, we'd like to know more and perhaps support your use case well, instead of providing access to these low level functions. In this case please open an issue in https://github.com/nixos/nixpkgs/issues/." self.modules.evalOptionValue;
     inherit (self.options) isOption mkEnableOption mkSinkUndeclaredOptions
       mergeDefaultOption mergeOneOption mergeEqualOption mergeUniqueOption
       getValues getFiles
