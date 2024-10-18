5e2c6d7abdfab47e369ce2d4ec7451e276665846
VuiMuich <vuimuich@quantentunnel.de>
2024-10-18 09:45:32 +0200

    leftwm: 0.5.1->0.5.2
    Update to release Version `0.5.2` also remove patches and fixes specific
    to `0.5.1`

--- pkgs/applications/window-managers/leftwm/default.nix
+++ pkgs/applications/window-managers/leftwm/default.nix
@@ -11,25 +11,16 @@ in
 
 rustPlatform.buildRustPackage rec {
   pname = "leftwm";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchFromGitHub {
     owner = "leftwm";
     repo = "leftwm";
     rev = "refs/tags/${version}";
-    hash = "sha256-wn5DurPWFwSUtc5naEL4lBSQpKWTJkugpN9mKx+Ed2Y=";
+    hash = "sha256-hxiSmWf907bVOnzDO5A9dhJWDI8iLoUQII+MyjBE5M0=";
   };
 
-  cargoPatches = [
-    # This pacth can be removed with the next version bump, it just updates the `time` crate
-    ./update-time-crate.patch
-  ];
-
-  # To show the "correct" git-hash in `leftwm-check` we manually set the GIT_HASH env variable
-  # can be remove together with the above patch
-  GIT_HASH = "36609e0 patched";
-
-  cargoHash = "sha256-SNq76pTAPSUGVRp/+fwCjSMP/lKVzh6wU+WZW5n/yjg=";
+  cargoHash = "sha256-qYnTcAhUG1DnHyATU0dHx2FYalmRcs1PrkC4A2hacxs=";
 
   buildInputs = rpathLibs;
 
