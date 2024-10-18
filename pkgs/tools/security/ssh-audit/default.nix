8d21786f3defdf16dfa14d97fb3fa065d6abdc27
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-18 22:26:51 +0200

    ssh-audit: 3.2.0 -> 3.3.0
    Diff: https://github.com/jtesta/ssh-audit/compare/refs/tags/v3.2.0...v3.3.0
    
    Changelog: https://github.com/jtesta/ssh-audit/releases/tag/v3.3.0

--- pkgs/tools/security/ssh-audit/default.nix
+++ pkgs/tools/security/ssh-audit/default.nix
@@ -1,31 +1,37 @@
-{ lib
-, fetchFromGitHub
-, installShellFiles
-, nixosTests
-, python3Packages
+{
+  lib,
+  fetchFromGitHub,
+  installShellFiles,
+  nixosTests,
+  python3Packages,
 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "ssh-audit";
-  version = "3.2.0";
-  format = "setuptools";
-  outputs = [ "out" "man" ];
+  version = "3.3.0";
+  pyproject = true;
+
+  outputs = [
+    "out"
+    "man"
+  ];
 
   src = fetchFromGitHub {
     owner = "jtesta";
-    repo = pname;
+    repo = "ssh-audit";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-g5h0A1BJqzOZaSVUxyi7IsCcrbto4+7+HpiVjFZy50Y=";
+    hash = "sha256-sjYKQpn37zH3xpuIiZAjCn0DyLqqoQDwuz7PKDfkeTM=";
   };
 
+  build-system = with python3Packages; [ setuptools ];
+
   nativeBuildInputs = [ installShellFiles ];
+
   postInstall = ''
     installManPage $src/ssh-audit.1
   '';
 
-  nativeCheckInputs = with python3Packages; [
-    pytestCheckHook
-  ];
+  nativeCheckInputs = with python3Packages; [ pytestCheckHook ];
 
   passthru.tests = {
     inherit (nixosTests) ssh-audit;
@@ -33,11 +39,14 @@ python3Packages.buildPythonApplication rec {
 
   meta = with lib; {
     description = "Tool for ssh server auditing";
-    mainProgram = "ssh-audit";
     homepage = "https://github.com/jtesta/ssh-audit";
     changelog = "https://github.com/jtesta/ssh-audit/releases/tag/v${version}";
     license = licenses.mit;
     platforms = platforms.all;
-    maintainers = with maintainers; [ tv SuperSandro2000 ];
+    maintainers = with maintainers; [
+      tv
+      SuperSandro2000
+    ];
+    mainProgram = "ssh-audit";
   };
 }
