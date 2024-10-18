7aec06f39a8fcefd4a70762e9f2f4238ef40cd05
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-18 10:42:18 +0000

    jmol: 16.2.21 -> 16.3.1

--- pkgs/applications/science/chemistry/jmol/default.nix
+++ pkgs/applications/science/chemistry/jmol/default.nix
@@ -25,14 +25,14 @@ let
   };
 in
 stdenv.mkDerivation rec {
-  version = "16.2.21";
+  version = "16.3.1";
   pname = "jmol";
 
   src = let
     baseVersion = "${lib.versions.major version}.${lib.versions.minor version}";
   in fetchurl {
     url = "mirror://sourceforge/jmol/Jmol/Version%20${baseVersion}/Jmol%20${version}/Jmol-${version}-binary.tar.gz";
-    hash = "sha256-9gIOrHoy0JyoPXaHOfBDHCL+ykmmHNam+Um12sHqZsE=";
+    hash = "sha256-mjID6o+2yhzKd1g3MO4as1dA11nhessjzCDHSUyUQBw=";
   };
 
   patchPhase = ''
