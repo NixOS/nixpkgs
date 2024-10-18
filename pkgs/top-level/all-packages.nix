e0afb5e2796884ec0bbd2d95b0de5b0bf1c1010b
phanirithvij <phanirithvij2000@gmail.com>
2024-10-18 17:02:06 +0530

    russ: 0.4.0 -> 0.5.0, move to by-name
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13106,8 +13106,6 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
-  russ = callPackage ../applications/networking/feedreaders/russ { };
-
   tunnelto = callPackage ../tools/networking/tunnelto {
     inherit (darwin.apple_sdk.frameworks) Security;
   };
