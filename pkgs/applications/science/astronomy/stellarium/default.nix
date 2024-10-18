6a4503e8fbb86af501ef9c2185d36e63c3b98ba4
kilianar <mail@kilianar.de>
2024-10-18 11:20:50 +0200

    stellarium: make qtwebengine an optional dependency
    This commit makes `qtwebengine` an optional dependency for stellarium. Since
    `qtwebengine` currently fails to build on Darwin, it is now disabled by default
    for that platform.

--- pkgs/applications/science/astronomy/stellarium/default.nix
+++ pkgs/applications/science/astronomy/stellarium/default.nix
@@ -24,6 +24,7 @@
   xvfb-run,
   gitUpdater,
   md4c,
+  withQtWebEngine ? !stdenv.hostPlatform.isDarwin,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -59,7 +60,6 @@ stdenv.mkDerivation (finalAttrs: {
     qtpositioning
     qtmultimedia
     qtserialport
-    qtwebengine
     calcmysky
     qxlsx
     indilib
@@ -70,6 +70,9 @@ stdenv.mkDerivation (finalAttrs: {
   ]
   ++ lib.optionals stdenv.hostPlatform.isLinux [
     qtwayland
+  ]
+  ++ lib.optionals withQtWebEngine [
+    qtwebengine
   ];
 
   preConfigure = ''
