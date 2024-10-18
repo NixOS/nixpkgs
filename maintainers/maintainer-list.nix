93a09a2aa25922fad37017f65a9187a0cb145863
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2024-10-18 15:00:29 +0400

    maintainers: add meanwhile131

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16774,6 +16774,11 @@
     githubId = 14259816;
     name = "Abin Simon";
   };
+  meanwhile131 = {
+    name = "meanwhile131";
+    github = "meanwhile131";
+    githubId = 51206659;
+  };
   meatcar = {
     email = "nixpkgs@denys.me";
     github = "meatcar";
