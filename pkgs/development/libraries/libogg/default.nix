NixOS/nixpkgs

commit 04bf3a5a24294b530a985b95ec8aa984c7d339ae

libogg: allow dependents to find it with cmake

pkgs/development/libraries/libogg/default.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  cmake,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -17,6 +18,14 @@ stdenv.mkDerivation (finalAttrs: {
     "out"
     "dev"
     "doc"
+  ];
+
+  nativeBuildInputs = [
+    # Can also be built with the `./configure` script available in the release,
+    # however using cmake makes sure the resulting tree would include
+    # `OggConfig.cmake` and other cmake files useful when packages try to look it
+    # up with cmake.
+    cmake
   ];
 
   meta = {

commit 3841fc7d5106f39469567d8c9bfcd25f14ae47bb

libogg: build  shared library
Fixes a regression from 04bf3a5a24294b530a985b95ec8aa984c7d339ae

pkgs/development/libraries/libogg/default.nix
@@ -28,6 +28,10 @@ stdenv.mkDerivation (finalAttrs: {
     cmake
   ];
 
+  cmakeFlags = [
+    (lib.cmakeBool "BUILD_SHARED_LIBS" (!stdenv.hostPlatform.isStatic))
+  ];
+
   meta = {
     description = "Media container library to manipulate Ogg files";
     longDescription = ''