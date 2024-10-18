46e20c864151901c883f8bbf3cdcc79bb1802135
Elliot Cameron <nixpkgs-commits@deshaw.com>
2024-10-18 15:47:39 -0400

    python312Packages.remctl: init at 3.18

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13555,6 +13555,10 @@ self: super: with self; {
 
   remarshal = callPackage ../development/python-modules/remarshal { };
 
+  remctl = callPackage ../development/python-modules/remctl {
+    remctl-c = pkgs.remctl;
+  };
+
   remi = callPackage ../development/python-modules/remi { };
 
   remote-pdb = callPackage ../development/python-modules/remote-pdb { };
