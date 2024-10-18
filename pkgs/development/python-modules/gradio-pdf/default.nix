0c76aae35f0739404cf02298ffd5225ad5561579
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-10-18 20:16:05 +0200

    python312Packages.gradio-pdf: 0.0.15 -> 0.0.17

--- pkgs/development/python-modules/gradio-pdf/default.nix
+++ pkgs/development/python-modules/gradio-pdf/default.nix
@@ -11,16 +11,16 @@
 
 buildPythonPackage rec {
   pname = "gradio-pdf";
-  version = "0.0.15";
-  format = "pyproject";
+  version = "0.0.17";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "gradio_pdf";
     inherit version;
-    hash = "sha256-R+4CDJo84/+PUTTxoz9CsZlHUVsldgjufWsRkALrfN4=";
+    hash = "sha256-LoVcwE7eGcK5Nc6qKTnrnI+rNlsDbekhKUP+Fzq2SQ8=";
   };
 
-  nativeBuildInputs = [
+  build-system = [
     hatch-fancy-pypi-readme
     hatch-requirements-txt
     hatchling
@@ -36,10 +36,10 @@ buildPythonPackage rec {
   # tested in `gradio`
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     description = "Python library for easily interacting with trained machine learning models";
     homepage = "https://pypi.org/project/gradio-pdf/";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ pbsds ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ pbsds ];
   };
 }
