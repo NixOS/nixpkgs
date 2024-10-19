8c64772d13ce2b36997412dc421f634e9af5a827
euxane <euxane.trangirard@pacien.net>
2024-10-19 16:34:15 +0200

    visidata: newScope -> python3Packages.callPackage

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13339,8 +13339,7 @@ with pkgs;
 
   vimwiki-markdown = python3Packages.callPackage ../tools/misc/vimwiki-markdown { };
 
-  visidata = (newScope python3Packages) ../applications/misc/visidata {
-  };
+  visidata = python3Packages.callPackage ../applications/misc/visidata { };
 
   vit = callPackage ../applications/misc/vit { };
 
