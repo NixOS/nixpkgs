660836b95cd34befad709bdaa9a4772595c2dfc1
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-10-19 15:11:51 +0200

    msolve: format with nixfmt

--- pkgs/by-name/ms/msolve/package.nix
+++ pkgs/by-name/ms/msolve/package.nix
@@ -1,10 +1,11 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, autoreconfHook
-, flint
-, gmp
-, mpfr
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  flint,
+  gmp,
+  mpfr,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -34,13 +35,13 @@ stdenv.mkDerivation (finalAttrs: {
 
   doCheck = true;
 
-  meta = with lib; {
+  meta = {
     description = "Library for polynomial system solving through algebraic methods";
     mainProgram = "msolve";
     homepage = "https://msolve.lip6.fr";
     changelog = "https://github.com/algebraic-solving/msolve/releases/tag/${finalAttrs.src.rev}";
-    license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ wegank ];
-    platforms = platforms.unix;
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ wegank ];
+    platforms = lib.platforms.unix;
   };
 })
