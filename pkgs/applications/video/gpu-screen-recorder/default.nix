6921f9f919cc8f87dbd90299083477e0d2b9a1a9
Keenan Weaver <keenanweaver@protonmail.com>
2024-10-18 19:09:20 -0500

    gpu-screen-recorder: 4.2.1 -> 4.2.3

--- pkgs/applications/video/gpu-screen-recorder/default.nix
+++ pkgs/applications/video/gpu-screen-recorder/default.nix
@@ -26,11 +26,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gpu-screen-recorder";
-  version = "4.2.1";
+  version = "4.2.3";
 
   src = fetchurl {
     url = "https://dec05eba.com/snapshot/gpu-screen-recorder.git.${finalAttrs.version}.tar.gz";
-    hash = "sha256-eCjAlPEg8lkL8T0lgxr0F8ouFGwqfsRxDSQuG6RbpZE=";
+    hash = "sha256-M2bk1WwLlbwspEoPIRMix17ihi72UuRWwiKBdPfim2M=";
   };
 
   sourceRoot = ".";
