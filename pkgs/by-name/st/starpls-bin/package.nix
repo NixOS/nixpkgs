f7e6035cfec8f6b4c47eb2fa7c14f7b497dbdb10
Aaron Jheng <wentworth@outlook.com>
2024-10-19 07:02:04 +0000

    starpls-bin: 0.1.14 -> 0.1.15

--- pkgs/by-name/st/starpls-bin/package.nix
+++ pkgs/by-name/st/starpls-bin/package.nix
@@ -1,19 +1,24 @@
-{ lib, stdenv, fetchurl, autoPatchelfHook }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  testers,
+  starpls-bin,
+}:
 
+let
+  manifest = lib.importJSON ./manifest.json;
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "starpls-bin";
-  version = "0.1.14";
+  version = manifest.version;
 
-  src = {
-    x86_64-linux = fetchurl {
-      url = "https://github.com/withered-magic/starpls/releases/download/v${finalAttrs.version}/starpls-linux-amd64";
-      hash = "sha256-PYU+Jv3uaJqJKw6zSNOPl+NlIQgfm38cOrRqTdNXY+8=";
-    };
-    aarch64-darwin = fetchurl {
-      url = "https://github.com/withered-magic/starpls/releases/download/v${finalAttrs.version}/starpls-darwin-arm64";
-      hash = "sha256-9d1ybebguEUJu2PvMcToQEd8M4ajRrQUvBZqS6o0sbw=";
-    };
-  }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+  src =
+    let
+      system = stdenv.hostPlatform.system;
+    in
+    fetchurl (manifest.assets.${system} or (throw "Unsupported system: ${system}"));
 
   dontUnpack = true;
   dontConfigure = true;
@@ -31,12 +36,22 @@ stdenv.mkDerivation (finalAttrs: {
     install -D $src $out/bin/starpls
   '';
 
-  meta = with lib; {
+  passthru = {
+    tests.version = testers.testVersion {
+      package = starpls-bin;
+      command = "starpls version";
+      version = "v${finalAttrs.version}";
+    };
+    updateScript = ./update.py;
+  };
+
+  meta = {
+    description = "Language server for Starlark";
     homepage = "https://github.com/withered-magic/starpls";
-    description = "A language server for Starlark";
-    license = licenses.asl20;
-    platforms = [ "aarch64-darwin" "x86_64-linux" ];
-    maintainers = with maintainers; [ aaronjheng ];
+    license = lib.licenses.asl20;
+    platforms = builtins.attrNames manifest.assets;
+    maintainers = with lib.maintainers; [ aaronjheng ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     mainProgram = "starpls";
     sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
   };
