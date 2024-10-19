086e5edd692cd42343ff43b0f0261c3d275742ce
jacekpoz <jacekpoz@proton.me>
2024-10-19 20:29:37 +0200

    waycorner: add missing updateScript

--- pkgs/by-name/wa/waycorner/package.nix
+++ pkgs/by-name/wa/waycorner/package.nix
@@ -5,6 +5,7 @@
   pkg-config,
   fetchFromGitHub,
   wayland,
+  gitUpdater,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "waycorner";
@@ -29,6 +30,8 @@ rustPlatform.buildRustPackage rec {
     wrapProgram $out/bin/waycorner \
       --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ wayland ]}
   '';
+
+  passthru.updateScript = gitUpdater { };
 
   meta = {
     description = "Hot corners for Wayland";
