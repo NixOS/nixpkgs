3b50cae2b01005c1505c64c67b6ecdfe5719e156
a-kenji <aks.kenji@protonmail.com>
2024-10-19 16:41:43 +0200

    desed: 1.2.1-unstable-2024-09-06 -> 1.2.2

--- pkgs/by-name/de/desed/package.nix
+++ pkgs/by-name/de/desed/package.nix
@@ -4,21 +4,21 @@
   fetchFromGitHub,
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "desed";
-  version = "1.2.1-unstable-2024-09-06";
+  version = "1.2.2";
 
   src = fetchFromGitHub {
     owner = "SoptikHa2";
     repo = "desed";
-    rev = "master";
-    hash = "sha256-iCpEfefXXognk4YI1LLb3mwgaqMw4m3haq/gdS1JbQU=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-FL9w+XdClLBCRp+cLqDzTVj8j9LMUp8jZ6hiG4KvIds=";
   };
 
-  cargoHash = "sha256-z2qv394C0GhQC21HuLyvlNjrM65KFEZh1XLj+Y/B9ZM=";
+  cargoHash = "sha256-inH8fUpUR0WXYY2JX72evZqVp3GlnGKBBlrbai/fU6U=";
 
   meta = {
-    changelog = "https://github.com/SoptikHa2/desed/releases/tag/v1.2.1";
+    changelog = "https://github.com/SoptikHa2/desed/releases/tag/v${version}";
     description = "Debugger for Sed: demystify and debug your sed scripts, from comfort of your terminal. ";
     homepage = "https://github.com/SoptikHa2/desed";
     license = lib.licenses.gpl3Only;
