1bfb62c3b28886e781308929999310ebd2223745
aleksana <me@aleksana.moe>
2024-10-19 17:54:53 +0800

    eboard: remove

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34495,8 +34495,6 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Foundation AudioUnit AudioToolbox;
   };
 
-  eboard = callPackage ../games/eboard { };
-
   ecwolf = callPackage ../games/ecwolf { };
 
   egoboo = callPackage ../games/egoboo { };
