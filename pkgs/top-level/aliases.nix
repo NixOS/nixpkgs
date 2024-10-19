c6cf2547a6e0d015d8fa88c9368e4f517aff5a6c
aleksana <me@aleksana.moe>
2024-10-19 18:49:54 +0800

    qxw: remove

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -986,6 +986,7 @@ mapAliases {
   quicklispPackagesFor = throw "Lisp packages have been redesigned. See 'lisp-modules' in the nixpkgs manual."; # Added 2024-05-07
   quicklispPackagesGCL = throw "Lisp packages have been redesigned. See 'lisp-modules' in the nixpkgs manual."; # Added 2024-05-07
   quicklispPackagesSBCL = throw "Lisp packages have been redesigned. See 'lisp-modules' in the nixpkgs manual."; # Added 2024-05-07
+  qxw = throw "'qxw' has been removed due to lack of maintenance upstream. Consider using 'crosswords' instead"; # Added 2024-10-19
 
   ### R ###
 
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32058,8 +32058,6 @@ with pkgs;
     inherit (__splicedPackages.libsForQt5) qtbase qtwebengine wrapQtAppsHook qtwayland;
   };
 
-  qxw = callPackage ../applications/editors/qxw { };
-
   rakarrack = callPackage ../applications/audio/rakarrack {
     fltk = fltk13;
   };
