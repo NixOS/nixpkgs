19a14987f5595e5f4a4055ffc197df078d024ad0
Steven Keuchel <steven.keuchel@gmail.com>
2024-10-20 16:37:19 +0200

    abseil-cpp_202407: apply pkgconfig fix for darwin

--- pkgs/development/libraries/abseil-cpp/202407.nix
+++ pkgs/development/libraries/abseil-cpp/202407.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , fetchFromGitHub
+, fetchpatch
 , cmake
 , gtest
 , static ? stdenv.hostPlatform.isStatic
@@ -17,6 +18,23 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "refs/tags/${finalAttrs.version}";
     hash = "sha256-51jpDhdZ0n+KLmxh8KVaTz53pZAB0dHjmILFX+OLud4=";
   };
+
+  patches = [
+    # Don't match -Wnon-virtual-dtor in the "flags are needed to suppress
+    # Needed to cleanly apply the #1738 fix below.
+    # https://github.com/abseil/abseil-cpp/issues/1737
+    (fetchpatch {
+      url = "https://github.com/abseil/abseil-cpp/commit/9cb5e5d15c142e5cc43a2c1db87c8e4e5b6d38a5.patch";
+      hash = "sha256-PTNmNJMk42Omwek0ackl4PjxifDP/+GaUitS60l+VB0=";
+    })
+
+    # Fix shell option group handling in pkgconfig files
+    # https://github.com/abseil/abseil-cpp/pull/1738
+    (fetchpatch {
+      url = "https://github.com/abseil/abseil-cpp/commit/bd0c9c58cac4463d96b574de3097422bb78215a8.patch";
+      hash = "sha256-fB9pvkyNBXoDKLrVaNwliqrWEPTa2Y6OJMe2xgl5IBc=";
+    })
+  ];
 
   cmakeFlags = [
     "-DABSL_BUILD_TEST_HELPERS=ON"
