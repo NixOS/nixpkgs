e270409cf8f37be21b10251669a4540c1390b438
networkException <git@nwex.de>
2024-10-20 20:04:44 +0200

    ungoogled-chromium: 129.0.6668.100-1 -> 130.0.6723.58-1
    https://chromereleases.googleblog.com/2024/10/stable-channel-update-for-desktop_15.html
    
    This update includes 17 security fixes.
    
    CVEs:
    CVE-2024-9954 CVE-2024-9955 CVE-2024-9956 CVE-2024-9957 CVE-2024-9958
    CVE-2024-9959 CVE-2024-9960 CVE-2024-9961 CVE-2024-9962 CVE-2024-9963
    CVE-2024-9964 CVE-2024-9965 CVE-2024-9966
    
    Co-authored-by: emilylange <git@emilylange.de>
    (cherry picked from commit 855780b11d252447d8eed7b6ecea387f226ac115)

--- pkgs/applications/networking/browsers/chromium/upstream-info.nix
+++ pkgs/applications/networking/browsers/chromium/upstream-info.nix
@@ -21,17 +21,17 @@
   ungoogled-chromium = {
     deps = {
       gn = {
-        hash = "sha256-8o3rDdojqVHMQCxI2T3MdJOXKlW3XX7lqpy3zWhJiaA=";
-        rev = "d010e218ca7077928ad7c9e9cc02fe43b5a8a0ad";
+        hash = "sha256-iNXRq3Mr8+wmY1SR4sV7yd2fDiIZ94eReelwFI0UhGU=";
+        rev = "20806f79c6b4ba295274e3a589d85db41a02fdaa";
         url = "https://gn.googlesource.com/gn";
-        version = "2024-08-19";
+        version = "2024-09-09";
       };
       ungoogled-patches = {
-        hash = "sha256-kvpLE6SbXFur5xi1C8Ukvm4OoU5YB8PQCJdiakhFSAM=";
-        rev = "129.0.6668.100-1";
+        hash = "sha256-M+aJ1hhFV88lBBPl9xBYpYRut7yHa/HJYXoclckaZVM=";
+        rev = "130.0.6723.58-1";
       };
     };
-    hash = "sha256-LOZ9EPw7VgBNEV7Wxb8H5WfSYTTWOL8EDP91uCrZAsA=";
-    version = "129.0.6668.100";
+    hash = "sha256-w1xQr+B7ROeCqBRN+M9vmh45YTRqVfjDYSsN5saDuDo=";
+    version = "130.0.6723.58";
   };
 }
