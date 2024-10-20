99add84d3776aec0f4477164b7e6004ae39273e6
diniamo <diniamo53@gmail.com>
2024-10-20 20:32:16 +0200

    nixos/transmission: adjust `settings.umask`
    * nixos/transmission: change `settings.umask` to be a string
    
    * nixos/transmission: change umask's default value to match the one in
    Transmission's documentation
    
    * nixos/doc: add entry for transmission umask option changes

--- nixos/modules/services/torrent/transmission.nix
+++ nixos/modules/services/torrent/transmission.nix
@@ -133,15 +133,13 @@ in
             description = "Executable to be run at torrent completion.";
           };
           options.umask = mkOption {
-            type = types.int;
-            default = 2;
+            type = types.str;
+            default = "022";
             description = ''
               Sets transmission's file mode creation mask.
               See the umask(2) manpage for more information.
               Users who want their saved torrents to be world-writable
-              may want to set this value to 0.
-              Bear in mind that the json markup language only accepts numbers in base 10,
-              so the standard umask(2) octal notation "022" is written in settings.json as 18.
+              may want to set this value to `000`.
             '';
           };
           options.utp-enabled = mkOption {
