44f8d31fd6ae860ba94e8a5025fe21987d81ab06
Felix Singer <felixsinger@posteo.net>
2024-10-20 18:53:30 +0200

    bluespec: Move package definition into by-name structure
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14323,7 +14323,7 @@ with pkgs;
     inherit (python3Packages) filecheck;
   };
 
-  bluespec = callPackage ../development/compilers/bluespec {
+  bluespec = callPackage ../by-name/bl/bluespec/package.nix {
     gmp-static = gmp.override { withStatic = true; };
   };
 
