55b61be4d48d919b4655a802ba7fe76d195fb782
расходный материал <78630225+aucub@users.noreply.github.com>
2024-10-21 06:53:37 +0800

    libportal: 0.7.1 → 0.8.1. Add libportal-qt6 variant
    https://github.com/flatpak/libportal/releases/tag/0.8.0
    https://github.com/flatpak/libportal/releases/tag/0.8.1
    
    Co-authored-by: dr56ekgbb <dr56ekgbb@mozmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9742,6 +9742,7 @@ with pkgs;
   libportal-gtk3 = libportal.override { variant = "gtk3"; };
   libportal-gtk4 = libportal.override { variant = "gtk4"; };
   libportal-qt5 = libportal.override { variant = "qt5"; };
+  libportal-qt6 = libportal.override { variant = "qt6"; };
 
   libmicrodns = callPackage ../development/libraries/libmicrodns { };
 
