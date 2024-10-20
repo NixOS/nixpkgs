ea6a02a0d178ad0e510ba373ab5b883360b7bf5d
Robert Schütz <mail@dotlambda.de>
2024-10-20 16:07:42 -0700

    electron-chromedriver_33: init at 33.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16930,7 +16930,9 @@ with pkgs;
     electron-chromedriver_29
     electron-chromedriver_30
     electron-chromedriver_31
-    electron-chromedriver_32;
+    electron-chromedriver_32
+    electron-chromedriver_33
+    ;
 
   electron_24 = electron_24-bin;
   electron_27 = electron_27-bin;
