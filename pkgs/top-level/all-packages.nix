fed0fcf7ecd237ec6907c1b0d6d1ceeda10db10f
Steffen Vogel <post@steffenvogel.de>
2024-10-20 16:32:22 +0200

    alice-lg: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1604,8 +1604,6 @@ with pkgs;
   inherit (recurseIntoAttrs (callPackage ../tools/package-management/akku { }))
     akku akkuPackages;
 
-  alice-lg = callPackage ../servers/alice-lg{ };
-
   alice-tools = callPackage ../tools/games/alice-tools {
     withGUI = false;
   };
