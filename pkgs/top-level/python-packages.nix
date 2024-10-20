f12a3771ac270d1abb197936232061ddb0129301
Steffen Vogel <post@steffenvogel.de>
2024-10-20 17:46:23 +0200

    stravaweblib: init at 0.0.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15086,6 +15086,8 @@ self: super: with self; {
 
   stravalib = callPackage ../development/python-modules/stravalib { };
 
+  stravaweblib = callPackage ../development/python-modules/stravaweblib { };
+
   strawberry-graphql = callPackage ../development/python-modules/strawberry-graphql { };
 
   strct = callPackage ../development/python-modules/strct { };
