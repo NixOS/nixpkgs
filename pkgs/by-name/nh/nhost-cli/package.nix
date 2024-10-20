5577994ed4b1ea4dc9b2e48dabd5a86af3e62af1
Moraxyc <i@qaq.li>
2024-10-20 18:05:04 +0800

    nhost-cli: 1.24.0 -> 1.24.5

--- pkgs/by-name/nh/nhost-cli/package.nix
+++ pkgs/by-name/nh/nhost-cli/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "nhost-cli";
-  version = "1.24.0";
+  version = "1.24.5";
 
   src = fetchFromGitHub {
     owner = "nhost";
     repo = "cli";
     rev = "v${version}";
-    hash = "sha256-8kul3tiwEJF6Pu1os29MteeYP+85X9hH12MojMpW9bY=";
+    hash = "sha256-/NkegGp+JoJWPIkO3YKp/aZ6Yp5tcjnRCONY0GHe+HI=";
   };
 
   vendorHash = null;
@@ -27,9 +27,12 @@ buildGoModule rec {
     mv $out/bin/cli $out/bin/nhost
   '';
 
+  checkFlags = [ "-skip=^TestMakeJSONRequest$" ];
+
   meta = {
-    description = "A tool for setting up a local development environment for Nhost";
+    description = "Tool for setting up a local development environment for Nhost";
     homepage = "https://github.com/nhost/cli";
+    changelog = "https://github.com/nhost/cli/releases/tag/v${version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ moraxyc ];
     mainProgram = "nhost";
