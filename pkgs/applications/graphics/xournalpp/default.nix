bb588507b3d34032b29f39a6dcdb1949e0133f21
Ryan Omasta <git@ryand.ca>
2024-10-20 14:39:37 -0600

    xournalpp: format using nixfmt-rfc-style

--- pkgs/applications/graphics/xournalpp/default.nix
+++ pkgs/applications/graphics/xournalpp/default.nix
@@ -1,28 +1,31 @@
-{ lib, stdenv
-, fetchFromGitHub
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
 
-, cmake
-, gettext
-, wrapGAppsHook3
-, pkg-config
+  cmake,
+  gettext,
+  wrapGAppsHook3,
+  pkg-config,
 
-, adwaita-icon-theme
-, alsa-lib
-, binutils
-, glib
-, gsettings-desktop-schemas
-, gtk3
-, gtksourceview4
-, librsvg
-, libsndfile
-, libxml2
-, libzip
-, pcre
-, poppler
-, portaudio
-, zlib
-# plugins
-, withLua ? true, lua
+  adwaita-icon-theme,
+  alsa-lib,
+  binutils,
+  glib,
+  gsettings-desktop-schemas,
+  gtk3,
+  gtksourceview4,
+  librsvg,
+  libsndfile,
+  libxml2,
+  libzip,
+  pcre,
+  poppler,
+  portaudio,
+  zlib,
+  # plugins
+  withLua ? true,
+  lua,
 }:
 
 stdenv.mkDerivation rec {
@@ -41,12 +44,18 @@ stdenv.mkDerivation rec {
       --replace-fail "addr2line" "${binutils}/bin/addr2line"
   '';
 
-  nativeBuildInputs = [ cmake gettext pkg-config wrapGAppsHook3 ];
+  nativeBuildInputs = [
+    cmake
+    gettext
+    pkg-config
+    wrapGAppsHook3
+  ];
 
   buildInputs =
     lib.optionals stdenv.hostPlatform.isLinux [
       alsa-lib
-    ] ++ [
+    ]
+    ++ [
       glib
       gsettings-desktop-schemas
       gtk3
@@ -72,11 +81,11 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Xournal++ is a handwriting Notetaking software with PDF annotation support";
-    homepage    = "https://xournalpp.github.io/";
-    changelog   = "https://github.com/xournalpp/xournalpp/blob/v${version}/CHANGELOG.md";
-    license     = licenses.gpl2Plus;
+    homepage = "https://xournalpp.github.io/";
+    changelog = "https://github.com/xournalpp/xournalpp/blob/v${version}/CHANGELOG.md";
+    license = licenses.gpl2Plus;
     maintainers = with maintainers; [ sikmir ];
-    platforms   = platforms.unix;
+    platforms = platforms.unix;
     mainProgram = "xournalpp";
   };
 }
