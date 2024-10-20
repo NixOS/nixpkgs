d242c9cf4858bdfe059eed7fe332ac164165f90d
William <baduhai@proton.me>
2024-10-20 18:10:58 -0300

    maintainers: change baduhai's name

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2309,7 +2309,7 @@
     email = "baduhai@pm.me";
     github = "baduhai";
     githubId = 31864305;
-    name = "William";
+    name = "William Hai";
   };
   baitinq = {
     email = "manuelpalenzuelamerino@gmail.com";
