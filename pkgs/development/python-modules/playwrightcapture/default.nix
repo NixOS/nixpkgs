f4a29d61d2300e364be149df3894c61a47aa94b1
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-21 21:54:34 +0200

    python312Packages.playwrightcapture: 1.26.1 -> 1.26.2
    Diff: https://github.com/Lookyloo/PlaywrightCapture/compare/refs/tags/v1.26.1...v1.26.2
    
    Changelog: https://github.com/Lookyloo/PlaywrightCapture/releases/tag/v1.26.2

--- pkgs/development/python-modules/playwrightcapture/default.nix
+++ pkgs/development/python-modules/playwrightcapture/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "playwrightcapture";
-  version = "1.26.1";
+  version = "1.26.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     owner = "Lookyloo";
     repo = "PlaywrightCapture";
     rev = "refs/tags/v${version}";
-    hash = "sha256-zVwsRTsxic0/K6HyTLVRYCnBpSIF9Ly0TRJS90nADTg=";
+    hash = "sha256-JSgWOHMwjxStwMxRe4sFYLeRRgOkfWXlIpqeDfGDSNQ=";
   };
 
   pythonRelaxDeps = [
