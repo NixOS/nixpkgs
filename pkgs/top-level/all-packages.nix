630c2bc16a1c0ed2546558b06f5da1ce07ffe5c3
Federico Beffa <beffa@fbengineering.ch>
2024-10-21 14:01:30 +0200

    xschem: move to name based directory

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36458,8 +36458,6 @@ with pkgs;
 
   xoscope = callPackage ../applications/science/electronics/xoscope { };
 
-  xschem = callPackage ../applications/science/electronics/xschem { };
-
   xyce = callPackage ../applications/science/electronics/xyce { };
 
   xyce-parallel = callPackage ../applications/science/electronics/xyce {
