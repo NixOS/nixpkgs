a707a2b221f0becfbb9561b8a4b10c37541813eb
Victor Engmark <victor@engmark.name>
2024-10-21 13:33:16 +1300

    mutmut: Format with nixfmt-rfc-style

--- pkgs/by-name/mu/mutmut/package.nix
+++ pkgs/by-name/mu/mutmut/package.nix
@@ -1,39 +1,50 @@
-{ lib
-, fetchFromGitHub
-, python3
-, testers
+{
+  lib,
+  fetchFromGitHub,
+  python3,
+  testers,
 }:
 
-let self = with python3.pkgs; buildPythonApplication rec {
-  pname = "mutmut";
-  version = "2.2.0";
+let
+  self =
+    with python3.pkgs;
+    buildPythonApplication rec {
+      pname = "mutmut";
+      version = "2.2.0";
 
-  src = fetchFromGitHub {
-    repo = pname;
-    owner = "boxed";
-    rev = version;
-    hash = "sha256-G+OL/9km2iUeZ1QCpU73CIWVWMexcs3r9RdCnAsESnY=";
-  };
+      src = fetchFromGitHub {
+        repo = pname;
+        owner = "boxed";
+        rev = version;
+        hash = "sha256-G+OL/9km2iUeZ1QCpU73CIWVWMexcs3r9RdCnAsESnY=";
+      };
 
-  postPatch = ''
-    substituteInPlace requirements.txt --replace 'junit-xml==1.8' 'junit-xml==1.9'
-  '';
+      postPatch = ''
+        substituteInPlace requirements.txt --replace 'junit-xml==1.8' 'junit-xml==1.9'
+      '';
 
-  disabled = pythonOlder "3.7";
+      disabled = pythonOlder "3.7";
 
-  doCheck = false;
+      doCheck = false;
 
-  propagatedBuildInputs = [ click glob2 parso pony junit-xml ];
+      propagatedBuildInputs = [
+        click
+        glob2
+        parso
+        pony
+        junit-xml
+      ];
 
-  passthru.tests.version = testers.testVersion { package = self; };
+      passthru.tests.version = testers.testVersion { package = self; };
 
-  meta = with lib; {
-    description = "mutation testing system for Python, with a strong focus on ease of use";
-    mainProgram = "mutmut";
-    homepage = "https://github.com/boxed/mutmut";
-    changelog = "https://github.com/boxed/mutmut/blob/${version}/HISTORY.rst";
-    license = licenses.bsd3;
-    maintainers = with maintainers; [ synthetica ];
-  };
-};
-in self
+      meta = with lib; {
+        description = "mutation testing system for Python, with a strong focus on ease of use";
+        mainProgram = "mutmut";
+        homepage = "https://github.com/boxed/mutmut";
+        changelog = "https://github.com/boxed/mutmut/blob/${version}/HISTORY.rst";
+        license = licenses.bsd3;
+        maintainers = with maintainers; [ synthetica ];
+      };
+    };
+in
+self
