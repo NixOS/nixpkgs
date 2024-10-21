144be9514540ca778ebb07aa9ee752e39d341486
Markus Kowalewski <markus.kowalewski@gmail.com>
2024-10-21 14:39:00 +0200

    pythonPackages.mpi4py: 3.1.6-unstable-2024-07-08 -> 4.0.1

--- pkgs/development/python-modules/mpi4py/default.nix
+++ pkgs/development/python-modules/mpi4py/default.nix
@@ -12,16 +12,14 @@
 
 buildPythonPackage rec {
   pname = "mpi4py";
-  # See https://github.com/mpi4py/mpi4py/issues/386 . Part of the changes since
-  # the last release include Python 3.12 fixes.
-  version = "3.1.6-unstable-2024-07-08";
+  version = "4.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     repo = "mpi4py";
     owner = "mpi4py";
-    rev = "e9a59719bbce1b9c351e1e30ecd3be3b459e97cd";
-    hash = "sha256-C/nidWGr8xsLV73u7HRtnXoQgYmoRJkD45DFrdXXTPI=";
+    rev = version;
+    hash = "sha256-pH4z+hyoFOSAUlXv9EKO54/SM5HyLxv7B+18xBidH2Q=";
   };
 
   build-system = [
@@ -40,10 +38,7 @@ buildPythonPackage rec {
     openssh
     mpiCheckPhaseHook
   ];
-  # Most tests pass, (besides `test_spawn.py`), but when reaching ~80% tests
-  # progress, an orted process hangs and the tests don't finish. This issue is
-  # probably due to the sandbox.
-  doCheck = false;
+  doCheck = true;
   disabledTestPaths = [
     # Almost all tests in this file fail (TODO: Report about this upstream..)
     "test/test_spawn.py"
