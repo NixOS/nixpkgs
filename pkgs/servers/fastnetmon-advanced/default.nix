d458ee7157517244ef6dc310709399b08a4bf1df
Yureka <yureka@cyberchaos.dev>
2024-10-21 08:07:39 +0200

    fastnetmon-advanced: 2.0.363 -> 2.0.367
    Change-Id: Ic89f3e168df4b5e62e2c8c20f451daac2b383ebc

--- pkgs/servers/fastnetmon-advanced/default.nix
+++ pkgs/servers/fastnetmon-advanced/default.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "fastnetmon-advanced";
-  version = "2.0.363";
+  version = "2.0.367";
 
   src = fetchurl {
     url = "https://repo.fastnetmon.com/fastnetmon_ubuntu_jammy/pool/fastnetmon/f/fastnetmon/fastnetmon_${version}_amd64.deb";
-    hash = "sha256-2AKUNPQ7OzuYOolJHwTnWHzB4Qpwun/77+dFCN/cE98=";
+    hash = "sha256-D/4kkT6ehEmlfRUeP1uLuO/hd9ZrMBJSKF5DKYXOPxs=";
   };
 
   nativeBuildInputs = [
@@ -27,18 +27,8 @@ stdenv.mkDerivation rec {
     ar xf $src
     tar xf data.tar.xz
 
-    # both clickhouse 2.0.0 and 2.3.0 libs are included, without versioning it will by
-    # default choose the first it finds, but we need 2.3.0 otherwise the fastnetmon
-    # binary will be missing symbols
-    rm -r opt/fastnetmon/libraries/libclickhouse_2_0_0
-
     # unused libraries, which have additional dependencies
     rm opt/fastnetmon/libraries/gcc1210/lib/libgccjit.so.0.0.1
-    rm opt/fastnetmon/libraries/poco_1_10_0/lib/libPocoCryptod.so.70
-    rm opt/fastnetmon/libraries/poco_1_10_0/lib/libPocoCrypto.so.70
-    rm opt/fastnetmon/libraries/poco_1_10_0/lib/libPocoJWTd.so.70
-    rm opt/fastnetmon/libraries/poco_1_10_0/lib/libPocoJWT.so.70
-    rm opt/fastnetmon/libraries/wkhtmltopdf-0.12.3/wkhtmltox/lib/libwkhtmltox.so.0.12.3
   '';
 
   installPhase = ''
