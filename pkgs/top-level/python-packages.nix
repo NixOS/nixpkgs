d325750efbdc2eba81fdf343738605ec683b3374
Noa Aarts <noa@voorwaarts.nl>
2024-10-21 21:50:33 +0200

    pythonPackages.world-bank-data: init at 0.1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17576,6 +17576,8 @@ self: super: with self; {
 
   wordfreq = callPackage ../development/python-modules/wordfreq { };
 
+  world-bank-data = callPackage ../development/python-modules/world-bank-data { };
+
   worldengine = callPackage ../development/python-modules/worldengine { };
 
   wrapio = callPackage ../development/python-modules/wrapio { };
