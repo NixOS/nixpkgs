f3eae108306b0ecf41517077b90593d8ea6e1748
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-22 00:10:24 +0200

    pretalx.plugins.media-ccc-de: 1.3.0 -> 1.4.0
    https://github.com/pretalx/pretalx-media-ccc-de/compare/v1.3.0...v1.4.0

--- pkgs/by-name/pr/pretalx/plugins/media-ccc-de.nix
+++ pkgs/by-name/pr/pretalx/plugins/media-ccc-de.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "pretalx-media-ccc-de";
-  version = "1.3.0";
+  version = "1.4.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pretalx";
     repo = "pretalx-media-ccc-de";
     rev = "v${version}";
-    hash = "sha256-Cr9qbkb1VOH2EtDLSA5jmLiCnn1ICdvHnmTugCvHLc0=";
+    hash = "sha256-U+26hit4xXUzN8JT3WL+iGohqomX1ENb+ihM9IT1XWQ=";
   };
 
   build-system = [ setuptools ];
