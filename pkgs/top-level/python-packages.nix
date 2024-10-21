89acd969efae30e7ea420cfaed48b212da6644bd
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-10-21 16:34:02 +0200

    python311Packages.oslo-utils: use only qemu-utils

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9566,9 +9566,7 @@ self: super: with self; {
 
   oslo-serialization = callPackage ../development/python-modules/oslo-serialization { };
 
-  oslo-utils = callPackage ../development/python-modules/oslo-utils {
-    qemu = pkgs.qemu;
-  };
+  oslo-utils = callPackage ../development/python-modules/oslo-utils { };
 
   oslotest = callPackage ../development/python-modules/oslotest { };
 
