fcb53106b26a284a7097976ee4c8f5b70cdf53eb
aleksana <me@aleksana.moe>
2024-10-21 15:46:26 +0800

    gweled: unstable-2021-02-11 -> 1.0-beta1

--- pkgs/by-name/gw/gweled/package.nix
+++ pkgs/by-name/gw/gweled/package.nix
@@ -1,57 +1,51 @@
 {
   lib,
   stdenv,
-  fetchbzr,
-  gettext,
-  gtk2,
-  wrapGAppsHook3,
-  autoreconfHook,
+  fetchgit,
+  meson,
+  ninja,
   pkg-config,
-  libmikmod,
-  librsvg,
-  libcanberra-gtk2,
-  hicolor-icon-theme,
+  wrapGAppsHook3,
+  desktop-file-utils,
+  clutter,
+  clutter-gtk,
+  gsound,
+  libgnome-games-support,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gweled";
-  version = "unstable-2021-02-11";
+  version = "1.0-beta1";
 
-  src = fetchbzr {
-    url = "lp:gweled";
-    rev = "108";
-    hash = "sha256-rM4dgbYfSrVqZwi+xzKuEtmtjK3HVvqeutmni1vleLo=";
+  src = fetchgit {
+    url = "https://git.launchpad.net/gweled";
+    rev = "refs/tags/${finalAttrs.version}";
+    hash = "sha256-cm1z6l2tfYBFVFcvsnQ6cI3pQDnJMzn6SUC20gnBF5w=";
   };
 
-  doCheck = false;
-
-  postPatch = ''
-    substituteInPlace configure.ac --replace "AM_GNU_GETTEXT_VERSION([0.19.8])" "AM_GNU_GETTEXT_VERSION([${gettext.version}])"
-  '';
-
   nativeBuildInputs = [
-    wrapGAppsHook3
-    gettext
-    autoreconfHook
+    meson
+    ninja
     pkg-config
+    wrapGAppsHook3
+    desktop-file-utils
   ];
 
   buildInputs = [
-    gtk2
-    libmikmod
-    librsvg
-    hicolor-icon-theme
-    libcanberra-gtk2
+    clutter
+    clutter-gtk
+    gsound
+    libgnome-games-support
   ];
 
   configureFlags = [ "--disable-setgid" ];
 
-  meta = with lib; {
-    description = "Bejeweled clone game";
+  meta = {
+    description = "Puzzle game similar to Bejeweled or Diamond Mine";
     mainProgram = "gweled";
     homepage = "https://gweled.org";
-    license = licenses.gpl2Plus;
-    platforms = platforms.linux;
-    maintainers = [ ];
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ aleksana ];
   };
-}
+})
