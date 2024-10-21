724f9699a5729040c50309c99d47790ae7e9ddb3
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-10-21 20:57:27 +0200

    linux: simplify toolchain selection
    When we set CROSS_COMPILE to set the toolchain prefix, everything
    works out of the box without using absolute paths everywhere. This
    removes a lot of duplication from the kernel derivations.

--- pkgs/os-specific/linux/kernel/generic.nix
+++ pkgs/os-specific/linux/kernel/generic.nix
@@ -171,34 +171,18 @@ let
 
     buildPhase = ''
       export buildRoot="''${buildRoot:-build}"
-      export HOSTCC=$CC_FOR_BUILD
-      export HOSTCXX=$CXX_FOR_BUILD
-      export HOSTAR=$AR_FOR_BUILD
-      export HOSTLD=$LD_FOR_BUILD
-
-      # Absolute paths for tools avoid any PATH-clobbering issues.
-      export CC=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc
-      export LD=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}ld
-      export AR=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}ar
-      export NM=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}nm
-      export STRIP=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}strip
-      export OBJCOPY=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}objcopy
-      export OBJDUMP=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}objdump
-      export READELF=${stdenv.cc}/bin/${stdenv.cc.targetPrefix}readelf
 
       # Get a basic config file for later refinement with $generateConfig.
       make $makeFlags \
           -C . O="$buildRoot" $kernelBaseConfig \
-          ARCH=$kernelArch \
-          HOSTCC=$HOSTCC HOSTCXX=$HOSTCXX HOSTAR=$HOSTAR HOSTLD=$HOSTLD \
-          CC=$CC OBJCOPY=$OBJCOPY OBJDUMP=$OBJDUMP READELF=$READELF \
-          LD=$LD AR=$AR NM=$NM STRIP=$STRIP \
+          ARCH=$kernelArch CROSS_COMPILE=${stdenv.cc.targetPrefix} \
           $makeFlags
 
       # Create the config file.
       echo "generating kernel configuration..."
       ln -s "$kernelConfigPath" "$buildRoot/kernel-config"
-      DEBUG=1 ARCH=$kernelArch KERNEL_CONFIG="$buildRoot/kernel-config" AUTO_MODULES=$autoModules \
+      DEBUG=1 ARCH=$kernelArch CROSS_COMPILE=${stdenv.cc.targetPrefix} \
+        KERNEL_CONFIG="$buildRoot/kernel-config" AUTO_MODULES=$autoModules \
         PREFER_BUILTIN=$preferBuiltin BUILD_ROOT="$buildRoot" SRC=. MAKE_FLAGS="$makeFlags" \
         perl -w $generateConfig
     '';
