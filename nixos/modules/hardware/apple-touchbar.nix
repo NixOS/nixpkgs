53eede8cfbdee3bcb9578c0b3871a0325d6a5959
andre4ik3 <andre4ik3@fastmail.com>
2024-10-21 22:36:33 +0400

    Stop using with lib

--- nixos/modules/hardware/apple-touchbar.nix
+++ nixos/modules/hardware/apple-touchbar.nix
@@ -5,7 +5,6 @@
   ...
 }:
 
-with lib;
 let
   cfg = config.hardware.apple.touchBar;
   format = pkgs.formats.toml { };
@@ -13,16 +12,16 @@ let
 in
 {
   options.hardware.apple.touchBar = {
-    enable = mkEnableOption "support for the Touch Bar on some Apple laptops using tiny-dfr";
+    enable = lib.mkEnableOption "support for the Touch Bar on some Apple laptops using tiny-dfr";
 
-    settings = mkOption {
+    settings = lib.mkOption {
       type = format.type;
       default = { };
       description = ''
         Configuration for tiny-dfr. See
         <https://github.com/WhatAmISupposedToPutHere/tiny-dfr/blob/master/share/tiny-dfr/config.toml>
       '';
-      example = literalExpression ''
+      example = lib.literalExpression ''
         {
           MediaLayerDefault = true;
           ShowButtonOutlines = false;
@@ -32,7 +31,7 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     assertions = [
       {
         assertion = cfg.enable -> config.services.udev.enable;
