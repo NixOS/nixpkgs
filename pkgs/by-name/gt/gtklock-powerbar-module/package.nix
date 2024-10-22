18b40fe3acf7c442a154af4ea7cbca10ae807087
aleksana <me@aleksana.moe>
2024-10-22 15:17:11 +0800

    gtklock-powerbar-module: 3.0.0 -> 4.0.0

--- pkgs/by-name/gt/gtklock-powerbar-module/package.nix
+++ pkgs/by-name/gt/gtklock-powerbar-module/package.nix
@@ -2,32 +2,39 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  meson,
+  ninja,
   pkg-config,
   gtk3,
+  gtklock,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gtklock-powerbar-module";
-  version = "3.0.0";
+  version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = "jovanlanik";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-uqGWr3/PaXif+JuxqRDlvfeiVG2nbausfe5dZOHcm7o=";
+    repo = "gtklock-powerbar-module";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-Zakdta1i0o7S2AbHydlonnh5OMGVgGjB2H/AiHgQT9A=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
 
   buildInputs = [ gtk3 ];
 
-  makeFlags = [ "PREFIX=$(out)" ];
+  passthru.tests.testModule = gtklock.testModule finalAttrs.finalPackage;
 
-  meta = with lib; {
+  meta = {
     description = "Gtklock module adding power controls to the lockscreen";
     homepage = "https://github.com/jovanlanik/gtklock-powerbar-module";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ aleksana ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ aleksana ];
+    platforms = lib.platforms.linux;
   };
-}
+})
