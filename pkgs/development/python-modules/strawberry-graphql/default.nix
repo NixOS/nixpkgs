cee263c4fd18788f8cb088b1d9f6b5361a8b5e2e
shobu13 <shobu13@hotmail.fr>
2024-10-22 11:23:58 +0000

    strawberry-graphql: added typer and graphlib-backport to build input, replaced click by typer in debug-server optional-dep

--- pkgs/development/python-modules/strawberry-graphql/default.nix
+++ pkgs/development/python-modules/strawberry-graphql/default.nix
@@ -5,7 +5,6 @@
   buildPythonPackage,
   chalice,
   channels,
-  click,
   daphne,
   django,
   email-validator,
@@ -40,6 +39,8 @@
   starlette,
   typing-extensions,
   uvicorn,
+  typer,
+  graphlib-backport
 }:
 
 buildPythonPackage rec {
@@ -92,7 +93,7 @@ buildPythonPackage rec {
       libcst
     ];
     debug-server = [
-      click
+      typer
       libcst
       pygments
       python-multipart
