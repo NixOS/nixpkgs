50ea63c313a3a39d55442225ed53038dabdeb376
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-22 11:47:09 +0200

    python312Packages.influxdb-client: 1.46.0 -> 1.47.0
    Diff: https://github.com/influxdata/influxdb-client-python/compare/refs/tags/v1.46.0...v1.47.0
    
    Changelog: https://github.com/influxdata/influxdb-client-python/blob/v1.47.0/CHANGELOG.md

--- pkgs/development/python-modules/influxdb-client/default.nix
+++ pkgs/development/python-modules/influxdb-client/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "influxdb-client";
-  version = "1.46.0";
+  version = "1.47.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "influxdata";
     repo = "influxdb-client-python";
     rev = "refs/tags/v${version}";
-    hash = "sha256-oq6VXsCizqs7ZGocFWvD6SK1HRgQerlAEDW6+SBoM+A=";
+    hash = "sha256-1QWy+mdVttowdbIBmtW6gYwkJ4p9sL0D+DxsHk3xgvc=";
   };
 
   build-system = [ setuptools ];
