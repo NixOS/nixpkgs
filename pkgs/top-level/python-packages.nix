2389fd114005a00da81c1ddfcd2359bb24daa8ee
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-10-22 11:16:46 +0200

    python312Packages.pyreqwest-impersonate: remove
    The package has been replaced by `python312Packages.primp`.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10320,10 +10320,6 @@ self: super: with self; {
 
   pyrender = callPackage ../development/python-modules/pyrender { };
 
-  pyreqwest-impersonate = callPackage ../development/python-modules/pyreqwest-impersonate {
-    inherit (pkgs.darwin.apple_sdk.frameworks) SystemConfiguration;
-  };
-
   pyrevolve = callPackage ../development/python-modules/pyrevolve { };
 
   pyrfxtrx = callPackage ../development/python-modules/pyrfxtrx { };
