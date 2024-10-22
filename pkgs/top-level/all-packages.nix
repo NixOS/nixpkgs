30d7a3f7b6627c684f0af3846a31118b644815c5
aleksana <me@aleksana.moe>
2024-10-22 19:39:51 +0800

    jalv: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30229,8 +30229,6 @@ with pkgs;
 
   jaeles = callPackage ../tools/security/jaeles { };
 
-  jalv = callPackage ../applications/audio/jalv { };
-
   jameica = callPackage ../applications/office/jameica {
     inherit (darwin.apple_sdk.frameworks) Cocoa;
   };
