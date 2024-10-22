c835b093f75e83737aa3d680f7f6e1db825792cb
aleksana <me@aleksana.moe>
2024-10-22 20:03:31 +0800

    jalv: 1.6.6 -> 1.6.8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -30240,6 +30240,8 @@ with pkgs;
 
   jaeles = callPackage ../tools/security/jaeles { };
 
+  jalv-qt = jalv.override { useQt = true; };
+
   jameica = callPackage ../applications/office/jameica {
     inherit (darwin.apple_sdk.frameworks) Cocoa;
   };
