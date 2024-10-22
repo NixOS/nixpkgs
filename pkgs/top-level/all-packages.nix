0fca4ca081bfbdd92244d5eeea81cc55b2eaf78e
wxt <3264117476@qq.com>
2024-10-22 21:40:44 +0800

    lenmus: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17884,8 +17884,6 @@ with pkgs;
 
   lemon = callPackage ../development/tools/parsing/lemon { };
 
-  lenmus = callPackage ../applications/misc/lenmus { };
-
   libtool = libtool_2;
 
   libtool_1_5 = callPackage ../development/tools/misc/libtool { };
