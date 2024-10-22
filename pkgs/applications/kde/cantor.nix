ac156c8a50d57b103ab4f9c3ad32767fe6d23a4d
wxt <3264117476@qq.com>
2024-10-22 19:25:14 +0800

    cantor: nixfmt

--- pkgs/applications/kde/cantor.nix
+++ pkgs/applications/kde/cantor.nix
@@ -1,46 +1,55 @@
-{ lib
-, mkDerivation
+{
+  lib,
+  mkDerivation,
 
-, cmake
-, extra-cmake-modules
-, makeWrapper
-, shared-mime-info
+  cmake,
+  extra-cmake-modules,
+  makeWrapper,
+  shared-mime-info,
 
-, qtbase
-, qtsvg
-, qttools
-, qtwebengine
-, qtxmlpatterns
+  qtbase,
+  qtsvg,
+  qttools,
+  qtwebengine,
+  qtxmlpatterns,
 
-, poppler
+  poppler,
 
-, karchive
-, kcompletion
-, kconfig
-, kcoreaddons
-, kcrash
-, kdoctools
-, ki18n
-, kiconthemes
-, kio
-, knewstuff
-, kparts
-, kpty
-, ktexteditor
-, ktextwidgets
-, kxmlgui
-, syntax-highlighting
+  karchive,
+  kcompletion,
+  kconfig,
+  kcoreaddons,
+  kcrash,
+  kdoctools,
+  ki18n,
+  kiconthemes,
+  kio,
+  knewstuff,
+  kparts,
+  kpty,
+  ktexteditor,
+  ktextwidgets,
+  kxmlgui,
+  syntax-highlighting,
 
-, libspectre
+  libspectre,
 
-# Backends. Set to null if you want to omit from the build
-, withAnalitza ? true, analitza
-, wtihJulia ? true, julia
-, withQalculate ? true, libqalculate
-, withLua ? true, luajit
-, withPython ? true, python3
-, withR ? true, R
-, withSage ? true, sage, sage-with-env ? sage.with-env
+  # Backends. Set to null if you want to omit from the build
+  withAnalitza ? true,
+  analitza,
+  wtihJulia ? true,
+  julia,
+  withQalculate ? true,
+  libqalculate,
+  withLua ? true,
+  luajit,
+  withPython ? true,
+  python3,
+  withR ? true,
+  R,
+  withSage ? true,
+  sage,
+  sage-with-env ? sage.with-env,
 }:
 
 mkDerivation {
@@ -54,42 +63,42 @@ mkDerivation {
     qttools
   ];
 
-  buildInputs = [
-    qtbase
-    qtsvg
-    qtwebengine
-    qtxmlpatterns
+  buildInputs =
+    [
+      qtbase
+      qtsvg
+      qtwebengine
+      qtxmlpatterns
 
-    poppler
+      poppler
 
-    karchive
-    kcompletion
-    kconfig
-    kcoreaddons
-    kcrash
-    kdoctools
-    ki18n
-    kiconthemes
-    kio
-    knewstuff
-    kparts
-    kpty
-    ktexteditor
-    ktextwidgets
-    kxmlgui
-    syntax-highlighting
+      karchive
+      kcompletion
+      kconfig
+      kcoreaddons
+      kcrash
+      kdoctools
+      ki18n
+      kiconthemes
+      kio
+      knewstuff
+      kparts
+      kpty
+      ktexteditor
+      ktextwidgets
+      kxmlgui
+      syntax-highlighting
 
-    libspectre
-  ]
-  # backends
-  ++ lib.optional withAnalitza analitza
-  ++ lib.optional wtihJulia julia
-  ++ lib.optional withQalculate libqalculate
-  ++ lib.optional withLua luajit
-  ++ lib.optional withPython python3
-  ++ lib.optional withR R
-  ++ lib.optional withSage sage-with-env
-  ;
+      libspectre
+    ]
+    # backends
+    ++ lib.optional withAnalitza analitza
+    ++ lib.optional wtihJulia julia
+    ++ lib.optional withQalculate libqalculate
+    ++ lib.optional withLua luajit
+    ++ lib.optional withPython python3
+    ++ lib.optional withR R
+    ++ lib.optional withSage sage-with-env;
 
   qtWrapperArgs = [
     "--prefix PATH : ${placeholder "out"}/bin"
@@ -101,7 +110,13 @@ mkDerivation {
   meta = with lib; {
     description = "Front end to powerful mathematics and statistics packages";
     homepage = "https://cantor.kde.org/";
-    license = with licenses; [ bsd3 cc0 gpl2Only gpl2Plus gpl3Only ];
+    license = with licenses; [
+      bsd3
+      cc0
+      gpl2Only
+      gpl2Plus
+      gpl3Only
+    ];
     maintainers = with maintainers; [ hqurve ];
   };
 }
