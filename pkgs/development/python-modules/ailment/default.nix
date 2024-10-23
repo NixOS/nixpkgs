ced4908ccd24996937ed9594cdd875106ec88c17
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-23 21:29:20 +0200

    python312Packages.ailment: 9.2.122 -> 9.2.124
    Diff: https://github.com/angr/ailment/compare/refs/tags/v9.2.122...v9.2.124

--- pkgs/development/python-modules/ailment/default.nix
+++ pkgs/development/python-modules/ailment/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "ailment";
-  version = "9.2.122";
+  version = "9.2.124";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "ailment";
     rev = "refs/tags/v${version}";
-    hash = "sha256-e+SDgi0bhiE0MdhRxxFlkKU0N5ExfehbEOOyO4+wAzQ=";
+    hash = "sha256-wGpLRaoW6rZIT+vEpbjtjSJhBZbNNrpxt/+q6zTuJ9A=";
   };
 
   build-system = [ setuptools ];
