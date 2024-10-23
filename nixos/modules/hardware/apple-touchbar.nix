02c4b8f1ae18c6ce29a11bfcc68316af08046c44
andre4ik3 <andre4ik3@fastmail.com>
2024-10-23 09:06:50 +0400

    Add package override option, further simplify

--- nixos/modules/hardware/apple-touchbar.nix
+++ nixos/modules/hardware/apple-touchbar.nix
@@ -13,13 +13,15 @@ in
 {
   options.hardware.apple.touchBar = {
     enable = lib.mkEnableOption "support for the Touch Bar on some Apple laptops using tiny-dfr";
+    package = lib.mkPackageOption pkgs "tiny-dfr" { };
 
     settings = lib.mkOption {
       type = format.type;
       default = { };
       description = ''
-        Configuration for tiny-dfr. See
-        <https://github.com/WhatAmISupposedToPutHere/tiny-dfr/blob/master/share/tiny-dfr/config.toml>
+        Configuration for tiny-dfr. See [example configuration][1] for available options.
+
+        [1]: https://github.com/WhatAmISupposedToPutHere/tiny-dfr/blob/master/share/tiny-dfr/config.toml
       '';
       example = lib.literalExpression ''
         {
@@ -32,18 +34,10 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = cfg.enable -> config.services.udev.enable;
-        message = "Touch Bar support via tiny-dfr requires udev.";
-      }
-    ];
+    systemd.packages = [ cfg.package ];
+    services.udev.packages = [ cfg.package ];
 
     environment.etc."tiny-dfr/config.toml".source = cfgFile;
-
-    systemd.packages = with pkgs; [ tiny-dfr ];
-    services.udev.packages = with pkgs; [ tiny-dfr ];
-
     systemd.services.tiny-dfr.restartTriggers = [ cfgFile ];
   };
 }
