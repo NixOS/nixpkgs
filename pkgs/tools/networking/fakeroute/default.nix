785796942622eed53d9c975d22b0d5fa64fea7ab
rnhmjoj <rnhmjoj@inventati.org>
2024-10-23 09:55:41 +0200

    fakeroute: update source url
    maxwell.ydns.eu is no more, the repository is now at maxwell.eurofusion.eu.

--- pkgs/tools/networking/fakeroute/default.nix
+++ pkgs/tools/networking/fakeroute/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation rec {
   version = "0.3";
 
   src = fetchurl {
-    url = "https://maxwell.ydns.eu/git/rnhmjoj/fakeroute/releases/download/v${version}/fakeroute-${version}.tar.gz";
+    url = "https://maxwell.eurofusion.eu/git/rnhmjoj/fakeroute/releases/download/v${version}/fakeroute-${version}.tar.gz";
     hash = "sha256-DoXGJm8vOlAD6ZuvVAt6bkgfahc8WgyYIXCrgqzfiWg=";
   };
 
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     description = ''
       Make your machine appears to be anywhere on the internet in a traceroute
     '';
-    homepage = "https://maxwell.ydns.eu/git/rnhmjoj/fakeroute";
+    homepage = "https://maxwell.eurofusion.eu/git/rnhmjoj/fakeroute";
     license = licenses.bsd3;
     platforms = platforms.linux;
     mainProgram = "fakeroute";
