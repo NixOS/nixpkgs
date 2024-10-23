f96f4979c7d865237ab035bb55a2f5436052d8be
t4ccer <t4ccer@gmail.com>
2024-10-22 20:11:48 -0600

    nixos/pds: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1549,6 +1549,7 @@
   ./services/web-apps/mobilizon.nix
   ./services/web-apps/openwebrx.nix
   ./services/web-apps/outline.nix
+  ./services/web-apps/pds.nix
   ./services/web-apps/peering-manager.nix
   ./services/web-apps/peertube.nix
   ./services/web-apps/pgpkeyserver-lite.nix
