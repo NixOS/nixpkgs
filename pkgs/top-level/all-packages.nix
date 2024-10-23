0227f52e3310de7316fb3a39bdf9f70c059824be
Franz Pletz <fpletz@fnordicwalking.de>
2024-10-23 16:32:37 +0200

    janus-gateway: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24376,8 +24376,6 @@ with pkgs;
 
   ircdHybrid = callPackage ../servers/irc/ircd-hybrid { };
 
-  janus-gateway = callPackage ../servers/janus-gateway { };
-
   janusgraph = callPackage ../servers/nosql/janusgraph { };
 
   jboss = callPackage ../servers/http/jboss { };
