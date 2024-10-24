a2289e0d8f55595b697fd3e7100ab831465ad679
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-24 18:39:21 +0200

    i3altlayout: 0.3 -> 1.1.2
    Changelog: https://github.com/deadc0de6/i3altlayout/releases/tag/v1.1.2

--- pkgs/applications/window-managers/i3/altlayout.nix
+++ pkgs/applications/window-managers/i3/altlayout.nix
@@ -1,26 +1,37 @@
-{ lib, python3Packages, fetchPypi }:
+{
+  lib,
+  python3Packages,
+  fetchPypi,
+}:
 
 python3Packages.buildPythonApplication rec {
   pname = "i3altlayout";
-  version = "0.3";
+  version = "1.1.2";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1h0phf3s6ljffxw0bs73k041wildaz01h37iv5mxhami41wrh4qf";
+    hash = "sha256-Zdp5zPeT3Cg/7uUG9WIZEFP1T11W6orQ9fEPljCk1H8=";
   };
 
-  pythonPath = with python3Packages; [ enum-compat i3ipc docopt ];
+  build-system  = with python3Packages; [ setuptools ];
+
+  pythonPath = with python3Packages; [
+    i3ipc
+    docopt
+  ];
 
   doCheck = false;
 
   pythonImportsCheck = [ "i3altlayout" ];
 
   meta = with lib; {
-    maintainers = with maintainers; [ magnetophon ];
     description = "Helps you handle more efficiently your screen real estate in i3wm by auto-splitting windows on their longest side";
-    mainProgram = "i3altlayout";
     homepage = "https://github.com/deadc0de6/i3altlayout";
+    changelog = "https://github.com/deadc0de6/i3altlayout/releases/tag/v${version}";
     license = licenses.gpl3Only;
+    maintainers = with maintainers; [ magnetophon ];
+    mainProgram = "i3altlayout";
     platforms = platforms.linux;
   };
 }
