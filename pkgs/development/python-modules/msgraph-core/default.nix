6e01a76614767a2799bf3f70252e246530a1d0bb
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-24 22:05:25 +0200

    python312Packages.msgraph-core: 1.1.5 -> 1.1.6
    Diff: https://github.com/microsoftgraph/msgraph-sdk-python-core/compare/refs/tags/v1.1.5...v1.1.6
    
    Changelog: https://github.com/microsoftgraph/msgraph-sdk-python-core/releases/tag/v1.1.6

--- pkgs/development/python-modules/msgraph-core/default.nix
+++ pkgs/development/python-modules/msgraph-core/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "msgraph-core";
-  version = "1.1.5";
+  version = "1.1.6";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "microsoftgraph";
     repo = "msgraph-sdk-python-core";
     rev = "refs/tags/v${version}";
-    hash = "sha256-cyqhNpV3gowSDmZhdI3NbbJ1VOwnrJTXZYR/rYNnFVE=";
+    hash = "sha256-W1tttt4jWGftW8zzklHV3SrAyugi6XZv0PYGs3T5gZ4=";
   };
 
   build-system = [ setuptools ];
