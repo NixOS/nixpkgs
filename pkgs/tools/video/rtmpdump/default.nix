4750ed25a60039ccf0684e5c3ca15fd83cbe6ddd
Simon Hollingshead <me@simonhollingshead.com>
2024-10-24 07:08:55 +0100

    rtmpdump: unstable-2021-02-19 -> 2.6

--- pkgs/tools/video/rtmpdump/default.nix
+++ pkgs/tools/video/rtmpdump/default.nix
@@ -1,7 +1,6 @@
 { lib
 , stdenv
 , fetchgit
-, fetchpatch
 , zlib
 , gnutlsSupport ? false
 , gnutls
@@ -14,22 +13,14 @@ assert (gnutlsSupport || opensslSupport);
 
 stdenv.mkDerivation {
   pname = "rtmpdump";
-  version = "unstable-2021-02-19";
+  version = "2.6";
 
   src = fetchgit {
     url = "git://git.ffmpeg.org/rtmpdump";
-    # Currently the latest commit is used (a release has not been made since 2011, i.e. '2.4')
-    rev = "f1b83c10d8beb43fcc70a6e88cf4325499f25857";
-    sha256 = "0vchr0f0d5fi0zaa16jywva5db3x9dyws7clqaq32gwh5drbkvs0";
+    # Releases are not tagged.
+    rev = "6f6bb1353fc84f4cc37138baa99f586750028a01";
+    hash = "sha256-rwMA9eougKnkpG+fe6vZIwOBt2CC1d9qI9a079EbE5o=";
   };
-
-  patches = [
-    # Fix build with OpenSSL 1.1
-    (fetchpatch {
-      url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/media-video/rtmpdump/files/rtmpdump-openssl-1.1.patch?id=1e7bef484f96e7647f5f0911d3c8caa48131c33b";
-      sha256 = "1wds98pk8qr7shkfl8k49iirxiwd972h18w84bamiqln29wv6ql1";
-    })
-  ];
 
   preBuild = ''
     makeFlagsArray+=(CC="$CC")
