9d6822f8c26d650f9745067c561160a5324f296a
Anthony ROUSSEL <anthony@roussel.dev>
2024-10-24 21:39:24 +0200

    nagiosPlugins: add recurseIntoAttrs

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24817,7 +24817,7 @@ with pkgs;
 
   nagios = callPackage ../servers/monitoring/nagios { };
 
-  nagiosPlugins = callPackages ../servers/monitoring/nagios-plugins { };
+  nagiosPlugins = recurseIntoAttrs (callPackages ../servers/monitoring/nagios-plugins { });
 
   monitoring-plugins = callPackage ../servers/monitoring/plugins { };
 
