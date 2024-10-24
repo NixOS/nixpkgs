ec87002aa01d91af807e16c39bd244163cb49a34
Austin Horstman <khaneliman12@gmail.com>
2024-10-23 22:10:34 -0500

    cava: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28408,8 +28408,6 @@ with pkgs;
 
   catt = callPackage ../applications/video/catt { };
 
-  cava = callPackage ../applications/audio/cava { };
-
   cb2bib = libsForQt5.callPackage ../applications/office/cb2bib { };
 
   cbatticon = callPackage ../applications/misc/cbatticon { };
