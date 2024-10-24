59ec08c4fbe96453703114484eef91c04ccd4dbf
Pyrox <pyrox@pyrox.dev>
2024-10-24 18:27:55 -0400

    python3Packages.gpapi: refactor and modernize

--- pkgs/development/python-modules/gpapi/default.nix
+++ pkgs/development/python-modules/gpapi/default.nix
@@ -7,21 +7,39 @@
   pycryptodome,
   requests,
   protobuf_27,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   version = "0.4.4";
-  format = "setuptools";
   pname = "gpapi";
+  pyproject = true;
 
   src = fetchPypi {
     inherit version pname;
-    sha256 = "0ampvsv97r3hy1cakif4kmyk1ynf3scbvh4fbk02x7xrxn4kl38w";
+    sha256 = "sha256-HA06ie25ny7AXI7AvZgezvowfZ3ExalY8HDkk7betyo=";
   };
 
   postPatch = ''
     substituteInPlace setup.py \
       --replace-fail 'PROTOC_EXEC = "protoc"' 'PROTOC_EXEC = "${lib.getExe protobuf_27}"'
+  '';
+
+  build-system = [ setuptools ];
+
+  buildInputs = [
+    protobuf_27
+  ];
+
+  dependencies = [
+    cryptography
+    protobuf
+    pycryptodome
+    requests
+  ];
+
+  preBuild = ''
+    export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION="python"
   '';
 
   # package doesn't contain unit tests
@@ -30,24 +48,9 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "gpapi.googleplay" ];
 
-  preBuild = ''
-    export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION="python"
-  '';
-
-  buildInputs = [
-    protobuf_27
-  ];
-
-  propagatedBuildInputs = [
-    cryptography
-    protobuf
-    pycryptodome
-    requests
-  ];
-
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/NoMore201/googleplay-api";
-    license = licenses.gpl3Only;
+    license = lib.licenses.gpl3Only;
     description = "Google Play Unofficial Python API";
     maintainers = [ ];
   };
