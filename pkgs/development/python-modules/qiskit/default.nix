a5b6f35521be2fc25fcd6a536d0029b3f780a218
comavius <comavius@proton.me>
2024-10-24 21:49:06 +0900

    python3Packages: qiskit 1.0.1 -> 1.2.4

--- pkgs/development/python-modules/qiskit/default.nix
+++ pkgs/development/python-modules/qiskit/default.nix
@@ -1,68 +1,75 @@
 {
   lib,
   pythonOlder,
+  pythonAtLeast,
   buildPythonPackage,
   fetchFromGitHub,
 
   # build-system
   setuptools,
+  setuptools-rust,
+  rustPlatform,
+  rustc,
+  cargo,
+
 
   # Python Inputs
-  qiskit-aer,
-  qiskit-ibmq-provider,
-  qiskit-ignis,
-  qiskit-terra,
+  rustworkx,
+  numpy,
+  scipy,
+  sympy,
+  dill,
+  python-dateutil,
+  stevedore,
+  typing-extensions,
+  symengine,
   # Optional inputs
   withOptionalPackages ? true,
   qiskit-finance,
   qiskit-machine-learning,
   qiskit-nature,
   qiskit-optimization,
-  # Check Inputs
-  pytestCheckHook,
 }:
-
-let
-  optionalQiskitPackages = [
-    qiskit-finance
-    qiskit-machine-learning
-    qiskit-nature
-    qiskit-optimization
-  ];
-in
 buildPythonPackage rec {
   pname = "qiskit";
   # NOTE: This version denotes a specific set of subpackages. See https://qiskit.org/documentation/release_notes.html#version-history
-  version = "1.0.1";
+  version = "1.2.4";
   pyproject = true;
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.7" || pythonAtLeast "3.12";
 
   src = fetchFromGitHub {
     owner = "Qiskit";
     repo = "qiskit";
     rev = "refs/tags/${version}";
-    hash = "sha256-Cjfn+9h8W08FcAlVC7b7O8Z+VGx5UeHosSgYJin/evE=";
+    hash = "sha256-kNjhCxdgXXteVXjFKvItCrlyMGFXKX9HNDtGNTjDvA4=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = "${src}/Cargo.lock";
+  };
+
+  nativeBuildInputs = [
+    rustPlatform.cargoSetupHook
+    rustc
+    cargo
+    setuptools
+    setuptools-rust
+  ];
 
   propagatedBuildInputs = [
-    qiskit-aer
-    qiskit-ibmq-provider
-    qiskit-ignis
-    qiskit-terra
-  ] ++ lib.optionals withOptionalPackages optionalQiskitPackages;
-
-  nativeCheckInputs = [ pytestCheckHook ];
-
-  pythonImportsCheck = [
-    "qiskit"
-    "qiskit.circuit"
-    "qiskit.ignis"
-    "qiskit.providers.aer"
-    "qiskit.providers.ibmq"
+    numpy
+    scipy
+    sympy
+    dill
+    python-dateutil
+    stevedore
+    typing-extensions
+    symengine
+    rustworkx
   ];
+
+  doCheck = false;
 
   meta = with lib; {
     description = "Software for developing quantum computing programs";
