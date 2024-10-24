5c25c973b21d6da34d8916dd1d856799088535fe
Artemis Tosini <me@artem.ist>
2024-10-24 00:15:36 +0000

    bintools-wrapper: Set dynamic-linker on OpenBSD
    Now that ld.so is symlinked into the libc derivation,
    we can use it as a dynamic linker.
    In my testing, OpenBSD does not have any issues with executing from
    a symlinked ld.so.

--- pkgs/build-support/bintools-wrapper/default.nix
+++ pkgs/build-support/bintools-wrapper/default.nix
@@ -134,6 +134,7 @@ let
     else if targetPlatform.isLoongArch64              then "${sharedLibraryLoader}/lib/ld-linux-loongarch*.so.1"
     else if targetPlatform.isDarwin                   then "/usr/lib/dyld"
     else if targetPlatform.isFreeBSD                  then "${sharedLibraryLoader}/libexec/ld-elf.so.1"
+    else if targetPlatform.isOpenBSD                  then "${sharedLibraryLoader}/libexec/ld.so"
     else if hasSuffix "pc-gnu" targetPlatform.config then "ld.so.1"
     else "";
 
