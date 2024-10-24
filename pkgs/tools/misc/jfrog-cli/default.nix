8e40ef353e78c5b3742965dd1f255b4a719fa489
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-24 11:31:43 +0000

    jfrog-cli: 2.70.0 -> 2.71.0

--- pkgs/tools/misc/jfrog-cli/default.nix
+++ pkgs/tools/misc/jfrog-cli/default.nix
@@ -7,17 +7,17 @@
 
 buildGo123Module rec {
   pname = "jfrog-cli";
-  version = "2.70.0";
+  version = "2.71.0";
 
   src = fetchFromGitHub {
     owner = "jfrog";
     repo = "jfrog-cli";
     rev = "refs/tags/v${version}";
-    hash = "sha256-ddwGmXb616kDNNNTNUykiEWX/2ihUpgetZ/va943RiQ=";
+    hash = "sha256-hblOe6YMlfWBVvWD5MKEKvAB55jUx98OMeqFDdgVrmM=";
   };
 
   proxyVendor = true;
-  vendorHash = "sha256-1xUCQF2UDHAmzibixv9pR6G2gvXxIStCyBuz608UpIQ=";
+  vendorHash = "sha256-px+eXSIOe3v4iNBibXAtcFMROrHq6YYJIHbCC7nhNS4=";
 
   postPatch = ''
     # Patch out broken test cleanup.
