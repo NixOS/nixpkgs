937e3ac6bb15dfc28921c8a2452616dd2d998c93
Alyssa Ross <hi@alyssa.is>
2024-10-24 12:55:57 +0200

    Revert "linux_testing: disable NFS_LOCALIO on aarch64-linux"
    This reverts commit bda6c82a8162ebbf3c25e028992a4b8a9c323065.
    
    > This was fixed via v6.12-rc3 commit 009b15b57485 ("nfs_common: fix
    > Kconfig for NFS_COMMON_LOCALIO_SUPPORT")
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=219370#c5

--- pkgs/os-specific/linux/kernel/common-config.nix
+++ pkgs/os-specific/linux/kernel/common-config.nix
@@ -642,10 +642,7 @@ let
       NFS_V4_1              = yes;  # NFSv4.1 client support
       NFS_V4_2              = yes;
       NFS_V4_SECURITY_LABEL = yes;
-
-      # Fails with
-      # `fs/nfs/localio.o: relocation R_AARCH64_ADR_PREL_PG_HI21 against symbol `nfs_to' which may bind externally can not be used when making a shared object; recompile with -fPIC`
-      NFS_LOCALIO           = lib.mkIf (!stdenv.hostPlatform.isAarch64) (whenAtLeast "6.12" yes);
+      NFS_LOCALIO           = whenAtLeast "6.12" yes;
 
       CIFS_XATTR        = yes;
       CIFS_POSIX        = option yes;
