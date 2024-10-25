10c3b8745a32c720bf87e2f2df230b9283531e85
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-26 00:11:56 +0200

    python312Packages.cle: 9.2.124 -> 9.2.125
    Diff: https://github.com/angr/cle/compare/refs/tags/v9.2.125...v9.2.125

--- pkgs/development/python-modules/cle/default.nix
+++ pkgs/development/python-modules/cle/default.nix
@@ -18,14 +18,14 @@
 
 let
   # The binaries are following the argr projects release cycle
-  version = "9.2.124";
+  version = "9.2.125";
 
   # Binary files from https://github.com/angr/binaries (only used for testing and only here)
   binaries = fetchFromGitHub {
     owner = "angr";
     repo = "binaries";
     rev = "refs/tags/v${version}";
-    hash = "sha256-eUx71aCk0DHtD735YmHS8+awcoN6WYZi1fA8+pS8mXo=";
+    hash = "sha256-22srtaY8CWT+XqnSZpMndBEVe20rDL+2LpJr9MByyLU=";
   };
 in
 buildPythonPackage rec {
@@ -39,7 +39,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "cle";
     rev = "refs/tags/v${version}";
-    hash = "sha256-qwItp8HHqvjzXJIGfEtGAM09eOhz0GEKPYwgbXJBdXw=";
+    hash = "sha256-Xe9tOt94kSKsP0Xc8K0OaeyiUKFUkujZt9AsamgKwBw=";
   };
 
   build-system = [ setuptools ];
