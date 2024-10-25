d9309fd8eba307d74e0e0195a7d6a110fe205850
Gaetan Lepage <gaetan@glepage.com>
2024-10-25 14:33:01 +0200

    uv: 0.4.25 -> 0.4.26
    Diff: https://github.com/astral-sh/uv/compare/refs/tags/0.4.25...0.4.26
    
    Changelog: https://github.com/astral-sh/uv/blob/refs/tags/0.4.26/CHANGELOG.md

--- pkgs/by-name/uv/uv/package.nix
+++ pkgs/by-name/uv/uv/package.nix
@@ -15,14 +15,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "uv";
-  version = "0.4.25";
+  version = "0.4.26";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "astral-sh";
     repo = "uv";
     rev = "refs/tags/${version}";
-    hash = "sha256-qAfM9I2NboYkUukWnOjuGcdjp8IONAI6Qwwg1r9kCGg=";
+    hash = "sha256-aeyKbuJXYJvpyxbyDWvx0FdK+Lg33xhVr5q0zbDob3M=";
   };
 
   cargoDeps = rustPlatform.importCargoLock {
@@ -31,7 +31,6 @@ python3Packages.buildPythonApplication rec {
       "async_zip-0.0.17" = "sha256-3k9rc4yHWhqsCUJ17K55F8aQoCKdVamrWAn6IDWo3Ss=";
       "pubgrub-0.2.1" = "sha256-mSpRBdQJWtKKD1zHkV7vuyfKTDY6Ejgjll5q5ryCfmY=";
       "reqwest-middleware-0.3.3" = "sha256-KjyXB65a7SAfwmxokH2PQFFcJc6io0xuIBQ/yZELJzM=";
-      "rust-netrc-0.1.1" = "sha256-DeDAm2k4/2A9Nw8zXeKOMdxhbseGIrRXH0KgGf2shOc=";
       "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
     };
   };
