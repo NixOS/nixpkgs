327550eaaf5006d6d8e22da3ba56b3af6f069ef6
Conroy Cheers <conroy@dromeda.com.au>
2024-10-25 18:09:15 +1100

    ray: update hashes

--- pkgs/development/python-modules/ray/binary-hashes.nix
+++ pkgs/development/python-modules/ray/binary-hashes.nix
@@ -1,26 +1,26 @@
 {
   "cp310-x86_64-linux" = {
     platform = "manylinux2014_x86_64";
-    hash = "sha256-KZMkQeaKt9rTWydsdjZwv0Lr9yHN3E9N6CAL2SrAXFg=";
+    hash = "sha256-On1vcVm85BF7/o+cPQtl/yclf+LdjXN9rQ84aWZkQNo=";
   };
   "cp311-x86_64-linux" = {
     platform = "manylinux2014_x86_64";
-    hash = "sha256-tCAnnKFPAswn/Fkv8fKNqaoIuWIxa/Zd3zcNuHcILpE=";
+    hash = "sha256-7OgCzzocECtT9juLyQ2UeXHEs4feryM8Ik7Y7zSh88s=";
   };
   "cp312-x86_64-linux" = {
     platform = "manylinux2014_x86_64";
-    hash = "sha256-AmjHvC6Ltu+buJaSmd61hXv2cr/LWdqV23SVqKUC+Lo=";
+    hash = "sha256-B1B9L5lh6NU5DA62Bt8kkhbvWvsf+BhVgfPpIEHWYpM=";
   };
   "cp310-aarch64-linux" = {
     platform = "manylinux2014_aarch64";
-    hash = "sha256-ruf/GJ/VJTDQILE8Xn5tpV5lRWGTo0nTljWnKYHlIds=";
+    hash = "sha256-PN1xYX+TWg10GGTpQGEJPRT61lnmcnHJp3kQiHgpSsM=";
   };
   "cp311-aarch64-linux" = {
     platform = "manylinux2014_aarch64";
-    hash = "sha256-YCmOGZ2ZONO+dBjgZFquMS8Sg+MRI5kQU9NtD/Hk7EM=";
+    hash = "sha256-KCoybShI1BHDzjBeV+Leg1fiTLm+y+x+UH6IAFcsSH4=";
   };
   "cp312-aarch64-linux" = {
     platform = "manylinux2014_aarch64";
-    hash = "sha256-dc2aH28zKsANdxVLJL049LRqTmAM0CokQOabkYJztHU=";
+    hash = "sha256-1O+vHPxyfWDXjMcRL/jqpnY0pTJ+KoT43Kq10Wf+f+w=";
   };
 }
