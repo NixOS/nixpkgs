9cd88b7666eb793b731afc1d2cb5eeeb9cb0f55f
Martin Weinelt <mweinelt@users.noreply.github.com>
2024-10-25 21:28:43 +0200

    nss_latest: 3.105 -> 3.106 (#350999)

--- pkgs/development/libraries/nss/latest.nix
+++ pkgs/development/libraries/nss/latest.nix
@@ -5,6 +5,6 @@
 #       Example: nix-shell ./maintainers/scripts/update.nix --argstr package cacert
 
 import ./generic.nix {
-  version = "3.105";
-  hash = "sha256-Nfd7u0sdGqUCznnUxEJQFs+QuRSb+b7rZrcvKryimOQ=";
+  version = "3.106";
+  hash = "sha256-j8B5RgEdBbtqPmIUniNF8ToGPIrPJ8gVTMplcv72fD0=";
 }
