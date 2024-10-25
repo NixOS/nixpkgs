eb54466be22573c34a54ef10d99b493f0028a771
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-26 00:18:39 +0200

    python312Packages.aioairzone-cloud: 0.6.7 -> 0.6.8
    Diff: https://github.com/Noltari/aioairzone-cloud/compare/refs/tags/0.6.7...0.6.8
    
    Changelog: https://github.com/Noltari/aioairzone-cloud/releases/tag/0.6.8

--- pkgs/development/python-modules/aioairzone-cloud/default.nix
+++ pkgs/development/python-modules/aioairzone-cloud/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "aioairzone-cloud";
-  version = "0.6.7";
+  version = "0.6.8";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "Noltari";
     repo = "aioairzone-cloud";
     rev = "refs/tags/${version}";
-    hash = "sha256-6M80Qm5stk3wsZ55wcrJc8Y/TDd5zLWwiOhKj+5FQHs=";
+    hash = "sha256-vjN1xImbeFm/0kvwtVIp5Bs5/fB4no4DW+fRUxF1C34=";
   };
 
   build-system = [ setuptools ];
