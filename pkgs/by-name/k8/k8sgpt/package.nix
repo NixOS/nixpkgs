67e4064b20d6bcda17a23cc342d838d1b044d84c
Marcelo Giles <marcelogiles@gmail.com>
2024-10-24 17:10:23 -0700

    k8sgpt: 0.3.30 -> 0.3.41

--- pkgs/by-name/k8/k8sgpt/package.nix
+++ pkgs/by-name/k8/k8sgpt/package.nix
@@ -1,22 +1,34 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
 
 buildGoModule rec {
   pname = "k8sgpt";
-  version = "0.3.30";
+  version = "0.3.41";
 
   src = fetchFromGitHub {
     owner = "k8sgpt-ai";
     repo = "k8sgpt";
     rev = "v${version}";
-    hash = "sha256-iseyvGo9fitt8bAlbU3wF7bBLz66fijb8h35aank+0k=";
+    hash = "sha256-p53gVpFhxr60OEZSASRU5KX7TkWzYCDAvG9aQj2NFHI=";
   };
 
-  vendorHash = "sha256-YpCn7hZkMj3/dIC/ZMslTjXcumCH3LH/A7pjfJ0pUd4=";
+  vendorHash = "sha256-9H6E1JUbxfcx3Baithu9Jr6MpxfuKE+XWz7HrTCdxA8=";
 
+  # https://nixos.org/manual/nixpkgs/stable/#var-go-CGO_ENABLED
   CGO_ENABLED = 0;
 
+  # https://nixos.org/manual/nixpkgs/stable/#ssec-skip-go-tests
+  checkFlags = [
+    "-skip=TestActivate/trivy"
+  ];
+
+  # https://nixos.org/manual/nixpkgs/stable/#ssec-skip-go-tests
   ldflags = [
-    "-s" "-w"
+    "-s"
+    "-w"
     "-X main.version=v${version}"
     "-X main.commit=${src.rev}"
     "-X main.date=1970-01-01-00:00:01"
@@ -28,6 +40,10 @@ buildGoModule rec {
     homepage = "https://k8sgpt.ai";
     changelog = "https://github.com/k8sgpt-ai/k8sgpt/releases/tag/v${version}";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ developer-guy kranurag7 ];
+    maintainers = with lib.maintainers; [
+      developer-guy
+      kranurag7
+      mrgiles
+    ];
   };
 }
