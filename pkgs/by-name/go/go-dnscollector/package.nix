a5d0a7e39000e5ba85556d503af61eea8c95f74b
Vincent Palmer <shift@someone.section.me>
2024-10-25 15:26:52 +0200

    go-dnscollector: init at 1.1.0
    Signed-off-by: Vincent Palmer <shift@someone.section.me>

---
+++ pkgs/by-name/go/go-dnscollector/package.nix
@@ -0,0 +1,26 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "go-dnscollector";
+  version = "1.1.0";
+
+  src = fetchFromGitHub {
+    owner = "dmachard";
+    repo = "go-dnscollector";
+    rev = "v${version}";
+    sha256 = "sha256-+EE9miEm1sHxTHcQ+Zmnj5ljSisKtds9L+SLIvgIfb4="; # Replace with the actual SHA256 of the repo
+  };
+
+  # Set Go module dependencies
+  vendorHash = "sha256-Njv8EGPS45NpCs0+poBxVGWLHhH+mSZuI80kIpsijDQ=";
+  # Specify any subpackages if necessary
+  subPackages = [ "." ];
+
+  meta = with lib; {
+    description = "Ingesting, pipelining, and enhancing your DNS logs with usage indicators, security analysis, and additional metadata.";
+    homepage = "https://github.com/dmachard/go-dnscollector";
+    license = licenses.mit;
+    maintainers = with maintainers; [ shift ];
+  };
+}
+
