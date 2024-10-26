042a4db5741985e0fdefda763699233989b07f51
Morgan Helton <mhelton@gmail.com>
2024-10-26 11:15:58 -0500

    readarr: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11647,8 +11647,6 @@ with pkgs;
 
   react-native-debugger = callPackage ../development/tools/react-native-debugger { };
 
-  readarr = callPackage ../servers/readarr { };
-
   read-edid = callPackage ../os-specific/linux/read-edid { };
 
   readstat = callPackage ../applications/science/math/readstat { };
