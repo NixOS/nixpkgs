dccd62a88ceebf39f4203f8858abacaad31beebf
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-26 08:22:35 +0000

    zoekt: 3.7.2-2-unstable-2024-10-01 -> 3.7.2-2-unstable-2024-10-24

--- pkgs/by-name/zo/zoekt/package.nix
+++ pkgs/by-name/zo/zoekt/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule {
   pname = "zoekt";
-  version = "3.7.2-2-unstable-2024-10-01";
+  version = "3.7.2-2-unstable-2024-10-24";
 
   src = fetchFromGitHub {
     owner = "sourcegraph";
     repo = "zoekt";
-    rev = "650136078a98ef99acf4159310a2c12fd25075d7";
-    hash = "sha256-MtN/x5o0iHZeDqIQCuio8y5pmZXq5qFIE6FySgIY2CY=";
+    rev = "bfd8ee868c4c3fe509fa0fd4f2b8c68d84805ff9";
+    hash = "sha256-hoKMD/nTX0r2PEM0qRhAQFXM45UhDztwK0epL2EIMY8=";
   };
 
-  vendorHash = "sha256-+ayixWCD2e+7Nh9WJmDAloSzp63v9hQYQd8UMuo8qxQ=";
+  vendorHash = "sha256-QZysaEBZ1/ISPRkUPr6UIEUlWv/aHEwk8B/wxaYe7zU=";
 
   nativeCheckInputs = [
     git
