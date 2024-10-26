358f74f65df292cd104c81f830b345a778d41943
Reno Dakota <170618376+paparodeo@users.noreply.github.com>
2024-10-26 04:36:07 +0000

    rehex: fix darwin
    add libiconv to fix darwin build and remove now unneeded frameworks

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11489,9 +11489,7 @@ with pkgs;
 
   remarshal = with python3Packages; toPythonApplication remarshal;
 
-  rehex = darwin.apple_sdk_11_0.callPackage ../applications/editors/rehex {
-    inherit (darwin.apple_sdk_11_0.frameworks) Carbon Cocoa IOKit;
-  };
+  rehex = callPackage ../applications/editors/rehex { };
 
   rio = callPackage ../applications/terminal-emulators/rio { };
 
