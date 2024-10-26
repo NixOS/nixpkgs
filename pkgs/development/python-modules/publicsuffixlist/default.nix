1af9eb5f48e0426730dfa4958083a711d8859dbf
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-26 11:45:43 +0200

    python312Packages.publicsuffixlist: 1.0.2.20241023 -> 1.0.2.20241026
    Changelog: https://github.com/ko-zu/psl/blob/v1.0.2.20241026-gha/CHANGES.md

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "publicsuffixlist";
-  version = "1.0.2.20241023";
+  version = "1.0.2.20241026";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-WQs4DJygcyOTzDZHlbjW+uZAna5oswDDk4NwgS/Vr1c=";
+    hash = "sha256-vCTHs5HFW21d7W0G/W92ugA2F9JbqE6IwmHHHb6rdUs=";
   };
 
   build-system = [ setuptools ];
