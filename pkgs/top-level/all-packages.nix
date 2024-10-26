c5048f97ec943f819a22b8c5290bfc1e9516ad44
Robert James Hernandez <rob@sarcasticadmin.com>
2024-10-26 18:55:55 +0000

    pat: move to pkg/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29831,8 +29831,6 @@ with pkgs;
 
   passky-desktop = callPackage ../applications/misc/passky-desktop { };
 
-  pat = callPackage ../applications/radio/pat { };
-
   pinboard = with python3Packages; toPythonApplication pinboard;
 
   pinboard-notes-backup = haskell.lib.compose.justStaticExecutables haskellPackages.pinboard-notes-backup;
