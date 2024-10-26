0eb5eee4bbe013d2a7a6d2a2940c8d86ef614582
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-10-26 23:03:07 +0530

    libinput-gestures: nixfmt-rfc-style
    replace -> replace-fail in substituteInPlace
    
    rec -> finalAttrs

--- pkgs/tools/inputmethods/libinput-gestures/default.nix
+++ pkgs/tools/inputmethods/libinput-gestures/default.nix
@@ -1,16 +1,23 @@
-{ lib, stdenv, fetchFromGitHub, makeWrapper,
-  libinput, wmctrl, python3,
-  coreutils, xdotool ? null,
-  extraUtilsPath ? lib.optional (xdotool != null) xdotool
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  libinput,
+  wmctrl,
+  python3,
+  coreutils,
+  xdotool ? null,
+  extraUtilsPath ? lib.optional (xdotool != null) xdotool,
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libinput-gestures";
   version = "2.77";
 
   src = fetchFromGitHub {
     owner = "bulletmark";
     repo = "libinput-gestures";
-    rev = version;
+    rev = "ref/tags/${finalAttrs.version}";
     hash = "sha256-eMXNlSgQSuN+/5SXJQjsylC1ygHS87sIEmnVGFk3pzA=";
   };
   patches = [
@@ -21,36 +28,35 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ python3 ];
 
-  postPatch =
-    ''
-      substituteInPlace libinput-gestures-setup --replace /usr/ /
+  postPatch = ''
+    substituteInPlace libinput-gestures-setup --replace-fail /usr/ /
 
-      substituteInPlace libinput-gestures \
-        --replace      /etc     "$out/etc" \
-        --subst-var-by libinput "${libinput}/bin/libinput" \
-        --subst-var-by wmctrl   "${wmctrl}/bin/wmctrl"
-    '';
-  installPhase =
-    ''
-      runHook preInstall
-      ${stdenv.shell} libinput-gestures-setup -d "$out" install
-      runHook postInstall
-    '';
-  postFixup =
-    ''
-      rm "$out/bin/libinput-gestures-setup"
-      substituteInPlace "$out/share/systemd/user/libinput-gestures.service" --replace "/usr" "$out"
-      substituteInPlace "$out/share/applications/libinput-gestures.desktop" --replace "/usr" "$out"
-      chmod +x "$out/share/applications/libinput-gestures.desktop"
-      wrapProgram "$out/bin/libinput-gestures" --prefix PATH : "${lib.makeBinPath ([coreutils] ++ extraUtilsPath)}"
-    '';
+    substituteInPlace libinput-gestures \
+      --replace-fail      /etc     "$out/etc" \
+      --subst-var-by libinput "${libinput}/bin/libinput" \
+      --subst-var-by wmctrl   "${wmctrl}/bin/wmctrl"
+  '';
+  installPhase = ''
+    runHook preInstall
+    ${stdenv.shell} libinput-gestures-setup -d "$out" install
+    runHook postInstall
+  '';
+  postFixup = ''
+    rm "$out/bin/libinput-gestures-setup"
+    substituteInPlace "$out/share/systemd/user/libinput-gestures.service" --replace "/usr" "$out"
+    substituteInPlace "$out/share/applications/libinput-gestures.desktop" --replace "/usr" "$out"
+    chmod +x "$out/share/applications/libinput-gestures.desktop"
+    wrapProgram "$out/bin/libinput-gestures" --prefix PATH : "${
+      lib.makeBinPath ([ coreutils ] ++ extraUtilsPath)
+    }"
+  '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/bulletmark/libinput-gestures";
     description = "Gesture mapper for libinput";
     mainProgram = "libinput-gestures";
-    license = licenses.gpl3Plus;
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ teozkr ];
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ teozkr ];
   };
-}
+})
