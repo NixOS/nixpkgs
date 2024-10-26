d3f4dbd173ba17ec8e9ec6ce0b73c7ea869722f1
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-27 00:29:25 +0200

    python312Packages.yaramod: 3.23.0 -> 4.0.0
    Diff: https://github.com/avast/yaramod/compare/refs/tags/v3.23.0...v4.0.0
    
    Changelog: https://github.com/avast/yaramod/blob/v4.0.0/CHANGELOG.md

--- pkgs/development/python-modules/yaramod/default.nix
+++ pkgs/development/python-modules/yaramod/default.nix
@@ -21,7 +21,7 @@ let
 in
 buildPythonPackage rec {
   pname = "yaramod";
-  version = "3.23.0";
+  version = "4.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = "avast";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-TB0dnWA+5beyHAoYUjqocmw5uGFgo/h9eKDbuKbmfsw=";
+    hash = "sha256-YbsNFtDk5u5UyTsS1aGKUv/HWYxEwERinZ3G84/mC9o=";
   };
 
   postPatch = ''
