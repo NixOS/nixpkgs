e20f75b610556f8e488e51c5a953f692ffb2bc88
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-26 10:19:43 +0200

    python312Packages.google-cloud-datacatalog: 3.20.1 -> 3.21.0
    Changelog: https://github.com/googleapis/google-cloud-python/blob/google-cloud-datacatalog-v3.21.0/packages/google-cloud-datacatalog/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-datacatalog/default.nix
+++ pkgs/development/python-modules/google-cloud-datacatalog/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-datacatalog";
-  version = "3.20.1";
+  version = "3.21.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "google_cloud_datacatalog";
     inherit version;
-    hash = "sha256-ggzQ85PzgEk4nnD9ykYd1VGPmtf2T6guAzv51SLlkmI=";
+    hash = "sha256-JH3WW8v1VrTebNyEy7EDL9nvMaRf2gQTf6fnWbqMbCA=";
   };
 
   build-system = [ setuptools ];
