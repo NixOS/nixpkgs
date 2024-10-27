aab244ee4c387eaa6be9d48264b91b67df82489f
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-27 12:56:42 +0100

    python312Packages.reflex-chakra: update disabled

--- pkgs/development/python-modules/reflex-chakra/default.nix
+++ pkgs/development/python-modules/reflex-chakra/default.nix
@@ -11,7 +11,7 @@ buildPythonPackage rec {
   version = "0.6.2";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "reflex-dev";
