NixOS/nixpkgs

commit da16a24f0aff890f114d545b7d5fee89f458dc8b

python312Packages.grpcio: 1.64.1 -> 1.67.0
Changelog: https://github.com/grpc/grpc/releases/tag/v1.67.0

pkgs/development/python-modules/grpcio/default.nix
@@ -2,24 +2,24 @@
   lib,
   stdenv,
   buildPythonPackage,
+  c-ares,
+  cython,
   fetchPypi,
   grpc,
-  six,
-  protobuf,
-  enum34 ? null,
-  futures ? null,
-  isPy27,
-  pkg-config,
-  cython,
-  c-ares,
   openssl,
+  pkg-config,
+  protobuf,
+  pythonOlder,
+  setuptools,
   zlib,
 }:
 
 buildPythonPackage rec {
   pname = "grpcio";
-  format = "setuptools";
-  version = "1.66.2";
+  version = "1.67.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
@@ -31,6 +31,8 @@ buildPythonPackage rec {
     "dev"
   ];
 
+  build-system = [ setuptools ];
+
   nativeBuildInputs = [
     cython
     pkg-config
@@ -41,15 +43,8 @@ buildPythonPackage rec {
     openssl
     zlib
   ];
-  propagatedBuildInputs =
-    [
-      six
-      protobuf
-    ]
-    ++ lib.optionals (isPy27) [
-      enum34
-      futures
-    ];
+
+  dependencies = [ protobuf ];
 
   preBuild =
     ''
@@ -76,8 +71,9 @@ buildPythonPackage rec {
 
   meta = with lib; {
     description = "HTTP/2-based RPC framework";
-    license = licenses.asl20;
     homepage = "https://grpc.io/grpc/python/";
+    changelog = "https://github.com/grpc/grpc/releases/tag/v${version}";
+    license = licenses.asl20;
     maintainers = [ ];
   };
 }

commit 8fbfbf557a7aae568bf492710c34c9e75def8bf3

python312Packages.grpcio: fix src hash
The version was bumped, but src wasn't updated to reflect that.

Fixes: da16a24 ("python312Packages.grpcio: 1.64.1 -> 1.67.0")

pkgs/development/python-modules/grpcio/default.nix
@@ -5,7 +5,6 @@
   c-ares,
   cython,
   fetchPypi,
-  grpc,
   openssl,
   pkg-config,
   protobuf,
@@ -23,7 +22,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-VjWIxYe3XDS5KLxChUjlsA6jjEaXIYGk2Ldbp+PyQjE=";
+    hash = "sha256-4JCyVT4Noch1RJyOdQc91EFd1xyb3mpAYkD99MDuRnw=";
   };
 
   outputs = [