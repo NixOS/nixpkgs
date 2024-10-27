d00f774d8d3fdd6859b17f8e5c581f2b1960da7f
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-27 17:40:17 +0100

    tun2proxy: add changelog to meta

--- pkgs/by-name/tu/tun2proxy/package.nix
+++ pkgs/by-name/tu/tun2proxy/package.nix
@@ -3,6 +3,7 @@
   rustPlatform,
   fetchFromGitHub,
 }:
+
 rustPlatform.buildRustPackage rec {
   pname = "tun2proxy";
   version = "0.6.0";
@@ -10,7 +11,7 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "tun2proxy";
     repo = "tun2proxy";
-    rev = "v${version}";
+    rev = "refs/tags/v${version}";
     hash = "sha256-LY7vVD85GVFqARYOBDeb4fS6rL2PwPXYXIAJtwm2goo=";
   };
 
@@ -23,6 +24,7 @@ rustPlatform.buildRustPackage rec {
   meta = {
     homepage = "https://github.com/tun2proxy/tun2proxy";
     description = "Tunnel (TUN) interface for SOCKS and HTTP proxies";
+    changelog = "https://github.com/tun2proxy/tun2proxy/releases/tag/v${version}";
     license = lib.licenses.mit;
     platforms = lib.platforms.linux;
     mainProgram = "tun2proxy-bin";
