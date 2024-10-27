c9414dab1376f81a60680ac4cc41768612b5b2bc
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-27 12:47:49 +0000

    fflogs: 8.14.0 -> 8.14.21

--- pkgs/by-name/ff/fflogs/package.nix
+++ pkgs/by-name/ff/fflogs/package.nix
@@ -5,10 +5,10 @@
 
 let
   pname = "fflogs";
-  version = "8.14.0";
+  version = "8.14.21";
   src = fetchurl {
     url = "https://github.com/RPGLogs/Uploaders-fflogs/releases/download/v${version}/fflogs-v${version}.AppImage";
-    hash = "sha256-hHgPtr25X2vZY+MKQn9FgkMLrF5vgklMQHzx4ksGrDk=";
+    hash = "sha256-sBC2qvtSje2uXzSZAYXBFmycKxf0B6/JZhOvWNyMp20=";
   };
   extracted = appimageTools.extractType2 { inherit pname version src; };
 in
