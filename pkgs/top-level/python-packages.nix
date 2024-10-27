02ba8257a947f7b6e1a0eda726697713f1074617
happysalada <raphael@megzari.com>
2024-10-27 08:12:07 -0400

    pythonPackages.einx: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3967,6 +3967,8 @@ self: super: with self; {
 
   einops = callPackage ../development/python-modules/einops { };
 
+  einx = callPackage ../development/python-modules/einx { };
+
   eiswarnung = callPackage ../development/python-modules/eiswarnung { };
 
   elgato = callPackage ../development/python-modules/elgato { };
