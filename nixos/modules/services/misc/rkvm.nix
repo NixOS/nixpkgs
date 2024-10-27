47a069172763378567d20c6cf43c4d6750f604c3
Niklas Gollenstede <gollenstede@ibr.cs.tu-bs.de>
2024-10-27 14:41:00 +0100

    nixos/rkvm: refactor (deduplicate options)

--- nixos/modules/services/misc/rkvm.nix
+++ nixos/modules/services/misc/rkvm.nix
@@ -1,134 +1,112 @@
 { options, config, pkgs, lib, ... }:
-
-with lib;
 let
   opt = options.services.rkvm;
   cfg = config.services.rkvm;
   toml = pkgs.formats.toml { };
+  inherit (lib) mkOption mkEnableOption types mkIf;
 in
 {
   meta.maintainers = [ lib.maintainers.NiklasGollenstede ];
 
-  options.services.rkvm = {
+  options.services.rkvm = let
+
     enable = mkOption {
       default = cfg.server.enable || cfg.client.enable;
-      defaultText = literalExpression "config.${opt.server.enable} || config.${opt.client.enable}";
+      defaultText = lib.literalExpression "config.${opt.server.enable} || config.${opt.client.enable}";
       type = types.bool;
       description = ''
         Whether to enable rkvm, a Virtual KVM switch for Linux machines.
       '';
     };
 
-    package = mkPackageOption pkgs "rkvm" { };
+    package = lib.mkPackageOption pkgs "rkvm" { };
 
-    server = {
-      enable = mkEnableOption "the rkvm server daemon (input transmitter)";
+    mkCommonOptions = component: extraSettings: {
+      enable = mkEnableOption "the rkvm ${component} daemon (input ${
+        if component == "server" then "transmitter" else "receiver"
+      })";
 
       settings = mkOption {
-        type = types.submodule
-          {
-            freeformType = toml.type;
-            options = {
-              listen = mkOption {
-                type = types.str;
-                default = "0.0.0.0:5258";
-                description = ''
-                  An internet socket address to listen on, either IPv4 or IPv6.
-                '';
-              };
-
-              switch-keys = mkOption {
-                type = types.listOf types.str;
-                default = [ "left-alt" "left-ctrl" ];
-                description = ''
-                  A key list specifying a host switch combination.
-
-                  _A list of key names is available in <https://github.com/htrefil/rkvm/blob/master/switch-keys.md>._
-                '';
-              };
-
-              certificate = mkOption {
-                type = types.path;
-                default = "/etc/rkvm/certificate.pem";
-                description = ''
-                  TLS certificate path.
-
-                  ::: {.note}
-                  This should be generated with {command}`rkvm-certificate-gen`.
-                  :::
-                '';
-              };
-
-              key = mkOption {
-                type = types.path;
-                default = "/etc/rkvm/key.pem";
-                description = ''
-                  TLS key path.
-
-                  ::: {.note}
-                  This should be generated with {command}`rkvm-certificate-gen`.
-                  :::
-                '';
-              };
-
-              password = mkOption {
-                type = types.str;
-                description = ''
-                  Shared secret token to authenticate the client.
-                  Make sure this matches your client's config.
-                '';
-              };
-            };
-          };
-
+        description = "Structured ${component} daemon configuration";
         default = { };
-        description = "Structured server daemon configuration";
-      };
-    };
+        type = types.submodule {
+          freeformType = toml.type;
+          options = ({
 
-    client = {
-      enable = mkEnableOption "the rkvm client daemon (input receiver)";
+            certificate = mkOption {
+              type = types.path; default = "/etc/rkvm/certificate.pem";
+              description = ''
+                Path to the TLS public certificate file.
 
-      settings = mkOption {
-        type = types.submodule
-          {
-            freeformType = toml.type;
-            options = {
-              server = mkOption {
-                type = types.str;
-                example = "192.168.0.123:5258";
-                description = ''
-                  An RKVM server's internet socket address, either IPv4 or IPv6.
-                '';
-              };
-
-              certificate = mkOption {
-                type = types.path;
-                default = "/etc/rkvm/certificate.pem";
-                description = ''
-                  TLS ceritficate path.
-
-                  ::: {.note}
-                  This should be generated with {command}`rkvm-certificate-gen`.
-                  :::
-                '';
-              };
-
-              password = mkOption {
-                type = types.str;
-                description = ''
-                  Shared secret token to authenticate the client.
-                  Make sure this matches your server's config.
-                '';
-              };
+                ::: {.note}
+                This can be generated with {command}`rkvm-certificate-gen`.
+                :::
+              '';
             };
-          };
 
-        default = {};
-        description = "Structured client daemon configuration";
+            password = mkOption {
+              type = types.nullOr types.str; default = null;
+              description = ''
+                Shared **secret** token to authenticate the client.
+
+                ::: {.warning}
+                You should NOT set this option directly, since this is a secret and all options set in nix itself will be world-readable.
+                Use {option}`..passwordFile` instead.
+                :::
+              '';
+            };
+
+          } // extraSettings);
+        };
+
       };
     };
 
+  in {
+    inherit enable package;
+
+    server = mkCommonOptions "server" {
+
+      listen = mkOption {
+        type = types.str;
+        default = "0.0.0.0:5258";
+        description = ''
+          Hostname or IPv4/6 address and port number to listen on.
+        '';
+      };
+
+      key = mkOption {
+        type = types.path; default = "/etc/rkvm/key.pem";
+        description = ''
+          Path to the TLS private key file (that matches the {option}`.certificate`).
+
+          ::: {.warning}
+          As a secret, this should not be stored in the nix store. Do not add the (unencrypted) file to your repository, and do not use an unquoted path to reference it.
+          :::
+        '';
+      };
+
+      switch-keys = mkOption {
+        type = types.listOf types.str;
+        default = [ "left-alt" "left-ctrl" ];
+        description = ''
+          A list of keys that, when pressed simultaneously, switch between the clients registered at the server (for this, the server itself is considered a client).
+
+          _A list of available key names is available at <https://github.com/htrefil/rkvm/blob/${cfg.package.version}/switch-keys.md>._
+        '';
+      };
+
+    };
+
+    client = mkCommonOptions "client" {
+      server = mkOption {
+        type = types.str;
+        example = "192.168.0.123:5258";
+        description = ''
+          An RKVM server's internet socket address, either IPv4 or IPv6.
+        '';
+      };
+    };
   };
 
   config = mkIf cfg.enable {
