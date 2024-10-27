fa192bdc1240ee804384c85895b6ad6fe12f20af
Nico Felbinger <nico@felbinger.eu>
2024-10-27 09:51:05 +0100

    nixos/suricata: add module to modules-list
    (cherry picked from commit f2de541dfd15c0ce2be09d255e4f0eaa3e32d1a4)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1239,6 +1239,7 @@
   ./services/networking/sunshine.nix
   ./services/networking/supplicant.nix
   ./services/networking/supybot.nix
+  ./services/networking/suricata/default.nix
   ./services/networking/syncplay.nix
   ./services/networking/syncthing-relay.nix
   ./services/networking/syncthing.nix
