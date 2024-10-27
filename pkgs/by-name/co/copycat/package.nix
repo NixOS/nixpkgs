557abf7321d9925aae2f64d3e9e95a8e931362b7
deekahy <lennart.diego.kahn@gmail.com>
2024-10-27 22:20:04 +0100

    copycat: 001 -> 002

--- pkgs/by-name/co/copycat/package.nix
+++ pkgs/by-name/co/copycat/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ccat";
-  version = "001";
+  version = "002";
 
   src = fetchFromGitHub {
     owner = "DeeKahy";
     repo = "CopyCat";
     rev = "refs/tags/${version}";
-    hash = "sha256-zllxQifRMNEMa3RO5WKrwGAUf1xQg6YrQBzIHzy43F0=";
+    hash = "sha256-0pqC6fxuvqOPuO10Em63tFguc3VJNnniPCHM6TcFDN0=";
   };
 
-  cargoHash = "sha256-LYVhvq5l+PCZXW+elWi3zZFxLekgPn+plo4dybbLK9g=";
+  cargoHash = "sha256-oNX1MUpOjRG02FHOU7zpktLAYKu/1+R2d96jC/VA0co=";
 
   buildInputs = lib.optionals (stdenv.isDarwin) [
     darwin.apple_sdk_11_0.frameworks.AppKit
