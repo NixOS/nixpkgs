4da8422970ef2d7fe91dc8ef2412ee5fbd8ee3b3
Ashish SHUKLA <ashish.is@lostca.se>
2024-10-27 14:39:12 +0000

    ugrep: 7.0.0 -> 7.0.1
    Changes: https://github.com/Genivia/ugrep/releases/tag/v7.0.1

--- pkgs/tools/text/ugrep/default.nix
+++ pkgs/tools/text/ugrep/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ugrep";
-  version = "7.0.0";
+  version = "7.0.1";
 
   src = fetchFromGitHub {
     owner = "Genivia";
     repo = "ugrep";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-bq/j+9FVKs8c7YHo1wHiQ3eFgD1dpe1buwfgrH2SZKI=";
+    hash = "sha256-Kekt4RnVGtyfLhVkAZQTM0VQ8lAg7GJ/VR/1v+SUVOY=";
   };
 
   buildInputs = [
