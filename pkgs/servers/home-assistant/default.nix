ba34a644c8e4c26452a6ae6c82dd6313fd444830
Robert Schütz <mail@dotlambda.de>
2024-10-27 19:00:51 -0700

    python312Packages.versioningit: don't test with pydantic_1

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -335,10 +335,6 @@ let
         ];
       };
 
-      versioningit = super.versioningit.overridePythonAttrs {
-        doCheck = false;
-      };
-
       # Pinned due to API changes ~1.0
       vultr = super.vultr.overridePythonAttrs (oldAttrs: rec {
         version = "0.1.2";
