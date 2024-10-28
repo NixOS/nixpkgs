4169bfe01f3a17b7c9e98beed448452e4ca60d62
Patka <patka@patka.dev>
2024-10-28 16:10:06 +0100

    paratest: 7.4.3 -> 7.6.0

--- pkgs/by-name/pa/paratest/package.nix
+++ pkgs/by-name/pa/paratest/package.nix
@@ -3,19 +3,19 @@
 , lib
 }:
 
-(php.withExtensions ({ enabled, all }: enabled ++ [ all.pcov ])).buildComposerProject (finalAttrs: {
+(php.withExtensions ({ enabled, all }: enabled ++ [ all.pcov ])).buildComposerProject2 (finalAttrs: {
   pname = "paratest";
-  version = "7.4.3";
+  version = "7.6.0";
 
   src = fetchFromGitHub {
     owner = "paratestphp";
     repo = "paratest";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Shf/fsGhDmupFn/qERzXGg3ko7mBgUqYzafO/VPqmoU=";
+    hash = "sha256-p7m/MDHy+NOh+MnoIWy74Ipm/5hevp4x6Qwn4uPIEAM=";
   };
 
   composerLock = ./composer.lock;
-  vendorHash = "sha256-9KFh6Vwzt17v6WlEutRpwCauLOcj05hR4JGDcPbYL1U=";
+  vendorHash = "sha256-NQWwfSYgvAmvWnr563vAKh+IdFRDB/CJZReDUzftOvw=";
 
   meta = {
     changelog = "https://github.com/paratestphp/paratest/releases/tag/v${finalAttrs.version}";
