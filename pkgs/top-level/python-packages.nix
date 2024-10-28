7451482dbe2fc2108b65b554d2a27047c4292e11
Reno Dakota <170618376+paparodeo@users.noreply.github.com>
2024-10-28 09:45:14 +0000

    python311Packages.tesnsorflow: fix evaulation error
    remove invalid argument grpc argument from override as it was removed in
    8fbfbf557a7aae568bf492710c34c9e75def8bf3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15478,7 +15478,6 @@ self: super: with self; {
       };
       grpcioTF = self.grpcio.override {
         protobuf = protobufTF;
-        grpc = grpcTF;
       };
       tensorboard-plugin-profileTF = self.tensorboard-plugin-profile.override {
         protobuf = protobuf-pythonTF;
