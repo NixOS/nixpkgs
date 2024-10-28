ce50474f660bfaaf55cefbdf1e30685e1b156641
wxt <3264117476@qq.com>
2024-10-28 15:09:29 +0800

    pyditz: remove testPhase

--- pkgs/by-name/py/pyditz/package.nix
+++ pkgs/by-name/py/pyditz/package.nix
@@ -7,26 +7,27 @@
 python3Packages.buildPythonApplication rec {
   pname = "pyditz";
   version = "0.11";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-2gNlrpBk4wxKJ1JvsNeoAv2lyGUc2mmQ0Xvn7eiaJVE=";
   };
-  nativeBuildInputs = with python3Packages; [ setuptools-scm ];
-  propagatedBuildInputs = with python3Packages; [
+
+  build-system = with python3Packages; [ setuptools-scm ];
+
+  dependencies = with python3Packages; [
     pyyaml
     six
     jinja2
     cerberus
   ];
 
-  nativeCheckInputs = with python3Packages; [ unittestCheckHook ];
-
-  meta = with lib; {
-    homepage = "https://pypi.org/project/pyditz/";
+  meta = {
+    homepage = "https://hg.sr.ht/~zondo/pyditz";
     description = "Drop-in replacement for the Ditz distributed issue tracker";
-    maintainers = [ maintainers.ilikeavocadoes ];
-    license = licenses.lgpl2;
-    platforms = platforms.linux;
+    maintainers = with lib.maintainers; [ ilikeavocadoes ];
+    license = lib.licenses.lgpl2Plus;
+    platforms = lib.platforms.linux;
   };
 }
