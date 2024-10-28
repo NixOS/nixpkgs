4402e56386df52ce214ca375d69009577f9c2821
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-28 19:38:16 +0100

    python313Packages.dulwich: 0.22.1 -> 0.22.2
    https://github.com/jelmer/dulwich/blob/dulwich-0.22.2/NEWS

--- pkgs/development/python-modules/dulwich/default.nix
+++ pkgs/development/python-modules/dulwich/default.nix
@@ -20,7 +20,7 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.22.1";
+  version = "0.22.2";
   pname = "dulwich";
   format = "setuptools";
 
@@ -30,23 +30,15 @@ buildPythonPackage rec {
     owner = "jelmer";
     repo = "dulwich";
     rev = "refs/tags/dulwich-${version}";
-    hash = "sha256-bf3ZUMX4afpdTBpFnx0HMyzCNG6V/p4eOl36djxGbtk=";
+    hash = "sha256-yM1U8DowevtZt6qqSybur7xmwRnlJ0rwV0qJfQf+ZIs=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      name = "dulwich-geventhttpclient-api-breakage.patch";
-      url = "https://github.com/jelmer/dulwich/commit/5f0497de9c37ac4f4e8f27bed8decce13765d3df.patch";
-      hash = "sha256-0GgDgmYuLCsMc9nRRLNL2W6WYrkZ/1ZnZBQusEAzLKI=";
-    })
-  ];
 
   build-system = [
     setuptools
     setuptools-rust
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     urllib3
   ];
 
@@ -63,12 +55,12 @@ buildPythonPackage rec {
   nativeCheckInputs =
     [
       gevent
-      geventhttpclient
       git
       glibcLocales
       unittestCheckHook
     ]
-    ++ lib.flatten (lib.attrValues optional-dependencies);
+    ++ lib.flatten (lib.attrValues optional-dependencies)
+    ++ lib.optionals (pythonOlder "3.13") [ geventhttpclient ];
 
   preCheck = ''
     # requires swift config file
