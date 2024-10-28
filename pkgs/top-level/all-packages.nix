4f3646aea24fb5b480a30fb7dc774b13a8b83d34
Nikolay Korotkiy <sikmir@disroot.org>
2024-10-28 14:38:56 +0400

    dq: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6956,8 +6956,6 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Security;
   };
 
-  dq = callPackage ../tools/networking/dq { };
-
   dool = callPackage ../tools/system/dool { };
 
   dosfstools = callPackage ../tools/filesystems/dosfstools { };
