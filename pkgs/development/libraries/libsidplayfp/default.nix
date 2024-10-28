2e48e944c1f2d0bae86bb65bf6a9b4026716f671
OPNA2608 <opna2608@protonmail.com>
2024-10-29 00:27:12 +0100

    libsidplayfp: 2.10.0 -> 2.10.1

--- pkgs/development/libraries/libsidplayfp/default.nix
+++ pkgs/development/libraries/libsidplayfp/default.nix
@@ -19,14 +19,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libsidplayfp";
-  version = "2.10.0";
+  version = "2.10.1";
 
   src = fetchFromGitHub {
     owner = "libsidplayfp";
     repo = "libsidplayfp";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-pW2B/KClCpfWQjZNQQnXPtXDJlm5NKb4ivPlFUI/vFA=";
+    hash = "sha256-yZ2IiSzl78N/jrhKls/l2klnePJYKU1NCnZcBKUWiuU=";
   };
 
   outputs = [ "out" ] ++ lib.optionals docSupport [ "doc" ];
