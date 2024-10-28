33a040e9b74d2393969d8442dd027734cf7b48ac
griffi-gh <prasol258@gmail.com>
2024-10-28 11:07:32 +0100

    maintainers: add griffi-gh

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8297,6 +8297,13 @@
     githubId = 201997;
     name = "Eric Seidel";
   };
+  griffi-gh = {
+    name = "Alex Prasol";
+    email = "prasol258@gmail.com";
+    matrix = "@voxel:nyanbinary.rs";
+    github = "griffi-gh";
+    githubId = 45996170;
+  };
   grimmauld = {
     name = "Sören Bender";
     email = "soeren@benjos.de";
