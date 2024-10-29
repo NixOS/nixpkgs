8a123abf9c4bcc2a08a84bd8c9b1f6a23705fafe
R. RyanTM <ryantm-bot@ryantm.com>
2024-10-29 13:17:52 +1000

    go2rtc: 1.9.4 -> 1.9.5 (#352016)
    https://github.com/AlexxIT/go2rtc/releases/tag/v1.9.5

--- pkgs/tools/video/go2rtc/default.nix
+++ pkgs/tools/video/go2rtc/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "go2rtc";
-  version = "1.9.4";
+  version = "1.9.5";
 
   src = fetchFromGitHub {
     owner = "AlexxIT";
     repo = "go2rtc";
     rev = "refs/tags/v${version}";
-    hash = "sha256-LK2+hNgP9EhnAL9QKzvfn0+pYjuJJDB7l1fd57jeCWw=";
+    hash = "sha256-avak2cc8KDmlTrQutzULuvkWxSgdv3rMI7+G3+jNcdI=";
   };
 
-  vendorHash = "sha256-mUdUMZf3KhJyE0iv2yvWtkHa+pyXcv2RTZY+JtBLrSQ=";
+  vendorHash = "sha256-N8aJmxNQ/p2ddJG9DuIVVjcgzEC6TzD0sz992h3q0RU=";
 
   CGO_ENABLED = 0;
 
