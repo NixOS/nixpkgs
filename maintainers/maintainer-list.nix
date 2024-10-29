ad9f1a8962f141ed67e6be30ec067502ef02b421
mrbenjadmin <ben.strachan1@gmail.com>
2024-10-29 18:30:39 -0400

    maintainers: add mrbenjadmin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14695,6 +14695,12 @@
     githubId = 2072185;
     name = "Marc Scholten";
   };
+  mrbenjadmin = {
+    email = "mrbenjadmin@gmail.com";
+    github = "mrbenjadmin";
+    githubId = 68156310;
+    name = "Benjamin Strachan";
+  };
   mrcjkb = {
     email = "marc@jakobi.dev";
     matrix = "@mrcjk:matrix.org";
