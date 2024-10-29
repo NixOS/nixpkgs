f4ebf6a22fd0bfcdea296cd3501e9a557d55b78a
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-29 06:25:34 +0000

    melange: 0.11.3 -> 0.14.10

--- pkgs/development/tools/melange/default.nix
+++ pkgs/development/tools/melange/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "melange";
-  version = "0.11.3";
+  version = "0.14.10";
 
   src = fetchFromGitHub {
     owner = "chainguard-dev";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-+G6lSG3iaKWC1SU0cyeU1sU8r9VBUwwtU7D/yjZmu04=";
+    hash = "sha256-fHHIIlxKTF4q9LOeL3jyLS7HQ/IFmx+1fS/7PitrclM=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -25,7 +25,7 @@ buildGoModule rec {
     '';
   };
 
-  vendorHash = "sha256-GNY1ez7qC2aaEm8WAQqGy4vWZEZFQhze15r3QQZBNzA=";
+  vendorHash = "sha256-Okil1H6+O8YjTptI00mQQ1+04OiuO/zf1L8es4CdnNc=";
 
   subPackages = [ "." ];
 
