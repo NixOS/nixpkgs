0b45a711edaa0ae734facff6094df875500b7de6
Luca Mosetti <luka.mosetti@gmail.com>
2024-10-29 15:14:26 +0100

    clojure-lsp: 2024.04.22-11.50.26 -> 2024.08.05-18.16.00

--- pkgs/by-name/cl/clojure-lsp/package.nix
+++ pkgs/by-name/cl/clojure-lsp/package.nix
@@ -10,23 +10,23 @@
 
 buildGraalvmNativeImage rec {
   pname = "clojure-lsp";
-  version = "2024.04.22-11.50.26";
+  version = "2024.08.05-18.16.00";
 
   src = fetchFromGitHub {
     owner = "clojure-lsp";
     repo = "clojure-lsp";
     rev = version;
-    hash = "sha256-GyPIFYR+/BZ+vq6+yuer5HoVILXLWNw1sW8XpJ7q4SA=";
+    hash = "sha256-U66Zo0o50Pw1IAph/KmrR6pYGuOFVM9K6SzaSaYdx2M=";
   };
 
   jar = fetchurl {
     url = "https://github.com/clojure-lsp/clojure-lsp/releases/download/${version}/clojure-lsp-standalone.jar";
-    hash = "sha256-dB16225A7L3nWplvqlal+5gho+LmqqVGPN9dfasKaPk=";
+    hash = "sha256-rQlYrcmZwmBBWwa+28TcBFzzqmzTAM9Do3aH55Y6LFI=";
   };
 
   extraNativeImageBuildArgs = [
     # These build args mirror the build.clj upstream
-    # ref: https://github.com/clojure-lsp/clojure-lsp/blob/2024.04.22-11.50.26/cli/build.clj#L141-L144
+    # ref: https://github.com/clojure-lsp/clojure-lsp/blob/2024.08.05-18.16.00/cli/build.clj#L141-L144
     "--no-fallback"
     "--native-image-info"
     "--features=clj_easy.graal_build_time.InitClojureClasses"
