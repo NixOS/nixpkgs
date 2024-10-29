ed150c6969f9142f50652ab79b2175ebc9d325ea
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-29 20:53:55 +0100

    python312Packages.mitogen: 0.3.14 -> 0.3.15
    Diff: https://github.com/mitogen-hq/mitogen/compare/refs/tags/v0.3.14...v0.3.15
    
    Changelog: https://github.com/mitogen-hq/mitogen/blob/v0.3.15/docs/changelog.rst

--- pkgs/development/python-modules/mitogen/default.nix
+++ pkgs/development/python-modules/mitogen/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "mitogen";
-  version = "0.3.14";
+  version = "0.3.15";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "mitogen-hq";
     repo = "mitogen";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Gacn3EjyNq5LtjfbCczO+fqlq6+KgzxFs4d/K2xttHE=";
+    hash = "sha256-Zj5hVkIvKdyKLCUyXwhfDy5idH3jehmgIJNahmDdvJ4=";
   };
 
   build-system = [ setuptools ];
