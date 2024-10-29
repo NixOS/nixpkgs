03bbdf05ef1d75c46d478f0d268d1c9a919fcd47
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-10-29 09:30:14 -0400

    gdal: apply upstream patch for arrow 18

--- pkgs/development/libraries/gdal/default.nix
+++ pkgs/development/libraries/gdal/default.nix
@@ -94,6 +94,11 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://github.com/OSGeo/gdal/commit/40c3212fe4ba93e5176df4cd8ae5e29e06bb6027.patch";
       sha256 = "sha256-D55iT6E/YdpSyfN7KUDTh1gdmIDLHXW4VC5d6D9B7ls=";
     })
+    (fetchpatch {
+      name = "arrow-18.patch";
+      url = "https://github.com/OSGeo/gdal/commit/9a8c5c031404bbc81445291bad128bc13766cafa.patch";
+      sha256 = "sha256-tF46DmF7ZReqY8ACTTPXohWLsRn8lVxhKF1s+r254KM=";
+    })
   ];
 
   nativeBuildInputs = [
