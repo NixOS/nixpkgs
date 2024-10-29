f9d0de7fccd2eae95ddcdb099aca4e3938b21bef
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-29 23:19:57 +0100

    python312Packages.docstring-parser: 0.15 -> 0.16
    Diff: https://github.com/rr-/docstring_parser/compare/refs/tags/0.15...0.16
    
    Changelog: https://github.com/rr-/docstring_parser/blob/0.16/CHANGELOG.md

--- pkgs/development/python-modules/docstring-parser/default.nix
+++ pkgs/development/python-modules/docstring-parser/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "docstring-parser";
-  version = "0.15";
+  version = "0.16";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "rr-";
     repo = "docstring_parser";
     rev = "refs/tags/${version}";
-    hash = "sha256-rnDitZn/xI0I9KMQv6gxzVYevWUymDgyFETjAnRlEHw=";
+    hash = "sha256-xwV+mgCOC/MyCqGELkJVqQ3p2g2yw/Ieomc7k0HMXms=";
   };
 
   nativeBuildInputs = [ poetry-core ];
