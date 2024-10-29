95d8a648917ef0aa9b7754d0ef60a62db0327f43
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-29 20:22:08 +0100

    python312Packages.claripy: 9.2.125 -> 9.2.126
    Diff: https://github.com/angr/claripy/compare/refs/tags/v9.2.125...v9.2.126

--- pkgs/development/python-modules/claripy/default.nix
+++ pkgs/development/python-modules/claripy/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "claripy";
-  version = "9.2.125";
+  version = "9.2.126";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "claripy";
     rev = "refs/tags/v${version}";
-    hash = "sha256-UYB7QnrUtVGeWv0SiEOuOhb53AbcijghIh+DIWwLsE4=";
+    hash = "sha256-clS7O0WVY9jtuG1RF8c+14nlN9/U/29fEtVNnmGslQU=";
   };
 
   # z3 does not provide a dist-info, so python-runtime-deps-check will fail
