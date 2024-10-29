ada23c7e60c0040c8621157dfc986d539a8c96c1
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-29 00:50:03 +0000

    sentry-native: 0.7.10 -> 0.7.11

--- pkgs/development/libraries/sentry-native/default.nix
+++ pkgs/development/libraries/sentry-native/default.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "sentry-native";
-  version = "0.7.10";
+  version = "0.7.11";
 
   src = fetchFromGitHub {
     owner = "getsentry";
     repo = "sentry-native";
     rev = version;
-    hash = "sha256-1Pro2BXflitUGVbzvjwdSFqwWM/EeuDC3eHEj+7qwm8=";
+    hash = "sha256-IDtymBIG5ilTHyFZ/EA2YJK72wvz7kHn2o9gbu7ejKM=";
   };
 
   nativeBuildInputs = [
