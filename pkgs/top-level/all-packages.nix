2ed5f974951725675c99aee10724769712153cc3
nartsisss <nartsiss+git@proton.me>
2024-10-29 15:33:03 +0300

    telegram-bot-api: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32596,8 +32596,6 @@ with pkgs;
       else stdenv;
   };
 
-  telegram-bot-api = callPackage ../servers/telegram-bot-api { };
-
   tektoncd-cli = callPackage ../applications/networking/cluster/tektoncd-cli { };
 
   tg = python3Packages.callPackage ../applications/networking/instant-messengers/telegram/tg { };
