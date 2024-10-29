284f0944ae9352071ea2e9071085fc2f4d0846d7
nartsisss <nartsiss+git@proton.me>
2024-10-29 16:27:24 +0300

    cargo-modules: 0.16.8 -> 0.19.1

--- pkgs/development/tools/rust/cargo-modules/default.nix
+++ pkgs/development/tools/rust/cargo-modules/default.nix
@@ -2,26 +2,31 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-modules";
-  version = "0.16.8";
+  version = "0.19.1";
 
   src = fetchFromGitHub {
     owner = "regexident";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-F1hVKktU9zhdxXWhpLgymehJQlOX6WbXL2cqQGB+Y5k=";
+    repo = "cargo-modules";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-2wA34rQHHAvx+5NhmtugnGTMZ2Sj8MNpdkkIJhDK1HM=";
   };
 
-  cargoHash = "sha256-j3AYWKq7SrlNlsY7A80iwcWKW8wi6hx0U9UwDIQkXZs=";
+  cargoHash = "sha256-3R7rFkA2tObJZk5ypWPpMojEZvrDgKZsc1dN1wBJ63A=";
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
     darwin.apple_sdk.frameworks.CoreServices
   ];
 
   checkFlags = [
-    "--skip=cfg_test::smoke"
+    "--skip=cfg_test::with_tests::smoke"
+    "--skip=cfg_test::without_tests::smoke"
     "--skip=colors::ansi::smoke"
     "--skip=colors::plain::smoke"
     "--skip=colors::truecolor::smoke"
+    "--skip=fields::enum_fields"
+    "--skip=fields::struct_fields"
+    "--skip=fields::tuple_fields"
+    "--skip=fields::union_fields"
     "--skip=focus_on::glob_path::smoke"
     "--skip=focus_on::self_path::smoke"
     "--skip=focus_on::simple_path::smoke"
@@ -34,10 +39,6 @@ rustPlatform.buildRustPackage rec {
     "--skip=selection::no_modules::smoke"
     "--skip=selection::no_traits::smoke"
     "--skip=selection::no_types::smoke"
-    "--skip=fields::enum_fields"
-    "--skip=fields::struct_fields"
-    "--skip=fields::tuple_fields"
-    "--skip=fields::union_fields"
   ];
 
   meta = with lib; {
