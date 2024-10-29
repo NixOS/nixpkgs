5d85f83ba72141bdc8865383fd3d9ea123a1dc34
Sefa Eyeoglu <contact@scrumplex.net>
2024-10-29 20:10:06 +0100

    thunderbirdPackages.thunderbird-115: drop
    Last release was published in October 2024. On 2024/10/29 Mozilla
    published a security advisory and hasn't released an update for
    Thunderbird 115.
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32668,7 +32668,6 @@ with pkgs;
   thunderbird-unwrapped = thunderbirdPackages.thunderbird;
   thunderbird = wrapThunderbird thunderbird-unwrapped { };
 
-  thunderbird-115 = wrapThunderbird thunderbirdPackages.thunderbird-115 { };
   thunderbird-128 = wrapThunderbird thunderbirdPackages.thunderbird-128 { };
 
   thunderbird-bin = wrapThunderbird thunderbird-bin-unwrapped {
