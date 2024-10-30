47f8e090d22b3463599edc98db1a0985d45986a9
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-10-30 08:12:54 +0100

    ocamlPackages.dns: 9.0.0 → 9.1.0

--- pkgs/development/ocaml-modules/dns/default.nix
+++ pkgs/development/ocaml-modules/dns/default.nix
@@ -17,13 +17,13 @@
 
 buildDunePackage rec {
   pname = "dns";
-  version = "9.0.0";
+  version = "9.1.0";
 
   minimalOCamlVersion = "4.13";
 
   src = fetchurl {
     url = "https://github.com/mirage/ocaml-dns/releases/download/v${version}/dns-${version}.tbz";
-    hash = "sha256-HvXwTLVKw0wHV+xftL/z+yNA6UjxUTSdo/cC+s3qy/Y=";
+    hash = "sha256-jz7JWs8U9XQhm1RAponq4azCpJyx2KBm+bI6esaPRPA=";
   };
 
   propagatedBuildInputs = [ fmt logs ptime domain-name gmap ipaddr lru duration metrics base64 ohex ];
