0f0103b06b16af4368fe69ed6bebafce9e16a50c
Gliczy <129636582+Gliczy@users.noreply.github.com>
2024-10-30 23:01:15 +0100

    signal-desktop-beta: 7.30.0-beta.2-> 7.32.0-beta.1

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++ pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
@@ -2,7 +2,7 @@
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop-beta";
   dir = "Signal Beta";
-  version = "7.30.0-beta.2";
+  version = "7.32.0-beta.1";
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-mIdbaS4XNDh3wPTUHAyBqMQ67usp312e1McuFAGPrq0=";
+  hash = "sha256-WrHF7Y8OcEOr+W/Jhc+Hj5jCtOjKigZ02N9N/hE+XLA=";
 }
