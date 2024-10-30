8778d9c606a602e4951e0486de4d505059f8f817
aleksana <me@aleksana.moe>
2024-10-30 19:52:32 +0800

    toot: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32740,8 +32740,6 @@ with pkgs;
 
   tony = libsForQt5.callPackage ../applications/audio/tony { };
 
-  toot = callPackage ../applications/misc/toot { };
-
   toipe = callPackage ../applications/misc/toipe { };
 
   toxiproxy = callPackage ../development/tools/toxiproxy { };
