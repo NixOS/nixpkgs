11320a65971dfcc4d2deb9f4bdf0383a35a3055c
wxt <3264117476@qq.com>
2024-10-30 18:14:05 +0800

    zfp: nixfmt

--- pkgs/by-name/zf/zfp/package.nix
+++ pkgs/by-name/zf/zfp/package.nix
@@ -1,11 +1,20 @@
-{ cmake, cudatoolkit, fetchFromGitHub, gfortran, lib, llvmPackages, python3Packages, stdenv
-, config
-, enableCfp ? true
-, enableCuda ? config.cudaSupport
-, enableFortran ? builtins.elem stdenv.hostPlatform.system gfortran.meta.platforms
-, enableOpenMP ? true
-, enablePython ? true
-, enableUtilities ? true }:
+{
+  cmake,
+  cudatoolkit,
+  fetchFromGitHub,
+  gfortran,
+  lib,
+  llvmPackages,
+  python3Packages,
+  stdenv,
+  config,
+  enableCfp ? true,
+  enableCuda ? config.cudaSupport,
+  enableFortran ? builtins.elem stdenv.hostPlatform.system gfortran.meta.platforms,
+  enableOpenMP ? true,
+  enablePython ? true,
+  enableUtilities ? true,
+}:
 
 stdenv.mkDerivation rec {
   pname = "zfp";
@@ -20,10 +29,18 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake ];
 
-  buildInputs = lib.optional enableCuda cudatoolkit
+  buildInputs =
+    lib.optional enableCuda cudatoolkit
     ++ lib.optional enableFortran gfortran
     ++ lib.optional enableOpenMP llvmPackages.openmp
-    ++ lib.optionals enablePython (with python3Packages; [ cython numpy python ]);
+    ++ lib.optionals enablePython (
+      with python3Packages;
+      [
+        cython
+        numpy
+        python
+      ]
+    );
 
   # compile CUDA code for all extant GPUs so the binary will work with any GPU
   # and driver combination. to be ultimately solved upstream:
@@ -35,8 +52,10 @@ stdenv.mkDerivation rec {
     )
   '';
 
-  cmakeFlags = [
-  ] ++ lib.optional enableCfp "-DBUILD_CFP=ON"
+  cmakeFlags =
+    [
+    ]
+    ++ lib.optional enableCfp "-DBUILD_CFP=ON"
     ++ lib.optional enableCuda "-DZFP_WITH_CUDA=ON"
     ++ lib.optional enableFortran "-DBUILD_ZFORP=ON"
     ++ lib.optional enableOpenMP "-DZFP_WITH_OPENMP=ON"
