0670eadccd69d67302844b3ea8b7784fbe993a7b
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2024-10-30 10:40:59 +0100

    upscayl: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -26456,8 +26456,6 @@ with pkgs;
 
   upower = callPackage ../os-specific/linux/upower { };
 
-  upscayl = callPackage ../applications/graphics/upscayl { };
-
   usbguard = callPackage ../os-specific/linux/usbguard { };
 
   usbguard-notifier = callPackage ../os-specific/linux/usbguard-notifier { };
