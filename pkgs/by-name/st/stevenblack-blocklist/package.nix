ec406851c6e0f3b3158e2314f98dec6cd6b446ab
R. Ryantm <ryantm-bot@ryantm.com>
2024-10-30 13:03:17 +0000

    stevenblack-blocklist: 3.14.115 -> 3.14.127

--- pkgs/by-name/st/stevenblack-blocklist/package.nix
+++ pkgs/by-name/st/stevenblack-blocklist/package.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "stevenblack-blocklist";
-  version = "3.14.115";
+  version = "3.14.127";
 
   src = fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = "refs/tags/${finalAttrs.version}";
-    hash = "sha256-Iw3NUY0rWF+/lbGcO6zn96oNnRWRH33+8gs4e5yA1vM=";
+    hash = "sha256-85d3cDtmblTrTyCQJUgQgIh2KCAuQRZhBVuo7BXsJLQ=";
   };
 
   outputs = [
