4927202b750d301901786e13b9f0f86a9a1fe249
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-30 09:53:10 +0100

    python312Packages.pytapo: 3.3.30 -> 3.3.32

--- pkgs/development/python-modules/pytapo/default.nix
+++ pkgs/development/python-modules/pytapo/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pytapo";
-  version = "3.3.30";
+  version = "3.3.32";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-zSeDeGD/78bIoKm6B8BN4qWQE1ivNgyvnrGtgsekM3M=";
+    hash = "sha256-OYRcgX30O0/F+0H4x2jbPyVNh6xVeBSdfqbtuqUgoNE=";
   };
 
   build-system = [ setuptools ];
