9ce881027b195742f0c2e373e0c4f5199adf251a
Andrew Marshall <andrew@johnandrewmarshall.com>
2024-10-30 10:00:33 -0400

    zfs: remove recommendations to use unstable
    `pkgs.zfs_unstable` is pre-release software and may very well have
    critical bugs that make it unsuitable for general use. As such, we
    should not recommend it as a general solution to the problem of "my
    Kernel is too new for stable ZFS".

--- pkgs/os-specific/linux/zfs/generic.nix
+++ pkgs/os-specific/linux/zfs/generic.nix
@@ -237,8 +237,6 @@ let
 
       inherit maintainers;
       mainProgram = "zfs";
-      # If your Linux kernel version is not yet supported by zfs, try zfs_unstable.
-      # On NixOS set the option `boot.zfs.package = pkgs.zfs_unstable`.
       broken = buildKernel && (kernelCompatible != null) && !(kernelCompatible kernel);
     };
   };
