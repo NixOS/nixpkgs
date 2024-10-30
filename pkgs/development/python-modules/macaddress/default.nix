f363956c3c64f7e8450b99835d9d677ff655f41b
Wolfgang Walther <walther@technowledgy.de>
2024-10-30 21:56:25 +0100

    python3Packages.macaddress: remove useless $src from pytestFlagsArray
    We'd like to avoid bash eval in pytestFlagsArray, because we want to get
    rid of support for it. This case works just fine without.

--- pkgs/development/python-modules/macaddress/default.nix
+++ pkgs/development/python-modules/macaddress/default.nix
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     reprshed
   ];
 
-  pytestFlagsArray = [ "$src/test.py" ];
+  pytestFlagsArray = [ "test.py" ];
 
   meta = with lib; {
     homepage = "https://github.com/mentalisttraceur/python-macaddress";
