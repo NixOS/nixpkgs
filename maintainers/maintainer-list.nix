d3783c8e61344895a67a8751fdca3430ae16d773
Jorik <vanveenjorik@protonmail.com>
2024-10-30 22:58:24 +0100

    maintainers: add jorikvanveen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10578,6 +10578,12 @@
     github = "jordanisaacs";
     githubId = 19742638;
   };
+  jorikvanveen = {
+    email = "vanveenjorik@protonmail.com";
+    github = "jorikvanveen";
+    githubId = 33939820;
+    name = "Jorik van Veen";
+  };
   jorise = {
     email = "info@jorisengbers.nl";
     github = "JorisE";
