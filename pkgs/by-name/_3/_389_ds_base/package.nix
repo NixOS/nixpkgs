41769b4f0d3d459249f8239b21fa9fce2b3c69b3
wxt <3264117476@qq.com>
2024-10-30 18:59:59 +0800

    _389_ds_base: nixfmt

--- pkgs/by-name/_3/_389_ds_base/package.nix
+++ pkgs/by-name/_3/_389_ds_base/package.nix
@@ -1,38 +1,40 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, autoconf
-, automake
-, cargo
-, libtool
-, pkg-config
-, cracklib
-, lmdb
-, json_c
-, linux-pam
-, libevent
-, libxcrypt
-, nspr
-, nss
-, openldap
-, withOpenldap ? true
-, db
-, withBdb ? true
-, cyrus_sasl
-, icu
-, net-snmp
-, withNetSnmp ? true
-, krb5
-, pcre2
-, python3
-, rustPlatform
-, rustc
-, openssl
-, withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemd, systemd
-, zlib
-, rsync
-, withCockpit ? true
-, withAsan ? false
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoconf,
+  automake,
+  cargo,
+  libtool,
+  pkg-config,
+  cracklib,
+  lmdb,
+  json_c,
+  linux-pam,
+  libevent,
+  libxcrypt,
+  nspr,
+  nss,
+  openldap,
+  withOpenldap ? true,
+  db,
+  withBdb ? true,
+  cyrus_sasl,
+  icu,
+  net-snmp,
+  withNetSnmp ? true,
+  krb5,
+  pcre2,
+  python3,
+  rustPlatform,
+  rustc,
+  openssl,
+  withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemd,
+  systemd,
+  zlib,
+  rsync,
+  withCockpit ? true,
+  withAsan ? false,
 }:
 
 stdenv.mkDerivation rec {
@@ -61,29 +63,29 @@ stdenv.mkDerivation rec {
     python3
     cargo
     rustc
-  ]
-  ++ lib.optional withCockpit rsync;
+  ] ++ lib.optional withCockpit rsync;
 
-  buildInputs = [
-    cracklib
-    lmdb
-    json_c
-    linux-pam
-    libevent
-    libxcrypt
-    nspr
-    nss
-    cyrus_sasl
-    icu
-    krb5
-    pcre2
-    openssl
-    zlib
-  ]
-  ++ lib.optional withSystemd systemd
-  ++ lib.optional withOpenldap openldap
-  ++ lib.optional withBdb db
-  ++ lib.optional withNetSnmp net-snmp;
+  buildInputs =
+    [
+      cracklib
+      lmdb
+      json_c
+      linux-pam
+      libevent
+      libxcrypt
+      nspr
+      nss
+      cyrus_sasl
+      icu
+      krb5
+      pcre2
+      openssl
+      zlib
+    ]
+    ++ lib.optional withSystemd systemd
+    ++ lib.optional withOpenldap openldap
+    ++ lib.optional withBdb db
+    ++ lib.optional withNetSnmp net-snmp;
 
   postPatch = ''
     patchShebangs ./buildnum.py ./ldap/servers/slapd/mkDBErrStrs.py
@@ -98,27 +100,33 @@ stdenv.mkDerivation rec {
     tar -xzf ${cargoDeps} -C ./vendor --strip-components=1
   '';
 
-  configureFlags = [
-    "--enable-rust-offline"
-    "--enable-autobind"
-  ]
-  ++ lib.optionals withSystemd [
-    "--with-systemd"
-    "--with-systemdsystemunitdir=${placeholder "out"}/etc/systemd/system"
-  ] ++ lib.optionals withOpenldap [
-    "--with-openldap"
-  ] ++ lib.optionals withBdb [
-    "--with-db-inc=${lib.getDev db}/include"
-    "--with-db-lib=${lib.getLib db}/lib"
-  ] ++ lib.optionals withNetSnmp [
-    "--with-netsnmp-inc=${lib.getDev net-snmp}/include"
-    "--with-netsnmp-lib=${lib.getLib net-snmp}/lib"
-  ] ++ lib.optionals (!withCockpit) [
-    "--disable-cockpit"
-  ] ++ lib.optionals withAsan [
-    "--enable-asan"
-    "--enable-debug"
-  ];
+  configureFlags =
+    [
+      "--enable-rust-offline"
+      "--enable-autobind"
+    ]
+    ++ lib.optionals withSystemd [
+      "--with-systemd"
+      "--with-systemdsystemunitdir=${placeholder "out"}/etc/systemd/system"
+    ]
+    ++ lib.optionals withOpenldap [
+      "--with-openldap"
+    ]
+    ++ lib.optionals withBdb [
+      "--with-db-inc=${lib.getDev db}/include"
+      "--with-db-lib=${lib.getLib db}/lib"
+    ]
+    ++ lib.optionals withNetSnmp [
+      "--with-netsnmp-inc=${lib.getDev net-snmp}/include"
+      "--with-netsnmp-lib=${lib.getLib net-snmp}/lib"
+    ]
+    ++ lib.optionals (!withCockpit) [
+      "--disable-cockpit"
+    ]
+    ++ lib.optionals withAsan [
+      "--enable-asan"
+      "--enable-debug"
+    ];
 
   enableParallelBuilding = true;
   # Disable parallel builds as those lack some dependencies:
