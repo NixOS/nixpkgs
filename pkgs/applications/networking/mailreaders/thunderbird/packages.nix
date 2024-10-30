64900ea6545328e2dd3bf91f1c8d9fad3260eba5
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-30 15:21:41 +0100

    thunderbirdPackages.thunderbird-115: mark eol

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -27,6 +27,7 @@ let
       broken = stdenv.buildPlatform.is32bit; # since Firefox 60, build on 32-bit platforms fails with "out of memory".
                                              # not in `badPlatforms` because cross-compilation on 64-bit machine might work.
       license = licenses.mpl20;
+      knownVulnerabilities = lib.optionals (lib.versionOlder version "116") [ "Thunderbird 115 support ended in Octoboer 2024." ];
     };
   }).override {
     geolocationSupport = false;
