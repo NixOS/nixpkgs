07f09fcbbb58ae91af01f451aea51b6b5a12b21f
Cole Helbling <cole.e.helbling@outlook.com>
2024-10-30 08:34:50 -0700

    mullvad: 2024.6 -> 2024.7
    Also go back to the `GOBIN` hack in libwg. I don't know why it was
    working before and not now, but 🤷

--- pkgs/applications/networking/mullvad/libwg.nix
+++ pkgs/applications/networking/mullvad/libwg.nix
@@ -14,8 +14,14 @@ buildGoModule {
   proxyVendor = true;
   vendorHash = "sha256-uyAzY1hoCtS7da3wtjxTGx5wBb9c9m749TzihVr94rc=";
 
+  # XXX: hack to make the ar archive go to the correct place
+  # This is necessary because passing `-o ...` to `ldflags` does not work
+  # (this doesn't get communicated everywhere in the chain, apparently, so
+  # `go` complains that it can't find an `a.out` file).
+  GOBIN = "${placeholder "out"}/lib";
+
   subPackages = [ "." ];
-  ldflags = [ "-s" "-w" "-buildmode=c-archive" "-o" "${placeholder "out"}/lib" ];
+  ldflags = [ "-s" "-w" "-buildmode=c-archive" ];
   tags = [ "daita" ];
 
   postInstall = ''
