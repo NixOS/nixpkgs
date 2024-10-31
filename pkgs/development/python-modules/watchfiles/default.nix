26608c5e4a43987a69d843247a4aefda276a1d02
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-31 11:18:07 +0100

    python313Packages.watchfiles: 0.22.0 -> 0.24.0
    https://github.com/samuelcolvin/watchfiles/compare/refs/tags/v0.22.0...v0.24.0

--- pkgs/development/python-modules/watchfiles/default.nix
+++ pkgs/development/python-modules/watchfiles/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "watchfiles";
-  version = "0.22.0";
+  version = "0.24.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -27,13 +27,12 @@ buildPythonPackage rec {
     owner = "samuelcolvin";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-TtRSRgtMOqsnhdvsic3lg33xlA+r/DcYHlzewSOu/44=";
+    hash = "sha256-uc4CfczpNkS4NMevtRxhUOj9zTt59cxoC0BXnuHFzys=";
   };
 
-  cargoDeps = rustPlatform.fetchCargoTarball {
-    inherit src;
-    name = "${pname}-${version}";
-    hash = "sha256-n9yN/VRNQWCxh+BoliIMkKqJC51inpB9DQ9WtqR4oA0=";
+  cargoDeps = rustPlatform.importCargoLock {
+    lockFile = ./Cargo.lock;
+    outputHashes."notify-6.1.1" = "sha256-lT3R5ZQpjx52NVMEKTTQI90EWT16YnbqphqvZmNpw/I=";
   };
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
