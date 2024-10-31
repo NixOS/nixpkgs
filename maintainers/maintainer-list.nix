6329898381d15a6832d8cc2c79883197bbdd9801
fbettag <franz@bett.ag>
2024-10-31 12:24:25 +0100

    maintainers: add fbettag

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6969,6 +6969,12 @@
     githubId = 1211003;
     name = "Fredrik Bergroth";
   };
+  fbettag = {
+    email = "franz@bett.ag";
+    github = "fbettag";
+    githubId = 79918;
+    name = "Franz Bettag";
+  };
   fbrs = {
     email = "yuuki@protonmail.com";
     github = "cideM";
