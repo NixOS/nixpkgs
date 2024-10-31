6dc41785cf1e84db9d279a857dc76044664853a5
rewine <luhongxu@deepin.org>
2024-10-31 15:14:36 +0800

    qt6Packages.waylib: 0.1.1 -> 0.3.0-alpha

--- pkgs/development/libraries/waylib/default.nix
+++ pkgs/development/libraries/waylib/default.nix
@@ -1,46 +1,31 @@
 { stdenv
 , lib
 , fetchFromGitHub
-, fetchpatch
 , cmake
 , pkg-config
 , wayland-scanner
 , wrapQtAppsHook
 , qtbase
-, qtquick3d
+, qtdeclarative
 , qwlroots
 , wayland
 , wayland-protocols
 , wlr-protocols
 , pixman
 , libdrm
-, nixos-artwork
+, libinput
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "waylib";
-  version = "0.1.1";
+  version = "0.3.0-alpha";
 
   src = fetchFromGitHub {
     owner = "vioken";
     repo = "waylib";
     rev = finalAttrs.version;
-    hash = "sha256-3IdrChuXQyQGhJ/7kTqmkV0PyuSNP53Y0Po01Fc9Qi0=";
+    hash = "sha256-5IWe8VFpLwDSja4to/ugVS80s5+bcAbM6/fg1HPP52Q=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "fix-build-on-qt-6_7.patch";
-      url = "https://github.com/vioken/waylib/commit/09875ebedb074089ec57e71cbc8d8011f555be70.patch";
-      hash = "sha256-ulXlLxn7TOlXSl4N5mjXCy3PJhxVeyDwbwKeV9J/FSI=";
-    })
-  ];
-
-  postPatch = ''
-    substituteInPlace examples/tinywl/OutputDelegate.qml \
-      --replace "/usr/share/wallpapers/deepin/desktop.jpg" \
-                "${nixos-artwork.wallpapers.simple-blue}/share/backgrounds/nixos/nix-wallpaper-simple-blue.png"
-  '';
 
   depsBuildBuild = [
     # To find wayland-scanner
@@ -51,36 +36,30 @@ stdenv.mkDerivation (finalAttrs: {
     cmake
     pkg-config
     wayland-scanner
-    wrapQtAppsHook
   ];
 
   buildInputs = [
     qtbase
-    qtquick3d
+    qtdeclarative
+    qwlroots
     wayland
     wayland-protocols
     wlr-protocols
     pixman
     libdrm
-  ];
-
-  propagatedBuildInputs = [
-    qwlroots
-  ];
-
-  cmakeFlags = [
-    (lib.cmakeBool "INSTALL_TINYWL" true)
+    libinput
   ];
 
   strictDeps = true;
 
-  outputs = [ "out" "dev" "bin" ];
+  dontWrapQtApps = true;
+
+  outputs = [ "out" "dev" ];
 
   meta = {
     description = "Wrapper for wlroots based on Qt";
     homepage = "https://github.com/vioken/waylib";
     license = with lib.licenses; [ gpl3Only lgpl3Only asl20 ];
-    outputsToInstall = [ "out" ];
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ rewine ];
   };
