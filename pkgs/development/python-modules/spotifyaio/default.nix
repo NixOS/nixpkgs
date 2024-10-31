b30cb6821016aed9a3a6bb4d7ff0bd46549c0a0a
Fabian Affolter <mail@fabian-affolter.ch>
2024-10-31 23:46:03 +0100

    python312Packages.spotifyaio: 0.7.1 -> 0.8.1
    Diff: https://github.com/joostlek/python-spotify/compare/refs/tags/v0.7.1...v0.8.1
    
    Changelog: https://github.com/joostlek/python-spotify/releases/tag/v0.8.1

--- pkgs/development/python-modules/spotifyaio/default.nix
+++ pkgs/development/python-modules/spotifyaio/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "spotifyaio";
-  version = "0.7.1";
+  version = "0.8.1";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "joostlek";
     repo = "python-spotify";
     rev = "refs/tags/v${version}";
-    hash = "sha256-L5S7adnOOJQ0b+yUcL8rw9XkZS0meN2gN1Bi2WYOJks=";
+    hash = "sha256-R77bdJidZrsaQZjgCr7OLDab2NpJ10575j8rgmFjn2g=";
   };
 
   build-system = [ poetry-core ];
