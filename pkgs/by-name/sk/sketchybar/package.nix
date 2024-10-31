7e3d7dfd9e0186c5b69ed05abf2bce542e95f107
Austin Horstman <khaneliman12@gmail.com>
2024-10-31 09:33:29 -0500

    sketchybar: switch to apple-sdk_11

--- pkgs/by-name/sk/sketchybar/package.nix
+++ pkgs/by-name/sk/sketchybar/package.nix
@@ -1,26 +1,15 @@
 {
   lib,
-  overrideSDK,
   stdenv,
-  darwin,
   fetchFromGitHub,
   testers,
   nix-update-script,
+  apple-sdk_11,
+  darwinMinVersionHook,
 }:
 
 let
   inherit (stdenv.hostPlatform) system;
-  inherit (darwin.apple_sdk_11_0.frameworks)
-    AppKit
-    Carbon
-    CoreAudio
-    CoreWLAN
-    CoreVideo
-    DisplayServices
-    IOKit
-    MediaRemote
-    SkyLight
-    ;
 
   target =
     {
@@ -28,10 +17,8 @@ let
       "x86_64-darwin" = "x86";
     }
     .${system} or (throw "Unsupported system: ${system}");
-
-  stdenv' = if stdenv.hostPlatform.isDarwin then overrideSDK stdenv "11.0" else stdenv;
 in
-stdenv'.mkDerivation (finalAttrs: {
+stdenv.mkDerivation (finalAttrs: {
   pname = "sketchybar";
   version = "2.21.0";
 
@@ -43,15 +30,8 @@ stdenv'.mkDerivation (finalAttrs: {
   };
 
   buildInputs = [
-    AppKit
-    Carbon
-    CoreAudio
-    CoreWLAN
-    CoreVideo
-    DisplayServices
-    IOKit
-    MediaRemote
-    SkyLight
+    apple-sdk_11
+    (darwinMinVersionHook "10.13")
   ];
 
   makeFlags = [ target ];
