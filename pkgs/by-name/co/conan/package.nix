cfd186f0978147e6c71c50e907c794d103ed0370
Wolfgang Walther <walther@technowledgy.de>
2024-10-31 17:05:07 +0100

    python3Packages: remove left-over "--numprocesses"
    Those are provided by pytest-xdist's setup hook automatically anyway.

--- pkgs/by-name/co/conan/package.nix
+++ pkgs/by-name/co/conan/package.nix
@@ -65,11 +65,6 @@ python3.pkgs.buildPythonApplication rec {
     "conan"
   ];
 
-  pytestFlagsArray = [
-    "-n"
-    "$NIX_BUILD_CORES"
-  ];
-
   disabledTests = [
     # Tests require network access
     "TestFTP"
