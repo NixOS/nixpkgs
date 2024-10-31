eff46693028bfae03145c95334f5f3e13560420a
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-10-31 15:24:04 +0200

    raycast: move to `pkgs/by-name`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34913,8 +34913,6 @@ with pkgs;
 
   raylib-games = callPackage ../games/raylib-games { };
 
-  raycast = callPackage ../os-specific/darwin/raycast { };
-
   redeclipse = callPackage ../games/redeclipse { };
 
   rftg = callPackage ../games/rftg { };
