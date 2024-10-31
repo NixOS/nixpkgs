7e9594fe0fd6f75ee71cbcfdd0f0f718bd7b4317
Austin Horstman <khaneliman12@gmail.com>
2024-10-31 10:09:43 -0500

    nufmt: switch to apple-sdk_11

--- pkgs/by-name/nu/nufmt/package.nix
+++ pkgs/by-name/nu/nufmt/package.nix
@@ -3,12 +3,12 @@
   stdenv,
   fetchFromGitHub,
   rustPlatform,
-  darwin,
+  apple-sdk_11,
   llvmPackages,
   nix-update-script,
   ...
 }:
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage {
   pname = "nufmt";
   version = "0-unstable-2024-10-20";
 
@@ -19,12 +19,9 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-AurQGIZDYOkMMyAEXP01QziISQcSME3GFtvqjCDoeiw=";
   };
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin (
-    with darwin.apple_sdk.frameworks;
-    [
-      IOKit
-    ]
-  );
+  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
+    apple-sdk_11
+  ];
 
   env.LIBCLANG_PATH = lib.optionalString stdenv.cc.isClang "${llvmPackages.libclang.lib}/lib";
 
