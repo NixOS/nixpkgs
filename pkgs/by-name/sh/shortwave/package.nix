b8b5d8cfce9816ed5e9d34c5297f64105c7d8d69
zendo <linzway@qq.com>
2024-11-01 12:33:14 +0800

    shortwave: 3.2.0 -> 4.0.1

--- pkgs/by-name/sh/shortwave/package.nix
+++ pkgs/by-name/sh/shortwave/package.nix
@@ -1,44 +1,46 @@
-{ stdenv
-, lib
-, fetchFromGitLab
-, cargo
-, dbus
-, desktop-file-utils
-, gdk-pixbuf
-, gettext
-, gitMinimal
-, glib
-, gst_all_1
-, gtk4
-, libadwaita
-, meson
-, ninja
-, openssl
-, pkg-config
-, rustPlatform
-, rustc
-, sqlite
-, wrapGAppsHook4
-, cmake
-, libshumate
+{
+  stdenv,
+  lib,
+  fetchFromGitLab,
+  cargo,
+  dbus,
+  desktop-file-utils,
+  gdk-pixbuf,
+  gettext,
+  gitMinimal,
+  glib,
+  gst_all_1,
+  gtk4,
+  libadwaita,
+  meson,
+  ninja,
+  openssl,
+  pkg-config,
+  rustPlatform,
+  rustc,
+  sqlite,
+  wrapGAppsHook4,
+  libshumate,
+  libseccomp,
+  lcms2,
 }:
 
 stdenv.mkDerivation rec {
   pname = "shortwave";
-  version = "3.2.0";
+  version = "4.0.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "World";
     repo = "Shortwave";
     rev = version;
-    sha256 = "sha256-ESZ1yD1IuBar8bv83xMczZbtPtHbWRpe2yMVyr7K5gQ=";
+    sha256 = "sha256-W1eOMyiooDesI13lOze/JcxzhSSxYOW6FOY85NkVyps=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-8W46bGAitR2YbZbnsigAZMW5pSFTkDAe5JNaNOH5JfA=";
+    hash = "sha256-O5K5aNcWwUYkaJbGzTzS3FdNbEsQsdliyi0YShw+6PU=";
   };
 
   nativeBuildInputs = [
@@ -53,35 +55,34 @@ stdenv.mkDerivation rec {
     rustPlatform.cargoSetupHook
     rustc
     wrapGAppsHook4
-    cmake
   ];
 
-  buildInputs = [
-    dbus
-    gdk-pixbuf
-    glib
-    gtk4
-    libadwaita
-    openssl
-    sqlite
-    libshumate
-  ] ++ (with gst_all_1; [
-    gstreamer
-    gst-plugins-base
-    gst-plugins-good
-    gst-plugins-bad
-  ]);
+  buildInputs =
+    [
+      dbus
+      gdk-pixbuf
+      glib
+      gtk4
+      libadwaita
+      openssl
+      sqlite
+      libshumate
+      libseccomp
+      lcms2
+    ]
+    ++ (with gst_all_1; [
+      gstreamer
+      gst-plugins-base
+      gst-plugins-good
+      gst-plugins-bad
+    ]);
 
-  meta = with lib; {
+  meta = {
     homepage = "https://gitlab.gnome.org/World/Shortwave";
     description = "Find and listen to internet radio stations";
     mainProgram = "shortwave";
-    longDescription = ''
-      Shortwave is a streaming audio player designed for the GNOME
-      desktop. It is the successor to the older Gradio application.
-    '';
-    maintainers = with maintainers; [ lasandell ];
-    license = licenses.gpl3Plus;
-    platforms = platforms.linux;
+    maintainers = with lib.maintainers; [ lasandell ];
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
   };
 }
