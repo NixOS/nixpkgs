4f9b9c0e5d595f9ecbe99c1ae564b99b5ab21050
Manu [tennox] <tennox+git@txlab.io>
2024-11-01 17:21:51 +0000

    format task-keeper/default.nix
    https://github.com/NixOS/nixpkgs/pull/236516

--- pkgs/tools/misc/task-keeper/default.nix
+++ pkgs/tools/misc/task-keeper/default.nix
@@ -5,20 +5,21 @@ rustPlatform.buildRustPackage rec {
   version = "0.25.0";
 
   src = fetchFromGitHub {
-    owner = "tennox"; # Using my fork because of https://github.com/linux-china/task-keeper/pull/5
+    owner =
+      "tennox"; # Using my fork because of https://github.com/linux-china/task-keeper/pull/5
     repo = "task-keeper";
     rev = "4e39d65d01c44b44ae9876d8a3cb37066ce9f3f7";
     hash = "sha256-l/sqYuFrSDJN4Y9NN2fpkEI7AWctezLRi/pNAkPmHRk=";
   };
 
-  cargoLock = {
-    lockFile = "${./.}/Cargo.lock";
-  };
-  doCheck = false; # tests depend on many packages (java, node, python, sbt, ...) - which I'm not currently willing to set up 😅
+  cargoLock = { lockFile = "${./.}/Cargo.lock"; };
+  doCheck =
+    false; # tests depend on many packages (java, node, python, sbt, ...) - which I'm not currently willing to set up 😅
 
   meta = with lib; {
     homepage = "https://github.com/linux-china/task-keeper";
-    description = "CLI to manage tasks from different task runners or package managers";
+    description =
+      "CLI to manage tasks from different task runners or package managers";
     license = licenses.asl20;
     maintainers = with maintainers; [ tennox ];
     mainProgram = "tk";
