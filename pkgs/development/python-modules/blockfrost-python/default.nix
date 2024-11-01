f473d461cced6399ebecf2e74754f52ee15ea7fd
t4ccer <t4ccer@gmail.com>
2024-10-31 21:06:24 -0600

    python3Packages.blockfrost-python: patch version number

--- pkgs/development/python-modules/blockfrost-python/default.nix
+++ pkgs/development/python-modules/blockfrost-python/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch2,
   # Python deps
   requests,
   setuptools,
@@ -20,9 +21,20 @@ buildPythonPackage rec {
     hash = "sha256-mN26QXxizDR+o2V5C2MlqVEbRns1BTmwZdUnnHNcFzw=";
   };
 
-  propagatedBuildInputs = [
-    requests
+  patches = [
+    (fetchpatch2 {
+      name = "blockfrost-python-fix-version";
+      url = "https://github.com/blockfrost/blockfrost-python/commit/02fdc67ff6d1333c0855e740114585852bbfa0bc.patch?full_index=1";
+      hash = "sha256-070tnWxOnVNsCYXmBFo39JUgQDqphdpqx3A9OIuC94U=";
+    })
+  ];
+
+  build-system = [
     setuptools
+  ];
+
+  dependencies = [
+    requests
   ];
 
   pythonImportsCheck = [ "blockfrost" ];
