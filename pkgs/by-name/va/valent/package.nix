08dee690df68a9965e588b971c6613941c58fe96
aleksana <me@aleksana.moe>
2024-11-01 23:01:05 +0800

    valent: format with nixfmt-rfc-style

--- pkgs/by-name/va/valent/package.nix
+++ pkgs/by-name/va/valent/package.nix
@@ -1,26 +1,27 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, desktop-file-utils
-, meson
-, ninja
-, pkg-config
-, wrapGAppsHook4
-, vala
-, evolution-data-server-gtk4
-, gdk-pixbuf
-, glib
-, glib-networking
-, gnutls
-, gst_all_1
-, json-glib
-, libadwaita
-, libpeas2
-, libphonenumber
-, libportal-gtk4
-, pipewire
-, pulseaudio
-, tinysparql
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  desktop-file-utils,
+  meson,
+  ninja,
+  pkg-config,
+  wrapGAppsHook4,
+  vala,
+  evolution-data-server-gtk4,
+  gdk-pixbuf,
+  glib,
+  glib-networking,
+  gnutls,
+  gst_all_1,
+  json-glib,
+  libadwaita,
+  libpeas2,
+  libphonenumber,
+  libportal-gtk4,
+  pipewire,
+  pulseaudio,
+  tinysparql,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -88,7 +89,11 @@ stdenv.mkDerivation (finalAttrs: {
     '';
     homepage = "https://valent.andyholmes.ca";
     changelog = "https://github.com/andyholmes/valent/blob/${finalAttrs.src.rev}/CHANGELOG.md";
-    license = with lib.licenses; [ gpl3Plus cc0 cc-by-sa-30 ];
+    license = with lib.licenses; [
+      gpl3Plus
+      cc0
+      cc-by-sa-30
+    ];
     maintainers = with lib.maintainers; [ aleksana ];
     platforms = lib.platforms.linux;
   };
