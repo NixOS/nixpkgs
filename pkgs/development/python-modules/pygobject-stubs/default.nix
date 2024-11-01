11101516372f7daa7a587acc4cf3aeaaaf197d9b
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-01 08:48:19 +0100

    python312Packages.pygobject-stubs: update changelog URL

--- pkgs/development/python-modules/pygobject-stubs/default.nix
+++ pkgs/development/python-modules/pygobject-stubs/default.nix
@@ -28,7 +28,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "PEP 561 Typing Stubs for PyGObject";
     homepage = "https://github.com/pygobject/pygobject-stubs";
-    changelog = "https://github.com/pygobject/pygobject-stubs/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/pygobject/pygobject-stubs/blob/v${version}/CHANGELOG.md";
     license = licenses.lgpl21Plus;
     maintainers = with maintainers; [ hacker1024 ];
   };
