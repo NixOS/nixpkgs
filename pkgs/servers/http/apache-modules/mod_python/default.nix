231fdd7404ee5636610af3beb0bc4161f3b6770c
zi3m5f <113244000+zi3m5f@users.noreply.github.com>
2024-11-01 22:31:28 +0100

    apacheHttpdPackages.mod_python: fix for python3.12
    adding distutils, packaging and setuptools to fix:
    - ModuleNotFoundError: No module named 'distutils'
    - ModuleNotFoundError: No module named 'packaging'
    - error: invalid command 'install'
    
    and ensureNewerSourcesForZipFilesHook for:
    - ValueError: ZIP does not support timestamps before 1980

--- pkgs/servers/http/apache-modules/mod_python/default.nix
+++ pkgs/servers/http/apache-modules/mod_python/default.nix
@@ -1,5 +1,6 @@
 {
   apacheHttpd,
+  ensureNewerSourcesForZipFilesHook,
   fetchFromGitHub,
   lib,
   libintl,
@@ -8,15 +9,15 @@
   stdenv,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mod_python";
   version = "3.5.0.2";
 
   src = fetchFromGitHub {
     owner = "grisha";
-    repo = pname;
-    rev = "refs/tags/${version}";
-    hash = "sha256-EH8wrXqUAOFWyPKfysGeiIezgrVc789RYO4AHeSA6t4=";
+    repo = "mod_python";
+    rev = "refs/tags/${finalAttrs.version}";
+    hash = "sha256-++yHNKVe1u3w47DaB0zvYyuTrBcQdmuDm22areAeejs=";
   };
 
   patches = [ ./install.patch ];
@@ -26,10 +27,18 @@ stdenv.mkDerivation rec {
     "BINDIR=$(out)/bin"
   ];
 
+  nativeBuildInputs = [
+    ensureNewerSourcesForZipFilesHook
+  ];
+
   buildInputs =
     [
       apacheHttpd
-      python3
+      (python3.withPackages (ps: with ps; [
+        distutils
+        packaging
+        setuptools
+      ]))
     ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [
       libintl
@@ -40,12 +49,12 @@ stdenv.mkDerivation rec {
     updateScript = nix-update-script { };
   };
 
-  meta = with lib; {
+  meta = {
     homepage = "https://modpython.org/";
-    changelog = "https://github.com/grisha/mod_python/blob/${version}/NEWS";
+    changelog = "https://github.com/grisha/mod_python/blob/master/NEWS";
     description = "Apache module that embeds the Python interpreter within the server";
     mainProgram = "mod_python";
-    platforms = platforms.unix;
+    platforms = lib.platforms.unix;
     maintainers = [ ];
   };
-}
+})
