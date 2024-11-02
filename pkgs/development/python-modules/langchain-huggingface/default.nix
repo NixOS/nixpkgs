3fbd1bde35a588956dc0509951dd5d2c6721d75b
sarahec <sarahec@nextquestion.net>
2024-11-01 21:18:56 -0700

    python312Packages.langchain-huggingface: correct to 0.1.2

--- pkgs/development/python-modules/langchain-huggingface/default.nix
+++ pkgs/development/python-modules/langchain-huggingface/default.nix
@@ -30,13 +30,13 @@
 
 buildPythonPackage rec {
   pname = "langchain-huggingface";
-  version = "0.3.6";
+  version = "0.1.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langchain";
-    rev = "refs/tags/langchain-core==${version}";
+    rev = "refs/tags/langchain-huggingface==${version}";
     hash = "sha256-ACR+JzKcnYXROGOQe6DlZeqcYd40KlesgXSUOybOT20=";
   };
 
@@ -80,6 +80,9 @@ buildPythonPackage rec {
     description = "An integration package connecting Huggingface related classes and LangChain";
     homepage = "https://github.com/langchain-ai/langchain/tree/master/libs/partners/huggingface";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ natsukium ];
+    maintainers = with lib.maintainers; [
+      natsukium
+      sarahec
+    ];
   };
 }
