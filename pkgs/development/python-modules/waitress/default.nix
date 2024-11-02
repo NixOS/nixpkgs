6f1355e00be64f6d829dcf7c1caaab6c3c7f80ff
natsukium <tomoya.otabi@gmail.com>
2024-11-02 23:45:47 +0900

    python312Packages.waitress: refactor

--- pkgs/development/python-modules/waitress/default.nix
+++ pkgs/development/python-modules/waitress/default.nix
@@ -2,25 +2,40 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
+  pytestCheckHook,
+  pytest-cov-stub,
 }:
 
 buildPythonPackage rec {
   pname = "waitress";
   version = "3.0.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-7wwfAg2fEqUVxOxlwHkgpwJhOvytHb/cO87CVrbAcrM=";
   };
 
-  doCheck = false;
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "waitress" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-cov-stub
+  ];
+
+  disabledTests = [
+    # access to socket
+    "test_service_port"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/Pylons/waitress";
     description = "Waitress WSGI server";
     mainProgram = "waitress-serve";
-    license = licenses.zpl20;
+    license = licenses.zpl21;
     maintainers = with maintainers; [ domenkozar ];
   };
 }
