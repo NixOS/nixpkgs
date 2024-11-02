92aa7ca014817358c361f6a9ed536fe3c6351329
Pyrox <pyrox@pyrox.dev>
2024-11-02 16:51:57 -0400

    python3Packages.tyro: init at 0.8.14

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17374,6 +17374,8 @@ self: super: with self; {
 
   typogrify = callPackage ../development/python-modules/typogrify { };
 
+  tyro = callPackage ../development/python-modules/tyro { };
+
   tzdata = callPackage ../development/python-modules/tzdata { };
 
   tzlocal = callPackage ../development/python-modules/tzlocal { };
