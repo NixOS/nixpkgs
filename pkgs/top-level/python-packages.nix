c101250229a68257797ff8514ec24dba8928af64
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-11-02 20:49:20 +0100

    python312Packages.marko: init at 2.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7748,6 +7748,8 @@ self: super: with self; {
 
   markdownify  = callPackage ../development/python-modules/markdownify { };
 
+  marko = callPackage ../development/python-modules/marko { };
+
   markupsafe = callPackage ../development/python-modules/markupsafe { };
 
   markuppy = callPackage ../development/python-modules/markuppy { };
