53ad27b76f89f8d7aee1be91176cc689d71704cf
sarahec <sarahec@nextquestion.net>
2024-11-01 20:05:43 -0700

    python312Packages.langgraph-checkpoint-duckdb: init at 2.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6945,6 +6945,8 @@ self: super: with self; {
 
   langgraph-checkpoint-postgres = callPackage ../development/python-modules/langgraph-checkpoint-postgres { };
 
+  langgraph-checkpoint-duckdb = callPackage ../development/python-modules/langgraph-checkpoint-duckdb { };
+
   langgraph-checkpoint-sqlite = callPackage ../development/python-modules/langgraph-checkpoint-sqlite { };
 
   langgraph-cli = callPackage ../development/python-modules/langgraph-cli { };
