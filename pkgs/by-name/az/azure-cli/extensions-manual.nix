63f1e599b423a7071dbeb8f24bd27d0136845faf
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-11-02 10:40:55 +0100

    azure-cli-extensions.connection-monitor-preview: remove
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- pkgs/by-name/az/azure-cli/extensions-manual.nix
+++ pkgs/by-name/az/azure-cli/extensions-manual.nix
@@ -138,5 +138,6 @@
   # Removed extensions
   adp = throw "The 'adp' extension for azure-cli was deprecated upstream"; # Added 2024-11-02, https://github.com/Azure/azure-cli-extensions/pull/8038
   blockchain = throw "The 'blockchain' extension for azure-cli was deprecated upstream"; # Added 2024-04-26
+  connection-monitor-preview = throw "The 'connection-monitor-preview' extension for azure-cli was deprecated upstream"; # Added 2024-11-02, https://github.com/Azure/azure-cli-extensions/pull/8194
   vm-repair = throw "The 'vm-repair' extension for azure-cli was deprecated upstream"; # Added 2024-08-06
 }
