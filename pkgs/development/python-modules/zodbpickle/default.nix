6d68f2054b769a7db1fb1fcf8267fea312c817e4
natsukium <tomoya.otabi@gmail.com>
2024-11-02 20:45:44 +0900

    python312Packages.zodbpickle: switch to pypa builder and enable tests

--- pkgs/development/python-modules/zodbpickle/default.nix
+++ pkgs/development/python-modules/zodbpickle/default.nix
@@ -3,12 +3,14 @@
   buildPythonPackage,
   pythonOlder,
   fetchPypi,
+  setuptools,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "zodbpickle";
   version = "4.1.1";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.8";
 
@@ -17,10 +19,24 @@ buildPythonPackage rec {
     hash = "sha256-38DJFe8Umd0GA5cPXBECxr1+t7asRkNLKabYQL8Cckg=";
   };
 
-  # fails..
-  doCheck = false;
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace "setuptools<74" "setuptools"
+  '';
+
+  build-system = [ setuptools ];
 
   pythonImportsCheck = [ "zodbpickle" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  # fails..
+  disabledTests = [
+    "test_dump"
+    "test_dumps"
+    "test_load"
+    "test_loads"
+  ];
 
   meta = with lib; {
     description = "Fork of Python's pickle module to work with ZODB";
