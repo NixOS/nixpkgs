08ba2dbe3d2d74e1bcbd13eb3970ee852090a6a3
Peter Lehmann <xgwq@xnee.net>
2024-11-02 19:47:58 +0100

    trippy: generate shell completions

--- pkgs/tools/networking/trippy/default.nix
+++ pkgs/tools/networking/trippy/default.nix
@@ -1,6 +1,8 @@
 { lib
+, stdenv
 , rustPlatform
 , fetchFromGitHub
+, installShellFiles
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -14,7 +16,17 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-ArSIeu3u+TUy18rzJvhq0+/qvi5xPZmtQ7rPpwaEx9g=";
   };
 
+  nativeBuildInputs = [ installShellFiles ];
+
   cargoHash = "sha256-h1NQQFjtlpQuyTz7AHuAPUe1GxR0Q2yKzow8XB9375U=";
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    local INSTALL="$out/bin/trip"
+    installShellCompletion --cmd trip \
+      --bash <($out/bin/trip --generate bash) \
+      --fish <($out/bin/trip --generate fish) \
+      --zsh <($out/bin/trip --generate zsh)
+  '';
 
   meta = with lib; {
     description = "Network diagnostic tool";
