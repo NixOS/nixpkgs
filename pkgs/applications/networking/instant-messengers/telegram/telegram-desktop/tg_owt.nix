592e58c50a2c29d1e738b363b8a8c46aa68568bb
Ilya Fedin <fedin-ilja2010@ya.ru>
2024-11-03 01:21:49 +0400

    telegram-desktop.tg_owt: 0-unstable-2024-08-04 -> 0-unstable-2024-10-28

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/tg_owt.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/tg_owt.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch2,
   pkg-config,
   cmake,
   ninja,
@@ -34,27 +33,15 @@
 
 stdenv.mkDerivation {
   pname = "tg_owt";
-  version = "0-unstable-2024-08-04";
+  version = "0-unstable-2024-10-28";
 
   src = fetchFromGitHub {
     owner = "desktop-app";
     repo = "tg_owt";
-    rev = "dc17143230b5519f3c1a8da0079e00566bd4c5a8";
-    hash = "sha256-7j7hBIOXEdNJDnDSVUqy234nkTCaeZ9tDAzqvcuaq0o=";
+    rev = "8198c4d8b91e22d68eb5c7327fd408e3b6abcc79";
+    hash = "sha256-3sLa3Px0VWgRYqnnLlwLnx+WWnneqZyhQPrRMBriXQA=";
     fetchSubmodules = true;
   };
-
-  patches = [
-    # Remove usage of AVCodecContext::reordered_opaque
-    (fetchpatch2 {
-      name = "webrtc-ffmpeg-7.patch";
-      url = "https://webrtc.googlesource.com/src/+/e7d10047096880feb5e9846375f2da54aef91202%5E%21/?format=TEXT";
-      decode = "base64 -d";
-      stripLen = 1;
-      extraPrefix = "src/";
-      hash = "sha256-EdwHeVko8uDsP5GTw2ryWiQgRVCAdPc1me6hySdiwMU=";
-    })
-  ];
 
   postPatch = lib.optionalString stdenv.hostPlatform.isLinux ''
     substituteInPlace src/modules/desktop_capture/linux/wayland/egl_dmabuf.cc \
