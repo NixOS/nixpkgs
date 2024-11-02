57f60e978be7a341b1ef921571d5a04c49f470b5
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-02 13:25:47 +0100

    python312Packages.plugwise: 1.4.3 -> 1.5.0
    Diff: https://github.com/plugwise/python-plugwise/compare/refs/tags/v1.4.3...v1.5.0
    
    Changelog: https://github.com/plugwise/python-plugwise/releases/tag/v1.5.0

--- pkgs/development/python-modules/plugwise/default.nix
+++ pkgs/development/python-modules/plugwise/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "plugwise";
-  version = "1.4.3";
+  version = "1.5.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "plugwise";
     repo = "python-plugwise";
     rev = "refs/tags/v${version}";
-    hash = "sha256-eeytJ9X2ChcQDbAfTUPokMl/qKVSU6PUuI/a30xJ6WU=";
+    hash = "sha256-WcoHK8UZ7qOVoUBCSoTDZ+6fC568AyXFCBys/7sjJx8=";
   };
 
   postPatch = ''
