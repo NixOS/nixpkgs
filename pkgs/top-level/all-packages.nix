b85f35840192a64a494341ff0534b1629c5025e5
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-11-02 16:01:16 +0000

    distrho: move to by-name
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28551,8 +28551,6 @@ with pkgs;
 
   dirt = callPackage ../applications/audio/dirt { };
 
-  distrho = callPackage ../applications/audio/distrho { };
-
   dit = callPackage ../applications/editors/dit { };
 
   djvulibre = callPackage ../applications/misc/djvulibre { };
