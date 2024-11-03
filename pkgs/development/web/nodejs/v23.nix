982e3fc31d8f83e1f6792e5cfe0af21881a2c801
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-11-03 04:31:21 +0100

    nodejs_23: drop reverts

--- pkgs/development/web/nodejs/v23.nix
+++ pkgs/development/web/nodejs/v23.nix
@@ -24,29 +24,5 @@ buildNodejs {
     ./node-npm-build-npm-package-logic.patch
     ./use-correct-env-in-tests.patch
     ./bin-sh-node-run-v22.patch
-
-    # Those reverts are due to a mismatch with the libuv version used upstream
-    (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/dcbc5fbe65b068a90c3d0970155d3a68774caa38.patch?full_index=1";
-      hash = "sha256-Q7YrooolMjsGflTQEj5ra6hRVGhMP6APaydf1MGH54Q=";
-      revert = true;
-      excludes = [ "doc/*" ];
-    })
-    (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/ec867ac7ce4e4913a8415eda48a7af9fc226097d.patch?full_index=1";
-      hash = "sha256-zfnHxC7ZMZAiu0/6PsX7RFasTevHMETv+azhTZnKI64=";
-      revert = true;
-      excludes = [ "doc/*" ];
-    })
-    (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/f97865fab436fba24b46dad14435ec4b482243a2.patch?full_index=1";
-      hash = "sha256-o5aPQqUXubtJKMX28jn8LdjZHw37/BqENkYt6RAR3kY=";
-      revert = true;
-    })
-    (fetchpatch2 {
-      url = "https://github.com/nodejs/node/commit/54d55f2337ebe04451da770935ad453accb147f9.patch?full_index=1";
-      hash = "sha256-gmIyiSyNzC3pClL1SM2YicckWM+/2tsbV1xv2S3d5G0=";
-      revert = true;
-    })
   ];
 }
