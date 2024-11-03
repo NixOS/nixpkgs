32b45fbf84e45dac3518264eadd690d786ca0f06
Mukund Lakshman <me@yaymukund.com>
2024-11-03 17:38:30 +0000

    [teensy-loader-cli] add darwin build

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18372,7 +18372,9 @@ with pkgs;
 
   teensyduino = arduino-core.override { withGui = true; withTeensyduino = true; };
 
-  teensy-loader-cli = callPackage ../development/embedded/teensy-loader-cli { };
+  teensy-loader-cli = callPackage ../development/embedded/teensy-loader-cli {
+    inherit (darwin.apple_sdk.frameworks) IOKit;
+  };
 
   tytools = libsForQt5.callPackage ../development/embedded/tytools { };
 
