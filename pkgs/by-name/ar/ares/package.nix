d615427b82c32ad55d0b58c9fac7371bf29ebdf0
Anderson Torres <torres.anderson.85@protonmail.com>
2024-11-02 23:57:06 -0300

    ares: refactor
    - migrate patches to a directory
    - nixfmt-rfc-style
    - hammer

--- pkgs/by-name/ar/ares/package.nix
+++ pkgs/by-name/ar/ares/package.nix
@@ -1,25 +1,26 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, SDL2
-, alsa-lib
-, gtk3
-, gtksourceview3
-, libGL
-, libGLU
-, libX11
-, libXv
-, libao
-, libicns
-, libpulseaudio
-, openal
-, pkg-config
-, udev
-, which
-, wrapGAppsHook3
-, apple-sdk_11
-, vulkan-loader
-, autoPatchelfHook
+{
+  lib,
+  SDL2,
+  alsa-lib,
+  apple-sdk_11,
+  autoPatchelfHook,
+  fetchFromGitHub,
+  gtk3,
+  gtksourceview3,
+  libGL,
+  libGLU,
+  libX11,
+  libXv,
+  libao,
+  libicns,
+  libpulseaudio,
+  openal,
+  pkg-config,
+  stdenv,
+  udev,
+  vulkan-loader,
+  which,
+  wrapGAppsHook3,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -34,64 +35,76 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   patches = [
-    ./001-dont-rebuild-on-install.patch
-    ./002-fix-ruby.diff
-    ./003-darwin-specific.patch
+    ./patches/001-dont-rebuild-on-install.patch
+    ./patches/002-fix-ruby.diff
+    ./patches/003-darwin-specific.patch
   ];
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-    pkg-config
-    which
-    wrapGAppsHook3
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    libicns
-  ];
+  nativeBuildInputs =
+    [
+      autoPatchelfHook
+      pkg-config
+      which
+      wrapGAppsHook3
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      libicns
+    ];
 
-  buildInputs = [
-    SDL2
-    libao
-  ] ++ lib.optionals stdenv.hostPlatform.isLinux [
-    alsa-lib
-    gtk3
-    gtksourceview3
-    libGL
-    libGLU
-    libX11
-    libXv
-    libpulseaudio
-    openal
-    udev
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    apple-sdk_11
-  ];
+  buildInputs =
+    [
+      SDL2
+      libao
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [
+      alsa-lib
+      gtk3
+      gtksourceview3
+      libGL
+      libGLU
+      libX11
+      libXv
+      libpulseaudio
+      openal
+      udev
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      apple-sdk_11
+    ];
 
   appendRunpaths = [ (lib.makeLibraryPath [ vulkan-loader ]) ];
 
-  enableParallelBuilding = true;
+  makeFlags =
+    lib.optionals stdenv.hostPlatform.isLinux [
+      "hiro=gtk3"
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      "hiro=cocoa"
+      "lto=false"
+      "vulkan=false"
+    ]
+    ++ [
+      "local=false"
+      "openmp=true"
+      "prefix=$(out)"
+    ];
 
-  makeFlags = lib.optionals stdenv.hostPlatform.isLinux [
-    "hiro=gtk3"
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    "hiro=cocoa"
-    "lto=false"
-    "vulkan=false"
-  ] ++ [
-    "local=false"
-    "openmp=true"
-    "prefix=$(out)"
-  ];
+  enableParallelBuilding = true;
 
   env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isDarwin "-mmacosx-version-min=10.14";
 
   meta = {
     homepage = "https://ares-emu.net";
     description = "Open-source multi-system emulator with a focus on accuracy and preservation";
-    mainProgram = "ares";
     license = lib.licenses.isc;
-    maintainers = with lib.maintainers; [ Madouura AndersonTorres ];
+    mainProgram = "ares";
+    maintainers = with lib.maintainers; [
+      Madouura
+      AndersonTorres
+    ];
     platforms = lib.platforms.unix;
     broken = stdenv.hostPlatform.isDarwin;
   };
 })
 # TODO: select between Qt and GTK3
+# TODO: call Darwin hackers to deal with specific errors
