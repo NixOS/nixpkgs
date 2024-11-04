2a64a9a22a7540182b64b90728b0ac51cffd5145
wxt <3264117476@qq.com>
2024-11-04 21:23:11 +0800

    aws-assume-role: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3131,8 +3131,6 @@ with pkgs;
 
   awsume = python3Packages.callPackage ../tools/admin/awsume { };
 
-  aws-assume-role = callPackage ../tools/admin/aws-assume-role { };
-
   aws-lambda-rie = callPackage ../tools/admin/aws-lambda-runtime-interface-emulator { };
 
   aws-mfa = python3Packages.callPackage ../tools/admin/aws-mfa { };
