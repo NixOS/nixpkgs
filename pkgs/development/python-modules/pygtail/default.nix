6b6595801400dc24ad9242c8891409a895347902
natsukium <tomoya.otabi@gmail.com>
2024-11-04 22:39:25 +0900

    python312Packages.pygtail: refactor

--- pkgs/development/python-modules/pygtail/default.nix
+++ pkgs/development/python-modules/pygtail/default.nix
@@ -2,20 +2,27 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "pygtail";
   version = "0.14.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bgreenlee";
-    repo = pname;
+    repo = "pygtail";
     rev = version;
-    sha256 = "sha256-TlXTlxeGDd+elGpMjxcJCmRuJmp5k9xj6MrViRzcST4=";
+    hash = "sha256-TlXTlxeGDd+elGpMjxcJCmRuJmp5k9xj6MrViRzcST4=";
   };
 
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "pygtail" ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
 
   meta = with lib; {
     description = "Library for reading log file lines that have not been read";
