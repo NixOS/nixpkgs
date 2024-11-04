b74ff634219402fafd6cca41884f77b242a2ca89
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-04 01:02:13 +0000

    python312Packages.cohere: 5.11.2 -> 5.11.3

--- pkgs/development/python-modules/cohere/default.nix
+++ pkgs/development/python-modules/cohere/default.nix
@@ -21,14 +21,14 @@
 
 buildPythonPackage rec {
   pname = "cohere";
-  version = "5.11.2";
+  version = "5.11.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "cohere-ai";
     repo = "cohere-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-QwloUY22Zy+gu8ggpLReXHXw11sZ/P0/Ao75a85MKN4=";
+    hash = "sha256-0Y/zSw2pdrwsYpBctiV/tVf2xPhf3jG7c18frSjBass=";
   };
 
   build-system = [ poetry-core ];
