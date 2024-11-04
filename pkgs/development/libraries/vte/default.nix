23c03e457af6ec83edc0c9d21fdb5d80d8907b47
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-11-04 20:42:27 +0100

    vte: 0.78.0 -> 0.78.1, fix darwin build

--- pkgs/development/libraries/vte/default.nix
+++ pkgs/development/libraries/vte/default.nix
@@ -26,6 +26,7 @@
 , icu
 , systemd
 , systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemd
+, fast-float
 , nixosTests
 , blackbox-terminal
 }:
@@ -54,6 +55,23 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://git.alpinelinux.org/aports/plain/community/vte3/fix-W_EXITCODE.patch?id=4d35c076ce77bfac7655f60c4c3e4c86933ab7dd";
       hash = "sha256-FkVyhsM0mRUzZmS2Gh172oqwcfXv6PyD6IEgjBhy2uU=";
     })
+    # build: Add fast_float dependency
+    # https://gitlab.gnome.org/GNOME/vte/-/issues/2823
+    (fetchpatch {
+      name = "0003-build-Add-fast_float-dependency.patch";
+      url = "https://gitlab.gnome.org/GNOME/vte/-/commit/f6095fca4d1baf950817e7010e6f1e7c313b9e2e.patch";
+      hash = "sha256-EL9PPiI5pDJOXf4Ck4nkRte/jHx/QWbxkjDFRSsp+so=";
+    })
+    (fetchpatch {
+      name = "0003-widget-termprops-Use-fast_float.patch";
+      url = "https://gitlab.gnome.org/GNOME/vte/-/commit/6c2761f51a0400772f443f12ea23a75576e195d3.patch";
+      hash = "sha256-jjM9bhl8EhtylUIQ2nMSNX3ugnkZQP/2POvSUDW0LM0=";
+    })
+    (fetchpatch {
+      name = "0003-build-Use-correct-path-to-include-fast_float.h.patch";
+      url = "https://gitlab.gnome.org/GNOME/vte/-/commit/d09330585e648b5c9991dffab4a06d1f127bf916.patch";
+      hash = "sha256-YGVXt2VojljYgTcmahQ2YEZGEysyUSwk+snQfoipJ+E=";
+    })
   ];
 
   nativeBuildInputs = [
@@ -77,6 +95,7 @@ stdenv.mkDerivation (finalAttrs: {
     pcre2
     lz4
     icu
+    fast-float
   ] ++ lib.optionals systemdSupport [
     systemd
   ];
