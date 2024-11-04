a0b8a795a6e33aaf3a88e4c82f43be359f796d1f
wxt <3264117476@qq.com>
2024-11-04 21:26:31 +0800

    aws-assume-role: fix build

--- pkgs/by-name/aw/aws-assume-role/package.nix
+++ pkgs/by-name/aw/aws-assume-role/package.nix
@@ -20,11 +20,14 @@ buildGoModule rec {
     sha256 = "sha256-7+9qi9lYzv1YCFhUyla+5Gqs5nBUiiazhFwiqHzMFd4=";
   };
 
-  vendorHash = null;
+  deleteVendor = true;
 
-  postPatch = ''
-    go mod init github.com/remind101/assume-role
-  '';
+  vendorHash = "sha256-NIY6w/hQQ357KHEDEHUYVLbkQKsm8FLtRf3/AbbgukA=";
+
+  patches = [
+    # Generate with go mod init github.com/remind101/assume-role && go mod tidy
+    ./0001-add-go.mod-go.sum.patch
+  ];
 
   postInstall = ''
     install -Dm444 -t $out/share/doc/aws-assume-role README.md
