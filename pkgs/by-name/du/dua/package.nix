031d30c4ccddad2492ff3b05e5a04ebe6fafc4f6
Gaetan Lepage <gaetan@glepage.com>
2024-11-04 09:28:44 +0100

    dua: 2.29.3 -> 2.29.4
    Diff: https://github.com/Byron/dua-cli/compare/v2.29.3...v2.29.4
    
    Changelog: https://github.com/Byron/dua-cli/blob/v2.29.4/CHANGELOG.md

--- pkgs/by-name/du/dua/package.nix
+++ pkgs/by-name/du/dua/package.nix
@@ -2,19 +2,19 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
-  stdenv,
-  darwin,
+  versionCheckHook,
+  nix-update-script,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "dua";
-  version = "2.29.3";
+  version = "2.29.4";
 
   src = fetchFromGitHub {
     owner = "Byron";
     repo = "dua-cli";
-    rev = "v${version}";
-    hash = "sha256-cxMims1b4zOmpRZSn9rovUCnUT66omgNYjDJWuIDnSk=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-TVwRz5bAdJMtmhhzfZZ/NuV+YrLcnuK6d86Oj/JmgW4=";
     # Remove unicode file names which leads to different checksums on HFS+
     # vs. other filesystems because of unicode normalisation.
     postFetch = ''
@@ -22,20 +22,30 @@ rustPlatform.buildRustPackage rec {
     '';
   };
 
-  cargoHash = "sha256-/7A1XW6EbOQpXeAlsOe1YoY6MdBQ3kC98TTBMs3Zfy8=";
+  cargoHash = "sha256-h4Z0Gb4lf/KXrBxU6gEGcRvuhqjUABsggcv/A+AFclo=";
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    darwin.apple_sdk.frameworks.Foundation
+  checkFlags = [
+    # Skip interactive tests
+    "--skip=interactive::app::tests::journeys_readonly::simple_user_journey_read_only"
+    "--skip=interactive::app::tests::journeys_with_writes::basic_user_journey_with_deletion"
+    "--skip=interactive::app::tests::unit::it_can_handle_ending_traversal_reaching_top_but_skipping_levels"
+    "--skip=interactive::app::tests::unit::it_can_handle_ending_traversal_without_reaching_the_top"
   ];
 
-  doCheck = false;
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+  versionCheckProgramArg = [ "--version" ];
+  doInstallCheck = true;
 
-  meta = with lib; {
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
     description = "Tool to conveniently learn about the disk usage of directories";
     homepage = "https://github.com/Byron/dua-cli";
     changelog = "https://github.com/Byron/dua-cli/blob/v${version}/CHANGELOG.md";
-    license = with licenses; [ mit ];
-    maintainers = with maintainers; [
+    license = with lib.licenses; [ mit ];
+    maintainers = with lib.maintainers; [
       figsoda
       killercup
     ];
