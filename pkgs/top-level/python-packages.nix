88890306fe58a4e1ef1fe26f4314ca1789f2bfac
Jonas Heinrich <onny@project-insanity.org>
2024-11-04 15:06:34 +0100

    python3Packages.djangocms-text-ckeditor: init at 5.1.6

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3321,6 +3321,8 @@ self: super: with self; {
 
   djangocms-admin-style = callPackage ../development/python-modules/djangocms-admin-style { };
 
+  djangocms-text-ckeditor = callPackage ../development/python-modules/djangocms-text-ckeditor { };
+
   django-admin-datta = callPackage ../development/python-modules/django-admin-datta { };
 
   django-admin-sortable2 = callPackage ../development/python-modules/django-admin-sortable2 { };
