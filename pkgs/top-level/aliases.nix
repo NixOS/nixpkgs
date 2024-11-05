3cb9a4aaccf19620d522064834af8c550fda44a7
Emily <vcs@emily.moe>
2024-11-05 17:53:23 +0000

    nrf-command-line-tools: drop
    Depends on `segger-jlink`.
    
    This reverts commit 9058bc4e8f118f83f877c34a4c59f87bbf295d2d.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -835,6 +835,7 @@ mapAliases {
   nixUnstable = throw "nixUnstable has been removed. For bleeding edge (Nix master, roughly weekly updated) use nixVersions.git, otherwise use nixVersions.latest."; # Converted to throw 2024-04-22
   nix_2_3 = nixVersions.nix_2_3;
   nixfmt = lib.warn "nixfmt was renamed to nixfmt-classic. The nixfmt attribute may be used for the new RFC 166-style formatter in the future, which is currently available as nixfmt-rfc-style" nixfmt-classic; # Added 2024-03-31
+  nrf-command-line-tools = throw "nrf-command-line-tools has been removed as it depends on segger-jlink, which was removed as it requires Qt 4, which has been end‐of‐life since 2015"; # Added 2024-11-05
 
   # When the nixops_unstable alias is removed, nixops_unstable_minimal can be renamed to nixops_unstable.
 
