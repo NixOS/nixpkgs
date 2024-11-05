ddc51f709f0a84fa78231e56b5b15e98da620afa
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-11-05 04:25:43 +0100

    home-assistant-custom-components.prometheus_sensor: 1.1.0 -> 1.1.2
    https://github.com/mweinelt/ha-prometheus-sensor/blob/ecc3cb42e9b8cd7df684a329e528faa127161b18/CHANGELOG.md

--- pkgs/servers/home-assistant/custom-components/prometheus_sensor/package.nix
+++ pkgs/servers/home-assistant/custom-components/prometheus_sensor/package.nix
@@ -6,13 +6,13 @@
 buildHomeAssistantComponent rec {
   owner = "mweinelt";
   domain = "prometheus_sensor";
-  version = "1.1.0";
+  version = "1.1.2";
 
   src = fetchFromGitHub {
     owner = "mweinelt";
     repo = "ha-prometheus-sensor";
     rev = "refs/tags/${version}";
-    hash = "sha256-xfLAfTBgJjrRU1EFcbRvzUSq4m+dd6izaxP9DMisz/0=";
+    hash = "sha256-+28mMvzNKVInknnDh++YolXR+/b1wsve1VEn4olR7Fs=";
   };
 
   dontBuild = true;
