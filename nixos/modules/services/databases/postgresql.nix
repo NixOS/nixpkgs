2b3a2f48942f78359068c464eb5a0c945b71c6a1
Hugo Herter <git@hugoherter.com>
2024-11-05 16:47:00 +0100

    Update nixos/modules/services/databases/postgresql.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- nixos/modules/services/databases/postgresql.nix
+++ nixos/modules/services/databases/postgresql.nix
@@ -379,7 +379,7 @@ in
           The value takes the form of a comma-separated list of host names and/or numeric IP addresses.
           The special entry `*` corresponds to all available IP interfaces.
 
-          See the PostgreSQL documentation on [listen_address](https://www.postgresql.org/docs/16/runtime-config-connection.html#GUC-LISTEN-ADDRESSES).
+          See the PostgreSQL documentation on [listen_address](https://www.postgresql.org/docs/current/runtime-config-connection.html#GUC-LISTEN-ADDRESSES).
         '';
       };
 
