7c1cc43d50ec4d51004ffa815341ab4a33010730
Austin Horstman <khaneliman12@gmail.com>
2024-11-04 19:30:13 -0600

    asleap: modernize

--- pkgs/tools/networking/asleap/default.nix
+++ pkgs/tools/networking/asleap/default.nix
@@ -7,13 +7,13 @@
   libxcrypt,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "asleap";
   version = "unstable-2021-06-20";
 
   src = fetchFromGitHub {
     owner = "zackw";
-    repo = pname;
+    repo = "asleap";
     rev = "eb3bd42098cba42b65f499c9d8c73d890861b94f";
     sha256 = "sha256-S6jS0cg9tHSfmP6VHyISkXJxczhPx3HDdxT46c+YmE8=";
   };
@@ -25,14 +25,18 @@ stdenv.mkDerivation rec {
   ];
 
   installPhase = ''
+    runHook preInstall
+
     install -Dm755 asleap $out/bin/asleap
     install -Dm755 genkeys $out/bin/genkeys
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/zackw/asleap";
     description = "Recovers weak LEAP and PPTP passwords";
-    license = licenses.gpl2Only;
-    maintainers = with maintainers; [ pyrox0 ];
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [ pyrox0 ];
   };
 }
