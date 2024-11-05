0846f0d19b69ca49c274a24775137a0ee91c8968
Noa Aarts <noa@voorwaarts.nl>
2024-11-05 14:37:38 +0100

    lunatic: fix build by patching in updated Cargo.lock

--- pkgs/development/interpreters/lunatic/default.nix
+++ pkgs/development/interpreters/lunatic/default.nix
@@ -1,10 +1,11 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-, pkg-config
-, openssl
-, stdenv
-, darwin
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  stdenv,
+  darwin,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -13,22 +14,29 @@ rustPlatform.buildRustPackage rec {
 
   src = fetchFromGitHub {
     owner = "lunatic-solutions";
-    repo = pname;
+    repo = "lunatic";
     rev = "v${version}";
     hash = "sha256-uMMssZaPDZn3bOtQIho+GvUCPmzRllv7eJ+SJuKaYtg=";
   };
 
-  cargoHash = "sha256-ALjlQsxmZVREyi3ZGMJMv/38kkMjYh+hTSr/0avYJVs=";
+  cargoPatches = [
+    # the `time` dependency was out of date and that broke the build
+    ./0001-update-dependencies-cargo-lock.patch
+  ];
+
+  cargoHash = "sha256-yGeCpzpeoVjTioGEL3yBJ4tDveJnoy+6UMfog84yahY=";
 
   nativeBuildInputs = [
     pkg-config
   ];
 
-  buildInputs = [
-    openssl
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    darwin.apple_sdk.frameworks.Security
-  ];
+  buildInputs =
+    [
+      openssl
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+    ];
 
   checkFlags = [
     # requires simd support which is not always available on hydra
@@ -39,7 +47,10 @@ rustPlatform.buildRustPackage rec {
     description = "Erlang inspired runtime for WebAssembly";
     homepage = "https://lunatic.solutions";
     changelog = "https://github.com/lunatic-solutions/lunatic/blob/v${version}/CHANGELOG.md";
-    license = with licenses; [ mit /* or */ asl20 ];
+    license = with licenses; [
+      mit # or
+      asl20
+    ];
     maintainers = with maintainers; [ figsoda ];
   };
 }
