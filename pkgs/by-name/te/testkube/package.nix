ddb6b26b47d6f699a1e4631253f7d713a73b0f86
Mathieu St-Louis <matfino+gh@gmail.com>
2024-11-04 22:13:36 -0500

    testkube: init at 2.1.56

---
+++ pkgs/by-name/te/testkube/package.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule rec {
+  pname = "testkube";
+  version = "2.1.56";
+
+  src = fetchFromGitHub {
+    owner = "kubeshop";
+    repo = "testkube";
+    rev = "v${version}";
+    sha256 = "sha256-P+A9lUMzQ3M0SEVZBMDSMj8S0uCsEhadv5vDRxbQORA=";
+  };
+
+  vendorHash = "sha256-44aIwddMH6CMfTno90xGkHgna4DO2Ii3KhpMwv6Zjmo=";
+
+  ldflags = [
+    "-X main.version=${version}"
+    "-X main.builtBy=nixpkgs"
+    "-X main.commit=v${version}"
+    "-X main.date=1970-01-01-00:00:01"
+  ];
+
+  doCheck = false;
+  subPackages = [ "cmd/kubectl-testkube" ];
+
+  meta = with lib; {
+    description = "Kubernetes-native framework for test definition and execution";
+    homepage = "https://github.com/kubeshop/testkube/";
+    license = licenses.mit;
+    mainProgram = "kubectl-testkube";
+    maintainers = with maintainers; [ ];
+  };
+}
