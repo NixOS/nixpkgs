1413992f40fd343cbea4f325c2dd4c1b1121a37d
06kellyjac <dev@j-k.io>
2024-11-06 11:14:08 +0000

    nerdctl: 1.7.7 -> 2.0.0
    Diff: https://github.com/containerd/nerdctl/compare/v1.7.7...v2.0.0
    
    Changelog: https://github.com/containerd/nerdctl/releases/tag/v2.0.0

--- pkgs/by-name/ne/nerdctl/package.nix
+++ pkgs/by-name/ne/nerdctl/package.nix
@@ -1,30 +1,44 @@
-{ lib
-, buildGoModule
-, fetchFromGitHub
-, makeWrapper
-, installShellFiles
-, buildkit
-, cni-plugins
-, extraPackages ? [ ]
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  makeWrapper,
+  installShellFiles,
+  buildkit,
+  cni-plugins,
+  extraPackages ? [ ],
 }:
 
 buildGoModule rec {
   pname = "nerdctl";
-  version = "1.7.7";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "containerd";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-GHFs8QvLcXu+DZ851TCLI7EVc9wMS5fRC4TYBXzyv3Q=";
+    hash = "sha256-SYZikFZa2jcdPE4+POVdwlX9ugtgQyRJ252uWJvrgbo=";
   };
 
-  vendorHash = "sha256-5LRsT04T/CKv+YHaiM2g6giimWWXyzPju3iZuj2DfAY=";
+  vendorHash = "sha256-h8D1ESaqBsyAVR/hJNtuog/pRgcqLgR2xl5plkI2cic=";
 
-  nativeBuildInputs = [ makeWrapper installShellFiles ];
+  nativeBuildInputs = [
+    makeWrapper
+    installShellFiles
+  ];
 
-  ldflags = let t = "github.com/containerd/nerdctl/pkg/version"; in
-    [ "-s" "-w" "-X ${t}.Version=v${version}" "-X ${t}.Revision=<unknown>" ];
+  ldflags =
+    let
+      t = "github.com/containerd/nerdctl/v${lib.versions.major version}/pkg/version";
+    in
+    [
+      "-s"
+      "-w"
+      "-X ${t}.Version=v${version}"
+      "-X ${t}.Revision=<unknown>"
+    ];
+
+  subPackage = [ "./cmd/nerdctl" ];
 
   # Many checks require a containerd socket and running nerdctl after it's built
   doCheck = false;
@@ -54,7 +68,22 @@ buildGoModule rec {
     description = "Docker-compatible CLI for containerd";
     mainProgram = "nerdctl";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ developer-guy jk ];
-    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [
+      developer-guy
+      jk
+    ];
+    platforms = [
+      "x86_64-linux"
+      "i686-linux"
+
+      "aarch64-linux"
+      "armv7l-linux"
+
+      "powerpc64le-linux"
+      "riscv64-linux"
+      "s390x-linux"
+
+      "x86_64-freebsd"
+    ];
   };
 }
