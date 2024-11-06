d73bbaaebdda8b4948ed3d8fa4e6c00899698144
Franz Pletz <fpletz@fnordicwalking.de>
2024-11-06 07:45:24 +0100

    ncmpc: reformat with nixfmt-rfc-style

--- pkgs/by-name/nc/ncmpc/package.nix
+++ pkgs/by-name/nc/ncmpc/package.nix
@@ -1,16 +1,18 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, meson
-, ninja
-, pkg-config
-, glib
-, ncurses
-, libmpdclient
-, gettext
-, boost
-, fmt
-, pcreSupport ? false, pcre ? null
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  glib,
+  ncurses,
+  libmpdclient,
+  gettext,
+  boost,
+  fmt,
+  pcreSupport ? false,
+  pcre ? null,
 }:
 
 assert pcreSupport -> pcre != null;
@@ -20,15 +22,26 @@ stdenv.mkDerivation rec {
   version = "0.51";
 
   src = fetchFromGitHub {
-    owner  = "MusicPlayerDaemon";
-    repo   = "ncmpc";
-    rev    = "v${version}";
+    owner = "MusicPlayerDaemon";
+    repo = "ncmpc";
+    rev = "v${version}";
     sha256 = "sha256-mFZ8szJT7eTPHQHxjpP5pThCcY0YERGkGR8528Xu9MA=";
   };
 
-  buildInputs = [ glib ncurses libmpdclient boost fmt ]
-    ++ lib.optional pcreSupport pcre;
-  nativeBuildInputs = [ meson ninja pkg-config gettext ];
+  buildInputs = [
+    glib
+    ncurses
+    libmpdclient
+    boost
+    fmt
+  ] ++ lib.optional pcreSupport pcre;
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    gettext
+  ];
 
   mesonFlags = [
     "-Dlirc=disabled"
@@ -37,9 +50,9 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "Curses-based interface for MPD (music player daemon)";
-    homepage    = "https://www.musicpd.org/clients/ncmpc/";
-    license     = licenses.gpl2Plus;
-    platforms   = platforms.all;
+    homepage = "https://www.musicpd.org/clients/ncmpc/";
+    license = licenses.gpl2Plus;
+    platforms = platforms.all;
     maintainers = with maintainers; [ fpletz ];
     mainProgram = "ncmpc";
   };
