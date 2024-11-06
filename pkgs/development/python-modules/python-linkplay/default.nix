5d32c9c77ef6d6f4a5ce8731427b1c0491559a8f
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-11-06 22:04:39 +0100

    python312Packages.python-linkplay: 0.0.16 -> 0.0.17
    https://github.com/Velleman/python-linkplay/releases/tag/v0.0.17

--- pkgs/development/python-modules/python-linkplay/default.nix
+++ pkgs/development/python-modules/python-linkplay/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "python-linkplay";
-  version = "0.0.16";
+  version = "0.0.17";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Velleman";
     repo = "python-linkplay";
     rev = "refs/tags/v${version}";
-    hash = "sha256-YjsRjzkYac3IDuAq5s73INELNfWO6EhU5BMM7xQO7pk=";
+    hash = "sha256-TJ5QWt5bCDy9L4EShf1rW1TDIrF0LfeThEv9PiCPEp8=";
   };
 
   build-system = [ setuptools ];
