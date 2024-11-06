4c35f583d4e4d175b32e3e02070b9ddc9d101194
Nick Cao <nickcao@nichi.co>
2024-11-06 07:56:32 -0500

    dmlive: use new darwin sdk pattern

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23982,10 +23982,7 @@ with pkgs;
 
   dmarc-metrics-exporter = callPackage ../servers/monitoring/prometheus/dmarc-metrics-exporter { };
 
-  dmlive = callPackage ../applications/video/dmlive {
-    inherit (darwin) configd;
-    inherit (darwin.apple_sdk.frameworks) Security;
-  };
+  dmlive = callPackage ../applications/video/dmlive { };
 
   do-agent = callPackage ../servers/monitoring/do-agent { };
 
