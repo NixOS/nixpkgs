2bef359ecfbf3172f795a47ed30be39c48da4410
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-11-06 21:26:51 +0100

    python312Packages.hass-nabucasa: 0.82.0 -> 0.83.0
    https://github.com/NabuCasa/hass-nabucasa/releases/tag/0.83.0

--- pkgs/development/python-modules/hass-nabucasa/default.nix
+++ pkgs/development/python-modules/hass-nabucasa/default.nix
@@ -8,7 +8,6 @@
   ciso8601,
   cryptography,
   fetchFromGitHub,
-  fetchpatch2,
   pycognito,
   pyjwt,
   pytest-aiohttp,
@@ -18,21 +17,22 @@
   setuptools,
   snitun,
   syrupy,
+  webrtc-models,
   xmltodict,
 }:
 
 buildPythonPackage rec {
   pname = "hass-nabucasa";
-  version = "0.82.0";
+  version = "0.83.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.11";
+  disabled = pythonOlder "3.12";
 
   src = fetchFromGitHub {
     owner = "nabucasa";
     repo = "hass-nabucasa";
     rev = "refs/tags/${version}";
-    hash = "sha256-hRhRXpiIPrI3umOhsVWLwkSwtEfwevC3fNvJElhKy+I=";
+    hash = "sha256-1l0nLSY+r5ujYo3pHzicqnmZ49OH8elVLGpK3lqCaTo=";
   };
 
   pythonRelaxDeps = [ "acme" ];
@@ -49,6 +49,7 @@ buildPythonPackage rec {
     pycognito
     pyjwt
     snitun
+    webrtc-models
   ];
 
   nativeCheckInputs = [
