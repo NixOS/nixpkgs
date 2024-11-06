b6176b5fd2941ddf40563bef9c1a4a7359b3102b
Jonas Heinrich <onny@project-insanity.org>
2024-11-06 07:35:54 +0100

    eintopf.frontend: cleanup and modernize

--- pkgs/by-name/ei/eintopf/frontend.nix
+++ pkgs/by-name/ei/eintopf/frontend.nix
@@ -1,12 +1,12 @@
 {
   stdenv,
   fetchYarnDeps,
-  fixup-yarn-lock,
-  yarn,
   src,
   version,
   nodejs,
   eintopf,
+  yarnConfigHook,
+  yarnBuildHook,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -21,30 +21,11 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   nativeBuildInputs = [
-    fixup-yarn-lock
+    yarnConfigHook
+    yarnBuildHook
+    # Needed for executing package.json scripts
     nodejs
-    yarn
   ];
-
-  configurePhase = ''
-    runHook preConfigure
-
-    export HOME=$(mktemp -d)
-    yarn config --offline set yarn-offline-mirror $offlineCache
-    fixup-yarn-lock yarn.lock
-    yarn --offline --frozen-lockfile --ignore-platform --ignore-scripts --no-progress --non-interactive install
-    patchShebangs node_modules
-
-    runHook postConfigure
-  '';
-
-  buildPhase = ''
-    runHook preBuild
-
-    yarn --offline build
-
-    runHook postBuild
-  '';
 
   installPhase = ''
     runHook preInstall
