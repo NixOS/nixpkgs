fc465864227e4492ca9f889d6931fc6116497886
Artturin <Artturin@artturin.com>
2024-11-06 05:28:09 +0200

    openjdk: Fix cross for most versions
    `jdk8` cross still broken
    
    Using `__spliced.depsBuildBuild` to reduce rebuilds/space usage because it does not seem to matter if
    it's that or `buildHost`
    
    `depsBuildBuild` `configure: error: Could not find required tool for BUILD_CC`
    
    `zlib` in `nativeBuildInputs`
    
    ```
    /build/source/src/java.base/share/native/libzip/Adler32.c:33:10: fatal error: zlib.h: No such file or directory
       33 | #include <zlib.h>
          |          ^~~~~~~~
    compilation terminated.
    make[4]: *** [lib/CoreLibraries.gmk:88: /build/source/build/linux-aarch64-server-release/buildjdk/support/native/java.base/libzip/Adler32.o] Error 1
    make[4]: *** Waiting for unfinished jobs....
    /build/source/src/java.base/share/native/libzip/CRC32.c:32:10: fatal error: zlib.h: No such file or directory
       32 | #include <zlib.h>
          |          ^~~~~~~~
    compilation terminated.
    /build/source/src/java.base/share/native/libzip/Deflater.c:35:10: fatal error: zlib.h: No such file or directory
       35 | #include <zlib.h>
          |          ^~~~~~~~
    compilation terminated.
    make[4]: *** [lib/CoreLibraries.gmk:88: /build/source/build/linux-aarch64-server-release/buildjdk/support/native/java.base/libzip/CRC32.o] Error 1
    make[4]: *** [lib/CoreLibraries.gmk:88: /build/source/build/linux-aarch64-server-release/buildjdk/support/native/java.base/libzip/Deflater.o] Error 1
    make[3]: *** [Main.gmk:191: java.base-libs] Error 2
    make[2]: *** [make/Main.gmk:589: create-buildjdk-interim-image] Error 2
    make[2]: *** Waiting for unfinished jobs....
    ERROR: Build failed for target 'images' in configuration 'linux-aarch64-server-release' (exit code 2)
    Stopping javac server
    ```

--- pkgs/development/compilers/openjdk/generic.nix
+++ pkgs/development/compilers/openjdk/generic.nix
@@ -7,6 +7,7 @@
   fetchurl,
   fetchpatch,
 
+  buildPackages,
   pkg-config,
   autoconf,
   lndir,
@@ -77,11 +78,11 @@
   temurin-bin-23,
   jdk-bootstrap ?
     {
-      "8" = temurin-bin-8;
-      "11" = temurin-bin-11;
-      "17" = temurin-bin-17;
-      "21" = temurin-bin-21;
-      "23" = temurin-bin-23;
+      "8" = temurin-bin-8.__spliced.buildBuild or temurin-bin-8;
+      "11" = temurin-bin-11.__spliced.buildBuild or temurin-bin-11;
+      "17" = temurin-bin-17.__spliced.buildBuild or temurin-bin-17;
+      "21" = temurin-bin-21.__spliced.buildBuild or temurin-bin-21;
+      "23" = temurin-bin-23.__spliced.buildBuild or temurin-bin-23;
     }
     .${featureVersion},
 }:
@@ -241,6 +242,10 @@ stdenv.mkDerivation (finalAttrs: {
       )
     ];
 
+  strictDeps = true;
+
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+
   nativeBuildInputs =
     [
       pkg-config
@@ -250,9 +255,15 @@ stdenv.mkDerivation (finalAttrs: {
     ]
     ++ lib.optionals (!atLeast11) [
       lndir
+      # Certificates generated using perl in `installPhase`
+      perl
     ]
     ++ [
       unzip
+      zip
+      which
+      # Probably for BUILD_CC but not sure, not in closure.
+      zlib
     ]
     ++ lib.optionals atLeast21 [
       ensureNewerSourcesForZipFilesHook
@@ -262,11 +273,8 @@ stdenv.mkDerivation (finalAttrs: {
     [
       # TODO: Many of these should likely be in `nativeBuildInputs`.
       cpio
+      # `-lmagic` in NIX_LDFLAGS
       file
-      which
-      zip
-      perl
-      zlib
       cups
       freetype
     ]
@@ -305,7 +313,6 @@ stdenv.mkDerivation (finalAttrs: {
       libXcursor
       libXrandr
       fontconfig
-      jdk-bootstrap'
     ]
     ++ lib.optionals (!headless && enableGtk) [
       (if atLeast11 then gtk3 else gtk2)
@@ -330,6 +337,14 @@ stdenv.mkDerivation (finalAttrs: {
   configureFlags =
     [
       "--with-boot-jdk=${jdk-bootstrap'.home}"
+      # https://github.com/openjdk/jdk/blob/471f112bca715d04304cbe35c6ed63df8c7b7fee/make/autoconf/util_paths.m4#L315
+      # Ignoring value of READELF from the environment. Use command line variables instead.
+      "READELF=${stdenv.cc.targetPrefix}readelf"
+      "AR=${stdenv.cc.targetPrefix}ar"
+      "STRIP=${stdenv.cc.targetPrefix}strip"
+      "NM=${stdenv.cc.targetPrefix}nm"
+      "OBJDUMP=${stdenv.cc.targetPrefix}objdump"
+      "OBJCOPY=${stdenv.cc.targetPrefix}objcopy"
     ]
     ++ (
       if atLeast23 then
