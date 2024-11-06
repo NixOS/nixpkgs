27ecc78052d39551bb142c678b91e04102a64b0b
Pavel Anpin <pavel@anpin.fyi>
2024-11-06 09:11:59 -0300

    azure-artifacts-credprovider: fix formating

--- pkgs/by-name/az/azure-artifacts-credprovider/package.nix
+++ pkgs/by-name/az/azure-artifacts-credprovider/package.nix
@@ -1,31 +1,32 @@
-{ lib
-, buildDotnetModule
-, dotnetCorePackages
-, fetchFromGitHub
+{
+  lib,
+  buildDotnetModule,
+  dotnetCorePackages,
+  fetchFromGitHub,
 }:
 buildDotnetModule rec {
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
   dotnet-runtime = dotnetCorePackages.runtime_8_0;
   version = "1.3.0";
   src = fetchFromGitHub {
-      owner = "microsoft";
-      repo = "artifacts-credprovider";
-      rev = "v${version}";
-      sha256 = "sha256-JbcoDs4c/+uKIgVWZkuo4jqd1hlqe+H949jNfkDwZls=";
+    owner = "microsoft";
+    repo = "artifacts-credprovider";
+    rev = "v${version}";
+    sha256 = "sha256-JbcoDs4c/+uKIgVWZkuo4jqd1hlqe+H949jNfkDwZls=";
   };
   pname = "azure-artifacts-credprovider";
-  projectFile =  "CredentialProvider.Microsoft/CredentialProvider.Microsoft.csproj";
+  projectFile = "CredentialProvider.Microsoft/CredentialProvider.Microsoft.csproj";
   testProjectFile = "CredentialProvider.Microsoft.Tests/CredentialProvider.Microsoft.Tests.csproj";
   nugetDeps = ./deps.nix;
   passthru.updateScript = ./update.sh;
   patchPhase = ''
     sed -i 's|<TargetFrameworks>.*</TargetFrameworks>|<TargetFramework>net8.0</TargetFramework>|' Build.props
   '';
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/microsoft/artifacts-credprovider";
     description = "Azure Artifacts Credential Provider";
-    license = licenses.mit;
-    maintainers = with maintainers; [ anpin ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ anpin ];
     mainProgram = "CredentialProvider.Microsoft";
     platforms = [ "x86_64-linux" ];
   };
