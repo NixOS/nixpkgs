cc2623c2b44e49853a25b930787322e2acb5d202
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-11-06 22:09:20 +0100

    dependabot-cli: Split image version into a separate derivation

--- pkgs/by-name/de/dependabot-cli/package.nix
+++ pkgs/by-name/de/dependabot-cli/package.nix
@@ -6,6 +6,7 @@
   installShellFiles,
   lib,
   makeWrapper,
+  symlinkJoin,
   testers,
 }:
 let
@@ -65,13 +66,6 @@ buildGoModule {
       --bash <($out/bin/dependabot completion bash) \
       --fish <($out/bin/dependabot completion fish) \
       --zsh <($out/bin/dependabot completion zsh)
-
-    # Create a wrapper that pins the docker images that are depended upon
-    makeWrapper $out/bin/dependabot $out/bin/dependabot-pinned \
-      --run "docker load --input ${updateJobProxy}" \
-      --add-flags "--proxy-image=dependabot-update-job-proxy:${tag}" \
-      --run "docker load --input ${updaterGitHubActions}" \
-      --add-flags "--updater-image=dependabot-updater-github-actions:${tag}"
   '';
 
   checkFlags = [
@@ -82,6 +76,20 @@ buildGoModule {
   installCheckPhase = ''
     $out/bin/dependabot --help
   '';
+
+  passthru.withDockerImages = symlinkJoin {
+    name = "dependabot-cli-with-docker-images";
+    paths = [ dependabot-cli ];
+    buildInputs = [ makeWrapper ];
+    postBuild = ''
+      # Create a wrapper that pins the docker images that are depended upon
+      wrapProgram $out/bin/dependabot \
+        --run "docker load --input ${updateJobProxy} >&2" \
+        --add-flags "--proxy-image=dependabot-update-job-proxy:${tag}" \
+        --run "docker load --input ${updaterGitHubActions} >&2" \
+        --add-flags "--updater-image=dependabot-updater-github-actions:${tag}"
+    '';
+  };
 
   passthru.tests.version = testers.testVersion {
     package = dependabot-cli;
