7a9fb50f24bb71d85382022cbab227309c64cfc2
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2024-11-06 14:51:52 +0100

    python3Packages.telegram: removal (#353915)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15465,8 +15465,6 @@ self: super: with self; {
 
   teamcity-messages = callPackage ../development/python-modules/teamcity-messages { };
 
-  telegram = callPackage ../development/python-modules/telegram { };
-
   telegraph = callPackage ../development/python-modules/telegraph { };
 
   telepath = callPackage ../development/python-modules/telepath { };
