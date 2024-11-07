92d3bc6044aa30ac51cd03c17fd7df81ec28e023
Enric Morales <me@enric.me>
2024-11-07 11:51:45 +0100

    basedpyright: 1.19.0 -> 1.21.0
    Diff:
    https://github.com/detachhead/basedpyright/compare/refs/tags/v1.19.0...v1.21.0
    
    Changelog:
    https://github.com/detachhead/basedpyright/releases/tag/1.21.0

--- pkgs/by-name/ba/basedpyright/package.nix
+++ pkgs/by-name/ba/basedpyright/package.nix
@@ -3,112 +3,62 @@
   fetchFromGitHub,
   runCommand,
   buildNpmPackage,
-  stdenvNoCC,
   docify,
   testers,
   writeText,
   jq,
-  python3,
   basedpyright,
+  pkg-config,
+  libsecret,
+  nix-update-script,
+  versionCheckHook,
 }:
 
-let
-  version = "1.19.0";
+buildNpmPackage rec {
+  pname = "basedpyright";
+  version = "1.21.0";
 
   src = fetchFromGitHub {
     owner = "detachhead";
     repo = "basedpyright";
     rev = "refs/tags/v${version}";
-    hash = "sha256-R8QEINTlO84YrnfRsZU5vNEwYcy5uZEKmmTPJ4qxYk8=";
+    hash = "sha256-OQXqwpvYIitWGWshEv1/j2hAphFnGXDuvbRav5TilI4=";
   };
 
-  # To regenerate the patched package-lock.json, copy the patched package.json
-  # and run `nix-shell -p nodejs --command 'npm update --package-lock'`
-  patchedPackageJSON = runCommand "package.json" { } ''
-    ${jq}/bin/jq '
-      .devDependencies |= with_entries(select(.key == "glob" or .key == "jsonc-parser" or .key == "@detachhead/ts-helpers"))
-      | .scripts =  {  }
-      ' ${src}/package.json > $out
+  npmDepsHash = "sha256-hCZ68sLpQs/7SYVf3pMAHfstRm1C/d80j8fESIFdhnw=";
+  npmWorkspace = "packages/pyright";
+
+  preBuild = ''
+    # Build the docstubs
+    cp -r packages/pyright-internal/typeshed-fallback docstubs
+    docify docstubs/stdlib --builtins-only --in-place
   '';
 
-  pyright-root = buildNpmPackage {
-    pname = "pyright-root";
-    inherit version src;
-    npmDepsHash = "sha256-vxfoaShk3ihmhr/5/2GSOuMqeo6rxebO6aiD3DybjW4=";
-    dontNpmBuild = true;
-    postPatch = ''
-      cp ${patchedPackageJSON} ./package.json
-      cp ${./package-lock.json} ./package-lock.json
-    '';
-    installPhase = ''
-      runHook preInstall
-      cp -r . "$out"
-      runHook postInstall
-    '';
-  };
+  nativeBuildInputs = [
+    docify
+    pkg-config
+  ];
 
-  pyright-internal = buildNpmPackage {
-    pname = "pyright-internal";
-    inherit version src;
-    sourceRoot = "${src.name}/packages/pyright-internal";
-    npmDepsHash = "sha256-Xswt1kH9Iet6DSAlZctEV3qCpn+IWwceQgldyFzsaRk=";
-    dontNpmBuild = true;
-    # Uncomment this flag when using unreleased peer dependencies
-    # npmFlags = [ "--legacy-peer-deps" ];
-    installPhase = ''
-      runHook preInstall
-      cp -r . "$out"
-      runHook postInstall
-    '';
-  };
-
-  docstubs = stdenvNoCC.mkDerivation {
-    name = "docstubs";
-    inherit src;
-    nativeBuildInputs = [ docify ];
-
-    installPhase = ''
-      runHook preInstall
-      cp -r packages/pyright-internal/typeshed-fallback docstubs
-      docify docstubs/stdlib --builtins-only --in-place
-      cp -rv docstubs "$out"
-      runHook postInstall
-    '';
-  };
-in
-buildNpmPackage rec {
-  pname = "basedpyright";
-  inherit version src;
-
-  sourceRoot = "${src.name}/packages/pyright";
-  npmDepsHash = "sha256-YhXVoh5sAnUkuc4iDMd/N15y92vMhUwT7PlLn2pzb20=";
-
-  postPatch = ''
-    chmod +w ../../
-    ln -s ${docstubs} ../../docstubs
-    ln -s ${pyright-root}/node_modules ../../node_modules
-    chmod +w ../pyright-internal
-    ln -s ${pyright-internal}/node_modules ../pyright-internal/node_modules
-  '';
+  buildInputs = [ libsecret ];
 
   postInstall = ''
     mv "$out/bin/pyright" "$out/bin/basedpyright"
     mv "$out/bin/pyright-langserver" "$out/bin/basedpyright-langserver"
   '';
 
-  dontNpmBuild = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = [ "--version" ];
+  doInstallCheck = true;
 
   passthru = {
-    updateScript = ./update.sh;
+    updateScript = nix-update-script { };
     tests = {
-      version = testers.testVersion { package = basedpyright; };
-
-      # We are expecting 3 errors. Any other amount would indicate, not working
+      # We are expecting 4 errors. Any other amount would indicate not working
       # stub files, for instance.
       simple = testers.testEqualContents {
         assertion = "simple type checking";
         expected = writeText "expected" ''
-          3
+          4
         '';
         actual =
           runCommand "actual"
@@ -117,16 +67,24 @@ buildNpmPackage rec {
                 jq
                 basedpyright
               ];
-              base = writeText "base" ''
+              base = writeText "test.py" ''
                 import sys
+                from time import tzset
+
+                def print_string(a_string: str):
+                    a_string += 42
+                    print(a_string)
 
                 if sys.platform == "win32":
-                    a = "a" + 1
-
-                print(3)
-                nonexistentfunction(3)
+                    print_string(69)
+                    this_function_does_not_exist("nice!")
+                else:
+                    result_of_tzset_is_None: str = tzset()
               '';
-
+              configFile = writeText "pyproject.toml" ''
+                [tool.pyright]
+                typeCheckingMode = "strict"
+              '';
             }
             ''
               (basedpyright --outputjson $base || true) | jq -r .summary.errorCount > $out
