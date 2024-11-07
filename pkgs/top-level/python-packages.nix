34a1772e9b8db35d1ac85c47c1cd01c4c4c4b40b
Adrien Faure <adrien.faure@protonmail.com>
2024-11-07 22:36:03 +0100

    mozjpeg_lossless_optimization: init at 1.1.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8713,6 +8713,8 @@ self: super: with self; {
 
   mozilla-django-oidc = callPackage ../development/python-modules/mozilla-django-oidc { };
 
+  mozjpeg_lossless_optimization = callPackage ../development/python-modules/mozjpeg_lossless_optimization {};
+
   mpd2 = callPackage ../development/python-modules/mpd2 { };
 
   mpegdash = callPackage ../development/python-modules/mpegdash { };
