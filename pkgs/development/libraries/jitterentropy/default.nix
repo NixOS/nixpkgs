01a06c648ebd2c122ad91b500cf26cb07a447ad7
Matteo Pacini <m@matteopacini.me>
2024-11-07 14:15:07 +0000

    jitterentropy: switch to cmake build, darwin support

--- pkgs/development/libraries/jitterentropy/default.nix
+++ pkgs/development/libraries/jitterentropy/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub }:
+{ lib, stdenv, cmake, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "jitterentropy";
@@ -11,24 +11,18 @@ stdenv.mkDerivation rec {
     hash = "sha256-CPvgc/W5Z2OfbP9Lp2tQevUQZr+xlh6q5r5Fp2WUHhg=";
   };
 
+  nativeBuildInputs = [ cmake ];
+
   outputs = [ "out" "dev" ];
 
-  enableParallelBuilding = true;
   hardeningDisable = [ "fortify" ]; # avoid warnings
-
-  # prevent jitterentropy from builtin strip to allow controlling this from the derivation's
-  # settings. Also fixes a strange issue, where this strip may fail when cross-compiling.
-  installFlags = [
-    "INSTALL_STRIP=install"
-    "PREFIX=${placeholder "out"}"
-  ];
 
   meta = with lib; {
     description = "Provides a noise source using the CPU execution timing jitter";
     homepage = "https://github.com/smuellerDD/jitterentropy-library";
     changelog = "https://github.com/smuellerDD/jitterentropy-library/raw/v${version}/CHANGES.md";
     license = with licenses; [ bsd3 /* OR */ gpl2Only ];
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.darwin;
     maintainers = with maintainers; [ johnazoidberg c0bw3b ];
   };
 }
