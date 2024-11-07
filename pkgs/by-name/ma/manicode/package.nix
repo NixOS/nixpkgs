1fbe6001a2d1ac69f40cf3c1aa7e3307c417d981
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-07 15:13:05 +0000

    manicode: 1.0.94 -> 1.0.99

--- pkgs/by-name/ma/manicode/package.nix
+++ pkgs/by-name/ma/manicode/package.nix
@@ -6,14 +6,14 @@
 
 buildNpmPackage rec {
   pname = "manicode";
-  version = "1.0.94";
+  version = "1.0.99";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/manicode/-/manicode-${version}.tgz";
-    hash = "sha256-mn3bxZashP4zgCO7pB9yqjZ7uLglTC+pa3ifxlIW4BY=";
+    hash = "sha256-LVTh8yOfP92zGSdxLpThC+U9E8cBjoL0+iMQOldNO8A=";
   };
 
-  npmDepsHash = "sha256-PnySdTtlgZ9J0qIegwgiDoGuqa7/KyUxUpiZ8yepuZI=";
+  npmDepsHash = "sha256-MAm/FE8M6BBDZD5Fy2k6GcM5Qv35jNeUwHcemmbUj/A=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
