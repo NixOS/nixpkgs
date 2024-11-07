e7a3e8cb63ff48fb0a424faf591f3e2beb36f412
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-07 09:44:54 +0000

    erlang-language-platform: 2024-07-16 -> 2024-11-07

--- pkgs/by-name/er/erlang-language-platform/package.nix
+++ pkgs/by-name/er/erlang-language-platform/package.nix
@@ -13,24 +13,24 @@ let
       "linux-${arch}-unknown-linux-gnu";
 
   hashes = {
-    linux-aarch64-unknown-linux-gnu = "sha256-vWMrq/uFU/uyuDnsxZK0ZyvtraVCZwvGjzO1a5QjR8g=";
-    linux-x86_64-unknown-linux-gnu = "sha256-iE/zH6M51C6sFZrsUMwZTQ0+hzfpRFJtiKh3MS9bDto=";
-    macos-aarch64-apple-darwin = "sha256-55LSChvO0wRHGL0H29MLy/JW8V52GFr3z/qoxdIPun0=";
-    macos-x86_64-apple-darwin = "sha256-l9bzQ5z9hQ/N2dOkAjPAU4OfRbLCUoRt1eQB6EZE0NI=";
+    linux-aarch64-unknown-linux-gnu = "sha256-Fte7oZD5+aFph5xGrKtbSimb3aHewkjsJRXB+64IW5A=";
+    linux-x86_64-unknown-linux-gnu = "sha256-tu4uloIyXjq5DjDaU4qxbf8a/S7yv3RsNcUwusfMztw=";
+    macos-aarch64-apple-darwin = "sha256-3K3sPizBR/+DJIX67GsYqm2X5k7kq3kBRg8P2WALTZs=";
+    macos-x86_64-apple-darwin = "sha256-j+AVmLfe/yCvKvYhL5ikhXA1g+zQ1CDlMl1FYO6q1yA=";
   };
 in
 stdenv.mkDerivation rec {
   pname = "erlang-language-platform";
-  version = "2024-07-16";
+  version = "2024-11-07";
 
   src = fetchurl {
     url = "https://github.com/WhatsApp/erlang-language-platform/releases/download/${version}/elp-${release}-otp-26.2.tar.gz";
     hash = hashes.${release};
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isElf [ autoPatchelfHook ];
 
-  buildInputs = [ (lib.getLib stdenv.cc.cc) ];
+  buildInputs = lib.optionals stdenv.hostPlatform.isElf [ (lib.getLib stdenv.cc.cc) ];
 
   sourceRoot = ".";
 
