a4a636d1237f3e9e9ee263917da950b757972836
Noa Aarts <noa@voorwaarts.nl>
2024-11-07 21:14:53 +0100

    python312Packages.oddsprout: 0.1.0 -> 0.1.1

--- pkgs/development/python-modules/oddsprout/default.nix
+++ pkgs/development/python-modules/oddsprout/default.nix
@@ -3,24 +3,37 @@
   buildPythonPackage,
   fetchFromGitHub,
   poetry-core,
+  pytestCheckHook,
+  pythonOlder,
   dahlia,
-  ixia
+  ixia,
 }:
 
 buildPythonPackage rec {
   pname = "oddsprout";
-  version = "0.1.0";
+  version = "0.1.1";
   pyproject = true;
+
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "trag1c";
     repo = "oddsprout";
     rev = "refs/tags/v${version}";
-    hash = "sha256-k5/mBoW4PxGUbkwaZyHgS3MGI4533V/nNoGqEg+VXpM=";
+    hash = "sha256-BOUYq4yny3ScgzCzx2cpeK4e7nxxwTj8mJ42nr59mFA=";
   };
 
   build-system = [ poetry-core ];
-  dependencies = [ dahlia ixia ];
+
+  dependencies = [
+    dahlia
+    ixia
+  ];
+
+  # has one test `test_main_recursion_error`
+  # that has a very low (~1%) but nonzero chance to fail,
+  # this is known upstream (https://github.com/trag1c/oddsprout/issues/5)
+  nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "oddsprout" ];
 
