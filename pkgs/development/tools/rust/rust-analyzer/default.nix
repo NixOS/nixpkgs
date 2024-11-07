cbaf11a261b9147b3d5c78dc96125e7eb12bce81
Austin Horstman <khaneliman12@gmail.com>
2024-11-07 13:36:45 -0600

    rust-analyzer-unwrapped: 2024-09-02 -> 2024-11-04

--- pkgs/development/tools/rust/rust-analyzer/default.nix
+++ pkgs/development/tools/rust/rust-analyzer/default.nix
@@ -13,14 +13,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rust-analyzer-unwrapped";
-  version = "2024-09-02";
-  cargoHash = "sha256-t45RzYkuywGByGWwUON3dW0aKjLYcFXB8uy4CybPuf4=";
+  version = "2024-11-04";
+  cargoHash = "sha256-eiZuKnif63usgS2fSon9KtxxDYbAvHs3sTMRCib40Io=";
 
   src = fetchFromGitHub {
     owner = "rust-lang";
     repo = "rust-analyzer";
     rev = version;
-    hash = "sha256-YH0kH5CSOnAuPUB1BUzUqvnKiv5SgDhfMNjrkki9Ahk=";
+    hash = "sha256-5gBDDKKwiMzR7W/b4PROLeNiXbB4ux1YDDLebaFzrmM=";
   };
 
   cargoBuildFlags = [ "--bin" "rust-analyzer" "--bin" "rust-analyzer-proc-macro-srv" ];
