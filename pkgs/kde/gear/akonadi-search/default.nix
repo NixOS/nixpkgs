78fff8b158ad7a88f368a02d1de815148c5a7a7e
K900 <me@0upti.me>
2024-11-07 23:02:52 +0300

    kdePackages: Gear 24.08.2 -> 24.08.3

--- pkgs/kde/gear/akonadi-search/default.nix
+++ pkgs/kde/gear/akonadi-search/default.nix
@@ -7,7 +7,7 @@
   cargo,
   rustc,
   # provided as callPackage input to enable easier overrides through overlays
-  cargoHash ? "sha256-E+QaicYnFF79FDIhhvuEPQLikiLk5oKIjvLA132RUZo=",
+  cargoHash ? "sha256-9cwNQSw0ueWyyL/qgWXXqIRsi4Lk/DX48CIbvQFNgP8=",
 }:
 mkKdeDerivation rec {
   pname = "akonadi-search";
