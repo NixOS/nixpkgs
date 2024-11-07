9a4bbbf72de6d46e8bc375b5e5c69e2536e6a33a
r-vdp <ramses@well-founded.dev>
2024-11-07 13:43:54 +0100

    zellij: adapt to new darwin SDK pattern

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13856,9 +13856,7 @@ with pkgs;
 
   zed = callPackage ../development/tools/zed { };
 
-  zellij = callPackage ../tools/misc/zellij {
-    inherit (darwin.apple_sdk.frameworks) DiskArbitration Foundation;
-  };
+  zellij = callPackage ../tools/misc/zellij { };
 
   zenith = callPackage ../tools/system/zenith {
     inherit (darwin.apple_sdk.frameworks) IOKit;
