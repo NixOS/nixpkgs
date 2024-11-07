fe896ca861eb2431863d25f9f8da7b4bd980c012
Maximilian Bosch <maximilian@mbosch.me>
2024-11-07 21:20:08 +0100

    nextcloud28: 28.0.11 -> 28.0.12
    ChangeLog: https://github.com/nextcloud/server/releases/tag/v28.0.12

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -59,8 +59,8 @@ let
 in
 {
   nextcloud28 = generic {
-    version = "28.0.11";
-    hash = "sha256-S6rs7GpvFFgy28PGNdcuIM1IBKytmmZOanS5CnmB40g=";
+    version = "28.0.12";
+    hash = "sha256-KgDKT3mTYPCYf8vIXZmywlj30sz35vfgxMzxehJ/AQU=";
     packages = nextcloud28Packages;
   };
 
