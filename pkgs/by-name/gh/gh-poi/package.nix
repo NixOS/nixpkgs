038b963062e5ee9d983584e5b570390272fbec4b
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-07 20:43:24 +0000

    gh-poi: 0.10.1 -> 0.11.0

--- pkgs/by-name/gh/gh-poi/package.nix
+++ pkgs/by-name/gh/gh-poi/package.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "gh-poi";
-  version = "0.10.1";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "seachicken";
     repo = "gh-poi";
     rev = "v${version}";
-    hash = "sha256-ZQkXXaa4n88bJdgP2FSXtgBrUi39teO98SzZq+I5doM=";
+    hash = "sha256-xLeIqTGwizvBmOgwisqoCrFY4KdWoWrA0YYgPWtQT/M=";
   };
 
   ldflags = [ "-s" "-w" ];
@@ -19,7 +19,7 @@ buildGoModule rec {
   vendorHash = "sha256-D/YZLwwGJWCekq9mpfCECzJyJ/xSlg7fC6leJh+e8i0=";
 
   # Skip checks because some of test suites require fixture.
-  # See: https://github.com/seachicken/gh-poi/blob/v0.10.1/.github/workflows/contract-test.yml#L28-L29
+  # See: https://github.com/seachicken/gh-poi/blob/v0.11.0/.github/workflows/contract-test.yml#L28-L29
   doCheck = false;
 
   meta = with lib; {
