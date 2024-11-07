f7afee0d5d65a163a9d725f9a98c3b67360806f7
sarahec <sarahec@nextquestion.net>
2024-11-07 14:23:11 -0800

    python312Packages.langgraph: Fix unit tests that were breaking Hydra

--- pkgs/development/python-modules/langgraph/default.nix
+++ pkgs/development/python-modules/langgraph/default.nix
@@ -88,8 +88,6 @@ buildPythonPackage rec {
     postgresqlTestHook
   ];
 
-  pytestFlagsArray = [ "tests/unit_tests" ];
-
   disabledTests = [
     # test is flaky due to pydantic error on the exception
     "test_doesnt_warn_valid_schema"
@@ -116,9 +114,7 @@ buildPythonPackage rec {
     "tests/test_pregel.py"
   ];
 
-  passthru = {
-    updateScript = langgraph-sdk.updateScript;
-  };
+  passthru.updateScript = langgraph-sdk.updateScript;
 
   meta = {
     description = "Build resilient language agents as graphs";
