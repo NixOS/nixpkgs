857373a41c8df1fb47fc34f303593a32c2d765df
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-07 22:10:01 +0000

    python312Packages.pyngrok: 7.2.0 -> 7.2.1

--- pkgs/development/python-modules/pyngrok/default.nix
+++ pkgs/development/python-modules/pyngrok/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pyngrok";
-  version = "7.2.0";
+  version = "7.2.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-TkOvmy8hzu2NITeXAo/ogjAD8YW0l5Lk04MwI2XIFRU=";
+    hash = "sha256-UJIbCcsQzKmFeH8gvfxFVN4ovT7eCnSHBsCCooInm/E=";
   };
 
   nativeBuildInputs = [ setuptools ];
