ea2c62c2a61b50e2dc51be888db6f459c4e54cf0
José Romildo <malaquias@gmail.com>
2024-11-06 23:22:16 -0300

    lxqt.pcmanfm-qt: 2.0.0 -> 2.1.0

--- pkgs/desktops/lxqt/pcmanfm-qt/default.nix
+++ pkgs/desktops/lxqt/pcmanfm-qt/default.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "pcmanfm-qt";
-  version = "2.0.0";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "lxqt";
     repo = pname;
     rev = version;
-    hash = "sha256-PyCtcn+QHwX/iy85A3y7Phf8ogdSRrwtXrJYGxrjyLM=";
+    hash = "sha256-3LdoJLlGoUsv0MdbxIOZrzLaPnZ2hI6m+bLp4GNonng=";
   };
 
   nativeBuildInputs = [
