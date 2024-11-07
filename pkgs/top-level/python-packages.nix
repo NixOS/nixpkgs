093faf756d382827f9f8688e8be189a73eb168da
Ben Darwin <bcdarwin@gmail.com>
2024-11-07 14:29:23 -0500

    python312Packages.pandera: init at 0.20.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9949,6 +9949,8 @@ self: super: with self; {
 
   pandas-datareader = callPackage ../development/python-modules/pandas-datareader { };
 
+  pandera = callPackage ../development/python-modules/pandera { };
+
   pandoc-attributes = callPackage ../development/python-modules/pandoc-attributes { };
 
   pandoc-latex-environment = callPackage ../development/python-modules/pandoc-latex-environment { };
