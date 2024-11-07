0ada8d6d0dd2d5bd58ac6753853c1b4bb54043e7
Zuruh <zuruh.dev@proton.me>
2024-11-07 19:49:58 +0100

    phpactor: 2024.06.30.0 -> 2024.11.05.0 (#354051)

--- pkgs/by-name/ph/phpactor/package.nix
+++ pkgs/by-name/ph/phpactor/package.nix
@@ -7,16 +7,16 @@
 
 php.buildComposerProject2 (finalAttrs: {
   pname = "phpactor";
-  version = "2024.06.30.0";
+  version = "2024.11.05.0";
 
   src = fetchFromGitHub {
     owner = "phpactor";
     repo = "phpactor";
     rev = finalAttrs.version;
-    hash = "sha256-QcKkkgpWWypapQPawK1hu+6tkF9c5ICPeEPWqCwrUBM=";
+    hash = "sha256-/h7Apqo0N4aQvLfzfV/v35npo1wwOOZksokJKhCp8oA=";
   };
 
-  vendorHash = "sha256-Q72EeGeVqjaOZeW8VAB59OY0E/wvL8Ljq/9XC4iK/rg=";
+  vendorHash = "sha256-nfy2H6isjW7m0UdPaX9Kqt2iwA2IwOhHE+xmqJ2t1qo=";
 
   nativeBuildInputs = [ installShellFiles ];
 
