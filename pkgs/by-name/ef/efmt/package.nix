2dbe40bcca39aa4a74aeb6b674e4432e5e03cbb2
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-07 16:47:10 +0000

    efmt: 0.18.2 -> 0.18.3

--- pkgs/by-name/ef/efmt/package.nix
+++ pkgs/by-name/ef/efmt/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "efmt";
-  version = "0.18.2";
+  version = "0.18.3";
 
   src = fetchFromGitHub {
     owner = "sile";
     repo = "efmt";
     rev = version;
-    hash = "sha256-sS6OqcVuRZXGjrTv5gYz3ECrHNOsu/1eN7Jqs9QRm3Q=";
+    hash = "sha256-3CL9NcwDsAs6fHUWA/ibkwqOW0Ur4glpHVZTrfLQUXs=";
   };
 
-  cargoHash = "sha256-gcgKOgWdQwSFN9WMTJ/PBob+iuAqG+yTrSnbevM+csI=";
+  cargoHash = "sha256-hEJb1SOQ3daY2otroCds8zitoodSjRyRTyR7GwF8dgk=";
 
   meta = {
     description = "Erlang code formatter";
