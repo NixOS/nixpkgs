9c8c11f5c2d1dc232f26335c3e5a98805ce7458b
seth <getchoo@tuta.io>
2024-11-07 14:28:58 -0500

    binary: run meson checks
    also clean up a few things

--- pkgs/by-name/bi/binary/package.nix
+++ pkgs/by-name/bi/binary/package.nix
@@ -1,23 +1,24 @@
 {
   lib,
-  fetchFromGitHub,
-  python3Packages,
   appstream,
   blueprint-compiler,
   desktop-file-utils,
+  fetchFromGitHub,
   glib,
   gobject-introspection,
+  gtk4,
   libadwaita,
   meson,
   ninja,
   pkg-config,
+  python3Packages,
   wrapGAppsHook4,
 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "binary";
   version = "5.1";
-  format = "other";
+  pyproject = false;
 
   src = fetchFromGitHub {
     owner = "fizzyizzy05";
@@ -26,12 +27,15 @@ python3Packages.buildPythonApplication rec {
     hash = "sha256-HBmWaT0cjYz3UAO1r5chFW7KARpL1EnY8wEeR9etPE0=";
   };
 
+  strictDeps = true;
+
   nativeBuildInputs = [
     appstream
     blueprint-compiler
-    desktop-file-utils
-    glib # need glib-compile-schemas
+    desktop-file-utils # for `desktop-file-validate`
+    glib # for `glib-compile-schemas`
     gobject-introspection
+    gtk4 # for `gtk-update-icon-cache`
     meson
     ninja
     pkg-config
@@ -44,6 +48,13 @@ python3Packages.buildPythonApplication rec {
 
   dontWrapGApps = true;
   makeWrapperArgs = [ "\${gappsWrapperArgs[@]}" ];
+
+  # NOTE: `postCheck` is intentionally not used here, as the entire checkPhase
+  # is skipped by `buildPythonApplication`
+  # https://github.com/NixOS/nixpkgs/blob/9d4343b7b27a3e6f08fc22ead568233ff24bbbde/pkgs/development/interpreters/python/mk-python-derivation.nix#L296
+  postInstallCheck = ''
+    mesonCheckPhase
+  '';
 
   meta = {
     description = "Small and simple app to convert numbers to a different base";
