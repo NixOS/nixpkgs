780f200e8fe823dac74e9e614131ea69865914c5
K900 <me@0upti.me>
2024-11-07 23:02:52 +0300

    kdePackages: Gear 24.08.2 -> 24.08.3

--- pkgs/kde/gear/kdepim-addons/default.nix
+++ pkgs/kde/gear/kdepim-addons/default.nix
@@ -8,7 +8,7 @@
   corrosion,
   alpaka,
   # provided as callPackage input to enable easier overrides through overlays
-  cargoHash ? "sha256-t7izRAYjuCYA0YMZaCnvwbVo2UvfTTvdlYUd69T6w/Q=",
+  cargoHash ? "sha256-2t4i7lTJxVNjsU5eO6svadZBIerSyXt6BMScpEl119s=",
 }:
 mkKdeDerivation rec {
   pname = "kdepim-addons";
