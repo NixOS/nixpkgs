56bd9ca03df0156b6356cb3de1ae2218526bce21
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-08 22:34:24 +0000

    audacious-plugins: 4.4.1 -> 4.4.2

--- pkgs/applications/audio/audacious/plugins.nix
+++ pkgs/applications/audio/audacious/plugins.nix
@@ -46,13 +46,13 @@
 
 stdenv.mkDerivation rec {
   pname = "audacious-plugins";
-  version = "4.4.1";
+  version = "4.4.2";
 
   src = fetchFromGitHub {
     owner = "audacious-media-player";
     repo = "audacious-plugins";
     rev = "${pname}-${version}";
-    hash = "sha256-F2kcGc6VCaBsL5Zx7qtZjPvqzaxmR87Q9LTFEU+nqmo=";
+    hash = "sha256-fA7xB04WLlleLIBDEZOVqDQguKAXtTsJoedQ9A/ZHQg=";
   };
 
   patches = [ ./0001-Set-plugindir-to-PREFIX-lib-audacious.patch ];
