9c0369c5be59a399f51a8168bd89f3249ad67a7e
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-08 14:14:48 +0000

    fastp: 0.23.4 -> 0.24.0

--- pkgs/applications/science/biology/fastp/default.nix
+++ pkgs/applications/science/biology/fastp/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fastp";
-  version = "0.23.4";
+  version = "0.24.0";
 
   src = fetchFromGitHub {
     owner = "OpenGene";
     repo = "fastp";
     rev = "v${version}";
-    sha256 = "sha256-hkCo8CiZNJuVcL9Eg/R7YzM7/FEcGEnovV325oWa7y8=";
+    sha256 = "sha256-Jl2Os/6yLWXTDv6irInYlUHpz6QTw8CEMVGFXblAay0=";
   };
 
   buildInputs = [ zlib libdeflate isa-l ];
