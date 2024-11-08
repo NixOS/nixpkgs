098ca6d703be31a658812264a8d7f9f2b29160eb
Maximilian Bosch <maximilian@mbosch.me>
2024-11-08 19:41:32 +0100

    php83: 8.3.12 -> 8.3.13
    ChangeLog: https://www.php.net/ChangeLog-8.php#PHP_8_3

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -682,6 +682,13 @@ in {
               url = "https://github.com/php/php-src/commit/4fe821311cafb18ca8bdf20b9d796c48a13ba552.patch";
               hash = "sha256-YC3I0BQi3o3+VmRu/UqpqPpaSC+ekPqzbORTHftbPvY=";
             })
+          ]
+          ++ lib.optionals (lib.versionAtLeast php.version "8.3") [
+            # https://github.com/php/php-src/pull/16733 (fix soap test)
+            (fetchpatch {
+              url = "https://github.com/php/php-src/commit/5c308d61db104854e4ff84ab123e3ea56e1b4046.patch";
+              hash = "sha256-xQ4Sg4kL0cgHYauRW2AzGgFXfcqtxeRVhI9zNh7CsoM=";
+            })
           ];
         }
         {
