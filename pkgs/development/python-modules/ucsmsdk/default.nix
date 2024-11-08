f544bec29238d0a7f8127f11cfcfee1d4e212f84
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-08 21:05:35 +0000

    python312Packages.ucsmsdk: 0.9.20 -> 0.9.21

--- pkgs/development/python-modules/ucsmsdk/default.nix
+++ pkgs/development/python-modules/ucsmsdk/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "ucsmsdk";
-  version = "0.9.20";
+  version = "0.9.21";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "CiscoUcs";
     repo = "ucsmsdk";
     rev = "refs/tags/v${version}";
-    hash = "sha256-X8lGpfVjIZIpNneOM/mE+9RvglW9FlYoGz7pFs7ellk=";
+    hash = "sha256-iqwU42Im4/YyxefkJZ8Zn5CddjUzuZk/7zjLD/0pHm4=";
   };
 
   propagatedBuildInputs = [
