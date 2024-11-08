2f9dcca3954d5eceaf3af6a611d96f265386e534
Sergei Trofimovich <slyich@gmail.com>
2024-11-08 06:50:12 +0000

    iwd: 3.0 -> 3.1
    Changes:  https://git.kernel.org/pub/scm/network/wireless/iwd.git/tree/ChangeLog?h=3.1

--- pkgs/os-specific/linux/iwd/default.nix
+++ pkgs/os-specific/linux/iwd/default.nix
@@ -13,12 +13,12 @@
 
 stdenv.mkDerivation rec {
   pname = "iwd";
-  version = "3.0";
+  version = "3.1";
 
   src = fetchgit {
     url = "https://git.kernel.org/pub/scm/network/wireless/iwd.git";
     rev = version;
-    hash = "sha256-pkren8mF0xg5yrkaIrMJ5auq+7w8VAldbgVflE2BmlM=";
+    hash = "sha256-WVtZiNcmQRo6reMu9nkB0Z/Y2AAq+FF8F1xB0Tcth0U=";
   };
 
   outputs = [ "out" "man" "doc" ]
