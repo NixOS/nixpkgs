33d6717e7844725807aec5f55eb4aad520ec1271
John Garcia <jgarcia3788@yahoo.co.uk>
2024-11-08 11:45:58 +0000

    apx-gui: format with nixfmt

--- pkgs/by-name/ap/apx-gui/package.nix
+++ pkgs/by-name/ap/apx-gui/package.nix
@@ -1,18 +1,19 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, appstream-glib
-, desktop-file-utils
-, gobject-introspection
-, libadwaita
-, meson
-, ninja
-, pkg-config
-, python3
-, wrapGAppsHook4
-, apx
-, gnome-console
-, vte-gtk4
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  appstream-glib,
+  desktop-file-utils,
+  gobject-introspection,
+  libadwaita,
+  meson,
+  ninja,
+  pkg-config,
+  python3,
+  wrapGAppsHook4,
+  apx,
+  gnome-console,
+  vte-gtk4,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -20,9 +21,9 @@ stdenv.mkDerivation (finalAttrs: {
   version = "1.0.4";
 
   src = fetchFromGitHub {
-    owner  = "Vanilla-OS";
-    repo   = "apx-gui";
-    rev    = "v${finalAttrs.version}";
+    owner = "Vanilla-OS";
+    repo = "apx-gui";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-nZMbVXeWEgfBsVgX2iESRzDgu0tjiqC1dTCaTlW0iWA=";
   };
 
@@ -35,7 +36,11 @@ stdenv.mkDerivation (finalAttrs: {
     meson
     ninja
     pkg-config
-    (python3.withPackages (ps: [ ps.pygobject3 ps.pyyaml ps.requests ]))
+    (python3.withPackages (ps: [
+      ps.pygobject3
+      ps.pyyaml
+      ps.requests
+    ]))
     wrapGAppsHook4
   ];
 
@@ -46,15 +51,20 @@ stdenv.mkDerivation (finalAttrs: {
 
   preFixup = ''
     gappsWrapperArgs+=(
-      --prefix PATH : "${lib.makeBinPath [ apx gnome-console ]}"
+      --prefix PATH : "${
+        lib.makeBinPath [
+          apx
+          gnome-console
+        ]
+      }"
     )
   '';
 
   meta = {
     description = "GUI frontend for Apx in GTK 4 and Libadwaita";
-    homepage    = "https://github.com/Vanilla-OS/apx-gui";
-    license     = lib.licenses.gpl3Only;
-    platforms   = lib.platforms.linux;
+    homepage = "https://github.com/Vanilla-OS/apx-gui";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ chewblacka ];
     mainProgram = "apx-gui";
   };
