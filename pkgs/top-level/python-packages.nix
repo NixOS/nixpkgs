43c3db8be9e00af9fa7cedb8be46fced26d30c05
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-11-08 13:36:51 +0100

    python312Packages.swh-scanner: init at 0.8.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15363,6 +15363,8 @@ self: super: with self; {
 
   swh-model = callPackage ../development/python-modules/swh-model { };
 
+  swh-scanner = callPackage ../development/python-modules/swh-scanner { };
+
   swh-web-client = callPackage ../development/python-modules/swh-web-client { };
 
   switchbot-api = callPackage ../development/python-modules/switchbot-api { };
