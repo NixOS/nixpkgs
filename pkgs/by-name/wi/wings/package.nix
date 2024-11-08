0c6fe0b99602aba4e7424c113c826855e60830f7
NotAShelf <raf@notashelf.dev>
2024-11-08 19:38:31 +0300

    wings: 2.2.4 -> 2.3

--- pkgs/by-name/wi/wings/package.nix
+++ pkgs/by-name/wi/wings/package.nix
@@ -2,29 +2,22 @@
   lib,
   stdenv,
   fetchurl,
-  fetchpatch,
   erlang,
   cl,
   libGL,
   libGLU,
+  libigl,
+  eigen,
   runtimeShell,
 }:
-
 stdenv.mkDerivation rec {
   pname = "wings";
-  version = "2.2.4";
+  version = "2.3";
 
   src = fetchurl {
     url = "mirror://sourceforge/wings/wings-${version}.tar.bz2";
-    sha256 = "1xcmifs4vq2810pqqvsjsm8z3lz24ys4c05xkh82nyppip2s89a3";
+    sha256 = "sha256-dEf6iPbPCLmMqvWjvgEROVACZW8SCsXKi3TWlic+bws=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/dgud/wings/commit/94b3a3c6a0cfdcdbd98edce055d5c83ecb361f37.patch";
-      hash = "sha256-DHT1TiYoowloIWrdutBu70mL+557cTFr1dRcNgwbkpE=";
-    })
-  ];
 
   postPatch = ''
     find . -type f -name "Makefile" -exec sed -i 's,-Werror ,,' {} \;
@@ -35,6 +28,18 @@ stdenv.mkDerivation rec {
     find . -type f -name "*.[eh]rl" -exec sed -i 's,wings/src/,../src/,' {} \;
     find . -type f -name "*.[eh]rl" -exec sed -i 's,wings/e3d/,../e3d/,' {} \;
     find . -type f -name "*.[eh]rl" -exec sed -i 's,wings/intl_tools/,../intl_tools/,' {} \;
+    # Remove external dependency rules (such as libigl, eigen, cl)
+    find . -type f -name "Makefile" -exec sed -i 's,libigl: _deps/libigl,,g' {} \;
+    find . -type f -name "Makefile" -exec sed -i 's,eigen: _deps/eigen,,g' {} \;
+    find . -type f -name "Makefile" -exec sed -i 's,cl: _deps/cl,,g' {} \;
+    # The following lines were added during an update to 2.3 due to changes to the Makefile. Chances are
+    # there are better ways to do this, but I'm not aware if there are. PRs welcome.
+    # Remove any targets that depend on these dependencies (e.g., c_src)
+    find . -type f -name "Makefile" -exec sed -i 's,c_src: $(DEPS),c_src:,g' {} \;
+    find . -type f -name "Makefile" -exec sed -i 's,src: intl_tools e3d,src: intl_tools,g' {} \;
+    find . -type f -name "Makefile" -exec sed -i 's,plugins_src: intl_tools src,plugins_src: intl_tools,g' {} \;
+    # Make sure the Makefile is able to find Eigen. Without this line, it will fail.
+    find . -type f -name "Makefile" -exec sed -i 's,-I../_deps/eigen,-I${eigen}/include/eigen3,' {} \;
   '';
 
   buildInputs = [
@@ -42,12 +47,16 @@ stdenv.mkDerivation rec {
     cl
     libGL
     libGLU
+    libigl
+    eigen
   ];
 
   ERL_LIBS = "${cl}/lib/erlang/lib";
 
   # I did not test the *cl* part. I added the -pa just by imitation.
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin $out/lib/wings-${version}/ebin
     cp ebin/* $out/lib/wings-${version}/ebin
     cp -R textures shaders plugins $out/lib/wings-${version}
@@ -56,15 +65,17 @@ stdenv.mkDerivation rec {
     ${erlang}/bin/erl \
       -pa $out/lib/wings-${version}/ebin -run wings_start start_halt "$@"
     EOF
+
     chmod +x $out/bin/wings
+    runHook postInstall
   '';
 
   meta = {
     homepage = "http://www.wings3d.com/";
     description = "Subdivision modeler inspired by Nendo and Mirai from Izware";
     license = lib.licenses.tcltk;
-    maintainers = [ ];
-    platforms = with lib.platforms; linux;
+    maintainers = with lib.maintainers; [ NotAShelf ];
+    platforms = lib.platforms.linux;
     mainProgram = "wings";
   };
 }
