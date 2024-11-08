98b7a76dc23ffdf9fea9684bf9d2f23dcb10188b
wxt <3264117476@qq.com>
2024-11-08 12:28:51 +0800

    livepeer: 0.5.20 -> 0.8.0

--- pkgs/by-name/li/livepeer/package.nix
+++ pkgs/by-name/li/livepeer/package.nix
@@ -3,31 +3,39 @@
   fetchFromGitHub,
   buildGoModule,
   pkg-config,
-  ffmpeg,
+  ffmpeg-livepeer,
   gnutls,
   nix-update-script,
 }:
 
 buildGoModule rec {
   pname = "livepeer";
-  version = "0.5.20";
+  version = "0.8.0";
 
   proxyVendor = true;
-  vendorHash = "sha256-aRZoAEnRai8i5H08ReW8lEFlbmarYxU0lBRhR/Llw+M=";
+  vendorHash = "sha256-FCTdPVa10/DUYYuZDLtZsrCXCRoDRfuvnkzhmHJNvrk=";
 
   src = fetchFromGitHub {
     owner = "livepeer";
     repo = "go-livepeer";
     rev = "refs/tags/v${version}";
-    hash = "sha256-cOxIL093Mi+g9Al/SQJ6vdaeBAXUN6ZGsSaVvEIiJpU=";
+    hash = "sha256-UVL5y8z62pHi0mLueIp+iBxtzGf57LpGh+Czwg2pV0Q=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+  ];
 
   buildInputs = [
-    ffmpeg
+    ffmpeg-livepeer
     gnutls
   ];
+
+  __darwinAllowLocalNetworking = true;
+
+  postPatch = ''
+    rm -rf test/e2e # Require docker
+  '';
 
   passthru.updateScript = nix-update-script { };
 
