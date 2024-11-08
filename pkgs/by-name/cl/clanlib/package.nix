6d0d288cccb1be3aba5c7b05c971663a6306b70b
wxt <3264117476@qq.com>
2024-11-08 20:51:18 +0800

    clanlib: add passthru.updateScript

--- pkgs/by-name/cl/clanlib/package.nix
+++ pkgs/by-name/cl/clanlib/package.nix
@@ -12,26 +12,19 @@
   fontconfig,
   alsa-lib,
   libXrender,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "clanlib";
   version = "4.1.0";
 
   src = fetchFromGitHub {
     repo = "ClanLib";
     owner = "sphair";
-    rev = "v${version}";
-    sha256 = "sha256-SVsLWcTP+PCIGDWLkadMpJPj4coLK9dJrW4sc2+HotE=";
+    rev = "refs/tags/v${finalAttrs.version}";
+    hash = "sha256-SVsLWcTP+PCIGDWLkadMpJPj4coLK9dJrW4sc2+HotE=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "clanlib-add-support-for-riscv.patch";
-      url = "https://github.com/sphair/ClanLib/commit/f5f694205054b66dc800135c9b01673f69a7a671.patch";
-      hash = "sha256-/1XLFaTZDQAlT2mG9P6SJzXtTg7IWYGQ18Sx0e9zh0s=";
-    })
-  ];
 
   nativeBuildInputs = [
     pkg-config
@@ -48,6 +41,8 @@ stdenv.mkDerivation rec {
     libXrender
   ];
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     homepage = "https://github.com/sphair/ClanLib";
     description = "Cross platform toolkit library with a primary focus on game creation";
@@ -55,4 +50,4 @@ stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ nixinator ];
     platforms = with lib.platforms; lib.intersectLists linux (x86 ++ arm ++ aarch64 ++ riscv);
   };
-}
+})
