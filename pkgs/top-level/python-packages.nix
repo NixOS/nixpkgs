58b254d97f029d9dcd1f946414542c6504d75687
dish <pyrox@pyrox.dev>
2024-11-09 12:40:43 -0500

    python3Packages.django-huey-monitor: init at 0.9.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3525,6 +3525,8 @@ self: super: with self; {
 
   django-huey = callPackage ../development/python-modules/django-huey { };
 
+  django-huey-monitor = callPackage ../development/python-modules/django-huey-monitor { };
+
   django-i18nfield = callPackage ../development/python-modules/django-i18nfield { };
 
   django-import-export = callPackage ../development/python-modules/django-import-export { };
