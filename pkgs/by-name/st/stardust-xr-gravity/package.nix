10424d6209e480916ff61947209f0665014fa936
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-09 14:27:50 +0000

    stardust-xr-gravity: 0-unstable-2024-08-20 -> 0-unstable-2024-08-21

--- pkgs/by-name/st/stardust-xr-gravity/package.nix
+++ pkgs/by-name/st/stardust-xr-gravity/package.nix
@@ -7,7 +7,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "stardust-xr-gravity";
-  version = "0-unstable-2024-08-20";
+  version = "0-unstable-2024-08-21";
 
   src = fetchFromGitHub {
     owner = "stardustxr";
@@ -19,8 +19,8 @@ rustPlatform.buildRustPackage rec {
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "stardust-xr-0.45.0" = "sha256-1Bor53L+Fe18SU6MKwPLQXDGZq6E9++gtwDy4zkzZXw=";
       "color-rs-0.8.0" = "sha256-/p4wYiLryY0+h0HBJUo4OV2jdZpcVn2kqv+8XewM4gM=";
+      "stardust-xr-0.45.0" = "sha256-1Bor53L+Fe18SU6MKwPLQXDGZq6E9++gtwDy4zkzZXw=";
     };
   };
 
