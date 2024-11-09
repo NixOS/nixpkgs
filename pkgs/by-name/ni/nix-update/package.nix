df11922a6da8b4e8879d1ccf7f7fc78909b8bde0
patka <patka@patka.dev>
2024-11-09 09:22:04 +0100

    nix-update: 1.5.2 -> 1.6.0

--- pkgs/by-name/ni/nix-update/package.nix
+++ pkgs/by-name/ni/nix-update/package.nix
@@ -11,14 +11,14 @@
 let
   self = python3Packages.buildPythonApplication {
     pname = "nix-update";
-    version = "1.5.2";
+    version = "1.6.0";
     pyproject = true;
 
     src = fetchFromGitHub {
       owner = "Mic92";
       repo = "nix-update";
       rev = "refs/tags/${self.version}";
-      hash = "sha256-6kR4UEBZvbQNoR3l8/It5ZTCC+mB14jzj7MNnFoQJwE=";
+      hash = "sha256-CpvLZ5cXNkIbAZjO1HebFaOiLyeesnkQxJ95V0anaII=";
     };
 
     build-system = [ python3Packages.setuptools ];
