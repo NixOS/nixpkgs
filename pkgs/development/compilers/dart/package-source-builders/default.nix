da4bf61402cd30013f586bb51f4189179a625bf9
aucub <78630225+aucub@users.noreply.github.com>
2024-11-10 05:35:55 +0800

    dart.fvp: init

--- pkgs/development/compilers/dart/package-source-builders/default.nix
+++ pkgs/development/compilers/dart/package-source-builders/default.nix
@@ -2,6 +2,7 @@
 
 {
   flutter_secure_storage_linux = callPackage ./flutter-secure-storage-linux { };
+  fvp = callPackage ./fvp { };
   handy_window = callPackage ./handy-window { };
   matrix = callPackage ./matrix { };
   media_kit_libs_linux = callPackage ./media_kit_libs_linux { };
