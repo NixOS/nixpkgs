4d089cffa9252954bb66efb72013307b46eff2e2
Yongun Seong <nevivurn@nevi.dev>
2024-11-09 12:09:10 +0900

    kubelogin-oidc: 1.30.1 -> 1.31.0

--- pkgs/by-name/ku/kubelogin-oidc/package.nix
+++ pkgs/by-name/ku/kubelogin-oidc/package.nix
@@ -6,13 +6,13 @@
 
 buildGo123Module rec {
   pname = "kubelogin";
-  version = "1.30.1";
+  version = "1.31.0";
 
   src = fetchFromGitHub {
     owner = "int128";
     repo = "kubelogin";
     rev = "v${version}";
-    hash = "sha256-aoLPT3lX+q426QlxAPsjeQyTZMnmqMGh85jJPU7lQVU=";
+    hash = "sha256-IEKP5mHxJyGzlxNL4h0MR/Ge7g1kKidIjinYr+bL6as=";
   };
 
   subPackages = [ "." ];
@@ -22,7 +22,7 @@ buildGo123Module rec {
     "-X main.version=v${version}"
   ];
 
-  vendorHash = "sha256-gr+SsC7MiLj/MZ8kca5Hcfge+7Pm4y963TfwyKHEhBY=";
+  vendorHash = "sha256-3hNc16zpazm9YqDx2M/RafCBLev2S8TzJzClGRkKydg=";
 
   # test all packages
   preCheck = ''
