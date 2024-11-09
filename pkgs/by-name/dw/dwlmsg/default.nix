dce27ba0f23b0e0cea1133bc058926caeab6275e
Person1873 <87143469+Person1873@users.noreply.github.com>
2024-11-09 19:02:20 +1100

    Update default.nix
    Apply nixfmt

--- pkgs/by-name/dw/dwlmsg/default.nix
+++ pkgs/by-name/dw/dwlmsg/default.nix
@@ -2,7 +2,10 @@ let
 
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
 
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
 
 in
 
