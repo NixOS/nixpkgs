cddebdcedc78fc5da339d8d30602eb8e256a8f22
Moraxyc <i@qaq.li>
2024-11-09 16:08:48 +0800

    python312Packages.python-axolotl-curve25519: fix build on darwin

--- pkgs/development/python-modules/python-axolotl-curve25519/default.nix
+++ pkgs/development/python-modules/python-axolotl-curve25519/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  fetchpatch,
 }:
 
 buildPythonPackage rec {
@@ -13,6 +14,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "0705a66297ebd2f508a60dc94e22881c754301eb81db93963322f6b3bdcb63a3";
   };
+
+  patches = [
+    # https://github.com/tgalal/python-axolotl-curve25519/pull/26
+    (fetchpatch {
+      url = "https://github.com/tgalal/python-axolotl-curve25519/commit/901f4fb12e1290b72fbd26ea1f40755b079fa241.patch";
+      hash = "sha256-hdhaOysRXI9q5D9e/bfy0887bpEFSvUyrbl32nBgteQ=";
+    })
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/tgalal/python-axolotl-curve25519";
