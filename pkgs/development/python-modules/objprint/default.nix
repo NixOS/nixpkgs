cb6f35a464d15a912600a5c80b27caa313a3294b
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-09 06:28:16 +0000

    python312Packages.objprint: 0.2.3 -> 0.3.0

--- pkgs/development/python-modules/objprint/default.nix
+++ pkgs/development/python-modules/objprint/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "objprint";
-  version = "0.2.3";
+  version = "0.3.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "gaogaotiantian";
     repo = "objprint";
-    rev = version;
-    hash = "sha256-IGYjDdi3JzYk53ITVOhVnm9EDsa+4HXSVtVUE3wQWTo=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-+OS034bikrKy4F27b6ic97fHTW6rSMxQ0dx4caF6cUM=";
   };
 
   build-system = [
