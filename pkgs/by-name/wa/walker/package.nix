da28ad5895735cb2ef35f29b1d1d54d7ffba794f
Brandon <bb.fifield@gmail.com>
2024-11-09 20:02:44 -0330

    walker: 0.7.7 -> 0.8.7
    Vendor hash had changed, which prevented walker from being updated.

--- pkgs/by-name/wa/walker/package.nix
+++ pkgs/by-name/wa/walker/package.nix
@@ -12,16 +12,16 @@
 
 buildGoModule rec {
   pname = "walker";
-  version = "0.7.7";
+  version = "0.8.7";
 
   src = fetchFromGitHub {
     owner = "abenz1267";
     repo = "walker";
     rev = "v${version}";
-    hash = "sha256-UJxLVnKfMLPkzec3XwNHcHE1x5kPFMoyqed0VNgFJ4w=";
+    hash = "sha256-elnF3WG8sga3QLpUAPjyrYSL1pYqiFfDoLFEjbWDQ/A=";
   };
 
-  vendorHash = "sha256-xLhpHrggOGq5VLjQO7OvH/Ei5YivJJhTsy2ek2AudRs=";
+  vendorHash = "sha256-nc/WKBhUxhs1aNUg/GM7vhrKd7FrUdl2uKp7MX2VCdE=";
   subPackages = [ "cmd/walker.go" ];
 
   passthru.updateScript = nix-update-script { };
