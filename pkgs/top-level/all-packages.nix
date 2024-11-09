9ba75eb753b55910ccd974bd3e7e61f00eda493f
Theodore Ni <3806110+tjni@users.noreply.github.com>
2024-11-09 01:50:56 -0800

    mysql-shell-innovation: add libutil on darwin; refactor to new SDK pattern

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -549,8 +549,6 @@ with pkgs;
   ;
 
   mysql-shell-innovation = callPackage ../development/tools/mysql-shell/innovation.nix {
-    inherit (darwin) DarwinTools;
-    inherit (darwin.apple_sdk.frameworks) CoreServices;
     antlr = antlr4_10;
     icu =  icu73;
     protobuf = protobuf_24;
