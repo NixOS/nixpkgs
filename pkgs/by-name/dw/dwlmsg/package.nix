d6b3d4d3147e9c9e77aec3aec5062ca09dd8647d
Person1873 <87143469+Person1873@users.noreply.github.com>
2024-11-09 18:23:07 +1100

    Create package.nix

---
+++ pkgs/by-name/dw/dwlmsg/package.nix
@@ -0,0 +1,44 @@
+{
+  fetchFromGitea,
+  pkg-config,
+  stdenv,
+  wayland,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "dwlmsg";
+  version = "git";
+
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "notchoc";
+    repo = "dwlmsg";
+    rev = "3fc9780f4e";
+    hash = "sha256-LTrWb9IxGvm9yUy47+Om9V5GbLygDcEAbVAExGWgFQw=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs =
+    [
+      wayland
+    ];
+
+  outputs = [
+    "out"
+  ];
+
+  makeFlags =
+    [
+      "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
+      "PREFIX=$(out)"
+    ];
+
+  strictDeps = true;
+
+  # required for whitespaces in makeFlags
+  __structuredAttrs = true;
+
+})
