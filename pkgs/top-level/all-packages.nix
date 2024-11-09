11b2bc73cb943f14898bf3c8cdb426e09cbbb605
Xinglu Chen <public@yoctocell.xyz>
2024-11-09 10:48:30 +0100

    Revert "protobuf3_20: remove"
    This reverts commit 2cc1c9efd81ef0411bf00f932773a40c36c07504.
    
    Needed for packaging python3Packages.lark-oapi.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10161,6 +10161,9 @@ with pkgs;
       protobuf_21 = callPackage ../development/libraries/protobuf/21.nix {
         abseil-cpp = abseil-cpp_202103;
       };
+      protobuf3_20 = callPackage ../development/libraries/protobuf/3.20.nix {
+        abseil-cpp = abseil-cpp_202103;
+      };
     })
     protobuf_29
     protobuf_28
@@ -10170,6 +10173,7 @@ with pkgs;
     protobuf_24
     protobuf_23
     protobuf_21
+    protobuf3_20
     ;
 
   flatbuffers = callPackage ../development/libraries/flatbuffers { };
