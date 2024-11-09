ac839047d981ebf9237699fbe71b90f41a4d16ce
Person1873 <87143469+Person1873@users.noreply.github.com>
2024-11-09 18:18:15 +1100

    Create default.nix

---
+++ pkgs/by-name/dw/dwlmsg/default.nix
@@ -0,0 +1,13 @@
+let
+
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+in
+
+{
+
+  dwlmsg = pkgs.callPackage ./package.nix { };
+
+}
