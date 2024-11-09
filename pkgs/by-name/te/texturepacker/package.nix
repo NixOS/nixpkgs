46c4c6b55337643b0a8eedf09d8282a98430cb80
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-11-09 22:53:33 +0100

    texturepacker: 7.4.0 -> 7.5.0

--- pkgs/by-name/te/texturepacker/package.nix
+++ pkgs/by-name/te/texturepacker/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "texturepacker";
-  version = "7.4.0";
+  version = "7.5.0";
 
   src = fetchurl {
     url = "https://www.codeandweb.com/download/texturepacker/${finalAttrs.version}/TexturePacker-${finalAttrs.version}.deb";
-    hash = "sha256-v+azjIIscmp72WB3gki0CKb+z+FYsuJxIx9jvdfs+qM=";
+    hash = "sha256-zUT9NnBNtgFqNr7e9IAqWuK61MjrQuC+gCi1D2m1kGc=";
   };
 
   nativeBuildInputs = [
