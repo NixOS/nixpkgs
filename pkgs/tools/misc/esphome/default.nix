6993aac440f0cbd0163891806bad9734f0223c68
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-11-10 00:32:15 +0100

    esphome: 2024.10.2 -> 2024.10.3
    https://github.com/esphome/esphome/releases/tag/2024.10.3

--- pkgs/tools/misc/esphome/default.nix
+++ pkgs/tools/misc/esphome/default.nix
@@ -21,14 +21,14 @@ let
 in
 python.pkgs.buildPythonApplication rec {
   pname = "esphome";
-  version = "2024.10.2";
+  version = "2024.10.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-WEsFgmwH6OGkAn1c0h/HBhBJr2329YHSKMZzjEDTKNg=";
+    hash = "sha256-13hNX9uaQbO/IKUkGaOITKh+REqUCHirbTPRgomzHBU=";
   };
 
   build-systems = with python.pkgs; [
