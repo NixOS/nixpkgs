44f3b00ac2f540b4c86b225f750faa20103b8c8c
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-09 11:30:09 +0100

    python311Packages.angr: 9.2.126 -> 9.2.127
    Diff: https://github.com/angr/angr/compare/refs/tags/v9.2.126...v9.2.127

--- pkgs/development/python-modules/angr/default.nix
+++ pkgs/development/python-modules/angr/default.nix
@@ -36,7 +36,7 @@
 
 buildPythonPackage rec {
   pname = "angr";
-  version = "9.2.126";
+  version = "9.2.127";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -45,7 +45,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "angr";
     rev = "refs/tags/v${version}";
-    hash = "sha256-M74RNmjld7nuybotPjNZ1bYMDlYnOn1c/clS2ZlMe/o=";
+    hash = "sha256-hGKxLgXOL+PhhYxxJJQIMcEmFjG4WC9PQE4yP+/QuBg=";
   };
 
   postPatch = ''
