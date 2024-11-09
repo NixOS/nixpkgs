bbdbe60e4941f6a63a0f1a972b9dc69356043e6b
wxt <3264117476@qq.com>
2024-11-09 12:35:06 +0800

    methane: add passthru.updateScript
    (cherry picked from commit 1bdfbcd3e849900cb2b266f0e052a5c7ff65c24b)

--- pkgs/by-name/me/methane/package.nix
+++ pkgs/by-name/me/methane/package.nix
@@ -18,23 +18,25 @@
   libXrender,
   libmikmod,
   alsa-lib,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs:{
   pname = "methane";
   version = "2.0.1";
 
   src = fetchFromGitHub {
     repo = "methane";
     owner = "rombust";
-    rev = "v${version}";
-    sha256 = "sha256-STS2+wfZ8E1jpr0PYQOBQsztxhJU0Dt3IhWBE3sjdWE=";
+    rev = "refs/tags/v${finalAttrs.version}";
+    hash = "sha256-STS2+wfZ8E1jpr0PYQOBQsztxhJU0Dt3IhWBE3sjdWE=";
   };
 
   nativeBuildInputs = [
     gettext
     pkg-config
   ];
+
   buildInputs = [
     SDL2
     SDL2_image
@@ -61,12 +63,14 @@ stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
-  meta = with lib; {
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
     homepage = "https://github.com/rombust/methane";
     description = "Clone of Taito's Bubble Bobble arcade game released for Amiga in 1993 by Apache Software";
     mainProgram = "methane";
-    license = licenses.gpl2Only;
-    maintainers = with maintainers; [ nixinator ];
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [ nixinator ];
     platforms = [ "x86_64-linux" ];
   };
-}
+})
