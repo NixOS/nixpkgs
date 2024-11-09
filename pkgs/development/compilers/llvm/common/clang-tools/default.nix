bb4b99af963209ac6152f7bac80a29a3b368a1b0
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-11-09 13:13:17 +0100

    clang-tools: Improve wrapper script, add tests
    clang-tools invokes clangd through a wrapper script whose sole purpose
    is to extend `CPATH` and `CPLUS_INCLUDE_PATH` envs, so that clangd is
    later able to find built-in and stdlib headers.
    
    This script does two things wrong:
    
    1. We extend `CPATH` et al with entries from both
    `@clang@/nix-support/libc-cflags` and `@clang@/resource-root/include`,
    which is kind of a self-made problem, since clangd already has some
    header auto-detection logic which we (used to) circumvent (for more
    details, see the diff).
    
    2. We extend `CPATH` et al unconditionally, breaking the
    `--query-driver` option. I'm not sure what's the perfect solution here,
    but skipping this logic when we detect `--query-driver` should be good
    enough. I mean, ideally we wouldn't be playing with `CPATH` etc.
    whatsoever, but well.
    
    Closes https://github.com/NixOS/nixpkgs/issues/351962
    Closes https://github.com/NixOS/nixpkgs/issues/348791
    Closes https://github.com/NixOS/nixpkgs/issues/345704

--- pkgs/development/compilers/llvm/common/clang-tools/default.nix
+++ pkgs/development/compilers/llvm/common/clang-tools/default.nix
@@ -1,6 +1,9 @@
 {
   lib,
   stdenv,
+  runCommand,
+  writeText,
+  linkFarm,
   clang-unwrapped,
   clang,
   libcxxClang,
@@ -10,50 +13,99 @@
   enableLibcxx ? false,
 }:
 
-stdenv.mkDerivation {
-  unwrapped = clang-unwrapped;
+let
+  clang-tools = stdenv.mkDerivation {
+    # clang-unwrapped extracts headers outside the main derivation, into
+    # clang-unwrapped.lib - this is at odds with clangd's header auto-detection
+    # logic, which assumes it'll have them at <prefix>/lib.
+    #
+    # For simplicity, instead of working around this with the `CPATH` and
+    # `CPLUS_INCLUDE_PATH` envs, let's just reconstruct the directory structure
+    # expected by clangd - this avoids extra logic within the wrapper script.
+    unwrapped = runCommand "clang-unwrapped-with-lib" { } ''
+      mkdir $out
+      mkdir $out/bin
 
-  pname = "clang-tools";
-  version = lib.getVersion clang-unwrapped;
-  dontUnpack = true;
-  clang = if enableLibcxx then libcxxClang else clang;
+      cp -a ${clang-unwrapped}/bin/clangd $out/bin/clangd
+      ln -s ${clang-unwrapped.lib}/lib $out/lib
+    '';
 
-  installPhase = ''
-    runHook preInstall
+    pname = "clang-tools";
+    version = lib.getVersion clang-unwrapped;
+    dontUnpack = true;
+    clang = if enableLibcxx then libcxxClang else clang;
 
-    mkdir -p $out/bin
+    installPhase = ''
+      runHook preInstall
 
-    for tool in $unwrapped/bin/clang-*; do
-      tool=$(basename "$tool")
+      mkdir -p $out/bin
 
-      # Compilers have their own derivation, no need to include them here:
-      if [[ $tool == "clang-cl" || $tool == "clang-cpp" ]]; then
-        continue
+      for tool in ${clang-unwrapped}/bin/clang-*; do
+        tool=$(basename "$tool")
+
+        # Compilers have their own derivation, no need to include them here:
+        if [[ $tool == "clang-cl" || $tool == "clang-cpp" ]]; then
+          continue
+        fi
+
+        # Clang's derivation produces a lot of binaries, but the tools we are
+        # interested in follow the `clang-something` naming convention - except
+        # for clang-$version (e.g. clang-13), which is the compiler again:
+        if [[ ! $tool =~ ^clang\-[a-zA-Z_\-]+$ ]]; then
+          continue
+        fi
+
+        ln -s $out/bin/clangd $out/bin/$tool
+      done
+
+      if [[ -z "$(ls -A $out/bin)" ]]; then
+        echo "Found no binaries - maybe their location or naming convention changed?"
+        exit 1
       fi
 
-      # Clang's derivation produces a lot of binaries, but the tools we are
-      # interested in follow the `clang-something` naming convention - except
-      # for clang-$version (e.g. clang-13), which is the compiler again:
-      if [[ ! $tool =~ ^clang\-[a-zA-Z_\-]+$ ]]; then
-        continue
-      fi
+      substituteAll ${./wrapper} $out/bin/clangd
+      chmod +x $out/bin/clangd
 
-      ln -s $out/bin/clangd $out/bin/$tool
-    done
+      runHook postInstall
+    '';
 
-    if [[ -z "$(ls -A $out/bin)" ]]; then
-      echo "Found no binaries - maybe their location or naming convention changed?"
-      exit 1
-    fi
+    passthru.tests.smokeOk =
+      let
+        src = writeText "main.cpp" ''
+          #include <iostream>
 
-    substituteAll ${./wrapper} $out/bin/clangd
-    chmod +x $out/bin/clangd
+          int main() {
+            std::cout << "Hi!";
+          }
+        '';
 
-    runHook postInstall
-  '';
+      in
+      runCommand "clang-tools-test-smoke-ok" { } ''
+        ${clang-tools}/bin/clangd --check=${src}
+        touch $out
+      '';
 
-  meta = llvm_meta // {
-    description = "Standalone command line tools for C++ development";
-    maintainers = with lib.maintainers; [ patryk27 ];
+    passthru.tests.smokeErr =
+      let
+        src = writeText "main.cpp" ''
+          int main() {
+             std::cout << "Hi!";
+          }
+        '';
+
+      in
+      runCommand "clang-tools-test-smoke-err" { } ''
+        (${clang-tools}/bin/clangd --check=${src} 2>&1 || true) \
+            | grep 'use of undeclared identifier'
+
+        touch $out
+      '';
+
+    meta = llvm_meta // {
+      description = "Standalone command line tools for C++ development";
+      maintainers = with lib.maintainers; [ patryk27 ];
+    };
   };
-}
+
+in
+clang-tools
