58f36628e906020dadd40f81d9c6ed44d15f1f9f
Robert Schütz <mail@dotlambda.de>
2024-11-09 19:02:52 -0800

    prs: 0.5.1 -> 0.5.2
    Diff: https://gitlab.com/timvisee/prs/-/compare/refs/tags/v0.5.1...v0.5.2
    
    Changelog: https://gitlab.com/timvisee/prs/-/blob/v0.5.2/CHANGELOG.md

--- pkgs/by-name/pr/prs/package.nix
+++ pkgs/by-name/pr/prs/package.nix
@@ -1,48 +1,45 @@
-{ lib
-, rustPlatform
-, fetchFromGitLab
-, installShellFiles
-, pkg-config
-, python3
-, dbus
-, glib
-, gpgme
-, gtk3
-, libxcb
-, libxkbcommon
-, stdenv
+{
+  lib,
+  rustPlatform,
+  fetchFromGitLab,
+  installShellFiles,
+  pkg-config,
+  python3,
+  glib,
+  gpgme,
+  gtk3,
+  stdenv,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "prs";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchFromGitLab {
     owner = "timvisee";
     repo = "prs";
     rev = "refs/tags/v${version}";
-    hash = "sha256-MvQ0B35NF/AuGHBMa201FkFlU/UX0WXMcBRxTJwpUFw=";
+    hash = "sha256-W5wNmZWjSsM6Xe50fCpa/aGsJ8PDyh2INs1Oj86et04=";
   };
 
-  cargoHash = "sha256-YDcAjBIdUboOKvGSnGW6b1JVbhQaB3ccXcSmK78M7DI=";
+  cargoHash = "sha256-T57RqIzurpYLHyeFhvqxmC+DoB6zUf+iTu1YkMmwtp8=";
 
-  postPatch = ''
-    # The GPGME backend is recommended
-    for f in "gtk3/Cargo.toml" "cli/Cargo.toml"; do
-      substituteInPlace "$f" --replace \
-        'default = ["backend-gnupg-bin"' 'default = ["backend-gpgme"'
-    done
-  '';
+  nativeBuildInputs = [
+    gpgme
+    installShellFiles
+    pkg-config
+    python3
+  ];
 
-  nativeBuildInputs = [ gpgme installShellFiles pkg-config python3 ];
+  cargoBuildFlags = [
+    "--no-default-features"
+    "--features=alias,backend-gpgme,clipboard,notify,select-fzf-bin,select-skim-bin,tomb,totp"
+  ];
 
   buildInputs = [
-    dbus
     glib
     gpgme
     gtk3
-    libxcb
-    libxkbcommon
   ];
 
   postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
@@ -57,7 +54,7 @@ rustPlatform.buildRustPackage rec {
     changelog = "https://gitlab.com/timvisee/prs/-/blob/v${version}/CHANGELOG.md";
     license = with licenses; [
       lgpl3Only # lib
-      gpl3Only  # everything else
+      gpl3Only # everything else
     ];
     maintainers = with maintainers; [ dotlambda ];
     mainProgram = "prs";
