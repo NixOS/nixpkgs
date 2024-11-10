d0fb29ebf83e7acb7cc9c8a21980965749ecd7b7
Moritz Sanft <58110325+msanft@users.noreply.github.com>
2024-11-10 15:02:16 +0100

    python3Packages.pygreat: 2019.5.1.dev0 -> 2024.0.2

--- pkgs/development/python-modules/pygreat/default.nix
+++ pkgs/development/python-modules/pygreat/default.nix
@@ -1,22 +1,21 @@
 {
   lib,
   buildPythonPackage,
-  isPy3k,
   fetchFromGitHub,
   future,
   pyusb,
 }:
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "pygreat";
-  version = "2019.5.1.dev0";
+  version = "2024.0.2";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "greatscottgadgets";
     repo = "libgreat";
-    rev = "14c00b7c8f036f4d467e4b1a324ffa3566b126fa";
-    sha256 = "1h0z83k1k4z8j36z936h61l8j3cjr3wsxr86k91v5c5h93g9dkqh";
+    rev = "v${version}";
+    sha256 = "sha256-yYp+2y4QIOykkrObWaXbZMMc2fsRn/+tGWqySA7V534=";
   };
 
   propagatedBuildInputs = [
@@ -24,13 +23,12 @@ buildPythonPackage {
     pyusb
   ];
 
-  disabled = !isPy3k;
+  sourceRoot = "${src.name}/host";
 
-  preBuild = ''
-    cd host
-    substituteInPlace setup.py --replace "'backports.functools_lru_cache'" ""
-    substituteInPlace pygreat/comms.py --replace "from backports.functools_lru_cache import lru_cache as memoize_with_lru_cache" "from functools import lru_cache as memoize_with_lru_cache"
-    echo "$version" > ../VERSION
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace ', "setuptools-git-versioning<2"' "" \
+      --replace 'dynamic = ["version"]' 'version = "${version}"'
   '';
 
   meta = with lib; {
