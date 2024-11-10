4b239e8fff1895c70a60f5179779ea2f00156ec2
wxt <3264117476@qq.com>
2024-11-10 21:21:43 +0800

    python3Packages.globus-sdk: add bot-wxt1221 as maintainers

--- pkgs/development/python-modules/globus-sdk/default.nix
+++ pkgs/development/python-modules/globus-sdk/default.nix
@@ -44,10 +44,11 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "globus_sdk" ];
 
-  meta = with lib; {
+  meta = {
     description = "Interface to Globus REST APIs, including the Transfer API and the Globus Auth API";
     homepage = "https://github.com/globus/globus-sdk-python";
     changelog = "https://github.com/globus/globus-sdk-python/releases/tag/${version}";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ bot-wxt1221 ];
   };
 }
