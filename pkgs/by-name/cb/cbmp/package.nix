acfeff6d5439f3f23937d6d0853c23d2ed5f7ed5
Sandro <sandro.jaeckel@gmail.com>
2024-11-11 14:07:06 +0100

    Update pkgs/by-name/cb/cbmp/package.nix

--- pkgs/by-name/cb/cbmp/package.nix
+++ pkgs/by-name/cb/cbmp/package.nix
@@ -16,7 +16,7 @@ buildNpmPackage rec {
 
   src = fetchFromGitHub {
     owner = "ful1e5";
-    repo = pname;
+    repo = "cbmp";
     rev = "v${version}";
     hash = "sha256-vOEz2KGJLCiiX+Or9y0JE9UF7sYbwaSCVm5iBv4jIdI=";
   };
