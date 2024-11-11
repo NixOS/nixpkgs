4131a70db7dfc1d0eda31af08b13d01a0e2a527e
phaer <hello@phaer.org>
2024-11-11 16:12:11 +0100

    python312Packages.tinycss2: 1.3.0 -> 1.4.0

--- pkgs/development/python-modules/tinycss2/default.nix
+++ pkgs/development/python-modules/tinycss2/default.nix
@@ -10,8 +10,8 @@
 
 buildPythonPackage rec {
   pname = "tinycss2";
-  version = "1.3.0";
-  format = "pyproject";
+  version = "1.4.0";
+  pyproject = true;
 
   disabled = pythonOlder "3.8";
 
@@ -19,23 +19,14 @@ buildPythonPackage rec {
     owner = "kozea";
     repo = "tinycss2";
 
-    # Tag v1.3.0 is missing the actual version number bump.
-    rev = "bda62b101530588718d931d61bcc343a628b9af9";
+    rev = "v1.4.0";
     # for tests
     fetchSubmodules = true;
-    hash = "sha256-Exjxdm0VnnjHUKjquXsC/zDmwA7bELHdX1f55IGBjYk=";
+    hash = "sha256-GVymUobWAE0YS65lti9dXRIIGpx0YkwF/vSb3y7cpYY=";
   };
 
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace "'pytest-cov', 'pytest-flake8', 'pytest-isort', 'coverage[toml]'" "" \
-      --replace "--isort --flake8 --cov --no-cov-on-fail" ""
-  '';
-
-  nativeBuildInputs = [ flit-core ];
-
-  propagatedBuildInputs = [ webencodings ];
-
+  build-system = [ flit-core ];
+  dependencies = [ webencodings ];
   nativeCheckInputs = [ pytestCheckHook ];
 
   meta = with lib; {
