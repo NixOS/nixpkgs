dff192f9c010ddcc48c66d3b34df88ea922df005
K900 <me@0upti.me>
2024-11-11 07:35:57 +0300

    vaultwarden: 1.32.3 -> 1.32.4
    Diff: https://github.com/dani-garcia/vaultwarden/compare/1.32.3...1.32.4
    
    Changelog: https://github.com/dani-garcia/vaultwarden/releases/tag/1.32.4
    (cherry picked from commit 7d4246729b4466e8b8a7385815e711420c9f4b45)

--- pkgs/tools/security/vaultwarden/default.nix
+++ pkgs/tools/security/vaultwarden/default.nix
@@ -9,16 +9,16 @@ in
 
 rustPlatform.buildRustPackage rec {
   pname = "vaultwarden";
-  version = "1.32.3";
+  version = "1.32.4";
 
   src = fetchFromGitHub {
     owner = "dani-garcia";
     repo = "vaultwarden";
     rev = version;
-    hash = "sha256-dtzC5T96fBrIvy5LFDnwLfx1wF9aVGv/FuAUDH56yto=";
+    hash = "sha256-fT1o+nR7k1fLFS4TeoP1Gm1P0uLTu6Dai6hMGraAKjE=";
   };
 
-  cargoHash = "sha256-a4OwL7yD2i4uJLnSzWpwfRZRSgFFammfUOE+PHA2iw0=";
+  cargoHash = "sha256-xidkmkRVIyQ3mutkmR3oObEwi1uRLCiRSyF5GpWVM9w=";
 
   # used for "Server Installed" version in admin panel
   env.VW_VERSION = version;
