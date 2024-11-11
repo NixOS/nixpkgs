c6845aaa75e2cc50adce6266d7760856dd5d8861
Federico Beffa <beffa@fbengineering.ch>
2024-11-11 15:06:10 +0100

    python3Packages.rectangle-packer: init at 2.0.2

---
+++ pkgs/development/python-modules/rectangle-packer/default.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  cython,
+}:
+
+buildPythonPackage rec {
+  pname = "rectangle-packer";
+  version = "2.0.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Penlect";
+    repo = "rectangle-packer";
+    rev = version;
+    hash = "sha256-YsMLB9jfAC5yB8TnlY9j6ybXM2ILireOgQ8m8wYo4ts=";
+  };
+
+  build-system = [
+    cython
+    setuptools
+  ];
+
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace-fail 'Cython<3.0.0' 'Cython'
+  '';
+
+  pythonImportsCheck = [ "rpack" ];
+
+  meta = with lib; {
+    description = "Pack a set of rectangles into a bounding box with minimum area";
+    homepage = "https://github.com/Penlect/rectangle-packer";
+    license = licenses.mit;
+    maintainers = with maintainers; [ fbeffa ];
+  };
+}
