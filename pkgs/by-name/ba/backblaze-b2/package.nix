6c232a0de15e809a76c90631a2630e093f3757c9
Philip White <philip@0xfff.me>
2024-11-10 21:11:13 -0800

    backblaze-b2: 4.0.1 -> 4.2.0
    Also run `nixfmt` and add myself as a maintainer.

--- pkgs/by-name/ba/backblaze-b2/package.nix
+++ pkgs/by-name/ba/backblaze-b2/package.nix
@@ -1,23 +1,24 @@
-{ lib
-, python3Packages
-, fetchFromGitHub
-, installShellFiles
-, testers
-, backblaze-b2
-# executable is renamed to backblaze-b2 by default, to avoid collision with boost's 'b2'
-, execName ? "backblaze-b2"
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+  installShellFiles,
+  testers,
+  backblaze-b2,
+  # executable is renamed to backblaze-b2 by default, to avoid collision with boost's 'b2'
+  execName ? "backblaze-b2",
 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "backblaze-b2";
-  version = "4.0.1";
+  version = "4.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Backblaze";
     repo = "B2_Command_Line_Tool";
     rev = "refs/tags/v${version}";
-    hash = "sha256-rZUWPSI7CrKOdEKdsSpekwBerbIMf2iiVrWkV8WrqSc=";
+    hash = "sha256-a0XJq8M1yw4GmD5ndIAJtmHFKqS0rYdvYIxK7t7oyZw=";
   };
 
   nativeBuildInputs = with python3Packages; [
@@ -39,8 +40,6 @@ python3Packages.buildPythonApplication rec {
     tabulate
     tqdm
     platformdirs
-    packaging
-    setuptools
   ];
 
   nativeCheckInputs = with python3Packages; [
@@ -50,10 +49,6 @@ python3Packages.buildPythonApplication rec {
     pytestCheckHook
     pytest-xdist
   ];
-
-  preCheck = ''
-    export HOME=$(mktemp -d)
-  '';
 
   disabledTestPaths = [
     # Test requires network
@@ -73,32 +68,39 @@ python3Packages.buildPythonApplication rec {
     "test_install_autocomplete"
   ];
 
-  postInstall = lib.optionalString (execName != "b2") ''
-    mv "$out/bin/b2" "$out/bin/${execName}"
-  ''
-  + ''
-    installShellCompletion --cmd ${execName} \
-      --bash <(register-python-argcomplete ${execName}) \
-      --zsh <(register-python-argcomplete ${execName})
-  '';
-
-  passthru.tests.version = (testers.testVersion {
-    package = backblaze-b2;
-    command = "${execName} version --short";
-  }).overrideAttrs (old: {
-    # workaround the error: Permission denied: '/homeless-shelter'
-    # backblaze-b2 fails to create a 'b2' directory under the XDG config path
-    preHook = ''
-      export HOME=$(mktemp -d)
+  postInstall =
+    lib.optionalString (execName != "b2") ''
+      mv "$out/bin/b2" "$out/bin/${execName}"
+    ''
+    + ''
+      installShellCompletion --cmd ${execName} \
+        --bash <(register-python-argcomplete ${execName}) \
+        --zsh <(register-python-argcomplete ${execName})
     '';
-  });
+
+  passthru.tests.version =
+    (testers.testVersion {
+      package = backblaze-b2;
+      command = "${execName} version --short";
+    }).overrideAttrs
+      (old: {
+        # workaround the error: Permission denied: '/homeless-shelter'
+        # backblaze-b2 fails to create a 'b2' directory under the XDG config path
+        preHook = ''
+          export HOME=$(mktemp -d)
+        '';
+      });
 
   meta = with lib; {
     description = "Command-line tool for accessing the Backblaze B2 storage service";
     homepage = "https://github.com/Backblaze/B2_Command_Line_Tool";
     changelog = "https://github.com/Backblaze/B2_Command_Line_Tool/blob/v${version}/CHANGELOG.md";
     license = licenses.mit;
-    maintainers = with maintainers; [ hrdinka tomhoule ];
+    maintainers = with maintainers; [
+      hrdinka
+      pmw
+      tomhoule
+    ];
     mainProgram = "backblaze-b2";
   };
 }
