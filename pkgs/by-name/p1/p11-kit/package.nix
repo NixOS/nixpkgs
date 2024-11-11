NixOS/nixpkgs

commit 34f5abab7a2e7b02d4ca67bf4ea1b0979f061dcf

p11-kit: conditionalize check flags for now

pkgs/by-name/p1/p11-kit/package.nix
@@ -60,7 +60,7 @@ stdenv.mkDerivation rec {
     ]))
   ];
 
-  mesonCheckFlags = [
+  mesonCheckFlags = lib.optionals (stdenv.buildPlatform.isDarwin && stdenv.buildPlatform.isx86_64) [
     # Tests regularly exceed the default timeout on `x86_64-darwin`.
     "--timeout-multiplier=0"
   ];

commit 2cb56f387af6364695f4d5b2dd779bcd3849d89b

p11-kit: fix check flag conditionalization
Fixes: 34f5abab7a2e7b02d4ca67bf4ea1b0979f061dcf

pkgs/by-name/p1/p11-kit/package.nix
@@ -60,7 +60,7 @@ stdenv.mkDerivation rec {
     ]))
   ];
 
-  mesonCheckFlags = lib.optionals (stdenv.buildPlatform.isDarwin && stdenv.buildPlatform.isx86_64) [
+  ${if stdenv.buildPlatform.isDarwin && stdenv.buildPlatform.isx86_64 then "mesonCheckFlags" else null} = [
     # Tests regularly exceed the default timeout on `x86_64-darwin`.
     "--timeout-multiplier=0"
   ];