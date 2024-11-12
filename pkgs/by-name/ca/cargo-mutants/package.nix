03ea16610a5606c65a276466601cf458d5cb15d1
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-12 06:43:43 +0000

    cargo-mutants: 24.9.0 -> 24.11.0

--- pkgs/by-name/ca/cargo-mutants/package.nix
+++ pkgs/by-name/ca/cargo-mutants/package.nix
@@ -1,26 +1,21 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-, stdenv
-, darwin
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-mutants";
-  version = "24.9.0";
+  version = "24.11.0";
 
   src = fetchFromGitHub {
     owner = "sourcefrog";
     repo = "cargo-mutants";
     rev = "v${version}";
-    hash = "sha256-fMw3Whyl+zTPpDTdYpwvzMQtSdr42ueEvkdmRI0N2aA=";
+    hash = "sha256-A4DlHJKKopReKVF14w3c4wUXJe/vYUzbwMswuvMGs/E=";
   };
 
-  cargoHash = "sha256-+vI/HPw0oe9K0kWpJXGBM0r7oVBh3+RJzSwklaywa54=";
-
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    darwin.apple_sdk.frameworks.SystemConfiguration
-  ];
+  cargoHash = "sha256-QyQrLkLDvnytojf92P+INYlvfBc+t3240O2cx36CzTU=";
 
   # too many tests require internet access
   doCheck = false;
