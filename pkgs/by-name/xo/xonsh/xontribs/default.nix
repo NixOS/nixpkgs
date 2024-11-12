7fe076110f502e6944e4c483e9e893eb25967f26
Infinidoge <infinidoge@inx.moe>
2024-11-12 09:31:40 -0500

    xonsh.xontribs.xontrib-debug-tools: init at 0.0.1

--- pkgs/by-name/xo/xonsh/xontribs/default.nix
+++ pkgs/by-name/xo/xonsh/xontribs/default.nix
@@ -8,4 +8,5 @@ in
   xonsh-direnv = callPackage ./xonsh-direnv { };
   xontrib-abbrevs = callPackage ./xontrib-abbrevs { };
   xontrib-bashisms = callPackage ./xontrib-bashisms { };
+  xontrib-debug-tools = callPackage ./xontrib-debug-tools { };
 }
