25e2203eaa0b3cec131b5663732b332931dd60a0
Emily <vcs@emily.moe>
2024-11-12 15:32:09 +0000

    torrenttools: patch for `fmt_9`
    It’d take more work to get this working with the latest version,
    and this project seems abandoned, so I didn’t bother for now.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16048,7 +16048,7 @@ with pkgs;
   };
 
   torrenttools = callPackage ../tools/misc/torrenttools {
-    fmt = fmt_8;
+    fmt = fmt_9;
   };
 
   tony = libsForQt5.callPackage ../applications/audio/tony { };
