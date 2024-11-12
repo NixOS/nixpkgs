e4ab3bf4d3a80f261b2b63ff21354176525454ff
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-11-12 21:56:12 +0000

    xen: 4.19.0 -> 4.19.0-unstable-2024-11-12
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/by-name/xe/xen/package.nix
+++ pkgs/by-name/xe/xen/package.nix
@@ -5,7 +5,7 @@
 
 buildXenPackage.override { inherit python3Packages; } {
   pname = "xen";
-  version = "4.19.0";
-  rev = "026c9fa29716b0ff0f8b7c687908e71ba29cf239";
-  hash = "sha256-Q6x+2fZ4ITBz6sKICI0NHGx773Rc919cl+wzI89UY+Q=";
+  version = "4.19.0-unstable-2024-11-12";
+  rev = "251a9496485a86f302980a3f8d3c656831b5a62f";
+  hash = "sha256-kHuB6kagH3AU+Wsx4oD7HnNsZpxCu7x3v/m4/1xi6lY=";
 }
