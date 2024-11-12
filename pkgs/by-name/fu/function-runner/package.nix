30efc7f5e7fe9b4b622710bec6e4c7507f2b2327
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-12 05:08:47 +0000

    function-runner: 6.2.1 -> 6.3.0

--- pkgs/by-name/fu/function-runner/package.nix
+++ pkgs/by-name/fu/function-runner/package.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "function-runner";
-  version = "6.2.1";
+  version = "6.3.0";
 
   src = fetchFromGitHub {
     owner = "Shopify";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-5X/d6phYXmJcCacHvGkk5o/J91SdlFamxJrqc5X/Y4Y=";
+    sha256 = "sha256-DJX9P3Dauzc8qrpvqIGgr85gwIPeYwVDyFlIVh1RIq0=";
   };
 
-  cargoHash = "sha256-D6BTP/a3wOpcOLnGUASyBL3pzAieAllLzEZuaEv2Oco=";
+  cargoHash = "sha256-rlQGAHISrLuXTsoM9RWRD3roQi/sgU6BPBlOj0ecgn4=";
 
   meta = with lib; {
     description = "CLI tool which allows you to run Wasm Functions intended for the Shopify Functions infrastructure";
