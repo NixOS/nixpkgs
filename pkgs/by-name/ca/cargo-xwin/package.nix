838e784cd4e92cea82fd34829217147ca5c3591b
Sarah Clark <seclark@nextquestion.net>
2024-11-12 15:12:13 -0800

    cargo-xwin: format rfc style

--- pkgs/by-name/ca/cargo-xwin/package.nix
+++ pkgs/by-name/ca/cargo-xwin/package.nix
@@ -1,8 +1,9 @@
-{ lib
-, stdenv
-, rustPlatform
-, fetchFromGitHub
-, darwin
+{
+  lib,
+  stdenv,
+  rustPlatform,
+  fetchFromGitHub,
+  darwin,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -18,9 +19,7 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-xVG1nET020rfMIjhIcCtNr9ZCj8SgQAvXePjyKSPjUs=";
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    darwin.apple_sdk.frameworks.Security
-  ];
+  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 
   meta = with lib; {
     description = "Cross compile Cargo project to Windows MSVC target with ease";
