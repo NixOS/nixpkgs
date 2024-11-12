fafa05878de04748cbb817cfbcf418bc957ac5da
techknowlogick <techknowlogick@gitea.com>
2024-11-11 20:36:01 -0500

    buildkite-agent: 3.82.1 -> 3.85.1
    Diff: https://github.com/buildkite/agent/compare/v3.82.1...v3.85.1

--- pkgs/by-name/bu/buildkite-agent/package.nix
+++ pkgs/by-name/bu/buildkite-agent/package.nix
@@ -13,16 +13,16 @@
 }:
 buildGoModule rec {
   pname = "buildkite-agent";
-  version = "3.82.1";
+  version = "3.85.1";
 
   src = fetchFromGitHub {
     owner = "buildkite";
     repo = "agent";
     rev = "v${version}";
-    hash = "sha256-xTF8zmpwEFHkLwDYBICXJZ4gjJYpcH5i76kPQM6qt5o=";
+    hash = "sha256-aRgjXzwTC1wCWZ7n0MJpNHcHZgvendFPr4vCrBnCJCk=";
   };
 
-  vendorHash = "sha256-xKAQ2yvFYl9ld3H6IGafYgCdA8jn9xig/AAej56ACns=";
+  vendorHash = "sha256-UMnDVxZgqI4430IlA8fSygKEOT86RjCwuzGsvkQ8XIo=";
 
   postPatch = ''
     substituteInPlace clicommand/agent_start.go --replace /bin/bash ${bash}/bin/bash
