383242a1016d7f75b3df3185f8af781c7291165b
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-12 17:55:00 +0000

    automatic-timezoned: 2.0.34 -> 2.0.38

--- pkgs/by-name/au/automatic-timezoned/package.nix
+++ pkgs/by-name/au/automatic-timezoned/package.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "automatic-timezoned";
-  version = "2.0.34";
+  version = "2.0.38";
 
   src = fetchFromGitHub {
     owner = "maxbrunet";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-NWVc+rPlW3GFHTADKEHjzDAHiicKd4HlJSD4MR2mqGQ=";
+    sha256 = "sha256-yGDMH0MmC+jihbKfKwQ/CEflBo+/q/OPzsi+CiiRqfE=";
   };
 
-  cargoHash = "sha256-6QPgBP8K4HvivlVxWzIX8y37ltHEScUzmf5Js8gMN34=";
+  cargoHash = "sha256-qUAst/goPhpJO26RLUPw6+q/FYHuGXaiFADOHIYmGZ8=";
 
   meta = with lib; {
     description = "Automatically update system timezone based on location";
