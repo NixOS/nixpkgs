8e3554d3e3fbe6e286c77fbbc1a19b244a6d7c78
Emily <vcs@emily.moe>
2024-11-12 03:44:30 +0000

    termbench-pro: bump to `fmt_11`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11081,7 +11081,7 @@ with pkgs;
     harfbuzz = harfbuzzFull;
   };
 
-  termbench-pro = callPackage ../development/libraries/termbench-pro { fmt = fmt_8; };
+  termbench-pro = callPackage ../development/libraries/termbench-pro { fmt = fmt_11; };
 
   texpresso = callPackage ../tools/typesetting/tex/texpresso {
     texpresso-tectonic = callPackage ../tools/typesetting/tex/texpresso/tectonic.nix { };
