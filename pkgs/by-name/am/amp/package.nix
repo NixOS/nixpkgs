164c9b1f62bb5e3815ef3ece5c691e492afea0d7
Sarah Clark <seclark@nextquestion.net>
2024-11-12 11:56:26 -0800

    amp: migrate to new apple-sdk pattern

--- pkgs/by-name/am/amp/package.nix
+++ pkgs/by-name/am/amp/package.nix
@@ -9,7 +9,6 @@
   xorg,
   cmake,
   libgit2,
-  darwin,
   curl,
 }:
 
@@ -33,20 +32,11 @@ rustPlatform.buildRustPackage rec {
     pkg-config
     python3
   ];
-  buildInputs =
-    [
-      openssl
-      xorg.libxcb
-      libgit2
-    ]
-    ++ lib.optionals stdenv.hostPlatform.isDarwin (
-      with darwin.apple_sdk.frameworks;
-      [
-        curl
-        Security
-        AppKit
-      ]
-    );
+  buildInputs = [
+    openssl
+    xorg.libxcb
+    libgit2
+  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ curl ];
 
   # Tests need to write to the theme directory in HOME.
   preCheck = "export HOME=`mktemp -d`";
