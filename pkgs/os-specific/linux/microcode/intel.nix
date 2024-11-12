1ad84cc52331c9ff06ee8e379ce28dc09fe18b8b
Felix Singer <felixsinger@posteo.net>
2024-11-12 22:45:08 +0100

    microcode-intel: 20241029 -> 20241112
    Release notes: https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files/blob/microcode-20241112/releasenote.md
    
    Signed-off-by: Felix Singer <felixsinger@posteo.net>
    (cherry picked from commit 2bdf3a8fd9e22afb375f38d3c286e18877eb6e1b)

--- pkgs/os-specific/linux/microcode/intel.nix
+++ pkgs/os-specific/linux/microcode/intel.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "microcode-intel";
-  version = "20241029";
+  version = "20241112";
 
   src = fetchFromGitHub {
     owner = "intel";
     repo = "Intel-Linux-Processor-Microcode-Data-Files";
     rev = "microcode-${version}";
-    hash = "sha256-iS7OSWjAYBRHP7X1C+DiMwX2xudA6tXX6cEv/IcICxc=";
+    hash = "sha256-dvXt4SX/90oFM4GR9R7C2udhZ+plxnabOZpbtwN3V8Q=";
   };
 
   nativeBuildInputs = [ iucode-tool libarchive ];
