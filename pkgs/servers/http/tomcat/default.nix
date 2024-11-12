f7f3af6db6e24111653be3efbd79a47f6af18a4b
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-12 20:39:00 +0000

    tomcat10: 10.1.30 -> 10.1.33

--- pkgs/servers/http/tomcat/default.nix
+++ pkgs/servers/http/tomcat/default.nix
@@ -65,8 +65,8 @@ in
   };
 
   tomcat10 = common {
-    version = "10.1.30";
-    hash = "sha256-jeWoCPPcdirOZ5SM2Q0TJ7EWgWYiBE3IdQ8EIH35Ci4=";
+    version = "10.1.33";
+    hash = "sha256-Oysh1hzVh7X4ONoYQ3Y/6qXoZP4Fl1oQHa3gki3lCJU=";
   };
 
   tomcat11 = common {
