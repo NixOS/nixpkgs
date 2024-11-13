783a860caaedbeacd073537550307fb026b636b6
Uriel <imurx@proton.me>
2024-11-13 04:15:42 +0100

    beatsabermodmanager: format using nixfmt

--- pkgs/by-name/be/beatsabermodmanager/package.nix
+++ pkgs/by-name/be/beatsabermodmanager/package.nix
@@ -21,10 +21,12 @@ buildDotnetModule {
     fetchSubmodules = true; # It vendors BSIPA-Linux
   };
 
-  dotnet-sdk = with dotnetCorePackages; combinePackages [
-    sdk_8_0
-    sdk_6_0
-  ];
+  dotnet-sdk =
+    with dotnetCorePackages;
+    combinePackages [
+      sdk_8_0
+      sdk_6_0
+    ];
 
   dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
