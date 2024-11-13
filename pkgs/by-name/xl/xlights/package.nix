2528d14a3acd2f5692a5decf3c9a69cb1e15438e
Andreas <asamonik@student.tugraz.at>
2024-11-13 12:25:43 +0100

    xlights: 2024.16 -> 2024.17
    Signed-off-by: Andreas <asamonik@student.tugraz.at>

--- pkgs/by-name/xl/xlights/package.nix
+++ pkgs/by-name/xl/xlights/package.nix
@@ -2,11 +2,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "xlights";
-  version = "2024.16";
+  version = "2024.17";
 
   src = fetchurl {
     url = "https://github.com/smeighan/xLights/releases/download/${version}/xLights-${version}-x86_64.AppImage";
-    hash = "sha256-r9NtNuPmc8J1y3MxA4oSdRmPKDQeDQENdKbNVlcGwAI=";
+    hash = "sha256-KirclA2BP+iya2/MRHphY2wvhBCgtpz5iMoEOaqX4HE=";
   };
 
   meta = {
