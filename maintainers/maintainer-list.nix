8feba149a095b206e4ed56628cfb0db7aa3a3eee
Christian Höppner <chris@mkaito.net>
2024-11-13 11:56:11 +0000

    mkaito: Remove

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14550,12 +14550,6 @@
     githubId = 16974598;
     name = "Mike Playle";
   };
-  mkaito = {
-    email = "chris@mkaito.net";
-    github = "mkaito";
-    githubId = 20434;
-    name = "Christian Höppner";
-  };
   mkazulak = {
     email = "kazulakm@gmail.com";
     github = "mulderr";
