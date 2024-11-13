f46f5f6e032f197a18f9399e389f97ea8eca1787
Emily <vcs@emily.moe>
2024-11-13 16:03:57 +0000

    edencommon: convert to new Darwin SDK pattern
    (cherry picked from commit 3c14c26a35418f54f3ec3f1d767a92de8fdb5341)
    (cherry picked from commit 353788c210496995fbd4c58af73362558999da14)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9022,7 +9022,7 @@ with pkgs;
     stdenv = if stdenv.hostPlatform.isDarwin then gccStdenv else stdenv;
   };
 
-  edencommon = darwin.apple_sdk_11_0.callPackage ../development/libraries/edencommon { };
+  edencommon = callPackage ../development/libraries/edencommon { };
 
   eigen = callPackage ../development/libraries/eigen { };
 
