b870da1d4b3a659625e9622718e6d9afcac435b8
Sarah Clark <seclark@nextquestion.net>
2024-11-12 19:41:15 -0800

    cargo-vibe: migrate to new apple-sdk pattern

--- pkgs/by-name/ca/cargo-vibe/package.nix
+++ pkgs/by-name/ca/cargo-vibe/package.nix
@@ -7,7 +7,6 @@
   dbus,
   udev,
   openssl,
-  darwin,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cargo-vibe";
@@ -23,20 +22,10 @@ rustPlatform.buildRustPackage rec {
 
   nativeBuildInputs = [ pkg-config ];
 
-  buildInputs =
-    [
-      dbus
-      openssl
-    ]
-    ++ lib.optional stdenv.hostPlatform.isLinux udev
-    ++ lib.optionals stdenv.hostPlatform.isDarwin (
-      with darwin.apple_sdk.frameworks;
-      [
-        AppKit
-        IOKit
-        CoreBluetooth
-      ]
-    );
+  buildInputs = [
+    dbus
+    openssl
+  ] ++ lib.optional stdenv.hostPlatform.isLinux udev;
 
   meta = with lib; {
     description = "Cargo x Buttplug.io";
