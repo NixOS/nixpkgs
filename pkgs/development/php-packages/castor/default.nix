0b609ebe3e3a60fb3b7941807c702fd597a8f645
Romain Gautier <mykiwi@users.noreply.github.com>
2024-11-13 15:54:36 +0000

    php83Packages.castor: 0.18.2 -> 0.20.0
    Signed-off-by: Romain Gautier <mail@romain.sh>

--- pkgs/development/php-packages/castor/default.nix
+++ pkgs/development/php-packages/castor/default.nix
@@ -9,16 +9,16 @@
 
 php.buildComposerProject2 (finalAttrs: {
   pname = "castor";
-  version = "0.18.2";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "jolicode";
     repo = "castor";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-f+vz3SFXnZS67dyHQyycONBtfydMVh2XjB/4r9QIak8=";
+    hash = "sha256-/ceWw2/ct0+89XKgLzNywkd7/tIYI1+k1h05c6vaqIU=";
   };
 
-  vendorHash = "sha256-U/L+iJ/DKCiUEbSUc/BgYeKakv0BdK6Eq5BJjtwb1Yk=";
+  vendorHash = "sha256-w8CyN3iLgC8seN01yC8ikQQs773A/rT3z5n+emEKqDE=";
 
   nativeBuildInputs = [ installShellFiles ];
 
