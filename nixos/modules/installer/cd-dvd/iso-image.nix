8c94b70e80c6b6eae160e23bc80141aa5be1f777
Will Fancher <elvishjerricco@gmail.com>
2024-11-13 02:07:33 -0500

    nixos/iso-image: Remove extra boot options

--- nixos/modules/installer/cd-dvd/iso-image.nix
+++ nixos/modules/installer/cd-dvd/iso-image.nix
@@ -57,18 +57,6 @@ let
     in
     menuBuilderGrub2 finalCfg [
       { class = "installer"; }
-      {
-        class = "nomodeset";
-        params = "nomodeset";
-      }
-      {
-        class = "copytoram";
-        params = "copytoram";
-      }
-      {
-        class = "debug";
-        params = "debug";
-      }
     ];
 
   # Timeout in syslinux is in units of 1/10 of a second.
@@ -111,33 +99,6 @@ let
     APPEND init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
     INITRD /boot/${config.system.boot.loader.initrdFile}
 
-    # A variant to boot with 'nomodeset'
-    LABEL boot-nomodeset
-    MENU LABEL ${config.isoImage.prependToMenuLabel}${config.system.nixos.distroName} ${config.system.nixos.label}${config.isoImage.appendToMenuLabel} (nomodeset)
-    LINUX /boot/${config.system.boot.loader.kernelFile}
-    APPEND init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams} nomodeset
-    INITRD /boot/${config.system.boot.loader.initrdFile}
-
-    # A variant to boot with 'copytoram'
-    LABEL boot-copytoram
-    MENU LABEL ${config.isoImage.prependToMenuLabel}${config.system.nixos.distroName} ${config.system.nixos.label}${config.isoImage.appendToMenuLabel} (copytoram)
-    LINUX /boot/${config.system.boot.loader.kernelFile}
-    APPEND init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams} copytoram
-    INITRD /boot/${config.system.boot.loader.initrdFile}
-
-    # A variant to boot with verbose logging to the console
-    LABEL boot-debug
-    MENU LABEL ${config.isoImage.prependToMenuLabel}${config.system.nixos.distroName} ${config.system.nixos.label}${config.isoImage.appendToMenuLabel} (debug)
-    LINUX /boot/${config.system.boot.loader.kernelFile}
-    APPEND init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams} loglevel=7
-    INITRD /boot/${config.system.boot.loader.initrdFile}
-
-    # A variant to boot with a serial console enabled
-    LABEL boot-serial
-    MENU LABEL ${config.isoImage.prependToMenuLabel}${config.system.nixos.distroName} ${config.system.nixos.label}${config.isoImage.appendToMenuLabel} (serial console=ttyS0,115200n8)
-    LINUX /boot/${config.system.boot.loader.kernelFile}
-    APPEND init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams} console=ttyS0,115200n8
-    INITRD /boot/${config.system.boot.loader.initrdFile}
   '';
 
   isolinuxMemtest86Entry = ''
@@ -364,56 +325,6 @@ let
         #
 
         ${buildMenuGrub2}
-        submenu "HiDPI, Quirks and Accessibility" --class hidpi --class submenu {
-          ${grubMenuCfg}
-          submenu "Suggests resolution @720p" --class hidpi-720p {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "video=1280x720@60"}
-          }
-          submenu "Suggests resolution @1080p" --class hidpi-1080p {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "video=1920x1080@60"}
-          }
-
-          # If we boot into a graphical environment where X is autoran
-          # and always crashes, it makes the media unusable. Allow the user
-          # to disable this.
-          submenu "Disable display-manager" --class quirk-disable-displaymanager {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "systemd.mask=display-manager.service"}
-          }
-
-          # Some laptop and convertibles have the panel installed in an
-          # inconvenient way, rotated away from the keyboard.
-          # Those entries makes it easier to use the installer.
-          submenu "" {return}
-          submenu "Rotate framebuffer Clockwise" --class rotate-90cw {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "fbcon=rotate:1"}
-          }
-          submenu "Rotate framebuffer Upside-Down" --class rotate-180 {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "fbcon=rotate:2"}
-          }
-          submenu "Rotate framebuffer Counter-Clockwise" --class rotate-90ccw {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "fbcon=rotate:3"}
-          }
-
-          # As a proof of concept, mainly. (Not sure it has accessibility merits.)
-          submenu "" {return}
-          submenu "Use black on white" --class accessibility-blakconwhite {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "vt.default_red=0xFF,0xBC,0x4F,0xB4,0x56,0xBC,0x4F,0x00,0xA1,0xCF,0x84,0xCA,0x8D,0xB4,0x84,0x68 vt.default_grn=0xFF,0x55,0xBA,0xBA,0x4D,0x4D,0xB3,0x00,0xA0,0x8F,0xB3,0xCA,0x88,0x93,0xA4,0x68 vt.default_blu=0xFF,0x58,0x5F,0x58,0xC5,0xBD,0xC5,0x00,0xA8,0xBB,0xAB,0x97,0xBD,0xC7,0xC5,0x68"}
-          }
-
-          # Serial access is a must!
-          submenu "" {return}
-          submenu "Serial console=ttyS0,115200n8" --class serial {
-            ${grubMenuCfg}
-            ${buildMenuAdditionalParamsGrub2 "console=ttyS0,115200n8"}
-          }
-        }
 
         ${lib.optionalString (refindBinary != null) ''
           # GRUB apparently cannot do "chainloader" operations on "CD".
