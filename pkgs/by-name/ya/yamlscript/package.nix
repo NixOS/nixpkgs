8bb12a39a205688ee5e1467ce04653fc9811410d
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-13 00:14:26 +0000

    yamlscript: 0.1.79 -> 0.1.83

--- pkgs/by-name/ya/yamlscript/package.nix
+++ pkgs/by-name/ya/yamlscript/package.nix
@@ -2,11 +2,11 @@
 
 buildGraalvmNativeImage rec {
   pname = "yamlscript";
-  version = "0.1.79";
+  version = "0.1.83";
 
   src = fetchurl {
     url = "https://github.com/yaml/yamlscript/releases/download/${version}/yamlscript.cli-${version}-standalone.jar";
-    hash = "sha256-Jx3mnAgsTi/5nqcy6DVYPoM4GlQJf+10uuQzhUWp5ZU=";
+    hash = "sha256-c38jNaQSFTQA6+P2joduGe2fqSZmNiokKVf91vkJtEA=";
   };
 
   executable = "ys";
