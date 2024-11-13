9c30b10d82adab63472bb962af6520cfada6b857
Rahul Rameshbabu <sergeantsagara@protonmail.com>
2024-11-13 13:41:34 -0800

    Revert "globalprotect-openconnect: remove deprecated 1.x package"
    This reverts commit b08d6a664f548c86446439a1e24e484b0ee140ea.
    
    Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>
    (cherry picked from commit 8f2fa04fd99a0159c902f1d42e6b1633c4811ba1)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1055,6 +1055,7 @@
   ./services/networking/gdomap.nix
   ./services/networking/ghostunnel.nix
   ./services/networking/git-daemon.nix
+  ./services/networking/globalprotect-vpn.nix
   ./services/networking/gns3-server.nix
   ./services/networking/gnunet.nix
   ./services/networking/go-autoconfig.nix
