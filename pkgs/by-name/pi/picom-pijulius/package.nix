3d381bc1a352ead7543e390e76a8b3fb5f002c0d
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-11-13 10:18:29 +0100

    picom-pijulius: nixfmt, order attrs

--- pkgs/by-name/pi/picom-pijulius/package.nix
+++ pkgs/by-name/pi/picom-pijulius/package.nix
@@ -3,8 +3,9 @@
   lib,
   writeShellScript,
   fetchFromGitHub,
-  unstableGitUpdater
+  unstableGitUpdater,
 }:
+
 picom.overrideAttrs (previousAttrs: {
   pname = "picom-pijulius";
   version = "8.2-unstable-2024-11-12";
@@ -18,23 +19,25 @@ picom.overrideAttrs (previousAttrs: {
 
   dontVersionCheck = true;
 
+  passthru.updateScript = unstableGitUpdater {
+    tagFormat = "v([A-Z]+)([a-z]+)|v([1-9]).([1-9])|v([1-9])-rc([1-9])";
+    tagConverter = writeShellScript "picom-pijulius-tag-converter.sh" ''
+      sed -e 's/v//g' -e 's/([A-Z])([a-z])+/8.2/g' -e 's/-rc([1-9])|-rc//g' -e 's/0/8.2/g'
+    '';
+  };
+
   meta = {
     inherit (previousAttrs.meta)
       license
-      platforms
-      mainProgram
       longDescription
+      mainProgram
+      platforms
       ;
 
     description = "Pijulius's picom fork with extensive animation support";
     homepage = "https://github.com/pijulius/picom";
-    maintainers = with lib.maintainers; [ YvesStraten ];
-  };
-
-  passthru.updateScript = unstableGitUpdater {
-    tagFormat = "v([A-Z]+)([a-z]+)|v([1-9]).([1-9])|v([1-9])-rc([1-9])";
-    tagConverter = writeShellScript "picom-pijulius-tag-converter.sh" ''
-sed -e 's/v//g' -e 's/([A-Z])([a-z])+/8.2/g' -e 's/-rc([1-9])|-rc//g' -e 's/0/8.2/g'
-'';
+    maintainers = with lib.maintainers; [
+      YvesStraten
+    ];
   };
 })
