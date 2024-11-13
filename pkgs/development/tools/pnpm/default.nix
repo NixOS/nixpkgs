89c0a1604a40a608fce404a86b416b3fac92308d
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-11-13 13:57:28 +0100

    pnpm: 9.12.3 -> 9.14.2

--- pkgs/development/tools/pnpm/default.nix
+++ pkgs/development/tools/pnpm/default.nix
@@ -12,8 +12,8 @@ let
       hash = "sha256-2qJ6C1QbxjUyP/lsLe2ZVGf/n+bWn/ZwIVWKqa2dzDY=";
     };
     "9" = {
-      version = "9.12.3";
-      hash = "sha256-JCNXcsxKyCpiYnzUf4NMcmZ6LOh3mahG7E6OVV4tS4s=";
+      version = "9.14.2";
+      hash = "sha256-BuZaSWW6/21gl/nI91w19tQgl028A9d1AJBWpp7f0nE=";
     };
   };
 
