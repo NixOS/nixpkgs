be97ac3fe63710b41b15a95fb7aff75fb1627f00
Emily <vcs@emily.moe>
2024-11-13 15:29:09 +0000

    fb303: move to `pkgs/by-name`

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9242,8 +9242,6 @@ with pkgs;
     autoreconfHook = buildPackages.autoreconfHook269;
   };
 
-  fb303 = callPackage ../development/libraries/fb303 { };
-
   inherit (callPackage ../development/libraries/ffmpeg { })
     ffmpeg_4
     ffmpeg_4-headless
