f278e7f42c94fcd3944295699cea8307d5413f63
Noa Aarts <noa@voorwaarts.nl>
2024-11-14 12:22:47 +0100

    python3Packages.hikari: fix build Remove the `asyncio_default_fixture_loop_scope` from pyproject.toml

--- pkgs/development/python-modules/hikari/default.nix
+++ pkgs/development/python-modules/hikari/default.nix
@@ -63,6 +63,9 @@ buildPythonPackage rec {
   postPatch = ''
     substituteInPlace hikari/_about.py \
       --replace-fail "__git_sha1__: typing.Final[str] = \"HEAD\"" "__git_sha1__: typing.Final[str] = \"$(cat $src/COMMIT)\""
+    # XXX: Remove once pytest-asyncio is updated to 0.24+
+    substituteInPlace pyproject.toml \
+      --replace-fail "asyncio_default_fixture_loop_scope = \"func\"" ""
   '';
 
   meta = {
