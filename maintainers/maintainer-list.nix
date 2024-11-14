75a5d7a240629a3c001b3439cc6ce05341b6fbf1
purepani <pani0028@umn.edu>
2024-11-13 19:20:46 -0600

    maintainers: add purepani as maintainer

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18069,6 +18069,12 @@
     githubId = 5636;
     name = "Steve Purcell";
   };
+  purepani = {
+    email = "purepani+nixpkgs@pm.me";
+    github = "purepani";
+    githubId = 7587353;
+    name = "Satwik Pani";
+  };
   purpole = {
     email = "mail@purpole.io";
     github = "purpole";
