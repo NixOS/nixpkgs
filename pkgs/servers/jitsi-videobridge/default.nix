1cdc18fbbfb05ad76df3c10dc8adddb9edbac210
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-14 23:22:02 +0000

    jitsi-videobridge: 2.3-160-g97a1f15b -> 2.3-174-gd011ddf7

--- pkgs/servers/jitsi-videobridge/default.nix
+++ pkgs/servers/jitsi-videobridge/default.nix
@@ -2,10 +2,10 @@
 
 let
   pname = "jitsi-videobridge2";
-  version = "2.3-160-g97a1f15b";
+  version = "2.3-174-gd011ddf7";
   src = fetchurl {
     url = "https://download.jitsi.org/stable/${pname}_${version}-1_all.deb";
-    sha256 = "fxAmpsScNTwSrITZYE2dsKTlGqlMcAAf8RVixpmcqvI=";
+    sha256 = "qBI++Awoe2NlCedgxXmqm4zXR3fYekVCyXRBF/SirWk=";
   };
 in
 stdenv.mkDerivation {
