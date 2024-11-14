c45a3c3604585572af39d8768826a90d134167c2
Matthieu Daniel-Thomas <matthieu.daniel-thomas@cea.fr>
2024-11-14 17:09:51 +0100

    vscode-extensions.nsd.vscode-epics: init at 1.1.0

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -3803,6 +3803,8 @@ let
         };
       };
 
+      nsd.vscode-epics = callPackage ./nsd.vscode-epics { };
+
       ocamllabs.ocaml-platform = buildVscodeMarketplaceExtension {
         meta = {
           changelog = "https://marketplace.visualstudio.com/items/ocamllabs.ocaml-platform/changelog";
