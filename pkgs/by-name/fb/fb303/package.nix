51d81d49530275aabd3fd9c9fda3f804d39a1d44
Emily <vcs@emily.moe>
2024-11-14 03:13:56 +0000

    fb303: split outputs
    (cherry picked from commit 79dc5a8b54c7e7149674fd1d4348c8dd412119f9)

--- pkgs/by-name/fb/fb303/package.nix
+++ pkgs/by-name/fb/fb303/package.nix
@@ -21,6 +21,11 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "fb303";
   version = "2024.11.18.00";
 
+  outputs = [
+    "out"
+    "dev"
+  ];
+
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "fb303";
@@ -53,6 +58,11 @@ stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "CMAKE_INSTALL_RPATH_USE_LINK_PATH" true)
 
     (lib.cmakeBool "PYTHON_EXTENSIONS" false)
+
+    (lib.cmakeFeature "INCLUDE_INSTALL_DIR" "${placeholder "dev"}/include")
+    (lib.cmakeFeature "LIB_INSTALL_DIR" "${placeholder "out"}/lib")
+    (lib.cmakeFeature "CMAKE_INSTALL_DIR" "${placeholder "dev"}/lib/cmake/fb303")
+    (lib.cmakeFeature "CMAKE_INSTALL_PREFIX" (placeholder "dev"))
   ];
 
   meta = {
