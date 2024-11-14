8e327be3a81a229ab1a6526b358f39f1f75c720b
wxt <3264117476@qq.com>
2024-11-14 18:21:59 +0800

    python3Packages.jenkins-job-builder: adopt

--- pkgs/development/python-modules/jenkins-job-builder/default.nix
+++ pkgs/development/python-modules/jenkins-job-builder/default.nix
@@ -38,11 +38,11 @@ buildPythonPackage rec {
   # Need to fix test deps, relies on stestr and a few other packages that aren't available on nixpkgs
   checkPhase = "$out/bin/jenkins-jobs --help";
 
-  meta = with lib; {
+  meta = {
     description = "Jenkins Job Builder is a system for configuring Jenkins jobs using simple YAML files stored in Git";
     mainProgram = "jenkins-jobs";
     homepage = "https://jenkins-job-builder.readthedocs.io/en/latest/";
-    license = licenses.asl20;
-    maintainers = [ ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ bot-wxt1221 ];
   };
 }
