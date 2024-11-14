643493f00fb4dc8d8fb60c5a4602aba743e729b6
aleksana <me@aleksana.moe>
2024-11-14 15:42:49 +0800

    openvi: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15379,8 +15379,6 @@ with pkgs;
 
   opentx = libsForQt5.callPackage ../applications/misc/opentx { };
 
-  openvi = darwin.apple_sdk_11_0.callPackage ../applications/editors/openvi { };
-
   organicmaps = qt6Packages.callPackage ../applications/misc/organicmaps { };
 
   owofetch = callPackage ../tools/misc/owofetch {
