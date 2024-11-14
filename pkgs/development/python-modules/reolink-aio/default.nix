977b48ba2fa7731b541992f7f4b97ab9865e8edd
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-14 09:44:08 +0100

    python312Packages.reolink-aio: 0.11.0b1 -> 0.11.0
    Diff: https://github.com/starkillerOG/reolink_aio/compare/refs/tags/0.11.0b1...0.11.0
    
    Changelog: https://github.com/starkillerOG/reolink_aio/releases/tag/0.11.0
    (cherry picked from commit 960e9106afee832cb696478c27e54414fc2d6586)

--- pkgs/development/python-modules/reolink-aio/default.nix
+++ pkgs/development/python-modules/reolink-aio/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "reolink-aio";
-  version = "0.11.0b1";
+  version = "0.11.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "starkillerOG";
     repo = "reolink_aio";
     rev = "refs/tags/${version}";
-    hash = "sha256-kLrdhwIUdmZh08I7XOHT2ciiVn8JE9lcfVcMJf05bkk=";
+    hash = "sha256-CRKCqIi4WxO5t0PdyGV3fJlcK99j1PKVSBoisw4zuZ0=";
   };
 
   build-system = [ setuptools ];
