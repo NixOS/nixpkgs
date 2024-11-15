059a8da6b10b5d301a9b0c100d699f197800ac8b
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-11-15 17:53:14 +0530

    nixos/g810-led: add to modules-list.nix

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -588,6 +588,7 @@
   ./services/hardware/fancontrol.nix
   ./services/hardware/freefall.nix
   ./services/hardware/fwupd.nix
+  ./services/hardware/g810-led.nix
   ./services/hardware/handheld-daemon.nix
   ./services/hardware/hddfancontrol.nix
   ./services/hardware/illum.nix
