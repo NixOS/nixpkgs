4954729a0c07c37cc544a3a091e55fb4b384b414
Noa Aarts <noa@voorwaarts.nl>
2024-11-15 23:02:07 +0100

    neovide: only require libtool on darwin
    (cherry picked from commit a9bd18d88d6b5e014b82d8f83a51cd22c67f0eab)

--- pkgs/by-name/ne/neovide/package.nix
+++ pkgs/by-name/ne/neovide/package.nix
@@ -64,13 +64,16 @@ rustPlatform.buildRustPackage.override { stdenv = clangStdenv; } rec {
   SKIA_GN_COMMAND = "${gn}/bin/gn";
   SKIA_NINJA_COMMAND = "${ninja}/bin/ninja";
 
-  nativeBuildInputs = [
-    cctools.libtool
-    makeWrapper
-    pkg-config
-    python3 # skia
-    removeReferencesTo
-  ];
+  nativeBuildInputs =
+    [
+      makeWrapper
+      pkg-config
+      python3 # skia
+      removeReferencesTo
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      cctools.libtool
+    ];
 
   nativeCheckInputs = [ neovim ];
 
