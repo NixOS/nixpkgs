c4c859391edba218c3ccd19d8e77992ee98bcdb3
aucub <78630225+aucub@users.noreply.github.com>
2024-11-15 22:46:31 +0800

    dolt: 1.43.1 -> 1.43.14

--- pkgs/by-name/do/dolt/package.nix
+++ pkgs/by-name/do/dolt/package.nix
@@ -1,27 +1,31 @@
-{ fetchFromGitHub, lib, buildGoModule }:
+{
+  fetchFromGitHub,
+  lib,
+  buildGoModule,
+}:
 
 buildGoModule rec {
   pname = "dolt";
-  version = "1.43.1";
+  version = "1.43.14";
 
   src = fetchFromGitHub {
     owner = "dolthub";
     repo = "dolt";
     rev = "v${version}";
-    sha256 = "sha256-i+WW8udVjLQGMg0eu03hInfXaLZzVJ4liKE4eQ/KDww=";
+    sha256 = "sha256-ocQtbYdpqKoumG5WEE4q4WiQPgT2ezCVu11DvfG+vXo=";
   };
 
   modRoot = "./go";
   subPackages = [ "cmd/dolt" ];
-  vendorHash = "sha256-0SvM6I+zGelSUMlbeeqljfzITw0Cnl2BZHJ78n+m5ds=";
+  vendorHash = "sha256-ruGyOI+RmVxTe321a5APkKcQzLbOSw05q4zqQ9Fe1MY=";
   proxyVendor = true;
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     description = "Relational database with version control and CLI a-la Git";
     mainProgram = "dolt";
     homepage = "https://github.com/dolthub/dolt";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ danbst ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ danbst ];
   };
 }
