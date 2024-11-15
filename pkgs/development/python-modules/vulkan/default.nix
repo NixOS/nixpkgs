8a970598f696c2af0b5aee837065a97fd3158c55
Grimmauld <Grimmauld@grimmauld.de>
2024-11-15 16:42:36 +0100

    python312Packages.vulkan: init at 1.3.275.1
    (cherry picked from commit 73db838c15264acf40864f5fa641b44c9c5ea5ff)

---
+++ pkgs/development/python-modules/vulkan/default.nix
@@ -0,0 +1,63 @@
+{
+  buildPythonPackage,
+  cffi,
+  fetchFromGitHub,
+  inflection,
+  jinja2,
+  lib,
+  pysdl2,
+  setuptools,
+  vulkan-loader,
+  wheel,
+  xmltodict,
+}:
+
+buildPythonPackage rec {
+  pname = "vulkan";
+  version = "1.3.275.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "realitix";
+    repo = "vulkan";
+    rev = "refs/tags/${version}";
+    hash = "sha256-b1jHNKdHF7pIC6H4O2yxy36Ppb60J0uN2P0WaCw51Gc=";
+  };
+
+  postPatch = ''
+    substituteInPlace vulkan/_vulkan.py \
+      --replace-fail 'lib = ffi.dlopen(name)' 'lib = ffi.dlopen("${vulkan-loader}/lib/" + name)'
+  '';
+
+  buildInputs = [
+    vulkan-loader
+  ];
+
+  build-system = [
+    setuptools
+    wheel
+  ];
+
+  dependencies = [
+    inflection
+    jinja2
+    pysdl2
+    xmltodict
+    cffi
+  ];
+
+  pythonImportsCheck = [
+    "vulkan"
+  ];
+
+  meta = {
+    description = "Ultimate Python binding for Vulkan API";
+    homepage = "https://github.com/realitix/vulkan";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [
+      grimmauld
+      getchoo
+    ];
+  };
+}
