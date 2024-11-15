12d32a594696556927b30485fcde006902fe6216
Wolfgang Walther <walther@technowledgy.de>
2024-11-15 20:55:49 +0000

    postgresqlPackages.plpgsql_check: 2.7.5 -> 2.7.12

--- pkgs/servers/sql/postgresql/ext/plpgsql_check.nix
+++ pkgs/servers/sql/postgresql/ext/plpgsql_check.nix
@@ -2,13 +2,13 @@
 
 buildPostgresqlExtension (finalAttrs: {
   pname = "plpgsql-check";
-  version = "2.7.5";
+  version = "2.7.12";
 
   src = fetchFromGitHub {
     owner = "okbob";
     repo = "plpgsql_check";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-CD/G/wX6o+mC6gowlpFe1DdJWyh3cB9wxSsW2GXrENE=";
+    hash = "sha256-sLakN4595z+Smt7oaK7IPIJZp/JIGwL5UB4OXQek7JU=";
   };
 
   passthru.tests.extension = postgresqlTestExtension {
