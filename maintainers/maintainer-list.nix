6c94f637f58afc8b577cce8c088d848ca0866b54
David E. C. Kopczynski <david.kop.dk@gmail.com>
2024-11-15 04:42:16 +0100

    maintainers: add Jojo4GH
    (cherry picked from commit f640d0570df9caca074823baef64f5be8fa6a62c)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10733,6 +10733,11 @@
     name = "Joonas Rautiola";
     keys = [ { fingerprint = "87EC DD30 6614 E510 5299  F0D4 090E B48A 4669 AA54"; } ];
   };
+  Jojo4GH = {
+    name = "Jonas Broeckmann";
+    github = "Jojo4GH";
+    githubId = 36777568;
+  };
   jojosch = {
     name = "Johannes Schleifenbaum";
     email = "johannes@js-webcoding.de";
