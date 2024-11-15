e6eee7e5386888258cee66f0b48a586eecbd1dfc
Christoph Hollizeck <christoph.hollizeck@hey.com>
2024-11-15 20:17:15 +0100

    pyfa: 2.60.2 -> 2.61.0

--- pkgs/by-name/py/pyfa/package.nix
+++ pkgs/by-name/py/pyfa/package.nix
@@ -1,20 +1,90 @@
 {
   lib,
-  appimageTools,
-  fetchurl,
+  python3Packages,
+  fetchFromGitHub,
+
+  # dependecies
+  webkitgtk_4_1,
+  python312,
 }:
 let
-  version = "2.60.2";
+  version = "2.61.0";
 in
-appimageTools.wrapType2 {
+python3Packages.buildPythonApplication rec {
   inherit version;
   pname = "pyfa";
+  pyproject = false;
 
-  src = fetchurl {
-    name = "pyfa-appimage-${version}";
-    url = "https://github.com/pyfa-org/Pyfa/releases/download/v${version}/pyfa-v${version}-linux.AppImage";
-    hash = "sha256-6doetQ6E1Occ/SqewfxRqPEX1MnUuFomm+8VmetIz4Y=";
+  src = fetchFromGitHub {
+    owner = "pyfa-org";
+    repo = "Pyfa";
+    rev = "v${version}";
+    hash = "sha256-VSuRQugUODc+LbhKbzsA09pnqPEIAt0pQS2An/p7r9A=";
   };
+
+  build-system = with python3Packages; [
+    setuptools
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    wxpython
+    logbook
+    matplotlib
+    dateutil
+    requests
+    sqlalchemy_1_4
+    cryptography
+    markdown2
+    beautifulsoup4
+    pyaml
+    roman
+    numpy
+    python-jose
+    requests-cache
+
+    # non python dependency
+    webkitgtk_4_1
+  ];
+
+  preBuild = ''
+    cat > setup.py << EOF
+    from setuptools import setup
+
+    setup(
+      name='${pname}',
+      version='${version}',
+      scripts=[
+        '${pname}.py',
+      ],
+      package_dir=' ',
+      packages=setuptools.find_packages(),
+    )
+    EOF
+
+    touch __init__.py
+  '';
+
+  installPhase = ''
+    PYTHONDONTWRITEBYTECODE=1 ${python312}/bin/python3 db_update.py
+
+    install -dm755 $out/bin/pyfa
+    install -Dm755 pyfa.py $out/bin/pyfa
+
+    install -Dm644 config.py $out/bin/pyfa
+    install -Dm644 db_update.py $out/bin/pyfa
+    install -Dm644 eve.db $out/bin/pyfa
+    install -Dm644 README.md $out/bin/pyfa
+    install -Dm644 version.yml $out/bin/pyfa
+
+    cp -a eos $out/bin/pyfa
+    cp -a graphs $out/bin/pyfa
+    cp -a gui $out/bin/pyfa
+    cp -a imgs $out/bin/pyfa
+    cp -a service $out/bin/pyfa
+    cp -a utils $out/bin/pyfa
+  '';
+
+  doCheck = false;
 
   meta = {
     description = "Python fitting assistant, cross-platform fitting tool for EVE Online";
