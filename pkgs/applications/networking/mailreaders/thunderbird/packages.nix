b8fe9967b53e377f95b7f07c12107d66c3e8cd53
Vladimír Čunát <v@cunat.cz>
2024-11-15 08:32:05 +0100

    thunderbird-latest: 132.0 -> 132.0.1
    https://www.thunderbird.net/en-US/thunderbird/132.0.1/releasenotes/

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -57,8 +57,8 @@ in rec {
   thunderbird = thunderbird-esr;
 
   thunderbird-latest = common {
-    version = "132.0";
-    sha512 = "71206606d691e3b257b4b163e56604eaff221a43f88015fcbdbbbb3bbd708a7459f61b0470f7534ce9adafd41561e11b3487484fbfe3e95a06674785cae97029";
+    version = "132.0.1";
+    sha512 = "ff2ff1344c3ab6594fba2e03d3f0548221934b94972819cfb6de9cea0bf9bac4ec67d14e7c6b9ae854ecdbc472b814a5f6f7204a9ce71ddac6167ff536a6276a";
 
     updateScript = callPackage ./update.nix {
       attrPath = "thunderbirdPackages.thunderbird-latest";
