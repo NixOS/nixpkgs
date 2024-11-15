fb63f605c5a14c23d596d070fffc9e89da59526e
Toast <39011842+toast003@users.noreply.github.com>
2024-11-15 10:49:15 +0100

    handheld-daemon-ui: 3.2.1 -> 3.2.3

--- pkgs/by-name/ha/handheld-daemon-ui/package.nix
+++ pkgs/by-name/ha/handheld-daemon-ui/package.nix
@@ -5,11 +5,11 @@
 }:
 let
   pname = "handheld-daemon-ui";
-  version = "3.2.1";
+  version = "3.2.3";
 
   src = fetchurl {
     url = "https://github.com/hhd-dev/hhd-ui/releases/download/v${version}/hhd-ui.Appimage";
-    hash = "sha256-RRXVoeWOO/pR+CAEY0J6Buf/RhA+G0PdxGQVMdAHfwA=";
+    hash = "sha256-VhJrOvE+BebJIAeQlwOOsPfqSrvBnJQavGT7glTrG2o=";
   };
   extractedFiles = appimageTools.extractType2 { inherit pname version src; };
 in
