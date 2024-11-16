0a6862aa2c5ff9b5ef1fc8572f2da30880d5b599
Gliczy <129636582+Gliczy@users.noreply.github.com>
2024-11-16 18:30:11 +0100

    nwg-panel: avoid `with lib;`

--- pkgs/by-name/nw/nwg-panel/package.nix
+++ pkgs/by-name/nw/nwg-panel/package.nix
@@ -89,13 +89,13 @@ python3Packages.buildPythonApplication rec {
     )
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/nwg-piotr/nwg-panel";
     changelog = "https://github.com/nwg-piotr/nwg-panel/releases/tag/v${version}";
     description = "GTK3-based panel for Sway window manager";
-    license = licenses.mit;
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ ludovicopiero ];
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ ludovicopiero ];
     mainProgram = "nwg-panel";
   };
 }
