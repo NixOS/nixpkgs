4de62b4c0fc4a24e5efae581c440a5196773bd5a
Kenichi Kamiya <kachick1@gmail.com>
2024-11-16 12:06:31 +0900

    nb: add passthru.tests.version

--- pkgs/by-name/nb/nb/package.nix
+++ pkgs/by-name/nb/nb/package.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchFromGitHub, installShellFiles }:
+{ stdenv, lib, fetchFromGitHub, installShellFiles, testers, nb }:
 
 stdenv.mkDerivation rec {
   pname = "nb";
@@ -24,6 +24,14 @@ stdenv.mkDerivation rec {
   postInstall = ''
     installShellCompletion --cmd nb etc/nb-completion.{bash,zsh,fish}
   '';
+
+  passthru = {
+    tests.version = testers.testVersion {
+      package = nb;
+      # Setting EDITOR to avoid: "Command line text editor not found"
+      command = "EDITOR=nano nb --version";
+    };
+  };
 
   meta = with lib; {
     description = "Command line note-taking, bookmarking, archiving, and knowledge base application";
