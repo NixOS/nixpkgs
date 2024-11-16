ceb44177d5cabc4374a38148b53f084416f43724
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-16 16:44:23 +0100

    python312Packages.google-cloud-resource-manager: 1.13.0 -> 1.13.1
    Changelog: https://github.com/googleapis/google-cloud-python/blob/google-cloud-resource-manager-v1.13.1/packages/google-cloud-resource-manager/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-resource-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-resource-manager/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-resource-manager";
-  version = "1.13.0";
+  version = "1.13.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "google_cloud_resource_manager";
     inherit version;
-    hash = "sha256-rkv2lEPxSzcAfU2EFQEVsJQuiwFlD9eh/G/03Bdg5cQ=";
+    hash = "sha256-vuny+x2FZzEYK3zAWYDSFqroSJR8za3yhIosZMzWu+o=";
   };
 
   build-system = [ setuptools ];
