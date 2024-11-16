a3b4688866b8fb15a995667374aa2b3beb588294
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-16 09:05:58 +0100

    python312Packages.hishel: update disabled

--- pkgs/development/python-modules/hishel/default.nix
+++ pkgs/development/python-modules/hishel/default.nix
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   version = "0.1.1";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "karpetrosyan";
