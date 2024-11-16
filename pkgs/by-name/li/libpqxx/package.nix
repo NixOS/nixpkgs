c0ea1836e0daa07ad04f36f02f6241543ae1cf85
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-11-16 09:48:29 +0100

    libpqxx: nixfmt
    Co-authored-by: superherointj <5861043+superherointj@users.noreply.github.com>

--- pkgs/by-name/li/libpqxx/package.nix
+++ pkgs/by-name/li/libpqxx/package.nix
@@ -1,4 +1,10 @@
-{ lib, stdenv, fetchFromGitHub, postgresql, python3 }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  postgresql,
+  python3,
+}:
 
 stdenv.mkDerivation rec {
   pname = "libpqxx";
@@ -11,15 +17,24 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-mvGPMbk4b8NmPvzy5hS+Au69NtDGha8ONTEQf6I3VZE=";
   };
 
-  nativeBuildInputs = [ python3 ];
-  buildInputs = [ postgresql ];
+  nativeBuildInputs = [
+    python3
+  ];
+
+  buildInputs = [
+    postgresql
+  ];
 
   preConfigure = ''
     patchShebangs ./tools/splitconfig
   '';
 
-  configureFlags = [ "--enable-shared --disable-documentation" ];
-  CXXFLAGS = [ "-std=c++17" ];
+  configureFlags = [
+    "--enable-shared --disable-documentation"
+  ];
+  CXXFLAGS = [
+    "-std=c++17"
+  ];
 
   meta = {
     description = "C++ library to access PostgreSQL databases";
