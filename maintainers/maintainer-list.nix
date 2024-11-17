adaf5f9b58aadbebb4ca12767e60f16f33ee1f5d
Sam Connelly <myclevorname@gmail.com>
2024-11-17 14:31:41 -0500

    maintainers: drop luc65r

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12852,12 +12852,6 @@
     githubId = 37505890;
     name = "Luis Wirth";
   };
-  luc65r = {
-    email = "lucas@ransan.fr";
-    github = "luc65r";
-    githubId = 59375051;
-    name = "Lucas Ransan";
-  };
   LucaGuerra = {
     email = "luca@guerra.sh";
     github = "LucaGuerra";
