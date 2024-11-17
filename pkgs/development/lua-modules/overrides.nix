55e9d7c44446a1efd76139ccb7dd2f1824a70e25
Austin Horstman <khaneliman12@gmail.com>
2024-11-17 09:13:42 -0600

    luaPackages.rustaceanvim: refactor checkPhase
    Move checkInputs into custom checkPhase so they propogate to vim plugin.

--- pkgs/development/lua-modules/overrides.nix
+++ pkgs/development/lua-modules/overrides.nix
@@ -772,15 +772,14 @@ in
       '';
   });
 
-  rustaceanvim  = prev.rustaceanvim.overrideAttrs(oa: {
+  rustaceanvim = prev.rustaceanvim.overrideAttrs (oa: {
     doCheck = lua.luaversion == "5.1";
-    nativeCheckInputs = [ final.nlua final.busted ];
     checkPhase = ''
       runHook preCheck
       export HOME=$(mktemp -d)
-      busted --lua=nlua
+      ${lib.getExe final.busted} --lua=${lib.getExe final.nlua}
       runHook postCheck
-      '';
+    '';
   });
 
   sqlite = prev.sqlite.overrideAttrs (drv: {
