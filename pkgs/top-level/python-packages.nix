cc51d5006fa31d20f3b5a02cfd59ffd21be75692
Daniël de Kok <me@danieldk.eu>
2024-11-17 13:54:32 +0100

    python3Packages.curated-transformers: init at 0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2793,6 +2793,8 @@ self: super: with self; {
 
   curated-tokenizers = callPackage ../development/python-modules/curated-tokenizers { };
 
+  curated-transformers = callPackage ../development/python-modules/curated-transformers { };
+
   customtkinter = callPackage ../development/python-modules/customtkinter { };
 
   cucumber-tag-expressions = callPackage ../development/python-modules/cucumber-tag-expressions { };
