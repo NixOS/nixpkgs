d99d28675785e8140a4bdb68965670035a9e1907
Reno Dakota <paparodeo@proton.me>
2024-11-16 20:36:36 -0800

    ocamlPackages.mirage-crypto-rng: disable test due to slow aarch64 counter
    the test and underlying routine expects a different result each time the
    clock is sampled, however the clock only increments at 24MHz (1GHz on
    M3) causing the test to fail. Convert the test into a warning.
    
    `https://github.com/mirage/mirage-crypto/issues/216`

--- pkgs/development/ocaml-modules/mirage-crypto/rng.nix
+++ pkgs/development/ocaml-modules/mirage-crypto/rng.nix
@@ -1,10 +1,19 @@
-{ buildDunePackage, mirage-crypto, ohex, ounit2, randomconv, dune-configurator
-, digestif, duration, logs }:
+{ lib, stdenv, buildDunePackage, mirage-crypto, ohex, ounit2, randomconv
+, dune-configurator, digestif, duration, logs }:
 
 buildDunePackage rec {
   pname = "mirage-crypto-rng";
 
   inherit (mirage-crypto) version src;
+
+  # https://github.com/mirage/mirage-crypto/issues/216
+  # aarch64 counter may run at 24MHz causing test to fail
+  postPatch = lib.optionalString stdenv.hostPlatform.isAarch64 ''
+    substituteInPlace tests/test_entropy.ml \
+      --replace-fail \
+        'failwith ("same data from timer at "' \
+        'print_endline ("WARNING: same data from timer at "'
+  '';
 
   doCheck = true;
   checkInputs = [ ohex ounit2 randomconv ];
