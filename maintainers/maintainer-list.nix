06d460dbb27eb48dd24912e09867f16a92c8f492
Gurjaka <esartia.gurika@gmail.com>
2024-11-17 16:18:50 +0400

    maintainers: add gurjaka

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8451,6 +8451,12 @@
     githubId = 6578603;
     name = "Jonas Rembser";
   };
+  gurjaka = {
+    name = "Gurami Esartia";
+    email = "esartia.gurika@gmail.com";
+    github = "Gurjaka";
+    githubId = 143032436;
+  };
   guserav = {
     github = "guserav";
     githubId = 28863828;
