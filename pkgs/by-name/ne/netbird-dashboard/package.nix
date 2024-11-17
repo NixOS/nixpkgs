640a6391c7cf5117d4cad5b1f0dcd8321c36541a
Vri <vrifox@vrifox.cc>
2024-11-17 17:01:09 +0100

    netbird-dashboard: 2.5.0 -> 2.7.0

--- pkgs/by-name/ne/netbird-dashboard/package.nix
+++ pkgs/by-name/ne/netbird-dashboard/package.nix
@@ -6,13 +6,13 @@
 
 buildNpmPackage rec {
   pname = "netbird-dashboard";
-  version = "2.5.0";
+  version = "2.7.0";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "dashboard";
     rev = "v${version}";
-    hash = "sha256-PissALQ/3gARnm2hiURABFqF2OKkFITMKf2Rww56hEM=";
+    hash = "sha256-wvmwxWvRaSdDHe3cc22U1vLQGGONpjz2zR4vmvpThdo=";
   };
 
   npmDepsHash = "sha256-A8wzM8gYxYHIhPwxZeIWkNwHJ3skSD7MyNA5qWmX0q8=";
@@ -30,6 +30,6 @@ buildNpmPackage rec {
     description = "NetBird Management Service Web UI Panel";
     homepage = "https://github.com/netbirdio/dashboard";
     license = licenses.bsd3;
-    maintainers = [ ];
+    maintainers = with maintainers; [ vrifox ];
   };
 }
