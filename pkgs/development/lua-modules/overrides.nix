35c6fe78f553c74f42ef208b788775be2216d4e8
Austin Horstman <khaneliman12@gmail.com>
2024-11-17 09:18:09 -0600

    luaPackages.lz-n: refactor checkPhase
    Move checkInputs into custom checkPhase so they propogate to vim plugin.

--- pkgs/development/lua-modules/overrides.nix
+++ pkgs/development/lua-modules/overrides.nix
@@ -534,13 +534,12 @@ in
 
   lz-n  = prev.lz-n.overrideAttrs(oa: {
     doCheck = lua.luaversion == "5.1";
-    nativeCheckInputs = [ final.nlua final.busted ];
     checkPhase = ''
       runHook preCheck
       export HOME=$(mktemp -d)
-      busted --lua=nlua
+      ${lib.getExe final.busted} --lua=${lib.getExe final.nlua}
       runHook postCheck
-      '';
+    '';
   });
 
   lze  = prev.lze.overrideAttrs(oa: {
