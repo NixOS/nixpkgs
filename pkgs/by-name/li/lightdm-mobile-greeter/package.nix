db4a8e680821a1871e8fdb92a57bd29a18d88404
wxt <3264117476@qq.com>
2024-11-18 07:21:50 +0000

    lightdm-mobile-greeter: use new cargo fetcher

--- pkgs/by-name/li/lightdm-mobile-greeter/package.nix
+++ pkgs/by-name/li/lightdm-mobile-greeter/package.nix
@@ -1,12 +1,13 @@
-{ lib
-, fetchFromGitea
-, gtk3
-, libhandy_0
-, lightdm
-, lightdm-mobile-greeter
-, linkFarm
-, pkg-config
-, rustPlatform
+{
+  lib,
+  fetchFromGitea,
+  gtk3,
+  libhandy_0,
+  lightdm,
+  lightdm-mobile-greeter,
+  linkFarm,
+  pkg-config,
+  rustPlatform,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -20,12 +21,8 @@ rustPlatform.buildRustPackage rec {
     rev = "8c8d6dfce62799307320c8c5a1f0dd5c8c18e4d3";
     hash = "sha256-SrAR2+An3BN/doFl/s8PcYZMUHLfVPXKZOo6ndO60nY=";
   };
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "light-dm-sys-0.0.1" = "sha256-91MZhbO/Or0QOt0yVAUhtorpMBBzElFg6U59mF7WB0k=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-9beOnuyzr1vC511il3Yjy9OVcQ0ZP9RQ88eCzx3xLsA=";
 
   buildInputs = [
     gtk3
@@ -43,10 +40,12 @@ rustPlatform.buildRustPackage rec {
       --replace lightdm-mobile-greeter $out/bin/lightdm-mobile-greeter
   '';
 
-  passthru.xgreeters = linkFarm "lightdm-mobile-greeter-xgreeters" [{
-    path = "${lightdm-mobile-greeter}/share/xgreeters/lightdm-mobile-greeter.desktop";
-    name = "lightdm-mobile-greeter.desktop";
-  }];
+  passthru.xgreeters = linkFarm "lightdm-mobile-greeter-xgreeters" [
+    {
+      path = "${lightdm-mobile-greeter}/share/xgreeters/lightdm-mobile-greeter.desktop";
+      name = "lightdm-mobile-greeter.desktop";
+    }
+  ];
 
   meta = with lib; {
     description = "Simple log in screen for use on touch screens";
