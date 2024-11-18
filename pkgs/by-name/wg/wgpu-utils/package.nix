9c943e2a07b86e0acfad456a2a4ce3b5837c1620
wxt <3264117476@qq.com>
2024-11-18 23:55:14 +0000

    wgpu-utils: use new cargo fetcher

--- pkgs/by-name/wg/wgpu-utils/package.nix
+++ pkgs/by-name/wg/wgpu-utils/package.nix
@@ -23,13 +23,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-Gtq0xYZoWNwW+BKVLqVVKGqc+4HjaD7NN1hlzyFP5g0=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "noise-0.8.2" = "sha256-7GvShJeSNfwMCBIfqLghXgKQv7EDMqVchJw0uxPhNr4=";
-      "rspirv-0.11.0+sdk-1.2.198" = "sha256-AcJqkcXBr/+SHdUDXd63sQ0h5eosMqRhV4aUREJH8Bw=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-G23H0rhhHhfHaoTPR2d5mN1obF8xE1+h7PSPUN4Q3Fo=";
 
   nativeBuildInputs = [
     pkg-config
