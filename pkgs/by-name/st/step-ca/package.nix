34941a6aa430580d3ebda1f6cb17f58fa13d9ae8
techknowlogick <techknowlogick@gitea.com>
2024-11-18 16:09:35 -0500

    step-ca: 0.27.5 -> 0.28.0
    Diff: https://github.com/smallstep/certificates/compare/refs/tags/v0.27.5...v0.28.0
    
    Changelog: https://github.com/smallstep/certificates/releases/tag/v0.28.0

--- pkgs/by-name/st/step-ca/package.nix
+++ pkgs/by-name/st/step-ca/package.nix
@@ -13,16 +13,16 @@
 
 buildGoModule rec {
   pname = "step-ca";
-  version = "0.27.5";
+  version = "0.28.0";
 
   src = fetchFromGitHub {
     owner = "smallstep";
     repo = "certificates";
     rev = "refs/tags/v${version}";
-    hash = "sha256-0KIAO9KPP9Lrrw9IIaRdlmmfJ0mwQK0ne//Zofu3TfE=";
+    hash = "sha256-FmhfsfRq6pxDhGDsNqY2IQczHk8NYEdxNaN2hfbhDYw=";
   };
 
-  vendorHash = "sha256-yi4mbuCaT6ydnZwhqqhqMI7bF6IwHm0UqfR5JM81/Ik=";
+  vendorHash = "sha256-ZziypdQJaQHdL5/bcX6P5VHGVtbcl+twtlOg7DlmLZM=";
 
   ldflags = [
     "-w"
