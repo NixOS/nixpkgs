82a647872a198aa4c872c343be4f06d06159b90a
wxt <3264117476@qq.com>
2024-11-18 13:04:40 +0000

    kord: use new cargo fetcher

--- pkgs/by-name/ko/kord/package.nix
+++ pkgs/by-name/ko/kord/package.nix
@@ -1,11 +1,12 @@
-{ lib
-, stdenv
-, darwin
-, fetchFromGitHub
-, fetchpatch
-, rustPlatform
-, pkg-config
-, alsa-lib
+{
+  lib,
+  stdenv,
+  darwin,
+  fetchFromGitHub,
+  fetchpatch,
+  rustPlatform,
+  pkg-config,
+  alsa-lib,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -22,12 +23,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-CeMh6yB4fGoxtGLbkQe4OMMvBM0jesyP+8JtU5kCP84=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "bincode-2.0.0-rc.2" = "sha256-0BfKKGOi5EVIoF0HvIk0QS2fHUMG3tpsMLe2SkXeZlo=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-DpZsi2eIhuetHnLLYGAvv871mbPfAIUevqBLaV8ljGA=";
 
   patches = [
     # Fixes build issues due to refactored Rust compiler feature annotations.
@@ -40,10 +37,12 @@ rustPlatform.buildRustPackage rec {
     })
   ];
 
-  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ pkg-config ]
+  nativeBuildInputs =
+    lib.optionals stdenv.hostPlatform.isLinux [ pkg-config ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [ rustPlatform.bindgenHook ];
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [ alsa-lib ]
+  buildInputs =
+    lib.optionals stdenv.hostPlatform.isLinux [ alsa-lib ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [ darwin.apple_sdk.frameworks.AudioUnit ];
 
   meta = with lib; {
