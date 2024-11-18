a3b1fad3cb4fee683781ca366089ae14d8456704
wxt <3264117476@qq.com>
2024-11-18 23:46:14 +0000

    pixi: use new cargo fetcher

--- pkgs/by-name/pi/pixi/package.nix
+++ pkgs/by-name/pi/pixi/package.nix
@@ -23,16 +23,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-pXJna0WuosQ21u+ImIc70OaG63xVODLaWFkuYqxUc/Y=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "async_zip-0.0.17" = "sha256-3k9rc4yHWhqsCUJ17K55F8aQoCKdVamrWAn6IDWo3Ss=";
-      "cache-key-0.0.1" = "sha256-JEGcX4dT/cVLb07n2Y0nai17jW0tXpV18qaYVnoEpew=";
-      "pubgrub-0.2.1" = "sha256-pU+F6hwqy+r6tz5OBoB6gU0+vdH6F3ikUaPrcvYRX2c=";
-      "reqwest-middleware-0.3.3" = "sha256-csQN7jZTifliSTsOm6YrjPVgsXBOfelY7LkHD1HkNGQ=";
-      "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-vCtTvhXGLnc1qIo9WpFGmFhTEnhs69D7KFemVH1gaz4=";
 
   nativeBuildInputs = [
     pkg-config
