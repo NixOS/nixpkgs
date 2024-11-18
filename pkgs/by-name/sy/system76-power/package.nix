a7227ebcd7935becc24f8acd666468d0729ad0ce
wxt <3264117476@qq.com>
2024-11-18 12:57:12 +0000

    system76-power: use new cargo fetcher

--- pkgs/by-name/sy/system76-power/package.nix
+++ pkgs/by-name/sy/system76-power/package.nix
@@ -1,4 +1,11 @@
-{ pkg-config, libusb1, dbus, lib, rustPlatform, fetchFromGitHub }:
+{
+  pkg-config,
+  libusb1,
+  dbus,
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
 
 rustPlatform.buildRustPackage rec {
   pname = "system76-power";
@@ -12,14 +19,13 @@ rustPlatform.buildRustPackage rec {
   };
 
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ dbus libusb1 ];
+  buildInputs = [
+    dbus
+    libusb1
+  ];
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "sysfs-class-0.1.3" = "sha256-ztfwfCRAkxUd/LLNG5fpVuFdgX+tCKL3F35qYJ2GDm8=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-/dhuWgHysJ2oWgJ6jc9u4tsUOxlFt/awlK/9Jk1GHCM=";
 
   postInstall = ''
     install -D -m 0644 data/com.system76.PowerDaemon.conf $out/etc/dbus-1/system.d/com.system76.PowerDaemon.conf
@@ -32,7 +38,14 @@ rustPlatform.buildRustPackage rec {
     mainProgram = "system76-power";
     homepage = "https://github.com/pop-os/system76-power";
     license = lib.licenses.gpl3Plus;
-    platforms = [ "i686-linux" "x86_64-linux" "aarch64-linux" ];
-    maintainers = with lib.maintainers; [ smonson ahoneybun ];
+    platforms = [
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    maintainers = with lib.maintainers; [
+      smonson
+      ahoneybun
+    ];
   };
 }
