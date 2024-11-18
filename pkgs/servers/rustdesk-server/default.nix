1166ebac00cf34f2e167ba012ebb12f2853faf7d
wxt <3264117476@qq.com>
2024-11-18 10:36:58 +0000

    rustdesk-server: use new cargo fetcher

--- pkgs/servers/rustdesk-server/default.nix
+++ pkgs/servers/rustdesk-server/default.nix
@@ -1,14 +1,15 @@
-{ stdenv
-, lib
-, rustPlatform
-, fetchFromGitHub
-, pkg-config
-, libsodium
-, Security
-, sqlite
-, nix-update-script
-, testers
-, rustdesk-server
+{
+  stdenv,
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  libsodium,
+  Security,
+  sqlite,
+  nix-update-script,
+  testers,
+  rustdesk-server,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -22,25 +23,21 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-dAw1xKyZovPkz1qw58QymIvv7ABhmHs2lFx/H6g7GgI=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "async-speed-limit-0.3.1" = "sha256-iOel6XA07RPrBjQAFLnxXX4VBpDrYZaqQc9clnsOorI=";
-      "confy-0.4.0" = "sha256-e91cvEixhpPzIthAxzTa3fDY6eCsHUy/eZQAqs7QTDo=";
-      "tokio-socks-0.5.1" = "sha256-inmAJk0fAlsVNIwfD/M+htwIdQHwGSTRrEy6N/mspMI=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-BP+7Zt2GqI5PQZU7GZs3yJkIAmONOl5YpCtTzrKqVko=";
 
   nativeBuildInputs = [
     pkg-config
   ];
 
-  buildInputs = [
-    libsodium
-    sqlite
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    Security
-  ];
+  buildInputs =
+    [
+      libsodium
+      sqlite
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      Security
+    ];
 
   passthru = {
     updateScript = nix-update-script { };
@@ -57,6 +54,9 @@ rustPlatform.buildRustPackage rec {
     changelog = "https://github.com/rustdesk/rustdesk-server/releases/tag/${version}";
     license = licenses.agpl3Only;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ gaelreyrol tjni ];
+    maintainers = with maintainers; [
+      gaelreyrol
+      tjni
+    ];
   };
 }
