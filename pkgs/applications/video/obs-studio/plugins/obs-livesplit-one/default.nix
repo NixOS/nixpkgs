6f76852ceb11993a8c78978fd186524b8a0482ba
wxt <3264117476@qq.com>
2024-11-18 09:03:24 +0000

    obs-livesplit-one: use new cargo fetcher

--- pkgs/applications/video/obs-studio/plugins/obs-livesplit-one/default.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-livesplit-one/default.nix
@@ -1,10 +1,11 @@
-{ lib
-, fetchFromGitHub
-, rustPlatform
-, cmake
-, fontconfig
-, obs-studio
-, pkg-config
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  cmake,
+  fontconfig,
+  obs-studio,
+  pkg-config,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -18,15 +19,17 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-3B3P1PlzIlpVqHJMKWpEnWXGgD/IaiWM1FVKn0BtRj0=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "livesplit-auto-splitting-0.1.0" = "sha256-/xQEVJH6m6nH5Z1kuOPEElOcOqJmiG9Q8cOx0e6p3Wc=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-JHocpqDeF24Qn9lUr+YCnZqgckLhGRpWQD7WGCxVmd8=";
 
-  nativeBuildInputs = [ cmake pkg-config ];
-  buildInputs = [ fontconfig obs-studio ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+  buildInputs = [
+    fontconfig
+    obs-studio
+  ];
 
   postInstall = ''
     mkdir $out/lib/obs-plugins/
@@ -36,7 +39,10 @@ rustPlatform.buildRustPackage rec {
   meta = with lib; {
     description = "OBS Studio plugin for adding LiveSplit One as a source";
     homepage = "https://github.com/LiveSplit/obs-livesplit-one";
-    license = with licenses; [ asl20 mit ];
+    license = with licenses; [
+      asl20
+      mit
+    ];
     maintainers = [ maintainers.Bauke ];
     platforms = obs-studio.meta.platforms;
   };
