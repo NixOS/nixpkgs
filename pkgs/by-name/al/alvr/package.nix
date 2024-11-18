a7e26dce08de7755815cde5f7e8a48b1c12483b6
wxt <3264117476@qq.com>
2024-11-18 23:42:24 +0000

    alvr: use new cargo fetcher

--- pkgs/by-name/al/alvr/package.nix
+++ pkgs/by-name/al/alvr/package.nix
@@ -50,13 +50,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-zqeh9U0A/KHlRieq9Lf+7f04K3JG/vpE2gZ916ReXLc=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "openxr-0.19.0" = "sha256-bnMSjJh+zjLw4Pdxr7LLm6qYAJOK7hz5xORKZ2pVcGw=";
-      "settings-schema-0.2.0" = "sha256-luEdAKDTq76dMeo5kA+QDTHpRMFUg3n0qvyQ7DkId0k=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-m0Cm8xKIdUtMy2+T6duPsB6aYm+vV7o9e+jrvQQfeSM=";
 
   patches = [
     (substituteAll {
