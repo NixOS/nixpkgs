e4295f63656a73cd09d29ad303d961f9cbab1126
wxt <3264117476@qq.com>
2024-11-18 11:19:24 +0000

    ajour: use new cargo fetcher

--- pkgs/tools/games/ajour/default.nix
+++ pkgs/tools/games/ajour/default.nix
@@ -1,26 +1,27 @@
-{ stdenv
-, lib
-, fetchFromGitHub
-, rustPlatform
-, autoPatchelfHook
-, cmake
-, makeWrapper
-, pkg-config
-, python3
-, expat
-, freetype
-, kdialog
-, zenity
-, openssl
-, libglvnd
-, libX11
-, libxcb
-, libXcursor
-, libXi
-, libxkbcommon
-, libXrandr
-, vulkan-loader
-, wayland
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  autoPatchelfHook,
+  cmake,
+  makeWrapper,
+  pkg-config,
+  python3,
+  expat,
+  freetype,
+  kdialog,
+  zenity,
+  openssl,
+  libglvnd,
+  libX11,
+  libxcb,
+  libXcursor,
+  libXi,
+  libxkbcommon,
+  libXrandr,
+  vulkan-loader,
+  wayland,
 }:
 
 let
@@ -35,7 +36,8 @@ let
     wayland
   ];
 
-in rustPlatform.buildRustPackage rec {
+in
+rustPlatform.buildRustPackage rec {
   pname = "ajour";
   version = "1.3.2";
 
@@ -46,12 +48,8 @@ in rustPlatform.buildRustPackage rec {
     sha256 = "sha256-oVaNLclU0EVNtxAASE8plXcC+clkwhBeb9pz1vXufV0=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "iced-0.3.0" = "sha256-cPQ0qqcdCMx2agSpAKSvVDBEoF/vUffGg1UkX85KmfY=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-/bmPqKxhPt9AGHRPGPE8JzaZKcTAWIRzvR3FiTCVMho=";
 
   nativeBuildInputs = [
     autoPatchelfHook
@@ -78,7 +76,12 @@ in rustPlatform.buildRustPackage rec {
 
   fixupPhase = ''
     patchelf --set-rpath "${lib.makeLibraryPath rpathLibs}:$(patchelf --print-rpath $out/bin/ajour)" $out/bin/ajour
-    wrapProgram $out/bin/ajour --prefix PATH ":" ${lib.makeBinPath [ zenity kdialog ]}
+    wrapProgram $out/bin/ajour --prefix PATH ":" ${
+      lib.makeBinPath [
+        zenity
+        kdialog
+      ]
+    }
   '';
 
   meta = with lib; {
