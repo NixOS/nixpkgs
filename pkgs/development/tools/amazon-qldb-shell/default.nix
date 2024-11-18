d652bc4607f7e27979000e868f726cf452b15a3c
wxt <3264117476@qq.com>
2024-11-18 09:12:31 +0000

    amazon-qldb-shell: use new cargo fetcher

--- pkgs/development/tools/amazon-qldb-shell/default.nix
+++ pkgs/development/tools/amazon-qldb-shell/default.nix
@@ -27,12 +27,8 @@ let
     ];
     buildInputs = lib.optional stdenv.hostPlatform.isDarwin Security;
 
-    cargoLock = {
-      lockFile = ./Cargo.lock;
-      outputHashes = {
-        "amazon-qldb-driver-0.1.0" = "sha256-az0rANBcryHHnpGWvo15TGGW4KMUULZHaj5msIHts14=";
-      };
-    };
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-tD35Py81QLDVlBahYzgskOQK5lQW03xuCnUwVUi4oLU=";
 
     passthru.tests.version = testers.testVersion { inherit package; };
 
