b39217d46878624de1c56a71ec3772f15177bdc6
wxt <3264117476@qq.com>
2024-11-18 06:41:20 +0000

    gurk-rs: use new cargo fetcher

--- pkgs/applications/networking/instant-messengers/gurk-rs/default.nix
+++ pkgs/applications/networking/instant-messengers/gurk-rs/default.nix
@@ -1,12 +1,13 @@
-{ stdenv
-, lib
-, protobuf
-, rustPlatform
-, fetchFromGitHub
-, Cocoa
-, pkgsBuildHost
-, openssl
-, pkg-config
+{
+  stdenv,
+  lib,
+  protobuf,
+  rustPlatform,
+  fetchFromGitHub,
+  Cocoa,
+  pkgsBuildHost,
+  openssl,
+  pkg-config,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -24,23 +25,20 @@ rustPlatform.buildRustPackage rec {
     rm .cargo/config.toml
   '';
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "libsignal-protocol-0.1.0" = "sha256-4aHINlpVAqVTtm7npwXQRutZUmIxYgkhXhApg7jSM4M=";
-      "libsignal-service-0.1.0" = "sha256-AOGw76A9R2qH3hc7B+MBE3okzW8b5LTZdepzUDOv9lM=";
-      "curve25519-dalek-4.1.3" = "sha256-bPh7eEgcZnq9C3wmSnnYv0C4aAP+7pnwk9Io29GrI4A=";
-      "presage-0.6.2" = "sha256-t9t8ecPtefI/jYlk+Ul8WdgH26VJIkfMptbKxprekS0=";
-      "qr2term-0.3.1" = "sha256-U8YLouVZTtDwsvzZiO6YB4Pe75RXGkZXOxHCQcCOyT8=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-s/Z9NEdXtLyzvb22Je9hKLkmOxsvI/qoEHlcNbeBY+0=";
 
-  nativeBuildInputs = [ protobuf pkg-config ];
+  nativeBuildInputs = [
+    protobuf
+    pkg-config
+  ];
 
-  buildInputs = [ openssl ]
-    ++ lib.optionals stdenv.hostPlatform.isDarwin [ Cocoa ];
+  buildInputs = [ openssl ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ Cocoa ];
 
-  NIX_LDFLAGS = lib.optionals (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64) [ "-framework" "AppKit" ];
+  NIX_LDFLAGS = lib.optionals (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64) [
+    "-framework"
+    "AppKit"
+  ];
 
   PROTOC = "${pkgsBuildHost.protobuf}/bin/protoc";
 
