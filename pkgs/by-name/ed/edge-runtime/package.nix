916c2ca2a3d7c6aea44ad9e50273d684509c010b
wxt <3264117476@qq.com>
2024-11-18 23:37:56 +0800

    edge-runtime: use new cargo fetcher

--- pkgs/by-name/ed/edge-runtime/package.nix
+++ pkgs/by-name/ed/edge-runtime/package.nix
@@ -25,14 +25,8 @@ rustPlatform.buildRustPackage {
     fetchSubmodules = true;
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "deno_core-0.293.0" = "sha256-5WBORHokzWPdro9rmBijcsx/j81YFaEfMIK276bfI1o=";
-      "eszip-0.72.2" = "sha256-lzJeapA5NTWBnBaQY8i82OeYN2DIukJZKoRAXFG4qVA=";
-      "ort-2.0.0-rc.2" = "sha256-jOKeulnW/m+q1xf77jaNzLT+In/AFcSDDbz7xF23x5g=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-BMM2wnBE7eUwGU1uWKhZFDnj0Ehhx5oocvcmFaUTO80=";
 
   nativeBuildInputs = [
     pkg-config
