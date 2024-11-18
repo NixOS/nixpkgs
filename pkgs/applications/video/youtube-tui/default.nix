5e9853b17b47a6edd7d690185eeb3d4ec78d8416
wxt <3264117476@qq.com>
2024-11-18 09:09:53 +0000

    youtube-tui: use new cargo fetcher

--- pkgs/applications/video/youtube-tui/default.nix
+++ pkgs/applications/video/youtube-tui/default.nix
@@ -1,17 +1,18 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-, pkg-config
-, openssl
-, xorg
-, stdenv
-, python3
-, makeBinaryWrapper
-, libsixel
-, mpv
-, CoreFoundation
-, Security
-, AppKit
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  xorg,
+  stdenv,
+  python3,
+  makeBinaryWrapper,
+  libsixel,
+  mpv,
+  CoreFoundation,
+  Security,
+  AppKit,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -25,12 +26,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-FOiK3yQcQuwdCEjBtRPW4iBd+8uNsvZ6l5tclHVzL+M=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "libmpv-2.0.1" = "sha256-efbXk0oXkzlIqgbP4wKm7sWlVZBT2vzDSN3iwsw2vL0=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-hzCVMP8RDpdwK6OlYKBvKmJXPImaeevdAFRG0fRa780=";
 
   nativeBuildInputs = [
     pkg-config
@@ -38,21 +35,23 @@ rustPlatform.buildRustPackage rec {
     makeBinaryWrapper
   ];
 
-  buildInputs = [
-    openssl
-    xorg.libxcb
-    libsixel
-    mpv
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    CoreFoundation
-    Security
-    AppKit
-  ];
+  buildInputs =
+    [
+      openssl
+      xorg.libxcb
+      libsixel
+      mpv
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      CoreFoundation
+      Security
+      AppKit
+    ];
 
   # sixel-sys is dynamically linked to libsixel
   postInstall = lib.optionalString stdenv.hostPlatform.isDarwin ''
     wrapProgram $out/bin/youtube-tui \
-      --prefix DYLD_LIBRARY_PATH : "${lib.makeLibraryPath [libsixel]}"
+      --prefix DYLD_LIBRARY_PATH : "${lib.makeLibraryPath [ libsixel ]}"
   '';
 
   meta = with lib; {
