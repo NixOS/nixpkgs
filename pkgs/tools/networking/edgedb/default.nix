48b12bd867d468a1c50b191057d87445b253c637
wxt <3264117476@qq.com>
2024-11-18 10:44:28 +0000

    edgedb: use new cargo fetcher

--- pkgs/tools/networking/edgedb/default.nix
+++ pkgs/tools/networking/edgedb/default.nix
@@ -26,18 +26,8 @@ rustPlatform.buildRustPackage rec {
     fetchSubmodules = true;
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "edgedb-derive-0.5.2" = "sha256-5nvpkmTRcGO4a/Mc+qLp2+u9bWSnHQ/1NT2FW9ii0AU=";
-      "edgeql-parser-0.1.0" = "sha256-dhLwBW4ellai9R9TjlJ/qEhZQRXE9D/+QxZsq3I9PRk=";
-      "rexpect-0.5.0" = "sha256-vstAL/fJWWx7WbmRxNItKpzvgGF3SvJDs5isq9ym/OA=";
-      "scram-0.7.0" = "sha256-QTPxyXBpMXCDkRRJEMYly1GKp90khrwwuMI1eHc2H+Y=";
-      "serde_str-1.0.0" = "sha256-CMBh5lxdQb2085y0jc/DrV6B8iiXvVO2aoZH/lFFjak=";
-      "test-utils-0.1.0" = "sha256-FoF/U89Q9E2Dlmpoh+cfDcScmhcsSNut+rE7BECJSJI=";
-      "warp-0.3.6" = "sha256-knDt2aw/PJ0iabhKg+okwwnEzCY+vQVhE7HKCTM6QbE=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-5BSj0dwUGpKeBKg9+RGs0OUszBNBd/8dT05QAe6JnDA=";
 
   nativeBuildInputs = [
     makeBinaryWrapper
