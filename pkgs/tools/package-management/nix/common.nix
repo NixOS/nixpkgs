b3676d1ee144d34a58821d7584a5403eee9b780f
Jörg Thalheim <joerg@thalheim.io>
2024-11-18 20:13:40 +0100

    nixVersions.nix_2_25: add missing python build dep for mdbook
    Needed for
    https://github.com/NixOS/nix/blob/master/doc/manual/substitute.py

--- pkgs/tools/package-management/nix/common.nix
+++ pkgs/tools/package-management/nix/common.nix
@@ -21,6 +21,7 @@ let
   atLeast220 = lib.versionAtLeast version "2.20pre";
   atLeast221 = lib.versionAtLeast version "2.21pre";
   atLeast224 = lib.versionAtLeast version "2.24pre";
+  atLeast225 = lib.versionAtLeast version "2.25pre";
   # Major.minor versions unaffected by CVE-2024-27297
   unaffectedByFodSandboxEscape = [
     "2.3"
@@ -72,6 +73,7 @@ in
 , nixVersions
 , openssl
 , perl
+, python3
 , pkg-config
 , rapidcheck
 , Security
@@ -151,6 +153,8 @@ self = stdenv.mkDerivation {
     libgit2
   ] ++ lib.optionals (atLeast224 || lib.versionAtLeast version "pre20240626") [
     toml11
+  ] ++ lib.optionals (atLeast225 && enableDocumentation) [
+    python3
   ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
     Security
   ] ++ lib.optionals (stdenv.hostPlatform.isx86_64) [
