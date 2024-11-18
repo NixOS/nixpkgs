a7598427cb042fafc3d86ed65370ca8a940057d4
Alyssa Ross <hi@alyssa.is>
2024-11-18 15:04:27 +0100

    linuxHeaders: 6.10 -> 6.12

--- pkgs/os-specific/linux/kernel-headers/default.nix
+++ pkgs/os-specific/linux/kernel-headers/default.nix
@@ -111,12 +111,12 @@ let
 in {
   inherit makeLinuxHeaders;
 
-  linuxHeaders = let version = "6.10"; in
+  linuxHeaders = let version = "6.12"; in
     makeLinuxHeaders {
       inherit version;
       src = fetchurl {
         url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
-        hash = "sha256-d0aYQi7lTF8ecERW83xlwGtRtOmosIZvNFgNhv744iY=";
+        hash = "sha256-saJWK+VuQq+z+EidTCp6xHKsIwmPHvHB5A2mAfVGJes=";
       };
       patches = [
         ./no-relocs.patch # for building x86 kernel headers on non-ELF platforms
