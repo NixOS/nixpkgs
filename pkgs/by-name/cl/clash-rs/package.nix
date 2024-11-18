5d34f5cc1bcecf478317c0d9b8570c75beee67c4
wxt <3264117476@qq.com>
2024-11-18 05:18:55 +0000

    clash-rs: use new cargo fetcher

--- pkgs/by-name/cl/clash-rs/package.nix
+++ pkgs/by-name/cl/clash-rs/package.nix
@@ -16,19 +16,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-0deMVI51XHTCrnLTycqDsaY5Lq+wx14uMUlkG5OViNA=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "boringtun-0.6.0" = "sha256-HBNo53b+CpCGmTXZYH4NBBvNmekyaBKAk1pSRzZdavg=";
-      "netstack-lwip-0.3.4" = "sha256-lcauDyaw5gAaECRcGNXQDHbWmnyxil18qWFkZ/p/C50=";
-      "rustls-0.23.12" = "sha256-grt94JG44MljRQRooVZbXL4h4XLI1/KoIdwGv03MoIU=";
-      "tokio-rustls-0.26.0" = "sha256-Bmi36j8hbR4kkY/xnHbluaInk+YH5/eTln0VYfHulGA=";
-      "tracing-oslog-0.2.0" = "sha256-JYaCslbVOgsyBhjeBkplPWcjSgFccjr4s6OAGIUu5kg=";
-      "tuic-1.3.1" = "sha256-WMd+O2UEu0AEI+gNeQtdBhEgIB8LPanoIpMcDAUUWrM=";
-      "tun-0.6.1" = "sha256-j4yQSu4Mw7DBFak8vJGQomYq81+pfaeEDdN4NNBve+E=";
-      "unix-udp-sock-0.7.0" = "sha256-TekBfaxecFPpOfq7PVjLHwc0uIp3yJGV/Cgav5VfKaA=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-OEr+APs6WIQZ+L+1mWNqTOHzH8JPAOqMwotMOpKVdzg=";
 
   env = {
     PROTOC = "${protobuf}/bin/protoc";
