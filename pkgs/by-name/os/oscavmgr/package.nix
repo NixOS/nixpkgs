3ecd66bc5e0d03c53c13fbb2f7058bc4db88f8be
wxt <3264117476@qq.com>
2024-11-18 22:59:12 +0800

    oscavmgr: use new cargo fetcher

--- pkgs/by-name/os/oscavmgr/package.nix
+++ pkgs/by-name/os/oscavmgr/package.nix
@@ -20,14 +20,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-mOa9eUI/p0ErePza6wXy1jUcHg5Q9tvC7/lThQabU94=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "alvr_common-20.11.1" = "sha256-d4KldPii8W1HcfnMSD8Fn+IGO/a3r8747APPjRCnbe8=";
-      "openxr-0.19.0" = "sha256-kbEYoN4UvUEaZA9LJWEKx1X1r+l91GjTWs1hNXhr7cw=";
-      "settings-schema-0.2.0" = "sha256-luEdAKDTq76dMeo5kA+QDTHpRMFUg3n0qvyQ7DkId0k=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-u1uoXKslRe3yIKYnPvhzfSswEZ2T/J8w1KRBQjciVrM=";
 
   nativeBuildInputs = [
     pkg-config
