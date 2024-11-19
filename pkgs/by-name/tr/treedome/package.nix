2ae70e260cc33275f600908f717c982b8e6e268e
wxt <3264117476@qq.com>
2024-11-19 00:12:51 +0000

    treedome: use new cargo fetcher

--- pkgs/by-name/tr/treedome/package.nix
+++ pkgs/by-name/tr/treedome/package.nix
@@ -61,12 +61,8 @@ rustPlatform.buildRustPackage {
   inherit version pname src;
   sourceRoot = "${src.name}/src-tauri";
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "fix-path-env-0.0.0" = "sha256-ewE3CwqLC8dvi94UrQsWbp0mjmrzEJIGPDYtdmQ/sGs=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-TI8GSLHD1qJ6zzG4nt0Yz0RHJVaF6hjUwE8N8Y1uHmk=";
 
   env = {
     VERGEN_GIT_DESCRIBE = version;
