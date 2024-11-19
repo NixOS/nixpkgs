3bb5ff0ceb41d527e15e43ef6b9933cf72dd8e40
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-11-19 17:28:49 +0000

    libretro.mame2010: move to retroarch/cores

--- pkgs/applications/emulators/retroarch/cores.nix
+++ pkgs/applications/emulators/retroarch/cores.nix
@@ -177,16 +177,7 @@ lib.makeScope newScope (self: rec {
 
   mame2003-plus = self.callPackage ./cores/mame2003-plus.nix {  };
 
-  mame2010 = mkLibretroCore {
-    core = "mame2010";
-    makefile = "Makefile";
-    makeFlags = lib.optionals stdenv.hostPlatform.isAarch64 [ "PTR64=1" "ARM_ENABLED=1" "X86_SH2DRC=0" "FORCE_DRC_C_BACKEND=1" ];
-    meta = {
-      description = "Port of MAME ~2010 to libretro, compatible with MAME 0.139 sets";
-      # MAME license, non-commercial clause
-      license = lib.licenses.unfreeRedistributable;
-    };
-  };
+  mame2010 = self.callPackage ./cores/mame2010.nix {  };
 
   mame2015 = mkLibretroCore {
     core = "mame2015";
