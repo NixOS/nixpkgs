7bde3a9c47919986c0ad389cdb8000016b271603
Maximilian Bosch <maximilian@mbosch.me>
2024-11-19 23:27:51 +0100

    grafana: 10.4.12 -> 10.4.13
    ChangeLog: https://github.com/grafana/grafana/releases/tag/v10.4.13

--- pkgs/servers/monitoring/grafana/default.nix
+++ pkgs/servers/monitoring/grafana/default.nix
@@ -28,7 +28,7 @@ let
 in
 buildGoModule rec {
   pname = "grafana";
-  version = "10.4.12";
+  version = "10.4.13";
 
   subPackages = [ "pkg/cmd/grafana" "pkg/cmd/grafana-server" "pkg/cmd/grafana-cli" ];
 
@@ -36,7 +36,7 @@ buildGoModule rec {
     owner = "grafana";
     repo = "grafana";
     rev = "v${version}";
-    hash = "sha256-DYuLOBNapNgosJDc5t5gG0NgkjRJ8l78x520JJ0/x4Q=";
+    hash = "sha256-nJpZSW5eHleBZjPoPScMmXl9BAqUnEGrKUu/UJzHEfI=";
   };
 
   # borrowed from: https://github.com/NixOS/nixpkgs/blob/d70d9425f49f9aba3c49e2c389fe6d42bac8c5b0/pkgs/development/tools/analysis/snyk/default.nix#L20-L22
@@ -82,7 +82,7 @@ buildGoModule rec {
 
   disallowedRequisites = [ offlineCache ];
 
-  vendorHash = "sha256-gP/lW6CXQpBv9dPRTOV5+vmaqTcq+91X2jls9gDrnm0=";
+  vendorHash = "sha256-Md2MgaI7PluJFbz11t9YABLi3S4LVc35/7SIF6hY7ag=";
 
   proxyVendor = true;
 
