48b5772ed4653fbea1b9401da591051e5cc6fe4d
wxt <3264117476@qq.com>
2024-11-19 00:29:13 +0000

    chroot-realpath: use new cargo fetcher

--- pkgs/by-name/ch/chroot-realpath/package.nix
+++ pkgs/by-name/ch/chroot-realpath/package.nix
@@ -12,7 +12,8 @@ rustPlatform.buildRustPackage {
 
   src = ./src;
 
-  cargoLock.lockFile = ./src/Cargo.lock;
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-W3Hpeuw+rMY5uXS87h9P2tae3fOSjriztehTrbfnjZE=";
 
   meta = {
     description = "Output a path's realpath within a chroot.";
