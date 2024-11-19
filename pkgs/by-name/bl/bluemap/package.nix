4ec42ff88a249faac5db6ee688aff5ec54d5f956
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-19 05:58:30 +0000

    bluemap: 5.4 -> 5.5

--- pkgs/by-name/bl/bluemap/package.nix
+++ pkgs/by-name/bl/bluemap/package.nix
@@ -2,11 +2,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "bluemap";
-  version = "5.4";
+  version = "5.5";
 
   src = fetchurl {
     url = "https://github.com/BlueMap-Minecraft/BlueMap/releases/download/v${version}/BlueMap-${version}-cli.jar";
-    hash = "sha256-ycgCYot3lTdkufJdOSX/PMWI2fnAWz8f5I9IWZpKppw=";
+    hash = "sha256-dm2WLWDbbrsQIfD+gbRTreJNbZryEHgejskxlXL/I9E=";
   };
 
   dontUnpack = true;
