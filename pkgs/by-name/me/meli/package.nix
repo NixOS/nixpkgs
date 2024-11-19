60ee8fb18c2754be640e1c7337bf601bf3b3556b
Erlend Hamberg <erlend@hamberg.no>
2024-11-19 19:30:24 +0100

    meli: 0.8.7 -> 0.8.8

--- pkgs/by-name/me/meli/package.nix
+++ pkgs/by-name/me/meli/package.nix
@@ -24,7 +24,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "meli";
-  version = "0.8.7";
+  version = "0.8.8";
 
   src = fetchzip {
     urls = [
@@ -32,19 +32,20 @@ rustPlatform.buildRustPackage rec {
       "https://codeberg.org/meli/meli/archive/v${version}.tar.gz"
       "https://github.com/meli/meli/archive/refs/tags/v${version}.tar.gz"
     ];
-    hash = "sha256-2+JIehi2wuWdARbhFPvNPIJ9ucZKWjNSORszEG9lyjw=";
+    hash = "sha256-XOUOIlFKxI7eL7KEEfLyYTsNqc2lc9sJNt9RqPavuW8=";
   };
 
-  cargoHash = "sha256-ZVhUkpiiPKbWcf56cXFgn3Nyr63STHLlD7mpYEetNIY=";
-
-  cargoPatches = [
+    cargoPatches = [
     (fetchpatch {
-      # https://github.com/NixOS/nixpkgs/issues/332957#issuecomment-2278578811
-      name = "fix-rust-1.80-compat.patch";
-      url = "https://git.meli-email.org/meli/meli/commit/6b05279a0987315c401516cac8ff0b016a8e02a8.patch";
-      hash = "sha256-mh8H7wmHMXAe01UTvdY8vJeeLyH6ZFwylNLFFL+4LO0=";
+      # https://git.meli-email.org/meli/meli/issues/522
+      # https://git.meli-email.org/meli/meli/issues/524
+      name = "fix test_fd_locks() on platforms without OFD support";
+      url = "https://git.meli-email.org/meli/meli/commit/b7e215f9c238f8364e2a1f0d10ac668d0cfe91ad.patch";
+      hash = "sha256-227vnFuxhQ0Hh5A/J8y7Ei89AxbNXReMn3c3EVRN4Tc=";
     })
   ];
+
+  cargoHash = "sha256-SMvpmWEHUWo0snR/DiUmfZJnXy1QtVOowO8CErM9Xjg=";
 
   # Needed to get openssl-sys to use pkg-config
   OPENSSL_NO_VENDOR=1;
@@ -81,9 +82,7 @@ rustPlatform.buildRustPackage rec {
   '';
 
   checkFlags = [
-    "--skip=conf::tests::test_config_parse"            # panicking due to sandbox
-    "--skip=utils::tests::test_shellexpandtrait_impls" # panicking due to sandbox
-    "--skip=utils::tests::test_shellexpandtrait"       # panicking due to sandbox
+    "--skip=test_cli_subcommands" # panicking due to sandbox
   ];
 
   meta = with lib; {
@@ -93,6 +92,6 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://meli.delivery";
     license = licenses.gpl3;
     maintainers = with maintainers; [ _0x4A6F matthiasbeyer ];
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.darwin;
   };
 }
