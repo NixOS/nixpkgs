4d5c03fa7fec2b6d9a4d55fe7435d61db2d6ac00
eljamm <fedi.jamoussi@protonmail.ch>
2024-11-19 16:07:21 +0100

    omnom: 0-unstable-2024-08-29 -> 0-unstable-2024-10-01

--- pkgs/by-name/om/omnom/package.nix
+++ pkgs/by-name/om/omnom/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule {
   pname = "omnom";
-  version = "0-unstable-2024-08-29";
+  version = "0-unstable-2024-10-01";
 
   src = fetchFromGitHub {
     owner = "asciimoo";
     repo = "omnom";
-    rev = "1fcd7787886503f703bbcd31b193d4c93acc5610";
-    hash = "sha256-o/n8rgngQkYEn8J0aFpCiD4qrWVFaaa305OxiscU6+8=";
+    rev = "5e82dd40a36b38572b12fa8cbee6097bb40b499d";
+    hash = "sha256-IhiiabJZi+UFvtmgfxLBeuUghB64qiaR4zk3Yotld7g=";
     fetchSubmodules = true;
   };
 
@@ -33,7 +33,7 @@ buildGoModule {
     # `db.sqlite3` in a temporary directory since they need to be writeable.
     #
     # NOTE: Currently, `static/data` only holds the snapshots directory.
-    substituteInPlace config.yml \
+    substituteInPlace config.yml_sample \
       --replace-fail 'root: "./static/data"' 'root: "/tmp/omnom/static/data"' \
       --replace-fail 'connection: "./db.sqlite3"' 'connection: "/tmp/omnom/db.sqlite3"' \
       --replace-fail 'debug: true' 'debug: false'
@@ -41,7 +41,7 @@ buildGoModule {
 
   postInstall = ''
     mkdir -p $out/share
-    cp -r config.yml static templates $out/share
+    cp -r config.yml_sample static templates $out/share
 
     wrapProgram $out/bin/omnom \
       --chdir $out/share \
