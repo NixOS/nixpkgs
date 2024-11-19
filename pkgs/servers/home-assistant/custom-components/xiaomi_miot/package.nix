6da5d4e441a71adf4ee119a3c6e1aeae151d1284
Zhong Jianxin <azuwis@gmail.com>
2024-11-19 20:31:02 +0800

    home-assistant-custom-components.xiaomi_miot: add meta.longDescription, remove `with lib`
    (cherry picked from commit c4c6f50302096e7957a17b35b05ec9c4c4db6298)

--- pkgs/servers/home-assistant/custom-components/xiaomi_miot/package.nix
+++ pkgs/servers/home-assistant/custom-components/xiaomi_miot/package.nix
@@ -30,11 +30,24 @@ buildHomeAssistantComponent rec {
 
   passthru.updateScript = nix-update-script { };
 
-  meta = with lib; {
+  meta = {
     changelog = "https://github.com/al-one/hass-xiaomi-miot/releases/tag/v${version}";
     description = "Automatic integrate all Xiaomi devices to HomeAssistant via miot-spec, support Wi-Fi, BLE, ZigBee devices";
+    longDescription = ''
+      Xiaomi Miot For HomeAssistant depends on `ffmpeg` and `homekit`, example how to setup in NixOS `configuration.nix`:
+
+      ```
+      { config, lib, pkgs, ... }:
+      {
+        services.home-assistant = {
+          customComponents = [ pkgs.home-assistant-custom-components.xiaomi_miot ];
+          extraComponents = [ "ffmpeg" "homekit" ];
+        };
+      }
+      ```
+    '';
     homepage = "https://github.com/al-one/hass-xiaomi-miot";
-    maintainers = with maintainers; [ azuwis ];
-    license = licenses.asl20;
+    maintainers = with lib.maintainers; [ azuwis ];
+    license = lib.licenses.asl20;
   };
 }
