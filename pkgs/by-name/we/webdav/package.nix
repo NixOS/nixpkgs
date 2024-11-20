f1bf4d74ef99397b15d17167656886d5a416586c
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-11-20 18:40:58 +0100

    webdav: 5.4.2 -> 5.4.3
    Diff: https://github.com/hacdias/webdav/compare/v5.4.2...v5.4.3

--- pkgs/by-name/we/webdav/package.nix
+++ pkgs/by-name/we/webdav/package.nix
@@ -2,13 +2,13 @@
 
 buildGo123Module rec {
   pname = "webdav";
-  version = "5.4.2";
+  version = "5.4.3";
 
   src = fetchFromGitHub {
     owner = "hacdias";
     repo = "webdav";
     rev = "v${version}";
-    sha256 = "sha256-a1XAlmEW8gNTgVZiJ200CP/wvbwvyv4Lc3L6KP/4Mik=";
+    sha256 = "sha256-ASc+ioVBpCFESEryI0EwKYZln1JzPCOKLJJWmh7L8oA=";
   };
 
   vendorHash = "sha256-d8WauJ1i429dr79iHgrbFRZCmx+W6OobSINy8aNGG6w=";
