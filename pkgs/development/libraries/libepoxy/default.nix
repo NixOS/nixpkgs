07f8a2abe573ce12801c110e47d86bf2567d4936
Jordan Williams <jordan@jwillikers.com>
2024-11-20 07:57:56 -0600

    libepoxy: fix overuse of with lib

--- pkgs/development/libraries/libepoxy/default.nix
+++ pkgs/development/libraries/libepoxy/default.nix
@@ -92,12 +92,12 @@ stdenv.mkDerivation (finalAttrs: {
     };
   };
 
-  meta = with lib; {
+  meta = {
     description = "Library for handling OpenGL function pointer management";
     homepage = "https://github.com/anholt/libepoxy";
-    license = licenses.mit;
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ jwillikers ];
-    platforms = platforms.unix;
+    platforms = lib.platforms.unix;
     pkgConfigModules = [ "epoxy" ];
   };
 })
