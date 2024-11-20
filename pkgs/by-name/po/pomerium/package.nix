511ae6d1787e96c0c89e0d73380bd35717d35d26
Morgan Helton <mhelton@gmail.com>
2024-11-20 05:20:14 -0600

    pomerium: 0.27.2 -> 0.28.0

--- pkgs/by-name/po/pomerium/package.nix
+++ pkgs/by-name/po/pomerium/package.nix
@@ -13,15 +13,15 @@ let
 in
 buildGo123Module rec {
   pname = "pomerium";
-  version = "0.27.2";
+  version = "0.28.0";
   src = fetchFromGitHub {
     owner = "pomerium";
     repo = "pomerium";
     rev = "v${version}";
-    hash = "sha256-t1j5usgr/SO3Ev3JpCJWb3Ys8wgZUTGQVb6mo0oIsEc=";
+    hash = "sha256-Uj/mVklFRaoDNQjCFS5NW/AhSU+7V1XxPiZBAUuly7s=";
   };
 
-  vendorHash = "sha256-nTEFSLP0/GUVgtujVG6lQIxnj6DOEifc0MVh9CNxt8s=";
+  vendorHash = "sha256-s6EZUZoGNBpy5RaLAPiCCCVFli+YzZ0PHJ/aH3s4APA=";
 
   ui = mkYarnPackage {
     inherit version;
