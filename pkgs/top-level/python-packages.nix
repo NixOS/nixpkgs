717d47f31e7f4918ba57e4fd23542dced57537aa
Antoine VIALLON <antoine.viallon@thalesgroup.com>
2024-11-20 17:39:42 +0100

    python3Packages.snmpsim: init at 1.1.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14779,6 +14779,8 @@ self: super: with self; {
 
   snitun = callPackage ../development/python-modules/snitun { };
 
+  snmpsim = callPackage ../development/python-modules/snmpsim { };
+
   snorkel = callPackage ../development/python-modules/snorkel { };
 
   snowballstemmer = callPackage ../development/python-modules/snowballstemmer { };
