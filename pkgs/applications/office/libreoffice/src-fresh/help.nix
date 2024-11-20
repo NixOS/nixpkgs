e7766d29b5dc42a6ae9cd9fe8227b8cfb3067d6a
K900 <me@0upti.me>
2024-11-20 15:51:08 +0300

    libreoffice-fresh: 24.8.2.1 -> 24.8.3.2

--- pkgs/applications/office/libreoffice/src-fresh/help.nix
+++ pkgs/applications/office/libreoffice/src-fresh/help.nix
@@ -1,5 +1,5 @@
 { fetchurl, ... }:
 fetchurl {
-  sha256 = "1xlfs1380h9axqx37kp9nwq4bwlg08rm136ayzglaz57vx87vxsg";
-  url = "https://download.documentfoundation.org/libreoffice/src/24.8.2/libreoffice-help-24.8.2.1.tar.xz";
+  sha256 = "04z77jzn816r19ig54fhl0p1arh3fpamn5bh2wc2v17rz73chp8y";
+  url = "https://download.documentfoundation.org/libreoffice/src/24.8.3/libreoffice-help-24.8.3.2.tar.xz";
 }
