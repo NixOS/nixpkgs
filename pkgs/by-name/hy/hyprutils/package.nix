6bf67e7301695bd7f8ac9660978dd72b705b9175
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-20 04:40:17 +0000

    hyprutils: 0.2.5 -> 0.2.6

--- pkgs/by-name/hy/hyprutils/package.nix
+++ pkgs/by-name/hy/hyprutils/package.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "hyprutils";
-  version = "0.2.5";
+  version = "0.2.6";
 
   src = fetchFromGitHub {
     owner = "hyprwm";
     repo = "hyprutils";
     rev = "refs/tags/v${finalAttrs.version}";
-    hash = "sha256-SO/9G2h5nXCnMahTP9m46eqsu0Lr8A60Fhc0ABkx0Y0=";
+    hash = "sha256-+JeO9gevnXannQxMfR5xzZtF4sYmSlWkX/BPmPx0mWk=";
   };
 
   nativeBuildInputs = [
