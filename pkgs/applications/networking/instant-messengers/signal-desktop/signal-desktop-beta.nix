f8c5ffa59f340ae03b339e6db75262cd859edc9d
teutat3s <10206665+teutat3s@users.noreply.github.com>
2024-11-20 16:43:14 +0100

    signal-desktop-beta: drop
    The beta increases the complexity of the signal-desktop package for
    maintainers and releases of signal-desktop happen very frequently.

--- pkgs/applications/networking/instant-messengers/signal-desktop/signal-desktop-beta.nix
+++
@@ -1,8 +0,0 @@
-{ callPackage }:
-callPackage ./generic.nix { } rec {
-  pname = "signal-desktop-beta";
-  dir = "Signal Beta";
-  version = "7.34.0-beta.2";
-  url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop-beta/signal-desktop-beta_${version}_amd64.deb";
-  hash = "sha256-fwSZ0j2iHJUhRc2MZY5Cj6guoAST3XfrXFnj8uUCl9k=";
-}
