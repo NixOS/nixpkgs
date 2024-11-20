46ab30c61f7bf51f5b1446498684b78c4e21178f
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-11-20 13:11:20 +0000

    libretro.play: move to retroarch/cores

--- pkgs/applications/emulators/retroarch/cores.nix
+++ pkgs/applications/emulators/retroarch/cores.nix
@@ -218,21 +218,7 @@ lib.makeScope newScope (self: rec {
 
   picodrive = self.callPackage ./cores/picodrive.nix {  };
 
-  play = mkLibretroCore {
-    core = "play";
-    extraBuildInputs = [ boost bzip2 curl openssl icu libGL libGLU xorg.libX11 ];
-    extraNativeBuildInputs = [ cmake ];
-    makefile = "Makefile";
-    cmakeFlags = [ "-DBUILD_PLAY=OFF" "-DBUILD_LIBRETRO_CORE=ON" ];
-    postBuild = "cd Source/ui_libretro";
-    # FIXME: workaround the following GCC 13 error:
-    # error: 'printf' was not declared in this scop
-    CXXFLAGS = "-include cstdio";
-    meta = {
-      description = "Port of Play! to libretro";
-      license = lib.licenses.bsd2;
-    };
-  };
+  play = self.callPackage ./cores/play.nix {  };
 
   ppsspp = mkLibretroCore {
     core = "ppsspp";
