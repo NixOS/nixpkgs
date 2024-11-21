5378b5f6f408a15ffe19d7e80a0275571f031eb9
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-21 10:33:05 +0000

    python312Packages.mkdocs-material: 9.5.39 -> 9.5.45

--- pkgs/development/python-modules/mkdocs-material/default.nix
+++ pkgs/development/python-modules/mkdocs-material/default.nix
@@ -28,7 +28,7 @@
 
 buildPythonPackage rec {
   pname = "mkdocs-material";
-  version = "9.5.39";
+  version = "9.5.45";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -37,7 +37,7 @@ buildPythonPackage rec {
     owner = "squidfunk";
     repo = "mkdocs-material";
     rev = "refs/tags/${version}";
-    hash = "sha256-ArCd7NbqvPw3kHJd4MG62FplgXwW1gFTfdCHZqfxuqU=";
+    hash = "sha256-uhUatalHcObQbar1xbdR44FnQs/+qS4ad0sdB2jH/OU=";
   };
 
   nativeBuildInputs = [
