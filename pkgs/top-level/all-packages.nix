d8efb6fdb06c549c2614f8030e83e5b2232556db
liberodark <liberodark@gmail.com>
2024-11-21 08:20:23 +0100

    doing: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19081,7 +19081,6 @@ with pkgs;
 
   openbsd = callPackage ../os-specific/bsd/openbsd { };
 
-  doing = callPackage ../applications/misc/doing  { };
 
   alibuild = callPackage ../development/tools/build-managers/alibuild {
     python = python3;
