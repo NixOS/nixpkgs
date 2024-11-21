32f1b9bdb3ea19f5c9bfd301dc4d4dcccef8ea8b
Kenichi Kamiya <kachick1@gmail.com>
2024-11-21 11:28:32 +0900

    zettlr: 3.2.0 -> 3.3.1
    Diff: https://github.com/Zettlr/Zettlr/compare/v3.2.0...v3.3.1
    
    Bumped to Electron 33 in https://github.com/Zettlr/Zettlr/commit/faa1f1e1d93993f3ed4f1ad49bd98391c18343ce

--- pkgs/applications/misc/zettlr/default.nix
+++ pkgs/applications/misc/zettlr/default.nix
@@ -2,7 +2,7 @@
 
 builtins.mapAttrs (pname: attrs: callPackage ./generic.nix (attrs // { inherit pname; })) {
   zettlr = {
-    version = "3.2.0";
-    hash = "sha256-gttDGWFJ/VmOyqgOSKnCqqPtNTKJd1fmDpa0ZAX3xc8=";
+    version = "3.3.1";
+    hash = "sha256-jKqe3dEWEtg/SqY2sfgi+AaPOk2fn4tfEMhNDGlJMQw=";
   };
 }
