81001625a7a756dbfc50fbe5ae75d52b26c93e3f
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-11-21 17:09:17 +0100

    linuxPackages_latest.gasket: fix build with 6.12

--- pkgs/os-specific/linux/gasket/default.nix
+++ pkgs/os-specific/linux/gasket/default.nix
@@ -1,4 +1,10 @@
-{ stdenv, lib, fetchFromGitHub, kernel }:
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  fetchpatch2,
+  kernel
+}:
 
 stdenv.mkDerivation rec {
   pname = "gasket";
@@ -11,6 +17,20 @@ stdenv.mkDerivation rec {
     sha256 = "O17+msok1fY5tdX1DvqYVw6plkUDF25i8sqwd6mxYf8=";
   };
 
+  patches = [
+    (fetchpatch2 {
+      # https://github.com/google/gasket-driver/issues/36
+      # https://github.com/google/gasket-driver/pull/35
+      name = "linux-6.12-compat.patch";
+      url = "https://github.com/google/gasket-driver/commit/4b2a1464f3b619daaf0f6c664c954a42c4b7ce00.patch";
+      hash = "sha256-UOoOSEnpUMa4QXWVFpGFxBoF5szXaLEfcWtfKatO5XY=";
+    })
+  ];
+
+  postPatch = ''
+    cd src
+  '';
+
   makeFlags = kernel.makeFlags ++ [
     "-C"
     "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
@@ -21,7 +41,6 @@ stdenv.mkDerivation rec {
   installFlags = [ "INSTALL_MOD_PATH=${placeholder "out"}" ];
   installTargets = [ "modules_install" ];
 
-  sourceRoot = "${src.name}/src";
   hardeningDisable = [ "pic" "format" ];
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
