f4b2553be7b3e17978f8bbcbe20827dd3dd6b792
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-21 12:03:28 +0100

    python312Packages.restview: 3.0.1 -> 3.0.2
    Changelog: https://github.com/mgedmin/restview/blob/3.0.2/CHANGES.rst

--- pkgs/development/python-modules/restview/default.nix
+++ pkgs/development/python-modules/restview/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "restview";
-  version = "3.0.1";
+  version = "3.0.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-jBoXHBWdRtFdVWn3cCGCiIOhIdb5uvdY1kH8HlSwWuU=";
+    hash = "sha256-i011oL7Xa2e0Vu9wEfTrbJilVsn4N2Qt8iAscxL8zBo=";
   };
 
   propagatedBuildInputs = [
