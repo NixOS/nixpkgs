fd98e31bec328b6b2d4b549b85f640ca20d7eef8
Emily <vcs@emily.moe>
2024-11-21 23:01:14 +0000

    vtk: disable function pointer warnings on GCC too
    I tried to devendor these libraries but it seemed hopeless. See
    <https://github.com/NixOS/nixpkgs/pull/171934> for a more comprehensive
    attempt. This will have to do for now.

--- pkgs/development/libraries/vtk/generic.nix
+++ pkgs/development/libraries/vtk/generic.nix
@@ -100,8 +100,13 @@ in stdenv.mkDerivation {
       "-DVTK_PYTHON_VERSION:STRING=${pythonMajor}"
     ];
 
-  env = lib.optionalAttrs stdenv.cc.isClang {
-    NIX_CFLAGS_COMPILE = "-Wno-error=incompatible-function-pointer-types";
+  env = {
+    # Lots of warnings in vendored code…
+    NIX_CFLAGS_COMPILE =
+      if stdenv.cc.isClang then
+        "-Wno-error=incompatible-function-pointer-types"
+      else
+        "-Wno-error=incompatible-pointer-types";
   };
 
   postInstall = optionalString enablePython ''
