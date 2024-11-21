04bebf26cafa09e8ac8b41c257bde9cd2a979fce
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-21 01:04:24 +0000

    python312Packages.strawberry-graphql: 0.243.1 -> 0.250.1

--- pkgs/development/python-modules/strawberry-graphql/default.nix
+++ pkgs/development/python-modules/strawberry-graphql/default.nix
@@ -44,7 +44,7 @@
 
 buildPythonPackage rec {
   pname = "strawberry-graphql";
-  version = "0.243.1";
+  version = "0.250.1";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -53,7 +53,7 @@ buildPythonPackage rec {
     owner = "strawberry-graphql";
     repo = "strawberry";
     rev = "refs/tags/${version}";
-    hash = "sha256-m+KaUd0rl0RRZzVPI+ldwW77YGLq45egkV5vYYz4dW8=";
+    hash = "sha256-/iy9V0yUHmyJGtujsJPfBeMBL2V5U2h/W6YnuC/r1I8=";
   };
 
   postPatch = ''
