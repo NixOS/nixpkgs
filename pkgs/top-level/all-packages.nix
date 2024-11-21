6560a6f22e0fabb43db30c90e7c3210de05c132e
liberodark <liberodark@gmail.com>
2024-11-21 08:20:23 +0100

    rofi: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15693,7 +15693,6 @@ with pkgs;
   rke2 = rke2_stable;
 
   rofi-unwrapped = callPackage ../applications/misc/rofi { };
-  rofi = callPackage ../applications/misc/rofi/wrapper.nix { };
   rofi-wayland-unwrapped = callPackage ../applications/misc/rofi/wayland.nix { };
   rofi-wayland = callPackage ../applications/misc/rofi/wrapper.nix {
     rofi-unwrapped = rofi-wayland-unwrapped;
