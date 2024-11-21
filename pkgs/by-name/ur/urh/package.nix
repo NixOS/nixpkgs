6572fd096ae0e52f2cedc1c927228a2eed519fdd
Paw Møller <pawsen@gmail.com>
2024-11-21 17:08:59 +0100

    urh: install desktop-file

--- pkgs/by-name/ur/urh/package.nix
+++ pkgs/by-name/ur/urh/package.nix
@@ -1,6 +1,9 @@
 { stdenv, lib, fetchFromGitHub, python3Packages
 , hackrf, rtl-sdr, airspy, limesuite, libiio
 , libbladeRF
+, imagemagick
+, makeDesktopItem
+, copyDesktopItems
 , qt5
 , wrapGAppsHook3
 , USRPSupport ? false, uhd }:
@@ -16,7 +19,9 @@ python3Packages.buildPythonApplication rec {
     sha256 = "sha256-r3d80dzGwgf5Tuwt1IWGcmNbblwBNKTKKm+GGx1r2HE=";
   };
 
-  nativeBuildInputs = [ qt5.wrapQtAppsHook wrapGAppsHook3 ];
+  nativeBuildInputs = [ qt5.wrapQtAppsHook wrapGAppsHook3 copyDesktopItems
+                        imagemagick # for scaling icons
+                      ];
   buildInputs = [ hackrf rtl-sdr airspy limesuite libiio libbladeRF ]
     ++ lib.optional USRPSupport uhd
     ++ lib.optional stdenv.hostPlatform.isLinux qt5.qtwayland;
@@ -37,6 +42,23 @@ python3Packages.buildPythonApplication rec {
   '';
 
   doCheck = false;
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = pname;
+      exec = "urh";
+      icon = "urh";
+      desktopName = "Universal Radio Hacker";
+      categories = [ "HamRadio" ];
+      comment = meta.description;
+    })
+  ];
+
+  postInstall = ''
+    mkdir -p $out/share/icons/hicolor/{48x48,128x128}/apps
+    convert $src/data/icons/appicon.png -resize 48x48 $out/share/icons/hicolor/48x48/apps/urh.png
+    convert $src/data/icons/appicon.png -resize 128x128 $out/share/icons/hicolor/128x128/apps/urh.png
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/jopohl/urh";
