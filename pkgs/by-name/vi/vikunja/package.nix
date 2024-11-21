321173b3e1be6104c018d3d2f2484aea39a3d7c6
Leona Maroni <dev@leona.is>
2024-11-21 21:34:56 +0100

    vikunja: 0.24.4 -> 0.24.5
    https://vikunja.io/changelog/vikunja-v0.24.5-was-released
    (cherry picked from commit 04d5b525759d3cbe06bef54c668fe935c8b26ea9)

--- pkgs/by-name/vi/vikunja/package.nix
+++ pkgs/by-name/vi/vikunja/package.nix
@@ -1,12 +1,12 @@
 { lib, fetchFromGitHub, stdenv, nodejs, pnpm, buildGoModule, mage, writeShellScriptBin, nixosTests }:
 
 let
-  version = "0.24.4";
+  version = "0.24.5";
   src = fetchFromGitHub {
     owner = "go-vikunja";
     repo = "vikunja";
     rev = "v${version}";
-    hash = "sha256-h3Jz28HYQYZC+oWGXNeKv2iNsrU0gbBRfWgOvuKijtw=";
+    hash = "sha256-P5H+NfjE8wTmPD1VOI72hPi2DlDb4pCyq0nphK1VGK0=";
   };
 
   frontend = stdenv.mkDerivation (finalAttrs: {
@@ -67,7 +67,7 @@ buildGoModule {
     in
     [ fakeGit mage ];
 
-  vendorHash = "sha256-d2BNzsBeWlpZGbU7PkXWO5e9FLJA/Wda5ImXwqh/WV4=";
+  vendorHash = "sha256-OsKejno8QGg7HzRsrftngiWGiWHFc1jDLi5mQ9/NjI4=";
 
   inherit frontend;
 
