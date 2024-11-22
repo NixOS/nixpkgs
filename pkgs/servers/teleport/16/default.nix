577e342404b1f6ec88ae9dd9b5d4c1260b43d41e
techknowlogick <techknowlogick@gitea.com>
2024-11-21 22:03:29 -0500

    teleport_16: 16.4.6 -> 16.4.8

--- pkgs/servers/teleport/16/default.nix
+++ pkgs/servers/teleport/16/default.nix
@@ -1,9 +1,9 @@
 { wasm-bindgen-cli, ... }@args:
 import ../generic.nix (args // {
-  version = "16.4.6";
-  hash = "sha256-TdOCFs6YeqINM8aPryrjYPaXEjc/gIqu7kzVYDnMsjg=";
-  vendorHash = "sha256-iyYfht0aB9Vv2hsaqrieFHXbDhlotKQYfLn4JFqpve8=";
-  pnpmHash = "sha256-NF45Wp4itYud01VzxC8bRHZ3xZ1T1du1QmZTDMS5nOk=";
+  version = "16.4.8";
+  hash = "sha256-izOmea4FP93OvGWi/JlLUZJ9e38d4AfhvusVHj0MWVU=";
+  vendorHash = "sha256-xA8Pbf6D2DKU8wztU+pCh3fizIfBVp9wc1zzAg48YcY=";
+  pnpmHash = "sha256-+/5dgjxevJwos35txvu+R53FMp3Pp9yxv8YFCuBaNCU=";
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
