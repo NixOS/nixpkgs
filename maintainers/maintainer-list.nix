cfc99360d01c5f3d766fb66da40df602fe5001b0
Mistyttm <contact@mistyttm.dev>
2024-11-23 02:06:10 +1000

    maintainers: add mistyttm

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15091,6 +15091,12 @@
     githubId = 780485;
     name = "Misty De Méo";
   };
+  mistyttm = {
+    name = "Emmey Rose";
+    email = "contact@mistyttm.dev";
+    github = "Mistyttm";
+    githubId = 51770769;
+  };
   misuzu = {
     email = "bakalolka@gmail.com";
     github = "misuzu";
