983931612864cb44f567aaed7190c3a6ef28ddc2
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-22 03:56:12 +0000

    python312Packages.jsonformatter: 0.3.2 -> 0.3.4

--- pkgs/development/python-modules/jsonformatter/default.nix
+++ pkgs/development/python-modules/jsonformatter/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "jsonformatter";
-  version = "0.3.2";
+  version = "0.3.4";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "MyColorfulDays";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-oK419J/MIxRT+1j/5Yklj1F+4d3wuMXR8IVqJAMKPNw=";
+    hash = "sha256-A+lsSBrm/64w7yMabmuAbRCLwUUdulGH3jB/DbYJ2QY=";
   };
 
   nativeBuildInputs = [ setuptools ];
