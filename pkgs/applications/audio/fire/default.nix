90a076c9fd7f6744bc54195270c3907a7435a63b
OPNA2608 <opna2608@protonmail.com>
2024-11-22 16:59:55 +0100

    fire: 1.0.0.3 -> 1.0.1-unstable-2024-10-22
    (cherry picked from commit b8c9d52b8465cb716a12536fd5e5af16d38d1a36)

--- pkgs/applications/audio/fire/default.nix
+++ pkgs/applications/audio/fire/default.nix
@@ -1,9 +1,10 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, unstableGitUpdater
+, catch2_3
 , cmake
 , pkg-config
-, catch2
 , libX11
 , libXrandr
 , libXinerama
@@ -11,56 +12,37 @@
 , libXcursor
 , freetype
 , alsa-lib
-, Accelerate
-, Cocoa
-, WebKit
-, CoreServices
-, DiscRecording
-, CoreAudioKit
-, MetalKit
-, simd
+, apple-sdk_11
 }:
 
-let
-  # FetchContent replacement, check CMakeLists.txt for requested versions (Nixpkgs' Catch2 works)
-  readerwriterqueue = fetchFromGitHub {
-    owner = "cameron314";
-    repo = "readerwriterqueue";
-    rev = "v1.0.6";
-    sha256 = "sha256-g7NX7Ucl5GWw3u6TiUOITjhv7492ByTzACtWR0Ph2Jc=";
-  };
-in
 stdenv.mkDerivation rec {
   pname = "fire";
-  version = "1.0.0.3";
+  version = "1.0.1-unstable-2024-10-22";
 
   src = fetchFromGitHub {
     owner = "jerryuhoo";
     repo = "Fire";
-    rev = "v${version}";
+    rev = "4c3275caa0dbd9e65c6c3d574cc5ac1c828e529a";
     fetchSubmodules = true;
-    hash = "sha256-X3pzTrNd0G6BouCDkr3dukQTFDzZ7qblIYxFQActKGE=";
+    hash = "sha256-DyYP/uDawa+m2FtNvEvu36iRl6zfMyGNMMad5f/rX4k=";
   };
 
-  patches = [
-    ./0001-Remove-FetchContent-usage.patch
-  ];
-
   postPatch = ''
-    # 1. Remove hardcoded LTO flags: needs extra setup on Linux,
-    #    possibly broken on Darwin
-    # 2. Disable automatic copying of built plugins during buildPhase, it defaults
-    #    into user home and we want to have building & installing separated.
-    sed -i \
-      -e '/juce::juce_recommended_lto_flags/d' \
-      -e 's/COPY_PLUGIN_AFTER_BUILD TRUE/COPY_PLUGIN_AFTER_BUILD FALSE/g' \
-      CMakeLists.txt
+    # Disable automatic copying of built plugins during buildPhase, it defaults
+    # into user home and we want to have building & installing separated.
+    substituteInPlace CMakeLists.txt \
+      --replace-fail 'COPY_PLUGIN_AFTER_BUILD TRUE' 'COPY_PLUGIN_AFTER_BUILD FALSE'
+  '' + lib.optionalString stdenv.hostPlatform.isLinux ''
+    # Remove hardcoded LTO flags: needs extra setup on Linux
+    substituteInPlace CMakeLists.txt \
+      --replace-fail 'juce::juce_recommended_lto_flags' '# Not forcing LTO'
+  '' + lib.optionalString (!doCheck) ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail 'include(Tests)' '# Not building tests' \
+      --replace-fail 'include(Benchmarks)' '# Not building benchmark test'
   '';
 
-  preConfigure = ''
-    ln -s ${readerwriterqueue} readerwriterqueue
-    ln -s ${catch2.src} Catch2
-  '';
+  strictDeps = true;
 
   nativeBuildInputs = [
     cmake
@@ -76,37 +58,38 @@ stdenv.mkDerivation rec {
     freetype
     alsa-lib
   ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    Accelerate
-    Cocoa
-    WebKit
-    CoreServices
-    DiscRecording
-    CoreAudioKit
-    MetalKit
-    simd
+    apple-sdk_11
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "FETCHCONTENT_FULLY_DISCONNECTED" true)
+    (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_CATCH2" "${catch2_3.src}")
   ];
 
   installPhase = let
-    vst3Dir = "${placeholder "out"}/${if stdenv.hostPlatform.isDarwin then "Library/Audio/Plug-Ins/VST3" else "lib/vst3"}";
-    auDir = "${placeholder "out"}/Library/Audio/Plug-Ins/Components";
+    pathMappings = [
+      { from = "LV2"; to = "${placeholder "out"}/${if stdenv.hostPlatform.isDarwin then "Library/Audio/Plug-Ins/LV2" else "lib/lv2"}"; }
+      { from = "VST3"; to = "${placeholder "out"}/${if stdenv.hostPlatform.isDarwin then "Library/Audio/Plug-Ins/VST3" else "lib/vst3"}"; }
+      # this one's a guess, don't know where ppl have agreed to put them yet
+      { from = "CLAP"; to = "${placeholder "out"}/${if stdenv.hostPlatform.isDarwin then "Library/Audio/Plug-Ins/CLAP" else "lib/clap"}"; }
+    ] ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      { from = "AU"; to = "${placeholder "out"}/Library/Audio/Plug-Ins/Components"; }
+    ];
   in ''
     runHook preInstall
 
-    mkdir -p ${vst3Dir}
+  '' + lib.strings.concatMapStringsSep "\n" (entry: ''
+    mkdir -p ${entry.to}
     # Exact path of the build artefact depends on used CMAKE_BUILD_TYPE
-    cp -R Fire_artefacts/*/VST3/* ${vst3Dir}/
-  '' + lib.optionalString stdenv.hostPlatform.isDarwin ''
-    mkdir -p ${auDir}
-    cp -R Fire_artefacts/*/AU/* ${auDir}/
-  '' + ''
+    cp -r Fire_artefacts/*/${entry.from}/* ${entry.to}/
+  '') pathMappings + ''
 
     runHook postInstall
   '';
 
-  # Fails to find fp.h on its own
-  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isDarwin "-isystem ${CoreServices}/Library/Frameworks/CoreServices.framework/Versions/Current/Frameworks/CarbonCore.framework/Versions/Current/Headers/";
-
   doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
+
+  passthru.updateScript = unstableGitUpdater { tagPrefix = "v"; };
 
   meta = with lib; {
     description = "Multi-band distortion plugin by Wings";
