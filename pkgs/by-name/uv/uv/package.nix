56d9f8528290abf34f27683fd560239174dc5499
Gaetan Lepage <gaetan@glepage.com>
2024-11-22 22:41:52 +0100

    uv: switch to fetchCargoVendor

--- pkgs/by-name/uv/uv/package.nix
+++ pkgs/by-name/uv/uv/package.nix
@@ -22,13 +22,9 @@ python3Packages.buildPythonApplication rec {
     hash = "sha256-Kieh76RUrDEwdL04mvCXOqbXHasMMpXRqp0LwMOIHdM=";
   };
 
-  cargoDeps = rustPlatform.importCargoLock {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "async_zip-0.0.17" = "sha256-VfQg2ZY5F2cFoYQZrtf2DHj0lWgivZtFaFJKZ4oyYdo=";
-      "pubgrub-0.2.1" = "sha256-OPJb5exbRuHPVE9BS3kq1iqGBWWklFDH6X30dMWmnlI=";
-      "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
-    };
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit pname version src;
+    hash = "sha256-ZhHUKotXVDdwJs2kqIxME5I1FitWetx7FrQtluuIUWo=";
   };
 
   nativeBuildInputs = [
