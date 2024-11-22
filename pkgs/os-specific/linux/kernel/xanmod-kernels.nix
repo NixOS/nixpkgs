8f89eb9e52fa80e76e782618ec7ab1a5add811c4
Shawn8901 <shawn8901@googlemail.com>
2024-11-22 19:44:28 +0100

    linux_xanmod_latest: 6.11.9 -> 6.11.10

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -18,8 +18,8 @@ let
       hash = "sha256-P4B6r3p+Buu1Hf+RQsw5h2oUANVvQvQ4e/2gQcZ0vKw=";
     };
     main = {
-      version = "6.11.9";
-      hash = "sha256-lR7GXFy3eYq75+LwVlXScPYWbdVW6wAV+He0YZ+5AF4=";
+      version = "6.11.10";
+      hash = "sha256-WNzMM+P8c8Mv+FdrwcEPHwv/ppvgN2fiM+SHMmlAPYw=";
     };
   };
 
