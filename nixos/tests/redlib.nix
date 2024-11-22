0f77709e0f82f88e5735a2d81b86740742fce3ef
Guanran Wang <guanran928@outlook.com>
2024-11-22 15:42:09 +0800

    nixosTests.redlib: test settings mechanic

--- nixos/tests/redlib.nix
+++ nixos/tests/redlib.nix
@@ -13,6 +13,10 @@ import ./make-test-python.nix (
         enable = true;
         # Test CAP_NET_BIND_SERVICE
         port = 80;
+
+        settings = {
+          REDLIB_DEFAULT_USE_HLS = true;
+        };
       };
     };
 
@@ -21,6 +25,7 @@ import ./make-test-python.nix (
       machine.wait_for_open_port(80)
       # Query a page that does not require Internet access
       machine.succeed("curl --fail http://localhost:80/settings")
+      machine.succeed("curl --fail http://localhost:80/info | grep '<tr><td>Use HLS</td><td>on</td></tr>'")
     '';
   }
 )
