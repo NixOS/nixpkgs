cfc3088edcc646522b7a0208a43a3c678cfd1414
Olivér Falvai <ofalvai@gmail.com>
2024-11-23 20:17:30 +0100

    swiftpm2nix: use nurl instead of nix-prefetch-git

--- pkgs/development/compilers/swift/swiftpm2nix/default.nix
+++ pkgs/development/compilers/swift/swiftpm2nix/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, callPackage, makeWrapper, jq, nix-prefetch-git }:
+{ lib, stdenv, callPackage, makeWrapper, jq, nurl }:
 
 stdenv.mkDerivation {
   name = "swiftpm2nix";
@@ -10,7 +10,7 @@ stdenv.mkDerivation {
   installPhase = ''
     install -vD ${./swiftpm2nix.sh} $out/bin/swiftpm2nix
     wrapProgram $out/bin/$name \
-      --prefix PATH : ${lib.makeBinPath [ jq nix-prefetch-git ]} \
+      --prefix PATH : ${lib.makeBinPath [ jq nurl ]} \
   '';
 
   preferLocalBuild = true;
