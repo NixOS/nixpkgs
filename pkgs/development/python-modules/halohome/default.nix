8ab204a201a52df7fa04bbad3e8dea9cd095f128
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-23 08:42:56 +0100

    python312Packages.halohome: refactor

--- pkgs/development/python-modules/halohome/default.nix
+++ pkgs/development/python-modules/halohome/default.nix
@@ -6,23 +6,26 @@
   csrmesh,
   fetchFromGitHub,
   pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "halohome";
   version = "0.6.0";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "nayaverdier";
-    repo = pname;
+    repo = "halohome";
     rev = "refs/tags/${version}";
     hash = "sha256-oGwg9frE5LaUxmXjwaD0ZtY6D8D7f8tH0knZDaSm+XI=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     aiohttp
     bleak
     csrmesh
@@ -36,6 +39,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library to control Eaton HALO Home Smart Lights";
     homepage = "https://github.com/nayaverdier/halohome";
+    changelog = "https://github.com/nayaverdier/halohome/blob/${version}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
