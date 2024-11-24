4ca369a023de28575b42f6b282d57a02ca79d8cd
Doron Behar <doron.behar@gmail.com>
2024-11-24 18:34:38 +0200

    gopass-jsonapi: 1.15.14 -> 1.15.15
    Diff: https://github.com/gopasspw/gopass-jsonapi/compare/v1.15.14...v1.15.15
    
    Changelog: https://github.com/gopasspw/gopass-jsonapi/blob/v1.15.15/CHANGELOG.md

--- pkgs/by-name/go/gopass-jsonapi/package.nix
+++ pkgs/by-name/go/gopass-jsonapi/package.nix
@@ -1,30 +1,37 @@
 {
   lib,
+  stdenv,
   makeWrapper,
   buildGoModule,
   fetchFromGitHub,
   installShellFiles,
   gopass,
+  apple-sdk_14,
 }:
 
 buildGoModule rec {
   pname = "gopass-jsonapi";
-  version = "1.15.14";
+  version = "1.15.15";
 
   src = fetchFromGitHub {
     owner = "gopasspw";
     repo = "gopass-jsonapi";
     rev = "v${version}";
-    hash = "sha256-DbfmjgIUqgWVYyPqkcaeE5JKzqElNbrGnx62Fd8v7Hg=";
+    hash = "sha256-nayg7NTJH6bAPiguyuN37JivfWkpOUX/xI/+PHDi3UI=";
   };
 
-  vendorHash = "sha256-Aahu0afi6bPnvz/NSZznbp0y9vMJWDj1Bq7tWGRmm7g=";
+  vendorHash = "sha256-khX1CdzN+5T8q2hA3NyCxtz7uw9uDd9u61q3UslTtqs=";
 
   subPackages = [ "." ];
 
   nativeBuildInputs = [
     installShellFiles
     makeWrapper
+  ];
+
+  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
+    # For ScreenCaptureKit.h, see https://github.com/NixOS/nixpkgs/pull/358760#discussion_r1858327365
+    apple-sdk_14
   ];
 
   ldflags = [
