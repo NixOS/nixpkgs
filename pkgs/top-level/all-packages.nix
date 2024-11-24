60ae502470bb252e212b320dd938c69efe400e72
Emily <vcs@emily.moe>
2024-11-24 06:58:06 +0000

    libbitcoin{,-client,-explorer,-network,-protocol}: drop
    These are the last packages in the tree that depend on Boost
    1.75. There hasn’t been a release since 2023; upstream have stated
    in <https://github.com/libbitcoin/libbitcoin-system/issues/1234> that
    they do not plan to maintain the current version, and are currently
    working on a backwards‐incompatible major update.
    
    Nothing in the tree uses these libraries. The only other software
    distributions on Repology packaging them are YACP and the AUR, neither
    of which has had an update since 2019. They were removed from Ubuntu
    after 16.04, and have more recently been removed from [SlackBuilds.org]
    and [Homebrew] with the same reasoning that they’re keeping Boost
    1.75 around unnecessarily and no packaged software wants them.
    
    [SlackBuilds.org]: https://github.com/SlackBuildsOrg/slackbuilds/commit/17c7d0e430eadaab2ea02d435b0f48977961eba9
    [Homebrew]: https://github.com/Homebrew/homebrew-core/pull/157427
    
    Since these packages are also unmaintained in Nixpkgs, let’s just
    drop them and if anyone wants to package the 4.x version when it’s
    out they can re‐add it.
    
    (cherry picked from commit c983d7bc802f0112aa644b90b8215ad6142c91c2)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13491,22 +13491,6 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) Cocoa;
   };
 
-  libbitcoin = callPackage ../tools/misc/libbitcoin/libbitcoin.nix {
-    boost = boost175; # fatal error: 'boost/interprocess/detail/posix_time_types_wrk.hpp' file not found
-  };
-  libbitcoin-protocol = callPackage ../tools/misc/libbitcoin/libbitcoin-protocol.nix {
-    boost = boost175;
-  };
-  libbitcoin-client   = callPackage ../tools/misc/libbitcoin/libbitcoin-client.nix {
-    boost = boost175;
-  };
-  libbitcoin-network  = callPackage ../tools/misc/libbitcoin/libbitcoin-network.nix {
-    boost = boost175;
-  };
-  libbitcoin-explorer = callPackage ../tools/misc/libbitcoin/libbitcoin-explorer.nix {
-    boost = boost175;
-  };
-
   ausweisapp = qt6Packages.callPackage ../applications/misc/ausweisapp { };
 
   avidemux = libsForQt5.callPackage ../applications/video/avidemux { };
