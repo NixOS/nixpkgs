34706db4de9b6b8d349717c55a738306011cab97
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-25 14:30:36 +0000

    ocamlPackages.gsl: 1.25.0 -> 1.25.1

--- pkgs/development/ocaml-modules/gsl/default.nix
+++ pkgs/development/ocaml-modules/gsl/default.nix
@@ -2,7 +2,7 @@
 
 buildDunePackage rec {
   pname = "gsl";
-  version = "1.25.0";
+  version = "1.25.1";
 
   minimalOCamlVersion = "4.12";
 
@@ -10,7 +10,7 @@ buildDunePackage rec {
     owner = "mmottl";
     repo = "gsl-ocaml";
     rev = version;
-    hash = "sha256-vxXv0ZcToXmdYu5k0aLdV3seNn3Y6Sgg+8dpy3Iw68I=";
+    hash = "sha256-h1jO2RheBBzxiBgig2yEPk4YyBaZxStt5f+KNZqHdBo=";
   };
 
   nativeBuildInputs = [ pkg-config ];
