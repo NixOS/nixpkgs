d5037c09819e90602b0e40a76108bda51a3d8c08
wxt <3264117476@qq.com>
2024-11-25 11:29:58 +0800

    python3Packages.globus-sdk: 3.45.0 -> 3.48.0
    (cherry picked from commit f5ed2b6041363505e9169043fb0636989041bc3d)

--- pkgs/development/python-modules/globus-sdk/default.nix
+++ pkgs/development/python-modules/globus-sdk/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "globus-sdk";
-  version = "3.45.0";
+  version = "3.48.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "globus";
     repo = "globus-sdk-python";
     rev = "refs/tags/${version}";
-    hash = "sha256-EBYhT2RLHjvSwnSMbx+Xo3Fmxc9jttEPeljjlKbQd8Q=";
+    hash = "sha256-NYDmDhsgYNMZZDiSZ5ZtRauTzzckjqtETbfs8OvkgE4=";
   };
 
   build-system = [ setuptools ];
