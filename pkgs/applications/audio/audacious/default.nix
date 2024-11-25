5a39f9caaabbd519ceb9267ec1133cfe855709b8
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-11-25 17:26:16 +0000

    audacious: format with nixfmt-rfc-style

--- pkgs/applications/audio/audacious/default.nix
+++ pkgs/applications/audio/audacious/default.nix
@@ -1,14 +1,15 @@
-{ lib
-, stdenv
-, audacious-plugins
-, fetchFromGitHub
-, meson
-, ninja
-, pkg-config
-, qtbase
-, qtsvg
-, qtwayland
-, wrapQtAppsHook
+{
+  lib,
+  stdenv,
+  audacious-plugins,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  qtbase,
+  qtsvg,
+  qtwayland,
+  wrapQtAppsHook,
 }:
 
 stdenv.mkDerivation rec {
@@ -50,14 +51,18 @@ stdenv.mkDerivation rec {
     homepage = "https://audacious-media-player.org";
     downloadPage = "https://github.com/audacious-media-player/audacious";
     mainProgram = "audacious";
-    maintainers = with lib.maintainers; [ ramkromberg ttuegel thiagokokada ];
+    maintainers = with lib.maintainers; [
+      ramkromberg
+      ttuegel
+      thiagokokada
+    ];
     platforms = lib.platforms.linux;
     license = with lib.licenses; [
       bsd2
-      bsd3 #https://github.com/audacious-media-player/audacious/blob/master/COPYING
+      bsd3 # https://github.com/audacious-media-player/audacious/blob/master/COPYING
       gpl2
       gpl3
-      lgpl2Plus #http://redmine.audacious-media-player.org/issues/46
+      lgpl2Plus # http://redmine.audacious-media-player.org/issues/46
     ];
   };
 }
