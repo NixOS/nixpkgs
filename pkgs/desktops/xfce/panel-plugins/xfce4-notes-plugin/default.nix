57975ac5968532919ecdcdcdc323699ed8e859c1
Bobby Rong <rjl931189261@126.com>
2024-11-25 22:09:34 +0800

    xfce.xfce4-notes-plugin: Generate C code with newer Vala
    Release tarball dists C code generated by Vala 0.56.14, which is not
    new enough to support GCC 14.
    
    https://gitlab.xfce.org/panel-plugins/xfce4-notes-plugin/-/issues/84

--- pkgs/desktops/xfce/panel-plugins/xfce4-notes-plugin/default.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-notes-plugin/default.nix
@@ -1,31 +1,24 @@
-{ lib
-, stdenv
-, fetchurl
-, pkg-config
-, intltool
-, glib
-, gtk3
-, libxfce4ui
-, libxfce4util
-, xfce4-panel
-, xfconf
-, gitUpdater
+{
+  mkXfceDerivation,
+  lib,
+  vala,
+  glib,
+  gtk3,
+  libxfce4ui,
+  libxfce4util,
+  xfce4-panel,
+  xfconf,
 }:
 
-let
+mkXfceDerivation {
   category = "panel-plugins";
-in stdenv.mkDerivation rec {
-  pname  = "xfce4-notes-plugin";
+  pname = "xfce4-notes-plugin";
   version = "1.11.0";
-
-  src = fetchurl {
-    url = "mirror://xfce/src/${category}/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.bz2";
-    sha256 = "sha256-6zgkbesPyJU1+p/5uVPHYs7OIytVhdghD6uau/KCquM=";
-  };
+  sha256 = "sha256-hAGgJIZaUjrizuriW2yX4uOKqRxpZ6BiUhnj4u3BOBA=";
+  odd-unstable = false;
 
   nativeBuildInputs = [
-    pkg-config
-    intltool
+    vala
   ];
 
   buildInputs = [
@@ -36,11 +29,6 @@ in stdenv.mkDerivation rec {
     xfce4-panel
     xfconf
   ];
-
-  passthru.updateScript = gitUpdater {
-    url = "https://gitlab.xfce.org/panel-plugins/${pname}";
-    rev-prefix = "${pname}-";
-  };
 
   meta = with lib; {
     homepage = "https://docs.xfce.org/panel-plugins/xfce4-notes-plugin";
