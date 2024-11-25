8f1334f790fcb5ab0e23839828cb06cf5ecb1419
wxt <3264117476@qq.com>
2024-11-25 16:00:56 +0800

    horizon-eda: nixfmt

--- pkgs/by-name/ho/horizon-eda/package.nix
+++ pkgs/by-name/ho/horizon-eda/package.nix
@@ -1,17 +1,24 @@
-{ stdenv
-, boost
-, callPackage
-, coreutils
-, libspnav
-, python3
-, wrapGAppsHook3
+{
+  stdenv,
+  boost,
+  callPackage,
+  coreutils,
+  libspnav,
+  python3,
+  wrapGAppsHook3,
 }:
 
 let
   base = callPackage ./base.nix { };
 in
 stdenv.mkDerivation rec {
-  inherit (base) pname version src meta CASROOT;
+  inherit (base)
+    pname
+    version
+    src
+    meta
+    CASROOT
+    ;
 
   # provide base for python module
   passthru = {
