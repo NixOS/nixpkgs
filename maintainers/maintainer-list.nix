6d0eadc43951dcc0730fc18b34a6494f30251d58
parth <parth@mehrotra.me>
2024-11-25 13:39:09 -0500

    maintainers: add parth

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16771,6 +16771,11 @@
     githubId = 33826198;
     name = "Philipp Arras";
   };
+  parth = {
+    github = "parth";
+    githubId = 821972;
+    name = "Parth Mehrotra";
+  };
   pashashocky = {
     email = "pashashocky@gmail.com";
     github = "pashashocky";
