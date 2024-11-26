0335841a2929573d52188dc198c90cce2ce8c6bb
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-11-26 22:48:19 +0100

    nextcloud-client: 3.14.3 -> 3.15.0
    Diff: https://github.com/nextcloud-releases/desktop/compare/refs/tags/v3.14.3...v3.15.0
    
    Changelog: https://github.com/nextcloud/desktop/releases/tag/v3.15.0

--- pkgs/applications/networking/nextcloud-client/default.nix
+++ pkgs/applications/networking/nextcloud-client/default.nix
@@ -27,7 +27,7 @@
 
 stdenv.mkDerivation rec {
   pname = "nextcloud-client";
-  version = "3.14.3";
+  version = "3.15.0";
 
   outputs = [ "out" "dev" ];
 
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
     owner = "nextcloud-releases";
     repo = "desktop";
     rev = "refs/tags/v${version}";
-    hash = "sha256-nYoBs5EnWiqYRsqc5CPxCIs0NAxSprI9PV0lO/c8khw=";
+    hash = "sha256-XaZzSDDEgPPtQdxdTXaDsiPUiJ0N/uK4AMWSGj2nBIo=";
   };
 
   patches = [
