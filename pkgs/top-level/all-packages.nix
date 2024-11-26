b75af08b9908488c008c778932770f4ce48c1548
Anuragh K.P <36616831+kpanuragh@users.noreply.github.com>
2024-11-26 20:29:27 +0530

    Update pkgs/top-level/all-packages.nix
    Co-authored-by: aucub <78630225+aucub@users.noreply.github.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19241,5 +19241,4 @@ with pkgs;
   tree-from-tags = callPackage ../by-name/tr/tree-from-tags/package.nix {
     ruby = ruby_3_1;
   };
-  sublist3r = callPackage ../tools/security/sublist3r { };
 }
