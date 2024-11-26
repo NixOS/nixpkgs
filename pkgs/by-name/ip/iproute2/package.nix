NixOS/nixpkgs

commit e74ce644d04bda2fc239ade98f5b139655fda827

iproute2: 6.11.0 -> 6.12.0
Changes: https://www.spinics.net/lists/netdev/msg1047599.html

pkgs/by-name/ip/iproute2/package.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, fetchpatch
+{ lib, stdenv, fetchurl
 , buildPackages, bison, flex, pkg-config
 , db, iptables, elfutils, libmnl ,libbpf
 , gitUpdater, pkgsStatic
@@ -6,35 +6,12 @@
 
 stdenv.mkDerivation rec {
   pname = "iproute2";
-  version = "6.11.0";
+  version = "6.12.0";
 
   src = fetchurl {
     url = "mirror://kernel/linux/utils/net/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-H3lTmKBK6qzQao9qziz9kTwz+llTypnaroO7XFNGEcM=";
+    hash = "sha256-u9FB73tdASfMIVKEO6YfJ03DKBT6Pg8T59B6CAvvU9k=";
   };
-
-  patches = [
-    (fetchurl {
-      name = "musl-endian.patch";
-      url = "https://lore.kernel.org/netdev/20240712191209.31324-1-contact@hacktivis.me/raw";
-      hash = "sha256-MX+P+PSEh6XlhoWgzZEBlOV9aXhJNd20Gi0fJCcSZ5E=";
-    })
-    (fetchurl {
-      name = "musl-msghdr.patch";
-      url = "https://lore.kernel.org/netdev/20240712191209.31324-2-contact@hacktivis.me/raw";
-      hash = "sha256-X5BYSZBxcvdjtX1069a1GfcpdoVd0loSAe4xTpbCipA=";
-    })
-    (fetchurl {
-      name = "musl-basename.patch";
-      url = "https://lore.kernel.org/netdev/20240804161054.942439-1-dilfridge@gentoo.org/raw";
-      hash = "sha256-47obv6mIn/HO47lt47slpTAFDxiQ3U/voHKzIiIGCTM=";
-    })
-    (fetchpatch {
-      name = "musl-mst.patch";
-      url = "https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/patch/?id=6a77abab92516e65f07f8657fc4e384c4541ce0e";
-      hash = "sha256-19FzTDvgnmqVFBykVgXl4VIsHs8Cy9NWGOLpxifxVlI=";
-    })
-  ];
 
   postPatch = ''
     substituteInPlace Makefile \

commit 62e24cdb9bf5b3795050bb71e444c6762b6409ef

iproute2: fix build for musl
These patches were removed when iproute2 was updated, but are still
necessary.  (They fix implicit function declarations, which are an
error with GCC 14.)

Fixes: e74ce644d04b ("iproute2: 6.11.0 -> 6.12.0")

pkgs/by-name/ip/iproute2/package.nix
@@ -13,6 +13,19 @@ stdenv.mkDerivation rec {
     hash = "sha256-u9FB73tdASfMIVKEO6YfJ03DKBT6Pg8T59B6CAvvU9k=";
   };
 
+  patches = [
+    (fetchurl {
+      name = "musl-endian.patch";
+      url = "https://lore.kernel.org/netdev/20240712191209.31324-1-contact@hacktivis.me/raw";
+      hash = "sha256-MX+P+PSEh6XlhoWgzZEBlOV9aXhJNd20Gi0fJCcSZ5E=";
+    })
+    (fetchurl {
+      name = "musl-basename.patch";
+      url = "https://lore.kernel.org/netdev/20240804161054.942439-1-dilfridge@gentoo.org/raw";
+      hash = "sha256-47obv6mIn/HO47lt47slpTAFDxiQ3U/voHKzIiIGCTM=";
+    })
+  ];
+
   postPatch = ''
     substituteInPlace Makefile \
       --replace "CC := gcc" "CC ?= $CC"