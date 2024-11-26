416be31d221d366ca3ede57a358db066aa73338b
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-26 09:32:17 +0100

    python312Packages.nsapi: refactor

--- pkgs/development/python-modules/nsapi/default.nix
+++ pkgs/development/python-modules/nsapi/default.nix
@@ -2,16 +2,17 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  future,
   pythonOlder,
   pytz,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "nsapi";
   version = "3.1.2";
-  format = "setuptools";
-  disabled = pythonOlder "3.7";
+  pyproject = true;
+
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "aquatix";
@@ -20,10 +21,9 @@ buildPythonPackage rec {
     sha256 = "sha256-H8qxqzcGZ52W/HbTuKdnfnaYdZFaxzuUhrniS1zsL2w=";
   };
 
-  propagatedBuildInputs = [
-    future
-    pytz
-  ];
+  build-system = [ setuptools ];
+
+  dependencies = [ pytz ];
 
   # Project has no tests
   doCheck = false;
@@ -33,7 +33,8 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python module to query routes of the Dutch railways";
     homepage = "https://github.com/aquatix/ns-api/";
-    license = with licenses; [ mit ];
+    changelog = "https://github.com/aquatix/ns-api/releases/tag/v${version}";
+    license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
 }
