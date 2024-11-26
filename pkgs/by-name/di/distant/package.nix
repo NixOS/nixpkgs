e56741588337bbdb7e27f8df8b4b39b10a6fb2c8
fe2-Nyxar <153492661+fe2-Nyxar@users.noreply.github.com>
2024-11-26 18:32:44 +0100

    Create package.nix

---
+++ pkgs/by-name/di/distant/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  perl,
+  pkg-config,
+  openssl,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "distant";
+  version = "0.20.0";
+
+  src = fetchFromGitHub {
+    owner = "chipsenkbeil";
+    repo = "distant";
+    rev = "v${version}";
+    hash = "sha256-DcnleJUAeYg3GSLZljC3gO9ihiFz04dzT/ddMnypr48=";
+  };
+
+  cargoHash = "sha256-7MNNdm4b9u5YNX04nBtKcrw+phUlpzIXo0tJVfcgb40=";
+  nativeBuildInputs = [
+    pkg-config
+    perl
+  ];
+  buildInputs = [ openssl ];
+  doCheck = false;
+
+  meta = {
+    description = "Fast, secure remote development tool";
+    homepage = "https://github.com/chipsenkbeil/distant";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ fe2-Nyxar ];
+  };
+}
