7025e30f43c184061f3cf25790ad56c3136e104e
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-26 22:12:19 +0100

    python312Packages.pyflipper: minor changes

--- pkgs/development/python-modules/pyflipper/default.nix
+++ pkgs/development/python-modules/pyflipper/default.nix
@@ -3,8 +3,9 @@
   buildPythonPackage,
   fetchFromGitHub,
   pyserial,
-  websocket-client,
+  pythonOlder,
   setuptools,
+  websocket-client,
 }:
 
 buildPythonPackage rec {
@@ -12,24 +13,32 @@ buildPythonPackage rec {
   version = "0.18-unstable-2024-04-15";
   pyproject = true;
 
+  disabled = pythonOlder "3.10";
+
   src = fetchFromGitHub {
     owner = "wh00hw";
     repo = "pyFlipper";
+    # https://github.com/wh00hw/pyFlipper/issues/20
     rev = "e8a82a25eb766fac53a2e6e5fff6505f60cf0897";
     hash = "sha256-CQ6oVVkLxyoNoe7L0USfal1980VkfiuHc4cqXTsZ2Jc=";
   };
 
   build-system = [ setuptools ];
-  dependencies = [ pyserial websocket-client ];
 
-  pythonImportsCheck = [
-    "pyflipper"
+  dependencies = [
+    pyserial
+    websocket-client
   ];
 
-  meta = with lib; {
+  # Module has no tests
+  doCheck = false;
+
+  pythonImportsCheck = [ "pyflipper" ];
+
+  meta = {
     description = "Flipper Zero Python CLI Wrapper";
     homepage = "https://github.com/wh00hw/pyFlipper";
-    license = licenses.mit;
-    maintainers = with maintainers; [ siraben ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ siraben ];
   };
 }
