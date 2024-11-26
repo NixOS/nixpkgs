d232880c20d309c1bce36887d75575e7c4dd7b36
Wolfgang Walther <walther@technowledgy.de>
2024-11-26 22:02:37 +0100

    various: remove left-over rtc_cmos rootModule
    This is not needed anymore after
    65a50ea453edc76b3b749b16dcc442e42df08451.

--- pkgs/build-support/vm/default.nix
+++ pkgs/build-support/vm/default.nix
@@ -6,7 +6,6 @@
 , storeDir ? builtins.storeDir
 , rootModules ?
     [ "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_balloon" "virtio_rng" "ext4" "unix" "9p" "9pnet_virtio" "crc32c_generic" ]
-      ++ pkgs.lib.optional pkgs.stdenv.hostPlatform.isx86 "rtc_cmos"
 }:
 
 let
