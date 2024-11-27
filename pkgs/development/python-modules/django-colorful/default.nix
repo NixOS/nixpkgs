49dcd55e30d8671d8e8ae9ebe621e138d17c4286
Fabian Affolter <mail@fabian-affolter.ch>
2024-11-27 08:51:52 +0100

    python312Packages.django-colorful: refactor

--- pkgs/development/python-modules/django-colorful/default.nix
+++ pkgs/development/python-modules/django-colorful/default.nix
@@ -3,27 +3,32 @@
   buildPythonPackage,
   fetchPypi,
   django,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "django-colorful";
   version = "1.3";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "fd246f2fb297ed074dc4349966d33a1c82d0308b7fb0d6ef6e2e76b90cefffb7";
+    hash = "sha256-/SRvL7KX7QdNxDSZZtM6HILQMIt/sNbvbi52uQzv/7c=";
   };
+
+  build-system = [ setuptools ];
+
+  buildInputs = [ django ];
 
   # Tests aren't run
   doCheck = false;
 
-  # Requires Django >= 1.8
-  buildInputs = [ django ];
+  pythonImportsCheck = [ "colorful" ];
 
   meta = with lib; {
     description = "Django extension that provides database and form color fields";
     homepage = "https://github.com/charettes/django-colorful";
     license = licenses.mit;
+    maintainers = [ ];
   };
 }
