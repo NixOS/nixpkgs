f6def22d89dfce156023272266944600dbcbe55d
hakan-demirli <ehdemirli@gmail.com>
2024-11-27 07:58:19 +0300

    python312Packages.local-attention: init at 1.9.15

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7569,6 +7569,8 @@ self: super: with self; {
 
   localimport = callPackage ../development/python-modules/localimport { };
 
+  local-attention = callPackage ../development/python-modules/local-attention { };
+
   localstack = callPackage ../development/python-modules/localstack { };
 
   localstack-client = callPackage ../development/python-modules/localstack-client { };
