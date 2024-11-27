d9ad7e2c75fd8057d23ebe8a91cf0f035c879305
Makuru <Makuru@Makuru.org>
2024-11-27 15:08:31 +0100

    nixos/modules/image: added the option to compress to qcow2

--- nixos/modules/image/repart.nix
+++ nixos/modules/image/repart.nix
@@ -113,7 +113,7 @@ in
       enable = lib.mkEnableOption "Image compression";
 
       algorithm = lib.mkOption {
-        type = lib.types.enum [ "zstd" "xz" ];
+        type = lib.types.enum [ "zstd" "xz" "qcow2-compressed" ];
         default = "zstd";
         description = "Compression algorithm";
       };
@@ -274,6 +274,7 @@ in
           {
             "zstd" = ".zst";
             "xz" = ".xz";
+            "qcow2-compressed" = ".qcow2";
           }."${cfg.compression.algorithm}";
 
         makeClosure = paths: pkgs.closureInfo { rootPaths = paths; };
