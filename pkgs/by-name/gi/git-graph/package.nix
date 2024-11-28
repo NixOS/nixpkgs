64a4e67f75501d651c2ca139b63a3fb83dc9c75e
Matthias Beyer <mail@beyermatthias.de>
2024-11-28 10:50:12 +0100

    git-graph: unstable-23-01-14 -> 0.6.0
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>
    Co-authored-by: Aleksana <alexander.huang.y@gmail.com>
    (cherry picked from commit c0f23c4e5d54491c10f4504b66dee9c3dc36f495)

--- pkgs/by-name/gi/git-graph/package.nix
+++ pkgs/by-name/gi/git-graph/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "git-graph";
-  version = "unstable-2023-01-14";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "mlange-42";
     repo = pname;
-    rev = "9bd54eb0aed6f108364bce9ad0bdff12077038fc";
-    hash = "sha256-tMM/mpt9yzZYSpnOGBuGLM0XTJiiyChfUrERMuyn3mQ=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-xYUpLujePO1MS0c25UJX5rRdmPzkaFgF5zJonzQOJqM=";
   };
 
-  cargoHash = "sha256-ZLF/l2HnIbmkayWXhjYr01M6lGaGiK2UYyp654ncxgo=";
+  cargoHash = "sha256-y5tVjWj/LczblkL793878vzDG0Gtj3kIo2jZlRA6GJE=";
 
   meta = with lib; {
     description = "Command line tool to show clear git graphs arranged for your branching model";
