65ed34e2b70a3ad9829b07c113705b89c01d0e4f
Shogo Takata <peshogo@gmail.com>
2024-11-28 16:57:38 +0900

    cargo-deb: fix build failure
    (cherry picked from commit ae379c99c9e408db1b4a60434cd34b61131aeea4)

--- pkgs/by-name/ca/cargo-deb/package.nix
+++ pkgs/by-name/ca/cargo-deb/package.nix
@@ -1,8 +1,9 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-, makeWrapper
-, dpkg
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  makeWrapper,
+  dpkg,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -22,10 +23,23 @@ rustPlatform.buildRustPackage rec {
     makeWrapper
   ];
 
-  # This is an FHS specific assert depending on glibc location
   checkFlags = [
+    # This is an FHS specific assert depending on glibc location
     "--skip=dependencies::resolve_test"
     "--skip=run_cargo_deb_command_on_example_dir_with_separate_debug_symbols"
+    # The following tests require empty CARGO_BUILD_TARGET env variable, but we
+    # set it ever since https://github.com/NixOS/nixpkgs/pull/298108.
+    "--skip=build_with_command_line_compress_gz"
+    "--skip=build_with_command_line_compress_xz"
+    "--skip=build_with_explicit_compress_type_gz"
+    "--skip=build_with_explicit_compress_type_xz"
+    "--skip=build_workspaces"
+    "--skip=cwd_dir1"
+    "--skip=cwd_dir2"
+    "--skip=cwd_dir3"
+    "--skip=run_cargo_deb_command_on_example_dir"
+    "--skip=run_cargo_deb_command_on_example_dir_with_variant"
+    "--skip=run_cargo_deb_command_on_example_dir_with_version"
   ];
 
   postInstall = ''
@@ -38,6 +52,9 @@ rustPlatform.buildRustPackage rec {
     mainProgram = "cargo-deb";
     homepage = "https://github.com/kornelski/cargo-deb";
     license = licenses.mit;
-    maintainers = with maintainers; [ Br1ght0ne matthiasbeyer ];
+    maintainers = with maintainers; [
+      Br1ght0ne
+      matthiasbeyer
+    ];
   };
 }
