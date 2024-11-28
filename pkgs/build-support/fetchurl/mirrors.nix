48ecbdfa9a871b57ed131c146c26523cdc098166
Jan Malakhovski <oxij@oxij.org>
2024-11-28 12:00:00 +0000

    fetchurl: improve GNU Savannah mirrors

--- pkgs/build-support/fetchurl/mirrors.nix
+++ pkgs/build-support/fetchurl/mirrors.nix
@@ -221,7 +221,13 @@
 
   # GNU Savannah
   savannah = [
-    # Mirrors from https://download-mirror.savannah.gnu.org/releases/00_MIRRORS.html
+    # Try the official HTTP(S) dispatchers first.
+    # These generate redirects to mirrors appropriate for the user.
+    "https://download.savannah.gnu.org/releases/"
+    "https://download.savannah.nongnu.org/releases/"
+
+    # If the above fail, try some individual mirrors.
+    # These are taken from https://download-mirror.savannah.gnu.org/releases/00_MIRRORS.html
     "https://mirror.easyname.at/nongnu/"
     "https://savannah.c3sl.ufpr.br/"
     "https://mirror.csclub.uwaterloo.ca/nongnu/"
