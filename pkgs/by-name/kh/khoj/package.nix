a9b4b752196d278074b7e6dc8e97064b6769c158
happysalada <raphael@megzari.com>
2024-11-28 03:21:49 -0500

    khoj: 1.20.2 -> 1.30.7

--- pkgs/by-name/kh/khoj/package.nix
+++ pkgs/by-name/kh/khoj/package.nix
@@ -6,16 +6,21 @@
   postgresqlTestHook,
 }:
 
+let
+  django_phonenumber_field = python3.pkgs.django-phonenumber-field.override {
+    django = python3.pkgs.django_5;
+  };
+in
 python3.pkgs.buildPythonApplication rec {
   pname = "khoj";
-  version = "1.20.2";
+  version = "1.30.7";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "debanjum";
     repo = "khoj";
     rev = "refs/tags/${version}";
-    hash = "sha256-llVn9OR2EZvZsA0sRRSRqpqF3E67FkslQZeorya4hBA=";
+    hash = "sha256-upZqXNlDhTk/aXrLtnvdOJdrE58CnbdqUZdiaqM3xao=";
   };
 
   env = {
@@ -23,65 +28,79 @@ python3.pkgs.buildPythonApplication rec {
     postgresqlEnableTCP = 1;
   };
 
-  nativeBuildInputs = with python3.pkgs; [
+  build-system = with python3.pkgs; [
     hatch-vcs
     hatchling
   ];
 
-  propagatedBuildInputs = with python3.pkgs; [
-    beautifulsoup4
-    dateparser
-    defusedxml
-    fastapi
-    python-multipart
-    jinja2
-    openai
-    tiktoken
-    tenacity
-    magika
-    pillow
-    pydantic
-    pyyaml
-    rich
-    schedule
-    sentence-transformers
-    einops
-    transformers
-    torch
-    uvicorn
-    aiohttp
-    langchain
-    langchain-openai
-    langchain-community
-    requests
-    # Tenacity is duplicated in the upstream pyproject.toml file
-    anyio
-    pymupdf
-    django
-    authlib
-    # llama-cpp-python
-    itsdangerous
-    httpx
-    pgvector
-    psycopg2
-    lxml
-    tzdata
-    rapidocr-onnxruntime
-    openai-whisper
-    django-phonenumber-field
-    phonenumbers
-    markdownify
-    markdown-it-py
-    websockets
-    psutil
-    huggingface-hub
-    apscheduler
-    pytz
-    cron-descriptor
-    # django_appscheduler
-    anthropic
-    docx2txt
+  pythonRelaxDeps = true;
+
+  pythonRemoveDeps = [
+    "django-apscheduler"
+    "llama-cpp-python"
+    "pymupdf"
+    "sentence-transformers"
+    "psycopg2-binary"
   ];
+
+  dependencies =
+    with python3.pkgs;
+    [
+      beautifulsoup4
+      dateparser
+      defusedxml
+      fastapi
+      python-multipart
+      jinja2
+      openai
+      tiktoken
+      tenacity
+      magika
+      pillow
+      pydantic
+      pyyaml
+      rich
+      schedule
+      sentence-transformers
+      einops
+      transformers
+      torch
+      uvicorn
+      aiohttp
+      langchain
+      langchain-openai
+      langchain-community
+      requests
+      # Tenacity is duplicated in the upstream pyproject.toml file
+      anyio
+      pymupdf
+      django_5
+      authlib
+      # llama-cpp-python # not yet packaged
+      itsdangerous
+      httpx
+      pgvector
+      psycopg2
+      lxml
+      tzdata
+      rapidocr-onnxruntime
+      openai-whisper
+      django_phonenumber_field
+      phonenumbers
+      markdownify
+      markdown-it-py
+      websockets
+      psutil
+      huggingface-hub
+      apscheduler
+      pytz
+      cron-descriptor
+      django-apscheduler
+      anthropic
+      docx2txt
+      google-generativeai
+    ]
+    ++ python3.pkgs.pydantic.optional-dependencies.email;
 
   nativeCheckInputs =
     with python3.pkgs;
