02711f4db588a80e569699626f77b584e15f68ce
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-28 15:07:11 +0000

    c-blosc2: 2.15.1 -> 2.15.2

--- pkgs/development/libraries/c-blosc/2.nix
+++ pkgs/development/libraries/c-blosc/2.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "c-blosc2";
-  version = "2.15.1";
+  version = "2.15.2";
 
   src = fetchFromGitHub {
     owner = "Blosc";
     repo = "c-blosc2";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-g/MUgVWeVVREiHX59XYbhXjtigJesyooxjnEdKA844M=";
+    sha256 = "sha256-Xac0meyaHfdCIu0ut2ioPDFYtAGpOBRb/G8ZK/jmeJ4=";
   };
 
   # https://github.com/NixOS/nixpkgs/issues/144170
