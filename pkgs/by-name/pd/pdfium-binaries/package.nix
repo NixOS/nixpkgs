430f805cfe58abdc5360d858a3e0402eea740593
aucub <78630225+aucub@users.noreply.github.com>
2024-11-28 19:35:50 +0000

    pdfium-binaries: 6721 -> 6858

--- pkgs/by-name/pd/pdfium-binaries/package.nix
+++ pkgs/by-name/pd/pdfium-binaries/package.nix
@@ -4,7 +4,7 @@
   stdenv,
 }:
 let
-  version = "6721";
+  version = "6858";
   src =
     let
       inherit (stdenv.hostPlatform) system;
@@ -16,10 +16,10 @@ let
         aarch64-darwin = "mac-arm64";
       };
       hash = selectSystem {
-        x86_64-linux = "sha256-SHCYdw3X8Uy9CgT8SN90FKdPKIk6VZFjIb6NYfOgoCo=";
-        aarch64-linux = "sha256-RqyzxQ2RnBuoFBAxJVC8x/XDpJJWdJ45dJXV/Yzh7pM=";
-        x86_64-darwin = "sha256-7XuBlG2pUtSN5kdcTTLbijEHHX5IU5kTj1aW8ZHHS2M=";
-        aarch64-darwin = "sha256-ZNsQkay8ZF2RQ6dKJ24GawIthjFgB5TUa08LNKpZv1A=";
+        x86_64-linux = "sha256-8DrWkttbpt3DvGmtAlFpL+OJAjRAp4oBcX7njKv8cn8=";
+        aarch64-linux = "sha256-7VMvnLayxFh8BvH3A6B+MpWDAQwg1CMpDaQJJ5WGxfo=";
+        x86_64-darwin = "sha256-YO1id+aKx9G8zqVzW0bwRtB4hMr797MAwCWh349ClzU=";
+        aarch64-darwin = "sha256-c+QZTFa1iIMhmccGhzULHpgJyD+QAcJzhphyYTCp0LU=";
       };
     in
     fetchzip {
