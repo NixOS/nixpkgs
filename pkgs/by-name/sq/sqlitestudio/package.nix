053ae2cfff10077edf6f1223b445c3115fdaff6b
Chatnoir Miki <cmiki@amono.me>
2024-11-28 11:21:44 +0800

    sqlitestudio: 3.4.5 -> 3.4.6

--- pkgs/by-name/sq/sqlitestudio/package.nix
+++ pkgs/by-name/sq/sqlitestudio/package.nix
@@ -11,13 +11,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "sqlitestudio";
-  version = "3.4.5";
+  version = "3.4.6";
 
   src = fetchFromGitHub {
     owner = "pawelsalawa";
     repo = "sqlitestudio";
     rev = version;
-    hash = "sha256-51gvjQULHI4MC+odsLPjK2n/9dIMrhq8S9R/uMGueXU=";
+    hash = "sha256-ETahZYAbayQEo+1mlSlJkUgzskIgdDdDM/MMOL2kXMk=";
   };
 
   nativeBuildInputs =
