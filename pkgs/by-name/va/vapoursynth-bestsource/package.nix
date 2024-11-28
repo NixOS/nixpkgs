bd2f027586d5f1920273778459f5516c4f86765c
Alex James <git@alextjam.es>
2024-11-28 01:55:37 -0600

    vapoursynth-bestsource: 6 → 10
    Changelog: https://github.com/vapoursynth/bestsource/releases/tag/R10

--- pkgs/by-name/va/vapoursynth-bestsource/package.nix
+++ pkgs/by-name/va/vapoursynth-bestsource/package.nix
@@ -13,7 +13,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vapoursynth-bestsource";
-  version = "6";
+  version = "10";
 
   outputs = [
     "out"
@@ -25,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "vapoursynth";
     repo = "bestsource";
     tag = "R${finalAttrs.version}";
-    hash = "sha256-ICkdIomlkHUdK6kMeui45fvUn4OMxSrP8svB2IN+GCg=";
+    hash = "sha256-zqQsyfB2POa+9IekwxJWdLToX2wY7ZR/hoENhGRuvhI=";
   };
 
   nativeBuildInputs = [
