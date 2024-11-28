b353d98f9d8f6b83148303d106b82c2d7763a254
Mathias Zhang <me@zzzsy.top>
2024-11-28 16:27:04 +0800

    libjxl: 0.11.0 -> 0.11.1
    (cherry picked from commit 095b2f82190eb1dd02bc4dbc34f9b9ba301df806)

--- pkgs/by-name/li/libjxl/package.nix
+++ pkgs/by-name/li/libjxl/package.nix
@@ -27,7 +27,7 @@ in
 
 stdenv.mkDerivation rec {
   pname = "libjxl";
-  version = "0.11.0";
+  version = "0.11.1";
 
   outputs = [ "out" "dev" ];
 
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
     owner = "libjxl";
     repo = "libjxl";
     rev = "v${version}";
-    hash = "sha256-lBc0zP+f44YadwOU9+I+YYWzTrAg7FSfF3IQuh4LjM4=";
+    hash = "sha256-ORwhKOp5Nog366UkLbuWpjz/6sJhxUO6+SkoJGH+3fE=";
     # There are various submodules in `third_party/`.
     fetchSubmodules = true;
   };
