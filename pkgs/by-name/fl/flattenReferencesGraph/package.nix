9ae0f7757760d269c0f3865aed98a3dfaafd6d3d
Adrian Gierakowski <agierakowski@gmail.com>
2024-11-29 22:00:15 +0000

    flattenReferencesGraph: use lib.fileset instead of gitignoreSource

--- pkgs/by-name/fl/flattenReferencesGraph/package.nix
+++ pkgs/by-name/fl/flattenReferencesGraph/package.nix
@@ -1,19 +1,22 @@
 {
   callPackage,
+  lib,
   nix-gitignore,
   python3Packages,
 }:
 let
+  inherit (lib) fileset;
   helpers = callPackage ./helpers.nix { };
   pythonPackages = python3Packages;
-
 in
 pythonPackages.buildPythonApplication {
   version = "0.1.0";
   pname = "flatten-references-graph";
 
-  # Note: this uses only ./src/.gitignore
-  src = nix-gitignore.gitignoreSource [ ] ./src;
+  src = fileset.toSource {
+    root = ./src;
+    fileset = fileset.difference ./src (fileset.maybeMissing ./src/__pycache__);
+  };
 
   propagatedBuildInputs = with pythonPackages; [
     igraph
