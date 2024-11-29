cc79df8c586204939da4bf1f68bffc34be0e5216
techknowlogick <techknowlogick@gitea.com>
2024-11-29 10:58:35 -0500

    cirrus-cli: 0.132.0 -> 0.133.0
    Diff: https://github.com/cirruslabs/cirrus-cli/compare/v0.132.0...v0.133.0

--- pkgs/by-name/ci/cirrus-cli/package.nix
+++ pkgs/by-name/ci/cirrus-cli/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "cirrus-cli";
-  version = "0.132.0";
+  version = "0.133.0";
 
   src = fetchFromGitHub {
     owner = "cirruslabs";
     repo = "cirrus-cli";
     rev = "v${version}";
-    hash = "sha256-juDe/Dw8tdVoz5F1bGGQMwZYz9HbQaQ0xZo8LqD03RA=";
+    hash = "sha256-uID2rHHsYYfGBDi4bg19G3Em0ztWGe5goBj1hIdAMkE=";
   };
 
   vendorHash = "sha256-+OMhaAGA+pmiDUyXDo9UfQ0SFEAN9zuNZjnLkgr7a+0=";
