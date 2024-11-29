8f28ab10cfe39674cffe01a939d1f21b0c34349d
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-11-29 02:19:43 -0700

    to-html: 0.1.4 -> 0.1.6

--- pkgs/by-name/to/to-html/package.nix
+++ pkgs/by-name/to/to-html/package.nix
@@ -4,17 +4,17 @@
 }:
 
 rustPlatform.buildRustPackage rec {
-  version = "0.1.4";
+  version = "0.1.6";
   pname = "to-html";
 
   src = fetchFromGitHub {
     owner = "Aloso";
     repo = "to-html";
     rev = "v${version}";
-    hash = "sha256-zkTBjsMFhRz7lVRh8i+XkaJ/qWmTAMPnkH5aDhbHf8U=";
+    hash = "sha256-eNFt9/yK4oHOspNM8PMTewhx8APaHzmgNdrWqrUuQSU=";
   };
 
-  cargoHash = "sha256-hXc+lB3DKnRZkp1U5wW/vPKSZ0c1UknQCAxDfE7Eubg=";
+  cargoHash = "sha256-UxDyk7m6eWvuWPHWUY2OJI59p43/bag8pcZyeGJr/QA=";
 
   # Requires external resources
   doCheck = false;
