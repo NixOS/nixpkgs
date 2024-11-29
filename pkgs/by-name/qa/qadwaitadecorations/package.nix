d566f2494f0eccc23ed30e232355e772dae1a747
R. Ryantm <ryantm-bot@ryantm.com>
2024-11-29 06:52:03 +0000

    qadwaitadecorations-qt6: 0.1.5 -> 0.1.6

--- pkgs/by-name/qa/qadwaitadecorations/package.nix
+++ pkgs/by-name/qa/qadwaitadecorations/package.nix
@@ -19,13 +19,13 @@ let
 
 in stdenv.mkDerivation (finalAttrs: {
   pname = "qadwaitadecorations";
-  version = "0.1.5";
+  version = "0.1.6";
 
   src = fetchFromGitHub {
     owner = "FedoraQt";
     repo = "QAdwaitaDecorations";
     rev = finalAttrs.version;
-    hash = "sha256-aqjm93tmBfDkmce1WG5xx8MCDCvo6AOrRHArj/+Ko9E=";
+    hash = "sha256-ZU3cwFwQECh4Z6YcTzD2WooZmJ2nSUABYft3dfakSuY=";
   };
 
   nativeBuildInputs = [
