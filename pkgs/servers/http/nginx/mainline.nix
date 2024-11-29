44636db274cb81d91d6970a531353adcf383ed39
Sergei Trofimovich <slyich@gmail.com>
2024-11-29 06:15:12 +0000

    nginxMainline: 1.27.2 -> 1.27.3
    Changes: https://nginx.org/en/CHANGES

--- pkgs/servers/http/nginx/mainline.nix
+++ pkgs/servers/http/nginx/mainline.nix
@@ -1,6 +1,6 @@
 { callPackage, ... }@args:
 
 callPackage ./generic.nix args {
-  version = "1.27.2";
-  hash = "sha256-qR7Pw6CzosFBOvymJ72IbXbgQUuBytD7eHKpZVobJfo=";
+  version = "1.27.3";
+  hash = "sha256-uiOpVo9EIDa2HNDim9ZqR7kGNO+pHgss8tcZBXqbeQM=";
 }
