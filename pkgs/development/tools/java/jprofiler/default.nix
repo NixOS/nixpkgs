fdef5ed12337f9ea1fa79a1c374c3f4b8b603932
aucub <78630225+aucub@users.noreply.github.com>
2024-11-30 15:17:53 +0800

    jprofiler: format
    (cherry picked from commit 5b9283c4ef773e451aec6706fbb756a1b9dd2531)

--- pkgs/development/tools/java/jprofiler/default.nix
+++ pkgs/development/tools/java/jprofiler/default.nix
@@ -1,38 +1,46 @@
-{ stdenv
-, lib
-, fetchurl
-, makeWrapper
-, makeDesktopItem
-, copyDesktopItems
-, _7zz
-, jdk
+{
+  stdenv,
+  lib,
+  fetchurl,
+  makeWrapper,
+  makeDesktopItem,
+  copyDesktopItems,
+  _7zz,
+  jdk,
 }:
 
 let
-  inherit (stdenv.hostPlatform) system;
   pname = "jprofiler";
 
   version = "13.0.6";
   nameApp = "JProfiler";
 
-  meta = with lib; {
+  meta = {
     description = "JProfiler's intuitive UI helps you resolve performance bottlenecks";
     longDescription = ''
       JProfiler's intuitive UI helps you resolve performance bottlenecks,
       pin down memory leaks and understand threading issues.
     '';
     homepage = "https://www.ej-technologies.com/products/jprofiler/overview.html";
-    license = licenses.unfree;
-    maintainers = with maintainers; [ ];
+    license = lib.licenses.unfree;
+    maintainers = [ ];
   };
 
-  src = if stdenv.hostPlatform.isLinux then fetchurl {
-    url = "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_linux_${lib.replaceStrings ["."] ["_"]  version}.tar.gz";
-    hash = "sha256-orjBSaC7NvKcak+RSEa9V05oL3EZIBnp7TyaX/8XFyg=";
-  } else fetchurl {
-    url = "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_macos_${lib.replaceStrings ["."] ["_"]  version}.dmg";
-    hash = "sha256-OI6NSPqYws5Rv25U5jIPzkyJtB8LF04qHB3NPR9XBWg=";
-  };
+  src =
+    if stdenv.hostPlatform.isLinux then
+      fetchurl {
+        url = "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_linux_${
+          lib.replaceStrings [ "." ] [ "_" ] version
+        }.tar.gz";
+        hash = "sha256-orjBSaC7NvKcak+RSEa9V05oL3EZIBnp7TyaX/8XFyg=";
+      }
+    else
+      fetchurl {
+        url = "https://download-gcdn.ej-technologies.com/jprofiler/jprofiler_macos_${
+          lib.replaceStrings [ "." ] [ "_" ] version
+        }.dmg";
+        hash = "sha256-OI6NSPqYws5Rv25U5jIPzkyJtB8LF04qHB3NPR9XBWg=";
+      };
 
   srcIcon = fetchurl {
     url = "https://www.ej-technologies.com/assets/content/header-product-jprofiler@2x-24bc4d84bd2a4eb641a5c8531758ff7c.png";
@@ -52,12 +60,21 @@ let
   ];
 
   linux = stdenv.mkDerivation {
-    inherit pname version src desktopItems;
+    inherit
+      pname
+      version
+      src
+      desktopItems
+      ;
 
-    nativeBuildInputs = [ makeWrapper copyDesktopItems ];
+    nativeBuildInputs = [
+      makeWrapper
+      copyDesktopItems
+    ];
 
     installPhase = ''
       runHook preInstall
+
       cp -r . $out
 
       rm -f $out/bin/updater
@@ -71,20 +88,28 @@ let
 
       install -Dm644 "${srcIcon}" \
         "$out/share/icons/hicolor/scalable/apps/jprofiler.png"
+
       runHook postInstall
     '';
 
-    meta = meta // { platforms = lib.platforms.linux; };
+    meta = meta // {
+      platforms = lib.platforms.linux;
+    };
   };
 
   darwin = stdenv.mkDerivation {
     inherit pname version src;
 
-    nativeBuildInputs = [ makeWrapper _7zz ];
+    nativeBuildInputs = [
+      makeWrapper
+      _7zz
+    ];
 
     unpackPhase = ''
       runHook preUnpack
+
       7zz x $src -x!JProfiler/\[\]
+
       runHook postUnpack
     '';
 
@@ -92,13 +117,17 @@ let
 
     installPhase = ''
       runHook preInstall
+
       mkdir -p $out/{Applications,bin}
       cp -R ${nameApp}.app $out/Applications/
       makeWrapper $out/Applications/${nameApp}.app/Contents/MacOS/JavaApplicationStub $out/bin/${pname}
+
       runHook postInstall
     '';
 
-    meta = meta // { platforms = lib.platforms.darwin; };
+    meta = meta // {
+      platforms = lib.platforms.darwin;
+    };
   };
 in
 if stdenv.hostPlatform.isDarwin then darwin else linux
