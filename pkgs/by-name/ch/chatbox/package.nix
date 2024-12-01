8abb08841f1d46ff2ce4403efca1d6910cabe96d
Celio Grand <celiogrand@outlook.com>
2024-12-01 15:43:04 +0800

    chatbox: install extra files

--- pkgs/by-name/ch/chatbox/package.nix
+++ pkgs/by-name/ch/chatbox/package.nix
@@ -1,5 +1,9 @@
-{ lib, appimageTools, fetchurl }:
-appimageTools.wrapType2 rec {
+{
+  lib,
+  appimageTools,
+  fetchurl,
+}:
+let
   pname = "chatbox";
   version = "1.6.1";
 
@@ -7,6 +11,19 @@ appimageTools.wrapType2 rec {
     url = "https://download.chatboxai.app/releases/Chatbox-${version}-x86_64.AppImage";
     hash = "sha256-Gr7Un5K0AJFRzy9IMYAWmn66dijsWtKwrS/o/UW4Zrk=";
   };
+
+  appimageContents = appimageTools.extract { inherit pname version src; };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/xyz.chatboxapp.app.desktop $out/share/applications/${pname}.desktop
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/512x512/apps/xyz.chatboxapp.app.png $out/share/icons/hicolor/512x512/apps/${pname}.png
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace-fail 'Exec=AppRun' 'Exec=${pname}' \
+      --replace-fail 'Icon=xyz.chatboxapp.app' 'Icon=${pname}'
+  '';
 
   meta = with lib; {
     description = "Chatbox AI is an AI client application and smart assistant.";
