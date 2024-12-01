3345e02220886d8bc3404a8baa3a092173cf876c
Vincenzo Mantova <1962985+xworld21@users.noreply.github.com>
2024-12-01 09:14:27 +0000

    texlive.pkgs: export containers instead of the `build` derivations

--- pkgs/tools/typesetting/tex/texlive/build-texlive-package.nix
+++ pkgs/tools/typesetting/tex/texlive/build-texlive-package.nix
@@ -91,6 +91,11 @@ let
     lib.optional hasInfo "info";
   outputDrvs = lib.getAttrs outputs containers;
 
+  # derivation for texlive.pkgs
+  mainDrv = removeAttrs (if outputs == [ ] then fakeTeX else containers.${builtins.head outputs}) [ "outputSpecified" ]
+    # pretend to have all outputs (for use in build-tex-env.nix)
+    // { inherit outputs; };
+
   passthru = {
     # metadata
     inherit pname;
@@ -98,6 +103,8 @@ let
     version = version + extraVersion;
     # containers behave like specified outputs
     outputSpecified = true;
+    # derivation for top level texlivePackages
+    inherit build;
   } // lib.optionalAttrs (args ? deps) { tlDeps = args.deps; }
   // lib.optionalAttrs (args ? fontMaps) { inherit (args) fontMaps; }
   // lib.optionalAttrs (args ? formats) { inherit (args) formats; }
@@ -246,4 +253,4 @@ let
       fi
     '';
 in
-if outputs == [ ] then removeAttrs fakeTeX [ "outputSpecified" ] else build // outputDrvs
+mainDrv
