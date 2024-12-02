014764ffc9935c91e0813553f13c649ab81b3363
Jordan Williams <jordan@jwillikers.com>
2024-12-02 09:52:00 -0600

    cbconvert-gui: install icon to correct directory

--- pkgs/by-name/cb/cbconvert/gui.nix
+++ pkgs/by-name/cb/cbconvert/gui.nix
@@ -33,7 +33,7 @@ buildGoModule rec {
   ];
 
   postInstall = ''
-    install -D --mode=0644 --target-directory=$out/icons/hicolor/256x256/apps dist/linux/io.github.gen2brain.cbconvert.png
+    install -D --mode=0644 --target-directory=$out/share/icons/hicolor/256x256/apps dist/linux/io.github.gen2brain.cbconvert.png
     install -D --mode=0644 --target-directory=$out/share/applications/ dist/linux/io.github.gen2brain.cbconvert.desktop
     install -D --mode=0644 --target-directory=$out/share/metainfo dist/linux/io.github.gen2brain.cbconvert.metainfo.xml
     install -D --mode=0644 --target-directory=$out/share/thumbnailers dist/linux/io.github.gen2brain.cbconvert.thumbnailer
