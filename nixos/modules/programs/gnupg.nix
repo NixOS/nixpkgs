2ae24b12461ece3c37f2fbe6e8674377ae8a330e
Guilherme Baeta Fontes <48162143+gui-baeta@users.noreply.github.com>
2024-12-02 12:48:39 +0000

    nixos/gnupg: fix typo

--- nixos/modules/programs/gnupg.nix
+++ nixos/modules/programs/gnupg.nix
@@ -58,7 +58,7 @@ in
       defaultText = lib.literalMD "matching the configured desktop environment or `pkgs.pinentry-curses`";
       description = ''
         Which pinentry package to use. The path to the mainProgram as defined in
-        the package's meta attriutes will be set in /etc/gnupg/gpg-agent.conf.
+        the package's meta attributes will be set in /etc/gnupg/gpg-agent.conf.
         If not set by the user, it'll pick an appropriate flavor depending on the
         system configuration (qt flavor for lxqt and plasma5, gtk2 for xfce,
         gnome3 on all other systems with X enabled, curses otherwise).
