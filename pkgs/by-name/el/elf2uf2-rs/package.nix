c3883863ba6b3723dcdf40974651df0001692929
moni <lythe1107@gmail.com>
2024-12-03 04:45:55 +0800

    elf2uf2-rs: 2.0.0 -> 2.1.1

--- pkgs/by-name/el/elf2uf2-rs/package.nix
+++ pkgs/by-name/el/elf2uf2-rs/package.nix
@@ -1,26 +1,21 @@
-{ lib, stdenv, rustPlatform, fetchCrate, pkg-config, udev, CoreFoundation, DiskArbitration, Foundation }:
+{ lib, stdenv, rustPlatform, fetchCrate, pkg-config, udev }:
 
 rustPlatform.buildRustPackage rec {
   pname = "elf2uf2-rs";
-  version = "2.0.0";
+  version = "2.1.1";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-cmiCOykORue0Cg2uUUWa/nXviX1ddbGNC5gRKe+1kYs=";
+    hash = "sha256-7RS2OC00tjsSBYFvg0/FQf1HN515FdrmCoKhJBu4fvI=";
   };
 
   nativeBuildInputs = [
     pkg-config
   ];
 
-  buildInputs = lib.optional stdenv.hostPlatform.isLinux udev
-    ++ lib.optionals stdenv.hostPlatform.isDarwin [
-      CoreFoundation
-      DiskArbitration
-      Foundation
-    ];
+  buildInputs = lib.optional stdenv.hostPlatform.isLinux udev;
 
-  cargoHash = "sha256-TBH3pLB6vQVGnfShLtFPNKjciuUIuTkvp3Gayzo+X9E=";
+  cargoHash = "sha256-oz2XVqDWmv/8HLrIFL+xJinZNUdoWk4KVHDPZr2v+Ls=";
 
   meta = with lib; {
     description = "Convert ELF files to UF2 for USB Flashing Bootloaders";
