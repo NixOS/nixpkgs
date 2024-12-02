NixOS/nixpkgs

commit 11472f0d1b105ffd39fdf82a7a1671b9a6d10d40

busybox-sandbox-shell: replace pkgsStatic with useMusl

pkgs/os-specific/linux/busybox/sandbox-shell.nix
@@ -1,9 +1,12 @@
-{ busybox}:
+{ lib, stdenv, busybox, musl }:
 
 # Minimal shell for use as basic /bin/sh in sandbox builds
 busybox.override {
   enableStatic = true;
   enableMinimal = true;
+
+  useMusl = stdenv.hostPlatform.isGnu && lib.meta.availableOn stdenv.hostPlatform musl;
+
   extraConfig = ''
     CONFIG_FEATURE_FANCY_ECHO y
     CONFIG_FEATURE_SH_MATH y

pkgs/top-level/all-packages.nix
@@ -12328,12 +12328,7 @@ with pkgs;
       overrideCC stdenv buildPackages.llvmPackages.clangNoLibcxx
     else stdenv;
   };
-  busybox-sandbox-shell = callPackage ../os-specific/linux/busybox/sandbox-shell.nix {
-    # musl roadmap has RISC-V support projected for 1.1.20
-    busybox = if !stdenv.hostPlatform.isRiscV && !stdenv.hostPlatform.isLoongArch64 && stdenv.hostPlatform.libc != "bionic"
-              then pkgsStatic.busybox
-              else busybox;
-  };
+  busybox-sandbox-shell = callPackage ../os-specific/linux/busybox/sandbox-shell.nix { };
 
   cm-rgb = python3Packages.callPackage ../tools/system/cm-rgb { };
 

commit 89f4dd9602ba360710e6cc51bac21cab555bdeca

busybox-sandbox-shell: fix eval for musl
Setting useMusl all the time to a condition dependent on isGnu meant
that for musl platforms, useMusl ended up being set to false, tripping
the assertion in the busybox expression.  The correct thing to do is
to leave useMusl at its default value, except for in the specific case
where we want to force it to true.

Fixes: 11472f0d1b10 ("busybox-sandbox-shell: replace pkgsStatic with useMusl")

pkgs/os-specific/linux/busybox/sandbox-shell.nix
@@ -1,11 +1,9 @@
 { lib, stdenv, busybox, musl }:
 
 # Minimal shell for use as basic /bin/sh in sandbox builds
-busybox.override {
+busybox.override ({
   enableStatic = true;
   enableMinimal = true;
-
-  useMusl = stdenv.hostPlatform.isGnu && lib.meta.availableOn stdenv.hostPlatform musl;
 
   extraConfig = ''
     CONFIG_FEATURE_FANCY_ECHO y
@@ -26,4 +24,6 @@ busybox.override {
     CONFIG_ASH_PRINTF y
     CONFIG_ASH_TEST y
   '';
-}
+} // lib.optionalAttrs (stdenv.hostPlatform.isGnu && lib.meta.availableOn stdenv.hostPlatform musl) {
+  useMusl = true;
+})