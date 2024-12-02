27716e2acf1bb44d3c78b5bc24505fad9b57e458
David McFarland <corngood@gmail.com>
2024-12-02 11:27:00 -0400

    beatsabermodmanager: 0.0.5 -> 0.0.6

--- pkgs/by-name/be/beatsabermodmanager/package.nix
+++ pkgs/by-name/be/beatsabermodmanager/package.nix
@@ -5,27 +5,26 @@
 
   dotnetCorePackages,
 
+  nix-update-script,
+  testers,
   xdg-utils,
 }:
 
-buildDotnetModule rec {
+buildDotnetModule (finalAttrs: rec {
   pname = "beatsabermodmanager";
-  version = "0.0.5";
+  version = "0.0.6";
 
   src = fetchFromGitHub {
     owner = "affederaffe";
     repo = "BeatSaberModManager";
     rev = "v${version}";
-    hash = "sha256-HHWC+MAwJ+AMCuBzSuR7FbW3k+wLri0B9J1DftyfNEU=";
+    hash = "sha256-f7W3GTACxZ+FuCXCm20V8aDrJcsSBMNTUtxtFLPh8DY=";
     fetchSubmodules = true; # It vendors BSIPA-Linux
   };
 
-  dotnet-sdk = with dotnetCorePackages; combinePackages [
-    sdk_7_0
-    sdk_6_0
-  ];
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
 
-  dotnet-runtime = dotnetCorePackages.runtime_7_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
   projectFile = [ "BeatSaberModManager/BeatSaberModManager.csproj" ];
 
@@ -33,16 +32,12 @@ buildDotnetModule rec {
 
   nugetDeps = ./deps.nix;
 
-  preConfigureNuGet = ''
-    # This should really be in the upstream nuget.config
-    dotnet nuget add source https://api.nuget.org/v3/index.json \
-      -n nuget.org --configfile nuget.config
-  '';
-
   # Required for OneClick
   makeWrapperArgs = [
     ''--suffix PATH : "${lib.makeBinPath [ xdg-utils ]}"''
   ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = with lib; {
     description = "Yet another mod installer for Beat Saber, heavily inspired by ModAssistant";
@@ -65,4 +60,4 @@ buildDotnetModule rec {
     maintainers = with maintainers; [ atemu ];
     platforms = with platforms; linux;
   };
-}
+})
