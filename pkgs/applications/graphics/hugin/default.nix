6f1a52d0bffb982f42e7aa0e5f3fdeae680c5996
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-02 17:42:11 +0000

    hugin: 2023.0.0 -> 2024.0.1

--- pkgs/applications/graphics/hugin/default.nix
+++ pkgs/applications/graphics/hugin/default.nix
@@ -37,11 +37,11 @@
 
 stdenv.mkDerivation rec {
   pname = "hugin";
-  version = "2023.0.0";
+  version = "2024.0.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/hugin/hugin-${version}.tar.bz2";
-    hash = "sha256-BKOfzMYBfgVExjm9IjCUcsV001s0Vcut4fw4cOYxYys=";
+    hash = "sha256-E+wM3utOtjFJyDN2jT43Tnz1pqjY0C1QiFzklvBbp+Q=";
   };
 
   buildInputs = [
