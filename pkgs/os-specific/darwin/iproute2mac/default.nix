4bc254a7308259e0365f1fc7ffa76fffe59e8ee2
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-12-03 01:57:25 +0100

    darwin.iproute2mac: 1.4.1 -> 1.5.4

--- pkgs/os-specific/darwin/iproute2mac/default.nix
+++ pkgs/os-specific/darwin/iproute2mac/default.nix
@@ -3,24 +3,27 @@
   stdenv,
   fetchFromGitHub,
   darwin,
+  makeWrapper,
   python3,
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.4.1";
+  version = "1.5.4";
   pname = "iproute2mac";
 
   src = fetchFromGitHub {
     owner = "brona";
     repo = "iproute2mac";
     rev = "v${version}";
-    hash = "sha256-MaL8eb9UOZ71BL4Jvc6Od+EJ+F6j96n9a+vRnHeveIU=";
+    hash = "sha256-hmSqJ2gc0DOXUuFrp1ZG8usjFdo07zjV/1JLs5r/E04=";
   };
+
+  nativeBuildInputs = [ makeWrapper ];
 
   buildInputs = [ python3 ];
 
   postPatch = ''
-    substituteInPlace src/ip.py \
+    substituteInPlace src/iproute2mac.py \
       --replace-fail /sbin/ifconfig ${darwin.network_cmds}/bin/ifconfig \
       --replace-fail /sbin/route ${darwin.network_cmds}/bin/route \
       --replace-fail /usr/sbin/netstat ${darwin.network_cmds}/bin/netstat \
@@ -32,8 +35,12 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/bin
-    install -D -m 755 src/ip.py $out/bin/ip
+    mkdir -p $out/bin $out/libexec
+    install -D -m 755 src/iproute2mac.py $out/libexec/iproute2mac.py
+    install -D -m 755 src/ip.py $out/libexec/ip
+    install -D -m 755 src/bridge.py $out/libexec/bridge
+    makeWrapper $out/libexec/ip $out/bin/ip
+    makeWrapper $out/libexec/bridge $out/bin/bridge
 
     runHook postInstall
   '';
