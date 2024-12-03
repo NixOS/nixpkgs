48c86fdd87531b068c709fd948eaa4e0405dd62e
David McFarland <corngood@gmail.com>
2024-12-02 22:35:10 -0400

    fable: add version test
    (cherry picked from commit 79e336184f5c5d0a1d55abd721193863534e27fe)

--- pkgs/development/tools/fable/default.nix
+++ pkgs/development/tools/fable/default.nix
@@ -1,10 +1,21 @@
-{ buildDotnetGlobalTool, lib }:
+{
+  buildDotnetGlobalTool,
+  lib,
+  testers,
+}:
 
-buildDotnetGlobalTool {
+buildDotnetGlobalTool (finalAttrs: {
   pname = "fable";
   version = "4.20.0";
 
   nugetHash = "sha256-K3908gEbl9crT4wmZfBtvag5Z6qYABfalBfLZlqZuDk=";
+
+  passthru.tests = testers.testVersion {
+    package = finalAttrs.finalPackage;
+    # the version is written with an escape sequence for colour, and I couldn't
+    # find a way to disable it
+    version = "[37m${finalAttrs.version}";
+  };
 
   meta = with lib; {
     description = "Fable is an F# to JavaScript compiler";
@@ -13,6 +24,9 @@ buildDotnetGlobalTool {
     changelog = "https://github.com/fable-compiler/fable/releases/tag/v${version}";
     license = licenses.mit;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ anpin mdarocha ];
+    maintainers = with maintainers; [
+      anpin
+      mdarocha
+    ];
   };
-}
+})
