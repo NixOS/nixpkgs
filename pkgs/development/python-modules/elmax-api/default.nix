355f04597793a5545ea0cab13f3c1ee6f1315878
Fabian Affolter <mail@fabian-affolter.ch>
2024-12-03 23:47:09 +0100

    python312Packages.elmax-api: 0.0.6.2 -> 0.0.6.3
    Diff: https://github.com/albertogeniola/elmax-api/compare/refs/tags/v0.0.6.2...v0.0.6.3
    
    Changelog: https://github.com/albertogeniola/elmax-api/releases/tag/v0.0.6.3

--- pkgs/development/python-modules/elmax-api/default.nix
+++ pkgs/development/python-modules/elmax-api/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "elmax-api";
-  version = "0.0.6.2";
+  version = "0.0.6.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "albertogeniola";
     repo = pname;
     rev = "refs/tags/v${version}";
-    hash = "sha256-71ewBzAMWncvgXII2VAcbSDwudxqA+MBCl+TCCwTxa4=";
+    hash = "sha256-jnm1AFnPxZIgD815ZFxV/i9ar4cZfsYJ0+xDpM3hKmg=";
   };
 
   build-system = [ setuptools ];
