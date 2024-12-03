144ea0e222682d4aeb8b28091d1bfb42734f5dd0
Robert Hensing <robert@roberthensing.nl>
2024-12-03 17:09:36 +0100

    testers.hasPkgConfigModules: Shorten derivation name
    Overly long names are annoying, and grab attention that these tests
    usually don't need.
    Keep it short and sweet.

--- pkgs/build-support/testers/hasPkgConfigModules/tester.nix
+++ pkgs/build-support/testers/hasPkgConfigModules/tester.nix
@@ -1,10 +1,20 @@
 # Static arguments
 { lib, runCommand, pkg-config }:
 
+let
+  genTestName = moduleNames:
+    # An overly long name made the progress bar weird and distraction, and same
+    # for the build log prefix.
+    if lib.length moduleNames > 1 then
+      "modules"
+    else
+      lib.head moduleNames;
+in
+
 # Tester arguments
 { package,
   moduleNames ? package.meta.pkgConfigModules,
-  testName ? "check-pkg-config-${lib.concatStringsSep "-" moduleNames}",
+  testName ? "check-pkg-config-${genTestName moduleNames}",
   version ? package.version or null,
   versionCheck ? false,
 }:
