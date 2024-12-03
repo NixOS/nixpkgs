596eb72246b60689a821ffb2d4b92fbcc7c40430
Anders Kaseorg <andersk@mit.edu>
2024-12-02 19:02:56 -0800

    twelf: Fix Emacs mode syntax for Emacs ≥ 27
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pkgs/by-name/tw/twelf/package.nix
+++ pkgs/by-name/tw/twelf/package.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, pkg-config, smlnj, rsync }:
+{ lib, stdenv, fetchurl, fetchpatch, pkg-config, smlnj, rsync }:
 
 stdenv.mkDerivation rec {
   pname = "twelf";
@@ -8,6 +8,14 @@ stdenv.mkDerivation rec {
     url = "http://twelf.plparty.org/releases/twelf-src-${version}.tar.gz";
     sha256 = "0fi1kbs9hrdrm1x4k13angpjasxlyd1gc3ys8ah54i75qbcd9c4i";
   };
+
+  patches = [
+    # Fix Emacs old-style backquotes: https://github.com/standardml/twelf/pull/3
+    (fetchpatch {
+      url = "https://github.com/standardml/twelf/commit/7b3f3dbb8b8ec8d16d843875fce1e2bd6a50e3ae.patch";
+      hash = "sha256-cSrgQFRPL+4zRtFXv3rLsAasjLfal0TZpXasEUtUSHc=";
+    })
+  ];
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ smlnj rsync ];
