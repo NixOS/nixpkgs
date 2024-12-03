366263d96fcf69bbde01ed4a32c9de6571239846
Nick Cao <nickcao@nichi.co>
2024-12-03 08:07:03 -0500

    keycloak: 26.0.6 -> 26.0.7
    Diff: https://github.com/keycloak/keycloak/compare/26.0.6...26.0.7
    Upgrading: https://www.keycloak.org/docs/latest/upgrading/index.html#migrating-to-26-0-7

--- pkgs/by-name/ke/keycloak/package.nix
+++ pkgs/by-name/ke/keycloak/package.nix
@@ -18,11 +18,11 @@ let
   '';
 in stdenv.mkDerivation rec {
   pname = "keycloak";
-  version = "26.0.6";
+  version = "26.0.7";
 
   src = fetchzip {
     url = "https://github.com/keycloak/keycloak/releases/download/${version}/keycloak-${version}.zip";
-    hash = "sha256-c5f2fxba2VlgRG1kPluWmHgZsokj3MTiZM96lul9GV8=";
+    hash = "sha256-yIv9gAjCfzjWDLZHQbgGEjhMefY1idzZTEbqVyXjFdw=";
   };
 
   nativeBuildInputs = [ makeWrapper jre ];
