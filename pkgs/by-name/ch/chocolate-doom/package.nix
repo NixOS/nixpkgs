df106844fa229a47c5959c4b393bb4f2b828d143
Gliczy <129636582+Gliczy@users.noreply.github.com>
2024-12-03 10:22:54 +0100

    chocolate-doom: remove hydraPlatforms

--- pkgs/by-name/ch/chocolate-doom/package.nix
+++ pkgs/by-name/ch/chocolate-doom/package.nix
@@ -53,7 +53,6 @@ stdenv.mkDerivation (finalAttrs: {
     mainProgram = "chocolate-doom";
     license = lib.licenses.gpl2Plus;
     platforms = lib.platforms.unix;
-    hydraPlatforms = lib.platforms.linux; # darwin times out
     maintainers = with lib.maintainers; [ Gliczy ];
   };
 })
