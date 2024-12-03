568972716244e03727a29da0d889a14c4238f4bc
Krisztian Szabo <thenonameguy24@gmail.com>
2024-12-03 23:19:25 +0100

    graphql-inspector: init at 5.0.7

--- pkgs/development/node-packages/aliases.nix
+++ pkgs/development/node-packages/aliases.nix
@@ -109,6 +109,7 @@ mapAliases {
   grammarly-languageserver = throw "grammarly-languageserver was removed because it requires EOL Node.js 16"; # added 2024-07-15
   inherit (pkgs) graphite-cli; # added 2024-01-25
   inherit (pkgs) graphqurl; # added 2023-08-19
+  inherit (pkgs) graphql-inspector; # added 2024-12-03
   gtop = pkgs.gtop; # added 2023-07-31
   hs-client = pkgs.hsd; # added 2023-08-20
   inherit (pkgs) hsd; # added 2023-08-19
