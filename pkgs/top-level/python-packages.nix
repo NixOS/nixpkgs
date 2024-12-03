db447cab67b88f9ac0b8125117fe2a5fed460b37
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-12-03 15:29:54 +0100

    python3Packages.torchtnt: init at v0.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16152,6 +16152,8 @@ self: super: with self; {
 
   torchsummary = callPackage ../development/python-modules/torchsummary { };
 
+  torchtnt = callPackage ../development/python-modules/torchtnt { };
+
   torchvision = callPackage ../development/python-modules/torchvision { };
 
   torchvision-bin = callPackage ../development/python-modules/torchvision/bin.nix {
