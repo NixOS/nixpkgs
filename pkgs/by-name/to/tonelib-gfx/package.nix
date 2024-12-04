89b7c3c57a24b40cc4b3264228c8dc8582be606b
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-12-04 16:05:35 +0100

    tonelib-gfx: 4.8.5 -> 4.8.7
    Added comment for fetching the latest vesion and release link.
    
    Example:
    ```sh
    curl --silent https://tonelib.net/downloads.html | \
        grep -P 'ToneLib GFX</h3' -A3 | \
        sed -nE 's/^.*Version: ([0-9.]+).*/\1/p'
    
    4.8.7
    ```
    
    ```sh
    $ curl --head 'https://www.tonelib.net/download.php?id=gfx&os=lnx'
    
    HTTP/2 302
    server: nginx
    date: Wed, 04 Dec 2024 15:05:49 GMT
    content-type: text/html; charset=UTF-8
    location: https://tonelib.vip/download/24-10-24/ToneLib-GFX-amd64.deb
    ```

--- pkgs/by-name/to/tonelib-gfx/package.nix
+++ pkgs/by-name/to/tonelib-gfx/package.nix
@@ -17,11 +17,21 @@
 
 stdenv.mkDerivation rec {
   pname = "tonelib-gfx";
-  version = "4.8.5";
+  version = "4.8.7";
 
+  # It's hard to find out when a release happens and what version that release is,
+  #  without visiting the site directly.
+  #
+  # The following command can retrieve the latest released version.
+  # curl --silent https://tonelib.net/downloads.html | \
+  #     grep -P 'ToneLib GFX</h3' -A3 | \
+  #     sed -nE 's/^.*Version: ([0-9.]+).*/\1/p'
+  #
+  # The following command gives us the URL for the latest release without intermediate redirects.
+  # curl --head 'https://www.tonelib.net/download.php?id=gfx&os=lnx'
   src = fetchurl {
-    url = "https://tonelib.vip/download/24-10-03/ToneLib-GFX-amd64.deb";
-    hash = "sha256-RG5rliF4/9LDd07i5dSFQzTGPqyF6UmTfatKb59LZA4=";
+    url = "https://tonelib.vip/download/24-10-24/ToneLib-GFX-amd64.deb";
+    hash = "sha256-2ao6tTRbPMpE2Y/7/gwQN3G5Z6Uu+SQel9o1ejwD9v4=";
   };
 
   nativeBuildInputs = [
