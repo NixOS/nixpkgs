00bcf5cb457368df62bd1448e6f34b3c8f9792ff
Thomas Gerbet <thomas@gerbet.me>
2024-12-04 19:22:06 +0100

    python3Packages.django_5: 5.1.3 -> 5.1.4
    Fixes CVE-2024-53907 and CVE-2024-53908.
    
    Changes:
    https://docs.djangoproject.com/en/dev/releases/5.1.4/

--- pkgs/development/python-modules/django/5.nix
+++ pkgs/development/python-modules/django/5.nix
@@ -43,7 +43,7 @@
 
 buildPythonPackage rec {
   pname = "django";
-  version = "5.1.3";
+  version = "5.1.4";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -52,7 +52,7 @@ buildPythonPackage rec {
     owner = "django";
     repo = "django";
     rev = "refs/tags/${version}";
-    hash = "sha256-TqOVe+QkwNx/SpI/6X/AQaqLHk3LDSupoRl3RKL6kac=";
+    hash = "sha256-rE/aErydql5UjS/IiLpDtA3YOsAa+0uRyo40RsjCGLc=";
   };
 
   patches =
