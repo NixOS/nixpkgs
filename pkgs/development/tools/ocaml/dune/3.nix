553c0c781c29c5487b85858c37975aeb36f98cd0
Sora Morimoto <sora@morimoto.io>
2024-12-05 04:37:45 +0900

    dune_3: 3.16.1 -> 3.17.0
    Co-authored-by: Vincent Laporte <Vincent.Laporte@gmail.com>
    (cherry picked from commit df6fb983d2ffb1bab854bd245e7f47175767d8c7)

--- pkgs/development/tools/ocaml/dune/3.nix
+++ pkgs/development/tools/ocaml/dune/3.nix
@@ -6,11 +6,11 @@ else
 
 stdenv.mkDerivation rec {
   pname = "dune";
-  version = "3.16.1";
+  version = "3.17.0";
 
   src = fetchurl {
     url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-    hash = "sha256-t4GuIPh2E8KhG9BxeAngBHDILWFeFSZPmmTgMwUaw94=";
+    hash = "sha256-LDqmxB7Tnj1sGiktdfSAa9gDEIQa/FFnOqWc6cgWUHw=";
   };
 
   nativeBuildInputs = [ ocaml findlib ];
