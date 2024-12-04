9c1ac764a23b24a2de59212f2d37e0881cf36380
Florian Agbuya <fa@m-labs.ph>
2024-12-04 12:08:00 +0800

    python3Packages.hightime: init at 0.2.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5818,6 +5818,8 @@ self: super: with self; {
 
   highspy = callPackage ../development/python-modules/highspy { };
 
+  hightime = callPackage ../development/python-modules/hightime { };
+
   hid = callPackage ../development/python-modules/hid {
     inherit (pkgs) hidapi;
   };
