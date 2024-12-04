7c393176966921a5f51d3604c04178ea8e8873db
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-04 00:43:16 +0000

    linuxPackages.r8125: 9.013.02 -> 9.014.01

--- pkgs/os-specific/linux/r8125/default.nix
+++ pkgs/os-specific/linux/r8125/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   pname = "r8125";
   # On update please verify (using `diff -r`) that the source matches the
   # realtek version.
-  version = "9.013.02";
+  version = "9.014.01";
 
   # This is a mirror. The original website[1] doesn't allow non-interactive
   # downloads, instead emailing you a download link.
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
     owner = "louistakepillz";
     repo = "r8125";
     rev = version;
-    sha256 = "sha256-i45xKF5WVN+nNhpD6HWZHvGgxuaD/YhMHERqW8/bC5Y=";
+    sha256 = "sha256-vYgAOmKFQZDKrZsS3ynXB0DrT3wU0JWzNTYO6FyMG9M=";
   };
 
   hardeningDisable = [ "pic" ];
