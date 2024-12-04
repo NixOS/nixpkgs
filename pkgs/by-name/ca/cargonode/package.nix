284d5e77d5fe86643a34f3cf3de388b4f3e204f9
xosnrdev <successxodev@gmail.com>
2024-12-03 21:53:34 -0500

    cargonode: nixfmt
    Fix formatting of package.nix using nixfmt.

--- pkgs/by-name/ca/cargonode/package.nix
+++ pkgs/by-name/ca/cargonode/package.nix
@@ -1,9 +1,10 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-, pkg-config
-, bzip2
-, git
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  bzip2,
+  git,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -19,20 +20,19 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-v+Fs2VJrpnIOk9nPRanYYChlR7WOfkXF1kbYOKjOUYc=";
 
-  nativeBuildInputs = [
-    pkg-config
-  ];
+  nativeBuildInputs = [ pkg-config ];
 
-  nativeCheckInputs = [
-    git
-  ];
+  nativeCheckInputs = [ git ];
 
   meta = with lib; {
     description = "Unified tooling for Node.js";
     mainProgram = "cargonode";
     homepage = "https://github.com/xosnrdev/cargonode";
     changelog = "https://github.com/xosnrdev/cargonode/blob/${version}/CHANGELOG.md";
-    license = with licenses; [ asl20 /* or */ mit ];
+    license = with licenses; [
+      asl20 # or
+      mit
+    ];
     maintainers = with maintainers; [ xosnrdev ];
   };
 }
