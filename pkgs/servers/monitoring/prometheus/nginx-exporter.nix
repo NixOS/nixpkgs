2df2c8b25a8346e3d47551679e6021a11d264d08
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-04 20:16:49 +0000

    prometheus-nginx-exporter: 1.3.0 -> 1.4.0

--- pkgs/servers/monitoring/prometheus/nginx-exporter.nix
+++ pkgs/servers/monitoring/prometheus/nginx-exporter.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "nginx_exporter";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchFromGitHub {
     owner = "nginxinc";
     repo = "nginx-prometheus-exporter";
     rev = "v${version}";
-    sha256 = "sha256-TcQXYZsr3hjE93GfeBt8AJLuQQA7UM+wv1SmCnqON+M=";
+    sha256 = "sha256-aA6wQjTVkyEx+f1xBdrvN05NGqnJkgLVwrNLSh5+Ll0=";
   };
 
-  vendorHash = "sha256-qmyqAbwQYgUFFQwCLakQGta2XMGCbCOvCB93S8/TWIs=";
+  vendorHash = "sha256-ua3Cm1VXRs3M58vJ/fEt7SH+ZYegt0WjOGRV/iU8qZk=";
 
   ldflags = let t = "github.com/prometheus/common/version"; in [
     "-s"
