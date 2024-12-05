fae10f50108a712007b68c187ba559d24d38f420
Dmitry Ivankov <boltzmannrain@gmail.com>
2024-12-06 00:24:02 +0100

    bazel_8: init at 8.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7546,6 +7546,16 @@ with pkgs;
     bazel_self = bazel_7;
   };
 
+  bazel_8 = darwin.apple_sdk_11_0.callPackage ../by-name/ba/bazel_8/package.nix {
+    inherit (darwin) sigtool;
+    inherit (darwin.apple_sdk_11_0.frameworks) CoreFoundation CoreServices Foundation IOKit;
+    buildJdk = jdk21_headless;
+    runJdk = jdk21_headless;
+    stdenv = if stdenv.hostPlatform.isDarwin then darwin.apple_sdk_11_0.stdenv
+      else if stdenv.cc.isClang then llvmPackages.stdenv
+      else stdenv;
+  };
+
   buildifier = bazel-buildtools;
   buildozer = bazel-buildtools;
   unused_deps = bazel-buildtools;
