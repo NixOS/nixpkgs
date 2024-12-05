bf2b5c745b0ccb4229a1fe1ce676277100336234
dish <pyrox@pyrox.dev>
2024-12-05 12:29:06 -0500

    python312Packages.azure-mgmt-reservations: modernize

--- pkgs/development/python-modules/azure-mgmt-reservations/default.nix
+++ pkgs/development/python-modules/azure-mgmt-reservations/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   msrest,
   azure-common,
   azure-mgmt-core,
@@ -12,9 +13,7 @@
 buildPythonPackage rec {
   pname = "azure-mgmt-reservations";
   version = "2.3.0";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.6";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
@@ -22,7 +21,9 @@ buildPythonPackage rec {
     hash = "sha256-BHCFEFst5jfyIEo0hm86belpxW7EygZCBJ8PTqzqHKc=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     msrest
     azure-common
     azure-mgmt-core
@@ -31,10 +32,10 @@ buildPythonPackage rec {
   # has no tests
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     description = "This is the Microsoft Azure Reservations Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
-    license = licenses.mit;
-    maintainers = with maintainers; [ maxwilson ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ maxwilson ];
   };
 }
