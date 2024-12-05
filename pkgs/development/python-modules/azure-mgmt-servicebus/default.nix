e4391d9bd440159f6f32d39c827c6a1fb4eb73f0
dish <pyrox@pyrox.dev>
2024-12-05 12:46:59 -0500

    python312Packages.azure-mgmt-servicebus: modernize

--- pkgs/development/python-modules/azure-mgmt-servicebus/default.nix
+++ pkgs/development/python-modules/azure-mgmt-servicebus/default.nix
@@ -5,15 +5,13 @@
   msrest,
   azure-common,
   azure-mgmt-core,
-  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "azure-mgmt-servicebus";
   version = "8.2.0";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.7";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
@@ -21,7 +19,9 @@ buildPythonPackage rec {
     hash = "sha256-i+kgjxQdmnifaNuNIZdU/3gGn9j5OQ6fdkS7laO+nsI=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     msrest
     azure-common
     azure-mgmt-core
@@ -30,10 +30,14 @@ buildPythonPackage rec {
   # Module has no tests
   doCheck = false;
 
-  meta = with lib; {
+  pythonNamespaces = [ "azure.mgmt" ];
+
+  pythonImportsCheck = [ "azure.mgmt.servicebus" ];
+
+  meta = {
     description = "This is the Microsoft Azure Service Bus Management Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
-    license = licenses.mit;
-    maintainers = with maintainers; [ maxwilson ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ maxwilson ];
   };
 }
