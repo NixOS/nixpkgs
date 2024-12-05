e4b06f587ee4317ed44d4ebc1a4e9c6f3022f07c
John Wang <32222098+jzw2@users.noreply.github.com>
2024-12-05 23:36:16 +0100

    sptlrx: 1.1.0 -> 1.2.2 (#360405)
    Update pkgs/by-name/sp/sptlrx/package.nix
    
    Co-authored-by: seth <getchoo@tuta.io>

--- pkgs/by-name/sp/sptlrx/package.nix
+++ pkgs/by-name/sp/sptlrx/package.nix
@@ -2,18 +2,25 @@
 
 buildGoModule rec {
   pname = "sptlrx";
-  version = "1.1.0";
+  version = "1.2.2";
 
   src = fetchFromGitHub {
     owner = "raitonoberu";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-6GbefTWrhH6RdASmSrugd4xESkwqFVF5qwFmf0JUDTY=";
+    hash = "sha256-b8ALhEjolH0RH+I9HVQeOagPBi2isLNUxqKdj5u2O9s=";
   };
 
-  vendorHash = "sha256-Ll5jUjpx4165BAE86/z95i4xa8fdKlfxqrUc/gDLqJ0=";
+  vendorHash = "sha256-pExSQcYjqliZZg/91t52yk6UJ4QCbpToMpONIFUNkwc=";
 
   ldflags = [ "-s" "-w" ];
+
+  checkFlags =
+    let
+      # Requires network access
+      skippedTests = [ "TestGetIndex" ];
+    in
+    [ "-skip=^${lib.concatStringsSep "$|^" skippedTests}$" ];
 
   passthru = {
     updateScript = nix-update-script { };
@@ -26,7 +33,8 @@ buildGoModule rec {
   meta = with lib; {
     description = "Spotify lyrics in your terminal";
     homepage = "https://github.com/raitonoberu/sptlrx";
-    changelog = "https://github.com/raitonoberu/sptlrx/releases/tag/v${version}";
+    changelog =
+      "https://github.com/raitonoberu/sptlrx/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ MoritzBoehme ];
     mainProgram = "sptlrx";
