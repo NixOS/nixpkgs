0cd4fe6847dd30e47991a0e30eb8ffb64ab71eaf
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-05 15:32:24 +0000

    python312Packages.botocore-stubs: 1.35.72 -> 1.35.76

--- pkgs/development/python-modules/botocore-stubs/default.nix
+++ pkgs/development/python-modules/botocore-stubs/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "botocore-stubs";
-  version = "1.35.72";
+  version = "1.35.76";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "botocore_stubs";
     inherit version;
-    hash = "sha256-bdIERxT6J4JNSbSejnjNlR9Eo8w8vcSBFNgac7e+DTI=";
+    hash = "sha256-yXegSUgdUKFL8tsO8VAgt2c0/2KNS44Od7jRxlMYNp4=";
   };
 
   nativeBuildInputs = [ setuptools ];
