7376dd3c393d1a5644e5c2a086b27453cc020a2d
terrorbyte <1601039+terrorbyte@users.noreply.github.com>
2024-12-04 17:48:20 -0700

    qcp: init at 0.3.0
    Adds the `qcp` package. This package is an experimental high-performance remote file copy utility used for long-distance transfers.

---
+++ pkgs/by-name/qc/qcp/package.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "qcp";
+  version = "0.3.0";
+
+  src = fetchFromGitHub {
+    owner = "crazyscot";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-9nJ01OPAU60veLpL2BlSSUTMu/xdUBDVkV0YEFNQ3FU=";
+  };
+
+  cargoHash = "sha256-7LfwJa64ZUE0i8/bMecMoxzykJvuINzXctopyh4Qlak=";
+
+  checkFlags = [
+    # SSH home directory tests will not work in nix sandbox
+    "--skip=config::ssh::includes::test::home_dir"
+  ];
+
+  meta = {
+    description = "The QUIC Copier (qcp) is an experimental high-performance remote file copy utility for long-distance internet connections.";
+    homepage = "https://github.com/crazyscot/qcp";
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ poptart ];
+  };
+
+  postInstall = ''
+    mkdir -p $out/share/man/man{1,5} $out/share/doc/qcp $out/etc/systcl.d/
+    install -Dm644 $src/misc/qcp.1 $out/share/man/man1/qcp.1
+    install -Dm644 $src/misc/qcp_config.5 $out/share/man/man5/qcp_config.5
+    install -Dm644 $src/misc/20-qcp.conf $out/etc/sysctl.d/20-qcp.conf
+    install -Dm644 $src/misc/qcp.conf $out/etc/qcp.conf
+    install -Dm644 $src/README.md $out/share/doc/qcp/README.md
+    install -Dm644 $src/LICENSE $out/share/doc/qcp/LICENSE
+  '';
+}
