af547db43325ad84e5f8b07b63ec1c5060e05464
Thomas Gerbet <thomas@gerbet.me>
2024-12-06 23:58:16 +0100

    yara-x: 0.11.0 -> 0.11.1
    Changes:
    https://github.com/VirusTotal/yara-x/releases/tag/v0.11.1

--- pkgs/by-name/ya/yara-x/package.nix
+++ pkgs/by-name/ya/yara-x/package.nix
@@ -10,16 +10,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "yara-x";
-  version = "0.11.0";
+  version = "0.11.1";
 
   src = fetchFromGitHub {
     owner = "VirusTotal";
     repo = "yara-x";
     rev = "refs/tags/v${version}";
-    hash = "sha256-14YHaaZpqB8448MGdKsYqxZ4N/+p92khQWRov3cO/eU=";
+    hash = "sha256-eRA1Vov+K7nLOkvcC8KS0S2eNSSDn++UcQqDFVJOhME=";
   };
 
-  cargoHash = "sha256-gcRLnPlNVzA5nxdviWz8dDzblFujytYPrt7eyFgm5Lc=";
+  cargoHash = "sha256-iWgfI5jiEbBHkew82Ej7Ku17JDVI4O0iiOxs9lxEJS4=";
 
   nativeBuildInputs = [ cmake installShellFiles ];
 
