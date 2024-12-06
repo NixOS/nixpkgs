bffbca7defc93915a8b36f99ee4e7fd759fdfc8b
Sarah Clark <seclark@nextquestion.net>
2024-12-06 10:55:53 -0800

    python312Packages.scalene: 1.5.47 -> 1.5.49

--- pkgs/development/python-modules/scalene/default.nix
+++ pkgs/development/python-modules/scalene/default.nix
@@ -39,7 +39,7 @@ in
 
 buildPythonPackage rec {
   pname = "scalene";
-  version = "1.5.47";
+  version = "1.5.49";
   pyproject = true;
   disabled = pythonOlder "3.9";
 
@@ -47,7 +47,7 @@ buildPythonPackage rec {
     owner = "plasma-umass";
     repo = "scalene";
     rev = "v${version}";
-    hash = "sha256-NrXXwzp+NYdie7p5xsJa7lMGgKED3FCzeAjO7x+USHU=";
+    hash = "sha256-Ivce90+W9NBMQjebj3zCB5eqDJydT8OTPYy4fjbybgI=";
   };
 
   patches = [
