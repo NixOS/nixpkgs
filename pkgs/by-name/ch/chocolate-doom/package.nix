fd39bbae626b449f1737983ddcaeb77a4fb889bf
Gliczy <129636582+Gliczy@users.noreply.github.com>
2024-12-06 19:44:40 +0100

    chocolate-doom: 3.0.1 -> 3.1.0 (#361411)
    * chocolate-doom: format with `nixfmt-rfc-style`
    
    * chocolate-doom: refactor
    
    * chocolate-doom: update homepage
    
    * chocolate-doom: add mainProgram
    
    * chocolate-doom: add myself as maintainer
    
    * chocolate-doom: 3.0.1 -> 3.1.0
    
    * chocolate-doom: remove hydraPlatforms
    
    * chocolate-doom: add `meta.changelog`
    
    * chocolate-doom: add missing dependencies

--- pkgs/by-name/ch/chocolate-doom/package.nix
+++ pkgs/by-name/ch/chocolate-doom/package.nix
@@ -1,31 +1,29 @@
-{ lib, stdenv, autoreconfHook, pkg-config, SDL2, SDL2_mixer, SDL2_net
-, fetchFromGitHub, fetchpatch, python3 }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  libpng,
+  libsamplerate,
+  pkg-config,
+  python3,
+  SDL2,
+  SDL2_mixer,
+  SDL2_net,
+}:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "chocolate-doom";
-  version = "3.0.1";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "chocolate-doom";
-    repo = pname;
-    rev = "${pname}-${version}";
-    sha256 = "1zlcqhd49c5n8vaahgaqrc2y10z86xng51sbd82xm3rk2dly25jp";
+    repo = "chocolate-doom";
+    rev = "refs/tags/chocolate-doom-${finalAttrs.version}";
+    hash = "sha256-yDPfqCuzRbDhOQisIDAGo2bmmMjT+0lds5xc9C2pqoU=";
   };
 
-  patches = [
-    # Pull upstream patch to fix builx against gcc-10:
-    #   https://github.com/chocolate-doom/chocolate-doom/pull/1257
-    (fetchpatch {
-      name = "fno-common.patch";
-      url = "https://github.com/chocolate-doom/chocolate-doom/commit/a8fd4b1f563d24d4296c3e8225c8404e2724d4c2.patch";
-      sha256 = "1dmbygn952sy5n8qqp0asg11pmygwgygl17lrj7i0fxa0nrhixhj";
-    })
-  ];
-
-  outputs = [ "out" "man" ];
-
   postPatch = ''
-    sed -e 's#/games#/bin#g' -i src{,/setup}/Makefile.am
     patchShebangs --build man/{simplecpp,docgen}
   '';
 
@@ -35,15 +33,31 @@ stdenv.mkDerivation rec {
     # for documentation
     python3
   ];
-  buildInputs = [ SDL2 SDL2_mixer SDL2_net ];
+
+  buildInputs = [
+    libpng
+    libsamplerate
+    SDL2
+    SDL2_mixer
+    SDL2_net
+  ];
+
+  outputs = [
+    "out"
+    "man"
+  ];
+
   enableParallelBuilding = true;
 
+  strictDeps = true;
+
   meta = {
-    homepage = "http://chocolate-doom.org/";
+    homepage = "https://www.chocolate-doom.org";
+    changelog = "https://github.com/chocolate-doom/chocolate-doom/releases/tag/chocolate-doom-${finalAttrs.version}";
     description = "Doom source port that accurately reproduces the experience of Doom as it was played in the 1990s";
+    mainProgram = "chocolate-doom";
     license = lib.licenses.gpl2Plus;
     platforms = lib.platforms.unix;
-    hydraPlatforms = lib.platforms.linux; # darwin times out
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ Gliczy ];
   };
-}
+})
