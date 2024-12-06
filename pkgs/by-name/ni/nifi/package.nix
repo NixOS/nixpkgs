b30c5a1d94a13d1385e0b1587fc62f97fa4a21a9
aucub <78630225+aucub@users.noreply.github.com>
2024-12-06 23:13:00 +0800

    nifi: 1.28.0 -> 2.0.0

--- pkgs/by-name/ni/nifi/package.nix
+++ pkgs/by-name/ni/nifi/package.nix
@@ -1,46 +1,50 @@
-{ lib, stdenv, fetchzip, makeWrapper, jdk11, nixosTests }:
-
+{
+  lib,
+  stdenv,
+  fetchzip,
+  jdk,
+  nixosTests,
+}:
 stdenv.mkDerivation rec {
   pname = "nifi";
-  version = "1.28.0";
+  version = "2.0.0";
 
   src = fetchzip {
     url = "mirror://apache/nifi/${version}/nifi-${version}-bin.zip";
-    hash = "sha256-EQqosdwNdaSUIcP3QlvzJZhDNu1JbHg08FlnZo6ogmQ=";
+    hash = "sha256-uEMDmF758wBk/RlPCJV5Hd1G1lORc094R/rXnaI79HA=";
   };
 
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ jdk11 ];
+  buildInputs = [ jdk ];
 
   installPhase = ''
-    cp -r ../$sourceRoot $out
-    rm -f $out/bin/*bat
-    rm -rf $out/extensions
-    mkdir -p $out/share/nifi
-    mv $out/conf $out/share/nifi
-    mv $out/docs $out/share/nifi
-    mv $out/{LICENSE,NOTICE,README} $out/share/nifi
+    runHook preInstall
 
+    cp -r . $out
+    mkdir -p $out/share/doc/nifi $out/share/nifi
+    mv $out/{LICENSE,NOTICE,README} $out/share/doc/nifi
     substituteInPlace $out/bin/nifi.sh \
-      --replace "/bin/sh" "${stdenv.shell}"
+      --replace-fail "/bin/sh" "${stdenv.shell}"
     substituteInPlace $out/bin/nifi-env.sh \
-      --replace "#export JAVA_HOME=/usr/java/jdk1.8.0/" "export JAVA_HOME=${jdk11}"
+      --replace-fail "#export JAVA_HOME=/usr/java/jdk/" "export JAVA_HOME=${jdk}/" \
+      --replace-fail "export NIFI_PID_DIR" "export NIFI_PID_DIR=\$HOME/.nifi/run" \
+      --replace-fail "export NIFI_LOG_DIR" "export NIFI_LOG_DIR=\$HOME/.nifi/logs"
+    ln -s $out/conf $out/share/nifi/conf
+
+    runHook postInstall
   '';
 
-  passthru = {
-    tests.nifi = nixosTests.nifi;
-  };
+  passthru.tests.nifi = nixosTests.nifi;
 
-  meta = with lib; {
+  meta = {
     description = "Easy to use, powerful, and reliable system to process and distribute data";
     longDescription = ''
       Apache NiFi supports powerful and scalable directed graphs of data routing,
       transformation, and system mediation logic.
     '';
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
     homepage = "https://nifi.apache.org";
     platforms = [ "x86_64-linux" ];
-    sourceProvenance = with sourceTypes; [ binaryBytecode ];
-    maintainers = with maintainers; [ izorkin ];
+    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
+    maintainers = with lib.maintainers; [ izorkin ];
   };
 }
