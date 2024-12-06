7c87ea18820bfa3b65e9d883cc843206e2c53e60
Ulysses Zhan <UlyssesZhan@gmail.com>
2024-12-05 21:00:36 -0800

    megacmd: add darwin support

--- pkgs/by-name/me/megacmd/package.nix
+++ pkgs/by-name/me/megacmd/package.nix
@@ -25,17 +25,29 @@
 let
   pname = "megacmd";
   version = "1.7.0";
+  srcOptions =
+    if stdenv.isLinux then
+      {
+        tag = "${version}_Linux";
+        hash = "sha256-UlSqwM8GQKeG8/K0t5DbM034NQOeBg+ujNi/MMsVCuM=";
+      }
+    else
+      {
+        tag = "${version}_macOS";
+        hash = "sha256-UlSqwM8GQKeG8/K0t5DbM034NQOeBg+ujNi/MMsVCuM=";
+      };
 in
 stdenv.mkDerivation {
   inherit pname version;
 
-  src = fetchFromGitHub {
-    owner = "meganz";
-    repo = "MEGAcmd";
-    rev = "${version}_Linux";
-    hash = "sha256-UlSqwM8GQKeG8/K0t5DbM034NQOeBg+ujNi/MMsVCuM=";
-    fetchSubmodules = true;
-  };
+  src = fetchFromGitHub (
+    srcOptions
+    // {
+      owner = "meganz";
+      repo = "MEGAcmd";
+      fetchSubmodules = true;
+    }
+  );
 
   enableParallelBuilding = true;
   nativeBuildInputs = [
@@ -43,25 +55,26 @@ stdenv.mkDerivation {
     pkg-config
   ];
 
-  buildInputs = [
-    c-ares
-    cryptopp
-    curl
-    ffmpeg
-    icu
-    gcc-unwrapped
-    libmediainfo
-    libraw
-    libsodium
-    libuv
-    libzen
-    pcre-cpp
-    readline
-    sqlite
-  ] ++ lib.optionals withFreeImage [ freeimage ];
+  buildInputs =
+    lib.optionals stdenv.isLinux [ gcc-unwrapped ] # fix: ld: cannot find lib64/libstdc++fs.a
+    ++ [
+      c-ares
+      cryptopp
+      curl
+      ffmpeg
+      icu
+      libmediainfo
+      libraw
+      libsodium
+      libuv
+      libzen
+      pcre-cpp
+      readline
+      sqlite
+    ]
+    ++ lib.optionals withFreeImage [ freeimage ];
 
   configureFlags = [
-    "--disable-curl-checks"
     "--disable-examples"
     "--with-cares"
     "--with-cryptopp"
@@ -77,8 +90,14 @@ stdenv.mkDerivation {
     "--with-termcap"
   ] ++ (if withFreeImage then [ "--with-freeimage" ] else [ "--without-freeimage" ]);
 
+  # On darwin, some macros defined in AssertMacros.h (from apple-sdk) are conflicting.
+  postConfigure = ''
+    echo '#define __ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES 0' >> sdk/include/mega/config.h
+  '';
+
   patches = [
     ./fix-ffmpeg.patch # https://github.com/meganz/sdk/issues/2635#issuecomment-1495405085
+    ./fix-darwin.patch # fix: libtool tag not found; MacFileSystemAccess not declared; server cannot init
   ];
 
   meta = {
@@ -88,10 +107,7 @@ stdenv.mkDerivation {
       bsd2
       gpl3Only
     ];
-    platforms = [
-      "i686-linux"
-      "x86_64-linux"
-    ];
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
     maintainers = with lib.maintainers; [
       lunik1
       ulysseszhan
