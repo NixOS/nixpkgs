1d48b03ec67943361362c541bb501ba792c69f85
Gary Guo <gary@garyguo.net>
2024-12-06 15:59:15 +0000

    zulip: expand platforms to all Linux
    (cherry picked from commit 29d7565e1bf51275ec74db901f24008cf56c03ea)

--- pkgs/by-name/zu/zulip/package.nix
+++ pkgs/by-name/zu/zulip/package.nix
@@ -74,7 +74,7 @@ buildNpmPackage rec {
     homepage = "https://zulip.com";
     license = licenses.asl20;
     maintainers = with maintainers; [ andersk ];
-    platforms = [ "x86_64-linux" ];
+    platforms = lib.platforms.linux;
     mainProgram = "zulip";
   };
 }
