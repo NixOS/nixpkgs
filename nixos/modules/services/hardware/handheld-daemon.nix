9d330719900d0e88261b4a93829088d5c003d5f5
Toast <39011842+toast003@users.noreply.github.com>
2024-12-07 14:47:49 +0100

    nixos/handheld-daemon: add adjustor options

--- nixos/modules/services/hardware/handheld-daemon.nix
+++ nixos/modules/services/hardware/handheld-daemon.nix
@@ -16,6 +16,11 @@ in
       package = mkPackageOption pkgs "handheld-daemon-ui" { };
     };
 
+    adjustor = {
+      enable = mkEnableOption "Handheld Daemon TDP control plugin";
+      package = mkPackageOption pkgs "adjustor" { };
+    };
+
     user = mkOption {
       type = types.str;
       description = ''
@@ -24,13 +29,22 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
+  config = let
+    hhdPackage = cfg.package.override {
+      withAdjustor = cfg.adjustor.enable;
+      adjustor = cfg.adjustor.package;
+    };
+  in mkIf cfg.enable {
     services.handheld-daemon.ui.enable = mkDefault true;
     environment.systemPackages = [
-      cfg.package
+      hhdPackage
     ] ++ lib.optional cfg.ui.enable cfg.ui.package;
-    services.udev.packages = [ cfg.package ];
-    systemd.packages = [ cfg.package ];
+    services.udev.packages = [ hhdPackage ];
+    systemd.packages = [ hhdPackage ];
+
+    boot.extraModulePackages = mkIf cfg.adjustor.enable [
+      config.boot.kernelPackages.acpi_call
+    ];
 
     systemd.services.handheld-daemon = {
       description = "Handheld Daemon";
@@ -45,7 +59,7 @@ in
       ];
 
       serviceConfig = {
-        ExecStart = "${ lib.getExe cfg.package } --user ${ cfg.user }";
+        ExecStart = "${ lib.getExe hhdPackage } --user ${ cfg.user }";
         Nice = "-12";
         Restart = "on-failure";
         RestartSec = "10";
