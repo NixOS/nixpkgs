d68fd845547cd8822d112f046cd129dae1fdd692
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-07 21:06:01 +0000

    ansible: 2.17.6 -> 2.18.1

--- pkgs/development/python-modules/ansible/core.nix
+++ pkgs/development/python-modules/ansible/core.nix
@@ -31,13 +31,13 @@
 
 buildPythonPackage rec {
   pname = "ansible-core";
-  version = "2.17.6";
+  version = "2.18.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "ansible_core";
     inherit version;
-    hash = "sha256-PlOXC3zr/irbObcRweL4u/y+2sgo2lHcA1ehkHBjjpU=";
+    hash = "sha256-FMrB+Su9rogcsGFu3esXkl6MtQfkhgh5deckUz2d508=";
   };
 
   # ansible_connection is already wrapped, so don't pass it through
