ad8830af00b07558136c963d9f15e159ca891299
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-07 19:25:51 +0000

    go-ios: 1.0.155 -> 1.0.162

--- pkgs/by-name/go/go-ios/package.nix
+++ pkgs/by-name/go/go-ios/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "go-ios";
-  version = "1.0.155";
+  version = "1.0.162";
 
   src = fetchFromGitHub {
     owner = "danielpaulus";
     repo = "go-ios";
     rev = "v${version}";
-    sha256 = "sha256-cIB03BSdhOgXaw/Pjvh40S02egE7P53gf0TmwhqbbZY=";
+    sha256 = "sha256-lquOUs6oI3xMYPI1mNNU9xEsCRvQpbNElr6LoK3+aV0=";
   };
 
   proxyVendor = true;
