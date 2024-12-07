b6788a461272886f9dda218ab1b5b2006607309a
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-12-07 22:41:17 +0530

    datadog-integrations-core: moderinize derivation

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13544,7 +13544,6 @@ with pkgs;
   };
   datadog-process-agent = callPackage ../tools/networking/dd-agent/datadog-process-agent.nix { };
   datadog-integrations-core = extras: callPackage ../tools/networking/dd-agent/integrations-core.nix {
-    python = python3;
     extraIntegrations = extras;
   };
 
