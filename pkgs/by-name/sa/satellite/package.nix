6ad564a324d33163e41fcc8d5aeff42b9c14b260
aleksana <me@aleksana.moe>
2024-12-07 21:49:15 +0800

    satellite: refactor

--- pkgs/by-name/sa/satellite/package.nix
+++ pkgs/by-name/sa/satellite/package.nix
@@ -1,11 +1,12 @@
-{ lib
-, python3
-, fetchFromGitea
-, gobject-introspection
-, gtk3
-, libhandy
-, modemmanager
-, wrapGAppsHook3
+{
+  lib,
+  python3,
+  fetchFromGitea,
+  gobject-introspection,
+  gtk3,
+  libhandy,
+  modemmanager,
+  wrapGAppsHook3,
 }:
 
 python3.pkgs.buildPythonApplication rec {
@@ -15,18 +16,19 @@ python3.pkgs.buildPythonApplication rec {
   pyproject = true;
 
   src = fetchFromGitea {
-    domain ="codeberg.org";
+    domain = "codeberg.org";
     owner = "tpikonen";
     repo = "satellite";
-    rev = version;
+    tag = version;
     hash = "sha256-61HCk0W07w0LybSVB4APvQX4PMSsqH9mKGzc+Rmno90=";
   };
 
   nativeBuildInputs = [
     gobject-introspection
-    python3.pkgs.setuptools
     wrapGAppsHook3
   ];
+
+  build-system = with python3.pkgs; [ setuptools ];
 
   buildInputs = [
     gtk3
@@ -34,7 +36,7 @@ python3.pkgs.buildPythonApplication rec {
     modemmanager
   ];
 
-  propagatedBuildInputs = with python3.pkgs; [
+  dependencies = with python3.pkgs; [
     gpxpy
     pygobject3
     pynmea2
@@ -42,16 +44,16 @@ python3.pkgs.buildPythonApplication rec {
 
   strictDeps = true;
 
-  meta = with lib; {
+  meta = {
     description = "Program for showing navigation satellite data";
     longDescription = ''
       Satellite is an adaptive GTK3 / libhandy application which displays global navigation satellite system (GNSS: GPS et al.) data obtained from ModemManager or gnss-share.
       It can also save your position to a GPX-file.
     '';
     homepage = "https://codeberg.org/tpikonen/satellite";
-    license = licenses.gpl3Only;
+    license = lib.licenses.gpl3Only;
     mainProgram = "satellite";
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ Luflosi ];
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ Luflosi ];
   };
 }
