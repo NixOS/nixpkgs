e5949f158f0c5abddd6f3a240a226d80f90b9cba
Sergei Trofimovich <slyich@gmail.com>
2024-12-07 06:17:53 +0000

    graphviz: 12.2.0 -> 12.2.1
    Changes: https://gitlab.com/graphviz/graphviz/-/blob/12.2.1/CHANGELOG.md?ref_type=tags

--- pkgs/tools/graphics/graphviz/default.nix
+++ pkgs/tools/graphics/graphviz/default.nix
@@ -32,13 +32,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "graphviz";
-  version = "12.2.0";
+  version = "12.2.1";
 
   src = fetchFromGitLab {
     owner = "graphviz";
     repo = "graphviz";
     rev = version;
-    hash = "sha256-BSqCI9nIDjymPbCPWGFdWmqfyjuTkIsL1r0qv+Qjy10=";
+    hash = "sha256-Uxqg/7+LpSGX4lGH12uRBxukVw0IswFPfpb2EkLsaiI=";
   };
 
   nativeBuildInputs = [
