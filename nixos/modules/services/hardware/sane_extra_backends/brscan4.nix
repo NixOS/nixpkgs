6b63c25d855f919f42921850a7351d124dcad072
Felix Buehler <account@buehler.rocks>
2024-12-08 13:18:26 +0100

    nixos/hardware.sane.brscan4: remove `with lib;`

--- nixos/modules/services/hardware/sane_extra_backends/brscan4.nix
+++ nixos/modules/services/hardware/sane_extra_backends/brscan4.nix
@@ -1,11 +1,8 @@
 { config, lib, pkgs, ... }:
-
-with lib;
-
 let
   cfg = config.hardware.sane.brscan4;
 
-  netDeviceList = attrValues cfg.netDevices;
+  netDeviceList = lib.attrValues cfg.netDevices;
 
   etcFiles = pkgs.callPackage ./brscan4_etc_files.nix { netDevices = netDeviceList; };
 
@@ -13,8 +10,8 @@ let
 
     options = {
 
-      name = mkOption {
-        type = types.str;
+      name = lib.mkOption {
+        type = lib.types.str;
         description = ''
           The friendly name you give to the network device. If undefined,
           the name of attribute will be used.
@@ -23,8 +20,8 @@ let
         example = "office1";
       };
 
-      model = mkOption {
-        type = types.str;
+      model = lib.mkOption {
+        type = lib.types.str;
         description = ''
           The model of the network device.
         '';
@@ -32,8 +29,8 @@ let
         example = "MFC-7860DW";
       };
 
-      ip = mkOption {
-        type = with types; nullOr str;
+      ip = lib.mkOption {
+        type = with lib.types; nullOr str;
         default = null;
         description = ''
           The ip address of the device. If undefined, you will have to
@@ -43,8 +40,8 @@ let
         example = "192.168.1.2";
       };
 
-      nodename = mkOption {
-        type = with types; nullOr str;
+      nodename = lib.mkOption {
+        type = with lib.types; nullOr str;
         default = null;
         description = ''
           The node name of the device. If undefined, you will have to
@@ -58,7 +55,7 @@ let
 
 
     config =
-      { name = mkDefault name;
+      { name = lib.mkDefault name;
       };
   };
 
@@ -68,20 +65,20 @@ in
   options = {
 
     hardware.sane.brscan4.enable =
-      mkEnableOption "Brother's brscan4 scan backend" // {
+      lib.mkEnableOption "Brother's brscan4 scan backend" // {
       description = ''
         When enabled, will automatically register the "brscan4" sane
         backend and bring configuration files to their expected location.
       '';
     };
 
-    hardware.sane.brscan4.netDevices = mkOption {
+    hardware.sane.brscan4.netDevices = lib.mkOption {
       default = {};
       example =
         { office1 = { model = "MFC-7860DW"; ip = "192.168.1.2"; };
           office2 = { model = "MFC-7860DW"; nodename = "BRW0080927AFBCE"; };
         };
-      type = with types; attrsOf (submodule netDeviceOpts);
+      type = with lib.types; attrsOf (submodule netDeviceOpts);
       description = ''
         The list of network devices that will be registered against the brscan4
         sane backend.
@@ -89,7 +86,7 @@ in
     };
   };
 
-  config = mkIf (config.hardware.sane.enable && cfg.enable) {
+  config = lib.mkIf (config.hardware.sane.enable && cfg.enable) {
 
     hardware.sane.extraBackends = [
       pkgs.brscan4
@@ -99,7 +96,7 @@ in
       { source = "${etcFiles}/etc/opt/brother/scanner/brscan4"; };
 
     assertions = [
-      { assertion = all (x: !(null != x.ip && null != x.nodename)) netDeviceList;
+      { assertion = lib.all (x: !(null != x.ip && null != x.nodename)) netDeviceList;
         message = ''
           When describing a network device as part of the attribute list
           `hardware.sane.brscan4.netDevices`, only one of its `ip` or `nodename`
