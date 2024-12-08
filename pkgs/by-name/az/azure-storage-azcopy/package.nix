8847327afe3ccb2c3a6e3669b4f6c22d028c4058
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-08 04:34:59 +0000

    azure-storage-azcopy: 10.27.0 -> 10.27.1

--- pkgs/by-name/az/azure-storage-azcopy/package.nix
+++ pkgs/by-name/az/azure-storage-azcopy/package.nix
@@ -5,18 +5,18 @@
 
 buildGoModule rec {
   pname = "azure-storage-azcopy";
-  version = "10.27.0";
+  version = "10.27.1";
 
   src = fetchFromGitHub {
     owner = "Azure";
     repo = "azure-storage-azcopy";
     rev = "refs/tags/v${version}";
-    hash = "sha256-TF0vChuM3OF/YbCsP8Vg4x609Q1QgqwBNmKUdWCHHUc=";
+    hash = "sha256-h/EiJZMX2vHn/t1FPM1ZkXf9I8LYiqK1GqkSDAlXBYY=";
   };
 
   subPackages = [ "." ];
 
-  vendorHash = "sha256-dYIZb8sSh1Y8yllWOSsWEpiaIwcwZL2wCet3Terl0Ro=";
+  vendorHash = "sha256-7ZUQMJPYE62M47VqEwahVePC85x3TyOmADOVczUrj7w=";
 
   doCheck = false;
 
