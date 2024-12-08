a7b26f9199ca36f729f4348e9e1a7a99e48bd6bb
Felix Buehler <account@buehler.rocks>
2024-12-08 13:18:25 +0100

    nixos/services.zeitgeist: remove `with lib;`

--- nixos/modules/services/desktops/zeitgeist.nix
+++ nixos/modules/services/desktops/zeitgeist.nix
@@ -1,9 +1,5 @@
 # Zeitgeist
-
 { config, lib, pkgs, ... }:
-
-with lib;
-
 {
 
   meta = with lib; {
@@ -14,13 +10,13 @@ with lib;
 
   options = {
     services.zeitgeist = {
-      enable = mkEnableOption "zeitgeist, a service which logs the users' activities and events";
+      enable = lib.mkEnableOption "zeitgeist, a service which logs the users' activities and events";
     };
   };
 
   ###### implementation
 
-  config = mkIf config.services.zeitgeist.enable {
+  config = lib.mkIf config.services.zeitgeist.enable {
 
     environment.systemPackages = [ pkgs.zeitgeist ];
 
