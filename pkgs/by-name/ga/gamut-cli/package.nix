b5e1e3b6ecc2065c805fd9c6a42783b69437a5b9
nikolaizombie1 <fabio.matos999@gmail.com>
2024-12-08 12:13:07 -0500

    gamut-cli: init at v0.9.0

--- pkgs/by-name/ga/gamut-cli/package.nix
+++ pkgs/by-name/ga/gamut-cli/package.nix
@@ -3,30 +3,30 @@
   fetchFromGitHub,
   lib,
 }:
-  buildGoModule rec {
-    pname = "gamut-cli";
-    version = "0.9.0";
+buildGoModule rec {
+  pname = "gamut-cli";
+  version = "0.9.0";
 
-    src = fetchFromGitHub {
-      owner = "nikolaizombie1";
-      repo = "gamut-cli";
-      rev = "v${version}";
-      hash = "sha256-ayBpx9GRE0pkVl8Zo9clsEoW+3Gwd7ynl5Ov1XO8q1I=";
-    };
+  src = fetchFromGitHub {
+    owner = "nikolaizombie1";
+    repo = "gamut-cli";
+    rev = "v${version}";
+    hash = "sha256-ayBpx9GRE0pkVl8Zo9clsEoW+3Gwd7ynl5Ov1XO8q1I=";
+  };
 
-    vendorHash = "sha256-2KtQpzgz6xTupbejkm95OVxnamZmBwanPmzUkjzkB+o=";
+  vendorHash = "sha256-2KtQpzgz6xTupbejkm95OVxnamZmBwanPmzUkjzkB+o=";
 
-    meta = with lib; {
-      description = "A command line interface for the gamut library made by muesli.";
-      homepage = "https://github.com/nikolaizombie1/gamut-cli/tree/main";
-      license = lib.licenses.gpl3;
-      maintainers = [
-        {
-          name = "Fabio J. Matos Nieves";
-          email = "fabio.matos999@gmail.com";
-          githubId = "70602908";
-          github = "nikolaizombie1";
-        }
-      ];
-    };
-  }
+  meta = with lib; {
+    description = "A command line interface for the gamut library made by muesli.";
+    homepage = "https://github.com/nikolaizombie1/gamut-cli/tree/main";
+    license = lib.licenses.gpl3;
+    maintainers = [
+      {
+        name = "Fabio J. Matos Nieves";
+        email = "fabio.matos999@gmail.com";
+        githubId = "70602908";
+        github = "nikolaizombie1";
+      }
+    ];
+  };
+}
