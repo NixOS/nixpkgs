3e973ae2ebc7bc05e23ba36e7324b5fe2c70abbd
Robert Schütz <mail@dotlambda.de>
2024-12-08 15:59:10 -0800

    python312Packages.safety: 3.2.9 -> 3.2.11
    Diff: https://github.com/pyupio/safety/compare/refs/tags/3.2.9...3.2.11
    
    Changelog: https://github.com/pyupio/safety/blob/3.2.11/CHANGELOG.md

--- pkgs/development/python-modules/safety/default.nix
+++ pkgs/development/python-modules/safety/default.nix
@@ -26,7 +26,7 @@
 
 buildPythonPackage rec {
   pname = "safety";
-  version = "3.2.9";
+  version = "3.2.11";
 
   disabled = pythonOlder "3.7";
 
@@ -36,7 +36,7 @@ buildPythonPackage rec {
     owner = "pyupio";
     repo = "safety";
     rev = "refs/tags/${version}";
-    hash = "sha256-etA/S/i87w4ihsqQo5JJjt6hWC7Jt9/q8vhqyo+DTek=";
+    hash = "sha256-ekJ0o+KWS8Fih1M/scboGEjETX0/2Uh6h/zne4h+3wI=";
   };
 
   postPatch = ''
@@ -54,7 +54,6 @@ buildPythonPackage rec {
   build-system = [ setuptools ];
 
   pythonRelaxDeps = [
-    "dparse"
     "filelock"
   ];
 
