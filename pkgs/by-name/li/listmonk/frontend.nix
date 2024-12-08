bd391ad92ae59cae3e8278090c1775e9f46222c7
aucub <78630225+aucub@users.noreply.github.com>
2024-12-09 05:21:36 +0800

    listmonk: 3.0.0 -> 4.1.0

--- pkgs/by-name/li/listmonk/frontend.nix
+++ pkgs/by-name/li/listmonk/frontend.nix
@@ -1,22 +1,22 @@
-{ stdenv
-, fetchYarnDeps
-, yarnConfigHook
-, yarnBuildHook
-, nodejs
-, meta
-, version
-, src
+{
+  stdenv,
+  yarnConfigHook,
+  yarnBuildHook,
+  fetchYarnDeps,
+  nodejs,
+  version,
+  src,
+  meta,
 }:
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "listmonk-frontend";
-  inherit version;
+  inherit version meta;
 
   src = "${src}/frontend";
 
   offlineCache = fetchYarnDeps {
     yarnLock = "${src}/frontend/yarn.lock";
-    hash = "sha256-TdrglyRtb2Q8SFtoiCoDj/zBV2+7DwzIm/Fzlt0ZvSo=";
+    hash = "sha256-2STFJtlneyR6QBsy/RVIINV/0NMggnfZwyz1pki8iPk=";
   };
 
   nativeBuildInputs = [
@@ -26,9 +26,10 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   installPhase = ''
-    mkdir $out
-    cp -R dist/* $out
-  '';
+    runHook preInstall
 
-  inherit meta;
+    cp -r dist $out
+
+    runHook postInstall
+  '';
 })
