5db7ab0fc5995c27caef25b8c86cfd1abae956dd
Maximilian Bosch <maximilian@mbosch.me>
2024-12-08 13:04:14 +0100

    grafana: 10.4.13 -> 10.4.14
    ChangeLog: https://github.com/grafana/grafana/releases/tag/v10.4.14

--- pkgs/servers/monitoring/grafana/default.nix
+++ pkgs/servers/monitoring/grafana/default.nix
@@ -28,7 +28,7 @@ let
 in
 buildGoModule rec {
   pname = "grafana";
-  version = "10.4.13";
+  version = "10.4.14";
 
   subPackages = [ "pkg/cmd/grafana" "pkg/cmd/grafana-server" "pkg/cmd/grafana-cli" ];
 
@@ -36,7 +36,7 @@ buildGoModule rec {
     owner = "grafana";
     repo = "grafana";
     rev = "v${version}";
-    hash = "sha256-nJpZSW5eHleBZjPoPScMmXl9BAqUnEGrKUu/UJzHEfI=";
+    hash = "sha256-JbrJEJU/i7NvwcGR1smPueqGbYVxVmHp2n5putnoRyo=";
   };
 
   # borrowed from: https://github.com/NixOS/nixpkgs/blob/d70d9425f49f9aba3c49e2c389fe6d42bac8c5b0/pkgs/development/tools/analysis/snyk/default.nix#L20-L22
