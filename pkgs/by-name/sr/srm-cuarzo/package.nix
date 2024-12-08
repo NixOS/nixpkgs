a67dc5b123f66fa2c6038d81285d34154a7c9714
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-08 03:27:40 +0000

    srm-cuarzo: 0.8.0-1 -> 0.10.0-1

--- pkgs/by-name/sr/srm-cuarzo/package.nix
+++ pkgs/by-name/sr/srm-cuarzo/package.nix
@@ -14,9 +14,9 @@
 }:
 stdenv.mkDerivation (self: {
   pname = "srm-cuarzo";
-  version = "0.8.0-1";
+  version = "0.10.0-1";
   rev = "v${self.version}";
-  hash = "sha256-QQYn7TR2+6nZHOohjqEg6uMt4OBvfYD4+bFiDIaC7SU=";
+  hash = "sha256-qkJVVoGU07wwcPsrIJi1gkl0uJaZM42kAX8PFQ68HDM=";
 
   src = fetchFromGitHub {
     inherit (self) rev hash;
