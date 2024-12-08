342bc6bd0c01c773e79fc74a3b715617f2af4fae
Sefa Eyeoglu <contact@scrumplex.net>
2024-12-08 02:42:13 +0100

    vokoscreen-ng: 4.2.0 -> 4.3.0 (#360742)
    * vokoscreen-ng: 4.2.0 -> 4.3.0
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    
    * vokoscreen-ng: improve installPhase
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    
    * vokoscreen-ng: move to by-name
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    
    * vokoscreen-ng: nixfmt
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    
    ---------
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/by-name/vo/vokoscreen-ng/package.nix
+++ pkgs/by-name/vo/vokoscreen-ng/package.nix
@@ -1,34 +1,35 @@
-{ fetchFromGitHub
-, gst_all_1
-, gst-plugins-bad
-, gst-plugins-base
-, gst-plugins-good
-, gst-plugins-ugly
-, gstreamer
-, lib
-, libX11
-, pipewire
-, pkg-config
-, pulseaudio
-, qt6
-, stdenv
-, wayland
+{
+  fetchFromGitHub,
+  gst_all_1,
+  lib,
+  libX11,
+  pipewire,
+  pkg-config,
+  pulseaudio,
+  qt6,
+  stdenv,
+  wayland,
 }:
 
 stdenv.mkDerivation rec {
   pname = "vokoscreen-ng";
-  version = "4.2.0";
+  version = "4.3.0";
 
   src = fetchFromGitHub {
     owner = "vkohaupt";
     repo = "vokoscreenNG";
-    rev = version;
-    hash = "sha256-PLgKOdSx0Kdobex5KaeCxWcindHEN9p4+xaVN/gr7Pk=";
+    rev = "refs/tags/${version}";
+    hash = "sha256-efgvq/jl/ecjtINy5BdqtYRp2gxEvOsMzQVyCZ3ig+Q=";
   };
 
   qmakeFlags = [ "src/vokoscreenNG.pro" ];
 
-  nativeBuildInputs = [ qt6.qttools pkg-config qt6.qmake qt6.wrapQtAppsHook ];
+  nativeBuildInputs = [
+    qt6.qttools
+    pkg-config
+    qt6.qmake
+    qt6.wrapQtAppsHook
+  ];
   buildInputs = [
     gst_all_1.gstreamer
     libX11
@@ -43,29 +44,32 @@ stdenv.mkDerivation rec {
     gst_all_1.gst-plugins-ugly
   ];
 
-  postPatch = ''
-    substituteInPlace src/vokoscreenNG.pro \
-      --replace lrelease-qt5 lrelease
-  '';
-
+  # TODO: translations don't get built by the qmake project
   preBuild = ''
     lrelease src/language/*.ts
   '';
 
-  postInstall = ''
-    mkdir -p $out/bin $out/share/applications $out/share/icons
-    cp ./vokoscreenNG $out/bin/
-    cp ./src/applications/vokoscreenNG.desktop $out/share/applications/
-    cp ./src/applications/vokoscreenNG.png $out/share/icons/
+  # upstream doesn't provide an install target
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 -t $out/bin vokoscreenNG
+    install -Dm644 -t $out/share/applications src/applications/vokoscreenNG.desktop
+    install -Dm644 -t $out/share/icons src/applications/vokoscreenNG.png
+
     qtWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
-    wrapQtApp $out/bin/vokoscreenNG
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "User friendly Open Source screencaster for Linux and Windows";
     license = licenses.gpl2Plus;
     homepage = "https://github.com/vkohaupt/vokoscreenNG";
-    maintainers = with maintainers; [ shamilton dietmarw ];
+    maintainers = with maintainers; [
+      shamilton
+      dietmarw
+    ];
     platforms = platforms.linux;
     mainProgram = "vokoscreenNG";
   };
