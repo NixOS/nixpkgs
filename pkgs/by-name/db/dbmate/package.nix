5c635fc4ff772b49c67c85c04e04bcac17072da9
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-08 18:37:32 +0000

    dbmate: 2.22.0 -> 2.23.0

--- pkgs/by-name/db/dbmate/package.nix
+++ pkgs/by-name/db/dbmate/package.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "dbmate";
-  version = "2.22.0";
+  version = "2.23.0";
 
   src = fetchFromGitHub {
     owner = "amacneil";
     repo = "dbmate";
     rev = "refs/tags/v${version}";
-    hash = "sha256-stemIBwUYW1TsDUBrgQLq858kPBiaHaXaRIr65lnWQo=";
+    hash = "sha256-xmQS0eBOgll8+AU/kQriClqtwrfIz606/o7jEJlQLV8=";
   };
 
   vendorHash = "sha256-xJIY0vaN7gw/EhqeepKQPhaKISXNNPnaAMbowmHSUz4=";
