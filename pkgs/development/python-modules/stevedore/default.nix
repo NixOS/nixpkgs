6cb776b31c7b46e23eefc34e3f575b55e4836f57
Fabian Affolter <mail@fabian-affolter.ch>
2024-12-08 10:08:46 +0100

    python312Packages.stevedore: update disabled

--- pkgs/development/python-modules/stevedore/default.nix
+++ pkgs/development/python-modules/stevedore/default.nix
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   version = "5.4.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
