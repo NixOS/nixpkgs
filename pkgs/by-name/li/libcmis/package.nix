6bc84bc69ac6d2a15820332decffdcd83ba7260f
Yureka <yureka@cyberchaos.dev>
2024-12-09 11:51:15 +0100

    libcmis: apply patch for boost 1.86
    Change-Id: Iea6b3755a948a174fe061eb6b511106c71bb26b2

--- pkgs/by-name/li/libcmis/package.nix
+++ pkgs/by-name/li/libcmis/package.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, boost, libxml2, pkg-config, docbook2x, curl, autoreconfHook, cppunit }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch, boost, libxml2, pkg-config, docbook2x, curl, autoreconfHook, cppunit }:
 
 stdenv.mkDerivation rec {
   pname = "libcmis";
@@ -10,6 +10,14 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "sha256-HXiyQKjOlQXWABY10XrOiYxPqfpmUJC3a6xD98LIHDw=";
   };
+
+  patches = [
+    # Backport to fix build with boost 1.86
+    (fetchpatch {
+      url = "https://github.com/tdf/libcmis/commit/3659d32999ff7593662dcf5136bcb7ac15c13f61.patch";
+      hash = "sha256-EXmQcXCHaVnF/dwU3Z4WLtaiHjYHeeonlKdyK27UkiY=";
+    })
+  ];
 
   nativeBuildInputs = [ autoreconfHook pkg-config docbook2x ];
   buildInputs = [ boost libxml2 curl cppunit ];
