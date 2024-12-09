3993addae195a936379c21a3fdf60991f849a7c7
PopeRigby <poperigby@mailbox.org>
2024-12-09 14:35:24 -0800

    Update pkgs/by-name/ta/tacent/package.nix
    Co-authored-by: Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>

--- pkgs/by-name/ta/tacent/package.nix
+++ pkgs/by-name/ta/tacent/package.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "bluescan";
     repo = "tacent";
-    rev = "refs/tags/${version}";
+    tag  = version;
     hash = "sha256-z8VuJS8OaVw5CeO/udvBEmcURKIy1oWVYUv6Ai8lTI8=";
   };
 
