bf2897cca1cab574fc705d2d90299beb212d7067
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-12-09 19:21:47 +0200

    bartender: add `passthru.updateScript`

--- pkgs/by-name/ba/bartender/package.nix
+++ pkgs/by-name/ba/bartender/package.nix
@@ -2,6 +2,11 @@
 , stdenvNoCC
 , fetchurl
 , _7zz
+, curl
+, cacert
+, xmlstarlet
+, writeShellApplication
+, common-updater-scripts
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
@@ -31,6 +36,22 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
     runHook postInstall
   '';
+
+  passthru.updateScript = lib.getExe (writeShellApplication {
+    name = "bartender-update-script";
+    runtimeInputs = [
+      curl
+      cacert
+      xmlstarlet
+      common-updater-scripts
+    ];
+    text = ''
+      version_major="${lib.versions.major finalAttrs.version}"
+      url="https://www.macbartender.com/B2/updates/AppcastB$version_major.xml"
+      version=$(curl -s "$url" | xmlstarlet sel -t -v '(//item)[last()]/sparkle:shortVersionString' -n)
+      update-source-version bartender "$version"
+    '';
+  });
 
   meta = {
     description = "Take control of your menu bar";
