ed99939b5a71b11b8eb486366290b830b12cff9d
Fernando Rodrigues <alpha@sigmasquadron.net>
2024-12-09 12:36:45 -0300

    xen: 4.19.0-unstable-2024-11-12 -> 4.19.1
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/by-name/xe/xen/package.nix
+++ pkgs/by-name/xe/xen/package.nix
@@ -1,12 +1,18 @@
 {
   buildXenPackage,
   python3Packages,
+  fetchpatch,
 }:
 
 buildXenPackage.override { inherit python3Packages; } {
   pname = "xen";
-  version = "4.19.0-unstable-2024-11-12";
-  upstreamVersion = "4.19.1-pre"; # We track the stable branches. Despite the version number, this is actually 4.19.0, not 4.19.1.
-  rev = "251a9496485a86f302980a3f8d3c656831b5a62f";
-  hash = "sha256-kHuB6kagH3AU+Wsx4oD7HnNsZpxCu7x3v/m4/1xi6lY=";
+  version = "4.19.1";
+  patches = [
+    (fetchpatch {
+      url = "https://lore.kernel.org/xen-devel/e2caa6648a0b6c429349a9826d8fbc4338222482.1733766758.git.andrii.sultanov@cloud.com/raw";
+      hash = "sha256-JC1ueXuC1Jdi2gtUsjOHmTeEx56zjotMMLde5vBonxc=";
+    })
+  ];
+  rev = "ccf400846780289ae779c62ef0c94757ff43bb60";
+  hash = "sha256-s0eCBCd6ybl+kLtXCC6E1sk++w7txXn/B/Cg5acQFfY=";
 }
