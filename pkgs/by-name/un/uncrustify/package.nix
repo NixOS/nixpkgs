b4c1690beedbb2530e3b5ca50b7919e8515d3d0b
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-09 07:01:11 +0000

    uncrustify: 0.80.0 -> 0.80.1

--- pkgs/by-name/un/uncrustify/package.nix
+++ pkgs/by-name/un/uncrustify/package.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "uncrustify";
-  version = "0.80.0";
+  version = "0.80.1";
 
   src = fetchFromGitHub {
     owner = "uncrustify";
     repo = "uncrustify";
     rev = "uncrustify-${version}";
-    sha256 = "sha256-1W4g9ISMVwmc0efa6loFtyqsHNm0kwR+NVKE8eB/jEA=";
+    sha256 = "sha256-SsPuqPyv3IFjYtAqANjzK8cdTGsyGBXVLzGWEUl28dg=";
   };
 
   nativeBuildInputs = [ cmake python3 ];
