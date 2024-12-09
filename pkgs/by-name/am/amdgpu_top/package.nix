21fab6b5d6f92613fcbf9e8e27fd6851dac2bc3b
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-09 13:05:53 +0000

    amdgpu_top: 0.9.2 -> 0.10.0

--- pkgs/by-name/am/amdgpu_top/package.nix
+++ pkgs/by-name/am/amdgpu_top/package.nix
@@ -16,19 +16,19 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "amdgpu_top";
-  version = "0.9.2";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "Umio-Yasuno";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-eXUeXBFW2ejkOeMUujMwY0gidzdaX9yWWOfx1ZwA3lk=";
+    hash = "sha256-lX4NC69UhrrHcGoVUJ2dknK6HUwwKm934js/vCnTO2E=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "libdrm_amdgpu_sys-0.7.5" = "sha256-IkA1kJggkaSWzdDBHpNjU8WZr6wpSQWohERiHWQS2UY=";
+      "libdrm_amdgpu_sys-0.7.6" = "sha256-xf1tQYct5SlRAavW9dHkcCQa4zBehD2XJ4jUE7lblPc=";
     };
   };
 
