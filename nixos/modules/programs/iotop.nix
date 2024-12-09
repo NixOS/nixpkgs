7106ab2af0ea4b402baa27e07bad21dcd75e5a53
Mitchell Pleune <mitchpleune@gmail.com>
2024-12-08 22:56:33 -0500

    Add option to enable iotop with delayacct tracking in kernel
    Since Linux commit e4042ad492357fa995921376462b04a025dd53b6 (May 2021)
    this is disabled by default as it is "not used much."

--- nixos/modules/programs/iotop.nix
+++ nixos/modules/programs/iotop.nix
@@ -3,8 +3,9 @@
 let
   cfg = config.programs.iotop;
 in {
-  options = {
-    programs.iotop.enable = lib.mkEnableOption "iotop + setcap wrapper";
+  options.programs.iotop = {
+    enable = lib.mkEnableOption "iotop + setcap wrapper";
+    useDelayacct = lib.mkEnableOption "Enable the task_delayacct kernel task delay accounting in order to show all statistics.";
   };
   config = lib.mkIf cfg.enable {
     security.wrappers.iotop = {
@@ -13,5 +14,6 @@ in {
       capabilities = "cap_net_admin+p";
       source = "${pkgs.iotop}/bin/iotop";
     };
+    boot.kernel.sysctl = lib.mkIf cfg.useDelayacct { "kernel.task_delayacct" = 1; };
   };
 }
