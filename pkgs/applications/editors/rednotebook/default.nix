ee4b6a62d170894eec9c9f9daaae77036e7a101e
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-09 13:24:53 +0000

    rednotebook: 2.35 -> 2.36

--- pkgs/applications/editors/rednotebook/default.nix
+++ pkgs/applications/editors/rednotebook/default.nix
@@ -5,14 +5,14 @@
 
 buildPythonApplication rec {
   pname = "rednotebook";
-  version = "2.35";
+  version = "2.36";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jendrikseipp";
     repo = "rednotebook";
-    rev = "refs/tags/v${version}";
-    sha256 = "sha256-sGwdZZ3YGm3sXJoxnYwj6HQcYMnC1pEzba3N9KLfRHM=";
+    tag = "v${version}";
+    sha256 = "sha256-ISwYQPI7qM0+cEdWnHLdrJA/07BUjDP4gzF/GzqNcsE=";
   };
 
   # We have not packaged tests.
