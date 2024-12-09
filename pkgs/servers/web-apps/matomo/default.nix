99ee13fb9db4d0d15f0be6018738850911a077ad
Niklas Korz <niklas@niklaskorz.de>
2024-12-09 16:58:56 +0100

    matomo_5: 5.1.1 -> 5.1.2
    (cherry picked from commit 00553f86fd2ed1d4d6a22be53f659fc15da06eaa)

--- pkgs/servers/web-apps/matomo/default.nix
+++ pkgs/servers/web-apps/matomo/default.nix
@@ -7,8 +7,8 @@ let
       hash = "sha256-cGnsxfpvt7FyhxFcA2/gWWe7CyanVGZVKtCDES3XLdI=";
     };
     matomo_5 = {
-      version = "5.1.1";
-      hash = "sha256-xi6R9O/pOxBgga6+wwqziwDKK7Q1Ispldvxg+0mpdeQ=";
+      version = "5.1.2";
+      hash = "sha256-6kR6OOyqwQfV+pRqHO+VMLM1eZQb0om65EilAnIlW9U=";
     };
     matomo-beta = {
       version = "5.0.0";
