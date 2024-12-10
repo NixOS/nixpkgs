8eafe1f165e5b6dacdb72e3b38769fc7fb8d140d
Florian <florian.brandes@posteo.de>
2024-12-10 06:41:25 +0100

    perkeep: 0.11 -> 0-unstable-2024-04-23 (#360175)
    * perkeep: format
    
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>
    
    * perkeep: 0.11 -> 0-unstable-2024-04-23
    
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>
    
    * perkeep: add gador as co-maintainer
    
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>
    
    ---------
    
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/by-name/pe/perkeep/package.nix
+++ pkgs/by-name/pe/perkeep/package.nix
@@ -1,60 +1,45 @@
-{ buildGoModule, fetchurl, fetchFromGitHub, lib }:
+{
+  buildGoModule,
+  fetchFromGitHub,
+  lib,
+}:
 
-let
-  gouiJS = fetchurl {
-    url = "https://storage.googleapis.com/perkeep-release/gopherjs/goui.js";
-    hash = "sha256-z8A5vbkAaXCw/pv9t9sFJ2xNbEOjg4vkr/YBkNptc3U=";
-  };
-
-  publisherJS = fetchurl {
-    url = "https://storage.googleapis.com/perkeep-release/gopherjs/publisher.js";
-    hash = "sha256-4iKMkOEKPCo6Xrq0L5IglVZpB9wyLymCgRYz3cE9DSY=";
-  };
-
-  packages = [
-    "perkeep.org/server/perkeepd"
-    "perkeep.org/cmd/pk"
-    "perkeep.org/cmd/pk-get"
-    "perkeep.org/cmd/pk-put"
-    "perkeep.org/cmd/pk-mount"
-  ];
-
-in
-buildGoModule rec {
+buildGoModule {
   pname = "perkeep";
-  version = "0.11";
+  # no release or tag since 2020
+  version = "0-unstable-2024-04-23";
 
   src = fetchFromGitHub {
     owner = "perkeep";
     repo = "perkeep";
-    rev = version;
-    hash = "sha256-lGZb9tH1MrclCRkkmNB85dP/Hl+kkue/WplNMul9RR4=";
+    rev = "bb15e6eb48bc9d614673f3af9432c70a76707c22";
+    hash = "sha256-FUr+OgxYHVUzaahrG/3Adn5KNYHb0S/SKKFddskuvZA=";
   };
 
-  vendorHash = "sha256-y+AYUG15tsj5SppY2bTg/dh3LPpp+14smCo7nLJRyak=";
-  deleteVendor = true; # Vendor is out of sync with go.mod
+  vendorHash = "sha256-+l1QV7/P0sS1S26xdyQygRZQGKqwbLUhgVtm/yHL6Cc=";
 
-  buildPhase = ''
-    cd "$NIX_BUILD_TOP/source"
+  subPackages = [
+    "server/perkeepd"
+    "cmd/pk"
+    "cmd/pk-get"
+    "cmd/pk-put"
+    "cmd/pk-mount"
+  ];
 
-    # Skip network fetches
-    cp ${publisherJS} app/publisher/publisher.js
-    cp ${gouiJS} server/perkeepd/ui/goui.js
-
-    go run make.go -offline=true -targets=${lib.concatStringsSep "," packages}
-  '';
-
-  # genfileembed gets built regardless of -targets, to embed static
+  # genfileembed gets built regardless of subPackages, to embed static
   # content into the Perkeep binaries. Remove it in post-install to
   # avoid polluting paths.
   postInstall = ''
     rm -f $out/bin/genfileembed
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Way of storing, syncing, sharing, modelling and backing up content (née Camlistore)";
     homepage = "https://perkeep.org";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ kalbasit ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
+      kalbasit
+      gador
+    ];
   };
 }
