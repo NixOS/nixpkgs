9b67b79b6e99326052327a38139e4a47764f9612
Stéphan Kochen <git@stephank.nl>
2024-12-10 14:53:02 +0100

    nixos/tests/rustls-libssl: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -909,6 +909,7 @@ in {
   rsyslogd = handleTest ./rsyslogd.nix {};
   rtkit = runTest ./rtkit.nix;
   rtorrent = handleTest ./rtorrent.nix {};
+  rustls-libssl = handleTest ./rustls-libssl.nix {};
   rxe = handleTest ./rxe.nix {};
   sabnzbd = handleTest ./sabnzbd.nix {};
   samba = handleTest ./samba.nix {};
