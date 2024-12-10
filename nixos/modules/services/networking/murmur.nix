d6e6a8b27453fcbaed2a1b8aa54b8052337cc5c4
Felix Singer <felixsinger@posteo.net>
2024-12-10 05:22:11 +0100

    nixos/murmur: Drop CAP_NET_BIND_SERVICE capability
    This capability is only needed for privileged ports (lower than 1024).
    There is not much reason to use these ports, so drop the capability.
    
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- nixos/modules/services/networking/murmur.nix
+++ nixos/modules/services/networking/murmur.nix
@@ -354,8 +354,8 @@ in
         Group = cfg.group;
 
         # service hardening
-        AmbientCapabilities = "CAP_NET_BIND_SERVICE";
-        CapabilityBoundingSet = "CAP_NET_BIND_SERVICE";
+        AmbientCapabilities = "";
+        CapabilityBoundingSet = "";
         LockPersonality = true;
         MemoryDenyWriteExecute = true;
         NoNewPrivileges = true;
