f9593a4851490ff669a41ae6f647c2653a25bb83
thomaslepoix <thomas.lepoix@protonmail.ch>
2024-12-11 03:56:10 +0100

    qucs-s: 24.1.0 -> 24.4.1

--- pkgs/applications/science/electronics/qucs-s/default.nix
+++ pkgs/applications/science/electronics/qucs-s/default.nix
@@ -4,6 +4,7 @@
 , flex
 , bison
 , qtbase
+, qtcharts
 , qttools
 , qtsvg
 , qtwayland
@@ -13,22 +14,27 @@
 , gperf
 , adms
 , ngspice
-, kernels ? [ ngspice ]
+, qucsator-rf
+, kernels ? [ ngspice qucsator-rf ]
 }:
 
 stdenv.mkDerivation rec {
   pname = "qucs-s";
-  version = "24.1.0";
+  version = "24.4.1";
 
   src = fetchFromGitHub {
     owner = "ra3xdh";
     repo = "qucs_s";
     rev = version;
-    sha256 = "sha256-ei9CPlJg+Kfjh7vu5VnT6DNLmmnA8wZ2A1jXnm//Fgo=";
+    hash = "sha256-ll5P8cqJBzoieExElggn5tRbDcmH7L3yvcbtAQ0BBww=";
   };
 
   nativeBuildInputs = [ flex bison wrapQtAppsHook cmake ];
-  buildInputs = [ qtbase qttools qtsvg qtwayland libX11 gperf adms ] ++ kernels;
+  buildInputs = [ qtbase qttools qtcharts qtsvg qtwayland libX11 gperf adms ] ++ kernels;
+
+  cmakeFlags = [
+    "-DWITH_QT6=ON"
+  ];
 
   # Make custom kernels avaible from qucs-s
   qtWrapperArgs = [ "--prefix" "PATH" ":" (lib.makeBinPath kernels) ];
@@ -48,7 +54,8 @@ stdenv.mkDerivation rec {
     '';
     homepage = "https://ra3xdh.github.io/";
     license = licenses.gpl2Plus;
-    maintainers = with maintainers; [ mazurel kashw2 ];
+    mainProgram = "qucs-s";
+    maintainers = with maintainers; [ mazurel kashw2 thomaslepoix ];
     platforms = with platforms; linux;
   };
 }
