210f2e6346d20a6c46690a7c28ae15c09ccbe556
Weijia Wang <9713184+wegank@users.noreply.github.com>
2024-12-11 16:52:51 +0100

    treewide: format new/changed Nix files

--- pkgs/development/libraries/qt-6/modules/qtdeclarative/default.nix
+++ pkgs/development/libraries/qt-6/modules/qtdeclarative/default.nix
@@ -1,17 +1,23 @@
-{ qtModule
-, qtbase
-, qtlanguageserver
-, qtshadertools
-, openssl
-, stdenv
-, lib
-, pkgsBuildBuild
+{
+  qtModule,
+  qtbase,
+  qtlanguageserver,
+  qtshadertools,
+  openssl,
+  stdenv,
+  lib,
+  pkgsBuildBuild,
 }:
 
 qtModule {
   pname = "qtdeclarative";
 
-  propagatedBuildInputs = [ qtbase qtlanguageserver qtshadertools openssl ];
+  propagatedBuildInputs = [
+    qtbase
+    qtlanguageserver
+    qtshadertools
+    openssl
+  ];
   strictDeps = true;
 
   patches = [
@@ -21,13 +27,14 @@ qtModule {
     ./use-versioned-import-path.patch
   ];
 
-  cmakeFlags = [
-    "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderTools"
-    # for some reason doesn't get found automatically on Darwin
-    "-DPython_EXECUTABLE=${lib.getExe pkgsBuildBuild.python3}"
-  ]
-  # Conditional is required to prevent infinite recursion during a cross build
-  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
-    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
-  ];
+  cmakeFlags =
+    [
+      "-DQt6ShaderToolsTools_DIR=${pkgsBuildBuild.qt6.qtshadertools}/lib/cmake/Qt6ShaderTools"
+      # for some reason doesn't get found automatically on Darwin
+      "-DPython_EXECUTABLE=${lib.getExe pkgsBuildBuild.python3}"
+    ]
+    # Conditional is required to prevent infinite recursion during a cross build
+    ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+      "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    ];
 }
