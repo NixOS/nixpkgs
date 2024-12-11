dfa235c99aa7608da507bd2eb621a7d4581e618c
adisbladis <adisbladis@gmail.com>
2024-12-11 00:21:37 +0000

    home-assistant-component-tests: Remove top level attribute
    See https://github.com/NixOS/nixpkgs/pull/155060#issuecomment-2530045955 and onward for motivation.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11608,8 +11608,6 @@ with pkgs;
 
   home-assistant-cli = callPackage ../servers/home-assistant/cli.nix { };
 
-  home-assistant-component-tests = recurseIntoAttrs home-assistant.tests.components;
-
   icingaweb2-ipl = callPackage ../servers/icingaweb2/ipl.nix { };
   icingaweb2-thirdparty = callPackage ../servers/icingaweb2/thirdparty.nix { };
   icingaweb2 = callPackage ../servers/icingaweb2 { };
