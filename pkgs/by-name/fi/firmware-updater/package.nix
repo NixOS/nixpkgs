34302b95f79ab9cb6349b050c4ba1834b8ff5cb8
Maciej Krüger <mkg20001@gmail.com>
2024-12-11 02:22:47 +0100

    firmware-updater: 0-unstable-2024-10-03 -> 0-unstable-2024-20-11

--- pkgs/by-name/fi/firmware-updater/package.nix
+++ pkgs/by-name/fi/firmware-updater/package.nix
@@ -7,11 +7,11 @@
 
 flutter.buildFlutterApplication rec {
   pname = "firmware-updater";
-  version = "0-unstable-2024-10-03";
+  version = "0-unstable-2024-20-11";
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
 
-  sourceRoot = "./source/packages/firmware_updater";
+  sourceRoot = "./source/apps/firmware_updater";
 
   gitHashes = {
     fwupd = "sha256-l/+HrrJk1mE2Mrau+NmoQ7bu9qhHU6wX68+m++9Hjd4=";
@@ -20,8 +20,8 @@ flutter.buildFlutterApplication rec {
   src = fetchFromGitHub {
     owner = "canonical";
     repo = "firmware-updater";
-    rev = "ce300838d95c5955423eedcac8b05589b14a8c52";
-    hash = "sha256-o3OU43pEzo8FC5e6kknB8BV9n7U4RMqg/+CDbHraAKw=";
+    rev = "ab5d44d594d68d106aafb511252a94a24e94d601";
+    hash = "sha256-4a0OojgNvOpvM4+8uSslxxKb6uwKDfDkvHo29rMXynQ=";
   };
 
   meta = with lib; {
