96c42191419e0f8a3e98a5a514e8ae33d7559b62
Matt Sturgeon <matt@sturgeon.me.uk>
2024-12-11 14:47:56 +0000

    jetbrains: rename source-package arg `buildVer` -> `buildNumber`
    (cherry picked from commit 11369cdb18a1b047e6721fb12bc2893eb8dfd78e)

--- pkgs/applications/editors/jetbrains/source/default.nix
+++ pkgs/applications/editors/jetbrains/source/default.nix
@@ -4,7 +4,7 @@
 {
   idea-community = callPackage ./build.nix {
     version = "2024.1.3";
-    buildVer = "241.17890.1";
+    buildNumber = "241.17890.1";
     buildType = "idea";
     ideaHash = "sha256-jWFnewxRkriSmV6CgGX1r//uaErMINfx3Z+JpkE34jk=";
     androidHash = "sha256-hX2YdRYNRg0guskNiYfxdl9osgZojRen82IhgA6G0Eo=";
@@ -14,7 +14,7 @@
   };
   pycharm-community = callPackage ./build.nix {
     version = "2024.1.3";
-    buildVer = "241.17890.14";
+    buildNumber = "241.17890.14";
     buildType = "pycharm";
     ideaHash = "sha256-tTB91/RHEWP/ZILPNFAbolVBLvgjLXTdD/uF/pdJ22Y=";
     androidHash = "sha256-hX2YdRYNRg0guskNiYfxdl9osgZojRen82IhgA6G0Eo=";
