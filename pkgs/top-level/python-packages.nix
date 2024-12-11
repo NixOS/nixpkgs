ad253f9d6731003d90c367daa8043b8105fbf65b
Levi Gross <levi@levigross.com>
2024-12-11 14:28:42 -0500

    python3Packages.splunk-sdk: init at 2.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15233,6 +15233,8 @@ self: super: with self; {
 
   splinter = callPackage ../development/python-modules/splinter { };
 
+  splunk-sdk = callPackage ../development/python-modules/splunk-sdk { };
+
   spotifyaio = callPackage ../development/python-modules/spotifyaio { };
 
   spotipy = callPackage ../development/python-modules/spotipy { };
