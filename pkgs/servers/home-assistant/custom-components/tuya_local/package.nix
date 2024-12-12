e354b144199ca8f4706107affe26e39be35d0d79
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-12-12 03:10:50 +0100

    home-assistant-custom-components.tuya_local: 2024.8.0 -> 2024.11.4
    https://github.com/make-all/tuya-local/releases/tag/2024.11.4

--- pkgs/servers/home-assistant/custom-components/tuya_local/package.nix
+++ pkgs/servers/home-assistant/custom-components/tuya_local/package.nix
@@ -11,13 +11,13 @@
 buildHomeAssistantComponent rec {
   owner = "make-all";
   domain = "tuya_local";
-  version = "2024.8.0";
+  version = "2024.11.4";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "tuya-local";
     rev = "refs/tags/${version}";
-    hash = "sha256-IHTWcNxmNXJk7SNnrLNFbaXJQSg6VYkAgAVmyt3JmRw=";
+    hash = "sha256-Mmcq0GBWiE6IWUVL6q9lw29wKEymAQn439/pOSqWgdQ=";
   };
 
   dependencies = [
