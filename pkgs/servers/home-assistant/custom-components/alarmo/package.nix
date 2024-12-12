c96b349fcbd5735ede2feb74f1e80c7ff4c5be56
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-12-12 02:53:30 +0100

    home-assistant-custom-components.alarmo: 1.10.4 -> 1.10.7
    https://github.com/nielsfaber/alarmo/releases/tag/v1.10.7

--- pkgs/servers/home-assistant/custom-components/alarmo/package.nix
+++ pkgs/servers/home-assistant/custom-components/alarmo/package.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent rec {
   owner = "nielsfaber";
   domain = "alarmo";
-  version = "1.10.4";
+  version = "1.10.7";
 
   src = fetchFromGitHub {
     owner = "nielsfaber";
     repo = "alarmo";
     rev = "refs/tags/v${version}";
-    hash = "sha256-/hNzGPckLHUX0mrBF3ugAXstrOc1mWdati+nRJCwldc=";
+    hash = "sha256-EFR8GveMNpwhrIA0nP+Ny3YUTHAOFw+IF72hH1+wMSM=";
   };
 
   postPatch = ''
