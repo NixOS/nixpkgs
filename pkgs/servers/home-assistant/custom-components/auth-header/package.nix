8fcc2a1e57b0d6a835c09d651b8b4c70bf6cb188
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-12-12 02:53:50 +0100

    home-assistant-custom-components.auth-header: 1.10-unstable-2024-02-26 -> 1.11
    https://github.com/BeryJu/hass-auth-header/releases/tag/v1.11

--- pkgs/servers/home-assistant/custom-components/auth-header/package.nix
+++ pkgs/servers/home-assistant/custom-components/auth-header/package.nix
@@ -7,19 +7,17 @@
 buildHomeAssistantComponent rec {
   owner = "BeryJu";
   domain = "auth_header";
-  version = "1.10-unstable-2024-02-26";
+  version = "1.11";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "hass-auth-header";
-    rev = "5923cb33b57a9d3c23513d54cc74b02ebd243409";
-    hash = "sha256-ZYd1EduzoljaY3OnpjsKEAwtf03435zJmZtgqzbdjjA=";
+    tag = "v${version}";
+    hash = "sha256-N2jEFyb/OWsO48rAuQBDHtQ5yKfIrGTcwlEb2P3LyVc=";
   };
 
-  # build step just runs linter
-  dontBuild = true;
-
   meta = with lib; {
+    changelog = "https://github.com/BeryJu/hass-auth-header/releases/tag/v${version}";
     description = "Home Assistant custom component which allows you to delegate authentication to a reverse proxy";
     homepage = "https://github.com/BeryJu/hass-auth-header";
     maintainers = with maintainers; [ mjm ];
