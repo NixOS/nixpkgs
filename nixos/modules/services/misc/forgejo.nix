NixOS/nixpkgs

commit 402b5c67a8ce96709d31c40e000bfdd339b23409

nixos/forgejo: pass {env}`GIT_PROTOCOL` via ssh to forgejo
when using the host's openssh service (not the builtin golang one).

This enables the use of the much faster and more efficient wire protocol
version 2.
See https://git-scm.com/docs/protocol-v2

nixos/modules/services/misc/forgejo.nix
@@ -632,6 +632,8 @@ in
       };
     };
 
+    services.openssh.settings.AcceptEnv = mkIf (!cfg.settings.START_SSH_SERVER or false) "GIT_PROTOCOL";
+
     users.users = mkIf (cfg.user == "forgejo") {
       forgejo = {
         home = cfg.stateDir;

commit ecd595ef8069a328110c976e5f2df2fab18a96cc

nixos/forgejo: fix typo in builtin ssh server conditional
This conditional was introduced in 402b5c67a8ce96709d31c40e000bfdd339b23409
and is supposed to check for `server.START_SSH_SERVER` but instead
checked for `START_SSH_SERVER`.

Co-Authored-By: Pyrox <pyrox@pyrox.dev>

nixos/modules/services/misc/forgejo.nix
@@ -789,7 +789,9 @@ in
       } // lib.listToAttrs (map (e: lib.nameValuePair e.env "%d/${e.env}") secrets);
     };
 
-    services.openssh.settings.AcceptEnv = mkIf (!cfg.settings.START_SSH_SERVER or false) "GIT_PROTOCOL";
+    services.openssh.settings.AcceptEnv = mkIf (
+      !cfg.settings.server.START_SSH_SERVER or false
+    ) "GIT_PROTOCOL";
 
     users.users = mkIf (cfg.user == "forgejo") {
       forgejo = {