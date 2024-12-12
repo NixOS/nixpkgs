a326aedefd8b0fdb6955a248cf3f62dce3a84475
Felix Buehler <account@buehler.rocks>
2024-12-12 22:57:31 +0100

    python3Packages.dugong: deprecate

--- pkgs/top-level/python-aliases.nix
+++ pkgs/top-level/python-aliases.nix
@@ -189,6 +189,7 @@ mapAliases ({
   doctest-ignore-unicode = throw "doctest-ignore-unicode has been removed since it has been unmaintained for 11 years"; # added 2024-05-20
   dogpile_cache = dogpile-cache; # added 2021-10-28
   dogpile-core = throw "dogpile-core is no longer maintained, use dogpile-cache instead"; # added 2021-11-20
+  dugong = throw "dugong is unmaintained since 2022 and has therefore been removed"; # added 2024-12-12
   eebrightbox = throw "eebrightbox is unmaintained upstream and has therefore been removed"; # added 2022-02-03
   EasyProcess = easyprocess; # added 2023-02-19
   email_validator = email-validator; # added 2022-06-22
