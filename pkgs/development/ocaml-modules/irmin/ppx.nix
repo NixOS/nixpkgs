7170b6e60fb3c40800c92bb69e7b440902416864
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-12 01:36:15 +0000

    ocamlPackages.ppx_irmin: 3.9.0 -> 3.10.0

--- pkgs/development/ocaml-modules/irmin/ppx.nix
+++ pkgs/development/ocaml-modules/irmin/ppx.nix
@@ -9,11 +9,11 @@
 
 buildDunePackage rec {
   pname = "ppx_irmin";
-  version = "3.9.0";
+  version = "3.10.0";
 
   src = fetchurl {
     url = "https://github.com/mirage/irmin/releases/download/${version}/irmin-${version}.tbz";
-    hash = "sha256-jgc6vhtf+1ttWMMmBsnX2rwyxTUBdWvoCpLtR3etUaA=";
+    hash = "sha256-kqneegoqNcL+ugw1qAax8N8kwcDRUWTuvz+RkpbSZxU=";
   };
 
   minimalOCamlVersion = "4.10";
