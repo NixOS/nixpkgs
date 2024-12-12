3108d20fe802e8f3ddca93fc6bf2dd4f159c078a
Leah Amelia Chen <hi@pluie.me>
2024-12-12 12:41:58 +0100

    glycin-loaders: replace glycinPathsPatch with patchHook
    `glycinPathsPatch` doesn't work with apps that use `rustPlatform.cargoSetupHook` since it assumes a
    setup where snapshots of GNOME sources already contain vendored Rust dependencies under `vendor/`.
    
    `patchHook` instead provides a much more flexible approach that uses the `$cargoDepsCopy` variable
    provided by `cargoSetupHook`, which points to the local, modifiable copy of Cargo dependencies,
    to patch `src/sandbox.rs` just like `glycinPathsPatch`. If `$cargoDepsCopy` is not found, then the
    old behavior is used where Cargo dependencies are assumed to be under `vendor/`.
    
    The `patchHook` also consolidates the various checksum patches and additions to `gappsWrapperArgs`,
    making packages that use glycin much more DRY. Hopefully, new packages using glycin would be able to
    simply add `glycin-loaders.patchHook` to their `nativeBuildInputs`, and have it work OOTB.

--- pkgs/by-name/gl/glycin-loaders/package.nix
+++ pkgs/by-name/gl/glycin-loaders/package.nix
@@ -2,8 +2,6 @@
   stdenv,
   lib,
   fetchurl,
-  substituteAll,
-  bubblewrap,
   cairo,
   cargo,
   git,
@@ -19,6 +17,11 @@
   ninja,
   pkg-config,
   rustc,
+
+  makeSetupHook,
+  bubblewrap,
+  jq,
+  moreutils,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -29,14 +32,6 @@ stdenv.mkDerivation (finalAttrs: {
     url = "mirror://gnome/sources/glycin/${lib.versions.majorMinor finalAttrs.version}/glycin-${finalAttrs.version}.tar.xz";
     hash = "sha256-Qccr4eybpV2pDIL8GFc7dC3/WCsJr8N7RWXEfpnMj/Q=";
   };
-
-  patches = [
-    # Fix paths in glycin library.
-    # Not actually needed for this package since we are only building loaders
-    # and this patch is relevant just to apps that use the loaders
-    # but apply it here to ensure the patch continues to apply.
-    finalAttrs.passthru.glycinPathsPatch
-  ];
 
   nativeBuildInputs = [
     cargo
@@ -70,10 +65,17 @@ stdenv.mkDerivation (finalAttrs: {
       packageName = "glycin";
     };
 
-    glycinPathsPatch = substituteAll {
-      src = ./fix-glycin-paths.patch;
-      bwrap = "${bubblewrap}/bin/bwrap";
-    };
+    patchHook = makeSetupHook {
+      name = "glycin-loader-patch-hook";
+      propagatedBuildInputs = [
+        jq
+        moreutils
+      ];
+      substitutions = {
+        bwrap = lib.getExe bubblewrap;
+        glycinloaders = finalAttrs.finalPackage;
+      };
+    } ./hook.sh;
   };
 
   meta = {
