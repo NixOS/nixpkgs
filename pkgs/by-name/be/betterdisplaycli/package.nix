f0d443e3c8ba2fd7dc100ecefab1d839eecd08dd
quinneden <quinnyxboy@gmail.com>
2024-12-12 21:40:32 -0800

    betterdisplaycli: init at v1.0.1

---
+++ pkgs/by-name/be/betterdisplaycli/package.nix
@@ -0,0 +1,39 @@
+{
+  fetchFromGitHub,
+  lib,
+  nix-update-script,
+  stdenv,
+  swift,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "betterdisplaycli";
+  version = "1.0.1";
+
+  src = fetchFromGitHub {
+    owner = "waydabber";
+    repo = "betterdisplaycli";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-rfJDoPzIsBaKkGhgsGciErAoDycMDojxjYnT+oQZjnA=";
+  };
+
+  buildInputs = [ swift ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp betterdisplaycli $out/bin
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Simple CLI access for BetterDisplay";
+    homepage = "https://github.com/waydabber/betterdisplaycli";
+    license = lib.licenses.mit;
+    mainProgram = "betterdisplaycli";
+    maintainers = [ ];
+    platforms = [ "aarch64-darwin" ];
+  };
+})
