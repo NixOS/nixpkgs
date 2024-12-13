3fde1968606c78e4b58fc50ef18e69d1508d4d96
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-12-13 15:40:29 +0100

    yt-dlp: 2024.12.6 -> 2024.12.13
    Changelog: https://github.com/yt-dlp/yt-dlp/blob/HEAD/Changelog.md

--- pkgs/by-name/yt/yt-dlp/package.nix
+++ pkgs/by-name/yt/yt-dlp/package.nix
@@ -17,13 +17,13 @@ python3Packages.buildPythonApplication rec {
   # The websites yt-dlp deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2024.12.6";
+  version = "2024.12.13";
   pyproject = true;
 
   src = fetchPypi {
     inherit version;
     pname = "yt_dlp";
-    hash = "sha256-dD2+CB6ocb4/X/CD4s2V2oZt6nc/xwrmsQmDjPv3KsQ=";
+    hash = "sha256-d+Fa+51GDstylKObteOdyfTopl86N+9NtYgAuU0JVRE=";
   };
 
   build-system = with python3Packages; [
