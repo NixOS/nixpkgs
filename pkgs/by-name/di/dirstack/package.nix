ffd6fb824d1146410ad2ff5aabb67681a7ca10a7
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-13 01:58:28 +0100

    Add dirstack package
    A cd quicklist interface

---
+++ pkgs/by-name/di/dirstack/package.nix
@@ -0,0 +1,43 @@
+{
+  fetchFromGitHub,
+  fzf,
+  lib,
+  rustPlatform,
+  stdenv,
+}: let
+  src = fetchFromGitHub {
+    owner = "NewDawn0";
+    repo = "dirStack";
+    rev = "8ef0f19ae366868fb046f6acb0d396bc8436ef31";
+    hash = "sha256-mzw3uDZ0eM81WJM0YNcOlMHs4fNMUBgwxS6VBSS+VS8=";
+  };
+  meta = with lib; {
+    description = "A cd quicklist";
+    homepage = "https://github.com/NewDawn0/dirStack";
+    maintainers = with maintainers; [NewDawn0];
+    license = licenses.mit;
+  };
+  pkg = rustPlatform.buildRustPackage {
+    inherit meta src;
+    pname = "dirStack";
+    version = "1.0.0";
+    propagatedBuildInputs = [fzf];
+    cargoLock.lockFile = "${src}/Cargo.lock";
+  };
+in
+  stdenv.mkDerivation {
+    inherit meta;
+    name = "dirStack-wrapped";
+    version = "1.0.0";
+    phases = ["installPhase"];
+    installPhase = ''
+      mkdir -p $out/bin $out/lib
+      cp ${pkg}/bin/dirStack $out/bin/
+      echo "#!/usr/bin/env bash" > $out/lib/SOURCE_ME.sh
+      $out/bin/dirStack --init >> $out/lib/SOURCE_ME.sh
+      chmod +x $out/lib/SOURCE_ME.sh
+    '';
+    shellHook = ''
+      source $out/lib/SOURCE_ME.sh
+    '';
+  }
