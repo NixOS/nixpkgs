fead3415549509ae9f36f6e9534c995bbce13584
Robert Schütz <mail@dotlambda.de>
2024-12-13 08:22:27 -0800

    bitwarden-cli: 2024.11.1 -> 2024.12.0
    Diff: https://github.com/bitwarden/clients/compare/cli-v2024.11.1...cli-v2024.12.0
    
    Changelog: https://github.com/bitwarden/clients/releases/tag/cli-v2024.12.0

--- pkgs/by-name/bi/bitwarden-cli/package.nix
+++ pkgs/by-name/bi/bitwarden-cli/package.nix
@@ -13,13 +13,13 @@
 
 buildNpmPackage rec {
   pname = "bitwarden-cli";
-  version = "2024.11.1";
+  version = "2024.12.0";
 
   src = fetchFromGitHub {
     owner = "bitwarden";
     repo = "clients";
     rev = "cli-v${version}";
-    hash = "sha256-J7gmrSAiu59LLP9pKfbv9+H00vXGQCrjkd4GBhkcyTY=";
+    hash = "sha256-3aN2t8/qhN0sjACvtip45efHQJl8nEMNre0+oBL1/go=";
   };
 
   postPatch = ''
@@ -29,7 +29,7 @@ buildNpmPackage rec {
 
   nodejs = nodejs_20;
 
-  npmDepsHash = "sha256-MZoreHKmiUUxhq3tmL4lPp6vPmoQIqG3IPpZE56Z1Kc=";
+  npmDepsHash = "sha256-EtIcqbubAYN9I9wbw17oHiVshd3GtQayFtdgqWP7Pgg=";
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
     cctools
