9b99a9851e779143b811235274a3856b43d2e891
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-13 03:33:00 +0000

    govc: 0.46.2 -> 0.46.3

--- pkgs/by-name/go/govc/package.nix
+++ pkgs/by-name/go/govc/package.nix
@@ -6,7 +6,7 @@
 
 buildGoModule rec {
   pname = "govc";
-  version = "0.46.2";
+  version = "0.46.3";
 
   subPackages = [ "govc" ];
 
@@ -14,7 +14,7 @@ buildGoModule rec {
     rev = "v${version}";
     owner = "vmware";
     repo = "govmomi";
-    sha256 = "sha256-g93buOK2JqkI1PkU1ImoBfj+qf5fOks3ceevbif/Kcs=";
+    sha256 = "sha256-vwvhVC+Avi55uenlW68tWGREEHRZw8RUjgiC0r7BgQ4=";
   };
 
   vendorHash = "sha256-ddofXjBnyHRn7apS8hpM57S1oo+1w5i4n0Z6ZPKQEDI=";
