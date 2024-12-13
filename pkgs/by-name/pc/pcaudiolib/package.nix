921233985c512efec8119b7de55b9958ad0110f9
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-13 00:58:02 +0000

    pcaudiolib: 1.2 -> 1.3

--- pkgs/by-name/pc/pcaudiolib/package.nix
+++ pkgs/by-name/pc/pcaudiolib/package.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pcaudiolib";
-  version = "1.2";
+  version = "1.3";
 
   src = fetchFromGitHub {
     owner = "espeak-ng";
     repo = "pcaudiolib";
     rev = finalAttrs.version;
-    hash = "sha256-ZG/HBk5DHaZP/H3M01vDr3M2nP9awwsPuKpwtalz3EE=";
+    hash = "sha256-bBiGvAySEwAv6Qj2iSawb9oZfMCGBDCDIP8AYdbtQQc=";
   };
 
   nativeBuildInputs = [
