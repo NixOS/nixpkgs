c122fdeec9fda792ec509811df4abfe2852fd780
Antoine Viallon <antoine@lesviallon.fr>
2024-12-13 23:47:16 +0100

    whisper-cpp: convert to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8143,12 +8143,6 @@ with pkgs;
 
   openai-whisper = with python3.pkgs; toPythonApplication openai-whisper;
 
-  whisper-cpp = callPackage ../tools/audio/openai-whisper-cpp { };
-
-  whisper-cpp-vulkan = whisper-cpp.override {
-    vulkanSupport = true;
-  };
-
   openocd-rp2040 = openocd.overrideAttrs (old: {
     pname = "openocd-rp2040";
     src = fetchFromGitHub {
@@ -8418,6 +8412,10 @@ with pkgs;
 
   whatstyle = callPackage ../development/tools/misc/whatstyle {
     inherit (llvmPackages) clang-unwrapped;
+  };
+
+  whisper-cpp-vulkan = whisper-cpp.override {
+    vulkanSupport = true;
   };
 
   watson-ruby = callPackage ../development/tools/misc/watson-ruby { };
