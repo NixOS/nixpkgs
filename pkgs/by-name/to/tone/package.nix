b4bd73ceed445af90744883a45f315891d3811ef
Jordan Williams <jordan@jwillikers.com>
2024-12-13 14:50:34 -0600

    tone: clean up

--- pkgs/by-name/to/tone/package.nix
+++ pkgs/by-name/to/tone/package.nix
@@ -12,9 +12,9 @@ buildDotnetModule rec {
 
   src = fetchFromGitHub {
     owner = "sandreas";
-    repo = pname;
+    repo = "tone";
     rev = "v${version}";
-    sha256 = "sha256-HhXyOPoDtraT7ef0kpE7SCQbvGFLrTddzS6Kdu0LxW4=";
+    hash = "sha256-HhXyOPoDtraT7ef0kpE7SCQbvGFLrTddzS6Kdu0LxW4=";
   };
 
   projectFile = "tone/tone.csproj";
@@ -26,14 +26,15 @@ buildDotnetModule rec {
   ];
 
   dotnet-sdk = dotnetCorePackages.sdk_6_0;
+  dotnet-runtime = dotnetCorePackages.sdk_6_0;
   runtimeDeps = [ ffmpeg-full ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/sandreas/tone";
     description = "Cross platform utility to dump and modify audio metadata for a wide variety of formats";
-    license = licenses.asl20;
-    maintainers = [ maintainers.jvanbruegge ];
-    platforms = platforms.linux;
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ jvanbruegge ];
+    platforms = lib.platforms.linux;
     mainProgram = "tone";
   };
 }
