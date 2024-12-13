b0de57ddb8a877c1cccdea974644b047378bc1cc
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-13 05:23:56 +0000

    netscanner: 0.6.1 -> 0.6.2

--- pkgs/by-name/ne/netscanner/package.nix
+++ pkgs/by-name/ne/netscanner/package.nix
@@ -7,7 +7,7 @@
 }:
 let
   pname = "netscanner";
-  version = "0.6.1";
+  version = "0.6.2";
 in
 rustPlatform.buildRustPackage {
   inherit pname version;
@@ -18,10 +18,10 @@ rustPlatform.buildRustPackage {
     owner = "Chleba";
     repo = "netscanner";
     rev = "refs/tags/v${version}";
-    hash = "sha256-/EHg8YnsGwFlXEfdsh1eiWoNmapDHGOws4lkEGqjhoo=";
+    hash = "sha256-/8UhnJ4ZPaJ+CQPCeKr1uH0Iw6A4/DARiSukK6+ZZeY=";
   };
 
-  cargoHash = "sha256-uqTSr+c+pDAi2r6roHeZfW0LCyN/1J0M2+8grdAtX7U=";
+  cargoHash = "sha256-u6Q9QPyOSMc356fkO4XVWUL0GGN1c2CoKhVvt1P30AU=";
 
   postFixup = ''
     wrapProgram $out/bin/netscanner \
