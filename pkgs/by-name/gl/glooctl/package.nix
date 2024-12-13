9f58354e35d906b441bb2c14896a135ea246abc5
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-13 02:21:38 +0000

    glooctl: 1.17.16 -> 1.18.0

--- pkgs/by-name/gl/glooctl/package.nix
+++ pkgs/by-name/gl/glooctl/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule rec {
   pname = "glooctl";
-  version = "1.17.16";
+  version = "1.18.0";
 
   src = fetchFromGitHub {
     owner = "solo-io";
     repo = "gloo";
     rev = "v${version}";
-    hash = "sha256-GU6Vdnb4eQnpv9DwpLJvwUONwmEG9mOORojBsJZNATw=";
+    hash = "sha256-lzO5jRHGjk6G7Rt4jPlCrw9ecO7KXvfQEFJZ4/6LtkM=";
   };
 
-  vendorHash = "sha256-NXQTDiUBQEr/QzpDV8b13nGUJ3YmkZNYBrlT4tiRalk=";
+  vendorHash = "sha256-i2FBlywKuFXjc+kwZw8A0Anea26eMbw3v7gBH5dh0WE=";
 
   subPackages = [ "projects/gloo/cli/cmd" ];
 
