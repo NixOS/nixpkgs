NixOS/nixpkgs

commit 7f8278a2640dd74794550ba4c99e851f4b74c5a2

nixos/zfs: do not auto-import pools marked noauto
This requires all datasets for the pool specified in `fileSystems` to be
marked noauto.

Note that this implicitly skips some tasks for the pool, such as
`expandOnBoot`, as the pool will not be imported at that time.

nixos/modules/tasks/filesystems/zfs.nix
@@ -128,7 +128,9 @@ let
         "systemd-modules-load.service"
         "systemd-ask-password-console.service"
       ] ++ lib.optional (config.boot.initrd.clevis.useTang) "network-online.target";
-      requiredBy = getPoolMounts prefix pool ++ [ "zfs-import.target" ];
+      requiredBy = let
+        noauto = lib.all (fs: lib.elem "noauto" fs.options) (getPoolFilesystems pool);
+      in getPoolMounts prefix pool ++ lib.optional (!noauto) "zfs-import.target";
       before = getPoolMounts prefix pool ++ [ "shutdown.target" "zfs-import.target" ];
       conflicts = [ "shutdown.target" ];
       unitConfig = {

commit 4ed27101c608458967053d2b601a4dd281f80365

nixos/zfs: fix not auto-importing pools without any `fileSystems`
`lib.all (_: false) [ ]` is `true`, which is not the semantics we want
here, and interacts poorly with `boot.zfs.extraPools` where
`getPoolFilesystems` may return `[]`. So explicitly handle this case.
This does mean there isn’t a straightforward way to disable auto-import
with `extraPools` and no associated `fileSystems` while keeping the unit
around, but that’s probably okay for now.

See https://github.com/NixOS/nixpkgs/issues/364995.
Broken in 7f8278a2640dd74794550ba4c99e851f4b74c5a2.

nixos/modules/tasks/filesystems/zfs.nix
@@ -129,7 +129,8 @@ let
         "systemd-ask-password-console.service"
       ] ++ lib.optional (config.boot.initrd.clevis.useTang) "network-online.target";
       requiredBy = let
-        noauto = lib.all (fs: lib.elem "noauto" fs.options) (getPoolFilesystems pool);
+        poolFilesystems = getPoolFilesystems pool;
+        noauto = poolFilesystems != [ ] && lib.all (fs: lib.elem "noauto" fs.options) poolFilesystems;
       in getPoolMounts prefix pool ++ lib.optional (!noauto) "zfs-import.target";
       before = getPoolMounts prefix pool ++ [ "shutdown.target" "zfs-import.target" ];
       conflicts = [ "shutdown.target" ];