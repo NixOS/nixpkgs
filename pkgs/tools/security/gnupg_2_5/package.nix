d9bf3cff51a89547985e9b03bd9c2c111d63d4fd
test <lunaticare@users.noreply.github.com>
2024-12-14 18:38:21 +0000

    fix

--- pkgs/tools/security/gnupg_2_5/package.nix
+++ pkgs/tools/security/gnupg_2_5/package.nix
@@ -73,8 +73,6 @@ stdenv.mkDerivation rec {
     ./accept-subkeys-with-a-good-revocation-but-no-self-sig.patch
     ./24-allow-import-of-previously-known-keys-even-without-UI.patch
     ./24-revert-rfc4880bis-defaults.patch
-    # Patch for DoS vuln from https://seclists.org/oss-sec/2022/q3/27
-    # ./v3-0001-Disallow-compressed-signatures-and-certificates.patch
   ];
 
   postPatch = ''
