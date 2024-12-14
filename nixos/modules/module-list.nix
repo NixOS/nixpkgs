fcce2fda481ff06fa6956b2a2e1cf539b7100ec8
thiloho <123883702+thiloho@users.noreply.github.com>
2024-12-14 16:36:03 +0100

    nixos/archtika: init module
    nixos/manual: add archtika module to 25.05 release notes
    
    nixos/archtika: fix module formatting, add description and remove trailing whitespace
    
    nixos/archtika: refactor module
    
    nixos/archtika: refactor module
    
    nixos/archtika: make SystemCallFilter addition for postgres systemd service
    
    nixos/archtika: refactor module
    
    nixos/archtika: grant only necessary authentication permissions to archtika db

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1445,6 +1445,7 @@
   ./services/web-apps/agorakit.nix
   ./services/web-apps/alps.nix
   ./services/web-apps/anuko-time-tracker.nix
+  ./services/web-apps/archtika.nix
   ./services/web-apps/artalk.nix
   ./services/web-apps/audiobookshelf.nix
   ./services/web-apps/bluemap.nix
