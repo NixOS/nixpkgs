e11fe5a933c21746951e9cfd9e27204adeb77962
Fabian Affolter <mail@fabian-affolter.ch>
2024-12-15 11:59:20 +0100

    python312Packages.bluemaestro-ble: refactor

--- pkgs/development/python-modules/bluemaestro-ble/default.nix
+++ pkgs/development/python-modules/bluemaestro-ble/default.nix
@@ -13,20 +13,20 @@
 buildPythonPackage rec {
   pname = "bluemaestro-ble";
   version = "0.2.3";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "Bluetooth-Devices";
-    repo = pname;
+    repo = "bluemaestro-ble";
     rev = "refs/tags/v${version}";
     hash = "sha256-mJ5JNGN4F9U3WMJQDwiZwuxE0zOirwo1sWF3/bVwXhY=";
   };
 
-  nativeBuildInputs = [ poetry-core ];
+  build-system = [ poetry-core ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     bluetooth-data-tools
     bluetooth-sensor-state-data
     sensor-state-data
@@ -45,7 +45,7 @@ buildPythonPackage rec {
     description = "Library for bluemaestro BLE devices";
     homepage = "https://github.com/Bluetooth-Devices/bluemaestro-ble";
     changelog = "https://github.com/Bluetooth-Devices/bluemaestro-ble/blob/v${version}/CHANGELOG.md";
-    license = with licenses; [ mit ];
+    license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
 }
