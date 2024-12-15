fb69f66d0c961c5791967b571a5f3eba34d25480
Thiago Kenji Okada <thiagokokada@gmail.com>
2024-12-15 05:47:33 +0000

    nixos-rebuild-ng: WITH_NIX_2_18 should default to true

--- pkgs/by-name/ni/nixos-rebuild-ng/src/nixos_rebuild/constants.py
+++ pkgs/by-name/ni/nixos-rebuild-ng/src/nixos_rebuild/constants.py
@@ -1,6 +1,9 @@
 # Build-time flags
-# Strings to avoid breaking standalone (e.g.: `python -m nixos_rebuild`) usage
+# Use strings to avoid breaking standalone (e.g.: `python -m nixos_rebuild`)
+# usage
 EXECUTABLE = "@executable@"
-WITH_NIX_2_18 = "@withNix218@" == "true"  # type: ignore
+# Use either `== "true"` if the default (e.g.: `python -m nixos_rebuld`) is
+# `False` or `!= "false"` if the default is `True`
+WITH_NIX_2_18 = "@withNix218@" != "false"  # type: ignore
 WITH_REEXEC = "@withReexec@" == "true"  # type: ignore
 WITH_SHELL_FILES = "@withShellFiles@" == "true"  # type: ignore
