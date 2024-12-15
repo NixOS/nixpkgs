9650c6720641add9329eca868a89e6caf37204dc
Fabian Affolter <mail@fabian-affolter.ch>
2024-12-15 13:11:20 +0100

    python312Packages.ld2410-ble: refactor

--- pkgs/development/python-modules/ld2410-ble/default.nix
+++ pkgs/development/python-modules/ld2410-ble/default.nix
@@ -13,13 +13,13 @@
 buildPythonPackage rec {
   pname = "ld2410-ble";
   version = "0.2.0";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "930913";
-    repo = pname;
+    repo = "ld2410-ble";
     rev = "refs/tags/v${version}";
     hash = "sha256-wQnE2hNT0UOnPJbHq1eayIO8g0XRZvEH6V19DL6RqoA=";
   };
@@ -29,9 +29,9 @@ buildPythonPackage rec {
       --replace " --cov=ld2410_ble --cov-report=term-missing:skip-covered" ""
   '';
 
-  nativeBuildInputs = [ poetry-core ];
+  build-system = [ poetry-core ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     async-timeout
     bleak
     bleak-retry-connector
@@ -45,7 +45,7 @@ buildPythonPackage rec {
     description = "Library for the LD2410B modules from HiLinks";
     homepage = "https://github.com/930913/ld2410-ble";
     changelog = "https://github.com/930913/ld2410-ble/blob/v${version}/CHANGELOG.md";
-    license = with licenses; [ mit ];
+    license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
 }
