a20970d8a2c6eddaccb14ec1c403584619c65ad5
Fabian Affolter <mail@fabian-affolter.ch>
2024-12-15 13:00:11 +0100

    python312Packages.qingping-ble: refactor

--- pkgs/development/python-modules/qingping-ble/default.nix
+++ pkgs/development/python-modules/qingping-ble/default.nix
@@ -13,20 +13,20 @@
 buildPythonPackage rec {
   pname = "qingping-ble";
   version = "0.10.0";
-  format = "pyproject";
+  pyproject = true;
 
   disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "bluetooth-devices";
-    repo = pname;
+    repo = "qingping-ble";
     rev = "refs/tags/v${version}";
     hash = "sha256-5w3KGJLdHFv6kURKTz3YImZNjaETiVqbbJTJpBSLSo8=";
   };
 
-  nativeBuildInputs = [ poetry-core ];
+  build-system = [ poetry-core ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     bluetooth-data-tools
     bluetooth-sensor-state-data
     sensor-state-data
@@ -45,7 +45,7 @@ buildPythonPackage rec {
     description = "Library for Qingping BLE devices";
     homepage = "https://github.com/bluetooth-devices/qingping-ble";
     changelog = "https://github.com/Bluetooth-Devices/qingping-ble/blob/v${version}/CHANGELOG.md";
-    license = with licenses; [ mit ];
+    license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
 }
