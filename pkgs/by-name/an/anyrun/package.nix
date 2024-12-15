4f8e075ba1624137549196f0b95bcebeb19168b2
Austin Horstman <khaneliman12@gmail.com>
2024-12-14 23:27:28 -0600

    anyrun: 0-unstable-2023-12-01 -> 0-unstable-2024-11-08

--- pkgs/by-name/an/anyrun/package.nix
+++ pkgs/by-name/an/anyrun/package.nix
@@ -17,16 +17,16 @@
 
 rustPlatform.buildRustPackage {
   pname = "anyrun";
-  version = "0-unstable-2023-12-01";
+  version = "0-unstable-2024-11-08";
 
   src = fetchFromGitHub {
     owner = "kirottu";
     repo = "anyrun";
-    rev = "e14da6c37337ffa3ee1bc66965d58ef64c1590e5";
-    hash = "sha256-hI9+KBShsSfvWX7bmRa/1VI20WGat3lDXmbceMZzMS4=";
+    rev = "d2017f224b2bfd7e33573c7070e7c3e2960c7dcc";
+    hash = "sha256-L1hLXf4IDZ0KoXbFiSNNZJ7IrReEr/J+CLt6Rl4Ea3M=";
   };
 
-  cargoHash = "sha256-apOQc9Z6YANoaeKcbNxBfAv7mmGFB+CagrYRPgC5wLY=";
+  cargoHash = "sha256-DgUNSRr2Hs+GeYeR2ex8WiwotmT12G1cjSvQVK2py3c=";
 
   strictDeps = true;
   enableParallelBuilding = true;
