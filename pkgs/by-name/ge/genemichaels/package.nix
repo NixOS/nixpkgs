1fe3502e9590477be84ed3b739df956bc666d11b
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-14 21:53:42 -0800

    genemichaels: apply patch to only write files if changed
    When formatting rust files with `genemichaels`, the timestamp of the
    file is always modified whether or not the file was changed. This is
    problematic for detecting whether a file has been reformatted or not,
    such as when using tools such as `pre-commit`, `treefmt`, and
    `treefmt-nix`.
    
    See the following:
    https://github.com/andrewbaxter/genemichaels/issues/95
    https://github.com/andrewbaxter/genemichaels/pull/96
    https://github.com/numtide/treefmt-nix/pull/277

--- pkgs/by-name/ge/genemichaels/package.nix
+++ pkgs/by-name/ge/genemichaels/package.nix
@@ -15,6 +15,14 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-qPyIC9AbRQI+inBft7Dd5R5v+tXtJcZdiV4lBIBwpyM=";
   };
 
+  patches = [
+    # TODO(@djacu): remove this if this patch is merged and the version is
+    # bumped to a release that has this change
+    # See: https://github.com/andrewbaxter/genemichaels/issues/95
+    # See: https://github.com/andrewbaxter/genemichaels/pull/96
+    ./genemichaels-only-write-if-changed.patch
+  ];
+
   cargoHash = "sha256-yPxgYf8N3Dqns/uKhdM++jpzG7zTPhCVVq+7WA9G/SM=";
 
   cargoBuildFlags = [ "--package ${pname}" ];
