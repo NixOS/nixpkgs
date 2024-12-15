f8b6887e5003cfa7076aa46d24a37588363237dc
Fabian Affolter <mail@fabian-affolter.ch>
2024-12-15 11:55:04 +0100

    python312Packages.ruuvitag-ble: migrate to pytest-cov-stub

--- pkgs/development/python-modules/ruuvitag-ble/default.nix
+++ pkgs/development/python-modules/ruuvitag-ble/default.nix
@@ -6,6 +6,7 @@
   fetchFromGitHub,
   hatchling,
   home-assistant-bluetooth,
+  pytest-cov-stub,
   pytestCheckHook,
   pythonOlder,
   sensor-state-data,
@@ -34,12 +35,10 @@ buildPythonPackage rec {
     sensor-state-data
   ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
-
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace " --cov=ruuvitag_ble --cov-report=term-missing:skip-covered" ""
-  '';
+  nativeCheckInputs = [
+    pytest-cov-stub
+    pytestCheckHook
+  ];
 
   pythonImportsCheck = [ "ruuvitag_ble" ];
 
