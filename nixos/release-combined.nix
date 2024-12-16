b0324c5e601d5507e63723c1e406ac4432be3d39
Arian van Putten <arian.vanputten@gmail.com>
2024-12-16 16:01:47 +0100

    nixos/release-combined: add nixos.tests.ec2-userdata
    This is to catch issues like https://github.com/NixOS/nixpkgs/pull/340489#issuecomment-253149474
    We do not want to push NixOS updates that break SSH access on EC2 instances.

--- nixos/release-combined.nix
+++ nixos/release-combined.nix
@@ -87,6 +87,7 @@ rec {
         (onSystems [ "x86_64-linux" ] "nixos.tests.docker")
         (onFullSupported "nixos.tests.ecryptfs")
         (onFullSupported "nixos.tests.env")
+        (onSystems [ "x86_64-linux" "aarch64-linux" ] "nixos.tests.ec2-userdata")
 
         # Way too many manual retries required on Hydra.
         #  Apparently it's hard to track down the cause.
