7003629f87be452515259f9ab324692e3123f03d
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-16 18:13:37 +0000

    moodle: 4.4.4 -> 4.5.1

--- pkgs/servers/web-apps/moodle/default.nix
+++ pkgs/servers/web-apps/moodle/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchurl, writeText, plugins ? [ ], nixosTests }:
 
 let
-  version = "4.4.4";
+  version = "4.5.1";
 
   versionParts = lib.take 2 (lib.splitVersion version);
   # 4.2 -> 402, 3.11 -> 311
@@ -15,7 +15,7 @@ in stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://download.moodle.org/download.php/direct/stable${stableVersion}/${pname}-${version}.tgz";
-    hash = "sha256-9ZMQvv0fL6JwUD5ZJnzfzZkiGOfOV9zMc0hX3PKttBw=";
+    hash = "sha256-ycqtEu2KjPpP9rM74QNMssnP1kY3sG1MAHYxbELkGd0=";
   };
 
   phpConfig = writeText "config.php" ''
