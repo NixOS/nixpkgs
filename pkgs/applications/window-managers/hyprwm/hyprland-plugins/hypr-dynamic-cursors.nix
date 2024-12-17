e445c289a8424e9e21037d669681bbd70487866e
Austin Horstman <khaneliman12@gmail.com>
2024-12-17 09:54:55 -0600

    hyprlandPlugins.hypr-dynamic-cursors: 0-unstable-2024-11-19 -> 0-unstable-2024-12-14

--- pkgs/applications/window-managers/hyprwm/hyprland-plugins/hypr-dynamic-cursors.nix
+++ pkgs/applications/window-managers/hyprwm/hyprland-plugins/hypr-dynamic-cursors.nix
@@ -8,13 +8,13 @@
 
 mkHyprlandPlugin hyprland {
   pluginName = "hypr-dynamic-cursors";
-  version = "0-unstable-2024-11-19";
+  version = "0-unstable-2024-12-14";
 
   src = fetchFromGitHub {
     owner = "VirtCode";
     repo = "hypr-dynamic-cursors";
-    rev = "81f4b964f997a3174596ef22c7a1dee8a5f616c7";
-    hash = "sha256-3SDwq2i2QW9nu7HBCPuDtLmrwLt2kajzImBsawKRZ+s=";
+    rev = "f0cef070c531e3a3a1f713b7062bded357b4c468";
+    hash = "sha256-XFQqJWxo6xUqLpAKqc0bHg1pZ21eFQ1HueDTxw7N6r8=";
   };
 
   dontUseCmakeConfigure = true;
