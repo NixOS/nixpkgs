fbd8e3e405efcd88dbc43aeda9128dd88d20748b
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-17 10:58:10 -0800

    genemichaels: 0.5.7 -> 0.5.13
    Bumps to the latest working version of genemichaels. Includes an update
    so that files are not written if they are already formatted correctly.
    Removes a failing test that was present from version 0.5.8 to 0.5.12.

--- pkgs/by-name/ge/genemichaels/package.nix
+++ pkgs/by-name/ge/genemichaels/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "genemichaels";
-  version = "0.5.7";
+  version = "0.5.13";
 
   src = fetchFromGitHub {
     owner = "andrewbaxter";
     repo = "genemichaels";
     rev = "genemichaels-v${version}";
-    hash = "sha256-qPyIC9AbRQI+inBft7Dd5R5v+tXtJcZdiV4lBIBwpyM=";
+    hash = "sha256-pzGTKswETm7RR0up1eSWC+X633rsVmEAJ3DYM8z6paQ=";
   };
 
-  cargoHash = "sha256-yPxgYf8N3Dqns/uKhdM++jpzG7zTPhCVVq+7WA9G/SM=";
+  cargoHash = "sha256-FjggpBTzxj9AOJjUq5PmbuE/ImmTMpxN0se9uxRy4KQ=";
 
   cargoBuildFlags = [ "--package ${pname}" ];
 
