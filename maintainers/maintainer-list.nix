625f4b9a80dccebb1a2078687228879b20f5f99c
Lennart-rth <80266866+lennart-rth@users.noreply.github.com>
2024-12-18 00:41:13 +0100

    maintainers: add lennart-rth

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12638,6 +12638,11 @@
     github = "lenivaya";
     githubId = 49302467;
   };
+  lennartrth = {
+    name = "Lennart rth";
+    github = "lennart-rth";
+    githubId = 80266866;
+  };
   lenny = {
     name = "Lenny.";
     matrix = "lenny@flipdot.org";
