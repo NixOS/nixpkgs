65472e5aafd661a4fe6a068b9752d4d092c542f1
Bryan Gardiner <bog@khumba.net>
2024-12-16 21:50:52 -0800

    linuxPackages.system76: 1.0.16 -> 1.0.17
    This newer version fixes the build against Linux 6.12.

--- pkgs/os-specific/linux/system76/default.nix
+++ pkgs/os-specific/linux/system76/default.nix
@@ -5,8 +5,8 @@
   kernel,
 }:
 let
-  version = "1.0.16";
-  hash = "sha256-jsBLy5WDl2l8o/2ccIk1XMqOukeDX5eZ+VH5Dyo5BaA=";
+  version = "1.0.17";
+  hash = "sha256-9/t+Mvfnq0KkPbe1mnrVy4mzNaK7vAgLuhUnOeEvBfI=";
 in
 stdenv.mkDerivation {
   name = "system76-module-${version}-${kernel.version}";
