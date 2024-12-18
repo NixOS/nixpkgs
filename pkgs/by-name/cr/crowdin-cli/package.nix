4cc536d4fb230301e0959735b17e4695bad16281
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-18 14:34:37 +0000

    crowdin-cli: 4.4.1 -> 4.5.0

--- pkgs/by-name/cr/crowdin-cli/package.nix
+++ pkgs/by-name/cr/crowdin-cli/package.nix
@@ -14,11 +14,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "crowdin-cli";
-  version = "4.4.1";
+  version = "4.5.0";
 
   src = fetchurl {
     url = "https://github.com/crowdin/crowdin-cli/releases/download/${finalAttrs.version}/crowdin-cli.zip";
-    hash = "sha256-u1drLK/eLvNijCL3BupXyAO7yb+FD9EQwD0+9hQJsgQ=";
+    hash = "sha256-Qcw1935ENMftohZrwqRZRrXoXYThogfBXNzf/6jJxaM=";
   };
 
   nativeBuildInputs = [
