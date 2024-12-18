01b2f47e2920f7f91c84e13bd0847f3ec44250c7
GGG <gggkiller2@gmail.com>
2024-12-17 23:19:17 -0300

    formula: format file
    (cherry picked from commit 59d969cf96b389c2e97c4894836590d8042ace6a)

--- pkgs/by-name/fo/formula/package.nix
+++ pkgs/by-name/fo/formula/package.nix
@@ -1,4 +1,11 @@
-{ lib, stdenv, fetchFromGitHub, buildDotnetModule, dotnetCorePackages, unstableGitUpdater }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  buildDotnetModule,
+  dotnetCorePackages,
+  unstableGitUpdater,
+}:
 
 buildDotnetModule rec {
   pname = "formula-dotnet";
@@ -15,13 +22,17 @@ buildDotnetModule rec {
   nugetDeps = ./nuget.json;
   projectFile = "Src/CommandLine/CommandLine.csproj";
 
-  postFixup = if stdenv.hostPlatform.isLinux then ''
-    mv $out/bin/CommandLine $out/bin/formula
-  '' else lib.optionalString stdenv.hostPlatform.isDarwin ''
-    makeWrapper ${dotnetCorePackages.runtime_6_0}/bin/dotnet $out/bin/formula \
-      --add-flags "$out/lib/formula-dotnet/CommandLine.dll" \
-      --prefix DYLD_LIBRARY_PATH : $out/lib/formula-dotnet/runtimes/macos/native
-  '';
+  postFixup =
+    if stdenv.hostPlatform.isLinux then
+      ''
+        mv $out/bin/CommandLine $out/bin/formula
+      ''
+    else
+      lib.optionalString stdenv.hostPlatform.isDarwin ''
+        makeWrapper ${dotnetCorePackages.runtime_6_0}/bin/dotnet $out/bin/formula \
+          --add-flags "$out/lib/formula-dotnet/CommandLine.dll" \
+          --prefix DYLD_LIBRARY_PATH : $out/lib/formula-dotnet/runtimes/macos/native
+      '';
 
   passthru.updateScript = unstableGitUpdater { url = meta.homepage; };
 
