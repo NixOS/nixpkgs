d01921a0108e6416b1af925bed7300781878ec60
aucub <78630225+aucub@users.noreply.github.com>
2024-12-18 20:11:40 +0800

    sublime4: 4180 -> 4186

--- pkgs/applications/editors/sublime/4/packages.nix
+++ pkgs/applications/editors/sublime/4/packages.nix
@@ -5,9 +5,9 @@ let
 in
 {
   sublime4 = common {
-    buildVersion = "4180";
-    x64sha256 = "pl42AR4zWF3vx3wPSZkfIP7Oksune5nsbmciyJUv8D4=";
-    aarch64sha256 = "zRg2jfhi+g6iLrMF1TGAYT+QQKSNI1W4Yv1bz9oEXHg=";
+    buildVersion = "4186";
+    x64sha256 = "Rw/N2uSsMb6uMvMt9lEzBFSAvAePFTwfX09ARDX1mSs=";
+    aarch64sha256 = "6RKaSIDj0zOqjYAGb77q3+sKqmp+VFqcgODyU06ajrc=";
   } { };
 
   sublime4-dev = common {
