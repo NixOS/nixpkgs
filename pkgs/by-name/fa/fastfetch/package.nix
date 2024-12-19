f170d0e047fe25d4eab30d15a78577709d421f79
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-12-19 19:31:41 +0530

    fastfetch: 2.31.0 → 2.32.1

--- pkgs/by-name/fa/fastfetch/package.nix
+++ pkgs/by-name/fa/fastfetch/package.nix
@@ -44,13 +44,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "2.31.0";
+  version = "2.32.1";
 
   src = fetchFromGitHub {
     owner = "fastfetch-cli";
     repo = "fastfetch";
     rev = finalAttrs.version;
-    hash = "sha256-CXJbNaCZXt5DJaCRjbPoSo3rfhrOLiMkOEQU0Icdggk=";
+    hash = "sha256-EfnHndj7WPXkL7wxGzeigJ7iE2WdgUf74sPDQhob+Ho=";
   };
 
   outputs = [
