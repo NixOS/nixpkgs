eb43c10b09aa53c6cc66383bca97065bf1799bce
tropf <tropf@users.noreply.github.com>
2024-12-19 14:36:43 +0100

    inkscape-with-extensions: actually use all extensions by default
    The mechanism to include all extensions by default in
    inkscape-with-extensions relied on [] == null, which is false. This
    comparison is fixed, now including all extensions by default.

--- pkgs/applications/graphics/inkscape/with-extensions.nix
+++ pkgs/applications/graphics/inkscape/with-extensions.nix
@@ -11,7 +11,7 @@ let
   allExtensions = lib.filter (pkg: lib.isDerivation pkg && !pkg.meta.broken or false) (
     lib.attrValues inkscape-extensions
   );
-  selectedExtensions = if inkscapeExtensions == null then allExtensions else inkscapeExtensions;
+  selectedExtensions = if inkscapeExtensions == [ ] then allExtensions else inkscapeExtensions;
 in
 
 symlinkJoin {
