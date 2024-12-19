b74d17a2318dbd41dd79ff84196925c787210ccd
Doron Behar <doron.behar@gmail.com>
2024-12-20 00:23:42 +0200

    python312Packages.pickpack: init at 2.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10338,6 +10338,8 @@ self: super: with self; {
 
   pick = callPackage ../development/python-modules/pick { };
 
+  pickpack = callPackage ../development/python-modules/pickpack { };
+
   pickleshare = callPackage ../development/python-modules/pickleshare { };
 
   picobox = callPackage ../development/python-modules/picobox { };
