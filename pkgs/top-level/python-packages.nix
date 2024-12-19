270708871c752e579526af3ec09aa4137a181df1
Doron Behar <doron.behar@gmail.com>
2024-12-19 11:05:25 +0200

    python312Packages.beaupy: init at 3.10.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1595,6 +1595,8 @@ self: super: with self; {
 
   beartype = callPackage ../development/python-modules/beartype { };
 
+  beaupy = callPackage ../development/python-modules/beaupy { };
+
   beautiful-date = callPackage ../development/python-modules/beautiful-date { };
 
   beautifulsoup4 = callPackage ../development/python-modules/beautifulsoup4 { };
