09ce500ce85d0fbe387b63617dc2e7deafba483d
IceDBorn <git.outsider841@simplelogin.fr>
2024-12-19 11:19:52 +0200

    maintainers: add icedborn

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9459,6 +9459,12 @@
     githubId = 3661115;
     name = "Ingo Blechschmidt";
   };
+  icedborn = {
+    email = "github.envenomed@dralias.com";
+    github = "icedborn";
+    githubId = 51162078;
+    name = "IceDBorn";
+  };
   icewind1991 = {
     name = "Robin Appelman";
     email = "robin@icewind.nl";
