53953bbde55d5b8c4a495cf3a6d2de8332024768
Gaetan Lepage <gaetan@glepage.com>
2024-12-19 10:58:30 +0100

    softmaker-office: 2021.1032 -> 2024.1222

--- pkgs/applications/office/softmaker/softmaker_office.nix
+++ pkgs/applications/office/softmaker/softmaker_office.nix
@@ -7,9 +7,9 @@
   # Softmaker Office or when the upstream archive was replaced and
   # nixpkgs is not in sync yet.
   officeVersion ? {
-    version = "1032";
-    edition = "2021";
-    hash = "sha256-LchSqLVBdkmWJQ8hCEvtwRPgIUSDE0URKPzCkEexGbc=";
+    version = "1222";
+    edition = "2024";
+    hash = "sha256-eyYBK5ZxPcBakOvXUQZIU2aftyH6PXh/rtqC/1BJhg4=";
   },
 
   ...
