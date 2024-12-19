b4b5794f924fd300512348df7f876a5d7ef99a38
Charles Hovine <hovinecharles@gmail.com>
2024-12-19 16:18:42 +0100

    python312Packages.certbot-dns-linode: init at 2.11.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8823,6 +8823,7 @@ with pkgs;
 
   certbot-full = certbot.withPlugins (cp: with cp; [
     certbot-dns-cloudflare
+    certbot-dns-linode
     certbot-dns-google
     certbot-dns-ovh
     certbot-dns-rfc2136
