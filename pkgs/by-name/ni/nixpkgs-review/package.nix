8defbfa014800c0872e6a343f700395087f01e73
Gaetan Lepage <gaetan@glepage.com>
2024-12-19 17:15:08 +0100

    nixpkgs-review: 2.12.0 -> 3.0.0
    Diff: https://github.com/Mic92/nixpkgs-review/compare/refs/tags/2.12.0...3.0.0
    
    Changelog: https://github.com/Mic92/nixpkgs-review/releases/tag/3.0.0
    (cherry picked from commit 170a250e10d47c77eb32ba3f5f8afb79977a272a)

--- pkgs/by-name/ni/nixpkgs-review/package.nix
+++ pkgs/by-name/ni/nixpkgs-review/package.nix
@@ -18,14 +18,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "nixpkgs-review";
-  version = "2.12.0";
+  version = "3.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = "nixpkgs-review";
-    rev = "refs/tags/${version}";
-    hash = "sha256-yNdBqL3tceuoUHx8/j2y5ZTq1zeVDAm37RZtlCbC6rg=";
+    tag = version;
+    hash = "sha256-C2JAlCgH5OCMAZr/Rvi0H7xAwesnkVCJ3kZyyVYaLg4=";
   };
 
   build-system = [
@@ -76,7 +76,7 @@ python3Packages.buildPythonApplication rec {
   versionCheckProgramArg = [ "--version" ];
 
   meta = {
-    changelog = "https://github.com/Mic92/nixpkgs-review/releases/tag/${version}";
+    changelog = "https://github.com/Mic92/nixpkgs-review/releases/tag/${src.tag}";
     description = "Review pull-requests on https://github.com/NixOS/nixpkgs";
     homepage = "https://github.com/Mic92/nixpkgs-review";
     license = lib.licenses.mit;
