c08a669a5ab6854edb9d971aed0ed64d2772a47c
Rishik Yalamanchili <73787402+Rishik-Y@users.noreply.github.com>
2024-12-19 08:12:29 +0530

    Update Bottles-unwrapped (For NixOS 23.05) v51.05 → 51.11

--- pkgs/applications/misc/bottles/default.nix
+++ pkgs/applications/misc/bottles/default.nix
@@ -1,51 +1,45 @@
-{ lib
-, fetchFromGitHub
-, fetchpatch
-, gitUpdater
-, python3Packages
-, blueprint-compiler
-, meson
-, ninja
-, pkg-config
-, wrapGAppsHook4
-, appstream-glib
-, desktop-file-utils
-, librsvg
-, gtk4
-, gtksourceview5
-, libadwaita
-, cabextract
-, p7zip
-, xdpyinfo
-, imagemagick
-, lsb-release
-, pciutils
-, procps
-, gamescope
-, mangohud
-, vkbasalt-cli
-, vmtouch
+{
+  lib,
+  fetchFromGitHub,
+  gitUpdater,
+  python3Packages,
+  blueprint-compiler,
+  meson,
+  ninja,
+  pkg-config,
+  wrapGAppsHook4,
+  appstream-glib,
+  desktop-file-utils,
+  librsvg,
+  gtk4,
+  gtksourceview5,
+  libadwaita,
+  cabextract,
+  p7zip,
+  xdpyinfo,
+  imagemagick,
+  lsb-release,
+  pciutils,
+  procps,
+  gamescope,
+  mangohud,
+  vkbasalt-cli,
+  vmtouch,
 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "bottles-unwrapped";
-  version = "51.5";
+  version = "51.11";
 
   src = fetchFromGitHub {
     owner = "bottlesdevs";
     repo = "bottles";
     rev = version;
-    sha256 = "sha256-8VF/CD0Wu2eV6wOpj/M6peKDthFWlcg+1NzzTSIH4S8=";
+    sha256 = "sha256-uS3xmTu+LrVFX93bYcJvYjl6179d3IjpxLKrOXn8Z8Y=";
   };
 
   patches = [
     ./vulkan_icd.patch
-
-    # Remove next version
-    (fetchpatch {
-      url = "https://github.com/bottlesdevs/Bottles/commit/7cb284f9bac0b71bf632bfc70d94f7a53bc51267.patch";
-      hash = "sha256-mRF+BtQ0qM7Yvx7SONeH2wc04F87fEyNRlBuyQrzN8Y=";
-    })
   ];
 
   # https://github.com/bottlesdevs/Bottles/wiki/Packaging
@@ -67,38 +61,41 @@ python3Packages.buildPythonApplication rec {
     libadwaita
   ];
 
-  propagatedBuildInputs = with python3Packages; [
-    pathvalidate
-    pycurl
-    pyyaml
-    requests
-    pygobject3
-    patool
-    markdown
-    fvs
-    pefile
-    urllib3
-    chardet
-    certifi
-    idna
-    orjson
-    icoextract
-  ] ++ [
-    cabextract
-    p7zip
-    xdpyinfo
-    imagemagick
-    vkbasalt-cli
+  propagatedBuildInputs =
+    with python3Packages;
+    [
+      pathvalidate
+      pycurl
+      pyyaml
+      requests
+      pygobject3
+      patool
+      markdown
+      fvs
+      pefile
+      urllib3
+      chardet
+      certifi
+      idna
+      orjson
+      icoextract
+    ]
+    ++ [
+      cabextract
+      p7zip
+      xdpyinfo
+      imagemagick
+      vkbasalt-cli
 
-    gamescope
-    mangohud
-    vmtouch
+      gamescope
+      mangohud
+      vmtouch
 
-    # Undocumented (subprocess.Popen())
-    lsb-release
-    pciutils
-    procps
-  ];
+      # Undocumented (subprocess.Popen())
+      lsb-release
+      pciutils
+      procps
+    ];
 
   format = "other";
   dontWrapGApps = true; # prevent double wrapping
@@ -114,7 +111,11 @@ python3Packages.buildPythonApplication rec {
     homepage = "https://usebottles.com/";
     downloadPage = "https://github.com/bottlesdevs/Bottles/releases";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ psydvl shamilton ];
+    maintainers = with maintainers; [
+      psydvl
+      shamilton
+    ];
     platforms = platforms.linux;
+    mainProgram = "bottles";
   };
 }
