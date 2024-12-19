NixOS/nixpkgs

commit ebd18cf115cc212d9780092e9207e19b800f67a0

treewide: replace substituteAll with replaceVars (#1)
Driving #237216 forward.

pkgs/misc/my-env/default.nix
@@ -58,7 +58,7 @@
 
 {
   mkDerivation,
-  substituteAll,
+  replaceVars,
   pkgs,
 }:
 {
@@ -80,8 +80,7 @@ mkDerivation {
     "buildPhase"
     "fixupPhase"
   ];
-  setupNew = substituteAll {
-    src = ../../stdenv/generic/setup.sh;
+  setupNew = replaceVars ../../stdenv/generic/setup.sh {
     inherit gcc;
   };
 

pkgs/applications/audio/easyabc/default.nix
@@ -2,7 +2,7 @@
   lib,
   fetchFromGitHub,
   fetchPypi,
-  substituteAll,
+  replaceVars,
   python39,
   fluidsynth,
   soundfont-fluid,
@@ -58,8 +58,7 @@ python.pkgs.buildPythonApplication {
   strictDeps = false;
 
   patches = [
-    (substituteAll {
-      src = ./hardcoded-paths.patch;
+    (replaceVars ./hardcoded-paths.patch {
       fluidsynth = "${fluidsynth}/lib/libfluidsynth.so";
       soundfont = "${soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2";
       ghostscript = "${ghostscript}/bin/gs";

pkgs/applications/editors/emacs/elisp-packages/manual-packages/lsp-bridge/default.nix
@@ -3,7 +3,7 @@
   python3,
   melpaBuild,
   fetchFromGitHub,
-  substituteAll,
+  replaceVars,
   acm,
   markdown-mode,
   basedpyright,
@@ -42,8 +42,7 @@ melpaBuild {
   patches = [
     # Hardcode the python dependencies needed for lsp-bridge, so users
     # don't have to modify their global environment
-    (substituteAll {
-      src = ./hardcode-dependencies.patch;
+    (replaceVars ./hardcode-dependencies.patch {
       python = python.interpreter;
     })
   ];

pkgs/applications/graphics/leocad/default.nix
@@ -6,7 +6,7 @@
   povray,
   qmake,
   qttools,
-  substituteAll,
+  replaceVars,
   zlib,
 }:
 
@@ -43,8 +43,7 @@ mkDerivation rec {
   propagatedBuildInputs = [ povray ];
 
   patches = [
-    (substituteAll {
-      src = ./povray.patch;
+    (replaceVars ./povray.patch {
       inherit povray;
     })
   ];

pkgs/applications/graphics/qvge/default.nix
@@ -2,7 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  substituteAll,
+  replaceVars,
   wrapQtAppsHook,
   qmake,
   qtsvg,
@@ -24,8 +24,7 @@ stdenv.mkDerivation rec {
   sourceRoot = "${src.name}/src";
 
   patches = (
-    substituteAll {
-      src = ./set-graphviz-path.patch;
+    replaceVars ./set-graphviz-path.patch {
       inherit graphviz;
     }
   );

pkgs/applications/kde/kdenlive/default.nix
@@ -1,6 +1,6 @@
 {
   mkDerivation,
-  substituteAll,
+  replaceVars,
   lib,
   extra-cmake-modules,
   breeze-icons,
@@ -89,8 +89,7 @@ mkDerivation {
   # doesn't find them. See:
   # https://github.com/NixOS/nixpkgs/issues/83885
   patches = [
-    (substituteAll {
-      src = ./dependency-paths.patch;
+    (replaceVars ./dependency-paths.patch {
       inherit mediainfo glaxnimate;
       ffmpeg = ffmpeg-full;
       mlt = mlt-full;

pkgs/applications/misc/dmensamenu/default.nix
@@ -2,7 +2,7 @@
   lib,
   buildPythonApplication,
   fetchFromGitHub,
-  substituteAll,
+  replaceVars,
   requests,
   dmenu,
 }:
@@ -19,8 +19,7 @@ buildPythonApplication rec {
   };
 
   patches = [
-    (substituteAll {
-      src = ./dmenu-path.patch;
+    (replaceVars ./dmenu-path.patch {
       inherit dmenu;
     })
   ];

pkgs/applications/misc/inochi2d/default.nix
@@ -2,7 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  substituteAll,
+  replaceVars,
   callPackage,
 }:
 
@@ -35,8 +35,7 @@ in
 
     patches = [
       # Upstream asks that we change the bug tracker URL to not point to the upsteam bug tracker
-      (substituteAll {
-        src = ./support-url.patch;
+      (replaceVars ./support-url.patch {
         assignees = "TomaSajt"; # should be a comma separated list of the github usernames of the maintainers
       })
       # Change how duplicate locales differentiate themselves (the store paths were too long)

commit a07222588d06932e5a6d6f276c00b4fe59366c3c

myEnvFun: fix build
This broke in ebd18cf.

replaceVars is not needed on setup.sh since 15103e5e5.

pkgs/misc/my-env/default.nix
@@ -66,7 +66,6 @@
   name,
   buildInputs ? [ ],
   propagatedBuildInputs ? [ ],
-  gcc ? stdenv.cc,
   extraCmds ? "",
   cleanupCmds ? "",
   shell ? "${pkgs.bashInteractive}/bin/bash --norc",
@@ -80,9 +79,7 @@ mkDerivation {
     "buildPhase"
     "fixupPhase"
   ];
-  setupNew = replaceVars ../../stdenv/generic/setup.sh {
-    inherit gcc;
-  };
+  setupNew = ../../stdenv/generic/setup.sh;
 
   buildPhase =
     let