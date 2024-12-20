2198eca7d87235ce2b2de8b5333206c9c0fb7fd9
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-20 07:00:23 +0000

    buf: 1.47.2 -> 1.48.0

--- pkgs/by-name/bu/buf/package.nix
+++ pkgs/by-name/bu/buf/package.nix
@@ -10,16 +10,16 @@
 
 buildGoModule rec {
   pname = "buf";
-  version = "1.47.2";
+  version = "1.48.0";
 
   src = fetchFromGitHub {
     owner = "bufbuild";
     repo = "buf";
     rev = "v${version}";
-    hash = "sha256-rEz39E6sid9nNV5lzXwLh/GHrz5aVl67+BKfooH3+Nk=";
+    hash = "sha256-F1ZmhVAjm8KVFePXLeOnyvh1TvjXBDCwUizwQSpp6L4=";
   };
 
-  vendorHash = "sha256-dSNmMc7iWtrItFuz341FpYWo5AyExgO7e7Fbib2gCS8=";
+  vendorHash = "sha256-M5q93hJjEsdMG4N+bjHTTUqBLgy2b7oIRmkizuGxeoE=";
 
   patches = [
     # Skip a test that requires networking to be available to work.
