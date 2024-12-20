2307cca54548be438a0e043ec8cba991c1b1b61f
Aaron Jheng <wentworth@outlook.com>
2024-12-20 12:08:01 +0000

    pixi: 0.38.0 -> 0.39.4

--- pkgs/by-name/pi/pixi/package.nix
+++ pkgs/by-name/pi/pixi/package.nix
@@ -13,30 +13,22 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pixi";
-  version = "0.38.0";
+  version = "0.39.4";
 
   src = fetchFromGitHub {
     owner = "prefix-dev";
     repo = "pixi";
-    rev = "v${version}";
-    hash = "sha256-F15GDw6zolaa4IheKyJ9kdmdiLazUiDUhnUM8gH/hgk=";
+    tag = "v${version}";
+    hash = "sha256-GOjJYRONOZqoJxv3Lqz8UthruAx5P+Dpg7LWT1eLBII=";
   };
 
-  postPatch = ''
+  cargoPatches = [
     # There are multiple `version-ranges` entries which is not supported by buildRustPackage.
-    cp -f ${./Cargo.lock} Cargo.lock
-  '';
+    ./Cargo.lock.patch
+  ];
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "async_zip-0.0.17" = "sha256-3k9rc4yHWhqsCUJ17K55F8aQoCKdVamrWAn6IDWo3Ss=";
-      "pubgrub-0.2.1" = "sha256-8TrOQ6fYJrYgFNuqiqnGztnHOqFIEDi2MFZEBA+oks4=";
-      "reqwest-middleware-0.3.3" = "sha256-KjyXB65a7SAfwmxokH2PQFFcJc6io0xuIBQ/yZELJzM=";
-      "tl-0.7.8" = "sha256-F06zVeSZA4adT6AzLzz1i9uxpI1b8P1h+05fFfjm3GQ=";
-      "uv-auth-0.0.1" = "sha256-xy/fgy3+YvSdfq5ngPVbAmRpYyJH27Cft5QxBwFQumU=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-WbkSjpdjOztW/979yPBjh4BGbQbzOJUZAZcUeHoAJ4w=";
 
   nativeBuildInputs = [
     pkg-config
