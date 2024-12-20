785ec6b49ad9b9448579828e32d46a8e0289c0c7
seth <getchoo@tuta.io>
2024-12-20 15:00:03 -0500

    zlib: use default configure script on windows
    This avoids the pitfalls of win32/Makefile.gcc (which prevents building
    on compilers other than gcc without patching and has non-standard
    installation behavior) and fixes cross compilation for ucrtAarch64
    
    Putting the DLL's in `$out/bin` was given the approval of:
    
    John Ericson <John.Ericson@Obsidian.Systems>

--- pkgs/development/libraries/zlib/default.nix
+++ pkgs/development/libraries/zlib/default.nix
@@ -49,6 +49,8 @@ stdenv.mkDerivation (finalAttrs: {
   # https://github.com/madler/zlib/pull/1171
   patches = [
     ./export-variable.patch
+    # https://github.com/madler/zlib/pull/1296
+    ./mingw-shared.patch
   ];
 
   postPatch = ''
@@ -69,15 +71,13 @@ stdenv.mkDerivation (finalAttrs: {
   setOutputFlags = false;
   outputDoc = "dev"; # single tiny man3 page
 
-  dontConfigure = (stdenv.hostPlatform.isMinGW || stdenv.hostPlatform.isCygwin);
-
   preConfigure = lib.optionalString (stdenv.hostPlatform != stdenv.buildPlatform) ''
     export CHOST=${stdenv.hostPlatform.config}
   '';
 
   configureFlags = [
     "--includedir=${placeholder "dev"}/include"
-    "--sharedlibdir=${placeholder "out"}/lib"
+    "--sharedlibdir=${placeholder "out"}/${if stdenv.hostPlatform.isWindows then "bin" else "lib"}"
     "--libdir=${placeholder (if splitStaticOutput then "static" else "out")}/lib"
     # See comment near splitStaticOutput argument
     (lib.enableFeature shared "shared")
@@ -101,11 +101,6 @@ stdenv.mkDerivation (finalAttrs: {
       for file in $out/lib/*.so* $out/lib/*.dylib* ; do
         ${stdenv.cc.bintools.targetPrefix}install_name_tool -id "$file" $file
       done
-    ''
-    # Non-typical naming confuses libtool which then refuses to use zlib's DLL
-    # in some cases, e.g. when compiling libpng.
-    + lib.optionalString (stdenv.hostPlatform.isMinGW && shared) ''
-      ln -s zlib1.dll $out/bin/libz.dll
     '';
 
   env = lib.optionalAttrs (!stdenv.hostPlatform.isDarwin) {
@@ -134,18 +129,9 @@ stdenv.mkDerivation (finalAttrs: {
     "PREFIX=${stdenv.cc.targetPrefix}"
     "pkgconfigdir=${placeholder "dev"}/share/pkgconfig"
   ]
-  ++ lib.optionals (stdenv.hostPlatform.isMinGW || stdenv.hostPlatform.isCygwin) [
-    "-f"
-    "win32/Makefile.gcc"
-  ]
   ++ lib.optionals stdenv.hostPlatform.isCygwin [
     "SHAREDLIB=cygz.dll"
     "IMPLIB=libz.dll.a"
-  ]
-  ++ lib.optionals shared [
-    # Note that as of writing (zlib 1.2.11), this flag only has an effect
-    # for Windows as it is specific to `win32/Makefile.gcc`.
-    "SHARED_MODE=1"
   ];
 
   passthru.tests = {
