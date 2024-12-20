effaa4b3b8f0e5cce17aaa2afef7dadefc08c99a
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-20 00:33:50 +0000

    uiua-unstable: 0.14.0-rc.2 -> 0.14.0-rc.3

--- pkgs/by-name/ui/uiua/unstable.nix
+++ pkgs/by-name/ui/uiua/unstable.nix
@@ -1,7 +1,7 @@
 rec {
-  version = "0.14.0-rc.2";
+  version = "0.14.0-rc.3";
   rev = version;
-  hash = "sha256-8jYoZpzepgqeM3EsyJguoU6LVPE3kywbE20Nz/8/fTQ=";
-  cargoHash = "sha256-5narrhVxM2Yl6hZIfFijxST3jfy5q0ox3Y7v+2VggUE=";
+  hash = "sha256-znHHohWMoVttubvYx5h3b7pQcQVYhn3nZLFoHrJsits=";
+  cargoHash = "sha256-d+N5ij5zbQRZwkLtwLAM7BY82G/tNsdkUb6/7wnD51g=";
   updateScript = ./update-unstable.sh;
 }
