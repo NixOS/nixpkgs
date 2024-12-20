e864bcec29b217d3a723b006702cc009530694d3
K900 <me@0upti.me>
2024-12-20 10:34:52 +0300

    mesa: 24.3.1 -> 24.3.2
    Diff: https://gitlab.freedesktop.org/mesa/mesa/-/compare/mesa-24.3.1...mesa-24.3.2
    
    Changelog: https://docs.mesa3d.org/relnotes/24.3.2.html

--- pkgs/development/libraries/mesa/common.nix
+++ pkgs/development/libraries/mesa/common.nix
@@ -5,14 +5,14 @@
 # nix build .#legacyPackages.x86_64-darwin.mesa .#legacyPackages.aarch64-darwin.mesa
 rec {
   pname = "mesa";
-  version = "24.3.1";
+  version = "24.3.2";
 
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "mesa";
     repo = "mesa";
     rev = "mesa-${version}";
-    hash = "sha256-+UyeS2KkVexYdHj9WW8qVE8GLXyQn8G3ZW/Htog/m3I=";
+    hash = "sha256-6EcSOE73wEz+aS4C+GUVfcbJtGB0MvIL4a6zA1ohVGA=";
   };
 
   meta = {
