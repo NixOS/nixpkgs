737936028e41c24411233a7674a6a8564fd0c508
Justin Wood <me@ankhers.dev>
2024-12-20 07:46:31 -0500

    Fix link to OTP compatibility matrix
    Co-authored-by: Filip Vavera <github@sgiath.dev>

--- pkgs/development/interpreters/elixir/1.18.nix
+++ pkgs/development/interpreters/elixir/1.18.nix
@@ -2,7 +2,7 @@
 mkDerivation {
   version = "1.18.0";
   sha256 = "sha256-fT3J8h2uuJ+dSR58kwlUkN023yFlmTwq2/O12KbjJc4=";
-  # https://hexdocs.pm/elixir/1.17.3/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
+  # https://hexdocs.pm/elixir/1.18.0/compatibility-and-deprecations.html#between-elixir-and-erlang-otp
   minimumOTPVersion = "25";
   escriptPath = "lib/elixir/scripts/generate_app.escript";
 }
