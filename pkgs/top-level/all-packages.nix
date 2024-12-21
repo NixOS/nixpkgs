5240d1f2e3620bba5ddff347c2cbda92203f8394
Ilya Epifanov <mail@ilya.network>
2024-12-21 10:49:37 +0100

    inav-configurator: moved to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17877,7 +17877,7 @@ with pkgs;
 
   apmplanner2 = libsForQt5.callPackage ../applications/science/robotics/apmplanner2 { };
 
-  inav-configurator = callPackage ../applications/science/robotics/inav-configurator {
+  inav-configurator = callPackage ../by-name/in/inav-configurator/package.nix {
     version = "5.1.0";
     hash = "sha256-ZvZxQICa5fnJBTx0aW/hqQCuhQW9MkcVa2sOjPYaPXM=";
     packed = true;
