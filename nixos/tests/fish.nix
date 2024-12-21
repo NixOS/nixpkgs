71b6fd8e8e0851820e17482e5f9a9cca3164c8cd
r-vdp <ramses@well-founded.dev>
2024-12-21 22:27:11 +0200

    fish: use runTest for the nixos test

--- nixos/tests/fish.nix
+++ nixos/tests/fish.nix
@@ -1,20 +1,20 @@
-import ./make-test-python.nix (
-  { pkgs, ... }:
-  {
-    name = "fish";
+{
+  name = "fish";
 
-    nodes.machine =
-      { pkgs, ... }:
+  nodes.machine =
+    { pkgs, ... }:
 
-      {
-        programs.fish.enable = true;
-        environment.systemPackages = with pkgs; [
-          coreutils
-          procps # kill collides with coreutils' to test https://github.com/NixOS/nixpkgs/issues/56432
-        ];
-      };
+    {
+      programs.fish.enable = true;
+      environment.systemPackages = with pkgs; [
+        coreutils
+        procps # kill collides with coreutils' to test https://github.com/NixOS/nixpkgs/issues/56432
+      ];
+    };
 
-    testScript = ''
+  testScript =
+    #python
+    ''
       start_all()
       machine.wait_for_file("/etc/fish/generated_completions/coreutils.fish")
       machine.wait_for_file("/etc/fish/generated_completions/kill.fish")
@@ -22,5 +22,4 @@ import ./make-test-python.nix (
           "fish -ic 'echo $fish_complete_path' | grep -q '/share/fish/completions /etc/fish/generated_completions /root/.local/share/fish/generated_completions$'"
       )
     '';
-  }
-)
+}
