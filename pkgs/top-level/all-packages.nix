f8a17e5772370e0c68d42e43c3dc4c88d9cd6a7a
Ilya Epifanov <mail@ilya.network>
2024-12-21 11:44:30 +0100

    betaflight-configurator_10_9: init at 10.9.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18303,6 +18303,10 @@ with pkgs;
     version = "10.10.0";
     sha256 = "sha256-UB5Vr5wyCUZbOaQNckJQ1tAXwh8VSLNI1IgTiJzxV08";
   };
+  betaflight-configurator10_9 = callPackage ../by-name/be/betaflight-configurator/package.nix {
+    version = "10.9.0";
+    sha256 = "0lkf7llbcw2l7r28g3djwgblb91k0685hlvvbiryvnhi2b4cqp7l";
+  };
 
   ### MISC
 
