d0df3b9fedeafbc8523d065b2370fdf772ab6476
nikolaizombie1 <fabio.matos999@gmail.com>
2024-12-21 18:33:52 -0500

    maintainers: add nikolaizombie1

--- pkgs/by-name/co/color-scheme-generator/package.nix
+++ pkgs/by-name/co/color-scheme-generator/package.nix
@@ -5,6 +5,7 @@
   openssl,
   sqlite,
   pkg-config,
+  nix-update-script,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -30,15 +31,18 @@ rustPlatform.buildRustPackage rec {
     OPENSSL_NO_VENDOR = 1;
   };
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
-    description = "Quickly generate color schemes for waybar from an image.";
+    description = "Image-based color scheme generator for Waybar";
     longDescription = ''
       color_scheme_generator is a command line utility used to analyze images and generate color themes from them given a path to an image.
       This command line utility behaves like a standard UNIX utility where the path to the image can be either piped in or sent a command line argument.
       The intended purpose of this application is to automatically create color themes for Waybar, but it can be used for the bar in AwesomeWM or other applications to theme based on the on an image. This utility has a cache for the image analysis. This means that once an image has been analyzed once, the result will be saved in the cache and when an image is analyzed again, the results will be returned instantly
     '';
     homepage = "https://github.com/nikolaizombie1/color_scheme_generator";
-    license = with lib.licenses; [ gpl3 ];
-    maintainers = with lib.maintainers; [ nikolaizombie1 ];
+    changelog = "https://github.com/nikolaizombie1/color_scheme_generator/releases/tag/v${version}";
+    license = lib.licenses.gpl3Plus;
+    maintainers = [ lib.maintainers.nikolaizombie1 ];
   };
 }
