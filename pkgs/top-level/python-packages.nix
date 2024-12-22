a38200136670b4f995bc3577b557528b11320a40
PopeRigby <poperigby@mailbox.org>
2024-12-22 15:37:31 -0800

    python3Packages.chime: init at 0.7-unstable-2023-03-06

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2272,6 +2272,8 @@ self: super: with self; {
 
   chex = callPackage ../development/python-modules/chex { };
 
+  chime = callPackage ../development/python-modules/chime { };
+
   chirpstack-api = callPackage ../development/python-modules/chirpstack-api { };
 
   chispa = callPackage ../development/python-modules/chispa { };
