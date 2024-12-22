43debdea55a62230ac7d5b0da940d09c043ee928
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-22 10:35:39 +0000

    rocmPackages.hip-common: 6.0.2 -> 6.3.1

--- pkgs/development/rocm-modules/6/hip-common/default.nix
+++ pkgs/development/rocm-modules/6/hip-common/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "hip-common";
-  version = "6.0.2";
+  version = "6.3.1";
 
   src = fetchFromGitHub {
     owner = "ROCm";
     repo = "HIP";
     rev = "rocm-${finalAttrs.version}";
-    hash = "sha256-51u3By0R4LKoWiklNacFP6HILL845jxpN6FD7rQB+zQ=";
+    hash = "sha256-y85S2fULvbQfwxZukIsMLuQAqWEv1kHL8fdozK4kj5I=";
   };
 
   dontConfigure = true;
