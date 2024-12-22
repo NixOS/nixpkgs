ba828700e58af49345a799f57a4e7fa995d7359e
Artemis Tosini <me@artem.ist>
2024-12-22 21:47:34 +0000

    openbsd.getty: Fix paths

--- pkgs/os-specific/bsd/openbsd/pkgs/getty/package.nix
+++ pkgs/os-specific/bsd/openbsd/pkgs/getty/package.nix
@@ -1,6 +1,13 @@
 {
   mkDerivation,
+  login,
 }:
 mkDerivation {
   path = "libexec/getty";
+
+  postPatch = ''
+    substituteInPlace $BSDSRCDIR/libexec/getty/pathnames.h \
+        --replace-fail "/usr/libexec/getty" "$out/bin/getty" \
+        --replace-fail "/usr/bin/login" "${login}/bin/login"
+  '';
 }
