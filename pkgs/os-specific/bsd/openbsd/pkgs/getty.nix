a4c34314db31c94272382c92d9b1d747f04a1780
Artemis Tosini <me@artem.ist>
2024-12-22 21:58:42 +0000

    openbsd.getty: copy out gettytab

--- pkgs/os-specific/bsd/openbsd/pkgs/getty.nix
+++ pkgs/os-specific/bsd/openbsd/pkgs/getty.nix
@@ -4,10 +4,16 @@
 }:
 mkDerivation {
   path = "libexec/getty";
+  extraPaths = [ "etc/gettytab" ];
 
   postPatch = ''
     substituteInPlace $BSDSRCDIR/libexec/getty/pathnames.h \
         --replace-fail "/usr/libexec/getty" "$out/bin/getty" \
         --replace-fail "/usr/bin/login" "${login}/bin/login"
+  '';
+
+  postInstall = ''
+    mkdir -p $out/etc
+    cp $BSDSRCDIR/etc/gettytab $out/etc/gettytab
   '';
 }
