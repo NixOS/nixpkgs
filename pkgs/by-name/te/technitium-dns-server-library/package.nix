a01bf4e56c80f7ba556f35f5c02163e176a779d2
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-22 06:50:21 +0000

    technitium-dns-server-library: dns-server-v13.2 -> dns-server-v13.3

--- pkgs/by-name/te/technitium-dns-server-library/package.nix
+++ pkgs/by-name/te/technitium-dns-server-library/package.nix
@@ -7,13 +7,13 @@
 }:
 buildDotnetModule rec {
   pname = "technitium-dns-server-library";
-  version = "dns-server-v13.2";
+  version = "dns-server-v13.3";
 
   src = fetchFromGitHub {
     owner = "TechnitiumSoftware";
     repo = "TechnitiumLibrary";
     rev = "refs/tags/${version}";
-    hash = "sha256-stfxYe0flE1daPuXw/GAgY52ZD7pkqnBIBvmSVPWWjI=";
+    hash = "sha256-SyCeLiowbqqABdhQTs/u3R0ogzojLZZLr75Kr3YtCdE=";
     name = "${pname}-${version}";
   };
 
