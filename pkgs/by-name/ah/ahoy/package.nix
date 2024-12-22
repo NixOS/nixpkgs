af1cebee40cac198dfca5d75c31b2f5872f76959
darwincereska <157426553+darwincereska@users.noreply.github.com>
2024-12-21 19:55:15 -0500

    Formatted

--- pkgs/by-name/ah/ahoy/package.nix
+++ pkgs/by-name/ah/ahoy/package.nix
@@ -1,13 +1,13 @@
 {
- lib,
- fetchFromGitHub,
- buildGoModule,
- go,
- imagemagick
+  lib,
+  fetchFromGitHub,
+  buildGoModule,
+  go,
+  imagemagick,
 }:
-buildGoModule rec{
-  pname="ahoy";
-  version="2.2.0";
+buildGoModule rec {
+  pname = "ahoy";
+  version = "2.2.0";
   src = fetchFromGitHub {
     owner = "ahoy-cli";
     repo = "ahoy";
@@ -15,28 +15,18 @@ buildGoModule rec{
     hash = "sha256-xwjfY9HudxVz3xEEyRPtWysbojtan56ABBL3KgG0J/8=";
   };
   vendorHash = null;
-  outputs = ["out"];
-  ldflags = ["-s -w -X main.version=${version} -X main.GitCommit= -X main.GitBranch= -X main.BuildTime="];
-  nativeBuildInputs = [go];
-  buildInputs = [imagemagick];
-  # buildPhase = ''
-  #   mkdir -p build
-  #   go build -ldflags "$LDFLAGS" -v -o build/ahoy
-  # '';
-  # installPhase = ''
-  #   mkdir -p $out/bin
-  #   chmod +x build/ahoy
-  #   cp build/ahoy $out/bin
-  # '';
-  # postInstall = ''
-  #     install -D $out/bin/ahoy $out/bin/ahoy
-  # '';
+  outputs = [ "out" ];
+  ldflags = [
+    "-s -w -X main.version=${version} -X main.GitCommit= -X main.GitBranch= -X main.BuildTime="
+  ];
+  nativeBuildInputs = [ go ];
+  buildInputs = [ imagemagick ];
   meta = {
     description = "Create self-documenting cli programs from YAML files. Easily wrap bash, grunt, npm, docker, (anything) to standardize your processes and make the lives of the people working on your project better.";
     homepage = "https://github.com/ahoy-cli/ahoy";
     changelog = "https://github.com/ahoy-cli/ahoy/releases/tag/${version}";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [darwincereska];
+    maintainers = with lib.maintainers; [ darwincereska ];
     platforms = lib.platforms.linux;
     mainProgram = "ahoy";
   };
