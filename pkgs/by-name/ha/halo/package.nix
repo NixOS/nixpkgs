7d68f2cfad486bdbd0fdd7386aeee3f093015beb
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-24 09:07:48 +0000

    halo: 2.20.11 -> 2.20.12

--- pkgs/by-name/ha/halo/package.nix
+++ pkgs/by-name/ha/halo/package.nix
@@ -8,10 +8,10 @@
 }:
 stdenv.mkDerivation rec {
   pname = "halo";
-  version = "2.20.11";
+  version = "2.20.12";
   src = fetchurl {
     url = "https://github.com/halo-dev/halo/releases/download/v${version}/halo-${version}.jar";
-    hash = "sha256-4Y89yMR6rVREA1V1Xm8WAGeBKovxmy0/XPzM9CKluzE=";
+    hash = "sha256-BixJ36qlUnifIK7xWCZeY2nOGBAy2p6zL2CQvlgSMj8=";
   };
 
   nativeBuildInputs = [
