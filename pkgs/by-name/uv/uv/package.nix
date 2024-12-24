023eba06340a7e994253887e5207dfb8f35a3cc3
Gaetan Lepage <gaetan@glepage.com>
2024-12-24 09:46:26 +0100

    uv: use stdenv.hostPlatform.emulator to generate the shell completion unconditionally

--- pkgs/by-name/uv/uv/package.nix
+++ pkgs/by-name/uv/uv/package.nix
@@ -1,11 +1,15 @@
 {
   lib,
   stdenv,
-  cmake,
+  rustPlatform,
   fetchFromGitHub,
+
+  # nativeBuildInputs
+  cmake,
   installShellFiles,
   pkg-config,
-  rustPlatform,
+
+  buildPackages,
   versionCheckHook,
   python3Packages,
   nix-update-script,
@@ -41,13 +45,16 @@ rustPlatform.buildRustPackage rec {
   # Tests require python3
   doCheck = false;
 
-  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    export HOME=$TMPDIR
-    installShellCompletion --cmd uv \
-      --bash <($out/bin/uv --generate-shell-completion bash) \
-      --fish <($out/bin/uv --generate-shell-completion fish) \
-      --zsh <($out/bin/uv --generate-shell-completion zsh)
-  '';
+  postInstall =
+    let
+      emulator = stdenv.hostPlatform.emulator buildPackages;
+    in
+    ''
+      installShellCompletion --cmd uv \
+        --bash <(${emulator} $out/bin/uv generate-shell-completion bash) \
+        --fish <(${emulator} $out/bin/uv generate-shell-completion fish) \
+        --zsh <(${emulator} $out/bin/uv generate-shell-completion zsh)
+    '';
 
   nativeInstallCheckInputs = [
     versionCheckHook
