670dc0df54453aa130f94e43c226059f25c98f31
sternenseemann <sternenseemann@systemli.org>
2024-12-24 12:26:54 +0100

    haskellPackages.amqp-streamly: don't attempt to run docker tests

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -464,6 +464,9 @@ self: super: builtins.intersectAttrs super {
   # Disable tests because they require a running dbus session
   xmonad-dbus = dontCheck super.xmonad-dbus;
 
+  # Test suite requires running a docker container via testcontainers
+  amqp-streamly = dontCheck super.amqp-streamly;
+
   # wxc supports wxGTX >= 3.0, but our current default version points to 2.8.
   # http://hydra.cryp.to/build/1331287/log/raw
   wxc = (addBuildDepend self.split super.wxc).override { wxGTK = pkgs.wxGTK32; };
