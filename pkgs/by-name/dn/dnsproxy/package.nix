fadb7d407e8a408e2fe2e45994b1cc15a6bf9d40
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-24 04:33:45 +0000

    dnsproxy: 0.73.4 -> 0.73.5

--- pkgs/by-name/dn/dnsproxy/package.nix
+++ pkgs/by-name/dn/dnsproxy/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "dnsproxy";
-  version = "0.73.4";
+  version = "0.73.5";
 
   src = fetchFromGitHub {
     owner = "AdguardTeam";
     repo = "dnsproxy";
     rev = "v${version}";
-    hash = "sha256-LCo/SWfsmwbHhDZRf9PRN0FbB81js616YLUQG/Xtz2A=";
+    hash = "sha256-U3OqRfJUtH0rMdVypaoFF3Mi6oBvgt08ufnERPFVjC4=";
   };
 
-  vendorHash = "sha256-5NTHIeKwruVMO7mBxcO+wglUslOJVFmxS2krDfdFAgg=";
+  vendorHash = "sha256-d1WrUfE31Gvgz8tw7cVdPhWf4OHsuuyEHDSn9bETCjI=";
 
   ldflags = [
     "-s"
