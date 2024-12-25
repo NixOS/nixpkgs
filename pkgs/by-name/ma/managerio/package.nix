ba0963653aa6dce279ba0faf5208cdf1436675c9
Darwin <accfordarwin@gmail.com>
2024-12-24 22:24:47 -0500

    formatted

--- pkgs/by-name/ma/managerio/package.nix
+++ pkgs/by-name/ma/managerio/package.nix
@@ -11,8 +11,17 @@ appimageTools.wrapType2 rec {
     hash = "sha256-Q4bH1cFvZfNSOWGXmg/RAOtjK6u5p2iRfUrOvetnoOs=";
   };
   meta = {
-    description = "Accounting software. Available for Windows, Mac and Linux.";
-    homepage = "https://github.com/Manager-io/Manager";
+    description = "Free accounting software for Windows, Mac and Linux";
+    homepage = "https://www.manager.io/";
     maintainers = with lib.maintainers; [ darwincereska ];
+   license = with lib.licenses; [ free ]; # Unspecified freeware
+   sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    platforms = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      # "x86_64-windows" # Windows ones are optional, really, feel free to exclude them
+      # "i686-windows"
+   ];
   };
 }
