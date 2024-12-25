957dadeb01b0f42dc4c4da953827e71bf8bf49cc
Darwin <accfordarwin@gmail.com>
2024-12-24 22:02:59 -0500

    managerio: init 24.12.23.1999

---
+++ pkgs/by-name/ma/managerio/package.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  appimageTools,
+  fetchurl,
+}:
+appimageTools.wrapType2 rec {
+  pname = "manager";
+  version = "24.12.23.1999";
+  src = fetchurl {
+    url = "https://github.com/Manager-io/Manager/releases/download/${version}/Manager-linux-x64.AppImage";
+    hash = "sha256-Q4bH1cFvZfNSOWGXmg/RAOtjK6u5p2iRfUrOvetnoOs=";
+  };
+  meta = {
+    description = "Accounting software. Available for Windows, Mac and Linux.";
+    homepage = "https://github.com/Manager-io/Manager";
+    maintainers = with lib.maintainers; [ darwincereska ];
+  };
+}
