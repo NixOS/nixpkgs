01637a1f5d6b8206afa25478584f6b6a76dc2d98
Darwin Cereska <157426553+darwincereska@users.noreply.github.com>
2024-12-25 12:21:44 -0500

    Update pkgs/by-name/ma/manager-io/package.nix
    Co-authored-by: aucub <78630225+aucub@users.noreply.github.com>

--- pkgs/by-name/ma/manager-io/package.nix
+++ pkgs/by-name/ma/manager-io/package.nix
@@ -10,6 +10,11 @@ appimageTools.wrapType2 rec {
     url = "https://github.com/Manager-io/Manager/releases/download/${version}/Manager-linux-x64.AppImage";
     hash = "sha256-Q4bH1cFvZfNSOWGXmg/RAOtjK6u5p2iRfUrOvetnoOs=";
   };
+
+  extraPkgs =
+    pkgs: with pkgs; [
+      icu
+    ];
   meta = {
     description = "Accounting software for Windows, Mac and Linux";
     homepage = "https://www.manager.io";
