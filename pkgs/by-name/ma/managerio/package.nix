5507b054d7d90cb6e6061296680cce689107a5cb
Darwin Cereska <157426553+darwincereska@users.noreply.github.com>
2024-12-25 09:24:00 -0500

    Update package.nix

--- pkgs/by-name/ma/managerio/package.nix
+++ pkgs/by-name/ma/managerio/package.nix
@@ -4,24 +4,21 @@
   fetchurl,
 }:
 appimageTools.wrapType2 rec {
-  pname = "manager";
+  pname = "manager-io";
   version = "24.12.23.1999";
   src = fetchurl {
     url = "https://github.com/Manager-io/Manager/releases/download/${version}/Manager-linux-x64.AppImage";
     hash = "sha256-Q4bH1cFvZfNSOWGXmg/RAOtjK6u5p2iRfUrOvetnoOs=";
   };
   meta = {
-    description = "Free accounting software for Windows, Mac and Linux";
-    homepage = "https://www.manager.io/";
+    description = "Accounting software for Windows, Mac and Linux";
+    homepage = "https://www.manager.io";
     maintainers = with lib.maintainers; [ darwincereska ];
-    license = with lib.licenses; [ free ]; # Unspecified freeware
+    license = with lib.licenses; [ unfree ]; # Unspecified freeware
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     platforms = [
       "x86_64-linux"
       "x86_64-darwin"
-      "aarch64-darwin"
-      # "x86_64-windows" # Windows ones are optional, really, feel free to exclude them
-      # "i686-windows"
     ];
   };
 }
