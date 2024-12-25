542c24caaf45b8d5b1a52e06de48e4ac5975e98c
OPNA2608 <opna2608@protonmail.com>
2024-12-25 22:00:33 +0100

    qt6Packages.libqtpas: init at 3.6-0

--- pkgs/development/compilers/fpc/libqtpas.nix
+++ pkgs/development/compilers/fpc/libqtpas.nix
@@ -4,32 +4,41 @@
   lazarus,
   qmake,
   qtbase,
-  qtx11extras,
+  # Not in Qt6 anymore
+  qtx11extras ? null,
 }:
 
+let
+  qtVersion = lib.versions.major qtbase.version;
+in
 stdenv.mkDerivation {
   pname = "libqtpas";
   inherit (lazarus) version src;
 
-  sourceRoot = "lazarus/lcl/interfaces/qt5/cbindings";
+  sourceRoot = "lazarus/lcl/interfaces/qt${qtVersion}/cbindings";
 
   postPatch = ''
-    substituteInPlace Qt5Pas.pro \
+    substituteInPlace Qt${qtVersion}Pas.pro \
       --replace 'target.path = $$[QT_INSTALL_LIBS]' "target.path = $out/lib"
   '';
 
   nativeBuildInputs = [ qmake ];
 
-  buildInputs = [
-    qtbase
-    qtx11extras
-  ];
+  buildInputs =
+    [
+      qtbase
+    ]
+    ++ lib.optionals (qtVersion == "5") [
+      qtx11extras
+    ];
 
   dontWrapQtApps = true;
 
   meta = with lib; {
-    description = "Free Pascal Qt5 binding library";
-    homepage = "https://wiki.freepascal.org/Qt5_Interface#libqt5pas";
+    description = "Free Pascal Qt${qtVersion} binding library";
+    homepage =
+      "https://wiki.freepascal.org/Qt${qtVersion}_Interface"
+      + lib.optionalString (qtVersion == "5") "#libqt5pas";
     maintainers = with maintainers; [ sikmir ];
     inherit (lazarus.meta) license platforms;
   };
