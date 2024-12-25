5529ee3590175ee4695e6b16fad1212f4a9dc316
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-12-24 19:47:47 -0500

    pythonPackages.basic-colormath: init at 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1516,6 +1516,8 @@ self: super: with self; {
 
   bases = callPackage ../development/python-modules/bases { };
 
+  basic-colormath = callPackage ../development/python-modules/basic-colormath { };
+
   bash-kernel = callPackage ../development/python-modules/bash-kernel { };
 
   bashlex = callPackage ../development/python-modules/bashlex { };
