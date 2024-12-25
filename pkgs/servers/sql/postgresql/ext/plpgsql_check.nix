1e36a67e2352ca923584b48c0766bd98afd27cc0
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-25 22:16:10 +0000

    postgresqlPackages.plpgsql_check: 2.7.12 -> 2.7.13
    (cherry picked from commit cc0b31cbce08067180da050f1e9062dba6685c10)

--- pkgs/servers/sql/postgresql/ext/plpgsql_check.nix
+++ pkgs/servers/sql/postgresql/ext/plpgsql_check.nix
@@ -9,13 +9,13 @@
 
 buildPostgresqlExtension (finalAttrs: {
   pname = "plpgsql-check";
-  version = "2.7.12";
+  version = "2.7.13";
 
   src = fetchFromGitHub {
     owner = "okbob";
     repo = "plpgsql_check";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-sLakN4595z+Smt7oaK7IPIJZp/JIGwL5UB4OXQek7JU=";
+    hash = "sha256-9xfL/nhjvVb2hQj0OLOFYdpRfuEXikgTC7rSsEZ8ws8=";
   };
 
   passthru.tests.extension = postgresqlTestExtension {
