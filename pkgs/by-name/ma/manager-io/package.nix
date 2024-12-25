29ac0ebf2549631b411f17e15b21e85e8e0f7db6
Darwin Cereska <157426553+darwincereska@users.noreply.github.com>
2024-12-25 12:02:04 -0500

    manager-io: init at 24.12.23.1999

---
+++ pkgs/by-name/ma/manager-io/package.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  appimageTools,
+  fetchurl,
+}:
+appimageTools.wrapType2 rec {
+  pname = "manager-io";
+  version = "24.12.23.1999";
+  src = fetchurl {
+    url = "https://github.com/Manager-io/Manager/releases/download/${version}/Manager-linux-x64.AppImage";
+    hash = "sha256-Q4bH1cFvZfNSOWGXmg/RAOtjK6u5p2iRfUrOvetnoOs=";
+  };
+  meta = {
+    description = "Accounting software for Windows, Mac and Linux";
+    homepage = "https://www.manager.io";
+    maintainers = with lib.maintainers; [ darwincereska ];
+    license = with lib.licenses; [ unfree ]; # Unspecified freeware
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    platforms = [
+      "x86_64-linux"
+      "x86_64-darwin"
+    ];
+  };
+}
