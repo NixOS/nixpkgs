97c2795e1277eb3e931952790ac50e3f43671d23
Darwin Cereska <157426553+darwincereska@users.noreply.github.com>
2024-12-25 21:45:25 -0500

    Updated package.nix

--- pkgs/by-name/ma/manager-io/package.nix
+++ pkgs/by-name/ma/manager-io/package.nix
@@ -14,7 +14,6 @@
 let
   pname = "manager-io";
   version = "24.12.23.1999";
-  name = "${pname}-${version}";
   src = fetchurl {
     url = "https://github.com/Manager-io/Manager/releases/download/${version}/Manager-linux-x64.AppImage";
     hash = "sha256-Q4bH1cFvZfNSOWGXmg/RAOtjK6u5p2iRfUrOvetnoOs=";
