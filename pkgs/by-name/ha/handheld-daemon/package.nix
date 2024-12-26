e560123760f902c4d3005d1bac65370d9bc8fa9d
Toast <39011842+toast003@users.noreply.github.com>
2024-12-26 12:40:39 +0100

    handheld-daemon: 3.8.1 -> 3.9.0

--- pkgs/by-name/ha/handheld-daemon/package.nix
+++ pkgs/by-name/ha/handheld-daemon/package.nix
@@ -14,14 +14,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "handheld-daemon";
-  version = "3.8.1";
+  version = "3.9.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "hhd-dev";
     repo = "hhd";
     tag = "v${version}";
-    hash = "sha256-LDZ3z1IZTkqdJ/xli9b65yO9t1Pj6a4F0DrYdOnOgNU=";
+    hash = "sha256-y3CxdWqQEwdNYs4m1NEUeRjTvvhEpS5S739wyFlluWo=";
   };
 
   # This package relies on several programs expected to be on the user's PATH.
