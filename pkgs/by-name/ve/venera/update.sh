aaa70920ea216a1539fd018a92d13deed85d07d0
aucub <78630225+aucub@users.noreply.github.com>
2024-12-26 23:43:16 +0000

    venera: 1.1.2 -> 1.1.3

--- pkgs/by-name/ve/venera/update.sh
+++ pkgs/by-name/ve/venera/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -I nixpkgs=./. -i bash -p curl gnused jq yq nixVersions.latest bash coreutils nix-update
+#!nix-shell -I nixpkgs=./. -i bash -p curl gnused jq yq nix bash coreutils nix-update
 
 set -eou pipefail
 
