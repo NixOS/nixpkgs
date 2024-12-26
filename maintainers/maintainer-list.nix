55cc7d6625fd1d4846f70ee6d1374f352967ac21
Darwin Cereska <157426553+darwincereska@users.noreply.github.com>
2024-12-25 23:47:42 -0500

    maintainers: added darwincereska

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -5073,6 +5073,12 @@
     github = "Daru-san";
     githubId = 135046711;
   };
+  darwincereska = {
+    name = "Darwin Cereska";
+    email = "darwin@durrstudios.dev";
+    github = "darwincereska";
+    githubId = 157426553;
+  };
   das-g = {
     email = "nixpkgs@raphael.dasgupta.ch";
     github = "das-g";
