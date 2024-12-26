54323cae8df26085af4e483cb30e482c9ac4ce93
rein <manavrj.07@gmail.com>
2024-12-27 01:20:52 +0530

    Update default.nix

--- pkgs/development/python-modules/miniforge/default.nix
+++ pkgs/development/python-modules/miniforge/default.nix
@@ -1,4 +1,11 @@
-{ lib, buildPythonPackage, stdenv, fetchFromGitHub, python3, conda }:
+{
+  lib,
+  buildPythonPackage,
+  stdenv,
+  fetchFromGitHub,
+  python3,
+  conda,
+}:
 
 stdenv.mkDerivation rec {
   pname = "miniforge";
@@ -8,10 +15,13 @@ stdenv.mkDerivation rec {
     owner = "conda-forge";
     repo = "miniforge";
     rev = version;
-    hash = "sha256-Mtw0TI5LWv7aC2kCx7EStYXEUa9J3xTwUPpN/z9GvAQ="; 
+    hash = "sha256-Mtw0TI5LWv7aC2kCx7EStYXEUa9J3xTwUPpN/z9GvAQ=";
   };
 
-  buildInputs = [ python3 conda ];
+  buildInputs = [
+    python3
+    conda
+  ];
 
   installPhase = ''
     mkdir -p $out/bin
@@ -24,7 +34,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/conda-forge/miniforge";
     license = licenses.bsd3;
     platforms = platforms.unix;
-    maintainers = with maintainers;
-      [ qxrein ]; 
+    maintainers = with maintainers; [ qxrein ];
   };
 }
