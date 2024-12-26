ea9da69e10323e091bdde1fe0d07d61f165cf1d3
OPNA2608 <opna2608@protonmail.com>
2024-12-26 13:31:05 +0100

    gtklock-userinfo-module: Fix segfault on login

--- pkgs/by-name/gt/gtklock-userinfo-module/package.nix
+++ pkgs/by-name/gt/gtklock-userinfo-module/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   meson,
   ninja,
   pkg-config,
@@ -21,6 +22,16 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "v${finalAttrs.version}";
     hash = "sha256-d9S0Tj7aII2JQ5/PZmt8HaUIb5caoD4GND0PGvuRMn8=";
   };
+
+  patches = [
+    # Fix segfault with newer accountsservice on login
+    # Remove when version > 4.0.0
+    (fetchpatch {
+      name = "0001-gtklock-userinfo-module-Fix-segfault-on-login.patch";
+      url = "https://github.com/jovanlanik/gtklock-userinfo-module/commit/aa4b5832185d2dd9e7f88826fd9d03e3d5ea1ad6.patch";
+      hash = "sha256-BTCQpWNmMMim7zFet35zoB7VLkAbqCaKFpurdgH+3qg=";
+    })
+  ];
 
   nativeBuildInputs = [
     meson
