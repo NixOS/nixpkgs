88ed9b0162115ca30133b6981237a9c148e3d42c
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-27 14:01:02 +0000

    protoc-gen-go: 1.36.0 -> 1.36.1

--- pkgs/by-name/pr/protoc-gen-go/package.nix
+++ pkgs/by-name/pr/protoc-gen-go/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "protoc-gen-go";
-  version = "1.36.0";
+  version = "1.36.1";
 
   src = fetchFromGitHub {
     owner = "protocolbuffers";
     repo = "protobuf-go";
     rev = "v${version}";
-    hash = "sha256-tJlOIOlYXz6mjxRQphwbpbpkv5bcIJtc4kf9UNlP1Kg=";
+    hash = "sha256-+OFG7dUrxQveG+UmR9gKXxHUJYtVYb/FH1bWxgxwiOw=";
   };
 
   vendorHash = "sha256-nGI/Bd6eMEoY0sBwWEtyhFowHVvwLKjbT4yfzFz6Z3E=";
