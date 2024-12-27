bf80a77952a895d92025c593b71d815bda9e109e
Felix Buehler <account@buehler.rocks>
2024-12-27 18:36:59 +0100

    nixos/homer: add tests

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -437,6 +437,7 @@ in {
   hedgedoc = handleTest ./hedgedoc.nix {};
   herbstluftwm = handleTest ./herbstluftwm.nix {};
   homebox = handleTest ./homebox.nix {};
+  homer = handleTest ./homer {};
   homepage-dashboard = handleTest ./homepage-dashboard.nix {};
   honk = runTest ./honk.nix;
   installed-tests = pkgs.recurseIntoAttrs (handleTest ./installed-tests {});
