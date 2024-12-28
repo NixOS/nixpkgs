57b16f3e7c5195d8ce50dbebe552d1fe5423b838
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-28 15:36:32 +0000

    gqlgenc: 0.27.4 -> 0.30.2

--- pkgs/by-name/gq/gqlgenc/package.nix
+++ pkgs/by-name/gq/gqlgenc/package.nix
@@ -6,18 +6,18 @@
 
 buildGoModule rec {
   pname = "gqlgenc";
-  version = "0.27.4";
+  version = "0.30.2";
 
   src = fetchFromGitHub {
     owner = "yamashou";
     repo = "gqlgenc";
     rev = "v${version}";
-    sha256 = "sha256-NqFF3ppdg3nUZJ3ij0Zx3uKXz4Xhr/JEnkAzYNbPqOE=";
+    sha256 = "sha256-F6EuOqB9ccat9pytJn8glBn5X9eEsEUN2+8+FqVvEbY=";
   };
 
   excludedPackages = [ "example" ];
 
-  vendorHash = "sha256-ln26CHD0q+iPyAx5DalOGyCtVB1QR+7ls1ZjNK8APBU=";
+  vendorHash = "sha256-h3ePmfRkGqVXdtjX2cU5y2HnX+VkmTWNwrEkhLAmrlU=";
 
   meta = with lib; {
     description = "Go tool for building GraphQL client with gqlgen";
