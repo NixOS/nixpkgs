b2c88d5789c0fff9b172781ee0ed1ed54fab5b5b
Sefa Eyeoglu <contact@scrumplex.net>
2024-12-28 11:45:17 +0100

    alcom: 1.8.2 -> 0.1.17
    We were using the wrong tag as pointed out in https://github.com/NixOS/nixpkgs/pull/348118#issuecomment-2563921040
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/by-name/al/alcom/package.nix
+++ pkgs/by-name/al/alcom/package.nix
@@ -1,42 +1,36 @@
 {
   buildDotnetModule,
   cargo-about,
-  cargo-tauri_1,
+  cargo-tauri,
   dotnetCorePackages,
   fetchFromGitHub,
   fetchNpmDeps,
   glib-networking,
-  google-fonts,
   lib,
-  libsoup_2_4,
+  libsoup_3,
+  makeBinaryWrapper,
   nodejs,
   npmHooks,
   openssl,
   pkg-config,
   rustPlatform,
   stdenv,
-  webkitgtk_4_0,
+  webkitgtk_4_1,
+  writeShellScriptBin,
 }:
 let
   pname = "alcom";
-  version = "1.8.2";
+  version = "0.1.17";
 
   src = fetchFromGitHub {
     owner = "vrc-get";
     repo = "vrc-get";
-    rev = "refs/tags/v${version}";
+    tag = "gui-v${version}";
     fetchSubmodules = true;
-    hash = "sha256-jkhjJTb/U2dXj/vyaip+gWoqIOdfFKSExeDl0T11DE4=";
+    hash = "sha256-ph9dJhm4DbI6/we/HD4T1LOafaWylhHoNT+zaYgl4f8=";
   };
 
   subdir = "vrc-get-gui";
-
-  google-fonts' = google-fonts.override {
-    fonts = [
-      "NotoSans"
-      "NotoSansJP"
-    ];
-  };
 
   dotnetSdk = dotnetCorePackages.sdk_8_0;
   dotnetRuntime = dotnetCorePackages.runtime_8_0;
@@ -59,51 +53,45 @@ rustPlatform.buildRustPackage {
 
   nativeBuildInputs = [
     cargo-about
-    cargo-tauri_1.hook
+    cargo-tauri.hook
     dotnetSdk
     nodejs
     npmHooks.npmConfigHook
     pkg-config
+    (writeShellScriptBin "git" "echo 'nixpkgs-${src.tag}'")
   ];
 
   buildInputs =
     [ openssl ]
     ++ lib.optionals stdenv.hostPlatform.isLinux [
       glib-networking
-      libsoup_2_4
-      webkitgtk_4_0
+      libsoup_3
+      makeBinaryWrapper
+      webkitgtk_4_1
     ]
     ++ dotnetSdk.packages
     ++ dotnetBuild.nugetDeps;
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "tauri-plugin-single-instance-0.0.0" = "sha256-Mf2/cnKotd751ZcSHfiSLNe2nxBfo4dMBdoCwQhe7yI=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-C03XPIPzPfVG3K/KoPKU+9475OljtYgkwjE+dxVKYls=";
   buildAndTestSubdir = subdir;
 
   npmDeps = fetchNpmDeps {
     inherit src;
     sourceRoot = "${src.name}/${subdir}";
-    hash = "sha256-4zokKLhLgW2u1GxeTlIAAxJINSpxHRtY5HXmhi9nj6c=";
+    hash = "sha256-QAUoz/mzF9aDvkILKX3rxkYUC+VmJeRVjG8vJ/b0Dho=";
   };
   npmRoot = subdir;
-
-  patches = [
-    ./use-local-fonts.patch
-  ];
-
-  postPatch = ''
-    install -Dm644 "${google-fonts'}/share/fonts/truetype/NotoSans[wdth,wght].ttf" ${subdir}/app/fonts/noto-sans.ttf
-    install -Dm644 "${google-fonts'}/share/fonts/truetype/NotoSansJP[wght].ttf" ${subdir}/app/fonts/noto-sans-jp.ttf
-  '';
 
   preConfigure = ''
     dotnet restore "vrc-get-litedb/dotnet/vrc-get-litedb.csproj" \
       -p:ContinuousIntegrationBuild=true \
       -p:Deterministic=true
+  '';
+
+  postInstall = lib.optionalString stdenv.hostPlatform.isLinux ''
+    wrapProgram $out/bin/ALCOM \
+      --set APPIMAGE ALCOM
   '';
 
   passthru = {
@@ -115,8 +103,7 @@ rustPlatform.buildRustPackage {
     homepage = "https://github.com/vrc-get/vrc-get";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ Scrumplex ];
-    # aarch64-linux: Error failed to build app: Target aarch64-unknown-linux-gnu does not exist. Please run `rustup target list` to see the available targets.
-    broken = stdenv.hostPlatform.isDarwin || stdenv.hostPlatform.isAarch64;
+    broken = stdenv.hostPlatform.isDarwin;
     mainProgram = "alcom";
   };
 }
