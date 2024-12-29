dbbaa9e1c48e0ef2b6bc72797aae64168ae36580
André Silva <andrerfosilva@gmail.com>
2024-12-29 15:18:06 +0000

    polkadot: use new cargo fetcher

--- pkgs/applications/blockchains/polkadot/default.nix
+++ pkgs/applications/blockchains/polkadot/default.nix
@@ -45,19 +45,8 @@ rustPlatform.buildRustPackage rec {
     rm .git_commit
   '';
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "ark-secret-scalar-0.0.2" = "sha256-yvgTxccxeUrnBhElI7AY3ad0PqmGCDlsoi8jH2Cceks=";
-      "common-0.1.0" = "sha256-Jbl5b0zIDFBcu2lYi5LYRdABq3vxgPlE4EsFucTWQd8=";
-      "fflonk-0.1.0" = "sha256-+BvZ03AhYNP0D8Wq9EMsP+lSgPA6BBlnWkoxTffVLwo=";
-      "ipfs-hasher-0.21.3" = "sha256-AH3NMil07F+kkWjqAbMaMbjnTisSQiCd3tJz934ZICw=";
-      "simple-mermaid-0.1.0" = "sha256-IekTldxYq+uoXwGvbpkVTXv2xrcZ0TQfyyE2i2zH+6w=";
-      "sp-ark-bls12-381-0.4.2" = "sha256-nNr0amKhSvvI9BlsoP+8v6Xppx/s7zkf0l9Lm3DW8w8=";
-      "sp-crypto-ec-utils-0.4.1" = "sha256-KXyG43YIzMG2r6kCTyQyCIHSAkXSlZv8pbFEXXvC4JU=";
-      "sp-debug-derive-8.0.0" = "sha256-/Sw1ZM/JcJBokFE4y2mv/P43ciTL5DEm0PDG0jZvMkI=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-ueTEx6oqfMzM1ytXavRxLrWf4+8jYqVY9JJJbl8j2YY=";
 
   buildType = "production";
   buildAndTestSubdir = "polkadot";
