0aa644b17489162d50b7e844e6cd502ad47583af
Felix Buehler <account@buehler.rocks>
2024-12-29 22:10:51 +0100

    nixos/services.xandikos: fmt

--- nixos/modules/services/networking/xandikos.nix
+++ nixos/modules/services/networking/xandikos.nix
@@ -84,58 +84,60 @@ in
 
   };
 
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    {
-      meta.maintainers = with lib.maintainers; [ _0x4A6F ];
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      {
+        meta.maintainers = with lib.maintainers; [ _0x4A6F ];
 
-      systemd.services.xandikos = {
-        description = "A Simple Calendar and Contact Server";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
+        systemd.services.xandikos = {
+          description = "A Simple Calendar and Contact Server";
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
 
-        serviceConfig = {
-          User = "xandikos";
-          Group = "xandikos";
-          DynamicUser = "yes";
-          RuntimeDirectory = "xandikos";
-          StateDirectory = "xandikos";
-          StateDirectoryMode = "0700";
-          PrivateDevices = true;
-          # Sandboxing
-          CapabilityBoundingSet = "CAP_NET_RAW CAP_NET_ADMIN";
-          ProtectSystem = "strict";
-          ProtectHome = true;
-          PrivateTmp = true;
-          ProtectKernelTunables = true;
-          ProtectKernelModules = true;
-          ProtectControlGroups = true;
-          RestrictAddressFamilies = "AF_INET AF_INET6 AF_UNIX AF_PACKET AF_NETLINK";
-          RestrictNamespaces = true;
-          LockPersonality = true;
-          MemoryDenyWriteExecute = true;
-          RestrictRealtime = true;
-          RestrictSUIDSGID = true;
-          ExecStart = ''
-            ${cfg.package}/bin/xandikos \
-              --directory /var/lib/xandikos \
-              --listen-address ${cfg.address} \
-              --port ${toString cfg.port} \
-              --route-prefix ${cfg.routePrefix} \
-              ${lib.concatStringsSep " " cfg.extraOptions}
-          '';
-        };
-      };
-    }
-
-    (lib.mkIf cfg.nginx.enable {
-      services.nginx = {
-        enable = true;
-        virtualHosts."${cfg.nginx.hostName}" = {
-          locations."/" = {
-            proxyPass = "http://${cfg.address}:${toString cfg.port}/";
+          serviceConfig = {
+            User = "xandikos";
+            Group = "xandikos";
+            DynamicUser = "yes";
+            RuntimeDirectory = "xandikos";
+            StateDirectory = "xandikos";
+            StateDirectoryMode = "0700";
+            PrivateDevices = true;
+            # Sandboxing
+            CapabilityBoundingSet = "CAP_NET_RAW CAP_NET_ADMIN";
+            ProtectSystem = "strict";
+            ProtectHome = true;
+            PrivateTmp = true;
+            ProtectKernelTunables = true;
+            ProtectKernelModules = true;
+            ProtectControlGroups = true;
+            RestrictAddressFamilies = "AF_INET AF_INET6 AF_UNIX AF_PACKET AF_NETLINK";
+            RestrictNamespaces = true;
+            LockPersonality = true;
+            MemoryDenyWriteExecute = true;
+            RestrictRealtime = true;
+            RestrictSUIDSGID = true;
+            ExecStart = ''
+              ${cfg.package}/bin/xandikos \
+                --directory /var/lib/xandikos \
+                --listen-address ${cfg.address} \
+                --port ${toString cfg.port} \
+                --route-prefix ${cfg.routePrefix} \
+                ${lib.concatStringsSep " " cfg.extraOptions}
+            '';
           };
         };
-      };
-    })
-  ]);
+      }
+
+      (lib.mkIf cfg.nginx.enable {
+        services.nginx = {
+          enable = true;
+          virtualHosts."${cfg.nginx.hostName}" = {
+            locations."/" = {
+              proxyPass = "http://${cfg.address}:${toString cfg.port}/";
+            };
+          };
+        };
+      })
+    ]
+  );
 }
