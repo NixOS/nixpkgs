919525d7414a1cdfa0e9272d4bd325a3dffc57ef
Felix Buehler <account@buehler.rocks>
2024-12-29 23:34:51 +0100

    nixos/networking.rxe: remove `with lib;`

--- nixos/modules/services/networking/rxe.nix
+++ nixos/modules/services/networking/rxe.nix
@@ -1,7 +1,4 @@
 { config, lib, pkgs, ... }:
-
-with lib;
-
 let
   cfg = config.networking.rxe;
 
@@ -10,9 +7,9 @@ in {
 
   options = {
     networking.rxe = {
-      enable = mkEnableOption "RDMA over converged ethernet";
-      interfaces = mkOption {
-        type = types.listOf types.str;
+      enable = lib.mkEnableOption "RDMA over converged ethernet";
+      interfaces = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
         default = [ ];
         example = [ "eth0" ];
         description = ''
@@ -26,7 +23,7 @@ in {
 
   ###### implementation
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
 
     systemd.services.rxe = {
       description = "RoCE interfaces";
