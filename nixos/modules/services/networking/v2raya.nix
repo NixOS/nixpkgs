0770f7e23aa13f56fbde99e0520332c22a3f3032
Felix Buehler <account@buehler.rocks>
2024-12-29 21:50:32 +0100

    nixos/services.v2raya: remove `with lib;`

--- nixos/modules/services/networking/v2raya.nix
+++ nixos/modules/services/networking/v2raya.nix
@@ -4,9 +4,6 @@
   lib,
   ...
 }:
-
-with lib;
-
 let
   cfg = config.services.v2raya;
 in
@@ -14,17 +11,17 @@ in
 {
   options = {
     services.v2raya = {
-      enable = options.mkEnableOption "the v2rayA service";
+      enable = lib.mkEnableOption "the v2rayA service";
 
-      package = options.mkPackageOption pkgs "v2raya" { };
-      cliPackage = options.mkPackageOption pkgs "v2ray" {
+      package = lib.mkPackageOption pkgs "v2raya" { };
+      cliPackage = lib.mkPackageOption pkgs "v2ray" {
         example = "pkgs.xray";
         extraDescription = "This is the package used for overriding the value of the `v2ray` attribute in the package set by `services.v2raya.package`.";
       };
     };
   };
 
-  config = mkIf config.services.v2raya.enable {
+  config = lib.mkIf config.services.v2raya.enable {
     environment.systemPackages = [ (cfg.package.override { v2ray = cfg.cliPackage; }) ];
 
     systemd.services.v2raya =
@@ -32,7 +29,7 @@ in
         nftablesEnabled = config.networking.nftables.enable;
         iptablesServices = [
           "iptables.service"
-        ] ++ optional config.networking.enableIPv6 "ip6tables.service";
+        ] ++ lib.optional config.networking.enableIPv6 "ip6tables.service";
         tableServices = if nftablesEnabled then [ "nftables.service" ] else iptablesServices;
       in
       {
@@ -48,7 +45,7 @@ in
 
         serviceConfig = {
           User = "root";
-          ExecStart = "${getExe (cfg.package.override { v2ray = cfg.cliPackage; })} --log-disable-timestamp";
+          ExecStart = "${lib.getExe (cfg.package.override { v2ray = cfg.cliPackage; })} --log-disable-timestamp";
           Environment = [ "V2RAYA_LOG_FILE=/var/log/v2raya/v2raya.log" ];
           LimitNPROC = 500;
           LimitNOFILE = 1000000;
@@ -68,5 +65,5 @@ in
       };
   };
 
-  meta.maintainers = with maintainers; [ elliot ];
+  meta.maintainers = with lib.maintainers; [ elliot ];
 }
