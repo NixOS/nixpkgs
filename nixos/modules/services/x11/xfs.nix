cac14b639daaf9bb04c892a25d4a14e05a434b2f
Felix Buehler <account@buehler.rocks>
2024-12-29 21:50:45 +0100

    nixos/services.xfs: remove `with lib;`

--- nixos/modules/services/x11/xfs.nix
+++ nixos/modules/services/x11/xfs.nix
@@ -4,9 +4,6 @@
   pkgs,
   ...
 }:
-
-with lib;
-
 let
 
   configFile = ./xfs.conf;
@@ -21,8 +18,8 @@ in
 
     services.xfs = {
 
-      enable = mkOption {
-        type = types.bool;
+      enable = lib.mkOption {
+        type = lib.types.bool;
         default = false;
         description = "Whether to enable the X Font Server.";
       };
@@ -33,8 +30,8 @@ in
 
   ###### implementation
 
-  config = mkIf config.services.xfs.enable {
-    assertions = singleton {
+  config = lib.mkIf config.services.xfs.enable {
+    assertions = lib.singleton {
       assertion = config.fonts.enableFontDir;
       message = "Please enable fonts.enableFontDir to use the X Font Server.";
     };
