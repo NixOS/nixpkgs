575a263149be54993c0648c4324cf4224dca19d5
Felix Buehler <account@buehler.rocks>
2024-12-29 21:50:31 +0100

    nixos/services.keepalived: remove `with lib;`

--- nixos/modules/services/networking/keepalived/vrrp-instance-options.nix
+++ nixos/modules/services/networking/keepalived/vrrp-instance-options.nix
@@ -1,18 +1,16 @@
 { lib }:
-
-with lib;
 {
   options = {
 
-    interface = mkOption {
-      type = types.str;
+    interface = lib.mkOption {
+      type = lib.types.str;
       description = ''
         Interface for inside_network, bound by vrrp.
       '';
     };
 
-    state = mkOption {
-      type = types.enum [
+    state = lib.mkOption {
+      type = lib.types.enum [
         "MASTER"
         "BACKUP"
       ];
@@ -24,16 +22,16 @@ with lib;
       '';
     };
 
-    virtualRouterId = mkOption {
-      type = types.ints.between 1 255;
+    virtualRouterId = lib.mkOption {
+      type = lib.types.ints.between 1 255;
       description = ''
         Arbitrary unique number 1..255. Used to differentiate multiple instances
         of vrrpd running on the same NIC (and hence same socket).
       '';
     };
 
-    priority = mkOption {
-      type = types.int;
+    priority = lib.mkOption {
+      type = lib.types.int;
       default = 100;
       description = ''
         For electing MASTER, highest priority wins. To be MASTER, make 50 more
@@ -41,8 +39,8 @@ with lib;
       '';
     };
 
-    noPreempt = mkOption {
-      type = types.bool;
+    noPreempt = lib.mkOption {
+      type = lib.types.bool;
       default = false;
       description = ''
         VRRP will normally preempt a lower priority machine when a higher
@@ -53,16 +51,16 @@ with lib;
       '';
     };
 
-    useVmac = mkOption {
-      type = types.bool;
+    useVmac = lib.mkOption {
+      type = lib.types.bool;
       default = false;
       description = ''
         Use VRRP Virtual MAC.
       '';
     };
 
-    vmacInterface = mkOption {
-      type = types.nullOr types.str;
+    vmacInterface = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
       default = null;
       description = ''
         Name of the vmac interface to use. keepalived will come up with a name
@@ -70,16 +68,16 @@ with lib;
       '';
     };
 
-    vmacXmitBase = mkOption {
-      type = types.bool;
+    vmacXmitBase = lib.mkOption {
+      type = lib.types.bool;
       default = false;
       description = ''
         Send/Recv VRRP messages from base interface instead of VMAC interface.
       '';
     };
 
-    unicastSrcIp = mkOption {
-      type = types.nullOr types.str;
+    unicastSrcIp = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
       default = null;
       description = ''
         Default IP for binding vrrpd is the primary IP on interface. If you
@@ -88,8 +86,8 @@ with lib;
       '';
     };
 
-    unicastPeers = mkOption {
-      type = types.listOf types.str;
+    unicastPeers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
       default = [ ];
       description = ''
         Do not send VRRP adverts over VRRP multicast group. Instead it sends
@@ -100,9 +98,9 @@ with lib;
       '';
     };
 
-    virtualIps = mkOption {
-      type = types.listOf (
-        types.submodule (
+    virtualIps = lib.mkOption {
+      type = lib.types.listOf (
+        lib.types.submodule (
           import ./virtual-ip-options.nix {
             inherit lib;
           }
@@ -113,8 +111,8 @@ with lib;
       description = "Declarative vhost config";
     };
 
-    trackScripts = mkOption {
-      type = types.listOf types.str;
+    trackScripts = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
       default = [ ];
       example = [
         "chk_cmd1"
@@ -123,8 +121,8 @@ with lib;
       description = "List of script names to invoke for health tracking.";
     };
 
-    trackInterfaces = mkOption {
-      type = types.listOf types.str;
+    trackInterfaces = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
       default = [ ];
       example = [
         "eth0"
@@ -133,8 +131,8 @@ with lib;
       description = "List of network interfaces to monitor for health tracking.";
     };
 
-    extraConfig = mkOption {
-      type = types.lines;
+    extraConfig = lib.mkOption {
+      type = lib.types.lines;
       default = "";
       description = ''
         Extra lines to be added verbatim to the vrrp_instance section.
--- nixos/modules/services/networking/keepalived/vrrp-script-options.nix
+++ nixos/modules/services/networking/keepalived/vrrp-script-options.nix
@@ -1,59 +1,57 @@
 { lib }:
-
-with lib;
 with lib.types;
 {
   options = {
 
-    script = mkOption {
+    script = lib.mkOption {
       type = str;
-      example = literalExpression ''"''${pkgs.curl} -f http://localhost:80"'';
+      example = lib.literalExpression ''"''${pkgs.curl} -f http://localhost:80"'';
       description = "(Path of) Script command to execute followed by args, i.e. cmd [args]...";
     };
 
-    interval = mkOption {
+    interval = lib.mkOption {
       type = int;
       default = 1;
       description = "Seconds between script invocations.";
     };
 
-    timeout = mkOption {
+    timeout = lib.mkOption {
       type = int;
       default = 5;
       description = "Seconds after which script is considered to have failed.";
     };
 
-    weight = mkOption {
+    weight = lib.mkOption {
       type = int;
       default = 0;
       description = "Following a failure, adjust the priority by this weight.";
     };
 
-    rise = mkOption {
+    rise = lib.mkOption {
       type = int;
       default = 5;
       description = "Required number of successes for OK transition.";
     };
 
-    fall = mkOption {
+    fall = lib.mkOption {
       type = int;
       default = 3;
       description = "Required number of failures for KO transition.";
     };
 
-    user = mkOption {
+    user = lib.mkOption {
       type = str;
       default = "keepalived_script";
       description = "Name of user to run the script under.";
     };
 
-    group = mkOption {
+    group = lib.mkOption {
       type = nullOr str;
       default = null;
       description = "Name of group to run the script under. Defaults to user group.";
     };
 
-    extraConfig = mkOption {
+    extraConfig = lib.mkOption {
       type = lines;
       default = "";
       description = "Extra lines to be added verbatim to the vrrp_script section.";
