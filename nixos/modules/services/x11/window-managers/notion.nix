4ddad25ec6034371fe5ade6e623193fd271a0881
Felix Buehler <account@buehler.rocks>
2024-12-30 14:44:46 +0100

    nixos/services.xserver.windowManager.notion: remove `with lib;`

--- nixos/modules/services/x11/window-managers/notion.nix
+++ nixos/modules/services/x11/window-managers/notion.nix
@@ -4,19 +4,16 @@
   pkgs,
   ...
 }:
-
-with lib;
-
 let
   cfg = config.services.xserver.windowManager.notion;
 in
 
 {
   options = {
-    services.xserver.windowManager.notion.enable = mkEnableOption "notion";
+    services.xserver.windowManager.notion.enable = lib.mkEnableOption "notion";
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     services.xserver.windowManager = {
       session = [
         {
