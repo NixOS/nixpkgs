5cbc2887e562a392d6b7ea9633ad3936ab70da95
Anthony Rodriguez <anthony@nezia.dev>
2024-12-31 00:42:17 +0100

    bolt-launcher: fix build

--- pkgs/by-name/bo/bolt-launcher/package.nix
+++ pkgs/by-name/bo/bolt-launcher/package.nix
@@ -32,59 +32,55 @@
   makeDesktopItem,
   copyDesktopItems,
   enableRS3 ? false,
-}:
-let
-  cef = libcef.overrideAttrs (oldAttrs: {
-    installPhase =
-      let
-        gl_rpath = lib.makeLibraryPath [
-          stdenv.cc.cc.lib
-        ];
-        rpath = lib.makeLibraryPath [
-          glib
-          nss
-          nspr
-          atk
-          at-spi2-atk
-          libdrm
-          expat
-          xorg.libxcb
-          libxkbcommon
-          xorg.libX11
-          xorg.libXcomposite
-          xorg.libXdamage
-          xorg.libXext
-          xorg.libXfixes
-          xorg.libXrandr
-          libgbm
-          gtk3
-          pango
-          cairo
-          alsa-lib
-          dbus
-          at-spi2-core
-          cups
-          xorg.libxshmfence
-          systemd
-        ];
-      in
-      ''
-        mkdir -p $out/lib/ $out/share/cef/
-        cp libcef_dll_wrapper/libcef_dll_wrapper.a $out/lib/
-        cp -r ../Resources/* $out/lib/
-        cp -r ../Release/* $out/lib/
-        patchelf --set-rpath "${rpath}" $out/lib/libcef.so
-        patchelf --set-rpath "${gl_rpath}" $out/lib/libEGL.so
-        patchelf --set-rpath "${gl_rpath}" $out/lib/libGLESv2.so
-        cp ../Release/*.bin $out/share/cef/
-        cp -r ../Resources/* $out/share/cef/
-        cp -r ../include $out
-        cp -r ../libcef_dll $out
-        cp -r ../cmake $out
-      '';
+}: let
+  cef = libcef.overrideAttrs (_: {
+    installPhase = let
+      gl_rpath = lib.makeLibraryPath [
+        stdenv.cc.cc.lib
+      ];
+      rpath = lib.makeLibraryPath [
+        glib
+        nss
+        nspr
+        atk
+        at-spi2-atk
+        libdrm
+        expat
+        xorg.libxcb
+        libxkbcommon
+        xorg.libX11
+        xorg.libXcomposite
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXrandr
+        libgbm
+        gtk3
+        pango
+        cairo
+        alsa-lib
+        dbus
+        at-spi2-core
+        cups
+        xorg.libxshmfence
+        systemd
+      ];
+    in ''
+      mkdir -p $out/lib/ $out/share/cef/
+      cp libcef_dll_wrapper/libcef_dll_wrapper.a $out/lib/
+      cp -r ../Resources/* $out/lib/
+      cp -r ../Release/* $out/lib/
+      patchelf --set-rpath "${rpath}" $out/lib/libcef.so
+      patchelf --set-rpath "${gl_rpath}" $out/lib/libEGL.so
+      patchelf --set-rpath "${gl_rpath}" $out/lib/libGLESv2.so
+      cp ../Release/*.bin $out/share/cef/
+      cp -r ../Resources/* $out/share/cef/
+      cp -r ../include $out
+      cp -r ../libcef_dll $out
+      cp -r ../cmake $out
+    '';
   });
-in
-let
+in let
   bolt = stdenv.mkDerivation (finalAttrs: {
     pname = "bolt-launcher";
     version = "0.9.0";
@@ -138,7 +134,7 @@ let
     '';
 
     postFixup = ''
-      makeWrapper "$out/opt/bolt-launcher/bolt" "$out/bin/${finalAttrs.pname}-${finalAttrs.version}" \
+      makeWrapper "$out/opt/bolt-launcher/bolt" "$out/bin/${finalAttrs.pname}" \
       --set JAVA_HOME ${jdk17}
       mkdir -p $out/lib
       cp $out/usr/local/lib/libbolt-plugin.so $out/lib
@@ -154,61 +150,59 @@ let
         desktopName = "Bolt Launcher";
         genericName = finalAttrs.pname;
         comment = "An alternative launcher for RuneScape";
-        exec = "${finalAttrs.pname}-${finalAttrs.version}";
+        exec = "${finalAttrs.pname}";
         icon = finalAttrs.pname;
-        categories = [ "Game" ];
+        categories = ["Game"];
       })
     ];
   });
 in
-buildFHSEnv {
-  inherit (bolt) pname version;
+  buildFHSEnv {
+    inherit (bolt) pname version;
 
-  targetPkgs =
-    pkgs:
-    [ bolt ]
-    ++ (with pkgs; [
-      xorg.libSM
-      xorg.libXxf86vm
-      xorg.libX11
-      glib
-      pango
-      cairo
-      gdk-pixbuf
-      libz
-      libcap
-      libsecret
-      SDL2
-      libGL
-    ])
-    ++ lib.optionals enableRS3 (
-      with pkgs;
-      [
-        gtk2-x11
-        openssl_1_1
-      ]
-    );
+    targetPkgs = pkgs:
+      [bolt]
+      ++ (with pkgs; [
+        xorg.libSM
+        xorg.libXxf86vm
+        xorg.libX11
+        glib
+        pango
+        cairo
+        gdk-pixbuf
+        libz
+        libcap
+        libsecret
+        SDL2
+        libGL
+      ])
+      ++ lib.optionals enableRS3 (
+        with pkgs; [
+          gtk2-x11
+          openssl_1_1
+        ]
+      );
 
-  extraInstallCommands = ''
-    mkdir -p $out/share/applications
-    mkdir -p $out/share/icons/hicolor/256x256/apps
+    extraInstallCommands = ''
+      mkdir -p $out/share/applications
+      mkdir -p $out/share/icons/hicolor/256x256/apps
 
-    ln -s ${bolt}/share/applications/*.desktop $out/share/applications/
+      ln -s ${bolt}/share/applications/*.desktop $out/share/applications/
 
-    ln -s ${bolt}/share/icons/hicolor/256x256/apps/*.png $out/share/icons/hicolor/256x256/apps/
-  '';
-
-  runScript = "${bolt.name}";
-
-  meta = {
-    homepage = "https://github.com/Adamcake/Bolt";
-    description = "An alternative launcher for RuneScape.";
-    longDescription = ''
-      Bolt Launcher supports HDOS/RuneLite by default with an optional feature flag for RS3 (enableRS3).
+      ln -s ${bolt}/share/icons/hicolor/256x256/apps/*.png $out/share/icons/hicolor/256x256/apps/
     '';
-    license = lib.licenses.agpl3Plus;
-    maintainers = with lib.maintainers; [ nezia ];
-    platforms = lib.platforms.linux;
-    mainProgram = "${bolt.name}";
-  };
-}
+
+    runScript = "${bolt.pname}";
+
+    meta = {
+      homepage = "https://github.com/Adamcake/Bolt";
+      description = "An alternative launcher for RuneScape.";
+      longDescription = ''
+        Bolt Launcher supports HDOS/RuneLite by default with an optional feature flag for RS3 (enableRS3).
+      '';
+      license = lib.licenses.agpl3Plus;
+      maintainers = with lib.maintainers; [nezia];
+      platforms = lib.platforms.linux;
+      mainProgram = "${bolt.pname}";
+    };
+  }
