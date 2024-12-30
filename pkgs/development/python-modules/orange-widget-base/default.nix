3607001bec447572b7976db5b41786a89c6c0bd0
lucasew <lucas59356@gmail.com>
2024-12-29 22:54:56 -0300

    python3Packages.orange-widget-base: rev as tag
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pkgs/development/python-modules/orange-widget-base/default.nix
+++ pkgs/development/python-modules/orange-widget-base/default.nix
@@ -27,7 +27,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "biolab";
     repo = "orange-widget-base";
-    rev = "refs/tags/${version}";
+    tag = version;
     hash = "sha256-tWo8vwC8xLA2ZKeab2GqpXGdlHhaqOT9wu1efqSHZSg=";
   };
 
