dc68c37d997614a555883ef15f6dc264c8754dc9
Gaetan Lepage <gaetan@glepage.com>
2024-12-30 22:29:08 +0100

    rustpython: improve derivation

--- pkgs/by-name/ru/rustpython/package.nix
+++ pkgs/by-name/ru/rustpython/package.nix
@@ -3,8 +3,9 @@
   stdenv,
   rustPlatform,
   fetchFromGitHub,
-  SystemConfiguration,
   python3,
+  versionCheckHook,
+  nix-update-script,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -14,33 +15,34 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "RustPython";
     repo = "RustPython";
-    rev = "refs/tags/${version}";
+    tag = version;
     hash = "sha256-BYYqvPJu/eFJ9lt07A0p7pd8pGFccUe/okFqGEObhY4=";
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "rustpython-doc-0.3.0" = "sha256-34ERuLFKzUD9Xmf1zlafe42GLWZfUlw17ejf/NN6yH4=";
-    };
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-LuxET01n5drYmPXXhCl0Cs9yoCQKwWah8FWfmKmLdsg=";
 
   # freeze the stdlib into the rustpython binary
   cargoBuildFlags = [ "--features=freeze-stdlib" ];
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ SystemConfiguration ];
-
   nativeCheckInputs = [ python3 ];
 
-  meta = with lib; {
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+  versionCheckProgramArg = [ "--version" ];
+  doInstallCheck = true;
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
     description = "Python 3 interpreter in written Rust";
     homepage = "https://rustpython.github.io";
-    license = licenses.mit;
-    maintainers = with maintainers; [ prusnak ];
-    #   = note: Undefined symbols for architecture x86_64:
-    #       "_utimensat", referenced from:
-    #           rustpython_vm::function::builtin::IntoPyNativeFn::into_func::... in
-    #           rustpython-10386d81555652a7.rustpython_vm-f0b5bedfcf056d0b.rustpython_vm.7926b68e665728ca-cgu.08.rcgu.o.rcgu.o
     broken = stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ prusnak ];
+    mainProgram = "rustpython";
   };
 }
