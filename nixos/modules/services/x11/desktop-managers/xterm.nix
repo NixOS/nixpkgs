dcd408ab4aac66db2a40c8d4aa3edcc0bfc2f439
Felix Buehler <account@buehler.rocks>
2024-12-30 14:44:40 +0100

    nixos/services.xserver.desktopManager.xterm: remove `with lib;`

--- nixos/modules/services/x11/desktop-managers/xterm.nix
+++ nixos/modules/services/x11/desktop-managers/xterm.nix
@@ -4,9 +4,6 @@
   pkgs,
   ...
 }:
-
-with lib;
-
 let
 
   cfg = config.services.xserver.desktopManager.xterm;
@@ -17,18 +14,18 @@ in
 {
   options = {
 
-    services.xserver.desktopManager.xterm.enable = mkOption {
-      type = types.bool;
-      default = versionOlder config.system.stateVersion "19.09" && xSessionEnabled;
-      defaultText = literalExpression ''versionOlder config.system.stateVersion "19.09" && config.services.xserver.enable;'';
+    services.xserver.desktopManager.xterm.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = lib.versionOlder config.system.stateVersion "19.09" && xSessionEnabled;
+      defaultText = lib.literalExpression ''versionOlder config.system.stateVersion "19.09" && config.services.xserver.enable;'';
       description = "Enable a xterm terminal as a desktop manager.";
     };
 
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
 
-    services.xserver.desktopManager.session = singleton {
+    services.xserver.desktopManager.session = lib.singleton {
       name = "xterm";
       start = ''
         ${pkgs.xterm}/bin/xterm -ls &
