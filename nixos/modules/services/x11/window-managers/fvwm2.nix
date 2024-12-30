6752537076d8311fa67f881b9bbe8f40bd7592ef
Felix Buehler <account@buehler.rocks>
2024-12-30 14:44:44 +0100

    nixos/services.xserver.windowManager.fvwm2: remove `with lib;`

--- nixos/modules/services/x11/window-managers/fvwm2.nix
+++ nixos/modules/services/x11/window-managers/fvwm2.nix
@@ -4,9 +4,6 @@
   pkgs,
   ...
 }:
-
-with lib;
-
 let
   cfg = config.services.xserver.windowManager.fvwm2;
   fvwm2 = pkgs.fvwm2.override { enableGestures = cfg.gestures; };
@@ -15,7 +12,7 @@ in
 {
 
   imports = [
-    (mkRenamedOptionModule
+    (lib.mkRenamedOptionModule
       [ "services" "xserver" "windowManager" "fvwm" ]
       [ "services" "xserver" "windowManager" "fvwm2" ]
     )
@@ -25,11 +22,11 @@ in
 
   options = {
     services.xserver.windowManager.fvwm2 = {
-      enable = mkEnableOption "Fvwm2 window manager";
+      enable = lib.mkEnableOption "Fvwm2 window manager";
 
-      gestures = mkOption {
+      gestures = lib.mkOption {
         default = false;
-        type = types.bool;
+        type = lib.types.bool;
         description = "Whether or not to enable libstroke for gesture support";
       };
     };
@@ -37,8 +34,8 @@ in
 
   ###### implementation
 
-  config = mkIf cfg.enable {
-    services.xserver.windowManager.session = singleton {
+  config = lib.mkIf cfg.enable {
+    services.xserver.windowManager.session = lib.singleton {
       name = "fvwm2";
       start = ''
         ${fvwm2}/bin/fvwm &
