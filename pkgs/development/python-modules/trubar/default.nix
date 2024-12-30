45ac8f5b9a7b50a31f15a1ec9c4d50366442dbab
lucasew <lucas59356@gmail.com>
2024-12-29 22:54:21 -0300

    python3Packages.trubar: rev as tag
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pkgs/development/python-modules/trubar/default.nix
+++ pkgs/development/python-modules/trubar/default.nix
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "janezd";
     repo = "trubar";
-    rev = "refs/tags/${version}";
+    tag = version;
     hash = "sha256-ChKmeACEMnFcMYSdkdVlFiE3td171ihUS2A+qsP5ASk=";
   };
 
