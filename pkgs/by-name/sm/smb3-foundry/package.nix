e4e72477dd2f8163ca3de2cc331dd02a9aa15c9c
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-12-30 11:13:46 +0100

    smb3-foundry: do some small cleanup

--- pkgs/by-name/sm/smb3-foundry/package.nix
+++ pkgs/by-name/sm/smb3-foundry/package.nix
@@ -6,15 +6,14 @@
 }:
 
 let
-  python = (
-    python3.withPackages (
-      ps: with ps; [
-        pyside6
-        py65
-        qdarkstyle
-      ]
-    )
+  python = python3.withPackages (
+    ps: with ps; [
+      pyside6
+      py65
+      qdarkstyle
+    ]
   );
+
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "smb3-foundry";
@@ -23,7 +22,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "mchlnix";
     repo = "SMB3-Foundry";
-    rev = "refs/tags/${finalAttrs.version}";
+    tag = finalAttrs.version;
     hash = "sha256-9pztxzgdPqrTNUMtD9boxtqb32LCsGMoa/bBq5GSJ1I=";
   };
 
@@ -42,7 +41,7 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   meta = {
-    changelog = "https://github.com/mchlnix/SMB3-Foundry/releases/tag/${finalAttrs.version}";
+    changelog = "https://github.com/mchlnix/SMB3-Foundry/releases/tag/${finalAttrs.src.tag}";
     description = "Modern Super Mario Bros. 3 Level Editor";
     homepage = "https://github.com/mchlnix/SMB3-Foundry";
     license = lib.licenses.gpl3Only;
