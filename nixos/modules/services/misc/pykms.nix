c42d9efaa4136fa6fc9e3b7f5abe520e943e1c2d
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-01-01 00:35:10 +0800

    nixos/pykms: allow overriding default package

--- nixos/modules/services/misc/pykms.nix
+++ nixos/modules/services/misc/pykms.nix
@@ -24,6 +24,8 @@ in
         description = "Whether to enable the PyKMS service.";
       };
 
+      package = lib.mkPackageOption pkgs "pykms" { };
+
       listenAddress = lib.mkOption {
         type = lib.types.str;
         default = "0.0.0.0";
@@ -78,13 +80,13 @@ in
       wantedBy = [ "multi-user.target" ];
       # python programs with DynamicUser = true require HOME to be set
       environment.HOME = libDir;
-      serviceConfig = with pkgs; {
+      serviceConfig = {
         DynamicUser = true;
         StateDirectory = baseNameOf libDir;
-        ExecStartPre = "${lib.getBin pykms}/libexec/create_pykms_db.sh ${libDir}/clients.db";
+        ExecStartPre = "${lib.getBin cfg.package}/libexec/create_pykms_db.sh ${libDir}/clients.db";
         ExecStart = lib.concatStringsSep " " (
           [
-            "${lib.getBin pykms}/bin/server"
+            "${lib.getBin cfg.package}/bin/server"
             "--logfile=STDOUT"
             "--loglevel=${cfg.logLevel}"
             "--sqlite=${libDir}/clients.db"
