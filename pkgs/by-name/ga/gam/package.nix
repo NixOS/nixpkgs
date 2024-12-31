b2a421da81bd070c866ad187f46ad6dd7ffa94e8
Thane Gill <me@thanegill.com>
2024-12-31 14:10:23 -0800

    gam: 6.58 -> 7.02.06
    Fixes #355980

--- pkgs/by-name/ga/gam/package.nix
+++ pkgs/by-name/ga/gam/package.nix
@@ -1,20 +1,25 @@
-{
-  lib,
-  fetchFromGitHub,
-  python3,
+{ lib
+, fetchFromGitHub
+, python3
+, yubikey-manager
+, gitUpdater
+, cacert
+,
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "gam";
-  version = "6.58";
-  format = "other";
+  version = "7.02.06";
+  format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "GAM-team";
     repo = "GAM";
     rev = "refs/tags/v${version}";
-    sha256 = "sha256-AIaPzYavbBlJyi9arZN8HTmUXM7Tef0SIfE07PmV9Oo=";
+    sha256 = "sha256-NzqAj+MNuMfnj5TUF7fXxNASPZUqaHEiqbrXNQ2F5gQ=";
   };
+
+  passthru.updateScript = gitUpdater { rev-prefix = "v"; };
 
   sourceRoot = "${src.name}/src";
 
@@ -25,13 +30,14 @@ python3.pkgs.buildPythonApplication rec {
     filelock
     google-api-python-client
     google-auth
+    google-auth-httplib2
     google-auth-oauthlib
     httplib2
     lxml
     passlib
     pathvalidate
     python-dateutil
-    setuptools
+    yubikey-manager
   ];
 
   # Use XDG-ish dirs for configuration. These would otherwise be in the gam
@@ -41,27 +47,23 @@ python3.pkgs.buildPythonApplication rec {
   # at build time and then single quotes the vars in the wrapper, thus they
   # wouldn't get expanded. But using --run allows setting default vars that are
   # evaluated on run and not during build time.
+  # Detailed on this page: https://github.com/GAM-team/GAM/wiki/gam.cfg
   makeWrapperArgs = [
+    ''--set-default GAM_CA_FILE "${cacert}/etc/ssl/certs/ca-bundle.crt"''
+    ''--run 'export GAMCFGDIR="''${XDG_CONFIG_HOME:-$HOME/.config}/gam"' ''
     ''--run 'export GAMUSERCONFIGDIR="''${XDG_CONFIG_HOME:-$HOME/.config}/gam"' ''
     ''--run 'export GAMSITECONFIGDIR="''${XDG_CONFIG_HOME:-$HOME/.config}/gam"' ''
     ''--run 'export GAMCACHEDIR="''${XDG_CACHE_HOME:-$HOME/.cache}/gam"' ''
     ''--run 'export GAMDRIVEDIR="$PWD"' ''
   ];
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp gam.py $out/bin/gam
-    mkdir -p $out/${python3.sitePackages}
-    cp -r gam $out/${python3.sitePackages}
-    runHook postInstall
+  postPatch = ''
+    cp ../README.md readme.md
+    substituteInPlace setup.cfg \
+      --replace "version = attr: gam.var.GAM_VERSION" "version = ${version}" \
   '';
 
-  checkPhase = ''
-    runHook preCheck
-    ${python3.interpreter} -m unittest discover --pattern "*_test.py" --buffer
-    runHook postCheck
-  '';
+  pythonImportsCheck = [ "gam" ];
 
   meta = with lib; {
     description = "Command line management for Google Workspace";
