63e6f2f8d280bc568e9a39c17cc5696cc72ef74f
Jan Tojnar <jtojnar@gmail.com>
2024-12-31 19:20:55 +0000

    gtk4: 4.16.3 → 4.16.7
    https://gitlab.gnome.org/GNOME/gtk/-/compare/4.16.3...4.16.7
    (cherry picked from commit 8efc701451f14fa4c758b2b0e1ddf55780f87b7f)

--- pkgs/development/libraries/gtk/4.x.nix
+++ pkgs/development/libraries/gtk/4.x.nix
@@ -70,7 +70,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gtk4";
-  version = "4.16.3";
+  version = "4.16.7";
 
   outputs = [ "out" "dev" ] ++ lib.optionals x11Support [ "devdoc" ];
   outputBin = "dev";
@@ -82,7 +82,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = with finalAttrs; "mirror://gnome/sources/gtk/${lib.versions.majorMinor version}/gtk-${version}.tar.xz";
-    hash = "sha256-LsU+B9GMnwA7OeSmqDgFTZJZ4Ei2xMBdgMDQWqch2UQ=";
+    hash = "sha256-UwPHYk4VpIiAWRud3UM4mvuj3k+5KiGXGVGbsWQs49w=";
   };
 
   depsBuildBuild = [
