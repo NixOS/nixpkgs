ea0737c3e7beba4924e4209c1998679055f31c85
Pim Kunis <pim@kunis.nl>
2024-12-31 14:04:56 +0100

    prowlarr: add pizzapim to maintainers

--- pkgs/servers/prowlarr/default.nix
+++ pkgs/servers/prowlarr/default.nix
@@ -62,7 +62,7 @@ in stdenv.mkDerivation rec {
     homepage = "https://wiki.servarr.com/prowlarr";
     changelog = "https://github.com/Prowlarr/Prowlarr/releases/tag/v${version}";
     license = licenses.gpl3Only;
-    maintainers = with maintainers; [ ];
+    maintainers = with maintainers; [ pizzapim ];
     mainProgram = "Prowlarr";
     platforms = [
       "aarch64-darwin"
