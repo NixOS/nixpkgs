82e56e24c213d3f7d33a16226a27d95a0398e49c
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-31 12:30:51 +0000

    buildkite-cli: 3.4.1 -> 3.4.2

--- pkgs/by-name/bu/buildkite-cli/package.nix
+++ pkgs/by-name/bu/buildkite-cli/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "buildkite-cli";
-  version = "3.4.1";
+  version = "3.4.2";
 
   src = fetchFromGitHub {
     owner = "buildkite";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-4gg0x0Brw1BQLVZ9S3WRXnvOiwvuedj47OBhfSiRMyQ=";
+    sha256 = "sha256-Tb9Hyhyf+ib/tLUHc+gUccZ7TuXltxxjaIpCfvnoVkA=";
   };
 
-  vendorHash = "sha256-xSueBahhzD6sIFyRwvsu5ACgC9kHthDqGJ4qdNTZfPU=";
+  vendorHash = "sha256-1QXcRykXoNGYCR8+6ut1eU8TVNtx8b2O2MIjmL8rfQk=";
 
   doCheck = false;
 
