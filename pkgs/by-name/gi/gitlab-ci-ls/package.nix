934ff8e8a4f5c98a613802aacd0d026dba92bf47
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-31 21:31:37 +0000

    gitlab-ci-ls: 0.22.2 -> 1.0.0

--- pkgs/by-name/gi/gitlab-ci-ls/package.nix
+++ pkgs/by-name/gi/gitlab-ci-ls/package.nix
@@ -10,15 +10,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "gitlab-ci-ls";
-  version = "0.22.2";
+  version = "1.0.0";
   src = fetchFromGitHub {
     owner = "alesbrelih";
     repo = "gitlab-ci-ls";
     rev = "${version}";
-    hash = "sha256-xXatX0U4JBlsH1uLT9l5+FZj52zGyP9XztMa8eQ8eWk=";
+    hash = "sha256-HpFdebG4pRa+g2ycOOfnWgN/+DcYdPHAN/RHBHN6S8A=";
   };
 
-  cargoHash = "sha256-yGxty10EJXLsNV4zW89X5W1y2jY/cWpmiJ9PgOtqKCo=";
+  cargoHash = "sha256-CS3MkCumqHmTcRf/YJr/M8s7BoGos/ixXm97BCCXBvw=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs =
