NixOS/nixpkgs

commit 518ba1789aa216d697a320bf6b40defdd1c18993

antora-ui-default: 0-unstable-2024-09-20 -> 0-unstable-2024-12-26

pkgs/by-name/an/antora-ui-default/package.nix
@@ -8,12 +8,12 @@ let
     hash = "sha256-q2FwkwzjanxTIxjMpCyMpzPt782uYZiWVdZ7Eev79oM=";
     owner = "trueNAHO";
     repo = "antora-ui-default";
-    rev = "83bf9bf5f22a6dee397f8b089eb0315c14a278b5";
+    rev = "11f563294248e9b64124b9289d639e349f2e9f5f";
   };
 in
 stdenvNoCC.mkDerivation {
   pname = "antora-ui-default";
-  version = "0-unstable-2024-09-20";
+  version = "0-unstable-2024-12-26";
 
   # The UI bundle is fetched from lib.maintainers.naho's antora-ui-default fork
   # for the following reasons:
@@ -37,7 +37,7 @@ stdenvNoCC.mkDerivation {
   #
   # For reference, the UI bundle from [3] is 300K large.
   #
-  # [3]: https://gitlab.com/trueNAHO/antora-ui-default/-/commit/83bf9bf5f22a6dee397f8b089eb0315c14a278b5
+  # [3]: https://gitlab.com/trueNAHO/antora-ui-default/-/commit/11f563294248e9b64124b9289d639e349f2e9f5f
   src = fetchFromGitLab srcFetchFromGitLab;
 
   phases = [ "installPhase" ];

commit 6fba45b6e5a3130eb8f37250bc5f7d012ad09856

antora-ui-default: update hash
Fixes: 518ba1789aa2 ("antora-ui-default: 0-unstable-2024-09-20 -> 0-unstable-2024-12-26")

pkgs/by-name/an/antora-ui-default/package.nix
@@ -5,7 +5,7 @@
 }:
 let
   srcFetchFromGitLab = {
-    hash = "sha256-q2FwkwzjanxTIxjMpCyMpzPt782uYZiWVdZ7Eev79oM=";
+    hash = "sha256-gUQLLjnWZ1OsAe005IOPIfoM0qmjoevcUuGBRD3oHXA=";
     owner = "trueNAHO";
     repo = "antora-ui-default";
     rev = "11f563294248e9b64124b9289d639e349f2e9f5f";