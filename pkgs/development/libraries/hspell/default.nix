ad3be3261e5f71d2e6755f845721d04f733a3c53
Jan Tojnar <jtojnar@gmail.com>
2024-12-31 08:49:04 +0100

    hspell: Add zlib
    As mentioned by the configure script output, hspell will fall back
    on using `gzip` program when not linked against `zlib`:
    
        No Zlib library, defaulting to using pipes
    
    It has been removed in 67f0a216e5c8cec5a1d1787c57dbf8b4fb56ccf9 for some reason.
    
    Without this patch, Geary is stuck loading messages, even though I can interact
    with the rest of its UI. I am also getting the following in the log so broken
    piping might be breaking webkitgtk for some reason:
    
        Hspell: can't run gzip -dc '/nix/store/14qk9r9ywvpqn0a24fvw1iwjv302d04j-hspell-1.4/share/hspell/hebrew.wgz.prefixes'.
        Could not create GBM EGL display: EGL_NOT_INITIALIZED. Aborting...
    
    And after quitting Geary:
    
        *[wrn] 18:29:13.0226 geary:application-client.vala:464: Forcing shutdown of Geary, 7s passed...
        gzip:
        gzip: stdout: Broken pipe
        stdout: Broken pipe
    
    Co-authored-by: Colin <colin@uninsane.org>

--- pkgs/development/libraries/hspell/default.nix
+++ pkgs/development/libraries/hspell/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   patches = [ ./remove-shared-library-checks.patch ];
   postPatch = "patchShebangs .";
   preBuild = lib.optionalString (stdenv.hostPlatform != stdenv.buildPlatform) ''
-    make CC=${buildPackages.stdenv.cc}/bin/cc find_sizes
+    make CC='${buildPackages.stdenv.cc}/bin/cc -I${lib.getDev buildPackages.zlib}/include -L${buildPackages.zlib}/lib' find_sizes
     mv find_sizes find_sizes_build
     make clean
 
@@ -41,7 +41,10 @@ stdenv.mkDerivation rec {
     perl
     zlib
   ];
-  buildInputs = [ perl ];
+  buildInputs = [
+    perl
+    zlib
+  ];
 
   strictDeps = true;
 
