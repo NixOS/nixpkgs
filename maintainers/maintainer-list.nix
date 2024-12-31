e7125d678fe955ecf8808daa34e15bcf4c238e5c
Kaushal M <kaushal.m@proton.me>
2024-12-31 07:24:33 +0530

    cyphernetes: init at v0.15.3

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12608,6 +12608,11 @@
     github = "krzaczek";
     githubId = 5773701;
   };
+  kshlm = {
+    name = "kshlm";
+    github = "kshlm";
+    githubId = 362086;
+  };
   KSJ2000 = {
     email = "katsho123@outlook.com";
     name = "KSJ2000";
@@ -22104,6 +22109,12 @@
     github = "fehrenbach";
     githubId = 203168;
     name = "Stefan Fehrenbach";
+  };
+  stefankeidel = {
+    email = "stefan@keidel.me";
+    github = "stefankeidel";
+    githubId = 1188614;
+    name = "Stefan Keidel";
   };
   stehessel = {
     email = "stephan@stehessel.de";
