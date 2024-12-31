f822a9705e800d1f7d10fbc249c5bd63d4727fc1
R. Ryantm <ryantm-bot@ryantm.com>
2024-12-31 18:16:23 +0000

    libretro.nestopia: 0-unstable-2024-12-14 -> 0-unstable-2024-12-22

--- pkgs/applications/emulators/libretro/cores/nestopia.nix
+++ pkgs/applications/emulators/libretro/cores/nestopia.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "nestopia";
-  version = "0-unstable-2024-12-14";
+  version = "0-unstable-2024-12-22";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "nestopia";
-    rev = "e7b65504ffc7f14fc5c74954d02b18f44c3aaf43";
-    hash = "sha256-WI/EPWfTmcPM8SSVroqlya6U5cEgmesSJGbPOjxCSUg=";
+    rev = "6bbfff9a56ead67f0da696ab2c3aea3c11896964";
+    hash = "sha256-D2FtfabikcZq0dl+ot/NJJkOaQXj0Sl5P2ioNrvxgSs=";
   };
 
   makefile = "Makefile";
