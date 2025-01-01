2f9a5661ef5f6ccc3af57abbad07720b693a770c
natsukium <tomoya.otabi@gmail.com>
2025-01-01 12:01:30 +0900

    python312Packages.scikits-odes-core: init at 3.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14456,6 +14456,8 @@ self: super: with self; {
 
   scikits-odes = callPackage ../development/python-modules/scikits-odes { };
 
+  scikits-odes-core = callPackage ../development/python-modules/scikits-odes-core { };
+
   scikit-tda = callPackage ../development/python-modules/scikit-tda { };
 
   scipy = callPackage ../development/python-modules/scipy { };
