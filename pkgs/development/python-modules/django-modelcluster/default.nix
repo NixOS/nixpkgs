97d8d573562f6247a880e42043cb92ffe782e164
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-01 16:35:54 +0100

    python312Packages.django-modelcluster: drop support for python 3.8

--- pkgs/development/python-modules/django-modelcluster/default.nix
+++ pkgs/development/python-modules/django-modelcluster/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "6.4";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "wagtail";
