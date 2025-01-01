11b2466fcd17d06e217937af76f3bbc3a1251424
ProxyVT <tikit.us@outlook.com>
2025-01-01 17:38:42 +0300

    linuxPackages.nvidiaPackages.legacy_470: add patches for compatibility with kernel 6.12 or older

--- pkgs/os-specific/linux/nvidia-x11/default.nix
+++ pkgs/os-specific/linux/nvidia-x11/default.nix
@@ -151,6 +151,11 @@ rec {
         hash = "sha256-XcPjyvKRLU5W88szWEqiWfJHFtm1IGr9jBzyt9qtdWQ=";
       };
       patchset = [
+        "0001-Fix-conftest-to-ignore-implicit-function-declaration.patch"
+        "0002-Fix-conftest-to-use-a-short-wchar_t.patch"
+        "0003-Fix-conftest-to-use-nv_drm_gem_vmap-which-has-the-se.patch"
+        "kernel-6.10.patch"
+        "kernel-6.12.patch"
       ];
     in
     generic {
@@ -160,6 +165,8 @@ rec {
       settingsSha256 = "sha256-kftQ4JB0iSlE8r/Ze/+UMnwLzn0nfQtqYXBj+t6Aguk=";
       persistencedSha256 = "sha256-iYoSib9VEdwjOPBP1+Hx5wCIMhW8q8cCHu9PULWfnyQ=";
 
+      prePatch = "pushd kernel";
+      postPatch = "popd";
       patches = (map (patch: "${aurPatches}/${patch}") patchset);
     };
 
