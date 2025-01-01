1a77b3b360f6f0dc2ef891ebcb58166e0fed88c9
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-01 10:03:12 +0100

    python312Packages.aiorussound: 4.3.0 -> 4.4.0
    Diff: https://github.com/noahhusby/aiorussound/compare/refs/tags/4.3.0...4.4.0
    
    Changelog: https://github.com/noahhusby/aiorussound/releases/tag/4.4.0

--- pkgs/development/python-modules/aiorussound/default.nix
+++ pkgs/development/python-modules/aiorussound/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "aiorussound";
-  version = "4.3.0";
+  version = "4.4.0";
   pyproject = true;
 
   # requires newer f-strings introduced in 3.12
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "noahhusby";
     repo = "aiorussound";
     rev = "refs/tags/${version}";
-    hash = "sha256-qHplUG9kzpWOxwDq200LBLiX+VCZfhGpJfGCRd4KdGg=";
+    hash = "sha256-y1ArQ4o9RRngJFT3uk3wO/EVZu3kWYXUx2pnTSFUruc=";
   };
 
   build-system = [ poetry-core ];
