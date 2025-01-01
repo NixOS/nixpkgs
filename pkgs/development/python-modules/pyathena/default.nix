0540153fd551d94c656657d7cfb96be0a11b36de
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-01 16:19:40 +0100

    python312Packages.pyathena: drop support for python 3.8
    Co-authored-by: Nick Cao <nickcao@nichi.co>

--- pkgs/development/python-modules/pyathena/default.nix
+++ pkgs/development/python-modules/pyathena/default.nix
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   version = "3.12.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
