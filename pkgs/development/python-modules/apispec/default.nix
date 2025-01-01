76fe7e071a784f6cdb90a1d056e622c19d58523d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-01 19:19:26 +0100

    python312Packages.apispec: drop support for python 3.8
    Co-authored-by: Nick Cao <nickcao@nichi.co>

--- pkgs/development/python-modules/apispec/default.nix
+++ pkgs/development/python-modules/apispec/default.nix
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   version = "6.8.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
