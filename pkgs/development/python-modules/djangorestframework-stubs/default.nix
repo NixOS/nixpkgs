ec16462f08fd6f1ee6983bc5996058e2dccdea0d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-01 05:02:32 +0100

    python312Packages.djangorestframework-stubs: drop support for python 3.8
    Co-authored-by: Nick Cao <nickcao@nichi.co>

--- pkgs/development/python-modules/djangorestframework-stubs/default.nix
+++ pkgs/development/python-modules/djangorestframework-stubs/default.nix
@@ -22,7 +22,7 @@ buildPythonPackage rec {
   version = "3.15.2";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "typeddjango";
