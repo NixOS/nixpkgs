ad71a8ac5c35d84214f6b10ee2da6f6543358d21
natsukium <tomoya.otabi@gmail.com>
2025-01-01 13:46:05 +0900

    python312Packages.keyrings-google-artifactregistry-auth: refactor

--- pkgs/development/python-modules/keyrings-google-artifactregistry-auth/default.nix
+++ pkgs/development/python-modules/keyrings-google-artifactregistry-auth/default.nix
@@ -5,31 +5,28 @@
   google-auth,
   keyring,
   pluggy,
-  pythonOlder,
   requests,
+  setuptools,
   setuptools-scm,
-  toml,
 }:
 
 buildPythonPackage rec {
-  pname = "keyrings.google-artifactregistry-auth";
+  pname = "keyrings-google-artifactregistry-auth";
   version = "1.1.2";
-
-  disabled = pythonOlder "3.6";
-
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "keyrings.google-artifactregistry-auth";
+    inherit version;
     hash = "sha256-vWq7cnQNLf60pcA8OxBcb326FpyqKd7jlZaU8fAsd94=";
   };
 
-  buildInputs = [
+  build-system = [
+    setuptools
     setuptools-scm
-    toml
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     google-auth
     keyring
     pluggy
@@ -41,11 +38,11 @@ buildPythonPackage rec {
   # upstream has no tests
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     changelog = "https://github.com/GoogleCloudPlatform/artifact-registry-python-tools/blob/main/HISTORY.md";
     description = "Python package which allows you to configure keyring to interact with Python repositories stored in Artifact Registry";
-    homepage = "https://pypi.org/project/keyrings.google-artifactregistry-auth";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ lovesegfault ];
+    homepage = "https://github.com/GoogleCloudPlatform/artifact-registry-python-tools";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ lovesegfault ];
   };
 }
