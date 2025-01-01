NixOS/nixpkgs

commit 3e2ceab20157c37566df2f2ea35d4a6dac95732c

boost: fix build for non x86_64 hostPlatforms

pkgs/development/libraries/boost/generic.nix
@@ -64,6 +64,16 @@ let
     "link=${link}"
     "-sEXPAT_INCLUDE=${expat.dev}/include"
     "-sEXPAT_LIBPATH=${expat.out}/lib"
+    (
+      # The stacktrace from exception feature causes memory leaks when built
+      # with libc++. For all other standard library implementations, i.e.
+      # libstdc++, we must aknowledge this or stacktrace refuses to compile.
+      # Issue upstream: https://github.com/boostorg/stacktrace/issues/163
+      if (stdenv.cc.libcxx != null) then
+        "boost.stacktrace.from_exception=off"
+      else
+        "define=BOOST_STACKTRACE_LIBCXX_RUNTIME_MAY_CAUSE_MEMORY_LEAK"
+    )
 
     # TODO: make this unconditional
   ] ++ lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform ||

commit 45e2426453f3e43271fae4b2e1b94adfb995b44c

boost{177,178,179,180,181,182,183}: conditionalize stacktrace flag
This library was introduced in Boost 1.85.

Fixes: 3e2ceab20157c37566df2f2ea35d4a6dac95732c

pkgs/development/libraries/boost/generic.nix
@@ -89,6 +89,8 @@ let
       "link=${link}"
       "-sEXPAT_INCLUDE=${expat.dev}/include"
       "-sEXPAT_LIBPATH=${expat.out}/lib"
+    ]
+    ++ lib.optionals (lib.versionAtLeast version "1.85") [
       (
         # The stacktrace from exception feature causes memory leaks when built
         # with libc++. For all other standard library implementations, i.e.
@@ -99,9 +101,8 @@ let
         else
           "define=BOOST_STACKTRACE_LIBCXX_RUNTIME_MAY_CAUSE_MEMORY_LEAK"
       )
-
-      # TODO: make this unconditional
     ]
+    # TODO: make this unconditional
     ++
       lib.optionals
         (