699c165503c78cf649b95a0b015dcb76428a1592
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-02 10:23:29 +0000

    factoriolab: 3.8.9 -> 3.8.10

--- pkgs/by-name/fa/factoriolab/package.nix
+++ pkgs/by-name/fa/factoriolab/package.nix
@@ -10,13 +10,13 @@
 }:
 buildNpmPackage rec {
   pname = "factoriolab";
-  version = "3.8.9";
+  version = "3.8.10";
 
   src = fetchFromGitHub {
     owner = "factoriolab";
     repo = "factoriolab";
     rev = "refs/tags/v${version}";
-    hash = "sha256-H+l/eDdTc/IMqE85Xh4IAo2ZWZRYKKUnH3rXkON2L1s=";
+    hash = "sha256-vwcqUsqlggmqqlQ3fyn+s3NKQ0oNcVztfjTpJPW1mFs=";
   };
   buildInputs = [ vips ];
   nativeBuildInputs = [
