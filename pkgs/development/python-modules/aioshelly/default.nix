4d23fffeea931080063c7102a7bc8cf29cc72aa9
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-02 23:58:00 +0100

    python313Packages.aioshelly: 12.1.0 -> 12.2.0
    https://github.com/home-assistant-libs/aioshelly/releases/tag/12.2.0

--- pkgs/development/python-modules/aioshelly/default.nix
+++ pkgs/development/python-modules/aioshelly/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "aioshelly";
-  version = "12.1.0";
+  version = "12.2.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "home-assistant-libs";
     repo = "aioshelly";
     tag = version;
-    hash = "sha256-QcVyWabELa1bB2MOwQNPItXLgeOXlFpRh69dS+m1FLI=";
+    hash = "sha256-Y6gFFo0kzdHGaRqNVHhr0vvWBwRO2kOaoR4BFUbyYUQ=";
   };
 
   build-system = [ setuptools ];
