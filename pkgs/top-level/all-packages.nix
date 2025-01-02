7f99289d0f30c5f237096575bbe57988c7389a9e
usertam <heiyiutam@gmail.com>
2025-01-03 00:47:55 +0800

    freeimage: drop autoSignDarwinBinariesHook

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8964,9 +8964,7 @@ with pkgs;
   fplll = callPackage ../development/libraries/fplll { };
   fplll_20160331 = callPackage ../development/libraries/fplll/20160331.nix { };
 
-  freeimage = callPackage ../development/libraries/freeimage {
-    inherit (darwin) autoSignDarwinBinariesHook;
-  };
+  freeimage = callPackage ../development/libraries/freeimage { };
 
   freeipa = callPackage ../os-specific/linux/freeipa {
     kerberos = krb5.override {
