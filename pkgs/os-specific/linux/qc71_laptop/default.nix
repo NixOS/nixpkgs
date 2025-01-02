c0702131d0b6be4b1010083eac909046eef2ddfa
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-01-02 20:48:56 +0000

    qc71_laptop: 2023-03-02 -> 2025-01-07

--- pkgs/os-specific/linux/qc71_laptop/default.nix
+++ pkgs/os-specific/linux/qc71_laptop/default.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "qc71_laptop";
-  version = "unstable-2023-03-02";
+  version = "0-unstable-2025-01-07";
 
   src = fetchFromGitHub {
     owner = "pobrn";
     repo = "qc71_laptop";
-    rev = "8805dc5639f6659addf153a295ad4bbaa2483fa3";
-    hash = "sha256-wg7APGArjrl9DEAHTG6BknOBx+UbtNrzziwmLueKPfA=";
+    rev = "ebab4af0b2c5b162bb9f27c80cd284c36b8fb7a9";
+    hash = "sha256-sRvxcdocYKnwMH/qYkKj66uClI1bSmMSxXHrHsc7uco=";
   };
 
   nativeBuildInputs = kernel.moduleBuildDependencies;
