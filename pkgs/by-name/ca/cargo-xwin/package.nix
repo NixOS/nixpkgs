a0162035a92fafd284a4f07548a9c61d76d33638
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-02 23:19:05 +0000

    cargo-xwin: 0.18.2 -> 0.18.3

--- pkgs/by-name/ca/cargo-xwin/package.nix
+++ pkgs/by-name/ca/cargo-xwin/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-xwin";
-  version = "0.18.2";
+  version = "0.18.3";
 
   src = fetchFromGitHub {
     owner = "rust-cross";
     repo = "cargo-xwin";
     rev = "v${version}";
-    hash = "sha256-bK0bEaJaPTXpwLeDCS4kwf4S0mLP6MrI8BxzOM9ha8M=";
+    hash = "sha256-JwN0eUtj2qa5Hdl4aeDr9FtEpTB/Y36/q8ValfuvN/A=";
   };
 
-  cargoHash = "sha256-bDnkSN3rt23j3EZRJdPLtYzltSvRGyYV+rlVAZPzjS4=";
+  cargoHash = "sha256-pHy5sImtaN5MzkewygFbPK1yFLRmHrzPDa7NOoUoA5M=";
 
   meta = with lib; {
     description = "Cross compile Cargo project to Windows MSVC target with ease";
