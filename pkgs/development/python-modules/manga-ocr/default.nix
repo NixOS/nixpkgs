3792c41da804209b4f3fb0eff400d83decae0b83
Euan Kemp <euank@euank.com>
2025-01-02 14:56:36 +0900

    python3Packages.manga-ocr: 0.1.13 -> 0.1.14
    The numpy -> numpy2 change we added at build-time got incorperated
    upstream and released as a new version already, so switch to that.
    
    I'm doing this by hand rather than relying on the dependency bot because
    we want to remove the postPatch I just added as well.

--- pkgs/development/python-modules/manga-ocr/default.nix
+++ pkgs/development/python-modules/manga-ocr/default.nix
@@ -8,15 +8,14 @@ with python3Packages;
 
 buildPythonPackage rec {
   pname = "manga-ocr";
-  version = "0.1.13";
-  disabled = pythonOlder "3.7";
+  version = "0.1.14";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "kha-white";
     repo = "manga-ocr";
     tag = "v${version}";
-    hash = "sha256-0EwXDMnA9SCmSsMVXnMenSFSzs74lorFNNym9y/NNsI=";
+    hash = "sha256-fCLgFeo6GYPSpCX229TK2MXTKt3p1tQV06phZYD6UeE=";
   };
 
   build-system = [
@@ -37,15 +36,11 @@ buildPythonPackage rec {
     unidic-lite
   ];
 
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace-fail "numpy<2" "numpy"
-  '';
-
   meta = with lib; {
+    mainProgram = "manga_ocr";
     description = "Optical character recognition for Japanese text, with the main focus being Japanese manga";
     homepage = "https://github.com/kha-white/manga-ocr";
-    changelog = "https://github.com/kha-white/manga-ocr/releases/tag/${version}";
+    changelog = "https://github.com/kha-white/manga-ocr/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ laurent-f1z1 ];
   };
