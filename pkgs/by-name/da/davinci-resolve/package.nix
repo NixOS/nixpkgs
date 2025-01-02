74413ffa79fd29e72cbc33fc1cc81c4969683870
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-02 14:52:30 +0000

    davinci-resolve: 19.1.1 -> 19.1.2

--- pkgs/by-name/da/davinci-resolve/package.nix
+++ pkgs/by-name/da/davinci-resolve/package.nix
@@ -35,7 +35,7 @@ let
   davinci = (
     stdenv.mkDerivation rec {
       pname = "davinci-resolve${lib.optionalString studioVariant "-studio"}";
-      version = "19.1.1";
+      version = "19.1.2";
 
       nativeBuildInputs = [
         (appimage-run.override { buildFHSEnv = buildFHSEnvChroot; })
@@ -57,9 +57,9 @@ let
             outputHashAlgo = "sha256";
             outputHash =
               if studioVariant then
-                "sha256-yDkw7L9iTzE5g2XAw910nqtTyXcgcb6u+kP2sKtNcJ8="
+                "sha256-yLlvl/zm/epNs1e4sSDrfHNApcpPQV2fMF3Y09ml7is="
               else
-                "sha256-8oq5Nj8GrdHAmbM6Z5q98a0Lazjymk3dOCITkKiTJvQ=";
+                "sha256-ssX1ffB7ZSJObETQa2r5R4oHddAccC4GB14jaLs8bGg=";
 
             impureEnvVars = lib.fetchers.proxyImpureEnvVars;
 
