76279f35bf801c717a7c8ee74632a16c038e27bd
Peter H. Hoeg <peter@hoeg.com>
2025-01-02 21:27:19 +0100

    ncftp: 3.2.6 -> 3.2.7

--- pkgs/by-name/nc/ncftp/package.nix
+++ pkgs/by-name/nc/ncftp/package.nix
@@ -6,31 +6,29 @@
   coreutils,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "ncftp";
-  version = "3.2.6";
+  version = "3.2.7";
 
   src = fetchurl {
-    url = "ftp://ftp.ncftp.com/ncftp/ncftp-${version}-src.tar.xz";
-    sha256 = "1389657cwgw5a3kljnqmhvfh4vr2gcr71dwz1mlhf22xq23hc82z";
+    url = "https://www.ncftp.com/public_ftp/ncftp/ncftp-${finalAttrs.version}-src.tar.xz";
+    hash = "sha256-1BxcTWYUqOri7U5NetprbTr8yftlpO2bhxE0S+8k9+g=";
   };
 
   buildInputs = [ ncurses ];
 
   enableParallelBuilding = true;
 
-  # Workaround build failure on -fno-common toolchains like upstream
-  # gcc-10. Otherwise build fails as:
-  #   ld: bookmark.o: (.bss+0x20): multiple definition of `gBm';
-  #     gpshare.o:(.bss+0x0): first defined here
-  env.NIX_CFLAGS_COMPILE = toString (
-    [ "-fcommon" ]
-    # these are required for the configure script to work with clang
-    ++ lib.optionals stdenv.hostPlatform.isDarwin [
-      "-Wno-implicit-int"
-      "-Wno-implicit-function-declaration"
-    ]
-  );
+  env.NIX_CFLAGS_COMPILE = toString [
+    # Workaround build failure on -fno-common toolchains like upstream
+    # gcc-10. Otherwise build fails as:
+    #   ld: bookmark.o: (.bss+0x20): multiple definition of `gBm';
+    #     gpshare.o:(.bss+0x0): first defined here
+    "-fcommon"
+    # these are required for the configure script to work with clang and gcc14
+    "-Wno-implicit-int"
+    "-Wno-implicit-function-declaration"
+  ];
 
   preConfigure = ''
     find -name Makefile.in | xargs sed -i '/^TMPDIR=/d'
@@ -57,5 +55,6 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ bjornfor ];
     platforms = platforms.unix;
     license = licenses.clArtistic;
+    mainProgram = "ncftp";
   };
-}
+})
