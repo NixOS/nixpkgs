f556a41d0d6ede0eccd08cdb22e6d7ea177e2f5b
FliegendeWurst <arne.keller@posteo.de>
2025-01-02 22:40:29 +0100

    tk-8_5: fix build with GCC 14

--- pkgs/development/interpreters/tcl/generic.nix
+++ pkgs/development/interpreters/tcl/generic.nix
@@ -12,6 +12,7 @@
   release,
   version,
   src,
+  extraPatch ? "",
   ...
 }:
 
@@ -27,13 +28,15 @@ let
 
     setOutputFlags = false;
 
-    postPatch = ''
-      substituteInPlace library/clock.tcl \
-        --replace "/usr/share/zoneinfo" "${tzdata}/share/zoneinfo" \
-        --replace "/usr/share/lib/zoneinfo" "" \
-        --replace "/usr/lib/zoneinfo" "" \
-        --replace "/usr/local/etc/zoneinfo" ""
-    '';
+    postPatch =
+      ''
+        substituteInPlace library/clock.tcl \
+          --replace "/usr/share/zoneinfo" "${tzdata}/share/zoneinfo" \
+          --replace "/usr/share/lib/zoneinfo" "" \
+          --replace "/usr/lib/zoneinfo" "" \
+          --replace "/usr/local/etc/zoneinfo" ""
+      ''
+      + extraPatch;
 
     nativeBuildInputs = lib.optionals (lib.versionAtLeast version "9.0") [
       # Only used to detect the presence of zlib. Could be replaced with a stub.
