9f2f5463f5451fc73f868d39c356fd500f490e11
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2025-01-02 15:55:31 -0500

    maintainers: add tebriel

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22181,6 +22181,12 @@
     github = "teatwig";
     githubId = 18734648;
   };
+  tebriel = {
+    email = "tebriel@frodux.in";
+    name = "tebriel";
+    github = "tebriel";
+    githubId = 821688;
+  };
   techknowlogick = {
     email = "techknowlogick@gitea.com";
     github = "techknowlogick";
