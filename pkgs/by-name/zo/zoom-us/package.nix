916cfc6b8a0db809710913dbab700f15ad41fa93
Philip Taron <philip.taron@gmail.com>
2025-01-02 10:13:42 -0800

    zoom-us: 6.3.0.* -> 6.3.1.*

--- pkgs/by-name/zo/zoom-us/package.nix
+++ pkgs/by-name/zo/zoom-us/package.nix
@@ -50,23 +50,23 @@ let
   # and often with different versions.  We write them on three lines
   # like this (rather than using {}) so that the updater script can
   # find where to edit them.
-  versions.aarch64-darwin = "6.3.0.44805";
-  versions.x86_64-darwin = "6.3.0.44805";
-  versions.x86_64-linux = "6.3.0.5527";
+  versions.aarch64-darwin = "6.3.1.45300";
+  versions.x86_64-darwin = "6.3.1.45300";
+  versions.x86_64-linux = "6.3.1.5673";
 
   srcs = {
     aarch64-darwin = fetchurl {
       url = "https://zoom.us/client/${versions.aarch64-darwin}/zoomusInstallerFull.pkg?archType=arm64";
       name = "zoomusInstallerFull.pkg";
-      hash = "sha256-S4uBJw0FPcPv1O2zcTbEaEONrNLkRCkWYazpR5chYbE=";
+      hash = "sha256-WPhqYof/XR6TDkuA4NK2a30ksdhN7NBfs4KCQwqKJ0g=";
     };
     x86_64-darwin = fetchurl {
       url = "https://zoom.us/client/${versions.x86_64-darwin}/zoomusInstallerFull.pkg";
-      hash = "sha256-PKIxhy+bnBpMn0oUL1cAfhOT+osXPDBIRoY1HLgO26s=";
+      hash = "sha256-BywBvJCcNXARHTkO/UJbOFRjuiXRkmFWmSuZsW9t1pk=";
     };
     x86_64-linux = fetchurl {
       url = "https://zoom.us/client/${versions.x86_64-linux}/zoom_x86_64.pkg.tar.xz";
-      hash = "sha256-5Tk8lU6D0cgyr5pe1oVZFCURQ1YznBpWPPM9nogOT6Q=";
+      hash = "sha256-0gREcRnGkiFj6kIra0laR5PUqrb0Jvoj6gDfpAVLRtg=";
     };
   };
 
