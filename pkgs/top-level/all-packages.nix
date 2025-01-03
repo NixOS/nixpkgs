018d2967996cf3b913b7b5d3b20ebbec60a8d1b7
sanana <umutinanerdogan@pm.me>
2025-01-03 18:12:41 +0300

    lib.systems: add mlibc as a libc option

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8010,6 +8010,8 @@ with pkgs;
       newlib-nano
     else if libc == "musl" then
       musl
+    else if libc == "mlibc" then
+      mlibc
     else if libc == "msvcrt" then
       if stdenv.hostPlatform.isMinGW then windows.mingw_w64 else windows.sdk
     else if libc == "ucrt" then
