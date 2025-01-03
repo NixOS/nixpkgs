35c85e8e6cb50246d2b38082977bca5b9e97e7c3
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-03 01:42:40 +0100

    python313Packages.uiprotect: 6.6.5 -> 7.1.0
    https://github.com/uilibs/uiprotect/blob/refs/tags/v7.1.0/CHANGELOG.md

--- pkgs/development/python-modules/uiprotect/default.nix
+++ pkgs/development/python-modules/uiprotect/default.nix
@@ -20,6 +20,7 @@
   platformdirs,
   propcache,
   pydantic,
+  pydantic-extra-types,
   pyjwt,
   rich,
   typer,
@@ -39,7 +40,7 @@
 
 buildPythonPackage rec {
   pname = "uiprotect";
-  version = "6.6.5";
+  version = "7.1.0";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -48,7 +49,7 @@ buildPythonPackage rec {
     owner = "uilibs";
     repo = "uiprotect";
     tag = "v${version}";
-    hash = "sha256-ZohQTXOLc2E0vfD21IUh6ECTfbAd2SZOg/73lk/UMO0=";
+    hash = "sha256-TALjJmTI5lMXyXcJtfyMVpqLo8dBbENG22EX7B+3QKQ=";
   };
 
   build-system = [ poetry-core ];
@@ -71,6 +72,7 @@ buildPythonPackage rec {
     platformdirs
     propcache
     pydantic
+    pydantic-extra-types
     pyjwt
     rich
     typer
