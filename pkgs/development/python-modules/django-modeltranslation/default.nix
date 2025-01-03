589a4e88742464f10ff9673d53058412785aea9a
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-03 05:29:31 +0000

    python312Packages.django-modeltranslation: 0.19.11 -> 0.19.12

--- pkgs/development/python-modules/django-modeltranslation/default.nix
+++ pkgs/development/python-modules/django-modeltranslation/default.nix
@@ -13,7 +13,7 @@
 let
   # 0.18.12 was yanked from PyPI, it refers to this issue:
   # https://github.com/deschler/django-modeltranslation/issues/701
-  version = "0.19.11";
+  version = "0.19.12";
 in
 buildPythonPackage {
   pname = "django-modeltranslation";
@@ -23,7 +23,7 @@ buildPythonPackage {
     owner = "deschler";
     repo = "django-modeltranslation";
     tag = "v${version}";
-    hash = "sha256-J/D0rHhxJMpOSGu9LQ6tPKnjBZhqTX6I5YcSkx5+qXk=";
+    hash = "sha256-j5IKAjGes1EUNX9XE1RAPkGJGviABa4VFl789Mj2dyc=";
   };
 
   disabled = pythonOlder "3.6";
