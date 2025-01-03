885468cd3840c91f4cb1f00f92d8c7ecdfb072fe
Pascal Dietrich <pascal.1.dietrich@hotmail.com>
2025-01-03 14:28:48 +0100

    Update EmptyEpsilon to 2024.12.08

--- pkgs/by-name/em/empty-epsilon/package.nix
+++ pkgs/by-name/em/empty-epsilon/package.nix
@@ -15,8 +15,8 @@
 
 let
   version = {
-    seriousproton = "2024.08.09";
-    emptyepsilon = "2024.08.09";
+    seriousproton = "2024.12.08";
+    emptyepsilon = "2024.12.08";
     basis-universal = "v1_15_update2";
   };
 
@@ -35,7 +35,7 @@ let
       owner = "daid";
       repo = "SeriousProton";
       rev = "EE-${version.seriousproton}";
-      hash = "sha256-B7BUe5rtN/eABJwkuSyn+h1lIHuV/tZUNcGXTyaWIr4=";
+      hash = "sha256-k1YCB7EJIL+kdlHEU4cJjmLZZAZyxIPU0XlSn2t4C90=";
     };
 
     nativeBuildInputs = [ cmake ];
@@ -70,7 +70,7 @@ stdenv.mkDerivation {
     owner = "daid";
     repo = "EmptyEpsilon";
     rev = "EE-${version.emptyepsilon}";
-    hash = "sha256-DxaasUyJa8n0ha8RqAfEnqCVELs5Or0zvIOgcK75TnU=";
+    hash = "sha256-JsHFwbt4VGsgaZz9uxEmwzZGfkYTNsIZTKkpvCCmI48=";
   };
 
   nativeBuildInputs = [ cmake ];
