765ff758f7889d19127c1d8203392936ff193dce
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-03 09:54:09 +0000

    python312Packages.pyvesync: 2.1.12 -> 2.1.15

--- pkgs/development/python-modules/pyvesync/default.nix
+++ pkgs/development/python-modules/pyvesync/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "pyvesync";
-  version = "2.1.12";
+  version = "2.1.15";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-k9eAbHFFo5rFYTNVxN4qEFnW2zmXThr3Ki1lZtj9V/g=";
+    hash = "sha256-iXahtcD5jKP5hRtXxtgA3x0+zxaWu1zo/QrWfFEmB0w=";
   };
 
   propagatedBuildInputs = [ requests ];
