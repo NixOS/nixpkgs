65695338ee291128542f3905260510756fdf9d8f
Connor Baker <ConnorBaker01@gmail.com>
2025-01-03 20:42:52 +0000

    ucc: general fixups

--- pkgs/by-name/uc/ucc/package.nix
+++ pkgs/by-name/uc/ucc/package.nix
@@ -1,28 +1,45 @@
 {
-  stdenv,
-  lib,
-  fetchFromGitHub,
-  libtool,
-  automake,
   autoconf,
-  ucx,
+  automake,
   config,
-  enableCuda ? config.cudaSupport,
   cudaPackages,
+  fetchFromGitHub,
+  lib,
+  libtool,
+  stdenv,
+  ucx,
+  # Configuration options
   enableAvx ? stdenv.hostPlatform.avxSupport,
+  enableCuda ? config.cudaSupport,
   enableSse41 ? stdenv.hostPlatform.sse4_1Support,
   enableSse42 ? stdenv.hostPlatform.sse4_2Support,
 }:
+let
+  inherit (lib.lists) optionals;
+  inherit (lib.strings) concatStringsSep;
 
-stdenv.mkDerivation rec {
+  inherit (cudaPackages)
+    cuda_cccl
+    cuda_cudart
+    cuda_nvcc
+    cudaFlags
+    ;
+in
+stdenv.mkDerivation (finalAttrs: {
+  __structuredAttrs = true;
+  # TODO: When strictDeps is enabled, the CUDA build fails during configurePhase because it can't find all the CUDA
+  # dependencies. As such, we hold off on enabling strictDeps until CUDA compilation works.
+  # https://github.com/openucx/ucc/blob/0c0fc21559835044ab107199e334f7157d6a0d3d/config/m4/cuda.m4
+  strictDeps = false;
+
   pname = "ucc";
   version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "openucx";
     repo = "ucc";
-    rev = "v${version}";
-    sha256 = "sha256-xcJLYktkxNK2ewWRgm8zH/dMaIoI+9JexuswXi7MpAU=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-xcJLYktkxNK2ewWRgm8zH/dMaIoI+9JexuswXi7MpAU=";
   };
 
   outputs = [
@@ -32,44 +49,46 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
+  # NOTE: We use --replace-quiet because not all Makefile.am files contain /bin/bash.
   postPatch = ''
-
     for comp in $(find src/components -name Makefile.am); do
-      substituteInPlace $comp \
-        --replace "/bin/bash" "${stdenv.shell}"
+      substituteInPlace "$comp" \
+        --replace-quiet \
+          "/bin/bash" \
+          "${stdenv.shell}"
     done
   '';
 
   nativeBuildInputs = [
-    libtool
-    automake
     autoconf
-  ] ++ lib.optionals enableCuda [ cudaPackages.cuda_nvcc ];
+    automake
+    libtool
+  ] ++ optionals enableCuda [ cuda_nvcc ];
+
   buildInputs =
     [ ucx ]
-    ++ lib.optionals enableCuda [
-      cudaPackages.cuda_cccl
-      cudaPackages.cuda_cudart
+    ++ optionals enableCuda [
+      cuda_cccl
+      cuda_cudart
     ];
 
-  preConfigure =
-    ''
-      ./autogen.sh
-    ''
-    + lib.optionalString enableCuda ''
-      configureFlagsArray+=( "--with-nvcc-gencode=${builtins.concatStringsSep " " cudaPackages.cudaFlags.gencode}" )
-    '';
+  preConfigure = ''
+    ./autogen.sh
+  '';
+
   configureFlags =
-    [ ]
-    ++ lib.optional enableSse41 "--with-sse41"
-    ++ lib.optional enableSse42 "--with-sse42"
-    ++ lib.optional enableAvx "--with-avx"
-    ++ lib.optional enableCuda "--with-cuda=${cudaPackages.cuda_cudart}";
+    optionals enableSse41 [ "--with-sse41" ]
+    ++ optionals enableSse42 [ "--with-sse42" ]
+    ++ optionals enableAvx [ "--with-avx" ]
+    ++ optionals enableCuda [
+      "--with-cuda=${cuda_cudart}"
+      "--with-nvcc-gencode=${concatStringsSep " " cudaFlags.gencode}"
+    ];
 
   postInstall = ''
-    find $out/lib/ -name "*.la" -exec rm -f \{} \;
+    find "$out/lib/" -name "*.la" -exec rm -f \{} \;
 
-    moveToOutput bin/ucc_info $dev
+    moveToOutput bin/ucc_info "$dev"
   '';
 
   meta = with lib; {
@@ -79,4 +98,4 @@ stdenv.mkDerivation rec {
     maintainers = [ maintainers.markuskowa ];
     platforms = platforms.linux;
   };
-}
+})
