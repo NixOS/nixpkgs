68be507599c89393b440cce3362417a151e154e6
Austin Horstman <khaneliman12@gmail.com>
2025-01-02 20:29:14 -0600

    git-interactive-rebase-tool: remove explicit libiconv

--- pkgs/by-name/gi/git-interactive-rebase-tool/package.nix
+++ pkgs/by-name/gi/git-interactive-rebase-tool/package.nix
@@ -1,9 +1,7 @@
 {
   lib,
-  stdenv,
   fetchFromGitHub,
   rustPlatform,
-  libiconv,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -18,10 +16,6 @@ rustPlatform.buildRustPackage rec {
   };
 
   cargoHash = "sha256-9pUUKxPpyoX9f10ZiLWsol2rv66WzQqwa6VubRTrT9Y=";
-
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    libiconv
-  ];
 
   # Compilation during tests fails if this env var is not set.
   preCheck = "export GIRT_BUILD_GIT_HASH=${version}";
