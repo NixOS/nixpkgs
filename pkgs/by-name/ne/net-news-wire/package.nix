a27e3395e3e158ce642bb57351baf42e07e105be
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2025-01-04 09:19:18 +0200

    net-news-wire: 6.1.5 -> 6.1.8

--- pkgs/by-name/ne/net-news-wire/package.nix
+++ pkgs/by-name/ne/net-news-wire/package.nix
@@ -1,17 +1,18 @@
-{ lib
-, stdenvNoCC
-, fetchurl
-, unzip
-, nix-update-script
+{
+  lib,
+  stdenvNoCC,
+  fetchurl,
+  unzip,
+  nix-update-script,
 }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "net-news-wire";
-  version = "6.1.5";
+  version = "6.1.8";
 
   src = fetchurl {
     url = "https://github.com/Ranchero-Software/NetNewsWire/releases/download/mac-${version}/NetNewsWire${version}.zip";
-    hash = "sha256-92hsVSEpa661qhebeSd5lxt8MtIJRn7YZyKlMs0vle0=";
+    hash = "sha256-/xhy0gF2YHYBVPUAlwySH0/yIelMNeFlU7Ya/ADx1NI=";
   };
 
   sourceRoot = ".";
@@ -34,8 +35,7 @@ stdenvNoCC.mkDerivation rec {
       NetNewsWire shows you articles from your favorite blogs and news sites and keeps track of what you've read.
     '';
     homepage = "https://github.com/Ranchero-Software/NetNewsWire";
-    changelog =
-      "https://github.com/Ranchero-Software/NetNewsWire/releases/tag/mac-${version}";
+    changelog = "https://github.com/Ranchero-Software/NetNewsWire/releases/tag/mac-${version}";
     license = licenses.mit;
     platforms = platforms.darwin;
     maintainers = with maintainers; [ jakuzure ];
