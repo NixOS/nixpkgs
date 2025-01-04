7402afc67705ba96b1343f8426921efb282c3bc8
Thomas Gerbet <thomas@gerbet.me>
2025-01-04 10:38:09 +0100

    zizmor: 0.10.0 -> 1.0.0
    Changes:
    https://github.com/woodruffw/zizmor/releases/tag/v1.0.0

--- pkgs/by-name/zi/zizmor/package.nix
+++ pkgs/by-name/zi/zizmor/package.nix
@@ -10,16 +10,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "zizmor";
-  version = "0.10.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "woodruffw";
     repo = "zizmor";
     tag = "v${version}";
-    hash = "sha256-fq+J1+CrxFSbCimM8SIshwQciEjRjPcjAmdVKbVV13s=";
+    hash = "sha256-95YtFlnC8xFBz1v+qC3rh7jEUp+4JBoMGgVQd/6IFwE=";
   };
 
-  cargoHash = "sha256-OUwl9vBB8jMY40SbOc9YK4yyxvgWQTgQRWw2LN07W08=";
+  cargoHash = "sha256-imq7ElZcC9E4nDkHaaFiBf8r1VuMtw5zOn9O7EzIPkQ=";
 
   buildInputs = [ openssl ];
 
