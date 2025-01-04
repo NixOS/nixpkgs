NixOS/nixpkgs

commit 97e04d2088399a307638928123766187ee78bb75

qt6Packages.stdenv: lift out of scope
This of course caused warnings for every package using
`qt6Package.callPackage`. I trusted eval to flag up warnings but the
new CI apparently currently swallows them.

Fixes: 75432e26894e5e70054a7d2fa348922c273bd863

pkgs/top-level/qt6-packages.nix
@@ -27,7 +27,6 @@ makeScopeWithSplicing' {
     inherit (self) callPackage;
     noExtraAttrs = set: lib.attrsets.removeAttrs set [ "extend" "override" "overrideScope" "overrideDerivation" ];
   in (noExtraAttrs qt6) // {
-  stdenv = lib.warn "qt6Packages.stdenv is deprecated. Use stdenv instead." stdenv;
 
   # LIBRARIES
   accounts-qt = callPackage ../development/libraries/accounts-qt { };
@@ -125,4 +124,6 @@ makeScopeWithSplicing' {
 
   xwaylandvideobridge = kdePackages.callPackage ../tools/wayland/xwaylandvideobridge { };
   });
+} // {
+  stdenv = lib.warn "qt6Packages.stdenv is deprecated. Use stdenv instead." stdenv; # Added for 25.05
 }

commit f6eb13f1373191fe802203ef8685a38613508b2f

qt6Packages.stdenv: condition on `config.allowAliases`
Should fix the warnings for good.

Fixes: 97e04d2088399a307638928123766187ee78bb75

pkgs/top-level/all-packages.nix
@@ -10482,7 +10482,7 @@ with pkgs;
   qt6 = recurseIntoAttrs (callPackage ../development/libraries/qt-6 { });
 
   qt6Packages = recurseIntoAttrs (import ./qt6-packages.nix {
-    inherit lib __splicedPackages makeScopeWithSplicing' generateSplicesForMkScope pkgsHostTarget kdePackages;
+    inherit lib config __splicedPackages makeScopeWithSplicing' generateSplicesForMkScope pkgsHostTarget kdePackages;
     inherit stdenv;
   });
 

pkgs/top-level/qt6-packages.nix
@@ -5,6 +5,7 @@
 # this file.
 
 { lib
+, config
 , __splicedPackages
 , makeScopeWithSplicing'
 , generateSplicesForMkScope
@@ -124,6 +125,6 @@ makeScopeWithSplicing' {
 
   xwaylandvideobridge = kdePackages.callPackage ../tools/wayland/xwaylandvideobridge { };
   });
-} // {
+} // lib.optionalAttrs config.allowAliases {
   stdenv = lib.warn "qt6Packages.stdenv is deprecated. Use stdenv instead." stdenv; # Added for 25.05
 }