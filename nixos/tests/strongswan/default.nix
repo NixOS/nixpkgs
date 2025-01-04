7c44399792a13daec7b8825e61c9b1ffcea5b7c5
Johan Thomsen <write@ownrisk.dk>
2025-01-04 12:31:45 +0100

    nixos/tests/strongswan: prepare for more tests with multiple packages

---
+++ nixos/tests/strongswan/default.nix
@@ -0,0 +1,22 @@
+{
+  system ? builtins.currentSystem,
+  pkgs ? import ../../.. { inherit system; },
+  lib ? pkgs.lib,
+}:
+let
+  withPackages = with pkgs; [
+    strongswan
+  ];
+  tests = {
+    swanctl = import ./swanctl.nix;
+  };
+in
+lib.mergeAttrsList (
+  map (
+    package:
+    lib.mapAttrs' (testName: test: {
+      name = "${package.pname}-${testName}";
+      value = test { inherit package; };
+    }) tests
+  ) withPackages
+)
