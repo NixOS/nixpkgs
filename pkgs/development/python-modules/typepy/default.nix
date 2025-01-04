eb0916038419cfa7456f864b397207d7a12f98b3
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-04 11:13:33 +0000

    python312Packages.typepy: 1.3.2 -> 1.3.4

--- pkgs/development/python-modules/typepy/default.nix
+++ pkgs/development/python-modules/typepy/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "typepy";
-  version = "1.3.2";
+  version = "1.3.4";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "thombashi";
     repo = pname;
     tag = "v${version}";
-    hash = "sha256-oIDVjJwapHun0Rk04zOZ4IjAh7qZ2k0BXK6zqFmtVds=";
+    hash = "sha256-lgwXoEtv2nBRKiWQH5bDrAIfikKN3cOqcHLEdnSAMpc=";
   };
 
   propagatedBuildInputs = [ mbstrdecoder ];
