e6f803f58c8bebd51752750b11a201ad01b02b9a
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2025-01-04 16:22:49 +0200

    lips: init at 1.0.0-beta.20

---
+++ pkgs/by-name/li/lips/package.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+  versionCheckHook,
+}:
+
+buildNpmPackage rec {
+  pname = "lips";
+  version = "1.0.0-beta.20";
+
+  src = fetchFromGitHub {
+    owner = "jcubic";
+    repo = "lips";
+    tag = version;
+    hash = "sha256-zvdtFfa+1Ols3TZSe2XCbGX9hColwGV/ReTJcTrrA4k=";
+  };
+
+  npmDepsHash = "sha256-7YeKTcBGsyiI6U0PeddAcs2x/O0LL/DT00KuSkqfy2A=";
+  npmInstallFlags = [ "--only=prod" ];
+  dontBuild = true; # dist folder is checked in
+  dontNpmBuild = true;
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+
+  meta = {
+    description = "Powerful Scheme based Lisp in JavaScript";
+    homepage = "https://lips.js.org";
+    changelog = "https://github.com/jcubic/lips/releases/tag/${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ DimitarNestorov ];
+    platforms = lib.platforms.all;
+    mainProgram = "lips";
+  };
+}
