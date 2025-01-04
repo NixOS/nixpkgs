NixOS/nixpkgs

commit 2b969610d645c8a38865aade14daedbdb824f1cd

treewide: replace darwin.apple_sdk_11_0.stdenv with stdenv on darwin

pkgs/top-level/qt6-packages.nix
@@ -27,7 +27,7 @@ makeScopeWithSplicing' {
     inherit (self) callPackage;
     noExtraAttrs = set: lib.attrsets.removeAttrs set [ "extend" "override" "overrideScope" "overrideDerivation" ];
   in (noExtraAttrs qt6) // {
-  inherit stdenv;
+  stdenv = lib.warn "qt6Packages.stdenv is deprecated. Use stdenv instead." stdenv;
 
   # LIBRARIES
   accounts-qt = callPackage ../development/libraries/accounts-qt { };

pkgs/by-name/ne/nextcloud-client/package.nix
@@ -2,6 +2,7 @@
 , gitUpdater
 , fetchFromGitHub
 , qt6Packages
+, stdenv
 , cmake
 , extra-cmake-modules
 , inotify-tools
@@ -18,7 +19,7 @@
 , libsysprof-capture
 }:
 
-qt6Packages.stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "nextcloud-client";
   version = "3.15.2";
 

pkgs/by-name/op/openimagedenoise/package.nix
@@ -13,10 +13,7 @@
   xcodebuild,
 }:
 
-let
-  stdenv' = if stdenv.hostPlatform.isDarwin then darwin.apple_sdk_11_0.stdenv else stdenv;
-in
-stdenv'.mkDerivation (finalAttrs: {
+stdenv.mkDerivation (finalAttrs: {
   pname = "openimagedenoise";
   version = "2.2.2";
 

pkgs/by-name/qo/qownnotes/package.nix
@@ -17,7 +17,7 @@ let
   appname = "QOwnNotes";
   version = "24.12.7";
 in
-qt6Packages.stdenv.mkDerivation {
+stdenv.mkDerivation {
   inherit pname version;
 
   src = fetchurl {

pkgs/development/libraries/mongocxx/default.nix
@@ -1,5 +1,5 @@
 { lib
-, pkgs
+, stdenv
 , fetchFromGitHub
 , mongoc
 , openssl
@@ -9,8 +9,6 @@
 , testers
 , darwin
 }:
-
-let stdenv = if pkgs.stdenv.hostPlatform.isDarwin then darwin.apple_sdk_11_0.stdenv else pkgs.stdenv; in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mongocxx";

pkgs/development/libraries/qscintilla/default.nix
@@ -10,10 +10,7 @@
   darwin,
 }:
 
-let
-  stdenv' = if stdenv.hostPlatform.isDarwin then darwin.apple_sdk_11_0.stdenv else stdenv;
-in
-stdenv'.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "qscintilla-qt5";
   version = "2.13.2";
 

pkgs/development/python-modules/openusd/default.nix
@@ -62,8 +62,7 @@ buildPythonPackage rec {
     hash = "sha256-slBJleeDi0mCVThty4NUX4M9vaCLV+E8rnp1Ab77TmE=";
   };
 
-  stdenv =
-    if python.stdenv.hostPlatform.isDarwin then darwin.apple_sdk_11_0.stdenv else python.stdenv;
+  stdenv = python.stdenv;
 
   outputs = [ "out" ] ++ lib.optional withDocs "doc";
 

pkgs/development/python-modules/python-mapnik/default.nix
@@ -53,8 +53,7 @@ buildPythonPackage rec {
     ./python-mapnik_std_optional.patch
   ];
 
-  stdenv =
-    if python.stdenv.hostPlatform.isDarwin then darwin.apple_sdk_11_0.stdenv else python.stdenv;
+  stdenv = python.stdenv;
 
   build-system = [ setuptools ];
 

commit 9f2d119bb1016d62a1ae4c3c97120b67436e3673

qt6Packages.stdenv: lift out of scope
This of course caused warnings for every package using
`qt6Package.callPackage`. I trusted eval to flag up warnings but the
new CI apparently currently swallows them.

Fixes: 2b969610d645c8a38865aade14daedbdb824f1cd

pkgs/top-level/qt6-packages.nix
@@ -27,7 +27,6 @@ makeScopeWithSplicing' {
     inherit (self) callPackage;
     noExtraAttrs = set: lib.attrsets.removeAttrs set [ "extend" "override" "overrideScope" "overrideDerivation" ];
   in (noExtraAttrs qt6) // {
-  stdenv = lib.warn "qt6Packages.stdenv is deprecated. Use stdenv instead." stdenv;
 
   # LIBRARIES
   accounts-qt = callPackage ../development/libraries/accounts-qt { };
@@ -125,4 +124,6 @@ makeScopeWithSplicing' {
 
   xwaylandvideobridge = kdePackages.callPackage ../tools/wayland/xwaylandvideobridge { };
   });
+} // {
+  stdenv = lib.warn "qt6Packages.stdenv is deprecated. Use stdenv instead." stdenv; # Added for 25.05
 }