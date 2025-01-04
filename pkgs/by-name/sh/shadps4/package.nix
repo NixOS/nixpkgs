6850ea717888f6dddc1d68186821a4c43a789543
liberodark <liberodark@gmail.com>
2025-01-04 11:22:17 +0100

    shadps4: add liberodark to maintainers
    (cherry picked from commit 676d6e036432d10b9e9870be02a4335197d297fa)

--- pkgs/by-name/sh/shadps4/package.nix
+++ pkgs/by-name/sh/shadps4/package.nix
@@ -130,7 +130,10 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Early in development PS4 emulator";
     homepage = "https://github.com/shadps4-emu/shadPS4";
     license = lib.licenses.gpl2Plus;
-    maintainers = with lib.maintainers; [ ryand56 ];
+    maintainers = with lib.maintainers; [
+      ryand56
+      liberodark
+    ];
     mainProgram = "shadps4";
     platforms = lib.intersectLists lib.platforms.linux lib.platforms.x86_64;
   };
