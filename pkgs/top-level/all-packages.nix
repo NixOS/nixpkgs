e58feb90e45926bdfaab25f133efd5636975cbbb
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-05 20:39:34 +0100

    mmex: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14470,12 +14470,6 @@ with pkgs;
     ocamlPackages = ocaml-ng.ocamlPackages_4_14_unsafe_string;
   };
 
-  mmex = callPackage ../applications/office/mmex {
-    wxGTK32 = wxGTK32.override {
-      withWebKit = true;
-    };
-  };
-
   mmlgui = callPackage ../applications/audio/mmlgui {
     inherit (darwin.apple_sdk.frameworks) Carbon Cocoa;
     libvgm = libvgm.override {
