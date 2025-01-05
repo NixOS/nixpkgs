61ff8b71095a94c58c3a6fb0041a61a4d9818c49
Nikolay Korotkiy <sikmir@disroot.org>
2025-01-05 17:49:36 +0400

    flashmq: modernize

--- pkgs/by-name/fl/flashmq/package.nix
+++ pkgs/by-name/fl/flashmq/package.nix
@@ -7,14 +7,14 @@
   openssl,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "flashmq";
   version = "1.19.0";
 
   src = fetchFromGitHub {
     owner = "halfgaar";
     repo = "FlashMQ";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-sMpXDriH/uko0zvrliK+knAcw2unBbDHQfYHG7brhTk=";
   };
 
@@ -34,12 +34,12 @@ stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Fast light-weight MQTT broker/server";
     mainProgram = "flashmq";
     homepage = "https://www.flashmq.org/";
-    license = licenses.agpl3Only;
-    maintainers = with maintainers; [ sikmir ];
-    platforms = platforms.linux;
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ sikmir ];
+    platforms = lib.platforms.linux;
   };
-}
+})
