efcf424422dbe5c0792ad9e1a18125f4e3a60014
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-05 14:26:10 +0100

    python312Packages.pyemd: refactor

--- pkgs/development/python-modules/pyemd/default.nix
+++ pkgs/development/python-modules/pyemd/default.nix
@@ -1,48 +1,50 @@
 {
   lib,
   buildPythonPackage,
-  pythonOlder,
-  fetchPypi,
   cython,
+  fetchPypi,
+  numpy,
   oldest-supported-numpy,
   packaging,
-  setuptools,
-  setuptools-scm,
-  wheel,
-  numpy,
   pytestCheckHook,
+  pythonOlder,
+  setuptools-scm,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "pyemd";
   version = "1.0.0";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
-
-  format = "pyproject";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-tCta57LRWx1N7mOBDqeYo5IX6Kdre0nA62OoTg/ZAP4=";
   };
 
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
   nativeBuildInputs = [
     cython
     numpy
     oldest-supported-numpy
     packaging
-    setuptools
-    setuptools-scm
-    wheel
   ];
 
-  propagatedBuildInputs = [ numpy ];
+  dependencies = [ numpy ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
   meta = with lib; {
     description = "Python wrapper for Ofir Pele and Michael Werman's implementation of the Earth Mover's Distance";
     homepage = "https://github.com/wmayner/pyemd";
+    changelog = "https://github.com/wmayner/pyemd/releases/tag/${version}";
     license = licenses.mit;
+    maintainers = [ ];
   };
 }
