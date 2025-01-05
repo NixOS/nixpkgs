617f704cc5cffc4a84431fc9f4c4c7d494b19138
sternenseemann <sternenseemann@systemli.org>
2025-01-05 18:55:06 +0100

    haskellPackages: remove all references to removed versioned attrs
    The bump to Stackage LTS 23 has removed these attributes because they
    are the default version now (I've verified this manually). With this,
    haskellPackages (haskell.packages.ghc98 only) evaluates successfully to
    the satisfaction of nix-env(1).
    
    WARNING: Builds where not tested, some of the removals may have broken
    something. Also new eval failures of previously working attributes due
    to (new) dependencies that are marked as broken were note addressed.

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -956,7 +956,6 @@ self: super: builtins.intersectAttrs super {
   # break infinite recursion with base-orphans
   primitive = dontCheck super.primitive;
   primitive_0_7_1_0 = dontCheck super.primitive_0_7_1_0;
-  primitive_0_9_0_0 = dontCheck super.primitive_0_9_0_0;
 
   cut-the-crap =
     let path = pkgs.lib.makeBinPath [ pkgs.ffmpeg pkgs.youtube-dl ];
@@ -1457,7 +1456,7 @@ self: super: builtins.intersectAttrs super {
         mpiImpl = pkgs.mpi.pname;
         disableUnused = with builtins; map disableCabalFlag (filter (n: n != mpiImpl) validMpi);
      in lib.pipe
-          (super.mpi-hs_0_7_3_1.override { ompi = pkgs.mpi; })
+          (super.mpi-hs.override { ompi = pkgs.mpi; })
           ( [ (addTestToolDepends [ pkgs.openssh pkgs.mpiCheckPhaseHook ]) ]
             ++ disableUnused
             ++ lib.optional (builtins.elem mpiImpl validMpi) (enableCabalFlag mpiImpl)
