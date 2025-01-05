99eabc0ab3dab1634eadc0092fb2efad8321d9c3
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-05 04:15:24 +0100

    zigbee2mqtt_2: init at 2.1.1
    https://github.com/Koenkk/zigbee2mqtt/releases/tag/2.0.0
    https://github.com/Koenkk/zigbee2mqtt/releases/tag/2.1.0
    https://github.com/Koenkk/zigbee2mqtt/releases/tag/2.1.1

--- pkgs/by-name/zi/zigbee2mqtt_2/package.nix
+++ pkgs/by-name/zi/zigbee2mqtt_2/package.nix
@@ -1,38 +1,60 @@
 {
   lib,
   stdenv,
-  buildNpmPackage,
   fetchFromGitHub,
+  nodejs,
+  npmHooks,
+  pnpm_9,
   systemdMinimal,
   nixosTests,
   nix-update-script,
   withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemdMinimal,
 }:
 
-buildNpmPackage rec {
+let
+  pnpm = pnpm_9;
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "zigbee2mqtt";
-  version = "1.42.0";
+  version = "2.1.1";
 
   src = fetchFromGitHub {
     owner = "Koenkk";
     repo = "zigbee2mqtt";
-    rev = version;
-    hash = "sha256-/7mZrf3FyIliCzsy6yzVRJYMy4bViphYi81UY43iO98=";
+    tag = finalAttrs.version;
+    hash = "sha256-YVsQ4Zo0nlIb9m4wiK7xcqB7eE/h2ZvTTqEYLDagoHM=";
   };
 
-  npmDepsHash = "sha256-heqTYLC+TQPQ2dc5MrVdvJeNqrygC4tUgkLcfKvlYvE=";
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-Wr7FngKfedUrtOLpaTxvAdJZfCXo1/+oLMIJMOCgafk=";
+  };
+
+  nativeBuildInputs = [
+    nodejs
+    npmHooks.npmInstallHook
+    pnpm.configHook
+  ];
 
   buildInputs = lib.optionals withSystemd [
     systemdMinimal
   ];
 
-  npmFlags = lib.optionals (!withSystemd) [ "--omit=optional" ];
+  buildPhase = ''
+    runHook preBuild
 
-  passthru.tests.zigbee2mqtt = nixosTests.zigbee2mqtt_1;
+    pnpm run build
+
+    runHook postBuild
+  '';
+
+  dontNpmPrune = true;
+
+  passthru.tests.zigbee2mqtt = nixosTests.zigbee2mqtt_2;
   passthru.updateScript = nix-update-script { };
 
   meta = with lib; {
-    changelog = "https://github.com/Koenkk/zigbee2mqtt/releases/tag/${version}";
+    changelog = "https://github.com/Koenkk/zigbee2mqtt/releases/tag/${finalAttrs.version}";
     description = "Zigbee to MQTT bridge using zigbee-shepherd";
     homepage = "https://github.com/Koenkk/zigbee2mqtt";
     license = licenses.gpl3;
@@ -48,4 +70,4 @@ buildNpmPackage rec {
     ];
     mainProgram = "zigbee2mqtt";
   };
-}
+})
