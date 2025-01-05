7ead99ca4a780b8b2b319609dbbf441e3561a1d5
sternenseemann <sternenseemann@systemli.org>
2025-01-05 18:41:59 +0100

    haskellPackages.ghc-lib*: 9.10.1.20241103 -> 9.10.1.20250103

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2876,7 +2876,7 @@ self: super: {
   }) super.kmonad;
 
   ghc-syntax-highlighter_0_0_12_0 = super.ghc-syntax-highlighter_0_0_12_0.overrideScope(self: super: {
-    ghc-lib-parser = self.ghc-lib-parser_9_10_1_20241103;
+    ghc-lib-parser = self.ghc-lib-parser_9_10_1_20250103;
   });
 
   # 2024-03-17: broken
