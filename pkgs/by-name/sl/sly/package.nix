202622e373ddba7bc55a1bc60cbf54a060dce1d2
aucub <78630225+aucub@users.noreply.github.com>
2025-01-06 23:57:08 +0800

    sly: add update script

--- pkgs/by-name/sl/sly/package.nix
+++ pkgs/by-name/sl/sly/package.nix
@@ -2,6 +2,11 @@
   lib,
   fetchFromGitHub,
   flutter327,
+  runCommand,
+  yq,
+  sly,
+  _experimental-update-script-combinators,
+  gitUpdater,
 }:
 flutter327.buildFlutterApplication rec {
   pname = "sly";
@@ -20,6 +25,22 @@ flutter327.buildFlutterApplication rec {
     install -Dm0644 ./packaging/linux/page.kramo.Sly.svg $out/share/icons/hicolor/scalable/apps/page.kramo.Sly.svg
     install -Dm0644 ./packaging/linux/page.kramo.Sly.desktop $out/share/applications/sly.desktop
   '';
+
+  passthru = {
+    pubspecSource =
+      runCommand "pubspec.lock.json"
+        {
+          buildInputs = [ yq ];
+          inherit (sly) src;
+        }
+        ''
+          cat $src/pubspec.lock | yq > $out
+        '';
+    updateScript = _experimental-update-script-combinators.sequence [
+      (gitUpdater { rev-prefix = "v"; })
+      (_experimental-update-script-combinators.copyAttrOutputToFile "sly.pubspecSource" ./pubspec.lock.json)
+    ];
+  };
 
   meta = {
     description = "Friendly image editor";
