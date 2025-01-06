85b24c850977afc6ded0a6d7aaf84bdb0226bbf7
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2025-01-06 09:11:18 +0000

    cargo-guppy: unstable-2024-07-29 -> unstable-2025-01-05

--- pkgs/by-name/ca/cargo-guppy/package.nix
+++ pkgs/by-name/ca/cargo-guppy/package.nix
@@ -10,16 +10,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-guppy";
-  version = "unstable-2024-07-29";
+  version = "unstable-2025-01-05";
 
   src = fetchFromGitHub {
     owner = "guppy-rs";
     repo = "guppy";
-    rev = "37e651b65d0d2bbdbccf06376b4b968b1f9ff926";
-    sha256 = "sha256-2CVdSXP1attUxP1ruRdMVb1vKGnr8da7kguWm0sDPcA=";
+    rev = "78b252665d044488086958e09c29bcd5580c4f41";
+    sha256 = "sha256-+IjtK4kSm2vThgIxDsBLpoh0j9cDhhEqI6Hr2BmC7hc=";
   };
 
-  cargoHash = "sha256-jtBUCXLjZhEQF/D6xuSp/41ADkAxFcHaMAvWVTkqEPk=";
+  cargoHash = "sha256-u39EALYAtS04Bu2bPZsbrJahk8s3aVNdwsosivnTAIk=";
 
   nativeBuildInputs = [ pkg-config ];
 
