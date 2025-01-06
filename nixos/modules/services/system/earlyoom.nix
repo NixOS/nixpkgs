1a1462916fd3920635f550b11390b0ebb2b436e4
oxalica <oxalicc@pm.me>
2025-01-06 15:39:01 -0500

    nixos/earlyoom: mention `extraArgs` breakage and fix example
    The shell-escaping breaks space handling and also makes the example
    outdated. This breakage is missed in the previous change.
    
    Also mention it in the release note.

--- nixos/modules/services/system/earlyoom.nix
+++ nixos/modules/services/system/earlyoom.nix
@@ -139,9 +139,14 @@ in
       default = [ ];
       example = [
         "-g"
-        "--prefer '(^|/)(java|chromium)$'"
+        "--prefer"
+        "(^|/)(java|chromium)$"
       ];
-      description = "Extra command-line arguments to be passed to earlyoom.";
+      description = ''
+        Extra command-line arguments to be passed to earlyoom. Each element in
+        the value list will be escaped as an argument without further
+        word-breaking.
+      '';
     };
   };
 
