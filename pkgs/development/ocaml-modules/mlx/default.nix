e74e8f4924f511107abd5bb66391591e86e6f8f4
Yuri Albuquerque <yuridenommus@gmail.com>
2025-01-06 09:16:58 -0300

    ocamlPackages.mlx: add upper bound

--- pkgs/development/ocaml-modules/mlx/default.nix
+++ pkgs/development/ocaml-modules/mlx/default.nix
@@ -4,30 +4,37 @@
   buildDunePackage,
   ppxlib,
   menhir,
+  ocaml,
 }:
 
-buildDunePackage rec {
+let
   pname = "mlx";
   version = "0.9";
+in
+lib.throwIf (lib.versionAtLeast ocaml.version "5.3")
+  "${pname}-${version} is not available for OCaml ${ocaml.version}"
+  buildDunePackage
+  {
+    inherit pname version;
 
-  minimalOCamlVersion = "4.14";
+    minimalOCamlVersion = "4.14";
 
-  src = fetchFromGitHub {
-    owner = "ocaml-mlx";
-    repo = "mlx";
-    rev = version;
-    hash = "sha256-3hPtyBKD2dp4UJBykOudW6KR2KXPnBuDnuJ1UNLpAp0=";
-  };
+    src = fetchFromGitHub {
+      owner = "ocaml-mlx";
+      repo = "mlx";
+      rev = version;
+      hash = "sha256-3hPtyBKD2dp4UJBykOudW6KR2KXPnBuDnuJ1UNLpAp0=";
+    };
 
-  buildInputs = [
-    ppxlib
-    menhir
-  ];
+    buildInputs = [
+      ppxlib
+      menhir
+    ];
 
-  meta = with lib; {
-    description = "OCaml syntax dialect which adds JSX syntax expressions";
-    homepage = "https://github.com/ocaml-mlx/mlx";
-    license = licenses.lgpl21Plus;
-    maintainers = with maintainers; [ Denommus ];
-  };
-}
+    meta = with lib; {
+      description = "OCaml syntax dialect which adds JSX syntax expressions";
+      homepage = "https://github.com/ocaml-mlx/mlx";
+      license = licenses.lgpl21Plus;
+      maintainers = with maintainers; [ Denommus ];
+    };
+  }
