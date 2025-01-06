3b8a45a8da53bb4b22689d6bfaa790a822d904d9
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2025-01-06 08:56:35 +0000

    cargo-hakari: 0.9.33 -> 0.9.35

--- pkgs/by-name/ca/cargo-hakari/package.nix
+++ pkgs/by-name/ca/cargo-hakari/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-hakari";
-  version = "0.9.30";
+  version = "0.9.35";
 
   src = fetchFromGitHub {
     owner = "guppy-rs";
     repo = "guppy";
     rev = "cargo-hakari-${version}";
-    sha256 = "sha256-oJZiGXsOl00Bim/olYYSqt/p3j6dTw25IURcwdXYrAo=";
+    sha256 = "sha256-+IjtK4kSm2vThgIxDsBLpoh0j9cDhhEqI6Hr2BmC7hc=";
   };
 
-  cargoHash = "sha256-V9QmaZYBXj26HJrP8gABwhhUPwBxnyLoO4O45lnPyew=";
+  cargoHash = "sha256-xMgNk82U32AtMeZJ8v2M0faHLAPkXrdIJHwOJcqE9m8=";
 
   cargoBuildFlags = [
     "-p"
