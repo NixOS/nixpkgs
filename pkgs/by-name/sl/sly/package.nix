51df474aaadda60eaa6db96e69e9fba19d7e28cb
aucub <78630225+aucub@users.noreply.github.com>
2025-01-06 23:54:33 +0800

    sly: 0.4.0 -> 1.0.0

--- pkgs/by-name/sl/sly/package.nix
+++ pkgs/by-name/sl/sly/package.nix
@@ -1,31 +1,29 @@
 {
   lib,
   fetchFromGitHub,
-  flutter,
+  flutter327,
 }:
-let
-  version = "0.4.0";
+flutter327.buildFlutterApplication rec {
+  pname = "sly";
+  version = "1.0.0";
+
   src = fetchFromGitHub {
     owner = "kra-mo";
-    repo = "sly";
-    rev = "v${version}";
-    hash = "sha256-P7LhhXQQDRsDQ8bZgfvWazLRMYVGhFhMTD41fgs718g=";
+    repo = "Sly";
+    tag = "v${version}";
+    hash = "sha256-pFTP+oDY3pCSgO26ZtqUR+puMJSFZAEdbM2AqmfkNX8=";
   };
-in
-flutter.buildFlutterApplication {
-  pname = "sly";
-  inherit version src;
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
 
   postInstall = ''
     install -Dm0644 ./packaging/linux/page.kramo.Sly.svg $out/share/icons/hicolor/scalable/apps/page.kramo.Sly.svg
-    install -Dm0644 ./packaging/linux/page.kramo.Sly.desktop $out/share/applications/page.kramo.Sly.desktop
+    install -Dm0644 ./packaging/linux/page.kramo.Sly.desktop $out/share/applications/sly.desktop
   '';
 
   meta = {
     description = "Friendly image editor";
-    homepage = "https://github.com/kra-mo/sly";
+    homepage = "https://github.com/kra-mo/Sly";
     mainProgram = "sly";
     license = with lib.licenses; [ gpl3Plus ];
     maintainers = with lib.maintainers; [ aucub ];
