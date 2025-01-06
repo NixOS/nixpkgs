056f3012bf69b7e38a554ff48ab6cc4da69d67d5
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-06 23:04:38 +0100

    python312Packages.pynecil: 3.0.1 -> 4.0.1
    Diff: https://github.com/tr4nt0r/pynecil/compare/refs/tags/v3.0.1...v4.0.1
    
    Changelog: https://github.com/tr4nt0r/pynecil/releases/tag/v4.0.1

--- pkgs/development/python-modules/pynecil/default.nix
+++ pkgs/development/python-modules/pynecil/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "pynecil";
-  version = "3.0.1";
+  version = "4.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tr4nt0r";
     repo = "pynecil";
     tag = "v${version}";
-    hash = "sha256-Z4QuX562LKDtNbl1rWcnJbB3Qw0ZaQcJskPPy7DWvQs=";
+    hash = "sha256-mIMtmhnBctymjXlp47YEyW4RXqUU8+YDR/vG/oYSmQI=";
   };
 
   pythonRelaxDeps = [ "aiohttp" ];
