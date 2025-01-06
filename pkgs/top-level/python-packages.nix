6b6a023c0146e2e54eacdb50039483608e34560b
clement <clement.jvdm@gmail.com>
2025-01-06 23:49:17 +0100

    python312Packages.pygerber: init at 2.4.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10562,6 +10562,8 @@ self: super: with self; {
 
   pygelf = callPackage ../development/python-modules/pygelf { };
 
+  pygerber = callPackage ../development/python-modules/pygerber { };
+
   pygments-style-github = callPackage ../development/python-modules/pygments-style-github { };
 
   pygnmi = callPackage ../development/python-modules/pygnmi { };
