51dd724f2257df8069ce4ecfcc50d37064fad7ac
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-06 13:09:30 +0000

    vim: 9.1.0905 -> 9.1.0990
    (cherry picked from commit ae6e24d9729f393246f7f36fb99092b611987098)

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,6 +1,6 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "9.1.0905";
+  version = "9.1.0990";
 
   outputs = [
     "out"
@@ -11,7 +11,7 @@ rec {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    hash = "sha256-sFsTONGeSocn1M8NZo5LjIhagmq/nR1zrGRN7p86Q4o=";
+    hash = "sha256-KKjJCuGczwYP+NVx5f6YQRsKR0zW44ciNNvmjQ03ytM=";
   };
 
   enableParallelBuilding = true;
