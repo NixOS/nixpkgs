84e25b533bb2a65b18ad9fad0f94df4f6a582f99
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2025-01-07 12:17:10 +0200

    protonvpn-gui: 4.8.1 -> 4.8.2
    Update the package to the latest version.

--- pkgs/applications/networking/protonvpn-gui/default.nix
+++ pkgs/applications/networking/protonvpn-gui/default.nix
@@ -21,14 +21,14 @@
 
 buildPythonApplication rec {
   pname = "protonvpn-gui";
-  version = "4.8.1";
+  version = "4.8.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ProtonVPN";
     repo = "proton-vpn-gtk-app";
     tag = "v${version}";
-    hash = "sha256-+UnMsjIJ0M1YiF+BcY7RObUoCeDZGtE0ul6KBaODzeY=";
+    hash = "sha256-kNWwrNpXCkAPvXXqv8HwOx0msYEVsO0JgrtG3wUVmQ4=";
   };
 
   nativeBuildInputs = [
