77616a0fdbf1a81d7730fa6fac035f9817dc7764
r-vdp <ramses@well-founded.dev>
2025-01-07 22:13:55 +0200

    fish: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5541,8 +5541,6 @@ with pkgs;
     buildGoModule = buildGo123Module;
   };
 
-  fish = callPackage ../shells/fish { };
-
   wrapFish = callPackage ../shells/fish/wrapper.nix { };
 
   fishPlugins = recurseIntoAttrs (callPackage ../shells/fish/plugins { });
