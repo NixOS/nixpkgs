704ce510229dcde009a04898965a19f42d2ebcf5
damhiya <damhiya@gmail.com>
2025-01-07 13:51:02 +0900

    coqPackages.paco: 4.2.0 -> 4.2.2

--- pkgs/development/coq-modules/paco/default.nix
+++ pkgs/development/coq-modules/paco/default.nix
@@ -15,7 +15,7 @@ mkCoqDerivation {
     lib.switch coq.coq-version [
       {
         case = range "8.13" "8.20";
-        out = "4.2.0";
+        out = "4.2.2";
       }
       {
         case = range "8.12" "8.17";
@@ -34,6 +34,8 @@ mkCoqDerivation {
         out = "1.2.8";
       }
     ] null;
+  release."4.2.2".sha256 = "sha256-qr6o45Q90FK+kkBUJ+W7QhiA0YQRb1RUwwXUpTTFt8A=";
+  release."4.2.1".sha256 = "sha256-rDYlpT2Be3gEW0IcJeSFXfXBbDZa0i99cmg0huFAwFw=";
   release."4.2.0".sha256 = "sha256-YHYtiz9hium96n3owL/C99AjJAFTlTCmmb2+ttevgMY=";
   release."4.1.2".sha256 = "sha256:1l8mwakqp4wnppsldl8wp2j24h1jvadnvrsgf35xnvdyygypjp2v";
   release."4.1.1".sha256 = "1qap8cyv649lr1s11r7h5jzdjd4hsna8kph15qy5fw24h5nx6byy";
@@ -57,6 +59,7 @@ mkCoqDerivation {
     maintainers = with lib.maintainers; [
       jwiegley
       ptival
+      damhiya
     ];
   };
 }
