3c199ff682cae83f60c5714018199e86ae537664
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-07 04:14:44 +0000

    python312Packages.python-roborock: 2.8.4 -> 2.8.5

--- pkgs/development/python-modules/python-roborock/default.nix
+++ pkgs/development/python-modules/python-roborock/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "python-roborock";
-  version = "2.8.4";
+  version = "2.8.5";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "humbertogontijo";
     repo = "python-roborock";
     tag = "v${version}";
-    hash = "sha256-/VDDonWJ/BdWjOzIFSddh9dZEZxIP2XwODsOpnPKbHs=";
+    hash = "sha256-3wUf2J7TncxUdqN3SgAdHwMLbkNXj9Pwf7Zk3OmAThY=";
   };
 
   postPatch = ''
