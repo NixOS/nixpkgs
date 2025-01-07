fcf4803a889b208069226b289872e173cc3fba87
tris203 <admin@snappeh.com>
2025-01-07 21:23:30 +0000

    maintainers: add tris203
    (cherry picked from commit 56a034bda41b01a088ee171de7c8787d1bcf5fb9)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22576,6 +22576,11 @@
     githubId = 9870613;
     name = "Hubert Mühlhans";
   };
+  tris203 = {
+    email = "admin@snappeh.com";
+    github = "tris203";
+    githubId = 18444302;
+  };
   trishtzy = {
     github = "trishtzy";
     githubId = 5356506;
