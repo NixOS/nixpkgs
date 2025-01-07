163c06e0443ea0a466b99d829274a3dc1f62f849
Reno Dakota <paparodeo@proton.me>
2025-01-07 03:06:11 -0800

    dune3d: unpin LLVM17
    pin added in 893a4feeb5749f2072a6ec58ccfe722cd49f7458 when default LLVM
    was 16. Now default is 19 remove the pin.

--- pkgs/by-name/du/dune3d/package.nix
+++ pkgs/by-name/du/dune3d/package.nix
@@ -13,7 +13,6 @@
   libspnav,
   libuuid,
   libxml2,
-  llvmPackages_17,
   meson,
   ninja,
   opencascade-occt_7_6,
@@ -24,10 +23,9 @@
 }:
 
 let
-  stdenv' = if stdenv.hostPlatform.isDarwin then llvmPackages_17.stdenv else stdenv;
   opencascade-occt = opencascade-occt_7_6;
 in
-stdenv'.mkDerivation (finalAttrs: {
+stdenv.mkDerivation (finalAttrs: {
   pname = "dune3d";
   version = "1.2.0";
 
