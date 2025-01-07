1e8bb016d8d137c648c3f2158eafa83592e08ee3
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-07 06:17:32 +0000

    opnborg: 0.1.51 -> 0.1.54

--- pkgs/by-name/op/opnborg/package.nix
+++ pkgs/by-name/op/opnborg/package.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "opnborg";
-  version = "0.1.51";
+  version = "0.1.54";
 
   src = fetchFromGitHub {
     owner = "paepckehh";
     repo = "opnborg";
     rev = "v${version}";
-    hash = "sha256-HcQl5R+xzk5HUVBUecI7KsrCfz/WK80hZ4oCOSHOGs8=";
+    hash = "sha256-N6tZqHyL3bOkXJrn4qMVpUR3tQHa8DiurRqy6BJnK+Y=";
   };
 
   vendorHash = "sha256-REXJryUcu+/AdVx1aK0nJ98Wq/EdhrZqL24kC1wK6mc=";
