584a0cdcbc63774b87181136703ad74157bfe3ec
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-01-07 16:25:11 +0000

    maintainers: add lucasfa

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15252,6 +15252,12 @@
     githubId = 15693688;
     name = "Lucas Eduardo Wendt";
   };
+  lucasfa = {
+    name = "Lucas Fehlau Arbulu";
+    githubId = 23667494;
+    github = "LucasFA";
+    matrix = "@lucasfa:matrix.org";
+  };
   lucastso10 = {
     email = "lucastso10@gmail.com";
     github = "lucastso10";
