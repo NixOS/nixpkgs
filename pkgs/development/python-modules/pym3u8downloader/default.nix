4941d21d074ffe95f9b7f9af8ac78e78a69ef949
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-08 14:32:23 +0000

    python312Packages.pym3u8downloader: 0.1.5 -> 0.1.8

--- pkgs/development/python-modules/pym3u8downloader/default.nix
+++ pkgs/development/python-modules/pym3u8downloader/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "pym3u8downloader";
-  version = "0.1.5";
+  version = "0.1.8";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "coldsofttech";
     repo = "pym3u8downloader";
-    rev = version;
-    hash = "sha256-Kcvtl4jP2pSiETTKUmuiBsysxaFfd4K/E2/nXY8Vlw8=";
+    tag = version;
+    hash = "sha256-VfNzHysvEVUNx8OK28v2l3QYTMn0ydE/LH+DBXpLfE8=";
   };
 
   build-system = [ setuptools ];
