3cd722ec5539ddeba2868d2b3dd003092045c895
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-07 22:16:20 -0500

    ingrid-core: init at 1.3.1

---
+++ pkgs/by-name/in/ingrid-core/package.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  rustPlatform,
+  fetchCrate,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "ingrid_core";
+  version = "1.3.1";
+
+  src = fetchCrate {
+    inherit pname version;
+    hash = "sha256-/B7EEq101Bu4y+rFY9ISI7kGPXEfMdOzf+Oj9w/ND2U=";
+  };
+  cargoHash = "sha256-wO2b5Ckt6vtWjbYPfmrKSo9+KomGBL3PTk/UbMHn2sM=";
+
+  meta = with lib; {
+    description = "Crossword-generating library and CLI tool";
+    homepage = "https://ingrid.cx";
+  };
+}
