c0ff0651a9586de81f806334259f074fd726a542
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-01-08 01:38:05 +0100

    webdav: modernize

--- pkgs/by-name/we/webdav/package.nix
+++ pkgs/by-name/we/webdav/package.nix
@@ -7,19 +7,19 @@ buildGo123Module rec {
   src = fetchFromGitHub {
     owner = "hacdias";
     repo = "webdav";
-    rev = "v${version}";
-    sha256 = "sha256-nLQ77RuOGYaL+U3X3yb4Kq47NA1A3SSUMKBbFnRP6o4=";
+    tag = "v${version}";
+    hash = "sha256-nLQ77RuOGYaL+U3X3yb4Kq47NA1A3SSUMKBbFnRP6o4=";
   };
 
   vendorHash = "sha256-x5CUy46c4SunzMw/v2DWpdahuXFZnJdGInQ0lSho/es=";
 
   __darwinAllowLocalNetworking = true;
 
-  meta = with lib; {
+  meta = {
     description = "Simple WebDAV server";
     homepage = "https://github.com/hacdias/webdav";
-    license = licenses.mit;
-    maintainers = with maintainers; [
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
       pmy
       pbsds
     ];
