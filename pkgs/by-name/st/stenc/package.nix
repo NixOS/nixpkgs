c046a23681343c3ee23e4c0af7c2c597de41d035
Sergei Zimmerman <145775305+xokdvium@users.noreply.github.com>
2025-01-09 01:17:16 +0300

    stenc: 1.1.1 -> 2.0.0

--- pkgs/by-name/st/stenc/package.nix
+++ pkgs/by-name/st/stenc/package.nix
@@ -4,27 +4,31 @@
   fetchFromGitHub,
   gitUpdater,
   autoreconfHook,
+  pkg-config,
+  pandoc,
 }:
 
 stdenv.mkDerivation rec {
   pname = "stenc";
-  version = "1.1.1";
+  version = "2.0.0";
+
+  outputs = [
+    "out"
+    "man"
+  ];
 
   src = fetchFromGitHub {
     owner = "scsitape";
     repo = "stenc";
-    rev = version;
-    sha256 = "GcCRVkv+1mREq3MhMRn5fICthwI4WRQJSP6InuzxP1Q=";
+    tag = version;
+    sha256 = "sha256-L0g285H8bf3g+HDYUDRWBZMOBCnWz3Vm38Ijttu404U=";
   };
 
-  postPatch = ''
-    # Fix gcc-13 build by pulling missing header. UPstream also fixed it
-    # in next major version, but there are many other patch dependencies.
-    # TODO: remove on next major version update
-    sed -e '1i #include <cstdint>' -i src/scsiencrypt.h
-  '';
-
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    pandoc
+  ];
 
   passthru.updateScript = gitUpdater { };
 
