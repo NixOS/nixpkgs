8df742d1f2e67b55ed8caa95d0f674976dbb4d79
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-08 18:24:58 +0000

    python312Packages.internetarchive: 5.0.4 -> 5.1.0

--- pkgs/development/python-modules/internetarchive/default.nix
+++ pkgs/development/python-modules/internetarchive/default.nix
@@ -16,16 +16,16 @@
 
 buildPythonPackage rec {
   pname = "internetarchive";
-  version = "5.0.4";
+  version = "5.1.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "jjjake";
     repo = "internetarchive";
     tag = "v${version}";
-    hash = "sha256-x4EzVm22iZhGysg2iMtuil2tNJn4/8cWYyULLowODGc=";
+    hash = "sha256-HwE8oEd5ss8HkpuQuUwdfzENYHn2z/QmzvhRIBvo1qA=";
   };
 
   build-system = [ setuptools ];
