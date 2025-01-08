0c1f5b4da7e86e0e68bb4bf83428722d79344a55
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-08 07:28:33 +0000

    moar: 1.30.0 -> 1.31.0

--- pkgs/by-name/mo/moar/package.nix
+++ pkgs/by-name/mo/moar/package.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "moar";
-  version = "1.30.0";
+  version = "1.31.0";
 
   src = fetchFromGitHub {
     owner = "walles";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-fEJttsZt3pET1Xga8AYkLCX5aGkW23R2jGQvML3w+eo=";
+    hash = "sha256-0HDFK2wPQtwIAVup/pVSjrdt1zpbfLV08HxeVPWC8dE=";
   };
 
   vendorHash = "sha256-J9u7LxzXk4npRyymmMKyN2ZTmhT4WwKjy0X5ITcHtoE=";
