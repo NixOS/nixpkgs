a09f632722c6f6f5f80366b69a710c55a680771b
Doron Behar <doron.behar@gmail.com>
2025-01-08 14:01:05 +0200

    bicon: modernize

--- pkgs/by-name/bi/bicon/package.nix
+++ pkgs/by-name/bi/bicon/package.nix
@@ -10,13 +10,13 @@
   xkbutils,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "bicon";
-  version = "unstable-2024-01-31";
+  version = "0.5-unstable-2024-01-31";
 
   src = fetchFromGitHub {
     owner = "behdad";
-    repo = pname;
+    repo = "bicon";
     rev = "48720c0f22197d4d5f7b0f5162a3d8f071e6e8a8";
     hash = "sha256-4pvI4T+fdgCirHDc0h3vP5AZyqfnBKv5R3fJICnpmF4=";
   };
@@ -36,15 +36,15 @@ stdenv.mkDerivation rec {
     patchShebangs .
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Bidirectional console";
     homepage = "https://github.com/behdad/bicon";
-    license = [
-      licenses.lgpl21
-      licenses.psfl
-      licenses.bsd0
+    license = with lib.licenses; [
+      lgpl21
+      psfl
+      bsd0
     ];
     maintainers = [ ];
-    platforms = platforms.linux;
+    platforms = lib.platforms.linux;
   };
-}
+})
