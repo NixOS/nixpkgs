083c22d2977dffd5d6179706e7f5f2c3fab5cc4b
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-08 09:42:14 +0100

    python313Packages.pygitguardian: 1.18.0 -> 1.19.0
    Diff: https://github.com/GitGuardian/py-gitguardian/compare/refs/tags/v1.18.0...v1.19.0
    
    Changelog: https://github.com/GitGuardian/py-gitguardian/blob/v1.19.0/CHANGELOG.md

--- pkgs/development/python-modules/pygitguardian/default.nix
+++ pkgs/development/python-modules/pygitguardian/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "pygitguardian";
-  version = "1.18.0";
+  version = "1.19.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "GitGuardian";
     repo = "py-gitguardian";
     tag = "v${version}";
-    hash = "sha256-4qMcBtelLiqLXKV8YIw9M6e97Nbo/K8UpsKoR0W7uKM=";
+    hash = "sha256-g3OH6pPk6Whd0JW6voILEK40/z6gWrdT6ibSa5kW47Q=";
   };
 
   pythonRelaxDeps = [
@@ -33,9 +33,7 @@ buildPythonPackage rec {
     "setuptools"
   ];
 
-  build-system = [
-    pdm-backend
-  ];
+  build-system = [ pdm-backend ];
 
   dependencies = [
     marshmallow
@@ -53,33 +51,7 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "pygitguardian" ];
 
-  disabledTests = [
-    # Tests require an API key
-    "test_api_tokens"
-    "test_bogus_rate_limit"
-    "test_compute_sca_files"
-    "test_content_scan_exceptions"
-    "test_content_scan"
-    "test_create_honeytoken"
-    "test_create_jwt"
-    "test_extra_headers"
-    "test_health_check"
-    "test_multi_content_exceptions"
-    "test_multi_content_scan"
-    "test_multiscan_parameters"
-    "test_quota_overview"
-    "test_rate_limit"
-    "test_read_metadata_bad_response"
-    "test_read_metadata_no_remediation_message"
-    "test_read_metadata_remediation_message"
-    "test_retrieve_secret_incident"
-    "test_sca_client_scan_diff"
-    "test_sca_scan_all_with_params"
-    "test_sca_scan_directory_invalid_tar"
-    "test_sca_scan_directory"
-    "test_scan"
-    "test_versions_from_headers"
-  ];
+  env.GITGUARDIAN_API_KEY = "Test key for tests";
 
   meta = with lib; {
     description = "Library to access the GitGuardian API";
