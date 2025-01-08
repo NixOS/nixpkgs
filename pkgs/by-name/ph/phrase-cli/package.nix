cfb9969afb88bc739b657794a01cd24a65665eb2
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-08 19:06:50 +0000

    phrase-cli: 2.35.4 -> 2.35.5

--- pkgs/by-name/ph/phrase-cli/package.nix
+++ pkgs/by-name/ph/phrase-cli/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "phrase-cli";
-  version = "2.35.4";
+  version = "2.35.5";
 
   src = fetchFromGitHub {
     owner = "phrase";
     repo = "phrase-cli";
     rev = version;
-    sha256 = "sha256-PXeB4tSmRTPQDHG1YdXQ4GIotEsAV+kRUvyczamOtHc=";
+    sha256 = "sha256-hPaZquiOcvRfWXG9nCKTvy7sPMueH/G/ikmdo87/0pg=";
   };
 
-  vendorHash = "sha256-iJ2QXLP3XYyPXKTY3879LwyjXL+Ifv7H8ibgfTBiaoA=";
+  vendorHash = "sha256-2IgEkRgHxFagNQ7qgT2GchukALxjkCkkcGlyzQTJUDQ=";
 
   ldflags = [ "-X=github.com/phrase/phrase-cli/cmd.PHRASE_CLIENT_VERSION=${version}" ];
 
