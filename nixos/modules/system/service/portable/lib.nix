30c57d53bcba05f37eaebea761c25f81fcd6371e
Robert Hensing <robert@roberthensing.nl>
2025-01-08 17:10:49 +0100

    format

--- nixos/modules/system/service/portable/lib.nix
+++ nixos/modules/system/service/portable/lib.nix
@@ -3,29 +3,31 @@ let
   inherit (lib) concatLists mapAttrsToList showOption;
 in
 rec {
-  flattenMapServicesConfigToList = f: loc: config:
+  flattenMapServicesConfigToList =
+    f: loc: config:
     f loc config
-    ++ concatLists
-        (mapAttrsToList
-          (k: v:
-            flattenMapServicesConfigToList
-              f
-              (loc ++ ["services" k])
-              v)
-          config.services);
+    ++ concatLists (
+      mapAttrsToList (
+        k: v:
+        flattenMapServicesConfigToList f (
+          loc
+          ++ [
+            "services"
+            k
+          ]
+        ) v
+      ) config.services
+    );
 
-  getWarnings =
-    flattenMapServicesConfigToList
-      (loc: config:
-        map (msg: "in ${showOption loc}: ${msg}") config.warnings);
+  getWarnings = flattenMapServicesConfigToList (
+    loc: config: map (msg: "in ${showOption loc}: ${msg}") config.warnings
+  );
 
-  getAssertions =
-    flattenMapServicesConfigToList
-      (loc: config:
-        map
-          (ass: {
-            message = "in ${showOption loc}: ${ass.message}";
-            assertion = ass.assertion;
-          })
-          config.assertions);
+  getAssertions = flattenMapServicesConfigToList (
+    loc: config:
+    map (ass: {
+      message = "in ${showOption loc}: ${ass.message}";
+      assertion = ass.assertion;
+    }) config.assertions
+  );
 }
