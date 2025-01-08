fcee17f472747fbe188c75e32e030512989840da
FliegendeWurst <arne.keller@posteo.de>
2025-01-08 08:45:26 +0100

    simg2img: fix GCC 14 build

--- pkgs/by-name/si/simg2img/package.nix
+++ pkgs/by-name/si/simg2img/package.nix
@@ -17,6 +17,17 @@ stdenv.mkDerivation rec {
     hash = "sha256-sNHdSbms35YnENASFEG+VMLJGkV/JAlQUVMquDrePDc=";
   };
 
+  # fix GCC 14 error
+  # https://github.com/anestisb/android-simg2img/pull/41
+  postPatch = ''
+    substituteInPlace backed_block.cpp \
+      --replace-fail 'calloc(sizeof(struct backed_block_list), 1));' 'calloc(1, sizeof(struct backed_block_list)));'
+    substituteInPlace sparse.cpp \
+      --replace-fail 'calloc(sizeof(struct sparse_file), 1));' 'calloc(1, sizeof(struct sparse_file)));'
+    substituteInPlace simg2simg.cpp \
+      --replace-fail 'calloc(sizeof(struct sparse_file*), files);' 'calloc(files, sizeof(struct sparse_file*));'
+  '';
+
   buildInputs = [ zlib ];
 
   makeFlags = [
