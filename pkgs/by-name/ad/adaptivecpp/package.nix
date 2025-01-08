91842dcd27c5fc6e137f40b2d4e4ef0a7c7d0091
blenderfreaky <blenderfreaky@mailbox.org>
2025-01-08 13:41:10 +0100

    adaptivecpp: mark as broken for rocm <6.3.1

--- pkgs/by-name/ad/adaptivecpp/package.nix
+++ pkgs/by-name/ad/adaptivecpp/package.nix
@@ -111,6 +111,7 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Multi-backend implementation of SYCL for CPUs and GPUs";
     mainProgram = "acpp";
     maintainers = with maintainers; [ yboettcher ];
+    broken = rocmSupport && !(lib.versionAtLeast rocmPackages.clr.version "6.3.1");
     license = licenses.bsd2;
   };
 })
