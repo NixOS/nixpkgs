dd3c5771a1a43c11253fec060e68a3f549a1c43c
Gaetan Lepage <gaetan@glepage.com>
2025-01-08 22:12:29 +0100

    protobuf: 29.2 -> 29.3
    Diff: https://github.com/protocolbuffers/protobuf/compare/v29.2...v29.3
    
    Changelog:
    https://github.com/protocolbuffers/protobuf/releases/tag/v29.3

--- pkgs/development/python-modules/protobuf/default.nix
+++ pkgs/development/python-modules/protobuf/default.nix
@@ -8,12 +8,12 @@
 
 buildPythonPackage rec {
   pname = "protobuf";
-  version = "5.29.2";
+  version = "5.29.3";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-ssyOi7fJMmmW8OFgE3sIYfGoIWJQJljfKVEgnQywMJ4=";
+    hash = "sha256-XaD0HtrxF73jFkBLrRpIbLTt7fjkpUiRKW9kjo4HZiA=";
   };
 
   build-system = [ setuptools ];
