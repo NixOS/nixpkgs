2c77ba2460983b3b2155d55a6b4c051a7fca8873
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-08 11:33:06 +0000

    nzbhydra2: 7.11.0 -> 7.11.3

--- pkgs/by-name/nz/nzbhydra2/package.nix
+++ pkgs/by-name/nz/nzbhydra2/package.nix
@@ -10,11 +10,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "nzbhydra2";
-  version = "7.11.0";
+  version = "7.11.3";
 
   src = fetchzip {
     url = "https://github.com/theotherp/nzbhydra2/releases/download/v${version}/nzbhydra2-${version}-generic.zip";
-    hash = "sha256-vFPpBM438TU9kthrt80soNki0T34j6EktWYhvIJNhWw=";
+    hash = "sha256-OR0Yk0tVajfMtMHisWqBbLeCScgenjyYTmNWTcMG/KE=";
     stripRoot = false;
   };
 
