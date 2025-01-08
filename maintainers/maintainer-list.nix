b558ea07704886c5154057837c2de175d80ddf45
ZarredFelicite <zarred.f@gmail.com>
2025-01-08 11:28:24 +1100

    maintainers: add zsenai

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25559,6 +25559,12 @@
     githubId = 8100652;
     name = "David Mell";
   };
+  zsenai = {
+    email = "zarred.f@gmail.com";
+    github = "ZarredFelicite";
+    githubId = 54928291;
+    name = "Zarred Felicite";
+  };
   zshipko = {
     email = "zachshipko@gmail.com";
     github = "zshipko";
