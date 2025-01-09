44df1ea19854d5c3205c0c69a438a9b11c2e5a8a
Mephist0phel3s <alaskandrifter49@icloud.com>
2025-01-09 10:22:03 -0600

    s

--- pkgs/games/WarThunder/default.nix
+++ pkgs/games/WarThunder/default.nix
@@ -145,14 +145,14 @@ libPath = lib.makeLibraryPath [ autoPatchelfHook stdenv.cc.cc stdenv.cc.cc.lib r
   '';
 
 
-  desktopItem = [(makeDesktopItem {
+  desktopItem = (makeDesktopItem = {
     name = "War-Thunder";
     exec = "acesx86_64";
     icon = "launcher";
     desktopName = "War Thunder";
     genericName = "War Thunder";
     categories = [ "Game" ];
-    })];
+    });
 
 
   meta = with lib; {
