0db972d556e9a2c25c27c78624e6f4ebc85c735a
toonn <toonn@toonn.io>
2025-01-09 15:06:21 +0100

    wire-desktop: mac 3.37.5164 -> 3.38.5196

--- pkgs/by-name/wi/wire-desktop/package.nix
+++ pkgs/by-name/wi/wire-desktop/package.nix
@@ -28,7 +28,7 @@ let
 
   version =
     let
-      x86_64-darwin = "3.37.5164";
+      x86_64-darwin = "3.38.5196";
     in
     {
       inherit x86_64-darwin;
@@ -39,7 +39,7 @@ let
 
   hash =
     let
-      x86_64-darwin = "sha256-xQ6EaZFE6qdpgFj1Lom37wT8URd5FsRwG89YeM/+U7k=";
+      x86_64-darwin = "sha256-AdimMmbkF2cNgidze1utBgAsK+hAKW+h3nWvV4qZsn4=";
     in
     {
       inherit x86_64-darwin;
