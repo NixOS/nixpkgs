2ac403cc7cf211744f4dca0a19d241a25e84fe71
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-09 19:09:39 +0000

    libretro.fbneo: 0-unstable-2024-10-03 -> 0-unstable-2025-01-06

--- pkgs/applications/emulators/libretro/cores/fbneo.nix
+++ pkgs/applications/emulators/libretro/cores/fbneo.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "fbneo";
-  version = "0-unstable-2024-10-03";
+  version = "0-unstable-2025-01-06";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "fbneo";
-    rev = "d72f49f4a45dbfc5a855956d1a75ce2d0601c1c5";
-    hash = "sha256-+T+HQo6IfY8+oE/mOg54Vn9NhasGYNCLXksFdSDT/xE=";
+    rev = "b8780c057029db8768c9a057b0bc28f9a12609d8";
+    hash = "sha256-cK3ILA0Ape6rHf5dPbXOMmQ69ZPZ/qrxeKYA1LniBEk=";
   };
 
   makefile = "Makefile";
