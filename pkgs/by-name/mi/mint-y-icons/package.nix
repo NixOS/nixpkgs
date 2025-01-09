0b011fda2f4b6e93b1f7bfd68b29ea7b8f6a6ca1
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-09 03:50:37 +0000

    mint-y-icons: 1.8.0 -> 1.8.1

--- pkgs/by-name/mi/mint-y-icons/package.nix
+++ pkgs/by-name/mi/mint-y-icons/package.nix
@@ -10,13 +10,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "mint-y-icons";
-  version = "1.8.0";
+  version = "1.8.1";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-X62n7HQwlpnfl4rqy6CK1K9Do/tUyD8pB0bRW8djm6o=";
+    hash = "sha256-hVPDuDhmYhc+m+W703T9VdSHB5z0e6lMPDHYkcoB6nM=";
   };
 
   propagatedBuildInputs = [
