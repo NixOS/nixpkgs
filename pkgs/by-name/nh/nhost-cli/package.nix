3b44a8f198250f04db99acfbf91c51da91433cd9
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-09 02:42:46 +0000

    nhost-cli: 1.28.2 -> 1.28.3

--- pkgs/by-name/nh/nhost-cli/package.nix
+++ pkgs/by-name/nh/nhost-cli/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "nhost-cli";
-  version = "1.28.2";
+  version = "1.28.3";
 
   src = fetchFromGitHub {
     owner = "nhost";
     repo = "cli";
     tag = "v${version}";
-    hash = "sha256-MnsF4/TCl3AwBsYkT6eMLyjKY+7WcRpXT0fl8hdP/pA=";
+    hash = "sha256-RLWS7/SI2duolOdXKEKCpZHGne+Cl6ALLOznrvAUd6Q=";
   };
 
   vendorHash = null;
