76b4fe2fbb345f846b6c07cf4b8f647a46fcf306
Alexis Praga <alexis@praga.dev>
2025-01-09 22:26:53 +0100

    spectra: init at 0.0.11.
    Dependency for multiqc

---
+++ pkgs/development/python-modules/spectra/default.nix
@@ -0,0 +1,25 @@
+{
+  buildPythonPackage,
+  colormath,
+  fetchPypi,
+  lib,
+}:
+
+buildPythonPackage rec {
+  version = "0.0.11";
+  pname = "spectra";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-jrNipRh8tjzuE80BGGeZwMeRo6077FeyeRMuElIXYrg=";
+  };
+
+  propagatedBuildInputs = [ colormath ];
+
+  meta = with lib; {
+    description = "Python library that makes color math, color scales, and color-space conversion easy";
+    homepage = "https://github.com/jsvine/spectra";
+    license = licenses.mit;
+    maintainers = with maintainers; [ apraga ];
+  };
+}
