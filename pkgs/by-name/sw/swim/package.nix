b9e9f5782057a5fa49c9a6ef86b6c3c8bd1492ff
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-01-09 16:48:21 +0000

    swim: 0.11.0 -> 0.12.0
    Diff: https://gitlab.com/spade-lang/swim/-/compare/v0.11.0...v0.12.0
    
    Changelog: https://gitlab.com/spade-lang/swim/-/blob/v0.12.0/CHANGELOG.md

--- pkgs/by-name/sw/swim/package.nix
+++ pkgs/by-name/sw/swim/package.nix
@@ -12,16 +12,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "swim";
-  version = "0.11.0";
+  version = "0.12.0";
 
   src = fetchFromGitLab {
     owner = "spade-lang";
     repo = "swim";
     rev = "v${version}";
-    hash = "sha256-1F9C7robIe/8pWh9Dl/CdyvKEZhrFkLrp5/8QC9Z0X4=";
+    hash = "sha256-GJQzz9o0QKbnE8l1MEjdW36S/rJo2GoaK02Qnl9dtEA=";
   };
 
-  cargoHash = "sha256-kY3eFnei70jfco+7TK9WS/YvbR/hf4JtrUbaRg7KN+8=";
+  cargoHash = "sha256-OQpKy/TVsJV/AjqvvdqTnAnAc1+YhN83N1EyNfgF4qg=";
 
   preConfigure = ''
     # de-vendor spade git submodule
