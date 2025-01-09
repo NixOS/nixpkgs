d519cb43a5bc3c6429c30b5f6f0619289fea6a55
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-01-09 02:34:34 +0100

    ocamlPackages_5_3.ocaml: init at 5.3.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -2099,7 +2099,9 @@ in let inherit (pkgs) callPackage; in rec
 
   ocamlPackages_5_2 = mkOcamlPackages (callPackage ../development/compilers/ocaml/5.2.nix { });
 
-  ocamlPackages_latest = ocamlPackages_5_2;
+  ocamlPackages_5_3 = mkOcamlPackages (callPackage ../development/compilers/ocaml/5.3.nix { });
+
+  ocamlPackages_latest = ocamlPackages_5_3;
 
   ocamlPackages = ocamlPackages_5_2;
 
