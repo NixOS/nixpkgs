651e59260a31bf9b4cf642ceb51812a80e0dda9d
9R <git@9-r.net>
2025-01-09 18:18:13 +0100

    python3.pkgs.liquidctl: 1.13.0 -> 1.14.0

--- pkgs/development/python-modules/liquidctl/default.nix
+++ pkgs/development/python-modules/liquidctl/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "liquidctl";
-  version = "1.13.0";
+  version = "1.14.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -30,16 +30,8 @@ buildPythonPackage rec {
     owner = pname;
     repo = pname;
     tag = "v${version}";
-    hash = "sha256-LU8rQmXrEIoOBTTFotGvMeHqksYGrtNo2YSl2l2e/UI=";
+    hash = "sha256-HkMxYULeWcAYdlEI4N4qv7AGh/+xY0zuvV2mFPStPq8=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "tests-pillow-10.2.0-compat.patch";
-      url = "https://github.com/liquidctl/liquidctl/commit/c50afa4e610bd2e268e85c347e2644794c817a78.diff";
-      hash = "sha256-1cKk3drl3RybHmnPXdlJoeYK6UDz25jHSS2YS/XLHIY=";
-    })
-  ];
 
   nativeBuildInputs = [
     installShellFiles
