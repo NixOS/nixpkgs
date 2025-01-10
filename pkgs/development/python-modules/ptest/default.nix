8e6fb30f2984aa02ba5b258a7dc318bef83f4504
natsukium <tomoya.otabi@gmail.com>
2025-01-10 23:39:47 +0900

    python312Packages.ptest: refactor

--- pkgs/development/python-modules/ptest/default.nix
+++ pkgs/development/python-modules/ptest/default.nix
@@ -2,13 +2,14 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   nix-update-script,
 }:
 
 buildPythonPackage rec {
   pname = "ptest";
   version = "2.0.3";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "KarlGong";
@@ -17,6 +18,13 @@ buildPythonPackage rec {
     hash = "sha256-lmiBqFWGfYdsBXCh6dQ9xed+HhpP6PWa9Csr68GtLxs=";
   };
 
+  build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "ptest" ];
+
+  # I don't know how to run the tests
+  doCheck = false;
+
   passthru.updateScript = nix-update-script {
     extraArgs = [
       "--version-regex"
@@ -24,9 +32,10 @@ buildPythonPackage rec {
     ];
   };
 
-  meta = with lib; {
+  meta = {
     description = "Test classes and test cases using decorators, execute test cases by command line, and get clear reports";
     homepage = "https://pypi.python.org/pypi/ptest";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
+    mainProgram = "ptest";
   };
 }
