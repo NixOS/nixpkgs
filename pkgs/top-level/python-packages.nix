daf2506c851e123649fe524c7899e6a13ffbd6ac
Luka Leer <luka.leer@gmail.com>
2025-01-10 22:32:49 +0100

    python3Packages.mkdocs-material-insiders: init at 9.5.49-insiders-4.53.14

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8291,6 +8291,8 @@ self: super: with self; {
 
   mkdocs-material-extensions = callPackage ../development/python-modules/mkdocs-material/mkdocs-material-extensions.nix { };
 
+  mkdocs-material-insiders = callPackage ../development/python-modules/mkdocs-material-insiders { };
+
   mkdocs-mermaid2-plugin = callPackage ../development/python-modules/mkdocs-mermaid2-plugin { };
 
   mkdocs-minify-plugin = callPackage ../development/python-modules/mkdocs-minify-plugin { };
