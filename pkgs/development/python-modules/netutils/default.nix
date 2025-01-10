a35a1abc6d459fced8da00e2c0c8f43ae0955d2a
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-10 12:44:41 +0100

    python313Packages.netutils: 1.10.0 -> 1.12.0
    Diff: https://github.com/networktocode/netutils/compare/refs/tags/v1.10.0...v1.12.0
    
    Changelog: https://github.com/networktocode/netutils/releases/tag/v1.12.0

--- pkgs/development/python-modules/netutils/default.nix
+++ pkgs/development/python-modules/netutils/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "netutils";
-  version = "1.10.0";
+  version = "1.12.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "networktocode";
     repo = "netutils";
     tag = "v${version}";
-    hash = "sha256-VhX0KDlGf0J6fiO1RzOoqJ4WMDM8Bb2+lYYMlgQ9nkc=";
+    hash = "sha256-LslE/ROfr/2gU5VPiLKgaTxd4miM+Bvrh9OdY06zkr0=";
   };
 
   build-system = [ poetry-core ];
