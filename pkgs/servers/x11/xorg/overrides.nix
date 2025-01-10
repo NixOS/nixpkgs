5a84512b4d4fb691687afe5b843630cbc4730abe
Alexander V. Nikolaev <avn@avnik.info>
2025-01-10 17:54:00 +0200

    xorg.{utilmacros,xkeyboardconfig}: treat autotools as native inputs
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- pkgs/servers/x11/xorg/overrides.nix
+++ pkgs/servers/x11/xorg/overrides.nix
@@ -468,7 +468,7 @@ self: super:
   transset = addMainProgram super.transset { };
 
   utilmacros = super.utilmacros.overrideAttrs (attrs: { # not needed for releases, we propagate the needed tools
-    propagatedBuildInputs = attrs.propagatedBuildInputs or [] ++ [ automake autoconf libtool ];
+    propagatedNativeBuildInputs = attrs.propagatedNativeBuildInputs or [] ++ [ automake autoconf libtool ];
   });
 
   viewres = addMainProgram super.viewres { };
@@ -725,7 +725,7 @@ self: super:
     '';
   in
     xorg.xkeyboardconfig.overrideAttrs (old: {
-      buildInputs = old.buildInputs ++ [ automake ];
+      nativeBuildInputs = old.nativeBuildInputs ++ [ automake ];
       postPatch   = lib.concatStrings (lib.mapAttrsToList patchIn layouts);
     });
 
