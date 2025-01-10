6b8e2709d6e7976cb9c8bcfbe6c1478031a47cc8
aucub <78630225+aucub@users.noreply.github.com>
2025-01-10 08:24:53 +0800

    waydroid-helper: init at 0.1.2

---
+++ pkgs/by-name/wa/waydroid-helper/package.nix
@@ -0,0 +1,93 @@
+{
+  lib,
+  fetchFromGitHub,
+  nix-update-script,
+  desktop-file-utils,
+  libadwaita,
+  meson,
+  ninja,
+  pkg-config,
+  wrapGAppsHook4,
+  libxml2,
+  python3Packages,
+  appstream,
+  glib,
+  cmake,
+  dbus,
+  systemd,
+  bash,
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "waydroid-helper";
+  version = "0.1.2";
+  pyproject = false; # uses meson
+
+  src = fetchFromGitHub {
+    owner = "ayasa520";
+    repo = "waydroid-helper";
+    tag = "v${version}";
+    hash = "sha256-dYduO5Wi8Ia/pR1xQKPhC6Ek/1Q9fm2RaVuhm9KYiU0=";
+  };
+
+  postPatch = ''
+    substituteInPlace dbus/meson.build \
+      --replace-fail "dbus_policy_dir," "'${placeholder "out"}/share/dbus-1/system.d'," \
+      --replace-fail "dbus_service_dir," "'${placeholder "out"}/share/dbus-1/system-services',"
+    substituteInPlace systemd/meson.build \
+      --replace-fail ": systemd_system_unit_dir" ": '${placeholder "out"}/lib/systemd/system'" \
+      --replace-fail ": systemd_user_unit_dir" ": '${placeholder "out"}/lib/sysusers.d'"
+    sed -i '/test(/{N;/Validate appstream file/!b;:a;N;/)/!ba;d}' data/meson.build
+    substituteInPlace waydroid_helper/waydroid-cli.in \
+      --replace-fail "/bin/bash" "${bash}/bin/bash"
+  '';
+
+  nativeBuildInputs = [
+    appstream
+    glib
+    cmake
+    meson
+    ninja
+    pkg-config
+    wrapGAppsHook4
+    desktop-file-utils
+  ];
+
+  buildInputs = [
+    libxml2
+    libadwaita
+    dbus
+    systemd
+  ];
+
+  dontUseCmakeConfigure = true;
+
+  dependencies = with python3Packages; [
+    pygobject3
+    httpx
+    pyyaml
+    aiofiles
+    dbus-python
+  ];
+
+  strictDeps = true;
+
+  dontWrapGApps = true;
+
+  makeWrapperArgs = [ "\${gappsWrapperArgs[@]}" ];
+
+  postInstallCheck = ''
+    mesonCheckPhase
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Provides a user-friendly way to configure Waydroid and install extensions";
+    homepage = "https://github.com/ayasa520/waydroid-helper";
+    mainProgram = "waydroid-helper";
+    platforms = lib.platforms.linux;
+    license = with lib.licenses; [ gpl3Plus ];
+    maintainers = with lib.maintainers; [ aucub ];
+  };
+}
