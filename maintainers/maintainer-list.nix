05ba0713450d93d11da2e8b5ed605aad9b772ee7
Malik <abdelmalik.najhi@stud.hs-kempten.de>
2025-01-10 19:45:06 +0100

    maintainers: add malik

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13868,6 +13868,13 @@
     githubId = 1678126;
     name = "Marco A L Barbosa";
   };
+  malik = {
+    name = "Malik";
+    email = "abdelmalik.najhi@stud.hs-kempten.de";
+    github = "malikwirin";
+    githubId = 117918464;
+    keys = [ { fingerprint = "B5ED 595C 8C7E 133C 6B68  63C8 CFEF 1E35 0351 F72D"; } ];
+  };
   malo = {
     email = "mbourgon@gmail.com";
     github = "malob";
