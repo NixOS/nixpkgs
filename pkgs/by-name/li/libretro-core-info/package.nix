26f3b7fa0b9a73ac7e49fd7d224161a43c29358e
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-10 07:07:01 +0000

    libretro-core-info: 1.19.0 -> 1.20.0

--- pkgs/by-name/li/libretro-core-info/package.nix
+++ pkgs/by-name/li/libretro-core-info/package.nix
@@ -7,13 +7,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "libretro-core-info";
-  version = "1.19.0";
+  version = "1.20.0";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "libretro-core-info";
     rev = "v${version}";
-    hash = "sha256-dMMX9i2xPKay6BKC52fYxElcGSllHkSZyOXX/t3l6Io=";
+    hash = "sha256-T/La/Y9txRX8SHAyOi45KcQA8zb3puXdfTKlPSm42+U=";
   };
 
   makeFlags = [
