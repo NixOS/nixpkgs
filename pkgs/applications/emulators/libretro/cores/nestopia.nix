52a764450bf4dfdc54311612d2a77ad7ca6499d4
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-01-10 18:08:25 +0000

    libretro.nestopia: 0-unstable-2024-12-22 -> 0-unstable-2025-01-05

--- pkgs/applications/emulators/libretro/cores/nestopia.nix
+++ pkgs/applications/emulators/libretro/cores/nestopia.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "nestopia";
-  version = "0-unstable-2024-12-22";
+  version = "0-unstable-2025-01-05";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "nestopia";
-    rev = "6bbfff9a56ead67f0da696ab2c3aea3c11896964";
-    hash = "sha256-D2FtfabikcZq0dl+ot/NJJkOaQXj0Sl5P2ioNrvxgSs=";
+    rev = "9762adc00668f3a2e1016f3ad07ff9cbf9d67459";
+    hash = "sha256-CLEwhQ91dxoTLyhlQwssoCL/dEqY6SetwWLogfJi8RU=";
   };
 
   makefile = "Makefile";
