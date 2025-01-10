7ec59c4a666a9f1f612554cd2a8e10d3c7fa3a41
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-10 15:01:56 +0000

    rasm: 2.2.11 -> 2.3.0

--- pkgs/by-name/ra/rasm/package.nix
+++ pkgs/by-name/ra/rasm/package.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "rasm";
-  version = "2.2.11";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "EdouardBERGE";
     repo = "rasm";
     rev = "v${version}";
-    hash = "sha256-wFdRMWnOZ3gVp9RwTwD1bJEKVJ9khPRSQoCi75/YiPM=";
+    hash = "sha256-xDiGVPXi12P2YzSESO4I4WGeQgWU9UPhJbenoN8sADc=";
   };
 
   # by default the EXEC variable contains `rasm.exe`
