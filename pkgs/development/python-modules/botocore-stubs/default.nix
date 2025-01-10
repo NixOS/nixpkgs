ce2499ed45275e3b60175844e55a0e04469b3cc7
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-10 08:45:11 +0100

    python313Packages.botocore-stubs: 1.35.95 -> 1.35.96

--- pkgs/development/python-modules/botocore-stubs/default.nix
+++ pkgs/development/python-modules/botocore-stubs/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "botocore-stubs";
-  version = "1.35.95";
+  version = "1.35.96";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "botocore_stubs";
     inherit version;
-    hash = "sha256-ZLiCnm4QcizMGrbWjl4LG/MZizsRvn0XRxGdfFzDnpk=";
+    hash = "sha256-pBfc3PEn2mRMl1ljvHQDsYjSrlqMG7kHhJ1LhoHk+5A=";
   };
 
   nativeBuildInputs = [ setuptools ];
