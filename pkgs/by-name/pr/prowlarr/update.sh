5151af3ed27639c89f032d8c9f52b001cf195e58
Pim Kunis <pim@kunis.nl>
2025-01-12 00:12:21 +0100

    prowlarr: move to pkgs/by-name
    (cherry picked from commit 010dba88d4e9a3186213d0f54c0bf91de65073d9)

--- pkgs/by-name/pr/prowlarr/update.sh
+++ pkgs/by-name/pr/prowlarr/update.sh
@@ -19,15 +19,15 @@ updateHash()
     hash=$(nix-prefetch-url --type sha256 $url)
     sriHash="$(nix hash to-sri --type sha256 $hash)"
 
-    sed -i "s|$system = \"sha256-[a-zA-Z0-9\/+-=]*\";|$system = \"$sriHash\";|g" "$dirname/default.nix"
+    sed -i "s|$system = \"sha256-[a-zA-Z0-9\/+-=]*\";|$system = \"$sriHash\";|g" "$dirname/package.nix"
 }
 
 updateVersion()
 {
-    sed -i "s/version = \"[0-9.]*\";/version = \"$1\";/g" "$dirname/default.nix"
+    sed -i "s/version = \"[0-9.]*\";/version = \"$1\";/g" "$dirname/package.nix"
 }
 
-currentVersion=$(cd $dirname && nix eval --raw -f ../../.. prowlarr.version)
+currentVersion=$(cd $dirname && nix eval --raw -f ../../../.. prowlarr.version)
 
 latestTag=$(curl https://api.github.com/repos/Prowlarr/Prowlarr/releases/latest | jq -r ".tag_name")
 latestVersion="$(expr $latestTag : 'v\(.*\)')"
