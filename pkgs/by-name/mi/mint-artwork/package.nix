daf46f59c2356cd730db9370acd8396500528b14
Bobby Rong <rjl931189261@126.com>
2025-01-11 11:00:13 +0800

    mint-artwork: 1.8.4 -> 1.8.9
    * cinnamon styles: Remove references to obsolete theme

--- pkgs/by-name/mi/mint-artwork/package.nix
+++ pkgs/by-name/mi/mint-artwork/package.nix
@@ -8,14 +8,14 @@
 
 stdenv.mkDerivation rec {
   pname = "mint-artwork";
-  version = "1.8.4";
+  version = "1.8.9";
 
   src = fetchurl {
     urls = [
       "http://packages.linuxmint.com/pool/main/m/mint-artwork/mint-artwork_${version}.tar.xz"
-      "https://web.archive.org/web/20240707114553/http://packages.linuxmint.com/pool/main/m/mint-artwork/mint-artwork_${version}.tar.xz"
+      "https://web.archive.org/web/20250111022232/http://packages.linuxmint.com/pool/main/m/mint-artwork/mint-artwork_${version}.tar.xz"
     ];
-    hash = "sha256-DpRoGTVB1vaif4UNiCEaVO/pyQY8Hc/QG1vmYkVr3Iw=";
+    hash = "sha256-MKXKne3wqxfIqmOawpmZbX/NRVSA5fBetpSE+mr/eqA=";
   };
 
   nativeBuildInputs = [
