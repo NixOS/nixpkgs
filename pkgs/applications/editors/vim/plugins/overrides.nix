4b5fd0d457583129544fe7453780b96a4bec87e1
"thinceller" <"thinceller@gmail.com">
2025-01-11 20:08:22 +0900

    vimPlugins.ddc-fuzzy: init at 2025-01-07

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -916,6 +916,10 @@ in
     dependencies = [ self.ddc-vim ];
   };
 
+  ddc-fuzzy = super.ddc-fuzzy.overrideAttrs {
+    dependencies = [ self.ddc-vim ];
+  };
+
   defx-nvim = super.defx-nvim.overrideAttrs {
     dependencies = [ self.nvim-yarp ];
   };
