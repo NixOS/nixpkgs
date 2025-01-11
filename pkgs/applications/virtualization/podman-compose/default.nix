65b9553bc0b66bb8bc00cb9ec3ad4dc52481130b
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-11 12:09:32 +0000

    podman-compose: 1.2.0 -> 1.3.0

--- pkgs/applications/virtualization/podman-compose/default.nix
+++ pkgs/applications/virtualization/podman-compose/default.nix
@@ -9,15 +9,15 @@
 }:
 
 buildPythonApplication rec {
-  version = "1.2.0";
+  version = "1.3.0";
   pname = "podman-compose";
   pyproject = true;
 
   src = fetchFromGitHub {
     repo = "podman-compose";
     owner = "containers";
-    rev = "v${version}";
-    hash = "sha256-40RatexY/6eRfCodaiBeJpyt1sDUj2STSPL0gBECdRs=";
+    tag = "v${version}";
+    hash = "sha256-0k+vJwWYEXQ6zxkcvjxBv9cq8nIBS15F7ul5VwqYtys=";
   };
 
   build-system = [
