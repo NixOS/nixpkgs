2a6ba68c88497faa0b09b88958d620c45c614092
OPNA2608 <opna2608@protonmail.com>
2025-01-12 01:32:45 +0100

    famistudio: 4.3.0 -> 4.3.1

--- pkgs/by-name/fa/famistudio/package.nix
+++ pkgs/by-name/fa/famistudio/package.nix
@@ -28,13 +28,13 @@ let
 in
 buildDotnetModule (finalAttrs: {
   pname = "famistudio";
-  version = "4.3.0";
+  version = "4.3.1";
 
   src = fetchFromGitHub {
     owner = "BleuBleu";
     repo = "FamiStudio";
     tag = finalAttrs.version;
-    hash = "sha256-Ldht7w1qgLTiqbRUJJvFQgl1VW6k+14w/jz58kAeMl0=";
+    hash = "sha256-ISOMsnsvsO3wOp9J/CilCr4wBgkHc29od2a2sBssN7k=";
   };
 
   postPatch =
