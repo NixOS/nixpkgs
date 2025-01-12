fe79487033013ddf1293c46eb7a66abb7947728d
John <johnramsden@riseup.net>
2025-01-11 16:51:31 -0800

    maintainers: remove johnramsden (#373017)
    * Remove maintainer
    
    * Remove John Ramsden from maintainer-list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11068,12 +11068,6 @@
     github = "johnpyp";
     githubId = 20625636;
   };
-  johnramsden = {
-    email = "johnramsden@riseup.net";
-    github = "johnramsden";
-    githubId = 8735102;
-    name = "John Ramsden";
-  };
   johnrichardrinehart = {
     email = "johnrichardrinehart@gmail.com";
     github = "johnrichardrinehart";
