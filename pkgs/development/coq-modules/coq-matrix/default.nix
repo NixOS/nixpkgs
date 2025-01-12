f5b2fe5c3c2a6e444fc0dc435716991ae5aeabce
RungChungo <155569880+RungChungo@users.noreply.github.com>
2025-01-12 21:36:36 +0000

    Update default.nix

--- pkgs/development/coq-modules/coq-matrix/default.nix
+++ pkgs/development/coq-modules/coq-matrix/default.nix
@@ -10,10 +10,6 @@ mkCoqDerivation {
   release = {
     "1.0.6".sha256 = "sha256-Myxwy749ZCBpqia6bf91cMTyJn0nRzXskD7Ue8kc37c="
   };
-  releaseRev = v: "v${v}";
-  installPhase = ''
-    make -f Makefile.coq COQMF_COQLIB=$out/lib/coq/${coq.coq-version}/ install
-  '';
   meta = {
     homepage = "https://github.com/zhengpushi/CoqMatrix";
     description = "Matrix math";
