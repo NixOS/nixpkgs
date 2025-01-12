a02399eb94adfd464b045aaea29f5a215d16c74c
RungChungo <155569880+RungChungo@users.noreply.github.com>
2025-01-12 21:29:12 +0000

    Update default.nix

--- pkgs/development/coq-modules/coq-matrix/default.nix
+++ pkgs/development/coq-modules/coq-matrix/default.nix
@@ -1,1 +1,23 @@
-a
+{
+  lib,
+  mkCoqDerivation,
+  coq
+}:
+mkCoqDerivation {
+  pname = "matrix";
+  owner = "zhengpushi";
+  defaultVersion = "1.0.6";
+  release = {
+    "1.0.6".sha256 = "sha256-Myxwy749ZCBpqia6bf91cMTyJn0nRzXskD7Ue8kc37c="
+  };
+  releaseRev = v: "v${v}";
+  installPhase = ''
+    make -f Makefile.coq COQMF_COQLIB=$out/lib/coq/${coq.coq-version}/ install
+  '';
+  meta = {
+    homepage = "https://github.com/zhengpushi/CoqMatrix";
+    description = "Matrix math";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ damhiya ];
+  };
+}
