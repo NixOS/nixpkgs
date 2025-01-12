93c7b4328a72e6ba852f7edd290b27cd1807f740
RungChungo <155569880+RungChungo@users.noreply.github.com>
2025-01-12 21:53:50 +0000

    Update default.nix

--- pkgs/development/coq-modules/coq-matrix/default.nix
+++ pkgs/development/coq-modules/coq-matrix/default.nix
@@ -9,7 +9,7 @@ mkCoqDerivation {
   repo = "CoqMatrix";
   defaultVersion = "1.0.6";
   release = {
-    "1.0.6".sha256 = "sha256-Myxwy749ZCBpqia6bf91cMTyJn0nRzXskD7Ue8kc37c="
+    "1.0.6".sha256 = "sha256-Myxwy749ZCBpqia6bf91cMTyJn0nRzXskD7Ue8kc37c=";
   };
   meta = {
     homepage = "https://github.com/zhengpushi/CoqMatrix";
