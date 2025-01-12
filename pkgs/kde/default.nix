baf24d0443b72f92654fcc8f251133e411960f63
eymeric <eymericdechelette@gmail.com>
2025-01-12 12:34:59 +0100

    kde-geometry-change: init at 1.3

--- pkgs/kde/default.nix
+++ pkgs/kde/default.nix
@@ -87,6 +87,7 @@ let
 
         applet-window-buttons6 = self.callPackage ./third-party/applet-window-buttons6 { };
         karousel = self.callPackage ./third-party/karousel { };
+        kde-geometry-change = self.callPackage ./third-party/kde-geometry-change { };
         koi = self.callPackage ./third-party/koi { };
         krohnkite = self.callPackage ./third-party/krohnkite { };
         kzones = self.callPackage ./third-party/kzones { };
