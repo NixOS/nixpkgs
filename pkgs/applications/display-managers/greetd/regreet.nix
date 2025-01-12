1c456e2ade92c0794c13dd8ef4c8c9a3b41e526f
Mihai Fufezan <mihai@fufexan.net>
2025-01-12 15:13:42 +0200

    greetd.regreet: 0.1.3 -> 0.2.0

--- pkgs/applications/display-managers/greetd/regreet.nix
+++ pkgs/applications/display-managers/greetd/regreet.nix
@@ -12,16 +12,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "regreet";
-  version = "0.1.3";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "rharish101";
     repo = "ReGreet";
     rev = version;
-    hash = "sha256-PYBRfBdy6+cv3VKBFu5RUec/yfuKrAEkRxpanihIt1E=";
+    hash = "sha256-f8Xvno5QqmWz4SUiFYDvs8lFU1ZaqQ8gpTaVzWxW4T8=";
   };
 
-  cargoHash = "sha256-SUIyekcuDbPIh/9+EKaAoep9hZDJv8BW+ovtWyDqiCI=";
+  cargoHash = "sha256-XdJbghHT2NR+pWAsgayz748C8yw/Rv86lNfvjABwJws=";
 
   buildFeatures = [ "gtk4_8" ];
 
