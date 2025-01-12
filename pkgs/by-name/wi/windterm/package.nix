1b208f6a5d06491a0a5ea0c6040aeef1cda76407
aucub <78630225+aucub@users.noreply.github.com>
2025-01-12 15:07:46 +0800

    windterm: 2.6.1 -> 2.7-prerelease-2

--- pkgs/by-name/wi/windterm/package.nix
+++ pkgs/by-name/wi/windterm/package.nix
@@ -22,16 +22,15 @@
     path = "~";
   },
 }:
+
 stdenv.mkDerivation rec {
   pname = "windterm";
-  version = "2.6.1";
+  version = "2.7-prerelease-2";
 
   src = fetchurl {
-    url = "https://github.com/kingToolbox/WindTerm/releases/download/2.6.0/WindTerm_${version}_Linux_Portable_x86_64.tar.gz";
-    hash = "sha256-JwTsfUkESl2vUx48TanKYAOVWw6q4xGY+i0PrN9GfpA=";
+    url = "https://github.com/kingToolbox/WindTerm/releases/download/${version}/WindTerm_2.7.0_Prerelease_2_Linux_Portable_x86_64.tar.gz";
+    hash = "sha256-ZIFA3ASa3vQd1zr+8v4bA18NxGumNKP7f0cEzJVANEg=";
   };
-
-  dontBuild = true;
 
   nativeBuildInputs = [
     makeWrapper
@@ -41,19 +40,19 @@ stdenv.mkDerivation rec {
   buildInputs = [
     xorg.libxcb
     xorg.xcbutil
+    xorg.libXtst
     xorg.xcbutilimage
     xorg.xcbutilkeysyms
     xorg.xcbutilrenderutil
-    libsForQt5.qt5.qtbase
-    libsForQt5.qt5.qtmultimedia
+    libsForQt5.qtbase
+    libsForQt5.qtmultimedia
+    gst_all_1.gst-plugins-base
     fontconfig
     freetype
     libGL
     glib
     alsa-lib
     pulseaudio
-    gst_all_1.gst-plugins-base
-    xorg.libXtst
     gtk3
     atk
     pango
@@ -62,33 +61,32 @@ stdenv.mkDerivation rec {
     (lib.getLib stdenv.cc.cc)
   ];
 
+  dontBuild = true;
+
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out
-    cp -r ./* $out/
-    find $out -type d -exec chmod 755 {} \;
-    find $out -type f -exec chmod 644 {} \;
-    find $out -type f -executable -exec chmod 755 {} \;
-    chmod 755 $out/WindTerm
-    mkdir -p $out/bin/ $out/share/applications/ $out/share/pixmaps/ $out/share/licenses/${pname}/
-    cat > $out/profiles.config <<EOF
+    mkdir -p $out/app $out/share/applications $out/share/pixmaps $out/share/licenses/windterm
+    cp --recursive --no-preserve=mode . $out/app/windterm
+    cat > $out/app/windterm/profiles.config <<EOF
     ${builtins.toJSON profiles}
     EOF
-    mv $out/license.txt $out/share/licenses/${pname}/license.txt
-    mv $out/windterm.desktop $out/share/applications/windterm.desktop
-    mv $out/windterm.png $out/share/pixmaps/windterm.png
+    install -Dm644 $out/app/windterm/license.txt $out/share/licenses/windterm/license.txt
+    install -Dm644 $out/app/windterm/windterm.desktop $out/share/applications/windterm.desktop
+    install -Dm644 $out/app/windterm/windterm.png $out/share/pixmaps/windterm.png
     substituteInPlace $out/share/applications/windterm.desktop \
-      --replace-fail "Exec=/usr/bin/windterm" "Exec=windterm"
+      --replace-fail "/usr/bin/" ""
+    chmod +x $out/app/windterm/WindTerm
 
     runHook postInstall
   '';
 
   dontWrapQtApps = true;
 
-  preFixup = ''
-    makeWrapper $out/WindTerm $out/bin/windterm \
-      --prefix QT_PLUGIN_PATH : "$out/lib" \
+  postFixup = ''
+    mkdir $out/bin
+    makeWrapper $out/app/windterm/WindTerm $out/bin/windterm \
+      --prefix QT_PLUGIN_PATH : $out/app/windterm/lib \
       ''${qtWrapperArgs[@]}
   '';
 
@@ -96,9 +94,9 @@ stdenv.mkDerivation rec {
     description = "Professional cross-platform SSH/Sftp/Shell/Telnet/Serial terminal";
     homepage = "https://github.com/kingToolbox/WindTerm";
     mainProgram = "windterm";
-    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     license = lib.licenses.unfree;
-    maintainers = with lib.maintainers; [ aucub ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     platforms = [ "x86_64-linux" ];
+    maintainers = with lib.maintainers; [ aucub ];
   };
 }
