3eddefd0b4a0417139779c753a35e4b5ca4d384d
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-12 03:03:47 +0000

    python312Packages.edk2-pytool-library: 0.22.4 -> 0.22.5

--- pkgs/development/python-modules/edk2-pytool-library/default.nix
+++ pkgs/development/python-modules/edk2-pytool-library/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "edk2-pytool-library";
-  version = "0.22.4";
+  version = "0.22.5";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "tianocore";
     repo = "edk2-pytool-library";
     tag = "v${version}";
-    hash = "sha256-uTXE5b9iqGhrkbevz6GIw4MMDh3yA4Op0JHCTRo0ZfY=";
+    hash = "sha256-cnJTDMlkegd21X1meNTbS57y4qPYR+JkRtiTPof+PiQ=";
   };
 
   build-system = [
