3fe3266a151ba80cb6130a3ed889695bf04e5261
wxt <3264117476@qq.com>
2025-01-12 18:10:19 +0800

    n2048: fix build

--- pkgs/by-name/n2/n2048/package.nix
+++ pkgs/by-name/n2/n2048/package.nix
@@ -5,28 +5,39 @@
   ncurses,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "n2048";
   version = "0.1";
+
   src = fetchurl {
-    url = "http://www.dettus.net/n2048/n2048_v${version}.tar.gz";
-    sha256 = "184z2rr0rnj4p740qb4mzqr6kgd76ynb5gw9bj8hrfshcxdcg1kk";
+    url = "http://www.dettus.net/n2048/n2048_v${finalAttrs.version}.tar.gz";
+    hash = "sha256-c4bHWmdQuwyRXIm/sqw3p71pMv6VLAzIuUTaDHIWn6A=";
   };
+
+  env = {
+    NIX_CFLAGS_COMPILE = toString [
+      "-Wno-error=implicit-function-declaration"
+    ];
+  };
+
   buildInputs = [
     ncurses
   ];
+
   makeFlags = [
     "DESTDIR=$(out)"
   ];
+
   preInstall = ''
     mkdir -p "$out"/{share/man,bin}
   '';
-  meta = with lib; {
+
+  meta = {
     description = "Console implementation of 2048 game";
     mainProgram = "n2048";
-    license = licenses.bsd2;
-    maintainers = with maintainers; [ raskin ];
-    platforms = platforms.linux;
+    license = lib.licenses.bsd2;
+    maintainers = with lib.maintainers; [ raskin ];
+    platforms = lib.platforms.linux;
     homepage = "http://www.dettus.net/n2048/";
   };
-}
+})
