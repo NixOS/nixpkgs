1c38971f5e49648ad216df678ece0e2c64ecf2e6
Manuel Bergler <berglerma@gmail.com>
2025-01-12 11:38:46 +0100

    aqbanking: 6.5.12beta -> 6.6.0

--- pkgs/development/libraries/aqbanking/sources.nix
+++ pkgs/development/libraries/aqbanking/sources.nix
@@ -15,8 +15,8 @@
 
   # https://www.aquamaniac.de/rdm/projects/aqbanking/files
   aqbanking = {
-    version = "6.5.12beta";
-    hash = "sha256-TH6+eEiULmOciB1Mqo4vjgF9JbF4BW+llrTjS6BtctY=";
-    releaseId = "526";
+    version = "6.6.0";
+    hash = "sha256-N2NEh7lbrXKxshOXvOCMtLFeE8slOIZ2fJjzFxLkC/s=";
+    releaseId = "531";
   };
 }
