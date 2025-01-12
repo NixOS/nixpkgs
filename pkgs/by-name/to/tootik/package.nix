ef03167e42af24df002e1a554fa4c3342e3f0d42
Nikolay Korotkiy <sikmir@disroot.org>
2025-01-13 01:44:29 +0400

    tootik: 0.14.1 → 0.15.0

--- pkgs/by-name/to/tootik/package.nix
+++ pkgs/by-name/to/tootik/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "tootik";
-  version = "0.14.1";
+  version = "0.15.0";
 
   src = fetchFromGitHub {
     owner = "dimkr";
     repo = "tootik";
     tag = version;
-    hash = "sha256-uYMQ62OUDhd+LT7ORCDttDC+ojGN+7IDBpqKhShm6Nk=";
+    hash = "sha256-Onl64ps8rfLVnZYQ2KPzS+mslgeNz2wpJuabwsqhXlM=";
   };
 
   vendorHash = "sha256-GSi+sQz7kgp1YHEzH/Y7rOOEEhhvzd75cVhSow3URaU=";
