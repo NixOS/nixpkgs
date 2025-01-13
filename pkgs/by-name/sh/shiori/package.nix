24354a4acb8c8db0f7651a7ecccd98e1a69b52ff
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-13 07:28:07 +0000

    shiori: 1.7.3 -> 1.7.4

--- pkgs/by-name/sh/shiori/package.nix
+++ pkgs/by-name/sh/shiori/package.nix
@@ -9,7 +9,7 @@
 
 buildGoModule rec {
   pname = "shiori";
-  version = "1.7.3";
+  version = "1.7.4";
 
   vendorHash = "sha256-RTnaDAl79LScbeKKAGJOI/YOiHEwwlxS2CmNhw80KL0=";
 
@@ -19,7 +19,7 @@ buildGoModule rec {
     owner = "go-shiori";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-e60eeP0vgQ2/ZE/kO3LcG50EITeF1eN7m1/Md23/HwU=";
+    sha256 = "sha256-T4EFwvejLgNkcykPjSHU8WXJwqSqYPFaAD+9JX+uiJU=";
   };
 
   ldflags = [
