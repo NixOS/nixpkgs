f385b97133fb4695b41346b6a5c533910ecc975d
Peter Bex <peter@more-magic.net>
2025-01-13 09:02:56 +0100

    Apply feedback on PR

--- pkgs/os-specific/linux/purism/librem-ec-acpi/default.nix
+++ pkgs/os-specific/linux/purism/librem-ec-acpi/default.nix
@@ -5,15 +5,18 @@
   kernel,
 }:
 
-stdenv.mkDerivation rec {
+let
+  version = "0.9.2";
+in
+stdenv.mkDerivation {
   pname = "librem-ec-acpi";
-  version = "0.9.2-${kernel.version}";
+  version = "${version}-${kernel.version}";
 
   src = fetchFromGitLab {
     domain = "source.puri.sm";
     owner = "nicole.faerber";
     repo = "librem-ec-acpi-dkms";
-    rev = "cec8c0e8bf1532c9c605f60bb02a2ef0f98e5d77";
+    rev = "v${version}";
     sha256 = "gPb1/4UPbwjJiqtP27KKjiIKxIYEl2nlQACUAlxVEFU=";
   };
 
@@ -24,17 +27,17 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
-  buildFlags = [ "all" ];
-
   installFlags = [ "DEPMOD=true" ];
   enableParallelBuilding = true;
 
+  # Patch required to drop assumption of location of module build directory
+  # and ensure installation of compiled module
   patches = [ ./Makefile.patch ];
 
   meta = with lib; {
     homepage = "https://source.puri.sm/nicole.faerber/librem-ec-acpi-dkms";
     maintainers = [ maintainers.sjamaan ];
-    license = lib.licenses.gpl2;
+    license = lib.licenses.gpl2Only;
     description = "Kernel module for the Purism Librem EC ACPI DKMS";
     platforms = platforms.linux;
   };
