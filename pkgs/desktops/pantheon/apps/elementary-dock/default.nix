be992859a89003aac2e95318945b0dd2d1ea7f7b
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-13 16:31:19 +0000

    pantheon.elementary-dock: 8.0.1 -> 8.0.2

--- pkgs/desktops/pantheon/apps/elementary-dock/default.nix
+++ pkgs/desktops/pantheon/apps/elementary-dock/default.nix
@@ -17,7 +17,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "elementary-dock";
-  version = "8.0.1";
+  version = "8.0.2";
 
   outputs = [
     "out"
@@ -28,7 +28,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "elementary";
     repo = "dock";
     rev = finalAttrs.version;
-    hash = "sha256-Q4Y9FVqzPXoz2Nti1qB5SOJQ0tETPcv2fZPOMkJaND8=";
+    hash = "sha256-bixNYpPdWU2FndiCPX7SxNTz2MEttRuj35NaWn3GJrI=";
   };
 
   depsBuildBuild = [ pkg-config ];
