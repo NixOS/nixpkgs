b89f886685f3b872c14b9c28f3497090451dd5a8
Mutsuha Asada <me@momee.mt>
2025-01-13 23:43:57 +0900

    ocamlPackages.afl-persistent: 1.3 -> 1.4 Changelog: https://github.com/stedolan/ocaml-afl-persistent/releases/tag/v1.4 Diff: https://github.com/stedolan/ocaml-afl-persistent/compare/v1.3...v1.4

--- pkgs/development/ocaml-modules/afl-persistent/default.nix
+++ pkgs/development/ocaml-modules/afl-persistent/default.nix
@@ -1,46 +1,23 @@
 {
   lib,
-  stdenv,
   fetchFromGitHub,
-  ocaml,
-  findlib,
-  opaline,
+  buildDunePackage,
 }:
 
-stdenv.mkDerivation rec {
+buildDunePackage rec {
   pname = "afl-persistent";
-  version = "1.3";
-  name = "ocaml${ocaml.version}-${pname}-${version}";
+  version = "1.4";
+
+  minimalOCamlVersion = "4.05";
 
   src = fetchFromGitHub {
     owner = "stedolan";
     repo = "ocaml-${pname}";
     rev = "v${version}";
-    sha256 = "06yyds2vcwlfr2nd3gvyrazlijjcrd1abnvkfpkaadgwdw3qam1i";
+    sha256 = "sha256-kksGXaoO7lhaPI+rDOOQ39BUJjk6MGPBYQLumuW9gTg=";
   };
 
-  strictDeps = true;
-
-  nativeBuildInputs = [
-    ocaml
-    findlib
-  ];
-
-  # don't run tests in buildPhase
-  # don't overwrite test binary
-  postPatch = ''
-    sed -i 's/ && \.\/test$//' build.sh
-    sed -i '/^ocamlopt.*test.ml -o test$/ s/$/2/' build.sh
-    patchShebangs build.sh
-  '';
-
-  buildPhase = "./build.sh";
-  installPhase = ''
-    ${opaline}/bin/opaline -prefix $out -libdir $out/lib/ocaml/${ocaml.version}/site-lib/ ${pname}.install
-  '';
-
   doCheck = true;
-  checkPhase = "./_build/test && ./_build/test2";
 
   meta = with lib; {
     homepage = "https://github.com/stedolan/ocaml-afl-persistent";
