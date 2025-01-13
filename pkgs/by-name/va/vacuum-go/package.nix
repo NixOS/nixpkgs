7dd1809e22473e55b09ef8768c20e8c7018550bb
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-13 19:03:58 +0000

    vacuum-go: 0.15.0 -> 0.15.3

--- pkgs/by-name/va/vacuum-go/package.nix
+++ pkgs/by-name/va/vacuum-go/package.nix
@@ -8,14 +8,14 @@
 
 buildGoModule rec {
   pname = "vacuum-go";
-  version = "0.15.0";
+  version = "0.15.3";
 
   src = fetchFromGitHub {
     owner = "daveshanley";
     repo = "vacuum";
     # using refs/tags because simple version gives: 'the given path has multiple possibilities' error
     tag = "v${version}";
-    hash = "sha256-OsujmYu0QNyfyj6W8+aPcV4xsjpwZDWSJC7WjIw+rOc=";
+    hash = "sha256-SI32ODbd3X4zMhS3LI4vwVn9/gwgabWZ/8Nx+L3KPYE=";
   };
 
   vendorHash = "sha256-xTqrKkCRO6lUbzXI4/UrBoZsKU9mQW8cMrnZ2X3wzog=";
