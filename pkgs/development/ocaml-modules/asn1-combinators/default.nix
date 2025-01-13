d6ccc9702870ed1706153244088c370bd6559b19
Mutsuha Asada <me@momee.mt>
2025-01-14 00:08:14 +0900

    ocamlPackages.asn1-combinators: 0.3.1 -> 0.3.2 Changelog: https://github.com/mirleft/ocaml-asn1-combinators/releases/tag/v0.3.2 Diff: https://github.com/mirleft/ocaml-asn1-combinators/compare/v0.3.1...v0.3.2

--- pkgs/development/ocaml-modules/asn1-combinators/default.nix
+++ pkgs/development/ocaml-modules/asn1-combinators/default.nix
@@ -4,23 +4,24 @@
   fetchurl,
   ptime,
   alcotest,
+  ohex,
 }:
 
 buildDunePackage rec {
-  minimalOCamlVersion = "4.08";
+  minimalOCamlVersion = "4.13.0";
 
   pname = "asn1-combinators";
-  version = "0.3.1";
+  version = "0.3.2";
 
   src = fetchurl {
     url = "https://github.com/mirleft/ocaml-asn1-combinators/releases/download/v${version}/asn1-combinators-${version}.tbz";
-    hash = "sha256-+imExupuHhxP4gM/AWWvYRljwkAM4roFEAS3ffxVfE4=";
+    hash = "sha256-KyaYX24nIgc9zZ+ENVvWdX4SZDtaSOMLPAf/fPsNin8=";
   };
 
   propagatedBuildInputs = [ ptime ];
 
   doCheck = true;
-  checkInputs = [ alcotest ];
+  checkInputs = [ alcotest ohex ];
 
   meta = with lib; {
     homepage = "https://github.com/mirleft/ocaml-asn1-combinators";
