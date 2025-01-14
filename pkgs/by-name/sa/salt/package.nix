a57a29c2cfd8352db6741b9e0a69bded5be11974
SpiritLooper <julienclaisse@orange.fr>
2025-01-14 15:09:57 +0100

    salt: fix urllib.parse module
    Patch applied from Fedora
    https://src.fedoraproject.org/rpms/salt/blob/1c6e7b7a88fb81902f5fcee32e04fa80713b81f8/f/urllib.patch
    
    (cherry picked from commit 55e1713d43fee296bbe9c1131ae0fb83266a7c1e)

--- pkgs/by-name/sa/salt/package.nix
+++ pkgs/by-name/sa/salt/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   python3,
+  fetchpatch,
   fetchPypi,
   openssl,
   # Many Salt modules require various Python modules to be installed,
@@ -21,6 +22,11 @@ python3.pkgs.buildPythonApplication rec {
 
   patches = [
     ./fix-libcrypto-loading.patch
+    (fetchpatch {
+      name = "urllib.patch";
+      url = "https://src.fedoraproject.org/rpms/salt/raw/1c6e7b7a88fb81902f5fcee32e04fa80713b81f8/f/urllib.patch";
+      hash = "sha256-yldIurafduOAYpf2X0PcTQyyNjz5KKl/N7J2OTEF/c0=";
+    })
   ];
 
   postPatch = ''
