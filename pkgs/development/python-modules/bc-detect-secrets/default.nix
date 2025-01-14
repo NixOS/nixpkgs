7e43ccdf5de3ed8b4f68373f47bb491d2bb34220
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-14 20:48:00 +0100

    python313Packages.bc-detect-secrets: 1.5.33 -> 1.5.34
    Diff: https://github.com/bridgecrewio/detect-secrets/compare/refs/tags/1.5.33...1.5.34

--- pkgs/development/python-modules/bc-detect-secrets/default.nix
+++ pkgs/development/python-modules/bc-detect-secrets/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "bc-detect-secrets";
-  version = "1.5.33";
+  version = "1.5.34";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "bridgecrewio";
     repo = "detect-secrets";
     tag = version;
-    hash = "sha256-z/mjZcC1ZXebDWXlYvcdxDk+HoZ0gctfJSpQSkIbQmA=";
+    hash = "sha256-i03xAtdZUnKvhZTMw4AmavdGRC9hSf5FpdroJC/UQaU=";
   };
 
   build-system = [ setuptools ];
