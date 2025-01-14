a21c18ab26ee1b05d5143d3f2bac6f768cc85442
Mutsuha Asada <me@momee.mt>
2025-01-14 14:10:17 +0900

    ocamlPackages.bigstringaf: 0.9.0 -> 0.10.0 Changelog: https://github.com/inhabitedtype/bigstringaf/releases/tag/0.10.0 Diff: https://github.com/inhabitedtype/bigstringaf/compare/0.9.0...0.10.0

--- pkgs/development/ocaml-modules/bigstringaf/default.nix
+++ pkgs/development/ocaml-modules/bigstringaf/default.nix
@@ -4,23 +4,25 @@
   buildDunePackage,
   alcotest,
   pkg-config,
+  dune-configurator,
 }:
 
 buildDunePackage rec {
   pname = "bigstringaf";
-  version = "0.9.0";
+  version = "0.10.0";
 
   minimalOCamlVersion = "4.08";
-  duneVersion = "3";
 
   src = fetchFromGitHub {
     owner = "inhabitedtype";
     repo = pname;
     rev = version;
-    hash = "sha256-HXPjnE56auy2MI6HV2XuBX/VeqsO50HFzTul17lKEqE=";
+    hash = "sha256-p1hdB3ArOd2UX7S6YvXCFbYjEiXdMDmBaC/lFQgua7Q=";
   };
 
   nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ dune-configurator ];
+
   checkInputs = [ alcotest ];
   doCheck = true;
 
