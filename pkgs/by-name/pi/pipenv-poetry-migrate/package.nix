28c4ad4cc1ce692b0c8cbeb6d1022af2313b238e
natsukium <tomoya.otabi@gmail.com>
2025-01-14 18:37:38 +0900

    pipenv-poetry-migrate: refactor

--- pkgs/by-name/pi/pipenv-poetry-migrate/package.nix
+++ pkgs/by-name/pi/pipenv-poetry-migrate/package.nix
@@ -7,7 +7,7 @@
 python3Packages.buildPythonApplication rec {
   pname = "pipenv-poetry-migrate";
   version = "0.6.0";
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "yhino";
@@ -16,9 +16,9 @@ python3Packages.buildPythonApplication rec {
     hash = "sha256-M31bOvKGUlkzfZRQAxTkxhX8m9cCzEvsNZdyIyipwGI=";
   };
 
-  nativeBuildInputs = [ python3Packages.poetry-core ];
+  build-system = [ python3Packages.poetry-core ];
 
-  propagatedBuildInputs = with python3Packages; [
+  dependencies = with python3Packages; [
     setuptools # for pkg_resources
     tomlkit
     typer
@@ -26,12 +26,12 @@ python3Packages.buildPythonApplication rec {
 
   nativeCheckInputs = [ python3Packages.pytestCheckHook ];
 
-  meta = with lib; {
+  meta = {
     description = "This is simple migration script, migrate pipenv to poetry";
     mainProgram = "pipenv-poetry-migrate";
     homepage = "https://github.com/yhino/pipenv-poetry-migrate";
     changelog = "https://github.com/yhino/pipenv-poetry-migrate/blob/v${version}/CHANGELOG.md";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ gador ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ gador ];
   };
 }
