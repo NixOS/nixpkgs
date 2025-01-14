63b68332efcf176acb4a0d421d10bc7b7e7daa8b
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-14 20:45:09 +0100

    trufflehog: 3.88.1 -> 3.88.2
    Diff: https://github.com/trufflesecurity/trufflehog/compare/refs/tags/v3.88.1...v3.88.2
    
    Changelog: https://github.com/trufflesecurity/trufflehog/releases/tag/v3.88.2

--- pkgs/tools/security/trufflehog/default.nix
+++ pkgs/tools/security/trufflehog/default.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "trufflehog";
-  version = "3.88.1";
+  version = "3.88.2";
 
   src = fetchFromGitHub {
     owner = "trufflesecurity";
     repo = "trufflehog";
     tag = "v${version}";
-    hash = "sha256-peZGCJ3g/u4gAznYM9wzgaqsEYGk0SSzI23g/xglngE=";
+    hash = "sha256-p5vB9ug/H7xV7Y4GIk2BUUMFJ4/tcKj+V4qvYwCJlRU=";
   };
 
   vendorHash = "sha256-YsUAu2gEXzpjM/jg4VJ7KTvf1/cLTO04hLOLmUDeYk0=";
