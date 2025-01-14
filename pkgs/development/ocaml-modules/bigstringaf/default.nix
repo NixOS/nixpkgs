12732826b270eb8f3ef972862179985b4a6732c5
Mutsuha Asada <me@momee.mt>
2025-01-14 14:13:21 +0900

    ocamlPackages.bigstringaf: modernize the derivation and add changelog and longDescription

--- pkgs/development/ocaml-modules/bigstringaf/default.nix
+++ pkgs/development/ocaml-modules/bigstringaf/default.nix
@@ -16,7 +16,7 @@ buildDunePackage rec {
   src = fetchFromGitHub {
     owner = "inhabitedtype";
     repo = pname;
-    rev = version;
+    tag = version;
     hash = "sha256-p1hdB3ArOd2UX7S6YvXCFbYjEiXdMDmBaC/lFQgua7Q=";
   };
 
@@ -28,6 +28,18 @@ buildDunePackage rec {
 
   meta = {
     description = "Bigstring intrinsics and fast blits based on memcpy/memmove";
+    longDescription = ''
+      Bigstring intrinsics and fast blits based on memcpy/memmove
+
+      The OCaml compiler has a bunch of intrinsics for Bigstrings, but they're not
+      widely-known, sometimes misused, and so programs that use Bigstrings are slower
+      than they have to be. And even if a library got that part right and exposed the
+      intrinsics properly, the compiler doesn't have any fast blits between
+      Bigstrings and other string-like types.
+
+      So here they are. Go crazy.
+    '';
+    changelog = "https://github.com/inhabitedtype/bigstringaf/releases/tag/${version}";
     license = lib.licenses.bsd3;
     maintainers = [ lib.maintainers.vbgl ];
     inherit (src.meta) homepage;
