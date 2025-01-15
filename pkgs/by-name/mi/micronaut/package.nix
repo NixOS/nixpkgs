dba7d04ff3914c9007d14129099f1999584279a8
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-15 00:28:27 +0000

    micronaut: 4.7.3 -> 4.7.4

--- pkgs/by-name/mi/micronaut/package.nix
+++ pkgs/by-name/mi/micronaut/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "micronaut";
-  version = "4.7.3";
+  version = "4.7.4";
 
   src = fetchzip {
     url = "https://github.com/micronaut-projects/micronaut-starter/releases/download/v${version}/micronaut-cli-${version}.zip";
-    sha256 = "sha256-rCGJnZ+WD0chYVa6XqkhHJqJUQKw2K+AebzYSftAsOk=";
+    sha256 = "sha256-R/7ozlbx7WAX7rLdhgckJGt9troBj+0rFcFDBz6eZiM=";
   };
 
   nativeBuildInputs = [
