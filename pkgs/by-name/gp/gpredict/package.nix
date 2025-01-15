f8a4313cf38df56a0bf95952771ab52eab272304
Gavin John <gavinnjohn@gmail.com>
2025-01-15 14:45:46 -0800

    gpredict: move to by-name

--- pkgs/by-name/gp/gpredict/package.nix
+++ pkgs/by-name/gp/gpredict/package.nix
@@ -11,7 +11,7 @@
   curl,
   goocanvas2,
   gpsd,
-  hamlib,
+  hamlib_4,
   wrapGAppsHook3,
 }:
 
@@ -56,7 +56,7 @@ stdenv.mkDerivation rec {
     gtk3
     goocanvas2
     gpsd
-    hamlib
+    hamlib_4
   ];
 
   meta = {
