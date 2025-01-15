e28435917445b7761cfb992043d55e92226dddbb
Kilgarragh <tomp.newbern@icloud.com>
2025-01-14 21:40:56 -0500

    maintainers: add kilgarragh

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11976,6 +11976,11 @@
     githubId = 464625;
     name = "Enric Morales";
   };
+  kilgarragh = {
+    github = "Kilgarragh";
+    githubId = 126100118;
+    name = "Kilgarragh";
+  };
   kilianar = {
     email = "mail@kilianar.de";
     github = "kilianar";
