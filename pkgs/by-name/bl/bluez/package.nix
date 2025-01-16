NixOS/nixpkgs

commit 83b7cf4a00c0052c3467896d333c45df1829a1fe

bluez: 5.78 -> 5.79

pkgs/by-name/bl/bluez/package.nix
@@ -25,28 +25,20 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bluez";
-  version = "5.78";
+  version = "5.79";
 
   src = fetchurl {
     url = "mirror://kernel/linux/bluetooth/bluez-${finalAttrs.version}.tar.xz";
-    hash = "sha256-gw/tGRXF03W43g9eb0X83qDcxf9f+z0x227Q8A1zxeM=";
+    hash = "sha256-QWSlMDqfcccPSMA/9gvjQjG1aNk6mtXnmSjTTmqg6oo=";
   };
 
   patches = [
-    # Upstream fix is wrong:
-    # https://github.com/bluez/bluez/issues/843#issuecomment-2352696535
-    (fetchurl {
-      name = "basename.patch";
-      url = "https://github.com/void-linux/void-packages/raw/187b45d47d93b6857a95cae10c2132d76e4955fc/srcpkgs/bluez/patches/basename.patch";
-      hash = "sha256-Jb4u7rxIShDp1yUgaQVDJo2HJfZBzRoVlcDEWxooFgk=";
+    (fetchpatch {
+      name = "musl.patch";
+      url = "https://git.kernel.org/pub/scm/bluetooth/bluez.git/patch/?id=9d69dba21f1e46b34cdd8ae27fec11d0803907ee";
+      hash = "sha256-yMXPRPK8aT+luVoXNxx9zIa4c6E0BKYKS55DCfr8EQ0=";
     })
-  ] ++ lib.optional (stdenv.hostPlatform.isMusl && stdenv.hostPlatform.isx86_64)
-    # Disable one failing test with musl libc, also seen by alpine
-    # https://github.com/bluez/bluez/issues/726
-    (fetchurl {
-      url = "https://git.alpinelinux.org/aports/plain/main/bluez/disable_aics_unit_testcases.patch?id=8e96f7faf01a45f0ad8449c1cd825db63a8dfd48";
-      hash = "sha256-1PJkipqBO3qxxOqRFQKfpWlne1kzTCgtnTFYI1cFQt4=";
-    });
+  ];
 
   buildInputs = [
     alsa-lib

commit 2503ce9aad3b11c2b441cf0530414db1fa77baa9

bluez: re-disable test on x86_64-unknown-linux-musl
I optimistically removed this because I could no longer reproduce it,
but it seems that it is still happening on some machines. :(

Fixes: 83b7cf4a00c0 ("bluez: 5.78 -> 5.79")

pkgs/by-name/bl/bluez/package.nix
@@ -35,13 +35,23 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-QWSlMDqfcccPSMA/9gvjQjG1aNk6mtXnmSjTTmqg6oo=";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "musl.patch";
-      url = "https://git.kernel.org/pub/scm/bluetooth/bluez.git/patch/?id=9d69dba21f1e46b34cdd8ae27fec11d0803907ee";
-      hash = "sha256-yMXPRPK8aT+luVoXNxx9zIa4c6E0BKYKS55DCfr8EQ0=";
-    })
-  ];
+  patches =
+    [
+      (fetchpatch {
+        name = "musl.patch";
+        url = "https://git.kernel.org/pub/scm/bluetooth/bluez.git/patch/?id=9d69dba21f1e46b34cdd8ae27fec11d0803907ee";
+        hash = "sha256-yMXPRPK8aT+luVoXNxx9zIa4c6E0BKYKS55DCfr8EQ0=";
+      })
+    ]
+    ++ lib.optional (stdenv.hostPlatform.isMusl && stdenv.hostPlatform.isx86_64)
+      # Disable one failing test with musl libc, also seen by alpine
+      # https://github.com/bluez/bluez/issues/726
+      (
+        fetchurl {
+          url = "https://git.alpinelinux.org/aports/plain/main/bluez/disable_aics_unit_testcases.patch?id=8e96f7faf01a45f0ad8449c1cd825db63a8dfd48";
+          hash = "sha256-1PJkipqBO3qxxOqRFQKfpWlne1kzTCgtnTFYI1cFQt4=";
+        }
+      );
 
   buildInputs = [
     alsa-lib