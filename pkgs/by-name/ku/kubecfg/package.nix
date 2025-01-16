f7d7be581d44657b518e58effc14c9a6dede2b46
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-16 02:47:58 +0000

    kubecfg: 0.35.1 -> 0.35.2

--- pkgs/by-name/ku/kubecfg/package.nix
+++ pkgs/by-name/ku/kubecfg/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule rec {
   pname = "kubecfg";
-  version = "0.35.1";
+  version = "0.35.2";
 
   src = fetchFromGitHub {
     owner = "kubecfg";
     repo = "kubecfg";
     rev = "v${version}";
-    hash = "sha256-5xs9iE6sfFzoTq24DTNKOj4D+A5ezBKN1lfIdJCt+pk=";
+    hash = "sha256-iCeeS6CVGEFrNqa0NvdV2AqViDQIfG02Hx8yOBO1Vsk=";
   };
 
-  vendorHash = "sha256-K2IyljE5QS/SZ6EXV42q/a5ru+0UXZ69oLNi94XKxw4=";
+  vendorHash = "sha256-nAjm4AotRYZGRv05A+dviNq6Moo53Zo/bOiQf972ZF8=";
 
   ldflags = [
     "-s"
