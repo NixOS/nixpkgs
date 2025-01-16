NixOS/nixpkgs

commit dd9608a5d82e72d260fed32e5da079e833d03c10

dotnet/update.nix: use tag name to check for updates
This stops it from missing SDK-only updates.

pkgs/development/compilers/dotnet/update.nix
@@ -18,7 +18,7 @@
 }:
 
 let
-  inherit (lib.importJSON releaseManifestFile) channel release;
+  inherit (lib.importJSON releaseManifestFile) channel tag;
 
   pkg = stdenvNoCC.mkDerivation {
     name = "update-dotnet-vmr-env";
@@ -96,9 +96,8 @@ writeScript "update-dotnet-vmr.sh" ''
       cd "$tmp"
 
       curl -fsSL "$releaseUrl" -o release.json
-      release=$(jq -r .release release.json)
 
-      if [[ -z $tag && "$release" == "${release}" ]]; then
+      if [[ -z $tag && "$tagName" == "${tag}" ]]; then
           >&2 echo "release is already $release"
           exit
       fi

commit 3d8f84280a624eec0e719d7876220a956d761010

dotnet/update.nix: fix error when package is up-to-date
Fixes: dd9608a5d82e72d260fed32e5da079e833d03c10

pkgs/development/compilers/dotnet/update.nix
@@ -98,7 +98,7 @@ writeScript "update-dotnet-vmr.sh" ''
       curl -fsSL "$releaseUrl" -o release.json
 
       if [[ -z $tag && "$tagName" == "${tag}" ]]; then
-          >&2 echo "release is already $release"
+          >&2 echo "release is already $tagName"
           exit
       fi
 