bf3d444d8a157be95d44a8a0f0aed5747facd479
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-16 02:49:35 +0000

    maltego: 4.8.1 -> 4.9.0

--- pkgs/by-name/ma/maltego/package.nix
+++ pkgs/by-name/ma/maltego/package.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "maltego";
-  version = "4.8.1";
+  version = "4.9.0";
 
   src = fetchzip {
     url = "https://downloads.maltego.com/maltego-v4/linux/Maltego.v${finalAttrs.version}.linux.zip";
-    hash = "sha256-FH2gyz3/4wDBRsOQl3l2pbavvSyK73HuAXvJ0YBC1dw=";
+    hash = "sha256-K5Nh9tfNsKViNyMkkLFZ3c1ebGnlj+glEUZ/Iio2bxE=";
   };
 
   postPatch = ''
