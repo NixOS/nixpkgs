7f5eb863e478943ec7d740a854e8a2789334e477
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-16 09:56:36 +0100

    python312Packages.mypy-boto3-mediaconvert: 1.35.93 -> 1.36.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -862,8 +862,8 @@ rec {
       "sha256-kNE9LvLz9ltNM6UcMhXeyGWm+uHMAK3E5V/t30QYo+0=";
 
   mypy-boto3-mediaconvert =
-    buildMypyBoto3Package "mediaconvert" "1.35.93"
-      "sha256-EmRIwSS0LJ2UcxTSk5zru6XiNz4yj6ICyeZrEDFHcgU=";
+    buildMypyBoto3Package "mediaconvert" "1.36.0"
+      "sha256-d4AwaMKqhplnUnpLQv+zukeKX7+TlhGt5YwTZFCPlKo=";
 
   mypy-boto3-medialive =
     buildMypyBoto3Package "medialive" "1.35.93"
