8312f47225cf43c0393bb3ae3b0f3c2a204b4be8
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-01-16 17:48:25 +0200

    kubectl-ktop: remove duplicate

--- pkgs/by-name/ku/kubectl-ktop/package.nix
+++ pkgs/by-name/ku/kubectl-ktop/package.nix
@@ -17,7 +17,7 @@ buildGoModule rec {
 
   vendorHash = "sha256-MLIcTHWo7lsqtAqH8naSvpS013t8KBVPRbch+CfeUNk=";
 
-  subPackages = [ "." ];
+  excludedPackages = [ ".ci" ];
 
   ldflags = [
     "-s"
@@ -28,13 +28,23 @@ buildGoModule rec {
 
   postInstall = ''
     ln -s $out/bin/ktop $out/bin/kubectl-ktop
+    rm $out/bin/hack
   '';
+
+  doCheck = false;
 
   meta = with lib; {
     description = "Top-like tool for your Kubernetes clusters";
+    mainProgram = "kubectl-ktop";
+    longDescription = ''
+      Following the tradition of Unix/Linux top tools, ktop is a tool that displays useful metrics information about nodes, pods, and other workload resources running in a Kubernetes cluster.
+    '';
     homepage = "https://github.com/vladimirvivien/ktop";
     changelog = "https://github.com/vladimirvivien/ktop/releases/tag/v${version}";
     license = licenses.asl20;
-    maintainers = [ maintainers.ivankovnatsky ];
+    maintainers = with maintainers; [
+      qjoly
+      ivankovnatsky
+    ];
   };
 }
