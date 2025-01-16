2e6a5e602ee532be82e2a5f2b3afd1cb0f3e5bf9
Eric Roberts <eric@ericroberts.dev>
2025-01-16 13:44:44 +0000

    maintainers: add three

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23027,6 +23027,12 @@
     githubId = 1391883;
     name = "Tom Hall";
   };
+  three = {
+    email = "eric@ericroberts.dev";
+    github = "three";
+    githubId = 1761259;
+    name = "Eric Roberts";
+  };
   thtrf = {
     email = "thtrf@proton.me";
     github = "thtrf";
