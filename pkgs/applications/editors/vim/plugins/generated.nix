13b7572ae3eb2534e248da31e03b896513b5b6ae
Austin Horstman <khaneliman12@gmail.com>
2025-01-16 14:38:58 -0600

    vimPlugins.monaspace-nvim: init at 2025-01-14

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -7455,6 +7455,18 @@ final: prev:
     meta.homepage = "https://github.com/benlubas/molten-nvim/";
   };
 
+  monaspace-nvim = buildVimPlugin {
+    pname = "monaspace.nvim";
+    version = "2025-01-14";
+    src = fetchFromGitHub {
+      owner = "jackplus-xyz";
+      repo = "monaspace.nvim";
+      rev = "8f6e5e64393b530fd1d8e0ea96c51ffbb4046186";
+      sha256 = "08c5kj2wmlykamww1bilgws45mmx8yqb4y0f58cazf3wl1rjbldr";
+    };
+    meta.homepage = "https://github.com/jackplus-xyz/monaspace.nvim/";
+  };
+
   monokai-pro-nvim = buildVimPlugin {
     pname = "monokai-pro.nvim";
     version = "2024-12-16";
