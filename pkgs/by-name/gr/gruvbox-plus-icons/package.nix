3ac3e36585829d2a3c391cf5e71cb27f0332e809
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-01-16 14:30:13 +0100

    gruvbox-plus-icons: add option to override colors

--- pkgs/by-name/gr/gruvbox-plus-icons/package.nix
+++ pkgs/by-name/gr/gruvbox-plus-icons/package.nix
@@ -7,6 +7,7 @@
   gnome-icon-theme,
   hicolor-icon-theme,
   nix-update-script,
+  folder-color ? "plasma", # Supported colors: black blue caramel citron firebrick gold green grey highland jade lavender lime olive orange pistachio plasma pumpkin purple red rust sapphire tomato violet white yellow
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
@@ -19,6 +20,8 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     rev = "f83e7d27528c03ce4a1e769df86a9519993259d4";
     hash = "sha256-NDdx8BXIIlO+Wi3+TsGOdfhhGYCp7uZ4e+vW6zYpD3I=";
   };
+
+  patches = [ ./folder-color.patch ];
 
   nativeBuildInputs = [ gtk3 ];
 
@@ -33,6 +36,8 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
     mkdir -p $out/share/icons
     cp -r Gruvbox-Plus-Dark $out/share/icons/
+    patchShebangs scripts/folders-color-chooser
+    ./scripts/folders-color-chooser -c ${folder-color}
     gtk-update-icon-cache $out/share/icons/Gruvbox-Plus-Dark
 
     runHook postInstall
