86d2327219949d3d54d861708ad8525cd81cbbf8
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-17 06:03:43 +0000

    python312Packages.torch-audiomentations: 0.11.1 -> 0.12.0

--- pkgs/development/python-modules/torch-audiomentations/default.nix
+++ pkgs/development/python-modules/torch-audiomentations/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "torch-audiomentations";
-  version = "0.11.1";
+  version = "0.12.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "asteroid-team";
     repo = "torch-audiomentations";
     tag = "v${version}";
-    hash = "sha256-0+5wc+mP4c221q6mdaqPalfumTOtdnkjnIPtLErOp9E=";
+    hash = "sha256-5ccVO1ECiIn0q7m8ZLHxqD2fhaXeMDKUEswa49dRTsY=";
   };
 
   pythonRelaxDeps = [ "torchaudio" ];
