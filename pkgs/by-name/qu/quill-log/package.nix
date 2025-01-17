333e5cbf966bc0e75555b257b090484d034e960d
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-17 00:36:23 +0000

    quill-log: 7.5.0 -> 8.0.0

--- pkgs/by-name/qu/quill-log/package.nix
+++ pkgs/by-name/qu/quill-log/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "quill-log";
-  version = "7.5.0";
+  version = "8.0.0";
 
   src = fetchFromGitHub {
     owner = "odygrd";
     repo = "quill";
     rev = "v${version}";
-    hash = "sha256-sALrDsOxyBAtUCSRFTpZV4/2B29pss+KbiXT9xEfLTI=";
+    hash = "sha256-vwi5TU9yIdMgVXwPrhGcmmZtpq57DoVok38NY+hKlkU=";
   };
 
   nativeBuildInputs = [ cmake ];
