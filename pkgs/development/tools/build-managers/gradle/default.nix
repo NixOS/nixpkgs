8f47296374d9c26609a619c6fd7c58fcd4e47671
FliegendeWurst <arne.keller@posteo.de>
2025-01-17 08:21:59 +0100

    gradle: fix cross builds

--- pkgs/development/tools/build-managers/gradle/default.nix
+++ pkgs/development/tools/build-managers/gradle/default.nix
@@ -48,6 +48,7 @@ rec {
       runCommand,
       writeText,
       autoPatchelfHook,
+      buildPackages,
 
       # The JDK/JRE used for running Gradle.
       java ? defaultJava,
@@ -78,7 +79,6 @@ rec {
         ];
 
       buildInputs = [
-        java
         stdenv.cc.cc
         ncurses5
         ncurses6
@@ -123,6 +123,8 @@ rec {
           newFileEvents = toString (lib.versionAtLeast version "8.12");
         in
         ''
+          # get the correct jar executable for cross
+          export PATH="${buildPackages.jdk}/bin:$PATH"
           . ${./patching.sh}
 
           nativeVersion="$(extractVersion native-platform $out/lib/gradle/lib/native-platform-*.jar)"
