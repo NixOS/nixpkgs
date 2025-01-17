9e36ea2dc8dd2cf8141c26049686b14b864ed282
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-17 14:26:49 +0000

    python312Packages.lmdb: 1.5.1 -> 1.6.2

--- pkgs/development/python-modules/lmdb/default.nix
+++ pkgs/development/python-modules/lmdb/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "lmdb";
-  version = "1.5.1";
+  version = "1.6.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-cXwlWCfTMeAvckK0QFGqBkZskPbXMuywezHt+x4Gxno=";
+    hash = "sha256-0o4/pZk1/2iIWHYOxS8gLsuMEImj9o0fFi6jB40VHnM=";
   };
 
   buildInputs = [ lmdb ];
