a823c30cd02dab3308c0a7ca609ae5a804c85c62
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-17 13:24:28 +0000

    kubernetes-helmPlugins.helm-diff: 3.9.13 -> 3.9.14

--- pkgs/applications/networking/cluster/helm/plugins/helm-diff.nix
+++ pkgs/applications/networking/cluster/helm/plugins/helm-diff.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "helm-diff";
-  version = "3.9.13";
+  version = "3.9.14";
 
   src = fetchFromGitHub {
     owner = "databus23";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-676xMnedfGF3aVub78eQo2KYJgJLxKg9g3Nm6D9lYA0=";
+    hash = "sha256-9YXsbxcth6v+4OW2nJjRK47dR4b8fn5izvjLfGeS5qI=";
   };
 
-  vendorHash = "sha256-qfdxEXiNJlaJPzpwY4GY+mYZAxjkMJyZO8PgcqJCPos=";
+  vendorHash = "sha256-pn5ipX2kXuR2tHO2LE5m34xJLpB7R6jLYW+KALSPgxo=";
 
   ldflags = [
     "-s"
