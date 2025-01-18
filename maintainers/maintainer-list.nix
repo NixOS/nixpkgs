b331d661de5d58572e44d388cd81895d1e645b94
boralg <thinnrthinkr@gmail.com>
2025-01-18 19:47:47 +0200

    maintainers: rename thinnerthinker -> boralg

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3211,6 +3211,11 @@
     matrix = "@boozedog:matrix.org";
     name = "David A. Buser";
   };
+  boralg = {
+    name = "boralg";
+    github = "boralg";
+    githubId = 144260188;
+  };
   borisbabic = {
     email = "boris.ivan.babic@gmail.com";
     github = "borisbabic";
@@ -22647,11 +22652,6 @@
     email = "thilo.billerbeck@officerent.de";
     github = "thilobillerbeck";
     githubId = 7442383;
-  };
-  thinnerthinker = {
-    name = "thinnerthinker";
-    github = "thinnerthinker";
-    githubId = 144260188;
   };
   thled = {
     name = "Thomas Le Duc";
