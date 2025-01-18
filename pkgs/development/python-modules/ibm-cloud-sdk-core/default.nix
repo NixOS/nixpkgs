50a0cef7191313d3e07fe9513ba14bb0f3b6de1d
Martin Weinelt <mweinelt@users.noreply.github.com>
2025-01-18 17:01:59 +0100

    Update pkgs/development/python-modules/ibm-cloud-sdk-core/default.nix
    Co-authored-by: Nick Cao <nickcao@nichi.co>

--- pkgs/development/python-modules/ibm-cloud-sdk-core/default.nix
+++ pkgs/development/python-modules/ibm-cloud-sdk-core/default.nix
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   version = "3.22.1";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "IBM";
