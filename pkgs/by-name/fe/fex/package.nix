35c9c1cf04a5c8b4990023d6e9329e962811e41c
György Kurucz <me@kuruczgy.com>
2025-01-18 01:52:54 +0100

    fex: minimize set of cloned submodules
    This change also removes the submodules that contained prebuilt (test)
    binaries, so now we can more confidently add `sourceProvenance = [
    lib.sourceTypes.fromSource ];`.

--- pkgs/by-name/fe/fex/package.nix
+++ pkgs/by-name/fe/fex/package.nix
@@ -8,6 +8,8 @@
   gitMinimal,
   qt5,
   python3,
+  xxHash,
+  fmt,
 }:
 
 llvmPackages.stdenv.mkDerivation (finalAttrs: {
@@ -18,8 +20,33 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     owner = "FEX-Emu";
     repo = "FEX";
     tag = "FEX-${finalAttrs.version}";
-    hash = "sha256-NnYod6DeRv3/6h8SGkGYtgC+RRuIafxoQm3j1Sqk0mU=";
-    fetchSubmodules = true;
+
+    hash = "sha256-yjxwwqnI0jartl97zGfqP9GJSQ6HRwZ6ZJUWPsFsvUw=";
+
+    leaveDotGit = true;
+    postFetch = ''
+      cd $out
+      git reset
+
+      # Only fetch required submodules
+      git submodule update --init --depth 1 \
+        External/Vulkan-Headers \
+        External/drm-headers \
+        External/jemalloc \
+        External/jemalloc_glibc \
+        External/robin-map \
+        External/vixl \
+        Source/Common/cpp-optparse \
+        External/Catch2 \
+        External/xbyak
+
+      find . -name .git -print0 | xargs -0 rm -rf
+
+      # Remove some more unnecessary directories
+      rm -r \
+        External/vixl/src/aarch32 \
+        External/vixl/test
+    '';
   };
 
   nativeBuildInputs = [
@@ -38,12 +65,17 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     ))
   ];
 
-  buildInputs = with qt5; [
-    qtbase
-    qtdeclarative
-    qtquickcontrols
-    qtquickcontrols2
-  ];
+  buildInputs =
+    [
+      xxHash
+      fmt
+    ]
+    ++ (with qt5; [
+      qtbase
+      qtdeclarative
+      qtquickcontrols
+      qtquickcontrols2
+    ]);
 
   cmakeFlags = [
     "-DCMAKE_BUILD_TYPE=Release"
@@ -67,6 +99,7 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     description = "Fast usermode x86 and x86-64 emulator for Arm64 Linux";
     homepage = "https://fex-emu.com/";
     platforms = [ "aarch64-linux" ];
+    sourceProvenance = [ lib.sourceTypes.fromSource ];
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ andre4ik3 ];
     mainProgram = "FEXBash";
