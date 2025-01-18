34fb7a6132bdd8f5b6eee38e81c381a8043e4ee0
R. RyanTM <ryantm-bot@ryantm.com>
2025-01-19 06:23:05 +1000

    python312Packages.troposphere: 4.8.3 -> 4.9.0 (#374716)
    https://github.com/cloudtools/troposphere/blob/4.9.0/CHANGELOG.rst

--- pkgs/development/python-modules/troposphere/default.nix
+++ pkgs/development/python-modules/troposphere/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "troposphere";
-  version = "4.8.3";
+  version = "4.9.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "cloudtools";
     repo = pname;
     tag = version;
-    hash = "sha256-Mz6n7QjNvK2HEQeWCySdxioA1dD5CLUh7jCQb1Q6mdc=";
+    hash = "sha256-1Qq1vV3DsslnwJq0d0MZ9bCadYD08gxLsM3tQmti4Pw=";
   };
 
   propagatedBuildInputs = [ cfn-flip ] ++ lib.optionals (pythonOlder "3.8") [ typing-extensions ];
