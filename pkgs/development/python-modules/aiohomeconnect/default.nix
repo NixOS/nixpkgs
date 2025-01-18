4f9211545ba8995a7692ed03753eb434f9339a70
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-18 09:19:09 +0100

    python313Packages.aiohomeconnect: 0.11.0 -> 0.11.2
    Diff: https://github.com/MartinHjelmare/aiohomeconnect/compare/refs/tags/v0.11.0...v0.11.2
    
    Changelog: https://github.com/MartinHjelmare/aiohomeconnect/blob/refs/tags/v0.11.2/CHANGELOG.md

--- pkgs/development/python-modules/aiohomeconnect/default.nix
+++ pkgs/development/python-modules/aiohomeconnect/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "aiohomeconnect";
-  version = "0.11.0";
+  version = "0.11.2";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "MartinHjelmare";
     repo = "aiohomeconnect";
     tag = "v${version}";
-    hash = "sha256-fg3WusrONJtPO/IpHlXzO17C2OuQe28kprXrBG9PqMY=";
+    hash = "sha256-YnQypKeVYoHD445fv9n643qIqjTCInCfbTHPUrMePJI=";
   };
 
   pythonRelaxDeps = [ "httpx" ];
