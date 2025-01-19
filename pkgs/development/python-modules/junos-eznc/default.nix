a0ab3008ea0af99eb67ba72aa95e6e50c3489015
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:16:03 +0100

    python3Packages.junos-eznc: 2.7.1 -> 2.7.2
    https://github.com/Juniper/py-junos-eznc/releases/tag/2.7.2

--- pkgs/development/python-modules/junos-eznc/default.nix
+++ pkgs/development/python-modules/junos-eznc/default.nix
@@ -24,7 +24,7 @@
 
 buildPythonPackage rec {
   pname = "junos-eznc";
-  version = "2.7.1";
+  version = "2.7.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -33,7 +33,7 @@ buildPythonPackage rec {
     owner = "Juniper";
     repo = "py-junos-eznc";
     tag = version;
-    hash = "sha256-aoi+in5A8qSdQNY3V4S4wBBfPchR1an7G6GQHDhgxpQ=";
+    hash = "sha256-2Y4Zwmf+154t+ul4qE++lunsK5DmdMAVWxV6kMa//D0=";
   };
 
   build-system = [ setuptools ];
@@ -78,7 +78,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Junos 'EZ' automation for non-programmers";
     homepage = "https://github.com/Juniper/py-junos-eznc";
-    changelog = "https://github.com/Juniper/py-junos-eznc/releases/tag/${version}";
+    changelog = "https://github.com/Juniper/py-junos-eznc/releases/tag/${src.tag}";
     license = licenses.asl20;
     maintainers = with maintainers; [ xnaveira ];
   };
