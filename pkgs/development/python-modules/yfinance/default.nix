91e24280b156967c381e31df8bb0059bc3a1dcd6
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:19:19 +0100

    python3Packages.yfinance: 0.2.51 -> 0.2.52
    https://github.com/ranaroussi/yfinance/blob/0.2.52/CHANGELOG.rst

--- pkgs/development/python-modules/yfinance/default.nix
+++ pkgs/development/python-modules/yfinance/default.nix
@@ -23,7 +23,7 @@
 
 buildPythonPackage rec {
   pname = "yfinance";
-  version = "0.2.51";
+  version = "0.2.52";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -32,7 +32,7 @@ buildPythonPackage rec {
     owner = "ranaroussi";
     repo = "yfinance";
     tag = version;
-    hash = "sha256-H4DrbatteETvxtDjz2kYGTMvGFbmmgbb5wMKWSUkOGc=";
+    hash = "sha256-bXscFrrsIz/mGqV00VqPN1URyJB7G/jH5bzcKWus44g=";
   };
 
   build-system = [ setuptools ];
@@ -70,7 +70,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Module to doiwnload Yahoo! Finance market data";
     homepage = "https://github.com/ranaroussi/yfinance";
-    changelog = "https://github.com/ranaroussi/yfinance/blob/${version}/CHANGELOG.rst";
+    changelog = "https://github.com/ranaroussi/yfinance/blob/${src.tag}/CHANGELOG.rst";
     license = licenses.asl20;
     maintainers = with maintainers; [ drewrisinger ];
   };
