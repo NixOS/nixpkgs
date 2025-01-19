4301500dbfec689582ff44a2fdebbeb3fe0d3f77
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-01-18 19:14:34 -0500

    python3Packages.word2number: init at 1.1

---
+++ pkgs/development/python-modules/word2number/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools-scm,
+  future,
+  python,
+}:
+
+buildPythonPackage rec {
+  pname = "word2number";
+  version = "1.1";
+  pyproject = true;
+
+  # Pull from github because pypi package doesn't include unit tests
+  src = fetchFromGitHub {
+    owner = "akshaynagpal";
+    repo = "w2n";
+    tag = version;
+    hash = "sha256-dgHPEfieNDZnP6+YvywvN3ZzmeICav0WMYKkWDSJ/LE=";
+  };
+
+  build-system = [
+    setuptools-scm
+  ];
+
+  dependencies = [
+    future
+  ];
+
+  pythonImportsCheck = [
+    "word2number"
+  ];
+
+  checkPhase = ''
+    ${lib.getExe python} unit_testing.py
+  '';
+
+  meta = {
+    changelog = "https://github.com/akshaynagpal/w2n/releases/tag/${version}";
+    description = "Convert number words (eg. twenty one) to numeric digits (21)";
+    homepage = "http://w2n.readthedocs.io/";
+    license = [ lib.licenses.mit ];
+    maintainers = [ lib.maintainers.booxter ];
+  };
+}
