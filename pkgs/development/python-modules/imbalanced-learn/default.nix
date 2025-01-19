0348e03f9a25906e48185efa2921ea0d1c0697f8
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:15:55 +0100

    python3Packages.imbalanced-learn: 0.12.4 -> 0.13.0
    https://github.com/scikit-learn-contrib/imbalanced-learn/releases/tag/0.13.0

--- pkgs/development/python-modules/imbalanced-learn/default.nix
+++ pkgs/development/python-modules/imbalanced-learn/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "imbalanced-learn";
-  version = "0.12.4";
+  version = "0.13.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-gVO6OF0pawfZfgkBomJKhsBrSMlML5LaOlNUgnaXt6M=";
+    hash = "sha256-1KjWNyCGmCNQ1t4cflse1ezp5Bv5MEO00OnqHEIqZgY=";
   };
 
   nativeBuildInputs = [ setuptools ];
