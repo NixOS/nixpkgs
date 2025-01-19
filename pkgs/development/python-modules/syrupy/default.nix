650ff3e34f360a63e78a22369cc40de25294a24d
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:18:42 +0100

    python3Packages.syrupy: 4.8.0 -> 4.8.1
    https://github.com/syrupy-project/syrupy/blob/v4.8.1/CHANGELOG.md

--- pkgs/development/python-modules/syrupy/default.nix
+++ pkgs/development/python-modules/syrupy/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "syrupy";
-  version = "4.8.0";
+  version = "4.8.1";
   pyproject = true;
 
   disabled = lib.versionOlder python.version "3.8.1";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "syrupy-project";
     repo = "syrupy";
     tag = "v${version}";
-    hash = "sha256-IifGufCUhjbl8Tqvcjm8XF4QPvOsRacPWxI1yT79eNs=";
+    hash = "sha256-H/R2MKXdn/Oqow1Nt2tr45UQqJQs7O72hfA+lRU9V6w=";
   };
 
   build-system = [ poetry-core ];
@@ -43,7 +43,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "syrupy" ];
 
   meta = {
-    changelog = "https://github.com/syrupy-project/syrupy/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/syrupy-project/syrupy/blob/${src.tag}/CHANGELOG.md";
     description = "Pytest Snapshot Test Utility";
     homepage = "https://github.com/syrupy-project/syrupy";
     license = lib.licenses.asl20;
