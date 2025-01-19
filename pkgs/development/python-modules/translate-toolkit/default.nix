19380a2ffeba528fd3ebf7a5e6e4e706c300911c
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:18:53 +0100

    python3Packages.translate-toolkit: 3.14.1 -> 3.14.6

--- pkgs/development/python-modules/translate-toolkit/default.nix
+++ pkgs/development/python-modules/translate-toolkit/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "translate-toolkit";
-  version = "3.14.1";
+  version = "3.14.6";
 
   pyproject = true;
   build-system = [ setuptools-scm ];
@@ -30,7 +30,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "translate_toolkit";
     inherit version;
-    hash = "sha256-IUjEN8Up1Or4nFo71WkDduq+6Xw8ObfUgkABp88zPoY=";
+    hash = "sha256-2FCt8D+GSEv5xeriA8kT7T2Rjb86jwCNmxVgKnKG554=";
   };
 
   dependencies = [
