86a54b60e46970c18b35479a7c659a5c7cbe7d35
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:15:44 +0100

    python3Packages.habluetooth: 3.7.0 -> 3.9.0
    https://github.com/Bluetooth-Devices/habluetooth/blob/v3.9.0/CHANGELOG.md

--- pkgs/development/python-modules/habluetooth/default.nix
+++ pkgs/development/python-modules/habluetooth/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "habluetooth";
-  version = "3.7.0";
+  version = "3.9.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "Bluetooth-Devices";
     repo = "habluetooth";
     tag = "v${version}";
-    hash = "sha256-wOWQaM1hfWaqLFIzwB7O1yOS/CJPvZ+aGbKzvAE2DAE=";
+    hash = "sha256-JDmdbqgCcgYuPNDNBlqN/kZLRhm3+C0hf9ZC+OoEbTU=";
   };
 
   build-system = [
@@ -59,7 +59,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Library for high availability Bluetooth";
     homepage = "https://github.com/Bluetooth-Devices/habluetooth";
-    changelog = "https://github.com/Bluetooth-Devices/habluetooth/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/Bluetooth-Devices/habluetooth/blob/${src.tag}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
