96cc37552bda4e3354591426617a4f122d9719f0
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-19 11:46:33 +0000

    python312Packages.apispec: 6.8.0 -> 6.8.1

--- pkgs/development/python-modules/apispec/default.nix
+++ pkgs/development/python-modules/apispec/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "apispec";
-  version = "6.8.0";
+  version = "6.8.1";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-hhzKgrvAZSyprOqJaSGyVJRFgDQv3oSfhvbawazGypY=";
+    hash = "sha256-9JFsu3vhVpY7GPWSmg5CvSNJE1g0toCoGxJDK8+qmjk=";
   };
 
   nativeBuildInputs = [ flit-core ];
