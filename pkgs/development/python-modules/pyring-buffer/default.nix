47547f72dc8e1345a218cd27f2f25043d42bbe50
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:17:38 +0100

    python3Packages.pyring-buffer: 1.0.0 -> 1.0.1
    https://github.com/rhasspy/pyring-buffer/blob/v1.0.1/CHANGELOG.md

--- pkgs/development/python-modules/pyring-buffer/default.nix
+++ pkgs/development/python-modules/pyring-buffer/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pyring-buffer";
-  version = "1.0.0";
+  version = "1.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "rhasspy";
     repo = "pyring-buffer";
-    rev = "382290312fa2ad5d75bd42c040a43e25dad9c8a7";
-    hash = "sha256-bHhcBU4tjFAyZ3/GjaP/hDXz2N73mCChTNYHsZyBCSM=";
+    tag = "v${version}";
+    hash = "sha256-s0iIcY2cwTbOMrpw14uAYfeAl/MOJTLfZbJr+tmTM6U=";
   };
 
   nativeBuildInputs = [ setuptools ];
@@ -26,7 +26,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Pure Python ring buffer for bytes";
     homepage = "https://github.com/rhasspy/pyring-buffer";
-    changelog = "https://github.com/rhasspy/pyring-buffer/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/rhasspy/pyring-buffer/blob/${src.tag}/CHANGELOG.md";
     license = licenses.asl20;
     maintainers = with maintainers; [ hexa ];
   };
