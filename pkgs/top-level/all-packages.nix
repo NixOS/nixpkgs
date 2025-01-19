fefe7c1c52470074d6629f74b551bdfbea2b1c47
Thomas Gerbet <thomas@gerbet.me>
2025-01-19 22:52:22 +0100

    imagemagick: move to openexr_3
    OpenEXR 2 is no maintained anymore.
    
    (cherry picked from commit ec1cb63901aa61783746a5fd6cb2de087ab19d9b)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14521,6 +14521,7 @@ with pkgs;
 
   imagemagick = lowPrio (callPackage ../applications/graphics/ImageMagick {
     inherit (darwin.apple_sdk.frameworks) ApplicationServices Foundation;
+    openexr = openexr_3;
   });
 
   imagemagickBig = lowPrio (imagemagick.override {
