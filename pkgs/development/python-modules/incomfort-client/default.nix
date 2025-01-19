c970c46c8c09578644e8bd5712af62b9df5516ab
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:15:56 +0100

    python3Packages.incomfort-client: 0.6.5 -> 0.6.5-3
    https://github.com/jbouwh/incomfort-client/releases/tag/v0.6.5-3

--- pkgs/development/python-modules/incomfort-client/default.nix
+++ pkgs/development/python-modules/incomfort-client/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "incomfort-client";
-  version = "0.6.5";
+  version = "0.6.5-3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "zxdavb";
     repo = "incomfort-client";
     tag = "v${version}";
-    hash = "sha256-6n6uvUJm6y0AynAZMQG+NKNIIaeMSpOJqWDdpPdGy4Q=";
+    hash = "sha256-j8MJX2Ix1Q2RPNATlE6lyRRHA9P0Ixu6GfA17JooEkI=";
   };
 
   build-system = [ setuptools ];
@@ -39,7 +39,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python module to poll Intergas boilers via a Lan2RF gateway";
     homepage = "https://github.com/zxdavb/incomfort-client";
-    changelog = "https://github.com/jbouwh/incomfort-client/releases/tag/v${version}";
+    changelog = "https://github.com/jbouwh/incomfort-client/releases/tag/${src.tag}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
