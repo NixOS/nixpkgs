e016ecf0233fe1a7a09d602b7b391ab4c956cccd
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:15:40 +0100

    python3Packages.graphrag: 1.0.0 -> 1.2.0
    https://github.com/microsoft/graphrag/blob/v1.2.0/CHANGELOG.md

--- pkgs/development/python-modules/graphrag/default.nix
+++ pkgs/development/python-modules/graphrag/default.nix
@@ -40,14 +40,14 @@
 
 buildPythonPackage rec {
   pname = "graphrag";
-  version = "1.0.0";
+  version = "1.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "graphrag";
-    rev = "refs/tags/v${version}";
-    hash = "sha256-037PBiIyxcdJSut9kF4L23wNEJBdKM/M2I/0f4iVayo=";
+    tag = "v${version}";
+    hash = "sha256-z3gO0wV8YBNi2Z53avujAt/Es9mSzugEFa/qRgq7ItM=";
   };
 
   build-system = [
@@ -115,7 +115,7 @@ buildPythonPackage rec {
   meta = {
     description = "Modular graph-based Retrieval-Augmented Generation (RAG) system";
     homepage = "https://github.com/microsoft/graphrag";
-    changelog = "https://github.com/microsoft/graphrag/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/microsoft/graphrag/blob/${src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ natsukium ];
   };
