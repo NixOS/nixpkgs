17a53b2775d0e24f050182b515809f79a0a15713
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:17:27 +0100

    python3Packages.pyheos: 0.7.4 -> 1.0.0
    https://github.com/andrewsayre/pyheos/releases/tag/1.0.0

--- pkgs/development/python-modules/pyheos/default.nix
+++ pkgs/development/python-modules/pyheos/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pyheos";
-  version = "0.7.4";
+  version = "1.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "andrewsayre";
     repo = "pyheos";
     tag = version;
-    hash = "sha256-vz81FepXWcCdlY1v7ozp+/l+XpYb91mNmRiLKwjrC4A=";
+    hash = "sha256-0td3Xv2BwOwcuU0ZlPA86eQd326vRjB7UMysN/RGjMU=";
   };
 
   build-system = [ setuptools ];
@@ -34,7 +34,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "pyheos" ];
 
   meta = with lib; {
-    changelog = "https://github.com/andrewsayre/pyheos/releases/tag/${version}";
+    changelog = "https://github.com/andrewsayre/pyheos/releases/tag/${src.tag}";
     description = "Async python library for controlling HEOS devices through the HEOS CLI Protocol";
     homepage = "https://github.com/andrewsayre/pyheos";
     license = licenses.asl20;
