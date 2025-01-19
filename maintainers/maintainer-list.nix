875aa2ab4afee6082a6383b26ee47ee263af031a
Petr Zahradnik <junior@zahradnik.cz>
2025-01-19 21:37:51 +0100

    maintainers: add petrzjunior

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18184,6 +18184,11 @@
     githubId = 939420;
     name = "Petros Angelatos";
   };
+  petrzjunior = {
+    github = "petrzjunior";
+    githubId = 7000918;
+    name = "Petr Zahradnik";
+  };
   petterstorvik = {
     email = "petterstorvik@gmail.com";
     github = "storvik";
