e9eec7a01f55b575b6e43e533cf1b460ebacd147
Morgan Jones <me@numin.it>
2025-01-19 01:48:18 -0800

    mattermostLatest: track 10.x instead of 10.4.x
    We get a new 10.x release every month, so just track latest.

--- pkgs/by-name/ma/mattermostLatest/package.nix
+++ pkgs/by-name/ma/mattermostLatest/package.nix
@@ -10,7 +10,7 @@ mattermost.override {
     # See https://docs.mattermost.com/about/mattermost-server-releases.html
     # and make sure the version regex is up to date here.
     # Ensure you also check ../mattermost/package.nix for ESR releases.
-    regex = "^v(10\.4\.[0-9]+)$";
+    regex = "^v(10\.[0-9]+\.[0-9]+)$";
     version = "10.4.1";
     srcHash = "sha256-e7uT30tWhJpEQzlcDUY2huFcupDbe4l8B19Dgub2pg0=";
     vendorHash = "sha256-AcemUxcBoytE/ZoXqaIlxkzAnmGV/C1laDqziMuE+XE=";
