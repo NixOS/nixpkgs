06b4ac7ba4857eb749394d142136fc1c6ad369fe
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:17:54 +0100

    python3Packages.python-tado: 0.17.7 -> 0.18.5
    https://github.com/wmalgadey/PyTado/releases/tag/0.18.5

--- pkgs/development/python-modules/python-tado/default.nix
+++ pkgs/development/python-modules/python-tado/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "python-tado";
-  version = "0.17.7";
+  version = "0.18.5";
   pyproject = true;
 
   disabled = pythonOlder "3.5";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "wmalgadey";
     repo = "PyTado";
     tag = version;
-    hash = "sha256-WpGznYNVpis1pM9PRXHnQVev3JW6baUT5J9iPxwd0Uk=";
+    hash = "sha256-NW3Au4meVf9QFVqmsx6f2TQus6QxanILx5U5GlVc3TE=";
   };
 
   build-system = [ setuptools ];
@@ -33,7 +33,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python binding for Tado web API. Pythonize your central heating!";
     homepage = "https://github.com/wmalgadey/PyTado";
-    changelog = "https://github.com/wmalgadey/PyTado/releases/tag/${version}";
+    changelog = "https://github.com/wmalgadey/PyTado/releases/tag/${src.tag}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ jamiemagee ];
     mainProgram = "pytado";
