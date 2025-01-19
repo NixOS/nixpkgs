17a21e0ebc1c79d181376c956d6e9193bfdd37d9
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:17:39 +0100

    python3Packages.pysdl2: 0.9.16 -> 0.9.17

--- pkgs/development/python-modules/pysdl2/default.nix
+++ pkgs/development/python-modules/pysdl2/default.nix
@@ -2,41 +2,37 @@
   stdenv,
   lib,
   replaceVars,
-  fetchPypi,
+  fetchFromGitHub,
   buildPythonPackage,
+  setuptools,
+
+  # native dependencies
   SDL2,
   SDL2_ttf,
   SDL2_image,
   SDL2_gfx,
   SDL2_mixer,
+
+  # tests
+  numpy,
+  pillow,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "pysdl2";
-  version = "0.9.16";
+  version = "0.9.17";
+  pyproject = true;
 
-  # The tests use OpenGL using find_library, which would have to be
-  # patched; also they seem to actually open X windows and test stuff
-  # like "screensaver disabling", which would have to be cleverly
-  # sandboxed. Disable for now.
-  doCheck = false;
   pythonImportsCheck = [ "sdl2" ];
 
-  src = fetchPypi {
-    pname = "PySDL2";
-    inherit version;
-    hash = "sha256-ECdAa62+zdMP5W6AClp2rX1ycaOuwLes94DuJqAPLUA=";
+  src = fetchFromGitHub {
+    owner = "py-sdl";
+    repo = "py-sdl2";
+    tag = version;
+    hash = "sha256-FqVgDGpImLN2a51TnU6eKjLK3rwW3ujdzOkK58ERBbE=";
   };
 
-  # Deliberately not in propagated build inputs; users can decide
-  # which library they want to include.
-  buildInputs = [
-    SDL2_ttf
-    SDL2_image
-    SDL2_gfx
-    SDL2_mixer
-  ];
-  propagatedBuildInputs = [ SDL2 ];
   patches = [
     (replaceVars ./PySDL2-dll.patch (
       builtins.mapAttrs
@@ -53,9 +49,38 @@ buildPythonPackage rec {
     ))
   ];
 
+  build-system = [ setuptools ];
+
+  buildInputs = [
+    SDL2
+    SDL2_ttf
+    SDL2_image
+    SDL2_gfx
+    SDL2_mixer
+  ];
+
+  env = {
+    SDL_VIDEODRIVER = "dummy";
+    SDL_AUDIODRIVER = "dummy";
+    SDL_RENDER_DRIVER = "software";
+    PYTHONFAULTHANDLER = "1";
+  };
+
+  nativeCheckInputs = [
+    numpy
+    pillow
+    pytestCheckHook
+  ];
+
+  disabledTests = [
+    # GetPrefPath for OrgName/AppName is None
+    "test_SDL_GetPrefPath"
+  ];
+
   meta = {
+    changelog = "https://github.com/py-sdl/py-sdl2/releases/tag/${src.tag}";
     description = "Wrapper around the SDL2 library and as such similar to the discontinued PySDL project";
-    homepage = "https://github.com/marcusva/py-sdl2";
+    homepage = "https://github.com/py-sdl/py-sdl2";
     license = lib.licenses.publicDomain;
     maintainers = with lib.maintainers; [ pmiddend ];
   };
