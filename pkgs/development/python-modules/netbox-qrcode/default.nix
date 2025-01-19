0d2527ecd34e8ba3293e63ff45e598fbb3c66851
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:16:45 +0100

    python3Packages.netbox-qrcode: 0.0.15 -> 0.0.16
    https://github.com/netbox-community/netbox-qrcode/releases/tag/v0.0.16

--- pkgs/development/python-modules/netbox-qrcode/default.nix
+++ pkgs/development/python-modules/netbox-qrcode/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "netbox-qrcode";
-  version = "0.0.15";
+  version = "0.0.16";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "netbox-community";
     repo = "netbox-qrcode";
     tag = "v${version}";
-    hash = "sha256-gEBOjmX7dcLjiy0u6raAtAaqZUVYsPz3YhupjRRqVDE=";
+    hash = "sha256-3d2UugM08qGW5YnpxYVp9FfgMTevJaExAnTpaKwxcUw=";
   };
 
   build-system = [ setuptools ];
@@ -44,7 +44,7 @@ buildPythonPackage rec {
   meta = {
     description = "Netbox plugin for generate QR codes for objects: Rack, Device, Cable.";
     homepage = "https://github.com/netbox-community/netbox-qrcode";
-    changelog = "https://github.com/netbox-community/netbox-qrcode/releases/tag/${src.rev}";
+    changelog = "https://github.com/netbox-community/netbox-qrcode/releases/tag/${src.tag}";
     license = lib.licenses.asl20;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ felbinger ];
