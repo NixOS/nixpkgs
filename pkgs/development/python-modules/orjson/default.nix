751541516c22d4c4457e0591b10a4aa8da53351f
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 05:21:48 +0100

    python313Packages.orjson: 3.10.11 -> 3.10.15
    https://github.com/ijl/orjson/blob/3.10.15/CHANGELOG.md

--- pkgs/development/python-modules/orjson/default.nix
+++ pkgs/development/python-modules/orjson/default.nix
@@ -30,7 +30,7 @@
 
 buildPythonPackage rec {
   pname = "orjson";
-  version = "3.10.11";
+  version = "3.10.15";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -39,13 +39,13 @@ buildPythonPackage rec {
     owner = "ijl";
     repo = "orjson";
     tag = version;
-    hash = "sha256-RJcTyLf2pLb1kHd7+5K9dGMWja4KFdKIwdRAp6Ud+Ps=";
+    hash = "sha256-FlcWf6BhUP2Y5ivRQx1W0G8sgfvbuAQN7qpBJbd3N2I=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-HlvsV3Bsxa4Ud1+RrEnDWKX82DRyfgBS7GvK9827/wE=";
+    hash = "sha256-YvZl0zYuUBTIBAdIh6IDR3vIWlk5ye5e3cLB0j/41pk=";
   };
 
   nativeBuildInputs =
