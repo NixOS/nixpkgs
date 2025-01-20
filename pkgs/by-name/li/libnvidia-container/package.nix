NixOS/nixpkgs

commit a5fef0d0f4bc225fa6742506fe0758dd0ef49e78

libnvidia-container: fix cross-compilation for aarch64-linux
Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

pkgs/by-name/li/libnvidia-container/package.nix
@@ -84,12 +84,19 @@ stdenv.mkDerivation rec {
     #    libtirpc (for now)
     # 4. prevent installation of static libraries because of step 3
     # 5. prevent installation of libnvidia-container-go.so twice
+    # 6. Replace pkg-config and objcopy with target platform's one
+    # 7. Stub ldconfig
+    #
     sed -i Makefile \
       -e 's#DESTDIR=\$(DEPS_DIR)#DESTDIR=""#g' \
       -e 's#\$(DEPS_DIR)\$#\$#g' \
       -e 's#all: shared static tools#all: shared tools#g' \
       -e '/$(INSTALL) -m 644 $(LIB_STATIC) $(DESTDIR)$(libdir)/d' \
-      -e '/$(INSTALL) -m 755 $(libdir)\/$(LIBGO_SHARED) $(DESTDIR)$(libdir)/d'
+      -e '/$(INSTALL) -m 755 $(libdir)\/$(LIBGO_SHARED) $(DESTDIR)$(libdir)/d' \
+      -e "s,pkg-config,$PKG_CONFIG,g"
+    substituteInPlace mk/common.mk \
+      --replace-fail objcopy '$(OBJCOPY)' \
+      --replace-fail ldconfig true
   '';
 
   enableParallelBuilding = true;
@@ -98,7 +105,12 @@ stdenv.mkDerivation rec {
     HOME="$(mktemp -d)"
   '';
 
-  env.NIX_CFLAGS_COMPILE = toString [ "-I${lib.getInclude libtirpc}/include/tirpc" ];
+  env = {
+    NIX_CFLAGS_COMPILE = toString [ "-I${lib.getInclude libtirpc}/include/tirpc" ];
+    CGO_ENABLED = "1"; # Needed for cross-compilation
+    GOFLAGS = "-trimpath"; # Don't include paths to Go stdlib to resulting binary
+    inherit (go) GOARCH GOOS;
+  };
   NIX_LDFLAGS = [
     "-L${lib.getLib libtirpc}/lib"
     "-ltirpc"

commit 28c845cd31dd93ac7cb4ec2dbd5c8035199ea045

libnvidia-container: fix dangling symlinks
https://github.com/NixOS/nixpkgs/pull/372148/commits/a5fef0d0f4bc225fa6742506fe0758dd0ef49e78
caused some dangling symlinks in the package output by stubbing
`ldconfig`. This fixes it by recreating the symlinks after the package
is built, keeping the compilation working on `aarch64-linux`.

Can be tested via checking the `result/lib` directory before the
mentioned commit, after it, and with this fix.

Signed-off-by: Moritz Sanft <58110325+msanft@users.noreply.github.com>

pkgs/by-name/li/libnvidia-container/package.nix
@@ -101,6 +101,15 @@ stdenv.mkDerivation rec {
       --replace-fail ldconfig true
   '';
 
+  postFixup = ''
+    # Recreate library symlinks which ldconfig would have created
+    for lib in libnvidia-container libnvidia-container-go; do
+      rm -f "$out/lib/$lib.so"
+      ln -s "$out/lib/$lib.so.${version}" "$out/lib/$lib.so.1"
+      ln -s "$out/lib/$lib.so.1" "$out/lib/$lib.so"
+    done
+  '';
+
   enableParallelBuilding = true;
 
   preBuild = ''