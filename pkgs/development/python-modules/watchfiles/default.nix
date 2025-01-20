c4868a205bf008bdf283c4924c0de23a0be509a0
natsukium <tomoya.otabi@gmail.com>
2025-01-20 16:05:43 +0900

    python312Packages.watchfiles: 0.24.0 -> 1.0.4
    Diff: https://github.com/samuelcolvin/watchfiles/compare/refs/tags/v0.24.0...v1.0.4

--- pkgs/development/python-modules/watchfiles/default.nix
+++ pkgs/development/python-modules/watchfiles/default.nix
@@ -12,27 +12,27 @@
   pytest-mock,
   pytest-timeout,
   pytestCheckHook,
+  typing-extensions,
   CoreServices,
   libiconv,
 }:
 
 buildPythonPackage rec {
   pname = "watchfiles";
-  version = "0.24.0";
-  format = "pyproject";
+  version = "1.0.4";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "samuelcolvin";
     repo = pname;
     tag = "v${version}";
-    hash = "sha256-uc4CfczpNkS4NMevtRxhUOj9zTt59cxoC0BXnuHFzys=";
+    hash = "sha256-0JBnUi/aRM9UFTkb8OkP9UkJV+BF2EieZptymRvAXc0=";
   };
 
   cargoDeps = rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
-    outputHashes."notify-6.1.1" = "sha256-lT3R5ZQpjx52NVMEKTTQI90EWT16YnbqphqvZmNpw/I=";
   };
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
@@ -42,12 +42,13 @@ buildPythonPackage rec {
 
   nativeBuildInputs = [
     rustPlatform.cargoSetupHook
-    rustPlatform.maturinBuildHook
     cargo
     rustc
   ];
 
-  propagatedBuildInputs = [ anyio ];
+  build-system = [ rustPlatform.maturinBuildHook ];
+
+  dependencies = [ anyio ];
 
   # Tests need these permissions in order to use the FSEvents API on macOS.
   sandboxProfile = ''
