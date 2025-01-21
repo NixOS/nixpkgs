e7ec6378b43efe58b229e9e8149b6521dbe20afb
natsukium <tomoya.otabi@gmail.com>
2025-01-21 22:48:01 +0900

    fixup! python3Packages.tree-sitter: 0.23.2 -> 0.24.0

--- pkgs/development/python-modules/tree-sitter/default.nix
+++ pkgs/development/python-modules/tree-sitter/default.nix
@@ -2,7 +2,6 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch,
   pytestCheckHook,
   pythonOlder,
   setuptools,
@@ -18,7 +17,7 @@ buildPythonPackage rec {
   version = "0.24.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.10";
 
   src = fetchFromGitHub {
     owner = "tree-sitter";
@@ -27,12 +26,6 @@ buildPythonPackage rec {
     hash = "sha256-ZDt/8suteaAjGdk71l8eej7jDkkVpVDBIZS63SA8tsU=";
     fetchSubmodules = true;
   };
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/tree-sitter/py-tree-sitter/commit/a85342e16d28c78a1cf1e14c74f4598cd2a5f3e0.patch";
-      hash = "sha256-gm79KciA/KoDqrRfWuSB3GOD1jBx6Skd1olt4zoofaw=";
-    })
-  ];
 
   build-system = [ setuptools ];
 
@@ -51,6 +44,11 @@ buildPythonPackage rec {
     # https://github.com/NixOS/nixpkgs/issues/255262#issuecomment-1721265871
     rm -r tree_sitter
   '';
+
+  disabledTests = [
+    # test fails in nix sandbox
+    "test_dot_graphs"
+  ];
 
   meta = with lib; {
     description = "Python bindings to the Tree-sitter parsing library";
