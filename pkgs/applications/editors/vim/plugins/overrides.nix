c887fa83780e95bf746a430bcc760d83e4c48905
Austin Horstman <khaneliman12@gmail.com>
2025-01-20 21:21:29 -0600

    vimPlugins.nvim-dap-view: init at 2025-01-19

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -2229,6 +2229,10 @@ in
     doInstallCheck = true;
   };
 
+  nvim-dap-view = super.nvim-dap-view.overrideAttrs {
+    dependencies = [ self.nvim-dap ];
+  };
+
   nvim-dap-virtual-text = super.nvim-dap-virtual-text.overrideAttrs {
     dependencies = [ self.nvim-dap ];
   };
