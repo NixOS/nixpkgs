ec60993b23ec081b544fdebfa3d4377ac808069b
rewine <luhongxu@deepin.org>
2025-01-21 21:30:52 +0800

    wev: 1.0.0 -> 1.0.0-unstable-2022-09-14

--- pkgs/by-name/we/wev/package.nix
+++ pkgs/by-name/we/wev/package.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wev";
-  version = "1.0.0";
+  version = "1.0.0-unstable-2022-09-14";
 
   src = fetchFromSourcehut {
     owner = "~sircmpwn";
     repo = pname;
-    rev = version;
-    sha256 = "0l71v3fzgiiv6xkk365q1l08qvaymxd4kpaya6r2g8yzkr7i2hms";
+    rev = "83de8e931ab04ce3322a58b359d8effa7901b21c";
+    sha256 = "sha256-lNFgjRXO/ZbcXJF06DykPoJJ6/a8ZfVA6g95i+rNdWs=";
   };
 
   strictDeps = true;
