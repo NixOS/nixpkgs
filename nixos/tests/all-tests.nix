59e837a9837536561f672b00ef0248fcf4386974
oxalica <oxalicc@pm.me>
2025-01-21 16:50:49 -0500

    nixosTests.earlyoom-notify: init for desktop notification

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -292,6 +292,7 @@ in {
   drbd-driver = handleTest ./drbd-driver.nix {};
   dublin-traceroute = handleTest ./dublin-traceroute.nix {};
   earlyoom = runTestOn ["x86_64-linux"] ./earlyoom.nix;
+  earlyoom-notify = runTestOn ["x86_64-linux"] ./earlyoom-notify.nix;
   early-mount-options = handleTest ./early-mount-options.nix {};
   ec2-config = (handleTestOn ["x86_64-linux"] ./ec2.nix {}).boot-ec2-config or {};
   ec2-nixops = (handleTestOn ["x86_64-linux"] ./ec2.nix {}).boot-ec2-nixops or {};
