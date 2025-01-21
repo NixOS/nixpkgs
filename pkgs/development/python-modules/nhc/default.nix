caad79d0a7fd1394d23a165ce89be1209f525264
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-21 01:04:10 +0100

    python313Packages.nhc: 0.3.3 -> 0.3.4
    https://github.com/vandeurenglenn/nhc/blob/v0.3.4/CHANGELOG.md

--- pkgs/development/python-modules/nhc/default.nix
+++ pkgs/development/python-modules/nhc/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "nhc";
-  version = "0.3.3";
+  version = "0.3.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "vandeurenglenn";
     repo = "nhc";
     tag = "v${version}";
-    hash = "sha256-s3DVdnjhRUZRG/LwKwOuZSiNtzpccBtHl/PNvux/NwQ=";
+    hash = "sha256-TSfOcSeedr0cSDHo1bqftUOZ3AeCW3Zgb80IBv3eerM=";
   };
 
   build-system = [ setuptools ];
