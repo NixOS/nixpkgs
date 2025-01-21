2ec1207df4e57a600890d128236702b7542f6b40
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-21 01:16:32 +0100

    fixup! python3Packages.python-dbusmock: 0.32.2 -> 0.34.2

--- pkgs/development/python-modules/python-dbusmock/default.nix
+++ pkgs/development/python-modules/python-dbusmock/default.nix
@@ -36,28 +36,10 @@ buildPythonPackage rec {
 
   src = fetchFromGitHub {
     owner = "martinpitt";
-    repo = pname;
+    repo = "python-dbusmock";
     tag = version;
     hash = "sha256-7h5SIcgWcbzInmCkbGz/ulfPJvqPPguWLJY+AXJuo0c=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "musl.patch";
-      url = "https://github.com/martinpitt/python-dbusmock/commit/1a8d8722068ef7e5f061336047a72d1a0f253b98.patch";
-      hash = "sha256-0j3UXsTMDh1+UolkmoLQXlwHXve81yKiGJ7gDWNZVPY=";
-    })
-    (fetchpatch {
-      name = "os-release.patch";
-      url = "https://github.com/martinpitt/python-dbusmock/commit/4b99cff50e8c741f20aef4527b27ccdb2a4053d2.patch";
-      hash = "sha256-Xcovv44JeuTvPAtXWJvWE+MxlyloClSJGKZz+C3P5bE=";
-    })
-    (fetchpatch {
-      name = "tests-bluez-5.79.patch";
-      url = "https://github.com/martinpitt/python-dbusmock/commit/d5e449bff924ea2b2837843237fbb5d9751c4f89.patch";
-      hash = "sha256-CafQ/RhFynjI9eY4Xeu5yS+a29ZiJJnSYUmd74/2Dpg=";
-    })
-  ];
 
   build-system = [
     setuptools
