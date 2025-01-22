ce834c1662784b23f29f187dbcec17462b236083
Malo Bourgon <mbourgon@gmail.com>
2025-01-22 10:29:49 -0800

    signalbackup-tools: 20250113-1 -> 20250122
    Diff: https://github.com/bepaald/signalbackup-tools/compare/20250113-1...20250122

--- pkgs/by-name/si/signalbackup-tools/package.nix
+++ pkgs/by-name/si/signalbackup-tools/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "signalbackup-tools";
-  version = "20250113-1";
+  version = "20250122";
 
   src = fetchFromGitHub {
     owner = "bepaald";
     repo = "signalbackup-tools";
     rev = version;
-    hash = "sha256-aunXBPPWHTlgWVNwsBZAZJi+/dE/BwFC1kfBj0AXMos=";
+    hash = "sha256-rsA0i87zC6hyQa8xIyveID/nNwAD2YMG3BHYtWKxg1I=";
   };
 
   nativeBuildInputs = [
