e5821e3f6e86c70f1255a3531bc39b0da9cc3391
liberodark <liberodark@gmail.com>
2025-01-22 15:04:40 +0100

    nixos/rundeck: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1179,6 +1179,7 @@ in
   rsyslogd = handleTest ./rsyslogd.nix { };
   rtkit = runTest ./rtkit.nix;
   rtorrent = handleTest ./rtorrent.nix { };
+  rundeck = runTest ./rundeck.nix { };
   rush = runTest ./rush.nix;
   rustls-libssl = handleTest ./rustls-libssl.nix { };
   rxe = handleTest ./rxe.nix { };
