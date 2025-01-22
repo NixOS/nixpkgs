fbff6da1e340474d4b902a1fb7aeededb7d00629
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-22 04:43:16 +0000

    python312Packages.oelint-parser: 6.5.0 -> 7.0.0

--- pkgs/development/python-modules/oelint-parser/default.nix
+++ pkgs/development/python-modules/oelint-parser/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "oelint-parser";
-  version = "6.5.0";
+  version = "7.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "priv-kweihmann";
     repo = "oelint-parser";
     tag = version;
-    hash = "sha256-RSdcBWFJoD3aOJCQeC1fnpyzOJvn/CM9K0r2bzq9fsY=";
+    hash = "sha256-NTMAgAN/YJu8vdk1AV4Ji962MIq4Wf5w+0yryz9cLh4=";
   };
 
   pythonRelaxDeps = [ "regex" ];
