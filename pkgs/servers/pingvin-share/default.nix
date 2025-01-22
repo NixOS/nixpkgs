50415274585401e4614d61306fdcf083c289c0e1
RatCornu <balthazar.patiachvili@crans.org>
2025-01-22 10:47:19 +0100

    pingvin-share: 1.8.1 -> 1.8.2

--- pkgs/servers/pingvin-share/default.nix
+++ pkgs/servers/pingvin-share/default.nix
@@ -5,12 +5,12 @@
 }:
 
 let
-  version = "1.8.1";
+  version = "1.8.2";
   src = fetchFromGitHub {
     owner = "stonith404";
     repo = "pingvin-share";
     rev = "v${version}";
-    hash = "sha256-d34VlsYhaIMXsdb0194hF84j2aqzbtX/GKzicIa9+J0=";
+    hash = "sha256-GlI/ilr95EOW2dOnZ5nmH0mkGp0rrTFj3tpnGTM2sJ8=";
   };
 in
 
