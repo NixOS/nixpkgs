6ff344c188a55d256276e9efa0f99b915c4c0bc4
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-22 16:09:50 +0000

    python312Packages.diffusers: 0.30.3 -> 0.32.2

--- pkgs/development/python-modules/diffusers/default.nix
+++ pkgs/development/python-modules/diffusers/default.nix
@@ -40,7 +40,7 @@
 
 buildPythonPackage rec {
   pname = "diffusers";
-  version = "0.30.3";
+  version = "0.32.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -49,7 +49,7 @@ buildPythonPackage rec {
     owner = "huggingface";
     repo = "diffusers";
     tag = "v${version}";
-    hash = "sha256-/3lHJdsNblKb6xX03OluSCApMK3EXJbRLboBk8CjobE=";
+    hash = "sha256-TwmII38EA0Vux+Jh39pTAA6r+FRNuKHQWOOqsEe2Z+E=";
   };
 
   build-system = [ setuptools ];
