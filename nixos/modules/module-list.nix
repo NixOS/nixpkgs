9002df31b91b785abf1eb806842d8867c3c97b88
liberodark <liberodark@gmail.com>
2025-01-22 15:04:40 +0100

    nixos/rundeck: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1761,6 +1761,7 @@
   ./services/web-apps/rimgo.nix
   ./services/web-apps/rss-bridge.nix
   ./services/web-apps/rsshub.nix
+  ./services/web-apps/rundeck.nix
   ./services/web-apps/rutorrent.nix
   ./services/web-apps/screego.nix
   ./services/web-apps/selfoss.nix
