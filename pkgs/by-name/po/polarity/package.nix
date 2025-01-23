25cfb7e58f0bab98a282ed74fc2f87c418e7786e
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-23 21:03:48 +0000

    polarity: latest-unstable-2025-01-08 -> latest-unstable-2025-01-23

--- pkgs/by-name/po/polarity/package.nix
+++ pkgs/by-name/po/polarity/package.nix
@@ -7,19 +7,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "polarity";
-  version = "latest-unstable-2025-01-08";
+  version = "latest-unstable-2025-01-23";
 
   src = fetchFromGitHub {
     owner = "polarity-lang";
     repo = "polarity";
-    rev = "bbc91efbc25f77f505d244e96cc566f63f6dc858";
-    hash = "sha256-3wKMFX4ubp8bLNhdLoMkZlsP2vfu0wcNpEZrs95xwAY=";
+    rev = "b5449d485a86fec9d88621cc38fcdfd9b4c3c2d1";
+    hash = "sha256-MFem+/7LegSXJXlIQkaVLbTfxvuG46V10TI06NohQ9A=";
   };
 
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "codespan-0.11.1" = "sha256-0cUndjWQ44X5zXVfg7YX/asBByWq/hFV1n9tHPBTcfY=";
       "tower-lsp-0.20.0" = "sha256-f3S2CyFFX6yylaxMoXhB1/bfizVsLfNldLM+dXl5Y8k=";
     };
   };
