5c44b61481617d1ff1fd7734006fdadee0c0a49c
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-23 22:59:31 +0100

    nvc: 1.15.0 -> 1.15.1

--- pkgs/by-name/nv/nvc/package.nix
+++ pkgs/by-name/nv/nvc/package.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation rec {
   pname = "nvc";
-  version = "1.15.0";
+  version = "1.15.1";
 
   src = fetchFromGitHub {
     owner = "nickg";
     repo = "nvc";
     rev = "r${version}";
-    hash = "sha256-EpDJhgj1oYz+050qPwrToQAJNMtkSb423+Bh09qlka0=";
+    hash = "sha256-E/Q2f5b6Y0ZGEyq18I09SXHK0PJrBMiuCwpStyd/E8M=";
   };
 
   nativeBuildInputs = [
