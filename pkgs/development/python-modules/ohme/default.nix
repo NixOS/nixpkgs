76769596e4c30a5db7fee449e305394e73da0d68
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-23 13:18:18 +0100

    python313Packages.ohme: 1.2.3 -> 1.2.5
    Diff: https://github.com/dan-r/ohmepy/compare/refs/tags/v1.2.3...v1.2.5
    
    Changelog: https://github.com/dan-r/ohmepy/releases/tag/v1.2.5

--- pkgs/development/python-modules/ohme/default.nix
+++ pkgs/development/python-modules/ohme/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "ohme";
-  version = "1.2.3";
+  version = "1.2.5";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "dan-r";
     repo = "ohmepy";
     tag = "v${version}";
-    hash = "sha256-hCwaB6YXtNHCNOyjRxDSonBdTpKqX9GmLwFMvrY9UpI=";
+    hash = "sha256-5BpztK8HXawEcafZ8ORdnDyz3s/K37B8hDb0dG6EitM=";
   };
 
   build-system = [ setuptools ];
