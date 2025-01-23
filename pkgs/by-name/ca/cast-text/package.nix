ff4bf6e0adacf14651b0ad752f1b91f09c663971
Darwin Cereska <157426553+darwincereska@users.noreply.github.com>
2025-01-22 21:46:38 -0500

    cast-text: init at 0.1.3

---
+++ pkgs/by-name/ca/cast-text/package.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildGoModule
+}:
+buildGoModule rec {
+  pname = "cast-text";
+  version = "0.1.3";
+  src = fetchFromGitHub {
+    owner ="piqoni";
+    repo = "cast-text";
+    tag = "v${version}";
+    hash = "sha256-PU0haunPF2iAHehRT76wHXsdv5oWiBH7xVJUkQbr4QU=";
+  };
+  
+  vendorHash = "sha256-rTrV8qzGHNNC8wTLJVerOHwFjjCGQmVuHOnBJxuYMAk=";
+  
+  meta = {
+    description = "Zero latency, easy-to-use full-text news terminal reader";
+    homepage = "https://github.com/piqoni/cast-text";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ darwincereskax ];
+  };
+}
