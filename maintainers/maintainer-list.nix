d0e1fd73121746fc0b60d337e59f279a565bf741
Rylee Randall <rylee.randall@livepreso.com>
2025-01-23 17:54:25 +1100

    prettier-plugin-sql-cst: init at 0.12.2

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9830,6 +9830,11 @@
     githubId = 3661115;
     name = "Ingo Blechschmidt";
   };
+  ibrokemypie = {
+    github = "ibrokemypie";
+    githubId = 7827846;
+    name = "ibrokemypie";
+  };
   icedborn = {
     email = "github.envenomed@dralias.com";
     github = "icedborn";
