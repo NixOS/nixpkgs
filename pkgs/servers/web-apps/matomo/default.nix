63d66b2958823be0a86c71cf8892fbf40acce5a5
Niklas Korz <niklas@niklaskorz.de>
2025-01-24 14:44:52 +0100

    matomo-beta: 5.2.1 -> 5.2.2

--- pkgs/servers/web-apps/matomo/default.nix
+++ pkgs/servers/web-apps/matomo/default.nix
@@ -18,11 +18,11 @@ let
       hash = "sha256-ZEwz/KKZZwTFsKfwR0iKZM1ta4CUXJsWgBXika+pjb0=";
     };
     matomo-beta = {
-      version = "5.2.1";
+      version = "5.2.2";
       # `beta` examples: "b1", "rc1", null
       # when updating: use null if stable version is >= latest beta or release candidate
       beta = null;
-      hash = "sha256-5glMwwIG0Uo8bu904u40FUa+yaUlrQe1nUCkv9/ATks=";
+      hash = "sha256-ZEwz/KKZZwTFsKfwR0iKZM1ta4CUXJsWgBXika+pjb0=";
     };
   };
   common =
