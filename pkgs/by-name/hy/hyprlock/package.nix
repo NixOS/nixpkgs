0cb19affdd728c8e2d81260e9e107375131b56a8
Lin Xianyi <iynaix@gmail.com>
2025-01-24 23:45:34 +0800

    hyprlock: 0.6.1 -> 0.6.2 (#376284)
    Changelog: https://github.com/hyprwm/hyprlock/releases/tag/v0.6.2
    Diff: https://github.com/hyprwm/hyprlock/compare/v0.6.1...v0.6.2

--- pkgs/by-name/hy/hyprlock/package.nix
+++ pkgs/by-name/hy/hyprlock/package.nix
@@ -28,13 +28,13 @@
 
 gcc14Stdenv.mkDerivation (finalAttrs: {
   pname = "hyprlock";
-  version = "0.6.1";
+  version = "0.6.2";
 
   src = fetchFromGitHub {
     owner = "hyprwm";
     repo = "hyprlock";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-lT6f/5NB73xj9cVesi2SNsL5jVciwZJp8QRohiv+3Hk=";
+    hash = "sha256-PotjNmR69yAEZP/Dn4lB0p7sHBjAPclNDbc5WkBZx4s=";
   };
 
   nativeBuildInputs = [
