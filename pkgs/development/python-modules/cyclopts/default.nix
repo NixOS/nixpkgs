c71f6614797b59d6b91bb88e991a51dcd895b843
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-24 17:14:46 +0000

    python312Packages.cyclopts: 3.3.0 -> 3.3.1

--- pkgs/development/python-modules/cyclopts/default.nix
+++ pkgs/development/python-modules/cyclopts/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "cyclopts";
-  version = "3.3.0";
+  version = "3.3.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "BrianPugh";
     repo = "cyclopts";
     tag = "v${version}";
-    hash = "sha256-sXTKBDfQSqMWBX+wrhP6fCugGSuXM8mOkEroFWPalVs=";
+    hash = "sha256-PrhaUUzFQJ7tqUdu7nwi9k+NxQUPQWYDUc9vsvnmfeY=";
   };
 
   build-system = [
