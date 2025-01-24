89b8e777a824888707394461873038ee3a7d9e25
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-24 01:44:17 +0000

    micromdm: 1.12.1 -> 1.13.0

--- pkgs/by-name/mi/micromdm/package.nix
+++ pkgs/by-name/mi/micromdm/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "micromdm";
-  version = "1.12.1";
+  version = "1.13.0";
 
   src = fetchFromGitHub {
     owner = "micromdm";
     repo = "micromdm";
     rev = "v${version}";
-    hash = "sha256-hFoInkeJAd5h6UiF19YE9f6kkIZRmhVFVvUAkSkSqlM=";
+    hash = "sha256-o/HK1bjaUwsSQG7QbYe0gFnD/OKV00cHXLXpftNa3iY=";
   };
 
-  vendorHash = "sha256-XYrv/cjma2ZYHs2x6hSXxifuS10Xa/zUx4s5O/OMLf4=";
+  vendorHash = "sha256-aKm8a/PS+1ozImh1aL2EliALyUqjPMMBh4NTbL0H/ng=";
 
   meta = {
     description = "Mobile Device Management server for Apple Devices, focused on giving you all the power through an API";
