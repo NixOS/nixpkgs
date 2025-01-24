0509185dc1a2360f8b7b563f2fddebfd95ce6078
Jan Tojnar <jtojnar@gmail.com>
2025-01-24 00:55:20 +0000

    xdg-desktop-portal-gnome: 47.1 → 47.2
    https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome/-/compare/47.1...47.2

--- pkgs/by-name/xd/xdg-desktop-portal-gnome/package.nix
+++ pkgs/by-name/xd/xdg-desktop-portal-gnome/package.nix
@@ -22,11 +22,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xdg-desktop-portal-gnome";
-  version = "47.1";
+  version = "47.2";
 
   src = fetchurl {
     url = "mirror://gnome/sources/xdg-desktop-portal-gnome/${lib.versions.major finalAttrs.version}/xdg-desktop-portal-gnome-${finalAttrs.version}.tar.xz";
-    hash = "sha256-1CD/chc7BNyUkdPX3YzJYgT38/J8TL6mAkNh6pg592k=";
+    hash = "sha256-S1NoqhncCqYsioTkgciiZJC013x5ALjlydQyzpHxJ0w=";
   };
 
   nativeBuildInputs = [
