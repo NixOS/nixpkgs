ca0f54cd7f0197747b033699d2db948c41fa24f1
Nick Cao <nickcao@nichi.co>
2025-01-24 15:00:21 -0500

    telegram-desktop: 5.10.4 -> 5.10.5
    Diff: https://github.com/telegramdesktop/tdesktop/compare/v5.10.4...v5.10.5
    
    Changelog: https://github.com/telegramdesktop/tdesktop/releases/tag/v5.10.5

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
@@ -46,14 +46,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "telegram-desktop-unwrapped";
-  version = "5.10.4";
+  version = "5.10.5";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-3v9B4tdD9oNyQ96t21VRmV43Ib7l6ki8DlFXmbk7DJw=";
+    hash = "sha256-0BfKWwb+mnlaMD55KKvEMRIFvqBDQCs272lemvXBnjw=";
   };
 
   postPatch = lib.optionalString stdenv.hostPlatform.isLinux ''
