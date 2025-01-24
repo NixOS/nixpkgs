a8764bd12b5241ec09a6f42925b1e979564a1566
Alyssa Ross <hi@alyssa.is>
2025-01-24 22:11:30 +0100

    kanidm_1_3,kanidm_1_4: useFetchCargoVendor
    Cargo 1.84.0 seems to have changed the output format of cargo vendor
    again, once again invalidating fetchCargoTarball FOD hashes.  It's
    time to fix this once and for all, switching across the board to
    fetchCargoVendor, which is not dependent on cargo vendor's output
    format.

--- pkgs/by-name/ka/kanidm/1_4.nix
+++ pkgs/by-name/ka/kanidm/1_4.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
   version = "1.4.6";
   hash = "sha256-pjJyq52wO5p34LN2Jmt0npgWBDcWin8gIX4skZ7Ff8E=";
-  cargoHash = "sha256-XyyvXxZOvaxSzH8Jd23IxE4Wyd/6h5N3HRVEvdQ8RtY=";
+  cargoHash = "sha256-33HRoH/vWPe/wOZJtQLWV9eBocbj0iR/XUu4zMehu8M=";
   patchDir = ./patches/1_4;
 }
