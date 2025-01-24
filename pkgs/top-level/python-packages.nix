30ad08f88cb5db649cf7333a611d290ded336a23
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-24 04:05:42 +0100

    python313Packages.durationpy: init at 0.9
    New dependency for the kubernets python package.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3971,6 +3971,8 @@ self: super: with self; {
 
   duo-client = callPackage ../development/python-modules/duo-client { };
 
+  durationpy = callPackage ../development/python-modules/durationpy { };
+
   durus = callPackage ../development/python-modules/durus {  };
 
   dvc = callPackage ../development/python-modules/dvc {  };
