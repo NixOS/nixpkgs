ba29f37281306935c8fc33f75fab45670c2931e4
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-24 07:29:58 +0000

    apacheHttpdPackages.php: 8.3.15 -> 8.3.16

--- pkgs/development/interpreters/php/8.3.nix
+++ pkgs/development/interpreters/php/8.3.nix
@@ -4,8 +4,8 @@ let
   base = callPackage ./generic.nix (
     _args
     // {
-      version = "8.3.15";
-      hash = "sha256-sWdaT/cwtYEbjmp2h0iMQug14Vapl3aqPm8Ber2jvpg=";
+      version = "8.3.16";
+      hash = "sha256-6SCCGMvcuBaDS2xe2N3FdI+xL/d3z54OA7tIlidmCLY=";
     }
   );
 in
