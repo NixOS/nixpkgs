2d35fd37468b646e28d67c02579ffc5f629f6a0e
scraptux <git@thomasjasny.de>
2025-01-25 13:53:10 +0100

    python312Packages.grpcio-status: 1.69.0 -> 1.70.0

--- pkgs/development/python-modules/grpcio-status/default.nix
+++ pkgs/development/python-modules/grpcio-status/default.nix
@@ -13,7 +13,7 @@
 # nixpkgs-update: no auto update
 buildPythonPackage rec {
   pname = "grpcio-status";
-  version = "1.69.0";
+  version = "1.70.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "grpcio_status";
     inherit version;
-    hash = "sha256-WV74TlF41igcqnMsz2j/gyWSQWCNJrDpxApeZu7iotI=";
+    hash = "sha256-DntCgWUSQzsYuddkKF/wKb3gWenUH4/hCmBjG9g0gQE=";
   };
 
   postPatch = ''
