8670f5456c34f1214afff9a52164ff384f41e6af
Alyssa Ross <hi@alyssa.is>
2025-01-25 11:04:28 +0100

    panopticon: remove
    Unmaintained upstream, broken for more than a year, and lockfile is
    too old to use fetchCargoVendor.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1047,6 +1047,7 @@ mapAliases {
   PageEdit = pageedit; # Added 2024-01-21
   p2pvc = throw "p2pvc has been removed as it is unmaintained upstream and depends on OpenCV 2"; # Added 2024-08-20
   packet-cli = throw "'packet-cli' has been renamed to/replaced by 'metal-cli'"; # Converted to throw 2024-10-17
+  panopticon = throw "'panopticon' has been removed because it is unmaintained upstream"; # Added 2025-01-25
   paperoni = throw "paperoni has been removed, because it is unmaintained"; # Added 2024-07-14
   paperless = throw "'paperless' has been renamed to/replaced by 'paperless-ngx'"; # Converted to throw 2024-10-17
   paperless-ng = paperless-ngx; # Added 2022-04-11
