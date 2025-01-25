9597dcf2fac7e0463ce3e83e86111f3c5ad13ac2
pavankumar <katakampavan.btech@gmail.com>
2025-01-25 12:54:55 +0530

    maintainers: add knvpk

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11510,6 +11510,12 @@
     githubId = 361496;
     name = "Nikola Knežević";
   };
+  knvpk = {
+    email = "katakampavan.btech@gmail.com";
+    github = "knvpk";
+    githubId = 6078669;
+    name = "Pavan Katakam";
+  };
   kolaente = {
     email = "k@knt.li";
     github = "kolaente";
