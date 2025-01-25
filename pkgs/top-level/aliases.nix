5afa8af59cbcfb82838c99bdd2df508abff70bcf
Alyssa Ross <hi@alyssa.is>
2025-01-25 12:20:28 +0100

    rnix-hashes: remove
    Upstream repo has been archived since 2022.  Lockfile is too old for
    fetchCargoVendor.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1198,6 +1198,7 @@ mapAliases {
   rippled = throw "rippled has been removed as it was broken and had not been updated since 2022"; # Added 2024-11-25
   rippled-validator-keys-tool = throw "rippled-validator-keys-tool has been removed as it was broken and had not been updated since 2022"; # Added 2024-11-25
   rockbox_utility = rockbox-utility; # Added 2022-03-17
+  rnix-hashes = throw "'rnix-hashes' has been removed due to lack of upstream maintenance"; # Added 2025-01-25
   rpiboot-unstable = throw "'rpiboot-unstable' has been renamed to/replaced by 'rpiboot'"; # Converted to throw 2024-10-17
   rr-unstable = rr; # Added 2022-09-17
   rs-git-fsmonitor = throw "'rs-git-fsmonitor' has been removed due to lack of upstream maintenance"; # Added 2025-01-25
