1d9a743f42ab3a55ec94a1b07dd2974db0b406ef
matthiasdotsh <git@matthias.sh>
2025-01-25 13:49:01 +0100

    python3Packages.soundfile: 0.12.1 -> 0.13.1

--- pkgs/development/python-modules/soundfile/default.nix
+++ pkgs/development/python-modules/soundfile/default.nix
@@ -12,17 +12,15 @@
 
 buildPythonPackage rec {
   pname = "soundfile";
-  version = "0.12.1";
+  version = "0.13.1";
   format = "setuptools";
   # https://github.com/bastibe/python-soundfile/issues/157
   disabled = isPyPy || stdenv.hostPlatform.isi686;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-6OEBeyzx3adnrvGdL9nuXr4H4FDUMPd6Cnxmugi4za4=";
+    hash = "sha256-ssaNqx4wKXMXCApbQ99X4wJYTEnilC3v3eCszMU/Dls=";
   };
-
-  patches = [ ./0001-Fix-build-on-linux-arm64.patch ];
 
   postPatch = ''
     substituteInPlace soundfile.py --replace "_find_library('sndfile')" "'${libsndfile.out}/lib/libsndfile${stdenv.hostPlatform.extensions.sharedLibrary}'"
