74234814c278c7291cc8ec0a04a22512dde0c115
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-25 02:54:33 +0000

    fcitx5-pinyin-moegirl: 20250111 -> 20250113

--- pkgs/by-name/fc/fcitx5-pinyin-moegirl/package.nix
+++ pkgs/by-name/fc/fcitx5-pinyin-moegirl/package.nix
@@ -6,11 +6,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "fcitx5-pinyin-moegirl";
-  version = "20250111";
+  version = "20250113";
 
   src = fetchurl {
     url = "https://github.com/outloudvi/mw2fcitx/releases/download/${finalAttrs.version}/moegirl.dict";
-    hash = "sha256-ooYpkrqeOc5OrjeGTbobfv4b8x5uV/ot3QaU3d5MRLQ=";
+    hash = "sha256-Z6MqYZMpvTGcz61NnzZQy63GXfcaTWE8dezPoYht6q0=";
   };
 
   dontUnpack = true;
