b4093b7d91e999ffaaaf62859b9d12a4921924a4
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-25 00:39:45 +0000

    copywrite: 0.19.0 -> 0.20.0

--- pkgs/by-name/co/copywrite/package.nix
+++ pkgs/by-name/co/copywrite/package.nix
@@ -14,16 +14,16 @@ let
 in
 buildGoModule rec {
   pname = "copywrite";
-  version = "0.19.0";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = "copywrite";
     rev = "v${version}";
-    hash = "sha256-DmlPioaw/wMk8GoBYNG24P4J1C6h0bjVjjOuMYW6Tgo=";
+    hash = "sha256-7+XBymJuFIWfpeBNfe1hXkZDQkhG+u+H+FzmNvs/Ca4=";
   };
 
-  vendorHash = "sha256-ZIu0/fue3xi+YVE9GFsVjCNs8t3c3TWH8O0xUzJdim8=";
+  vendorHash = "sha256-Qxp6BwN/Y6Xb1BwFGT/T8WYsXGPgN27mzoTE0i6cS1Q=";
 
   ldflags = [
     "-s"
