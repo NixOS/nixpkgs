2cb93fc62fcd9714050a39b84b4c9b75247f0159
macronova <trivial@invariantspace.com>
2025-01-25 16:02:30 +0800

    Minor format

--- pkgs/kde/default.nix
+++ pkgs/kde/default.nix
@@ -93,7 +93,7 @@ let
         koi = self.callPackage ./third-party/koi { };
         krohnkite = self.callPackage ./third-party/krohnkite { };
         kzones = self.callPackage ./third-party/kzones { };
-        wallpaper-engine-plugin = self.callPackage ./third-party/wallpaper-engine-plugin {};
+        wallpaper-engine-plugin = self.callPackage ./third-party/wallpaper-engine-plugin { };
       }
     );
 in
