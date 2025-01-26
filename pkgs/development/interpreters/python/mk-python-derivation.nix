677880c6c1f5222067456abeab908e0b3b78ef6f
Yueh-Shun Li <shamrocklee@posteo.net>
2025-01-27 01:07:30 +0800

    python3Packages.buildPythonPackage: pass passthru.dependencies empty or not

--- pkgs/development/interpreters/python/mk-python-derivation.nix
+++ pkgs/development/interpreters/python/mk-python-derivation.nix
@@ -402,6 +402,9 @@ let
           inherit
             disabled
             pyproject
+            build-system
+            dependencies
+            optional-dependencies
             ;
         }
         // {
@@ -413,15 +416,6 @@ let
               update-python-libraries
               filename
             ];
-        }
-        // optionalAttrs (dependencies != [ ]) {
-          inherit dependencies;
-        }
-        // optionalAttrs (optional-dependencies != { }) {
-          inherit optional-dependencies;
-        }
-        // optionalAttrs (build-system != [ ]) {
-          inherit build-system;
         }
         // attrs.passthru or { };
 
