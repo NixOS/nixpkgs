7a5a104e1bf2094fe83d5416bc4cabdec362486d
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-26 15:28:40 +0000

    xpra: 6.2.2 -> 6.2.3

--- pkgs/tools/X11/xpra/default.nix
+++ pkgs/tools/X11/xpra/default.nix
@@ -82,7 +82,7 @@ let
 in
 buildPythonApplication rec {
   pname = "xpra";
-  version = "6.2.2";
+  version = "6.2.3";
 
   stdenv = if withNvenc then cudaPackages.backendStdenv else args.stdenv;
 
@@ -90,7 +90,7 @@ buildPythonApplication rec {
     owner = "Xpra-org";
     repo = "xpra";
     rev = "v${version}";
-    hash = "sha256-YLz2Ex3gHabicPbGj5BFP1pwU/8K/bu4R7cWi1Fu2oM=";
+    hash = "sha256-5f6yHz3uc5qsU1F6D8r0KPo8tbrFP4pfxXTvIJYqKuI=";
   };
 
   patches = [
