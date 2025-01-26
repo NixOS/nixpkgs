NixOS/nixpkgs

commit c98ef4642cb92d9e8d992f0c269ed6c1257f7187

tectonic: build with nixpkgs harfbuzz
Build with the harfbuzz shared library from nixpkgs. This is well
supported by upstream through a cargo feature flag.

Previously the build uses a vendored harfbuzz housed in a git submodule.
This can be inconvenient when developing the package. This commit
eliminate the need to fetch git submodules.

pkgs/tools/typesetting/tectonic/default.nix
@@ -26,13 +26,14 @@ rustPlatform.buildRustPackage rec {
     owner = "tectonic-typesetting";
     repo = "tectonic";
     rev = "tectonic@${version}";
-    fetchSubmodules = true;
     sha256 = "sha256-xZHYiaQ8ASUwu0ieHIXcjRaH06SQoB6OR1y7Ok+FjAs=";
   };
 
   cargoHash = "sha256-niMgb4zsTWHw5yaa4kJOZzpOzO5gMG4k3cTHwSV+wmY=";
 
   nativeBuildInputs = [ pkg-config ];
+
+  buildFeatures = [ "external-harfbuzz" ];
 
   buildInputs = [ icu fontconfig harfbuzz openssl ]
     ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ ApplicationServices Cocoa Foundation ]);

commit 73882c4c929c5da4befc02452f6f8bb159402815

tectonic-unwrapped.src: update hash
fetchSubmodules = true was removed without updating the hash.

Fixes: c98ef4642cb9 ("tectonic: build with nixpkgs harfbuzz")

pkgs/tools/typesetting/tectonic/default.nix
@@ -28,7 +28,7 @@ rustPlatform.buildRustPackage rec {
     owner = "tectonic-typesetting";
     repo = "tectonic";
     rev = "tectonic@${version}";
-    sha256 = "sha256-xZHYiaQ8ASUwu0ieHIXcjRaH06SQoB6OR1y7Ok+FjAs=";
+    sha256 = "sha256-dZnUu0g86WJIIvwMgdmwb6oYqItxoYrGQTFNX7I61Bs=";
   };
 
   patches = [