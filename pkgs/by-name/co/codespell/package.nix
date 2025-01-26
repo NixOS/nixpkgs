a19b4bc8418827347a7de60786830f79a9abc41c
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-26 12:17:20 +0000

    codespell: 2.3.0 -> 2.4.0

--- pkgs/by-name/co/codespell/package.nix
+++ pkgs/by-name/co/codespell/package.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "codespell";
-  version = "2.3.0";
+  version = "2.4.0";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "codespell-project";
     repo = "codespell";
-    rev = "v${version}";
-    sha256 = "sha256-X3Pueu0E7Q57sbKSXqCZki4/PUb1WyWk/Zmj+lhVTM8=";
+    tag = "v${version}";
+    sha256 = "sha256-TZH3+ZzsThh0GDtiSU1ZEStmCHmEuNDrk/Vyc8E2ESI=";
   };
 
   nativeBuildInputs = with python3.pkgs; [
