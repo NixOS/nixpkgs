e847e256ff9fe3112ad707bdfe3bde02aba61b76
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-01-26 21:33:56 +0100

    ja2-stracciatella: remove helper derivation by using cargoSetupHook

--- pkgs/by-name/ja/ja2-stracciatella/package.nix
+++ pkgs/by-name/ja/ja2-stracciatella/package.nix
@@ -6,44 +6,45 @@
   cmake,
   python3,
   rustPlatform,
+  cargo,
+  rustc,
   SDL2,
   fltk,
   rapidjson,
   gtest,
 }:
-let
-  version = "0.17.0";
-  src = fetchFromGitHub {
-    owner = "ja2-stracciatella";
-    repo = "ja2-stracciatella";
-    rev = "v${version}";
-    sha256 = "0m6rvgkba29jy3yq5hs1sn26mwrjl6mamqnv4plrid5fqaivhn6j";
-  };
-  libstracciatella = rustPlatform.buildRustPackage {
-    pname = "libstracciatella";
-    inherit version;
-    src = "${src}/rust";
-    cargoHash = "sha256-asUt+wUpwwDvSyuNZds6yMC4Ef4D8woMYWamzcJJiy4=";
 
-    preBuild = ''
-      mkdir -p $out/include/stracciatella
-      export HEADER_LOCATION=$out/include/stracciatella/stracciatella.h
-    '';
-  };
+let
   stringTheoryUrl = "https://github.com/zrax/string_theory/archive/3.1.tar.gz";
   stringTheory = fetchurl {
     url = stringTheoryUrl;
     sha256 = "1flq26kkvx2m1yd38ldcq2k046yqw07jahms8a6614m924bmbv41";
   };
 in
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "ja2-stracciatella";
-  inherit src version;
+  version = "0.17.0";
+
+  src = fetchFromGitHub {
+    owner = "ja2-stracciatella";
+    repo = "ja2-stracciatella";
+    rev = "v${version}";
+    sha256 = "0m6rvgkba29jy3yq5hs1sn26mwrjl6mamqnv4plrid5fqaivhn6j";
+  };
+
+  patches = [
+    # Note: this patch is only relevant for darwin
+    ./dont-use-vendored-sdl2.patch
+  ];
 
   nativeBuildInputs = [
     cmake
     python3
+    rustPlatform.cargoSetupHook
+    cargo
+    rustc
   ];
+
   buildInputs = [
     SDL2
     fltk
@@ -51,17 +52,19 @@ stdenv.mkDerivation {
     gtest
   ];
 
-  patches = [
-    ./remove-rust-buildstep.patch
-  ];
+  cargoRoot = "rust";
+
+  cargoDeps = rustPlatform.fetchCargoTarball {
+    inherit
+      pname
+      version
+      src
+      cargoRoot
+      ;
+    hash = "sha256-zkctR16QIjCTiDmrhFqPPOThbPwsm0T0nlnCekkXNN8=";
+  };
 
   preConfigure = ''
-    # Use rust library built with nix
-    substituteInPlace CMakeLists.txt \
-      --replace lib/libstracciatella_c_api.a ${libstracciatella}/lib/libstracciatella_c_api.a \
-      --replace include/stracciatella ${libstracciatella}/include/stracciatella \
-      --replace bin/ja2-resource-pack ${libstracciatella}/bin/ja2-resource-pack
-
     # Patch dependencies that are usually loaded by url
     substituteInPlace dependencies/lib-string_theory/builder/CMakeLists.txt.in \
       --replace ${stringTheoryUrl} file://${stringTheory}
