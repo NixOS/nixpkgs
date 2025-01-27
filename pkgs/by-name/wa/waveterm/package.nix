d2daaf0efaf6577a7696c8de203804b62b48147a
liberodark <liberodark@gmail.com>
2025-01-27 18:14:32 +0100

    waveterm: 0.10.4 -> 0.11.0

--- pkgs/by-name/wa/waveterm/package.nix
+++ pkgs/by-name/wa/waveterm/package.nix
@@ -33,7 +33,7 @@
 let
   selectSystem = attrs: attrs.${stdenv.hostPlatform.system};
   pname = "waveterm";
-  version = "0.10.4";
+  version = "0.11.0";
 
   passthru.updateScript = ./update.sh;
 
@@ -64,7 +64,7 @@ let
       fetchurl {
         url = "https://github.com/wavetermdev/waveterm/releases/download/v${version}/waveterm-linux-${arch}-${version}.deb";
         hash = selectSystem {
-          x86_64-linux = "sha256-dwBnRuskajMpfaBQ5zr19+CQ3A/qen2RtxmV7GnXx0E=";
+          x86_64-linux = "sha256-4GKsO7gIQaIC7e7IH1QgIpXQqIX6NHyw8wXO0OWXRUI=";
           aarch64-linux = "sha256-HfzvbAV8RkmuwvuBtgvHgzAslbejlPJJJO7juGSMm1o=";
         };
       };
