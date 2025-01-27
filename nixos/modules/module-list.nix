a082c771468b7e4283f59c6fda149a677d5e1083
Scandiravian <nixos@scandiravian.com>
2025-01-27 11:20:44 +0100

    nixos/grist-core: init
    Co-authored-by: phaer <hello@phaer.org>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1542,6 +1542,7 @@
   ./services/web-apps/gotify-server.nix
   ./services/web-apps/gotosocial.nix
   ./services/web-apps/grav.nix
+  ./services/web-apps/grist-core.nix
   ./services/web-apps/grocy.nix
   ./services/web-apps/part-db.nix
   ./services/web-apps/pixelfed.nix
