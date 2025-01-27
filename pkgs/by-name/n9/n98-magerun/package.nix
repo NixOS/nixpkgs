86e93f9cfd3d6781a7a5911e2bbaea3dffd153d4
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2025-01-27 10:52:38 +0100

    n98-magerun: switch to buildComposerProject2

--- pkgs/by-name/n9/n98-magerun/package.nix
+++ pkgs/by-name/n9/n98-magerun/package.nix
@@ -5,7 +5,7 @@
   nix-update-script,
 }:
 
-php81.buildComposerProject (finalAttrs: {
+php81.buildComposerProject2 (finalAttrs: {
   pname = "n98-magerun";
   version = "2.3.0";
 
@@ -16,7 +16,7 @@ php81.buildComposerProject (finalAttrs: {
     hash = "sha256-/RffdYgl2cs8mlq4vHtzUZ6j0viV8Ot/cB/cB1dstFM=";
   };
 
-  vendorHash = "sha256-n608AY6AQdVuN3hfVQk02vJQ6hl/0+4LVBOsBL5o3+8=";
+  vendorHash = "sha256-huYLbqJaxeSST2WGcSdk4gR3d3KWSkIK/6tzVpCw0oQ=";
 
   passthru.updateScript = nix-update-script {
     # Excludes 1.x versions from the Github tags list
