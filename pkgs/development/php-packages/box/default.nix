e9aa78b91a051fa2b36bbfbe9991bdf12d1af6af
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2025-01-27 17:07:58 +0100

    php.packages.box: switch to buildComposerProject2

--- pkgs/development/php-packages/box/default.nix
+++ pkgs/development/php-packages/box/default.nix
@@ -4,7 +4,7 @@
   fetchFromGitHub,
 }:
 
-php82.buildComposerProject (finalAttrs: {
+php82.buildComposerProject2 (finalAttrs: {
   pname = "box";
   version = "4.6.2";
 
@@ -15,7 +15,7 @@ php82.buildComposerProject (finalAttrs: {
     hash = "sha256-gYIAP9pTjahNkpNNXx0c8sQm+9Kaq6/IAo/xI5bNy7Y=";
   };
 
-  vendorHash = "sha256-HCbjW4HdyQNWDEHXj9U1t3S3EKcrPV1z/9I1ClFsMsc=";
+  vendorHash = "sha256-TAubvl+rsdQdqKz+lRg1oX/ENuRyHoJQVmL1ELz24fg=";
 
   meta = {
     changelog = "https://github.com/box-project/box/releases/tag/${finalAttrs.version}";
