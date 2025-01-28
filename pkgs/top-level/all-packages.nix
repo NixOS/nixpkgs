fd7329e1509850fe3210726b4d64f3840d34c32b
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-01-28 12:23:50 +0100

    ja2-stracciatella: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16308,8 +16308,6 @@ with pkgs;
   # used as base package for iortcw forks
   iortcw_sp = callPackage ../games/iortcw/sp.nix { };
 
-  ja2-stracciatella = callPackage ../games/ja2-stracciatella { };
-
   katagoWithCuda = katago.override {
     backend = "cuda";
     cudaPackages = cudaPackages_12;
