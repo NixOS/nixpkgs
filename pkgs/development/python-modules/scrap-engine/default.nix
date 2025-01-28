52071485c97300c6c543d834d4b0cd5f97fb6637
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-28 10:43:11 +0000

    python312Packages.scrap-engine: 1.4.1 -> 1.4.2

--- pkgs/development/python-modules/scrap-engine/default.nix
+++ pkgs/development/python-modules/scrap-engine/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "scrap-engine";
-  version = "1.4.1";
+  version = "1.4.2";
   pyproject = true;
 
   src = fetchPypi {
     pname = "scrap_engine";
     inherit version;
-    hash = "sha256-qxzbVYFcSKcL2HtMlH9epO/sCx9HckWAt/NyVD8QJBQ=";
+    hash = "sha256-9jiStwFD5vhPSxWsEVmHOvfRgNgVOUvlzAa1Rmf+faE=";
   };
 
   build-system = [
