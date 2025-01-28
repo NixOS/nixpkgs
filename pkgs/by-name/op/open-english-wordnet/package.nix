e37947402f003cc8473b863af6ac55d8ea401e18
nicoo <nicoo@mur.at>
2025-01-28 15:26:56 +0000

    open-english-wordnet: 2022 → 2024

--- pkgs/by-name/op/open-english-wordnet/package.nix
+++ pkgs/by-name/op/open-english-wordnet/package.nix
@@ -1,7 +1,6 @@
 {
   lib,
   fetchFromGitHub,
-  fetchpatch,
   gzip,
   python3,
   stdenvNoCC,
@@ -9,30 +8,14 @@
 
 stdenvNoCC.mkDerivation (self: {
   pname = "open-english-wordnet";
-  version = "2022";
+  version = "2024";
 
   src = fetchFromGitHub {
     owner = "globalwordnet";
     repo = "english-wordnet";
     rev = "${self.version}-edition";
-    hash = "sha256-a1fWIp39uuJZL1aFX/r+ttLB1+kwh/XPHwphgENTQ5M=";
+    hash = "sha256-9HGcVoI4xnkIkGw03oF6jT0a5xqv4pZz1s0l0gvqsyY=";
   };
-
-  patches =
-    lib.mapAttrsToList
-      (
-        rev: hash:
-        fetchpatch {
-          url = "https://github.com/globalwordnet/english-wordnet/commit/${rev}.patch";
-          inherit hash;
-        }
-      )
-      {
-        # Upstream commit bumping the version number, accidentally ommited from the tagged release
-        "bc07902f8995b62c70f01a282b23f40f30630540" = "sha256-1e4MG/k86g3OFUhiShCCbNXnvDKrYFr1KlGVsGl++KI=";
-        # PR #982, “merge.py: Make result independent of filesystem order”
-        "6da46a48dd76a48ad9ff563e6c807b8271fc83cd" = "sha256-QkkJH7NVGy/IbeSWkotU80IGF4esz0b8mIL9soHdQtQ=";
-      };
 
   # TODO(nicoo): make compression optional?
   nativeBuildInputs = [
@@ -45,13 +28,19 @@ stdenvNoCC.mkDerivation (self: {
     runHook preBuild
 
     echo Generating wn.xml
-    python scripts/from-yaml.py
-    python scripts/merge.py
+    python scripts/from_yaml.py
 
     echo Compressing
     gzip --best --no-name --stdout ./wn.xml > 'oewn:${self.version}.xml.gz'
 
     runHook postBuild
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    runHook preCheck
+    python scripts/validate.py
+    runHook postCheck
   '';
 
   installPhase = ''
