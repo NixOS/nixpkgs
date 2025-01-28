dbbe6b2ce3aa46c225bb1e4dcec4c132813a049c
r-vdp <ramses@well-founded.dev>
2025-01-28 12:00:10 +0100

    nixos/activation: document the arguments that are passed to the pre-switch-check script

--- nixos/modules/system/activation/pre-switch-check.nix
+++ nixos/modules/system/activation/pre-switch-check.nix
@@ -44,6 +44,9 @@ in
       A set of shell script fragments that are executed before the switch to a
       new NixOS system configuration. A failure in any of these fragments will
       cause the switch to fail and exit early.
+      The scripts receive the new configuration path and the action verb passed
+      to switch-to-configuration, as the first and second positional arguments
+      (meaning that you can access them using `$1` and `$2`, respectively).
     '';
 
     type = lib.types.attrsOf lib.types.str;
