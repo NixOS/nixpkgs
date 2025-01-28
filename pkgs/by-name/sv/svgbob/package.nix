ce57aac04036f26df08b316be500b6d05632a33d
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-28 00:55:01 +0000

    svgbob: 0.7.4 -> 0.7.6

--- pkgs/by-name/sv/svgbob/package.nix
+++ pkgs/by-name/sv/svgbob/package.nix
@@ -6,15 +6,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "svgbob";
-  version = "0.7.4";
+  version = "0.7.6";
 
   src = fetchCrate {
     inherit version;
     crateName = "svgbob_cli";
-    hash = "sha256-qSY12WjSPMoWqJHkYnPvhCtZAuI3eq+sA+/Yr9Yssp8=";
+    hash = "sha256-mPF6GcsXl/Bcri9d8SS21+/sjssT4//ktwC620NrEUg=";
   };
 
-  cargoHash = "sha256-dgOEztAlX823M+bc+vnrOvmeWtxxCsCR6+k1Yho82EM=";
+  cargoHash = "sha256-WXjHcfccoZa+7hqFfA4vbWLMlhbkfafG1Fgilbmi60E=";
 
   postInstall = ''
     mv $out/bin/svgbob_cli $out/bin/svgbob
