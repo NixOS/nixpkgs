aacba94b741f54190d187168495bcba8fb44c89a
Diogo Correia <dv_correia@hotmail.com>
2025-01-29 00:12:25 +0000

    copywrite: add `passthru.updateScript`

--- pkgs/by-name/co/copywrite/package.nix
+++ pkgs/by-name/co/copywrite/package.nix
@@ -5,6 +5,7 @@
   fetchFromGitHub,
   installShellFiles,
   versionCheckHook,
+  writeScript,
 }:
 
 let
@@ -43,6 +44,22 @@ buildGoModule rec {
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   doInstallCheck = true;
+
+  passthru.updateScript = writeScript "update-copywrite" ''
+    #!/usr/bin/env nix-shell
+    #!nix-shell -i bash -p curl jq nix-update
+
+    set -eu -o pipefail
+
+    gh_metadata="$(curl -sS https://api.github.com/repos/hashicorp/copywrite/tags)"
+    version="$(echo "$gh_metadata" | jq -r '.[] | .name' | sort --version-sort | tail -1)"
+    commit_hash="$(echo "$gh_metadata" | jq -r --arg ver "$version" '.[] | select(.name == $ver).commit.sha')"
+
+    filename="$(nix-instantiate --eval -E "with import ./. {}; (builtins.unsafeGetAttrPos \"version\" copywrite).file" | tr -d '"')"
+    sed -i "s/commitHash = \"[^\"]*\"/commitHash = \"$commit_hash\"/" $filename
+
+    nix-update copywrite
+  '';
 
   meta = {
     description = "Automate copyright headers and license files at scale";
