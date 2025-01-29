c11f9721046e93b2670585195a82ad82a2413903
Jared Baur <jaredbaur@fastmail.com>
2025-01-28 17:25:29 -0800

    vboot_reference: 111.15329 -> 133.16151

--- pkgs/by-name/vb/vboot_reference/package.nix
+++ pkgs/by-name/vb/vboot_reference/package.nix
@@ -9,15 +9,15 @@
   xz,
 }:
 
-stdenv.mkDerivation rec {
-  version = "111.15329";
+stdenv.mkDerivation {
+  version = "133.16151";
 
   pname = "vboot_reference";
 
   src = fetchFromGitiles {
     url = "https://chromium.googlesource.com/chromiumos/platform/vboot_reference";
-    rev = "1a1cb5c9a38030a5868e2aaad295c68432c680fd"; # refs/heads/release-R111-15329.B
-    sha256 = "sha256-56/hqqFiKHw0/ah0D20U1ueIU2iq8I4Wn5DiEWxB9qA=";
+    rev = "3f94e2c7ed58c4e67d6e7dc6052ec615dbbb9bb4"; # refs/heads/release-R133-16151.B
+    hash = "sha256-47dMD6wTwQRVP1lufBoMbPd5HocKsrqxMdlMJd5pWIo=";
   };
 
   nativeBuildInputs = [ pkg-config ];
@@ -54,7 +54,7 @@ stdenv.mkDerivation rec {
     "HOST_ARCH=${stdenv.hostPlatform.parsed.cpu.name}"
     "USE_FLASHROM=0"
     # Upstream has weird opinions about DESTDIR
-    # https://chromium.googlesource.com/chromiumos/platform/vboot_reference/+/refs/heads/release-R111-15329.B/Makefile#51
+    # https://chromium.googlesource.com/chromiumos/platform/vboot_reference/+/refs/heads/release-R133-16151.B/Makefile#51
     "UB_DIR=${placeholder "out"}/bin"
     "UL_DIR=${placeholder "out"}/lib"
     "UI_DIR=${placeholder "out"}/include/vboot"
