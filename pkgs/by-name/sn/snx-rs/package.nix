d66a7f2c8771efd7fad3e3bd89fda8dae05a3fea
Marco Desiderati <desideratimarco@gmail.com>
2025-01-29 09:01:38 +0100

    snx-rs: 2.2.3 -> 2.9.0
    https://github.com/ancwrd1/snx-rs/blob/v2.9.0/CHANGELOG.md
    
    Added dependencies for snx-rs GUI to work.

--- pkgs/by-name/sn/snx-rs/package.nix
+++ pkgs/by-name/sn/snx-rs/package.nix
@@ -1,35 +1,56 @@
 {
   fetchFromGitHub,
-  rustPlatform,
-  lib,
-  pkg-config,
-  openssl,
   glib,
-  atk,
   gtk3,
+  iproute2,
+  kdePackages,
+  lib,
+  libappindicator,
+  libappindicator-gtk2,
+  libappindicator-gtk3,
+  libayatana-appindicator,
   libsoup_3,
+  openssl,
+  pkg-config,
+  rustPlatform,
   webkitgtk_4_1,
+  nix-update-script,
 }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "snx-rs";
-  version = "2.2.3";
+  version = "2.9.0";
 
   src = fetchFromGitHub {
     owner = "ancwrd1";
     repo = "snx-rs";
-    rev = "v2.2.3";
-    hash = "sha256-tBl67uDeYVmVBwi8NQVclfFQ0Sj1dl+hR8Jct1iE2LI=";
+    rev = "v2.9.0";
+    hash = "sha256-9uTawt9Lk3YJbw8hzrP91bEaPGT1wZehrmCWDKYrw5w=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  passthru.updateScript = nix-update-script { };
+
+  nativeBuildInputs = [
+    iproute2
+    pkg-config
+  ];
+
   buildInputs = [
-    openssl
     glib
-    atk
     gtk3
+    kdePackages.kstatusnotifieritem
+    libappindicator
+    libappindicator-gtk2
+    libappindicator-gtk3
+    libayatana-appindicator
     libsoup_3
+    openssl
     webkitgtk_4_1
   ];
+
+  postPatch = ''
+    substituteInPlace $cargoDepsCopy/libappindicator-sys-*/src/lib.rs \
+      --replace-fail "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1"
+  '';
 
   checkFlags = [
     "--skip=platform::linux::net::tests::test_default_ip"
@@ -38,7 +59,7 @@ rustPlatform.buildRustPackage {
   cargoLock = {
     lockFile = ./Cargo.lock;
     outputHashes = {
-      "isakmp-0.1.0" = "sha256-6v5xhkt9iaQg3Eh8S1tXW55oLv4YFDYvY0cfsepMuIM=";
+      "isakmp-0.1.0" = "sha256-H8yLwIUzlaPA0kJEfdGn8jvSjs27zWeIFdHlF3HitRk=";
     };
   };
 
@@ -46,6 +67,10 @@ rustPlatform.buildRustPackage {
     description = "Open source Linux client for Checkpoint VPN tunnels";
     homepage = "https://github.com/ancwrd1/snx-rs";
     license = lib.licenses.agpl3Plus;
-    maintainers = [ ];
+    changelog = "https://github.com/ancwrd1/snx-rs/blob/v${version}/CHANGELOG.md";
+    maintainers = with lib.maintainers; [
+      shavyn
+    ];
+    mainProgram = "snx-rs";
   };
 }
