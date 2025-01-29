f8c001d17ddd3e66f9690de23ad5bd37f5270326
Diogo Correia <dv_correia@hotmail.com>
2025-01-29 00:13:58 +0000

    copywrite: 0.19.0 -> 0.20.0

--- pkgs/by-name/co/copywrite/package.nix
+++ pkgs/by-name/co/copywrite/package.nix
@@ -9,21 +9,21 @@
 }:
 
 let
-  commitHash = "6ed520a710166c6094098b786c63f212604654a4"; # matches tag release
+  commitHash = "87b93e541e2552e683f4ac2c656eb9e0301f414a"; # matches tag release
   shortCommitHash = builtins.substring 0 7 commitHash;
 in
 buildGoModule rec {
   pname = "copywrite";
-  version = "0.19.0";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = "copywrite";
     tag = "v${version}";
-    hash = "sha256-DmlPioaw/wMk8GoBYNG24P4J1C6h0bjVjjOuMYW6Tgo=";
+    hash = "sha256-7+XBymJuFIWfpeBNfe1hXkZDQkhG+u+H+FzmNvs/Ca4=";
   };
 
-  vendorHash = "sha256-ZIu0/fue3xi+YVE9GFsVjCNs8t3c3TWH8O0xUzJdim8=";
+  vendorHash = "sha256-Qxp6BwN/Y6Xb1BwFGT/T8WYsXGPgN27mzoTE0i6cS1Q=";
 
   ldflags = [
     "-s"
