25c8a3a38d10e6a04a45021f92b856f48b8c2cd5
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-01-29 23:57:52 +0000

    gallery-dl: 1.28.4 -> 1.28.5
    Changelog: https://github.com/mikf/gallery-dl/releases/tag/v1.28.5
    Diff: https://github.com/mikf/gallery-dl/compare/v1.28.4...v1.28.5

--- pkgs/by-name/ga/gallery-dl/package.nix
+++ pkgs/by-name/ga/gallery-dl/package.nix
@@ -8,7 +8,7 @@
 
 let
   pname = "gallery-dl";
-  version = "1.28.4";
+  version = "1.28.5";
 in
 python3Packages.buildPythonApplication {
   inherit pname version;
@@ -18,7 +18,7 @@ python3Packages.buildPythonApplication {
     owner = "mikf";
     repo = "gallery-dl";
     tag = "v${version}";
-    hash = "sha256-bfE3QUu+ytxd4YkWxsnQ3hTEDE1OgJN8zJxSah2u08I=";
+    hash = "sha256-wFQtpHkMPscrGhk5tNGc38rPHbFuoDMuvO50KYR+j9M=";
   };
 
   build-system = [ python3Packages.setuptools ];
