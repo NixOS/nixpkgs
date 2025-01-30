0d2182998d634c037545a993eeb53c02367b09d2
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-30 08:36:59 +0000

    trealla: 2.63.15 -> 2.63.33

--- pkgs/by-name/tr/trealla/package.nix
+++ pkgs/by-name/tr/trealla/package.nix
@@ -23,13 +23,13 @@ assert lib.elem lineEditingLibrary [
 ];
 stdenv.mkDerivation (finalAttrs: {
   pname = "trealla";
-  version = "2.63.15";
+  version = "2.63.33";
 
   src = fetchFromGitHub {
     owner = "trealla-prolog";
     repo = "trealla";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-4YQCfkAJTTS0n0hDUcc25ks0XtToqNL7eFVM1aPjE/w=";
+    hash = "sha256-ivbjnYKuZW3xvyPxOF3Ov/0obI71lri/itsVFJ4Cb4k=";
   };
 
   postPatch = ''
