acdb1a700f777ccbc8e35fc968a66c07f2dbd7b7
Théo Bori <theo1.bori@epitech.eu>
2025-01-30 22:51:10 +0100

    vscode-extensions.nhoizey.gremlins: init at 0.26.0
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -3752,6 +3752,23 @@ let
         };
       };
 
+      nhoizey.gremlins = buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "gremlins";
+          publisher = "nhoizey";
+          version = "0.26.0";
+          hash = "sha256-ML04SccSOrj5qY0HHJ5jiNbWkPElU1+zZNSX2i1K2uk=";
+        };
+        meta = {
+          changelog = "https://marketplace.visualstudio.com/items/nhoizey.gremlins/changelog";
+          description = "Reveals some characters that can be harmful because they are invisible or looking like legitimate ones";
+          downloadPage = "https://marketplace.visualstudio.com/items?itemName=nhoizey.gremlins";
+          homepage = "https://github.com/nhoizey/vscode-gremlins";
+          license = lib.licenses.mit;
+          maintainers = [ lib.maintainers.theobori ];
+        };
+      };
+
       nimlang.nimlang = callPackage ./nimlang.nimlang { };
 
       njpwerner.autodocstring = buildVscodeMarketplaceExtension {
