296082164ea7871c64fb1d5cce3fb641122d11b6
Christina Sørensen <ces@fem.gg>
2025-01-30 09:21:38 +0100

    eza: 0.20.18 -> 0.20.19
    changelog: https://github.com/eza-community/eza/releases/tag/v0.20.19

--- pkgs/by-name/ez/eza/package.nix
+++ pkgs/by-name/ez/eza/package.nix
@@ -15,16 +15,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "eza";
-  version = "0.20.18";
+  version = "0.20.19";
 
   src = fetchFromGitHub {
     owner = "eza-community";
     repo = "eza";
     rev = "v${version}";
-    hash = "sha256-XJ8RYfBduTK5CkvFjVRF6yGkgBdojuewFUrEvRcTJsA=";
+    hash = "sha256-Fgb2+tKDTL7KxJhRavNc5K8AG98eZFOmDOc+sVY7rdk=";
   };
 
-  cargoHash = "sha256-FUd2dtSwZEBJDCs5EbgrtHfO2oquZP/4wxhVreCaayg=";
+  cargoHash = "sha256-+scWqtriW9qh6nqiPV3LNLC333JRw5WZI04BHIZlqE4=";
 
   nativeBuildInputs = [
     cmake
