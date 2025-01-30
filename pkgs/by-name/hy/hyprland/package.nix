9c1943c452f86cff081615d37a25201e06d6d0ee
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-01-30 10:22:14 +0100

    hyprland: 0.47.0 -> 0.47.1

--- pkgs/by-name/hy/hyprland/package.nix
+++ pkgs/by-name/hy/hyprland/package.nix
@@ -86,14 +86,14 @@ assert assertMsg (!hidpiXWayland)
 
 customStdenv.mkDerivation (finalAttrs: {
   pname = "hyprland" + optionalString debug "-debug";
-  version = "0.47.0";
+  version = "0.47.1";
 
   src = fetchFromGitHub {
     owner = "hyprwm";
     repo = "hyprland";
     fetchSubmodules = true;
     tag = "v${finalAttrs.version}";
-    hash = "sha256-rxvLiGNRUSVYPFsKIsqEbhxpR0sbWmHoo3azZKQ9fTY=";
+    hash = "sha256-374Ul6LCnuDdZ7o1xl2lDxTfmwt46K6csf6eniqFRYA=";
   };
 
   postPatch = ''
