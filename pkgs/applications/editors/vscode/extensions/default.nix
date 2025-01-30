f318e49807498bd68c56fc099bc01250470edc9a
Felix Kimmel <91203390+TheRobot2105@users.noreply.github.com>
2025-01-30 22:54:33 +0100

    vscode-extensions.platformio.platformio-vscode-ide: init at 3.3.4

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -4039,6 +4039,23 @@ let
         };
       };
 
+      platformio.platformio-vscode-ide = buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "platformio-ide";
+          publisher = "platformio";
+          version = "3.3.4";
+          hash = "sha256-qfNz4IYjCmCMFLtAkbGTW5xnsVT8iDnFWjrgkmr2Slk=";
+        };
+        meta = {
+          changelog = "https://marketplace.visualstudio.com/items/platformio.platformio-ide/changelog";
+          description = "Open source ecosystem for IoT development in VSCode";
+          downloadPage = "https://marketplace.visualstudio.com/items?itemName=platformio.platformio-ide";
+          homepage = "https://platformio.org/";
+          license = lib.licenses.asl20;
+          maintainers = [ lib.maintainers.therobot2105 ];
+        };
+      };
+
       pollywoggames.pico8-ls = buildVscodeMarketplaceExtension {
         mktplcRef = {
           name = "pico8-ls";
