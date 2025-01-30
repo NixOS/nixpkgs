b95c02330b0e5d7cb78f1bdad184328b99a624f1
Yechiel Worenklein <41305372+yechielw@users.noreply.github.com>
2025-01-30 11:33:08 +0200

    burpsuite: 2024.11.1 2025.1

--- pkgs/by-name/bu/burpsuite/package.nix
+++ pkgs/by-name/bu/burpsuite/package.nix
@@ -9,20 +9,20 @@
 }:
 
 let
-  version = "2024.11.2";
+  version = "2025.1";
 
   product =
     if proEdition then
       {
         productName = "pro";
         productDesktop = "Burp Suite Professional Edition";
-        hash = "sha256-VoihG+JjA39wQ1Z7rRwenggwQCNTgg9wgi3vF1tAi6A=";
+        hash = "sha256-esZOy1kTdgM7qsTBMTDh6HtGCUX7MwUUTUCwambzvjU=";
       }
     else
       {
         productName = "community";
         productDesktop = "Burp Suite Community Edition";
-        hash = "sha256-KSpZ+QO+R1c5wc/I07TQIhfEoh8lrHQR8H+2C1Al+wk=";
+        hash = "sha256-O2n3Bh90t4zGUak7OCv5peBJLo2PEDPnQEMaV1V29g4=";
       };
 
   src = fetchurl {
