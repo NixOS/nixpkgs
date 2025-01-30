0024b06135ff151a1af301babb872609748c98a9
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-30 18:12:55 +0000

    python312Packages.quantile-forest: 1.3.11 -> 1.4.0

--- pkgs/development/python-modules/quantile-forest/default.nix
+++ pkgs/development/python-modules/quantile-forest/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "quantile-forest";
-  version = "1.3.11";
+  version = "1.4.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "zillow";
     repo = "quantile-forest";
     tag = "v${version}";
-    hash = "sha256-0gdXjr+mRInZtvmgH1vRjLCkXZmTn1zWmD3E1ass9hQ=";
+    hash = "sha256-VzHGjV2WWCebUh3UINFi4DbrXBnZXVK2GQtcJb0oKoM=";
   };
 
   build-system = [
