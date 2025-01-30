af393325d578823e4864d71f4b6cdf351c7575d3
Wolf Mermelstein <wolf@404wolf.com>
2025-01-29 22:23:38 -0500

    python3Packages.std2: 0-unstable-2024-09-02 -> 0.1.10

--- pkgs/development/python-modules/std2/default.nix
+++ pkgs/development/python-modules/std2/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage {
   pname = "std2";
-  version = "unstable-2023-10-07";
+  version = "0.1.10";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ms-jpq";
     repo = "std2";
-    rev = "6332e559ee51c3a7c956804afdd7e1cc6ad47965";
-    hash = "sha256-huN7P/Ws6anrFXDG7L5xxMenS25BHquV9cMi1s7WFJ4=";
+    rev = "808a4ae1e050033a3863c35175894aa2a97320b2";
+    hash = "sha256-Setty21ZMUAedN80ZwiMisQNiQmQR7E9khgVsExEHNc=";
   };
 
   nativeBuildInputs = [ setuptools ];
@@ -23,6 +23,6 @@ buildPythonPackage {
     homepage = "https://github.com/ms-jpq/std2";
     description = "Dependency to chadtree and coq_nvim plugins";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ GaetanLepage ];
+    maintainers = with maintainers; [ GaetanLepage _404wolf ];
   };
 }
