1c421da5b03a046095eb4fecd62deb6b5dd00f79
R. Ryantm <ryantm-bot@ryantm.com>
2025-01-30 18:41:29 +0000

    python312Packages.aiolifx: 1.1.2 -> 1.1.3

--- pkgs/development/python-modules/aiolifx/default.nix
+++ pkgs/development/python-modules/aiolifx/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "aiolifx";
-  version = "1.1.2";
+  version = "1.1.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-M4if9eSRG7544OaVj3HXtvOWPm1mg8Nz5dh6nl0hx+4=";
+    hash = "sha256-4zLvrOHsQg8NhZlTSkAzNffN56H89JeYkKxwHNb3n+Y=";
   };
 
   build-system = [ setuptools ];
