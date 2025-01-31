b24892b5b2a0e7f208874cef8acc03288aa010ef
Will Lucas <mevatron@gmail.com>
2025-01-30 20:33:43 -0600

    python312Packages.py-datastruct: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9380,6 +9380,8 @@ self: super: with self; {
 
   py-ccm15 = callPackage ../development/python-modules/py-ccm15 { };
 
+  py-datastruct = callPackage ../development/python-modules/py-datastruct { };
+
   py-deprecate = callPackage ../development/python-modules/py-deprecate { };
 
   py-ecc = callPackage ../development/python-modules/py-ecc { };
