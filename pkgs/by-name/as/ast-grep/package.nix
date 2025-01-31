37b69d35c71507595458e801d61eae0f98c01412
ZHAO Jin-Xiang <xiaoxiangmoe@gmail.com>
2025-01-31 07:20:39 +0000

    ast-grep: 0.33.1 -> 0.34.3

--- pkgs/by-name/as/ast-grep/package.nix
+++ pkgs/by-name/as/ast-grep/package.nix
@@ -12,23 +12,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "ast-grep";
-  version = "0.33.1";
+  version = "0.34.3";
 
   src = fetchFromGitHub {
     owner = "ast-grep";
     repo = "ast-grep";
     tag = version;
-    hash = "sha256-p7SJhkCoo4jBDyr+Z2+qxjUwWXWpVMuXd2/DDOM7Z/Q=";
+    hash = "sha256-r82BDCncRUSmIBQFwsrKDwKEKmvGm/lKtz1rYC47Ems=";
   };
 
-  cargoHash = "sha256-aCBEL+Jx4Kk7PWsxIgpdRdI7AnUAUEtRU4+JMxQ4Swk=";
+  cargoHash = "sha256-ro2RbXDf9TsU6WdI1tGzCix+xTJVxzN38vyVZonAuUg=";
 
   nativeBuildInputs = [ installShellFiles ];
-
-  # error: linker `aarch64-linux-gnu-gcc` not found
-  postPatch = ''
-    rm .cargo/config.toml
-  '';
 
   cargoBuildFlags = [
     "--package ast-grep --bin ast-grep"
@@ -52,21 +47,6 @@ rustPlatform.buildRustPackage rec {
     ''
   );
 
-  checkFlags =
-    [
-      # disable flaky test
-      "--skip=test::test_load_parser_mac"
-
-      # BUG: Broke by 0.12.1 update (https://github.com/NixOS/nixpkgs/pull/257385)
-      # Please check if this is fixed in future updates of the package
-      "--skip=verify::test_case::tests::test_unmatching_id"
-    ]
-    ++ lib.optionals (with stdenv.hostPlatform; (isDarwin && isx86_64) || (isLinux && isAarch64)) [
-      # x86_64-darwin: source/benches/fixtures/json-mac.so\' (no such file), \'/private/tmp/nix-build-.../source/benches/fixtures/json-mac.so\' (mach-o file, but is an incompatible architecture (have \'arm64\', need \'x86_64h\' or \'x86_64\'))" })
-      # aarch64-linux: /build/source/benches/fixtures/json-linux.so: cannot open shared object file: No such file or directory"
-      "--skip=test::test_load_parser"
-      "--skip=test::test_register_lang"
-    ];
   nativeInstallCheckInputs = [
     versionCheckHook
   ];
