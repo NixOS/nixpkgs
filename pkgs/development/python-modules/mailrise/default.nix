6c707ada17d02bb0e897c567276aeed7b26df901
Kyle Robinson <84939608+kr-nn@users.noreply.github.com>
2025-02-01 17:08:35 -0500

    Update pkgs/development/python-modules/mailrise/default.nix
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/development/python-modules/mailrise/default.nix
+++ pkgs/development/python-modules/mailrise/default.nix
@@ -9,17 +9,20 @@
 buildPythonPackage rec {
   pname = "mailrise";
   version = "1.4.0";
-  dependencies = [
-    apprise
-    aiosmtpd
-    pyaml
-  ];
+
   src = fetchFromGitHub {
     owner = "YoRyan";
     repo = "mailrise";
     tag = version;
     hash = "sha256-QgeGDQeSsfvopgBgQQsWrx036SX1FhE67LI8M8rJM/Q=";
   };
+
+  dependencies = [
+    apprise
+    aiosmtpd
+    pyaml
+  ];
+
   meta = {
     description = "SMTP gateway for Apprise notifications";
     homepage = "https://github.com/YoRyan/mailrise";
