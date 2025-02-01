272d208437b541a8183a3e68090f51e3509b4682
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-01 01:00:37 +0100

    python313Packages.teamcity-messages: 1.32 -> 1.33
    Diff: https://github.com/JetBrains/teamcity-messages/compare/v1.32...v1.33

--- pkgs/development/python-modules/teamcity-messages/default.nix
+++ pkgs/development/python-modules/teamcity-messages/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "teamcity-messages";
-  version = "1.32";
+  version = "1.33";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "JetBrains";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-9az7kD7nKqMF2b3/eFgF+pOKKIYLvTy2sf4TSJfHRnA=";
+    hash = "sha256-BAwAfe54J+gbbiz03Yiu3eC/9RnI7P0mfR3nfM1oKZw=";
   };
 
   nativeCheckInputs = [ pytestCheckHook ];
