a6c7ea1cf23ec738bd455be9eaa163e947a04334
Thomas Gerbet <thomas@gerbet.me>
2025-02-01 12:06:33 +0100

    elasticsearch: 7.17.16 -> 7.17.27
    Fixes CVE-2024-43709, CVE-2024-23450 and CVE-2024-43709
    https://discuss.elastic.co/t/elasticsearch-7-17-21-and-8-13-3-security-update-esa-2024-25/373442
    https://discuss.elastic.co/t/elasticsearch-8-13-0-7-17-19-security-update-esa-2024-06/356314
    https://discuss.elastic.co/t/elasticsearch-7-17-21-and-8-13-3-security-update-esa-2024-25/373442
    
    Changes:
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.27.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.26.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.25.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.24.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.23.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.22.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.21.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.20.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.19.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.18.html
    https://www.elastic.co/guide/en/elasticsearch/reference/7.17/release-notes-7.17.17.html
    (cherry picked from commit 2ae4878af75c7d3c70e04af2908edfa87ffc28bc)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3345,7 +3345,7 @@ with pkgs;
 
   # The latest version used by elasticsearch, logstash, kibana and the the beats from elastic.
   # When updating make sure to update all plugins or they will break!
-  elk7Version = "7.17.16";
+  elk7Version = "7.17.27";
 
   elasticsearch7 = callPackage ../servers/search/elasticsearch/7.x.nix {
     util-linux = util-linuxMinimal;
