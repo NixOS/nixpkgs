f36dffbfa4c08a03e785a7b90370563a14864596
Markus Kowalewski <markus.kowalewski@gmail.com>
2025-02-01 11:49:12 +0100

    zenoh-*: 1.1.1 -> 1.2.0

--- pkgs/by-name/ze/zenoh-plugin-mqtt/package.nix
+++ pkgs/by-name/ze/zenoh-plugin-mqtt/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "zenoh-plugin-mqtt";
-  version = "1.1.1";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "eclipse-zenoh";
     repo = "zenoh-plugin-mqtt";
     tag = version;
-    hash = "sha256-boe4AI0U0JHNuChhHOlfCMlKJ0Zo5yUGG3xubR/Msrc=";
+    hash = "sha256-ath1RWr+5nU2GJY6rlWPPBKHX9K/92DFUhgQWVXoWb8=";
   };
 
-  cargoHash = "sha256-uB/geIBpNStXx82zL2sX6e6sLsoWRkbUsx14xLvDdsw=";
+  cargoHash = "sha256-/Af4nANgFK23pZHA2wQvooF4s/raRQ7c2Xv2f4v+z74=";
 
   # Some test time out
   doCheck = false;
