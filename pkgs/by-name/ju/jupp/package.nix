21567c829b8304cdcb03d941d5ad78472f410693
Nobody_alias_N <165517462+Nobody-alias-N@users.noreply.github.com>
2025-02-02 00:36:20 +0200

    jupp: 40 -> 41

--- pkgs/by-name/ju/jupp/package.nix
+++ pkgs/by-name/ju/jupp/package.nix
@@ -8,15 +8,15 @@
 
 stdenv.mkDerivation rec {
   pname = "jupp";
-  version = "40";
+  version = "41";
   srcName = "joe-3.1${pname}${version}";
 
   src = fetchurl {
     urls = [
       "https://www.mirbsd.org/MirOS/dist/jupp/${srcName}.tgz"
-      "https://pub.allbsd.org/MirOS/dist/jupp/${srcName}.tgz"
+      "https://mbsd.evolvis.org/MirOS/dist/jupp/${srcName}.tgz"
     ];
-    sha256 = "S+1DnN5/K+KU6W5J7z6RPqkPvl6RTbiIQD46J+gDWxo=";
+    hash = "sha256-e7jqivUZvv7/+T7DyeMhCNfyuDIWybx7Aa71CYhhyC8=";
   };
 
   preConfigure = "chmod +x ./configure";
@@ -34,7 +34,7 @@ stdenv.mkDerivation rec {
     "--enable-termidx"
   ];
 
-  meta = with lib; {
+  meta = {
     homepage = "http://www.mirbsd.org/jupp.htm";
     downloadPage = "https://www.mirbsd.org/MirOS/dist/jupp/";
     description = "Portable fork of Joe's editor";
@@ -46,8 +46,8 @@ stdenv.mkDerivation rec {
       and has a lot of bugs fixed. It is based upon an older version of joe
       because these behave better overall.
     '';
-    license = licenses.gpl1Only;
-    maintainers = with maintainers; [ AndersonTorres ];
-    platforms = with platforms; unix;
+    license = lib.licenses.gpl1Only;
+    maintainers = with lib.maintainers; [ AndersonTorres ];
+    platforms = with lib.platforms; unix;
   };
 }
