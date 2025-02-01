3afa8bc0b9101dc53528868fe78e18b2af21ec7d
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-01 13:38:12 +0000

    ultrastardx: 2025.1.0 -> 2025.2.0

--- pkgs/by-name/ul/ultrastardx/package.nix
+++ pkgs/by-name/ul/ultrastardx/package.nix
@@ -47,13 +47,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "ultrastardx";
-  version = "2025.1.0";
+  version = "2025.2.0";
 
   src = fetchFromGitHub {
     owner = "UltraStar-Deluxe";
     repo = "USDX";
     rev = "v${version}";
-    hash = "sha256-8/qFzPP3Gw9YAGsnyI+wJUP3Jo8UoZkihRgYg4P5MVo=";
+    hash = "sha256-qYM57ooqVAOUDDhBWEpk2q0Z14WE+8LdqdHFpgfzooY=";
   };
 
   nativeBuildInputs = [
