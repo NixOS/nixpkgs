de85fc8eadf19ed375d229db4b4aee2eddf87277
Morgan Jones <me@numin.it>
2025-02-01 21:31:15 -0800

    tpm2-pkcs11: add numinit as maintainer

--- pkgs/by-name/tp/tpm2-pkcs11/package.nix
+++ pkgs/by-name/tp/tpm2-pkcs11/package.nix
@@ -142,12 +142,12 @@ chosenStdenv.mkDerivation (finalAttrs: {
     };
   };
 
-  meta = with lib; {
+  meta = {
     description = "PKCS#11 interface for TPM2 hardware";
     homepage = "https://github.com/tpm2-software/tpm2-pkcs11";
-    license = licenses.bsd2;
-    platforms = platforms.linux;
-    maintainers = [ ];
+    license = lib.licenses.bsd2;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ numinit ];
     mainProgram = "tpm2_ptool";
   };
 })
