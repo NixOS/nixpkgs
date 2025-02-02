f83f6a25ce92fd1b6b331f3473f91bb5626ce67f
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2025-02-02 13:12:04 +0100

    installer: fix broken search suggestion in default config

--- nixos/modules/installer/tools/tools.nix
+++ nixos/modules/installer/tools/tools.nix
@@ -117,8 +117,8 @@ let
 
       # programs.firefox.enable = true;
 
-      # List packages installed in system profile. To search, run:
-      # \$ nix search wget
+      # List packages installed in system profile.
+      # You can use https://search.nixos.org/ to find more packages (and options).
       # environment.systemPackages = with pkgs; [
       #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       #   wget
