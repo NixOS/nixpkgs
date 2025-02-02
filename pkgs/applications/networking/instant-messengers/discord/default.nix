13b1d08271114b22353a87f737e1fc4c2e6ecd67
jopejoe1 <jopejoe1@missing.ninja>
2025-02-02 15:51:50 +0000

    pkgsCross.aarch64-darwin.discord-canary: 0.0.686 -> 0.0.692

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -18,7 +18,7 @@ let
       {
         stable = "0.0.334";
         ptb = "0.0.157";
-        canary = "0.0.686";
+        canary = "0.0.692";
         development = "0.0.78";
       };
   version = versions.${branch};
@@ -52,7 +52,7 @@ let
       };
       canary = fetchurl {
         url = "https://canary.dl2.discordapp.net/apps/osx/${version}/DiscordCanary.dmg";
-        hash = "sha256-F3vLcLjNhYRGnOyuNziwtbBNxiUKgw6wZaa1LbVF5PU=";
+        hash = "sha256-xA9TU6ODC6c8m+dLHxc5ZxD+SmTfXIjXl1wbA5JVay4=";
       };
       development = fetchurl {
         url = "https://development.dl2.discordapp.net/apps/osx/${version}/DiscordDevelopment.dmg";
