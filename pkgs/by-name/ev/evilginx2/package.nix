08488fb8b6391964836e8f6975a3ab1c7b20f81b
bryanster <45668775+bryanster@users.noreply.github.com>
2025-02-02 18:13:58 +0100

    style: format

--- pkgs/by-name/ev/evilginx2/package.nix
+++ pkgs/by-name/ev/evilginx2/package.nix
@@ -1,4 +1,9 @@
-{ stdenv, fetchzip, unzip, lib }:
+{
+  stdenv,
+  fetchzip,
+  unzip,
+  lib,
+}:
 stdenv.mkDerivation {
   pname = "evilginx2";
   version = "3.3.0";
@@ -15,14 +20,14 @@ stdenv.mkDerivation {
   meta = with lib; {
     description = "a man-in-the-middle attack framework used for phishing login credentials along with session cookies";
     longDescription = ''
-    **Evilginx** is a man-in-the-middle attack framework used for phishing login credentials along with session cookies, which in turn allows to bypass 2-factor authentication protection.
+      **Evilginx** is a man-in-the-middle attack framework used for phishing login credentials along with session cookies, which in turn allows to bypass 2-factor authentication protection.
 
-    This tool is a successor to [Evilginx](https://github.com/kgretzky/evilginx), released in 2017, which used a custom version of nginx HTTP server to provide man-in-the-middle functionality to act as a proxy between a browser and phished website.
-    Present version is fully written in GO as a standalone application, which implements its own HTTP and DNS server, making it extremely easy to set up and use.
+      This tool is a successor to [Evilginx](https://github.com/kgretzky/evilginx), released in 2017, which used a custom version of nginx HTTP server to provide man-in-the-middle functionality to act as a proxy between a browser and phished website.
+      Present version is fully written in GO as a standalone application, which implements its own HTTP and DNS server, making it extremely easy to set up and use.
     '';
     homepage = "https://github.com/kgretzky/evilginx2";
     license = licenses.bsd3;
-    mainProgram = "evilginx";
+    mainProgram = "evilginx2";
     platforms = with lib.platforms; [ "x86_64-linux" ];
   };
 }
