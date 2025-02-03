9779a3e4ad55bc21f9273737535433c37c92b48f
Keenan Weaver <keenanweaver@protonmail.com>
2025-02-03 17:48:13 -0600

    doomrunner: 1.8.3 -> 1.9.0, cleanup

--- pkgs/by-name/do/doomrunner/package.nix
+++ pkgs/by-name/do/doomrunner/package.nix
@@ -1,30 +1,26 @@
 {
   lib,
   stdenv,
-  qtbase,
-  qmake,
-  makeDesktopItem,
-  wrapQtAppsHook,
-  imagemagick,
+  kdePackages,
   fetchFromGitHub,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "doomrunner";
-  version = "1.8.3";
+  version = "1.9.0";
 
   src = fetchFromGitHub {
     owner = "Youda008";
     repo = "DoomRunner";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-NpNhl3cGXpxI8Qu4l8PjojCCXCZdGBEkBzz5XxLm/mY=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-rCoMTPGjIFAcNncBGg1IMdUahzjH0WlFZBZS0UmNI/g=";
   };
 
-  buildInputs = [ qtbase ];
+  buildInputs = [ kdePackages.qtbase ];
+
   nativeBuildInputs = [
-    qmake
-    wrapQtAppsHook
-    imagemagick
+    kdePackages.qmake
+    kdePackages.wrapQtAppsHook
   ];
 
   makeFlags = [
@@ -32,35 +28,20 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   postInstall = ''
-    mkdir -p $out/{bin,share/applications}
+    mkdir -p $out/{bin,share/applications,share/icons/hicolor/128x128/apps}
+    install -Dm444 $src/Install/XDG/DoomRunner.128x128.png $out/share/icons/hicolor/128x128/apps/DoomRunner.png
+    install -Dm444 $src/Install/XDG/DoomRunner.desktop $out/share/applications/DoomRunner.desktop
     install -Dm755 $out/usr/bin/DoomRunner $out/bin/DoomRunner
-
-    for size in 16 24 32 48 64 128; do
-      mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
-      convert -background none -resize "$size"x"$size" $PWD/Resources/DoomRunner.ico -flatten $out/share/icons/hicolor/"$size"x"$size"/apps/DoomRunner.png
-    done;
-
-    install -m 444 -D "$desktopItem/share/applications/"* -t $out/share/applications/
     rm -rf $out/usr
   '';
 
-  desktopItem = makeDesktopItem {
-    name = "DoomRunner";
-    desktopName = "DoomRunner";
-    comment = "Preset-oriented graphical launcher of ZDoom and derivatives";
-    categories = [ "Game" ];
-    icon = "DoomRunner";
-    type = "Application";
-    exec = "DoomRunner";
-  };
-
-  meta = with lib; {
-    description = "Graphical launcher of ZDoom and derivatives";
+  meta = {
+    description = "Preset-oriented graphical launcher of various ported Doom engines";
     mainProgram = "DoomRunner";
-    homepage = "https://github.com/Youda008/DoomRunner/";
+    homepage = "https://github.com/Youda008/DoomRunner";
     changelog = "https://github.com/Youda008/DoomRunner/blob/${finalAttrs.src.rev}/changelog.txt";
-    license = licenses.gpl3Only;
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ keenanweaver ];
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ keenanweaver ];
   };
 })
