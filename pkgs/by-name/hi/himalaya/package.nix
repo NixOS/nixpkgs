NixOS/nixpkgs

commit 63bff8c132d17b960f98d3878d10a410ec584472

treewide: migrate to fetchCargoVendor, batch 1
Cargo 1.84.0 seems to have changed the output format of cargo vendor
again, once again invalidating fetchCargoTarball FOD hashes.  It's
time to fix this once and for all, switching across the board to
fetchCargoVendor, which is not dependent on cargo vendor's output
format.

It should be possible to reproduce this diff.  To do so, get the list
of files changed by this commit, e.g. with git diff --name-only, then
run the following two commands, each with that list of files as their
standard input:

	xargs sed -i 's/^\(. *\)\(cargoHash\)\b/\1useFetchCargoVendor = true;\n\1cargoHash/'
	cut -d / -f 4 | xargs -n 1 nix-update --version=skip

This will take a long time.  It might be possible to parallelize it
using xargs' -P option.  I haven't tested it.

pkgs/by-name/hi/himalaya/package.nix
@@ -19,7 +19,8 @@
 let
   version = "1.1.0";
   hash = "sha256-gdrhzyhxRHZkALB3SG/aWOdA5iMYkel3Cjk5VBy3E4M=";
-  cargoHash = "sha256-MLPXcPA90YZY55jwC7XUZ6KVJf4Pn8w19iT5E2HqZV0=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-1KtS/lzVUEDiDCo3b8RFDYeTMsbHk2tIdniFZfoTxYk=";
 
   noDefaultFeatures = lib.warnIf
     (args ? buildNoDefaultFeatures)

pkgs/by-name/aa/aaa/package.nix
@@ -14,7 +14,8 @@ rustPlatform.buildRustPackage rec {
     rev = "v${version}";
     sha256 = "sha256-gIOlPjZOcmVLi9oOn4gBv6F+3Eq6t5b/3fKzoFqxclw=";
   };
-  cargoHash = "sha256-ugB0r9qiGRurc30GrJH4MKM6fWZ99+f1Gy7/1lSmrwU=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-CHX+Ugy4ND36cpxNEFpnqid6ALHMPXmfXi+D4aktPRk=";
 
   meta = with lib; {
     description = "Terminal viewer for 3a format";

pkgs/by-name/aa/aarch64-esr-decoder/package.nix
@@ -15,7 +15,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-U9i5L3s4oQOIqlECSaKkHxS2Vzr6SY4tIUpvl3+oSl0=";
   };
 
-  cargoHash = "sha256-BdxRvvU3AovlT7QloZ/LlkjRTVCWEsPUj4NkP4gBPsY=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-LiNnTNpluQkomQhIOsAnUbbBftTgqgNdpT8heCrBayg=";
 
   meta = with lib; {
     description = "Utility for decoding aarch64 ESR register values";

pkgs/by-name/aa/aardvark-dns/package.nix
@@ -16,7 +16,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-VXuTHc/t+5QidTqMptoQqircHTtYzeEXfFFII8jeOrI=";
   };
 
-  cargoHash = "sha256-MHNIb1hNoL/62aUDNs0wd4086m3Zm4bnRp+VKXE4gJs=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-ydvvV3w8VmfdSTXeJvMuN0oqkkZdWs6/9KocalRGWpg=";
 
   passthru.tests = { inherit (nixosTests) podman; };
 

pkgs/by-name/ab/ab-av1/package.nix
@@ -11,7 +11,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-I9XApll0/mvfhL/BLWoVwL0ffqa5r3dOBWYTHizJ0hc=";
   };
 
-  cargoHash = "sha256-jTCAB4O+SePCaKivHwbfFJZKjlxZmzP4uLT5idKmiO4=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-y+5rW3LruV1WeJXizbhHs01jkWrYsMXZkn1NG0Xz3bg=";
 
   nativeBuildInputs = [ installShellFiles ];
 

pkgs/by-name/ab/aba/package.nix
@@ -20,7 +20,8 @@ rustPlatform.buildRustPackage {
     hash = "sha256-2zVQNchL4DFh2v2/kwupJTBSmXiKqlxzUMrP9TbfCMs=";
   };
 
-  cargoHash = "sha256-YhSzbfcEIJjKWlyYq1lK70qt4f/Z71n7hgaaZ/D/U80=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-U1f68DY+yV6Uxwk1Re0eSNCFWYrYKCN08hJAYcp4ksE=";
 
   nativeBuildInputs = [
     just

pkgs/by-name/ac/action-validator/package.nix
@@ -16,7 +16,8 @@ rustPlatform.buildRustPackage rec {
     fetchSubmodules = true;
   };
 
-  cargoHash = "sha256-mBY+J6JcIhV++tO6Dhw5JvYLSwoYZR3lB3l0KTjkcQM=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-dy66ZkU9lIYGe9T3oR8m5cGcBQO5MF1KsLjfaHTtvlY=";
 
   meta = with lib; {
     description = "Tool to validate GitHub Action and Workflow YAML files";

pkgs/by-name/ad/ad/package.nix
@@ -17,7 +17,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-c2oSQ81qCZbwUl4TqovDWm8TUqI6RdrQPvoP/FaTI/A=";
   };
 
-  cargoHash = "sha256-VgzaAQy0z1JReMeRcX7LbbgKqrwTmnVecuLd1kyekHA=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-WEU1pipIQmlvfIBUmXiGYDN45M/C7OJdf4nzFbNQhLc=";
 
   nativeBuildInputs = [ installShellFiles ];
 

commit 294979c38bc02810dcecfc49030ca16f05fcdaf1

himalaya: fix useFetchCargoVendor
The script I ran to bulk migrate packages didn't account for cargoHash
being in a let, so here it just introduced an unused
useFetchCargoVendor binding rather than passing it to
buildRustPackage.

Fixes: 63bff8c132d1 ("treewide: migrate to fetchCargoVendor, batch 1")

pkgs/by-name/hi/himalaya/package.nix
@@ -19,8 +19,7 @@
 let
   version = "1.1.0";
   hash = "sha256-gdrhzyhxRHZkALB3SG/aWOdA5iMYkel3Cjk5VBy3E4M=";
-  useFetchCargoVendor = true;
-  cargoHash = "sha256-1KtS/lzVUEDiDCo3b8RFDYeTMsbHk2tIdniFZfoTxYk=";
+  cargoHash = "sha256-ulqMjpW3UI509vs3jVHXAEQUhxU/f/hN8XiIo8UBRq8=";
 
   noDefaultFeatures = lib.warnIf
     (args ? buildNoDefaultFeatures)
@@ -44,6 +43,8 @@ rustPlatform.buildRustPackage {
     repo = "himalaya";
     rev = "v${version}";
   };
+
+  useFetchCargoVendor = true;
 
   buildNoDefaultFeatures = noDefaultFeatures;
   buildFeatures = features;