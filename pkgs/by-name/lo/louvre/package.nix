0aea22dc8ac389b4a962006fa6dc0f127c38c0b5
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-03 23:44:00 +0000

    louvre: 2.13.0-1 -> 2.14.0-1

--- pkgs/by-name/lo/louvre/package.nix
+++ pkgs/by-name/lo/louvre/package.nix
@@ -24,13 +24,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "louvre";
-  version = "2.13.0-1";
+  version = "2.14.0-1";
 
   src = fetchFromGitHub {
     owner = "CuarzoSoftware";
     repo = "Louvre";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-ZUGan5lcE44Lt6xEvVO9Nd1ONBEvvwj8QuLDnAaenpU=";
+    hash = "sha256-2qFtgKOqTBPTKiZ5/RuQEUK8GPwm9Nv8bc7+y+3+0Ow=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/src";
