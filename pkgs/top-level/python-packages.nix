6acabcecfd0c1d5c84fbb5258c5675f462a1070d
Sarah Clark <seclark@nextquestion.net>
2025-02-02 21:34:10 -0800

    python312Packages.langchain-groq: init at 0.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7117,6 +7117,8 @@ self: super: with self; {
 
   langchain-core = callPackage ../development/python-modules/langchain-core { };
 
+  langchain-groq = callPackage ../development/python-modules/langchain-groq { };
+
   langchain-huggingface = callPackage ../development/python-modules/langchain-huggingface { };
 
   langchain-mongodb = callPackage ../development/python-modules/langchain-mongodb { };
