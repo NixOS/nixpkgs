NixOS/nixpkgs

commit 518e9508bc69e6f23d915db4138f1c6fb665b714

treewide: migrate to fetchCargoVendor, batch 2
Cargo 1.84.0 seems to have changed the output format of cargo vendor
again, once again invalidating fetchCargoTarball FOD hashes.  It's
time to fix this once and for all, switching across the board to
fetchCargoVendor, which is not dependent on cargo vendor's output
format.

It should be possible to reproduce this diff.  To generate it, I first
ran:

	xargs sed -i 's/^\(. *\)\(cargoHash =\)/\1useFetchCargoVendor = true;\n\1cargoHash =/'

The following manually identified list of files were given as standard
input:

	pkgs/applications/audio/listenbrainz-mpd/default.nix
	pkgs/applications/audio/minidsp/default.nix
	pkgs/applications/audio/muso/default.nix
	pkgs/applications/audio/parrot/default.nix
	pkgs/applications/blockchains/electrs/default.nix
	pkgs/applications/blockchains/snarkos/default.nix
	pkgs/applications/blockchains/teos/default.nix
	pkgs/applications/blockchains/zcash/default.nix
	pkgs/applications/display-managers/greetd/default.nix
	pkgs/applications/display-managers/greetd/regreet.nix
	pkgs/applications/display-managers/greetd/tuigreet.nix
	pkgs/applications/display-managers/greetd/wlgreet.nix
	pkgs/applications/editors/emacs/elisp-packages/manual-packages/lspce/module.nix
	pkgs/applications/editors/neovim/gnvim/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/LanguageClient-neovim/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/avante-nvim/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/codesnap-nvim/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/cord-nvim/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/nvim-spectre/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/sg-nvim/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/sniprun/default.nix
	pkgs/applications/editors/vim/plugins/non-generated/vim-markdown-composer/default.nix

pkgs/tools/misc/lorri/default.nix
@@ -17,7 +17,8 @@ let
   # Also don’t forget to run `nix-build -A lorri.tests`
   version = "1.7.1";
   sha256 = "sha256-dEdKMgE4Jd8CCvtGQDZNDCYOomZAV8aR7Cmtyn8RfTo=";
-  cargoHash = "sha256-+sKxKxc2DVHn54uQa8K+CKmm0A0ym9SXgtOcfRZ6R5E=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-d1kXwb2WsF3J5AyD8wrrnGtCMYQ4P2mtiuJLMyCDPdM=";
 
 in
 (rustPlatform.buildRustPackage rec {

pkgs/applications/audio/listenbrainz-mpd/default.nix
@@ -26,7 +26,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-QBc0avci232UIxzTKlS0pjL7cCuvwAFgw6dSwdtYAtU=";
   };
 
-  cargoHash = "sha256-jnDS9tIJ387A2P9oUSYB3tXrXjwwVmQ26erIIlHBkao=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-NQXXR6b1XZDihVoRNFJLXtMNjlzOIzkc4rthwx0A7AE=";
 
   nativeBuildInputs = [
     pkg-config

pkgs/applications/audio/minidsp/default.nix
@@ -19,7 +19,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-8bKP9/byVRKj1P1MP3ZVg8yw0WaNB0BcqarCti7B8CA=";
   };
 
-  cargoHash = "sha256-GUrYEFpTo83lKuDyENaVN3VhnZ2Y/igtsbEY7kNa1os=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-JIm0XcgqXGPXlkQ1rhG5D38bQkQT9K44F71ZaCT2g8o=";
 
   cargoBuildFlags = [ "-p minidsp -p minidsp-daemon" ];
 

pkgs/applications/audio/muso/default.nix
@@ -35,7 +35,8 @@ rustPlatform.buildRustPackage rec {
     cp share/* $out/share/muso/
   '';
 
-  cargoHash = "sha256-+UVUejKCfjC6zdW315wmu7f3A5GmnoQ3rIk8SK6LIRI=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-L0ZQoz9J5Hxg98puk1RbKuybLboIoOsy5qqGnvEPi1U=";
 
   meta = with lib; {
     broken = stdenv.hostPlatform.isDarwin;

pkgs/applications/audio/parrot/default.nix
@@ -24,7 +24,8 @@ rustPlatform.buildRustPackage {
     hash = "sha256-3YTXIKj1iqCB+tN7/0v1DAaMM6aJiSxBYHO98uK8KFo=";
   };
 
-  cargoHash = "sha256-3G7NwSZaiocjgfdtmJVWfMZOHCNhC08NgolPa9AvPfE=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-be/gGKCd8/VgcjzhyMKDl5TzAuavm1rPNYBm8RLTP90=";
 
   nativeBuildInputs = [
     cmake

pkgs/applications/blockchains/electrs/default.nix
@@ -21,7 +21,8 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-L26jzAn8vwnw9kFd6ciyYS/OLEFTbN8doNKy3P8qKRE=";
   };
 
-  cargoHash = "sha256-/0XS4xF5gzEBWXS39f0FsIK1dFwmGT4McaExR/srB5Y=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-dqVnIiaGXnoKLvwQm/aiwSptKksikNGgAOu704rhDPA=";
 
   # needed for librocksdb-sys
   nativeBuildInputs = [ rustPlatform.bindgenHook ];

pkgs/applications/blockchains/snarkos/default.nix
@@ -19,7 +19,8 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256-+z9dgg5HdR+Gomug03gI1zdCU6t4SBHkl1Pxoq69wrc=";
   };
 
-  cargoHash = "sha256-qW/ZV4JqpNqqh8BYc+/d5g8junwhdZ38NhHclx+k/0M=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-riUOxmuXDP5+BPSPu5+cLBP43bZxAqvVG/k5kvThSAs=";
 
   # buildAndTestSubdir = "cli";
 

pkgs/applications/blockchains/teos/default.nix
@@ -32,7 +32,8 @@ in
     pname = "teos";
     inherit version src;
 
-    cargoHash = "sha256-U0imKEPszlBOaS6xEd3kfzy/w2SYe3EY/E1e0L+ViDk=";
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-lod5I94T4wGwXEDtvh2AyaDYM0byCfaSBP8emKV7+3M=";
 
     buildAndTestSubdir = "teos";
 
@@ -58,7 +59,8 @@ in
     pname = "teos-watchtower-plugin";
     inherit version src;
 
-    cargoHash = "sha256-3ke1qTFw/4I5dPLuPjIGp1n2C/eRfPB7A6ErMFfwUzE=";
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-lod5I94T4wGwXEDtvh2AyaDYM0byCfaSBP8emKV7+3M=";
 
     buildAndTestSubdir = "watchtower-plugin";
 

commit 8a6de3561340a340acdd17b9f33e3738f12750e0

lorri: fix useFetchCargoVendor
The script I ran to bulk migrate packages didn't account for cargoHash
being in a let, so here it just introduced an unused
useFetchCargoVendor binding rather than passing it to
buildRustPackage.

Fixes: 518e9508bc69 ("treewide: migrate to fetchCargoVendor, batch 2")

pkgs/tools/misc/lorri/default.nix
@@ -17,8 +17,7 @@ let
   # Also don’t forget to run `nix-build -A lorri.tests`
   version = "1.7.1";
   sha256 = "sha256-dEdKMgE4Jd8CCvtGQDZNDCYOomZAV8aR7Cmtyn8RfTo=";
-  useFetchCargoVendor = true;
-  cargoHash = "sha256-d1kXwb2WsF3J5AyD8wrrnGtCMYQ4P2mtiuJLMyCDPdM=";
+  cargoHash = "sha256-pRtc0cDVIBqbCbC1weFOhZP29rKAE1XdmM6HE5nJKRU=";
 
 in
 (rustPlatform.buildRustPackage rec {
@@ -38,6 +37,7 @@ in
     "doc"
   ];
 
+  useFetchCargoVendor = true;
   inherit cargoHash;
   doCheck = false;
 