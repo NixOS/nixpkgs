a74eedf0b22c5c35c3c86176bc803029bb6644df
qbisi <qbisicwate@gmail.com>
2025-02-04 19:40:22 +0800

    petsc: 3.21.4 -> 3.22.3

--- pkgs/by-name/pe/petsc/package.nix
+++ pkgs/by-name/pe/petsc/package.nix
@@ -29,11 +29,11 @@ assert petsc-withp4est -> p4est.mpiSupport;
 
 stdenv.mkDerivation rec {
   pname = "petsc";
-  version = "3.21.4";
+  version = "3.22.3";
 
   src = fetchzip {
     url = "https://web.cels.anl.gov/projects/petsc/download/release-snapshots/petsc-${version}.tar.gz";
-    hash = "sha256-l7v+ASBL9FLbBmBGTRWDwBihjwLe3uLz+GwXtn8u7e0=";
+    hash = "sha256-MPcOdHp5rMhIA9Yw/btOLcwYn4CpNZkRgPb5nh+9wko=";
   };
 
   strictDeps = true;
