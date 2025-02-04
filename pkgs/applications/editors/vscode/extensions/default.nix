d4115f2b4726162d088d7aba57ff6166fa3a23e7
Ivy Fan-Chiang <dev@ivyfanchiang.ca>
2025-02-04 14:43:12 -0500

    vscode-extensions.dendron.dendron: init at 0.124.0
    (cherry picked from commit 04c8fc297e130800b5c93e5b9415b684cea814d7)

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -1282,6 +1282,8 @@ let
         };
       };
 
+      dendron.dendron = callPackage ./dendron.dendron { };
+
       denoland.vscode-deno = buildVscodeMarketplaceExtension {
         mktplcRef = {
           name = "vscode-deno";
