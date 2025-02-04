7eeeae929033eda631d19e5115f8ec90a570a69d
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-04 18:45:26 +0000

    twilio-cli: 5.22.9 -> 5.22.10

--- pkgs/by-name/tw/twilio-cli/package.nix
+++ pkgs/by-name/tw/twilio-cli/package.nix
@@ -8,11 +8,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "twilio-cli";
-  version = "5.22.9";
+  version = "5.22.10";
 
   src = fetchzip {
     url = "https://twilio-cli-prod.s3.amazonaws.com/twilio-v${finalAttrs.version}/twilio-v${finalAttrs.version}.tar.gz";
-    hash = "sha256-drQUlu5xc9HUlVP/OHDDWNEaSxT/90yhiKYh9bq2G4E=";
+    hash = "sha256-ZesjUeAoOuNWITzM1qjWsvh2DMehyy8XJDhecylx4V4=";
   };
 
   buildInputs = [ nodejs-slim ];
