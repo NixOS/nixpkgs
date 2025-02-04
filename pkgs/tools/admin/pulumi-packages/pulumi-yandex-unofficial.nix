e8948806e828895f539420dba1b88cebdb9ad220
Ivan Trubach <mr.trubach@icloud.com>
2025-02-04 17:18:19 +0300

    pulumiPackages.pulumi-yandex-unofficial: 0.98.0 -> 0.99.1

--- pkgs/tools/admin/pulumi-packages/pulumi-yandex-unofficial.nix
+++ pkgs/tools/admin/pulumi-packages/pulumi-yandex-unofficial.nix
@@ -7,19 +7,20 @@
 mkPulumiPackage rec {
   owner = "Regrau";
   repo = "pulumi-yandex";
-  version = "0.98.0";
+  version = "0.99.1";
   rev = "v${version}";
-  hash = "sha256-Olwl4JNrJUiJaGha7ZT0Qb0+6hRKxOOy06eKMJfYf0I=";
-  vendorHash = "sha256-8mu0msSq59f5GZNo7YIGuNTYealGyEL9kwk0jCcSO68=";
+  hash = "sha256-LCWrt5TIvzXssLjV523K27LWzd+za88WLzgbLLnK+sw=";
+  vendorHash = "sha256-z9UAGX3PRInlO8v/1zgPYR8SlTnDuZfgEruBWJfVNiU=";
   cmdGen = "pulumi-tfgen-yandex";
   cmdRes = "pulumi-resource-yandex";
   extraLdflags = [
-    "-X github.com/regrau/${repo}/provider/pkg/version.Version=v${version}"
+    "-X=github.com/regrau/${repo}/provider/pkg/version.Version=v${version}"
   ];
   __darwinAllowLocalNetworking = true;
   env.GOWORK = "off";
   meta = with lib; {
     description = "Unofficial Yandex Cloud Resource Provider";
+    mainProgram = "pulumi-resource-yandex";
     homepage = "https://github.com/Regrau/pulumi-yandex";
     license = licenses.asl20;
     maintainers = with maintainers; [
@@ -27,6 +28,5 @@ mkPulumiPackage rec {
       veehaitch
       trundle
     ];
-    mainProgram = cmdRes;
   };
 }
