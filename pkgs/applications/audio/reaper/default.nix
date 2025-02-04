455e70ecfe6654a96f512f42db409f8fddad0b83
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-04 07:03:24 +0000

    reaper: 7.30 -> 7.33

--- pkgs/applications/audio/reaper/default.nix
+++ pkgs/applications/audio/reaper/default.nix
@@ -38,17 +38,17 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "reaper";
-  version = "7.30";
+  version = "7.33";
 
   src = fetchurl {
     url = url_for_platform version stdenv.hostPlatform.qemuArch;
     hash =
       if stdenv.hostPlatform.isDarwin then
-        "sha256-nPt2dWbbctRrC3+UufMMLiAikOaMB33tDfFCscJx5cA="
+        "sha256-EBPMuW2cuuw9B6nbUDQiRyugQOItWHldGPvlijW7s9Y="
       else
         {
-          x86_64-linux = "sha256-rN4SMGkoO03JCmPOKabOdOlk2nfmGDYk1tSfniPyRnQ=";
-          aarch64-linux = "sha256-kASD5099XAoy6w3K5z0y8xrSNpomTWAFOWjtHgULwAA=";
+          x86_64-linux = "sha256-C+iJO6hQib2Z5FKB2dBRD963x9ezSt8G3E3ocaY56TI=";
+          aarch64-linux = "sha256-Lbf8yY6UqtfqHWYkLRzUkasdjxyPxFoxdehJG0+0u44=";
         }
         .${stdenv.hostPlatform.system};
   };
