36131981408c94edfb5c5ad17b752bed3ac38c6b
agj <agj@users.noreply.github.com>
2025-02-04 18:46:59 -0300

    php84Extensions.decimal: better adhere to Nixpkgs standards
    Co-authored-by: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/php-packages/decimal/default.nix
+++ pkgs/development/php-packages/decimal/default.nix
@@ -11,9 +11,8 @@ buildPecl {
   pname = "decimal";
 
   version = version;
-  sha256 = "it8w8hOLYwtCZoDYhaP5k5TD/pQLtj37K2lSESF80ok=";
+  hash = "sha256-it8w8hOLYwtCZoDYhaP5k5TD/pQLtj37K2lSESF80ok=";
 
-  internalDeps = lib.optional (lib.versionOlder php.version "8.0") php.extensions.json;
   buildInputs = [ mpdecimal ];
   configureFlags = [ "--with-libmpdec-path=${mpdecimal}" ];
 
@@ -22,7 +21,6 @@ buildPecl {
     homepage = "https://php-decimal.github.io";
     changelog = "https://pecl.php.net/package-changelog.php?package=decimal&release=${version}";
     license = lib.licenses.mit;
-    broken = lib.versionOlder php.version "7.0";
     maintainers = lib.teams.php.members;
   };
 }
