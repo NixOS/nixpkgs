8d798171351b303e62f14ef3fc5277586f20fb13
Guy Chronister <guychronister@outlook.com>
2025-02-04 10:12:47 -0600

    alfaview: 9.19.0 -> 9.20.0

--- pkgs/by-name/al/alfaview/package.nix
+++ pkgs/by-name/al/alfaview/package.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "alfaview";
-  version = "9.19.0";
+  version = "9.20.0";
 
   src = fetchurl {
     url = "https://assets.alfaview.com/stable/linux/deb/${pname}_${version}.deb";
-    hash = "sha256-o0sB+JLwemUNRflp8cR0exc5QvCkpxIAVd/g4vwix6Q=";
+    hash = "sha256-CnD75PTi5pfkS+F8a3sf+J3zz5Ro/8m2Z4GINTvQV6k=";
   };
 
   nativeBuildInputs = [
