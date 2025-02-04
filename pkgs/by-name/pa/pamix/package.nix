3d382942301fa18d6fd5a58ce94f756ee6a4dc3e
Defelo <mail@defelo.de>
2025-02-04 01:57:51 +0100

    pamix: 1.6 -> 2.0

--- pkgs/by-name/pa/pamix/package.nix
+++ pkgs/by-name/pa/pamix/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   pkg-config,
   cmake,
   libpulseaudio,
@@ -12,28 +11,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pamix";
-  version = "1.6";
+  version = "2.0";
 
   src = fetchFromGitHub {
     owner = "patroclos";
     repo = "pamix";
     tag = finalAttrs.version;
-    hash = "sha256-l6NCqqrq11sYJyVshEMCU6ixC5E3lSSMc0+4ye17hLQ=";
+    hash = "sha256-7UPz6YpsnZHpW7sOJdJU2wQ5jyFPWTHxoknago0W+Ss=";
   };
-
-  patches = [
-    # ncurses-6.3 support, included in next release
-    (fetchpatch {
-      name = "ncurses-6.3-p1.patch";
-      url = "https://github.com/patroclos/PAmix/commit/3400b9c048706c572373e4617b4d5fcdb8dd2505.patch";
-      sha256 = "0rw56a844pz876ad9p8hfvn2fkd5rh29gpp47h55g08spf0vwb2z";
-    })
-    (fetchpatch {
-      name = "ncurses-6.3-p2.patch";
-      url = "https://github.com/patroclos/PAmix/commit/5ef67fc5ef6fc0dc0b48ff07ba48093881561d9c.patch";
-      sha256 = "0f8shpdv2swxdz04bkqgmkvl6c17r5mn4slzr7xd6pvw8hh51p4h";
-    })
-  ];
 
   preConfigure = ''
     substituteInPlace CMakeLists.txt --replace-fail "/etc" "$out/etc/xdg"
