d055f8d2b5f7d91fd604278e952a875a8bf4d720
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-05 14:02:52 +0000

    nushell: 0.101.0 -> 0.102.0

--- pkgs/shells/nushell/default.nix
+++ pkgs/shells/nushell/default.nix
@@ -22,7 +22,7 @@
 }:
 
 let
-  version = "0.101.0";
+  version = "0.102.0";
 in
 
 rustPlatform.buildRustPackage {
@@ -33,10 +33,10 @@ rustPlatform.buildRustPackage {
     owner = "nushell";
     repo = "nushell";
     tag = version;
-    hash = "sha256-Ptctp2ECypmSd0BHa6l09/U7wEjtLsvRSQV/ISz9+3w=";
+    hash = "sha256-ISiixFQ2m++WKX66SnvF1VZpgYJjgchRWSlo/Cp/Ybs=";
   };
 
-  cargoHash = "sha256-KOIVlF8V5bdtGIFbboTQpVSieETegA6iF7Hi6/F+2bE=";
+  cargoHash = "sha256-pgOR4yPOTOeeYTcSwdnAJi3pu4mX9LW25bwO5Pb695Y=";
 
   nativeBuildInputs =
     [ pkg-config ]
