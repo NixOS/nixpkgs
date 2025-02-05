cb0620a0712154bee55e10af8ebbd290c953d144
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-05 23:38:57 +0000

    cimg: 3.4.3 -> 3.5.2

--- pkgs/by-name/ci/cimg/package.nix
+++ pkgs/by-name/ci/cimg/package.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "cimg";
-  version = "3.4.3";
+  version = "3.5.2";
 
   src = fetchFromGitHub {
     owner = "GreycLab";
     repo = "CImg";
     tag = "v.${finalAttrs.version}";
-    hash = "sha256-Abe7mSbdj06MossUQXCKZPzzfyvJyRVeLfzINYNPv4g=";
+    hash = "sha256-7pbVx3Ro0iey0zpA9ReseAlj3sOqhvvcBPwm9wH04X0=";
   };
 
   outputs = [
