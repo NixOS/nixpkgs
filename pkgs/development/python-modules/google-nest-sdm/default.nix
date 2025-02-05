5418bdb0a48925388a2205197f0c68c9d006efa7
Fabian Affolter <fabian@affolter-engineering.ch>
2025-02-05 18:02:59 +0100

    python313Packages.google-nest-sdm: 7.0.0 -> 7.1.1 (#378507)
    https://github.com/allenporter/python-google-nest-sdm/releases/tag/7.1.0
    https://github.com/allenporter/python-google-nest-sdm/releases/tag/7.1.1

--- pkgs/development/python-modules/google-nest-sdm/default.nix
+++ pkgs/development/python-modules/google-nest-sdm/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "google-nest-sdm";
-  version = "7.0.0";
+  version = "7.1.1";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "allenporter";
     repo = "python-google-nest-sdm";
     tag = version;
-    hash = "sha256-j3L0U4dcmU35eHyrB8ZASGso8lWFwSD3PGq5fRSGOg0=";
+    hash = "sha256-hBpAwDRmqRRWW0DiG3aXy+OXZPCXdhPzAP79mXOn1kg=";
   };
 
   build-system = [ setuptools ];
