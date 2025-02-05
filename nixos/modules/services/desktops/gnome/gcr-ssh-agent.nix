e8ad4854a6d5dbefba55befa49a8d6e5cddfc5c7
Anthony Rodriguez <anthony@nezia.dev>
2025-02-06 00:18:18 +0100

    nixos/gcr-ssh-agent: init
    Co-authored-by: lilyinstarlight <lily@lily.flowers>

---
+++ nixos/modules/services/desktops/gnome/gcr-ssh-agent.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.services.gnome.gcr-ssh-agent;
+in
+{
+  meta = {
+    maintainers = lib.teams.gnome.members;
+  };
+
+  options = {
+    services.gnome.gcr-ssh-agent = {
+      enable = lib.mkEnableOption "GCR ssh-agent";
+      package = lib.mkPackageOption pkgs "GCR" {
+        default = [ "gcr_4" ];
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.packages = [ cfg.package ];
+
+    # Set SSH_AUTH_SOCK in session environment since not all DEs/display managers will use environment variables from systemd
+    environment.extraInit = ''
+      if [ -z "$SSH_AUTH_SOCK" ] && [ -n "$XDG_RUNTIME_DIR" ]; then
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+      fi
+    '';
+  };
+}
