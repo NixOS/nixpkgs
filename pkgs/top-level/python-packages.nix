19f628ac01fb93056fb5dd7717be60cd36219920
Carles Rafols <41967813+karb94@users.noreply.github.com>
2025-02-05 19:36:10 +0000

    python3Packages.opentelemetry-instrumentation-urllib3: init at 0.48b0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9841,6 +9841,8 @@ self: super: with self; {
 
   opentelemetry-instrumentation-grpc = callPackage ../development/python-modules/opentelemetry-instrumentation-grpc { };
 
+  opentelemetry-instrumentation-urllib3 = callPackage ../development/python-modules/opentelemetry-instrumentation-urllib3 { };
+
   opentelemetry-instrumentation-logging = callPackage ../development/python-modules/opentelemetry-instrumentation-logging { };
 
   opentelemetry-instrumentation-psycopg2 = callPackage ../development/python-modules/opentelemetry-instrumentation-psycopg2 { };
