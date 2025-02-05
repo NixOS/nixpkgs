867888dfcfb3ad86de1666818070b4694551ceba
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-02-05 06:33:11 +0100

    ocamlPackages.multipart_form-lwt: init at 0.6.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1246,6 +1246,8 @@ let
 
     multipart_form =  callPackage ../development/ocaml-modules/multipart_form { };
 
+    multipart_form-lwt =  callPackage ../development/ocaml-modules/multipart_form/lwt.nix { };
+
     multipart-form-data =  callPackage ../development/ocaml-modules/multipart-form-data { };
 
     mustache =  callPackage ../development/ocaml-modules/mustache { };
