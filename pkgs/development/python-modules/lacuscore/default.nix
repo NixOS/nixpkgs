ec5e69d9d5de78deea083c2676525e53e8864816
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-05 11:29:02 +0100

    python312Packages.lacuscore: 1.12.8 -> 1.12.9
    Diff: https://github.com/ail-project/LacusCore/compare/refs/tags/v1.12.8...v1.12.9
    
    Changelog: https://github.com/ail-project/LacusCore/releases/tag/v1.12.9

--- pkgs/development/python-modules/lacuscore/default.nix
+++ pkgs/development/python-modules/lacuscore/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "lacuscore";
-  version = "1.12.8";
+  version = "1.12.9";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "ail-project";
     repo = "LacusCore";
     tag = "v${version}";
-    hash = "sha256-blQhnQoNMXQhNQJ7EXawoYHxbPEgPmLltoLQTzsKwtA=";
+    hash = "sha256-kh/INvFeSDvvhzryhkSzgtTsIIh4YF6FjDxjUG0hz6E=";
   };
 
   pythonRelaxDeps = [
