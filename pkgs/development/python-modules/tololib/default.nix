3371595e1f0f1afaa472031a260fdd846a52daa3
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-05 08:59:26 +0100

    python313Packages.tololib: 1.2.0 -> 1.2.2
    Diff: https://gitlab.com/MatthiasLohr/tololib/-/compare/refs/tags/v1.2.0...v1.2.2
    
    Changelog: https://gitlab.com/MatthiasLohr/tololib/-/blob/v1.2.2/CHANGELOG.md

--- pkgs/development/python-modules/tololib/default.nix
+++ pkgs/development/python-modules/tololib/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "tololib";
-  version = "1.2.0";
+  version = "1.2.2";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "MatthiasLohr";
     repo = "tololib";
     rev = "refs/tags/v${version}";
-    hash = "sha256-CVibq5dgjjXW4Fxs3doBmUyXMMbtGfN9fLgNtuTqqiA=";
+    hash = "sha256-dfyc96VcauERv2E5I6nOIXAHbmTHiRLqS+0XH0GB5HM=";
   };
 
   build-system = [ setuptools-scm ];
