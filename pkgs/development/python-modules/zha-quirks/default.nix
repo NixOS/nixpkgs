e8ce42843e0cf8cdd2d156bab324ad1d5f58ad8d
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-02-05 19:32:42 +0100

    python313Packages.zha-quirks: 0.0.130 -> 0.0.131
    https://github.com/zigpy/zha-device-handlers/releases/tag/0.0.131

--- pkgs/development/python-modules/zha-quirks/default.nix
+++ pkgs/development/python-modules/zha-quirks/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "zha-quirks";
-  version = "0.0.130";
+  version = "0.0.131";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zha-device-handlers";
     tag = version;
-    hash = "sha256-G5q4W9Dxjhjbkt5rcYhseeaSWC73MJCo/pX/OfNlPrQ=";
+    hash = "sha256-MSTlSvZhamWX7lNzJjDGxyo9KfshZ7T1ED8axx1Aghs=";
   };
 
   postPatch = ''
