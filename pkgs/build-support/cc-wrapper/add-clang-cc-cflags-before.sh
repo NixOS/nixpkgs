6de8a094854be9c626276e5473457d8d85bb44e8
Konrad Stepniak <me@th7nder.net>
2025-02-05 13:30:54 +0100

    fix: warn about cross-compile only with non-default target

--- pkgs/build-support/cc-wrapper/add-clang-cc-cflags-before.sh
+++ pkgs/build-support/cc-wrapper/add-clang-cc-cflags-before.sh
@@ -1,14 +1,23 @@
 needsTarget=true
+targetValue=""
 
 for p in "${params[@]}"; do
     case "$p" in
-    -target | --target=*)
+    -target)
         needsTarget=false
-
-        echo "Warning: supplying the --target argument to a nix-wrapped compiler may not work correctly - cc-wrapper is currently not designed with multi-target compilers in mind. You may want to use an un-wrapped compiler instead." >&2
+        targetValue=$2
+        shift 2
+        ;;
+    --target=*)
+        needsTarget=false
+        targetValue="${p#*=}"
         ;;
     esac
 done
+
+if ! $needsTarget && [[ "$targetValue" != "@defaultTarget@" ]]; then
+    echo "Warning: supplying the --target argument to a nix-wrapped compiler may not work correctly - cc-wrapper is currently not designed with multi-target compilers in mind. You may want to use an un-wrapped compiler instead." >&2
+fi
 
 if $needsTarget && [[ $0 != *cpp ]]; then
     extraBefore+=(-target @defaultTarget@ @machineFlags@)
