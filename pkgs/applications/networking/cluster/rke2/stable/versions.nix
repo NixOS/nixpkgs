16517b39f611a079be3cc07f7c633b4fee41e7e2
Robert Rose <robert.rose@mailbox.org>
2025-02-05 10:25:40 +0100

    rke2: 1.31.4+rke2r1 -> 1.31.5+rke2r1
    https://github.com/rancher/rke2/releases/tag/v1.31.5%2Brke2r1

--- pkgs/applications/networking/cluster/rke2/stable/versions.nix
+++ pkgs/applications/networking/cluster/rke2/stable/versions.nix
@@ -1,14 +1,14 @@
 {
-  rke2Version = "1.31.4+rke2r1";
-  rke2Commit = "5142beec71f7a61804840df5b434c2fd7137ce82";
-  rke2TarballHash = "sha256-Lebi3a7kNA9IQCVVkYfUoGEeEiLScqpOx1aTCFElDvw=";
-  rke2VendorHash = "sha256-DGmu1vFNcu1O2wuEwRZRBTL/TP2lJ9ggQ2M/Ix+jknM=";
-  k8sVersion = "v1.31.4";
-  k8sImageTag = "v1.31.4-rke2r1-build20241212";
+  rke2Version = "1.31.5+rke2r1";
+  rke2Commit = "08e198bbe3f0b8d4c9b0af4d92085c06bb94aa89";
+  rke2TarballHash = "sha256-GG1GOs/kLWDCvc/+l0ymRpJzEthIyGpampCjvfnEPB8=";
+  rke2VendorHash = "sha256-xWqMidOWiLgJXp6AEITkyOieLw4yi1JMmi80YS4RNy0=";
+  k8sVersion = "v1.31.5";
+  k8sImageTag = "v1.31.5-rke2r1-build20250115";
   etcdVersion = "v3.5.16-k3s1-build20241106";
   pauseVersion = "3.6";
   ccmVersion = "v1.31.2-0.20241016053446-0955fa330f90-build20241016";
-  dockerizedVersion = "v1.31.4-rke2r1";
-  golangVersion = "go1.22.9";
+  dockerizedVersion = "v1.31.5-rke2r1";
+  golangVersion = "go1.22.10";
   eol = "2025-10-28";
 }
