1236b61f3c5266cf1fa8d8b255c971b00a02f423
sternenseemann <sternenseemann@systemli.org>
2025-02-06 14:32:45 +0100

    haskell.packages.ghc{90,810}.nothunks:don't pull in wherefrom-compat
    Upstream only requests the package for ghc >= 9.2 since (?)
    wherefrom-compat doesn't support ghc < 9.2.

--- pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix
@@ -64,6 +64,11 @@ self: super: {
   # Becomes a core package in GHC >= 9.8
   semaphore-compat = doDistribute self.semaphore-compat_1_0_0;
 
+  # Only required for ghc >= 9.2
+  nothunks = super.nothunks.override {
+    wherefrom-compat = null;
+  };
+
   # For GHC < 9.4, some packages need data-array-byte as an extra dependency
   # For GHC < 9.2, os-string is not required.
   primitive = addBuildDepends [ self.data-array-byte ] super.primitive;
