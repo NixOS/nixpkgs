814ab7ddf771c6772395a1dd83f58d14dfa5f980
Niklas Korz <niklas@niklaskorz.de>
2025-02-06 18:06:08 +0100

    cargo-lambda: refactor src

--- pkgs/by-name/ca/cargo-lambda/package.nix
+++ pkgs/by-name/ca/cargo-lambda/package.nix
@@ -17,9 +17,9 @@ rustPlatform.buildRustPackage rec {
   version = "1.6.3";
 
   src = fetchFromGitHub {
-    owner = pname;
-    repo = pname;
-    rev = "v${version}";
+    owner = "cargo-lambda";
+    repo = "cargo-lambda";
+    tag = "v${version}";
     hash = "sha256-GiV5yjlzU4iU4BJ8Fq8I9uOchVCF2UGb+WLMMr7n8pc=";
   };
 
