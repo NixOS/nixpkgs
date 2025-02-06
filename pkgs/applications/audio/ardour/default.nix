c0148636adf0ca76b1e39d88b9d9a5abf23e4891
emaryn <emaryn@users.noreply.github.com>
2025-02-06 11:07:58 +0800

    ardour: 8.10 -> 8.11

--- pkgs/applications/audio/ardour/default.nix
+++ pkgs/applications/audio/ardour/default.nix
@@ -64,14 +64,14 @@
 }:
 stdenv.mkDerivation rec {
   pname = "ardour";
-  version = "8.10";
+  version = "8.11";
 
   # We can't use `fetchFromGitea` here, as attempting to fetch release archives from git.ardour.org
   # result in an empty archive. See https://tracker.ardour.org/view.php?id=7328 for more info.
   src = fetchgit {
     url = "git://git.ardour.org/ardour/ardour.git";
     rev = version;
-    hash = "sha256-y4eNo0ukRL6v0T1XvJ46sYnsiVSdL527punnkmf/TIU=";
+    hash = "sha256-z+rIWFVua1IG4GZ8kH3quKaBbN+I7Yr62vukJZk6KAg=";
   };
 
   bundledContent = fetchzip {
