f7c72d072fd69a9267dc35376cf761d5521dd305
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-06 08:51:26 +0100

    python312Packages.habiticalib: 0.3.4 -> 0.3.5
    Diff: https://github.com/tr4nt0r/habiticalib/compare/refs/tags/v0.3.4...v0.3.5
    
    Changelog: https://github.com/tr4nt0r/habiticalib/releases/tag/v0.3.5

--- pkgs/development/python-modules/habiticalib/default.nix
+++ pkgs/development/python-modules/habiticalib/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "habiticalib";
-  version = "0.3.4";
+  version = "0.3.5";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "tr4nt0r";
     repo = "habiticalib";
     tag = "v${version}";
-    hash = "sha256-3/37pku6B+986YKkqdoBTzo70wJStaumLSWqmjyJqw0=";
+    hash = "sha256-r0wpWd6hFPwo5S0ft0srL1a74rODWDH0elkY1BN74Gs=";
   };
 
   build-system = [
