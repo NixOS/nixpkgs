8a96e70049321937a772f756e63fc85d3373d624
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-06 23:05:21 +0100

    python312Packages.aiohomeconnect: 0.12.2 -> 0.12.3
    Diff: https://github.com/MartinHjelmare/aiohomeconnect/compare/refs/tags/v0.12.2...v0.12.3
    
    Changelog: https://github.com/MartinHjelmare/aiohomeconnect/blob/refs/tags/v0.12.3/CHANGELOG.md

--- pkgs/development/python-modules/aiohomeconnect/default.nix
+++ pkgs/development/python-modules/aiohomeconnect/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "aiohomeconnect";
-  version = "0.12.2";
+  version = "0.12.3";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "MartinHjelmare";
     repo = "aiohomeconnect";
     tag = "v${version}";
-    hash = "sha256-wOtvKA1Tf7Dsmvb0qmbXH47ZTlMowGxj458+QY7RJjo=";
+    hash = "sha256-zb4ld92bqgl2rt6YD8w2tz9CEMaiLBbXNPk5bB32IcI=";
   };
 
   pythonRelaxDeps = [ "httpx" ];
