6dec9eee60daeb4d2b184dc1255ed1ae7e6e1d33
Ionut Nechita <ionut_n2001@yahoo.com>
2025-02-07 21:28:06 +0200

    wildfly: init 24.0.1
    Change-Id: I82368c8c595dbb1c745a7ba09f05bf0211e433ed
    Signed-off-by: Ionut Nechita <ionut_n2001@yahoo.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1633,6 +1633,7 @@
   ./services/web-servers/unit/default.nix
   ./services/web-servers/uwsgi.nix
   ./services/web-servers/varnish/default.nix
+  ./services/web-servers/wildfly/default.nix
   ./services/x11/clight.nix
   ./services/x11/colord.nix
   ./services/x11/desktop-managers/default.nix
