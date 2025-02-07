ff7687bdc0e1fbd7085bbcd7f8117d014d77c1fd
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-02-07 20:41:14 +0100

    python313Packages.zha: 0.0.47 -> 0.0.48
    https://github.com/zigpy/zha/releases/tag/0.0.48

--- pkgs/development/python-modules/zha/default.nix
+++ pkgs/development/python-modules/zha/default.nix
@@ -27,7 +27,7 @@
 
 buildPythonPackage rec {
   pname = "zha";
-  version = "0.0.47";
+  version = "0.0.48";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -36,7 +36,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zha";
     tag = version;
-    hash = "sha256-BDZqReJWmxodP1w3fdxUHbjY9dL+6hUQZ0YazeiERrM=";
+    hash = "sha256-qPqFmFfvCR6gk4qmZNrkC/DcdOUzyLv7KALv35R6oNw=";
   };
 
   postPatch = ''
