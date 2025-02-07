8f8f39d332ec1df988bdfda8ef3c7672cbc83a86
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-07 09:12:25 +0000

    dracula-theme: 4.0.0-unstable-2025-01-10 -> 4.0.0-unstable-2025-02-05

--- pkgs/by-name/dr/dracula-theme/package.nix
+++ pkgs/by-name/dr/dracula-theme/package.nix
@@ -8,7 +8,7 @@
 
 let
   themeName = "Dracula";
-  version = "4.0.0-unstable-2025-01-10";
+  version = "4.0.0-unstable-2025-02-05";
 in
 stdenvNoCC.mkDerivation {
   pname = "dracula-theme";
@@ -17,8 +17,8 @@ stdenvNoCC.mkDerivation {
   src = fetchFromGitHub {
     owner = "dracula";
     repo = "gtk";
-    rev = "79235b53c33f02d826d76cf991f5aa8f04ed4e76";
-    hash = "sha256-glcDK0zUSYRU3SOrjxDUI170Ky995Js24DPxmL1rSPA=";
+    rev = "c2210b503b6acf7948218ea99c6b472744b345b5";
+    hash = "sha256-b6OMY++9UdK+jb7cAr+9+Fyy9ARYcIA/27UHRSIVu0g=";
   };
 
   propagatedUserEnvPkgs = [
