4861782ba9b0b56573848e373cd423d6896ed2a4
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-08 12:22:56 +0100

    python313Packages.pytenable: 1.6.0 -> 1.7.1
    Changelog: https://github.com/tenable/pyTenable/releases/tag/1.7.1

--- pkgs/development/python-modules/pytenable/default.nix
+++ pkgs/development/python-modules/pytenable/default.nix
@@ -7,6 +7,8 @@
   gql,
   graphql-core,
   marshmallow,
+  pydantic-extra-types,
+  pydantic,
   pytest-cov-stub,
   pytest-datafiles,
   pytest-vcr,
@@ -26,16 +28,16 @@
 
 buildPythonPackage rec {
   pname = "pytenable";
-  version = "1.6.0";
+  version = "1.7.1";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.10";
 
   src = fetchFromGitHub {
     owner = "tenable";
     repo = "pyTenable";
     tag = version;
-    hash = "sha256-I6GlYPQI8qF9eyq8p4Wtkz8UEGth51ZALwA+Mu3TqhQ=";
+    hash = "sha256-IPhuxjUmj4g5UxuCZsthZytgDsFiplPt+dIsCHgfMdg=";
   };
 
   pythonRelaxDeps = [
@@ -46,12 +48,14 @@ buildPythonPackage rec {
   build-system = [ setuptools ];
 
   dependencies = [
-    defusedxml
-    marshmallow
-    python-box
     cryptography
+    defusedxml
     gql
     graphql-core
+    marshmallow
+    pydantic
+    pydantic-extra-types
+    python-box
     python-dateutil
     requests
     requests-toolbelt
