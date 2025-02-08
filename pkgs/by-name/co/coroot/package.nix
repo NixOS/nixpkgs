c179c770134f1044dd2807fd75d1b124e83f340c
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-08 17:14:38 +0000

    coroot: 1.6.8 -> 1.8.2

--- pkgs/by-name/co/coroot/package.nix
+++ pkgs/by-name/co/coroot/package.nix
@@ -11,13 +11,13 @@
 
 buildGoModule rec {
   pname = "coroot";
-  version = "1.6.8";
+  version = "1.8.2";
 
   src = fetchFromGitHub {
     owner = "coroot";
     repo = "coroot";
     rev = "v${version}";
-    hash = "sha256-ndzn0ZcuzdmCXnijUr6jq3KSFqSdF2FYd/2qi5Huk5Y=";
+    hash = "sha256-jPsaZID9H2zDPUCUSmwTkLEs+mZ+6esyHNyyW5gwQc0=";
   };
 
   vendorHash = "sha256-wyxNT8g5TUCjlxauL7NmCf4HZ91V2nD64L1L/rYH864=";
