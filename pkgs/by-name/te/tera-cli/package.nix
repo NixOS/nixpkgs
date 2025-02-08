94897a593e363330e9c1e3027570a259b7571b8a
Uwe Schlifkowitz <supertuwe@gmail.com>
2025-02-08 19:05:45 +0100

    tera-cli: 0.4.0 -> 0.5.0

--- pkgs/by-name/te/tera-cli/package.nix
+++ pkgs/by-name/te/tera-cli/package.nix
@@ -5,17 +5,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "tera-cli";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "chevdor";
     repo = "tera-cli";
     rev = "v${version}";
-    hash = "sha256-mYFvqzSnTljzRbb9W4/hY7fOO35UF31P5M49JkM58R8=";
+    hash = "sha256-TN3zkxZC0Y9lev2wmvzwyLU+t4rNwut/dQILIA7+qbw=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-7UKdnq5xmkO95Sf9YR3LqS2RoUYGbvWqRYUXNQXfkf0=";
+  cargoHash = "sha256-+qf/MlifpVXzDpADJoTqxU40wDntcPu+bW7eq6/iubk=";
 
   meta = with lib; {
     description = "Command line utility to render templates from json|toml|yaml and ENV, using the tera templating engine";
