2ff721d2080ef91078514dccfa37c128900be978
โทสฺตัล <toastal@posteo.net>
2025-02-08 15:38:28 +0700

    h2o: remove rec in favor of finalAttrs

--- pkgs/by-name/h2/h2o/package.nix
+++ pkgs/by-name/h2/h2o/package.nix
@@ -10,14 +10,14 @@
   zlib,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "h2o";
   version = "2.3.0-beta2";
 
   src = fetchFromGitHub {
     owner = "h2o";
     repo = "h2o";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     sha256 = "0lwg5sfsr7fw7cfy0hrhadgixm35b5cgcvlhwhbk89j72y1bqi6n";
   };
 
@@ -46,4 +46,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ thoughtpolice ];
     platforms = platforms.linux;
   };
-}
+})
