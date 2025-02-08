7f172099e048fd3cb0f25338f68f17fa8d5701de
melvyn <melvyn2@dnsense.pub>
2025-02-08 16:12:02 -0500

    python3Packages.xdis: remove backport patches after update to 6.1.3

--- pkgs/development/python-modules/xdis/default.nix
+++ pkgs/development/python-modules/xdis/default.nix
@@ -24,30 +24,6 @@ buildPythonPackage rec {
     hash = "sha256-fdLZg3MFMS9EIPC5EPE8DAT6K5a6l780msCfzTiZwoo=";
   };
 
-  # Backport magics for newer newer python versions
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/rocky/python-xdis/commit/fcba74a7f64c5e2879ca0779ff10f38f9229e7da.patch";
-      hash = "sha256-D7eJ97g4G6pmYL/guq0Ndf8yKTVBD2gAuUCAKwvlYbE=";
-    })
-    (fetchpatch {
-      url = "https://github.com/rocky/python-xdis/commit/b66976ff53a2c6e17a73fb7652ddd6c8054df8db.patch";
-      hash = "sha256-KO1y0nDTPmEZ+0/3Pjh+CvTdpr/p4AYZ8XdH5J+XzXo=";
-    })
-    (fetchpatch {
-      url = "https://github.com/rocky/python-xdis/commit/a9f50c0ba77cdbf4693388404c13a02796a4221a.patch";
-      hash = "sha256-gwMagKBY7d/+ohESTSl6M2IEjzABxfrddpdr58VJAk8=";
-    })
-    (fetchpatch {
-      url = "https://github.com/rocky/python-xdis/commit/d9e15acae76a413667912a10fbf8259711ed9c65.patch";
-      hash = "sha256-hpmKg+K1RiLSnmUIS8KtZRVBfvTO9bWbpsNhBFUM38o=";
-    })
-    (fetchpatch {
-      url = "https://github.com/rocky/python-xdis/commit/b412c878d0bc1b516bd01612d46d8830c36a14ad.patch";
-      hash = "sha256-W1JuIXYLO6iyjWiSnzCoXzFsedZjesq31gEPgrtjxas=";
-    })
-  ];
-
   build-system = [
     setuptools
   ];
