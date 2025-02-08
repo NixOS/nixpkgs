895e56f86e00ce5ce8665b38a2e10f5a25841d71
โทสฺตัล <toastal@posteo.net>
2025-02-08 16:31:02 +0700

    h20: 2.3.0-beta1 → 2.3.0.20250130
    > Release Policy
    >
    > As of April 2023, we have ceased to release versions. Rather, the each
    > commit to master branch is considered stable and ready for general use
    > except for the features marked as experimental.
    >
    > — https://h2o.examp1e.net/install.html
    
    Seems trunk-based version now :)

--- pkgs/by-name/h2/h2o/package.nix
+++ pkgs/by-name/h2/h2o/package.nix
@@ -5,20 +5,24 @@
   pkg-config,
   cmake,
   ninja,
+  perl,
+  brotli,
   openssl,
+  libcap,
   libuv,
+  wslay,
   zlib,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "h2o";
-  version = "2.3.0-beta2";
+  version = "2.3.0.20250130";
 
   src = fetchFromGitHub {
     owner = "h2o";
     repo = "h2o";
-    tag = "v${finalAttrs.version}";
-    sha256 = "0lwg5sfsr7fw7cfy0hrhadgixm35b5cgcvlhwhbk89j72y1bqi6n";
+    rev = "26b116e9536be8cf07036185e3edf9d721c9bac2";
+    sha256 = "sha256-WjsUUnSs3kXjAmh+V/lzL1QlxxXNCph99UsC29YAirQ=";
   };
 
   outputs = [
@@ -32,11 +36,15 @@ stdenv.mkDerivation (finalAttrs: {
     pkg-config
     cmake
     ninja
+    perl
   ];
   buildInputs = [
+    brotli
     openssl
+    libcap
     libuv
     zlib
+    wslay
   ];
 
   meta = with lib; {
