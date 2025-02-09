b86432cf5fe4736af60ca9d21afe83f5c55e6a8c
Wolfgang Walther <walther@technowledgy.de>
2025-02-09 22:09:40 +0100

    bpftools: fix build

--- pkgs/by-name/bp/bpftools/package.nix
+++ pkgs/by-name/bp/bpftools/package.nix
@@ -23,30 +23,6 @@ stdenv.mkDerivation rec {
 
   separateDebugInfo = true;
 
-  patches = [
-    # fix unknown type name '__vector128' on ppc64le
-    ./include-asm-types-for-ppc64le.patch
-    # fix build for riscv64
-    (fetchpatch {
-      # libbpf: Add missing per-arch include path
-      # https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-1-bjorn@kernel.org/
-      url = "https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-1-bjorn@kernel.org/raw/";
-      hash = "sha256-edXY/ejHW5L7rGgY5B2GmVZxUgnLdBadNhBOSAgcL7M=";
-    })
-    (fetchpatch {
-      # selftests: bpf: Add missing per-arch include path
-      # https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-2-bjorn@kernel.org/
-      url = "https://patchwork.kernel.org/project/linux-riscv/patch/20240927131355.350918-2-bjorn@kernel.org/raw/";
-      hash = "sha256-7yNWE/L/qd3vcLtJYoSyGxB3ytySlr20R0D3t5ni2Fc=";
-    })
-    (fetchpatch {
-      # tools: Override makefile ARCH variable if defined, but empty
-      # https://patchwork.kernel.org/project/linux-riscv/patch/20241127101748.165693-1-bjorn@kernel.org/
-      url = "https://patchwork.kernel.org/project/linux-riscv/patch/20241127101748.165693-1-bjorn@kernel.org/raw/";
-      hash = "sha256-y8N71Hm1XfX9g3S6PzW2m7Lxp6wQQMfQE9L0QNt8cYY=";
-    })
-  ];
-
   depsBuildBuild = [ buildPackages.stdenv.cc ];
   nativeBuildInputs = [
     python3
