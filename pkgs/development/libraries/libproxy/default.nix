7a3530c8dc478fe497369c93a713de587dc651fb
Alyssa Ross <hi@alyssa.is>
2025-02-09 18:50:17 +0100

    libproxy: fix build with introspection unavailable
    Fixes e.g. cross build for s390-linux.

--- pkgs/development/libraries/libproxy/default.nix
+++ pkgs/development/libraries/libproxy/default.nix
@@ -17,17 +17,24 @@
   stdenv,
   substituteAll,
   vala,
+  buildPackages,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libproxy";
   version = "0.5.9";
 
-  outputs = [
-    "out"
-    "dev"
-    "devdoc"
-  ];
+  outputs =
+    [
+      "out"
+      "dev"
+    ]
+    ++ lib.optionals withIntrospection [
+      "devdoc"
+    ];
 
   src = fetchFromGitHub {
     owner = "libproxy";
@@ -65,14 +72,17 @@ stdenv.mkDerivation (finalAttrs: {
       --replace-fail "requires_private: 'gobject-2.0'" "requires: 'gobject-2.0'"
   '';
 
-  nativeBuildInputs = [
-    gi-docgen
-    gobject-introspection
-    meson
-    ninja
-    pkg-config
-    vala
-  ];
+  nativeBuildInputs =
+    [
+      meson
+      ninja
+      pkg-config
+    ]
+    ++ lib.optionals withIntrospection [
+      gi-docgen
+      gobject-introspection
+      vala
+    ];
 
   buildInputs =
     [
@@ -95,6 +105,8 @@ stdenv.mkDerivation (finalAttrs: {
     [
       # Prevent installing commit hook.
       "-Drelease=true"
+      (lib.mesonBool "docs" withIntrospection)
+      (lib.mesonBool "introspection" withIntrospection)
     ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [
       "-Dconfig-gnome=false"
