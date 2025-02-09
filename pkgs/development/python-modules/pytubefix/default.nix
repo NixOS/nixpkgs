40e50b5535255f4b1c5c7f0baa6f7249e71c477b
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-09 13:29:22 +0100

    python313Packages.pytubefix: 8.12.0 -> 8.12.1
    Diff: https://github.com/JuanBindez/pytubefix/compare/refs/tags/v8.12.0...v8.12.1
    
    Changelog: https://github.com/JuanBindez/pytubefix/releases/tag/v8.12.1

--- pkgs/development/python-modules/pytubefix/default.nix
+++ pkgs/development/python-modules/pytubefix/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "pytubefix";
-  version = "8.12.0";
+  version = "8.12.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "JuanBindez";
     repo = "pytubefix";
     tag = "v${version}";
-    hash = "sha256-m27iuiQDk70yHHhmayp9558S8ZYLM0/sWyCfC4VvLsQ=";
+    hash = "sha256-PZxwF8rAPHmPpw6MKI8OVrl7CRNn9ldPnsPmHlAYahM=";
   };
 
   build-system = [ setuptools ];
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   nativeCheckInputs = [ pytestCheckHook ];
 
   disabledTestPaths = [
-    # require network access
+    # Tests require network access
     "tests/test_captions.py"
     "tests/test_cli.py"
     "tests/test_exceptions.py"
@@ -33,11 +33,18 @@ buildPythonPackage rec {
     "tests/test_streams.py"
   ];
 
+  disabledTests = [
+    "test_playlist_failed_pagination"
+    "test_playlist_pagination"
+    "test_create_mock_html_json"
+  ];
+
   pythonImportsCheck = [ "pytubefix" ];
 
   meta = {
-    homepage = "https://github.com/JuanBindez/pytubefix";
     description = "Pytube fork with additional features and fixes";
+    homepage = "https://github.com/JuanBindez/pytubefix";
+    changelog = "https://github.com/JuanBindez/pytubefix/releases/tag/v${version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ youhaveme9 ];
   };
