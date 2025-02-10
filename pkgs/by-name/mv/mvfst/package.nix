488a9cb894c1623c618845fbf2745a3ea7a15836
K900 <me@0upti.me>
2025-02-10 17:59:26 +0300

    mvfst: 2025.01.27.00 -> 2025.02.03.00
    Diff: https://github.com/facebook/mvfst/compare/refs/tags/v2025.01.27.00...v2025.02.03.00

--- pkgs/by-name/mv/mvfst/package.nix
+++ pkgs/by-name/mv/mvfst/package.nix
@@ -21,7 +21,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mvfst";
-  version = "2025.01.27.00";
+  version = "2025.02.03.00";
 
   outputs = [
     "bin"
@@ -33,7 +33,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "facebook";
     repo = "mvfst";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-8qYybElhtZyx87aZnR6r5exrLewsInS1U2m+zVPXSyw=";
+    hash = "sha256-IsBydt1T33yedlaoyKl43fB7Dsuu4RPPiJuUtwZIUGg=";
   };
 
   patches = [
