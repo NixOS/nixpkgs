cdbc358506b14af5bfba9146f538d235df3b2eca
Hendrik Sokolowski <hensoko@gssws.de>
2025-02-10 03:06:39 +0100

    python3Packages.pyforked-daapd: init at 0.1.14

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10691,6 +10691,8 @@ self: super: with self; {
 
   pyfluidsynth = callPackage ../development/python-modules/pyfluidsynth { };
 
+  pyforked-daapd = callPackage ../development/python-modules/pyforked-daapd { };
+
   pyformlang = callPackage ../development/python-modules/pyformlang { };
 
   pyfreedompro = callPackage ../development/python-modules/pyfreedompro { };
