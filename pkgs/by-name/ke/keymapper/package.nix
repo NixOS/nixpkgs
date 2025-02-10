db8270b1819f9a6eccfabb4f82f9ac83cd93b812
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-10 17:44:54 +0000

    keymapper: 4.10.0 -> 4.10.1

--- pkgs/by-name/ke/keymapper/package.nix
+++ pkgs/by-name/ke/keymapper/package.nix
@@ -17,13 +17,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "keymapper";
-  version = "4.10.0";
+  version = "4.10.1";
 
   src = fetchFromGitHub {
     owner = "houmain";
     repo = "keymapper";
     rev = finalAttrs.version;
-    hash = "sha256-F6SQMsb4juH9lXA2Hr4CeaOGAlvVssFX5pCrAkn5r3w=";
+    hash = "sha256-GgcmKZc1h4xefP9vIK2DcQdqU7YqVGeAncsIy1DH+Lw=";
   };
 
   # all the following must be in nativeBuildInputs
