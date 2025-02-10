ac5ce4015a329252c5fdaed3386c6c8a0c9c173b
maskudo <manujkarki101@gmail.com>
2025-02-10 16:30:26 +0545

    chore: formatting and styling

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13979,7 +13979,7 @@ with pkgs;
 
   ir.lv2 = callPackage ../applications/audio/ir.lv2 { };
 
-  iwe = callPackage ../by-name/iw/iwe/package.nix {};
+  iwe = callPackage ./../by-name/iw/iwe/package.nix { /* ... */ };
 
   jabcode = callPackage ../development/libraries/jabcode { };
 
