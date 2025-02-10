b849b6f3aa7610d3ac2529b828bb6262925c36cb
Sergei Trofimovich <slyich@gmail.com>
2025-02-10 09:27:08 +0000

    SDL2: 2.30.12 -> 2.32.0
    Changes: https://github.com/libsdl-org/SDL/releases/tag/release-2.32.0

--- pkgs/development/libraries/SDL2/default.nix
+++ pkgs/development/libraries/SDL2/default.nix
@@ -70,13 +70,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "SDL2";
-  version = "2.30.12";
+  version = "2.32.0";
 
   src = fetchFromGitHub {
     owner = "libsdl-org";
     repo = "SDL";
     rev = "release-${finalAttrs.version}";
-    hash = "sha256-9f7V15p8Cso1TVxcd7bEFGO3C3oIhQikyLh4pWXLaiA=";
+    hash = "sha256-BdyMlttrDwQMoz+bO6bfU3bh0xKnT8yQF6nb6OGRbHw=";
   };
   dontDisableStatic = if withStatic then 1 else 0;
   outputs = [
