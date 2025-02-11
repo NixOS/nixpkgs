bcfe8878f02b35e05e93b3683694db4bef847cca
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-02-11 17:00:18 +0100

    ubi_reader: 0.8.9 -> 0.8.10

--- pkgs/by-name/ub/ubi_reader/package.nix
+++ pkgs/by-name/ub/ubi_reader/package.nix
@@ -7,14 +7,15 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "ubi_reader";
-  version = "0.8.9";
+  version = "0.8.10";
   pyproject = true;
+  disabled = python3.pkgs.pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "onekey-sec";
     repo = "ubi_reader";
     rev = "v${version}";
-    hash = "sha256-04HwzkonPzzWfX8VE//fMoVv5ggAS+61zx2W8VEUIy4=";
+    hash = "sha256-fXJiQZ1QWUmkRM+WI8DSIsay9s1w3hKloRuCcUNwZjM=";
   };
 
   build-system = [ python3.pkgs.poetry-core ];
