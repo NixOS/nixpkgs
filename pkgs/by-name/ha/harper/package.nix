f1e517ad487091710c74818dd654d7ffdd6b14e7
Sumner Evans <sumner.evans@automattic.com>
2025-02-11 09:36:21 -0700

    harper: only build harper-ls
    From @elijah-potter:
    
    `harper-cli` is a debugging tool for us. It shouldn't be published at all.
    
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- pkgs/by-name/ha/harper/package.nix
+++ pkgs/by-name/ha/harper/package.nix
@@ -15,6 +15,7 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-8JeF1HxsP+Y+C1g3YJ0B0+JHoRFkBjz4/T8rVr2KgGw=";
   };
 
+  buildAndTestSubdir = "harper-ls";
   useFetchCargoVendor = true;
   cargoHash = "sha256-uVjDFo5mJi4Xbq0Z+XOjy5VqXqkm0a+4xu+dVnjWXCU=";
 
@@ -23,7 +24,10 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/Automattic/harper";
     changelog = "https://github.com/Automattic/harper/releases/tag/v${version}";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ pbsds ];
-    mainProgram = "harper-cli";
+    maintainers = with lib.maintainers; [
+      pbsds
+      sumnerevans
+    ];
+    mainProgram = "harper-ls";
   };
 }
