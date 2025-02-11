72f62e3e7843657da6cb85d2279415e5015cf608
binarydigitz01 <binarydigitz01@protonmail.com>
2025-02-11 18:55:04 +0530

    maintainers: add binarydigitz01

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3053,6 +3053,12 @@
     githubId = 19915050;
     name = "binarycat";
   };
+  binarydigitz01 = {
+    email = "binarydigitz01@protonmail.com";
+    github = "binarydigitz01";
+    githubId = 47600778;
+    name = "Arnav Vijaywargiya";
+  };
   binary-eater = {
     email = "sergeantsagara@protonmail.com";
     github = "Binary-Eater";
