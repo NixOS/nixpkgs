4c0774a077951769a2ddbed08206ea47f10387b0
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-11 14:20:48 +0100

    python313Packages.pyvera: refactor

--- pkgs/development/python-modules/pyvera/default.nix
+++ pkgs/development/python-modules/pyvera/default.nix
@@ -8,24 +8,27 @@
   pytest-timeout,
   responses,
   pytestCheckHook,
+  pythonOlder,
   requests,
 }:
 
 buildPythonPackage rec {
   pname = "pyvera";
   version = "0.3.16";
-  format = "pyproject";
+  pyproject = true;
+
+  disabled = pythonOlder "3.11";
 
   src = fetchFromGitHub {
     owner = "pavoni";
-    repo = pname;
+    repo = "pyvera";
     tag = version;
     hash = "sha256-WLzVOQEykST2BsVRHmcBhrsd/am0jI/f7D0PmpCTbdQ=";
   };
 
-  nativeBuildInputs = [ poetry-core ];
+  build-system = [ poetry-core ];
 
-  propagatedBuildInputs = [ requests ];
+  dependencies = [ requests ];
 
   nativeCheckInputs = [
     pytest-asyncio
@@ -40,7 +43,8 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library to control devices via the Vera hub";
     homepage = "https://github.com/pavoni/pyvera";
-    license = with licenses; [ gpl2Only ];
+    changelog = "https://github.com/maximvelichko/pyvera/releases/tag/${version}";
+    license = licenses.gpl2Only;
     maintainers = with maintainers; [ fab ];
   };
 }
