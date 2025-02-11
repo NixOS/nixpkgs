NixOS/nixpkgs

commit 63c1ea97a19f189387cc0056ef24b4c4e76a8002

ndpi: 4.10 -> 4.12

pkgs/by-name/nd/ndpi/package.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ndpi";
-  version = "4.10";
+  version = "4.12";
 
   src = fetchFromGitHub {
     owner = "ntop";
     repo = "nDPI";
     rev = "refs/tags/${finalAttrs.version}";
-    hash = "sha256-iXqvDMJsOXcg9YkqKFgInLLfH6j/HEp4bEaIl6dpVtc=";
+    hash = "sha256-jdyKvM/Tb9pRWQPxpB/UQOOKamWrTS24Ofc3M5M1Zso=";
   };
 
   nativeBuildInputs = [

commit f3b435416cb8ff45c20ea9f5e52831f4271637cf

ndpi: 4.12 -> 4.10
Downgrade ndpi to fix building ntopng, which is the only user of this
library. (Note that ndpi 4.11 doesn't exist.)

Fixes: 63c1ea97a19f189387cc0056ef24b4c4e76a8002 ("ndpi: 4.10 -> 4.12")
(A plain revert fails because of treewide changes.)

Ref. https://github.com/NixOS/nixpkgs/issues/380866

pkgs/by-name/nd/ndpi/package.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ndpi";
-  version = "4.12";
+  version = "4.10";
 
   src = fetchFromGitHub {
     owner = "ntop";
     repo = "nDPI";
     tag = finalAttrs.version;
-    hash = "sha256-jdyKvM/Tb9pRWQPxpB/UQOOKamWrTS24Ofc3M5M1Zso=";
+    hash = "sha256-iXqvDMJsOXcg9YkqKFgInLLfH6j/HEp4bEaIl6dpVtc=";
   };
 
   nativeBuildInputs = [