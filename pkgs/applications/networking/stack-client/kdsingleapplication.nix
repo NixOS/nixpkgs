9b21017bd99306b5fce43f185f00f38fc94ab30d
Tim Oudesluijs-Zelle <tim@oudesluijszelle.nl>
2025-02-12 00:03:23 +0100

    stack-client: format nix files with nixfmt

--- pkgs/applications/networking/stack-client/kdsingleapplication.nix
+++ pkgs/applications/networking/stack-client/kdsingleapplication.nix
@@ -1,10 +1,4 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, cmake
-, extra-cmake-modules
-, qt6
-}:
+{ lib, stdenv, fetchFromGitHub, cmake, extra-cmake-modules, qt6 }:
 
 stdenv.mkDerivation rec {
   pname = "kdsingleapplication-qt6";
@@ -17,20 +11,12 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-Ymm+qOZMWULg7u5xEpGzcAfIrbWBQ3jsndnFSnh6/PA=";
   };
 
-nativeBuildInputs = [
-cmake
-extra-cmake-modules
-qt6.wrapQtAppsHook
-];
+  nativeBuildInputs = [ cmake extra-cmake-modules qt6.wrapQtAppsHook ];
 
-buildInputs = [
-qt6.qtbase
-];
+  buildInputs = [ qt6.qtbase ];
 
-cmakeFlags = [
-"-DKDSingleApplication_QT6=ON"
-"-DKDSingleApplication_EXAMPLES=OFF"
-];
+  cmakeFlags =
+    [ "-DKDSingleApplication_QT6=ON" "-DKDSingleApplication_EXAMPLES=OFF" ];
 
   meta = with lib; {
     description = "Single instance application library for Qt6";
