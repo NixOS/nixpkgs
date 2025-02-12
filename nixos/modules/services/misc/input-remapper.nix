999c0796d6a595b0d7be0180691613599924a6a3
dish <pyrox@pyrox.dev>
2025-02-12 11:17:16 -0500

    nixos/input-remapper: fix documentation link formatting

--- nixos/modules/services/misc/input-remapper.nix
+++ nixos/modules/services/misc/input-remapper.nix
@@ -12,7 +12,7 @@ in
     services.input-remapper = {
       enable = lib.mkEnableOption "input-remapper, an easy to use tool to change the mapping of your input device buttons";
       package = lib.mkPackageOption pkgs "input-remapper" { };
-      enableUdevRules = lib.mkEnableOption "udev rules added by input-remapper to handle hotplugged devices. Currently disabled by default due to https://github.com/sezanzeb/input-remapper/issues/140";
+      enableUdevRules = lib.mkEnableOption "udev rules added by input-remapper to handle hotplugged devices. Currently disabled by default due to <https://github.com/sezanzeb/input-remapper/issues/140>";
       serviceWantedBy = lib.mkOption {
         default = [ "graphical.target" ];
         example = [ "multi-user.target" ];
