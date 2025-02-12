321eb37d4f5e0f580bf211cb0ddcface6bfa78d3
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-12 23:48:19 +0000

    python312Packages.google-cloud-vpc-access: 1.12.0 -> 1.13.0

--- pkgs/development/python-modules/google-cloud-vpc-access/default.nix
+++ pkgs/development/python-modules/google-cloud-vpc-access/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-vpc-access";
-  version = "1.12.0";
+  version = "1.13.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "google_cloud_vpc_access";
     inherit version;
-    hash = "sha256-SmtqD6b5VnHMrL8kmWeFZZTt/WMRzMEc4VxX9FYmUiE=";
+    hash = "sha256-jMZtqtbNtocz6NeUJupU+Gkk2L48S5FfrvRrQ5/SZx8=";
   };
 
   build-system = [ setuptools ];
