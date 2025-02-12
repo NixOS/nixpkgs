a4b30e8f0c4d3e61adaf2ed4652807b49d6c65c1
Tim Keller <tjkeller.xyz>
2025-02-11 19:22:16 -0600

    add suggested changes

--- pkgs/by-name/pa/pavolctld/package.nix
+++ pkgs/by-name/pa/pavolctld/package.nix
@@ -1,28 +1,30 @@
 {
   lib,
-  fetchzip,
-  libpulseaudio,
   stdenv,
+  fetchgit,
+  libpulseaudio,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "pavolctld";
   version = "1.0.1";
 
-  src = fetchzip {
-    url = "https://git.tjkeller.xyz/${pname}/snapshot/${pname}-${version}.tar.xz";
-    sha256 = "sha256-nmN8bQKQNGpCGvHQnZK9YQgB80SxRSds0V9Y7h/Guh8=";
+  src = fetchgit {
+    url = "https://git.tjkeller.xyz/pavolctld";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-nmN8bQKQNGpCGvHQnZK9YQgB80SxRSds0V9Y7h/Guh8=";
   };
 
   buildInputs = [ libpulseaudio ];
 
   installFlags = [ "PREFIX=$(out)" ];
 
-  meta = with lib; {
-    description = "A minimal volume control/monitoring daemon for PulseAudio and PipeWire";
+  meta = {
+    description = "Minimal volume control/monitoring daemon for PulseAudio and PipeWire";
     homepage = "https://tjkeller.xyz/projects/pavolctld";
-    platforms = platforms.linux;
-    license = licenses.mit;
-    maintainers = [ maintainers.tjkeller ];
+    platforms = lib.platforms.linux;
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.tjkeller ];
+    mainProgram = "pavolctld";
   };
-}
+})
