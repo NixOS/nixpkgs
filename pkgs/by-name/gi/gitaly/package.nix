d63e1f3f4d41644fa8dac1031403b11142363d82
Leona Maroni <dev@leona.is>
2025-02-12 13:08:35 +0100

    gitlab: 17.8.1 -> 17.8.2
    https://about.gitlab.com/releases/2025/02/12/patch-release-gitlab-17-8-2-released/

--- pkgs/by-name/gi/gitaly/package.nix
+++ pkgs/by-name/gi/gitaly/package.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "17.8.1";
+  version = "17.8.2";
   package_version = "v${lib.versions.major version}";
   gitaly_package = "gitlab.com/gitlab-org/gitaly/${package_version}";
 
@@ -21,7 +21,7 @@ let
       owner = "gitlab-org";
       repo = "gitaly";
       rev = "v${version}";
-      hash = "sha256-ahYvfA1PvB/OP3swTABH++pZubejsb3Cohy+Z5fcAo4=";
+      hash = "sha256-/bUXtdczGbbLQWvhDOM9Un9SowIRImct9LNQ0gD4hHw=";
     };
 
     vendorHash = "sha256-rR3dsKUoIVDj0NviN8p8g3mSAW8PTNChBacqd23yDf8=";
