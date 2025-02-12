359808da25d045594f2e584eefc096ace6049d94
Donovan Glover <donovan@dglover.co>
2025-02-12 18:11:36 -0500

    kakoune-cr: drop dependency on crystal 1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3823,7 +3823,7 @@ with pkgs;
     inherit (darwin.apple_sdk.frameworks) CoreServices Security SystemConfiguration;
   };
 
-  kakoune-cr = callPackage ../tools/misc/kakoune-cr { crystal = crystal_1_2; };
+  kakoune-cr = callPackage ../tools/misc/kakoune-cr { };
 
   kbs2 = callPackage ../tools/security/kbs2 {
     inherit (darwin.apple_sdk.frameworks) AppKit SystemConfiguration;
