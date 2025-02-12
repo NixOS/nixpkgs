226cb34a82799a2d6cf541a287bd9a8e44797612
Audrey Dutcher <audrey@rhelmot.io>
2025-02-12 15:53:25 -0700

    jq: fix compilation on OpenBSD/clang19

--- pkgs/by-name/jq/jq/package.nix
+++ pkgs/by-name/jq/jq/package.nix
@@ -63,7 +63,9 @@ stdenv.mkDerivation rec {
     ]
     ++ lib.optional (!onigurumaSupport) "--with-oniguruma=no"
     # jq is linked to libjq:
-    ++ lib.optional (!stdenv.hostPlatform.isDarwin) "LDFLAGS=-Wl,-rpath,\\\${libdir}";
+    ++ lib.optional (!stdenv.hostPlatform.isDarwin) "LDFLAGS=-Wl,-rpath,\\\${libdir}"
+    # https://github.com/jqlang/jq/issues/3252
+    ++ lib.optional stdenv.hostPlatform.isOpenBSD "CFLAGS=-D_BSD_SOURCE=1";
 
   # jq binary includes the whole `configureFlags` in:
   # https://github.com/jqlang/jq/commit/583e4a27188a2db097dd043dd203b9c106bba100
