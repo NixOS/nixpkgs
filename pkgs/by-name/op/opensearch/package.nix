be9aad24abda12bf99502b619712cfcf4bc3b116
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-12 13:08:50 +0000

    opensearch: 2.18.0 -> 2.19.0

--- pkgs/by-name/op/opensearch/package.nix
+++ pkgs/by-name/op/opensearch/package.nix
@@ -12,11 +12,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opensearch";
-  version = "2.18.0";
+  version = "2.19.0";
 
   src = fetchurl {
     url = "https://artifacts.opensearch.org/releases/bundle/opensearch/${finalAttrs.version}/opensearch-${finalAttrs.version}-linux-x64.tar.gz";
-    hash = "sha256-rjy0EHsuDNu5uYu05fLwGbc2to6ZVELHGORZ05/wHfE=";
+    hash = "sha256-RVuQAYLvKnGT1EPZaq6lX1NOwPkbG61tLqzbrqYB8W4=";
   };
 
   nativeBuildInputs = [
