ca22d0a6d5edaefe75d7bbc94e2f0d21824a3c96
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-12 09:21:45 +0000

    python312Packages.type-infer: 0.0.20 -> 0.0.21

--- pkgs/development/python-modules/type-infer/default.nix
+++ pkgs/development/python-modules/type-infer/default.nix
@@ -28,7 +28,7 @@ let
     ];
   };
 
-  version = "0.0.20";
+  version = "0.0.21";
   tag = "v${version}";
 in
 buildPythonPackage rec {
@@ -42,7 +42,7 @@ buildPythonPackage rec {
     owner = "mindsdb";
     repo = "type_infer";
     inherit tag;
-    hash = "sha256-2Y+NPwUnQMj0oXoCMfUOG40lqduy9GTcqxfyuFDOkHc=";
+    hash = "sha256-Q5f4WihaT88R+x4jMUuRNBvWglkGdS5oi+o9jOk+tSE=";
   };
 
   pythonRelaxDeps = [
