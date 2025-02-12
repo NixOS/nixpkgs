6eaa839c3a220dcf15a3d8d2deda1c5e799de1da
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-12 06:55:01 +0000

    kubo: 0.33.0 -> 0.33.1

--- pkgs/by-name/ku/kubo/package.nix
+++ pkgs/by-name/ku/kubo/package.nix
@@ -8,7 +8,7 @@
 
 buildGoModule rec {
   pname = "kubo";
-  version = "0.33.0"; # When updating, also check if the repo version changed and adjust repoVersion below
+  version = "0.33.1"; # When updating, also check if the repo version changed and adjust repoVersion below
   rev = "v${version}";
 
   passthru.repoVersion = "16"; # Also update kubo-migrator when changing the repo version
@@ -16,7 +16,7 @@ buildGoModule rec {
   # Kubo makes changes to its source tarball that don't match the git source.
   src = fetchurl {
     url = "https://github.com/ipfs/kubo/releases/download/${rev}/kubo-source.tar.gz";
-    hash = "sha256-TcjxdeVnGdk+pA1MizIZ3UBKy6PWHI1h66/PNWrY2DA=";
+    hash = "sha256-ybsQ25tuRPCp5XKZyxpq/a0x/h69P41+SLukro1xuMc=";
   };
 
   # tarball contains multiple files/directories
