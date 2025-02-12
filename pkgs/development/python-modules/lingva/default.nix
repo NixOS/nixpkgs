fbd12fb18b9098e2d29def26fe9e0e36d51abac0
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-12 08:40:02 +0100

    python313Packages.lingva: 5.0.4 -> 5.0.5
    Diff: https://github.com/vacanza/lingva/compare/refs/tags/v5.0.4...v5.0.5
    
    Changelog: https://github.com/vacanza/lingva/blob/5.0.5/changes.rst

--- pkgs/development/python-modules/lingva/default.nix
+++ pkgs/development/python-modules/lingva/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "lingva";
-  version = "5.0.4";
+  version = "5.0.5";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "vacanza";
     repo = "lingva";
     tag = "v${version}";
-    hash = "sha256-2h3J+pvXRmjD7noMA7Cyu5Tf/9R8Akv08A7xJMLVD08=";
+    hash = "sha256-zKEGRLaqQSqbOP4ZAidIxMgGQbDIC9pAGfjWqoQTouc=";
   };
 
   build-system = [ setuptools ];
