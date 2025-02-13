8895b89d29ce27dd88b166ec1b851ab7b85f0459
miranda <miranda@pulusound.fi>
2025-02-13 03:11:40 +0200

    mpd: use bonjour for zeroconf on darwin

--- pkgs/servers/mpd/default.nix
+++ pkgs/servers/mpd/default.nix
@@ -264,7 +264,9 @@ let
         ]
         ++ map (x: "-D${x}=enabled") features_
         ++ map (x: "-D${x}=disabled") (lib.subtractLists features_ knownFeatures)
-        ++ lib.optional (builtins.elem "zeroconf" features_) "-Dzeroconf=avahi"
+        ++ lib.optional (builtins.elem "zeroconf" features_) (
+          "-Dzeroconf=" + (if stdenv.hostPlatform.isDarwin then "bonjour" else "avahi")
+        )
         ++ lib.optional (builtins.elem "systemd" features_) "-Dsystemd_system_unit_dir=etc/systemd/system"
         ++ lib.optional (builtins.elem "qobuz" features_) "-Dnlohmann_json=enabled";
 
