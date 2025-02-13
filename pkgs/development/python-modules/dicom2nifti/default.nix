14c2350eddc4be0c1def19d1100b006f4840210c
osbm <osmanfbayram@gmail.com>
2025-02-14 01:42:17 +0300

    python312Packages.dicom2nifti: fix build

--- pkgs/development/python-modules/dicom2nifti/default.nix
+++ pkgs/development/python-modules/dicom2nifti/default.nix
@@ -1,25 +1,30 @@
 {
-  lib,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
-  pytestCheckHook,
+  fetchpatch,
+  lib,
+
+  # build-system
+  setuptools,
+
+  # dependencies
   gdcm,
   nibabel,
   numpy,
+  pillow,
   pydicom,
-  pylibjpeg,
-  pylibjpeg-libjpeg,
   scipy,
-  setuptools,
+
+  # tests
+  pylibjpeg-libjpeg,
+  pylibjpeg,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "dicom2nifti";
   version = "2.5.1";
   pyproject = true;
-
-  disabled = pythonOlder "3.6";
 
   # no tests in PyPI dist
   src = fetchFromGitHub {
@@ -29,14 +34,15 @@ buildPythonPackage rec {
     hash = "sha256-lPaBKqYO8B138fCgeKH6vpwGQhN3JCOnDj5PgaYfRPA=";
   };
 
-  build-system = [ setuptools ];
-
-  propagatedBuildInputs = [
-    gdcm
-    nibabel
-    numpy
-    pydicom
-    scipy
+  patches = [
+    # Hotfix for the OverflowError in tests and package usage.
+    # TODO(@osbm): Remove this patch when the upstream is fixed.
+    # https://github.com/icometrix/dicom2nifti/pull/152
+    (fetchpatch {
+      url = "https://github.com/icometrix/dicom2nifti/pull/152/commits/dac9ff492bec6d13157e5712d774ec1ea349abff.patch";
+      name = "fix-integer-scaling.patch";
+      sha256 = "sha256-57t1a55sBasZzR64kUOqKzh+JiUwGWm8xMy8MN6FiaQ=";
+    })
   ];
 
   postPatch = ''
@@ -44,19 +50,33 @@ buildPythonPackage rec {
     substituteInPlace tests/test_ge.py --replace-fail "import convert_generic" "import dicom2nifti.convert_generic as convert_generic"
   '';
 
-  nativeCheckInputs = [
-    pytestCheckHook
-    pylibjpeg
-    pylibjpeg-libjpeg
+  build-system = [ setuptools ];
+
+  dependencies = [
+    gdcm
+    nibabel
+    numpy
+    pydicom
+    scipy
   ];
 
   pythonImportsCheck = [ "dicom2nifti" ];
 
-  meta = with lib; {
+  nativeCheckInputs = [
+    pillow
+    pylibjpeg
+    pylibjpeg-libjpeg
+    pytestCheckHook
+  ];
+
+  meta = {
     homepage = "https://github.com/icometrix/dicom2nifti";
     description = "Library for converting dicom files to nifti";
     mainProgram = "dicom2nifti";
-    license = licenses.mit;
-    maintainers = with maintainers; [ bcdarwin ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      bcdarwin
+      osbm
+    ];
   };
 }
