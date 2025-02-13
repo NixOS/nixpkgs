2a578fd78d5efd3c4be46fe2a5d6d305281bb830
Gaetan Lepage <gaetan@glepage.com>
2025-02-13 15:10:52 +0100

    python312Packages.coffea: skip tests crashing on aarch64-linux

--- pkgs/development/python-modules/coffea/default.nix
+++ pkgs/development/python-modules/coffea/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  stdenv,
   buildPythonPackage,
   fetchFromGitHub,
 
@@ -36,8 +37,8 @@
   # checks
   distributed,
   pyinstrument,
-  pytestCheckHook,
   pytest-xdist,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
@@ -91,30 +92,46 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     distributed
     pyinstrument
-    pytestCheckHook
     pytest-xdist
+    pytestCheckHook
   ];
 
   pythonImportsCheck = [ "coffea" ];
 
-  disabledTests = [
-    # Requires internet access
-    # https://github.com/CoffeaTeam/coffea/issues/1094
-    "test_lumimask"
+  disabledTests =
+    [
+      # Requires internet access
+      # https://github.com/CoffeaTeam/coffea/issues/1094
+      "test_lumimask"
 
-    # Flaky: FileNotFoundError: [Errno 2] No such file or directory
-    # https://github.com/scikit-hep/coffea/issues/1246
-    "test_packed_selection_cutflow_dak" # cutflow.npz
-    "test_packed_selection_nminusone_dak" # nminusone.npz
+      # Flaky: FileNotFoundError: [Errno 2] No such file or directory
+      # https://github.com/scikit-hep/coffea/issues/1246
+      "test_packed_selection_cutflow_dak" # cutflow.npz
+      "test_packed_selection_nminusone_dak" # nminusone.npz
 
-    # AssertionError: bug in Awkward Array: attempt to convert TypeTracerArray into a concrete array
-    "test_apply_to_fileset"
-    "test_lorentz_behavior"
+      # AssertionError: bug in Awkward Array: attempt to convert TypeTracerArray into a concrete array
+      "test_apply_to_fileset"
+      "test_lorentz_behavior"
 
-    # ValueError: The array to mask was deleted before it could be masked.
-    # If you want to construct this mask, you must either keep the array alive or use 'ak.mask' explicitly.
-    "test_read_nanomc"
-  ];
+      # ValueError: The array to mask was deleted before it could be masked.
+      # If you want to construct this mask, you must either keep the array alive or use 'ak.mask' explicitly.
+      "test_read_nanomc"
+    ]
+    ++ lib.optionals (stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64) [
+      # Fatal Python error: Segmentation fault
+      # coffea/nanoevents/transforms.py", line 287 in index_range
+      "test_KaonParent_to_PionDaughters_Loop"
+      "test_MCRecoAssociations"
+      "test_MC_daughters"
+      "test_MC_parents"
+      "test_field_is_present"
+
+      # Fatal Python error: Segmentation fault
+      # File "/build/source/tests/test_lumi_tools.py", line 37 in test_lumidata
+      "test_lumidata"
+      # coffea/lumi_tools/lumi_tools.py", line 113 in get_lumi
+      "test_lumilist"
+    ];
 
   __darwinAllowLocalNetworking = true;
 
