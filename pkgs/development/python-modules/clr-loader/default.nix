NixOS/nixpkgs

commit b573e126dbfd2f7d79e15ab855329d4180434e39

python3Packages.clr-loader: 0.2.6 -> 0.2.7.post0

pkgs/development/python-modules/clr-loader/default.nix
@@ -13,11 +13,11 @@
 
 let
   pname = "clr-loader";
-  version = "0.2.6";
+  version = "0.2.7.post0";
   src = fetchPypi {
     pname = "clr_loader";
     inherit version;
-    hash = "sha256-AZNIrmtqg8ekBtFFN8J3zs96OlOyY+w0LIHe1YRaZ+4=";
+    hash = "sha256-t6iz+PuxvLu2OC2IfiHRdC1PELXqIJ5K2VVo/pfhx8Y=";
   };
   patches = [ ./dotnet-8-upgrade.patch ];
 

commit 520e25b477094d28c70d58444a4b589abd959d67

python3Packages.clr-loader: fix build
  NuGet.targets(174,5): error : '0.2.7.post0' is not a valid version string. (Parameter 'value')

Fixes: b573e126dbfd2f7d79e15ab855329d4180434e39

pkgs/development/python-modules/clr-loader/default.nix
@@ -20,6 +20,11 @@ let
     hash = "sha256-t6iz+PuxvLu2OC2IfiHRdC1PELXqIJ5K2VVo/pfhx8Y=";
   };
   patches = [ ./dotnet-8-upgrade.patch ];
+  # This stops msbuild from picking up $version from the environment
+  postPatch = ''
+    echo '<Project><PropertyGroup><Version/></PropertyGroup></Project>' > \
+      Directory.Build.props
+  '';
 
   # This buildDotnetModule is used only to get nuget sources, the actual
   # build is done in `buildPythonPackage` below.
@@ -29,6 +34,7 @@ let
       version
       src
       patches
+      postPatch
       ;
     projectFile = [
       "netfx_loader/ClrLoader.csproj"
@@ -44,6 +50,7 @@ buildPythonPackage {
     version
     src
     patches
+    postPatch
     ;
 
   format = "pyproject";