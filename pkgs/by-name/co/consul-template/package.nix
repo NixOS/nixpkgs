7d032875e7e771c02424a3f778b3bb4f6b4db1af
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-14 12:55:48 +0000

    consul-template: 0.39.1 -> 0.40.0

--- pkgs/by-name/co/consul-template/package.nix
+++ pkgs/by-name/co/consul-template/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule rec {
   pname = "consul-template";
-  version = "0.39.1";
+  version = "0.40.0";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = "consul-template";
     rev = "v${version}";
-    hash = "sha256-VaNAy6bB59QRf29Sgprv7HtVfjvCNic7r31gU2e6prQ=";
+    hash = "sha256-iqBcY7FYCO4qqQRAHxA2sxTnyL5c0vLVWKuhqvX0oJE=";
   };
 
-  vendorHash = "sha256-OTpon/UIDbH5zbcIyAWqKgTTMNGxytgcMlgaiHOBmeo=";
+  vendorHash = "sha256-Slp+x+cYYIC/XgICb9qDdqq1gTDxLNwXyClaWZt1fs8=";
 
   # consul-template tests depend on vault and consul services running to
   # execute tests so we skip them here
