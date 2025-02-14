3be5373d24512f2e9afadbfd75d476259e1c2ec0
nicoo <nicoo@mur.at>
2025-02-14 18:41:46 +0000

    ansible: tidy-up using `pythonRelaxBuildDepsHook`

--- pkgs/development/python-modules/ansible/core.nix
+++ pkgs/development/python-modules/ansible/core.nix
@@ -48,15 +48,6 @@ buildPythonPackage rec {
       --replace "[python," "["
 
     patchShebangs --build packaging/cli-doc/build.py
-
-    SETUPTOOLS_PATTERN='"setuptools[0-9 <>=.,]+"'
-    PYPROJECT=$(cat pyproject.toml)
-    if [[ "$PYPROJECT" =~ $SETUPTOOLS_PATTERN ]]; then
-      echo "setuptools replace: ''${BASH_REMATCH[0]}"
-      echo "''${PYPROJECT//''${BASH_REMATCH[0]}/'"setuptools"'}" > pyproject.toml
-    else
-      exit 2
-    fi
   '';
 
   nativeBuildInputs = [
@@ -89,6 +80,8 @@ buildPythonPackage rec {
     xmltodict
   ] ++ lib.optionals windowsSupport [ pywinrm ];
 
+  __structuredAttrs = true;  # required for pyRelaxBuildDeps
+  pythonRelaxBuildDeps = [ "setuptools" ];
   pythonRelaxDeps = [ "resolvelib" ];
 
   postInstall = ''
