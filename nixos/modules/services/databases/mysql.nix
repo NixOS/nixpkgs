72982b3f33fba5aa2b680b3a1424317008fd9d2a
6543 <6543@obermui.de>
2025-02-14 13:28:28 +0100

    services.mysql: on create initial databases add savety statement 'IF NOT EXISTS' for edgecases

--- nixos/modules/services/databases/mysql.nix
+++ nixos/modules/services/databases/mysql.nix
@@ -467,7 +467,7 @@ in
                 # Create initial databases
                 if ! test -e "${cfg.dataDir}/${database.name}"; then
                     echo "Creating initial database: ${database.name}"
-                    ( echo 'create database `${database.name}`;'
+                    ( echo 'CREATE DATABASE IF NOT EXISTS `${database.name}`;'
 
                       ${lib.optionalString (database.schema != null) ''
                         echo 'use `${database.name}`;'
