7e8cf0e3fdaf4fdd49cbcbf1742e1c96e8c0d53a
Mikael Voss <mvs@nyantec.com>
2025-02-14 12:27:26 +0100

    lpcnetfreedv: unstable-2022-08-22 -> 0.5-unstable-2025-01-19

--- pkgs/by-name/lp/lpcnetfreedv/package.nix
+++ pkgs/by-name/lp/lpcnetfreedv/package.nix
@@ -16,13 +16,13 @@ let
 in
 stdenv.mkDerivation {
   pname = "lpcnetfreedv";
-  version = "unstable-2022-08-22";
+  version = "0.5-unstable-2025-01-19";
 
   src = fetchFromGitHub {
     owner = "drowe67";
     repo = "LPCNet";
-    rev = "67a6eb74d0c07faddcdce199856862cc45779d25";
-    sha256 = "sha256-eHYZoDgoZBuuLvQn9X7H/zmK5onOAniOgY1/8RVn8gk=";
+    rev = "c8e51ac5e2fe674849cb53e7da44689b572cc246";
+    sha256 = "sha256-0Knoym+deTuFAyJrrD55MijVh6DlhJp3lss66BJUHiA=";
   };
 
   nativeBuildInputs = [ cmake ];
