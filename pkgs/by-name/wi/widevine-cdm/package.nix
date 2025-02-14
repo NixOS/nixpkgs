db9bc910a059df8f7effbf1a59e4124c70d1daed
Tristan Ross <tristan.ross@midstall.com>
2025-02-14 12:55:22 -0800

    widevine-cdm: fix eval outside unsupported platforms

--- pkgs/by-name/wi/widevine-cdm/package.nix
+++ pkgs/by-name/wi/widevine-cdm/package.nix
@@ -1,3 +1,13 @@
-{ stdenv, callPackage }:
+{
+  lib,
+  stdenv,
+  callPackage,
+}:
 
-callPackage (./. + "/${stdenv.hostPlatform.system}.nix") { inherit stdenv; }
+let
+  targets = lib.genAttrs [
+    "aarch64-linux"
+    "x86_64-linux"
+  ] (name: ./. + "/${name}.nix");
+in
+callPackage (targets."${stdenv.hostPlatform.system}" or targets.x86_64-linux) { inherit stdenv; }
