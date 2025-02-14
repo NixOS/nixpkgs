8a3be9972bdfe7813e16b49b37c017ade1400d5b
Mikael Voss <mvs@nyantec.com>
2025-02-14 12:57:10 +0100

    flex-ndax: remove use of with lib;

--- pkgs/by-name/fl/flex-ndax/package.nix
+++ pkgs/by-name/fl/flex-ndax/package.nix
@@ -21,12 +21,12 @@ buildGoModule rec {
 
   vendorHash = "sha256-05LWJm4MoJqjJaFrBZvutKlqSTGl4dSp433AfHHO6LU=";
 
-  meta = with lib; {
+  meta = {
     broken = stdenv.hostPlatform.isDarwin;
     homepage = "https://github.com/kc2g-flex-tools/nDAX";
     description = "FlexRadio digital audio transport (DAX) connector for PulseAudio";
-    license = licenses.mit;
-    maintainers = with maintainers; [ mvs ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ mvs ];
     mainProgram = "nDAX";
   };
 }
