27eed856469ce9eded99ac28e1468b35c7f7dc2f
nagymathev <nagymathev@gmail.com>
2025-02-15 23:19:27 +0100

    run through nixfmt-rfc-style

--- pkgs/by-name/ft/ftb-app/package.nix
+++ pkgs/by-name/ft/ftb-app/package.nix
@@ -2,8 +2,9 @@
   lib,
   fetchurl,
   appimageTools,
-  makeDesktopItem,
-}: let
+  nix-update-script,
+}:
+let
   pname = "ftb-app";
   version = "1.27.2";
 
@@ -11,33 +12,33 @@
     url = "https://piston.feed-the-beast.com/app/ftb-app-linux-${version}-x86_64.AppImage";
     hash = "sha256-35GEI1OBvVkUvHvQAzzGz8ux9h+5W3acH0Wr5VkqyBw=";
   };
-  
+
   appimageContents = appimageTools.extractType2 { inherit pname src version; };
 in
-  appimageTools.wrapType2 {
-    inherit pname src version;
-    
-    extraInstallCommands = ''
-      for size in 16x16 32x32 48x48 64x64 128x128 256x256 512x512; do
-        install -Dm644 ${appimageContents}/usr/share/icons/hicolor/$size/apps/ftb-app.png \
-          $out/share/icons/hicolor/$size/apps/ftb-app.png
-      done
-      
-      install -Dm644 ${appimageContents}/ftb-app.desktop \
-        $out/share/applications/ftb-app.desktop
-      substituteInPlace $out/share/applications/ftb-app.desktop \
-        --replace-fail 'Exec=AppRun' 'Exec=${placeholder "out"}/bin/ftb-app'
-    '';
+appimageTools.wrapType2 {
+  inherit pname src version;
 
-    passthru.updateScript = nix-update-script { };
-    
-    meta = {
-      description = "Feed the Beast desktop app";
-      homepage = "https://www.feed-the-beast.com/ftb-app";
-      changelog = "https://www.feed-the-beast.com/ftb-app/changes#${version}";
-      license = lib.licenses.lgpl21Only;
-      maintainers = with lib.maintainers; [ nagymathev ];
-      mainProgram = "ftb-app";
-      platforms = lib.platforms.linux;
-    };
-  }
+  extraInstallCommands = ''
+    for size in 16x16 32x32 48x48 64x64 128x128 256x256 512x512; do
+      install -Dm644 ${appimageContents}/usr/share/icons/hicolor/$size/apps/ftb-app.png \
+        $out/share/icons/hicolor/$size/apps/ftb-app.png
+    done
+
+    install -Dm644 ${appimageContents}/ftb-app.desktop \
+      $out/share/applications/ftb-app.desktop
+    substituteInPlace $out/share/applications/ftb-app.desktop \
+      --replace-fail 'Exec=AppRun' 'Exec=${placeholder "out"}/bin/ftb-app'
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Feed the Beast desktop app";
+    homepage = "https://www.feed-the-beast.com/ftb-app";
+    changelog = "https://www.feed-the-beast.com/ftb-app/changes#${version}";
+    license = lib.licenses.lgpl21Only;
+    maintainers = with lib.maintainers; [ nagymathev ];
+    mainProgram = "ftb-app";
+    platforms = lib.platforms.linux;
+  };
+}
