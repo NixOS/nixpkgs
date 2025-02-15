4be1d74fdcfb942ae328d22aa1559b6bebf603c4
nagymathev <49335802+nagymathev@users.noreply.github.com>
2025-02-15 23:07:38 +0100

    Apply suggestions from code review
    Co-authored-by: Heitor Augusto <IAm.HeitorALN@proton.me>

--- pkgs/by-name/ft/ftb-app/package.nix
+++ pkgs/by-name/ft/ftb-app/package.nix
@@ -3,38 +3,39 @@
   fetchurl,
   appimageTools,
   makeDesktopItem,
-  copyDesktopItems,
 }: let
   pname = "ftb-app";
   version = "1.27.2";
 
   src = fetchurl {
     url = "https://piston.feed-the-beast.com/app/ftb-app-linux-${version}-x86_64.AppImage";
-    sha256 = "35GEI1OBvVkUvHvQAzzGz8ux9h+5W3acH0Wr5VkqyBw=";
+    hash = "sha256-35GEI1OBvVkUvHvQAzzGz8ux9h+5W3acH0Wr5VkqyBw=";
   };
+  
+  appimageContents = appimageTools.extractType2 { inherit pname src version; };
 in
-  appimageTools.wrapType2 rec {
+  appimageTools.wrapType2 {
     inherit pname src version;
-
-    desktopItem = makeDesktopItem {
-      name = "FTB App";
-      desktopName = "FTB App";
-      exec = "ftb-app";
-      icon = "ftb-app";
-      comment = "Feed the Beast desktop app";
-      categories = ["Game"];
-    };
-
+    
     extraInstallCommands = ''
-      mkdir -p $out/share/applications
-      cp ${desktopItem}/share/applications/* $out/share/applications/
+      for size in 16x16 32x32 48x48 64x64 128x128 256x256 512x512; do
+        install -Dm644 ${appimageContents}/usr/share/icons/hicolor/$size/apps/ftb-app.png \
+          $out/share/icons/hicolor/$size/apps/ftb-app.png
+      done
+      
+      install -Dm644 ${appimageContents}/ftb-app.desktop \
+        $out/share/applications/ftb-app.desktop
+      substituteInPlace $out/share/applications/ftb-app.desktop \
+        --replace-fail 'Exec=AppRun' 'Exec=${placeholder "out"}/bin/ftb-app'
     '';
 
+    passthru.updateScript = nix-update-script { };
+    
     meta = {
       description = "Feed the Beast desktop app";
       homepage = "https://www.feed-the-beast.com/ftb-app";
-      changelog = "https://www.feed-the-beast.com/ftb-app/changes#1.27.2";
-      license = lib.licenses.lgpl21;
+      changelog = "https://www.feed-the-beast.com/ftb-app/changes#${version}";
+      license = lib.licenses.lgpl21Only;
       maintainers = with lib.maintainers; [ nagymathev ];
       mainProgram = "ftb-app";
       platforms = lib.platforms.linux;
