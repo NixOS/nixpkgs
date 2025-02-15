ae40213396fd6703095767c8364c7d8489ca8f22
Cryolitia PukNgae <Cryolitia@gmail.com>
2025-02-15 21:47:44 +0800

    edukai: 4.0 -> 5.0
    (cherry picked from commit 992f37867062d8ffde722795fe65041ec81f10b4)

--- pkgs/by-name/ed/edukai/package.nix
+++ pkgs/by-name/ed/edukai/package.nix
@@ -6,12 +6,12 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "edukai";
-  version = "4.0";
+  version = "5.0";
 
   src = fetchzip {
     name = "${pname}-${version}";
-    url = "http://language.moe.gov.tw/001/Upload/Files/site_content/M0001/edukai-4.0.zip";
-    sha256 = "10m9srvbazvg9gc43943dc89rjzcfc8mm4lx9gb5hnplrn22zrcn";
+    url = "https://language.moe.gov.tw/001/Upload/Files/site_content/M0001/edukai-${version}.zip";
+    sha256 = "sha256-3+w9n6GJQg9+HfHYukC7tlm4GVs8vEOO23hrLw6qjTY=";
   };
 
   installPhase = ''
