721751bd2326dad2902a81c80a21f621e79e5fd0
Yueh-Shun Li <shamrocklee@posteo.net>
2025-02-16 05:43:30 +0800

    buildRustPackage: reformat after refactoring

--- pkgs/build-support/rust/build-rust-package/default.nix
+++ pkgs/build-support/rust/build-rust-package/default.nix
@@ -30,173 +30,173 @@ lib.extendMkDerivation {
 
   extendDrvArgs =
     finalAttrs:
-{
-  name ? "${args.pname}-${args.version}",
+    {
+      name ? "${args.pname}-${args.version}",
 
-  # Name for the vendored dependencies tarball
-  cargoDepsName ? name,
+      # Name for the vendored dependencies tarball
+      cargoDepsName ? name,
 
-  src ? null,
-  srcs ? null,
-  preUnpack ? null,
-  unpackPhase ? null,
-  postUnpack ? null,
-  cargoPatches ? [ ],
-  patches ? [ ],
-  sourceRoot ? null,
-  cargoRoot ? null,
-  logLevel ? "",
-  buildInputs ? [ ],
-  nativeBuildInputs ? [ ],
-  cargoUpdateHook ? "",
-  cargoDepsHook ? "",
-  buildType ? "release",
-  meta ? { },
-  useFetchCargoVendor ? false,
-  cargoDeps ? null,
-  cargoLock ? null,
-  cargoVendorDir ? null,
-  checkType ? buildType,
-  buildNoDefaultFeatures ? false,
-  checkNoDefaultFeatures ? buildNoDefaultFeatures,
-  buildFeatures ? [ ],
-  checkFeatures ? buildFeatures,
-  useNextest ? false,
-  auditable ? !cargo-auditable.meta.broken,
+      src ? null,
+      srcs ? null,
+      preUnpack ? null,
+      unpackPhase ? null,
+      postUnpack ? null,
+      cargoPatches ? [ ],
+      patches ? [ ],
+      sourceRoot ? null,
+      cargoRoot ? null,
+      logLevel ? "",
+      buildInputs ? [ ],
+      nativeBuildInputs ? [ ],
+      cargoUpdateHook ? "",
+      cargoDepsHook ? "",
+      buildType ? "release",
+      meta ? { },
+      useFetchCargoVendor ? false,
+      cargoDeps ? null,
+      cargoLock ? null,
+      cargoVendorDir ? null,
+      checkType ? buildType,
+      buildNoDefaultFeatures ? false,
+      checkNoDefaultFeatures ? buildNoDefaultFeatures,
+      buildFeatures ? [ ],
+      checkFeatures ? buildFeatures,
+      useNextest ? false,
+      auditable ? !cargo-auditable.meta.broken,
 
-  depsExtraArgs ? { },
+      depsExtraArgs ? { },
 
-  # Needed to `pushd`/`popd` into a subdir of a tarball if this subdir
-  # contains a Cargo.toml, but isn't part of a workspace (which is e.g. the
-  # case for `rustfmt`/etc from the `rust-sources).
-  # Otherwise, everything from the tarball would've been built/tested.
-  buildAndTestSubdir ? null,
-  ...
-}@args:
+      # Needed to `pushd`/`popd` into a subdir of a tarball if this subdir
+      # contains a Cargo.toml, but isn't part of a workspace (which is e.g. the
+      # case for `rustfmt`/etc from the `rust-sources).
+      # Otherwise, everything from the tarball would've been built/tested.
+      buildAndTestSubdir ? null,
+      ...
+    }@args:
 
-let
+    let
 
-  cargoDeps' =
-    if cargoVendorDir != null then
-      null
-    else if cargoDeps != null then
-      cargoDeps
-    else if cargoLock != null then
-      importCargoLock cargoLock
-    else if (args.cargoHash or null == null) && (args.cargoSha256 or null == null) then
-      throw "cargoHash, cargoVendorDir, cargoDeps, or cargoLock must be set"
-    else if useFetchCargoVendor then
-      fetchCargoVendor (
-        {
-          inherit
-            src
-            srcs
-            sourceRoot
-            cargoRoot
-            preUnpack
-            unpackPhase
-            postUnpack
-            ;
-          name = cargoDepsName;
-          patches = cargoPatches;
-          hash = args.cargoHash;
-        }
-        // depsExtraArgs
-      )
-    else
-      fetchCargoTarball (
-        {
-          inherit
-            src
-            srcs
-            sourceRoot
-            cargoRoot
-            preUnpack
-            unpackPhase
-            postUnpack
-            cargoUpdateHook
-            ;
-          name = cargoDepsName;
-          patches = cargoPatches;
-        }
-        // lib.optionalAttrs (args ? cargoHash) {
-          hash = args.cargoHash;
-        }
-        // lib.optionalAttrs (args ? cargoSha256) {
-          sha256 = lib.warn "cargoSha256 is deprecated. Please use cargoHash with SRI hash instead" args.cargoSha256;
-        }
-        // depsExtraArgs
-      );
+      cargoDeps' =
+        if cargoVendorDir != null then
+          null
+        else if cargoDeps != null then
+          cargoDeps
+        else if cargoLock != null then
+          importCargoLock cargoLock
+        else if (args.cargoHash or null == null) && (args.cargoSha256 or null == null) then
+          throw "cargoHash, cargoVendorDir, cargoDeps, or cargoLock must be set"
+        else if useFetchCargoVendor then
+          fetchCargoVendor (
+            {
+              inherit
+                src
+                srcs
+                sourceRoot
+                cargoRoot
+                preUnpack
+                unpackPhase
+                postUnpack
+                ;
+              name = cargoDepsName;
+              patches = cargoPatches;
+              hash = args.cargoHash;
+            }
+            // depsExtraArgs
+          )
+        else
+          fetchCargoTarball (
+            {
+              inherit
+                src
+                srcs
+                sourceRoot
+                cargoRoot
+                preUnpack
+                unpackPhase
+                postUnpack
+                cargoUpdateHook
+                ;
+              name = cargoDepsName;
+              patches = cargoPatches;
+            }
+            // lib.optionalAttrs (args ? cargoHash) {
+              hash = args.cargoHash;
+            }
+            // lib.optionalAttrs (args ? cargoSha256) {
+              sha256 = lib.warn "cargoSha256 is deprecated. Please use cargoHash with SRI hash instead" args.cargoSha256;
+            }
+            // depsExtraArgs
+          );
 
-  target = stdenv.hostPlatform.rust.rustcTargetSpec;
-  targetIsJSON = lib.hasSuffix ".json" target;
-in
-  lib.optionalAttrs (stdenv.hostPlatform.isDarwin && buildType == "debug") {
-    RUSTFLAGS = "-C split-debuginfo=packed " + (args.RUSTFLAGS or "");
-  }
-  // {
-    cargoDeps = cargoDeps';
-    inherit buildAndTestSubdir;
+      target = stdenv.hostPlatform.rust.rustcTargetSpec;
+      targetIsJSON = lib.hasSuffix ".json" target;
+    in
+    lib.optionalAttrs (stdenv.hostPlatform.isDarwin && buildType == "debug") {
+      RUSTFLAGS = "-C split-debuginfo=packed " + (args.RUSTFLAGS or "");
+    }
+    // {
+      cargoDeps = cargoDeps';
+      inherit buildAndTestSubdir;
 
-    cargoBuildType = buildType;
+      cargoBuildType = buildType;
 
-    cargoCheckType = checkType;
+      cargoCheckType = checkType;
 
-    cargoBuildNoDefaultFeatures = buildNoDefaultFeatures;
+      cargoBuildNoDefaultFeatures = buildNoDefaultFeatures;
 
-    cargoCheckNoDefaultFeatures = checkNoDefaultFeatures;
+      cargoCheckNoDefaultFeatures = checkNoDefaultFeatures;
 
-    cargoBuildFeatures = buildFeatures;
+      cargoBuildFeatures = buildFeatures;
 
-    cargoCheckFeatures = checkFeatures;
+      cargoCheckFeatures = checkFeatures;
 
-    nativeBuildInputs =
-      nativeBuildInputs
-      ++ lib.optionals auditable [
-        (buildPackages.cargo-auditable-cargo-wrapper.override {
-          inherit cargo cargo-auditable;
-        })
-      ]
-      ++ [
-        cargoBuildHook
-        (if useNextest then cargoNextestHook else cargoCheckHook)
-        cargoInstallHook
-        cargoSetupHook
-        rustc
-        cargo
-      ];
+      nativeBuildInputs =
+        nativeBuildInputs
+        ++ lib.optionals auditable [
+          (buildPackages.cargo-auditable-cargo-wrapper.override {
+            inherit cargo cargo-auditable;
+          })
+        ]
+        ++ [
+          cargoBuildHook
+          (if useNextest then cargoNextestHook else cargoCheckHook)
+          cargoInstallHook
+          cargoSetupHook
+          rustc
+          cargo
+        ];
 
-    buildInputs =
-      buildInputs
-      ++ lib.optionals stdenv.hostPlatform.isDarwin [ libiconv ]
-      ++ lib.optionals stdenv.hostPlatform.isMinGW [ windows.pthreads ];
+      buildInputs =
+        buildInputs
+        ++ lib.optionals stdenv.hostPlatform.isDarwin [ libiconv ]
+        ++ lib.optionals stdenv.hostPlatform.isMinGW [ windows.pthreads ];
 
-    patches = cargoPatches ++ patches;
+      patches = cargoPatches ++ patches;
 
-    PKG_CONFIG_ALLOW_CROSS = if stdenv.buildPlatform != stdenv.hostPlatform then 1 else 0;
+      PKG_CONFIG_ALLOW_CROSS = if stdenv.buildPlatform != stdenv.hostPlatform then 1 else 0;
 
-    postUnpack =
-      ''
-        eval "$cargoDepsHook"
+      postUnpack =
+        ''
+          eval "$cargoDepsHook"
 
-        export RUST_LOG=${logLevel}
-      ''
-      + (args.postUnpack or "");
+          export RUST_LOG=${logLevel}
+        ''
+        + (args.postUnpack or "");
 
-    configurePhase =
-      args.configurePhase or ''
-        runHook preConfigure
-        runHook postConfigure
-      '';
+      configurePhase =
+        args.configurePhase or ''
+          runHook preConfigure
+          runHook postConfigure
+        '';
 
-    doCheck = args.doCheck or true;
+      doCheck = args.doCheck or true;
 
-    strictDeps = true;
+      strictDeps = true;
 
-    meta = meta // {
-      badPlatforms = meta.badPlatforms or [ ] ++ rustc.badTargetPlatforms;
-      # default to Rust's platforms
-      platforms = lib.intersectLists meta.platforms or lib.platforms.all rustc.targetPlatforms;
+      meta = meta // {
+        badPlatforms = meta.badPlatforms or [ ] ++ rustc.badTargetPlatforms;
+        # default to Rust's platforms
+        platforms = lib.intersectLists meta.platforms or lib.platforms.all rustc.targetPlatforms;
+      };
     };
-  };
 }
