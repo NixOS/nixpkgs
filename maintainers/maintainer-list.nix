95fb56faff8e3f043ac60f2aaf7ec9e122c39146
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-15 16:45:50 +1100

    maintainers: add yunz

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25780,6 +25780,11 @@
     githubId = 73419713;
     name = "Yunfachi";
   };
+  yunz = {
+    github = "yunz-dev";
+    githubId = 112053367;
+    name = "Yunus";
+  };
   yureien = {
     email = "contact@sohamsen.me";
     github = "Yureien";
