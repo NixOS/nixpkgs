32ef95f442571ae12c31efc40925ce960f4109bc
r-vdp <ramses@well-founded.dev>
2025-02-15 14:23:27 +0100

    flashrom: 1.3.0 -> 1.5.1
    I cherry-picked the commits from https://github.com/NixOS/nixpkgs/pull/330581
    and then added the 1.5.1 update on top.
    
    We can drop the darwin patch since the affected code is only present in
    the tests and those don't work on darwin anyway, so we can better just
    don't compile them at all on that platform.
    
    On Linux we do compile and now also run the tests.

--- pkgs/by-name/fl/flashrom/package.nix
+++ pkgs/by-name/fl/flashrom/package.nix
@@ -7,6 +7,7 @@
   libftdi1,
   libjaylink,
   libusb1,
+  openssl,
   meson,
   ninja,
   pciutils,
@@ -17,24 +18,21 @@
 
 stdenv.mkDerivation rec {
   pname = "flashrom";
-  version = "1.4.0";
+  version = "1.5.1";
 
   src = fetchurl {
-    url = "https://download.flashrom.org/releases/flashrom-${version}.tar.xz";
-    hash = "sha256-rX7htJI5xvtPj1XjZwb81zFDXbGkvS+rPYDx9yUIzO4=";
+    url = "https://download.flashrom.org/releases/flashrom-v${version}.tar.xz";
+    hash = "sha256-H5NLB27UnqziA2Vewkn8eGGmuOh/5K73MuR7bkhbYpM=";
   };
 
-  patches =
-    [
-      # Release notes for 1.4.0 state that Promontory chipsets are unsupported, and that attempting to read flash on those systems may crash the system.
-      # The patch that removes this (broken) support only made it into the 1.3.0 release, seemingly by mistake, and the relevant code has been essentially untouched since.
-      # We cherry-pick the upstream patch from 1.3.0, though amended to reference the relevant bug in the error message, rather than requesting the user email upstream.
-      # https://ticket.coreboot.org/issues/370
-      # https://review.coreboot.org/c/flashrom/+/68824
-      ./0001-sb600spi.c-Drop-Promontory-support.patch
-    ]
-    # Darwin's memcpy is a macro, and requires braced initializer lists to be parenthesized
-    ++ lib.optional stdenv.isDarwin ./0002-tests-erase-parenthesize-braced-initializer.patch;
+  patches = [
+    # Release notes for 1.4.0 state that Promontory chipsets are unsupported, and that attempting to read flash on those systems may crash the system.
+    # The patch that removes this (broken) support only made it into the 1.3.0 release, seemingly by mistake, and the relevant code has been essentially untouched since.
+    # We cherry-pick the upstream patch from 1.3.0, though amended to reference the relevant bug in the error message, rather than requesting the user email upstream.
+    # https://ticket.coreboot.org/issues/370
+    # https://review.coreboot.org/c/flashrom/+/68824
+    ./0001-sb600spi.c-Drop-Promontory-support.patch
+  ];
 
   nativeBuildInputs = [
     meson
@@ -45,17 +43,26 @@ stdenv.mkDerivation rec {
   ];
   buildInputs =
     [
+      openssl
       cmocka
       libftdi1
       libusb1
     ]
-    ++ lib.optionals (!stdenv.isDarwin) [ pciutils ]
+    ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ pciutils ]
     ++ lib.optional jlinkSupport libjaylink;
 
   postPatch = ''
     substituteInPlace util/flashrom_udev.rules \
       --replace 'GROUP="plugdev"' 'TAG+="uaccess", TAG+="udev-acl"'
   '';
+
+  mesonFlags = [
+    (lib.mesonOption "programmer" "auto")
+    (lib.mesonEnable "man-pages" true)
+    (lib.mesonEnable "tests" (!stdenv.buildPlatform.isDarwin))
+  ];
+
+  doCheck = !stdenv.hostPlatform.isDarwin;
 
   postInstall = ''
     install -Dm644 $NIX_BUILD_TOP/$sourceRoot/util/flashrom_udev.rules $out/lib/udev/rules.d/flashrom.rules
