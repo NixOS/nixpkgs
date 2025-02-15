48366422ae6aeedac6ea6f2ea6c7e92e9cad665e
Adam Sutch <adamsutch@gmail.com>
2025-02-15 18:38:07 -0500

    incus.ui: 0.7 -> incus-0.14.6
    fix(incus-ui): update version format and tag reference

--- pkgs/by-name/in/incus/ui.nix
+++ pkgs/by-name/in/incus/ui.nix
@@ -12,32 +12,28 @@
 
 stdenv.mkDerivation rec {
   pname = "incus-ui";
-  version = "0.7";
+  version = "0.14.6";
 
   src = fetchFromGitHub {
-    owner = "canonical";
-    repo = "lxd-ui";
-    rev = "refs/tags/${version}";
-    hash = "sha256-DJLkXZpParmEYHbTpl6KFC9l9y5DqzUTrC0pb2dJXI4=";
+    owner = "zabbly";
+    repo = "incus-ui-canonical";
+    tag = "incus-${version}";
+    hash = "sha256-An2mhIj3D2EdB1Bgnry1l2m6r/GIKTee4anSYNTq8B8=";
   };
 
   offlineCache = fetchYarnDeps {
     yarnLock = "${src}/yarn.lock";
-    hash = "sha256-ckTWE/czzvxbGOF8fsJ3W1sal7+NaHquoSjZSPjkGj4=";
+    hash = "sha256-dkATFNjAPhrPbXhcJ/R4eIpcagKEwBSnRfLwqTPIe6c=";
   };
 
   zabbly = fetchFromGitHub {
     owner = "zabbly";
     repo = "incus";
-    rev = "c83023587eb0e3b01c99ba26e63f757ac15c6f9c";
-    hash = "sha256-cWKp4ALrae6nEBLvWcOM1T+Aca7eHLwsRguH9aSb10Y=";
+    rev = "36714d7c38eb3cc3e4e821c7aed44e066e1e84ca";
+    hash = "sha256-H6gjXmwCv3oGXrzn1NENfgO3CWXMnmp94GdJv2Q8n0w=";
   };
 
   patchPhase = ''
-    for p in $zabbly/patches/ui-canonical*patch; do
-      echo "applying patch $p"
-      git apply -p1 "$p"
-    done
     sed -i -f "$zabbly/patches/ui-canonical-renames.sed" src/*/*.ts* src/*/*/*.ts* src/*/*/*/*.ts*
   '';
 
@@ -80,7 +76,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Web user interface for Incus, based on LXD webui";
-    homepage = "https://github.com/canonical/lxd-ui";
+    homepage = "https://github.com/zabbly/incus-ui-canonical";
     license = lib.licenses.gpl3;
     maintainers = lib.teams.lxc.members;
     platforms = lib.platforms.linux;
