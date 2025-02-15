172fa69cbdf07b2e4a9d2c84929c7d65cebe6156
Maximilian Bosch <maximilian@mbosch.me>
2025-02-15 11:28:40 +0100

    postgresql_16: 16.6 -> 16.8
    ChangeLog:
    https://www.postgresql.org/about/news/postgresql-173-167-1511-1416-and-1319-released-3015/
    https://www.postgresql.org/about/news/out-of-cycle-release-scheduled-for-february-20-2025-3016/
    
    Co-authored-by: Wolfgang Walther <walther@technowledgy.de>

--- pkgs/servers/sql/postgresql/16.nix
+++ pkgs/servers/sql/postgresql/16.nix
@@ -1,7 +1,8 @@
 import ./generic.nix {
-  version = "16.6";
-  rev = "ref/tags/REL_16_6";
-  hash = "sha256-cw/bSR9ZI6HFtbSTQxvuB7ng7V6g5SZZ7B0oMCz4E7Q=";
+  version = "16.8";
+  # "Stamp 16.8"
+  rev = "71eb35c0b18de96537bd3876ec9bf8075bfd484f";
+  hash = "sha256-nVUGBuvCDFXozTyEDAAQa+IR3expCdztH90J68FhAXQ=";
   muslPatches = {
     dont-use-locale-a = {
       url = "https://git.alpinelinux.org/aports/plain/main/postgresql16/dont-use-locale-a-on-musl.patch?id=08a24be262339fd093e641860680944c3590238e";
