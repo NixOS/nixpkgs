f770af18836bdc5857aa92231e6c088f8ef0fe0d
Maximilian Bosch <maximilian@mbosch.me>
2025-02-15 11:29:09 +0100

    postgresql_17: 17.2 -> 17.4
    ChangeLog:
    https://www.postgresql.org/about/news/postgresql-173-167-1511-1416-and-1319-released-3015/
    https://www.postgresql.org/about/news/out-of-cycle-release-scheduled-for-february-20-2025-3016/
    
    Co-authored-by: Wolfgang Walther <walther@technowledgy.de>
    (cherry picked from commit 70d670e42c6519b1c35cd27f1a2d7697e4b124aa)

--- pkgs/servers/sql/postgresql/17.nix
+++ pkgs/servers/sql/postgresql/17.nix
@@ -1,7 +1,8 @@
 import ./generic.nix {
-  version = "17.2";
-  rev = "ref/tags/REL_17_2";
-  hash = "sha256-P7IwvMcOI6vW14PiB2R0NEzAEPeaKg0zaUKTw2GJ5DA=";
+  version = "17.4";
+  # "Stamp 17.4"
+  rev = "f8554dee417ffc4540c94cf357f7bf7d4b6e5d80";
+  hash = "sha256-TEpvX28chR3CXiOQsNY12t8WfM9ywoZVX1e/6mj9DqE=";
   muslPatches = {
     dont-use-locale-a = {
       url = "https://git.alpinelinux.org/aports/plain/main/postgresql17/dont-use-locale-a-on-musl.patch?id=d69ead2c87230118ae7f72cef7d761e761e1f37e";
