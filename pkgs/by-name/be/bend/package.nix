8343124eba3bd452e86370a5f3439d57cb478d09
Okko <okko.contact@proton.me>
2025-02-15 16:29:39 +0200

    bend: 0.2.36 -> 0.2.37

--- pkgs/by-name/be/bend/package.nix
+++ pkgs/by-name/be/bend/package.nix
@@ -8,17 +8,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "Bend";
-  version = "0.2.36";
+  version = "0.2.37";
 
   src = fetchFromGitHub {
     owner = "HigherOrderCO";
     repo = "Bend";
     tag = version;
-    hash = "sha256-j4YMdeSxIbhp7xT42L42/y0ZncFPKBkxTh0LgO/RjkY=";
+    hash = "sha256-8uBEI9GKUETk8t6Oanb0OECe3MlJ486QnccOuhIxPuY=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-mGpH08VMmqoxXm8EUPdsBvSW/CFyRPY4bfyr8JI1es0=";
+  cargoHash = "sha256-xac5Gb0hcamT3vymq6SKaMiuEn8NU9Bfhu+t/dcjhTE=";
 
   nativeBuildInputs = [
     hvm
