3faa44dd9c22c60ff0cdcc5dccb02fd3be5ad712
nayeko <196556004+nayeko@users.noreply.github.com>
2025-02-15 18:10:49 +0000

    bililiverecorder:2.13.0 -> 2.15.1

--- pkgs/by-name/bi/bililiverecorder/package.nix
+++ pkgs/by-name/bi/bililiverecorder/package.nix
@@ -7,26 +7,21 @@
 }:
 
 let
-  pname = "bililiverecorder";
-
   dotnet =
     with dotnetCorePackages;
     combinePackages [
-      runtime_6_0-bin
-      aspnetcore_6_0-bin
+      runtime_8_0-bin
+      aspnetcore_8_0-bin
     ];
-
-  version = "2.13.0";
-  hash = "sha256-4OQ2gut/eLk4CXRN5E3Z8XobXsT3bSmtmJEcHzHcz/0=";
-
 in
-stdenv.mkDerivation {
-  inherit pname version;
+stdenv.mkDerivation rec {
+  pname = "bililiverecorder";
+  version = "2.15.1";
 
   src = fetchzip {
     url = "https://github.com/BililiveRecorder/BililiveRecorder/releases/download/v${version}/BililiveRecorder-CLI-any.zip";
+    hash = "sha256-ugzFiuLe+Al3aRvEM3D4kqnaFrFR4Pr95UlEg0VGvvU=";
     stripRoot = false;
-    inherit hash;
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -34,11 +29,10 @@ stdenv.mkDerivation {
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/{bin,share/${pname}-${version}}
-    cp -r * $out/share/${pname}-${version}/.
-
-    makeWrapper "${dotnet}/bin/dotnet" $out/bin/BililiveRecorder \
-      --add-flags "$out/share/${pname}-${version}/BililiveRecorder.Cli.dll"
+    mkdir -p $out/{bin,lib}
+    cp -r . $out/lib/bililiverecorder
+    makeWrapper ${dotnet}/bin/dotnet $out/bin/BililiveRecorder \
+      --add-flags $out/lib/bililiverecorder/BililiveRecorder.Cli.dll
 
     runHook postInstall
   '';
