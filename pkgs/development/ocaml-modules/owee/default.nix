23c6c377d9c1e6ee20e8d404d0913aa55d39b2de
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-16 01:37:34 +0000

    ocamlPackages.owee: 0.7 -> 0.8

--- pkgs/development/ocaml-modules/owee/default.nix
+++ pkgs/development/ocaml-modules/owee/default.nix
@@ -7,11 +7,11 @@
 buildDunePackage rec {
   minimalOCamlVersion = "4.06";
   pname = "owee";
-  version = "0.7";
+  version = "0.8";
 
   src = fetchurl {
     url = "https://github.com/let-def/owee/releases/download/v${version}/owee-${version}.tbz";
-    hash = "sha256-9FXcmddHg5mk5UWgYd4kTPOLOY/p6A/OBuvfas4elUA=";
+    hash = "sha256-YsTRsm12iy52LfM3MF90mf9cy4JoGWelqxJlI/vPs9A=";
   };
 
   meta = with lib; {
