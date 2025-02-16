a660719bb427f3ed781636845dccb2490e78b00a
blackzeshi <105582686+zeshi09@users.noreply.github.com>
2025-02-16 20:28:12 +0500

    Update default.nix

--- pkgs/development/python-modules/posting/default.nix
+++ pkgs/development/python-modules/posting/default.nix
@@ -1,14 +1,16 @@
-{pkgs ? import <nixpkgs> {}}: let
-  textual-0_86_2 = pkgs.python3Packages.buildPythonPackage rec {
+{ pkgs ? import <nixpkgs> {} }: let
+  python3Packages = pkgs.python3Packages;
+
+  textual-0_86_2 = python3Packages.buildPythonPackage rec {
     pname = "textual";
     version = "0.86.2";
 
-    src = pkgs.fetchPypi {
+    src = python3Packages.fetchPypi {
       inherit pname version;
       sha256 = "sha256-Qwc/nrUtDzilry8SS6aLN4+R0mDfSxyS84Sjtcu7y3A=";
     };
 
-    propagatedBuildInputs = with pkgs.python3Packages; [
+    propagatedBuildInputs = with python3Packages; [
       rich
       platformdirs
       typing-extensions
@@ -16,70 +18,70 @@
     ];
 
     format = "pyproject";
-    nativeBuildInputs = with pkgs.python312Packages; [
+    nativeBuildInputs = with python3Packages; [
       poetry-core
     ];
   };
 
-  textual-autocomplete = pkgs.python3Packages.buildPythonPackage rec {
+  textual-autocomplete = python3Packages.buildPythonPackage rec {
     pname = "textual_autocomplete";
     version = "3.0.0a13";
 
-    src = pkgs.fetchPypi {
+    src = python3Packages.fetchPypi {
       inherit pname version;
       sha256 = "sha256-21pK6VbdfW3s5T9/aV6X8qt1gZ3Za4ocBk7Flms6sRM=";
     };
 
-    propagatedBuildInputs = with pkgs.python3Packages; [
+    propagatedBuildInputs = with python3Packages; [
       textual-0_86_2
     ];
 
     format = "pyproject";
-    nativeBuildInputs = with pkgs.python312Packages; [
+    nativeBuildInputs = with python3Packages; [
       hatchling
     ];
   };
-in
-  pkgs.python3Packages.buildPythonPackage rec {
-    pname = "posting";
-    version = "2.3.0";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "darrenburns";
-      repo = "posting";
-      rev = version;
-      sha256 = "sha256-lL85gJxFw8/e8Js+UCE9VxBMcmWRUkHh8Cq5wTC93KA=";
-    };
+in python3Packages.buildPythonPackage rec {
+  pname = "posting";
+  version = "2.3.0";
 
-    nativeBuildInputs = with pkgs.python312Packages; [
-      hatchling
-    ];
+  src = python3Packages.fetchFromGitHub {
+    owner = "darrenburns";
+    repo = "posting";
+    rev = version;
+    sha256 = "sha256-lL85gJxFw8/e8Js+UCE9VxBMcmWRUkHh8Cq5wTC93KA=";
+  };
 
-    propagatedBuildInputs = with pkgs.python312Packages; [
-      httpx
-      textual-0_86_2
-      textual-autocomplete
-      click
-      xdg-base-dirs
-      click-default-group
-      pyperclip
-      pyyaml
-      pydantic-settings
-      python-dotenv
-      watchfiles
-      rich
-      toml
-      pydantic
-      typer
-    ];
+  nativeBuildInputs = with python3Packages; [
+    hatchling
+  ];
 
-    format = "pyproject";
-    doCheck = false;
-    pythonImportsCheck = ["posting"];
+  propagatedBuildInputs = with python3Packages; [
+    httpx
+    textual-0_86_2
+    textual-autocomplete
+    click
+    xdg-base-dirs
+    click-default-group
+    pyperclip
+    pyyaml
+    pydantic-settings
+    python-dotenv
+    watchfiles
+    rich
+    toml
+    pydantic
+    typer
+  ];
 
-    meta = with pkgs.lib; {
-      description = "Command-line HTTP client";
-      homepage = "https://github.com/darrenburns/posting";
-      license = licenses.mit;
-    };
-  }
+  format = "pyproject";
+  doCheck = false;
+  pythonImportsCheck = ["posting"];
+
+  meta = with pkgs.lib; {
+    description = "Command-line HTTP client";
+    homepage = "https://github.com/darrenburns/posting";
+    license = licenses.mit;
+  };
+}
