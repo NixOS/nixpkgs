e1e65656455b264ae20c4720698cc5207fa0c183
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-16 22:44:34 +0000

    opera: 116.0.5366.71 -> 117.0.5408.32
    (cherry picked from commit 834b999e8d5597133e8554d9da1df91674e5424a)

--- pkgs/by-name/op/opera/package.nix
+++ pkgs/by-name/op/opera/package.nix
@@ -52,11 +52,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "opera";
-  version = "116.0.5366.71";
+  version = "117.0.5408.32";
 
   src = fetchurl {
     url = "${mirror}/${version}/linux/${pname}-stable_${version}_amd64.deb";
-    hash = "sha256-EgHr8YWAACXiT/S+zThAbTGoFVbOXVXcJ/MK//TWzL4=";
+    hash = "sha256-gDhSme2jUQrKqfZZef7raIylZotyzbfS35YEAuxKnBo=";
   };
 
   unpackPhase = "dpkg-deb -x $src .";
