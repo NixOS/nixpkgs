d02334183eca5883f31efc506d6481f3383df190
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-16 11:12:47 +0000

    python312Packages.cohere: 5.13.11 -> 5.13.12

--- pkgs/development/python-modules/cohere/default.nix
+++ pkgs/development/python-modules/cohere/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "cohere";
-  version = "5.13.11";
+  version = "5.13.12";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "cohere-ai";
     repo = "cohere-python";
     tag = version;
-    hash = "sha256-vDf5EoXCnYJhPnn9uj9L2cAnj7z1HVG1KxtxXByjwt8=";
+    hash = "sha256-umHqkrYECz6SGwnNdHmWoD0n6+aNnyREYPK0i8AUfss=";
   };
 
   build-system = [ poetry-core ];
@@ -52,7 +52,7 @@ buildPythonPackage rec {
   meta = {
     description = "Simplify interfacing with the Cohere API";
     homepage = "https://docs.cohere.com/docs";
-    changelog = "https://github.com/cohere-ai/cohere-python/releases/tag/${version}";
+    changelog = "https://github.com/cohere-ai/cohere-python/releases/tag/${src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ natsukium ];
   };
