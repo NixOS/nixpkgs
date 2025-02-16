7e5cbae0a550c09a03d4d94907fecd2f3042c43b
RatCornu <balthazar.patiachvili@crans.org>
2025-02-16 19:31:19 +0100

    szurubooru: init at 2.5-unstable-2025-02-11

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9721,6 +9721,8 @@ with pkgs;
 
   svxlink = libsForQt5.callPackage ../applications/radio/svxlink { };
 
+  szurubooru = callPackage ../servers/web-apps/szurubooru { };
+
   tclap = tclap_1_2;
 
   tclap_1_2 = callPackage ../development/libraries/tclap/1.2.nix { };
