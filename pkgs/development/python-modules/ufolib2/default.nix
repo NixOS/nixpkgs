29314a92af8de8cb14db9d5c232d0f97966e4916
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-16 09:17:49 +0000

    python312Packages.ufolib2: 0.17.0 -> 0.17.1

--- pkgs/development/python-modules/ufolib2/default.nix
+++ pkgs/development/python-modules/ufolib2/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "ufolib2";
-  version = "0.17.0";
+  version = "0.17.1";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "fonttools";
     repo = "ufoLib2";
     tag = "v${version}";
-    hash = "sha256-WSy+5tH+/ThbmfOC5KiTzCagcLSiXZXPuiIEJZ07KK0=";
+    hash = "sha256-pVwQOVtUUDphBZIUoiIf19DdZ+t7uS32Ery8+e2ZLlE=";
   };
 
   build-system = [ setuptools-scm ];
