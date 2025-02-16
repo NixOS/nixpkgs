1b14de6df34f9db80962905c13796feddca1e18a
Solomon Victorino <git@solomonvictorino.com>
2025-02-16 14:38:30 -0700

    nixos/openssh: add post-quantum KexAlgorithms introduced in v9.9
    `mlkem768x25519-sha256` and the vendor-neutral `sntrup761x25519-sha512`
    name are new in OpenSSH 9.9 <https://www.openssh.com/txt/release-9.9>
    
    Co-authored-by: Mynacol <Mynacol@users.noreply.github.com>

--- nixos/modules/services/networking/ssh/sshd.nix
+++ nixos/modules/services/networking/ssh/sshd.nix
@@ -415,6 +415,8 @@ in
             KexAlgorithms = lib.mkOption {
               type = lib.types.nullOr (lib.types.listOf lib.types.str);
               default = [
+                "mlkem768x25519-sha256"
+                "sntrup761x25519-sha512"
                 "sntrup761x25519-sha512@openssh.com"
                 "curve25519-sha256"
                 "curve25519-sha256@libssh.org"
