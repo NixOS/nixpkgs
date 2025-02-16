6deb5909499928fd7d3a79b5c40686f44b8fc215
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-02-16 18:56:09 +0100

    python312Packages.pytest-textual-snapshot: install resources

--- pkgs/development/python-modules/pytest-textual-snapshot/default.nix
+++ pkgs/development/python-modules/pytest-textual-snapshot/default.nix
@@ -28,6 +28,17 @@ buildPythonPackage rec {
   nativeBuildInputs = [ poetry-core ];
 
   buildInputs = [ pytest ];
+  # The script looks for `resources/snapshot_report_template.jinja2` in the parent folder which
+  # is lib/python3.X/site-packages
+  # Let's avoid to have a random 'resources' folder in the PYTHONPATH.
+  # Instead, we move this `resources` folder in `$out/share` (see postInstall below) and patch the
+  # path in the script.
+  postPatch = ''
+    substituteInPlace pytest_textual_snapshot.py \
+      --replace-fail \
+        "this_file_path.parent" \
+        "Path('$out/share/pytest-textual-snapshot/')"
+  '';
 
   propagatedBuildInputs = [
     jinja2
@@ -44,6 +55,11 @@ buildPythonPackage rec {
   doCheck = false;
 
   pythonImportsCheck = [ "pytest_textual_snapshot" ];
+
+  postInstall = ''
+    mkdir -p $out/share/pytest-textual-snapshot/
+    cp -r resources $out/share/pytest-textual-snapshot/
+  '';
 
   meta = with lib; {
     description = "Snapshot testing for Textual applications";
