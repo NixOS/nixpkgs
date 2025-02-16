NixOS/nixpkgs

commit 8a0e50ad4e69ffb3b6b2c3c4945f7ed6178e66b3

simple-scan: install missing icons

pkgs/by-name/si/simple-scan/package.nix
@@ -64,6 +64,11 @@ stdenv.mkDerivation rec {
     patchShebangs data/meson_compile_gschema.py
   '';
 
+  postInstall = ''
+    mkdir -p $out/share/icons/hicolor/scalable/actions/
+    install -m 444 ../data/icons/scalable/actions/* $out/share/icons/hicolor/scalable/actions/
+  '';
+
   doCheck = true;
 
   passthru = {

commit 3355b5e13508f3b547efe4aa4af88977032a1c86

simple-scan: Backport fix for RDNN app ID
Otherwise GResourced icons are not used.

Fixes: 8a0e50ad4e69ffb3b6b2c3c4945f7ed6178e66b3

pkgs/by-name/si/simple-scan/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  fetchpatch,
   meson,
   ninja,
   pkg-config,
@@ -34,6 +35,15 @@ stdenv.mkDerivation rec {
     hash = "sha256-wW5lkBQv5WO+UUMSKzu7U/awCn2p2VL2HEf6Jve08Kk=";
   };
 
+  patches = [
+    # simple-scan: Use RDNN app ID
+    # https://gitlab.gnome.org/GNOME/simple-scan/-/issues/390
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/simple-scan/-/commit/c09a6def153e52494072a36233c7e7b3307b67bf.patch";
+      hash = "sha256-deyssrsVwPAfT5ru6c0LFwR2pEFnZ0v8wMqoi96tw8s=";
+    })
+  ];
+
   nativeBuildInputs = [
     meson
     ninja
@@ -62,11 +72,6 @@ stdenv.mkDerivation rec {
 
   postPatch = ''
     patchShebangs data/meson_compile_gschema.py
-  '';
-
-  postInstall = ''
-    mkdir -p $out/share/icons/hicolor/scalable/actions/
-    install -m 444 ../data/icons/scalable/actions/* $out/share/icons/hicolor/scalable/actions/
   '';
 
   doCheck = true;