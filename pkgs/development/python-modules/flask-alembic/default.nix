61d32a081b4fc1a9292beb3fb07b6afc6a710d69
Kerstin Humm <kerstin@erictapen.name>
2025-02-17 17:50:29 +0100

    python3Packages.flask-alembic: fix build

--- pkgs/development/python-modules/flask-alembic/default.nix
+++ pkgs/development/python-modules/flask-alembic/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   pythonAtLeast,
   flit-core,
   alembic,
@@ -25,6 +26,13 @@ buildPythonPackage rec {
     hash = "sha256-iHJr9l3w1WwZXDl573IV7+A7RDcawGL20sxxhAQQ628=";
   };
 
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/pallets-eco/flask-alembic/pull/50/commits/d99f5752be040a4ce34cf1bc20a21f2ed1eb42f7.patch";
+      hash = "sha256-JzeRQRrKtCsBhU1N287MNYJfitOZd4PyXCbypAdNyDU=";
+    })
+  ];
+
   build-system = [ flit-core ];
 
   dependencies = [
@@ -41,12 +49,12 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "flask_alembic" ];
 
-  meta = with lib; {
+  meta = {
     # https://github.com/pallets-eco/flask-alembic/issues/47
     broken = pythonAtLeast "3.13";
     homepage = "https://github.com/pallets-eco/flask-alembic";
     changelog = "https://github.com/pallets-eco/flask-alembic/blob/${src.tag}/CHANGES.md";
-    license = licenses.mit;
-    maintainers = with maintainers; [ erictapen ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ erictapen ];
   };
 }
