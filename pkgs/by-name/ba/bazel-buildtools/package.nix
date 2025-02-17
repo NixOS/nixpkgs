fbcaac4e7a42847b19d8019fb2fb897f345f9f0f
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-17 15:42:26 +0000

    bazel-buildtools: 7.3.1 -> 8.0.3

--- pkgs/by-name/ba/bazel-buildtools/package.nix
+++ pkgs/by-name/ba/bazel-buildtools/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "bazel-buildtools";
-  version = "7.3.1";
+  version = "8.0.3";
 
   src = fetchFromGitHub {
     owner = "bazelbuild";
     repo = "buildtools";
     rev = "v${version}";
-    hash = "sha256-AdwJDRw+AY3F+ZDaKqn5YzAVyAzvrV+d1WTk8OJtUdk=";
+    hash = "sha256-m1z3i1rddwXZK5VF4qwhaMCS5U3FZ5SawJz/IgdRgdU=";
   };
 
   vendorHash = "sha256-sYZ7ogQY0dWOwJMvLljOjaKeYGYdLrF5AnetregdlYY=";
