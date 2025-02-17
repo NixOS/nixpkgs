afded69f04eb63b86013c248abf2d3634d026ef1
Malte Voos <git@mal.tc>
2025-02-17 01:13:36 +0100

    libguestfs: remove hydra opt-out and add comment about updating the package

--- pkgs/by-name/li/libguestfs/package.nix
+++ pkgs/by-name/li/libguestfs/package.nix
@@ -47,6 +47,11 @@ assert appliance == null || lib.isDerivation appliance;
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libguestfs";
+
+  # It is advisable to keep the `libguestfs` and `libguestfs-appliance-debian`
+  # packages in sync to prevent issues like https://github.com/NixOS/nixpkgs/issues/280881.
+  # In particular, this package should be updated only after the update in
+  # question lands in Debian testing (see `libguestfs-appliance-debian` package).
   version = "1.54.1";
 
   src = fetchurl {
@@ -195,7 +200,5 @@ stdenv.mkDerivation (finalAttrs: {
       lukts30
     ];
     platforms = lib.platforms.linux;
-    # this is to avoid "output size exceeded"
-    hydraPlatforms = if appliance != null then appliance.meta.hydraPlatforms else lib.platforms.linux;
   };
 })
