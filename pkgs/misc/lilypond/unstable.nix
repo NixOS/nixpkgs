db6d741ca71b112a890db636d7b5dcdd6df362e4
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-17 17:21:21 +0000

    lilypond-unstable: 2.25.22 -> 2.25.24

--- pkgs/misc/lilypond/unstable.nix
+++ pkgs/misc/lilypond/unstable.nix
@@ -5,10 +5,10 @@
 }:
 
 lilypond.overrideAttrs (oldAttrs: rec {
-  version = "2.25.22";
+  version = "2.25.24";
   src = fetchurl {
     url = "https://lilypond.org/download/sources/v${lib.versions.majorMinor version}/lilypond-${version}.tar.gz";
-    hash = "sha256-+p0D//hU/AKVJ6DuPhoK9EOqZxZpTVRugabxidmHYLU=";
+    hash = "sha256-1n6mJBbZcfQYsmrjWxcG/EtIyF9uHNRVT7fX3+zoXc4=";
   };
 
   passthru.updateScript = {
