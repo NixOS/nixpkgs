732fb8c1aec0e9586bcac880fdd97b0217a91613
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-17 04:19:19 +0000

    python312Packages.awscrt: 0.23.9 -> 0.23.10

--- pkgs/development/python-modules/awscrt/default.nix
+++ pkgs/development/python-modules/awscrt/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "awscrt";
-  version = "0.23.9";
+  version = "0.23.10";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-eknHs2t4Yl76gcAvRSVWjobTxI9gavCAhd6oYpdF9jQ=";
+    hash = "sha256-M5S+PraZCZprzX2KcsAxD1Vy4Y2IswNWklZzC+iHjw4=";
   };
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
