efc0aa128b9287af88fb219c582de8b417900154
Malte Voos <git@mal.tc>
2025-02-17 01:10:24 +0100

    libguestfs-with-appliance: use debian-based instead of upstream appliance

--- pkgs/by-name/li/libguestfs-with-appliance/package.nix
+++ pkgs/by-name/li/libguestfs-with-appliance/package.nix
@@ -1,12 +1,16 @@
 {
   lib,
   libguestfs,
-  libguestfs-appliance,
+  libguestfs-appliance-debian,
 }:
 
 # https://github.com/NixOS/nixpkgs/issues/280881
-lib.warnIf (builtins.compareVersions libguestfs.version libguestfs-appliance.version > 0)
-  "libguestfs has a higher version than libguestfs-appliance (${libguestfs.version} > ${libguestfs-appliance.version}), runtime errors may occur!"
+lib.warnIf
+  (
+    builtins.compareVersions libguestfs.version libguestfs-appliance-debian.version != 0
+    && libguestfs.version != "1.54.0" # FIXME(malte-v): remove this clause once libguestfs-appliance-debian is at v1.54.0
+  )
+  "libguestfs has a different version than libguestfs-appliance-debian (${libguestfs.version} != ${libguestfs-appliance-debian.version}), runtime errors may occur!"
 
   libguestfs.override
-  { appliance = libguestfs-appliance; }
+  { appliance = libguestfs-appliance-debian; }
