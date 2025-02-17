6d010bc353b182735b29da72dda250203bde4aeb
Moraxyc <i@qaq.li>
2025-02-17 10:48:58 +0800

    pizauth: 1.0.6 -> 1.0.7

--- pkgs/by-name/pi/pizauth/package.nix
+++ pkgs/by-name/pi/pizauth/package.nix
@@ -7,17 +7,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pizauth";
-  version = "1.0.6";
+  version = "1.0.7";
 
   src = fetchFromGitHub {
     owner = "ltratt";
     repo = "pizauth";
     tag = "pizauth-${version}";
-    hash = "sha256-x3LdutVrQFrkXvbGPVzBV7Y8P9okKgv2rh2YdnDXvsc=";
+    hash = "sha256-lvG50Ej0ius4gHEsyMKOXLD20700mc4iWJxHK5DvYJc=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-5xrPCGvWHceZYPycQdH1wwOH6tmJxHBshOE5866YiKg=";
+  cargoHash = "sha256-WyQIk74AKfsv0noafCGMRS6o+Lq6CeP99AFSdYq+QHg=";
 
   nativeBuildInputs = [ installShellFiles ];
 
