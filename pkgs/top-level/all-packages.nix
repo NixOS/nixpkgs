5aa1fe8ea995ab8baa1147ad99b9fc8842424edc
Mitchell Pleune <mitchpleune@gmail.com>
2025-02-17 18:10:59 +0000

    mxnet: remove cuda support
    mxnet is unmaintained, and requires cuda<=11 to compile. Removed
    support completely as most projects are migrating away from mxnet, and
    nixpkgs will soon stop supporting these old cuda versions.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17313,9 +17313,7 @@ with pkgs;
     lisp-compiler = ecl;
   };
 
-  mxnet = callPackage ../applications/science/math/mxnet {
-    inherit (linuxPackages) nvidia_x11;
-  };
+  mxnet = callPackage ../applications/science/math/mxnet { };
 
   wxmaxima = callPackage ../applications/science/math/wxmaxima {
     wxGTK = wxGTK32.override {
