8f82a9a5edebca9a3f8bc4aed7c09331de8b9d5a
RatCornu <balthazar.patiachvili@crans.org>
2025-02-18 19:46:57 +0100

    nixos/szurubooru: init at 2.5-unstable-2025-02-11

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1603,6 +1603,7 @@
   ./services/web-apps/sogo.nix
   ./services/web-apps/stirling-pdf.nix
   ./services/web-apps/stash.nix
+  ./services/web-apps/szurubooru.nix
   ./services/web-apps/trilium.nix
   ./services/web-apps/tt-rss.nix
   ./services/web-apps/vikunja.nix
