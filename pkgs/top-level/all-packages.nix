117fb2d1f23163e32bc5683cbeeebc0f321176af
Randy Eckenrode <randy@largeandhighquality.com>
2025-02-18 11:25:23 -0500

    Revert "icu: make darwin.ICU the default on Darwin"
    This reverts commit d85513502835d4ab055c567b0d17e91347221ce7.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9231,12 +9231,7 @@ with pkgs;
     icu76
   ;
 
-  # Use Apple’s fork of ICU by default, which provides additional APIs that are not present in upstream ICU.
-  #
-  # `icuReal` is provided in case the upstream icu package is needed on Darwin instead of the fork.
-  # Note that the versioned icu packages always correspond to the upstream versions.
-  icuReal = icu76;
-  icu = if stdenv.hostPlatform.isDarwin then darwin.ICU else icuReal;
+  icu = icu76;
 
   idasen = with python3Packages; toPythonApplication idasen;
 
