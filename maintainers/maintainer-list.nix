afca0bd1b938d5140c5dbfa1c9bfd95ff4303f79
jaroslavpesek <jaroslav.pesek@fit.cvut.cz>
2025-02-18 16:50:02 +0100

    maintainers: add jaroslavpesek

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10551,6 +10551,11 @@
     github = "jaredmontoya";
     githubId = 49511278;
   };
+  jaroslavpesek = {
+    name = "Jaroslav Pesek";
+    github = "jaroslavpesek";
+    githubId = 81391249;
+  };
   jasoncarr = {
     email = "jcarr250@gmail.com";
     github = "jasoncarr0";
