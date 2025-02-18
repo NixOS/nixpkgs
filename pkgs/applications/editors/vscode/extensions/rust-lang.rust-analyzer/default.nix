84aa895ddfb359f7385bb9b3e5379614cf170077
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-02-18 08:54:33 +0100

    vscode-extensions.rust-lang.rust-analyzer: move to buildNpmPackage
    The existing node2nix-based build was broken with #382557. I could not
    salvage it, however apparently it is considered the deprecated way of
    building node packages in nixpkgs.
    
    This commit moves rust-analyzer’s vsix build to buildNpmPackage.
    
    Fixes: #382845
    Part-Of: #229475

--- pkgs/applications/editors/vscode/extensions/rust-lang.rust-analyzer/default.nix
+++ pkgs/applications/editors/vscode/extensions/rust-lang.rust-analyzer/default.nix
@@ -1,10 +1,11 @@
 {
+  pkgsBuildBuild,
   lib,
   fetchFromGitHub,
   vscode-utils,
   jq,
   rust-analyzer,
-  nodePackages,
+  buildNpmPackage,
   moreutils,
   esbuild,
   pkg-config,
@@ -30,19 +31,14 @@ let
     hash = "sha256-STmaV9Zu74QtkGGrbr9uMhskwagfCjJqOAYapXabiuk=";
   };
 
-  build-deps =
-    nodePackages."rust-analyzer-build-deps-../../applications/editors/vscode/extensions/rust-lang.rust-analyzer/build-deps";
-  # FIXME: Making a new derivation to link `node_modules` and run `npm run package`
-  # will cause a build failure.
-  vsix = build-deps.override {
+  vsix = buildNpmPackage {
+    inherit pname releaseTag;
+    version = lib.trim (lib.readFile ./version.txt);
     src = "${src}/editors/code";
-    outputs = [
-      "vsix"
-      "out"
+    npmDepsHash = "sha256-EtkgnNOAKDQP7BDHI667SPu73tYrz1Hq6TmeeObXnf4=";
+    buildInputs = [
+      pkgsBuildBuild.libsecret
     ];
-
-    inherit releaseTag;
-
     nativeBuildInputs =
       [
         jq
@@ -50,7 +46,6 @@ let
         esbuild
         # Required by `keytar`, which is a dependency of `vsce`.
         pkg-config
-        libsecret
       ]
       ++ lib.optionals stdenv.hostPlatform.isDarwin [
         darwin.apple_sdk.frameworks.AppKit
@@ -58,7 +53,7 @@ let
       ];
 
     # Follows https://github.com/rust-lang/rust-analyzer/blob/41949748a6123fd6061eb984a47f4fe780525e63/xtask/src/dist.rs#L39-L65
-    postRebuild = ''
+    installPhase = ''
       jq '
         .version = $ENV.version |
         .releaseTag = $ENV.releaseTag |
@@ -66,10 +61,11 @@ let
         walk(del(.["$generated-start"]?) | del(.["$generated-end"]?))
       ' package.json | sponge package.json
 
-      mkdir -p $vsix
-      npx vsce package -o $vsix/${pname}.zip
+      mkdir -p $out
+      npx vsce package -o $out/${pname}.zip
     '';
   };
+
 in
 vscode-utils.buildVscodeExtension {
   inherit version vsix pname;
