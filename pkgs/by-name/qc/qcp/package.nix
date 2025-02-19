f3cdeae20c0c967b748f95e453d36ba84f5a7879
terrorbyte <terrorbyte@users.noreply.github.com>
2025-02-19 09:20:23 -0700

    switch qcp to tag based vs rev based

--- pkgs/by-name/qc/qcp/package.nix
+++ pkgs/by-name/qc/qcp/package.nix
@@ -1,7 +1,9 @@
 {
   lib,
-  fetchFromGitHub,
   rustPlatform,
+  fetchFromGitHub,
+  versionCheckHook,
+  nix-update-script,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -10,24 +12,13 @@ rustPlatform.buildRustPackage rec {
 
   src = fetchFromGitHub {
     owner = "crazyscot";
-    repo = pname;
-    rev = "v${version}";
+    repo = "qcp";
+    tag = "v${version}";
     hash = "sha256-9nJ01OPAU60veLpL2BlSSUTMu/xdUBDVkV0YEFNQ3FU=";
   };
 
-  cargoHash = "sha256-7LfwJa64ZUE0i8/bMecMoxzykJvuINzXctopyh4Qlak=";
-
-  checkFlags = [
-    # SSH home directory tests will not work in nix sandbox
-    "--skip=config::ssh::includes::test::home_dir"
-  ];
-
-  meta = {
-    description = "The QUIC Copier (qcp) is an experimental high-performance remote file copy utility for long-distance internet connections.";
-    homepage = "https://github.com/crazyscot/qcp";
-    license = lib.licenses.agpl3Only;
-    maintainers = with lib.maintainers; [ poptart ];
-  };
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-vVlwhXaCumgwTcHjnGwD6mi+ZtZvqmtCWukQaPBQcsA=";
 
   postInstall = ''
     install -Dm644 $src/misc/qcp.1 $out/share/man/man1/qcp.1
@@ -37,4 +28,24 @@ rustPlatform.buildRustPackage rec {
     install -Dm644 $src/README.md $out/share/doc/qcp/README.md
     install -Dm644 $src/LICENSE $out/share/doc/qcp/LICENSE
   '';
+
+  checkFlags = [
+    # SSH home directory tests will not work in nix sandbox
+    "--skip=config::ssh::includes::test::home_dir"
+  ];
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+  versionCheckProgramArg = "--version";
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Experimental high-performance remote file copy utility for long-distance internet connections";
+    homepage = "https://github.com/crazyscot/qcp";
+    changelog = "https://github.com/crazyscot/qcp/releases/tag/v{version}";
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ poptart ];
+    mainProgram = "qcp";
+  };
 }
