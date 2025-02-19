394c14c5c8bc668e584240c404f176f285d7be6e
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-19 04:41:18 +0000

    earthlyls: 0.5.3 -> 0.5.5

--- pkgs/by-name/ea/earthlyls/package.nix
+++ pkgs/by-name/ea/earthlyls/package.nix
@@ -10,17 +10,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "earthlyls";
-  version = "0.5.3";
+  version = "0.5.5";
 
   src = fetchFromGitHub {
     owner = "glehmann";
     repo = "earthlyls";
     rev = version;
-    hash = "sha256-wn+6Aa4xTC5o4S+N7snB/vlyw0i23XkmaXUmrhfXuaA=";
+    hash = "sha256-GnFzfCjT4kjb9WViKIFDkIU7zVpiI6HDuUeddgHGQuc=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-Xc1m5WpZEKBP8fGtrQmwHFdqxdQ6wZzgSihSmXnLOhE=";
+  cargoHash = "sha256-sWbYN92Jfr/Pr3qoHWkew/ASIdq8DQg0WHpdyklGBLo=";
 
   passthru = {
     updateScript = nix-update-script { };
