ae35385e0fa7c898284cb0183c33d8f5d1bdd7ac
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-19 21:33:11 +0000

    garnet: 1.0.55 -> 1.0.57

--- pkgs/by-name/ga/garnet/package.nix
+++ pkgs/by-name/ga/garnet/package.nix
@@ -8,13 +8,13 @@
 
 buildDotnetModule rec {
   pname = "garnet";
-  version = "1.0.55";
+  version = "1.0.57";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "garnet";
     tag = "v${version}";
-    hash = "sha256-yeaSSQXsTw6JsF2ugCRSIWCSwuOYuWLErb77/CBraQE=";
+    hash = "sha256-rkU9Fb/CAVG920sYv96JizygPj9vuNL/1x3jVVIfBoo=";
   };
 
   projectFile = "main/GarnetServer/GarnetServer.csproj";
