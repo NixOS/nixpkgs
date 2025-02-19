49da8af214355ea99575d58e4293ced364e16724
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-19 18:34:01 +0000

    python312Packages.discogs-client: 2.7.1 -> 2.8

--- pkgs/development/python-modules/discogs-client/default.nix
+++ pkgs/development/python-modules/discogs-client/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "discogs-client";
-  version = "2.7.1";
+  version = "2.8";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "joalla";
     repo = "discogs_client";
     tag = "v${version}";
-    hash = "sha256-3MwGuYh4Jp/T7LNd5QjajHScdJJOa+VRXmKwFVqfURU=";
+    hash = "sha256-2mMBfOM5sOJsuoxrT3Ku99zDQ8wDw12zRloRLHRDRL0=";
   };
 
   propagatedBuildInputs = [
@@ -36,7 +36,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Unofficial Python API client for Discogs";
     homepage = "https://github.com/joalla/discogs_client";
-    changelog = "https://github.com/joalla/discogs_client/releases/tag/v${version}";
+    changelog = "https://github.com/joalla/discogs_client/releases/tag/${src.tag}";
     license = licenses.bsd2;
     maintainers = with maintainers; [ fab ];
   };
