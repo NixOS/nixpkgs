946b6c921f50b0c2f54f0dba8a2480e62b8d8b83
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-19 11:34:04 +0100

    python312Packages.pygithub: 2.5.0 -> 2.6.0
    Diff: https://github.com/PyGithub/PyGithub/compare/refs/tags/v2.5.0...v2.6.0
    
    Changelog: https://github.com/PyGithub/PyGithub/raw/v2.6.0/doc/changes.rst

--- pkgs/development/python-modules/pygithub/default.nix
+++ pkgs/development/python-modules/pygithub/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "pygithub";
-  version = "2.5.0";
+  version = "2.6.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "PyGithub";
     repo = "PyGithub";
     tag = "v${version}";
-    hash = "sha256-rKa441hiO4M/sC6wxU9yeCu6IQJMZroyi7bad/j6brw=";
+    hash = "sha256-/j90wLVZyOpukRAWO368+LxoKgnKTOs+gBc2SseNXRg=";
   };
 
   build-system = [
