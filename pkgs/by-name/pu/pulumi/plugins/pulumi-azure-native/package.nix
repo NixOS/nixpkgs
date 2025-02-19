d05a8167c1313958006f2ad6d7ce68b7792c7404
Ivan Trubach <mr.trubach@icloud.com>
2025-02-19 19:06:30 +0300

    pulumiPackages.pulumi-azure-native: 2.13.0 -> 2.88.0

--- pkgs/by-name/pu/pulumi/plugins/pulumi-azure-native/package.nix
+++ pkgs/by-name/pu/pulumi/plugins/pulumi-azure-native/package.nix
@@ -1,44 +1,133 @@
 {
   lib,
-  mkPulumiPackage,
+  fetchFromGitHub,
+  buildGoModule,
+  buildPackages,
+  python3Packages,
+  testResourceSchema,
+  pulumi,
+  pulumi-nodejs,
+  pulumi-python,
+  pulumi-azure-native,
+  nix-update-script,
 }:
-mkPulumiPackage rec {
-  owner = "pulumi";
-  repo = "pulumi-azure-native";
-  version = "2.13.0";
-  rev = "v${version}";
-  hash = "sha256-YyJxACeXyY7hZkTbLXT/ASNWa1uv9h3cvPoItR183fU=";
-  vendorHash = "sha256-20wHbNE/fenxP9wgTSzAnx6b1UYlw4i1fi6SesTs0sc=";
-  cmdGen = "pulumi-gen-azure-native";
-  cmdRes = "pulumi-resource-azure-native";
-  extraLdflags = [
-    "-X github.com/pulumi/${repo}/v2/provider/pkg/version.Version=${version}"
+buildGoModule rec {
+  pname = "pulumi-azure-native";
+  version = "2.88.0";
+
+  outputs = [
+    "out"
+    "sdk"
   ];
+
+  src = fetchFromGitHub {
+    owner = "pulumi";
+    repo = "pulumi-azure-native";
+    tag = "v${version}";
+    hash = "sha256-elhORziQGE6c6feq5Wu7KxTaQDuZfRtLy2Xrc4ibsR4=";
+    fetchSubmodules = true;
+  };
+
+  patches = [
+    ./specs-path.patch
+  ];
+
+  sourceRoot = "${src.name}/provider";
+
+  vendorHash = "sha256-MBlm5FThKke61t47mbsLlG7ygmTd6qZ9sLRBjmTEjdk=";
+
+  nativeBuildInputs = [
+    pulumi
+    pulumi-nodejs
+    pulumi-python
+  ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=github.com/pulumi/pulumi-azure-native/v2/provider/pkg/version.Version=${version}"
+  ];
+
+  checkFlags = [
+    # Skip integration tests.
+    # https://github.com/pulumi/pulumi-azure-native/blob/ef5931469519463ce7053e34b40fa6e969bd0302/provider/pkg/provider/provider_e2e_test.go#L149
+    "-short"
+  ];
+
+  excludedPackages = [
+    "cmd/pulumi-gen-azure-native"
+  ];
+
+  codegen = buildPackages.buildGoModule {
+    pname = "pulumi-gen-azure-native";
+    inherit
+      src
+      patches
+      version
+      sourceRoot
+      vendorHash
+      ldflags
+      ;
+    # https://github.com/pulumi/pulumi-azure-native/blob/ef5931469519463ce7053e34b40fa6e969bd0302/Makefile#L314
+    postConfigure = ''
+      pushd ..
+      cp -T \
+        "versions/v''${version%%.*}.yaml" \
+        provider/pkg/versionLookup/default-versions.yaml
+      popd
+    '';
+    subPackages = [ "cmd/pulumi-gen-azure-native" ];
+  };
+
+  postUnpack = ''
+    chmod -R +w "$sourceRoot"/..
+  '';
+
+  # https://github.com/pulumi/pulumi-azure-native/blob/ef5931469519463ce7053e34b40fa6e969bd0302/Makefile#L308
   postConfigure = ''
     pushd ..
-
-    chmod +w . provider/cmd/${cmdRes} sdk/
-    chmod -R +w reports/ versions/
-    mkdir bin
-    ${cmdGen} schema ${version}
-
-    cp bin/schema-full.json provider/cmd/${cmdRes}
-    cp bin/metadata-compact.json provider/cmd/${cmdRes}
-
+    rm provider/cmd/pulumi-resource-azure-native/schema.json
+    "$codegen"/bin/pulumi-gen-azure-native schema
+    cp -t provider/cmd/pulumi-resource-azure-native \
+      bin/schema-full.json bin/metadata-compact.json
+    cp -T \
+      "versions/v''${version%%.*}.yaml" \
+      provider/pkg/versionLookup/default-versions.yaml
     popd
-
-    VERSION=v${version} go generate cmd/${cmdRes}/main.go
   '';
-  fetchSubmodules = true;
+
+  postInstall = ''
+    pushd ..
+    "$codegen"/bin/pulumi-gen-azure-native go
+    mkdir -p "$sdk/go"
+    cp -r -T "sdk/pulumi-azure-native-sdk" "$sdk/go"
+    for lang in nodejs python; do
+      pulumi package gen-sdk bin/schema-full.json \
+        --language "$lang" --version "$version" --out "$sdk"
+    done
+    popd
+    cp -t "$sdk/python" ../README.md
+  '';
+
   __darwinAllowLocalNetworking = true;
-  meta = with lib; {
-    description = "Native Azure Pulumi Provider";
+
+  passthru.tests.schema = testResourceSchema {
+    package = pulumi-azure-native;
+  };
+
+  passthru.sdks.python = python3Packages.pulumi-azure-native;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Native Pulumi package for creating and managing Azure resources";
     mainProgram = "pulumi-resource-azure-native";
     homepage = "https://github.com/pulumi/pulumi-azure-native";
-    license = licenses.asl20;
-    maintainers = with maintainers; [
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
       veehaitch
       trundle
+      tie
     ];
   };
 }
