55e531a5e579b0af9c925d2375850b3c0e36fd7d
Ivan Trubach <mr.trubach@icloud.com>
2025-02-19 18:24:11 +0300

    pulumi: move withPackages implementation to separate file
    Also do not set LD_LIBRARY_PATH for wrapper. Pulumi and its plugins are
    self-contained executables, so it does not make sense to set it.

--- pkgs/by-name/pu/pulumi/package.nix
+++ pkgs/by-name/pu/pulumi/package.nix
@@ -7,8 +7,7 @@
   git,
   buildPackages,
   # passthru
-  runCommand,
-  makeWrapper,
+  callPackage,
   testers,
   pulumi,
   pulumiPackages,
@@ -118,18 +117,7 @@ buildGoModule rec {
 
   passthru = {
     pkgs = pulumiPackages;
-    withPackages =
-      f:
-      runCommand "${pulumi.name}-with-packages"
-        {
-          nativeBuildInputs = [ makeWrapper ];
-        }
-        ''
-          mkdir -p $out/bin
-          makeWrapper ${pulumi}/bin/pulumi $out/bin/pulumi \
-            --suffix PATH : ${lib.makeBinPath (f pulumiPackages)} \
-            --set LD_LIBRARY_PATH "${lib.getLib stdenv.cc.cc}/lib"
-        '';
+    withPackages = callPackage ./with-packages.nix { };
     tests = {
       version = testers.testVersion {
         package = pulumi;
