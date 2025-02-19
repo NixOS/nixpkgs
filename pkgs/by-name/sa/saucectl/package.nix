996381c6713cf4e58b35c9ba865a775bbe8a0e55
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2025-02-19 17:17:27 +0100

    saucectl: 0.193.0 -> 0.193.1

--- pkgs/by-name/sa/saucectl/package.nix
+++ pkgs/by-name/sa/saucectl/package.nix
@@ -5,7 +5,7 @@
 }:
 let
   pname = "saucectl";
-  version = "0.193.0";
+  version = "0.193.1";
 in
 buildGoModule {
   inherit pname version;
@@ -14,7 +14,7 @@ buildGoModule {
     owner = "saucelabs";
     repo = "saucectl";
     tag = "v${version}";
-    hash = "sha256-f4OtXjqtqcMIIssXq5UjPX450Ji2objhOcgm82m52V0=";
+    hash = "sha256-LlDI1gikyInQuwGbR4QyVJ/JUnfkWXsmA+cuhVQEiMc=";
   };
 
   ldflags = [
