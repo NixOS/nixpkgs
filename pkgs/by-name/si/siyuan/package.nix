1da88a3301c559a67511d8cd0f870470fa86a23f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2025-02-19 20:22:50 +1100

    siyuan: unpin go

--- pkgs/by-name/si/siyuan/package.nix
+++ pkgs/by-name/si/siyuan/package.nix
@@ -2,7 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  buildGo123Module,
+  buildGoModule,
   replaceVars,
   pandoc,
   nodejs,
@@ -44,7 +44,7 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-i02WcXN6IKD4N0z8qMKBvxpzI3rrlWRVSH+7cvlRndw=";
   };
 
-  kernel = buildGo123Module {
+  kernel = buildGoModule {
     name = "${finalAttrs.pname}-${finalAttrs.version}-kernel";
     inherit (finalAttrs) src;
     sourceRoot = "${finalAttrs.src.name}/kernel";
