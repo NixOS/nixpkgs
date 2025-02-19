729218b2014bce491963b1eff3da6d054c8a13c7
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2025-02-19 21:04:13 +1100

    atlas: squash

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1900,8 +1900,6 @@ with pkgs;
 
   asmrepl = callPackage ../development/interpreters/asmrepl { };
 
-  atlas = callPackage ./../by-name/at/atlas/package.nix { /* .. */ };
-
   avahi = callPackage ../development/libraries/avahi { };
 
   avahi-compat = callPackage ../development/libraries/avahi {
