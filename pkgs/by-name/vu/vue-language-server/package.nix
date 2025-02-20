89888968cffccdddfe78427901475f4f3ef380da
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-20 11:08:42 +0000

    vue-language-server: 2.2.0 -> 2.2.2

--- pkgs/by-name/vu/vue-language-server/package.nix
+++ pkgs/by-name/vu/vue-language-server/package.nix
@@ -6,14 +6,14 @@
 
 buildNpmPackage rec {
   pname = "vue-language-server";
-  version = "2.2.0";
+  version = "2.2.2";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@vue/language-server/-/language-server-${version}.tgz";
-    hash = "sha256-foWKEhK8YE4ZsbejJUKKPNR+G7ZrJWIPEk/1PVe6YRo=";
+    hash = "sha256-lQvyGh3mvw7VjKDdNYyY1v2kL6tYSiOWD7DBax21BZQ=";
   };
 
-  npmDepsHash = "sha256-4wpVJt6C4Yt53NWWq3MZLtzX+Spf3by4bM6hjmF2y6A=";
+  npmDepsHash = "sha256-Vnlv4gJZPTPLKfCp0elfsVZulFdz5k6n9aA3x82rKMQ=";
 
   postPatch = ''
     ln -s ${./package-lock.json} package-lock.json
