a3374b2d94b0d78fc5300880a97ce0084cbeaa63
gileri <e+pgp9225@linuxw.info>
2025-02-20 21:23:54 +0100

    tt-rss: Fix feed icons
    (cherry picked from commit 4f9bd2b00f52a9dbd855397dc87e5df04b0698f5)

--- nixos/modules/services/web-apps/tt-rss.nix
+++ nixos/modules/services/web-apps/tt-rss.nix
@@ -619,8 +619,10 @@ in
             index = "index.php";
           };
 
-          locations."^~ /feed-icons" = {
-            root = "${cfg.root}";
+          # some clients might still access this old path directly, forward them to the API instead
+          # e.g. https://apps.apple.com/de/app/tiny-reader-rss/id689519762 at version 2.2.0
+          locations."~* /feed-icons/(\\d+)\\.ico" = {
+            return = "302 /public.php?op=feed_icon&id=$1";
           };
 
           locations."~ \\.php$" = {
@@ -641,7 +643,7 @@ in
       "d '${cfg.root}/cache/upload' 0755 ${cfg.user} tt_rss - -"
       "d '${cfg.root}/cache/images' 0755 ${cfg.user} tt_rss - -"
       "d '${cfg.root}/cache/export' 0755 ${cfg.user} tt_rss - -"
-      "d '${cfg.root}/feed-icons' 0755 ${cfg.user} tt_rss - -"
+      "d '${cfg.root}/cache/feed-icons' 0755 ${cfg.user} tt_rss - -"
       "L+ '${cfg.root}/www' - - - - ${servedRoot}"
     ];
 
