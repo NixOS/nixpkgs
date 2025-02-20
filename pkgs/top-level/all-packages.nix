d7629f126f0d49d63772c782c74bac97fe2e2c15
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2025-02-20 10:32:34 -0500

    sparrow: drop fhsenv
    The sparrow package will be modified to provide the path
    to the pcsclite library, to the JVM. Therefore, the fhsenv
    will no longer be necessary.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4997,8 +4997,6 @@ with pkgs;
     openjdk = jdk23.override { enableJavaFX = true; };
   };
 
-  sparrow = callPackage ../applications/blockchains/sparrow/fhsenv.nix { };
-
   steck = callPackage ../servers/pinnwand/steck.nix { };
 
   stm32loader = with python3Packages; toPythonApplication stm32loader;
