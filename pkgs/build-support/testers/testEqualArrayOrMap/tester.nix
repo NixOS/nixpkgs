731b74db8b1e2e55147e23b66ea248c9cdf1abd8
Connor Baker <ConnorBaker01@gmail.com>
2025-02-20 22:37:59 +0000

    testers.testEqualArrayOrMap: use buildCommandPath and change checkSetupScript argument to script

--- pkgs/build-support/testers/testEqualArrayOrMap/tester.nix
+++ pkgs/build-support/testers/testEqualArrayOrMap/tester.nix
@@ -3,12 +3,10 @@
 # our negative tests will not work. See ./tests.nix for more information.
 {
   lib,
-  runCommand,
+  stdenvNoCC,
 }:
 let
-  inherit (lib) maintainers;
   inherit (lib.customisation) makeOverridable;
-  inherit (lib.strings) optionalString;
 
   testEqualArrayOrMap =
     {
@@ -17,76 +15,25 @@ let
       valuesMap ? null,
       expectedArray ? null,
       expectedMap ? null,
-      checkSetupScript ? ''
-        nixErrorLog "no checkSetupScript provided!"
-        exit 1
-      '',
+      script,
     }:
-    runCommand name
-      {
-        __structuredAttrs = true;
-        strictDeps = true;
+    stdenvNoCC.mkDerivation (finalAttrs: {
+      __structuredAttrs = true;
+      strictDeps = true;
 
-        nativeBuildInputs = [
-          ./assert-equal-array.sh
-          ./assert-equal-map.sh
-        ];
+      inherit name;
 
-        inherit valuesArray valuesMap;
-        inherit expectedArray expectedMap;
+      nativeBuildInputs = [
+        ./assert-equal-array.sh
+        ./assert-equal-map.sh
+      ];
 
-        preCheckSetupScript =
-          optionalString (expectedArray == null && expectedMap == null) ''
-            nixErrorLog "neither expectedArray nor expectedMap were set, so test is meaningless!"
-            exit 1
-          ''
-          + optionalString (valuesArray != null) ''
-            nixLog "using valuesArray: $(declare -p valuesArray)"
-          ''
-          + optionalString (valuesMap != null) ''
-            nixLog "using valuesMap: $(declare -p valuesMap)"
-          ''
-          + optionalString (expectedArray != null) ''
-            nixLog "using expectedArray: $(declare -p expectedArray)"
-            declare -ag actualArray
-          ''
-          + optionalString (expectedMap != null) ''
-            nixLog "using expectedMap: $(declare -p expectedMap)"
-            declare -Ag actualMap
-          '';
+      inherit valuesArray valuesMap;
+      inherit expectedArray expectedMap;
 
-        # NOTE:
-        # The singular task of checkSetupScript is to populate actualArray or actualMap. To do this, checkSetupScript
-        # may access valuesArray, valuesMap, actualArray, and actualMap, but should *never* access or modify expectedArray,
-        # or expectedMap.
-        inherit checkSetupScript;
+      inherit script;
 
-        postCheckSetupScript =
-          optionalString (expectedArray != null) ''
-            nixLog "using actualArray: $(declare -p actualArray)"
-            nixLog "comparing actualArray against expectedArray"
-            assertEqualArray expectedArray actualArray
-            nixLog "actualArray matches expectedArray"
-          ''
-          + optionalString (expectedMap != null) ''
-            nixLog "using actualMap: $(declare -p actualMap)"
-            nixLog "comparing actualMap against expectedMap"
-            assertEqualMap expectedMap actualMap
-            nixLog "actualMap matches expectedMap"
-          '';
-      }
-      ''
-        nixLog "running preCheckSetupScript"
-        runHook preCheckSetupScript
-
-        nixLog "running checkSetupScript"
-        runHook checkSetupScript
-
-        nixLog "running postCheckSetupScript"
-        runHook postCheckSetupScript
-
-        nixLog "test passed"
-        touch "$out"
-      '';
+      buildCommandPath = ./build-command.sh;
+    });
 in
 makeOverridable testEqualArrayOrMap
