a8f2557c194f09e679ff213f11d0eccca3751f76
Defelo <mail@defelo.de>
2025-02-20 14:55:53 +0100

    python312Packages.rembg, rembg: init at 2.0.65

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14737,6 +14737,8 @@ self: super: with self; {
 
   remarshal = callPackage ../development/python-modules/remarshal { };
 
+  rembg = callPackage ../development/python-modules/rembg { };
+
   remctl = callPackage ../development/python-modules/remctl { remctl-c = pkgs.remctl; };
 
   remi = callPackage ../development/python-modules/remi { };
