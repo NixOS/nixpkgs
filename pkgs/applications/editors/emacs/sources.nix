e4422e73eba5b79f17596d02fd50d1fbe2160671
pancho horrillo <pancho@pancho.name>
2025-02-20 10:42:15 +0100

    emacs30: 30.0.93 -> 30.1-rc1
    Announcement: https://lists.gnu.org/archive/html/info-gnu-emacs/2025-02/msg00008.html
    
    Diff: https://github.com/emacs-mirror/emacs/compare/emacs-30.0.93...emacs-30.1-rc1

--- pkgs/applications/editors/emacs/sources.nix
+++ pkgs/applications/editors/emacs/sources.nix
@@ -146,10 +146,10 @@ in
 
   emacs30 = import ./make-emacs.nix (mkArgs {
     pname = "emacs";
-    version = "30.0.93";
+    version = "30.1-rc1";
     variant = "mainline";
-    rev = "30.0.93";
-    hash = "sha256-lcpB4lKD3tjvCn646hUyAskxWNCQwEJ0RX/ZddRBRRU=";
+    rev = "30.1-rc1";
+    hash = "sha256-eUEeSCr58uVPxTJMhRZvVSEbfrJ6n59iuYxR6LQkWJw=";
     patches = fetchpatch: [
       (builtins.path {
         name = "inhibit-lexical-cookie-warning-67916.patch";
