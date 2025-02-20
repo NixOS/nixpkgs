1622811843d560cbb5121b28b5ba8e606bac8b7b
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-20 04:50:09 +0000

    kamp: 0.2.2 -> 0.2.3

--- pkgs/by-name/ka/kamp/package.nix
+++ pkgs/by-name/ka/kamp/package.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kamp";
-  version = "0.2.2";
+  version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "vbauerster";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-JZe8z6OYJ+I0+dcq+sCoQPzlz6eB4z98jWj8MDXdODY=";
+    hash = "sha256-coBKVqSqYBpf0PdWKIODnbfQxbOyp5Di45+O66ZGK1Q=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-QGTa2emaz3Nzoe3UuvbKCoopOoKD29RSWPG5wvPe+RE=";
+  cargoHash = "sha256-+Jc3+3sN+muUk7yGZ0sDWR0xAwffZN14X0mcyF4EY20=";
 
   postInstall = ''
     install scripts/* -Dt $out/bin
