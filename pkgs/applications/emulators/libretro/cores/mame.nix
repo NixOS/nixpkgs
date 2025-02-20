28f4195aa8940be0bf6cf0b2f58a4a914c1d5a13
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-20 08:08:59 +0000

    libretro.mame: 0-unstable-2025-01-04 -> 0-unstable-2025-02-19

--- pkgs/applications/emulators/libretro/cores/mame.nix
+++ pkgs/applications/emulators/libretro/cores/mame.nix
@@ -9,13 +9,13 @@
 }:
 mkLibretroCore {
   core = "mame";
-  version = "0-unstable-2025-01-04";
+  version = "0-unstable-2025-02-19";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "mame";
-    rev = "20db0f242e4e11a476b548dd57d2ef9cc3e84f03";
-    hash = "sha256-+xShU96m+KCHrFleEy55fBD5vCM+hsYMqIvRZQtzsr8=";
+    rev = "0b42e882798b4c815eefcc0d1b6c228e807406eb";
+    hash = "sha256-a3hNvdaq4af4+yF4/3CIu/C+WaQsy3frehRwuRHhtsY=";
     fetchSubmodules = true;
   };
 
