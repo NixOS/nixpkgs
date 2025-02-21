e51bee0af3b9cc0d661934a4dabe0a1c25bac0ba
Bonus <root@bonusplay.pl>
2025-02-21 21:40:03 +0100

    nixos/mautrix-slack: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -744,6 +744,7 @@
   ./services/matrix/maubot.nix
   ./services/matrix/mautrix-meta.nix
   ./services/matrix/mautrix-signal.nix
+  ./services/matrix/mautrix-slack.nix
   ./services/matrix/mautrix-telegram.nix
   ./services/matrix/mautrix-whatsapp.nix
   ./services/matrix/mjolnir.nix
