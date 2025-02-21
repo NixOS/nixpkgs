6492405cb0887552c8dca2f3666748ff42edb754
Marcin Serwin <marcin@serwin.dev>
2025-02-21 19:21:47 +0100

    signal-desktop(aarch64-linux): 7.36.0 -> 7.46.0-1 from COPR

--- pkgs/by-name/si/signal-desktop/signal-desktop-aarch64.nix
+++ pkgs/by-name/si/signal-desktop/signal-desktop-aarch64.nix
@@ -1,8 +1,16 @@
 { callPackage }:
 callPackage ./generic.nix { } rec {
   pname = "signal-desktop";
+  version = "7.46.0-1";
+
   dir = "Signal";
-  version = "7.36.0";
-  url = "https://github.com/0mniteck/Signal-Desktop-Mobian/raw/${version}/builds/release/signal-desktop_${version}_arm64.deb";
-  hash = "sha256-nmAqFDw35pdZg5tiq9MUlqXnbRLRkSOX9SWhccnE2Xw=";
+  libdir = "usr/lib64/${pname}";
+  bindir = "usr/bin";
+  extractPkg = ''
+    mkdir -p $out
+    bsdtar -xf $downloadedFile -C "$out"
+  '';
+
+  url = "https://download.copr.fedorainfracloud.org/results/useidel/signal-desktop/fedora-42-aarch64/08759579-signal-desktop/signal-desktop-7.46.0-1.fc42.aarch64.rpm";
+  hash = "sha256-kiNwaB+jNOgkfkJ4V5Fj23RNILP3IOZfKWKAehMmtZ0=";
 }
