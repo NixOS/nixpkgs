008e9aac12b8ede1bb563ef534e76732736ded00
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-21 22:07:50 +0000

    meerk40t: 0.9.5300 -> 0.9.7010

--- pkgs/applications/misc/meerk40t/default.nix
+++ pkgs/applications/misc/meerk40t/default.nix
@@ -9,14 +9,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "MeerK40t";
-  version = "0.9.5300";
+  version = "0.9.7010";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "meerk40t";
     repo = pname;
     tag = version;
-    hash = "sha256-dybmbmEvvTka0wMBIUDYemqDaCvG9odgCbIWYhROJLI=";
+    hash = "sha256-pH1sVY8YVpg4gHU8/K87QcR7bGKgblRp2HpgKkb3+NQ=";
   };
 
   nativeBuildInputs =
@@ -77,7 +77,7 @@ python3Packages.buildPythonApplication rec {
   '';
 
   meta = with lib; {
-    changelog = "https://github.com/meerk40t/meerk40t/releases/tag/${version}";
+    changelog = "https://github.com/meerk40t/meerk40t/releases/tag/${src.tag}";
     description = "MeerK40t LaserCutter Software";
     mainProgram = "meerk40t";
     homepage = "https://github.com/meerk40t/meerk40t";
