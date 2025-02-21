488b08ffa8e26525ffa07a62ca238595f87bc194
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2025-02-21 11:54:42 +0100

    haven: init at 1.0.5

---
+++ pkgs/by-name/ha/haven/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule rec {
+  pname = "haven";
+  version = "1.0.5";
+
+  src = fetchFromGitHub {
+    owner = "bitvora";
+    repo = "haven";
+    rev = "v${version}";
+    sha256 = "sha256-rSycrHW53TgqbsfgaRn3492EWtpu440GtbegozqnzMQ=";
+  };
+
+  vendorHash = "sha256-5d6C2sNG8aCaC+z+hyLgOiEPWP/NmAcRRbRVC4KuCEw=";
+
+  postInstall = ''
+    mkdir -p $out/share/haven
+    cp -r $src/templates $out/share/haven/
+  '';
+
+  meta = with lib; {
+    description = "High Availability Vault for Events on Nostr";
+    homepage = "https://github.com/bitvora/haven";
+    changelog = "https://github.com/bitvora/haven/releases/tag/v${version}";
+    license = licenses.mit;
+    maintainers = with maintainers; [ felixzieger ];
+    mainProgram = "haven";
+  };
+}
