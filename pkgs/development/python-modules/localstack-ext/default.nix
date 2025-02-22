aa06c67367fc55a9d9faa7430d0b8f303e5a4761
Adam C. Stephens <adam@valkor.net>
2025-02-22 00:21:38 -0500

    python3Packages.localstack-ext: 4.0.3 -> 4.1.1

--- pkgs/development/python-modules/localstack-ext/default.nix
+++ pkgs/development/python-modules/localstack-ext/default.nix
@@ -21,13 +21,13 @@
 
 buildPythonPackage rec {
   pname = "localstack-ext";
-  version = "4.0.3";
+  version = "4.1.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "localstack_ext";
     inherit version;
-    hash = "sha256-vivEdEk32wJln8jfhrAtygO5CEvtsdXI7sxrj0dqIdA=";
+    hash = "sha256-Fgblk8eL5JnF4yLeH73yGuOeH9anSu1o9H1UxcjTyco=";
   };
 
   build-system = [
