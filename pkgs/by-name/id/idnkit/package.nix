58a975e193308aaad1fd13aff1ac7fc973f73885
Moraxyc <i@qaq.li>
2025-02-22 22:40:11 +0800

    idnkit: modernize

--- pkgs/by-name/id/idnkit/package.nix
+++ pkgs/by-name/id/idnkit/package.nix
@@ -5,13 +5,13 @@
   libiconv,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "idnkit";
   version = "2.3";
 
   src = fetchurl {
-    url = "https://jprs.co.jp/idn/${pname}-${version}.tar.bz2";
-    sha256 = "0zp9yc84ff5s0g2i6v9yfyza2n2x4xh0kq7hjd3anhh0clbp3l16";
+    url = "https://jprs.co.jp/idn/${finalAttrs.pname}-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-JtBxF2UAQqtGk/DgCWAnXVihvnc+bRPFA7o4RxDz6X4=";
   };
 
   buildInputs = [ libiconv ];
@@ -20,13 +20,13 @@ stdenv.mkDerivation rec {
   #   localconverter.c:602:21/607:26/633:26: error: passing argument 2 of 'iconv' from incompatible pointer type
   env.NIX_CFLAGS_COMPILE = "-Wno-error=incompatible-pointer-types";
 
-  meta = with lib; {
+  meta = {
     homepage = "https://jprs.co.jp/idn/index-e.html";
     description = "Provides functionalities about i18n domain name processing";
     license = {
       fullName = "Open Source Code License version 1.1";
       url = "https://jprs.co.jp/idn/idnkit2-OSCL.txt";
     };
-    platforms = platforms.linux;
+    platforms = lib.platforms.linux;
   };
-}
+})
