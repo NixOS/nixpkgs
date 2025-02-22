4c82997e90415c98cacf0d65c0f3a6e19fa512d3
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-22 14:43:36 +0000

    kdePackages.wayland-protocols: 1.40 -> 1.41

--- pkgs/development/libraries/wayland/protocols.nix
+++ pkgs/development/libraries/wayland/protocols.nix
@@ -14,7 +14,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "wayland-protocols";
-  version = "1.40";
+  version = "1.41";
 
   doCheck =
     stdenv.hostPlatform == stdenv.buildPlatform
@@ -25,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://gitlab.freedesktop.org/wayland/${finalAttrs.pname}/-/releases/${finalAttrs.version}/downloads/${finalAttrs.pname}-${finalAttrs.version}.tar.xz";
-    hash = "sha256-shcReTJHwsQnY5FDkt+p/LnjcoyktKoRCtuNkV/ABok=";
+    hash = "sha256-J4a2sbeZZeMT8sKJwSB1ue1wDUGESBDFGv2hDuMpV2s=";
   };
 
   postPatch = lib.optionalString finalAttrs.doCheck ''
