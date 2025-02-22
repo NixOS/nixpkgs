82b2f479240b418a9b1660a3845b087f110b527c
Moraxyc <i@qaq.li>
2025-02-22 21:54:18 +0800

    hiredis-vip: modernize

--- pkgs/by-name/hi/hiredis-vip/package.nix
+++ pkgs/by-name/hi/hiredis-vip/package.nix
@@ -1,21 +1,21 @@
 {
-  stdenv,
   lib,
+  stdenv,
   fetchFromGitHub,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "hiredis-vip";
   version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "vipshop";
     repo = "hiredis-vip";
-    rev = version;
-    sha256 = "1z9zry635pxqv6d2cgk3dghb5qfpg9m7dz34ld4djw9b53hjr2z2";
+    tag = finalAttrs.version;
+    hash = "sha256-4oss4SgrcdlIo2T8dmp61+Gy4GtjPiaa2bjfMozPP/0=";
   };
 
-  makeFlags = [ "PREFIX=$(out)" ];
+  makeFlags = [ "PREFIX=${placeholder "out"}" ];
 
   # Function are declared after they are used in the file, this is error since gcc-14.
   #   command.c:1668:9: error: implicit declaration of function 'free' [-Wimplicit-function-declaration]
@@ -26,5 +26,4 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/vipshop/hiredis-vip";
     license = lib.licenses.bsd3;
   };
-
-}
+})
