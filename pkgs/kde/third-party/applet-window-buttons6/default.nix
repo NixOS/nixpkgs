bdc52091a501bb5ac978d24cb4762f3b6608d22a
Florian Franzen <Florian.Franzen@gmail.com>
2025-02-22 13:46:00 +0100

    applet-window-buttons6: fix build with plasma 6.3
    Fixes #382742

--- pkgs/kde/third-party/applet-window-buttons6/default.nix
+++ pkgs/kde/third-party/applet-window-buttons6/default.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   cmake,
   extra-cmake-modules,
+  fetchpatch,
   kcoreaddons,
   kdeclarative,
   kdecoration,
@@ -20,6 +21,18 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-S7JcDPo4QDqi/RtvreFNoPKwTg14bgaFGsuGSDxs5nM=";
   };
+
+  patches = [
+    # Needed for the new kdecorations3 (part of plasma 6.3) and should no longer be needed with next release.
+    (fetchpatch {
+      url = "https://github.com/moodyhunter/applet-window-buttons6/commit/326382805641d340c9902689b549e4488682f553.patch";
+      hash = "sha256-xlrmA/SSPH41VE7xjYJ8xQ1EVBnE8jMbwnuDSpKWQPM=";
+    })
+    (fetchpatch {
+      url = "https://github.com/moodyhunter/applet-window-buttons6/commit/e27cd7559581e84b559a5da2c7bc6ea5a3f5bf15.patch";
+      hash = "sha256-1GwZh2ZR9+cB+4ggiwsNN1KT5m8tsi/AEGZK0Cx5sdw=";
+    })
+  ];
 
   dontWrapQtApps = true;
 
