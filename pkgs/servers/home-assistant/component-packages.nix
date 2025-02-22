429d5a2e323517fb00c5c2ae1f2e51730c65bdbb
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-02-22 04:41:56 +0100

    home-assistant: 2025.2.4 -> 2025.2.5
    https://github.com/home-assistant/core/releases/tag/2025.2.5

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -2,7 +2,7 @@
 # Do not edit!
 
 {
-  version = "2025.2.4";
+  version = "2025.2.5";
   components = {
     "3_day_blinds" =
       ps: with ps; [
--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -8,7 +8,7 @@ buildPythonPackage rec {
   # the frontend version corresponding to a specific home-assistant version can be found here
   # https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/frontend/manifest.json
   pname = "home-assistant-frontend";
-  version = "20250214.0";
+  version = "20250221.0";
   format = "wheel";
 
   src = fetchPypi {
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     pname = "home_assistant_frontend";
     dist = "py3";
     python = "py3";
-    hash = "sha256-dIV0P/9GsHV4xN84+HGe/I7ay1FM3jLw9F60WpxaEIM=";
+    hash = "sha256-SgNtsXtg/i9AO+xu5O0loDbAifO4rn1GRFE1LmeJLfU=";
   };
 
   # there is nothing to strip in this package
