c7c3aab35196f275dd7b915839e81920dae2d61b
Moraxyc <i@qaq.li>
2025-02-22 21:47:23 +0800

    hiredis-vip: fix build on gcc-14

--- pkgs/by-name/hi/hiredis-vip/package.nix
+++ pkgs/by-name/hi/hiredis-vip/package.nix
@@ -17,6 +17,10 @@ stdenv.mkDerivation rec {
 
   makeFlags = [ "PREFIX=$(out)" ];
 
+  # Function are declared after they are used in the file, this is error since gcc-14.
+  #   command.c:1668:9: error: implicit declaration of function 'free' [-Wimplicit-function-declaration]
+  env.NIX_CFLAGS_COMPILE = "-Wno-error=implicit-function-declaration";
+
   meta = {
     description = "C client library for the Redis database";
     homepage = "https://github.com/vipshop/hiredis-vip";
