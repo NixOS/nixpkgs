b7d10d6af3417381b54b85f0555174668c9a03f8
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-22 17:04:42 +0000

    flatbuffers: 25.1.24 -> 25.2.10

--- pkgs/development/libraries/flatbuffers/default.nix
+++ pkgs/development/libraries/flatbuffers/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "flatbuffers";
-  version = "25.1.24";
+  version = "25.2.10";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "flatbuffers";
     rev = "v${version}";
-    hash = "sha256-LApjF14eoqcT11Jp9SaJQbtvK8iIelTY4GJKqjVCpCM=";
+    hash = "sha256-u5AVjbep3iWwGNXLrkPJUnF8SbmIXlHOYoy3NIlUl/E=";
   };
 
   nativeBuildInputs = [
