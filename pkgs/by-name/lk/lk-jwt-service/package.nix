db7af8689339eab27b8e1cb0367046bcc307d379
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-22 10:01:06 +0000

    lk-jwt-service: 0.1.1 -> 0.1.2

--- pkgs/by-name/lk/lk-jwt-service/package.nix
+++ pkgs/by-name/lk/lk-jwt-service/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "lk-jwt-service";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "element-hq";
     repo = "lk-jwt-service";
     tag = "v${version}";
-    hash = "sha256-RbfJdAhLz2wfTC17i533U25TIUtJEkHTgqJC2R+j1uM=";
+    hash = "sha256-Jkme+jQgWYSdnOxrYcIh0M6KmwlmpaUGK1vMU3WfdVo=";
   };
 
-  vendorHash = "sha256-7wruthAqC9jVpAhPiIDdqAB51l38fLHEhl2QOaBJiL0=";
+  vendorHash = "sha256-AGkwjzdTjfDA8K6ko24QSSxbTQeFGpu9sv5m8ZCmNJI=";
 
   meta = with lib; {
     description = "Minimal service to issue LiveKit JWTs for MatrixRTC";
