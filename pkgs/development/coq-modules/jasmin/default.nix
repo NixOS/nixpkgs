822a449b230adb821fb5aef3af1e75bb7fa30057
Pierre Roux <pierre.roux@onera.fr>
2025-02-23 16:19:04 +0100

    coqPackages.mathcomp*: update stdlib dependencies

--- pkgs/development/coq-modules/jasmin/default.nix
+++ pkgs/development/coq-modules/jasmin/default.nix
@@ -3,6 +3,7 @@
   mkCoqDerivation,
   coq,
   mathcomp,
+  mathcomp-algebra-tactics,
   mathcomp-word,
   version ? null,
 }:
@@ -23,6 +24,7 @@ mkCoqDerivation {
   release."2024.07.2".sha256 = "sha256-aF8SYY5jRxQ6iEr7t6mRN3BEmIDhJ53PGhuZiJGB+i8=";
 
   propagatedBuildInputs = [
+    mathcomp-algebra-tactics
     mathcomp-word
   ];
 
