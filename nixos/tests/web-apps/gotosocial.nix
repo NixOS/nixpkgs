86ea83a06dd771aa4292d1a9386ee135222a199b
transcaffeine <transcaffeine@finally.coffee>
2025-02-23 15:59:57 +0100

    nixos/tests/gotosocial: adjust for gotosocial 0.18.1+

--- nixos/tests/web-apps/gotosocial.nix
+++ nixos/tests/web-apps/gotosocial.nix
@@ -13,6 +13,7 @@
         settings = {
           host = "localhost:8081";
           port = 8081;
+          instance-stats-mode = "serve";
         };
       };
     };
@@ -21,9 +22,11 @@
     machine.wait_for_unit("gotosocial.service")
     machine.wait_for_unit("postgresql.service")
     machine.wait_for_open_port(8081)
+    # Database migrations are running, wait until gotosocial no longer serves 503
+    machine.wait_until_succeeds("curl -sS -f http://localhost:8081/readyz", timeout=300)
 
     # check user registration via cli
     machine.succeed("gotosocial-admin account create --username nickname --email email@example.com --password kurtz575VPeBgjVm")
-    machine.succeed("curl -sS -f http://localhost:8081/nodeinfo/2.0 | jq '.usage.users.total' | grep -q '^1$'")
+    machine.wait_until_succeeds("curl -sS -f http://localhost:8081/nodeinfo/2.0 | jq '.usage.users.total' | grep -q '^1$'")
   '';
 }
