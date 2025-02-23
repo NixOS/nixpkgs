d9b27d635a5beeeb69ada05440f3904d78abda14
Ivan Trubach <mr.trubach@icloud.com>
2025-02-23 20:52:22 +0300

    pulumiPackages.pulumi-language-python: clean up

--- pkgs/tools/admin/pulumi-packages/pulumi-language-python.nix
+++ pkgs/tools/admin/pulumi-packages/pulumi-language-python.nix
@@ -2,37 +2,46 @@
   lib,
   buildGoModule,
   pulumi,
+  bash,
   python3,
 }:
 buildGoModule rec {
-  inherit (pulumi) version src;
-
   pname = "pulumi-language-python";
+  inherit (pulumi) version src;
 
   sourceRoot = "${src.name}/sdk/python/cmd/pulumi-language-python";
 
   vendorHash = "sha256-Q8nnYJJN5+W2luY8JQJj1X9KIk9ad511FBywr+0wBNg=";
 
-  postPatch = ''
-    substituteInPlace main_test.go \
-      --replace "TestDeterminePulumiPackages" \
-                "SkipTestDeterminePulumiPackages"
-  '';
-
   ldflags = [
     "-s"
     "-w"
-    "-X github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${version}"
+    "-X=github.com/pulumi/pulumi/sdk/v3/go/common/version.Version=${version}"
+  ];
+
+  checkFlags = [
+    "-skip=^${
+      lib.concatStringsSep "$|^" [
+        "TestDeterminePulumiPackages"
+      ]
+    }$"
   ];
 
   nativeCheckInputs = [
     python3
   ];
 
+  # For patchShebangsAuto (see scripts copied in postInstall).
+  buildInputs = [
+    bash
+    python3
+  ];
+
   postInstall = ''
-    cp ../pulumi-language-python-exec           $out/bin
-    cp ../../dist/pulumi-resource-pulumi-python $out/bin
-    cp ../../dist/pulumi-analyzer-policy-python $out/bin
+    cp -t "$out/bin" \
+      ../pulumi-language-python-exec \
+      ../../dist/pulumi-resource-pulumi-python \
+      ../../dist/pulumi-analyzer-policy-python
   '';
 
   meta = {
