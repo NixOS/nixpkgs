4036afc4786ec66361b9faddc6e44fd84771575b
Morgan Jones <me@numin.it>
2025-02-22 20:03:35 -0800

    androidenv: support if-supported for include* options
    This lets us stop hardcoding platforms and start filtering for which
    archives have sources, which is the most accurate way to do it.
    
    Tested on x86_64-linux, aarch64-linux, and aarch64-darwin.

--- pkgs/development/mobile/androidenv/cmake.nix
+++ pkgs/development/mobile/androidenv/cmake.nix
@@ -1,7 +1,7 @@
-{deployAndroidPackage, lib, package, os, autoPatchelfHook, pkgs, stdenv, meta}:
+{deployAndroidPackage, lib, package, os, arch, autoPatchelfHook, pkgs, stdenv, meta}:
 
 deployAndroidPackage {
-  inherit package;
+  inherit package os arch;
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
   buildInputs = lib.optionals (os == "linux") [ pkgs.stdenv.cc.libc pkgs.stdenv.cc.cc pkgs.ncurses5 ];
   patchInstructions = lib.optionalString (os == "linux") ''
