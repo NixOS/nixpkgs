4ff8bff8d9531df13ddcd995bf02a94a7bd82b81
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-02-22 23:46:34 -0500

    python3Packages.urlmatch: init at 1.0.0
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17935,6 +17935,8 @@ self: super: with self; {
 
   urlman = callPackage ../development/python-modules/urlman { };
 
+  urlmatch = callPackage ../development/python-modules/urlmatch { };
+
   urlpy = callPackage ../development/python-modules/urlpy { };
 
   urwid = callPackage ../development/python-modules/urwid { };
