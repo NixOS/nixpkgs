4d282e836609aa1cfcabd188879da1b89f327ba8
emaryn <emaryn@users.noreply.github.com>
2025-02-24 01:24:39 +0800

    ibus-engines.libpinyin: 1.15.8 -> 1.16.0

--- pkgs/tools/inputmethods/ibus-engines/ibus-libpinyin/default.nix
+++ pkgs/tools/inputmethods/ibus-engines/ibus-libpinyin/default.nix
@@ -17,17 +17,18 @@
   opencc,
   libsoup_3,
   json-glib,
+  libnotify,
 }:
 
 stdenv.mkDerivation rec {
   pname = "ibus-libpinyin";
-  version = "1.15.8";
+  version = "1.16.0";
 
   src = fetchFromGitHub {
     owner = "libpinyin";
     repo = "ibus-libpinyin";
     tag = version;
-    hash = "sha256-u21avBSSu/78tLoyFI9XGocC7rT/64L5HqQQj3Zg1Mc=";
+    hash = "sha256-aeyoBfl5x9Jo35vESMmLBbl72Qx5F3JwZHZaBr3c0Jk=";
   };
 
   nativeBuildInputs = [
@@ -59,6 +60,7 @@ stdenv.mkDerivation rec {
     opencc
     libsoup_3
     json-glib
+    libnotify
   ];
 
   meta = {
