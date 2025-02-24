e9492a2a09b3215bc0b68ae67fa04ddb740583fd
Martin Häcker <mhaecker@mac.com>
2025-02-24 07:34:21 +0100

    mumble: enable building on darwin

--- pkgs/applications/networking/mumble/default.nix
+++ pkgs/applications/networking/mumble/default.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   pkg-config,
   qt5,
   cmake,
@@ -24,11 +23,12 @@
   libogg,
   libvorbis,
   stdenv_32bit,
+  alsaSupport ? stdenv.hostPlatform.isLinux,
   iceSupport ? true,
   zeroc-ice,
   jackSupport ? false,
   libjack2,
-  pipewireSupport ? true,
+  pipewireSupport ? stdenv.hostPlatform.isLinux,
   pipewire,
   pulseSupport ? true,
   libpulseaudio,
@@ -36,6 +36,8 @@
   speechd-minimal,
   microsoft-gsl,
   nlohmann_json,
+  xar,
+  makeWrapper,
 }:
 
 let
@@ -57,12 +59,14 @@ let
           qt5.qttools
         ] ++ (overrides.nativeBuildInputs or [ ]);
 
-        buildInputs = [
-          avahi
-          boost
-          poco
-          protobuf
-        ] ++ (overrides.buildInputs or [ ]);
+        buildInputs =
+          [
+            boost
+            poco
+            protobuf
+          ]
+          ++ lib.optionals stdenv.hostPlatform.isLinux [ avahi ]
+          ++ (overrides.buildInputs or [ ]);
 
         cmakeFlags = [
           "-D g15=OFF"
@@ -84,7 +88,7 @@ let
             felixsinger
             lilacious
           ];
-          platforms = platforms.linux;
+          platforms = platforms.linux ++ (overrides.platforms or [ ]);
         };
       }
     );
@@ -94,6 +98,7 @@ let
     generic {
       type = "mumble";
 
+      platforms = lib.platforms.darwin;
       nativeBuildInputs = [ qt5.qttools ];
       buildInputs =
         [
@@ -109,12 +114,15 @@ let
           microsoft-gsl
           nlohmann_json
         ]
-        ++ lib.optional (!jackSupport) alsa-lib
+        ++ lib.optional (!jackSupport && alsaSupport) alsa-lib
         ++ lib.optional jackSupport libjack2
         ++ lib.optional speechdSupport speechd-minimal
         ++ lib.optional pulseSupport libpulseaudio
-        ++ lib.optional pipewireSupport pipewire;
-
+        ++ lib.optional pipewireSupport pipewire
+        ++ lib.optionals stdenv.hostPlatform.isDarwin [
+          xar
+          makeWrapper
+        ];
 
       cmakeFlags = [
         "-D server=OFF"
@@ -131,12 +139,36 @@ let
         (lib.cmakeBool "pulseaudio" pulseSupport)
         (lib.cmakeBool "pipewire" pipewireSupport)
         (lib.cmakeBool "jackaudio" jackSupport)
-        (lib.cmakeBool "alsa" (!jackSupport))
+        (lib.cmakeBool "alsa" (!jackSupport && alsaSupport))
       ];
 
       env.NIX_CFLAGS_COMPILE = lib.optionalString speechdSupport "-I${speechd-minimal}/include/speech-dispatcher";
 
-      postFixup = ''
+      patches = [
+        ./disable-overlay-build.patch
+        ./fix-plugin-copy.patch
+      ];
+
+      postInstall = lib.optionalString stdenv.hostPlatform.isDarwin ''
+        # The build erraneously marks the *.dylib as executable
+        # which causes the qt-hook to wrap it, which then prevents the app from loading it
+        chmod -x $out/lib/mumble/plugins/*.dylib
+
+        # Post-processing for the app bundle
+        $NIX_BUILD_TOP/source/macx/scripts/osxdist.py \
+          --source-dir=$NIX_BUILD_TOP/source/ \
+          --binary-dir=$out \
+          --only-appbundle \
+          --version "${source.version}"
+
+        mkdir -p $out/Applications $out/bin
+        mv $out/Mumble.app $out/Applications/Mumble.app
+
+        # ensure that the app can be started from the shell
+        makeWrapper $out/Applications/Mumble.app/Contents/MacOS/mumble $out/bin/mumble
+      '';
+
+      postFixup = lib.optionalString stdenv.hostPlatform.isLinux ''
         wrapProgram $out/bin/mumble \
           --prefix LD_LIBRARY_PATH : "${
             lib.makeLibraryPath (
@@ -144,6 +176,7 @@ let
             )
           }"
       '';
+
     } source;
 
   server =
