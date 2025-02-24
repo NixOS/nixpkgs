8cf71f1fa8ae1b72ab760b6bc0c31fa8957a3b55
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-24 20:42:28 +0000

    maintainers: add kaeeraa

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11947,6 +11947,12 @@
     githubId = 44864956;
     keys = [ { fingerprint = "3F87 0A7C A7B4 3731 2F13  6083 749F D4DF A2E9 4236"; } ];
   };
+  kaeeraa = {
+    name = "kaeeraa";
+    email = "kaeeraa@nebula-nook.ru";
+    github = "kaeeraa";
+    githubId = 99148867;
+  };
   kaiha = {
     email = "kai.harries@gmail.com";
     github = "KaiHa";
