26c29f9d1b2b92a3d3b58aaf2432fced0a29fec3
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-24 01:11:04 +0000

    gnomeExtensions.easyScreenCast: 1.10.0 -> 1.11.0

--- pkgs/desktops/gnome/extensions/EasyScreenCast/default.nix
+++ pkgs/desktops/gnome/extensions/EasyScreenCast/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-shell-extension-EasyScreenCast";
-  version = "1.10.0";
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "EasyScreenCast";
     repo = "EasyScreenCast";
     rev = finalAttrs.version;
-    hash = "sha256-5PJB+lm4NKeNpS2vg9xaVl5aUR0Rofmt6sEKXfuGG6c=";
+    hash = "sha256-CK9ta+2Kf7IFKb+uQhI1AtdNkJZpBgIL7JDM3JqsV4c=";
   };
 
   patches = [
