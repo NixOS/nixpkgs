b3d860c1b61ddbd2bab65a7402cbe1bb8356b883
Remy van Velthuijsen <remy@remysplace.de>
2025-02-24 22:33:42 +0100

    ddev: install autocompletion scripts

--- pkgs/by-name/dd/ddev/package.nix
+++ pkgs/by-name/dd/ddev/package.nix
@@ -1,4 +1,12 @@
-{ lib, buildGoModule, fetchFromGitHub, testers, ddev }:
+{
+  lib,
+  stdenv,
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  testers,
+  ddev,
+}:
 
 buildGoModule rec {
   pname = "ddev";
@@ -11,6 +19,10 @@ buildGoModule rec {
     hash = "sha256-Bvp1mx07PXnLChmbaiBv2wnoN/oAZED5xQSoKMUo9tY=";
   };
 
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
   vendorHash = null;
 
   ldflags = [
@@ -21,6 +33,17 @@ buildGoModule rec {
 
   # Tests need docker.
   doCheck = false;
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    # DDEV will try to create $HOME/.ddev, so we set $HOME to a temporary
+    # directory.
+    export HOME=$(mktemp -d)
+    $out/bin/ddev_gen_autocomplete
+    installShellCompletion --cmd ddev \
+      --bash .gotmp/bin/completions/ddev_bash_completion.sh \
+      --fish .gotmp/bin/completions/ddev_fish_completion.sh \
+      --zsh .gotmp/bin/completions/ddev_zsh_completion.sh
+  '';
 
   passthru.tests.version = testers.testVersion {
     package = ddev;
