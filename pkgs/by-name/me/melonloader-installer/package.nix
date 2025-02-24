e7b9fda37a00ed4569fa747f23f71ff87ceec1ce
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2025-02-23 18:00:21 -0800

    melonloader-installer: init at 4.1.1 https://github.com/LavaGang/MelonLoader.Installer/releases/tag/4.1.1
    melonloader-installer: implement feedback from nixfmt
    
    melonloader-installer: make platforms a list
    
    melonloader-installer: implement feedback from review

---
+++ pkgs/by-name/me/melonloader-installer/package.nix
@@ -0,0 +1,35 @@
+{
+  fetchFromGitHub,
+  dotnetCorePackages,
+  buildDotnetModule,
+  lib,
+  gitUpdater,
+}:
+buildDotnetModule rec {
+  pname = "melonloader-installer";
+  version = "4.1.1";
+
+  src = fetchFromGitHub {
+    owner = "LavaGang";
+    repo = "MelonLoader.Installer";
+    tag = version;
+    hash = "sha256-1Tdf2xOGBtSW68fycH6Eal4EF7uN2ZAEVBE9st6ruvg=";
+  };
+
+  projectFile = "MelonLoader.Installer/MelonLoader.Installer.csproj";
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
+  nugetDeps = ./deps.json;
+  selfContainedBuild = true;
+
+  passthru.updateScript = gitUpdater { };
+
+  meta = {
+    homepage = "https://melonwiki.xyz";
+    mainProgram = "MelonLoader.Installer.Linux";
+    description = "Automated installer for MelonLoader, the universal mod-loader for games built in the Unity Engine";
+    license = lib.licenses.asl20;
+    platforms = [ "x86_64-linux" ];
+    maintainers = with lib.maintainers; [ WillemToorenburgh ];
+  };
+}
