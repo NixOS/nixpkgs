61f47e0da0b905a3f6be90382fc0d705bd740404
Willem Toorenburgh <willemtoorenburgh@gmail.com>
2025-02-23 17:50:40 -0800

    maintainers: add WillemToorenburgh

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25659,6 +25659,12 @@
     githubId = 5185341;
     name = "Will Cohen";
   };
+  WillemToorenburgh = {
+    email = "willemtoorenburgh@gmail.com";
+    github = "willemtoorenburgh";
+    githubId = 50425141;
+    name = "Willem Toorenburgh";
+  };
   williamvds = {
     email = "nixpkgs@williamvds.me";
     github = "williamvds";
