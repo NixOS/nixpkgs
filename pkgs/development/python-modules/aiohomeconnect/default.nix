f2fd6f72bc08a831599a480493b5f67e16b6eef8
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-24 23:46:51 +0100

    python313Packages.aiohomeconnect: 0.12.3 -> 0.13.0
    Diff: https://github.com/MartinHjelmare/aiohomeconnect/compare/refs/tags/v0.12.3...v0.13.0
    
    Changelog: https://github.com/MartinHjelmare/aiohomeconnect/blob/refs/tags/v0.13.0/CHANGELOG.md

--- pkgs/development/python-modules/aiohomeconnect/default.nix
+++ pkgs/development/python-modules/aiohomeconnect/default.nix
@@ -4,22 +4,22 @@
   buildPythonPackage,
   fastapi,
   fetchFromGitHub,
-  httpx,
   httpx-sse,
+  httpx,
   mashumaro,
-  poetry-core,
   pytest-asyncio,
   pytest-cov-stub,
   pytest-httpx,
   pytestCheckHook,
   pythonOlder,
+  setuptools,
   typer,
   uvicorn,
 }:
 
 buildPythonPackage rec {
   pname = "aiohomeconnect";
-  version = "0.12.3";
+  version = "0.13.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -28,12 +28,10 @@ buildPythonPackage rec {
     owner = "MartinHjelmare";
     repo = "aiohomeconnect";
     tag = "v${version}";
-    hash = "sha256-zb4ld92bqgl2rt6YD8w2tz9CEMaiLBbXNPk5bB32IcI=";
+    hash = "sha256-pwdi9TdiX3C5Pw214QJaB1hsi99AoMQ+l2Iu1ULYW8Q=";
   };
 
-  pythonRelaxDeps = [ "httpx" ];
-
-  build-system = [ poetry-core ];
+  build-system = [ setuptools ];
 
   dependencies = [
     httpx
