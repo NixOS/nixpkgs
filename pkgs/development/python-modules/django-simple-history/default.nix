1bdf22607eff3132840a4a43d1be0b620dbca941
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-02-25 16:10:00 +0100

    python312Packages.django-simple-history: drop support for python 3.8
    Co-authored-by: Nick Cao <nickcao@nichi.co>

--- pkgs/development/python-modules/django-simple-history/default.nix
+++ pkgs/development/python-modules/django-simple-history/default.nix
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   version = "3.9.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "jazzband";
