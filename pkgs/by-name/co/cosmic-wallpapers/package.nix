ba2be6cc1dc11c7535367a28072623f95f814bbe
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-02-25 14:57:58 -0300

    cosmic-wallpapers: fix updateScript

--- pkgs/by-name/co/cosmic-wallpapers/package.nix
+++ pkgs/by-name/co/cosmic-wallpapers/package.nix
@@ -30,6 +30,8 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   passthru.updateScript = nix-update-script {
     extraArgs = [
+      "--version"
+      "unstable"
       "--version-regex"
       "epoch-(.*)"
     ];
