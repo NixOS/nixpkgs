858a382a7cf9b3d4bb317051e96af42c5259c839
heisfer <heisfer@refract.dev>
2025-02-25 16:14:14 +0200

    laravel: 5.9.2 -> 5.12.2

--- pkgs/by-name/la/laravel/package.nix
+++ pkgs/by-name/la/laravel/package.nix
@@ -6,24 +6,26 @@
 }:
 php.buildComposerProject2 (finalAttrs: {
   pname = "laravel";
-  version = "5.9.2";
+  version = "5.12.2";
 
   src = fetchFromGitHub {
     owner = "laravel";
     repo = "installer";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-XE1KYOlWehj1peSNj3sKNr6CKchCxRNpIjXHq7slVME=";
+    hash = "sha256-YV6dUaDR2Xyp/RBj1GKE4wGP0r3eat8uiw2anMZrDg4=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   composerLock = ./composer.lock;
-  vendorHash = "sha256-jUg0hmkShzK1CAO3+Btqe3/5GFKVxRKDtIxmUuU3EdU=";
+  vendorHash = "sha256-CTvSpOL3d/8i5C7BXP8gSPiOxldouuU6Lwmx04/kMZg=";
 
   postInstall = ''
     wrapProgram $out/bin/laravel \
       --suffix PATH : ${lib.makeBinPath [ php.packages.composer ]}
   '';
+
+  passthru.updateScript = ./update.sh;
 
   meta = {
     description = "Laravel application installer";
