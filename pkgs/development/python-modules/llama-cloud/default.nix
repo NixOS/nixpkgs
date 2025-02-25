b3c8316f99e1cc571e0695d8c9186a9395360539
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-25 10:51:41 +0000

    python312Packages.llama-cloud: 0.1.12 -> 0.1.13

--- pkgs/development/python-modules/llama-cloud/default.nix
+++ pkgs/development/python-modules/llama-cloud/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "llama-cloud";
-  version = "0.1.12";
+  version = "0.1.13";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_cloud";
     inherit version;
-    hash = "sha256-1R0mzExUI5ijSQgTvHkfdQSkApgiXmLtkYlRv1cmbio=";
+    hash = "sha256-y2Ui+9D15MHNKCXnC7lD0NiRaBbiMqXOO+OnJy8ymow=";
   };
 
   build-system = [ poetry-core ];
