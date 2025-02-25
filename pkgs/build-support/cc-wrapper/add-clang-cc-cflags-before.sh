47131bd1f1c46d16c6e673555b3a5a47f34e6169
Konrad Stepniak <me@th7nder.net>
2025-02-25 11:55:12 +0100

    Parse flags properly

--- pkgs/build-support/cc-wrapper/add-clang-cc-cflags-before.sh
+++ pkgs/build-support/cc-wrapper/add-clang-cc-cflags-before.sh
@@ -1,12 +1,23 @@
 needsTarget=true
 targetValue=""
 
-for p in "${params[@]}"; do
+declare -i n=0
+nParams=${#params[@]}
+while (("$n" < "$nParams")); do
+    p=${params[n]}
+    v=${params[n + 1]:-} # handle `p` being last one
+    n+=1
+
     case "$p" in
     -target)
+        if [ -z "$v" ]; then
+            echo "Error: -target requires an argument" >&2
+            exit 1
+        fi
         needsTarget=false
-        targetValue=$2
-        shift 2
+        targetValue=$v
+        # skip parsing the value of -target
+        n+=1
         ;;
     --target=*)
         needsTarget=false
@@ -16,7 +27,7 @@ for p in "${params[@]}"; do
 done
 
 if ! $needsTarget && [[ "$targetValue" != "@defaultTarget@" ]]; then
-    echo "Warning: supplying the --target argument to a nix-wrapped compiler may not work correctly - cc-wrapper is currently not designed with multi-target compilers in mind. You may want to use an un-wrapped compiler instead." >&2
+    echo "Warning: supplying the --target $targetValue != @defaultTarget@ argument to a nix-wrapped compiler may not work correctly - cc-wrapper is currently not designed with multi-target compilers in mind. You may want to use an un-wrapped compiler instead." >&2
 fi
 
 if $needsTarget && [[ $0 != *cpp ]]; then
