4d4fb04d4a793a6ab454f587205f21fb40c1f1d0
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-26 17:29:42 +0100

    python312Packages.mypy-boto3-kinesis-video-archived-media: 1.36.0 -> 1.37.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -734,8 +734,8 @@ rec {
       "sha256-W3ivXrEvOQrLH17MuXDsFxKUrUaOo3PQmpq0uyaj1ys=";
 
   mypy-boto3-kinesis-video-archived-media =
-    buildMypyBoto3Package "kinesis-video-archived-media" "1.36.0"
-      "sha256-9noyiHF4RCoqjSgJrstb0OgUDIGIH7jzgFNiO6Ir+K0=";
+    buildMypyBoto3Package "kinesis-video-archived-media" "1.37.0"
+      "sha256-t5Z7T540M+ifCdUT1cV04PKqERHzW0XIqKwvYMQ5od4=";
 
   mypy-boto3-kinesis-video-media =
     buildMypyBoto3Package "kinesis-video-media" "1.36.0"
