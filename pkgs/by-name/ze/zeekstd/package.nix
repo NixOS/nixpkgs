d473b8702625d3fda02c120c70733c355c3b833a
Robert Rose <robert.rose@mailbox.org>
2025-02-26 08:47:06 +0100

    nixos/repart: add zstd-seekable compression algorithm
    The seekable format splits compressed data into a series of independent
    frames, each of which can be decompressed individually. This allows to
    distribute images in smaller chunks and allows image downloads to be
    paused and resumed later from the same point.
    
    Seekable archives as a whole can be decompressed with any regular zstd
    decompressor. However, partial decompression requires to know the
    starting position of the desired frame, which can be extracted from a
    skippable frame (aka seektable) that is appended to the compressed data.

--- pkgs/by-name/ze/zeekstd/package.nix
+++ pkgs/by-name/ze/zeekstd/package.nix
@@ -3,30 +3,26 @@
   lib,
   rustPlatform,
 }:
-let
-  version = "0.2.2";
-in
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zeekstd";
-  inherit version;
+  version = "0.2.2";
 
   src = fetchFromGitHub {
     owner = "rorosen";
     repo = "zeekstd";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-Blyp5GpnytB3S4k6lp2fAwXueaUtXqPW+WLEmFNPZc0=";
   };
 
   useFetchCargoVendor = true;
   cargoHash = "sha256-bbl0zHxd2HYkctX029mtxDciC2tnPVTlHxYyetmtuw0=";
-  stripAllList = [ "bin" ];
 
   meta = {
     description = "CLI tool that works with the zstd seekable format";
     homepage = "https://github.com/rorosen/zeekstd";
-    changelog = "https://github.com/rorosen/zeekstd/releases/tag/v${version}";
+    changelog = "https://github.com/rorosen/zeekstd/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.bsd2;
     maintainers = [ lib.maintainers.rorosen ];
     mainProgram = "zeekstd";
   };
-}
+})
