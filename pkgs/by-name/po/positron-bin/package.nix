763e76f0188b1d0becc9c7872aca33d3cc994a04
Bruno Rodrigues <bruno@brodrigues.co>
2025-02-26 11:27:56 +0100

    positron-bin: 2025.02.0-171 -> 2025.03.0-116

--- pkgs/by-name/po/positron-bin/package.nix
+++ pkgs/by-name/po/positron-bin/package.nix
@@ -22,7 +22,7 @@
 }:
 let
   pname = "positron-bin";
-  version = "2025.02.0-171";
+  version = "2025.03.0-116";
 in
 stdenv.mkDerivation {
   inherit version pname;
@@ -30,13 +30,13 @@ stdenv.mkDerivation {
   src =
     if stdenv.hostPlatform.isDarwin then
       fetchurl {
-        url = "https://github.com/posit-dev/positron/releases/download/${version}/Positron-${version}.dmg";
-        hash = "sha256-b5o1+UXt5JAuHkm1K1jrMLV+7PHfKJTOff4aTk8xm2I=";
+        url = "https://cdn.posit.co/positron/dailies/mac/universal/Positron-${version}.dmg";
+        hash = "sha256-ss+iYdxMjUDuIwunivtOSOUVZFlSxstT+PUEjJfcaag=";
       }
     else
       fetchurl {
-        url = "https://github.com/posit-dev/positron/releases/download/${version}/Positron-${version}-x64.deb";
-        hash = "sha256-TjQc/Y4Sa2MlLslbygYVFbIk3raArMvYstSiSEYzfo0=";
+        url = "https://cdn.posit.co/positron/dailies/deb/x86_64/Positron-${version}-x64.deb";
+        hash = "sha256-ay4coAhTUCht77FR26JQ4bAGsZmDbMHejy3BDkrPhSQ=";
       };
 
   buildInputs =
