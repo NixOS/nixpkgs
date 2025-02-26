48156a1e5da619fa8d045f36acf9b62b5c282b60
Tert0 <tert0byte@gmail.com>
2025-02-26 10:51:26 +0100

    cmatrix: refactor and adopt

--- pkgs/by-name/cm/cmatrix/package.nix
+++ pkgs/by-name/cm/cmatrix/package.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   autoreconfHook,
   ncurses,
+  versionCheckHook,
 }:
 
 stdenv.mkDerivation rec {
@@ -13,23 +14,27 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "abishekvashok";
     repo = "cmatrix";
-    rev = "v${version}";
+    tag = "v${version}";
     sha256 = "1h9jz4m4s5l8c3figaq46ja0km1gimrkfxm4dg7mf4s84icmasbm";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
   buildInputs = [ ncurses ];
 
-  meta = with lib; {
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "-V";
+  doInstallCheck = true;
+
+  meta = {
     description = "Simulates the falling characters theme from The Matrix movie";
-    license = licenses.gpl3;
     longDescription = ''
       CMatrix simulates the display from "The Matrix" and is based
       on the screensaver from the movie's website.
     '';
     homepage = "https://github.com/abishekvashok/cmatrix";
     platforms = ncurses.meta.platforms;
-    maintainers = [ ];
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ Tert0 ];
     mainProgram = "cmatrix";
   };
 }
