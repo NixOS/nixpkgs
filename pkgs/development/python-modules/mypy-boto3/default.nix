60f7b7ea0eb399a04998676290c32bd0a68a00f9
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-26 17:28:55 +0100

    python312Packages.mypy-boto3-glacier: 1.36.0 -> 1.37.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -566,8 +566,8 @@ rec {
       "sha256-p32uq+oF5IVsI2eoBjmjmZO5actsyghuB5R81fuCEYA=";
 
   mypy-boto3-glacier =
-    buildMypyBoto3Package "glacier" "1.36.0"
-      "sha256-OvHk8XMzUC/UgOAHVgf0lZ+LK0r8oq/m7PWYgLLoj40=";
+    buildMypyBoto3Package "glacier" "1.37.0"
+      "sha256-H/k893rbdr/Z8h8eP2bO5hWnwYNsgSWpXSPaBa/DPHs=";
 
   mypy-boto3-globalaccelerator =
     buildMypyBoto3Package "globalaccelerator" "1.36.0"
