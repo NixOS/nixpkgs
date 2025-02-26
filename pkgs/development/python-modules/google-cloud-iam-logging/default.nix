338ab72c897b27a539b0858b23c60e819c989cc8
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-26 17:17:14 +0000

    python312Packages.google-cloud-iam-logging: 1.4.0 -> 1.4.1

--- pkgs/development/python-modules/google-cloud-iam-logging/default.nix
+++ pkgs/development/python-modules/google-cloud-iam-logging/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-iam-logging";
-  version = "1.4.0";
+  version = "1.4.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "google_cloud_iam_logging";
     inherit version;
-    hash = "sha256-+7rRPIPXmxMbcTRqKTfYXciH1Op760Vm2d969zqJNnM=";
+    hash = "sha256-+rgtIj4Q+ZL8ENeigmF0dJYzIbJ797A1bOs2mbH4AKI=";
   };
 
   build-system = [ setuptools ];
