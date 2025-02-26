8a031ae182bd3a8b321140e5e5252769fd85b8ef
Sefa Eyeoglu <contact@scrumplex.net>
2025-02-26 09:14:54 +0100

    ddnet: fix licenses
    See https://github.com/ddnet/ddnet/blob/33ae91335044b95bf28d9b753e6c8a20baa04eeb/license.txt
    
    Upstream code is licensed under zlib, some assets are CC-BY-SA-3.0 and
    fonts are OFL-1.1
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/by-name/dd/ddnet/package.nix
+++ pkgs/by-name/dd/ddnet/package.nix
@@ -116,7 +116,7 @@ stdenv.mkDerivation rec {
     install_name_tool -change "$out/lib/libsteam_api.dylib" "$out/lib/ddnet/libsteam_api.dylib" "$out/bin/DDNet"
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Teeworlds modification with a unique cooperative gameplay";
     longDescription = ''
       DDraceNetwork (DDNet) is an actively maintained version of DDRace,
@@ -126,8 +126,12 @@ stdenv.mkDerivation rec {
       design your own maps, or run your own server.
     '';
     homepage = "https://ddnet.org";
-    license = licenses.asl20;
-    maintainers = with maintainers; [
+    license = with lib.licenses; [
+      zlib
+      ofl
+      cc-by-sa-30
+    ];
+    maintainers = with lib.maintainers; [
       lom
       ncfavier
       Scrumplex
