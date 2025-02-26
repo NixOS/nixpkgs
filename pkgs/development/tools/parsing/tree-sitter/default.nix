ba1ba831130f0d3a16a6ab1a56d3189971bd7096
Adrien Faure <adrien.faure@protonmail.com>
2025-02-26 12:00:32 +0100

    remove broken grammars invoked as dependencies

--- pkgs/development/tools/parsing/tree-sitter/default.nix
+++ pkgs/development/tools/parsing/tree-sitter/default.nix
@@ -134,7 +134,7 @@ let
           };
         };
     in
-    lib.mapAttrs build (lib.filterAttrs (_: v: !(v ? isBroken && v.isBroken)) grammars);
+    lib.mapAttrs build grammars;
 
   # Usage:
   # pkgs.tree-sitter.withPlugins (p: [ p.tree-sitter-c p.tree-sitter-java ... ])
