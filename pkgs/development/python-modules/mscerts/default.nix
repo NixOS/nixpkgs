254e3d6fd869365b6e69662efe7a96b7b355e0ac
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-26 17:50:19 +0100

    python313Packages.mscerts: 2024.5.29 -> 2025.2.26
    Diff: https://github.com/ralphje/mscerts/compare/refs/tags/2024.5.29...2025.2.26

--- pkgs/development/python-modules/mscerts/default.nix
+++ pkgs/development/python-modules/mscerts/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "mscerts";
-  version = "2024.5.29";
+  version = "2025.2.26";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "ralphje";
     repo = "mscerts";
     tag = version;
-    hash = "sha256-1k0k5BSEyiJ1Brx7P+sgUQI63k1eT59edghnPVuCuZE=";
+    hash = "sha256-W9F+M/fMsQB8vld6+m18NDTJr526N/AVHvgLrlVIfcI=";
   };
 
   build-system = [ setuptools ];
