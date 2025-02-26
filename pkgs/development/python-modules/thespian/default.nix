68168ba73cc170342f6c2c5f7985e7aa0cc75a66
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-02-27 00:37:52 +0100

    python312Packages.thespian: refactor

--- pkgs/development/python-modules/thespian/default.nix
+++ pkgs/development/python-modules/thespian/default.nix
@@ -1,13 +1,14 @@
 {
-  fetchPypi,
-  buildPythonPackage,
   lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
-  version = "4.0.0";
-  format = "setuptools";
   pname = "thespian";
+  version = "4.0.0";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
@@ -15,16 +16,25 @@ buildPythonPackage rec {
     hash = "sha256-92krWgkXCmH7Qa0Q+0cY2KKwKjDeJYLA8I0DtSmoRog=";
   };
 
+  build-system = [
+    setuptools
+  ];
+
   # Do not run the test suite: it takes a long time and uses
   # significant system resources, including requiring localhost
   # network operations.  Thespian tests are performed via its Travis
   # CI configuration and do not need to be duplicated here.
   doCheck = false;
 
-  meta = with lib; {
+  pythonImportsCheck = [
+    "thespian"
+    "thespian.actors"
+  ];
+
+  meta = {
     description = "Python Actor concurrency library";
     homepage = "http://thespianpy.com/";
-    license = licenses.mit;
-    maintainers = [ maintainers.kquick ];
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.kquick ];
   };
 }
