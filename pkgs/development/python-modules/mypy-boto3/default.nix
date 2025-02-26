830635c5b3c99ce9941f334bf67ddd5ba22ec18c
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-26 17:27:27 +0100

    python312Packages.mypy-boto3-codebuild: 1.36.25 -> 1.37.1

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -266,8 +266,8 @@ rec {
       "sha256-nVAeLXy2hjJZDjNTjFccKClY7DS0oLv9Q9QLlngkX6M=";
 
   mypy-boto3-codebuild =
-    buildMypyBoto3Package "codebuild" "1.36.25"
-      "sha256-0sAhsUYgi3ajoNQPTD1X9JfpCERJEjiT1Fq947WyYFk=";
+    buildMypyBoto3Package "codebuild" "1.37.1"
+      "sha256-8Z+3exV0XBznoiPVFcisI+8BP7HL5ZLxJ6NHUoD6lNU=";
 
   mypy-boto3-codecatalyst =
     buildMypyBoto3Package "codecatalyst" "1.36.0"
