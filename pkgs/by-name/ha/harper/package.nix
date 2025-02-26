fd628a8cfc1849562c9144c7ca328c9bf5bb5c53
Sumner Evans <sumner.evans@automattic.com>
2025-02-26 13:25:36 -0700

    harper: 0.22.0 -> 0.25.1
    https://github.com/Automattic/harper/releases/tag/v0.23.0
    https://github.com/Automattic/harper/releases/tag/v0.24.0
    https://github.com/Automattic/harper/releases/tag/v0.25.0
    https://github.com/Automattic/harper/releases/tag/v0.25.1
    
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- pkgs/by-name/ha/harper/package.nix
+++ pkgs/by-name/ha/harper/package.nix
@@ -7,18 +7,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "harper";
-  version = "0.22.0";
+  version = "0.25.1";
 
   src = fetchFromGitHub {
     owner = "Automattic";
     repo = "harper";
     rev = "v${version}";
-    hash = "sha256-MwShWO1XXV2Ln70+w5KGPDChtnAOhsa2fENTNql3bo4=";
+    hash = "sha256-nH1DyWGJMYiisdS4YRw+kUIJLX4twB9ZJ7OWH+QLlIA=";
   };
 
   buildAndTestSubdir = "harper-ls";
   useFetchCargoVendor = true;
-  cargoHash = "sha256-+rtFGD/go5W+RCpE0+3Tkb2TbwzH8J2nl0/VkipSjI0=";
+  cargoHash = "sha256-C5+5cxsnyM6cZ724C2czuoCfmIE0nQJXCwYCjfW7sgE=";
 
   passthru.updateScript = nix-update-script { };
 
