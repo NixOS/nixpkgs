cd747cddc8ccbe3a7147ab76a9da19c92d3857dd
R. Ryantm <ryantm-bot@ryantm.com>
2025-02-27 03:38:15 +0000

    python312Packages.netbox-dns: 1.1.7 -> 1.2.4

--- pkgs/development/python-modules/netbox-dns/default.nix
+++ pkgs/development/python-modules/netbox-dns/default.nix
@@ -7,14 +7,14 @@
 }:
 buildPythonPackage rec {
   pname = "netbox-plugin-dns";
-  version = "1.1.7";
+  version = "1.2.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "peteeckel";
     repo = "netbox-plugin-dns";
     tag = version;
-    hash = "sha256-wrVTsVKjGPJCIoSo6uuWtorJBjn4Kh2kYkBXxM88kCo";
+    hash = "sha256-ESjggIy3o76lsH8YksH64Nm8YafEjv/tDYdL9Y0/jwM=";
   };
 
   build-system = [ setuptools ];
