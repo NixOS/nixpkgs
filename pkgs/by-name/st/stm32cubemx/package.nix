619ca80c9431395f4c09bc3d4f36960b26f847d1
PickaxeHit <95094338+PickaxeHit@users.noreply.github.com>
2025-02-27 13:52:07 +0800

    stm32cubemx: fix missing icon
    (cherry picked from commit 63045b429f1b6320546c4f2d1dc15800ad5edcf2)

--- pkgs/by-name/st/stm32cubemx/package.nix
+++ pkgs/by-name/st/stm32cubemx/package.nix
@@ -97,8 +97,9 @@ buildFHSEnv {
   inherit (package) pname version meta;
   runScript = "${package.outPath}/bin/stm32cubemx";
   extraInstallCommands = ''
-    mkdir -p $out/share/applications
+    mkdir -p $out/share/{applications,icons}
     ln -sf ${package.outPath}/share/applications/* $out/share/applications/
+    ln -sf ${package.outPath}/share/icons/* $out/share/icons/
   '';
   targetPkgs =
     pkgs: with pkgs; [
