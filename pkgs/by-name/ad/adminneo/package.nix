1f739f8ba24cd9de0c4abfea372cbe101014ff01
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-02-27 14:27:25 +0530

    adminneo: change package name upstream changed name again, from adminerneo to adminneo.
    Apparently its due to a trademark violation.
    https://github.com/adminneo-org/adminneo/discussions/57
    New url is now: https://github.com/adminneo-org/adminneo
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- pkgs/by-name/ad/adminneo/package.nix
+++ pkgs/by-name/ad/adminneo/package.nix
@@ -9,12 +9,12 @@
   plugins ? [ ],
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
-  pname = "adminerneo";
+  pname = "adminneo";
   version = "4.14";
 
   src = fetchFromGitHub {
-    owner = "adminerneo";
-    repo = "adminerneo";
+    owner = "adminneo-org";
+    repo = "adminneo";
     tag = "v${finalAttrs.version}";
     hash = "sha256-GxkITh6Hh7E6qKEsCYs8M1xLeCbdI1WQqM1Zjdb6BVE=";
   };
@@ -63,7 +63,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Database management in a single PHP file (fork of Adminer)";
-    homepage = "https://github.com/adminerneo/adminerneo";
+    homepage = "https://github.com/adminneo-org/adminneo";
     license = with lib.licenses; [
       asl20
       gpl2Only
