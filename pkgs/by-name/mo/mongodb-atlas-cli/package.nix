faf467b015c6ee42303be8b585bcf75f2867463f
iamanaws <78835633+Iamanaws@users.noreply.github.com>
2025-02-26 19:52:09 -0800

    mongodb-atlas-cli: merge with previous init pr
    - update homepage
    - add changelog

--- pkgs/by-name/mo/mongodb-atlas-cli/package.nix
+++ pkgs/by-name/mo/mongodb-atlas-cli/package.nix
@@ -1,7 +1,8 @@
 {
+  stdenv,
+  fetchFromGitHub,
   lib,
   buildGoModule,
-  fetchFromGitHub,
   installShellFiles,
   nix-update-script,
   testers,
@@ -12,14 +13,14 @@ buildGoModule rec {
   pname = "mongodb-atlas-cli";
   version = "1.45.1";
 
-  vendorHash = "sha256-4qkB2PdMiMtyxdHodwR+w9Lbt1JaT1/wUS+h23ajDD4=";
-
   src = fetchFromGitHub {
     owner = "mongodb";
     repo = "mongodb-atlas-cli";
-    rev = "refs/tags/atlascli/v${version}";
-    sha256 = "sha256-Pk7C8CzhRB1XRkqHPECIeaFSwWEWZqJ4sTONTEiqZvg=";
+    tag = "atlascli/v${version}";
+    hash = "sha256-Pk7C8CzhRB1XRkqHPECIeaFSwWEWZqJ4sTONTEiqZvg=";
   };
+
+  vendorHash = "sha256-4qkB2PdMiMtyxdHodwR+w9Lbt1JaT1/wUS+h23ajDD4=";
 
   nativeBuildInputs = [ installShellFiles ];
 
@@ -30,7 +31,9 @@ buildGoModule rec {
     "-X github.com/mongodb/mongodb-atlas-cli/atlascli/internal/version.Version=v${version}"
   ];
 
-  postInstall = ''
+  subPackages = [ "cmd/atlas" ];
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
     installShellCompletion --cmd atlas \
       --bash <($out/bin/atlas completion bash) \
       --fish <($out/bin/atlas completion fish) \
@@ -48,10 +51,14 @@ buildGoModule rec {
   };
 
   meta = {
-    homepage = "https://www.mongodb.com/try/download/shell";
     description = "CLI utility to manage MongoDB Atlas from the terminal";
-    maintainers = with lib.maintainers; [ aduh95 ];
+    homepage = "https://github.com/mongodb/mongodb-atlas-cli";
+    changelog = "https://www.mongodb.com/docs/atlas/cli/current/atlas-cli-changelog/#atlas-cli-${version}";
     license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
+      aduh95
+      iamanaws
+    ];
     mainProgram = "atlas";
   };
 }
