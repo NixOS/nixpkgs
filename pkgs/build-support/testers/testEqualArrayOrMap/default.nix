becc932119d945bbac29e8e55c561fdd8ef66dde
Connor Baker <ConnorBaker01@gmail.com>
2025-02-27 18:06:43 +0000

    testers.testEqualArrayOrMap: inline let expression

--- pkgs/build-support/testers/testEqualArrayOrMap/default.nix
+++ pkgs/build-support/testers/testEqualArrayOrMap/default.nix
@@ -2,45 +2,39 @@
   lib,
   stdenvNoCC,
 }:
-let
-  inherit (lib) maintainers;
-  inherit (lib.asserts) assertMsg;
-  inherit (lib.customisation) makeOverridable;
+lib.makeOverridable (
+  {
+    name,
+    valuesArray ? null,
+    valuesMap ? null,
+    expectedArray ? null,
+    expectedMap ? null,
+    script,
+  }:
+  assert lib.assertMsg (
+    expectedArray != null || expectedMap != null
+  ) "testEqualArrayOrMap: at least one of 'expectedArray' or 'expectedMap' must be provided";
+  stdenvNoCC.mkDerivation {
+    __structuredAttrs = true;
+    strictDeps = true;
 
-  testEqualArrayOrMap =
-    {
-      name,
-      valuesArray ? null,
-      valuesMap ? null,
-      expectedArray ? null,
-      expectedMap ? null,
-      script,
-    }:
-    assert assertMsg (
-      expectedArray != null || expectedMap != null
-    ) "testEqualArrayOrMap: at least one of 'expectedArray' or 'expectedMap' must be provided";
-    stdenvNoCC.mkDerivation {
-      __structuredAttrs = true;
-      strictDeps = true;
+    inherit name;
 
-      inherit name;
+    nativeBuildInputs = [
+      ./assert-equal-array.sh
+      ./assert-equal-map.sh
+    ];
 
-      nativeBuildInputs = [
-        ./assert-equal-array.sh
-        ./assert-equal-map.sh
-      ];
+    inherit valuesArray valuesMap;
+    inherit expectedArray expectedMap;
 
-      inherit valuesArray valuesMap;
-      inherit expectedArray expectedMap;
+    inherit script;
 
-      inherit script;
+    buildCommandPath = ./build-command.sh;
 
-      buildCommandPath = ./build-command.sh;
-
-      meta = {
-        description = "Tests for equality of bash arrays (including associative arrays)";
-        maintainers = [ maintainers.connorbaker ];
-      };
+    meta = {
+      description = "Tests for equality of bash arrays (including associative arrays)";
+      maintainers = [ lib.maintainers.connorbaker ];
     };
-in
-makeOverridable testEqualArrayOrMap
+  }
+)
