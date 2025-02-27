a72314b7e230797eb2a7ec8c8e9ebdbc0c7adc85
Fabian Affolter <mail@fabian-affolter.ch>
2025-02-28 00:11:52 +0100

    python312Packages.mypy-boto3-storagegateway: 1.37.0 -> 1.37.3

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1326,8 +1326,8 @@ rec {
       "sha256-TI4+rVDXiOqI1viEFwbSlvDbwzdKPbTne4pZsdK9HRo=";
 
   mypy-boto3-storagegateway =
-    buildMypyBoto3Package "storagegateway" "1.37.0"
-      "sha256-FfRrqnBL9TyS3dDXPqvT5drH3X07vEqo00OWFY6mjLg=";
+    buildMypyBoto3Package "storagegateway" "1.37.3"
+      "sha256-g7IxHH+/7ZUHGsRKdsCh2xsPjYUbg9CmL7Rqni/ANNM=";
 
   mypy-boto3-sts =
     buildMypyBoto3Package "sts" "1.37.0"
