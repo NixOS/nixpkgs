33c162c0902736cd8911485438dfbb0abec8d481
Connor Baker <ConnorBaker01@gmail.com>
2025-02-27 18:01:30 +0000

    testers.testBuildFailure': inline let expression

--- pkgs/build-support/testers/testBuildFailurePrime/default.nix
+++ pkgs/build-support/testers/testBuildFailurePrime/default.nix
@@ -3,37 +3,35 @@
   stdenvNoCC,
   testers,
 }:
-let
-  # See https://nixos.org/manual/nixpkgs/unstable/#tester-testBuildFailurePrime
-  # or doc/build-helpers/testers.chapter.md
-  testBuildFailure' =
-    {
-      drv,
-      name ? "testBuildFailure-${drv.name}",
-      expectedBuilderExitCode ? 1,
-      expectedBuilderLogEntries ? [ ],
-      script ? "",
-    }:
-    stdenvNoCC.mkDerivation (finalAttrs: {
-      __structuredAttrs = true;
-      strictDeps = true;
+# See https://nixos.org/manual/nixpkgs/unstable/#tester-testBuildFailurePrime
+# or doc/build-helpers/testers.chapter.md
+lib.makeOverridable (
+  {
+    drv,
+    name ? "testBuildFailure-${drv.name}",
+    expectedBuilderExitCode ? 1,
+    expectedBuilderLogEntries ? [ ],
+    script ? "",
+  }:
+  stdenvNoCC.mkDerivation (finalAttrs: {
+    __structuredAttrs = true;
+    strictDeps = true;
 
-      inherit name;
+    inherit name;
 
-      nativeBuildInputs = [ finalAttrs.failed ];
+    nativeBuildInputs = [ finalAttrs.failed ];
 
-      failed = testers.testBuildFailure drv;
+    failed = testers.testBuildFailure drv;
 
-      inherit expectedBuilderExitCode expectedBuilderLogEntries;
+    inherit expectedBuilderExitCode expectedBuilderLogEntries;
 
-      inherit script;
+    inherit script;
 
-      buildCommandPath = ./build-command.sh;
+    buildCommandPath = ./build-command.sh;
 
-      meta = {
-        description = "A wrapper around testers.testBuildFailure to simplify common use cases";
-        maintainers = [ lib.maintainers.connorbaker ];
-      };
-    });
-in
-lib.makeOverridable testBuildFailure'
+    meta = {
+      description = "A wrapper around testers.testBuildFailure to simplify common use cases";
+      maintainers = [ lib.maintainers.connorbaker ];
+    };
+  })
+)
