963db77e0c2c54dee5738cb4594ae08b8a1f2229
Wolfgang Walther <walther@technowledgy.de>
2025-02-28 20:53:52 +0100

    postgresql: use lib.getExe

--- pkgs/servers/sql/postgresql/generic.nix
+++ pkgs/servers/sql/postgresql/generic.nix
@@ -253,7 +253,7 @@ let
         CFLAGS =
           "-fdata-sections -ffunction-sections"
           + (if stdenv'.cc.isClang then " -flto" else " -fmerge-constants -Wl,--gc-sections");
-      } // lib.optionalAttrs pythonSupport { PYTHON = "${python3}/bin/python"; };
+      } // lib.optionalAttrs pythonSupport { PYTHON = lib.getExe python3; };
 
       configureFlags =
         let
