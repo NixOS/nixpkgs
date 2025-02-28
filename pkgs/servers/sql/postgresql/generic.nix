d68dcf33b251aaa5dffd12e9911ade1828b92239
Wolfgang Walther <walther@technowledgy.de>
2025-02-28 15:46:17 +0100

    postgresql: provide plpython3 extension as package
    This allows us to always build the extension, but still have the user
    explicitly enable it without causing rebuilds.

--- pkgs/servers/sql/postgresql/generic.nix
+++ pkgs/servers/sql/postgresql/generic.nix
@@ -93,7 +93,13 @@ let
       perl,
 
       # PL/Python
-      pythonSupport ? false,
+      pythonSupport ?
+        lib.meta.availableOn stdenv.hostPlatform python3
+        # Building with python in pkgsStatic gives this error:
+        #  checking how to link an embedded Python application... configure: error: could not find shared library for Python
+        && !stdenv.hostPlatform.isStatic
+        # configure tries to call the python executable
+        && stdenv.buildPlatform.canExecute stdenv.hostPlatform,
       python3,
 
       # PL/Tcl
@@ -153,7 +159,7 @@ let
         "doc"
         "lib"
         "man"
-      ];
+      ] ++ lib.optionals pythonSupport [ "plpython3" ];
       outputChecks = {
         out = {
           disallowedReferences = [
@@ -399,16 +405,16 @@ let
           # to their own output for installation, will then fail to find "postgres" during linking.
           substituteInPlace "$dev/lib/pgxs/src/Makefile.port" \
             --replace-fail '-bundle_loader $(bindir)/postgres' "-bundle_loader $out/bin/postgres"
-        '';
-
-      postFixup =
-        lib.optionalString stdenv'.hostPlatform.isGnu ''
-          # initdb needs access to "locale" command from glibc.
-          wrapProgram $out/bin/initdb --prefix PATH ":" ${glibc.bin}/bin
         ''
         + lib.optionalString pythonSupport ''
-          wrapProgram "$out/bin/postgres" --set PYTHONPATH "${python3}/${python3.sitePackages}"
+          moveToOutput "lib/*plpython3*" "$plpython3"
+          moveToOutput "share/postgresql/extension/*plpython3*" "$plpython3"
         '';
+
+      postFixup = lib.optionalString stdenv'.hostPlatform.isGnu ''
+        # initdb needs access to "locale" command from glibc.
+        wrapProgram $out/bin/initdb --prefix PATH ":" ${glibc.bin}/bin
+      '';
 
       # Running tests as "install check" to work around SIP issue on macOS:
       # https://www.postgresql.org/message-id/flat/4D8E1BC5-BBCF-4B19-8226-359201EA8305%40gmail.com
@@ -443,7 +449,7 @@ let
           pkgs =
             let
               scope = {
-                inherit jitSupport;
+                inherit jitSupport pythonSupport;
                 inherit (llvmPackages) llvm;
                 postgresql = this;
                 stdenv = stdenv';
@@ -458,7 +464,7 @@ let
             import ./ext.nix newSelf newSuper;
 
           withPackages = postgresqlWithPackages {
-            inherit buildEnv;
+            inherit buildEnv lib makeBinaryWrapper;
             postgresql = this;
           };
 
@@ -506,7 +512,12 @@ let
     });
 
   postgresqlWithPackages =
-    { postgresql, buildEnv }:
+    {
+      postgresql,
+      buildEnv,
+      lib,
+      makeBinaryWrapper,
+    }:
     f:
     let
       installedExtensions = f postgresql.pkgs;
@@ -518,7 +529,19 @@ let
         postgresql.man # in case user installs this into environment
       ];
 
-      pathsToLink = [ "/" ];
+      pathsToLink = [
+        "/"
+        "/bin"
+      ];
+
+      nativeBuildInputs = [ makeBinaryWrapper ];
+      postBuild =
+        let
+          args = lib.concatMap (ext: ext.wrapperArgs or [ ]) installedExtensions;
+        in
+        ''
+          wrapProgram "$out/bin/postgres" ${lib.concatStringsSep " " args}
+        '';
 
       passthru = {
         inherit installedExtensions;
@@ -528,11 +551,11 @@ let
           ;
 
         withJIT = postgresqlWithPackages {
-          inherit buildEnv;
+          inherit buildEnv lib makeBinaryWrapper;
           postgresql = postgresql.withJIT;
         } f;
         withoutJIT = postgresqlWithPackages {
-          inherit buildEnv;
+          inherit buildEnv lib makeBinaryWrapper;
           postgresql = postgresql.withoutJIT;
         } f;
       };
