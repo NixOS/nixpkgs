6411d22fa4c70bc7b04e6131eaf09e93383fca96
Keenan Weaver <keenanweaver@protonmail.com>
2025-02-28 05:53:28 -0600

    openrct2: add keenanweaver to maintainers

--- pkgs/by-name/op/openrct2/package.nix
+++ pkgs/by-name/op/openrct2/package.nix
@@ -147,6 +147,9 @@ stdenv.mkDerivation {
     downloadPage = "https://github.com/OpenRCT2/OpenRCT2/releases";
     license = licenses.gpl3Only;
     platforms = platforms.linux;
-    maintainers = with maintainers; [ oxzi ];
+    maintainers = with maintainers; [
+      oxzi
+      keenanweaver
+    ];
   };
 }
