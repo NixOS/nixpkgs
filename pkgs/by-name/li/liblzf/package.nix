2228bc075dcbf4a0e226f543349530039add37c7
Anton Tetov <anton@tetov.se>
2025-03-01 20:41:01 +0100

    liblzf: patches to header & build system, tests
      - Adds a debian patch wrapping header in extern "C" for C++
      - Includes patch to configure.in and a new Makefile.am to enable
        libtool for shared library and pkg-config file
      - Sets VERSION_INFO to 1:0:0 for the shared library like in Debian[1],
        Ubuntu[2] and Fedora[3]
      - Creates a dev output so pkg-config path is handled automagically
      - Moves tests to passthru.tests and adds test for the executable
      - Moves C file for shared lib tests to separate file
      - Changes description to follow nixpkgs conventions
    
    [1]: https://salsa.debian.org/debian/liblzf/-/blob/0f44198284fd6dce166cd92e7104849bf91c41a8/debian/extras/CMakeLists.txt#L52
    [2]: https://git.launchpad.net/ubuntu/+source/liblzf/tree/debian/extras/CMakeLists.txt?h=applied/ubuntu/oracular&id=299acc7828f458f14e0466f118bf57df95538d94#n52
    [3]: https://src.fedoraproject.org/rpms/liblzf/blob/53da654eead51a24ac81a28e1b1c531eb1afab28/f/liblzf-3.6-autoconf-20140314.patch#_151

--- pkgs/by-name/li/liblzf/package.nix
+++ pkgs/by-name/li/liblzf/package.nix
@@ -1,13 +1,20 @@
 {
   lib,
-  fetchurl,
   stdenv,
+  autoreconfHook,
+  fetchDebianPatch,
+  fetchurl,
+  pkg-config,
+  testers,
+  validatePkgConfig,
+  writeText,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "liblzf";
   version = "3.6";
+  shlib_version = "1:0:0"; # for libtool
 
-  # used for changelog and test file
+  # used for changelog
   cvsTag = "rel-${builtins.replaceStrings [ "." ] [ "_" ] finalAttrs.version}";
 
   src = fetchurl {
@@ -15,52 +22,118 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-nF3gH3ucyuQMP2GdJqer7JmGwGw20mDBec7dBLiftGo=";
   };
 
-  doCheck = true;
+  patches = [
+    (fetchDebianPatch {
+      inherit (finalAttrs) pname version;
+      debianRevision = "4";
+      patch = "0001-Make-sure-that-the-library-is-linked-with-C-symbols.patch";
+      hash = "sha256-Rgfp/TysRcEJaogOo/Xno+G4HZzj9Loa69DL43Bp1Ok=";
+    })
+    # we patch configure.ac to enable libtool and add a pkg-config file
+    ./0001-shared_library.patch
+  ];
 
-  preCheck = ''
-    cat > test.c <<'EOF'
-    #include <stdio.h>
-    #include <string.h>
-    #include "lzf.h"
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+    validatePkgConfig
+  ];
 
-    int main(void) {
-        const char *test = "liblzf test for nixpkgs";
-        char compressed[100];
-        char decompressed[100];
+  postPatch =
+    let
+      pkgConfigIn = writeText "liblzf.pc.in" ''
+        prefix=@prefix@
+        exec_prefix=@exec_prefix@
+        libdir=@libdir@
+        includedir=@includedir@
 
-        unsigned int clen = lzf_compress(test, strlen(test), compressed, sizeof(compressed));
-        if (!clen) return 1;
+        Name: ${finalAttrs.pname}
+        Description: ${finalAttrs.meta.description}
+        URL: ${finalAttrs.meta.homepage}
+        Version: @VERSION@
+        Libs: -L$${libdir} -llzf
+        Cflags: -I$${includedir}
+      '';
+    in
+    ''
+      echo "${finalAttrs.version}" > VERSION
+      echo "${finalAttrs.shlib_version}" > VERSION_INFO
+      cp ${./Makefile.am} ./Makefile.am
+      cp ${pkgConfigIn} ./liblzf.pc.in
+    '';
 
-        unsigned int dlen = lzf_decompress(compressed, clen, decompressed, sizeof(decompressed));
-        if (!dlen) return 2;
+  outputs = [
+    "out"
+    "dev"
+  ];
 
-        decompressed[dlen] = '\0';
-        return strcmp(test, decompressed) != 0;
-    }
-    EOF
-  '';
+  passthru.tests = {
+    pkgConfigTest = testers.hasPkgConfigModules {
+      package = finalAttrs.finalPackage;
+      versionCheck = true;
+    };
 
-  checkPhase = ''
-    runHook preCheck
+    exeTest = testers.runCommand {
+      name = "${finalAttrs.pname}-exe-test";
+      buildInputs = [ finalAttrs.finalPackage ];
+      script = ''
+        lzf -h 2> /dev/null
 
-    $CC $CFLAGS -g test.c -o test -L. -llzf -Wl,-rpath,$PWD
+        echo "LZFLZFLZFLZFLZFLZFLZFLZF" > test.txt
 
-    ./test
+        # unlzf writes to filename minus ".lzf"
+        cp test.txt test.txt.orig
 
-    runHook postCheck
-  '';
+        lzf test.txt
+        unlzf test.txt.lzf
+
+        # Compare results
+        if ! cmp -s test.txt test.txt.orig; then
+          echo "Executable test failed: files don't match"
+          exit 1
+        fi
+
+        echo "Decompressed output matches test string (lzf & unlzf)"
+
+        touch $out
+      '';
+    };
+
+    shlibTest = testers.runCommand {
+      name = "${finalAttrs.pname}-shlib-test";
+      inherit stdenv; # with CC
+      nativeBuildInputs = [ pkg-config ];
+      buildInputs = [
+        finalAttrs.finalPackage.dev
+        finalAttrs.finalPackage
+      ];
+      # tests both the library and pkg-config file
+      script = ''
+        $CC -g ${./lib_test.c} -o lib_test \
+          $(pkg-config --cflags --libs liblzf)
+
+        ./lib_test >/dev/null
+
+        echo "Built and tested file linked against liblzf using pkg-config"
+        touch $out
+      '';
+    };
+  };
 
   meta = {
-    homepage = "http://software.schmorp.de/pkg/liblzf.html";
-    description = "LibLZF is a very small data compression library.";
     changelog = "http://cvs.schmorp.de/liblzf/Changes?pathrev=rel-$finalAttrs.cvsTag}";
+    description = "Small data compression library";
+    downloadPage = "http://dist.schmorp.de/liblzf/";
+    homepage = "http://software.schmorp.de/pkg/liblzf.html";
     license = with lib.licenses; [
       bsd2
       gpl2Plus
     ];
+    mainProgram = "lzf";
     maintainers = with lib.maintainers; [
       tetov
     ];
     platforms = lib.platforms.unix;
+    pkgConfigModules = [ "liblzf" ];
   };
 })
