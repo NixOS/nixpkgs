017fc9ab086db52d20c99db830f5b6db658beae2
Tyler Hardin <th020394@gmail.com>
2025-03-01 14:39:04 -0500

    maintainers: add thardin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23324,6 +23324,12 @@
     githubId = 7060816;
     name = "Thao-Tran Le-Phuong";
   };
+  thardin = {
+    email = "th020394@gmail.com";
+    github = "Tyler-Hardin";
+    githubId = 5404976;
+    name = "Tyler Hardin";
+  };
   thblt = {
     name = "Thibault Polge";
     email = "thibault@thb.lt";
