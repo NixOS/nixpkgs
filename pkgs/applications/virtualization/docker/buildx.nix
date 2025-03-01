a294d7c791df1cf5b4c5115dd7caed525f1288bb
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-01 09:00:50 +0000

    docker-buildx: 0.20.1 -> 0.21.1

--- pkgs/applications/virtualization/docker/buildx.nix
+++ pkgs/applications/virtualization/docker/buildx.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "docker-buildx";
-  version = "0.20.1";
+  version = "0.21.1";
 
   src = fetchFromGitHub {
     owner = "docker";
     repo = "buildx";
     rev = "v${version}";
-    hash = "sha256-OgCNVNzAtVsYrAf3ZAB0n8LoKm8+naZ8KEdW3nVzMos=";
+    hash = "sha256-Dhlb8ZKZS+JzMydahl5CLPPK3QK7qLuhqOXsiEVBQMQ=";
   };
 
   doCheck = false;
