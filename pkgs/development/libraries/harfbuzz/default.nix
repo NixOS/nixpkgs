6047530a62f59b70f7c85bef1ef08be7f16c6718
misilelaboratory <misileminecord@gmail.com>
2025-03-02 00:53:27 +0900

    harfbuzz: 10.2.0 -> 11.0.1
    https://github.com/harfbuzz/harfbuzz/releases/tag/10.3.0
    https://github.com/harfbuzz/harfbuzz/releases/tag/10.4.0
    https://github.com/harfbuzz/harfbuzz/releases/tag/11.0.0
    https://github.com/harfbuzz/harfbuzz/releases/tag/11.0.1
    
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- pkgs/development/libraries/harfbuzz/default.nix
+++ pkgs/development/libraries/harfbuzz/default.nix
@@ -36,11 +36,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "harfbuzz${lib.optionalString withIcu "-icu"}";
-  version = "10.2.0";
+  version = "11.0.1";
 
   src = fetchurl {
     url = "https://github.com/harfbuzz/harfbuzz/releases/download/${finalAttrs.version}/harfbuzz-${finalAttrs.version}.tar.xz";
-    hash = "sha256-Yg40aPrsLqhoXTLEalhGm4UO9jBAs1Zc3gWVmCW0gic=";
+    hash = "sha256-SniQCQU4E222R0IHOvS013ari1DmhVZ2qBZeuLf2C3o=";
   };
 
   postPatch =
