387bc07e73fe0cb7d7363f13f9b4e1823c349245
bas <bas@noemail.invalid>
2025-03-01 20:37:09 +0100

    nixos/tandoor-recipes: fix database leak when serving media

--- pkgs/applications/misc/tandoor-recipes/default.nix
+++ pkgs/applications/misc/tandoor-recipes/default.nix
@@ -143,7 +143,7 @@ python.pkgs.pythonPackages.buildPythonPackage {
     updateScript = ./update.sh;
 
     tests = {
-      inherit (nixosTests) tandoor-recipes tandoor-recipes-script-name;
+      inherit (nixosTests) tandoor-recipes tandoor-recipes-script-name tandoor-recipes-media;
     };
   };
 
