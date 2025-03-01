0c19a1775e036318182cdf55b6b2af5634eed7c1
Nils <mail@nils.cc>
2025-03-02 00:18:41 +0100

    plover: 4.0.0.dev10 -> 4.0.0

--- pkgs/applications/misc/plover/default.nix
+++ pkgs/applications/misc/plover/default.nix
@@ -1,16 +1,16 @@
-{ lib, fetchFromGitHub, python3Packages, wmctrl, qtbase, mkDerivationWith }:
+{ lib, fetchFromGitHub, python3, wmctrl, qtbase, mkDerivationWith }:
 
 {
   stable = throw "plover.stable was removed because it used Python 2. Use plover.dev instead."; # added 2022-06-05
 
-  dev = with python3Packages; mkDerivationWith buildPythonPackage rec {
+  dev = with python3.pkgs; mkDerivationWith buildPythonPackage rec {
     pname = "plover";
-    version = "4.0.0.dev10";
+    version = "4.0.0";
 
     meta = with lib; {
       broken = stdenv.hostPlatform.isDarwin;
       description = "OpenSteno Plover stenography software";
-      maintainers = with maintainers; [ twey kovirobi ];
+      maintainers = with maintainers; [ twey kovirobi nilscc ];
       license     = licenses.gpl2;
     };
 
@@ -18,15 +18,15 @@
       owner = "openstenoproject";
       repo = "plover";
       rev = "v${version}";
-      sha256 = "sha256-oJ7+R3ZWhUbNTTAw1AfMg2ur8vW1XEbsa5FgSTam1Ns=";
+      hash = "sha256-9oDsAbpF8YbLZyRzj9j5tk8QGi0o1F+8vB5YLJGqN+4=";
     };
 
     # I'm not sure why we don't find PyQt5 here but there's a similar
     # sed on many of the platforms Plover builds for
     postPatch = "sed -i /PyQt5/d setup.cfg";
 
-    nativeCheckInputs           = [ pytest mock ];
-    propagatedBuildInputs = [ babel pyqt5 xlib pyserial appdirs wcwidth setuptools ];
+    nativeCheckInputs     = [ pytest mock ];
+    propagatedBuildInputs = [ babel pyqt5 xlib pyserial appdirs wcwidth setuptools rtf_tokenize evdev plover-stroke ];
 
     dontWrapQtApps = true;
 
