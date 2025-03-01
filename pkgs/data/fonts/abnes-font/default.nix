43d1aaf0270aa299ea65a570c697be1f9bdd1b89
Dr. Void <dr.void_industries@proton.me>
2025-03-01 17:31:29 -0500

    Abnes-font: init 1.0.0 - removed trailing whitespace

--- pkgs/data/fonts/abnes-font/default.nix
+++ pkgs/data/fonts/abnes-font/default.nix
@@ -1,4 +1,4 @@
-{ 
+{
   stdenvNoCC,
   fetchzip,
   lib,
@@ -6,7 +6,7 @@
 stdenvNoCC.mkDerivation {
   pname = "abnes-font";
   version = "1.0.0";
-  
+
   src = fetchzip {
     url = "https://fonts.resourceboy.com/file/rb-fonts/2024/08/58962/Abnes-Font.zip";
     hash = "sha256-x/AhooCW0+JF66kNaY96iB3+HpAd+l6QJ3bDZxf1xXk=";
@@ -27,9 +27,9 @@ stdenvNoCC.mkDerivation {
 
   meta = {
     description = "Abnes Font made by 177 Studio free only for indivduals";
-    homepage = "https://177studio.com/product/abnes-elegant-sans-serif-font/"; 
+    homepage = "https://177studio.com/product/abnes-elegant-sans-serif-font/";
     license = lib.licenses.unfreeRedistributable;
-    platforms = lib.platforms.all; 
+    platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [
       DrVoidest
     ];
