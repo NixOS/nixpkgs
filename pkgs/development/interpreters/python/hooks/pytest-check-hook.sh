f6f00d4c0fbfbd790602e703de8ad6d95e4ff69e
Yueh-Shun Li <shamrocklee@posteo.net>
2025-03-02 18:19:18 +0800

    python3Packages.pytestCheckHook: support enabledTestPaths

--- pkgs/development/interpreters/python/hooks/pytest-check-hook.sh
+++ pkgs/development/interpreters/python/hooks/pytest-check-hook.sh
@@ -10,7 +10,33 @@ function pytestCheckPhase() {
     # Compose arguments
     local -a flagsArray=(-m pytest)
 
-    local -a _pathsArray=()
+    local -a _pathsArray
+    local path
+
+    _pathsArray=()
+    concatTo _pathsArray enabledTestPaths
+    for path in "${_pathsArray[@]}"; do
+        if [[ "$path" =~ "::" ]]; then
+            flagsArray+=("$path")
+        else
+            # The `|| kill "$$"` trick propagates the errors from the process substitutiton subshell,
+            # which is suggested by a StackOverflow answer: https://unix.stackexchange.com/a/217643
+            readarray -t -O"${#flagsArray[@]}" flagsArray < <(@pythonCheckInterpreter@ - "$path" <<EOF || kill "$$")
+import glob
+import sys
+path_glob=sys.argv[1]
+if not len(path_glob):
+    sys.exit('Got an empty enabled tests path glob. Aborting')
+path_expanded = glob.glob(path_glob)
+if not len(path_expanded):
+    sys.exit('Enabled tests path glob "{}" does not match any paths. Aborting'.format(path_glob))
+for path in path_expanded:
+    print(path)
+EOF
+        fi
+    done
+
+    _pathsArray=()
     concatTo _pathsArray disabledTestPaths
     for path in "${_pathsArray[@]}"; do
         if [[ "$path" =~ "::" ]]; then
