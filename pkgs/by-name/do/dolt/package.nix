9d08214d3fb46e0bf9dbd7969d6eddfc41e0c836
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-02 21:12:12 +0000

    dolt: 1.49.1 -> 1.50.0

--- pkgs/by-name/do/dolt/package.nix
+++ pkgs/by-name/do/dolt/package.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "dolt";
-  version = "1.49.1";
+  version = "1.50.0";
 
   src = fetchFromGitHub {
     owner = "dolthub";
     repo = "dolt";
     rev = "v${version}";
-    sha256 = "sha256-AaJlp7bLjZ8OlBwztUGvG6uswPCuHoRuGU4CwNog0E8=";
+    sha256 = "sha256-Q4Ri/n0guAmBKp47D1t7CgQlO4QWPuQE3T/vRiTu1qs=";
   };
 
   modRoot = "./go";
   subPackages = [ "cmd/dolt" ];
-  vendorHash = "sha256-QZ34FecHoZ8mhupmpIhguDnhmk/L5vKIa2AnapQCbXM=";
+  vendorHash = "sha256-HRKl2qEv2DLCTUS+ldnpRtY6TeEcG91/MTI5WFLriz4=";
   proxyVendor = true;
   doCheck = false;
 
