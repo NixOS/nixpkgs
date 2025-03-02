17653aecdd3b5c73e7f8762c55b4f524265a6a38
Sigmanificient <edhyjox@gmail.com>
2025-03-03 00:53:34 +0100

    gcovr: move to pkgs/by-name

--- pkgs/by-name/gc/gcovr/package.nix
+++ pkgs/by-name/gc/gcovr/package.nix
@@ -1,31 +1,22 @@
 {
   lib,
-  buildPythonPackage,
+  python3Packages,
   fetchPypi,
-  colorlog,
-  jinja2,
-  lxml,
-  pygments,
-  pythonOlder,
-  hatchling,
-  hatch-fancy-pypi-readme,
-  hatch-vcs,
-  tomli,
 }:
 
-buildPythonPackage rec {
+python3Packages.buildPythonPackage rec {
   pname = "gcovr";
   version = "8.3";
   pyproject = true;
 
-  disabled = pythonOlder "3.9";
+  disabled = python3Packages.pythonOlder "3.9";
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-+qNx+cSn94yYANplUQfU+Z8EtxjRwNn0jK/cvvAEkHk=";
   };
 
-  build-system = [ hatchling ];
+  build-system = [ python3Packages.hatchling ];
 
   # pythonRelaxDeps do not work on pyproject.toml
   preBuild = ''
@@ -33,14 +24,19 @@ buildPythonPackage rec {
       --replace-fail "hatchling==1.26.1" "hatchling"
   '';
 
-  dependencies = [
-    colorlog
-    jinja2
-    lxml
-    pygments
-    hatch-fancy-pypi-readme
-    hatch-vcs
-  ] ++ lib.optionals (pythonOlder "3.11") [ tomli ];
+  dependencies =
+    with python3Packages;
+    (
+      [
+        colorlog
+        jinja2
+        lxml
+        pygments
+        hatch-fancy-pypi-readme
+        hatch-vcs
+      ]
+      ++ lib.optionals (pythonOlder "3.11") [ tomli ]
+    );
 
   # There are no unit tests in the pypi tarball. Most of the unit tests on the
   # github repository currently only work with gcc5, so we just disable them.
