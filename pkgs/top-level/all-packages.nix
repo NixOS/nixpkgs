b7cb0d230ab0ff115669fd41f3be619922ad59b4
usertam <code@usertam.dev>
2025-03-03 01:39:58 +0800

    colmap: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4546,9 +4546,7 @@ with pkgs;
     inherit (llvmPackages) lld stdenv;
   };
 
-  colmap = libsForQt5.callPackage ../applications/science/misc/colmap {
-    inherit (config) cudaSupport;
-  };
+  colmap = libsForQt5.callPackage ../by-name/co/colmap/package.nix { };
   colmapWithCuda = colmap.override { cudaSupport = true; };
 
   opensplatWithCuda = opensplat.override { cudaSupport = true; };
