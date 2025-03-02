340300f8f9c1aaa93d803c7aea00cb2f85a75e7b
Robert Schütz <nix@dotlambda.de>
2025-03-02 11:52:15 -0800

    home-assistant.python.pkgs.home-assistant-frontend: 20250221.0 -> 20250228.0
    Changelog: https://github.com/home-assistant/frontend/releases/tag/20250228.0

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -8,7 +8,7 @@ buildPythonPackage rec {
   # the frontend version corresponding to a specific home-assistant version can be found here
   # https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/frontend/manifest.json
   pname = "home-assistant-frontend";
-  version = "20250221.0";
+  version = "20250228.0";
   format = "wheel";
 
   src = fetchPypi {
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     pname = "home_assistant_frontend";
     dist = "py3";
     python = "py3";
-    hash = "sha256-SgNtsXtg/i9AO+xu5O0loDbAifO4rn1GRFE1LmeJLfU=";
+    hash = "sha256-KQ7an1lIL80BYPOLMpNACaVD/OnEwlL1wLF2MpIMtbg=";
   };
 
   # there is nothing to strip in this package
