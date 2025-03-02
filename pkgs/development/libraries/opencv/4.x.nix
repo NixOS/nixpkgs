fbced4e70d4663a5b5c7c317fe4dfe899c6b8d66
Melody Kelly <melody@melody.blue>
2025-03-02 19:46:53 +1000

    opencv: use c++ 17 to fix protobuf support
    fixes #386327

--- pkgs/development/libraries/opencv/4.x.nix
+++ pkgs/development/libraries/opencv/4.x.nix
@@ -417,6 +417,7 @@ effectiveStdenv.mkDerivation {
     (cmakeBool "OPENCV_GENERATE_PKGCONFIG" true)
     (cmakeBool "WITH_OPENMP" true)
     (cmakeBool "BUILD_PROTOBUF" false)
+    (cmakeFeature "CMAKE_CXX_STANDARD" "17") # required to enable protobuf
     (cmakeBool "PROTOBUF_UPDATE_FILES" true)
     (cmakeBool "OPENCV_ENABLE_NONFREE" enableUnfree)
     (cmakeBool "BUILD_TESTS" runAccuracyTests)
