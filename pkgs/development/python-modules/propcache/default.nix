239298e02c19b00c4210fab3d62e8538aec788a2
Robert Schütz <nix@dotlambda.de>
2025-03-02 12:36:57 -0800

    Revert "python313Packages.propcache: 0.2.1 -> 0.3.0"
    This reverts commit d83d42f400a5670b794677a76a89a3a878085478.

--- pkgs/development/python-modules/propcache/default.nix
+++ pkgs/development/python-modules/propcache/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "propcache";
-  version = "0.3.0";
+  version = "0.2.1";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -23,13 +23,8 @@ buildPythonPackage rec {
     owner = "aio-libs";
     repo = "propcache";
     tag = "v${version}";
-    hash = "sha256-3jsQnRkXBB7/6xY44kv7JuAXz/P8oxUg8Hyg1O5w2Cg=";
+    hash = "sha256-j2PjSaOx0IKijoMjhtYVNrpqEwRjVFsON5OU/Fv5idc=";
   };
-
-  postPatch = ''
-    substituteInPlace packaging/pep517_backend/_backend.py \
-      --replace "Cython ~= 3.0.12" Cython
-  '';
 
   build-system = [
     cython
@@ -49,7 +44,7 @@ buildPythonPackage rec {
   meta = {
     description = "Fast property caching";
     homepage = "https://github.com/aio-libs/propcache";
-    changelog = "https://github.com/aio-libs/propcache/blob/${src.tag}/CHANGES.rst";
+    changelog = "https://github.com/aio-libs/propcache/blob/${src.rev}/CHANGES.rst";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
