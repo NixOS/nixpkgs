ac60f998db08a0c7fe41e86b0a24576feb682bf6
Jan Tojnar <jtojnar@gmail.com>
2025-03-02 23:58:49 +0000

    glycin-loaders: 1.1.6 → 1.2.rc
    https://gitlab.gnome.org/GNOME/glycin/-/compare/1.1.6...1.2.rc
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/by-name/gl/glycin-loaders/package.nix
+++ pkgs/by-name/gl/glycin-loaders/package.nix
@@ -24,11 +24,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "glycin-loaders";
-  version = "1.1.6";
+  version = "1.2.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/glycin/${lib.versions.majorMinor finalAttrs.version}/glycin-${finalAttrs.version}.tar.xz";
-    hash = "sha256-2EzFaBTyKEEArTQ5pDCDe7IfD5jUbg0rWGifLBlwjwQ=";
+    hash = "sha256-n9kqQEdMIJzUA75GBL1cT42jah6mXnuG0D3KPx7bxhE=";
   };
 
   patches = [
@@ -81,7 +81,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = with lib; {
     description = "Glycin loaders for several formats";
-    homepage = "https://gitlab.gnome.org/sophie-h/glycin";
+    homepage = "https://gitlab.gnome.org/GNOME/glycin";
     teams = [ teams.gnome ];
     license = with licenses; [
       mpl20 # or
