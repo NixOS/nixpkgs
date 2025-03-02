e7116b414454483efab2713fb2e86c761da18170
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-03-02 13:37:38 +1000

    sshamble: Init at 0.0.5

---
+++ pkgs/by-name/ss/sshamble/package.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (rec {
+  pname = "sshamble";
+  version = "0.0.5";
+
+  src = fetchFromGitHub {
+    owner = "runZeroInc";
+    repo = "sshamble";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-EKz7Jz+kILVphpmLhkF4wz43LItGYzXfQGXL2EO8ulw=";
+  };
+
+  vendorHash = "sha256-8p7livwkZ9TYl/4rhWeyKS08z9QkfIV5vAgshFyUi98=";
+
+  doCheck = false; # tries to download from the Internet
+
+  meta = {
+    description = "SSH-protocol pentesting utility";
+    homepage = "https://github.com/runZeroInc/sshamble";
+    license = lib.licenses.bsd2;
+    maintainers = with lib.maintainers; [ YoshiRulz ];
+    mainProgram = "sshamble";
+  };
+})
