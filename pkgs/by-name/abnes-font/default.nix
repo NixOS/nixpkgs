5cceda2be9cff81e804176c269399dfc01a53ed4
Dr. Void <dr.void_industries@proton.me>
2025-03-01 22:34:05 -0500

    Abnes-font: init 1.0.0 - added myself to the maintaners list
    Abnes-font: init 1.0.0 - Moved package to correct location
    
    Abnes-font: init 1.0.0 - Corrected upstream url and update description

--- pkgs/by-name/abnes-font/default.nix
+++ pkgs/by-name/abnes-font/default.nix
@@ -8,7 +8,7 @@ stdenvNoCC.mkDerivation {
   version = "1.0.0";
 
   src = fetchzip {
-    url = "https://fonts.resourceboy.com/file/rb-fonts/2024/08/58962/Abnes-Font.zip";
+    url = "https://befonts.com/wp-content/uploads/2020/10/abnes.zip";
     hash = "sha256-x/AhooCW0+JF66kNaY96iB3+HpAd+l6QJ3bDZxf1xXk=";
     stripRoot = false;
   };
@@ -26,7 +26,7 @@ stdenvNoCC.mkDerivation {
   '';
 
   meta = {
-    description = "Abnes Font made by 177 Studio free only for indivduals";
+    description = "Font made by 177 Studio free for personal use.";
     homepage = "https://177studio.com/product/abnes-elegant-sans-serif-font/";
     license = lib.licenses.unfreeRedistributable;
     platforms = lib.platforms.all;
