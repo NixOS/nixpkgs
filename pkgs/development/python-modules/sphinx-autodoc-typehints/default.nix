1141a3f9d7389854edf1c7731d54309a0d9eedce
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-03-03 06:43:42 +0100

    python313Packages.sphinx-autodoc-typehints: 3.0.1 -> 3.1.0
    https://github.com/tox-dev/sphinx-autodoc-typehints/releases/tag/3.1.0

--- pkgs/development/python-modules/sphinx-autodoc-typehints/default.nix
+++ pkgs/development/python-modules/sphinx-autodoc-typehints/default.nix
@@ -11,7 +11,7 @@
 
 let
   pname = "sphinx-autodoc-typehints";
-  version = "3.0.1";
+  version = "3.1.0";
 in
 
 buildPythonPackage {
@@ -23,7 +23,7 @@ buildPythonPackage {
   src = fetchPypi {
     pname = "sphinx_autodoc_typehints";
     inherit version;
-    hash = "sha256-ubQN0V3uVPb4EMkk+GP5zxxU+fMmXElRQOoBvn9E+lU=";
+    hash = "sha256-prewtt8KOAeDzlspFQwtMDUnRvAno+KU03GDmV0/I+0=";
   };
 
   pythonRelaxDeps = [ "sphinx" ];
