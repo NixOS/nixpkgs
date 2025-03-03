4aec8560e39e0a6bf231fa29bbb3855e25ae8763
Matthias Beyer <mail@beyermatthias.de>
2025-03-03 11:13:46 +0100

    cargo-insta: Fix build
    I messed up by merging to fast. This fixes the build cargo-insta.
    
    Fixes: 9e7fe7a3974b430ad7271cedf2b76c4a3d11be4d ("update cargo-insta 1.40 -> 1.42.2")
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- pkgs/by-name/ca/cargo-insta/package.nix
+++ pkgs/by-name/ca/cargo-insta/package.nix
@@ -16,13 +16,17 @@ rustPlatform.buildRustPackage rec {
 
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-bRxtkuNtCelcYJkWPMl8xkMuGcbGCxvdz5cCzPUk44k=";
+  cargoHash = "sha256-Vx26ArAcsW0NNVNZa4M5hqz/sm1gecrun/bbRWMu07Q=";
 
   checkFlags = [
-  # Depends on `rustfmt` and does not matter for packaging.
-  "--skip=utils::test_format_rust_expression"
-  # Requires networking
-  "--skip=test_force_update_snapshots"
+    # Depends on `rustfmt` and does not matter for packaging.
+    "--skip=utils::test_format_rust_expression"
+    # Requires networking
+    "--skip=test_force_update_snapshots"
+
+    "--skip=test_ignored_snapshots"
+    "--skip=workspace::test_insta_workspace_root"
+    "--skip=env::test_get_cargo_workspace_manifest_dir"
   ];
 
   meta = with lib; {
