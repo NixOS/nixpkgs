c413d82c241a23e86bb6508fa5ed9220798512a4
Mutsuha Asada <me@momee.mt>
2025-03-03 19:28:58 +0900

    satysfiPackages.zrbase: init at 0.4.0

---
+++ pkgs/development/satysfi-packages/zrbase/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+  satyrographosInstallHook,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  name = "satysfi-zrbase";
+  version = "0.4.0";
+
+  src = fetchFromGitHub {
+    owner = "zr-tex8r";
+    repo = "satysfi-zrbase";
+    tag = finalAttrs.version;
+    hash = "sha256-waqV3IJEeFOoex0vqI8zPogc/t3nDF7gQalm3Gfu0cA=";
+  };
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  nativeBuildInputs = [ satyrographosInstallHook ];
+
+  meta = {
+    description = "A collection of packages to make programming in SATySFi more comfortable";
+    homepage = "https://github.com/zr-tex8r/satysfi-zrbase";
+    changelog = "https://github.com/zr-tex8r/satysfi-zrbase/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ momeemt ];
+  };
+})
