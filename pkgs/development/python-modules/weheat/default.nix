9b7ea7fa8e3a4ed16b2aaa9dea386b6e81740b10
Robert Schütz <nix@dotlambda.de>
2025-03-03 09:24:02 -0800

    python313Packages.weheat: 2025.2.13 -> 2025.2.22
    Diff: https://github.com/wefabricate/wh-python/compare/refs/tags/2025.2.13...2025.2.22

--- pkgs/development/python-modules/weheat/default.nix
+++ pkgs/development/python-modules/weheat/default.nix
@@ -7,28 +7,24 @@
   fetchFromGitHub,
   pydantic,
   python-dateutil,
-  pythonOlder,
   setuptools,
   urllib3,
 }:
 
 buildPythonPackage rec {
   pname = "weheat";
-  version = "2025.2.13";
+  version = "2025.2.22";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "wefabricate";
     repo = "wh-python";
     tag = version;
-    hash = "sha256-YXo+pwgCjw2CWUCS9DY9jaLjhj+q+ZqfW3uh1aSXnM0=";
+    hash = "sha256-PhI3aTxkL7UjhjkMB6bCjt/CEu18Y4Sk85rsWhEFwOw=";
   };
 
   pythonRelaxDeps = [
     "urllib3"
-    "pydantic"
   ];
 
   build-system = [ setuptools ];
