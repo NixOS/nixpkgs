c3b1c33c7fca82d66f9ac1c0f2e60bc0b610473b
Adam C. Stephens <adam@valkor.net>
2025-03-03 17:45:10 -0500

    incus: 6.10.0 -> 6.10.1
    https://github.com/lxc/incus/releases/tag/v6.10.1

--- pkgs/by-name/in/incus/package.nix
+++ pkgs/by-name/in/incus/package.nix
@@ -1,11 +1,8 @@
 import ./generic.nix {
-  hash = "sha256-na+YkpjymfpXGf6Y27PzlQH/Ol2hE7z9y7vruClUIb4=";
-  version = "6.10.0";
+  hash = "sha256-uuaJoUrAZ1kSeO2xdXhfdI8Zy4zbA9r1pIhPMqzUios=";
+  version = "6.10.1";
   vendorHash = "sha256-/8aBG8RiC03+oVI9lgPFwbi3b4juumziXL4WH0k/4PA=";
-  patches = [
-    ./20dff3230ea0a1d0a0e26a45c1a4e6c95fb3ada2.patch
-    ./62b7ba1b1a28a9e87793af78db5589aef67dd6b9.patch
-  ];
+  patches = [ ];
   nixUpdateExtraArgs = [
     "--override-filename=pkgs/by-name/in/incus/package.nix"
   ];
