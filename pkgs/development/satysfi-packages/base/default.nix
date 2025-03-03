df2b316e8b0a02100400fbfde8eda1a9a1d20729
Mutsuha Asada <me@momee.mt>
2025-03-03 19:30:35 +0900

    satysfiPackages.base: init at 1.6.0

---
+++ pkgs/development/satysfi-packages/base/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  satyrographosInstallHook,
+  satysfiPackages,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  name = "satysfi-base";
+  version = "1.6.0";
+
+  src = fetchFromGitHub {
+    owner = "nyuichi";
+    repo = "satysfi-base";
+    tag = finalAttrs.version;
+    hash = "sha256-bgWgGmuSHj/rlmNOtMX6o8E7RShvwnkK4hbqjChWF1U=";
+  };
+
+  dontBuild = true;
+  dontUnpack = true;
+
+  nativeBuildInputs = [ satyrographosInstallHook ];
+
+  dependencies = with satysfiPackages; [
+    fonts-dejavu
+    test
+  ];
+
+  meta = {
+    description = "Complementary collection of useful functions and modules for SATySFi";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ momeemt ];
+  };
+})
