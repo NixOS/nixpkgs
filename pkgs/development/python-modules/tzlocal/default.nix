a00e1f9643598c66f181eb7176df19c35e9ac0c6
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-03-03 11:52:35 +0100

    python313Packages.tzlocal: 5.2 -> 5.3
    https://github.com/regebro/tzlocal/blob/5.3/CHANGES.txt

--- pkgs/development/python-modules/tzlocal/default.nix
+++ pkgs/development/python-modules/tzlocal/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "tzlocal";
-  version = "5.2"; # version needs to be compatible with APScheduler
+  version = "5.3"; # version needs to be compatible with APScheduler
 
   disabled = pythonOlder "3.8";
 
@@ -19,7 +19,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-jTmSBVePGpNCgWQJzB5GqT69V1XjnqLYUzS+qRG/Dm4=";
+    hash = "sha256-L6+/wH6di0mt4Y+JjWvNN66IzjrWSGhCouTwOvaDI9I=";
   };
 
   nativeBuildInputs = [ setuptools ];
