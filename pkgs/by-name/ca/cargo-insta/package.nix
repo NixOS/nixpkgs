NixOS/nixpkgs

commit e3f8b2f1445a7f9758186b28aa1d75d8ed4fd381

update cargo-insta 1.40 -> 1.42.2

pkgs/by-name/ca/cargo-insta/package.nix
@@ -5,14 +5,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-insta";
-  version = "1.40";
+  version = "1.42.2";
 
   src = fetchFromGitHub {
     owner = "mitsuhiko";
     repo = "insta";
-    rev = "83f33653b687c84823fe6af00806107e1dd4f4b8";
-    hash = "sha256-eau5h75oZpxufTrf0fLHfr+3TIOFXB/kSgHX+o2GtiE=";
+    rev = "e81bae9b7b7f536bd9057158fe5a219facced116";
+    hash = "sha256-5IGp4WuC34wRB7xSiDWzScLvV26yjsdw/LT/7CN9hWc=";
   };
+
 
   useFetchCargoVendor = true;
   cargoHash = "sha256-bRxtkuNtCelcYJkWPMl8xkMuGcbGCxvdz5cCzPUk44k=";

commit 13392df49ad13fdf5ac911f9f23505f9d52de7d9

cargo-insta: Fix build
I messed up by merging to fast. This fixes the build cargo-insta.

Fixes: e3f8b2f1445a7f9758186b28aa1d75d8ed4fd381 ("update cargo-insta 1.40 -> 1.42.2")
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

pkgs/by-name/ca/cargo-insta/package.nix
@@ -16,13 +16,17 @@ rustPlatform.buildRustPackage rec {
 
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-bRxtkuNtCelcYJkWPMl8xkMuGcbGCxvdz5cCzPUk44k=";
+  cargoHash = "sha256-Vx26ArAcsW0NNVNZa4M5hqz/sm1gecrun/bbRWMu07Q=";
 
   checkFlags = [
-  # Depends on `rustfmt` and does not matter for packaging.
-  "--skip=utils::test_format_rust_expression"
-  # Requires networking
-  "--skip=test_force_update_snapshots"
+    # Depends on `rustfmt` and does not matter for packaging.
+    "--skip=utils::test_format_rust_expression"
+    # Requires networking
+    "--skip=test_force_update_snapshots"
+
+    "--skip=test_ignored_snapshots"
+    "--skip=workspace::test_insta_workspace_root"
+    "--skip=env::test_get_cargo_workspace_manifest_dir"
   ];
 
   meta = with lib; {