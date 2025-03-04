2fdddedd68ebed31d2d0a6e0489c27c848f281ba
polyfloyd <floyd@polyfloyd.net>
2025-03-04 21:12:09 +0100

    python3Packages.gnucash: Build from existing gnucash package

--- pkgs/by-name/gn/gnucash/package.nix
+++ pkgs/by-name/gn/gnucash/package.nix
@@ -26,7 +26,11 @@
   swig,
   webkitgtk_4_0,
   wrapGAppsHook3,
+  python ? null,
+  enablePython ? false,
 }:
+
+assert enablePython -> (python != null);
 
 stdenv.mkDerivation rec {
   pname = "gnucash";
@@ -44,6 +48,11 @@ stdenv.mkDerivation rec {
     makeWrapper
     wrapGAppsHook3
     pkg-config
+  ];
+
+  cmakeFlags = lib.optional enablePython [
+    "-DWITH_PYTHON=\"ON\""
+    "-DPYTHON_SYSCONFIG_BUILD=\"$out\""
   ];
 
   buildInputs =
@@ -69,7 +78,8 @@ stdenv.mkDerivation rec {
       JSONParse
       FinanceQuote
       perl
-    ]);
+    ])
+    ++ lib.optional enablePython python;
 
   patches = [
     # this patch disables test-gnc-timezone and test-gnc-datetime which fail due to nix datetime challenges
@@ -80,6 +90,8 @@ stdenv.mkDerivation rec {
     ./0003-remove-valgrind.patch
     # this patch makes gnucash exec the Finance::Quote wrapper directly
     ./0004-exec-fq-wrapper.patch
+    # SWIG adds a new function arg that is not present in the source. Idk, might be due to SWIG 3 vs 4 differences
+    ./0005-fix-swig-invocation.patch
     # this patch fixes the build against icu 76
     (fetchpatch {
       name = "icu-76.patch";
@@ -94,6 +106,20 @@ stdenv.mkDerivation rec {
     })
   ];
 
+  postPatch = ''
+    patch -p0 <<END_PATCH
+      +++ bindings/python/__init__.py
+      @@ -1,3 +1,7 @@
+      +import os
+      +os.environ['GNC_DBD_DIR'] = '${libdbiDrivers}/lib/dbd'
+      +os.environ['GSETTINGS_SCHEMA_DIR'] = '${glib.makeSchemaPath "$out" "gnucash-${version}"}'
+      +
+       # import all the symbols from gnucash_core, so basic gnucash stuff can be
+       # loaded with:
+       # >>> from gnucash import thingy
+        END_PATCH
+  '';
+
   # this needs to be an environment variable and not a cmake flag to suppress
   # guile warning
   env.GUILE_AUTO_COMPILE = "0";
@@ -107,6 +133,7 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
   enableParallelChecking = true;
+  enableParallelBuilding = true;
   checkTarget = "check";
 
   passthru.docs = stdenv.mkDerivation {
