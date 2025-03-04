b356c941f4046676795c7668c7cd3b9247e8676e
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-03-04 11:18:21 +0100

    mumble: Remove bundled-celt option
    This option has been removed by upstream for a long time, as nobody uses
    that codec anymore.
    
    For details see upstream 4d05018c2e4f1bda48f6244b38f1e7bdc06de808

--- pkgs/applications/networking/mumble/default.nix
+++ pkgs/applications/networking/mumble/default.nix
@@ -115,7 +115,6 @@ let
 
       cmakeFlags = [
         "-D server=OFF"
-        "-D bundled-celt=ON"
         "-D bundle-qt-translations=OFF"
         "-D bundled-json=OFF"
         "-D bundled-gsl=OFF"
