6a214373fd4eead61c44fcc7f017de4f34710d19
nicoo <nicoo@mur.at>
2025-03-04 22:00:50 +0000

    fluxviz: init at unstable-2024-11-08

---
+++ pkgs/by-name/fl/fluxviz/package.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  python3,
+  fetchFromGitHub,
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "fluxvis";
+  version = "unstable-2024-11-08";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "adafruit";
+    repo = "fluxvis";
+    rev = "cc567b8611af0f1110efd19b853516b00663155e";
+    hash = "sha256-YySlyfN1JDNyBycATXLWxpqmNYPQH2aEQY6kJ3pjTTU=";
+  };
+
+  # Can't be handled by pythonRelaxDepsHook; clean up when #382125 lands
+  prePatch = ''
+    sed -i '/setuptools_scm\[toml\]/d' pyproject.toml
+  '';
+
+  build-system = with python3.pkgs; [
+    setuptools
+    wheel
+  ];
+
+  dependencies = with python3.pkgs; [
+    numpy
+    scikit-image
+  ];
+
+  pythonImportsCheck = [ "fluxvis" ];
+
+  meta = {
+    description = "A magnetic flux visualizer for Greazeweazle-compatible floppy images";
+    homepage = "https://github.com/adafruit/fluxvis/";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ nicoo ];
+    mainProgram = "fluxvis";
+  };
+}
