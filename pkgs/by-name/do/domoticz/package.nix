ce97605f238bff04d1e42874de9cedcd3b176dab
Petr Zahradnik <junior@zahradnik.cz>
2025-03-04 23:05:47 +0100

    domoticz: fix build on GCC 14, Boost 1.87

--- pkgs/by-name/do/domoticz/package.nix
+++ pkgs/by-name/do/domoticz/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   makeWrapper,
   cmake,
   python3,
@@ -17,6 +18,7 @@
   git,
   libusb-compat-0_1,
   cereal,
+  minizip,
 }:
 
 stdenv.mkDerivation rec {
@@ -28,8 +30,15 @@ stdenv.mkDerivation rec {
     repo = pname;
     rev = version;
     hash = "sha256-D8U1kK3m1zT83YvZ42hGSU9PzBfS1VGr2mxUYbM2vNQ=";
-    fetchSubmodules = true;
   };
+
+  patches = [
+    # Boost 1.87 compatibility, remove once upgraded to 2025.1
+    (fetchpatch {
+      url = "https://github.com/domoticz/domoticz/commit/5d0db89bbd120ed5dc05b4ff8c136f14a42f0cd3.patch";
+      hash = "sha256-FPe83yJKJEgnY3kABy9CTRe1CBh42dPG1ZWCUE5PO8E=";
+    })
+  ];
 
   buildInputs = [
     openssl
@@ -44,6 +53,7 @@ stdenv.mkDerivation rec {
     git
     libusb-compat-0_1
     cereal
+    minizip
   ];
 
   nativeBuildInputs = [
@@ -60,7 +70,7 @@ stdenv.mkDerivation rec {
     "-DUSE_BUILTIN_ZLIB=false"
     "-DUSE_OPENSSL_STATIC=false"
     "-DUSE_STATIC_BOOST=false"
-    "-DUSE_BUILTIN_MINIZIP=true"
+    "-DUSE_BUILTIN_MINIZIP=false"
   ];
 
   installPhase = ''
