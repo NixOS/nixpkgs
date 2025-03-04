fa99e410193228886b9f21ec7bc7b009f97163f8
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-04 11:24:17 +0000

    wasm-tools: 1.225.0 -> 1.226.0

--- pkgs/by-name/wa/wasm-tools/package.nix
+++ pkgs/by-name/wa/wasm-tools/package.nix
@@ -6,20 +6,20 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wasm-tools";
-  version = "1.225.0";
+  version = "1.226.0";
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = "wasm-tools";
     rev = "v${version}";
-    hash = "sha256-8F5mzxHyv6r2YVg3OYGrkXoPUrL1Ff6kE/+S3blOavM=";
+    hash = "sha256-k9E4gzajwm+l+LmCD4mSZ7SoD1GuKpOOo8/BdZGtT3o=";
     fetchSubmodules = true;
   };
 
   # Disable cargo-auditable until https://github.com/rust-secure-code/cargo-auditable/issues/124 is solved.
   auditable = false;
   useFetchCargoVendor = true;
-  cargoHash = "sha256-GbsI3eiAsVPlTkF6DdQTvccpNKGZgCQmAfOCL3LfGjY=";
+  cargoHash = "sha256-HXUvB4aCJR2U5W73Tiq4NuxbtpLJ3EDFuepqed7JWHY=";
   cargoBuildFlags = [
     "--package"
     "wasm-tools"
