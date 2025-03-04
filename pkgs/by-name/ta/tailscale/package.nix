599808056ec5e6d4ab419c9bc70a954a4eb1fefa
06kellyjac <dev@j-k.io>
2025-03-04 21:29:08 +0000

    tailscale: 1.80.2 -> 1.80.3
    Diff: https://github.com/tailscale/tailscale/compare/v1.80.2...v1.80.3
    
    Changelog: https://github.com/tailscale/tailscale/releases/tag/v1.80.3

--- pkgs/by-name/ta/tailscale/package.nix
+++ pkgs/by-name/ta/tailscale/package.nix
@@ -22,7 +22,7 @@
 }:
 
 let
-  version = "1.80.2";
+  version = "1.80.3";
 in
 buildGoModule {
   pname = "tailscale";
@@ -37,7 +37,7 @@ buildGoModule {
     owner = "tailscale";
     repo = "tailscale";
     rev = "v${version}";
-    hash = "sha256-5HGY9hVSnzqmAdXNJdQ+ZvsK/PmyZ94201UHlHclQE8=";
+    hash = "sha256-UOz2EAUlYZx2XBzw8hADO0ti9bgwz19MTg60rSefSB8=";
   };
 
   patches = [
