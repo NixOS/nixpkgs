66d96a76b590edf9fd4f749ad9ad644f762a748e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-03-04 19:03:31 +0100

    phel: update hash, add `installCheck`

--- pkgs/by-name/ph/phel/package.nix
+++ pkgs/by-name/ph/phel/package.nix
@@ -2,6 +2,7 @@
   lib,
   fetchFromGitHub,
   php,
+  versionCheckHook,
 }:
 
 php.buildComposerProject2 (finalAttrs: {
@@ -15,12 +16,13 @@ php.buildComposerProject2 (finalAttrs: {
     hash = "sha256-irN1Dnov6vDiU4xGsf2nxz7/kz1YOMq0yOLYt4HY1EM=";
   };
 
-  vendorHash = "sha256-jcMfGPnGoSP8E1JXPEqKr53amV0d08GvGfe8niwM8Q4=";
+  vendorHash = "sha256-SDLpl2gBvtVjREfcy1WDFqsGRK1fKr2wKPuBkPhApNI=";
 
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+  versionCheckProgramArg = [ "--version" ];
   doInstallCheck = true;
-  postInstallCheck = ''
-    $out/bin/phel --version
-  '';
 
   meta = {
     changelog = "https://github.com/phel-lang/phel-lang/releases/tag/v${finalAttrs.version}";
