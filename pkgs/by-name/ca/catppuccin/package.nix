2e70385ddbca46b5dbb53c4bcab4bf17e723b21b
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-03-04 02:45:01 +0100

    catppuccin: add element

--- pkgs/by-name/ca/catppuccin/package.nix
+++ pkgs/by-name/ca/catppuccin/package.nix
@@ -3,6 +3,7 @@ let
     "bat"
     "bottom"
     "btop"
+    "element"
     "grub"
     "hyprland"
     "k9s"
@@ -76,6 +77,14 @@ let
       repo = "btop";
       rev = "f437574b600f1c6d932627050b15ff5153b58fa3";
       hash = "sha256-mEGZwScVPWGu+Vbtddc/sJ+mNdD2kKienGZVUcTSl+c=";
+    };
+
+    element = fetchFromGitHub {
+      name = "element";
+      owner = "catppuccin";
+      repo = "element";
+      rev = "ddced941a2014107918484263b63e030889777fe";
+      hash = "sha256-8EP/IQW3rdtomHBfnQNIjGbiD6OapPzXPFLjziNDcmc=";
     };
 
     grub = fetchFromGitHub {
@@ -227,6 +236,11 @@ lib.checkListOfEnum "${pname}: variant" validVariants [ variant ] lib.checkListO
       + lib.optionalString (lib.elem "bottom" themeList) ''
         mkdir -p "$out/bottom"
         cp "${sources.bottom}/themes/${variant}.toml" "$out/bottom/"
+
+      ''
+      + lib.optionalString (lib.elem "element" themeList) ''
+        mkdir -p "$out/element"
+        cp -r "${sources.element}/themes/Catppuccin-${variant}.json" "$out/element/"
 
       ''
       + lib.optionalString (lib.elem "grub" themeList) ''
