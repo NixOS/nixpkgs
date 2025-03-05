acd6a10a1a54fa557088a34d7f14123dd833d7c0
ZHAO Jin-Xiang <xiaoxiangmoe@gmail.com>
2025-03-05 12:52:21 +0800

    wtfutil: add xiaoxiangmoe as maintainer

--- pkgs/by-name/wt/wtfutil/package.nix
+++ pkgs/by-name/wt/wtfutil/package.nix
@@ -42,7 +42,10 @@ buildGoModule rec {
     homepage = "https://wtfutil.com/";
     changelog = "https://github.com/wtfutil/wtf/raw/v${version}/CHANGELOG.md";
     license = licenses.mpl20;
-    maintainers = with maintainers; [ kalbasit ];
+    maintainers = with maintainers; [
+      xiaoxiangmoe
+      kalbasit
+    ];
     mainProgram = "wtfutil";
     platforms = platforms.linux ++ platforms.darwin;
   };
