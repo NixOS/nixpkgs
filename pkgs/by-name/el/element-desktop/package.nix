2144da32f61a0cfc10e70ced6e8b973701f4d143
teutat3s <10206665+teutat3s@users.noreply.github.com>
2025-03-05 10:32:04 +0100

    element-desktop: use electron_34, follow upstream
    version bump, released in v1.11.92
    
    https://github.com/element-hq/element-desktop/commit/9ce980d2006e26f32300acdd78d1bac497a87e73

--- pkgs/by-name/el/element-desktop/package.nix
+++ pkgs/by-name/el/element-desktop/package.nix
@@ -8,7 +8,7 @@
   nodejs,
   fetchYarnDeps,
   jq,
-  electron_33,
+  electron_34,
   element-web,
   sqlcipher,
   callPackage,
@@ -22,7 +22,7 @@ let
   pinData = import ./element-desktop-pin.nix;
   inherit (pinData.hashes) desktopSrcHash desktopYarnHash;
   executableName = "element-desktop";
-  electron = electron_33;
+  electron = electron_34;
   keytar = callPackage ./keytar {
     inherit electron;
   };
