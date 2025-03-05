5169b9576ac7ee6dc2bd47fa57bfc84adac2712e
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-03-05 20:42:08 -0300

    komac: migrate to `finalAttrs`

--- pkgs/by-name/ko/komac/package.nix
+++ pkgs/by-name/ko/komac/package.nix
@@ -15,18 +15,14 @@
   nix-update-script,
   bzip2,
 }:
-
-let
+rustPlatform.buildRustPackage (finalAttrs: {
   version = "2.10.1";
   src = fetchFromGitHub {
     owner = "russellbanks";
     repo = "Komac";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-oqnFenSFWCe3vax5mqstvNNTFWOecLOkuhJzaxv78yE=";
   };
-in
-rustPlatform.buildRustPackage {
-  inherit version src;
 
   pname = "komac";
 
@@ -72,7 +68,7 @@ rustPlatform.buildRustPackage {
 
   passthru = {
     tests.version = testers.testVersion {
-      inherit version;
+      inherit (finalAttrs) version;
 
       package = komac;
       command = "komac --version";
@@ -84,7 +80,7 @@ rustPlatform.buildRustPackage {
   meta = {
     description = "Community Manifest Creator for WinGet";
     homepage = "https://github.com/russellbanks/Komac";
-    changelog = "https://github.com/russellbanks/Komac/releases/tag/v${version}";
+    changelog = "https://github.com/russellbanks/Komac/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [
       kachick
@@ -92,4 +88,4 @@ rustPlatform.buildRustPackage {
     ];
     mainProgram = "komac";
   };
-}
+})
