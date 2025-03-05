eec6308ec9df295ad86540223cf435f302114128
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-03-05 09:39:20 +0100

    mumble: simplify how zeroc-ice is integrated
    zeroc-ice does not have separate lib and dev outputs, and also the
    CMAKE_PREFIX_PATH is not required to build the server.

--- pkgs/applications/networking/mumble/default.nix
+++ pkgs/applications/networking/mumble/default.nix
@@ -147,8 +147,7 @@ let
           (lib.cmakeBool "ice" iceSupport)
         ]
         ++ lib.optionals iceSupport [
-          "-D Ice_HOME=${lib.getDev zeroc-ice};${lib.getLib zeroc-ice}"
-          "-D CMAKE_PREFIX_PATH=${lib.getDev zeroc-ice};${lib.getLib zeroc-ice}"
+          "-D Ice_HOME=${lib.getDev zeroc-ice}"
           "-D Ice_SLICE_DIR=${lib.getDev zeroc-ice}/share/ice/slice"
         ];
 
