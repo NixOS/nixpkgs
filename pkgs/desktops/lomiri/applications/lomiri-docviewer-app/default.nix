2817a42f7437901e4a826b38439d3bf8b14d24b9
OPNA2608 <opna2608@protonmail.com>
2025-03-05 21:41:43 +0100

    lomiri.lomiri-docviewer-app: 3.1.0 -> 3.1.1

--- pkgs/desktops/lomiri/applications/lomiri-docviewer-app/default.nix
+++ pkgs/desktops/lomiri/applications/lomiri-docviewer-app/default.nix
@@ -2,8 +2,6 @@
   stdenv,
   lib,
   fetchFromGitLab,
-  fetchpatch,
-  fetchpatch2,
   gitUpdater,
   nixosTests,
   cmake,
@@ -21,31 +19,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lomiri-docviewer-app";
-  version = "3.1.0";
+  version = "3.1.1";
 
   src = fetchFromGitLab {
     owner = "ubports";
     repo = "development/apps/lomiri-docviewer-app";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-zesBZmaMiMJwHtj3SoaNeHPiM9VNGEa4nTIiG8nskqI=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-U3Rn8KmvUKUI3FcgI8S5sVUb+lGvCz7Hmh5bB4GUAJo=";
   };
-
-  patches = [
-    # Remove when https://gitlab.com/ubports/development/apps/lomiri-docviewer-app/-/merge_requests/76 merged & in release
-    # fetchpatch2 because there's a file rename
-    (fetchpatch2 {
-      name = "1041-lomiri-docviewer-app-Configurable-LibreOffice-path.patch";
-      url = "https://gitlab.com/ubports/development/apps/lomiri-docviewer-app/-/commit/6e1aee99b31b88a90b07f3c5fcf6340c54ce9aaa.patch";
-      hash = "sha256-KdHyKXM0hMMIFkuDn5JZJOEuitWAXT2QQOuR+1AolP0=";
-    })
-
-    # Remove when https://gitlab.com/ubports/development/apps/lomiri-docviewer-app/-/merge_requests/81 merged & in release
-    (fetchpatch {
-      name = "1051-lomiri-docviewer-app-XDGify-icon.patch";
-      url = "https://gitlab.com/ubports/development/apps/lomiri-docviewer-app/-/commit/a319e648ba15a7868d9ceb3a77ea15ad196e515b.patch";
-      hash = "sha256-JMSnN8EyWPHhqHzaJxy3JIhNaOvPLYkVDnNCrPGbO4E=";
-    })
-  ];
 
   postPatch = ''
     substituteInPlace cmake/modules/Click.cmake \
