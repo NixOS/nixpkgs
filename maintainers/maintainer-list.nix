4e5bd986e4563474d35c60ec6be8517875e2e1cd
vaavaav <the.jprp@gmail.com>
2025-03-05 14:17:08 +0000

    Add @vaavaav to the maintainers list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -24450,6 +24450,11 @@
     github = "deviant";
     githubId = 68829907;
   };
+  vaavaav = {
+    name = "Pedro Peixoto";
+    github = "vaavaav";
+    githubId = 56087034;
+  };
   vaci = {
     email = "vaci@vaci.org";
     github = "vaci";
