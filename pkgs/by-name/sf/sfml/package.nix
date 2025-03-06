04e213a26cf6f4ae44178589bfd439956b380760
Gaetan Lepage <gaetan@glepage.com>
2025-03-07 00:59:24 +0100

    sfml: 2.5.1 -> 3.0.0, cleanup
    Diff: https://github.com/SFML/SFML/compare/2.5.1...3.0.0
    
    Changelog: https://github.com/SFML/SFML/blob/3.0.0/changelog.md

--- pkgs/by-name/sf/sfml/package.nix
+++ pkgs/by-name/sf/sfml/package.nix
@@ -2,79 +2,75 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
+
+  # nativeBuildInputs
   cmake,
-  libX11,
-  freetype,
-  libjpeg,
-  openal,
+
+  # buildInputs
   flac,
-  libvorbis,
+  freetype,
   glew,
+  libjpeg,
+  libvorbis,
+  openal,
+  udev,
+  libX11,
+  libXcursor,
+  libXi,
   libXrandr,
   libXrender,
-  udev,
   xcbutilimage,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "sfml";
-  version = "2.5.1";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "SFML";
     repo = "SFML";
-    rev = version;
-    sha256 = "sha256-Xt2Ct4vV459AsSvJxQfwMsNs6iA5y3epT95pLWJGeSk=";
+    tag = finalAttrs.version;
+    hash = "sha256-e6x/L2D3eT6F/DBLQDZ+j0XD5NL9RalWZA8kcm9lZ3g=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/macports/macports-ports/raw/4df1fc235a708ff28200ffc0a39120974ed4b6e1/multimedia/sfml/files/patch-apple-silicon.diff";
-      extraPrefix = "";
-      sha256 = "sha256-9dNawJaYtkugR+2NvhQOhgsf6w9ZXHkBgsDRh8yAJc0=";
-    })
-    (fetchpatch {
-      url = "https://github.com/SFML/SFML/commit/bf92efe9a4035fee0258386173d53556aa196e49.patch";
-      hash = "sha256-1htwPfpn7Z6s/3b+/i1tQ+btjr/tWv5m6IyDVMBNqQA=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake ];
   buildInputs =
     [
-      freetype
-      libjpeg
-      openal
       flac
-      libvorbis
+      freetype
       glew
+      libjpeg
+      libvorbis
+      openal
     ]
     ++ lib.optional stdenv.hostPlatform.isLinux udev
     ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [
       libX11
+      libXcursor
+      libXi
       libXrandr
       libXrender
       xcbutilimage
     ];
 
   cmakeFlags = [
-    "-DSFML_INSTALL_PKGCONFIG_FILES=yes"
-    "-DSFML_MISC_INSTALL_PREFIX=share/SFML"
-    "-DSFML_BUILD_FRAMEWORKS=no"
-    "-DSFML_USE_SYSTEM_DEPS=yes"
+    (lib.cmakeBool "SFML_INSTALL_PKGCONFIG_FILES" true)
+    (lib.cmakeFeature "SFML_MISC_INSTALL_PREFIX" "share/SFML")
+    (lib.cmakeBool "SFML_BUILD_FRAMEWORKS" false)
+    (lib.cmakeBool "SFML_USE_SYSTEM_DEPS" true)
   ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://www.sfml-dev.org/";
+    changelog = "https://github.com/SFML/SFML/blob/${finalAttrs.version}/changelog.md";
     description = "Simple and fast multimedia library";
     longDescription = ''
       SFML is a simple, fast, cross-platform and object-oriented multimedia API.
       It provides access to windowing, graphics, audio and network.
       It is written in C++, and has bindings for various languages such as C, .Net, Ruby, Python.
     '';
-    license = licenses.zlib;
-    maintainers = [ maintainers.astsmtl ];
-    platforms = platforms.unix;
+    license = lib.licenses.zlib;
+    maintainers = [ lib.maintainers.astsmtl ];
+    platforms = lib.platforms.unix;
   };
-}
+})
