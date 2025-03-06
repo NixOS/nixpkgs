e88633d02a7f00577027cb9a21e378ae3edc93b2
wuyoli <wuyoli@tilde.team>
2025-03-06 16:49:15 +0100

    interactive-html-bom: init at v9.2.0

---
+++ pkgs/by-name/in/interactive-html-bom/package.nix
@@ -0,0 +1,38 @@
+{
+  fetchFromGitHub,
+  kicad-small,
+  lib,
+  python3Packages,
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "interactiveHtmlBom";
+  version = "2.9.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "openscopeproject";
+    repo = "InteractiveHtmlBom";
+    tag = "v${version}";
+    hash = "sha256-jUHEI0dWMFPQlXei3+0m1ruHzpG1hcRnxptNOXzXDqQ=";
+  };
+
+  build-system = [ python3Packages.hatchling ];
+
+  dependencies = [
+    python3Packages.jsonschema
+    python3Packages.wxpython
+    kicad-small
+  ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Interactive HTML BOM generation for KiCad, EasyEDA, Eagle, Fusion360 and Allegro PCB designer";
+    homepage = "https://github.com/openscopeproject/InteractiveHtmlBom/";
+    license = lib.licenses.mit;
+    changelog = "https://github.com/openscopeproject/InteractiveHtmlBom/releases/tag/v${version}";
+    maintainers = with lib.maintainers; [ wuyoli ];
+    mainProgram = "generate_interactive_bom";
+  };
+}
