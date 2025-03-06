7f9491b68401199d5b8cead65c3d728fc9098b1c
techknowlogick <techknowlogick@gitea.com>
2025-03-05 19:03:54 -0500

    vault: 1.18.5 -> 1.19.0
    Diff: https://github.com/hashicorp/vault/compare/v1.18.5...v1.19.0
    
    Changelog: https://github.com/hashicorp/vault/blob/v1.19.0/CHANGELOG.md

--- pkgs/by-name/va/vault/package.nix
+++ pkgs/by-name/va/vault/package.nix
@@ -12,16 +12,16 @@
 
 buildGoModule rec {
   pname = "vault";
-  version = "1.18.5";
+  version = "1.19.0";
 
   src = fetchFromGitHub {
     owner = "hashicorp";
     repo = "vault";
     rev = "v${version}";
-    hash = "sha256-ZpfUQJdwt7byRUs8sJRdVQABLYGqZVFPfaXwCdK2rR4=";
+    hash = "sha256-upIUKjFvGI8ndDDixgBKkv1vYr8gl8WpDtwbpkvkWmU=";
   };
 
-  vendorHash = "sha256-B3NsCykCBWpWSvWULyf7/3TNToYSciLI/CEVgVF2TKM=";
+  vendorHash = "sha256-xwx6wKBsZ+q4lIxFvxw+K55ARmAN92Xf6nwMLDvVipA=";
 
   proxyVendor = true;
 
