e30004efddcf765bfd0564d6d6df50d91a6435bb
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-06 22:30:51 +0000

    python3Packages.langchain-community: 0.3.17 -> 0.3.19
    Diff: https://github.com/langchain-ai/langchain/compare/refs/tags/langchain-community==0.3.17...langchain-community==0.3.19
    
    Changelog: https://github.com/langchain-ai/langchain/releases/tag/langchain-community==0.3.19

--- pkgs/development/python-modules/langchain-community/default.nix
+++ pkgs/development/python-modules/langchain-community/default.nix
@@ -36,14 +36,14 @@
 
 buildPythonPackage rec {
   pname = "langchain-community";
-  version = "0.3.17";
+  version = "0.3.19";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langchain";
     tag = "langchain-community==${version}";
-    hash = "sha256-+10Q8em74G5RU6VtDqhQJuDsjJ4/EjGM4a3xQzs3Qzo=";
+    hash = "sha256-U7L60GyxRQL9ze22Wy7g6ZdI/IFyAtUe1bRCconv6pg=";
   };
 
   sourceRoot = "${src.name}/libs/community";
