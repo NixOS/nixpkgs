672199a31195c9c483b36e2c18d50c76c4cd9176
Fiona Behrens <me@kloenk.dev>
2025-03-06 17:31:06 +0100

    strongswan: add enableTPM2 option and tpm2-tss library
    Add a enableTPM2 option to the strongswan package and
    conditionaly add tpm2-tss as input. This enables using tpm2 private
    keys for strongswan as per documentation [0]
    
    [0]: https://docs.strongswan.org/docs/5.9/tpm/tpm2.html

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5037,6 +5037,7 @@ with pkgs;
   stutter = haskell.lib.compose.justStaticExecutables haskellPackages.stutter;
 
   strongswanTNC = strongswan.override { enableTNC = true; };
+  strongswanTPM = strongswan.override { enableTPM2 = true; };
   strongswanNM  = strongswan.override { enableNetworkManager = true; };
 
   stylish-haskell = haskell.lib.compose.justStaticExecutables haskellPackages.stylish-haskell;
