173f3304ce360b9d27aba1375bac90276fe59a1a
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-07 10:07:51 +0000

    jumppad: 0.16.1 -> 0.17.1

--- pkgs/by-name/ju/jumppad/package.nix
+++ pkgs/by-name/ju/jumppad/package.nix
@@ -6,15 +6,15 @@
 
 buildGoModule rec {
   pname = "jumppad";
-  version = "0.16.1";
+  version = "0.17.1";
 
   src = fetchFromGitHub {
     owner = "jumppad-labs";
     repo = "jumppad";
     rev = version;
-    hash = "sha256-2xJsTkgvBYbbKGLr24Mpgcpd3AJkuePi+FXnbZJP2wg=";
+    hash = "sha256-SoiF5y0Vc2T8qT75cII3HqNSEZhWAzKEk3xw4BGICpo=";
   };
-  vendorHash = "sha256-dxMCkLrE97fpRI60PqWoo/+HKDbTLKmMNtegDigQPAI=";
+  vendorHash = "sha256-mJKawveIoDu2v+GxIoljmFbCwle9d1SQiHzsoerP66I=";
 
   subPackages = [ "." ];
 
