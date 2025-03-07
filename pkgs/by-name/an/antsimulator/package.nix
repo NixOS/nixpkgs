cf6cb14d6a2ec30f01d09056bdeabaa4485411d3
Gaetan Lepage <gaetan@glepage.com>
2025-03-07 23:39:34 +0100

    antsimulator: cleanup, use sfml_2

--- pkgs/by-name/an/antsimulator/package.nix
+++ pkgs/by-name/an/antsimulator/package.nix
@@ -3,45 +3,49 @@
   stdenv,
   fetchFromGitHub,
   cmake,
-  sfml,
+  sfml_2,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "antsimulator";
   version = "3.1";
 
   src = fetchFromGitHub {
     owner = "johnBuffer";
     repo = "AntSimulator";
-    rev = "v${version}";
-    sha256 = "sha256-1KWoGbdjF8VI4th/ZjAzASgsLEuS3xiwObulzxQAppA=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-1KWoGbdjF8VI4th/ZjAzASgsLEuS3xiwObulzxQAppA=";
   };
 
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ sfml ];
+  buildInputs = [ sfml_2 ];
 
   postPatch = ''
     substituteInPlace src/main.cpp \
-      --replace "res/" "$out/opt/antsimulator/"
+      --replace-fail "res/" "$out/opt/antsimulator/"
 
     substituteInPlace include/simulation/config.hpp \
-      --replace "res/" "$out/opt/antsimulator/"
+      --replace-fail "res/" "$out/opt/antsimulator/"
 
     substituteInPlace include/render/colony_renderer.hpp \
-      --replace "res/" "$out/opt/antsimulator/"
+      --replace-fail "res/" "$out/opt/antsimulator/"
   '';
 
   installPhase = ''
+    runHook preInstall
+
     install -Dm644 -t $out/opt/antsimulator res/*
     install -Dm755 ./AntSimulator $out/bin/antsimulator
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/johnBuffer/AntSimulator";
     description = "Simple Ants simulator";
     mainProgram = "antsimulator";
-    license = licenses.mit;
+    license = lib.licenses.mit;
     maintainers = [ ];
-    platforms = platforms.unix;
+    platforms = lib.platforms.unix;
   };
-}
+})
