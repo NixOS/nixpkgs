e97e847bc5f5fd98d0ba7f7ac56390ebc8c327b1
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-07 05:34:28 +0000

    above: 2.7 -> 2.8

--- pkgs/by-name/ab/above/package.nix
+++ pkgs/by-name/ab/above/package.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "above";
-  version = "2.7";
+  version = "2.8";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "casterbyte";
     repo = "Above";
     tag = "v${version}";
-    hash = "sha256-tOSAci9aIALNCL3nLui96EdvqjNxnnuj2/dMdWLY9yI=";
+    hash = "sha256-kG+eaTT72jmeC9R9IKwfd/+9oLAzHLJoKfFJhJDJzDM=";
   };
 
   build-system = with python3.pkgs; [ setuptools ];
@@ -33,7 +33,7 @@ python3.pkgs.buildPythonApplication rec {
   meta = {
     description = "Invisible network protocol sniffer";
     homepage = "https://github.com/casterbyte/Above";
-    changelog = "https://github.com/casterbyte/Above/releases/tag/v${version}";
+    changelog = "https://github.com/casterbyte/Above/releases/tag/${src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "above";
