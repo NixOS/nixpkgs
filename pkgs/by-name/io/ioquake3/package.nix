5097d869283804b352f7b4e91edce52111ac2c09
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-03-07 21:07:47 +0100

    ioquake3: Fix build on darwin

--- pkgs/by-name/io/ioquake3/package.nix
+++ pkgs/by-name/io/ioquake3/package.nix
@@ -18,6 +18,7 @@
   makeDesktopItem,
   freetype,
   mumble,
+  bc,
 }:
 
 stdenv.mkDerivation {
@@ -36,6 +37,7 @@ stdenv.mkDerivation {
     makeBinaryWrapper
     pkg-config
     which
+    bc
   ];
 
   buildInputs = [
@@ -58,16 +60,48 @@ stdenv.mkDerivation {
     cp ${./Makefile.local} ./Makefile.local
   '';
 
+  preBuild = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    substituteInPlace Makefile \
+      --replace-fail \
+        "-I/Library/Frameworks/SDL2.framework/Headers" \
+        "-I${lib.getDev SDL2}/include/SDL2" \
+      --replace-fail \
+        "CLIENT_LIBS += -framework SDL2" \
+        "CLIENT_LIBS += -L${lib.getDev SDL2}/lib -lSDL2" \
+      --replace-fail \
+        "RENDERER_LIBS += -framework SDL2" \
+        "RENDERER_LIBS += -L${lib.getDev SDL2}/lib -lSDL2" \
+      --replace-fail \
+        "-I/System/Library/Frameworks/OpenAL.framework/Headers" \
+        "-I${lib.getDev openal}/include/AL" \
+      --replace-fail \
+        "CLIENT_LIBS += -framework OpenAL" \
+        "CLIENT_LIBS += -L${lib.getDev openal}/lib -lopenal" \
+      --replace-fail \
+        "TOOLS_CC = gcc" \
+        "TOOLS_CC = clang"
+  '';
+
+  postBuild = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    echo "Building Application Bundle for Darwin / MacOS"
+    ./make-macosx.sh ${stdenv.hostPlatform.darwinArch}
+  '';
+
   installTargets = [ "copyfiles" ];
 
   installFlags = [ "COPYDIR=$(out)/share/ioquake3" ];
 
-  postInstall = ''
-    install -Dm644 misc/quake3.svg $out/share/icons/hicolor/scalable/apps/ioquake3.svg
+  postInstall =
+    ''
+      install -Dm644 misc/quake3.svg $out/share/icons/hicolor/scalable/apps/ioquake3.svg
 
-    makeWrapper $out/share/ioquake3/ioquake3.* $out/bin/ioquake3
-    makeWrapper $out/share/ioquake3/ioq3ded.* $out/bin/ioq3ded
-  '';
+      makeWrapper $out/share/ioquake3/ioquake3.* $out/bin/ioquake3
+      makeWrapper $out/share/ioquake3/ioq3ded.* $out/bin/ioq3ded
+    ''
+    + lib.optionalString stdenv.hostPlatform.isDarwin ''
+      mkdir -p $out/Applications
+      mv build/release-darwin-${stdenv.hostPlatform.darwinArch}/ioquake3.app $out/Applications/
+    '';
 
   desktopItems = [
     (makeDesktopItem {
@@ -93,6 +127,6 @@ stdenv.mkDerivation {
       drupol
       rvolosatovs
     ];
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.unix;
   };
 }
