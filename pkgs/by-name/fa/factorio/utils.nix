36c68494a173ee5dc5699f157c9e1ffc1f8c8e78
Henrik Lievonen <henrik.lievonen@hotmail.com>
2025-03-07 17:34:27 +0200

    nixos/factorio: autoformat files

--- pkgs/by-name/fa/factorio/utils.nix
+++ pkgs/by-name/fa/factorio/utils.nix
@@ -14,49 +14,57 @@ let
     ;
 in
 {
-  mkModDirDrv = mods: modsDatFile: # a list of mod derivations
+  mkModDirDrv =
+    mods: modsDatFile: # a list of mod derivations
     let
-      recursiveDeps = modDrv: [modDrv] ++ map recursiveDeps modDrv.deps;
+      recursiveDeps = modDrv: [ modDrv ] ++ map recursiveDeps modDrv.deps;
       modDrvs = unique (flatten (map recursiveDeps mods));
     in
     stdenv.mkDerivation {
       name = "factorio-mod-directory";
 
       preferLocalBuild = true;
-      buildCommand = ''
-        mkdir -p $out
-        for modDrv in ${toString modDrvs}; do
-          # NB: there will only ever be a single zip file in each mod derivation's output dir
-          ln -s $modDrv/*.zip $out
-        done
-      '' + (optionalString (modsDatFile != null) ''
-       cp ${modsDatFile} $out/mod-settings.dat
-      '');
+      buildCommand =
+        ''
+          mkdir -p $out
+          for modDrv in ${toString modDrvs}; do
+            # NB: there will only ever be a single zip file in each mod derivation's output dir
+            ln -s $modDrv/*.zip $out
+          done
+        ''
+        + (optionalString (modsDatFile != null) ''
+          cp ${modsDatFile} $out/mod-settings.dat
+        '');
     };
 
-    modDrv = { allRecommendedMods, allOptionalMods }:
-      { src
-      , name ? null
-      , deps ? []
-      , optionalDeps ? []
-      , recommendedDeps ? []
-      }: stdenv.mkDerivation {
+  modDrv =
+    { allRecommendedMods, allOptionalMods }:
+    {
+      src,
+      name ? null,
+      deps ? [ ],
+      optionalDeps ? [ ],
+      recommendedDeps ? [ ],
+    }:
+    stdenv.mkDerivation {
 
-        inherit src;
+      inherit src;
 
-        # Use the name of the zip, but endstrip ".zip" and possibly the querystring that gets left in by fetchurl
-        name = replaceStrings ["_"] ["-"] (if name != null then name else removeSuffix ".zip" (head (splitString "?" src.name)));
+      # Use the name of the zip, but endstrip ".zip" and possibly the querystring that gets left in by fetchurl
+      name = replaceStrings [ "_" ] [ "-" ] (
+        if name != null then name else removeSuffix ".zip" (head (splitString "?" src.name))
+      );
 
-        deps = deps ++ optionals allOptionalMods optionalDeps
-                    ++ optionals allRecommendedMods recommendedDeps;
+      deps =
+        deps ++ optionals allOptionalMods optionalDeps ++ optionals allRecommendedMods recommendedDeps;
 
-        preferLocalBuild = true;
-        buildCommand = ''
-          mkdir -p $out
-          srcBase=$(basename $src)
-          srcBase=''${srcBase#*-}  # strip nix hash
-          srcBase=''${srcBase%\?*} # strip querystring leftover from fetchurl
-          cp $src $out/$srcBase
-        '';
-      };
+      preferLocalBuild = true;
+      buildCommand = ''
+        mkdir -p $out
+        srcBase=$(basename $src)
+        srcBase=''${srcBase#*-}  # strip nix hash
+        srcBase=''${srcBase%\?*} # strip querystring leftover from fetchurl
+        cp $src $out/$srcBase
+      '';
+    };
 }
