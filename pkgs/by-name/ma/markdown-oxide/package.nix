fcaec330392e6fe6872c6c5053fd456c493595ea
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-03-07 21:44:43 -0300

    markdown-oxide: add HeitorAugustoLN as a maintainer

--- pkgs/by-name/ma/markdown-oxide/package.nix
+++ pkgs/by-name/ma/markdown-oxide/package.nix
@@ -24,6 +24,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     maintainers = with lib.maintainers; [
       linsui
       jukremer
+      HeitorAugustoLN
     ];
     mainProgram = "markdown-oxide";
   };
