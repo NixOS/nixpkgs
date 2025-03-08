84febf04e906806516cca8c2fadcec7a2ffb7606
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-08 00:53:43 +0000

    nixpacks: 1.33.0 -> 1.34.0

--- pkgs/by-name/ni/nixpacks/package.nix
+++ pkgs/by-name/ni/nixpacks/package.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "nixpacks";
-  version = "1.33.0";
+  version = "1.34.0";
 
   src = fetchFromGitHub {
     owner = "railwayapp";
     repo = "nixpacks";
     rev = "v${version}";
-    hash = "sha256-YI/4hr1910ScL3HvjiijRmFq4P3xMxn3el1xPd7i53o=";
+    hash = "sha256-pCCyGFAeeRV9OV6bp5KIUb8/ZD7mktBR0pV+bB14AZc=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-WQqGKkOSmbAcVoVVxL9YdNl9DnirNKGsg5d+s9uwTWc=";
+  cargoHash = "sha256-/Q92s2I/JG745Nd2ZewKx4P/SMdyhXIMsR4oa7TImm8=";
 
   # skip test due FHS dependency
   doCheck = false;
