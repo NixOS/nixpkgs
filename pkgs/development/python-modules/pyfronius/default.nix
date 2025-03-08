14b03f89c1faad141d1e86b40e2f2a68582a37ea
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-08 01:41:46 +0100

    python313Packages.pyfronius: remove disabled

--- pkgs/development/python-modules/pyfronius/default.nix
+++ pkgs/development/python-modules/pyfronius/default.nix
@@ -4,7 +4,6 @@
   aiounittest,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
   pytestCheckHook,
   setuptools,
 }:
@@ -13,8 +12,6 @@ buildPythonPackage rec {
   pname = "pyfronius";
   version = "0.7.7";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "nielstron";
