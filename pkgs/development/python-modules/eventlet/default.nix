7df94eaa676a2db6e09898d00a7ba5b36f8bbee4
K900 <me@0upti.me>
2025-03-09 10:58:25 +0300

    python312Packages.eventlet: disable tests on Darwin

--- pkgs/development/python-modules/eventlet/default.nix
+++ pkgs/development/python-modules/eventlet/default.nix
@@ -49,7 +49,8 @@ buildPythonPackage rec {
   ];
 
   # tests hang on pypy indefinitely
-  doCheck = !isPyPy;
+  # most tests also fail/flake on Darwin
+  doCheck = !isPyPy && !stdenv.hostPlatform.isDarwin;
 
   preCheck = lib.optionalString doCheck ''
     echo "nameserver 127.0.0.1" > resolv.conf
