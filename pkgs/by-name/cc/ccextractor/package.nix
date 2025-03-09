c1d2184a49c7e590536020fc8121531dc96a5527
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-03-09 19:38:36 +0100

    ccextractor: directly pass cargoRoot to fetcher

--- pkgs/by-name/cc/ccextractor/package.nix
+++ pkgs/by-name/cc/ccextractor/package.nix
@@ -49,10 +49,8 @@ stdenv.mkDerivation (finalAttrs: {
   cargoRoot = "src/rust";
 
   cargoDeps = rustPlatform.fetchCargoVendor {
-    inherit (finalAttrs) src;
-    sourceRoot = "${finalAttrs.src.name}/${finalAttrs.cargoRoot}";
+    inherit (finalAttrs) src cargoRoot;
     patches = [ ./use-rsmpeg-0.15.patch ];
-    patchFlags = [ "-p3" ];
     hash = "sha256-7v3gQghByUDWZLJRRGa/7X2ivUumirq6BbexNQcCXCk=";
   };
 
