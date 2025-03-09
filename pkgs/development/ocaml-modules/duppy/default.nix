d687ef9f68c5bb71e55d86cabd6b30e283402403
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-09 05:41:29 +0000

    ocamlPackages.duppy: 0.9.4 -> 0.9.5

--- pkgs/development/ocaml-modules/duppy/default.nix
+++ pkgs/development/ocaml-modules/duppy/default.nix
@@ -7,7 +7,7 @@
 
 buildDunePackage rec {
   pname = "duppy";
-  version = "0.9.4";
+  version = "0.9.5";
 
   minimalOCamlVersion = "4.07";
 
@@ -15,7 +15,7 @@ buildDunePackage rec {
     owner = "savonet";
     repo = "ocaml-duppy";
     rev = "v${version}";
-    sha256 = "sha256-rVdfAMu26YgS/TZk2XPqaR6KTDLbh9Elkf8rjhSnNO4=";
+    sha256 = "sha256-hWR7utYMxMjz8Cw0j6cgoHlUj4Jc7Q4vJHD5kGHN4Rc=";
   };
 
   propagatedBuildInputs = [ re ];
