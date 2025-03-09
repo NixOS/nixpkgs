0b8a6620149e35cdc24c54e2eb2c89cf410cae43
Sigmanificient <edhyjox@gmail.com>
2025-03-09 18:52:16 +0100

    bristol: modernize

--- pkgs/by-name/br/bristol/package.nix
+++ pkgs/by-name/br/bristol/package.nix
@@ -5,8 +5,10 @@
   alsa-lib,
   libjack2,
   pkg-config,
+  libX11,
+  libXext,
+  xorgproto,
   libpulseaudio,
-  xorg,
   copyDesktopItems,
   makeDesktopItem,
 }:
@@ -17,20 +19,21 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://sourceforge/bristol/${pname}-${version}.tar.gz";
-    sha256 = "1fi2m4gmvxdi260821y09lxsimq82yv4k5bbgk3kyc3x1nyhn7vx";
+    hash = "sha256-fR8LvQ19MD/HfGuVSbYXCNeoO03AB4GAEbH1XR+pIro=";
   };
 
   nativeBuildInputs = [
     pkg-config
     copyDesktopItems
   ];
+
   buildInputs = [
     alsa-lib
     libjack2
     libpulseaudio
-    xorg.libX11
-    xorg.libXext
-    xorg.xorgproto
+    libX11
+    libXext
+    xorgproto
   ];
 
   postPatch = ''
@@ -38,7 +41,10 @@ stdenv.mkDerivation rec {
     sed -i '35i void doPitchWheel(Baudio *baudio);' bristol/bristolmemorymoog.c
   '';
 
-  configurePhase = "./configure --prefix=$out --enable-jack-default-audio --enable-jack-default-midi";
+  configureFlags = [
+    "--enable-jack-default-audio"
+    "--enable-jack-default-midi"
+  ];
 
   # Workaround build failure on -fno-common toolchains like upstream
   # gcc-10. Otherwise build fails as:
@@ -67,10 +73,10 @@ stdenv.mkDerivation rec {
     })
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Range of synthesiser, electric piano and organ emulations";
     homepage = "https://bristol.sourceforge.net";
-    license = licenses.gpl3;
+    license = lib.licenses.gpl3;
     platforms = [
       "x86_64-linux"
       "i686-linux"
