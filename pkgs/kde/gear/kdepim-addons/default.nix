87eced35408b80db580b632970c91ed14fce72cf
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-03-09 19:23:42 +0100

    kdePackages.kdepim-addons: directly pass cargoRoot to fetcher

--- pkgs/kde/gear/kdepim-addons/default.nix
+++ pkgs/kde/gear/kdepim-addons/default.nix
@@ -16,10 +16,8 @@ mkKdeDerivation rec {
   cargoRoot = "plugins/webengineurlinterceptor/adblock";
 
   cargoDeps = rustPlatform.fetchCargoVendor {
-    # include version in the name so we invalidate the FOD
-    name = "${pname}-${version}";
+    inherit pname version cargoRoot;
     src = sources.${pname};
-    sourceRoot = "${pname}-${version}/${cargoRoot}";
     hash = "sha256-v1TJ8xu4zXXig+ESYT0ZL6l1gOTbqyVA1P/6T/YnW0k=";
   };
 
