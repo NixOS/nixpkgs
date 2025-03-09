1aac9d4617f8d077e0ed045d13224847fdaa7894
Marc Scholten <marc@digitallyinduced.com>
2025-03-09 11:36:51 -0700

    haskellPackages.blucontrol: jailbreak

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2736,6 +2736,12 @@ self: super: {
       sha256 = "sha256-ZdCXMhni+RGisRODiElObW5c4hKy2giWQmWnatqeRJo=";
     }))
   ];
+
+  # jailbreak to allow rerefined >=0.7.0
+  binrep = doJailbreak super.binrep;
+
+  # jailbreak to allow deepseq >= 1.5
+  blucontrol = doJailbreak super.blucontrol;
 } // import ./configuration-tensorflow.nix {inherit pkgs haskellLib;} self super
 
 # Gogol Packages
