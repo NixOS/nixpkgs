0805a446473b892fb7f3093ac147a9cb5221dacc
emaryn <emaryn@users.noreply.github.com>
2025-03-11 06:42:17 +0800

    vscode-extensions.asvetliakov.vscode-neovim: 1.18.17 -> 1.18.18

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -408,8 +408,8 @@ let
         mktplcRef = {
           name = "vscode-neovim";
           publisher = "asvetliakov";
-          version = "1.18.17";
-          hash = "sha256-lMiLfHYeu53fk+xVH18G4Z1AmVlnugMXwuvbFLjU1T8=";
+          version = "1.18.18";
+          hash = "sha256-pPsUSEsolAWWJs9epvLipOJuOJ38zBWTpV2XEtLgdmY=";
         };
         meta = {
           changelog = "https://marketplace.visualstudio.com/items/asvetliakov.vscode-neovim/changelog";
