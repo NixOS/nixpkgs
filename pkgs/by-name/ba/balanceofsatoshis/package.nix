b7a0370d54cd7c873696e39dbb61dc56b43ff88a
Maria <tuy_mearly@aleeas.com>
2025-03-10 09:03:05 +0300

    balanceofsatoshis: 15.8.15 -> 19.4.10

--- pkgs/by-name/ba/balanceofsatoshis/package.nix
+++ pkgs/by-name/ba/balanceofsatoshis/package.nix
@@ -8,16 +8,16 @@
 
 buildNpmPackage rec {
   pname = "balanceofsatoshis";
-  version = "15.8.15";
+  version = "19.4.10";
 
   src = fetchFromGitHub {
     owner = "alexbosworth";
     repo = "balanceofsatoshis";
     rev = "v${version}";
-    hash = "sha256-8GWITeFn7ELUH7bxcNlmQvgperQutBwVUhp2yjeEWrM=";
+    hash = "sha256-WJuxe3k8ZLlkB5SpvE1DSyxQsc5bYEKVsM8tt5vdYOU=";
   };
 
-  npmDepsHash = "sha256-lTXv4pEjrzcOK68RO1K007r7dCbAyc45G8Oy8V3XLts=";
+  npmDepsHash = "sha256-dsWYUCPbiF/L3RcdcaWVn6TnU1/XMy9l7eQgHrBYW4o=";
 
   nativeBuildInputs = [
     installShellFiles
@@ -28,12 +28,14 @@ buildNpmPackage rec {
 
   npmFlags = [ "--ignore-scripts" ];
 
-  postInstall = ''
-    installShellCompletion --cmd bos \
-        --bash <($out/bin/bos completion bash) \
-        --zsh <($out/bin/bos completion zsh) \
-        --fish <($out/bin/bos completion fish)
-  '';
+  # Completion changed since v19
+  # https://github.com/alexbosworth/balanceofsatoshis?tab=readme-ov-file#usage
+  #postInstall = ''
+  #  installShellCompletion --cmd bos \
+  #      --bash <($out/bin/bos completion bash) \
+  #      --zsh <($out/bin/bos completion zsh) \
+  #      --fish <($out/bin/bos completion fish)
+  #'';
 
   meta = {
     changelog = "https://github.com/alexbosworth/balanceofsatoshis/blob/${src.rev}/CHANGELOG.md";
@@ -41,6 +43,6 @@ buildNpmPackage rec {
     homepage = "https://github.com/alexbosworth/balanceofsatoshis";
     license = lib.licenses.mit;
     mainProgram = "bos";
-    maintainers = with lib.maintainers; [ mmilata ];
+    maintainers = with lib.maintainers; [ mmilata mariaa144 ];
   };
 }
