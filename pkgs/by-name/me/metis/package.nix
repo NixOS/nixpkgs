3c3275a819407f7644c3b05c42f9686a049c9dd0
qbisi <qbisicwate@gmail.com>
2025-03-10 23:32:09 +0800

    metis: 5.1.0->5.2.1

--- pkgs/by-name/me/metis/package.nix
+++ pkgs/by-name/me/metis/package.nix
@@ -1,34 +1,62 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  unzip,
+  fetchFromGitHub,
+  fetchpatch2,
   cmake,
+  gklib,
+  isILP64 ? false,
+  precision ? "single",
 }:
-
-stdenv.mkDerivation rec {
+assert precision == "single" || precision == "double";
+let
+  configFlags = toString (
+    lib.optional isILP64 "i64=1" ++ lib.optional (precision == "double") "r64=1"
+  );
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "metis";
-  version = "5.1.0";
+  version = "5.2.1";
 
-  src = fetchurl {
-    url = "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${version}.tar.gz";
-    sha256 = "1cjxgh41r8k6j029yxs8msp3z6lcnpm16g5pvckk35kc7zhfpykn";
+  src = fetchFromGitHub {
+    owner = "KarypisLab";
+    repo = "METIS";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-eddLR6DvZ+2LeR0DkknN6zzRvnW+hLN2qeI+ETUPcac=";
   };
 
-  cmakeFlags = [
-    "-DGKLIB_PATH=../GKlib"
-    # remove once updated past https://github.com/KarypisLab/METIS/commit/521a2c360dc21ace5c4feb6dc0b7992433e3cb0f
-    "-DCMAKE_SKIP_BUILD_RPATH=ON"
+  # link metis to gklib
+  patches = [
+    (fetchpatch2 {
+      url = "https://github.com/KarypisLab/METIS/pull/77/commits/8170703201722b25dfae631fc6a6997e9f01a655.patch";
+      hash = "sha256-FKvrepKp0Z3FuXhtpFn+ggveKkzOqCldkKjE4kYu3JY=";
+    })
   ];
+
   nativeBuildInputs = [
-    unzip
     cmake
   ];
 
+  buildInputs = [
+    gklib
+  ];
+
+  preConfigure = ''
+    make ${configFlags} config
+  '';
+
+  cmakeFlags = [
+    (lib.cmakeBool "SHARED" (!stdenv.hostPlatform.isStatic))
+  ];
+
+  passthru = {
+    inherit isILP64 precision;
+  };
+
   meta = {
     description = "Serial graph partitioning and fill-reducing matrix ordering";
-    homepage = "http://glaros.dtc.umn.edu/gkhome/metis/metis/overview";
+    homepage = "https://karypis.github.io/glaros/software/metis/overview.html";
     license = lib.licenses.asl20;
     platforms = lib.platforms.all;
   };
-}
+})
