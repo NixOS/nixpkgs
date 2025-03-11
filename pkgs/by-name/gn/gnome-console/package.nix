5d97670bbe64e92697c76d6e347ebdbd63153ff0
Jan Tojnar <jtojnar@gmail.com>
2025-03-11 23:23:55 +0000

    gnome-console: 47.1 → 48.rc
    https://gitlab.gnome.org/GNOME/gnome-console/-/compare/47.1...48.rc
    
    The TERM=xterm-256color hack is still needed.
    
    Changelog-Reviewed-By: Bobby Rong <rjl931189261@126.com>

--- pkgs/by-name/gn/gnome-console/package.nix
+++ pkgs/by-name/gn/gnome-console/package.nix
@@ -20,11 +20,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-console";
-  version = "47.1";
+  version = "48.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-console/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    hash = "sha256-0/YAtFtRcWaRrukocDMunJqMqJ1VNWXzEx2eKAdHJdA=";
+    hash = "sha256-pOokEmWfm3Cypm2wnke1yjGwbN4Fd/LgqD1baO0fYUk=";
   };
 
   nativeBuildInputs = [
