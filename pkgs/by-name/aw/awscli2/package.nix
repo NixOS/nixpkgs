6dd39fc783f8b1e6313f2754a9584f3064cd0357
Philip White <pmw@amazon.com>
2025-03-11 15:47:29 -0700

    awscli2: 2.24.16 -> 2.24.22

--- pkgs/by-name/aw/awscli2/package.nix
+++ pkgs/by-name/aw/awscli2/package.nix
@@ -64,14 +64,14 @@ let
 in
 py.pkgs.buildPythonApplication rec {
   pname = "awscli2";
-  version = "2.24.16"; # N.B: if you change this, check if overrides are still up-to-date
+  version = "2.24.22"; # N.B: if you change this, check if overrides are still up-to-date
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = "aws-cli";
     tag = version;
-    hash = "sha256-WbCJ3sBCI5wC6QbxY1KKNpJ8hkK6yhonq7F2Gzxw8DA=";
+    hash = "sha256-cqDBUwc9E9TPN5E4CaCxc5sAZgCXalgl2ejGftyzV1k=";
   };
 
   postPatch = ''
