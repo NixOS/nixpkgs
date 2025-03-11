0cbaac610cc648b95460a1a05872323244e26e35
Matthieu Barthel <matthieu@imatt.ch>
2025-03-11 10:12:46 +0100

    nixos/postal: init module
    Co-authored-by: xanderio <alex@xanderio.de>
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -757,6 +757,7 @@
   ./services/mail/opendkim.nix
   ./services/mail/opensmtpd.nix
   ./services/mail/pfix-srsd.nix
+  ./services/mail/postal.nix
   ./services/mail/postfix-tlspol.nix
   ./services/mail/postfix.nix
   ./services/mail/postgrey.nix
