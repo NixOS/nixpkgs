2de4b41a06467c2742d572f0bf2920e3840bcaa7
Savyasachee Jha <genghizkhan91@hawkradius.com>
2025-03-12 01:50:31 +0530

    bookstack: 24.12.1 -> 25.02

---
+++ pkgs/by-name/bo/bookstack/package.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  fetchFromGitHub,
+  php83,
+  nixosTests,
+  dataDir ? "/var/lib/bookstack",
+}:
+
+php83.buildComposerProject2 (finalAttrs: {
+  pname = "bookstack";
+  version = "25.02";
+
+  src = fetchFromGitHub {
+    owner = "bookstackapp";
+    repo = "bookstack";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Aj9R9+jXqq2Th76kfED5ZfZaMyFyK6c8r4LwXGMomL0=";
+  };
+
+  vendorHash = "sha256-VFGCV8g6ph3TckpJQMk0g/F256Mz0t0VFW52jzBfmm0=";
+
+  passthru = {
+    phpPackage = php83;
+    tests = nixosTests.bookstack;
+  };
+
+  postInstall = ''
+    chmod -R u+w $out/share
+    mv $out/share/php/bookstack/* $out
+    rm -R $out/share $out/storage $out/bootstrap/cache $out/public/uploads
+    ln -s ${dataDir}/storage $out/storage
+    ln -s ${dataDir}/cache $out/bootstrap/cache
+    ln -s ${dataDir}/public/uploads $out/public/uploads
+  '';
+
+  meta = {
+    description = "Platform to create documentation/wiki content built with PHP & Laravel";
+    longDescription = ''
+      A platform for storing and organising information and documentation.
+      Details for BookStack can be found on the official website at https://www.bookstackapp.com/.
+    '';
+    homepage = "https://www.bookstackapp.com/";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      ymarkus
+      savyajha
+    ];
+    platforms = lib.platforms.linux;
+  };
+})
