a2fee0e2f0bde8b630dbe30bf87251cbacb926f5
Jonas Heinrich <onny@project-insanity.org>
2025-03-11 17:06:59 +0100

    python3Packages.irisclient: init 1.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4765,8 +4765,6 @@ self: super: with self; {
     callPackage ../development/python-modules/faraday-agent-parameters-types
       { };
 
-  falcon-cors = callPackage ../development/python-modules/falcon-cors { };
-
   faraday-plugins = callPackage ../development/python-modules/faraday-plugins { };
 
   farama-notifications = callPackage ../development/python-modules/farama-notifications { };
@@ -6912,6 +6910,8 @@ self: super: with self; {
   ircstates = callPackage ../development/python-modules/ircstates { };
 
   irctokens = callPackage ../development/python-modules/irctokens { };
+
+  irisclient = callPackage ../development/python-modules/irisclient { };
 
   isal = callPackage ../development/python-modules/isal { };
 
