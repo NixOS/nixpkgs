c03f309ef71008e22f48f51b7daa4c7b21a6543c
Marcell Tóth <elfenermarcell@gmail.com>
2025-03-11 18:13:56 +0100

    maintainers: add elfenermarcell

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6812,6 +6812,12 @@
     githubId = 8146662;
     name = "Eric Lesiuta";
   };
+  elfenermarcell = {
+    email = "elfenermarcell@gmail.com";
+    github = "elfenermarcell";
+    githubId = 183738665;
+    name = "Marcell Tóth";
+  };
   eliandoran = {
     email = "contact@eliandoran.me";
     name = "Elian Doran";
