450a967ffb232dcf99cb5ec9b99545ad36ed601f
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-11 03:23:27 +0000

    python312Packages.trytond: 7.4.4 -> 7.4.7

--- pkgs/development/python-modules/trytond/default.nix
+++ pkgs/development/python-modules/trytond/default.nix
@@ -27,14 +27,14 @@
 
 buildPythonPackage rec {
   pname = "trytond";
-  version = "7.4.4";
+  version = "7.4.7";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-rmwen1XmH7NkUJpgobVPkIGuDbwjzuWKNvOAj8Mwiq8=";
+    hash = "sha256-L0U1SSabD7dQx1Gidpyxno5yIyO0AYYbIddUS82gjCg=";
   };
 
   build-system = [ setuptools ];
