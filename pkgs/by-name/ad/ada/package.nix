605c9d49060b1f28122b39e391b9ebd1e9a54687
Guy Chronister <guychronister@outlook.com>
2025-03-11 15:36:19 -0500

    ada: 3.1.2 -> 3.1.3

--- pkgs/by-name/ad/ada/package.nix
+++ pkgs/by-name/ad/ada/package.nix
@@ -3,17 +3,18 @@
   stdenv,
   fetchFromGitHub,
   cmake,
+  nix-update-script,
 }:
 
 stdenv.mkDerivation rec {
   pname = "ada";
-  version = "3.1.3";
+  version = "3.2.1";
 
   src = fetchFromGitHub {
     owner = "ada-url";
     repo = "ada";
     tag = "v${version}";
-    hash = "sha256-WQjScror93W7E8j34PbVL6FENy83MpnirTgit3/+dWw=";
+    hash = "sha256-x58zGkuKEUqlJEdIvLFzwHZHJAzCsFV9EQCpi+zmV1o=";
   };
 
   nativeBuildInputs = [ cmake ];
@@ -23,6 +24,8 @@ stdenv.mkDerivation rec {
     (lib.cmakeBool "ADA_TOOLS" false)
     (lib.cmakeBool "ADA_TESTING" false)
   ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "WHATWG-compliant and fast URL parser written in modern C";
