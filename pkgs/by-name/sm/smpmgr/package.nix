c41e7d49e65c31d59a10d2e84abe25963762a40f
Otavio Salvador <otavio@ossystems.com.br>
2025-03-11 20:05:06 -0300

    smpmgr: init at 0.11.0
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ pkgs/by-name/sm/smpmgr/package.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  python3,
+  fetchFromGitHub,
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "smpmgr";
+  version = "0.11.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "intercreate";
+    repo = "smpmgr";
+    rev = version;
+    hash = "sha256-MFrM2ckTIngnXcIp8hFDc6ikjHa81mDsYA1t3KWrxdc=";
+  };
+
+  build-system = [
+    python3.pkgs.poetry-core
+    python3.pkgs.setuptools
+    python3.pkgs.wheel
+  ];
+
+  dependencies = with python3.pkgs; [
+    readchar
+    smpclient
+    typer
+  ];
+
+  pythonImportsCheck = [
+    "smpmgr"
+  ];
+
+  meta = {
+    description = "Simple Management Protocol (SMP) Manager for remotely managing MCU firmware";
+    homepage = "https://github.com/intercreate/smpmgr";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ otavio ];
+    mainProgram = "smpmgr";
+  };
+}
