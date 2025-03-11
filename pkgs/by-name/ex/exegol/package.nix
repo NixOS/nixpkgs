27b03627ce845c5b9f6e39c3fdafd21df876cc01
0xthefr34k <tayayassine6@live.fr>
2025-03-11 21:09:27 +0000

    4.3.9 -> 4.3.10

--- pkgs/by-name/ex/exegol/package.nix
+++ pkgs/by-name/ex/exegol/package.nix
@@ -1,32 +1,20 @@
-{
-  fetchPypi,
-  lib,
-  python3,
-  xorg,
-}:
+{ fetchPypi, lib, python3, xorg, }:
 python3.pkgs.buildPythonApplication rec {
   pname = "exegol";
-  version = "4.3.9";
-  format = "setuptools";
+  version = "4.3.10";
+  format = "pyproject";
 
-  # Project has no unit tests
-  doCheck = false;
+  doCheck = true;
+  pythonRelaxDeps = true;
 
-  propagatedBuildInputs =
-    with python3.pkgs;
-    [
-      pyyaml
-      gitpython
-      docker
-      requests
-      rich
-      argcomplete
-    ]
-    ++ [ xorg.xhost ];
+  nativeBuildInputs = with python3.pkgs; [ pdm-backend ];
+
+  propagatedBuildInputs = with python3.pkgs;
+    [ pyyaml gitpython docker requests rich argcomplete ] ++ [ xorg.xhost ];
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-CoPQMEk8eagYU/TfaPAM6ItfSCZbrvzUww8H9ND8VUk=";
+    hash = "sha256-BtOW7EBbFil7yyhL6uayTUUkDldI8+xxolfQZtX+00c=";
   };
 
   meta = with lib; {
@@ -43,9 +31,7 @@ python3.pkgs.buildPythonApplication rec {
     changelog = "https://github.com/ThePorgs/Exegol/releases/tag/${version}";
     license = licenses.gpl3Only;
     mainProgram = "exegol";
-    maintainers = with maintainers; [
-      _0b11stan
-      charB66
-    ];
+    maintainers = with maintainers; [ _0b11stan charB66 ];
   };
 }
+
