887a02ef6907001bcfb1aa0d5e2b2c3bc1aee7ee
Naufal Fikri <naufal@naufik.net>
2025-03-12 19:58:21 +1100

    python3Packages.marqo: init at 3.11.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8108,6 +8108,8 @@ self: super: with self; {
 
   markups = callPackage ../development/python-modules/markups { };
 
+  marqo = callPackage ../development/python-modules/marqo { };
+
   marshmallow = callPackage ../development/python-modules/marshmallow { };
 
   marshmallow-dataclass = callPackage ../development/python-modules/marshmallow-dataclass { };
