d097c8f639cf904bb5ea816a1890547a1d55419a
goyalyashpal <yashpalgoyal1304@gmail.com>
2025-03-12 23:14:56 +0530

    nixos/bash: Simplify prompt set title for non emacs

--- nixos/modules/programs/bash/bash.nix
+++ nixos/modules/programs/bash/bash.nix
@@ -89,12 +89,14 @@ in
 
             PROMPT_COLOR="1;31m"
             ((UID)) && PROMPT_COLOR="1;32m"
-            if [ -n "$INSIDE_EMACS" ]; then
-              # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-              PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
-            else
-              PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+
+            # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+            if [ ! -n "$INSIDE_EMACS" ]; then
+              local TITLE="\[\e]0;\u@\h: \w\a\]"
             fi
+
+            PS1="\n\[\033[$PROMPT_COLOR\][$TITLE\u@\h:\w]\\$\[\033[0m\] "
+
             if test "$TERM" = "xterm"; then
               PS1="\[\033]2;\h:\u:\w\007\]$PS1"
             fi
