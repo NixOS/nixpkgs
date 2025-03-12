99252a24a9373310388d6ce0d26c200532e06693
Austin Horstman <khaneliman12@gmail.com>
2025-03-12 12:55:49 -0500

    vimPlugins.blink-nerdfont-nvim: init at 2025-02-06

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -1527,6 +1527,19 @@ final: prev:
     meta.hydraPlatforms = [ ];
   };
 
+  blink-nerdfont-nvim = buildVimPlugin {
+    pname = "blink-nerdfont.nvim";
+    version = "2025-02-06";
+    src = fetchFromGitHub {
+      owner = "MahanRahmati";
+      repo = "blink-nerdfont.nvim";
+      rev = "2f3cedda78dcf4ef547128ce7f72f7b80e25501d";
+      sha256 = "17cab659iiv6l67xjwk5kj624nhxmmng7xjbgimr4v86yhmirk4m";
+    };
+    meta.homepage = "https://github.com/MahanRahmati/blink-nerdfont.nvim/";
+    meta.hydraPlatforms = [ ];
+  };
+
   blink-ripgrep-nvim = buildVimPlugin {
     pname = "blink-ripgrep.nvim";
     version = "2025-03-08";
