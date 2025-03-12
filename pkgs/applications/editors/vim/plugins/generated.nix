833c26f046357a7c6e313707d2fd2932b856c7e1
Austin Horstman <khaneliman12@gmail.com>
2025-03-12 13:07:08 -0500

    vimPlugins.blink-cmp-conventional-commits: init at 2025-02-18

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -1423,6 +1423,19 @@ final: prev:
     meta.hydraPlatforms = [ ];
   };
 
+  blink-cmp-conventional-commits = buildVimPlugin {
+    pname = "blink-cmp-conventional-commits";
+    version = "2025-02-18";
+    src = fetchFromGitHub {
+      owner = "disrupted";
+      repo = "blink-cmp-conventional-commits";
+      rev = "e7ce3ddcc6d4044067d6d77ab41d480202e814ad";
+      sha256 = "0m72nyg3pm1ig04hvch6lzd4hy77dnmdxxmbq154qwkjhbm0bs6m";
+    };
+    meta.homepage = "https://github.com/disrupted/blink-cmp-conventional-commits/";
+    meta.hydraPlatforms = [ ];
+  };
+
   blink-cmp-copilot = buildVimPlugin {
     pname = "blink-cmp-copilot";
     version = "2025-02-21";
