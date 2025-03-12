cdf14029a155aacb0d897b12c24934b90d94767d
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-03-12 14:07:18 +0000

    maintainers: added szkiba

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22971,6 +22971,12 @@
     githubId = 203195;
     name = "Szczyp";
   };
+  szkiba = {
+    email = "iszkiba@gmail.com";
+    github = "szkiba";
+    githubId = 16244553;
+    name = "Iván Szkiba";
+  };
   szlend = {
     email = "pub.nix@zlender.si";
     github = "szlend";
