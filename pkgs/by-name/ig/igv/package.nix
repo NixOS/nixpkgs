15f0fd87a8157a0c55347706cd80d93c8247beab
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-12 10:58:03 +0000

    igv: 2.17.4 -> 2.19.2

--- pkgs/by-name/ig/igv/package.nix
+++ pkgs/by-name/ig/igv/package.nix
@@ -10,10 +10,10 @@
 
 stdenv.mkDerivation rec {
   pname = "igv";
-  version = "2.17.4";
+  version = "2.19.2";
   src = fetchzip {
     url = "https://data.broadinstitute.org/igv/projects/downloads/${lib.versions.majorMinor version}/IGV_${version}.zip";
-    sha256 = "sha256-LF/rwm/XlLHAJjiAlQVTmx5l+5Np2b5rPjoCdN/qERU=";
+    sha256 = "sha256-zqI2qxxFz2ryCbFx3MYsAcvDmyoRHs1fhzaKG6OzILI=";
   };
 
   installPhase = ''
