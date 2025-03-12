faac29a6d8d8d000f841177c262aecbef190aa88
Picnoir <picnoir@alternativebit.fr>
2025-03-12 11:40:51 +0100

    pleroma: 2.9.0 -> 2.9.1
    Security update.
    
    Full changelog: https://git.pleroma.social/pleroma/pleroma/-/releases/v2.9.1

--- pkgs/servers/pleroma/default.nix
+++ pkgs/servers/pleroma/default.nix
@@ -19,14 +19,14 @@
 
 beamPackages.mixRelease rec {
   pname = "pleroma";
-  version = "2.9.0";
+  version = "2.9.1";
 
   src = fetchFromGitLab {
     domain = "git.pleroma.social";
     owner = "pleroma";
     repo = "pleroma";
     rev = "v${version}";
-    sha256 = "sha256-P+BcPC5TRxJppOFu3hXrse/10ZOU9Yph/GmPTY44CaE=";
+    sha256 = "sha256-mZcr+LlRQFDZVU5yAm0XkFdFHCDp4DZNLoVUlWxknMI=";
   };
 
   patches = [ ./Revert-Config-Restrict-permissions-of-OTP-config.patch ];
