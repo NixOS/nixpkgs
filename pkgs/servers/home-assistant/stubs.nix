a1f29311601d43f0d9faa6e792990f9af7fcf447
Robert Schütz <nix@dotlambda.de>
2025-03-11 21:48:39 -0700

    python313Packages.homeassistant-stubs: 2025.3.1 -> 2025.3.2
    Diff: https://github.com/KapJI/homeassistant-stubs/compare/refs/tags/2025.3.1...2025.3.2
    
    Changelog: https://github.com/KapJI/homeassistant-stubs/releases/tag/2025.3.2

--- pkgs/servers/home-assistant/stubs.nix
+++ pkgs/servers/home-assistant/stubs.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "homeassistant-stubs";
-  version = "2025.3.1";
+  version = "2025.3.2";
   pyproject = true;
 
   disabled = python.version != home-assistant.python.version;
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "KapJI";
     repo = "homeassistant-stubs";
     rev = "refs/tags/${version}";
-    hash = "sha256-EtgSd9TXyGjNvFYB7klcdJNhiVQsAfF18b6J35/7bUM=";
+    hash = "sha256-aUkXbAyuyL8iZuWxFwVJNNYA6ANK8R2UrV5bA7MCLZc=";
   };
 
   build-system = [
