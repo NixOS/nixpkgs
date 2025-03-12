14e7c92e2d832d8d9c5fd07d7c6fbd99c054a636
polyfloyd <floyd@polyfloyd.net>
2025-03-12 19:43:07 +0100

    bash: Make it possible to set alternative LS_COLORS

--- nixos/modules/programs/bash/ls-colors.nix
+++ nixos/modules/programs/bash/ls-colors.nix
@@ -13,11 +13,17 @@ in
     programs.bash.enableLsColors = lib.mkEnableOption "extra colors in directory listings" // {
       default = true;
     };
+    programs.bash.lsColorsFile = lib.mkOption {
+      type = lib.types.path;
+      default = "";
+      example = lib.literalExpression "\${pkgs.dircolors-solarized}/ansi-dark";
+      description = "Alternative colorscheme for ls colors";
+    };
   };
 
   config = lib.mkIf enable {
     programs.bash.promptPluginInit = ''
-      eval "$(${pkgs.coreutils}/bin/dircolors -b)"
+      eval "$(${pkgs.coreutils}/bin/dircolors -b ${config.programs.bash.lsColorsFile})"
     '';
   };
 }
