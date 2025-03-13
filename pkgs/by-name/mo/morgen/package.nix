6ecd6c11e8b9a48df352004d6557329ba2b66988
Ariel Ebersberger <ariel@ebersberger.io>
2025-03-13 19:28:14 +0100

    morgen: 3.6.6 -> 3.6.9

--- pkgs/by-name/mo/morgen/package.nix
+++ pkgs/by-name/mo/morgen/package.nix
@@ -3,12 +3,12 @@
 
 stdenv.mkDerivation rec {
   pname = "morgen";
-  version = "3.6.6";
+  version = "3.6.9";
 
   src = fetchurl {
     name = "morgen-${version}.deb";
     url = "https://dl.todesktop.com/210203cqcj00tw1/versions/${version}/linux/deb";
-    hash = "sha256-QisOXMj6uUMluPruUjtOEK93l+aZe7qsJUAZ589RzvU=";
+    hash = "sha256-An3CfJQ89aqpxMOf20YG+cuEVDrhgMGAm/Scn4hMV8E=";
   };
 
   nativeBuildInputs = [
