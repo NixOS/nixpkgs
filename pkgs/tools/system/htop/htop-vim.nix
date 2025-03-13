3d7450c9708e6b8102a5608b8405fd121f89de2c
Thiago Kenji Okada <thiagokokada@gmail.com>
2025-03-13 22:54:23 +0000

    htop-vim: fix build by not depending in htop derivation
    Fix #389663.

--- pkgs/tools/system/htop/htop-vim.nix
+++ pkgs/tools/system/htop/htop-vim.nix
@@ -1,10 +1,23 @@
 {
   lib,
-  htop,
   fetchFromGitHub,
+  fetchpatch2,
+  stdenv,
+  autoreconfHook,
+  pkg-config,
+  ncurses,
+  darwin,
+  libcap,
+  libnl,
+  sensorsSupport ? stdenv.hostPlatform.isLinux,
+  lm_sensors,
+  systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemd,
+  systemd,
 }:
 
-htop.overrideAttrs (oldAttrs: rec {
+assert systemdSupport -> stdenv.hostPlatform.isLinux;
+
+stdenv.mkDerivation rec {
   pname = "htop-vim";
   version = "unstable-2023-02-16";
 
@@ -15,11 +28,56 @@ htop.overrideAttrs (oldAttrs: rec {
     hash = "sha256-ZfdBAlnjoy8g6xwrR/i2+dGldMOfLlX6DRlNqB8pkGM=";
   };
 
+  patches = [
+    # See https://github.com/htop-dev/htop/pull/1412
+    # Remove when updating to 3.4.0
+    (fetchpatch2 {
+      name = "htop-resolve-configuration-path.patch";
+      url = "https://github.com/htop-dev/htop/commit/0dac8e7d38ec3aeae901a987717b5177986197e4.patch";
+      hash = "sha256-Er1d/yV1fioYfEmXNlLO5ayAyXkyy+IaGSx1KWXvlv0=";
+    })
+  ];
+
+  nativeBuildInputs = [ autoreconfHook ] ++ lib.optional stdenv.hostPlatform.isLinux pkg-config;
+
+  buildInputs =
+    [ ncurses ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [ darwin.IOKit ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [
+      libcap
+      libnl
+    ]
+    ++ lib.optional sensorsSupport lm_sensors
+    ++ lib.optional systemdSupport systemd;
+
+  configureFlags =
+    [
+      "--enable-unicode"
+      "--sysconfdir=/etc"
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [
+      "--enable-affinity"
+      "--enable-capabilities"
+      "--enable-delayacct"
+    ]
+    ++ lib.optional sensorsSupport "--enable-sensors";
+
+  postFixup =
+    let
+      optionalPatch = pred: so: lib.optionalString pred "patchelf --add-needed ${so} $out/bin/htop";
+    in
+    lib.optionalString (!stdenv.hostPlatform.isStatic) ''
+      ${optionalPatch sensorsSupport "${lib.getLib lm_sensors}/lib/libsensors.so"}
+      ${optionalPatch systemdSupport "${systemd}/lib/libsystemd.so"}
+    '';
+
   meta = with lib; {
-    inherit (oldAttrs.meta) platforms license;
-    description = "Interactive process viewer for Linux, with vim-style keybindings";
-    homepage = "https://github.com/KoffeinFlummi/htop-vim";
+    description = "Interactive process viewer";
+    homepage = "https://htop.dev";
+    license = licenses.gpl2Only;
+    platforms = platforms.all;
     maintainers = with maintainers; [ thiagokokada ];
+    changelog = "https://github.com/htop-dev/htop/blob/${version}/ChangeLog";
     mainProgram = "htop";
   };
-})
+}
