46c83dec5fb90a57f1f57c09f91717a803c64466
J. T. Elscott <78996170+99jte@users.noreply.github.com>
2025-03-13 10:05:47 -0700

    kdePackages.khelpcenter: fix manpage search
    Previously the shebang in khc_mansearch.py pointed to /bin/env and caused it to fail; add python3 to the extraBuildInputs to that it gets converted to a nix path.
    Also get the man-db binaries by their nix path rather than hoping that they're in the runtime PATH.

--- pkgs/kde/gear/khelpcenter/default.nix
+++ pkgs/kde/gear/khelpcenter/default.nix
@@ -2,6 +2,8 @@
   mkKdeDerivation,
   qtwebengine,
   xapian,
+  man-db,
+  python3,
 }:
 mkKdeDerivation {
   pname = "khelpcenter";
@@ -9,6 +11,13 @@ mkKdeDerivation {
   extraBuildInputs = [
     qtwebengine
     xapian
+    man-db
+    python3
   ];
+  postPatch = ''
+    substituteInPlace searchhandlers/khc_mansearch.py \
+      --replace-fail "'whatis'" "'${man-db}/bin/whatis'" \
+      --replace-fail "'apropos'" "'${man-db}/bin/apropos'"
+  '';
   meta.mainProgram = "khelpcenter";
 }
