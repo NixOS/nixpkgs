e8102bc46fc1fb3090e0fb3dfeb7521361f98298
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-03-12 20:24:31 -0400

    cobalt: modernize
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/by-name/co/cobalt/package.nix
+++ pkgs/by-name/co/cobalt/package.nix
@@ -3,28 +3,38 @@
   stdenv,
   fetchFromGitHub,
   rustPlatform,
+  versionCheckHook,
+  nix-update-script,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cobalt";
   version = "0.19.8";
 
   src = fetchFromGitHub {
     owner = "cobalt-org";
     repo = "cobalt.rs";
-    rev = "v${version}";
-    sha256 = "sha256-neOJ3UqRisCcyarRIXfHyl9nAe2Wl9IXVDNwIYEQYys=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-neOJ3UqRisCcyarRIXfHyl9nAe2Wl9IXVDNwIYEQYys=";
   };
 
   useFetchCargoVendor = true;
   cargoHash = "sha256-j2xmEoMV7lVhqj4lKWA3QdEDEGUpRlZc4ikZoDQJlB8=";
 
-  meta = with lib; {
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
+  doInstallCheck = true;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
     description = "Static site generator written in Rust";
-    homepage = "https://github.com/cobalt-org/cobalt.rs/";
-    license = licenses.mit;
-    maintainers = with maintainers; [ ethancedwards8 ];
-    platforms = platforms.unix;
+    homepage = "https://cobalt-org.github.io/";
+    downloadPage = "https://github.com/cobalt-org/cobalt.rs/";
+    changelog = "https://github.com/cobalt-org/cobalt.rs/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ethancedwards8 ];
+    platforms = lib.platforms.unix;
     mainProgram = "cobalt";
   };
-}
+})
