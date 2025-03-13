52bb83f3bdc91af64f087d37d0e5c93c2d443d6a
0xthefr34k <tayayassine6@live.fr>
2025-03-13 21:25:49 +0000

    powerview: init at 2025.0.4

---
+++ pkgs/by-name/po/powerview/package.nix
@@ -0,0 +1,53 @@
+{
+  fetchFromGitHub,
+  lib,
+  python3Packages,
+}:
+python3Packages.buildPythonApplication rec {
+  pname = "powerview";
+  version = "2025.0.4";
+  format = "pyproject";
+
+  src = fetchFromGitHub {
+    owner = "aniqfakhrul";
+    repo = "powerview.py";
+    rev = "main";
+    hash = "sha256-855smi1qmEgeso30fXj2ivnvVYtAUrs3ps0EclDbZz0=";
+  };
+
+  build-system = with python3Packages; [
+    poetry-core
+  ];
+
+  dependencies = with python3Packages; [
+    impacket
+    ldap3-bleeding-edge
+    dnspython
+    future
+    gnureadline
+    validators
+    dsinternals
+    chardet
+    tabulate
+    requests-ntlm
+    python-dateutil
+    flask
+  ];
+
+  # Exclude argparse from runtime dependency checks since it is part of Python's standard library
+  pythonRemoveDeps = [ "argparse" ];
+
+  doCheck = false;
+
+  meta = {
+    description = "PowerView.py is an alternative for the awesome original PowerView.ps1 script.";
+    longDescription = ''
+      PowerView.py is an alternative for the awesome original PowerView.ps1 script.
+      Most of the modules used in PowerView are available here (some of the flags are changed).
+      The main goal is to achieve an interactive session without having to repeatedly authenticate to LDAP.
+    '';
+    homepage = "https://github.com/aniqfakhrul/powerview.py";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "powerview";
+  };
+}
