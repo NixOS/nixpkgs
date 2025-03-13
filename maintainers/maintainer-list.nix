f05c639c70c68ebff835d04ff6b333e7965a695e
Kaushal M <kaushal.m@proton.me>
2025-03-13 19:50:30 +0530

    maintainers: add kshlm & stefankeidel

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12875,6 +12875,11 @@
     github = "krzaczek";
     githubId = 5773701;
   };
+  kshlm = {
+    name = "kshlm";
+    github = "kshlm";
+    githubId = 362086;
+  };
   KSJ2000 = {
     email = "katsho123@outlook.com";
     name = "KSJ2000";
@@ -22565,6 +22570,12 @@
     github = "fehrenbach";
     githubId = 203168;
     name = "Stefan Fehrenbach";
+  };
+  stefankeidel = {
+    email = "stefan@keidel.me";
+    github = "stefankeidel";
+    githubId = 1188614;
+    name = "Stefan Keidel";
   };
   stehessel = {
     email = "stephan@stehessel.de";
