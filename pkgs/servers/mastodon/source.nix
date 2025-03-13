e7655a5a3181d065f9ec59134e237d2174a436a3
Kerstin Humm <kerstin@erictapen.name>
2025-03-13 14:59:30 +0100

    mastodon: 4.3.5 -> 4.3.6
    Changelog: https://github.com/mastodon/mastodon/releases/tag/v4.3.6

--- pkgs/servers/mastodon/source.nix
+++ pkgs/servers/mastodon/source.nix
@@ -5,14 +5,14 @@
   patches ? [ ],
 }:
 let
-  version = "4.3.5";
+  version = "4.3.6";
 in
 (applyPatches {
   src = fetchFromGitHub {
     owner = "mastodon";
     repo = "mastodon";
     rev = "v${version}";
-    hash = "sha256-sGjb9D2BcCd8a7f8XPUI/FN6Za7uTv5i123/zkS24i0=";
+    hash = "sha256-cTJmSrQ+xlDfrAyHKeiIvrUOgAh9DQT2NNizN8XT3P0=";
   };
   patches = patches ++ [ ];
 })
