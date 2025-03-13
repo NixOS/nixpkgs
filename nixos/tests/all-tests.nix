ac09a7398e2a2f79ec34a2acdf4583def0b4ff36
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-03-13 23:31:20 +0100

    nixosTests.apparmor: migrate to runTest
    Part Of #386873

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -173,7 +173,7 @@ in {
   apfs = runTest ./apfs.nix;
   appliance-repart-image = runTest ./appliance-repart-image.nix;
   appliance-repart-image-verity-store = runTest ./appliance-repart-image-verity-store.nix;
-  apparmor = handleTest ./apparmor {};
+  apparmor = runTest ./apparmor;
   archi = handleTest ./archi.nix {};
   aria2 = handleTest ./aria2.nix {};
   armagetronad = handleTest ./armagetronad.nix {};
