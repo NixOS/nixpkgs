NixOS/nixpkgs

commit 40cbb21ce6eacbf2f253852b7e46cd8ff0282640

python3Packages.pylsl: 1.16.2 -> 1.17.6

pkgs/development/python-modules/pylsl/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pylsl";
-  version = "1.16.2";
+  version = "1.17.6";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "labstreaminglayer";
     repo = "pylsl";
-    rev = "v${version}";
-    hash = "sha256-rReoPirf1rdQppKEBfHMk3J2htdsnFfIdlNQIprOoUg=";
+    tag = "v${version}";
+    hash = "sha256-PEeTG+bQNEce9j0obDoaTYXMGp0MRUibbWVXM1IvGGY=";
   };
 
   postPatch = ''

commit 6848f63f6914187b14523bac3dda2038c01fa921

python3.pkgs.pysls: Fix build
Broken in 40cbb21ce6eacbf2f253852b7e46cd8ff0282640 / #375144

pkgs/development/python-modules/pylsl/default.nix
@@ -3,7 +3,9 @@
   liblsl,
   fetchFromGitHub,
   buildPythonPackage,
+  numpy,
   setuptools,
+  setuptools-scm,
   wheel,
 }:
 
@@ -20,16 +22,20 @@ buildPythonPackage rec {
   };
 
   postPatch = ''
-    substituteInPlace pylsl/pylsl.py \
+    substituteInPlace src/pylsl/lib/__init__.py \
       --replace "def find_liblsl_libraries(verbose=False):" "$(echo -e "def find_liblsl_libraries(verbose=False):\n    yield '${liblsl}/lib/liblsl.so'")"
   '';
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
+    setuptools-scm
     wheel
   ];
 
-  buildImputs = [ liblsl ];
+  dependencies = [
+    liblsl
+    numpy
+  ];
 
   pythonImportsCheck = [ "pylsl" ];
 