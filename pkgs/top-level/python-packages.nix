68c06a055ac83cace7e733dc4815d08ddabd9696
Gaetan Lepage <gaetan@glepage.com>
2025-03-14 22:08:23 +0100

    python312Packages.rtslib: rename to rtslib-fb

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14696,7 +14696,7 @@ self: super: with self; {
     inherit (pkgs) libspatialindex;
   };
 
-  rtslib = callPackage ../development/python-modules/rtslib { };
+  rtslib-fb = callPackage ../development/python-modules/rtslib-fb { };
 
   rtsp-to-webrtc = callPackage ../development/python-modules/rtsp-to-webrtc { };
 
