bca1e72100d2ea1e726c3c9cd8623aff8219790e
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-14 14:25:01 +0000

    pyfa: 2.61.3 -> 2.62.1

--- pkgs/by-name/py/pyfa/package.nix
+++ pkgs/by-name/py/pyfa/package.nix
@@ -10,7 +10,7 @@
   copyDesktopItems,
 }:
 let
-  version = "2.61.3";
+  version = "2.62.1";
 in
 python3Packages.buildPythonApplication rec {
   inherit version;
@@ -21,7 +21,7 @@ python3Packages.buildPythonApplication rec {
     owner = "pyfa-org";
     repo = "Pyfa";
     tag = "v${version}";
-    hash = "sha256-i8NcRTn817gqwQP6j0RPUJkq09eTI4nfe3EVqYnWRpo=";
+    hash = "sha256-yNMqP4YsfwTrf92wizstBXiTbhxAwIFoml4CUecqjbo=";
   };
 
   desktopItems = [
