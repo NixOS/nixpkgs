e45b54c53340d5da2c7a87894fcb3e405d43aba0
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-14 00:55:41 +0000

    xk6: 0.14.3 -> 0.14.4-4c1

--- pkgs/by-name/xk/xk6/package.nix
+++ pkgs/by-name/xk/xk6/package.nix
@@ -9,13 +9,13 @@
 
 buildGoModule rec {
   pname = "xk6";
-  version = "0.14.3";
+  version = "0.14.4-4c1";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "xk6";
     tag = "v${version}";
-    hash = "sha256-pxiZelbAEEtibNdpEuRplPFtJ61YicoiAnAs8oaX6IA=";
+    hash = "sha256-r8DbE6H4VFFzRq2uKj+N2mgN/V0w5PVWf3FvmB0YwbM=";
   };
 
   vendorHash = null;
