b757ce39d6b1dfede9a78909c541dd7ee795053f
Morgan Jones <me@numin.it>
2025-03-14 01:19:49 -0700

    androidenv: support minPlatformVersion and maxPlatformVersion
    With the release of Android API 36, it was getting rather annoying to
    need to keep these updated. Allow just specifying the min and max
    instead.

--- pkgs/development/mobile/androidenv/default.nix
+++ pkgs/development/mobile/androidenv/default.nix
@@ -18,16 +18,7 @@ lib.recurseIntoAttrs rec {
   };
 
   androidPkgs = composeAndroidPackages {
-    platformVersions = [
-      "28"
-      "29"
-      "30"
-      "31"
-      "32"
-      "33"
-      "34"
-      "35"
-    ];
+    minPlatformVersion = 28;
     includeEmulator = "if-supported";
     includeSystemImages = "if-supported";
     includeNDK = "if-supported";
