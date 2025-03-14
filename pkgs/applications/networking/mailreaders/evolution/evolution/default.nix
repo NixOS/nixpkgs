ceec89de1898e02adf83dd51d120ecee73012a04
Jan Tojnar <jtojnar@gmail.com>
2025-03-14 19:28:10 +0000

    evolution: 3.55.3 → 3.56.0
    https://gitlab.gnome.org/GNOME/evolution/-/compare/3.55.3...3.56.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
+++ pkgs/applications/networking/mailreaders/evolution/evolution/default.nix
@@ -46,11 +46,11 @@
 
 stdenv.mkDerivation rec {
   pname = "evolution";
-  version = "3.55.3";
+  version = "3.56.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/evolution/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    hash = "sha256-d42YZQ0tZfDZ8QDdpS5cr5p07H2RqwBRAeEjWWrj9iY=";
+    hash = "sha256-UUCc0w0mBGxW4jL6GV62FXGg2Q4t/i3qXFshKJaMT1g=";
   };
 
   nativeBuildInputs = [
