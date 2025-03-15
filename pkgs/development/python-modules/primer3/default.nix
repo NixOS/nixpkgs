9d7a78c17d0a3ed6fc305b09bd0229b709de6f91
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-15 08:15:19 +0100

    python312Packages.primer3: refactor

--- pkgs/development/python-modules/primer3/default.nix
+++ pkgs/development/python-modules/primer3/default.nix
@@ -25,10 +25,9 @@ buildPythonPackage rec {
     hash = "sha256-Kp4JH57gEdj7SzY+7XGBzGloWuTSwUQRBK9QbgXQfUE=";
   };
 
-  nativeBuildInputs = [
-    cython
-    setuptools
-  ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ gcc ];
+  build-system = [ setuptools ];
+
+  nativeBuildInputs = [ cython ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ gcc ];
 
   nativeCheckInputs = [
     click
@@ -49,7 +48,7 @@ buildPythonPackage rec {
     description = "Oligo analysis and primer design";
     homepage = "https://github.com/libnano/primer3-py";
     changelog = "https://github.com/libnano/primer3-py/blob/${src.tag}/CHANGES";
-    license = with licenses; [ gpl2Only ];
+    license = licenses.gpl2Only;
     maintainers = with maintainers; [ fab ];
   };
 }
