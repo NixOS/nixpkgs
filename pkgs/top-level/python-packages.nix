bf96994e7c2f036c283cebd41519409df2208d85
Nico Felbinger <nico@felbinger.eu>
2025-03-15 14:57:24 +0100

    python312Packages.netbox-attachments: init at 7.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9369,6 +9369,8 @@ self: super: with self; {
 
   netapp-ontap = callPackage ../development/python-modules/netapp-ontap { };
 
+  netbox-attachments = callPackage ../development/python-modules/netbox-attachments { };
+
   netbox-bgp = callPackage ../development/python-modules/netbox-bgp { };
 
   netbox-dns = callPackage ../development/python-modules/netbox-dns { };
