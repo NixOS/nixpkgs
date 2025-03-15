83698fa96c806facc5f58a30235f4b0ef3a6c973
Serhii Khoma <srghma@gmail.com>
2025-03-15 18:40:10 +0700

    hubstaff: 1.6.29-7f771670 -> 1.6.31-a6da06ad

--- pkgs/by-name/hu/hubstaff/package.nix
+++ pkgs/by-name/hu/hubstaff/package.nix
@@ -1,17 +1,17 @@
 { lib, stdenv, fetchurl, unzip, makeWrapper, libX11, zlib, libSM, libICE
-, libXext , freetype, libXrender, fontconfig, libXft, libXinerama
+, libXext , freetype, libXrender, fontconfig, libXft, libXinerama, libXcursor, cairo
 , libXfixes, libXScrnSaver, libnotify, glib , gtk3, libappindicator-gtk3
 , curl, writeShellScript, common-updater-scripts, xmlstarlet }:
 
 let
-  url = "https://app.hubstaff.com/download/8792-standard-linux-1-6-29-release/sh";
-  version = "1.6.29-7f771670";
-  sha256 = "sha256:09vdpsmaj26bmnbsyxp76g3677lzi8p86gz66qbdvxly6a4x1hq9";
+  url = "https://app.hubstaff.com/download/9353-standard-linux-1-6-31-release/sh";
+  version = "1.6.31-a6da06ad";
+  sha256 = "sha256:07x8di08ln15mv3x7vp01wgs8zcd1rrzvmzygrl9anhx9zhgsr3b";
 
   rpath = lib.makeLibraryPath
     [ libX11 zlib libSM libICE libXext freetype libXrender fontconfig libXft
       libXinerama stdenv.cc.cc libnotify glib gtk3 libappindicator-gtk3
-      curl libXfixes libXScrnSaver ];
+      curl libXfixes libXScrnSaver libXcursor cairo ];
 
 in
 
