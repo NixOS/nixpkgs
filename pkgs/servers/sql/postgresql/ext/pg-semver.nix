1e1d0294fe319e3a4555c941515307609d13450a
Wolfgang Walther <walther@technowledgy.de>
2025-03-16 20:25:49 +0100

    postgresqlPackages.pg-semver: modernize

--- pkgs/servers/sql/postgresql/ext/pg-semver.nix
+++ pkgs/servers/sql/postgresql/ext/pg-semver.nix
@@ -1,10 +1,10 @@
 {
-  lib,
+  buildPostgresqlExtension,
   fetchFromGitHub,
+  lib,
   postgresql,
   postgresqlTestExtension,
   testers,
-  buildPostgresqlExtension,
 }:
 
 buildPostgresqlExtension (finalAttrs: {
@@ -14,7 +14,7 @@ buildPostgresqlExtension (finalAttrs: {
   src = fetchFromGitHub {
     owner = "theory";
     repo = "pg-semver";
-    rev = "refs/tags/v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-9f+QuGupjTUK3cQk7DFDrL7MOIwDE9SAUyVZ9RfrdDM=";
   };
 
