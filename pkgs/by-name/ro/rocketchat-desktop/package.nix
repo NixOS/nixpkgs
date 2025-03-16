84d25010448ea2bfff17566e6bac1e9d2151d945
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-16 23:06:16 +0000

    rocketchat-desktop: 4.1.2 -> 4.2.0

--- pkgs/by-name/ro/rocketchat-desktop/package.nix
+++ pkgs/by-name/ro/rocketchat-desktop/package.nix
@@ -10,11 +10,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "rocketchat-desktop";
-  version = "4.1.2";
+  version = "4.2.0";
 
   src = fetchurl {
     url = "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/${version}/rocketchat-${version}-linux-amd64.deb";
-    hash = "sha256-lcMxBEcZkJARFd+ZQTAdLQLVy4/SIi4ps8N798PomyM=";
+    hash = "sha256-6eoZbjmwzvXz7u+zkyrvzebens8GF9jGQA40cKxX26M=";
   };
 
   nativeBuildInputs = [
