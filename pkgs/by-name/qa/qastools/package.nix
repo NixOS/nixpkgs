fb385d93845243654fc5133a66f66de372ecd1fd
Alexander V. Nikolaev <avn@avnik.info>
2025-03-16 16:06:19 +0200

    qastools: 0.23.0 -> 1.4.0
    * Update to actual 1.4.0 version
    * Modernize
    * Switch to Qt6
    * Move to pkgs/by-name
    * Add myself as maintainer
    
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- pkgs/by-name/qa/qastools/package.nix
+++ pkgs/by-name/qa/qastools/package.nix
@@ -1,40 +1,47 @@
 {
-  mkDerivation,
+  stdenv,
   lib,
   fetchFromGitLab,
   cmake,
   alsa-lib,
   udev,
-  qtbase,
-  qtsvg,
-  qttools,
+  qt6Packages,
 }:
 
-mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "qastools";
-  version = "0.23.0";
+  version = "1.4.0";
 
   src = fetchFromGitLab {
     owner = "sebholt";
     repo = pname;
     rev = "v${version}";
-    sha256 = "19hn64hnvryfmrkdg6rqyzahdbqf1s5y3dcca389jj6sdx93b3ip";
+    hash = "sha256-mJjhM1y50f44kvxMidM7uqlkkXx1sbJC21vIMEDenoU=";
   };
 
-  nativeBuildInputs = [ cmake ];
-  buildInputs = [
-    alsa-lib
-    udev
-    qtbase
-    qtsvg
-    qttools
+  nativeBuildInputs = [
+    cmake
+    qt6Packages.wrapQtAppsHook
   ];
+  buildInputs =
+    [
+      alsa-lib
+      udev
+    ]
+    ++ (with qt6Packages; [
+      qtbase
+      qtsvg
+      qttools
+    ]);
 
   meta = with lib; {
     description = "Collection of desktop applications for ALSA configuration";
     homepage = "https://gitlab.com/sebholt/qastools";
     license = licenses.mit;
-    maintainers = with maintainers; [ orivej ];
+    maintainers = with maintainers; [
+      avnik
+      orivej
+    ];
     platforms = platforms.linux;
   };
 }
