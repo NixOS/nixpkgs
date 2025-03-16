93a0cef96d87dc4a2ad31ecc94e45cde7e1ea58d
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-16 15:49:47 +0000

    reindeer: 2025.02.24.00 -> 2025.03.10.00

--- pkgs/by-name/re/reindeer/package.nix
+++ pkgs/by-name/re/reindeer/package.nix
@@ -9,17 +9,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "reindeer";
-  version = "2025.02.24.00";
+  version = "2025.03.10.00";
 
   src = fetchFromGitHub {
     owner = "facebookincubator";
     repo = "reindeer";
     tag = "v${version}";
-    hash = "sha256-+uiVUEaBDO7c2QYo0NcCy9Ms+wz+09p6kD0muRAvOlo=";
+    hash = "sha256-yE1ARmsEbuG9OskipZfPKbA0c/4VBVBFHFEiy5nzwXg=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-G+NAljFX0R73+sj30KHHkU78AfQCg7e3PM5oOB9iTbE=";
+  cargoHash = "sha256-VU0mLaQ8pWQw/pFTB35/ZOKUnqEVEO9f1IKZ0gAmqj8=";
 
   nativeBuildInputs = [ pkg-config ];
 
