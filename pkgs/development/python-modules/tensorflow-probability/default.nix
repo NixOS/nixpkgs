ee5903fd73ca44c7e66104e2e24cb41325fd5d70
Samuel Ainsworth <skainsworth@gmail.com>
2025-03-16 16:01:12 -0400

    python3Packages.tensorflow-probability: update fetchAttrs hash

--- pkgs/development/python-modules/tensorflow-probability/default.nix
+++ pkgs/development/python-modules/tensorflow-probability/default.nix
@@ -66,7 +66,7 @@ let
     LIBTOOL = lib.optionalString stdenv.hostPlatform.isDarwin "${cctools}/bin/libtool";
 
     fetchAttrs = {
-      sha256 = "sha256-TbWcWYidyXuAMgBnO2/k0NKCzc4wThf2uUeC3QxdBJY=";
+      hash = "sha256-1VVrKARZsWg/UT9K8rcGjl5A91Sif+DgoeAu+oeaFV8=";
     };
 
     buildAttrs = {
