86ecdbde74093981ef79028a8ee06e0e69a62f4a
cr0n <cr0n@cypherpunks.cc>
2025-03-16 17:15:15 +0100

    necesse-server: init at 0.31.1-17664948
    Dedicated server for Necesse. Release notes for this version:
    https://steamcommunity.com/games/1169040/announcements/detail/534344040319026817

---
+++ pkgs/by-name/ne/necesse-server/package.nix
@@ -0,0 +1,44 @@
+{
+  fetchzip,
+  jre,
+  lib,
+  stdenvNoCC,
+}:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "necesse-server";
+  version = "0.31.1-17664948";
+  urlVersion = lib.replaceStrings [ "." ] [ "-" ] version;
+
+  src = fetchzip {
+    url = "https://necessegame.com/content/server/${urlVersion}/necesse-server-linux64-${urlVersion}.zip";
+    sha256 = "sha256-H7/fc3zkuEMuv9Uq00TLSLF4rT8+UWsofnuCFrmUtjU=";
+  };
+
+  inherit jre;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp -r ./* $out/
+    params='-nogui "$@"'
+    cat >$out/bin/necesse-server <<EOF
+    #! $SHELL -e
+    exec $jre/bin/java -jar $out/Server.jar $params
+    EOF
+    chmod +x $out/bin/necesse-server
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://necessegame.com/server/";
+    description = "Dedicated server for Necesse";
+    sourceProvenance = with sourceTypes; [ binaryBytecode ];
+    platforms = platforms.linux;
+    license = licenses.unfreeRedistributable;
+    mainProgram = "necesse-server";
+    maintainers = with maintainers; [ cr0n ];
+  };
+}
