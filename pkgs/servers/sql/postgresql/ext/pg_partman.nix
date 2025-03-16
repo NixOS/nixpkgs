c8acb21179f09bca51cdcee68fe6f7db676cb468
Wolfgang Walther <walther@technowledgy.de>
2025-03-16 20:33:45 +0100

    postgresqlPackages.pg_partman: modernize

--- pkgs/servers/sql/postgresql/ext/pg_partman.nix
+++ pkgs/servers/sql/postgresql/ext/pg_partman.nix
@@ -1,9 +1,9 @@
 {
-  lib,
-  stdenv,
-  fetchFromGitHub,
-  postgresql,
   buildPostgresqlExtension,
+  fetchFromGitHub,
+  lib,
+  postgresql,
+  stdenv,
 }:
 
 buildPostgresqlExtension rec {
@@ -12,18 +12,18 @@ buildPostgresqlExtension rec {
 
   src = fetchFromGitHub {
     owner = "pgpartman";
-    repo = pname;
-    rev = "refs/tags/v${version}";
-    sha256 = "sha256-i/o+JZEXnJRO17kfdTw87aca28+I8pvuFZsPMA/kf+w=";
+    repo = "pg_partman";
+    tag = "v${version}";
+    hash = "sha256-i/o+JZEXnJRO17kfdTw87aca28+I8pvuFZsPMA/kf+w=";
   };
 
-  meta = with lib; {
+  meta = {
     description = "Partition management extension for PostgreSQL";
     homepage = "https://github.com/pgpartman/pg_partman";
     changelog = "https://github.com/pgpartman/pg_partman/blob/v${version}/CHANGELOG.md";
-    maintainers = with maintainers; [ ggpeti ];
+    maintainers = with lib.maintainers; [ ggpeti ];
     platforms = postgresql.meta.platforms;
-    license = licenses.postgresql;
-    broken = versionOlder postgresql.version "14";
+    license = lib.licenses.postgresql;
+    broken = lib.versionOlder postgresql.version "14";
   };
 }
