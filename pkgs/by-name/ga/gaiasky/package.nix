89789e54714740e93109b0be84547ad35a2a9d6f
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2025-03-16 13:47:37 +0530

    gaiasky: init at 3.6.6
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

---
+++ pkgs/by-name/ga/gaiasky/package.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  appimageTools,
+  fetchurl,
+}:
+
+let
+  pname = "gaiasky";
+  version = "3.6.6";
+  commit = "caf73d2de";
+
+  src = fetchurl {
+    url = "https://codeberg.org/gaiasky/gaiasky/releases/download/${version}/gaiasky_${version}.${commit}_x86_64.appimage";
+    hash = "sha256-eq3EEONLM5Q4J/esMy4U6M9QdY08paWHHasEEO0d8xo=";
+  };
+
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/gaiasky.desktop -t $out/share/applications
+    cp -r ${appimageContents}/*.png $out/share
+  '';
+
+  meta = with lib; {
+    description = "Open source 3D universe visualization software for desktop and VR with support for more than a billion objects.";
+    homepage = "https://gaiasky.space";
+    license = licenses.mpl20;
+    maintainers = with maintainers; [ reputable2772 ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
