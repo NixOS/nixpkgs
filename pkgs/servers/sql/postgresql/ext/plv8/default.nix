cbd120c28b6dcc17a31d955d3cb3de30372401c0
Wolfgang Walther <walther@technowledgy.de>
2025-03-16 20:06:21 +0100

    postgresqlPackages.plv8: modernize

--- pkgs/servers/sql/postgresql/ext/plv8/default.nix
+++ pkgs/servers/sql/postgresql/ext/plv8/default.nix
@@ -1,16 +1,16 @@
 {
-  stdenv,
-  lib,
+  buildPostgresqlExtension,
   fetchFromGitHub,
+  jitSupport,
+  lib,
   nodejs_20,
   perl,
   postgresql,
-  jitSupport,
-  buildPostgresqlExtension,
+  stdenv,
   # For test
-  runCommand,
   coreutils,
   gnugrep,
+  runCommand,
 }:
 
 let
@@ -23,7 +23,7 @@ buildPostgresqlExtension (finalAttrs: {
   src = fetchFromGitHub {
     owner = "plv8";
     repo = "plv8";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-ivQZJSNn5giWF351fqZ7mBZoJkGtby5T7beK45g3Zqs=";
   };
 
@@ -130,7 +130,7 @@ buildPostgresqlExtension (finalAttrs: {
       };
   };
 
-  meta = with lib; {
+  meta = {
     description = "V8 Engine Javascript Procedural Language add-on for PostgreSQL";
     homepage = "https://plv8.github.io/";
     changelog = "https://github.com/plv8/plv8/blob/r${finalAttrs.version}/Changes";
@@ -139,7 +139,7 @@ buildPostgresqlExtension (finalAttrs: {
       "x86_64-linux"
       "aarch64-linux"
     ];
-    license = licenses.postgresql;
+    license = lib.licenses.postgresql;
     broken = jitSupport;
   };
 })
