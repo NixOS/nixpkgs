e84cc9e86c0dd6f9fba9337887854611e44db38e
Jordan Justen <jljusten@gmail.com>
2025-03-16 03:00:10 -0700

    vulkan-cts: Drop deqp-vksc from vulkan-cts package
    The Vulkan SC (Safety Critical) CTS has different release tags and
    build dependencies from the Vulkan CTS.
    
    When updating the vulkan-cts package version, I found that the newer
    version would fail to build if we attempted to build the deqp-vksc
    executable.
    
    I suspect that building the Vulkan SC CTS executable, deqp-vksc, may
    not be supported unless using a vulkansc-cts-* tag.
    
    This change adds cmake build define SELECTED_BUILD_TARGETS=deqp-vk,
    which was introduced in vulkan-cts-1.3.7.0, and reduces the amount of
    code built. Notably, it causes the build to no longer attempt to build
    deqp-vksc.
    
    Ref: vulkan-cts b22a8f06d ("Allow selecting a subset of targets at configuration time")
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- pkgs/by-name/vu/vulkan-cts/package.nix
+++ pkgs/by-name/vu/vulkan-cts/package.nix
@@ -107,6 +107,7 @@ stdenv.mkDerivation (finalAttrs: {
     # For vulkan-validation-layers
     "-DGLSLANG_INSTALL_DIR=${glslang}"
     "-DSPIRV_HEADERS_INSTALL_DIR=${spirv-headers}"
+    "-DSELECTED_BUILD_TARGETS=deqp-vk"
   ];
 
   postInstall = ''
@@ -114,7 +115,7 @@ stdenv.mkDerivation (finalAttrs: {
     ! test -e $out
 
     mkdir -p $out/bin $out/archive-dir
-    cp -a external/vulkancts/modules/vulkan/deqp-vk external/vulkancts/modules/vulkan/deqp-vksc $out/bin/
+    cp -a external/vulkancts/modules/vulkan/deqp-vk $out/bin/
     cp -a external/vulkancts/modules/vulkan/vulkan $out/archive-dir/
     cp -a external/vulkancts/modules/vulkan/vk-default $out/
 
