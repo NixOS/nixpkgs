d5e410e393053dae577ba70728577dc161419ff0
trollmii <trollmii@outlook.de>
2025-03-17 13:23:30 +0100

    isobiscuit: version 0.3.13

--- pkgs/by-name/is/isobiscuit/package.nix
+++ pkgs/by-name/is/isobiscuit/package.nix
@@ -15,15 +15,22 @@ buildPythonPackage rec {
     sha256 = "03ivhmh9ikc51mi2547m0my2fvgicgnnc40lqjgc8g276imfhq13";
   };
 
+  format = "pyproject";
+
+  nativeBuildInputs = with python3Packages; [
+    poetry-core
+    hatchling
+  ];
+
   propagatedBuildInputs = with python3Packages; [
     requests
     pyyaml
     colorama
-    poetry-core
   ];
 
   meta = with lib; {
     description = "IsoBiscuit is a tool for virtualization, where processes are running in biscuits.";
+    homepage = "https://github.com/isobiscuit/isobiscuit";
     license = licenses.asl20;
     maintainers = with maintainers; [ trollmii ];
   };
