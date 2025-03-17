d1b6af4b4514c6ab2a3efd4bd536a4eb368b76b3
silvanshade <silvanshade@users.noreply.github.com>
2025-03-17 13:40:12 -0600

    libblake3: 1.5.5 -> 1.7.0
    (cherry picked from commit b49ff29cff6f8951ec14f36edb3fe85d3fbe5ad7)
    
    Modified to apply from commit 5060af0dbe680b1df4a84ed6a1656a4fade8d232

--- pkgs/by-name/li/libblake3/package.nix
+++ pkgs/by-name/li/libblake3/package.nix
@@ -3,23 +3,30 @@
   stdenv,
   cmake,
   fetchFromGitHub,
+  tbb_2022_0,
+  useTBB ? true,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libblake3";
-  version = "1.5.5";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "BLAKE3-team";
     repo = "BLAKE3";
     tag = finalAttrs.version;
-    hash = "sha256-2M8OQNmtWwfDcbZYspaxpGz2clpfILru//4+P6dClNw=";
+    hash = "sha256-08D5hnU3I0VJ+RM/TNk2LxsEAvOLuO52+08zlKssXbc=";
   };
 
   sourceRoot = finalAttrs.src.name + "/c";
 
-  nativeBuildInputs = [
-    cmake
+  nativeBuildInputs = [ cmake ];
+
+  buildInputs = lib.optionals useTBB [ tbb_2022_0 ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "BLAKE3_USE_TBB" useTBB)
+    (lib.cmakeBool "BUILD_SHARED_LIBS" (!stdenv.hostPlatform.isStatic))
   ];
 
   meta = {
