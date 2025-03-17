aa4bd7c8904fcf2612af809aa855c472cea0f304
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-03-18 00:08:28 +0100

    python312Packages.ncclient: modernize

--- pkgs/development/python-modules/ncclient/default.nix
+++ pkgs/development/python-modules/ncclient/default.nix
@@ -4,29 +4,32 @@
   fetchFromGitHub,
   lxml,
   paramiko,
+  setuptools,
   pytestCheckHook,
   pythonOlder,
-  six,
 }:
 
 buildPythonPackage rec {
   pname = "ncclient";
   version = "0.6.19";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
-    owner = pname;
-    repo = pname;
+    owner = "ncclient";
+    repo = "ncclient";
     tag = "v${version}";
     hash = "sha256-l2c+8fMXSx6rMUu+lbyABs1G3llZFD4rI4V1Y729OMs=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
     paramiko
     lxml
-    six
   ];
 
   nativeCheckInputs = [ pytestCheckHook ];
