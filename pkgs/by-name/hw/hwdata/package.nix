5d4521c3c3988511754d9ec3d0bab0ca997baf09
cy <cy@cy7.sh>
2025-03-17 10:13:46 -0400

    hwdata: remove configureFlags

--- pkgs/by-name/hw/hwdata/package.nix
+++ pkgs/by-name/hw/hwdata/package.nix
@@ -11,11 +11,9 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "vcrhonek";
     repo = "hwdata";
-    rev = "v${version}";
+    tag = "v${version}";
     hash = "sha256-RDp5NY9VYD0gylvzYpg9BytfRdQ6dim1jJtv32yeF3k=";
   };
-
-  configureFlags = [ "--datadir=${placeholder "out"}/share" ];
 
   doCheck = false; # this does build machine-specific checks (e.g. enumerates PCI bus)
 
