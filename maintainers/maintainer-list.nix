3b37adcbae85ee06d89ae28a99fe816ed3b97afe
Suwon Park <suwonpark@haedosa.xyz>
2025-03-18 15:01:31 +0900

    maintainers: add sepiabrown

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21615,6 +21615,12 @@
     githubId = 529649;
     name = "Raffael Mancini";
   };
+  sepiabrown = {
+    email = "bboxone@gmail.com";
+    github = "sepiabrown";
+    githubId = 35622998;
+    name = "Suwon Park";
+  };
   seppeljordan = {
     email = "sebastian.jordan.mail@googlemail.com";
     github = "seppeljordan";
