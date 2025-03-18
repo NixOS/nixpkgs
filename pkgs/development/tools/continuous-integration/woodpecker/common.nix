80c5a0a779d608042900a0ff03b657666e8f943c
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-18 15:32:35 +0000

    woodpecker-server: 3.3.0 -> 3.4.0

--- pkgs/development/tools/continuous-integration/woodpecker/common.nix
+++ pkgs/development/tools/continuous-integration/woodpecker/common.nix
@@ -1,7 +1,7 @@
 { lib, fetchzip }:
 let
-  version = "3.3.0";
-  srcHash = "sha256-f0XQ3cbao8IM5toSkxvul7pp1L89N9oiEo3Yx3mkGhg=";
+  version = "3.4.0";
+  srcHash = "sha256-2zKzNaNe057iJkhhLaEBfE84YkR0X5lGpB4b3WNq3ec=";
   # The tarball contains vendored dependencies
   vendorHash = null;
 in
