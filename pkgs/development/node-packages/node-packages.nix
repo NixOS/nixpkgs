436703eb597d859ebcddd80250b24289d233d2ee
Robert Schütz <nix@dotlambda.de>
2025-03-18 13:17:48 -0700

    nodePackages.ijavascript: drop
    It fails to build with
    
        npm error /nix/store/bhc0grw1nwzwgh55qapfqwb3vil5s49y-node-sources/deps/v8/include/v8-local-handle.h: In instantiation of 'v8::Local<T>::Local(v8::Local<S>) [with S = v8::Data; T = v8::Value]':
        npm error ../../nan/nan_callbacks_12_inl.h:175:53:   required from here
        npm error   175 |       cbinfo(info, obj->GetInternalField(kDataIndex));
        npm error       |                                                     ^
        npm error /nix/store/bhc0grw1nwzwgh55qapfqwb3vil5s49y-node-sources/deps/v8/include/v8-local-handle.h:269:42: error: static assertion failed: type check
        npm error   269 |     static_assert(std::is_base_of<T, S>::value, "type check");
        npm error       |                                          ^~~~~
        npm error /nix/store/bhc0grw1nwzwgh55qapfqwb3vil5s49y-node-sources/deps/v8/include/v8-local-handle.h:269:42: note: 'std::integral_constant<bool, false>::value' evaluates to false

--- pkgs/development/node-packages/node-packages.nix
+++ pkgs/development/node-packages/node-packages.nix
@@ -54272,33 +54272,6 @@ in
     bypassCache = true;
     reconstructLock = true;
   };
-  ijavascript = nodeEnv.buildNodePackage {
-    name = "ijavascript";
-    packageName = "ijavascript";
-    version = "5.2.1";
-    src = fetchurl {
-      url = "https://registry.npmjs.org/ijavascript/-/ijavascript-5.2.1.tgz";
-      sha512 = "kH7hudp+S+8++ngjUXbiyHOhp3qa4oDVUkmf6p7+7s15PIBDv5zx878pxNRdGcWhYGy5TT683EOqeKMQw8jrFA==";
-    };
-    dependencies = [
-      sources."jmp-2.0.0"
-      sources."jp-kernel-2.0.0"
-      sources."nan-2.17.0"
-      sources."nel-1.3.0"
-      sources."node-gyp-build-4.8.4"
-      sources."uuid-3.4.0"
-      sources."zeromq-5.3.1"
-    ];
-    buildInputs = globalBuildInputs;
-    meta = {
-      description = "IJavascript is a Javascript kernel for the Jupyter notebook";
-      homepage = "https://n-riesco.github.io/ijavascript";
-      license = "BSD-3-Clause";
-    };
-    production = true;
-    bypassCache = true;
-    reconstructLock = true;
-  };
   imapnotify = nodeEnv.buildNodePackage {
     name = "imapnotify";
     packageName = "imapnotify";
