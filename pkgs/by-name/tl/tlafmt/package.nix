f758e6b39f318249f6ca1e8dacbcd6ec56cc22a1
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-18 23:27:48 +0000

    tlafmt: 0.3.0 -> 0.3.2

--- pkgs/by-name/tl/tlafmt/package.nix
+++ pkgs/by-name/tl/tlafmt/package.nix
@@ -5,18 +5,18 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "tlafmt";
-  version = "0.3.0";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
     owner = "domodwyer";
     repo = "tlafmt";
     tag = "v${version}";
-    hash = "sha256-jBY7erB2LuKwCkshVHLV5kFVRJ8lkT63z1gt1Tikei4=";
+    hash = "sha256-V7KTzjCLOdt31UO01iTHVk2zpPc+GdSpsrEfwwbjZrk=";
   };
 
   useFetchCargoVendor = true;
 
-  cargoHash = "sha256-765tp4wUh7G92vaoViufo6Kk2c/w2d1XjZ3aN5UUAv0=";
+  cargoHash = "sha256-KUN7Et3wW5oLu+FK7ySWKSqpo1IL0ggww74IpFX0aSQ=";
 
   meta = {
     description = "Formatter for TLA+ specs";
