090ab43ceb978ec9c06d16f4b7444d9cc7c785c9
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-18 10:34:33 +0000

    python312Packages.hikari-crescent: 1.1.0 -> 1.2.0

--- pkgs/development/python-modules/hikari-crescent/default.nix
+++ pkgs/development/python-modules/hikari-crescent/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "hikari-crescent";
-  version = "1.1.0";
+  version = "1.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "hikari-crescent";
     repo = "hikari-crescent";
     tag = "v${version}";
-    hash = "sha256-37QA/yBBDo7w/e7SvgXnzfxeCulyYgB+xtJY25HJ9FM=";
+    hash = "sha256-aQjT5sAaqConUtRGcqddzwcbBJkbwYOCxvnNJpKu3yI=";
   };
 
   build-system = [ poetry-core ];
