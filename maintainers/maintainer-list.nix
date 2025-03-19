38bdfec0aa7ac87e4d90c679165606ebb0bdf050
Izel Nakri <contact@izelnakri.com>
2025-03-19 19:59:32 +0100

    maintainers: add izelnakri

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10548,6 +10548,12 @@
     githubId = 94313;
     name = "Xianyi Lin";
   };
+  izelnakri = {
+    email = "contact@izelnakri.com";
+    github = "izelnakri";
+    githubId = 1190931;
+    name = "Izel Nakri";
+  };
   izorkin = {
     email = "Izorkin@gmail.com";
     github = "Izorkin";
