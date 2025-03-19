e021ee13f909e6eb7c019b5cd1acf9eac0cd0f67
Colin <colin@uninsane.org>
2025-03-19 06:59:49 +0000

    makeFontsCache: fix cross compilation
    lifting the `callPackage` to scope level ensures splicing works as expected.
    we still need to emulate fc-cache to produce usable cache entries though.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8764,10 +8764,7 @@ with pkgs;
 
   makeFontsConf = callPackage ../development/libraries/fontconfig/make-fonts-conf.nix { };
 
-  makeFontsCache = let fontconfig_ = fontconfig; in {fontconfig ? fontconfig_, fontDirectories}:
-    callPackage ../development/libraries/fontconfig/make-fonts-cache.nix {
-      inherit fontconfig fontDirectories;
-    };
+  makeFontsCache = callPackage ../development/libraries/fontconfig/make-fonts-cache.nix { };
 
   freenect = callPackage ../development/libraries/freenect {
     inherit (darwin.apple_sdk.frameworks) Cocoa GLUT;
