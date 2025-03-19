51950a5e8370f5a77d0ec7bf0bf7d3f30382dd11
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-19 19:51:26 +0000

    iosevka-bin: 33.0.1 -> 33.1.0

--- pkgs/by-name/io/iosevka-bin/package.nix
+++ pkgs/by-name/io/iosevka-bin/package.nix
@@ -17,7 +17,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "${name}-bin";
-  version = "33.0.1";
+  version = "33.1.0";
 
   src = fetchurl {
     url = "https://github.com/be5invis/Iosevka/releases/download/v${version}/PkgTTC-${name}-${version}.zip";
