eacd5b9415fcc62b089dbfe8e3b371af6be2f48a
Kerstin Humm <kerstin@erictapen.name>
2025-03-19 14:58:55 +0100

    python3Packages.pytest-scim2-server: init at 0.1.2

---
+++ pkgs/development/python-modules/pytest-scim2-server/default.nix
@@ -0,0 +1,49 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  hatchling,
+  portpicker,
+  pytest,
+  scim2-server,
+  pytestCheckHook,
+  scim2-client,
+  cacert,
+}:
+
+buildPythonPackage rec {
+  pname = "pytest-scim2-server";
+  version = "0.1.2";
+
+  pyproject = true;
+
+  # Pypi doesn't link a VCS repository
+  src = fetchPypi {
+    pname = "pytest_scim2_server";
+    inherit version;
+    hash = "sha256-uWWFqkloTkvtIfpBNy93fa+c+b/FATjwytLKRde4bHg=";
+  };
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    portpicker
+    pytest
+    scim2-server
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    scim2-client
+  ] ++ scim2-client.optional-dependencies.httpx;
+
+  env.SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+  pythonImportsCheck = [ "pytest_scim2_server" ];
+
+  meta = {
+    homepage = "https://pypi.org/project/pytest-scim2-server";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ erictapen ];
+  };
+}
