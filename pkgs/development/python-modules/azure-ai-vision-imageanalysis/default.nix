d9886be26ef7786a61d24f7b4935972c3928e891
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-20 15:32:09 +0000

    python312Packages.azure-ai-vision-imageanalysis: 1.0.0b3 -> 34.2.0

--- pkgs/development/python-modules/azure-ai-vision-imageanalysis/default.nix
+++ pkgs/development/python-modules/azure-ai-vision-imageanalysis/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "azure-ai-vision-imageanalysis";
-  version = "1.0.0b3";
+  version = "34.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Azure";
     repo = "azure-sdk-for-python";
-    rev = "refs/tags/azure-ai-vision-imageanalysis_${version}";
-    hash = "sha256-Hkj9mrjCc8Li8z6e1BjpzANRVx6+DjN0MhTLANMT78E=";
+    tag = "azure-mgmt-containerservice_${version}";
+    hash = "sha256-xZY+U1Y1Ogbk4/9BTrIbmheeouoYXpeFHEEseAsoixo=";
   };
 
   sourceRoot = "${src.name}/sdk/vision/azure-ai-vision-imageanalysis";
