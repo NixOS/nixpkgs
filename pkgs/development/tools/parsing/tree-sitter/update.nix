3669cefd0488dca13cb00360f3139467f48ef353
Adrien Faure <adrien.faure@protonmail.com>
2025-03-20 19:46:14 +0100

    tree-sitter/update: run grammar tests during update process

--- pkgs/development/tools/parsing/tree-sitter/update.nix
+++ pkgs/development/tools/parsing/tree-sitter/update.nix
@@ -8,6 +8,7 @@
   coreutils,
   curl,
   xe,
+  tree-sitter,
 }:
 
 # Grammar list:
@@ -507,6 +508,7 @@ let
           curl = "${curl}/bin/curl";
           nix-prefetch-git = "${nix-prefetch-git}/bin/nix-prefetch-git";
           printf = "${coreutils}/bin/printf";
+          tree-sitter = "${lib.getExe tree-sitter}";
         };
         inherit
           knownTreeSitterOrgGrammarRepos
