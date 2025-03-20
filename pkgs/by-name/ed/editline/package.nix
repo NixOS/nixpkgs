3cb09c397b51fe8f14cbec0e558be58aa44148d2
Rebecca Turner <rbt@sent.as>
2025-03-19 18:09:16 -0700

    editline: recognize `Alt-Left`/`Alt-Right` in more platforms
    This applies a merged but not released patch from upstream (`editline`
    has not cut a release since 2020).
    
    See: https://github.com/troglobit/editline/pull/70
    See: https://github.com/NixOS/nixpkgs/pull/341131

--- pkgs/by-name/ed/editline/package.nix
+++ pkgs/by-name/ed/editline/package.nix
@@ -31,6 +31,16 @@ stdenv.mkDerivation rec {
       url = "https://github.com/troglobit/editline/commit/f444a316f5178b8e20fe31e7b2d979e651da077e.patch";
       hash = "sha256-m3jExTkPvE+ZBwHzf/A+ugzzfbLmeWYn726l7Po7f10=";
     })
+
+    # Recognize `Alt-Left` and `Alt-Right` for navigating by words in more
+    # terminals/shells/platforms.
+    #
+    # See: https://github.com/troglobit/editline/pull/70
+    (fetchpatch {
+      name = "alt-left-alt-right-word-navigation.patch";
+      url = "https://github.com/troglobit/editline/commit/fb4d7268de024ed31ad2417f533cc0cbc2cd9b29.diff";
+      hash = "sha256-5zMsmpU5zFoffRUwFhI/vP57pEhGotcMPgn9AfI1SNg=";
+    })
   ];
 
   configureFlags = [ (lib.enableFeature true "sigstop") ];
