588bd6e8469b82a12192d28e0d5ec9efe8499541
Benno Bielmeier <nix@bbenno.com>
2025-03-21 00:43:15 +0100

    graylog-5_2: remove
    Graylog 5.2 is EOL since October 18th 2024.
    https://endoflife.date/graylog

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3473,8 +3473,6 @@ with pkgs;
 
   grails = callPackage ../development/web/grails { jdk = null; };
 
-  graylog-5_2 = callPackage ../tools/misc/graylog/5.2.nix { };
-
   graylog-6_0 = callPackage ../tools/misc/graylog/6.0.nix { };
 
   graylogPlugins = recurseIntoAttrs (
