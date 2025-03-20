94775f8a0024b28d2357c4d113b5b8b82f7fa223
Sergey Volkov <taranarmo@gmail.com>
2025-03-20 12:23:27 +0100

    python3Packages.aiohttp-swagger: fix tests

--- pkgs/development/python-modules/aiohttp-swagger/default.nix
+++ pkgs/development/python-modules/aiohttp-swagger/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   setuptools,
   aiohttp,
   jinja2,
@@ -23,6 +24,17 @@ buildPythonPackage rec {
     tag = version;
     hash = "sha256-M43sNpbXWXFRTd549cZhvhO35nBB6OH+ki36BzSk87Q=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/cr0hn/aiohttp-swagger/pull/108/commits/a4be5331c8a50d46ea64c4c69e00292300245f3b.patch";
+      hash = "sha256-lPrXzctXl6aNr+SIUpqAFhq49hMeoqTFfckvh5awgPQ=";
+    })
+    (fetchpatch {
+      url = "https://github.com/cr0hn/aiohttp-swagger/pull/108/commits/968f40b23e5bf5cc3e19053c764806574026d3e7.patch";
+      hash = "sha256-tkZ2PbRuciFzV6bx/1HYn9ZWbP/4bEmBHL0ObPz4fMQ=";
+    })
+  ];
 
   build-system = [
     setuptools
