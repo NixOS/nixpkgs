24e8c6acb5cb3e768a33523d5137117414a5d785
Andrew Field <27866671+andrew-field@users.noreply.github.com>
2025-03-20 16:56:42 +0200

    feat: add alterware package nix

---
+++ pkgs/by-name/al/alterware/package.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  perl,
+}:
+
+with import <nixpkgs>
+{
+  overlays = [
+    (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+  ];
+};
+let
+  rustPlatform = makeRustPlatform {
+    cargo = rust-bin.stable.latest.minimal;
+    rustc = rust-bin.stable.latest.minimal;
+  };
+in
+# The above is needed until the rustc version is at least 1.78 in rustPlatform.buildRustPackage.
+
+rustPlatform.buildRustPackage rec {
+  pname = "alterware";
+  version = "0.10.2";
+
+  src = fetchFromGitHub {
+    owner = "mxve";
+    repo = "alterware-launcher";
+    rev = "v${version}";
+    sha256 = "1sqh40jhdcbd5nr8lzjqkj4zdg9r2l5cfda3cv6dhwx46qm180gd";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-E3TDgEogpd/YGZnQmre5igxZyZdKQtOx4O4IZEci5II=";
+
+  nativeBuildInputs = [
+    perl
+  ];
+
+  meta = {
+    description = "Official launcher for AlterWare Call of Duty mods";
+    homepage = "https://github.com/mxve/alterware-launcher";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ andrew-field ];
+    mainProgram = "alterware";
+  };
+}
