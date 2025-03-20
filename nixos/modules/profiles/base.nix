5e2d52c43069bc9130a1aefdcfad8bf5c7c75166
MakiseKurisu <MakiseKurisu@users.noreply.github.com>
2025-03-20 13:29:28 +0800

    nixos/profiles/base: nixfmt

--- nixos/modules/profiles/base.nix
+++ nixos/modules/profiles/base.nix
@@ -11,44 +11,46 @@
 {
   # Include some utilities that are useful for installing or repairing
   # the system.
-  environment.systemPackages = [
-    pkgs.w3m-nographics # needed for the manual anyway
-    pkgs.testdisk # useful for repairing boot problems
-    pkgs.ms-sys # for writing Microsoft boot sectors / MBRs
-    pkgs.efibootmgr
-    pkgs.efivar
-    pkgs.parted
-    pkgs.gptfdisk
-    pkgs.ddrescue
-    pkgs.ccrypt
-    pkgs.cryptsetup # needed for dm-crypt volumes
+  environment.systemPackages =
+    [
+      pkgs.w3m-nographics # needed for the manual anyway
+      pkgs.testdisk # useful for repairing boot problems
+      pkgs.ms-sys # for writing Microsoft boot sectors / MBRs
+      pkgs.efibootmgr
+      pkgs.efivar
+      pkgs.parted
+      pkgs.gptfdisk
+      pkgs.ddrescue
+      pkgs.ccrypt
+      pkgs.cryptsetup # needed for dm-crypt volumes
 
-    # Some text editors.
-    pkgs.vim
+      # Some text editors.
+      pkgs.vim
 
-    # Some networking tools.
-    pkgs.fuse
-    pkgs.fuse3
-    pkgs.sshfs-fuse
-    pkgs.socat
-    pkgs.screen
-    pkgs.tcpdump
+      # Some networking tools.
+      pkgs.fuse
+      pkgs.fuse3
+      pkgs.sshfs-fuse
+      pkgs.socat
+      pkgs.screen
+      pkgs.tcpdump
 
-    # Hardware-related tools.
-    pkgs.sdparm
-    pkgs.hdparm
-    pkgs.smartmontools # for diagnosing hard disks
-    pkgs.pciutils
-    pkgs.usbutils
-    pkgs.nvme-cli
+      # Hardware-related tools.
+      pkgs.sdparm
+      pkgs.hdparm
+      pkgs.smartmontools # for diagnosing hard disks
+      pkgs.pciutils
+      pkgs.usbutils
+      pkgs.nvme-cli
 
-    # Some compression/archiver tools.
-    pkgs.unzip
-    pkgs.zip
-  ] ++
-  # Upstream bug?
-  # https://github.com/koverstreet/bcachefs-tools/issues/276
-  lib.optional (config.boot.supportedFilesystems.bcachefs or false) pkgs.keyutils;
+      # Some compression/archiver tools.
+      pkgs.unzip
+      pkgs.zip
+    ]
+    ++
+    # Upstream bug?
+    # https://github.com/koverstreet/bcachefs-tools/issues/276
+    lib.optional (config.boot.supportedFilesystems.bcachefs or false) pkgs.keyutils;
 
   # Include support for various filesystems and tools to create / manipulate them.
   boot.supportedFilesystems = lib.mkMerge [
