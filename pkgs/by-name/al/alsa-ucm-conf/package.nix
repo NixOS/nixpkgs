eb07ec2d8ed387f0f52ca358c81d3edb3805573d
Mikael Voss <mvs@nyantec.com>
2025-03-22 00:58:01 +0100

    alsa-lib: validate UCM configuration

--- pkgs/by-name/al/alsa-ucm-conf/package.nix
+++ pkgs/by-name/al/alsa-ucm-conf/package.nix
@@ -5,6 +5,7 @@
   stdenvNoCC,
   coreutils,
   kmod,
+  alsa-lib,
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
@@ -40,8 +41,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
       runHook postInstall
     '';
 
-  passthru.updateScript = nix-update-script {
-    extraArgs = [ "--version=branch" ];
+  passthru = {
+    tests = { inherit alsa-lib; };
+    updateScript = nix-update-script {
+      extraArgs = [ "--version=branch" ];
+    };
   };
 
   meta = {
