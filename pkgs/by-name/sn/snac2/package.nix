b2abbbe3be4993ee62e46729cb7fac1d806fac8a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-03-21 09:06:14 +0100

    snac2: switch to `finalAttrs`

--- pkgs/by-name/sn/snac2/package.nix
+++ pkgs/by-name/sn/snac2/package.nix
@@ -9,15 +9,15 @@
   snac2,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "snac2";
   version = "2.73";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "grunfink";
-    repo = pname;
-    rev = version;
+    repo = "snac2";
+    tag = finalAttrs.version;
     hash = "sha256-5LKDwp5f5BWhm+9uVBlv3mJpLLQ+ETP9lcRXlfD579Y=";
   };
 
@@ -38,18 +38,18 @@ stdenv.mkDerivation rec {
   passthru = {
     tests.version = testers.testVersion {
       package = snac2;
-      command = "${meta.mainProgram} || true";
+      command = "${finalAttrs.meta.mainProgram} || true";
     };
     updateScript = nix-update-script { };
   };
 
-  meta = with lib; {
-    homepage = "https://codeberg.org/grunfink/snac2";
+  meta = {
+    changelog = "https://codeberg.org/grunfink/snac2/src/tag/${finalAttrs.version}/RELEASE_NOTES.md";
     description = "Simple, minimalistic ActivityPub instance (2.x, C)";
-    changelog = "https://codeberg.org/grunfink/snac2/src/tag/${version}/RELEASE_NOTES.md";
-    license = licenses.mit;
-    maintainers = with maintainers; [ misuzu ];
-    platforms = platforms.unix;
+    homepage = "https://codeberg.org/grunfink/snac2";
+    license = lib.licenses.mit;
     mainProgram = "snac";
+    maintainers = with lib.maintainers; [ misuzu ];
+    platforms = lib.platforms.unix;
   };
-}
+})
