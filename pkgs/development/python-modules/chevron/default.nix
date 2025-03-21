f76618eeeff6b4ceb9506251320736eeed36f07a
Wolfgang Walther <walther@technowledgy.de>
2025-03-21 15:30:18 +0100

    python3Packages.chevron: modernize

--- pkgs/development/python-modules/chevron/default.nix
+++ pkgs/development/python-modules/chevron/default.nix
@@ -1,32 +1,37 @@
 {
-  lib,
   buildPythonPackage,
   fetchFromGitHub,
+  lib,
   python,
+  setuptools,
 }:
 
 buildPythonPackage {
   pname = "chevron";
   version = "0.14.0-unstable-2021-03-21";
-  format = "setuptools";
+  pyproject = true;
 
   # No tests available in the PyPI tarball
   src = fetchFromGitHub {
     owner = "noahmorrison";
     repo = "chevron";
     rev = "5e1c12827b7fc3db30cb3b24cae9a7ee3092822b";
-    sha256 = "sha256-44cxkliJJ+IozmhS4ekbb+pCa7tcUuX9tRNYTK0mC+w=";
+    hash = "sha256-44cxkliJJ+IozmhS4ekbb+pCa7tcUuX9tRNYTK0mC+w=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   checkPhase = ''
     ${python.interpreter} test_spec.py
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/noahmorrison/chevron";
     description = "Python implementation of the mustache templating language";
     mainProgram = "chevron";
-    license = licenses.mit;
-    maintainers = with maintainers; [ dhkl ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ dhkl ];
   };
 }
