89f28f22effe06974da2a5370e3c1b054a9a9231
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-03-21 17:32:30 -0300

    cosmic-workspaces-epoch: refactor

--- pkgs/by-name/co/cosmic-workspaces-epoch/package.nix
+++ pkgs/by-name/co/cosmic-workspaces-epoch/package.nix
@@ -11,14 +11,14 @@
   wayland,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-workspaces-epoch";
   version = "1.0.0-alpha.5.1";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-workspaces-epoch";
-    rev = "epoch-${version}";
+    tag = "epoch-${finalAttrs.version}";
     hash = "sha256-lAK7DZWwNMr30u6Uopew9O/6FIG6e2SgcdA+cD/K5Ok=";
   };
 
@@ -28,6 +28,7 @@ rustPlatform.buildRustPackage rec {
   separateDebugInfo = true;
 
   nativeBuildInputs = [ pkg-config ];
+
   buildInputs = [
     libxkbcommon
     libinput
@@ -53,12 +54,12 @@ rustPlatform.buildRustPackage rec {
     "-Wl,--pop-state"
   ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/pop-os/cosmic-workspaces-epoch";
     description = "Workspaces Epoch for the COSMIC Desktop Environment";
     mainProgram = "cosmic-workspaces";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ nyabinary ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ nyabinary ];
+    platforms = lib.platforms.linux;
   };
-}
+})
