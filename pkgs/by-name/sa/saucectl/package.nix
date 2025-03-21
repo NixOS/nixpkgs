fd257a8d4bed13b82028c3f8d7ee8880d54144f6
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-21 12:18:18 +0000

    saucectl: 0.193.0 -> 0.194.0

--- pkgs/by-name/sa/saucectl/package.nix
+++ pkgs/by-name/sa/saucectl/package.nix
@@ -5,7 +5,7 @@
 }:
 let
   pname = "saucectl";
-  version = "0.193.0";
+  version = "0.194.0";
 in
 buildGoModule {
   inherit pname version;
@@ -14,7 +14,7 @@ buildGoModule {
     owner = "saucelabs";
     repo = "saucectl";
     tag = "v${version}";
-    hash = "sha256-f4OtXjqtqcMIIssXq5UjPX450Ji2objhOcgm82m52V0=";
+    hash = "sha256-Fetm/Qh6v+4UNBvhexf+mQMBBWUvlpisHOoExuqRunk=";
   };
 
   ldflags = [
