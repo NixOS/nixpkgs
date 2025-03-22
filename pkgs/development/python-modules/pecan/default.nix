705d58f778cc25a776451affa055bd2bbf462637
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-22 01:48:05 +0000

    python312Packages.pecan: 1.5.1 -> 1.6.0

--- pkgs/development/python-modules/pecan/default.nix
+++ pkgs/development/python-modules/pecan/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "pecan";
-  version = "1.5.1";
+  version = "1.6.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-YGMnLV+GB3P7tLSyrhsJ2oyVQGLvhxFQwGz9sjkdk1U=";
+    hash = "sha256-X9RGlYPu0t7Te00QpHDhGl3j88lj3IeYTncuJcVv7T4=";
   };
 
   propagatedBuildInputs = [
