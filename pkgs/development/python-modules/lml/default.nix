e589a04f5e0ca048e93fce24e58bb4446a632447
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-22 08:36:57 +0100

    python312Packages.lml: refactor

--- pkgs/development/python-modules/lml/default.nix
+++ pkgs/development/python-modules/lml/default.nix
@@ -4,17 +4,20 @@
   fetchPypi,
   pytestCheckHook,
   mock,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "lml";
   version = "0.2.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-jdWvtDZ6WT0c2yFEoFh0zZk49SZr67DJ4UEyAEI8DXQ=";
+    hash = "sha256-jdWvtDZ6WT0c2yFEoFh0zZk49SZr67DJ4UEyAEI8DXQ=";
   };
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [
     pytestCheckHook
@@ -25,8 +28,9 @@ buildPythonPackage rec {
   doCheck = false;
 
   meta = {
-    description = "Load me later. A lazy plugin management system for Python";
+    description = "Plugin management system for Python";
     homepage = "http://lml.readthedocs.io/";
+    changelog = "https://github.com/python-lml/lml/releases/tag/v${version}";
     license = lib.licenses.bsd3;
     maintainers = [ ];
   };
