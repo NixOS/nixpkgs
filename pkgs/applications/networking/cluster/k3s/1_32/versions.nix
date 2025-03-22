ee0169324a97c93bb3852683b3c9ae1bdb1c94d0
Spencer Heywood <github@heywoodlh.io>
2025-03-21 18:54:18 -0600

    k3s: update containerd version

--- pkgs/applications/networking/cluster/k3s/1_32/versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_32/versions.nix
@@ -9,7 +9,7 @@
   k3sRootSha256 = "0svbi42agqxqh5q2ri7xmaw2a2c70s7q5y587ls0qkflw5vx4sl7";
   k3sCNIVersion = "1.6.0-k3s1";
   k3sCNISha256 = "0g7zczvwba5xqawk37b0v96xysdwanyf1grxn3l3lhxsgjjsmkd7";
-  containerdVersion = "1.7.23-k3s2";
-  containerdSha256 = "0lp9vxq7xj74wa7hbivvl5hwg2wzqgsxav22wa0p1l7lc1dqw8dm";
+  containerdVersion = "2.0.4-k3s2";
+  containerdSha256 = "sha256-Bmukq/OBCFhga9WVoG87kK3lWp7tv5I0KItegA20ZGw=";
   criCtlVersion = "1.31.0-k3s2";
 }
