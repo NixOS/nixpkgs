262036e47900b39f59ee5765432f73006a049004
Doron Behar <doron.behar@gmail.com>
2025-03-22 19:11:27 +0200

    python312Packages.sciform: init at 0.39.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14865,6 +14865,8 @@ self: super: with self; {
 
   scienceplots = callPackage ../development/python-modules/scienceplots { };
 
+  sciform = callPackage ../development/python-modules/sciform { };
+
   scim2-client = callPackage ../development/python-modules/scim2-client { };
 
   scim2-filter-parser = callPackage ../development/python-modules/scim2-filter-parser { };
