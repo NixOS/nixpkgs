a4c3dd1f121912ef998d27a50844d8fcccf6553b
Clément <git@drawbu.dev>
2025-03-22 10:28:55 +0100

    clpeak: 1.1.0 -> 1.1.4
    Updated version. Previous commit fixes the build, but as the library is
    updated, this build as a ton of warnings.

--- pkgs/by-name/cl/clpeak/package.nix
+++ pkgs/by-name/cl/clpeak/package.nix
@@ -9,23 +9,15 @@
 
 stdenv.mkDerivation rec {
   pname = "clpeak";
-  version = "1.1.0";
+  version = "1.1.4";
 
   src = fetchFromGitHub {
     owner = "krrishnarraj";
     repo = "clpeak";
     tag = version;
     fetchSubmodules = true;
-    sha256 = "1wkjpvn4r89c3y06rv7gfpwpqw6ljmqwz0w0mljl9y5hn1r4pkx2";
+    hash = "sha256-unQLZ5EExL9lU2XuYLJjASeFzDA74+TnU0CQTWyNYiQ=";
   };
-
-  patches = [
-    # The cl.hpp header was removed from opencl-clhpp. This patch
-    # updates clpeak to use the new cp2.hpp header. The patch comes
-    # from the following PR and was updated to apply against more
-    # recent versions: https://github.com/krrishnarraj/clpeak/pull/46
-    ./clpeak-clhpp2.diff
-  ];
 
   nativeBuildInputs = [ cmake ];
 
