ead07f0ba3efb7efd74dbf246d1919e7d8fc9a12
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-22 20:05:25 +0100

    python313Packages.pyhidra: enable tests

--- pkgs/development/python-modules/pyhidra/default.nix
+++ pkgs/development/python-modules/pyhidra/default.nix
@@ -3,8 +3,9 @@
   buildPythonPackage,
   fetchFromGitHub,
   jpype1,
+  pytest-datadir,
+  pytestCheckHook,
   setuptools,
-  wheel,
 }:
 
 buildPythonPackage rec {
@@ -23,7 +24,28 @@ buildPythonPackage rec {
 
   dependencies = [ jpype1 ];
 
+  nativeCheckInputs = [
+    pytest-datadir
+    pytestCheckHook
+  ];
+
   pythonImportsCheck = [ "pyhidra" ];
+
+  disabledTests = [
+    # Tests require a running Ghidra instance
+    "test_invalid_jpype_keyword_arg"
+    "test_invalid_vm_arg_succeed"
+    "test_run_script"
+    "test_open_program"
+    "test_no_compiler"
+    "test_no_language_with_compiler"
+    "test_loader"
+    "test_invalid_loader_type"
+    "test_no_project"
+    "test_no_program"
+    "test_import_script"
+    "test_import_ghidra_base_java_packages"
+  ];
 
   meta = {
     description = "Provides direct access to the Ghidra API within a native CPython interpreter using jpype";
