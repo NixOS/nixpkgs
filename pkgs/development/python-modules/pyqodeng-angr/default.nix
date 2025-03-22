f0f8b0d5615a67044201c5746a108721eb7bc81a
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-22 22:02:02 +0100

    python312Packages.pyqodeng-angr: 0.0.11 -> 0.0.13
    Diff: https://github.com/angr/pyqodeng/compare/refs/tags/v0.0.11...v0.0.13
    
    Changelog: https://github.com/angr/pyqodeng/blob/refs/tags/v0.0.13/CHANGELOG.rst

--- pkgs/development/python-modules/pyqodeng-angr/default.nix
+++ pkgs/development/python-modules/pyqodeng-angr/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "pyqodeng-angr";
-  version = "0.0.11";
+  version = "0.0.13";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "angr";
     repo = "pyqodeng";
     tag = "v${version}";
-    hash = "sha256-QA/h5CHGrv3HYIrRaJ1Eq+iXXmQ6dYbq2zixeO9rS1c=";
+    hash = "sha256-t4LcPVQfktAaTqTr9L2VDCEHbSO7qxCvUDz6rj0Zre4=";
   };
 
   postPatch = ''
