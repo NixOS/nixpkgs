3aa743ed60fa9d1c012802479758d55cd62b1a74
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-23 09:39:56 +0100

    python312Packages.pyads: update disabled

--- pkgs/development/python-modules/pyads/default.nix
+++ pkgs/development/python-modules/pyads/default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   version = "3.5.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "stlehmann";
