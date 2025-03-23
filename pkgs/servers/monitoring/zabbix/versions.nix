14e8d4c920e8d2635c2660bdf155ae42e5261a29
Thomas Gerbet <thomas@gerbet.me>
2025-03-23 19:08:51 +0100

    zabbix: remove not needed vendorHash in versions file

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -2,16 +2,13 @@ generic: {
   v72 = generic {
     version = "7.2.5";
     hash = "sha256-DQGzk90isqYLNvs3qY/PEIHGg62Ygyot3YeUOhIAg54=";
-    vendorHash = null;
   };
   v70 = generic {
     version = "7.0.11";
     hash = "sha256-r9VNy3bRuFvJAL+ZQEbKjCAS3+AvetnWAwFjLxFVuGU=";
-    vendorHash = null;
   };
   v60 = generic {
     version = "6.0.36";
     hash = "sha256-Ne0OY6NGzTYOn3sDVd+5tfawBu5VBjxNRtlxasubGCk=";
-    vendorHash = null;
   };
 }
