d5a48e618f60b013cc2602ddce73a8288572695b
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-23 09:41:18 +0100

    python312Packages.snakemake-storage-plugin-s3: add changelog to meta

--- pkgs/development/python-modules/snakemake-storage-plugin-s3/default.nix
+++ pkgs/development/python-modules/snakemake-storage-plugin-s3/default.nix
@@ -18,7 +18,7 @@ buildPythonPackage rec {
 
   src = fetchFromGitHub {
     owner = "snakemake";
-    repo = pname;
+    repo = "snakemake-storage-plugin-s3";
     tag = "v${version}";
     hash = "sha256-WbGlXu6/7e9U2GfVd4IF86bRfulNxjP1Tw5pVHpEj/g=";
   };
@@ -40,6 +40,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Snakemake storage plugin for S3 API storage (AWS S3, MinIO, etc.)";
     homepage = "https://github.com/snakemake/snakemake-storage-plugin-s3";
+    changelog = "https://github.com/snakemake/snakemake-storage-plugin-s3/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ veprbl ];
   };
