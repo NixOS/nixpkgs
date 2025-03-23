6976734598bda1e72787aca1d8e3b6a99170c576
K900 <me@0upti.me>
2025-03-23 08:58:09 +0300

    linux_6_11{,_hardened}: drop

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -197,14 +197,6 @@ in {
       ];
     };
 
-    linux_6_11 = callPackage ../os-specific/linux/kernel/mainline.nix {
-      branch = "6.11";
-      kernelPatches = [
-        kernelPatches.bridge_stp_helper
-        kernelPatches.request_key_helper
-      ];
-    };
-
     linux_6_12 = callPackage ../os-specific/linux/kernel/mainline.nix {
       branch = "6.12";
       kernelPatches = [
@@ -299,7 +291,6 @@ in {
     linux_5_15_hardened = hardenedKernelFor kernels.linux_5_15 { };
     linux_6_1_hardened = hardenedKernelFor kernels.linux_6_1 { };
     linux_6_6_hardened = hardenedKernelFor kernels.linux_6_6 { };
-    linux_6_11_hardened = hardenedKernelFor kernels.linux_6_11 { };
     linux_6_12_hardened = hardenedKernelFor kernels.linux_6_12 { };
     linux_6_13_hardened = hardenedKernelFor kernels.linux_6_13 { };
 
@@ -312,6 +303,7 @@ in {
     linux_6_8 = throw "linux 6.8 was removed because it has reached its end of life upstream";
     linux_6_9 = throw "linux 6.9 was removed because it has reached its end of life upstream";
     linux_6_10 = throw "linux 6.10 was removed because it has reached its end of life upstream";
+    linux_6_11 = throw "linux 6.11 was removed because it has reached its end of life upstream";
 
     linux_xanmod_tt = throw "linux_xanmod_tt was removed because upstream no longer offers this option";
 
@@ -323,6 +315,7 @@ in {
     linux_6_8_hardened = throw "linux 6.8 was removed because it has reached its end of life upstream";
     linux_6_9_hardened = throw "linux 6.9 was removed because it has reached its end of life upstream";
     linux_6_10_hardened = throw "linux 6.10 was removed because it has reached its end of life upstream";
+    linux_6_11_hardened = throw "linux 6.11 was removed because it has reached its end of life upstream";
   }));
   /*  Linux kernel modules are inherently tied to a specific kernel.  So
     rather than provide specific instances of those packages for a
@@ -677,7 +670,6 @@ in {
     linux_5_15 = recurseIntoAttrs (packagesFor kernels.linux_5_15);
     linux_6_1 = recurseIntoAttrs (packagesFor kernels.linux_6_1);
     linux_6_6 = recurseIntoAttrs (packagesFor kernels.linux_6_6);
-    linux_6_11 = recurseIntoAttrs (packagesFor kernels.linux_6_11);
     linux_6_12 = recurseIntoAttrs (packagesFor kernels.linux_6_12);
     linux_6_13 = recurseIntoAttrs (packagesFor kernels.linux_6_13);
   } // lib.optionalAttrs config.allowAliases {
@@ -689,6 +681,7 @@ in {
     linux_6_8 = throw "linux 6.8 was removed because it reached its end of life upstream"; # Added 2024-08-02
     linux_6_9 = throw "linux 6.9 was removed because it reached its end of life upstream"; # Added 2024-08-02
     linux_6_10 = throw "linux 6.10 was removed because it reached its end of life upstream"; # Added 2024-10-23
+    linux_6_11 = throw "linux 6.11 was removed because it reached its end of life upstream"; # Added 2025-03-23
   };
 
   rtPackages = {
@@ -719,7 +712,6 @@ in {
     linux_5_15_hardened = recurseIntoAttrs (packagesFor kernels.linux_5_15_hardened);
     linux_6_1_hardened = recurseIntoAttrs (packagesFor kernels.linux_6_1_hardened);
     linux_6_6_hardened = recurseIntoAttrs (packagesFor kernels.linux_6_6_hardened);
-    linux_6_11_hardened = recurseIntoAttrs (packagesFor kernels.linux_6_11_hardened);
     linux_6_12_hardened = recurseIntoAttrs (packagesFor kernels.linux_6_12_hardened);
     linux_6_13_hardened = recurseIntoAttrs (packagesFor kernels.linux_6_13_hardened);
 
@@ -743,6 +735,7 @@ in {
     linux_6_7_hardened = throw "linux 6.7 was removed because it has reached its end of life upstream";
     linux_6_8_hardened = throw "linux 6.8 was removed because it has reached its end of life upstream";
     linux_6_9_hardened = throw "linux 6.9 was removed because it has reached its end of life upstream";
+    linux_6_11_hardened = throw "linux 6.11 was removed because it has reached its end of life upstream";
     linux_xanmod_tt = throw "linux_xanmod_tt was removed because upstream no longer offers this option";
   });
 
