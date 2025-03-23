95a7376b0f7e2b3a5c7abc44985fd98324cefbd0
Jan Tojnar <jtojnar@gmail.com>
2025-03-23 04:56:39 +0000

    tinysparql: 3.9.rc → 3.9.1
    https://gitlab.gnome.org/GNOME/tinysparql/-/compare/3.9.rc...3.9.1

--- pkgs/by-name/ti/tinysparql/package.nix
+++ pkgs/by-name/ti/tinysparql/package.nix
@@ -37,7 +37,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "tinysparql";
-  version = "3.9.rc";
+  version = "3.9.1";
 
   outputs = [
     "out"
@@ -49,7 +49,7 @@ stdenv.mkDerivation (finalAttrs: {
     url =
       with finalAttrs;
       "mirror://gnome/sources/tinysparql/${lib.versions.majorMinor version}/tinysparql-${version}.tar.xz";
-    hash = "sha256-IWxMRC1d09R2t0YVpMwrOSs4+21NFtFqo7kV3JLy0hM=";
+    hash = "sha256-mOiTO5RtjFSnfq2r1/HRTN3Yj6Q+7w0KzjwPzOZGcis=";
   };
 
   strictDeps = true;
