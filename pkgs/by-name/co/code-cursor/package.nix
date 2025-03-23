8a54cb34b66968a785f15d2dcf38b16100e0ea47
kolaente <k@knt.li>
2025-03-23 14:52:08 +0100

    code-cursor: fix updateScript
    Upstream changed their update api, that's why the old script stopped working

--- pkgs/by-name/co/code-cursor/package.nix
+++ pkgs/by-name/co/code-cursor/package.nix
@@ -100,45 +100,43 @@ stdenvNoCC.mkDerivation {
     inherit sources;
     updateScript = writeScript "update.sh" ''
       #!/usr/bin/env nix-shell
-      #!nix-shell -i bash -p curl yq coreutils gnused trurl common-updater-scripts
+      #!nix-shell -i bash -p curl jq coreutils gnused trurl common-updater-scripts
       set -eu -o pipefail
 
-      baseUrl="https://download.todesktop.com/230313mzl4w4u92"
-      latestLinux="$(curl -s $baseUrl/latest-linux.yml)"
-      latestDarwin="$(curl -s $baseUrl/latest-mac.yml)"
-      linuxVersion="$(echo "$latestLinux" | yq -r .version)"
+      baseUrl="https://www.cursor.com/api/download"
 
+      latestLinux="$(curl -s "''${baseUrl}?releaseTrack=stable&platform=linux-x64")"
+      latestDarwin="$(curl -s "''${baseUrl}?releaseTrack=stable&platform=darwin-arm64")"
+
+      linuxVersion="$(echo "$latestLinux" | jq -r .version)"
       currentVersion=$(nix-instantiate --eval -E "with import ./. {}; code-cursor.version or (lib.getVersion code-cursor)" | tr -d '"')
 
       if [[ "$linuxVersion" != "$currentVersion" ]]; then
-          darwinVersion="$(echo "$latestDarwin" | yq -r .version)"
+          darwinVersion="$(echo "$latestDarwin" | jq -r .version)"
           if [ "$linuxVersion" != "$darwinVersion" ]; then
               echo "Linux version ($linuxVersion) and Darwin version ($darwinVersion) do not match"
               exit 1
           fi
           version="$linuxVersion"
 
-          linuxFilename="$(echo "$latestLinux" | yq -r '.files[] | .url | select(. | endswith(".AppImage"))' | head -n 1)"
-          linuxStem="$(echo "$linuxFilename" | sed -E s/^\(.+build.+\)-[^-]+AppImage$/\\1/)"
-
-          darwinFilename="$(echo "$latestDarwin" | yq -r '.files[] | .url | select(. | endswith(".dmg"))' | head -n 1)"
-          darwinStem="$(echo "$darwinFilename" | sed -E s/^\(.+Build[^-]+\)-.+dmg$/\\1/)"
-
-          for platform in  "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"; do
+          # For each platform, get the download URL directly from the API
+          for platform in "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"; do
               if [ $platform = "x86_64-linux" ]; then
-                  url="$baseUrl/$linuxStem-x86_64.AppImage"
+                  apiPlatform="linux-x64"
               elif [ $platform = "aarch64-linux" ]; then
-                  url="$baseUrl/$linuxStem-arm64.AppImage"
+                  apiPlatform="linux-arm64"
               elif [ $platform = "x86_64-darwin" ]; then
-                  url="$baseUrl/$darwinStem-x64.dmg"
+                  apiPlatform="darwin-x64"
               elif [ $platform = "aarch64-darwin" ]; then
-                  url="$baseUrl/$darwinStem-arm64.dmg"
+                  apiPlatform="darwin-arm64"
               else
                   echo "Unsupported platform: $platform"
                   exit 1
               fi
 
-              url=$(trurl --accept-space "$url")
+              response="$(curl -s "''${baseUrl}?releaseTrack=stable&platform=''${apiPlatform}")"
+              url="$(echo "$response" | jq -r .downloadUrl)"
+
               hash=$(nix-hash --to-sri --type sha256 "$(nix-prefetch-url "$url" --name "cursor-$version")")
               update-source-version code-cursor $version $hash $url --system=$platform --ignore-same-version --source-key="sources.$platform"
           done
