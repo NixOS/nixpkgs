b58971a11949bde5527af647f6a1ecf809f4b045
Jan Tojnar <jtojnar@gmail.com>
2025-03-23 04:55:45 +0000

    sysprof: 48.rc → 48.0
    https://gitlab.gnome.org/GNOME/sysprof/-/compare/48.rc...48.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/development/tools/profiling/sysprof/default.nix
+++ pkgs/development/tools/profiling/sysprof/default.nix
@@ -26,7 +26,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sysprof";
-  version = "48.rc";
+  version = "48.0";
 
   outputs = [
     "out"
@@ -36,7 +36,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/sysprof/${lib.versions.major finalAttrs.version}/sysprof-${finalAttrs.version}.tar.xz";
-    hash = "sha256-KfA1DAoGungDtOgzRWw4ofZe6Fz/B0tLRvAgfKhjyu8=";
+    hash = "sha256-Gw8DgPLzBwi6h4KTIaBv7h2zbfqHeXu/B/CnrPRJjRg=";
   };
 
   nativeBuildInputs = [
