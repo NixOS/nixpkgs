9af8214b59d4019653ccb12baeed733e42ded92b
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-23 09:47:43 +0100

    python312Packages.nethsm: refactor

--- pkgs/development/python-modules/nethsm/default.nix
+++ pkgs/development/python-modules/nethsm/default.nix
@@ -10,13 +10,9 @@
   urllib3,
 }:
 
-let
+buildPythonPackage rec {
   pname = "nethsm";
   version = "1.3.0";
-in
-
-buildPythonPackage {
-  inherit pname version;
   pyproject = true;
 
   src = fetchPypi {
@@ -24,7 +20,11 @@ buildPythonPackage {
     hash = "sha256-F0jSlT/xM8xaQWfUp87p+2RY/8hG7vRq8/VJ4a5Fkhw=";
   };
 
-  propagatedBuildInputs = [
+  pythonRelaxDeps = true;
+
+  build-system = [ flit-core ];
+
+  dependencies = [
     certifi
     cryptography
     python-dateutil
@@ -32,19 +32,13 @@ buildPythonPackage {
     urllib3
   ];
 
-  nativeBuildInputs = [
-    flit-core
-  ];
-
-  pythonRelaxDeps = true;
-
   pythonImportsCheck = [ "nethsm" ];
 
   meta = with lib; {
     description = "Client-side Python SDK for NetHSM";
     homepage = "https://github.com/Nitrokey/nethsm-sdk-py";
     changelog = "https://github.com/Nitrokey/nethsm-sdk-py/releases/tag/v${version}";
-    license = with licenses; [ asl20 ];
+    license = licenses.asl20;
     maintainers = with maintainers; [ frogamic ];
   };
 }
