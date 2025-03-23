9fa319eb6ea5985def5e69eb614424f2969887d0
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-23 11:52:20 +0100

    python313Packages.motmetrics: refactor
    - add changelog to meta

--- pkgs/development/python-modules/motmetrics/default.nix
+++ pkgs/development/python-modules/motmetrics/default.nix
@@ -25,14 +25,14 @@ buildPythonPackage {
   src = fetchFromGitHub {
     owner = "cheind";
     repo = "py-motmetrics";
-    # latest release is not compatible with pandas 2.0
+    # Latest release is not compatible with pandas 2.0
     rev = "c199b3e853d589af4b6a7d88f5bcc8b8802fc434";
     hash = "sha256-DJ82nioW3jdIVo1B623BE8bBhVa1oMzYIkhhit4Z4dg=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     numpy
     pandas
     scipy
@@ -44,11 +44,14 @@ buildPythonPackage {
     pytest-benchmark
   ];
 
+  pytestFlagsArray = [ "--benchmark-disable" ];
+
   pythonImportsCheck = [ "motmetrics" ];
 
   meta = with lib; {
-    description = "Bar_chart: Benchmark multiple object trackers (MOT) in Python";
+    description = "Benchmark multiple object trackers (MOT) in Python";
     homepage = "https://github.com/cheind/py-motmetrics";
+    changelog = "https://github.com/cheind/py-motmetrics/releases/tag/${version}";
     license = licenses.mit;
     maintainers = [ ];
   };
