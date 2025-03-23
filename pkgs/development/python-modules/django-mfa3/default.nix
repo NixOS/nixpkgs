64fac4268d4f2ee2b3f71e0c5591de9b1043d7b9
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-23 20:09:09 +0100

    python312Packages.django-mfa3: 0.13.0 -> 0.15.1
    Diff: https://github.com/xi/django-mfa3/compare/refs/tags/0.13.0...0.15.1
    
    Changelog: https://github.com/xi/django-mfa3/blob/refs/tags/0.15.1/CHANGES.md

--- pkgs/development/python-modules/django-mfa3/default.nix
+++ pkgs/development/python-modules/django-mfa3/default.nix
@@ -8,30 +8,19 @@
   fido2,
   qrcode,
   python,
-  fetchpatch,
 }:
 
 buildPythonPackage rec {
   pname = "django-mfa3";
-  version = "0.13.0";
+  version = "0.15.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "xi";
     repo = "django-mfa3";
     tag = version;
-    hash = "sha256-O8po7VevqyHlP2isnNnLbpgfs1p4sFezxIZKMTgnwuY=";
+    hash = "sha256-HcurgGSzPnKVRpL9NVq0vkCmYDvj/HoWYVbnIrK5pSI=";
   };
-
-  patches = [
-    # Fix for tests.tests.FIDO2Test.test_origin_https
-    # https://github.com/xi/django-mfa3/issues/24
-    (fetchpatch {
-      url = "https://github.com/xi/django-mfa3/commit/49003746783e32cd60e55c4593bef5d7e709c4bd.patch";
-      hash = "sha256-D3fPURAB+RC16fSd2COpCIcmjZW/1h92GOOhRczSVec=";
-      name = "test_origin_https_fix.patch";
-    })
-  ];
 
   build-system = [ setuptools ];
 
