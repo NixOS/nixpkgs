411d2a05833030cc6add196e826f28c3ec859e61
yeoldegrove <git@yeoldegrove.de>
2025-03-23 17:07:35 +0100

    maintainers: add @yeoldegrove

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -26219,6 +26219,12 @@
     githubId = 364160;
     matrix = "@woobilicious:matrix.org";
   };
+  yeoldegrove = {
+    email = "git@yeoldegrove.de";
+    github = "yeoldegrove";
+    githubId = 8668070;
+    name = "Eike Waldt";
+  };
   yesbox = {
     email = "jesper.geertsen.jonsson@gmail.com";
     github = "yesbox";
