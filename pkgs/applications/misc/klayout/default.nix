0817c203ba05ec82adbb4f07275b6defc39ef353
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-24 08:54:26 +0000

    klayout: 0.29.12 -> 0.30.0

--- pkgs/applications/misc/klayout/default.nix
+++ pkgs/applications/misc/klayout/default.nix
@@ -5,13 +5,13 @@
 
 mkDerivation rec {
   pname = "klayout";
-  version = "0.29.12";
+  version = "0.30.0";
 
   src = fetchFromGitHub {
     owner = "KLayout";
     repo = "klayout";
     rev = "v${version}";
-    hash = "sha256-TLLAIlZYKGeQENtzfc9ilWwl4yu2ln7yBy+VW7Zwexc=";
+    hash = "sha256-i7MQqkVf+NZkmcf589BpLofwqc5KGxRNqdr1Go84M9A=";
   };
 
   postPatch = ''
