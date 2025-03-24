c4ee9fb2a7184d8d995d4bcefc34af50208838d9
lucasew <lucas59356@gmail.com>
2025-03-23 23:33:57 -0300

    xpra-html5: init at 17
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ pkgs/by-name/xp/xpra-html5/package.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenvNoCC,
+  python3,
+  uglify-js,
+  gitUpdater,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  name = "xpra-html5";
+  version = "17";
+
+  src = fetchFromGitHub {
+    owner = "Xpra-org";
+    repo = "xpra-html5";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-SwP7NazsiUyDD4LUziCwN0X9GTQVq0lYM2jXqNaXLEA=";
+  };
+
+  buildInputs = [
+    python3
+    uglify-js
+  ];
+
+  installPhase = "python $src/setup.py install $out /share/xpra/www /share/xpra/www";
+
+  passthru.updateScript = gitUpdater { rev-prefix = "v"; };
+
+  meta = {
+    homepage = "https://xpra.org/";
+    downloadPage = "https://xpra.org/src/";
+    description = "HTML5 client for Xpra";
+    changelog = "https://github.com/Xpra-org/xpra-html5/releases/tag/v${finalAttrs.version}";
+    platforms = lib.platforms.linux;
+    license = lib.licenses.mpl20;
+    maintainers = with lib.maintainers; [
+      catern
+      lucasew
+    ];
+  };
+})
