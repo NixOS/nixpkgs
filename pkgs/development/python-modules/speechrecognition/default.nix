27cc3de345481077ce744638169140157e6767d1
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-24 02:28:55 +0000

    python312Packages.speechrecognition: 3.14.1 -> 3.14.2

--- pkgs/development/python-modules/speechrecognition/default.nix
+++ pkgs/development/python-modules/speechrecognition/default.nix
@@ -24,7 +24,7 @@
 
 buildPythonPackage rec {
   pname = "speechrecognition";
-  version = "3.14.1";
+  version = "3.14.2";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -33,7 +33,7 @@ buildPythonPackage rec {
     owner = "Uberi";
     repo = "speech_recognition";
     tag = version;
-    hash = "sha256-4FrFiDRqTZnLB3hTy8hZovlakZsRFEg2ZGitJhJ6DA0=";
+    hash = "sha256-Y2kmwHws/auzIa7dLir3sYZdFK5l70GEyfgwscQhMic=";
   };
 
   postPatch = ''
