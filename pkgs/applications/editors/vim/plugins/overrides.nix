5f037b8fb17cccefc4b2ee8d4d3e97e40ca8a290
Austin Horstman <khaneliman12@gmail.com>
2025-03-23 21:10:55 -0500

    vimplugins.coc-*: move into cocPlugins
    coc is breaking out packages into top-level, but I don't know if we
    really need separate folders for each of these. Can just create another
    set to merge in.

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -16,11 +16,6 @@
   mkYarnModules,
   python3,
   # Misc dependencies
-  coc-clangd,
-  coc-css,
-  coc-diagnostic,
-  coc-pyright,
-  coc-toml,
   code-minimap,
   dasht,
   deno,
@@ -609,33 +604,6 @@ in
       "cobalt2.plugins.marks"
       "cobalt2.theme"
     ];
-  };
-
-  coc-clangd = buildVimPlugin {
-    inherit (coc-clangd) pname version meta;
-    src = "${coc-clangd}/lib/node_modules/coc-clangd";
-  };
-
-  coc-css = buildVimPlugin {
-    inherit (coc-css) pname version meta;
-    src = "${coc-css}/lib/node_modules/coc-css";
-  };
-
-  coc-diagnostic = buildVimPlugin {
-    inherit (coc-diagnostic) pname version meta;
-    src = "${coc-diagnostic}/lib/node_modules/coc-diagnostic";
-  };
-
-  coc-pyright = buildVimPlugin {
-    pname = "coc-pyright";
-    inherit (coc-pyright) version meta;
-    src = "${coc-pyright}/lib/node_modules/coc-pyright";
-  };
-
-  coc-toml = buildVimPlugin {
-    pname = "coc-toml";
-    inherit (coc-toml) version meta;
-    src = "${coc-toml}/lib/node_modules/coc-toml";
   };
 
   codecompanion-nvim = super.codecompanion-nvim.overrideAttrs {
