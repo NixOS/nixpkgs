eaec6a5eaffafa7929531f81a5c9df4995c439a5
thiloho <123883702+thiloho@users.noreply.github.com>
2025-03-24 11:41:04 +0100

    archtika: fix update script by passing src to passthru

--- pkgs/by-name/ar/archtika/package.nix
+++ pkgs/by-name/ar/archtika/package.nix
@@ -9,13 +9,13 @@
 }:
 
 let
-  version = "1.2.0";
+  version = "1.2.1";
 
   src = fetchFromGitHub {
     owner = "archtika";
     repo = "archtika";
     tag = "v${version}";
-    hash = "sha256-ba9da7LqCE/e2lhRVHD7GOhwOj1fNTBbN/pARPMzIg4=";
+    hash = "sha256-GffYAtLs12v2Lt1WoKJOG5dZsmzDcySZKFBQwCT9nnY=";
   };
 
   web = buildNpmPackage {
@@ -50,7 +50,10 @@ symlinkJoin {
     api
   ];
 
-  passthru.updateScript = nix-update-script { };
+  passthru = {
+    inherit src;
+    updateScript = nix-update-script { };
+  };
 
   meta = {
     description = "Modern, performant and lightweight CMS";
