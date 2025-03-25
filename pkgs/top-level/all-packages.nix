a42498f649b63e116bc533d65daa0e9df8a8f8e7
jopejoe1 <jopejoe1@missing.ninja>
2025-03-25 17:44:35 +0100

    libcdio: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9311,8 +9311,6 @@ with pkgs;
 
   libcef = callPackage ../development/libraries/libcef { };
 
-  libcdio = callPackage ../development/libraries/libcdio { };
-
   libcdio-paranoia = callPackage ../development/libraries/libcdio-paranoia {
     inherit (darwin.apple_sdk.frameworks) DiskArbitration IOKit;
   };
