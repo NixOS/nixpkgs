4f7feb9685286ec9f3bcb4d1496af51c9af9deba
Jörg Thalheim <joerg@thalheim.io>
2025-03-25 09:28:24 +0100

    thepeg: as broken on macOS

--- pkgs/by-name/th/thepeg/package.nix
+++ pkgs/by-name/th/thepeg/package.nix
@@ -55,6 +55,12 @@ stdenv.mkDerivation rec {
     description = "Toolkit for High Energy Physics Event Generation";
     homepage = "https://herwig.hepforge.org/";
     license = licenses.gpl3Only;
+    # Broken on macOS/clang?
+    #        > ../include/ThePEG/Config/std.h:101:12: error: no member named 'mem_fun' in namespace 'std'; did you mean 'mem_fn'?
+    #   >   101 | using std::mem_fun;
+    #   >       |       ~~~~~^~~~~~~
+    #   >       |            mem_fn
+    broken = stdenv.hostPlatform.isDarwin;
     maintainers = with maintainers; [ veprbl ];
     platforms = platforms.unix;
   };
