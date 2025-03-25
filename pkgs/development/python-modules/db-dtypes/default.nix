fb56f0fa3bae6f2850be036fdca9c2288035435a
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-03-25 23:37:46 +0100

    python312Packages.db-dtypes: drop support for python 3.7

--- pkgs/development/python-modules/db-dtypes/default.nix
+++ pkgs/development/python-modules/db-dtypes/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "1.4.2";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "googleapis";
