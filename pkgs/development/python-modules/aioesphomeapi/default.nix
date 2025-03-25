dd602ac316c1b17155a09b67d826bebef9bb3af1
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-25 12:30:31 +0000

    python312Packages.aioesphomeapi: 29.4.0 -> 29.7.0

--- pkgs/development/python-modules/aioesphomeapi/default.nix
+++ pkgs/development/python-modules/aioesphomeapi/default.nix
@@ -26,7 +26,7 @@
 
 buildPythonPackage rec {
   pname = "aioesphomeapi";
-  version = "29.4.0";
+  version = "29.7.0";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -35,7 +35,7 @@ buildPythonPackage rec {
     owner = "esphome";
     repo = "aioesphomeapi";
     tag = "v${version}";
-    hash = "sha256-KBQYLaE2J/5/VYquajknF3gCJEwRENjgkxiZahqnRZA=";
+    hash = "sha256-VOa6tVCsBwX2Q0fu/gwfErZZ35zzZelFe9QmOt7/uGI=";
   };
 
   build-system = [
