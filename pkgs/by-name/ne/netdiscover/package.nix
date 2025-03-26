cf781848ad63984a40f934aa3f554355c001b5a7
Fabian Affolter <mail@fabian-affolter.ch>
2025-03-26 19:56:30 +0100

    netdiscover: add changelog to meta

--- pkgs/by-name/ne/netdiscover/package.nix
+++ pkgs/by-name/ne/netdiscover/package.nix
@@ -14,11 +14,12 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "netdiscover-scanner";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-LUM6vl6pohrTW3X9c1FaDE7j9mvMuAiDkFBKWc4KZso=";
+    tag = version;
+    hash = "sha256-LUM6vl6pohrTW3X9c1FaDE7j9mvMuAiDkFBKWc4KZso=";
   };
 
   nativeBuildInputs = [ autoreconfHook ];
+
   buildInputs = [
     libpcap
     libnet
@@ -29,6 +30,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "Network address discovering tool, developed mainly for those wireless networks without dhcp server, it also works on hub/switched networks";
     homepage = "https://github.com/netdiscover-scanner/netdiscover";
+    changelog = "https://github.com/netdiscover-scanner/netdiscover/releases/tag/${src.tag}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ vdot0x23 ];
     platforms = platforms.unix;
