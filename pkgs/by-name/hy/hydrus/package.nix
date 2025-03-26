58561fac4b3db9ddc4dde1ba441ea250facd66ed
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-26 07:23:55 +0000

    hydrus: 612 -> 614

--- pkgs/by-name/hy/hydrus/package.nix
+++ pkgs/by-name/hy/hydrus/package.nix
@@ -16,14 +16,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "hydrus";
-  version = "612";
+  version = "614";
   format = "other";
 
   src = fetchFromGitHub {
     owner = "hydrusnetwork";
     repo = "hydrus";
     tag = "v${version}";
-    hash = "sha256-zRaabW1QwNNgA3F84CFSDbxyALV74T3t8u8WdL8GwG0=";
+    hash = "sha256-7UYi2dbpoGy373akOKFJjssxMdCKjpv4IgPMqBoe93Q=";
   };
 
   nativeBuildInputs = [
@@ -162,7 +162,7 @@ python3Packages.buildPythonApplication rec {
     description = "Danbooru-like image tagging and searching system for the desktop";
     license = lib.licenses.wtfpl;
     homepage = "https://hydrusnetwork.github.io/hydrus/";
-    changelog = "https://github.com/hydrusnetwork/hydrus/releases/tag/v${version}";
+    changelog = "https://github.com/hydrusnetwork/hydrus/releases/tag/${src.tag}";
     maintainers = with lib.maintainers; [
       dandellion
       evanjs
