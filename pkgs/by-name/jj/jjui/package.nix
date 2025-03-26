1ada9d7b9577c850bbbfa89db5a8410bc57b9602
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-26 09:47:17 +0000

    jjui: 0.7 -> 0.7.2

--- pkgs/by-name/jj/jjui/package.nix
+++ pkgs/by-name/jj/jjui/package.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule rec {
   pname = "jjui";
-  version = "0.7";
+  version = "0.7.2";
 
   src = fetchFromGitHub {
     owner = "idursun";
     repo = "jjui";
     tag = "v${version}";
-    hash = "sha256-y93ANgIlHafwZJ1jOIzGxt81G6YbHO7/PFqYovgGBMc=";
+    hash = "sha256-dtMkq94p9e6c336WWg+0noJMIezuca8mt5h+zLuYpCg=";
   };
 
   vendorHash = "sha256-84VMhT+Zbub9sw+lAKEZba1aXcRaTIbnYhJ7zJt118Y=";
