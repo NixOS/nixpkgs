ba61bcfe8c72c6f853c2f7c22e52bad17f66ec44
Sergey Volkov <taranarmo@gmail.com>
2025-03-26 10:33:42 +0100

    python3Packages.cadwyn: init at 5.6.1
    Newer versions currently don't build with fastapi 0.121
    
    Co-authored-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2383,6 +2383,8 @@ self: super: with self; {
 
   cachy = callPackage ../development/python-modules/cachy { };
 
+  cadwyn = callPackage ../development/python-modules/cadwyn { };
+
   caffe = toPythonModule (
     pkgs.caffe.override {
       pythonSupport = true;
