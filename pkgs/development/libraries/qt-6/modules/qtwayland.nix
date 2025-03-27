5df8cbb9055042c8c3c53dc3c1febcf3ab9d193e
K900 <me@0upti.me>
2025-03-27 12:16:52 +0300

    qt6: 6.8.3 -> 6.9

--- pkgs/development/libraries/qt-6/modules/qtwayland.nix
+++ pkgs/development/libraries/qt-6/modules/qtwayland.nix
@@ -7,7 +7,6 @@
   wayland-scanner,
   pkg-config,
   libdrm,
-  fetchpatch2,
 }:
 
 qtModule {
@@ -27,15 +26,6 @@ qtModule {
   ];
   buildInputs = [ libdrm ];
   nativeBuildInputs = [ pkg-config ];
-
-  patches = [
-    # run waylandscanner with private-code to avoid conflict with symbols from libwayland
-    # better solution for https://github.com/NixOS/nixpkgs/pull/337913
-    (fetchpatch2 {
-      url = "https://invent.kde.org/qt/qt/qtwayland/-/commit/67f121cc4c3865aa3a93cf563caa1d9da3c92695.patch";
-      hash = "sha256-uh5lecHlHCWyO1/EU5kQ00VS7eti3PEvPA2HBCL9K0k=";
-    })
-  ];
 
   meta = {
     platforms = lib.platforms.unix;
