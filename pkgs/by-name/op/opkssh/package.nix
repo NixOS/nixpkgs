4a26827d408578017c77cf24aba736449e7b6a5e
Robert James Hernandez <rob@sarcasticadmin.com>
2025-03-27 22:43:16 +0000

    opkssh: update description

--- pkgs/by-name/op/opkssh/package.nix
+++ pkgs/by-name/op/opkssh/package.nix
@@ -31,7 +31,7 @@ buildGoModule (finalAttrs: {
 
   meta = {
     homepage = "https://github.com/openpubkey/opkssh";
-    description = "Integrating SSO with SSH - short-lived SSH keys with an OpenID provider";
+    description = "Enables SSH to be used with OpenID Connect";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [
       johnrichardrinehart
