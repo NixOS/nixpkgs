8816db8f26dd39f4615c8ed04d95be889dc23056
K900 <me@0upti.me>
2025-03-27 12:15:07 +0300

    qt6: 6.8.2 -> 6.8.3
    (cherry picked from commit e313574366549402067b6ccc1ccde4baa420117d)

--- pkgs/development/libraries/qt-6/modules/qtbase/default.nix
+++ pkgs/development/libraries/qt-6/modules/qtbase/default.nix
@@ -230,20 +230,6 @@ stdenv.mkDerivation rec {
     ./qmlimportscanner-import-path.patch
     # don't pass qtbase's QML directory to qmlimportscanner if it's empty
     ./skip-missing-qml-directory.patch
-
-    # FIXME: 6.8.3 backports recommended by KDE
-    (fetchpatch {
-      url = "https://invent.kde.org/qt/qt/qtbase/-/commit/12d4bf1ab52748cb84894f50d437064b439e0b7d.patch";
-      hash = "sha256-HBwmQyAyaJh+in50Kd+mMa/6t+GZC3UmQWSe7Ugvn2Y=";
-    })
-    (fetchpatch {
-      url = "https://invent.kde.org/qt/qt/qtbase/-/commit/2ef615228bba9a8eb282437bfb7472f925610e89.patch";
-      hash = "sha256-pkKA7o7er9n5mu8EfJsjs8NeEq/SlKpEoRZwsDor1+c=";
-    })
-    (fetchpatch {
-      url = "https://invent.kde.org/qt/qt/qtbase/-/commit/a43c7e58046604796aa69974ea1c5d3e2648c755.patch";
-      hash = "sha256-4KJn7RTpSi8IFUElt3LEoMsuJmkYSf+bp2/Jmf42Ygs=";
-    })
   ];
 
   postPatch = lib.optionalString stdenv.hostPlatform.isDarwin ''
