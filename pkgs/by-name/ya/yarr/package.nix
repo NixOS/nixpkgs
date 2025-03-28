3590df4d9fba7f8dc45b4c60486fd82880b21f16
Christoph Heiss <christoph@c8h4.io>
2025-03-28 08:47:59 +0100

    yarr: switch to versionCheckHook
    Signed-off-by: Christoph Heiss <christoph@c8h4.io>

--- pkgs/by-name/ya/yarr/package.nix
+++ pkgs/by-name/ya/yarr/package.nix
@@ -2,8 +2,7 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
-  testers,
-  yarr,
+  versionCheckHook,
 }:
 
 buildGoModule rec {
@@ -31,11 +30,9 @@ buildGoModule rec {
     "sqlite_json"
   ];
 
-
-  passthru.tests.version = testers.testVersion {
-    package = yarr;
-    version = "v${version}";
-  };
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
 
   meta = with lib; {
     description = "Yet another rss reader";
