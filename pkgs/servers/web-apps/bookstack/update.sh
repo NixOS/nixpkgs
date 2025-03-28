68d881c188597b545e5ac9185de41b04bcdbe9ad
Hritwik <hritwiksinghal@protonmail.com>
2025-03-29 02:10:34 +0530

    bookstack: 25.02.1: fix lint errors

--- pkgs/servers/web-apps/bookstack/update.sh
+++ pkgs/servers/web-apps/bookstack/update.sh
@@ -23,10 +23,9 @@ fi
 curl -LO "$BOOKSTACK/composer.json"
 curl -LO "$BOOKSTACK/composer.lock"
 
-# this will fix the faulty url of htmldiff in composer.lock. 
+# this will fix the faulty url of htmldiff in composer.lock.
 # If you dont see "https://codeberg.org/api/v1/repos/danb/HtmlDiff/archive/%prettyVersion%.zip" in "ssddanbrown/htmldiff" section of compose.lock AND the build is breaking, try removing below line.
 sed -i 's/%prettyVersion%/main/g' composer.lock
-
 
 composer2nix --name "bookstack" \
   --composition=composition.nix \
