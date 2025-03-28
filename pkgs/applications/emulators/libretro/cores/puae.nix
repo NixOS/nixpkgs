56cc9421572c969246d263d6d201e8850701e735
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-28 11:09:37 +0000

    libretro.puae: 0-unstable-2025-02-20 -> 0-unstable-2025-03-27

--- pkgs/applications/emulators/libretro/cores/puae.nix
+++ pkgs/applications/emulators/libretro/cores/puae.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "puae";
-  version = "0-unstable-2025-02-20";
+  version = "0-unstable-2025-03-27";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "libretro-uae";
-    rev = "12972a7da5aeb849ba7234dff342625d23617eaf";
-    hash = "sha256-/w1Rbw0eStWyoR+UDD7a298vVAa0vb67qUPfiqhfm/k=";
+    rev = "987ac9bf14b813bf14ee6ab0f9d1f95c9d19ea78";
+    hash = "sha256-ONL7KjDMF+syiwBG+ivU7b7D7qFVr2gpw5ulnV3OZU8=";
   };
 
   makefile = "Makefile";
