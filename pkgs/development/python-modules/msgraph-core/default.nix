822f7f09b353afec8b56cd655aab8355420d4b66
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-28 16:48:09 +0000

    python312Packages.msgraph-core: 1.3.2 -> 1.3.3

--- pkgs/development/python-modules/msgraph-core/default.nix
+++ pkgs/development/python-modules/msgraph-core/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "msgraph-core";
-  version = "1.3.2";
+  version = "1.3.3";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "microsoftgraph";
     repo = "msgraph-sdk-python-core";
     tag = "v${version}";
-    hash = "sha256-SKuqYEDsHiasvs7nwrizYqfA6tTcc8XfZo8BD8guJHA=";
+    hash = "sha256-gYZWKv70lyuZIYXpchNnZ02J65hN45agDkxZVFed28s=";
   };
 
   build-system = [ setuptools ];
