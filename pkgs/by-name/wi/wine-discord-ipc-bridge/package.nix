e3e49405442e46f9477e248879251c9f8f71ebd6
George Huebner <george@feyor.sh>
2025-03-27 22:47:19 -0500

    wine-discord-ipc-bridge: add support for darwin hosts
    macOS cannot run 32-bit code (https://support.apple.com/en-us/103076),
    so system calls must be made with 64-bit calling convention and with
    `syscall` instead of `int 0x80`. Additionally, XNU requires that
    0x20000000 is added to syscall number.

--- pkgs/by-name/wi/wine-discord-ipc-bridge/package.nix
+++ pkgs/by-name/wi/wine-discord-ipc-bridge/package.nix
@@ -1,38 +1,36 @@
 {
-  lib,
-  stdenv,
-  fetchFromGitHub,
+  stdenvNoCC,
+  pkgsCross,
+  symlinkJoin,
 }:
+let
+  innerDrv =
+    if stdenvNoCC.hostPlatform.isDarwin then
+      pkgsCross.mingwW64.callPackage ./unwrapped.nix {
+        darwinSupport = true;
+      }
+    else
+      pkgsCross.mingw32.callPackage ./unwrapped.nix { };
+in
+symlinkJoin {
+  name = "wine-discord-ipc-bridge-${innerDrv.version}";
 
-stdenv.mkDerivation (finalAttrs: {
-  pname = "wine-discord-ipc-bridge";
-  version = "0.0.3";
+  paths = [ innerDrv ];
 
-  src = fetchFromGitHub {
-    owner = "0e4ef622";
-    repo = "wine-discord-ipc-bridge";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-jzsbOKMakNQ6RNMlioX088fGzFBDxOP45Atlsfm2RKg=";
-  };
-
-  postPatch = ''
-    patchShebangs winediscordipcbridge-steam.sh
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp winediscordipcbridge.exe $out/bin
-    cp winediscordipcbridge-steam.sh $out/bin
-    runHook postInstall
-  '';
+  passthru.wine-discord-ipc-bridge = innerDrv;
 
   meta = {
-    description = "Enable games running under wine to use Discord Rich Presence";
-    homepage = "https://github.com/0e4ef622/wine-discord-ipc-bridge";
-    license = lib.licenses.mit;
-    maintainers = [ lib.maintainers.uku3lig ];
-    mainProgram = "winediscordipcbridge";
-    platforms = [ "i686-windows" ];
+    inherit (innerDrv.meta)
+      description
+      homepage
+      license
+      maintainers
+      mainProgram
+      ;
+    platforms = [
+      "i386-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+    ];
   };
-})
+}
