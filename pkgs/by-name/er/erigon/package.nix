3989a9153e54556f2c35da0b82e5848acf9da4fb
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-28 08:43:44 +0000

    erigon: 2.61.3 -> 3.0.0

--- pkgs/by-name/er/erigon/package.nix
+++ pkgs/by-name/er/erigon/package.nix
@@ -7,7 +7,7 @@
 
 let
   pname = "erigon";
-  version = "2.61.3";
+  version = "3.0.0";
 in
 buildGoModule {
   inherit pname version;
@@ -16,11 +16,11 @@ buildGoModule {
     owner = "ledgerwatch";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-VGLuPaGYx/DQc3Oc9wAbELXAtkuxr8cbePVBExlZikk=";
+    hash = "sha256-63wh9D5D9qzABEvKCBxBRHYuGBnlX+hrme56STkyoQU=";
     fetchSubmodules = true;
   };
 
-  vendorHash = "sha256-1LB2T0o9LjFdpl86NPMKx1lFLrQZefAGldcSQyL6O7M=";
+  vendorHash = "sha256-Mvat9+mbgpept9g8pYNf4a/bAGZGBM0MAM417DDIT9w=";
   proxyVendor = true;
 
   # Build errors in mdbx when format hardening is enabled:
