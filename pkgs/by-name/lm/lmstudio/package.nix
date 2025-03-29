ac93e7a034306179c1af767a050f89786a5a2672
DeftDawg <deftdawg@gmail.com>
2025-03-29 01:05:37 -0400

    lmstudio: 0.3.14-3 -> 0.3.14-5

--- pkgs/by-name/lm/lmstudio/package.nix
+++ pkgs/by-name/lm/lmstudio/package.nix
@@ -2,13 +2,18 @@
   lib,
   stdenv,
   callPackage,
-  version ? "0.3.14",
-  rev ? "3",
   ...
 }@args:
 let
   pname = "lmstudio";
-  packageVersion = "${version}-${rev}"; # Combine version and rev
+
+  version_aarch64-darwin = "0.3.14-5";
+  hash_aarch64-darwin = "sha256-8OTfjEZ27ubRFvRQ84em2Gz3mS9w3oev41Qg6MMNjNU=";
+  version_x86_64-linux = "0.3.14-5";
+  hash_x86_64-linux = "sha256-WrO95ez81/A0U1Tt1Oi2PyUp6nvsmQMzK0VUVH1TYbg=";
+
+  passthru.updateScript = ./update.sh;
+
   meta = {
     description = "LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models (LLMs)";
     homepage = "https://lmstudio.ai/";
@@ -26,18 +31,18 @@ in
 if stdenv.hostPlatform.isDarwin then
   callPackage ./darwin.nix {
     inherit pname meta;
-    version = packageVersion;
+    version = version_aarch64-darwin;
     url =
       args.url
-        or "https://installers.lmstudio.ai/darwin/arm64/${version}-${rev}/LM-Studio-${version}-${rev}-arm64.dmg";
-    hash = args.hash or "sha256-doAhCbWFwDWlBQ+4YfJz6p7I4NZJxIOtdLYTr3mOGds=";
+        or "https://installers.lmstudio.ai/darwin/arm64/${version_aarch64-darwin}/LM-Studio-${version_aarch64-darwin}-arm64.dmg";
+    hash = args.hash or hash_aarch64-darwin;
   }
 else
   callPackage ./linux.nix {
     inherit pname meta;
-    version = packageVersion;
+    version = version_x86_64-linux;
     url =
       args.url
-        or "https://installers.lmstudio.ai/linux/x64/${version}-${rev}/LM-Studio-${version}-${rev}-x64.AppImage";
-    hash = args.hash or "sha256-IIJMk0cfLQdrx0nTSbpsbqOvD+f/qrH+rGdYN4mygaw=";
+        or "https://installers.lmstudio.ai/linux/x64/${version_x86_64-linux}/LM-Studio-${version_x86_64-linux}-x64.AppImage";
+    hash = args.hash or hash_x86_64-linux;
   }
