834edfe2cdd9f3cf3dd7eada498e193108819869
Petr Portnov <mrjarviscraft+nix@gmail.com>
2025-03-30 00:53:12 +0300

    gradle-dependency-tree-diff: init at 1.2.1

---
+++ pkgs/by-name/gr/gradle-dependency-tree-diff/package.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  gradle,
+  makeWrapper,
+  jre,
+  zulu11,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "gradle-dependency-tree-diff";
+  version = "1.2.1";
+  src = fetchFromGitHub {
+    owner = "JakeWharton";
+    repo = "dependency-tree-diff";
+    tag = "${finalAttrs.version}";
+    hash = "sha256-7ObmZygzSp7aAnqsJuMcPk+I3z993kjHCJMug3JkONg=";
+  };
+
+  nativeBuildInputs = [
+    gradle
+    makeWrapper
+  ];
+
+  mitmCache = gradle.fetchDeps {
+    inherit (finalAttrs) pname;
+    data = ./deps.json;
+  };
+  __darwinAllowLocalNetworking = true;
+
+  # There is a requirement on the specific Java toolchain.
+  gradleFlags = [ "-Dorg.gradle.java.home=${zulu11}" ];
+
+  gradleBuildTask = "build";
+  doCheck = true;
+
+  installPhase = ''
+    mkdir -p $out/{bin,share/dependency-tree-diff}
+    cp build/dependency-tree-diff.jar $out/share/dependency-tree-diff
+    makeWrapper ${lib.getExe jre} $out/bin/dependency-tree-diff \
+      --add-flags "-jar $out/share/dependency-tree-diff/dependency-tree-diff.jar"
+  '';
+
+  meta = {
+    description = "An intelligent diff tool for the output of Gradle's dependencies task";
+    mainProgram = "dependency-tree-diff";
+    homepage = "https://github.com/JakeWharton/dependency-tree-diff";
+    changelog = "https://github.com/JakeWharton/dependency-tree-diff/releases/tag/${finalAttrs.version}";
+    sourceProvenance = with lib.sourceTypes; [
+      fromSource
+      binaryBytecode
+    ];
+    license = lib.licenses.asl20;
+    maintainers = [ lib.maintainers.progrm_jarvis ];
+    platforms = lib.platforms.all;
+  };
+})
