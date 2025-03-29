d920a4a5351270a65b48c3f65f306c653f95ea2b
Sergei Trofimovich <slyich@gmail.com>
2025-03-29 21:36:36 +0000

    libnotify: 0.8.4 -> 0.8.6
    Changes: https://gitlab.gnome.org/GNOME/libnotify/-/compare/0.8.4...0.8.6

--- pkgs/by-name/li/libnotify/package.nix
+++ pkgs/by-name/li/libnotify/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchurl,
-  fetchpatch,
   meson,
   ninja,
   pkg-config,
@@ -20,7 +19,7 @@
 
 stdenv.mkDerivation rec {
   pname = "libnotify";
-  version = "0.8.4";
+  version = "0.8.6";
 
   outputs = [
     "out"
@@ -30,19 +29,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    hash = "sha256-j6BNTr3BVbCiOd+IvZ8J6PJznVcHoTkLQnq0mF+D0lo=";
+    hash = "sha256-xVQKrvtg4dY7HFh8BfIoTr5y7OfQwOXkp3jP1YRLa1g=";
   };
-
-  patches = [
-    # Revert to avoid manpages breakage:
-    #   https://gitlab.gnome.org/GNOME/libnotify/-/merge_requests/37#note_2366448
-    (fetchpatch {
-      revert = true;
-      name = "unbreak-man.path";
-      url = "https://gitlab.gnome.org/GNOME/libnotify/-/commit/2f99025b7ad54f29dc5236aa7dfcfa97d1c8efde.patch";
-      hash = "sha256-/qbBs+bJjzCR2I7swy0RTsTaXap+gkYzvfchobeOzPQ=";
-    })
-  ];
 
   mesonFlags = [
     # disable tests as we don't need to depend on GTK (2/3)
