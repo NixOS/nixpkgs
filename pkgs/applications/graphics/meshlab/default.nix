996da135c3f114d2afed09a7865017adef5f3256
Zexin Yuan <git@yzx9.xyz>
2025-03-29 13:07:20 +0800

    meshlab: add darwin support

--- pkgs/applications/graphics/meshlab/default.nix
+++ pkgs/applications/graphics/meshlab/default.nix
@@ -1,7 +1,8 @@
 {
-  mkDerivation,
-  lib,
+  llvmPackages_18,
+  stdenv,
   fetchFromGitHub,
+  lib,
   libGLU,
   qtbase,
   qtscript,
@@ -26,86 +27,127 @@
   corto,
   openctm,
   structuresynth,
+  wrapQtAppsHook,
 }:
 
 let
+  llvmPackages = llvmPackages_18;
+  stdenv' = if stdenv.hostPlatform.isDarwin then llvmPackages.stdenv else stdenv;
+
   tinygltf-src = fetchFromGitHub {
     owner = "syoyo";
     repo = "tinygltf";
-    rev = "v2.6.3";
+    tag = "v2.6.3";
     hash = "sha256-IyezvHzgLRyc3z8HdNsQMqDEhP+Ytw0stFNak3C8lTo=";
   };
+
+  downloads = [
+    "DLL_EMBREE"
+    "SOURCE_BOOST"
+    "SOURCE_CGAL"
+    "SOURCE_EMBREE"
+    "SOURCE_LEVMAR"
+    "SOURCE_LIB3DS"
+    "SOURCE_LIBE57"
+    "SOURCE_LIBIGL"
+    "SOURCE_MUPARSER"
+    "SOURCE_NEXUS"
+    "SOURCE_OPENCTM"
+    "SOURCE_QHULL"
+    "SOURCE_STRUCTURE_SYNTH"
+    "SOURCE_TINYGLTF"
+    "SOURCE_U3D"
+    "SOURCE_XERCE"
+  ];
+  cmakeFlagsDisallowDownload = lib.map (x: "-DMESHLAB_ALLOW_DOWNLOAD_${x}=OFF") downloads;
 in
-mkDerivation rec {
+stdenv'.mkDerivation (finalAttrs: {
   pname = "meshlab";
   version = "2023.12";
 
   src = fetchFromGitHub {
     owner = "cnr-isti-vclab";
     repo = "meshlab";
-    rev = "MeshLab-${version}";
-    sha256 = "sha256-AdUAWS741RQclYaSE3Tz1/I0YSinNAnfSaqef+Tib8Y=";
+    tag = "MeshLab-${finalAttrs.version}";
+    hash = "sha256-AdUAWS741RQclYaSE3Tz1/I0YSinNAnfSaqef+Tib8Y=";
   };
 
-  buildInputs = [
-    libGLU
-    qtbase
-    qtscript
-    qtxmlpatterns
-    lib3ds
-    bzip2
-    muparser
-    eigen
-    glew
-    gmp
-    levmar
-    qhull
-    cgal
-    boost
-    mpfr
-    xercesc
-    tbb
-    embree
-    vcg
-    libigl
-    corto
-    openctm
-    structuresynth
-  ];
+  buildInputs =
+    [
+      libGLU
+      qtbase
+      qtscript
+      qtxmlpatterns
+      lib3ds
+      bzip2
+      muparser
+      eigen
+      glew
+      gmp
+      qhull
+      cgal
+      boost
+      mpfr
+      xercesc
+      tbb
+      embree
+      vcg
+      libigl
+      corto
+      openctm
+      structuresynth
+    ]
+    ++ lib.optional (!stdenv'.cc.isClang) levmar # FIXME: clang throw error implicit-function-declaration
+    ++ lib.optional stdenv'.cc.isClang llvmPackages.openmp;
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake ] ++ lib.optional stdenv'.hostPlatform.isDarwin wrapQtAppsHook;
 
-  preConfigure = ''
-    substituteAll ${./meshlab.desktop} resources/linux/meshlab.desktop
-    substituteInPlace src/external/tinygltf.cmake \
-      --replace-fail '$'{MESHLAB_EXTERNAL_DOWNLOAD_DIR}/tinygltf-2.6.3 ${tinygltf-src}
-    substituteInPlace src/external/libigl.cmake \
-      --replace-fail '$'{MESHLAB_EXTERNAL_DOWNLOAD_DIR}/libigl-2.4.0 ${libigl}
-    substituteInPlace src/external/nexus.cmake \
-      --replace-fail '$'{NEXUS_DIR}/src/corto ${corto.src}
-    substituteInPlace src/external/levmar.cmake \
-      --replace-fail '$'{LEVMAR_LINK} ${levmar.src} \
-      --replace-warn "MD5 ''${LEVMAR_MD5}" ""
-    substituteInPlace src/external/ssynth.cmake \
-      --replace-fail '$'{SSYNTH_LINK} ${structuresynth.src} \
-      --replace-warn "MD5 ''${SSYNTH_MD5}" ""
-    substituteInPlace src/common_gui/CMakeLists.txt \
-      --replace-warn "MESHLAB_LIB_INSTALL_DIR" "CMAKE_INSTALL_LIBDIR"
-  '';
+  patchPhase =
+    ''
+      substituteInPlace src/external/tinygltf.cmake \
+        --replace-fail '$'{MESHLAB_EXTERNAL_DOWNLOAD_DIR}/tinygltf-2.6.3 ${tinygltf-src}
+      substituteInPlace src/external/libigl.cmake \
+        --replace-fail '$'{MESHLAB_EXTERNAL_DOWNLOAD_DIR}/libigl-2.4.0 ${libigl}
+      substituteInPlace src/external/nexus.cmake \
+        --replace-fail '$'{NEXUS_DIR}/src/corto ${corto.src}
+      substituteInPlace src/external/levmar.cmake \
+        --replace-fail '$'{LEVMAR_LINK} ${levmar.src} \
+        --replace-warn "MD5 ''${LEVMAR_MD5}" ""
+      substituteInPlace src/external/ssynth.cmake \
+        --replace-fail '$'{SSYNTH_LINK} ${structuresynth.src} \
+        --replace-warn "MD5 ''${SSYNTH_MD5}" ""
+      substituteInPlace src/common_gui/CMakeLists.txt \
+        --replace-warn "MESHLAB_LIB_INSTALL_DIR" "CMAKE_INSTALL_LIBDIR"
+    ''
+    + lib.optionalString stdenv'.hostPlatform.isLinux ''
+      substituteAll ${./meshlab.desktop} resources/linux/meshlab.desktop
+    ''
+    + lib.optionalString stdenv'.hostPlatform.isDarwin ''
+      substituteInPlace src/meshlab/CMakeLists.txt \
+        --replace-fail "set_additional_settings_info_plist(" "# set_additional_settings_info_plist(" \
+        --replace-fail "	TARGET meshlab" "#	TARGET meshlab" \
+        --replace-fail "	FILE \''${MESHLAB_BUILD_DISTRIB_DIR}/meshlab.app/Contents/Info.plist)" \
+                       "#	FILE \''${MESHLAB_BUILD_DISTRIB_DIR}/meshlab.app/Contents/Info.plist)"
+    '';
 
   cmakeFlags = [
     "-DVCGDIR=${vcg.src}"
-  ];
+  ] ++ cmakeFlagsDisallowDownload;
 
-  postFixup = ''
+  postInstall = lib.optionalString stdenv'.hostPlatform.isDarwin ''
+    mkdir -p $out/{Applications,bin,lib}
+    mv $out/meshlab.app $out/Applications/
+    ln $out/Applications/meshlab.app/Contents/Frameworks/libmeshlab-common.dylib $out/lib/
+    makeWrapper $out/{Applications/meshlab.app/Contents/MacOS,bin}/meshlab
+  '';
+
+  postFixup = lib.optionalString stdenv'.hostPlatform.isLinux ''
     patchelf --add-needed $out/lib/meshlab/libmeshlab-common.so $out/bin/.meshlab-wrapped
   '';
 
   # display a black screen on wayland, so force XWayland for now.
   # Might be fixed when upstream will be ready for Qt6.
-  qtWrapperArgs = [
-    "--set QT_QPA_PLATFORM xcb"
-  ];
+  qtWrapperArgs = lib.optional stdenv'.hostPlatform.isLinux "--set QT_QPA_PLATFORM xcb";
 
   meta = {
     description = "System for processing and editing 3D triangular meshes";
@@ -113,6 +155,6 @@ mkDerivation rec {
     homepage = "https://www.meshlab.net/";
     license = lib.licenses.gpl3Only;
     maintainers = [ ];
-    platforms = with lib.platforms; linux;
+    platforms = with lib.platforms; linux ++ darwin;
   };
-}
+})
