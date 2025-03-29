550270ca1111a0a97c8671df701ef755629c5750
Morteza NourelahiAlamdari <m@0t1.me>
2025-03-29 18:04:53 +0100

    Fix wait4x repository reference

--- pkgs/by-name/wa/wait4x/package.nix
+++ pkgs/by-name/wa/wait4x/package.nix
@@ -11,7 +11,7 @@ buildGoModule {
   inherit pname version;
 
   src = fetchFromGitHub {
-    owner = "atkrad";
+    owner = "wait4x";
     repo = pname;
     rev = "v${version}";
     hash = "sha256-UVs8tCOgPU/IwM3Z3/ehFnAbHTSOHGAO9VpcB/NItGM=";
@@ -24,7 +24,7 @@ buildGoModule {
 
   meta = with lib; {
     description = "Wait4X allows you to wait for a port or a service to enter the requested state";
-    homepage = "https://github.com/atkrad/wait4x";
+    homepage = "https://github.com/wait4x/wait4x";
     license = licenses.asl20;
     maintainers = with maintainers; [ jfvillablanca ];
     mainProgram = "wait4x";
