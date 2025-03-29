58caa65f527e065b7af6a2c0d99a96f4be1d35ad
Seth Flynn <getchoo@tuta.io>
2025-03-29 00:09:46 -0400

    komikku: modernize

--- pkgs/by-name/ko/komikku/package.nix
+++ pkgs/by-name/ko/komikku/package.nix
@@ -54,7 +54,7 @@ python3.pkgs.buildPythonApplication rec {
     webkitgtk_6_0
   ];
 
-  propagatedBuildInputs = with python3.pkgs; [
+  dependencies = with python3.pkgs; [
     beautifulsoup4
     brotli
     colorthief
@@ -92,12 +92,7 @@ python3.pkgs.buildPythonApplication rec {
 
   # Prevent double wrapping.
   dontWrapGApps = true;
-
-  preFixup = ''
-    makeWrapperArgs+=(
-      "''${gappsWrapperArgs[@]}"
-    )
-  '';
+  makeWrapperArgs = [ "\${gappsWrapperArgs[@]}" ];
 
   passthru = {
     updateScript = nix-update-script { };
