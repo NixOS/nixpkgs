18e285b5029f2ee629d50a71644cd97831636571
ghpzin <ghpzin@gmail.com>
2025-03-30 14:48:54 +0300

    untie: fix build with gcc14
    - add patch that adds `#define _GNU_SOURCE` to `untie.c`, as glibc
    wrapper over clone syscall is not declared otherwise.
    Fixes `-Wimplicit-function-declaration` error:
    ```
    untie.c:411:15: error: implicit declaration of function 'clone';
    did you mean 'close'? []
      411 |         ret = clone(child_process, &stack[STACK_SIZE - 1], flags, &args_info);
          |               ^~~~~
          |               close
    make: *** [<builtin>: untie.o] Error 1
    ```

--- pkgs/by-name/un/untie/package.nix
+++ pkgs/by-name/un/untie/package.nix
@@ -12,6 +12,11 @@ stdenv.mkDerivation rec {
     sha256 = "1334ngvbi4arcch462mzi5vxvxck4sy1nf0m58116d9xmx83ak0m";
   };
 
+  patches = [
+    # fix build with gcc14
+    ./add-define-gnu-source.patch
+  ];
+
   makeFlags = [ "PREFIX=$(out)" ];
 
   meta = with lib; {
