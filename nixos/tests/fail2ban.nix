a51dd9aa2c0a1f07a9af4d89c6416b24de50b6e2
Philipp Bartsch <phil@grmr.de>
2025-03-30 20:08:47 +0200

    nixosTests.fail2ban: migrate to runTest

--- nixos/tests/fail2ban.nix
+++ nixos/tests/fail2ban.nix
@@ -1,21 +1,19 @@
-import ./make-test-python.nix (
-  { pkgs, ... }:
-  {
-    name = "fail2ban";
+{ pkgs, ... }:
+{
+  name = "fail2ban";
 
-    nodes.machine = _: {
-      services.fail2ban = {
-        enable = true;
-        bantime-increment.enable = true;
-      };
-
-      services.openssh.enable = true;
+  nodes.machine = _: {
+    services.fail2ban = {
+      enable = true;
+      bantime-increment.enable = true;
     };
 
-    testScript = ''
-      machine.wait_for_unit("multi-user.target")
+    services.openssh.enable = true;
+  };
 
-      machine.wait_for_unit("fail2ban")
-    '';
-  }
-)
+  testScript = ''
+    machine.wait_for_unit("multi-user.target")
+
+    machine.wait_for_unit("fail2ban")
+  '';
+}
