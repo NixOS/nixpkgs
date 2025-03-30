19237dde43fe18a5fcd93a555e51ea1fc000b9e7
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-30 18:03:15 +0000

    qemu-user: 9.2.2 -> 9.2.3

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -70,11 +70,11 @@ stdenv.mkDerivation (finalAttrs: {
     + lib.optionalString nixosTestRunner "-for-vm-tests"
     + lib.optionalString toolsOnly "-utils"
     + lib.optionalString userOnly "-user";
-  version = "9.2.2";
+  version = "9.2.3";
 
   src = fetchurl {
     url = "https://download.qemu.org/qemu-${finalAttrs.version}.tar.xz";
-    hash = "sha256-dS6u63cpI6c9U2sjHgW8wJybH1FpCkGtmXPZAOTsn78=";
+    hash = "sha256-uu1JQnDDYb9pgWrMhFEuPv7XHHoj92aRZCuAvD3naT4=";
   };
 
   depsBuildBuild = [ buildPackages.stdenv.cc ]
