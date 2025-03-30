a180d093b0378ddd869683f94fcb0b166e5e69e3
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-30 01:47:10 +0000

    xrsh: 0-unstable-2025-02-28 -> 0-unstable-2025-03-28

--- pkgs/by-name/xr/xrsh/package.nix
+++ pkgs/by-name/xr/xrsh/package.nix
@@ -10,15 +10,15 @@
 
 stdenv.mkDerivation {
   pname = "xrsh";
-  version = "0-unstable-2025-02-28";
+  version = "0-unstable-2025-03-28";
 
   src = fetchFromGitea {
     fetchSubmodules = true;
     domain = "codeberg.org";
     owner = "xrsh";
     repo = "xrsh";
-    rev = "5d6e9f4615e57f01ba0859000d90f4f162a06653";
-    hash = "sha256-9I6c1GkO7XQZ242ZBCxPiPxkZaYYi/UDLoUvS6nE+M0=";
+    rev = "6eb4feb04ddd4aa96319340921d12b9fdd1bbe59";
+    hash = "sha256-EPrp9tsBDo7A4Si0wV8cup3zOBeMG3em1ePSZwjXUcI=";
   };
 
   dontBuild = true;
