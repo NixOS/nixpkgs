45dd437758b26f20a9ebd5f54470fb5f5176dc1b
ghpzin <ghpzin@gmail.com>
2025-03-30 20:46:31 +0300

    pies: 1.3 -> 1.8
    - update to latest release, fixes `implicit-function-declaration` errors
    with gcc14:
    ```
    pp.c: In function 'pp_add_option':
    pp.c:29:7: error: implicit declaration of function 'exit' []
       29 |       exit (EX_CONFIG);
          |       ^~~~
    ```

--- pkgs/by-name/pi/pies/package.nix
+++ pkgs/by-name/pi/pies/package.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "pies";
-  version = "1.3";
+  version = "1.8";
 
   src = fetchurl {
     url = "mirror://gnu/pies/${pname}-${version}.tar.bz2";
-    sha256 = "12r7rjjyibjdj08dvwbp0iflfpzl4s0zhn6cr6zj3hwf9gbzgl1g";
+    hash = "sha256-ZSi00WmC6il4+aSohqFKrKjtp6xFXYE7IIRGVwFmHWw=";
   };
 
   buildInputs = [ libxcrypt ];
