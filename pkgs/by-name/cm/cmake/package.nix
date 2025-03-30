496308923c315d0693652d358145c014feb94dc5
Alexander Borsuk <me@alex.bio>
2025-03-30 12:47:15 +0200

    cmake: 3.31.6 -> 4.0.0
    - Update cmake to the latest 4.0.0 release:
      https://cmake.org/cmake/help/latest/release/4.0.html
    - Fix previous patches for a newer source code

--- pkgs/by-name/cm/cmake/package.nix
+++ pkgs/by-name/cm/cmake/package.nix
@@ -48,11 +48,11 @@ stdenv.mkDerivation (finalAttrs: {
     + lib.optionalString isMinimalBuild "-minimal"
     + lib.optionalString cursesUI "-cursesUI"
     + lib.optionalString qt5UI "-qt5UI";
-  version = "3.31.6";
+  version = "4.0.0";
 
   src = fetchurl {
     url = "https://cmake.org/files/v${lib.versions.majorMinor finalAttrs.version}/cmake-${finalAttrs.version}.tar.gz";
-    hash = "sha256-ZTQn8PUBR1Cq//InJ/sqpgxscyypGAjPt4ziLd2eVfA=";
+    hash = "sha256-3cVK1juH4VPPUL5FCmWA8bF7SIHeiUHalj/1aZGkCDs=";
   };
 
   patches = [
