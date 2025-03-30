040b7934a1b8e37b297c73dde41db671681dbeb5
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-30 22:08:13 +0200

    linux_xanmod: 6.12.20 -> 6.12.21
    (cherry picked from commit d9196be0b62016caff762f47cbdc8fe30c04021a)

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -14,8 +14,8 @@ let
   # kernel config in the xanmod version commit
   variants = {
     lts = {
-      version = "6.12.20";
-      hash = "sha256-Fvm17GIaGnNfWShh9fX4XK2ovAS6XMx7cHb1u9bET8E=";
+      version = "6.12.21";
+      hash = "sha256-Zb/n+hLho94+6u5BHAmRYfit/kv1xlh/Tp39kI3kfjA=";
     };
     main = {
       version = "6.13.8";
