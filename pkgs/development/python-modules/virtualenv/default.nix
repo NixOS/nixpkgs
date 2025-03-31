0f3704aaa6823de19326125af83136c1f7c60f56
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-03-31 04:26:29 +0200

    python313Packages.virtualenv: 20.29.2 -> 20.29.3
    https://github.com/pypa/virtualenv/blob/20.29.3/docs/changelog.rst

--- pkgs/development/python-modules/virtualenv/default.nix
+++ pkgs/development/python-modules/virtualenv/default.nix
@@ -21,14 +21,14 @@
 
 buildPythonPackage rec {
   pname = "virtualenv";
-  version = "20.29.2";
+  version = "20.29.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-/aq+v20DtbqDrgoCz+lvSKcW9PrlVkYdGAglhm91tyg=";
+    hash = "sha256-leOUA/zzlArEW8cXWX26FhELdFBhMYRdm2h9XnPZR6w=";
   };
 
   nativeBuildInputs = [
