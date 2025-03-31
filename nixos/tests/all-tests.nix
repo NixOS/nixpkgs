79c1d5b7c73f478e26301ccf01f49bd13a8c092b
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2025-03-31 22:37:56 +0200

    nixosTests.bazarr: migrate to runTest
    Part of #386873

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -203,7 +203,7 @@ in {
   };
   ayatana-indicators = runTest ./ayatana-indicators.nix;
   babeld = runTest ./babeld.nix;
-  bazarr = handleTest ./bazarr.nix {};
+  bazarr = runTest ./bazarr.nix;
   bcachefs = handleTestOn ["x86_64-linux" "aarch64-linux"] ./bcachefs.nix {};
   beanstalkd = handleTest ./beanstalkd.nix {};
   bees = handleTest ./bees.nix {};
