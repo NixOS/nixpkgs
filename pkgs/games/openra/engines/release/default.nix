de99fbad128233e6ea2a728a2e4e57172fad9cfa
mdarocha <git@mdarocha.pl>
2025-03-31 07:39:09 +0200

    openra: add updateScript A generic updateScript that can support any openra release paths
    (cherry picked from commit 0233a69802c4ce48f781fb1fe4596bef12118ddf)

--- pkgs/games/openra/engines/release/default.nix
+++ pkgs/games/openra/engines/release/default.nix
@@ -3,6 +3,6 @@
 buildOpenRAEngine {
   build = "release";
   version = "20231010";
-  sha256 = "sha256-klJkRoDLTcU7j2iwo4yT9CaKy8QXWDkYw7ApkopSDNM=";
+  hash = "sha256-klJkRoDLTcU7j2iwo4yT9CaKy8QXWDkYw7ApkopSDNM=";
   deps = ./deps.json;
 }
