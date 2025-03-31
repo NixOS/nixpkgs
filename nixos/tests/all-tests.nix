1d245ae938e889c7bddf09c73bd5788b68414ba5
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2025-03-31 22:43:40 +0200

    nixosTests.doas: migrate to runTest
    Part of #386873

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -330,7 +330,7 @@ in {
   discourse = handleTest ./discourse.nix {};
   dnscrypt-proxy2 = handleTestOn ["x86_64-linux"] ./dnscrypt-proxy2.nix {};
   dnsdist = import ./dnsdist.nix { inherit pkgs runTest; };
-  doas = handleTest ./doas.nix {};
+  doas = runTest ./doas.nix;
   docker = handleTestOn ["aarch64-linux" "x86_64-linux"] ./docker.nix {};
   docker-rootless = handleTestOn ["aarch64-linux" "x86_64-linux"] ./docker-rootless.nix {};
   docker-registry = handleTest ./docker-registry.nix {};
