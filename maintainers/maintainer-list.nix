7bf751da6e0b93c84c494b00bf8fa7f88c406cb7
PucklaJ <kassandra@pucher.space>
2025-03-31 15:26:42 +0000

    maintainers: add puckla

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20950,6 +20950,12 @@
     githubId = 488734;
     name = "Puck Meerburg";
   };
+  puckla = {
+    email = "kassandra@pucher.space";
+    github = "PucklaJ";
+    githubId = 25499721;
+    name = "Kassandra Pucher";
+  };
   PuercoPop = {
     email = "pirata@gmail.com";
     github = "PuercoPop";
