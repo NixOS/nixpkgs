fe7a2ce5aecb1d9ab504ae71acb6b98980f99bca
Mikael Voss <mvs@nyantec.com>
2025-03-31 20:27:40 +0200

    portaudio: check pkg-config modules

--- pkgs/by-name/po/portaudio/package.nix
+++ pkgs/by-name/po/portaudio/package.nix
@@ -7,6 +7,7 @@
   libjack2,
   pkg-config,
   which,
+  testers,
   nix-update-script,
 }:
 
@@ -64,10 +65,13 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.mit;
     maintainers = [ ];
     platforms = lib.platforms.unix;
+
+    pkgConfigModules = [ "portaudio-2.0" ];
   };
 
   passthru = {
     api_version = 19;
+    tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
     updateScript = nix-update-script {
       extraArgs = [ "--version=branch" ];
     };
