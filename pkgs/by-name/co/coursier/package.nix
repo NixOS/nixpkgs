2d3c50daa9768056763ac9e235e1a1eba710c052
R. Ryantm <ryantm-bot@ryantm.com>
2025-03-31 04:35:47 +0000

    coursier: 2.1.24 -> 2.1.25-M4

--- pkgs/by-name/co/coursier/package.nix
+++ pkgs/by-name/co/coursier/package.nix
@@ -20,11 +20,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "coursier";
-  version = "2.1.24";
+  version = "2.1.25-M4";
 
   src = fetchurl {
     url = "https://github.com/coursier/coursier/releases/download/v${version}/coursier";
-    hash = "sha256-eql18SRpcm1ruHhSEHr+C41vPIKxKknvQ8xmR8TgV8o=";
+    hash = "sha256-osDv1YhiPYP3Xhff6lwRBJxiaCV980/yl1Sopi2ZYjU=";
   };
 
   dontUnpack = true;
