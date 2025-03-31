479b2c1a95090dc77b38e26b21920b20757872e8
Gaetan Lepage <gaetan@glepage.com>
2025-03-31 14:57:02 +0200

    python312Packages.ray-cpp: init at 2.44.0

--- pkgs/development/python-modules/ray/prefetch.sh
+++ pkgs/development/python-modules/ray/prefetch.sh
@@ -6,23 +6,27 @@ outfile="ray-hashes.nix"
 echo "" >$outfile
 
 prefetch() {
-    package_attr="python${1}Packages.ray"
-    echo "Fetching hash for $package_attr on $2"
+    package_attr="python${2}Packages.${1}"
+    echo "Fetching hash for $package_attr on $3"
 
-    expr="(import <nixpkgs> { system = \"$2\"; }).$package_attr.src.url"
+    expr="(import <nixpkgs> { system = \"$3\"; }).$package_attr.src.url"
     url=$(NIX_PATH=.. nix-instantiate --eval -E "$expr" | jq -r)
 
     sha256=$(nix-prefetch-url "$url")
     hash=$(nix hash convert --to sri --hash-algo sha256 "$sha256")
 
-    echo -e "  cp${1} = \"${hash}\";" >>$outfile
+    echo -e "    cp${2} = \"${hash}\";" >>$outfile
     echo
 }
 
-for system in "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"; do
-    echo "${system} = {" >>$outfile
-    for python_version in "39" "310" "311" "312"; do
-        prefetch "$python_version" "$system"
+for package_name in "ray" "ray-cpp"; do
+    echo "${package_name} = {" >>$outfile
+    for system in "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"; do
+        echo "  ${system} = {" >>$outfile
+        for python_version in "39" "310" "311" "312"; do
+            prefetch "$package_name" "$python_version" "$system"
+        done
+        echo "  };" >>$outfile
     done
     echo "};" >>$outfile
 done
