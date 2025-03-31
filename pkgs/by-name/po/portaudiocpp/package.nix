dd65e22cac947f4617e8390ba6600f77b3c83513
Mikael Voss <mvs@nyantec.com>
2025-03-31 20:56:14 +0200

    portaudiocpp: split off from portaudio

---
+++ pkgs/by-name/po/portaudiocpp/package.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  stdenv,
+  portaudio,
+  cmake,
+  pkg-config,
+  tre,
+  testers,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "portaudiocpp";
+  inherit (portaudio) version src;
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  sourceRoot = "${finalAttrs.src.name}/bindings/cpp";
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+
+  buildInputs = [
+    portaudio
+    tre
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "BUILD_SHARED_LIBS" (!stdenv.hostPlatform.isStatic))
+  ];
+
+  postPatch = ''
+    # allow compilation of C sources
+    sed -E -i 's/^(project\(.* LANGUAGES CXX)\)$/\1 C)/' \
+      CMakeLists.txt
+
+    # correct installation paths
+    sed -E -i \
+      -e 's/^(prefix=).*/\1@CMAKE_INSTALL_PREFIX@/' \
+      -e 's/^(libdir=).*/\1@CMAKE_INSTALL_FULL_LIBDIR@/' \
+      -e 's/^(includedir=).*/\1@CMAKE_INSTALL_FULL_INCLUDEDIR@/' \
+      cmake/portaudiocpp.pc.in
+  '';
+
+  passthru = {
+    tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+  };
+
+  meta = {
+    description = "PortAudio C++ bindings";
+    inherit (portaudio.meta)
+      homepage
+      license
+      maintainers
+      platforms
+      ;
+
+    pkgConfigModules = [ "portaudiocpp" ];
+  };
+})
