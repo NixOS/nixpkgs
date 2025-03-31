b2d74669e96a24d9b424fab14c9ccb2098e1a836
maryjane <ndukauba.oluchi@gmail.com>
2025-03-31 19:30:14 +0100

    cnsprcy: 0.2.0 -> 0.3.0

--- pkgs/by-name/cn/cnsprcy/package.nix
+++ pkgs/by-name/cn/cnsprcy/package.nix
@@ -1,33 +1,51 @@
 {
-  lib,
+  lib, # Dependencies/arguments
   fetchFromSourcehut,
   rustPlatform,
   sqlite,
 }:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
+  # Main package definition
   pname = "cnsprcy";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchFromSourcehut {
+    # Fetching the sourcecode
     owner = "~xaos";
     repo = "cnsprcy";
-    rev = "v0.2.0";
-    hash = "sha256-f+DauSU4bT3EljY8/ig7jLnUgyDPEo2NSBQcPN0iKx0=";
+    rev = "v0.3.0";
+    hash = "sha256-OBNl2ZZi1jmJjJvzCwaIN9DSYjGaEo0g4o7/vVOF5aQ=";
   };
 
-  useFetchCargoVendor = true;
-  cargoHash = "sha256-lPTufjKOXMvPy+cP1UyVCTfRXkOmzZqDR6yraIgk+Dg=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  postPatch = ''
+    ln -s ${./Cargo.lock} Cargo.lock
+  '';
+
+  passthru.updateScript = lib.mkUpdateScript {
+    extraFetchers = [ fetchFromSourcehut ];
+  };
 
   RUSTC_BOOTSTRAP = true;
 
   buildInputs = [ sqlite ];
 
   meta = {
+    # Metatdata
     description = "End to end encrypted connections between trusted devices";
     homepage = "https://git.sr.ht/~xaos/cnsprcy";
     license = lib.licenses.gpl3;
-    maintainers = with lib.maintainers; [ supinie ];
+    maintainers =
+      with lib.maintainers;
+      [
+        supinie
+        oluchitheanalyst
+      ]
+      ++ lib.teams.ngi.members;
     mainProgram = "cnspr";
     platforms = lib.platforms.linux;
   };
