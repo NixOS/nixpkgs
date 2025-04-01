32bcc8f6f5d837f9e972944d5bd24bce432dde0a
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-01 07:14:18 +0200

    python3Packages.oslo-serialization: 5.6.0 -> 5.7.0
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/oslo-serialization/default.nix
+++ pkgs/development/python-modules/oslo-serialization/default.nix
@@ -12,13 +12,13 @@
 
 buildPythonPackage rec {
   pname = "oslo-serialization";
-  version = "5.6.0";
+  version = "5.7.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "oslo.serialization";
     inherit version;
-    hash = "sha256-TH1OEtqFPMTwS5EjBBE06Iboyf9Xq1fBli061Kh7f3w=";
+    hash = "sha256-vcTT3Ze4BjmzUF5G2apDn8lQKIFBd/MLkXQ+gTZsO+c=";
   };
 
   postPatch = ''
