e8fbc646d98f07a3635793daa4d0008fa6fe3c81
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-01 07:14:42 +0200

    python3Packages.roadlib: 0.29.1 -> 1.3.1
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/roadlib/default.nix
+++ pkgs/development/python-modules/roadlib/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "roadlib";
-  version = "0.29.1";
+  version = "1.3.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-147ej4qRH0pR5jeWd0+RjL8SgMu/eVRw9yFx1qJmy/Q=";
+    hash = "sha256-RI6gUqCaOeLesIwHtsASEkTtdRxLCAP6+C5Yj8mBb2o=";
   };
 
   build-system = [ setuptools ];
