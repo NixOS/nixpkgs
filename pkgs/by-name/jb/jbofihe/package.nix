74c5c8069d5d6503af52cc58d09d07b7cf5d2303
ghpzin <ghpzin@gmail.com>
2025-04-01 13:40:54 +0300

    jbofihe: fix build with gcc14
    - add patch fixing gcc14 errors:
    `implicit-int`:
    ```
    rpc_tab.c:1:8: error: type defaults to 'int' in declaration of
    'elide_trace_reduce' [-Wimplicit-int]
        1 | extern elide_trace_reduce(int, int);
          |        ^~~~~~~~~~~~~~~~~~
    rpc_tab.c:2:8: error: type defaults to 'int' in declaration of
    'elide_trace_shift' [-Wimplicit-int]
        2 | extern elide_trace_shift(int,int);
          |        ^~~~~~~~~~~~~~~~~
    rpc_tab.c:3:8: error: type defaults to 'int' in declaration of
    'report_trace_shift' [-Wimplicit-int]
        3 | extern report_trace_shift(int);
          |        ^~~~~~~~~~~~~~~~~~
    rpc_tab.c:4:8: error: type defaults to 'int' in declaration of
    'report_trace_reduce' [-Wimplicit-int]
        4 | extern report_trace_reduce(int, int);
          |        ^~~~~~~~~~~~~~~~~~~
    rpc_tab.c:5:8: error: type defaults to 'int' in declaration of
    'report_trace_error' [-Wimplicit-int]
        5 | extern report_trace_error(short *yyss, short *yyssp);
          |        ^~~~~~~~~~~~~~~~~~
    ```
    
    `implicit-function-declaration`:
    ```
    parse.tab.c: In function 'yyparse':
    parse.tab.c:1148:16: error: implicit declaration of function 'yylex'
    [-Wimplicit-function-declaration]
     1148 |       yychar = yylex ();
          |                ^~~~~
    
    parse.tab.c:1595:7: error: implicit declaration of function 'yyerror';
    did you mean 'yyerrok'? [-Wimplicit-function-declaration]
     1595 |       yyerror (YY_("syntax error"));
          |       ^~~~~~~
          |       yyerrok
    
    n2d.c: In function 'main':
    n2d.c:1529:12: error: implicit declaration of function 'yyparse'
    [-Wimplicit-function-declaration]
     1529 |   result = yyparse();
          |            ^~~~~~~
    ```

--- pkgs/by-name/jb/jbofihe/package.nix
+++ pkgs/by-name/jb/jbofihe/package.nix
@@ -18,6 +18,12 @@ stdenv.mkDerivation rec {
     sha256 = "1xx7x1256sjncyzx656jl6jl546vn8zz0siymqalz6v9yf341p98";
   };
 
+  patches = [
+    # fix build with gcc14:
+    # https://github.com/lojban/jbofihe/pull/19
+    ./fix-gcc14-errors.patch
+  ];
+
   nativeBuildInputs = [
     bison
     flex
