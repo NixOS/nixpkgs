d9db649607a90cd6eb8098e539d032e3a7b2f438
Hyphastorm <luke@hyphastorm.com>
2025-04-01 14:05:45 +0100

    Add wfs-tools package

---
+++ pkgs/tools/filesystems/wfs-tools/default.nix
@@ -0,0 +1,40 @@
+{ lib, stdenv, fetchurl, makeWrapper, fuse, unzip }:
+
+stdenv.mkDerivation rec {
+  pname = "wfs-tools";
+  version = "1.2.3";
+
+  src = fetchurl {
+    url =
+      "https://github.com/koolkdev/wfs-tools/releases/download/v${version}/wfs-tools-v${version}-linux-x86-64.zip";
+    hash = "sha256-lISRaVl39tr7ZD2yLyFaQ/2iyr1WJsWiXsqfOSe1lBU=";
+  };
+
+  nativeBuildInputs = [ makeWrapper unzip ];
+
+  buildInputs = [ fuse ];
+
+  unpackPhase = ''
+    mkdir -p $out/bin
+    unzip $src -d $out/bin
+    chmod +x $out/bin/wfs-*
+  '';
+
+  installPhase = ''
+    # Wrap binaries with proper library paths
+    for binary in $out/bin/wfs-*; do
+      mv "$binary" "$binary.unwrapped"
+      makeWrapper "$binary.unwrapped" "$binary" \
+        --set LD_LIBRARY_PATH "${lib.makeLibraryPath [ fuse ]}" \
+        --suffix PATH : "${lib.makeBinPath [ fuse ]}"
+    done
+  '';
+
+  meta = with lib; {
+    description = "Tools for working with Wii U WFS filesystem";
+    homepage = "https://github.com/koolkdev/wfs-tools";
+    license = licenses.mit;
+    maintainers = with maintainers; [ Hyphastorm ];
+    platforms = platforms.linux;
+  };
+}
---
+++ pkgs/tools/filesystems/wfs-tools/test.nix
@@ -0,0 +1,16 @@
+{ lib, stdenv, wfs-tools }:
+
+stdenv.mkDerivation {
+  name = "wfs-tools-test";
+
+  buildCommand = ''
+    # Test that all binaries are present and executable
+    ${wfs-tools}/bin/wfs-fuse --help
+    ${wfs-tools}/bin/wfs-extract --help
+    ${wfs-tools}/bin/wfs-info --help
+    ${wfs-tools}/bin/wfs-reencryptor --help
+    ${wfs-tools}/bin/wfs-file-injector --help
+
+    touch $out
+  '';
+}
