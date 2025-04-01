cb04e9ab555a52757c23f70eefd9f14173c908d1
Robert Schütz <nix@dotlambda.de>
2025-04-01 16:43:12 -0700

    python313Packages.pyflakes: modernize

--- pkgs/development/python-modules/pyflakes/default.nix
+++ pkgs/development/python-modules/pyflakes/default.nix
@@ -2,8 +2,6 @@
   lib,
   buildPythonPackage,
   isPyPy,
-  pythonAtLeast,
-  pythonOlder,
   fetchFromGitHub,
   setuptools,
   pytestCheckHook,
@@ -12,39 +10,31 @@
 buildPythonPackage rec {
   pname = "pyflakes";
   version = "3.3.2";
-
-  disabled = pythonOlder "3.8";
-
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "PyCQA";
     repo = "pyflakes";
-    rev = version;
+    tag = version;
     hash = "sha256-nNug9EZ0coI095/QJu/eK1Ozlt01INT+mLlYdqrJuzE=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
-  disabledTests =
-    lib.optionals (pythonAtLeast "3.13") [
-      # https://github.com/PyCQA/pyflakes/issues/812
-      "test_errors_syntax"
-    ]
-    ++ lib.optionals isPyPy [
-      # https://github.com/PyCQA/pyflakes/issues/779
-      "test_eofSyntaxError"
-      "test_misencodedFileUTF8"
-      "test_multilineSyntaxError"
-    ];
+  disabledTests = lib.optionals isPyPy [
+    # https://github.com/PyCQA/pyflakes/issues/779
+    "test_eofSyntaxError"
+    "test_misencodedFileUTF8"
+    "test_multilineSyntaxError"
+  ];
 
   pythonImportsCheck = [ "pyflakes" ];
 
   meta = with lib; {
     homepage = "https://github.com/PyCQA/pyflakes";
-    changelog = "https://github.com/PyCQA/pyflakes/blob/${src.rev}/NEWS.rst";
+    changelog = "https://github.com/PyCQA/pyflakes/blob/${src.tag}/NEWS.rst";
     description = "Simple program which checks Python source files for errors";
     mainProgram = "pyflakes";
     license = licenses.mit;
