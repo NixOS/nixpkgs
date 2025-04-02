befb6fd3a2c23bde3e260f476925cb585d39ee85
Brian Li <me@brian14708.dev>
2025-04-02 16:16:36 +0800

    cloud-init: fix python error

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2235,7 +2235,7 @@ with pkgs;
 
   clickgen = with python3Packages; toPythonApplication clickgen;
 
-  cloud-init = python3.pkgs.callPackage ../tools/virtualization/cloud-init { inherit systemd; };
+  cloud-init = callPackage ../tools/virtualization/cloud-init { inherit systemd; };
 
   cloudflared = callPackage ../applications/networking/cloudflared {
     # https://github.com/cloudflare/cloudflared/issues/1151#issuecomment-1888819250
