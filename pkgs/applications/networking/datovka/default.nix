c7d1fc9dcd99ad4395a064b602782e6ca68b5527
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-02 00:38:49 +0000

    datovka: 4.25.0 -> 4.26.0

--- pkgs/applications/networking/datovka/default.nix
+++ pkgs/applications/networking/datovka/default.nix
@@ -12,11 +12,11 @@
 
 mkDerivation rec {
   pname = "datovka";
-  version = "4.25.0";
+  version = "4.26.0";
 
   src = fetchurl {
     url = "https://gitlab.nic.cz/datovka/datovka/-/archive/v${version}/datovka-v${version}.tar.gz";
-    sha256 = "sha256-Snm9dDtHZQsx4T82tML77auBTb1lvITUOfL+kmhY4es=";
+    sha256 = "sha256-pEdjh/c4vhirj2R9bYDdi2FL7N9x67kTOyfXiJDzMKE=";
   };
 
   buildInputs = [ libdatovka qmake qtbase qtsvg libxml2 qtwebsockets ];
