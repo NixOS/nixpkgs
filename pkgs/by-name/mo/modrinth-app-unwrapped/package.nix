0031eb183e95f6d99a240a323923a0da9ab63c12
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-03 00:25:01 +0200

    formatting

--- pkgs/by-name/mo/modrinth-app-unwrapped/package.nix
+++ pkgs/by-name/mo/modrinth-app-unwrapped/package.nix
@@ -1,10 +1,25 @@
-{ lib, stdenv, cacert, cargo-tauri, desktop-file-utils, fetchFromGitHub
-, makeBinaryWrapper, nodejs, openssl, pkg-config, pnpm_9, rustPlatform, turbo
-, webkitgtk_4_1, }:
+{
+  lib,
+  stdenv,
+  cacert,
+  cargo-tauri,
+  desktop-file-utils,
+  fetchFromGitHub,
+  makeBinaryWrapper,
+  nodejs,
+  openssl,
+  pkg-config,
+  pnpm_9,
+  rustPlatform,
+  turbo,
+  webkitgtk_4_1,
+}:
 
-let pnpm = pnpm_9;
+let
+  pnpm = pnpm_9;
 
-in rustPlatform.buildRustPackage rec {
+in
+rustPlatform.buildRustPackage rec {
   pname = "modrinth-app-unwrapped";
   version = "0.9.3";
 
@@ -32,25 +47,31 @@ in rustPlatform.buildRustPackage rec {
     pnpm.configHook
   ] ++ lib.optional stdenv.hostPlatform.isDarwin makeBinaryWrapper;
 
-  buildInputs = [ openssl ]
-    ++ lib.optional stdenv.hostPlatform.isLinux webkitgtk_4_1;
+  buildInputs = [ openssl ] ++ lib.optional stdenv.hostPlatform.isLinux webkitgtk_4_1;
 
   # Tests fail on other, unrelated packages in the monorepo
-  cargoTestFlags = [ "--package" "theseus_gui" ];
+  cargoTestFlags = [
+    "--package"
+    "theseus_gui"
+  ];
 
-  env = { TURBO_BINARY_PATH = lib.getExe turbo; };
+  env = {
+    TURBO_BINARY_PATH = lib.getExe turbo;
+  };
 
-  postInstall = lib.optionalString stdenv.hostPlatform.isDarwin ''
-    makeBinaryWrapper "$out"/Applications/Modrinth\ App.app/Contents/MacOS/Modrinth\ App "$out"/bin/ModrinthApp
-  '' + lib.optionalString stdenv.hostPlatform.isLinux ''
-    desktop-file-edit \
-      --set-comment "Modrinth's game launcher" \
-      --set-key="StartupNotify" --set-value="true" \
-      --set-key="Categories" --set-value="Game;ActionGame;AdventureGame;Simulation;" \
-      --set-key="Keywords" --set-value="game;minecraft;mc;" \
-      --set-key="StartupWMClass" --set-value="ModrinthApp" \
-      $out/share/applications/Modrinth\ App.desktop
-  '';
+  postInstall =
+    lib.optionalString stdenv.hostPlatform.isDarwin ''
+      makeBinaryWrapper "$out"/Applications/Modrinth\ App.app/Contents/MacOS/Modrinth\ App "$out"/bin/ModrinthApp
+    ''
+    + lib.optionalString stdenv.hostPlatform.isLinux ''
+      desktop-file-edit \
+        --set-comment "Modrinth's game launcher" \
+        --set-key="StartupNotify" --set-value="true" \
+        --set-key="Categories" --set-value="Game;ActionGame;AdventureGame;Simulation;" \
+        --set-key="Keywords" --set-value="game;minecraft;mc;" \
+        --set-key="StartupWMClass" --set-value="ModrinthApp" \
+        $out/share/applications/Modrinth\ App.desktop
+    '';
 
   meta = {
     description = "Modrinth's game launcher";
@@ -59,7 +80,10 @@ in rustPlatform.buildRustPackage rec {
       and keep them up to date, all in one neat little package
     '';
     homepage = "https://modrinth.com";
-    license = with lib.licenses; [ gpl3Plus unfreeRedistributable ];
+    license = with lib.licenses; [
+      gpl3Plus
+      unfreeRedistributable
+    ];
     maintainers = with lib.maintainers; [ getchoo ];
     mainProgram = "ModrinthApp";
     platforms = with lib; platforms.linux ++ platforms.darwin;
