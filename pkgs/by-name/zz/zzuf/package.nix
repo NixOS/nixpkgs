7eb5d48a3b741cefa0cbfe3f473b425aada85a33
ghpzin <ghpzin@gmail.com>
2025-04-02 16:55:46 +0300

    zzuf: fix build with gcc14
    - add patch from fedora:
    https://src.fedoraproject.org/rpms/zzuf/c/998c7e5e632ea4c635a53437a01bfb48cbd744ac
    fixing `implicit-function-declaration` error:
    ```
    zzat.c: In function 'run':
    zzat.c:482:32: error: implicit declaration of function '_IO_getc'
      482 |             MY_FREAD(ch = (n = _IO_getc(f)), &ch, (n != EOF));
          |                                ^~~~~~~~
    ```
    
    - add patch from fedora (vendored because patch is in format that does not
    apply with `fetchpatch`):
    https://src.fedoraproject.org/rpms/zzuf/c/ca7e406989e7ff461600084f2277ad15a8c00058
    fixing `implicit-function-declaration` error:
    ```
    zzat.c:565:26: error: implicit declaration of function '__fseeko64'; did you mean 'fseeko64'?
      565 |             MY_FSEEK(l = __fseeko64(f, l1, SEEK_CUR),
          |                          ^~~~~~~~~~
    ```

--- pkgs/by-name/zz/zzuf/package.nix
+++ pkgs/by-name/zz/zzuf/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   pkg-config,
   autoreconfHook,
 }:
@@ -16,6 +17,17 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "0li1s11xf32dafxq1jbnc8c63313hy9ry09dja2rymk9mza4x2n9";
   };
+
+  patches = [
+    # fix build with gcc14
+    # https://src.fedoraproject.org/rpms/zzuf/c/998c7e5e632ea4c635a53437a01bfb48cbd744ac
+    (fetchpatch {
+      url = "https://src.fedoraproject.org/rpms/zzuf/raw/998c7e5e632ea4c635a53437a01bfb48cbd744ac/f/zzuf-zzat-c99.patch";
+      hash = "sha256-pQQzwsIjKg+9g+dnhFGn2PUlxHlQ5Mj+e4a1D1k2oEo=";
+    })
+    # https://src.fedoraproject.org/rpms/zzuf/c/ca7e406989e7ff461600084f2277ad15a8c00058
+    ./zzuf-glibc.patch
+  ];
 
   nativeBuildInputs = [
     pkg-config
