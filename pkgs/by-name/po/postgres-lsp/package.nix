184816f17829c5bf1b96ad85a006b71024b3cc1a
myypo <nikirsmcgl@gmail.com>
2025-04-02 22:22:19 +0300

    postgres-lsp: 0-unstable-2024-03-24 -> 0.3.1

--- pkgs/by-name/po/postgres-lsp/package.nix
+++ pkgs/by-name/po/postgres-lsp/package.nix
@@ -3,38 +3,45 @@
   rustPlatform,
   fetchFromGitHub,
   protobuf,
+  postgresql,
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "postgres-lsp";
-  version = "0-unstable-2024-03-24";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "postgres_lsp";
-    rev = "43ca9b675cb152ca7f38cfa6aff6dd2131dfa9a2";
-    hash = "sha256-n7Qbt9fGzC0CcleAtTWDInPz4oaPjI+pvIPrR5EYJ9U=";
+    tag = version;
+    hash = "sha256-UXBxqhfg3zxG+U4XhKQWqhxnmndmiRiXb8SZ0OlpuYg=";
     fetchSubmodules = true;
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-9T3bm/TSjnFeF8iE4338I44espnFq6l36yOq8YFPaPQ=";
+  cargoHash = "sha256-wV1zqaNlihrjS/R9q8b50g5gY6FxddGi62R91adivQE=";
 
   nativeBuildInputs = [
     protobuf
     rustPlatform.bindgenHook
+    postgresql
   ];
 
-  cargoBuildFlags = [ "-p=postgres_lsp" ];
+  env = {
+    SQLX_OFFLINE = 1;
+
+    # As specified in the upstream: https://github.com/supabase-community/postgres-language-server/blob/main/.github/workflows/release.yml
+    RUSTFLAGS = "-C strip=symbols -C codegen-units=1";
+    PGT_VERSION = version;
+  };
+
+  cargoBuildFlags = [ "-p=pgt_cli" ];
   cargoTestFlags = cargoBuildFlags;
 
-  RUSTC_BOOTSTRAP = 1; # We need rust unstable features
-
-  meta = with lib; {
+  meta = {
     description = "Language Server for Postgres";
     homepage = "https://github.com/supabase/postgres_lsp";
-    license = licenses.mit;
-    maintainers = with maintainers; [ figsoda ];
-    mainProgram = "postgres_lsp";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ figsoda ];
+    mainProgram = "postgrestools";
   };
 }
