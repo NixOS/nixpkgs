b412a9381d2461d14c149d179f40905a53e62acd
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-02 22:12:10 +0000

    python312Packages.llama-index-vector-stores-qdrant: 0.4.3 -> 0.6.0

--- pkgs/development/python-modules/llama-index-vector-stores-qdrant/default.nix
+++ pkgs/development/python-modules/llama-index-vector-stores-qdrant/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-vector-stores-qdrant";
-  version = "0.4.3";
+  version = "0.6.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_index_vector_stores_qdrant";
     inherit version;
-    hash = "sha256-J5q+ZeuDlCqHq7g3cipLd8q4OA1jmtvjJ1vt7/kGEQU=";
+    hash = "sha256-8vBnEOX6Ob4cXxQofmuxJBZumxdFug88JhGt1UnuAGs=";
   };
 
   build-system = [ poetry-core ];
