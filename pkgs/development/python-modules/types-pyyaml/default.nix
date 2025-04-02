c47e20b21190cfc264d1f9afae64ceacc22f7184
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-02 15:31:05 +0200

    python313Packages.types-pyyaml: 6.0.12.20250326 -> 6.0.12.20250402

--- pkgs/development/python-modules/types-pyyaml/default.nix
+++ pkgs/development/python-modules/types-pyyaml/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "types-pyyaml";
-  version = "6.0.12.20250326";
+  version = "6.0.12.20250402";
   pyproject = true;
 
   src = fetchPypi {
     pname = "types_pyyaml";
     inherit version;
-    hash = "sha256-Xi2G2HBml4A/NhuguBiO7ymZ4cNyzU+u5OuwhEuKQZA=";
+    hash = "sha256-18E8Pm0zW2r0sBIqAf8dJwq6hKuW0aGhBj7Lo+E+wHU=";
   };
 
   build-system = [ setuptools ];
