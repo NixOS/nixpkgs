aa58e7ff83a44c5049b2c6182e4afd92a86e6247
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-04-02 23:31:23 +0300

    teleport_17: 17.4.1 -> 17.4.2
    Changelog: https://github.com/gravitational/teleport/releases/tag/v17.4.2
    Diff: https://github.com/gravitational/teleport/compare/v17.4.1...v17.4.2

--- pkgs/servers/teleport/17/default.nix
+++ pkgs/servers/teleport/17/default.nix
@@ -2,9 +2,9 @@
 import ../generic.nix (
   args
   // {
-    version = "17.4.1";
-    hash = "sha256-wXQ2ZFbx/Nx8BzJK/yOGrtGs9ELhRRYlPQIbgBegOHQ=";
-    vendorHash = "sha256-C2YpZr9baUUE3pPHXNCIppujYQkioC9DWzSqeigmzmE=";
+    version = "17.4.2";
+    hash = "sha256-hiitFUN7bJR68sh/HrWsQMUm1lM2J3yjSoIT7mv/ldo=";
+    vendorHash = "sha256-03qkUH7UfAF0FwbG5enKf9Ke1teN89vmzk8yRfGvmPg=";
     pnpmHash = "sha256-Hh4R+mkJJp9CR4NHw+VFzLPxb7e9T1BQkey0in2t934=";
     cargoHash = "sha256-0PT9y56V/WHo3M5TcpVWBuHcQMZ0w2L4rEuXuTvVNFU=";
   }
