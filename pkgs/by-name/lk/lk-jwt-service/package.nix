cd0b0ceec66c833191ebd7f0e7636ec134ea0ae8
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-02 17:55:16 +0200

    lk-jwt-service: 0.2.1 -> 0.2.2
    https://github.com/element-hq/lk-jwt-service/compare/refs/tags/v0.2.1...v0.2.2

--- pkgs/by-name/lk/lk-jwt-service/package.nix
+++ pkgs/by-name/lk/lk-jwt-service/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "lk-jwt-service";
-  version = "0.2.1";
+  version = "0.2.2";
 
   src = fetchFromGitHub {
     owner = "element-hq";
     repo = "lk-jwt-service";
     tag = "v${version}";
-    hash = "sha256-R4YqmHp0i+RpJJkENJPZJDNCVg+O+70JMoCR8ZlesyM=";
+    hash = "sha256-ONL2qKBXL2FtTv5Eao61qPKWP2h9t3KyoHlS5nAHMGA=";
   };
 
-  vendorHash = "sha256-evzltyQZCBQ4/k641sQrmUvw6yIBWFEic/WUa/WX5xE=";
+  vendorHash = "sha256-47eJO1Ai78RuhlEPn/J1cd+YSqvmfUD8cuPZIqsdxvI=";
 
   meta = with lib; {
     description = "Minimal service to issue LiveKit JWTs for MatrixRTC";
