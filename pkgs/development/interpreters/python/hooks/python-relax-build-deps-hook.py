7317d0f0b4dc894018fdc5ef1448b96110aaf42a
nicoo <nicoo@mur.at>
2025-04-03 12:33:25 +0000

    pythonPackages.pythonRelaxBuildDepsHook: clarify and comment
    Also fixed a minor bug: `pythonRelaxBuildDeps` was defaulting to `{}`, not `[]`

--- pkgs/development/interpreters/python/hooks/python-relax-build-deps-hook.py
+++ pkgs/development/interpreters/python/hooks/python-relax-build-deps-hook.py
@@ -5,24 +5,28 @@ from pathlib import Path
 import json, tomlkit
 
 
+# JSON input is expected, so the derivation must set __structuredAttrs
 nixAttrs = json.load(Path(environ['NIX_ATTRS_JSON_FILE']).open())
 removeDeps = frozenset(nixAttrs.get("pythonRemoveBuildDeps", []))
 
-relaxDeps = nixAttrs.get("pythonRelaxBuildDeps", {})
-if isinstance(relaxDeps, list):
-    relaxDeps = { req.name: req for req in map(Requirement, relaxDeps) }
-else:
-    # No support (yet?) for `pythonRelaxBuildDeps.foo = ">1, <2";`; see
-    #  https://github.com/NixOS/nixpkgs/pull/382125#discussion_r1956658253
-    raise ValueError(f"Expected a list for `pythonRelaxBuildDeps`, was '{type(relaxDeps)}'")
+match nixAttrs.get("pythonRelaxBuildDeps", []):
+    case [ *requirements ]:
+        relaxDeps = { req.name: req for req in map(Requirement, requirements) }
+    case _:
+        # No support (yet?) for `pythonRelaxBuildDeps.foo = ">1, <2";`; see
+        #  https://github.com/NixOS/nixpkgs/pull/382125#discussion_r1956658253
+        raise ValueError(f"Expected a list for `pythonRelaxBuildDeps`, was '{type(relaxDeps)}'")
 
-overlappingDeps = removeDeps.intersection(relaxDeps)
-if overlappingDeps:
-    raise ValueError(
-        "The following dependencies occur in both 'pythonRelaxBuildDeps` and 'pythonRemoveBuildDeps`: " + \
-        ", ".join(overlappingDeps)
-    )
+# Quick input validation
+match removeDeps.intersection(relaxDeps):
+    case []: pass
+    case [ *overlap ]:
+        raise ValueError(
+            "The following dependencies occur in both 'pythonRelaxBuildDeps` and 'pythonRemoveBuildDeps`: " + \
+            ", ".join(overlappingDeps)
+        )
 
+# Parse pyproject.toml, update as needed, and write back out
 pyPrjPath = Path("pyproject.toml")
 pyProject = tomlkit.load(pyPrjPath.open())
 
