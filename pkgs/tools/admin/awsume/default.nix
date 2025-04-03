f8ee256231430a76d15a3908de06cb114a60fac1
FliegendeWurst <arne.keller@posteo.de>
2025-04-04 00:07:41 +0200

    awsume: enable tests, add optional dependencies, cleanup

--- pkgs/tools/admin/awsume/default.nix
+++ pkgs/tools/admin/awsume/default.nix
@@ -4,36 +4,51 @@
   installShellFiles,
   buildPythonApplication,
   fetchFromGitHub,
+  bash,
   boto3,
   colorama,
   psutil,
   pluggy,
+  pytestCheckHook,
+  levenshtein,
   pyyaml,
   setuptools,
+  writableTmpDirAsHomeHook,
+  xmltodict,
 }:
 
 buildPythonApplication rec {
   pname = "awsume";
   version = "4.5.5";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "trek10inc";
     repo = "awsume";
-    rev = version;
-    sha256 = "sha256-lm9YANYckyHDoNbB1wytBm55iyBmUuxFPmZupfpReqc=";
+    tag = version;
+    hash = "sha256-lm9YANYckyHDoNbB1wytBm55iyBmUuxFPmZupfpReqc=";
   };
 
-  AWSUME_SKIP_ALIAS_SETUP = 1;
+  env.AWSUME_SKIP_ALIAS_SETUP = 1;
 
   nativeBuildInputs = [ installShellFiles ];
 
-  propagatedBuildInputs = [
+  buildInputs = [
+    bash
+  ];
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
     colorama
     boto3
     psutil
     pluggy
+    levenshtein
     pyyaml
-    setuptools
+    xmltodict
   ];
 
   postPatch = ''
@@ -43,21 +58,35 @@ buildPythonApplication rec {
   '';
 
   postInstall = ''
+    patchShebangs --update --host $out/bin/awsume
     installShellCompletion --cmd awsume \
-      --bash <(PYTHONPATH=./awsume/configure ${python}/bin/python3 -c"import autocomplete; print(autocomplete.SCRIPTS['bash'])") \
-      --zsh <(PYTHONPATH=./awsume/configure ${python}/bin/python3 -c"import autocomplete; print(autocomplete.ZSH_AUTOCOMPLETE_FUNCTION)") \
-      --fish <(PYTHONPATH=./awsume/configure ${python}/bin/python3 -c"import autocomplete; print(autocomplete.SCRIPTS['fish'])") \
+      --bash <(PYTHONPATH=./awsume/configure ${python.pythonOnBuildForHost.interpreter} -c"import autocomplete; print(autocomplete.SCRIPTS['bash'])") \
+      --zsh <(PYTHONPATH=./awsume/configure ${python.pythonOnBuildForHost.interpreter} -c"import autocomplete; print(autocomplete.ZSH_AUTOCOMPLETE_FUNCTION)") \
+      --fish <(PYTHONPATH=./awsume/configure ${python.pythonOnBuildForHost.interpreter} -c"import autocomplete; print(autocomplete.SCRIPTS['fish'])") \
 
     rm -f $out/bin/awsume.bat
   '';
 
-  doCheck = false;
+  doCheck = true;
 
-  meta = with lib; {
+  nativeCheckInputs = [
+    pytestCheckHook
+    writableTmpDirAsHomeHook
+  ];
+
+  disabledTests = [
+    # rely on configuration setup in $HOME
+    "test_safe_print"
+    "test_safe_print_color"
+    "test_safe_print_end"
+    "test_safe_print_ignore_color_on_windows"
+  ];
+
+  meta = {
     description = "Utility for easily assuming AWS IAM roles from the command line";
-    homepage = "https://github.com/trek10inc/awsume";
-    license = [ licenses.mit ];
+    homepage = "https://awsu.me/";
+    license = [ lib.licenses.mit ];
     mainProgram = "awsume";
-    maintainers = [ maintainers.nilp0inter ];
+    maintainers = [ lib.maintainers.nilp0inter ];
   };
 }
