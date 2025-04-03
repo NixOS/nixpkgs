eea74970e2ba18066a5d75fb90af9381fb89f424
nyukuru <nicholasod@proton.me>
2025-04-03 03:17:54 -0400

    nixos/waybar: add settings option

--- nixos/modules/programs/wayland/waybar.nix
+++ nixos/modules/programs/wayland/waybar.nix
@@ -6,16 +6,16 @@
 }:
 
 let
+  json = pkgs.formats.json { };
   cfg = config.programs.waybar;
 in
 {
   options.programs.waybar = {
     enable = lib.mkEnableOption "waybar, a highly customizable Wayland bar for Sway and Wlroots based compositors";
-    package =
-      lib.mkPackageOption pkgs "waybar" { }
-      // lib.mkOption {
-        apply = pkg: pkg.override { systemdSupport = true; };
-      };
+    package = lib.mkPackageOption pkgs "waybar" { } // {
+      apply = pkg: pkg.override { systemdSupport = true; };
+    };
+
     systemd.target = lib.mkOption {
       type = lib.types.str;
       description = ''
@@ -23,10 +23,41 @@ in
       '';
       default = "graphical-session.target";
     };
+
+    settings = lib.mkOption {
+      type = json.type;
+      default = { };
+      description = "waybar configuration, see man waybar(5)";
+      example = lib.literalExpression ''
+        {
+          layer = "top";
+          modules-left = [ "sway/workspaces" "sway/mode" ];
+          modules-center = [ "sway/window" ];
+          modules-right = [ "battery" "clock" ];
+
+          "sway/window" = {
+            max-length = 50;
+          };
+
+          battery = {
+            format = "{capacity}% {icon}";
+            format-icons = ["" "" "" "" ""];
+          };
+
+          clock = {
+            format-alt = "{:%a, %d. %b  %H:%M}";
+          };
+        }
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ cfg.package ];
+    environment = {
+      systemPackages = [ cfg.package ];
+      etc."xdg/waybar/config".source = json.generate "waybar-config" cfg.settings;
+    };
+
     systemd = {
       packages = [ cfg.package ];
       user.services.waybar.wantedBy = [ cfg.systemd.target ];
