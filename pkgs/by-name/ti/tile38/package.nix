05be0e39ef99a420ceedf81795b8419ddab17653
Nikolay Korotkiy <sikmir@disroot.org>
2025-04-03 22:32:18 +0400

    tile38: modernize

--- pkgs/by-name/ti/tile38/package.nix
+++ pkgs/by-name/ti/tile38/package.nix
@@ -10,8 +10,8 @@ buildGoModule rec {
 
   src = fetchFromGitHub {
     owner = "tidwall";
-    repo = pname;
-    rev = version;
+    repo = "tile38";
+    tag = version;
     hash = "sha256-F6mSIKivRkqbuixWyl9EmUskHsiQyzNrEt1ONvDVKrg=";
   };
 
@@ -28,14 +28,14 @@ buildGoModule rec {
     "-X github.com/tidwall/tile38/core.Version=${version}"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Real-time Geospatial and Geofencing";
     longDescription = ''
       Tile38 is an in-memory geolocation data store, spatial index, and realtime geofence.
       It supports a variety of object types including lat/lon points, bounding boxes, XYZ tiles, Geohashes, and GeoJSON.
     '';
     homepage = "https://tile38.com/";
-    license = licenses.mit;
-    maintainers = teams.geospatial.members;
+    license = lib.licenses.mit;
+    maintainers = lib.teams.geospatial.members;
   };
 }
