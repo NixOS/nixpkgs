09b7be57bbe800b448df50c3dc744b4e030ea7cd
Vladimír Čunát <v@cunat.cz>
2025-04-03 21:13:20 +0200

    xz: 5.8.0 -> 5.8.1
    Security update: CVE-2025-31115
    https://github.com/tukaani-project/xz/releases/tag/v5.8.1

--- pkgs/tools/compression/xz/default.nix
+++ pkgs/tools/compression/xz/default.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xz";
-  version = "5.8.0";
+  version = "5.8.1";
 
   src = fetchurl {
     url =
       with finalAttrs;
       "https://github.com/tukaani-project/xz/releases/download/v${version}/xz-${version}.tar.xz";
-    hash = "sha256-BeytnnGRn0/Knxn7vJeeoo4jAYjtEj3G8GuYAx6hRUI=";
+    hash = "sha256-C1T3nfhZElBN4LFK7Hlx4/lkSRrxgS2DRHAFgHUTzZ4=";
   };
 
   strictDeps = true;
