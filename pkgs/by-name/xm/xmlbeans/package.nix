df9f9211c891f07abf11a20d632e6f54df4a5c42
emaryn <emaryn@users.noreply.github.com>
2025-04-05 03:57:25 +0800

    xmlbeans: 5.1.1-20220819 -> 5.3.0-20241203

--- pkgs/by-name/xm/xmlbeans/package.nix
+++ pkgs/by-name/xm/xmlbeans/package.nix
@@ -5,41 +5,46 @@
   jre_headless,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xmlbeans";
-  version = "5.1.1-20220819";
+  version = "5.3.0-20241203";
 
   src = fetchzip {
     # old releases are deleted from the cdn
-    url = "https://web.archive.org/web/20230313151507/https://dlcdn.apache.org/poi/xmlbeans/release/bin/xmlbeans-bin-${version}.zip";
-    sha256 = "sha256-TDnWo1uJWL6k6Z8/uaF2LBNzRVQMHYopYze/2Fb/0aI=";
+    url = "https://web.archive.org/web/20250404194918/https://dlcdn.apache.org/poi/xmlbeans/release/bin/apache-xmlbeans-bin-5.3.0-20241203.zip";
+    hash = "sha256-AeV+s0VfBgb0YbsY6dNJeqcsguZhDmjuyqXT/415a3k=";
+    stripRoot = false;
   };
 
   postPatch = ''
+    cp -r apache-xmlbeans-*/* .
+    rm -r apache-xmlbeans-*
     rm bin/*.cmd
     substituteInPlace bin/dumpxsb \
-      --replace 'echo `dirname $0`' ""
-
+      --replace-fail 'echo `dirname $0`' ""
     substituteInPlace bin/_setlib \
-      --replace 'echo XMLBEANS_LIB=$XMLBEANS_LIB' ""
-
+      --replace-fail 'echo XMLBEANS_LIB=$XMLBEANS_LIB' ""
     for file in bin/*; do
       substituteInPlace $file \
-        --replace "java " "${jre_headless}/bin/java "
+        --replace-warn "java " "${jre_headless}/bin/java "
     done
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out
     chmod +x bin/*
     cp -r bin/ lib/ $out/
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Java library for accessing XML by binding it to Java types";
     homepage = "https://xmlbeans.apache.org/";
     downloadPage = "https://dlcdn.apache.org/poi/xmlbeans/release/bin/";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
     maintainers = [ ];
   };
-}
+})
