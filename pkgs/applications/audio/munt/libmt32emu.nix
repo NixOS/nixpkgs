639fea7a097b836955e04e4bed38bf1ed372e5a2
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-04 13:14:30 +0000

    libmt32emu: 2.7.1 -> 2.7.2

--- pkgs/applications/audio/munt/libmt32emu.nix
+++ pkgs/applications/audio/munt/libmt32emu.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libmt32emu";
-  version = "2.7.1";
+  version = "2.7.2";
 
   src = fetchFromGitHub {
     owner = "munt";
     repo = "munt";
     rev = "${pname}_${lib.replaceStrings [ "." ] [ "_" ] version}";
-    sha256 = "sha256-zY1AFcm8uvFkrKUZHsqtKY2CYTY4bWmkTJ7bZPqXoxk=";
+    sha256 = "sha256-wXIvdGoup/AOQggkeXvtbi3pXhyKUKWmyt/ZbGzufds=";
   };
 
   outputs = [
