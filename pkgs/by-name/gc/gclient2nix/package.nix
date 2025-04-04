48ac1dfdf68bcf3ce1157690f460cd5ecf583119
Yureka <yureka@cyberchaos.dev>
2025-04-04 16:07:52 +0200

    gclient2nix: add importGclientDeps & gclientUnpackHook

--- pkgs/by-name/gc/gclient2nix/package.nix
+++ pkgs/by-name/gc/gclient2nix/package.nix
@@ -6,9 +6,49 @@
   path,
   fetchgit,
   nurl,
+  writers,
+  callPackage,
+  fetchFromGitiles,
+  fetchFromGitHub,
 }:
 
 let
+  fetchers = {
+    inherit fetchgit fetchFromGitiles fetchFromGitHub;
+  };
+
+  importGclientDeps =
+    depsAttrsOrFile:
+    let
+      depsAttrs = if lib.isAttrs depsAttrsOrFile then depsAttrsOrFile else lib.importJSON depsAttrsOrFile;
+      fetchdep = dep: fetchers.${dep.fetcher} dep.args;
+      fetchedDeps = lib.mapAttrs (_name: fetchdep) depsAttrs;
+      manifestContents = lib.mapAttrs (_: dep: {
+        path = dep;
+      }) fetchedDeps;
+      manifest = writers.writeJSON "gclient-manifest.json" manifestContents;
+    in
+    manifestContents
+    // {
+      inherit manifest;
+      __toString = _: manifest;
+    };
+
+  gclientUnpackHook = callPackage (
+    {
+      lib,
+      makeSetupHook,
+      jq,
+    }:
+
+    makeSetupHook {
+      name = "gclient-unpack-hook";
+      substitutions = {
+        jq = lib.getExe jq;
+      };
+    } ./gclient-unpack-hook.sh
+  ) { };
+
   python = python3.withPackages (
     ps: with ps; [
       joblib
@@ -31,6 +71,10 @@ runCommand "gclient2nix"
       url = "https://chromium.googlesource.com/chromium/tools/depot_tools";
       rev = "452fe3be37f78fbecefa1b4b0d359531bcd70d0d";
       hash = "sha256-8IiJOm0FLa/u1Vd96tb33Ruj4IUTCeYgBpTk88znhPw=";
+    };
+
+    passthru = {
+      inherit fetchers importGclientDeps gclientUnpackHook;
     };
   }
   ''
