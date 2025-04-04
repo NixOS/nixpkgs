e3273f86707c4c2231373857673914a4537f9c1e
commiterate <111539270+commiterate@users.noreply.github.com>
2025-04-03 22:04:35 -0400

    gitlab-runner: Add main program, version check hook, and Nix update script.

--- pkgs/by-name/gi/gitlab-runner/package.nix
+++ pkgs/by-name/gi/gitlab-runner/package.nix
@@ -1,28 +1,15 @@
 {
   lib,
+  bash,
   buildGoModule,
   fetchFromGitLab,
-  bash,
+  nix-update-script,
+  versionCheckHook,
 }:
 
-let
-  version = "17.2.0";
-in
 buildGoModule rec {
-  inherit version;
   pname = "gitlab-runner";
-
-  commonPackagePath = "gitlab.com/gitlab-org/gitlab-runner/common";
-  ldflags = [
-    "-X ${commonPackagePath}.NAME=gitlab-runner"
-    "-X ${commonPackagePath}.VERSION=${version}"
-    "-X ${commonPackagePath}.REVISION=v${version}"
-  ];
-
-  # For patchShebangs
-  buildInputs = [ bash ];
-
-  vendorHash = "sha256-1MwHss76apA9KoFhEU6lYiUACrPMGYzjhds6nTyNuJI=";
+  version = "17.2.0";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
@@ -31,24 +18,29 @@ buildGoModule rec {
     hash = "sha256-a2Igy4DS3fYTvPW1vvDrH/DjMQ4lG9cm/P3mFr+y9s4=";
   };
 
+  vendorHash = "sha256-1MwHss76apA9KoFhEU6lYiUACrPMGYzjhds6nTyNuJI=";
+
+  # For patchShebangs.
+  buildInputs = [ bash ];
+
   patches = [
     ./fix-shell-path.patch
     ./remove-bash-test.patch
   ];
 
   prePatch = ''
-    # Remove some tests that can't work during a nix build
+    # Remove some tests that can't work during a nix build.
 
     # Requires to run in a git repo
     sed -i "s/func TestCacheArchiverAddingUntrackedFiles/func OFF_TestCacheArchiverAddingUntrackedFiles/" commands/helpers/file_archiver_test.go
     sed -i "s/func TestCacheArchiverAddingUntrackedUnicodeFiles/func OFF_TestCacheArchiverAddingUntrackedUnicodeFiles/" commands/helpers/file_archiver_test.go
 
-    # No writable developer environment
+    # No writable developer environment.
     rm common/build_test.go
     rm common/build_settings_test.go
     rm executors/custom/custom_test.go
 
-    # No docker during build
+    # No docker during build.
     rm executors/docker/terminal_test.go
     rm executors/docker/docker_test.go
     rm helpers/docker/auth/auth_test.go
@@ -56,25 +48,46 @@ buildGoModule rec {
   '';
 
   excludedPackages = [
-    # CI helper script for pushing images to Docker and ECR registries
+    # CI helper script for pushing images to Docker and ECR registries.
+    #
     # https://gitlab.com/gitlab-org/gitlab-runner/-/merge_requests/4139
     "./scripts/sync-docker-images"
   ];
+
+  ldflags =
+    let
+      ldflagsPackageVariablePrefix = "gitlab.com/gitlab-org/gitlab-runner/common";
+    in
+    [
+      "-X ${ldflagsPackageVariablePrefix}.NAME=gitlab-runner"
+      "-X ${ldflagsPackageVariablePrefix}.VERSION=v${version}"
+      "-X ${ldflagsPackageVariablePrefix}.REVISION=${src.rev}"
+    ];
+
+  preCheck = ''
+    # Make the tests pass outside of GitLab CI.
+    export CI=0
+  '';
 
   postInstall = ''
     install packaging/root/usr/share/gitlab-runner/clear-docker-cache $out/bin
   '';
 
-  preCheck = ''
-    # Make the tests pass outside of GitLab CI
-    export CI=0
-  '';
+  doInstallCheck = true;
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  versionCheckProgramArg = "--version";
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
 
   meta = with lib; {
     description = "GitLab Runner the continuous integration executor of GitLab";
+    homepage = "https://docs.gitlab.com/runner";
     license = licenses.mit;
-    homepage = "https://docs.gitlab.com/runner/";
-    platforms = platforms.unix ++ platforms.darwin;
+    mainProgram = "gitlab-runner";
     maintainers = with maintainers; [ zimbatm ] ++ teams.gitlab.members;
   };
 }
