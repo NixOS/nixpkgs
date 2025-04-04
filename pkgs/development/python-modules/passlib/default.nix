5983b10dd89d5ea0d17b1650c21c9fc6a15b184a
Robert Schütz <nix@dotlambda.de>
2025-04-04 08:39:01 -0700

    python313Packages.passlib: turn into wrapper around libpass

--- pkgs/development/python-modules/passlib/default.nix
+++ pkgs/development/python-modules/passlib/default.nix
@@ -1,58 +1,14 @@
 {
-  lib,
-  buildPythonPackage,
-  fetchFromGitHub,
-  argon2-cffi,
-  bcrypt,
-  cryptography,
-  hatchling,
-  pytestCheckHook,
-  pytest-archon,
-  pytest-xdist,
-  typing-extensions,
+  libpass,
+  mkPythonMetaPackage,
 }:
 
-buildPythonPackage rec {
+mkPythonMetaPackage {
   pname = "passlib";
-  version = "1.9.0";
-  pyproject = true;
-
-  src = fetchFromGitHub {
-    owner = "ThirVondukr";
-    repo = "passlib";
-    tag = version;
-    hash = "sha256-Q5OEQkty0/DugRvF5LA+PaDDlF/6ysx4Nel5K2kH5s4=";
-  };
-
-  build-system = [ hatchling ];
-
-  dependencies = [ typing-extensions ];
-
-  optional-dependencies = {
-    argon2 = [ argon2-cffi ];
-    bcrypt = [ bcrypt ];
-    totp = [ cryptography ];
-  };
-
-  nativeCheckInputs = [
-    pytestCheckHook
-    pytest-archon
-    pytest-xdist
-  ] ++ lib.flatten (lib.attrValues optional-dependencies);
-
-  pythonImportsCheck = [ "passlib" ];
-
-  disabledTests = [
-    # timming sensitive
-    "test_dummy_verify"
-    "test_encrypt_cost_timing"
-  ];
-
+  inherit (libpass) version;
+  dependencies = [ libpass ];
+  optional-dependencies = libpass.optional-dependencies or { };
   meta = {
-    changelog = "https://github.com/ThirVondukr/passlib/blob/${src.tag}/CHANGELOG.md";
-    description = "Password hashing library for Python";
-    homepage = "https://github.com/ThirVondukr/passlib";
-    license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [ dotlambda ];
+    inherit (libpass.meta) changelog description homepage;
   };
 }
