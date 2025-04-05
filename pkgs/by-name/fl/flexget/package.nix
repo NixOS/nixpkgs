e4f76a0f03b259b82e108ef099982b7b60132ac7
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-05 05:04:29 +0000

    flexget: 3.15.30 -> 3.15.31

--- pkgs/by-name/fl/flexget/package.nix
+++ pkgs/by-name/fl/flexget/package.nix
@@ -7,14 +7,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "flexget";
-  version = "3.15.30";
+  version = "3.15.31";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Flexget";
     repo = "Flexget";
     tag = "v${version}";
-    hash = "sha256-iGEGQxzvyyOeL/v0j1TiAjx2fa2tCZeRJ7VQByWpxJg=";
+    hash = "sha256-Q1o7jSFvwTD9qPXst6J9Vg/pY9olEPSUegBHOZueDEk=";
   };
 
   pythonRelaxDeps = true;
