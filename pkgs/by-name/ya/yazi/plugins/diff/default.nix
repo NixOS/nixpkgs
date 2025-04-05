21f4687dd0ccfca0fff222d775efcb23df8bae61
Austin Horstman <khaneliman12@gmail.com>
2025-04-04 22:21:56 -0500

    yaziPlugins.diff: init at 25.2.7-unstable-2025-03-02

---
+++ pkgs/by-name/ya/yazi/plugins/diff/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "diff.yazi";
+  version = "25.2.7-unstable-2025-03-02";
+
+  src = fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "b44c245500b34e713732a9130bf436b13b4777e9";
+    hash = "sha256-nZ8yfnKvNLM5aA+mmQ3PkfM5lwSKwWnkQewcg9GwseI=";
+  };
+
+  # NOTE: License is a relative symbolic link
+  # We remove the link and copy the true license
+  installPhase = ''
+    runHook preInstall
+
+    cp -r diff.yazi $out
+    rm $out/LICENSE
+    cp LICENSE $out
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Diff the selected file with the hovered file, create a living patch, and copy it to the clipboard";
+    homepage = "https://yazi-rs.github.io";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ khaneliman ];
+  };
+}
