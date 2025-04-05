eecafc9b97de8d7728a8cf7549b3a8b07517f6e9
Austin Horstman <khaneliman12@gmail.com>
2025-04-04 22:12:37 -0500

    yaziPlugins.full-border: init at 25.2.26-unstable-2025-03-11

---
+++ pkgs/by-name/ya/yazi/plugins/default.nix
@@ -0,0 +1,10 @@
+{ lib, callPackage }:
+let
+  root = ./.;
+  call = name: callPackage (root + "/${name}") { };
+in
+lib.pipe root [
+  builtins.readDir
+  (lib.filterAttrs (_: type: type == "directory"))
+  (builtins.mapAttrs (name: _: call name))
+]
