bbe2ccb7286f5c4decda72495557ec1469f15ee6
Austin Horstman <khaneliman12@gmail.com>
2025-04-04 22:25:40 -0500

    yaziPlugins.git: init at 25.2.26-unstable-2025-03-17

---
+++ pkgs/by-name/ya/yazi/plugins/git/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "git.yazi";
+  version = "25.2.26-unstable-2025-03-17";
+
+  src = fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "79193b3917d0f1b82ee41b4e64ae4df58f2284f6";
+    hash = "sha256-ZLL/dFjNsryjm51kFNOmw5DhSGl2K5IfatHpe1PkuFE=";
+  };
+
+  # NOTE: License is a relative symbolic link
+  # We remove the link and copy the true license
+  installPhase = ''
+    runHook preInstall
+
+    cp -r git.yazi $out
+    rm $out/LICENSE
+    cp LICENSE $out
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Show the status of Git file changes as linemode in the file list";
+    homepage = "https://yazi-rs.github.io";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ khaneliman ];
+  };
+}
