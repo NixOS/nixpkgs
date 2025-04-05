f919d29a9c0e200591066913bccc5cb3695ae439
iamanaws <78835633+Iamanaws@users.noreply.github.com>
2025-04-04 20:06:46 -0700

    maintainers: add iamanaws

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10037,6 +10037,12 @@
     github = "I-Al-Istannen";
     githubId = 20284688;
   };
+  iamanaws = {
+    email = "nixpkgs.yjzaw@slmail.me";
+    github = "Iamanaws";
+    githubId = 78835633;
+    name = "Angel J";
+  };
   iammrinal0 = {
     email = "nixpkgs@mrinalpurohit.in";
     matrix = "@iammrinal0:nixos.dev";
