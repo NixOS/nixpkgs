e392690f70ae27f80193cb2abc9ee866e1cb4af5
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-05 06:30:17 +0000

    aider-chat: 0.81.0 -> 0.81.2.dev

--- pkgs/development/python-modules/aider-chat/default.nix
+++ pkgs/development/python-modules/aider-chat/default.nix
@@ -124,7 +124,7 @@ let
     ];
   };
 
-  version = "0.81.0";
+  version = "0.81.2.dev";
   aider-chat = buildPythonPackage {
     pname = "aider-chat";
     inherit version;
@@ -137,7 +137,7 @@ let
       owner = "Aider-AI";
       repo = "aider";
       tag = "v${version}";
-      hash = "sha256-xWOXsffLAVBZvJM8PuAJ12IrmNLfXuqHrbIMtPM1leE=";
+      hash = "sha256-kMBSQhhmYEvAJ7zgxkXC8aca85tEZlQuKOQa9Mx5O1Y=";
     };
 
     pythonRelaxDeps = true;
