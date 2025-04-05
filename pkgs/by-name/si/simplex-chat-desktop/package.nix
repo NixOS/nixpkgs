e7d09152c152c60c10626890428e5408edc5c892
misilelab <misileminecord@gmail.com>
2025-04-05 13:20:25 +0900

    simplex-chat-desktop: 6.3.0 -> 6.3.1
    https://github.com/simplex-chat/simplex-chat/releases/tag/v6.3.1
    
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- pkgs/by-name/si/simplex-chat-desktop/package.nix
+++ pkgs/by-name/si/simplex-chat-desktop/package.nix
@@ -7,11 +7,11 @@
 
 let
   pname = "simplex-chat-desktop";
-  version = "6.3.0";
+  version = "6.3.1";
 
   src = fetchurl {
     url = "https://github.com/simplex-chat/simplex-chat/releases/download/v${version}/simplex-desktop-x86_64.AppImage";
-    hash = "sha256-p2NPBJyAfWkpM6Ws0Xm4Vyt3DIbfl+soYfYfqjV4cY0=";
+    hash = "sha256-jCBCZ5nzA8WxIQFsGpuuyZ1Jmt+PJmqybFHFOUkw9L0=";
   };
 
   appimageContents = appimageTools.extract {
