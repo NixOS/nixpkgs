250a092a21af6e7ac8ca58d717eced6e64344ed3
Tom van Dijk <18gatenmaker6@gmail.com>
2025-04-05 19:24:48 +0200

    nixos-artwork.wallpapers.nineish-catppuccin-frappe: init at 2025-01-27

--- pkgs/data/misc/nixos-artwork/wallpapers.nix
+++ pkgs/data/misc/nixos-artwork/wallpapers.nix
@@ -258,6 +258,16 @@ rec {
     license = lib.licenses.cc-by-sa-40;
   };
 
+  nineish-catppuccin-frappe = mkNixBackground {
+    name = "nineish-catppuccin-frappe-2025-01-27";
+    description = "Catppuccin Frappe wallpaper for Nix inspired by simpler times";
+    src = fetchurl {
+      url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/33856d7837cb8ba76c4fc9e26f91a659066ee31f/wallpapers/nix-wallpaper-nineish-catppuccin-frappe.png";
+      hash = "sha256-/HAtpGwLxjNfJvX5/4YZfM8jPNStaM3gisK8+ImRmQ4=";
+    };
+    license = lib.licenses.cc-by-sa-40;
+  };
+
   recursive = mkNixBackground {
     name = "recursive-2022-04-19";
     description = "3D wallpaper for Nix";
