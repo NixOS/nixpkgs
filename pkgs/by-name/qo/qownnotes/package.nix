04f70b1132832e378fa2a672f77b84cad08ff304
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-06 06:00:22 +0000

    qownnotes: 25.3.3 -> 25.4.1

--- pkgs/by-name/qo/qownnotes/package.nix
+++ pkgs/by-name/qo/qownnotes/package.nix
@@ -17,11 +17,11 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "qownnotes";
   appname = "QOwnNotes";
-  version = "25.3.3";
+  version = "25.4.1";
 
   src = fetchurl {
     url = "https://github.com/pbek/QOwnNotes/releases/download/v${finalAttrs.version}/qownnotes-${finalAttrs.version}.tar.xz";
-    hash = "sha256-waWFOPXxKgCb1h1dVSzXDVQwcKyFBKXuAwYXu63ofPo=";
+    hash = "sha256-1Als8iv9L9fOL9JNXRsM5ilfSXwd/l4kE6wnWkz1BBE=";
   };
 
   nativeBuildInputs =
