e322f445c4be2732242a93476b4a4000df4a5547
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-06 19:48:38 +0000

    supersonic: 0.14.0 -> 0.15.0

--- pkgs/by-name/su/supersonic/package.nix
+++ pkgs/by-name/su/supersonic/package.nix
@@ -21,16 +21,16 @@ assert waylandSupport -> stdenv.hostPlatform.isLinux;
 
 buildGoModule rec {
   pname = "supersonic" + lib.optionalString waylandSupport "-wayland";
-  version = "0.14.0";
+  version = "0.15.0";
 
   src = fetchFromGitHub {
     owner = "dweymouth";
     repo = "supersonic";
     rev = "v${version}";
-    hash = "sha256-ua2INyKPncXDOwzmKrgnRCb7q8CFEApEaYuBbQeau98=";
+    hash = "sha256-U6J1L2KDAEhIrSpbS5wMSc1Q9y5YMahhFNe6ArbwV2s=";
   };
 
-  vendorHash = "sha256-5LxYD9kLUvKgXmDCw1SNBM6ay8Vayj+PyoZRVptSM0c=";
+  vendorHash = "sha256-fc86z8bvdFI3LdlyHej2G42O554hpRszqre+e3WUOKI=";
 
   nativeBuildInputs =
     [
