80b1f41c37ac58578b18e4189dd2d635621472df
Qiming Chu <cchuqiming@gmail.com>
2025-04-06 21:21:01 +0800

    circt: 1.111.1 -> 1.112.0
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- pkgs/by-name/ci/circt/package.nix
+++ pkgs/by-name/ci/circt/package.nix
@@ -19,12 +19,12 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "circt";
-  version = "1.111.1";
+  version = "1.112.0";
   src = fetchFromGitHub {
     owner = "llvm";
     repo = "circt";
     rev = "firtool-${version}";
-    hash = "sha256-eOoUwYNQS83/uNfbbEPUbTf9aIjqc2vklIC3zi9U9KU=";
+    hash = "sha256-yh7/2QsUO2/sCyrgT8QTBC/BENEZNmXoHTIiz5abngw=";
     fetchSubmodules = true;
   };
 
