3519e57becdf2c947b67d24508e8d3e230590814
Mikael Voss <mvs@nyantec.com>
2025-04-06 23:45:58 +0200

    u-config: allow building of libc‐free pkg-config

--- pkgs/by-name/u-/u-config/package.nix
+++ pkgs/by-name/u-/u-config/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  noLibc ? stdenv.hostPlatform.isx86 && (stdenv.hostPlatform.isLinux || stdenv.hostPlatform.isMinGW),
   installShellFiles,
   versionCheckHook,
   nix-update-script,
@@ -10,10 +11,28 @@
 stdenv.mkDerivation (
   finalAttrs:
   let
-    inherit (lib) escapeShellArgs;
+    inherit (lib) optionals escapeShellArgs;
+    inherit (stdenv) hostPlatform;
 
-    exe = stdenv.hostPlatform.extensions.executable;
+    exe = hostPlatform.extensions.executable;
     binary = finalAttrs.meta.mainProgram + exe;
+
+    platform =
+      if noLibc then
+        (
+          with hostPlatform;
+          if isLinux && isx86_32 then
+            "linux_i686"
+          else if isLinux && isx86_64 then
+            "linux_amd64"
+          else if isMinGW then
+            "win32"
+          else
+            lib.warn "unsupported system ${system} for libc‐free version, falling back to generic" "generic"
+        )
+      else
+        "generic";
+
     cppflags = lib.mapAttrsToList (name: value: "-D${name}=\"${value}\"") {
       # set these to empty strings as there are no central directories for
       # pkg-config modules, header files or libraries
@@ -22,6 +41,36 @@ stdenv.mkDerivation (
       PKG_CONFIG_SYSTEM_INCLUDE_PATH = "";
       PKG_CONFIG_SYSTEM_LIBRARY_PATH = "";
     };
+
+    cflags = optionals noLibc (
+      [
+        # expose memset and memcpy in libmemory.c
+        "-DMEMSET"
+        "-DMEMCPY"
+
+        "-fno-builtin"
+        "-fno-asynchronous-unwind-tables"
+        "-Wl,--gc-sections"
+      ]
+      ++ (
+        if hostPlatform.isMinGW then
+          [
+            "-nostartfiles"
+          ]
+        else
+          [
+            "-nostdlib"
+            "-static"
+          ]
+      )
+    );
+
+    libs = optionals noLibc (
+      [ finalAttrs.libmemory ]
+      ++ optionals hostPlatform.isMinGW [
+        "-lkernel32"
+      ]
+    );
   in
   {
     pname = "u-config";
@@ -33,11 +82,22 @@ stdenv.mkDerivation (
       hash = "sha256-wtSGgU99/UJ06Ww5+tmorhBx70It+cvfp2EO8fCH9ew=";
     };
 
+    # provides memset and memcpy for libc‐free versions
+    libmemory = fetchurl {
+      url = "https://raw.githubusercontent.com/skeeto/w64devkit/cbb4c3f0df7e1412559fde793006b14b5d60b21c/src/libmemory.c";
+      hash = "sha256-7hkgWNsFpMk3x+cdOs7UgwOE8NvR8QM/q6izNHNYEjc=";
+    };
+
     nativeBuildInputs = [ installShellFiles ];
     nativeInstallCheckInputs = [ versionCheckHook ];
 
+    hardeningDisable = optionals noLibc [
+      "pie"
+      "stackprotector"
+    ];
+
     dontConfigure = true;
-    doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
+    doCheck = stdenv.buildPlatform.canExecute hostPlatform;
     doInstallCheck = true;
     versionCheckProgram = "${placeholder "out"}/bin/${binary}";
 
@@ -45,7 +105,7 @@ stdenv.mkDerivation (
     buildPhase = ''
       runHook preBuild
 
-      $CC -o ${binary} ${escapeShellArgs cppflags} generic_main.c
+      $CC -o ${binary} ${escapeShellArgs cppflags} ${escapeShellArgs cflags} ${platform}_main.c ${escapeShellArgs libs}
 
       runHook postBuild
     '';
