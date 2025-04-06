e7069a528bc5bde256286a8d9f05b568dfdc9886
Pedro Mendes <pedrohrmendes@proton.me>
2025-04-06 15:07:29 -0300

    iwe: 0.0.30 -> 0.0.31

--- pkgs/by-name/iw/iwe/package.nix
+++ pkgs/by-name/iw/iwe/package.nix
@@ -7,17 +7,17 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "iwe";
-  version = "0.0.30";
+  version = "0.0.31";
 
   src = fetchFromGitHub {
     owner = "iwe-org";
     repo = "iwe";
     tag = "iwe-v${finalAttrs.version}";
-    hash = "sha256-KIhBzPFEPayihDPGy9Zlb59Nf5oOiySKCIS0OClqZnU=";
+    hash = "sha256-5+5PdLxqVz2K6gqTqjBcQO6EKvHy/Mvx3v8MbpZbDSU=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-mADJKDRTixToyol9KEtYWsOy/P70itLAtjh3UbqUz9E=";
+  cargoHash = "sha256-MtS/1imbhsPwLKKxjIju2dsk3RuykujcQYe2PDk4CcI=";
 
   cargoBuildFlags = [
     "--package=iwe"
