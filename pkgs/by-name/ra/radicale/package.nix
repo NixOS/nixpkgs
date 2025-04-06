6dfaea182d024605fe9af6b8d7665f265e6742fd
Robert Schütz <nix@dotlambda.de>
2025-04-05 18:52:01 -0700

    radicale: 3.5.0 -> 3.5.1
    Diff: https://github.com/Kozea/Radicale/compare/refs/tags/v3.5.0...v3.5.1
    
    Changelog: https://github.com/Kozea/Radicale/blob/v3.5.1/CHANGELOG.md

--- pkgs/by-name/ra/radicale/package.nix
+++ pkgs/by-name/ra/radicale/package.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "radicale";
-  version = "3.5.0";
+  version = "3.5.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Kozea";
     repo = "Radicale";
     tag = "v${version}";
-    hash = "sha256-OWOCwioW0TPVk6vIkfVuAaYJdIWJ6Do4NcYWR/b3sQs=";
+    hash = "sha256-CM4ljD2fXwQIiJW135G9cIEO0YNmhbS0Cwiv0EU+Bsk=";
   };
 
   build-system = with python3.pkgs; [
@@ -28,7 +28,7 @@ python3.pkgs.buildPythonApplication rec {
       passlib
       vobject
       pika
-      python-dateutil
+      requests
       pytz # https://github.com/Kozea/Radicale/issues/816
       ldap3
     ]
