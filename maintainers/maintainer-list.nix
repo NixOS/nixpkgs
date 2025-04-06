fee9d68c3e1b59e7f95bc334520d60eca8cc7462
nobbele <realnobbele@gmail.com>
2025-04-06 16:17:05 +0200

    maintainers: add nobbele

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17296,6 +17296,12 @@
     github = "noahgitsham";
     githubId = 73707948;
   };
+  nobbele = {
+    name = "nobbele";
+    email = "realnobbele@gmail.com";
+    github = "nobbele";
+    githubId = 17962514;
+  };
   nobbz = {
     name = "Norbert Melzer";
     email = "timmelzer+nixpkgs@gmail.com";
