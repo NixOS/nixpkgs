f32e350b857bf65641ee9559b8156db6929a556b
Guy Chronister <guychronister@outlook.com>
2025-04-06 11:21:35 -0500

    honeycomb: Migrate to by-name
    Fix scripts
    
    Fix formatting.
    
    run nixfmt on scripts.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19551,12 +19551,6 @@ with pkgs;
     inherit (plasma5Packages) breeze-icons;
   };
 
-  honeymarker = callPackage ../servers/tracing/honeycomb/honeymarker { };
-
-  honeytail = callPackage ../servers/tracing/honeycomb/honeytail { };
-
-  honeyvent = callPackage ../servers/tracing/honeycomb/honeyvent { };
-
   swift-corelibs-libdispatch = swiftPackages.Dispatch;
 
   aitrack = libsForQt5.callPackage ../applications/misc/aitrack { };
