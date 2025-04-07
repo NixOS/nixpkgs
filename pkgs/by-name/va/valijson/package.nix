7b5ddb765a1d0c6b5cb4001f4a68191df5b72509
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-07 12:41:43 +0000

    valijson: 1.0.4 -> 1.0.5

--- pkgs/by-name/va/valijson/package.nix
+++ pkgs/by-name/va/valijson/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "valijson";
-  version = "1.0.4";
+  version = "1.0.5";
 
   src = fetchFromGitHub {
     owner = "tristanpenman";
     repo = "valijson";
     rev = "v${version}";
-    hash = "sha256-nIXcS8PMKoo8D616uUZ+GdIbkGb3VGSlCvg7QqZtz20=";
+    hash = "sha256-f/pYJTvtSXtAmVbKWQfcz/jGSdj7Yt2HNvlCFHx871Q=";
   };
 
   nativeBuildInputs = [
