d940d4a0b185ad3ce6458b11bd3fa7df60c43b36
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-07 10:20:18 +0200

    checkov: 3.2.397 -> 3.2.399
    Diff: https://github.com/bridgecrewio/checkov/compare/refs/tags/3.2.397...3.2.399
    
    Changelog: https://github.com/bridgecrewio/checkov/releases/tag/3.2.399

--- pkgs/by-name/ch/checkov/package.nix
+++ pkgs/by-name/ch/checkov/package.nix
@@ -25,14 +25,14 @@ with py.pkgs;
 
 python3.pkgs.buildPythonApplication rec {
   pname = "checkov";
-  version = "3.2.397";
+  version = "3.2.399";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bridgecrewio";
     repo = "checkov";
     tag = version;
-    hash = "sha256-TFAieHFWM/gQqZyLMKqmGjvTUMPwXbn6LzOBGkSBqk0=";
+    hash = "sha256-/ssPTHHW3AeQRjIDdBBZe6Jnoj/UvQXiLYFhuOJZI5o=";
   };
 
   pythonRelaxDeps = [
