e642e887098837b95faa021ffba62e9f4bde913d
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-04-07 13:20:16 +0300

    jd-diff-patch: 2.1.2 -> 2.2.2
    Upstream does not ship a vendor directory anymore and moved the v2
    sources into a subdirectory.

--- pkgs/by-name/jd/jd-diff-patch/package.nix
+++ pkgs/by-name/jd/jd-diff-patch/package.nix
@@ -6,14 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "jd-diff-patch";
-  version = "2.1.2";
+  version = "2.2.2";
 
   src = fetchFromGitHub {
     owner = "josephburnett";
     repo = "jd";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-chCxbbRZEE29KVnTQWID889kJ2H4qJGVL+vsxzr6VtA=";
+    hash = "sha256-sA3NzCl9dR6cZnZNFa7Sqb1KUPSWA1h8ReZqR+SRjgk";
   };
+
+  sourceRoot = "${finalAttrs.src.name}/v2";
 
   # not including web ui
   excludedPackages = [
@@ -21,7 +23,7 @@ buildGoModule (finalAttrs: {
     "pack"
   ];
 
-  vendorHash = null;
+  vendorHash = "sha256-Ol+9YwtJ5P6au1aW2ss9mrU9l5G3iBviX5q1qC0K+vc=";
 
   meta = {
     description = "Commandline utility and Go library for diffing and patching JSON values";
