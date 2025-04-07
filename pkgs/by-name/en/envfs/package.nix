16ee1d95a92088538a9102a539ae19e5da61a639
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-04-07 21:43:15 +0530

    envfs: cleanup, add nix-update-script
    Move to finalAttrs, remove with lib;
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- pkgs/by-name/en/envfs/package.nix
+++ pkgs/by-name/en/envfs/package.nix
@@ -1,34 +1,42 @@
 {
-  rustPlatform,
   lib,
   fetchFromGitHub,
+  rustPlatform,
   nixosTests,
+  nix-update-script,
 }:
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "envfs";
   version = "1.1.0";
+
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = "envfs";
-    rev = version;
+    rev = finalAttrs.version;
     hash = "sha256-bpATdm/lB+zomPYGCxA7omWK/SKPIaqr94J+fjMaXfE=";
   };
+
   useFetchCargoVendor = true;
   cargoHash = "sha256-nMUdAFRHJZDwvLASBVykzzkwk3HxslDehqqm1U99qYg=";
-
-  passthru.tests = {
-    envfs = nixosTests.envfs;
-  };
 
   postInstall = ''
     ln -s envfs $out/bin/mount.envfs
     ln -s envfs $out/bin/mount.fuse.envfs
   '';
-  meta = with lib; {
+
+  passthru = {
+    tests = {
+      envfs = nixosTests.envfs;
+    };
+
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
     description = "Fuse filesystem that returns symlinks to executables based on the PATH of the requesting process";
     homepage = "https://github.com/Mic92/envfs";
-    license = licenses.mit;
-    maintainers = with maintainers; [ mic92 ];
-    platforms = platforms.linux;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ mic92 ];
+    platforms = lib.platforms.linux;
   };
-}
+})
