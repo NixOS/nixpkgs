3fbfc28b9ae7a1659c158d5dbaf61fde09358245
h7x4 <h7x4@nani.wtf>
2025-04-07 13:33:08 +0200

    libreoffice: lib.flatten map -> lib.concatMap

--- pkgs/applications/office/libreoffice/default.nix
+++ pkgs/applications/office/libreoffice/default.nix
@@ -166,8 +166,8 @@ assert builtins.elem variant [
 
 let
   inherit (lib)
-    flatten
     flip
+    concatMap
     concatMapStrings
     concatStringsSep
     getDev
@@ -249,8 +249,8 @@ let
   # See `postPatch` for details
   kdeDeps = symlinkJoin {
     name = "libreoffice-kde-dependencies-${version}";
-    paths = flatten (
-      map
+    paths =
+      concatMap
         (e: [
           (getDev e)
           (getLib e)
@@ -263,8 +263,7 @@ let
           kdePackages.ki18n
           kdePackages.kio
           kdePackages.kwindowsystem
-        ]
-    );
+        ];
   };
   tarballPath = "external/tarballs";
 
