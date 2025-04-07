b5b1c6d6c93f46f3be8d6e39774b9a1786a7a4d5
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-04-07 13:20:03 +0100

    xivlauncher: add gamemode support

--- nixos/modules/programs/xivlauncher.nix
+++ nixos/modules/programs/xivlauncher.nix
@@ -10,10 +10,19 @@ in
 {
   options.programs.xivlauncher = {
     enable = lib.mkEnableOption "Custom launcher for FFXIV";
+
+    enableGameMode = lib.mkOption {
+      type = lib.types.bool;
+      default = config.programs.gamemode.enable;
+      defaultText = lib.literalExpression "config.programs.gamemode.enable";
+      description = ''
+        Whether to enable GameMode to optimise system performance on demand.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.xivlauncher ];
+    environment.systemPackages = [ (pkgs.xivlauncher.override { useGameMode = cfg.enableGameMode; }) ];
   };
 
   meta.maintainers = with lib.maintainers; [
