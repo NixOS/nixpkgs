a31f2d27b23613e2d0f72af537b8c4ae627506df
Stefan Frijters <sfrijters@gmail.com>
2025-04-07 09:58:49 +0200

    python3Packages.desktop-notifier: 6.0.0 -> 6.1.0

--- pkgs/development/python-modules/desktop-notifier/default.nix
+++ pkgs/development/python-modules/desktop-notifier/default.nix
@@ -1,28 +1,29 @@
 {
   lib,
-  buildPythonPackage,
-  fetchFromGitHub,
-  pythonOlder,
   stdenv,
+  buildPythonPackage,
+  pythonOlder,
+  fetchFromGitHub,
+  setuptools,
   bidict,
   packaging,
-  setuptools,
+  typing-extensions,
   dbus-fast,
   rubicon-objc,
 }:
 
 buildPythonPackage rec {
   pname = "desktop-notifier";
-  version = "6.0.0";
+  version = "6.1.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "SamSchott";
     repo = "desktop-notifier";
     tag = "v${version}";
-    hash = "sha256-HynREkiPxv/1y1/ICVwqANIe9tAkIvdpDy4oXxQarec=";
+    hash = "sha256-cSeEekjX9NeoEoe9mDokCanK5zrawyvdHK1c4RJ9kSk=";
   };
 
   build-system = [ setuptools ];
@@ -31,6 +32,7 @@ buildPythonPackage rec {
     [
       bidict
       packaging
+      typing-extensions
     ]
     ++ lib.optionals stdenv.hostPlatform.isLinux [ dbus-fast ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [ rubicon-objc ];
