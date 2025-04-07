844aa706e90b499579663962bf19fd2bbdd836de
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-04-07 13:19:38 +0300

    jd-diff-patch: modernize

--- pkgs/by-name/jd/jd-diff-patch/package.nix
+++ pkgs/by-name/jd/jd-diff-patch/package.nix
@@ -4,15 +4,15 @@
   fetchFromGitHub,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "jd-diff-patch";
   version = "2.1.2";
 
   src = fetchFromGitHub {
     owner = "josephburnett";
     repo = "jd";
-    rev = "v${version}";
-    sha256 = "sha256-chCxbbRZEE29KVnTQWID889kJ2H4qJGVL+vsxzr6VtA=";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-chCxbbRZEE29KVnTQWID889kJ2H4qJGVL+vsxzr6VtA=";
   };
 
   # not including web ui
@@ -23,13 +23,13 @@ buildGoModule rec {
 
   vendorHash = null;
 
-  meta = with lib; {
+  meta = {
     description = "Commandline utility and Go library for diffing and patching JSON values";
     homepage = "https://github.com/josephburnett/jd";
-    license = licenses.mit;
-    maintainers = with maintainers; [
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
       bryanasdev000
     ];
     mainProgram = "jd";
   };
-}
+})
