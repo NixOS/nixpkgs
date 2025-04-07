7daee97ab78e456095c9a58c7cf76b8432bdd23f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-04-06 21:09:46 -0400

    loadwatch: fix build
    Fix a missing <string.h> include; regenerate configure script to use a
    compliant conftest program with explicit main() return type.
    
    Also, modernized the derivation and updated the git repo url.
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- pkgs/by-name/lo/loadwatch/package.nix
+++ pkgs/by-name/lo/loadwatch/package.nix
@@ -2,27 +2,40 @@
   lib,
   stdenv,
   fetchgit,
+  autoreconfHook,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "loadwatch";
   version = "1.1-1-g6d2544c";
 
   src = fetchgit {
-    url = "git://woffs.de/git/fd/loadwatch.git";
-    sha256 = "1bhw5ywvhyb6snidsnllfpdi1migy73wg2gchhsfbcpm8aaz9c9b";
+    url = "https://git.sr.ht/~woffs/loadwatch";
+    hash = "sha256-K7H0lUL1suU0hOyJx8fxL9YQ23WUWt2i1WZ5uLkvHK4=";
     rev = "6d2544c0caaa8a64bbafc3f851e06b8056c30e6e";
   };
+
+  patches = [
+    # autoheader fails when template is not defined
+    ./missing-kstat-template-autoheader.patch
+    # implicit <string.h> not allowed in c99 mode
+    ./fix-c99-failure.patch
+  ];
+
+  nativeBuildInputs = [
+    # conftest program uses main() with implicit return type
+    autoreconfHook
+  ];
 
   installPhase = ''
     mkdir -p $out/bin
     install loadwatch lw-ctl $out/bin
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Run a program using only idle cycles";
-    license = licenses.gpl2Only;
-    maintainers = with maintainers; [ woffs ];
-    platforms = platforms.all;
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [ woffs ];
+    platforms = lib.platforms.all;
   };
-}
+})
