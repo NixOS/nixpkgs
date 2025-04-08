952b8e985491f84219931bfb94b75f43899506c5
transcaffeine <transcaffeine@finally.coffee>
2025-04-08 21:18:14 +0200

    element-desktop: 1.11.96 -> 1.11.97
    Release notes: https://github.com/element-hq/element-desktop/releases/tag/v1.11.97
    Full changelog: https://github.com/element-hq/element-desktop/compare/v1.11.96...v1.11.97

--- pkgs/by-name/el/element-desktop/element-desktop-pin.nix
+++ pkgs/by-name/el/element-desktop/element-desktop-pin.nix
@@ -1,7 +1,7 @@
 {
-  "version" = "1.11.96";
+  "version" = "1.11.97";
   "hashes" = {
-    "desktopSrcHash" = "sha256-oTU/Pvl4gBp69OrUrXEYXupl0WphsEWt32sB4v6T+gA=";
-    "desktopYarnHash" = "sha256-zMdSA/CkMDXirWZ2uCPTgZ5iErV7rGyR+xcLh9sPDA8=";
+    "desktopSrcHash" = "sha256-4ea5C1I/bdGsy4CxsNB1GWL8RQnJpHr/MbIOB5Z+gFA=";
+    "desktopYarnHash" = "sha256-HI/aRf9pGao8m8z3oA0wdbjDQth3zijJeskV2D+KsgM=";
   };
 }
