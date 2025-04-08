34b52464cc8f90c774ec18df016ba845add3da33
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-08 01:25:24 +0000

    jql: 8.0.4 -> 8.0.5

--- pkgs/by-name/jq/jql/package.nix
+++ pkgs/by-name/jq/jql/package.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "jql";
-  version = "8.0.4";
+  version = "8.0.5";
 
   src = fetchFromGitHub {
     owner = "yamafaktory";
     repo = "jql";
     rev = "jql-v${version}";
-    hash = "sha256-J+Zqmfev2DyD0SLFGaI0egVgmEC+a2nqBrNDGX4zNnE=";
+    hash = "sha256-0sQEC2kUnuuKp73DJsNBFB0VL0rkBkudmr7ZQpS1v04=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-tMxy0bi3518VjzJuy4Agpq+UydMEyJRqavX5kIsBYjY=";
+  cargoHash = "sha256-10eM7tczFoQVYagyP1btsCp4PHm+zRoh2oAEVVxsROA=";
 
   meta = with lib; {
     description = "JSON Query Language CLI tool built with Rust";
