a65df6e64efce27c42573aaae2acf0b6d35b6019
GGG <gggkiller2@gmail.com>
2025-04-08 17:01:07 -0300

    vscode-extensions.ms-dotnettools.csdevkit: 1.16.6 -> 1.18.23

--- pkgs/applications/editors/vscode/extensions/ms-dotnettools.csdevkit/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-dotnettools.csdevkit/default.nix
@@ -10,30 +10,33 @@
   libxml2,
 }:
 let
-  inherit (stdenv.hostPlatform) system;
-  inherit (vscode-utils) buildVscodeMarketplaceExtension;
-
-  lockfile = lib.importJSON ./lockfile.json;
-  extInfo =
-    (arch: {
-      inherit arch;
-      inherit (lockfile.${arch}) hash binaries;
-    })
-      (
-        {
-          x86_64-linux = "linux-x64";
-          aarch64-linux = "linux-arm64";
-          x86_64-darwin = "darwin-x64";
-          aarch64-darwin = "darwin-arm64";
-        }
-        .${system} or (throw "Unsupported system: ${system}")
-      );
+  extInfo = (
+    {
+      x86_64-linux = {
+        arch = "linux-x64";
+        hash = "sha256-5B2LAV5azD1mGDFWnxIOYjyyJMXxqxyL5qP0zs6jWgs=";
+      };
+      aarch64-linux = {
+        arch = "linux-arm64";
+        hash = "sha256-6tmHa8XbuNLheUzY8pr6oEJlkUJpeCeT3O7xfo2Iqls=";
+      };
+      x86_64-darwin = {
+        arch = "darwin-x64";
+        hash = "sha256-KRJ148+N3Hzht1GgnO0s5Mv27q+onPyZlY9myXw3zjg=";
+      };
+      aarch64-darwin = {
+        arch = "darwin-arm64";
+        hash = "sha256-vmtE6oeLs1B9s69yaOHASdFKznV1p3v+SsJUAJrwUrU=";
+      };
+    }
+    .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}")
+  );
 in
-buildVscodeMarketplaceExtension {
+vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "csdevkit";
     publisher = "ms-dotnettools";
-    inherit (lockfile) version;
+    version = "1.18.23";
     inherit (extInfo) hash arch;
   };
   sourceRoot = "extension"; # This has more than one folder.
@@ -56,9 +59,23 @@ buildVscodeMarketplaceExtension {
 
     substituteInPlace dist/extension.js \
       --replace-fail 'e.extensionPath,"cache"' 'require("os").tmpdir(),"'"$ext_unique_id"'"' \
-      --replace-fail 't.setExecuteBit=async function(e){if("win32"!==process.platform){const t=o.join(e[a.SERVICEHUB_CONTROLLER_COMPONENT_NAME],"Microsoft.ServiceHub.Controller"),r=o.join(e[a.SERVICEHUB_HOST_COMPONENT_NAME],(0,a.getServiceHubHostEntrypointName)()),n=[(0,a.getServerPath)(e),t,r,(0,c.getReliabilityMonitorPath)(e)];await Promise.all(n.map((e=>(0,i.chmod)(e,"0755"))))}}' 't.setExecuteBit=async function(e){}'
+      --replace-fail 't.setExecuteBit=async function(e){if("win32"!==process.platform){const t=i.join(e[a.SERVICEHUB_CONTROLLER_COMPONENT_NAME],"Microsoft.VisualStudio.Code.ServiceController"),r=i.join(e[a.SERVICEHUB_HOST_COMPONENT_NAME],(0,a.getServiceHubHostEntrypointName)()),n=[(0,a.getServerPath)(e),t,r,(0,c.getReliabilityMonitorPath)(e)];await Promise.all(n.map((e=>(0,o.chmod)(e,"0755"))))}}' 't.setExecuteBit=async function(e){}'
+  '';
 
-    chmod +x ${lib.escapeShellArgs extInfo.binaries}
+  preFixup = ''
+    (
+      shopt -s globstar
+      shopt -s dotglob
+      for file in "$out"/**/*; do
+        if [[ ! -f "$file" || "$file" == *.so || "$file" == *.dylib ]] ||
+            (! isELF "$file" && ! isMachO "$file"); then
+            continue
+        fi
+
+        echo Making "$file" executable...
+        chmod +x "$file"
+      done
+    )
   '';
 
   passthru.updateScript = ./update.sh;
