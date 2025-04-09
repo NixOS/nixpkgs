c61b18d674c5bf4a650a84eb9a370495144cf1b2
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-09 07:04:41 +0000

    lock: 1.5.1 -> 1.5.2

--- pkgs/by-name/lo/lock/package.nix
+++ pkgs/by-name/lo/lock/package.nix
@@ -19,13 +19,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lock";
-  version = "1.5.1";
+  version = "1.5.2";
 
   src = fetchFromGitHub {
     owner = "konstantintutsch";
     repo = "Lock";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-HXyU+b4CtTeQJrX0vVKV94twCXJRWZn/dGsjyC8fpc8=";
+    hash = "sha256-FcL1BH2ypzBNXBNBR6oZ95dP6Kt50uOoYj8+2U7iYNg=";
   };
 
   strictDeps = true;
