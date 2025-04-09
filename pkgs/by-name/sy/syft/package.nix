9f87431a2153aa75e69a4d7cfbb2753ecf5f8c25
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-09 18:47:34 +0000

    syft: 1.21.0 -> 1.22.0

--- pkgs/by-name/sy/syft/package.nix
+++ pkgs/by-name/sy/syft/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "syft";
-  version = "1.21.0";
+  version = "1.22.0";
 
   src = fetchFromGitHub {
     owner = "anchore";
     repo = "syft";
     tag = "v${version}";
-    hash = "sha256-48Guq4FBt0J+v1LIZsiTa4+I4zMCyhocMdXLM7FwvoA=";
+    hash = "sha256-gaOpPgFwnGaDWqJqgZSCZr8kNBhJsb1D3nfaBGwPeG4=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -28,7 +28,7 @@ buildGoModule rec {
   # hash mismatch with darwin
   proxyVendor = true;
 
-  vendorHash = "sha256-0mqVQooeRvkIfIxv2SzAYkEjSDR1ngzjzrrylJj6hK4=";
+  vendorHash = "sha256-cYZ/7bZHQcKYy9Jg/RrlwZoqOHL3uLAn60JFC3TsVnE=";
 
   nativeBuildInputs = [ installShellFiles ];
 
