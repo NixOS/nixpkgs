2a5e791943c0e9f44eada6b16bf90a8a90550c83
Robert Schütz <nix@dotlambda.de>
2025-04-09 16:22:12 -0700

    python313Packages.pymodbus-repl: init at 2.0.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12637,6 +12637,8 @@ self: super: with self; {
 
   pymodbus = callPackage ../development/python-modules/pymodbus { };
 
+  pymodbus-repl = callPackage ../development/python-modules/pymodbus-repl { };
+
   pymodes = callPackage ../development/python-modules/pymodes { };
 
   pymonctl = callPackage ../development/python-modules/pymonctl { };
