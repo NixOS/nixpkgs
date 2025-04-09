b779a72fa79066275d532e0c76de3e58505ba5d3
GueLaKais <koroyeldiores@gmail.com>
2025-04-09 22:47:25 +0200

    python312Packages.colcon-ros-domain-id-coordinator: init at 0.2.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2681,6 +2681,10 @@ self: super: with self; {
 
   colcon-notification = callPackage ../development/python-modules/colcon-notification { };
 
+  colcon-ros-domain-id-coordinator =
+    callPackage ../development/python-modules/colcon-ros-domain-id-coordinator
+      { };
+
   collections-extended = callPackage ../development/python-modules/collections-extended { };
 
   collidoscope = callPackage ../development/python-modules/collidoscope { };
