85dde6b3a1e4f31e1301a21793fbd53f9c340648
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-04-09 13:01:05 +0200

    hpp-pinocchio: patch for pinocchio v3.5.0

--- pkgs/by-name/hp/hpp-pinocchio/package.nix
+++ pkgs/by-name/hp/hpp-pinocchio/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   fetchFromGitHub,
+  fetchpatch,
   stdenv,
 
   # nativeBuildInputs
@@ -25,6 +26,28 @@ stdenv.mkDerivation (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-SDH129hkDTfLBkKBHkqxGng9G3Wd7dbLCarHJhOFryc=";
   };
+
+  # Update for pinocchio v3.5.0
+  # ref. https://github.com/humanoid-path-planner/hpp-pinocchio/pull/234
+  # These were merged upstream and can be removed on next release
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/humanoid-path-planner/hpp-pinocchio/commit/e542606c5a0f4f7b3080c2d12b70b37301efade4.patch";
+      hash = "sha256-HC+Vd+ip+zq7ogFXfYLmKDHJyPwFHr5uIH4KAI2HMFE=";
+    })
+    (fetchpatch {
+      url = "https://github.com/humanoid-path-planner/hpp-pinocchio/commit/26f5e996f2be210da47111af5f5a24681313d94b.patch";
+      hash = "sha256-npsr3bNsSzqmZ32xmUJh2G8jnlH8Jzkqvtt3v1u9EuA=";
+    })
+    (fetchpatch {
+      url = "https://github.com/humanoid-path-planner/hpp-pinocchio/commit/acccdc88414966ce6284792212cf3b4262a3dc8c.patch";
+      hash = "sha256-/oVxJlIBc1nRdsEkWVSehiEOmoz1QusiYXT2Uc40m0w=";
+    })
+    (fetchpatch {
+      url = "https://github.com/humanoid-path-planner/hpp-pinocchio/commit/eae6838a3802533ae82529a0308c59237d17737b.patch";
+      hash = "sha256-i+i5k+CJUCHOfHmD7edmBZtykTrg7iq1/b1evYGrC+A=";
+    })
+  ];
 
   strictDeps = true;
 
