b2b5f8be2832ba8f705c59553845b3ba33feaf08
Wolfgang Walther <walther@technowledgy.de>
2025-04-09 15:11:58 +0200

    postgres-websockets: init at 0.11.2.2
    The package itself is currently broken, but will be unbroken with the
    next merge of haskell-updates.

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -480,6 +480,9 @@ builtins.intersectAttrs super {
   hasql-pool = dontCheck super.hasql-pool;
   hasql-transaction = dontCheck super.hasql-transaction;
 
+  # Avoid compiling twice by providing executable as a separate output (with small closure size),
+  postgres-websockets = enableSeparateBinOutput super.postgres-websockets;
+
   # Test suite requires a running postgresql server,
   # avoid compiling twice by providing executable as a separate output (with small closure size),
   # generate shell completion
