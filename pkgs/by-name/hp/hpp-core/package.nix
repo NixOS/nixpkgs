cb0d835e09504f1c357fe72bfb7d871d6663a999
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-04-09 13:26:39 +0200

    hpp-core: patch for pinocchio v3.5.0

--- pkgs/by-name/hp/hpp-core/package.nix
+++ pkgs/by-name/hp/hpp-core/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   fetchFromGitHub,
+  fetchpatch,
   stdenv,
 
   # nativeBuildInputs
@@ -23,6 +24,21 @@ stdenv.mkDerivation (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash = "sha256-W4gUjvzULxUqG51zBV3P9u2XWNM7FQDK4pk/DMuWb2k=";
   };
+
+  # Fix for pinocchio v3.5.0
+  # ref. https://github.com/humanoid-path-planner/hpp-core/pull/364
+  # and https://github.com/humanoid-path-planner/hpp-core/pull/378
+  # this was merged upstream and can be removed on next release
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/humanoid-path-planner/hpp-core/commit/919353e3b6a10261a0e520d4db4760553062f17f.patch";
+      hash = "sha256-pHaPUK+asOocGeltENeyEwx93TTKJPX0UCWWhMcFUFc=";
+    })
+    (fetchpatch {
+      url = "https://github.com/humanoid-path-planner/hpp-core/commit/5a518ce3bc7419f30499a9e6af3391261c0b6ac2.patch";
+      hash = "sha256-ixxVhovj74LpAn6Yms338agLgdyOg1aRZcsiY9KpKYE=";
+    })
+  ];
 
   strictDeps = true;
 
