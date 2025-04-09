1b6b33df221b2f3dfd1cbcef68aa55a671a760f6
Defelo <mail@defelo.de>
2025-04-09 16:16:31 +0200

    whoami: add defelo as maintainer

--- pkgs/by-name/wh/whoami/package.nix
+++ pkgs/by-name/wh/whoami/package.nix
@@ -30,6 +30,9 @@ buildGoModule (finalAttrs: {
     homepage = "https://github.com/traefik/whoami";
     changelog = "https://github.com/traefik/whoami/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ dvcorreia ];
+    maintainers = with lib.maintainers; [
+      dvcorreia
+      defelo
+    ];
   };
 })
