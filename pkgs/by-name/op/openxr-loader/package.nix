dbe728c887466998463113aae3e2910e64fb8899
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-09 01:48:12 +0000

    openxr-loader: 1.1.46 -> 1.1.47

--- pkgs/by-name/op/openxr-loader/package.nix
+++ pkgs/by-name/op/openxr-loader/package.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation rec {
   pname = "openxr-loader";
-  version = "1.1.46";
+  version = "1.1.47";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "OpenXR-SDK-Source";
     tag = "release-${version}";
-    hash = "sha256-C2PGPIDA4C/d37jjXLO+2lqD5VSV3skSqraRiT8RT+c=";
+    hash = "sha256-7VW99dtE7gz0Y9pKyAdyeKHL6zgk5KvA8jPfgG1O5sc=";
   };
 
   nativeBuildInputs = [
