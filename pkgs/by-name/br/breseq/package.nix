2d3aa8872fd20007251c946ad2f21fb469d9d185
croots <38054423+croots@users.noreply.github.com>
2025-04-09 19:47:13 -0500

    Fix formatting

--- pkgs/by-name/br/breseq/package.nix
+++ pkgs/by-name/br/breseq/package.nix
@@ -1,33 +1,35 @@
-{ stdenv, lib, fetchFromGitHub, callPackage,
-libz,
-autoconf,
-automake,
-libtool,
-perl,
-R,
-bowtie2}:
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  callPackage,
+  libz,
+  autoconf,
+  automake,
+  libtool,
+  perl,
+  R,
+  bowtie2,
+}:
 
 stdenv.mkDerivation rec {
-    name = "breseq-${version}";
-    version = "0.39.0";
+  name = "breseq-${version}";
+  version = "0.39.0";
 
-    src = fetchFromGitHub {
-        owner = "barricklab";
-        repo = "breseq";
-        rev = "v${version}";
-        sha256 = "DsDX2oGn7Ex50Wnp1phJjCziCzZIeeZOHriUGJbejsk=";
-    };
+  src = fetchFromGitHub {
+    owner = "barricklab";
+    repo = "breseq";
+    rev = "v${version}";
+    sha256 = "DsDX2oGn7Ex50Wnp1phJjCziCzZIeeZOHriUGJbejsk=";
+  };
 
-    buildInputs = [
-        perl
-        libz
-        autoconf
-        automake
-        libtool
-    ];
-
-  REQUIRED_R = R.outPath;
-  REQUIRED_BOWTIE = bowtie2.outPath;
+  buildInputs = [
+    perl
+    libz
+    autoconf
+    automake
+    libtool
+  ];
 
   buildPhase = ''
     ./bootstrap.sh
@@ -42,13 +44,13 @@ stdenv.mkDerivation rec {
     mkdir $out/bin
     touch $out/bin/breseq
     touch $out/bin/gdtools
-    echo -e "#! /usr/bin/bash\n\nexport PATH=$REQUIRED_R/bin:$REQUIRED_BOWTIE/bin:\$PATH\n\nexec $out/unwrapped/bin/breseq \$@" >> $out/bin/breseq
-    echo -e "#! /usr/bin/bash\n\nexport PATH=$REQUIRED_R/bin:$REQUIRED_BOWTIE/bin:\$PATH\n\nexec $out/unwrapped/bin/gdtools \$@" >> $out/bin/gdtools
+    echo -e "#! /usr/bin/bash\n\nexport PATH=${R.outPath}/bin:${bowtie2.outPath}/bin:\$PATH\n\nexec $out/unwrapped/bin/breseq \$@" >> $out/bin/breseq
+    echo -e "#! /usr/bin/bash\n\nexport PATH=${R.outPath}/bin:${bowtie2.outPath}/bin:\$PATH\n\nexec $out/unwrapped/bin/gdtools \$@" >> $out/bin/gdtools
     chmod +x $out/bin/breseq
     chmod +x $out/bin/gdtools
     # Clean up dependency tree
     find $out -type f -exec patchelf --shrink-rpath '{}' \; -exec strip '{}' \; 2>/dev/null
-    # Copy over tests and license
+    # Copy over tests (incl necessary datasets) and license
     cp LICENSE $out/license
     mkdir $out/tests
     mkdir $out/tests/data
@@ -67,8 +69,11 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     description = "A computational pipeline for finding mutations relative to a reference sequence in short-read DNA re-sequencing data.";
     homepage = "https://github.com/barricklab/breseq";
-    license = with lib.licenses; [ gpl2 gpl3 ];
+    license = with lib.licenses; [
+      gpl2
+      gpl3
+    ];
     maintainers = with maintainers; [ croots ];
     platforms = platforms.all;
-    };
+  };
 }
--- pkgs/by-name/br/breseq/tests/breseq.nix
+++ pkgs/by-name/br/breseq/tests/breseq.nix
@@ -1,27 +1,30 @@
-{ runCommand, phoronix-test-suite, breseq}:
+{
+  runCommand,
+  phoronix-test-suite,
+  breseq,
+}:
 
 let
   inherit (phoronix-test-suite) pname version;
 in
 
-runCommand "${pname}-tests" { meta.timeout = 60; }
-  ''
-    ${phoronix-test-suite}/bin/phoronix-test-suite enterprise-setup >/dev/null
-    if [[ `${phoronix-test-suite}/bin/phoronix-test-suite version` != *"${version}"*  ]]; then
-      echo "Error: program version does not match package version"
-      exit 1
-    fi
+runCommand "${pname}-tests" { meta.timeout = 60; } ''
+  ${phoronix-test-suite}/bin/phoronix-test-suite enterprise-setup >/dev/null
+  if [[ `${phoronix-test-suite}/bin/phoronix-test-suite version` != *"${version}"*  ]]; then
+    echo "Error: program version does not match package version"
+    exit 1
+  fi
 
-    export TESTBINPREFIX=${breseq}/bin
-    export TESTDIR=$(mktemp -d)
-    trap "rm -rf \"$TESTDIR\"" EXIT
-    cp ${breseq}/tests $TESTDIR/breseqtests -r
-    chmod -R +w $TESTDIR/breseqtests
-    echo "Testing breseq - breseq executable"
-    output=$($TESTDIR/breseqtests/tmv_plasmid_circular_deletion/testcmd.sh 2>&1) || {
-      echo "$output"
-      echo "Error testing breseq - breseq executable"
-      exit 1
-    }
-    touch $out
-  ''
+  export TESTBINPREFIX=${breseq}/bin
+  export TESTDIR=$(mktemp -d)
+  trap "rm -rf \"$TESTDIR\"" EXIT
+  cp ${breseq}/tests $TESTDIR/breseqtests -r
+  chmod -R +w $TESTDIR/breseqtests
+  echo "Testing breseq - breseq executable"
+  output=$($TESTDIR/breseqtests/tmv_plasmid_circular_deletion/testcmd.sh 2>&1) || {
+    echo "$output"
+    echo "Error testing breseq - breseq executable"
+    exit 1
+  }
+  touch $out
+''
