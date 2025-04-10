5c62091036eb8490b735b93fc2841df548a20c09
Tristan Ross <tristan.ross@midstall.com>
2025-04-09 23:01:35 -0700

    findup: mark as broken

--- pkgs/by-name/fi/findup/package.nix
+++ pkgs/by-name/fi/findup/package.nix
@@ -3,11 +3,8 @@
   stdenv,
   fetchFromGitHub,
   testers,
-  zig_0_11,
+  zig,
 }:
-let
-  zig = zig_0_11;
-in
 stdenv.mkDerivation (finalAttrs: {
   pname = "findup";
   version = "1.1.2";
@@ -24,6 +21,8 @@ stdenv.mkDerivation (finalAttrs: {
   passthru.tests.version = testers.testVersion { package = finalAttrs.finalPackage; };
 
   meta = {
+    # Doesn't support zig 0.12 or newer, last commit was 2 years ago.
+    broken = lib.versionAtLeast zig.version "0.12";
     homepage = "https://github.com/booniepepper/findup";
     description = "Search parent directories for sentinel files";
     license = lib.licenses.mit;
