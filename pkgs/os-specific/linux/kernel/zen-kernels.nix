9909bf4720e4a00f9c88f7e5f62e60184d7fa71a
Katherine Jamison <axertheaxe@proton.me>
2025-04-10 16:49:12 -0600

    linuxKernel.kernels.linux_zen: 6.14.1-zen1 -> 6.14.3-zen1
    (cherry picked from commit 1eaa411e3dd3b96da2407e5be0ddfc69cc9c5f39)

--- pkgs/os-specific/linux/kernel/zen-kernels.nix
+++ pkgs/os-specific/linux/kernel/zen-kernels.nix
@@ -12,9 +12,9 @@ let
   variants = {
     # ./update-zen.py zen
     zen = {
-      version = "6.14.1"; # zen
+      version = "6.14.3"; # zen
       suffix = "zen1"; # zen
-      sha256 = "07fif9yj33lidp7dp8r66bsqyyh6fckjb3nhxynaikgb17hx9w5b"; # zen
+      sha256 = "17r4gmxbgs0aizlp35pdq515ag50zc3q20fxapbya4yp5qs6ncz0"; # zen
       isLqx = false;
     };
     # ./update-zen.py lqx
