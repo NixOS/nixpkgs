d318f0074d20303c6b4fcfb8454c80d3eab1dc3a
Lunitur <karlo.puselj@gmail.com>
2025-04-11 19:46:08 +0200

    mfcl3730cdncupswrapper: init at 1.0.2-0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19129,6 +19129,7 @@ with pkgs;
   mfcl3770cdwcupswrapper = (callPackage ../misc/cups/drivers/brother/mfcl3770cdw { }).cupswrapper;
 
   mfcl3730cdnlpr = (callPackage ../misc/cups/drivers/brother/mfcl3730cdn { }).driver;
+  mfcl3730cdncupswrapper = (callPackage ../misc/cups/drivers/brother/mfcl3730cdn { }).cupswrapper;
 
   samsung-unified-linux-driver_1_00_37 = callPackage ../misc/cups/drivers/samsung/1.00.37.nix { };
   samsung-unified-linux-driver_4_01_17 = callPackage ../misc/cups/drivers/samsung/4.01.17.nix { };
