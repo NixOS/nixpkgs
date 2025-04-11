d05cf49610be237b07782c4c39742a9f39a4aec0
nicoo <nicoo@mur.at>
2025-04-11 13:30:15 +0000

    cloud-provider-kind: init at 0.6.0

---
+++ pkgs/by-name/cl/cloud-provider-kind/package.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  gitUpdater,
+}:
+buildGoModule rec {
+  pname = "cloud-provider-kind";
+  version = "0.6.0";
+
+  src = fetchFromGitHub {
+    owner = "kubernetes-sigs";
+    repo = "cloud-provider-kind";
+    tag = "v${version}";
+    hash = "sha256-6HdP6/uUCtLyZ7vjFGB2NLqe73v/yolRTUE5s/KyIIk=";
+  };
+  passthru.updateScript = gitUpdater { rev-prefix = "v"; };
+
+  vendorHash = null;
+
+  meta = {
+    description = "Load Balancer implementation for Kubernetes-in-Docker";
+    homepage = "https://github.com/kubernetes-sigs/cloud-provider-kind";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ nicoo ];
+    mainProgram = "cloud-provider-kind";
+  };
+}
