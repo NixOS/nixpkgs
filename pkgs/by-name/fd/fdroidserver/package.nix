e07ece7c52808867fb14fd4006a079d368a97d9c
linsui <36977733+linsui@users.noreply.github.com>
2025-04-12 22:10:56 +0800

    fdroidserver: 2.3.5 -> 2.4.0

--- pkgs/by-name/fd/fdroidserver/package.nix
+++ pkgs/by-name/fd/fdroidserver/package.nix
@@ -10,7 +10,7 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "fdroidserver";
-  version = "2.3.5";
+  version = "2.4.0";
 
   pyproject = true;
 
@@ -18,7 +18,7 @@ python3Packages.buildPythonApplication rec {
     owner = "fdroid";
     repo = "fdroidserver";
     tag = version;
-    hash = "sha256-ESSjC44tdWpmtB+VhEDnUTQz+ZJVQ4jLBjj+0hMSAQA=";
+    hash = "sha256-PQZz3dyX6vCS0axHfSINMMX5ETdVs44K9XjR87gtd3s=";
   };
 
   pythonRelaxDeps = [
