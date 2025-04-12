d5771c73ba9daaa6d1ddb7de9ba2a97f59481c98
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-12 11:04:55 +0200

    prowler: 5.4.3 -> 5.4.4
    Diff: https://github.com/prowler-cloud/prowler/compare/refs/tags/5.4.3...5.4.4
    
    Changelog: https://github.com/prowler-cloud/prowler/releases/tag/5.4.4

--- pkgs/by-name/pr/prowler/package.nix
+++ pkgs/by-name/pr/prowler/package.nix
@@ -21,14 +21,14 @@ let
 in
 py.pkgs.buildPythonApplication rec {
   pname = "prowler";
-  version = "5.4.3";
+  version = "5.4.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "prowler-cloud";
     repo = "prowler";
     tag = version;
-    hash = "sha256-Rg1r1G1INmDK5bN4NIrs51CTnxNiwOOSoTFC3AlEUoI=";
+    hash = "sha256-OuPhP4PMBqs3dAwY/Ln0Z4hwO3lPaJm4eYcl7AHy9vM=";
   };
 
   pythonRelaxDeps = true;
