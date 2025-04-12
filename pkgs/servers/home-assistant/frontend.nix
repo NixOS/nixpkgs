b059d3afc7af459ae145bd5705ae9ef26a94ad03
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-12 17:03:50 +0200

    home-assistant: 2025.4.1 -> 2025.4.2
    https://github.com/home-assistant/core/releases/tag/2025.4.2

--- pkgs/servers/home-assistant/frontend.nix
+++ pkgs/servers/home-assistant/frontend.nix
@@ -8,7 +8,7 @@ buildPythonPackage rec {
   # the frontend version corresponding to a specific home-assistant version can be found here
   # https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/frontend/manifest.json
   pname = "home-assistant-frontend";
-  version = "20250404.0";
+  version = "20250411.0";
   format = "wheel";
 
   src = fetchPypi {
@@ -16,7 +16,7 @@ buildPythonPackage rec {
     pname = "home_assistant_frontend";
     dist = "py3";
     python = "py3";
-    hash = "sha256-og/8P3wh5ztjpQJhI+pF/YHKeNjz/AOoCzxGQRl0Aac=";
+    hash = "sha256-oVvYSEvkJ2bvmoC4DHEUi4EE9iOBn3dJyUnyObWSx1k=";
   };
 
   # there is nothing to strip in this package
