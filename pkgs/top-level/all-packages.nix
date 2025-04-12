f2e08abb07a994af6bcd621b85d762088bc04320
Pavol Rusnak <pavol@rusnak.io>
2025-04-12 21:34:00 +0200

    klipper-firmware: use the latest gcc-arm-embedded

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2541,9 +2541,7 @@ with pkgs;
 
   klipper = callPackage ../servers/klipper { };
 
-  klipper-firmware = callPackage ../servers/klipper/klipper-firmware.nix {
-    gcc-arm-embedded = gcc-arm-embedded-13;
-  };
+  klipper-firmware = callPackage ../servers/klipper/klipper-firmware.nix { };
 
   klipper-flash = callPackage ../servers/klipper/klipper-flash.nix { };
 
