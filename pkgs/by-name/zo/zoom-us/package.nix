5941b8d88acd1e2fd61344b538ec871b840e5bfc
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2025-04-12 16:21:30 +0200

    zoom-us: drop Darwin support

--- pkgs/by-name/zo/zoom-us/package.nix
+++ pkgs/by-name/zo/zoom-us/package.nix
@@ -2,9 +2,6 @@
   stdenv,
   lib,
   fetchurl,
-  makeWrapper,
-  xar,
-  cpio,
   pulseaudioSupport ? true,
   xdgDesktopPortalSupport ? true,
   callPackage,
@@ -12,88 +9,37 @@
 }:
 
 let
-  inherit (stdenv.hostPlatform) system;
-  throwSystem = throw "Unsupported system: ${system}";
+  unpacked = stdenv.mkDerivation (finalAttrs: {
+    pname = "zoom";
+    version = "6.4.6.1370";
 
-  # Zoom versions are released at different times for each platform
-  # and often with different versions. We write them on three lines
-  # like this (rather than using {}) so that the updater script can
-  # find where to edit them.
-  versions.aarch64-darwin = "6.4.6.53970";
-  versions.x86_64-darwin = "6.4.6.53970";
-  versions.x86_64-linux = "6.4.6.1370";
-
-  srcs = {
-    aarch64-darwin = fetchurl {
-      url = "https://zoom.us/client/${versions.aarch64-darwin}/zoomusInstallerFull.pkg?archType=arm64";
-      name = "zoomusInstallerFull.pkg";
-      hash = "sha256-yNsiFZNte4432d8DUyDhPUOVbLul7gUdvr+3qK/Y+tk=";
-    };
-    x86_64-darwin = fetchurl {
-      url = "https://zoom.us/client/${versions.x86_64-darwin}/zoomusInstallerFull.pkg";
-      hash = "sha256-Ut93qQFFN0d58wXD5r8u0B17HbihFg3FgY3a1L8nsIA=";
-    };
-    x86_64-linux = fetchurl {
-      url = "https://zoom.us/client/${versions.x86_64-linux}/zoom_x86_64.pkg.tar.xz";
+    src = fetchurl {
+      url = "https://zoom.us/client/${finalAttrs.version}/zoom_x86_64.pkg.tar.xz";
       hash = "sha256-Y+8garSqDcKLCVv1cTiqGEfrGKpK3UoXIq8X4E8CF+8=";
     };
-  };
 
-  unpacked = stdenv.mkDerivation {
-    pname = "zoom";
-    version = versions.${system} or throwSystem;
+    dontUnpack = true;
 
-    src = srcs.${system} or throwSystem;
-
-    dontUnpack = stdenv.hostPlatform.isLinux;
-    unpackPhase = lib.optionalString stdenv.hostPlatform.isDarwin ''
-      xar -xf $src
-      zcat < zoomus.pkg/Payload | cpio -i
-    '';
-
-    # Note: In order to uncover missing libraries
-    # on x86_64-linux, add "pkgs" to this file's arguments
+    # Note: In order to uncover missing libraries,
+    # add "pkgs" to this file's arguments
     # (at the top of this file), then add these attributes here:
     # > buildInputs = linuxGetDependencies pkgs;
     # > dontAutoPatchelf = true;
     # > dontWrapQtApps = true;
+    # > nativeBuildInputs = [ pkgs.autoPatchelfHook ];
     # > preFixup = ''
     # >   addAutoPatchelfSearchPath $out/opt/zoom
     # >   autoPatchelf $out/opt/zoom/{cef,Qt,*.so*,aomhost,zoom,zopen,ZoomLauncher,ZoomWebviewHost}
     # > '';
-    # ...and finally "pkgs.autoPatchelfHook"
-    # to `nativeBuildInputs` right below.
     # Then build `zoom-us.unpacked`:
     # `autoPatchelfHook` will report missing library files.
-    nativeBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-      makeWrapper
-      xar
-      cpio
-    ];
 
     installPhase = ''
       runHook preInstall
-      ${
-        rec {
-          aarch64-darwin = ''
-            mkdir -p $out/Applications
-            cp -R zoom.us.app $out/Applications/
-          '';
-          # darwin steps same on both architectures
-          x86_64-darwin = aarch64-darwin;
-          x86_64-linux = ''
-            mkdir $out
-            tar -C $out -xf $src
-            mv $out/usr/* $out/
-          '';
-        }
-        .${system} or throwSystem
-      }
+      mkdir $out
+      tar -C $out -xf $src
+      mv $out/usr/* $out/
       runHook postInstall
-    '';
-
-    postFixup = lib.optionalString stdenv.hostPlatform.isDarwin ''
-      makeWrapper $out/Applications/zoom.us.app/Contents/MacOS/zoom.us $out/bin/zoom
     '';
 
     dontPatchELF = true;
@@ -107,18 +53,13 @@ let
       description = "zoom.us video conferencing application";
       sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
       license = lib.licenses.unfree;
-      platforms = builtins.attrNames srcs;
+      platforms = [ "x86_64-linux" ];
       maintainers = with lib.maintainers; [
         danbst
         tadfisher
       ];
-      mainProgram = "zoom";
     };
-  };
-  packages.aarch64-darwin = unpacked;
-  packages.x86_64-darwin = unpacked;
-
-  # linux definitions
+  });
 
   linuxGetDependencies =
     pkgs:
@@ -191,35 +132,35 @@ let
       pkgs.xdg-desktop-portal-xapp
     ];
 
-  # We add the `unpacked` zoom archive to the FHS env
-  # and also bind-mount its `/opt` directory.
-  # This should assist Zoom in finding all its
-  # files in the places where it expects them to be.
-  packages.x86_64-linux = buildFHSEnv {
-    pname = "zoom"; # Will also be the program's name!
-    version = versions.${system} or throwSystem;
-
-    targetPkgs = pkgs: (linuxGetDependencies pkgs) ++ [ unpacked ];
-    extraPreBwrapCmds = "unset QT_PLUGIN_PATH";
-    extraBwrapArgs = [ "--ro-bind ${unpacked}/opt /opt" ];
-    runScript = "/opt/zoom/ZoomLauncher";
-
-    extraInstallCommands = ''
-      cp -Rt $out/ ${unpacked}/share
-      substituteInPlace \
-          $out/share/applications/Zoom.desktop \
-          --replace-fail Exec={/usr/bin/,}zoom
-
-      # Backwards compatibility: we used to call it zoom-us
-      ln -s $out/bin/{zoom,zoom-us}
-    '';
-
-    passthru = unpacked.passthru // {
-      inherit unpacked;
-    };
-    inherit (unpacked) meta;
-  };
-
 in
 
-packages.${system} or throwSystem
+# We add the `unpacked` zoom archive to the FHS env
+# and also bind-mount its `/opt` directory.
+# This should assist Zoom in finding all its
+# files in the places where it expects them to be.
+buildFHSEnv rec {
+  pname = "zoom"; # Will also be the program's name!
+  inherit (unpacked) version;
+
+  targetPkgs = pkgs: (linuxGetDependencies pkgs) ++ [ unpacked ];
+  extraPreBwrapCmds = "unset QT_PLUGIN_PATH";
+  extraBwrapArgs = [ "--ro-bind ${unpacked}/opt /opt" ];
+  runScript = "/opt/zoom/ZoomLauncher";
+
+  extraInstallCommands = ''
+    cp -Rt $out/ ${unpacked}/share
+    substituteInPlace \
+        $out/share/applications/Zoom.desktop \
+        --replace-fail Exec={/usr/bin/,}zoom
+
+    # Backwards compatibility: we used to call it zoom-us
+    ln -s $out/bin/{zoom,zoom-us}
+  '';
+
+  passthru = unpacked.passthru // {
+    inherit unpacked;
+  };
+  meta = unpacked.meta // {
+    mainProgram = pname;
+  };
+}
