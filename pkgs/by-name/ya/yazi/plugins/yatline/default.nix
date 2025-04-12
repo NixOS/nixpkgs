60dac5e176b0fc6318169be9613504d2f9a96253
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-12 08:22:15 +0000

    yaziPlugins.yatline: 0-unstable-2025-03-05 -> 0-unstable-2025-04-11

--- pkgs/by-name/ya/yazi/plugins/yatline/default.nix
+++ pkgs/by-name/ya/yazi/plugins/yatline/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "yatline.yazi";
-  version = "0-unstable-2025-03-05";
+  version = "0-unstable-2025-04-11";
 
   src = fetchFromGitHub {
     owner = "imsi32";
     repo = "yatline.yazi";
-    rev = "6b0fc1ef5eca034460b4c69cb87c424103227a2e";
-    hash = "sha256-JiUik4umi+ujsJmHliUbLsIj7ZTKayrWAAM8HmRdjwk=";
+    rev = "90e0284b22f922e7e024c403e7e596359e3aa2a0";
+    hash = "sha256-99HcvxylfPf5MlAnDOi/eg3C1XwzKnGz/vmMTBnSm/o=";
   };
 
   meta = {
