e06b22b25468c790815aa3e6d897e2a4b6047c1f
Wolfgang Walther <walther@technowledgy.de>
2025-04-12 22:13:13 +0200

    postgresqlPackages.timescaledb: move test from VM to postgresqlTestExtension

--- nixos/tests/postgresql/default.nix
+++ nixos/tests/postgresql/default.nix
@@ -38,7 +38,6 @@ in
   anonymizer = importWithArgs ./anonymizer.nix;
   pgjwt = importWithArgs ./pgjwt.nix;
   pgvecto-rs = importWithArgs ./pgvecto-rs.nix;
-  timescaledb = importWithArgs ./timescaledb.nix;
   tsja = importWithArgs ./tsja.nix;
   wal2json = importWithArgs ./wal2json.nix;
 }
