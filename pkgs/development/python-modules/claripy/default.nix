f7ee964445899de5183de3ab3e71b0079f890370
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-12 18:59:52 +0000

    python312Packages.claripy: 9.2.148 -> 9.2.149

--- pkgs/development/python-modules/claripy/default.nix
+++ pkgs/development/python-modules/claripy/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "claripy";
-  version = "9.2.148";
+  version = "9.2.149";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "claripy";
     tag = "v${version}";
-    hash = "sha256-WfDsitb3Ziw3Ss7/zOblBhzfD+foZSGfyD0rGAI+HcA=";
+    hash = "sha256-jWT4PhGDxU42DrqV49sz/5bN0653lig9/f0T1zOq6eA=";
   };
 
   # z3 does not provide a dist-info, so python-runtime-deps-check will fail
