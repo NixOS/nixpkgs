6572a9c92693d5d795970d1995cd907e97e9b8bc
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-12 09:38:50 +0000

    php83: 8.3.19 -> 8.3.20

--- pkgs/development/interpreters/php/8.3.nix
+++ pkgs/development/interpreters/php/8.3.nix
@@ -4,8 +4,8 @@ let
   base = callPackage ./generic.nix (
     _args
     // {
-      version = "8.3.19";
-      hash = "sha256-rWkCoqAvlOw+mYIGniZou6POR+L2u7q5Xra323vz01A=";
+      version = "8.3.20";
+      hash = "sha256-NrTmoyPNRWc6VPKW6dJma39d9XZBAxx3xP2BN7xOvrM=";
     }
   );
 in
