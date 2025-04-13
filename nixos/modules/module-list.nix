4d2c9f9992c7827d34835be31d1d18c13b90b27a
Kritnich <kritnich@kritni.ch>
2025-04-13 12:44:26 +0200

    ism7mqtt: init at 0.0.17

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -732,6 +732,7 @@
   ./services/home-automation/home-assistant-matter-hub.nix
   ./services/home-automation/home-assistant.nix
   ./services/home-automation/homebridge.nix
+  ./services/home-automation/ism7mqtt.nix
   ./services/home-automation/matter-server.nix
   ./services/home-automation/openthread-border-router.nix
   ./services/home-automation/wyoming/faster-whisper.nix
