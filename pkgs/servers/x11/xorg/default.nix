b9bac34e4e22375248addee726f64287dfbfba61
Zitrone <general@dev.quantenzitrone.eu>
2025-04-13 21:38:28 +0200

    lndir: refactor and move to pkgs/by-name from xorg namespace

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -7,6 +7,7 @@
   libpciaccess,
   libpthread-stubs,
   libxcvt,
+  lndir,
   luit,
   makedepend,
   pixman,
@@ -27,6 +28,7 @@ self: with self; {
     gccmakedep
     libpciaccess
     libxcvt
+    lndir
     luit
     makedepend
     pixman
@@ -3399,38 +3401,6 @@ self: with self; {
         xorgproto
         libXt
       ];
-      passthru.tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
-      meta = {
-        pkgConfigModules = [ ];
-        platforms = lib.platforms.unix;
-      };
-    })
-  ) { };
-
-  # THIS IS A GENERATED FILE.  DO NOT EDIT!
-  lndir = callPackage (
-    {
-      stdenv,
-      pkg-config,
-      fetchurl,
-      xorgproto,
-      testers,
-    }:
-    stdenv.mkDerivation (finalAttrs: {
-      pname = "lndir";
-      version = "1.0.5";
-      builder = ./builder.sh;
-      src = fetchurl {
-        url = "mirror://xorg/individual/util/lndir-1.0.5.tar.xz";
-        sha256 = "1nsd23kz6iqxfcis3432zq01i54n98b94m2gcsay1k3mamx5fr9v";
-      };
-      hardeningDisable = [
-        "bindnow"
-        "relro"
-      ];
-      strictDeps = true;
-      nativeBuildInputs = [ pkg-config ];
-      buildInputs = [ xorgproto ];
       passthru.tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
       meta = {
         pkgConfigModules = [ ];
