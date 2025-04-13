071703a2fcfb61676ca206270cdb09b49c6db47b
OPNA2608 <opna2608@protonmail.com>
2025-04-13 16:35:56 +0200

    nixosTests.lomiri-camera-app: Optimise
    - use wait_for_window to know when windows have opened
    - sleep before doing some wait_for_text's
      this improved runtime speed on my laptop from 732.81s to 498.06s
    - Generate camera feed image ahead of time
    - pgrep & pkill running processes with exact options, make sure apps are actually closed before continuing
    - Instead of pasting qr-decoded content into text file & OCRing for it, write it to a file named after the content and wait_for_file for it

--- nixos/tests/lomiri-camera-app.nix
+++ nixos/tests/lomiri-camera-app.nix
@@ -1,4 +1,9 @@
 { lib, ... }:
+let
+  qrLabel = "Image";
+  qrContent = "Test";
+  feedImage = "feed.png";
+in
 {
   name = "lomiri-camera-app-standalone";
   meta.maintainers = lib.teams.lomiri.members;
@@ -11,14 +16,26 @@
       services.xserver.enable = true;
 
       environment = {
+        etc."${feedImage}".source =
+          pkgs.runCommand feedImage
+            {
+              nativeBuildInputs = with pkgs; [
+                (imagemagick.override { ghostscriptSupport = true; }) # add label for OCR
+                qrtool # generate QR code
+              ];
+            }
+            ''
+              qrtool encode '${qrContent}' -s 20 -m 10 > tmp.png
+              # Horizontal flip, add text, flip back. Camera displays image mirrored, so need reversed text for OCR
+              magick tmp.png -flop -pointsize 70 -fill black -annotate +100+100 '${qrLabel}' -flop $out
+            '';
         systemPackages =
           with pkgs;
           [
             feh # view photo result
             ffmpeg # fake webcam stream
             gnome-text-editor # somewhere to paste QR result
-            (imagemagick.override { ghostscriptSupport = true; }) # add label for OCR
-            qrtool # generate QR code
+            imagemagick # un-flip camera photo to verify result
             xdotool # clicking on QR button
           ]
           ++ (with pkgs.lomiri; [
@@ -47,28 +64,29 @@
 
   testScript =
     let
-      qrLabel = "Image";
-      qrContent = "Test";
     in
     ''
       machine.wait_for_x()
 
       with subtest("lomiri camera launches"):
           machine.succeed("lomiri-camera-app >&2 &")
+          machine.wait_for_window("Camera")
+          machine.sleep(10) # Optimise OCR
           machine.wait_for_text("Cannot access")
           machine.screenshot("lomiri-camera_open")
 
-      machine.succeed("pkill -f lomiri-camera-app")
+      machine.succeed("pgrep -afx lomiri-camera-app >&2")
+      machine.succeed("pkill -efx lomiri-camera-app >&2")
+      machine.wait_until_fails("pgrep -afx lomiri-camera-app >&2")
 
       # Setup fake v4l2 camera
       machine.succeed("modprobe v4l2loopback video_nr=10 card_label=Video-Loopback exclusive_caps=1")
-      machine.succeed("qrtool encode '${qrContent}' -s 20 -m 10 > qr.png")
-      # Horizontal flip, add text, flip back. Camera displays image mirrored, so need reversed text for OCR
-      machine.succeed("magick qr.png -flop -pointsize 70 -fill black -annotate +100+100 '${qrLabel}' -flop output.png")
-      machine.succeed("ffmpeg -re -loop 1 -i output.png -vf format=yuv420p -f v4l2 /dev/video10 -loglevel fatal >&2 &")
+      machine.succeed("ffmpeg -re -loop 1 -i /etc/${feedImage} -vf format=yuv420p -f v4l2 /dev/video10 -loglevel fatal >&2 &")
 
       with subtest("lomiri camera uses camera"):
           machine.succeed("lomiri-camera-app >&2 &")
+          machine.wait_for_window("Camera")
+          machine.sleep(10) # Optimise OCR
           machine.wait_for_text("${qrLabel}")
           machine.screenshot("lomiri-camera_feed")
 
@@ -79,15 +97,24 @@
           machine.send_key("ctrl-alt-right")
           machine.succeed("magick /root/Pictures/camera.ubports/IMG_00000001.jpg -flop photo_flip.png")
           machine.succeed("feh photo_flip.png >&2 &")
+          machine.wait_for_window("feh")
+          machine.sleep(10) # Optimise OCR
           machine.wait_for_text("${qrLabel}")
           machine.screenshot("lomiri-camera_photo")
 
-      machine.succeed("pkill -f feh")
+      # feh is weird to exact pgrep/pkill for, just send it a "q" and wait for it to close itself
+      machine.succeed("pgrep -a feh >&2")
+      machine.succeed("pgrep -afx lomiri-camera-app >&2")
+      machine.send_key("q")
+      machine.succeed("pkill -efx lomiri-camera-app >&2")
+      machine.wait_until_fails("pgrep -a feh >&2")
+      machine.wait_until_fails("pgrep -afx lomiri-camera-app >&2")
       machine.send_key("ctrl-alt-left")
-      machine.succeed("pkill -f lomiri-camera-app")
 
       with subtest("lomiri barcode scanner uses camera"):
           machine.succeed("lomiri-camera-app --mode=barcode-reader >&2 &")
+          machine.wait_for_window("Camera")
+          machine.sleep(10) # Optimise OCR
           machine.wait_for_text("${qrLabel}")
           machine.succeed("xdotool mousemove 320 610 click 1") # open up QR decode result
 
@@ -100,36 +127,31 @@
           # Need to make a new window without closing camera app, otherwise clipboard content gets lost?
           machine.send_key("ctrl-alt-right")
           machine.succeed("gnome-text-editor >&2 &")
-          machine.wait_for_text("New")
+          machine.wait_for_window("New Document")
+          # Window might be up, but catching the save keybind might not be
+          machine.sleep(10)
 
-          # Font size up to help with OCR
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
-          machine.send_key("ctrl-kp_add")
+          machine.send_key("ctrl-s")
+          machine.wait_for_window("Save As")
 
           machine.send_key("ctrl-v")
-          machine.wait_for_text("${qrContent}")
+          machine.sleep(10) # Make sure it's in the buffer
+          machine.screenshot("gnome-text-editor_saveas") # See where it actually puts the file
+          machine.send_key("ret")
 
-      machine.succeed("pkill -f gnome-text-editor")
+          machine.wait_for_file("/tmp/${qrContent}.txt")
+
+      machine.succeed("pgrep -afx gnome-text-editor >&2")
+      machine.succeed("pgrep -afx 'lomiri-camera-app --mode=barcode-reader' >&2")
+      machine.succeed("pkill -efx gnome-text-editor >&2")
+      machine.succeed("pkill -efx 'lomiri-camera-app --mode=barcode-reader' >&2")
+      machine.wait_until_fails("pgrep -afx gnome-text-editor >&2")
+      machine.wait_until_fails("pgrep -afx 'lomiri-camera-app --mode=barcode-reader' >&2")
       machine.send_key("ctrl-alt-left")
-      machine.succeed("pkill -f lomiri-camera-app")
 
       with subtest("lomiri camera localisation works"):
           machine.succeed("env LANG=de_DE.UTF-8 lomiri-camera-app >&2 &")
-          machine.wait_for_text("Kamera")
+          machine.wait_for_window("Kamera")
           machine.screenshot("lomiri-camera_localised")
     '';
 }
