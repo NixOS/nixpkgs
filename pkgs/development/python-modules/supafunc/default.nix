161cc90e93dcc48f61804e582329fcd5ac4e2830
Siege <15473103+Siegema@users.noreply.github.com>
2025-04-13 14:15:45 +0000

    supafunc: init at 0.10.1

---
+++ pkgs/development/python-modules/supafunc/default.nix
@@ -0,0 +1,35 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, poetry-core
+, strenum
+, httpx
+, h2
+}:
+
+buildPythonPackage rec {
+  pname = "supafunc";
+  version = "0.10.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-pbM8i67La1KX0l2imiUD4uxn7mmG89RME35lG4pZoX0=";
+  };
+
+  propagatedBuildInputs = [ strenum httpx h2 ];
+
+  nativeBuildInputs = [ poetry-core ];
+
+  pythonImportsCheck = [ "supafunc" ];
+
+  # tests are not in pypi package
+  doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://github.com/supabase/functions-py.git";
+    license = licenses.mit;
+    description = "Library for Supabase Functions";
+    maintainers = with maintainers; [ siegema ];
+  };
+}
