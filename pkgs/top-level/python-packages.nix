432d05bf223e302a55377d3fc76b4e16c3300a8f
Siege <15473103+Siegema@users.noreply.github.com>
2025-04-13 11:37:44 +0000

    realtime: init at 2.5.2
    (cherry picked from commit 3e22ed764efc77f87d6a570456f3d244097ac5d2)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14947,6 +14947,8 @@ self: super: with self; {
 
   readthedocs-sphinx-ext = callPackage ../development/python-modules/readthedocs-sphinx-ext { };
 
+  realtime = callPackage ../development/python-modules/realtime { };
+
   rebulk = callPackage ../development/python-modules/rebulk { };
 
   recipe-scrapers = callPackage ../development/python-modules/recipe-scrapers { };
