bf62a22844bba7f75c05fca9f23f80920068fd84
Randy Eckenrode <randy@largeandhighquality.com>
2025-04-13 10:07:24 -0400

    llvmPackages.libcxx: make `postPatch` unconditional
    Having it be conditional for every version of libc++ except for
    libc++ 16 makes it difficult to share patches between versions.

--- pkgs/development/compilers/llvm/common/libcxx/default.nix
+++ pkgs/development/compilers/llvm/common/libcxx/default.nix
@@ -147,172 +147,161 @@ let
 
 in
 
-stdenv.mkDerivation (
-  finalAttrs:
-  {
-    pname = "libcxx";
-    inherit version cmakeFlags;
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libcxx";
+  inherit version cmakeFlags;
 
-    src =
-      if monorepoSrc != null then
-        runCommand "libcxx-src-${version}" { inherit (monorepoSrc) passthru; } (
-          ''
-            mkdir -p "$out/llvm"
-          ''
-          + (lib.optionalString (lib.versionAtLeast release_version "14") ''
-            cp -r ${monorepoSrc}/cmake "$out"
-          '')
-          + ''
-            cp -r ${monorepoSrc}/libcxx "$out"
-            cp -r ${monorepoSrc}/llvm/cmake "$out/llvm"
-            cp -r ${monorepoSrc}/llvm/utils "$out/llvm"
-          ''
-          + (lib.optionalString (lib.versionAtLeast release_version "14") ''
-            cp -r ${monorepoSrc}/third-party "$out"
-          '')
-          + (lib.optionalString (lib.versionAtLeast release_version "20") ''
-            cp -r ${monorepoSrc}/libc "$out"
-          '')
-          + ''
-            cp -r ${monorepoSrc}/runtimes "$out"
-          ''
-          + (lib.optionalString (cxxabi == null) ''
-            cp -r ${monorepoSrc}/libcxxabi "$out"
-          '')
-        )
-      else
-        src;
+  src =
+    if monorepoSrc != null then
+      runCommand "libcxx-src-${version}" { inherit (monorepoSrc) passthru; } (
+        ''
+          mkdir -p "$out/llvm"
+        ''
+        + (lib.optionalString (lib.versionAtLeast release_version "14") ''
+          cp -r ${monorepoSrc}/cmake "$out"
+        '')
+        + ''
+          cp -r ${monorepoSrc}/libcxx "$out"
+          cp -r ${monorepoSrc}/llvm/cmake "$out/llvm"
+          cp -r ${monorepoSrc}/llvm/utils "$out/llvm"
+        ''
+        + (lib.optionalString (lib.versionAtLeast release_version "14") ''
+          cp -r ${monorepoSrc}/third-party "$out"
+        '')
+        + (lib.optionalString (lib.versionAtLeast release_version "20") ''
+          cp -r ${monorepoSrc}/libc "$out"
+        '')
+        + ''
+          cp -r ${monorepoSrc}/runtimes "$out"
+        ''
+        + (lib.optionalString (cxxabi == null) ''
+          cp -r ${monorepoSrc}/libcxxabi "$out"
+        '')
+      )
+    else
+      src;
 
-    outputs = [
-      "out"
-      "dev"
-    ];
+  outputs = [
+    "out"
+    "dev"
+  ];
 
-    preConfigure = lib.optionalString stdenv.hostPlatform.isMusl ''
-      patchShebangs utils/cat_files.py
-    '';
+  preConfigure = lib.optionalString stdenv.hostPlatform.isMusl ''
+    patchShebangs utils/cat_files.py
+  '';
 
-    patches = lib.optionals (lib.versionOlder release_version "16") (
-      lib.optional (lib.versions.major release_version == "15")
-        # See:
-        #   - https://reviews.llvm.org/D133566
-        #   - https://github.com/NixOS/nixpkgs/issues/214524#issuecomment-1429146432
-        # !!! Drop in LLVM 16+
-        (
-          fetchpatch {
-            url = "https://github.com/llvm/llvm-project/commit/57c7bb3ec89565c68f858d316504668f9d214d59.patch";
-            hash = "sha256-B07vHmSjy5BhhkGSj3e1E0XmMv5/9+mvC/k70Z29VwY=";
-          }
-        )
-      ++ [
-        (substitute {
-          src = ../libcxxabi/wasm.patch;
-          substitutions = [
-            "--replace-fail"
-            "/cmake/"
-            "/llvm/cmake/"
-          ];
-        })
-      ]
-      ++ lib.optional stdenv.hostPlatform.isMusl (substitute {
-        src = ./libcxx-0001-musl-hacks.patch;
+  patches = lib.optionals (lib.versionOlder release_version "16") (
+    lib.optional (lib.versions.major release_version == "15")
+      # See:
+      #   - https://reviews.llvm.org/D133566
+      #   - https://github.com/NixOS/nixpkgs/issues/214524#issuecomment-1429146432
+      # !!! Drop in LLVM 16+
+      (
+        fetchpatch {
+          url = "https://github.com/llvm/llvm-project/commit/57c7bb3ec89565c68f858d316504668f9d214d59.patch";
+          hash = "sha256-B07vHmSjy5BhhkGSj3e1E0XmMv5/9+mvC/k70Z29VwY=";
+        }
+      )
+    ++ [
+      (substitute {
+        src = ../libcxxabi/wasm.patch;
         substitutions = [
           "--replace-fail"
           "/include/"
           "/libcxx/include/"
         ];
       })
-    );
-
-    nativeBuildInputs =
-      [
-        cmake
-        ninja
-        python3
-      ]
-      ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames
-      ++ lib.optional (cxxabi != null) lndir;
-
-    buildInputs =
-      [ cxxabi ]
-      ++ lib.optionals (useLLVM && !stdenv.hostPlatform.isWasm && !stdenv.hostPlatform.isFreeBSD) [
-        libunwind
+    ]
+    ++ lib.optional stdenv.hostPlatform.isMusl (substitute {
+      src = ./libcxx-0001-musl-hacks.patch;
+      substitutions = [
+        "--replace-fail"
+        "/include/"
+        "/libcxx/include/"
       ];
+    })
+  );
 
-    # libc++.so is a linker script which expands to multiple libraries,
-    # libc++.so.1 and libc++abi.so or the external cxxabi. ld-wrapper doesn't
-    # support linker scripts so the external cxxabi needs to be symlinked in
-    postInstall =
-      lib.optionalString (cxxabi != null) ''
-        lndir ${lib.getDev cxxabi}/include $dev/include/c++/v1
-        lndir ${lib.getLib cxxabi}/lib $out/lib
-        libcxxabi=$out/lib/lib${cxxabi.libName}.a
+  postPatch =
+    (lib.optionalString
+      (lib.versionAtLeast release_version "14" && lib.versionOlder release_version "15")
       ''
-      # LIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON doesn't work for LLVM < 16 or
-      # external cxxabi libraries so merge libc++abi.a into libc++.a ourselves.
+        # fix CMake error when static and LIBCXXABI_USE_LLVM_UNWINDER=ON. aren't
+        # building unwind so don't need to depend on it
+        substituteInPlace libcxx/src/CMakeLists.txt \
+          --replace-fail "add_dependencies(cxx_static unwind)" "# add_dependencies(cxx_static unwind)"
+      ''
+    )
+    + ''
+      cd runtimes
+    '';
 
-      # GNU binutils emits objects in LIFO order in MRI scripts so after the merge
-      # the objects are in reversed order so a second MRI script is required so the
-      # objects in the archive are listed in proper order (libc++.a, libc++abi.a)
-      + lib.optionalString (cxxabi != null || lib.versionOlder release_version "16") ''
-        libcxxabi=''${libcxxabi-$out/lib/libc++abi.a}
-        if [[ -f $out/lib/libc++.a && -e $libcxxabi ]]; then
-          $AR -M <<MRI
-            create $out/lib/libc++.a
-            addlib $out/lib/libc++.a
-            addlib $libcxxabi
-            save
-            end
-        MRI
-          $AR -M <<MRI
-            create $out/lib/libc++.a
-            addlib $out/lib/libc++.a
-            save
-            end
-        MRI
-        fi
-      '';
+  nativeBuildInputs =
+    [
+      cmake
+      ninja
+      python3
+    ]
+    ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames
+    ++ lib.optional (cxxabi != null) lndir;
 
-    passthru = {
-      inherit abiNamespace;
-      isLLVM = true;
-    };
+  buildInputs =
+    [ cxxabi ]
+    ++ lib.optionals (useLLVM && !stdenv.hostPlatform.isWasm && !stdenv.hostPlatform.isFreeBSD) [
+      libunwind
+    ];
 
-    meta = llvm_meta // {
-      homepage = "https://libcxx.llvm.org/";
-      description = "C++ standard library";
-      longDescription = ''
-        libc++ is an implementation of the C++ standard library, targeting C++11,
-        C++14 and above.
-      '';
-      # "All of the code in libc++ is dual licensed under the MIT license and the
-      # UIUC License (a BSD-like license)":
-      license = with lib.licenses; [
-        mit
-        ncsa
-      ];
-    };
-  }
-  // (
-    if (lib.versionOlder release_version "16" || lib.versionAtLeast release_version "17") then
-      {
-        postPatch =
-          (lib.optionalString
-            (lib.versionAtLeast release_version "14" && lib.versionOlder release_version "15")
-            ''
-              # fix CMake error when static and LIBCXXABI_USE_LLVM_UNWINDER=ON. aren't
-              # building unwind so don't need to depend on it
-              substituteInPlace libcxx/src/CMakeLists.txt \
-                --replace-fail "add_dependencies(cxx_static unwind)" "# add_dependencies(cxx_static unwind)"
-            ''
-          )
-          + ''
-            cd runtimes
-          '';
-      }
-    else
-      {
-        sourceRoot = "${finalAttrs.src.name}/runtimes";
-      }
-  )
-)
+  # libc++.so is a linker script which expands to multiple libraries,
+  # libc++.so.1 and libc++abi.so or the external cxxabi. ld-wrapper doesn't
+  # support linker scripts so the external cxxabi needs to be symlinked in
+  postInstall =
+    lib.optionalString (cxxabi != null) ''
+      lndir ${lib.getDev cxxabi}/include $dev/include/c++/v1
+      lndir ${lib.getLib cxxabi}/lib $out/lib
+      libcxxabi=$out/lib/lib${cxxabi.libName}.a
+    ''
+    # LIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON doesn't work for LLVM < 16 or
+    # external cxxabi libraries so merge libc++abi.a into libc++.a ourselves.
+
+    # GNU binutils emits objects in LIFO order in MRI scripts so after the merge
+    # the objects are in reversed order so a second MRI script is required so the
+    # objects in the archive are listed in proper order (libc++.a, libc++abi.a)
+    + lib.optionalString (cxxabi != null || lib.versionOlder release_version "16") ''
+      libcxxabi=''${libcxxabi-$out/lib/libc++abi.a}
+      if [[ -f $out/lib/libc++.a && -e $libcxxabi ]]; then
+        $AR -M <<MRI
+          create $out/lib/libc++.a
+          addlib $out/lib/libc++.a
+          addlib $libcxxabi
+          save
+          end
+      MRI
+        $AR -M <<MRI
+          create $out/lib/libc++.a
+          addlib $out/lib/libc++.a
+          save
+          end
+      MRI
+      fi
+    '';
+
+  passthru = {
+    inherit abiNamespace;
+    isLLVM = true;
+  };
+
+  meta = llvm_meta // {
+    homepage = "https://libcxx.llvm.org/";
+    description = "C++ standard library";
+    longDescription = ''
+      libc++ is an implementation of the C++ standard library, targeting C++11,
+      C++14 and above.
+    '';
+    # "All of the code in libc++ is dual licensed under the MIT license and the
+    # UIUC License (a BSD-like license)":
+    license = with lib.licenses; [
+      mit
+      ncsa
+    ];
+  };
+})
