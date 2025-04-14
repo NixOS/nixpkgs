b1d63aad70ed93bc34e2cdf8dcda6a3c496f926d
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-14 20:46:29 +0800

    nixos/firewalld: add systemd service

--- nixos/modules/services/networking/firewalld/default.nix
+++ nixos/modules/services/networking/firewalld/default.nix
@@ -17,6 +17,13 @@ in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ cfg.package ];
     services.dbus.packages = [ cfg.package ];
+
+    systemd.packages = [ cfg.package ];
+    systemd.services.firewalld = {
+      aliases = [ "dbus-org.fedoraproject.FirewallD1.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+    };
   };
 
   meta.maintainers = with lib.maintainers; [ prince213 ];
