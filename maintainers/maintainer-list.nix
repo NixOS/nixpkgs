cdf96002012576ee0b23a7fd522661610767178f
Siege <15473103+Siegema@users.noreply.github.com>
2025-04-13 22:45:16 -0400

    maintainers: add siegema

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23155,6 +23155,12 @@
     githubId = 4294323;
     name = "Langston Barrett";
   };
+  siegema = {
+    email = "bee@sharpbeedevelopment.com";
+    name = "Martin";
+    github = "Siegema";
+    githubId = 15473103;
+  };
   sielicki = {
     name = "Nicholas Sielicki";
     email = "nix@opensource.nslick.com";
