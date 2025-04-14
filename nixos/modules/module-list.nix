cb2857835654be9da63cd2b8bf7b4ff7e40c7a93
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-14 17:24:41 +0800

    nixos/firewalld: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1165,6 +1165,7 @@
   ./services/networking/firewall-iptables.nix
   ./services/networking/firewall-nftables.nix
   ./services/networking/firewall.nix
+  ./services/networking/firewalld
   ./services/networking/firezone/gateway.nix
   ./services/networking/firezone/gui-client.nix
   ./services/networking/firezone/headless-client.nix
