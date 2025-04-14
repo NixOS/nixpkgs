3481e1937ea63dc890f727dde793f1a2fed8749d
Defelo <mail@defelo.de>
2025-04-14 18:25:18 +0200

    python3Packages.symbolic_10: init at 10.2.1
    required by glitchtip

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16788,6 +16788,8 @@ self: super: with self; {
 
   symbolic = callPackage ../development/python-modules/symbolic { };
 
+  symbolic_10 = callPackage ../development/python-modules/symbolic/10.nix { };
+
   symengine = callPackage ../development/python-modules/symengine { inherit (pkgs) symengine; };
 
   symfc = callPackage ../development/python-modules/symfc { };
