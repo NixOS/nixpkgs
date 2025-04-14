f95e522749fab9ab1b087a46e589bdc5c2c881e8
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-14 21:37:14 +0800

    nixos/firewalld: support firewalld files from other packages

--- nixos/modules/services/networking/firewalld/default.nix
+++ nixos/modules/services/networking/firewalld/default.nix
@@ -7,22 +7,33 @@
 
 let
   cfg = config.services.firewalld;
+  paths = pkgs.buildEnv {
+    name = "firewalld-paths";
+    paths = cfg.packages;
+    pathsToLink = [ "/lib/firewalld" ];
+  };
 in
 {
   options.services.firewalld = {
     enable = lib.mkEnableOption "FirewallD";
     package = lib.mkPackageOption pkgs "firewalld" { };
+    packages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      description = "Packages providing firewalld zones and other files.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [ cfg.package ];
     services.dbus.packages = [ cfg.package ];
+    services.firewalld.packages = [ cfg.package ];
 
     systemd.packages = [ cfg.package ];
     systemd.services.firewalld = {
       aliases = [ "dbus-org.fedoraproject.FirewallD1.service" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig.ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+      environment.NIX_FIREWALLD_CONFIG_PATH = "${paths}/lib/firewalld";
     };
   };
 
