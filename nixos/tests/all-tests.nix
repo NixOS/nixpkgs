3d7db6377ae1070c57a3f63e73999d2bc7f53f93
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-15 16:40:15 +0800

    nixos/tests/firewalld: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -524,6 +524,7 @@ in
     imports = [ ./firewall.nix ];
     _module.args.backend = "iptables";
   };
+  firewalld = runTest ./firewalld.nix;
   firewall-firewalld = runTest {
     imports = [ ./firewall.nix ];
     _module.args.backend = "firewalld";
