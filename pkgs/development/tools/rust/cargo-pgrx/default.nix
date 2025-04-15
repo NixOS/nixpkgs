d803773d4a1c40d90f034b130de4242eb52aed5c
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-15 02:08:44 +0000

    cargo-pgrx_0_12_0_alpha_1: 0.12.0-alpha.1 -> 0.14.1

--- pkgs/development/tools/rust/cargo-pgrx/default.nix
+++ pkgs/development/tools/rust/cargo-pgrx/default.nix
@@ -59,9 +59,9 @@ let
 in
 {
   cargo-pgrx_0_12_0_alpha_1 = generic {
-    version = "0.12.0-alpha.1";
-    hash = "sha256-0m9oaqjU42RYyttkTihADDrRMjr2WoK/8sInZALeHws=";
-    cargoHash = "sha256-zYjqE7LZLnTaVxWAPWC1ncEjCMlrhy4THtgecB7wBYY=";
+    version = "0.14.1";
+    hash = "sha256-oMToAhKkRiCyC8JYS0gmo/XX3QVcVtF5mUV0aQjd+p8=";
+    cargoHash = "sha256-RawGAQGtG2QVDCMbwjmUEaH6rDeRiBvvJsGCY8wySw0=";
   };
 
   cargo-pgrx_0_12_5 = generic {
