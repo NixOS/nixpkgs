145c9dd28d448fbbaa47dd0df61c7f5f738e80f4
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-15 22:22:07 +0800

    gqlgenc: add versionCheckHook

--- pkgs/by-name/gq/gqlgenc/package.nix
+++ pkgs/by-name/gq/gqlgenc/package.nix
@@ -2,6 +2,7 @@
   buildGoModule,
   fetchFromGitHub,
   lib,
+  versionCheckHook,
 }:
 
 buildGoModule rec {
@@ -15,9 +16,26 @@ buildGoModule rec {
     sha256 = "sha256-AGbE+R3502Igl4/HaN8yvFVJBsKQ6iVff8IEvddJLEo=";
   };
 
+  patches = [
+    ./fix-version.patch
+  ];
+
   excludedPackages = [ "example" ];
 
   vendorHash = "sha256-kBv9Kit5KdPB48V/g1OaeB0ABFd1A1I/9F5LaQDWxUE=";
+
+  ldflags = [
+    "-X"
+    "main.version=${version}"
+  ];
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "version";
+
+  # FAIL: TestLoadConfig_LoadSchema/correct_schema
+  # panic: httptest: failed to listen on a port: listen tcp6 [::1]:0: bind: operation not permitted
+  __darwinAllowLocalNetworking = true;
 
   meta = with lib; {
     description = "Go tool for building GraphQL client with gqlgen";
