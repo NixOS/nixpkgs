78c4b24a81d62ab56f12dc3eafdecc131385e675
emaryn <emaryn@users.noreply.github.com>
2025-04-16 05:15:46 +0800

    ashpd-demo: 0.4.1 -> 0.11.0

--- pkgs/by-name/as/ashpd-demo/package.nix
+++ pkgs/by-name/as/ashpd-demo/package.nix
@@ -1,6 +1,6 @@
 {
-  stdenv,
   lib,
+  stdenv,
   fetchFromGitHub,
   cargo,
   meson,
@@ -19,20 +19,33 @@
   desktop-file-utils,
 }:
 
+let
+  libadwaita' = libadwaita.overrideAttrs (oldAttrs: {
+    version = "1.6.2-unstable-2025-01-02";
+    src = oldAttrs.src.override {
+      tag = null;
+      rev = "f5f0e7ce69405846a8f8bdad11cef2e2a7e99010";
+      hash = "sha256-n5RbGHtt2g627T/Tg8m3PjYIl9wfYTIcrplq1pdKAXk=";
+    };
+
+    # `test-application-window` is flaky on aarch64-linux
+    doCheck = false;
+  });
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "ashpd-demo";
-  version = "0.4.1";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "bilelmoussaoui";
     repo = "ashpd";
-    rev = "${finalAttrs.version}-demo";
-    hash = "sha256-fIyJEUcyTcjTbBycjuJb99wALQelMT7Zq6PHKcL2F80=";
+    tag = "${finalAttrs.version}";
+    hash = "sha256-zWxkI5Cq+taIkJ27qsVMslcFr6EqfxstQm9YvvSj3so=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     src = "${finalAttrs.src}/ashpd-demo";
-    hash = "sha256-iluV24uSEHDcYi6pO2HNrKs4ShwFvZ/ryv8ioopaNMI=";
+    hash = "sha256-eGFz3wWXXLhDesVU9yqj/fAX46RN6FxHFqhKwvr4C24=";
   };
 
   nativeBuildInputs = [
@@ -53,22 +66,22 @@ stdenv.mkDerivation (finalAttrs: {
     gtk4
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
-    libadwaita
     pipewire
     wayland
     libshumate
+    libadwaita'
   ];
 
   postPatch = ''
     cd ashpd-demo
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Tool for playing with XDG desktop portals";
     mainProgram = "ashpd-demo";
     homepage = "https://github.com/bilelmoussaoui/ashpd/tree/master/ashpd-demo";
-    license = licenses.mit;
-    maintainers = with maintainers; [ jtojnar ];
-    platforms = platforms.linux;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ jtojnar ];
+    platforms = lib.platforms.linux;
   };
 })
