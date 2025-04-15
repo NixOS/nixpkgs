25956e40f24f0b5bf193c6719ab6953c4ebef8c7
Dom Rodriguez <shymega@shymega.org.uk>
2025-04-15 19:35:43 +0100

    gkeepapi: init at 0.1.60
    Had to use the latest commit hash with this version in the metadata, as
    the repository has no tagged releases.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5855,6 +5855,8 @@ self: super: with self; {
 
   giturlparse = callPackage ../development/python-modules/giturlparse { };
 
+  gkeepapi = callPackage ../development/python-modules/gkeepapi { };
+
   glad = callPackage ../development/python-modules/glad { };
 
   glad2 = callPackage ../development/python-modules/glad2 { };
