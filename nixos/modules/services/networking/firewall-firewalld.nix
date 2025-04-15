8816da830fb004eb48f1dba60b49550226209dab
Sizhe Zhao <prc.zhao@outlook.com>
2025-04-15 16:35:00 +0800

    nixos/firewall: adopt more options for firewalld

--- nixos/modules/services/networking/firewall-firewalld.nix
+++ nixos/modules/services/networking/firewall-firewalld.nix
@@ -12,15 +12,29 @@ in
     lib.mkIf (cfg.enable && cfg.firewalld.enable && config.networking.nftables.enable == false)
       {
         services.firewalld = {
-          settings.DefaultZone = "nixos-fw-default";
-          zones.nixos-fw-default = {
-            ports =
-              let
-                f = protocol: port: { inherit protocol port; };
-                tcpPorts = map (f "tcp") (cfg.allowedTCPPorts ++ cfg.allowedTCPPortRanges);
-                udpPorts = map (f "udp") (cfg.allowedUDPPorts ++ cfg.allowedUDPPortRanges);
-              in
-              tcpPorts ++ udpPorts;
+          settings = {
+            DefaultZone = "nixos-fw-default";
+            LogDenied =
+              if cfg.logRefusedConnections then
+                (if cfg.logRefusedUnicastsOnly then "unicast" else "all")
+              else
+                "off";
+          };
+          zones = {
+            nixos-fw-default = {
+              target = if cfg.rejectPackets then "%%REJECT%%" else "DROP";
+              ports =
+                let
+                  f = protocol: port: { inherit protocol port; };
+                  tcpPorts = map (f "tcp") (cfg.allowedTCPPorts ++ cfg.allowedTCPPortRanges);
+                  udpPorts = map (f "udp") (cfg.allowedUDPPorts ++ cfg.allowedUDPPortRanges);
+                in
+                tcpPorts ++ udpPorts;
+            };
+            nixos-fw-trusted = {
+              target = "ACCEPT";
+              interfaces = cfg.trustedInterfaces;
+            };
           };
         };
       };
