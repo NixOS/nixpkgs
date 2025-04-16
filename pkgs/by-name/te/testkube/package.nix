fb3d1b637339f75c6482129784fd7735e05c2a33
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-16 17:35:25 +0000

    testkube: 2.1.125 -> 2.1.141

--- pkgs/by-name/te/testkube/package.nix
+++ pkgs/by-name/te/testkube/package.nix
@@ -5,16 +5,16 @@
 }:
 buildGoModule rec {
   pname = "testkube";
-  version = "2.1.125";
+  version = "2.1.141";
 
   src = fetchFromGitHub {
     owner = "kubeshop";
     repo = "testkube";
     rev = "v${version}";
-    hash = "sha256-Coi/aPEZb1VoMCOtzl2AcURomo4uS76AZDS449B4EcU=";
+    hash = "sha256-oiy0PrGwFuY3C8esP7wbwwnHdXmR+xM6oGJIzGYFBU4=";
   };
 
-  vendorHash = "sha256-Z8or7JvTBZQmka71I2IpGldgnyTeh4O0aIlB/wssYoM=";
+  vendorHash = "sha256-lS6RLag9HZSu08Bv0p6qgp2wnsnJpJEUn6P4eeAUXWo=";
 
   ldflags = [
     "-X main.version=${version}"
