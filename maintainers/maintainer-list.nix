2644c9b598c0115e9fbdf630c7c2d7dc2289a3e2
jf-uu <181011550+jf-uu@users.noreply.github.com>
2025-04-16 18:36:19 +0100

    maintainers: add jf-uu

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11555,6 +11555,11 @@
     githubId = 110620;
     name = "Jevin Maltais";
   };
+  jf-uu = {
+    github = "jf-uu";
+    githubId = 181011550;
+    name = "jf-uu";
+  };
   jfchevrette = {
     email = "jfchevrette@gmail.com";
     github = "jfchevrette";
