e9d0f34404f83e300aea35700a6e0e184a068f96
Florian Klink <flokli@flokli.de>
2025-04-16 14:43:39 +0200

    signal-desktop-source: rename to signal-desktop
    This effectively migrates users of the `signal-desktop` attribute to the
    from-source build.
    
    We confirmed the from source build is also able to parse state produced
    by the binary version (not just with pretty empty state, but also bigger
    states with more historical data).
    
    Rename the binary variant to `signal-desktop-bin`, consistent with how
    this is handled in other places in nixpkgs. Also update the pname to
    reflect that.

--- pkgs/by-name/si/signal-desktop-bin/update.sh
+++ pkgs/by-name/si/signal-desktop-bin/update.sh
@@ -17,17 +17,17 @@ latestBuildAarch64=$(jq '.id' <<< $latestBuildInfoAarch64)
 latestVersionAarch64=$(jq -r '.source_package.version' <<< $latestBuildInfoAarch64)
 
 echo "Updating signal-desktop for x86_64-linux"
-update-source-version signal-desktop "$latestVersion" \
+update-source-version signal-desktop-bin "$latestVersion" \
   --system=x86_64-linux \
   --file="$SCRIPT_DIR/signal-desktop.nix"
 
 echo "Updating signal-desktop for aarch64-linux"
-update-source-version signal-desktop "$latestVersionAarch64" "" \
+update-source-version signal-desktop-bin "$latestVersionAarch64" "" \
   "https://download.copr.fedorainfracloud.org/results/useidel/signal-desktop/fedora-42-aarch64/$(printf "%08d" $latestBuildAarch64)-signal-desktop/signal-desktop-$latestVersionAarch64.fc42.aarch64.rpm" \
   --system=aarch64-linux \
   --file="$SCRIPT_DIR/signal-desktop-aarch64.nix"
 
 echo "Updating signal-desktop for darwin"
-update-source-version signal-desktop "$latestVersion" \
+update-source-version signal-desktop-bin "$latestVersion" \
   --system=aarch64-darwin \
   --file="$SCRIPT_DIR/signal-desktop-darwin.nix"
