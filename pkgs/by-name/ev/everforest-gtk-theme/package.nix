a935b9dbe693f535caf3e95845a52736551cb1e2
Sena <contact@sena.pink>
2025-04-17 00:13:32 +0300

    everforest-gtk-theme: format using nixfmt

--- pkgs/by-name/ev/everforest-gtk-theme/package.nix
+++ pkgs/by-name/ev/everforest-gtk-theme/package.nix
@@ -49,69 +49,81 @@ let
     "Light"
   ];
 in
-lib.checkListOfEnum "${pname}: colorVariants" colorVariantList colorVariants
-lib.checkListOfEnum "${pname}: sizeVariants" sizeVariantList sizeVariants
-lib.checkListOfEnum "${pname}: themeVariants" themeVariantList themeVariants
-lib.checkListOfEnum "${pname}: tweakVariants" tweakVariantList tweakVariants
-lib.checkListOfEnum "${pname}: iconVariants" iconVariantList iconVariants
+lib.checkListOfEnum "${pname}: colorVariants" colorVariantList colorVariants lib.checkListOfEnum
+  "${pname}: sizeVariants"
+  sizeVariantList
+  sizeVariants
+  lib.checkListOfEnum
+  "${pname}: themeVariants"
+  themeVariantList
+  themeVariants
+  lib.checkListOfEnum
+  "${pname}: tweakVariants"
+  tweakVariantList
+  tweakVariants
+  lib.checkListOfEnum
+  "${pname}: iconVariants"
+  iconVariantList
+  iconVariants
 
-stdenvNoCC.mkDerivation {
-  inherit pname;
-  version = "0-unstable-2025-04-11";
+  stdenvNoCC.mkDerivation
+  {
+    inherit pname;
+    version = "0-unstable-2025-04-11";
 
-  src = fetchFromGitHub {
-    owner = "Fausto-Korpsvart";
-    repo = "Everforest-GTK-Theme";
-    rev = "dbd1014f4f3b66d5258bf81e9135e0e75cea084d";
-    hash = "sha256-QfawNkstj3cdIEN60dLrk3a1U4lNTclF7NLB++PrnHE=";
-  };
+    src = fetchFromGitHub {
+      owner = "Fausto-Korpsvart";
+      repo = "Everforest-GTK-Theme";
+      rev = "dbd1014f4f3b66d5258bf81e9135e0e75cea084d";
+      hash = "sha256-QfawNkstj3cdIEN60dLrk3a1U4lNTclF7NLB++PrnHE=";
+    };
 
-  propagatedUserEnvPkgs = [
-    gtk-engine-murrine
-  ];
+    propagatedUserEnvPkgs = [
+      gtk-engine-murrine
+    ];
 
-  nativeBuildInputs = [
-    gnome-shell
-    sassc
-  ];
-  buildInputs = [
-    gnome-themes-extra
-  ];
+    nativeBuildInputs = [
+      gnome-shell
+      sassc
+    ];
+    buildInputs = [
+      gnome-themes-extra
+    ];
 
-  dontBuild = true;
+    dontBuild = true;
 
-  passthru.updateScript = unstableGitUpdater { };
+    passthru.updateScript = unstableGitUpdater { };
 
-  postPatch = ''
-    patchShebangs themes/install.sh
-  '';
+    postPatch = ''
+      patchShebangs themes/install.sh
+    '';
 
-  installPhase = ''
-    runHook preInstall
+    installPhase = ''
+      runHook preInstall
 
-    cd themes
-    mkdir -p "$out/share/themes"
-    ./install.sh -n Everforest \
-      ${lib.optionalString (colorVariants != [ ]) "-c " + toString colorVariants} \
-      ${lib.optionalString (sizeVariants != [ ]) "-s " + toString sizeVariants} \
-      ${lib.optionalString (themeVariants != [ ]) "-t " + toString themeVariants} \
-      ${lib.optionalString (tweakVariants != [ ]) "--tweaks " + toString tweakVariants} \
-      -d "$out/share/themes"
+      cd themes
+      mkdir -p "$out/share/themes"
+      ./install.sh -n Everforest \
+        ${lib.optionalString (colorVariants != [ ]) "-c " + toString colorVariants} \
+        ${lib.optionalString (sizeVariants != [ ]) "-s " + toString sizeVariants} \
+        ${lib.optionalString (themeVariants != [ ]) "-t " + toString themeVariants} \
+        ${lib.optionalString (tweakVariants != [ ]) "--tweaks " + toString tweakVariants} \
+        -d "$out/share/themes"
 
-    cd ../icons
-    ${lib.optionalString (iconVariants != [ ]) ''
-      mkdir -p "$out/share/icons"
-      cp -a ${toString (map (v: "Everforest-${v}") iconVariants)} "$out/share/icons"
-    ''}
+      cd ../icons
+      ${lib.optionalString (iconVariants != [ ]) ''
+        mkdir -p "$out/share/icons"
+        cp -a ${toString (map (v: "Everforest-${v}") iconVariants)} "$out/share/icons"
+      ''}
 
-    runHook postInstall
-  '';
+      runHook postInstall
+    '';
 
-  meta = {
-    description = "Everforest colour palette for GTK";
-    homepage = "https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme";
-    license = lib.licenses.gpl3Plus;
-    maintainers = with lib.maintainers; [ jn-sena ];
-    platforms = lib.platforms.unix;
-  };
-}
+    meta = {
+      description = "Everforest colour palette for GTK";
+      homepage = "https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme";
+      license = lib.licenses.gpl3Plus;
+      maintainers = with lib.maintainers; [ jn-sena ];
+      platforms = lib.platforms.unix;
+    };
+  }
