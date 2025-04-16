3206197960cf939e7bcd5c6d9227ee608701bf0f
Sena <contact@sena.pink>
2025-04-16 23:58:30 +0300

    everforest-gtk-theme: add variants

--- pkgs/by-name/ev/everforest-gtk-theme/package.nix
+++ pkgs/by-name/ev/everforest-gtk-theme/package.nix
@@ -2,12 +2,60 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
+  gnome-shell,
+  sassc,
   gnome-themes-extra,
   gtk-engine-murrine,
+  colorVariants ? [ ],
+  sizeVariants ? [ ],
+  themeVariants ? [ ],
+  tweakVariants ? [ ],
+  iconVariants ? [ ],
 }:
 
-stdenvNoCC.mkDerivation {
+let
   pname = "everforest-gtk-theme";
+  colorVariantList = [
+    "dark"
+    "light"
+  ];
+  sizeVariantList = [
+    "compact"
+    "standard"
+  ];
+  themeVariantList = [
+    "default"
+    "green"
+    "grey"
+    "orange"
+    "pink"
+    "purple"
+    "red"
+    "teal"
+    "yellow"
+    "all"
+  ];
+  tweakVariantList = [
+    "medium"
+    "soft"
+    "black"
+    "float"
+    "outline"
+    "macos"
+  ];
+  iconVariantList = [
+    "Dark"
+    "Light"
+  ];
+in
+lib.checkListOfEnum "${pname}: colorVariants" colorVariantList colorVariants
+lib.checkListOfEnum "${pname}: sizeVariants" sizeVariantList sizeVariants
+lib.checkListOfEnum "${pname}: themeVariants" themeVariantList themeVariants
+lib.checkListOfEnum "${pname}: tweakVariants" tweakVariantList tweakVariants
+lib.checkListOfEnum "${pname}: iconVariants" iconVariantList iconVariants
+
+stdenvNoCC.mkDerivation {
+  inherit pname;
   version = "0-unstable-2023-03-20";
 
   src = fetchFromGitHub {
@@ -21,17 +69,38 @@ stdenvNoCC.mkDerivation {
     gtk-engine-murrine
   ];
 
+  nativeBuildInputs = [
+    gnome-shell
+    sassc
+  ];
   buildInputs = [
     gnome-themes-extra
   ];
 
   dontBuild = true;
 
+  postPatch = ''
+    patchShebangs themes/install.sh
+  '';
+
   installPhase = ''
     runHook preInstall
-    mkdir -p "$out/share/"{themes,icons}
-    cp -a icons/* "$out/share/icons/"
-    cp -a themes/* "$out/share/themes/"
+
+    cd themes
+    mkdir -p "$out/share/themes"
+    ./install.sh -n Everforest \
+      ${lib.optionalString (colorVariants != [ ]) "-c" + toString colorVariants} \
+      ${lib.optionalString (sizeVariants != [ ]) "-s" + toString sizeVariants} \
+      ${lib.optionalString (themeVariants != [ ]) "-t" + toString themeVariants} \
+      ${lib.optionalString (tweakVariants != [ ]) "--tweaks" + toString tweakVariants} \
+      -d "$out/share/themes"
+
+    cd ../icons
+    ${lib.optionalString (iconVariants != [ ]) ''
+      mkdir -p "$out/share/icons"
+      cp -a ${toString (map (v: "Everforest-${v}") iconVariants)} "$out/share/icons"
+    ''}
+
     runHook postInstall
   '';
 
