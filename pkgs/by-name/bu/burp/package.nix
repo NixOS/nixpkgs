da99bbdf8f50c0927e810cdcf66029dffbe2d29c
emaryn <emaryn@users.noreply.github.com>
2025-04-16 15:55:40 +0800

    burp: 2.4.0 -> 3.1.4
    Diff: https://github.com/grke/burp/compare/2.4.0...3.1.4

--- pkgs/by-name/bu/burp/package.nix
+++ pkgs/by-name/bu/burp/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   autoreconfHook,
   pkg-config,
   acl,
@@ -13,30 +12,22 @@
   uthash,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "burp";
-  version = "2.4.0";
+  version = "3.1.4";
 
   src = fetchFromGitHub {
     owner = "grke";
     repo = "burp";
-    rev = version;
-    sha256 = "sha256-y6kRd1jD6t+Q6d5t7W9MDuk+m2Iq1THQkP50PJwI7Nc=";
+    tag = finalAttrs.version;
+    hash = "sha256-/vYon0XUIuMAaaaRNehzMspKMHWp0tJm8JubRt1KmZU=";
   };
-
-  patches = [
-    # Pull upstream fix for ncurses-6.3 support
-    (fetchpatch {
-      name = "ncurses-6.3.patch";
-      url = "https://github.com/grke/burp/commit/1d6c931af7c11f164cf7ad3479781e8f03413496.patch";
-      sha256 = "14sfbfahlankz3xg6v10i8fnmpnmqpp73q9xm0l0hnjh25igv6bl";
-    })
-  ];
 
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
   ];
+
   # use openssl_legacy due to burp-2.4.0 not supporting file encryption with openssl 3.0
   # replace with 'openssl' once burp-3.x has been declared stable and this package upgraded
   buildInputs = [
@@ -47,15 +38,18 @@ stdenv.mkDerivation rec {
     uthash
   ] ++ lib.optional (!stdenv.hostPlatform.isDarwin) acl;
 
-  configureFlags = [ "--localstatedir=/var" ];
+  configureFlags = [
+    "--sysconfdir=/etc/burp"
+    "--localstatedir=/var"
+  ];
 
   installFlags = [ "localstatedir=/tmp" ];
 
-  meta = with lib; {
+  meta = {
     description = "BURP - BackUp and Restore Program";
     homepage = "https://burp.grke.org";
-    license = licenses.agpl3Plus;
-    maintainers = with maintainers; [ arjan-s ];
-    platforms = platforms.all;
+    license = lib.licenses.agpl3Plus;
+    maintainers = with lib.maintainers; [ arjan-s ];
+    platforms = lib.platforms.all;
   };
-}
+})
