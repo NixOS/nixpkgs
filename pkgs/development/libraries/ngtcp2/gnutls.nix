3f4340a53d986f9479b84212c2931ec0b01989ea
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-16 13:24:26 +0000

    ngtcp2-gnutls: 1.11.0 -> 1.12.0

--- pkgs/development/libraries/ngtcp2/gnutls.nix
+++ pkgs/development/libraries/ngtcp2/gnutls.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ngtcp2";
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchFromGitHub {
     owner = "ngtcp2";
     repo = "ngtcp2";
     rev = "v${version}";
-    hash = "sha256-m9kJrEDYfxPSreH14M/h/f+7vgk6Ho6pmvVdJ1S8acc=";
+    hash = "sha256-WAZKlIGwSn/U3MPF2RDOscZ5EVqQyViViy9beQeroNs=";
   };
 
   outputs = [
