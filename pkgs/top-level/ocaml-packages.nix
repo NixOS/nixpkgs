ca8e5a200851fbcb841714788de9ac4c867f0e2f
Yifei Sun <ysun@hey.com>
2025-04-17 17:28:33 -0400

    ocamlPackages.linol: 0.6 -> 0.10

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1138,6 +1138,8 @@ let
 
         linol = callPackage ../development/ocaml-modules/linol { };
 
+        linol-eio = callPackage ../development/ocaml-modules/linol/eio.nix { };
+
         linol-lwt = callPackage ../development/ocaml-modules/linol/lwt.nix { };
 
         llvm = callPackage ../development/ocaml-modules/llvm {
