6e994a40878922ad59a538d4bee2a51af3d832b2
Martin Joerg <martin.joerg@gmail.com>
2025-04-17 15:45:31 +0000

    kdePackages.itinerary: make compatible with Qt 6.9

--- pkgs/kde/gear/itinerary/default.nix
+++ pkgs/kde/gear/itinerary/default.nix
@@ -1,5 +1,6 @@
 {
   mkKdeDerivation,
+  fetchpatch,
   pkg-config,
   qtlocation,
   qtpositioning,
@@ -9,8 +10,31 @@
 mkKdeDerivation {
   pname = "itinerary";
 
-  # FIXME: this should really be fixed at ECM level somehow
-  patches = [ ./optional-runtime-dependencies.patch ];
+  patches = [
+    # FIXME: this should really be fixed at ECM level somehow
+    ./optional-runtime-dependencies.patch
+    # make compatible with Qt 6.9 (QTBUG-98101)
+    (fetchpatch {
+      name = "Make-code-work-with-Qt-6.9-too.patch";
+      url = "https://invent.kde.org/pim/itinerary/-/commit/4ac22f865b0577f0a11440417e416670231cf9dd.patch";
+      hash = "sha256-JiDuGD71g0xtGolbP2NrXIW0owjJy5JeguGFwbLFAys=";
+    })
+  ];
+
+  # make compatible with Qt 6.9 (QTBUG-98101)
+  postPatch = ''
+    substituteInPlace src/app/PassPage.qml \
+      --replace-fail \
+        'import Qt.labs.qmlmodels as Models' \
+        'import Qt.labs.qmlmodels
+    import QtQml.Models' \
+      --replace-fail \
+        'Models.DelegateChoice' \
+        'DelegateChoice' \
+      --replace-fail \
+        'Models.DelegateChooser' \
+        'DelegateChooser'
+  '';
 
   extraNativeBuildInputs = [
     pkg-config
