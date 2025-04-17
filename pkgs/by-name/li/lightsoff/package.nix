27289f333ecb866a925c40029922cde4b9158837
Jan Tojnar <jtojnar@gmail.com>
2025-04-17 19:15:16 +0000

    lightsoff: 48.0 → 48.1
    https://gitlab.gnome.org/GNOME/lightsoff/-/compare/48.0...48.1
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/by-name/li/lightsoff/package.nix
+++ pkgs/by-name/li/lightsoff/package.nix
@@ -20,11 +20,11 @@
 
 stdenv.mkDerivation rec {
   pname = "lightsoff";
-  version = "48.0";
+  version = "48.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/lightsoff/${lib.versions.major version}/lightsoff-${version}.tar.xz";
-    hash = "sha256-3mkpt0vcnC68Pz9Sx8sxQsCf1fPSZk45CmOH3a4MdoQ=";
+    hash = "sha256-LsmVAXE9vNE8WlZaLhGMxMwrUCg2s4enc2z7pAqLOYk=";
   };
 
   nativeBuildInputs = [
