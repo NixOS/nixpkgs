b17b17382e771aa6e6043e109b071ecc57a13a44
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-17 20:47:42 +0000

    source-meta-json-schema: 8.0.0 -> 9.1.0

--- pkgs/by-name/so/source-meta-json-schema/package.nix
+++ pkgs/by-name/so/source-meta-json-schema/package.nix
@@ -5,7 +5,7 @@
   cmake,
 }:
 let
-  version = "8.0.0";
+  version = "9.1.0";
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "source-meta-json-schema";
@@ -15,7 +15,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "sourcemeta";
     repo = "jsonschema";
     rev = "v${version}";
-    hash = "sha256-EH+wi8MAgAxTy7OPQK/faX6OVY38/Z5fXhaK92xKkyA=";
+    hash = "sha256-YxIRDTAAvkltT4HGUvPt/davarGRfE808OoI9UheqCA=";
   };
 
   nativeBuildInputs = [
