e383618893a66971485ce2f4ead3ac9c320a1279
Jan Tojnar <jtojnar@gmail.com>
2025-04-17 19:14:24 +0000

    gnome-software: 48.0 → 48.1
    https://gitlab.gnome.org/GNOME/gnome-software/-/compare/48.0...48.1
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/by-name/gn/gnome-software/package.nix
+++ pkgs/by-name/gn/gnome-software/package.nix
@@ -48,11 +48,11 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-software";
-  version = "48.0";
+  version = "48.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-software/${lib.versions.major finalAttrs.version}/gnome-software-${finalAttrs.version}.tar.xz";
-    hash = "sha256-5gevVU6Dj9bQfBYx9jSyDovU5q3xb8dTXEUgh0r1RPc=";
+    hash = "sha256-CEqYUEApTDZFS0ZKIUT5gcAnSQa0xJ1xYT5hztapbo8=";
   };
 
   patches = [
