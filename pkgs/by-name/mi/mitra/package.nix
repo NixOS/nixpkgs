4e30fe14b2f9d43f5f2397cb871831fb1bb95f5b
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-17 03:31:15 +0000

    mitra: 3.22.0 -> 3.23.0

--- pkgs/by-name/mi/mitra/package.nix
+++ pkgs/by-name/mi/mitra/package.nix
@@ -6,18 +6,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mitra";
-  version = "3.22.0";
+  version = "3.23.0";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "silverpill";
     repo = "mitra";
     rev = "v${version}";
-    hash = "sha256-TWU8df6IjzyvRyMLt3YE9Uuc9PoZBbzF62ojRaLzsvw=";
+    hash = "sha256-QE/K9Hhu7e1y1JzuA8YxRTGDV6uAoSA+h1iWXH/4M8g=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-kRGhf/B6ni4gtl28ycQgIb5z+OP3CEHonTNId2MZsW4=";
+  cargoHash = "sha256-JmX4Cbpj5gA0XWA5NQLfyFAQnoJmRCOaiNew96sWABI=";
 
   # require running database
   doCheck = false;
