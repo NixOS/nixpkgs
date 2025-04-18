983a9c26d1c09380f41daf8cff5a8f6ec91f3b78
Sergei Zimmerman <xokdvium@proton.me>
2025-04-18 21:22:50 +0000

    otf2: fix cross

--- pkgs/by-name/ot/otf2/package.nix
+++ pkgs/by-name/ot/otf2/package.nix
@@ -2,6 +2,8 @@
   lib,
   stdenv,
   fetchurl,
+  buildPackages,
+  which,
   versionCheckHook,
 }:
 
@@ -14,8 +16,29 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-Wk4BOlGsTteU/jXFW3AM1yA0b9p/M+yEx2uGpfuICm4=";
   };
 
-  configureFlags = [
-    "--enable-backend-test-runs"
+  postPatch = ''
+    substituteInPlace build-config/common/platforms/platform-backend-user-provided \
+      --replace-fail 'CC=' 'CC=${stdenv.cc.targetPrefix}cc' \
+      --replace-fail 'CXX=' 'CXX=${stdenv.cc.targetPrefix}c++'
+    substituteInPlace build-config/common/platforms/platform-frontend-user-provided \
+      --replace-fail 'CC_FOR_BUILD=' 'CC_FOR_BUILD=${buildPackages.stdenv.cc.targetPrefix}cc' \
+      --replace-fail 'CXX_FOR_BUILD=' 'CXX_FOR_BUILD=${buildPackages.stdenv.cc.targetPrefix}c++'
+  '';
+
+  strictDeps = true;
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+
+  configureFlags =
+    [
+      (lib.enableFeature finalAttrs.finalPackage.doCheck "backend-test-runs")
+      (lib.withFeature true "custom-compilers")
+    ]
+    ++ lib.optionals (!lib.systems.equals stdenv.buildPlatform stdenv.hostPlatform) [
+      "ac_scorep_cross_compiling=yes"
+    ];
+
+  nativeBuildInputs = [
+    which # used in configure script
   ];
 
   enableParallelBuilding = true;
