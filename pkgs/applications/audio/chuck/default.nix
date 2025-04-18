b8c5f615ca456cc0bf0b434d844064cd8587f35e
emaryn <emaryn@users.noreply.github.com>
2025-04-19 02:38:08 +0800

    chuck: 1.4.2.0 -> 1.5.5.0

--- pkgs/applications/audio/chuck/default.nix
+++ pkgs/applications/audio/chuck/default.nix
@@ -1,6 +1,6 @@
 {
-  stdenv,
   lib,
+  stdenv,
   fetchurl,
   alsa-lib,
   bison,
@@ -18,13 +18,13 @@
   MultitouchSupport,
 }:
 
-stdenv.mkDerivation rec {
-  version = "1.4.2.0";
+stdenv.mkDerivation (finalAttrs: {
   pname = "chuck";
+  version = "1.5.5.0";
 
   src = fetchurl {
-    url = "http://chuck.cs.princeton.edu/release/files/chuck-${version}.tgz";
-    sha256 = "sha256-hIwsC9rYgXWSTFqUufKGqoT0Gnsf4nR4KQ0iSVbj8xg=";
+    url = "http://chuck.cs.princeton.edu/release/files/chuck-${finalAttrs.version}.tgz";
+    hash = "sha256-jjWBCtTBybFy5+YZgPRJaUOW/AQAu1Zij69Px4f46gY=";
   };
 
   nativeBuildInputs =
@@ -51,20 +51,19 @@ stdenv.mkDerivation rec {
       MultitouchSupport
     ];
 
-  patches = [ ./darwin-limits.patch ];
-
   makeFlags = [
     "-C src"
     "DESTDIR=$(out)/bin"
   ];
+
   buildFlags = [ (if stdenv.hostPlatform.isDarwin then "mac" else "linux-alsa") ];
 
-  meta = with lib; {
+  meta = {
     description = "Programming language for real-time sound synthesis and music creation";
     homepage = "http://chuck.cs.princeton.edu";
-    license = licenses.gpl2;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ ftrvxmtrx ];
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [ ftrvxmtrx ];
     mainProgram = "chuck";
   };
-}
+})
