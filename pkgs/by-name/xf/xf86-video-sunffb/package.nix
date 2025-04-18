5d33ec209a9ac423c538fd3054f66f6554480727
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-04-18 00:32:44 -0400

    xorg.*: remove several darwin exclusions for several packages
    These are now successfully built because xorg.xorgserver now ships
    with xorg-server.pc as expected.
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- pkgs/by-name/xf/xf86-video-sunffb/package.nix
+++ pkgs/by-name/xf/xf86-video-sunffb/package.nix
@@ -50,6 +50,5 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.mit;
     maintainers = [ ];
     platforms = lib.platforms.unix;
-    broken = stdenv.hostPlatform.isDarwin; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xf86-video-sunffb.x86_64-darwin
   };
 })
