550a552de2f415ea9419085ce8c2ea4c039e06e5
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-18 09:21:05 +0000

    python312Packages.pytenable: 1.7.4 -> 1.7.5

--- pkgs/development/python-modules/pytenable/default.nix
+++ pkgs/development/python-modules/pytenable/default.nix
@@ -28,7 +28,7 @@
 
 buildPythonPackage rec {
   pname = "pytenable";
-  version = "1.7.4";
+  version = "1.7.5";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -37,7 +37,7 @@ buildPythonPackage rec {
     owner = "tenable";
     repo = "pyTenable";
     tag = version;
-    hash = "sha256-kIjAmGtfOZSNqSKOmagttfzVt2onqVwrCkYM6SCOlCg=";
+    hash = "sha256-oTFlIDlntbB2YwE1zuU9DjouPncgIMKU+lDf5bcPKiQ=";
   };
 
   pythonRelaxDeps = [
@@ -94,7 +94,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library for the Tenable.io and TenableSC API";
     homepage = "https://github.com/tenable/pyTenable";
-    changelog = "https://github.com/tenable/pyTenable/releases/tag/${version}";
+    changelog = "https://github.com/tenable/pyTenable/releases/tag/${src.tag}";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
   };
