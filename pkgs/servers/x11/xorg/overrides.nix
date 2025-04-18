2bc20f189332801265eed684f71e8717aac0e05b
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-04-18 00:32:44 -0400

    xorg.*: remove several darwin exclusions for several packages
    These are now successfully built because xorg.xorgserver now ships
    with xorg-server.pc as expected.
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- pkgs/servers/x11/xorg/overrides.nix
+++ pkgs/servers/x11/xorg/overrides.nix
@@ -73,14 +73,6 @@ let
         inherit mainProgram;
       };
     });
-
-  brokenOnDarwin =
-    pkg:
-    pkg.overrideAttrs (attrs: {
-      meta = attrs.meta // {
-        broken = isDarwin;
-      };
-    });
 in
 self: super:
 {
@@ -163,7 +155,7 @@ self: super:
       "--with-sdkdir=${placeholder "out"}/include/xorg"
     ];
     meta = attrs.meta // {
-      broken = isDarwin; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xorg.xf86inputmouse.x86_64-darwin
+      broken = isDarwin; # no darwin driver
     };
   });
 
@@ -172,7 +164,7 @@ self: super:
       "--with-sdkdir=${placeholder "out"}/include/xorg"
     ];
     meta = attrs.meta // {
-      broken = isDarwin; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xorg.xf86inputjoystick.x86_64-darwin
+      broken = isDarwin; # no darwin driver
     };
   });
 
@@ -217,9 +209,6 @@ self: super:
       ];
     };
   });
-
-  xf86inputvoid = brokenOnDarwin super.xf86inputvoid; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xorg.xf86inputvoid.x86_64-darwin
-  xf86videodummy = brokenOnDarwin super.xf86videodummy; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xorg.xf86videodummy.x86_64-darwin
 
   # Obsolete drivers that don't compile anymore.
   xf86videoark = super.xf86videoark.overrideAttrs (attrs: {
@@ -298,24 +287,6 @@ self: super:
     meta = attrs.meta // {
       broken = true;
     };
-  });
-
-  xf86videosuncg6 = super.xf86videosuncg6.overrideAttrs (attrs: {
-    meta = attrs.meta // {
-      broken = isDarwin;
-    }; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xorg.xf86videosuncg6.x86_64-darwin
-  });
-
-  xf86videosunffb = super.xf86videosunffb.overrideAttrs (attrs: {
-    meta = attrs.meta // {
-      broken = isDarwin;
-    }; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xorg.xf86videosunffb.x86_64-darwin
-  });
-
-  xf86videosunleo = super.xf86videosunleo.overrideAttrs (attrs: {
-    meta = attrs.meta // {
-      broken = isDarwin;
-    }; # never worked: https://hydra.nixos.org/job/nixpkgs/trunk/xorg.xf86videosunleo.x86_64-darwin
   });
 
   xf86videovmware = super.xf86videovmware.overrideAttrs (attrs: {
