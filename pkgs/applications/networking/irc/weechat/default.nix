c61b352e719c69e0ea6c09f73c3ae717a8a177a9
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-18 20:56:35 +0000

    weechat-unwrapped: 4.5.1 -> 4.6.1

--- pkgs/applications/networking/irc/weechat/default.nix
+++ pkgs/applications/networking/irc/weechat/default.nix
@@ -100,14 +100,14 @@ let
 in
 assert lib.all (p: p.enabled -> !(builtins.elem null p.buildInputs)) plugins;
 stdenv.mkDerivation rec {
-  version = "4.5.1";
+  version = "4.6.1";
   pname = "weechat";
 
   hardeningEnable = [ "pie" ];
 
   src = fetchurl {
     url = "https://weechat.org/files/src/weechat-${version}.tar.xz";
-    hash = "sha256-Z8FDx7xw5om56obfZ0yan/PPRMzJzf8hvmpWHV6vxSg=";
+    hash = "sha256-1DRL2Ban8d2yHqf7gTWvh767y7nhuDYs10MpAdGQIGU=";
   };
 
   # Why is this needed? https://github.com/weechat/weechat/issues/2031
