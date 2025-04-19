e414df13d86b427d59e99c8f3d1354e1659ed8dc
Defelo <mail@defelo.de>
2025-04-19 12:36:28 +0000

    uiua-unstable: 0.15.1 -> 0.16.0-dev.1

--- pkgs/by-name/ui/uiua/unstable.nix
+++ pkgs/by-name/ui/uiua/unstable.nix
@@ -1,7 +1,7 @@
 rec {
-  version = "0.15.1";
+  version = "0.16.0-dev.1";
   tag = version;
-  hash = "sha256-PTpGjJCTqc8bgCfKoq8Bh5eb5vWc1ZjWtdZXMY2j2f8=";
-  cargoHash = "sha256-UAwlnJsZfHnKzchhn79cBsX3iRPV+N52szRMiv2NBno=";
+  hash = "sha256-fW8ntGSK6MX6shXqnKxLD52vDovFol+/tZtr0Hw5Btc=";
+  cargoHash = "sha256-1pcDM7VUVqigBeJ+loEUEOOPlggOsYir/Dge663UtSQ=";
   updateScript = ./update-unstable.sh;
 }
