ef788799ae985100201bcd996f9fff94260293c5
Tom Herbers <mail@tomherbers.de>
2025-04-20 01:03:22 +0200

    borgbackup: 1.4.0 -> 1.4.1
    Changelog: https://github.com/borgbackup/borg/blob/1.4.1/docs/changes.rst#version-141-2025-04-19
    (cherry picked from commit 91a4e191ea8ff52d9a81768e58e3de59dd685b78)

--- pkgs/by-name/bo/borgbackup/package.nix
+++ pkgs/by-name/bo/borgbackup/package.nix
@@ -20,14 +20,14 @@ let
 in
 python.pkgs.buildPythonApplication rec {
   pname = "borgbackup";
-  version = "1.4.0";
+  version = "1.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "borgbackup";
     repo = "borg";
     tag = version;
-    hash = "sha256-n1hCM7Sp0t2bOJEzErEd1PS/Xc7c+KDmJ4PjQuuF140=";
+    hash = "sha256-1RRizsHY6q1ruofTkRZ4sSN4k6Hoo+sG85w2zz+7yL8=";
   };
 
   postPatch = ''
@@ -45,6 +45,7 @@ python.pkgs.buildPythonApplication rec {
   nativeBuildInputs = with python.pkgs; [
     # docs
     sphinxHook
+    sphinxcontrib-jquery
     guzzle-sphinx-theme
 
     # shell completions
@@ -77,6 +78,13 @@ python.pkgs.buildPythonApplication rec {
   makeWrapperArgs = [
     ''--prefix PATH ':' "${openssh}/bin"''
   ];
+
+  preInstallSphinx = ''
+    # remove invalid outputs for manpages
+    rm .sphinx/man/man/_static/jquery.js
+    rm .sphinx/man/man/_static/_sphinx_javascript_frameworks_compat.js
+    rmdir .sphinx/man/man/_static/
+  '';
 
   postInstall = ''
     installShellCompletion --cmd borg \
