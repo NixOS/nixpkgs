16b1cdc350b4075b947e0d42abbf6223a9b26d7f
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-19 20:30:04 +0000

    vscode-extensions.rooveterinaryinc.roo-cline: 3.13.0 -> 3.13.2

--- pkgs/applications/editors/vscode/extensions/rooveterinaryinc.roo-cline/default.nix
+++ pkgs/applications/editors/vscode/extensions/rooveterinaryinc.roo-cline/default.nix
@@ -8,8 +8,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     publisher = "RooVeterinaryInc";
     name = "roo-cline";
-    version = "3.13.0";
-    hash = "sha256-vVl8hMMpCrLP+/U6KJ3Qz/q0OYgQuEMfu8UzpScER8o=";
+    version = "3.13.2";
+    hash = "sha256-ezmBS84RonkqlguLcgGZa4663GpY6yReKKb+r6YzQms=";
   };
 
   passthru.updateScript = vscode-extensions-update-script { };
