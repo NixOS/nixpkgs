0bb6a910f5caf7e8bba4afbdc5b0af0c272ebcb5
fettgoenner <paulmatti@protonmail.com>
2025-04-19 11:02:49 +0200

    scipopt-papilo: 2.4.1 -> 2.4.2

--- pkgs/by-name/sc/scipopt-papilo/package.nix
+++ pkgs/by-name/sc/scipopt-papilo/package.nix
@@ -12,16 +12,16 @@
 
 stdenv.mkDerivation rec {
   pname = "scipopt-papilo";
-  version = "2.4.1";
+  version = "2.4.2";
 
   # To correlate scipVersion and version, check: https://scipopt.org/#news
-  scipVersion = "9.2.1";
+  scipVersion = "9.2.2";
 
   src = fetchFromGitHub {
     owner = "scipopt";
     repo = "papilo";
     tag = "v${version}";
-    hash = "sha256-oQ9iq5UkFK0ghUx6uxdJIOo5niQjniHegSZptqi2fgE=";
+    hash = "sha256-/1AsAesUh/5YXeCU2OYopoG3SXAwAecPD88QvGkb2bY=";
   };
 
   nativeBuildInputs = [ cmake ];
@@ -42,10 +42,6 @@ stdenv.mkDerivation rec {
     #   > include/boost/multiprecision/mpfr.hpp:22: fatal error: mpfr.h: No such file or directory
     #   > compilation terminated.
     (lib.cmakeBool "SOPLEX" false)
-
-    # (lib.cmakeBool "GMP" true)
-    # (lib.cmakeBool "QUADMATH" true)
-    # (lib.cmakeBool "TBB" true)
   ];
   doCheck = true;
   meta = {
