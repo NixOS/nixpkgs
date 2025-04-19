7cf7a3869ac9575156e8cbaa55f034f8e6c1b4d2
Sergei Zimmerman <xokdvium@proton.me>
2025-04-19 11:19:37 +0000

    eztrace: add meta.homepage

--- pkgs/by-name/ez/eztrace/package.nix
+++ pkgs/by-name/ez/eztrace/package.nix
@@ -71,6 +71,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Tool that aims at generating automatically execution trace from HPC programs";
+    homepage = "https://eztrace.gitlab.io/eztrace/index.html";
     license = lib.licenses.cecill-b;
     maintainers = [ lib.maintainers.xokdvium ];
     mainProgram = "eztrace";
