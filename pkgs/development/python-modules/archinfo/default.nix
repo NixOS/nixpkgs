aa3778882fd25b274f48fdab83fd2658fe5719e8
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-19 12:22:42 +0200

    python313Packages.archinfo: 9.2.149 -> 9.2.150
    Diff: https://github.com/angr/archinfo/compare/refs/tags/v9.2.149...refs/tags/v9.2.150

--- pkgs/development/python-modules/archinfo/default.nix
+++ pkgs/development/python-modules/archinfo/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "archinfo";
-  version = "9.2.149";
+  version = "9.2.150";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "archinfo";
     tag = "v${version}";
-    hash = "sha256-hP7rQOEl2BuzNIGT/+S7Gwp6UdLcwWKagq1wCcWV+L4=";
+    hash = "sha256-FCawauMXhUfPsMdpRDgbZ5mUBGDBAwp1mVWkJ+UTuxs=";
   };
 
   build-system = [ setuptools ];
