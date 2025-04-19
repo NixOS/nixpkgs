266c18ee538c68dfa9e3ab50854581e1e7c43627
Bobby Rong <rjl931189261@126.com>
2025-04-19 13:50:27 +0800

    fixup! xfce.tumbler: backport PDF thumbnail generation fix
    Small nitpicks but I don't want to block merge :-)
    I will squash merge this.

--- pkgs/desktops/xfce/core/tumbler/default.nix
+++ pkgs/desktops/xfce/core/tumbler/default.nix
@@ -26,6 +26,16 @@ mkXfceDerivation {
 
   sha256 = "sha256-GmEMdG8Ikd4Tq/1ntCHiN0S7ehUXqzMX7OtXsycLd6E=";
 
+  patches = [
+    # Fixes PDF previews staying low resolution
+    # https://gitlab.xfce.org/xfce/tumbler/-/merge_requests/35
+    (fetchpatch {
+      name = "only-use-embedded-pdf-thumbnail-if-resolution-suffices.patch";
+      url = "https://gitlab.xfce.org/xfce/tumbler/-/commit/69a704e0f4e622861ce4007f6f3f4f6f6b962689.patch";
+      hash = "sha256-aFJoWWzTaikqCw6C1LH+BFxst/uKkOGT1QK9Mx8/8/c=";
+    })
+  ];
+
   buildInputs = [
     libxfce4util
     ffmpegthumbnailer
@@ -58,14 +68,6 @@ mkXfceDerivation {
   postFixup = ''
     wrapGApp $out/lib/tumbler-1/tumblerd
   '';
-
-  patches = [
-    (fetchpatch {
-      name = "only-use-embedded-pdf-thumbnail-if-resolution-suffices.patch";
-      url = "https://gitlab.xfce.org/xfce/tumbler/-/merge_requests/35.patch";
-      hash = "sha256-aFJoWWzTaikqCw6C1LH+BFxst/uKkOGT1QK9Mx8/8/c=";
-    })
-  ];
 
   meta = with lib; {
     description = "D-Bus thumbnailer service";
