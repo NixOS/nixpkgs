9fb95bb222918723c78a964da17ca255eaa0bdbc
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-19 05:25:05 +0000

    tremotesf: 2.7.5 -> 2.8.2

--- pkgs/by-name/tr/tremotesf/package.nix
+++ pkgs/by-name/tr/tremotesf/package.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "tremotesf";
-  version = "2.7.5";
+  version = "2.8.2";
 
   src = fetchFromGitHub {
     owner = "equeim";
     repo = "tremotesf2";
     tag = finalAttrs.version;
-    hash = "sha256-LJ73ZynofPOMS5rSohJSY94vSQvGfNiNFRyGu6LPfU0=";
+    hash = "sha256-o6GSuRKO2LNpaFUuSu6VUeZN/blfpsz/piE7RZ47YfI=";
     # We need this for src/libtremotesf
     fetchSubmodules = true;
   };
