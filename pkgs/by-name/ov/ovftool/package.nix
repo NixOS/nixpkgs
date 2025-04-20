7bb40beeef320849a9d1256a8cc86d57019f5cdf
Emily <vcs@emily.moe>
2025-04-20 17:01:02 +0100

    ovftool: remove references to deprecated `darwin.Libsystem` stub

--- pkgs/by-name/ov/ovftool/package.nix
+++ pkgs/by-name/ov/ovftool/package.nix
@@ -72,7 +72,6 @@ stdenv.mkDerivation {
       glibc
     ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [
-      darwin.Libsystem
       libxml2
     ];
 
@@ -186,7 +185,6 @@ stdenv.mkDerivation {
       done
 
       # Patches for ovftool binary
-      change_args+=(-change /usr/lib/libSystem.B.dylib ${darwin.Libsystem}/lib/libSystem.B.dylib)
       change_args+=(-change /usr/lib/libc++.1.dylib ${stdenv.cc.libcxx}/lib/libc++.1.dylib)
       change_args+=(-change /usr/lib/libiconv.2.dylib ${libiconv}/lib/libiconv.2.dylib)
       change_args+=(-change /usr/lib/libxml2.2.dylib ${libxml2}/lib/libxml2.2.dylib)
@@ -201,7 +199,7 @@ stdenv.mkDerivation {
       install_name_tool "''${change_args[@]}" "$out/libexec/ovftool"
 
       # Additional patches for ovftool dylibs
-      change_args+=(-change /usr/lib/libresolv.9.dylib ${darwin.Libsystem}/lib/libresolv.9.dylib)
+      change_args+=(-change /usr/lib/libresolv.9.dylib ${lib.getLib darwin.libresolv}/lib/libresolv.9.dylib)
       change_args+=(-change @loader_path/libcares.2.dylib ${c-ares}/lib/libcares.2.dylib)
       change_args+=(-change @loader_path/libexpat.dylib ${expat}/lib/libexpat.dylib)
       change_args+=(-change @loader_path/libicudata.60.2.dylib ${icu60}/lib/libicudata.60.2.dylib)
