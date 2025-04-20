54513868ccbaebe734a3042b4b6ba5dfb95ed6e1
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-20 12:36:48 +0000

    altair: 8.2.1 -> 8.2.2

--- pkgs/by-name/al/altair/package.nix
+++ pkgs/by-name/al/altair/package.nix
@@ -7,11 +7,11 @@
 
 let
   pname = "altair";
-  version = "8.2.1";
+  version = "8.2.2";
 
   src = fetchurl {
     url = "https://github.com/imolorhe/altair/releases/download/v${version}/altair_${version}_x86_64_linux.AppImage";
-    sha256 = "sha256-DO4T/NgLSxZIxVK4oEz6QNsQJRacF8KRcwAvWToxIy8=";
+    sha256 = "sha256-3tLBZNuiqhSRg/a2g2PC53esnNb1jVFpCO2YiC7Rw6k=";
   };
 
   appimageContents = appimageTools.extract { inherit pname version src; };
