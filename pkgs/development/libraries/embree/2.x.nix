22b2e84417e621dc237283ffd9a49e9fa6051e1a
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-04-20 16:21:12 +0200

    embree2: set updateScript

--- pkgs/development/libraries/embree/2.x.nix
+++ pkgs/development/libraries/embree/2.x.nix
@@ -12,16 +12,17 @@
   libpng,
   libpthreadstubs,
   libX11,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "embree";
   version = "2.17.4";
 
   src = fetchFromGitHub {
     owner = "embree";
     repo = "embree";
-    rev = "v2.17.4";
+    tag = "v${finalAttrs.version}";
     sha256 = "0q3r724r58j4b6cbyy657fsb78z7a2c7d5mwdp7552skynsn2mn9";
   };
 
@@ -41,6 +42,16 @@ stdenv.mkDerivation {
     libX11
     libpthreadstubs
   ];
+
+  passthru = {
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "v(2.*)"
+      ];
+    };
+  };
+
   meta = with lib; {
     description = "High performance ray tracing kernels from Intel";
     homepage = "https://embree.github.io/";
@@ -48,4 +59,4 @@ stdenv.mkDerivation {
     license = licenses.asl20;
     platforms = [ "x86_64-linux" ];
   };
-}
+})
