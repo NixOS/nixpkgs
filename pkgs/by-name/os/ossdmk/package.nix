4e6d40114f8ae75fc32f6267a7bd20b6db199b5f
udontur <h@hadrian.cc>
2025-04-21 17:19:42 +0800

    ossdmk: init 1.0.2

---
+++ pkgs/by-name/os/ossdmk/package.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "ossdmk";
+  version = "1.0.2";
+
+  src = fetchFromGitHub {
+    owner = "udontur";
+    repo = "ossdmk";
+    rev = "v${version}";
+    hash = "sha256-QF3VJHxkw4K4BjpaL0+OA02cU9mSpLQkI70kBgxkGH0=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r ./ossdmk $out/bin/ossdmk
+  '';
+
+  meta = {
+    description = "Ontario Secondary School Diploma (OSSD) Mark Calculator CLI Tool";
+    homepage = "https://github.com/udontur/ossdmk";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ udontur ];
+    mainProgram = "ossdmk";
+    platforms = lib.platforms.linux;
+  };
+}
