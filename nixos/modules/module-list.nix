fab3444527c4bacc4fd0e2dccc93480c725de61f
Jack Wilsdon <jack@wilsdon.me>
2025-04-21 18:32:41 +0100

    nixos/dufs: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -834,6 +834,7 @@
   ./services/misc/domoticz.nix
   ./services/misc/duckdns.nix
   ./services/misc/duckling.nix
+  ./services/misc/dufs.nix
   ./services/misc/dump1090-fa.nix
   ./services/misc/dwm-status.nix
   ./services/misc/dysnomia.nix
