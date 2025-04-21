f591a9f748ce7d25fab66572a60c098baeb0fc22
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-21 10:31:53 +0000

    calls: 47.0 -> 48.1

--- pkgs/by-name/ca/calls/package.nix
+++ pkgs/by-name/ca/calls/package.nix
@@ -36,7 +36,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "calls";
-  version = "47.0";
+  version = "48.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
@@ -44,7 +44,7 @@ stdenv.mkDerivation (finalAttrs: {
     repo = "calls";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-HzQz5jmlXwUHLXC6nhHCa8E8zczaA89YxteZgxSV0YY=";
+    hash = "sha256-JK+bG/5p+c37aP7ZTGiPDh8tBHgWsuFkD7AsyZXePaw=";
   };
 
   outputs = [
