4999a844eecc6224f28f4a043d946838ef50fd74
Jachym Putta <jachym.putta@yale.edu>
2025-04-21 12:49:01 -0400

    vimPlugins.dailies-nvim: init at 2025-04-15

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -17,6 +17,7 @@
   python3,
   # Misc dependencies
   code-minimap,
+  dailies,
   dasht,
   deno,
   direnv,
@@ -882,6 +883,12 @@ in
       patchShebangs .
       ./install.sh
     '';
+  };
+
+  dailies-nvim = super.dailies-nvim.overrideAttrs {
+    runtimeDeps = [
+      dailies
+    ];
   };
 
   darkearth-nvim = super.darkearth-nvim.overrideAttrs {
