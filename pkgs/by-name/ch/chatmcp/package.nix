74f6b2f0b60800ce69a9eeb6f2b803e1f4b62d96
emaryn <emaryn@users.noreply.github.com>
2025-04-21 11:05:11 +0800

    chatmcp: 0.0.21-alpha -> 0.0.26-alpha

--- pkgs/by-name/ch/chatmcp/package.nix
+++ pkgs/by-name/ch/chatmcp/package.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  flutter327,
+  flutter329,
   fetchFromGitHub,
   autoPatchelfHook,
   copyDesktopItems,
@@ -12,15 +12,15 @@
   gitUpdater,
 }:
 
-flutter327.buildFlutterApplication rec {
+flutter329.buildFlutterApplication rec {
   pname = "chatmcp";
-  version = "0.0.21-alpha";
+  version = "0.0.26-alpha";
 
   src = fetchFromGitHub {
     owner = "daodao97";
     repo = "chatmcp";
     tag = "v${version}";
-    hash = "sha256-T0HVvnMBH/Tnhks7NTCdE3QkHPbg4XaPnT1aJ+LSH6E=";
+    hash = "sha256-ayGpP1Tiz2437YDSXIwuFNXWpqReHUuI0Nuaf/aus+Q=";
   };
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
