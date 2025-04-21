f1ebd5aa42e9a95cef24d57910bf9bb90c1dfaa6
udontur <h@hadrian.cc>
2025-04-21 17:18:27 +0800

    maintainers: add udontur

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25050,6 +25050,12 @@
     githubId = 347983;
     name = "Udo Spallek";
   };
+  udontur = {
+    email = "contact@hadrian.cc";
+    github = "udontur";
+    githubId = 118540129;
+    name = "udontur";
+  };
   ufUNnxagpM = {
     github = "ufUNnxagpM";
     githubId = 12422133;
