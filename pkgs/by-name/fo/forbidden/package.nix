6d07fea2324e8e464287f301161d968d3dacf62e
emaryn <emaryn@users.noreply.github.com>
2025-04-22 08:03:16 +0800

    forbidden: 12.6 -> 13.2
    Diff: https://github.com/ivan-sincek/forbidden/compare/refs/tags/v12.6...refs/tags/v13.2
    
    Changelog: https://github.com/ivan-sincek/forbidden/releases/tag/v13.2

--- pkgs/by-name/fo/forbidden/package.nix
+++ pkgs/by-name/fo/forbidden/package.nix
@@ -1,24 +1,22 @@
 {
   lib,
-  fetchFromGitHub,
   python3,
+  fetchFromGitHub,
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "forbidden";
-  version = "12.6";
+  version = "13.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ivan-sincek";
     repo = "forbidden";
     tag = "v${version}";
-    hash = "sha256-KE2FHn0ocAna5ReRi9re/u8gdNVkCygSbSRj8gEudgI=";
+    hash = "sha256-DQ8zjiLTgBBoqp8AP5BYULz4KGnVEt8e7bkfYRGWvFw=";
   };
 
-  build-system = with python3.pkgs; [
-    setuptools
-  ];
+  build-system = with python3.pkgs; [ setuptools ];
 
   dependencies = with python3.pkgs; [
     alive-progress
@@ -29,18 +27,20 @@ python3.pkgs.buildPythonApplication rec {
     requests
     tabulate
     termcolor
+    bot-safe-agents
+    cryptography
   ];
 
   pythonImportsCheck = [
     "forbidden"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Tool to bypass 4xx HTTP response status code";
     homepage = "https://github.com/ivan-sincek/forbidden";
     changelog = "https://github.com/ivan-sincek/forbidden/releases/tag/v${version}";
-    license = licenses.mit;
-    maintainers = with maintainers; [ fab ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ fab ];
     mainProgram = "forbidden";
   };
 }
