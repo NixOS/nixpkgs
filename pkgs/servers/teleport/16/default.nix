ac2b6f73361656fac169a6f89cf38e50b5dac0e9
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-04-22 11:02:35 +0300

    teleport_16: 16.5.1 -> 16.5.3
    Changelogs: https://github.com/gravitational/teleport/releases/tag/v16.5.1 https://github.com/gravitational/teleport/releases/tag/v16.5.3
    Diff: https://github.com/gravitational/teleport/compare/v16.5.1...v16.5.3
    (cherry picked from commit a95e5e51cf2bd8bb4154ee2ef75e006b50efa307)

--- pkgs/servers/teleport/16/default.nix
+++ pkgs/servers/teleport/16/default.nix
@@ -2,10 +2,10 @@
 import ../generic.nix (
   args
   // {
-    version = "16.5.1";
-    hash = "sha256-qDXJuKQ1IJ0+v4m5glEKNZVUBuLhG95MOzo0u3Ma2Qw=";
+    version = "16.5.3";
+    hash = "sha256-yaHy75oS+HuO++qHEDaGByz11vwAqfaDT9qb94iKs3Y=";
     vendorHash = "sha256-pHAiJ080lyWtb7xbwSeD9g8JlyXZyqtZC2IpsUJ7YaY=";
-    pnpmHash = "sha256-XHiox+UYhGYgo+inrnOVy0qvPXm7xoCaGfAC4FQmaMM=";
+    pnpmHash = "sha256-JQca2eFxcKJDHIaheJBg93ivZU95UWMRgbcK7QE4R10=";
     cargoHash = "sha256-04zykCcVTptEPGy35MIWG+tROKFzEepLBmn04mSbt7I=";
 
     # wasm-bindgen-cli version must match the version of wasm-bindgen in Cargo.lock
