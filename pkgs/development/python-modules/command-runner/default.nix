1d98270725be93cd9d56bce2158aa48555f7f38f
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-22 08:35:46 +0000

    python312Packages.command-runner: 1.7.2 -> 1.7.3

--- pkgs/development/python-modules/command-runner/default.nix
+++ pkgs/development/python-modules/command-runner/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "command-runner";
-  version = "1.7.2";
+  version = "1.7.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "netinvent";
     repo = "command_runner";
     tag = "v${version}";
-    hash = "sha256-2+Tvp0nFDcD6D99z/13RGRTrioFR0dhPDnicX9ZbIxY=";
+    hash = "sha256-BNjMMs44eDnGmcFXiMydJIU0RpsFOyd2TjH7BOGQP2E=";
   };
 
   build-system = [ setuptools ];
