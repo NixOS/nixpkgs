30dafa3ae47b3c848d0cbe32c31f915f3276e205
ed9w2in6 <22890124+ed9w2in6@users.noreply.github.com>
2025-04-22 13:05:06 +0800

    mas: refactor
    Allow optionally to build from source:
    mas-from-src.nix
    
    Original logic reside at:
    mas-from-release.nix
    
    When build by default, it should yield identical mas binaries.
    
    Both version additionally installs shell compeltions from offical
    repository.
    
    Both provides additional option to fix up the swift
    dylib.  This is because Apple stop distributing them natively to old
    version macOS that they drop support.  This option simplifies it.
    https://github.com/mas-cli/mas/issues/777
    
    version, repoRev, repoSrc, releaseSrc are made overridable too.
    
    Add vendor patch create_source_mas_package_dot_swift.patch
    because source build logic relies on code generation on build time.
    It is easier to detect overwrites than a manual patchPhase.
    
    Conflits with pull request #398976:
    https://github.com/NixOS/nixpkgs/pull/398976/

--- pkgs/by-name/ma/mas/package.nix
+++ pkgs/by-name/ma/mas/package.nix
@@ -1,65 +1,84 @@
 {
   lib,
-  stdenvNoCC,
+  pkgs,
+  fetchFromGitHub,
   fetchurl,
-  libarchive,
-  p7zip,
-  testers,
-  mas,
-}:
-
-stdenvNoCC.mkDerivation rec {
-  pname = "mas";
-  version = "1.9.0";
-
-  src = fetchurl {
-    url = "https://github.com/mas-cli/mas/releases/download/v${version}/mas-${version}.pkg";
+  version ? "1.9.0",
+  repoRev ? "v${version}",
+  repoSrc ? fetchFromGitHub {
+    owner = "mas-cli";
+    repo = "mas";
+    rev = repoRev;
+    hash = "sha256-2Hvz1616cYItsgUK/FI0/s0f+GJ1qI2ZfEVLmw6iqdk=";
+    name = "mas-${version}-source";
+  },
+  releaseSrc ? fetchurl {
+    url = "https://github.com/mas-cli/mas/releases/download/${repoRev}/mas-${version}.pkg";
     hash = "sha256-MiSrCHLby3diTAzDPCYX1ZwdmzcHwOx/UJuWrlRJe54=";
-  };
+    name = "mas-${version}.pkg";
+  },
+  enableSwiftDylibFix ? false,
+  masBuildFromSource ? false,
+}:
+let
+  self = rec {
+    meta = with lib; {
+      description = "Mac App Store command line interface";
+      homepage = "https://github.com/mas-cli/mas";
+      license = licenses.mit;
+      maintainers = with maintainers; [
+        # they seems inactive
+        steinybot
+        zachcoyle
+      ];
+      platforms = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      mainProgram = "mas";
+    };
 
-  nativeBuildInputs = [
-    libarchive
-    p7zip
-  ];
+    # Apple provides swift dylib updates only via softwareupdate
+    # if Apple drop supports for some old version macOS
+    # one will need to get these dylib updates from other sources
+    # or mas won't run
 
-  unpackPhase = ''
-    runHook preUnpack
+    # needs install_name_tool from stdenv
+    # from swift on nixpkgs, read there for caveats
+    fixSwiftDylib = swift: masRpathOrigin: ''
+      declare -A systemLibs=(
+      [libswiftCore.dylib]=1
+      [libswiftDarwin.dylib]=1
+      [libswiftSwiftOnoneSupport.dylib]=1
+      # [libswift_Concurrency.dylib]=1 # using rpath
+      )
 
-    7z x $src
-    bsdtar -xf Payload~
+      for systemLib in "''${!systemLibs[@]}"; do
+      install_name_tool -change "/usr/lib/swift/$systemLib" "${swift.swift.lib}/${swift.swiftLibSubdir}/$systemLib" "$out/bin/mas"
+      done
+      install_name_tool -rpath "${masRpathOrigin}" "${swift.swift.lib}/${swift.swiftLibSubdir}" "$out/bin/mas"
+    '';
 
-    runHook postUnpack
-  '';
-
-  dontBuild = true;
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin
-    cp mas $out/bin
-
-    runHook postInstall
-  '';
-
-  passthru.tests = {
-    version = testers.testVersion {
-      package = mas;
-      command = "mas version";
+    mas-from-src = pkgs.callPackage ./mas-from-src.nix {
+      inherit mas-from-src;
+      inherit
+        version
+        meta
+        repoSrc
+        enableSwiftDylibFix
+        fixSwiftDylib
+        ;
+    };
+    mas-from-release = pkgs.callPackage ./mas-from-release.nix {
+      inherit mas-from-release releaseSrc;
+      inherit
+        version
+        meta
+        repoSrc
+        enableSwiftDylibFix
+        fixSwiftDylib
+        ;
     };
   };
-
-  meta = with lib; {
-    description = "Mac App Store command line interface";
-    homepage = "https://github.com/mas-cli/mas";
-    license = licenses.mit;
-    maintainers = with maintainers; [
-      steinybot
-      zachcoyle
-    ];
-    platforms = [
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-  };
-}
+in
+if masBuildFromSource then self.mas-from-src else self.mas-from-release
