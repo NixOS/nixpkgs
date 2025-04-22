f5becf99024e26118486505a7287b5ba252ccc59
rafaelrc7 <contact@rafaelrc.com>
2025-04-22 17:02:45 -0300

    starsector: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15232,10 +15232,6 @@ with pkgs;
     buildShareware = true;
   };
 
-  starsector = callPackage ../games/starsector {
-    openjdk = openjdk17;
-  };
-
   scummvm = callPackage ../games/scummvm { };
 
   inherit (callPackage ../games/scummvm/games.nix { })
