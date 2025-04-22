2496f5d8f2fd8c2f0ac0a18f1f28356c4a3147a8
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-04-22 03:22:28 +0200

    rspamd: enable fasttext

--- pkgs/by-name/rs/rspamd/package.nix
+++ pkgs/by-name/rs/rspamd/package.nix
@@ -13,6 +13,7 @@
   pkg-config,
   sqlite,
   ragel,
+  fasttext,
   icu,
   vectorscan,
   jemalloc,
@@ -57,6 +58,7 @@ stdenv.mkDerivation rec {
       pcre
       sqlite
       ragel
+      fasttext
       icu
       jemalloc
       libsodium
@@ -81,6 +83,7 @@ stdenv.mkDerivation rec {
     "-DLOGDIR=/var/log/rspamd"
     "-DLOCAL_CONFDIR=/etc/rspamd"
     "-DENABLE_BLAS=${if withBlas then "ON" else "OFF"}"
+    "-DENABLE_FASTTEXT=ON"
     "-DENABLE_JEMALLOC=ON"
     "-DSYSTEM_DOCTEST=ON"
     "-DSYSTEM_FMT=ON"
