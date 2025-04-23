e78e311eb5d9b16d4c1bfffe0caa1bb4c08b609b
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-23 07:40:35 +0000

    all-the-package-names: 2.0.2129 -> 2.0.2137

--- pkgs/by-name/al/all-the-package-names/package.nix
+++ pkgs/by-name/al/all-the-package-names/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "all-the-package-names";
-  version = "2.0.2129";
+  version = "2.0.2137";
 
   src = fetchFromGitHub {
     owner = "nice-registry";
     repo = "all-the-package-names";
     tag = "v${version}";
-    hash = "sha256-aq4WxaeMprw9ohcwd9HuiWq5GUpW6dI3FSudMyUo6Fg=";
+    hash = "sha256-1HabSjZUHjPWkKZc+Xeh11bzb6ycQ1V7GS/XGycFoKc=";
   };
 
-  npmDepsHash = "sha256-WM2FO/qsDcXaV/2oqBlRGXc+VHaPUxFVg1DF6F0iGso=";
+  npmDepsHash = "sha256-zSGpKDcmBYod81wCeoSzOqse1zSao4aURngh55wq6CA=";
 
   passthru.updateScript = nix-update-script { };
 
