12ae4f298465493678e7a82acb4442010c2ec0f7
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-23 09:15:15 +0200

    python312Packages.mypy-boto3-s3: 1.37.24 -> 1.38.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1162,8 +1162,8 @@ rec {
       "sha256-gBkjxkSmaolRcBzYEbUwRvaZpEcPSD4cmfx/mi7VPM4=";
 
   mypy-boto3-s3 =
-    buildMypyBoto3Package "s3" "1.37.24"
-      "sha256-TfCXUlYTKrRSiWudNlcYZqgWFX1RnPEsZheVspBuLpw=";
+    buildMypyBoto3Package "s3" "1.38.0"
+      "sha256-+P5YbkUSP/zTBaDDCEcSjzkx2IhknitMWlL0Ehg8hAo=";
 
   mypy-boto3-s3control =
     buildMypyBoto3Package "s3control" "1.37.28"
