d15765c0e91d1a8c1eb61e82318ae2945d93f5b2
silvanshade <silvanshade@users.noreply.github.com>
2025-04-23 16:05:10 -0600

    libblake3: fix FreeBSD cross-compile builds

--- pkgs/by-name/li/libblake3/package.nix
+++ pkgs/by-name/li/libblake3/package.nix
@@ -3,6 +3,7 @@
   stdenv,
   cmake,
   fetchFromGitHub,
+  fetchpatch,
   tbb_2021_11,
 
   useTBB ? true,
@@ -26,6 +27,15 @@ stdenv.mkDerivation (finalAttrs: {
   propagatedBuildInputs = lib.optionals useTBB [
     # 2022.0 crashes on macOS at the moment
     tbb_2021_11
+  ];
+
+  patches = [
+    # build(cmake): Relax Clang frontend variant detection (BLAKE3-team/BLAKE3#477)
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/BLAKE3-team/BLAKE3/pull/477.patch";
+      hash = "sha256-kidCMGd/i9D9HLLTt7l1DbiU71sFTEyr3Vew4XHUHls=";
+      relative = "c";
+    })
   ];
 
   cmakeFlags = [
