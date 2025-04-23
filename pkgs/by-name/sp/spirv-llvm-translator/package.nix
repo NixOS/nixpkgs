468cca660d9e5be4c1680f4cd9f5057d4e1aa76b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-04-23 18:22:20 +0200

    spirv-llvm-translator: 19.1.0 -> 19.1.6

--- pkgs/by-name/sp/spirv-llvm-translator/package.nix
+++ pkgs/by-name/sp/spirv-llvm-translator/package.nix
@@ -18,9 +18,9 @@ let
 
   # ROCm, if actively updated will always be at the latest version
   versions = {
-    "19" = {
-      version = "19.1.0";
-      rev = "dad1f0eaab8047a4f73c50ed5f3d1694b78aae97";
+    "19" = rec {
+      version = "19.1.6";
+      rev = "v${version}";
       hash = "sha256-mUvDF5y+cBnqUaHjyiiE8cJGH5MfQMqGFy6bYv9vCVY=";
     };
     "18" = rec {
