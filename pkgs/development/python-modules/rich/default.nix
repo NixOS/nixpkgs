f11db086da4e47ef8dc940766ffcbc037a1ee0a0
Maxwell L-T <maxwell.lt@live.com>
2025-04-22 21:11:47 -0400

    Revert "python313Packages.rich: 13.9.4 -> 14.0.0"
    This reverts commit 6c22a1c2f187b1d6fdfcdb9104aa9b8a3c8e312b.

--- pkgs/development/python-modules/rich/default.nix
+++ pkgs/development/python-modules/rich/default.nix
@@ -29,7 +29,7 @@
 
 buildPythonPackage rec {
   pname = "rich";
-  version = "14.0.0";
+  version = "13.9.4";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -38,7 +38,7 @@ buildPythonPackage rec {
     owner = "Textualize";
     repo = "rich";
     tag = "v${version}";
-    hash = "sha256-gnKzb4lw4zgepTfJahHnpw2/vcg8o1kv8KfeVDSHcQI=";
+    hash = "sha256-Zaop9zR+Sz9lMQjQP1ddJSid5jEmf0tQYuTeLuWNGA8=";
   };
 
   build-system = [ poetry-core ];
