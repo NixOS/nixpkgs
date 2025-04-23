8409eba79296e85e4f8a960384189fb74387ec54
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-23 09:13:02 +0200

    python312Packages.mypy-boto3-imagebuilder: 1.37.0 -> 1.38.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -614,8 +614,8 @@ rec {
       "sha256-n11mu3O2o2sPL13xPtwlWVvpNfVUdV7l7NO2lXg+ZyM=";
 
   mypy-boto3-imagebuilder =
-    buildMypyBoto3Package "imagebuilder" "1.37.0"
-      "sha256-9YhNPBhrgqEcZfJ91StQOu44p6XEyUTpOjq45mLBOi0=";
+    buildMypyBoto3Package "imagebuilder" "1.38.0"
+      "sha256-DIO9k4N3y0WM/Em5io7lPeZ1USFDFPuS9/G/pEsuoGc=";
 
   mypy-boto3-importexport =
     buildMypyBoto3Package "importexport" "1.37.0"
