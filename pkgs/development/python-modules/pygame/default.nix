45213d1c9c8cb30f6f9332b6a56136477e8d6dda
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-04-23 21:14:21 +0200

    python3Packages.pygame: add traceback for segfaults in checkPhase

--- pkgs/development/python-modules/pygame/default.nix
+++ pkgs/development/python-modules/pygame/default.nix
@@ -106,6 +106,8 @@ buildPythonPackage rec {
     # No audio or video device in test environment
     export SDL_VIDEODRIVER=dummy
     export SDL_AUDIODRIVER=disk
+    # traceback for segfaults
+    export PYTHONFAULTHANDLER=1
 
     ${python.interpreter} -m pygame.tests -v \
       --exclude opengl,timing \
