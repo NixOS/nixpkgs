4c3197859b104cbc12caf3b723f38eb4e8f97e93
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-23 09:11:29 +0200

    python312Packages.mypy-boto3-cloudformation: 1.37.22 -> 1.38.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -226,8 +226,8 @@ rec {
       "sha256-E/vP9Z9Cd6QZDJ2es245eXiUjTSsV1XNN9qNnIbXJ7E=";
 
   mypy-boto3-cloudformation =
-    buildMypyBoto3Package "cloudformation" "1.37.22"
-      "sha256-v2vtJ9GtgtEVaWStq+sNYntVWrhw1okwWhYMuT4O8lU=";
+    buildMypyBoto3Package "cloudformation" "1.38.0"
+      "sha256-VjOZFmwH6R4GlfseWBA6JIsr7g214sPwcVV3bdYxGAU=";
 
   mypy-boto3-cloudfront =
     buildMypyBoto3Package "cloudfront" "1.37.9"
