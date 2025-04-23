645e76c0aaafd4d6f967c30d39429110cded43e2
Rouven Czerwinski <rouven@czerwinskis.de>
2025-04-23 13:09:18 +0200

    barebox: undrop package
    This reverts commit 0a94677daadaa0a712a5a001c4578d5a094e1e9c.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8243,6 +8243,12 @@ with pkgs;
     ubootWandboard
     ;
 
+  # Upstream Barebox:
+  inherit (callPackage ../misc/barebox { })
+    buildBarebox
+    bareboxTools
+    ;
+
   usbrelay = callPackage ../os-specific/linux/usbrelay { };
   usbrelayd = callPackage ../os-specific/linux/usbrelay/daemon.nix { };
 
