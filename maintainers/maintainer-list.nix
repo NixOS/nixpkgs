a810844cacdb0fd3244468f7180020697447a5a9
Francisco Ayala Le Brun <2482295+fayalalebrun@users.noreply.github.com>
2025-04-24 15:19:44 +0200

    maintainers: add fayalalebrun

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7804,6 +7804,12 @@
     name = "A Frederick Christensen";
     keys = [ { fingerprint = "5A49 F4F9 3EDC 21E9 B7CC  4E94 9EEF 4142 5355 8AC4"; } ];
   };
+  fayalalebrun = {
+    email = "fayalalebrun@proton.me";
+    github = "fayalalebrun";
+    githubId = 2482295;
+    name = "Francisco Ayala Le Brun";
+  };
   fazzi = {
     email = "faaris.ansari@proton.me";
     github = "fxzzi";
