54e960363a5b009cbd09efea8c53ff90b6aeadf8
Mikael Voss <mvs@nyantec.com>
2025-04-24 15:01:07 +0200

    sof-firmware: replace src.sha256 with src.hash

--- pkgs/by-name/so/sof-firmware/package.nix
+++ pkgs/by-name/so/sof-firmware/package.nix
@@ -11,7 +11,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://github.com/thesofproject/sof-bin/releases/download/v${finalAttrs.version}/sof-bin-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-o2IQ2cJF6BsNlnTWsn0f1BIpaM+SWu/FW0htNlD4gyM=";
+    hash = "sha256-o2IQ2cJF6BsNlnTWsn0f1BIpaM+SWu/FW0htNlD4gyM=";
   };
 
   dontFixup = true; # binaries must not be stripped or patchelfed
