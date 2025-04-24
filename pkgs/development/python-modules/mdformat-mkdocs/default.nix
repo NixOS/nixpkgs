6113962b889ee48d91164256ba8f450be6c389cb
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-24 07:33:33 +0000

    python312Packages.mdformat-mkdocs: 4.1.1 -> 4.1.2

--- pkgs/development/python-modules/mdformat-mkdocs/default.nix
+++ pkgs/development/python-modules/mdformat-mkdocs/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "mdformat-mkdocs";
-  version = "4.1.1";
+  version = "4.1.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "KyleKing";
     repo = "mdformat-mkdocs";
     tag = "v${version}";
-    hash = "sha256-KH0edaqH5STRYtcSqbR0uTdlZVKuHRS7AUgd25fjvEE=";
+    hash = "sha256-+2w7UrOPSCUDc6jnLAW0/njq+aJ4y+H8n7gshxLj8/Q=";
   };
 
   nativeBuildInputs = [ flit-core ];
