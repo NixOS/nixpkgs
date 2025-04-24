301b193768a17b5c3a62a4d6080b7b0f8276c2ad
dane <danerieber@gmail.com>
2025-04-24 13:56:46 -0500

    vintagestory: 1.20.7 -> 1.20.9

--- pkgs/by-name/vi/vintagestory/package.nix
+++ pkgs/by-name/vi/vintagestory/package.nix
@@ -21,11 +21,11 @@
 
 stdenv.mkDerivation rec {
   pname = "vintagestory";
-  version = "1.20.7";
+  version = "1.20.9";
 
   src = fetchurl {
     url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
-    hash = "sha256-1SBJ4E3rQBFPSCgb2Z3knEO5Y+xyJE4kyJW4we75CWA=";
+    hash = "sha256-pia2Dv0FY28nkATOk60GqiAEnZmxgZvZfvkM8U/bZzU=";
   };
 
   nativeBuildInputs = [
