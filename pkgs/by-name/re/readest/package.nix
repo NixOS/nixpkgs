80483db032e8342c7d6bcdf25133dc8b9e2729d8
Jost Alemann <jost_alemann@me.com>
2025-04-24 22:17:08 +0200

    readest: 0.9.36 -> 0.9.37
    Changelog: https://github.com/readest/readest/releases/tag/v0.9.37
    Diff: https://github.com/readest/readest/compare/v0.9.36...v0.9.37

--- pkgs/by-name/re/readest/package.nix
+++ pkgs/by-name/re/readest/package.nix
@@ -19,13 +19,13 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "readest";
-  version = "0.9.36";
+  version = "0.9.37";
 
   src = fetchFromGitHub {
     owner = "readest";
     repo = "readest";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-r/mpqeHKo6oQqNCnFByxHZf7RSWD5esZbweAEuda9ME=";
+    hash = "sha256-p5qZwNb2HtdMgCH/GWLnprL5LHpre8z+ffmjfCFaHh8=";
     fetchSubmodules = true;
   };
 
