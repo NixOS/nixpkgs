50dae1449f8d047403061425c75f78231507b1e7
Ilan Joselevich <personal@ilanjoselevich.com>
2025-04-24 21:23:30 +0300

    unifi: 9.0.114 -> 9.1.119

--- pkgs/by-name/un/unifi/package.nix
+++ pkgs/by-name/un/unifi/package.nix
@@ -10,12 +10,12 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "unifi-controller";
-  version = "9.0.114";
+  version = "9.1.119";
 
   # see https://community.ui.com/releases / https://www.ui.com/download/unifi
   src = fetchurl {
     url = "https://dl.ui.com/unifi/${finalAttrs.version}/unifi_sysvinit_all.deb";
-    hash = "sha256-3xumIIzr+tx60kPhPfSs2Kz2iJ39Kt5934Vca/MpUu4=";
+    hash = "sha256-YKioJZG8lnVCIh1hrMxFElBKLPHurxWiGJMHKDlX1yE=";
   };
 
   nativeBuildInputs = [
