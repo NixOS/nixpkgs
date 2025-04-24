469f550b9cf5dd57a447284174b4831bde7da16d
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-24 00:36:07 +0000

    concord: 2.3.0 -> 2.4.0

--- pkgs/by-name/co/concord/package.nix
+++ pkgs/by-name/co/concord/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "concord";
-  version = "2.3.0";
+  version = "2.4.0";
 
   src = fetchFromGitHub {
     owner = "Cogmasters";
     repo = "concord";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-yTFGRnhDzxU+dPeZbCWlm52gsmEgD2el+46c8XQBQng=";
+    hash = "sha256-Ak1eOLwmz3XkWpRrzgf0ayzb8Q9B6uWXfkhrdBLA2Uw=";
   };
 
   makeFlags = [ "PREFIX=${placeholder "out"}" ];
