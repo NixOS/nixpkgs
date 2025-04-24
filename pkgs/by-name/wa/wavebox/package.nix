e823d537086f3f8c474a1ed265600dbf77f543ab
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-24 08:21:55 +0000

    wavebox: 10.135.15-2 -> 10.135.21-2

--- pkgs/by-name/wa/wavebox/package.nix
+++ pkgs/by-name/wa/wavebox/package.nix
@@ -156,11 +156,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "wavebox";
-  version = "10.135.15-2";
+  version = "10.135.21-2";
 
   src = fetchurl {
     url = "https://download.wavebox.app/stable/linux/deb/amd64/wavebox_${finalAttrs.version}_amd64.deb";
-    hash = "sha256-nQWaPDHkwBjV7IUYf+e1NkYzXpnV15XZmqkT/Vz16Gc=";
+    hash = "sha256-NnJ+pP6JrMiMmoXS/yYm5qMLmv5oyguhmYF/M0Pv4/g=";
   };
 
   nativeBuildInputs = [
