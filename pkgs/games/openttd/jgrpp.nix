40d04f402bd793f51f6e610c764ce1debab06a94
Tom van Dijk <18gatenmaker6@gmail.com>
2025-04-24 13:22:14 +0200

    openttd-jgrpp: 0.65.1 → 0.65.2

--- pkgs/games/openttd/jgrpp.nix
+++ pkgs/games/openttd/jgrpp.nix
@@ -7,13 +7,13 @@
 
 openttd.overrideAttrs (oldAttrs: rec {
   pname = "openttd-jgrpp";
-  version = "0.65.1";
+  version = "0.65.2";
 
   src = fetchFromGitHub {
     owner = "JGRennison";
     repo = "OpenTTD-patches";
     rev = "jgrpp-${version}";
-    hash = "sha256-nS9yUcCwRkzmC9vIjQvPaRDcdPvHt4Nzt0cyfUX+XTY=";
+    hash = "sha256-CoWtFhR8U1PjvV4ID7R5cuFAD6jV8Ps6DtLOu6V55TM=";
   };
   patches = [ ];
 
