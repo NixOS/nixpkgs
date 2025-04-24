b7803f20cca6d64545d0112dd7a3609c98345a71
teutat3s <10206665+teutat3s@users.noreply.github.com>
2025-04-24 20:42:17 +0200

    grafanaPlugins.volkovlabs-form-panel: 4.6.0 -> 5.1.0

--- pkgs/servers/monitoring/grafana/plugins/volkovlabs-form-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/volkovlabs-form-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "volkovlabs-form-panel";
-  version = "4.6.0";
-  zipHash = "sha256-ne2dfCr+PBodeaxGfZL0VrAxHLYEAaeQfuZQf2F3s0s=";
+  version = "5.1.0";
+  zipHash = "sha256-aFIrKrfcTk4dGBaGVMv6mMLQqys5QaD9XgZIGmtgA5s=";
   meta = with lib; {
     description = "The Data Manipulation Panel is the first plugin that allows inserting and updating application data, as well as modifying configuration directly from your Grafana dashboard.";
     license = licenses.asl20;
