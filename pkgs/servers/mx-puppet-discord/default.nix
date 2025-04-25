527b0dce5817522fadb8bf2d1211b611ea6838e8
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-04-25 23:08:43 +0200

    mx-discord-puppet: use yarn-berry, unbreak with newer nodejs

--- pkgs/servers/mx-puppet-discord/default.nix
+++ pkgs/servers/mx-puppet-discord/default.nix
@@ -1,11 +1,13 @@
 {
+  lib,
   stdenv,
   fetchFromGitLab,
-  pkgs,
-  lib,
-  node-pre-gyp,
-  nodejs_20,
+
   pkg-config,
+  nodejs,
+  yarn-berry_3,
+  makeWrapper,
+
   libjpeg,
   pixman,
   cairo,
@@ -15,8 +17,10 @@
 }:
 
 let
-  nodejs = nodejs_20;
-
+  yarn-berry = yarn-berry_3;
+in
+stdenv.mkDerivation rec {
+  pname = "mx-puppet-discord";
   version = "0.1.1";
 
   src = fetchFromGitLab {
@@ -27,21 +31,30 @@ let
     hash = "sha256-ZhyjUt6Bz/0R4+Lq/IoY9rNjdwVE2qp4ZQLc684+T/0=";
   };
 
-  myNodePackages = import ./node-composition.nix {
-    inherit pkgs nodejs;
-    inherit (stdenv.hostPlatform) system;
+  patches = [
+    # generated via the following commands:
+    # `yarn up canvas -R`
+    # `yarn set resolution "better-sqlite3@npm:^7.4.1" "npm:11.9.1"`
+    ./a.patch
+  ];
+
+  missingHashes = ./missing-hashes.json;
+  yarnOfflineCache = yarn-berry.fetchYarnBerryDeps {
+    inherit src missingHashes patches;
+    hash = "sha256-3xgGrifz23LwnjUtganRvXfNonrMmKfCU8b+f0YurJA=";
   };
 
-in
-myNodePackages.package.override {
-  inherit version src;
-
   nativeBuildInputs = [
-    node-pre-gyp
-    nodejs.pkgs.node-gyp-build
+
+    makeWrapper
+    nodejs
+    (nodejs.python.withPackages (ps: [ ps.setuptools ]))
     pkg-config
     which
+    yarn-berry
+    yarn-berry.yarnBerryConfigHook
   ];
+
   buildInputs = [
     libjpeg
     pixman
@@ -50,30 +63,32 @@ myNodePackages.package.override {
     libpq
   ];
 
-  postRebuild = ''
-    # Build typescript stuff
-    npm run build
+  buildPhase = ''
+    runHook preBuild
+
+    yarn build
+
+    runHook postBuild
   '';
 
-  postInstall = ''
-    # Make an executable to run the server
-    mkdir -p $out/bin
-    cat <<EOF > $out/bin/mx-puppet-discord
-    #!/bin/sh
-    exec ${nodejs}/bin/node $out/lib/node_modules/@mx-puppet/discord/build/index.js "\$@"
-    EOF
-    chmod +x $out/bin/mx-puppet-discord
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/share/mx-puppet-discord"
+    cp -r ./build ./node_modules "$out/share/mx-puppet-discord"
+
+    makeWrapper ${lib.getExe nodejs} "$out/bin/mx-puppet-discord" \
+      --add-flags "$out/share/mx-puppet-discord/build/index.js"
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Discord puppeting bridge for matrix";
-    license = licenses.asl20;
+    license = lib.licenses.asl20;
     homepage = "https://gitlab.com/mx-puppet/discord/mx-puppet-discord";
     maintainers = [ ];
-    platforms = platforms.unix;
-    # never built on aarch64-darwin since first introduction in nixpkgs
-    # Depends on nodejs_18 that has been removed.
-    broken = true;
+    platforms = lib.platforms.unix;
     mainProgram = "mx-puppet-discord";
   };
 }
