4aca7fda1d310311527a3ceef58af08eb5f58d95
Stefan Kranich <mail@stefankranich.de>
2025-04-25 14:31:18 +0200

    tmuxPlugins.vim-tmux-navigator: unstable-2022-08-21 -> unstable-2025-04-25

--- pkgs/misc/tmux-plugins/default.nix
+++ pkgs/misc/tmux-plugins/default.nix
@@ -995,11 +995,11 @@ in
   vim-tmux-navigator = mkTmuxPlugin {
     pluginName = "vim-tmux-navigator";
     rtpFilePath = "vim-tmux-navigator.tmux";
-    version = "unstable-2022-08-21";
+    version = "unstable-2025-04-25";
     src = fetchFromGitHub {
       owner = "christoomey";
       repo = "vim-tmux-navigator";
-      rev = "afb45a55b452b9238159047ce7c6e161bd4a9907";
+      rev = "33afa80db65113561dc53fa732b7f5e53d5ecfd0";
       hash = "sha256-8A+Yt9uhhAP76EiqUopE8vl7/UXkgU2x000EOcF7pl0=";
     };
   };
