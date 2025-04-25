2f1b7aaac2d6aa9f08ee99a9548b64a76ed63c7e
GueLaKais <koroyeldiores@gmail.com>
2025-04-25 13:22:20 +0200

    python313Packages.rospkg: init at 1.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15341,6 +15341,8 @@ self: super: with self; {
 
   ropper = callPackage ../development/python-modules/ropper { };
 
+  rospkg = callPackage ../development/python-modules/rospkg { };
+
   rotary-embedding-torch = callPackage ../development/python-modules/rotary-embedding-torch { };
 
   rouge-score = callPackage ../development/python-modules/rouge-score { };
