7001d0632ac52634540b58064897786a4b4a7598
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-25 11:00:48 +0000

    termius: 9.18.1 -> 9.19.1

--- pkgs/by-name/te/termius/package.nix
+++ pkgs/by-name/te/termius/package.nix
@@ -16,8 +16,8 @@
 
 stdenv.mkDerivation rec {
   pname = "termius";
-  version = "9.18.1";
-  revision = "220";
+  version = "9.19.1";
+  revision = "222";
 
   src = fetchurl {
     # find the latest version with
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
     # and the sha512 with
     # curl -H 'X-Ubuntu-Series: 16' https://api.snapcraft.io/api/v1/snaps/details/termius-app | jq '.download_sha512' -r
     url = "https://api.snapcraft.io/api/v1/snaps/download/WkTBXwoX81rBe3s3OTt3EiiLKBx2QhuS_${revision}.snap";
-    hash = "sha512-IpFMTY161PlXtsr64YAdE21BupqYHxZZC7ZNbh3Zd10F1zJhVd+qAQOLOv6vdryu0HdhPv91Z1AEfz6BwLGVYA==";
+    hash = "sha512-WwFXunaA7hERnOl1ZKxLC0QR2ZdBkvJBdBDRcNDDzAnBqHOdu9TibqXKdUk11CbE5j/q+ak9DoavemK2s3MKKg==";
   };
 
   desktopItem = makeDesktopItem {
