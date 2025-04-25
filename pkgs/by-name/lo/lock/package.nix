61e90f9c5571d82fb0a59386be53ef674037f1c0
Seth Flynn <getchoo@tuta.io>
2025-04-25 02:14:39 +0000

    lock: 1.5.2 -> 1.6.0
    Diff: https://github.com/konstantintutsch/Lock/compare/v1.5.2...v1.6.0

--- pkgs/by-name/lo/lock/package.nix
+++ pkgs/by-name/lo/lock/package.nix
@@ -19,13 +19,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lock";
-  version = "1.5.2";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "konstantintutsch";
     repo = "Lock";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-FcL1BH2ypzBNXBNBR6oZ95dP6Kt50uOoYj8+2U7iYNg=";
+    hash = "sha256-K1iZ5XjuOyoa/ONOqmTjK5suwqBDwBHa/pTtxDZSj2k=";
   };
 
   strictDeps = true;
