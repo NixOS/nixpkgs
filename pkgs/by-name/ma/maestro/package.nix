f46386e5430d70d6562b7680d0215c9989485e12
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-25 19:48:06 +0000

    maestro: 1.39.13 -> 1.40.0

--- pkgs/by-name/ma/maestro/package.nix
+++ pkgs/by-name/ma/maestro/package.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "maestro";
-  version = "1.39.13";
+  version = "1.40.0";
 
   src = fetchurl {
     url = "https://github.com/mobile-dev-inc/maestro/releases/download/cli-${finalAttrs.version}/maestro.zip";
-    hash = "sha256-J1Gy12VF5C7OTDCOs4s7sWuwixZR1tz4hQyRxKcwahM=";
+    hash = "sha256-K0KSlSA2Jyeqck0CXZw3kh0qexIvL6vDn3EhAtIzCJc=";
   };
 
   dontUnpack = true;
