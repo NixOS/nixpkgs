a6cb76d1028ec9c8a72a9735ee290462039ebc50
emaryn <emaryn@users.noreply.github.com>
2025-04-26 21:00:19 +0000

    vscode-extensions.coder.coder-remote: 1.7.1 -> 1.8.0

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -1015,8 +1015,8 @@ let
         mktplcRef = {
           name = "coder-remote";
           publisher = "coder";
-          version = "1.7.1";
-          hash = "sha256-egtB8mF9bbGb5YJ2pS9uGMzLmJcHAZ7UTswrn6k2k3A=";
+          version = "1.8.0";
+          hash = "sha256-zAe2IFT69oZ/OLVSaaY5lGSiF/7FGiQngz/EXekwQtM=";
         };
         meta = {
           description = "Extension for Visual Studio Code to open any Coder workspace in VS Code with a single click";
