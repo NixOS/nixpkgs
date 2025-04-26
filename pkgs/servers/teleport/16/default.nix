02e71e901b370668b15288ba896808762336fa62
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-04-26 11:52:27 +0300

    teleport_16: 16.5.3 -> 16.5.7
    Changelogs: https://github.com/gravitational/teleport/releases/tag/v16.5.4 https://github.com/gravitational/teleport/releases/tag/v16.5.5 https://github.com/gravitational/teleport/releases/tag/v16.5.6 https://github.com/gravitational/teleport/releases/tag/v16.5.7
    Diff: https://github.com/gravitational/teleport/compare/v16.5.3...v16.5.7

--- pkgs/servers/teleport/16/default.nix
+++ pkgs/servers/teleport/16/default.nix
@@ -2,9 +2,9 @@ args:
 import ../generic.nix (
   args
   // {
-    version = "16.5.3";
-    hash = "sha256-yaHy75oS+HuO++qHEDaGByz11vwAqfaDT9qb94iKs3Y=";
-    vendorHash = "sha256-pHAiJ080lyWtb7xbwSeD9g8JlyXZyqtZC2IpsUJ7YaY=";
+    version = "16.5.7";
+    hash = "sha256-L210AvpAFWD3Atkbp6/qiDnjdIsu3Nheh5i7r3jDokg=";
+    vendorHash = "sha256-DdVBtMwz0AIGCYj/QLczG8GPP9mqKrdF+M0NqmM6J0I=";
     pnpmHash = "sha256-JQca2eFxcKJDHIaheJBg93ivZU95UWMRgbcK7QE4R10=";
     cargoHash = "sha256-04zykCcVTptEPGy35MIWG+tROKFzEepLBmn04mSbt7I=";
   }
