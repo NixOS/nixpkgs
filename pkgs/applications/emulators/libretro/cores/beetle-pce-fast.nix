b4bd495f8a2fb4db5e830dd669ba5d090e189586
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-26 21:40:56 +0000

    libretro.beetle-pce-fast: 0-unstable-2025-03-28 -> 0-unstable-2025-04-18

--- pkgs/applications/emulators/libretro/cores/beetle-pce-fast.nix
+++ pkgs/applications/emulators/libretro/cores/beetle-pce-fast.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "mednafen-pce-fast";
-  version = "0-unstable-2025-03-28";
+  version = "0-unstable-2025-04-18";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "beetle-pce-fast-libretro";
-    rev = "4ee33ff536f14295c178a037f9b5d5a960ce3c6f";
-    hash = "sha256-ZL+aV469RHp5SSBFmK0q+1h2MdcM1q+TZu5Rrv/N0DU=";
+    rev = "baa35513545b011faa8b0b5db334d24ba15539ed";
+    hash = "sha256-jEvbHCK9AB4u3raWbAivscHqBiH9s+RkgExTVW1yvnk=";
   };
 
   makefile = "Makefile";
