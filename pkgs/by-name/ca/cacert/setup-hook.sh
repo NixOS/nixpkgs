1ce0c4f5ba1d14fff4c399d978884bc873e471df
Tim Cuthbertson <tim@gfxmonk.net>
2025-04-26 14:29:46 +1000

    cacert: avoid using unreadable inherited certificate bundle

--- pkgs/by-name/ca/cacert/setup-hook.sh
+++ pkgs/by-name/ca/cacert/setup-hook.sh
@@ -1,4 +1,8 @@
 export NIX_SSL_CERT_FILE="${NIX_SSL_CERT_FILE:-@out@/etc/ssl/certs/ca-bundle.crt}"
+if test ! -r "$NIX_SSL_CERT_FILE"; then
+  # If we inherited an unreadable cert file path, just use the bundled one
+  export NIX_SSL_CERT_FILE="@out@/etc/ssl/certs/ca-bundle.crt"
+fi
 
 # compatibility
 #  - openssl
