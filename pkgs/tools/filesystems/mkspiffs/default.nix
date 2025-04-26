2f9967ffdad3c85238e5d1362cdab1f5e0bf872b
Stefan Frijters <sfrijters@gmail.com>
2025-04-26 02:42:17 +0200

    mkspiffs: modernize derivation

--- pkgs/tools/filesystems/mkspiffs/default.nix
+++ pkgs/tools/filesystems/mkspiffs/default.nix
@@ -7,14 +7,14 @@
 # Changing the variables CPPFLAGS and BUILD_CONFIG_NAME can be done by
 # overriding the same-named attributes. See ./presets.nix for examples.
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "mkspiffs";
   version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "igrr";
     repo = "mkspiffs";
-    rev = version;
+    tag = finalAttrs.version;
     fetchSubmodules = true;
     hash = "sha256-oa6Lmo2yb66IjtEKkZyJBgM/p7rdvmrKfgNd2rAM/Lk=";
   };
@@ -27,21 +27,20 @@ stdenv.mkDerivation rec {
   buildFlags = [ "dist" ];
 
   makeFlags = [
-    "VERSION=${version}"
+    "VERSION=${finalAttrs.version}"
     "SPIFFS_VERSION=unknown"
   ];
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp mkspiffs $out/bin
+    install -Dm755 -t $out/bin mkspiffs
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Tool to build and unpack SPIFFS images";
-    license = licenses.mit;
+    license = lib.licenses.mit;
     homepage = "https://github.com/igrr/mkspiffs";
-    maintainers = with maintainers; [ haslersn ];
+    maintainers = [ lib.maintainers.haslersn ];
     platforms = lib.platforms.all;
     mainProgram = "mkspiffs";
   };
-}
+})
