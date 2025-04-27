2f436a4a1db7026be7393cfe57002b5543b74300
Sapphire <imsapphire0@gmail.com>
2025-04-26 19:19:38 -0500

    vrcx: Build from source

--- pkgs/by-name/vr/vrcx/package.nix
+++ pkgs/by-name/vr/vrcx/package.nix
@@ -1,35 +1,85 @@
 {
   lib,
-  fetchurl,
-  appimageTools,
-  dotnet-runtime_9,
+  buildNpmPackage,
+  electron_34,
+  makeWrapper,
+  buildDotnetModule,
+  fetchFromGitHub,
+  dotnetCorePackages,
 }:
 let
   pname = "vrcx";
   version = "2025.03.01";
-  filename = builtins.replaceStrings [ "." ] [ "" ] version;
-  src = fetchurl {
-    hash = "sha256-d+sqebPDZC0GWtd+5/R1KXIKUbpZ0k9YFupsf29IHCs=";
-    url = "https://github.com/vrcx-team/VRCX/releases/download/v${version}/VRCX_${filename}.AppImage";
+  electron = electron_34;
+
+  src = fetchFromGitHub {
+    owner = "vrcx-team";
+    repo = "VRCX";
+    # v2025.03.01 tag is actually behind a few commits, namely the one that bumps the version (so it complains about not being up-to-date)
+    #tag = "v${version}";
+    rev = "1980eeb4cccebfcf33826d44b7833a9aa6f5a955";
+    hash = "sha256-HiFcHnytynWYbeUd+KsG38dLU1FhDu0VD3JPT3kUO6s=";
   };
-  appimageContents = appimageTools.extract {
-    inherit pname src version;
+
+  backend = buildDotnetModule {
+    inherit version src;
+    pname = "${pname}-backend";
+
+    nugetDeps = ./deps.json;
+    dotnet-sdk = dotnetCorePackages.dotnet_9.sdk;
+    dotnet-runtime = dotnetCorePackages.dotnet_9.runtime;
+    projectFile = "Dotnet/VRCX-Electron.csproj";
+
+    installPhase = ''
+      runHook preInstall
+
+      cp -r build/Electron $out
+
+      runHook postInstall
+    '';
   };
 in
-appimageTools.wrapType2 rec {
+buildNpmPackage {
   inherit pname version src;
-  extraPkgs = pkgs: [ dotnet-runtime_9 ];
-  extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/vrcx.desktop \
-      $out/share/applications/VRCX.desktop
-    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/vrcx.png \
-      $out/share/icons/hicolor/256x256/apps/VRCX.png
 
-    substituteInPlace $out/share/applications/VRCX.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=${pname} --no-install --ozone-platform-hint=auto'
-    substituteInPlace $out/share/applications/VRCX.desktop \
-      --replace-fail 'Icon=VRCX' "Icon=$out/share/icons/hicolor/256x256/apps/VRCX.png"
+  npmDepsHash = "sha256-2ZU+9NPPx3GbU75XfjVroZCpjXr7IK2HtEIqLJLOjyw=";
+  npmFlags = [ "--ignore-scripts" ];
+  makeCacheWritable = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    env PLATFORM=linux npm exec webpack -- --config webpack.config.js --mode production
+    node src-electron/patch-package-version.js
+    npm exec electron-builder -- --dir \
+      -c.electronDist=${electron.dist} \
+      -c.electronVersion=${electron.version}
+    node src-electron/patch-node-api-dotnet.js
+
+    runHook postBuild
   '';
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/vrcx
+    cp -r build/*-unpacked/resources "$out/lib/vrcx/"
+    mkdir -p $out/lib/vrcx/resources/app.asar.unpacked/build
+    cp -r ${backend} "$out/lib/vrcx/resources/app.asar.unpacked/build/Electron"
+
+    makeWrapper '${electron}/bin/electron' "$out/bin/vrcx" \
+      --add-flags "$out/lib/vrcx/resources/app.asar" \
+      --set NODE_ENV production
+
+    runHook postInstall
+  '';
+
+  passthru = {
+    inherit backend;
+  };
 
   meta = {
     description = "Friendship management tool for VRChat";
@@ -40,8 +90,10 @@ appimageTools.wrapType2 rec {
     license = lib.licenses.mit;
     homepage = "https://github.com/vrcx-team/VRCX";
     downloadPage = "https://github.com/vrcx-team/VRCX/releases";
-    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
-    maintainers = with lib.maintainers; [ ShyAssassin ];
+    maintainers = with lib.maintainers; [
+      ShyAssassin
+      ImSapphire
+    ];
     platforms = lib.platforms.linux;
   };
 }
