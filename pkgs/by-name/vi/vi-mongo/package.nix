57464937945ddd6fe2998af9c6675dfe9d7d53d0
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-27 04:34:16 +0000

    vi-mongo: 0.1.24 -> 0.1.25

--- pkgs/by-name/vi/vi-mongo/package.nix
+++ pkgs/by-name/vi/vi-mongo/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule rec {
   pname = "vi-mongo";
-  version = "0.1.24";
+  version = "0.1.25";
 
   src = fetchFromGitHub {
     owner = "kopecmaciej";
     repo = "vi-mongo";
     tag = "v${version}";
-    hash = "sha256-L4LbVeH8Fgz7RD0tcyyKpP7+/aTvn1zfl+RpsWwiSfA=";
+    hash = "sha256-9Pbrj9leZfKEtlmNb/9iw0I75H425ucWRxlky9sOjc8=";
   };
 
-  vendorHash = "sha256-rKXrmK0ns3FB6EGyCJ2nYrCUsQ7yPm8dmzJioiVzHIc=";
+  vendorHash = "sha256-OVd2wIssVJHamWpNrK+piQFl9Lz0xgYnnz/4D5yl1D4=";
 
   ldflags = [
     "-s"
