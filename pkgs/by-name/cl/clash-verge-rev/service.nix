9bc0408470c4295c9f3d4abc048fe1f31b4a705b
wxt <3264117476@qq.com>
2025-04-27 21:34:19 +0800

    clash-verge-rev: apply moraxyc's patch for security problem

--- pkgs/by-name/cl/clash-verge-rev/service.nix
+++ pkgs/by-name/cl/clash-verge-rev/service.nix
@@ -15,6 +15,15 @@ rustPlatform.buildRustPackage {
   src = src-service;
   sourceRoot = "${src-service.name}";
 
+  patches = [
+    # Only fix for nix by moraxyc
+    # FIXME: remove until upstream fix it
+    # https://github.com/clash-verge-rev/clash-verge-rev/issues/3428
+    # Patch: Restrict bin_path in spawn_process to be under the clash-verge-service directory.
+    # This prevents arbitrary code execution by ensuring only trusted binaries from the Nix store are allowed to run.
+    ./0001-valid-bin-path.patch
+  ];
+
   nativeBuildInputs = [
     pkg-config
   ];
