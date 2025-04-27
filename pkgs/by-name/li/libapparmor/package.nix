NixOS/nixpkgs

commit eba90bba663352aec63632d2547c07e19414923b

apparmor: 4.0.3 -> 4.1.0, rewrite
Upstream release: https://gitlab.com/apparmor/apparmor/-/releases/v4.1.0

Notable changes made to the packaging:
- apparmor-kernel-patches: drop. This was outdated and unused, and wouldn't even apply to any kernel anymore.
- aa-teardown: migrate to writeShellAPplication
- apparmor-*: migrate to by-name
- apparmor-*: enable checks and actually run checks
- libapparmor: test python module imports correctly
- libapparmor: no seperate $python output (makes no sense)

pkgs/by-name/li/libapparmor/package.nix
@@ -1,0 +1,108 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitLab,
+  autoreconfHook,
+  autoconf-archive,
+  pkg-config,
+  which,
+  flex,
+  bison,
+  withPerl ?
+    stdenv.hostPlatform == stdenv.buildPlatform && lib.meta.availableOn stdenv.hostPlatform perl,
+  perl,
+  withPython ? lib.meta.availableOn stdenv.hostPlatform python3Packages.python,
+  python3Packages,
+  swig,
+  ncurses,
+  libxcrypt,
+
+  # test
+  dejagnu,
+
+  # passthru
+  nix-update-script,
+  nixosTests,
+  callPackage,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "libapparmor";
+  version = "4.1.0";
+
+  src = fetchFromGitLab {
+    owner = "apparmor";
+    repo = "apparmor";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-oj6mGw/gvoRGpJqw72Lk6LJuurg8efjiV1pvZYbXz6A=";
+  };
+  sourceRoot = "${finalAttrs.src.name}/libraries/libapparmor";
+
+  postPatch = ''
+    substituteInPlace swig/perl/Makefile.am \
+      --replace-fail install_vendor install_site
+  '';
+
+  strictDeps = true;
+
+  nativeBuildInputs =
+    [
+      autoconf-archive
+      autoreconfHook
+      bison
+      flex
+      pkg-config
+      swig
+      ncurses
+      which
+      dejagnu
+    ]
+    ++ lib.optionals withPython [
+      python3Packages.setuptools
+    ];
+
+  nativeCheckInputs = [
+    python3Packages.pythonImportsCheckHook
+    perl
+  ];
+
+  buildInputs =
+    [ libxcrypt ] ++ (lib.optional withPerl perl) ++ (lib.optional withPython python3Packages.python);
+
+  # required to build apparmor-parser
+  dontDisableStatic = true;
+
+  # https://gitlab.com/apparmor/apparmor/issues/1
+  configureFlags = [
+    (lib.withFeature withPerl "perl")
+    (lib.withFeature withPython "python")
+  ];
+
+  doCheck = withPerl && withPython;
+
+  checkInputs = [ dejagnu ];
+
+  pythonImportsCheck = [
+    "LibAppArmor"
+  ];
+
+  passthru = {
+    updateScript = nix-update-script { };
+    tests.nixos = nixosTests.apparmor;
+    apparmorRulesFromClosure = callPackage ./apparmorRulesFromClosure.nix { };
+  };
+
+  meta = {
+    homepage = "https://apparmor.net/";
+    description = "Mandatory access control system - core library";
+    license = with lib.licenses; [
+      gpl2Only
+      lgpl21Only
+    ];
+    maintainers = with lib.maintainers; [
+      julm
+      thoughtpolice
+      grimmauld
+    ];
+    platforms = lib.platforms.linux;
+  };
+})

pkgs/by-name/ap/apparmor-bin-utils/package.nix
@@ -1,0 +1,53 @@
+{
+  lib,
+  stdenv,
+  pkg-config,
+  which,
+  buildPackages,
+
+  # apparmor deps
+  libapparmor,
+
+  # testing
+  perl,
+}:
+stdenv.mkDerivation {
+  pname = "apparmor-bin-utils";
+  inherit (libapparmor)
+    version
+    src
+    ;
+
+  sourceRoot = "${libapparmor.src.name}/binutils";
+
+  nativeBuildInputs = [
+    pkg-config
+    libapparmor
+    which
+  ];
+
+  buildInputs = [
+    libapparmor
+  ];
+
+  makeFlags = [
+    "LANGS="
+    "USE_SYSTEM=1"
+    "POD2MAN=${lib.getExe' buildPackages.perl "pod2man"}"
+    "POD2HTML=${lib.getExe' buildPackages.perl "pod2html"}"
+    "MANDIR=share/man"
+  ];
+
+  doCheck = true;
+  checkInputs = [ perl ];
+
+  installFlags = [
+    "DESTDIR=$(out)"
+    "BINDIR=$(out)/bin"
+    "SBINDIR=$(out)/bin"
+  ];
+
+  meta = libapparmor.meta // {
+    description = "Mandatory access control system - binary user-land utilities";
+  };
+}

pkgs/by-name/ap/apparmor-pam/package.nix
@@ -1,0 +1,40 @@
+{
+  stdenv,
+  pkg-config,
+  which,
+  pam,
+
+  # apparmor deps
+  libapparmor,
+}:
+stdenv.mkDerivation {
+  pname = "apparmor-pam";
+  inherit (libapparmor)
+    version
+    src
+    ;
+
+  postPatch = ''
+    substituteInPlace Makefile \
+      --replace-fail "pkg-config" "$PKG_CONFIG"
+  '';
+
+  nativeBuildInputs = [
+    pkg-config
+    which
+  ];
+
+  buildInputs = [
+    libapparmor
+    pam
+  ];
+
+  sourceRoot = "${libapparmor.src.name}/changehat/pam_apparmor";
+
+  makeFlags = [ "USE_SYSTEM=1" ];
+  installFlags = [ "DESTDIR=$(out)" ];
+
+  meta = libapparmor.meta // {
+    description = "Mandatory access control system - PAM service";
+  };
+}

pkgs/by-name/ap/apparmor-parser/package.nix
@@ -1,0 +1,75 @@
+{
+  lib,
+  stdenv,
+  which,
+  flex,
+  bison,
+  linuxHeaders ? stdenv.cc.libc.linuxHeaders,
+  buildPackages,
+
+  # apparmor deps
+  libapparmor,
+
+  # testing
+  perl,
+  python3,
+  bashInteractive,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "apparmor-parser";
+  inherit (libapparmor) version src;
+
+  postPatch = ''
+    patchShebangs .
+    cd parser
+
+    substituteInPlace Makefile \
+      --replace-fail "/usr/include/linux/capability.h" "${linuxHeaders}/include/linux/capability.h"
+    substituteInPlace rc.apparmor.functions \
+      --replace-fail "/sbin/apparmor_parser" "$out/bin/apparmor_parser" # FIXME
+    substituteInPlace rc.apparmor.functions \
+      --replace-fail "/usr/sbin/aa-status" '$(which aa-status)'
+    sed -i rc.apparmor.functions -e '2i . ${./fix-rc.apparmor.functions.sh}'
+  '';
+
+  nativeBuildInputs = [
+    bison
+    flex
+    which
+  ];
+
+  buildInputs = [ libapparmor ];
+
+  makeFlags = [
+    "LANGS="
+    "USE_SYSTEM=1"
+    "INCLUDEDIR=${libapparmor}/include"
+    "AR=${stdenv.cc.bintools.targetPrefix}ar"
+    "POD2MAN=${lib.getExe' buildPackages.perl "pod2man"}"
+    "POD2HTML=${lib.getExe' buildPackages.perl "pod2html"}"
+    "MANDIR=share/man"
+  ] ++ lib.optional finalAttrs.doCheck "PROVE=${lib.getExe' perl "prove"}";
+
+  installFlags = [
+    "DESTDIR=$(out)"
+    "DISTRO=unknown"
+  ];
+
+  preCheck = "pushd ./tst";
+
+  checkTarget = "tests";
+
+  postCheck = "popd";
+
+  doCheck = stdenv.hostPlatform == stdenv.buildPlatform && !stdenv.hostPlatform.isMusl;
+  checkInputs = [
+    bashInteractive
+    perl
+    python3
+  ];
+
+  meta = libapparmor.meta // {
+    description = "Mandatory access control system - core library";
+    mainProgram = "apparmor_parser";
+  };
+})

pkgs/by-name/ap/apparmor-profiles/package.nix
@@ -1,0 +1,42 @@
+{
+  stdenv,
+  which,
+  callPackage,
+
+  # apparmor deps
+  libapparmor,
+  apparmor-parser,
+  apparmor-utils,
+}:
+stdenv.mkDerivation {
+  pname = "apparmor-profiles";
+  inherit (libapparmor) version src;
+
+  sourceRoot = "${libapparmor.src.name}/profiles";
+
+  nativeBuildInputs = [ which ];
+
+  installFlags = [
+    "DESTDIR=$(out)"
+    "EXTRAS_DEST=$(out)/share/apparmor/extra-profiles"
+  ];
+
+  checkTarget = "check";
+
+  checkInputs = [
+    apparmor-parser
+    apparmor-utils
+  ];
+
+  preCheck = ''
+    export USE_SYSTEM=1
+    export LOGPROF="aa-logprof --configdir ${callPackage ./test_config.nix { }} --no-check-mountpoint"
+  '';
+
+  doCheck = true;
+
+  meta = libapparmor.meta // {
+    description = "Mandatory access control system - profiles";
+    mainProgram = "apparmor_parser";
+  };
+}

pkgs/by-name/ap/apparmor-teardown/package.nix
@@ -1,0 +1,33 @@
+{
+  writeShellApplication,
+  coreutils,
+  gnused,
+  gnugrep,
+  which,
+
+  # apparmor deps
+  apparmor-parser,
+  apparmor-bin-utils,
+  libapparmor,
+}:
+writeShellApplication {
+  name = "apparmor-teardown";
+  runtimeInputs = [
+    apparmor-parser
+    apparmor-bin-utils
+    coreutils
+    gnused
+    gnugrep
+    which
+  ];
+
+  text = ''
+    set +e # the imported script tries to `read` an empty line
+    # shellcheck source=/dev/null
+    . ${apparmor-parser}/lib/apparmor/rc.apparmor.functions
+    remove_profiles
+    exit 0
+  '';
+
+  inherit (libapparmor) meta;
+}

pkgs/by-name/ap/apparmor-utils/package.nix
@@ -1,0 +1,86 @@
+{
+  lib,
+  makeWrapper,
+  gawk,
+  perl,
+  bash,
+  stdenv,
+  which,
+  linuxHeaders ? stdenv.cc.libc.linuxHeaders,
+  python3Packages,
+  bashNonInteractive,
+  buildPackages,
+
+  # apparmor deps
+  libapparmor,
+  apparmor-parser,
+  apparmor-teardown,
+}:
+python3Packages.buildPythonApplication {
+  pname = "apparmor-utils";
+  inherit (libapparmor) version src;
+
+  postPatch =
+    ''
+      patchShebangs .
+      cd utils
+
+      substituteInPlace aa-remove-unknown \
+        --replace-fail "/lib/apparmor/rc.apparmor.functions" "${apparmor-parser}/lib/apparmor/rc.apparmor.functions"
+      substituteInPlace Makefile \
+        --replace-fail "/usr/include/linux/capability.h" "${linuxHeaders}/include/linux/capability.h"
+      sed -i -E 's/^(DESTDIR|BINDIR|PYPREFIX)=.*//g' Makefile
+      sed -i aa-unconfined -e "/my_env\['PATH'\]/d"
+    ''
+    + (lib.optionalString stdenv.hostPlatform.isMusl ''
+      sed -i Makefile -e "/\<vim\>/d"
+    '');
+
+  format = "other";
+  strictDeps = true;
+
+  doCheck = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+    which
+    bashNonInteractive
+    python3Packages.setuptools
+  ];
+
+  buildInputs = [
+    bash
+    perl
+  ];
+
+  pythonPath = [
+    python3Packages.notify2
+    python3Packages.psutil
+    libapparmor
+  ];
+
+  makeFlags = [
+    "LANGS="
+    "POD2MAN=${lib.getExe' buildPackages.perl "pod2man"}"
+    "POD2HTML=${lib.getExe' buildPackages.perl "pod2html"}"
+    "MANDIR=share/man"
+  ];
+
+  installFlags = [
+    "DESTDIR=$(out)"
+    "BINDIR=$(out)/bin"
+    "VIM_INSTALL_PATH=$(out)/share"
+    "PYPREFIX="
+  ];
+
+  postInstall = ''
+    wrapProgram $out/bin/aa-remove-unknown \
+     --prefix PATH : ${lib.makeBinPath [ gawk ]}
+
+    ln -s ${lib.getExe apparmor-teardown} $out/bin/aa-teardown
+  '';
+
+  meta = libapparmor.meta // {
+    description = "Mandatory access control system - script user-land utilities";
+  };
+}

commit f4949baf8c7175bfb9ffb77fa130c61561da6312

libapparmor: fix static
The Python bindings were recently enabled when cross compiling, but it
still doesn't make sense to build them (a shared library) in
pkgsStatic.

Fixes: eba90bba6633 ("apparmor: 4.0.3 -> 4.1.0, rewrite")

pkgs/by-name/li/libapparmor/package.nix
@@ -11,7 +11,8 @@
   withPerl ?
     stdenv.hostPlatform == stdenv.buildPlatform && lib.meta.availableOn stdenv.hostPlatform perl,
   perl,
-  withPython ? lib.meta.availableOn stdenv.hostPlatform python3Packages.python,
+  withPython ?
+    !stdenv.hostPlatform.isStatic && lib.meta.availableOn stdenv.hostPlatform python3Packages.python,
   python3Packages,
   swig,
   ncurses,