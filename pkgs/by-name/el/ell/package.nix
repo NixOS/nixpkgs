9409c70274d3a6a5de53c1d9938195a8b93e5b38
Sergei Trofimovich <slyich@gmail.com>
2025-04-27 08:35:24 +0100

    ell: 0.73 -> 0.76
    Changes: https://git.kernel.org/pub/scm/libs/ell/ell.git/tree/ChangeLog?h=0.76

--- pkgs/by-name/el/ell/package.nix
+++ pkgs/by-name/el/ell/package.nix
@@ -11,7 +11,7 @@
 
 stdenv.mkDerivation rec {
   pname = "ell";
-  version = "0.73";
+  version = "0.76";
 
   outputs = [
     "out"
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = "https://git.kernel.org/pub/scm/libs/ell/ell.git";
     rev = version;
-    hash = "sha256-pwAlRh+rkfPA6dXOGZcIidyCD2ioxVPSJjnyvrWuwow=";
+    hash = "sha256-LSTmcVBKI+EpDiTpiKFEeIIXIXc6C5gOYn5zf7sHe/I=";
   };
 
   nativeBuildInputs = [
