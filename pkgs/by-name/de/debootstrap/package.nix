dfb20deed2934c41ab1d26963cfc1ae9acc015d5
R. Ryantm <ryantm-bot@ryantm.com>
2025-04-28 03:37:48 +0000

    debootstrap: 1.0.140_bpo12+1 -> 1.0.141

--- pkgs/by-name/de/debootstrap/package.nix
+++ pkgs/by-name/de/debootstrap/package.nix
@@ -46,14 +46,14 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "debootstrap";
-  version = "1.0.140_bpo12+1";
+  version = "1.0.141";
 
   src = fetchFromGitLab {
     domain = "salsa.debian.org";
     owner = "installer-team";
     repo = "debootstrap";
     rev = "refs/tags/${version}";
-    hash = "sha256-4vINaMRo6IrZ6e2/DAJ06ODy2BWm4COR1JDSY52upUc=";
+    hash = "sha256-Up0qWASVETCwqXq4rqBsPDsKHETaoj+LiRHc51kSvPU=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
