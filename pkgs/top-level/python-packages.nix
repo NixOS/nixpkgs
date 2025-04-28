857b66f40c82d437a548c0de371f98c1223a7360
Ben Darwin <bcdarwin@gmail.com>
2025-04-28 15:37:03 -0400

    python313Packages.meds: init at 0.4.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9211,6 +9211,8 @@ self: super: with self; {
 
   medpy = callPackage ../development/python-modules/medpy { };
 
+  meds = callPackage ../development/python-modules/meds { };
+
   medvol = callPackage ../development/python-modules/medvol { };
 
   meeko = callPackage ../development/python-modules/meeko { };
