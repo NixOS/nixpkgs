c89bdf13c72eb458a4954fc208f727e47340ec1b
Austin Horstman <khaneliman12@gmail.com>
2025-04-28 15:41:51 -0500

    teams-for-linux: 2.0.8 -> 2.0.11
    (cherry picked from commit 709543eec11d307f281cc2c034c0ad97496aa9cd)

--- pkgs/by-name/te/teams-for-linux/package.nix
+++ pkgs/by-name/te/teams-for-linux/package.nix
@@ -16,16 +16,16 @@
 
 buildNpmPackage rec {
   pname = "teams-for-linux";
-  version = "2.0.8";
+  version = "2.0.11";
 
   src = fetchFromGitHub {
     owner = "IsmaelMartinez";
     repo = "teams-for-linux";
     tag = "v${version}";
-    hash = "sha256-w5Mnn1L1qr8IrLx9IpZoiMd74qD8Gf7ODvtgApp71cQ=";
+    hash = "sha256-vqeDzLImp0c9qPC8oNmjoTFLMCUJzJGHznALCbKdzWc=";
   };
 
-  npmDepsHash = "sha256-sUdWzVe9FbC9ayj4UrdAAGC3QbmDSSnFaVrROG6Jxjo=";
+  npmDepsHash = "sha256-3mXQ1InZGCsCCz1mVNSOE4tvA/qMW7eWZzES8yzriFQ=";
 
   nativeBuildInputs = [
     makeWrapper
