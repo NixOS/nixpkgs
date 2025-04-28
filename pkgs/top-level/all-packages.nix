692d13d72f0a6a6439a5871f402b5aaf1d8fdd1d
Edwin Hermans <edwin@madtech.cx>
2025-04-28 15:51:43 -0400

    devbox: 0.14.0 -> 0.14.2
    Update references for jetpack.io to jetify.com
    
    Project switched to 1.24 in jetify-com/devbox#2533

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8176,7 +8176,7 @@ with pkgs;
 
   dbt = with python3Packages; toPythonApplication dbt-core;
 
-  devbox = callPackage ../development/tools/devbox { buildGoModule = buildGo123Module; };
+  devbox = callPackage ../development/tools/devbox { buildGoModule = buildGo124Module; };
 
   libcxx = llvmPackages.libcxx;
 
