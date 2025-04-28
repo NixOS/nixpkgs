adeef1ee8234ebb828d8dc0b427866a33fd648a5
Marcin Serwin <marcin@serwin.dev>
2025-04-28 18:00:11 +0200

    sdl2-compat: simplify tests
    SDL2 now points to this package, no need to override it manually.

--- pkgs/by-name/sd/sdl2-compat/package.nix
+++ pkgs/by-name/sd/sdl2-compat/package.nix
@@ -73,20 +73,20 @@ stdenv.mkDerivation (finalAttrs: {
 
   passthru = {
     tests =
-      let
-        replaceSDL2 = drv: drv.override { SDL2 = finalAttrs.finalPackage; };
-      in
       {
-        pkg-config = testers.hasPkgConfigModules { package = finalAttrs.finalPackage; };
-        SDL2_ttf = replaceSDL2 SDL2_ttf;
-        SDL2_net = replaceSDL2 SDL2_net;
-        SDL2_gfx = replaceSDL2 SDL2_gfx;
-        SDL2_sound = replaceSDL2 SDL2_sound;
-        SDL2_mixer = replaceSDL2 SDL2_mixer;
-        SDL2_image = replaceSDL2 SDL2_image;
+        pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+
+        inherit
+          SDL2_ttf
+          SDL2_net
+          SDL2_gfx
+          SDL2_sound
+          SDL2_mixer
+          SDL2_image
+          ;
       }
       // lib.optionalAttrs stdenv.hostPlatform.isLinux {
-        monado = replaceSDL2 monado;
+        inherit monado;
       };
 
     updateScript = nix-update-script {
