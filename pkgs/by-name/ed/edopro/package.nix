b2eb53b42eba28126371e51bef1ac66e62ce5ae3
OPNA2608 <opna2608@protonmail.com>
2025-04-28 23:37:46 +0200

    edopro: 40.1.4 -> 41.0.2
    - Rename ocgcore library to match expected platform-specific name
    
    (cherry picked from commit 160d74ddfba4ff8f77023f5f10f400174c707b99)

--- pkgs/by-name/ed/edopro/package.nix
+++ pkgs/by-name/ed/edopro/package.nix
@@ -14,8 +14,7 @@
   curl,
   envsubst,
   flac,
-  # Use fmt 10+ after release 40.1.4+
-  fmt_9,
+  fmt,
   freetype,
   irrlicht,
   libevent,
@@ -28,7 +27,6 @@
   libX11,
   libxkbcommon,
   libXxf86vm,
-  lua5_3,
   mono,
   nlohmann_json,
   openal,
@@ -118,61 +116,77 @@ let
     };
   };
 
-  ocgcore = stdenv.mkDerivation {
-    pname = "ocgcore-edopro";
-    version = deps.edopro-version;
+  ocgcore =
+    let
+      # Refer to CORENAME EPRO_TEXT in <edopro>/gframe/dllinterface.cpp for this
+      ocgcoreName = lib.strings.concatStrings [
+        (lib.optionalString (!stdenv.hostPlatform.isWindows) "lib")
+        "ocgcore"
+        (
+          if stdenv.hostPlatform.isiOS then
+            "-ios"
+          else if stdenv.hostPlatform.isAndroid then
+            (
+              if stdenv.hostPlatform.isx86_64 then
+                "x64"
+              else if stdenv.hostPlatform.isx86_32 then
+                "x86"
+              else if stdenv.hostPlatform.isAarch64 then
+                "v8"
+              else if stdenv.hostPlatform.isAarch32 then
+                "v7"
+              else
+                throw "Don't know what platform suffix edopro expects for ocgcore on: ${stdenv.hostPlatform.system}"
+            )
+          else
+            lib.optionalString (stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64) ".aarch64"
+        )
+        stdenv.hostPlatform.extensions.sharedLibrary
+      ];
+    in
+    stdenv.mkDerivation {
+      pname = "ocgcore-edopro";
+      version = deps.edopro-version;
 
-    src = edopro-src;
-    sourceRoot = "${edopro-src.name}/ocgcore";
+      src = edopro-src;
+      sourceRoot = "${edopro-src.name}/ocgcore";
 
-    patches = [
-      # Fix linking against our Lua (different name mangling, C in Lua vs C++ in ocgcore)
-      ./ocgcore-lua-symbols.patch
-    ];
+      nativeBuildInputs = [
+        premake5
+      ];
 
-    nativeBuildInputs = [
-      premake5
-    ];
+      enableParallelBuilding = true;
 
-    # Drop when edopro version >= 41
-    buildInputs = [ lua5_3 ];
-    preBuild = ''
-      premake5 gmake2 \
-        --lua-path="${lua5_3}"
-    '';
+      buildFlags = [
+        "verbose=true"
+        "config=release"
+        "ocgcoreshared"
+      ];
 
-    enableParallelBuilding = true;
+      makeFlags = [
+        "-C"
+        "build"
+      ];
 
-    buildFlags = [
-      "verbose=true"
-      "config=release"
-      "ocgcoreshared"
-    ];
+      # To make sure linking errors are discovered at build time, not when edopro runs into them during loading
+      env.NIX_LDFLAGS = "--unresolved-symbols=report-all";
 
-    makeFlags = [
-      "-C"
-      "build"
-    ];
+      installPhase = ''
+        runHook preInstall
 
-    # To make sure linking errors are discovered at build time, not when edopro runs into them during loading
-    env.NIX_LDFLAGS = "--unresolved-symbols=report-all";
+        install -Dm644 bin/release/*ocgcore*${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/${ocgcoreName}
 
-    installPhase = ''
-      runHook preInstall
+        runHook postInstall
+      '';
 
-      install -Dm644 -t $out/lib bin/release/libocgcore*${stdenv.hostPlatform.extensions.sharedLibrary}
-
-      runHook postInstall
-    '';
-
-    meta = {
-      description = "YGOPro script engine";
-      homepage = "https://github.com/edo9300/ygopro-core";
-      license = lib.licenses.agpl3Plus;
-      inherit maintainers;
-      platforms = lib.platforms.unix;
+      meta = {
+        description = "YGOPro script engine";
+        homepage = "https://github.com/edo9300/ygopro-core";
+        license = lib.licenses.agpl3Plus;
+        inherit maintainers;
+        platforms = lib.platforms.unix;
+      };
     };
-  };
 
   edopro = stdenv.mkDerivation {
     pname = "edopro";
@@ -189,7 +203,7 @@ let
       bzip2
       curl
       flac
-      fmt_9
+      fmt
       freetype
       irrlicht-edopro
       libevent
