22d53809449771b9523396accd8e6add655c92b1
Fabian Affolter <mail@fabian-affolter.ch>
2025-04-29 10:11:04 +0200

    python313Packages.cyclopts: 3.13.1 -> 3.14.0
    Diff: https://github.com/BrianPugh/cyclopts/compare/refs/tags/v3.13.1...refs/tags/v3.14.0
    
    Changelog: https://github.com/BrianPugh/cyclopts/releases/tag/v3.14.0

--- pkgs/development/python-modules/cyclopts/default.nix
+++ pkgs/development/python-modules/cyclopts/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "cyclopts";
-  version = "3.13.1";
+  version = "3.14.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "BrianPugh";
     repo = "cyclopts";
     tag = "v${version}";
-    hash = "sha256-clufN+oFw32hKhbPFhIWWjwq512WP5Vxe3cvM/wze60=";
+    hash = "sha256-alXKOlSM4QPkXEVhWHwoW+YxXOTQtOffbIuK8WCWTtA=";
   };
 
   build-system = [
