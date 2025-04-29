fbf8306c7ef7d56eacc71ddff447351bcc253578
Sergei Trofimovich <slyich@gmail.com>
2025-04-29 21:09:46 +0100

    wayland-protocols: 1.43 -> 1.44
    Changes: https://gitlab.freedesktop.org/wayland/wayland-protocols/-/compare/1.43...1.44?from_project_id=2891

--- pkgs/development/libraries/wayland/protocols.nix
+++ pkgs/development/libraries/wayland/protocols.nix
@@ -14,7 +14,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "wayland-protocols";
-  version = "1.43";
+  version = "1.44";
 
   doCheck =
     stdenv.hostPlatform == stdenv.buildPlatform
@@ -25,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://gitlab.freedesktop.org/wayland/${finalAttrs.pname}/-/releases/${finalAttrs.version}/downloads/${finalAttrs.pname}-${finalAttrs.version}.tar.xz";
-    hash = "sha256-ujw0Jd0nxXtSkek9upe+EkeWAeALyrJNJkcZSMtkNlM=";
+    hash = "sha256-PfEQfs+L/W7oeK7KXTt6/YEkikgDHhTK9q4B8U7rtQ4=";
   };
 
   postPatch = lib.optionalString finalAttrs.finalPackage.doCheck ''
