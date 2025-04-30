ad4a12aede594c06152277b6e3437b861c54917e
qbisi <qbisicwate@gmail.com>
2025-04-30 23:06:38 +0800

    python312Packages.recursivenodes: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14884,6 +14884,8 @@ self: super: with self; {
     callPackage ../development/python-modules/recursive-pth-loader { }
   );
 
+  recursivenodes = callPackage ../development/python-modules/recursivenodes { };
+
   red-black-tree-mod = callPackage ../development/python-modules/red-black-tree-mod { };
 
   redbaron = callPackage ../development/python-modules/redbaron { };
