2f2814870fde53c51202c99c05727548cea1a095
Yueh-Shun Li <shamrocklee@posteo.net>
2025-04-30 23:21:53 +0800

    python3Packages.aspell-python: pytestFlagsArray -> enabledTestPaths

--- pkgs/development/python-modules/aspell-python/default.nix
+++ pkgs/development/python-modules/aspell-python/default.nix
@@ -35,7 +35,7 @@ buildPythonPackage rec {
     export HOME=$(mktemp -d)
   '';
 
-  pytestFlagsArray = [ "test/unittests.py" ];
+  enabledTestPaths = [ "test/unittests.py" ];
 
   disabledTests = lib.optionals (pythonAtLeast "3.10") [
     # https://github.com/WojciechMula/aspell-python/issues/22
