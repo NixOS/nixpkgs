76bf90e619c96f0a3fab2ddd4d0a2b59aa8f0545
nsfisis <nsfisis@gmail.com>
2025-04-30 20:45:43 +0900

    gomi: embed version information

--- pkgs/by-name/go/gomi/package.nix
+++ pkgs/by-name/go/gomi/package.nix
@@ -19,6 +19,15 @@ buildGoModule rec {
 
   subPackages = [ "." ];
 
+  # https://github.com/babarot/gomi/blob/v1.6.0/.goreleaser.yaml#L20-L22
+  ldflags = [
+    "-X main.version=v${version}"
+    # cd path/to/gomi; git switch -d "v${version}"; git show --format='%h' HEAD --quiet
+    "-X main.revision=da44e9f"
+    # cd path/to/gomi; git switch -d "v${version}"; date --date="@$(git show --format='%ct' HEAD --quiet)" --utc +'%Y-%m-%dT%H:%M:%SZ'
+    "-X main.buildDate=2025-03-17T05:38:26Z"
+  ];
+
   meta = {
     description = "Replacement for UNIX rm command";
     homepage = "https://github.com/b4b4r07/gomi";
