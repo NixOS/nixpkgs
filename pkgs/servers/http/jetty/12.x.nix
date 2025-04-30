5e6fec9b68c873e5acc6659bb239ea3bd2b3b94c
Anthony ROUSSEL <anthony@roussel.dev>
2025-04-30 21:54:02 +0200

    jetty_12: 12.0.14 -> 12.0.19
    https://github.com/jetty/jetty.project/releases/tag/jetty-12.0.14
    https://github.com/jetty/jetty.project/releases/tag/jetty-12.0.15
    https://github.com/jetty/jetty.project/releases/tag/jetty-12.0.16
    https://github.com/jetty/jetty.project/releases/tag/jetty-12.0.17
    https://github.com/jetty/jetty.project/releases/tag/jetty-12.0.18
    https://github.com/jetty/jetty.project/releases/tag/jetty-12.0.19

--- pkgs/servers/http/jetty/12.x.nix
+++ pkgs/servers/http/jetty/12.x.nix
@@ -1,4 +1,4 @@
 import ./common.nix {
-  version = "12.0.14";
-  hash = "sha256-Ps8pVq377QLFyQZg6AoyEr1G9PonFaVxOCD69/VTGEY=";
+  version = "12.0.19";
+  hash = "sha256-rURjebDGDVNILjrim8zBTp5SvIFkWKxpC+oi8b5FTj8=";
 }
