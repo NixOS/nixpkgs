NixOS/nixpkgs

commit 3a83abf73ff3e31ca9de21353da3799b47b78ccd

libcamera: 0.4.0 -> 0.5.0

pkgs/by-name/li/libcamera/package.nix
@@ -26,12 +26,12 @@
 
 stdenv.mkDerivation rec {
   pname = "libcamera";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchgit {
     url = "https://git.libcamera.org/libcamera/libcamera.git";
     rev = "v${version}";
-    hash = "sha256-m55SojGt5v5AEatBZiVqQA3xP9eeRWqHa+C3JsTiErQ=";
+    hash = "sha256-zlMjeLlEeigzisVr7kWVrTI5gRbpJb2pZvqXNdErITQ=";
   };
 
   outputs = [

commit 8fcd9969943e268457049d5371b2885063423805

libcamera: fix build on aarch64
Fixes: 3a83abf73ff3 ("libcamera: 0.4.0 -> 0.5.0")

pkgs/by-name/li/libcamera/package.nix
@@ -17,6 +17,7 @@
   python3,
   python3Packages,
   systemd, # for libudev
+  libpisp,
   withTracing ? lib.meta.availableOn stdenv.hostPlatform lttng-ust,
   lttng-ust, # withTracing
   withQcam ? false,
@@ -85,6 +86,7 @@ stdenv.mkDerivation rec {
 
       gtest
     ]
+    ++ lib.optionals stdenv.hostPlatform.isAarch [ libpisp ]
     ++ lib.optionals withTracing [ lttng-ust ]
     ++ lib.optionals withQcam [
       libtiff