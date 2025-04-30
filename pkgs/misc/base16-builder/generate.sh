c4e68484531473ecc1f518404445c66ea49ecf2c
JJ Brown <jj.brown@oracle.com>
2025-04-30 12:36:44 -0600

    Using newer version of node for base16-builder

--- pkgs/misc/base16-builder/generate.sh
+++ pkgs/misc/base16-builder/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p nodePackages.node2nix
-exec node2nix --nodejs-18 \
+exec node2nix --pkg-name nodejs_22 \
      --input node-packages.json \
      --output node-packages-generated.nix \
      --composition node-packages.nix \
