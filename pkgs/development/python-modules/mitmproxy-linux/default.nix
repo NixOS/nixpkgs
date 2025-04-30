10db73fe5f64ba627382ba17831f2db8c8564a73
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-04-30 13:09:46 +0200

    python313Packages.mitmproxy-linux: 0.11.5 -> 0.12.3, remove things duplicated and in sync with mitmproxy-rs
    Changelog: https://github.com/mitmproxy/mitmproxy_rs/blob/v0.12.3/CHANGELOG.md

--- pkgs/development/python-modules/mitmproxy-linux/default.nix
+++ pkgs/development/python-modules/mitmproxy-linux/default.nix
@@ -2,22 +2,14 @@
   lib,
   buildPythonPackage,
   bpf-linker,
-  fetchFromGitHub,
   rustPlatform,
-  mitmproxy,
+  mitmproxy-rs,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "mitmproxy-linux";
-  version = "0.11.5";
+  inherit (mitmproxy-rs) version src cargoDeps;
   pyproject = true;
-
-  src = fetchFromGitHub {
-    owner = "mitmproxy";
-    repo = "mitmproxy_rs";
-    tag = "v${version}";
-    hash = "sha256-vC+Vsv7UWjkO+6lm7gAb91Ig04Y7r9gYQoz6R9xpxsA=";
-  };
 
   postPatch = ''
     substituteInPlace mitmproxy-linux/build.rs \
@@ -27,11 +19,6 @@ buildPythonPackage rec {
     substituteInPlace mitmproxy-linux-ebpf/.cargo/config.toml \
       --replace-fail 'build-std = ["core"]' ""
   '';
-
-  cargoDeps = rustPlatform.fetchCargoVendor {
-    inherit pname version src;
-    hash = "sha256-CFsefq1zQLIYjZcfoy3afYfP/0MlBoi9kVx7FVGEKr0=";
-  };
 
   RUSTFLAGS = "-C target-feature=";
   RUSTC_BOOTSTRAP = 1;
@@ -49,13 +36,13 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "mitmproxy_linux" ];
 
-  meta = {
-    description = "Rust bits in mitmproxy";
-    homepage = "https://github.com/mitmproxy/mitmproxy_rs/tree/main/mitmproxy-linux";
-    changelog = "https://github.com/mitmproxy/mitmproxy_rs/blob/${src.rev}/CHANGELOG.md#${
-      lib.replaceStrings [ "." ] [ "" ] version
-    }";
-    license = lib.licenses.mit;
-    inherit (mitmproxy.meta) maintainers;
-  };
+  meta =
+    {
+      inherit (mitmproxy-rs.meta) changelog license maintainers;
+    }
+    // {
+      description = "Linux Rust bits in mitmproxy";
+      homepage = "https://github.com/mitmproxy/mitmproxy_rs/tree/main/mitmproxy-linux";
+      platforms = lib.platforms.linux;
+    };
 }
