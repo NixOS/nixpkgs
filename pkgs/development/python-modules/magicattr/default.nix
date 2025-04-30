319a82af87055a9d00f5b6ceceaeae400a86eb9e
Matt Wittmann <mcwitt@gmail.com>
2025-04-30 11:57:09 -0700

    python3Packages.magicattr: init at 0.1.6

---
+++ pkgs/development/python-modules/magicattr/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  wheel,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "magicattr";
+  version = "0.1.6";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "frmdstryr";
+    repo = "magicattr";
+    tag = "v${version}";
+    hash = "sha256-hV425AnXoYL3oSYMhbXaF8VRe/B1s5f5noAZYz4MMwc=";
+  };
+
+  build-system = [
+    setuptools
+    wheel
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  enabledTestPaths = [
+    "tests.py"
+  ];
+
+  pythonImportsCheck = [
+    "magicattr"
+  ];
+
+  meta = {
+    description = "A getattr and setattr that works on nested objects, lists, dicts, and any combination thereof without resorting to eval";
+    homepage = "https://github.com/frmdstryr/magicattr";
+    changelog = "https://github.com/frmdstryr/magicattr/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ mcwitt ];
+  };
+}
