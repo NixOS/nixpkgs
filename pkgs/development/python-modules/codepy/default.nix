8bef766ef7ce908f8565f136cff12af7b8400fee
qbisi <qbisicwate@gmail.com>
2025-05-02 01:32:21 +0800

    python312Packages.codepy: 2019.1 -> 2025.1

--- pkgs/development/python-modules/codepy/default.nix
+++ pkgs/development/python-modules/codepy/default.nix
@@ -2,40 +2,60 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  pytools,
-  appdirs,
-  six,
+  hatchling,
   cgen,
+  numpy,
+  pytools,
+  platformdirs,
+  typing-extensions,
+  boost,
+  ruff,
+  pytestCheckHook,
+  writableTmpDirAsHomeHook,
 }:
 
 buildPythonPackage rec {
   pname = "codepy";
-  version = "2019.1";
-  format = "setuptools";
+  version = "2025.1";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "inducer";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-viMfB/nDrvDA/IGRZEX+yXylxbbmqbh/fgdYXBzK0zM=";
+    tag = "v${version}";
+    hash = "sha256-PHIC3q9jQlRRoUoemVtyrl5hcZXMX28gRkI5Xpk9yBY=";
   };
 
-  buildInputs = [
-    pytools
-    six
-    cgen
+  build-system = [
+    hatchling
   ];
-  propagatedBuildInputs = [ appdirs ];
+
+  dependencies = [
+    cgen
+    numpy
+    pytools
+    platformdirs
+    typing-extensions
+    # boost is needed for generating boostPython code
+    boost
+  ];
 
   pythonImportsCheck = [ "codepy" ];
 
-  # Tests are broken
-  doCheck = false;
+  nativeCheckInputs = [
+    ruff
+    pytestCheckHook
+    writableTmpDirAsHomeHook
+  ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/inducer/codepy";
     description = "Generate and execute native code at run time, from Python";
-    license = licenses.mit;
-    maintainers = with maintainers; [ atila ];
+    changelog = "https://github.com/inducer/codepy/releases/tag/${src.tag}";
+    license = lib.licenses.mit;
+    # Dynamic compilation and linking are so far only supported in Linux with the GNU toolchain.
+    # See https://github.com/inducer/codepy/blob/c23f44dd3986f7d3d116df88dc5097404f639256/README.rst?plain=1#L14.
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ atila ];
   };
 }
