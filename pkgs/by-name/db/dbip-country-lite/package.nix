5920d56fec9c60e0750fbbe1aa75d5da5a7fdef4
Nick Cao <nickcao@nichi.co>
2025-05-01 17:03:09 -0400

    dbip-country-lite: 2025-04 -> 2025-05

--- pkgs/by-name/db/dbip-country-lite/package.nix
+++ pkgs/by-name/db/dbip-country-lite/package.nix
@@ -5,11 +5,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "dbip-country-lite";
-  version = "2025-04";
+  version = "2025-05";
 
   src = fetchurl {
     url = "https://download.db-ip.com/free/dbip-country-lite-${finalAttrs.version}.mmdb.gz";
-    hash = "sha256-Be9Q4VQUb5ql0BLreSu4YBGA+NHJkofjsp46aN7tI1E=";
+    hash = "sha256-7ZmodKYVMID/5yOMZuEpRkb41/DTrgprJPG7DYnYNak=";
   };
 
   dontUnpack = true;
