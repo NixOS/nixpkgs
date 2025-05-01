b155e924d6a697a8e42d33bf776ee66f74d2de2b
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-01 07:50:06 +0000

    python312Packages.pytest-recording: 0.13.2 -> 0.13.3

--- pkgs/development/python-modules/pytest-recording/default.nix
+++ pkgs/development/python-modules/pytest-recording/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "pytest-recording";
-  version = "0.13.2";
+  version = "0.13.3";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "kiwicom";
     repo = "pytest-recording";
     tag = "v${version}";
-    hash = "sha256-C6uNp3knKKY0AX7fQYU85N82L6kyyO4HcExTz1bBtpE=";
+    hash = "sha256-CBJkz4JaeU4G4BrPPWgw5B/oQHAonzZer4sAWBooacg=";
   };
 
   buildInputs = [
