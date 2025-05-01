76729e88f41749cbf5936e9f0d7b1d5e69451ea0
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2025-05-01 18:26:33 +0200

    gildas: fix build on aarch64-linux and x86_64-linux
    Remove unused dependency on lesstif, which does not build on *-linux.
    
    ZHF: #403336

--- pkgs/by-name/gi/gildas/package.nix
+++ pkgs/by-name/gi/gildas/package.nix
@@ -6,7 +6,6 @@
   pkg-config,
   python3,
   gfortran,
-  lesstif,
   cfitsio,
   getopt,
   perl,
@@ -52,7 +51,6 @@ stdenv.mkDerivation rec {
   buildInputs =
     [
       gtk2-x11
-      lesstif
       cfitsio
       python3Env
       ncurses
