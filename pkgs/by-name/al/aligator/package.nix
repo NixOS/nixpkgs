fd2943c8dc44924992009437c0aff1908793d169
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-05-02 14:20:35 +0200

    aligator: clean

--- pkgs/by-name/al/aligator/package.nix
+++ pkgs/by-name/al/aligator/package.nix
@@ -1,21 +1,29 @@
 {
-  cmake,
-  crocoddyl,
-  doxygen,
-  fetchFromGitHub,
-  fmt,
-  fontconfig,
-  gbenchmark,
-  graphviz,
   lib,
+  stdenv,
+  fetchFromGitHub,
+  fontconfig,
   llvmPackages,
-  pinocchio,
-  pkg-config,
-  proxsuite-nlp,
   python3Packages,
   pythonSupport ? false,
-  stdenv,
+
+  # nativeBuildInputs
+  doxygen,
+  cmake,
+  graphviz,
+  pkg-config,
+
+  # buildInputs
+  fmt,
+
+  # propagatedBuildInputs
   suitesparse,
+  crocoddyl,
+  pinocchio,
+  proxsuite-nlp,
+
+  # checkInputs
+  gbenchmark,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
