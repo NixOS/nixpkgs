11cbc36a1bcce62615ecf78eef2b4c6511cd5b2d
Felix Schröter <dev@felschr.com>
2025-05-02 21:35:04 +0200

    mullvad-browser: 14.5 -> 14.5.1
    https://github.com/mullvad/mullvad-browser/releases/tag/14.5.1

--- pkgs/by-name/mu/mullvad-browser/package.nix
+++ pkgs/by-name/mu/mullvad-browser/package.nix
@@ -97,7 +97,7 @@ let
     ++ lib.optionals mediaSupport [ ffmpeg ]
   );
 
-  version = "14.5";
+  version = "14.5.1";
 
   sources = {
     x86_64-linux = fetchurl {
@@ -109,7 +109,7 @@ let
         "https://tor.eff.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
         "https://tor.calyxinstitute.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
       ];
-      hash = "sha256-uqwsDXbS8tfG/bgTQKvdiaPzchVhssoQccQStncNWOk=";
+      hash = "sha256-FQdPdCkdxh+wieC12lT90qaSvu56o4BDgUkaDOm5xSE=";
     };
   };
 
