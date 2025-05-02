9f71c9d5d258dbe6228b447438e062be4492a5f0
Jan Tojnar <jtojnar@gmail.com>
2025-05-02 20:19:42 +0000

    gnome-music: 48.beta → 48.0
    https://gitlab.gnome.org/GNOME/gnome-music/-/compare/48.beta...48.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/by-name/gn/gnome-music/package.nix
+++ pkgs/by-name/gn/gnome-music/package.nix
@@ -31,13 +31,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "gnome-music";
-  version = "48.beta";
+  version = "48.0";
 
   format = "other";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-music/${lib.versions.major version}/gnome-music-${version}.tar.xz";
-    hash = "sha256-qIw01hGwr0zg2LFAvTPfhowpTSMYMQxTZJN9QwgmJOw=";
+    hash = "sha256-jNqs0FJitjuxYIolrK1RboksMyIyNXy5t/aPLPuG1m4=";
   };
 
   nativeBuildInputs = [
