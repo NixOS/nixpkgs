842fad17bc0cf5a1de9d84474d50bffeb1a7a6d5
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-02 04:37:46 +0000

    gosmee: 0.24.0 -> 0.25.0

--- pkgs/by-name/go/gosmee/package.nix
+++ pkgs/by-name/go/gosmee/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "gosmee";
-  version = "0.24.0";
+  version = "0.25.0";
 
   src = fetchFromGitHub {
     owner = "chmouel";
     repo = "gosmee";
     rev = "v${version}";
-    hash = "sha256-hE9iZkIkMzCICw9n1XhJ2PO5SvqE0EVhLJQO7tCUw3U=";
+    hash = "sha256-aQ25V6FKfvXWFjeWkTQLWQfeZGBMlBUe2L9IkDPN+88=";
   };
   vendorHash = null;
 
