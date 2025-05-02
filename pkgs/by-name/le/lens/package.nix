8a6a861a6bf259b833725033643319260e0f0087
ElXreno <elxreno@gmail.com>
2025-05-02 21:35:44 +0300

    lens-desktop: 2024.11.261604 --> 2025.9.151055

--- pkgs/by-name/le/lens/package.nix
+++ pkgs/by-name/le/lens/package.nix
@@ -8,20 +8,20 @@
 let
 
   pname = "lens-desktop";
-  version = "2024.11.261604";
+  version = "2025.9.151055";
 
   sources = {
     x86_64-linux = {
       url = "https://api.k8slens.dev/binaries/Lens-${version}-latest.x86_64.AppImage";
-      hash = "sha256-AbuEU5gOckVU+eDIFnomc7ryLq68ihuk3c0XosoJp74=";
+      hash = "sha256-XIEAhda75yseNmGSbHwOSqkzSSWCHUiyZm1rf+eEqs0=";
     };
     x86_64-darwin = {
       url = "https://api.k8slens.dev/binaries/Lens-${version}-latest.dmg";
-      hash = "sha256-MQQRGTCe+LEHXJi6zjnpENbtlWNP+XVH9rWXRMk+26w=";
+      hash = "sha256-ORwUdpyLDvPCcC8v3HeFt3f2I+qNSIi9UgERrvgH2AE=";
     };
     aarch64-darwin = {
       url = "https://api.k8slens.dev/binaries/Lens-${version}-latest-arm64.dmg";
-      hash = "sha256-aakJCLnQBAnUdrrniTcahS+q3/kP09mlaPTV8FW5afI=";
+      hash = "sha256-cu9TUTk3TJgdotJDUVielIDIGTocCIPVQiwJHEKeiIA=";
     };
   };
 
