26cd77618de77c433bbfb241ecb9699460c74aec
Doron Behar <doron.behar@gmail.com>
2025-05-03 20:33:55 +0300

    LPCNet: small semantic modernizing changes

--- pkgs/by-name/lp/LPCNet/package.nix
+++ pkgs/by-name/lp/LPCNet/package.nix
@@ -16,9 +16,10 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "drowe67";
     repo = "LPCNet";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-tHZLKXmuM86A6OpfS3CRRjhFbqj1Q/w1w56msdgLHb0=";
   };
+
   passthru = {
     # Prebuilt neural network model that is needed during the build - can be overwritten
     nnmodel = fetchurl {
@@ -26,6 +27,7 @@ stdenv.mkDerivation (finalAttrs: {
       hash = "sha256-UJRAkkdR/dh/+qVoPuPd3ZN69cgzuRBMzOZdUWFJJsg=";
     };
   };
+
   preConfigure = ''
     mkdir build
     cp \
@@ -37,20 +39,28 @@ stdenv.mkDerivation (finalAttrs: {
     patchShebangs *.sh unittest/*.sh
   '';
 
-  nativeBuildInputs = [ cmake ];
-  buildInputs = [ codec2 ];
-  nativeCheckInputs = [ octave ];
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  buildInputs = [
+    codec2
+  ];
+
+  nativeCheckInputs = [
+    octave
+  ];
 
   doCheck = true;
   preCheck = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH''${LD_LIBRARY_PATH:+:}/build/source/build/src"
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Experimental Neural Net speech coding for FreeDV";
     homepage = "https://github.com/drowe67/LPCNet";
-    license = licenses.bsd3;
-    maintainers = with maintainers; [ doronbehar ];
-    platforms = platforms.all;
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ doronbehar ];
+    platforms = lib.platforms.all;
   };
 })
