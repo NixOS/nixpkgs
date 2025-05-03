6d230d72a68e1ee0a63aaa55d0471ffdb363eba1
Patrick <patrick@failmail.dev>
2025-05-03 10:31:28 +0200

    netbird-dashboard: 2.9.0 -> 2.11.0

--- pkgs/by-name/ne/netbird-dashboard/package.nix
+++ pkgs/by-name/ne/netbird-dashboard/package.nix
@@ -6,13 +6,13 @@
 
 buildNpmPackage rec {
   pname = "netbird-dashboard";
-  version = "2.9.0";
+  version = "2.11.0";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "dashboard";
     rev = "v${version}";
-    hash = "sha256-PY/jK96FK6Y0++Ie4Yg/7GrGoLtLcgCSzXIkqySxe2M=";
+    hash = "sha256-JHpFxWhN5ZXd0Yn0AY98pl/nrby+RsWO6l8qUospkak=";
   };
 
   npmDepsHash = "sha256-TELyc62l/8IaX9eL2lxRFth0AAZ4LXsV2WNzXSHRnTw=";
@@ -30,6 +30,9 @@ buildNpmPackage rec {
     description = "NetBird Management Service Web UI Panel";
     homepage = "https://github.com/netbirdio/dashboard";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ vrifox ];
+    maintainers = with maintainers; [
+      vrifox
+      patrickdag
+    ];
   };
 }
