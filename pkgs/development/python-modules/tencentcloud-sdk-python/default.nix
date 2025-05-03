640b536d93f6e7fdaa68f97df2858b9ecf945944
Fabian Affolter <mail@fabian-affolter.ch>
2025-05-03 12:40:25 +0200

    python313Packages.tencentcloud-sdk-python: 3.0.1371 -> 3.0.1372
    Diff: https://github.com/TencentCloud/tencentcloud-sdk-python/compare/refs/tags/3.0.1371...refs/tags/3.0.1372
    
    Changelog: https://github.com/TencentCloud/tencentcloud-sdk-python/blob/3.0.1372/CHANGELOG.md

--- pkgs/development/python-modules/tencentcloud-sdk-python/default.nix
+++ pkgs/development/python-modules/tencentcloud-sdk-python/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "tencentcloud-sdk-python";
-  version = "3.0.1371";
+  version = "3.0.1372";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "TencentCloud";
     repo = "tencentcloud-sdk-python";
     tag = version;
-    hash = "sha256-kgm4mrY4dEEkMfUy/TwH5t8cxn2FptC0BVB/blkTYaA=";
+    hash = "sha256-XV1qBhTY6NVLM+wKVUzQ3DnNbOLnB+Z5fDEprfSdkIQ=";
   };
 
   build-system = [ setuptools ];
