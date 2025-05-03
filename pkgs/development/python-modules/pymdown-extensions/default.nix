a516ab5492f77ad28206aebc9ff8112cabaabef3
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-03 15:42:37 +0000

    python3Packages.pymdown-extensions: 10.14.3 -> 10.15

--- pkgs/development/python-modules/pymdown-extensions/default.nix
+++ pkgs/development/python-modules/pymdown-extensions/default.nix
@@ -45,14 +45,14 @@ let
 in
 buildPythonPackage rec {
   pname = "pymdown-extensions";
-  version = "10.14.3";
+  version = "10.15";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "facelessuser";
     repo = "pymdown-extensions";
     tag = version;
-    hash = "sha256-i0i9hyq60RbfJ6YjFsAPXVYwbV8VrFaTIfDCkl8BvhQ=";
+    hash = "sha256-ADl1l1cgJC3T8EN+cGNq14VfShGLG51ElXFRx+tdnCg=";
   };
 
   build-system = [ hatchling ];
