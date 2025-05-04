7c04179b64196642067fd360a9a949c4369d2bdb
June Stepp <git@junestepp.me>
2025-05-03 22:51:52 -0500

    anki-addons.reviewer-refocus-card: init at 0-unstable-2022-12-24

--- pkgs/games/anki/addons/default.nix
+++ pkgs/games/anki/addons/default.nix
@@ -77,4 +77,29 @@ in
       maintainers = with lib.maintainers; [ junestepp ];
     };
   });
+
+  reviewer-refocus-card = buildAnkiAddon {
+    pname = "reviewer-refocus-card";
+    version = "0-unstable-2022-12-24";
+    src = fetchFromGitHub {
+      owner = "glutanimate";
+      repo = "anki-addons-misc";
+      rev = "7b981836e0a6637a1853f3e8d73d022ab95fed31";
+      sparseCheckout = [ "src/reviewer_refocus_card" ];
+      hash = "sha256-181hyc4ED+0lBzn1FnrBvNIYIUQF8xEDB3uHK6SkpHw=";
+    };
+    sourceRoot = "source/src/reviewer_refocus_card";
+    passthru.updateScript = nix-update-script {
+      extraArgs = [ "--version=branch" ];
+    };
+    meta = {
+      description = ''
+        Sets focus to the card area, allowing you to scroll through your cards using
+        Page Up / Page Down, etc
+      '';
+      homepage = "https://github.com/glutanimate/anki-addons-misc";
+      license = lib.licenses.agpl3Only;
+      maintainers = with lib.maintainers; [ junestepp ];
+    };
+  };
 }
