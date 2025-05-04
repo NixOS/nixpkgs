bf1c1815c18f125492c84fa15adfcb5e06ca8d50
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-05-04 21:32:59 +0200

    espansoPlugins.english-lorem: init at 0.1.0

---
+++ pkgs/by-name/es/espanso/plugins/english-lorem/default.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  fetchFromGitHub,
+  mkEspansoPlugin,
+}:
+mkEspansoPlugin {
+  pname = "english-lorem";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "espanso";
+    repo = "hub";
+    rev = "d62bde95fc6f6cf90d4c23ee243922037c053def";
+    hash = "sha256-1cPtknrIi+9uTQt0pMaOBnY3C1GAGXPAA4fIgBh5Wgs=";
+  };
+
+  meta = {
+    description = "Lorem ipsum sentences and paragraphs generator but in English!";
+    maintainers = with lib.maintainers; [ delafthi ];
+  };
+}
