96ebf937cdfeb343254563223c890d38f57cb1ac
June Stepp <git@junestepp.me>
2025-05-03 22:55:54 -0500

    anki-addons.local-audio-yomichan: init at 0-unstable-2025-04-26

--- pkgs/games/anki/addons/default.nix
+++ pkgs/games/anki/addons/default.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   fetchFromSourcehut,
   nix-update-script,
+  python3,
 }:
 let
   inherit (anki-utils) buildAnkiAddon;
@@ -119,6 +120,42 @@ in
       description = "An audio server for yomichan that scrapes forvo for audio files";
       homepage = "https://github.com/jamesnicolas/yomichan-forvo-server";
       license = lib.licenses.unlicense;
+      maintainers = with lib.maintainers; [ junestepp ];
+    };
+  };
+
+  local-audio-yomichan = buildAnkiAddon {
+    pname = "local-audio-yomichan";
+    version = "0-unstable-2025-04-26";
+    src = fetchFromGitHub {
+      owner = "yomidevs";
+      repo = "local-audio-yomichan";
+      rev = "34750f1d8ca1cb473128fea7976a4d981e5e78a4";
+      sparseCheckout = [ "plugin" ];
+      hash = "sha256-2gyggcvxParay+1B7Sg2COKyocoxaRO1WTz+ymdRp4w=";
+    };
+    sourceRoot = "source/plugin";
+    processUserFiles = ''
+      # Addon will try to load extra stuff unless Python package name is "plugin".
+      temp=$(mktemp -d)
+      ln -s $PWD $temp/plugin
+      # Addoon expects `user_files` dir at `$XDG_DATA_HOME/local-audio-yomichan`
+      ln -s $PWD/user_files $temp/local-audio-yomichan
+
+      PYTHONPATH=$temp \
+      WO_ANKI=1 \
+      XDG_DATA_HOME=$temp \
+      ${lib.getExe python3} -c \
+        "from plugin import db_utils; \
+         db_utils.init_db()"
+    '';
+    passthru.updateScript = nix-update-script {
+      extraArgs = [ "--version=branch" ];
+    };
+    meta = {
+      description = "Anki add-on to run a local audio server for Yomitan";
+      homepage = "https://github.com/yomidevs/local-audio-yomichan";
+      license = lib.licenses.mit;
       maintainers = with lib.maintainers; [ junestepp ];
     };
   };
