2b3366856abeb139cc6c0a24c6eb431a75d8e191
Zexin Yuan <git@yzx9.xyz>
2025-05-04 21:56:47 +0800

    hmat-oss: modernize

--- pkgs/by-name/hm/hmat-oss/package.nix
+++ pkgs/by-name/hm/hmat-oss/package.nix
@@ -8,15 +8,15 @@
   lapack,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "hmat-oss";
   version = "1.10.0";
 
   src = fetchFromGitHub {
     owner = "jeromerobert";
     repo = "hmat-oss";
-    tag = version;
-    sha256 = "sha256-GnFlvZCEzSCcBVLjFWLe+AKXVA6UMs/gycrOJ2TBqrE=";
+    tag = finalAttrs.version;
+    hash = "sha256-GnFlvZCEzSCcBVLjFWLe+AKXVA6UMs/gycrOJ2TBqrE=";
   };
 
   cmakeFlags = [
@@ -38,11 +38,11 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  meta = with lib; {
+  meta = {
     description = "Hierarchical matrix C/C++ library";
     homepage = "https://github.com/jeromerobert/hmat-oss";
-    license = licenses.gpl2;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ gdinh ];
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [ gdinh ];
   };
-}
+})
