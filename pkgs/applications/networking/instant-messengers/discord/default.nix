0d61a96b9b8e96750d0b953183e48686fae877af
jopejoe1 <jopejoe1@missing.ninja>
2025-05-04 13:18:54 +0000

    discord-development: 0.0.73 -> 0.0.74

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -12,7 +12,7 @@ let
         stable = "0.0.93";
         ptb = "0.0.141";
         canary = "0.0.668";
-        development = "0.0.73";
+        development = "0.0.74";
       }
     else
       {
@@ -38,7 +38,7 @@ let
       };
       development = fetchurl {
         url = "https://development.dl2.discordapp.net/apps/linux/${version}/discord-development-${version}.tar.gz";
-        hash = "sha256-zd8KpyBTdj+7jb1kRY99a48thdCRq3RNSC8oWKAQJXg=";
+        hash = "sha256-tF3Evi9SiGuBWJa+O9O6TpSJXiBgioZe6nmdjwcahwY=";
       };
     };
     x86_64-darwin = {
