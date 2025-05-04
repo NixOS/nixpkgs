6300e30393bec57caf8ef57cf08ed91c25d5c6e8
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-05-04 21:35:34 +0200

    espansoPlugins.italian-accents: init at 0.2.0

---
+++ pkgs/by-name/es/espanso/plugins/italian-accents/default.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  fetchFromGitHub,
+  mkEspansoPlugin,
+}:
+mkEspansoPlugin {
+  pname = "italian-accents";
+  version = "0.2.0";
+
+  src = fetchFromGitHub {
+    owner = "espanso";
+    repo = "hub";
+    rev = "d62bde95fc6f6cf90d4c23ee243922037c053def";
+    hash = "sha256-1cPtknrIi+9uTQt0pMaOBnY3C1GAGXPAA4fIgBh5Wgs=";
+  };
+
+  meta = {
+    description = "Include Italian accents substitutions to espanso.";
+    maintainers = with lib.maintainers; [ delafthi ];
+  };
+}
