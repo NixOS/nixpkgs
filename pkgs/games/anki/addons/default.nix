e003114bc695d0190fd503a578e3a5511ad65dd5
June Stepp <git@junestepp.me>
2025-05-03 22:48:03 -0500

    anki-addons.anki-connect: init at 24.7.25.0

--- pkgs/games/anki/addons/default.nix
+++ pkgs/games/anki/addons/default.nix
@@ -2,6 +2,7 @@
   lib,
   anki-utils,
   fetchFromGitHub,
+  fetchFromSourcehut,
   nix-update-script,
 }:
 let
@@ -22,6 +23,28 @@ in
       description = "Adds a new menu item for adjusting the sound volume";
       homepage = "https://github.com/lambdadog/passfail2";
       license = lib.licenses.agpl3Only;
+      maintainers = with lib.maintainers; [ junestepp ];
+    };
+  });
+
+  anki-connect = buildAnkiAddon (finalAttrs: {
+    pname = "anki-connect";
+    version = "24.7.25.0";
+    src = fetchFromSourcehut {
+      owner = "~foosoft";
+      repo = "anki-connect";
+      rev = finalAttrs.version;
+      hash = "sha256-N98EoCE/Bx+9QUQVeU64FXHXSek7ASBVv1b9ltJ4G1U=";
+    };
+    sourceRoot = "source/plugin";
+    passthru.updateScript = nix-update-script { };
+    meta = {
+      description = ''
+        Enables external applications such as Yomichan to communicate
+        with Anki over a simple HTTP API
+      '';
+      homepage = "https://foosoft.net/projects/anki-connect/";
+      license = lib.licenses.gpl3Plus;
       maintainers = with lib.maintainers; [ junestepp ];
     };
   });
