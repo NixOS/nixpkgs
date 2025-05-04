fc4e8f1a2e1a4b5e11c5f461ac44fc51431f9bbb
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-05-04 21:31:25 +0200

    espansoPlugins.brand-names: init at 1.0.2

---
+++ pkgs/by-name/es/espanso/plugins/brand-names/default.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  fetchFromGitHub,
+  mkEspansoPlugin,
+}:
+mkEspansoPlugin {
+  pname = "brand-names";
+  version = "1.0.2";
+
+  src = fetchFromGitHub {
+    owner = "espanso";
+    repo = "hub";
+    rev = "d62bde95fc6f6cf90d4c23ee243922037c053def";
+    hash = "sha256-1cPtknrIi+9uTQt0pMaOBnY3C1GAGXPAA4fIgBh5Wgs=";
+  };
+
+  meta = {
+    description = "Automatically capitalize common brand and product names.";
+    homepage = "https://github.com/joshjhall/espanso-brand-names";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ delafthi ];
+  };
+}
