ed9d8b1d06b723fda3d529a10c6f6974b883278e
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-05-04 17:18:45 -0300

    omnigres: attempt to hack extensions into it & fix config phase

--- pkgs/servers/sql/postgresql/ext/omnigres.nix
+++ pkgs/servers/sql/postgresql/ext/omnigres.nix
@@ -1,12 +1,18 @@
 {
+  buildEnv,
   bison,
   boost,
+  brotli,
+  bzip2,
   cmake,
+  doxygen,
   fetchFromGitHub,
   flex,
   git,
   lib,
+  libcap,
   libtool,
+  libuv,
   libxml2,
   lld,
   netcat,
@@ -15,18 +21,21 @@
   pkg-config,
   postgresql,
   postgresqlBuildExtension,
-  # TODO: enable this later
-  # postgresqlTestExtension,
+  postgresqlTestExtension,
+  postgresqlPackages,
   python3,
   readline,
   stdenv,
   zlib,
-  # Package options
-  pg_version ? "17"
 }:
 
 let
-  postgresqlPy = postgresql.override { pythonSupport = true; python3 = python3;};
+  # TODO: I'll remove this as well, just testing stuff
+  extensions =
+    buildEnv {
+      name = "postgresql-custom";
+      paths = [ postgresql postgresqlPackages.plpython3 ];
+    };
 in
 postgresqlBuildExtension (finalAttrs: {
   pname = "omnigres";
@@ -40,24 +49,37 @@ postgresqlBuildExtension (finalAttrs: {
   };
 
   nativeBuildInputs = [ cmake flex libtool pkg-config ];
-  buildInputs = [
-    bison
-    boost
-    flex
-    git
-    libxml2
-    lld
-    netcat
-    openssl
-    perl
-    postgresqlPy
-    python3
-    readline
-    zlib
-  ];
+  buildInputs =
+    postgresql.buildInputs ++ [
+      bison
+      boost
+      brotli
+      bzip2
+      doxygen
+      flex
+      git
+      libcap
+      libuv
+      libxml2
+      lld
+      netcat
+      openssl
+      perl
+      python3
+      readline
+      zlib
+      # TODO: just some hacky tests, remove this later
+      extensions
+    ];
+
+  preConfigure = ''
+    substituteInPlace cmake/PostgreSQLExtension.cmake \
+      --replace "\''${_ext_dir}" "$out"
+  '';
 
   cmakeFlags = [
      "-DCMAKE_BUILD_TYPE=Release"
+     "-DPGVER=${postgresql.version}"
      "-DNETCAT=${netcat}/bin/nc"
      "-DOPENSSL_CONFIGURED=1"
      "-DPG_CONFIG=${postgresql.pg_config}/bin/pg_config"
@@ -66,16 +88,25 @@ postgresqlBuildExtension (finalAttrs: {
      "-DCREATEDB=${postgresql}/bin/createdb"
      "-DINITDB=${postgresql}/bin/initdb"
      "-DPostgreSQL_LIBRARY_DIRS=${postgresql}/lib"
-     "-DPostgreSQL_EXTENSION_DIR=${postgresql}/share/postgresql/extension"
+     "-DPostgreSQL_EXTENSION_DIR=${extensions}/share/postgresql/extension"
      "-DPostgreSQL_SERVER_INCLUDE_DIRS=${lib.getDev postgresql}/include"
      "-DPython3_EXECUTABLE=${python3}/bin/python3"
      "-DPython_EXECUTABLE=${python3}/bin/python3"
+     "-DDOXYGEN_EXECUTABLE=${doxygen}/bin/doxygen"
   ];
 
   buildPhase = ''
     cmake -S . -B build
-    cmake --build build --parallel
+    make all
   '';
+
+  passthru.tests.extension = postgresqlTestExtension {
+    inherit (finalAttrs) finalPackage;
+    withPackages = [ "plpython3" "omnigres" ];
+    sql = ''
+      CREATE EXTENSION omni;
+    '';
+  };
 
   meta = {
     description = "Postgres as a Business Operating System.";
