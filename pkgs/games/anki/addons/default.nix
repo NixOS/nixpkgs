35bf9378a26c1e525bc1c55cf54c3c70fd1593aa
June Stepp <git@junestepp.me>
2025-05-03 22:46:34 -0500

    anki-addons.adjust-sound-volume: init at 0.0.6

--- pkgs/games/anki/addons/default.nix
+++ pkgs/games/anki/addons/default.nix
@@ -1,9 +1,28 @@
 {
+  lib,
   anki-utils,
+  fetchFromGitHub,
+  nix-update-script,
 }:
 let
   inherit (anki-utils) buildAnkiAddon;
 in
 {
-
+  adjust-sound-volume = buildAnkiAddon (finalAttrs: {
+    pname = "adjust-sound-volume";
+    version = "0.0.6";
+    src = fetchFromGitHub {
+      owner = "mnogu";
+      repo = "adjust-sound-volume";
+      rev = "v${finalAttrs.version}";
+      hash = "sha256-6reIUz+tHKd4KQpuofLa/tIL5lCloj3yODZ8Cz29jFU=";
+    };
+    passthru.updateScript = nix-update-script { };
+    meta = {
+      description = "Adds a new menu item for adjusting the sound volume";
+      homepage = "https://github.com/lambdadog/passfail2";
+      license = lib.licenses.agpl3Only;
+      maintainers = with lib.maintainers; [ junestepp ];
+    };
+  });
 }
