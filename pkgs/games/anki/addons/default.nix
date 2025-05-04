ac0355042ca2ddc624b32dc5c33bbabd2cc9752b
June Stepp <git@junestepp.me>
2025-05-03 22:53:56 -0500

    anki-addons.yomichan-forvo-server: init at 0-unstable-2024-10-21

--- pkgs/games/anki/addons/default.nix
+++ pkgs/games/anki/addons/default.nix
@@ -102,4 +102,24 @@ in
       maintainers = with lib.maintainers; [ junestepp ];
     };
   };
+
+  yomichan-forvo-server = buildAnkiAddon {
+    pname = "yomichan-forvo-server";
+    version = "0-unstable-2024-10-21";
+    src = fetchFromGitHub {
+      owner = "jamesnicolas";
+      repo = "yomichan-forvo-server";
+      rev = "364fc6d5d10969f516e0fa283460dfaf08c98e15";
+      hash = "sha256-Jpee9hkXCiBmSW7hzJ1rAg45XVIiLC8WENc09+ySFVI=";
+    };
+    passthru.updateScript = nix-update-script {
+      extraArgs = [ "--version=branch" ];
+    };
+    meta = {
+      description = "An audio server for yomichan that scrapes forvo for audio files";
+      homepage = "https://github.com/jamesnicolas/yomichan-forvo-server";
+      license = lib.licenses.unlicense;
+      maintainers = with lib.maintainers; [ junestepp ];
+    };
+  };
 }
