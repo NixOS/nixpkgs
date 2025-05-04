4db95214f56e87a7ba740d1de6b6e7dc74501263
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-05-04 21:42:00 +0200

    espansoPlugins.turkish-accent: init at 0.1.0

---
+++ pkgs/by-name/es/espanso/plugins/turkish-accent/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  fetchFromGitHub,
+  mkEspansoPlugin,
+}:
+mkEspansoPlugin {
+  pname = "turkish-accent";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "espanso";
+    repo = "hub";
+    rev = "d62bde95fc6f6cf90d4c23ee243922037c053def";
+    hash = "sha256-1cPtknrIi+9uTQt0pMaOBnY3C1GAGXPAA4fIgBh5Wgs=";
+  };
+
+  meta = {
+    description = "A simple package to convert English characters to Turkish.";
+    homepage = "https://github.com/enderahmetyurt/espanso-package-example";
+    maintainers = with lib.maintainers; [ delafthi ];
+  };
+}
