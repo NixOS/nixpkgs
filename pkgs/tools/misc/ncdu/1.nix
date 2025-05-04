71a3a2b52965f5051a640b44408f9bb3696f38a3
Anton Mosich <anton@mosich.at>
2025-05-04 14:29:08 +0200

    ncdu_1: modernize
    (cherry picked from commit f5871d0d3a91f6acbf25f4a046c19612acd46645)

--- pkgs/tools/misc/ncdu/1.nix
+++ pkgs/tools/misc/ncdu/1.nix
@@ -4,14 +4,15 @@
   fetchurl,
   pkg-config,
   ncurses,
+  versionCheckHook,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "ncdu";
   version = "1.22";
 
   src = fetchurl {
-    url = "https://dev.yorhel.nl/download/${pname}-${version}.tar.gz";
+    url = "https://dev.yorhel.nl/download/ncdu-${finalAttrs.version}.tar.gz";
     sha256 = "sha256-CtbAltwE1RIFgRBHYMAbj06X1BkdbJ73llT6PGkaF2s=";
   };
 
@@ -19,12 +20,15 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ ncurses ];
 
-  meta = with lib; {
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+
+  meta = {
     description = "Disk usage analyzer with an ncurses interface";
     homepage = "https://dev.yorhel.nl/ncdu";
-    license = licenses.mit;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ pSub ];
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ pSub ];
     mainProgram = "ncdu";
   };
-}
+})
