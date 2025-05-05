51737a103d10a242fccfabbfc95ec3c18b29b6d3
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-05 12:49:48 +0000

    aws-lc: 1.50.0 -> 1.50.1

--- pkgs/by-name/aw/aws-lc/package.nix
+++ pkgs/by-name/aw/aws-lc/package.nix
@@ -10,13 +10,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "aws-lc";
-  version = "1.50.0";
+  version = "1.50.1";
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = "aws-lc";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-aYill04Fs/mJFCmzP/tlwUcPRHJz6R9knQWKGEOMwfQ=";
+    hash = "sha256-iMNevsOF/6Of2MA2zd5+R9zRkitfhgXRWrKEa5ENSFE=";
   };
 
   outputs = [
