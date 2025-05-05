c34acf686fdce14a1f9bcb233e3f00da77e8bb85
r-vdp <ramses@well-founded.dev>
2025-05-05 11:45:54 +0200

    usb-modeswitch: fix udevadm verify issues

--- pkgs/by-name/us/usb-modeswitch/data.nix
+++ pkgs/by-name/us/usb-modeswitch/data.nix
@@ -20,9 +20,15 @@ stdenv.mkDerivation rec {
     "DESTDIR=$(out)"
   ];
 
-  prePatch = ''
-    sed -i 's@usb_modeswitch@${usb-modeswitch}/lib/udev/usb_modeswitch@g' 40-usb_modeswitch.rules
-  '';
+  postPatch =
+    # bash
+    ''
+      substituteInPlace 40-usb_modeswitch.rules \
+        --replace-fail "usb_modeswitch" "${usb-modeswitch}/lib/udev/usb_modeswitch"
+
+      # Fix issue reported by udevadm verify
+      sed -i 's/,,/,/g' 40-usb_modeswitch.rules
+    '';
 
   # we add tcl here so we can patch in support for new devices by dropping config into
   # the usb_modeswitch.d directory
