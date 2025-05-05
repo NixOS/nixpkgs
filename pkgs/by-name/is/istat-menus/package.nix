e4c83a9c7e0fab0f2e0de0133890070829820a22
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-05 17:10:58 +0300

    istat-menus: 7.02.10 -> 7.10.2

--- pkgs/by-name/is/istat-menus/package.nix
+++ pkgs/by-name/is/istat-menus/package.nix
@@ -10,11 +10,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "istat-menus";
-  version = "7.02.10";
+  version = "7.10.2";
 
   src = fetchurl {
     url = "https://cdn.istatmenus.app/files/istatmenus${lib.versions.major finalAttrs.version}/versions/iStatMenus${finalAttrs.version}.zip";
-    hash = "sha256-ckYIQsJ0QEsIpXRFo1xioSCOwEL06d0cJrATa1URMIQ=";
+    hash = "sha256-NDjIsCAxTcCo6APaWyuYmil8b0gzJEmSxFxqm2IQ5dU=";
   };
 
   sourceRoot = ".";
