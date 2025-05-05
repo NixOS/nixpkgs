961c05dcc1d9c21e3d7836bbd0533f6f3979f46d
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-05 13:02:05 +0000

    sentry-cli: 2.43.1 -> 2.44.0

--- pkgs/development/tools/sentry-cli/default.nix
+++ pkgs/development/tools/sentry-cli/default.nix
@@ -9,13 +9,13 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "sentry-cli";
-  version = "2.43.1";
+  version = "2.44.0";
 
   src = fetchFromGitHub {
     owner = "getsentry";
     repo = "sentry-cli";
     rev = version;
-    hash = "sha256-uGYL+xEXcf7+qe9NUvzFVjGGx33UpwjS7EHD/xVV+9Q=";
+    hash = "sha256-TzkIdX6a3xMkCj88GlygEEPSqbokDV9tOl7XZVtOQyM=";
   };
   doCheck = false;
 
@@ -29,7 +29,7 @@ rustPlatform.buildRustPackage rec {
   ];
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-RnFsV9m9ChmUW1PcxSNR5i6lwKBfqp9XXUNpezjCfeY=";
+  cargoHash = "sha256-6jV0mhHGEZGDgxpBueBSz8kqRVMG1e+skJFUn2NlvXY=";
 
   postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
     installShellCompletion --cmd sentry-cli \
