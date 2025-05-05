d308c24ca841a46eab3a13d2b10bdd4404f0e3ef
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-05-05 22:54:17 +0200

    harper: 0.32.1 -> 0.33.0
    Changelog: https://github.com/Automattic/harper/releases/tag/v0.33.0

--- pkgs/by-name/ha/harper/package.nix
+++ pkgs/by-name/ha/harper/package.nix
@@ -7,18 +7,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "harper";
-  version = "0.32.1";
+  version = "0.33.0";
 
   src = fetchFromGitHub {
     owner = "Automattic";
     repo = "harper";
     rev = "v${version}";
-    hash = "sha256-6W1NFZ5wRL3RxNYbgRH4qCSDNwOc3uQFeFujffusjGo=";
+    hash = "sha256-QLTDL+iczBeKxEnvdPeGsMQeLPocJVyDVYyRbgBa5Cs=";
   };
 
   buildAndTestSubdir = "harper-ls";
   useFetchCargoVendor = true;
-  cargoHash = "sha256-PKqLf7UrtQp8pLIJbFgZ53iW+So6Yq7cpETARsnMFvI=";
+  cargoHash = "sha256-k0xBDhW+1J1bqBic+3KLt1D84Ehv9KnVvvDIkx4AlS0=";
 
   passthru.updateScript = nix-update-script { };
 
