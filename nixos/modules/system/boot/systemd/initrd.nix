6950c2525d3c9b640e5a2148810066fd3b8c4e28
Gerg-L <GregLeyda@proton.me>
2025-05-05 19:07:58 -0400

    nixos/systemd-initrd: deprecate strip
    It only saved ~1MiB of initramfs size, but caused a few issues
    like unloadable kernel modules.

--- nixos/modules/system/boot/systemd/initrd.nix
+++ nixos/modules/system/boot/systemd/initrd.nix
@@ -132,13 +132,20 @@ let
   initialRamdisk = pkgs.makeInitrdNG {
     name = "initrd-${kernel-name}";
     inherit (config.boot.initrd) compressor compressorArgs prepend;
-    inherit (cfg) strip;
 
     contents = lib.filter ({ source, ... }: !lib.elem source cfg.suppressedStorePaths) cfg.storePaths;
   };
 
 in
 {
+  imports = [
+    (lib.mkRemovedOptionModule [ "boot" "initrd" "systemd" "strip" ] ''
+      The option to strip ELF files in initrd has been removed.
+      It only saved ~1MiB of initramfs size, but caused a few issues
+      like unloadable kernel modules.
+    '')
+  ];
+
   options.boot.initrd.systemd = {
     enable = mkEnableOption "systemd in initrd" // {
       description = ''
@@ -206,19 +213,6 @@ in
       '';
       type = utils.systemdUtils.types.initrdStorePath;
       default = [ ];
-    };
-
-    strip = mkOption {
-      description = ''
-        Whether to completely strip executables and libraries copied to the initramfs.
-
-        Setting this to false may save on the order of 30MiB on the
-        machine building the system (by avoiding a binutils
-        reference), at the cost of ~1MiB of initramfs size. This puts
-        this option firmly in the territory of micro-optimisation.
-      '';
-      type = types.bool;
-      default = true;
     };
 
     extraBin = mkOption {
