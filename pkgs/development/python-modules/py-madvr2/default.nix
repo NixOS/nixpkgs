8fe128cb0ce31049f5c8311c6fb5a2c025165c60
Robert Schütz <nix@dotlambda.de>
2025-05-06 06:16:51 -0700

    python313Packages.py-madvr2: 1.6.33 -> 1.7.1
    Diff: https://github.com/iloveicedgreentea/py-madvr/compare/refs/tags/v1.6.33...refs/tags/v1.7.1
    
    Changelog: https://github.com/iloveicedgreentea/py-madvr/releases/tag/v1.7.0

--- pkgs/development/python-modules/py-madvr2/default.nix
+++ pkgs/development/python-modules/py-madvr2/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "py-madvr2";
-  version = "1.6.33";
+  version = "1.7.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "iloveicedgreentea";
     repo = "py-madvr";
     tag = "v${version}";
-    hash = "sha256-z+PVLz9eApGJ94I/Jp0MyqNpKQwIemk8j+OyqFmIbgI=";
+    hash = "sha256-tZaQlu59s7AuVHnoyoFc6VGgc85q4r9TRMl6KxdPdrA=";
   };
 
   build-system = [ setuptools ];
@@ -28,8 +28,14 @@ buildPythonPackage rec {
     pytestCheckHook
   ];
 
+  disabledTests = [
+    # https://github.com/iloveicedgreentea/py-madvr/issues/16
+    "test_async_add_tasks"
+    "test_send_heartbeat"
+  ];
+
   meta = {
-    changelog = "https://github.com/iloveicedgreentea/py-madvr/releases/tag/v${version}";
+    changelog = "https://github.com/iloveicedgreentea/py-madvr/releases/tag/${src.tag}";
     description = "Control MadVR Envy over IP";
     homepage = "https://github.com/iloveicedgreentea/py-madvr";
     license = lib.licenses.mit;
