NixOS/nixpkgs

commit fb879e8e2e6e00aa4b0a4d8b38b0cac177fb6911

librewolf-unwrapped: 137.0.2-1 -> 138.0.1-2

pkgs/applications/networking/browsers/librewolf/librewolf.nix
@@ -15,8 +15,12 @@ rec {
 
   extraPostPatch = ''
     while read patch_name; do
-      echo "applying LibreWolf patch: $patch_name"
-      patch -p1 < ${source}/$patch_name
+      if [ "$patch_name" -neq "patches/macos-import-vector.patch"]; then
+        echo "applying LibreWolf patch: $patch_name"
+        patch -p1 < ${source}/$patch_name
+      else
+        echo "skipping LibreWolf patch: $patch"
+      fi
     done <${source}/assets/patches.txt
 
     cp -r ${source}/themes/browser .

commit c30ca434d06b5b032928fcf6b8d91051e376492b

librewolf: fix syntax error causing patches to not be applied
Broken in fb879e8e2e6e00aa4b0a4d8b38b0cac177fb6911.

pkgs/applications/networking/browsers/librewolf/librewolf.nix
@@ -15,7 +15,7 @@ rec {
 
   extraPostPatch = ''
     while read patch_name; do
-      if [ "$patch_name" -neq "patches/macos-import-vector.patch"]; then
+      if [ "$patch_name" != "patches/macos-import-vector.patch" ]; then
         echo "applying LibreWolf patch: $patch_name"
         patch -p1 < ${source}/$patch_name
       else