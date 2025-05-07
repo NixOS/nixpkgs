9a7fb9124b71b3603bad0ab29557964bc797e7dc
Philip Taron <philip.taron@gmail.com>
2025-05-07 16:46:11 -0700

    llm: add enable-llm-anthropic override

--- pkgs/by-name/ll/llm/package.nix
+++ pkgs/by-name/ll/llm/package.nix
@@ -1,3 +1,17 @@
-{ python3Packages }:
+{
+  lib,
+  python3Packages,
+  enable-llm-anthropic ? false,
+}:
 
-python3Packages.toPythonApplication python3Packages.llm
+let
+  inherit (python3Packages) toPythonApplication llm llm-anthropic;
+in
+
+toPythonApplication (
+  llm.overrideAttrs (finalAttrs: {
+    propagatedBuildInputs =
+      (finalAttrs.propagatedBuildInputs or [])
+      ++ lib.optionals enable-llm-anthropic [ llm-anthropic ];
+  })
+)
