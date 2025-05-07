46ad01d380305079f054718a52fad6e225f93e32
Guy Chronister <guychronister@outlook.com>
2025-05-07 10:43:25 -0500

    anystyle-cli: 1.3.1 -> 1.4.5

--- pkgs/tools/misc/anystyle-cli/default.nix
+++ pkgs/tools/misc/anystyle-cli/default.nix
@@ -1,47 +1,25 @@
 {
   lib,
-  buildRubyGem,
   bundlerEnv,
+  bundlerUpdateScript,
   ruby,
-  poppler-utils,
 }:
-let
-  deps = bundlerEnv rec {
-    name = "anystyle-cli-${version}";
-    source.sha256 = lib.fakeSha256;
-    version = "1.3.1";
-    inherit ruby;
-    gemdir = ./.;
-    gemset = lib.recursiveUpdate (import ./gemset.nix) {
-      anystyle.source = {
-        remotes = [ "https://rubygems.org" ];
-        sha256 = "1w79zcia60nnnyrmyvpd10pmxrpk5c7lj9gmmblhwi8x5mfq9k0n";
-        type = "gem";
-      };
-    };
-  };
-in
-buildRubyGem rec {
+
+bundlerEnv {
+  pname = "anystyle-cli";
+  version = "1.4.5";
+
   inherit ruby;
-  gemName = "anystyle-cli";
-  pname = gemName;
-  version = "1.3.1";
-  source.sha256 = "1a3ifwxwqkp5dnfk9r8qq8kgfb8k1pl7jjdghbb8ixbxz9ac7awy";
+  gemdir = ./.;
 
-  propagatedBuildInputs = [ deps ];
+  passthru.updateScript = bundlerUpdateScript "anystyle-cli";
 
-  preFixup = ''
-    wrapProgram $out/bin/anystyle --prefix PATH : ${poppler-utils}/bin
-  '';
-
-  meta = with lib; {
+  meta = {
     description = "Command line interface to the AnyStyle Parser and Finder";
     homepage = "https://anystyle.io/";
-    license = licenses.bsd2;
-    maintainers = with maintainers; [ shamilton ];
-    mainProgram = "anystyle";
-    platforms = platforms.unix;
-    # error: passing argument 2 of 'rb_hash_foreach' from incompatible pointer type [-Wincompatible-pointer-types]
-    broken = true;
+    license = [ lib.licenses.bsd2 ];
+    maintainers = [ lib.maintainers.shamilton ];
+    mainProgram = "anystyle-cli";
+    platforms = [ lib.platforms.unix ];
   };
 }
