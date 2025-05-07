b848e06a6e275b65ac00b5d60af3ecb34e56fbb9
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-05-06 22:07:12 -0400

    python312Packages.dbt-common: relax protobuf dependency
    Upstream requires protobuf<6 but we would have to downgrade protobuf for
    half the python world to satisfy this requirement.
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- pkgs/development/python-modules/dbt-common/default.nix
+++ pkgs/development/python-modules/dbt-common/default.nix
@@ -46,6 +46,7 @@ buildPythonPackage rec {
     # 0.6.x -> 0.7.2 doesn't seem too risky at a glance
     # https://pypi.org/project/isodate/0.7.2/
     "isodate"
+    "protobuf"
   ];
 
   dependencies = [
