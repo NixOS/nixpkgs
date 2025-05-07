2b9b4d5f953d1f7581e3ab1ffe9a7975d7594523
kilianar <mail@kilianar.de>
2025-05-07 09:22:08 +0200

    portfolio: 0.76.0 -> 0.76.1
    https://github.com/portfolio-performance/portfolio/releases/tag/0.76.1

--- pkgs/by-name/po/portfolio/package.nix
+++ pkgs/by-name/po/portfolio/package.nix
@@ -34,11 +34,11 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "PortfolioPerformance";
-  version = "0.76.0";
+  version = "0.76.1";
 
   src = fetchurl {
     url = "https://github.com/buchen/portfolio/releases/download/${finalAttrs.version}/PortfolioPerformance-${finalAttrs.version}-linux.gtk.x86_64.tar.gz";
-    hash = "sha256-F3U6CU6hZktN2Lq9pPRAw7OSLubEz6ddTAym5azRj1c=";
+    hash = "sha256-e4ZdygS7x5cD9SD9+n7fBWAfAPiJLwQXlApTR8OkXeI=";
   };
 
   nativeBuildInputs = [
