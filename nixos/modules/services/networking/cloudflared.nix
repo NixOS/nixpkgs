91e8299e15892f4a64fc0f3c13aff07419c976c4
= <=>
2025-05-07 13:52:24 +0900

    nixos/cloudflared-service: add assertions and tokenFile

--- nixos/modules/services/networking/cloudflared.nix
+++ nixos/modules/services/networking/cloudflared.nix
@@ -203,7 +203,8 @@ in
               inherit certificateFile originRequest;
 
               credentialsFile = lib.mkOption {
-                type = lib.types.path;
+                type = with lib.types; nullOr path;
+                default = null;
                 description = ''
                   Credential file.
 
@@ -223,6 +224,20 @@ in
                   rules configured via the Cloudflare dashboard.
                 '';
                 example = "eyJhIj...zUzZg==";
+              };
+
+              tokenFile = lib.mkOption {
+                type = with lib.types; nullOr path;
+                default = null;
+                description = ''
+                  Cloudflare tunnel tokenFile to use instead of a configuration file.
+                  If set, the tunnel will be run using `cloudflared tunnel run --token-file <tokenFile>`.
+                  This option is mutually exclusive with the config file-based approach
+                  (which requires `credentialsFile`).
+                  Note: Using a tokenFile bypasses locally defined ingress rules and uses
+                  rules configured via the Cloudflare dashboard.
+                '';
+                example = "/path/to/my/token_file";
               };
 
               warp-routing = {
@@ -245,6 +260,7 @@ in
                   See `service`.
                 '';
                 example = "http_status:404";
+                default = "http_status:404";
               };
 
               ingress = lib.mkOption {
@@ -322,6 +338,24 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    warnings = lib.mapAttrsToList (
+      name: tunnel:
+      if tunnel.token != null then
+        "[cloudflared-service | ${name}] Tunnel is using raw token value, which is world-readable!"
+      else
+        ""
+    ) cfg.tunnels;
+
+    assertions = lib.mapAttrsToList (name: tunnel: {
+      assertion =
+        (lib.count (x: !builtins.isNull x) [
+          tunnel.token
+          tunnel.tokenFile
+          tunnel.credentialsFile
+        ]) <= 1;
+      message = "[cloudflared-service | ${name}] You must specify AT MOST ONE of 'token', 'tokenFile', or 'credentialsFile'.";
+    }) cfg.tunnels;
+
     systemd.targets = lib.mapAttrs' (
       name: tunnel:
       lib.nameValuePair "cloudflared-tunnel-${name}" {
@@ -335,8 +369,6 @@ in
     systemd.services = lib.mapAttrs' (
       name: tunnel:
       let
-        isTokenBased = tunnel.token != null;
-
         filterConfig = lib.attrsets.filterAttrsRecursive (
           _: v:
           !builtins.elem v [
@@ -352,7 +384,7 @@ in
         ingressesSet = filterIngressSet tunnel.ingress;
         ingressesStr = filterIngressStr tunnel.ingress;
 
-        fullConfig = lib.mkIf (!isTokenBased) (filterConfig {
+        fullConfig = filterConfig {
           tunnel = name;
           credentials-file = "/run/credentials/cloudflared-tunnel-${name}.service/credentials.json";
           warp-routing = filterConfig tunnel.warp-routing;
@@ -370,7 +402,7 @@ in
               service = lib.getAttr key ingressesStr;
             }) (lib.attrNames ingressesStr))
             ++ [ { service = tunnel.default; } ];
-        });
+        };
 
         mkConfigFile = pkgs.writeText "cloudflared.yml" (builtins.toJSON fullConfig);
         certFile = if (tunnel.certificateFile != null) then tunnel.certificateFile else cfg.certificateFile;
@@ -388,16 +420,15 @@ in
         serviceConfig = {
           RuntimeDirectory = "cloudflared-tunnel-${name}";
           RuntimeDirectoryMode = "0400";
-          LoadCredential = lib.mkIf (!isTokenBased) (
-            [
-              "credentials.json:${tunnel.credentialsFile}"
-            ]
-            ++ (lib.optional (certFile != null) "cert.pem:${certFile}")
-          );
+          LoadCredential = [
+            (lib.optional (tunnel.credentialsFile != null) ("credentials.json:${tunnel.credentialsFile}"))
+          ] ++ (lib.optional (certFile != null) "cert.pem:${certFile}");
 
           ExecStart =
-            if isTokenBased then
+            if tunnel.token != null then
               "${cfg.package}/bin/cloudflared tunnel run --token ${lib.escapeShellArg tunnel.token}"
+            else if tunnel.tokenFile != null then
+              "${cfg.package}/bin/cloudflared tunnel run --token-file ${lib.escapeShellArg tunnel.token}"
             else
               "${cfg.package}/bin/cloudflared tunnel --config=${mkConfigFile} --no-autoupdate run";
           Restart = "on-failure";
