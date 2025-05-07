0e777d21f44b17576c14a400130acafbfb5b22a5
Johannes Fleck <jfleck@posteo.de>
2025-05-07 16:08:38 +0200

    python3Packages.yoctopuce: init at 2.1.6320

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20267,6 +20267,8 @@ self: super: with self; {
 
   yfinance = callPackage ../development/python-modules/yfinance { };
 
+  yoctopuce = callPackage ../development/python-modules/yoctopuce { };
+
   yoda = toPythonModule (pkgs.yoda.override { python3 = python; });
 
   yolink-api = callPackage ../development/python-modules/yolink-api { };
