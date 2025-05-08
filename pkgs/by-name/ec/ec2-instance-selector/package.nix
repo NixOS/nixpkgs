bde237e5ee5b4aebfaa79225106b70863f5571d3
Willi Carlsen <carlsenwilli@gmail.com>
2025-05-08 20:13:24 +0200

    ec2-instance-selector: init at 3.1.1
    Signed-off-by: Willi Carlsen <carlsenwilli@gmail.com>

---
+++ pkgs/by-name/ec/ec2-instance-selector/package.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "ec2-instance-selector";
+  version = "3.1.1";
+
+  src = fetchFromGitHub {
+    owner = "aws";
+    repo = "amazon-ec2-instance-selector";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-4J66/LiFFeUW20du2clqjz9ozLV+Sn2VVqF9VISXpb0=";
+  };
+
+  vendorHash = "sha256-ocysHrbkmFQ96dEVJvc5YuuBiaXToAcMUUPFiLpMCpU=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.versionID=${finalAttrs.version}"
+    "-X=github.com/aws/amazon-ec2-instance-selector/v3/pkg/selector.versionID=${finalAttrs.version}"
+  ];
+
+  postInstall = ''
+    mv $out/bin/cmd $out/bin/ec2-instance-selector
+  '';
+
+  meta = {
+    description = "Recommends instance types based on resource criteria like vcpus and memory";
+    homepage = "https://github.com/aws/amazon-ec2-instance-selector";
+    changelog = "https://github.com/aws/amazon-ec2-instance-selector/tags/v${finalAttrs.version}";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ wcarlsen ];
+    mainProgram = "ec2-instance-selector";
+  };
+})
