dbc67632b917d2f3a4ec9d37192afb10352a53e2
SchweGELBin <abramjannikmichael06@gmail.com>
2025-05-08 14:15:36 +0200

    catspeak: optimize evaluation
    Co-authored-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/by-name/ca/catspeak/package.nix
+++ pkgs/by-name/ca/catspeak/package.nix
@@ -4,24 +4,24 @@
   rustPlatform,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "catspeak";
   version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "SchweGELBin";
     repo = "catspeak";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-2NYYnhIsfE889fvbTimPVTy6dCR+i/1WaGT4Dhfuyno=";
   };
 
   cargoHash = "sha256-8PATds7OaD7/8FQ6XDKrCMdTcpZwi453UYq9dX/sSBk=";
 
-  meta = with lib; {
+  meta = {
     description = "A cowsay like program of a speaking cat, written in rust.";
     homepage = "https://github.com/SchweGELBin/catspeak";
-    changelog = "https://github.com/SchweGELBin/catspeak/blob/main/CHANGELOG.md";
-    license = licenses.mit;
-    maintainers = with maintainers; [ SchweGELBin ];
+    changelog = "https://github.com/SchweGELBin/catspeak/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ SchweGELBin ];
   };
-}
+})
