b8a7b821f7b011381126ebc84a1abc834041263d
Philip Taron <philip.taron@gmail.com>
2025-05-07 17:00:40 -0700

    llm: add enable-llm-cmd override

--- pkgs/by-name/ll/llm/package.nix
+++ pkgs/by-name/ll/llm/package.nix
@@ -2,16 +2,23 @@
   lib,
   python3Packages,
   enable-llm-anthropic ? false,
+  enable-llm-cmd ? false,
 }:
 
 let
-  inherit (python3Packages) toPythonApplication llm llm-anthropic;
+  inherit (python3Packages)
+    toPythonApplication
+    llm
+    llm-anthropic
+    llm-cmd
+    ;
 in
 
 toPythonApplication (
   llm.overrideAttrs (finalAttrs: {
     propagatedBuildInputs =
-      (finalAttrs.propagatedBuildInputs or [])
-      ++ lib.optionals enable-llm-anthropic [ llm-anthropic ];
+      (finalAttrs.propagatedBuildInputs or [ ])
+      ++ lib.optionals enable-llm-anthropic [ llm-anthropic ]
+      ++ lib.optionals enable-llm-cmd [ llm-cmd ];
   })
 )
