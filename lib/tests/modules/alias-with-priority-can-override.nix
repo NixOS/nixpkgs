0ad514ea8b218953a2c48ba83d90a197f1d8265c
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-08 17:55:35 +0100

    lib/tests/modules: remove unnecessary `warning` options from alias tests
    `mkRenamedOptionModule` can set the `warnings` option, if it is
    declared. `mkAliasOptionModule` will never define any warnings.
    
    In either case, it is unnecessary to declare the option if we aren't
    _reading_ any warnings in the test.

--- lib/tests/modules/alias-with-priority-can-override.nix
+++ lib/tests/modules/alias-with-priority-can-override.nix
@@ -26,18 +26,6 @@ in
         Some descriptive text
       '';
     };
-
-    # mkAliasOptionModule sets warnings, so this has to be defined.
-    warnings = mkOption {
-      internal = true;
-      default = [ ];
-      type = types.listOf types.str;
-      example = [ "The `foo' service is deprecated and will go away soon!" ];
-      description = ''
-        This option allows modules to show warnings to users during
-        the evaluation of the system configuration.
-      '';
-    };
   };
 
   imports = [
