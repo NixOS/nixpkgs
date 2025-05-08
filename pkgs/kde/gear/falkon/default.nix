511960a1736b95bc5183da5dbdb07b601d1fcae4
K900 <me@0upti.me>
2025-05-08 15:06:07 +0300

    kdePackages: Gear 25.04.0 -> 25.04.1

--- pkgs/kde/gear/falkon/default.nix
+++ pkgs/kde/gear/falkon/default.nix
@@ -5,19 +5,9 @@
   qtwebengine,
   qttools,
   python3Packages,
-  fetchpatch,
 }:
 mkKdeDerivation {
   pname = "falkon";
-
-  # Fix crash on startup
-  # FIXME: remove in 25.04.1
-  patches = [
-    (fetchpatch {
-      url = "https://invent.kde.org/network/falkon/-/commit/31ba9472369256804400a2db36b3dca3b4be2d73.patch";
-      hash = "sha256-jLJjL4Bp03aZfM/OPXZzgL56T0C/2hHSzNERpbTitzw=";
-    })
-  ];
 
   extraNativeBuildInputs = [
     qttools
