684380187e30d3791789f48b9077bd839187dc04
Cory Schwartz <cory.is.evil@gmail.com>
2025-05-08 10:58:54 -0700

    space2meta: refactor to use finalAttrs; move to pkgs/by-name

--- pkgs/by-name/sp/space2meta/package.nix
+++ pkgs/by-name/sp/space2meta/package.nix
@@ -5,15 +5,15 @@
   cmake,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation( finalAttrs: {
   pname = "space2meta";
   version = "0.2.0";
 
   src = fetchFromGitLab {
     group = "interception";
     owner = "linux/plugins";
-    repo = pname;
-    rev = "v${version}";
+    repo = "space2meta";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-MvpIe230I0TNzTO6ol1+DrpcFgqw0gF9Z22WMQLujb4=";
   };
 
@@ -21,10 +21,10 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     homepage = "https://gitlab.com/interception/linux/plugins/space2meta";
-    description = "Turn your space key into the meta key (a.k.a. win key, OS key, super key) when chorded to another key (on key release only).";
+    description = "space2meta is a plugin for interception-tools to convert space key to meta key";
     mainProgram = "space2meta";
     license = licenses.mit;
     maintainers = [ maintainers.vyp ];
     platforms = platforms.linux;
   };
-}
+})
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2702,7 +2702,7 @@ with pkgs;
     dual-function-keys =
       callPackage ../tools/inputmethods/interception-tools/dual-function-keys.nix
         { };
-    space2meta = callPackage ../tools/inputmethods/interception-tools/space2meta.nix { };
+    space2meta = callPackage ../by-name/sp/space2meta/package.nix { };
   };
 
   blacken-docs = with python3Packages; toPythonApplication blacken-docs;
