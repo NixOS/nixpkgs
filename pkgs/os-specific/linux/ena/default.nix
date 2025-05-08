3c7d9837aaa20ab28b134d7569aef3f900f84ff3
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-08 22:59:52 +0000

    linuxPackages.ena: 2.13.3 -> 2.14.0

--- pkgs/os-specific/linux/ena/default.nix
+++ pkgs/os-specific/linux/ena/default.nix
@@ -8,7 +8,7 @@
 }:
 let
   rev-prefix = "ena_linux_";
-  version = "2.13.3";
+  version = "2.14.0";
 in
 stdenv.mkDerivation {
   inherit version;
@@ -18,7 +18,7 @@ stdenv.mkDerivation {
     owner = "amzn";
     repo = "amzn-drivers";
     rev = "${rev-prefix}${version}";
-    hash = "sha256-rxogTQ2zNyzuOY5P87NGZQHVJPvv1T+g5nwh6SLLf50=";
+    hash = "sha256-1XL1X7Sv3w3zBe7rJyPiUXtREBWg6LRPs2T0CQ5YpdE=";
   };
 
   hardeningDisable = [ "pic" ];
