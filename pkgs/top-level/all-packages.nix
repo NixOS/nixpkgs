e3674fd92e26077a32b8f07cea1e6f6ebfcbe69a
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-05-08 16:54:47 +0200

    sbomnix: use top-level callPackage
    and migrate to PEP517 python lingo.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7647,7 +7647,7 @@ with pkgs;
     buildDocs = false;
   };
 
-  sbomnix = python3.pkgs.callPackage ../tools/security/sbomnix { };
+  sbomnix = callPackage ../tools/security/sbomnix { };
 
   seer = libsForQt5.callPackage ../development/tools/misc/seer { };
 
