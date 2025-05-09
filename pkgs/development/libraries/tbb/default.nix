af38c34b4650a9c77fbc190cb5f6a81a18f839ea
silvanshade <silvanshade@users.noreply.github.com>
2025-05-09 12:10:11 -0600

    tbb_2021_11_0: fix static builds
    Fixes: https://github.com/NixOS/nix/issues/13160
    
    This is a backport of https://github.com/uxlfoundation/oneTBB/pull/1248

--- pkgs/development/libraries/tbb/default.nix
+++ pkgs/development/libraries/tbb/default.nix
@@ -50,6 +50,9 @@ stdenv.mkDerivation rec {
       url = "https://patch-diff.githubusercontent.com/raw/uxlfoundation/oneTBB/pull/1696.patch";
       hash = "sha256-yjX2FkOK8bz29a/XSA7qXgQw9lxzx8VIgEBREW32NN4=";
     })
+    # Fix Threads::Threads target for static from https://github.com/oneapi-src/oneTBB/pull/1248
+    # This is a conflict-resolved cherry-pick of the above PR to due to formatting differences.
+    ./patches/fix-cmake-threads-threads-target-for-static.patch
   ];
 
   # Fix build with modern gcc
