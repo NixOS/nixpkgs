a1f5d9437a0286dd561dfa96af5f68f8eed9e09f
Jade Lynn Masker <donottellmetonottellyou@gmail.com>
2025-05-09 17:12:02 -0400

    legendsviewer-next: init at 1.2.0

---
+++ pkgs/by-name/le/legendsviewer-next/package.nix
@@ -0,0 +1,105 @@
+{
+  lib,
+  buildDotnetModule,
+  buildNpmPackage,
+  dotnetCorePackages,
+  fetchFromGitHub,
+  makeDesktopItem,
+  copyDesktopItems,
+  nix-update-script,
+  nodejs_22,
+  writeShellScriptBin,
+
+  legendsviewer-next,
+}:
+let
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.aspnetcore_8_0;
+  frontend = import ./frontend.nix {
+    nodejs = nodejs_22;
+    inherit buildNpmPackage legendsviewer-next;
+  };
+in
+buildDotnetModule rec {
+  pname = "legendsviewer-next";
+  version = "1.2.0";
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = pname;
+      desktopName = "Legends Viewer";
+      genericName = "DF Legends Export Browser";
+      comment = meta.description;
+      icon = "${frontend}/dist/ceretelina.png";
+      tryExec = "LegendsViewer";
+      exec = "LegendsViewer";
+
+      # Until upstream supports multiple launches, we need a terminal to keep
+      # track of if it's already launched.
+      terminal = true;
+
+      categories = [
+        "Game"
+        "RolePlaying"
+        "Simulation"
+      ];
+      keywords = [
+        "df"
+        "dwarf"
+        "fortress"
+        "legends"
+        "viewer"
+      ];
+    })
+  ];
+
+  src = fetchFromGitHub {
+    owner = "Kromtec";
+    repo = "LegendsViewer-Next";
+    tag = "v${version}";
+    hash = "sha256-Fi4KARGsgDmplH/oG9OIxY2XqhHNYgUB6OxwtoTaCt4=";
+  };
+
+  nugetDeps = ./deps.json;
+  projectFile = "./LegendsViewer.Backend/LegendsViewer.Backend.csproj";
+  testProjectFile = "./LegendsViewer.Backend.Tests/LegendsViewer.Backend.Tests.csproj";
+  executables = [ meta.mainProgram ];
+  inherit dotnet-sdk dotnet-runtime;
+
+  nativeBuildInputs = [
+    # This fixes a build failure, we don't care about the frontend node build here
+    (writeShellScriptBin "npm" "")
+
+    copyDesktopItems
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    lib=$out/lib/${pname}
+
+    mkdir -p $lib
+
+    cp ./LegendsViewer.Backend/bin/Release/net8.0/*/* $lib
+    ln -s ${frontend} $lib/${frontend.pname}
+
+    runHook postInstall
+  '';
+
+  passthru = {
+    updateScript = nix-update-script {
+      extraArgs = [ "--subpackage frontend" ];
+    };
+  };
+
+  meta = {
+    description = "Recreates Dwarf Fortress' Legends Mode from exported files";
+    homepage = "https://github.com/${src.owner}/${src.repo}";
+    license = lib.licenses.mit;
+
+    mainProgram = "LegendsViewer";
+    platforms = with lib.platforms; lib.intersectLists (darwin ++ linux) (x86_64 ++ aarch64);
+
+    maintainers = with lib.maintainers; [ donottellmetonottellyou ];
+  };
+}
