3a18d86937ea137a350936e143c185eca25bdab2
Saterfield990 <traysaterfield@outlook.com>
2025-05-09 17:14:44 +0300

    creduce: 2.10.0 -> 2.10.0-unstable-2024-06-01

--- pkgs/by-name/cr/creduce/package.nix
+++ pkgs/by-name/cr/creduce/package.nix
@@ -1,62 +1,56 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
   fetchpatch,
   cmake,
-  makeWrapper,
+  makeBinaryWrapper,
   llvm,
   libclang,
   flex,
   zlib,
   perlPackages,
   util-linux,
+  versionCheckHook,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "creduce";
-  version = "2.10.0";
+  version = "2.10.0-unstable-2024-06-01";
 
-  src = fetchurl {
-    url = "https://embed.cs.utah.edu/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "2xwPEjln8k1iCwQM69UwAb89zwPkAPeFVqL/LhH+oGM=";
+  src = fetchFromGitHub {
+    owner = "csmith-project";
+    repo = "creduce";
+    rev = "31e855e290970cba0286e5032971509c0e7c0a80";
+    hash = "sha256-RbxFqZegsCxnUaIIA5OfTzx1wflCPeF+enQt90VwMgA=";
   };
 
+  #LLVM 19 support
   patches = [
-    # Port to LLVM 15
     (fetchpatch {
-      url = "https://github.com/csmith-project/creduce/commit/e507cca4ccb32585c5692d49b8d907c1051c826c.patch";
-      hash = "sha256-jO5E85AvHcjlErbUhzuQDXwQkhQsXklcTMQfWBd09OU=";
-    })
-    (fetchpatch {
-      url = "https://github.com/csmith-project/creduce/commit/8d56bee3e1d2577fc8afd2ecc03b1323d6873404.patch";
-      hash = "sha256-dRaBaJAYkvMyxKvfriOcg4D+4i6+6orZ85zws1AFx/s=";
-    })
-    # Port to LLVM 16
-    (fetchpatch {
-      url = "https://github.com/csmith-project/creduce/commit/8ab9a69caf13ce24172737e8bfd09de51a1ecb6a.patch";
-      hash = "sha256-gPNXxYHnsyUvXmC0CGtsulH2Fu/EMnDE4GdOYc0UbiQ=";
+      url = "https://patch-diff.githubusercontent.com/raw/csmith-project/creduce/pull/285.patch";
+      hash = "sha256-uEkPOeRyrojjH+BCSr/jkOkJazg9zZU/BLn+RTIzHjs=";
     })
   ];
 
-  postPatch =
-    ''
-      substituteInPlace CMakeLists.txt \
-        --replace "-std=c++11" "-std=c++17"
-    ''
-    # On Linux, c-reduce's preferred way to reason about
-    # the cpu architecture/topology is to use 'lscpu',
-    # so let's make sure it knows where to find it:
-    + lib.optionalString stdenv.hostPlatform.isLinux ''
-      substituteInPlace creduce/creduce_utils.pm --replace \
-        lscpu ${util-linux}/bin/lscpu
-    '';
+  postUnpack = ''
+    substituteInPlace source/CMakeLists.txt --replace-fail "2.11.0" "${finalAttrs.version}"
+  '';
+
+  # On Linux, c-reduce's preferred way to reason about
+  # the cpu architecture/topology is to use 'lscpu',
+  # so let's make sure it knows where to find it:
+  postPatch = lib.optionalString stdenv.hostPlatform.isLinux ''
+    substituteInPlace creduce/creduce_utils.pm --replace-fail "lscpu" "${lib.getExe' util-linux "lscpu"}"
+  '';
 
   nativeBuildInputs = [
     cmake
-    makeWrapper
+    makeBinaryWrapper
     llvm.dev
   ];
+
   buildInputs =
     [
       # Ensure stdenv's CC is on PATH before clang-unwrapped
@@ -80,12 +74,17 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/creduce --prefix PERL5LIB : "$PERL5LIB"
   '';
 
-  meta = with lib; {
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+
+  meta = {
     description = "C program reducer";
-    mainProgram = "creduce";
     homepage = "https://embed.cs.utah.edu/creduce";
     # Officially, the license is: https://github.com/csmith-project/creduce/blob/master/COPYING
-    license = licenses.ncsa;
+    license = lib.licenses.ncsa;
     longDescription = ''
       C-Reduce is a tool that takes a large C or C++ program that has a
       property of interest (such as triggering a compiler bug) and
@@ -93,7 +92,8 @@ stdenv.mkDerivation rec {
       property.  It is intended for use by people who discover and report
       bugs in compilers and other tools that process C/C++ code.
     '';
+    mainProgram = "creduce";
     maintainers = [ ];
-    platforms = platforms.all;
+    platforms = lib.platforms.all;
   };
-}
+})
