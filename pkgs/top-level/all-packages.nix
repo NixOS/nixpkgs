d3c39128dc984e276a03dc805515f55791e2db91
Bjørn Forsman <bjorn.forsman@gmail.com>
2025-05-09 17:16:58 +0200

    qmmp: move to pkgs/by-name
    Do the necessary adaptation to be compatible with pkgs/by-name, by
    taking top-level qt6 as input instead of the individual qt modules
    (qtbase, ...), which were previously explicitly taken from qt6Packages.
    
    No rebuild.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13818,8 +13818,6 @@ with pkgs;
 
   qmidinet = libsForQt5.callPackage ../applications/audio/qmidinet { };
 
-  qmmp = qt6Packages.callPackage ../applications/audio/qmmp { };
-
   qnotero = libsForQt5.callPackage ../applications/office/qnotero { };
 
   qpwgraph = qt6Packages.callPackage ../applications/audio/qpwgraph { };
