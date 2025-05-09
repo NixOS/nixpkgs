1a37c3d6fa40fb8f0c57b7aeba108c309c099b97
Martin Joerg <martin.joerg@gmail.com>
2025-05-09 08:59:03 +0000

    pdf4qt: 1.5.0.0 -> 1.5.1.0
    https://github.com/JakubMelka/PDF4QT/releases/tag/v1.5.1.0
    https://github.com/JakubMelka/PDF4QT/compare/refs/tags/v1.5.0.0...refs/tags/v1.5.1.0

--- pkgs/by-name/pd/pdf4qt/package.nix
+++ pkgs/by-name/pd/pdf4qt/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pdf4qt";
-  version = "1.5.0.0";
+  version = "1.5.1.0";
 
   src = fetchFromGitHub {
     owner = "JakubMelka";
     repo = "PDF4QT";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-ELdmnOEKFGCtuf240R/0M6r8aPwRQiXurAxrqcCZvOI=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Ysrz/uCSTFK5wGNdTXhpq6QVf7Ju1xWisNVUtBtdEjc=";
   };
 
   patches = [
@@ -29,19 +29,6 @@ stdenv.mkDerivation (finalAttrs: {
     # header files instead.
     ./find_lcms2_path.patch
   ];
-
-  # make calls to QString::arg compatible with Qt 6.9
-  # see https://doc-snapshots.qt.io/qt6-6.9/whatsnew69.html#new-features-in-qt-6-9
-  postPatch = ''
-    substituteInPlace Pdf4QtLibCore/sources/pdf{documentsanitizer,optimizer}.cpp \
-      --replace-fail \
-        '.arg(counter)' \
-        '.arg<PDFInteger>(counter)'
-    substituteInPlace Pdf4QtLibCore/sources/pdfoptimizer.cpp \
-      --replace-fail \
-        '.arg(bytesSaved)' \
-        '.arg<PDFInteger>(bytesSaved)'
-  '';
 
   nativeBuildInputs = [
     cmake
@@ -83,8 +70,9 @@ stdenv.mkDerivation (finalAttrs: {
       functionality based on PDF Reference 2.0.
     '';
     homepage = "https://jakubmelka.github.io";
-    license = lib.licenses.lgpl3Only;
-    mainProgram = "Pdf4QtViewerLite";
+    changelog = "https://github.com/JakubMelka/PDF4QT/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    mainProgram = "Pdf4QtViewer";
     maintainers = with lib.maintainers; [ aleksana ];
     platforms = lib.platforms.linux;
   };
