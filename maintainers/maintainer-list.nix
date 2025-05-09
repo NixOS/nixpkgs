5c45af7c479b780de84be1d5384633137a62365c
goodylove <goodyc474@gmail.com>
2025-05-09 15:59:03 +0100

    maintainers: add goodylove

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9103,6 +9103,12 @@
     githubId = 1621335;
     name = "Andrew Trachenko";
   };
+  goodylove = {
+    github = "goodylove";
+    email = "goodyc474@gmail.com";
+    githubId = 104577296;
+    name = "Nwachukwu Goodness";
+  };
   gordon-bp = {
     email = "gordy@hanakano.com";
     github = "Gordon-BP";
