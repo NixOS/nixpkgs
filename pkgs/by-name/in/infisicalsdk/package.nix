2f9d8bf3ce6bb99c5646150c587be729ff183463
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-05-10 18:38:41 +0300

    infisicalsdk: init at 1.0.8
    Co-authored-by: emaryn <197520219+emaryn@users.noreply.github.com>
    
    Update pkgs/by-name/in/infisicalsdk/package.nix
    
    Co-authored-by: emaryn <197520219+emaryn@users.noreply.github.com>

---
+++ pkgs/by-name/in/infisicalsdk/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "infisicalsdk";
+  version = "1.0.8";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Infisical";
+    repo = "python-sdk-official";
+    tag = "v${version}";
+    hash = "sha256-C8pHypKVPISDGGUlC2LXP4MPoae/ZS7Mb92yN7+VoPM=";
+  };
+
+  build-system = [ python3Packages.setuptools ];
+
+  dependencies = with python3Packages; [
+    python-dateutil
+    aenum
+    requests
+    boto3
+    botocore
+  ];
+
+  meta = {
+    homepage = "https://github.com/Infisical/python-sdk-official";
+    description = "Infisical Python SDK";
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ artur-sannikov ];
+  };
+}
