d8e3bfc261c50e8fc77f04fe5f1b6951178ef1dc
Niklas Korz <niklas@niklaskorz.de>
2025-05-10 11:56:05 +0200

    shader-slang: 2025.6.1 -> 2025.8.1
    Changelogs:
    
    - https://github.com/shader-slang/slang/releases/tag/v2025.8.1
    - https://github.com/shader-slang/slang/releases/tag/v2025.8
    - https://github.com/shader-slang/slang/releases/tag/v2025.7.1
    - https://github.com/shader-slang/slang/releases/tag/v2025.7
    - https://github.com/shader-slang/slang/releases/tag/v2025.6.4
    - https://github.com/shader-slang/slang/releases/tag/v2025.6.3
    - https://github.com/shader-slang/slang/releases/tag/v2025.6.2
    
    (cherry picked from commit 04727324c2902f0e7403a3e386b6aabd5c279b95)

--- pkgs/by-name/sh/shader-slang/package.nix
+++ pkgs/by-name/sh/shader-slang/package.nix
@@ -27,13 +27,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "shader-slang";
-  version = "2025.6.1";
+  version = "2025.8.1";
 
   src = fetchFromGitHub {
     owner = "shader-slang";
     repo = "slang";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-yNPAJX7OxxQLXDm3s7Hx5QA9fxy1qbAMp4LKYVqxMVM=";
+    hash = "sha256-PScbMkJJ4rh8I8ID709GKtLmd5+4Z2x2BlFEdWpoesI=";
     fetchSubmodules = true;
   };
 
