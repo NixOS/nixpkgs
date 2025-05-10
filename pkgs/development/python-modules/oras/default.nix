f2f17616acc64c49973d93ec7fe8296d6cc4bf2e
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-10 00:20:26 +0000

    python3Packages.oras: 0.2.29 -> 0.2.31

--- pkgs/development/python-modules/oras/default.nix
+++ pkgs/development/python-modules/oras/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "oras";
-  version = "0.2.29";
+  version = "0.2.31";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "oras-project";
     repo = "oras-py";
     tag = version;
-    hash = "sha256-+31DTtUie+Ve5H3jx/8AFzJ5YHPQzOKN3+fq2ujtj28=";
+    hash = "sha256-8dew0GtVYFHwm/M4yIkJqqVaC1PURn5Pn3fPf7vWQzk=";
   };
 
   build-system = [ setuptools ];
