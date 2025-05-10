d649cba1a3183dab1f386b91dac18db6ebf2d36a
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-05-11 01:52:00 +0200

    llhttp: 9.2.1 -> 9.3.0

--- pkgs/by-name/op/opendht/package.nix
+++ pkgs/by-name/op/opendht/package.nix
@@ -15,6 +15,7 @@
   llhttp,
   openssl,
   fmt,
+  fetchpatch2,
   enableProxyServerAndClient ? false,
   enablePushNotifications ? false,
 }:
@@ -29,6 +30,17 @@ stdenv.mkDerivation {
     rev = "5237f0a3b3eb8965f294de706ad73596569ae1dd";
     hash = "sha256-qErVKyZQR/asJ8qr0sRDaXZ8jUV7RaSLnJka5baWa7Q=";
   };
+
+  patches = [
+    (fetchpatch2 {
+      url = "https://github.com/savoirfairelinux/opendht/commit/2c26030108ede63e74efcc2d9c23ad47fdc8372e.patch?full_index=1";
+      hash = "sha256-kWXQZ2NS9xUUPKQVaJn4DyOdhtwnNCNcYtvzex/60dk=";
+    })
+    (fetchpatch2 {
+      url = "https://github.com/savoirfairelinux/opendht/commit/2bc46e9657c94adcce2479807a0a04c2f783bd4e.patch?full_index=1";
+      hash = "sha256-Zb1JrggIT4ghK+h0QZmLfLHZwbp5LFW68gwhRM3J1Ko=";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
