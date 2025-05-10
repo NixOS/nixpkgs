46c91f1737fa5bd333cb845cda747a968ac087e2
kpbaks <kristoffer.pbs@tuta.io>
2025-05-10 18:08:15 +0200

    tuistash: init at 0.7.1

---
+++ pkgs/by-name/tu/tuistash/package.nix
@@ -0,0 +1,29 @@
+{
+  fetchFromGitHub,
+  lib,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "tuistash";
+  version = "0.7.1";
+
+  src = fetchFromGitHub {
+    owner = "edmocosta";
+    repo = "tuistash";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-LWmH/xHvdiY6lC7gsRh2gX31b9Fh4fWekrVdQ++8moQ=";
+  };
+
+  cargoHash = "sha256-mLtzdWHC7HN+hju71WQQZ4nJDMzybEfjzckbfeu32Qo=";
+
+  meta = {
+    description = "Terminal User Interface for Logstash";
+    homepage = "https://github.com/edmocosta/tuistash";
+    changelog = "https://github.com/edmocosta/tuistash/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = [ lib.licenses.asl20 ];
+    maintainers = [ lib.maintainers.kpbaks ];
+    mainProgram = "tuistash";
+    platforms = lib.platforms.all;
+  };
+})
