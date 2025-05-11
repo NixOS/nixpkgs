f1076a635793f9d82a4b9e2b5e76d26ad641a3a2
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-11 21:39:21 +0200

    dotool-udev-rules: init at 1.5

---
+++ pkgs/by-name/do/dotool-udev-rules/package.nix
@@ -0,0 +1,26 @@
+{
+  stdenv,
+  dotool,
+}:
+
+## Usage
+# In NixOS, set programs.dotool.enable = true;
+
+stdenv.mkDerivation {
+  pname = "dotool-udev-rules";
+  inherit (dotool) version src;
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -D 80-dotool.rules $out/lib/udev/rules.d/80-dotool.rules
+
+    runHook postInstall
+  '';
+
+  meta = dotool.meta // {
+    description = "UDev rules for dotool";
+  };
+}
