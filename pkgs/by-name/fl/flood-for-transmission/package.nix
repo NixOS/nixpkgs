b5811613ac04d6dea59cdbc64ea8a8559114bad4
Alex James <git@alextjam.es>
2025-05-11 13:37:56 -0500

    flood-for-transmission: use `finalAttrs` pattern

--- pkgs/by-name/fl/flood-for-transmission/package.nix
+++ pkgs/by-name/fl/flood-for-transmission/package.nix
@@ -11,20 +11,18 @@
   floodSettings ? null,
 }:
 
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "flood-for-transmission";
   version = "2025-07-19T10-51-22";
 
   src = fetchFromGitHub {
     owner = "johman10";
     repo = "flood-for-transmission";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-2oHEVvZZcxH9RBKreaiwFKp7Iu5ijYdpXdgVknCxwKw=";
   };
 
   npmDepsHash = "sha256-IUdsUGsm6yAbXqf4UGkz1VPa366TnWsTakkbywbLeTU=";
-
-  strictDeps = true;
 
   installPhase = ''
     runHook preInstall
@@ -48,4 +46,4 @@ buildNpmPackage rec {
     maintainers = with lib.maintainers; [ al3xtjames ];
     platforms = lib.platforms.all;
   };
-}
+})
