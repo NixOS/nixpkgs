0e1a30e4bb0033dc31e3c3f56d73dfce5d7a2aaa
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-11 21:39:35 +0200

    nixos/dotool: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -191,6 +191,7 @@
   ./programs/digitalbitbox/default.nix
   ./programs/direnv.nix
   ./programs/dmrconfig.nix
+  ./programs/dotool.nix
   ./programs/droidcam.nix
   ./programs/dublin-traceroute.nix
   ./programs/ecryptfs.nix
