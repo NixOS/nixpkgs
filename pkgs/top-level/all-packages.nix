3747dd70c5a95d1faf56e9f6b86e42df79c27ff6
Guy Chronister <guychronister@outlook.com>
2025-05-11 14:41:23 +0000

    ledger-web: migrate to by-name
    (cherry picked from commit b117131ade13571a827f7811e1f1e5d87b065fbf)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13250,8 +13250,6 @@ with pkgs;
   libutp = callPackage ../applications/networking/p2p/libutp { };
   libutp_3_4 = callPackage ../applications/networking/p2p/libutp/3.4.nix { };
 
-  ledger-web = callPackage ../applications/office/ledger-web { };
-
   linphone = libsForQt5.callPackage ../applications/networking/instant-messengers/linphone { };
 
   lmms = libsForQt5.callPackage ../applications/audio/lmms {
