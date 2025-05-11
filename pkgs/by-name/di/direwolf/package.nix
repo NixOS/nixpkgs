675ddbb87b74deae60fddbd9c5a19a50bcb475fe
Gavin John <gavinnjohn@gmail.com>
2025-05-11 00:27:47 -0700

    direwolf: remove with lib

--- pkgs/by-name/di/direwolf/package.nix
+++ pkgs/by-name/di/direwolf/package.nix
@@ -84,12 +84,12 @@ stdenv.mkDerivation (finalAttrs: {
 
   doInstallCheck = true;
 
-  meta = with lib; {
+  meta = {
     description = "Soundcard Packet TNC, APRS Digipeater, IGate, APRStt gateway";
     homepage = "https://github.com/wb2osz/direwolf/";
-    license = licenses.gpl2;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [
       lasandell
       sarcasticadmin
     ];
