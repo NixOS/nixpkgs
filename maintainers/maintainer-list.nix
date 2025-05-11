699f0ac2110ae235c1d7785496d8bb80d37a3e41
hbere <git@berezowski.de>
2025-05-11 19:39:54 +0200

    maintainers: add berezowski

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3466,6 +3466,12 @@
     githubId = 20448408;
     keys = [ { fingerprint = "D446 E58D 87A0 31C7 EC15  88D7 B461 2924 45C6 E696"; } ];
   };
+  berezowski = {
+    email = "nix@berezowski.de";
+    github = "berezowski";
+    githubId = 2563136;
+    name = "Hubert Berezowski";
+  };
   bergey = {
     email = "bergey@teallabs.org";
     github = "bergey";
