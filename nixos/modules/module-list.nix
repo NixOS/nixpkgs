03b28420c9102af3909fb3c408e5176adf40246f
thiloho <123883702+thiloho@users.noreply.github.com>
2025-05-11 16:27:16 +0200

    nixos/archtika: remove module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1502,7 +1502,6 @@
   ./services/web-apps/akkoma.nix
   ./services/web-apps/alps.nix
   ./services/web-apps/anuko-time-tracker.nix
-  ./services/web-apps/archtika.nix
   ./services/web-apps/artalk.nix
   ./services/web-apps/audiobookshelf.nix
   ./services/web-apps/baikal.nix
