b742f1501146b227f8f442661b9c7b7a909eb249
patka <patka@patka.dev>
2025-05-12 21:46:10 +0200

    silc_client: drop
    An unmaintained chat client written in C from 2014.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1680,6 +1680,7 @@ mapAliases {
   signal-desktop-beta = throw "signal-desktop-beta has been removed to make the signal-desktop package easier to maintain";
   signal-desktop-source = lib.warnOnInstantiate "'signal-desktop-source' is now exposed at 'signal-desktop'." signal-desktop; # Added 2025-04-16
   silc_server = throw "'silc_server' has been removed because it is unmaintained"; # Added 2025-05-12
+  silc_client = throw "'silc_client' has been removed because it is unmaintained"; # Added 2025-05-12
   sheesy-cli = throw "'sheesy-cli' has been removed due to lack of upstream maintenance"; # Added 2025-01-26
   shout = nodePackages.shout; # Added unknown; moved 2024-10-19
   sky = throw "'sky' has been removed because its upstream website disappeared"; # Added 2024-07-21
