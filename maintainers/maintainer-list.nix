0fab92b2c418981319c3fa168dfb1e424684a358
Sanfer <sanferdsouza@gmail.com>
2025-05-12 14:57:46 +0300

    maintainers: add sanferdsouza

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21883,6 +21883,11 @@
     githubId = 96694484;
     name = "Sandeep Patel";
   };
+  sanferdsouza = {
+    name = "Sanfer D'souza";
+    github = "sanferdsouza";
+    githubId = 9338001;
+  };
   Sanskarzz = {
     email = "sanskar.gur@gmail.com";
     github = "Sanskarzz";
