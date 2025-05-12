8229fdbb9dfbfdfe4f7c2668ba9057fed5bd5d33
Nick Cao <nickcao@nichi.co>
2025-05-12 17:14:53 -0400

    Update pkgs/development/python-modules/dogpile-cache/default.nix

--- pkgs/development/python-modules/dogpile-cache/default.nix
+++ pkgs/development/python-modules/dogpile-cache/default.nix
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   version = "1.4.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.6";
+  disabled = pythonOlder "3.9";
 
   src = fetchPypi {
     pname = "dogpile_cache";
