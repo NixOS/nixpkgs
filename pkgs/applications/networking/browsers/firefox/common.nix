ca9a49a736129458e344cf1c8d826444b69d3d51
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-05-12 03:51:57 +0200

    buildMozillaMach: update nss version range
    While building Firefox ESR with the latest NSS version generally works
    we and upstream actually prefer using the ESR versions together.

--- pkgs/applications/networking/browsers/firefox/common.nix
+++ pkgs/applications/networking/browsers/firefox/common.nix
@@ -560,7 +560,7 @@ buildStdenv.mkDerivation {
         xorg.xorgproto
         zlib
         (
-          if (lib.versionAtLeast version "116") then nss_latest else nss_esr # 3.90
+          if (lib.versionAtLeast version "129") then nss_latest else nss_esr # 3.90
         )
       ]
       ++ lib.optional alsaSupport alsa-lib
