d2429dbb06fdfc135a0328daf8aca585b5777238
Neil Mayhew <nix@neil.mayhew.name>
2025-05-12 13:11:16 -0600

    haskellPackages.cabal-lenses: build with required version of Cabal
    Also remove broken status

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -58,6 +58,11 @@ self: super:
     Cabal = self.Cabal_3_14_1_1;
   };
 
+  # Needs Cabal>=3.14
+  cabal-lenses = super.cabal-lenses.override {
+    Cabal = self.Cabal_3_14_1_1;
+  };
+
   # cabal-install needs most recent versions of Cabal and Cabal-syntax,
   # so we need to put some extra work for non-latest GHCs
   inherit
