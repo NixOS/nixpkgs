e13feaae995b022e23b78b7f3fed12abbfceeb42
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-12 11:26:20 +0000

    spicetify-cli: 2.40.5 -> 2.40.7

--- pkgs/by-name/sp/spicetify-cli/package.nix
+++ pkgs/by-name/sp/spicetify-cli/package.nix
@@ -8,16 +8,16 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "spicetify-cli";
-  version = "2.40.5";
+  version = "2.40.7";
 
   src = fetchFromGitHub {
     owner = "spicetify";
     repo = "cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-qBUGi4Q1RZnJ7cXNT9fjSPj5CVdku37h5+4Pv42/B7Q=";
+    hash = "sha256-iNRjRfRrK/pLL4xZX6Q/LV45NyNG1u4CyQGZtZYb2X8=";
   };
 
-  vendorHash = "sha256-yCxEpfqZRJcx4KevS+vqq6taHCZyEw1VK4Xt6BPPFAo=";
+  vendorHash = "sha256-901njlGcAxr12F9w6yQ+ESsptlwsZsMvKPUmlHxehmA=";
 
   ldflags = [
     "-s -w"
