a17acc0bec26718f79171e9adeb0a20b4dfbb8ee
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-12 08:18:07 +0000

    vengi-tools: 0.0.36 -> 0.0.37

--- pkgs/by-name/ve/vengi-tools/package.nix
+++ pkgs/by-name/ve/vengi-tools/package.nix
@@ -34,13 +34,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vengi-tools";
-  version = "0.0.36";
+  version = "0.0.37";
 
   src = fetchFromGitHub {
     owner = "vengi-voxel";
     repo = "vengi";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-6MJw8BaHvd9cZNCMIFwAtk8UzxP+RSfnuv9py8sUgnY=";
+    hash = "sha256-Pm3vGS6u//iktkj/2RR7xaCTt8fevCWm0a1Hlfkxry8=";
   };
 
   prePatch = lib.optionalString stdenv.hostPlatform.isDarwin ''
