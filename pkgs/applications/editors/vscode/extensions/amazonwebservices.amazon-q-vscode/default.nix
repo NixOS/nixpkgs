2d1675a0d63feee5d83f59f03075bb873bcf19a2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-05-12 15:10:44 +0200

    vscode-extensions.amazonwebservices.amazon-q-vscode: init at 1.66.0

---
+++ pkgs/applications/editors/vscode/extensions/amazonwebservices.amazon-q-vscode/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  vscode-utils,
+}:
+
+vscode-utils.buildVscodeMarketplaceExtension (finalAttrs: {
+  mktplcRef = {
+    name = "amazon-q-vscode";
+    publisher = "AmazonWebServices";
+    version = "1.66.0";
+    hash = "sha256-EnNwlSmJWBcSfFCayxJS94qVUqgQlbX0RLCB4jJsn+4=";
+  };
+
+  meta = {
+    changelog = "https://github.com/aws/aws-toolkit-vscode/releases/tag/amazonq%2Fv${finalAttrs.version}";
+    description = "Amazon Q, CodeCatalyst, Local Lambda debug, SAM/CFN syntax, ECS Terminal, AWS resources";
+    downloadPage = "https://marketplace.visualstudio.com/items?itemName=AmazonWebServices.amazon-q-vscode";
+    homepage = "https://github.com/aws/aws-toolkit-vscode";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ drupol ];
+  };
+})
