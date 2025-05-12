48adf63a43758a88f8ea0cc6933e36d58e9ae7e5
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-12 03:48:10 +0000

    nextcloud-talk-desktop: 1.1.7 -> 1.1.8

--- pkgs/by-name/ne/nextcloud-talk-desktop/package.nix
+++ pkgs/by-name/ne/nextcloud-talk-desktop/package.nix
@@ -25,13 +25,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "nextcloud-talk-desktop";
-  version = "1.1.7";
+  version = "1.1.8";
 
   # Building from source would require building also building Server and Talk components
   # See https://github.com/nextcloud/talk-desktop?tab=readme-ov-file#%EF%B8%8F-prerequisites
   src = fetchzip {
     url = "https://github.com/nextcloud-releases/talk-desktop/releases/download/v${finalAttrs.version}/Nextcloud.Talk-linux-x64.zip";
-    hash = "sha256-tcg3ctqbarJKJ9oROI+PHVTddJoioA1rF/QLR659q0Q=";
+    hash = "sha256-rKUf6BQk/snwI6Sf67+PbNEnoZZY1u+EOEWE11p3dYw=";
     stripRoot = false;
   };
 
