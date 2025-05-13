6b604051c4fa5b25ec50e71704b6f603c71d28fe
Fernando Rodrigues <alpha@sigmasquadron.net>
2025-05-12 22:20:12 -0300

    xen: move main package builder to by-name
    This causes the removal of buildXenPackage, as the recommended way to
    build a custom Xen is to override the package.
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14360,8 +14360,6 @@ with pkgs;
           ;
       };
 
-  buildXenPackage = callPackage ../build-support/xen { };
-
   gxneur = callPackage ../applications/misc/gxneur {
     inherit (gnome2) libglade GConf;
   };
