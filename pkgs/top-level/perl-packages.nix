0598f4b0bfa682fcdcaac1991f1e18042972408b
Malo Bourgon <mbourgon@gmail.com>
2025-05-13 01:09:27 -0700

    perlPackages.FileLibMagic: unbreak on Darwin

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -13562,7 +13562,6 @@ with self;
         artistic1
         gpl1Plus
       ];
-      broken = stdenv.hostPlatform.isDarwin; # never built on Hydra https://hydra.nixos.org/job/nixpkgs/staging-next/perl534Packages.FileLibMagic.x86_64-darwin
     };
   };
 
