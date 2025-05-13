94cb7bbfbe30f633f3fd0c798648fd388eae7b71
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-13 20:20:48 +0000

    cozette: 1.28.0 -> 1.29.0

--- pkgs/by-name/co/cozette/package.nix
+++ pkgs/by-name/co/cozette/package.nix
@@ -6,13 +6,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "cozette";
-  version = "1.28.0";
+  version = "1.29.0";
 
   src = fetchzip {
     url = "https://github.com/slavfox/Cozette/releases/download/v.${version}/CozetteFonts-v-${
       builtins.replaceStrings [ "." ] [ "-" ] version
     }.zip";
-    hash = "sha256-KxlChsydFtm26CFugbWqeJi5DQP0BxONSz5mOcyiE28=";
+    hash = "sha256-DHUnCzp6c3d57cfkO2kH+czXRiqRWn6DBTo9NVTghQ0=";
   };
 
   installPhase = ''
