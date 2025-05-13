c0018708750841182b7cbec0898400b1af4eb7d4
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-05-13 17:31:32 -0300

    luau-lsp: 1.45.0 -> 1.46.0
    Diff: https://github.com/JohnnyMorganz/luau-lsp/compare/refs/tags/1.45.0...refs/tags/1.46.0
    
    Changelog: https://github.com/JohnnyMorganz/luau-lsp/blob/1.46.0/CHANGELOG.md

--- pkgs/by-name/lu/luau-lsp/package.nix
+++ pkgs/by-name/lu/luau-lsp/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "luau-lsp";
-  version = "1.45.0";
+  version = "1.46.0";
 
   src = fetchFromGitHub {
     owner = "JohnnyMorganz";
     repo = "luau-lsp";
     tag = finalAttrs.version;
-    hash = "sha256-OJAjTy0vTRb43TTiPeXafWq4kjIpnDXoTprVzbMnaWQ=";
+    hash = "sha256-UlZ+v202E2daRsV3KBK2WD3NaXcIbhIFL32DZjZ/eus=";
     fetchSubmodules = true;
   };
 
