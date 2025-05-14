47228f512490a64a322b82e533880a6ba3c38860
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-14 05:21:22 +0000

    nezha: 1.12.0 -> 1.12.4

--- pkgs/by-name/ne/nezha/package.nix
+++ pkgs/by-name/ne/nezha/package.nix
@@ -14,7 +14,7 @@
 
 let
   pname = "nezha";
-  version = "1.12.0";
+  version = "1.12.4";
 
   frontendName = lib.removePrefix "nezha-theme-";
 
@@ -58,7 +58,7 @@ buildGo124Module {
     owner = "nezhahq";
     repo = "nezha";
     tag = "v${version}";
-    hash = "sha256-ajjAsoR+1HRHfIjyqlJFHtn1nzDAdbP5TzKOqnHlAXw=";
+    hash = "sha256-f9zP9koWjP8PPtQhbYx56J1mW8+xKuZCfxgOLw6XGc0=";
   };
 
   proxyVendor = true;
@@ -97,7 +97,7 @@ buildGo124Module {
     GOROOT=''${GOROOT-$(go env GOROOT)} swag init --pd -d . -g ./cmd/dashboard/main.go -o ./cmd/dashboard/docs --parseGoList=false
   '';
 
-  vendorHash = "sha256-8pOeMUiBEUbB7D3MnlpOTdanktR8XdXm3YB53XMCDWQ=";
+  vendorHash = "sha256-Pj5HfrwIuWt3Uwt2Y9Tz96B2kL7Svq5rzU1hKf/RZ4s=";
 
   ldflags = [
     "-s"
