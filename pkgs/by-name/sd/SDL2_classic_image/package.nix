e5abe98245d0efd7260b42bd5898a319f422df93
Grimmauld <Grimmauld@grimmauld.de>
2025-05-14 23:45:22 +0200

    fixup! SDL2_classic_image: defer to SDL2.sdlLibs

--- pkgs/by-name/sd/SDL2_classic_image/package.nix
+++ pkgs/by-name/sd/SDL2_classic_image/package.nix
@@ -2,4 +2,4 @@
   SDL2_classic,
   enableSTB ? null,
 }:
-if enableSTB == null then SDL2_classic.sdlLibs.SDL2_image else SDL2_classic.sdlLibs.SDL2_image.override { }
+if enableSTB == null then SDL2_classic.sdlLibs.SDL2_image else SDL2_classic.sdlLibs.SDL2_image.override { inherit enableSTB; }
