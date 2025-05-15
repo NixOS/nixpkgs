2f530facddd1e102d8aa967ec45b27d7d581e4dc
emaryn <emaryn@users.noreply.github.com>
2025-05-16 01:20:57 +0800

    refine: remove libadwaita'

--- pkgs/by-name/re/refine/package.nix
+++ pkgs/by-name/re/refine/package.nix
@@ -19,20 +19,6 @@
   glib,
 }:
 
-let
-  libadwaita' = libadwaita.overrideAttrs (oldAttrs: {
-    version = "1.6.2-unstable-2025-01-02";
-    src = oldAttrs.src.override {
-      tag = null;
-      rev = "f5f0e7ce69405846a8f8bdad11cef2e2a7e99010";
-      hash = "sha256-n5RbGHtt2g627T/Tg8m3PjYIl9wfYTIcrplq1pdKAXk=";
-    };
-
-    # `test-application-window` is flaky on aarch64-linux
-    doCheck = false;
-  });
-in
-
 python3Packages.buildPythonApplication rec {
   pname = "refine";
   version = "0.5.8";
@@ -61,7 +47,7 @@ python3Packages.buildPythonApplication rec {
 
   buildInputs = [
     libxml2
-    libadwaita'
+    libadwaita
   ];
 
   dependencies =
