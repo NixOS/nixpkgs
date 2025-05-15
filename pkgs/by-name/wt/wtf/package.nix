2330c66b8abfadd076921fa4f0230a94a421dce3
MikeHorn-git <MikeHornProton@proton.me>
2025-05-15 15:03:52 +0000

    wtf: init at 0.5.7

---
+++ pkgs/by-name/wt/wtf/package.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+  cmake,
+  ninja,
+  pkg-config,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "wtf";
+  version = "0.5.7";
+
+  src = fetchFromGitHub {
+    owner = "0vercl0k";
+    repo = "wtf";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-lfPx9RQEW457VQkDbvu/D9EFZrdNLz2ToQ9dfa7+tzY=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    pkg-config
+  ];
+
+  sourceRoot = "source/src";
+
+  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+
+  installPhase = ''
+    install -Dm755 wtf $out/bin/wtf
+  '';
+
+  meta = {
+    description = "Cross-platform snapshot-based fuzzer";
+    homepage = "https://github.com/0vercl0k/wtf";
+    changelog = "https://github.com/0vercl0k/wtf/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ mikehorn ];
+    mainProgram = "wtf";
+  };
+})
