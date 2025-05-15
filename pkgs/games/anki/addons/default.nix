4149a992fa87e056fb6155cd4c9f29d6f0938394
June Stepp <git@junestepp.me>
2025-05-15 13:29:45 -0500

    anki-addons.recolor: init at 3.1

--- pkgs/games/anki/addons/default.nix
+++ pkgs/games/anki/addons/default.nix
@@ -79,6 +79,30 @@ in
     };
   });
 
+  recolor = anki-utils.buildAnkiAddon (finalAttrs: {
+    pname = "recolor";
+    version = "3.1";
+    src = fetchFromGitHub {
+      owner = "AnKing-VIP";
+      repo = "AnkiRecolor";
+      rev = finalAttrs.version;
+      sparseCheckout = [ "src/addon" ];
+      hash = "sha256-28DJq2l9DP8O6OsbNQCZ0pm4S6CQ3Yz0Vfvlj+iQw8Y=";
+    };
+    sourceRoot = "source/src/addon";
+    passthru.updateScript = nix-update-script { };
+    meta = {
+      description = ''
+        ReColor your Anki desktop to whatever aesthetic you like
+      '';
+      homepage = "https://github.com/AnKing-VIP/AnkiRecolor";
+      # No license file, but it can be assumed to be AGPL3 based on
+      # https://ankiweb.net/account/terms.
+      license = lib.licenses.agpl3Only;
+      maintainers = with lib.maintainers; [ junestepp ];
+    };
+  });
+
   reviewer-refocus-card = buildAnkiAddon {
     pname = "reviewer-refocus-card";
     version = "0-unstable-2022-12-24";
