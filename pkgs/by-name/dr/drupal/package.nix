8f236fe461d21594cab7e08303c7b654a5389f01
Andrew Benbow <andrew.benbow@posteo.com>
2025-05-15 11:02:51 -0400

    drupal: init at 11.1.7

---
+++ pkgs/by-name/dr/drupal/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  fetchFromGitLab,
+  php,
+}:
+
+php.buildComposerProject2 (finalAttrs: {
+  pname = "drupal";
+  version = "11.1.7";
+
+  src = fetchFromGitLab {
+    domain = "git.drupalcode.org";
+    owner = "project";
+    repo = "drupal";
+    tag = finalAttrs.version;
+    hash = "sha256-jf28r44VDP9MzShoJMFD+6xSUcKBRGYJ1/ruQ3nGTRE=";
+  };
+
+  vendorHash = "sha256-LUZTf/Zn8p+V2K1LjhvrgaGBiTcSmGRsG1t9vXUcbeY=";
+  composerNoPlugins = false;
+
+  meta = {
+    description = "Drupal CMS";
+    license = lib.licenses.mit;
+    homepage = "https://drupal.org/";
+    maintainers = with lib.maintainers; [
+      drupol
+      OulipianSummer
+    ];
+    platforms = php.meta.platforms;
+  };
+})
