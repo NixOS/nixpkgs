32d80a5e7c7fff0ff25d67b17e1a3b1046dca405
K900 <me@0upti.me>
2025-05-15 19:54:06 +0300

    kdePackages: Plasma 6.3 -> 6.4 beta 2
    - aurorae moved to its own library, move our patch there
    - kwin-x11 is a copy of kwin(-wayland), but some bits removed
    - ksystemstats and libksysguard need capabilities via helpers

--- pkgs/kde/plasma/krdp/default.nix
+++ pkgs/kde/plasma/krdp/default.nix
@@ -6,7 +6,6 @@
   pkg-config,
   qtwayland,
   freerdp,
-  fetchpatch,
   wayland,
   wayland-protocols,
 }:
@@ -16,11 +15,6 @@ mkKdeDerivation {
   patches = [
     (replaceVars ./hardcode-openssl-path.patch {
       openssl = lib.getExe openssl;
-    })
-    (fetchpatch {
-      # support for freerdp3, can be dropped with krdp 6.4
-      url = "https://invent.kde.org/plasma/krdp/-/merge_requests/69.patch";
-      hash = "sha256-5x9JUbFTw/POxBW8G/BOlo/wtcUjPU9J1V/wba1EI/o=";
     })
   ];
 
