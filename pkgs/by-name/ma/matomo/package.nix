79797b554863a746a640b49b956f7350eb33da58
Niklas Korz <niklas@niklaskorz.de>
2025-05-15 11:30:48 +0200

    matomo: 5.3.1 -> 5.3.2
    Changelog: https://matomo.org/changelog/matomo-5-3-2/

--- pkgs/by-name/ma/matomo/package.nix
+++ pkgs/by-name/ma/matomo/package.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "matomo";
-  version = "5.3.1";
+  version = "5.3.2";
 
   src = fetchurl {
     url = "https://builds.matomo.org/matomo-${finalAttrs.version}.tar.gz";
-    hash = "sha256-ynG5M21YQzGhII19kmJv0y5L3HIoEdf30dZA+nScuYA=";
+    hash = "sha256-rn5Lr2BSrGitI16MLlP91znSPm2Asd6j0qI8N+1c+Lo=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
