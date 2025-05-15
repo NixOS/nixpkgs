dacc7ff54dd928ffea86949e1e95f518c1e74048
Florian Klink <flokli@flokli.de>
2025-05-15 16:36:01 +0300

    irccat: 0.4.8 -> 0.4.11

--- pkgs/by-name/ir/irccat/package.nix
+++ pkgs/by-name/ir/irccat/package.nix
@@ -2,26 +2,37 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  fetchpatch,
 }:
 
 buildGoModule rec {
   pname = "irccat";
-  version = "0.4.8";
+  version = "0.4.11";
 
   src = fetchFromGitHub {
     owner = "irccloud";
     repo = "irccat";
     rev = "v${version}";
-    hash = "sha256-fr5x1usviJPbc4t5SpIVgV9Q6071XG8eYtyeyraddts=";
+    hash = "sha256-DwJTwpkb2/YHocUHhF0DwxEzDW6DLOxADda/Ed5Xg7c=";
   };
 
-  vendorHash = "sha256-IRXyM000ZDiLPHX20lXlx00tkCzBe5PqvdgXAvm0EAw=";
+  patches = [
+    # predeclared any requires go1.18 or later (-lang was set to go1.16; check go.mod)
+    # https://github.com/irccloud/irccat/pull/38
+    (fetchpatch {
+      url = "https://github.com/irccloud/irccat/pull/38/commits/6bbe6e5762e93080f916a87a93aaeb90ec5fe12c.patch";
+      hash = "sha256-1QsMxAOdZLT/ReDhaVzQtYiGejl5mOrTC58PQpIygIY=";
+    })
+  ];
 
-  meta = with lib; {
+  vendorHash = "sha256-SUE868jVJywqE0A5yjMWohrMw58OUnxGGxvcR8UzPfE=";
+
+  meta = {
     homepage = "https://github.com/irccloud/irccat";
+    changelog = "https://github.com/irccloud/irccat/releases/tag/v${version}0.4.11";
     description = "Send events to IRC channels from scripts and other applications";
     mainProgram = "irccat";
-    maintainers = with maintainers; [ qyliss ];
-    license = licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ qyliss ];
+    license = lib.licenses.gpl3Only;
   };
 }
