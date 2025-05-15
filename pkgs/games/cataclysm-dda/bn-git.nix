67924d8edbbba7e43b12582d43b794d633766246
caryoscelus <caryoscelus@gmx.com>
2025-05-15 01:08:25 +0000

    cataclysm-bn-git: fix meta

--- pkgs/games/cataclysm-dda/bn-git.nix
+++ pkgs/games/cataclysm-dda/bn-git.nix
@@ -35,8 +35,27 @@ let
     ];
 
     meta = common.meta // {
-      maintainers = with lib.maintainers;
-      common.meta.maintainers ++ [ rardiol ];
+      description = "Roguelike with sci-fi elements set in a post-apocalyptic world";
+      longDescription = ''
+        Cataclysm: Bright Nights is a roguelike with sci-fi elements set in a
+        post-apocalyptic world, a fork of Cataclysm: Dark Days Ahead.
+
+        While some have described it as a "zombie game", there is far more to
+        Cataclysm than that. Struggle to survive in a harsh, persistent,
+        procedurally generated world. Scavenge the remnants of a dead
+        civilization for food, equipment, or, if you are lucky, a vehicle with a
+        full tank of gas to get you the hell out of there.
+
+        Fight to defeat or escape from a wide variety of powerful monstrosities,
+        from zombies to giant insects to killer robots and things far stranger
+        and deadlier, and against the others like yourself, who want what you
+        have.
+
+        Find a way to stop the Cataclysm ... or become one of its strongest
+        monsters.
+      '';
+      homepage = "https://cataclysmbn.org/";
+      maintainers = with lib.maintainers; common.meta.maintainers ++ [ caryoscelus ];
     };
   });
 in
