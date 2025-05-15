c5970cbb781078b252092bb3ac5598ebe2d09f52
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-15 02:27:16 +0000

    python3Packages.pylamarzocco: 2.0.1 -> 2.0.3
    (cherry picked from commit 579d3f632cda83dbcbbeb447933440a74d7560d3)

--- pkgs/development/python-modules/pylamarzocco/default.nix
+++ pkgs/development/python-modules/pylamarzocco/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "pylamarzocco";
-  version = "2.0.1";
+  version = "2.0.3";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "zweckj";
     repo = "pylamarzocco";
     tag = "v${version}";
-    hash = "sha256-ZfixljgkRElDZFkSbZrFK7EO6NGU7jsal+Ra52/N82A=";
+    hash = "sha256-zQbE+GMXwmdUSOd4xqgiC2QENFdLvcUS3JLKTz4rq8s=";
   };
 
   build-system = [ setuptools ];
