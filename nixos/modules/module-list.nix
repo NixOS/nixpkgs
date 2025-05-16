ccb826ffcf9fa48cdb63f6f7481f5b1d81feebfb
soyouzpanda <soyouzpanda@soyouzpanda.fr>
2025-05-16 14:42:13 +0200

    nixos/lasuite-meet: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1594,6 +1594,7 @@
   ./services/web-apps/komga.nix
   ./services/web-apps/lanraragi.nix
   ./services/web-apps/lasuite-docs.nix
+  ./services/web-apps/lasuite-meet.nix
   ./services/web-apps/lemmy.nix
   ./services/web-apps/limesurvey.nix
   ./services/web-apps/mainsail.nix
