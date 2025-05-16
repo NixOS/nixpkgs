bea7b1a7916eb198c9330c05b31e67b6c7964389
themadbit <marktanui75@gmail.com>
2025-05-16 15:50:02 +0300

    python3Packages.twomemo: init 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10274,6 +10274,8 @@ self: super: with self; {
 
   omegaconf = callPackage ../development/python-modules/omegaconf { };
 
+  omemo = callPackage ../development/python-modules/omemo { };
+
   omemo-dr = callPackage ../development/python-modules/omemo-dr { };
 
   omnikinverter = callPackage ../development/python-modules/omnikinverter { };
@@ -17852,6 +17854,8 @@ self: super: with self; {
   twitterapi = callPackage ../development/python-modules/twitterapi { };
 
   twofish = callPackage ../development/python-modules/twofish { };
+
+  twomemo = callPackage ../development/python-modules/twomemo { };
 
   twscrape = callPackage ../development/python-modules/twscrape { };
 
