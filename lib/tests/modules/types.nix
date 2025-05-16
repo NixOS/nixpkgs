1980e9a444ecac30e157248d98910b074c5b4d54
Robert Hensing <robert@roberthensing.nl>
2025-05-16 12:11:37 +0200

    lib/tests/modules: Test attrNamesToTrue

--- lib/tests/modules/types.nix
+++ lib/tests/modules/types.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, lib, ... }:
 let
   inherit (builtins)
     storeDir
@@ -10,7 +10,10 @@ let
 in
 {
   options = {
+    check = mkOption { };
+    # NB: types are tested in multiple places, so this list is far from exhaustive
     pathInStore = mkOption { type = types.lazyAttrsOf types.pathInStore; };
+    attrNamesToTrue = mkOption { type = types.lazyAttrsOf types.attrNamesToTrue; };
   };
   config = {
     pathInStore.ok1 = "${storeDir}/0lz9p8xhf89kb1c1kk6jxrzskaiygnlh-bash-5.2-p15.drv";
@@ -21,5 +24,42 @@ in
     pathInStore.bad3 = "${storeDir}/";
     pathInStore.bad4 = "${storeDir}/.links"; # technically true, but not reasonable
     pathInStore.bad5 = "/foo/bar";
+    attrNamesToTrue.justNames = [
+      "a"
+      "b"
+      "c"
+    ];
+    attrNamesToTrue.mixed = lib.mkMerge [
+      {
+        a = true;
+        b = false;
+      }
+      [ "c" ]
+    ];
+    attrNamesToTrue.trivial = {
+      a = true;
+      b = false;
+      c = true;
+    };
+    check =
+      assert
+        config.attrNamesToTrue.justNames == {
+          a = true;
+          b = true;
+          c = true;
+        };
+      assert
+        config.attrNamesToTrue.mixed == {
+          a = true;
+          b = false;
+          c = true;
+        };
+      assert
+        config.attrNamesToTrue.trivial == {
+          a = true;
+          b = false;
+          c = true;
+        };
+      "ok";
   };
 }
