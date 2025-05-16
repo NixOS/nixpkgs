def8389c97748118172e7d273fcba8c2e00c2178
palumbou <palumbo.u+github@gmail.com>
2025-05-16 18:21:05 +0200

    catppuccin-plymouth: fix formatting using "nix fmt" as written in the CONTRIBUTING.md documentation

--- pkgs/by-name/ca/catppuccin-plymouth/package.nix
+++ pkgs/by-name/ca/catppuccin-plymouth/package.nix
@@ -1,9 +1,9 @@
-{ stdenvNoCC
-, lib
-, fetchFromGitHub
-, unstableGitUpdater
-, variant ? "macchiato"
-,
+{
+  stdenvNoCC,
+  lib,
+  fetchFromGitHub,
+  unstableGitUpdater,
+  variant ? "macchiato",
 }:
 
 let
@@ -17,40 +17,39 @@ let
 in
 assert lib.assertOneOf "${pname}: color variant" variant validVariants;
 
-stdenvNoCC.mkDerivation
-  (finalAttrs: {
-    inherit pname;
-    version = "0-unstable-2024-10-19";
+stdenvNoCC.mkDerivation (finalAttrs: {
+  inherit pname;
+  version = "0-unstable-2024-10-19";
 
-    src = fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "plymouth";
-      rev = "e0f58d6fcf3dbc2d35dfc4fec394217fbfa92666";
-      hash = "sha256-He6ER1QNrJCUthFoBBGHBINouW/tozxQy3R79F5tsuo=";
-    };
+  src = fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "plymouth";
+    rev = "e0f58d6fcf3dbc2d35dfc4fec394217fbfa92666";
+    hash = "sha256-He6ER1QNrJCUthFoBBGHBINouW/tozxQy3R79F5tsuo=";
+  };
 
-    sourceRoot = "${finalAttrs.src.name}/themes/catppuccin-${variant}";
+  sourceRoot = "${finalAttrs.src.name}/themes/catppuccin-${variant}";
 
-    installPhase = ''
-      runHook preInstall
+  installPhase = ''
+    runHook preInstall
 
-      sed -i 's:\(^ImageDir=\)/usr:\1'"$out"':' catppuccin-${variant}.plymouth
-      mkdir -p $out/share/plymouth/themes/catppuccin-${variant}
-      cp * $out/share/plymouth/themes/catppuccin-${variant}
+    sed -i 's:\(^ImageDir=\)/usr:\1'"$out"':' catppuccin-${variant}.plymouth
+    mkdir -p $out/share/plymouth/themes/catppuccin-${variant}
+    cp * $out/share/plymouth/themes/catppuccin-${variant}
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
-    passthru.updateScript = unstableGitUpdater { };
+  passthru.updateScript = unstableGitUpdater { };
 
-    meta = {
-      description = "Soothing pastel theme for Plymouth";
-      homepage = "https://github.com/catppuccin/plymouth";
-      license = lib.licenses.mit;
-      platforms = lib.platforms.linux;
-      maintainers = with lib.maintainers; [
-        johnrtitor
-        spectre256
-      ];
-    };
-  })
+  meta = {
+    description = "Soothing pastel theme for Plymouth";
+    homepage = "https://github.com/catppuccin/plymouth";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [
+      johnrtitor
+      spectre256
+    ];
+  };
+})
