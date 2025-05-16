060cd6315ef8d51525dcfe6f22a85073b68d2f17
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-16 12:57:58 +0000

    python3Packages.fastai: 2.7.19 -> 2.8.1

--- pkgs/development/python-modules/fastai/default.nix
+++ pkgs/development/python-modules/fastai/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "fastai";
-  version = "2.7.19";
+  version = "2.8.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-CoBABtO2NkrNVUTzQ58vBdRe2rr4ji26iJOzema4Nsg=";
+    hash = "sha256-H8+Chlw9ZtJfDDOMHL7j+P4P7AjuwJnhtg2gxkFhqNs=";
   };
 
   propagatedBuildInputs = [
