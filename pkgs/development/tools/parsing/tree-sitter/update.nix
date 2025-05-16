461ed2ac7dc9338b5301de4e3ea5a1b36358370b
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2025-05-16 20:47:10 +0200

    tree-sitter-grammars: cleanup

--- pkgs/development/tools/parsing/tree-sitter/update.nix
+++ pkgs/development/tools/parsing/tree-sitter/update.nix
@@ -578,15 +578,13 @@ let
      }
      ${updateImpl} print-all-grammars-nix-file "$(< ${
        jsonFile "all-grammars.json" {
-         allGrammars = (
-           lib.mapAttrsToList (
-             nixRepoAttrName: attrs:
-             attrs
-             // {
-               inherit nixRepoAttrName;
-             }
-           ) allGrammars
-         );
+         allGrammars = lib.mapAttrsToList (
+           nixRepoAttrName: attrs:
+           attrs
+           // {
+             inherit nixRepoAttrName;
+           }
+         ) allGrammars;
          inherit outputDir;
        }
      })"
