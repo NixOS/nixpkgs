aa949dadab7a89292aa5f59b507fc62710cf696c
matthewcroughan <matt@croughan.sh>
2025-05-16 11:13:00 +0100

    csend: init at 0.1.1

---
+++ pkgs/by-name/cs/csend/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "csend";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "matthewdeaves";
+    repo = "csend";
+    rev = "v${version}";
+    hash = "sha256-v0vTdbMquB4xC/8bJ8nlSaRoxUGb0LWiPD344z7ljYA=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mv build/posix/csend_posix $out/bin
+  '';
+
+  meta = {
+    description = "A peer-to-peer (P2P) chat application designed to run on both modern POSIX-compliant systems";
+    longDescription = "A peer-to-peer (P2P) chat application designed to run on both modern POSIX-compliant systems (Linux, macOS) and vintage Classic Macintosh computers (System 7.5.3/MacTCP, built with Retro68). Features UDP-based peer discovery and TCP-based text messaging using a simple custom protocol. Includes Docker support for the POSIX version";
+    homepage = "https://github.com/matthewdeaves/csend";
+    # https://github.com/matthewdeaves/csend/issues/44
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ matthewcroughan ];
+    mainProgram = "csend_posix";
+    platforms = lib.platforms.unix;
+  };
+}
