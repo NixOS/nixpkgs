f1a47ff05a59a028f36f2b1481f39bba20309622
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-05-18 00:21:40 +0200

    python313Packages.tox: 4.23.2 -> 4.26.0
    https://github.com/tox-dev/tox/releases/tag/4.26.0

--- pkgs/development/python-modules/tox/default.nix
+++ pkgs/development/python-modules/tox/default.nix
@@ -24,14 +24,14 @@
 
 buildPythonPackage rec {
   pname = "tox";
-  version = "4.23.2";
+  version = "4.26.0";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "tox-dev";
     repo = "tox";
     tag = version;
-    hash = "sha256-rjz+CSGuGlQy9oneISJJo8doQf7abE9gNVpAnhD8Os8=";
+    hash = "sha256-VySdeZDC71vi2mOtjdFJ4iCSpWbFEW3nzrVucPUz/oc=";
   };
 
   postPatch = ''
