98c6384abe2441560bfbc2bd6b274ca0ecac42a2
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-17 18:39:14 +0000

    uncrustify: 0.80.1 -> 0.81.0

--- pkgs/by-name/un/uncrustify/package.nix
+++ pkgs/by-name/un/uncrustify/package.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   pname = "uncrustify";
-  version = "0.80.1";
+  version = "0.81.0";
 
   src = fetchFromGitHub {
     owner = "uncrustify";
     repo = "uncrustify";
     rev = "uncrustify-${version}";
-    sha256 = "sha256-SsPuqPyv3IFjYtAqANjzK8cdTGsyGBXVLzGWEUl28dg=";
+    sha256 = "sha256-8KTsrXUYOfqsWSGBAl0mZpGOYr+duFrRB0ITmq2Auqg=";
   };
 
   nativeBuildInputs = [
