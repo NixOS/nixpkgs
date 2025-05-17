37385d91928fa69399f3250aad952cf85e2b4221
Leona Maroni <dev@leona.is>
2025-05-17 14:49:47 +0200

    lasso: fix build with gcc14

--- pkgs/by-name/la/lasso/package.nix
+++ pkgs/by-name/la/lasso/package.nix
@@ -32,6 +32,17 @@ stdenv.mkDerivation rec {
       url = "https://git.entrouvert.org/entrouvert/lasso/commit/ffaddeb015a61db3e52c391de00430107a23e2f1.patch";
       hash = "sha256-D2npxpIuR/KrNYiKO3KXCvHEb/XVXUKIP0HQUd+w56k=";
     })
+    # Fix GCC 14 implicit declaration of function
+    # backported patch of https://git.entrouvert.org/entrouvert/lasso/commit/9767cdf7645a146bcc596a705ce32b855855a590
+    ./fix-gcc14-implicit-function-declaration.diff
+    # Fix GCC 14 incompatible pointer
+    # backported patch of https://git.entrouvert.org/entrouvert/lasso/commit/cbe2c45455d93ed793dc4be59e3d2d26f1bd1111
+    ./fix-gcc14-incompatible-pointer.diff
+    # Fix GCC 14 int-conversion (xmlsec)
+    (fetchpatch {
+      url = "https://git.entrouvert.org/entrouvert/lasso/commit/66c9f50f1f6b00d621a9a0ca2f924875f94d14ae.patch";
+      hash = "sha256-UkWxznKx2xAbjY29+NQ+cjIDhWLuyoWsmBTSiLUxWgU=";
+    })
   ];
 
   nativeBuildInputs = [
