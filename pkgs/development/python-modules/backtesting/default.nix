584cbef20641d4ed7e0c31d475e717282b86903a
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-17 12:35:54 +0000

    python3Packages.backtesting: 0.6.3 -> 0.6.4

--- pkgs/development/python-modules/backtesting/default.nix
+++ pkgs/development/python-modules/backtesting/default.nix
@@ -12,12 +12,12 @@
 
 buildPythonPackage rec {
   pname = "backtesting";
-  version = "0.6.3";
+  version = "0.6.4";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-xryzvjKT+FRbF4pnniHvkRA98jrZVoCyYOmjYU93Ta4=";
+    hash = "sha256-8Xasb7VG39XXQ/A47lgkYk5Vo4pJPE3Vghcxt0yGeq4=";
   };
 
   build-system = [
