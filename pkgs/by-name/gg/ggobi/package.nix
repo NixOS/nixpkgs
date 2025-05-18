00b44442b2b834decf562ea1bcf66da60f54fb54
Patrick <patrick@failmail.dev>
2025-05-18 15:36:56 +0200

    ggobi: 2.11.0 -> 2.12.0-unstable-2024-06-29

--- pkgs/by-name/gg/ggobi/package.nix
+++ pkgs/by-name/gg/ggobi/package.nix
@@ -1,30 +1,45 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
   pkg-config,
   libxml2,
   gtk2,
+  autoconf,
+  automake,
+  libtool,
+  texliveFull,
 }:
 
 stdenv.mkDerivation rec {
-  version = "2.1.11";
+  version = "2.1.12-unstable-2024-06-29";
   pname = "ggobi";
 
-  src = fetchurl {
-    url = "http://www.ggobi.org/downloads/ggobi-${version}.tar.bz2";
-    sha256 = "2c4ddc3ab71877ba184523e47b0637526e6f3701bd9afb6472e6dfc25646aed7";
+  src = fetchFromGitHub {
+    owner = "ggobi";
+    repo = "ggobi";
+    rev = "93a7148305778eb89bb838ea1b3228685a956f33";
+    hash = "sha256-pVzj4Q/FAGGPTt+b8U9GAock32XYBQW+HUWNHuBZiUE=";
   };
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+    autoconf
+    automake
+    texliveFull
+  ];
   buildInputs = [
     libxml2
+    libtool
     gtk2
   ];
 
   configureFlags = [ "--with-all-plugins" ];
 
   hardeningDisable = [ "format" ];
+  preConfigure = ''
+    ./bootstrap
+  '';
 
   meta = with lib; {
     description = "Visualization program for exploring high-dimensional data";
