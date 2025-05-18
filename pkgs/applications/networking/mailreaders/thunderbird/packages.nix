12206c8aad5f0a41c3cfdf11c52895595322c0ef
Thomas Gerbet <thomas@gerbet.me>
2025-05-18 11:11:03 +0200

    thunderbird-latest-unwrapped: 138.0 -> 138.0.1
    Fixes: CVE-2025-3875, CVE-2025-3877, CVE-2025-3909, CVE-2025-3932
    https://www.mozilla.org/en-US/security/advisories/mfsa2025-35/
    
    https://www.thunderbird.net/en-US/thunderbird/138.0.1/releasenotes/
    (cherry picked from commit 47b01e7cbdfe7f58f149e6e3a5222efb9f5fb534)

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -92,8 +92,8 @@ rec {
   thunderbird = thunderbird-latest;
 
   thunderbird-latest = common {
-    version = "138.0";
-    sha512 = "923d76cf0a14f29146e5dcfc75dd9522d465512f6c604de6e0acc0812d4240331c170913a821fc0aa03d5945019577f996053498c9a7c691b21a2678a622ac02";
+    version = "138.0.1";
+    sha512 = "2e71ee537292ec1a49237e93c43ed4c1a9eae58becfc7fa9ca0daf1e982c38704cb6d44e92b1bf7b45c5b8c27b23eb3aa7f48b375580f49ee60884dadc5d85b5";
 
     updateScript = callPackage ./update.nix {
       attrPath = "thunderbirdPackages.thunderbird-latest";
