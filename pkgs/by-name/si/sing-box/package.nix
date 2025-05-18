5d8c99d7280d612eee3ef6e04ce9b02ee75812ab
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-18 16:59:46 +0000

    sing-box: 1.11.10 -> 1.11.11
    (cherry picked from commit d17aba125e3a3d8bb04dddc08339ab1592e6a044)

--- pkgs/by-name/si/sing-box/package.nix
+++ pkgs/by-name/si/sing-box/package.nix
@@ -10,16 +10,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "sing-box";
-  version = "1.11.10";
+  version = "1.11.11";
 
   src = fetchFromGitHub {
     owner = "SagerNet";
     repo = "sing-box";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-GcoAxfFH/eP97E7tN//LbNCSbEASfzDL5OYHyL54eh8=";
+    hash = "sha256-hdYYjKBXnTqScYTUCfMmXozDD8GtIorLXnsU2Fmwg/c=";
   };
 
-  vendorHash = "sha256-7VfAFkdI4XFkMp2GJIzPFSXyUWNwT3o+EN5gI8U/wIQ=";
+  vendorHash = "sha256-/0pwsZbMbyAFXCrukbNf2RmQjQJ1E/ZUwzrC+5NEZcc=";
 
   tags = [
     "with_quic"
