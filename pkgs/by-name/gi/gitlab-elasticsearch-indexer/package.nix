816aa29cd6535fd97334b3ef5051d08d07b36044
Yaya <github@uwu.is>
2025-05-18 13:57:21 +0200

    gitlab: 17.11.2 -> 18.0.0
    https://gitlab.com/gitlab-org/gitlab/-/blob/v18.0.0-ee/CHANGELOG.md

--- pkgs/by-name/gi/gitlab-elasticsearch-indexer/package.nix
+++ pkgs/by-name/gi/gitlab-elasticsearch-indexer/package.nix
@@ -8,17 +8,17 @@
 
 buildGoModule rec {
   pname = "gitlab-elasticsearch-indexer";
-  version = "5.5.0";
+  version = "5.5.1";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-elasticsearch-indexer";
     rev = "v${version}";
-    hash = "sha256-b2kXp77pb9MXMRJsbDdNOXub8eZbZkHRwu/Ru0Voi60=";
+    hash = "sha256-N2H9jLpsP39nKrokWwphAspQwXcL3stAdvNVItIHFyo=";
   };
 
-  vendorHash = "sha256-9T8LbMROLcQYm9cT32Uc6Cuxwt9OYj3WzSYFgSQg1HQ=";
+  vendorHash = "sha256-Go02W09799Vu9v7y+P7z1gj7ijG3No5AVprRrmspPZE=";
 
   buildInputs = [ icu ];
   nativeBuildInputs = [ pkg-config ];
