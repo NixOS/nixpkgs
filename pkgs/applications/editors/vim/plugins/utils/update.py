e5f5af090fba6b1f70ed38cf2c9c4482d9a471d7
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-19 13:01:59 +0200

    vimPluginsUpdater: silence luaPackages error

--- pkgs/applications/editors/vim/plugins/utils/update.py
+++ pkgs/applications/editors/vim/plugins/utils/update.py
@@ -105,8 +105,9 @@ class VimEditor(pluginupdate.Editor):
     def plugin2nix(
         self, pdesc: PluginDesc, plugin: pluginupdate.Plugin, isNeovim: bool
     ) -> str:
-        if isNeovim:
-            raise RuntimeError(f"Plugin {plugin.name} is already packaged in `luaPackages`, please use that")
+        # re: https://github.com/NixOS/nixpkgs/issues/407318
+        # if isNeovim:
+        #     raise RuntimeError(f"Plugin {plugin.name} is already packaged in `luaPackages`, please use that")
         repo = pdesc.repo
 
         content = f"  {plugin.normalized_name} = "
