0d74bf610d5f8c8fc301a562d28be866e654f63e
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-05-19 08:38:01 +0200

    ocamlPackages.lwt_ppx: 2.1.0 → 5.9.1

--- pkgs/development/ocaml-modules/lwt/ppx.nix
+++ pkgs/development/ocaml-modules/lwt/ppx.nix
@@ -7,24 +7,7 @@
 
 buildDunePackage {
   pname = "lwt_ppx";
-  version = "2.1.0";
-  duneVersion = "3";
-
-  minimalOCamlVersion = "4.04";
-
-  # `lwt_ppx` has a different release cycle than Lwt, but it's included in
-  # one of its release bundles.
-  # Because there could exist an Lwt release _without_ a `lwt_ppx` release,
-  # this `src` field doesn't inherit from the Lwt derivation.
-  #
-  # This is particularly useful for overriding Lwt without breaking `lwt_ppx`,
-  # as new Lwt releases may contain broken `lwt_ppx` code.
-  src = fetchFromGitHub {
-    owner = "ocsigen";
-    repo = "lwt";
-    rev = "5.6.0";
-    hash = "sha256-DLQupCkZ14kOuSQatbb7j07I+jvvDCKpdlaR3rijT4s=";
-  };
+  inherit (lwt) version src;
 
   propagatedBuildInputs = [
     lwt
