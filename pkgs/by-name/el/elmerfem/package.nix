58354cfcc47ca5bf185fb01150056639deedb106
qbisi <qbisicwate@gmail.com>
2025-05-20 05:21:00 +0800

    elmerfem: unstable-2023-09-18 -> 9.0-unstable-2025-05-25

--- pkgs/by-name/el/elmerfem/package.nix
+++ pkgs/by-name/el/elmerfem/package.nix
@@ -14,19 +14,20 @@
   libsForQt5,
   tbb,
   vtkWithQt5,
+  llvmPackages,
 }:
 let
   opencascade-occt = opencascade-occt_7_6;
 in
 stdenv.mkDerivation rec {
   pname = "elmerfem";
-  version = "unstable-2023-09-18";
+  version = "9.0-unstable-2025-05-25";
 
   src = fetchFromGitHub {
     owner = "elmercsc";
     repo = "elmerfem";
-    rev = "0fcced06f91c93f44557efd6a5f10b2da5c7066c";
-    hash = "sha256-UuARDYW7D3a4dB6I86s2Ed5ecQxc+Y/es3YIeF2VyTc=";
+    rev = "2f7360ddf491c34f19fea9a723f340cca0fbe1d4";
+    hash = "sha256-2vzIFGh8+YrMxb5px6+aQyTerOAJmHOh2I7eterY6zI=";
   };
 
   hardeningDisable = [ "format" ];
@@ -50,7 +51,7 @@ stdenv.mkDerivation rec {
     opencascade-occt
     tbb
     vtkWithQt5
-  ];
+  ] ++ lib.optional stdenv.cc.isClang llvmPackages.openmp;
 
   preConfigure = ''
     patchShebangs ./
@@ -71,6 +72,7 @@ stdenv.mkDerivation rec {
     "-DCMAKE_INSTALL_LIBDIR=lib"
     "-DCMAKE_INSTALL_INCLUDEDIR=include"
     "-DCMAKE_OpenGL_GL_PREFERENCE=GLVND"
+    "-DUSE_MACOS_PACKAGE_MANAGER=False"
   ];
 
   meta = with lib; {
