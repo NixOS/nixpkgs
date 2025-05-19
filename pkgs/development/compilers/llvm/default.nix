99189289b479779e59df8b694b24df153915e2d0
Tristan Ross <tristan.ross@midstall.com>
2025-05-18 22:14:15 -0700

    llvmPackages_git: 21.0.0-unstable-2025-05-11 -> 21.0.0-unstable-2025-05-18
    (cherry picked from commit 8f55ad257761e92f05f42a4dcb10a31847c41c45)

--- pkgs/development/compilers/llvm/default.nix
+++ pkgs/development/compilers/llvm/default.nix
@@ -33,9 +33,9 @@ let
     "19.1.7".officialRelease.sha256 = "sha256-cZAB5vZjeTsXt9QHbP5xluWNQnAHByHtHnAhVDV0E6I=";
     "20.1.5".officialRelease.sha256 = "sha256-WKfY+VvAsZEEc0xYgF6+MsXDXZz7haMU6bxqmUpaHuQ=";
     "21.0.0-git".gitRelease = {
-      rev = "5b91756c0ca7ef4d75c33c2617bfd0f9719907dc";
-      rev-version = "21.0.0-unstable-2025-05-11";
-      sha256 = "sha256-5e72pOZO2/hYY7/1Kt0ITtEjJzWwKR58ufCU/9EkdS0=";
+      rev = "3d0c616ce30cdc3d9c26dda8fdc608a6c85f00a6";
+      rev-version = "21.0.0-unstable-2025-05-18";
+      sha256 = "sha256-5tfGng/urJNSqlFy5sxIYsR5brtf2ZlL5tssGD6Oef8=";
     };
   } // llvmVersions;
 
