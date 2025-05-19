d3e45988517a031c6b2d7e81ee42a115e3d74b58
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-05-19 02:32:01 +0200

    python313Packages.pyroaring: 1.0.0 -> 1.0.1
    https://github.com/Ezibenroc/PyRoaringBitMap/releases/tag/1.0.1

--- pkgs/development/python-modules/pyroaring/default.nix
+++ pkgs/development/python-modules/pyroaring/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "pyroaring";
-  version = "1.0.0";
+  version = "1.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Ezibenroc";
     repo = "PyRoaringBitMap";
     tag = version;
-    hash = "sha256-pnANvqyQ5DpG4NWSgWkAkXvSNLO67nfa97nEz3fYf1Y=";
+    hash = "sha256-Zs/MO1R4iBHDfTRVizMl6KyEWI6k2iDX7jAkBZs7kZE=";
   };
 
   build-system = [
