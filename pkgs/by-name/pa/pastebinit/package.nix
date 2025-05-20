2f385985c6f066f54ae8622a6f731fd121cc44c6
emaryn <emaryn@users.noreply.github.com>
2025-05-21 06:21:34 +0800

    pastebinit: 1.6.2 -> 1.7.1
    Diff: https://github.com/pastebinit/pastebinit/compare/1.6.2...1.7.1

--- pkgs/by-name/pa/pastebinit/package.nix
+++ pkgs/by-name/pa/pastebinit/package.nix
@@ -6,22 +6,23 @@
   libxslt,
   docbook_xsl,
   installShellFiles,
-  callPackage,
 }:
-stdenv.mkDerivation rec {
-  version = "1.6.2";
+
+stdenv.mkDerivation (finalAttrs: {
   pname = "pastebinit";
+  version = "1.7.1";
 
   src = fetchFromGitHub {
-    owner = pname;
-    repo = pname;
-    rev = version;
-    hash = "sha256-vuAWkHlQM6QTWarThpSbY0qrxzej0GvLU0jT2JOS/qc=";
+    owner = "pastebinit";
+    repo = "pastebinit";
+    tag = finalAttrs.version;
+    hash = "sha256-9cPo4yI0VEl1F5TB+Kl7qdpijnOVpuQm2WfsAd9bMSw=";
   };
 
-  patches = [
-    ./use-drv-etc.patch
-  ];
+  postPatch = ''
+    substituteInPlace pastebinit \
+      --replace-fail "['/usr/share', '/usr/local/share']" "['@etc@', '/usr/share', '/usr/local/share']"
+  '';
 
   nativeBuildInputs = [
     libxslt
@@ -33,10 +34,16 @@ stdenv.mkDerivation rec {
   ];
 
   buildPhase = ''
+    runHook preBuild
+
     xsltproc --nonet ${docbook_xsl}/xml/xsl/docbook/manpages/docbook.xsl pastebinit.xml
+
+    runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     mkdir -p $out/etc
     cp -a pastebinit $out/bin
@@ -44,16 +51,18 @@ stdenv.mkDerivation rec {
     cp -a pastebin.d $out/etc
     substituteInPlace $out/bin/pastebinit --subst-var-by "etc" "$out/etc"
     installManPage pastebinit.1
+
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://stgraber.org/category/pastebinit/";
     description = "Software that lets you send anything you want directly to a pastebin from the command line";
-    maintainers = with maintainers; [
+    maintainers = with lib.maintainers; [
       raboof
       samuel-martineau
     ];
-    license = licenses.gpl2Plus;
-    platforms = platforms.linux ++ lib.platforms.darwin;
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
   };
-}
+})
