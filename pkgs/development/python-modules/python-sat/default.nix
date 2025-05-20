a5a8e223666a97e6129fe10c7c7b4763ad47b79a
Christoph Jabs <contact@christophjabs.info>
2025-05-20 10:20:37 +0300

    python3Packages.python-sat: 0.1.7.dev1 -> 0.1.8.dev17

--- pkgs/development/python-modules/python-sat/default.nix
+++ pkgs/development/python-modules/python-sat/default.nix
@@ -6,17 +6,16 @@
   pypblib,
   pytestCheckHook,
 }:
-
 buildPythonPackage rec {
   pname = "python-sat";
-  version = "0.1.7.dev1";
+  version = "0.1.8.dev17";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "pysathq";
     repo = "pysat";
-    rev = version;
-    hash = "sha256-zGdgD+SgoMB7/zDQI/trmV70l91TB7OkDxaJ30W3dkI=";
+    rev = "a04763de6dafb8d3a0d7f1b231fc0d30be1de4c0"; # upstream does not tag releases
+    hash = "sha256-FG6oAAI8XKXumj6Ys2QjjYcRp1TpwkUZzyfpkdq5V6E=";
   };
 
   propagatedBuildInputs = [
@@ -26,23 +25,18 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [ pytestCheckHook ];
 
-  # https://github.com/pysathq/pysat/pull/102
-  postPatch = ''
-        # Fix for case-insensitive filesystem
-        cat >>solvers/patches/cadical.patch <<EOF
-    diff --git solvers/cadical/VERSION solvers/cdc/VERSION
-    deleted file mode 100644
-    --- solvers/cadical/VERSION
-    +++ /dev/null
-    @@ -1 +0,0 @@
-    -1.0.3
-    EOF
-  '';
+  disabledTestPaths = [ "tests/test_unique_mus.py" ];
 
   meta = with lib; {
     description = "Toolkit to provide interface for various SAT (without optional dependancy py-aiger-cnf)";
     homepage = "https://github.com/pysathq/pysat";
+    changelog = "https://pysathq.github.io/updates/";
     license = licenses.mit;
-    maintainers = [ maintainers.marius851000 ];
+    maintainers = [
+      maintainers.marius851000
+      maintainers.chrjabs
+    ];
+    platforms = lib.platforms.all;
+    badPlatforms = lib.platforms.darwin ++ [ "i686-linux" ];
   };
 }
