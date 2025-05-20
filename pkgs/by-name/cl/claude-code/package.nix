17424b113500efad8686ae094cf97392de2201bf
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-20 03:25:04 +0000

    claude-code: 0.2.109 -> 0.2.122
    (cherry picked from commit 0a34ae39914ed02556daecc3b212b83f26708329)

--- pkgs/by-name/cl/claude-code/package.nix
+++ pkgs/by-name/cl/claude-code/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "0.2.109";
+  version = "0.2.122";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-XANo3GKiD5n66GZrB+gZ15o2zTAOKkjcFNbGE3TT1NA=";
+    hash = "sha256-9w1wjuOTSphEfgKfNJhDFiTKuIJqfV6Bz4XEYXMIaGE=";
   };
 
-  npmDepsHash = "sha256-u5AZXNlN/NAag+35uz3rzLh6ItbKAdV8RSSjzCGk6uA=";
+  npmDepsHash = "sha256-71YPsgeI8bmb+wwFHS5XOXKJXsagWZj+hBf2T/+GYMU=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
