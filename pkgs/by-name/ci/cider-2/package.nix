2c149f37c8989f2f82157d11ede1d4288177179c
lorev <lorenzopasqui@gmail.com>
2025-05-20 18:06:33 +0200

    cider-2: 2.6.1 -> 3.0.2

--- pkgs/by-name/ci/cider-2/package.nix
+++ pkgs/by-name/ci/cider-2/package.nix
@@ -7,12 +7,12 @@
 
 appimageTools.wrapType2 rec {
   pname = "cider-2";
-  version = "2.6.1";
+  version = "3.0.2";
 
   src = requireFile {
     name = "cider-linux-x64.AppImage";
     url = "https://cidercollective.itch.io/cider";
-    sha256 = "0qjhsssccxiq92zs04zhi53bkaf2qwfq7ryic1w9sha59ffyxqbf";
+    sha256 = "1rfraf1r1zmp163kn8qg833qxrxmx1m1hycw8q9hc94d0hr62l2x";
   };
 
   nativeBuildInputs = [ makeWrapper ];
