e333ab64e23ed381110e5b1822bffb8968333b03
misilelab <misileminecord@gmail.com>
2025-05-20 11:11:07 +0900

    docker-compose: 2.36.0 -> 2.37.1
    https://github.com/docker/compose/releases/tag/v2.36.1
    https://github.com/docker/compose/releases/tag/v2.36.2
    https://github.com/docker/compose/releases/tag/v2.37.0
    https://github.com/docker/compose/releases/tag/v2.37.1
    
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- pkgs/applications/virtualization/docker/compose.nix
+++ pkgs/applications/virtualization/docker/compose.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "docker-compose";
-  version = "2.36.0";
+  version = "2.37.1";
 
   src = fetchFromGitHub {
     owner = "docker";
     repo = "compose";
     rev = "v${version}";
-    hash = "sha256-3WaTUylmXk2urn9Pe76AdnckJvFBJBSccXCvFSRotyo=";
+    hash = "sha256-jz5ve9VzmsRMv05fGrxSUUg0mGRbtQzpWrs4jyAnubI=";
   };
 
   postPatch = ''
@@ -20,7 +20,7 @@ buildGoModule rec {
     rm -rf pkg/e2e/
   '';
 
-  vendorHash = "sha256-AcBBtetOuTnCA6ZQVUta+6lJQoSJhBXifEoToUhrpAk=";
+  vendorHash = "sha256-i23na2J4XTsHFZZO5ASY7XVsd/rOZPkAMEH0HWzr+0s=";
 
   ldflags = [
     "-X github.com/docker/compose/v2/internal.Version=${version}"
