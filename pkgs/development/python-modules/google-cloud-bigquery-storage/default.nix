aa71c0a6c73bd19ab93364c4baf6520494c613d5
Sarah Clark <seclark@nextquestion.net>
2025-05-21 15:46:49 -0700

    python3Packages.google-cloud-bigquery-storage: build from GitHub

--- pkgs/development/python-modules/google-cloud-bigquery-storage/default.nix
+++ pkgs/development/python-modules/google-cloud-bigquery-storage/default.nix
@@ -2,7 +2,7 @@
   lib,
   buildPythonPackage,
   fastavro,
-  fetchPypi,
+  fetchFromGitHub,
   google-api-core,
   google-auth,
   google-cloud-bigquery,
@@ -10,7 +10,6 @@
   protobuf,
   pyarrow,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
@@ -19,12 +18,11 @@ buildPythonPackage rec {
   version = "2.30.0";
   pyproject = true;
 
-  disabled = pythonOlder "3.7";
-
-  src = fetchPypi {
-    pname = "google_cloud_bigquery_storage";
-    inherit version;
-    hash = "sha256-QayD+p7dvIIBAhd5hKuS+Le736fZDqZLOgr17LT8o/I=";
+  src = fetchFromGitHub {
+    owner = "googleapis";
+    repo = "python-bigquery-storage";
+    tag = "v${version}";
+    hash = "sha256-Wu2+6i4D3pye+pYjOVnNRBJcOd11GyHjJmiXWaT8zjw=";
   };
 
   build-system = [ setuptools ];
@@ -59,12 +57,13 @@ buildPythonPackage rec {
     "google.cloud.bigquery_storage_v1beta2"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "BigQuery Storage API API client library";
     homepage = "https://github.com/googleapis/python-bigquery-storage";
     changelog = "https://github.com/googleapis/python-bigquery-storage/blob/v${version}/CHANGELOG.md";
-    license = licenses.asl20;
-    maintainers = [ ];
+
+    license = lib.licenses.asl20;
+    maintainers = [ lib.maintainers.sarahec ];
     mainProgram = "fixup_bigquery_storage_v1_keywords.py";
   };
 }
