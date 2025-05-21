c7e36d4abf572cc44388841a48603d331f97bf78
phanirithvij <phanirithvij2000@gmail.com>
2025-05-21 20:01:35 +0530

    dprint-plugin-typescript: 0.95.1 -> 0.95.3
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/dp/dprint/plugins/dprint-plugin-typescript.nix
+++ pkgs/by-name/dp/dprint/plugins/dprint-plugin-typescript.nix
@@ -1,7 +1,7 @@
 { mkDprintPlugin }:
 mkDprintPlugin {
   description = "TypeScript/JavaScript code formatter.";
-  hash = "sha256-IHbpEwTATwAmVa/ihGjQzQL/WwWo8owslJtWYUjVF+g=";
+  hash = "sha256-0uLA+z+ErScA76g7OqQLY9y8qYFqW8kPqoXITn62Ksg=";
   initConfig = {
     configExcludes = [ "**/node_modules" ];
     configKey = "typescript";
@@ -16,6 +16,6 @@ mkDprintPlugin {
   };
   pname = "dprint-plugin-typescript";
   updateUrl = "https://plugins.dprint.dev/dprint/typescript/latest.json";
-  url = "https://plugins.dprint.dev/typescript-0.95.1.wasm";
-  version = "0.95.1";
+  url = "https://plugins.dprint.dev/typescript-0.95.3.wasm";
+  version = "0.95.3";
 }
