431decfef1847725658ea124fe94329e59d7128f
programmerlexi <lexi@reyer.name>
2025-05-21 21:13:26 +0200

    nixos/limine: add test for specialisations

---
+++ nixos/tests/limine/specialisations.nix
@@ -0,0 +1,28 @@
+{ lib, ... }:
+{
+  name = "specialisations";
+  meta.maintainers = with lib.maintainers; [
+    lzcunt
+    phip1611
+    programmerlexi
+  ];
+  nodes.machine =
+    { ... }:
+    {
+      virtualisation.useBootLoader = true;
+      virtualisation.useEFIBoot = true;
+
+      specialisation.test = { };
+
+      boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.limine.enable = true;
+      boot.loader.limine.efiSupport = true;
+      boot.loader.timeout = 0;
+    };
+
+  testScript = ''
+    machine.start()
+    with subtest('Machine boots correctly'):
+      machine.wait_for_unit('multi-user.target')
+  '';
+}
