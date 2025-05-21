90d3bd212979e47f00a7614ec408b046199c7ce7
adamjhf <git@freeth.dev>
2025-05-21 11:56:28 +0000

    cargo-leptos: 0.2.28 -> 0.2.35
    cargo-leptos: 0.2.28 -> 0.2.35
    
    cargo-leptos: fix darwin

--- pkgs/by-name/ca/cargo-leptos/package.nix
+++ pkgs/by-name/ca/cargo-leptos/package.nix
@@ -1,25 +1,34 @@
 {
   fetchFromGitHub,
   lib,
+  stdenv,
   rustPlatform,
+  perl,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cargo-leptos";
-  version = "0.2.28";
+  version = "0.2.35";
 
   src = fetchFromGitHub {
     owner = "leptos-rs";
     repo = "cargo-leptos";
     rev = "v${version}";
-    hash = "sha256-SjpfM963Zux+H5QhK8prvDLuI56fP5PqX5gcVbthRx4=";
+    hash = "sha256-CNktytEm6+5QTPAlxNz07+s7gue9dA5zZM82YQOWFSw=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-Da9ei4yAOfhSQmQgrUDZCmMeJXTfGnYhI1+L0JT/ECs=";
+  cargoHash = "sha256-O/JyB47UP6rLeAG1rt1dXhaKfZ71QPg7qAeciHAvrAk=";
 
   # https://github.com/leptos-rs/cargo-leptos#dependencies
   buildFeatures = [ "no_downloads" ]; # cargo-leptos will try to install missing dependencies on its own otherwise
   doCheck = false; # Check phase tries to query crates.io
+
+  nativeBuildInputs = [ perl ]; # Compile vendored-openssl
+
+  # Fix for C++ compiler version on darwin for wasm-opt
+  preBuild = lib.optionalString stdenv.isDarwin ''
+    export CRATE_CC_NO_DEFAULTS=1
+  '';
 
   meta = with lib; {
     description = "Build tool for the Leptos web framework";
