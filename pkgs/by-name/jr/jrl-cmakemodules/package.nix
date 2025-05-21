216cc3d03911bdcf2f284f485532cde9f7cbef12
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-21 15:42:27 +0200

    jrl-cmakemodules: 0-unstable-2025-01-29 -> 0-unstable-2025-05-21

--- pkgs/by-name/jr/jrl-cmakemodules/package.nix
+++ pkgs/by-name/jr/jrl-cmakemodules/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation {
   pname = "jrl-cmakemodules";
-  version = "0-unstable-2025-01-29";
+  version = "0-unstable-2025-05-21";
 
   src = fetchFromGitHub {
     owner = "jrl-umi3218";
     repo = "jrl-cmakemodules";
-    rev = "2ede15d1cb9d66401ba96788444ad64c44ffccd8";
-    hash = "sha256-0o5DKt9BxZlAYTHp/BjzF6eJRP/d6lVlaV5P4xlzKnA=";
+    rev = "3632eb9ed43e29d7acbc5b35d30c9b2aee801702";
+    hash = "sha256-0d6l5gg829b10glmygs56f0ag6dq8jqj3bfiq16j41rb9l2p6fal";
   };
 
   nativeBuildInputs = [ cmake ];
