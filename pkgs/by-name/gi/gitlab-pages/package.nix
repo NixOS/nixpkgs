c20fc9e97d7679fea65855a0465027968378e200
Leona Maroni <dev@leona.is>
2025-05-21 22:10:11 +0200

    gitlab: 18.0.0 -> 18.0.1
    https://about.gitlab.com/releases/2025/05/21/patch-release-gitlab-18-0-1-released/
    (cherry picked from commit 3b258c1272ce786f33c78a7f4b68be54e17e8c1f)

--- pkgs/by-name/gi/gitlab-pages/package.nix
+++ pkgs/by-name/gi/gitlab-pages/package.nix
@@ -6,14 +6,14 @@
 
 buildGoModule rec {
   pname = "gitlab-pages";
-  version = "18.0.0";
+  version = "18.0.1";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-pages";
     rev = "v${version}";
-    hash = "sha256-c2y11lMeHBi62Ql63HGolF454GkFp7UCErHGk5Bvgvw=";
+    hash = "sha256-EslNXyCzGpsJAG3SOQF56xbU2vhVVo4qdtfFtf9qqW0=";
   };
 
   vendorHash = "sha256-BjCwPt1duDINHP7L0qT2KNTjOZ62bWgVij88ztjjyPg=";
