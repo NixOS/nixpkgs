8b57454f106c09762a90a2e2d3ca3345db0ad02c
soyouzpanda <soyouzpanda@soyouzpanda.fr>
2025-05-21 16:24:40 +0200

    python3Packages.django-ltree: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4246,6 +4246,8 @@ self: super: with self; {
     callPackage ../development/python-modules/django-login-required-middleware
       { };
 
+  django-ltree = callPackage ../development/python-modules/django-ltree { };
+
   django-mailbox = callPackage ../development/python-modules/django-mailbox { };
 
   django-mailman3 = callPackage ../development/python-modules/django-mailman3 { };
