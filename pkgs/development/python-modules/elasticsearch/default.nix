69b494debf5e34154be71f1a58f4b02225b65464
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-05-21 19:00:59 +0200

    Revert "python313Packages.elasticsearch: 8.17.2 -> 9.0.1"
    This reverts commit 123add8e35e30a3c2cda29eb3a65bfd9efbca324.
    
    Breaks elasticsearch-dsl, which has not support for the 9.0 series.

--- pkgs/development/python-modules/elasticsearch/default.nix
+++ pkgs/development/python-modules/elasticsearch/default.nix
@@ -7,31 +7,25 @@
   hatchling,
   orjson,
   pyarrow,
-  python-dateutil,
   pythonOlder,
   requests,
-  typing-extensions,
 }:
 
 buildPythonPackage rec {
   pname = "elasticsearch";
-  version = "9.0.1";
+  version = "8.17.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-dvm1Gc/RWoYPYVqUupDcuVQ7chMGvFFE7MFfC01dJ4E=";
+    hash = "sha256-/38duK7v2HzrpO3OOqQHCZRYLmzwKdLme3TmbWNFCds=";
   };
 
   build-system = [ hatchling ];
 
-  dependencies = [
-    elastic-transport
-    python-dateutil
-    typing-extensions
-  ];
+  dependencies = [ elastic-transport ];
 
   optional-dependencies = {
     requests = [ requests ];
