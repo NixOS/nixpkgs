ea0669797328c66ed0e515c99991312aa472179f
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-21 13:32:03 +0000

    optipng: 0.7.8 -> 7.9.1

--- pkgs/by-name/op/optipng/package.nix
+++ pkgs/by-name/op/optipng/package.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation rec {
   pname = "optipng";
-  version = "0.7.8";
+  version = "7.9.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/optipng/optipng-${version}.tar.gz";
-    hash = "sha256-JaO9aEgfIVAsyqD0wT+E3PayAzjkxOjFHyzvvYUTOYw=";
+    hash = "sha256-wleb5YwsZtrp1jFU7cs9Qn/vZMsA7Ar/B5ydFW7Ebyk=";
   };
 
   buildInputs = [ libpng ];
