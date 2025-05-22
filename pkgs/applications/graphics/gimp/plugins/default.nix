085d1e858bdafa8d1de864e6bf046599107cdbf4
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-05-23 01:43:49 +0200

    gimp3Plugins.resynthesizer3: init at 3.0

--- pkgs/applications/graphics/gimp/plugins/default.nix
+++ pkgs/applications/graphics/gimp/plugins/default.nix
@@ -240,6 +240,35 @@ lib.makeScope pkgs.newScope (
       };
     };
 
+    resynthesizer3 = pluginDerivation rec {
+      /*
+        menu:
+        Edit/Fill with Pattern Seamless...
+        Filters/Enhance/Heal Selection...
+        Filters/Enhance/Heal Transparency...
+        Filters/Enhance/Uncrop...
+        Filters/Map/Resynthesize...
+        Filters/Map/Style...
+        Filters/Render/Texture...
+      */
+      pname = "resynthesizer";
+      version = "3.0";
+      nativeBuildInputs = with pkgs; [
+        meson
+        ninja
+      ];
+      src = fetchFromGitHub {
+        owner = "bootchk";
+        repo = "resynthesizer";
+        rev = "v${version}";
+        hash = "sha256-/Py5R1RxiftTR0z++mQzgTn/J9v4p8efuGZSfhe6FfA=";
+      };
+
+      meta = {
+        broken = gimp.majorVersion != "3.0";
+      };
+    };
+
     texturize = pluginDerivation {
       pname = "texturize";
       version = "2.2+unstable=2021-12-03";
