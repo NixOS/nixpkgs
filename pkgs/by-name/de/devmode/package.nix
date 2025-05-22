a0a169ff48e17c607787baf29de76f20c5519ce2
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-22 15:05:53 +0200

    devmode: use writeShellApplication

--- pkgs/by-name/de/devmode/package.nix
+++ pkgs/by-name/de/devmode/package.nix
@@ -1,93 +1,111 @@
 {
-  lib,
   live-server,
   parallel,
   watchexec,
-  writeShellScriptBin,
+  writeShellApplication,
   # arguments to `nix-build`, e.g. `"foo.nix -A bar"`
   buildArgs ? "",
   # what path to open a browser at
   open ? "/index.html",
 }:
 let
-  error-page = writeShellScriptBin "error-page" ''
-    rm -rf $serve
-    mkdir -p "$(dirname $error_page_absolute)"
+  error-page = writeShellApplication {
+    name = "error-page";
+    text = ''
+      rm -rf "''${serve:?}"
+      mkdir -p "$(dirname "''${error_page_absolute:?}")"
 
-    cat > $error_page_absolute << EOF
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <style>
-        @media (prefers-color-scheme: dark) {
-          :root { filter: invert(100%); }
-        }
-      </style>
-    </head>
-    <body><pre>$1</pre></body>
-    </html>
-    EOF
-  '';
+      cat > "''${error_page_absolute:?}" << EOF
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style>
+          @media (prefers-color-scheme: dark) {
+            :root { filter: invert(100%); }
+          }
+        </style>
+      </head>
+      <body><pre>$1</pre></body>
+      </html>
+      EOF
+    '';
+  };
 
-  build-and-link = writeShellScriptBin "build-and-link" ''
-    set -euxo pipefail
+  build-and-link = writeShellApplication {
+    name = "build-and-link";
+    runtimeInputs = [ error-page ];
+    text = ''
+      set +e
+      stderr=$(2>&1 nix-build --out-link "''${staging:?}" ${buildArgs})
+      exit_status=$?
+      set -e
 
-    set +e
-    stderr=$(2>&1 nix-build --out-link $staging ${buildArgs})
-    exit_status=$?
-    set -e
+      rm -rf "''${serve:?}"
 
-    rm -rf $serve
-
-    if [ $exit_status -eq 0 ]; then
-      mv $staging $serve
-    else
-      ${lib.getExe error-page} "$stderr"
-    fi
-  '';
+      if [ $exit_status -eq 0 ]; then
+        mv "''${staging:?}" "''${serve:?}"
+      else
+        error-page "$stderr"
+      fi
+    '';
+  };
 
   # https://watchexec.github.io/
-  watcher = writeShellScriptBin "watcher" ''
-    set -euxo pipefail
-
-    ${lib.getExe watchexec} \
-      --shell=none \
-      --restart \
-      --print-events \
-      ${lib.getExe build-and-link}
-  '';
+  watcher = writeShellApplication {
+    name = "watcher";
+    runtimeInputs = [
+      watchexec
+      build-and-link
+    ];
+    text = ''
+      watchexec \
+        --shell=none \
+        --restart \
+        --print-events \
+        build-and-link
+    '';
+  };
 
   # https://crates.io/crates/live-server
-  server = writeShellScriptBin "server" ''
-    set -euxo pipefail
-
-    ${lib.getExe live-server} \
-      --host=127.0.0.1 \
-      --open=${open} \
-      $serve
-  '';
+  server = writeShellApplication {
+    name = "server";
+    runtimeInputs = [ live-server ];
+    text = ''
+      live-server \
+        --host=127.0.0.1 \
+        --open=${open} \
+        "''${serve:?}"
+    '';
+  };
 in
-writeShellScriptBin "devmode" ''
-  set -euxo pipefail
+writeShellApplication {
+  name = "devmode";
+  runtimeInputs = [
+    parallel
+    watcher
+    server
+    error-page
+  ];
+  text = ''
+    function handle_exit {
+      rm -rf "$tmpdir"
+    }
 
-  function handle_exit {
-    rm -rf "$tmpdir"
-  }
+    tmpdir="$(mktemp -d)"
+    trap handle_exit EXIT
 
-  tmpdir=$(mktemp -d)
-  trap handle_exit EXIT
+    export serve="$tmpdir/serve"
+    export staging="$tmpdir/staging"
 
-  export serve="$tmpdir/serve"
-  export staging="$tmpdir/staging"
-  export error_page_absolute="$serve/${open}"
+    export error_page_absolute="$serve/${open}"
+    error-page "building …"
 
-  ${lib.getExe error-page} "building …"
-
-  ${lib.getExe parallel} \
-    --will-cite \
-    --line-buffer \
-    --tagstr '{/}' \
-    ::: \
-    "${lib.getExe watcher}" \
-    "${lib.getExe server}"
-''
+    parallel \
+      --will-cite \
+      --line-buffer \
+      --tagstr '{/}' \
+      ::: \
+      watcher \
+      server
+  '';
+}
