NixOS/nixpkgs

commit dd0f03a56cf2be99f0904ff1f64e1cc5fb201d43

treewide: remove usage of deprecated apple_sdk framework stubs
They are not doing anything right now. This is in preparation for their
complete removal from the tree.

Note: several changes that affect the derivation inputs (e.g. removal of
references to stub paths in build instructions) were left out. They will
be cleaned up the next iteration and will require special care.

Note: this PR is a result of a mix of ugly regex (not AST) based
automation and some manual labor. For reference, the regex automation
part was hacked in: https://github.com/booxter/nix-clean-apple_sdk

Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

pkgs/development/compilers/dotnet/wrapper.nix
@@ -12,7 +12,6 @@
   callPackage,
   zlib,
   swiftPackages,
-  darwin,
   icu,
   lndir,
   replaceVars,
@@ -222,17 +221,9 @@ stdenvNoCC.mkDerivation (finalAttrs: {
                 [
                   zlib
                 ]
-                ++ lib.optional stdenv.hostPlatform.isDarwin (
-                  with darwin;
-                  with apple_sdk.frameworks;
-                  [
-                    swiftPackages.swift
-                    Foundation
-                    CryptoKit
-                    GSS
-                    ICU
-                  ]
-                );
+                ++ lib.optional stdenv.hostPlatform.isDarwin [
+                  swiftPackages.swift
+                ];
               build = ''
                 dotnet restore -p:PublishAot=true
                 dotnet publish -p:PublishAot=true -o $out/bin

commit b0fd9b063816d42247769799a18335d6b22c0daa

dotnet: add darwin.ICU dependency to AOT test
This dependency seems to have been removed accidentally along with the
framework dependencies in #398707, so the test has been broken since
then.

Fixes: dd0f03a56cf2be99f0904ff1f64e1cc5fb201d43

pkgs/development/compilers/dotnet/wrapper.nix
@@ -6,6 +6,7 @@
   testers,
   runCommand,
   runCommandWith,
+  darwin,
   expect,
   curl,
   installShellFiles,
@@ -223,6 +224,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
                 ]
                 ++ lib.optional stdenv.hostPlatform.isDarwin [
                   swiftPackages.swift
+                  darwin.ICU
                 ];
               build = ''
                 dotnet restore -p:PublishAot=true