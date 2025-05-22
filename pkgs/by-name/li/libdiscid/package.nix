e606674bfa4c9f9146439248bc058137ee2bccbe
Anton Mosich <anton@mosich.at>
2025-05-22 12:19:35 +0200

    libdiscid: modernize
    The change to fetchFromGitHub necessitates a hash change, as the
    tarballs being distributed are not equal to the content of the git
    repository at the tag[1].
    
    [1]: https://github.com/metabrainz/libdiscid/blob/v0.6.5/CMakeLists.txt#L172-L178

--- pkgs/by-name/li/libdiscid/package.nix
+++ pkgs/by-name/li/libdiscid/package.nix
@@ -1,12 +1,13 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
   cmake,
   pkg-config,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libdiscid";
   version = "0.6.4";
 
@@ -15,18 +16,22 @@ stdenv.mkDerivation rec {
     pkg-config
   ];
 
-  src = fetchurl {
-    url = "http://ftp.musicbrainz.org/pub/musicbrainz/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "sha256-3V6PHJrq1ELiO3SanMkzY3LmLoitcHmitiiVsDkMsoI=";
+  src = fetchFromGitHub {
+    owner = "metabrainz";
+    repo = "libdiscid";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-oN/qpzdTa5+rD7kwDOW6UCg7bbDOy/AYmP8sv9Q8+Kk=";
   };
 
   NIX_LDFLAGS = lib.optionalString stdenv.hostPlatform.isDarwin "-framework CoreFoundation -framework IOKit";
 
+  passthru.updateScript = nix-update-script { };
+
   meta = with lib; {
     description = "C library for creating MusicBrainz DiscIDs from audio CDs";
-    homepage = "http://musicbrainz.org/doc/libdiscid";
+    homepage = "https://musicbrainz.org/doc/libdiscid";
     maintainers = with maintainers; [ ehmry ];
     license = licenses.lgpl21;
     platforms = platforms.all;
   };
-}
+})
