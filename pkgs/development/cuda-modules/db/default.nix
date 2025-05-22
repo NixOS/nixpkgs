a9309154716305944fc58ebed2dd885a1d9d2644
SomeoneSerge <else@someonex.net>
2025-05-22 01:15:16 +0000

    tests.cuda.db: we only ever model tensorrt using releases.nix

--- pkgs/development/cuda-modules/db/default.nix
+++ pkgs/development/cuda-modules/db/default.nix
@@ -19,23 +19,20 @@ let
       lib.optionals (lib.pathIsDirectory manifestsDir) manifests
     ) (builtins.attrNames (builtins.readDir root));
   jsonToModule = import ./json.nix { inherit lib; };
-  parseReleasesFile = import ./release_file.nix { inherit lib; };
+  ingestTensorrt = import ./ingest_tensorrt.nix { inherit lib; };
   modulesPath = ../../../../.;
 in
 {
   manifests ? intreeManifests, # :: List Path
-  releaseFiles ? [ ../tensorrt/releases.nix ],
-  extraModules ? [ ],
+  extraModules ? builtins.map ingestTensorrt [ ../tensorrt/releases.nix ],
 }:
 let
   manifestModules = builtins.map jsonToModule manifests;
-  releasesModules = builtins.map parseReleasesFile releaseFiles;
   evaluated =
     evalModules {
       specialArgs = { inherit modulesPath; };
       modules =
         extraModules
-        ++ releasesModules
         ++ manifestModules
         ++ [
           ./schema.nix
@@ -44,9 +41,14 @@ let
     // {
       inherit intreeManifests;
     };
-  errors = builtins.concatMap ({ assertion, message }: lib.optionals (!assertion) [message]) evaluated.config.assertions;
+  errors = builtins.concatMap (
+    { assertion, message }: lib.optionals (!assertion) [ message ]
+  ) evaluated.config.assertions;
   error = if errors == [ ] then null else lib.concatStringsSep "\n" errors;
 in
-evaluated // {
-  validConfig = assert lib.assertMsg (errors == [ ]) error; evaluated.config;
+evaluated
+// {
+  validConfig =
+    assert lib.assertMsg (errors == [ ]) error;
+    evaluated.config;
 }
