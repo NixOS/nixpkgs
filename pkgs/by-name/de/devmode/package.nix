f3c72c34f198631b84c513f799ef00f3aa192285
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-22 15:05:53 +0200

    devmode: use writeShellApplication

--- pkgs/by-name/de/devmode/package.nix
+++ pkgs/by-name/de/devmode/package.nix
@@ -1,63 +1,84 @@
 {
-  lib,
   live-server,
   parallel,
   watchexec,
-  writeShellScriptBin,
+  writeShellApplication,
   # arguments to `nix-build`, e.g. `"foo.nix -A bar"`
   buildArgs ? "",
   # what path to open a browser at
   open ? "/index.html",
 }:
 let
-  build-and-link = writeShellScriptBin "build-and-link" ''
-    set -euxo pipefail
+  build-and-link = writeShellApplication {
+    name = "build-and-link";
+    text = ''
+      set -euxo pipefail
 
-    out_link="$(nix-build --no-out-link ${buildArgs})"
-    rm -rf $serve
-    ln -sf $out_link $serve
-  '';
+      out_link="$(nix-build --no-out-link ${buildArgs})"
+      rm -rf "''${serve:?}"
+      ln -sf "''${out_link:?}" "''${serve:?}"
+    '';
+  };
 
   # https://watchexec.github.io/
-  watcher = writeShellScriptBin "watcher" ''
-    set -euxo pipefail
+  watcher = writeShellApplication {
+    name = "watcher";
+    runtimeInputs = [
+      watchexec
+      build-and-link
+    ];
+    text = ''
+      set -euxo pipefail
 
-    ${lib.getExe watchexec} \
-      --shell=none \
-      --restart \
-      --print-events \
-      ${lib.getExe build-and-link}
-  '';
+      watchexec \
+        --shell=none \
+        --restart \
+        --print-events \
+        build-and-link
+    '';
+  };
 
   # https://crates.io/crates/live-server
-  server = writeShellScriptBin "server" ''
+  server = writeShellApplication {
+    name = "server";
+    runtimeInputs = [ live-server ];
+    text = ''
+      set -euxo pipefail
+
+      live-server \
+        --host=127.0.0.1 \
+        --open=${open} \
+        "''${serve:?}"
+    '';
+  };
+in
+writeShellApplication {
+  name = "devmode";
+  runtimeInputs = [
+    parallel
+    watcher
+    server
+  ];
+  text = ''
     set -euxo pipefail
 
-    ${lib.getExe live-server} \
-      --host=127.0.0.1 \
-      --open=${open} \
-      $serve
+    function handle_exit {
+      rm -rf "$tmpdir"
+    }
+
+    tmpdir="$(mktemp -d)"
+    trap handle_exit EXIT
+
+    export serve="$tmpdir/serve"
+
+    mkdir "$serve"
+
+    parallel \
+      --will-cite \
+      --line-buffer \
+      --tagstr '{/}' \
+      ::: \
+      watcher \
+      server
   '';
-in
-writeShellScriptBin "devmode" ''
-  set -euxo pipefail
-
-  function handle_exit {
-    rm -rf "$tmpdir"
-  }
-
-  tmpdir=$(mktemp -d)
-  trap handle_exit EXIT
-
-  export serve="$tmpdir/serve"
-
-  mkdir $serve
-
-  ${lib.getExe parallel} \
-    --will-cite \
-    --line-buffer \
-    --tagstr '{/}' \
-    ::: \
-    "${lib.getExe watcher}" \
-    "${lib.getExe server}"
-''
+}
