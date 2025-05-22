5b53db3dba24641cb8a5ab23ff2c5355967471e2
Sarah Clark <seclark@nextquestion.net>
2025-05-21 17:04:34 -0700

    python3Packages.google-cloud-iam: 2.18.3 -> 2.19.0
    changelog: https://github.com/googleapis/google-cloud-python/releases/tag/google-cloud-iam-v2.19.0
    
    diff: https://github.com/googleapis/google-cloud-python/compare/google-cloud-iam-v2.18.3...google-cloud-iam-v2.19.0

--- pkgs/development/python-modules/google-cloud-iam/default.nix
+++ pkgs/development/python-modules/google-cloud-iam/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-iam";
-  version = "2.18.3";
+  version = "2.19.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "googleapis";
     repo = "google-cloud-python";
     tag = "google-cloud-iam-v${version}";
-    sha256 = "sha256-om/VLyv9jE64uEc0960MavzqAhBrLBvmLNeIFsniaAE=";
+    hash = "sha256-E1LISOLQcXqUMTTPLR+lwkR6gF1fuGGB44j38cIK/Z4=";
   };
 
   sourceRoot = "${src.name}/packages/google-cloud-iam";
