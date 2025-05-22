d79c79308aee9882c30e10777f3f46a28e49e4c8
Gwenn Le Bihan <hey@ewen.works>
2025-05-22 14:49:07 +0200

    python3Packages.xacro: init at 2.0.13

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19170,6 +19170,8 @@ self: super: with self; {
 
   x690 = callPackage ../development/python-modules/x690 { };
 
+  xacro = callPackage ../development/python-modules/xacro { };
+
   xapian = callPackage ../development/python-modules/xapian { inherit (pkgs) xapian; };
 
   xarray = callPackage ../development/python-modules/xarray { };
