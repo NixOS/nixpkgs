6c20afc2637f040df1b525321f52541bb8584b50
Benjamin Große <ste3ls@gmail.com>
2025-05-23 15:26:38 +0100

    maintainers: add benjajaja

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3060,6 +3060,12 @@
     github = "benhiemer";
     githubId = 16649926;
   };
+  benjajaja = {
+    name = "Benjamin Große";
+    email = "ste3ls@gmail.com";
+    github = "benjajaja";
+    githubId = 310215;
+  };
   benjaminedwardwebb = {
     name = "Ben Webb";
     email = "benjaminedwardwebb@gmail.com";
