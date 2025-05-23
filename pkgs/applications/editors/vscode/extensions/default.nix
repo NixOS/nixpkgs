5d25809917b24ae4e6bc7d0fa87d9024cc35e941
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-05-23 23:20:24 +0530

    vscode-extensions.nhoizey.gremlins: move them to a different file
    inspired by https://github.com/NixOS/nixpkgs/pull/402784#pullrequestreview-2822373244
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -3752,22 +3752,7 @@ let
         };
       };
 
-      nhoizey.gremlins = buildVscodeMarketplaceExtension {
-        mktplcRef = {
-          name = "gremlins";
-          publisher = "nhoizey";
-          version = "0.26.0";
-          hash = "sha256-ML04SccSOrj5qY0HHJ5jiNbWkPElU1+zZNSX2i1K2uk=";
-        };
-        meta = {
-          changelog = "https://marketplace.visualstudio.com/items/nhoizey.gremlins/changelog";
-          description = "Reveals some characters that can be harmful because they are invisible or looking like legitimate ones";
-          downloadPage = "https://marketplace.visualstudio.com/items?itemName=nhoizey.gremlins";
-          homepage = "https://github.com/nhoizey/vscode-gremlins";
-          license = lib.licenses.mit;
-          maintainers = [ lib.maintainers.theobori ];
-        };
-      };
+      nhoizey.gremlins = callPackage ./nhoizey.gremlins { };
 
       nimlang.nimlang = callPackage ./nimlang.nimlang { };
 
