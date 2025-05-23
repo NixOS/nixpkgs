69984d594fc4d175fd0cf0762f98801dc598b99d
Tom van Dijk <18gatenmaker6@gmail.com>
2025-05-23 18:05:58 +0200

    tailscale: re-enable `packet_filter_test.go`
    Co-authored-by: Kenichi Kamiya <kachick1@gmail.com>

--- pkgs/by-name/ta/tailscale/package.nix
+++ pkgs/by-name/ta/tailscale/package.nix
@@ -95,12 +95,10 @@ buildGoModule {
     # want but also limits the tests
     unset subPackages
 
-    # several tests hang
-    rm tsnet/tsnet_test.go
-
-    # new test, probably missing import
-    # reported: https://github.com/tailscale/tailscale/issues/16051
-    rm tsnet/packet_filter_test.go
+    # several tests hang, but keeping the file for tsnet/packet_filter_test.go
+    # packet_filter_test issue: https://github.com/tailscale/tailscale/issues/16051
+    substituteInPlace tsnet/tsnet_test.go \
+      --replace-fail 'func Test' 'func skippedTest'
   '';
 
   checkFlags =
@@ -148,6 +146,9 @@ buildGoModule {
           # timeout 10m
           "TestTaildropIntegration"
           "TestTaildropIntegration_Fresh"
+
+          # context deadline exceeded
+          "TestPacketFilterFromNetmap"
         ]
         ++ lib.optionals stdenv.hostPlatform.isDarwin [
           # syscall default route interface en0 differs from netstat
