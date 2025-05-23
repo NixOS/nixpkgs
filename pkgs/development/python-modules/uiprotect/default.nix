b6a930fd7acaa1e2190e3f95b62ec367f3c042b5
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-05-23 04:32:46 +0200

    python313Packages.uiprotect: 7.6.0 -> 7.8.0
    https://github.com/uilibs/uiprotect/blob/v7.8.0/CHANGELOG.md

--- pkgs/development/python-modules/uiprotect/default.nix
+++ pkgs/development/python-modules/uiprotect/default.nix
@@ -40,7 +40,7 @@
 
 buildPythonPackage rec {
   pname = "uiprotect";
-  version = "7.6.0";
+  version = "7.8.0";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -49,7 +49,7 @@ buildPythonPackage rec {
     owner = "uilibs";
     repo = "uiprotect";
     tag = "v${version}";
-    hash = "sha256-jFRBupuP0T/6e07qdJmzcz7P/IAYbY1+QxF6ErsFox0=";
+    hash = "sha256-IzCi1UeAb/XIxZptgb2gXh71alZ7Bko7FCoD1B/ZBio=";
   };
 
   build-system = [ poetry-core ];
