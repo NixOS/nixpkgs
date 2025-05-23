6b27e449f90dd2acfa3d4f5cd60cf9b04ba21ca2
noiioiu <noiioiu@cocaine.ninja>
2025-05-22 20:28:24 -0500

    python3Packages.snappy: init at 3.2 Co-authored-by: Alex Epelde <alex@epelde.net>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17285,6 +17285,8 @@ self: super: with self; {
 
   snapcast = callPackage ../development/python-modules/snapcast { };
 
+  snappy = callPackage ../development/python-modules/snappy { };
+
   snappy-15-knots = callPackage ../development/python-modules/snappy-15-knots { };
 
   snappy-manifolds = callPackage ../development/python-modules/snappy-manifolds { };
