985bef2fb4bd9e43b09b3aa00a0673ec3febf8e2
Gurjaka <esartia.gurika@gmail.com>
2025-05-23 19:19:52 +0400

    nixos/dwl: init module

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -412,6 +412,7 @@ in
   druid = handleTestOn [ "x86_64-linux" ] ./druid { };
   drbd-driver = runTest ./drbd-driver.nix;
   dublin-traceroute = runTest ./dublin-traceroute.nix;
+  dwl = runTestOn [ "x86_64-linux" "aarch64-linux" ] ./dwl.nix;
   earlyoom = handleTestOn [ "x86_64-linux" ] ./earlyoom.nix { };
   early-mount-options = handleTest ./early-mount-options.nix { };
   ec2-config = (handleTestOn [ "x86_64-linux" ] ./ec2.nix { }).boot-ec2-config or { };
