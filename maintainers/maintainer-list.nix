25527c7ebb1bf3c335bdc6eded5a6347a3766932
Matija Obid <matija.obid@posteo.net>
2025-05-23 23:00:37 +0200

    maintainers: add mobid

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16459,6 +16459,12 @@
     githubId = 7831184;
     name = "John Mercier";
   };
+  mobid = {
+    email = "matija.obid@posteo.net";
+    github = "mobid";
+    githubId = 10497126;
+    name = "Matija Obid";
+  };
   modderme123 = {
     email = "modderme123@gmail.com";
     github = "milomg";
