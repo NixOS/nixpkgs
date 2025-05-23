c9519e8299e3618c707be1e4a849df64a6e46aad
noiioiu <noiioiu@cocaine.ninja>
2025-05-22 20:27:09 -0500

    python3Packages.spherogram: init at 2.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16479,6 +16479,8 @@ self: super: with self; {
 
   spglib = callPackage ../development/python-modules/spglib { };
 
+  spherogram = callPackage ../development/python-modules/spherogram { };
+
   sphfile = callPackage ../development/python-modules/sphfile { };
 
   sphinx = callPackage ../development/python-modules/sphinx { };
