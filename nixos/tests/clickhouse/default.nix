16fa38af2997c740cf294a454a9016e3e8a36fca
Jonathan Davies <jpds@protonmail.com>
2025-05-24 18:30:50 +0000

    nixos/clickhouse: Add S3 testcase

--- nixos/tests/clickhouse/default.nix
+++ nixos/tests/clickhouse/default.nix
@@ -8,4 +8,5 @@
   base = import ./base.nix { inherit system pkgs; };
   kafka = import ./kafka.nix { inherit system pkgs; };
   keeper = import ./keeper.nix { inherit system pkgs; };
+  s3 = import ./s3.nix { inherit system pkgs; };
 }
