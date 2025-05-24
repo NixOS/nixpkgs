aa56e773ed26848a9586b37fbab225fdef23c355
transcaffeine <transcaffeine@finally.coffee>
2025-05-24 16:30:43 +0200

    element-web-unwrapped: 1.11.100 -> 1.11.102
    Release notes:
      - https://github.com/element-hq/element-web/releases/tag/v1.11.101
      - https://github.com/element-hq/element-web/releases/tag/v1.11.102
    Changelog: https://github.com/element-hq/element-web/compare/v1.11.100...v1.11.102

--- pkgs/by-name/el/element-web-unwrapped/element-web-pin.nix
+++ pkgs/by-name/el/element-web-unwrapped/element-web-pin.nix
@@ -1,7 +1,7 @@
 {
-  "version" = "1.11.100";
+  "version" = "1.11.102";
   "hashes" = {
-    "webSrcHash" = "sha256-FiYjWOJ50Vhbs9vgEqK64HTVtwSuy4/BZAkPK4c6DXQ=";
-    "webYarnHash" = "sha256-C1yVJHU9ClTJHQfMLkdZEeRWVVu68eJp2kxnIlLinY8=";
+    "webSrcHash" = "sha256-B21fBRcksX8MoyWiqf1sa9yowJ/Z/wcM3vEqslunv7A=";
+    "webYarnHash" = "sha256-NXoGMEJM4uxFMCb5YX0ue9hgxe22hzG0MTeAOaBrJO4=";
   };
 }
