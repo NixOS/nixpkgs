29e0c7b2cb918278512ba9355bb9b5f1bd00ccb1
Kimberly Swanson <khswanson24@gmail.com>
2025-05-24 19:22:15 +0100

    opentelemetry-cpp: update versions. opentelemetry-proto 1.3.2 -> 1.5.0 https://github.com/open-telemetry/opentelemetry-proto/compare/v1.3.2...v1.5.0 opentelemetry 1.16.1 -> 1.20.0 https://github.com/open-telemetry/opentelemetry-cpp/compare/v1.16.1...v1.20.0

--- pkgs/by-name/op/opentelemetry-cpp/package.nix
+++ pkgs/by-name/op/opentelemetry-cpp/package.nix
@@ -22,19 +22,19 @@ let
   opentelemetry-proto = fetchFromGitHub {
     owner = "open-telemetry";
     repo = "opentelemetry-proto";
-    rev = "v1.3.2";
-    hash = "sha256-bkVqPSVhyMHrmFvlI9DTAloZzDozj3sefIEwfW7OVrI=";
+    rev = "v1.5.0";
+    hash = "sha256-PkG0npG3nKQwq6SxWdIliIQ/wrYAOG9qVb26IeVkBfc=";
   };
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "opentelemetry-cpp";
-  version = "1.16.1";
+  version = "1.20.0";
 
   src = fetchFromGitHub {
     owner = "open-telemetry";
     repo = "opentelemetry-cpp";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-31zwIZ4oehhfn+oCyg8VQTurPOmdgp72plH1Pf/9UKQ=";
+    hash = "sha256-ibLuHIg01wGYPhLRz+LVYA34WaWzlUlNtg7DSONLe9g=";
   };
 
   patches = [
@@ -49,8 +49,7 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   propagatedBuildInputs =
-    [ abseil-cpp ]
-    ++ lib.optionals enableGrpc [
+    lib.optionals enableGrpc [
       protobuf
       grpc
     ]
@@ -88,8 +87,8 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   postInstall = ''
-    substituteInPlace $out/lib/cmake/opentelemetry-cpp/opentelemetry-cpp-target.cmake \
-      --replace-fail "\''${_IMPORT_PREFIX}/include" "$dev/include"
+    substituteInPlace $out/lib/cmake/opentelemetry-cpp/opentelemetry-cpp*-target.cmake \
+      --replace-quiet "\''${_IMPORT_PREFIX}/include" "$dev/include"
   '';
 
   passthru.updateScript = nix-update-script { };
