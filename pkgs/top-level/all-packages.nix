063cb30f5e45faef6a0f2a65c21d1df1dfc210e9
Léana 江 <leana.jiang+git@icloud.com>
2025-05-24 22:08:45 +0200

    noto-fonts-cjk-serif: add static option

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11723,6 +11723,10 @@ with pkgs;
 
   nordic = libsForQt5.callPackage ../data/themes/nordic { };
 
+  noto-fonts-cjk-serif-static = callPackage ../by-name/no/noto-fonts-cjk-serif/package.nix {
+    static = true;
+  };
+
   noto-fonts-lgc-plus = callPackage ../by-name/no/noto-fonts/package.nix {
     suffix = "-lgc-plus";
     variants = [
