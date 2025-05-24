65bc844dc04e8412b69f482e3fdbb84c0ac02560
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-24 23:40:23 +0800

    nixosTests: handleTest -> runTest, batch 1
    Reference: https://github.com/NixOS/nixpkgs/issues/386873

--- nixos/tests/systemd-user-tmpfiles-rules.nix
+++ nixos/tests/systemd-user-tmpfiles-rules.nix
@@ -1,42 +1,40 @@
-import ./make-test-python.nix (
-  { lib, ... }:
-  {
-    name = "systemd-user-tmpfiles-rules";
+{ lib, ... }:
+{
+  name = "systemd-user-tmpfiles-rules";
 
-    meta = with lib.maintainers; {
-      maintainers = [ schnusch ];
-    };
+  meta = with lib.maintainers; {
+    maintainers = [ schnusch ];
+  };
 
-    nodes.machine =
-      { ... }:
-      {
-        users.users = {
-          alice.isNormalUser = true;
-          bob.isNormalUser = true;
-        };
-
-        systemd.user.tmpfiles = {
-          rules = [
-            "d %h/user_tmpfiles_created"
-          ];
-          users.alice.rules = [
-            "d %h/only_alice"
-          ];
-        };
+  nodes.machine =
+    { ... }:
+    {
+      users.users = {
+        alice.isNormalUser = true;
+        bob.isNormalUser = true;
       };
 
-    testScript =
-      { ... }:
-      ''
-        machine.succeed("loginctl enable-linger alice bob")
+      systemd.user.tmpfiles = {
+        rules = [
+          "d %h/user_tmpfiles_created"
+        ];
+        users.alice.rules = [
+          "d %h/only_alice"
+        ];
+      };
+    };
 
-        machine.wait_until_succeeds("systemctl --user --machine=alice@ is-active systemd-tmpfiles-setup.service")
-        machine.succeed("[ -d ~alice/user_tmpfiles_created ]")
-        machine.succeed("[ -d ~alice/only_alice ]")
+  testScript =
+    { ... }:
+    ''
+      machine.succeed("loginctl enable-linger alice bob")
 
-        machine.wait_until_succeeds("systemctl --user --machine=bob@ is-active systemd-tmpfiles-setup.service")
-        machine.succeed("[ -d ~bob/user_tmpfiles_created ]")
-        machine.succeed("[ ! -e ~bob/only_alice ]")
-      '';
-  }
-)
+      machine.wait_until_succeeds("systemctl --user --machine=alice@ is-active systemd-tmpfiles-setup.service")
+      machine.succeed("[ -d ~alice/user_tmpfiles_created ]")
+      machine.succeed("[ -d ~alice/only_alice ]")
+
+      machine.wait_until_succeeds("systemctl --user --machine=bob@ is-active systemd-tmpfiles-setup.service")
+      machine.succeed("[ -d ~bob/user_tmpfiles_created ]")
+      machine.succeed("[ ! -e ~bob/only_alice ]")
+    '';
+}
