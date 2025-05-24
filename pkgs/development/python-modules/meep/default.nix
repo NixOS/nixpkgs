3f27ee2dacca9556cec7a24b9cb20a74cef1d531
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-24 16:14:31 +0000

    python3Packages.meep: 1.30.0 -> 1.30.1
    (cherry picked from commit 802c794335c5de28f5b750100ba5235be001db02)

--- pkgs/development/python-modules/meep/default.nix
+++ pkgs/development/python-modules/meep/default.nix
@@ -35,13 +35,13 @@ assert !lapack.isILP64;
 
 buildPythonPackage rec {
   pname = "meep";
-  version = "1.30.0";
+  version = "1.30.1";
 
   src = fetchFromGitHub {
     owner = "NanoComp";
     repo = pname;
     tag = "v${version}";
-    hash = "sha256-9cQHvwUAeop5dRMVvedph+KQvTcmnkHdfqPQlSY280c=";
+    hash = "sha256-rXmOCkWm8SymhLiKNskSiS2bsHCmTlKMfLg5u3XniOk=";
   };
 
   format = "other";
