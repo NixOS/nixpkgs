d274a2acc660063029ff32107cf75fc5a7fbc076
Liam Hupfer <liam@hpfr.net>
2025-05-23 21:24:27 -0500

    maintainers: add hpfr
    (cherry picked from commit 5f72371b4d50fbb0a7e6b4074938e25861fa4417)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10066,6 +10066,12 @@
     githubId = 130903;
     name = "Ana Hobden";
   };
+  hpfr = {
+    email = "liam@hpfr.net";
+    github = "hpfr";
+    githubId = 44043764;
+    name = "Liam Hupfer";
+  };
   hqurve = {
     email = "hqurve@outlook.com";
     github = "hqurve";
