c8601ac270af0413c2993fcd3528e18893d8f741
Gaetan Lepage <gaetan@glepage.com>
2025-05-26 01:22:34 +0200

    python312Packages.pytensor: 2.31.1 -> 2.31.2
    Diff: https://github.com/pymc-devs/pytensor/compare/refs/tags/rel-2.31.1...rel-2.31.2
    
    Changelog: https://github.com/pymc-devs/pytensor/releases/tag/rel-2.31.2

--- pkgs/development/python-modules/pytensor/default.nix
+++ pkgs/development/python-modules/pytensor/default.nix
@@ -32,7 +32,7 @@
 
 buildPythonPackage rec {
   pname = "pytensor";
-  version = "2.31.1";
+  version = "2.31.2";
   pyproject = true;
 
   src = fetchFromGitHub {
@@ -42,7 +42,7 @@ buildPythonPackage rec {
     postFetch = ''
       sed -i 's/git_refnames = "[^"]*"/git_refnames = " (tag: ${src.tag})"/' $out/pytensor/_version.py
     '';
-    hash = "sha256-R/qHLAqNbFqoi2MQeptlVE148cInVOpN5n/wGr1AB9M=";
+    hash = "sha256-dFqJm9/77Nelh//1FjLauPAAQSogDa2fd1JJjlVFWsY=";
   };
 
   build-system = [
