4196e2ce85413d7f21a7ac932bfcf0ad70d2ef01
Duck <36255460+Duckuk@users.noreply.github.com>
2025-05-25 14:53:34 +0930

    vintagestory: 1.20.9 -> 1.20.11

--- pkgs/by-name/vi/vintagestory/package.nix
+++ pkgs/by-name/vi/vintagestory/package.nix
@@ -21,11 +21,11 @@
 
 stdenv.mkDerivation rec {
   pname = "vintagestory";
-  version = "1.20.9";
+  version = "1.20.11";
 
   src = fetchurl {
     url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
-    hash = "sha256-pia2Dv0FY28nkATOk60GqiAEnZmxgZvZfvkM8U/bZzU=";
+    hash = "sha256-IOreg6j/jLhOK8jm2AgSnYQrql5R6QxsshvPs8OUcQA=";
   };
 
   nativeBuildInputs = [
