775e23fe46dcca6f75d012738f6b1156b4760e75
noiioiu <noiioiu@cocaine.ninja>
2025-05-24 20:15:30 -0500

    python3Packages.fxrays: fetch from github instead of pypi

--- pkgs/development/python-modules/fxrays/default.nix
+++ pkgs/development/python-modules/fxrays/default.nix
@@ -1,19 +1,21 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   setuptools,
   cython,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "fxrays";
   version = "1.3.6";
   pyproject = true;
 
-  src = fetchPypi {
-    inherit version;
-    pname = "FXrays";
+  src = fetchFromGitHub {
+    owner = "3-manifolds";
+    repo = "FXrays";
+    # release of version 1.3.6
+    rev = "f02aaab228972044ca4e39fa55fdd46b3768900e";
     sha256 = "sha256-epNUWne1P/QJjSyXaMxUIrVKyMMXl9uWFgukiJTbHGg=";
   };
 
