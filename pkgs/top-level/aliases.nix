26a393c5f2b069991b5815f972ba61ae36737b03
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2025-05-25 17:02:02 +0200

    dclib: remove
    According to the tool's (Valknut, formerly DCGUI) website [^1],
    the original author stopped maintaining the tool this library was
    used for after the version packaged in nixpkgs up to now:
    
    > Nach der Veröffentlichung von Valknut 0.3.7 stellte Mathias Küster die Arbeit an Valknut ein.
    
    Someone else took over development, but the publishing folder for dclib
    doesn't contain any new versions after v. 0.3.23 (2009) either [^2].
    The GUI distribution folders also look abandoned.
    
    As the version from 2009 also fails with the same build error and
    this library isn't referenced anywhere else in nixpkgs (and there's
    no listed maintainer), I advocate for removing this package.
    
    [^1]: https://www.berlios.de/software/valknut/
    [^2]: https://sourceforge.net/projects/wxdcgui/files/dclib/

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -517,6 +517,7 @@ mapAliases {
   dave = throw "'dave' has been removed as it has been archived upstream. Consider using 'webdav' instead"; # Added 2025-02-03
   dbeaver = throw "'dbeaver' has been renamed to/replaced by 'dbeaver-bin'"; # Added 2024-05-16
   dbench = throw "'dbench' has been removed as it is unmaintained for 14 years and broken"; # Added 2025-05-17
+  dclib = throw "'dclib' has been removed as it is unmaintained for 16 years and broken"; # Added 2025-05-25
   dbus-map = throw "'dbus-map' has been dropped as it is unmaintained"; # Added 2024-11-01
   deadpixi-sam = deadpixi-sam-unstable;
 
