bc933834137a86068fdf6e5e2d3bddc72479d33b
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-25 13:02:49 +0000

    spacer: 0.3.8 -> 0.3.9

--- pkgs/by-name/sp/spacer/package.nix
+++ pkgs/by-name/sp/spacer/package.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "spacer";
-  version = "0.3.8";
+  version = "0.3.9";
 
   src = fetchFromGitHub {
     owner = "samwho";
     repo = "spacer";
     rev = "v${version}";
-    hash = "sha256-N6HNPuEYcbuqpHv8Qr43iRtRdtyBo4TP7YccIcbpIpc=";
+    hash = "sha256-8c8l8SJlh3z8spembPavO4fhzPcpCfaZVvU8dl3PUTc=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-/sUQy5dn83KLzBXZ2+QEMM6hOqE7T3tZ80Q3M510jjQ=";
+  cargoHash = "sha256-WNqzOZ38ogeeEYB6B58+C2VptJ/HNj5+DpWvvHBhTAQ=";
 
   meta = with lib; {
     description = "CLI tool to insert spacers when command output stops";
