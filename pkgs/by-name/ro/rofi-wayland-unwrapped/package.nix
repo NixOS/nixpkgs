010596e09384d17c608c49d670590baf10fdc224
Acid Bong <acidbong@tilde.club>
2025-05-25 10:59:20 +0300

    rofi{,-wayland}-unwrapped: refresh, remove antipatterns
    - replace `rev` with `tag` in source fetchers
    - replace `rec {}` with `(finalAttrs: {})`
    - remove `with lib`

--- pkgs/by-name/ro/rofi-wayland-unwrapped/package.nix
+++ pkgs/by-name/ro/rofi-wayland-unwrapped/package.nix
@@ -7,30 +7,34 @@
   wayland,
 }:
 
-rofi-unwrapped.overrideAttrs (oldAttrs: rec {
-  pname = "rofi-wayland-unwrapped";
-  version = "1.7.9+wayland1";
+rofi-unwrapped.overrideAttrs (
+  final: prev: {
+    pname = "rofi-wayland-unwrapped";
+    version = "1.7.9+wayland1";
 
-  src = fetchFromGitHub {
-    owner = "lbonn";
-    repo = "rofi";
-    rev = version;
-    fetchSubmodules = true;
-    hash = "sha256-tLSU0Q221Pg3JYCT+w9ZT4ZbbB5+s8FwsZa/ehfn00s=";
-  };
+    src = fetchFromGitHub {
+      owner = "lbonn";
+      repo = "rofi";
+      tag = final.version;
+      fetchSubmodules = true;
+      hash = "sha256-tLSU0Q221Pg3JYCT+w9ZT4ZbbB5+s8FwsZa/ehfn00s=";
+    };
 
-  depsBuildBuild = oldAttrs.depsBuildBuild ++ [ pkg-config ];
-  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-    wayland-protocols
-    wayland-scanner
-  ];
-  buildInputs = oldAttrs.buildInputs ++ [
-    wayland
-    wayland-protocols
-  ];
+    depsBuildBuild = prev.depsBuildBuild ++ [
+      pkg-config
+    ];
+    nativeBuildInputs = prev.nativeBuildInputs ++ [
+      wayland-protocols
+      wayland-scanner
+    ];
+    buildInputs = prev.buildInputs ++ [
+      wayland
+      wayland-protocols
+    ];
 
-  meta = oldAttrs.meta // {
-    description = "Window switcher, run dialog and dmenu replacement for Wayland";
-    homepage = "https://github.com/lbonn/rofi";
-  };
-})
+    meta = prev.meta // {
+      description = "Window switcher, run dialog and dmenu replacement for Wayland";
+      homepage = "https://github.com/lbonn/rofi";
+    };
+  }
+)
