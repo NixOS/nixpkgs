bc62ff092659a1ff50291fa89e201d4512683c16
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-25 02:23:46 +0000

    stellarsolver: 2.6 -> 2.7

--- pkgs/by-name/st/stellarsolver/package.nix
+++ pkgs/by-name/st/stellarsolver/package.nix
@@ -11,13 +11,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "stellarsolver";
-  version = "2.6";
+  version = "2.7";
 
   src = fetchFromGitHub {
     owner = "rlancaste";
     repo = "stellarsolver";
     rev = finalAttrs.version;
-    sha256 = "sha256-6WDiHaBhi9POtXynGU/eTeuqZSK81JJeuZv4SxOeVoE=";
+    sha256 = "sha256-eGyqJX9TZzIDBCrAlrblhUy4G49hpE5r4q89+/1pnfM=";
   };
 
   nativeBuildInputs = [ cmake ];
