6a07e51fe19c8927e91e701b2f146ffa32ccf3d7
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2025-05-26 09:45:04 +0200

    Remove 1000101 from blockbook maintainers
    As per their wish in https://github.com/NixOS/nixpkgs/pull/410469#issuecomment-2908132051

--- pkgs/by-name/bl/blockbook/package.nix
+++ pkgs/by-name/bl/blockbook/package.nix
@@ -82,7 +82,6 @@ buildGoModule rec {
     license = licenses.agpl3Only;
     maintainers = with maintainers; [
       mmahut
-      _1000101
     ];
     platforms = platforms.unix;
     mainProgram = "blockbook";
