ce04a3d08aa225fdec45e97ff0032cc6e385179b
GueLaKais <koroyeldiores@gmail.com>
2025-05-26 20:22:50 +0200

    python313Packages.colcon-hardware-acceleration: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2880,6 +2880,10 @@ self: super: with self; {
 
   colcon-devtools = callPackage ../development/python-modules/colcon-devtools { };
 
+  colcon-hardware-acceleration =
+    callPackage ../development/python-modules/colcon-hardware-acceleration
+      { };
+
   colcon-installed-package-information =
     callPackage ../development/python-modules/colcon-installed-package-information
       { };
