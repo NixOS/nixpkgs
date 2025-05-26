9a55e6bbfb20fd8ec76c4742c477fa68483f062a
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-26 07:46:45 +0000

    yaziPlugins.restore: 25.2.7-unstable-2025-04-24 -> 25.4.8-unstable-2025-05-19

--- pkgs/by-name/ya/yazi/plugins/restore/default.nix
+++ pkgs/by-name/ya/yazi/plugins/restore/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "restore.yazi";
-  version = "25.2.7-unstable-2025-04-24";
+  version = "25.4.8-unstable-2025-05-19";
 
   src = fetchFromGitHub {
     owner = "boydaihungst";
     repo = "restore.yazi";
-    rev = "539aad5077dc8b342a580036e416f2b949b6590e";
-    hash = "sha256-ngwbweKF7pSEpzy1TNzbKz8cFIWaDison5vCiGxkHFk=";
+    rev = "1bfd5f71cb59bebde192126e2255bf4dc9f3b249";
+    hash = "sha256-JaeJxFnLqZZGduSwwvGD3q1bdJUSEyu0W6EI+4Hwwc0=";
   };
 
   meta = {
