77b27eb180e1dac6867cf3e7fbe7bba13e58914e
GueLaKais <koroyeldiores@gmail.com>
2025-05-26 16:14:14 +0200

    python313Packages.colcon-top-level-workspace: init at 0-unstable-2024-11-22

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2699,6 +2699,10 @@ self: super: with self; {
     callPackage ../development/python-modules/colcon-ros-domain-id-coordinator
       { };
 
+  colcon-top-level-workspace =
+    callPackage ../development/python-modules/colcon-top-level-workspace
+      { };
+
   collections-extended = callPackage ../development/python-modules/collections-extended { };
 
   collidoscope = callPackage ../development/python-modules/collidoscope { };
