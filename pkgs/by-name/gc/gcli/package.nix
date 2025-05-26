2e2912943aaa2746ed19c9f3f5e4beee0cbbf106
Johannes Maier <johannes.maier@mailbox.org>
2025-05-26 09:45:38 +0200

    gcli: 2.7.0 -> 2.8.0

--- pkgs/by-name/gc/gcli/package.nix
+++ pkgs/by-name/gc/gcli/package.nix
@@ -11,23 +11,14 @@
 
 stdenv.mkDerivation rec {
   pname = "gcli";
-  version = "2.7.0";
+  version = "2.8.0";
 
   src = fetchFromGitHub {
     owner = "herrhotzenplotz";
     repo = "gcli";
     rev = "v${version}";
-    hash = "sha256-N5dzGhyXPDWcm/cNUSUQt4rR+PzaD1OUssRO3Sdfmoo=";
+    hash = "sha256-s71RImg/N8v9h472upI94Pae/Zlw27f4Kc9SCavP1oY=";
   };
-
-  patches = [
-    # Darwin builds are fixed in master, but the change is unreleased.
-    (fetchpatch {
-      name = "darwin-build-fix.patch";
-      url = "https://github.com/herrhotzenplotz/gcli/commit/720e372250fd363bdd90e9452907508563e30f93.patch";
-      hash = "sha256-TpjIisje20YObN2wf8iQlwHlY5kg0S7xTkUWxAmK+po=";
-    })
-  ];
 
   nativeBuildInputs = [
     pkg-config
