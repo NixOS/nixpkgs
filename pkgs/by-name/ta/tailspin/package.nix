47ebc67fae7db64e92d1738f7a43891b6ee0e8b7
misilelab <misileminecord@gmail.com>
2025-05-27 01:21:52 +0900

    tailspin: 4.0.0 -> 5.4.3
    https://github.com/bensadeh/tailspin/releases/tag/5.0.0
    https://github.com/bensadeh/tailspin/releases/tag/5.1.0
    https://github.com/bensadeh/tailspin/releases/tag/5.2.0
    https://github.com/bensadeh/tailspin/releases/tag/5.2.1
    https://github.com/bensadeh/tailspin/releases/tag/5.3.0
    https://github.com/bensadeh/tailspin/releases/tag/5.4.0
    https://github.com/bensadeh/tailspin/releases/tag/5.4.1
    https://github.com/bensadeh/tailspin/releases/tag/5.4.2
    https://github.com/bensadeh/tailspin/releases/tag/5.4.3
    
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- pkgs/by-name/ta/tailspin/package.nix
+++ pkgs/by-name/ta/tailspin/package.nix
@@ -9,17 +9,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "tailspin";
-  version = "4.0.0";
+  version = "5.4.3";
 
   src = fetchFromGitHub {
     owner = "bensadeh";
     repo = "tailspin";
     tag = finalAttrs.version;
-    hash = "sha256-5VbxQDK69If5N8EiS8sIKNqHkCAfquOz8nUS7ynp+nA=";
+    hash = "sha256-Y0qMmSFfCXP8jErecVFgzle7USus14kOPlj+n22QZAo=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-QkdnmeXor2K0c5m/TV5hYl1oSPWpykPfZy/ZRqFUt1s=";
+  cargoHash = "sha256-9FLMvdOxXEEe6P0toc8WaRIHvtt24SA1Au+PXb/bYOM=";
 
   postPatch = ''
     substituteInPlace tests/utils.rs --replace-fail \
