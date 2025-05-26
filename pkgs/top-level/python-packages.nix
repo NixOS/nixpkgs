9e0d7c9448b354c91a8f0eda821e2b10d5439524
GueLaKais <koroyeldiores@gmail.com>
2025-05-26 19:42:52 +0200

    python313Packages.colcon-package-selection: init at 0.2.10

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2791,6 +2791,8 @@ self: super: with self; {
     callPackage ../development/python-modules/colcon-package-information
       { };
 
+  colcon-package-selection = callPackage ../development/python-modules/colcon-package-selection { };
+
   colcon-parallel-executor = callPackage ../development/python-modules/colcon-parallel-executor { };
 
   colcon-ros-domain-id-coordinator =
