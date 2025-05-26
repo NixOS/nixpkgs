39a66ff85368bef02f2b8d6d036ce0a54bf17926
Kenichi Kamiya <kachick1@gmail.com>
2025-05-26 14:57:53 +0900

    typescript: 5.8.2 -> 5.8.3
    Diff: https://github.com/microsoft/TypeScript/compare/v5.8.2...v5.8.3

--- pkgs/by-name/ty/typescript/package.nix
+++ pkgs/by-name/ty/typescript/package.nix
@@ -2,26 +2,51 @@
   lib,
   buildNpmPackage,
   fetchFromGitHub,
+  replaceVars,
   versionCheckHook,
   nix-update-script,
 }:
 
 buildNpmPackage (finalAttrs: {
   pname = "typescript";
-  version = "5.8.2";
+  version = "5.8.3";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "TypeScript";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-fOA5IblxUd+C9ST3oI8IUmTTRL3exC63MPqW5hoWN0M=";
+    hash = "sha256-/XxjZO/pJLLAvsP7x4TOC+XDbOOR+HHmdpn+8qP77L8=";
   };
 
   patches = [
     ./disable-dprint-dstBundler.patch
+
+    # Should be removed in next 5.9.X TypeScript update: https://github.com/microsoft/TypeScript/pull/61218#issuecomment-2911264050
+    (replaceVars ./revert-bump-to-v5.9.patch {
+      inherit (finalAttrs) version;
+    })
   ];
 
-  npmDepsHash = "sha256-ytdkxIjAd3UsU90o9IFZa5lGEv39zblBmgTTseVRGKQ=";
+  postPatch = ''
+    # The test run in the build script is redundant with checkPhase
+    substituteInPlace package.json \
+     --replace-fail ' && npm run build:tests' ""
+
+    # Should be removed in next 5.9.X TypeScript update
+    substituteInPlace src/compiler/corePublic.ts \
+      --replace-fail 'versionMajorMinor = "5.9"' 'versionMajorMinor = "5.8"'
+    substituteInPlace tests/baselines/reference/api/typescript.d.ts \
+      --replace-fail 'versionMajorMinor = "5.9"' 'versionMajorMinor = "5.8"'
+
+    # Imported from https://github.com/microsoft/TypeScript/blob/b504a1eed45e35b5f54694a1e0a09f35d0a5663c/.github/workflows/set-version.yaml#L75
+    sed -i -e 's/const version\(: string\)\{0,1\} = .*;/const version = "${finalAttrs.version}" as string;/g' src/compiler/corePublic.ts
+  '';
+
+  npmDepsHash = "sha256-BHJGezzZensC/WFsUumJSk6TYAqbS50Inuvw2nV5vUk=";
+
+  postBuild = ''
+    npx hereby lkg
+  '';
 
   nativeInstallCheckInputs = [
     versionCheckHook
