0b151394a69207849ce6f160f4bb142b42659593
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-27 18:30:19 +0000

    zeekstd: 0.2.3 -> 0.4.0-lib

--- pkgs/by-name/ze/zeekstd/package.nix
+++ pkgs/by-name/ze/zeekstd/package.nix
@@ -5,17 +5,17 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zeekstd";
-  version = "0.2.3";
+  version = "0.4.0-lib";
 
   src = fetchFromGitHub {
     owner = "rorosen";
     repo = "zeekstd";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-URQ8UiCy8qnm0VM55BqPgIthR4AIyRk+fgowAFxvXwM=";
+    hash = "sha256-+WOTMZwsTTkwgSCP1IOPTo/dl6ceC7Jg6bMT8kNrRwI=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-lS4RQuwvY6bRpsev7RI3XWBPbrdek5im/rkzP+Cmgpc=";
+  cargoHash = "sha256-iAj3QxJbclh7yhI4A0WmxcnZGy4p/3C5G3z+HelJr7g=";
 
   meta = {
     description = "CLI tool that works with the zstd seekable format";
