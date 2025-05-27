38b1b87e90340d1a10f3b96cc602ddf9ba950c4c
Kenichi Kamiya <kachick1@gmail.com>
2025-05-28 00:38:03 +0900

    typescript: prefer npmjs for src

--- pkgs/by-name/ty/typescript/package.nix
+++ pkgs/by-name/ty/typescript/package.nix
@@ -1,52 +1,38 @@
 {
   lib,
   buildNpmPackage,
-  fetchFromGitHub,
-  replaceVars,
+  fetchurl,
   versionCheckHook,
-  nix-update-script,
+  writeShellApplication,
+  nodejs,
+  gnutar,
+  nix-update,
+  prefetch-npm-deps,
+  gnused,
 }:
 
 buildNpmPackage (finalAttrs: {
   pname = "typescript";
   version = "5.8.3";
 
-  src = fetchFromGitHub {
-    owner = "microsoft";
-    repo = "TypeScript";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-/XxjZO/pJLLAvsP7x4TOC+XDbOOR+HHmdpn+8qP77L8=";
+  # Prefer npmjs over the GitHub repository for source code.
+  # The TypeScript project typically publishes stable, versioned code to npmjs,
+  # whereas GitHub tags may sometimes include development versions.
+  # For example:
+  #   - https://github.com/microsoft/TypeScript/pull/61218#issuecomment-2911264050
+  #   - https://github.com/microsoft/TypeScript/pull/60150#issuecomment-2648791588, 5.8.3 includes this 5.9 breaking change
+  src = fetchurl {
+    url = "https://registry.npmjs.org/typescript/-/typescript-${finalAttrs.version}.tgz";
+    hash = "sha256-cuddvrksLm65o0y1nXT6tcLubzKgMkqJQF9hZdWgg3Q=";
   };
 
-  patches = [
-    ./disable-dprint-dstBundler.patch
-
-    # Should be removed in next 5.9.X TypeScript update: https://github.com/microsoft/TypeScript/pull/61218#issuecomment-2911264050
-    (replaceVars ./revert-bump-to-v5.9.patch {
-      inherit (finalAttrs) version;
-    })
-  ];
-
   postPatch = ''
-    # The test run in the build script is redundant with checkPhase
-    substituteInPlace package.json \
-     --replace-fail ' && npm run build:tests' ""
-
-    # Should be removed in next 5.9.X TypeScript update
-    substituteInPlace src/compiler/corePublic.ts \
-      --replace-fail 'versionMajorMinor = "5.9"' 'versionMajorMinor = "5.8"'
-    substituteInPlace tests/baselines/reference/api/typescript.d.ts \
-      --replace-fail 'versionMajorMinor = "5.9"' 'versionMajorMinor = "5.8"'
-
-    # Imported from https://github.com/microsoft/TypeScript/blob/b504a1eed45e35b5f54694a1e0a09f35d0a5663c/.github/workflows/set-version.yaml#L75
-    sed -i -e 's/const version\(: string\)\{0,1\} = .*;/const version = "${finalAttrs.version}" as string;/g' src/compiler/corePublic.ts
+    ln -s '${./package-lock.json}' package-lock.json
   '';
 
-  npmDepsHash = "sha256-BHJGezzZensC/WFsUumJSk6TYAqbS50Inuvw2nV5vUk=";
+  npmDepsHash = "sha256-Y/+QPAVOQWKxrHBNEejC3UZrYKQNm7CleR0whFm2sLw=";
 
-  postBuild = ''
-    npx hereby lkg
-  '';
+  dontNpmBuild = true;
 
   nativeInstallCheckInputs = [
     versionCheckHook
@@ -56,11 +42,21 @@ buildNpmPackage (finalAttrs: {
   versionCheckProgramArg = "--version";
 
   passthru = {
-    updateScript = nix-update-script {
-      extraArgs = [
-        "--version-regex=^v([\\d.]+)$"
+    updateScript = lib.getExe (writeShellApplication {
+      name = "${finalAttrs.pname}-updater";
+      runtimeInputs = [
+        nodejs
+        gnutar
+        nix-update
+        prefetch-npm-deps
+        gnused
       ];
-    };
+      runtimeEnv = {
+        PNAME = finalAttrs.pname;
+        PKG_DIR = builtins.toString ./.;
+      };
+      text = builtins.readFile ./update.bash;
+    });
   };
 
   meta = {
