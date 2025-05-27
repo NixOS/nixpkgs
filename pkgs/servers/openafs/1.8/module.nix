2ea8ba53a356770308f1f5f5e2031b202ec983aa
Anders Kaseorg <andersk@mit.edu>
2025-05-27 01:20:39 -0700

    linuxPackages.openafs: Patch for Linux kernel 6.15
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pkgs/servers/openafs/1.8/module.nix
+++ pkgs/servers/openafs/1.8/module.nix
@@ -40,6 +40,42 @@ stdenv.mkDerivation {
       decode = "base64 -d";
       hash = "sha256-08jedwZ1KX1RSs8y9sh7BUvv5xK9tlzZ6uBOR4kS0Jo=";
     })
+    # Linux: Add required MODULE_DESCRIPTION
+    (fetchpatch {
+      url = "https://gerrit.openafs.org/changes/16372/revisions/39189eba45542376e668636bd79a93ae6a8a7cd2/patch";
+      decode = "base64 -d";
+      hash = "sha256-j5ckKQvybEvmlnFs5jX8g8Dfw37LYWGnfsl4hnZ3+A4=";
+    })
+    # linux: inode_ops mkdir returns struct dentry *
+    (fetchpatch {
+      url = "https://gerrit.openafs.org/changes/16373/revisions/769847e205d5908a0c430f7bcfbd2f48e19f8bf8/patch";
+      decode = "base64 -d";
+      hash = "sha256-znv5gunyPnJgi4SRFERJudtYFqiS+AVYDWfvr52Ku3s=";
+    })
+    # Linux: Use __filemap_get_folio()
+    (fetchpatch {
+      url = "https://gerrit.openafs.org/changes/16374/revisions/f187add554da9e9c52752edbfa98f486f683cf25/patch";
+      decode = "base64 -d";
+      hash = "sha256-+ay87ThSn6QyPZcN0+oE01Wqbxmz0Z1KXYwocQCvYLg=";
+    })
+    # Linux: Use folio_wait_locked()
+    (fetchpatch {
+      url = "https://gerrit.openafs.org/changes/16375/revisions/87a93f6488585553d833e1397e7f0dae0545cb7e/patch";
+      decode = "base64 -d";
+      hash = "sha256-MOVX2LFe8OBnvsQ2UdLvwKrwztOmnu1rdIou4CF+EBs=";
+    })
+    # cf: Introduce AC_CHECK_LINUX_SYMBOL
+    (fetchpatch {
+      url = "https://gerrit.openafs.org/changes/16376/revisions/bab5968d7f4639d4a0cbe81aaa3e9716dda00632/patch";
+      decode = "base64 -d";
+      hash = "sha256-d6DZqDTW1uEKSB5PsomS4ix9fYYQzdQVmDATKl6n7x4=";
+    })
+    # cf: check for dentry flag macros/enums
+    (fetchpatch {
+      url = "https://gerrit.openafs.org/changes/16377/revisions/f791d8ca4804486c656bc7c221076480df39b465/patch";
+      decode = "base64 -d";
+      hash = "sha256-7B0VJE3FeSQU1ElvXI5zXCPq1JRLAycyhqIQuDdR7xE=";
+    })
   ];
 
   nativeBuildInputs = [
