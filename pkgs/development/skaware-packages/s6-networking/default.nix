51445f8cd908ca72833ccc94a4cb12c9ffd0a47c
Daniel Sampliner <samplinerD@gmail.com>
2025-05-27 22:14:04 +0000

    skawarePackages.s6-networking-man-pages: 2.7.0.3.1 -> 2.7.0.4.1

--- pkgs/development/skaware-packages/s6-networking/default.nix
+++ pkgs/development/skaware-packages/s6-networking/default.nix
@@ -30,8 +30,8 @@ skawarePackages.buildPackage {
 
   manpages = skawarePackages.buildManPages {
     pname = "s6-networking-man-pages";
-    version = "2.7.0.3.1";
-    sha256 = "9u2C1TF9vma+7Qo+00uZ6eOCn/9eMgKALgHDVgMcrfg=";
+    version = "2.7.0.4.1";
+    sha256 = "sha256-ocYUZVnkuhO/1qgW3mSooZRoqqch1SgIRoygS3AjeZI=";
     description = "Port of the documentation for the s6-networking suite to mdoc";
     maintainers = [ lib.maintainers.sternenseemann ];
   };
