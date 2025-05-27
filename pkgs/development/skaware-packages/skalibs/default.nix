b784522f3865de84b47cf98675c1b713c85c51dc
Daniel Sampliner <samplinerD@gmail.com>
2025-05-27 22:14:09 +0000

    skawarePackages.skalibs: 2.14.3.0 -> 2.14.4.0

--- pkgs/development/skaware-packages/skalibs/default.nix
+++ pkgs/development/skaware-packages/skalibs/default.nix
@@ -7,8 +7,8 @@
 
 skawarePackages.buildPackage {
   pname = "skalibs";
-  version = "2.14.3.0";
-  sha256 = "sha256-oUqlWMmwmwYvoWrOxiOyyPk9afXLpNB/bQxYkTBmxCc=";
+  version = "2.14.4.0";
+  sha256 = "sha256-DmJiYYSMySBzj5L9UKJMFLIeMDBt/tl7hDU2n0uuAKU=";
 
   description = "Set of general-purpose C programming libraries";
 
