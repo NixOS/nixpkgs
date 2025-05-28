1996ea5031f1a548afa4d0a7596aefbe345f5367
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-28 17:17:35 +0000

    kubevpn: 2.7.11 -> 2.7.12

--- pkgs/by-name/ku/kubevpn/package.nix
+++ pkgs/by-name/ku/kubevpn/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "kubevpn";
-  version = "2.7.11";
+  version = "2.7.12";
 
   src = fetchFromGitHub {
     owner = "KubeNetworks";
     repo = "kubevpn";
     rev = "v${version}";
-    hash = "sha256-PqKgBJugibgG/4gGBINuFxWAxSYEKRpPXpofiOKmmIs=";
+    hash = "sha256-Tf0hhhabSP4MxXMb046dBzcjF7T+cmhcCF/1+ZNo1fM=";
   };
 
   vendorHash = null;
