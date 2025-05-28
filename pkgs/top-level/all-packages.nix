7a37b0ca28c4cbb0f23336fb6749c8ac9ac653a5
rewine <luhongxu@deepin.org>
2025-05-28 10:17:14 +0800

    wlroots_0_19: init at 0.19.0
    (cherry picked from commit 2d1418e6ff82a427a738be0595b5ad41faa8a6f5)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12810,6 +12810,7 @@ with pkgs;
   inherit (callPackages ../development/libraries/wlroots { })
     wlroots_0_17
     wlroots_0_18
+    wlroots_0_19
     ;
 
   sway-contrib = recurseIntoAttrs (callPackages ../applications/misc/sway-contrib { });
