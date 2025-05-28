ce3e1175c703a2f26b6b475d7074bf07fe79d7b5
Kiskae <Kiskae@users.noreply.github.com>
2025-05-28 22:05:49 +0200

    nvidia-vaapi-driver: backport 575 driver fixes
    Fixes #410585

--- pkgs/by-name/nv/nvidia-vaapi-driver/package.nix
+++ pkgs/by-name/nv/nvidia-vaapi-driver/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   meson,
   ninja,
   pkg-config,
@@ -26,6 +27,21 @@ stdenv.mkDerivation (finalAttrs: {
 
   patches = [
     ./0001-hardcode-install_dir.patch
+    (fetchpatch {
+      # Quick fix for driver version 575 failures.
+      url = "https://github.com/elFarto/nvidia-vaapi-driver/commit/0ef97c1849fac3e07cdf39ffe4cd9aa9f352896f.patch";
+      hash = "sha256-Hyo5iO7/Ls5R1pRJo7FhKE3RPihc2xTh619HCFaODEg=";
+      # Last part of the patch wasn't in the release, drop it.
+      postFetch = ''
+        head -n -10 "$out" > truncated.patch
+        mv truncated.patch "$out"
+      '';
+    })
+    (fetchpatch {
+      # Fix 545.29 driver version check.
+      url = "https://github.com/elFarto/nvidia-vaapi-driver/commit/14b519df56fe2655248c54d1553bced51ac154a9.patch";
+      hash = "sha256-72mpEyhaSMXdrSkCicYh9ezR5pxGydXQV3nu4mCdvNo=";
+    })
   ];
 
   nativeBuildInputs = [
