6c93afb84bc3b54969fed7353a4f0c5b6199470e
GueLaKais <koroyeldiores@gmail.com>
2025-05-28 21:20:02 +0200

    python313Packages.colcon-ed: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3016,6 +3016,8 @@ self: super: with self; {
 
   colcon-devtools = callPackage ../development/python-modules/colcon-devtools { };
 
+  colcon-ed = callPackage ../development/python-modules/colcon-ed { };
+
   colcon-hardware-acceleration =
     callPackage ../development/python-modules/colcon-hardware-acceleration
       { };
