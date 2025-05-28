b71e790357ab5463392cffba92a02bf9d1d35523
jopejoe1 <jopejoe1@missing.ninja>
2025-05-28 18:23:54 +0000

    discord-canary: 0.0.678 -> 0.0.687

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -11,7 +11,7 @@ let
       {
         stable = "0.0.95";
         ptb = "0.0.144";
-        canary = "0.0.678";
+        canary = "0.0.687";
         development = "0.0.75";
       }
     else
@@ -34,7 +34,7 @@ let
       };
       canary = fetchurl {
         url = "https://canary.dl2.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-        hash = "sha256-Z+pTuR5xZfbmucdx9aBti5diNp5oCpP1mu60mq7oPB8=";
+        hash = "sha256-OaDN+Qklxieo9xlP8qVeCwWzPBe6bLXoFUkMOFCoqPg=";
       };
       development = fetchurl {
         url = "https://development.dl2.discordapp.net/apps/linux/${version}/discord-development-${version}.tar.gz";
