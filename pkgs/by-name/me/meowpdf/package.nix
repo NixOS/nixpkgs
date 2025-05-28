2621a2d838de4b8678e46a0a73b73be15b890e09
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-28 20:04:25 +0000

    meowpdf: 1.0.0 -> 1.1.0

--- pkgs/by-name/me/meowpdf/package.nix
+++ pkgs/by-name/me/meowpdf/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "meowpdf";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "monoamine11231";
     repo = "meowpdf";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-C5GqyZW0pDmBuaKM890hx2JZtkZqZx+x/RZFCPhpjho=";
+    hash = "sha256-2mV/B0bu6HX2mBF7ogcTeZrfepDCzBemad3SwT1aHDs=";
   };
 
-  cargoHash = "sha256-hCGMm0ORKuyyWU5D9k+nthSwmq8ALz0qASLDaMiW30U=";
+  cargoHash = "sha256-dSM2F5HAgCOMJHSGeGpGqwnqcSkaTBD+8LV+/gJuDYQ=";
 
   nativeBuildInputs = [
     pkg-config
