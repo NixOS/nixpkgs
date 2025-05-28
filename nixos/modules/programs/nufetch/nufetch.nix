75fd387a702de7367b9e4a76308128808721341f
Maxwell Rupp <gig@gignsky.com>
2025-05-28 17:23:54 -0400

    nixos/nufetch: followed suggestion to remove with lib;

--- nixos/modules/programs/nufetch/nufetch.nix
+++ nixos/modules/programs/nufetch/nufetch.nix
@@ -5,15 +5,13 @@
   ...
 }:
 
-with lib;
-
 let
   cfg = config.programs.nufetch;
   configFile = pkgs.writeText "nufetch.conf" (import ./lib/config-maker.nix { inherit cfg lib; });
 in
 {
 
-  config = mkIf config.programs.nufetch.enable {
+  config = lib.mkIf config.programs.nufetch.enable {
     environment.systemPackages = [
       (pkgs.writeShellScriptBin "neofetch" ''
         exec ${pkgs.nufetch-for-nixos-module}/bin/neofetch --config /etc/nufetch.conf "$@"
@@ -27,149 +25,149 @@ in
   };
 
   options.programs.nufetch = {
-    enable = mkEnableOption "Enable Neofetch with custom options, a command-line utility to display system information.";
-    package = mkOption {
+    enable = lib.mkEnableOption "Enable Neofetch with custom options, a command-line utility to display system information.";
+    package = lib.mkOption {
       type = types.package;
       default = pkgs.nufetch-for-nixos-module;
       defaultText = "pkgs.nufetch-for-nixos-module";
       description = "Set version of nufetch package to use.";
     };
-    os = mkOption {
+    os = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch OS information display.";
     };
-    host = mkOption {
+    host = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Host information display.";
     };
-    kernel = mkOption {
+    kernel = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Kernel information display.";
     };
-    uptime = mkOption {
+    uptime = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Uptime information display.";
     };
-    packages = mkOption {
+    packages = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Packages information display.";
     };
-    shell = mkOption {
+    shell = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Shell information display.";
     };
-    resolution = mkOption {
+    resolution = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Resolution information display.";
     };
-    de = mkOption {
+    de = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Desktop Environment information display.";
     };
-    wm = mkOption {
+    wm = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Window Manager information display.";
     };
-    wm_theme = mkOption {
+    wm_theme = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Window Manager Theme information display.";
     };
-    theme = mkOption {
+    theme = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Theme information display.";
     };
-    icons = mkOption {
+    icons = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Icons display.";
     };
-    terminal = mkOption {
+    terminal = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Terminal information display.";
     };
-    terminal_font = mkOption {
+    terminal_font = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Terminal information display.";
     };
-    cpu = mkOption {
+    cpu = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch CPU information display.";
     };
-    gpu = mkOption {
+    gpu = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch GPU information display.";
     };
-    memory = mkOption {
+    memory = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Memory information display.";
     };
-    cpu_usage = mkOption {
+    cpu_usage = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch CPU Usage information display.";
     };
-    disk = mkOption {
+    disk = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Disk information display.";
     };
-    battery = mkOption {
+    battery = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Battery information display.";
     };
-    font = mkOption {
+    font = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Font information display.";
     };
-    song = mkOption {
+    song = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Song information display.";
     };
-    local_ip = mkOption {
+    local_ip = lib.mkOption {
       type = types.bool;
       default = true;
       description = "Enable Neofetch Local IP information display.";
     };
-    public_ip = mkOption {
+    public_ip = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Public IP information display.";
     };
-    users = mkOption {
+    users = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Users information display.";
     };
-    birthday = mkOption {
+    birthday = lib.mkOption {
       type = types.bool;
       default = false;
       description = "Enable Neofetch Birthday information display.";
     };
-    extraPrintInfoFields = mkOption {
+    extraPrintInfoFields = lib.mkOption {
       type = types.str;
       default = "";
       description = "Extra config lines to append to the generated Neofetch config. Use a long string (e.g. '''') for multi-line content.";
     };
-    extraGenericFields = mkOption {
+    extraGenericFields = lib.mkOption {
       type = types.str;
       default = "";
       description = "Extra config lines to append to the generated Neofetch config. Use a long string (e.g. '''') for multi-line content.";
