18732316d799e0f436041b34ff029cda4974309a
Tomas Drtina <tdrtina-ext@arista.com>
2025-05-28 14:09:08 +0200

    apache-airflow.pythonPackages.pytzdata: bring back removed package

--- pkgs/by-name/ap/apache-airflow/package.nix
+++ pkgs/by-name/ap/apache-airflow/package.nix
@@ -2,6 +2,7 @@
   fetchFromGitHub,
   fetchpatch,
   fetchPypi,
+  lib,
   python311,
 }:
 
@@ -143,6 +144,26 @@ let
           hash = "sha256-S4ThQx4H3UlKhunJo35esPClZiEn7gX/Qwo4kE1QMTI=";
         };
       });
+
+      pytzdata = pySuper.buildPythonPackage rec {
+        pname = "pytzdata";
+        version = "2020.1";
+        format = "setuptools";
+
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "3efa13b335a00a8de1d345ae41ec78dd11c9f8807f522d39850f2dd828681540";
+        };
+
+        # No tests
+        doCheck = false;
+
+        meta = with lib; {
+          description = "Timezone database for Python";
+          homepage = "https://github.com/sdispater/pytzdata";
+          license = licenses.mit;
+        };
+      };
 
       rich-argparse = pySuper.rich-argparse.overridePythonAttrs (o: {
         disabledTests = (o.disabledTests or []) ++ [ "test_generated_usage" ];
