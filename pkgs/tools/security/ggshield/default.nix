a189a533cf8dde852c826dcb4a6194b674fee2d2
Fabian Affolter <mail@fabian-affolter.ch>
2025-05-28 22:57:32 +0200

    ggshield: 1.39.0 -> 1.40.0
    Diff: https://github.com/GitGuardian/ggshield/compare/refs/tags/v1.39.0...refs/tags/v1.40.0
    
    Changelog: https://github.com/GitGuardian/ggshield/blob/1.40.0/CHANGELOG.md

--- pkgs/tools/security/ggshield/default.nix
+++ pkgs/tools/security/ggshield/default.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "ggshield";
-  version = "1.39.0";
+  version = "1.40.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "GitGuardian";
     repo = "ggshield";
     tag = "v${version}";
-    hash = "sha256-9VQLWeZPYz3ZqNUzw1vLC5no2NjRru4GcUpjW4QhuBY=";
+    hash = "sha256-Y42MBRyjPljUAGTwhH2FS8drUAceuJse8Qd1GbctWQs=";
   };
 
   pythonRelaxDeps = true;
