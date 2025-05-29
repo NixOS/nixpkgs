0f1eaa6ab196d5d31dca29d0a0aab2bbc3ebd40f
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-29 19:25:55 +0000

    meteor-git: 0.26.0 -> 0.27.0

--- pkgs/by-name/me/meteor-git/package.nix
+++ pkgs/by-name/me/meteor-git/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "meteor-git";
-  version = "0.26.0";
+  version = "0.27.0";
 
   src = fetchFromGitHub {
     owner = "stefanlogue";
     repo = "meteor";
     rev = "v${version}";
-    hash = "sha256-DWnwofNJhILdVITRDh8MNlGl6PSENCS2PkaFm1o+MW8=";
+    hash = "sha256-OH4WQNSbTMaysaHoKcy3S9V7BWu93Djr8toWtQ7Xj/w=";
   };
 
   vendorHash = "sha256-jKd/eJwp5SZvTrP3RN7xT7ibAB0PQondGR3RT+HQXIo=";
