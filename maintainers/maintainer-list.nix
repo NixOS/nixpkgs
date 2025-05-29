57af53a790274c6cdc22678a9f6fe14439b6b377
Wulpey <wulpine@proton.me>
2025-05-29 16:41:44 +0300

    maintainers: add wulpine

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -26831,6 +26831,12 @@
     githubId = 13378502;
     name = "Wulfsta";
   };
+  wulpine = {
+    name = "Wulpey";
+    email = "wulpine@proton.me";
+    github = "wulpine";
+    githubId = 59339992;
+  };
   wunderbrick = {
     name = "Andrew Phipps";
     email = "lambdafuzz@tutanota.com";
