61eb02225c5b23ea50e65cd0f600524f3ed75e88
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-29 16:41:54 +0300

    maintainers: donteatoreo remove expired keys

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6543,7 +6543,6 @@
     name = "DontEatOreo";
     github = "DontEatOreo";
     githubId = 57304299;
-    keys = [ { fingerprint = "33CD 5C0A 673C C54D 661E  5E4C 0DB5 361B EEE5 30AB"; } ];
   };
   dopplerian = {
     name = "Dopplerian";
