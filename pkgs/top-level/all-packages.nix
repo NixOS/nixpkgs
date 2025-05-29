004b2978ef598abd918b652ae685857b85d23e74
Tobias M <tobias@miglix.eu>
2025-05-29 22:49:37 +0200

    hotspot: use qt6
    Port hotspot as well as kddockwidgets to use qt6.
    As hotspot is the only reverse dependency of kddockwidgets,
    kddockwidgets is simply moved to qt6 alongside hotspot.
    Also prepare for by-name migration by taking top-level qt6 as an input.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3312,7 +3312,7 @@ with pkgs;
 
   hotdoc = python3Packages.callPackage ../development/tools/hotdoc { };
 
-  hotspot = libsForQt5.callPackage ../development/tools/analysis/hotspot { };
+  hotspot = callPackage ../development/tools/analysis/hotspot { };
 
   hpccm = with python3Packages; toPythonApplication hpccm;
 
@@ -8602,7 +8602,7 @@ with pkgs;
 
   json2yaml = haskell.lib.compose.justStaticExecutables haskellPackages.json2yaml;
 
-  kddockwidgets = libsForQt5.callPackage ../development/libraries/kddockwidgets { };
+  kddockwidgets = callPackage ../development/libraries/kddockwidgets { };
 
   keybinder = callPackage ../development/libraries/keybinder {
     automake = automake111x;
