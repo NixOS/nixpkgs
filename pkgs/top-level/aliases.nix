fe7065bfe0e73b5395b2de1fd06e5899404ffcd0
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-05-30 00:29:16 +0200

    thefuck: drop
    It was pinned to python311 last summer due to two uses of the removed module 'imp'.
    One of the uses are fixed on the upstream master branch but is unreleased, and the second use is fixed by two separate open PRs. The author is unresponsive and has been inactive since january 2024.
    Rather than rebase and apply the two fixes I feel it makes more sense to drop the package.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1884,6 +1884,7 @@ mapAliases {
   testVersion = testers.testVersion; # Added 2022-04-20
   tfplugindocs = terraform-plugin-docs; # Added 2023-11-01
   thiefmd = throw "'thiefmd' has been removed due to lack of maintenance upstream and incompatible with newer Pandoc. Please use 'apostrophe' or 'folio' instead"; # Added 2025-02-20
+  thefuck = throw "'thefuck' has been removed due to lack of maintenance upstream and incompatible with python 3.12+. Consider using 'pay-respects' instead"; # Added 2025-05-30
   invalidateFetcherByDrvHash = testers.invalidateFetcherByDrvHash; # Added 2022-05-05
   tijolo = throw "'tijolo' has been removed due to being unmaintained"; # Added 2024-12-27
   timescale-prometheus = throw "'timescale-prometheus' has been renamed to/replaced by 'promscale'"; # Converted to throw 2024-10-17
