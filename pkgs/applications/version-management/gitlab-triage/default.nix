ee441b312a21fbf30c338b880531947e3dc4cd74
Michael Daniels <mdaniels5757@gmail.com>
2025-05-29 12:51:49 -0400

    gitlab-triage: 1.23.1 -> 1.44.5
    Changelog: https://gitlab.com/gitlab-org/ruby/gems/gitlab-triage/-/releases

--- pkgs/applications/version-management/gitlab-triage/default.nix
+++ pkgs/applications/version-management/gitlab-triage/default.nix
@@ -13,7 +13,7 @@ bundlerApp {
 
   meta = with lib; {
     description = "GitLab's issues and merge requests triage, automated!";
-    homepage = "https://gitlab.com/gitlab-org/gitlab-triage";
+    homepage = "https://gitlab.com/gitlab-org/ruby/gems/gitlab-triage";
     license = licenses.mit;
     maintainers = [ ];
     mainProgram = "gitlab-triage";
