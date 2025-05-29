efafd2dbc70c5ede9421718166c0dc583c47b2a0
Adrian Grucza <adrian.grucza@iress.com>
2025-05-29 22:29:40 +1000

    Fix formatting

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1164,6 +1164,29 @@ with pkgs;
     jdk = jdk8;
   };
 
+  sonar-scanner-cli-minimal =
+    (sonar-scanner-cli.override {
+      jre = jre_minimal.override {
+        jdk = jdk21_headless;
+        modules = [
+          "java.base"
+          "java.logging"
+          "java.naming"
+          "java.sql"
+          "java.xml"
+          "jdk.crypto.ec"
+          "jdk.jdwp.agent"
+          "jdk.unsupported"
+        ];
+      };
+    }).overrideAttrs
+      (oldAttrs: {
+        name = "${oldAttrs.pname}-minimal-${oldAttrs.version}";
+        meta = oldAttrs.meta // {
+          description = "${oldAttrs.meta.description} (minimal JRE)";
+        };
+      });
+
   supermin = callPackage ../tools/virtualization/supermin {
     ocamlPackages = ocaml-ng.ocamlPackages_4_14;
   };
