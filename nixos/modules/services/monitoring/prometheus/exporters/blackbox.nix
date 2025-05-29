d6becde4a36ad85ec170b01138c17c13d0b76158
Tim Englart <timenglartdev@gmail.com>
2025-05-29 23:45:09 +1000

    nixos/prometheus: add dynamic config option to blackbox-exporter

--- nixos/modules/services/monitoring/prometheus/exporters/blackbox.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/blackbox.nix
@@ -47,6 +47,8 @@ let
         ln -s ${coerceConfigFile file} $out
         blackbox_exporter --config.check --config.file $out
       '';
+
+  yamlFormat = pkgs.formats.yaml { };
 in
 {
   port = 9115;
@@ -55,6 +57,19 @@ in
       type = types.path;
       description = ''
         Path to configuration file.
+      '';
+    };
+    config = mkOption {
+      type = yamlFormat.type;
+      default = null;
+      description = ''
+        Blackbox Exporter YAML configuration as a nix attrset
+
+        Documentation assoicated with the configuration schema and layout, see:
+        [Blackbox Exporter Configuration Docs](https://github.com/prometheus/blackbox_exporter/blob/master/CONFIGURATION.md)
+
+        Secrets can be handled using `_path` options, but require that the service has access to the files.
+        This can be achieved by either giving the blackbox-exporter user read perms to the secret file or adding the credentials to the systemd service
       '';
     };
     enableConfigCheck = mkOption {
@@ -70,11 +85,18 @@ in
 
   serviceOpts =
     let
+      configFileToUse =
+        if cfg.config != null then
+          yamlFormat.generate "blackbox-exporter-config.yaml" cfg.config
+        else
+          cfg.configFile;
+
       adjustedConfigFile =
-        if cfg.enableConfigCheck then checkConfig cfg.configFile else checkConfigLocation cfg.configFile;
+        if cfg.enableConfigCheck then checkConfig configFileToUse else checkConfigLocation configFileToUse;
     in
     {
       serviceConfig = {
+        DynamicUser = false;
         AmbientCapabilities = [ "CAP_NET_RAW" ]; # for ping probes
         ExecStart = ''
           ${pkgs.prometheus-blackbox-exporter}/bin/blackbox_exporter \
