ceda087e2196cdf518a467e3e2aeda4d2b9641c4
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-29 02:40:24 +0000

    surrealdb: 2.3.2 -> 2.3.3

--- pkgs/by-name/su/surrealdb/package.nix
+++ pkgs/by-name/su/surrealdb/package.nix
@@ -11,17 +11,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "surrealdb";
-  version = "2.3.2";
+  version = "2.3.3";
 
   src = fetchFromGitHub {
     owner = "surrealdb";
     repo = "surrealdb";
     tag = "v${version}";
-    hash = "sha256-D7eujFJiFHmWlEoVkMkUSIdJt5fx+jTTqDwZs0JJw4o=";
+    hash = "sha256-1qdO9uRuR/s7j58HjA9k3XQWoqdPMRVcReTqIWTdWGc=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-dXLJKwwNIv4QcbROJyWB9oGBqh6bztz37tXMzc5jYUM=";
+  cargoHash = "sha256-OfSSrHwjqIZ8DYE2XAPnHBsPy4ILS+57hLXJdDgafGk=";
 
   # error: linker `aarch64-linux-gnu-gcc` not found
   postPatch = ''
