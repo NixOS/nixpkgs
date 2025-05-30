b792083689859d9ff48b84ae9220d9957e027e94
Johannes Kirschbauer <hsjobeki@gmail.com>
2025-05-30 17:25:51 +0200

    lib/modules: add new merge.v2 for 'types.{either,coercedTo}'

--- lib/modules.nix
+++ lib/modules.nix
@@ -1131,8 +1131,13 @@ let
     # Type-check the remaining definitions, and merge them. Or throw if no definitions.
     mergedValue =
       if isDefined then
-        if type.checkAndMerge or null != null then
-          checkedAndMerged.value
+        if type.merge ? v2 then
+          # check and merge share the same closure
+          # .headError is either non-present null or an error describing string
+          if checkedAndMerged.headError or null != null then
+            throw "A definition for option `${showOption loc}' is not of type `${type.description}'. TypeError: ${checkedAndMerged.headError}"
+          else
+            checkedAndMerged.value
         else if all (def: type.check def.value) defsFinal then
           type.merge loc defsFinal
         else
@@ -1146,14 +1151,29 @@ let
         throw
           "The option `${showOption loc}' was accessed but has no value defined. Try setting the option.";
 
-    checkedAndMerged =
-      if type.checkAndMerge or null != null then
-        type.checkAndMerge loc defsFinal
+    ensureMergedValueEnvelope =
+      v:
+      if attrNames v == attrNames defaultCheckedAndMerged then
+        v
       else
-        {
-          value = mergedValue;
-          valueMeta = { };
-        };
+        throw "Invalid 'type.merge.v2' of type: '${type.name}' must return exactly the following attributes: ${builtins.toJSON (attrNames defaultCheckedAndMerged)} but got ${builtins.toJSON (attrNames v)}";
+
+    defaultCheckedAndMerged = {
+      headError = null;
+      value = mergedValue;
+      valueMeta = { };
+    };
+
+    checkedAndMerged = ensureMergedValueEnvelope (
+      if type.merge ? v2 then
+        type.merge.v2 {
+          inherit loc;
+          defs = defsFinal;
+        }
+      else
+        defaultCheckedAndMerged
+
+    );
 
     isDefined = defsFinal != [ ];
 
