3490c956a1d746911aeadb06d0cd9ea3792d17e9
emaryn <emaryn@users.noreply.github.com>
2025-05-30 11:25:32 +0800

    yubioath-flutter: 7.1.1 -> 7.2.2

--- pkgs/by-name/yu/yubioath-flutter/package.nix
+++ pkgs/by-name/yu/yubioath-flutter/package.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  flutter324,
+  flutter332,
   python3,
   fetchFromGitHub,
   pcre2,
@@ -10,65 +10,33 @@
   gnome-screenshot,
   makeWrapper,
   removeReferencesTo,
+  runCommand,
+  yq,
+  yubioath-flutter,
+  _experimental-update-script-combinators,
+  gitUpdater,
 }:
 
-flutter324.buildFlutterApplication rec {
+flutter332.buildFlutterApplication rec {
   pname = "yubioath-flutter";
-  version = "7.1.1";
+  version = "7.2.2";
 
   src = fetchFromGitHub {
     owner = "Yubico";
     repo = "yubioath-flutter";
-    rev = version;
-    hash = "sha256-MpY6yJvGBaFiEwuGEme2Uvyi5INCYhZJHyaRpC9pCuk=";
+    tag = version;
+    hash = "sha256-XkRSyy845hxQX5Ew5Sx3ZLh9UXfFMF6YdZJx+p/PJpo=";
   };
-
-  passthru.helper = python3.pkgs.callPackage ./helper.nix { inherit src version meta; };
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
-  gitHashes = {
-    window_manager = "sha256-mLX51nbWFccsAfcqLQIYDjYz69y9wAz4U1RZ8TIYSj0=";
-  };
+
+  gitHashes.window_manager = "sha256-WKcNwEOthXj1S2lKlpdhy+r8JZslVqhwY2ywXeTSBEs=";
 
   postPatch = ''
     rm -f pubspec.lock
 
     substituteInPlace linux/CMakeLists.txt \
       --replace-fail "../build/linux/helper" "${passthru.helper}/libexec/helper"
-  '';
-
-  preInstall = ''
-    # Make sure we have permission to delete things CMake has copied in to our build directory from elsewhere.
-    chmod -R +w build
-  '';
-
-  postInstall = ''
-    # Swap the authenticator-helper symlink with the correct symlink.
-    ln -fs "${passthru.helper}/bin/authenticator-helper" "$out/app/$pname/helper/authenticator-helper"
-
-    # Move the icon.
-    mkdir $out/share/icons
-    mv $out/app/$pname/linux_support/com.yubico.yubioath.png $out/share/icons
-
-    # Cleanup.
-    rm -rf \
-      "$out/app/$pname/README.adoc" \
-      "$out/app/$pname/desktop_integration.sh" \
-      "$out/app/$pname/linux_support" \
-      $out/bin/* # We will repopulate this directory later.
-
-    # Symlink binary.
-    ln -sf "$out/app/$pname/authenticator" "$out/bin/yubioath-flutter"
-
-    # Set the correct path to the binary in desktop file.
-    substituteInPlace "$out/share/applications/com.yubico.authenticator.desktop" \
-      --replace "@EXEC_PATH/authenticator" "$out/bin/yubioath-flutter" \
-      --replace "@EXEC_PATH/linux_support/com.yubico.yubioath.png" "$out/share/icons/com.yubico.yubioath.png"
-  '';
-
-  # Needed for QR scanning to work
-  extraWrapProgramArgs = ''
-    --prefix PATH : ${lib.makeBinPath [ gnome-screenshot ]}
   '';
 
   nativeBuildInputs = [
@@ -83,12 +51,63 @@ flutter324.buildFlutterApplication rec {
     libappindicator
   ];
 
-  meta = with lib; {
+  preInstall = ''
+    # Make sure we have permission to delete things CMake has copied in to our build directory from elsewhere.
+    chmod -R +w build
+  '';
+
+  postInstall = ''
+    # Swap the authenticator-helper symlink with the correct symlink.
+    ln -fs "${passthru.helper}/bin/authenticator-helper" "$out/app/$pname/helper/authenticator-helper"
+
+    # Move the icon.
+    mkdir $out/share/pixmaps
+    mv $out/app/$pname/linux_support/com.yubico.yubioath.png $out/share/pixmaps
+
+    # Cleanup.
+    rm -rf \
+      "$out/app/$pname/README.adoc" \
+      "$out/app/$pname/desktop_integration.sh" \
+      "$out/app/$pname/linux_support" \
+      $out/bin/* # We will repopulate this directory later.
+
+    # Symlink binary.
+    ln -sf "$out/app/$pname/authenticator" "$out/bin/yubioath-flutter"
+
+    # Set the correct path to the binary in desktop file.
+    substituteInPlace "$out/share/applications/com.yubico.yubioath.desktop" \
+      --replace-fail '"@EXEC_PATH/authenticator"' "yubioath-flutter" \
+      --replace-fail "@EXEC_PATH/linux_support/com.yubico.yubioath.png" "com.yubico.yubioath"
+  '';
+
+  # Needed for QR scanning to work
+  extraWrapProgramArgs = ''
+    --prefix PATH : ${lib.makeBinPath [ gnome-screenshot ]}
+  '';
+
+  passthru = {
+    helper = python3.pkgs.callPackage ./helper.nix { inherit src version meta; };
+    pubspecSource =
+      runCommand "pubspec.lock.json"
+        {
+          nativeBuildInputs = [ yq ];
+          inherit (yubioath-flutter) src;
+        }
+        ''
+          cat $src/pubspec.lock | yq > $out
+        '';
+    updateScript = _experimental-update-script-combinators.sequence [
+      (gitUpdater { })
+      (_experimental-update-script-combinators.copyAttrOutputToFile "yubioath-flutter.pubspecSource" ./pubspec.lock.json)
+    ];
+  };
+
+  meta = {
     description = "Yubico Authenticator for Desktop";
     mainProgram = "yubioath-flutter";
     homepage = "https://github.com/Yubico/yubioath-flutter";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ lukegb ];
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ lukegb ];
     platforms = [
       "x86_64-linux"
       "aarch64-linux"
