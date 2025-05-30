edf25f8e3edc96647a71f8ce6f8d5e17677a9c30
Vinicius Provenzano <vinicius@provenzano.me>
2025-05-30 18:45:46 +0200

    (filebot)(refactor)(prefer native libmediainfo)

--- pkgs/by-name/fi/filebot/package.nix
+++ pkgs/by-name/fi/filebot/package.nix
@@ -21,6 +21,7 @@ let
     url = "https://search.maven.org/remotecontent?filepath=com/googlecode/lanterna/lanterna/3.1.1/lanterna-3.1.1.jar";
     hash = "sha256-7zxCeXYW5v9ritnvkwRpPKdgSptCmkT3HJOaNgQHUmQ=";
   };
+  system = stdenv.hostPlatform.system;
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "filebot";
@@ -54,20 +55,40 @@ stdenv.mkDerivation (finalAttrs: {
 
   dontBuild = true;
   installPhase = ''
-    mkdir -p $out/opt $out/bin
-    # Since FileBot has dependencies on relative paths between files, all required files are copied to the same location as is.
-    cp -r filebot.sh lib/ jar/ $out/opt/
-    # Filebot writes to $APP_DATA, which fails due to read-only filesystem. Using current user .local directory instead.
-    substituteInPlace $out/opt/filebot.sh \
-      --replace 'APP_DATA="$FILEBOT_HOME/data/$(id -u)"' 'APP_DATA=''${XDG_DATA_HOME:-$HOME/.local/share}/filebot/data' \
-      --replace '$FILEBOT_HOME/data/.license' '$APP_DATA/.license' \
-      --replace '-jar "$FILEBOT_HOME/jar/filebot.jar"' '-Dcom.googlecode.lanterna.terminal.UnixTerminal.sttyCommand=${coreutils}/bin/stty -jar "$FILEBOT_HOME/jar/filebot.jar"'
-    wrapProgram $out/opt/filebot.sh \
-      --prefix PATH : ${lib.makeBinPath [ openjdk17 ]}
-    # Expose the binary in bin to make runnable.
-    ln -s $out/opt/filebot.sh $out/bin/filebot
-  '';
+        mkdir -p $out/opt $out/bin
+        # Since FileBot has dependencies on relative paths between files, all required files are copied to the same location as is.
+        cp -r filebot.sh lib/ jar/ $out/opt/
+        # Filebot writes to $APP_DATA, which fails due to read-only filesystem. Using current user .local directory instead.
+        substituteInPlace $out/opt/filebot.sh \
+          --replace 'APP_DATA="$FILEBOT_HOME/data/$(id -u)"' 'APP_DATA=''${XDG_DATA_HOME:-$HOME/.local/share}/filebot/data' \
+          --replace '$FILEBOT_HOME/data/.license' '$APP_DATA/.license' \
+          --replace '-jar "$FILEBOT_HOME/jar/filebot.jar"' '-Dcom.googlecode.lanterna.terminal.UnixTerminal.sttyCommand=${coreutils}/bin/stty -jar "$FILEBOT_HOME/jar/filebot.jar"'
+        wrapProgram $out/opt/filebot.sh \
+          --prefix PATH : ${lib.makeBinPath [ openjdk17 ]}
+        # Expose the binary in bin to make runnable.
+        ln -s $out/opt/filebot.sh $out/bin/filebot
 
+    # Use nixpkgs default libmediainfo to avoid conflicts
+    system="${system}"
+
+    case $system in
+      x86_64-linux)
+        rm $out/opt/lib/Linux-x86_64/libmediainfo.so
+        ln -s ${lib.getLib libmediainfo}/lib/libmediainfo.so $out/opt/lib/Linux-x86_64/
+        ;;
+      aarch64-linux)
+        rm $out/opt/lib/Linux-aarch64/libmediainfo.so
+        ln -s ${lib.getLib libmediainfo}/lib/libmediainfo.so $out/opt/lib/Linux-aarch64/
+        ;;
+      *)
+    # Keep default libs
+    exit 1
+    esac
+
+
+
+
+  '';
   passthru.updateScript = genericUpdater {
     versionLister = writeShellScript "filebot-versionLister" ''
       curl -s https://www.filebot.net \
