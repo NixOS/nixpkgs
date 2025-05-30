93ff0a9ffc7c7692c5aa397d369d0a6745af474c
R. Ryantm <ryantm-bot@ryantm.com>
2025-05-30 05:13:20 +0000

    python3Packages.python-docs-theme: 2025.4.1 -> 2025.5

--- pkgs/development/python-modules/python-docs-theme/default.nix
+++ pkgs/development/python-modules/python-docs-theme/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "python-docs-theme";
-  version = "2025.4.1";
+  version = "2025.5";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "python";
     repo = "python-docs-theme";
     tag = version;
-    hash = "sha256-fHhgr8JPNLpATAGOBE1lJYA5rBNKZg5paY+MQsgWXqQ=";
+    hash = "sha256-dcxlZGPB36q2MO2WHtshkgNE5ryKtI/RfuVG7hcLcas=";
   };
 
   build-system = [ flit-core ];
