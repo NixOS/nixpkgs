caa704f8b225090bf19039b9a27b6dfdc2ef10b3
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-05-30 12:29:25 +0700

    Updated EM_CACHE fixup with respect to #411251 merge

--- pkgs/development/compilers/emscripten/locate_cache.sh
+++ pkgs/development/compilers/emscripten/locate_cache.sh
@@ -1,6 +1,2 @@
-if [ -z "${EM_CACHE}" ]
-then
-  export EM_CACHE="/tmp/$(basename $(realpath $(dirname $(which emcc))/..))_cache"
-  cp -r $(dirname $(which emcc))/../share/emscripten/cache $EM_CACHE
-  chmod +rwX -R $EM_CACHE
-fi
+export EM_CACHE=${EM_CACHE:-"/tmp/$(basename $(realpath $(dirname $(which emcc))/..))_cache"}
+
