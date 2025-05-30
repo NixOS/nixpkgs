46567de79c4f2e668ac9eceac6b8a11116a8ef65
Vinicius Provenzano <vinicius@provenzano.me>
2025-05-30 19:16:19 +0200

    (filebot)(refactor)(native libmediainfo fix)

--- pkgs/by-name/fi/filebot/package.nix
+++ pkgs/by-name/fi/filebot/package.nix
@@ -1,51 +1,28 @@
-{ system,
-  lib,
-  stdenv,
-  fetchurl,
-  coreutils,
-  openjdk17,
-  makeWrapper,
-  autoPatchelfHook,
-  zlib,
-  libzen,
-  libmediainfo,
-  curlWithGnuTls,
-  libmms,
-  glib,
-  genericUpdater,
-  writeShellScript,
-}:
+{ system, lib, stdenv, fetchurl, coreutils, openjdk17, makeWrapper
+, autoPatchelfHook, zlib, libzen, libmediainfo, curlWithGnuTls, libmms, glib
+, genericUpdater, writeShellScript, }:
 
 let
   lanterna = fetchurl {
-    url = "https://search.maven.org/remotecontent?filepath=com/googlecode/lanterna/lanterna/3.1.1/lanterna-3.1.1.jar";
+    url =
+      "https://search.maven.org/remotecontent?filepath=com/googlecode/lanterna/lanterna/3.1.1/lanterna-3.1.1.jar";
     hash = "sha256-7zxCeXYW5v9ritnvkwRpPKdgSptCmkT3HJOaNgQHUmQ=";
   };
-in
-stdenv.mkDerivation (finalAttrs: {
+in stdenv.mkDerivation (finalAttrs: {
   pname = "filebot";
   version = "5.1.7";
 
   src = fetchurl {
-    url = "https://web.archive.org/web/20230917142929/https://get.filebot.net/filebot/FileBot_${finalAttrs.version}/FileBot_${finalAttrs.version}-portable.tar.xz";
+    url =
+      "https://web.archive.org/web/20230917142929/https://get.filebot.net/filebot/FileBot_${finalAttrs.version}/FileBot_${finalAttrs.version}-portable.tar.xz";
     hash = "sha256-GpjWo2+AsT0hD3CJJ8Pf/K5TbWtG0ZE2tIpH/UEGTws=";
   };
 
   unpackPhase = "tar xvf $src";
 
-  nativeBuildInputs = [
-    makeWrapper
-    autoPatchelfHook
-  ];
+  nativeBuildInputs = [ makeWrapper autoPatchelfHook ];
 
-  buildInputs = [
-    zlib
-    libzen
-    libmediainfo
-    curlWithGnuTls
-    libmms
-    glib
-  ];
+  buildInputs = [ zlib libzen libmediainfo curlWithGnuTls libmms glib ];
 
   postPatch = ''
     # replace lanterna.jar to be able to specify `com.googlecode.lanterna.terminal.UnixTerminal.sttyCommand`
@@ -54,39 +31,43 @@ stdenv.mkDerivation (finalAttrs: {
 
   dontBuild = true;
   installPhase = ''
-    mkdir -p $out/opt $out/bin
-    # Since FileBot has dependencies on relative paths between files, all required files are copied to the same location as is.
-    cp -r filebot.sh lib/ jar/ $out/opt/
-    # Filebot writes to $APP_DATA, which fails due to read-only filesystem. Using current user .local directory instead.
-    substituteInPlace $out/opt/filebot.sh \
-      --replace 'APP_DATA="$FILEBOT_HOME/data/$(id -u)"' 'APP_DATA=''${XDG_DATA_HOME:-$HOME/.local/share}/filebot/data' \
-      --replace '$FILEBOT_HOME/data/.license' '$APP_DATA/.license' \
-      --replace '-jar "$FILEBOT_HOME/jar/filebot.jar"' '-Dcom.googlecode.lanterna.terminal.UnixTerminal.sttyCommand=${coreutils}/bin/stty -jar "$FILEBOT_HOME/jar/filebot.jar"'
-    wrapProgram $out/opt/filebot.sh \
-      --prefix PATH : ${lib.makeBinPath [ openjdk17 ]}
-    # Expose the binary in bin to make runnable.
-    ln -s $out/opt/filebot.sh $out/bin/filebot
+        mkdir -p $out/opt $out/bin
+        # Since FileBot has dependencies on relative paths between files, all required files are copied to the same location as is.
+        cp -r filebot.sh lib/ jar/ $out/opt/
+        # Filebot writes to $APP_DATA, which fails due to read-only filesystem. Using current user .local directory instead.
+        substituteInPlace $out/opt/filebot.sh \
+          --replace 'APP_DATA="$FILEBOT_HOME/data/$(id -u)"' 'APP_DATA=''${XDG_DATA_HOME:-$HOME/.local/share}/filebot/data' \
+          --replace '$FILEBOT_HOME/data/.license' '$APP_DATA/.license' \
+          --replace '-jar "$FILEBOT_HOME/jar/filebot.jar"' '-Dcom.googlecode.lanterna.terminal.UnixTerminal.sttyCommand=${coreutils}/bin/stty -jar "$FILEBOT_HOME/jar/filebot.jar"'
+        wrapProgram $out/opt/filebot.sh \
+          --prefix PATH : ${lib.makeBinPath [ openjdk17 ]}
+        # Expose the binary in bin to make runnable.
+        ln -s $out/opt/filebot.sh $out/bin/filebot
 
-# Use nixpkgs default libmediainfo to avoid conflicts
-system="${system}"
+    # Use nixpkgs default libmediainfo to avoid conflicts
+    system="${system}"
 
-case $system in
-  x86_64-linux)
-    rm $out/opt/lib/Linux-x86_64/libmediainfo.so
-    ln -s ${lib.getLib libmediainfo}/lib/libmediainfo.so $out/opt/lib/Linux-x86_64/
-    ;;
-  aarch64-linux)
-    rm $out/opt/lib/Linux-aarch64/libmediainfo.so
-    ln -s ${lib.getLib libmediainfo}/lib/libmediainfo.so $out/opt/lib/Linux-aarch64/
-    ;;
-  *)
-# Keep default libs
-exit 1
-esac
+    case $system in
+      x86_64-linux)
+        rm $out/opt/lib/Linux-x86_64/libmediainfo.so
+        ln -s ${
+          lib.getLib libmediainfo
+        }/lib/libmediainfo.so $out/opt/lib/Linux-x86_64/
+        ;;
+      aarch64-linux)
+        rm $out/opt/lib/Linux-aarch64/libmediainfo.so
+        ln -s ${
+          lib.getLib libmediainfo
+        }/lib/libmediainfo.so $out/opt/lib/Linux-aarch64/
+        ;;
+      *)
+    # Keep default libs
+    exit 1
+    esac
 
 
 
- '';
+  '';
 
   passthru.updateScript = genericUpdater {
     versionLister = writeShellScript "filebot-versionLister" ''
@@ -104,15 +85,9 @@ esac
     '';
     homepage = "https://filebot.net";
     changelog = "https://www.filebot.net/forums/viewforum.php?f=7";
-    sourceProvenance = with sourceTypes; [
-      binaryBytecode
-      binaryNativeCode
-    ];
+    sourceProvenance = with sourceTypes; [ binaryBytecode binaryNativeCode ];
     license = licenses.unfreeRedistributable;
-    maintainers = with maintainers; [
-      gleber
-      felschr
-    ];
+    maintainers = with maintainers; [ gleber felschr ];
     platforms = platforms.linux;
     mainProgram = "filebot";
   };
