f87e577212cc269425b4a2fc66501aba94adf095
Gavin John <gavinnjohn@gmail.com>
2025-05-29 23:58:22 -0700

    qgroundcontrol: 4.4.5 -> 5.0.2

--- pkgs/by-name/qg/qgroundcontrol/package.nix
+++ pkgs/by-name/qg/qgroundcontrol/package.nix
@@ -2,72 +2,98 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  cmake,
+  cpm-cmake,
+  geographiclib,
+  ulog-cpp,
+  ninja,
   SDL2,
-  libsForQt5,
   gst_all_1,
   wayland,
   pkg-config,
+  qt6,
 }:
 
-stdenv.mkDerivation rec {
+# TODO:
+#  * Upstream patch
+stdenv.mkDerivation (finalAttrs: {
   pname = "qgroundcontrol";
-  version = "4.4.5";
+  version = "5.0.2";
 
-  propagatedBuildInputs = with libsForQt5; [
+  src = fetchFromGitHub {
+    owner = "mavlink";
+    repo = "qgroundcontrol";
+    tag = "v${finalAttrs.version}";
+    fetchSubmodules = true;
+    hash = "sha256-MNhW4/a/fEVtbp88KBYWl5XE7sleszhXBr8WZ5UOGxg=";
+  };
+
+  patches = [
+    ./disable-bad-message.patch
+  ];
+
+  # Unvendor CPM
+  postPatch = ''
+    rm cmake/modules/CPM.cmake
+    ln -s ${cpm-cmake}/share/cpm/CPM.cmake cmake/modules/CPM.cmake
+  '';
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    pkg-config
+  ] ++ (with qt6; [
+    qttools
+    wrapQtAppsHook
+  ]);
+
+  propagatedBuildInputs = with qt6; [
     qtbase
     qtcharts
     qtlocation
     qtserialport
     qtsvg
-    qtquickcontrols2
-    qtgraphicaleffects
     qtspeech
-    qtx11extras
+    qtsensors
+    qt5compat
   ];
 
   gstInputs = with gst_all_1; [
     gstreamer
     gst-plugins-base
-    (gst-plugins-good.override { qt5Support = true; })
+    (gst-plugins-good.override { qt6Support = true; })
     gst-plugins-bad
     gst-libav
     wayland
   ];
 
-  buildInputs = [ SDL2 ] ++ gstInputs ++ propagatedBuildInputs;
-  nativeBuildInputs =
-    [ pkg-config ]
-    ++ (with libsForQt5; [
-      qmake
-      qttools
-      wrapQtAppsHook
-    ]);
+  buildInputs = [
+    SDL2
+    geographiclib
+    ulog-cpp
+  ] ++ finalAttrs.gstInputs ++ finalAttrs.propagatedBuildInputs;
 
-  preConfigure = ''
-    mkdir build
-    cd build
-  '';
-
-  qmakeFlags = [
-    "CONFIG+=StableBuild"
+  cmakeFlags = [
+    "-DQGC_BUILD_STABLE=ON"
+    # Override maximum version to always accept the packaged qr6
+    "-DQGC_QT_MAXIMUM_VERSION=${qt6.qtbase.version}"
     # Default install tries to copy Qt files into package
-    "CONFIG+=QGC_DISABLE_BUILD_SETUP"
+    "-DQGC_DISABLE_BUILD_SETUP=ON"
+    # Disables an /etc/group check that has false positives on nixos
+    "-DQGC_DISABLE_ETC_GROUP_CHECKS=ON"
     # Tries to download x86_64-only prebuilt binaries
-    "DEFINES+=DISABLE_AIRMAP"
-    "../qgroundcontrol.pro"
+    "-DDISABLE_AIRMAP=ON"
   ];
 
   installPhase = ''
     runHook preInstall
 
-    cd ..
-
     mkdir -p $out/share/applications
-    sed 's/Exec=.*$/Exec=QGroundControl/g' --in-place deploy/qgroundcontrol.desktop
+    sed 's/Exec=.*/Exec=QGroundControl/' --in-place deploy/qgroundcontrol.desktop
     cp -v deploy/qgroundcontrol.desktop $out/share/applications
 
     mkdir -p $out/bin
-    cp -v build/staging/QGroundControl "$out/bin/"
+    cp -v bin/QGroundControl $out/bin/
 
     mkdir -p $out/share/qgroundcontrol
     cp -rv resources/ $out/share/qgroundcontrol
@@ -82,19 +108,6 @@ stdenv.mkDerivation rec {
     qtWrapperArgs+=(--prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0")
   '';
 
-  # TODO: package mavlink so we can build from a normal source tarball
-  src = fetchFromGitHub {
-    owner = "mavlink";
-    repo = "qgroundcontrol";
-    rev = "v${version}";
-    hash = "sha256-wjrfwE97J+UzBPIARQ6cPadN6xIdqR8i+ZKbtiDproM=";
-    fetchSubmodules = true;
-  };
-
-  patches = [
-    ./disable-bad-message.patch
-  ];
-
   meta = {
     description = "Provides full ground station support and configuration for the PX4 and APM Flight Stacks";
     homepage = "https://qgroundcontrol.com/";
@@ -107,4 +120,4 @@ stdenv.mkDerivation rec {
     ];
     mainProgram = "QGroundControl";
   };
-}
+})
