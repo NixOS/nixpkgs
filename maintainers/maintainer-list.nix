41330bbd9ae34b60e43e53fb4d5f5a45f34dab7f
cardboardcpu <lcarratal7@alumno.uned.es>
2025-05-31 23:59:12 +0200

    basalt-tui: init at 0.5.0
    Signed-off-by: cardboardcpu <lcarratal7@alumno.uned.es>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4091,6 +4091,13 @@
     github = "CaptainJawZ";
     githubId = 43111068;
   };
+  # TODO: Figure out handles?
+  cardboardcpu = {
+    name = "Lucas Carratalá";
+    email = "lcarratal7@alumno.uned.es";
+    github = "cardboardcpu";
+    githubId = 185692243;
+  };
   CardboardTurkey = {
     name = "Kiran Ostrolenk";
     email = "kiran@ostrolenk.co.uk";
