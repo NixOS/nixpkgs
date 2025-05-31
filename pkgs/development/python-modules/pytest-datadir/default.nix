3adc150fd559ad371d05274aa7fd97feb47d3cd9
Kira Bruneau <kira.bruneau@pm.me>
2025-05-31 13:30:06 -0400

    python313Packages.pytest-datadir: 1.6.1 -> 1.7.2

--- pkgs/development/python-modules/pytest-datadir/default.nix
+++ pkgs/development/python-modules/pytest-datadir/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "pytest-datadir";
-  version = "1.6.1";
+  version = "1.7.2";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "gabrielcnr";
     repo = "pytest-datadir";
     tag = "v${version}";
-    hash = "sha256-qyJlg9Ck128NpJhLw2x7LuNtdSx3AaTQEUQ7fb2Aglg=";
+    hash = "sha256-0y+1Al8nocCJXZyu8gLbYnXJzUu/oD31Zhn901XxWds=";
   };
 
   nativeBuildInputs = [
