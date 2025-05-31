6236c5a25081f2623732dd66754cfcaf1d4f94c6
Alois Wohlschlager <alois1@gmx-topmail.de>
2025-05-31 11:49:50 +0200

    lixPackageSets.git: 2.94.0-pre-20250516_a7634f87aac5 -> 2.94.0-pre-20250602_fca0a30470b7

--- pkgs/tools/package-management/lix/default.nix
+++ pkgs/tools/package-management/lix/default.nix
@@ -234,14 +234,14 @@ lib.makeExtensible (self: {
     attrName = "git";
 
     lix-args = rec {
-      version = "2.94.0-pre-20250516_${builtins.substring 0 12 src.rev}";
+      version = "2.94.0-pre-20250602_${builtins.substring 0 12 src.rev}";
 
       src = fetchFromGitea {
         domain = "git.lix.systems";
         owner = "lix-project";
         repo = "lix";
-        rev = "a7634f87aac545fa01fa19878cc5ad2c994e2116";
-        hash = "sha256-+yX+xF1cZUd1Pub7MJ7uGcC6JQ0FN+CsEmBg6rGLfjU=";
+        rev = "fca0a30470b7040489feeb2a86bad05bf9b1aa95";
+        hash = "sha256-kbY82p5J5HP/y132Pn6JVO1/PKV/TqF336FsDArWkLo=";
       };
 
       cargoDeps = rustPlatform.fetchCargoVendor {
