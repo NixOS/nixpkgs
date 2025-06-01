5dc3c3c7b9b0cc0c9b4384a7ef0bfc63a4d8dbab
Fabian Affolter <mail@fabian-affolter.ch>
2025-06-01 08:31:28 +0200

    python313Packages.python-linkplay: 0.2.5 -> 0.2.9
    Diff: https://github.com/Velleman/python-linkplay/compare/refs/tags/v0.2.5...refs/tags/v0.2.9
    
    Changelog: https://github.com/Velleman/python-linkplay/releases/tag/v0.2.9

--- pkgs/development/python-modules/python-linkplay/default.nix
+++ pkgs/development/python-modules/python-linkplay/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "python-linkplay";
-  version = "0.2.5";
+  version = "0.2.9";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Velleman";
     repo = "python-linkplay";
     tag = "v${version}";
-    hash = "sha256-bPWQz7rJiRA1OI3o2LimU1R0hC80UXPxFtGYQpL190w=";
+    hash = "sha256-syCqa9xDuAmsq9eh29PACchdLN1bPj/vCCt3a++LqE4=";
   };
 
   build-system = [ setuptools ];
