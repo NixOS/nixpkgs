de8144a7e356e0adfcae232d3a3bd25522e75ad1
cybardev <50134239+cybardev@users.noreply.github.com>
2025-06-01 05:18:04 -0300

    pyrefly: init at 0.17.1
    Co-authored-by: QuiNzX <76129478+QuiNzX@users.noreply.github.com>

---
+++ pkgs/by-name/py/pyrefly/package.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  python3,
+  fetchPypi,
+  versionCheckHook,
+  rustPlatform,
+  maturin,
+}:
+python3.pkgs.buildPythonApplication rec {
+  pname = "pyrefly";
+  version = "0.17.1";
+  pyproject = true;
+
+  # fetch from PyPI instead of GitHub, since source repo does not have Cargo.lock
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-w4ivRtmApXiXQT95GI4vvYBop7yxdbbkpW+YTyFtgXM=";
+  };
+
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit src;
+    hash = "sha256-Op5ueVkzZTiJ1zeBGVi8oeLcfSzXMYfk5zEg4OGyA5g=";
+  };
+
+  build-system = [ maturin ];
+
+  nativeBuildInputs = with rustPlatform; [
+    cargoSetupHook
+    maturinBuildHook
+  ];
+
+  nativeCheckInputs = [ versionCheckHook ];
+
+  # requires unstable rust features
+  env.RUSTC_BOOTSTRAP = 1;
+
+  meta = {
+    description = "Fast type checker and IDE for Python";
+    homepage = "https://github.com/facebook/pyrefly";
+    license = lib.licenses.mit;
+    mainProgram = "pyrefly";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    maintainers = with lib.maintainers; [
+      cybardev
+      QuiNzX
+    ];
+  };
+}
