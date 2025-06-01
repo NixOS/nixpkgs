8d6042b1b48a5b643495563e7ea482acdbacfa77
Sergei Trofimovich <slyich@gmail.com>
2025-06-01 10:37:59 +0100

    libplacebo: revert `mpv`-breaking change
    Without the change `mpv` fails to output any video with
    errors like:
    
        $ nix run -f. mpv /tmp/img.jpg
        ...
        [vo/gpu/libplacebo] vk_compile_glsl(gpu, tmp, GLSL_SHADER_VERTEX, params->vertex_shader, &vert): VK_ERROR_INITIALIZATION_FAILED (../src/vulkan/gpu_pass.c:460)
    
    Let's revert it for now until upstream provides more details.

--- pkgs/by-name/li/libplacebo/package.nix
+++ pkgs/by-name/li/libplacebo/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitLab,
+  fetchpatch,
   meson,
   ninja,
   pkg-config,
@@ -30,6 +31,17 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     hash = "sha256-ccoEFpp6tOFdrfMyE0JNKKMAdN4Q95tP7j7vzUj+lSQ=";
   };
+
+  patches = [
+    # Breaks mpv vulkan shaders:
+    #   https://code.videolan.org/videolan/libplacebo/-/issues/335
+    (fetchpatch {
+      name = "fix-shaders.patch";
+      url = "https://github.com/haasn/libplacebo/commit/4c6d99edee23284f93b07f0f045cd660327465eb.patch";
+      revert = true;
+      hash = "sha256-zoCgd9POlhFTEOzQmSHFZmJXgO8Zg/f9LtSTSQq5nUA=";
+    })
+  ];
 
   nativeBuildInputs = [
     meson
