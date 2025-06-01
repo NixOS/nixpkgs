5f92b15f0151626c1b209e881a6520eacec4c0b9
lelgenio <lelgenio@lelgenio.com>
2025-06-01 03:45:32 -0300

    marge-bot: 0.10.1 -> 0.15.3

--- pkgs/by-name/ma/marge-bot/package.nix
+++ pkgs/by-name/ma/marge-bot/package.nix
@@ -2,55 +2,49 @@
   lib,
   python3,
   fetchFromGitLab,
+  git,
+  openssh,
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "marge-bot";
-  version = "0.10.1";
+  version = "0.15.3";
   pyproject = true;
 
   src = fetchFromGitLab {
     owner = "marge-org";
     repo = "marge-bot";
     rev = version;
-    hash = "sha256-2L7c/NEKyjscwpyf/5GtWXr7Ig14IQlRR5IbDYxp8jA=";
+    hash = "sha256-i/hnfoBxgP1mo4RV4F10+QOOkPP/fkcwqaLKBlOuP0I=";
   };
-
-  postPatch = ''
-    substituteInPlace setup.cfg --replace-fail "--flake8 --pylint" ""
-  '';
 
   nativeBuildInputs = [
     python3.pkgs.setuptools
   ];
 
-  propagatedBuildInputs = with python3.pkgs; [
-    configargparse
-    maya
-    pyyaml
-    requests
-  ];
+  propagatedBuildInputs =
+    (with python3.pkgs; [
+      configargparse
+      maya
+      pyyaml
+      requests
+      python-gitlab
+      hatchling
+    ])
+    ++ [
+      git
+      openssh
+    ];
 
-  nativeCheckInputs = with python3.pkgs; [
-    pytest-cov-stub
-    pytestCheckHook
-    pendulum
-  ];
-
-  disabledTests = [
-    # test broken when run under Nix:
-    #   "unittest.mock.InvalidSpecError: Cannot spec a Mock object."
-    "test_get_mr_ci_status"
-    # broken because of an incorrect assertion:
-    #   "AttributeError: 'has_calls' is not a valid assertion."
-    "test_reapprove"
-  ];
-
-  disabledTestPaths = [
-    # test errors due to API mismatch in test setup:
-    #   "ImportError: cannot import name 'set_test_now' from 'pendulum.helpers'"
-    "tests/test_interval.py"
-  ];
+  nativeCheckInputs =
+    (with python3.pkgs; [
+      pytest-cov-stub
+      pytestCheckHook
+      pendulum
+    ])
+    ++ [
+      git
+    ];
 
   pythonImportsCheck = [ "marge" ];
 
