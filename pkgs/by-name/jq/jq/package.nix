c07aad31f2d902646712cd3eba2abf26412049fb
Grimmauld <Grimmauld@grimmauld.de>
2025-06-01 10:57:02 +0200

    jq: 1.7.1 -> 1.8.0, modernize
    Upstream release notes: https://github.com/jqlang/jq/releases/tag/jq-1.8.0
    
    The security fixes were backported, so this is not immediately security-relevant.
    
    Breaking changes are included in 1.8.0, so this should not be backported to 25.05/24.11.
    
    Modernisation done:
    - update script
    - removed `with lib;`
    - removed patches that were merged upstream
    - enabled parallel building
    - enabled strict deps
    - replaced `--help > /dev/null` with `versionCheckHook`
    - replaced `rev` with `finalAttrs`
    - added changelog url

--- pkgs/by-name/jq/jq/package.nix
+++ pkgs/by-name/jq/jq/package.nix
@@ -7,16 +7,19 @@
   bison,
   onigurumaSupport ? true,
   oniguruma,
+  tzdata,
+  versionCheckHook,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "jq";
-  version = "1.7.1";
+  version = "1.8.0";
 
   # Note: do not use fetchpatch or fetchFromGitHub to keep this package available in __bootPackages
   src = fetchurl {
-    url = "https://github.com/jqlang/jq/releases/download/jq-${version}/jq-${version}.tar.gz";
-    hash = "sha256-R4ycoSn9LjRD/icxS0VeIR4NjGC8j/ffcDhz3u7lgMI=";
+    url = "https://github.com/jqlang/jq/releases/download/jq-${finalAttrs.version}/jq-${finalAttrs.version}.tar.gz";
+    hash = "sha256-kYEVd/kdmmGV/1DCv/7JtyyEKdwF7D6gIv2VwG0rMZw=";
   };
 
   outputs = [
@@ -25,30 +28,6 @@ stdenv.mkDerivation rec {
     "man"
     "dev"
     "out"
-  ];
-
-  patches = [
-    # can't fetchpatch because jq is in bootstrap for darwin
-    # CVE-2025-48060
-    # https://github.com/jqlang/jq/commit/dc849e9bb74a7a164a3ea52f661cc712b1ffbd43
-    ./0001-Improve-performance-of-repeating-strings-3272.patch
-
-    # needed for the other patches to apply correctly
-    # https://github.com/jqlang/jq/commit/b86ff49f46a4a37e5a8e75a140cb5fd6e1331384
-    ./0002-fix-jv_number_value-should-cache-the-double-value-of.patch
-
-    # CVE-2024-53427
-    # https://github.com/jqlang/jq/commit/a09a4dfd55e6c24d04b35062ccfe4509748b1dd3
-    ./0003-Reject-NaN-with-payload-while-parsing-JSON.patch
-
-    # CVE-2024-23337
-    # https://github.com/jqlang/jq/commit/de21386681c0df0104a99d9d09db23a9b2a78b1e
-    ./0004-Fix-signed-integer-overflow-in-jvp_array_write-and-j.patch
-
-    # CVE-2025-48060, part two
-    # Improve-performance-of-repeating-strings is only a partial fix
-    # https://github.com/jqlang/jq/commit/c6e041699d8cd31b97375a2596217aff2cfca85b
-    ./0005-Fix-heap-buffer-overflow-when-formatting-an-empty-st.patch
   ];
 
   # https://github.com/jqlang/jq/issues/2871
@@ -61,15 +40,18 @@ stdenv.mkDerivation rec {
   # doesn't keep.
   preConfigure = ''
     echo "#!/bin/sh" > scripts/version
-    echo "echo ${version}" >> scripts/version
+    echo "echo ${finalAttrs.version}" >> scripts/version
     patchShebangs scripts/version
   '';
 
   # paranoid mode: make sure we never use vendored version of oniguruma
   # Note: it must be run after automake, or automake will complain
   preBuild = ''
-    rm -r ./modules/oniguruma
+    rm -r ./vendor/oniguruma
   '';
+
+  strictDeps = true;
+  enableParallelBuilding = true;
 
   buildInputs = lib.optionals onigurumaSupport [ oniguruma ];
   nativeBuildInputs = [
@@ -87,9 +69,7 @@ stdenv.mkDerivation rec {
     ]
     ++ lib.optional (!onigurumaSupport) "--with-oniguruma=no"
     # jq is linked to libjq:
-    ++ lib.optional (!stdenv.hostPlatform.isDarwin) "LDFLAGS=-Wl,-rpath,\\\${libdir}"
-    # https://github.com/jqlang/jq/issues/3252
-    ++ lib.optional stdenv.hostPlatform.isOpenBSD "CFLAGS=-D_BSD_SOURCE=1";
+    ++ lib.optional (!stdenv.hostPlatform.isDarwin) "LDFLAGS=-Wl,-rpath,\\\${libdir}";
 
   # jq binary includes the whole `configureFlags` in:
   # https://github.com/jqlang/jq/commit/583e4a27188a2db097dd043dd203b9c106bba100
@@ -105,25 +85,39 @@ stdenv.mkDerivation rec {
 
   doInstallCheck = true;
   installCheckTarget = "check";
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  preInstallCheck = ''
+    substituteInPlace tests/shtest \
+      --replace-fail "TZ=" "TZ=${tzdata}/share/zoneinfo/"
+  '';
 
   postInstallCheck = ''
-    $bin/bin/jq --help >/dev/null
     $bin/bin/jq -r '.values[1]' <<< '{"values":["hello","world"]}' | grep '^world$' > /dev/null
   '';
 
-  passthru = { inherit onigurumaSupport; };
+  passthru = {
+    inherit onigurumaSupport;
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "jq-(.+)"
+      ];
+    };
+  };
 
-  meta = with lib; {
+  meta = {
+    changelog = "https://github.com/jqlang/jq/releases/tag/jq-${finalAttrs.version}";
     description = "Lightweight and flexible command-line JSON processor";
     homepage = "https://jqlang.github.io/jq/";
-    license = licenses.mit;
-    maintainers = with maintainers; [
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
       raskin
       artturin
       ncfavier
     ];
-    platforms = platforms.unix;
+    platforms = lib.platforms.unix;
     downloadPage = "https://jqlang.github.io/jq/download/";
     mainProgram = "jq";
   };
-}
+})
