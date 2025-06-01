6de8e57261066b5fa14589d5a9b5e6de3eb2407e
Marco "Capypara" Köpcke <hello@capypara.de>
2025-06-01 18:01:29 +0200

    jetbrains.plugins: fix pycharm-community detection (see also parent commit)

--- pkgs/applications/editors/jetbrains/plugins/update_plugins.py
+++ pkgs/applications/editors/jetbrains/plugins/update_plugins.py
@@ -171,7 +171,15 @@ def make_plugin_files(plugin_infos: dict, ide_versions: dict, quiet: bool, extra
             "builds": {},
             "name": names[pid]
         }
-        relevant_builds = [builds for ide, builds in ide_versions.items() if ide in plugin_versions["compatible"]] + [extra_builds]
+        relevant_builds = [
+            builds for ide, builds 
+            in ide_versions.items() 
+            if (
+                ide in plugin_versions["compatible"]
+                # TODO: Remove this once we removed pycharm-community
+                or (ide == "pycharm-community" and "pycharm" in plugin_versions["compatible"])
+            )
+        ] + [extra_builds]
         relevant_builds = sorted(list(set(flatten(relevant_builds))))  # Flatten, remove duplicates and sort
         for build in relevant_builds:
             plugin_versions["builds"][build] = get_newest_compatible(pid, build, plugin_infos[pid], quiet)
