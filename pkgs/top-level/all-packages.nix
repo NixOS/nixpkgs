245dd1ef6ca3ee4f46fbd4d09315a72962b8949e
Adrian Grucza <adrian.grucza@iress.com>
2025-06-02 09:26:04 +1000

    Remove redundant parentheses

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1164,23 +1164,21 @@ with pkgs;
     jdk = jdk8;
   };
 
-  sonar-scanner-cli-minimal = (
-    sonar-scanner-cli.override {
-      jre = jre_minimal.override {
-        jdk = jdk_headless;
-        modules = [
-          "java.base"
-          "java.logging"
-          "java.naming"
-          "java.sql"
-          "java.xml"
-          "jdk.crypto.ec"
-          "jdk.jdwp.agent"
-          "jdk.unsupported"
-        ];
-      };
-    }
-  );
+  sonar-scanner-cli-minimal = sonar-scanner-cli.override {
+    jre = jre_minimal.override {
+      jdk = jdk_headless;
+      modules = [
+        "java.base"
+        "java.logging"
+        "java.naming"
+        "java.sql"
+        "java.xml"
+        "jdk.crypto.ec"
+        "jdk.jdwp.agent"
+        "jdk.unsupported"
+      ];
+    };
+  };
 
   supermin = callPackage ../tools/virtualization/supermin {
     ocamlPackages = ocaml-ng.ocamlPackages_4_14;
