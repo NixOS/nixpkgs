450316edc4a528842dda14715345aee7f48d7a2c
Kalle Ahlström <kalle.ahlstrom@nitor.com>
2025-06-02 00:19:43 +0300

    intune-portal: 1.2405.17-jammy -> 1.2503.10-noble

--- pkgs/by-name/in/intune-portal/package.nix
+++ pkgs/by-name/in/intune-portal/package.nix
@@ -8,8 +8,8 @@
   curlMinimal,
   openssl_3,
   libsecret,
-  webkitgtk_4_0,
-  libsoup_2_4,
+  webkitgtk_4_1,
+  libsoup_3,
   gtk3,
   atk,
   pango,
@@ -19,6 +19,7 @@
   systemd,
   msalsdk-dbusclient,
   pam,
+  p11-kit,
   dbus,
   nixosTests,
 }:
@@ -29,11 +30,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "intune-portal";
-  version = "1.2405.17-jammy";
+  version = "1.2503.10-noble";
 
   src = fetchurl {
-    url = "https://packages.microsoft.com/ubuntu/22.04/prod/pool/main/i/intune-portal/intune-portal_${version}_amd64.deb";
-    hash = "sha256-WpVPWzh8jN092MaY2rMXhLfpVXsflMl9hOY9nNGJlLk=";
+    url = "https://packages.microsoft.com/ubuntu/24.04/prod/pool/main/i/intune-portal/intune-portal_${version}_amd64.deb";
+    hash = "sha256-NlJ8m7V1yLErOntprHs3EagPtwSzYWd7NBH0jc72+i4=";
   };
 
   nativeBuildInputs = [ dpkg ];
@@ -48,12 +49,13 @@ stdenv.mkDerivation rec {
           curlMinimal_openssl_3
           openssl_3
           libsecret
-          webkitgtk_4_0
-          libsoup_2_4
+          webkitgtk_4_1
+          libsoup_3
           gtk3
           atk
           glib
           pango
+          p11-kit
           sqlite
           zlib
           systemd
