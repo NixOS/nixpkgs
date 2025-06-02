869031cfa94e648990a1fe393571665258377bb7
Guy Chronister <guychronister@outlook.com>
2025-06-02 15:02:08 +0000

    treewide: remove usage of pname from all ruby projects

--- pkgs/by-name/co/coltrane/package.nix
+++ pkgs/by-name/co/coltrane/package.nix
@@ -4,19 +4,19 @@
   bundlerUpdateScript,
 }:
 
-bundlerApp rec {
+bundlerApp {
   pname = "coltrane";
   gemdir = ./.;
   exes = [ "coltrane" ];
 
-  passthru.updateScript = bundlerUpdateScript pname;
+  passthru.updateScript = bundlerUpdateScript "coltrane";
 
   meta = with lib; {
     homepage = "https://github.com/pedrozath/coltrane";
     description = "Music calculation library/CLI";
     longDescription = ''
       coltrane allows to search for Notes, Chords, Scales for
-      guitar, bass, piano and ukelele
+      guitar, bass, piano and ukulele
     '';
     license = licenses.mit;
     maintainers = [ maintainers.panaeon ];
