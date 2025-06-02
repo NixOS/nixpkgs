90b30289448dbcfa4df9299c06d9d50c991531d0
bandithedoge <bandithedoge@protonmail.com>
2025-06-02 08:18:55 +0200

    maintainers: add bandithedoge

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2757,6 +2757,13 @@
     githubId = 68944906;
     name = "Nikita";
   };
+  bandithedoge = {
+    email = "bandithedoge@protonmail.com";
+    matrix = "@bandithedoge:matrix.org";
+    github = "bandithedoge";
+    githubId = 26331682;
+    name = "Mikołaj Lercher";
+  };
   bandresen = {
     email = "bandresen@gmail.com";
     github = "bennyandresen";
