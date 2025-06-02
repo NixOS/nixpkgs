c9eb4b2cff5d68f2234abdc4a77a66156e95b72f
Fabian Affolter <mail@fabian-affolter.ch>
2025-06-02 23:10:43 +0200

    python313Packages.pyswitchbot: 0.64.1 -> 0.65.0
    Diff: https://github.com/Danielhiversen/pySwitchbot/compare/refs/tags/0.64.1...refs/tags/0.65.0
    
    Changelog: https://github.com/Danielhiversen/pySwitchbot/releases/tag/0.65.0

--- pkgs/development/python-modules/pyswitchbot/default.nix
+++ pkgs/development/python-modules/pyswitchbot/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "pyswitchbot";
-  version = "0.64.1";
+  version = "0.65.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "Danielhiversen";
     repo = "pySwitchbot";
     tag = version;
-    hash = "sha256-G5r/if9YM/YspLLxA7TAz/Ewc0Ryqtm7SAA+mYwQXVo=";
+    hash = "sha256-PxTkX0e3idwKR+3b5u6/71QN0YeJdmNmZx1vscAtfik=";
   };
 
   build-system = [ setuptools ];
