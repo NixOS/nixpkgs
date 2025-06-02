a52feb8cf0c53fc77a03aa03e694347c65ace8a6
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2025-06-01 23:04:08 -0400

    fittrackee: 0.9.10 -> 0.10.2
    https://github.com/SamR1/FitTrackee/releases/tag/v0.9.11
    
    https://github.com/SamR1/FitTrackee/releases/tag/v0.10.0
    
    https://github.com/SamR1/FitTrackee/releases/tag/v0.10.1
    
    https://github.com/SamR1/FitTrackee/releases/tag/v0.10.2

--- pkgs/by-name/fi/fittrackee/package.nix
+++ pkgs/by-name/fi/fittrackee/package.nix
@@ -8,14 +8,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "fittrackee";
-  version = "0.9.10";
+  version = "0.10.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "SamR1";
     repo = "FitTrackee";
     tag = "v${version}";
-    hash = "sha256-004M7Uhsl0K8BX19eVU4NrvBeAyUJx/mBlC/R27y9jg=";
+    hash = "sha256-ZCQ4Ft2TSjS62DmGDpQ7gG5Spnf82v82i5nnZtg1UmA=";
   };
 
   build-system = [
@@ -24,9 +24,11 @@ python3Packages.buildPythonApplication rec {
 
   pythonRelaxDeps = [
     "authlib"
+    "flask"
     "flask-limiter"
     "flask-migrate"
     "nh3"
+    "lxml"
     "pyopenssl"
     "pytz"
     "sqlalchemy"
@@ -39,6 +41,8 @@ python3Packages.buildPythonApplication rec {
       babel
       click
       dramatiq
+      dramatiq-abort
+      fitdecode
       flask
       flask-bcrypt
       flask-dramatiq
@@ -56,8 +60,9 @@ python3Packages.buildPythonApplication rec {
       pytz
       shortuuid
       sqlalchemy
-      staticmap
+      staticmap3
       ua-parser
+      xmltodict
     ]
     ++ dramatiq.optional-dependencies.redis
     ++ flask-limiter.optional-dependencies.redis;
