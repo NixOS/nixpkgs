NixOS/nixpkgs

commit 63e1340fbc7889a3496187907abf5fd1546c635a

skawarePackages.skalibs: 2.14.3.0 -> 2.14.4.0

pkgs/development/skaware-packages/skalibs/default.nix
@@ -7,8 +7,8 @@
 
 skawarePackages.buildPackage {
   pname = "skalibs";
-  version = "2.14.3.0";
-  sha256 = "sha256-oUqlWMmwmwYvoWrOxiOyyPk9afXLpNB/bQxYkTBmxCc=";
+  version = "2.14.4.0";
+  sha256 = "sha256-DmJiYYSMySBzj5L9UKJMFLIeMDBt/tl7hDU2n0uuAKU=";
 
   description = "Set of general-purpose C programming libraries";
 

commit d2ee1e306df1664d1c45b48536239e2d318b01dc

skalibs: fix cross
Fixes: 63e1340fbc78 ("skawarePackages.skalibs: 2.14.3.0 -> 2.14.4.0")

pkgs/development/skaware-packages/skalibs/default.nix
@@ -33,6 +33,15 @@ skawarePackages.buildPackage {
 
     ]
     ++ lib.optionals (stdenv.buildPlatform.config != stdenv.hostPlatform.config) [
+      # There's a fallback path for BSDs.
+      "--with-sysdep-procselfexe=${
+        if stdenv.hostPlatform.isLinux then
+          "/proc/self/exe"
+        else if stdenv.hostPlatform.isSunOS then
+          "/proc/self/path/a.out"
+        else
+          "none"
+      }"
       # ./configure: sysdep posixspawnearlyreturn cannot be autodetected
       # when cross-compiling. Please manually provide a value with the
       # --with-sysdep-posixspawnearlyreturn=yes|no|... option.