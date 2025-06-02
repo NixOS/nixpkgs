455eb44488c16dc38b82c10f194d92b319eabe49
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-06-02 15:38:30 +0300

    teleport_15: 15.4.33 -> 15.5.2

--- pkgs/servers/teleport/15/default.nix
+++ pkgs/servers/teleport/15/default.nix
@@ -2,11 +2,11 @@
 import ../generic.nix (
   args
   // {
-    version = "15.4.33";
-    hash = "sha256-glf0JCsUZD0o3C+3lJ9V2wvamZa8bKySVi/sLo11Mvk=";
-    vendorHash = "sha256-J4N2VbiIzfyZJtFvmwW6xZLxB2F9bDRRDfOaWLoHT/4=";
-    yarnHash = "sha256-3Ady46LXa3TSSwmWY+GDiS1j5qR5VIuJ5Z+iW5g0SAA=";
-    cargoHash = "sha256-aXKrUo+Q9gK09TP9W2PQ3TjDwBtIlUopZL6AJVAiszE=";
+    version = "15.5.2";
+    hash = "sha256-+6+OTQ7G3c4xYhTqJIYuV95xjlBjSw7lnts/7/lXXzM=";
+    vendorHash = "sha256-DNyC5duFufWBk0F2mMAMHHTVusCbNKVtZG2UOUQfZMI=";
+    yarnHash = "sha256-TOWqpMaZLGhdUaN3r3fGgviKi9BbZHoLcu3d7oLr2gk=";
+    cargoHash = "sha256-aXKrUo+Q9gK09TP9W2PQ3TjDwBtIlUopZL6AJVAiszE";
 
     wasm-bindgen-cli = wasm-bindgen-cli.override {
       version = "0.2.100";
