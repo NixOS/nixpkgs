524a55862fefed567ebce45756c285bb9d92eea1
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-04 01:47:17 +0800

    uv: 0.7.9 -> 0.7.10 (cherry picked from commit a714c291e04a18a610c2d646e9bfed99d80bff17)

--- pkgs/by-name/uv/uv/package.nix
+++ pkgs/by-name/uv/uv/package.nix
@@ -18,17 +18,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "uv";
-  version = "0.7.9";
+  version = "0.7.10";
 
   src = fetchFromGitHub {
     owner = "astral-sh";
     repo = "uv";
     tag = finalAttrs.version;
-    hash = "sha256-Q2HOR8kpHuCB4c61vS0yojyue5tYNCHpx/NuG0HGHl4=";
+    hash = "sha256-NdePwYsAIphAJxtD+sCC7vSouWt+i+uPEp+b0zRlLg0=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-oIgvufZsS65gL0SL8WQggjZ3UFPDfEpf0yM+kkkUiqg=";
+  cargoHash = "sha256-vPs6VwhnMsnoTpktdLlOdYcw1X35xN/alwZ5zEyHmEY=";
 
   buildInputs = [
     rust-jemalloc-sys
