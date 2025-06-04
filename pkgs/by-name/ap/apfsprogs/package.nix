f406f54c5bdef54ae58746b80960a148ce007a56
Luflosi <luflosi@luflosi.de>
2025-06-04 12:34:21 +0200

    apfsprogs: 0.2.0 -> 0.2.1
    https://github.com/linux-apfs/apfsprogs/releases/tag/v0.2.1

--- pkgs/by-name/ap/apfsprogs/package.nix
+++ pkgs/by-name/ap/apfsprogs/package.nix
@@ -16,13 +16,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "apfsprogs";
-  version = "0.2.0";
+  version = "0.2.1";
 
   src = fetchFromGitHub {
     owner = "linux-apfs";
     repo = "apfsprogs";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-rolbBLdE98jqlKC06fTo6eJU3abKzgB3QIlaw4bza9U=";
+    hash = "sha256-GhhuielfFvcpe9hL3fUcg2xlwFrzjiUS/ZLn0jkfkh8=";
   };
 
   postPatch = ''
