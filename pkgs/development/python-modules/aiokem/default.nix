04e405d68a89456801bda52fabfc41f02a096e1f
Robert Schütz <nix@dotlambda.de>
2025-06-04 09:25:23 -0700

    python3Packages.aiokem: 0.5.12 -> 1.0.1
    Diff: https://github.com/kohlerlibs/aiokem/compare/refs/tags/v0.5.12...refs/tags/v1.0.1
    
    Changelog: https://github.com/kohlerlibs/aiokem/blob/v1.0.1/CHANGELOG.md

--- pkgs/development/python-modules/aiokem/default.nix
+++ pkgs/development/python-modules/aiokem/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aiokem";
-  version = "0.5.12";
+  version = "1.0.1";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "kohlerlibs";
     repo = "aiokem";
     tag = "v${version}";
-    hash = "sha256-/NbPKN47aSfM7SllP04fHXV2fr+QQRr1x6NfbpeTfzc=";
+    hash = "sha256-1mhsHoHttMMdMkHJ4awDHQhoMHWLHnNkeC6scEd25Z4=";
   };
 
   build-system = [ setuptools ];
