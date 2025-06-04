d50977688fb322fec5f47131f1ab4dbc4c826045
linsui <36977733+linsui@users.noreply.github.com>
2025-06-05 03:47:00 +0800

    nixos/readeck: add back MemoryDenyWriteExecute
    SQLite driver is reverted to its CGO version so this can be enabled
    
    (cherry picked from commit 17f95268f30307f0353cf80f5d99cf78f97c5400)

--- nixos/modules/services/web-apps/readeck.nix
+++ nixos/modules/services/web-apps/readeck.nix
@@ -68,6 +68,7 @@ in
         ExecStart = "${lib.getExe cfg.package} serve -config ${configFile}";
         ProtectSystem = "full";
         SystemCallArchitectures = "native";
+        MemoryDenyWriteExecute = true;
         NoNewPrivileges = true;
         PrivateTmp = true;
         PrivateDevices = true;
