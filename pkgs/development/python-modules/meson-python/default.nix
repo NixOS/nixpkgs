dbb5a73dc54333db015cf00ca513c7775edab7a1
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-06-04 14:56:37 +0200

    python3Packages.meson-python: enable all tests
    I missed that it wasn't complaining about a missing git repo before, but
    want to create git repos instead.

--- pkgs/development/python-modules/meson-python/default.nix
+++ pkgs/development/python-modules/meson-python/default.nix
@@ -12,6 +12,7 @@
 
   # tests
   cython,
+  git,
   pytestCheckHook,
   pytest-mock,
 }:
@@ -41,47 +42,11 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [
     cython
+    git
     pytestCheckHook
     pytest-mock
   ];
 
-  disabledTests = [
-    # Tests require a Git checkout
-    "test_configure_data"
-    "test_contents"
-    "test_contents"
-    "test_contents_license_file"
-    "test_contents_subdirs"
-    "test_contents_unstaged"
-    "test_detect_wheel_tag_module"
-    "test_detect_wheel_tag_script"
-    "test_dynamic_version"
-    "test_editable_install"
-    "test_editable_verbose"
-    "test_editble_reentrant"
-    "test_entrypoints"
-    "test_executable_bit"
-    "test_executable_bit"
-    "test_generated_files"
-    "test_install_subdir"
-    "test_license_pep639"
-    "test_limited_api"
-    "test_link_library_in_subproject"
-    "test_local_lib"
-    "test_long_path"
-    "test_meson_build_metadata"
-    "test_pep621_metadata"
-    "test_pure"
-    "test_purelib_and_platlib"
-    "test_reproducible"
-    "test_rpath"
-    "test_scipy_like"
-    "test_sharedlib_in_package"
-    "test_symlinks"
-    "test_uneeded_rpath"
-    "test_user_args"
-    "test_vendored_meson"
-  ];
 
   setupHooks = [ ./add-build-flags.sh ];
 
