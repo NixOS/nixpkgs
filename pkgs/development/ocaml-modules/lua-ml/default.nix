067e0da8a40a2784ec2dac39cdc42be367c4e98a
Mutsuha Asada <me@momee.mt>
2025-06-05 23:33:30 +0900

    ocamlPackages.lua-ml: 0.9.2 -> 0.9.4
    Diff: https://github.com/lindig/lua-ml/compare/0.9.2...0.9.4

--- pkgs/development/ocaml-modules/lua-ml/default.nix
+++ pkgs/development/ocaml-modules/lua-ml/default.nix
@@ -1,49 +1,25 @@
 {
-  stdenv,
   lib,
   fetchFromGitHub,
-  ocaml,
-  findlib,
-  ocamlbuild,
-  opaline,
+  buildDunePackage,
 }:
+buildDunePackage rec {
+  pname = "lua-ml";
+  version = "0.9.4";
 
-if lib.versionOlder ocaml.version "4.07" then
-  throw "lua-ml is not available for OCaml ${ocaml.version}"
-else
+  minimalOCamlVersion = "4.07";
 
-  stdenv.mkDerivation rec {
-    pname = "lua-ml";
-    name = "ocaml${ocaml.version}-${pname}-${version}";
-    version = "0.9.2";
+  src = fetchFromGitHub {
+    owner = "lindig";
+    repo = "lua-ml";
+    tag = version;
+    hash = "sha256-kMBTHzmlrRWNpWwG321jYcM61rE1J3YQkygSrfnZ6Wc=";
+  };
 
-    src = fetchFromGitHub {
-      owner = "lindig";
-      repo = pname;
-      rev = version;
-      sha256 = "sha256-xkjsjKD89W7Y5XK8kfL/ZErYKS14z0u0QCARN0DbTC8=";
-    };
-
-    nativeBuildInputs = [
-      opaline
-      ocaml
-      findlib
-      ocamlbuild
-    ];
-
-    strictDeps = true;
-
-    buildFlags = [ "lib" ];
-
-    installPhase = ''
-      opaline -prefix $out -libdir $OCAMLFIND_DESTDIR
-    '';
-
-    meta = {
-      description = "Embeddable Lua 2.5 interpreter implemented in OCaml";
-      inherit (src.meta) homepage;
-      inherit (ocaml.meta) platforms;
-      license = lib.licenses.bsd2;
-      maintainers = [ lib.maintainers.vbgl ];
-    };
-  }
+  meta = {
+    description = "Embeddable Lua 2.5 interpreter implemented in OCaml";
+    inherit (src.meta) homepage;
+    license = lib.licenses.bsd2;
+    maintainers = [ lib.maintainers.vbgl ];
+  };
+}
