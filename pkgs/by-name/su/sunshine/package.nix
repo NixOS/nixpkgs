a743bd4070764b53a2d1be61233913462b3a3148
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-05 19:04:05 +0000

    sunshine: 2025.122.141614 -> 2025.531.135549

--- pkgs/by-name/su/sunshine/package.nix
+++ pkgs/by-name/su/sunshine/package.nix
@@ -56,13 +56,13 @@ let
 in
 stdenv'.mkDerivation rec {
   pname = "sunshine";
-  version = "2025.122.141614";
+  version = "2025.531.135549";
 
   src = fetchFromGitHub {
     owner = "LizardByte";
     repo = "Sunshine";
     tag = "v${version}";
-    hash = "sha256-rHf+lj5dycXA//fu3RPuimYz2hrJnoVt7GA2xuHGXJk=";
+    hash = "sha256-f0UU72aCUmB+/H4uH8PdQChKZcr+iyrlX8dXfYUVF3U=";
     fetchSubmodules = true;
   };
 
@@ -70,7 +70,7 @@ stdenv'.mkDerivation rec {
   ui = buildNpmPackage {
     inherit src version;
     pname = "sunshine-ui";
-    npmDepsHash = "sha256-sWCmx1dMEyRyuYeeuqAjHZLVnckskgQO4saFM64s4Y4=";
+    npmDepsHash = "sha256-kM/M6afOswJ5POfA7tVJDKUGmxCt5Vhcfrl3lddG50c=";
 
     # use generated package-lock.json as upstream does not provide one
     postPatch = ''
