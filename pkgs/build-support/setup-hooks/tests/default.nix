e230668356e9f5fe6932f32e847a2328af326b98
DavHau <hsngrmpf+github@gmail.com>
2025-06-05 23:58:42 +0700

    audit-tmpdir.sh: optimize - make use of parallelMap

--- pkgs/build-support/setup-hooks/tests/default.nix
+++ pkgs/build-support/setup-hooks/tests/default.nix
@@ -3,18 +3,21 @@
 }:
 {
   # test based on bootstrap tools to prevent rebuilding stdenv on each change
-  parallel = derivation {
-    name = "test-parallel-hook";
-    system = stdenv.system;
-    builder = "${stdenv.bootstrapTools}/bin/bash";
-    PATH = "${stdenv.bootstrapTools}/bin";
-    args = [
-      "-c"
-      ''
-        . ${../parallel.sh}
-        . ${./test-parallel.sh}
-      ''
-    ];
-    meta = { };
-  };
+  parallel =
+    (derivation {
+      name = "test-parallel-hook";
+      system = stdenv.system;
+      builder = "${stdenv.bootstrapTools}/bin/bash";
+      PATH = "${stdenv.bootstrapTools}/bin";
+      args = [
+        "-c"
+        ''
+          . ${../parallel.sh}
+          . ${./test-parallel.sh}
+        ''
+      ];
+    })
+    // {
+      meta = { };
+    };
 }
