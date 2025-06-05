ba3e042970294ebbddcb8cee89a53baad3df01ba
FliegendeWurst <arne.keller@posteo.de>
2025-06-05 08:56:24 +0200

    menu-cache: 1.1.0 -> 1.1.1

--- pkgs/by-name/me/menu-cache/package.nix
+++ pkgs/by-name/me/menu-cache/package.nix
@@ -1,33 +1,36 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  fetchpatch,
+  autoconf,
+  automake,
+  autoreconfHook,
+  fetchFromGitHub,
   glib,
+  gtk-doc,
   pkg-config,
   libfm-extra,
 }:
 
 stdenv.mkDerivation rec {
   pname = "menu-cache";
-  version = "1.1.0";
+  version = "1.1.1";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/lxde/menu-cache-${version}.tar.xz";
-    sha256 = "1iry4zlpppww8qai2cw4zid4081hh7fz8nzsp5lqyffbkm2yn0pd";
+  src = fetchFromGitHub {
+    owner = "lxde";
+    repo = "menu-cache";
+    tag = version;
+    hash = "sha256-5Vp2btrflimy+Hq+3MLpic/quZMJ3uwsMq12G7s4DGI=";
   };
 
-  patches = [
-    # Pull patch pending upstream inclusion for -fno-common toolchain support:
-    #   https://github.com/lxde/menu-cache/pull/19
-    (fetchpatch {
-      name = "fno-common.patch";
-      url = "https://github.com/lxde/menu-cache/commit/1ce739649b4d66339a03fc0ec9ee7a2f7c141780.patch";
-      sha256 = "08x3h0w2pl8ifj83v9jkf4j3zxcwsyzh251divlhhnwx0rw1pyn7";
-    })
-  ];
+  preAutoreconf = "./autogen.sh";
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    autoreconfHook
+    autoconf
+    automake
+    gtk-doc
+    pkg-config
+  ];
 
   buildInputs = [
     glib
