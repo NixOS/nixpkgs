6fd6151705b53048dcac0d97f09742242e4f88c4
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-06-04 22:42:08 -0400

    ocamlPackages.prelude: init at 0.5
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1786,6 +1786,8 @@ let
 
         pratter = callPackage ../development/ocaml-modules/pratter { };
 
+        prelude = callPackage ../development/ocaml-modules/prelude { };
+
         prettym = callPackage ../development/ocaml-modules/prettym { };
 
         printbox = callPackage ../development/ocaml-modules/printbox { };
