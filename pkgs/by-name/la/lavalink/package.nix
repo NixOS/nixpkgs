654329e538d1450ad635ff22c9beeaa0f1f5952f
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-05 03:18:54 +0000

    lavalink: 4.0.8 -> 4.1.0

--- pkgs/by-name/la/lavalink/package.nix
+++ pkgs/by-name/la/lavalink/package.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lavalink";
-  version = "4.0.8";
+  version = "4.1.0";
 
   src = fetchurl {
     url = "https://github.com/lavalink-devs/Lavalink/releases/download/${finalAttrs.version}/Lavalink.jar";
-    hash = "sha256-G4a9ltPq/L0vcazTQjStTlOOtwrBi37bYUNQHy5CV9Y=";
+    hash = "sha256-1rtcBmJXFfPcQdhYpDswsYdDHP5k4vvxJldKQ7rmHnQ=";
   };
 
   nativeBuildInputs = [
