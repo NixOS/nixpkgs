4884f77879201dd6ef33d92c81855e2fff84b902
Grimmauld <Grimmauld@grimmauld.de>
2025-06-05 10:27:56 +0200

    sdl3: 3.2.14 -> 3.2.16
    Release notes: https://github.com/libsdl-org/SDL/releases/tag/release-3.2.16
    Diff: https://github.com/libsdl-org/SDL/compare/release-3.2.14...release-3.2.16
    
    Mostly bugfixes, most of which only relevant for windows, xbox or android.
    
    Some details about (linux) audio changed, specifically how device leases work.
    `ffplay` still plays audio just fine, so nothing seems broken.
    
    (cherry picked from commit 7219e88caddee067b7e5be468fda07035d819e5a)

--- pkgs/by-name/sd/sdl3/package.nix
+++ pkgs/by-name/sd/sdl3/package.nix
@@ -61,7 +61,7 @@ assert lib.assertMsg (
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sdl3";
-  version = "3.2.14";
+  version = "3.2.16";
 
   outputs = [
     "lib"
@@ -73,7 +73,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "libsdl-org";
     repo = "SDL";
     tag = "release-${finalAttrs.version}";
-    hash = "sha256-+CcbvF1nxxsVwuO5g50sBVGth0sr5WTFojSfT6B6bok=";
+    hash = "sha256-xFWE/i4l3sU1KritwbqvN67kJ3/WUfNP3iScMfQUbwA=";
   };
 
   postPatch =
