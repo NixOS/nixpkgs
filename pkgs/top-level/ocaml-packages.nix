9e2513cae79b87a72fa92331259ec4702e7989e6
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-06-04 22:48:19 -0400

    ocamlPackages.patricia-tree: init at 0.11.0
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1670,6 +1670,8 @@ let
 
         path_glob = callPackage ../development/ocaml-modules/path_glob { };
 
+        patricia-tree = callPackage ../development/ocaml-modules/patricia-tree { };
+
         pbkdf = callPackage ../development/ocaml-modules/pbkdf { };
 
         pbrt = callPackage ../development/ocaml-modules/pbrt { };
