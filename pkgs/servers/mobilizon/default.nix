2e501491d67897b6b03cbe4189cba747208ea9cb
Kerstin Humm <kerstin@erictapen.name>
2025-06-06 12:14:22 +0200

    mobilizon: 5.1.2 -> 5.1.4
    Changelog: https://framagit.org/kaihuri/mobilizon/-/releases/5.1.4

--- pkgs/servers/mobilizon/default.nix
+++ pkgs/servers/mobilizon/default.nix
@@ -20,19 +20,6 @@ in
 mixRelease rec {
   inherit (common) pname version src;
 
-  patches = [
-    # Version 5.1.2 failed to bump their internal package version,
-    # which causes issues with static file serving in the NixOS module.
-    ./0001-fix-version.patch
-    # Mobilizon uses chunked Transfer-Encoding for the media proxy but also
-    # sets the Content-Length header. This is a HTTP/1.1 protocol violation
-    # and results in nginx >=1.24 rejecting the response with this error:
-    # 'upstream sent "Content-Length" and "Transfer-Encoding" headers at the same
-    # time while reading response header from upstream'
-    # Upstream PR: https://framagit.org/framasoft/mobilizon/-/merge_requests/1604
-    ./0002-fix-media-proxy.patch
-  ];
-
   nativeBuildInputs = [
     git
     cmake
