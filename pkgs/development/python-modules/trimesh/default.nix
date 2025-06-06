36b311045c490f51d21731699107db44207ef679
Peder Bergebakken Sundt <pbsds@hotmail.com>
2025-06-06 20:10:15 +0200

    python3Packages.trimesh: 4.6.10 -> 4.6.11
    Changelog: https://github.com/mikedh/trimesh/releases/tag/4.6.11

--- pkgs/development/python-modules/trimesh/default.nix
+++ pkgs/development/python-modules/trimesh/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "trimesh";
-  version = "4.6.10";
+  version = "4.6.11";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "mikedh";
     repo = "trimesh";
     tag = version;
-    hash = "sha256-tlIsRZLmnRCqVHcLQOh6Jmne3rZGS95QudnvJHMu1qs=";
+    hash = "sha256-wltoJsfHl5qtceG96oT5yqLiJl0zeX9IOw+eeKXfJb0=";
   };
 
   build-system = [ setuptools ];
