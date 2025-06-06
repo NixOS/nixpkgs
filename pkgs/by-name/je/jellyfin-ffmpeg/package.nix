6e588bcc5e614e6f50d1c6126588629baa0b6d25
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-06 12:11:15 +0000

    jellyfin-ffmpeg: 7.1.1-3 -> 7.1.1-4

--- pkgs/by-name/je/jellyfin-ffmpeg/package.nix
+++ pkgs/by-name/je/jellyfin-ffmpeg/package.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  version = "7.1.1-3";
+  version = "7.1.1-4";
 in
 
 (ffmpeg_7-full.override {
@@ -14,7 +14,7 @@ in
     owner = "jellyfin";
     repo = "jellyfin-ffmpeg";
     rev = "v${version}";
-    hash = "sha256-pJLIhXDPDRhEqzmc1bXViSTSnRifFhMlixkEbGA0GRE=";
+    hash = "sha256-7KHo+kVZl/fkXiChFJ5L+WhuFPx0oqQG+/CQQwTH+/4=";
   };
 }).overrideAttrs
   (old: {
