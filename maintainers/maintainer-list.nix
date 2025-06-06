d8581df44f99aae1eb7daec85fe6e19cc1fde4ca
DreamingCodes <me@dreaming.codes>
2025-06-06 15:45:54 +0200

    add dreamingcodes to contributors

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6506,6 +6506,12 @@
     githubId = 699961;
     name = "Dmytro Kyrychuk";
   };
+  dreamingcodes = {
+    email = "me@dreaming.codes";
+    github = "Dreaming-Codes";
+    githubId = 10401566;
+    name = "Lorenzo Rizzotti";
+  };
   dnr = {
     email = "dnr@dnr.im";
     github = "dnr";
