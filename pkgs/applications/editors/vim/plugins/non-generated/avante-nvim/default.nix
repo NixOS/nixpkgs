2b8abd6910c7149c5889eb794112c41ddbe54076
Austin Horstman <khaneliman12@gmail.com>
2025-06-05 23:38:35 -0500

    vimPlugins.avante-nvim: 0.0.23-unstable-2025-06-02 -> 0.0.24-unstable-2025-06-05
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- pkgs/applications/editors/vim/plugins/non-generated/avante-nvim/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/avante-nvim/default.nix
@@ -12,12 +12,12 @@
   pkgs,
 }:
 let
-  version = "0.0.23-unstable-2025-06-02";
+  version = "0.0.24-unstable-2025-06-05";
   src = fetchFromGitHub {
     owner = "yetone";
     repo = "avante.nvim";
-    rev = "647a459a2b87e5c9b2987cb44150b71beffdfb10";
-    hash = "sha256-GgLOcVp2IuMBr4aBRWJTvrAuWWiMEIHmec/S97piBaM=";
+    rev = "12bdb66ea4749f9f59861e7165a4de6cfbf4eb22";
+    hash = "sha256-dUtvMkb85HR7xzMoCHeeJIwwyQPdwHNdQHUMyA7+EsU=";
   };
   avante-nvim-lib = rustPlatform.buildRustPackage {
     pname = "avante-nvim-lib";
