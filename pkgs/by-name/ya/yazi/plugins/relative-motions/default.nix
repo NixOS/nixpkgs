bdf5d729001940ba9c3f5af7b7f6f977c4187443
Austin Horstman <khaneliman12@gmail.com>
2025-06-06 08:55:40 -0500

    yaziPlugins: update on 2025-06-06
    - mediainfo: 25.5.28-unstable-2025-05-30 → 25.5.31-unstable-2025-06-05
    - projects: 0-unstable-2025-05-29 → 0-unstable-2025-06-03
    - relative-motions: 25.4.8-unstable-2025-04-16 → 25.5.28-unstable-2025-06-05
    - restore: 25.5.28-unstable-2025-05-30 → 25.5.31-unstable-2025-06-05
    - rich-preview: 0-unstable-2025-05-30 → 0-unstable-2025-05-31
    - starship: 25.4.8-unstable-2025-05-30 → 25.4.8-unstable-2025-06-01
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- pkgs/by-name/ya/yazi/plugins/relative-motions/default.nix
+++ pkgs/by-name/ya/yazi/plugins/relative-motions/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "relative-motions.yazi";
-  version = "25.4.8-unstable-2025-04-16";
+  version = "25.5.28-unstable-2025-06-05";
 
   src = fetchFromGitHub {
     owner = "dedukun";
     repo = "relative-motions.yazi";
-    rev = "ce2e890227269cc15cdc71d23b35a58fae6d2c27";
-    hash = "sha256-Ijz1wYt+L+24Fb/rzHcDR8JBv84z2UxdCIPqTdzbD14=";
+    rev = "2e3b6172e6226e0db96aea12d09dea2d2e443fea";
+    hash = "sha256-v0e06ieBKNmt9DATdL7R4AyVFa9DlNBwpfME3LHozLA=";
   };
 
   meta = {
