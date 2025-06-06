8e750678c623dc7331b071285baee854e87d0c52
Fabian Affolter <mail@fabian-affolter.ch>
2025-06-06 15:17:58 +0200

    python312Packages.mypy-boto3-kms: 1.38.0 -> 1.38.31

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -762,8 +762,8 @@ rec {
       "sha256-PKOF/jd1Wjz8lCwbCd4cfHeStkvcDhQu7a7bpHaizgU=";
 
   mypy-boto3-kms =
-    buildMypyBoto3Package "kms" "1.38.0"
-      "sha256-s1D6mzq5T7G8jm2nxUr7Kq6QeT3oDBOF4yafahD+fq4=";
+    buildMypyBoto3Package "kms" "1.38.31"
+      "sha256-hCN7Docv7FRJrUW1JveU8v68OFl97PQ+xQRICQ8D9rY=";
 
   mypy-boto3-lakeformation =
     buildMypyBoto3Package "lakeformation" "1.38.0"
