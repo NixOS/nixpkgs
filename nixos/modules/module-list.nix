7793724371892208d5a386ef9d7ceeaf08e3ccac
Tristan Ross <tristan.ross@midstall.com>
2025-06-05 18:43:55 -0700

    nixos/refind: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1817,6 +1817,7 @@
   ./system/boot/loader/init-script/init-script.nix
   ./system/boot/loader/limine/limine.nix
   ./system/boot/loader/loader.nix
+  ./system/boot/loader/refind/refind.nix
   ./system/boot/loader/systemd-boot/systemd-boot.nix
   ./system/boot/luksroot.nix
   ./system/boot/modprobe.nix
