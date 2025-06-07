969bed3f901ad5c5f1cfb63baa11cf1c3c507aba
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-07 21:27:39 +0800

    nixosTests.etcd: handleTest -> runTest

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -473,7 +473,7 @@ in
   activation-etc-overlay-mutable = runTest ./activation/etc-overlay-mutable.nix;
   activation-etc-overlay-immutable = runTest ./activation/etc-overlay-immutable.nix;
   activation-perlless = runTest ./activation/perlless.nix;
-  etcd = handleTestOn [ "aarch64-linux" "x86_64-linux" ] ./etcd/etcd.nix { };
+  etcd = runTestOn [ "aarch64-linux" "x86_64-linux" ] ./etcd/etcd.nix;
   etcd-cluster = handleTestOn [ "aarch64-linux" "x86_64-linux" ] ./etcd/etcd-cluster.nix { };
   etebase-server = runTest ./etebase-server.nix;
   etesync-dav = runTest ./etesync-dav.nix;
