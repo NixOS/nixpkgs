a2da6fda27d4e80e8fa0eb6185b0da0a11301940
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-06-07 18:17:19 +0200

    rstudio: 2025.05.0+496 -> 2025.05.1+513, unlock jdk version
    (cherry picked from commit bc42fc0684e568768575a095894b2954da845c6a)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13960,9 +13960,7 @@ with pkgs;
     backend = "wayland";
   };
 
-  rstudio = callPackage ../applications/editors/rstudio {
-    jdk = jdk8;
-  };
+  rstudio = callPackage ../applications/editors/rstudio { };
 
   rstudio-server = rstudio.override { server = true; };
 
