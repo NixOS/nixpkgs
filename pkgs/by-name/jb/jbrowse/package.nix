17733501d0d718a8e95220043ae09b1833affebe
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-07 23:05:17 +0000

    jbrowse: 3.5.0 -> 3.5.1

--- pkgs/by-name/jb/jbrowse/package.nix
+++ pkgs/by-name/jb/jbrowse/package.nix
@@ -6,11 +6,11 @@
 
 let
   pname = "jbrowse";
-  version = "3.5.0";
+  version = "3.5.1";
 
   src = fetchurl {
     url = "https://github.com/GMOD/jbrowse-components/releases/download/v${version}/jbrowse-desktop-v${version}-linux.AppImage";
-    sha256 = "sha256-UAuKbfvJuCDIaERFVYo6rdhBG2ycp87ZnCrVPLDDv9g=";
+    sha256 = "sha256-1swQIG9rCzk2wP7apYo4UjUE+enMK/ZrMu1q4/ahfjA=";
   };
 
   appimageContents = appimageTools.extractType2 {
