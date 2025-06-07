50f3960dc8031aeeea5b0a4f14599c10e51518af
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-07 19:06:37 +0800

    nixosTests.ceph-multi-node: handleTest -> runTest

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -295,7 +295,7 @@ in
     _module.args.getPackage = pkgs: pkgs.cassandra;
   };
   centrifugo = runTest ./centrifugo.nix;
-  ceph-multi-node = handleTestOn [ "aarch64-linux" "x86_64-linux" ] ./ceph-multi-node.nix { };
+  ceph-multi-node = runTestOn [ "aarch64-linux" "x86_64-linux" ] ./ceph-multi-node.nix;
   ceph-single-node = handleTestOn [ "aarch64-linux" "x86_64-linux" ] ./ceph-single-node.nix { };
   ceph-single-node-bluestore = handleTestOn [
     "aarch64-linux"
