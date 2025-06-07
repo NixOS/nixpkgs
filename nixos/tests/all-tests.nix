b5feebede1212a573152d83ff6d42104c40826ca
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-07 18:26:26 +0800

    nixosTests.blocky: handleTest -> runTest

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -253,7 +253,7 @@ in
   bitcoind = runTest ./bitcoind.nix;
   bittorrent = runTest ./bittorrent.nix;
   blockbook-frontend = runTest ./blockbook-frontend.nix;
-  blocky = handleTest ./blocky.nix { };
+  blocky = runTest ./blocky.nix;
   boot = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./boot.nix { };
   bootspec = handleTestOn [ "x86_64-linux" ] ./bootspec.nix { };
   boot-stage1 = runTest ./boot-stage1.nix;
