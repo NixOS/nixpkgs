c1520707cc51e04a5fb7e1cc47ccd9473f6a52f6
etwas <ein@etwas.me>
2025-06-07 18:11:46 +0200

    kagiapi: init at 0.2.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7374,6 +7374,8 @@ self: super: with self; {
 
   kaggle = callPackage ../development/python-modules/kaggle { };
 
+  kagiapi = callPackage ../development/python-modules/kagiapi { };
+
   kahip = toPythonModule (
     pkgs.kahip.override {
       pythonSupport = true;
