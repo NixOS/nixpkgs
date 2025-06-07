3125f89d99b1e8492f4235fcf1269dc83b8fca66
Mutsuha Asada <me@momee.mt>
2025-06-07 16:03:45 +0900

    ocamlPackages.benchmark: modernized derivation - Added longDescription and changelog - Added minimalOCamlVersion

--- pkgs/development/ocaml-modules/benchmark/default.nix
+++ pkgs/development/ocaml-modules/benchmark/default.nix
@@ -8,6 +8,8 @@ buildDunePackage rec {
   pname = "benchmark";
   version = "1.7";
 
+  minimalOCamlVersion = "4.03";
+
   src = fetchurl {
     url = "https://github.com/Chris00/ocaml-benchmark/releases/download/v${version}/benchmark-${version}.tbz";
     hash = "sha256-Aij7vJzamNWQfjLeGgENlIp6Il8+Wc9hsahr4eDGs68=";
@@ -16,6 +18,12 @@ buildDunePackage rec {
   meta = {
     homepage = "https://github.com/Chris00/ocaml-benchmark";
     description = "Benchmark running times of code";
+    longDescription = ''
+      This module provides a set of tools to measure the running times of
+      your functions and to easily compare the results.  A statistical test
+      is used to determine whether the results truly differ.
+    '';
+    changelog = "https://raw.githubusercontent.com/Chris00/ocaml-benchmark/refs/tags/v${version}/CHANGES.md";
     license = lib.licenses.lgpl3;
   };
 }
