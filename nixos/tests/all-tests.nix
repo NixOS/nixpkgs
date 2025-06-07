7ea4b7c71aec46d92350bf7a58c3e7056e37a2ae
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-07 21:21:10 +0800

    nixosTests.earlyoom: handleTest -> runTest

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -434,7 +434,7 @@ in
   drbd-driver = runTest ./drbd-driver.nix;
   dublin-traceroute = runTest ./dublin-traceroute.nix;
   dwl = runTestOn [ "x86_64-linux" "aarch64-linux" ] ./dwl.nix;
-  earlyoom = handleTestOn [ "x86_64-linux" ] ./earlyoom.nix { };
+  earlyoom = runTestOn [ "x86_64-linux" ] ./earlyoom.nix;
   early-mount-options = handleTest ./early-mount-options.nix { };
   ec2-config = (handleTestOn [ "x86_64-linux" ] ./ec2.nix { }).boot-ec2-config or { };
   ec2-nixops = (handleTestOn [ "x86_64-linux" ] ./ec2.nix { }).boot-ec2-nixops or { };
