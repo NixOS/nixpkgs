4f26991b9f5dbb5eb71a13275f3b353089bf2c73
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-07 19:45:32 +0800

    nixosTests.chrony: handleTest -> runTest

--- nixos/tests/chrony.nix
+++ nixos/tests/chrony.nix
@@ -1,29 +1,23 @@
-import ./make-test-python.nix (
-  { lib, ... }:
-  {
-    name = "chrony";
+{ lib, ... }:
+{
+  name = "chrony";
 
-    meta = {
-      maintainers = with lib.maintainers; [ fpletz ];
+  meta.maintainers = with lib.maintainers; [ fpletz ];
+
+  nodes.machine = {
+    services.chrony.enable = true;
+
+    specialisation.hardened.configuration = {
+      services.chrony.enableMemoryLocking = true;
     };
+  };
 
-    nodes = {
-      machine = {
-        services.chrony.enable = true;
-
-        specialisation.hardened.configuration = {
-          services.chrony.enableMemoryLocking = true;
-        };
-      };
-    };
-
-    testScript = ''
-      machine.start()
-      machine.wait_for_unit('multi-user.target')
-      machine.succeed('systemctl is-active chronyd.service')
-      machine.succeed('/run/booted-system/specialisation/hardened/bin/switch-to-configuration test')
-      machine.succeed('systemctl restart chronyd.service')
-      machine.wait_for_unit('chronyd.service')
-    '';
-  }
-)
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit('multi-user.target')
+    machine.succeed('systemctl is-active chronyd.service')
+    machine.succeed('/run/booted-system/specialisation/hardened/bin/switch-to-configuration test')
+    machine.succeed('systemctl restart chronyd.service')
+    machine.wait_for_unit('chronyd.service')
+  '';
+}
