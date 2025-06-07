d14a16c25939baee31fff1542c83c15cb98cf248
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-06-06 21:33:18 -0700

    ao3downloader: init at 2025.6.1

---
+++ pkgs/by-name/ao/ao3downloader/package.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  python312Packages,
+  fetchFromGitHub,
+}:
+
+python312Packages.buildPythonApplication rec {
+  pname = "ao3downloader";
+  version = "2025.6.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "nianeyna";
+    repo = "ao3downloader";
+    tag = "v${version}";
+    hash = "sha256-ukS3uku8OW5nhM2Nr0IxAiG03XfqUn/Xyd0lZDGGkWw=";
+  };
+
+  build-system = with python312Packages; [
+    hatchling
+  ];
+
+  dependencies = with python312Packages; [
+    beautifulsoup4
+    mobi
+    pdfquery
+    requests
+    six
+    tqdm
+  ];
+
+  pythonRelaxDeps = [
+    "requests"
+  ];
+
+  pythonImportsCheck = [
+    "ao3downloader"
+  ];
+
+  nativeCheckInputs = with python312Packages; [
+    pytestCheckHook
+    syrupy
+    pythonImportsCheckHook
+  ];
+
+  meta = {
+    description = "Utility for downloading fanfiction in bulk from the Archive of Our Own";
+    changelog = "https://github.com/nianeyna/ao3downloader/releases/tag/v${version}";
+    mainProgram = "ao3downloader";
+    homepage = "https://nianeyna.dev/ao3downloader";
+    license = lib.licenses.gpl3;
+    maintainers = [ lib.maintainers.samasaur ];
+  };
+}
