6b95f042e79d750dcb34642d73e305cc08e9f650
Mutsuha Asada <me@momee.mt>
2025-06-08 13:41:33 +0900

    ocamlPackages.dum: modernized derivation - Removed with lib; - Added meta.longDescription - Changed --replace to --replace-fail

--- pkgs/development/ocaml-modules/dum/default.nix
+++ pkgs/development/ocaml-modules/dum/default.nix
@@ -16,16 +16,23 @@ buildDunePackage rec {
 
   postPatch = ''
     substituteInPlace "dum.ml" \
-    --replace "Lazy.lazy_is_val" "Lazy.is_val" \
-    --replace "Obj.final_tag" "Obj.custom_tag"
+    --replace-fail "Lazy.lazy_is_val" "Lazy.is_val" \
+    --replace-fail "Obj.final_tag" "Obj.custom_tag"
   '';
 
   propagatedBuildInputs = [ easy-format ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/mjambon/dum";
     description = "Inspect the runtime representation of arbitrary OCaml values";
-    license = licenses.lgpl21Plus;
-    maintainers = [ maintainers.alexfmpe ];
+    longDescription = ''
+      Dum is a library for inspecting the runtime representation of
+      arbitrary OCaml values. Shared or cyclic data are detected
+      and labelled. This guarantees that printing would always
+      terminate. This makes it possible to print values such as closures,
+      objects or exceptions in depth and without risk.
+    '';
+    license = lib.licenses.lgpl21Plus;
+    maintainers = with lib.maintainers; [ alexfmpe ];
   };
 }
