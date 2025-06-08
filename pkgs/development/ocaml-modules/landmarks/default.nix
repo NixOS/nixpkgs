59f180b4f69f87dcd0ebe7ff4fc85f3d3d14bd0f
Mutsuha Asada <me@momee.mt>
2025-06-09 08:34:39 +0900

    ocamlPackages.{landmarks, landmarks-ppx}: modernized derivation - Removed with lib; - Added meta.homepage, meta.longDescription, and meta.changelog
    (cherry picked from commit 95612d14de84651a677ebde0708f92550e68557e)

--- pkgs/development/ocaml-modules/landmarks/default.nix
+++ pkgs/development/ocaml-modules/landmarks/default.nix
@@ -19,9 +19,17 @@ buildDunePackage rec {
 
   doCheck = lib.versionAtLeast ocaml.version "4.08" && lib.versionOlder ocaml.version "5.0";
 
-  meta = with lib; {
+  meta = {
+    inherit (src.meta) homepage;
     description = "Simple Profiling Library for OCaml";
-    maintainers = [ maintainers.kenran ];
-    license = licenses.mit;
+    longDescription = ''
+      Landmarks is a simple profiling library for OCaml. It provides
+      primitives to measure time spent in portion of instrumented code. The
+      instrumentation of the code may either done by hand, automatically or
+      semi-automatically using the ppx pepreprocessor (see landmarks-ppx package).
+    '';
+    changelog = "https://raw.githubusercontent.com/LexiFi/landmarks/refs/tags/v${version}/CHANGES.md";
+    maintainers = with lib.maintainers; [ kenran ];
+    license = lib.licenses.mit;
   };
 }
