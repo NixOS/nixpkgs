fcfb053b7b2a2495b274313d03addd79868d89dc
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-08 06:47:30 +0000

    linuxPackages.prl-tools: 20.3.1-55959 -> 20.3.2-55975
    (cherry picked from commit 30b2aaa0edaa1e99b4031667691307064b752c01)

--- pkgs/os-specific/linux/prl-tools/default.nix
+++ pkgs/os-specific/linux/prl-tools/default.nix
@@ -43,13 +43,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "prl-tools";
-  version = "20.3.1-55959";
+  version = "20.3.2-55975";
 
   # We download the full distribution to extract prl-tools-lin.iso from
   # => ${dmg}/Parallels\ Desktop.app/Contents/Resources/Tools/prl-tools-lin.iso
   src = fetchurl {
     url = "https://download.parallels.com/desktop/v${lib.versions.major finalAttrs.version}/${finalAttrs.version}/ParallelsDesktop-${finalAttrs.version}.dmg";
-    hash = "sha256-/J6NouI2htptG06Undeg1rUfbWUu6q/nV2P9D+sS7OA=";
+    hash = "sha256-eazDR+eSUcp81XdRfYRHIt7E4FNCEjsh0M0wYQQYmMQ=";
   };
 
   hardeningDisable = [
