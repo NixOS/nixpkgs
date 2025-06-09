27f85a7685bd03301d60487d902f45dd86b4b3ac
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-09 14:59:39 +0000

    s6-rc-man-pages: 0.5.5.0.1 -> 0.5.4.1.2

--- pkgs/development/skaware-packages/s6-rc/default.nix
+++ pkgs/development/skaware-packages/s6-rc/default.nix
@@ -15,7 +15,7 @@ skawarePackages.buildPackage {
 
   manpages = skawarePackages.buildManPages {
     pname = "s6-rc-man-pages";
-    version = "0.5.5.0.1";
+    version = "0.5.4.1.2";
     sha256 = "sha256-Ywke3FG/xhhUd934auDB+iFRDCvy8IJs6IkirP6O/As=";
     description = "mdoc(7) versions of the documentation for the s6-rc service manager";
     maintainers = [ lib.maintainers.qyliss ];
