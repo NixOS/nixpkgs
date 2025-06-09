f83998780c494ec6dc7f0d7d8883f20cbda9dedd
emaryn <emaryn@users.noreply.github.com>
2025-06-09 14:40:56 +0800

    python313Packages.paddlex: init at 3.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10912,6 +10912,8 @@ self: super: with self; {
 
   paddlepaddle = callPackage ../development/python-modules/paddlepaddle { };
 
+  paddlex = callPackage ../development/python-modules/paddlex { };
+
   pagelabels = callPackage ../development/python-modules/pagelabels { };
 
   paginate = callPackage ../development/python-modules/paginate { };
