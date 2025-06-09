738616fb6fd5a12958af5b561d22efa2dcf6ad80
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-06-09 16:31:15 +0200

    nixos/nix-{gc,optimise}: do not start when switching

--- nixos/modules/services/misc/nix-gc.nix
+++ nixos/modules/services/misc/nix-gc.nix
@@ -88,6 +88,9 @@ in
       script = "exec ${config.nix.package.out}/bin/nix-collect-garbage ${cfg.options}";
       serviceConfig.Type = "oneshot";
       startAt = lib.optionals cfg.automatic cfg.dates;
+      # do not start and delay when switching
+      restartIfChanged = false;
+      stopIfChanged = false;
     };
 
     systemd.timers.nix-gc = lib.mkIf cfg.automatic {
