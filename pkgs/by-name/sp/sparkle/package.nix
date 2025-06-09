99795467f69ff12892f65add8b4787cc8c2f1f95
emaryn <emaryn@users.noreply.github.com>
2025-06-09 19:00:35 +0000

    sparkle: 1.6.6 -> 1.6.7

--- pkgs/by-name/sp/sparkle/package.nix
+++ pkgs/by-name/sp/sparkle/package.nix
@@ -16,7 +16,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sparkle";
-  version = "1.6.6";
+  version = "1.6.7";
 
   src =
     let
@@ -31,8 +31,8 @@ stdenv.mkDerivation (finalAttrs: {
     fetchurl {
       url = "https://github.com/xishang0128/sparkle/releases/download/${finalAttrs.version}/sparkle-linux-${finalAttrs.version}-${arch}.deb";
       hash = selectSystem {
-        x86_64-linux = "sha256-MTDPtkJJCEbj9fCbXtFGOnOyijklETKN/72cfHbMdDs=";
-        aarch64-linux = "sha256-w1R0EM4IAr0scoYhm+HX+w35nOeu+1NgySzysfDDfEo=";
+        x86_64-linux = "sha256-WpyHZzQ3LkBToiBXLUoUx5H4ZhK3J5JVqXYDl+9MtHo=";
+        aarch64-linux = "sha256-7u5CUB8i5b2PXhHw0x8rjCH8jcC2Ncrz00Nty7rqKng=";
       };
     };
 
