152febd25429bd77b896e6c796f1b189fed08f7c
ruffsl <roxfoxpox@gmail.com>
2025-06-08 19:14:21 -0500

    Stage temporary flake for development

---
+++ pkgs/by-name/wi/wifiman/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "WiFiman dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: 
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in {
+      packages.x86_64-linux.wifiman = pkgs.callPackage ./package.nix {};
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [ self.packages.x86_64-linux.wifiman ];
+      };
+    };
+}
