a98863ec71ab767b4380fcf8a77b9d13ed97540c
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-09 11:39:57 +0200

    spral: clean

--- pkgs/by-name/sp/spral/package.nix
+++ pkgs/by-name/sp/spral/package.nix
@@ -1,24 +1,31 @@
 {
-  blas,
-  fetchFromGitHub,
-  gfortran,
-  lapack,
   lib,
-  llvmPackages,
-  meson,
-  metis,
-  ninja,
+
+  fetchFromGitHub,
   stdenv,
+
+  nix-update-script,
+
+  # nativeBuildInputs
+  gfortran,
+  meson,
+  ninja,
+
+  # buildInputs
+  blas,
+  lapack,
+  llvmPackages,
+  metis,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "spral";
   version = "2025.05.20";
 
   src = fetchFromGitHub {
     owner = "ralna";
     repo = "spral";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-9QEcAOFB3CtGNqr8LoDaj2vP3KMONlUVooeXECtGsxc=";
   };
 
@@ -40,11 +47,13 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "Sparse Parallel Robust Algorithms Library";
     homepage = "https://github.com/ralna/spral";
-    changelog = "https://github.com/ralna/spral/blob/${src.rev}/ChangeLog";
+    changelog = "https://github.com/ralna/spral/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ nim65s ];
   };
-}
+})
