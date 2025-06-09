c26f14dc59543cfc14cfdb2ecf436483580aa1b2
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-09 10:37:03 +0000

    primecount: 7.18 -> 7.19

--- pkgs/by-name/pr/primecount/package.nix
+++ pkgs/by-name/pr/primecount/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "primecount";
-  version = "7.18";
+  version = "7.19";
 
   src = fetchFromGitHub {
     owner = "kimwalisch";
     repo = "primecount";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-iVRkgVeNO0AxwfyM9Dw5B0GoAf4GlrTtC+1npKlaV3E=";
+    hash = "sha256-prPNAmMSiZD1EbMyPSD6OmjFn/NQ7ULVxBM1AjCYWPo=";
   };
 
   outputs = [
