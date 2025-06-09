6b3d6a3b157e50f8a39b1797d6997be7e51cc4c6
Guy Chronister <guychronister@outlook.com>
2025-06-09 16:29:56 +0000

    spacebar: refactor package definitions

--- pkgs/by-name/sp/spacebar/package.nix
+++ pkgs/by-name/sp/spacebar/package.nix
@@ -5,14 +5,14 @@
   apple-sdk_12,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "spacebar";
   version = "1.4.0";
 
   src = fetchFromGitHub {
     owner = "cmacrae";
-    repo = pname;
-    rev = "v${version}";
+    repo = "spacebar";
+    tag = "v${finalAttrs.version}";
     sha256 = "sha256-4LiG43kPZtsm7SQ/28RaGMpYsDshCaGvc1mouPG3jFM=";
   };
 
@@ -21,17 +21,17 @@ stdenv.mkDerivation rec {
   ];
 
   installPhase = ''
-    mkdir -p $out/bin
-    mkdir -p $out/share/man/man1/
-    cp ./bin/spacebar $out/bin/spacebar
-    cp ./doc/spacebar.1 $out/share/man/man1/spacebar.1
+    runHook preInstall
+    install -Dm555 spacebar $out/bin
+    install -Dm644 ./doc/spacebar.1 $out/share/man/man1/spacebar.1
+    runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Minimal status bar for macOS";
     homepage = "https://github.com/cmacrae/spacebar";
-    platforms = platforms.darwin;
-    maintainers = [ maintainers.cmacrae ];
-    license = licenses.mit;
+    platforms = lib.platforms.darwin;
+    maintainers = with lib.maintainers; [ cmacrae ];
+    license = lib.licenses.mit;
   };
-}
+})
