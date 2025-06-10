f7c386cffdf81969931b97863540af272ff38141
scraptux <git@thomasjasny.de>
2025-06-10 17:54:59 +0200

    python3Packages.grpcio-channelz: 1.72.0 -> 1.73.0

--- pkgs/development/python-modules/grpcio-channelz/default.nix
+++ pkgs/development/python-modules/grpcio-channelz/default.nix
@@ -12,13 +12,13 @@
 # nixpkgs-update: no auto update
 buildPythonPackage rec {
   pname = "grpcio-channelz";
-  version = "1.72.0";
+  version = "1.73.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "grpcio_channelz";
     inherit version;
-    hash = "sha256-Pcst8cuckF99qvCLzsIU/QgoCaWa3xQFtm1E9W7w+tE=";
+    hash = "sha256-2trHRqOCcp/EmPEFIIZtplyrr4KJhbVR/xVmLnJngyU=";
   };
 
   build-system = [ setuptools ];
