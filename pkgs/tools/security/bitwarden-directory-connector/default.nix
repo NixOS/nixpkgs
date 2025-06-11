8494c8b45c339c4a8a066f6492b913a12c9dc046
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-11 06:38:44 +0000

    bitwarden-directory-connector: 2025.5.0 -> 2025.6.0

--- pkgs/tools/security/bitwarden-directory-connector/default.nix
+++ pkgs/tools/security/bitwarden-directory-connector/default.nix
@@ -19,14 +19,14 @@ let
     }:
     buildNpmPackage rec {
       pname = name;
-      version = "2025.5.0";
+      version = "2025.6.0";
       nodejs = nodejs_22;
 
       src = fetchFromGitHub {
         owner = "bitwarden";
         repo = "directory-connector";
         rev = "v${version}";
-        hash = "sha256-78AWsZd6j/GYyKDwr95d2x5tPh6jBbA34zs0CjtLIyg=";
+        hash = "sha256-qlD16U/S3SzaA7sMR+JZSYHy9g7c5cjedAmkFWRoxN0=";
       };
 
       postPatch = ''
@@ -38,7 +38,7 @@ let
           --replace-fail "AppImage" "dir"
       '';
 
-      npmDepsHash = "sha256-9sQWy+w3MdAWGTIzGD3yJypTLYh4aUzazBNf5GcW3Sc=";
+      npmDepsHash = "sha256-EIF7I4stB6+h3l2hLJqLHR8qzmRP20v161YwXVMJh44=";
 
       env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
