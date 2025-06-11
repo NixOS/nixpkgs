16feb821664ffc05846d6bc43d3dd87d1a56e882
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-11 15:25:07 +0000

    dune_3: 3.19.0 -> 3.19.1

--- pkgs/development/tools/ocaml/dune/3.nix
+++ pkgs/development/tools/ocaml/dune/3.nix
@@ -14,11 +14,11 @@ else
 
   stdenv.mkDerivation rec {
     pname = "dune";
-    version = "3.19.0";
+    version = "3.19.1";
 
     src = fetchurl {
       url = "https://github.com/ocaml/dune/releases/download/${version}/dune-${version}.tbz";
-      hash = "sha256-0vYX39NPfYgvQYGiLjWbuQtGZp7YeyZQ64QvBTL8aWw=";
+      hash = "sha256-oQOG+YDNqUF9FGVGa+1Q3SrvnJO50GoPf+7tsKFUEVg=";
     };
 
     nativeBuildInputs = [
