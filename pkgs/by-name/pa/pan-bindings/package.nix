7273f78b36735d5bc7ab326b6ad192777947cc57
Martin Häcker <spamfaenger@gmx.de>
2025-06-12 08:04:14 +0200

    pan-bindings: update to version 1.1.1

--- pkgs/by-name/pa/pan-bindings/package.nix
+++ pkgs/by-name/pa/pan-bindings/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch2,
   buildGoModule,
   cmake,
   ncurses,
@@ -9,19 +10,23 @@
 }:
 
 let
-  version = "unstable-2024-03-03";
+  version = "1.1.1";
   src = fetchFromGitHub {
     owner = "lschulz";
     repo = "pan-bindings";
-    rev = "4361d30f1c5145a70651c259f2d56369725b0d15";
-    hash = "sha256-0WxrgXTCM+BwGcjjWBBKiZawje2yxB5RRac6Sk5t3qc=";
+    tag = "v${version}";
+    hash = "sha256-FpjbfmJwCx+aZxipE1QQTgOlXHtv2VkIKWSMQylr+WM=";
   };
   goDeps = (
     buildGoModule {
       name = "pan-bindings-goDeps";
       inherit src version;
+      prePatch = ''
+        # can be removed once https://github.com/NixOS/nixpkgs/pull/414434 is fully merged
+        substituteInPlace go/go.mod --replace-fail "toolchain go1.24.4" "toolchain go1.24.3"
+      '';
       modRoot = "go";
-      vendorHash = "sha256-7EitdEJTRtiM29qmVnZUM6w68vCBI8mxZhCA7SnAxLA=";
+      vendorHash = "sha256-3MybV76pHDnKgN2ENRgsyAvynXQctv0fJcRGzesmlww=";
     }
   );
 in
@@ -36,12 +41,21 @@ stdenv.mkDerivation {
     "-DBUILD_EXAMPLES=0"
   ];
 
-  patchPhase = ''
-    runHook prePatch
+  prePatch = ''
     export HOME=$TMP
     cp -r --reflink=auto ${goDeps.goModules} go/vendor
-    runHook postPatch
+    # can be removed once https://github.com/NixOS/nixpkgs/pull/414434 is fully merged
+    substituteInPlace go/go.mod --replace-fail "toolchain go1.24.4" "toolchain go1.24.3"
   '';
+
+  patches = [
+    # can be removed in the next release when the pull request is merged or the build issue otherwise resolved
+    (fetchpatch2 {
+      name = "fix-darwin-build";
+      url = "https://patch-diff.githubusercontent.com/raw/lschulz/pan-bindings/pull/4.patch";
+      hash = "sha256-0/lg3TxSosELtKycn+oJtK0JBDFQrQBbwUPu2ah9lgI=";
+    })
+  ];
 
   buildInputs = [
     ncurses
