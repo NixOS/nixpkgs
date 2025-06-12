53cd67877df74c1c1e7316ea83a1e91ff04520ad
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-12 14:23:40 +0000

    python3Packages.editdistpy: 0.1.5 -> 0.1.6

--- pkgs/development/python-modules/editdistpy/default.nix
+++ pkgs/development/python-modules/editdistpy/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "editdistpy";
-  version = "0.1.5";
+  version = "0.1.6";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "mammothb";
     repo = "editdistpy";
     tag = "v${version}";
-    hash = "sha256-kTaJkx1fdd2Rl4uhzxdZAFP/ArsM0qTPweJ1jlUcjxQ=";
+    hash = "sha256-bUdwhMFDIhHuIlcqIZt6mSh8xwW/2igw0QiWGvQBLC8=";
   };
 
   build-system = [
@@ -58,7 +58,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Fast Levenshtein and Damerau optimal string alignment algorithms";
     homepage = "https://github.com/mammothb/editdistpy";
-    changelog = "https://github.com/mammothb/editdistpy/releases/tag/v${version}";
+    changelog = "https://github.com/mammothb/editdistpy/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ vizid ];
   };
