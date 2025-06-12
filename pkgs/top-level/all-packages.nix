763f4984bf2940194dfce47aac7f172a2e01adb4
Fugi <me@fugi.dev>
2025-06-12 14:03:10 +0200

    joplin-desktop: build from source

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12939,7 +12939,15 @@ with pkgs;
 
   jalv-qt = jalv.override { useQt = true; };
 
-  joplin-desktop = joplin-desktop-bin;
+  # fallback to joplin-desktop-bin on Darwin until build is fixed
+  joplin-desktop =
+    let
+      joplin-desktop-source = callPackage ../by-name/jo/joplin-desktop/package.nix { };
+      package = if lib.meta.availableOn stdenv.hostPlatform joplin-desktop-source
+        then joplin-desktop-source
+        else joplin-desktop-bin;
+    in
+      package;
 
   js8call = qt5.callPackage ../applications/radio/js8call { };
 
