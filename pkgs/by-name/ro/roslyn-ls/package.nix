64667c0d4cbff985f3dbd7a50168d4945082b5c2
Konrad Malik <konrad.malik@gmail.com>
2025-06-12 12:49:11 +0200

    roslyn-ls: 5.0.0-1.25266.2 -> 5.0.0-1.25302.10

--- pkgs/by-name/ro/roslyn-ls/package.nix
+++ pkgs/by-name/ro/roslyn-ls/package.nix
@@ -32,18 +32,18 @@ in
 buildDotnetModule rec {
   inherit pname dotnet-sdk dotnet-runtime;
 
-  vsVersion = "2.80.16";
+  vsVersion = "2.82.12";
   src = fetchFromGitHub {
     owner = "dotnet";
     repo = "roslyn";
     rev = "VSCode-CSharp-${vsVersion}";
-    hash = "sha256-4kFu0bxbLZ31ifGluSrN9sN5EwzLg3bVHnh9iB9TGpA=";
+    hash = "sha256-5QCiA2NxjWUFLut8gxboR2kTibN66QCxbe2g2jdrINo=";
   };
 
   # versioned independently from vscode-csharp
   # "roslyn" in here:
   # https://github.com/dotnet/vscode-csharp/blob/main/package.json
-  version = "5.0.0-1.25266.2";
+  version = "5.0.0-1.25302.10";
   projectFile = "src/LanguageServer/${project}/${project}.csproj";
   useDotnetFromEnv = true;
   nugetDeps = ./deps.json;
