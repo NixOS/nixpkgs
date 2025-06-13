d89cb1411f95577a1f231bfe1401111e272bc87a
musjj <72612857+musjj@users.noreply.github.com>
2025-06-13 18:34:14 +0700

    plasticscm-client-gui-unwrapped: init at 11.0.16.9656

---
+++ pkgs/by-name/pl/plasticscm-client-gui-unwrapped/package.nix
@@ -0,0 +1,52 @@
+{
+  dpkg,
+  fetchurl,
+  lib,
+  stdenvNoCC,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "plasticscm-client-gui-unwrapped";
+  version = "11.0.16.9656";
+
+  src = fetchurl {
+    url = "https://www.plasticscm.com/plasticrepo/stable/debian/amd64/plasticscm-client-gui_${finalAttrs.version}_amd64.deb";
+    hash = "sha256-H+9vi9Yfz3uVA/dUwpMmrPOfGt0elAJ+jeFs2usp5L0=";
+  };
+
+  nativeBuildInputs = [
+    dpkg
+  ];
+
+  dontPatch = true;
+  dontConfigure = true;
+  dontBuild = true;
+  doCheck = false;
+  dontFixup = true;
+
+  unpackPhase = ''
+    runHook preUnpack
+
+    dpkg-deb -x $src .
+
+    runHook postUnpack
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r opt usr/{share,bin} $out
+
+    runHook postInstall
+  '';
+
+  meta = {
+    homepage = "https://www.plasticscm.com";
+    description = "A Software Configuration Management system from Unity that tracks changes to source code and any digital asset over time";
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "plasticgui";
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ musjj ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+  };
+})
