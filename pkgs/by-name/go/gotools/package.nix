15c9c5466020294b8c675acf0000794e6644d7ed
phanirithvij <phanirithvij2000@gmail.com>
2025-06-13 23:29:06 +0530

    gotools: modernize derivation
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/go/gotools/package.nix
+++ pkgs/by-name/go/gotools/package.nix
@@ -6,15 +6,15 @@
   go,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "gotools";
   version = "0.34.0";
 
-  # using GitHub instead of https://go.googlesource.com/tools because Gitiles UI is to basic to browse
+  # using GitHub instead of https://go.googlesource.com/tools because Gitiles UI is too basic to browse
   src = fetchFromGitHub {
     owner = "golang";
     repo = "tools";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-C+P2JoD4NzSAkAQuA20bVrfLZrMHXekvXn8KPOM5Nj4=";
   };
 
@@ -45,16 +45,16 @@ buildGoModule rec {
       --suffix PATH : ${lib.makeBinPath [ go ]}
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Additional tools for Go development";
     longDescription = ''
       This package contains tools like: godoc, goimports, callgraph, digraph, stringer or toolstash.
     '';
     homepage = "https://go.googlesource.com/tools";
-    license = licenses.bsd3;
-    maintainers = with maintainers; [
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [
       SuperSandro2000
       techknowlogick
     ];
   };
-}
+})
