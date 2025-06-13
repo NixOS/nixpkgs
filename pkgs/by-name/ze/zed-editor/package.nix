2dcc1c25764c18d729ac5a6fb2223c458ac82411
botnk <github-botnk@korz.dev>
2025-06-13 01:45:45 +0000

    zed-editor: 0.189.5 -> 0.190.5

--- pkgs/by-name/ze/zed-editor/package.nix
+++ pkgs/by-name/ze/zed-editor/package.nix
@@ -99,7 +99,7 @@ let
 in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zed-editor";
-  version = "0.189.5";
+  version = "0.190.5";
 
   outputs =
     [ "out" ]
@@ -111,7 +111,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "zed-industries";
     repo = "zed";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-d1d3WgUVamrYWVosljQiEPZGNNDldtM1YwZhxseX4+w=";
+    hash = "sha256-PGxD/37Njm/Oie86iIirTYIiZxxYx9jdBIMCzamwaDY=";
   };
 
   patches = [
@@ -138,7 +138,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     '';
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-YhdwCNTbBphWugguoWQqrGf2fRB5Jv40MElW6hbcxtk=";
+  cargoHash = "sha256-cuEDKjHkPuaOAwaP1YGaeAtEbNStlL2FgZ0rNkPLKsM=";
 
   nativeBuildInputs =
     [
