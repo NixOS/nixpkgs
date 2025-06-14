34bcedc9617391ebcc5b6cae6f054263b70a23a6
Lukas Nagel <lukasnagel@fastmail.com>
2025-06-14 17:48:34 +0200

    php.extensions.systemd: init at 0.1.2-5-g22cf92a

---
+++ pkgs/development/php-packages/systemd/default.nix
@@ -0,0 +1,36 @@
+{
+  fetchFromGitHub,
+  buildPecl,
+  systemd,
+  lib,
+}:
+
+buildPecl {
+
+  pname = "systemd";
+  version = "0.1.2-5-g22cf92a";
+
+  src = fetchFromGitHub {
+    owner = "systemd";
+    repo = "php-systemd";
+    rev = "22cf92a6b54ef4c5c13c301fc97d7a7b1615ee62";
+    hash = "sha256-UhpE9QXChqKLBqutpQ2Y6neo/ULlJGNojf9DCYJfQMU=";
+  };
+
+  buildInputs = [ systemd.dev ];
+
+  configureFlags = [ "--with-systemd=${systemd.dev}" ];
+
+  installPhase = ''
+    runHook preInstall
+    install -D .libs/systemd.so $out/lib/php/extensions/systemd.so
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "PHP extension allowing native interaction with systemd and its journal";
+    homepage = "https://github.com/systemd/php-systemd";
+    license = lib.licenses.mit;
+    teams = [ lib.teams.php ];
+  };
+}
