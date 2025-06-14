9944b4a72a677e859a44f87779efb6cd4b092968
Lukas Nagel <lukasnagel@fastmail.com>
2025-06-14 19:33:04 +0200

    php-systemd: add patch to supress warning
    would show "Warning: Missing arginfo for sd_journal_send() in Unknown on line 0" otherwise

--- pkgs/development/php-packages/systemd/default.nix
+++ pkgs/development/php-packages/systemd/default.nix
@@ -1,6 +1,7 @@
 {
   fetchFromGitHub,
   buildPecl,
+  fetchpatch,
   systemd,
   lib,
 }:
@@ -16,6 +17,13 @@ buildPecl {
     rev = "22cf92a6b54ef4c5c13c301fc97d7a7b1615ee62";
     hash = "sha256-UhpE9QXChqKLBqutpQ2Y6neo/ULlJGNojf9DCYJfQMU=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/systemd/php-systemd/pull/8.diff";
+      hash = "sha256-g73qI6gKpj5keaGIXdujcii8IWSSBUK9h8gUUVNcKVc=";
+    })
+  ];
 
   buildInputs = [ systemd.dev ];
 
