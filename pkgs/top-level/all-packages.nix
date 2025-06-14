a3ec238accfc7ec7597c2be51067c17f9eeabbef
Stefan Frijters <sfrijters@gmail.com>
2025-06-14 13:50:28 +0200

    mailutils: explicitly use gsasl
    This is also explicitly referenced in the relevant configure flag
    and this means we can move mailutils to pkgs/by-name.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3738,9 +3738,7 @@ with pkgs;
     libtool = if stdenv.hostPlatform.isDarwin then cctools else libtool;
   };
 
-  mailutils = callPackage ../tools/networking/mailutils {
-    sasl = gsasl;
-  };
+  mailutils = callPackage ../tools/networking/mailutils { };
 
   man = man-db;
 
