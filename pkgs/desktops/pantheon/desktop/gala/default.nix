df364b6b451a55b4d3c9e260a973f4d6c2f0b81a
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-14 09:34:44 +0000

    pantheon.gala: 8.2.2 -> 8.2.3

--- pkgs/desktops/pantheon/desktop/gala/default.nix
+++ pkgs/desktops/pantheon/desktop/gala/default.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gala";
-  version = "8.2.2";
+  version = "8.2.3";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    hash = "sha256-S71NryqJjWXZgyBj6q088bdjt/NnAEJ6oeUO2OMJ3Z4=";
+    hash = "sha256-6M9IWwrCaJoi7b5e4ltdyZfdT7KkOgsollHNKhLPr9U=";
   };
 
   patches = [
