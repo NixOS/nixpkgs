7080eaf5cec9e9f8b251abb4151ac5f4be30f2cd
Aaron Jheng <wentworth@outlook.com>
2025-06-14 21:43:52 +0800

    mysql-shell-innovation: use protobuf_25

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -580,7 +580,7 @@ with pkgs;
   mysql-shell-innovation = callPackage ../development/tools/mysql-shell/innovation.nix {
     antlr = antlr4_10;
     icu = icu73;
-    protobuf = protobuf_24;
+    protobuf = protobuf_25;
     stdenv = if stdenv.hostPlatform.isDarwin then llvmPackages_18.stdenv else stdenv;
   };
 
