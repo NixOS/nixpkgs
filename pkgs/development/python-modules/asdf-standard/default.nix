3c5ab7f0aed8c578916a40a82187d4c19c250236
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-15 01:46:51 +0000

    python3Packages.asdf-standard: 1.2.0 -> 1.3.0

--- pkgs/development/python-modules/asdf-standard/default.nix
+++ pkgs/development/python-modules/asdf-standard/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "asdf-standard";
-  version = "1.2.0";
+  version = "1.3.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "asdf_standard";
     inherit version;
-    hash = "sha256-5wmRL68L4vWEOiOvJzHm927WwnmynfWYnhUgmS+jxc8=";
+    hash = "sha256-WViWHzmd6tIACnhyTaN/Wu6wSZp4C72a5Pw+y+Pq7WQ=";
   };
 
   nativeBuildInputs = [ setuptools-scm ];
