cfc89fc22777b99fc1aa97bf5f9eca0c589a35a4
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2025-06-15 08:46:37 +0200

    smplayer: fetchFromGitHub: rev -> tag
    Inspired by
    https://github.com/NixOS/nixpkgs/commit/cb9f9a1e5a51a87d59b373db7016cee1608debd5

--- pkgs/by-name/sm/smplayer/package.nix
+++ pkgs/by-name/sm/smplayer/package.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "smplayer-dev";
     repo = "smplayer";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-txGz6v9hkvnrmVmBHsi1B2eC/iNT1tg4dU5AcMsSCic=";
   };
 
@@ -51,7 +51,7 @@ stdenv.mkDerivation (finalAttrs: {
       options of MPlayer, SMPlayer adds other interesting features like the
       possibility to play Youtube videos or download subtitles.
     '';
-    changelog = "https://github.com/smplayer-dev/smplayer/releases/tag/${finalAttrs.src.rev}";
+    changelog = "https://github.com/smplayer-dev/smplayer/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ ];
     platforms = lib.platforms.linux;
