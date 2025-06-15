b1217fc569ff4d26a761ac8d94c0900a9cdd61be
Felix Salcher <dev.felix.salcher@gmail.com>
2025-06-15 17:32:12 +0200

    add sessionizer package

---
+++ pkgs/by-name/se/sessionizer/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  pkgs,
+  buildGoModule,
+}:
+buildGoModule {
+  pname = "sessionizer";
+  version = "0.1.4";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "salfel";
+    repo = "sessionizer";
+    rev = "v0.1.4";
+    sha256 = "sha256-JZaJPxUEkUcNKB5EvoBuoMpiSZuB+q3hyl+tYjcI/ag=";
+  };
+
+  goPackagePath = "github.com/salfel/sessionizer";
+
+  vendorHash = "sha256-04uCddd3+5YlAATkrNciHaem9lX/2P8AHDfRDH0ut7c=";
+
+  proxyVendor = true;
+
+  buildInputs = [pkgs.fzf];
+
+  meta = with lib; {
+    description = "Session manager for tmux";
+    homepage = "https://github.com/salfel/sessionizer";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [salfel];
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
