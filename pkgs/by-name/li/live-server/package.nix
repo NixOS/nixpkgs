a2ca055494d999e9f5f5a57a0f6938674c86a3ba
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-15 12:57:14 +0200

    live-server: 0.10.0 -> 0.10.0-unstable-2025-06-15

--- pkgs/by-name/li/live-server/package.nix
+++ pkgs/by-name/li/live-server/package.nix
@@ -8,17 +8,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "live-server";
-  version = "0.10.0";
+  version = "0.10.0-unstable-2025-06-15";
 
   src = fetchFromGitHub {
     owner = "lomirus";
     repo = "live-server";
-    rev = "v${version}";
-    hash = "sha256-8NM3XJ7RC7cUNKN1DPW2huvkx7tfA8zDrETkwDMbaT8=";
+    rev = "8cadd4bd40568c1bb8006f3a12c5cd269071c147";
+    hash = "sha256-BlVQKsf5wfqiqMv2fHMYaHm8a+CjbkcNvD6YgZbxd7k=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-tUKhVDv+ZDGRpJC/sSYcQxYhGsAyOsflc+GeUyBaeEk=";
+  cargoHash = "sha256-lW6sZHbroqMtt/JANXD+McFt4DXtlFXEZ9sDuXV25ns=";
 
   nativeBuildInputs = [ pkg-config ];
 
