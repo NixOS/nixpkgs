eaebbdb9a56b9bbda4e31ca29501e61644fbfec0
emilylange <git@emilylange.de>
2025-06-15 19:36:06 +0200

    nixos/sourcehut,sourcehut.*,nixosTests.sourcehut: drop
    Sourcehut went a year with no update in nixpkgs, the packages did not
    build for months, the module has issues at runtime, one of the
    maintainers stopped using NixOS entirely and the other two don't respond
    to issues.
    
    Upstream has since also deprecated the Arch Linux and Debian
    repositories to install Sourcehut. The only official way that remains is
    Alpine Linux on x86_64-linux.

--- nixos/modules/rename.nix
+++ nixos/modules/rename.nix
@@ -236,6 +236,9 @@ in
       the program being unmaintained. The options `programs.msmtp.*` can be
       used instead.
     '')
+    (mkRemovedOptionModule [ "services" "sourcehut" ] ''
+      The sourcehut packages and the corresponding module have been removed due to being broken and unmaintained.
+    '')
     (mkRemovedOptionModule [ "services" "tvheadend" ]
       "The tvheadend package and the corresponding module have been removed as nobody was willing to maintain them and they were stuck on an unmaintained version that required FFmpeg 4; please see https://github.com/NixOS/nixpkgs/pull/332259 if you are interested in maintaining a newer version."
     )
