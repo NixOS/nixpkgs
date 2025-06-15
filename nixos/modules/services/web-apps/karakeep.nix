d84209398565d771b6e1e7bf52ecf0550e78d5be
Anton Tetov <anton@tetov.se>
2025-06-15 21:02:28 +0200

    nixos/karakeep: expose meilisearch dumpless upgrades

--- nixos/modules/services/web-apps/karakeep.nix
+++ nixos/modules/services/web-apps/karakeep.nix
@@ -90,6 +90,22 @@ in
             required for text search.
           '';
         };
+
+        # TODO: remove when this is either handled by karakeep or becomes default
+        #       in services.meilisearch.
+        experimental_dumpless_upgrade = lib.mkOption {
+          default = true;
+          description = ''
+            Whether to enable (experimental) dumpless upgrade of the search index.
+            Allows upgrading Meilisearch without manually dumping and importing
+            the database.
+            {option}`services.meilisearch.settings.experimental_dumpless_upgrade`
+            overrides this option if set explicitly.
+
+            More information at https://www.meilisearch.com/docs/learn/update_and_migration/updating#dumpless-upgrade
+          '';
+          type = lib.types.bool;
+        };
       };
     };
   };
@@ -103,8 +119,9 @@ in
       group = "karakeep";
     };
 
-    services.meilisearch = lib.mkIf cfg.meilisearch.enable {
-      enable = true;
+    services.meilisearch = {
+      enable = cfg.meilisearch.enable;
+      settings.experimental_dumpless_upgrade = lib.mkDefault cfg.meilisearch.experimental_dumpless_upgrade;
     };
 
     systemd.services.karakeep-init = {
