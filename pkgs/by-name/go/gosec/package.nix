f1c39bb277d1e7c409b2e3954363343c46148c17
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-16 14:40:12 +0000

    gosec: 2.22.4 -> 2.22.5

--- pkgs/by-name/go/gosec/package.nix
+++ pkgs/by-name/go/gosec/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "gosec";
-  version = "2.22.4";
+  version = "2.22.5";
 
   src = fetchFromGitHub {
     owner = "securego";
     repo = "gosec";
     rev = "v${version}";
-    hash = "sha256-a1rxomazSWm/hLpV6lnkEsIpOKyworKQZRFe9ZuEQ1I=";
+    hash = "sha256-gvKqBXiOK1KF0Y2+d7f/8QhhuvspgV632iizoKZgaQk=";
   };
 
-  vendorHash = "sha256-UUwoEkpjz0v3c4HBlMhnhOqotZDG5TQcOXqexNgj3yE=";
+  vendorHash = "sha256-T29x5+n2MVIo3c1iabn5tQWrBKD96Cwo/EKWzHYgwrc=";
 
   subPackages = [
     "cmd/gosec"
