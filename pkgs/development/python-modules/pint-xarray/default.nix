1a55121ffd98c4d271551773944fd791282c678f
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-16 18:00:31 +0000

    python3Packages.pint-xarray: 0.4 -> 0.5.0

--- pkgs/development/python-modules/pint-xarray/default.nix
+++ pkgs/development/python-modules/pint-xarray/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pint-xarray";
-  version = "0.4";
+  version = "0.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "xarray-contrib";
     repo = "pint-xarray";
     tag = "v${version}";
-    hash = "sha256-IFHSgrnqS7ZpNhRzzSgHPRUP90WNv84jBH4um/DRMCU=";
+    hash = "sha256-fvVIQagrL1QvIcN/mePx6cT8mbsvx08vu6tGc4tuLwc=";
   };
 
   build-system = [
@@ -44,7 +44,7 @@ buildPythonPackage rec {
   meta = {
     description = "Interface for using pint with xarray, providing convenience accessors";
     homepage = "https://github.com/xarray-contrib/pint-xarray";
-    changelog = "https://github.com/xarray-contrib/pint-xarray/blob/v${version}/docs/whats-new.rst";
+    changelog = "https://github.com/xarray-contrib/pint-xarray/blob/${src.tag}/docs/whats-new.rst";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ doronbehar ];
   };
