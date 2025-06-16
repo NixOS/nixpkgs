039712e1904c514cca7e24c4e86d25755cdf3deb
Johan Herland <johan@herland.net>
2025-06-16 12:56:41 +0000

    maintainers: add jherland

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11697,6 +11697,12 @@
     githubId = 56772267;
     name = "Jürgen Hahn";
   };
+  jherland = {
+    email = "johan@herland.net";
+    github = "jherland";
+    githubId = 547031;
+    name = "Johan Herland";
+  };
   jhh = {
     email = "jeff@j3ff.io";
     github = "jhh";
