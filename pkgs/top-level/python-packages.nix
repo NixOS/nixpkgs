68cf4d4fec0c041e3cbc7ff82e3582f5afb833ae
Johan Herland <johan@herland.net>
2025-06-17 13:43:35 +0000

    python3Packages.pan-python: init at 0.25.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11260,6 +11260,8 @@ self: super: with self; {
 
   pamqp = callPackage ../development/python-modules/pamqp { };
 
+  pan-python = callPackage ../development/python-modules/pan-python { };
+
   panacotta = callPackage ../development/python-modules/panacotta { };
 
   panasonic-viera = callPackage ../development/python-modules/panasonic-viera { };
