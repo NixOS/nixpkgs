ff44a7ae68cd61052ebbac1a6cb10609d259290a
qbisi <qbisicwate@gmail.com>
2025-06-18 00:19:42 +0800

    netgen: typo fixup
    (cherry picked from commit d79bf12714703a6f316360b9361baeea43c4639b)

--- pkgs/by-name/ne/netgen/package.nix
+++ pkgs/by-name/ne/netgen/package.nix
@@ -164,7 +164,7 @@ stdenv.mkDerivation (finalAttrs: {
       mkdir -p $out/Applications/${finalAttrs.pname}.app/Contents/{MacOS,Resouces}
       substituteInPlace $out/Info.plist --replace-fail "Netgen1" "netgen"
       mv $out/Info.plist $out/Applications/${finalAttrs.pname}.app/Contents
-      mv $out/Netgen.icns $out/Applications/${finalAttrs.pname}.app/Contents/Resouces
+      mv $out/Netgen.icns $out/Applications/${finalAttrs.pname}.app/Contents/Resources
       ln -s $out/bin/netgen $out/Applications/${finalAttrs.pname}.app/Contents/MacOS/netgen
     ''
     + lib.optionalString stdenv.hostPlatform.isLinux ''
