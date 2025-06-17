9edd40908d9eb3d602eeba9af92f135b373f24e5
DavHau <hsngrmpf+github@gmail.com>
2025-06-17 22:14:56 +0700

    python3Minimal: remove pkg-config dependency
    Removes the build time dependency on pkg-config if python3 is build with the `withMinimal` flag enabled
    
    This change is driven by the motivation to use python3 earlier in stdenv for hooks.

--- pkgs/development/interpreters/python/cpython/default.nix
+++ pkgs/development/interpreters/python/cpython/default.nix
@@ -209,13 +209,15 @@ let
   nativeBuildInputs = [
     nukeReferences
   ]
-  ++ optionals (!stdenv.hostPlatform.isDarwin) [
+  ++ optionals (!stdenv.hostPlatform.isDarwin && !withMinimalDeps) [
     autoconf-archive # needed for AX_CHECK_COMPILE_FLAG
     autoreconfHook
   ]
-  ++ optionals (!stdenv.hostPlatform.isDarwin || passthru.pythonAtLeast "3.14") [
-    pkg-config
-  ]
+  ++
+    optionals ((!stdenv.hostPlatform.isDarwin || passthru.pythonAtLeast "3.14") && !withMinimalDeps)
+      [
+        pkg-config
+      ]
   ++ optionals (stdenv.hostPlatform != stdenv.buildPlatform) [
     buildPackages.stdenv.cc
     pythonOnBuildForHost
