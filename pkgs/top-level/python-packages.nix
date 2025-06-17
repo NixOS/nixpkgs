7831975bbdb55aec0bbfea9c22dd8cc970fd0e45
Johan Herland <johan@herland.net>
2025-06-17 09:40:06 +0000

    python3Packages.optuna-dashboard: init at 0.18.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -10831,6 +10831,8 @@ self: super: with self; {
 
   optuna = callPackage ../development/python-modules/optuna { };
 
+  optuna-dashboard = callPackage ../development/python-modules/optuna-dashboard { };
+
   optype = callPackage ../development/python-modules/optype { };
 
   opuslib = callPackage ../development/python-modules/opuslib { };
