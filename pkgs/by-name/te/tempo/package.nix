92b66a9a6dd7d1be3d1b272c1c9aaebff1a57e7e
Adam C. Stephens <adam@valkor.net>
2025-06-17 14:14:36 +0000

    tempo: 2.7.2 -> 2.8.0
    https://github.com/grafana/tempo/releases/tag/v2.8.0

--- pkgs/by-name/te/tempo/package.nix
+++ pkgs/by-name/te/tempo/package.nix
@@ -7,14 +7,14 @@
 
 buildGoModule rec {
   pname = "tempo";
-  version = "2.7.2";
+  version = "2.8.0";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "tempo";
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-JBbECknhqYKzFlxAOVinuUCf/2xuUdh2ryK9c35YZ9o=";
+    hash = "sha256-rloHxcGrgVhBGIKg29wzhPfa3NQ8pI8aISiVi2M/BJ4=";
   };
 
   vendorHash = null;
@@ -22,7 +22,6 @@ buildGoModule rec {
   subPackages = [
     "cmd/tempo-cli"
     "cmd/tempo-query"
-    "cmd/tempo-serverless"
     "cmd/tempo-vulture"
     "cmd/tempo"
   ];
