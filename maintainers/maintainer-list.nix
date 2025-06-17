aa90facd5b2eee84bed6be812f2b042905defcf6
Tor <torberge@outlook.com>
2025-06-17 08:26:19 +0200

    maintainers: add tbs1996

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -24500,6 +24500,11 @@
     githubId = 156964;
     name = "Thomas Boerger";
   };
+  tbs1996 = {
+    github = "tbs1996";
+    githubId = 56874491;
+    name = "Tor Berge Sæbjørnsen";
+  };
   tbwanderer = {
     github = "tbwanderer";
     githubId = 125365236;
