51823bd263e4bc51994551941b0be9c5810a4453
Adam C. Stephens <adam@valkor.net>
2025-06-18 13:44:09 +0000

    tempo: 2.8.0 -> 2.8.1
    https://github.com/grafana/tempo/releases/tag/v2.8.1

--- pkgs/by-name/te/tempo/package.nix
+++ pkgs/by-name/te/tempo/package.nix
@@ -7,14 +7,14 @@
 
 buildGoModule rec {
   pname = "tempo";
-  version = "2.8.0";
+  version = "2.8.1";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "tempo";
     rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-rloHxcGrgVhBGIKg29wzhPfa3NQ8pI8aISiVi2M/BJ4=";
+    hash = "sha256-RzDOx2ZyA0ZntFD1ryfipsgPsxVmsdOusZ37RCnQQnM=";
   };
 
   vendorHash = null;
