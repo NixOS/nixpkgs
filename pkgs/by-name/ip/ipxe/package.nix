fb10cd716c0e5ba162ec9e097530ba78be53b868
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-18 18:57:38 +0000

    ipxe: 1.21.1-unstable-2025-06-02 -> 1.21.1-unstable-2025-06-17

--- pkgs/by-name/ip/ipxe/package.nix
+++ pkgs/by-name/ip/ipxe/package.nix
@@ -48,7 +48,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ipxe";
-  version = "1.21.1-unstable-2025-06-02";
+  version = "1.21.1-unstable-2025-06-17";
 
   nativeBuildInputs = [
     mtools
@@ -65,8 +65,8 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "ipxe";
     repo = "ipxe";
-    rev = "5b3ebf8b24ae40a6f9f9f78491702d508f843e56";
-    hash = "sha256-uGR82lR6gpp6IKBVDsKYLtovnbTiWg3RgbVQt6mug+I=";
+    rev = "60e167c00b138fdb162e7b2192e9cbb196ed3cbd";
+    hash = "sha256-RaYu3gvqAxA+vEJdLOLMbCRtrt+PyrRd/Wqyj2hXYZU=";
   };
 
   # Calling syslinux on a FAT image isn't going to work on Aarch64.
