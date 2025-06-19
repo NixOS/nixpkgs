58991629e7a7a34498283542a0f49f1942a17f66
TigerGorilla2 <tigergorilla2@proton.me>
2025-06-19 16:02:32 +0000

    python3Packages.cysystemd: init at 2.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3168,6 +3168,8 @@ self: super: with self; {
 
   cysignals = callPackage ../development/python-modules/cysignals { };
 
+  cysystemd = callPackage ../development/python-modules/cysystemd { };
+
   cython = callPackage ../development/python-modules/cython { };
 
   cython-test-exception-raiser =
