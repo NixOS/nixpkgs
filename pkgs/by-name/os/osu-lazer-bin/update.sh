8c2c536ad58a81935a7d4ac82366cd1f864b5727
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-19 03:06:07 +0200

    osu-lazer-bin: fix update script
    (cherry picked from commit a468be49a81c2b16d122c15160ff94e20003b51c)

--- pkgs/by-name/os/osu-lazer-bin/update.sh
+++ pkgs/by-name/os/osu-lazer-bin/update.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 bin_file="$(realpath ./package.nix)"
 
-new_version="$(curl -s "https://api.github.com/repos/ppy/osu/releases?per_page=1" | jq -r '.[0].name')"
+new_version="$(curl -s "https://api.github.com/repos/ppy/osu/releases/latest" | jq -r '.name')"
 old_version="$(sed -nE 's/\s*version = "(.*)".*/\1/p' ./package.nix)"
 if [[ "$new_version" == "$old_version" ]]; then
     echo "Already up to date."
