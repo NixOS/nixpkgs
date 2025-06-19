7193460a42234899f827f91314e200c4fa6ff339
luigifab <31816829+luigifab@users.noreply.github.com>
2025-06-19 10:02:19 +0200

    Fix package.nix with dtomvan suggestions

--- pkgs/by-name/aw/awf/package.nix
+++ pkgs/by-name/aw/awf/package.nix
@@ -2,25 +2,25 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  replaceVars,
   autoreconfHook,
   gtk2,
   gtk3,
   gtk4,
   libnotify,
-  hicolor-icon-theme,
   pkg-config,
   wrapGAppsHook3,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "awf";
   version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "luigifab";
     repo = "awf-extended";
-    tag = "v${version}";
-    sha256 = "60e71e05a6620b54debc0a148ddbf911fc314b1e0cf6c9cf159a104bfca57bb6";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-HZEsg3eEysPSkj3hj6KwcZTUI5FOzbOEYbu6RDdx9q4=";
   };
 
   nativeBuildInputs = [
@@ -33,6 +33,19 @@ stdenv.mkDerivation rec {
     gtk2
     gtk3
     gtk4
+    libnotify
+  ];
+
+  patches = [
+    (replaceVars ./include-paths.patch {
+      gtk2 = gtk2.dev;
+      gtk3 = gtk3.dev;
+      gtk4 = gtk4.dev;
+      GTK2_SUFFIX = null;
+      GTK3_SUFFIX = null;
+      GTK4_SUFFIX = null;
+      GTK5_SUFFIX = null;
+    })
   ];
 
   configureFlags = [ "--disable-gtk5" ];
@@ -44,34 +57,35 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
 
+    mkdir -p $out/bin
+
     for gtk in "gtk2" "gtk3" "gtk4"; do
 
-        install -Dpm 644 data/awf-$$gtk.desktop $out/share/applications/awf-$$gtk.desktop
+        cp src/awf-$gtk $out/bin
+        install -Dpm 644 data/awf-$gtk.desktop $out/share/applications/awf-$gtk.desktop
 
-        cp -ar data/icons/ icons-$$gtk/
-        for file in icons-$$gtk/*/*/awf.png; do mv $$file `dirname $$file`/awf-$$gtk.png; done
-        for file in icons-$$gtk/*/*/awf.svg; do mv $$file `dirname $$file`/awf-$$gtk.svg; done
+        cp -ar data/icons/ icons-$gtk/
+        for file in icons-$gtk/*/*/awf.png; do mv $file `dirname $file`/awf-$gtk.png; done
+        for file in icons-$gtk/*/*/awf.svg; do mv $file `dirname $file`/awf-$gtk.svg; done
         install -dm 755 $out/share/icons/hicolor/
-        cp -a icons-$$gtk/* $out/share/icons/hicolor/
+        cp -a icons-$gtk/* $out/share/icons/hicolor/
 
-        install -Dpm 644 data/awf-$$gtk.bash $out/share/bash-completion/completions/awf-$$gtk
-        install -Dpm 644 data/awf-$$gtk.1 $out/share/man/man1/awf-$$gtk.1
-        install -Dpm 644 data/awf-$$gtk.fr.1 $out/share/man/fr/man1/awf-$$gtk.1
+        install -Dpm 644 data/awf-$gtk.bash $out/share/bash-completion/completions/awf-$gtk
+        install -Dpm 644 data/awf-$gtk.1 $out/share/man/man1/awf-$gtk.1
+        install -Dpm 644 data/awf-$gtk.fr.1 $out/share/man/fr/man1/awf-$gtk.1
 
         for file in src/po/*.po; do
-            code=`basename "$$file" .po`
-            mkdir -p locale-$$gtk/$$code/LC_MESSAGES/
-            msgfmt src/po/$$code.po -o locale-$$gtk/$$code/LC_MESSAGES/awf-$$gtk.mo
-            install -Dpm 644 locale-$$gtk/$$code/LC_MESSAGES/awf-$$gtk.mo $out/share/locale/$$code/LC_MESSAGES/awf-$$gtk.mo
+            code=`basename "$file" .po`
+            mkdir -p locale-$gtk/$code/LC_MESSAGES/
+            msgfmt src/po/$code.po -o locale-$gtk/$code/LC_MESSAGES/awf-$gtk.mo
+            install -Dpm 644 locale-$gtk/$code/LC_MESSAGES/awf-$gtk.mo $out/share/locale/$code/LC_MESSAGES/awf-$gtk.mo
         done
     done
-
-    rm -rf icons-gtk*/ locale-gtk*/
 
     runHook postInstall
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Theme preview application for GTK";
     longDescription = ''
       A widget factory is a theme preview application for GTK. It displays the
@@ -79,8 +93,8 @@ stdenv.mkDerivation rec {
       the visual effect of the applied theme.
     '';
     homepage = "https://github.com/luigifab/awf-extended";
-    license = licenses.gpl3Plus;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ michalrus ];
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ michalrus ];
   };
-}
+})
