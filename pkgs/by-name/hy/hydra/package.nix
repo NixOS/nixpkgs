4430bec5ea49bd66e78d23703284ddad39db203e
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-19 22:27:23 +0000

    hydra: 0-unstable-2025-05-27 -> 0-unstable-2025-06-15
    (cherry picked from commit d1b8d29209cc92cc4888cabdba85c2c17a64760b)

--- pkgs/by-name/hy/hydra/package.nix
+++ pkgs/by-name/hy/hydra/package.nix
@@ -130,13 +130,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "hydra";
-  version = "0-unstable-2025-05-27";
+  version = "0-unstable-2025-06-15";
 
   src = fetchFromGitHub {
     owner = "NixOS";
     repo = "hydra";
-    rev = "2e3c168ec49fb78554247bf1aa7d11fbf716e107";
-    hash = "sha256-S3gG8xXItXdefeSIaR4tTzjHv+pceu/i5s+wGQNHAWQ=";
+    rev = "ed500ca4345f7edaa6c3d84027a7599d38b15a29";
+    hash = "sha256-Mxz4n40VKxA2ILtIHX6fObgcy2WlgsM+ERfpjuVJ0BU=";
   };
 
   outputs = [
