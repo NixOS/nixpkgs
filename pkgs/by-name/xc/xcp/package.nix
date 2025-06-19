257439de746afe631d5df5174bdf932adc530a60
Jörg Thalheim <joerg@thalheim.io>
2025-06-19 14:20:51 +0200

    xcp: fix build on darwin
    Co-authored-by: Michael Hoang <enzime@users.noreply.github.com>

--- pkgs/by-name/xc/xcp/package.nix
+++ pkgs/by-name/xc/xcp/package.nix
@@ -5,6 +5,7 @@
   acl,
   nix-update-script,
   installShellFiles,
+  stdenv,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
@@ -22,7 +23,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   nativeBuildInputs = [ installShellFiles ];
 
-  checkInputs = [ acl ];
+  checkInputs = lib.optionals stdenv.isLinux [ acl ];
 
   # disable tests depending on special filesystem features
   checkNoDefaultFeatures = true;
@@ -33,6 +34,29 @@ rustPlatform.buildRustPackage (finalAttrs: {
     "test_no_acl"
     "test_no_xattr"
     "test_no_perms"
+  ];
+
+  # had concurrency issues on 64 cores, also tests are quite fast compared to build
+  dontUseCargoParallelTests = true;
+  checkFlags = lib.optionals stdenv.isDarwin [
+    # ---- test_socket_file::test_with_parallel_file_driver stdout ----
+    # STDOUT: 12:20:56 [WARN] Socket copy not supported by this OS: /private/tmp/nix-build-xcp-0.24.1.drv-0/source/targ>
+    #
+    # STDERR:
+    #
+    # thread 'test_socket_file::test_with_parallel_file_driver' panicked at tests/common.rs:1149:5:
+    # assertion failed: to.exists()
+    # note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+    "--skip=test_socket_file::test_with_parallel_file_driver"
+
+    # ---- test_sockets_dir::test_with_parallel_file_driver stdout ----
+    # STDOUT: 12:20:56 [WARN] Socket copy not supported by this OS: /private/tmp/nix-build-xcp-0.24.1.drv-0/source/targ>
+    #
+    # STDERR:
+    #
+    # thread 'test_sockets_dir::test_with_parallel_file_driver' panicked at tests/common.rs:1178:5:
+    # assertion failed: to.exists()
+    "--skip=test_sockets_dir::test_with_parallel_file_driver"
   ];
 
   postInstall = ''
