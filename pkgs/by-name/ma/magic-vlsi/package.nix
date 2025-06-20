561729a6f71a4474a53a752ee824b2c4de8627dc
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-20 22:14:52 +0000

    magic-vlsi: 8.3.528 -> 8.3.529

--- pkgs/by-name/ma/magic-vlsi/package.nix
+++ pkgs/by-name/ma/magic-vlsi/package.nix
@@ -15,11 +15,11 @@
 
 stdenv.mkDerivation rec {
   pname = "magic-vlsi";
-  version = "8.3.528";
+  version = "8.3.529";
 
   src = fetchurl {
     url = "http://opencircuitdesign.com/magic/archive/magic-${version}.tgz";
-    sha256 = "sha256-GueeY+UfiTJN05elfIrBq3OL+f4RNLs5navee5GSE44=";
+    sha256 = "sha256-O5NauR2TYRS2bzEVQlhCqkshlojIGqM1Y3KLN00YP40=";
   };
 
   nativeBuildInputs = [ python3 ];
