2071cbbe8135060f57f75cdb6c06b7a2ab67596a
Robert Hensing <robert@roberthensing.nl>
2025-06-21 13:28:15 +0200

    maintainers/scripts/nixos-tests-for-files.nix: Move and clarify intent

--- maintainers/scripts/nixos-tests-for-files.nix
+++ maintainers/scripts/nixos-tests-for-files.nix
@@ -1,24 +1,27 @@
 /**
   Returns a list of tests for a given list of NixOS module files (and/or other files which will be ignored).
 
+  This is a convenience wrapper for use with `nix-build`.
+
   A more detailed structure than a list is available in the `meta.tests` option value.
 
-  Example:
-    nix-build maintainers/nixos-tests-for-files.nix --arg files '[ ./nixos/modules/services/databases/postgresql.nix ]'
+  Example usage (to be run from the Nixpkgs root):
+    nix-build maintainers/scripts/nixos-tests-for-files.nix --arg files '[ ./nixos/modules/services/databases/postgresql.nix ]'
 */
 {
   files,
   system ? builtins.currentSystem,
+# NOTE: if you need finer control, you are encouraged to use `config.meta.tests` directly
 }:
 
 let
-  pkgs = import ../. {
+  pkgs = import ../.. {
     config = { };
     overlays = [ ];
     inherit system;
   };
   sample = pkgs.nixos {
-    imports = [ ../nixos/modules/virtualisation/qemu-vm.nix ];
+    imports = [ ../../nixos/modules/virtualisation/qemu-vm.nix ];
   };
 in
 # See nixos/modules/misc/meta.nix or :doc on an evaluated NixOS
