a73bbac70b635ad3c2edf27ed1e916ff90604fd4
Defelo <mail@defelo.de>
2025-06-21 22:46:15 +0200

    ytdl-sub: enable tests
    Co-authored-by: nialov <nikolasovaskainen@gmail.com>
    
    (cherry picked from commit 39504265f2434cdeaa16c01903512b20f464f9f5)

--- pkgs/by-name/yt/ytdl-sub/package.nix
+++ pkgs/by-name/yt/ytdl-sub/package.nix
@@ -42,8 +42,27 @@ python3Packages.buildPythonApplication rec {
     "--set YTDL_SUB_FFPROBE_PATH ${lib.getExe' ffmpeg "ffprobe"}"
   ];
 
-  nativeCheckInputs = [ versionCheckHook ];
+  nativeCheckInputs = [
+    versionCheckHook
+    python3Packages.pytestCheckHook
+  ];
   versionCheckProgramArg = "--version";
+
+  env = {
+    YTDL_SUB_FFMPEG_PATH = "${lib.getExe' ffmpeg "ffmpeg"}";
+    YTDL_SUB_FFPROBE_PATH = "${lib.getExe' ffmpeg "ffprobe"}";
+  };
+
+  disabledTests = [
+    "test_presets_run"
+    "test_logger_can_be_cleaned_during_execution"
+  ];
+
+  pytestFlagsArray = [
+    # According to documentation, e2e tests can be flaky:
+    # "This checksum can be inaccurate for end-to-end tests"
+    "--ignore=tests/e2e"
+  ];
 
   passthru.updateScript = ./update.sh;
 
