84983852e75bdcc8c7b873261c91970775d378d0
Tim Schumacher <timschumi@gmx.de>
2025-06-21 12:52:07 +0200

    ghidra: Use finalAttrs for extensions where sensible

--- pkgs/tools/security/ghidra/extensions/findcrypt/default.nix
+++ pkgs/tools/security/ghidra/extensions/findcrypt/default.nix
@@ -3,26 +3,23 @@
   fetchFromGitHub,
   buildGhidraExtension,
 }:
-let
-  version = "3.0.6";
-in
-buildGhidraExtension {
+buildGhidraExtension (finalAttrs: {
   pname = "findcrypt";
-  inherit version;
+  version = "3.0.6";
 
   src = fetchFromGitHub {
     owner = "antoniovazquezblanco";
     repo = "GhidraFindcrypt";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-VWi1MP72Vl4XCrbTvRA6qYPk2QyvRyVb9N8QQ/Zml0A=";
   };
 
   meta = {
     description = "Ghidra analysis plugin to locate cryptographic constants";
     homepage = "https://github.com/antoniovazquezblanco/GhidraFindcrypt";
-    downloadPage = "https://github.com/antoniovazquezblanco/GhidraFindcrypt/releases/tag/v${version}";
-    changelog = "https://github.com/antoniovazquezblanco/GhidraFindcrypt/releases/tag/v${version}";
+    downloadPage = "https://github.com/antoniovazquezblanco/GhidraFindcrypt/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/antoniovazquezblanco/GhidraFindcrypt/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.gpl3;
     maintainers = [ lib.maintainers.BonusPlay ];
   };
-}
+})
