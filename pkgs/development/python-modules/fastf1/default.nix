0eb22bbdc730fa066a42dfb4ef85318e450469c5
Vai <vai.sriv@icloud.com>
2025-06-21 00:46:50 -0400

    Update pkgs/development/python-modules/fastf1/default.nix
    Co-authored-by: emaryn <197520219+emaryn@users.noreply.github.com>

--- pkgs/development/python-modules/fastf1/default.nix
+++ pkgs/development/python-modules/fastf1/default.nix
@@ -69,7 +69,7 @@ buildPythonPackage {
 
   meta = with lib; {
     changelog = "https://github.com/theOehrly/Fast-F1/releases/tag/v${version}";
-    description = "Python package for accessing and analyzing Formula 1 results, schedules, timing data and telemetry.";
+    description = "Python package for accessing and analyzing Formula 1 results, schedules, timing data and telemetry";
     homepage = "https://github.com/theOehrly/Fast-F1";
     license = licenses.mit;
     maintainers = with maintainers; [ vaisriv ];
