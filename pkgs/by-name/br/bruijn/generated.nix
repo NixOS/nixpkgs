cb3208b79294faa621527a4d9a50e43e44e58edc
Defelo <mail@defelo.de>
2025-06-21 16:57:58 +0200

    bruijn: init at 0-unstable-2025-04-18

---
+++ pkgs/by-name/br/bruijn/generated.nix
@@ -0,0 +1,75 @@
+{
+  mkDerivation,
+  array,
+  base,
+  binary,
+  bitstring,
+  bytestring,
+  clock,
+  containers,
+  deepseq,
+  directory,
+  fetchzip,
+  filepath,
+  haskeline,
+  lib,
+  megaparsec,
+  mtl,
+  optparse-applicative,
+  process,
+  random,
+  time,
+}:
+mkDerivation {
+  pname = "bruijn";
+  version = "0.1.0.0";
+  src = fetchzip {
+    url = "https://github.com/marvinborner/bruijn/archive/20b49473bf3d450469698de3efce157d8ff7cc02.tar.gz";
+    sha256 = "04clkhdf8hr1410x2zkxzfsycgawfqb7cfzs8dyfb9vs8msn10wl";
+  };
+  isLibrary = true;
+  isExecutable = true;
+  enableSeparateDataOutput = true;
+  libraryHaskellDepends = [
+    array
+    base
+    binary
+    bitstring
+    bytestring
+    clock
+    containers
+    deepseq
+    directory
+    filepath
+    haskeline
+    megaparsec
+    mtl
+    optparse-applicative
+    process
+    random
+    time
+  ];
+  executableHaskellDepends = [
+    array
+    base
+    binary
+    bitstring
+    bytestring
+    clock
+    containers
+    deepseq
+    directory
+    filepath
+    haskeline
+    megaparsec
+    mtl
+    optparse-applicative
+    process
+    random
+    time
+  ];
+  homepage = "https://github.com/githubuser/bruijn#readme";
+  license = lib.licenses.mit;
+  mainProgram = "bruijn";
+  maintainers = [ lib.maintainers.defelo ];
+}
