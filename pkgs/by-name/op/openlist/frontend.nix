c453735f993584eeea0056600465ce76a1e3a7e5
Moraxyc <i@qaq.li>
2025-06-21 15:50:22 +0800

    openlist: init at 4.0.0

---
+++ pkgs/by-name/op/openlist/frontend.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+
+  nodejs,
+  pnpm_10,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "openlist-frontend";
+  version = "4.0.0-rc.4";
+
+  src = fetchFromGitHub {
+    owner = "OpenListTeam";
+    repo = "OpenList-Frontend";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-ZAEaGFOWzL3apV6KXokLV8GGhmp9C38npUhBngdPbq0=";
+  };
+
+  nativeBuildInputs = [
+    nodejs
+    pnpm_10.configHook
+  ];
+
+  pnpmDeps = pnpm_10.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-4NKzy4q6DjWgTL63WBEeDxjAsrgcM5vfHqtmZpS5dug=";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    pnpm build
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    cp -r dist $out
+    echo -n "v${finalAttrs.version}" > $out/VERSION
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Frontend of OpenList";
+    homepage = "https://github.com/OpenListTeam/OpenList-Frontend";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ moraxyc ];
+  };
+})
