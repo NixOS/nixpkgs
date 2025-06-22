c7758245ad23ee95b70e16331f7b37248cffaf7d
Gaetan Lepage <gaetan@glepage.com>
2025-06-23 00:41:49 +0200

    python3Packages.kmapper: cleanup

--- pkgs/development/python-modules/kmapper/default.nix
+++ pkgs/development/python-modules/kmapper/default.nix
@@ -2,17 +2,23 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+
+  # build-system
   setuptools,
-  scikit-learn,
-  numpy,
-  scipy,
+
+  # dependencies
   jinja2,
-  pytestCheckHook,
-  networkx,
-  matplotlib,
+  numpy,
+  scikit-learn,
+  scipy,
+
+  # tests
   igraph,
-  plotly,
   ipywidgets,
+  matplotlib,
+  networkx,
+  plotly,
+  pytestCheckHook,
 }:
 
 buildPythonPackage rec {
@@ -30,27 +36,28 @@ buildPythonPackage rec {
   build-system = [ setuptools ];
 
   dependencies = [
-    scikit-learn
-    numpy
-    scipy
     jinja2
+    numpy
+    scikit-learn
+    scipy
   ];
 
   pythonImportsCheck = [ "kmapper" ];
 
   nativeCheckInputs = [
-    pytestCheckHook
-    networkx
-    matplotlib
     igraph
-    plotly
     ipywidgets
+    matplotlib
+    networkx
+    plotly
+    pytestCheckHook
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Python implementation of Mapper algorithm for Topological Data Analysis";
     homepage = "https://kepler-mapper.scikit-tda.org/";
-    license = licenses.mit;
+    changelog = "https://github.com/scikit-tda/kepler-mapper/releases/tag/v${version}";
+    license = lib.licenses.mit;
     maintainers = [ ];
   };
 }
