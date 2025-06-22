8ff91dbe3a8200b6c0990158f8e8d0deb1995c7c
Thibaut Smith <thibaut.smith@mailbox.org>
2025-06-22 16:59:59 +0200

    maintainers: add videl

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -26359,6 +26359,12 @@
     githubId = 335406;
     name = "David Asabina";
   };
+  videl = {
+    email = "thibaut.smith@mailbox.org";
+    github = "videl";
+    githubId = 123554;
+    name = "Thibaut Smith";
+  };
   vidister = {
     email = "v@vidister.de";
     github = "vidister";
