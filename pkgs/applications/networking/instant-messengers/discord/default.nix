ee7c38cef0c4790f3d89745ba37dbea9e6148a30
Sefa Eyeoglu <contact@scrumplex.net>
2025-06-22 18:26:06 +0200

    discord-canary: 0.0.687 -> 0.0.702
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>
    (cherry picked from commit a649e49b9d060b18f2c2b81b7a4b11a93d5a3bca)

--- pkgs/applications/networking/instant-messengers/discord/default.nix
+++ pkgs/applications/networking/instant-messengers/discord/default.nix
@@ -11,7 +11,7 @@ let
       {
         stable = "0.0.98";
         ptb = "0.0.148";
-        canary = "0.0.687";
+        canary = "0.0.702";
         development = "0.0.75";
       }
     else
@@ -34,7 +34,7 @@ let
       };
       canary = fetchurl {
         url = "https://canary.dl2.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-        hash = "sha256-OaDN+Qklxieo9xlP8qVeCwWzPBe6bLXoFUkMOFCoqPg=";
+        hash = "sha256-OcRGqwf13yPnbDpYOyXZgEQN/zWshUXfaF5geiLetlc=";
       };
       development = fetchurl {
         url = "https://development.dl2.discordapp.net/apps/linux/${version}/discord-development-${version}.tar.gz";
