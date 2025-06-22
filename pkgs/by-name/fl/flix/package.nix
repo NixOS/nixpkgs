43992627fe5cf3bf1df6eac540fe66d7aa3c9c38
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-22 13:51:54 +0000

    flix: 0.59.0 -> 0.60.0

--- pkgs/by-name/fl/flix/package.nix
+++ pkgs/by-name/fl/flix/package.nix
@@ -8,11 +8,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "flix";
-  version = "0.59.0";
+  version = "0.60.0";
 
   src = fetchurl {
     url = "https://github.com/flix/flix/releases/download/v${version}/flix.jar";
-    sha256 = "sha256-Rh1i0wL6+Td0j+eJ4qCYYSz8dmG1Op7Z0cGBBjjJ68Q=";
+    sha256 = "sha256-mSCxB8m060K/F8b1VhQbTMjtv//feS3pobxShFBc08U=";
   };
 
   dontUnpack = true;
