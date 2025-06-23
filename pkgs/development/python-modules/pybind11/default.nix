1bed0591bd215eb3c8eddf9d2feeb7d9505891f9
Doron Behar <doron.behar@gmail.com>
2025-06-23 12:33:58 +0300

    python313Packages.pybind11: no with lib; in meta

--- pkgs/development/python-modules/pybind11/default.nix
+++ pkgs/development/python-modules/pybind11/default.nix
@@ -100,7 +100,7 @@ buildPythonPackage rec {
 
   hardeningDisable = lib.optional stdenv.hostPlatform.isMusl "fortify";
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/pybind/pybind11";
     changelog = "https://github.com/pybind/pybind11/blob/${src.rev}/docs/changelog.rst";
     description = "Seamless operability between C++11 and Python";
@@ -110,8 +110,8 @@ buildPythonPackage rec {
       C++ types in Python and vice versa, mainly to create Python
       bindings of existing C++ code.
     '';
-    license = licenses.bsd3;
-    maintainers = with maintainers; [
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [
       yuriaisaka
       dotlambda
     ];
