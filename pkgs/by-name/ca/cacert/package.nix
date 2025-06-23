ec049d732fb00865ee3dff919be685c5f4b33998
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-06-23 06:20:37 +0200

    cacert: 3.111 -> 3.113
    https://github.com/nss-dev/nss/blob/master/doc/rst/releases/nss_3_113.rst

--- pkgs/by-name/ca/cacert/package.nix
+++ pkgs/by-name/ca/cacert/package.nix
@@ -23,7 +23,7 @@ let
     lib.concatStringsSep "\n\n" extraCertificateStrings
   );
 
-  srcVersion = "3.111";
+  srcVersion = "3.113";
   version = if nssOverride != null then nssOverride.version else srcVersion;
   meta = with lib; {
     homepage = "https://curl.haxx.se/docs/caextract.html";
@@ -47,7 +47,7 @@ let
           owner = "nss-dev";
           repo = "nss";
           rev = "NSS_${lib.replaceStrings [ "." ] [ "_" ] version}_RTM";
-          hash = "sha256-GFtoSvLF5nAwBIiMa9CeEl5geAOK60gG2tjuQFubgYs=";
+          hash = "sha256-gjkJaNNTqtaMxO6PuRLZ8ATMP88L4LXCtpT84fahcmU=";
         };
 
     dontBuild = true;
