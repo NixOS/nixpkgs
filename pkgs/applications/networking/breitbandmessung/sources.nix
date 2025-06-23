4822bccad5259bab0fdba5051f73ba53d9c03cb8
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-23 09:02:59 +0000

    breitbandmessung: 3.8.0 -> 3.9.0

--- pkgs/applications/networking/breitbandmessung/sources.nix
+++ pkgs/applications/networking/breitbandmessung/sources.nix
@@ -1,11 +1,11 @@
 {
-  version = "3.8.0";
+  version = "3.9.0";
   x86_64-linux = {
-    url = "https://download.breitbandmessung.de/bbm/Breitbandmessung-3.8.0-linux.deb";
-    sha256 = "sha256-0smSLq8PAwEWK9yf4GeUjXcKlKVRoqfb+qd8w3ppQqA=";
+    url = "https://download.breitbandmessung.de/bbm/Breitbandmessung-3.9.0-linux.deb";
+    sha256 = "sha256-OG+oZr5UHIjrQOxPmLs/DzGJuUAd5pAyvLuTOvhC+20=";
   };
   x86_64-darwin = {
-    url = "https://download.breitbandmessung.de/bbm/Breitbandmessung-3.8.0-mac.dmg";
-    sha256 = "sha256-lysO25oDNDs86Es2VO3XSafVpZU5mcKAZXEdkn0AHig=";
+    url = "https://download.breitbandmessung.de/bbm/Breitbandmessung-3.9.0-mac.dmg";
+    sha256 = "sha256-Tvb2Cum/Bavu+VAVS/1O7pxSIVLdP2XzTG27fhgIh9E=";
   };
 }
