cb9a1c479f3e05bda7f2d24ecfb4bedff616a1fd
deng232 <78670850+deng232@users.noreply.github.com>
2025-06-22 22:33:36 -0400

    Update nixos/modules/services/networking/proxy-ns.nix
    Co-authored-by: Aleksana <alexander.huang.y@gmail.com>

--- nixos/modules/services/networking/proxy-ns.nix
+++ nixos/modules/services/networking/proxy-ns.nix
@@ -16,7 +16,7 @@ in
   options.services.proxy-ns = {
     enable = lib.mkEnableOption "proxy-ns";
     settings = lib.mkOption {
-      type = (with lib.types; nullOr (attrsOf unspecified));
+      type = configFormat.type;
       description = "";
       example = {
         tun_name = "default";
