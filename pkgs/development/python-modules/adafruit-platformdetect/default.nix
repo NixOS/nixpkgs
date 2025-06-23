89a43bad9f217cf559bc04b0e063a7d8d2f8c7c9
Fabian Affolter <mail@fabian-affolter.ch>
2025-06-23 11:31:11 +0200

    python313Packages.adafruit-platformdetect: refactor

--- pkgs/development/python-modules/adafruit-platformdetect/default.nix
+++ pkgs/development/python-modules/adafruit-platformdetect/default.nix
@@ -3,15 +3,12 @@
   buildPythonPackage,
   fetchPypi,
   setuptools-scm,
-  pythonOlder,
 }:
 
 buildPythonPackage rec {
   pname = "adafruit-platformdetect";
   version = "3.80.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "adafruit_platformdetect";
@@ -30,7 +27,7 @@ buildPythonPackage rec {
     description = "Platform detection for use by Adafruit libraries";
     homepage = "https://github.com/adafruit/Adafruit_Python_PlatformDetect";
     changelog = "https://github.com/adafruit/Adafruit_Python_PlatformDetect/releases/tag/${version}";
-    license = with licenses; [ mit ];
+    license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
 }
