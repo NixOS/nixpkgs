46a7d6950eb01a3713b29c20aa8de86adf39d05f
Alyssa Ross <hi@alyssa.is>
2025-06-24 14:19:47 +0200

    Revert "linuxPackages_ham: init"
    This reverts commit b987b0ba896bf16c288365432e7549d8954d435a.
    
    The config options previously enabled by linux_ham are now enabled in
    the default kernel.
    
    This is slightly altered to contain aliases `linux_ham = linux` to not
    break that interface on stable.
    
    (cherry picked from commit 56cbb4e5d8c3b45d577964ea9e11a5a1da9ee2aa)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11246,10 +11246,6 @@ with pkgs;
   linux-rt = linuxPackages-rt.kernel;
   linux-rt_latest = linuxPackages-rt_latest.kernel;
 
-  # Amateur Radio kernel
-  linuxPackages_ham = linuxKernel.packages.linux_ham;
-  linux_ham = linuxPackages_ham.kernel;
-
   # hardened kernels
   linuxPackages_hardened = linuxKernel.packages.linux_hardened;
   linux_hardened = linuxPackages_hardened.kernel;
