a9a4d79c197db7374b296ee461b9dde29fefe25a
Martin Häcker <spamfaenger@gmx.de>
2025-06-24 08:39:07 +0200

    llm-lmstudio: init at 0.2.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8904,6 +8904,8 @@ self: super: with self; {
 
   llm-llama-server = callPackage ../development/python-modules/llm-llama-server { };
 
+  llm-lmstudio = callPackage ../development/python-modules/llm-lmstudio { };
+
   llm-mistral = callPackage ../development/python-modules/llm-mistral { };
 
   llm-ollama = callPackage ../development/python-modules/llm-ollama { };
