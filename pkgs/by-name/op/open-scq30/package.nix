20b7af380c85af4482c0ca7d5d9c61879a8d10af
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-06-24 14:48:06 -0400

    open-scq30: Use cargo-wrapper, rec -> finalAttrs, remove with lib;

--- pkgs/by-name/op/open-scq30/package.nix
+++ pkgs/by-name/op/open-scq30/package.nix
@@ -12,59 +12,61 @@
   gtk4,
   libadwaita,
   pango,
-  cargo-make,
 }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "open-scq30";
-  version = "1.12.0";
+rustPlatform.buildRustPackage.override
+  (old: {
+    cargo = old.cargo.withCommands (c: [ c.cargo-make ]);
+  })
+  (finalAttrs: {
+    pname = "open-scq30";
+    version = "1.12.0";
 
-  src = fetchFromGitHub {
-    owner = "Oppzippy";
-    repo = "OpenSCQ30";
-    rev = "v${version}";
-    hash = "sha256-DL2hYm1j27K0nnBvE3iGnguqm0m1k56bkuG+6+u4u4c=";
-  };
+    src = fetchFromGitHub {
+      owner = "Oppzippy";
+      repo = "OpenSCQ30";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-DL2hYm1j27K0nnBvE3iGnguqm0m1k56bkuG+6+u4u4c=";
+    };
 
-  nativeBuildInputs = [
-    pkg-config
-    protobuf
-    wrapGAppsHook4
-    cargo-make
-  ];
+    nativeBuildInputs = [
+      pkg-config
+      protobuf
+      wrapGAppsHook4
+    ];
 
-  buildInputs = [
-    cairo
-    dbus
-    gdk-pixbuf
-    glib
-    gtk4
-    libadwaita
-    pango
-  ];
+    buildInputs = [
+      cairo
+      dbus
+      gdk-pixbuf
+      glib
+      gtk4
+      libadwaita
+      pango
+    ];
 
-  useFetchCargoVendor = true;
-  cargoHash = "sha256-3K+/CpTGWSjCRa2vOEcDvLIiZMdntugIqnzkXF4wkng=";
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-3K+/CpTGWSjCRa2vOEcDvLIiZMdntugIqnzkXF4wkng=";
 
-  INSTALL_PREFIX = placeholder "out";
+    INSTALL_PREFIX = placeholder "out";
 
-  # Requires headphones
-  doCheck = false;
+    # Requires headphones
+    doCheck = false;
 
-  buildPhase = ''
-    cargo make --profile release build
-  '';
+    buildPhase = ''
+      cargo make --profile release build
+    '';
 
-  installPhase = ''
-    cargo make --profile release install
-  '';
+    installPhase = ''
+      cargo make --profile release install
+    '';
 
-  meta = with lib; {
-    description = "Cross platform application for controlling settings of Soundcore headphones";
-    homepage = "https://github.com/Oppzippy/OpenSCQ30";
-    changelog = "https://github.com/Oppzippy/OpenSCQ30/blob/${src.rev}/CHANGELOG.md";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ mkg20001 ];
-    mainProgram = "open-scq30";
-  };
-}
+    meta = {
+      description = "Cross platform application for controlling settings of Soundcore headphones";
+      homepage = "https://github.com/Oppzippy/OpenSCQ30";
+      changelog = "https://github.com/Oppzippy/OpenSCQ30/blob/v${finalAttrs.version}/CHANGELOG.md";
+      license = lib.licenses.gpl3Only;
+      maintainers = with lib.maintainers; [ mkg20001 ];
+      mainProgram = "open-scq30";
+    };
+  })
