ce321c6cb250080f013601da51179f4729279269
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-24 11:18:25 +0000

    rime-wanxiang: 7.1.1 -> 8.1.4

--- pkgs/by-name/ri/rime-wanxiang/package.nix
+++ pkgs/by-name/ri/rime-wanxiang/package.nix
@@ -9,13 +9,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "rime-wanxiang";
-  version = "7.1.1";
+  version = "8.1.4";
 
   src = fetchFromGitHub {
     owner = "amzxyz";
     repo = "rime_wanxiang";
     tag = "v" + finalAttrs.version;
-    hash = "sha256-SPKUTWwyxN/pLn9cSFZL3+RfPYfQuNoOehYRMTOoC4I=";
+    hash = "sha256-d2HKzukfCJnh0zHeoEuhFKHLlQay/gp3P87Bik4Xt+k=";
   };
 
   nativeBuildInputs = [
