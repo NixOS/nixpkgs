1c8d13d3657feb25c98cc195df8ace6497d76379
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-24 23:51:17 +0000

    xivlauncher: 1.1.2 -> 1.2.0

--- pkgs/by-name/xi/xivlauncher/package.nix
+++ pkgs/by-name/xi/xivlauncher/package.nix
@@ -17,7 +17,7 @@
 }:
 
 let
-  rev = "1.1.2";
+  rev = "1.2.0";
 in
 buildDotnetModule rec {
   pname = "XIVLauncher";
@@ -27,7 +27,7 @@ buildDotnetModule rec {
     owner = "goatcorp";
     repo = "XIVLauncher.Core";
     inherit rev;
-    hash = "sha256-QkA18C3oWxcW0vK6sji7vbm4bY5LxwIWierAg5qf38Q=";
+    hash = "sha256-2ZL3giUrc4mUNh8rr+yI+k2Sx5rZsoFGR1ZBlTugH6M=";
     fetchSubmodules = true;
   };
 
