1a0d09ef5209ac7d54f657825d6ee7545bedbcdb
Grimmauld <Grimmauld@grimmauld.de>
2025-06-25 20:37:08 +0200

    libcap_ng: use github sources
    Using github sources simplifies usage of update script.
    It is also preferrable, as it gives less opportunity
    for ingenuine preconfigured artifacts to be included
    in the release tarball.
    
    Checked using diffoscope: The output is identical to
    what the build using `fetchurl` yields, apart from
    the `libtool` version string in `$out/lib/libdrop_ambient.la`
    and nix store hashes.

--- pkgs/by-name/li/libcap_ng/package.nix
+++ pkgs/by-name/li/libcap_ng/package.nix
@@ -1,7 +1,9 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
+  autoreconfHook,
+  swig,
   testers,
 }:
 
@@ -9,13 +11,25 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "libcap-ng";
   version = "0.8.5";
 
-  src = fetchurl {
-    url = "https://people.redhat.com/sgrubb/libcap-ng/libcap-ng-${finalAttrs.version}.tar.gz";
-    hash = "sha256-O6UpTRy9+pivqs+8ALavntK4PoohgXGF39hEzIx6xv8=";
+  src = fetchFromGitHub {
+    owner = "stevegrubb";
+    repo = "libcap-ng";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-qcHIHG59PDPfPsXA1r4hG4QhK2qyE7AgXOwUDjIy7lE=";
   };
+
+  # NEWS needs to exist or else the build fails
+  postPatch = ''
+    touch NEWS
+  '';
 
   strictDeps = true;
   enableParallelBuilding = true;
+
+  nativeBuildInputs = [
+    autoreconfHook
+    swig
+  ];
 
   outputs = [
     "out"
