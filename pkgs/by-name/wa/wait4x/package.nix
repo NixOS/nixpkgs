c9751941493a5f0e17d803f769a9a7784d7c6c6e
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-25 12:41:32 +0000

    wait4x: 3.3.1 -> 3.4.0

--- pkgs/by-name/wa/wait4x/package.nix
+++ pkgs/by-name/wa/wait4x/package.nix
@@ -5,7 +5,7 @@
 }:
 let
   pname = "wait4x";
-  version = "3.3.1";
+  version = "3.4.0";
 in
 buildGoModule {
   inherit pname version;
@@ -14,10 +14,10 @@ buildGoModule {
     owner = "wait4x";
     repo = "wait4x";
     rev = "v${version}";
-    hash = "sha256-3s+ug4KqFv1P55eqNfAB2jbSPVzySxlHmkDMuuVlJcQ=";
+    hash = "sha256-Pb2Klupm6cNYUQ3bWBHwr3NW1HCdit2NFFISn9/c860=";
   };
 
-  vendorHash = "sha256-dN7R2d7roA6H9wIz2sBaWctD8K6M8nbQbwPc3t/7rlk=";
+  vendorHash = "sha256-6gRiYQYtkADBAMNqma4PfuzIttseyE/bHnlkpOgsVjI=";
 
   # Tests make network access
   doCheck = false;
