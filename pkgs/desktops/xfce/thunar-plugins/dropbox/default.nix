2ec701aab71becb56f0744def78857779c4b64a2
Bobby Rong <rjl931189261@126.com>
2025-06-25 23:19:03 +0800

    xfce.thunar-dropbox-plugin: 0.3.1 -> 0.3.2
    https://github.com/Jeinzi/thunar-dropbox/compare/0.3.1...0.3.2

--- pkgs/desktops/xfce/thunar-plugins/dropbox/default.nix
+++ pkgs/desktops/xfce/thunar-plugins/dropbox/default.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation rec {
   pname = "thunar-dropbox";
-  version = "0.3.1";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
     owner = "Jeinzi";
     repo = "thunar-dropbox";
     rev = version;
-    sha256 = "sha256-q7tw/1JgEn9SyjH1KBZl0tintWJjd3ctUs4JUuCWULs=";
+    sha256 = "sha256-uYqO87ftEtnSRn/yMSF1jVGleYXR3hVj2Jb1/kAd64Y=";
   };
 
   nativeBuildInputs = [
