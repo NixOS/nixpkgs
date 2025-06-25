9de1b9a9e33fef0c2a7cbf183fe9a93168a5facb
Fang Deng <fang@elfang.com>
2025-06-24 22:39:05 -0400

    rime-wanxiang: update description

--- pkgs/by-name/ri/rime-wanxiang/package.nix
+++ pkgs/by-name/ri/rime-wanxiang/package.nix
@@ -59,27 +59,33 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   meta = {
     description = "Feature-rich pinyin schema for Rime, basic edition";
     longDescription = ''
-      万象拼音基础版 is a basic quanpin and shuangpin input schema for Rime based on
+      万象拼音 is a full and double pinyin input schema for Rime based on
       [万象 dictionaries and grammar models](https://github.com/amzxyz/RIME-LMDG),
-      supporting traditional shuangpin as well as tonal schemata such as 自然龙 and
-      龙码.
-
-      The schema requires to work the grammar model `wanxiang-lts-zh-hans.gram`.
-      However, this file is
-      [released](https://github.com/amzxyz/RIME-LMDG/releases/tag/LTS) by
-      carelessly overriding the old versions
-      (see the [discussion](https://github.com/amzxyz/RIME-LMDG/issues/22)). So
-      we can't pack it into Nixpkgs, which demands reproducibility. You have to
-      download it yourself and place it in the user directory of Rime.
+      supporting multiple input styles, tonal dictionaries and predictions.
 
       The upstream `default.yaml` is included as
-      `wanxiang_suggested_default.yaml`. To enable it, please modify your
-      `default.custom.yaml` as such:
+      `wanxiang_suggested_default.yaml`.
+      To enable it, please modify your `default.custom.yaml` as such:
 
       ```yaml
       patch:
         __include: wanxiang_suggested_default:/
       ```
+
+      For further fine-grained tweaks, refer to it's
+      [README](https://github.com/amzxyz/rime_wanxiang) page.
+
+      Please note that, the schema requires the grammar model
+      `wanxiang-lts-zh-hans.gram` to work. However, it is
+      [released](https://github.com/amzxyz/RIME-LMDG/releases/tag/LTS) by
+      carelessly overriding the old version of the file under the same tag
+      (see the [discussion](https://github.com/amzxyz/RIME-LMDG/issues/22)).
+      This is against reproducibility in Nix so we can't include it within this
+      package. Same goes for dictionary releases.
+
+      To make it easier updating grammar model and dictionaries, users can
+      run the helper script `update-rime-wanxiang-data` shipped with this
+      package.
     '';
     homepage = "https://github.com/amzxyz/rime_wanxiang";
     downloadPage = "https://github.com/amzxyz/rime_wanxiang/releases";
