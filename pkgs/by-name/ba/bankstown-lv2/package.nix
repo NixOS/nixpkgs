a02f22b52c715fc24a739022c3014bd282885251
Jasi <normalc@posteo.net>
2025-06-25 15:34:42 -0400

    bankstown-lv2: remove overriding default phases

--- pkgs/by-name/ba/bankstown-lv2/package.nix
+++ pkgs/by-name/ba/bankstown-lv2/package.nix
@@ -2,6 +2,7 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  stdenv,
 }:
 
 rustPlatform.buildRustPackage rec {
@@ -15,16 +16,21 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-IThXEY+mvT2MCw0PSWU/182xbUafd6dtm6hNjieLlKg=";
   };
 
+  postPatch = ''
+    substituteInPlace Makefile \
+      --replace-fail "target/release" \
+                     "target/${stdenv.hostPlatform.rust.cargoShortTarget}/$cargoBuildType"
+  '';
+
   useFetchCargoVendor = true;
   cargoHash = "sha256-eMN95QNnQtC7QDix9g3dwb9ZbtQuiVBj8+R+opFs0KI=";
 
-  installPhase = ''
-    export LIBDIR=$out/lib
-    mkdir -p $LIBDIR
+  dontCargoInstall = true;
 
-    make
-    make install
-  '';
+  installFlags = [
+    "DESTDIR=$(out)"
+    "LIBDIR=lib"
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/chadmed/bankstown";
