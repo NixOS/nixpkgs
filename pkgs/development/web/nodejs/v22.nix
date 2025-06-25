747692182b8ed18ed79cdf36442891db26905a68
Antoine du Hamel <duhamelantoine1995@gmail.com>
2025-06-25 02:19:14 +0200

    nodejs_22: 22.16.0 -> 22.17.0
    (cherry picked from commit 8f7bfa2f6b9a46b4bca4cce4ae52673ed1c263dd)

--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -14,8 +14,8 @@ let
 in
 buildNodejs {
   inherit enableNpm;
-  version = "22.16.0";
-  sha256 = "720894f323e5c1ac24968eb2676660c90730d715cb7f090be71a668662a17c37";
+  version = "22.17.0";
+  sha256 = "7a3ef2aedb905ea7926e5209157266e2376a5db619d9ac0cba3c967f6f5db4f9";
   patches = [
     ./configure-emulator.patch
     ./configure-armv6-vfpv2.patch
@@ -60,6 +60,13 @@ buildNodejs {
       url = "https://github.com/nodejs/node/commit/869d0cbca3b0b5e594b3254869a34d549664e089.patch?full_index=1";
       hash = "sha256-BBBShQwU20TSY8GtPehQ9i3AH4ZKUGIr8O0bRsgrpNo=";
       revert = true;
+    })
+
+    # Fix for flaky test
+    # TODO: remove when included in a release
+    (fetchpatch2 {
+        url = "https://github.com/nodejs/node/commit/cd685fe3b6b18d2a1433f2635470513896faebe6.patch?full_index=1";
+        hash = "sha256-KA7WBFnLXCKx+QVDGxFixsbj3Y7uJkAKEUTeLShI1Xo=";
     })
   ];
 }
