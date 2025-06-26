55c84376b6ab05fa029b505920ae3a9c3ad1e701
awwpotato <awwpotato@voidq.com>
2025-06-26 10:53:48 -0700

    qnial: drop
    (cherry picked from commit 1ba4241cbd61c5a5efff6e66cea28624320c9d70)

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1612,6 +1612,7 @@ mapAliases {
   qbittorrent-qt5 = throw "'qbittorrent-qt5' has been removed as qBittorrent 5 dropped support for Qt 5. Please use 'qbittorrent'"; # Added 2024-09-30
   qcsxcad = throw "'qcsxcad' has been renamed to/replaced by 'libsForQt5.qcsxcad'"; # Converted to throw 2024-10-17
   qflipper = qFlipper; # Added 2022-02-11
+  qnial = throw "'qnial' has been removed due to failing to build and being unmaintained"; # Added 2025-06-26
   qscintilla = libsForQt5.qscintilla; # Added 2023-09-20
   qscintilla-qt6 = qt6Packages.qscintilla; # Added 2023-09-20
   qt515 = qt5; # Added 2022-11-24
