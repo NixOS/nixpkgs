755251f687d05c540ad98b3882410d220c455a7b
deng232 <deng232@purdue.edu>
2025-06-26 13:08:16 -0400

    refactor package.nix structure

--- pkgs/by-name/pr/proxy-ns/package.nix
+++ pkgs/by-name/pr/proxy-ns/package.nix
@@ -5,34 +5,31 @@
   nix-update-script,
   lib,
 }:
-let
-  proxyns = buildGoModule rec {
-    name = "proxy-ns";
-    version = "2.2.3";
-    vendorHash = null;
-    src = fetchFromGitHub {
-      owner = "OkamiW";
-      repo = name;
-      rev = "v${version}";
-      hash = "sha256-km+Rd3oXeyOX7wCoLL9f67AU/AVZTuGlvU0jtgTvgJE=";
-    };
-    ldflags = [
-      "-s"
-      "-w"
-    ];
-    GOFLAGS = [
-      "-buildvcs=false"
-      #"-trimpath" added by default by buildGoModule
-    ];
-    env.CGO_ENABLED = 0;
-    passthru.updateScript = nix-update-script { };
-    meta = {
-      description = "Linux-specific command-line tool that can force any program to use your SOCKS5 proxy server.";
-      mainProgram = "proxy-ns";
-      homepage = "https://github.com/OkamiW/proxy-ns";
-      license = with lib.licenses; [ gpl3Plus ];
-      maintainers = with lib.maintainers; [ deng232 ];
-    };
+buildGoModule rec {
+  name = "proxy-ns";
+  version = "2.2.3";
+  vendorHash = null;
+  src = fetchFromGitHub {
+    owner = "OkamiW";
+    repo = name;
+    rev = "v${version}";
+    hash = "sha256-km+Rd3oXeyOX7wCoLL9f67AU/AVZTuGlvU0jtgTvgJE=";
   };
-in
-proxyns
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+  GOFLAGS = [
+    "-buildvcs=false"
+    #"-trimpath" added by default by buildGoModule
+  ];
+  env.CGO_ENABLED = 0;
+  passthru.updateScript = nix-update-script { };
+  meta = {
+    description = "Linux-specific command-line tool that can force any program to use your SOCKS5 proxy server.";
+    mainProgram = "proxy-ns";
+    homepage = "https://github.com/OkamiW/proxy-ns";
+    license = with lib.licenses; [ gpl3Plus ];
+    maintainers = with lib.maintainers; [ deng232 ];
+  };
+}
