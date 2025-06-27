7df9093e53ece43f34598a37c8c76f5793a1ffd5
Gavin John <gavinnjohn@gmail.com>
2025-06-27 10:16:37 -0400

    plover.dev: rename to plover

--- pkgs/applications/misc/plover/default.nix
+++ pkgs/applications/misc/plover/default.nix
@@ -3,6 +3,7 @@
   config,
   stdenv,
   plover,
+  buildPythonPackage ? python3Packages.buildPythonPackage,
   fetchFromGitHub,
   fetchpatch,
   versionCheckHook,
@@ -32,79 +33,81 @@ let
     wrapQtAppsHook
     ;
 in
-{
-  dev = (
-    buildPythonPackage rec {
-      pname = "plover";
-      version = "4.0.2";
-      pyproject = true;
+buildPythonPackage rec {
+  pname = "plover";
+  version = "4.0.2";
+  pyproject = true;
 
-      src = fetchFromGitHub {
-        owner = "openstenoproject";
-        repo = "plover";
-        tag = "v${version}";
-        hash = "sha256-VpQT25bl8yPG4J9IwLkhSkBt31Y8BgPJdwa88WlreA8=";
-      };
+  src = fetchFromGitHub {
+    owner = "openstenoproject";
+    repo = "plover";
+    tag = "v${version}";
+    hash = "sha256-VpQT25bl8yPG4J9IwLkhSkBt31Y8BgPJdwa88WlreA8=";
+  };
 
-      postPatch = ''
-        sed -i 's/,<77//g' pyproject.toml # pythonRelaxDepsHook doesn't work for this for some reason
-      '';
+  postPatch = ''
+    sed -i 's/,<77//g' pyproject.toml # pythonRelaxDepsHook doesn't work for this for some reason
+  '';
 
-      build-system = [
-        babel
-        setuptools
-        pyqt5
-        wheel
-      ];
-      dependencies = [
-        appdirs
-        evdev
-        pyqt5
-        pyserial
-        plover-stroke
-        rtf-tokenize
-        setuptools
-        wcwidth
-        xlib
-      ];
-      nativeBuildInputs = [
-        wrapQtAppsHook
-      ];
+  build-system = [
+    babel
+    setuptools
+    pyqt5
+    wheel
+  ];
+  dependencies = [
+    appdirs
+    evdev
+    pyqt5
+    pyserial
+    plover-stroke
+    rtf-tokenize
+    setuptools
+    wcwidth
+    xlib
+  ];
+  nativeBuildInputs = [
+    wrapQtAppsHook
+  ];
 
-      nativeCheckInputs = [
-        pytestCheckHook
-        versionCheckHook
-        pytest-qt
-        mock
-      ];
+  nativeCheckInputs = [
+    pytestCheckHook
+    versionCheckHook
+    pytest-qt
+    mock
+  ];
 
-      # Segfaults?!
-      disabledTestPaths = [ "test/gui_qt/test_dictionaries_widget.py" ];
+  # Segfaults?!
+  disabledTestPaths = [ "test/gui_qt/test_dictionaries_widget.py" ];
 
-      preFixup = ''
-        makeWrapperArgs+=("''${qtWrapperArgs[@]}")
-      '';
+  preFixup = ''
+    makeWrapperArgs+=("''${qtWrapperArgs[@]}")
+  '';
 
-      dontWrapQtApps = true;
+  dontWrapQtApps = true;
 
-      pythonImportsCheck = [ "plover" ];
+  pythonImportsCheck = [ "plover" ];
 
-      meta = {
-        description = "OpenSteno Plover stenography software";
-        homepage = "https://www.openstenoproject.org/plover/";
-        mainProgram = "plover";
-        maintainers = with lib.maintainers; [
-          twey
-          kovirobi
-          pandapip1
-        ];
-        license = lib.licenses.gpl2Plus;
-        platforms = lib.platforms.unix;
-        broken = stdenv.hostPlatform.isDarwin;
-      };
-    }
-  );
+  meta = {
+    description = "OpenSteno Plover stenography software";
+    homepage = "https://www.openstenoproject.org/plover/";
+    mainProgram = "plover";
+    maintainers = with lib.maintainers; [
+      twey
+      kovirobi
+      pandapip1
+    ];
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    broken = stdenv.hostPlatform.isDarwin;
+  };
 }
 // lib.optionalAttrs config.allowAliases {
-  stable = throw "plover.stable was removed because it used Python 2. Use plover.dev instead."; # added 2022-06-05
+  # TODO: After 26.05 branch-off, remove these aliases
+  dev =
+    if lib.versionOlder "25.05" lib.version then
+      throw "plover.dev was renamed. Use plover-dev instead." # added 2025-06-05`
+    else
+      plover;
+  stable = throw "plover.stable was renamed. Use plover instead."; # added 2022-06-05; updated 2025-06-27
 }
