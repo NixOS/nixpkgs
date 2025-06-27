e72f4703d14a5bc19134630fdd890cf0417a78a0
Tom van Dijk <18gatenmaker6@gmail.com>
2025-06-28 00:43:47 +0200

    nushellPlugins.hcl: refactor

--- pkgs/shells/nushell/plugins/hcl.nix
+++ pkgs/shells/nushell/plugins/hcl.nix
@@ -7,30 +7,28 @@
   fetchFromGitHub,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nushell_plugin_hcl";
   version = "0.105.1";
 
   src = fetchFromGitHub {
-    repo = "nu_plugin_hcl";
     owner = "Yethal";
-    tag = version;
+    repo = "nu_plugin_hcl";
+    tag = finalAttrs.version;
     hash = "sha256-V1RKZ0Tqq0LTGbHS2lLMyf6M4AgAgWSzkDeFUighO4k=";
   };
 
   cargoHash = "sha256-UbqKfQxut+76yB9F1gT8FEapbX/kHvaShltHpWUdhgc=";
 
   nativeBuildInputs = [ pkg-config ] ++ lib.optionals stdenv.cc.isClang [ rustPlatform.bindgenHook ];
-  cargoBuildFlags = [ "--package nu_plugin_hcl" ];
 
   passthru.updateScript = nix-update-script { };
 
-  meta = with lib; {
+  meta = {
     description = "Nushell plugin for parsing Hashicorp Configuration Language files";
     mainProgram = "nu_plugin_hcl";
     homepage = "https://github.com/Yethal/nu_plugin_hcl";
-    license = licenses.mit;
-    maintainers = with maintainers; [ yethal ];
-    platforms = with platforms; all;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ yethal ];
   };
-}
+})
