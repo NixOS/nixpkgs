66df66ab2d8733a58bf6147af846809a0bb7bbbe
Redlonghead <git@beardit.net>
2025-06-28 12:57:29 -0500

    maintainers: add redlonghead

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21094,6 +21094,12 @@
     githubId = 77415970;
     name = "Redhawk";
   };
+  redlonghead = {
+    email = "git@beardit.net";
+    github = "Redlonghead";
+    githubId = 52263558;
+    name = "Connor Beard";
+  };
   redvers = {
     email = "red@infect.me";
     github = "redvers";
