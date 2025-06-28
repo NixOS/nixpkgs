467b39837398e27d10a1a7ce5609b16187e1cf53
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-28 00:55:59 +0000

    wiremix: 0.4.0 -> 0.5.0

--- pkgs/by-name/wi/wiremix/package.nix
+++ pkgs/by-name/wi/wiremix/package.nix
@@ -8,15 +8,15 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "wiremix";
-  version = "0.4.0";
+  version = "0.5.0";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-LtwKG3phUuNgwXlAJMhZkOenYHGyXGRhNcr6+WKxVz0=";
+    hash = "sha256-8pXpbW6/XYiWUqulL8b+kGqhTa/X8+tkgL8EWguWs6c=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-Qc+VubikiYox1zqy2HO3InRI8aFT8AorrFZBQhNGFOQ=";
+  cargoHash = "sha256-czhPbXkhuwUdwwP2uizU1Ob8t/x0nrJr3rquRO8Zktw=";
 
   nativeBuildInputs = [
     pkg-config
