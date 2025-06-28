284b25a12c3fff7a680d57737b26c4b8d6180394
Diego <diego@strebel.com>
2025-06-28 21:46:58 +0200

    maintainers: add dstremur

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6849,6 +6849,11 @@
     github = "dstengele";
     githubId = 1706418;
   };
+  dstremur = {
+    name = "Diego Strebel";
+    github = "dstremur";
+    githubId = 76773187;
+  };
   dsuetin = {
     name = "Danil Suetin";
     email = "suetin085+nixpkgs@protonmail.com";
