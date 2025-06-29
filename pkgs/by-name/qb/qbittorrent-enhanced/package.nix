62b8eba48c870e70ce6974d094ab0321e6d18742
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-29 15:43:52 +0000

    qbittorrent-enhanced: 5.1.0.11 -> 5.1.1.10

--- pkgs/by-name/qb/qbittorrent-enhanced/package.nix
+++ pkgs/by-name/qb/qbittorrent-enhanced/package.nix
@@ -7,13 +7,13 @@
 
 (qbittorrent.override { inherit guiSupport; }).overrideAttrs (old: rec {
   pname = "qbittorrent-enhanced" + lib.optionalString (!guiSupport) "-nox";
-  version = "5.1.0.11";
+  version = "5.1.1.10";
 
   src = fetchFromGitHub {
     owner = "c0re100";
     repo = "qBittorrent-Enhanced-Edition";
     rev = "release-${version}";
-    hash = "sha256-LY79Y7sG6EHGOh1hRujAOMRK3MKSAblJ+FFuGlj13iQ=";
+    hash = "sha256-BUBx1GbrajmCPMYHBngc/4yES7o+jwNi36Wqwcu7BIc=";
   };
 
   meta = old.meta // {
