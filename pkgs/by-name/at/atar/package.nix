0b2468bb21b6aff8b079b4ef9e53623ef7011a50
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-06-29 17:22:40 +0200

    atar: init at 0.1.25

---
+++ pkgs/by-name/at/atar/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "atar";
+  version = "0.1.25";
+
+  src = fetchFromGitHub {
+    owner = "x71c9";
+    repo = "atar";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-aMOovfJ9Srex/dBHz7hRMYmDkCbpXPOFHjU+VM/qqi8=";
+  };
+
+  cargoHash = "sha256-6q+0YQeY38tVQdnJw3x80binLw4dLQNGNMX0PH/+KjA=";
+
+  nativeInstallCheckInputs = [ lib.versionCheckHook ];
+
+  meta = {
+    description = "Ephemeral Terraform runner that applies a configuration on start, displays output variables, and automatically destroys all resources on exit or failure";
+    homepage = "https://github.com/x71c9/atar";
+    changelog = "https://github.com/x71c9/atar/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ x71c9 ];
+    mainProgram = "atar";
+  };
+
+  passthru = {
+    versionCheckHook = true;
+    updateScript = lib.nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "v([0-9.]+)"
+      ];
+    };
+  };
+})
