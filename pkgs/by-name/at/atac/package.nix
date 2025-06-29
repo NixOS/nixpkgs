5a361fe8fa44a43ca5d415ec0a5b7f3b9e01ddba
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-29 20:38:52 +0000

    atac: 0.20.1 -> 0.20.2

--- pkgs/by-name/at/atac/package.nix
+++ pkgs/by-name/at/atac/package.nix
@@ -7,17 +7,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "atac";
-  version = "0.20.1";
+  version = "0.20.2";
 
   src = fetchFromGitHub {
     owner = "Julien-cpsn";
     repo = "ATAC";
     rev = "v${version}";
-    hash = "sha256-1Y32uz/GF981mRpVNRsix1xTz3kLihMdnzd4i/QGE7s=";
+    hash = "sha256-m+2D1Vrh6Qi7iDOCuio76p7d4eAkjuCSZXxJD9spPrw=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-s7Iu0ZxahQekG02oCvI0WH0OiqAez+l7wvJq1xCQINY=";
+  cargoHash = "sha256-W94oaBgws5FUF2Xax+O7faiQgfhcyTeGoCu+VJD0Wd0=";
 
   nativeBuildInputs = [ pkg-config ];
 
