d0ab40b45b26f79e3532d4808094e6785fe1d7c0
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-06-29 07:24:56 +0200

    nixos/installer: enable networkmanager by default
    This is the easiest pick for new users to get their footing on NixOS.
    
    NetworKManager allows managing a wide variety of different network
    interface types interactively and with ease. It replaces `useDHCP` since
    its default profiles for Ethernet and WLAN come with DHCP enabled by
    default.

--- nixos/modules/installer/tools/tools.nix
+++ nixos/modules/installer/tools/tools.nix
@@ -108,9 +108,9 @@ let
 
     $bootLoaderConfig
       # networking.hostName = "nixos"; # Define your hostname.
-      # Pick only one of the below networking options.
-      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-      # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+      # Configure network connections interactively with nmcli or nmtui.
+      networking.networkmanager.enable = true;
 
       # Set your time zone.
       # time.timeZone = "Europe/Amsterdam";
