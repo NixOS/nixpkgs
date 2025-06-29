b53071b01c74a2c888f524a88f9b1ff9740fade1
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-29 02:09:48 +0000

    nezha-agent: 1.12.3 -> 1.13.0

--- pkgs/by-name/ne/nezha-agent/package.nix
+++ pkgs/by-name/ne/nezha-agent/package.nix
@@ -8,13 +8,13 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "nezha-agent";
-  version = "1.12.3";
+  version = "1.13.0";
 
   src = fetchFromGitHub {
     owner = "nezhahq";
     repo = "agent";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-LhSuKbvTHO+bCCtxKENS7wbScDHE57Uf3nFrMDt2wOE=";
+    hash = "sha256-CFwHMO78v/A5hjNypFo9Fp0gdkrKgNvPRGz3RfMRRAU=";
   };
 
   vendorHash = "sha256-5g2AgkqaelCD10dxwcEmBLYy5nrd4IALiss0dTrTTno=";
