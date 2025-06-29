d616e96d03ba810c1253664cb3e968e517f39051
Moraxyc <i@qaq.li>
2025-06-30 01:07:52 +0800

    gotify-server: modernize
    - Use finalAttrs.
    - Add missing ldflags https://github.com/gotify/server/blob/v2.6.3/.github/workflows/build.yml#L33.

--- pkgs/by-name/go/gotify-server/package.nix
+++ pkgs/by-name/go/gotify-server/package.nix
@@ -8,19 +8,20 @@
   nix-update-script,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "gotify-server";
   version = "2.6.3";
 
   src = fetchFromGitHub {
     owner = "gotify";
     repo = "server";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-9vIReA29dWf3QwUYEW8JhzF9o74JZqG4zGobgI+gIWE=";
   };
 
   vendorHash = "sha256-rs6EfnJT6Jgif2TR5u5Tp5/Ozn+4uhSapksyKFnQiCo=";
 
+  # No test
   doCheck = false;
 
   buildInputs = [
@@ -39,7 +40,7 @@ buildGoModule rec {
   passthru = {
     # For nix-update to detect the location of this attribute from this
     # derivation.
-    inherit (ui) offlineCache;
+    inherit (finalAttrs.ui) offlineCache;
     updateScript = nix-update-script { };
     tests = {
       nixos = nixosTests.gotify-server;
@@ -50,16 +51,21 @@ buildGoModule rec {
   # produce binaries which panic when executed and are not interesting at all
   subPackages = [ "." ];
 
+  # Based on LD_FLAGS in upstream's .github/workflows/build.yml:
+  # https://github.com/gotify/server/blob/v2.6.3/.github/workflows/build.yml#L33
   ldflags = [
-    "-X main.Version=${version}"
+    "-s"
+    "-X main.Version=${finalAttrs.version}"
     "-X main.Mode=prod"
+    "-X main.Commit=refs/tags/v${finalAttrs.version}"
+    "-X main.BuildDate=unknown"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Simple server for sending and receiving messages in real-time per WebSocket";
     homepage = "https://gotify.net";
-    license = licenses.mit;
-    maintainers = with maintainers; [ doronbehar ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ doronbehar ];
     mainProgram = "server";
   };
-}
+})
