36662db6cc4044395d7830733ff28bebcf23bee4
Cameron Smith <cameron.ray.smith@gmail.com>
2025-06-30 00:35:49 -0400

    star: add aarch64 support and fix compilation issues
      - Add support for aarch64-linux and aarch64-darwin platforms
      - Disable AVX2 SIMD flags that are incompatible with ARM64
      - Use C++14 standard to avoid register keyword deprecation
      - Add SHM_NORESERVE=0 define for aarch64 platforms
      - Add OpenMP support for Darwin builds

--- pkgs/by-name/st/star/package.nix
+++ pkgs/by-name/st/star/package.nix
@@ -4,8 +4,10 @@
   fetchFromGitHub,
   xxd,
   zlib,
+  llvmPackages,
+  star,
+  testers,
 }:
-
 stdenv.mkDerivation rec {
   pname = "star";
   version = "2.7.11b";
@@ -21,7 +23,15 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ xxd ];
 
-  buildInputs = [ zlib ];
+  buildInputs = [ zlib ] ++ lib.optionals stdenv.isDarwin [ llvmPackages.openmp ];
+
+  makeFlags = [
+    "CXXFLAGS_SIMD="
+  ];
+
+  preBuild = ''
+    export CXXFLAGS="-std=c++14${lib.optionalString stdenv.isDarwin " -DSHM_NORESERVE=0"}"
+  '';
 
   buildFlags = [
     "STAR"
@@ -36,6 +46,12 @@ stdenv.mkDerivation rec {
     runHook postInstall
   '';
 
+  passthru.tests.version = testers.testVersion {
+    package = star;
+    command = "STAR --version || true";
+    version = "${version}";
+  };
+
   meta = with lib; {
     description = "Spliced Transcripts Alignment to a Reference";
     homepage = "https://github.com/alexdobin/STAR";
@@ -43,6 +59,8 @@ stdenv.mkDerivation rec {
     platforms = [
       "x86_64-linux"
       "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
     ];
     maintainers = [ maintainers.arcadio ];
   };
