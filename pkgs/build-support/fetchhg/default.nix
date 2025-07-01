923ebc3ced4fdc30384907b6d89397fd9e6c2b0d
Yueh-Shun Li <shamrocklee@posteo.net>
2025-07-01 17:47:25 +0800

    fetchhg: format with nixfmt-rfc-style

--- pkgs/build-support/fetchhg/default.nix
+++ pkgs/build-support/fetchhg/default.nix
@@ -9,43 +9,41 @@ lib.extendMkDerivation {
 
   extendDrvArgs =
     finalAttrs:
-{
-  name ? null,
-  url,
-  rev ? null,
-  sha256 ? null,
-  hash ? null,
-  fetchSubrepos ? false,
-  preferLocalBuild ? true,
-}:
-  # TODO: statically check if mercurial as the https support if the url starts with https.
-  {
-    name = "hg-archive" + (lib.optionalString (name != null) "-${name}");
-    builder = ./builder.sh;
-    nativeBuildInputs = [ mercurial ];
+    {
+      name ? null,
+      url,
+      rev ? null,
+      sha256 ? null,
+      hash ? null,
+      fetchSubrepos ? false,
+      preferLocalBuild ? true,
+    }:
+    # TODO: statically check if mercurial as the https support if the url starts with https.
+    {
+      name = "hg-archive" + (lib.optionalString (name != null) "-${name}");
+      builder = ./builder.sh;
+      nativeBuildInputs = [ mercurial ];
 
-    impureEnvVars = lib.fetchers.proxyImpureEnvVars;
+      impureEnvVars = lib.fetchers.proxyImpureEnvVars;
 
-    subrepoClause = lib.optionalString fetchSubrepos "S";
+      subrepoClause = lib.optionalString fetchSubrepos "S";
 
-    outputHashAlgo = if finalAttrs.hash != null && finalAttrs.hash != "" then null else "sha256";
-    outputHashMode = "recursive";
-    outputHash =
-      lib.throwIf
-        (finalAttrs.hash != null && sha256 != null)
-        "Only one of sha256 or hash can be set"
-        (
-          if finalAttrs.hash != null then
-            finalAttrs.hash
-          else if sha256 != null then
-            sha256
-          else
-            ""
-        );
+      outputHashAlgo = if finalAttrs.hash != null && finalAttrs.hash != "" then null else "sha256";
+      outputHashMode = "recursive";
+      outputHash =
+        lib.throwIf (finalAttrs.hash != null && sha256 != null) "Only one of sha256 or hash can be set"
+          (
+            if finalAttrs.hash != null then
+              finalAttrs.hash
+            else if sha256 != null then
+              sha256
+            else
+              ""
+          );
 
-    inherit url rev hash;
-    inherit preferLocalBuild;
-  };
+      inherit url rev hash;
+      inherit preferLocalBuild;
+    };
 
   # No ellipsis
   inheritFunctionArgs = false;
