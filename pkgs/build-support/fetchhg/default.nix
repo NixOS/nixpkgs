fc9e4be58ff61d0adf398bbb897a66d1d26ed736
Yueh-Shun Li <shamrocklee@posteo.net>
2025-07-01 11:53:18 +0800

    fetchhg: allow hash to be empty string and move assertion down under outputHash
    (cherry picked from commit c6b8e4470cae3b2e14049f5a4a59a82444fdbb5b)

--- pkgs/build-support/fetchhg/default.nix
+++ pkgs/build-support/fetchhg/default.nix
@@ -13,9 +13,6 @@
   preferLocalBuild ? true,
 }:
 
-if hash != null && sha256 != null then
-  throw "Only one of sha256 or hash can be set"
-else
   # TODO: statically check if mercurial as the https support if the url starts with https.
   stdenvNoCC.mkDerivation {
     name = "hg-archive" + (lib.optionalString (name != null) "-${name}");
@@ -26,15 +23,20 @@ else
 
     subrepoClause = lib.optionalString fetchSubrepos "S";
 
-    outputHashAlgo = if hash != null then null else "sha256";
+    outputHashAlgo = if hash != null && hash != "" then null else "sha256";
     outputHashMode = "recursive";
     outputHash =
-      if hash != null then
-        hash
-      else if sha256 != null then
-        sha256
-      else
-        lib.fakeSha256;
+      lib.throwIf
+        (hash != null && sha256 != null)
+        "Only one of sha256 or hash can be set"
+        (
+          if hash != null then
+            hash
+          else if sha256 != null then
+            sha256
+          else
+            ""
+        );
 
     inherit url rev;
     inherit preferLocalBuild;
