de600b99ebc060cb6a622779071e59e67f22e0a5
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-01 06:47:03 +0000

    rgbds: 0.9.2 -> 0.9.3

--- pkgs/by-name/rg/rgbds/package.nix
+++ pkgs/by-name/rg/rgbds/package.nix
@@ -10,12 +10,12 @@
 
 stdenv.mkDerivation rec {
   pname = "rgbds";
-  version = "0.9.2";
+  version = "0.9.3";
   src = fetchFromGitHub {
     owner = "gbdev";
     repo = "rgbds";
     rev = "v${version}";
-    hash = "sha256-Ho9aSpENukNutb5VscopY2p6RGXbRgvtIcRgxTtZews=";
+    hash = "sha256-G83AoURZWrKto64Aga2vpg4/vY9pwLS+SDkFX0arKQw=";
   };
   nativeBuildInputs = [
     bison
