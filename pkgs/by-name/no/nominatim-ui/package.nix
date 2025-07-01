cb468f8dc7e57bbcc8183aef22140867d4e55184
Ivan Mincik <ivan.mincik@gmail.com>
2025-07-01 12:44:56 +0200

    nominatim-ui: rewrite packaging to add missing files
    * Rewrite packaging to add missing files which needs to be
      built by yarn
    
    * Drop usage of finalAttrs which is failing when used with
      mkYarnPackage

--- pkgs/by-name/no/nominatim-ui/package.nix
+++ pkgs/by-name/no/nominatim-ui/package.nix
@@ -1,27 +1,57 @@
 {
   lib,
-  stdenv,
   fetchFromGitHub,
+  fetchYarnDeps,
+  mkYarnPackage,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
+# Notes for the upgrade:
+# * Download the tarball of the new version to use.
+# * Replace new `package.json` here.
+# * Update `version`+`hash` and rebuild.
+
+mkYarnPackage rec {
   pname = "nominatim-ui";
   version = "3.7.1";
 
   src = fetchFromGitHub {
     owner = "osm-search";
     repo = "nominatim-ui";
-    tag = "v${finalAttrs.version}";
+    tag = "v${version}";
     hash = "sha256-TliTWDKdIp7Z0uYw5P65i06NQAUNwNymUsSYrihVZFE=";
   };
+
+  offlineCache = fetchYarnDeps {
+    yarnLock = "${src}/yarn.lock";
+    hash = "sha256-IqwsXEd9RSJhkA4BONTJT4xYMTyG9+zddIpD47v6AFc=";
+  };
+
+  packageJSON = ./package.json;
+
+  buildPhase = ''
+    runHook preBuild
+
+    yarn --offline build
+
+    runHook postBuild
+  '';
+
+  # Minimal configuration
+  # See https://nominatim.org/release-docs/latest/admin/Setup-Nominatim-UI/#installing-nominatim-ui
+  preInstall = ''
+    echo "Nominatim_Config.Nominatim_API_Endpoint='https://localhost/';" \
+    > deps/nominatim-ui/dist/theme/config.theme.js
+  '';
 
   installPhase = ''
     runHook preInstall
 
-    cp --archive dist $out
+    cp --archive deps/nominatim-ui/dist $out
 
     runHook postInstall
   '';
+
+  doDist = false;
 
   meta = {
     description = "Debugging user interface for Nominatim geocoder";
@@ -31,6 +61,6 @@ stdenv.mkDerivation (finalAttrs: {
       geospatial
       ngi
     ];
-    changelog = "https://github.com/osm-search/nominatim-ui/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/osm-search/nominatim-ui/releases/tag/v${version}";
   };
-})
+}
