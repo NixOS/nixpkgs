NixOS/nixpkgs

commit c1cc6ff0c0680ae6d8228953ebba93c131e04775

patch-shebangs.sh: fix for macos sandbox
fixes https://github.com/NixOS/nixpkgs/issues/343576

pkgs/build-support/setup-hooks/patch-shebangs.sh
@@ -68,6 +68,18 @@ patchShebangs() {
         return 0
     fi
 
+    # like sponge from moreutils but in pure bash
+    _sponge() {
+        local content
+        local target
+        content=""
+        target="$1"
+        while IFS= read -r line || [[ -n "$line" ]]; do
+            content+="$line"$'\n'
+        done
+        printf '%s' "$content" > "$target"
+    }
+
     local f
     while IFS= read -r -d $'\0' f; do
         isScript "$f" || continue
@@ -126,11 +138,14 @@ patchShebangs() {
 
                 # Preserve times, see: https://github.com/NixOS/nixpkgs/pull/33281
                 timestamp=$(stat --printf "%y" "$f")
-                sed -i -e "1 s|.*|#\!$escapedInterpreterLine|" "$f"
+                sed -e "1 s|.*|#\!$escapedInterpreterLine|" "$f" | _sponge "$f"
+
                 touch --date "$timestamp" "$f"
             fi
         fi
     done < <(find "$@" -type f -perm -0100 -print0)
+
+    unset -f _sponge
 }
 
 patchShebangsAuto () {

commit 2029515f40f7289793da95972bd4b81ff25053d2

patchShebangs: fix crash on read-only files
Fixes a regression introduced by c1cc6ff0c0680ae6d8228953ebba93c131e04775 where patchShebangs would crash if a file is read-only

pkgs/build-support/setup-hooks/patch-shebangs.sh
@@ -72,12 +72,25 @@ patchShebangs() {
     _sponge() {
         local content
         local target
+        local restoreReadOnly
         content=""
         target="$1"
+
+        # Make file writable if it is read-only
+        if [[ ! -w "$target" ]]; then
+            chmod +w "$target"
+            restoreReadOnly=true
+        fi
+
         while IFS= read -r line || [[ -n "$line" ]]; do
             content+="$line"$'\n'
         done
         printf '%s' "$content" > "$target"
+
+        # Restore read-only if it was read-only before
+        if [[ -n "${restoreReadOnly:-}" ]]; then
+            chmod -w "$target"
+        fi
     }
 
     local f