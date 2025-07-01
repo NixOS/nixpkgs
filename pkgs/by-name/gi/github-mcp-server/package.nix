86609619db57ae6a1dd686922935700d155af7e0
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-01 20:45:41 +0000

    github-mcp-server: 0.5.0 -> 0.6.0

--- pkgs/by-name/gi/github-mcp-server/package.nix
+++ pkgs/by-name/gi/github-mcp-server/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "github-mcp-server";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "github";
     repo = "github-mcp-server";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dbzO8yTAIfdAwcZEdoJqp+loPQea8iRSsAHdk2DfZ2A=";
+    hash = "sha256-jW/X8vwV47J20aa3E+WqjRL5n9H+Pb2EQwIVIg1pfug=";
   };
 
-  vendorHash = "sha256-gVR7Md3xYrPpeMhHRTKCQKCJvRRIl85uXo+QwlVaPzk=";
+  vendorHash = "sha256-GYfK5QQH0DhoJqc4ynZBWuhhrG5t6KoGpUkZPSfWfEQ=";
 
   ldflags = [
     "-s"
