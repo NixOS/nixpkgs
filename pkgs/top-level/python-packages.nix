9a6f247002c4ad1261de0df5a640ce86761b2221
Johan Herland <johan@herland.net>
2025-07-02 12:47:02 +0000

    python3Packages.holistic-trace-analysis: init at 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6505,6 +6505,8 @@ self: super: with self; {
 
   holidays = callPackage ../development/python-modules/holidays { };
 
+  holistic-trace-analysis = callPackage ../development/python-modules/holistic-trace-analysis { };
+
   hologram = callPackage ../development/python-modules/hologram { };
 
   holoviews = callPackage ../development/python-modules/holoviews { };
