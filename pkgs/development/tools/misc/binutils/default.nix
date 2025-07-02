NixOS/nixpkgs

commit 437ad124ac972ba5ae75948c9f7a429d3138cf2d

binutils-unwrapped-all-targets: per-target gas
pwntools wants to be able to disassemble arbitrary programs, so it
wants to be able to find gas for an arbitrary architecture in the same
prefix.

pkgs/development/tools/misc/binutils/default.nix
@@ -37,6 +37,36 @@ let
   #INFO: The targetPrefix prepended to binary names to allow multiple binuntils
   # on the PATH to both be usable.
   targetPrefix = lib.optionalString (targetPlatform != hostPlatform) "${targetPlatform.config}-";
+
+  # gas isn't multi-target, even with --enable-targets=all, so we do
+  # separate builds of just gas for each target.
+  #
+  # There's no way to do this exhaustively, so feel free to add
+  # additional targets here as required.
+  allGasTargets =
+    allGasTargets'
+    ++ lib.optional (!lib.elem targetPlatform.config allGasTargets') targetPlatform.config;
+  allGasTargets' = [
+    "aarch64-unknown-linux-gnu"
+    "alpha-unknown-linux-gnu"
+    "arm-unknown-linux-gnu"
+    "avr-unknown-linux-gnu"
+    "cris-unknown-linux-gnu"
+    "hppa-unknown-linux-gnu"
+    "i686-unknown-linux-gnu"
+    "ia64-unknown-linux-gnu"
+    "m68k-unknown-linux-gnu"
+    "mips-unknown-linux-gnu"
+    "mips64-unknown-linux-gnu"
+    "msp430-unknown-linux-gnu"
+    "powerpc-unknown-linux-gnu"
+    "powerpc64-unknown-linux-gnu"
+    "s390-unknown-linux-gnu"
+    "sparc-unknown-linux-gnu"
+    "vax-unknown-linux-gnu"
+    "x86_64-unknown-linux-gnu"
+    "xscale-unknown-linux-gnu"
+  ];
 in
 
 stdenv.mkDerivation (finalAttrs: {
@@ -209,7 +239,11 @@ stdenv.mkDerivation (finalAttrs: {
       # path to force users to declare their use of these libraries.
       "--with-lib-path=:"
     ]
-    ++ lib.optionals withAllTargets [ "--enable-targets=all" ]
+    ++ lib.optionals withAllTargets [
+      "--enable-targets=all"
+      # gas will be built separately for each target.
+      "--disable-gas"
+    ]
     ++ lib.optionals enableGold [
       "--enable-gold${lib.optionalString enableGoldDefault "=default"}"
       "--enable-plugins"
@@ -237,12 +271,29 @@ stdenv.mkDerivation (finalAttrs: {
       ]
     );
 
+  postConfigure = lib.optionalString withAllTargets ''
+    for target in ${lib.escapeShellArgs allGasTargets}; do
+      mkdir "$NIX_BUILD_TOP/build-$target"
+      env -C "$NIX_BUILD_TOP/build-$target" \
+        "$configureScript" $configureFlags "''${configureFlagsArray[@]}" \
+        --enable-gas --program-prefix "$target-"  --target "$target"
+    done
+  '';
+
   makeFlags = [
     # As we regenerated configure build system tries hard to use
     # texinfo to regenerate manuals. Let's avoid the dependency
     # on texinfo in bootstrap path and keep manuals unmodified.
     "MAKEINFO=true"
   ];
+
+  postBuild = lib.optionalString withAllTargets ''
+    for target in ${lib.escapeShellArgs allGasTargets}; do
+      make -C "$NIX_BUILD_TOP/build-$target" -j"$NIX_BUILD_CORES" \
+        $makeFlags "''${makeFlagsArray[@]}" $buildFlags "''${buildFlagsArray[@]}" \
+        TARGET-gas=as-new all-gas
+    done
+  '';
 
   # Fails
   doCheck = false;
@@ -266,10 +317,19 @@ stdenv.mkDerivation (finalAttrs: {
   #   $out/$host/$target/include/* to $dev/include/*
   # TODO(trofi): fix installation paths upstream so we could remove this
   # code and have "lib" output unconditionally.
-  postInstall = lib.optionalString (hostPlatform.config != targetPlatform.config) ''
-    ln -s $out/${hostPlatform.config}/${targetPlatform.config}/lib/*     $out/lib/
-    ln -s $out/${hostPlatform.config}/${targetPlatform.config}/include/* $dev/include/
-  '';
+  postInstall =
+    lib.optionalString (hostPlatform.config != targetPlatform.config) ''
+      ln -s $out/${hostPlatform.config}/${targetPlatform.config}/lib/*     $out/lib/
+      ln -s $out/${hostPlatform.config}/${targetPlatform.config}/include/* $dev/include/
+    ''
+    + lib.optionalString withAllTargets ''
+      for target in ${lib.escapeShellArgs allGasTargets}; do
+        make -C "$NIX_BUILD_TOP/build-$target/gas" -j"$NIX_BUILD_CORES" \
+          $makeFlags "''${makeFlagsArray[@]}" $installFlags "''${installFlagsArray[@]}" \
+          install-exec-bindir
+      done
+      ln -s $out/bin/${stdenv.targetPlatform.config}-as $out/bin/as
+    '';
 
   passthru = {
     inherit targetPrefix;

commit b57b6a4782ab8664f167b067e3ad2e6937f2ca26

binutils-unwrapped-all-targets: fix build on Darwin
gas isn't built for Darwin, so previously this failed when trying to
install it:

	make: Entering directory '/private/tmp/nix-build-binutils-2.44.drv-0/build'
	make: *** /private/tmp/nix-build-binutils-2.44.drv-0/build-arm64-apple-darwin/gas: No such file or directory.  Stop.
	make: Leaving directory '/private/tmp/nix-build-binutils-2.44.drv-0/build'

Fixes: 437ad124ac97 ("binutils-unwrapped-all-targets: per-target gas")

pkgs/development/tools/misc/binutils/default.nix
@@ -38,6 +38,9 @@ let
   # on the PATH to both be usable.
   targetPrefix = lib.optionalString (targetPlatform != hostPlatform) "${targetPlatform.config}-";
 
+  # gas is disabled for some targets via noconfigdirs in configure.
+  targetHasGas = !stdenv.targetPlatform.isDarwin;
+
   # gas isn't multi-target, even with --enable-targets=all, so we do
   # separate builds of just gas for each target.
   #
@@ -45,7 +48,9 @@ let
   # additional targets here as required.
   allGasTargets =
     allGasTargets'
-    ++ lib.optional (!lib.elem targetPlatform.config allGasTargets') targetPlatform.config;
+    ++ lib.optional (
+      targetHasGas && !lib.elem targetPlatform.config allGasTargets'
+    ) targetPlatform.config;
   allGasTargets' = [
     "aarch64-unknown-linux-gnu"
     "alpha-unknown-linux-gnu"
@@ -328,6 +333,8 @@ stdenv.mkDerivation (finalAttrs: {
           $makeFlags "''${makeFlagsArray[@]}" $installFlags "''${installFlagsArray[@]}" \
           install-exec-bindir
       done
+    ''
+    + lib.optionalString (withAllTargets && targetHasGas) ''
       ln -s $out/bin/${stdenv.targetPlatform.config}-as $out/bin/as
     '';
 