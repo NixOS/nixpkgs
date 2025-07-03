71ff971e71cc34b03883f9e2da6e6cfcc8172018
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-03 01:04:27 +0000

    release-plz: 0.3.136 -> 0.3.137

--- pkgs/by-name/re/release-plz/package.nix
+++ pkgs/by-name/re/release-plz/package.nix
@@ -11,17 +11,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "release-plz";
-  version = "0.3.136";
+  version = "0.3.137";
 
   src = fetchFromGitHub {
     owner = "MarcoIeni";
     repo = "release-plz";
     rev = "release-plz-v${version}";
-    hash = "sha256-C/8/lukqnuxCeHZR0kSuJSJzWl71kr3hdTTmS5lMRLA=";
+    hash = "sha256-7sCwnUhCLfA/MACseZUT6IWR5+JjxKUyBfLSGwno/qQ=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-MsSGFNMmIaKVYXqJks4NzLNnrip8cz7K9pETtxHyLuI=";
+  cargoHash = "sha256-IoFJPRW4SXAWxfBKNBrgtxBAYfbRwxuN9Aig3P9QkOk=";
 
   nativeBuildInputs = [
     installShellFiles
