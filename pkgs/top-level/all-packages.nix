a3ed1c0e1e1f04a9e4ebd6b9ae84b30af5a45a0f
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-07-03 22:05:18 +0200

    flow: 0.238.3 → 0.274.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7232,9 +7232,7 @@ with pkgs;
     haskellPackages.callPackage ../tools/misc/fffuu { }
   );
 
-  flow = callPackage ../development/tools/analysis/flow {
-    ocamlPackages = ocaml-ng.ocamlPackages_4_14;
-  };
+  flow = callPackage ../development/tools/analysis/flow { };
 
   framac = callPackage ../by-name/fr/framac/package.nix {
     ocamlPackages = ocaml-ng.ocamlPackages_5_2;
