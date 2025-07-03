1916a8a856c337ac71db88de7ffa8ac1163a3fab
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-07-04 01:25:10 +0300

    teleport_17: 17.5.3 -> 17.5.4
    Changelog: https://github.com/gravitational/teleport/releases/tag/v17.5.4
    Diff: https://github.com/gravitational/teleport/compare/v17.5.3...v17.5.4

--- pkgs/by-name/te/teleport/package.nix
+++ pkgs/by-name/te/teleport/package.nix
@@ -21,8 +21,8 @@
 
   withRdpClient ? true,
 
-  version ? "17.5.3",
-  hash ? "sha256-VyuzSk388Dumm65+Gadu/SddVN7uUS3du5RGPm4xI6g=",
+  version ? "17.5.4",
+  hash ? "sha256-ojRIyPTrSG3/xuqdaUNrN4s5HP3E8pvzjG8h+qFEYrc=",
   vendorHash ? "sha256-IHXwCp1MdcEKJhIs9DNf77Vd93Ai2as7ROlh6AJT9+Q=",
   extPatches ? [ ],
   cargoHash ? "sha256-qz8gkooQTuBlPWC4lHtvBQpKkd+nEZ0Hl7AVg9JkPqs=",
