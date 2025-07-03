bc20d75ac9ee87734b8c7a835cca0575cd36ba4f
Morgan Jones <me@numin.it>
2025-07-03 01:30:52 -0700

    util-linuxMinimal: use fetchurlBoot
    The dependency chain goes something like:
    
    util-linuxMinimal -> fetchurl -> nss-cacert -> \
    python3.13-buildcatrust -> python3 which causes infinite recursion if
    a hash is left out. So override fetchurl to fetchurlBoot just for the
    minimal util-linux.
    
    (cherry picked from commit c897b562e13be7ac666d2b5fbcbe7dc8a57a7ad9)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11587,6 +11587,7 @@ with pkgs;
   usbrelayd = callPackage ../os-specific/linux/usbrelay/daemon.nix { };
 
   util-linuxMinimal = util-linux.override {
+    fetchurl = stdenv.fetchurlBoot;
     cryptsetupSupport = false;
     nlsSupport = false;
     ncursesSupport = false;
