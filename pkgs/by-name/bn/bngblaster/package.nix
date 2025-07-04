96148e86b84a0ff5e31cd11b86676ef0253431e0
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-04 16:22:08 +0000

    bngblaster: 0.9.15 -> 0.9.21

--- pkgs/by-name/bn/bngblaster/package.nix
+++ pkgs/by-name/bn/bngblaster/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bngblaster";
-  version = "0.9.15";
+  version = "0.9.21";
 
   src = fetchFromGitHub {
     owner = "rtbrick";
     repo = "bngblaster";
     rev = finalAttrs.version;
-    hash = "sha256-HqwyqUkLYoXTjx01pB8sL3hBXwLqe441M+LTqBhaZ58=";
+    hash = "sha256-0qku4myr38uaYLB5hjZGQa8OBnZFZtkpYhPz2aFIqpY=";
   };
 
   nativeBuildInputs = [ cmake ];
