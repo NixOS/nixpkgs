a6c92d2a8d1a61f960ee41dcb3bb8392c0785617
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-04 16:53:30 +0200

    python312Packages.mypy-boto3-location: 1.38.0 -> 1.39.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -806,8 +806,8 @@ rec {
       "sha256-nKniYx8pYpGOA4Zz560MDdcLWkV2lULW99ur+gSg0YY=";
 
   mypy-boto3-location =
-    buildMypyBoto3Package "location" "1.38.0"
-      "sha256-uypKW3Cqj98SLeWmSwCXVKhKpXWXEvwdUexqqFgXeEc=";
+    buildMypyBoto3Package "location" "1.39.0"
+      "sha256-2/+yZjd+2VBsPDdaHx0yO8Cjhacv/7HxO9XgG+Vut+Q=";
 
   mypy-boto3-logs =
     buildMypyBoto3Package "logs" "1.38.39"
