03aa17c1f8fdbc8c61665c34e89f0cf9cc79daf2
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-04 16:53:32 +0200

    python312Packages.mypy-boto3-lookoutequipment: 1.38.0 -> 1.39.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -814,8 +814,8 @@ rec {
       "sha256-zDwUlB76vZ+bdwg9Db5rIigCQCi9JI3Dt71VC2miOCc=";
 
   mypy-boto3-lookoutequipment =
-    buildMypyBoto3Package "lookoutequipment" "1.38.0"
-      "sha256-coTY1FRB1kmAFLvHXUCtD04LlISTaGeIFcCe0Huxg1U=";
+    buildMypyBoto3Package "lookoutequipment" "1.39.0"
+      "sha256-YfvZuDet0Se8VHWiHQYkiQIDuCOyntRYkQ/lK80xl/Q=";
 
   mypy-boto3-lookoutmetrics =
     buildMypyBoto3Package "lookoutmetrics" "1.38.0"
