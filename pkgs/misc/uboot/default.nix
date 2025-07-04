230538bfa48609dc36b403bf8f0d50304cc4fa19
Ophestra <contrib@gensokyo.uk>
2025-07-04 23:22:47 +0900

    ubootArmsomSige7: init

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -241,6 +241,18 @@ in
     filesToInstall = [ "u-boot-sunxi-with-spl.bin" ];
   };
 
+  ubootArmsomSige7 = buildUBoot {
+    defconfig = "sige7-rk3588_defconfig";
+    extraMeta.platforms = [ "aarch64-linux" ];
+    BL31 = "${armTrustedFirmwareRK3588}/bl31.elf";
+    ROCKCHIP_TPL = rkbin.TPL_RK3588;
+    filesToInstall = [
+      "u-boot.itb"
+      "idbloader.img"
+      "u-boot-rockchip.bin"
+    ];
+  };
+
   # http://git.denx.de/?p=u-boot.git;a=blob;f=board/solidrun/clearfog/README;hb=refs/heads/master
   ubootClearfog = buildUBoot {
     defconfig = "clearfog_defconfig";
