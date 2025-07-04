7eab456ad0aa409e8b8cfbc8771fb5a2ee9d2510
emaryn <emaryn@users.noreply.github.com>
2025-07-04 09:57:02 +0800

    python313Packages.dynaconf: init at 3.2.11

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4396,6 +4396,8 @@ self: super: with self; {
 
   dyn = callPackage ../development/python-modules/dyn { };
 
+  dynaconf = callPackage ../development/python-modules/dynaconf { };
+
   dynalite-devices = callPackage ../development/python-modules/dynalite-devices { };
 
   dynalite-panel = callPackage ../development/python-modules/dynalite-panel { };
