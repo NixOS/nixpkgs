4c4fc425a46bde00ca301ccceea1d6d825272b54
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-04 16:55:53 +0200

    python312Packages.mypy-boto3-waf-regional: 1.38.0 -> 1.39.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1394,8 +1394,8 @@ rec {
       "sha256-QTIy6jkvtyRoScjRZekScq0rzH2f7mALPPuaN0LOy60=";
 
   mypy-boto3-waf-regional =
-    buildMypyBoto3Package "waf-regional" "1.38.0"
-      "sha256-ndTROc1T2APmQHL7EsSeU3XxWPYzp1e32tzqe4UDs/8=";
+    buildMypyBoto3Package "waf-regional" "1.39.0"
+      "sha256-JlK6LrTtu9bpGHID7JrQlLVIqsEtPAMdfD8/0AC5yNA=";
 
   mypy-boto3-wafv2 =
     buildMypyBoto3Package "wafv2" "1.38.38"
