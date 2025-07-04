02ec09e48eb2ff5bc520de8c33d920fa2e4809a2
Fabian Affolter <fabian@affolter-engineering.ch>
2025-07-04 08:08:34 +0200

    python313Packages.types-awscrt: 0.27.2 -> 0.27.4
    Changelog: https://github.com/youtype/types-awscrt/releases/tag/0.27.4

--- pkgs/development/python-modules/types-awscrt/default.nix
+++ pkgs/development/python-modules/types-awscrt/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "types-awscrt";
-  version = "0.27.2";
+  version = "0.27.4";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "types_awscrt";
     inherit version;
-    hash = "sha256-rNBPVxGesVYmqwupFX/CRnJCHeVue9e59haB/t7kTpE=";
+    hash = "sha256-wBm6kaCX6KMdaUj2F27eExKWP0HNys+CSCrId8u885A=";
   };
 
   build-system = [ setuptools ];
