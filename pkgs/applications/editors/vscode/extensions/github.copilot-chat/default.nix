2a725beb5290870acf7304acf8af1037f6171a9d
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-04 10:32:15 +0000

    vscode-extensions.github.copilot-chat: 0.28.2 -> 0.28.5

--- pkgs/applications/editors/vscode/extensions/github.copilot-chat/default.nix
+++ pkgs/applications/editors/vscode/extensions/github.copilot-chat/default.nix
@@ -7,8 +7,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     publisher = "github";
     name = "copilot-chat";
-    version = "0.28.2";
-    hash = "sha256-o6h9AOeBMRqVkhSgHUE2/vmsmJCXciY21mIQD7SUHOU=";
+    version = "0.28.5";
+    hash = "sha256-sxbikIUxaIVT0ySaicUIZIiHuy0kzz5xyYcKYds6+XE=";
   };
 
   meta = {
