e6c46b2fe4903df5b99000c47c8b3e7f8bd17f85
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-04 16:50:40 +0200

    python312Packages.mypy-boto3-appflow: 1.38.0 -> 1.39.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -102,8 +102,8 @@ rec {
       "sha256-JQmcfLFNiO5mIOVuxNufLaRllfsIg1c13zr/p/udTNs=";
 
   mypy-boto3-appflow =
-    buildMypyBoto3Package "appflow" "1.38.0"
-      "sha256-tlXyfbwuw6OIWaioqEPT3QUl84NgkMLXFh8RHUBokyE=";
+    buildMypyBoto3Package "appflow" "1.39.0"
+      "sha256-etvr1Nu5aInbxZ7VINX0GAVSxymk11yWDPWiLcB+FXU=";
 
   mypy-boto3-appintegrations =
     buildMypyBoto3Package "appintegrations" "1.38.0"
