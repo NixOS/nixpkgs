379ebf010bd9ef59ce25c1e040ef701a135b0a1f
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-04 16:50:51 +0200

    python312Packages.mypy-boto3-auditmanager: 1.38.22 -> 1.39.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -146,8 +146,8 @@ rec {
       "sha256-0CtQqviDxHpTrANwKF/2E+fjbfCikjwffO/n1z8kqDw=";
 
   mypy-boto3-auditmanager =
-    buildMypyBoto3Package "auditmanager" "1.38.22"
-      "sha256-6OgMopfNAD7bpLnDFXlcejFqKYTOvrJOd/X39SHYa1A=";
+    buildMypyBoto3Package "auditmanager" "1.39.0"
+      "sha256-c6FiB1R4B0WN637BWzvI5OUUMNUDbx0wJoYxX0t8iCs=";
 
   mypy-boto3-autoscaling =
     buildMypyBoto3Package "autoscaling" "1.38.39"
