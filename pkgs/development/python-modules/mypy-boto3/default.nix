b0462e643fc1dc600d22db854c81efd3019a266b
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-04 16:55:33 +0200

    python312Packages.mypy-boto3-stepfunctions: 1.38.0 -> 1.39.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1322,8 +1322,8 @@ rec {
       "sha256-Cc3eTbro2qQ1x+r6Us0125nLZGIm28GXuvjLvx7OdVw=";
 
   mypy-boto3-stepfunctions =
-    buildMypyBoto3Package "stepfunctions" "1.38.0"
-      "sha256-sBAiZcSfoFN7210hRnfaiXN7eE9X7AI0DXIhBSgIipk=";
+    buildMypyBoto3Package "stepfunctions" "1.39.0"
+      "sha256-aZbDf61cGeoeyNArz8Uweft/wqVfZpQySklFXIpf7R4=";
 
   mypy-boto3-storagegateway =
     buildMypyBoto3Package "storagegateway" "1.38.44"
