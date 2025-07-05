9d2941a6fd7e5812ab5d63ce7430d96a0f5f7702
j-k <dev@j-k.io>
2025-07-05 11:32:59 +0100

    Revert "[Backport release-25.05] deploy-rs: 0-unstable-2024-06-12 -> 0-unstable-2025-06-05"

--- pkgs/by-name/de/deploy-rs/package.nix
+++ pkgs/by-name/de/deploy-rs/package.nix
@@ -6,26 +6,23 @@
 
 rustPlatform.buildRustPackage {
   pname = "deploy-rs";
-  version = "0-unstable-2025-06-05";
+  version = "0-unstable-2024-06-12";
 
   src = fetchFromGitHub {
     owner = "serokell";
     repo = "deploy-rs";
-    rev = "6bc76b872374845ba9d645a2f012b764fecd765f";
-    hash = "sha256-hXh76y/wDl15almBcqvjryB50B0BaiXJKk20f314RoE=";
+    rev = "3867348fa92bc892eba5d9ddb2d7a97b9e127a8a";
+    hash = "sha256-FaGrf7qwZ99ehPJCAwgvNY5sLCqQ3GDiE/6uLhxxwSY=";
   };
 
-  cargoHash = "sha256-9O93YTEz+e2oxenE0gwxsbz55clbKo9+37yVOqz7ErE=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-e+Exc0lEamAieZ7QHJBYvmnmM/9YHdLRD3La4U5FRMo=";
 
   meta = {
     description = "Multi-profile Nix-flake deploy tool";
     homepage = "https://github.com/serokell/deploy-rs";
     license = lib.licenses.mpl20;
-    maintainers = with lib.maintainers; [
-      teutat3s
-      jk
-    ];
-    teams = [ lib.teams.serokell ];
+    maintainers = with lib.maintainers; [ teutat3s ];
     mainProgram = "deploy";
   };
 }
