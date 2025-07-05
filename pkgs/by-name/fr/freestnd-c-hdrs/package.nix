d9763f4dcc921389707c496ad27c8b41154da5c2
sanana <umutinanerdogan@pm.me>
2025-07-05 16:56:58 +0300

    freestnd-*-hdrs: init at unstable-2025-04-08

---
+++ pkgs/by-name/fr/freestnd-c-hdrs/package.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  fetchFromGitea,
+  stdenvNoCC,
+}:
+stdenvNoCC.mkDerivation {
+  pname = "freestnd-c-hdrs";
+  version = "unstable-2025-04-08";
+
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "osdev";
+    repo = "freestnd-c-hdrs";
+    rev = "4039f438fb1dc1064d8e98f70e1cf122f91b763b";
+    sha256 = "sha256-JMb1DS9+IDqGXdivMdy326kh+5iBniGc2RHihCfhLeA=";
+  };
+
+  makeFlags = [
+    "DESTDIR=/"
+    "PREFIX=$(out)"
+  ];
+
+  meta = {
+    description = "Headers from GCC that can be used in a freestanding environment";
+    platforms = lib.platforms.all;
+    license = with lib.licenses; [
+      gpl3Plus # with GCC Runtime Library Exception
+    ];
+    maintainers = with lib.maintainers; [ lzcunt ];
+  };
+}
