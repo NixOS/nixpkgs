d18b612ed043fbd09f1c0f15cfb3acd16877cbec
Markus Zoppelt <markus@zoppelt.net>
2025-07-05 15:38:39 +0200

    maintainers: add MarkusZoppelt

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15756,6 +15756,12 @@
     githubId = 1913876;
     name = "Markus Scherer";
   };
+  MarkusZoppelt = {
+    email = "markus@zoppelt.net";
+    github = "MarkusZoppelt";
+    githubId = 2495125;
+    name = "Markus Zoppelt";
+  };
   marmolak = {
     email = "hack.robin@gmail.com";
     github = "marmolak";
