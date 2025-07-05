64b782df6730753296724022feb39104a1d16c44
Thierry Delafontaine <delafthi@pm.me>
2025-07-05 17:52:05 +0200

    espansoPlugins.typofixer-it: 1.0.4 -> 1.0.5

--- pkgs/by-name/es/espanso/plugins/typofixer-it/default.nix
+++ pkgs/by-name/es/espanso/plugins/typofixer-it/default.nix
@@ -5,13 +5,13 @@
 }:
 mkEspansoPlugin {
   pname = "typofixer-it";
-  version = "1.0.4";
+  version = "1.0.5";
 
   src = fetchFromGitHub {
     owner = "espanso";
     repo = "hub";
-    rev = "d62bde95fc6f6cf90d4c23ee243922037c053def";
-    hash = "sha256-1cPtknrIi+9uTQt0pMaOBnY3C1GAGXPAA4fIgBh5Wgs=";
+    rev = "a5b6dbc16fa52e717f92a3667323cfd8646b47e0";
+    hash = "sha256-QoQqI1cKgX9Xkl6u5t0TggrjiNsn0N+nZsRVGh812Mg=";
   };
 
   meta = {
