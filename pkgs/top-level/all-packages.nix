fcc23a3e14519ff8f96dfff5b12e983ce8f611cb
huantian <davidtianli@gmail.com>
2025-07-05 13:06:30 -0700

    unityhub: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16309,8 +16309,6 @@ with pkgs;
     buildGoModule = buildGo123Module;
   };
 
-  unityhub = callPackage ../development/tools/unityhub { };
-
   vaultenv = haskell.lib.justStaticExecutables haskellPackages.vaultenv;
 
   vaultwarden-sqlite = vaultwarden;
