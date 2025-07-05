1270d950ff9384dcecd4dcc5f880e12ed4706b47
phanirithvij <phanirithvij2000@gmail.com>
2025-07-05 20:39:18 +0530

    flawz: don't use emulator for manpages and shell completions
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/fl/flawz/package.nix
+++ pkgs/by-name/fl/flawz/package.nix
@@ -38,31 +38,39 @@ rustPlatform.buildRustPackage rec {
     "man"
   ];
 
-  postInstall =
-    let
-      emulator = stdenv.hostPlatform.emulator buildPackages;
-      flawz-mangen = "${emulator} $out/bin/flawz-mangen";
-      flawz-completions = "${emulator} $out/bin/flawz-completions";
-    in
-    lib.optionalString (stdenv.hostPlatform.emulatorAvailable buildPackages) ''
-      export OUT_DIR=$(mktemp -d)
+  postInstall = ''
+    ${lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform)
+      # bash
+      ''
+        export OUT_DIR=$(mktemp -d)
 
-      # Generate the man pages
-      ${flawz-mangen}
-      installManPage $OUT_DIR/flawz.1
+        # Generate the man pages
+        $out/bin/flawz-mangen
+        installManPage $OUT_DIR/flawz.1
 
-      # Generate shell completions
-      ${flawz-completions}
-      installShellCompletion \
-        --bash $OUT_DIR/flawz.bash \
-        --fish $OUT_DIR/flawz.fish \
-        --zsh $OUT_DIR/_flawz
+        # Generate shell completions
+        $out/bin/flawz-completions
+        installShellCompletion \
+          --bash $OUT_DIR/flawz.bash \
+          --fish $OUT_DIR/flawz.fish \
+          --zsh $OUT_DIR/_flawz
 
-      # Clean up temporary directory
-      rm -rf $OUT_DIR
-      # No need for these binaries to end up in the output
-      rm $out/bin/flawz-{completions,mangen}
-    '';
+        # Clean up temporary directory
+        rm -rf $OUT_DIR
+      ''
+    }
+    ${lib.optionalString (!stdenv.buildPlatform.canExecute stdenv.hostPlatform)
+      # bash
+      ''
+        # copy man pages and shell completions from buildPackage
+        mkdir -p $out/share $man/share/man
+        cp -rp ${buildPackages.flawz}/share $out
+        cp -rp ${buildPackages.flawz.man}/share $man
+      ''
+    }
+    # No need for these binaries to end up in the output
+    rm $out/bin/flawz-{completions,mangen}
+  '';
 
   meta = {
     description = "Terminal UI for browsing CVEs";
