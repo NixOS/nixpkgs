f70e619203921eb575a92ddf8f0cfcee977abfb8
Sizhe Zhao <prc.zhao@outlook.com>
2025-07-05 19:39:39 +0800

    briar-desktop: use finalAttrs
    Reference: https://github.com/NixOS/nixpkgs/issues/277994

--- pkgs/by-name/br/briar-desktop/package.nix
+++ pkgs/by-name/br/briar-desktop/package.nix
@@ -19,12 +19,12 @@ let
   '';
 
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "briar-desktop";
   version = "0.6.3-beta";
 
   src = fetchurl {
-    url = "https://desktop.briarproject.org/jars/linux/${version}/briar-desktop-linux-${version}.jar";
+    url = "https://desktop.briarproject.org/jars/linux/${finalAttrs.version}/briar-desktop-linux-${finalAttrs.version}.jar";
     hash = "sha256-8JX4cgRJZDCBlu5iVL7t5nZSZn8XTk3DU3rasViQgtg=";
   };
 
@@ -37,7 +37,7 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/{bin,lib}
-    cp ${src} $out/lib/briar-desktop.jar
+    cp ${finalAttrs.src} $out/lib/briar-desktop.jar
     makeWrapper ${openjdk}/bin/java $out/bin/briar-desktop \
       --add-flags "-jar $out/lib/briar-desktop.jar" \
       --prefix LD_LIBRARY_PATH : "${
@@ -70,4 +70,4 @@ stdenv.mkDerivation rec {
     teams = with lib.teams; [ ngi ];
     platforms = [ "x86_64-linux" ];
   };
-}
+})
