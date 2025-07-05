8c7caa2324ce606037a66e6be3dace3f9a792300
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-05 09:50:06 +0000

    python3Packages.snowflake-core: 1.4.0 -> 1.6.0

--- pkgs/development/python-modules/snowflake-core/default.nix
+++ pkgs/development/python-modules/snowflake-core/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "snowflake-core";
-  version = "1.4.0";
+  version = "1.6.0";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "snowflake_core";
     inherit version;
-    hash = "sha256-3BzO3s5BtS/cuF+JwKuAG8Usca5oo79ffp33TXUP5E8=";
+    hash = "sha256-W6itmcyJRdSAlTLxj2asYjrqGIjmhpdcRh+Gp8OXAik=";
   };
 
   build-system = [ hatchling ];
