6d532aa8bda21cf461b30f055b5f64a083e59306
Jamie Magee <jamie.magee@gmail.com>
2025-07-05 21:56:20 -0700

    python313Packages.pyiss: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12902,6 +12902,8 @@ self: super: with self; {
 
   pyiskra = callPackage ../development/python-modules/pyiskra { };
 
+  pyiss = callPackage ../development/python-modules/pyiss { };
+
   pyisy = callPackage ../development/python-modules/pyisy { };
 
   pyituran = callPackage ../development/python-modules/pyituran { };
