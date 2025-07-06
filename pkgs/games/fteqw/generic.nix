3c21dc88a701c39bb4ca997e56d2614f848d74b2
necrophcodr <nc@scalehost.eu>
2025-07-07 00:03:16 +0200

    fteqw: unstable-2024-04-13 -> unstable-2025-06-25

--- pkgs/games/fteqw/generic.nix
+++ pkgs/games/fteqw/generic.nix
@@ -22,13 +22,13 @@ stdenv.mkDerivation {
     nativeBuildInputs
     postFixup
     ;
-  version = "0-unstable-2024-04-13";
+  version = "0-unstable-2025-06-25";
 
   src = fetchFromGitHub {
     owner = "fte-team";
     repo = "fteqw";
-    rev = "1f9f3635f0aef3b2eed6b40e35fcf6223c6ad533";
-    hash = "sha256-AgTkkP8pT6yioIcVNpxmfCFF0M+7BGx3TXgQSkOgfPI=";
+    rev = "41f35720eda2d1e54d039975db28f46d68a963cb";
+    hash = "sha256-g8dKNRHAZvNfCT3ciDSyKJVqjENml39k26NqkP7sQvA=";
   };
 
   makeFlags = [
