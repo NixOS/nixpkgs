b9ab49b07face73b5399daaa0d41309ed71ad14d
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-06 19:05:15 +0000

    pinta: 3.0.1 -> 3.0.2

--- pkgs/by-name/pi/pinta/package.nix
+++ pkgs/by-name/pi/pinta/package.nix
@@ -12,13 +12,13 @@
 
 buildDotnetModule rec {
   pname = "Pinta";
-  version = "3.0.1";
+  version = "3.0.2";
 
   src = fetchFromGitHub {
     owner = "PintaProject";
     repo = "Pinta";
     rev = version;
-    hash = "sha256-TU4WBrWnhTQQM+OrBq9hpvrihittKTSS0Zo5laL98mE=";
+    hash = "sha256-DFlk3znvK3DAN9dHeTB+X2PdC1STK/i3DOom8Ol2D00=";
   };
 
   nativeBuildInputs = [
