NixOS/nixpkgs

commit 9e25ab57b1ca9976934e3094c78ddcfeeaae87be

v4l-utils: 1.24.1 -> 1.30.1
It now builds with meson, cf.
https://git.linuxtv.org/v4l-utils.git/commit/?id=c252c887c4e390fd7fd8b9f059680e5de3b5f540
It also no longer creates `.la` files for libtool.

We also switch to Qt6 (with `qt5compat`), cf.
https://git.linuxtv.org/v4l-utils.git/commit/?id=ac99ab1b812d7a68ad078e42f49035a7671a6433

On the other hand, it implicitely adds some content
to the packages that are derived from this recipe:

* share/doc now contains 6 MiB of html documentation
* lib/gconv apparently contains files to decode
  Japanese DVB tables (see `README.md` in the sources)
* new program `v4l2-tracer`
* new program `edid-decode`, obsoleting the `edid-decode` package, cf.
  https://git.linuxtv.org/v4l-utils.git/commit/?id=363495b6df0dc30a0d63d49720ebbfa33f65c91f

pkgs/os-specific/linux/v4l-utils/default.nix
@@ -2,22 +2,30 @@
   stdenv,
   lib,
   fetchurl,
+  glibc,
+  clang,
+  doxygen,
+  meson,
+  ninja,
   pkg-config,
   perl,
   argp-standalone,
   libjpeg,
+  json_c,
+  libbpf,
+  libelf,
   udev,
   udevCheckHook,
   withUtils ? true,
   withGUI ? true,
   alsa-lib,
-  libX11,
+  qt5compat,
   qtbase,
   libGLU,
   wrapQtAppsHook,
 }:
 
-# See libv4l in all-packages.nix for the libs only (overrides alsa, libX11 & QT)
+# See libv4l in all-packages.nix for the libs only (overrides alsa, QT)
 
 let
   withQt = withUtils && withGUI;
@@ -26,26 +34,22 @@ in
 # we need to use stdenv.mkDerivation in order not to pollute the libv4l’s closure with Qt
 stdenv.mkDerivation (finalAttrs: {
   pname = "v4l-utils";
-  version = "1.24.1";
+  version = "1.30.1";
 
   src = fetchurl {
-    url = "https://linuxtv.org/downloads/v4l-utils/v4l-utils-${finalAttrs.version}.tar.bz2";
-    hash = "sha256-y7f+imMH9c5TOgXN7XC7k8O6BjlaubbQB+tTt12AX1s=";
+    url = "https://linuxtv.org/downloads/v4l-utils/v4l-utils-${finalAttrs.version}.tar.xz";
+    hash = "sha256-wc9UnC7DzznrXse/FXMTSeYbJqIbXpY5IttCIzO64Zc=";
   };
 
   outputs = [ "out" ] ++ lib.optional withUtils "lib" ++ [ "dev" ];
 
-  configureFlags = (
-    if withUtils then
-      [
-        "--with-localedir=${placeholder "lib"}/share/locale"
-        "--with-udevdir=${placeholder "out"}/lib/udev"
-      ]
-    else
-      [
-        "--disable-v4l-utils"
-      ]
-  );
+  mesonFlags = [
+    (lib.mesonBool "v4l-utils" withUtils)
+    (lib.mesonEnable "qv4l2" withQt)
+    (lib.mesonEnable "qvidcap" withQt)
+    (lib.mesonOption "gconvsysdir" "${glibc.out}/lib/gconv")
+    (lib.mesonOption "udevdir" "${placeholder "out"}/lib/udev")
+  ];
 
   postFixup = ''
     # Create symlink for V4l1 compatibility
@@ -53,25 +57,45 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   nativeBuildInputs = [
+    clang
+    doxygen
+    meson
+    ninja
     pkg-config
     perl
     udevCheckHook
   ] ++ lib.optional withQt wrapQtAppsHook;
 
   buildInputs =
-    [ udev ]
+    [
+      json_c
+      libbpf
+      libelf
+      udev
+    ]
     ++ lib.optional (!stdenv.hostPlatform.isGnu) argp-standalone
     ++ lib.optionals withQt [
       alsa-lib
-      libX11
+      qt5compat
       qtbase
       libGLU
     ];
 
+  hardeningDisable = [ "zerocallusedregs" ];
+
   propagatedBuildInputs = [ libjpeg ];
 
+  # these two `substituteInPlace` have been sent upstream as patches
+  # https://lore.kernel.org/linux-media/867c4d2e-7871-4280-8c89-d4b654597f32@public-files.de/T/
+  # they might fail and have to be removed once the patches get accepted
   postPatch = ''
     patchShebangs utils/
+    substituteInPlace \
+      lib/libdvbv5/meson.build \
+      --replace-fail "install_dir: 'include/libdvbv5'" "install_dir: get_option('includedir') / 'libdvbv5'"
+    substituteInPlace \
+      meson.build \
+      --replace-fail "get_option('datadir') / 'locale'" "get_option('localedir')"
   '';
 
   enableParallelBuilding = true;

pkgs/top-level/all-packages.nix
@@ -11515,7 +11515,7 @@ with pkgs;
     translateManpages = false;
   };
 
-  v4l-utils = qt5.callPackage ../os-specific/linux/v4l-utils { };
+  v4l-utils = qt6.callPackage ../os-specific/linux/v4l-utils { };
 
   windows = callPackages ../os-specific/windows { };
 

commit 0a2c0a4789ee0faeaf5c4600e2dfc4bb6e3bdf76

v4l-utils: fix build for musl
Fixes: 9e25ab57b1ca ("v4l-utils: 1.24.1 -> 1.30.1")

pkgs/os-specific/linux/v4l-utils/default.nix
@@ -41,6 +41,13 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-wc9UnC7DzznrXse/FXMTSeYbJqIbXpY5IttCIzO64Zc=";
   };
 
+  patches = [
+    # Has been submitted upstream, but can't fetchurl/fetchpatch
+    # because patch doesn't know how to decode quoted-printable.
+    # https://lore.kernel.org/all/4dgJekVdP7lLqOQ6JNW05sRHSkRmLLMMQnEn8NGUHPoHDn4SBkaGlHUW89vkJJu3IeFDAh3p6mlplTJJlWJx8V4rr62-hd83quCJ2sIuqoA=@protonmail.com/
+    ./musl.patch
+  ];
+
   outputs =
     [ "out" ]
     ++ lib.optional withUtils "lib"
@@ -49,13 +56,17 @@ stdenv.mkDerivation (finalAttrs: {
       "dev"
     ];
 
-  mesonFlags = [
-    (lib.mesonBool "v4l-utils" withUtils)
-    (lib.mesonEnable "qv4l2" withQt)
-    (lib.mesonEnable "qvidcap" withQt)
-    (lib.mesonOption "gconvsysdir" "${glibc.out}/lib/gconv")
-    (lib.mesonOption "udevdir" "${placeholder "out"}/lib/udev")
-  ];
+  mesonFlags =
+    [
+      (lib.mesonBool "v4l-utils" withUtils)
+      (lib.mesonEnable "gconv" stdenv.hostPlatform.isGnu)
+      (lib.mesonEnable "qv4l2" withQt)
+      (lib.mesonEnable "qvidcap" withQt)
+      (lib.mesonOption "udevdir" "${placeholder "out"}/lib/udev")
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isGnu [
+      (lib.mesonOption "gconvsysdir" "${glibc.out}/lib/gconv")
+    ];
 
   postFixup = ''
     # Create symlink for V4l1 compatibility

pkgs/os-specific/linux/v4l-utils/musl.patch
@@ -1,0 +1,103 @@
+From fa120ba1a02437c762fc6a37a60728cac380aa41 Mon Sep 17 00:00:00 2001
+From: =?UTF-8?q?Zsolt=20Vad=C3=A1sz?= <zsolt_vadasz@protonmail.com>
+Date: Sun, 11 May 2025 12:34:28 +0000
+Subject: [PATCH] v4l2-tracer: Allow building on systems using musl
+
+Signed-off-by: Zsolt Vadasz <zsolt_vadasz@protonmail.com>
+Message-ID: <4dgJekVdP7lLqOQ6JNW05sRHSkRmLLMMQnEn8NGUHPoHDn4SBkaGlHUW89vkJJu3IeFDAh3p6mlplTJJlWJx8V4rr62-hd83quCJ2sIuqoA=@protonmail.com>
+---
+ utils/v4l2-tracer/retrace.cpp | 24 ++++++++++++++++++++++++
+ 1 file changed, 24 insertions(+)
+
+diff --git a/utils/v4l2-tracer/retrace.cpp b/utils/v4l2-tracer/retrace.cpp
+index 010936c0..0acce10c 100644
+--- a/utils/v4l2-tracer/retrace.cpp
++++ b/utils/v4l2-tracer/retrace.cpp
+@@ -10,10 +10,14 @@ extern struct retrace_context ctx_retrace;
+ void retrace_mmap(json_object *mmap_obj, bool is_mmap64)
+ {
+ 	json_object *mmap_args_obj;
++#if defined(linux) && defined(__GLIBC__)
+ 	if (is_mmap64)
+ 		json_object_object_get_ex(mmap_obj, "mmap64", &mmap_args_obj);
+ 	else
+ 		json_object_object_get_ex(mmap_obj, "mmap", &mmap_args_obj);
++#else
++	json_object_object_get_ex(mmap_obj, "mmap", &mmap_args_obj);
++#endif
+ 
+ 	json_object *len_obj;
+ 	json_object_object_get_ex(mmap_args_obj, "len", &len_obj);
+@@ -46,16 +50,24 @@ void retrace_mmap(json_object *mmap_obj, bool is_mmap64)
+ 		return;
+ 
+ 	void *buf_address_retrace_pointer = nullptr;
++#if defined(linux) && defined(__GLIBC__)
+ 	if (is_mmap64)
+ 		buf_address_retrace_pointer = mmap64(0, len, prot, flags, fd_retrace, off);
+ 	else
+ 		buf_address_retrace_pointer = mmap(0, len, prot, flags, fd_retrace, off);
++#else
++	buf_address_retrace_pointer = mmap(0, len, prot, flags, fd_retrace, off);
++#endif
+ 
+ 	if (buf_address_retrace_pointer == MAP_FAILED) {
++#if defined(linux) && defined(__GLIBC__)
+ 		if (is_mmap64)
+ 			perror("mmap64");
+ 		else
+ 			perror("mmap");
++#else
++        perror("mmap");
++#endif
+ 		debug_line_info();
+ 		print_context();
+ 		exit(EXIT_FAILURE);
+@@ -116,10 +128,14 @@ void retrace_open(json_object *jobj, bool is_open64)
+ 	int fd_trace = json_object_get_int(fd_trace_obj);
+ 
+ 	json_object *open_args_obj;
++#if defined(linux) && defined(__GLIBC__)
+ 	if (is_open64)
+ 		json_object_object_get_ex(jobj, "open64", &open_args_obj);
+ 	else
+ 		json_object_object_get_ex(jobj, "open", &open_args_obj);
++#else
++	json_object_object_get_ex(jobj, "open", &open_args_obj);
++#endif
+ 
+ 	json_object *path_obj;
+ 	std::string path_trace;
+@@ -148,10 +164,14 @@ void retrace_open(json_object *jobj, bool is_open64)
+ 		mode = s2number(json_object_get_string(mode_obj));
+ 
+ 	int fd_retrace = 0;
++#if defined(linux) && defined(__GLIBC__)
+ 	if (is_open64)
+ 		fd_retrace = open64(path_retrace.c_str(), oflag, mode);
+ 	else
+ 		fd_retrace = open(path_retrace.c_str(), oflag, mode);
++#else
++	fd_retrace = open(path_retrace.c_str(), oflag, mode);
++#endif
+ 
+ 	if (fd_retrace <= 0) {
+ 		line_info("\n\tCan't open: %s", path_retrace.c_str());
+@@ -162,10 +182,14 @@ void retrace_open(json_object *jobj, bool is_open64)
+ 
+ 	if (is_verbose() || errno != 0) {
+ 		fprintf(stderr, "path: %s ", path_retrace.c_str());
++#if defined(linux) && defined(__GLIBC__)
+ 		if (is_open64)
+ 			perror("open64");
+ 		else
+ 			perror("open");
++#else
++		perror("open");
++#endif
+ 		debug_line_info();
+ 		print_context();
+ 	}
+-- 
+2.49.0
+