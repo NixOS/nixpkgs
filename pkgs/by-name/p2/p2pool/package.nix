2c9a0f79266bfcbc1c48e50850b8a86e271e09ac
Jaco Malan <jacomalan.12@gmail.com>
2025-07-07 15:27:48 +0200

    p2pool: 4.8 -> 4.8.1

--- pkgs/by-name/p2/p2pool/package.nix
+++ pkgs/by-name/p2/p2pool/package.nix
@@ -16,13 +16,13 @@
 
 stdenv.mkDerivation rec {
   pname = "p2pool";
-  version = "4.8";
+  version = "4.8.1";
 
   src = fetchFromGitHub {
     owner = "SChernykh";
     repo = "p2pool";
     rev = "v${version}";
-    hash = "sha256-D1yQMcgRYVZf3/VGCmp6ZGu5YlWUmvlCx3pZqQF7JDM=";
+    hash = "sha256-UnvMR4s6o8n7K+9hig3iSFtbN/BmR6yqjc64X443ctk=";
     fetchSubmodules = true;
   };
 
