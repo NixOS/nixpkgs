fd9a920f354417246dcf3f2c6062dc8d2f9ebea6
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-07-07 13:45:07 -0500

    ocamlPackags.redis: init at 0.8

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1798,6 +1798,8 @@ let
 
         rebez = callPackage ../development/ocaml-modules/rebez { };
 
+        redis = callPackage ../development/ocaml-modules/redis/default.nix { };
+
         reperf = callPackage ../development/ocaml-modules/reperf { };
 
         repr = callPackage ../development/ocaml-modules/repr { };
