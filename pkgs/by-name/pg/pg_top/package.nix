bff300f26d81190c79b425b06d629ec64749dcf9
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-07 13:42:28 +0000

    pg_top: 4.1.1 -> 4.1.3

--- pkgs/by-name/pg/pg_top/package.nix
+++ pkgs/by-name/pg/pg_top/package.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation rec {
   pname = "pg_top";
-  version = "4.1.1";
+  version = "4.1.3";
 
   src = fetchurl {
     url = "https://pg_top.gitlab.io/source/pg_top-${version}.tar.xz";
-    sha256 = "sha256-85LObBS9aAt7Ck5yiK0g2CAKxiYtnWp5XnTAUB5ui/k=";
+    sha256 = "sha256-4El3GmfP5UDJOsDxyU5z/s3JKw0jlMb8EB/hvtywwVs=";
   };
 
   buildInputs = [
