1d2b0b9dee77b0b2e2d9b4baeaa81ffe3ab52f42
Finn Landweber <finn@landweber.xyz>
2025-07-07 19:17:37 +0200

    nixos/urlwatch: init module
    Added systemd service for urlwatch.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1033,6 +1033,7 @@
   ./services/monitoring/ups.nix
   ./services/monitoring/uptime-kuma.nix
   ./services/monitoring/uptime.nix
+  ./services/monitoring/urlwatch.nix
   ./services/monitoring/vlagent.nix
   ./services/monitoring/vmagent.nix
   ./services/monitoring/vmalert.nix
