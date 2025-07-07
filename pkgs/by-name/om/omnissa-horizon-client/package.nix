ba4508a64b81dc7aa189d90858786a87b7d6e128
Manuel Hutter <manuel@hutter.io>
2025-07-07 08:40:04 +0200

    omnissa-horizon-client: fix libxml2 build incompatibility
    Breaking ABI change for libxml2 recently, need to override to use
    supported libxml version.
    
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- pkgs/by-name/om/omnissa-horizon-client/package.nix
+++ pkgs/by-name/om/omnissa-horizon-client/package.nix
@@ -97,7 +97,6 @@ let
           libudev0-shim
           libuuid
           libv4l
-          libxml2
           pango
           pcsclite
           pixman
@@ -115,6 +114,22 @@ let
           xorg.libXScrnSaver
           xorg.libXtst
           zlib
+
+          # c.f. https://github.com/NixOS/nixpkgs/pull/418543
+          (libxml2.overrideAttrs (oldAttrs: rec {
+            version = "2.13.8";
+            src = fetchurl {
+              url = "mirror://gnome/sources/libxml2/${lib.versions.majorMinor version}/libxml2-${version}.tar.xz";
+              hash = "sha256-J3KUyzMRmrcbK8gfL0Rem8lDW4k60VuyzSsOhZoO6Eo=";
+            };
+            meta = oldAttrs.meta // {
+              knownVulnerabilities = oldAttrs.meta.knownVulnerabilities or [ ] ++ [
+                "CVE-2025-49794"
+                "CVE-2025-49796"
+                "CVE-2025-6021"
+              ];
+            };
+          }))
 
           (writeTextDir "etc/omnissa/config" configText)
         ];
