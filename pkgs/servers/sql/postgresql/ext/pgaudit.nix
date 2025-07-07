1a78867a15791b8b4df9f42b0991693f826cf507
Wolfgang Walther <walther@technowledgy.de>
2025-07-07 14:03:21 +0200

    postgresql18Packages.{pg_hint_plan,pg_safeupdate,pgaudit}: mark broken
    No source specification for v18 available, yet, so they should be marked
    broken instead of throwing random errors and breaking the
    maintainer/update script.

--- pkgs/servers/sql/postgresql/ext/pgaudit.nix
+++ pkgs/servers/sql/postgresql/ext/pgaudit.nix
@@ -8,31 +8,34 @@
 }:
 
 let
+  sources = {
+    "17" = {
+      version = "17.0";
+      hash = "sha256-3ksq09wiudQPuBQI3dhEQi8IkXKLVIsPFgBnwLiicro=";
+    };
+    "16" = {
+      version = "16.0";
+      hash = "sha256-8+tGOl1U5y9Zgu+9O5UDDE4bec4B0JC/BQ6GLhHzQzc=";
+    };
+    "15" = {
+      version = "1.7.0";
+      hash = "sha256-8pShPr4HJaJQPjW1iPJIpj3CutTx8Tgr+rOqoXtgCcw=";
+    };
+    "14" = {
+      version = "1.6.2";
+      hash = "sha256-Bl7Jk2B0deZUDiI391vk4nilwuVGHd1wuaQRSCoA3Mk=";
+    };
+    "13" = {
+      version = "1.5.2";
+      hash = "sha256-fyf2Ym0fAAXjc28iFCGDEftPAyDLXmEgi/0DaTJJiIg=";
+    };
+  };
+
   source =
-    {
-      "17" = {
-        version = "17.0";
-        hash = "sha256-3ksq09wiudQPuBQI3dhEQi8IkXKLVIsPFgBnwLiicro=";
-      };
-      "16" = {
-        version = "16.0";
-        hash = "sha256-8+tGOl1U5y9Zgu+9O5UDDE4bec4B0JC/BQ6GLhHzQzc=";
-      };
-      "15" = {
-        version = "1.7.0";
-        hash = "sha256-8pShPr4HJaJQPjW1iPJIpj3CutTx8Tgr+rOqoXtgCcw=";
-      };
-      "14" = {
-        version = "1.6.2";
-        hash = "sha256-Bl7Jk2B0deZUDiI391vk4nilwuVGHd1wuaQRSCoA3Mk=";
-      };
-      "13" = {
-        version = "1.5.2";
-        hash = "sha256-fyf2Ym0fAAXjc28iFCGDEftPAyDLXmEgi/0DaTJJiIg=";
-      };
-    }
-    .${lib.versions.major postgresql.version}
-    or (throw "Source for pgaudit is not available for ${postgresql.version}");
+    sources.${lib.versions.major postgresql.version} or {
+      version = "";
+      hash = throw "Source for pgaudit is not available for ${postgresql.version}";
+    };
 in
 postgresqlBuildExtension {
   pname = "pgaudit";
@@ -42,7 +45,7 @@ postgresqlBuildExtension {
     owner = "pgaudit";
     repo = "pgaudit";
     tag = source.version;
-    hash = source.hash;
+    inherit (source) hash;
   };
 
   buildInputs = [
@@ -55,6 +58,7 @@ postgresqlBuildExtension {
   enableUpdateScript = false;
 
   meta = {
+    broken = !builtins.elem (lib.versions.major postgresql.version) (builtins.attrNames sources);
     description = "Open Source PostgreSQL Audit Logging";
     homepage = "https://github.com/pgaudit/pgaudit";
     changelog = "https://github.com/pgaudit/pgaudit/releases/tag/${source.version}";
