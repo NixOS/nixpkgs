51996db0d8a74ca60cbebbe1f042d1006f1209d1
goyalyashpal <yashpalgoyal1304@gmail.com>
2025-07-08 23:14:13 +0530

    theia: init at 1.63.2

---
+++ pkgs/by-name/th/theia/package.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "theia";
+  version = "1.63.2";
+
+  src = fetchFromGitHub {
+    owner = "eclipse-theia";
+    repo = "theia";
+    rev = "v${version}";
+    hash = "sha256-XMI+QsxXdrNSU+zJe+lOfYGkTimYiUJszYVE/sKfVHk=";
+  };
+
+  meta = {
+    description = "Eclipse Theia is a cloud & desktop IDE framework implemented in TypeScript";
+    homepage = "https://github.com/eclipse-theia/theia";
+    changelog = "https://github.com/eclipse-theia/theia/blob/${src.rev}/CHANGELOG.md";
+    license = with lib.licenses; [ mit gpl2Only epl20 ];
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "theia";
+    platforms = lib.platforms.all;
+  };
+}
