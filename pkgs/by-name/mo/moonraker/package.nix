a4bd88403d56c9bc6a1c5456857182efe23df81b
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-08 19:39:35 +0000

    moonraker: 0.9.3-unstable-2025-06-12 -> 0.9.3-unstable-2025-07-08

--- pkgs/by-name/mo/moonraker/package.nix
+++ pkgs/by-name/mo/moonraker/package.nix
@@ -35,13 +35,13 @@ let
 in
 stdenvNoCC.mkDerivation rec {
   pname = "moonraker";
-  version = "0.9.3-unstable-2025-06-12";
+  version = "0.9.3-unstable-2025-07-08";
 
   src = fetchFromGitHub {
     owner = "Arksine";
     repo = "moonraker";
-    rev = "ccbfd6498941cb4d8b49d192c8dd34feacfbe84a";
-    sha256 = "sha256-SCexS9dRbdwNSy5bTrGaEfP9EmkO3c/IL2+OIG7/K24=";
+    rev = "06cdffa169109f5ca1309903ad0ee7c2f7f62111";
+    sha256 = "sha256-zTwC96WdlUsHCi1a1Owaun207AzbvDTT5SvhLaZmTVM=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
