b8e0be16e731071e8eaa360fccb4e3a1781aeab3
Guy Chronister <guychronister@outlook.com>
2025-07-08 16:32:15 -0500

    terraforming: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16304,8 +16304,6 @@ with pkgs;
     callPackage ../applications/networking/cluster/terraform-providers { }
   );
 
-  terraforming = callPackage ../applications/networking/cluster/terraforming { };
-
   terraform-landscape = callPackage ../applications/networking/cluster/terraform-landscape { };
 
   trufflehog = callPackage ../tools/security/trufflehog {
