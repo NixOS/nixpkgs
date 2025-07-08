a70122384366e280ff134d8b6d055eec73de4716
jack-avery <47289484+jack-avery@users.noreply.github.com>
2025-07-07 23:03:27 -0400

    python3Packages.rcon: init at 2.4.9

---
+++ pkgs/development/python-modules/rcon/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  pythonOlder,
+  setuptools-scm,
+  pytestCheckHook,
+}:
+buildPythonPackage rec {
+  pname = "rcon";
+  version = "2.4.9";
+
+  disabled = pythonOlder "3.10";
+
+  src = fetchPypi {
+    inherit pname version;
+    extension = "tar.gz";
+    hash = "sha256-1BqDEdwTNS2jUWP0ajzBPrIPXN4Sl7dbVudkXvdtCkg=";
+  };
+
+  nativeBuildInputs = [ setuptools-scm ];
+
+  nativeCheckInputs = [ pytestCheckHook ];
+
+  meta = {
+    changelog = "https://github.com/conqp/rcon/releases/tag/v${version}";
+    description = "Python RCON client library";
+    homepage = "https://pypi.org/project/rcon/";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ jack-avery ];
+  };
+}
