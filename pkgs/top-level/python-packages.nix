d56966bf1444254744cf9dd9cdd617c06a2123d3
Jamie Magee <jamie.magee@gmail.com>
2025-07-07 21:50:30 -0700

    python313Packages.pyforked-daapd: init at 0.1.14

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12719,6 +12719,8 @@ self: super: with self; {
 
   pyfnip = callPackage ../development/python-modules/pyfnip { };
 
+  pyforked-daapd = callPackage ../development/python-modules/pyforked-daapd { };
+
   pyformlang = callPackage ../development/python-modules/pyformlang { };
 
   pyfreedompro = callPackage ../development/python-modules/pyfreedompro { };
