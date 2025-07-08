1fdc9cfad6a03cda9bab8f97257f98480341dff0
Moraxyc <i@qaq.li>
2025-07-08 18:28:42 +0800

    valkey: 8.0.3 -> 8.1.3

--- pkgs/by-name/va/valkey/package.nix
+++ pkgs/by-name/va/valkey/package.nix
@@ -24,13 +24,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "valkey";
-  version = "8.0.3";
+  version = "8.1.3";
 
   src = fetchFromGitHub {
     owner = "valkey-io";
     repo = "valkey";
     rev = finalAttrs.version;
-    hash = "sha256-IzerctJUc478dJu2AH20s/A3psiAZWDjQG3USQWqpos=";
+    hash = "sha256-JFtStE1avSWGptgj9KtfAr55+J1FydEzD5plvSe2mjM=";
   };
 
   patches = [
