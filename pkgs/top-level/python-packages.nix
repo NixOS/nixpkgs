836fa3a2828396dc70e96c9671df95ed8414e231
Jamie Magee <jamie.magee@gmail.com>
2025-07-07 21:36:26 -0700

    python313Packages.pynzbgetapi: init at 0.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13345,6 +13345,8 @@ self: super: with self; {
 
   pynzb = callPackage ../development/python-modules/pynzb { };
 
+  pynzbgetapi = callPackage ../development/python-modules/pynzbgetapi { };
+
   pyobihai = callPackage ../development/python-modules/pyobihai { };
 
   pyobjc-core = callPackage ../development/python-modules/pyobjc-core { };
