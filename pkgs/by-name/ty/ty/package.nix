d290dd7c49d9f77ecd50924acca3788ff47e8cc8
Gaetan Lepage <gaetan@glepage.com>
2025-07-08 14:18:35 +0200

    ty: 0.0.1-alpha.13 -> 0.0.1-alpha.14
    Diff: https://github.com/astral-sh/ty/compare/refs/tags/0.0.1-alpha.13...refs/tags/0.0.1-alpha.14
    
    Changelog: https://github.com/astral-sh/ty/blob/0.0.1-alpha.14/CHANGELOG.md

--- pkgs/by-name/ty/ty/package.nix
+++ pkgs/by-name/ty/ty/package.nix
@@ -14,14 +14,14 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "ty";
-  version = "0.0.1-alpha.13";
+  version = "0.0.1-alpha.14";
 
   src = fetchFromGitHub {
     owner = "astral-sh";
     repo = "ty";
     tag = finalAttrs.version;
     fetchSubmodules = true;
-    hash = "sha256-w+eYclC9X0T1f/oj8MY4KG72lpAVGpMjjchER167sp0=";
+    hash = "sha256-q99wzor6inHggInT3R1mX7vLVZwoz68h41+mRg1oayk=";
   };
 
   # For Darwin platforms, remove the integration test for file notifications,
@@ -35,7 +35,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   cargoBuildFlags = [ "--package=ty" ];
 
-  cargoHash = "sha256-blf+ISQgOFZ99jZIHQec8Bhccb0ajZiNLvl0F1xUi/Y=";
+  cargoHash = "sha256-+MlTidF5g15ojRksfVpJIife+Dd3YTge5aBfrjz6qv8=";
 
   nativeBuildInputs = [ installShellFiles ];
 
