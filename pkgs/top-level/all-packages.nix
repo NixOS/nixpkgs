db70081c29bcb77b4bd4e52659b6ebf65ff74409
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-07-08 09:41:10 -0500

    cdrtools: move to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2485,8 +2485,6 @@ with pkgs;
 
   cdist = python3Packages.callPackage ../tools/admin/cdist { };
 
-  cdrtools = callPackage ../tools/cd-dvd/cdrtools { };
-
   cemu-ti = qt5.callPackage ../applications/science/math/cemu-ti { };
 
   libceph = ceph.lib;
