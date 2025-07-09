780b0fcd9a372a448bdea76b66e24c8903c33bc2
Niklas Hambüchen <mail@nh2.me>
2025-07-09 02:34:36 +0200

    pyhon3Packages.lazrs: init at 0.7.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7875,6 +7875,8 @@ self: super: with self; {
 
   lazr-uri = callPackage ../development/python-modules/lazr-uri { };
 
+  lazrs = callPackage ../development/python-modules/lazrs { };
+
   lazy = callPackage ../development/python-modules/lazy { };
 
   lazy-imports = callPackage ../development/python-modules/lazy-imports { };
