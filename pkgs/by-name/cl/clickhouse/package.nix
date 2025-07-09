bb55919962f89c4e1d5c01d1c684930c5861a0e6
Jonathan Davies <jpds@protonmail.com>
2025-07-09 19:44:39 +0000

    clickhouse: Add nix-update-script

--- pkgs/by-name/cl/clickhouse/package.nix
+++ pkgs/by-name/cl/clickhouse/package.nix
@@ -2,4 +2,10 @@ import ./generic.nix {
   version = "25.5.6.14-stable";
   hash = "sha256-gaKozR/QvvyZ3v21XEZLHV2YrhEStKuuAdOjjkd3+uc";
   lts = false;
+  nixUpdateExtraArgs = [
+    "--version-regex"
+    "^v?(.*-stable)$"
+    "--override-filename"
+    "pkgs/by-name/cl/clickhouse/package.nix"
+  ];
 }
