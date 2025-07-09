47aa11a937610ded3333bc5d0059830e6e651cb2
Willy Hille <willy.hille@intenta.de>
2025-07-09 15:16:32 +0000

    python3Packages.validate-pyproject: init at 0.24.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19100,6 +19100,8 @@ self: super: with self; {
 
   validate-email = callPackage ../development/python-modules/validate-email { };
 
+  validate-pyproject = callPackage ../development/python-modules/validate-pyproject { };
+
   validator-collection = callPackage ../development/python-modules/validator-collection { };
 
   validators = callPackage ../development/python-modules/validators { };
