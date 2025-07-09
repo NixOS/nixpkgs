061280ffd5ba86c9019ccd1115d5d7651ff396c8
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-09 14:19:36 +0000

    postgresqlPackages.plv8: 3.2.3 -> 3.2.4

--- pkgs/servers/sql/postgresql/ext/plv8/package.nix
+++ pkgs/servers/sql/postgresql/ext/plv8/package.nix
@@ -17,13 +17,13 @@ let
 in
 postgresqlBuildExtension (finalAttrs: {
   pname = "plv8";
-  version = "3.2.3";
+  version = "3.2.4";
 
   src = fetchFromGitHub {
     owner = "plv8";
     repo = "plv8";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ivQZJSNn5giWF351fqZ7mBZoJkGtby5T7beK45g3Zqs=";
+    hash = "sha256-v4r/6qwDwb3+PQPYV3FqmIcDEkSr8F46PVlFnhUWUGc=";
   };
 
   patches = [
