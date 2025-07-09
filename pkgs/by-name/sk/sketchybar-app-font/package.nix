e6061a9faba8fcef15a5db0a2f0589a023c27e81
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-09 10:55:23 +0000

    sketchybar-app-font: 2.0.35 -> 2.0.39

--- pkgs/by-name/sk/sketchybar-app-font/package.nix
+++ pkgs/by-name/sk/sketchybar-app-font/package.nix
@@ -9,13 +9,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "sketchybar-app-font";
-  version = "2.0.35";
+  version = "2.0.39";
 
   src = fetchFromGitHub {
     owner = "kvndrsslr";
     repo = "sketchybar-app-font";
-    rev = "v2.0.35";
-    hash = "sha256-qXXxpx/thEJuFnNUWmMXYMoir5CGqCktV1emzVPcTIY=";
+    rev = "v2.0.39";
+    hash = "sha256-TTkw/LhRnqJYBn6Mt+T3qgyLHhLIyACdUoShq/mIONY=";
   };
 
   pnpmDeps = pnpm_9.fetchDeps {
