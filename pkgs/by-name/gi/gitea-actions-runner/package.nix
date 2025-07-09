12892fbfd0ae8ba064ffb13e88b4d09211a3b677
Igor Svistoun <igor@svistoun.com>
2025-07-09 18:35:43 -0400

    use finalAttrs pattern

--- pkgs/by-name/gi/gitea-actions-runner/package.nix
+++ pkgs/by-name/gi/gitea-actions-runner/package.nix
@@ -6,7 +6,7 @@
   gitea-actions-runner,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "gitea-actions-runner";
   version = "0.2.12";
 
@@ -14,7 +14,7 @@ buildGoModule rec {
     domain = "gitea.com";
     owner = "gitea";
     repo = "act_runner";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-z/wEs110Y2IZ2Jm6bayxlD2sjyl2V/v+gP6l9pwGi5o=";
   };
 
@@ -23,20 +23,20 @@ buildGoModule rec {
   ldflags = [
     "-s"
     "-w"
-    "-X gitea.com/gitea/act_runner/internal/pkg/ver.version=v${version}"
+    "-X gitea.com/gitea/act_runner/internal/pkg/ver.version=v${finalAttrs.version}"
   ];
 
   passthru.tests.version = testers.testVersion {
     package = gitea-actions-runner;
-    version = "v${version}";
+    version = "v${finalAttrs.version}";
   };
 
   meta = {
     mainProgram = "act_runner";
     maintainers = with lib.maintainers; [ techknowlogick ];
     license = lib.licenses.mit;
-    changelog = "https://gitea.com/gitea/act_runner/releases/tag/v${version}";
+    changelog = "https://gitea.com/gitea/act_runner/releases/tag/v${finalAttrs.version}";
     homepage = "https://gitea.com/gitea/act_runner";
     description = "Runner for Gitea based on act";
   };
-}
+})
