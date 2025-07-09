b33a46aee691f9f406bc321f1f8c44365f8b10b5
Johannes Fleck <jfleck@posteo.de>
2025-07-09 20:46:47 +0200

    python3Packages.python-statemachine: init at 2.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14997,6 +14997,8 @@ self: super: with self; {
 
   python-sql = callPackage ../development/python-modules/python-sql { };
 
+  python-statemachine = callPackage ../development/python-modules/python-statemachine { };
+
   python-status = callPackage ../development/python-modules/python-status { };
 
   python-stdnum = callPackage ../development/python-modules/python-stdnum { };
