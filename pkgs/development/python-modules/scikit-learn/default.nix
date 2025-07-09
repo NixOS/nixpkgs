7cb200deae3d69eb85c0976be62221c41a9c718c
Sarah Clark <seclark@nextquestion.net>
2025-07-09 13:39:13 -0700

    python3Packages.scikit-learn: re-enable tests on Darwin and aarch64

--- pkgs/development/python-modules/scikit-learn/default.nix
+++ pkgs/development/python-modules/scikit-learn/default.nix
@@ -69,19 +69,10 @@ buildPythonPackage rec {
 
   env.LC_ALL = "en_US.UTF-8";
 
-  # PermissionError: [Errno 1] Operation not permitted: '/nix/nix-installer'
-  doCheck = !stdenv.hostPlatform.isDarwin;
-
-  disabledTests =
-    [
-      # Skip test_feature_importance_regression - does web fetch
-      "test_feature_importance_regression"
-    ]
-    ++ lib.optionals stdenv.hostPlatform.isAarch64 [
-      # doesn't seem to produce correct results?
-      # possibly relevant: https://github.com/scikit-learn/scikit-learn/issues/25838#issuecomment-2308650816
-      "test_sparse_input"
-    ];
+  disabledTests = [
+    # Skip test_feature_importance_regression - does web fetch
+    "test_feature_importance_regression"
+  ];
 
   pytestFlagsArray = [
     "--pyargs"
