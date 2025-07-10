2a3fdf9cf4591843dbc8cc11f8ffa3875bbdee5b
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-10 14:06:26 +0000

    nelm: 1.7.0 -> 1.7.2

--- pkgs/by-name/ne/nelm/package.nix
+++ pkgs/by-name/ne/nelm/package.nix
@@ -9,16 +9,16 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "nelm";
-  version = "1.7.0";
+  version = "1.7.2";
 
   src = fetchFromGitHub {
     owner = "werf";
     repo = "nelm";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-XAieAxfpNi2XpjG8lyatAqP13wicx3JFjckgSmiKqjA=";
+    hash = "sha256-EmsA9ppuAZnrYtJE96vg3s+QGlsl4gXcQj2A0Buur4g=";
   };
 
-  vendorHash = "sha256-bx8e5jV+ORnJg/35VwO7qodFjmSf7XbzTKZKp3b8hqc=";
+  vendorHash = "sha256-q7+/OdYt/mdf/oZfFh3/vU8uOKaUpX+lLAFAOHgy6mk=";
 
   subPackages = [ "cmd/nelm" ];
 
