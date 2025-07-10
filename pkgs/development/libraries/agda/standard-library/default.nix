9b250e5aef16cae13a6c943f37c15e1e94da6584
Naïm Camille Favier <n@monade.li>
2025-07-10 19:54:20 +0200

    agdaPackages.standard-library: 2.2 -> 2.2-unstable-2025-07-03

--- pkgs/development/libraries/agda/standard-library/default.nix
+++ pkgs/development/libraries/agda/standard-library/default.nix
@@ -8,13 +8,13 @@
 
 mkDerivation rec {
   pname = "standard-library";
-  version = "2.2";
+  version = "2.2-unstable-2025-07-03";
 
   src = fetchFromGitHub {
     repo = "agda-stdlib";
     owner = "agda";
-    rev = "v${version}";
-    hash = "sha256-/Fy5EOSbVNXt6Jq0yKSnlNPW4SYfn+eCTAYFnMZrbR0=";
+    rev = "6f8af9452e7fac27bc3b3ad068793b538f07668e";
+    hash = "sha256-LD6KasmQ9ZHRNQJ0N4wjyc6JiSkZpmyqQq9B0Wta1n0=";
   };
 
   nativeBuildInputs = [ (ghcWithPackages (self: [ self.filemanip ])) ];
