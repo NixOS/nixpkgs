817fe3efa6b24432b6cb1149229c1b1c8d47ee9e
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-10 13:12:20 +0200

    python313Packages.specfile: init at 0.36.0
    Library for parsing and manipulating RPM spec files
    
    https://github.com/packit/specfile

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16705,6 +16705,8 @@ self: super: with self; {
 
   speaklater3 = callPackage ../development/python-modules/speaklater3 { };
 
+  specfile = callPackage ../development/python-modules/specfile { };
+
   spectra = callPackage ../development/python-modules/spectra { };
 
   spectral-cube = callPackage ../development/python-modules/spectral-cube { };
