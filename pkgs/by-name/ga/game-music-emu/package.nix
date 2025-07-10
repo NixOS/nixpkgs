NixOS/nixpkgs

commit e65c57cbbc5d4258ee6445078afca9d1f0be0d66

game-music-emu: 0.6.3 -> 0.6.4 | source migration

pkgs/by-name/ga/game-music-emu/package.nix
@@ -1,21 +1,22 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
   cmake,
   removeReferencesTo,
   zlib,
 }:
 
 stdenv.mkDerivation rec {
-  version = "0.6.3";
+  version = "0.6.4";
   pname = "game-music-emu";
 
-  src = fetchurl {
-    url = "https://bitbucket.org/mpyne/game-music-emu/downloads/${pname}-${version}.tar.xz";
-    sha256 = "07857vdkak306d9s5g6fhmjyxk7vijzjhkmqb15s7ihfxx9lx8xb";
+  src = fetchFromGitHub {
+    owner = "libgme";
+    repo = "game-music-emu";
+    tag = version;
+    hash = "sha256-XmPcFfKsEe07hH7f0xMs9hRJshOO/p58Zm9fYsmCCoA=";
   };
-  cmakeFlags = [ "-DENABLE_UBSAN=OFF" ];
   nativeBuildInputs = [
     cmake
     removeReferencesTo
@@ -31,7 +32,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    homepage = "https://bitbucket.org/mpyne/game-music-emu/wiki/Home";
+    homepage = "https://github.com/libgme/game-music-emu/";
     description = "Collection of video game music file emulators";
     license = licenses.lgpl21Plus;
     platforms = platforms.all;

commit 4ae4ca60d85e732f6c09d1cd9cdfb060acac8fbe

game-music-emu: fix hash
I'm not sure what the hash here refers to, but it substitutes, and it
contains a version of the source that still tries to link with
libubsan, so the build fails.

Fixes: e65c57cbbc5d ("game-music-emu: 0.6.3 -> 0.6.4 | source migration")

pkgs/by-name/ga/game-music-emu/package.nix
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     owner = "libgme";
     repo = "game-music-emu";
     tag = version;
-    hash = "sha256-XmPcFfKsEe07hH7f0xMs9hRJshOO/p58Zm9fYsmCCoA=";
+    hash = "sha256-qGNWFFUUjv2R5e/nQrriAyDJCARISqNB8e5/1zEJ3fk=";
   };
   nativeBuildInputs = [
     cmake