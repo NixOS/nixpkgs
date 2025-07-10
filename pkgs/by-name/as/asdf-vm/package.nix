106191f662830b1d208d60808f32c2da84a8ee5e
Nathan Warner <nathan@frcv.net>
2025-07-10 09:40:30 -0600

    Fixed formatting

--- pkgs/by-name/as/asdf-vm/package.nix
+++ pkgs/by-name/as/asdf-vm/package.nix
@@ -28,28 +28,30 @@ buildGoModule rec {
   # Tests have additional requirements
   doCheck = false;
 
-  postInstall = ''
-        # Install profile.d script for environment setup
-        mkdir -p $out/etc/profile.d
-        cat > $out/etc/profile.d/asdf-prepare.sh <<'EOF'
-    export ASDF_DIR="$out"
-    export PATH="''${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-    EOF
+  postInstall =
+    ''
+      # Install profile.d script for environment setup
+      mkdir -p $out/etc/profile.d
+      cat > $out/etc/profile.d/asdf-prepare.sh <<'EOF'
+      export ASDF_DIR="$out"
+      export PATH="''${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+      EOF
 
-        # Wrap the asdf binary to set ASDF_DIR
-        wrapProgram $out/bin/asdf \
-          --set ASDF_DIR $out
+      # Wrap the asdf binary to set ASDF_DIR
+      wrapProgram $out/bin/asdf \
+        --set ASDF_DIR $out
 
-        # Generate and install shell completions
-        mkdir -p $out/share/completions
+      # Generate and install shell completions
+      mkdir -p $out/share/completions
 
-        # Generate completions using the built asdf binary
-'' + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-        installShellCompletion --cmd asdf \
-          --bash <($out/bin/asdf completion bash) \
-          --fish <($out/bin/asdf completion fish) \
-          --zsh <($out/bin/asdf completion zsh)
-  '';
+      # Generate completions using the built asdf binary
+    ''
+    + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+      installShellCompletion --cmd asdf \
+        --bash <($out/bin/asdf completion bash) \
+        --fish <($out/bin/asdf completion fish) \
+        --zsh <($out/bin/asdf completion zsh)
+    '';
 
   meta = with lib; {
     description = "Extendable version manager with support for Ruby, Node.js, Erlang & more";
