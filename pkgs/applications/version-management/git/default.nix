8219b55be7772c55e5e8b2680ccaaa7f666039cf
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-10 19:36:00 +0200

    git: 2.49.0 → 2.49.1 Release notes: https://github.com/git/git/blob/v2.49.1/Documentation/RelNotes/2.49.1.txt Fixes: CVE-2025-27613, CVE-2025-27614, CVE-2025-46334, CVE-2025-46835, CVE-2025-48384, CVE-2025-48385, CVE-2025-48386

--- pkgs/applications/version-management/git/default.nix
+++ pkgs/applications/version-management/git/default.nix
@@ -60,7 +60,7 @@ assert sendEmailSupport -> perlSupport;
 assert svnSupport -> perlSupport;
 
 let
-  version = "2.49.0";
+  version = "2.49.1";
   svn = subversionClient.override { perlBindings = perlSupport; };
   gitwebPerlLibs = with perlPackages; [
     CGI
@@ -89,7 +89,7 @@ stdenv.mkDerivation (finalAttrs: {
         }.tar.xz"
       else
         "https://www.kernel.org/pub/software/scm/git/git-${version}.tar.xz";
-    hash = "sha256-YYGQz1kLfp9sEfkfI7HSZ82Yw6szuFBBbYdY+LWoVig=";
+    hash = "sha256-MQgx3pZ/HIxej/VfkoB96on4PcPT0qXRbCCb0Box3vE=";
   };
 
   outputs = [ "out" ] ++ lib.optional withManual "doc";
