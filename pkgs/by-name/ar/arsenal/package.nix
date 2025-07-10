5a9ae7aafa4da2900e49e3798b8d8b135494f864
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-10 19:15:43 +0200

    arsenal: modernize

--- pkgs/by-name/ar/arsenal/package.nix
+++ pkgs/by-name/ar/arsenal/package.nix
@@ -2,17 +2,18 @@
   lib,
   fetchFromGitHub,
   python3,
+  versionCheckHook,
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "arsenal";
   version = "1.1.0";
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Orange-Cyberdefense";
     repo = "arsenal";
-    rev = version;
+    tag = version;
     sha256 = "sha256-NbNXyR5aNKvRJU9JWGk/ndwU1bhNgDOdcRqBkAY9nPA=";
   };
 
@@ -28,18 +29,18 @@ python3.pkgs.buildPythonApplication rec {
     pyyaml
   ];
 
-  # Project has no tests
-  doCheck = false;
+  nativeCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
 
   pythonImportsCheck = [
     "arsenal"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Tool to generate commands for security and network tools";
     homepage = "https://github.com/Orange-Cyberdefense/arsenal";
-    license = with licenses; [ gpl3Only ];
-    maintainers = with maintainers; [ fab ];
+    license = with lib.licenses; [ gpl3Only ];
+    maintainers = with lib.maintainers; [ fab ];
     mainProgram = "arsenal";
   };
 }
