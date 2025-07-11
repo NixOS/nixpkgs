c5ceecd3f397565d5273c7ccca027c65c385659a
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-07-11 22:17:38 +0200

    pkgs/by-name/{m,n}*: migrate to pyproject = true

--- pkgs/by-name/ml/mlarchive2maildir/package.nix
+++ pkgs/by-name/ml/mlarchive2maildir/package.nix
@@ -7,22 +7,27 @@
 python3.pkgs.buildPythonApplication rec {
   pname = "mlarchive2maildir";
   version = "0.0.9";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "02zjwa7zbcbqj76l0qmg7bbf3fqli60pl2apby3j4zwzcrrryczs";
   };
 
-  nativeBuildInputs = with python3.pkgs; [ setuptools-scm ];
+  build-system = with python3.pkgs; [
+    setuptools
+    setuptools-scm
+  ];
 
-  propagatedBuildInputs = with python3.pkgs; [
+  dependencies = with python3.pkgs; [
     beautifulsoup4
     click
     click-log
     requests
     six
   ];
+
+  pythonImportsCheck = [ "mlarchive2maildir" ];
 
   meta = with lib; {
     homepage = "https://github.com/flokli/mlarchive2maildir";
