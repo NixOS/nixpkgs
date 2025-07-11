50ed9bc66bf56b9892ee0bc067edb25645649aa0
totaltaxamount <shieldscoen@gmail.com>
2025-07-11 11:50:30 -0600

    pathplanner: review changes 2

--- pkgs/by-name/pa/pathplanner/package.nix
+++ pkgs/by-name/pa/pathplanner/package.nix
@@ -1,53 +1,50 @@
 {
-  flutter,
+  flutter327,
   fetchFromGitHub,
   util-linux,
-  wrapGAppsHook3,
+  copyDesktopItems,
   makeDesktopItem,
   lib,
 }:
 
-flutter.buildFlutterApplication rec {
+flutter327.buildFlutterApplication rec {
   pname = "pathplanner";
   version = "2025.2.2";
 
   src = fetchFromGitHub {
     owner = "mjansen4857";
-    repo = pname;
+    repo = "pathplanner";
     tag = "v${version}";
     hash = "sha256-RTLesH7j3R9JbvNr46Tk8bHbCeMm0daeTaxSOibkPjM=";
   };
 
   nativeBuildInputs = [
     util-linux
-  ];
-
-  propagatedBuildInputs = [
-    wrapGAppsHook3
+    copyDesktopItems
   ];
 
   pubspecLock = lib.importJSON ./pubspec.lock;
 
   desktop = makeDesktopItem {
     desktopName = "Path Planner";
-    name = pname;
-    exec = pname;
-    icon = pname;
+    name = "pathplanner";
+    exec = "pathplanner";
+    icon = "pathplanner";
     comment = "FRC motion planner";
     categories = [
       "Utility"
     ];
-    genericName = pname;
+    genericName = "pathplanner";
   };
 
+  desktopItems = [ desktop ];
+
   postInstall = ''
-    mkdir -p $out/share/icons $out/share/applications
-    cp $out/app/pathplanner/data/flutter_assets/images/icon.png $out/share/icons/${pname}.png
-    cp ${desktop}/share/applications/${pname}.desktop $out/share/applications
+    install -Dm0644 $out/app/pathplanner/data/flutter_assets/images/icon.png $out/share/icons/pathplanner.png
   '';
 
   meta = {
-    description = "A motion profile generator for FRC robots";
+    description = "Motion profile generator for FRC robots";
     homepage = "https://pathplanner.dev/home.html";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ totaltax ];
