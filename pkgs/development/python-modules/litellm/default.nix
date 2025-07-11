801dc586a9686b6bfc1827adb09e1feda056ccdc
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-11 00:24:56 +0000

    python3Packages.litellm: 1.73.6 -> 1.74.0

--- pkgs/development/python-modules/litellm/default.nix
+++ pkgs/development/python-modules/litellm/default.nix
@@ -46,7 +46,7 @@
 
 buildPythonPackage rec {
   pname = "litellm";
-  version = "1.73.6";
+  version = "1.74.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -55,7 +55,7 @@ buildPythonPackage rec {
     owner = "BerriAI";
     repo = "litellm";
     tag = "v${version}-stable";
-    hash = "sha256-L4cd72mKkCyawHrNsU71PBwiI69QIWAD6HHVObNtq3I=";
+    hash = "sha256-qjr08HHEELIwdL3IZ+GWJWGvIySTTX1nv46tYNBP53Y=";
   };
 
   build-system = [ poetry-core ];
