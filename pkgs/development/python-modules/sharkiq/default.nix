0c4218a2250e382a52be392877391ef1ccf2f719
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-11 20:53:10 +0200

    pythpn313Packages.sharkiq: refactor

--- pkgs/development/python-modules/sharkiq/default.nix
+++ pkgs/development/python-modules/sharkiq/default.nix
@@ -4,12 +4,13 @@
   buildPythonPackage,
   fetchFromGitHub,
   requests,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "sharkiq";
   version = "1.1.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "JeffResc";
@@ -18,12 +19,14 @@ buildPythonPackage rec {
     hash = "sha256-FIPU2D0e0JGcoxFKe5gf5nKZ0T/a18WS9I+LXeig1is=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     aiohttp
     requests
   ];
 
-  # Project has no tests
+  # Module has no tests
   doCheck = false;
 
   pythonImportsCheck = [ "sharkiq" ];
@@ -31,6 +34,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python API for Shark IQ robots";
     homepage = "https://github.com/JeffResc/sharkiq";
+    changelog = "https://github.com/JeffResc/sharkiq/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
