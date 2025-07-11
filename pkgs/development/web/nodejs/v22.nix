8e7053270808f3c80faaa41dc3cb6441983e9543
Alex James <git@alextjam.es>
2025-07-10 20:32:11 -0500

    nodejs: drop v8 instrumentation patch
    This is no longer needed now that apple-sdk_11 is the default SDK.

--- pkgs/development/web/nodejs/v22.nix
+++ pkgs/development/web/nodejs/v22.nix
@@ -51,7 +51,6 @@ buildNodejs {
     ++ gypPatches
     ++ [
       ./configure-armv6-vfpv2.patch
-      ./disable-darwin-v8-system-instrumentation-node19.patch
       ./bypass-darwin-xcrun-node16.patch
       ./node-npm-build-npm-package-logic.patch
       ./use-correct-env-in-tests.patch
