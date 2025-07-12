493071cdc6e18c60d6610dd46bdd59d008481965
Casey Avila <casey@theavilas.org>
2025-07-12 13:51:49 -0600

    maintainers: add caseyavila

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4218,6 +4218,12 @@
     githubId = 1945;
     name = "Casey Rodarmor";
   };
+  caseyavila = {
+    email = "casey@theavilas.org";
+    github = "caseyavila";
+    githubId = 53847249;
+    name = "Casey Avila";
+  };
   catap = {
     email = "kirill@korins.ky";
     github = "catap";
