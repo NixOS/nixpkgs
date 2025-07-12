d40a3ba10d57cd0f555dd5270189139a9eedbcc4
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-12 03:41:11 +0000

    dotenvx: 1.45.2 -> 1.47.5

--- pkgs/by-name/do/dotenvx/package.nix
+++ pkgs/by-name/do/dotenvx/package.nix
@@ -8,16 +8,16 @@
 
 buildNpmPackage rec {
   pname = "dotenvx";
-  version = "1.45.2";
+  version = "1.47.5";
 
   src = fetchFromGitHub {
     owner = "dotenvx";
     repo = "dotenvx";
     tag = "v${version}";
-    hash = "sha256-43CfqPd3GzBAZ97IUSQuS96xOy34941wNcieKMoLZV4=";
+    hash = "sha256-Nvh3PZSg5tpFwfyjImSIVzsH8AS48oszoxegD2AjFwQ=";
   };
 
-  npmDepsHash = "sha256-bSOZWZ3CJl3tnjAFb7ozHmaBTOV9FS+t/+9DVoma4ag=";
+  npmDepsHash = "sha256-KQnaiAtyRGiFTQnOICM5MTrLPvF/JYuy6kX4//mWYkE=";
 
   dontNpmBuild = true;
 
