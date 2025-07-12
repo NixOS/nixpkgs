cec96cc39356581b1459375e822b65985470621f
Guillaume Girol <symphorien+git@xlumurb.eu>
2025-07-12 12:00:00 +0000

    bitwuzla: 0.8.0 -> 0.8.1

--- pkgs/by-name/bi/bitwuzla/package.nix
+++ pkgs/by-name/bi/bitwuzla/package.nix
@@ -1,7 +1,6 @@
 {
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   lib,
   python3,
   meson,
@@ -22,35 +21,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bitwuzla";
-  version = "0.8.0";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "bitwuzla";
     repo = "bitwuzla";
     rev = finalAttrs.version;
-    hash = "sha256-Th2YkynOzxcZB4xqyH8D3QqcuQzkWYAd+hYfZ5OmrJ0=";
+    hash = "sha256-4Gf06aZ3iBEu0bNZAGgWCXVgKA0ew37Zf6XArnlVAXw=";
   };
-
-  patches = [
-    # fix tests https://github.com/bitwuzla/bitwuzla/issues/176
-    # remove on next release
-    (fetchpatch {
-      url = "https://github.com/bitwuzla/bitwuzla/commit/d090786042aaceef6d39cf55c22daf5fb74457f5.patch";
-      hash = "sha256-c3u2oL1Fhg7dvVlA7pfPMTW5wgWigbxuegz5wHFZquY=";
-    })
-    # fix tests https://github.com/bitwuzla/bitwuzla/issues/176
-    # remove on next release
-    (fetchpatch {
-      url = "https://github.com/bitwuzla/bitwuzla/commit/bdc83e1f06ce2851c86cce16a604c3c47b254d2d.patch";
-      hash = "sha256-i9u/11h+Ye3NHI+rkDLISbG1DYGCBqORL1nEEJryYzA=";
-    })
-    # fix tests https://github.com/bitwuzla/bitwuzla/issues/176
-    # remove on next release
-    (fetchpatch {
-      url = "https://github.com/bitwuzla/bitwuzla/commit/a623930e3c12c3932301bd68c54590a15e6bfa69.patch";
-      hash = "sha256-uua+wSvK621cmNLaKrRgjb0aNL/b4254lQMtAiGU9KM=";
-    })
-  ];
 
   strictDeps = true;
 
