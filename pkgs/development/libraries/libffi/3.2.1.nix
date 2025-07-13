e3315b2b91477dce5410d1cfd989de30b746ca60
blenderfreaky <blenderfreaky@mailbox.org>
2025-07-13 02:03:48 +0200

    libffi_3_2_1: init at 3.2.1
    This commit re-introduces a legacy version of libffi, which is required as a dependency for the Intel oneAPI Base/HPC Toolkits.
    
    The package is based off of the one from commit
     bac4d95aa2a92be7049dedafb8ec76606f79d8a2
    with minor changes such as changing `name` to `pname` and `version` and
    assigning myself as maintainer.
    
    View the original file here: https://github.com/NixOS/nixpkgs/blob/bac4d95aa2a92be7049dedafb8ec76606f79d8a2/pkgs/development/libraries/libffi/default.nix

---
+++ pkgs/development/libraries/libffi/3.2.1.nix
@@ -0,0 +1,109 @@
+{
+  stdenv,
+  fetchurl,
+  fetchpatch,
+  lib,
+  autoreconfHook,
+  # libffi is used in darwin stdenv
+  # we cannot run checks within it
+  doCheck ? !stdenv.isDarwin,
+  dejagnu,
+}:
+let
+  pname = "libffi";
+  version = "3.2.1";
+in
+stdenv.mkDerivation {
+  inherit pname version;
+
+  src = fetchurl {
+    url = "https://sourceware.org/pub/libffi/${pname}-${version}.tar.gz";
+    sha256 = "0dya49bnhianl0r65m65xndz6ls2jn1xngyn72gd28ls3n7bnvnh";
+  };
+
+  patches =
+    lib.optional stdenv.isCygwin ./3.2.1-cygwin.patch
+    ++ lib.optional stdenv.isAarch64 (fetchpatch {
+      url = "https://src.fedoraproject.org/rpms/libffi/raw/ccffc1700abfadb0969495a6e51b964117fc03f6/f/libffi-aarch64-rhbz1174037.patch";
+      sha256 = "1vpirrgny43hp0885rswgv3xski8hg7791vskpbg3wdjdpb20wbc";
+    })
+    ++ lib.optional stdenv.hostPlatform.isMusl (fetchpatch {
+      name = "gnu-linux-define.patch";
+      url = "https://git.alpinelinux.org/cgit/aports/plain/main/libffi/gnu-linux-define.patch?id=bb024fd8ec6f27a76d88396c9f7c5c4b5800d580";
+      sha256 = "11pvy3xkhyvnjfyy293v51f1xjy3x0azrahv1nw9y9mw8bifa2j2";
+    })
+    ++ lib.optional stdenv.hostPlatform.isRiscV (fetchpatch {
+      name = "riscv-support.patch";
+      url = "https://github.com/sorear/libffi-riscv/commit/e46492e8bb1695a19bc1053ed869e6c2bab02ff2.patch";
+      sha256 = "1vl1vbvdkigs617kckxvj8j4m2cwg62kxm1clav1w5rnw9afxg0y";
+    })
+    ++ lib.optionals stdenv.isMips [
+      (fetchpatch {
+        name = "0001-mips-Use-compiler-internal-define-for-linux.patch";
+        url = "https://cgit.openembedded.org/openembedded-core/plain/meta/recipes-support/libffi/libffi/0001-mips-Use-compiler-internal-define-for-linux.patch?id=318e33a708378652edcf61ce7d9d7f3a07743000";
+        sha256 = "1gc53lw90p6hc0cmhj3csrwincfz7va5ss995ksw5gm0yrr9mrvb";
+      })
+      (fetchpatch {
+        name = "0001-mips-fix-MIPS-softfloat-build-issue.patch";
+        url = "https://cgit.openembedded.org/openembedded-core/plain/meta/recipes-support/libffi/libffi/0001-mips-fix-MIPS-softfloat-build-issue.patch?id=318e33a708378652edcf61ce7d9d7f3a07743000";
+        sha256 = "0l8xgdciqalg4z9rcwyk87h8fdxpfv4hfqxwsy2agpnpszl5jjdq";
+      })
+    ];
+
+  outputs = [
+    "out"
+    "dev"
+    "man"
+    "info"
+  ];
+
+  nativeBuildInputs = lib.optional stdenv.hostPlatform.isRiscV autoreconfHook;
+
+  configureFlags = [
+    "--with-gcc-arch=generic" # no detection of -march= or -mtune=
+    "--enable-pax_emutramp"
+  ];
+
+  preCheck = ''
+    # The tests use -O0 which is not compatible with -D_FORTIFY_SOURCE.
+    NIX_HARDENING_ENABLE=''${NIX_HARDENING_ENABLE/fortify/}
+    NIX_HARDENING_ENABLE=''${NIX_HARDENING_ENABLE/fortify3/}
+  '';
+
+  checkInputs = [ dejagnu ];
+
+  inherit doCheck;
+
+  dontStrip = stdenv.hostPlatform != stdenv.buildPlatform; # Don't run the native `strip' when cross-compiling.
+
+  # Install headers and libs in the right places.
+  postFixup = ''
+    mkdir -p "$dev/"
+    mv "$out/lib/${pname}-${version}/include" "$dev/include"
+    rmdir "$out/lib/${pname}-${version}"
+    substituteInPlace "$dev/lib/pkgconfig/libffi.pc" \
+      --replace-fail 'includedir=''${libdir}/libffi-3.2.1' "includedir=$dev"
+  '';
+
+  meta = {
+    description = "A foreign function call interface library";
+    longDescription = ''
+      The libffi library provides a portable, high level programming
+      interface to various calling conventions.  This allows a
+      programmer to call any function specified by a call interface
+      description at run-time.
+
+      FFI stands for Foreign Function Interface.  A foreign function
+      interface is the popular name for the interface that allows code
+      written in one language to call code written in another
+      language.  The libffi library really only provides the lowest,
+      machine dependent layer of a fully featured foreign function
+      interface.  A layer must exist above libffi that handles type
+      conversions for values passed between the two languages.
+    '';
+    homepage = "https://sourceware.org/libffi/";
+    license = lib.licenses.mit;
+    maintainers = [ lib.maintainers.blenderfreaky ];
+    platforms = lib.platforms.all;
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7054,6 +7054,8 @@ with pkgs;
   #
   # `libffiReal` is provided in case the upstream libffi package is needed on Darwin instead of the fork.
   libffi = if stdenv.hostPlatform.isDarwin then darwin.libffi else libffiReal;
+  libffi_3_3 = callPackage ../development/libraries/libffi/3.3.nix { };
+  libffi_3_2_1 = callPackage ../development/libraries/libffi/3.2.1.nix { };
 
   # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=blob;f=README;h=fd6e1a83f55696c1f7a08f6dfca08b2d6b7617ec;hb=70058cd9f944d620764e57c838209afae8a58c78#l118
   libgpg-error-gen-posix-lock-obj = libgpg-error.override {
