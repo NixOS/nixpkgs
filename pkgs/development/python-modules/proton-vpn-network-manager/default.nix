46b0f0b3c938eb1f2ae4e3f3b01abab949f548dd
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2025-07-13 14:47:54 +0300

    python3Packages.proton-vpn-network-manager: 0.12.13 -> 0.12.14
    Update the package to the latest upstream version.

--- pkgs/development/python-modules/proton-vpn-network-manager/default.nix
+++ pkgs/development/python-modules/proton-vpn-network-manager/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "proton-vpn-network-manager";
-  version = "0.12.13";
+  version = "0.12.14";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ProtonVPN";
     repo = "python-proton-vpn-network-manager";
     tag = "v${version}";
-    hash = "sha256-LRjC1uuAG2OG52moRBSvTR7HvqdldNmW0Tv7AZmUf60=";
+    hash = "sha256-flZeEdmGXsSFHtlm6HrBtuwOcYJFjWmkMvGgnHL4cPw=";
   };
 
   nativeBuildInputs = [
