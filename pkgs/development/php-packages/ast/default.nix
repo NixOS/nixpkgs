3c911109892943a08a2d8690d7beb3e96c8528d8
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-07-13 21:03:41 +0200

    php85Extensions.ast: mark as broken

--- pkgs/development/php-packages/ast/default.nix
+++ pkgs/development/php-packages/ast/default.nix
@@ -2,6 +2,7 @@
   buildPecl,
   lib,
   fetchFromGitHub,
+  php,
 }:
 
 let
@@ -24,5 +25,6 @@ buildPecl {
     license = lib.licenses.bsd3;
     homepage = "https://pecl.php.net/package/ast";
     teams = [ lib.teams.php ];
+    broken = lib.versionAtLeast php.version "8.5";
   };
 }
