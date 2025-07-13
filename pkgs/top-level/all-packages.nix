beef19b83eba802ffbbdb0eb2541a8a1b84f29f0
Majiir Paktu <majiir@nabaal.net>
2025-07-13 11:31:29 -0400

    tpm2-totp: add plymouth integration support

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4275,6 +4275,10 @@ with pkgs;
     }
   );
 
+  tpm2-totp-with-plymouth = tpm2-totp.override {
+    withPlymouth = true;
+  };
+
   trackma-curses = trackma.override { withCurses = true; };
 
   trackma-gtk = trackma.override { withGTK = true; };
