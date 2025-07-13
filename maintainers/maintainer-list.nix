4713038ba52e91d754eb2cc938af9cafafe19e5a
fumnanya <fmowete@outlook.com>
2025-07-13 14:00:39 +0100

    maintainers: update @poopsicles to @dibenzepin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6385,6 +6385,12 @@
     githubId = 15774340;
     name = "Thomas Depierre";
   };
+  dibenzepin = {
+    name = "Fumnanya";
+    email = "fmowete@outlook.com";
+    github = "dibenzepin";
+    githubId = 87488715;
+  };
   DictXiong = {
     email = "me@beardic.cn";
     github = "DictXiong";
@@ -20185,12 +20191,6 @@
     github = "pongo1231";
     githubId = 4201956;
     name = "pongo1231";
-  };
-  poopsicles = {
-    name = "Fumnanya";
-    email = "fmowete@outlook.com";
-    github = "poopsicles";
-    githubId = 87488715;
   };
   PopeRigby = {
     name = "PopeRigby";
