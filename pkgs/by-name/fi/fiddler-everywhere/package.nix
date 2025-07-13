b924e943271f37a54624950c190434863b3eab61
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-13 04:07:40 +0000

    fiddler-everywhere: 6.6.0 -> 7.0.0

--- pkgs/by-name/fi/fiddler-everywhere/package.nix
+++ pkgs/by-name/fi/fiddler-everywhere/package.nix
@@ -8,11 +8,11 @@
 
 let
   pname = "fiddler-everywhere";
-  version = "6.6.0";
+  version = "7.0.0";
 
   src = fetchurl {
     url = "https://downloads.getfiddler.com/linux/fiddler-everywhere-${version}.AppImage";
-    hash = "sha256-qSQx9uz8P6k/wdPpe7XwhQ6qCZp6IUoLOZnkDWMFS3E=";
+    hash = "sha256-pDXvzTuivQUjEr0mi/Bw98il6a1D7gJfQ5x5OuQu2wE=";
   };
 
   appimageContents = appimageTools.extract {
