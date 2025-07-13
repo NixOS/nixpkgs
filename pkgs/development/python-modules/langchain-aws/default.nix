2aefa2c2237e8bbe480da2dec17bf8bb1f61e17a
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-13 17:12:43 +0000

    python3Packages.langchain-aws: 0.2.27 -> 0.2.28

--- pkgs/development/python-modules/langchain-aws/default.nix
+++ pkgs/development/python-modules/langchain-aws/default.nix
@@ -24,14 +24,14 @@
 
 buildPythonPackage rec {
   pname = "langchain-aws";
-  version = "0.2.27";
+  version = "0.2.28";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langchain-aws";
     tag = "langchain-aws==${version}";
-    hash = "sha256-FHWozXf0zEyKvFODZ+8JHMiwARJETJxmLh3z1HJSNV4=";
+    hash = "sha256-sfdijQxcw0TNK1/IOmHQTHznDIMDTvXqMWBb58cTPlI=";
   };
 
   postPatch = ''
