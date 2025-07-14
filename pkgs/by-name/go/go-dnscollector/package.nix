90de7fab2e2356fc6ed9d84620c5ca97cb10ecfb
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-14 13:53:03 +0000

    go-dnscollector: 1.8.0 -> 1.9.0

--- pkgs/by-name/go/go-dnscollector/package.nix
+++ pkgs/by-name/go/go-dnscollector/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "go-dnscollector";
-  version = "1.8.0";
+  version = "1.9.0";
 
   src = fetchFromGitHub {
     owner = "dmachard";
     repo = "go-dnscollector";
     rev = "v${version}";
-    sha256 = "sha256-q12hMnSqA/KCkmiqsmBpvDmyHtuEWhMBTKwOOyw3Wfs=";
+    sha256 = "sha256-ebl/edMN45oLV1pN6mCaOSgxSSyAugsBP2sQWbIiPTI=";
   };
 
-  vendorHash = "sha256-TtlOwmNyO2/eQCajPBu6Pgdbuk4gacpgtcnr1vZgZdg=";
+  vendorHash = "sha256-Y0LOtyRJWOFAQwfg8roisSer0oCxPiaYICE1FY/SEF8=";
 
   subPackages = [ "." ];
 
