4fb1b2367e1411b4e93f7c43177e87a2a70f4b8b
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-14 20:08:01 +0000

    strongswanNM: 5.9.14 -> 6.0.2

--- pkgs/by-name/st/strongswan/package.nix
+++ pkgs/by-name/st/strongswan/package.nix
@@ -37,13 +37,13 @@
 
 stdenv.mkDerivation rec {
   pname = "strongswan";
-  version = "5.9.14"; # Make sure to also update <nixpkgs/nixos/modules/services/networking/strongswan-swanctl/swanctl-params.nix> when upgrading!
+  version = "6.0.2"; # Make sure to also update <nixpkgs/nixos/modules/services/networking/strongswan-swanctl/swanctl-params.nix> when upgrading!
 
   src = fetchFromGitHub {
     owner = "strongswan";
     repo = "strongswan";
     rev = version;
-    hash = "sha256-qFM7ErfqiDlUsZdGXJQVW3nJoh+I6tEdKRwzrKteRVY=";
+    hash = "sha256-wjz41gt+Xu4XJkEXRRVl3b3ryEoEtijeqmfVFoRjnA4=";
   };
 
   dontPatchELF = true;
