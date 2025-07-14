a17ad4644b81e54152278d6472f38b6962fe573e
Tony Wasserka <neobrain@users.noreply.github.com>
2025-07-14 10:20:02 +0200

    etesync-dav: 0.34.0 -> 0.35.0

--- pkgs/by-name/et/etesync-dav/package.nix
+++ pkgs/by-name/et/etesync-dav/package.nix
@@ -8,14 +8,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "etesync-dav";
-  version = "0.34.0";
+  version = "0.35.0";
   format = "pyproject";
 
   src = fetchFromGitHub {
     owner = "etesync";
     repo = "etesync-dav";
     tag = "v${version}";
-    hash = "sha256-+rNqyksOmDUh0OuvgEDWv6tuZQkn1gizz35Ptr6izos=";
+    hash = "sha256-CD02nuA9GD/oe7mjExUHIftkPxM1pZQKyDalXSoOhXY=";
   };
 
   build-system = with python3Packages; [ setuptools ];
