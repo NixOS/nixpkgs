6f2255ec3de7558576b3d1655d7f60a718fcf543
PandapipBot <{{ env.EMAIL }}>
2025-07-14 14:16:40 +0000

    stalwart-mail: 0.12.4 -> 0.12.5

--- pkgs/by-name/st/stalwart-mail/package.nix
+++ pkgs/by-name/st/stalwart-mail/package.nix
@@ -20,17 +20,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "stalwart-mail" + (lib.optionalString stalwartEnterprise "-enterprise");
-  version = "0.12.4";
+  version = "0.12.5";
 
   src = fetchFromGitHub {
     owner = "stalwartlabs";
     repo = "stalwart";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-MUbWGBbb8+b5cp+M5w27A/cHHkMcoEtkN13++FyBvbM=";
+    hash = "sha256-+CdV9gRWx732cMZS+hrPMbBb4IDx6K6HjXP3lxj4N3E=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-G1c7hh0nScc4Cx7A1UUXv6slA6pP0fC6h00zR71BJIo=";
+  cargoHash = "sha256-62aPJdqiw7+9ilmULCPuGtfzembxKYwuZyGrgQX3xCE=";
 
   nativeBuildInputs = [
     pkg-config
