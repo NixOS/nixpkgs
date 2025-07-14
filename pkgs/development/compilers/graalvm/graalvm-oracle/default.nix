4b09641854c0b8afd83ba9d2dd42417eb4dd85c6
Sean Gilligan <sean@msgilligan.com>
2025-07-13 23:37:32 -0700

    graalvm-oracle_24: nix fmt
    Fix Nix formatting issue that is failing GitHub Actions checks.
    This should be squashed into PR #423224

--- pkgs/development/compilers/graalvm/graalvm-oracle/default.nix
+++ pkgs/development/compilers/graalvm/graalvm-oracle/default.nix
@@ -8,16 +8,24 @@
   version ? "24",
 }:
 
-let graal = (graalvmPackages.buildGraalvm {
-  inherit useMusl version;
-  src = fetchurl (import ./hashes.nix).${version}.${stdenv.system};
-  meta.platforms = builtins.attrNames (import ./hashes.nix).${version};
-  meta.license = lib.licenses.unfree;
-  pname = "graalvm-oracle";
-}).overrideAttrs (prev: {
-  propagatedBuildInputs = (prev.propagatedBuildInputs or []) ++ [ onnxruntime ];
-  postFixup = (prev.postFixup or "") + ''
-    patchelf --replace-needed libonnxruntime.so.1.18.0 libonnxruntime.so.1 $out/lib/svm/profile_inference/onnx/native/libonnxruntime4j_jni.so
-  '';
-});
-in graal.overrideAttrs (prev: { passthru.home = graal; })
+let
+  graal =
+    (graalvmPackages.buildGraalvm {
+      inherit useMusl version;
+      src = fetchurl (import ./hashes.nix).${version}.${stdenv.system};
+      meta.platforms = builtins.attrNames (import ./hashes.nix).${version};
+      meta.license = lib.licenses.unfree;
+      pname = "graalvm-oracle";
+    }).overrideAttrs
+      (prev: {
+        propagatedBuildInputs = (prev.propagatedBuildInputs or [ ]) ++ [ onnxruntime ];
+        postFixup =
+          (prev.postFixup or "")
+          + ''
+            patchelf --replace-needed libonnxruntime.so.1.18.0 libonnxruntime.so.1 $out/lib/svm/profile_inference/onnx/native/libonnxruntime4j_jni.so
+          '';
+      });
+in
+graal.overrideAttrs (prev: {
+  passthru.home = graal;
+})
