06f2d67e3a71ecd9bede10c00cf6e05606a29318
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-14 04:18:05 +0000

    ibus-engines.typing-booster-unwrapped: 2.27.67 -> 2.27.68

--- pkgs/tools/inputmethods/ibus-engines/ibus-typing-booster/default.nix
+++ pkgs/tools/inputmethods/ibus-engines/ibus-typing-booster/default.nix
@@ -25,13 +25,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "ibus-typing-booster";
-  version = "2.27.67";
+  version = "2.27.68";
 
   src = fetchFromGitHub {
     owner = "mike-fabian";
     repo = "ibus-typing-booster";
     rev = version;
-    hash = "sha256-DIezuI8pexIzqiGWFrgQQER1wx0jCwTZgcluFCBvpCw=";
+    hash = "sha256-jDBm6fo/dwE41aNH8CmpqJo8ZyPblMd4DQqxo5C0J8w=";
   };
 
   nativeBuildInputs = [
