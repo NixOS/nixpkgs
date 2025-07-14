5d4ae01a2fb646ca276bd9e46e5034fbb7db4184
teutat3s <10206665+teutat3s@users.noreply.github.com>
2025-07-14 10:35:52 +0200

    grafanaPlugins.grafana-lokiexplore-app: 1.0.13 -> 1.0.22

--- pkgs/servers/monitoring/grafana/plugins/grafana-lokiexplore-app/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-lokiexplore-app/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "grafana-lokiexplore-app";
-  version = "1.0.13";
-  zipHash = "sha256-oTiwvkKiKpeI7MUxyaRuxXot4UhMeSvuJh0N1VIfA5Q=";
+  version = "1.0.22";
+  zipHash = "sha256-y1WJ1RxUbJSsiSApz3xvrARefNnXdZxDVfzeGfDZbFo=";
   meta = with lib; {
     description = "The Grafana Logs Drilldown app offers a queryless experience for browsing Loki logs without the need for writing complex queries.";
     license = licenses.agpl3Only;
