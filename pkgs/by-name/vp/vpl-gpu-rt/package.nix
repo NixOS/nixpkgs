652ade036dc1c543d9baf0dd9c433dc0dd2113a4
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-15 02:34:40 +0000

    vpl-gpu-rt: 25.2.5 -> 25.2.6

--- pkgs/by-name/vp/vpl-gpu-rt/package.nix
+++ pkgs/by-name/vp/vpl-gpu-rt/package.nix
@@ -10,7 +10,7 @@
 
 stdenv.mkDerivation rec {
   pname = "vpl-gpu-rt";
-  version = "25.2.5";
+  version = "25.2.6";
 
   outputs = [
     "out"
@@ -21,7 +21,7 @@ stdenv.mkDerivation rec {
     owner = "intel";
     repo = "vpl-gpu-rt";
     rev = "intel-onevpl-${version}";
-    hash = "sha256-BK0LH1DeCtx/Ew3j6Yo2/xfiCtTtMxqKflmu9OwO/m0=";
+    hash = "sha256-Le21JODzWgJNLWojGyoYeyJUfi8zk2JNC4C27PC3RlA=";
   };
 
   nativeBuildInputs = [
