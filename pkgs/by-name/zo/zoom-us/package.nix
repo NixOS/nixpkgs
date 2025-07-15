1b1cb045a4e2f5f830488b5119707a83a2aa328b
Yarny0 <41838844+Yarny0@users.noreply.github.com>
2025-07-15 15:01:12 +0200

    zoom-us: maintainers := philiptaron & ryan2yin & yarny

--- pkgs/by-name/zo/zoom-us/package.nix
+++ pkgs/by-name/zo/zoom-us/package.nix
@@ -152,8 +152,9 @@ let
       license = lib.licenses.unfree;
       platforms = builtins.attrNames srcs;
       maintainers = with lib.maintainers; [
-        danbst
-        tadfisher
+        philiptaron
+        ryan4yin
+        yarny
       ];
       mainProgram = "zoom";
     };
