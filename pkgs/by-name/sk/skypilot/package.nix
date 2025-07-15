f7864bb0a7d0ede858ed8f9a16574416d1fdf813
Carl Thomé <carlthome@gmail.com>
2025-07-15 16:18:32 +0200

    python3Packages.skypilot: 0.8.1 -> 0.10.0

--- pkgs/by-name/sk/skypilot/package.nix
+++ pkgs/by-name/sk/skypilot/package.nix
@@ -6,43 +6,188 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "skypilot";
-  version = "0.8.1";
+  version = "0.10.0";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "skypilot-org";
     repo = "skypilot";
     tag = "v${version}";
-    hash = "sha256-jLjYsBkb5Tba3q/mdqCWK04FLg0pEdHyZH3vuMIP6tg=";
+    hash = "sha256-HOguSCe9YbmAqvZCiUHOeNIWH4DofmrxWoW8yDSCah8=";
   };
 
-  pyproject = true;
+  build-system = [
+    python3Packages.setuptools
+  ];
 
-  build-system = with python3Packages; [ setuptools ];
-
-  # when updating, please ensure package version constraints stipulaed
-  # in setup.py are met
-  propagatedBuildInputs = with python3Packages; [
+  dependencies = with python3Packages; [
+    aiofiles
     cachetools
+    casbin
     click
     colorama
     cryptography
+    fastapi
     filelock
+    httpx
     jinja2
     jsonschema
     networkx
     packaging
     pandas
+    passlib
     pendulum
     prettytable
+    prometheus-client
     psutil
-    python-dotenv
-    pyyaml
+    psycopg2-binary
     pulp
+    pydantic
+    pyjwt
+    python-dotenv
+    python-multipart
+    pyyaml
     requests
     rich
+    setproctitle
+    sqlalchemy
+    sqlalchemy-adapter
     tabulate
     typing-extensions
+    uvicorn
     wheel
+  ];
+
+  optional-dependencies = with python3Packages; {
+    all = [
+      awscli
+      azure-cli
+      azure-common
+      azure-core
+      azure-identity
+      azure-mgmt-compute
+      azure-mgmt-network
+      azure-storage-blob
+      boto3
+      botocore
+      casbin
+      colorama
+      cudo-compute
+      docker
+      google-api-python-client
+      google-cloud-storage
+      grpcio
+      ibm-cloud-sdk-core
+      ibm-cos-sdk
+      ibm-platform-services
+      ibm-vpc
+      kubernetes
+      msgraph-sdk
+      nebius
+      oci
+      passlib
+      protobuf
+      pydo
+      pyjwt
+      pyopenssl
+      pyvmomi
+      ray
+      runpod
+      sqlalchemy-adapter
+      vastai-sdk
+      websockets
+    ];
+    aws = [
+      awscli
+      boto3
+      botocore
+      colorama
+    ];
+    azure = [
+      azure-cli
+      azure-core
+      azure-identity
+      azure-mgmt-compute
+      azure-mgmt-network
+      azure-storage-blob
+      msgraph-sdk
+      ray
+    ];
+    cloudflare = [
+      awscli
+      boto3
+      botocore
+      colorama
+    ];
+    cudo = [
+      cudo-compute
+    ];
+    do = [
+      azure-common
+      azure-core
+      pydo
+    ];
+    docker = [
+      docker
+      ray
+    ];
+    gcp = [
+      google-api-python-client
+      google-cloud-storage
+      pyopenssl
+    ];
+    ibm = [
+      ibm-cloud-sdk-core
+      ibm-cos-sdk
+      ibm-platform-services
+      ibm-vpc
+      ray
+    ];
+    kubernetes = [
+      kubernetes
+      websockets
+    ];
+    nebius = [
+      awscli
+      boto3
+      botocore
+      colorama
+      nebius
+    ];
+    oci = [
+      oci
+      ray
+    ];
+    remote = [
+      grpcio
+      protobuf
+    ];
+    runpod = [
+      runpod
+    ];
+    scp = [
+      ray
+    ];
+    server = [
+      casbin
+      passlib
+      pyjwt
+      sqlalchemy-adapter
+    ];
+    ssh = [
+      kubernetes
+      websockets
+    ];
+    vast = [
+      vastai-sdk
+    ];
+    vsphere = [
+      pyvmomi
+    ];
+  };
+
+  pythonImportsCheck = [
+    "skypilot"
   ];
 
   meta = {
@@ -52,9 +197,12 @@ python3Packages.buildPythonApplication rec {
       cloud, offering maximum cost savings, highest GPU availability, and
       managed execution.
     '';
-    homepage = "https://github.com/skypilot-org/skypilot";
+    homepage = "https://pypi.org/project/skypilot/";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ seanrmurphy ];
+    maintainers = with lib.maintainers; [
+      seanrmurphy
+      carlthome
+    ];
     mainProgram = "sky";
   };
 }
