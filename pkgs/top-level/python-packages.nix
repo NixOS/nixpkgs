c5e973b53eaa1f650ad5440bb1033f2e6d26682d
GueLaKais <koroyeldiores@gmail.com>
2025-07-15 09:33:16 +0200

    python313Packages.dearpygui: init at 2.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3750,6 +3750,8 @@ self: super: with self; {
 
   deap = callPackage ../development/python-modules/deap { };
 
+  dearpygui = callPackage ../development/python-modules/dearpygui { };
+
   debian-inspector = callPackage ../development/python-modules/debian-inspector { };
 
   debianbts = callPackage ../development/python-modules/debianbts { };
