35505ad6716b0be7fe5ed507fc8aeb3745d54a24
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-15 03:49:57 +0000

    ktlint: 1.6.0 -> 1.7.0

--- pkgs/by-name/kt/ktlint/package.nix
+++ pkgs/by-name/kt/ktlint/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ktlint";
-  version = "1.6.0";
+  version = "1.7.0";
 
   src = fetchurl {
     url = "https://github.com/pinterest/ktlint/releases/download/${version}/ktlint";
-    sha256 = "sha256-W6GskXoGsPAtqqYNEKu+3SIg1gIWr2cMZ6RbkcdM+Ls=";
+    sha256 = "sha256-qH7TYYzshNqg+jbeBe8M/v08BM5bM+WlOD8hLFymito=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
