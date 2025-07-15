e40a297c4105953dbdc7d83b5535cde2b4d4f0fe
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-15 05:27:20 +0000

    fizz: 2025.04.21.00 -> 2025.07.14.00

--- pkgs/by-name/fi/fizz/package.nix
+++ pkgs/by-name/fi/fizz/package.nix
@@ -26,7 +26,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fizz";
-  version = "2025.04.21.00";
+  version = "2025.07.14.00";
 
   outputs = [
     "bin"
@@ -38,7 +38,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "facebookincubator";
     repo = "fizz";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-khaUbxcD8+9zznH0DE/BpweZeDKafTnr4EqPbmOpckU=";
+    hash = "sha256-p5VmFF5VDlHXbM2+82BhJdOUK1T6H69RPJQzXAi7oY0=";
   };
 
   patches = [
