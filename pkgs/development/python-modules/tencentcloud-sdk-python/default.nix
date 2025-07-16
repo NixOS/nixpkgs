7343d4a4b665bcdbe512b774e0242a060d064ed9
Fabian Affolter <fabian@affolter-engineering.ch>
2025-07-16 23:37:53 +0200

    python312Packages.tencentcloud-sdk-python: 3.0.1423 -> 3.0.1424
    Diff: https://github.com/TencentCloud/tencentcloud-sdk-python/compare/refs/tags/3.0.1423...refs/tags/3.0.1424
    
    Changelog: https://github.com/TencentCloud/tencentcloud-sdk-python/blob/3.0.1424/CHANGELOG.md

--- pkgs/development/python-modules/tencentcloud-sdk-python/default.nix
+++ pkgs/development/python-modules/tencentcloud-sdk-python/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "tencentcloud-sdk-python";
-  version = "3.0.1423";
+  version = "3.0.1424";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "TencentCloud";
     repo = "tencentcloud-sdk-python";
     tag = version;
-    hash = "sha256-HITx60SRPAXKdVCl3jMq+AknGl5Su6S0whWuPOTRIMU=";
+    hash = "sha256-2febGkP4zm2dmOslHcLmcKRfn1EFJ5O/Ywn9XrouMpw=";
   };
 
   build-system = [ setuptools ];
