dae9a439385c43af2aa1a184353670a590dbde0b
Jan van Brügge <supermanitu@gmail.com>
2025-07-16 15:27:37 +0100

    nixos/booklore: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1558,6 +1558,7 @@
   ./services/web-apps/baikal.nix
   ./services/web-apps/bluemap.nix
   ./services/web-apps/bluesky-pds.nix
+  ./services/web-apps/booklore.nix
   ./services/web-apps/bookstack.nix
   ./services/web-apps/c2fmzq-server.nix
   ./services/web-apps/calibre-web.nix
