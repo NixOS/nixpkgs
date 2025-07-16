3f48f88bfa9f525bdd20a6870fa20e2652b6dcb4
Kylie McClain <kylie@somas.is>
2025-07-16 01:38:56 -0400

    radiotray-ng: 0.2.8 -> 0.2.9

--- pkgs/applications/audio/radiotray-ng/default.nix
+++ pkgs/applications/audio/radiotray-ng/default.nix
@@ -51,13 +51,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "radiotray-ng";
-  version = "0.2.8";
+  version = "0.2.9";
 
   src = fetchFromGitHub {
     owner = "ebruck";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-/0GlQdSsIPKGrDT9CgxvaH8TpAbqxFduwL2A2+BSrEI=";
+    hash = "sha256-vGxDwU2E6VAKxydbS2YTBgkBWcJAQ0R5bltidd5CUHE=";
   };
 
   nativeBuildInputs = [
@@ -89,11 +89,6 @@ stdenv.mkDerivation rec {
 
   patches = [
     ./no-dl-googletest.patch
-    (fetchpatch {
-      name = "gcc13-fixes.patch";
-      url = "https://github.com/ebruck/radiotray-ng/commit/7a99bfa784f77be8f160961d25ab63dc2d5ccde0.patch";
-      hash = "sha256-7x3v0dp9WPgd/vsnxezgXIZGsBrIHkTwIiu+FMlLmyA=";
-    })
   ];
 
   postPatch = ''
@@ -113,22 +108,45 @@ stdenv.mkDerivation rec {
     "-DBUILD_TESTS=${if doCheck then "ON" else "OFF"}"
   ];
 
-  # 'wxFont::wxFont(int, int, int, int, bool, const wxString&, wxFontEncoding)' is deprecated
-  env.NIX_CFLAGS_COMPILE = "-Wno-error=deprecated-declarations";
+  # NOTE gtest >=1.17 now requires C++17, but this workaround is insufficient
+  # and it breaks tests compilation, even if you just restrict -std=c++17 to
+  # the tests/ subdirectory. It seems to require an upstream fix.
+  # > In file included from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gtest-1.17.0-dev/include/gtest/gtest-printers.h:122,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gtest-1.17.0-dev/include/gtest/gtest-matchers.h:49,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gtest-1.17.0-dev/include/gtest/internal/gtest-death-test-internal.h:47,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gtest-1.17.0-dev/include/gtest/gtest-death-test.h:43,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gtest-1.17.0-dev/include/gtest/gtest.h:64,
+  # >                  from /build/source/tests/bookmarks_test.cpp:20:
+  # > /build/source/tests/bookmarks_test.cpp: In member function 'virtual void Bookmarks_test_that_stations_are_added_and_removed_from_a_group_and_moved_Test::TestBody()':
+  # > /build/source/tests/bookmarks_test.cpp:218:39: error: ignoring return value of 'std::vector<_Tp, _Alloc>::reference std::vector<_Tp, _Alloc>::at(size_type) [with _Tp = IBookmarks::<unnamed struct>; _Alloc = std::allocator<IBookmarks::<unnamed struct> >; reference = IBookmarks::<unnamed struct>&; size_type = long unsigned int]', declared with attribute 'nodiscard' [-Werror=unused-result]
+  # >   218 |         EXPECT_THROW(bm[0].stations.at(100), std::out_of_range);
+  # >       |                      ~~~~~~~~~~~~~~~~~^~~~~
+  # > In file included from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gcc-14.3.0/include/c++/14.3.0/vector:66,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gcc-14.3.0/include/c++/14.3.0/functional:64,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-boost-1.87.0-dev/include/boost/parameter/aux_/unwrap_cv_reference.hpp:38,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-boost-1.87.0-dev/include/boost/parameter/aux_/tag.hpp:10,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-boost-1.87.0-dev/include/boost/parameter/keyword.hpp:10,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-boost-1.87.0-dev/include/boost/log/keywords/severity.hpp:18,
+  # >                  from /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-boost-1.87.0-dev/include/boost/log/trivial.hpp:22,
+  # >                  from /build/source/include/radiotray-ng/common.hpp:20,
+  # >                  from /build/source/src/radiotray-ng/bookmarks/bookmarks.hpp:20,
+  # >                  from /build/source/tests/bookmarks_test.cpp:18:
+  # > /nix/store/eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee-gcc-14.3.0/include/c++/14.3.0/bits/stl_vector.h:1193:7: note: declared here
 
+  env.NIX_CFLAGS_COMPILE = "-std=c++17";
   nativeCheckInputs = [ gtest ];
-  doCheck = !stdenv.hostPlatform.isAarch64; # single failure that I can't explain
+  doCheck = false;
 
   preFixup = ''
     gappsWrapperArgs+=(--suffix PATH : ${lib.makeBinPath [ dbus ]})
     wrapProgram $out/bin/rt2rtng --prefix PYTHONPATH : $PYTHONPATH
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Internet radio player for linux";
     homepage = "https://github.com/ebruck/radiotray-ng";
-    license = licenses.gpl3;
+    license = lib.licenses.gpl3;
     maintainers = [ ];
-    platforms = platforms.linux;
+    platforms = lib.platforms.linux;
   };
 }
