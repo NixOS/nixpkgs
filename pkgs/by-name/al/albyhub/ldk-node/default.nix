a02d3ae835db88b53680cae11b71544799c1a476
Brian Lee <blee@satstack.cloud>
2025-07-16 15:05:29 -0700

    albyhub: 1.17.2 -> 1.18.2

--- pkgs/by-name/al/albyhub/ldk-node/default.nix
+++ pkgs/by-name/al/albyhub/ldk-node/default.nix
@@ -8,19 +8,19 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "ldk-node";
-  version = "0-unstable-2025-05-21";
+  version = "0-unstable-2025-06-30";
 
   src = fetchFromGitHub {
     owner = "getAlby";
     repo = "ldk-node";
-    rev = "9bb381ad38dbaa71e17816738789d993158fc1a2";
-    hash = "sha256-Ie7FOSOd12mwmkEjD4r0p1ZmeYkXm5eN1LlQhWl0VG4=";
+    rev = "b00326e36445312a55f9fb583a8b54cc5763b6bf";
+    hash = "sha256-SDuHM7aawLXGSUxsKMlU0iH5+xs35VULX4vDGkS6xMA=";
   };
 
   buildFeatures = [ "uniffi" ];
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-hM6VCU0kIg5ZmJM8C4HoybSc/VXNj6GTE/oFLfqGMcY=";
+  cargoHash = "sha256-ONlN5xMU4A7ZTF4+XRbs5qAv8xQVYuMsU0bnD8Eh2gE=";
 
   # Skip tests because they download bitcoin-core and electrs zip files, and then fail
   doCheck = false;
