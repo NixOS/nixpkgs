c361e483a1e5c9f34e84bd838b42f6f65db7d670
George Huebner <george@feyor.sh>
2025-07-16 18:32:11 -0400

    python3Packages.abnf: init at 2.3.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -37,6 +37,8 @@ self: super: with self; {
 
   abjad = callPackage ../development/python-modules/abjad { };
 
+  abnf = callPackage ../development/python-modules/abnf { };
+
   about-time = callPackage ../development/python-modules/about-time { };
 
   absl-py = callPackage ../development/python-modules/absl-py { };
