581c395a580872904642cdddef3975a89b008f02
Doron Behar <doron.behar@gmail.com>
2025-07-17 10:01:51 +0300

    python313Packages.zarr: 3.0.4 -> 3.1.0
    Changelog: https://github.com/zarr-developers/zarr-python/releases/tag/v3.1.0

--- pkgs/development/python-modules/zarr/default.nix
+++ pkgs/development/python-modules/zarr/default.nix
@@ -25,18 +25,21 @@
   fsspec,
   moto,
   requests,
+  tomlkit,
+  uv,
+  writableTmpDirAsHomeHook,
 }:
 
 buildPythonPackage rec {
   pname = "zarr";
-  version = "3.0.4";
+  version = "3.1.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-br5tZdH26vteu45DT8bld7DQwDOv6/G7XNHv1GeU05g=";
+    hash = "sha256-rOWxEdxp1TFcsWVd/Q+BbFrPl5jSrZL0O2CKUsjIrCs=";
   };
 
   build-system = [
@@ -51,31 +54,40 @@ buildPythonPackage rec {
     fasteners
     numcodecs
     typing-extensions
-  ] ++ numcodecs.optional-dependencies.crc32c;
+  ]
+  ++ numcodecs.optional-dependencies.crc32c;
 
   optional-dependencies = {
     remote = [ fsspec ];
   };
 
-  nativeCheckInputs =
-    [
-      pytestCheckHook
-      pytest-asyncio
-      pytest-cov-stub
-      hypothesis
-      aiohttp
-      moto
-      requests
-    ]
-    ++ moto.optional-dependencies.s3
-    ++ moto.optional-dependencies.server
-    ++ optional-dependencies.remote;
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-asyncio
+    pytest-cov-stub
+    hypothesis
+    aiohttp
+    moto
+    requests
+    tomlkit
+    uv
+    writableTmpDirAsHomeHook
+  ]
+  ++ moto.optional-dependencies.s3
+  ++ moto.optional-dependencies.server
+  ++ optional-dependencies.remote;
   pytestFlagsArray = [
     # Don't measure the time it takes for hypothesis related tests to succeed.
     # See https://github.com/astropy/astropy/issues/17649 for a similar
     # discussion, and see:
     # https://github.com/zarr-developers/zarr-python/blob/v3.0.4/tests/conftest.py#L182C1-L187C2
     "--hypothesis-profile=ci"
+  ];
+  disabledTests = [
+    # 3 tests that require multiple Python versions to co-exist
+    "test_scripts_can_run"
+    "test_roundtrip_v2"
+    "test_roundtrip_v3"
   ];
 
   pythonImportsCheck = [ "zarr" ];
