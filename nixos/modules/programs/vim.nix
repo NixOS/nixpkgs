eb80016e4a77389cd5953c756e7e45232c72428c
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-07-17 01:25:53 -0400

    programs.vim: enforce enable setting
    Caught by manual code review.
    
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- nixos/modules/programs/vim.nix
+++ nixos/modules/programs/vim.nix
@@ -17,11 +17,7 @@ in
     package = lib.mkPackageOption pkgs "vim" { example = [ "vim-full" ]; };
   };
 
-  # TODO: convert it into assert after 24.11 release
-  config = lib.mkIf (cfg.enable || cfg.defaultEditor) {
-    warnings = lib.mkIf (cfg.defaultEditor && !cfg.enable) [
-      "programs.vim.defaultEditor will only work if programs.vim.enable is enabled, which will be enforced after the 24.11 release"
-    ];
+  config = lib.mkIf cfg.enable {
     environment = {
       systemPackages = [ cfg.package ];
       variables.EDITOR = lib.mkIf cfg.defaultEditor (lib.mkOverride 900 "vim");
