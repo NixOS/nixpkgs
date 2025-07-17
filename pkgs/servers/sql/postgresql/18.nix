53860c1e156009d0e83ab80c1431220b2f7ce1a4
Wolfgang Walther <walther@technowledgy.de>
2025-07-17 16:28:40 +0200

    postgresql_18: 18beta1 -> 18beta2
    Release notes:
    https://www.postgresql.org/about/news/postgresql-18-beta-2-released-3103/

--- pkgs/servers/sql/postgresql/18.nix
+++ pkgs/servers/sql/postgresql/18.nix
@@ -1,7 +1,7 @@
 import ./generic.nix {
-  version = "18beta1";
-  rev = "refs/tags/REL_18_BETA1";
-  hash = "sha256-P86bVYfPzkwK/rcvUInYZew/pKejk58/IcnDGx/BWno=";
+  version = "18beta2";
+  rev = "refs/tags/REL_18_BETA2";
+  hash = "sha256-n3NA0XJE2wvbOwrQOMXbzKn+7HLGAQSXgDU9ObhddZQ=";
   muslPatches = {
     dont-use-locale-a = {
       url = "https://git.alpinelinux.org/aports/plain/main/postgresql17/dont-use-locale-a-on-musl.patch?id=d69ead2c87230118ae7f72cef7d761e761e1f37e";
