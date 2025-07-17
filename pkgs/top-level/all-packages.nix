0110354675dcd0be2690e17eafe9e039549583b9
Philip Taron <philip.taron@gmail.com>
2025-07-16 21:50:18 -0700

    ensureNewerSourcesHook: use callPackage

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -220,25 +220,7 @@ with pkgs;
 
   appimageupdate-qt = appimageupdate.override { withQtUI = true; };
 
-  ensureNewerSourcesHook =
-    { year }:
-    makeSetupHook
-      {
-        name = "ensure-newer-sources-hook";
-      }
-      (
-        writeScript "ensure-newer-sources-hook.sh" ''
-          postUnpackHooks+=(_ensureNewerSources)
-          _ensureNewerSources() {
-            local r=$sourceRoot
-            # Avoid passing option-looking directory to find. The example is diffoscope-269:
-            #   https://salsa.debian.org/reproducible-builds/diffoscope/-/issues/378
-            [[ $r == -* ]] && r="./$r"
-            '${findutils}/bin/find' "$r" \
-              '!' -newermt '${year}-01-01' -exec touch -h -d '${year}-01-02' '{}' '+'
-          }
-        ''
-      );
+  ensureNewerSourcesHook = callPackage ../build-support/setup-hooks/ensure-newer-sources { };
 
   alacritty-graphics = callPackage ../by-name/al/alacritty/package.nix { withGraphics = true; };
 
