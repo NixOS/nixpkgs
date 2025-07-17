40df6d5813ed6ebb809a77416bbb1ddfbeedd846
Vladimír Čunát <v@cunat.cz>
2025-07-17 13:22:11 +0200

    systemd.disallowedReferences: avoid `null` values
    hydra-queue-runner complains like this:
      loading build 302783324 (nixpkgs:cross-trunk:rpi-musl.mpg123.x86_64-linux)
      queue monitor: error:
             … while loading build 302783324:
             … while parsing derivation '/nix/store/8r46dmzzchv9jhb09y8dlih4h5k6smkm-systemd-minimal-armv6l-unknown-linux-musleabihf-257.6.drv'
             error: attribute 'disallowedReferences' must be a list of strings
      checking the queue for builds...
      loading build 302783295 (nixpkgs:cross-trunk:rpi-musl.mpg123.aarch64-darwin)

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -883,7 +883,11 @@ stdenv.mkDerivation (finalAttrs: {
   disallowedReferences =
     lib.optionals (stdenv.buildPlatform != stdenv.hostPlatform)
       # 'or p' is for manually specified buildPackages as they dont have __spliced
-      (builtins.map (p: p.__spliced.buildHost or p) finalAttrs.nativeBuildInputs);
+      (
+        builtins.map (p: p.__spliced.buildHost or p) (
+          builtins.filter (p: p != null) finalAttrs.nativeBuildInputs
+        )
+      );
 
   passthru = {
     # The `interfaceVersion` attribute below points out the incompatibilities
