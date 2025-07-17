0220aeb76f5a52ed4c856e43a6f2f271722cfb0a
sdubey-ks <sdubey@keepersecurity.com>
2025-07-17 19:02:28 +0530

    maintainers: add sdubey-ks

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22701,6 +22701,12 @@
     githubId = 11613056;
     name = "Scott Dier";
   };
+  sdubey-ks = {
+    email = "sdubey@keepersecurity.com";
+    github = "sdubey-ks";
+    githubId = 221318348;
+    name = "Sharad Dubey";
+  };
   seanrmurphy = {
     email = "sean@gopaddy.ch";
     github = "seanrmurphy";
