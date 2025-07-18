cb331467bcfc5763c64753d960b5986ffad56157
mivorasu <mivorasu@users.noreply.github.com>
2025-07-18 15:25:41 +0800

    maintainers: add tuynia

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25985,6 +25985,11 @@
     githubId = 57819359;
     name = "Binh Nguyen";
   };
+  tuynia = {
+    github = "mivorasu";
+    githubId = 221005165;
+    name = "Qylia Vorlaque";
+  };
   tv = {
     email = "tv@krebsco.de";
     github = "4z3";
