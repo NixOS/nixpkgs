9ccd8fde1e4bcfad55e794350b2a3c46a437880a
znaniye <zn4niye@proton.me>
2025-07-18 16:32:13 -0300

    renode-unstable: init at 1.15.3-unstable-2025-07-18

---
+++ pkgs/by-name/re/renode-unstable/package.nix
@@ -0,0 +1,46 @@
+{
+  callPackage,
+  dotnet-runtime_8,
+  dotnet-sdk_6,
+  nix-update-script,
+  ...
+}:
+let
+  tlib = callPackage ./tlib.nix { };
+in
+(callPackage ../renode/common.nix {
+  dotnet-sdk = dotnet-sdk_6;
+  dotnet-runtime = dotnet-runtime_8;
+  version = "1.15.3-unstable-2025-07-18";
+  nugetDeps = ./deps.json;
+  rev = "2309db7fdb58e8d9d906ed9000dbd08bbcbdae4e";
+  hash = "sha256-et0W0BZ5c+Dwu8FAbiDDdHFi14oy40UTcDL6F0kmTZk=";
+  projectFile = "Renode_NET.sln";
+}).overrideAttrs
+  (old: {
+    postPatch = ''
+      ${old.postPatch}
+
+      # To fix value "" error in element <Import>
+      rm -rf src/Directory.Build.targets
+    '';
+
+    buildInputs = old.buildInputs ++ [ tlib ];
+
+    preBuild = ''
+      ${old.preBuild}
+
+      mkdir -p src/Infrastructure/src/Emulator/Cores/bin/Release/lib
+      ln -s ${tlib}/lib/*.so src/Infrastructure/src/Emulator/Cores/bin/Release/lib
+    '';
+
+    passthru = old.passthru // {
+      updateScript = nix-update-script {
+        extraArgs = [
+          "--version=branch"
+          "--override-filename"
+          "pkgs/by-name/re/renode-unstable/package.nix"
+        ];
+      };
+    };
+  })
