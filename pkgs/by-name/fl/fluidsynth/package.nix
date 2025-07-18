1986cfb231fb9e40851c4426bc91a7ff5c9d7d21
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-18 20:23:12 +0000

    fluidsynth: 2.4.6 -> 2.4.7

--- pkgs/by-name/fl/fluidsynth/package.nix
+++ pkgs/by-name/fl/fluidsynth/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fluidsynth";
-  version = "2.4.6";
+  version = "2.4.7";
 
   src = fetchFromGitHub {
     owner = "FluidSynth";
     repo = "fluidsynth";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-hy2kWJmvvmItXl05Nw9gWEYpoDg+NtP2veO2vZ00QhI=";
+    hash = "sha256-z7DIX8KpPdtEHEMoxH7ewW32aXm27gfmWPZawwITyRQ=";
   };
 
   outputs = [
