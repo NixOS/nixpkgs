52f3bf09d78c9d72c4a9495db85d8f7cf749b3e1
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-19 01:04:29 +0200

    jitsi-meet: 1.0.8043 -> 1.0.8726, from GitHub

--- pkgs/by-name/ji/jitsi-meet/package.nix
+++ pkgs/by-name/ji/jitsi-meet/package.nix
@@ -1,26 +1,50 @@
 {
   lib,
   stdenv,
-  fetchurl,
   nixosTests,
+
+  fetchFromGitHub,
+  fetchNpmDeps,
+  npmHooks,
+
+  libarchive,
+  nodejs,
+
   olm,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "jitsi-meet";
-  version = "1.0.8043";
+  version = "1.0.8726";
 
-  src = fetchurl {
-    url = "https://download.jitsi.org/jitsi-meet/src/jitsi-meet-${version}.tar.bz2";
-    sha256 = "XJlfCMQXnHjfHQhK916RXsdPzrU2U2IaOMiXIHL1sCI=";
+  src = fetchFromGitHub {
+    owner = "jitsi";
+    repo = "jitsi-meet";
+    tag = lib.last (lib.splitVersion finalAttrs.version);
+    hash = "sha256-cRIkawlzlsGVVuBxkYlzdJWU6kqjepZ7KV567Gq1sx0=";
   };
 
-  dontBuild = true;
+  env = {
+    makeFlags = "source-package";
+    makeCacheWritable = true;
+    npmDeps = fetchNpmDeps {
+      inherit (finalAttrs) src;
+      hash = "sha256-6aquQE3jWhpjv+wV/v5xccSBv24yZA+n2dIOqGTDyj8=";
+    };
+  };
 
+  nativeBuildInputs = [
+    libarchive
+    nodejs
+    npmHooks.npmConfigHook
+  ];
+
+  # yes, the only way in the build system is to generate a tarball and extract
+  # it immediately after
   installPhase = ''
     runHook preInstall
-    mkdir $out
-    mv * $out/
+    mkdir -p $out
+    bsdtar -xf jitsi-meet.tar.bz2 -C $out --strip-components 1
     runHook postInstall
   '';
 
@@ -43,4 +67,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.all;
     inherit (olm.meta) knownVulnerabilities;
   };
-}
+})
