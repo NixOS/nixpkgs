5deeec2a14e776e6f26081eeba102657c3a87b74
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-07-18 19:36:40 +0200

    glab: show short commit id when doing `glab version`

--- pkgs/by-name/gl/glab/package.nix
+++ pkgs/by-name/gl/glab/package.nix
@@ -16,7 +16,13 @@ buildGoModule (finalAttrs: {
     owner = "gitlab-org";
     repo = "cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-pouFnmHCfRFDy+MMbRI82o73jO8hRewHeXgGnJ4hnww=";
+    hash = "sha256-LM1doLLVdY7gOwfYfjwT/aFzVkyXxoD4/WPXJGqoPmw=";
+    leaveDotGit = true;
+    postFetch = ''
+      cd "$out"
+      git rev-parse --short HEAD > $out/COMMIT
+      find "$out" -name .git -print0 | xargs -0 rm -rf
+    '';
   };
 
   vendorHash = "sha256-COp4RebkU36OChbZFeLALUppXqHLRbrm3D64Hb5RmI4=";
@@ -27,7 +33,10 @@ buildGoModule (finalAttrs: {
     "-X main.version=${finalAttrs.version}"
   ];
 
-  nativeCheckInputs = [ writableTmpDirAsHomeHook ];
+  preBuild = ''
+    ldflags+=" -X main.commit=$(cat COMMIT)"
+  '';
+
 
   subPackages = [ "cmd/glab" ];
 
@@ -41,6 +50,8 @@ buildGoModule (finalAttrs: {
       --fish <($out/bin/glab completion -s fish) \
       --zsh <($out/bin/glab completion -s zsh)
   '';
+
+  nativeCheckInputs = [ writableTmpDirAsHomeHook ];
 
   passthru.updateScript = nix-update-script { };
 
