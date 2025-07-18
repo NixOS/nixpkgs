8f70a4bd528177416ed6bc2d3b802c0f41805525
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-19 01:04:29 +0200

    jitsi-meet: remove with lib

--- pkgs/by-name/ji/jitsi-meet/package.nix
+++ pkgs/by-name/ji/jitsi-meet/package.nix
@@ -55,16 +55,16 @@ stdenv.mkDerivation (finalAttrs: {
 
   passthru.updateScript = ./update.sh;
 
-  meta = with lib; {
+  meta = {
     description = "Secure, Simple and Scalable Video Conferences";
     longDescription = ''
       Jitsi Meet is an open-source (Apache) WebRTC JavaScript application that uses Jitsi Videobridge
       to provide high quality, secure and scalable video conferences.
     '';
     homepage = "https://github.com/jitsi/jitsi-meet";
-    license = licenses.asl20;
-    teams = [ teams.jitsi ];
-    platforms = platforms.all;
+    license = lib.licenses.asl20;
+    teams = [ lib.teams.jitsi ];
+    inherit (nodejs.meta) platforms;
     inherit (olm.meta) knownVulnerabilities;
   };
 })
