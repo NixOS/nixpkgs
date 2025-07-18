d607333a6077c3c8c05e9b51f7c8fcd98bc58972
Eric Ripa <eric@ripa.io>
2025-07-17 17:14:11 -0700

    blowfish-tools: init at 1.10.0

---
+++ pkgs/by-name/bl/blowfish-tools/package.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchFromGitHub,
+}:
+
+buildNpmPackage (finalAttrs: {
+  pname = "blowfish-tools";
+  version = "1.10.0";
+
+  src = fetchFromGitHub {
+    owner = "nunocoracao";
+    repo = "blowfish-tools";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-90EKsRKOO2Hb64Wy3TlwzlPU2K8AAlSxc17ek5ZLoG0=";
+  };
+
+  dontNpmBuild = true;
+
+  npmDepsHash = "sha256-P6XHXR4QcVCRz5ju36OzCTNxXtW9RYxkfhbp7kJVfoY=";
+
+  meta = {
+    description = "CLI to initialize and configure a Blowfish project";
+    homepage = "https://blowfish.page";
+    changelog = "https://github.com/nunocoracao/blowfish-tools/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ eripa ];
+    mainProgram = "blowfish-tools";
+  };
+})
