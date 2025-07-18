3d1c583068d103391a20ab413b770612382a51ed
rudesome <26667983+rudesome@users.noreply.github.com>
2025-07-18 11:06:46 +0200

    maintainers: add rudesome
    add myself to the maintainer list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22022,6 +22022,11 @@
     githubId = 6445619;
     name = "Ruben Cano Diaz";
   };
+  rudesome = {
+    name = "Ruud Voorjans";
+    github = "rudesome";
+    githubId = 26667983;
+  };
   RudiOnTheAir = {
     name = "Rüdiger Schwoon";
     email = "wolf@schwoon.info";
