16a11da89b21c7e04ace7120e4bc04964bdb5c9c
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-18 11:25:06 +0000

    python3Packages.githubkit: 0.12.13 -> 0.12.16

--- pkgs/development/python-modules/githubkit/default.nix
+++ pkgs/development/python-modules/githubkit/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "githubkit";
-  version = "0.12.13";
+  version = "0.12.16";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "yanyongyu";
     repo = "githubkit";
     tag = "v${version}";
-    hash = "sha256-TMn81YY44bXUyU6GHSGtLtQ7aC2/vA9nZf/PaGhBi0s=";
+    hash = "sha256-TfripZiNfSK0e8Pjpt25JSCQ6XHnJMvZGmEcvNDdGbA=";
   };
 
   pythonRelaxDeps = [ "hishel" ];
