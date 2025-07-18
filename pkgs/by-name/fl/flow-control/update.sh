61828ef2da93d7e6966f362c36955ed04cdb519f
DoM <lukacsdominik1@gmail.com>
2025-07-18 19:59:05 +0200

    flow-control: 0.3.2 -> 0.4.0
    Changelog: https://github.com/neurocyte/flow/releases/tag/v0.4.0
    Diff: https://github.com/neurocyte/flow/compare/v0.3.2...v0.4.0

--- pkgs/by-name/fl/flow-control/update.sh
+++ pkgs/by-name/fl/flow-control/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p bash common-updater-scripts gnused
+#!nix-shell -i bash -p bash wget common-updater-scripts gnused
 
 latest_tag=$(list-git-tags --url=https://github.com/neurocyte/flow | sed 's/^v//' | tail -n 1)
 
@@ -10,7 +10,8 @@ mkdir -p src/syntax
 wget "https://raw.githubusercontent.com/neurocyte/flow/v${latest_tag}/build.zig.zon"
 wget -P src/syntax "https://raw.githubusercontent.com/neurocyte/flow/v${latest_tag}/src/syntax/build.zig.zon"
 
-nix --extra-experimental-features 'nix-command flakes' run github:Cloudef/zig2nix#zon2nix -- build.zig.zon >pkgs/by-name/fl/flow-control/build.zig.zon.nix
+nix --extra-experimental-features 'nix-command flakes' run github:Cloudef/zig2nix#zon2nix -- build.zig.zon
+mv build.zig.zon.nix pkgs/by-name/fl/flow-control/build.zig.zon.nix
 
 # strip file protocol
 sed -i '\|file = unpackZigArtifact { inherit name; artifact = /. + path; };|d' pkgs/by-name/fl/flow-control/build.zig.zon.nix
