099c8f688362d5c70a46bcd4e2bb03dff2de4a5c
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-18 19:10:38 +0000

    pyrefly: 0.17.1 -> 0.24.2

--- pkgs/by-name/py/pyrefly/package.nix
+++ pkgs/by-name/py/pyrefly/package.nix
@@ -9,18 +9,18 @@
 }:
 python3.pkgs.buildPythonApplication rec {
   pname = "pyrefly";
-  version = "0.17.1";
+  version = "0.24.2";
   pyproject = true;
 
   # fetch from PyPI instead of GitHub, since source repo does not have Cargo.lock
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-w4ivRtmApXiXQT95GI4vvYBop7yxdbbkpW+YTyFtgXM=";
+    hash = "sha256-ZxuZM8Kj9kaYPeaLwEInNvfONkxPZF90JVlCOwubUVA=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
-    hash = "sha256-Op5ueVkzZTiJ1zeBGVi8oeLcfSzXMYfk5zEg4OGyA5g=";
+    hash = "sha256-/CClXkZU1YOQ1W5j8BuelZmL6ug+ipq7IYYqd4p0iQg=";
   };
 
   build-system = [ maturin ];
