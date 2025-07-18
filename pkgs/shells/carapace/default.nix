4781fd0afd8418b694fe67c70dee5c9d8565b935
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-18 20:33:41 +0000

    carapace: 1.3.3 -> 1.4.0

--- pkgs/shells/carapace/default.nix
+++ pkgs/shells/carapace/default.nix
@@ -9,16 +9,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "carapace";
-  version = "1.3.3";
+  version = "1.4.0";
 
   src = fetchFromGitHub {
     owner = "carapace-sh";
     repo = "carapace-bin";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dVM5XFFNXAVoN2xshq5k0Y6vSrfSNS0bIptcloX/uSg=";
+    hash = "sha256-4T/JZAP/y4zRUBJ6xz5CFXitbzBUIUB/31FlBjTqyQU=";
   };
 
-  vendorHash = "sha256-XRbqxL2ANWi2aZbB30tNBxJoBIoDoMxKXMpOx++JJ6M=";
+  vendorHash = "sha256-gUhPIlf4J/Ee/i5q0x++nLufyJfk16csi/iBidut9zg=";
 
   ldflags = [
     "-s"
