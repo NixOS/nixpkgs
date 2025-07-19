47fd69710510380cdf7df8a569e35adfd1639abd
sternenseemann <sternenseemann@systemli.org>
2025-07-19 23:35:01 +0200

    chroma: 2.15.0 -> 2.19.0
    https://github.com/alecthomas/chroma/releases/tag/v2.16.0
    https://github.com/alecthomas/chroma/releases/tag/v2.17.0
    https://github.com/alecthomas/chroma/releases/tag/v2.17.1
    https://github.com/alecthomas/chroma/releases/tag/v2.17.2
    https://github.com/alecthomas/chroma/releases/tag/v2.18.0
    https://github.com/alecthomas/chroma/releases/tag/v2.19.0

--- pkgs/by-name/ch/chroma/package.nix
+++ pkgs/by-name/ch/chroma/package.nix
@@ -10,7 +10,7 @@ in
 
 buildGoModule rec {
   pname = "chroma";
-  version = "2.15.0";
+  version = "2.19.0";
 
   # To update:
   # nix-prefetch-git --rev v${version} https://github.com/alecthomas/chroma.git > src.json
@@ -21,7 +21,7 @@ buildGoModule rec {
     inherit (srcInfo) sha256;
   };
 
-  vendorHash = "sha256:14jg809xz647yv6sc8rnnksg2bpnn75panj8a2kdk2v87yrpq2zr";
+  vendorHash = "sha256-Gqldcp68Rn4wkfQptbmKUjkwLSb+qaFboJNfmWVkrPU=";
 
   modRoot = "./cmd/chroma";
 
