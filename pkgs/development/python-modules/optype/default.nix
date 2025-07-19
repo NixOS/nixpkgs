1b09ef9bc79d9a4d4f8c8050791c90dcb2960239
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-19 16:17:38 +0000

    python3Packages.optype: 0.11.0 -> 0.12.0

--- pkgs/development/python-modules/optype/default.nix
+++ pkgs/development/python-modules/optype/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "optype";
-  version = "0.11.0";
+  version = "0.12.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jorenham";
     repo = "optype";
     tag = "v${version}";
-    hash = "sha256-jExwQiEkCLiVFwiFYp2dBvH5PiRlSVG20CneGnht+No=";
+    hash = "sha256-sDMB9gSYf108Elsqj6Obk+6B4QKiJcStkJndVrIAWQI=";
   };
 
   disabled = pythonOlder "3.11";
