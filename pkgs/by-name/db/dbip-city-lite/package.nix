843e706680b6f1b8ecbcfa329d2801cf85bd4ffe
Nick Cao <nickcao@nichi.co>
2025-07-18 20:46:42 -0400

    dbip-city-lite: 2025-06 -> 2025-07

--- pkgs/by-name/db/dbip-city-lite/package.nix
+++ pkgs/by-name/db/dbip-city-lite/package.nix
@@ -5,11 +5,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "dbip-city-lite";
-  version = "2025-06";
+  version = "2025-07";
 
   src = fetchurl {
     url = "https://download.db-ip.com/free/dbip-city-lite-${finalAttrs.version}.mmdb.gz";
-    hash = "sha256-iv4UfdLcmLE5d5y38kz4EFpDpecjScESr1I30dB+jDQ=";
+    hash = "sha256-1xKZZ8ZKg6UFbJ+WcjjQudsW7T7MhhxdG1wDWKtrD7w=";
   };
 
   dontUnpack = true;
