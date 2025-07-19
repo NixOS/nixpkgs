969e55f04605a1eac69afb58029dd296254cd832
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-19 18:25:17 +0000

    python3Packages.google-cloud-container: 2.57.0 -> 2.58.0

--- pkgs/development/python-modules/google-cloud-container/default.nix
+++ pkgs/development/python-modules/google-cloud-container/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-container";
-  version = "2.57.0";
+  version = "2.58.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "google_cloud_container";
     inherit version;
-    hash = "sha256-+n5vWT9YNIuYvW3Vk0TJpgOOn7psC5hY4j2dJ4NG8Sk=";
+    hash = "sha256-dA7lsWf3BmxIXXnPejksi21A7N9V0Ka27MeET8TNmts=";
   };
 
   build-system = [ setuptools ];
