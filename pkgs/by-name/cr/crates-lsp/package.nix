f2dc774b222b0129ac8412646a62fba16d82a995
kpbaks <kristoffer.pbs@tuta.io>
2025-07-19 15:11:17 +0200

    fixup! crates-lsp: init at version 0.4.3

--- pkgs/by-name/cr/crates-lsp/package.nix
+++ pkgs/by-name/cr/crates-lsp/package.nix
@@ -7,16 +7,23 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "crates-lsp";
-  version = "0.4.1";
+  version = "0.4.3";
 
   src = fetchFromGitHub {
     owner = "MathiasPius";
     repo = "crates-lsp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-9+0qgdUn5l9oQQavbnR6rHe5zp5WHhGuRyOXt2Dv8Tw=";
+    hash = "sha256-HzoOsizeV2LOXXc8BKA7u5mwBJbWNaBZvPepAaVeTCQ=";
   };
 
-  cargoHash = "sha256-oS6xi8BH5vCVOimYWsDoW0Na7eUXzeHKKSOwpK9wbu8=";
+  cargoHash = "sha256-tQBNCTqvVNYqT5ArQE7ji0MeDWxi7Bcd9AxPP3sHvX4=";
+
+  checkFlags = [
+    # These tests fail on system `x86_64-darwin` with error:
+    # failed to create crates-lsp .gitignore file.: Os { code: 13, kind: PermissionDenied, message: "Permission denied" }
+    "--skip=crates::api::tests::get_common_crates"
+    "--skip=crates::sparse::tests::get_common_crates"
+  ];
 
   passthru.updateScript = nix-update-script { };
 
