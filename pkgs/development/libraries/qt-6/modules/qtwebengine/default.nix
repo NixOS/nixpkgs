ce3414c7a60b474db0e6cb3eba8773e89337814e
K900 <me@0upti.me>
2025-07-19 16:32:23 +0300

    qt6.qtwebengine: drop unused git dependency
    More git reverse closure reduction.

--- pkgs/development/libraries/qt-6/modules/qtwebengine/default.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebengine/default.nix
@@ -9,7 +9,6 @@
   coreutils,
   fetchpatch2,
   flex,
-  git,
   gperf,
   ninja,
   pkg-config,
@@ -75,7 +74,6 @@ qtModule {
       bison
       coreutils
       flex
-      git
       gperf
       ninja
       pkg-config
