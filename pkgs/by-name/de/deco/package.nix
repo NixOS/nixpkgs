4c068743a798096674e81c4b10147c3ce2750526
Lein Matsumaru <applePrincess@applePrincess.io>
2025-07-20 14:42:19 +0000

    deco: unstable-2019-04-03 -> unstable-2025-07-07

--- pkgs/by-name/de/deco/package.nix
+++ pkgs/by-name/de/deco/package.nix
@@ -5,17 +5,18 @@
   scsh,
   feh,
   xorg,
+  xdg-user-dirs,
 }:
 
 stdenv.mkDerivation {
   pname = "deco";
-  version = "unstable-2019-04-03";
+  version = "0-unstable-2025-07-07";
 
   src = fetchFromGitHub {
-    owner = "ebzzry";
+    owner = "vedatechnologiesinc";
     repo = "deco";
-    rev = "dd8ec7905bc85d085eb2ee3bddabea451054288c";
-    sha256 = "sha256-/3GeNvWOCRPOYTUbodXDUxR5QVDEyx6x2Jt5PxsPdvk=";
+    rev = "2fd28241ed28c07b9d641061d4e1bf3cacfcc7a0";
+    sha256 = "sha256-kjXEvgYO1p/dX9nXQ3HHcXmJdtxDM6xzKqDQu3yM4Tw=";
   };
 
   installPhase = ''
@@ -28,10 +29,11 @@ stdenv.mkDerivation {
     substituteInPlace $out/bin/deco --replace "/usr/bin/env scsh" "${scsh}/bin/scsh"
     substituteInPlace $out/bin/deco --replace "feh" "${feh}/bin/feh"
     substituteInPlace $out/bin/deco --replace "xdpyinfo" "${xorg.xdpyinfo}/bin/xdpyinfo"
+    substituteInPlace $out/bin/deco --replace "xdg-user-dir" "${xdg-user-dirs}/bin/xdg-user-dir"
   '';
 
   meta = with lib; {
-    homepage = "https://github.com/ebzzry/deco";
+    homepage = "https://github.com/vedatechnologiesinc/deco";
     description = "Simple root image setter";
     license = licenses.mit;
     maintainers = [ maintainers.ebzzry ];
