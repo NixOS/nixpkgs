c8bfd9218025c7440dd4e5e69170ab51cc772e97
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-20 23:20:40 +0000

    readsb: 3.14.1683 -> 3.14.1690

--- pkgs/by-name/re/readsb/package.nix
+++ pkgs/by-name/re/readsb/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "readsb";
-  version = "3.14.1683";
+  version = "3.14.1690";
 
   src = fetchFromGitHub {
     owner = "wiedehopf";
     repo = "readsb";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-X2Z9vPXJle5IKSuCWiM6l6FBq4GomguStFB4YJ0Y+EE=";
+    hash = "sha256-Ubm3Q6+BVOxp3haO4fUO776ERqfB3F4mlmb7yZzI+LA=";
   };
 
   strictDeps = true;
