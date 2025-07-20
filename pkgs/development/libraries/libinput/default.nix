6986305661060adc8e07e7de16ee54ebd1077e1a
Alyssa Ross <hi@alyssa.is>
2025-07-20 10:25:06 +0200

    libinput: mark unavailable for static

--- pkgs/development/libraries/libinput/default.nix
+++ pkgs/development/libraries/libinput/default.nix
@@ -150,5 +150,9 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ codyopel ];
     teams = [ teams.freedesktop ];
     changelog = "https://gitlab.freedesktop.org/libinput/libinput/-/releases/${version}";
+    badPlatforms = [
+      # Mandatory shared library.
+      lib.systems.inspect.platformPatterns.isStatic
+    ];
   };
 }
