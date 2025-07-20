cefb1440a67556bbacfa42f2bc6f3d46f5123f35
Fabian Affolter <fabian@affolter-engineering.ch>
2025-07-20 12:09:19 +0200

    python312Packages.mypy-boto3-pricing: 1.39.0 -> 1.39.5

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1054,8 +1054,8 @@ rec {
       "sha256-3MoTYkhWWNDVMHsQvfFNPv2Gax6MGfDirAJMjJEukCY=";
 
   mypy-boto3-pricing =
-    buildMypyBoto3Package "pricing" "1.39.0"
-      "sha256-/rtNFPFGyuCf/dvmWnYVIl3TKEjG7tBDIgCSGiWuO2c=";
+    buildMypyBoto3Package "pricing" "1.39.5"
+      "sha256-KVd2ZoJF4byRAfogtMw7w9hoTK1eaCKYrwJc99+4FK8=";
 
   mypy-boto3-privatenetworks =
     buildMypyBoto3Package "privatenetworks" "1.38.0"
