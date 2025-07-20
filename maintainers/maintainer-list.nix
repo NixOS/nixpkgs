f5f8a380898c48c50477452e03c8e739f45659db
Wolfgang Walther <walther@technowledgy.de>
2025-07-20 13:00:01 +0200

    maintainers: drop bartsch
    Became maintainer of uboot in 2022, never joined the NixOS org, never
    reacted to any maintainer pings since. No other PRs, reviews or
    comments.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -2852,12 +2852,6 @@
     githubId = 75235;
     name = "Michael Walker";
   };
-  bartsch = {
-    email = "consume.noise@gmail.com";
-    github = "bartsch";
-    githubId = 3390885;
-    name = "Daniel Martin";
-  };
   bartuka = {
     email = "wand@hey.com";
     github = "wandersoncferreira";
