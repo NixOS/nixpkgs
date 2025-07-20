f48d59dd9e0f45acf978298ff203df01ed9d3958
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-20 18:51:49 +0000

    python3Packages.microsoft-kiota-serialization-multipart: 1.9.3 -> 1.9.5

--- pkgs/development/python-modules/microsoft-kiota-serialization-multipart/default.nix
+++ pkgs/development/python-modules/microsoft-kiota-serialization-multipart/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "microsoft-kiota-serialization-multipart";
-  version = "1.9.3";
+  version = "1.9.5";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,8 +21,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "kiota-python";
-    tag = "microsoft-kiota-serialization-multipart-v${version}";
-    hash = "sha256-FUfVkJbpD0X7U7DPzyoh+84Bk7C07iLT9dmbUeliFu8=";
+    tag = "microsoft-kiota-serialization-text-v${version}";
+    hash = "sha256-h0sZhBIGZyhzYtCvLHRAn73HspgyOAKU3p4xSn+uXFU=";
   };
 
   sourceRoot = "${src.name}/packages/serialization/multipart/";
