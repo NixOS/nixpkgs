bc65daa53e3356dd6b9700f9b8feaecd3dbad147
Wolfgang Walther <walther@technowledgy.de>
2025-07-20 14:28:52 +0200

    maintainers: drop jyp
    Inactive since 2021. Not a member of the nixos org (anymore), thus can't
    be pinged for review.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12893,12 +12893,6 @@
     githubId = 20658981;
     name = "Jarosław Wygoda";
   };
-  jyp = {
-    email = "jeanphilippe.bernardy@gmail.com";
-    github = "jyp";
-    githubId = 27747;
-    name = "Jean-Philippe Bernardy";
-  };
   jzellner = {
     email = "jeffz@eml.cc";
     github = "sofuture";
