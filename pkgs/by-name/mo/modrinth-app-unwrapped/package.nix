83ec6dcb08f6f0b1fc1a78ebbbdfe3392f78f330
Valentine Briese <valentinegb@icloud.com>
2025-07-20 22:43:53 -0700

    modrinth-app-unwrapped: 0.9.5 -> 0.10.3

--- pkgs/by-name/mo/modrinth-app-unwrapped/package.nix
+++ pkgs/by-name/mo/modrinth-app-unwrapped/package.nix
@@ -14,6 +14,8 @@
   rustPlatform,
   turbo,
   webkitgtk_4_1,
+  jdk,
+  gradle,
 }:
 
 let
@@ -22,22 +24,24 @@ in
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "modrinth-app-unwrapped";
-  version = "0.9.5";
+  version = "0.10.3";
 
   src = fetchFromGitHub {
     owner = "modrinth";
     repo = "code";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-1+Fmc8qyU3hCZmRNgp90nuvFgaB/GOH6SNc9AyWZYn0=";
+    hash = "sha256-XfJbjbVcP9N3exAhXQoMGpoHORpKAlb0dPhQq195roY=";
   };
 
+  patches = [ ./0001-build-script.patch ];
+
   useFetchCargoVendor = true;
-  cargoHash = "sha256-6hEnXzaL6PnME9s+T+MtmoTQmaux/0m/6xaQ99lwM2I=";
+  cargoHash = "sha256-jWMHii65hTnTmiBFHxZ4xO5V+Qt/MPCy75eJvnlyE4c=";
 
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 1;
-    hash = "sha256-Q6e942R+3+511qFe4oehxdquw1TgaWMyOGOmP3me54o=";
+    hash = "sha256-7iqXuIQPbP2p26vrWDjMoyZBPpbVQpigYAylhIg8+ZY=";
   };
 
   nativeBuildInputs = [
@@ -47,6 +51,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
     nodejs
     pkg-config
     pnpm.configHook
+    jdk
+    gradle
   ] ++ lib.optional stdenv.hostPlatform.isDarwin makeBinaryWrapper;
 
   buildInputs = [ openssl ] ++ lib.optional stdenv.hostPlatform.isLinux webkitgtk_4_1;
@@ -56,6 +62,16 @@ rustPlatform.buildRustPackage (finalAttrs: {
     "--package"
     "theseus_gui"
   ];
+
+  mitmCache = gradle.fetchDeps {
+    inherit (finalAttrs) pname;
+    data = ./deps.json;
+  };
+  __darwinAllowLocalNetworking = true;
+  gradleFlags = [ "--no-configuration-cache" ];
+  preGradleUpdate = "cd packages/app-lib/java";
+  dontUseGradleBuild = true;
+  dontUseGradleCheck = true;
 
   env = {
     TURBO_BINARY_PATH = lib.getExe turbo;
@@ -89,6 +105,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
     license = with lib.licenses; [
       gpl3Plus
       unfreeRedistributable
+    ];
+    sourceProvenance = with lib.sourceTypes; [
+      fromSource
+      binaryBytecode # mitm cache
     ];
     maintainers = with lib.maintainers; [ getchoo ];
     mainProgram = "ModrinthApp";
