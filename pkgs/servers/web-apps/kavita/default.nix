0f6de20c3688357da8714a7ac287feed88c43adb
Henrique Oliveira <ho.henrique@proton.me>
2025-07-21 11:50:02 +0200

    kavita: 0.8.5.11 -> 0.8.7

--- pkgs/servers/web-apps/kavita/default.nix
+++ pkgs/servers/web-apps/kavita/default.nix
@@ -2,7 +2,6 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
-  fetchpatch2,
   buildDotnetModule,
   buildNpmPackage,
   dotnetCorePackages,
@@ -11,13 +10,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "kavita";
-  version = "0.8.5.11";
+  version = "0.8.7";
 
   src = fetchFromGitHub {
     owner = "kareadita";
     repo = "kavita";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-HSVdEB0yhmm/SZseHQ5kTRBaVqCZZx934Ovq1pTmQkM=";
+    hash = "sha256-jRP7ts6+jogPdmP+DfzeijlG70kvJWgf8da/sTBT9d4=";
   };
 
   backend = buildDotnetModule {
@@ -33,13 +32,6 @@ stdenvNoCC.mkDerivation (finalAttrs: {
       # On update: check if more migrations need to be restored!
       # Migrations should at least allow updates from previous NixOS versions
       ./restore-migrations.diff
-      # Our nixos test depends on /api/locale; this patch fixes an upstream bug where the first response always fails
-      # https://github.com/Kareadita/Kavita/pull/3686 remove once upstream is fixed
-      (fetchpatch2 {
-        name = "fix-locale-cache.patch";
-        url = "https://github.com/Kareadita/Kavita/commit/5af07b9525c2164a6548092244bbdf66815b3e95.patch?full_index=1";
-        hash = "sha256-aFZRxijAbA2mXJM+3kC4P4p76d0p5fEXLiaRhNmjOAA=";
-      })
     ];
     postPatch = ''
       substituteInPlace API/Services/DirectoryService.cs --subst-var out
@@ -65,7 +57,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     npmBuildScript = "prod";
     npmFlags = [ "--legacy-peer-deps" ];
     npmRebuildFlags = [ "--ignore-scripts" ]; # Prevent playwright from trying to install browsers
-    npmDepsHash = "sha256-9SfiH567+q3Id6/7pqWeX0y934V2YFQ4EWIJ+66smgI=";
+    npmDepsHash = "sha256-/fBUOlZfNGyRu9mfniSMBKHPNgNsZNpbFZpVrFwBtb4=";
   };
 
   dontBuild = true;
