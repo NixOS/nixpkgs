bfd94754d6044a5bac9c9fe161a2a1bab916bbd8
Ilan Joselevich <personal@ilanjoselevich.com>
2025-07-21 18:03:11 +0300

    python3Packages.python-roborock: 2.24.0 -> 2.25.0
    Diff: https://github.com/humbertogontijo/python-roborock/compare/refs/tags/v2.24.0...refs/tags/v2.25.0
    
    Changelog: https://github.com/humbertogontijo/python-roborock/blob/v2.25.0/CHANGELOG.md

--- pkgs/development/python-modules/python-roborock/default.nix
+++ pkgs/development/python-modules/python-roborock/default.nix
@@ -24,7 +24,7 @@
 
 buildPythonPackage rec {
   pname = "python-roborock";
-  version = "2.24.0";
+  version = "2.25.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -33,7 +33,7 @@ buildPythonPackage rec {
     owner = "humbertogontijo";
     repo = "python-roborock";
     tag = "v${version}";
-    hash = "sha256-TMnJMAK0MHswT1vPH1OnC35hrEOCHyH2YodNpM/VjC8=";
+    hash = "sha256-RsNWhcScp81plqXg9NmRFJhF+aLA0ld0A5H6mHo60uE=";
   };
 
   postPatch = ''
