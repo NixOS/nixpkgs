cc901077f2c07941b0ec625dd8f000e0a05e96eb
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-21 17:47:30 +0000

    lunar-client: 3.4.4 -> 3.4.6

--- pkgs/by-name/lu/lunar-client/package.nix
+++ pkgs/by-name/lu/lunar-client/package.nix
@@ -7,11 +7,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "lunarclient";
-  version = "3.4.4";
+  version = "3.4.6";
 
   src = fetchurl {
     url = "https://launcherupdates.lunarclientcdn.com/Lunar%20Client-${version}.AppImage";
-    hash = "sha512-2ShSTAyFQtGDDQsBSqg7ZPZDZWpx/T3H3yuYUn/WMkcMJOXqQO+qyth73WjZYoa6hOqc1yp5OzE7H9lLGLIW/g==";
+    hash = "sha512-uSuFcCon2gQONSV7Av6x3MywgUS3+q0vluc8MJbf4VnUJ7P1SgO7dKhqY2X6UBS7HJIqsmjuhWxfW9jScxTPIQ==";
   };
 
   nativeBuildInputs = [ makeWrapper ];
