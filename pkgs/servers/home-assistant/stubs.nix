883c69cfa208dd6926edc345f9d03a307b9ec76c
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-07-22 22:44:55 +0200

    python3Packages.homeassistant-stubs: 2025.7.2 -> 2025.7.3
    https://github.com/KapJI/homeassistant-stubs/releases/tag/2025.7.3

--- pkgs/servers/home-assistant/stubs.nix
+++ pkgs/servers/home-assistant/stubs.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "homeassistant-stubs";
-  version = "2025.7.2";
+  version = "2025.7.3";
   pyproject = true;
 
   disabled = python.version != home-assistant.python.version;
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "KapJI";
     repo = "homeassistant-stubs";
     tag = version;
-    hash = "sha256-qpNDeJHnGmjxgFOfNDl1jyK+t/BFzC+VtkTu+zL8NaQ=";
+    hash = "sha256-iHNBFtdIQMAKiQGJ35xCBqDbYYphoAJPXjhmgIEEE5E=";
   };
 
   build-system = [
