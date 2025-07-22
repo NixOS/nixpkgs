3c677d4754405c2607295bc244e815f507aab1ee
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-07-22 05:18:02 +0200

    apr: unpin autotools

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7709,9 +7709,7 @@ with pkgs;
 
   appstream = callPackage ../development/libraries/appstream { };
 
-  apr = callPackage ../development/libraries/apr {
-    autoreconfHook = buildPackages.autoreconfHook269;
-  };
+  apr = callPackage ../development/libraries/apr { };
 
   argparse-manpage = with python3Packages; toPythonApplication argparse-manpage;
 
