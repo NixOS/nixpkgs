63a6941da1f5bc494519134968156e863fa83f71
Guy Chronister <guychronister@outlook.com>
2025-07-22 08:45:23 -0500

    punes: fix errors

--- pkgs/by-name/pu/punes/package.nix
+++ pkgs/by-name/pu/punes/package.nix
@@ -17,6 +17,9 @@
   withQt6 ? false,
 }:
 
+let
+  qtPackages = if withQt6 then qt6Packages else libsForQt5;
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "punes";
   version = "0.111";
@@ -28,20 +31,43 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-TIXjYkInWV3yVnvXrdHcmeWYeps5TcvkG2Xjg4roIds=";
   };
 
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-    libsForQt5.qttools
-    libsForQt5.wrapQtAppsHook
+  patches = [
+    # Fix FTBFS with Qt 6.7.1
+    # Remove when version > 0.111
+    (fetchpatch {
+      name = "0001-punes-Fix-compatibility-with-Qt-6.7.1.patch";
+      url = "https://github.com/punesemu/puNES/commit/6e51b1a6107ad3de97edd40ae4ec2d41b32d804f.patch";
+      hash = "sha256-xRalKIOb1qWgqJsFLcm7uUOblEfHDYbkukmcr4/+4Qc=";
+    })
+
+    # Fix FTBFS with Qt 6.9
+    # Remove when version > 0.111
+    (fetchpatch {
+      name = "0002-punes-Updated-code-for-Qt-6.9.0-compatibility.patch";
+      url = "https://github.com/punesemu/puNES/commit/ff906e0a79eeac9a2d16783e0accf65748bb275e.patch";
+      hash = "sha256-+s7AdaUBgCseQs6Mxat/cDmQ77s6K6J0fUfyihP82jM=";
+    })
   ];
+
+  nativeBuildInputs =
+    [
+      cmake
+      pkg-config
+    ]
+    ++ (with qtPackages; [
+      qttools
+      wrapQtAppsHook
+    ]);
 
   buildInputs =
     [
       ffmpeg
       libGLU
-      libsForQt5.qtbase
-      libsForQt5.qtsvg
     ]
+    ++ (with qtPackages; [
+      qtbase
+      qtsvg
+    ])
     ++ lib.optionals stdenv.hostPlatform.isLinux [
       alsa-lib
       libX11
@@ -56,7 +82,7 @@ stdenv.mkDerivation (finalAttrs: {
     "-DENABLE_RELEASE=ON"
     "-DENABLE_FFMPEG=ON"
     "-DENABLE_OPENGL=ON"
-    "-DENABLE_QT6_LIBS=OFF"
+    (lib.strings.cmakeBool "ENABLE_QT6_LIBS" withQt6)
   ];
 
   passthru.updateScript = gitUpdater {
