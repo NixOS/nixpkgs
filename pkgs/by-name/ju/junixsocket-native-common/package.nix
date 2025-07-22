c0fc30831c44f2ab9043b2c4e7d0357d134bc0e0
Volker Diels-Grabsch <v@njh.eu>
2025-07-22 20:09:38 +0200

    junixsocket-native-common: init at 2.10.1

---
+++ pkgs/by-name/ju/junixsocket-native-common/package.nix
@@ -0,0 +1,15 @@
+{
+  fetchMavenArtifact,
+  junixsocket-common,
+  lib,
+}:
+
+fetchMavenArtifact {
+  groupId = "com.kohlschutter.junixsocket";
+  artifactId = "junixsocket-native-common";
+  inherit (junixsocket-common) version;
+  hash = "sha256-ASbOC68c61de9ReAfU0rFLnzLwYYAgThLsc6tKdyVno=";
+  meta = junixsocket-common.meta // {
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+  };
+}
