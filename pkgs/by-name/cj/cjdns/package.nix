4b5c5664cc66ff707cb09431d00ad528cc85950f
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-23 11:30:15 +0200

    cjdns: enable darwin support

--- pkgs/by-name/cj/cjdns/package.nix
+++ pkgs/by-name/cj/cjdns/package.nix
@@ -85,6 +85,6 @@ rustPlatform.buildRustPackage rec {
     description = "Encrypted networking for regular people";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ ehmry ];
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
   };
 }
