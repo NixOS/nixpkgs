15c399ca6e68e2a1de93c17b8b6f0dac143a0fb7
Jinser Kafka <aimer@purejs.icu>
2025-07-24 04:19:53 +0800

    goldfish: init at 17.11.18
    Update pkgs/by-name/go/goldfish/package.nix
    
    Co-authored-by: Noa Virellia <cmiki@amono.me>
    
    goldfish: use prePatch in preference to patchPhase
    
    goldfish: platform inux only
    
    goldfish: use patch instead of sed
    
    goldfish: use finalAttrs instead of rec
    
    goldfish: use github

---
+++ pkgs/by-name/go/goldfish/package.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  xmake,
+  s7,
+  tbox,
+  writers,
+  readline,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "goldfish";
+  version = "17.11.17";
+
+  src = fetchFromGitHub {
+    owner = "XmacsLabs";
+    repo = "goldfish";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-Zli5n7SJgs4Kk9LFVi/LqV37YrjWM+2NH2bUwon4U2c=";
+  };
+
+  nativeBuildInputs = [
+    xmake
+  ];
+  buildInputs = [
+    s7
+    tbox
+    readline
+    # make xmake happy
+    (writers.writeBashBin "git" "exit 0")
+  ];
+
+  patches = [ ./unpin-deps.patch ];
+
+  configurePhase = ''
+    runHook preConfigure
+    export HOME=$(mktemp -d)
+    xmake global --network=private
+    xmake config -m release --verbose --yes --diagnosis
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+    xmake build --yes -j $NIX_BUILD_CORES goldfish
+    xmake build --yes -j $NIX_BUILD_CORES goldfish_repl
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    xmake install -o $out goldfish
+    xmake install -o $out goldfish_repl
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "R7RS-small scheme implementation based on s7 scheme";
+    homepage = "https://gitee.com/XmacsLabs/goldfish";
+    license = lib.licenses.asl20;
+    mainProgram = "goldfish";
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ jinser ];
+  };
+})
