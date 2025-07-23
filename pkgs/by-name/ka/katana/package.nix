4efdc8627a007ddc25052085a3d784b731fed564
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-23 19:18:31 +0000

    katana: 1.1.3 -> 1.2.0

--- pkgs/by-name/ka/katana/package.nix
+++ pkgs/by-name/ka/katana/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "katana";
-  version = "1.1.3";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "projectdiscovery";
     repo = "katana";
     tag = "v${version}";
-    hash = "sha256-KwnGEWWrWpano+5gSG0YO4UE2ceKvgXmrtlhlda2xq4=";
+    hash = "sha256-AErn0m4OLnMX+zAuoYm71AqXnJfQjsSt+KqWUP93/ws=";
   };
 
-  vendorHash = "sha256-L7ycSzLbZUJ/4E+2lyN52xQFOJoxiRopgTfJkflFP9Q=";
+  vendorHash = "sha256-st7gj4hynuY1dDUEhA4xbiOnxH3jV5T2RK+PhB46Lpk=";
 
   subPackages = [ "cmd/katana" ];
 
