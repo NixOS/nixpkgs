5a58dcff7b50036575ba1afe0305d327489b479a
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-23 14:23:16 +0000

    glaze: 5.5.0 -> 5.5.4

--- pkgs/by-name/gl/glaze/package.nix
+++ pkgs/by-name/gl/glaze/package.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation (final: {
   pname = "glaze";
-  version = "5.5.0";
+  version = "5.5.4";
 
   src = fetchFromGitHub {
     owner = "stephenberry";
     repo = "glaze";
     tag = "v${final.version}";
-    hash = "sha256-HC8R1wyNySVhuTZczdbiHkQ8STTXA/1GJLKdTXN9VAo=";
+    hash = "sha256-v6/IJlwc+nYgTAn8DJcbRC+qhZtUR6xu45dwm7rueV8=";
   };
 
   nativeBuildInputs = [ cmake ];
