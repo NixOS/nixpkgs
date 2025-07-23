525b40ebed4d4bcd70decab1e332190ab51b4168
Acid Bong <acidbong@tilde.club>
2025-07-23 21:04:48 +0300

    tuigreet: clean up and modernize
    - removed `with lib;`
    - replaced `rec` with `finalAttrs`
    - removed redundant `useFetchCargoVendor`
    - updated fetcher attrs

--- pkgs/by-name/tu/tuigreet/package.nix
+++ pkgs/by-name/tu/tuigreet/package.nix
@@ -6,15 +6,15 @@
   installShellFiles,
   scdoc,
 }:
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "tuigreet";
   version = "0.9.1";
 
   src = fetchFromGitHub {
     owner = "apognu";
     repo = "tuigreet";
-    rev = "refs/tags/${version}";
-    sha256 = "sha256-e0YtpakEaaWdgu+bMr2VFoUc6+SUMFk4hYtSyk5aApY=";
+    tag = finalAttrs.version;
+    hash = "sha256-e0YtpakEaaWdgu+bMr2VFoUc6+SUMFk4hYtSyk5aApY=";
   };
 
   cargoHash = "sha256-w6ZOqpwogKoN4oqqI1gFqY8xAnfvhEBVaL8/6JXpKXs=";
@@ -32,10 +32,10 @@ rustPlatform.buildRustPackage rec {
   meta = {
     description = "Graphical console greeter for greetd";
     homepage = "https://github.com/apognu/tuigreet";
-    changelog = "https://github.com/apognu/tuigreet/releases/tag/${version}";
+    changelog = "https://github.com/apognu/tuigreet/releases/tag/${finalAttrs.version}";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ ];
     platforms = lib.platforms.linux;
     mainProgram = "tuigreet";
   };
-}
+})
