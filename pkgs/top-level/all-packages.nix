fd54edc4d34ced343a95003d68e696c3bfd7d779
guylamar2006 <guychronister@outlook.com>
2025-07-22 21:42:57 -0500

    valeronoi: migrate to by-name; refactor (#427499)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1154,8 +1154,6 @@ with pkgs;
 
   ufolint = with python3Packages; toPythonApplication ufolint;
 
-  valeronoi = qt6Packages.callPackage ../tools/misc/valeronoi { };
-
   veikk-linux-driver-gui = libsForQt5.callPackage ../tools/misc/veikk-linux-driver-gui { };
 
   ventoy-full = ventoy.override {
