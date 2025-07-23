478930a7126a919b0494c4380d252f24413ad85b
Acid Bong <acidbong@tilde.club>
2025-07-23 20:42:55 +0300

    gtkgreet: clean up and modernize
    - removed `with lib;`
    - replaced `rec` with `finalAttrs`
    - updated fetcher attrs

--- pkgs/by-name/gt/gtkgreet/package.nix
+++ pkgs/by-name/gt/gtkgreet/package.nix
@@ -14,15 +14,15 @@
   scdoc,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gtkgreet";
   version = "0.8";
 
   src = fetchFromSourcehut {
     owner = "~kennylevinsen";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-GKBYql0hzqB6uY87SsAqHwf3qLAr7xznMnAjRtP4HS8=";
+    repo = "gtkgreet";
+    rev = finalAttrs.version;
+    hash = "sha256-GKBYql0hzqB6uY87SsAqHwf3qLAr7xznMnAjRtP4HS8=";
   };
 
   depsBuildBuild = [ pkg-config ];
@@ -54,12 +54,12 @@ stdenv.mkDerivation rec {
   # G_APPLICATION_FLAGS_NONE is deprecated in GLib 2.73.3+.
   env.NIX_CFLAGS_COMPILE = "-Wno-error=deprecated-declarations";
 
-  meta = with lib; {
+  meta = {
     description = "GTK based greeter for greetd, to be run under cage or similar";
     homepage = "https://git.sr.ht/~kennylevinsen/gtkgreet";
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ ];
+    platforms = lib.platforms.linux;
     mainProgram = "gtkgreet";
   };
-}
+})
