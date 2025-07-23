e039cb4f0ad305f6ab8ee82618cc3749ae4a58c9
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-23 10:49:42 +0000

    grafanaPlugins.volkovlabs-echarts-panel: 6.6.0 -> 7.0.0

--- pkgs/servers/monitoring/grafana/plugins/volkovlabs-echarts-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/volkovlabs-echarts-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "volkovlabs-echarts-panel";
-  version = "6.6.0";
-  zipHash = "sha256-SjZl33xoHVmE6y0D7FT9x2wVPil7HK1rYVgTXICpXZ4=";
+  version = "7.0.0";
+  zipHash = "sha256-ibM4h96R+hvqoG9k6xz+2xlhK1xQPb2F1BLt+mQVSxo=";
   meta = with lib; {
     description = "Visualization panel for Grafana that allows you to incorporate the popular Apache ECharts library into your Grafana dashboard";
     license = licenses.asl20;
