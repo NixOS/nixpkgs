3061ea704e5ec066dbbdb564c8af1928d252f6a3
Guy Chronister <guychronister@outlook.com>
2025-07-23 09:05:03 -0500

    pricehist: refactor package definitions and meta
    pricehist: add suggested changes

--- pkgs/by-name/pr/pricehist/package.nix
+++ pkgs/by-name/pr/pricehist/package.nix
@@ -1,18 +1,10 @@
 {
   lib,
-  buildPythonApplication,
+  python3Packages,
   fetchFromGitLab,
-  requests,
-  lxml,
-  cssselect,
-  curlify,
-  poetry-core,
-  pytest-mock,
-  responses,
-  pytestCheckHook,
 }:
 
-buildPythonApplication rec {
+python3Packages.buildPythonApplication rec {
   pname = "pricehist";
   version = "1.4.7";
   format = "pyproject";
@@ -20,32 +12,32 @@ buildPythonApplication rec {
   src = fetchFromGitLab {
     owner = "chrisberkhout";
     repo = "pricehist";
-    rev = version;
+    tag = version;
     hash = "sha256-SBRJxNnA+nOxO6h97WZZHwhxoXeNtb5+rDayn4Hw6so=";
   };
 
-  propagatedBuildInputs = [
+  dependencies = with python3Packages; [
     requests
     lxml
     cssselect
     curlify
+  ];
+
+  build-system = with python3Packages; [
     poetry-core
   ];
 
-  nativeBuildInputs = [
-  ];
-
-  nativeCheckInputs = [
+  nativeCheckInputs = with python3Packages; [
     responses
     pytest-mock
     pytestCheckHook
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Command-line tool for fetching and formatting historical price data, with support for multiple data sources and output formats";
     homepage = "https://gitlab.com/chrisberkhout/pricehist";
-    license = licenses.mit;
+    license = lib.licenses.mit;
     mainProgram = "pricehist";
-    maintainers = with maintainers; [ chrpinedo ];
+    maintainers = with lib.maintainers; [ chrpinedo ];
   };
 }
