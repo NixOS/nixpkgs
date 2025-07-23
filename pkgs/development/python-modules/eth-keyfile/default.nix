31bae0f6a5c451f592bff36051497b3350d2874a
Ben Siraphob <bensiraphob@gmail.com>
2025-07-22 21:23:51 -0700

    python3Packages.eth-keyfile: 0.8.1 -> 0.9.1

--- pkgs/development/python-modules/eth-keyfile/default.nix
+++ pkgs/development/python-modules/eth-keyfile/default.nix
@@ -7,13 +7,14 @@
   eth-keys,
   eth-utils,
   pycryptodome,
+  py-ecc,
   # nativeCheckInputs
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "eth-keyfile";
-  version = "0.8.1";
+  version = "0.9.1";
   pyproject = true;
 
   src = fetchFromGitHub {
@@ -21,7 +22,7 @@ buildPythonPackage rec {
     repo = "eth-keyfile";
     tag = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-HufsN3aXdQErcQmnG2PZnEm5joqpy4f8IWNm3VrzJSY=";
+    hash = "sha256-DR17EupRDnviN6OXF+B+RlCVdG8cfcvnIgIEKxrXFKs=";
   };
 
   build-system = [ setuptools ];
@@ -30,11 +31,16 @@ buildPythonPackage rec {
     eth-keys
     eth-utils
     pycryptodome
+    py-ecc
   ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "eth_keyfile" ];
+
+  disabledTests = [
+    "test_install_local_wheel"
+  ];
 
   meta = {
     description = "Tools for handling the encrypted keyfile format used to store private keys";
