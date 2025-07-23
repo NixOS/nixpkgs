d99ca2b37f1b9eb519a8b1e72e367bcf16522df6
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-23 00:29:46 +0000

    libastyle: 3.6.9 -> 3.6.10

--- pkgs/by-name/as/astyle/package.nix
+++ pkgs/by-name/as/astyle/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "astyle";
-  version = "3.6.9";
+  version = "3.6.10";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pname}/${pname}-${version}.tar.bz2";
-    hash = "sha256-tkRZdlTfW0AIe+SkZyPGUED3zlnzNp8bj2kPnBDKurw=";
+    hash = "sha256-HW7onAhKk93MKNGgVs2o0cNX0xjvjihEVtnwvSzrS20=";
   };
 
   nativeBuildInputs = [ cmake ];
