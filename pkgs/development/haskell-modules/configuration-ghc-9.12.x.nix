68b509de450964c4112740a5143fcb4aba7a1e9c
Peter Simons <simons@cryp.to>
2025-07-23 13:53:45 +0200

    haskell-xmobar: jailbreak for ghc-9.12.x to allow latest base library

--- pkgs/development/haskell-modules/configuration-ghc-9.12.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.12.x.nix
@@ -118,6 +118,7 @@ with haskellLib;
   timezone-olson = doJailbreak super.timezone-olson; # time <1.14
   cabal-plan = doJailbreak super.cabal-plan; # base <4.21
   dbus = doJailbreak super.dbus; # template-haskell <2.23
+  xmobar = doJailbreak super.xmobar; # base <4.21
 
   #
   # Test suite issues
