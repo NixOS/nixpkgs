570a5081de3d76edc525f2ec69fe51b813d903e3
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-24 04:13:40 +0000

    rat-king-adventure: 2.2.2 -> 2.2.3

--- pkgs/games/shattered-pixel-dungeon/rat-king-adventure/default.nix
+++ pkgs/games/shattered-pixel-dungeon/rat-king-adventure/default.nix
@@ -5,13 +5,13 @@
 
 callPackage ../generic.nix rec {
   pname = "rat-king-adventure";
-  version = "2.2.2";
+  version = "2.2.3";
 
   src = fetchFromGitHub {
     owner = "TrashboxBobylev";
     repo = "Rat-King-Adventure";
     rev = version;
-    hash = "sha256-9vK5wD/pA/xa4lpRrpHdiwbiRdaBkIB6TKAOIfHrjz0=";
+    hash = "sha256-6KmJRcYUjEdFym8nkCtg841CyKKYV1gACMJv/zOQ2Oc=";
   };
 
   desktopName = "Rat King Adventure";
