f40071e0ec940644f0d8d263e2b645ef00e511db
Guy Chronister <guychronister@outlook.com>
2025-07-24 09:12:13 -0500

    plater: refactor package definitions and meta

--- pkgs/by-name/pl/plater/package.nix
+++ pkgs/by-name/pl/plater/package.nix
@@ -1,15 +1,14 @@
 {
-  mkDerivation,
+  stdenv,
   cmake,
   fetchFromGitHub,
   lib,
   libGLU,
   makeDesktopItem,
-  qtbase,
-  wrapQtAppsHook,
+  libsForQt5,
 }:
 
-mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "plater";
   version = "2020-07-30";
 
@@ -17,22 +16,22 @@ mkDerivation rec {
     owner = "Rhoban";
     repo = "Plater";
     rev = "f8de6d038f95a9edebfcfe142c8e9783697d5b47";
-    sha256 = "0r20mbzd16zv1aiadjqdy7z6sp09rr6lgfxhvir4ll3cpakkynr4";
+    hash = "sha256-JFs/p7psUEpy3LC7R03OCVxt/vENy6aiCvub0P6qQGQ=";
   };
 
   nativeBuildInputs = [
     cmake
-    wrapQtAppsHook
+    libsForQt5.wrapQtAppsHook
   ];
   buildInputs = [
     libGLU
-    qtbase
+    libsForQt5.qtbase
   ];
 
   desktopItem = makeDesktopItem {
-    name = pname;
-    exec = pname;
-    icon = pname;
+    name = "plater";
+    exec = "plater";
+    icon = "plater";
     desktopName = "Ideamaker";
     genericName = meta.description;
     categories = [
@@ -47,11 +46,11 @@ mkDerivation rec {
     cp $src/gui/img/plater.png $out/share/pixmaps/${pname}.png
   '';
 
-  meta = with lib; {
+  meta = {
     description = "3D-printer parts placer and plate generator";
     homepage = "https://github.com/Rhoban/Plater";
-    maintainers = with maintainers; [ lovesegfault ];
-    platforms = platforms.linux;
-    license = licenses.cc-by-nc-30;
+    maintainers = with lib.maintainers; [ lovesegfault ];
+    platforms = lib.platforms.linux;
+    license = lib.licenses.cc-by-nc-30;
   };
 }
