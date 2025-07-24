bc541d82752ff8435207b1fa853b1bccd34a3570
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-07-24 16:06:55 +0200

    espeak-ng: backport TextToPhonemesWithTerminator support
    This is required for piper 1.3.0. There is no breaking API change.

--- pkgs/applications/audio/espeak-ng/default.nix
+++ pkgs/applications/audio/espeak-ng/default.nix
@@ -2,6 +2,7 @@
   stdenv,
   lib,
   fetchFromGitHub,
+  fetchpatch,
   replaceVars,
 
   # build system
@@ -70,6 +71,11 @@ stdenv.mkDerivation rec {
   patches = [
     # https://github.com/espeak-ng/espeak-ng/pull/2274
     ./libsonic.patch
+    (fetchpatch {
+      name = "espeak-ng-text-to-phonemes-with-terminator.patch";
+      url = "https://github.com/espeak-ng/espeak-ng/commit/2108b1e8ae02f49cc909894a1024efdfde6682fd.patch";
+      hash = "sha256-XjEc1r7F88xZOfeUey0R6Xv6vu4Wy8GtWxXFG2NTf9g=";
+    })
   ]
   ++ lib.optionals mbrolaSupport [
     # Hardcode correct mbrola paths.
