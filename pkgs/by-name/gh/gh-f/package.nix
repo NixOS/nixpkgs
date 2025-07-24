06bd2ab95a39bdd3eb4aa54161ec668103521c64
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-24 06:08:55 +0000

    gh-f: 1.3.0 -> 1.4.0

--- pkgs/by-name/gh/gh-f/package.nix
+++ pkgs/by-name/gh/gh-f/package.nix
@@ -25,13 +25,13 @@ let
 in
 stdenvNoCC.mkDerivation rec {
   pname = "gh-f";
-  version = "1.3.0";
+  version = "1.4.0";
 
   src = fetchFromGitHub {
     owner = "gennaro-tedesco";
     repo = "gh-f";
     rev = "v${version}";
-    hash = "sha256-CW6iAI5IomJoMuPBFq/3owhZJbcruKtOqoxzsh+FNVw=";
+    hash = "sha256-JlMJ5RplEtQ8ApN3x1Sl0Lkutb5kLpuMJrF96oKZC9k=";
   };
 
   nativeBuildInputs = [
