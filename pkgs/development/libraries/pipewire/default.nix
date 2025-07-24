a2df8d9237684708ca5bf4741e4a8bdd216bef25
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-24 01:13:50 +0000

    pipewire: 1.4.6 -> 1.4.7
    (cherry picked from commit b0a86ce5d511dc54707527e24db51742cfe97ddc)

--- pkgs/development/libraries/pipewire/default.nix
+++ pkgs/development/libraries/pipewire/default.nix
@@ -82,7 +82,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pipewire";
-  version = "1.4.6";
+  version = "1.4.7";
 
   outputs = [
     "out"
@@ -98,7 +98,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "pipewire";
     repo = "pipewire";
     rev = finalAttrs.version;
-    sha256 = "sha256-Hk43rKrKCJA6njQ9ap/Pje9AQKygrDc+GTlimaMh/pg=";
+    sha256 = "sha256-U9J7f6nDO4tp6OCBtBcZ9HP9KDKLfuuRWDEbgLL9Avs=";
   };
 
   patches = [
