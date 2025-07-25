3b519806b989c632099443533eec2c47c1952f6a
Nikolay Korotkiy <sikmir@disroot.org>
2025-07-25 22:08:30 +0400

    tg: 0.19.0 → 0.22.0

--- pkgs/applications/networking/instant-messengers/telegram/tg/default.nix
+++ pkgs/applications/networking/instant-messengers/telegram/tg/default.nix
@@ -2,49 +2,50 @@
   lib,
   buildPythonApplication,
   fetchFromGitHub,
+  poetry-core,
   pythonOlder,
-  fetchpatch,
   stdenv,
   libnotify,
+  mailcap-fix,
   python-telegram,
 }:
 
 buildPythonApplication rec {
   pname = "tg";
-  version = "0.19.0";
-  format = "setuptools";
-  disabled = pythonOlder "3.8";
+  version = "0.22.0";
+  pyproject = true;
+  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "paul-nameless";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-apHd26XnOz5nak+Kz8PJPsonQfTWDyPz7Mi/tWf7zwM=";
+    repo = "tg";
+    tag = "v${version}";
+    hash = "sha256-qzqYkksocR86QFmP75ZE93kMSVmdel+OTxPgt9uZHLI=";
   };
 
-  patches = [
-    # Fix sending messages
-    # https://github.com/paul-nameless/tg/pull/306
-    (fetchpatch {
-      url = "https://github.com/mindtheegab/tg/commit/13e2b266989d2d757a394b0fb8cb7fd6ccc2b70c.patch";
-      hash = "sha256-Wja6xBOlPuACzhbT8Yl3F8qSh3Kd9G1lnr9VarbPrfM=";
-    })
-  ];
-
-  # Fix notifications on platforms other than darwin by providing notify-send
-  postPatch = lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "poetry-core>=1.0.0,<2.0.0" "poetry-core"
+  ''
+  + lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
+    # Fix notifications on platforms other than darwin by providing notify-send
     sed -i 's|^NOTIFY_CMD = .*|NOTIFY_CMD = "${libnotify}/bin/notify-send {title} {message} -i {icon_path}"|' tg/config.py
   '';
 
-  propagatedBuildInputs = [ python-telegram ];
+  build-system = [ poetry-core ];
+
+  dependencies = [
+    mailcap-fix
+    python-telegram
+  ];
 
   doCheck = false; # No tests
 
-  meta = with lib; {
+  meta = {
     description = "Terminal client for telegram";
     mainProgram = "tg";
     homepage = "https://github.com/paul-nameless/tg";
-    license = licenses.unlicense;
-    maintainers = with maintainers; [ sikmir ];
+    license = lib.licenses.unlicense;
+    maintainers = with lib.maintainers; [ sikmir ];
   };
 }
