54e1c9fe000e98f0e3ca107b4d4b016029967763
Fabian Affolter <mail@fabian-affolter.ch>
2025-07-25 21:17:32 +0200

    cameradar: refactor

--- pkgs/by-name/ca/cameradar/package.nix
+++ pkgs/by-name/ca/cameradar/package.nix
@@ -13,29 +13,23 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "Ullaakut";
     repo = "cameradar";
-    rev = "v${version}";
-    sha256 = "sha256-nfqgBUgcLjPLdn8hs1q0FLDBHbloeMKETDrv3a5SZq0=";
+    tag = "v${version}";
+    hash = "sha256-nfqgBUgcLjPLdn8hs1q0FLDBHbloeMKETDrv3a5SZq0=";
   };
 
   vendorHash = "sha256-AIi57DWMvAKl0PhuwHO/0cHoDKk5e0bJsqHYBka4NiU=";
 
-  nativeBuildInputs = [
-    pkg-config
-  ];
+  nativeBuildInputs = [ pkg-config ];
 
-  buildInputs = [
-    curl
-  ];
+  buildInputs = [ curl ];
 
-  subPackages = [
-    "cmd/cameradar"
-  ];
+  subPackages = [ "cmd/cameradar" ];
 
   meta = {
     description = "RTSP stream access tool";
     homepage = "https://github.com/Ullaakut/cameradar";
-    changelog = "https://github.com/Ullaakut/cameradar/releases/tag/v${version}";
-    license = with lib.licenses; [ mit ];
+    changelog = "https://github.com/Ullaakut/cameradar/releases/tag/${src.tag}";
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
 }
