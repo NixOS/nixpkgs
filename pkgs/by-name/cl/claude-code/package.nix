fc373affaf72534e93e562e4c4f37decb2ccba31
Austin Seipp <aseipp@pobox.com>
2025-07-24 21:16:18 -0500

    claude-code: 1.0.58 -> 1.0.60
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    (cherry picked from commit 9bc7e0c5586f653f3440847660930705d838e2c5)

--- pkgs/by-name/cl/claude-code/package.nix
+++ pkgs/by-name/cl/claude-code/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "1.0.58";
+  version = "1.0.60";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-Mp3S269iifNGSEz83IF6bqbgdy6Im1bQjR8oaaL3W8c=";
+    hash = "sha256-ygeitenu4z9ACbezO53I2Xnk6NtE1fWVzCi3mZS7wF8=";
   };
 
-  npmDepsHash = "sha256-iAckljEIJFVtlySHjS414HUFg6XVSlPr8azYqTo1py8=";
+  npmDepsHash = "sha256-F4wAjwTSs5jYoVgJoCrrI1dHlBmxxfWF/KGvuu5Gl30=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
