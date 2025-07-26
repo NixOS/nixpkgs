19f88ab5f08f0738fbec21cf90f1b3a438977fde
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2025-07-26 17:35:48 -0400

    maintainers: drop dan4ik605743

--- pkgs/by-name/to/tonelib-gfx/package.nix
+++ pkgs/by-name/to/tonelib-gfx/package.nix
@@ -69,7 +69,6 @@ stdenv.mkDerivation rec {
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
     maintainers = with maintainers; [
-      dan4ik605743
       husjon
       orivej
     ];
