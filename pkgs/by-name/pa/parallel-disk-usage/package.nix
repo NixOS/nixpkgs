97f53d9f3236fb4572645e6999cd6fd6f9c8b177
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-26 14:22:33 +0000

    parallel-disk-usage: 0.13.1 -> 0.20.0

--- pkgs/by-name/pa/parallel-disk-usage/package.nix
+++ pkgs/by-name/pa/parallel-disk-usage/package.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "parallel-disk-usage";
-  version = "0.13.1";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "KSXGitHub";
     repo = "parallel-disk-usage";
     rev = version;
-    hash = "sha256-gzjNi7ujYe7OZKYyrYk1xqqo/k8yBJdIwRoNASm5Db4=";
+    hash = "sha256-Xzc+1cPs84o58G7n8hEN9vtaz3bw3TrwodphK45Iq2g=";
   };
 
-  cargoHash = "sha256-3U61AkCicX7VNh1bf0IHPH5YX7qAtp4PvWi8FRKoBQI=";
+  cargoHash = "sha256-qSQpjYydpjVUP5AO2sPshP/HZzeVihwlJxyabml67js=";
 
   meta = with lib; {
     description = "Highly parallelized, blazing fast directory tree analyzer";
