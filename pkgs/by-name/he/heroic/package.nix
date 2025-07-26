dd4c0cba1d30f6a028bcb33bd183bd4f30b22bb7
Aidan Gauland <aidalgol@fastmail.net>
2025-07-27 09:07:07 +1200

    heroic: remove FHS env wrapper
    Heroic by default uses umu-launcher to run Proton, which does not need
    the wrapper.  Native games often depend upon EOL libraries and can only
    be run using the Steam Runtime.

--- pkgs/by-name/he/heroic/package.nix
+++ pkgs/by-name/he/heroic/package.nix
@@ -1,160 +1,123 @@
 {
-  buildFHSEnv,
-  heroic-unwrapped,
-  extraPkgs ? pkgs: [ ],
-  extraLibraries ? pkgs: [ ],
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  nix-update-script,
+  # Pinned, because our FODs are not guaranteed to be stable between major versions.
+  pnpm_10,
+  nodejs,
+  python3,
+  makeWrapper,
+  # Electron updates frequently break Heroic, so pin same version as upstream, or newest non-EOL.
+  electron_36,
+  vulkan-helper,
+  gogdl,
+  legendary-heroic,
+  nile,
+  comet-gog,
 }:
 
-buildFHSEnv {
+let
+  electron = electron_36;
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "heroic";
-  inherit (heroic-unwrapped) version;
+  version = "2.18.0";
 
-  runScript = "heroic";
+  src = fetchFromGitHub {
+    owner = "Heroic-Games-Launcher";
+    repo = "HeroicGamesLauncher";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-DrE1gwer1pozTPWSzc5PpTWacDHhdNk/o6pA62E0uPA=";
+  };
 
-  # Many Wine and native games need 32-bit libraries.
-  multiArch = true;
+  pnpmDeps = pnpm_10.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    fetcherVersion = 1;
+    hash = "sha256-VaPWB5nUvmnGW6gkW2FwoqcHk/rMfObuhR7RwsyNghk=";
+  };
 
-  # required by Electron
-  unshareIpc = false;
+  nativeBuildInputs = [
+    nodejs
+    pnpm_10.configHook
+    python3
+    makeWrapper
+  ];
 
-  targetPkgs =
-    pkgs:
-    with pkgs;
-    [
-      heroic-unwrapped
-      gamemode
-      curl
-      gawk
-      zenity
-      kdePackages.kdialog
-      mangohud
-      net-tools
-      opencl-headers
-      p7zip
-      pciutils
-      perl
-      psmisc
-      python3
-      umu-launcher
-      unzip
-      which
-      xorg.xrandr
-      zstd
-    ]
-    ++ extraPkgs pkgs;
+  patches = [
+    # Make Heroic create Steam shortcuts (to non-steam games) with the correct path to heroic.
+    ./fix-non-steam-shortcuts.patch
+  ];
 
-  multiPkgs =
-    let
-      xorgDeps =
-        pkgs: with pkgs.xorg; [
-          libICE
-          libpthreadstubs
-          libSM
-          libX11
-          libXaw
-          libxcb
-          libXcomposite
-          libXcursor
-          libXdmcp
-          libXext
-          libXfixes
-          libXi
-          libXinerama
-          libXmu
-          libXrandr
-          libXrender
-          libXScrnSaver
-          libXt
-          libXtst
-          libXv
-          libXxf86vm
-        ];
-      gstreamerDeps =
-        pkgs: with pkgs.gst_all_1; [
-          gstreamer
-          gst-plugins-base
-          gst-plugins-good
-          gst-plugins-ugly
-          gst-plugins-bad
-          gst-libav
-        ];
-    in
-    pkgs:
-    with pkgs;
-    [
-      alsa-lib
-      alsa-plugins
-      bash
-      cabextract
-      cairo
-      coreutils
-      cups
-      dbus
-      freealut
-      freetype
-      fribidi
-      giflib
-      glib
-      gnutls
-      gtk3
-      icu
-      lcms2
-      libevdev
-      libgcrypt
-      libGLU
-      libglvnd
-      libgpg-error
-      libgudev
-      libjpeg
-      libkrb5
-      libmpeg2
-      libogg
-      libopus
-      libpng
-      libpulseaudio
-      libselinux
-      libsndfile
-      libtheora
-      libtiff
-      libunwind
-      libusb1
-      libv4l
-      libva
-      libvdpau
-      libvorbis
-      libvpx
-      libwebp
-      libxkbcommon
-      libxml2
-      mpg123
-      ncurses
-      ocl-icd
-      openal
-      openldap
-      openssl
-      pango
-      pipewire
-      samba4
-      sane-backends
-      SDL2
-      speex
-      sqlite
-      udev
-      unixODBC
-      util-linux
-      vulkan-loader
-      wayland
-      zlib
-    ]
-    ++ xorgDeps pkgs
-    ++ gstreamerDeps pkgs
-    ++ extraLibraries pkgs;
+  env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
-  extraInstallCommands = ''
-    mkdir -p $out/share
-    ln -s ${heroic-unwrapped}/share/applications $out/share
-    ln -s ${heroic-unwrapped}/share/icons $out/share
+  buildPhase = ''
+    runHook preBuild
+
+    # set nodedir to prevent node-gyp from downloading headers
+    export npm_config_nodedir=${electron.headers}
+
+    pnpm --offline electron-vite build
+    pnpm --offline electron-builder \
+      --linux \
+      --dir \
+      -c.asarUnpack="**/*.node" \
+      -c.electronDist=${electron.dist} \
+      -c.electronVersion=${electron.version}
+
+    runHook postBuild
   '';
 
-  meta = heroic-unwrapped.meta;
-}
+  # --disable-gpu-compositing is to work around upstream bug
+  # https://github.com/electron/electron/issues/32317
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/opt/heroic"
+    cp -r dist/linux-unpacked/resources "$out/opt/heroic"
+
+    cp -r public "$out/opt/heroic/resources/app.asar.unpacked/build"
+    rm -rf "$out/opt/heroic/resources/app.asar.unpacked/build/bin"
+    mkdir -p "$out/opt/heroic/resources/app.asar.unpacked/build/bin/x64/linux"
+    ln -s \
+      "${lib.getExe gogdl}" \
+      "${lib.getExe legendary-heroic}" \
+      "${lib.getExe nile}" \
+      "${lib.getExe comet-gog}" \
+      "${lib.getExe vulkan-helper}" \
+      "$out/opt/heroic/resources/app.asar.unpacked/build/bin/x64/linux"
+
+    makeWrapper "${electron}/bin/electron" "$out/bin/heroic" \
+      --inherit-argv0 \
+      --set ELECTRON_FORCE_IS_PACKAGED 1 \
+      --add-flags --disable-gpu-compositing \
+      --add-flags $out/opt/heroic/resources/app.asar \
+      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}"
+
+    install -D "flatpak/com.heroicgameslauncher.hgl.desktop" "$out/share/applications/com.heroicgameslauncher.hgl.desktop"
+    install -D "src/frontend/assets/heroic-icon.svg" "$out/share/icons/hicolor/scalable/apps/com.heroicgameslauncher.hgl.svg"
+    substituteInPlace "$out/share/applications/com.heroicgameslauncher.hgl.desktop" \
+      --replace-fail "Exec=heroic-run --ozone-platform-hint=auto" "Exec=heroic"
+
+    runHook postInstall
+  '';
+
+  passthru = {
+    inherit (finalAttrs) pnpmDeps;
+    updateScript = nix-update-script { };
+  };
+
+  meta = with lib; {
+    description = "Native GOG, Epic, and Amazon Games Launcher for Linux, Windows and Mac";
+    homepage = "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher";
+    changelog = "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ aidalgol ];
+    # Heroic may work on nix-darwin, but it needs a dedicated maintainer for the platform.
+    # It may also work on other Linux targets, but all the game stores only
+    # support x86 Linux, so it would require extra hacking to run games via QEMU
+    # user emulation.  Upstream provide Linux builds only for x86_64.
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "heroic";
+  };
+})
