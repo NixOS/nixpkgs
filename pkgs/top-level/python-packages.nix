209ed26e8170117c3c6fc47dccf4bc1c3740208b
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-07-27 14:10:40 -0400

    python3Packages.otr: init at 2.1.0
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11081,6 +11081,8 @@ self: super: with self; {
 
   otpauth = callPackage ../development/python-modules/otpauth { };
 
+  otr = callPackage ../development/python-modules/otr { };
+
   ots-python = callPackage ../development/python-modules/ots-python { };
 
   ourgroceries = callPackage ../development/python-modules/ourgroceries { };
