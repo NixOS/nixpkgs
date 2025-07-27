6fda4f93ee26114d10d3a9869f1018812638be3f
nikstur <nikstur@outlook.com>
2025-07-27 21:18:57 +0200

    linux-pam: move scripts to separate output
    This enables us to use the lib and out output without pulling in bash.
    
    - Enable structuredAttrs
    - Enable strictDeps
    - Add bash to disallowedRequisites

--- pkgs/by-name/li/linux-pam/package.nix
+++ pkgs/by-name/li/linux-pam/package.nix
@@ -9,6 +9,8 @@
   ninja,
   audit,
   libxcrypt,
+  bash,
+  bashNonInteractive,
   nixosTests,
   meson,
   pkg-config,
@@ -31,7 +33,10 @@ stdenv.mkDerivation (finalAttrs: {
     repo = "linux-pam";
     tag = "v${finalAttrs.version}";
     hash = "sha256-kANcwxifQz2tYPSrSBSFiYNTm51Gr10L/zroCqm8ZHQ=";
+
   };
+
+  __structuredAttrs = true;
 
   # patching unix_chkpwd is required as the nix store entry does not have the necessary bits
   postPatch = ''
@@ -43,8 +48,11 @@ stdenv.mkDerivation (finalAttrs: {
     "out"
     "doc"
     "man"
+    "scripts"
     # "modules"
   ];
+
+  strictDeps = true;
 
   depsBuildBuild = [ buildPackages.stdenv.cc ];
   nativeBuildInputs = [
@@ -65,6 +73,7 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     db4
     libxcrypt
+    bash
   ]
   ++ lib.optionals stdenv.buildPlatform.isLinux [
     audit
@@ -89,7 +98,17 @@ stdenv.mkDerivation (finalAttrs: {
     (lib.mesonBool "examples" false)
   ];
 
+  postInstall = ''
+    moveToOutput sbin/pam_namespace_helper $scripts
+    moveToOutput etc/security/namespace.init $scripts
+  '';
+
   doCheck = false; # fails
+
+  outputChecks.out.disallowedRequisites = [
+    bash
+    bashNonInteractive
+  ];
 
   passthru = {
     tests = {
