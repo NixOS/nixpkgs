0d53eba642f22a1e58a97419c00d6e3afc72253c
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-27 08:47:19 +0000

    python3Packages.metaflow: 2.15.20 -> 2.16.6

--- pkgs/development/python-modules/metaflow/default.nix
+++ pkgs/development/python-modules/metaflow/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "metaflow";
-  version = "2.15.20";
+  version = "2.16.6";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Netflix";
     repo = "metaflow";
     tag = version;
-    hash = "sha256-D4Pf2/xhEJ22kDmmxYS0PiT5k2BJ3CBfTJZ4GrvkI58=";
+    hash = "sha256-UY15ibdJySucT429VfyqaqpBR54gP8ytHwLcWRe4gIs=";
   };
 
   build-system = [
