bd540f838b68d50809106de4e2fd4addc5f51cec
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-07-27 13:54:41 -0400

    python3Packages.sipsimple: init at 5.3.3.2
    NGI project.
    
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16481,6 +16481,8 @@ self: super: with self; {
 
   siphashc = callPackage ../development/python-modules/siphashc { };
 
+  sipsimple = callPackage ../development/python-modules/sipsimple { };
+
   sipyco = callPackage ../development/python-modules/sipyco { };
 
   sirius = toPythonModule (
