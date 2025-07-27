3dcd2027b438798ee2665b177f9f00070fdb74e5
K900 <me@0upti.me>
2025-07-27 09:13:03 +0300

    umu-launcher: don't die with parent
    Boo Legendary.

--- pkgs/by-name/um/umu-launcher/package.nix
+++ pkgs/by-name/um/umu-launcher/package.nix
@@ -32,6 +32,11 @@ buildFHSEnv {
   executableName = umu-launcher-unwrapped.meta.mainProgram;
   runScript = lib.getExe umu-launcher-unwrapped;
 
+  # Legendary spawns UMU, doesn't wait for it to exit,
+  # and immediately exits itself. This makes it so we can't
+  # die with parent, because parent is already dead.
+  dieWithParent = false;
+
   extraInstallCommands = ''
     ln -s ${umu-launcher-unwrapped}/lib $out/lib
     ln -s ${umu-launcher-unwrapped}/share $out/share
