7e52ac3bab03523b501b07d165a7933a263236ee
nikstur <nikstur@outlook.com>
2025-07-27 21:18:57 +0200

    systemd: remove bashInteractive
    The only thing that needed bashInteractive were bash scripts for RPM.
    Remove the RPM scripts (we definitely do not need) to get rid of
    bashInteractive.

--- pkgs/os-specific/linux/systemd/default.nix
+++ pkgs/os-specific/linux/systemd/default.nix
@@ -63,7 +63,6 @@
   libseccomp,
   withKexectools ? lib.meta.availableOn stdenv.hostPlatform kexec-tools,
   kexec-tools,
-  bashInteractive,
   bash,
   libmicrohttpd,
   libfido2,
@@ -399,7 +398,6 @@ stdenv.mkDerivation (finalAttrs: {
     libcap
     libuuid
     linuxHeaders
-    bashInteractive # for patch shebangs
   ]
 
   ++ lib.optionals withGcrypt [
@@ -517,6 +515,10 @@ stdenv.mkDerivation (finalAttrs: {
     # SSH
     (lib.mesonOption "sshconfdir" "")
     (lib.mesonOption "sshdconfdir" "no")
+
+    # RPM
+    # This stops building/installing RPM specific tools.
+    (lib.mesonOption "rpmmacrosdir" "no")
 
     # Features
 
