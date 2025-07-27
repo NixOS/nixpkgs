3084dc1a0077c28fc2a6d5eead53e05a315d1e14
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-07-27 14:19:01 -0400

    python3Packages.xcaplib: init at 2.0.1-unstable-2025-03-20
    NGI project.
    
    Co-authored-by: Acid Bong <acidbong@tilde.club>
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19768,6 +19768,8 @@ self: super: with self; {
 
   xboxapi = callPackage ../development/python-modules/xboxapi { };
 
+  xcaplib = callPackage ../development/python-modules/xcaplib { };
+
   xcffib = callPackage ../development/python-modules/xcffib { };
 
   xdg = callPackage ../development/python-modules/xdg { };
