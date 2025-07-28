4c54e18421fd7dbddfd312a967161461e9609a18
Marcel <me@m4rc3l.de>
2025-07-28 21:04:52 +0200

    python3Packages.wgnlpy: fix exception when WGPEER_A_LAST_HANDSHAKE_TIME is None
    Cherry-picked from
    https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/83019:
    
    > The value of a peer's last handshake NLA can be None which triggers an TypeError
    > exception inside wgnlpy. This breaks ifstate from time to time when it is used
    > to configure wireguard VPNs and this bug is triggered. Although a PR[1] was
    > opened longer time ago the issue had not been fixed upstream, yet.
    >
    > [1]: https://github.com/ArgosyLabs/wgnlpy/pull/5

--- pkgs/development/python-modules/wgnlpy/default.nix
+++ pkgs/development/python-modules/wgnlpy/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   setuptools,
   cryptography,
   pyroute2,
@@ -18,6 +19,16 @@ buildPythonPackage rec {
     rev = "v${version}";
     hash = "sha256-5XAfBiKx4SqouA57PxmaCb0ea7mT2VeUI1tgnQE/ZwQ=";
   };
+
+  patches = [
+    # see https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/83019
+    # Required for ifstate
+    # Upstream Issue/PR: https://github.com/ArgosyLabs/wgnlpy/pull/5
+    (fetchpatch {
+      url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/1f78a31dc3e8d7ffd4ff4b8c32fabc3ad0265ae2/community/py3-wgnlpy/0001-fix-exception-when-WGPEER_A_LAST_HANDSHAKE_TIME-is-N.patch";
+      hash = "sha256-MO5MMDXnaCPdakMlxCkiCBCDCiTFdG3V66l+AKb95X4=";
+    })
+  ];
 
   build-system = [ setuptools ];
 
