2a866d5813e55ed5c1c22aeb0b0c06b5824c1111
K900 <me@0upti.me>
2025-07-28 07:30:52 +0300

    linux_6_16: init at 6.16

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -219,6 +219,14 @@ in
           ];
         };
 
+        linux_6_16 = callPackage ../os-specific/linux/kernel/mainline.nix {
+          branch = "6.16";
+          kernelPatches = [
+            kernelPatches.bridge_stp_helper
+            kernelPatches.request_key_helper
+          ];
+        };
+
         linux_testing =
           let
             testing = callPackage ../os-specific/linux/kernel/mainline.nix {
@@ -732,6 +740,7 @@ in
     linux_6_6 = recurseIntoAttrs (packagesFor kernels.linux_6_6);
     linux_6_12 = recurseIntoAttrs (packagesFor kernels.linux_6_12);
     linux_6_15 = recurseIntoAttrs (packagesFor kernels.linux_6_15);
+    linux_6_16 = recurseIntoAttrs (packagesFor kernels.linux_6_16);
   }
   // lib.optionalAttrs config.allowAliases {
     linux_4_19 = throw "linux 4.19 was removed because it will reach its end of life within 24.11"; # Added 2024-09-21
@@ -801,7 +810,7 @@ in
   packageAliases = {
     linux_default = packages.linux_6_12;
     # Update this when adding the newest kernel major version!
-    linux_latest = packages.linux_6_15;
+    linux_latest = packages.linux_6_16;
     linux_rt_default = packages.linux_rt_5_15;
     linux_rt_latest = packages.linux_rt_6_6;
   }
