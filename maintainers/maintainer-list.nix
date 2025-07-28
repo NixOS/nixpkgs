5f9fbe39d78f513570cf6ac53157432954e42e1c
timoteuszelle <tim@oudesluijszelle.nl>
2025-07-28 14:10:50 +0200

    maintainers: add timoteuszelle

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25481,6 +25481,11 @@
     githubId = 174156;
     name = "timor";
   };
+  timoteuszelle = {
+    name = "timoteuszelle";
+    github = "timoteuszelle";
+    githubId = 46320568;
+  };
   timput = {
     email = "tim@timput.com";
     github = "TimPut";
