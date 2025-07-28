5fdc599923e1fbf57d6c023fe418945c4a3167e8
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-07-28 11:45:08 +0200

    teleport_16: 16.5.13 -> 16.5.14
    Changelog: https://github.com/gravitational/teleport/releases/tag/v16.5.14
    Diff: https://github.com/gravitational/teleport/compare/v16.5.13...v16.5.14

--- pkgs/by-name/te/teleport_16/package.nix
+++ pkgs/by-name/te/teleport_16/package.nix
@@ -4,10 +4,10 @@
   wasm-bindgen-cli_0_2_95,
 }:
 buildTeleport rec {
-  version = "16.5.13";
-  hash = "sha256-X9Ujgvp+2dFCoku0tjGW4W05X8QrnExFE+H1kMhf91A=";
-  vendorHash = "sha256-0+7xbIONnZs7dPpfpHPmep+k4XxQE8TS/eKz4F5a3V0=";
-  pnpmHash = "sha256-waBzmNs20wbuoBDObVFnJjEYs3NJ/bzQksVz7ltMD7M=";
+  version = "16.5.14";
+  hash = "sha256-24NVmJ97igUs45Ruuj9llRcHVsGMU1dtRm8f4R0pYG4=";
+  vendorHash = "sha256-PDgrfol2zrmS8yzvJQoK486nl+akD9LP4Sh5IVz/jNE=";
+  pnpmHash = "sha256-b6D7i68rX47+thleTeEfW+G72NxuqWNLmNXxJqe8ha8=";
   cargoHash = "sha256-04zykCcVTptEPGy35MIWG+tROKFzEepLBmn04mSbt7I=";
 
   wasm-bindgen-cli = wasm-bindgen-cli_0_2_95;
