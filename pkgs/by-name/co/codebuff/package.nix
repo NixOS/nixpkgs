425262eab5f780a394fe00ac4d3c2d25b07dee4e
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-28 04:41:24 +0000

    codebuff: 1.0.436 -> 1.0.441

--- pkgs/by-name/co/codebuff/package.nix
+++ pkgs/by-name/co/codebuff/package.nix
@@ -6,14 +6,14 @@
 
 buildNpmPackage rec {
   pname = "codebuff";
-  version = "1.0.436";
+  version = "1.0.441";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/codebuff/-/codebuff-${version}.tgz";
-    hash = "sha256-eMoF+YrZttplzN+S9rEuHEBPSbGQnwWOFuZ+vVFrpik=";
+    hash = "sha256-l57ZQTvvIR8mpFJGJeF6AqE6sbjIUkQdjlvdQ4UAQ9g=";
   };
 
-  npmDepsHash = "sha256-VTThvsHSiKSHmE7PYBUb5yZW2SKhLB7O5VjV8RhO9ZU=";
+  npmDepsHash = "sha256-/LiXKA0HdFg3K7xyioL0SKjWicktCpih1oJkEPLDzIA=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
