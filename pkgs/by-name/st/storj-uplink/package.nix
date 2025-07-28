e6dbba13d740bfa35d9fc9487c8db6a4f8efba3d
Paul Meyer <katexochen0@gmail.com>
2025-07-28 08:44:01 +0200

    storj-uplink: renew
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- pkgs/by-name/st/storj-uplink/package.nix
+++ pkgs/by-name/st/storj-uplink/package.nix
@@ -4,14 +4,14 @@
   fetchFromGitHub,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "storj-uplink";
   version = "1.133.5";
 
   src = fetchFromGitHub {
     owner = "storj";
     repo = "storj";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-MvaA8AkoP4FszEEM33QKDt5zeXMZ3XDt5uU1t6C6Q1I=";
   };
 
@@ -19,19 +19,16 @@ buildGoModule rec {
 
   vendorHash = "sha256-N1rEEM+oH2VVOvg9c8gICQ1aDO9FS/faVbQl0kj1jYM=";
 
-  ldflags = [
-    "-s"
-    "-w"
-  ];
+  ldflags = [ "-s" ];
 
   # Tests fail with 'listen tcp 127.0.0.1:0: bind: operation not permitted'.
   __darwinAllowLocalNetworking = true;
 
-  meta = with lib; {
+  meta = {
     description = "Command-line tool for Storj";
     homepage = "https://storj.io";
-    license = licenses.agpl3Only;
+    license = lib.licenses.agpl3Only;
     mainProgram = "uplink";
-    maintainers = with maintainers; [ felipeqq2 ];
+    maintainers = with lib.maintainers; [ felipeqq2 ];
   };
-}
+})
