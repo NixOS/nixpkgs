74b04d689b483600d5de1df8f06745f30efe4fa9
Andreas Skielboe <skielboe@gmail.com>
2025-07-29 11:53:56 +0000

    swiftlint: 0.59.0 -> 0.59.1 and add linux support

--- pkgs/by-name/sw/swiftlint/package.nix
+++ pkgs/by-name/sw/swiftlint/package.nix
@@ -1,25 +1,61 @@
 {
   stdenvNoCC,
+  stdenv,
   lib,
   fetchurl,
   unzip,
   nix-update-script,
+  autoPatchelfHook,
+  curl,
+  libxml2,
   versionCheckHook,
 }:
+let
+  system = stdenvNoCC.hostPlatform.system;
+in
 stdenvNoCC.mkDerivation rec {
   pname = "swiftlint";
-  version = "0.59.0";
+  version = "0.59.1";
 
   src = fetchurl {
-    url = "https://github.com/realm/SwiftLint/releases/download/${version}/portable_swiftlint.zip";
-    hash = "sha256-rsGt9hy2EklZEA7eQ/Tp9eztVLVlVn9LMPTeZz9fQYA=";
+    url =
+      let
+        filename =
+          {
+            x86_64-darwin = "portable_swiftlint.zip";
+            aarch64-darwin = "portable_swiftlint.zip";
+            x86_64-linux = "swiftlint_linux.zip";
+          }
+          .${system};
+      in
+      "https://github.com/realm/SwiftLint/releases/download/${version}/${filename}";
+
+    hash =
+      {
+        x86_64-darwin = "sha256-WPm+ikZ3kAyUXixhgWgiP03WIKDMZcnMxeoPcEM+icE=";
+        aarch64-darwin = "sha256-WPm+ikZ3kAyUXixhgWgiP03WIKDMZcnMxeoPcEM+icE=";
+        x86_64-linux = "sha256-05zlMGHw/YAMVHQfXnh4OZ99LXP4QfP5POLZE1K+4Ps=";
+      }
+      .${system};
   };
 
   dontPatch = true;
   dontConfigure = true;
   dontBuild = true;
 
-  nativeBuildInputs = [ unzip ];
+  nativeBuildInputs = [ unzip ] ++ lib.optionals stdenvNoCC.hostPlatform.isLinux [ autoPatchelfHook ];
+
+  buildInputs = lib.optionals stdenvNoCC.hostPlatform.isLinux [
+    curl
+    libxml2
+    stdenv.cc.cc.lib
+  ];
+
+  postFixup = lib.optionalString stdenvNoCC.hostPlatform.isLinux ''
+    # Copy libxml2 library for compatibility with binary's expected .so.2 name
+    mkdir -p $out/lib
+    cp ${libxml2.out}/lib/libxml2.so.16.0.4 $out/lib/libxml2.so.2
+  '';
 
   sourceRoot = ".";
 
@@ -43,7 +79,11 @@ stdenvNoCC.mkDerivation rec {
       matteopacini
       DimitarNestorov
     ];
-    platforms = lib.platforms.darwin;
+    platforms = [
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "x86_64-linux"
+    ];
     sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
   };
 }
