e964eef816de44586cd96d992cb5be865d710e9e
polyfloyd <floyd@polyfloyd.net>
2025-07-29 14:31:45 +0200

    python3Packages.pydle: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12775,6 +12775,8 @@ self: super: with self; {
 
   pydispatcher = callPackage ../development/python-modules/pydispatcher { };
 
+  pydle = callPackage ../development/python-modules/pydle { };
+
   pydmd = callPackage ../development/python-modules/pydmd { };
 
   pydocstyle = callPackage ../development/python-modules/pydocstyle { };
