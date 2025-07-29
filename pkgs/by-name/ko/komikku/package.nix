47c320747b654f345ae685365bb59dfd25ed87fa
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-07-29 16:49:51 +0200

    komikku: 1.82.0 -> 1.83.0
    (cherry picked from commit b41c142c6305e5fb49c898e56b80c5ba3ebe51d9)

--- pkgs/by-name/ko/komikku/package.nix
+++ pkgs/by-name/ko/komikku/package.nix
@@ -23,7 +23,7 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "komikku";
-  version = "1.82.0";
+  version = "1.83.0";
   pyproject = false;
 
   src = fetchFromGitea {
@@ -31,7 +31,7 @@ python3.pkgs.buildPythonApplication rec {
     owner = "valos";
     repo = "Komikku";
     tag = "v${version}";
-    hash = "sha256-F+RlfnKnMqlPTk1iv79ah/UaEjd7Og+gV1YVsZqkIBk=";
+    hash = "sha256-cwNSjzCy4lv71O3XAcDXVF+75MhQ6gMrMz/IuePCdak=";
   };
 
   nativeBuildInputs = [
