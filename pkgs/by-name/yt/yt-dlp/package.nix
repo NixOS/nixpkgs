d8670c30ecaa1f52e9b211ab777893ba135f0740
Nicky Mouha <nmouha@users.noreply.github.com>
2025-07-29 01:07:16 -0400

    yt-dlp: fix impersonation with curl-cffi

--- pkgs/by-name/yt/yt-dlp/package.nix
+++ pkgs/by-name/yt/yt-dlp/package.nix
@@ -5,12 +5,13 @@
   ffmpeg-headless,
   rtmpdump,
   atomicparsley,
+  pandoc,
+  installShellFiles,
   atomicparsleySupport ? true,
   ffmpegSupport ? true,
   rtmpSupport ? true,
   withAlias ? false, # Provides bin/youtube-dl for backcompat
   nix-update-script,
-  update-python-libraries,
   stdenv,
   fetchurl,
   autoPatchelfHook,
@@ -31,8 +32,11 @@ python3Packages.buildPythonApplication rec {
     hash = "sha256-VNUkCdrzbOwD+iD9BZUQFJlWXRc0tWJAvLnVKNZNPhQ=";
   };
 
-  build-system = with python3Packages; [
-    hatchling
+  build-system = with python3Packages; [ hatchling ];
+
+  nativeBuildInputs = [
+    installShellFiles
+    pandoc
   ];
 
   # expose optional-dependencies, but provide all features
@@ -89,6 +93,21 @@ python3Packages.buildPythonApplication rec {
 
   pythonRelaxDeps = [ "websockets" ];
 
+  preBuild = ''
+    python devscripts/make_lazy_extractors.py
+  '';
+
+  postBuild = ''
+    python devscripts/prepare_manpage.py yt-dlp.1.temp.md
+    pandoc -s -f markdown-smart -t man yt-dlp.1.temp.md -o yt-dlp.1
+    rm yt-dlp.1.temp.md
+
+    mkdir -p completions/{bash,fish,zsh}
+    python devscripts/bash-completion.py completions/bash/yt-dlp
+    python devscripts/zsh-completion.py completions/zsh/_yt-dlp
+    python devscripts/fish-completion.py completions/fish/yt-dlp.fish
+  '';
+
   # Ensure these utilities are available in $PATH:
   # - ffmpeg: post-processing & transcoding support
   # - rtmpdump: download files over RTMP
@@ -105,25 +124,30 @@ python3Packages.buildPythonApplication rec {
       ''--prefix PATH : "${lib.makeBinPath packagesToBinPath}"''
     ];
 
-  setupPyBuildFlags = [
-    "build_lazy_extractors"
-  ];
-
   # Requires network
   doCheck = false;
 
-  postInstall = lib.optionalString withAlias ''
+  postInstall = ''
+    installManPage yt-dlp.1
+
+    installShellCompletion \
+      --bash completions/bash/yt-dlp \
+      --fish completions/fish/yt-dlp.fish \
+      --zsh completions/zsh/_yt-dlp
+
+    install -Dm644 Changelog.md README.md -t "$out/share/doc/yt_dlp"
+  ''
+  + lib.optionalString withAlias ''
     ln -s "$out/bin/yt-dlp" "$out/bin/youtube-dl"
   '';
 
-  passthru.updateScript = [
-    update-python-libraries
-    (toString ./.)
-  ];
+  passthru.updateScript = nix-update-script { };
 
-  meta = with lib; {
-    homepage = "https://github.com/yt-dlp/yt-dlp/";
+  meta = {
+    changelog = "https://github.com/yt-dlp/yt-dlp/blob/${version}/Changelog.md";
     description = "Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)";
+    homepage = "https://github.com/yt-dlp/yt-dlp/";
+    license = lib.licenses.unlicense;
     longDescription = ''
       yt-dlp is a youtube-dl fork based on the now inactive youtube-dlc.
 
@@ -132,12 +156,10 @@ python3Packages.buildPythonApplication rec {
       youtube-dl is released to the public domain, which means
       you can modify it, redistribute it or use it however you like.
     '';
-    changelog = "https://github.com/yt-dlp/yt-dlp/blob/HEAD/Changelog.md";
-    license = licenses.unlicense;
-    maintainers = with maintainers; [
+    mainProgram = "yt-dlp";
+    maintainers = with lib.maintainers; [
       SuperSandro2000
       donteatoreo
     ];
-    mainProgram = "yt-dlp";
   };
 }
