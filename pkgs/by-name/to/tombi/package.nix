23fa2224a89aae4c8cfe16911007c664f54c27f3
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-29 06:04:53 +0000

    tombi: 0.4.34 -> 0.4.37

--- pkgs/by-name/to/tombi/package.nix
+++ pkgs/by-name/to/tombi/package.nix
@@ -7,19 +7,19 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "tombi";
-  version = "0.4.34";
+  version = "0.4.37";
 
   src = fetchFromGitHub {
     owner = "tombi-toml";
     repo = "tombi";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-CCSqfgi7vTcDVgeOm/blqpw2Nq7rgov+R8+oYhlICHM=";
+    hash = "sha256-K2JIP9dqeAbfc4NefN8zdvzvRqGnH7qTLUzD11vDnd0=";
   };
 
   # Tests relies on the presence of network
   doCheck = false;
   cargoBuildFlags = [ "--package tombi-cli" ];
-  cargoHash = "sha256-2dLkZnGOVclp0EnAKsVH8HRTOXtq/1ADkoS5UiLGGnQ=";
+  cargoHash = "sha256-Vkj0BXvY9hGS2pq8mz0ZKhZBQLakh+hIIZ8Py5cgtl8=";
 
   postPatch = ''
     substituteInPlace Cargo.toml \
