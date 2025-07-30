cbd0e8a1d1a13f327349d882a4e7af5f09aba23f
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-30 14:02:49 +0000

    firejail: 0.9.74 -> 0.9.76

--- pkgs/by-name/fi/firejail/package.nix
+++ pkgs/by-name/fi/firejail/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "firejail";
-  version = "0.9.74";
+  version = "0.9.76";
 
   src = fetchFromGitHub {
     owner = "netblue30";
     repo = "firejail";
     rev = version;
-    sha256 = "sha256-BKEW2IWatzePGREAA479eaP6bJb1i2fRs/GZcyLinrM=";
+    sha256 = "sha256-0zb5olSuXOHeRj4dqeevubedjqOuDnUEBQaD/vOj2CM=";
   };
 
   nativeBuildInputs = [
