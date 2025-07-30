a0a49ea3b15fa7fd06c4501b9a4000aeeb0b1dc3
K900 <me@0upti.me>
2025-07-30 09:29:57 +0300

    gamescope: use pending upstream patch for unvendoring

--- pkgs/by-name/ga/gamescope/package.nix
+++ pkgs/by-name/ga/gamescope/package.nix
@@ -65,17 +65,11 @@ stdenv.mkDerivation (finalAttrs: {
     # patch relative gamescopereaper path with absolute
     ./gamescopereaper.patch
 
-    # Revert change to always use vendored stb/glm libraries
-    # Upstream discussion: https://github.com/ValveSoftware/gamescope/pull/1751
+    # Pending upstream patch to allow using system libraries
+    # See: https://github.com/ValveSoftware/gamescope/pull/1846
     (fetchpatch {
-      url = "https://github.com/ValveSoftware/gamescope/commit/baae74c4b13676fa76a8b200f21ac78f55079734.patch";
-      revert = true;
-      hash = "sha256-XpbyLQ4R9KgBR3hlrgPzmM7Zxr2jm4Q10zGjyhh/Qxw=";
-    })
-    (fetchpatch {
-      url = "https://github.com/ValveSoftware/gamescope/commit/72bae179ba2ebbbc91ed07c7f66e7e4964a4cd9e.patch";
-      revert = true;
-      hash = "sha256-aglfGvEuycNyPlaFYxqqvPAgFpWns3xZ3B2GiAefxtg=";
+      url = "https://github.com/ValveSoftware/gamescope/commit/4ce1a91fb219f570b0871071a2ec8ac97d90c0bc.diff";
+      hash = "sha256-O358ScIIndfkc1S0A8g2jKvFWoCzcXB/g6lRJamqOI4=";
     })
   ];
 
@@ -95,6 +89,9 @@ stdenv.mkDerivation (finalAttrs: {
   mesonFlags = [
     (lib.mesonBool "enable_gamescope" enableExecutable)
     (lib.mesonBool "enable_gamescope_wsi_layer" enableWsi)
+
+    (lib.mesonOption "glm_include_dir" "${lib.getInclude glm}/include")
+    (lib.mesonOption "stb_include_dir" "${lib.getInclude stb}/include/stb")
   ];
 
   # don't install vendored vkroots etc
@@ -133,7 +130,6 @@ stdenv.mkDerivation (finalAttrs: {
     wayland
     wayland-protocols
     vulkan-loader
-    glm
     luajit
   ]
   ++ lib.optionals enableWsi [
@@ -163,7 +159,6 @@ stdenv.mkDerivation (finalAttrs: {
       gbenchmark
       pixman
       libcap
-      stb
       lcms
     ]
   );
