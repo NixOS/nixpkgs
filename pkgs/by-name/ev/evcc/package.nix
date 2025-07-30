857f3432737a760ed8be9c3d130c391f4b845cb9
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-07-30 15:50:20 +0200

    evcc: 0.205.0 -> 0.206.0
    https://github.com/evcc-io/evcc/releases/tag/0.206.0

--- pkgs/by-name/ev/evcc/package.nix
+++ pkgs/by-name/ev/evcc/package.nix
@@ -17,16 +17,16 @@
 }:
 
 let
-  version = "0.205.0";
+  version = "0.206.0";
 
   src = fetchFromGitHub {
     owner = "evcc-io";
     repo = "evcc";
     tag = version;
-    hash = "sha256-T97f5K4TDaAdqm2zU8+cEyq1YIzwy35tX8mb3pPkhIo=";
+    hash = "sha256-ZDcfQQrybx11SenbTvTA2iY96NEkzJPMRzSOOlUP3So=";
   };
 
-  vendorHash = "sha256-gbTh9/Ny4JVCbpzz+opN92m6LQhPEBj/XuLLZ4M6tIc=";
+  vendorHash = "sha256-jiksFU0ce7RiDy7xqVZiKancEiWSMEHDazPc6+l6DNQ=";
 
   commonMeta = with lib; {
     license = licenses.mit;
@@ -52,7 +52,7 @@ buildGo124Module rec {
 
   npmDeps = fetchNpmDeps {
     inherit src;
-    hash = "sha256-OmiINzMCXbvceCDZ9zYTQKfWQ3iUgovMznmVoQkQ5DE=";
+    hash = "sha256-yQ+MnXjixNQfWSVnRzmxRtQAvLecdLMDQSXyuIdzGnU=";
   };
 
   nativeBuildInputs = [
