adab7a80d9cda6c92aba2ba1d8db54a932024a93
sternenseemann <sternenseemann@systemli.org>
2025-07-31 13:29:18 +0200

    ghc: 9.10.2 -> 9.10.3-rc1

---
+++ pkgs/development/compilers/ghc/9.10.3.nix
@@ -0,0 +1,5 @@
+import ./common-hadrian.nix rec {
+  version = "9.10.2.20250725";
+  url = "https://downloads.haskell.org/~ghc/9.10.3-rc1/ghc-${version}-src.tar.gz";
+  sha256 = "1008636a0673055ba649e8e9d604788a0859031f03d062a5700ffde62d363eb9";
+}
