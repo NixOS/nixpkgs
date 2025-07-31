c9e4a255dfc759ff3572a0bee8b4f6db16795afa
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-31 00:51:24 +0000

    cflow: 1.7 -> 1.8

--- pkgs/by-name/cf/cflow/package.nix
+++ pkgs/by-name/cf/cflow/package.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation rec {
   pname = "cflow";
-  version = "1.7";
+  version = "1.8";
 
   src = fetchurl {
     url = "mirror://gnu/${pname}/${pname}-${version}.tar.bz2";
-    sha256 = "sha256-0BFGyvkAHiZhM0F8KoJYpktfwW/LCCoU9lKCBNDJcIY=";
+    sha256 = "sha256-gyFie1W2x4d/akP8xvn4RqlLFHaggaA1Rl96eNNJmrg=";
   };
 
   patchPhase = ''
