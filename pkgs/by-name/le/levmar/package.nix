ddad5093338c09dbefe4aa2e3b0882f4585b0556
Zexin Yuan <git@yzx9.xyz>
2025-07-31 14:31:48 +0800

    levmar: fix build on darwin

--- pkgs/by-name/le/levmar/package.nix
+++ pkgs/by-name/le/levmar/package.nix
@@ -17,6 +17,11 @@ stdenv.mkDerivation (finalAttrs: {
     substituteInPlace levmar.h --replace-fail "define HAVE_LAPACK" "undef HAVE_LAPACK"
     sed -i 's/LAPACKLIBS=.*/LAPACKLIBS=/' Makefile
     substituteInPlace Makefile --replace-fail "gcc" "${stdenv.cc.targetPrefix}cc"
+  ''
+  + lib.optionalString stdenv.cc.isClang ''
+    substituteInPlace compiler.h \
+      --replace-fail "#define LM_FINITE finite // ICC, GCC" \
+                     "#define LM_FINITE isfinite // ICC, GCC"
   '';
 
   installPhase = ''
