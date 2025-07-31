2532dce655049f2696f983675416f4cee7364e65
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-31 22:06:15 +0000

    mkbrr: 1.13.1 -> 1.14.0

--- pkgs/by-name/mk/mkbrr/package.nix
+++ pkgs/by-name/mk/mkbrr/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "mkbrr";
-  version = "1.13.1";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "autobrr";
     repo = "mkbrr";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-fX58/PRTVhUATWX5HOTtf6d6lmSRuE3xSrg/Qtzv/rs=";
+    hash = "sha256-k8//hfqDwiF5NkpvflkGaSybYVb9lWwj91hH/KrofOU=";
   };
 
-  vendorHash = "sha256-G8WM5x99UZfAZUkE5W37Ogx/OKk8JypPzGBrIuBOFNo=";
+  vendorHash = "sha256-MEDzZd67iXPY/MioMd1FcTLY+8CdJN7+oC7qus63yJ8=";
 
   ldflags = [
     "-s"
