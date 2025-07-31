b740b5cdbf2949317ed23d7e5bb199207aecec29
Matthias Beyer <mail@beyermatthias.de>
2025-07-31 10:46:51 +0200

    fail2ban-dashboard: Add service module
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1450,6 +1450,7 @@
   ./services/security/endlessh-go.nix
   ./services/security/endlessh.nix
   ./services/security/esdm.nix
+  ./services/security/fail2ban-dashboard.nix
   ./services/security/fail2ban.nix
   ./services/security/fprintd.nix
   ./services/security/haveged.nix
