9d126d08ddc7b9e80a3f23d1cac30ceb3f842bd3
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-08-01 01:57:49 +0200

    nixos/tests/prosody: fix warning

--- nixos/tests/xmpp/prosody.nix
+++ nixos/tests/xmpp/prosody.nix
@@ -41,9 +41,9 @@ import ../make-test-python.nix {
       {
         security.pki.certificateFiles = [ "${cert pkgs}/cert.pem" ];
         networking.extraHosts = ''
-          ${nodes.server.config.networking.primaryIPAddress} example.com
-          ${nodes.server.config.networking.primaryIPAddress} conference.example.com
-          ${nodes.server.config.networking.primaryIPAddress} uploads.example.com
+          ${nodes.server.networking.primaryIPAddress} example.com
+          ${nodes.server.networking.primaryIPAddress} conference.example.com
+          ${nodes.server.networking.primaryIPAddress} uploads.example.com
         '';
         environment.systemPackages = [
           (pkgs.callPackage ./xmpp-sendmessage.nix { connectTo = "example.com"; })
