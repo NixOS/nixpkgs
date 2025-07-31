082f952c454b15be17309d8877178bf4ddb8d97e
Tau <git@alice-carroll.pet>
2025-07-31 17:15:21 +0300

    redo-apenwarr: refactor

--- pkgs/by-name/re/redo-apenwarr/package.nix
+++ pkgs/by-name/re/redo-apenwarr/package.nix
@@ -8,48 +8,69 @@
   perl,
   installShellFiles,
   gnumake42,
+  fetchpatch,
+  pkg-config,
+  gtk2,
+  libpng,
+  tetex,
+  openssl,
+  ghostscript,
   doCheck ? true,
 }:
-stdenv.mkDerivation rec {
-
+stdenv.mkDerivation (finalAttrs: {
   pname = "redo-apenwarr";
   version = "0.42d";
 
-  src = fetchFromGitHub rec {
+  src = fetchFromGitHub {
     owner = "apenwarr";
     repo = "redo";
-    rev = "${repo}-${version}";
-    sha256 = "/QIMXpVhVLAIJa3LiOlRKzbUztIWZygkWZUKN4Nrh+M=";
+    tag = "redo-${finalAttrs.version}";
+    hash = "sha256-/QIMXpVhVLAIJa3LiOlRKzbUztIWZygkWZUKN4Nrh+M=";
   };
 
-  postPatch = ''
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/magistau/redo/commit/bbad2ed2892ab942796431c5d087c2854daa10d8.diff";
+      hash = "sha256-o4MVAzcJdAmB1eIcdjK9hZ1/wWfE1/Yf8jXmm8lFTAs=";
+    })
+    (fetchpatch {
+      url = "https://github.com/magistau/redo/commit/76214f67e782f9c0bc4926fc053edd1e33992006.diff";
+      hash = "sha256-LNuExO5YDYFP3BxPAH07VAPR3VK6D45rOKF/8XXfA5g=";
+    })
+  ];
 
-    patchShebangs minimal/do
-
-  ''
-  + lib.optionalString doCheck ''
+  postPatch = lib.optionalString doCheck ''
     unset CC CXX
 
     substituteInPlace minimal/do.test \
-      --replace "/bin/pwd" "${coreutils}/bin/pwd"
+      --replace-warn "/bin/pwd" "${coreutils}/bin/pwd"
 
     substituteInPlace t/105-sympath/all.do \
-      --replace "/bin/pwd" "${coreutils}/bin/pwd"
+      --replace-warn "/bin/pwd" "${coreutils}/bin/pwd"
 
     substituteInPlace t/all.do \
-      --replace "/bin/ls" "ls"
+      --replace-warn "/bin/ls" "ls"
 
     substituteInPlace t/110-compile/hello.o.do \
-      --replace "/usr/include" "${lib.getDev stdenv.cc.libc}/include"
+      --replace-warn "/usr/include" "${lib.getDev stdenv.cc.libc}/include"
 
     substituteInPlace t/200-shell/nonshelltest.do \
-      --replace "/usr/bin/env perl" "${perl}/bin/perl"
-
+      --replace-warn "/usr/bin/env perl" "${perl}/bin/perl"
   '';
 
   inherit doCheck;
-
   checkTarget = "test";
+
+  nativeCheckInputs = [
+    pkg-config
+  ];
+  checkInputs = [
+    gtk2
+    libpng
+    tetex
+    openssl
+    ghostscript
+  ];
 
   outputs = [
     "out"
@@ -86,4 +107,4 @@ stdenv.mkDerivation rec {
     license = licenses.asl20;
     platforms = python3.meta.platforms;
   };
-}
+})
