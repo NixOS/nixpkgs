44430ebdfeced3d2aef4fce7bbd05713177319c6
06kellyjac <dev@j-k.io>
2025-07-31 13:08:48 +0100

    python3Packages.zalgolib: init at 0.2.2-unstable-2023-12-13

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20064,6 +20064,8 @@ self: super: with self; {
 
   zadnegoale = callPackage ../development/python-modules/zadnegoale { };
 
+  zalgolib = callPackage ../development/python-modules/zalgolib { };
+
   zamg = callPackage ../development/python-modules/zamg { };
 
   zammad-py = callPackage ../development/python-modules/zammad-py { };
