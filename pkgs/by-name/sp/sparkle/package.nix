0f24bee7a3106b60aab196bd637fbbd148472247
mivorasu <mivorasu@users.noreply.github.com>
2025-08-01 06:41:57 +0800

    sparkle: 1.6.9 -> 1.6.10

--- pkgs/by-name/sp/sparkle/package.nix
+++ pkgs/by-name/sp/sparkle/package.nix
@@ -2,21 +2,30 @@
   lib,
   stdenv,
   fetchurl,
-  dpkg,
   autoPatchelfHook,
-  nss,
-  nspr,
+  dpkg,
   alsa-lib,
-  openssl,
-  webkitgtk_4_0,
-  udev,
-  libayatana-appindicator,
+  at-spi2-atk,
+  cairo,
+  cups,
+  dbus,
+  expat,
+  glib,
+  gtk3,
   libGL,
+  libgbm,
+  libxkbcommon,
+  musl,
+  nspr,
+  nss,
+  pango,
+  udev,
+  xorg,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sparkle";
-  version = "1.6.9";
+  version = "1.6.10";
 
   src =
     let
@@ -31,22 +40,40 @@ stdenv.mkDerivation (finalAttrs: {
     fetchurl {
       url = "https://github.com/xishang0128/sparkle/releases/download/${finalAttrs.version}/sparkle-linux-${finalAttrs.version}-${arch}.deb";
       hash = selectSystem {
-        x86_64-linux = "sha256-db9YDfh8EA+EDtD/XO9pBaHkKcbqsDAkQF7lXxAo18I=";
-        aarch64-linux = "sha256-CfgcFhsu6G6Mm82paz/9yWwO4OltJZRBV6QffseHcrg=";
+        x86_64-linux = "sha256-07hgkZi3WVBz0pLo871tfzV5uUlAx8hizaDshpo5eOg=";
+        aarch64-linux = "sha256-N/oEer06zhn23/p6jQaZ8pCkE9F8dzIHL5n7kvHP8X0=";
       };
     };
 
   nativeBuildInputs = [
-    dpkg
     autoPatchelfHook
+    dpkg
   ];
 
   buildInputs = [
-    nss
-    nspr
     alsa-lib
-    openssl
-    webkitgtk_4_0
+    at-spi2-atk
+    cairo
+    cups
+    dbus
+    expat
+    glib
+    gtk3
+    libGL
+    libgbm
+    libxkbcommon
+    musl
+    nspr
+    nss
+    pango
+    udev
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libxcb
     (lib.getLib stdenv.cc.cc)
   ];
 
@@ -54,24 +81,18 @@ stdenv.mkDerivation (finalAttrs: {
     runHook preInstall
 
     mkdir -p $out/bin
+    chmod 0755 opt/sparkle/resources/files/sysproxy
     cp -r opt $out/opt
-    cp -r usr/share $out/share
-    substituteInPlace $out/share/applications/sparkle.desktop \
+    substituteInPlace usr/share/applications/sparkle.desktop \
       --replace-fail "/opt/sparkle/sparkle" "sparkle"
+    cp -r usr/share $out/share
     ln -s $out/opt/sparkle/sparkle $out/bin/sparkle
 
     runHook postInstall
   '';
 
   preFixup = ''
-    patchelf --add-needed libGL.so.1 \
-      --add-rpath ${
-        lib.makeLibraryPath [
-          libGL
-          udev
-          libayatana-appindicator
-        ]
-      } $out/opt/sparkle/sparkle
+    patchelf --add-needed libGL.so.1 $out/opt/sparkle/sparkle
   '';
 
   passthru.updateScript = ./update.sh;
@@ -79,13 +100,13 @@ stdenv.mkDerivation (finalAttrs: {
   meta = {
     description = "Another Mihomo GUI";
     homepage = "https://github.com/xishang0128/sparkle";
+    license = lib.licenses.gpl3Plus;
     mainProgram = "sparkle";
+    maintainers = with lib.maintainers; [ ];
     platforms = [
       "aarch64-linux"
       "x86_64-linux"
     ];
-    license = lib.licenses.gpl3Plus;
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
-    maintainers = with lib.maintainers; [ ];
   };
 })
