37c260304c5a019ae76605bef174b68a86567d62
Tristan Ross <tristan.ross@midstall.com>
2025-07-30 21:11:28 -0700

    rustc: fix pkgsLLVM
    Fixes `libunwind` from not being included due to improper usage of
    `lib.optional` by passing a list instead of a single item. Drops the
    hack for `llvmPackages` since just passing it through works now.

--- pkgs/development/compilers/rust/rustc.nix
+++ pkgs/development/compilers/rust/rustc.nix
@@ -94,7 +94,6 @@ stdenv.mkDerivation (finalAttrs: {
         (stdenv.hostPlatform.isLinux && !withBundledLLVM && !stdenv.targetPlatform.isFreeBSD && useLLVM)
         "--push-state --as-needed -L${llvmPackages.libcxx}/lib -lc++ -lc++abi -lLLVM-${lib.versions.major llvmPackages.llvm.version} --pop-state"
     ++ optional (stdenv.hostPlatform.isDarwin && !withBundledLLVM) "-lc++ -lc++abi"
-    ++ optional stdenv.hostPlatform.isFreeBSD "-rpath ${llvmPackages.libunwind}/lib"
     ++ optional stdenv.hostPlatform.isDarwin "-rpath ${llvmSharedForHost.lib}/lib"
   );
 
@@ -385,15 +384,7 @@ stdenv.mkDerivation (finalAttrs: {
     zlib
   ]
   ++ optional (!withBundledLLVM) llvmShared.lib
-  ++ optional (useLLVM && !withBundledLLVM && !stdenv.targetPlatform.isFreeBSD) [
-    llvmPackages.libunwind
-    # Hack which is used upstream https://github.com/gentoo/gentoo/blob/master/dev-lang/rust/rust-1.78.0.ebuild#L284
-    (runCommandLocal "libunwind-libgcc" { } ''
-      mkdir -p $out/lib
-      ln -s ${llvmPackages.libunwind}/lib/libunwind.so $out/lib/libgcc_s.so
-      ln -s ${llvmPackages.libunwind}/lib/libunwind.so $out/lib/libgcc_s.so.1
-    '')
-  ];
+  ++ optional (useLLVM && !withBundledLLVM) llvmPackages.libunwind;
 
   outputs = [
     "out"
