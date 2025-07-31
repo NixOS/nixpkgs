NixOS/nixpkgs

commit 1010b2606b6bda1b4e987886c88e6e94759d983c

e2fsprogs: 1.47.2 -> 1.47.3

pkgs/by-name/e2/e2fsprogs/package.nix
@@ -3,7 +3,6 @@
   stdenv,
   buildPackages,
   fetchurl,
-  fetchpatch,
   pkg-config,
   libuuid,
   gettext,
@@ -18,26 +17,12 @@
 
 stdenv.mkDerivation rec {
   pname = "e2fsprogs";
-  version = "1.47.2";
+  version = "1.47.3";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/tytso/e2fsprogs/v${version}/e2fsprogs-${version}.tar.xz";
-    hash = "sha256-CCQuZMoOgZTZwcqtSXYrGSCaBjGBmbY850rk7y105jw=";
+    hash = "sha256-hX5u+AD+qiu0V4+8gQIUvl08iLBy6lPFOEczqWVzcyk=";
   };
-
-  # 2025-05-31: Fix libarchive, from https://github.com/tytso/e2fsprogs/pull/230
-  patches = [
-    (fetchpatch {
-      name = "0001-create_inode_libarchive.c-define-libarchive-dylib-for-darwin.patch";
-      url = "https://github.com/tytso/e2fsprogs/commit/e86c65bc7ee276cd9ca920d96e18ed0cddab3412.patch";
-      hash = "sha256-HFZAznaNl5rzgVEvYx1LDKh2jd/VEXD/o0wypIh4TR8=";
-    })
-    (fetchpatch {
-      name = "0002-mkgnutar.pl-avoid-uninitialized-username-variable.patch";
-      url = "https://github.com/tytso/e2fsprogs/commit/9217c359db1d1b6d031a0e2ca9a885634fed00da.patch";
-      hash = "sha256-iDXmLq77eJolH1mkXSbvZ9tRVtGQt2F45CdkVphUZSs=";
-    })
-  ];
 
   # fuse2fs adds 14mb of dependencies
   outputs = [

commit cfb8ec72c8b922503cc31e67b7852788bc9cc680

e2fsprogs: fix build for musl
Patch has not been applied upstream yet, but appears to have been
submitted by the frequent contributor who authored the change that
introduced the regression.

Fixes: 1010b2606b6b ("e2fsprogs: 1.47.2 -> 1.47.3")

pkgs/by-name/e2/e2fsprogs/package.nix
@@ -24,6 +24,14 @@ stdenv.mkDerivation rec {
     hash = "sha256-hX5u+AD+qiu0V4+8gQIUvl08iLBy6lPFOEczqWVzcyk=";
   };
 
+  patches = [
+    (fetchurl {
+      name = "portability.patch";
+      url = "https://lore.kernel.org/linux-ext4/20250722194053.GO2672022@frogsfrogsfrogs/raw";
+      hash = "sha256-8I5bBZB9VA8wHXYJc8f9xSVBs1cTzj+5+4fVY/Ojvxw=";
+    })
+  ];
+
   # fuse2fs adds 14mb of dependencies
   outputs = [
     "bin"