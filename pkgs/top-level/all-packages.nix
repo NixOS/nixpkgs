amansakhuja/nixpkgs5

sddm-tokyo-night: init at 0-unstable-2023-06-13

pkgs/top-level/all-packages.nix
@@ -11422,6 +11422,8 @@ with pkgs;
 
   sddm-chili-theme = libsForQt5.callPackage ../data/themes/chili-sddm { };
 
+  sddm-tokyo-night = libForQt5.callPackage ../data/themes/sddm-tokyo-night { };
+
   sddm-sugar-dark = libsForQt5.callPackage ../data/themes/sddm-sugar-dark { };
 
   sdrangel = qt6Packages.callPackage ../applications/radio/sdrangel { };

NixOS/nixpkgs

pkgs/top-level/all-packages.nix
@@ -11422,6 +11422,8 @@ with pkgs;
 
   sddm-chili-theme = libsForQt5.callPackage ../data/themes/chili-sddm { };
 
+  sddm-tokyo-night = libsForQt5.callPackage ../data/themes/sddm-tokyo-night { };
+
   sddm-sugar-dark = libsForQt5.callPackage ../data/themes/sddm-sugar-dark { };
 
   sdrangel = qt6Packages.callPackage ../applications/radio/sdrangel { };
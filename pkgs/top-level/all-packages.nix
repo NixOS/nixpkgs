16859b9f186b17e2266957d39dd7862d511a1ba3
tohmais <callum.wishart@protonmail.com>
2025-05-07 12:08:50 +1000

    sddm-tokyo-night: init at 0-unstable-2023-06-13

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11422,6 +11422,8 @@ with pkgs;
 
   sddm-chili-theme = libsForQt5.callPackage ../data/themes/chili-sddm { };
 
+  sddm-tokyo-night = libsForQt5.callPackage ../data/themes/sddm-tokyo-night { };
+
   sddm-sugar-dark = libsForQt5.callPackage ../data/themes/sddm-sugar-dark { };
 
   sdrangel = qt6Packages.callPackage ../applications/radio/sdrangel { };
