78f91d94db3a57755f28be5817ec65a5014825fa
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-31 22:00:31 +0000

    mihomo-party: 1.7.6 -> 1.7.7

--- pkgs/by-name/mi/mihomo-party/package.nix
+++ pkgs/by-name/mi/mihomo-party/package.nix
@@ -16,7 +16,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mihomo-party";
-  version = "1.7.6";
+  version = "1.7.7";
 
   src =
     let
@@ -31,8 +31,8 @@ stdenv.mkDerivation (finalAttrs: {
     fetchurl {
       url = "https://github.com/mihomo-party-org/mihomo-party/releases/download/v${finalAttrs.version}/mihomo-party-linux-${finalAttrs.version}-${arch}.deb";
       hash = selectSystem {
-        x86_64-linux = "sha256-83RajPreGieOYBAkoR6FsFREnOGDDuMK6+Qg+R/koac=";
-        aarch64-linux = "sha256-oWOXLUYWRKRgPtNv9ZvM1ODd44dhymVTKHJBK/xxOOs=";
+        x86_64-linux = "sha256-u6h85NW7c57MDW0Vyg7lMbpYc0Ea2bDF6OKO9RJCGEU=";
+        aarch64-linux = "sha256-KemT52mV7BVaanDCOUqPD5YSfx7q6j8Q5B/KyYX9CK8=";
       };
     };
 
