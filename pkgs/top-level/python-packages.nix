5500ab05c9c87c0c70b377a41f17c79eff60e5b3
06kellyjac <dev@j-k.io>
2025-07-31 12:52:46 +0100

    python3Packages.ecoji: init at 0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4501,6 +4501,8 @@ self: super: with self; {
 
   ecoaliface = callPackage ../development/python-modules/ecoaliface { };
 
+  ecoji = callPackage ../development/python-modules/ecoji { };
+
   ecos = callPackage ../development/python-modules/ecos { };
 
   ecpy = callPackage ../development/python-modules/ecpy { };
