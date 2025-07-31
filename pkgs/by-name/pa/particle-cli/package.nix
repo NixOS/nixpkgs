ddcbb8f489fc6e2f85b6c62b444b961c9dd767d9
R. Ryantm <ryantm-bot@ryantm.com>
2025-07-31 23:30:00 +0000

    particle-cli: 3.38.2 -> 3.40.0

--- pkgs/by-name/pa/particle-cli/package.nix
+++ pkgs/by-name/pa/particle-cli/package.nix
@@ -8,16 +8,16 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "particle-cli";
-  version = "3.38.2";
+  version = "3.40.0";
 
   src = fetchFromGitHub {
     owner = "particle-iot";
     repo = "particle-cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-/MfT7+g3l+5Y3mRGl0yiDMRXL2heWZzVNm+LfTmy9SA=";
+    hash = "sha256-qfwj0hs+4p00pALrIMAjHCM7Cdoj6+sxV/nMyI2rYNg=";
   };
 
-  npmDepsHash = "sha256-L/DfZWvJRZzHvf9pP7bHEJt85KT0s46+KErkTNRgH04=";
+  npmDepsHash = "sha256-o8G6+xYOg80vgjJ6Skm7ydysuH71uN9erj7Nd4QOB2I=";
 
   buildInputs = [
     udev
