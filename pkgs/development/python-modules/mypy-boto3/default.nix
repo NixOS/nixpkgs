1d40f746d45a9d22cdb31e8e7cb1c21107823702
Fabian Affolter <fabian@affolter-engineering.ch>
2025-07-31 09:48:01 +0200

    python312Packages.mypy-boto3-iotwireless: 1.39.7 -> 1.39.17

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -694,8 +694,8 @@ rec {
       "sha256-uuYtLyzgiNttKZ8tpH8gRidBqRSvOJmIv/PgbaDGjCQ=";
 
   mypy-boto3-iotwireless =
-    buildMypyBoto3Package "iotwireless" "1.39.7"
-      "sha256-r5Ht/a2gnZ/t83dH4dKjj0dcr3zifHY2tQMQXVgl6+Y=";
+    buildMypyBoto3Package "iotwireless" "1.39.17"
+      "sha256-C/BE7hpb30LKAJQwNJuBIA6dtKs92MrdwfAiubvkJlY=";
 
   mypy-boto3-ivs =
     buildMypyBoto3Package "ivs" "1.39.0"
