881b7f172aea5fa801b9ff6ed33f1eb8e5ae5acf
Matthias Beyer <mail@beyermatthias.de>
2025-07-31 10:59:12 +0200

    fixup! fail2ban-dashboard: Add service module

--- nixos/modules/services/security/fail2ban-dashboard.nix
+++ nixos/modules/services/security/fail2ban-dashboard.nix
@@ -71,7 +71,10 @@ in
         ProtectProc = "noaccess";
         ProtectSystem = "strict";
         RemoveIPC = true;
-        RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
+        ];
         RestrictNamespaces = true;
         RestrictRealtime = true;
         RestrictSUIDSGID = true;
