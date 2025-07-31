99257be995d9529e13511c3d76fb155cf14040c2
06kellyjac <dev@j-k.io>
2025-07-31 15:51:17 +0100

    promptfoo: move to finalAttrs pattern

--- pkgs/by-name/pr/promptfoo/package.nix
+++ pkgs/by-name/pr/promptfoo/package.nix
@@ -4,14 +4,14 @@
   lib,
 }:
 
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "promptfoo";
   version = "0.79.0";
 
   src = fetchFromGitHub {
     owner = "promptfoo";
     repo = "promptfoo";
-    rev = "${version}";
+    rev = finalAttrs.version;
     hash = "sha256-sMBgjxPzG3SJ7RS4oTtOq7hJ1MYaKW3/6FF8Pn5l89c=";
   };
 
@@ -23,11 +23,11 @@ buildNpmPackage rec {
     description = "Test your prompts, models, RAGs. Evaluate and compare LLM outputs, catch regressions, and improve prompt quality";
     mainProgram = "promptfoo";
     homepage = "https://www.promptfoo.dev/";
-    changelog = "https://github.com/promptfoo/promptfoo/releases/tag/${version}";
+    changelog = "https://github.com/promptfoo/promptfoo/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [
       nathanielbrough
       jk
     ];
   };
-}
+})
