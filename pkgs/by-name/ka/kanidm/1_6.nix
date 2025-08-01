d37a0de7537ed6dd9a06be23e82d109bf2a68d67
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-01 07:43:10 +0000

    kanidm_1_6: 1.6.4 -> 1.7.0

--- pkgs/by-name/ka/kanidm/1_6.nix
+++ pkgs/by-name/ka/kanidm/1_6.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
-  version = "1.6.4";
-  hash = "sha256-ui3w1HDHXHARsjQ3WtJfZbM7Xgg3ODnUneXJMQwaOMw=";
-  cargoHash = "sha256-KJGELBzScwsLd6g3GR9Vk0nfDU2EjZBfXwlXJ+bZb1k=";
+  version = "1.7.0";
+  hash = "sha256-JM5C0tCvo4Lq06bqil8M35YnFMoQFjzA/J30gZOs4fU=";
+  cargoHash = "sha256-VmZk6gNRX556b+tf4pIxvuFh+58zVJbvZ5/P/z6BMFw=";
   patchDir = ./patches/1_6;
 }
