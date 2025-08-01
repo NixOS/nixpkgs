160dfda7c1de4adc560b4f1d440975610fa82c0b
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-01 21:02:37 +0200

    python312Packages.mypy-boto3-identitystore: 1.39.0 -> 1.40.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -610,8 +610,8 @@ rec {
       "sha256-uQCsVXN1Qo8LvDeqJP3SkB4ttwGK5E4Kr5nsD4SijUQ=";
 
   mypy-boto3-identitystore =
-    buildMypyBoto3Package "identitystore" "1.39.0"
-      "sha256-On3ZXIYkKHLIF455qpB7zZLB7irjcfoSm9OXDLQv840=";
+    buildMypyBoto3Package "identitystore" "1.40.0"
+      "sha256-RLcGOVDiwryD6xf9E6lWSdAAIKCP4hNaBQknjbwPAuQ=";
 
   mypy-boto3-imagebuilder =
     buildMypyBoto3Package "imagebuilder" "1.39.0"
