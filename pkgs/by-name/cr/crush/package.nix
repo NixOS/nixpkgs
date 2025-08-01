28bac1fe0a8e38e4ce69896c8c955e4776d9ae46
kiara <cinereal@riseup.net>
2025-08-01 10:33:22 +0200

    Update pkgs/by-name/cr/crush/package.nix - rev -> tag
    Co-authored-by: Yiyu Zhou <yiyuzhou19@gmail.com>

--- pkgs/by-name/cr/crush/package.nix
+++ pkgs/by-name/cr/crush/package.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "charmbracelet";
     repo = "crush";
-    rev = "v${version}";
+    tag = "v${version}";
     hash = "sha256-XHdudkll+NUksT+Rdvx3M8SKDpgx4z7M14gIWAY6/hI=";
   };
 
