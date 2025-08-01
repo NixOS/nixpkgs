b0dd124edf62a33d702da42db742dca1d5adfe25
frantathefranta <fb@franta.us>
2025-08-01 09:44:03 -0400

    maintainers: add frantathefranta

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8625,6 +8625,12 @@
     githubId = 10290864;
     name = "Peter Frank";
   };
+  frantathefranta = {
+    github = "frantathefranta";
+    githubId = 64412753;
+    name = "Franta Bartik";
+    email = "fb@franta.us";
+  };
   franzmondlichtmann = {
     name = "Franz Schroepf";
     email = "franz-schroepf@t-online.de";
