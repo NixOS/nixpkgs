4212967d277e87bd4dc0ae13fdd4ef40059b9b2b
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-08-01 12:18:55 -0400

    windows.mingw_w64_headers: Add libc assert, improve meta block

--- pkgs/os-specific/windows/mingw-w64/default.nix
+++ pkgs/os-specific/windows/mingw-w64/default.nix
@@ -35,7 +35,7 @@ stdenv.mkDerivation {
   enableParallelBuilding = true;
 
   nativeBuildInputs = [ autoreconfHook ];
-  buildInputs = [ windows.mingw_w64_headers ];
+  buildInputs = [ mingw_w64_headers ];
   hardeningDisable = [
     "stackprotector"
     "fortify"
