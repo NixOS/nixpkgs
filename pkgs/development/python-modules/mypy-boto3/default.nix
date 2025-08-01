4a7c0a9bda8c12289f765adc768014dfd4a89fac
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-01 20:58:55 +0200

    python312Packages.mypy-boto3-arc-zonal-shift: 1.39.0 -> 1.40.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -138,8 +138,8 @@ rec {
       "sha256-NgOa+Na/gU7IrtEJ8bVMJaSCNgTnGreX2TsjsAlIN+Y=";
 
   mypy-boto3-arc-zonal-shift =
-    buildMypyBoto3Package "arc-zonal-shift" "1.39.0"
-      "sha256-RQkAtXDMDfIfMhGEFx++kZ1CF7fmASv3SoXxPE9YHhY=";
+    buildMypyBoto3Package "arc-zonal-shift" "1.40.0"
+      "sha256-c8dkKeytZ3/lr1gWswCtcFGsHnv7F+7TOeNMNnocJhE=";
 
   mypy-boto3-athena =
     buildMypyBoto3Package "athena" "1.39.0"
