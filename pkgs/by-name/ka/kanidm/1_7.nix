4b2b60e1c7c1777ef6485e880332591da9361a18
Adam C. Stephens <adam@valkor.net>
2025-08-01 15:07:31 -0400

    kanidm_1_7: init at 1.7.0
    https://github.com/kanidm/kanidm/releases/tag/v1.7.0
    (cherry picked from commit 9d78192fbfa58a939898db27dfcd9d6941393099)

---
+++ pkgs/by-name/ka/kanidm/1_7.nix
@@ -0,0 +1,5 @@
+import ./generic.nix {
+  version = "1.7.0";
+  hash = "sha256-JM5C0tCvo4Lq06bqil8M35YnFMoQFjzA/J30gZOs4fU=";
+  cargoHash = "sha256-MvBjj/6ph+i29dZGtaysHHgRk03P8sMePXdIm+v2tpw=";
+}
