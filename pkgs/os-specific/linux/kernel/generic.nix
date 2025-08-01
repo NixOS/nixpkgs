NixOS/nixpkgs

commit e9b2edb4916328647571aee01b2a83c02e585df5

linux: un-simplify toolchain selection
This reverts commit 70cc251554d883130ae1a94ffd3508c2d521db6a.

This commit caused gcc to be pulled in as a target compiler for
pkgsLLVM.

Co-authored-by: Tristan Ross <tristan.ross@midstall.com>

pkgs/os-specific/linux/kernel/generic.nix
@@ -191,7 +191,7 @@ let
         ++ lib.optional (lib.versionAtLeast version "5.2") pahole
         ++ lib.optionals withRust [
           rust-bindgen
-          rustc
+          rustc.unwrapped
         ];
 
         RUST_LIB_SRC = lib.optionalString withRust rustPlatform.rustLibSrc;
@@ -201,11 +201,14 @@ let
         kernelBaseConfig =
           if defconfig != null then defconfig else stdenv.hostPlatform.linux-kernel.baseConfig;
 
-        makeFlags =
-          lib.optionals (
-            stdenv.hostPlatform.linux-kernel ? makeFlags
-          ) stdenv.hostPlatform.linux-kernel.makeFlags
-          ++ extraMakeFlags;
+        makeFlags = import ./common-flags.nix {
+          inherit
+            lib
+            stdenv
+            buildPackages
+            extraMakeFlags
+            ;
+        };
 
         postPatch = kernel.postPatch + ''
           # Patch kconfig to print "###" after every question so that

pkgs/os-specific/linux/kernel/manual-config.nix
@@ -170,7 +170,7 @@ lib.makeOverridable (
         ++ optional (lib.versionAtLeast version "5.13") zstd
         ++ optionals withRust [
           rustc
-          rust-bindgen
+          rust-bindgen.unwrapped
         ];
 
       in
@@ -230,7 +230,7 @@ lib.makeOverridable (
         ]
         ++ optionals withRust [
           rustc
-          rust-bindgen
+          rust-bindgen.unwrapped
         ];
 
         RUST_LIB_SRC = lib.optionalString withRust rustPlatform.rustLibSrc;
@@ -531,20 +531,14 @@ lib.makeOverridable (
         // extraMeta;
       };
 
-    # Absolute paths for compilers avoid any PATH-clobbering issues.
-    commonMakeFlags = [
-      "ARCH=${stdenv.hostPlatform.linuxArch}"
-      "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
-    ]
-    ++ lib.optionals (stdenv.isx86_64 && stdenv.cc.bintools.isLLVM) [
-      # The wrapper for ld.lld breaks linking the kernel. We use the
-      # unwrapped linker as workaround. See:
-      #
-      # https://github.com/NixOS/nixpkgs/issues/321667
-      "LD=${stdenv.cc.bintools.bintools}/bin/${stdenv.cc.targetPrefix}ld"
-    ]
-    ++ (stdenv.hostPlatform.linux-kernel.makeFlags or [ ])
-    ++ extraMakeFlags;
+    commonMakeFlags = import ./common-flags.nix {
+      inherit
+        lib
+        stdenv
+        buildPackages
+        extraMakeFlags
+        ;
+    };
   in
 
   stdenv.mkDerivation (

pkgs/os-specific/linux/kernel/common-config.nix
@@ -525,7 +525,9 @@ let
         DRM_AMD_DC_DCN = lib.mkIf (with stdenv.hostPlatform; isx86 || isPower64) (
           whenBetween "5.11" "6.4" yes
         );
-        DRM_AMD_DC_FP = whenAtLeast "6.4" yes;
+        # Not available when using clang
+        # See: https://github.com/torvalds/linux/blob/172a9d94339cea832d89630b89d314e41d622bd8/drivers/gpu/drm/amd/display/Kconfig#L14
+        DRM_AMD_DC_FP = lib.mkIf (!stdenv.cc.isClang) (whenAtLeast "6.4" yes);
         DRM_AMD_DC_HDCP = whenBetween "5.5" "6.4" yes;
         DRM_AMD_DC_SI = whenAtLeast "5.10" yes;
 

pkgs/os-specific/linux/kernel/common-flags.nix
@@ -1,0 +1,34 @@
+{
+  lib,
+  stdenv,
+  buildPackages,
+  extraMakeFlags ? [ ],
+}:
+# Absolute paths for compilers avoid any PATH-clobbering issues.
+[
+  #
+  # We use the unwrapped compiler, because the clang-wrapper doesn't like -target.
+  "CC=${lib.getExe stdenv.cc.cc}"
+  # The wrapper for ld.lld breaks linking the kernel. We use the unwrapped linker as workaround. See:
+  # https://github.com/NixOS/nixpkgs/issues/321667
+  "LD=${lib.getExe' stdenv.cc.bintools.bintools "${stdenv.cc.targetPrefix}ld"}"
+  "AR=${lib.getExe' stdenv.cc "${stdenv.cc.targetPrefix}ar"}"
+  "NM=${lib.getExe' stdenv.cc "${stdenv.cc.targetPrefix}nm"}"
+  "STRIP=${lib.getExe' stdenv.cc.bintools.bintools "${stdenv.cc.targetPrefix}strip"}"
+  "OBJCOPY=${lib.getExe' stdenv.cc "${stdenv.cc.targetPrefix}objcopy"}"
+  "OBJDUMP=${lib.getExe' stdenv.cc "${stdenv.cc.targetPrefix}objdump"}"
+  "READELF=${lib.getExe' stdenv.cc "${stdenv.cc.targetPrefix}readelf"}"
+  "HOSTCC=${lib.getExe' buildPackages.stdenv.cc "${buildPackages.stdenv.cc.targetPrefix}cc"}"
+  "HOSTCXX=${lib.getExe' buildPackages.stdenv.cc "${buildPackages.stdenv.cc.targetPrefix}c++"}"
+  "HOSTAR=${lib.getExe' buildPackages.stdenv.cc.bintools "${buildPackages.stdenv.cc.targetPrefix}ar"}"
+  "HOSTLD=${lib.getExe' buildPackages.stdenv.cc.bintools "${buildPackages.stdenv.cc.targetPrefix}ld"}"
+  "ARCH=${stdenv.hostPlatform.linuxArch}"
+  "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
+]
+# Add the built in headers the kernel needs
+++ lib.optionals (stdenv.cc.isClang) [
+  "CFLAGS_MODULE=-I${lib.getLib stdenv.cc.cc}/lib/clang/${lib.versions.major stdenv.cc.cc.version}/include"
+  "CFLAGS_KERNEL=-I${lib.getLib stdenv.cc.cc}/lib/clang/${lib.versions.major stdenv.cc.cc.version}/include"
+]
+++ (stdenv.hostPlatform.linux-kernel.makeFlags or [ ])
+++ extraMakeFlags

commit 84f52a829d69e8c24ff7847aa6c7500a92bb1170

linux: fix cross
The .unwrapped attributes are not correctly spliced.  Use the top
level attributes instead, which are spliced correctly.

Fixes: e9b2edb49163 ("linux: un-simplify toolchain selection")

pkgs/os-specific/linux/kernel/generic.nix
@@ -11,9 +11,9 @@
   pahole,
   lib,
   stdenv,
-  rustc,
+  rustc-unwrapped,
   rustPlatform,
-  rust-bindgen,
+  rust-bindgen-unwrapped,
   # testing
   emptyFile,
   nixos,
@@ -126,7 +126,7 @@ let
 
       commonStructuredConfig = import ./common-config.nix {
         inherit lib stdenv version;
-        rustAvailable = lib.meta.availableOn stdenv.hostPlatform rustc;
+        rustAvailable = lib.meta.availableOn stdenv.hostPlatform rustc-unwrapped;
 
         features = kernelFeatures; # Ensure we know of all extra patches, etc.
       };
@@ -190,8 +190,8 @@ let
         ]
         ++ lib.optional (lib.versionAtLeast version "5.2") pahole
         ++ lib.optionals withRust [
-          rust-bindgen.unwrapped
-          rustc.unwrapped
+          rust-bindgen-unwrapped
+          rustc-unwrapped
         ];
 
         RUST_LIB_SRC = lib.optionalString withRust rustPlatform.rustLibSrc;

pkgs/os-specific/linux/kernel/manual-config.nix
@@ -22,8 +22,8 @@
   kmod,
   ubootTools,
   fetchpatch,
-  rustc,
-  rust-bindgen,
+  rustc-unwrapped,
+  rust-bindgen-unwrapped,
   rustPlatform,
 }:
 
@@ -169,8 +169,8 @@ lib.makeOverridable (
         ]
         ++ optional (lib.versionAtLeast version "5.13") zstd
         ++ optionals withRust [
-          rustc.unwrapped
-          rust-bindgen.unwrapped
+          rustc-unwrapped
+          rust-bindgen-unwrapped
         ];
 
       in
@@ -229,8 +229,8 @@ lib.makeOverridable (
           zlib
         ]
         ++ optionals withRust [
-          rustc.unwrapped
-          rust-bindgen.unwrapped
+          rustc-unwrapped
+          rust-bindgen-unwrapped
         ];
 
         RUST_LIB_SRC = lib.optionalString withRust rustPlatform.rustLibSrc;