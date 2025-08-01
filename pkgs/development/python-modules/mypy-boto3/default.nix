1fc71c72319ae38a1b132d90bcad555bfa3de194
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-01 20:59:06 +0200

    python312Packages.mypy-boto3-backup-gateway: 1.39.0 -> 1.40.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -162,8 +162,8 @@ rec {
       "sha256-q7HoaGzpwKAPik9I6e9+esVbwH2zRrIhzPQoIW4juPA=";
 
   mypy-boto3-backup-gateway =
-    buildMypyBoto3Package "backup-gateway" "1.39.0"
-      "sha256-thoMZE5unC2SsMvRGiCytIuXUMy195RiKqWqaWE2FiE=";
+    buildMypyBoto3Package "backup-gateway" "1.40.0"
+      "sha256-7FIXPtt4FMjxXbHtiO4qbS4wUfi10rmVODshIORlfhs=";
 
   mypy-boto3-batch =
     buildMypyBoto3Package "batch" "1.39.16"
