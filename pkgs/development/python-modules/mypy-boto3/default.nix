67e5511add134594738c23270aa8f265d61f0294
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-01 21:01:40 +0200

    python312Packages.mypy-boto3-elb: 1.39.0 -> 1.40.0

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -490,8 +490,8 @@ rec {
       "sha256-4xpjAgNDfYP3Z8uPxINsLOQ1vvanXW1/QDbUcZ57e0Y=";
 
   mypy-boto3-elb =
-    buildMypyBoto3Package "elb" "1.39.0"
-      "sha256-h3JKp5sy5ffpgg0+WGdGap6tZXLTZvwRTiH9jwtVZQ4=";
+    buildMypyBoto3Package "elb" "1.40.0"
+      "sha256-/VNUPoXCvu+XIbq81YPL7wN1aCnec5K4Vv3ysVr2+eI=";
 
   mypy-boto3-elbv2 =
     buildMypyBoto3Package "elbv2" "1.39.0"
