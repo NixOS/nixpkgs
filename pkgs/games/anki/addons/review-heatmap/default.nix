2e75d35c586f7154493f5aab771ec8d3e77d9c94
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-08-01 19:59:00 -0400

    ankiAddons.review-heatmap: add function arguments
    Allows evaluation without using `with import <nixpkgs> {}`.

--- pkgs/games/anki/addons/review-heatmap/default.nix
+++ pkgs/games/anki/addons/review-heatmap/default.nix
@@ -1,4 +1,9 @@
-with import <nixpkgs> {};
+{
+  lib,
+  anki-utils,
+  fetchFromGitHub,
+  nix-update-script,
+}:
 anki-utils.buildAnkiAddon (finalAttrs: {
   pname = "review-heatmap";
   version = "0-unstable-2025-8-1";
@@ -6,15 +11,15 @@ anki-utils.buildAnkiAddon (finalAttrs: {
     owner = "glutanimate";
     repo = "review-heatmap";
     rev = finalAttrs.version;
-    sparseCheckout = [ "src/addon" ];
+    sparseCheckout = ["src/addon"];
     hash = "sha256-28DJq2l9DP8O6OsbNQCZ0pm4S6CQ3Yz0Vfvlj+iQw8Y=";
   };
   sourceRoot = "${finalAttrs.src.name}/src/addon";
-  passthru.updateScript = nix-update-script { };
+  passthru.updateScript = nix-update-script {};
   meta = {
     description = "Add a heatmap graph to Anki to visulaize your past and future card-review activity";
     homepage = "https://github.com/glutanimate/review-heatmap";
     license = lib.licenses.agpl3Only;
-    maintainers = with lib.maintainers; [ dastarruer ];
+    maintainers = with lib.maintainers; [dastarruer];
   };
 })
