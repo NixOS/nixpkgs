8e41855fcdea5a4c9e6915fc39cc4c93126b7b74
Guy Chronister <guychronister@outlook.com>
2025-08-02 12:46:20 +0000

    gp-saml-gui: migrate to by-name
    gp-saml-gui: refactor buildPythonPackage and meta

--- pkgs/by-name/gp/gp-saml-gui/package.nix
+++ pkgs/by-name/gp/gp-saml-gui/package.nix
@@ -2,16 +2,14 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  buildPythonPackage,
+  python3Packages,
   webkitgtk_4_1,
   wrapGAppsHook3,
   glib-networking,
   gobject-introspection,
   openconnect,
-  pygobject3,
-  requests,
 }:
-buildPythonPackage rec {
+python3Packages.buildPythonPackage rec {
   pname = "gp-saml-gui";
   version = "0.1+20240731-${lib.strings.substring 0 7 src.rev}";
   format = "setuptools";
@@ -20,7 +18,7 @@ buildPythonPackage rec {
     owner = "dlenski";
     repo = "gp-saml-gui";
     rev = "c46af04b3a6325b0ecc982840d7cfbd1629b6d43";
-    sha256 = "sha256-4MFHad1cuCWawy2hrqdXOgud0pXpYiV9J3Jwqyg4Udk=";
+    hash = "sha256-4MFHad1cuCWawy2hrqdXOgud0pXpYiV9J3Jwqyg4Udk=";
   };
 
   buildInputs = lib.optional stdenv.hostPlatform.isLinux glib-networking;
@@ -31,12 +29,16 @@ buildPythonPackage rec {
     glib-networking
   ];
 
-  propagatedBuildInputs = [
-    requests
-    pygobject3
-    openconnect
-  ]
-  ++ lib.optional stdenv.hostPlatform.isLinux webkitgtk_4_1;
+  dependencies =
+    with python3Packages;
+    [
+      requests
+      pygobject3
+    ]
+    ++ [
+      openconnect
+    ]
+    ++ lib.optional stdenv.hostPlatform.isLinux webkitgtk_4_1;
 
   preFixup = ''
     gappsWrapperArgs+=(
@@ -44,11 +46,11 @@ buildPythonPackage rec {
     )
   '';
 
-  meta = with lib; {
+  meta = {
     description = "Interactively authenticate to GlobalProtect VPNs that require SAML";
     mainProgram = "gp-saml-gui";
     homepage = "https://github.com/dlenski/gp-saml-gui";
-    license = licenses.gpl3Only;
-    maintainers = [ maintainers.pallix ];
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ pallix ];
   };
 }
