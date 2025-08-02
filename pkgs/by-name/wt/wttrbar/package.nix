ed980c87e2c0f9818dd03654729e47fa9c333fb6
Mikael Voss <mvs@nyantec.com>
2025-08-02 15:35:29 +0200

    wttrbar: provide changelog

--- pkgs/by-name/wt/wttrbar/package.nix
+++ pkgs/by-name/wt/wttrbar/package.nix
@@ -28,6 +28,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   meta = {
     description = "Simple but detailed weather indicator for Waybar using wttr.in";
     homepage = "https://github.com/bjesus/wttrbar";
+    changelog = "https://github.com/bjesus/wttrbar/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ khaneliman ];
     mainProgram = "wttrbar";
