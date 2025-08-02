4a3a5e154dffea0cc4d311ec5eb6c6b9964621c6
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-02 20:58:14 +0000

    python3Packages.aioamazondevices: 3.6.0 -> 4.0.0

--- pkgs/development/python-modules/aioamazondevices/default.nix
+++ pkgs/development/python-modules/aioamazondevices/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "aioamazondevices";
-  version = "3.6.0";
+  version = "4.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "chemelli74";
     repo = "aioamazondevices";
     tag = "v${version}";
-    hash = "sha256-CVwpu0TW6ah7Stx2GhQgw9EkmHJ6rRB7DN416EatUMo=";
+    hash = "sha256-Ch3IP7fMEDYYRFDk3eoZWjhjnkQY0qcAFJdmkoW4o/0=";
   };
 
   build-system = [ poetry-core ];
