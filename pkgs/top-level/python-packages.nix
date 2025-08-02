8b9255a3e0855786533cea95d1a1d85b690e67a2
eymeric <eymericdechelette@gmail.com>
2025-08-02 23:06:24 +0200

    python3Packages.luma-core: init at 2.5.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8693,6 +8693,8 @@ self: super: with self; {
 
   luhn = callPackage ../development/python-modules/luhn { };
 
+  luma-core = callPackage ../development/python-modules/luma-core { };
+
   luna-soc = callPackage ../development/python-modules/luna-soc { };
 
   luna-usb = callPackage ../development/python-modules/luna-usb { };
