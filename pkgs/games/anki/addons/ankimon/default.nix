da38941e2b6ac8343737ad3d2a5936ba9b20497a
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-08-01 23:48:58 -0400

    ankiAddons.ankimon: init at 1.288
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

---
+++ pkgs/games/anki/addons/ankimon/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  anki-utils,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+anki-utils.buildAnkiAddon (finalAttrs: {
+  pname = "ankimon";
+  version = "1.288";
+  src = fetchFromGitHub {
+    owner = "Unlucky-Life";
+    repo = "ankimon";
+    tag = finalAttrs.version;
+    hash = "sha256-PPTmZpxeBv0zb8wbxLEozDdRJG+eYjMqGyx8y5na1MQ=";
+  };
+  passthru.updateScript = nix-update-script { };
+  meta = {
+    description = "Ankimon is a Anki Addon to Gamify your learning experience";
+    homepage = "https://ankiweb.net/shared/info/1908235722";
+    downloadPage = "https://github.com/Unlucky-Life/ankimon";
+    changelog = "https://github.com/Unlucky-Life/ankimon/releases/tag/${finalAttrs.version}";
+    license = with lib.licenses; [
+      gpl3Plus # code
+      unfree # pokemon sprites
+    ];
+    # no use in having hydra deal with a ~500MB file
+    # when it essentially is just downloading and copying
+    # files
+    hydraPlatforms = [ ];
+    maintainers = with lib.maintainers; [ ethancedwards8 ];
+  };
+})
