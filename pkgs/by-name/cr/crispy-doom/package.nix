e3cd903257f5cf536e39174cad11e6aaca5e0739
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-08-02 12:54:17 +0200

    crispy-doom: refactor

--- pkgs/by-name/cr/crispy-doom/package.nix
+++ pkgs/by-name/cr/crispy-doom/package.nix
@@ -1,28 +1,27 @@
 {
   lib,
   stdenv,
+  fetchFromGitHub,
   autoreconfHook,
   pkg-config,
+  python3,
   SDL2,
   SDL2_mixer,
   SDL2_net,
-  fetchFromGitHub,
-  python3,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "crispy-doom";
   version = "7.0";
 
   src = fetchFromGitHub {
     owner = "fabiangreffrath";
     repo = "crispy-doom";
-    rev = "crispy-doom-${version}";
-    sha256 = "sha256-+rNZsb4GAjzNcIU5xZGBpmP+nXNOP16oVg68nfecMrw=";
+    tag = "crispy-doom-${finalAttrs.version}";
+    hash = "sha256-+rNZsb4GAjzNcIU5xZGBpmP+nXNOP16oVg68nfecMrw=";
   };
 
   postPatch = ''
-    sed -e 's#/games#/bin#g' -i src{,/setup}/Makefile.am
     for script in $(grep -lr '^#!/usr/bin/env python3$'); do patchShebangs $script; done
   '';
 
@@ -31,24 +30,28 @@ stdenv.mkDerivation rec {
     pkg-config
     python3
   ];
+
   buildInputs = [
     SDL2
     SDL2_mixer
     SDL2_net
   ];
+
   enableParallelBuilding = true;
 
   strictDeps = true;
 
   meta = {
-    homepage = "http://fabiangreffrath.github.io/crispy-doom";
+    homepage = "https://fabiangreffrath.github.io/crispy-homepage";
+    changelog = "https://github.com/fabiangreffrath/crispy-doom/releases/tag/crispy-doom-${finalAttrs.version}";
     description = "Limit-removing enhanced-resolution Doom source port based on Chocolate Doom";
     longDescription = ''
       Crispy Doom is a limit-removing enhanced-resolution Doom source port based on Chocolate Doom.
       Its name means that 640x400 looks \"crisp\" and is also a slight reference to its origin.
     '';
+    mainProgram = "crispy-doom";
     license = lib.licenses.gpl2Plus;
     platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ neonfuz ];
   };
-}
+})
