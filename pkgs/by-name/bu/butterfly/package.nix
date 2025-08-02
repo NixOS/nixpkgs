944375d2527d26dbe178866af9e61a2d817e7fed
mivorasu <mivorasu@users.noreply.github.com>
2025-08-03 05:27:58 +0800

    butterfly: 2.3.2 -> 2.3.3

--- pkgs/by-name/bu/butterfly/package.nix
+++ pkgs/by-name/bu/butterfly/package.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  flutter329,
+  flutter332,
   fetchFromGitHub,
   runCommand,
   butterfly,
@@ -9,15 +9,15 @@
   gitUpdater,
 }:
 
-flutter329.buildFlutterApplication rec {
+flutter332.buildFlutterApplication rec {
   pname = "butterfly";
-  version = "2.3.2";
+  version = "2.3.3";
 
   src = fetchFromGitHub {
     owner = "LinwoodDev";
     repo = "Butterfly";
     tag = "v${version}";
-    hash = "sha256-eAkepyZm4WgPo8ieBbWHoSSv/Zfr9U9HCsbxEyrzy0Y=";
+    hash = "sha256-3cDT1t74SrDUqUtFmNZFQHUx+eCdDjZhPseT3lhNOYE=";
   };
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
