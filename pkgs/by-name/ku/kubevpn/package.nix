ff44667f6cd5fbf1fca701789028081d3a8689fd
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-02 11:55:52 +0000

    kubevpn: 2.8.1 -> 2.9.0

--- pkgs/by-name/ku/kubevpn/package.nix
+++ pkgs/by-name/ku/kubevpn/package.nix
@@ -9,13 +9,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "kubevpn";
-  version = "2.8.1";
+  version = "2.9.0";
 
   src = fetchFromGitHub {
     owner = "KubeNetworks";
     repo = "kubevpn";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+TyaujgbeQXApxmjYvLnmhBZZUeIZMidzS7mL+Ach3o=";
+    hash = "sha256-vPmN1g3LNTYoFZNaUJWJbP+N+RG0973GLekQOsr+4bU=";
   };
 
   vendorHash = null;
