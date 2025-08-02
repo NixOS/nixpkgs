980fe63e5c9319440655b9b1c8159dca918f03bb
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-02 04:04:21 +0000

    igir: 4.1.1 -> 4.1.2

--- pkgs/by-name/ig/igir/package.nix
+++ pkgs/by-name/ig/igir/package.nix
@@ -18,16 +18,16 @@
 
 buildNpmPackage rec {
   pname = "igir";
-  version = "4.1.1";
+  version = "4.1.2";
 
   src = fetchFromGitHub {
     owner = "emmercm";
     repo = "igir";
     rev = "v${version}";
-    hash = "sha256-f/3XIBFMxSPwJpfZTBhuznU/psChfnQEwZASOoH4Ij0=";
+    hash = "sha256-L9bY3ep0HqRimYTqfW1yqbnnas4gjsD2emtJnWxGQaQ=";
   };
 
-  npmDepsHash = "sha256-qPyS2F5jt1C5SZxvRuyPX4+TkYZKTffcekanWtH82EY=";
+  npmDepsHash = "sha256-56pTJ1VZcoqDb56qzvfxEZUubu82n55O5R0JFRNy5HE=";
 
   # I have no clue why I have to do this
   postPatch = ''
