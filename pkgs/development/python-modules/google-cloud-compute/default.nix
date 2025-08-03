a5f948cea2e3e314dd14ad598122454bfd2e7345
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-03 22:57:47 +0200

    python313Packages.google-cloud-compute: 1.31.0 -> 1.33.0
    Changelog: https://github.com/googleapis/google-cloud-python/blob/google-cloud-compute-v1.33.0/packages/google-cloud-compute/CHANGELOG.md

--- pkgs/development/python-modules/google-cloud-compute/default.nix
+++ pkgs/development/python-modules/google-cloud-compute/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "google-cloud-compute";
-  version = "1.31.0";
+  version = "1.33.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "google_cloud_compute";
     inherit version;
-    hash = "sha256-SCX85iUIiCQdCym1jy961kkcCcyhTZOrey0/UIfRNLc=";
+    hash = "sha256-D/xWv8qVg8L6XyticbZ1ak1vXNvv4mVkIHESlykuQu4=";
   };
 
   build-system = [ setuptools ];
