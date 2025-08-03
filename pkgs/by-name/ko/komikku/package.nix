78545d5ec031c6d294d5d7960009220943df9d08
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-08-03 11:08:45 +0200

    komikku: 1.83.0 -> 1.84.0

--- pkgs/by-name/ko/komikku/package.nix
+++ pkgs/by-name/ko/komikku/package.nix
@@ -23,7 +23,7 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "komikku";
-  version = "1.83.0";
+  version = "1.84.0";
   pyproject = false;
 
   src = fetchFromGitea {
@@ -31,7 +31,7 @@ python3.pkgs.buildPythonApplication rec {
     owner = "valos";
     repo = "Komikku";
     tag = "v${version}";
-    hash = "sha256-cwNSjzCy4lv71O3XAcDXVF+75MhQ6gMrMz/IuePCdak=";
+    hash = "sha256-bzz49ILpqzOStBdRGMhBKkxI4RlVuDjCqhcIjQGGQmk=";
   };
 
   nativeBuildInputs = [
