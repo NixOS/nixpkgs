fd2d6109ba102faaf8ece2cd2843d4acf7aa9dfd
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-03 23:43:35 +0200

    python3Packages.pytest-relaxed: fix pytest8.4 compat

--- pkgs/development/python-modules/pytest-relaxed/default.nix
+++ pkgs/development/python-modules/pytest-relaxed/default.nix
@@ -21,6 +21,12 @@ buildPythonPackage rec {
     hash = "sha256-lW6gKOww27+2gN2Oe0p/uPgKI5WV6Ius4Bi/LA1xgkg=";
   };
 
+  patches = [
+    # https://github.com/bitprophet/pytest-relaxed/issues/28
+    # https://github.com/bitprophet/pytest-relaxed/pull/29
+    ./fix-oldstyle-hookimpl-setup.patch
+  ];
+
   buildInputs = [ pytest ];
 
   propagatedBuildInputs = [ decorator ];
@@ -31,6 +37,10 @@ buildPythonPackage rec {
   ];
 
   enabledTestPaths = [ "tests" ];
+
+  disabledTests = [
+    "test_skips_pytest_fixtures"
+  ];
 
   pythonImportsCheck = [ "pytest_relaxed" ];
 
