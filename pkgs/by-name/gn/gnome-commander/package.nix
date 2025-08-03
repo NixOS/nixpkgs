fe6d111f4d3a337d87314cc6eeafc630df7d2ef1
mivorasu <mivorasu@users.noreply.github.com>
2025-08-03 13:27:10 +0800

    gnome-commander: fix build

--- pkgs/by-name/gn/gnome-commander/package.nix
+++ pkgs/by-name/gn/gnome-commander/package.nix
@@ -29,13 +29,29 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-rSaj1Fg2seZKlzlERZZmz80kxJT1vZ+INiJlWfZ9m6g=";
   };
 
-  # hard-coded schema paths
-  postPatch = ''
-    substituteInPlace src/gnome-cmd-data.cc plugins/fileroller/file-roller-plugin.cc \
-      --replace-fail \
-        '/share/glib-2.0/schemas' \
-        '/share/gsettings-schemas/${finalAttrs.finalPackage.name}/glib-2.0/schemas'
-  '';
+  postPatch =
+    # hard-coded schema paths
+    ''
+      substituteInPlace src/gnome-cmd-data.cc plugins/fileroller/file-roller-plugin.cc \
+        --replace-fail \
+          '/share/glib-2.0/schemas' \
+          '/share/gsettings-schemas/${finalAttrs.finalPackage.name}/glib-2.0/schemas'
+    ''
+    # Fix missing semicolon in file-list sort_by_owner / sort_by_group
+    + ''
+      substituteInPlace src/gnome-cmd-file-list.cc \
+        --replace-fail "GetGfileAttributeString(G_FILE_ATTRIBUTE_OWNER_USER), raising)" \
+                 "GetGfileAttributeString(G_FILE_ATTRIBUTE_OWNER_USER), raising);" \
+        --replace-fail "GetGfileAttributeString(G_FILE_ATTRIBUTE_OWNER_GROUP), raising)" \
+                 "GetGfileAttributeString(G_FILE_ATTRIBUTE_OWNER_GROUP), raising);"
+    ''
+    # Update C/C++ language standards to C17 and C++17 to meet GTest 1.10+ requirements and ensure consistency across tests
+    + ''
+      substituteInPlace meson.build \
+        --replace-fail "'cpp_std=gnu++11', 'build.cpp_std=gnu++11'" "'cpp_std=gnu++17', 'build.cpp_std=gnu++17'"
+      substituteInPlace tests/meson.build \
+        --replace-fail "'c_std=c14', 'cpp_std=c++14'" "'c_std=c17', 'cpp_std=c++17'"
+    '';
 
   nativeBuildInputs = [
     meson
