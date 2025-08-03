8e3f77a9d019795f147ce62170477edcc87e6bd7
mivorasu <mivorasu@users.noreply.github.com>
2025-08-03 18:46:44 +0800

    bino3d: fix

--- pkgs/applications/video/bino3d/default.nix
+++ pkgs/applications/video/bino3d/default.nix
@@ -10,6 +10,7 @@
   qtbase,
   qtmultimedia,
   qttools,
+  pipewire,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -21,6 +22,27 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "bino-${finalAttrs.version}";
     hash = "sha256-vGPbSYTfRy414xVcLIvOnN4Te36HWVz7DQegNhYb3u4=";
   };
+
+  postPatch =
+    # after including QMediaPlayer, also include the Qt JSON headers
+    # so we can use QJsonDocument, QJsonObject, etc
+    ''
+      sed -i '/#include <QMediaPlayer>/a \
+      #include <QJsonDocument>\
+      #include <QJsonObject>\
+      #include <QJsonArray>\
+      #include <QJsonValue>' src/metadata.cpp
+    ''
+    # on macOS the OpenGL headers lack GL_MAX_FRAMEBUFFER_WIDTH
+    # and GL_MAX_FRAMEBUFFER_HEIGHT, so define them here with
+    # their standard enum values
+    + lib.optionalString stdenv.hostPlatform.isDarwin ''
+      sed -i '1i\
+      #ifndef GL_MAX_FRAMEBUFFER_WIDTH\n\
+      #define GL_MAX_FRAMEBUFFER_WIDTH 0x9315\n\
+      #define GL_MAX_FRAMEBUFFER_HEIGHT 0x9316\n\
+      #endif\n' src/widget.cpp
+    '';
 
   nativeBuildInputs = [
     cmake
@@ -35,6 +57,13 @@ stdenv.mkDerivation (finalAttrs: {
     qtmultimedia
     qttools
     # The optional QVR dependency is not currently packaged.
+  ];
+
+  qtWrapperArgs = lib.optionals stdenv.hostPlatform.isLinux [
+    # qt.multimedia.symbolsresolver: Couldn't load pipewire-0.3 library
+    "--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ pipewire ]}"
+    # Force Qt to use the X11 (xcb) platform plugin instead of Wayland or EGL
+    "--set QT_QPA_PLATFORM xcb"
   ];
 
   meta = {
