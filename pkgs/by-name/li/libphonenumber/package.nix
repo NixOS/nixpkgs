88ef6df3402b4d5ab47778e78b8544b606ad914b
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-04 07:33:58 +0000

    libphonenumber: 9.0.10 -> 9.0.12

--- pkgs/by-name/li/libphonenumber/package.nix
+++ pkgs/by-name/li/libphonenumber/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libphonenumber";
-  version = "9.0.10";
+  version = "9.0.12";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "libphonenumber";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-b/TQhHG7qPzXcFtiFNXOu1hkFfT9KnZNPa+AB/Fmk0E=";
+    hash = "sha256-ylEv1YtnzXGHLKVY1INEq2GXrxgfy6R0cL199ec8CyQ=";
   };
 
   patches = [
