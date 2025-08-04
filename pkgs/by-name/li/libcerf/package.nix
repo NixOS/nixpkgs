71b2f8cd5f604a699d3527596b3aaaf610d61a50
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-04 00:35:40 +0000

    libcerf: 3.0 -> 3.1

--- pkgs/by-name/li/libcerf/package.nix
+++ pkgs/by-name/li/libcerf/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libcerf";
-  version = "3.0";
+  version = "3.1";
 
   src = fetchurl {
     url = "https://jugit.fz-juelich.de/mlz/libcerf/-/archive/v${version}/libcerf-v${version}.tar.gz";
-    sha256 = "sha256-xhCPvaia839YgRnAxUK2wegkhFo2vqL6MfftLMGiRts=";
+    sha256 = "sha256-TAfiqOK00OTUjbng/JGRtDoOEg5XfVXYfibe6HRcb6s=";
   };
 
   nativeBuildInputs = [
