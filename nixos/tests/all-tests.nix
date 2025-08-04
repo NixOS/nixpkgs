amansakhuja/nixpkgs5

nixos/spacetimedb: init module

nixos/tests/all-tests.nix
@@ -1089,6 +1089,7 @@ in {
   sonic-server = handleTest ./sonic-server.nix {};
   sourcehut = handleTest ./sourcehut {};
   spacecookie = handleTest ./spacecookie.nix {};
+  spacetimedb = handleTest ./spacetimedb.nix {};
   spark = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./spark {};
   spiped = runTest ./spiped.nix;
   sqlite3-to-mysql = handleTest ./sqlite3-to-mysql.nix {};

NixOS/nixpkgs

nixos/tests/all-tests.nix
@@ -1335,14 +1335,16 @@ in
   snapcast = runTest ./snapcast.nix;
   snapper = runTest ./snapper.nix;
   snipe-it = runTest ./web-apps/snipe-it.nix;
-  soapui = runTest ./soapui.nix;
-  soft-serve = runTest ./soft-serve.nix;
-  sogo = runTest ./sogo.nix;
-  soju = runTest ./soju.nix;
-  solanum = runTest ./solanum.nix;
-  sonarr = runTest ./sonarr.nix;
-  sonic-server = runTest ./sonic-server.nix;
-  spacecookie = runTest ./spacecookie.nix;
+  soapui = handleTest ./soapui.nix { };
+  soft-serve = handleTest ./soft-serve.nix { };
+  sogo = handleTest ./sogo.nix { };
+  soju = handleTest ./soju.nix { };
+  solanum = handleTest ./solanum.nix { };
+  sonarr = handleTest ./sonarr.nix { };
+  sonic-server = handleTest ./sonic-server.nix { };
+  sourcehut = handleTest ./sourcehut { };
+  spacecookie = handleTest ./spacecookie.nix { };
+  spacetimedb = handleTest ./spacetimedb.nix { };
   spark = handleTestOn [ "x86_64-linux" "aarch64-linux" ] ./spark { };
   spiped = runTest ./spiped.nix;
   sqlite3-to-mysql = runTest ./sqlite3-to-mysql.nix;