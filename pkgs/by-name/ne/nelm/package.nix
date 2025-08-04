283548dd4ce5cb1a56441852c2984bf5fc80f358
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-04 23:14:32 +0000

    nelm: 1.8.0 -> 1.10.0

--- pkgs/by-name/ne/nelm/package.nix
+++ pkgs/by-name/ne/nelm/package.nix
@@ -9,16 +9,16 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "nelm";
-  version = "1.8.0";
+  version = "1.10.0";
 
   src = fetchFromGitHub {
     owner = "werf";
     repo = "nelm";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-IZO+uMWa2dxvfRhxeVy6JRIzRC1CduZxUHPrA8lqxi8=";
+    hash = "sha256-FtYRfGHdU3eScFQYDS4vAqQ7fgcGIUYhn7N5/X0rhXA=";
   };
 
-  vendorHash = "sha256-wrZe0YmsMOHD3NOOXRKM0ZLnnOvasFyPq26JIAqyXTQ=";
+  vendorHash = "sha256-QxzI+MO3wJxioW7NQwEigNq6NDu81Pu2BUBlwRvqyqk=";
 
   subPackages = [ "cmd/nelm" ];
 
