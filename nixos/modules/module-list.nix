12085a24e819e10e45c606e1200982333399445d
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-08-04 16:32:26 +0200

    nixos/throne: rename from nekoray

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -269,7 +269,6 @@
   ./programs/nano.nix
   ./programs/nautilus-open-any-terminal.nix
   ./programs/nbd.nix
-  ./programs/nekoray.nix
   ./programs/neovim.nix
   ./programs/nethoscope.nix
   ./programs/nexttrace.nix
@@ -327,6 +326,7 @@
   ./programs/systemtap.nix
   ./programs/tcpdump.nix
   ./programs/television.nix
+  ./programs/throne.nix
   ./programs/thunar.nix
   ./programs/thunderbird.nix
   ./programs/tmux.nix
