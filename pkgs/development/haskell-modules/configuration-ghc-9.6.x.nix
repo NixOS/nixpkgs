1b5377100b83e18d0b6b4e18fe2dadbadaf98e0f
Wolfgang Walther <walther@technowledgy.de>
2025-08-04 21:38:32 +0200

    haskell.packages.ghc{94,96,98}.ghc-lib-parser: fix eval
    The ghc-lib-parser expression is now created by running cabal2nix via
    GHC 9.10. This means that the conditional dependency ghc-internal is now
    added, but this is only available from GHC 9.10 onwards. Since this is
    not required when building with older compilers, we just set it to null.

--- pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.6.x.nix
@@ -67,6 +67,9 @@ in
   # Becomes a core package in GHC >= 9.10
   os-string = doDistribute self.os-string_2_0_7;
 
+  # Becomes a core package in GHC >= 9.10, no release compatible with GHC < 9.10 is available
+  ghc-internal = null;
+
   # Needs base-orphans for GHC < 9.8 / base < 4.19
   some = addBuildDepend self.base-orphans super.some;
 
