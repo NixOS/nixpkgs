c90bf12fe4cfd397f0204caa7e5dbe2610eff27b
Austin Butler <austin@butler.lol>
2025-08-04 12:33:20 -0700

    python3Packages.editdistance-s: remove

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4535,8 +4535,6 @@ self: super: with self; {
 
   editdistance = callPackage ../development/python-modules/editdistance { };
 
-  editdistance-s = callPackage ../development/python-modules/editdistance-s { };
-
   editdistpy = callPackage ../development/python-modules/editdistpy { };
 
   editor = callPackage ../development/python-modules/editor { };
