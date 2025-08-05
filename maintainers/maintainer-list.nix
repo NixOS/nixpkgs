6fe28d6fe798bb022ac0004c2415e6b270103fa8
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-08-05 12:31:50 -0300

    maintainers: add GustavoWidman

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9683,6 +9683,12 @@
     githubId = 28863828;
     name = "guserav";
   };
+  GustavoWidman = {
+    name = "Gustavo Widman";
+    email = "admin@r3dlust.com";
+    github = "GustavoWidman";
+    githubId = 123963822;
+  };
   guyonvarch = {
     github = "guyonvarch";
     githubId = 6768842;
