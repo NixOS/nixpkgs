cc799ff23fdc29527a2dfb4a7897895cfefce39f
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:42:04 +0200

    python3Packages.pikepdf: 9.8.1 -> 9.10.2
    https://github.com/pikepdf/pikepdf/blob/v9.10.2/docs/releasenotes/version9.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pikepdf/default.nix
+++ pkgs/development/python-modules/pikepdf/default.nix
@@ -25,7 +25,7 @@
 
 buildPythonPackage rec {
   pname = "pikepdf";
-  version = "9.8.1";
+  version = "9.10.2";
   pyproject = true;
 
   src = fetchFromGitHub {
@@ -38,7 +38,7 @@ buildPythonPackage rec {
     postFetch = ''
       rm "$out/.git_archival.txt"
     '';
-    hash = "sha256-gFaGHml1F5i+w68xapmPHYUK760rT4GJzTWTkDsIwC8=";
+    hash = "sha256-ZycC4mKsXopZ84BtkqAgTIJxiboIY4HmktRQIVhpfBQ=";
   };
 
   patches = [
