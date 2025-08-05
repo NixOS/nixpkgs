eefc1b7f43abdfd83b0a93c54423b20134bc31d8
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:44:25 +0200

    python3Packages.tree-sitter: 0.24.0 -> 0.25.0
    https://github.com/tree-sitter/py-tree-sitter/releases/tag/v0.25.0
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/tree-sitter/default.nix
+++ pkgs/development/python-modules/tree-sitter/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "tree-sitter";
-  version = "0.24.0";
+  version = "0.25.0";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-q9la9lyi9Pfso1Y0M5HtZp52Tzd0i1NSlG8A9/x45zQ=";
+    hash = "sha256-FciHdc8k2wZne6/mLfBYpkV9im3eZ7qkjdNyO5BeeaY=";
   };
 
   # see https://github.com/tree-sitter/py-tree-sitter/issues/330#issuecomment-2629403946
