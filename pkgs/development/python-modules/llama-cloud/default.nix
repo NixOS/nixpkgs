8e10d67e3e27705bf51fb59cbf290966c8bd966c
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:40:47 +0200

    python3Packages.llama-cloud: 0.1.34 -> 0.1.37
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/llama-cloud/default.nix
+++ pkgs/development/python-modules/llama-cloud/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "llama-cloud";
-  version = "0.1.34";
+  version = "0.1.37";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_cloud";
     inherit version;
-    hash = "sha256-aGbkurR9LBhAvfFpwTwGF2kxwdMGl6wfpxureUKgQek=";
+    hash = "sha256-ttYuc4bRqoWQW34/fBmkBpS+VMFZZmi86qRWzYTt5mY=";
   };
 
   build-system = [ poetry-core ];
