5ab327a8209c773f1787314aa01aed5117747a04
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:37:52 +0200

    python3Packages.django-stubs: 5.2.0 -> 5.2.2
    https://github.com/typeddjango/django-stubs/releases/tag/5.2.2
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/django-stubs/default.nix
+++ pkgs/development/python-modules/django-stubs/default.nix
@@ -3,11 +3,12 @@
   buildPythonPackage,
   django,
   django-stubs-ext,
-  fetchPypi,
+  fetchFromGitHub,
   mypy,
+  pytest-mypy-plugins,
   pytestCheckHook,
   pythonOlder,
-  setuptools,
+  hatchling,
   tomli,
   types-pytz,
   types-pyyaml,
@@ -16,23 +17,17 @@
 
 buildPythonPackage rec {
   pname = "django-stubs";
-  version = "5.2.0";
+  version = "5.2.2";
   pyproject = true;
 
-  disabled = pythonOlder "3.8";
-
-  src = fetchPypi {
-    pname = "django_stubs";
-    inherit version;
-    hash = "sha256-B+JcLTy/9b5UAif/N3GcyJ8hXfqqpesDinWwG7+7JyI=";
+  src = fetchFromGitHub {
+    owner = "typeddjango";
+    repo = "django-stubs";
+    tag = version;
+    hash = "sha256-kF5g0/rkMQxYTfSrTqzZ6BuqGlE42K/AVhc1/ARc+/c=";
   };
 
-  postPatch = ''
-    substituteInPlace pyproject.toml \
-      --replace-fail "setuptools<79.0.0" setuptools
-  '';
-
-  build-system = [ setuptools ];
+  build-system = [ hatchling ];
 
   dependencies = [
     django
@@ -48,9 +43,19 @@ buildPythonPackage rec {
   };
 
   nativeCheckInputs = [
+    pytest-mypy-plugins
     pytestCheckHook
   ]
-  ++ lib.flatten (builtins.attrValues optional-dependencies);
+  ++ lib.flatten (lib.attrValues optional-dependencies);
+
+  disabledTests = [
+    # AttributeError: module 'django.contrib.auth.forms' has no attribute 'SetUnusablePasswordMixin'
+    "test_find_classes_inheriting_from_generic"
+  ];
+
+  disabledTestPaths = [
+    "tests/typecheck"
+  ];
 
   pythonImportsCheck = [ "django-stubs" ];
 
