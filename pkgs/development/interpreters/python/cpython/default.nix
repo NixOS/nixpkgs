amansakhuja/nixpkgs5

python3Minimal: remove pkg-config dependency
Removes the build time dependency on pkg-config if python3 is build with the `withMinimal` flag enabled

This change is driven by the motivation to use python3 earlier in stdenv for hooks.

pkgs/development/interpreters/python/cpython/default.nix
@@ -209,11 +209,11 @@ let
     [
       nukeReferences
     ]
-    ++ optionals (!stdenv.hostPlatform.isDarwin) [
+    ++ optionals (!stdenv.hostPlatform.isDarwin && !withMinimal) [
       autoconf-archive # needed for AX_CHECK_COMPILE_FLAG
       autoreconfHook
     ]
-    ++ optionals (!stdenv.hostPlatform.isDarwin || passthru.pythonAtLeast "3.14") [
+    ++ optionals ((!stdenv.hostPlatform.isDarwin || passthru.pythonAtLeast "3.14") && !withMinimal) [
       pkg-config
     ]
     ++ optionals (stdenv.hostPlatform != stdenv.buildPlatform) [

NixOS/nixpkgs

pkgs/development/interpreters/python/cpython/default.nix
@@ -209,11 +209,11 @@ let
   nativeBuildInputs = [
     nukeReferences
   ]
-  ++ optionals (!stdenv.hostPlatform.isDarwin) [
+  ++ optionals (!stdenv.hostPlatform.isDarwin && !withMinimalDeps) [
     autoconf-archive # needed for AX_CHECK_COMPILE_FLAG
     autoreconfHook
   ]
-  ++ optionals (!stdenv.hostPlatform.isDarwin || passthru.pythonAtLeast "3.14") [
+  ++ optionals ((!stdenv.hostPlatform.isDarwin || passthru.pythonAtLeast "3.14") && !withMinimalDeps) [
     pkg-config
   ]
   ++ optionals (stdenv.hostPlatform != stdenv.buildPlatform) [