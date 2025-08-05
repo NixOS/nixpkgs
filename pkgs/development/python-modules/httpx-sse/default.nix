e44812adc2effb12bff9e479e00139dc4f9fbdd9
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:40:02 +0200

    python3Packages.httpx-sse: 0.4.0 -> 0.4.1
    https://github.com/florimondmanca/httpx-sse/blob/0.4.1/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/httpx-sse/default.nix
+++ pkgs/development/python-modules/httpx-sse/default.nix
@@ -2,10 +2,8 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch2,
   setuptools,
   setuptools-scm,
-  wheel,
   httpx,
   pytest-asyncio,
   pytestCheckHook,
@@ -14,22 +12,15 @@
 
 buildPythonPackage rec {
   pname = "httpx-sse";
-  version = "0.4.0";
+  version = "0.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "florimondmanca";
     repo = "httpx-sse";
     tag = version;
-    hash = "sha256-nU8vkmV/WynzQrSrq9+FQXtfAJPVLpMsRSuntU0HWrE=";
+    hash = "sha256-bSozSZmbRU5sc3jvVUOAXQWVBA8GhzM2R26uPdabS+w=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      url = "https://github.com/florimondmanca/httpx-sse/commit/643938c805e671fa20adcf314b447f862b77bcda.patch?full_index=1";
-      hash = "sha256-V2PyTlleyoLa0DuvdlU8zGNsI9C8bTjMUcLjx81/e5k=";
-    })
-  ];
 
   # pytest-cov configuration is not necessary for packaging
   postPatch = ''
@@ -39,7 +30,6 @@ buildPythonPackage rec {
   build-system = [
     setuptools
     setuptools-scm
-    wheel
   ];
 
   dependencies = [ httpx ];
@@ -55,7 +45,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Consume Server-Sent Event (SSE) messages with HTTPX";
     homepage = "https://github.com/florimondmanca/httpx-sse";
-    changelog = "https://github.com/florimondmanca/httpx-sse/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/florimondmanca/httpx-sse/blob/${src.tag}/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ natsukium ];
   };
