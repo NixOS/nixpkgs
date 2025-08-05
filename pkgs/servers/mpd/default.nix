171215f1828ef638de35a162a8eef4d7688f4ff5
Doron Behar <doron.behar@gmail.com>
2025-08-05 13:45:13 +0300

    mpd: format expression

--- pkgs/servers/mpd/default.nix
+++ pkgs/servers/mpd/default.nix
@@ -154,136 +154,136 @@ let
     ];
   };
 
-      # Disable platform specific features if needed
-      # using libmad to decode mp3 files on darwin is causing a segfault -- there
-      # is probably a solution, but I'm disabling it for now
-      platformMask =
-        lib.optionals stdenv.hostPlatform.isDarwin [
-          "mad"
-          "pulse"
-          "jack"
-          "smbclient"
-        ]
-        ++ lib.optionals (!stdenv.hostPlatform.isLinux) [
-          "alsa"
-          "pipewire"
-          "io_uring"
-          "systemd"
-          "syslog"
-        ];
+  # Disable platform specific features if needed
+  # using libmad to decode mp3 files on darwin is causing a segfault -- there
+  # is probably a solution, but I'm disabling it for now
+  platformMask =
+    lib.optionals stdenv.hostPlatform.isDarwin [
+      "mad"
+      "pulse"
+      "jack"
+      "smbclient"
+    ]
+    ++ lib.optionals (!stdenv.hostPlatform.isLinux) [
+      "alsa"
+      "pipewire"
+      "io_uring"
+      "systemd"
+      "syslog"
+    ];
 
-      knownFeatures =
-        builtins.attrNames featureDependencies ++ builtins.attrNames nativeFeatureDependencies;
-      platformFeatures = lib.subtractLists platformMask knownFeatures;
+  knownFeatures =
+    builtins.attrNames featureDependencies ++ builtins.attrNames nativeFeatureDependencies;
+  platformFeatures = lib.subtractLists platformMask knownFeatures;
 
-      features_ =
-        if (features == null) then
-          platformFeatures
+  features_ =
+    if (features == null) then
+      platformFeatures
+    else
+      let
+        unknown = lib.subtractLists knownFeatures features;
+      in
+      if (unknown != [ ]) then
+        throw "Unknown feature(s): ${lib.concatStringsSep " " unknown}"
+      else
+        let
+          unsupported = lib.subtractLists platformFeatures features;
+        in
+        if (unsupported != [ ]) then
+          throw "Feature(s) ${lib.concatStringsSep " " unsupported} are not supported on ${stdenv.hostPlatform.system}"
         else
-          let
-            unknown = lib.subtractLists knownFeatures features;
-          in
-          if (unknown != [ ]) then
-            throw "Unknown feature(s): ${lib.concatStringsSep " " unknown}"
-          else
-            let
-              unsupported = lib.subtractLists platformFeatures features;
-            in
-            if (unsupported != [ ]) then
-              throw "Feature(s) ${lib.concatStringsSep " " unsupported} are not supported on ${stdenv.hostPlatform.system}"
-            else
-              features;
+          features;
 
 in
-  stdenv.mkDerivation (finalAttrs: {
-      pname = "mpd";
-      version = "0.24.5";
+stdenv.mkDerivation (finalAttrs: {
+  pname = "mpd";
+  version = "0.24.5";
 
-      src = fetchFromGitHub {
-        owner = "MusicPlayerDaemon";
-        repo = "MPD";
-        rev = "v${finalAttrs.version}";
-        sha256 = "sha256-MgepOQeOl+n65+7b8zXe2u0fCHFAviSqL1aNu2iSXiM=";
-      };
+  src = fetchFromGitHub {
+    owner = "MusicPlayerDaemon";
+    repo = "MPD";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-MgepOQeOl+n65+7b8zXe2u0fCHFAviSqL1aNu2iSXiM=";
+  };
 
-      buildInputs = [
-        glib
-        fmt
-        # According to the configurePhase of meson, gtest is considered a
-        # runtime dependency. Quoting:
-        #
-        #    Run-time dependency GTest found: YES 1.10.0
-        gtest
-        libupnp
-      ]
-      ++ concatAttrVals features_ featureDependencies;
+  buildInputs = [
+    glib
+    fmt
+    # According to the configurePhase of meson, gtest is considered a
+    # runtime dependency. Quoting:
+    #
+    #    Run-time dependency GTest found: YES 1.10.0
+    gtest
+    libupnp
+  ]
+  ++ concatAttrVals features_ featureDependencies;
 
-      nativeBuildInputs = [
-        meson
-        ninja
-        pkg-config
-      ]
-      ++ concatAttrVals features_ nativeFeatureDependencies;
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ]
+  ++ concatAttrVals features_ nativeFeatureDependencies;
 
-      depsBuildBuild = [ buildPackages.stdenv.cc ];
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
 
-      postPatch =
-        lib.optionalString
-          (stdenv.hostPlatform.isDarwin && lib.versionOlder stdenv.hostPlatform.darwinSdkVersion "12.0")
-          ''
-            substituteInPlace src/output/plugins/OSXOutputPlugin.cxx \
-              --replace kAudioObjectPropertyElement{Main,Master} \
-              --replace kAudioHardwareServiceDeviceProperty_Virtual{Main,Master}Volume
-          '';
+  postPatch =
+    lib.optionalString
+      (stdenv.hostPlatform.isDarwin && lib.versionOlder stdenv.hostPlatform.darwinSdkVersion "12.0")
+      ''
+        substituteInPlace src/output/plugins/OSXOutputPlugin.cxx \
+          --replace kAudioObjectPropertyElement{Main,Master} \
+          --replace kAudioHardwareServiceDeviceProperty_Virtual{Main,Master}Volume
+      '';
 
-      # Otherwise, the meson log says:
-      #
-      #    Program zip found: NO
-      nativeCheckInputs = [ zip ];
+  # Otherwise, the meson log says:
+  #
+  #    Program zip found: NO
+  nativeCheckInputs = [ zip ];
 
-      doCheck = true;
+  doCheck = true;
 
-      mesonAutoFeatures = "disabled";
+  mesonAutoFeatures = "disabled";
 
-      outputs = [
-        "out"
-        "doc"
-      ]
-      ++ lib.optional (builtins.elem "documentation" features_) "man";
+  outputs = [
+    "out"
+    "doc"
+  ]
+  ++ lib.optional (builtins.elem "documentation" features_) "man";
 
-      CXXFLAGS = lib.optionals stdenv.hostPlatform.isDarwin [
-        "-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=0"
-      ];
+  CXXFLAGS = lib.optionals stdenv.hostPlatform.isDarwin [
+    "-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=0"
+  ];
 
-      mesonFlags = [
-        "-Dtest=true"
-        "-Dmanpages=true"
-        "-Dhtml_manual=true"
-      ]
-      ++ map (x: "-D${x}=enabled") features_
-      ++ map (x: "-D${x}=disabled") (lib.subtractLists features_ knownFeatures)
-      ++ lib.optional (builtins.elem "zeroconf" features_) (
-        "-Dzeroconf=" + (if stdenv.hostPlatform.isDarwin then "bonjour" else "avahi")
-      )
-      ++ lib.optional (builtins.elem "systemd" features_) "-Dsystemd_system_unit_dir=etc/systemd/system"
-      ++ lib.optional (builtins.elem "qobuz" features_) "-Dnlohmann_json=enabled";
+  mesonFlags = [
+    "-Dtest=true"
+    "-Dmanpages=true"
+    "-Dhtml_manual=true"
+  ]
+  ++ map (x: "-D${x}=enabled") features_
+  ++ map (x: "-D${x}=disabled") (lib.subtractLists features_ knownFeatures)
+  ++ lib.optional (builtins.elem "zeroconf" features_) (
+    "-Dzeroconf=" + (if stdenv.hostPlatform.isDarwin then "bonjour" else "avahi")
+  )
+  ++ lib.optional (builtins.elem "systemd" features_) "-Dsystemd_system_unit_dir=etc/systemd/system"
+  ++ lib.optional (builtins.elem "qobuz" features_) "-Dnlohmann_json=enabled";
 
-      passthru.tests.nixos = nixosTests.mpd;
+  passthru.tests.nixos = nixosTests.mpd;
 
-      meta = {
-        description = "Flexible, powerful daemon for playing music";
-        homepage = "https://www.musicpd.org/";
-        license = lib.licenses.gpl2Only;
-        maintainers = with lib.maintainers; [
-          tobim
-        ];
-        platforms = lib.platforms.unix;
-        mainProgram = "mpd";
+  meta = {
+    description = "Flexible, powerful daemon for playing music";
+    homepage = "https://www.musicpd.org/";
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [
+      tobim
+    ];
+    platforms = lib.platforms.unix;
+    mainProgram = "mpd";
 
-        longDescription = ''
-          Music Player Daemon (MPD) is a flexible, powerful daemon for playing
-          music. Through plugins and libraries it can play a variety of sound
-          files while being controlled by its network protocol.
-        '';
-      };
-    })
+    longDescription = ''
+      Music Player Daemon (MPD) is a flexible, powerful daemon for playing
+      music. Through plugins and libraries it can play a variety of sound
+      files while being controlled by its network protocol.
+    '';
+  };
+})
