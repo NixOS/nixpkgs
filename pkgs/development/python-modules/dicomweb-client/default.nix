a10361d443a16b3b533e49480b19474778c7be18
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:37:27 +0200

    python3Packages.dicomweb-client: 0.59.3 -> 0.60.1
    https://github.com/ImagingDataCommons/dicomweb-client/releases/tag/v0.60.1
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/dicomweb-client/default.nix
+++ pkgs/development/python-modules/dicomweb-client/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "dicomweb-client";
-  version = "0.59.3";
+  version = "0.60.1";
   pyproject = true;
 
   disabled = pythonOlder "3.6";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "ImagingDataCommons";
     repo = "dicomweb-client";
     tag = "v${version}";
-    hash = "sha256-D3j5EujrEdGTfR8/V3o2VJ/VkGdZ8IifPYMhP4ppXhw=";
+    hash = "sha256-ZxeZiCw8I5+Bf266PQ6WQA8mBRC7K3/kZrmuW4l6kQU=";
   };
 
   build-system = [ setuptools ];
@@ -47,7 +47,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python client for DICOMweb RESTful services";
     homepage = "https://dicomweb-client.readthedocs.io";
-    changelog = "https://github.com/ImagingDataCommons/dicomweb-client/releases/tag/v${version}";
+    changelog = "https://github.com/ImagingDataCommons/dicomweb-client/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ bcdarwin ];
     mainProgram = "dicomweb_client";
