ebdbb6a04c28e521c510e98ea9e9d574725a2df3
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:43:09 +0200

    python3Packages.pytransportnswv2: 0.8.7 -> 0.9.0
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pytransportnswv2/default.nix
+++ pkgs/development/python-modules/pytransportnswv2/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "pytransportnswv2";
-  version = "0.8.7";
+  version = "0.9.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "PyTransportNSWv2";
     inherit version;
-    hash = "sha256-Jg/oEgbxn0eGf9Bn9wte6Wqc1TYVb8GjzU1mLWk1qtE=";
+    hash = "sha256-J3OW8fWldbkKzCDlXSv7nucVdyEnDFx8uCicF+ELQkQ=";
   };
 
   build-system = [ setuptools ];
