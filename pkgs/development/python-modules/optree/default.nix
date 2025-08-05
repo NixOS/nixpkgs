6404cf5e23f4e040018350bcfdb9a9a296aeb5e4
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:41:51 +0200

    python3Packages.optree: 0.14.1 -> 0.17.0
    https://github.com/metaopt/optree/releases
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/optree/default.nix
+++ pkgs/development/python-modules/optree/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "optree";
-  version = "0.14.1";
+  version = "0.17.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "metaopt";
     repo = "optree";
     tag = "v${version}";
-    hash = "sha256-5PIe/mXPNohwM0oNT/zSPmNUycjXuujtIFCki5t7V1I=";
+    hash = "sha256-4ZkUdGF+Fauy6KWbyrGQ684Ay5XlFT2S2I9lv/1KeWs=";
   };
 
   dontUseCmakeConfigure = true;
