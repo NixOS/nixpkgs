f58468aafd6899b367089a1f1e6bef745b76e392
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:43:11 +0200

    python3Packages.pyvex: 9.2.154 -> 9.2.168
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pyvex/default.nix
+++ pkgs/development/python-modules/pyvex/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "pyvex";
-  version = "9.2.154";
+  version = "9.2.168";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-a3ei2w66v18QKAofpPvDUoM42zHRHPrNQic+FE+rLKY=";
+    hash = "sha256-FejNT17IXNIJZZyg1KHxh5dxtR3O2T1lYRfAECjSSl4=";
   };
 
   build-system = [ setuptools ];
