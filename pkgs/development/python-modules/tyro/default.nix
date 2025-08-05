f1160d31e0095c03aed99fedea6b5835e087ad54
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:44:35 +0200

    python3Packages.tyro: 0.9.19 -> 0.9.27
    https://github.com/brentyi/tyro/releases/tag/v0.9.27
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/tyro/default.nix
+++ pkgs/development/python-modules/tyro/default.nix
@@ -26,14 +26,14 @@
 
 buildPythonPackage rec {
   pname = "tyro";
-  version = "0.9.19";
+  version = "0.9.27";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "brentyi";
     repo = "tyro";
     tag = "v${version}";
-    hash = "sha256-A1Vplc84Xy8TufqmklPUzIdgiPpFcIjqV0eUgdKmYRM=";
+    hash = "sha256-2duLVdBwNpGWCV+WgtzyXjoVhukVjUUhIWXVBEk4QIA=";
   };
 
   build-system = [ hatchling ];
@@ -62,7 +62,7 @@ buildPythonPackage rec {
   meta = {
     description = "CLI interfaces & config objects, from types";
     homepage = "https://github.com/brentyi/tyro";
-    changelog = "https://github.com/brentyi/tyro/releases/tag/v${version}";
+    changelog = "https://github.com/brentyi/tyro/releases/tag/${src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ hoh ];
   };
