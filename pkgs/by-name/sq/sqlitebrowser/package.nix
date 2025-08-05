06a579de3eaf63ace8d1c8c0baa913b6f7ba9cf8
Peter H. Hoeg <peter@hoeg.com>
2025-08-05 18:33:20 +0200

    sqlitebrowser: use qscintilla and qcustomplot from nixpkgs

--- pkgs/by-name/sq/sqlitebrowser/package.nix
+++ pkgs/by-name/sq/sqlitebrowser/package.nix
@@ -3,10 +3,15 @@
   stdenv,
   fetchFromGitHub,
   cmake,
-  qt5,
+  pkg-config,
+  libsForQt5,
   sqlcipher,
 }:
 
+let
+  qt' = libsForQt5; # upstream has adopted qt6, but no released version supports it
+
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "sqlitebrowser";
   version = "3.13.1";
@@ -14,41 +19,52 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "sqlitebrowser";
     repo = "sqlitebrowser";
-    rev = "v${finalAttrs.version}";
-    sha256 = "sha256-bpZnO8i8MDgOm0f93pBmpy1sZLJQ9R4o4ZLnGfT0JRg=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-bpZnO8i8MDgOm0f93pBmpy1sZLJQ9R4o4ZLnGfT0JRg=";
   };
 
   patches = lib.optional stdenv.hostPlatform.isDarwin ./macos.patch;
 
-  # We should be using qscintilla from nixpkgs instead of the vendored version,
-  # but qscintilla is currently in a bit of a mess as some consumers expect a
-  # -qt4 or -qt5 prefix while others do not.
-  # We *really* should get that cleaned up.
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail '"Unknown"' '"${finalAttrs.src.rev}"'
+  '';
+
   buildInputs = [
-    qt5.qtbase
+    qt'.qtbase
+    qt'.qcustomplot
+    qt'.qscintilla
     sqlcipher
   ]
-  ++ lib.optional stdenv.hostPlatform.isDarwin qt5.qtmacextras;
+  ++ lib.optional stdenv.hostPlatform.isDarwin qt'.qtmacextras;
 
   nativeBuildInputs = [
     cmake
-    qt5.qttools
-    qt5.wrapQtAppsHook
+    pkg-config
+    qt'.qttools
+    qt'.wrapQtAppsHook
   ];
 
   cmakeFlags = [
-    "-Dsqlcipher=1"
+    "-Wno-dev"
+    (lib.cmakeBool "sqlcipher" true)
     (lib.cmakeBool "ENABLE_TESTING" (finalAttrs.finalPackage.doCheck or false))
+    (lib.cmakeBool "FORCE_INTERNAL_QSCINTILLA" false)
+    (lib.cmakeBool "FORCE_INTERNAL_QCUSTOMPLOT" false)
+    (lib.cmakeBool "FORCE_INTERNAL_QHEXEDIT" true) # TODO: package qhexedit
+    (lib.cmakeFeature "QSCINTILLA_INCLUDE_DIR" "${lib.getDev qt'.qscintilla}/include")
   ];
+
+  env.LANG = "C.UTF-8";
 
   doCheck = true;
 
-  meta = with lib; {
+  meta = {
     description = "DB Browser for SQLite";
     mainProgram = "sqlitebrowser";
     homepage = "https://sqlitebrowser.org/";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ peterhoeg ];
-    platforms = platforms.unix;
+    license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [ peterhoeg ];
+    platforms = lib.platforms.unix;
   };
 })
