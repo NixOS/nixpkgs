df2fedbf362b32f3f1a9b15800296c40e79708aa
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:38:35 +0200

    python3Packages.extension-helpers: 1.2.0 -> 1.4.0
    https://github.com/astropy/extension-helpers/blob/v1.4.0/CHANGES.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/extension-helpers/default.nix
+++ pkgs/development/python-modules/extension-helpers/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "extension-helpers";
-  version = "1.2.0";
+  version = "1.4.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "astropy";
     repo = "extension-helpers";
     tag = "v${version}";
-    hash = "sha256-qneulhSYB2gYiCdgoU7Dqg1luLWhVouFVihcKeOA37E=";
+    hash = "sha256-coSgaPoz93CqJRb65xYs1sNOwoGhcxWGJF7Jc9N2W1I=";
   };
 
   build-system = [
@@ -48,7 +48,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Helpers to assist with building Python packages with compiled C/Cython extensions";
     homepage = "https://github.com/astropy/extension-helpers";
-    changelog = "https://github.com/astropy/extension-helpers/blob/${version}/CHANGES.md";
+    changelog = "https://github.com/astropy/extension-helpers/blob/${src.tag}/CHANGES.md";
     license = licenses.bsd3;
     maintainers = with maintainers; [ fab ];
   };
