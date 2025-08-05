7d0308e229bf87d6fa978d2aa37ec5ae1cf5b369
Jack Maloney <jmmaloney4@gmail.com>
2025-08-05 10:37:35 -0500

    python3Packages.gcsfs: 2025.3.2 -> 2025.7.0

--- pkgs/development/python-modules/gcsfs/default.nix
+++ pkgs/development/python-modules/gcsfs/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "gcsfs";
-  version = "2025.3.2";
+  version = "2025.7.0";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "fsspec";
     repo = "gcsfs";
     tag = version;
-    hash = "sha256-aXBlj9ej3Ya7h4x/akl/iX6dDS/SgkkEsOQ2E9KmCDU=";
+    hash = "sha256-5s8t4pZVYGV9nVaza2JZ+sOnTXSlKX3qoZ66tfOPZQ4=";
   };
 
   build-system = [
