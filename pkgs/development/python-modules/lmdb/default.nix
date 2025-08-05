8e6409b195c3de88ad6f5968b95fc182e3ecab99
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:41:02 +0200

    python3Packages.lmdb: 1.6.2 -> 1.7.3
    https://github.com/jnwatson/py-lmdb/blob/py-lmdb_1.7.3/ChangeLog
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/lmdb/default.nix
+++ pkgs/development/python-modules/lmdb/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "lmdb";
-  version = "1.6.2";
+  version = "1.7.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0o4/pZk1/2iIWHYOxS8gLsuMEImj9o0fFi6jB40VHnM=";
+    hash = "sha256-1KJ7evT+OPNAnZ+/v0e2F7PZTe6YoAvIwqgzbM0/mxU=";
   };
 
   build-system = [ setuptools ];
