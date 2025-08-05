80de7210ac78a7b848e991985087ff8df0e040a7
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:45:04 +0200

    python3Packages.zha-quirks: 0.0.139 -> 0.0.142
    https://github.com/zigpy/zha-device-handlers/releases/tag/0.0.142
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/zha-quirks/default.nix
+++ pkgs/development/python-modules/zha-quirks/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "zha-quirks";
-  version = "0.0.139";
+  version = "0.0.142";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zha-device-handlers";
     tag = version;
-    hash = "sha256-16gv2t1hudIULybmAXK+sMl9MsFATHMGQeZWhmVhrkk=";
+    hash = "sha256-D1FIkyVSa4j6p4PHkjCAU08zpZjjXPGWoL5lKlWUHuU=";
   };
 
   postPatch = ''
