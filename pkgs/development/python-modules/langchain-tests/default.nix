a07a471bd28298f8502c461d1d0b07697c9d6571
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:40:33 +0200

    python3Packages.langchain-tests: 0.3.20 -> 0.3.72
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/langchain-tests/default.nix
+++ pkgs/development/python-modules/langchain-tests/default.nix
@@ -30,14 +30,14 @@
 
 buildPythonPackage rec {
   pname = "langchain-tests";
-  version = "0.3.20";
+  version = "0.3.72";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langchain";
-    tag = "langchain-tests==${version}";
-    hash = "sha256-RMuxWA/n8d71FReFKO3Y/5P0MYk4aZ5WU2/TRxf9UuE=";
+    tag = "langchain-core==${version}";
+    hash = "sha256-Q2uGMiODUtwkPdOyuSqp8vqjlLjiXk75QjXp7rr20tc=";
   };
 
   sourceRoot = "${src.name}/libs/standard-tests";
