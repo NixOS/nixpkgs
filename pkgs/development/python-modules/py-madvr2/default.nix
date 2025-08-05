8e007af33276a9a46508f0b610e9d5ea27249bdd
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:42:17 +0200

    python3Packages.py-madvr2: 1.6.33 -> 1.8.14
    https://github.com/iloveicedgreentea/py-madvr/releases/tag/v1.8.14
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/py-madvr2/default.nix
+++ pkgs/development/python-modules/py-madvr2/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "py-madvr2";
-  version = "1.6.33";
+  version = "1.8.14";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "iloveicedgreentea";
     repo = "py-madvr";
     tag = "v${version}";
-    hash = "sha256-z+PVLz9eApGJ94I/Jp0MyqNpKQwIemk8j+OyqFmIbgI=";
+    hash = "sha256-7zYvbEoPlY49YZ9Akq+SfzMmqClrr3xTszVW2FUx62Y=";
   };
 
   build-system = [ setuptools ];
@@ -29,7 +29,7 @@ buildPythonPackage rec {
   ];
 
   meta = {
-    changelog = "https://github.com/iloveicedgreentea/py-madvr/releases/tag/v${version}";
+    changelog = "https://github.com/iloveicedgreentea/py-madvr/releases/tag/${src.tag}";
     description = "Control MadVR Envy over IP";
     homepage = "https://github.com/iloveicedgreentea/py-madvr";
     license = lib.licenses.mit;
