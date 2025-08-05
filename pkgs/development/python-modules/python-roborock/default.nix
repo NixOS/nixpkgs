fb7cdec0b6e3971d0d3c9b22d96fd5655f6600c0
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:43:06 +0200

    python3Packages.python-roborock: 2.25.0 -> 2.27.0
    https://github.com/humbertogontijo/python-roborock/blob/v2.27.0/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/python-roborock/default.nix
+++ pkgs/development/python-modules/python-roborock/default.nix
@@ -25,7 +25,7 @@
 
 buildPythonPackage rec {
   pname = "python-roborock";
-  version = "2.25.0";
+  version = "2.27.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -34,7 +34,7 @@ buildPythonPackage rec {
     owner = "humbertogontijo";
     repo = "python-roborock";
     tag = "v${version}";
-    hash = "sha256-RsNWhcScp81plqXg9NmRFJhF+aLA0ld0A5H6mHo60uE=";
+    hash = "sha256-3iNqeRCdkkL77hYlh+LRo8ZKExcpk075eBbnp8oJQ8w=";
   };
 
   postPatch = ''
