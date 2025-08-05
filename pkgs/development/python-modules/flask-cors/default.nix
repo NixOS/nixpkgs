7c9ab5410e76f09d5c7564f348babc62d74bb230
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:39:03 +0200

    python3Packages.flask-cors: 6.0.0 -> 6.0.1
    https://github.com/corydolphin/flask-cors/releases/tag/6.0.1
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/flask-cors/default.nix
+++ pkgs/development/python-modules/flask-cors/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "flask-cors";
-  version = "6.0.0";
+  version = "6.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "corydolphin";
     repo = "flask-cors";
     tag = version;
-    hash = "sha256-J9OTWVS0GXxfSedfHeifaJ0LR8xFKksf0RGsKSc581E=";
+    hash = "sha256-ySn5o9yDlCYqHozGJ82cPtty/N+EK/NvIynxv9w+hwc=";
   };
 
   build-system = [
