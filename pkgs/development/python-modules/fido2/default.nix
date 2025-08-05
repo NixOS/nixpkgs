d190e78d7fa7e60877f54dd1918f541db7a716cc
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:38:51 +0200

    python3Packages.fido2: 1.2.0 -> 2.0.0
    https://github.com/Yubico/python-fido2/releases/tag/2.0.0
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/fido2/default.nix
+++ pkgs/development/python-modules/fido2/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "fido2";
-  version = "1.2.0";
+  version = "2.0.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-45+VkgEi1kKD/aXlWB2VogbnBPpChGv6RmL4aqDTMzs=";
+    hash = "sha256-MGHNBec7Og72r8O4A9V8gmqi1qlzLRar1ydzYfWOeWQ=";
   };
 
   build-system = [ poetry-core ];
