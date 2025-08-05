4582ac9f2ee26225b11870fdc2717ffa1e92dbe7
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:35:27 +0200

    python3Packages.azure-mgmt-iothub: 3.0.0 -> 4.0.0
    https://github.com/Azure/azure-sdk-for-python/blob/azure-mgmt-iothub_4.0.0/sdk/iothub/azure-mgmt-iothub/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/azure-mgmt-iothub/default.nix
+++ pkgs/development/python-modules/azure-mgmt-iothub/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-iothub";
-  version = "3.0.0";
+  version = "4.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-2vIfyYxoo1PsYWMYwOYr4EyNaJmWC+jCy/mRZzrItyI=";
+    hash = "sha256-B/Jb1vZzdLqxfMEZL5+SGzUONWAlHxkGnmZlg1Qe1Ng=";
   };
 
   propagatedBuildInputs = [
