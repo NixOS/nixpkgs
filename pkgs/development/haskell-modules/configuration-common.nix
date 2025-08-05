d3e9ec723da7d09766e0eef107041baac76279d4
sternenseemann <sternenseemann@systemli.org>
2025-08-05 15:21:35 +0200

    haskellPackages.chell-quickcheck: mark as broken
    Upstream doesn't seem to intend to maintain chell going forward, so this
    will probably not get fixed. chell-quickcheck is only used by two
    packages on Hackage, so we don't need to care too much.
    
    haskellPackages.gnuidn: disable test suite due to chell-quickcheck

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1815,6 +1815,7 @@ with haskellLib;
   # chell-quickcheck doesn't work with QuickCheck >= 2.15 with no known fix yet
   # https://github.com/typeclasses/chell/issues/5
   system-filepath = dontCheck super.system-filepath;
+  gnuidn = dontCheck super.gnuidn;
 
   # Tests rely on `Int` being 64-bit: https://github.com/hspec/hspec/issues/431.
   # Also, we need QuickCheck-2.14.x to build the test suite, which isn't easy in LTS-16.x.
