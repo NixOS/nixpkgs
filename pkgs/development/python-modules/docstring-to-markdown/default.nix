a6b12464e6f9d6fc88ae4019992b390cc3eee4ce
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:38:05 +0200

    python3Packages.docstring-to-markdown: 0.15 -> 0.17
    https://github.com/python-lsp/docstring-to-markdown/releases/tag/v0.17
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/docstring-to-markdown/default.nix
+++ pkgs/development/python-modules/docstring-to-markdown/default.nix
@@ -2,27 +2,34 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  importlib-metadata,
   pytestCheckHook,
-  pythonOlder,
+  setuptools,
+  typing-extensions,
 }:
 
 buildPythonPackage rec {
   pname = "docstring-to-markdown";
-  version = "0.15";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.7";
+  version = "0.17";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "python-lsp";
     repo = "docstring-to-markdown";
     tag = "v${version}";
-    hash = "sha256-ykqY7LFIOTuAddYkKDzIltq8FpLVz4v2ZA3Y0cZH9ms=";
+    hash = "sha256-conwwToBrlDL487zf2ldCOxFFKxP1a8LnU0KocI8riI=";
   };
 
   postPatch = ''
     sed -i -E '/--(cov|flake8)/d' setup.cfg
   '';
+
+  build-system = [ setuptools ];
+
+  dependencies = [
+    importlib-metadata
+    typing-extensions
+  ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
@@ -31,7 +38,7 @@ buildPythonPackage rec {
   meta = with lib; {
     homepage = "https://github.com/python-lsp/docstring-to-markdown";
     description = "On the fly conversion of Python docstrings to markdown";
-    changelog = "https://github.com/python-lsp/docstring-to-markdown/releases/tag/v${version}";
+    changelog = "https://github.com/python-lsp/docstring-to-markdown/releases/tag/${src.tag}";
     license = licenses.lgpl2Plus;
     maintainers = with maintainers; [ doronbehar ];
   };
