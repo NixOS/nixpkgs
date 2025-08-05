e778ec435a35a907c9866c44cf09c8400ab53ca6
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:41:06 +0200

    python3Packages.manifestoo-core: 1.9 -> 1.10
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/manifestoo-core/default.nix
+++ pkgs/development/python-modules/manifestoo-core/default.nix
@@ -11,13 +11,13 @@
 
 buildPythonPackage rec {
   pname = "manifestoo-core";
-  version = "1.9";
+  version = "1.10";
   format = "pyproject";
 
   src = fetchPypi {
     inherit version;
     pname = "manifestoo_core";
-    hash = "sha256-4cBgxbjXfOVMRQ+iQnjb/LdRUkoeb2hWI6VhSnqSMVM=";
+    hash = "sha256-LLxr96/cuAAncddMeBBVlFq2Hl5+pNXqgMbvbnfzcE8=";
   };
 
   nativeBuildInputs = [ hatch-vcs ];
