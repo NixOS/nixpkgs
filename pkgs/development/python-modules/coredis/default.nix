a519b73750da2717573bfe2434aa50f6a719d699
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:36:55 +0200

    python3Packages.coredis: 4.23.1 -> 5.0.1
    https://github.com/alisaifee/coredis/blob/5.0.1/HISTORY.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/coredis/default.nix
+++ pkgs/development/python-modules/coredis/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "coredis";
-  version = "4.23.1";
+  version = "5.0.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "alisaifee";
     repo = "coredis";
     tag = version;
-    hash = "sha256-5Ho9X2VCOwKo079M2ReJ93jqEpG2ZV6vKM5/qrgzjxM=";
+    hash = "sha256-LDK/tVGBsuhf0WzGjdCJznUVh9vrtRrjtU0wKpsr/Ag=";
   };
 
   postPatch = ''
