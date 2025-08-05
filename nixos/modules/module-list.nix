397bd279c251cbb0dacdc8270f75575f320e989a
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-08-05 12:18:58 -0300

    nixos/ctrld: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1112,6 +1112,7 @@
   ./services/networking/crab-hole.nix
   ./services/networking/create_ap.nix
   ./services/networking/croc.nix
+  ./services/networking/ctrld.nix
   ./services/networking/dae.nix
   ./services/networking/dante.nix
   ./services/networking/ddclient.nix
