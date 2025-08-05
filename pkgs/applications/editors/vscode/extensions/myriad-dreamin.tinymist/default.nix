41fc0babd1368111d083282c0026b13d5d13e766
Jost Alemann <jost_alemann@me.com>
2025-08-05 21:07:56 +0200

    tinymist: 0.13.18 -> 0.13.20
    Changelog:
    https://github.com/Myriad-Dreamin/tinymist/releases/tag/v0.13.20
    Diff:
    https://github.com/Myriad-Dreamin/tinymist/compare/v0.13.18...v0.13.20

--- pkgs/applications/editors/vscode/extensions/myriad-dreamin.tinymist/default.nix
+++ pkgs/applications/editors/vscode/extensions/myriad-dreamin.tinymist/default.nix
@@ -11,7 +11,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
     name = "tinymist";
     publisher = "myriad-dreamin";
     inherit (tinymist) version;
-    hash = "sha256-v/85T/MIEVrbXDDgIa6cBXPjpMmxZFXDglUEZvyRZSc=";
+    hash = "sha256-uHUnCg8hkJQI0QSZZ5Kbj/S7ktY9ANdQqHCvBB+p1EA=";
   };
 
   nativeBuildInputs = [
