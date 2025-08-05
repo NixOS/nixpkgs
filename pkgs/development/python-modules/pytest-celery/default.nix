3c8793812839a344bcb390f17a7aa142f489423c
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:42:54 +0200

    python3Packages.pytest-celery: 1.1.3 -> 1.2.1
    https://github.com/celery/pytest-celery/blob/v1.2.1/Changelog.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pytest-celery/default.nix
+++ pkgs/development/python-modules/pytest-celery/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pytest-celery";
-  version = "1.1.3";
+  version = "1.2.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "celery";
     repo = "pytest-celery";
     tag = "v${version}";
-    hash = "sha256-TUtKfGOxvVkiMhsUqyNDK08OTuzzKHrBiPU4JCKsIKM=";
+    hash = "sha256-E8GO/00IC9kUvQLZmTFaK4FFQ7d+/tw/kVTQbAqRRRM=";
   };
 
   postPatch = ''
@@ -60,7 +60,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Pytest plugin to enable celery.contrib.pytest";
     homepage = "https://github.com/celery/pytest-celery";
-    changelog = "https://github.com/celery/pytest-celery/blob/v${version}/Changelog.rst";
+    changelog = "https://github.com/celery/pytest-celery/blob/${src.tag}/Changelog.rst";
     license = licenses.mit;
     maintainers = [ ];
   };
