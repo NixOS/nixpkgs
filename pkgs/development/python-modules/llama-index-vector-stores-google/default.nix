779c01e2a58d8aefb35eccf9ac2a5a63802549a2
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:40:59 +0200

    python3Packages.llama-index-vector-stores-google: 0.3.0 -> 0.4.0
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/llama-index-vector-stores-google/default.nix
+++ pkgs/development/python-modules/llama-index-vector-stores-google/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "llama-index-vector-stores-google";
-  version = "0.3.0";
+  version = "0.4.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "llama_index_vector_stores_google";
     inherit version;
-    hash = "sha256-6l4MFO7h5xJexN3Sf78F+OgzaKHNWxOffQvkqRhXEJw=";
+    hash = "sha256-EjokpP+46z/OwgmtQO4OnL+w4mUFR0M+2MmycojAc7E=";
   };
 
   pythonRelaxDeps = [ "google-generativeai" ];
