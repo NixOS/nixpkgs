7084e3e278edf303ca4163d16ee54631ffe3c8b3
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:42:11 +0200

    python3Packages.progress: 1.6 -> 1.6.1
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/progress/default.nix
+++ pkgs/development/python-modules/progress/default.nix
@@ -6,13 +6,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "1.6";
+  version = "1.6.1";
   format = "setuptools";
   pname = "progress";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "c9c86e98b5c03fa1fe11e3b67c1feda4788b8d0fe7336c2ff7d5644ccfba34cd";
+    sha256 = "sha256-wbpxn4Ys6IUjKnWeq0eXH+dN/Hu3arilHvWUC601CGw=";
   };
 
   checkPhase = ''
