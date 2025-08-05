ebcb58360f9043b29d821fd30a7bbbb2dc57270c
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:42:44 +0200

    python3Packages.pyqt6-charts: 6.8.0 -> 6.9.0
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pyqt6-charts/default.nix
+++ pkgs/development/python-modules/pyqt6-charts/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "pyqt6-charts";
-  version = "6.8.0";
+  version = "6.9.0";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "PyQt6_Charts";
     inherit version;
-    hash = "sha256-+GcFuHQOMEFmfOIRrqogW3UOtrr0yQj04/bcjHINEPE=";
+    hash = "sha256-fvvpu35q1PmEUhGg7+D5HKXhT5Ni7RuoTVXyuFFQkfc=";
   };
 
   # fix include path and increase verbosity
