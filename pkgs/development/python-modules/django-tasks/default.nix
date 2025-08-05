b339dde0279e4d040dda18f29ffce8a24f9e11d4
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-05 04:37:53 +0200

    python3Packages.django-tasks: 0.7.0 -> 0.8.1
    https://github.com/RealOrangeOne/django-tasks/releases/tag/0.8.1
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/django-tasks/default.nix
+++ pkgs/development/python-modules/django-tasks/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "django-tasks";
-  version = "0.7.0";
+  version = "0.8.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "RealOrangeOne";
     repo = "django-tasks";
     tag = version;
-    hash = "sha256-AWsqAvn11uklrFXtiV2a6fR3owZ02osEzrdHZgDKkOM=";
+    hash = "sha256-fXXqPmpyIq+66okWDmTIBaoaslY8BSILXjJWn8cXnMM=";
   };
 
   build-system = [
@@ -74,7 +74,7 @@ buildPythonPackage rec {
   meta = {
     description = "Reference implementation and backport of background workers and tasks in Django";
     homepage = "https://github.com/RealOrangeOne/django-tasks";
-    changelog = "https://github.com/RealOrangeOne/django-tasks/releases/tag/${version}";
+    changelog = "https://github.com/RealOrangeOne/django-tasks/releases/tag/${src.tag}";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ GaetanLepage ];
   };
