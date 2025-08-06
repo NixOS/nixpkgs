d2c582a6f2cdf85d82092f4591172890496bedb6
Moritz Heidkamp <moritz@twoticketsplease.de>
2025-08-06 22:01:55 +0200

    babashka: Don't set DEPS_CLJ_TOOLS_DIR in wrapper
    Instead, introduce custom NIX_DEPS_CLJ_TOOLS_DIR so as not to also affect sub-processes spawned by
    babashka (see patch comment for more details).
    
    Resolves #431312

--- pkgs/development/interpreters/babashka/default.nix
+++ pkgs/development/interpreters/babashka/default.nix
@@ -4,6 +4,8 @@
   fetchurl,
   writeScript,
   installShellFiles,
+  unzip,
+  zip,
 }:
 
 buildGraalvmNativeImage (finalAttrs: {
@@ -15,7 +17,24 @@ buildGraalvmNativeImage (finalAttrs: {
     sha256 = "sha256-2JrfFddG5ZSJkDXjaNnAyYfvuwR3muDgEVyG82KRFkU=";
   };
 
-  nativeBuildInputs = [ installShellFiles ];
+  nativeBuildInputs = [
+    installShellFiles
+    unzip
+    zip
+  ];
+
+  dontUnpack = false;
+  unpackCmd = "unzip -d babashka $curSrc";
+
+  patches = [
+    ./default-tools-dir.patch
+  ];
+
+  preBuild = ''
+    zip -r ../babashka.jar .
+    cd ..
+    src=babashka.jar
+  '';
 
   extraNativeImageBuildArgs = [
     "-H:+ReportExceptionStackTraces"
