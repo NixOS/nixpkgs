417a8c733053d26c4524a95a6c1d423475963a76
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-06 17:27:17 +0000

    teamviewer: 15.67.4 -> 15.68.5

--- pkgs/applications/networking/remote/teamviewer/default.nix
+++ pkgs/applications/networking/remote/teamviewer/default.nix
@@ -30,7 +30,7 @@ mkDerivation rec {
     "out"
     "dev"
   ];
-  version = "15.67.4";
+  version = "15.68.5";
 
   src =
     let
@@ -39,7 +39,7 @@ mkDerivation rec {
     {
       x86_64-linux = fetchurl {
         url = "${base_url}/teamviewer_${version}_amd64.deb";
-        hash = "sha256-ibKRYgsvBmh18LfG29ve/yrDEFTdgsNZ3kJu8YkMbsw=";
+        hash = "sha256-+MTp2ArZTcGFr1YwHIRfBIpjkRm0i9C1Pt5TzDE1SNE=";
       };
       aarch64-linux = fetchurl {
         url = "${base_url}/teamviewer_${version}_arm64.deb";
