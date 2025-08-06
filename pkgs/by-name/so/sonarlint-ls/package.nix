6f94b39533232ef998c8c6d84e46b747603df123
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-06 18:58:00 +0000

    sonarlint-ls: 3.25.0.76263 -> 3.28.0.76421

--- pkgs/by-name/so/sonarlint-ls/package.nix
+++ pkgs/by-name/so/sonarlint-ls/package.nix
@@ -16,17 +16,17 @@
 
 maven.buildMavenPackage rec {
   pname = "sonarlint-ls";
-  version = "3.25.0.76263";
+  version = "3.28.0.76421";
 
   src = fetchFromGitHub {
     owner = "SonarSource";
     repo = "sonarlint-language-server";
     rev = version;
-    hash = "sha256-bnR6h2NRdGwmx04ydQIlE2VMe/C23YRqNxdbbb19yzE=";
+    hash = "sha256-hnzDiRUiplxR5Ooditbk1NbQ9ItZxhkBs40WJHLj0FE=";
   };
 
   mvnJdk = jdk17;
-  mvnHash = "sha256-cRDrd2QysN3KCndfdnTn8/hXuJ1xd28GcE8vrd6ILuM=";
+  mvnHash = "sha256-hH79BeXlPd+KjSpefx2KkEyyOYjW/Hfrv+mS5F7NYDc=";
 
   # Disables failing tests which either need network access or are flaky.
   mvnParameters = lib.escapeShellArgs [
