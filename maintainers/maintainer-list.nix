bcb324a007a80a6549beb6782d6d37978d5fc7b8
dmkhitaryan <davtot111@icloud.com>
2025-08-06 03:16:10 +0200

    maintainers: add dmkhitaryan

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6583,6 +6583,12 @@
     githubId = 875324;
     name = "David Johnson";
   };
+  dmkhitaryan = {
+    name = "David Mkhitaryan";
+    email = "d.mkhitaryan@mailbox.org";
+    github = "dmkhitaryan";
+    githubId = 63636798;
+  };
   dmvianna = {
     email = "dmlvianna@gmail.com";
     github = "dmvianna";
