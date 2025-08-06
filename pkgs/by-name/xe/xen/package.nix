6b445c19c41203c268d8354e0a811b880a5e8236
Fernando Rodrigues <alpha@sigmasquadron.net>
2025-08-06 13:42:59 +1000

    xen: 4.19.3-unstable-2025-07-09 -> 4.19.3
    4.19.3 has been released properly, so we don't need to track an unstable commit.
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/by-name/xe/xen/package.nix
+++ pkgs/by-name/xe/xen/package.nix
@@ -5,8 +5,7 @@
 
 buildXenPackage.override { inherit python3Packages; } {
   pname = "xen";
-  version = "4.19.3-unstable-2025-07-09";
-  upstreamVersion = "4.19.3-pre";
-  rev = "b026848daf3cca7a4e1d234c8a3b91ebed270e65";
-  hash = "sha256-ThlKeS8SgrcXJvwSIQIE+GvSd5WmTskw+S9JowwFVHg=";
+  version = "4.19.3";
+  rev = "077419f04a3125c58dcf9724c954f98d1e927392";
+  hash = "sha256-e9aPLgzNVxUn7WnLbBHwFIN02DAObfA24VjiqdiP+jA=";
 }
