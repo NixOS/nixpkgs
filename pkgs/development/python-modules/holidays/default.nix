b745d39bedcad23d3ba724ebbd835fabd25f21b5
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-06 21:00:02 +0200

    python3Packages.holidays: 0.75 -> 0.78
    https://github.com/vacanza/python-holidays/releases/tag/v0.78

--- pkgs/development/python-modules/holidays/default.nix
+++ pkgs/development/python-modules/holidays/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "holidays";
-  version = "0.75";
+  version = "0.78";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "vacanza";
     repo = "python-holidays";
     tag = "v${version}";
-    hash = "sha256-xlDCIQb1XeMi+uNIkqMgm1t+fDSuQlqgOx57LnAuyVo=";
+    hash = "sha256-THZg1125rN5HLoiw7xMiKwSNcKzXZgXL8DkbnCMiJ/c=";
   };
 
   build-system = [
