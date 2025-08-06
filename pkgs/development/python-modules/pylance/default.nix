e01d96c25ac15c406250b73e071d9dcd337d2093
Gaetan Lepage <gaetan@glepage.com>
2025-08-07 01:34:27 +0200

    python3Packages.pylance: 0.32.0 -> 0.32.1
    Diff: https://github.com/lancedb/lance/compare/refs/tags/v0.32.0...refs/tags/v0.32.1
    
    Changelog: https://github.com/lancedb/lance/releases/tag/v0.32.1

--- pkgs/development/python-modules/pylance/default.nix
+++ pkgs/development/python-modules/pylance/default.nix
@@ -32,14 +32,14 @@
 
 buildPythonPackage rec {
   pname = "pylance";
-  version = "0.32.0";
+  version = "0.32.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "lancedb";
     repo = "lance";
     tag = "v${version}";
-    hash = "sha256-hVWyZv978hDjAOdk4S9S9RJOkxqhOL0ZBpi4wGk0h1c=";
+    hash = "sha256-P40m8ak0A2l4zGAYcbvXGidQpIT3+ayERbleWcVuLbE=";
   };
 
   sourceRoot = "${src.name}/python";
@@ -51,7 +51,7 @@ buildPythonPackage rec {
       src
       sourceRoot
       ;
-    hash = "sha256-ZUNAZsOpLdpdsKhIp/6QD3Ys7MOeO6H3ve8au7g+riU=";
+    hash = "sha256-rCaLREl2LSfpu0vwa1Vx2rQ7phWsGz58RTjo6yfHKLU=";
   };
 
   nativeBuildInputs = [
