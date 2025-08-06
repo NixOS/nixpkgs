e195343469f3419f47d36db43d7a68fef378f8dc
sund3RRR <evenquantity@gmail.com>
2025-08-06 19:55:44 +0300

    amnezia-vpn: 4.8.9.1 -> 4.8.9.2

--- pkgs/by-name/am/amnezia-vpn/package.nix
+++ pkgs/by-name/am/amnezia-vpn/package.nix
@@ -65,13 +65,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "amnezia-vpn";
-  version = "4.8.9.1";
+  version = "4.8.9.2";
 
   src = fetchFromGitHub {
     owner = "amnezia-vpn";
     repo = "amnezia-client";
     tag = finalAttrs.version;
-    hash = "sha256-docQqOVzmgqWPhKzOmKeXhssjyhtfYy1fNn0ZGXjsZ0=";
+    hash = "sha256-UavKtAwnEa+Ym1a7XzC3JPDLovqggjsav4q2MiYUxbI=";
     fetchSubmodules = true;
   };
 
