2e11500ee74a6079c6bbddd4d80cd9c79d26614f
panicgh <79252025+panicgh@users.noreply.github.com>
2025-08-06 01:37:23 +0000

    python3Packages.fido2: skip device tests (#431308)
    By default, unit tests include hardware authenticator device tests. They
    hang nixpkgs builds for a long time waiting for a touch of the
    authenticator device. Skip such tests to avoid waiting.

--- pkgs/development/python-modules/fido2/default.nix
+++ pkgs/development/python-modules/fido2/default.nix
@@ -5,7 +5,6 @@
   fetchPypi,
   poetry-core,
   pyscard,
-  pythonOlder,
   pytestCheckHook,
 }:
 
@@ -13,8 +12,6 @@ buildPythonPackage rec {
   pname = "fido2";
   version = "2.0.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
@@ -33,7 +30,10 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [ pytestCheckHook ];
 
-  unittestFlagsArray = [ "-v" ];
+  pytestFlags = [
+    "-v"
+    "--no-device"
+  ];
 
   pythonImportsCheck = [ "fido2" ];
 
