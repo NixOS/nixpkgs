48aeb444ad8f37ed1f303519e37a29aa72cc0063
Adam C. Stephens <adam@valkor.net>
2025-08-06 17:15:06 +0000

    kanidm_1_7: 1.7.0 -> 1.7.1
    https://github.com/kanidm/kanidm/releases/tag/v1.7.1

--- pkgs/by-name/ka/kanidm/1_7.nix
+++ pkgs/by-name/ka/kanidm/1_7.nix
@@ -1,5 +1,5 @@
 import ./generic.nix {
-  version = "1.7.0";
-  hash = "sha256-JM5C0tCvo4Lq06bqil8M35YnFMoQFjzA/J30gZOs4fU=";
-  cargoHash = "sha256-MvBjj/6ph+i29dZGtaysHHgRk03P8sMePXdIm+v2tpw=";
+  version = "1.7.1";
+  hash = "sha256-CG4s6fYxTM2I/kFjD905g8/DSFkyB+0pnGVXgyRXtlE=";
+  cargoHash = "sha256-9bE3hSCFBJF8f3Lm5SzEuDtEpJBbCBijUDfqGiPnRsc=";
 }
