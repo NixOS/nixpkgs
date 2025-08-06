59d6c7b8cacafeea9f025f0c84a6507b176ccb0e
Seth Flynn <getchoo@tuta.io>
2025-08-06 13:17:38 -0400

    maturin: 1.9.1 -> 1.9.3
    Diff: https://github.com/PyO3/maturin/compare/v1.9.1...v1.9.3
    
    Changelog: https://github.com/PyO3/maturin/blob/v1.9.3/Changelog.md

--- pkgs/by-name/ma/maturin/package.nix
+++ pkgs/by-name/ma/maturin/package.nix
@@ -12,16 +12,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "maturin";
-  version = "1.9.1";
+  version = "1.9.3";
 
   src = fetchFromGitHub {
     owner = "PyO3";
     repo = "maturin";
     rev = "v${version}";
-    hash = "sha256-nqi3t1rK2ZTIwFra1avnnhJSMbRI64t5/wP3dCyCY3A=";
+    hash = "sha256-VhL4nKXyONXbxriEHta0vCnWY1j82oDOLoxVigaggSc=";
   };
 
-  cargoHash = "sha256-ZszmCXvPRpt6+cTDU1GD3K81Qw4UVFMDKLOlb1R6KiE=";
+  cargoHash = "sha256-Iom4GoTBFJ9P5UQnYF5JbeQeO2Eh1MwKOwbo+PhgtQM=";
 
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
     libiconv
