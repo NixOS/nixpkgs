093d9ba28cdcde43d760df1d5e8ea581f5e64a08
Connor Baker <ConnorBaker01@gmail.com>
2025-08-05 17:42:42 -0700

    nvidia-container-toolkit: add meta.mainProgram
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- pkgs/by-name/nv/nvidia-container-toolkit/package.nix
+++ pkgs/by-name/nv/nvidia-container-toolkit/package.nix
@@ -91,6 +91,7 @@ buildGoModule (finalAttrs: {
   meta = {
     homepage = "https://gitlab.com/nvidia/container-toolkit/container-toolkit";
     description = "NVIDIA Container Toolkit";
+    mainProgram = "nvidia-ctk";
     license = lib.licenses.asl20;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [
