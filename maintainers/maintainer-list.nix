c2f66a2699ea10d274a0c9ea5a4a6c4790f5a42d
Hendrik Heil <hendrik@stafftastic.com>
2025-08-07 07:34:49 +0000

    maintainers: add hendrikheil

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10032,6 +10032,12 @@
     githubId = 1631166;
     name = "Marcel Müller";
   };
+  hendrikheil = {
+    email = "hendrik@ciidyr.com";
+    github = "hendrikheil";
+    githubId = 19904485;
+    name = "Hendrik Heil";
+  };
   henkery = {
     email = "jim@reupload.nl";
     github = "henkery";
