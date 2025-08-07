945db36cff8316986c4e092f75f271b3c8757d67
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-07 05:51:24 +0200

    fixup! python3Packages.jupyter-lsp: 2.2.5 -> 2.2.6

--- pkgs/development/python-modules/jupyter-lsp/default.nix
+++ pkgs/development/python-modules/jupyter-lsp/default.nix
@@ -12,7 +12,8 @@ buildPythonPackage rec {
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "jupyter_lsp";
+    inherit version;
     hash = "sha256-BWa9m7BP2eZ3SpN+0BUitVW6eL43vr73h8irIt5MA2E=";
   };
 
