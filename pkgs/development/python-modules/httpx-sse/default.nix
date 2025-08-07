02e458919aebd3eeac485909aaa9bc009ec3ca9c
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-07 04:47:41 +0200

    fixup! python3Packages.httpx-sse: 0.4.0 -> 0.4.1

--- pkgs/development/python-modules/httpx-sse/default.nix
+++ pkgs/development/python-modules/httpx-sse/default.nix
@@ -2,10 +2,8 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch2,
   setuptools,
   setuptools-scm,
-  wheel,
   httpx,
   pytest-asyncio,
   pytestCheckHook,
@@ -24,13 +22,6 @@ buildPythonPackage rec {
     hash = "sha256-bSozSZmbRU5sc3jvVUOAXQWVBA8GhzM2R26uPdabS+w=";
   };
 
-  patches = [
-    (fetchpatch2 {
-      url = "https://github.com/florimondmanca/httpx-sse/commit/643938c805e671fa20adcf314b447f862b77bcda.patch?full_index=1";
-      hash = "sha256-V2PyTlleyoLa0DuvdlU8zGNsI9C8bTjMUcLjx81/e5k=";
-    })
-  ];
-
   # pytest-cov configuration is not necessary for packaging
   postPatch = ''
     rm setup.cfg
@@ -39,7 +30,6 @@ buildPythonPackage rec {
   build-system = [
     setuptools
     setuptools-scm
-    wheel
   ];
 
   dependencies = [ httpx ];
