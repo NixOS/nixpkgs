662d7c4cce82368d201807c9a86502fce2a7a4b9
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-07 01:18:54 +0000

    handheld-daemon: 3.17.6 -> 3.18.2

--- pkgs/by-name/ha/handheld-daemon/package.nix
+++ pkgs/by-name/ha/handheld-daemon/package.nix
@@ -16,14 +16,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "handheld-daemon";
-  version = "3.17.6";
+  version = "3.18.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "hhd-dev";
     repo = "hhd";
     tag = "v${version}";
-    hash = "sha256-JDTYqVzkdtMwMyMUyc+MABxzEKVsdRuQce9pMS2JnAE=";
+    hash = "sha256-tU5xDwqmeYFT+2byq2Qo/k29TncPfTQgYZF0JZUe/Iw=";
   };
 
   # Handheld-daemon runs some selinux-related utils which are not in nixpkgs.
