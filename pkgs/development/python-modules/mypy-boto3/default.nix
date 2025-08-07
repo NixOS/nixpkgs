5b99181687d3042c01c79618fa9e23d9c6ef21a6
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-07 17:45:26 +0200

    python312Packages.mypy-boto3-budgets: 1.40.0 -> 1.40.4

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -178,8 +178,8 @@ rec {
       "sha256-YZKJblhTzoW0I/ozKw+RzELF9nJ0+3Z/zjZhb/lEd80=";
 
   mypy-boto3-budgets =
-    buildMypyBoto3Package "budgets" "1.40.0"
-      "sha256-nY9RvXgjyeNsiuVtM+2pWsc0kGuAggZNpkF7SXkHVwk=";
+    buildMypyBoto3Package "budgets" "1.40.4"
+      "sha256-6kCRDur13G+GTZK8R7gknc1J3L/E3YA4/xi+9qQhVp0=";
 
   mypy-boto3-ce =
     buildMypyBoto3Package "ce" "1.40.0"
