7c418dcd9f97c8c938fdb117159f3e35ff0a309f
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-07 18:49:12 +0000

    airwindows: 0-unstable-2025-07-27 -> 0-unstable-2025-08-03

--- pkgs/by-name/ai/airwindows/package.nix
+++ pkgs/by-name/ai/airwindows/package.nix
@@ -8,13 +8,13 @@
 }:
 stdenv.mkDerivation {
   pname = "airwindows";
-  version = "0-unstable-2025-07-27";
+  version = "0-unstable-2025-08-03";
 
   src = fetchFromGitHub {
     owner = "airwindows";
     repo = "airwindows";
-    rev = "52565448b14ea481682b4c67cd6f4732fb6bc644";
-    hash = "sha256-VJhxe7hKjECUsyNfX32yGdEX4hkGD3CaO+QnNnXGBhc=";
+    rev = "f4ae8163f28f5b2ba42f2238ac7131f0c55cada0";
+    hash = "sha256-9PghblWAmaJLdDTOaCduX0W0B64771krzPhZJkFhD4s=";
   };
 
   # we patch helpers because honestly im spooked out by where those variables
