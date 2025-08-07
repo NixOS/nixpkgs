26a71ae0c9173943bfe09a3c14fa56c88088a9c9
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-07 23:33:09 +0000

    python3Packages.trl: 0.19.0 -> 0.21.0

--- pkgs/development/python-modules/trl/default.nix
+++ pkgs/development/python-modules/trl/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "trl";
-  version = "0.19.0";
+  version = "0.21.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "huggingface";
     repo = "trl";
     tag = "v${version}";
-    hash = "sha256-TlTq3tIQfNuI+CPvIy/qPFiKPhoSQd7g7FDj4F7C3CQ=";
+    hash = "sha256-9jbbbiGa/2NqHKe9rxDRyzfaWyy7tsoeHaMlpg0Oxk0=";
   };
 
   build-system = [
