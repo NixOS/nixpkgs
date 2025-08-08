972d1c9898a634dc7fe7aa86a399133c5d81b4c8
ibizaman <ibizaman@tiserbox.com>
2025-08-08 23:59:58 +0200

    secret contract: declare behavior test

--- nixos/modules/contracts/secret.nix
+++ nixos/modules/contracts/secret.nix
@@ -47,5 +47,84 @@ in
         '';
       };
     };
+
+    behaviorTest =
+      {
+        providerRoot,
+        extraModules ? [ ],
+      }:
+      {
+        nodes.machine =
+          { config, ... }:
+          {
+            imports = extraModules;
+
+            options.test = {
+              owner = mkOption {
+                type = str;
+                default = "root";
+              };
+
+              group = mkOption {
+                type = str;
+                default = "root";
+              };
+
+              mode = mkOption {
+                type = str;
+                default = "0400";
+              };
+
+              content = mkOption {
+                type = str;
+                default = "a super secret secret!";
+              };
+            };
+
+            config = lib.mkMerge [
+              (lib.setAttrByPath providerRoot {
+                # We set consumer.input and not input directly because the latter is readOnly.
+                consumer.input = {
+                  inherit (config.test) owner group mode;
+                };
+              })
+              (lib.mkIf (config.test.owner != "root") {
+                users.users.${config.test.owner}.isNormalUser = true;
+              })
+              (lib.mkIf (config.test.group != "root") {
+                users.groups.${config.test.group} = { };
+              })
+            ];
+          };
+
+        testScript =
+          { nodes, ... }:
+          let
+            cfg = nodes.machine;
+            inherit (lib.getAttrFromPath providerRoot nodes.machine) output;
+          in
+          ''
+            owner = machine.succeed("stat -c '%U' ${output.path}").strip()
+            print(f"Got owner {owner}")
+            if owner != "${cfg.test.owner}":
+                raise Exception(f"Owner should be '${cfg.test.owner}' but got '{owner}'")
+
+            group = machine.succeed("stat -c '%G' ${output.path}").strip()
+            print(f"Got group {group}")
+            if group != "${cfg.test.group}":
+                raise Exception(f"Group should be '${cfg.test.group}' but got '{group}'")
+
+            mode = str(int(machine.succeed("stat -c '%a' ${output.path}").strip()))
+            print(f"Got mode {mode}")
+            wantedMode = str(int("${cfg.test.mode}"))
+            if mode != wantedMode:
+                raise Exception(f"Mode should be '{wantedMode}' but got '{mode}'")
+
+            content = machine.succeed("cat ${output.path}").strip()
+            print(f"Got content {content}")
+            if content != "${cfg.test.content}":
+                raise Exception(f"Content should be '${cfg.test.content}' but got '{content}'")
+          '';
+      };
   };
 }
