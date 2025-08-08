a52ce18ff0487d5289f46dbba80378be0738e43c
ibizaman <ibizaman@tiserbox.com>
2025-08-09 01:10:03 +0200

    nextcloud: use file backup contract

--- nixos/modules/services/web-apps/nextcloud.nix
+++ nixos/modules/services/web-apps/nextcloud.nix
@@ -1019,6 +1019,10 @@ in
         The value can be customized for `nextcloud-cron.service` using this option.
       '';
     };
+
+    fileBackup = lib.mkOption {
+      type = config.contracts.fileBackup.consumer;
+    };
   };
 
   config = mkIf cfg.enable (mkMerge [
@@ -1555,6 +1559,18 @@ in
             rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json last;
           ''}
         '';
+      };
+    }
+
+    {
+      services.nextcloud.fileBackup.input = {
+        user = "nextcloud";
+        sourceDirectories = [
+          cfg.datadir
+        ];
+        excludePatterns = [
+          ".rnd"
+        ];
       };
     }
   ]);
