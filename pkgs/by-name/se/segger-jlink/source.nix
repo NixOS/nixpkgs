412d9bb78f3d274a39d89ccb7624f82dea9519ef
Sirio Balmelli <sirio@b-ad.ch>
2025-08-08 08:56:11 +0200

    segger-jlink: refactor source.nix and update.py
    Adds parameters necessary to add Darwin sources in a later commit.
    
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- pkgs/by-name/se/segger-jlink/source.nix
+++ pkgs/by-name/se/segger-jlink/source.nix
@@ -1,19 +1,27 @@
 {
   version = "810";
   x86_64-linux = {
+    os = "Linux";
     name = "x86_64";
+    ext = "tgz";
     hash = "sha256-cnax7D4T5On356mZrd/waH62IXP6BNSJtCM0rw0RnDo=";
   };
   i686-linux = {
+    os = "Linux";
     name = "i386";
+    ext = "tgz";
     hash = "sha256-ERpzSVK7IxriDTH1wkLsiioqV99fRkMIlg0Pd+V+h7g=";
   };
   aarch64-linux = {
+    os = "Linux";
     name = "arm64";
+    ext = "tgz";
     hash = "sha256-l+mxhK85GnGLKspXeEy2JOJ+fA8DVIr36tLVCBp7MhU=";
   };
   armv7l-linux = {
+    os = "Linux";
     name = "arm";
+    ext = "tgz";
     hash = "sha256-DFIaIq57HQbpur2vep1pO/VDOo913b17x3eWhq4wL40=";
   };
 }
