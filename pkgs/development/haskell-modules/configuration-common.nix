54487675162345ab2e9f7715fed5e802733c0930
mangoiv <contact@mangoiv.com>
2025-08-08 14:05:32 +0200

    haskellPackages.selective: allow new QuickCheck

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -400,6 +400,10 @@ with haskellLib;
   # https://github.com/fused-effects/fused-effects/pull/466
   fused-effects = doJailbreak super.fused-effects;
 
+  # 2025-08-08: Allow QuickCheck > 2.16 in selective's test-suite
+  # https://github.com/snowleopard/selective/pull/81
+  selective = doJailbreak super.selective;
+
   # 2024-06-23: Hourglass is archived and had its last commit 6 years ago.
   # Patch is needed to add support for time 1.10, which is only used in the tests
   # https://github.com/vincenthz/hs-hourglass/pull/56
