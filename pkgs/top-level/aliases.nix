ecba8f422b9139a20cde3271c105e21214ec7c52
Emily <vcs@emily.moe>
2025-08-08 18:22:54 +0100

    collada2gltf: drop
    Vendors outdated dependencies that don’t build with supported
    compilers. I tried my best to devendor, but I was unable to get it
    building, and upstream has not been active in half a decade.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -626,6 +626,7 @@ mapAliases {
   code-browser-gtk2 = throw "'code-browser-gtk2' has been removed, as it was broken since 22.11"; # Added 2025-08-22
   code-browser-qt = throw "'code-browser-qt' has been removed, as it was broken since 22.11"; # Added 2025-08-22
   collada-dom = opencollada; # added 2024-02-21
+  collada2gltf = throw "collada2gltf has been removed from Nixpkgs, as it has been unmaintained upstream for 5 years and does not build with supported GCC versions"; # Addd 2025-08-08
   colloid-kde = throw "'colloid-kde' has been removed, as it is only compatible with Plasma 5, which is EOL"; # Added 2025-08-20
   colorpicker = throw "'colorpicker' has been removed due to lack of maintenance upstream. Consider using 'xcolor', 'gcolor3', 'eyedropper' or 'gpick' instead"; # Added 2024-10-19
   colorstorm = throw "'colorstorm' has been removed because it was unmaintained in nixpkgs and upstream was rewritten."; # Added 2025-06-15
