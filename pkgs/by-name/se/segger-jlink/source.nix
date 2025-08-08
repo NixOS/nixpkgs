0d481a52058f8b824cce822b24ea8cbac385f6d9
Sirio Balmelli <sirio@b-ad.ch>
2025-08-08 08:56:12 +0200

    segger-jlink: 810 -> 824
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- pkgs/by-name/se/segger-jlink/source.nix
+++ pkgs/by-name/se/segger-jlink/source.nix
@@ -1,27 +1,27 @@
 {
-  version = "810";
+  version = "824";
   x86_64-linux = {
     os = "Linux";
     name = "x86_64";
     ext = "tgz";
-    hash = "sha256-cnax7D4T5On356mZrd/waH62IXP6BNSJtCM0rw0RnDo=";
+    hash = "sha256-TsNlwApXdbkHEI+pG9NVK7pJkKdvFYGIGgn+9xJ9q8A=";
   };
   i686-linux = {
     os = "Linux";
     name = "i386";
     ext = "tgz";
-    hash = "sha256-ERpzSVK7IxriDTH1wkLsiioqV99fRkMIlg0Pd+V+h7g=";
+    hash = "sha256-Gs4iifvchFY0d39bhQ61lX/TPGjCKPRidkJQbva91gA=";
   };
   aarch64-linux = {
     os = "Linux";
     name = "arm64";
     ext = "tgz";
-    hash = "sha256-l+mxhK85GnGLKspXeEy2JOJ+fA8DVIr36tLVCBp7MhU=";
+    hash = "sha256-YowEzJ//idmZ4eznPNgEW142ApsnvkboGAWfDCHfIS0=";
   };
   armv7l-linux = {
     os = "Linux";
     name = "arm";
     ext = "tgz";
-    hash = "sha256-DFIaIq57HQbpur2vep1pO/VDOo913b17x3eWhq4wL40=";
+    hash = "sha256-mLl/qNtxMfFOe/M0fCZnnebei7E2ON4gvE9Q7XIufag=";
   };
 }
