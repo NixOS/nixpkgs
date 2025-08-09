66ecae02c95f5ca3a697d56198614e042a799d98
Sean-on-Git <seanr707@tutanota.com>
2025-08-09 19:11:52 -0400

    Update tutanota-desktop to 301.250806.1
    Update Tutanota desktop AppImage to latest version

--- pkgs/by-name/tu/tutanota-desktop/package.nix
+++ pkgs/by-name/tu/tutanota-desktop/package.nix
@@ -7,11 +7,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "tutanota-desktop";
-  version = "277.250414.1";
+  version = "301.250806.1";
 
   src = fetchurl {
     url = "https://github.com/tutao/tutanota/releases/download/tutanota-desktop-release-${version}/tutanota-desktop-linux.AppImage";
-    hash = "sha256-Ke9c9kugI+Cym24Nf4juW1oFiCCTNURZy7C1+TFF0rc=";
+    sha256 = "1ab8rnv83vpjvj9a12a3m435f7mjgahmbb0qcdcy4bmgqgaz0wdy";
   };
 
   extraPkgs = pkgs: [ pkgs.libsecret ];
