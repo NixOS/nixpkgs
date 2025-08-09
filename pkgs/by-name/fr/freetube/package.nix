18ec1750c7d02fe584b658db91f3a1057dab8f2c
Jost Alemann <alemann@posteo.com>
2025-08-09 10:02:04 +0200

    freetube: 0.23.5 -> 0.23.6
    Changelog:
    https://github.com/FreeTubeApp/FreeTube/releases/tag/v0.23.6-beta
    Diff:
    https://github.com/FreeTubeApp/FreeTube/compare/v0.23.5-beta...v0.23.6-beta
    
    (cherry picked from commit 01f6508918a7af7583e4da274dc429b5ea831665)

--- pkgs/by-name/fr/freetube/package.nix
+++ pkgs/by-name/fr/freetube/package.nix
@@ -20,13 +20,13 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "freetube";
-  version = "0.23.5";
+  version = "0.23.6";
 
   src = fetchFromGitHub {
     owner = "FreeTubeApp";
     repo = "FreeTube";
     tag = "v${finalAttrs.version}-beta";
-    hash = "sha256-tVe//h20cTVgpHeo3IlfGfuAH+dM6H5MEfGny5Uhrjk=";
+    hash = "sha256-Z1L45RHlmylfqKBY37PC5TQ3ubOgH0AHFGM7VkmtkZ0=";
   };
 
   # Darwin requires writable Electron dist
@@ -49,7 +49,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   yarnOfflineCache = fetchYarnDeps {
     yarnLock = "${finalAttrs.src}/yarn.lock";
-    hash = "sha256-F1YcdshWGRCO7kHsCK0Ejs0aMR7BFXd5M9ITdRVcpbk=";
+    hash = "sha256-ia5wLRt3Hmo4/dsB1/rhGWGJ7LMnVR9ju9lSlQZDTTg=";
   };
 
   nativeBuildInputs = [
