77664c146ca7b4d9ab77697a8a0c39d42a499df2
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-08-09 12:02:43 +0700

    ocamlPackages.logs: 0.8.0 → 0.9.0

--- pkgs/development/ocaml-modules/logs/default.nix
+++ pkgs/development/ocaml-modules/logs/default.nix
@@ -3,6 +3,7 @@
   stdenv,
   fetchurl,
   ocaml,
+  version ? if lib.versionAtLeast ocaml.version "4.14" then "0.9.0" else "0.8.0",
   topkg,
   buildTopkgPackage,
   cmdlinerSupport ? true,
@@ -15,6 +16,19 @@
   lwt,
 }:
 let
+  param =
+    {
+      "0.8.0" = {
+        minimalOCamlVersion = "4.03";
+        sha512 = "c34c67b00d6a989a2660204ea70db8521736d6105f15d1ee0ec6287a662798fe5c4d47075c6e7c84f5d5372adb5af5c4c404f79db70d69140af5e0ebbea3b6a5";
+      };
+      "0.9.0" = {
+        minimalOCamlVersion = "4.14";
+        sha512 = "b75fb28e83f33461b06b5c9b60972c4a9a9a1599d637b4a0c7b1e86a87f34fe5361e817cb31f42ad7e7cbb822473b28fab9f58a02870eb189ebe88dae8e045ff";
+      };
+    }
+    .${version};
+
   pname = "logs";
   webpage = "https://erratique.ch/software/${pname}";
 
@@ -46,16 +60,13 @@ let
   ]) optional_deps;
   optional_buildInputs = map (d: d.pkg) (lib.filter (d: d.enabled) optional_deps);
 in
-
-buildTopkgPackage rec {
-  inherit pname;
-  version = "0.8.0";
-
-  minimalOCamlVersion = "4.03";
+buildTopkgPackage {
+  inherit pname version;
+  inherit (param) minimalOCamlVersion;
 
   src = fetchurl {
     url = "${webpage}/releases/${pname}-${version}.tbz";
-    hash = "sha256-mmFRQJX6QvMBIzJiO2yNYF1Ce+qQS2oNF3+OwziCNtg=";
+    inherit (param) sha512;
   };
 
   buildInputs = [ topkg ] ++ optional_buildInputs;
