70988ac6589ce3e5087e9847d3b3f5df9dcaef69
misilelab <misileminecord@gmail.com>
2025-08-09 14:13:02 +0900

    snyk: 1.1298.1 -> 1.1298.2
    https://github.com/snyk/cli/releases/tag/v1.1298.2
    
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- pkgs/by-name/sn/snyk/package.nix
+++ pkgs/by-name/sn/snyk/package.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  version = "1.1298.1";
+  version = "1.1298.2";
 in
 buildNpmPackage {
   pname = "snyk";
@@ -18,7 +18,7 @@ buildNpmPackage {
     owner = "snyk";
     repo = "cli";
     tag = "v${version}";
-    hash = "sha256-oLkzEm7OMBNqT+EDrwujqQek4LWwKgYFUoMRWhpqY4o=";
+    hash = "sha256-8VnbXxvz5mWWMq6sjffshMbHBf2H6s/xmPbQZsZC/4A=";
   };
 
   npmDepsHash = "sha256-7fHehEKjNNRdRk9+kARzn75G0r1pse7ULn/Oz6mQRKM=";
