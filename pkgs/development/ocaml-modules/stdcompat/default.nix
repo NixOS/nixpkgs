442dfb9fbd9c866097125e1f1f9a8d7a853fdb65
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-08-09 13:04:31 -0400

    ocamlPackages: 19 -> 21.0
    Supports ocaml 5.3 now.
    
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/development/ocaml-modules/stdcompat/default.nix
+++ pkgs/development/ocaml-modules/stdcompat/default.nix
@@ -2,30 +2,28 @@
   buildDunePackage,
   ocaml,
   lib,
-  fetchurl,
+  fetchFromGitHub,
 }:
 
-lib.throwIf (lib.versionAtLeast ocaml.version "5.2")
-  "stdcompat is not available for OCaml ${ocaml.version}"
+buildDunePackage rec {
+  pname = "stdcompat";
+  version = "21.0";
 
-  buildDunePackage
-  rec {
-    pname = "stdcompat";
-    version = "19";
+  minimalOCamlVersion = "4.06";
 
-    minimalOCamlVersion = "4.06";
+  src = fetchFromGitHub {
+    owner = "ocamllibs";
+    repo = "stdcompat";
+    tag = version;
+    hash = "sha256-Ks8m2QicIEohSADiMeijCz0WTPsTSgPifrGTn7FVcV0=";
+  };
 
-    src = fetchurl {
-      url = "https://github.com/thierry-martinez/stdcompat/releases/download/v${version}/stdcompat-${version}.tar.gz";
-      sha256 = "sha256-DKQGd4nnIN6SPls6hcA/2Jvc7ivYNpeMU6rYsVc1ClU=";
-    };
+  # Otherwise ./configure script will run and create files conflicting with dune.
+  dontConfigure = true;
 
-    # Otherwise ./configure script will run and create files conflicting with dune.
-    dontConfigure = true;
-
-    meta = {
-      homepage = "https://github.com/thierry-martinez/stdcompat";
-      license = lib.licenses.bsd2;
-      maintainers = [ lib.maintainers.vbgl ];
-    };
-  }
+  meta = {
+    homepage = "https://github.com/thierry-martinez/stdcompat";
+    license = lib.licenses.bsd2;
+    maintainers = [ lib.maintainers.vbgl ];
+  };
+}
