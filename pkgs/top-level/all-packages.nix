5e000f2c8fa3de57c12ea4f1baa7379eec64e86b
Emily <vcs@emily.moe>
2025-08-09 22:48:54 +0100

    creduce: 2.10.0 -> 2.10.0-unstable-2024-06-01
    We were already applying a ton of commits on top of the latest stable
    release, including fixes for newer LLVM versions. Let’s bump to
    the latest commit and pick up support for LLVM 18.
    
    There are in‐flight PRs for LLVM 19 and 20:
    
    * <https://github.com/csmith-project/creduce/pull/285>
    * <https://github.com/csmith-project/creduce/pull/287>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7011,7 +7011,7 @@ with pkgs;
   credstash = with python3Packages; toPythonApplication credstash;
 
   creduce = callPackage ../development/tools/misc/creduce {
-    inherit (llvmPackages_16) llvm libclang;
+    inherit (llvmPackages_18) llvm libclang;
   };
 
   inherit (nodePackages) csslint;
