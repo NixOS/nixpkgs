5c2db3c15676753a03b6ab4631fd74bc147bd61e
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-09 15:18:05 +0000

    filebeat8: 8.18.4 -> 8.19.1

--- pkgs/by-name/fi/filebeat8/package.nix
+++ pkgs/by-name/fi/filebeat8/package.nix
@@ -8,18 +8,18 @@
 
 buildGoModule rec {
   pname = "filebeat";
-  version = "8.18.4";
+  version = "8.19.1";
 
   src = fetchFromGitHub {
     owner = "elastic";
     repo = "beats";
     tag = "v${version}";
-    hash = "sha256-H7UKYp+REz7d9wKrP+AhIJp4ydCVS8NGKfBFvDFZWiA=";
+    hash = "sha256-T1cAfBa7ALf+JXxbs7k9c4oy1KMnAXWh5Zmd1LE8JMc=";
   };
 
   proxyVendor = true; # darwin/linux hash mismatch
 
-  vendorHash = "sha256-G4+FsmmPDyssD+n1N1BnCElYv/bW7kY2tF60r49ZhN8=";
+  vendorHash = "sha256-y8oAeH6RBD45+NEhl9EF4tPL1Ox4qHlLi+jSetjgKRE=";
 
   subPackages = [ "filebeat" ];
 
