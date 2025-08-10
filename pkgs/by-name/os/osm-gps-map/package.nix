9ae1b28889d015c492172f10cc51af45630f3218
Christina Sørensen <ces@fem.gg>
2025-08-10 20:27:59 +0200

    osm-gps-map: 1.2.0 -> unstable-2025-04-17
    Signed-off-by: Christina Sørensen <ces@fem.gg>

--- pkgs/by-name/os/osm-gps-map/package.nix
+++ pkgs/by-name/os/osm-gps-map/package.nix
@@ -1,23 +1,29 @@
 {
   cairo,
-  fetchzip,
+  fetchFromGitHub,
   glib,
-  libsoup_2_4,
+  libsoup_3,
   gnome-common,
   gtk3,
   gobject-introspection,
   pkg-config,
   lib,
   stdenv,
+  autoconf,
+  automake,
+  gtk-doc,
+  libtool,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "osm-gps-map";
-  version = "1.2.0";
+  version = "unstable-2025-04-17";
 
-  src = fetchzip {
-    url = "https://github.com/nzjrs/osm-gps-map/releases/download/${finalAttrs.version}/osm-gps-map-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-ciw28YXhR+GC6B2VPC+ZxjyhadOk3zYGuOssSgqjwH0=";
+  src = fetchFromGitHub {
+    owner = "nzjrs";
+    repo = "osm-gps-map";
+    rev = "2396aa6cb2da877847049e7f4243a7d196f9862e";
+    hash = "sha256-HAOzjPBoC/zpgIo09GYVlR98GNKqWkJ6117Ttqlr/bw=";
   };
 
   patches = [
@@ -35,6 +41,8 @@ stdenv.mkDerivation (finalAttrs: {
 
   nativeBuildInputs = [
     pkg-config
+    autoconf
+    automake
     gobject-introspection
     gnome-common
   ];
@@ -42,8 +50,12 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     cairo
     glib
-    libsoup_2_4
+    libsoup_3
+    gtk-doc
+    libtool
   ];
+
+  preConfigure = "./autogen.sh";
 
   propagatedBuildInputs = [
     gtk3
@@ -53,7 +65,10 @@ stdenv.mkDerivation (finalAttrs: {
     description = "GTK widget for displaying OpenStreetMap tiles";
     homepage = "https://nzjrs.github.io/osm-gps-map";
     license = lib.licenses.gpl2Plus;
-    maintainers = with lib.maintainers; [ hrdinka ];
+    maintainers = with lib.maintainers; [
+      hrdinka
+      cafkafk
+    ];
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
   };
 })
