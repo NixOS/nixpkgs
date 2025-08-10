7bc7258f0eeb9b74b24aae27eb5610bab794a38d
Marc Scholten <marc@digitallyinduced.com>
2025-08-10 13:57:02 +0200

    haskell.packages.ghc.910.ghc-lib{,-parser,-parser-ex}: 9.10.2.20250515 -> 9.12.2.20250421
    this fixes the build for ghc-syntax-highlighter, which fixes the build for mmark-ext

--- pkgs/development/haskell-modules/configuration-ghc-9.10.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.10.x.nix
@@ -76,10 +76,6 @@ in
   # Upgrade to accommodate new core library versions, where the authors have
   # already made the relevant changes.
   fourmolu = doDistribute self.fourmolu_0_16_0_0;
-  # https://github.com/digital-asset/ghc-lib/issues/600
-  ghc-lib = doDistribute self.ghc-lib_9_10_2_20250515;
-  ghc-lib-parser = doDistribute self.ghc-lib-parser_9_10_2_20250515;
-  ghc-lib-parser-ex = doDistribute self.ghc-lib-parser-ex_9_10_0_0;
   ormolu = doDistribute self.ormolu_0_7_7_0;
   stylish-haskell = doDistribute self.stylish-haskell_0_15_0_1;
 
