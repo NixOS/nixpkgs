af31e6c3118cba37a7a1811740d41883545c28a5
Rasheeq Azad <rasheeqhere@gmail.com>
2025-08-10 14:01:32 -0400

    maintainers: add howtonotwin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10365,6 +10365,12 @@
     githubId = 130903;
     name = "Ana Hobden";
   };
+  howtonotwin = {
+    email = "rasheeqhere@gmail.com";
+    github = "howtonotwin";
+    githubId = 15616055;
+    name = "Rasheeq Azad";
+  };
   hpfr = {
     email = "liam@hpfr.net";
     github = "hpfr";
