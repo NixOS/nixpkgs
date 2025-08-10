df9adc3ddd58c6375c18454eb798b04c2a1211ec
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-10 21:15:21 +0200

    python313Packages.identify: 2.6.12 -> 2.6.13
    Diff: https://github.com/pre-commit/identify/compare/refs/tags/v2.6.12...refs/tags/v2.6.13

--- pkgs/development/python-modules/identify/default.nix
+++ pkgs/development/python-modules/identify/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "identify";
-  version = "2.6.12";
+  version = "2.6.13";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "pre-commit";
     repo = "identify";
     tag = "v${version}";
-    hash = "sha256-zV9NRHFh/bfbg+pO0xX5aXunc1y4aGfKDugyCFLj/xA=";
+    hash = "sha256-Du96uRQCymduvAruHSgjKv/OrDIPaMwPU+x/OTchERQ=";
   };
 
   build-system = [ setuptools ];
