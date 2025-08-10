12022d379b0e12deac8a04788958d73266ca8942
Atemu <git@atemu.net>
2025-08-10 14:39:25 +0200

    linux-kernels: add LTS flag
    This serves to differentiate between a kernel that is an LTS release and one
    that is merely a regular "stable" release.

--- pkgs/os-specific/linux/kernel/generic.nix
+++ pkgs/os-specific/linux/kernel/generic.nix
@@ -72,6 +72,7 @@ let
       extraMeta ? { },
       extraPassthru ? { },
 
+      isLTS ? false,
       isZen ? false,
       isLibre ? false,
       isHardened ? false,
@@ -304,6 +305,7 @@ let
               commonStructuredConfig
               structuredExtraConfig
               extraMakeFlags
+              isLTS
               isZen
               isHardened
               isLibre
