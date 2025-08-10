977f44c84a781124b888ce1d24a17fb8395aabd5
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-10 07:54:08 +0000

    babashka-unwrapped: 1.12.206 -> 1.12.207

--- pkgs/development/interpreters/babashka/default.nix
+++ pkgs/development/interpreters/babashka/default.nix
@@ -8,11 +8,11 @@
 
 buildGraalvmNativeImage (finalAttrs: {
   pname = "babashka-unwrapped";
-  version = "1.12.206";
+  version = "1.12.207";
 
   src = fetchurl {
     url = "https://github.com/babashka/babashka/releases/download/v${finalAttrs.version}/babashka-${finalAttrs.version}-standalone.jar";
-    sha256 = "sha256-2JrfFddG5ZSJkDXjaNnAyYfvuwR3muDgEVyG82KRFkU=";
+    sha256 = "sha256-bFZZkST5rMWyRTEsviMS2KnKfJXGUBI2UefZ3XY1V0I=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
