4d24fcc604b90c1a941f10fe80e8fe314951f54c
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-10 14:35:34 +0000

    edencommon: 2025.04.21.00 -> 2025.08.04.00

--- pkgs/by-name/ed/edencommon/package.nix
+++ pkgs/by-name/ed/edencommon/package.nix
@@ -21,7 +21,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "edencommon";
-  version = "2025.04.21.00";
+  version = "2025.08.04.00";
 
   outputs = [
     "out"
@@ -32,7 +32,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "facebookexperimental";
     repo = "edencommon";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-WlLQb4O4rGhXp+bQrJA12CvrwcIS6vzO4W6bX04vKMM=";
+    hash = "sha256-pzYYb7OtuIHcGyKZatX1ObGAnCqFUdtaZ5U2c3/loiQ=";
   };
 
   patches = [
