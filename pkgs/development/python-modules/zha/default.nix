23e5a8509a73ffaf7ff9141bb237f62f2793f086
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-11 21:55:31 +0200

    python3Packages.zha: 0.0.66 -> 0.0.68
    https://github.com/zigpy/zha/releases/tag/0.0.68

--- pkgs/development/python-modules/zha/default.nix
+++ pkgs/development/python-modules/zha/default.nix
@@ -27,7 +27,7 @@
 
 buildPythonPackage rec {
   pname = "zha";
-  version = "0.0.66";
+  version = "0.0.68";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -36,7 +36,7 @@ buildPythonPackage rec {
     owner = "zigpy";
     repo = "zha";
     tag = version;
-    hash = "sha256-74rJ2bdscbGkaLuHMQr8sNxL1wNN72UrlpH4zIkSprY=";
+    hash = "sha256-NgUUzNfC1XngU5RBfbKpt/o8hu7XRXOUaDUb7OjSJJc=";
   };
 
   postPatch = ''
