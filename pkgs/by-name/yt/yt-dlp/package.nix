30cf9cb9aaa87ff2ca3443dabcca03d0fe572c97
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-11 08:51:23 +0000

    yt-dlp-light: 2025.07.21 -> 2025.08.11

--- pkgs/by-name/yt/yt-dlp/package.nix
+++ pkgs/by-name/yt/yt-dlp/package.nix
@@ -19,14 +19,14 @@ python3Packages.buildPythonApplication rec {
   # The websites yt-dlp deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2025.07.21";
+  version = "2025.08.11";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "yt-dlp";
     repo = "yt-dlp";
     tag = version;
-    hash = "sha256-VNUkCdrzbOwD+iD9BZUQFJlWXRc0tWJAvLnVKNZNPhQ=";
+    hash = "sha256-j7x844MPPFdXYTJiiMnru3CE79A/6JdfJDdh8it9KsU=";
   };
 
   build-system = with python3Packages; [ hatchling ];
