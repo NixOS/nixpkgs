8fdcd28f9d7d361c8b0798540fb7627773c2b2b5
andre4ik3 <andre4ik3@fastmail.com>
2025-08-11 04:44:22 +0000

    nixos/systemd/userdbd: add warning for system users >= UID 1000
    When a disposition is not set in a user record, systemd determines user
    disposition depending on the range the user's UID falls in. For system
    users with UIDs above 1000, this will cause them to be incorrectly
    identified as "regular" users.
    
    This will cause `userctl` to report the user as a regular user, and more
    importantly, `systemd-homed` will not run the first boot user creation
    flow, as regular users are already present on the machine (when they are
    really system users).
    
    The most common source of high UID system users will undoubtedly be Nix
    build users, so the warning provides additional guidance on how to
    remove them or adjust their IDs to be within the system range.
    
    The warning is shown only when userdbd/homed is enabled, and the option
    to hide the warning is deliberately hidden, to ensure users will have to
    read and acknowledge the warning before proceeding, as otherwise users
    could end up deploying an OS with no users and no way of creating one
    due to the first boot flow being skipped.

--- nixos/modules/system/boot/systemd/userdbd.nix
+++ nixos/modules/system/boot/systemd/userdbd.nix
@@ -2,6 +2,12 @@
 
 let
   cfg = config.services.userdbd;
+
+  # List of system users that will be incorrectly treated as regular/normal
+  # users by userdb.
+  highSystemUsers = lib.filter (
+    user: user.enable && user.isSystemUser && (lib.defaultTo 0 user.uid) >= 1000 && user.uid != 65534
+  ) (lib.attrValues config.users.users);
 in
 {
   options.services.userdbd = {
@@ -14,6 +20,14 @@ in
       enables modifying public keys at runtime, either by users managed by
       {option}`services.homed`, or globally via drop-in files
     '';
+
+    silenceHighSystemUsers = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      example = true;
+      description = "Silence warning about system users with high UIDs.";
+      visible = false;
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -21,6 +35,28 @@ in
       assertion = cfg.enableSSHSupport -> config.security.enableWrappers;
       message = "OpenSSH userdb integration requires security wrappers.";
     };
+
+    warnings = lib.optional (lib.length highSystemUsers > 0 && !cfg.silenceHighSystemUsers) ''
+      The following system users have UIDs higher than 1000:
+
+      ${lib.concatLines (lib.map (user: user.name) highSystemUsers)}
+
+      These users will be recognized by systemd-userdb as "regular" users, not
+      "system" users. This will affect programs that query regular users, such
+      as systemd-homed, which will not run the first boot user creation flow,
+      as regular users already exist.
+
+      To fix this issue, please remove or redefine these system users to have
+      UIDs below 1000. For Nix build users, it's possible to adjust the base
+      build user ID using the `ids.uids.nixbld` option, however care must be
+      taken to avoid collisions with UIDs of other services. Alternatively, you
+      may enable the `auto-allocate-uids` experimental feature and option in
+      the Nix configuration to avoid creating these users, however please note
+      that this option is experimental and subject to change.
+
+      Alternatively, to acknowledge and silence this warning, set
+      `services.userdbd.silenceHighSystemUsers` to true.
+    '';
 
     systemd.additionalUpstreamSystemUnits = [
       "systemd-userdbd.socket"
