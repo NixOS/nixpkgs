ef795d583cd6845a0e39e3f0afa0e2ae70f3b643
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-11 22:58:50 +0000

    garnet: 1.0.80 -> 1.0.81
    (cherry picked from commit 8124c57f91542996e55770437bd920ef9d693c38)

--- pkgs/by-name/ga/garnet/package.nix
+++ pkgs/by-name/ga/garnet/package.nix
@@ -8,13 +8,13 @@
 
 buildDotnetModule rec {
   pname = "garnet";
-  version = "1.0.80";
+  version = "1.0.81";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "garnet";
     tag = "v${version}";
-    hash = "sha256-9B2Ai+W6+rZ8xLrrO7d8jd6LYWaMGIq3a+lz8rY32uA=";
+    hash = "sha256-CEpxV6BoTfkC3Lka1Xuci3uyUYoWxoyYKTQTco5NVY4=";
   };
 
   projectFile = "main/GarnetServer/GarnetServer.csproj";
