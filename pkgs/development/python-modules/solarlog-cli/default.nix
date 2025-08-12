0b155b49d5ffb1812b43987ffeac00ce21513328
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-12 11:41:36 +0000

    python3Packages.solarlog-cli: 0.4.0 -> 0.5.0

--- pkgs/development/python-modules/solarlog-cli/default.nix
+++ pkgs/development/python-modules/solarlog-cli/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "solarlog-cli";
-  version = "0.4.0";
+  version = "0.5.0";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "dontinelli";
     repo = "solarlog_cli";
     tag = "v${version}";
-    hash = "sha256-G7DZHFg0NH3rY/tbWpXDchYHkrvG8dUvINNxBIUKnzk=";
+    hash = "sha256-MMn03i4Uu0auEzUiQHWCNfJyp32P+68pd4fDMy2GqnA=";
   };
 
   build-system = [ hatchling ];
@@ -43,7 +43,7 @@ buildPythonPackage rec {
   ];
 
   meta = {
-    changelog = "https://github.com/dontinelli/solarlog_cli/releases/tag/v${version}";
+    changelog = "https://github.com/dontinelli/solarlog_cli/releases/tag/${src.tag}";
     description = "Python library to access the Solar-Log JSON interface";
     homepage = "https://github.com/dontinelli/solarlog_cli";
     license = lib.licenses.gpl3Plus;
