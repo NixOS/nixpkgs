c4e82d17920db3b299fe98a0e0bd556571214b9c
nat <nat@nekopon.pl>
2025-08-12 19:28:17 +0200

    lucida-downloader: 0.2.0 -> 0.3.0

--- pkgs/by-name/lu/lucida-downloader/package.nix
+++ pkgs/by-name/lu/lucida-downloader/package.nix
@@ -7,18 +7,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "lucida-downloader";
-  version = "0.2.0";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "jelni";
     repo = "lucida-downloader";
     tag = "v${version}";
-    hash = "sha256-9wXnxsgZZprUez3PggBWbTU/Vx7JFkNC7fuOiqWG87Y=";
+    hash = "sha256-IO9X9JlLfc2UeOTnWQE73OcgWdYl03W8PuILOBFdXIg=";
   };
 
   passthru.updateScript = nix-update-script { };
 
-  cargoHash = "sha256-OfnCKFWUxpFu6NU4MNMCimXAbhspBf1n6Qz5ff7MHI4=";
+  cargoHash = "sha256-Vy9kmkzwcX5VwZuDnnFyDkaGbadVR2RTs6faliq0WZ0=";
 
   meta = {
     description = "Multithreaded client for downloading music for free with lucida";
