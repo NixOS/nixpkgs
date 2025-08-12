e370730f131a975394d0195c3fc63db5f0383f4a
Jamie Magee <jamie.magee@gmail.com>
2025-08-11 22:25:29 -0700

    python3Packages.pynetio: init at 0.1.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13382,6 +13382,8 @@ self: super: with self; {
 
   pynetgear = callPackage ../development/python-modules/pynetgear { };
 
+  pynetio = callPackage ../development/python-modules/pynetio { };
+
   pynfsclient = callPackage ../development/python-modules/pynfsclient { };
 
   pyngo = callPackage ../development/python-modules/pyngo { };
