ea46305f38ec6e3118b4a1371788db4c2e5f3f9e
dabao1955 <dabao1955@163.com>
2025-08-12 23:26:21 +0800

    ruri: 3.8 -> 3.9.1

--- pkgs/by-name/ru/ruri/package.nix
+++ pkgs/by-name/ru/ruri/package.nix
@@ -4,30 +4,33 @@
   fetchFromGitHub,
   libcap,
   libseccomp,
+  cmake,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ruri";
-  version = "3.8";
+  version = "3.9.1";
 
   src = fetchFromGitHub {
-    owner = "Moe-hacker";
+    owner = "RuriOSS";
     repo = "ruri";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = false;
-    sha256 = "sha256-gf+WJPGeLbMntBk8ryTSsV9L4J3N4Goh9eWBIBj5FA4=";
+    sha256 = "sha256-stM4hSLdSqmYUZ/XBD3Y1GylrrGRISlcy8LN07HREpQ=";
   };
+
+  patches = [
+    # See https://github.com/NixOS/nixpkgs/pull/433118#issuecomment-3194094492
+    # cmake installed a wrong path
+    ./fix-install.patch
+  ];
 
   buildInputs = [
     libcap
     libseccomp
   ];
 
-  installPhase = ''
-    runHook preInstall
-    install -Dm755 ruri $out/bin/ruri
-    runHook postInstall
-  '';
+  nativeBuildInputs = [ cmake ];
 
   meta = {
     description = "Self-contained Linux container implementation";
