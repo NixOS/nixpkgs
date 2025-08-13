7525696a119359e6bb34e5603f9af4f1fbeaff6a
Andrew Pan <a@tny.town>
2025-08-12 20:41:55 -0400

    python3Packages.trianglesolver: init at 1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19380,6 +19380,8 @@ self: super: with self; {
     pinentry = pkgs.pinentry-curses;
   };
 
+  trianglesolver = callPackage ../development/python-modules/trianglesolver { };
+
   trie = callPackage ../development/python-modules/trie { };
 
   triggercmd = callPackage ../development/python-modules/triggercmd { };
