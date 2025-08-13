3bead502da9976852a76ccaa2e3dcd4c0fad7af2
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-13 21:57:57 +0000

    vscode-extensions.ms-toolsai.jupyter: 2025.6.0 -> 2025.7.0

--- pkgs/applications/editors/vscode/extensions/ms-toolsai.jupyter/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-toolsai.jupyter/default.nix
@@ -9,8 +9,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "jupyter";
     publisher = "ms-toolsai";
-    version = "2025.6.0";
-    hash = "sha256-pJh+Iqgi6feFUPZ4/z7Ke6Hv76fmm6JvgU/e4iLvgmk=";
+    version = "2025.7.0";
+    hash = "sha256-wedMPo+mL3yvb9WqJComlyZWvSSaJXv/4LWcl0wwqdQ=";
   };
 
   nativeBuildInputs = [
