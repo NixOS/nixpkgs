96eb5b2ae6549ec0938f992c7bcdd805f43591c1
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-13 12:23:24 +0200

    inframap: add changelog to meta

--- pkgs/by-name/in/inframap/package.nix
+++ pkgs/by-name/in/inframap/package.nix
@@ -3,6 +3,7 @@
   fetchFromGitHub,
   lib,
 }:
+
 buildGoModule rec {
   pname = "inframap";
   version = "0.8.0";
@@ -10,14 +11,14 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "cycloidio";
     repo = "inframap";
-    rev = "v${version}";
+    tag = "v${version}";
     hash = "sha256-IdXP/gw81rQsaHz+uwEB9ThtHlbPYLXcYTYdwJynpVU=";
   };
 
   ldflags = [
     "-s"
     "-w"
-    "-X github.com/cycloidio/inframap/cmd.Version=${version}"
+    "-X=github.com/cycloidio/inframap/cmd.Version=${version}"
   ];
 
   vendorHash = "sha256-cEKrxuuksMEEVJEZ9/ZU2/MMxWZKlO05DkNX4n3ug/0=";
@@ -25,6 +26,7 @@ buildGoModule rec {
   meta = with lib; {
     description = "Read your tfstate or HCL to generate a graph specific for each provider, showing only the resources that are most important/relevant";
     homepage = "https://github.com/cycloidio/inframap";
+    changelog = "https://github.com/cycloidio/inframap/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ urandom ];
   };
