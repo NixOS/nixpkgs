49faa36ea1e8d319e7e4ca3bfd293fb09d3ee3b0
Paul Meyer <katexochen0@gmail.com>
2025-08-13 11:55:06 +0200

    gowitness: unpin buildGoModule
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15358,9 +15358,7 @@ with pkgs;
 
   gotrue-supabase = callPackage ../tools/security/gotrue/supabase.nix { };
 
-  gowitness = callPackage ../tools/security/gowitness {
-    buildGoModule = buildGo123Module;
-  };
+  gowitness = callPackage ../tools/security/gowitness { };
 
   helmfile = callPackage ../applications/networking/cluster/helmfile { };
 
