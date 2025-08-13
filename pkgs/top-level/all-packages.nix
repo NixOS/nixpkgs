ee8945f8269839ad5e50cdd95d9e3535bf0a0054
Paul Meyer <katexochen0@gmail.com>
2025-08-13 11:51:27 +0200

    carapace: unpin buildGoModule
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4620,9 +4620,7 @@ with pkgs;
   };
   bashInteractiveFHS = bashFHS;
 
-  carapace = callPackage ../shells/carapace {
-    buildGoModule = buildGo123Module;
-  };
+  carapace = callPackage ../shells/carapace { };
 
   wrapFish = callPackage ../shells/fish/wrapper.nix { };
 
