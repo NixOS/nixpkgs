bcf94dd3f07189b7475d823c8d67d08b58289905
Tristan Ross <tristan.ross@midstall.com>
2025-08-12 23:28:44 -0700

    llvmPackages_21: 21.1.0-rc2 -> 21.1.0-rc3

--- pkgs/development/compilers/llvm/default.nix
+++ pkgs/development/compilers/llvm/default.nix
@@ -32,7 +32,7 @@ let
     "18.1.8".officialRelease.sha256 = "sha256-iiZKMRo/WxJaBXct9GdAcAT3cz9d9pnAcO1mmR6oPNE=";
     "19.1.7".officialRelease.sha256 = "sha256-cZAB5vZjeTsXt9QHbP5xluWNQnAHByHtHnAhVDV0E6I=";
     "20.1.6".officialRelease.sha256 = "sha256-PfCzECiCM+k0hHqEUSr1TSpnII5nqIxg+Z8ICjmMj0Y=";
-    "21.1.0-rc2".officialRelease.sha256 = "sha256-7qE5MAYuB+gr5NmQm+7jJWCarIjoDUtyd8SDiJwvITw=";
+    "21.1.0-rc3".officialRelease.sha256 = "sha256-quZuqDIm8OrkDJqu7vJKUP8MF1xCuQNFwW9SnKMFoS8=";
     "22.0.0-git".gitRelease = {
       rev = "144cd87088dc82263b25e816c77fc03f29fd1288";
       rev-version = "22.0.0-unstable-2025-08-03";
