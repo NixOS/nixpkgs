8f1bb0b78475753867e80330062158efc28e7354
Doron Behar <doron.behar@gmail.com>
2025-08-13 17:06:01 +0300

    bmaptool: no with lib; in meta

--- pkgs/by-name/bm/bmaptool/package.nix
+++ pkgs/by-name/bm/bmaptool/package.nix
@@ -25,12 +25,12 @@ python3Packages.buildPythonApplication rec {
   # tests fail only on hydra.
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     description = "BMAP Tools";
     homepage = "https://github.com/yoctoproject/bmaptool";
-    license = licenses.gpl2Only;
-    maintainers = [ maintainers.dezgeg ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [ dezgeg ];
+    platforms = lib.platforms.linux;
     mainProgram = "bmaptool";
   };
 }
