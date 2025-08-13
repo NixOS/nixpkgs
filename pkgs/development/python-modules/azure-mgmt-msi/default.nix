903b894100d2e7d175d30e74505f7bf1599e7a38
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-13 11:21:39 +0200

    python313Packages.azure-mgmt-msi: 7.0.0 -> 7.1.0
    Changelog: https://github.com/Azure/azure-sdk-for-python/blob/azure-mgmt-msi_7.1.0/sdk/resources/azure-mgmt-msi/CHANGELOG.md

--- pkgs/development/python-modules/azure-mgmt-msi/default.nix
+++ pkgs/development/python-modules/azure-mgmt-msi/default.nix
@@ -3,34 +3,36 @@
   buildPythonPackage,
   pythonOlder,
   fetchPypi,
-  msrest,
+  isodate,
   azure-common,
   azure-mgmt-core,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "azure-mgmt-msi";
-  version = "7.0.0";
+  version = "7.1.0";
+  pyproject = true;
 
-  disabled = pythonOlder "3.6";
-
-  format = "setuptools";
+  disabled = pythonOlder "3.11";
 
   src = fetchPypi {
-    inherit pname version;
-    extension = "zip";
-    hash = "sha256-ctRsmmJ4PsTqthm+nRt4/+u9qhZNQG/TA/FjA/NyVrI=";
+    pname = "azure_mgmt_msi";
+    inherit version;
+    hash = "sha256-GgGgifH2bLDUsohmA9W6QV82Dv8L5vaFc37N1Zx4Ils=";
   };
 
-  propagatedBuildInputs = [
-    msrest
+  build-system = [ setuptools ];
+
+  dependencies = [
+    isodate
     azure-common
     azure-mgmt-core
   ];
 
   pythonNamespaces = [ "azure.mgmt" ];
 
-  # has no tests
+  # Module has no tests
   doCheck = false;
 
   pythonImportsCheck = [ "azure.mgmt.msi" ];
@@ -38,6 +40,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "This is the Microsoft Azure MSI Management Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/resources/azure-mgmt-msi";
+    changelog = "https://github.com/Azure/azure-sdk-for-python/blob/azure-mgmt-msi_${version}/sdk/resources/azure-mgmt-msi/CHANGELOG.md";
     license = licenses.mit;
     maintainers = with maintainers; [ maxwilson ];
   };
