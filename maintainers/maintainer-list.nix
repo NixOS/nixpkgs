9e47fbc5464fc74731ed1b4da13a0052c8f94e41
Loïs Postula <lois@postu.la>
2025-08-13 08:39:05 +0200

    maintainers: add mockersf

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16891,6 +16891,12 @@
     githubId = 7831184;
     name = "John Mercier";
   };
+  mockersf = {
+    email = "francois.mockers@vleue.com";
+    github = "mockersf";
+    githubId = 8672791;
+    name = "François Mockers";
+  };
   modderme123 = {
     email = "modderme123@gmail.com";
     github = "milomg";
