658c78a9d7a3d592ef2747c27c31518c1e800e37
bdim <i@bdim.moe>
2025-08-13 19:41:00 +0800

    maintainers: add bdim404

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3003,6 +3003,12 @@
     githubId = 354230;
     name = "Benjamin Esham";
   };
+  bdim404 = {
+    email = "i@bdim.moe";
+    github = "bdim404";
+    githubId = 102711645;
+    name = "bdim";
+  };
   bdimcheff = {
     email = "brandon@dimcheff.com";
     github = "bdimcheff";
