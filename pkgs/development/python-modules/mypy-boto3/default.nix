7de84ed7879bc562b57f20aaf1f01ef9b80bde1d
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-15 00:08:21 +0200

    python312Packages.mypy-boto3-ec2: 1.40.8 -> 1.40.10

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -446,8 +446,8 @@ rec {
       "sha256-p+NFAi4x4J6S4v0f2u0awDG+lb2V7r3XwgYwl5CvhHo=";
 
   mypy-boto3-ec2 =
-    buildMypyBoto3Package "ec2" "1.40.8"
-      "sha256-4qoBA1iajEhB0ove45iUUEdllcGds6Y08NhgPUS9TmQ=";
+    buildMypyBoto3Package "ec2" "1.40.10"
+      "sha256-mBpXSkz3eVzYlMqhzjVcCz+rVT21TvipBFaFAX4juHs=";
 
   mypy-boto3-ec2-instance-connect =
     buildMypyBoto3Package "ec2-instance-connect" "1.40.0"
