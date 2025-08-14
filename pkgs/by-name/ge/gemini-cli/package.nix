430284d979b4fd38c2d6e22b9ca07a0a42f751a4
Sergey Volkov <taranarmo@gmail.com>
2025-08-14 10:05:15 +0200

    gemini-cli: add taranarmo as maintainer

--- pkgs/by-name/ge/gemini-cli/package.nix
+++ pkgs/by-name/ge/gemini-cli/package.nix
@@ -59,7 +59,10 @@ buildNpmPackage (finalAttrs: {
     description = "AI agent that brings the power of Gemini directly into your terminal";
     homepage = "https://github.com/google-gemini/gemini-cli";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ donteatoreo ];
+    maintainers = with lib.maintainers; [
+      donteatoreo
+      taranarmo
+    ];
     platforms = lib.platforms.all;
     mainProgram = "gemini";
   };
