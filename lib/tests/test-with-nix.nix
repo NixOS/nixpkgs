c2698371ef823bb2158015a39347576ce0fcffe3
Sergei Zimmerman <sergei@zimmerman.foo>
2025-08-14 22:46:59 +0300

    lib/tests/test-with-nix: remove broken import ./check-eval
    This doesn't do the right thing here, because it evaluates
    the test with nix that is evaluating the `nixpkgs-lib-tests-nix-${nix.version}`
    derivation, not the Nix/Lix under test. This was just really busted for a long
    time.

--- lib/tests/test-with-nix.nix
+++ lib/tests/test-with-nix.nix
@@ -18,7 +18,6 @@
 pkgs.runCommand "nixpkgs-lib-tests-nix-${nix.version}"
   {
     buildInputs = [
-      (import ./check-eval.nix)
       (import ./fetchers.nix)
       (import ../path/tests {
         inherit pkgs;
