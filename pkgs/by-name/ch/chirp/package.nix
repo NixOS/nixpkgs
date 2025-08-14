176cafd107fa0dc28b3ba80a5bc271177c255b8c
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-14 10:37:26 +0000

    chirp: 0.4.0-unstable-2025-08-04 -> 0.4.0-unstable-2025-08-13

--- pkgs/by-name/ch/chirp/package.nix
+++ pkgs/by-name/ch/chirp/package.nix
@@ -11,14 +11,14 @@
 
 python3Packages.buildPythonApplication {
   pname = "chirp";
-  version = "0.4.0-unstable-2025-08-04";
+  version = "0.4.0-unstable-2025-08-13";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "kk7ds";
     repo = "chirp";
-    rev = "a8fb306f5627f2478d55541d21f7e9ed51363010";
-    hash = "sha256-SQWKHkIf9d23AKnJQv58XlTmuL07HRj6oS8LW4BM+7Y=";
+    rev = "acb1a78384a804dab1f2f0cc453b3da972d39072";
+    hash = "sha256-+1hzT7peZWtiREeOJqpCyrZNUxOVchxysv9RIAVKPds=";
   };
 
   nativeBuildInputs = [
