a951db063b9f39e55cade97db4c646e4fd01b8bc
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-14 23:49:41 +0000

    uwsm: 0.23.0 -> 0.23.2

--- pkgs/by-name/uw/uwsm/package.nix
+++ pkgs/by-name/uw/uwsm/package.nix
@@ -28,13 +28,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "uwsm";
-  version = "0.23.0";
+  version = "0.23.2";
 
   src = fetchFromGitHub {
     owner = "Vladimir-csp";
     repo = "uwsm";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VQhU88JvL7O2MP41JVuBdieIopmqrNiAWacGWvsNhSc=";
+    hash = "sha256-lKPCtlTfIAb7axthl/PUU82cULafYIhZ5xJolFKSOno=";
   };
 
   nativeBuildInputs = [
