3ced1f25e76d579d5c38b5c99a9ad93166f30c98
David Falk <spreetin@protonmail.com>
2025-08-15 06:49:23 +0000

    Apply suggestions from code review
    Co-authored-by: Yiyu Zhou <yiyuzhou19@gmail.com>

--- pkgs/by-name/go/gophertube/package.nix
+++ pkgs/by-name/go/gophertube/package.nix
@@ -1,34 +1,36 @@
 {
   lib,
-  pkgs,
   buildGoModule,
   fetchFromGitHub,
+  mpv,
+  fzf,
+  chafa,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "gophertube";
   version = "2.7.0";
 
   src = fetchFromGitHub {
     owner = "KrishnaSSH";
     repo = "GopherTube";
-    rev = "v${version}";
-    sha256 = "sha256-xsEK+iSobGOxiCx9lkQcxm6SIplTTbnXwP5/RzJQF+Q=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-xsEK+iSobGOxiCx9lkQcxm6SIplTTbnXwP5/RzJQF+Q=";
   };
 
   vendorHash = "sha256-WfVoCxzMk+h4AP1zgTNRXTpj8Ltu71YrsQ7OoU3Y4tg=";
 
-  buildInputs = with pkgs; [
+  buildInputs = [
     mpv
     fzf
     chafa
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Terminal user interface for search and watching YouTube videos using mpv and chafa";
     homepage = "https://github.com/KrishnaSSh/GopherTube";
-    license = licenses.gpl3Only;
-    maintainers = [ maintainers.spreetin ];
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ spreetin ];
     mainProgram = "gophertube";
   };
-}
+})
