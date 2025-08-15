31e71df7b33c22a8852cc41a0890dc8409085da9
NullString1 <kernd2005@gmail.com>
2025-08-15 17:13:26 +0100

    litecli: init at 1.12.3

---
+++ pkgs/development/python-modules/litecli/default.nix
@@ -0,0 +1,50 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools
+, click
+, pygments
+, prompt-toolkit
+, sqlparse
+, configobj
+, cli-helpers
+}:
+
+buildPythonPackage rec {
+  pname = "litecli";
+  version = "1.12.3";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-3XB8G8BAR6ptuhkW0gS+RwA6o4PPY4u+Dx1wvGVx1oE=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  propagatedBuildInputs = [
+    click
+    pygments
+    prompt-toolkit
+    sqlparse
+    configobj
+    cli-helpers
+  ];
+
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "litecli"
+  ];
+
+  meta = with lib; {
+    description = "CLI for SQLite Databases with auto-completion and syntax highlighting";
+    homepage = "https://github.com/dbcli/litecli";
+    changelog = "https://github.com/dbcli/litecli/releases/tag/v${version}";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ nullstring1 ];
+    mainProgram = "litecli";
+  };
+}
