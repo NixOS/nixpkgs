edc145b0d9c020547425fcbeb1da3fbe08ea6db0
Matt Sturgeon <matt@sturgeon.me.uk>
2025-08-15 20:19:12 +0100

    nexusmods-app: init update script for game_hashes_db
    (cherry picked from commit ee180d457a969287dcc55729833a344b7d014383)

--- pkgs/by-name/ne/nexusmods-app/game-hashes/default.nix
+++ pkgs/by-name/ne/nexusmods-app/game-hashes/default.nix
@@ -5,11 +5,13 @@ let
   repo = "game-hashes";
   repoURL = "https://github.com/${owner}/${repo}";
 
+  # Define a binding so that `update-source-version` can find it
   src = fetchurl {
     url = "${repoURL}/releases/download/${release}/game_hashes_db.zip";
     hash = "sha256-9xJ8yfLRkIV0o++NHK2igd2l83/tsgWc5cuwZO2zseY=";
     passthru = {
       inherit
+        src # Also for `update-source-version` support
         release
         owner
         repo
