b0702e6af517b82a554a776fcbb1cb5369f691aa
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-15 13:10:54 +0000

    python3Packages.wagtail-factories: 4.2.1 -> 4.3.0

--- pkgs/development/python-modules/wagtail-factories/default.nix
+++ pkgs/development/python-modules/wagtail-factories/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "wagtail-factories";
-  version = "4.2.1";
+  version = "4.3.0";
   format = "setuptools";
 
   src = fetchFromGitHub {
     repo = "wagtail-factories";
     owner = "wagtail";
-    tag = version;
-    sha256 = "sha256-jo8VwrmxHBJnORmuR6eTLrf/eupNL2vhXcw81EzfTxM=";
+    tag = "v${version}";
+    sha256 = "sha256-Rbu8D0vmUyF76YzF1QSQC5c0s12GxRrHNuUhMxcZdQY=";
   };
 
   propagatedBuildInputs = [
@@ -33,7 +33,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Factory boy classes for wagtail";
     homepage = "https://github.com/wagtail/wagtail-factories";
-    changelog = "https://github.com/wagtail/wagtail-factories/blob/${version}/CHANGES";
+    changelog = "https://github.com/wagtail/wagtail-factories/blob/${src.tag}/CHANGES";
     license = licenses.mit;
     maintainers = with maintainers; [ sephi ];
   };
