4eb50086c13f1bb5088183330f14027194aa75e3
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-15 03:46:42 +0000

    libretro.flycast: 0-unstable-2025-08-01 -> 0-unstable-2025-08-12

--- pkgs/applications/emulators/libretro/cores/flycast.nix
+++ pkgs/applications/emulators/libretro/cores/flycast.nix
@@ -8,13 +8,13 @@
 }:
 mkLibretroCore {
   core = "flycast";
-  version = "0-unstable-2025-08-01";
+  version = "0-unstable-2025-08-12";
 
   src = fetchFromGitHub {
     owner = "flyinghead";
     repo = "flycast";
-    rev = "40e400ab084175d3bd0f9e10cf8d6ac78c8b9544";
-    hash = "sha256-k/w1tmuGuRD98bR/kmc/9pLFGeobHMhKQapJOv8qVJo=";
+    rev = "33833cfd1ed2d94d907223442fdb8cdafd8d5d80";
+    hash = "sha256-6YXWJi3xbImfBMWILzsnwJGvj2XDoHcrWgLDPwaHfJs=";
     fetchSubmodules = true;
   };
 
