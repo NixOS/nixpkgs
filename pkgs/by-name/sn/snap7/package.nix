8f39c606839688b78bd56e60ca5442ea83b90597
06kellyjac <dev@j-k.io>
2025-08-15 16:42:49 +0100

    snap7: switch to `fetchurl` and `_7zz` with unpack in build

--- pkgs/by-name/sn/snap7/package.nix
+++ pkgs/by-name/sn/snap7/package.nix
@@ -1,22 +1,17 @@
 {
   stdenv,
   lib,
-  fetchzip,
-  p7zip,
+  fetchurl,
+  _7zz,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "snap7";
   version = "1.4.2";
 
-  src = fetchzip {
+  src = fetchurl {
     url = "mirror://sourceforge/snap7/${finalAttrs.version}/snap7-full-${finalAttrs.version}.7z";
-    sha256 = "1n5gs8bwb6g9vfllf3x12r5yzqzapmlq1bmc6hl854b8vkg30y8c";
-    postFetch = ''
-      ${p7zip}/bin/7z x $downloadedFile
-      mkdir $out
-      cp -r snap7-full-${finalAttrs.version}/* $out/
-    '';
+    hash = "sha256-H0JwzehoSVd3ChCh0xHCJuZw2VicaYQakBLoGPe5+A4=";
   };
 
   outputs = [
@@ -25,16 +20,22 @@ stdenv.mkDerivation (finalAttrs: {
     "doc"
   ];
 
-  makefile = "x86_64_linux.mk";
-  makeFlags = [ "LibInstall=$(out)/lib" ];
+  nativeBuildInputs = [
+    _7zz
+  ];
 
-  preBuild = "cd build/unix";
+  makefile = "x86_64_linux.mk";
+  makeFlags = [
+    "-C build/unix"
+    "LibInstall=$(out)/lib"
+  ];
+
   preInstall = ''
     mkdir -p $out/lib
     mkdir -p $dev/include
     mkdir -p $doc/share
-    cp $src/examples/cpp/snap7.h $dev/include
-    cp -r $src/doc $doc/share/
+    cp examples/cpp/snap7.h $dev/include
+    cp -r doc $doc/share/
   '';
 
   meta = {
