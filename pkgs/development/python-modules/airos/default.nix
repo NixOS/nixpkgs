ac1c58282bc5e950fa77817c7f9034a3774940d0
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-15 23:08:11 +0200

    python3Packages.airos: 0.2.7 -> 0.2.11
    https://github.com/CoMPaTech/python-airos/releases/tag/v0.2.11

--- pkgs/development/python-modules/airos/default.nix
+++ pkgs/development/python-modules/airos/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "airos";
-  version = "0.2.7";
+  version = "0.2.11";
   pyproject = true;
 
   disabled = pythonOlder "3.13";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "CoMPaTech";
     repo = "python-airos";
     tag = "v${version}";
-    hash = "sha256-c/YVjNEy1nI6ClYk8BgLai+zRCwiblTFSJ5Rxx3UaeU=";
+    hash = "sha256-rPgrnLMcqUDd1+WL6uwb5Umu3a844B301l+O/GEyCGA=";
   };
 
   build-system = [ setuptools ];
