853a22e6c3a15fdef1e5205b2dedec883ab8d346
mannerbund <apostalimus@gmail.com>
2025-08-15 16:21:32 +0300

    emacsPackages.eglot-booster: init at 0.1.0

---
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/eglot-booster/package.nix
@@ -0,0 +1,36 @@
+{ lib
+, melpaBuild
+, fetchFromGitHub
+, unstableGitUpdater
+, json-rpc-server
+, eglot
+, seq
+,
+}:
+
+melpaBuild {
+  pname = "eglot-booster";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "jdtsmith";
+    repo = "eglot-booster";
+    rev = "e6daa6bcaf4aceee29c8a5a949b43eb1b89900ed";
+    hash = "sha256-PLfaXELkdX5NZcSmR1s/kgmU16ODF8bn56nfTh9g6bs=";
+  };
+
+  packageRequires = [
+    json-rpc-server
+    eglot
+    seq
+  ];
+
+  passthru.updateScript = unstableGitUpdater { };
+
+  meta = {
+    homepage = "https://github.com/jdtsmith/eglot-booster";
+    description = "Boost eglot using lsp-booster";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maineinters; [ mannerbund ];
+  };
+}
