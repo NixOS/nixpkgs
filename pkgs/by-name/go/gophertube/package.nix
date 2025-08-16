46d3f9cabe9a7251d303d51f1edc3dd1c15333d4
David Falk <spreetin@protonmail.com>
2025-08-16 15:56:01 +0000

    Apply suggestions from code review
    Fixes some missed dependencies in the original derivation.
    
    Co-authored-by: Yiyu Zhou <yiyuzhou19@gmail.com>

--- pkgs/by-name/go/gophertube/package.nix
+++ pkgs/by-name/go/gophertube/package.nix
@@ -2,9 +2,11 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  yt-dlp,
   mpv,
   fzf,
   chafa,
+  makeBinaryWrapper,
 }:
 
 buildGoModule (finalAttrs: {
@@ -20,17 +22,28 @@ buildGoModule (finalAttrs: {
 
   vendorHash = "sha256-WfVoCxzMk+h4AP1zgTNRXTpj8Ltu71YrsQ7OoU3Y4tg=";
 
-  buildInputs = [
+  nativeBuildInputs = [ makeBinaryWrapper ];
+
+  propagatedUserEnvPkgs = [
+    yt-dlp
     mpv
     fzf
     chafa
   ];
 
+  postInstall = ''
+    wrapProgram $out/bin/gophertube \
+      --suffix PATH : ${lib.makeBinPath finalAttrs.propagatedUserEnvPkgs}
+  '';
+
   meta = {
     description = "Terminal user interface for search and watching YouTube videos using mpv and chafa";
     homepage = "https://github.com/KrishnaSSh/GopherTube";
     license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [ spreetin ];
+    maintainers = with lib.maintainers; [
+      spreetin
+      yiyu
+    ];
     mainProgram = "gophertube";
   };
 })
