8410660596f91f4c09d5aac84534cc2a879d2b49
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-16 13:16:53 +0200

    home-assistant.python.pkgs.pytest-homeassistant-custom-component: 0.13.270 -> 0.13.271
    https://github.com/MatthewFlamm/pytest-homeassistant-custom-component/blob/refs/tags/0.13.271/CHANGELOG.md

--- pkgs/servers/home-assistant/pytest-homeassistant-custom-component.nix
+++ pkgs/servers/home-assistant/pytest-homeassistant-custom-component.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pytest-homeassistant-custom-component";
-  version = "0.13.270";
+  version = "0.13.271";
   pyproject = true;
 
   disabled = pythonOlder "3.13";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "MatthewFlamm";
     repo = "pytest-homeassistant-custom-component";
     rev = "refs/tags/${version}";
-    hash = "sha256-SDeQuzBZ1Fd97qotyWL6oB2CTB+qfplMN6yOaJguRmI=";
+    hash = "sha256-2XRj7W0XKKQovP8azU3VzyZaGTCYYxCtzwPWvAVv75M=";
   };
 
   build-system = [ setuptools ];
