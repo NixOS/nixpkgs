b8208fb758c2a15ed0cc31021bdce65d4ae3b2f6
Defelo <mail@defelo.de>
2025-08-16 12:22:12 +0000

    ncdu: 2.8.2 -> 2.9
    Changelog: https://dev.yorhel.nl/ncdu/changes2

--- pkgs/by-name/nc/ncdu/package.nix
+++ pkgs/by-name/nc/ncdu/package.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ncdu";
-  version = "2.8.2";
+  version = "2.9";
 
   src = fetchurl {
     url = "https://dev.yorhel.nl/download/ncdu-${finalAttrs.version}.tar.gz";
-    hash = "sha256-Ai+nZdNaeXl6zcgMgxcH30PJo7pg0a4+bqTMG3osAT0=";
+    hash = "sha256-dfCsO85PwBLoGYtyUY21F56QMHmFjvzgi5EtXcxwlNM=";
   };
 
   nativeBuildInputs = [
