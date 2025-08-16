f77a3776d8895d5d446d8723bc67c31f94a82665
Defelo <mail@defelo.de>
2025-08-17 00:57:55 +0200

    versionCheckHook: add versionCheckScript parameter

--- pkgs/by-name/ve/versionCheckHook/hook.sh
+++ pkgs/by-name/ve/versionCheckHook/hook.sh
@@ -1,14 +1,16 @@
 _handleCmdOutput(){
-    local command=("$1" "$2")
     local versionOutput
 
     local envArgs=()
-    if [[ "$3" != "*" ]]; then
+    if [[ "$1" != "*" ]]; then
       envArgs+=("--ignore-environment")
-      for var in $3; do
+      for var in $1; do
         envArgs+=("$var=${!var}")
       done
     fi
+
+    shift 1
+    local command=("$@")
 
     versionOutput="$(env \
         --chdir=/ \
@@ -37,39 +39,45 @@ versionCheckHook(){
     # Don't keep any environment variables by default
     : "${versionCheckKeepEnvironment:=}"
 
-    local cmdProgram cmdArg echoPrefix
-    if [[ ! -z "${versionCheckProgram-}" ]]; then
-        cmdProgram="$versionCheckProgram"
-    elif [[ ! -z "${NIX_MAIN_PROGRAM-}" ]]; then
-        cmdProgram="${!outputBin}/bin/${NIX_MAIN_PROGRAM}"
-    elif [[ ! -z "${pname-}" ]]; then
-        echo "versionCheckHook: Package \`${pname}\` does not have the \`meta.mainProgram\` attribute." \
-            "We'll assume that the main program has the same name for now, but this behavior is deprecated," \
-            "because it leads to surprising errors when the assumption does not hold." \
-            "If the package has a main program, please set \`meta.mainProgram\` in its definition to make this warning go away." \
-            "Should the binary that outputs the intended version differ from \`meta.mainProgram\`, consider setting \`versionCheckProgram\` instead." >&2
-        cmdProgram="${!outputBin}/bin/${pname}"
-    else
-        echo "versionCheckHook: \$NIX_MAIN_PROGRAM, \$versionCheckProgram and \$pname are all empty, so" \
-            "we don't know how to run the versionCheckPhase." \
-            "To fix this, set one of \`meta.mainProgram\` or \`versionCheckProgram\`." >&2
-        exit 2
-    fi
+    local echoPrefix
 
-    if [[ ! -x "$cmdProgram" ]]; then
-        echo "versionCheckHook: $cmdProgram was not found, or is not an executable" >&2
-        exit 2
-    fi
-    if [[ -z "${versionCheckProgramArg}" ]]; then
-        for cmdArg in "--help" "--version"; do
-            echoPrefix="$(_handleCmdOutput "$cmdProgram" "$cmdArg" "$versionCheckKeepEnvironment")"
-            if [[ "$echoPrefix" == "Successfully managed to" ]]; then
-                break
-            fi
-        done
+    if [[ -n "${versionCheckScript-}" ]]; then
+      echoPrefix="$(_handleCmdOutput "$versionCheckKeepEnvironment" "@bash@" "-c" "$versionCheckScript")"
     else
-        cmdArg="$versionCheckProgramArg"
-        echoPrefix="$(_handleCmdOutput "$cmdProgram" "$cmdArg" "$versionCheckKeepEnvironment")"
+      local cmdProgram cmdArg
+      if [[ ! -z "${versionCheckProgram-}" ]]; then
+          cmdProgram="$versionCheckProgram"
+      elif [[ ! -z "${NIX_MAIN_PROGRAM-}" ]]; then
+          cmdProgram="${!outputBin}/bin/${NIX_MAIN_PROGRAM}"
+      elif [[ ! -z "${pname-}" ]]; then
+          echo "versionCheckHook: Package \`${pname}\` does not have the \`meta.mainProgram\` attribute." \
+              "We'll assume that the main program has the same name for now, but this behavior is deprecated," \
+              "because it leads to surprising errors when the assumption does not hold." \
+              "If the package has a main program, please set \`meta.mainProgram\` in its definition to make this warning go away." \
+              "Should the binary that outputs the intended version differ from \`meta.mainProgram\`, consider setting \`versionCheckProgram\` instead." >&2
+          cmdProgram="${!outputBin}/bin/${pname}"
+      else
+          echo "versionCheckHook: \$NIX_MAIN_PROGRAM, \$versionCheckProgram and \$pname are all empty, so" \
+              "we don't know how to run the versionCheckPhase." \
+              "To fix this, set one of \`meta.mainProgram\` or \`versionCheckProgram\`." >&2
+          exit 2
+      fi
+
+      if [[ ! -x "$cmdProgram" ]]; then
+          echo "versionCheckHook: $cmdProgram was not found, or is not an executable" >&2
+          exit 2
+      fi
+      if [[ -z "${versionCheckProgramArg}" ]]; then
+          for cmdArg in "--help" "--version"; do
+              echoPrefix="$(_handleCmdOutput "$versionCheckKeepEnvironment" "$cmdProgram" "$cmdArg")"
+              if [[ "$echoPrefix" == "Successfully managed to" ]]; then
+                  break
+              fi
+          done
+      else
+          cmdArg="$versionCheckProgramArg"
+          echoPrefix="$(_handleCmdOutput "$versionCheckKeepEnvironment" "$cmdProgram" "$cmdArg")"
+      fi
     fi
     if [[ "$echoPrefix" == "Did not" ]]; then
         exit 2
