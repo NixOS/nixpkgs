47360f32494357c028ca1e3d7086e0b32d41a07c
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-17 05:15:12 +0000

    python3Packages.pyatmo: 9.2.1 -> 9.2.3

--- pkgs/development/python-modules/pyatmo/default.nix
+++ pkgs/development/python-modules/pyatmo/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pyatmo";
-  version = "9.2.1";
+  version = "9.2.3";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "jabesq";
     repo = "pyatmo";
     tag = "v${version}";
-    hash = "sha256-vSyZsWhqyQqKFukD6GbtkAJd3QBmRwdmRIYD19DXQW0=";
+    hash = "sha256-czHn5pgiyQwn+78NQnJDo49knstL9m2Gp3neZeb75js=";
   };
 
   pythonRelaxDeps = [
