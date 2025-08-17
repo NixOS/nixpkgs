e28460ba46b0c85d67e8cd8afae08fb5b2bc0793
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-17 08:21:18 +0000

    vscode-extensions.fstarlang.fstar-vscode-assistant: 0.19.1 -> 0.19.2

--- pkgs/applications/editors/vscode/extensions/fstarlang.fstar-vscode-assistant/default.nix
+++ pkgs/applications/editors/vscode/extensions/fstarlang.fstar-vscode-assistant/default.nix
@@ -4,8 +4,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "fstar-vscode-assistant";
     publisher = "FStarLang";
-    version = "0.19.1";
-    hash = "sha256-bC9Kzhp4H9wykuitEKQUthYVhmVI/m8H0PloBqoFbvU=";
+    version = "0.19.2";
+    hash = "sha256-4EerlsxIBjKIpeSS388Nw40eD5tBL+cN5uNAsfu+gio=";
   };
   meta = {
     description = "Interactive editing mode VS Code extension for F*";
