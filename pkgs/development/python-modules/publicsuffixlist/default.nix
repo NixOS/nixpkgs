92edfad5583df11e0ff0795231892e1eb5e4d3a8
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-17 21:50:35 +0200

    python313Packages.publicsuffixlist: 1.0.2.20250814 -> 1.0.2.20250815
    Changelog: https://github.com/ko-zu/psl/blob/v1.0.2.20250815-gha/CHANGES.md

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "publicsuffixlist";
-  version = "1.0.2.20250814";
+  version = "1.0.2.20250815";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-pYOjopZlGfe28FzZmGP485l/AzqNzoW8ecft94p7q+c=";
+    hash = "sha256-N08LlCOmZvkBbIPUh80QY/NM5wxS4FefKnFF4XvlH0k=";
   };
 
   build-system = [ setuptools ];
