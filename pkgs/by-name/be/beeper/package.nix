e2d3c91bb4ec997f6e13f043a9b9867b6531d107
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-17 06:05:55 +0000

    beeper: 4.1.64 -> 4.1.111

--- pkgs/by-name/be/beeper/package.nix
+++ pkgs/by-name/be/beeper/package.nix
@@ -9,10 +9,10 @@
 }:
 let
   pname = "beeper";
-  version = "4.1.64";
+  version = "4.1.111";
   src = fetchurl {
     url = "https://beeper-desktop.download.beeper.com/builds/Beeper-${version}.AppImage";
-    hash = "sha256-8Gd7qSp0/anYLrLCUfq15OacVsKwFVWQxaVvN9xsXkY=";
+    hash = "sha256-0cr6syveIHIIy8+FyE23U8iidMYJXkN8CqhhbH0oNt0=";
   };
   appimageContents = appimageTools.extract {
     inherit pname version src;
