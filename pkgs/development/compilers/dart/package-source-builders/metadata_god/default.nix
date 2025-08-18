3ca3b35479e4021a7b73eb915eb04a3fb9f76466
qzylinra <qzylinra@users.noreply.github.com>
2025-08-19 06:05:38 +0800

    dart.metadata_god: update

--- pkgs/development/compilers/dart/package-source-builders/metadata_god/default.nix
+++ pkgs/development/compilers/dart/package-source-builders/metadata_god/default.nix
@@ -1,44 +1,49 @@
 {
   lib,
   stdenv,
-  fetchzip,
-  replaceVars,
+  rustPlatform,
+  writeText,
 }:
 
 { version, src, ... }:
 
 let
-  inherit (stdenv.hostPlatform) system;
-  selectSystem =
-    attrs: attrs.${system} or (throw "metadata_god: ${stdenv.hostPlatform.system} is not supported");
-  suffix = selectSystem {
-    x86_64-linux = "linux-x64";
-    aarch64-linux = "linux-arm64";
+  rustDep = rustPlatform.buildRustPackage {
+    pname = "metadata_god-rs";
+    inherit version src;
+
+    # failed to select a version
+    cargoLock.lockFile = ./Cargo.lock;
+
+    postUnpack = ''
+      echo 'resolver = "2"' >> ${src.name}/Cargo.toml
+      substituteInPlace ${src.name}/rust/Cargo.toml \
+        --replace-fail "1.0.89" "*" \
+        --replace-fail "=2.5.0" "*" \
+        --replace-fail "0.21.0" "*"
+      cp ${./Cargo.lock} ${src.name}/Cargo.lock
+    '';
+
+    passthru.libraryPath = "lib/libmetadata_god.so";
   };
-  metadata_god = fetchzip {
-    url = "https://github.com/KRTirtho/frb_plugins/releases/download/metadata_god-v0.5.3/linux.tar.gz";
-    hash = "sha256-ZR/q1dF8w4Yab6dRRiS5ZCChVnoecFUrtGiHXGlll9A=";
-    stripRoot = false;
-  };
+
+  fakeCargokitCmake = writeText "FakeCargokit.cmake" ''
+    function(apply_cargokit target manifest_dir lib_name any_symbol_name)
+      set("''${target}_cargokit_lib" ${rustDep}/${rustDep.passthru.libraryPath} PARENT_SCOPE)
+    endfunction()
+  '';
 in
 stdenv.mkDerivation {
   pname = "metadata_god";
   inherit version src;
   inherit (src) passthru;
 
-  patches = [
-    (replaceVars ./metadata_god.patch {
-      output_lib = "${metadata_god}/${suffix}/libmetadata_god.so";
-    })
-  ];
-
   installPhase = ''
     runHook preInstall
 
+    cp ${fakeCargokitCmake} cargokit/cmake/cargokit.cmake
     cp -r . $out
 
     runHook postInstall
   '';
-
-  meta.sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
 }
