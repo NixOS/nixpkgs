b077d61832cc52b0f727f154374d4da71c41e3c3
László Kupcsik <dev@biobits.be>
2025-08-18 11:12:15 +0200

    rPackages.graper: revert outdated fix
    This reverts commit d5cf10fb90cc3cea04a4ca241cce4cf2f800ea1d.

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -1999,13 +1999,6 @@ let
       postPatch = "patchShebangs configure";
     });
 
-    graper = old.graper.overrideAttrs (attrs: {
-      postPatch = ''
-        substituteInPlace "src/Makevars" \
-          --replace-fail "CXX_STD=CXX11" "CXX_STD=CXX14"
-      '';
-    });
-
     ocf = old.ocf.overrideAttrs (attrs: {
       postPatch = "patchShebangs configure";
     });
