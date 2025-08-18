73d8297d716412a960e7c9efe61fdf30ce04d4f4
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-08-18 22:11:05 +0700

    h2o: 2.3.0-untagged-2025-08-13 → 2.3.0-untagged-2025-08-14
    (cherry picked from commit 03e079da8e7ac9e046bb15e95b25a1e793c3598c)

--- pkgs/by-name/h2/h2o/package.nix
+++ pkgs/by-name/h2/h2o/package.nix
@@ -24,13 +24,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "h2o";
-  version = "2.3.0-untagged-2025-08-13";
+  version = "2.3.0-untagged-2025-08-14";
 
   src = fetchFromGitHub {
     owner = "h2o";
     repo = "h2o";
-    rev = "4729b661e3c6654198d2cc62997e1af58bef4b80";
-    sha256 = "sha256-3vVe2nveWohpxtqECq/wcPupAHaRRoG54sG94J+3eyQ=";
+    rev = "ffab9c49c33b1f0e9aec9804028156aae9db8ef0";
+    sha256 = "sha256-kdU2p9oUhxGnw8JU9qGjV4mn2jMeSUL0rLgveMs6NiI=";
   };
 
   patches = [
