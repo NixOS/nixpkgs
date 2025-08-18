db02b37b72500619d78e9970b0fcbffa6ebf0f2b
Ben Darwin <bcdarwin@gmail.com>
2025-08-18 12:32:58 -0400

    python313Packages.highdicom: use test data via passthru from pydicom
    This eliminates a spurious copy of pydicom-data.

--- pkgs/development/python-modules/highdicom/default.nix
+++ pkgs/development/python-modules/highdicom/default.nix
@@ -14,14 +14,6 @@
   typing-extensions,
 }:
 
-let
-  test_data = fetchFromGitHub {
-    owner = "pydicom";
-    repo = "pydicom-data";
-    rev = "cbb9b2148bccf0f550e3758c07aca3d0e328e768";
-    hash = "sha256-nF/j7pfcEpWHjjsqqTtIkW8hCEbuQ3J4IxpRk0qc1CQ=";
-  };
-in
 buildPythonPackage rec {
   pname = "highdicom";
   version = "0.26.1";
@@ -63,7 +55,7 @@ buildPythonPackage rec {
   preCheck = ''
     export HOME=$TMP/test-home
     mkdir -p $HOME/.pydicom/
-    ln -s ${test_data}/data_store/data $HOME/.pydicom/data
+    ln -s ${pydicom.passthru.pydicom-data}/data_store/data $HOME/.pydicom/data
   '';
 
   disabledTests = [
@@ -93,9 +85,6 @@ buildPythonPackage rec {
     "highdicom.sr"
     "highdicom.sc"
   ];
-
-  # updates the wrong fetcher
-  passthru.skipBulkUpdate = true;
 
   meta = {
     description = "High-level DICOM abstractions for Python";
