c82b05cf010bacfdfb3fad2694a41c48f9df68f3
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-18 07:15:01 +0000

    databricks-cli: 0.263.0 -> 0.264.2

--- pkgs/by-name/da/databricks-cli/package.nix
+++ pkgs/by-name/da/databricks-cli/package.nix
@@ -10,13 +10,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "databricks-cli";
-  version = "0.263.0";
+  version = "0.264.2";
 
   src = fetchFromGitHub {
     owner = "databricks";
     repo = "cli";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-bRHZGoO7+k7HoXcyJMusqDnn3XoAesgZ280j8jNgQYY=";
+    hash = "sha256-tPX0QOtNsRf8W0jP8BLL3QE3FDFq12WpObwra+FjH9I=";
   };
 
   # Otherwise these tests fail asserting that the version is 0.0.0-dev
@@ -25,7 +25,7 @@ buildGoModule (finalAttrs: {
       --replace-fail "cli/0.0.0-dev" "cli/${finalAttrs.version}"
   '';
 
-  vendorHash = "sha256-sJyinqKX4irO4rquJ1hxDU/GH4XcyxPGw7qH0ZLgdxU=";
+  vendorHash = "sha256-Uv2B9Z2qfuGEjTo4Jcwf0ygB+keWQixeymZrT+KdHlg=";
 
   excludedPackages = [
     "bundle/internal"
