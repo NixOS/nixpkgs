d7dda7c6937048e53adfff9fff51e7375b0844a1
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-08-18 23:27:29 +0200

    maintainers: add Swarsel

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -24907,6 +24907,17 @@
     githubId = 4572854;
     name = "Shawn Warren";
   };
+  swarsel = {
+    name = "Leon Schwarzäugl";
+    email = "leon@swarsel.win";
+    github = "Swarsel";
+    githubId = 32304731;
+    keys = [
+      {
+        fingerprint = "4BE7 9252 6228 9B47 6DBB  C17B 76FD 3810 215A E097";
+      }
+    ];
+  };
   swdunlop = {
     email = "swdunlop@gmail.com";
     github = "swdunlop";
