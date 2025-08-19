63ea54ac57217887c9cf15610821d28781d29d73
Herwig Hochleitner <herwig@bendlas.net>
2025-08-19 19:26:35 +0200

    gitea-mcp-server: 0.3.0 -> 0.3.1

--- pkgs/by-name/gi/gitea-mcp-server/package.nix
+++ pkgs/by-name/gi/gitea-mcp-server/package.nix
@@ -5,17 +5,17 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "gitea-mcp-server";
-  version = "0.3.0";
+  version = "0.3.1";
 
   src = fetchFromGitea {
     domain = "gitea.com";
     owner = "gitea";
     repo = "gitea-mcp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-hJQ0ryEcPg/WOi54RLZswhWZOjkbllZWOsYyOhe+4AA=";
+    hash = "sha256-wtQMwIm4bQ75t93cWnwEyzpcIA1ZlI2XOVrQJrX0xXo=";
   };
 
-  vendorHash = "sha256-u9jIjrbDUhnaaeBET+pKQTKhaQLUeQvKOXSBfS0vMJM=";
+  vendorHash = "sha256-LZIgADgUUNrMPBdCF0kz4koZUvGfHvzb8T+hwbiIYjs=";
 
   subPackages = [ "." ];
 
