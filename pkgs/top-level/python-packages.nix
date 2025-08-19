ca4e6b47aa4ed1ae7a6f7272d2b76f44a609aa6d
melvyn <melvyn2@dnsense.pub>
2025-08-19 11:03:21 -0700

    seafile-server: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16395,8 +16395,6 @@ self: super: with self; {
 
   seabreeze = callPackage ../development/python-modules/seabreeze { };
 
-  seaserv = toPythonModule (pkgs.seafile-server.override { python3 = self.python; });
-
   seasonal = callPackage ../development/python-modules/seasonal { };
 
   seatconnect = callPackage ../development/python-modules/seatconnect { };
