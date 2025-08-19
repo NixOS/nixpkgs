38658f5d17bd93fb230deb3db7b4042ad2b990c1
AsbjornOlling <asbjornolling@gmail.com>
2025-08-19 11:43:28 +0200

    llama-cpp: 6134 -> 6202

--- pkgs/by-name/ll/llama-cpp/package.nix
+++ pkgs/by-name/ll/llama-cpp/package.nix
@@ -72,21 +72,19 @@ let
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "llama-cpp";
-  version = "6134";
+  version = "6202";
 
   src = fetchFromGitHub {
     owner = "ggml-org";
     repo = "llama.cpp";
     tag = "b${finalAttrs.version}";
-    hash = "sha256-J/Z6xrCfdSkf504AGiOmgRqgrOUXXTpqq5BpXwgOI4g=";
+    hash = "sha256-bssXy4fGndq4r1B1+veR30ewz2Tuan0bhXm0+QumFWU=";
     leaveDotGit = true;
     postFetch = ''
       git -C "$out" rev-parse --short HEAD > $out/COMMIT
       find "$out" -name .git -print0 | xargs -0 rm -rf
     '';
   };
-
-  patches = lib.optionals vulkanSupport [ ./disable_bfloat16.patch ];
 
   postPatch = ''
     # Workaround for local-ai package which overrides this package to an older llama-cpp
