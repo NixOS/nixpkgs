aa4fdb037efcc683088831c3c43e2adb090f6121
Benjamin Lemouzy <blemouzy.ml@gmail.com>
2025-08-19 11:51:43 +0200

    segger-jlink: 824 -> 868

--- pkgs/by-name/se/segger-jlink/source.nix
+++ pkgs/by-name/se/segger-jlink/source.nix
@@ -1,39 +1,39 @@
 {
-  version = "824";
+  version = "868";
   x86_64-linux = {
     os = "Linux";
     name = "x86_64";
     ext = "tgz";
-    hash = "sha256-TsNlwApXdbkHEI+pG9NVK7pJkKdvFYGIGgn+9xJ9q8A=";
+    hash = "sha256-wvU+pCbujfeX0EG/LeFeyHZJfVsRn9Ns6n2x6IhSb7w=";
   };
   i686-linux = {
     os = "Linux";
     name = "i386";
     ext = "tgz";
-    hash = "sha256-Gs4iifvchFY0d39bhQ61lX/TPGjCKPRidkJQbva91gA=";
+    hash = "sha256-Kf3vwkcJCPg80oCBcx07IP7u5V4umRopBECjruz5Yek=";
   };
   aarch64-linux = {
     os = "Linux";
     name = "arm64";
     ext = "tgz";
-    hash = "sha256-YowEzJ//idmZ4eznPNgEW142ApsnvkboGAWfDCHfIS0=";
+    hash = "sha256-Eje2/7OmqL4YeIRI7cZ2Nj7s+SPBc0Njjev0grMlBVI=";
   };
   armv7l-linux = {
     os = "Linux";
     name = "arm";
     ext = "tgz";
-    hash = "sha256-mLl/qNtxMfFOe/M0fCZnnebei7E2ON4gvE9Q7XIufag=";
+    hash = "sha256-Ug8DqpAQJjPNLOY3ZBGUMZAwGJjKI4HhQVN4CGWHOZA=";
   };
   aarch64-darwin = {
     os = "MacOSX";
     name = "arm64";
     ext = "pkg";
-    hash = "sha256-GKlWof4XdxEwW7G8YmhdfjjJpXEXICqWapI7fly8Uvg=";
+    hash = "sha256-ZF3A802sMuXECFHcX+T1Uh2GdXNTTfeYhxSCwohXvHY=";
   };
   x86_64-darwin = {
     os = "MacOSX";
     name = "x86_64";
     ext = "pkg";
-    hash = "sha256-LWLUdzNVkN60qET7vhvLCoepg7kuHPqs2bZspRzZkGo=";
+    hash = "sha256-fKQrI2HUw1Umk0OYHZlw12hwyL0zm5p2waVTIP+CIOg=";
   };
 }
