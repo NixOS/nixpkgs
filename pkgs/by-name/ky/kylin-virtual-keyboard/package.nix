c6e6f2e13599acac2f2444d9c90602435074d112
Straying <dingdang66686@outlook.com>
2025-08-19 10:20:18 +0800

    kylin-virtual-keyboard: apply review comments.

--- pkgs/by-name/ky/kylin-virtual-keyboard/package.nix
+++ pkgs/by-name/ky/kylin-virtual-keyboard/package.nix
@@ -3,23 +3,27 @@
   stdenv,
   fetchFromGitHub,
   cmake,
-  fcitx5,
-  libsForQt5,
-  gsettings-qt,
-  glib,
   pkg-config,
+  libsForQt5,
+  glib,
+  fcitx5,
+  gsettings-qt,
+  bashNonInteractive,
+  nix-update-script,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "kylin-virtual-keyboard";
   version = "4.20.1.0";
 
   src = fetchFromGitHub {
     owner = "openkylin";
     repo = "kylin-virtual-keyboard";
-    rev = "upstream/${version}";
-    sha256 = "sha256-YM5FLJcgjhx7Ks1bgC26Ee4oDF8C6dK8yTtXSnD84aA=";
+    tag = "upstream/${finalAttrs.version}";
+    hash = "sha256-YM5FLJcgjhx7Ks1bgC26Ee4oDF8C6dK8yTtXSnD84aA=";
   };
+
+  strictDeps = true;
 
   nativeBuildInputs = [
     cmake
@@ -44,18 +48,15 @@ stdenv.mkDerivation rec {
     substituteInPlace CMakeLists.txt \
       --replace-fail "/etc/xdg/autostart" "${placeholder "out"}/etc/xdg/autostart"
 
-    substituteInPlace data/kylin-virtual-keyboard-xwayland \
-      --replace-fail "/usr" "${placeholder "out"}"
-
-    substituteInPlace data/kylin-virtual-keyboard.desktop \
-      --replace-fail "/usr" "${placeholder "out"}"
-
-    substituteInPlace data/org.fcitx.Fcitx5.VirtualKeyboard.service \
-      --replace-fail "/usr" "${placeholder "out"}"
+    substituteInPlace \
+      data/{kylin-virtual-keyboard-xwayland,kylin-virtual-keyboard.desktop,org.fcitx.Fcitx5.VirtualKeyboard.service} \
+        --replace-fail "/usr" "${placeholder "out"}"
   '';
 
   postInstall = ''
     glib-compile-schemas $out/share/glib-2.0/schemas
+    substituteInPlace $out/bin/kylin-virtual-keyboard-xwayland \
+      --replace-fail "#!/bin/bash" "#!${lib.getExe bashNonInteractive}"
   '';
 
   postFixup = ''
@@ -63,11 +64,13 @@ stdenv.mkDerivation rec {
       --prefix XDG_DATA_DIRS : "$out/share/gsettings-schemas/$pname-$version"
   '';
 
-  meta = with lib; {
-    description = "Kylin Virtual Keyboard - a modern on-screen keyboard for Linux";
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Modern on-screen keyboard for Linux";
     homepage = "https://gitee.com/openkylin/kylin-virtual-keyboard";
-    license = licenses.lgpl3;
-    maintainers = with maintainers; [ dingdang66686 ];
-    platforms = platforms.linux;
+    license = lib.licenses.lgpl3;
+    maintainers = with lib.maintainers; [ dingdang66686 ];
+    platforms = lib.platforms.linux;
   };
-}
+})
