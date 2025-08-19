71b757514bce9f46350f1de1404ae105d1e72b5d
jopejoe1 <jopejoe1@missing.ninja>
2025-08-19 14:42:47 +0200

    ocamlPackages: swap throws to meta.broken
    throws no longer work with the current ci we have in nixpkgs

--- pkgs/development/ocaml-modules/fmt/default.nix
+++ pkgs/development/ocaml-modules/fmt/default.nix
@@ -9,39 +9,36 @@
   cmdliner,
 }:
 
-if lib.versionOlder ocaml.version "4.08" then
-  throw "fmt is not available for OCaml ${ocaml.version}"
-else
+stdenv.mkDerivation rec {
+  version = "0.11.0";
+  pname = "ocaml${ocaml.version}-fmt";
 
-  stdenv.mkDerivation rec {
-    version = "0.11.0";
-    pname = "ocaml${ocaml.version}-fmt";
+  src = fetchurl {
+    url = "https://erratique.ch/software/fmt/releases/fmt-${version}.tbz";
+    sha256 = "sha256-hXz9R6VLUkKc2bPiZl5EFzzRvTtDW+znFy+YStU3ahs=";
+  };
 
-    src = fetchurl {
-      url = "https://erratique.ch/software/fmt/releases/fmt-${version}.tbz";
-      sha256 = "sha256-hXz9R6VLUkKc2bPiZl5EFzzRvTtDW+znFy+YStU3ahs=";
-    };
+  nativeBuildInputs = [
+    ocaml
+    findlib
+    ocamlbuild
+    topkg
+  ];
+  buildInputs = [
+    cmdliner
+    topkg
+  ];
 
-    nativeBuildInputs = [
-      ocaml
-      findlib
-      ocamlbuild
-      topkg
-    ];
-    buildInputs = [
-      cmdliner
-      topkg
-    ];
+  strictDeps = true;
 
-    strictDeps = true;
+  inherit (topkg) buildPhase installPhase;
 
-    inherit (topkg) buildPhase installPhase;
-
-    meta = with lib; {
-      homepage = "https://erratique.ch/software/fmt";
-      license = licenses.isc;
-      description = "OCaml Format pretty-printer combinators";
-      inherit (ocaml.meta) platforms;
-      maintainers = [ maintainers.vbgl ];
-    };
-  }
+  meta = with lib; {
+    homepage = "https://erratique.ch/software/fmt";
+    license = licenses.isc;
+    description = "OCaml Format pretty-printer combinators";
+    inherit (ocaml.meta) platforms;
+    maintainers = [ maintainers.vbgl ];
+    broken = lib.versionOlder ocaml.version "4.08";
+  };
+}
