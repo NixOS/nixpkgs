e2b7a8a02ce2142356781836092d5930dbb13911
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-19 06:40:48 +0000

    python3Packages.pyslim: 1.0.4 -> 1.1.0

--- pkgs/development/python-modules/pyslim/default.nix
+++ pkgs/development/python-modules/pyslim/default.nix
@@ -11,13 +11,13 @@
 
 buildPythonPackage rec {
   pname = "pyslim";
-  version = "1.0.4";
+  version = "1.1.0";
   format = "pyproject";
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-valAhPEVZNv/IYe85a88SGE+2/9O1omvBywz/HeeRco=";
+    hash = "sha256-T0ES0a7B4yTIUMXQgDEiIjxFJfZNmA7euTpa7JmryQU=";
   };
 
   nativeBuildInputs = [ setuptools-scm ];
