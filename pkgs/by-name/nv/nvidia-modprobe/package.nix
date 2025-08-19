b14631609135eaa240b38797af431a204d0687e9
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-19 04:02:33 +0000

    nvidia-modprobe: 580.65.06 -> 580.76.05

--- pkgs/by-name/nv/nvidia-modprobe/package.nix
+++ pkgs/by-name/nv/nvidia-modprobe/package.nix
@@ -6,13 +6,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "nvidia-modprobe";
-  version = "580.65.06";
+  version = "580.76.05";
 
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = "nvidia-modprobe";
     rev = version;
-    hash = "sha256-peEklk7lSnwz/RC6UlUEQf47clbTRL8M1xz8z4MgdHE=";
+    hash = "sha256-NGVd+uur4jYcnmhIuK5qy0ktm95da89adUQpJbRT3VI=";
   };
 
   nativeBuildInputs = [ gnum4 ];
