c3760f8d394697b93b0f5fc1dc48009d12acdb5c
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-19 06:13:20 +0000

    bookstack: 25.07 -> 25.07.1
    (cherry picked from commit 76c006aa46297a9eea3767d2929625edde008d26)

--- pkgs/by-name/bo/bookstack/package.nix
+++ pkgs/by-name/bo/bookstack/package.nix
@@ -8,16 +8,16 @@
 
 php83.buildComposerProject2 (finalAttrs: {
   pname = "bookstack";
-  version = "25.07";
+  version = "25.07.1";
 
   src = fetchFromGitHub {
     owner = "bookstackapp";
     repo = "bookstack";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-NlG5b/uiXplvV4opL6+SMDh4UhHAhN1wuGX7eyEnTew=";
+    hash = "sha256-POStEhG8c3gQyAT9KsRZYEafN3OrI3X/Esar/ZCBrZA=";
   };
 
-  vendorHash = "sha256-8wnNHFo+faut7qqHHy9/jvcUfLFD45uapBFGP4xAYFs=";
+  vendorHash = "sha256-LQKZjGabmAcpK1j+J5uwu/vCk3EHLB2UvuUpAr0pFxc=";
 
   passthru = {
     phpPackage = php83;
