5c7fc0c3e2344812e29ca544ec998d0eeb388d09
Yovko Lambrev <yovko@yovko.net>
2025-08-19 09:18:17 +0300

    go_1_24: 1.24.5 -> 1.24.6

--- pkgs/development/compilers/go/1.24.nix
+++ pkgs/development/compilers/go/1.24.nix
@@ -27,11 +27,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "go";
-  version = "1.24.5";
+  version = "1.24.6";
 
   src = fetchurl {
     url = "https://go.dev/dl/go${finalAttrs.version}.src.tar.gz";
-    hash = "sha256-dP2wnyNS4rJbeUPlaDbJtHNj0o3sHItWxKlXDzC49Z8=";
+    hash = "sha256-4ctVgqq1iGaLwEwH3hhogHD2uMmyqvNh+CHhm9R8/b0=";
   };
 
   strictDeps = true;
