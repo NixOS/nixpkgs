35b323261546300fdda4fa05f7fcb0897992f1c2
Benjamin Lemouzy <blemouzy.ml@gmail.com>
2025-08-19 11:51:43 +0200

    segger-jlink: 810 -> 860

--- pkgs/by-name/se/segger-jlink/source.nix
+++ pkgs/by-name/se/segger-jlink/source.nix
@@ -1,19 +1,19 @@
 {
-  version = "810";
+  version = "860";
   x86_64-linux = {
     name = "x86_64";
-    hash = "sha256-cnax7D4T5On356mZrd/waH62IXP6BNSJtCM0rw0RnDo=";
+    hash = "sha256-uT+TaZ0EW28cY70dPBaK/XS0kZcDaRAeJMvM1VW/8Ac=";
   };
   i686-linux = {
     name = "i386";
-    hash = "sha256-ERpzSVK7IxriDTH1wkLsiioqV99fRkMIlg0Pd+V+h7g=";
+    hash = "sha256-JWcOXfj4x3/osP2PZtKws368OY5RYfWz/QS44DFt33Q=";
   };
   aarch64-linux = {
     name = "arm64";
-    hash = "sha256-l+mxhK85GnGLKspXeEy2JOJ+fA8DVIr36tLVCBp7MhU=";
+    hash = "sha256-HQ8omRT22RNBGr/woMB5lhv1LLDY1qYMuZFVFMsc3cc=";
   };
   armv7l-linux = {
     name = "arm";
-    hash = "sha256-DFIaIq57HQbpur2vep1pO/VDOo913b17x3eWhq4wL40=";
+    hash = "sha256-AcNXQZgDaI9lSN8X9T9fv6soY8pvAJxcc/IFv4N0QUY=";
   };
 }
