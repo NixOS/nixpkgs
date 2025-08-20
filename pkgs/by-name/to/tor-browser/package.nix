8f6c3c698ac129056ffaad40f21700a2f6f46a05
C4 Patino <c4patino@gmail.com>
2025-08-19 20:22:59 -0500

    tor-browser: 14.5.5 -> 14.5.6
    (cherry picked from commit e41e1aea02539bb2d87c101ef5d4bd354702e6d3)

--- pkgs/by-name/to/tor-browser/package.nix
+++ pkgs/by-name/to/tor-browser/package.nix
@@ -109,7 +109,7 @@ lib.warnIf (useHardenedMalloc != null)
         ++ lib.optionals mediaSupport [ ffmpeg ]
       );
 
-      version = "14.5.5";
+      version = "14.5.6";
 
       sources = {
         x86_64-linux = fetchurl {
@@ -119,7 +119,7 @@ lib.warnIf (useHardenedMalloc != null)
             "https://tor.eff.org/dist/torbrowser/${version}/tor-browser-linux-x86_64-${version}.tar.xz"
             "https://tor.calyxinstitute.org/dist/torbrowser/${version}/tor-browser-linux-x86_64-${version}.tar.xz"
           ];
-          hash = "sha256-rJGhgSSktaeH7KSOtf1KjJbrl/m4sdz+9UdjUN9ovz0=";
+          hash = "sha256-GRLCWCCPixclqdk4UijfHqyDAJjx4eiMM7IwePSCZMI=";
         };
 
         i686-linux = fetchurl {
@@ -129,7 +129,7 @@ lib.warnIf (useHardenedMalloc != null)
             "https://tor.eff.org/dist/torbrowser/${version}/tor-browser-linux-i686-${version}.tar.xz"
             "https://tor.calyxinstitute.org/dist/torbrowser/${version}/tor-browser-linux-i686-${version}.tar.xz"
           ];
-          hash = "sha256-mvlx817/vLi4QyA0aSPyAuWSBfMLjfkFG9Zse9rmSzw=";
+          hash = "sha256-dhRPuMwtxzgA8DJdwct9oNjEOftFSS9Z9wP908wcwIw=";
         };
       };
 
