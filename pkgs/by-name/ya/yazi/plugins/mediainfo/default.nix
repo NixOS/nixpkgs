5bf001f262ee6a0262c8486d18d6ac204ca1f7a8
Austin Horstman <khaneliman12@gmail.com>
2025-08-19 21:52:57 -0500

    yaziPlugins: update on 2025-08-19
    - lazygit: 0-unstable-2025-03-31 → 0-unstable-2025-08-06
    - mediainfo: 25.5.31-unstable-2025-07-19 → 25.5.31-unstable-2025-08-06
    - mount: 25.5.31-unstable-2025-07-02 → 25.5.31-unstable-2025-08-11
    - restore: 25.5.31-unstable-2025-07-11 → 25.5.31-unstable-2025-08-12
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- pkgs/by-name/ya/yazi/plugins/mediainfo/default.nix
+++ pkgs/by-name/ya/yazi/plugins/mediainfo/default.nix
@@ -5,13 +5,13 @@
 }:
 mkYaziPlugin {
   pname = "mediainfo.yazi";
-  version = "25.5.31-unstable-2025-07-19";
+  version = "25.5.31-unstable-2025-08-06";
 
   src = fetchFromGitHub {
     owner = "boydaihungst";
     repo = "mediainfo.yazi";
-    rev = "f89605ce7ca33181ee6770e641d80ec4673093e0";
-    hash = "sha256-NloChkZWKo9JL636d+G7vgEY/HX24udngYftw/Ydzk4=";
+    rev = "0e2ae47cfb2b7c7a32d714c753b1cebbaa75d127";
+    hash = "sha256-CHigaujMHd1BuYyyxzI5B4ZYQhuH2YZptVVJToq39sY=";
   };
 
   meta = {
