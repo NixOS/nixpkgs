2965fa9c9a4c489292f2c56c7af56443b8b6b552
transcaffeine <transcaffeine@finally.coffee>
2025-08-20 16:50:04 +0200

    matrix-alertmanager-receiver: 2025.8.6 -> 2025.8.20
    Release notes: https://github.com/metio/matrix-alertmanager-receiver/releases/tag/2025.8.20
    Full changelog: https://github.com/metio/matrix-alertmanager-receiver/compare/2025.8.6...2025.8.20

--- pkgs/by-name/ma/matrix-alertmanager-receiver/package.nix
+++ pkgs/by-name/ma/matrix-alertmanager-receiver/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "matrix-alertmanager-receiver";
-  version = "2025.8.6";
+  version = "2025.8.20";
 
   src = fetchFromGitHub {
     owner = "metio";
     repo = "matrix-alertmanager-receiver";
     tag = finalAttrs.version;
-    hash = "sha256-rnGKpJppR7NoOAx/jGt7vxr1EVok3tMzkr9ry/k57L8=";
+    hash = "sha256-7mKpS80Iw35F+YiQujh80wGIjsxwcCEs8lDGLdq1v3U=";
   };
 
-  vendorHash = "sha256-JMjfrSdaN2zXgACkPddQ9h7SLV6jhpUvFTk56UfPWJg=";
+  vendorHash = "sha256-Y43bmnY9E9Mq3yjJ5XO+Ox+jjUmD2IB+SYdyn4IeXE4=";
 
   env.CGO_ENABLED = "0";
 
