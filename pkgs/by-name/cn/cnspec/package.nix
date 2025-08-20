5d71a099caa8b886b68e4b97cd30d8bded984726
Fabian Affolter <mail@fabian-affolter.ch>
2025-08-20 11:36:40 +0200

    cnspec: 11.67.1 -> 11.68.0
    Diff: https://github.com/mondoohq/cnspec/compare/v11.67.1...v11.68.0
    
    Changelog: https://github.com/mondoohq/cnspec/releases/tag/v11.68.0

--- pkgs/by-name/cn/cnspec/package.nix
+++ pkgs/by-name/cn/cnspec/package.nix
@@ -6,18 +6,18 @@
 
 buildGoModule rec {
   pname = "cnspec";
-  version = "11.67.1";
+  version = "11.68.0";
 
   src = fetchFromGitHub {
     owner = "mondoohq";
     repo = "cnspec";
     tag = "v${version}";
-    hash = "sha256-COVhnyZYJSUN3AUcZoHD97wrAkWvHlIbZmoMvaZ46Gw=";
+    hash = "sha256-OFV6xk8JbdmW0hj6UsrK/KW96kVNSCtzQ75dGLeuJCk=";
   };
 
   proxyVendor = true;
 
-  vendorHash = "sha256-wVltxYhh+VepkpvJrORCG3TxDBnpgV6weJH90RJwA1U=";
+  vendorHash = "sha256-vVCpA7ete2XPcwSZLmtcmdcc467g+KI5zXbZVjZsur4=";
 
   subPackages = [ "apps/cnspec" ];
 
