e94c9550dc4797cc0e832465d6b78667d20fedd6
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-21 15:50:33 +0000

    heptabase: 1.69.0 -> 1.70.1

--- pkgs/by-name/he/heptabase/package.nix
+++ pkgs/by-name/he/heptabase/package.nix
@@ -5,10 +5,10 @@
 }:
 let
   pname = "heptabase";
-  version = "1.69.0";
+  version = "1.70.1";
   src = fetchurl {
     url = "https://github.com/heptameta/project-meta/releases/download/v${version}/Heptabase-${version}.AppImage";
-    hash = "sha256-QB2N/RJ4o6IN25qSRbiB69/qGHEKA4GRbLdMYS2cRIQ=";
+    hash = "sha256-2soe9hBFlYiWkSBcLSKc6dNDn3EnZWFgDRgFu16mKHM=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
