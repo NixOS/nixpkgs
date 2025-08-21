8b9ae6bd832fa6d1691d4034876ddf514e0faf51
Robert Schütz <nix@dotlambda.de>
2025-08-20 21:54:16 -0700

    libadwaita: 1.7.5 -> 1.7.6
    Diff: https://gitlab.gnome.org/GNOME/libadwaita/-/compare/1.7.5...1.7.6
    
    Changelog: https://gitlab.gnome.org/GNOME/libadwaita/-/blob/1.7.6/NEWS

--- pkgs/by-name/li/libadwaita/package.nix
+++ pkgs/by-name/li/libadwaita/package.nix
@@ -23,7 +23,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libadwaita";
-  version = "1.7.5";
+  version = "1.7.6";
 
   outputs = [
     "out"
@@ -37,7 +37,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "GNOME";
     repo = "libadwaita";
     tag = finalAttrs.version;
-    hash = "sha256-KlaRwOWHvzm+VwMygiEh8wqJfEwwA+x7o9T72Qpqnmo=";
+    hash = "sha256-HpjP6VSkEAFeXIFXLbndQzEWJwVvHe6B3aSwCz6KiIM=";
   };
 
   depsBuildBuild = [
