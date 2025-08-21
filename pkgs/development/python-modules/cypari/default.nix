aa47475c612b2c9832defa057acad7a688b38d45
Alex Epelde <alex@epelde.net>
2025-08-20 20:42:49 -0400

    python3Packages.cypari: build gmp and pari separately

--- pkgs/development/python-modules/cypari/default.nix
+++ pkgs/development/python-modules/cypari/default.nix
@@ -3,29 +3,13 @@
   python,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchurl,
   setuptools,
   cython,
-  bash,
+  gmp,
+  pari,
   perl,
-  gnum4,
-  texliveBasic,
 }:
 
-let
-  pariVersion = "2.15.4";
-  gmpVersion = "6.3.0";
-
-  pariSrc = fetchurl {
-    url = "https://pari.math.u-bordeaux.fr/pub/pari/OLD/${lib.versions.majorMinor pariVersion}/pari-${pariVersion}.tar.gz";
-    hash = "sha256-w1Rb/uDG37QLd/tLurr5mdguYAabn20ovLbPAEyMXA8=";
-  };
-
-  gmpSrc = fetchurl {
-    url = "https://ftp.gnu.org/gnu/gmp/gmp-${gmpVersion}.tar.bz2";
-    hash = "sha256-rCghGnz7YJuuLiyNYFjWbI/pZDT3QM9v4uR7AA0cIMs=";
-  };
-in
 buildPythonPackage rec {
   pname = "cypari";
   version = "2.5.5";
@@ -39,13 +23,15 @@ buildPythonPackage rec {
   };
 
   postPatch = ''
-    substituteInPlace ./setup.py \
-      --replace-fail "/bin/bash" "${lib.getExe bash}"
     # final character is stripped from PARI error messages for some reason
     substituteInPlace ./cypari/handle_error.pyx \
       --replace-fail "not a function in function call" "not a function in function cal"
-    ln -s ${pariSrc} ${pariSrc.name}
-    ln -s ${gmpSrc} ${gmpSrc.name}
+  '';
+
+  preBuild = ''
+    mkdir libcache
+    ln -s ${gmp} libcache/gmp
+    ln -s ${pari} libcache/pari
   '';
 
   build-system = [
@@ -53,12 +39,8 @@ buildPythonPackage rec {
     cython
   ];
 
-  NIX_LDFLAGS = "-lc";
-
   nativeBuildInputs = [
-    gnum4
     perl
-    texliveBasic
   ];
 
   pythonImportsCheck = [ "cypari" ];
