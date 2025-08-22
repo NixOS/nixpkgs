cf9404b31580f213935838e10312ebcce118d059
wrrrzr <wozrer@proton.me>
2025-08-22 16:30:40 +0300

    nixos/byedpi: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1094,6 +1094,7 @@
   ./services/networking/bitlbee.nix
   ./services/networking/blockbook-frontend.nix
   ./services/networking/blocky.nix
+  ./services/networking/byedpi.nix
   ./services/networking/cato-client.nix
   ./services/networking/centrifugo.nix
   ./services/networking/cgit.nix
