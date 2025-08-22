4804fbc0badb5c4e058c271d57405913278b1f69
Tom Alexander <tom@fizz.buzz>
2025-08-22 18:28:39 -0400

    cysystemd: init at 2.0.1
    cysystemd is a python library for interacting with systemd. It is being added as a dependency of amd-debug-tools.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3282,6 +3282,8 @@ self: super: with self; {
 
   cysignals = callPackage ../development/python-modules/cysignals { };
 
+  cysystemd = callPackage ../development/python-modules/cysystemd { };
+
   cython = callPackage ../development/python-modules/cython { };
 
   cython-test-exception-raiser =
