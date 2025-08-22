89561f612470707f64742e8f11598f7a2a638d4e
Marie Ramlow <me@nycode.dev>
2025-08-22 17:01:23 +0200

    nghttp3: replace fetchFromGitHub with fetchurl
    Otherwise we run into infrec.

--- pkgs/by-name/ng/nghttp3/package.nix
+++ pkgs/by-name/ng/nghttp3/package.nix
@@ -1,7 +1,7 @@
 {
   lib,
   stdenv,
-  fetchFromGitHub,
+  fetchurl,
   cmake,
   curlHTTP3,
 }:
@@ -10,12 +10,9 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "nghttp3";
   version = "1.11.0";
 
-  src = fetchFromGitHub {
-    owner = "ngtcp2";
-    repo = "nghttp3";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-8WQfXzzF3K0IJNectrE1amQ6Njq4pZslrcVun6Uhi6E=";
-    fetchSubmodules = true;
+  src = fetchurl {
+    url = "https://github.com/ngtcp2/nghttp3/releases/download/v${finalAttrs.version}/nghttp3-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-AAKlyoVtsFmqbcac9zL7sA2aHnPteISPXUjyYh8gyoo=";
   };
 
   outputs = [
@@ -38,7 +35,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     homepage = "https://github.com/ngtcp2/nghttp3";
-    changelog = "https://github.com/ngtcp2/nghttp3/releases/tag/${finalAttrs.src.tag}";
+    changelog = "https://github.com/ngtcp2/nghttp3/releases/tag/v${finalAttrs.version}";
     description = "Implementation of HTTP/3 mapping over QUIC and QPACK in C";
     license = lib.licenses.mit;
     platforms = lib.platforms.unix;
