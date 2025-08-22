731517d12867fb8d8f97cbeb12eb3a5ab11fd9a4
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-22 20:41:08 +0200

    listres: refactor and move to pkgs/by-name from xorg namespace

--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -356,6 +356,7 @@ print OUT <<EOF;
   libxxf86dga,
   libxxf86misc,
   libxxf86vm,
+  listres,
   lndir,
   luit,
   makedepend,
@@ -397,6 +398,7 @@ self: with self; {
     libpciaccess
     libxcb
     libxcvt
+    listres
     lndir
     luit
     makedepend
