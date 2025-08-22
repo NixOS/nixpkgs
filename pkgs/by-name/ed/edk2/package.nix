6df082c53730035bd6921e1d13586f1a40ebd495
Adam C. Stephens <adam@valkor.net>
2025-08-22 09:50:26 -0400

    edk2: 202505 -> 202508
    Changelog: https://github.com/tianocore/edk2/releases/tag/edk2-stable202508

--- pkgs/by-name/ed/edk2/package.nix
+++ pkgs/by-name/ed/edk2/package.nix
@@ -33,14 +33,14 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "edk2";
-  version = "202505";
+  version = "202508";
 
   srcWithVendoring = fetchFromGitHub {
     owner = "tianocore";
     repo = "edk2";
     tag = "edk2-stable${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-VuiEqVpG/k7pfy0cOC6XmY+8NBtU/OHdDB9Y52tyNe8=";
+    hash = "sha256-YZcjPGPkUQ9CeJS9JxdHBmpdHsAj7T0ifSZWZKyNPMk=";
   };
 
   src = applyPatches {
@@ -53,12 +53,8 @@ stdenv.mkDerivation (finalAttrs: {
         url = "https://src.fedoraproject.org/rpms/edk2/raw/08f2354cd280b4ce5a7888aa85cf520e042955c3/f/0021-Tweak-the-tools_def-to-support-cross-compiling.patch";
         hash = "sha256-E1/fiFNVx0aB1kOej2DJ2DlBIs9tAAcxoedym2Zhjxw=";
       })
-      # https://github.com/tianocore/edk2/pull/5658
-      (fetchpatch {
-        name = "fix-cross-compilation-antlr-dlg.patch";
-        url = "https://github.com/tianocore/edk2/commit/a34ff4a8f69a7b8a52b9b299153a8fac702c7df1.patch";
-        hash = "sha256-u+niqwjuLV5tNPykW4xhb7PW2XvUmXhx5uvftG1UIbU=";
-      })
+
+      ./fix-cross-compilation-antlr-dlg.patch
     ];
 
     # FIXME: unvendor OpenSSL again once upstream updates
@@ -86,10 +82,13 @@ stdenv.mkDerivation (finalAttrs: {
 
   makeFlags = [ "-C BaseTools" ];
 
-  env.NIX_CFLAGS_COMPILE =
-    "-Wno-return-type"
-    + lib.optionalString (stdenv.cc.isGNU) " -Wno-error=stringop-truncation"
-    + lib.optionalString (stdenv.hostPlatform.isDarwin) " -Wno-error=macro-redefined";
+  env = {
+    NIX_CFLAGS_COMPILE =
+      "-Wno-return-type"
+      + lib.optionalString (stdenv.cc.isGNU) " -Wno-error=stringop-truncation"
+      + lib.optionalString (stdenv.hostPlatform.isDarwin) " -Wno-error=macro-redefined";
+    PYTHON_COMMAND = lib.getExe pythonEnv;
+  };
 
   hardeningDisable = [
     "format"
