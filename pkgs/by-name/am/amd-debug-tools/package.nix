de37cec50e8674c39bd900154e1f83f8bebfd7af
Tom Alexander <tom@fizz.buzz>
2025-08-22 18:30:42 -0400

    amd-debug-tools: init at 0.2.7
    amd-debug-tools contains a set of scripts that can be used to test/debug behavior on machines with AMD processors. This extends to s2idle sleep, pstates, and BIOS debugging.

---
+++ pkgs/by-name/am/amd-debug-tools/package.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  fetchgit,
+  python3Packages,
+  acpica-tools,
+  ethtool,
+  libdisplay-info,
+}:
+
+let
+  version = "0.2.7";
+in
+python3Packages.buildPythonApplication {
+  pname = "amd-debug-tools";
+  inherit version;
+  pyproject = true;
+
+  build-system = with python3Packages; [
+    setuptools
+    setuptools-git-versioning
+    setuptools-git
+    pyudev
+  ];
+  dependencies = with python3Packages; [
+    cysystemd
+    jinja2
+    matplotlib
+    pandas
+    pyudev
+    seaborn
+    tabulate
+    acpica-tools
+    ethtool
+    libdisplay-info
+  ];
+  src = fetchgit {
+    url = "https://git.kernel.org/pub/scm/linux/kernel/git/superm1/amd-debug-tools.git";
+    tag = version;
+    hash = "sha256-6X9cUKN0BkkKcYGU+YJYCGT+l5iUZDN+D8Fqq/ns98Q=";
+    leaveDotGit = true;
+  };
+
+  disabled = python3Packages.pythonOlder "3.7";
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail ', "setuptools-git-versioning>=2.0,<3"' ""
+  '';
+
+  pythonImportsCheck = [ "amd_debug" ];
+
+  meta = {
+    description = "Debug tools for AMD zen systems";
+    homepage = "https://git.kernel.org/pub/scm/linux/kernel/git/superm1/amd-debug-tools.git/";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
+  };
+}
