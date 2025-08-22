df532bdbfa04dfae201aee347810cce5fdc6c06e
Marie Ramlow <me@nycode.dev>
2025-08-22 17:01:23 +0200

    ngtcp2: replace fetchFromGitHub with fetchurl
    Otherwise we run into infrec

--- pkgs/development/libraries/ngtcp2/default.nix
+++ pkgs/development/libraries/ngtcp2/default.nix
@@ -1,7 +1,7 @@
 {
   lib,
   stdenv,
-  fetchFromGitHub,
+  fetchurl,
   cmake,
   brotli,
   libev,
@@ -16,13 +16,9 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "ngtcp2";
   version = "1.14.0";
 
-  src = fetchFromGitHub {
-    owner = "ngtcp2";
-    repo = "ngtcp2";
-    # must match version usage in meta.changelog
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-5Pmk752i/lgO/os2SegevGN+MKaVuQii2HrVWaR15Gg=";
-    fetchSubmodules = true;
+  src = fetchurl {
+    url = "https://github.com/ngtcp2/ngtcp2/releases/download/v${finalAttrs.version}/ngtcp2-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-I+Q2UvVwKzGm53S5ON2XtqAyW8UiyUM4R+bG/BYBvrU=";
   };
 
   outputs = [
