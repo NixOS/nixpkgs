7e8bf18621aea1161ffabeb073c7343402f70a9d
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-22 05:46:09 +0000

    python3Packages.archinfo: 9.2.154 -> 9.2.171

--- pkgs/development/python-modules/archinfo/default.nix
+++ pkgs/development/python-modules/archinfo/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "archinfo";
-  version = "9.2.154";
+  version = "9.2.171";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "archinfo";
     tag = "v${version}";
-    hash = "sha256-Vks7Rjd8x2zeHnJPs0laH56S4b8pnR1cK82SpK+XOgE=";
+    hash = "sha256-m3Qx/q+07G8iEpRjfJP5Z8Kdig7ocYS/DjB1RjCHdJs=";
   };
 
   build-system = [ setuptools ];
