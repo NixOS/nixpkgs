e46d5a1bba48d6be3a7786fae773a7904963c20f
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-22 20:30:31 +0200

    python312Packages.mypy-boto3-apigatewaymanagementapi: 1.40.0 -> 1.40.15

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -82,8 +82,8 @@ rec {
       "sha256-mfMTQ3XSVHDjTjQEY/EL1xq4t0KRaPwG2Nu0Pwsbk3o=";
 
   mypy-boto3-apigatewaymanagementapi =
-    buildMypyBoto3Package "apigatewaymanagementapi" "1.40.0"
-      "sha256-wt5RDgTkJZv+GZURGu98gGJRvM0a63JTePQ9aDrwLaE=";
+    buildMypyBoto3Package "apigatewaymanagementapi" "1.40.15"
+      "sha256-E7R5SOWEYyulF1Jh17x7iL89ucc0KpmDnOWo4thHFuk=";
 
   mypy-boto3-apigatewayv2 =
     buildMypyBoto3Package "apigatewayv2" "1.40.0"
