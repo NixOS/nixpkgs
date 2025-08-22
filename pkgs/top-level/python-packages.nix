f51a786cff2607ecd4c6653d909908ef65004227
Jamie Magee <jamie.magee@gmail.com>
2025-08-22 00:17:02 -0700

    python3Packages.rfk101py: init at 0.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15804,6 +15804,8 @@ self: super: with self; {
 
   rfcat = callPackage ../development/python-modules/rfcat { };
 
+  rfk101py = callPackage ../development/python-modules/rfk101py { };
+
   rflink = callPackage ../development/python-modules/rflink { };
 
   rgpio = toPythonModule (
