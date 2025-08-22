613db841c42cdd50de51c2dd36a877531602444c
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-22 08:04:15 +0000

    profanity: 0.15.0 -> 0.15.1

--- pkgs/applications/networking/instant-messengers/profanity/default.nix
+++ pkgs/applications/networking/instant-messengers/profanity/default.nix
@@ -39,13 +39,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "profanity";
-  version = "0.15.0";
+  version = "0.15.1";
 
   src = fetchFromGitHub {
     owner = "profanity-im";
     repo = "profanity";
     rev = finalAttrs.version;
-    hash = "sha256-3TmnbTnL8SPSd3seThavOOJVELi8kWLSlZlAub24KZ4=";
+    hash = "sha256-h+R+hasc45NZOneuqZ+z+yjfpsPm317OXq0LYe3t+cQ=";
   };
 
   patches = [
