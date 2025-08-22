cbfa9d3a813fb6d5127f8de0de179628d3300e23
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-22 19:10:37 +0000

    asciinema-automation: 0.2.0 -> 0.2.2

--- pkgs/by-name/as/asciinema-automation/package.nix
+++ pkgs/by-name/as/asciinema-automation/package.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "asciinema-automation";
-  version = "0.2.0";
+  version = "0.2.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "PierreMarchand20";
     repo = "asciinema_automation";
     rev = "v${version}";
-    hash = "sha256-SjUsPrhoNz2NTPAkmUH0Csv6hbDc6nyKdckGqV5S3GY=";
+    hash = "sha256-VfIr7w/5hQwr6XYuC3f8h71uScr0lBdx6PyO1hpiZhA=";
   };
 
   nativeBuildInputs = [
