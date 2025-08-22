28dc7132f648f90436a6532d0775ff95343a06bb
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-22 19:57:27 +0000

    python3Packages.eve: 2.2.0 -> 2.2.1

--- pkgs/development/python-modules/eve/default.nix
+++ pkgs/development/python-modules/eve/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "eve";
-  version = "2.2.0";
+  version = "2.2.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "pyeve";
     repo = "eve";
     tag = "v${version}";
-    hash = "sha256-BLDuJLAN6ieaD7vBPV6AwlpPqbAyb+LzEp9AfCHveSY=";
+    hash = "sha256-TZeRvuJflht36PvctkfuQL1hMPqjtWDUxqk06DRFfyM=";
   };
 
   pythonRelaxDeps = [ "events" ];
