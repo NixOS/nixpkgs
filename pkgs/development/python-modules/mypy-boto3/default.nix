679dbc23665c29fa8829863629a5d80cc2874c60
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-22 20:32:03 +0200

    python312Packages.mypy-boto3-route53-recovery-control-config: 1.40.0 -> 1.40.14

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1142,8 +1142,8 @@ rec {
       "sha256-bl9tBA5QmntHCYMcYhLW8N8w+oTTStr33J5C8kbXnZs=";
 
   mypy-boto3-route53-recovery-control-config =
-    buildMypyBoto3Package "route53-recovery-control-config" "1.40.0"
-      "sha256-4o3SPst6sQ91GZsH6JEVjk2PqPc7zUYQpdQxM/Ki3VI=";
+    buildMypyBoto3Package "route53-recovery-control-config" "1.40.14"
+      "sha256-xHo8vLsSgGMCM3uVYv1ihLAOpkSc3XjuPgKWLFTqDkk=";
 
   mypy-boto3-route53-recovery-readiness =
     buildMypyBoto3Package "route53-recovery-readiness" "1.40.0"
