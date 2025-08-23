0f2ff8a9ed5903701ec917479c2394d6ba3d4614
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-23 15:48:45 +0200

    libxpresent: refactor, move to pkgs/by-name and rename from xorg.libXpresent
    relevant changes:
    - add a few propagatedBuildInputs that are includes of libxpresents
      header file

--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -69,6 +69,7 @@ $pcMap{"xmu"} = "libXmu";
 $pcMap{"xmuu"} = "libXmu";
 $pcMap{"xp"} = "libXp";
 $pcMap{"xpm"} = "libXpm";
+$pcMap{"xpresent"} = "libXpresent";
 $pcMap{"xrandr"} = "libXrandr";
 $pcMap{"xrender"} = "libXrender";
 $pcMap{"xt"} = "libXt";
@@ -354,6 +355,7 @@ print OUT <<EOF;
   libxmu,
   libxp,
   libxpm,
+  libxpresent,
   libxrandr,
   libxrender,
   libxt,
@@ -455,6 +457,7 @@ self: with self; {
   libXmu = libxmu;
   libXp = libxp;
   libXpm = libxpm;
+  libXpresent = libxpresent;
   libXrandr = libxrandr;
   libXrender = libxrender;
   libXt = libxt;
