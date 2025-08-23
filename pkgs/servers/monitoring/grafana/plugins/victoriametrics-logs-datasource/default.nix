56fd2b520d188f90283c8392cab8e20aaa1511dc
Shawn8901 <shawn8901@googlemail.com>
2025-08-23 16:28:48 +0200

    grafanaPlugins.victoriametrics-logs-datasource: 0.19.2 -> 0.19.3

--- pkgs/servers/monitoring/grafana/plugins/victoriametrics-logs-datasource/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/victoriametrics-logs-datasource/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "victoriametrics-logs-datasource";
-  version = "0.19.2";
-  zipHash = "sha256-+mGW9A39GioPKW5j2vT2aNKrBc/A6qsaeIjo4EUrXs4=";
+  version = "0.19.3";
+  zipHash = "sha256-UMHcH4o6/Wr7Wct977HdOiXgvXo0j9LfZxPcRCqG2+U=";
   meta = {
     description = "Grafana datasource for VictoriaLogs";
     license = lib.licenses.asl20;
