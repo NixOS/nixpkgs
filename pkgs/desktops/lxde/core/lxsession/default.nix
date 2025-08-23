7d92766509bac98d1e07ed65dab41666a7cf4396
Sigmanificient <edhyjox@gmail.com>
2025-08-24 00:52:25 +0200

    lxde.lxsession: 0.5.5 -> 0.5.6

--- pkgs/desktops/lxde/core/lxsession/default.nix
+++ pkgs/desktops/lxde/core/lxsession/default.nix
@@ -1,10 +1,8 @@
 {
   lib,
   stdenv,
-  fetchpatch,
   fetchFromGitHub,
-  autoconf,
-  automake,
+  autoreconfHook,
   docbook_xml_dtd_412,
   docbook_xsl,
   intltool,
@@ -18,39 +16,26 @@
   vala,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "lxsession";
-  version = "0.5.5";
+  version = "0.5.6";
 
   src = fetchFromGitHub {
     owner = "lxde";
     repo = "lxsession";
-    rev = version;
-    sha256 = "17sqsx57ymrimm5jfmcyrp7b0nzi41bcvpxsqckmwbhl19g6c17d";
+    tag = finalAttrs.version;
+    hash = "sha256-3RnRF4oMCtZbIraHVqEPnkviAkELq7uYqyHY0uCf/lU=";
   };
 
-  patches = [
-    ./xmlcatalog_patch.patch
-
-    # lxsession compilation is broken upstream as of GCC 14
-    # https://sourceforge.net/p/lxde/bugs/973/
-    (fetchpatch {
-      name = "0001-Fix-build-on-GCC-14.patch";
-      url = "https://sourceforge.net/p/lxde/bugs/973/attachment/0001-Fix-build-on-GCC-14.patch";
-      hash = "sha256-lxF3HZy5uLK7Cfu8W1A03syZf7OWXpHiU2Fk+xBl39g=";
-    })
-  ];
-
   nativeBuildInputs = [
-    autoconf
-    automake
-    docbook_xml_dtd_412
-    docbook_xsl
+    autoreconfHook
     intltool
     libxml2
     libxslt
     pkg-config
     wrapGAppsHook3
+    docbook_xml_dtd_412
+    docbook_xsl
   ];
 
   buildInputs = [
@@ -64,16 +49,20 @@ stdenv.mkDerivation rec {
     "--enable-man"
     "--disable-buildin-clipboard"
     "--disable-buildin-polkit"
-    "--with-xml-catalog=${docbook_xml_dtd_412}/xml/dtd/docbook/catalog.xml"
   ];
 
-  preConfigure = "./autogen.sh";
+  postPatch = ''
+    mkdir -p m4
+  '';
 
-  meta = with lib; {
+  patches = [ ./repect-xml-catalog-file-var.patch ];
+
+  meta = {
     homepage = "https://wiki.lxde.org/en/LXSession";
     description = "Classic LXDE session manager";
-    license = licenses.gpl2Plus;
-    maintainers = [ maintainers.shamilton ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl2Plus;
+    maintainers = [ lib.maintainers.shamilton ];
+    platforms = lib.platforms.linux;
+    mainProgram = "lxsession";
   };
-}
+})
