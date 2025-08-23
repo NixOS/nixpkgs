267f471e940e60fef1fd760e277df8a5d33020bc
Maximilian Bosch <maximilian@mbosch.me>
2025-08-23 17:20:28 +0200

    linux: fix backwards-compat of structuredExtraConfig vs extraStructuredConfig
    Follow-up on #431115: the change from extraStructuredConfig
    to structuredExtraConfig is breaking. This patch accepts both variants
    (but not at the same time) and gives an eval-time warning about the
    hard-rename in unstable/25.11.

--- pkgs/os-specific/linux/kernel/generic.nix
+++ pkgs/os-specific/linux/kernel/generic.nix
@@ -137,10 +137,23 @@ let
       structuredConfigFromPatches = map (
         {
           structuredExtraConfig ? { },
+          extraStructuredConfig ? { },
           ...
         }:
         {
-          settings = structuredExtraConfig;
+          settings =
+            if extraStructuredConfig != { } then
+              assert lib.assertMsg (structuredExtraConfig == { }) ''
+                Using both `extraStructuredConfig` & `structuredExtraConfig` for a single kernel
+                patch is not supported!
+              '';
+              lib.warn ''
+                Passing `extraStructuredConfig` to the Linux kernel build
+                (e.g. via `boot.kernelPatches` in NixOS) is deprecated and will fail at
+                evaluation in 25.11. Use `structuredExtraConfig` instead.
+              '' extraStructuredConfig
+            else
+              structuredExtraConfig;
         }
       ) kernelPatches;
 
