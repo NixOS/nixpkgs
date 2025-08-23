5e3532545cfeb2c1605ded00074349dd2503f358
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-23 07:01:15 +0200

    libxcomposite: refactor, move to pkgs/by-name and rename from xorg.libXcomposite

--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -61,6 +61,7 @@ $pcMap{"xcb-keysyms"} = "xcbutilkeysyms";
 $pcMap{"xcb-proto"} = "xcbproto";
 $pcMap{"xcb-renderutil"} = "xcbutilrenderutil";
 $pcMap{"xcb-util"} = "xcbutil";
+$pcMap{"xcomposite"} = "libXcomposite";
 $pcMap{"xcursor"} = "libXcursor";
 $pcMap{"xdmcp"} = "libXdmcp";
 $pcMap{"xext"} = "libXext";
@@ -341,8 +342,9 @@ print OUT <<EOF;
   libxcb-render-util,
   libxcb-util,
   libxcb-wm,
-  libxcvt,
+  libxcomposite,
   libxcursor,
+  libxcvt,
   libxdmcp,
   libxext,
   libxfixes,
@@ -427,6 +429,7 @@ self: with self; {
   libX11 = libx11;
   libXau = libxau;
   libXaw = libxaw;
+  libXcomposite = libxcomposite;
   libXcursor = libxcursor;
   libXdmcp = libxdmcp;
   libXext = libxext;
