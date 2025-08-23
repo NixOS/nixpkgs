285958b65a588cdc937a98b5fc8ad3691d389d04
Bobby Rong <rjl931189261@126.com>
2025-08-23 03:44:06 +0000

    jsonrpc-glib: 3.44.1 → 3.44.2
    https://gitlab.gnome.org/GNOME/jsonrpc-glib/-/compare/3.44.1...3.44.2

--- pkgs/by-name/js/jsonrpc-glib/package.nix
+++ pkgs/by-name/js/jsonrpc-glib/package.nix
@@ -15,7 +15,7 @@
 
 stdenv.mkDerivation rec {
   pname = "jsonrpc-glib";
-  version = "3.44.1";
+  version = "3.44.2";
 
   outputs = [
     "out"
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "E2HRfpyAVkav5RAuWbr4ykUCOGAPyr0BWGxlS3i7MN8=";
+    sha256 = "llSWtuExTzRotIKl2ANA3DsDQKVALXeDytJBVK7nc5Y=";
   };
 
   nativeBuildInputs = [
