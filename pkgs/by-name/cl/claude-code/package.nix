d91402763eaa74b13551619201d4b5185e94f700
André Silva <andrerfosilva@gmail.com>
2025-08-23 10:35:57 +0100

    claude-code: 1.0.88 -> 1.0.89

--- pkgs/by-name/cl/claude-code/package.nix
+++ pkgs/by-name/cl/claude-code/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "1.0.88";
+  version = "1.0.89";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-o0A9P0sBB2Fk18CArGGv/QBi55ZtFgoJ2/3gHlDwyEU=";
+    hash = "sha256-ULOL4emd7VZNvHD5Hk3YI1Cyb1Q1YMCcDDxlRDUM1dc=";
   };
 
-  npmDepsHash = "sha256-KONgl7DYG8Fj7JM6+hUZBQ+ETTNAPEimdIgseQ1bptQ=";
+  npmDepsHash = "sha256-qoPyOrtuYTLwnqROVqjXapKgaGb7k31/hBwEA0V9FUQ=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
