3a2ece762eb14a50010435401848ac4409ceb08b
K900 <me@0upti.me>
2025-08-23 09:49:27 +0300

    nixos/all-tests: delete deleted test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1187,7 +1187,6 @@ in
   pingvin-share = runTest ./pingvin-share.nix;
   pinnwand = runTest ./pinnwand.nix;
   plantuml-server = runTest ./plantuml-server.nix;
-  plasma-bigscreen = runTest ./plasma-bigscreen.nix;
   plasma6 = runTest ./plasma6.nix;
   plausible = runTest ./plausible.nix;
   playwright-python = runTest ./playwright-python.nix;
