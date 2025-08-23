454c7a68de7db56d20509aadce8c9368f2721043
Yureka <yureka@cyberchaos.dev>
2025-08-23 19:05:58 +0200

    python3.pkgs.rencode: fix build on aarch64-linux

--- pkgs/development/python-modules/rencode/default.nix
+++ pkgs/development/python-modules/rencode/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   cython,
   poetry-core,
   setuptools,
@@ -19,6 +20,19 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-k2b6DoKwNeQBkmqSRXqaRTjK7CVX6IKuXCLG9lBdLLY=";
   };
+
+  patches = [
+    # backport fix for -msse being passed on aarch64-linux
+    (fetchpatch {
+      url = "https://github.com/aresch/rencode/commit/591b9f4d85d7e2d4f4e99441475ef15366389be2.patch";
+      hash = "sha256-KhfawtYa4CnYiVzBYdtMn/JRkeqCLJetHvLEm1YVOe4=";
+    })
+    # do not pass -march=native etc. on x86_64
+    (fetchpatch {
+      url = "https://github.com/aresch/rencode/commit/e7ec8ea718e73a8fee7dbc007c262e1584f7f94b.patch";
+      hash = "sha256-gNYjxBsMN1p4IAmutV73JF8yCj0iz3DIl7kg7WrBdbs=";
+    })
+  ];
 
   nativeBuildInputs = [
     poetry-core
