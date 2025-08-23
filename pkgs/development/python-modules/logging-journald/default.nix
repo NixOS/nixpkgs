bfcb3cc87217777dc4ef362339b1133ab7c936b1
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-23 08:11:59 +0000

    python3Packages.logging-journald: 0.6.7 -> 0.6.11

--- pkgs/development/python-modules/logging-journald/default.nix
+++ pkgs/development/python-modules/logging-journald/default.nix
@@ -8,16 +8,16 @@
 
 buildPythonPackage rec {
   pname = "logging-journald";
-  version = "0.6.7";
-  format = "pyproject";
+  version = "0.6.11";
+  pyproject = true;
 
-  disabled = pythonOlder "3.7";
+  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "mosquito";
     repo = "logging-journald";
     tag = version;
-    hash = "sha256-RQ9opkAOZfhYuqOXJ2Mtnig8soL+lCveYH2YdXL1AGM=";
+    hash = "sha256-L68Trq4dii25sYr3Fm2aU8w9nzpkU2F6t3OeNnny0mE=";
   };
 
   nativeBuildInputs = [ poetry-core ];
