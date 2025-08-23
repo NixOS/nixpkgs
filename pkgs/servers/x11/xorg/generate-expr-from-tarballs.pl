7e96b523d10942786a3b8371ef0eafabd1231a87
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-23 21:40:15 +0200

    appres: refactor and move to pkgs/by-name from xorg namespace

--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -317,6 +317,7 @@ print OUT <<EOF;
 # THIS IS A GENERATED FILE.  DO NOT EDIT!
 {
   lib,
+  appres,
   bdftopcf,
   font-alias,
   font-bh-ttf,
@@ -397,6 +398,7 @@ print OUT <<EOF;
 self: with self; {
 
   inherit
+    appres
     bdftopcf
     gccmakedep
     ico
