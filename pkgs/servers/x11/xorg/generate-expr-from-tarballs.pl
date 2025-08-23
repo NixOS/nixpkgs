ba8ae19dc7a1468957fe77e29f012d3fb7207f78
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-23 13:16:31 +0200

    libxft: refactor, move to pkgs/by-name and rename from xorg.libXft
    relevant changes:
    - remove the patched in 'Requires: freetype2' as it does not matter
      since the dependency is propagated anyway
    - remove 'inherit freetype fontconfig' from passthru because that was
      probably there through an error anyways. It was added here
      https://github.com/NixOS/nixpkgs/commit/9454f71fefcbe413fdfcb61b510256f1301c5f5c

--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -70,6 +70,7 @@ $pcMap{"xext"} = "libXext";
 $pcMap{"xfixes"} = "libXfixes";
 $pcMap{"xfont"} = "libXfont";
 $pcMap{"xfont2"} = "libXfont2";
+$pcMap{"xft"} = "libXft";
 $pcMap{"xmu"} = "libXmu";
 $pcMap{"xmuu"} = "libXmu";
 $pcMap{"xpm"} = "libXpm";
@@ -365,6 +366,7 @@ print OUT <<EOF;
   libxfixes,
   libxfont_1,
   libxfont_2,
+  libxft,
   libxmu,
   libxpm,
   libxrandr,
@@ -513,6 +515,7 @@ self: with self; {
   libXfixes = libxfixes;
   libXfont2 = libxfont_2;
   libXfont = libxfont_1;
+  libXft = libxft;
   libXmu = libxmu;
   libXpm = libxpm;
   libXrandr = libxrandr;
