18ab95eaf95e0508311370528585119a9a8e18f5
Sigmanificient <edhyjox@gmail.com>
2025-08-23 11:07:54 +0200

    menu-cache: 1.1.0 -> 1.1.1

--- pkgs/by-name/me/menu-cache/package.nix
+++ pkgs/by-name/me/menu-cache/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   glib,
   pkg-config,
   libfm-extra,
@@ -12,24 +11,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "menu-cache";
-  version = "1.1.0";
+  version = "1.1.1";
 
   src = fetchFromGitHub {
     owner = "lxde";
     repo = "menu-cache";
     tag = finalAttrs.version;
-    hash = "sha256-UPSBAoDjI4nwyDsGK5yIrAR03WhGMSxv1IePSQ3SzxE=";
+    hash = "sha256-5Vp2btrflimy+Hq+3MLpic/quZMJ3uwsMq12G7s4DGI=";
   };
-
-  patches = [
-    # Pull patch pending upstream inclusion for -fno-common toolchain support:
-    #   https://github.com/lxde/menu-cache/pull/19
-    (fetchpatch {
-      name = "fno-common.patch";
-      url = "https://github.com/lxde/menu-cache/commit/1ce739649b4d66339a03fc0ec9ee7a2f7c141780.patch";
-      sha256 = "08x3h0w2pl8ifj83v9jkf4j3zxcwsyzh251divlhhnwx0rw1pyn7";
-    })
-  ];
 
   nativeBuildInputs = [
     autoreconfHook
