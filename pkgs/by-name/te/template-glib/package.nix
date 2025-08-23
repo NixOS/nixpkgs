be91a3f92e6920b0ff7f0009e078155a03dd2f7e
Bobby Rong <rjl931189261@126.com>
2025-08-23 04:07:02 +0000

    template-glib: 3.36.3 → 3.37.1
    https://gitlab.gnome.org/GNOME/template-glib/-/compare/3.36.3...3.37.1

--- pkgs/by-name/te/template-glib/package.nix
+++ pkgs/by-name/te/template-glib/package.nix
@@ -19,7 +19,7 @@
 
 stdenv.mkDerivation rec {
   pname = "template-glib";
-  version = "3.36.3";
+  version = "3.37.1";
 
   outputs = [
     "out"
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://gnome/sources/template-glib/${lib.versions.majorMinor version}/template-glib-${version}.tar.xz";
-    hash = "sha256-1SizWyz5Dgfa5Q4l4S+62w6wSPV/1RUc+fbpjM4d8g4=";
+    hash = "sha256-16xEMdWUe6KSCGx+R7iK6Kzb7A+PjTiyvbiIoL3MBhw=";
   };
 
   nativeBuildInputs = [
