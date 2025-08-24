09215d73515bef579a9975076fa2733d5029e66e
Sigmanificient <edhyjox@gmail.com>
2025-08-24 13:54:46 +0200

    python3Packages.pygls: pin lsprotocol to 2023.0.1

--- pkgs/development/python-modules/pygls/default.nix
+++ pkgs/development/python-modules/pygls/default.nix
@@ -36,7 +36,16 @@ buildPythonPackage rec {
   ];
 
   propagatedBuildInputs = [
-    lsprotocol
+    (lsprotocol.overridePythonAttrs (old: {
+      version = "2023.0.1";
+
+      src = fetchFromGitHub {
+        owner = "microsoft";
+        repo = "lsprotocol";
+        tag = "2023.0.1";
+        hash = "sha256-PHjLKazMaT6W4Lve1xNxm6hEwqE3Lr2m5L7Q03fqb68=";
+      };
+    }))
     typeguard
   ];
 
