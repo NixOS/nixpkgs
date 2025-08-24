7189bf6bab12ee504fcebcd9ee60ed768c616f31
Jasi <normalc@posteo.net>
2025-08-23 21:29:04 -0400

    ruffle: bump up version from "0-" to "0.2-"

--- pkgs/by-name/ru/ruffle/package.nix
+++ pkgs/by-name/ru/ruffle/package.nix
@@ -21,12 +21,12 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "ruffle";
-  version = "0-nightly-2025-08-22";
+  version = "0.2-nightly-2025-08-22";
 
   src = fetchFromGitHub {
     owner = "ruffle-rs";
     repo = "ruffle";
-    tag = lib.strings.removePrefix "0-" finalAttrs.version;
+    tag = lib.strings.removePrefix "0.2-" finalAttrs.version;
     hash = "sha256-bv8ZQuEU8QqtC7fvtELXlkQkjPoGqqSglhE0lzsTEIk=";
   };
 
@@ -35,8 +35,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   env =
     let
-      tag = lib.strings.removePrefix "0-" finalAttrs.version;
-      versionDate = lib.strings.removePrefix "0-nightly-" finalAttrs.version;
+      tag = lib.strings.removePrefix "0.2-" finalAttrs.version;
+      versionDate = lib.strings.removePrefix "0.2-nightly-" finalAttrs.version;
     in
     {
       VERGEN_IDEMPOTENT = "1";
@@ -116,7 +116,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
           curl https://api.github.com/repos/ruffle-rs/ruffle/releases?per_page=1 | \
           jq -r ".[0].tag_name" \
         )"
-        exec nix-update --version "0-$version" ruffle
+        exec nix-update --version "0.2-$version" ruffle
       '';
     });
   };
@@ -135,7 +135,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     '';
     homepage = "https://ruffle.rs/";
     downloadPage = "https://ruffle.rs/downloads";
-    changelog = "https://github.com/ruffle-rs/ruffle/releases/tag/${lib.strings.removePrefix "0-" finalAttrs.version}";
+    changelog = "https://github.com/ruffle-rs/ruffle/releases/tag/${lib.strings.removePrefix "0.2" finalAttrs.version}";
     license = [
       lib.licenses.mit
       lib.licenses.asl20
