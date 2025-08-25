d96e3ea7af4e464fb53e161d35d85e90ee7730ce
Jamie Magee <jamie.magee@gmail.com>
2025-08-24 19:24:21 -0700

    python3Packages.gitterpy: init at 0.1.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5880,6 +5880,8 @@ self: super: with self; {
 
   gitpython = callPackage ../development/python-modules/gitpython { };
 
+  gitterpy = callPackage ../development/python-modules/gitterpy { };
+
   giturlparse = callPackage ../development/python-modules/giturlparse { };
 
   glad = callPackage ../development/python-modules/glad { };
