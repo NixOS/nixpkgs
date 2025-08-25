9d2965b185b85d609c1f222c4878920d6d92fc85
Guy Chronister <guychronister@outlook.com>
2025-08-25 07:46:27 -0500

    kdiskmark: 3.1.4 -> 3.2.0
    Diff: https://github.com/jonmagon/kdiskmark/compare/3.1.4...3.2.0

--- pkgs/by-name/kd/kdiskmark/package.nix
+++ pkgs/by-name/kd/kdiskmark/package.nix
@@ -9,13 +9,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "kdiskmark";
-  version = "3.1.4";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "jonmagon";
     repo = "kdiskmark";
     tag = finalAttrs.version;
-    hash = "sha256-JueY7zw9PIo9ETi7pQLpw8FGRhNXYXeXEvTzZGz9lbw=";
+    hash = "sha256-b42PNUrG10RyGct6dPtdT89oO222tEovkSPoRcROfaQ=";
     fetchSubmodules = true;
   };
 
@@ -35,7 +35,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   preConfigure = ''
     substituteInPlace CMakeLists.txt \
-      --replace \$\{POLKITQT-1_POLICY_FILES_INSTALL_DIR\} $out/share/polkit-1/actions
+      --replace-fail \$\{POLKITQT-1_POLICY_FILES_INSTALL_DIR\} $out/share/polkit-1/actions
   '';
 
   qtWrapperArgs = [
