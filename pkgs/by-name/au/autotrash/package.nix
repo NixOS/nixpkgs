NixOS/nixpkgs

commit 4469fd60bff0f02ee40abc5906fe0519b3598ae7

autotrash: add changelog to meta

pkgs/by-name/au/autotrash/package.nix
@@ -27,12 +27,12 @@ python3Packages.buildPythonPackage rec {
   postInstall = "installManPage doc/autotrash.1";
 
   pythonImportsCheck = [ "autotrash" ];
-  nativeCheckInputs = [ python3Packages.pytestCheckHook ];
 
   meta = {
     description = "Tool to automatically purge old trashed files";
     license = lib.licenses.gpl3Plus;
     homepage = "https://bneijt.nl/pr/autotrash";
+    changelog = "https://github.com/bneijt/autotrash/releases/tag/${src.tag}";
     maintainers = with lib.maintainers; [
       sigmanificient
       mithicspirit

commit 5e1d078877e93071d274dafd5c26c4778081a1f9

autotrash: reintroduce checking with pytest
Reverts regression from 4469fd60bff0f02ee40abc5906fe0519b3598ae7.

pkgs/by-name/au/autotrash/package.nix
@@ -35,6 +35,7 @@ python3Packages.buildPythonPackage rec {
   postInstall = "installManPage doc/autotrash.1";
 
   pythonImportsCheck = [ "autotrash" ];
+  nativeCheckInputs = [ python3Packages.pytestCheckHook ];
 
   meta = {
     description = "Tool to automatically purge old trashed files";