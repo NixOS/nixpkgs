246c823c58725aa0bb9151ca6f2f35f32a3308c7
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-25 16:43:55 +0200

    python312Packages.mypy-boto3-synthetics: 1.40.0 -> 1.40.16

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1345,8 +1345,8 @@ rec {
       "sha256-qkE3rF32WkR56WB5pu3dKJLCLY5e1rvMDPYAruyj9O8=";
 
   mypy-boto3-synthetics =
-    buildMypyBoto3Package "synthetics" "1.40.0"
-      "sha256-LCW7HDnLI4BurauxCef8W93Rt/NYjR3XM1gC6AQ9uuA=";
+    buildMypyBoto3Package "synthetics" "1.40.16"
+      "sha256-E7spYXuGbWE7BrOPNP/f8kUakXjTLGf1JAZuhd6m0Yg=";
 
   mypy-boto3-textract =
     buildMypyBoto3Package "textract" "1.40.0"
