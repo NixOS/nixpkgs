ddacce558859d778ab7b52f9b507848efe841035
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-08-25 14:58:45 -0700

    beeper: 4.1.111 -> 4.1.135

--- pkgs/by-name/be/beeper/package.nix
+++ pkgs/by-name/be/beeper/package.nix
@@ -9,10 +9,10 @@
 }:
 let
   pname = "beeper";
-  version = "4.1.111";
+  version = "4.1.135";
   src = fetchurl {
     url = "https://beeper-desktop.download.beeper.com/builds/Beeper-${version}.AppImage";
-    hash = "sha256-0cr6syveIHIIy8+FyE23U8iidMYJXkN8CqhhbH0oNt0=";
+    hash = "sha256-bp0RGU689A8kgphNgJJnlbQBh1fAubwWUvM9StzLwB4=";
   };
   appimageContents = appimageTools.extract {
     inherit pname version src;
