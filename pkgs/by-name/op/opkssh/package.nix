51ea6ddc5b2c9ee6300f98b39033398d597220b0
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-25 10:27:01 +0000

    opkssh: 0.8.0 -> 0.9.0

--- pkgs/by-name/op/opkssh/package.nix
+++ pkgs/by-name/op/opkssh/package.nix
@@ -8,18 +8,18 @@
 
 buildGoModule (finalAttrs: {
   pname = "opkssh";
-  version = "0.8.0";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "openpubkey";
     repo = "opkssh";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-8CWxxhqgSgFVRNjAvJ0faHI4rsSPQNkcSqVSzTHRJY4=";
+    hash = "sha256-c7cs7qSq5fhJuMhgRWAZmc4yWt+B219P2D0niP2sXlM=";
   };
 
   ldflags = [ "-X main.Version=${finalAttrs.version}" ];
 
-  vendorHash = "sha256-0H7hST5Czd/1rDQ0nO2FAnbG2lN3AZULs5M17zKa9FY=";
+  vendorHash = "sha256-naQGvGjIRTy4BlXxM4bXnrBUWTUCFTDHOYEp91ss1wI=";
 
   nativeInstallCheckInputs = [
     versionCheckHook
