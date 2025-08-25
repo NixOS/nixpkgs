c579b4f49081dc0cc642fff546851d0093b498ae
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2025-08-25 08:14:15 -0400

    komga: 1.23.1 -> 1.23.3
    changelog: https://github.com/gotson/komga/releases/tag/1.23.2
    changelog: https://github.com/gotson/komga/releases/tag/1.23.3

--- pkgs/by-name/ko/komga/package.nix
+++ pkgs/by-name/ko/komga/package.nix
@@ -10,11 +10,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "komga";
-  version = "1.23.1";
+  version = "1.23.3";
 
   src = fetchurl {
     url = "https://github.com/gotson/${pname}/releases/download/${version}/${pname}-${version}.jar";
-    sha256 = "sha256-yDmkM4ayyAanX3NmjfHZdp6Ya1CAVVOonwfQa2XeGY8=";
+    sha256 = "sha256-Q52u284BqN6IktMKOAbiwdM77ZOPQrHIzOkExz91Xeo=";
   };
 
   nativeBuildInputs = [
