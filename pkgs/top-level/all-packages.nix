d600a9fccfdf0fbb0239a3b4e8f71e651b57b3ff
qzylinra <qzylinra@users.noreply.github.com>
2025-08-25 09:46:19 +0800

    guile-sdl: drop
    The software build has failed. It has no maintainer, and the library is not used by any other packages. Since it is difficult to maintain, it should be removed.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6443,10 +6443,6 @@ with pkgs;
 
   guile = guile_3_0;
 
-  guile-sdl = callPackage ../by-name/gu/guile-sdl/package.nix {
-    guile = guile_2_2;
-  };
-
   guile-xcb = callPackage ../by-name/gu/guile-xcb/package.nix {
     guile = guile_2_2;
   };
