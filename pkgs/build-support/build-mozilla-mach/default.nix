f33d889bd7b7950110da6eb87faea8f1190311a9
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-25 05:06:06 +0200

    buildMozillaMach: prune obsolete patches
    (cherry picked from commit e11bcbb432b63317c9213d45c1953b1d77f7c244)

--- pkgs/build-support/build-mozilla-mach/default.nix
+++ pkgs/build-support/build-mozilla-mach/default.nix
@@ -304,15 +304,7 @@ buildStdenv.mkDerivation {
   ];
 
   patches =
-    lib.optionals (lib.versionAtLeast version "111" && lib.versionOlder version "133") [
-      ./env_var_for_system_dir-ff111.patch
-    ]
-    ++ lib.optionals (lib.versionAtLeast version "133") [ ./env_var_for_system_dir-ff133.patch ]
-    ++ lib.optionals (lib.versionAtLeast version "121" && lib.versionOlder version "136") [
-      ./no-buildconfig-ffx121.patch
-    ]
-    ++ lib.optionals (lib.versionAtLeast version "136") [ ./no-buildconfig-ffx136.patch ]
-    ++ lib.optionals (lib.versionAtLeast version "139" && lib.versionOlder version "141") [
+    lib.optionals (lib.versionAtLeast version "139" && lib.versionOlder version "141") [
       # https://bugzilla.mozilla.org/show_bug.cgi?id=1955112
       # https://hg-edge.mozilla.org/mozilla-central/rev/aa8a29bd1fb9
       ./139-wayland-drag-animation.patch
@@ -326,24 +318,6 @@ buildStdenv.mkDerivation {
         [
           ./142-relax-apple-sdk.patch
         ]
-    ++ lib.optionals (lib.versionOlder version "139") [
-      # Fix for missing vector header on macOS
-      # https://bugzilla.mozilla.org/show_bug.cgi?id=1959377
-      # Fixed on Firefox 139
-      ./firefox-mac-missing-vector-header.patch
-    ]
-    ++ lib.optionals (lib.versionOlder version "140") [
-      # https://bugzilla.mozilla.org/show_bug.cgi?id=1962497
-      # https://phabricator.services.mozilla.com/D246545
-      # Fixed on Firefox 140
-      ./build-fix-RELRHACK_LINKER-setting-when-linker-name-i.patch
-    ]
-    ++ lib.optionals (lib.versionOlder version "138") [
-      # https://bugzilla.mozilla.org/show_bug.cgi?id=1941479
-      # https://phabricator.services.mozilla.com/D240572
-      # Fixed on Firefox 138
-      ./firefox-cannot-find-type-Allocator.patch
-    ]
     ++ extraPatches;
 
   postPatch = ''
