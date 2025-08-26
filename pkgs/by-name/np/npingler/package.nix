7a5f2cee3100d50af4827333921c07b64800b0d2
Rebecca Turner <rbt@sent.as>
2025-08-26 15:55:59 -0700

    npingler: install shell completions

--- pkgs/by-name/np/npingler/package.nix
+++ pkgs/by-name/np/npingler/package.nix
@@ -1,10 +1,17 @@
 {
   lib,
+  stdenv,
+  buildPackages,
   rustPlatform,
   fetchFromGitHub,
+  installShellFiles,
   nix-update-script,
 }:
 
+let
+  emulatorAvailable = stdenv.hostPlatform.emulatorAvailable buildPackages;
+  emulator = stdenv.hostPlatform.emulator buildPackages;
+in
 rustPlatform.buildRustPackage {
   pname = "npingler";
   version = "unstable-2025-08-26";
@@ -17,6 +24,18 @@ rustPlatform.buildRustPackage {
   };
 
   cargoHash = "sha256-71uqdWsXBd6qsplwI3cA2TxXoj6JOThEHxnv9u6iraQ=";
+
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  postInstall = lib.optionalString emulatorAvailable ''
+    installShellCompletion --cmd npingler \
+      --bash <(${emulator} $out/bin/npingler util generate-completions bash) \
+      --fish <(${emulator} $out/bin/npingler util generate-completions fish) \
+      --zsh  <(${emulator} $out/bin/npingler util generate-completions zsh)
+  '';
 
   meta = {
     description = "Nix profile manager for use with npins";
