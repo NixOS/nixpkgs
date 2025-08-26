3bb83bd10ccd571bcbd7d8821f32a5ef16264f75
phaer <hello@phaer.org>
2025-08-26 14:58:23 +0200

    modules/image/repart: repart.imageFile(Basename) -> image.baseName
    Rename remaining uses of renamed option repart.imageFileBasename to
    image.baseName.
    
    Follow up to https://github.com/NixOS/nixpkgs/pull/401872
    Removes a warning when using repart-verity-store.nix.

--- nixos/modules/image/repart-image.nix
+++ nixos/modules/image/repart-image.nix
@@ -30,7 +30,7 @@
   # arguments
   name,
   version,
-  imageFileBasename,
+  baseName,
   compression,
   fileSystems,
   finalPartitions,
@@ -205,7 +205,7 @@ stdenvNoCC.mkDerivation (
       echo "Building image with systemd-repart..."
       unshare --map-root-user fakeroot systemd-repart \
         ''${systemdRepartFlags[@]} \
-        ${imageFileBasename}.raw \
+        ${baseName}.raw \
         | tee repart-output.json
 
       runHook postBuild
@@ -220,14 +220,14 @@ stdenvNoCC.mkDerivation (
     # separate derivation to allow users to save disk space. Disk images are
     # already very space intensive so we want to allow users to mitigate this.
     + lib.optionalString compression.enable ''
-      for f in ${imageFileBasename}*; do
+      for f in ${baseName}*; do
         echo "Compressing $f with ${compression.algorithm}..."
         # Keep the original file when compressing and only delete it afterwards
         ${compressionCommand} $f && rm $f
       done
     ''
     + ''
-      mv -v repart-output.json ${imageFileBasename}* $out
+      mv -v repart-output.json ${baseName}* $out
 
       runHook postInstall
     '';
