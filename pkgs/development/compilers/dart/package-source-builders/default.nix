4b4bc20c3aaa101d0fce4339c1ffaac2fb186e7a
qzylinra <qzylinra@users.noreply.github.com>
2025-08-27 05:43:26 +0800

    dart.fvp: drop

--- pkgs/development/compilers/dart/package-source-builders/default.nix
+++ pkgs/development/compilers/dart/package-source-builders/default.nix
@@ -7,7 +7,6 @@
   flutter_secure_storage_linux = callPackage ./flutter-secure-storage-linux { };
   flutter_vodozemac = callPackage ./flutter_vodozemac { };
   flutter_volume_controller = callPackage ./flutter_volume_controller { };
-  fvp = callPackage ./fvp { };
   handy_window = callPackage ./handy-window { };
   matrix = callPackage ./matrix { };
   media_kit_libs_linux = callPackage ./media_kit_libs_linux { };
