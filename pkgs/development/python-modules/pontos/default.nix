3048947bcc805b9b3f4bc94146f65bdaaf40218c
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-26 15:29:18 +0000

    python3Packages.pontos: 25.8.0 -> 25.8.1

--- pkgs/development/python-modules/pontos/default.nix
+++ pkgs/development/python-modules/pontos/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "pontos";
-  version = "25.8.0";
+  version = "25.8.1";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "greenbone";
     repo = "pontos";
     tag = "v${version}";
-    hash = "sha256-47YlSTw7dGsNE1OntjdXdn4jAO4IUhLHMdeBVWkpadg=";
+    hash = "sha256-uQe21Yp1PYeU/NgbQMbC2yBqEA1yUUaCiKc7k/oeS7g=";
   };
 
   build-system = [ poetry-core ];
