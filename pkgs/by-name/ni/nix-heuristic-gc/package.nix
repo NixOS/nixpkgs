247fd56d6fe58d388d4794fa319b895ceaecbc28
Jörg Thalheim <joerg@thalheim.io>
2025-08-26 09:29:13 +0200

    nix-heuristic-gc: bump to nix 2.30

--- pkgs/by-name/ni/nix-heuristic-gc/package.nix
+++ pkgs/by-name/ni/nix-heuristic-gc/package.nix
@@ -9,23 +9,23 @@
 }:
 python3Packages.buildPythonPackage rec {
   pname = "nix-heuristic-gc";
-  version = "0.6.1";
+  version = "0.6.2";
   format = "setuptools";
   src = fetchFromGitHub {
     owner = "risicle";
     repo = "nix-heuristic-gc";
     tag = "v${version}";
-    hash = "sha256-3SSIbfOx6oYsCZgK71bbx2H3bAMZ3VJxWfiMVPq5FaE=";
+    hash = "sha256-FHA1/zlBUr81wSWL0Cd5DVsxUDuUpD3UuLde8UgxOzQ=";
   };
 
   # NIX_SYSTEM suggested at
   # https://github.com/NixOS/nixpkgs/issues/386184#issuecomment-2692433531
-  NIX_SYSTEM = nixVersions.nix_2_24.stdenv.hostPlatform.system;
-  NIX_CFLAGS_COMPILE = [ "-I${lib.getDev nixVersions.nix_2_24}/include/nix" ];
+  NIX_SYSTEM = nixVersions.nixComponents_2_30.nix-store.stdenv.hostPlatform.system;
 
   buildInputs = [
     boost
-    nixVersions.nix_2_24
+    nixVersions.nixComponents_2_30.nix-store
+    nixVersions.nixComponents_2_30.nix-main
     python3Packages.pybind11
     python3Packages.setuptools
   ];
