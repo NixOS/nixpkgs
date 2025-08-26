10166ccaeea07ebd339ac69d0312a0da96666481
EndermanbugZJFC <175898536+Ezjfc@users.noreply.github.com>
2025-08-27 00:30:20 +0100

    animdustry: patchelf

--- pkgs/by-name/an/animdustry/package.nix
+++ pkgs/by-name/an/animdustry/package.nix
@@ -1,17 +1,23 @@
 {
+  stdenv,
   lib,
+  autoPatchelfHook,
   copyDesktopItems,
   makeDesktopItem,
-  makeBinaryWrapper,
   writeText,
 
   fetchFromGitHub,
   buildNimPackage,
   nim-1_0,
+  callPackage,
 
-  xorg,
+  libX11,
+  libXcursor,
+  libXrandr,
+  libXinerama,
+  libXi,
   libGL,
-  libGLU,
+  libXxf86vm,
   libpulseaudio,
 }:
 let
@@ -19,25 +25,14 @@ let
     nim2 = nim-1_0; # Intended for Nim 1, but resolves to Nim 2 because of "nim >= 1.6.2" in animdustry.nimble
   };
 
-  fau = fetchFromGitHub {
-    owner = "Anuken";
-    repo = "fau";
-    rev = "73df4a699873d0f82fd612a2a2ac63c21d3f2233";
-    hash = "sha256-9zwmFinDJV4+R/aiVVOQ/Bv30jX7NHJyufzMNWHGA+k=";
-    fetchSubmodules = true;
-  };
-  faupack = buildNimPackage' {
-    pname = "fau";
-    version = "0-unstable-2022-05-14";
-    src = fau;
-    lockFile = ./fau-lock.json;
-  };
-
+  fau = callPackage ./fau.nix { buildNimPackage = buildNimPackage'; };
   fauLock = builtins.fromJSON (builtins.readFile ./fau-lock.json);
   partialLock = builtins.fromJSON (builtins.readFile ./package-lock-partial.json);
-  lockFile = writeText "package-lock.json" (builtins.toJSON {
-    depends = fauLock.depends ++ partialLock.depends;
-  });
+  lockFile = writeText "package-lock.json" (
+    builtins.toJSON {
+      depends = fauLock.depends ++ partialLock.depends;
+    }
+  );
 in
 buildNimPackage' (finalAttrs: {
   inherit lockFile;
@@ -48,31 +43,36 @@ buildNimPackage' (finalAttrs: {
   src = fetchFromGitHub {
     owner = "Anuken";
     repo = "animdustry";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-RND5AhlD9uTMg9Koz5fq7WMgJt+Ajiyi6K60WFbF0bg=";
   };
 
   buildInputs = [
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXrandr
-    xorg.libXinerama
-    xorg.libXi
+    stdenv.cc.cc.lib
+    libX11
+    libXcursor
+    libXrandr
+    libXinerama
+    libXi
     libGL
-    libGLU
-    xorg.libXxf86vm
+    libXxf86vm
   ];
-  nativeBuildInputs = [
-    faupack
 
-    makeWrapper
+  nativeBuildInputs = [
+    fau.package
+
+    autoPatchelfHook
     copyDesktopItems
+  ];
+
+  runtimeDependencies = [
+    libpulseaudio
   ];
 
   nimFlags = [
     "--app:gui"
     "-d:danger"
-    "--path:\"${fau}/src\""
+    "--path:\"${fau.src}/src\""
 
     "-d:NimblePkgVersion=${finalAttrs.version}"
   ];
@@ -81,12 +81,6 @@ buildNimPackage' (finalAttrs: {
     runHook preInstall
 
     mv $out/bin/main $out/bin/animdustry
-    wrapProgram $out/bin/animdustry \
-      --suffix LD_LIBRARY_PATH : ${
-        lib.makeLibraryPath [
-          libpulseaudio
-        ]
-      }
     install -Dm644 ./assets/icon.png $out/share/icons/hicolor/64x64/apps/animdustry.png
 
     runHook postInstall
@@ -104,7 +98,7 @@ buildNimPackage' (finalAttrs: {
       categories = [ "Game" ];
     };
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/Anuken/animdustry";
     downloadPage = "https://github.com/Anuken/animdustry/releases";
     description = "Anime gacha rhythm game";
@@ -112,9 +106,9 @@ buildNimPackage' (finalAttrs: {
       the anime gacha bullet hell rhythm game
       created as a mindustry april 1st event
     '';
-    license = licenses.gpl3; # Listed in animdustry.nimble
-    platforms = platforms.all;
-    maintainers = with maintainers; [
+    license = lib.licenses.gpl3; # Listed in animdustry.nimble
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [
       Ezjfc
     ];
   };
