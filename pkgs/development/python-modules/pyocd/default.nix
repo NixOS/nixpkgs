a979b67c367998a533b239eac7f6b289d40607d4
Gaetan Lepage <gaetan@glepage.com>
2025-08-26 22:24:32 +0200

    python3Packages.pyocd: fix

--- pkgs/development/python-modules/pyocd/default.nix
+++ pkgs/development/python-modules/pyocd/default.nix
@@ -1,8 +1,13 @@
 {
   lib,
+  stdenv,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch,
+
+  # build-system
+  setuptools-scm,
+
+  # dependencies
   capstone,
   cmsis-pack-manager,
   colorama,
@@ -17,10 +22,10 @@
   pylink-square,
   pyusb,
   pyyaml,
-  setuptools-scm,
   typing-extensions,
-  stdenv,
   hidapi,
+
+  # tests
   pytestCheckHook,
 }:
 
@@ -35,16 +40,6 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-4fdVcTNH125e74S3mA/quuDun17ntGCazX6CV+obUGc=";
   };
-
-  patches = [
-    # https://github.com/pyocd/pyOCD/pull/1332
-    # merged into develop
-    (fetchpatch {
-      name = "libusb-package-optional.patch";
-      url = "https://github.com/pyocd/pyOCD/commit/0b980cf253e3714dd2eaf0bddeb7172d14089649.patch";
-      hash = "sha256-B2+50VntcQELeakJbCeJdgI1iBU+h2NkXqba+LRYa/0=";
-    })
-  ];
 
   pythonRelaxDeps = [ "capstone" ];
   pythonRemoveDeps = [ "libusb-package" ];
@@ -80,13 +75,13 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [ pytestCheckHook ];
 
-  meta = with lib; {
+  meta = {
     changelog = "https://github.com/pyocd/pyOCD/releases/tag/${src.tag}";
     description = "Python library for programming and debugging Arm Cortex-M microcontrollers";
     downloadPage = "https://github.com/pyocd/pyOCD";
     homepage = "https://pyocd.io";
-    license = licenses.asl20;
-    maintainers = with maintainers; [
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
       frogamic
       sbruder
     ];
