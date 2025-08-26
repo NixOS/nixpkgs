0c409ddebd36a3b747dad596fdaed8f68ef2ba8e
OPNA2608 <opna2608@protonmail.com>
2025-08-26 16:29:05 +0200

    python3Packages.plyer: Fix FTBFS

--- pkgs/development/python-modules/plyer/default.nix
+++ pkgs/development/python-modules/plyer/default.nix
@@ -3,8 +3,8 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   keyring,
-  mock,
   pytestCheckHook,
 }:
 
@@ -20,6 +20,30 @@ buildPythonPackage rec {
     sha256 = "sha256-7Icb2MVj5Uit86lRHxal6b7y9gIJ3UT2HNqpA9DYWVE=";
   };
 
+  patches = [
+    # Fix compatibility with Python 3.13
+    (fetchpatch {
+      name = "0001-plyer-Use-unittest-mock.patch";
+      url = "https://github.com/kivy/plyer/commit/c9e73f395e2b51d46ada68119abfae2973591c00.patch";
+      hash = "sha256-rWak2GOGnsw+GhEtdob9h1c39aa6Z1yU7vH9kdGjHO0=";
+    })
+    (fetchpatch {
+      name = "0002-plyer-Remove-whitespace-error-during-self.assertEqual-function-call.patch";
+      url = "https://github.com/kivy/plyer/commit/8393c61dfd3a7aa0362d3bf530ba9ca052878c1a.patch";
+      hash = "sha256-omjpQJQ+vZ6T12vL6LvKOuRSihiHfLdEZ1pDat8VuiM=";
+    })
+    (fetchpatch {
+      name = "0003-plyer-Replace obj.__doc__-with-getdoc-obj-to-automatically-remove-new-lines.patch";
+      url = "https://github.com/kivy/plyer/commit/675750f31e9f98cd5de2df732afe34648f343d3e.patch";
+      hash = "sha256-Lb7MbbcIjwbfnR8U6t9j0c+bqU7kK3/xEt13pSF8G6M=";
+    })
+    (fetchpatch {
+      name = "0004-plyer-Remove-newline-and-indentation-comparisons-during-self.assertEqual-function-call.patch";
+      url = "https://github.com/kivy/plyer/commit/31e96f689771cd43f0a925463a59fcbc49f58e46.patch";
+      hash = "sha256-ZYYftI4w+21Q6oKm1wku7NJg3xfkIpkjN+PvZY0YjyY=";
+    })
+  ];
+
   postPatch = ''
     rm -r examples
     # remove all the wifi stuff. Depends on a python wifi module that has not been updated since 2016
@@ -34,7 +58,6 @@ buildPythonPackage rec {
   propagatedBuildInputs = [ keyring ];
 
   nativeCheckInputs = [
-    mock
     pytestCheckHook
   ];
 
