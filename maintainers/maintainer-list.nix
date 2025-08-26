6724debf6b282a581ef1f0379c23d8a5e6749069
Jiatao Liang <jtliang24@gmail.com>
2025-08-26 18:09:10 -0400

    maintainers: add jtliang24

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12881,6 +12881,12 @@
     github = "jthulhu";
     githubId = 23179762;
   };
+  jtliang24 = {
+    name = "Jiatao Liang";
+    email = "jtliang24@gmail.com";
+    github = "jtliang24";
+    githubId = 170479425;
+  };
   jtobin = {
     email = "jared@jtobin.io";
     github = "jtobin";
