de66c73a1938d53625e552ab8e1cdfa62ae4fb1f
TornaxO7 <tornax@proton.me>
2025-08-27 00:14:42 +0200

    maintainers: add m0ustach3

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15260,6 +15260,11 @@
     github = "M0NsTeRRR";
     githubId = 37785089;
   };
+  m0ustache3 = {
+    name = "M0ustach3";
+    github = "M0ustach3";
+    githubId = 37956764;
+  };
   m1cr0man = {
     email = "lucas+nix@m1cr0man.com";
     github = "m1cr0man";
