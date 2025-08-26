765b7ffdb2f9051bf22508bebf5fa2f12696f83e
Sigmanificient <edhyjox@gmail.com>
2025-08-26 02:27:12 +0200

    python3Packages.pyechonest: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12771,8 +12771,6 @@ self: super: with self; {
 
   pyecharts = callPackage ../development/python-modules/pyecharts { };
 
-  pyechonest = callPackage ../development/python-modules/pyechonest { };
-
   pyeclib = callPackage ../development/python-modules/pyeclib { };
 
   pyecoforest = callPackage ../development/python-modules/pyecoforest { };
