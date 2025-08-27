f233e9cca64e3e5367c95366d784a4f3f72c0b8b
Jon Seager <jon@sgrs.uk>
2025-08-27 16:34:42 +0100

    python3Packages.craft-archives: 2.1.0 -> 2.2.0

--- pkgs/development/python-modules/craft-archives/default.nix
+++ pkgs/development/python-modules/craft-archives/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "craft-archives";
-  version = "2.1.0";
+  version = "2.2.0";
 
   pyproject = true;
 
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "canonical";
     repo = "craft-archives";
     tag = version;
-    hash = "sha256-VjGoAsmdYyoU7ngU69HVNauEk2/vbcEz2tMCTmjheF4=";
+    hash = "sha256-NXMBaY4sZT47Qi3XS5yuiXJEMKENghiXkLXnXHHYpRI=";
   };
 
   postPatch = ''
@@ -34,7 +34,7 @@ buildPythonPackage rec {
       --replace-fail "dev" "${version}"
 
     substituteInPlace pyproject.toml \
-      --replace-fail "setuptools==67.7.2" "setuptools"
+      --replace-fail "setuptools>=69.0" "setuptools"
   '';
 
   pythonRelaxDeps = [
