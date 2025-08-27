0793bb895dd19cfe910adcb1b378ef4b48061b7e
b521f771d8991e6f1d8e65ae05a8d783 <177119872+b521f771d8991e6f1d8e65ae05a8d783@users.noreply.github.com>
2025-08-27 13:16:29 +0200

    Update package.nix

--- pkgs/by-name/gn/gnustep-base-gcc/package.nix
+++ pkgs/by-name/gn/gnustep-base-gcc/package.nix
@@ -3,33 +3,11 @@
   gobjcStdenv,
   gnustep-make-gcc,
   gnustep-base,
-fetchpatch,
 }:
 
 gobjcStdenv.mkDerivation (finalAttrs: {
   pname = "gnustep-base-gcc";
-  inherit (gnustep-base) version src outputs nativeBuildInputs buildInputs propagatedBuildInputs;
-
-patches = [
-    ../gnustep-base/fixup-paths.patch
-    # https://github.com/gnustep/libs-base/issues/212 / https://www.sogo.nu/bugs/view.php?id=5416#c15585
-    (fetchpatch {
-      url = "https://github.com/gnustep/libs-base/commit/bd5f2909e6edc8012a0a6e44ea1402dfbe1353a4.patch";
-      revert = true;
-      sha256 = "02awigkbhqa60hfhqfh2wjsa960y3q6557qck1k2l231piz2xasa";
-    })
-    # https://github.com/gnustep/libs-base/issues/294
-    (fetchpatch {
-      url = "https://github.com/gnustep/libs-base/commit/37913d006d96a6bdcb963f4ca4889888dcce6094.patch";
-      sha256 = "PyOmzRIirSKG5SQY+UwD6moCidPb8PXCx3aFgfwxsXE=";
-    })
-    # https://github.com/gnustep/libs-base/pull/334
-    (fetchpatch {
-      url = "https://github.com/gnustep/libs-base/commit/b4feee311f2beaf499a5742967213f523de30f16.patch";
-      excludes = [ "ChangeLog" ];
-      hash = "sha256-r0qpxjpEM6y+F/gju6JhpDNxnFJNHFG/mt3NmC1hWrs=";
-    })
-  ];
+  inherit (gnustep-base) version src outputs nativeBuildInputs buildInputs propagatedBuildInputs patches;
 
   propagatedNativeBuildInputs = [
     gnustep-make-gcc
