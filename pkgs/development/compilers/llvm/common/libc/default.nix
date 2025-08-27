e3dc832e773f69ea4d1f585ec9f0f79c058cbcfb
Tristan Ross <tristan.ross@midstall.com>
2025-08-26 19:39:43 -0700

    llvmPackages_21.libc: fix building

--- pkgs/development/compilers/llvm/common/libc/default.nix
+++ pkgs/development/compilers/llvm/common/libc/default.nix
@@ -18,14 +18,19 @@
 let
   pname = "libc";
 
-  src' = runCommand "${pname}-src-${version}" { } (''
-    mkdir -p "$out"
-    cp -r ${monorepoSrc}/cmake "$out"
-    cp -r ${monorepoSrc}/runtimes "$out"
-    cp -r ${monorepoSrc}/llvm "$out"
-    cp -r ${monorepoSrc}/compiler-rt "$out"
-    cp -r ${monorepoSrc}/${pname} "$out"
-  '');
+  src' = runCommand "${pname}-src-${version}" { } (
+    ''
+      mkdir -p "$out"
+      cp -r ${monorepoSrc}/cmake "$out"
+      cp -r ${monorepoSrc}/runtimes "$out"
+      cp -r ${monorepoSrc}/llvm "$out"
+      cp -r ${monorepoSrc}/compiler-rt "$out"
+      cp -r ${monorepoSrc}/${pname} "$out"
+    ''
+    + lib.optionalString (lib.versionAtLeast release_version "21") ''
+      cp -r ${monorepoSrc}/third-party "$out"
+    ''
+  );
 in
 stdenv.mkDerivation (finalAttrs: {
   inherit pname version patches;
