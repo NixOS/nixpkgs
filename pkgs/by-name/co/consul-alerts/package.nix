66fb65e0eddbf1fa906fbe2a1e58304b4c25fc4c
Mia Moir <63592337+archessmn@users.noreply.github.com>
2025-08-27 15:30:09 +0100

    consul-alerts: drop leading article in description
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/by-name/co/consul-alerts/package.nix
+++ pkgs/by-name/co/consul-alerts/package.nix
@@ -25,7 +25,7 @@ buildGoModule rec {
 
   meta = with lib; {
     mainProgram = "consul-alerts";
-    description = "A highly available daemon for sending notifications and reminders based on Consul health checks";
+    description = "Highly available daemon for sending notifications and reminders based on Consul health checks";
     homepage = "https://github.com/AcalephStorage/consul-alerts";
     # As per README
     platforms = platforms.linux ++ platforms.freebsd ++ platforms.darwin;
