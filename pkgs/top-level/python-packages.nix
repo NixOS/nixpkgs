860995446343afbd6b2fdc860a2c1936b15d0085
Jamie Magee <jamie.magee@gmail.com>
2025-08-27 22:59:55 -0700

    python3Packages.travispy: init at 0.3.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18445,6 +18445,8 @@ self: super: with self; {
 
   transmission-rpc = callPackage ../development/python-modules/transmission-rpc { };
 
+  travispy = callPackage ../development/python-modules/travispy { };
+
   trectools = callPackage ../development/python-modules/trectools { };
 
   tree-sitter = callPackage ../development/python-modules/tree-sitter { };
