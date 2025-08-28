dc881d34786a10f9a4f1af65ae062b4857c16ac0
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-28 10:05:12 +0200

    python313Packages.skops: update formatting

--- pkgs/development/python-modules/skops/default.nix
+++ pkgs/development/python-modules/skops/default.nix
@@ -50,16 +50,20 @@ buildPythonPackage rec {
     pyyaml
     streamlit
   ];
+
   optional-dependencies = {
     rich = [ rich ];
-  };  
+  };
+
   enabledTestPaths = [ "skops" ];
+
   disabledTests = [
     # flaky
     "test_base_case_works_as_expected"
     # fairlearn is not available in nixpkgs
     "TestAddFairlearnMetricFrame"
   ];
+
   disabledTestPaths = [
     # minor output formatting issue
     "skops/card/_model_card.py"
@@ -68,19 +72,21 @@ buildPythonPackage rec {
     # Segfaults on darwin
     "skops/io/tests/test_persist.py"
   ];
+
   pytestFlags = [
     # Warning from scipy.optimize in skops/io/tests/test_persist.py::test_dump_and_load_with_file_wrapper
     # https://github.com/skops-dev/skops/issues/479
     "-Wignore::DeprecationWarning"
   ];
+
   pythonImportsCheck = [ "skops" ];
 
   meta = {
     description = "Library for saving/loading, sharing, and deploying scikit-learn based models";
-    mainProgram = "skops";
     homepage = "https://skops.readthedocs.io/en/stable";
     changelog = "https://github.com/skops-dev/skops/releases/tag/${src.tag}";
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.bcdarwin ];
+    mainProgram = "skops";
   };
 }
