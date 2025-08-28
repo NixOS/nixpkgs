fc7d5ef27f463aac3503ebd1cc1e6cfbecdc2e08
Varun Narravula <varun@snare.dev>
2025-08-28 00:34:05 -0700

    zf: use native nixpkgs fetcher for zig dependencies

--- pkgs/by-name/zf/zf/package.nix
+++ pkgs/by-name/zf/zf/package.nix
@@ -5,9 +5,8 @@
   installShellFiles,
   testers,
   zig_0_13,
-  callPackage,
+  nix-update-script,
 }:
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "zf";
   version = "0.10.2";
@@ -24,9 +23,11 @@ stdenv.mkDerivation (finalAttrs: {
     zig_0_13.hook
   ];
 
-  postPatch = ''
-    cp -a ${callPackage ./deps.nix { }}/. $ZIG_GLOBAL_CACHE_DIR/p
-  '';
+  zigDeps = zig_0_13.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-ncBYiRfX76uoYNhAEDSHMR9Lx1Rfi8zHUGXekIaXdtg=";
+    manuallyFetchLazyDeps = true;
+  };
 
   postInstall = ''
     installManPage doc/zf.1
@@ -36,7 +37,10 @@ stdenv.mkDerivation (finalAttrs: {
       --zsh complete/_zf
   '';
 
-  passthru.tests.version = testers.testVersion { package = finalAttrs.finalPackage; };
+  passthru = {
+    tests.version = testers.testVersion { package = finalAttrs.finalPackage; };
+    updateScript = nix-update-script { };
+  };
 
   meta = {
     homepage = "https://github.com/natecraddock/zf";
