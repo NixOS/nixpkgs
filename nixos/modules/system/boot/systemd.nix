NixOS/nixpkgs

commit 2c98ea1e3309989842c01eede6b744ffe68d301b

nixos/systemd: add upstream units for boot counting
These units are needed to be able to experiment with systemd's boot counting
functionality (e.g. with out-of-tree bootloaders like lanzaboote), and they
cannot easily be added through configuration without patching nixpkgs.
They are basically a no-op when boot entries do not contain any
counters, so AFAICT there is no disadvantage to them being enabled by default.

nixos/modules/system/boot/systemd.nix
@@ -113,6 +113,10 @@ let
     "systemd-rfkill.service"
     "systemd-rfkill.socket"
 
+    # Boot counting
+    "boot-complete.target"
+    "systemd-bless-boot.service"
+
     # Hibernate / suspend.
     "hibernate.target"
     "suspend.target"

commit 093cc758863188d36a1b296b4c7a9681248ddd11

nixos/systemd: fix enabling non-existent service
This service doesn't exist when withBootLoader is false.

Fixes: 2c98ea1e3309 ("nixos/systemd: add upstream units for boot counting")

nixos/modules/system/boot/systemd.nix
@@ -107,15 +107,16 @@ let
     # Maintaining state across reboots.
     "systemd-random-seed.service"
   ]
-  ++ (optional cfg.package.withBootloader "systemd-boot-random-seed.service")
+  ++ optionals cfg.package.withBootloader [
+    "systemd-boot-random-seed.service"
+    "systemd-bless-boot.service"
+  ]
   ++ [
     "systemd-backlight@.service"
     "systemd-rfkill.service"
     "systemd-rfkill.socket"
 
-    # Boot counting
     "boot-complete.target"
-    "systemd-bless-boot.service"
 
     # Hibernate / suspend.
     "hibernate.target"