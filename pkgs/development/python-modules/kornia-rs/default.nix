82e6a40c732ad1f5e2301fe9801a72fbf93f8803
Yuxuan Shui <yshuiv7@gmail.com>
2025-08-28 04:52:37 +0100

    python3Packages.kornia-rs: 0.1.2 -> 0.1.9
    Fixes: #395057

--- pkgs/development/python-modules/kornia-rs/default.nix
+++ pkgs/development/python-modules/kornia-rs/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "kornia-rs";
-  version = "0.1.2";
+  version = "0.1.9";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "kornia";
     repo = "kornia-rs";
     tag = "v${version}";
-    hash = "sha256-7toCMaHzFAzm6gThVLBxKLgQVgFJatdJseDlfdeS8RE=";
+    hash = "sha256-0Id1Iyd/xyqSqFvg/TXnlX1DgMUWuMS9KbtDXduwU+Y=";
   };
 
   nativeBuildInputs = [
@@ -30,20 +30,25 @@ buildPythonPackage rec {
 
   buildInputs = lib.optional stdenv.hostPlatform.isDarwin libiconv;
 
-  cargoRoot = "py-kornia";
+  postUnpack = ''
+    cp -v ${./Cargo.lock} source/${cargoRoot}/Cargo.lock
+  '';
+
+  cargoRoot = "kornia-py";
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit
       pname
       version
       src
       cargoRoot
+      postUnpack
       ;
-    hash = "sha256-VQtPfTmT9UGM0fIMeZF/1lUqQeyP63naoYZ7UuL6hFg=";
+    hash = "sha256-oihWdfCnWx7wpWwwl+FqPQAovIa3EIr4C8FgKj9tdq0=";
   };
 
   maturinBuildFlags = [
     "-m"
-    "py-kornia/Cargo.toml"
+    "kornia-py/Cargo.toml"
   ];
 
   dontUseCmakeConfigure = true; # We only want to use CMake to build some Rust dependencies.
