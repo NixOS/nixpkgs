9dcb2d494f65f75ab00a83a14233dd7173d09a38
Leona Maroni <dev@leona.is>
2025-08-28 09:25:15 +0200

    gitlab: 18.2.2 -> 18.2.5
    https://about.gitlab.com/releases/2025/08/27/patch-release-gitlab-18-3-1-released/
    (cherry picked from commit a481b621204c0a610c11fcd067e1201e7d06ccf3)

--- pkgs/by-name/gi/gitaly/package.nix
+++ pkgs/by-name/gi/gitaly/package.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "18.2.2";
+  version = "18.2.5";
   package_version = "v${lib.versions.major version}";
   gitaly_package = "gitlab.com/gitlab-org/gitaly/${package_version}";
 
@@ -21,7 +21,7 @@ let
       owner = "gitlab-org";
       repo = "gitaly";
       rev = "v${version}";
-      hash = "sha256-+OmduG9neb15m3i57qPfE42HI4w/zgmA5T6bhlwzrT0=";
+      hash = "sha256-/AyWxFUyNC0RCM4WMSlPrlh9okyZBacR2sPyzl9y4D0=";
     };
 
     vendorHash = "sha256-RjDV4NGmmdT9STQBHiYf3UUYwPmuSg6970/W/ekxin0=";
