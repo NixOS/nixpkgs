d2fe4abe76905be104113b53641aa50a6bbcb4fd
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-28 03:00:31 +0000

    libmsquic: 2.5.3 -> 2.5.4

--- pkgs/by-name/li/libmsquic/package.nix
+++ pkgs/by-name/li/libmsquic/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libmsquic";
-  version = "2.5.3";
+  version = "2.5.4";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "msquic";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-rL4uKcPx3IUzBDp8uGW2VprVyYuTD5p73WbdW+ebSGg=";
+    hash = "sha256-si9g67j/A6sbsCWWxs2YhZpXhx34GpxWNOFnWtaqnEQ=";
     fetchSubmodules = true;
   };
 
