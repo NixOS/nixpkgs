5af045390a0e884aa57280b63ddd20502fea43cf
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-28 12:32:32 +0000

    hyprland-per-window-layout: 2.15 -> 2.16

--- pkgs/by-name/hy/hyprland-per-window-layout/package.nix
+++ pkgs/by-name/hy/hyprland-per-window-layout/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "hyprland-per-window-layout";
-  version = "2.15";
+  version = "2.16";
 
   src = fetchFromGitHub {
     owner = "coffebar";
     repo = "hyprland-per-window-layout";
     rev = version;
-    hash = "sha256-SOT2nrk2JKTzKE1QNhjAY9zjyG5z5nYFz7RJRrS3Tsk=";
+    hash = "sha256-+1gmLNWuV5DhXjC6aRD1rHIOpX+OCX1Ak45pi+ixIFw=";
   };
 
-  cargoHash = "sha256-VzxO5xn864gnMR62iszTNwz1tU7A59dhQspsla90aRs=";
+  cargoHash = "sha256-k8YNGqKw5MPHdXye8loqGkexY75jjkJSnzaWmye945I=";
 
   meta = with lib; {
     description = "Per window keyboard layout (language) for Hyprland wayland compositor";
