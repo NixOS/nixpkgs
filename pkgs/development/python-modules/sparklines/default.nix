3f6f23155a8aafdd06756c87bad7fe679588fe17
David Chocholatý <chocholaty.david@protonmail.com>
2025-08-28 19:58:55 +0200

    python3Packages.sparklines: Move to pyproject, switch license GPLv3+ -> MIT

--- pkgs/development/python-modules/sparklines/default.nix
+++ pkgs/development/python-modules/sparklines/default.nix
@@ -2,14 +2,15 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  future,
+  hatchling,
+  termcolor,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "sparklines";
   version = "0.7.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "deeplook";
@@ -18,17 +19,29 @@ buildPythonPackage rec {
     sha256 = "sha256-jiMrxZMWN+moap0bDH+uy66gF4XdGst9HJpnboJrQm4=";
   };
 
-  propagatedBuildInputs = [ future ];
+  propagatedBuildInputs = [
+    hatchling
+    termcolor
+  ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "sparklines" ];
 
+  postPatch = ''
+    export TMPDIR=$PWD/tmp
+    mkdir -p $TMPDIR
+    substituteInPlace tests/test_sparkline.py \
+      --replace-fail "/tmp/" "$TMPDIR/"
+  '';
+
   meta = with lib; {
     description = "This Python package implements Edward Tufte's concept of sparklines, but limited to text only";
     mainProgram = "sparklines";
     homepage = "https://github.com/deeplook/sparklines";
-    maintainers = with maintainers; [ rhoriguchi ];
-    license = licenses.gpl3Only;
+    maintainers = with maintainers; [
+      rhoriguchi
+    ];
+    license = licenses.mit;
   };
 }
