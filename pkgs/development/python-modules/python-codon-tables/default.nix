c20e372ed2cffcd2bdf8892f06943435c64fe11d
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-28 17:35:55 +0200

    python313Packages.python-codon-tables: modernize

--- pkgs/development/python-modules/python-codon-tables/default.nix
+++ pkgs/development/python-modules/python-codon-tables/default.nix
@@ -2,18 +2,21 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "python-codon-tables";
   version = "0.1.18";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "python_codon_tables";
     inherit version;
     hash = "sha256-c/VSmArSkq+46LzW3r+CQEG1mwp87ACbZ7EWkMOGOQc=";
   };
+
+  build-system = [ setuptools ];
 
   # no tests in tarball
   doCheck = false;
