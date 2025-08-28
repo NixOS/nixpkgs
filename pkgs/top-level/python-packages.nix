0a0ba275686a1373199a593606215f8c06ead537
László Kupcsik <dev@biobits.be>
2025-08-28 15:34:25 +0200

    python3Packages.pyseq-align: init at v1.0.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14086,6 +14086,8 @@ self: super: with self; {
 
   pysensors = callPackage ../development/python-modules/pysensors { };
 
+  pyseq-align = callPackage ../development/python-modules/pyseq-align { };
+
   pysequoia = callPackage ../development/python-modules/pysequoia { };
 
   pyserial = callPackage ../development/python-modules/pyserial { };
