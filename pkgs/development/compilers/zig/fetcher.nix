29f79b32f8d9450505f7e1f0da7aaaab1080230b
Varun Narravula <varun@snare.dev>
2025-08-28 00:40:07 -0700

    zig: handle lazy dependencies in fetcher
    Prior to 0.15.1, the --fetch=all flag did not exist to eagerly fetch
    all dependencies, including lazy ones.
    
    This adds a parameter to the Zig fetcher to turn this behavior on
    for pre-0.15.1 Zig builds, and defaults to always fetching all
    dependencies for Zig versions afterwards.
    
    This fetching behavior should not be a default, since some packages
    have dependencies that require different Zig versions than the base
    package, oddly enough.
    
    If a package has the (probably rare) behavior of mixing lazy
    dependencies and different Zig versions amongst those dependencies,
    then this workaround may not work at all, and the toolchain should
    ideally be updated anyway.
    
    Additionally, this commit allows passing through patches that are
    required for building dependencies by using stdenv.mkDerivation
    to build the packages directory instead of pkgs.runCommand.

--- pkgs/development/compilers/zig/fetcher.nix
+++ pkgs/development/compilers/zig/fetcher.nix
@@ -1,7 +1,7 @@
 {
   lib,
+  stdenv,
   zig,
-  runCommand,
 }:
 {
   pname,
@@ -9,24 +9,87 @@
   name ? "${pname}-${version}",
   src,
   hash ? lib.fakeHash,
+  manuallyFetchLazyDeps ? false,
+  ...
 }@args:
-runCommand "${name}-zig-deps"
+
+assert if manuallyFetchLazyDeps then !(lib.versionAtLeast zig.version "0.15") else true;
+
+let
+  fetchCommand =
+    if lib.versionAtLeast zig.version "0.15" then
+      ''
+        TERM=dumb zig build --fetch=all
+      ''
+    else if !manuallyFetchLazyDeps then
+      ''
+        TERM=dumb zig build --fetch
+      ''
+    else
+      ''
+        manuallyFetchLazyDeps .
+      '';
+in
+stdenv.mkDerivation (
   {
-    inherit (args) src;
+    name = "${name}-zig-deps";
+    inherit src;
 
     nativeBuildInputs = [ zig ];
+
+    dontConfigure = true;
+
+    buildPhase = ''
+      runHook preBuild
+
+      export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
+
+      package_dirs_checked=()
+
+      # Manually try to recursively fetch all dependencies
+      # by stripping the .lazy = true from build.zig.zon
+      # manifests.
+      #
+      # This workaround fails if dependencies rely on diifferent
+      # Zig versions.
+      #
+      # This should not be used for post-0.15, since the
+      # --fetch=all flag exists.
+      function manuallyFetchLazyDeps() {
+        package="$1"
+
+        if [[ "''${package_dirs_checked[*]}" =~ $package ]]; then
+          return 0
+        fi
+        package_dirs_checked+=("$package")
+
+        if [ -e "$package/build.zig.zon" ]; then
+          sed -i '/^\s*\.lazy\s*=\s*true\s*,\s*$/d' "$package/build.zig.zon"
+
+          cd "$package" || exit 1
+          TERM=dumb zig build --fetch
+          for dir in "$ZIG_GLOBAL_CACHE_DIR"/p/*; do
+            manuallyFetchLazyDeps "$dir"
+          done
+        fi
+      }
+
+      ${fetchCommand}
+
+      runHook postBuild
+    '';
+
+    dontFixup = true;
+
+    installPhase = ''
+      runHook preInstall
+      mv $ZIG_GLOBAL_CACHE_DIR/p $out
+      runHook postInstall
+    '';
 
     outputHashAlgo = null;
     outputHashMode = "recursive";
     outputHash = hash;
   }
-  ''
-    export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
-
-    runHook unpackPhase
-
-    cd $sourceRoot
-    zig build --fetch
-
-    mv $ZIG_GLOBAL_CACHE_DIR/p $out
-  ''
+  // args
+)
