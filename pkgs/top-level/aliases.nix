70e1d138cfdb68678d300efc24d0a78f8107485f
qzylinra <qzylinra@users.noreply.github.com>
2025-08-29 10:58:30 +0800

    woof: drop
    The upstream code hasn’t been updated for nearly four years, and it’s already broken. https://github.com/simon-budig/woof/issues/19

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2439,6 +2439,7 @@ mapAliases {
   wkhtmltopdf-bin = wkhtmltopdf; # Added 2024-07-17
   wlroots_0_16 = throw "'wlroots_0_16' has been removed in favor of newer versions"; # Added 2024-07-14
   wlroots = wlroots_0_19; # wlroots is unstable, we must keep depending on 'wlroots_0_*', convert to package after a stable(1.x) release
+  woof = throw "'woof' has been removed as it is broken and unmaintained upstream."; # Added 2025-08-29
   wordpress6_3 = throw "'wordpress6_3' has been removed in favor of the latest version"; # Added 2024-08-03
   wordpress6_4 = throw "'wordpress6_4' has been removed in favor of the latest version"; # Added 2024-08-03
   wordpress6_5 = wordpress_6_5; # Added 2024-08-03
