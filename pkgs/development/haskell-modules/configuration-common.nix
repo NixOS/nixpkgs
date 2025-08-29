de495ba15b2872a23681bee017611a4a64c0a421
Vladimír Čunát <v@cunat.cz>
2025-08-29 18:24:07 +0200

    git-annex: patch another test failure
    https://git-annex.branchable.com/bugs/flaky_test_failure_add_dup/
    https://hydra.nixos.org/build/305309853
    Not-cherry-picked-because: nixpkgs master already contains this in src

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -516,6 +516,16 @@ self: super:
               "CHANGELOG"
             ];
           })
+          # https://git-annex.branchable.com/bugs/flaky_test_failure_add_dup/
+          (pkgs.fetchpatch {
+            name = "git-annex-workaround-for-git-2.50_bis.patch";
+            url = "https://git.joeyh.name/index.cgi/git-annex.git/patch/?id=cf449837ea9ab7687d8a157f21cad31ddf5bbfb6";
+            sha256 = "sha256-HmNJ85dLht5Hy85AUkjACnET9YLPP2MshYHsApUax+I=";
+            excludes = [
+              "doc/**"
+              "CHANGELOG"
+            ];
+          })
         ];
 
         postPatch = ''
