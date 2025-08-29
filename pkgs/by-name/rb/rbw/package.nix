606f051b8ec2a39bfbdbe04cb0f36a67fcb9f370
PerchunPak <git@perchun.it>
2025-08-29 11:42:29 +0200

    Revert "rbw: 1.13.2 -> 1.14.0"
    This reverts commit 9684c484070a8bc050244df21a988b82284c9d55.

--- pkgs/by-name/rb/rbw/package.nix
+++ pkgs/by-name/rb/rbw/package.nix
@@ -24,14 +24,14 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rbw";
-  version = "1.14.0";
+  version = "1.13.2";
 
   src = fetchzip {
     url = "https://git.tozt.net/rbw/snapshot/rbw-${version}.tar.gz";
-    hash = "sha256-5KEOjnhn12QnR2CBBuUMHLOOyfkCMY/R3LzoP/EfBbA=";
+    hash = "sha256-ebLbdIF+BybK7ssNtZacGWmAEwdNZh8b94QYgvcwzmM=";
   };
 
-  cargoHash = "sha256-GbPh999yM28qNRBpG2zs03EzwAKCG6CwqDP8JYedfl0=";
+  cargoHash = "sha256-xDb4shDHCbd0yuTSAt80i1aqyuhpkfd/fYF98CfXdcM=";
 
   nativeBuildInputs = [
     installShellFiles
