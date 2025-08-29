0beac304d7570c843ae914aea5af15c0f7f22297
Jörg Thalheim <joerg@thalheim.io>
2025-08-29 15:54:06 +0200

    ci: replace nix_2_24 with nix_2_28
    This is a partial backport of the nix_2_24 removal PR, containing
    only the CI testing changes that update the tested nix versions.
    
    (cherry picked from commit 1b7637ff0806095b09bfd4380d435050acffd006)

--- lib/tests/release.nix
+++ lib/tests/release.nix
@@ -16,7 +16,7 @@
   pkgsBB ? pkgs.pkgsBuildBuild,
   nix ? pkgs-nixVersions.stable,
   nixVersions ? [
-    pkgs-nixVersions.nix_2_24
+    pkgs-nixVersions.nix_2_28
     nix
     pkgs-nixVersions.latest
   ],
