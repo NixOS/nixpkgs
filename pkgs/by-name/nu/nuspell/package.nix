15f314e0449f4ad1d28305782ff42a02398538ee
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-08-29 17:26:56 -0400

    nuspell: Refactor to finalAttrs, ctestCheckHook, by-name

---
+++ pkgs/by-name/nu/nuspell/package.nix
@@ -0,0 +1,74 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  ctestCheckHook,
+  buildPackages,
+  pkg-config,
+  icu,
+  catch2_3,
+  testers,
+  enableManpages ? buildPackages.pandoc.compiler.bootstrapAvailable,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "nuspell";
+  version = "5.1.6";
+
+  src = fetchFromGitHub {
+    owner = "nuspell";
+    repo = "nuspell";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-U/lHSxpKsBnamf4ikE2aIjEPSU5fxjtuSmhZR0jxMAI=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ]
+  ++ lib.optional enableManpages buildPackages.pandoc;
+
+  strictDeps = true;
+  buildInputs = [ catch2_3 ];
+
+  propagatedBuildInputs = [ icu ];
+
+  cmakeFlags = lib.optional (!enableManpages) "-DBUILD_DOCS=OFF";
+
+  nativeCheckInputs = [
+    ctestCheckHook
+  ];
+
+  doCheck = true;
+
+  outputs = [
+    "out"
+    "lib"
+    "dev"
+  ];
+
+  passthru = {
+    tests = {
+      pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
+
+      cmake = testers.hasCmakeConfigModules {
+        moduleNames = [ "Nuspell" ];
+        package = finalAttrs.finalPackage;
+        version = finalAttrs.version;
+        versionCheck = true;
+      };
+    };
+  };
+
+  meta = {
+    description = "Free and open source C++ spell checking library";
+    mainProgram = "nuspell";
+    pkgConfigModules = [ "nuspell" ];
+    homepage = "https://nuspell.github.io/";
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ fpletz ];
+    license = lib.licenses.lgpl3Plus;
+    changelog = "https://github.com/nuspell/nuspell/blob/v${finalAttrs.version}/CHANGELOG.md";
+  };
+})
