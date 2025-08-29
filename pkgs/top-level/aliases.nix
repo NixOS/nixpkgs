9dd1ed3b6b9d3b14bb1c92238b65ab8edeb6b3c6
qzylinra <qzylinra@users.noreply.github.com>
2025-08-29 15:40:23 +0800

    wifi-password: drop
    The upstream project has not been updated for 9 years, and some features no longer work. https://github.com/rauchg/wifi-password/issues/34

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2432,6 +2432,7 @@ mapAliases {
   webmetro = throw "'webmetro' has been removed due to lack of upstream maintenance"; # Added 2025-01-25
   wg-bond = throw "'wg-bond' has been removed due to lack of upstream maintenance"; # Added 2025-01-25
   whatsapp-for-linux = wasistlos; # Added 2025-01-30
+  wifi-password = throw "'wifi-password' has been removed as it was unmaintained upstream"; # Added 2025-08-29
   wineWayland = wine-wayland;
   winhelpcgi = throw "'winhelpcgi' has been removed as it was unmaintained upstream and broken with GCC 14"; # Added 2025-06-14
   win-virtio = virtio-win; # Added 2023-10-17
