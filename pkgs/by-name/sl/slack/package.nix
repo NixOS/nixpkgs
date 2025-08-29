ec0c44f9c3f53716938c90090e3600f13b6eb6fe
Sizhe Zhao <prc.zhao@outlook.com>
2025-08-29 20:03:23 +0800

    slack: add sources.nix & fix update.sh

--- pkgs/by-name/sl/slack/package.nix
+++ pkgs/by-name/sl/slack/package.nix
@@ -7,45 +7,9 @@
 
 let
   inherit (stdenvNoCC.hostPlatform) system;
+  sources = import ./sources.nix { inherit fetchurl; };
 
   pname = "slack";
-
-  x86_64-darwin-version = "4.45.64";
-  x86_64-darwin-sha256 = "0skhh16lc0czxd5ifkqy39hibk4ydlfhn41zdif3hl0sd4vcqbvb";
-
-  x86_64-linux-version = "4.45.64";
-  x86_64-linux-sha256 = "7c6af86ab1d5778aec930d4e7d77b9f9948a83a87e8458e821d6f9e8dfed180f";
-
-  aarch64-darwin-version = "4.45.64";
-  aarch64-darwin-sha256 = "136crd17ybaznp680qb2rl0c8cllkkv21ymf3dck2jhkqbp7v2kj";
-
-  sources =
-    let
-      base = "https://downloads.slack-edge.com";
-    in
-    {
-      x86_64-darwin = rec {
-        version = x86_64-darwin-version;
-        src = fetchurl {
-          url = "${base}/desktop-releases/mac/universal/${version}/Slack-${version}-macOS.dmg";
-          sha256 = x86_64-darwin-sha256;
-        };
-      };
-      x86_64-linux = rec {
-        version = x86_64-linux-version;
-        src = fetchurl {
-          url = "${base}/desktop-releases/linux/x64/${version}/slack-desktop-${version}-amd64.deb";
-          sha256 = x86_64-linux-sha256;
-        };
-      };
-      aarch64-darwin = rec {
-        version = aarch64-darwin-version;
-        src = fetchurl {
-          url = "${base}/desktop-releases/mac/arm64/${version}/Slack-${version}-macOS.dmg";
-          sha256 = aarch64-darwin-sha256;
-        };
-      };
-    };
 
   passthru = {
     updateScript = ./update.sh;
