5a7411b4715e3e572c0d63fb7477a8d18efcfba7
Varun Narravula <varun@snare.dev>
2025-08-29 15:47:44 -0700

    superhtml: use native nixpkgs fetcher for zig dependencies

--- pkgs/by-name/su/superhtml/package.nix
+++ pkgs/by-name/su/superhtml/package.nix
@@ -1,28 +1,28 @@
 {
   lib,
-  callPackage,
   fetchFromGitHub,
   stdenv,
   zig_0_13,
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "superhtml";
   version = "0.5.3";
 
   src = fetchFromGitHub {
     owner = "kristoff-it";
     repo = "superhtml";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-rO7HS07nSqwOq6345q/SOL2imoD0cKV16QJcVVr6mHw=";
+  };
+
+  zigDeps = zig_0_13.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-ZkXhfamOlPzW3outAZy8h2PVS/z5AfcfF4jj1MDmtZA=";
   };
 
   nativeBuildInputs = [
     zig_0_13.hook
   ];
-
-  postPatch = ''
-    ln -s ${callPackage ./deps.nix { }} $ZIG_GLOBAL_CACHE_DIR/p
-  '';
 
   meta = with lib; {
     description = "HTML Language Server and Templating Language Library";
@@ -32,4 +32,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ petertriho ];
     platforms = platforms.unix;
   };
-}
+})
