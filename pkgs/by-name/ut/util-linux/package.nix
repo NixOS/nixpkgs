NixOS/nixpkgs

commit 025e6374343d4729e4c70ef5a4eb0200ddfdecb0

util-linux: split lastlog into dedicated output

pkgs/by-name/ut/util-linux/package.nix
@@ -28,6 +28,8 @@
   installShellFiles,
   writeSupport ? stdenv.hostPlatform.isLinux,
   shadowSupport ? stdenv.hostPlatform.isLinux,
+  # Doesn't build on Darwin, also doesn't really make sense on Darwin
+  withLastlog ? !stdenv.hostPlatform.isDarwin,
   gitUpdater,
 }:
 
@@ -72,10 +74,15 @@ stdenv.mkDerivation (finalPackage: rec {
     "out"
     "lib"
     "man"
+    "login"
   ]
-  ++ lib.optionals stdenv.hostPlatform.isLinux [ "mount" ]
-  ++ [ "login" ]
-  ++ lib.optionals stdenv.hostPlatform.isLinux [ "swap" ];
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    "mount"
+    "swap"
+  ]
+  ++ lib.optionals withLastlog [
+    "lastlog"
+  ];
   separateDebugInfo = true;
 
   postPatch = ''
@@ -129,8 +136,7 @@ stdenv.mkDerivation (finalPackage: rec {
     "--disable-ipcrm"
     "--disable-ipcs"
   ]
-  ++ lib.optionals stdenv.hostPlatform.isDarwin [
-    # Doesn't build on Darwin, also doesn't really make sense on Darwin
+  ++ lib.optionals (!withLastlog) [
     "--disable-liblastlog2"
   ]
   ++ lib.optionals stdenv.hostPlatform.isStatic [
@@ -182,6 +188,18 @@ stdenv.mkDerivation (finalPackage: rec {
       moveToOutput sbin/sulogin "$login"
       prefix=$login _moveSbin
       ln -svf "$login/bin/"* $bin/bin/
+    ''
+    + lib.optionalString withLastlog ''
+      # moveToOutput "lib/liblastlog2*" "$lastlog"
+      ${lib.optionalString (!stdenv.hostPlatform.isStatic) ''moveToOutput "lib/security" "$lastlog"''}
+      moveToOutput "lib/tmpfiles.d/lastlog2-tmpfiles.conf" "$lastlog"
+
+      moveToOutput "lib/systemd/system/lastlog2-import.service" "$lastlog"
+      substituteInPlace $lastlog/lib/systemd/system/lastlog2-import.service \
+        --replace-fail "$bin/bin/lastlog2" "$lastlog/bin/lastlog2"
+
+      moveToOutput "bin/lastlog2" "$lastlog"
+      ln -svf "$lastlog/bin/"* $bin/bin/
     ''
     + lib.optionalString stdenv.hostPlatform.isLinux ''
 

pkgs/top-level/all-packages.nix
@@ -11387,6 +11387,7 @@ with pkgs;
     shadowSupport = false;
     systemdSupport = false;
     translateManpages = false;
+    withLastlog = false;
   };
 
   v4l-utils = qt6.callPackage ../os-specific/linux/v4l-utils { };

commit 8fb0716cb97a6e8846e7abdcd82433880f3ed34d

util-linux: fix build with systemd disabled
Fixes: 025e6374343d ("util-linux: split lastlog into dedicated output")

pkgs/by-name/ut/util-linux/package.nix
@@ -188,12 +188,13 @@ stdenv.mkDerivation (finalPackage: rec {
       ${lib.optionalString (!stdenv.hostPlatform.isStatic) ''moveToOutput "lib/security" "$lastlog"''}
       moveToOutput "lib/tmpfiles.d/lastlog2-tmpfiles.conf" "$lastlog"
 
+      moveToOutput "bin/lastlog2" "$lastlog"
+      ln -svf "$lastlog/bin/"* $bin/bin/
+    ''
+    + lib.optionalString (withLastlog && systemdSupport) ''
       moveToOutput "lib/systemd/system/lastlog2-import.service" "$lastlog"
       substituteInPlace $lastlog/lib/systemd/system/lastlog2-import.service \
         --replace-fail "$bin/bin/lastlog2" "$lastlog/bin/lastlog2"
-
-      moveToOutput "bin/lastlog2" "$lastlog"
-      ln -svf "$lastlog/bin/"* $bin/bin/
     ''
     + lib.optionalString stdenv.hostPlatform.isLinux ''
 