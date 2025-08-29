fd9d91809dfb27cce9ff5c710bc85c2232b36352
Fabian Affolter <fabian@affolter-engineering.ch>
2025-08-29 12:13:41 +0200

    python312Packages.mypy-boto3-timestream-query: 1.40.0 -> 1.40.20

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1353,8 +1353,8 @@ rec {
       "sha256-hMLxKQ6aMwdquw2oOgeB33OigX25MOQFJhGEBfEyf7U=";
 
   mypy-boto3-timestream-query =
-    buildMypyBoto3Package "timestream-query" "1.40.0"
-      "sha256-lhLcDJRzRC1F22EtoZZaG5tXh8gs7xCnfU6GHb2O7bA=";
+    buildMypyBoto3Package "timestream-query" "1.40.20"
+      "sha256-EHQ0m8Pr8I+CxxOolkFP6LtY4V0qGosNQTPukYg8PQ8=";
 
   mypy-boto3-timestream-write =
     buildMypyBoto3Package "timestream-write" "1.40.0"
