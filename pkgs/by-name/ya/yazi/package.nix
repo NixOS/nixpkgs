f698e18f9b904c02a1c43e1f40751ecc2b77c4c4
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-08-29 16:56:46 +0300

    yazi: use `lib.getExe'`

--- pkgs/by-name/ya/yazi/package.nix
+++ pkgs/by-name/ya/yazi/package.nix
@@ -99,8 +99,8 @@ runCommand yazi-unwrapped.name
   ''
     mkdir -p "$out/bin"
     ln -s "${yazi-unwrapped}/share" "$out/share"
-    ln -s ${yazi-unwrapped}/bin/ya "$out/bin/ya"
-    makeWrapper ${yazi-unwrapped}/bin/yazi "$out/bin/yazi" \
+    ln -s ${lib.getExe' yazi-unwrapped "ya"} "$out/bin/ya"
+    makeWrapper ${lib.getExe' yazi-unwrapped "yazi"} "$out/bin/yazi" \
       --prefix PATH : ${lib.makeBinPath runtimePaths} \
       ${lib.optionalString (configHome != null) "--set YAZI_CONFIG_HOME ${configHome}"}
   ''
