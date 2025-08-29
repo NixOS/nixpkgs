f9a049cb1b4c4d7ab5077c9035d3f36822448748
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-08-29 18:29:21 -0400

    podman: enable additional man page checks
    `docs` target uses `man -l` to validate some issues with tables.

--- pkgs/by-name/po/podman/package.nix
+++ pkgs/by-name/po/podman/package.nix
@@ -35,6 +35,7 @@
   vfkit,
   versionCheckHook,
   writableTmpDirAsHomeHook,
+  man-db,
 }:
 let
   # do not add qemu to this wrapper, store paths get written to the podman vm config and break when GCed
@@ -102,6 +103,7 @@ buildGoModule rec {
   nativeBuildInputs = [
     pkg-config
     go-md2man
+    man-db
     installShellFiles
     makeWrapper
     python3
