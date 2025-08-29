71c1ffe6307421deacc6cfd21a6f21d3f9fd8e05
botnk <github-botnk@korz.dev>
2025-08-29 01:33:20 +0000

    zed-editor: 0.201.5 -> 0.201.6
    Changelog: https://github.com/zed-industries/zed/releases/tag/v0.201.6

--- pkgs/by-name/ze/zed-editor/package.nix
+++ pkgs/by-name/ze/zed-editor/package.nix
@@ -99,7 +99,7 @@ let
 in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zed-editor";
-  version = "0.201.5";
+  version = "0.201.6";
 
   outputs = [
     "out"
@@ -112,7 +112,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "zed-industries";
     repo = "zed";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-KMUF8k+cJg44Ta5Txb67vbO0zFTU2mKtLODhqbj6xXk=";
+    hash = "sha256-d1B04N5KS34ghUHOhStkCMZZosQe2yG2cu88KVxujKY=";
   };
 
   patches = [
@@ -138,7 +138,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
         --replace-fail "inner.redirect(policy)" "inner.redirect_policy(policy)"
     '';
 
-  cargoHash = "sha256-dKjp3gw+woqT3qqXbwbe57exqzcaufiMSR4RnrImXFI=";
+  cargoHash = "sha256-npuNvJUN9ENNB1G8kwcz8dRdFoLBJ/UgQHd+9ojW8PI=";
 
   nativeBuildInputs = [
     cmake
