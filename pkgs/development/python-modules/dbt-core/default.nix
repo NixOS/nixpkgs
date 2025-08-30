c4c9605f46a1e91a7dbe7afa8344ab095b57bfb9
R. Ryantm <ryantm-bot@ryantm.com>
2025-08-30 11:57:23 +0000

    dbt: 1.10.9 -> 1.9.10

--- pkgs/development/python-modules/dbt-core/default.nix
+++ pkgs/development/python-modules/dbt-core/default.nix
@@ -37,14 +37,14 @@
 
 buildPythonPackage rec {
   pname = "dbt-core";
-  version = "1.10.9";
+  version = "1.9.10";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dbt-labs";
     repo = "dbt-core";
     tag = "v${version}";
-    hash = "sha256-4K00EVTOTTUHWwTpBlXKoHGof/s/H2acoWZPJ9FmBuk=";
+    hash = "sha256-4lVuocNIjA7Sf9uSJYEqiC2ZxDQtqShP8lRNjQyz778=";
   };
 
   sourceRoot = "${src.name}/core";
