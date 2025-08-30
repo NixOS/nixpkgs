74bf1d7bd215840004eca6e9b8f2bef4bef43f01
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-30 21:52:08 +0200

    font-sun-misc: refactor, move to pkgs/by-name and rename from xorg.fontsunmisc
    relevant changes:
    - font is now in $out/share/fonts/X11 instead of $out/lib/X11/fonts
      i have no idea why fonts would be in lib, so lets see if this breaks
      anything
    - remove some unneeded deps and configureFlags

--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -342,6 +342,7 @@ print OUT <<EOF;
   font-schumacher-misc,
   font-screen-cyrillic,
   font-sony-misc,
+  font-sun-misc,
   font-util,
   gccmakedep,
   ico,
@@ -514,6 +515,7 @@ self: with self; {
   fontschumachermisc = font-schumacher-misc;
   fontscreencyrillic = font-screen-cyrillic;
   fontsonymisc = font-sony-misc;
+  fontsunmisc = font-sun-misc;
   fontutil = font-util;
   libAppleWM = libapplewm;
   libFS = libfs;
