7160501dbef5969e75f3a71f7fb530c40d9e1344
Arnout Engelen <arnout@bzzt.net>
2025-08-30 10:23:35 +0200

    lvm2: make manpage generation reproducible
    Fixes #434933

--- pkgs/os-specific/linux/lvm2/common.nix
+++ pkgs/os-specific/linux/lvm2/common.nix
@@ -128,6 +128,8 @@ stdenv.mkDerivation rec {
       }
     ))
     ./fix-stdio-usage.patch
+    # https://gitlab.com/lvmteam/lvm2/-/merge_requests/33
+    ./fix-manpage-reproducibility.patch
   ];
 
   doCheck = false; # requires root
