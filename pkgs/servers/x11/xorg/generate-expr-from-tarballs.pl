596a30a6efd9fde368d5dcc7d3e4079841d461c9
quantenzitrone <general@dev.quantenzitrone.eu>
2025-08-30 23:16:02 +0200

    font-xfree86-type1: refactor, move to pkgs/by-name and rename from xorg.fontxfree86type1
    relevant changes:
    - font is now in $out/share/fonts/X11 instead of $out/lib/X11/fonts
      i have no idea why fonts would be in lib, so lets see if this breaks
      anything
      - fix xorg nixos module accordingly
    - remove some unneeded deps and configureFlags

--- pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
+++ pkgs/servers/x11/xorg/generate-expr-from-tarballs.pl
@@ -345,6 +345,7 @@ print OUT <<EOF;
   font-sun-misc,
   font-util,
   font-winitzki-cyrillic,
+  font-xfree86-type1,
   gccmakedep,
   ico,
   imake,
@@ -519,6 +520,7 @@ self: with self; {
   fontsunmisc = font-sun-misc;
   fontutil = font-util;
   fontwinitzkicyrillic = font-winitzki-cyrillic;
+  fontxfree86type1 = font-xfree86-type1;
   libAppleWM = libapplewm;
   libFS = libfs;
   libICE = libice;
