ab82350f9c3fceb2b5c7c5a7a743fa72208e3b00
Wolfgang Walther <walther@technowledgy.de>
2025-08-30 14:57:06 +0200

    nixf-diagnose: 0.1.2 -> 0.1.3
    Changes:
    https://github.com/inclyc/nixf-diagnose/compare/0.1.2...0.1.3

--- pkgs/by-name/ni/nixf-diagnose/package.nix
+++ pkgs/by-name/ni/nixf-diagnose/package.nix
@@ -8,18 +8,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nixf-diagnose";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchFromGitHub {
     owner = "inclyc";
     repo = "nixf-diagnose";
     tag = finalAttrs.version;
-    hash = "sha256-gkeU3EwAl9810eRRp5/ddf1h0qpV6FrBBdntNBpBtsM=";
+    hash = "sha256-8kcA2/ZMREKtXUM5rlAWRQL/C8+JNocZegq2ZHqbiSA=";
   };
 
   env.NIXF_TIDY_PATH = lib.getExe nixf;
 
-  cargoHash = "sha256-nrr2/lTWPyH7MsG2hSMJjbFCpHsKWINEP8jwSYPhocg=";
+  cargoHash = "sha256-9rWQfoaMXFs83cYHtJPL0ogA9hPh7q3mK1DG4Q4CCq0=";
 
   passthru.updateScript = nix-update-script { };
 
