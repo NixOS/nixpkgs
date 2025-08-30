e90c4fcba7fa3cf4afbe280e0d9368bbae12794b
Austin Horstman <khaneliman12@gmail.com>
2025-08-30 14:51:36 -0500

    vimPlugins.statix: move to non-generated
    Looks like it was missed in `overrides.nix` when it should be in
    non-generated.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -44,7 +44,6 @@
   ripgrep,
   sqlite,
   sshfs,
-  statix,
   stylish-haskell,
   tabnine,
   taskwarrior2,
@@ -3258,20 +3257,6 @@ in
 
   startup-nvim = super.startup-nvim.overrideAttrs {
     dependencies = [ self.plenary-nvim ];
-  };
-
-  statix = buildVimPlugin rec {
-    inherit (statix) pname src meta;
-    version = "0.1.0";
-    postPatch = ''
-      # check that version is up to date
-      grep 'pname = "statix-vim"' -A 1 flake.nix \
-        | grep -F 'version = "${version}"'
-
-      cd vim-plugin
-      substituteInPlace ftplugin/nix.vim --replace-fail statix ${statix}/bin/statix
-      substituteInPlace plugin/statix.vim --replace-fail statix ${statix}/bin/statix
-    '';
   };
 
   stylish-nvim = super.stylish-nvim.overrideAttrs {
