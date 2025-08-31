ca1f929d0ffcb32ad4be770137d38eeef06c4ac3
Felix Singer <felixsinger@posteo.net>
2025-08-31 11:05:53 +0200

    ghidra: make scripts working
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- pkgs/tools/security/ghidra/default.nix
+++ pkgs/tools/security/ghidra/default.nix
@@ -74,6 +74,14 @@ stdenv.mkDerivation rec {
 
     # Needed by the Ghidra Server NixOS module. Create a symlink in order to provide a stable path.
     ln -s "$out/lib/ghidra/Ghidra/Features/GhidraServer/data/yajsw-stable-13.12" "$out/lib/ghidra/Ghidra/Features/GhidraServer/data/yajsw"
+
+    # TODO: description
+    substituteInPlace "$out/lib/ghidra/server/ghidraSvr" \
+      --replace-fail "\''${SCRIPT_DIR}/server.conf" "/etc/ghidra-server.conf"
+
+    # TODO: description
+    substituteInPlace "$out/lib/ghidra/server/svrAdmin" \
+      --replace-fail "\''${SCRIPT_DIR}/server.conf" "/etc/ghidra-server.conf"
   '';
 
   meta = with lib; {
