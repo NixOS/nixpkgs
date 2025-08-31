f24dc4fb6cc85d9cbcda64090892cab4b581c05b
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-08-31 08:35:30 +0300

    go-pray: refactor to remove build commit and time workaround
    This approach of keeping the required metadata in the actual package.nix
    maintains reproducibility while keeping metadata required by upstream
    package.

--- pkgs/by-name/go/go-pray/package.nix
+++ pkgs/by-name/go/go-pray/package.nix
@@ -7,22 +7,22 @@
   alsa-lib,
 }:
 
-buildGoModule (finalAttrs: {
+let
+  metaVersion = "0.1.13";
+  # NOTE: git rev-parse --verify v${metaVersion}
+  metaCommit = "1496a6f3c2bad88a5e4eb542b613015e0f480cdd";
+  # NOTE: date -ud "@$(git log -1 --pretty=%ct ${metaCommit})" '+%Y-%m-%dT%H:%M:%SZ'
+  metaCommitTimeRFC3339 = "2025-08-02T08:54:24Z";
+in
+buildGoModule {
   pname = "go-pray";
-  version = "0.1.13";
+  version = metaVersion;
 
   src = fetchFromGitHub {
     owner = "0xzer0x";
     repo = "go-pray";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-qMADzeZgZR9iu6A8UffTG+5MbPGm7vKMvhmH5fME7qg=";
-    # NOTE: Required for setting build time
-    leaveDotGit = true;
-    postFetch = ''
-      cd $out
-      date -u -d "@$(git log -1 --pretty=%ct)" "+%Y-%m-%dT%H:%M:%SZ" > $out/SOURCE_DATE_EPOCH
-      find "$out" -name .git -print0 | xargs -0 rm -rf
-    '';
+    rev = metaCommit;
+    hash = "sha256-wi/sUjzNbVLbqwtM1BglmXgkaqMamjIsGS6EjlyeC9Y=";
   };
 
   vendorHash = "sha256-qMTg2Vsk0nte1O8sbNWN5CCCpgpWLvcb2RuGMoEngYE=";
@@ -33,12 +33,10 @@ buildGoModule (finalAttrs: {
     installShellFiles
   ];
   ldflags = [
-    "-X github.com/0xzer0x/go-pray/internal/version.version=${finalAttrs.version}"
+    "-X github.com/0xzer0x/go-pray/internal/version.version=${metaVersion}"
+    "-X github.com/0xzer0x/go-pray/internal/version.buildCommit=${metaCommit}"
+    "-X github.com/0xzer0x/go-pray/internal/version.buildTime=${metaCommitTimeRFC3339}"
   ];
-
-  preBuild = ''
-    ldflags+=" -X github.com/0xzer0x/go-pray/internal/version.buildTime=$(cat SOURCE_DATE_EPOCH)"
-  '';
 
   buildInputs = [ alsa-lib ];
 
@@ -54,10 +52,10 @@ buildGoModule (finalAttrs: {
   meta = with lib; {
     description = "Prayer times CLI to remind you to Go pray";
     homepage = "https://github.com/0xzer0x/go-pray";
-    changelog = "https://github.com/0xzer0x/go-pray/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/0xzer0x/go-pray/releases/tag/v${metaVersion}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ _0xzer0x ];
     platforms = platforms.linux;
     mainProgram = "go-pray";
   };
-})
+}
