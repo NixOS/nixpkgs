d8b49c68a95b2006d98e68aa11b344538d0e635b
Shawn8901 <shawn8901@googlemail.com>
2025-08-31 19:56:00 +0200

    portfolio: 0.79.0 -> 0.79.1

--- pkgs/by-name/po/portfolio/package.nix
+++ pkgs/by-name/po/portfolio/package.nix
@@ -34,11 +34,11 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "PortfolioPerformance";
-  version = "0.79.0";
+  version = "0.79.1";
 
   src = fetchurl {
     url = "https://github.com/buchen/portfolio/releases/download/${finalAttrs.version}/PortfolioPerformance-${finalAttrs.version}-linux.gtk.x86_64.tar.gz";
-    hash = "sha256-BdpaueOSz+CH0za2uj7oJXKEgpd0R6yTyVO+Au78oCs=";
+    hash = "sha256-oJjYnJvgNpYpbJ5QSV4dwaB7FEaIqCbkAmzsYpjyfbc=";
   };
 
   nativeBuildInputs = [
