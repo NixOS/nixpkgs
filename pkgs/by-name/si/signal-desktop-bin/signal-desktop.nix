82e2bf139fb8e5650fefbfa50ed0da19fc4663f3
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-08-31 23:48:47 +0200

    signal-desktop-bin: 7.64.0 -> 7.69.0

--- pkgs/by-name/si/signal-desktop-bin/signal-desktop.nix
+++ pkgs/by-name/si/signal-desktop-bin/signal-desktop.nix
@@ -1,12 +1,12 @@
 { callPackage, commandLineArgs }:
 callPackage ./generic.nix { inherit commandLineArgs; } rec {
   pname = "signal-desktop-bin";
-  version = "7.64.0";
+  version = "7.69.0";
 
   libdir = "opt/Signal";
   bindir = libdir;
   extractPkg = "dpkg-deb -x $downloadedFile $out";
 
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-3mibhZJpY/yo4RzZ6LQbI5xUlGgK6zGtB48Q8yDgflc=";
+  hash = "sha256-QFK/6f+/NGHfKCu52EpbpW98dwOmCL52vjmDpxR3WXk=";
 }
