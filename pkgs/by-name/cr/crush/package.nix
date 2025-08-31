a4249d25b9c9d85018c77912d3eb6798782592a6
Herwig Hochleitner <herwig@bendlas.net>
2025-08-31 21:09:26 +0200

    crush: 0.7.2 -> 0.7.4

--- pkgs/by-name/cr/crush/package.nix
+++ pkgs/by-name/cr/crush/package.nix
@@ -9,13 +9,13 @@
 
 buildGo125Module (finalAttrs: {
   pname = "crush";
-  version = "0.7.2";
+  version = "0.7.4";
 
   src = fetchFromGitHub {
     owner = "charmbracelet";
     repo = "crush";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-UZVdtF4wjUBhFrhC8Ztc5DRBoAotatfrCrvoq+KTAbc=";
+    hash = "sha256-UWrvMPti+d0O7+22QMO8LjoRITNbDcKGf9oKbo40Lx0=";
   };
 
   vendorHash = "sha256-CXjQtc8DXGb15Qz3RSkE7S7uL3h2k3g6fuO8L1znSJ0=";
