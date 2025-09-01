cfa7d4d560265fc7d880108d0f416a8f028627ab
Wolfgang Walther <walther@technowledgy.de>
2025-09-01 22:07:27 +0200

    haskell.compiler.ghc966: drop
    Latest 9.6.x minor release is 9.6.7, which is also in Stackage 22.44.
    
    Thus, dropping according to the GHC Deprecation Policy.
    
    (cherry picked from commit a8920172f0ee823eb9e2371a24c600d0f9f599fc)

--- pkgs/top-level/release-haskell.nix
+++ pkgs/top-level/release-haskell.nix
@@ -68,7 +68,6 @@ let
     ghc928
     ghc948
     ghc963
-    ghc966
     ghc967
     ghc981
     ghc982
@@ -646,7 +645,7 @@ let
       semaphore-compat = [
         # Compiler < 9.8 don't have the semaphore-compat core package, but
         # requires unix >= 2.8.1.0 which implies GHC >= 9.6 for us.
-        compilerNames.ghc966
+        compilerNames.ghc967
       ];
       weeder = lib.subtractLists [
         compilerNames.ghc9101
