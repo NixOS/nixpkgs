3274ec954b9f95dd92506620bcb84ac571f13208
Aljaž Mur Eržen <aljaz@erzen.si>
2025-09-01 14:18:11 +0200

    maintainers: add aljazerzen

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1283,6 +1283,12 @@
     githubId = 8614547;
     name = "Ali Caglayan";
   };
+  aljazerzen = {
+    email = "aljaz@erzen.si";
+    github = "aljazerzen";
+    githubId = 11072061;
+    name = "Aljaž Mur Eržen";
+  };
   alkasm = {
     email = "alexreynolds00@gmail.com";
     github = "alkasm";
