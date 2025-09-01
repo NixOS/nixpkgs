4568367fbcca05f5114c0813b8d6cbae2de92198
Adam C. Stephens <adam@valkor.net>
2025-09-01 16:46:54 -0400

    kanidm_1_6: mark unsupported
    (cherry picked from commit 1b2fd8d56f282b59668b181147ec67b2120ae30b)

--- pkgs/by-name/ka/kanidm/1_6.nix
+++ pkgs/by-name/ka/kanidm/1_6.nix
@@ -2,4 +2,5 @@ import ./generic.nix {
   version = "1.6.4";
   hash = "sha256-ui3w1HDHXHARsjQ3WtJfZbM7Xgg3ODnUneXJMQwaOMw=";
   cargoHash = "sha256-KJGELBzScwsLd6g3GR9Vk0nfDU2EjZBfXwlXJ+bZb1k=";
+  unsupported = true;
 }
