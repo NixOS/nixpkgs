bdfb978c291bb691120ebfad96408cb97f16ae6a
kyehn <kyehn@users.noreply.github.com>
2025-09-01 18:08:58 +0800

    tun2proxy: use finalAttrs

--- pkgs/by-name/tu/tun2proxy/package.nix
+++ pkgs/by-name/tu/tun2proxy/package.nix
@@ -4,13 +4,13 @@
   fetchCrate,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "tun2proxy";
   version = "0.7.14";
 
   src = fetchCrate {
     pname = "tun2proxy";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-rrBlCtimcQJ8487X5wxsWVk20v9UK0+0B6HRdzV5Sj0=";
   };
 
@@ -21,9 +21,9 @@ rustPlatform.buildRustPackage rec {
   meta = {
     homepage = "https://github.com/tun2proxy/tun2proxy";
     description = "Tunnel (TUN) interface for SOCKS and HTTP proxies";
-    changelog = "https://github.com/tun2proxy/tun2proxy/releases/tag/v${version}";
+    changelog = "https://github.com/tun2proxy/tun2proxy/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     mainProgram = "tun2proxy-bin";
     maintainers = with lib.maintainers; [ mksafavi ];
   };
-}
+})
