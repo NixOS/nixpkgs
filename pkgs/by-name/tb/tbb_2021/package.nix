fe1f5a568b7e060e08427ddb0379276787edc814
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-09-01 15:59:48 +0200

    tbb_2021: put NIX_CFLAGS_COMPILE under env to fix withCFlags

--- pkgs/by-name/tb/tbb_2021/package.nix
+++ pkgs/by-name/tb/tbb_2021/package.nix
@@ -47,25 +47,28 @@ stdenv.mkDerivation (finalAttrs: {
     "--ignore-whitespace"
   ];
 
-  # Fix build with modern gcc
-  # In member function 'void std::__atomic_base<_IntTp>::store(__int_type, std::memory_order) [with _ITp = bool]',
-  NIX_CFLAGS_COMPILE =
-    lib.optionals stdenv.cc.isGNU [
-      "-Wno-error=array-bounds"
-      "-Wno-error=stringop-overflow"
-    ]
-    ++
-      # error: variable 'val' set but not used
-      lib.optionals stdenv.cc.isClang [ "-Wno-error=unused-but-set-variable" ]
-    ++
-      # Workaround for gcc-12 ICE when using -O3
-      # https://gcc.gnu.org/PR108854
-      lib.optionals (stdenv.cc.isGNU && stdenv.hostPlatform.isx86_32) [ "-O2" ];
+  env = {
+    # Fix build with modern gcc
+    # In member function 'void std::__atomic_base<_IntTp>::store(__int_type, std::memory_order) [with _ITp = bool]',
+    NIX_CFLAGS_COMPILE = toString (
+      lib.optionals stdenv.cc.isGNU [
+        "-Wno-error=array-bounds"
+        "-Wno-error=stringop-overflow"
+      ]
+      ++
+        # error: variable 'val' set but not used
+        lib.optionals stdenv.cc.isClang [ "-Wno-error=unused-but-set-variable" ]
+      ++
+        # Workaround for gcc-12 ICE when using -O3
+        # https://gcc.gnu.org/PR108854
+        lib.optionals (stdenv.cc.isGNU && stdenv.hostPlatform.isx86_32) [ "-O2" ]
+    );
 
-  # Fix undefined reference errors with version script under LLVM.
-  NIX_LDFLAGS = lib.optionalString (
-    stdenv.cc.bintools.isLLVM && lib.versionAtLeast stdenv.cc.bintools.version "17"
-  ) "--undefined-version";
+    # Fix undefined reference errors with version script under LLVM.
+    NIX_LDFLAGS = lib.optionalString (
+      stdenv.cc.bintools.isLLVM && lib.versionAtLeast stdenv.cc.bintools.version "17"
+    ) "--undefined-version";
+  };
 
   # Disable failing test on musl
   # test/conformance/conformance_resumable_tasks.cpp:37:24: error: ‘suspend’ is not a member of ‘tbb::v1::task’; did you mean ‘tbb::detail::r1::suspend’?
