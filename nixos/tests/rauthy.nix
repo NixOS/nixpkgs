988c84428483aa535772e234b9b4d077e5ca1d06
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-01 21:39:32 +0200

    nixos/rauthy: init

---
+++ nixos/tests/rauthy.nix
@@ -0,0 +1,31 @@
+{ lib, ... }:
+{
+  name = "rauthy";
+  meta.maintainers = with lib.maintainers; [
+    gepbird
+  ];
+
+  nodes.machine =
+    { ... }:
+    {
+      services.rauthy = {
+        enable = true;
+      };
+
+      services.postgresql = {
+        enable = true;
+        ensureDatabases = [ "rauthy" ];
+        ensureUsers = [
+          {
+            name = "rauthy";
+            ensureDBOwnership = true;
+          }
+        ];
+      };
+    };
+
+  testScript = ''
+    machine.wait_for_unit("rauthy.service")
+    machine.succeed("sleep 5")
+  '';
+}
