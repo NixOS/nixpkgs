49ea5bed2c761eda4cf60077b10d90fd3892868c
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-09-01 19:38:02 -0400

    catt: 0.12.11 -> 0.13.1

--- pkgs/by-name/ca/catt/package.nix
+++ pkgs/by-name/ca/catt/package.nix
@@ -1,60 +1,28 @@
 {
   lib,
   fetchPypi,
-  fetchpatch,
   python3Packages,
 }:
-let
-  pythonPackages = python3Packages.overrideScope (
-    _: prev: {
-      pychromecast = prev.pychromecast.overridePythonAttrs (
-        let
-          version = "13.1.0";
-        in
-        {
-          inherit version;
-
-          src = fetchPypi {
-            pname = "PyChromecast";
-            inherit version;
-            hash = "sha256-COYai1S9IRnTyasewBNtPYVjqpfgo7V4QViLm+YMJnY=";
-          };
-
-          postPatch = "";
-        }
-      );
-    }
-  );
-in
-pythonPackages.buildPythonApplication rec {
+python3Packages.buildPythonApplication rec {
   pname = "catt";
-  version = "0.12.11";
+  version = "0.13.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0bqYYfWwF7yYoAbjZPhi/f4CLcL89imWGYaMi5Bwhtc=";
+    hash = "sha256-hlCB06l4nzafvcnBNCXWiJsJNmP8n731bQgq5xvUZvM=";
   };
 
-  patches = [
-    (fetchpatch {
-      # set explicit build-system
-      url = "https://github.com/skorokithakis/catt/commit/08e7870a239e85badd30982556adc2aa8a8e4fc1.patch";
-      hash = "sha256-QH5uN3zQNVPP6Th2LHdDBF53WxwMhoyhhQUAZOeHh4k=";
-    })
-  ];
-
   build-system = [
-    pythonPackages.poetry-core
+    python3Packages.poetry-core
   ];
 
   dependencies = [
-    pythonPackages.click
-    pythonPackages.ifaddr
-    pythonPackages.pychromecast
-    pythonPackages.protobuf
-    pythonPackages.requests
-    pythonPackages.yt-dlp
+    python3Packages.click
+    python3Packages.ifaddr
+    python3Packages.pychromecast
+    python3Packages.requests
+    python3Packages.yt-dlp
   ];
 
   doCheck = false; # attempts to access various URLs
@@ -66,6 +34,7 @@ pythonPackages.buildPythonApplication rec {
   meta = {
     description = "Send media from online sources to Chromecast devices";
     homepage = "https://github.com/skorokithakis/catt";
+    changelog = "https://github.com/skorokithakis/catt/releases/tag/v${version}";
     license = lib.licenses.bsd2;
     maintainers = [ lib.maintainers.RossSmyth ];
     mainProgram = "catt";
