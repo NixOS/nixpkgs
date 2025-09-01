1222e4300638ca2d2463d68547d94ba595c9d1a9
Gus <58223632+thegu5@users.noreply.github.com>
2025-08-31 22:45:17 -0400

    maintainers: add thegu5

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25506,6 +25506,11 @@
     github = "thefossguy";
     githubId = 44400303;
   };
+  thegu5 = {
+    name = "Gus";
+    github = "thegu5";
+    githubId = 58223632;
+  };
   thehans255 = {
     name = "Hans Jorgensen";
     email = "foss-contact@thehans255.com";
