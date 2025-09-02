33a9a6b27ef280aba56617b85ddf7b2708d647ba
SteelPh0enix <wojciech_olech@hotmail.com>
2025-09-02 21:55:35 +0200

    Updated stm32cubemx to 6.15.0

--- pkgs/by-name/st/stm32cubemx/package.nix
+++ pkgs/by-name/st/stm32cubemx/package.nix
@@ -14,13 +14,13 @@ let
   iconame = "STM32CubeMX";
   package = stdenvNoCC.mkDerivation rec {
     pname = "stm32cubemx";
-    version = "6.14.0";
+    version = "6.15.0";
 
     src = fetchzip {
       url = "https://sw-center.st.com/packs/resource/library/stm32cube_mx_v${
         builtins.replaceStrings [ "." ] [ "" ] version
       }-lin.zip";
-      hash = "sha256-GOvoPyfPdQV/gjveuFpZjueTZD/BYuEWSHgQKBm3o3A=";
+      hash = "sha256-50P+/uvNH3NN1UN+T3RxGgR8QYBIgBDA56mAEU4BipI=";
       stripRoot = false;
     };
 
