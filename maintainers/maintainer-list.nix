fb6593ed46a8728ee26562b68b5e41c3f246b6fc
lzutao <taolzu@gmail.com>
2025-09-02 14:11:23 +0000

    maintainers: add tesuji

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25305,6 +25305,12 @@
     githubId = 26417242;
     name = "Mikolaj Galkowski";
   };
+  tesuji = {
+    email = "taolzu@gmail.com";
+    github = "tesuji";
+    githubId = 15225902;
+    name = "Lzu Tao";
+  };
   TethysSvensson = {
     email = "freaken@freaken.dk";
     github = "TethysSvensson";
