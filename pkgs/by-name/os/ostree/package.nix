22584f663436381534dac22e63d93b8670fce43e
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-02 07:19:10 +0000

    ostree: 2025.2 -> 2025.5

--- pkgs/by-name/os/ostree/package.nix
+++ pkgs/by-name/os/ostree/package.nix
@@ -54,7 +54,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "ostree";
-  version = "2025.2";
+  version = "2025.5";
 
   outputs = [
     "out"
@@ -65,7 +65,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://github.com/ostreedev/ostree/releases/download/v${finalAttrs.version}/libostree-${finalAttrs.version}.tar.xz";
-    hash = "sha256-8kSkCMkJmYp3jhJ/zCLBtQK00BPxXyaUj0fMcv/i7vQ=";
+    hash = "sha256-ot3UVLoOYg4I+3Mk3si9u20DNg9TExNpG0Lhm1nbbIg=";
   };
 
   patches = [
