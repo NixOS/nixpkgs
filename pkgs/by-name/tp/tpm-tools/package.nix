7f88c4cf8d002876c80f99737b3aff1d83f9d1d5
kyehn <kyehn@users.noreply.github.com>
2025-09-02 12:20:01 +0800

    tpm-tools: 1.3.9.1 -> 1.3.9.2

--- pkgs/by-name/tp/tpm-tools/package.nix
+++ pkgs/by-name/tp/tpm-tools/package.nix
@@ -2,34 +2,35 @@
   lib,
   stdenv,
   fetchurl,
+  autoreconfHook,
   trousers,
   openssl,
   opencryptoki,
   perl,
 }:
 
-let
-  version = "1.3.9.1";
-in
 stdenv.mkDerivation rec {
   pname = "tpm-tools";
-  inherit version;
+  version = "1.3.9.2";
 
   src = fetchurl {
-    url = "mirror://sourceforge/trousers/tpm-tools/${version}/${pname}-${version}.tar.gz";
-    sha256 = "0s7srgghykxnlb1g4izabzf2gfb1knxc0nzn6bly49h8cpi19dww";
+    url = "mirror://sourceforge/trousers/tpm-tools/${version}/tpm-tools-${version}.tar.gz";
+    hash = "sha256-ivg3lJouwwsZU4msiisxvEn+MVBQdRt9TQ1DK/eBKpc=";
   };
 
-  sourceRoot = ".";
+  postPatch = ''
+    mkdir -p po
+    mkdir -p m4
+    cp -R po_/* po/
+    touch po/Makefile.in.in
+    touch m4/Makefile.am
+  '';
 
-  patches = [
-    (fetchurl {
-      url = "https://sources.debian.org/data/main/t/tpm-tools/1.3.9.1-0.1/debian/patches/05-openssl1.1_fix_data_mgmt.patch";
-      sha256 = "161yysw4wgy3spsz6p1d0ib0h5pnrqm8bdh1l71c4hz6a6wpcyxj";
-    })
+  nativeBuildInputs = [
+    autoreconfHook
+    perl
   ];
 
-  nativeBuildInputs = [ perl ];
   buildInputs = [
     trousers
     openssl
