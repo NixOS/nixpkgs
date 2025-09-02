f9fdcc1e848cb739e37d5f6e16130369eb7f5d3b
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-02 12:48:43 +0000

    freerdp: 3.17.0 -> 3.17.1

--- pkgs/by-name/fr/freerdp/package.nix
+++ pkgs/by-name/fr/freerdp/package.nix
@@ -63,13 +63,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "freerdp";
-  version = "3.17.0";
+  version = "3.17.1";
 
   src = fetchFromGitHub {
     owner = "FreeRDP";
     repo = "FreeRDP";
     rev = finalAttrs.version;
-    hash = "sha256-86RbzRgC93ZOt3MHRKJIRklEuyCQs6tHff5jk++yFok=";
+    hash = "sha256-KAlxpoGOqvHTqKkb/yMrquSckFfMXgbEfxr2IuLPZFQ=";
   };
 
   postPatch = ''
