2aa4bd6d50a6293e972883ddbdd9e546a17440d2
Augustin Trancart <augustin.trancart@gmail.com>
2025-09-02 14:19:15 +0200

    python3Packages.pixelmatch: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11853,6 +11853,8 @@ self: super: with self; {
 
   pixel-ring = callPackage ../development/python-modules/pixel-ring { };
 
+  pixelmatch = callPackage ../development/python-modules/pixelmatch { };
+
   pizzapi = callPackage ../development/python-modules/pizzapi { };
 
   pjsua2 =
