NixOS/nixpkgs

commit 4a2ad8134b8b40f198c71f7d8d84467d250cd714

emacs: replace fetchFromSavannah with mirror
This commit re-apply a019344338d84d8e6a79ab0a5a1ede443234e331 with
fixes and minor improvements.

Refs: #427024 #435964
Co-authored-by: Lin Jian <me@linj.tech>

pkgs/applications/editors/emacs/default.nix
@@ -13,7 +13,7 @@ lib.makeScope pkgs.newScope (
       inherit lib;
       inherit (pkgs)
         fetchFromBitbucket
-        fetchFromSavannah
+        fetchzip
         ;
     };
 

pkgs/applications/editors/emacs/make-emacs.nix
@@ -69,7 +69,7 @@
   # Boolean flags
   withNativeCompilation ? stdenv.buildPlatform.canExecute stdenv.hostPlatform,
   noGui ? false,
-  srcRepo ? true,
+  srcRepo ? false,
   withAcl ? false,
   withAlsaLib ? false,
   withAthena ? false,
@@ -202,6 +202,11 @@ mkDerivation (finalAttrs: {
   postPatch = lib.concatStringsSep "\n" [
     (lib.optionalString srcRepo ''
       rm -fr .git
+    '')
+
+    # See: https://github.com/NixOS/nixpkgs/issues/170426
+    (lib.optionalString (!srcRepo) ''
+      find . -type f \( -name "*.elc" -o -name "*loaddefs.el" \) -exec rm {} \;
     '')
 
     # Add the name of the wrapped gvfsd

pkgs/applications/editors/emacs/sources.nix
@@ -1,7 +1,7 @@
 {
   lib,
   fetchFromBitbucket,
-  fetchFromSavannah,
+  fetchzip,
 }:
 
 let
@@ -26,9 +26,9 @@ let
       src =
         {
           "mainline" = (
-            fetchFromSavannah {
-              repo = "emacs";
-              inherit rev hash;
+            fetchzip {
+              url = "mirror://gnu/emacs/${rev}.tar.xz";
+              inherit hash;
             }
           );
           "macport" = (
@@ -107,8 +107,8 @@ in
     pname = "emacs";
     version = "30.2";
     variant = "mainline";
-    rev = "30.2";
-    hash = "sha256-3Lfb3HqdlXqSnwJfxe7npa4GGR9djldy8bKRpkQCdSA=";
+    rev = "emacs-30.2";
+    hash = "sha256-W2eZ+cNQhi/fMeRkwOqSKU7Vzvp43WUOpiwaLLNEXtg=";
     patches = fetchpatch: [
       (builtins.path {
         name = "inhibit-lexical-cookie-warning-67916.patch";

commit a8ea016dd8b38a3f9fecfe815b228f3850b56722

emacs-macport: fix build
I don’t love how this is separated from the actual declaration of
the source, but it seems to be what we have to do.

Fixes: 4a2ad8134b8b40f198c71f7d8d84467d250cd714

pkgs/applications/editors/emacs/default.nix
@@ -31,6 +31,11 @@ lib.makeScope pkgs.newScope (
       withPgtk = true;
     };
 
-    emacs29-macport = callPackage (self.sources.emacs29-macport) inheritedArgs;
+    emacs29-macport = callPackage (self.sources.emacs29-macport) (
+      inheritedArgs
+      // {
+        srcRepo = true;
+      }
+    );
   }
 )