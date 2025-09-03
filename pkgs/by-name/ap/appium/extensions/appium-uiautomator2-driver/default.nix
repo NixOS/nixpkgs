ad72148006d517114011dfdd3785cd353bbd9a4a
eyjhb <eyjhbb@gmail.com>
2025-09-03 13:37:01 +0200

    appium.extensions.appium-uiautomator2-driver: 4.0.3 -> 5.0.0

--- pkgs/by-name/ap/appium/extensions/appium-uiautomator2-driver/default.nix
+++ pkgs/by-name/ap/appium/extensions/appium-uiautomator2-driver/default.nix
@@ -1,20 +1,23 @@
-{ buildNpmPackage, fetchFromGitHub, jq }:
+{
+  buildNpmPackage,
+  fetchFromGitHub,
+  jq,
+}:
 
 buildNpmPackage rec {
   pname = "appium-uiautomator2-driver";
   packageName = pname;
-  version = "4.0.3";
+  version = "5.0.0";
 
   src = fetchFromGitHub {
     owner = "appium";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-r9pDyyYotm2hbND40MumSJ8hBUYqwPCkge5EHl0y02E=";
+    hash = "sha256-S/Xbm7ooz97YLzXPv9whpANfjB0FRP+9UcPPTupg4Q4=";
   };
-  npmDepsHash = "sha256-GhnWIqu3XYaP1QXUqXPvZ8AO6r4ZRXR1KIUmnc8ZBTc=";
+  npmDepsHash = "sha256-BYSfZLSwt0SdinhuGPURbR9xauTt5dYA8amW+gsaxS0=";
 
   postPatch = ''
-    # export APPIUM_SKIP_CHROMEDRIVER_INSTALL=true
     cp ${./package-lock.json} package-lock.json
     ${jq}/bin/jq 'del(.bin)' < package.json > package.json.patched && mv package.json.patched package.json
   '';
