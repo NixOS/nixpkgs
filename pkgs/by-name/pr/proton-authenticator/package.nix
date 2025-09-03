099c632d4d2bf01bc1c7b58f59ea26d1369e2276
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-03 00:31:54 +0000

    proton-authenticator: 1.0.0 -> 1.1.0

--- pkgs/by-name/pr/proton-authenticator/package.nix
+++ pkgs/by-name/pr/proton-authenticator/package.nix
@@ -11,11 +11,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "proton-authenticator";
-  version = "1.0.0";
+  version = "1.1.0";
 
   src = fetchurl {
     url = "https://proton.me/download/authenticator/linux/ProtonAuthenticator_${finalAttrs.version}_amd64.deb";
-    hash = "sha256-Ri6U7tuQa5nde4vjagQKffWgGXbZtANNmeph1X6PFuM=";
+    hash = "sha256-XXrh+fhLqch/Asc3tE1BfQELoh4bigA8oSuYGT0eepg=";
   };
 
   dontConfigure = true;
