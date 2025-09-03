1bda5b199d3bf976e9367ea3c51d854f6e8bcbde
Emily <vcs@emily.moe>
2025-09-03 03:42:44 +0100

    haskell.compiler.ghc{924,963,984}Binary: remove LLVM‐related dead code
    These binary packages are available for a fixed set of platforms,
    all of which support the native code generator. Therefore, the
    `llvmPackages` argument was never used. We leave an assertion around,
    just in case.

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -74,17 +74,11 @@ in
         llvmPackages = pkgs.llvmPackages_12;
       };
 
-      ghc924Binary = callPackage ../development/compilers/ghc/9.2.4-binary.nix {
-        llvmPackages = pkgs.llvmPackages_12;
-      };
+      ghc924Binary = callPackage ../development/compilers/ghc/9.2.4-binary.nix { };
 
-      ghc963Binary = callPackage ../development/compilers/ghc/9.6.3-binary.nix {
-        llvmPackages = pkgs.llvmPackages_15;
-      };
+      ghc963Binary = callPackage ../development/compilers/ghc/9.6.3-binary.nix { };
 
-      ghc984Binary = callPackage ../development/compilers/ghc/9.8.4-binary.nix {
-        llvmPackages = pkgs.llvmPackages_15;
-      };
+      ghc984Binary = callPackage ../development/compilers/ghc/9.8.4-binary.nix { };
 
       ghc948 = callPackage ../development/compilers/ghc/9.4.8.nix {
         bootPkgs =
