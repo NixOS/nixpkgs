ebcba080fa4d008007779f1fa33ac42acf98a94c
jopejoe1 <jopejoe1@missing.ninja>
2025-09-03 22:50:10 +0200

    lib.licenses: add licenses used in nixpkgs

--- lib/licenses.nix
+++ lib/licenses.nix
@@ -157,6 +157,11 @@ lib.mapAttrs mkLicense (
       fullName = "Apache License 2.0";
     };
 
+    baekmuk = {
+      spdxId = "Baekmuk";
+      fullName = "Baekmuk License";
+    };
+
     bitstreamVera = {
       spdxId = "Bitstream-Vera";
       fullName = "Bitstream Vera Font License";
@@ -170,6 +175,11 @@ lib.mapAttrs mkLicense (
     bitTorrent11 = {
       spdxId = "BitTorrent-1.1";
       fullName = " BitTorrent Open Source License v1.1";
+    };
+
+    boehmGC = {
+      spdxId = "Boehm-GC";
+      fullName = "Boehm-Demers-Weiser GC License";
     };
 
     bola11 = {
@@ -581,6 +591,11 @@ lib.mapAttrs mkLicense (
       free = false;
     };
 
+    fontException = {
+      spdxId = "Font-exception-2.0";
+      fullName = "Font exception 2.0";
+    };
+
     fraunhofer-fdk = {
       fullName = "Fraunhofer FDK AAC Codec Library";
       spdxId = "FDK-AAC";
@@ -588,6 +603,11 @@ lib.mapAttrs mkLicense (
 
     free = {
       fullName = "Unspecified free software license";
+    };
+
+    freeimage = {
+      spdxId = "FreeImage";
+      fullName = "FreeImage Public License v1.0";
     };
 
     fsl11Mit = {
@@ -754,6 +774,11 @@ lib.mapAttrs mkLicense (
       fullName = "INRIA Non-Commercial License Agreement for the Zélus compiler";
       url = "https://github.com/INRIA/zelus/raw/829f2b97cba93b0543a9ca0272269e6b8fdad356/LICENSE";
       free = false;
+    };
+
+    interbase = {
+      spdxId = "Interbase-1.0";
+      fullName = "Interbase Public License v1.0";
     };
 
     ipa = {
@@ -1027,6 +1052,11 @@ lib.mapAttrs mkLicense (
       redistributable = true; # Only if used in Netdata products.
     };
 
+    ngpl = {
+      spdxId = "NGPL";
+      fullName = "Nethack General Public License";
+    };
+
     nistSoftware = {
       spdxId = "NIST-Software";
       fullName = "NIST Software License";
@@ -1117,6 +1147,11 @@ lib.mapAttrs mkLicense (
     osl3 = {
       spdxId = "OSL-3.0";
       fullName = "Open Software License 3.0";
+    };
+
+    paratype = {
+      fullName = "ParaType Free Font Licensing Agreement";
+      url = "https://web.archive.org/web/20161209023955/http://www.paratype.ru/public/pt_openlicense_eng.asp";
     };
 
     parity70 = {
@@ -1252,6 +1287,11 @@ lib.mapAttrs mkLicense (
       redistributable = false; # only free to redistribute "for non-commercial purposes"
     };
 
+    tcpWrappers = {
+      spdxId = "TCP-wrappers";
+      fullName = "TCP Wrappers License";
+    };
+
     teamspeak = {
       fullName = "Teamspeak client license";
       url = "https://www.teamspeak.com/en/privacy-and-terms/";
@@ -1297,6 +1337,11 @@ lib.mapAttrs mkLicense (
       # TODO: add spdxId when it gets accepted to spdx
       # https://tools.spdx.org/app/license_requests/458
       # https://github.com/spdx/license-list-XML/issues/2757
+    };
+
+    torque11 = {
+      spdxId = "TORQUE-1.1";
+      fullName = "TORQUE v2.5+ Software License v1.1";
     };
 
     tsl = {
@@ -1429,6 +1474,12 @@ lib.mapAttrs mkLicense (
       fullName = "Do What The F*ck You Want To Public License";
     };
 
+    wxWindowsException3 = {
+      shortName = "WxWindows-exception-3";
+      url = "https://github.com/openscenegraph/osgQt/blob/master/LICENSE.txt";
+      fullName = "wxWindows Library Licence, Version 3";
+    };
+
     wxWindowsException31 = {
       spdxId = "WxWindows-exception-3.1";
       fullName = "wxWindows Library Licence, Version 3.1";
@@ -1437,6 +1488,11 @@ lib.mapAttrs mkLicense (
     x11 = {
       spdxId = "X11";
       fullName = "X11 License";
+    };
+
+    xerox = {
+      spdxId = "Xerox";
+      fullName = "Xerox License";
     };
 
     xfig = {
