021f672ff8c385dd4f798b4b8beeb0ca0fe9efe8
Philip Taron <philip.taron@gmail.com>
2025-09-03 11:04:38 -0700

    nixDependencies.boehmgc: use overrides to set initialMarkStackSize

--- pkgs/tools/package-management/nix/dependencies.nix
+++ pkgs/tools/package-management/nix/dependencies.nix
@@ -8,19 +8,17 @@ regular@{
 
 {
   scopeFunction = scope: {
-    boehmgc =
-      (regular.boehmgc.override {
-        enableLargeConfig = true;
-      }).overrideAttrs
-        (attrs: {
-          # Increase the initial mark stack size to avoid stack
-          # overflows, since these inhibit parallel marking (see
-          # GC_mark_some()). To check whether the mark stack is too
-          # small, run Nix with GC_PRINT_STATS=1 and look for messages
-          # such as `Mark stack overflow`, `No room to copy back mark
-          # stack`, and `Grew mark stack to ... frames`.
-          NIX_CFLAGS_COMPILE = "-DINITIAL_MARK_STACK_SIZE=1048576";
-        });
+    boehmgc = regular.boehmgc.override {
+      enableLargeConfig = true;
+
+      # Increase the initial mark stack size to avoid stack overflows, since these inhibit parallel
+      # marking (see `GC_mark_some()` in `mark.c`).
+      #
+      # Run Nix with `GC_PRINT_STATS=1` set to see if the mark stack is too small.
+      # Look for messages such as `Mark stack overflow`, `No room to copy back mark stack`, and
+      # `Grew mark stack to ... frames`.
+      initialMarkStackSize = 1048576;
+    };
 
     aws-sdk-cpp =
       (regular.aws-sdk-cpp.override {
