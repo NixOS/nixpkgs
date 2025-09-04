567b06f7b43266d06f79923a2cbdcb5e84e0fff5
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-04 08:23:25 +0000

    python3Packages.swh-model: 7.1.0 -> 8.4.0

--- pkgs/development/python-modules/swh-model/default.nix
+++ pkgs/development/python-modules/swh-model/default.nix
@@ -26,7 +26,7 @@
 
 buildPythonPackage rec {
   pname = "swh-model";
-  version = "7.1.0";
+  version = "8.4.0";
   pyproject = true;
 
   src = fetchFromGitLab {
@@ -35,7 +35,7 @@ buildPythonPackage rec {
     owner = "devel";
     repo = "swh-model";
     tag = "v${version}";
-    hash = "sha256-I0DaSipE5TVFqAdGkNo4e66l1x4A26EYk0F4tKMy33k=";
+    hash = "sha256-ILCfqtG9RP+a5aGTgsvXUIsm69ZOh5ZFhpPjg/S+DTo=";
   };
 
   build-system = [
