fd8ad3aea379311c736add370e1299838549ef73
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-04 18:44:49 +0000

    home-manager: 0-unstable-2025-09-03 -> 0-unstable-2025-09-04

--- pkgs/by-name/ho/home-manager/package.nix
+++ pkgs/by-name/ho/home-manager/package.nix
@@ -19,14 +19,14 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "home-manager";
-  version = "0-unstable-2025-09-03";
+  version = "0-unstable-2025-09-04";
 
   src = fetchFromGitHub {
     name = "home-manager-source";
     owner = "nix-community";
     repo = "home-manager";
-    rev = "30fc1b532645a21e157b6e33e3f8b4c154f86382";
-    hash = "sha256-XbtRMewPGJwTNhBC4pnBu3w/xT1XejvB0HfohC2Kga8=";
+    rev = "b08f8737776f10920c330657bee8b95834b7a70f";
+    hash = "sha256-4ve/3ah5H/SpL2m3qmZ9GU+VinQYp2MN1G7GamimTds=";
   };
 
   nativeBuildInputs = [
