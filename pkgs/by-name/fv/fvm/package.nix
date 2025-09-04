cea2daf73690778b30c1b8ef709b64a5450cba61
kyehn <kyehn@users.noreply.github.com>
2025-09-04 17:32:26 +0800

    fvm: add updateScript

--- pkgs/by-name/fv/fvm/package.nix
+++ pkgs/by-name/fv/fvm/package.nix
@@ -2,11 +2,13 @@
   lib,
   buildDartApplication,
   fetchFromGitHub,
-  nix-update-script,
+  runCommand,
+  yq-go,
+  _experimental-update-script-combinators,
+  gitUpdater,
 }:
 
-buildDartApplication rec {
-  pname = "fvm";
+let
   version = "3.2.1";
 
   src = fetchFromGitHub {
@@ -15,10 +17,28 @@ buildDartApplication rec {
     tag = version;
     hash = "sha256-i7sJRBrS5qyW8uGlx+zg+wDxsxgmolTMcikHyOzv3Bs=";
   };
+in
+buildDartApplication {
+  pname = "fvm";
+  inherit version src;
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
 
-  passthru.updateScript = nix-update-script { };
+  passthru = {
+    pubspecSource =
+      runCommand "pubspec.lock.json"
+        {
+          inherit src;
+          nativeBuildInputs = [ yq-go ];
+        }
+        ''
+          yq eval --output-format=json --prettyPrint $src/pubspec.lock > "$out"
+        '';
+    updateScript = _experimental-update-script-combinators.sequence [
+      (gitUpdater { })
+      (_experimental-update-script-combinators.copyAttrOutputToFile "fvm.pubspecSource" ./pubspec.lock.json)
+    ];
+  };
 
   meta = {
     description = "Simple CLI to manage Flutter SDK versions";
