33221999831983dd49e4904f547a0787f95d63ad
h7x4 <h7x4@nani.wtf>
2025-09-05 13:41:02 +0200

    nixos/gitea: use systemd service type `notify`

--- nixos/modules/services/misc/gitea.nix
+++ nixos/modules/services/misc/gitea.nix
@@ -890,7 +890,7 @@ in
         '';
 
       serviceConfig = {
-        Type = "simple";
+        Type = "notify";
         User = cfg.user;
         Group = cfg.group;
         WorkingDirectory = cfg.stateDir;
