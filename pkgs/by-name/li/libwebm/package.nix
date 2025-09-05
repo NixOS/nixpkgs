b97457fad59981703d1f901bc6e50208a7756e0b
Niklas Korz <niklas@niklaskorz.de>
2025-09-05 14:01:50 +0200

    libwebm: 1.0.0.31 -> 1.0.0.32
    https://github.com/webmproject/libwebm/compare/libwebm-1.0.0.31...libwebm-1.0.0.32

--- pkgs/by-name/li/libwebm/package.nix
+++ pkgs/by-name/li/libwebm/package.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libwebm";
-  version = "1.0.0.31";
+  version = "1.0.0.32";
 
   src = fetchFromGitHub {
     owner = "webmproject";
     repo = "libwebm";
     tag = "libwebm-${finalAttrs.version}";
-    hash = "sha256-+ayX33rcX/jkewsW8WrGalTe9X44qFBHOrIYTteOQzc=";
+    hash = "sha256-SxDGt7nPVkSxwRF/lMmcch1h+C2Dyh6GZUXoZjnXWb4=";
   };
 
   patches = [
