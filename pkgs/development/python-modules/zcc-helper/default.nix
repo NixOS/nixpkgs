18b715d988094c7516b5fb1cd2767225bb28335b
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-05 07:47:42 +0000

    python3Packages.zcc-helper: 3.6 -> 3.7

--- pkgs/development/python-modules/zcc-helper/default.nix
+++ pkgs/development/python-modules/zcc-helper/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "zcc-helper";
-  version = "3.6";
+  version = "3.7";
   pyproject = true;
 
   src = fetchFromBitbucket {
     owner = "mark_hannon";
     repo = "zcc";
     rev = "release_${version}";
-    hash = "sha256-93zSEGr5y00+heuG0hTME+BkLQBUmHnXXMH12ktMtM4=";
+    hash = "sha256-KMa0RfG3cT1sw5ujnecYtx+A4qjujUPiq6pM3PwNSLM=";
   };
 
   build-system = [ setuptools ];
