14381119fc29dae14881fd2b1092ed6ef4248604
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-09-05 12:51:20 +0200

    Add a patch from upstream

--- pkgs/by-name/cl/clickhouse/generic.nix
+++ pkgs/by-name/cl/clickhouse/generic.nix
@@ -10,6 +10,7 @@
   stdenv,
   llvmPackages_19,
   fetchFromGitHub,
+  fetchpatch,
   cmake,
   ninja,
   python3,
@@ -88,6 +89,13 @@ llvmPackages_19.stdenv.mkDerivation (finalAttrs: {
   buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [ libiconv ];
 
   dontCargoSetupPostUnpack = true;
+  patches = [
+    # Should not be necessary after 25.9
+    (fetchpatch {
+      url = "https://github.com/ClickHouse/ClickHouse/commit/67a42b78cdf1c793e78c1adbcc34162f67044032.patch";
+      sha256 = "7VF+JSztqTWD+aunCS3UVNxlRdwHc2W5fNqzDyeo3Fc=";
+    })
+  ];
 
   postPatch = ''
     patchShebangs src/ utils/
