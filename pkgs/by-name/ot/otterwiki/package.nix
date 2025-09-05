4cbad4f07c9bbbd8f5056e41d55d259f8d4bc798
euxane <euxane.trangirard@euxane.eu>
2025-09-05 22:09:55 +0200

    otterwiki: init at 2.14.0

---
+++ pkgs/by-name/ot/otterwiki/package.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  fetchFromGitHub,
+  nix-update-script,
+  python3,
+}:
+
+let
+  python = python3;
+
+in
+python.pkgs.buildPythonApplication rec {
+  pname = "otterwiki";
+  version = "2.14.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "redimp";
+    repo = "otterwiki";
+    rev = "v${version}";
+    hash = "sha256-esho5GmzHbCXL1uv0paTFDXHihHWwQqVjRAuOWl3kVk=";
+  };
+
+  build-system = with python.pkgs; [ setuptools ];
+
+  pythonRelaxDeps = true;
+  dependencies = with python.pkgs; [
+    werkzeug
+    flask-login
+    flask-mail
+    sqlalchemy
+    flask-sqlalchemy
+    flask
+    jinja2
+    gitpython
+    cython
+    (callPackage ./mistune.nix { }) # wants an old version of mistune
+    pygments
+    pillow
+    pyyaml
+    unidiff
+    beautifulsoup4
+    pluggy
+    regex
+    feedgen
+  ];
+
+  passthru = {
+    inherit python;
+    wsgiModule = "otterwiki.server:app";
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
+    description = "A minimalistic wiki powered by python, markdown and git";
+    homepage = "https://otterwiki.com/";
+    changelog = "https://github.com/redimp/otterwiki/blob/main/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ euxane ];
+  };
+}
