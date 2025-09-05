469484dbee40fc0af88884dbabfda65db116f4ec
Guy Chronister <guychronister@outlook.com>
2025-09-05 13:40:51 -0500

    treewide: refactor meta section

--- pkgs/by-name/ae/aeron/package.nix
+++ pkgs/by-name/ae/aeron/package.nix
@@ -119,14 +119,14 @@ stdenv.mkDerivation {
     jar = aeronAll.jar;
   };
 
-  meta = with lib; {
+  meta = {
     description = "Low-latency messaging library";
     homepage = "https://aeron.io/";
-    license = licenses.asl20;
-    mainProgram = "${pname}-media-driver";
-    maintainers = [ maintainers.vaci ];
-    sourceProvenance = [
-      sourceTypes.binaryBytecode
+    license = with lib.licenses; [ asl20 ];
+    mainProgram = "aeron-media-driver";
+    maintainers = with lib.maintainers; [ vaci ];
+    sourceProvenance = with lib.sourceTypes; [
+      binaryBytecode
     ];
   };
 }
