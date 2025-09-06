ae2487fa4916b5512726229d3eca1b83985abccc
Jan Tojnar <jtojnar@gmail.com>
2025-09-06 17:18:17 +0000

    libpanel: 1.10.0 → 1.10.1
    https://gitlab.gnome.org/GNOME/libpanel/-/compare/1.10.0...1.10.1
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/by-name/li/libpanel/package.nix
+++ pkgs/by-name/li/libpanel/package.nix
@@ -16,7 +16,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libpanel";
-  version = "1.10.0";
+  version = "1.10.1";
 
   outputs = [
     "out"
@@ -28,7 +28,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/libpanel/${lib.versions.majorMinor finalAttrs.version}/libpanel-${finalAttrs.version}.tar.xz";
-    hash = "sha256-V4zlEieP8rte7rtVCZOSxSU3pavZvQYpVn8QJTKziyU=";
+    hash = "sha256-k2u+lt+zg1VkghIP3dRTOlLS9JMDMoy2lIYWBkkkRes=";
   };
 
   strictDeps = true;
