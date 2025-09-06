7a488a72353874ea43d12cc013961b6b9f0bb952
Jan Tojnar <jtojnar@gmail.com>
2025-09-06 17:20:03 +0000

    gsettings-desktop-schemas: 48.0 → 49.rc
    https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas/-/compare/48.0...49.rc

--- pkgs/by-name/gs/gsettings-desktop-schemas/package.nix
+++ pkgs/by-name/gs/gsettings-desktop-schemas/package.nix
@@ -17,11 +17,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gsettings-desktop-schemas";
-  version = "48.0";
+  version = "49.rc";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    hash = "sha256-5o8VWBO/GPhlqLLI6dRzWItsytyvu2Zqt4iFfGwtG9M=";
+    hash = "sha256-YTbBW/NtbW+6Dd78Wx3G0HlERKPh9FE0zvlnVgxmIq0=";
   };
 
   strictDeps = true;
