acded487a80efdf2664a08c53fc8b4a53c3864ba
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-06 11:01:53 +0200

    rauthy: use finalAttrs and put frontend in main drv for easy overriding

--- pkgs/by-name/ra/rauthy/package.nix
+++ pkgs/by-name/ra/rauthy/package.nix
@@ -10,33 +10,16 @@
   nix-update-script,
 }:
 
-let
+rustPlatform.buildRustPackage (finalAttrs:{
+  pname = "rauthy";
   version = "0.32.1";
+
   src = fetchFromGitHub {
     owner = "sebadob";
     repo = "rauthy";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-rtn1JqWxH+MCu102KaQ/Xjjh9D3UtPolko78YQPJFXc=";
   };
-
-  frontend = buildNpmPackage {
-    pname = "rauthy-frontend";
-    inherit version src;
-
-    sourceRoot = "${src.name}/frontend";
-
-    patches = [
-      # otherwise permission denied error for trying to write outside of the build directory
-      ./0002-build-svelte-files-inside-the-current-directory.patch
-      ./0003-fix-aarch64-segfault.patch
-    ];
-
-    npmDepsHash = "sha256-IYzWw1Ctz5L5R+e54t88ntYjREhaaQOwXBCAh4Th/EU=";
-  };
-in
-rustPlatform.buildRustPackage {
-  pname = "rauthy";
-  inherit version src;
 
   cargoPatches = [
     # otherwise it tries to download swagger-ui at build time
@@ -57,12 +40,27 @@ rustPlatform.buildRustPackage {
   ];
 
   preBuild = ''
-    cp -r ${frontend}/lib/node_modules/frontend/dist/templates/html/ templates/html
-    cp -r ${frontend}/lib/node_modules/frontend/dist/static/ static
+    cp -r ${finalAttrs.frontend}/lib/node_modules/frontend/dist/templates/html/ templates/html
+    cp -r ${finalAttrs.frontend}/lib/node_modules/frontend/dist/static/ static
   '';
 
   # tests take long, require the app and a database to be running, and some of them fail
   doCheck = false;
+
+  frontend = buildNpmPackage {
+    pname = "rauthy-frontend";
+    inherit (finalAttrs) version src;
+
+    sourceRoot = "${finalAttrs.src.name}/frontend";
+
+    patches = [
+      # otherwise permission denied error for trying to write outside of the build directory
+      ./0002-build-svelte-files-inside-the-current-directory.patch
+      ./0003-fix-aarch64-segfault.patch
+    ];
+
+    npmDepsHash = "sha256-IYzWw1Ctz5L5R+e54t88ntYjREhaaQOwXBCAh4Th/EU=";
+  };
 
   passthru = {
     updateScript = nix-update-script {
@@ -70,11 +68,10 @@ rustPlatform.buildRustPackage {
         "--subpackage=frontend"
       ];
     };
-    inherit frontend;
   };
 
   meta = {
-    changelog = "https://github.com/sebadob/rauthy/releases/tag/v${version}";
+    changelog = "https://github.com/sebadob/rauthy/releases/tag/v${finalAttrs.version}";
     description = "OpenID Connect Single Sign-On Identity & Access Management";
     license = lib.licenses.asl20;
     mainProgram = "rauthy";
@@ -83,4 +80,4 @@ rustPlatform.buildRustPackage {
     ];
     platforms = with lib.platforms; linux;
   };
-}
+})
