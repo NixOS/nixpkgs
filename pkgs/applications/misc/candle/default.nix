72dd3ca3cb015bde4cc10aa3f2dfc08af7dd1867
MarcFontaine <MarcFontaine@users.noreply.github.com>
2025-09-06 13:18:20 +0200

    candle: 1.1 -> 1.1.8

--- pkgs/applications/misc/candle/default.nix
+++ pkgs/applications/misc/candle/default.nix
@@ -1,24 +1,22 @@
 {
-  mkDerivation,
+  stdenv,
   lib,
   fetchFromGitHub,
-  qtbase,
-  qtserialport,
-  qmake,
+  qt5,
 }:
 
-mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "candle";
-  version = "1.1";
+  version = "1.1.8";
 
   src = fetchFromGitHub {
     owner = "Denvi";
     repo = "Candle";
     rev = "v${version}";
-    sha256 = "1gpx08gdz8awbsj6lsczwgffp19z3q0r2fvm72a73qd9az29pmm0";
+    sha256 = "sha256-A53rHlabcuw/nWS7jsCyVrP3CUkmUI/UMRqpogyFOCM=";
   };
 
-  nativeBuildInputs = [ qmake ];
+  nativeBuildInputs = [ qt5.qmake ];
 
   sourceRoot = "${src.name}/src";
 
@@ -29,8 +27,9 @@ mkDerivation rec {
   '';
 
   buildInputs = [
-    qtbase
-    qtserialport
+    qt5.qtbase
+    qt5.qtserialport
+    qt5.wrapQtAppsHook
   ];
 
   meta = with lib; {
