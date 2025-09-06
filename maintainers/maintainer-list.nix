f044c8da44a676327100135518ad734d92897dc3
Jakob Getz <jakob@getz.de>
2025-09-06 17:04:54 +0200

    maintainers: add jakobgetz

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11456,6 +11456,12 @@
     githubId = 5283991;
     name = "Jake Waksbaum";
   };
+  jakobgetz = {
+    email = "jakob@getz.de";
+    github = "jakobgetz";
+    githubId = 64084560;
+    name = "Jakob Getz";
+  };
   jakubgs = {
     email = "jakub@gsokolowski.pl";
     github = "jakubgs";
