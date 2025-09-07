1f7c853dac4527680e6a06fce4244a5511bb51e7
Jaco Malan <jacomalan.12@gmail.com>
2025-09-07 12:13:49 +0200

    solana-cli: 1.18.26 -> 2.3.8

--- pkgs/by-name/so/solana-cli/package.nix
+++ pkgs/by-name/so/solana-cli/package.nix
@@ -5,28 +5,28 @@
   rustPlatform,
   udev,
   protobuf,
-  rocksdb_8_3,
   installShellFiles,
   pkg-config,
   openssl,
   nix-update-script,
   versionCheckHook,
+  clang,
+  libclang,
+  rocksdb,
   # Taken from https://github.com/solana-labs/solana/blob/master/scripts/cargo-install-all.sh#L84
   solanaPkgs ? [
-    "cargo-build-bpf"
-    "cargo-test-bpf"
     "cargo-build-sbf"
     "cargo-test-sbf"
     "solana"
     "solana-bench-tps"
     "solana-faucet"
     "solana-gossip"
-    "solana-install"
+    "agave-install"
     "solana-keygen"
-    "solana-ledger-tool"
+    "agave-ledger-tool"
     "solana-log-analyzer"
     "solana-net-shaper"
-    "solana-validator"
+    "agave-validator"
     "solana-test-validator"
   ]
   ++ [
@@ -36,37 +36,26 @@
   ],
 }:
 let
-  version = "1.18.26";
-  hash = "sha256-sJ0Zn5GMi64/S8zqomL/dYRVW8SOQWsP+bpcdatJC0A=";
-  rocksdb = rocksdb_8_3;
+  version = "2.3.8";
+  hash = "sha256-CqkedeQk66VXG6lQAIVGd7ci0KPltf2Qq69iErBAQGo=";
 in
 rustPlatform.buildRustPackage rec {
   pname = "solana-cli";
   inherit version;
 
   src = fetchFromGitHub {
-    owner = "solana-labs";
-    repo = "solana";
+    owner = "anza-xyz";
+    repo = "agave";
     tag = "v${version}";
     inherit hash;
   };
 
-  # The `crossbeam-epoch@0.9.5` crate used by the solana crates is their own fork,
-  # which exists due to performance-related reasons.
-  # The `solana-cli` build fails because this forked `crossbeam-epoch` crate contains a
-  # symlink that points outside of the crate into the root of the repository.
-  # This characteristic already existed in upstream `crossbeam-epoch@0.9.5`.
-  #
-  # As `buildRustPackage` vendors the dependencies of the `solana-cli` during the `buildPhase`,
-  # which occurs after the `patchPhase`, this `crossbeam-epoch` is not patchable in this build.
-  # The remaining solution is to make use of `cargoPatches` to remove the fork and bump to `crossbeam-epoch@0.9.16`,
-  # which is the first version that removed the `build.rs`.
-  cargoPatches = [ ./crossbeam-epoch.patch ];
-
-  cargoHash = "sha256-adzcLrOiUUYhz57gme/hEmD4E3kVcKCp0/jSoavZfjw=";
+  cargoHash = "sha256-J7gyR7K1hauV+VrzoNzRrooLuSkjk8U6A3aFn9O2yFY=";
 
   strictDeps = true;
   cargoBuildFlags = builtins.map (n: "--bin=${n}") solanaPkgs;
+  RUSTFLAGS = "-Amismatched_lifetime_syntaxes -Adead_code";
+  LIBCLANG_PATH = "${libclang.lib}/lib";
 
   # Even tho the tests work, a shit ton of them try to connect to a local RPC
   # or access internet in other ways, eventually failing due to Nix sandbox.
@@ -82,6 +71,8 @@ rustPlatform.buildRustPackage rec {
   ];
   buildInputs = [
     openssl
+    clang
+    libclang
     rustPlatform.bindgenHook
   ]
   ++ lib.optionals stdenv.hostPlatform.isLinux [ udev ];
@@ -97,10 +88,9 @@ rustPlatform.buildRustPackage rec {
       --bash <($out/bin/solana completion --shell bash) \
       --fish <($out/bin/solana completion --shell fish)
 
-    mkdir -p $out/bin/sdk/bpf
-    cp -a ./sdk/bpf/* $out/bin/sdk/bpf/
-    mkdir -p $out/bin/sdk/sbf
-    cp -a ./sdk/sbf/* $out/bin/sdk/sbf
+    mkdir -p $out/bin/platform-tools-sdk
+    cp -r ./platform-tools-sdk/sbf $out/bin/platform-tools-sdk
+
     mkdir -p $out/bin/deps
     find . -name libsolana_program.dylib -exec cp {} $out/bin/deps \;
     find . -name libsolana_program.rlib -exec cp {} $out/bin/deps \;
@@ -126,6 +116,7 @@ rustPlatform.buildRustPackage rec {
       netfox
       happysalada
       aikooo7
+      JacoMalan1
     ];
     platforms = platforms.unix;
   };
