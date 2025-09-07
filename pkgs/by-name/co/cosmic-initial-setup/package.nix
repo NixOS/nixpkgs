63bc621f5498753bf153bafeca2923df5b4d63c0
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-09-07 11:15:13 -0300

    cosmic-initial-setup: init at 1.0.0-beta.1

---
+++ pkgs/by-name/co/cosmic-initial-setup/package.nix
@@ -0,0 +1,93 @@
+{
+  lib,
+  stdenv,
+  rustPlatform,
+  fetchFromGitHub,
+  just,
+  libcosmicAppHook,
+  libinput,
+  openssl,
+  udev,
+  nixosTests,
+  nix-update-script,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "cosmic-initial-setup";
+  version = "1.0.0-beta.1";
+
+  src = fetchFromGitHub {
+    owner = "pop-os";
+    repo = "cosmic-initial-setup";
+    tag = "epoch-${finalAttrs.version}";
+    hash = "sha256-MEFa4YVjz2u3JhxEjpYxS7D1IsZ+zD0j4SIcGhfMLFM=";
+  };
+
+  cargoHash = "sha256-orwK9gcFXK4/+sfwRubcz0PP6YAFqsENRHnlSLttLxM=";
+
+  # cargo-auditable fails during the build when compiling the `crabtime::function`
+  # procedural macro. It panics because the `--out-dir` flag is not passed to
+  # the rustc wrapper.
+  auditable = false;
+
+  nativeBuildInputs = [
+    libcosmicAppHook
+    just
+  ];
+
+  buildInputs = [
+    libinput
+    openssl
+    udev
+  ];
+
+  postPatch = ''
+    # Installs in $out/etc/xdg/autostart instead of /etc/xdg/autostart
+    substituteInPlace justfile \
+      --replace-fail \
+      "autostart-dst := rootdir / 'etc' / 'xdg' / 'autostart' / desktop-entry" \
+      "autostart-dst := prefix / 'etc' / 'xdg' / 'autostart' / desktop-entry"
+  '';
+
+  dontUseJustBuild = true;
+  dontUseJustCheck = true;
+
+  justFlags = [
+    "--set"
+    "prefix"
+    (placeholder "out")
+    "--set"
+    "cargo-target-dir"
+    "target/${stdenv.hostPlatform.rust.cargoShortTarget}"
+  ];
+
+  env.DISABLE_IF_EXISTS = "/iso/nix-store.squashfs";
+
+  passthru = {
+    tests = {
+      inherit (nixosTests)
+        cosmic
+        cosmic-autologin
+        cosmic-noxwayland
+        cosmic-autologin-noxwayland
+        ;
+    };
+
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--version"
+        "unstable"
+        "--version-regex"
+        "epoch-(.*)"
+      ];
+    };
+  };
+
+  meta = {
+    description = "COSMIC Initial Setup";
+    homepage = "https://github.com/pop-os/cosmic-initial-setup";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "cosmic-initial-setup";
+    platforms = lib.platforms.linux;
+    teams = [ lib.teams.cosmic ];
+  };
+})
