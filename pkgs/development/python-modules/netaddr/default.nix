dcb9d2286d5aac860f4067b5e11573a0a46c1c6f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-09-08 00:21:12 +0000

    python3Packages.netaddr: fix static build

--- pkgs/development/python-modules/netaddr/default.nix
+++ pkgs/development/python-modules/netaddr/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  stdenv,
   buildPythonPackage,
   fetchPypi,
   pythonOlder,
@@ -18,6 +19,18 @@ buildPythonPackage rec {
     inherit pname version;
     hash = "sha256-XDw9mJW1Ubdjd5un23oDSH3B+OOzha+BmvNBrp725Io=";
   };
+
+  # Test suite uses internal packaging._musllinux module to detect libc flavor. The module assumes
+  # the python executable is dynamically linked - it then attempts to parse linked library name to
+  # detect musl. It won't work on a static build.
+  postPatch =
+    if (stdenv.targetPlatform.isMusl && stdenv.targetPlatform.isStatic) then
+      ''
+        substituteInPlace netaddr/tests/__init__.py \
+          --replace-fail "IS_MUSL = _get_musl_version(sys.executable) is not None" "IS_MUSL = True"
+      ''
+    else
+      null;
 
   nativeBuildInputs = [ setuptools ];
 
