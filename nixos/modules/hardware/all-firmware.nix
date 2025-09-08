NixOS/nixpkgs

commit 05aa80c06ab45da7c44b6bf1b858a87815665c71

hardware: add enableRedistributalFirmware
Due the recent inclusion of broadcom-bt-firmware in enableAllFirmware,
it was required to set `nixpkgs.config.allowUnfree` to obtain the full
list. To make this dependency more explicit an assertion is added and an
alternative option `enableRedistributalFirmware` is provided to only
obtain firmware with an license allowing redistribution.

nixos/modules/hardware/all-firmware.nix
@@ -2,7 +2,9 @@
 
 with lib;
 
-{
+let
+  cfg = config.hardware;
+in {
 
   ###### interface
 
@@ -12,7 +14,16 @@ with lib;
       default = false;
       type = types.bool;
       description = ''
-        Turn on this option if you want to enable all the firmware shipped in linux-firmware.
+        Turn on this option if you want to enable all the firmware.
+      '';
+    };
+
+    hardware.enableRedistributalFirmware = mkOption {
+      default = false;
+      type = types.bool;
+      description = ''
+        Turn on this option if you want to enable all the firmware with a license allowing redistribution.
+        (i.e. free firmware and <literal>firmware-linux-nonfree</literal>)
       '';
     };
 
@@ -21,15 +32,27 @@ with lib;
 
   ###### implementation
 
-  config = mkIf config.hardware.enableAllFirmware {
-    hardware.firmware = with pkgs; [
-      firmwareLinuxNonfree
-      intel2200BGFirmware
-      rtl8723bs-firmware
-      rtl8192su-firmware
-    ] ++ optionals config.nixpkgs.config.allowUnfree [
-      broadcom-bt-firmware
-    ];
-  };
-
+  config = mkMerge [
+    (mkIf (cfg.enableAllFirmware || cfg.enableRedistributalFirmware) {
+      hardware.firmware = with pkgs; [
+        firmwareLinuxNonfree
+        intel2200BGFirmware
+        rtl8723bs-firmware
+        rtl8192su-firmware
+      ];
+    })
+    (mkIf cfg.enableAllFirmware {
+      assertions = [{
+        assertion = !cfg.enableAllFirmware || (config.nixpkgs.config.allowUnfree or false);
+        message = ''
+          the list of hardware.enableAllFirmware contains non-redistributable licensed firmware files.
+            This requires nixpkgs.config.allowUnfree to be true.
+            An alternative is to use the hardware.enableRedistributalFirmware option.
+        '';
+      }];
+      hardware.firmware = with pkgs; [
+        broadcom-bt-firmware
+      ];
+    })
+  ];
 }

nixos/modules/hardware/network/broadcom-43xx.nix
@@ -1,3 +1,3 @@
 {
-  hardware.enableAllFirmware = true;
+  hardware.enableRedistributalFirmware = true;
 }

nixos/modules/hardware/network/intel-2030.nix
@@ -1,3 +1,3 @@
 {
-  hardware.enableAllFirmware = true;
+  hardware.enableRedistributalFirmware = true;
 }

nixos/modules/hardware/network/intel-2100bg.nix
@@ -23,7 +23,7 @@
 
   config = lib.mkIf config.networking.enableIntel2100BGFirmware {
 
-    hardware.enableAllFirmware = true;
+    hardware.enableRedistributalFirmware = true;
 
   };
 

nixos/modules/hardware/network/intel-3945abg.nix
@@ -22,7 +22,7 @@
 
   config = lib.mkIf config.networking.enableIntel3945ABGFirmware {
 
-    hardware.enableAllFirmware = true;
+    hardware.enableRedistributalFirmware = true;
 
   };
 

nixos/modules/hardware/network/intel-4965agn.nix
@@ -1,3 +1,3 @@
 {
-  hardware.enableAllFirmware = true;
+  hardware.enableRedistributalFirmware = true;
 }

nixos/modules/hardware/network/intel-5000.nix
@@ -1,3 +1,3 @@
 {
-  hardware.enableAllFirmware = true;
+  hardware.enableRedistributalFirmware = true;
 }

nixos/modules/hardware/network/intel-5150.nix
@@ -1,3 +1,3 @@
 {
-  hardware.enableAllFirmware = true;
+  hardware.enableRedistributalFirmware = true;
 }

commit 42fffd06471bea0753d46f4fd9bce35a1f2912a1

nixos/firmware: remove restrictive hardware.enableAllFirmware assertion
Remove the too restrictive hardware.enableAllFirmware assertion
introduced in commit 05aa80c06ab4 ("hardware: add
enableRedistributalFirmware").

This assertion is too restrictive because it enforces globally enabling
unfree packages without allowing explicit whitelisting:

       hardware.enableAllFirmware = true;

    -  nixpkgs.config.allowUnfree = true;
    +  nixpkgs.config.allowUnfreePredicate = pkg:
    +      builtins.elem (lib.getName pkg) [
    +        "b43-firmware"
    +        "broadcom-bt-firmware"
    +        "facetimehd-calibration"
    +        "facetimehd-firmware"
    +        "xow_dongle-firmware"
    +      ];

Declaring neither nixpkgs.config.allowUnfree nor
nixpkgs.config.allowUnfreePredicate without this
hardware.enableAllFirmware assertion results in detailed and instructive
evaluation error messages.

nixos/modules/hardware/all-firmware.nix
@@ -74,16 +74,6 @@ in
         ++ lib.optional pkgs.stdenv.hostPlatform.isAarch raspberrypiWirelessFirmware;
     })
     (lib.mkIf cfg.enableAllFirmware {
-      assertions = [
-        {
-          assertion = cfg.enableAllFirmware -> pkgs.config.allowUnfree;
-          message = ''
-            the list of hardware.enableAllFirmware contains non-redistributable licensed firmware files.
-              This requires nixpkgs.config.allowUnfree to be true.
-              An alternative is to use the hardware.enableRedistributableFirmware option.
-          '';
-        }
-      ];
       hardware.firmware =
         with pkgs;
         [