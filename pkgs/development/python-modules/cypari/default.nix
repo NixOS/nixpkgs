31a9f7bc131781ba340a78e88d8d5db5827bd7e4
Alex Epelde <alex@epelde.net>
2025-09-08 15:47:06 -0400

    python3Packages.cypari: fix build with cython 3.1

--- pkgs/development/python-modules/cypari/default.nix
+++ pkgs/development/python-modules/cypari/default.nix
@@ -23,16 +23,16 @@ buildPythonPackage rec {
     hash = "sha256-RJ9O1KsDHmMkTCIFUrcSUkA5ijTsxmoI939QCsCib0Y=";
   };
 
-  postPatch = ''
-    # final character is stripped from PARI error messages for some reason
-    substituteInPlace ./cypari/handle_error.pyx \
-      --replace-fail "not a function in function call" "not a function in function cal"
-  '';
   patches = [
     (fetchpatch {
       name = "support-aarch64-linux.patch";
       url = "https://github.com/3-manifolds/CyPari/commit/6197171b52ee4f44a4954ddd0e2e36769b189dee.patch";
       hash = "sha256-j2P7DEGD2B8q9Hh4G2mQng76fQdUpeAdFYoTD7Ui/Dk=";
+    })
+    (fetchpatch {
+      name = "fix-build-with-cython-3_1.patch";
+      url = "https://github.com/3-manifolds/CyPari/compare/622e112ffcf0383e2110954ff3ac3c42c006ebe1...50bcbd2b39177f5e4c5a3551a8a14f75ab05a5d6.patch";
+      hash = "sha256-6ayvtHMS3YtzzklHaaLzl9d4zHJhm0lVZQZFS9ykFY4=";
     })
   ];
 
