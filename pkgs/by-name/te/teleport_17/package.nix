d32c25ef77ac7269d5e710414da84a6391664012
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-09-08 15:52:47 +0200

    teleport_17: 17.7.0 -> 17.7.3
    Changelogs: https://github.com/gravitational/teleport/releases/tag/v17.7.1 https://github.com/gravitational/teleport/releases/tag/v17.7.2 https://github.com/gravitational/teleport/releases/tag/v17.7.3
    Diff: https://github.com/gravitational/teleport/compare/v17.7.0...v17.7.3
    (cherry picked from commit e171fab899c5237118128431aba6a262fe664a96)

--- pkgs/by-name/te/teleport_17/package.nix
+++ pkgs/by-name/te/teleport_17/package.nix
@@ -1,16 +1,16 @@
 {
   buildTeleport,
-  buildGo123Module,
+  buildGo124Module,
   wasm-bindgen-cli_0_2_95,
 }:
 
 buildTeleport rec {
-  version = "17.7.0";
-  hash = "sha256-+NfYpA6BDpD0/YCMj2y2torgw/ihd279SLTmPySIqvk=";
-  vendorHash = "sha256-NWZKLiub68OR0U3RkCOLCe4vrzXdOCitYm3ITOU3nhk=";
+  version = "17.7.3";
+  hash = "sha256-YSYkJRAeu7iPOs/gFnozZbks0Fx5srNH0VjrKvFmHZo=";
+  vendorHash = "sha256-7Rb94ERtp3H1Jwyh9d7AFT06d4xXdnfe5tpdvJQrbUQ=";
   cargoHash = "sha256-qz8gkooQTuBlPWC4lHtvBQpKkd+nEZ0Hl7AVg9JkPqs=";
-  pnpmHash = "sha256-I1mQ/F1ethOPA0jlrN+3ClByk7Ifw6LPbgjSvPx44D4=";
+  pnpmHash = "sha256-ZONs8z8mgBBQBmqaDGJKqhmtUKBrxE8BGYppbAqpQmg=";
 
   wasm-bindgen-cli = wasm-bindgen-cli_0_2_95;
-  buildGoModule = buildGo123Module;
+  buildGoModule = buildGo124Module;
 }
