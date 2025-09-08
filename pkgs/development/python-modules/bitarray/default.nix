528a3f55a99ad544dd75601d30cfa59e6f5383c5
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-08 21:32:46 +0000

    python3Packages.bitarray: 3.6.0 -> 3.7.1

--- pkgs/development/python-modules/bitarray/default.nix
+++ pkgs/development/python-modules/bitarray/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "bitarray";
-  version = "3.6.0";
+  version = "3.7.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-IP68hJofhY5qV6fUezI/6ecnxXnd1SbTF62IMXSKZqg=";
+    hash = "sha256-eVsXYEGKt1CCZCCuJPBvOSwI4h3CNPCjaaacwARE+Ow=";
   };
 
   build-system = [ setuptools ];
