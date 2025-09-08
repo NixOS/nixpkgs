2b78d8fdcbbf722820d91f93324a919542a41fcf
Sheikh <50134239+cybardev@users.noreply.github.com>
2025-09-08 01:11:24 -0300

    pyrefly: 0.28.1 -> 0.33.0

--- pkgs/by-name/py/pyrefly/package.nix
+++ pkgs/by-name/py/pyrefly/package.nix
@@ -7,17 +7,17 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "pyrefly";
-  version = "0.28.1";
+  version = "0.33.0";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "pyrefly";
     tag = finalAttrs.version;
-    hash = "sha256-KvMpuxQP4dofMXivMQnSgKnREv4rA8k2sj/TMuNycc0=";
+    hash = "sha256-sI+F+kI8ApE1cHpE3q/tSntaD/1c26jT5BLJ3BtzpBA=";
   };
 
   buildAndTestSubdir = "pyrefly";
-  cargoHash = "sha256-aOret3lR8oEF1ViHvn2atkalY3n9YS6vN4TlX/7QtCc=";
+  cargoHash = "sha256-+93SQA0KrJsskXUMA9S5eaq0EIWVK1/ZoUbp6wQ7Cc4=";
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   doInstallCheck = true;
