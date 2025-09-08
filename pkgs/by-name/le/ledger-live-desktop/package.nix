dece6131f790f63fdac918da0b5bae2005456e70
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-08 08:49:55 +0000

    ledger-live-desktop: 2.126.0 -> 2.126.2

--- pkgs/by-name/le/ledger-live-desktop/package.nix
+++ pkgs/by-name/le/ledger-live-desktop/package.nix
@@ -8,11 +8,11 @@
 
 let
   pname = "ledger-live-desktop";
-  version = "2.126.0";
+  version = "2.126.2";
 
   src = fetchurl {
     url = "https://download.live.ledger.com/${pname}-${version}-linux-x86_64.AppImage";
-    hash = "sha256-QAe8ft06Nna2rFyFTGK4mwfO1ueZKFcuacvKU4lln0k=";
+    hash = "sha256-cnY/XfJuzlT1HyyBJcmgsNF9ihkDBS/y35idTLj+DFE=";
   };
 
   appimageContents = appimageTools.extractType2 {
