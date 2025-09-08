47684e5689f5892be5009c2f81ea9aefb70603f4
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-08 15:16:35 +0000

    camunda-modeler: 5.38.1 -> 5.39.0

--- pkgs/by-name/ca/camunda-modeler/package.nix
+++ pkgs/by-name/ca/camunda-modeler/package.nix
@@ -10,11 +10,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "camunda-modeler";
-  version = "5.38.1";
+  version = "5.39.0";
 
   src = fetchurl {
     url = "https://github.com/camunda/camunda-modeler/releases/download/v${version}/camunda-modeler-${version}-linux-x64.tar.gz";
-    hash = "sha256-PsNOB2VTwxS9xZxZF79BE+ntF3EpQmdfTagXC2NFhtI=";
+    hash = "sha256-aDuJQWhgyQ0Aoh40Uyo4HPfNQDAhIg8Cd+veKEUynjs=";
   };
   sourceRoot = "camunda-modeler-${version}-linux-x64";
 
