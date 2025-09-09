cf617bc908503c56b024426f944466c44f03ba7b
Charles Hall <charles@computer.surgery>
2025-09-08 18:11:22 -0700

    engage: use finalAttrs

--- pkgs/by-name/en/engage/package.nix
+++ pkgs/by-name/en/engage/package.nix
@@ -6,18 +6,15 @@
   stdenv,
 }:
 
-let
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "engage";
   version = "0.2.0";
-in
-rustPlatform.buildRustPackage {
-  inherit pname version;
 
   src = fetchFromGitLab {
     domain = "gitlab.computer.surgery";
     owner = "charles";
     repo = "engage";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-niXh63xTpXSp9Wqwfi8hUBKJSClOUSvB+TPCTaqHfZk=";
   };
 
@@ -42,11 +39,11 @@ rustPlatform.buildRustPackage {
     description = "Task runner with DAG-based parallelism";
     mainProgram = "engage";
     homepage = "https://gitlab.computer.surgery/charles/engage";
-    changelog = "https://gitlab.computer.surgery/charles/engage/-/blob/v${version}/CHANGELOG.md";
+    changelog = "https://gitlab.computer.surgery/charles/engage/-/blob/v${finalAttrs.version}/CHANGELOG.md";
     license = with lib.licenses; [
       asl20
       mit
     ];
     maintainers = with lib.maintainers; [ CobaltCause ];
   };
-}
+})
