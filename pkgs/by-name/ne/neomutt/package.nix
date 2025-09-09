2f729ef07a01ea7c8ce4afb9da5ee682871b8151
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-09 14:56:51 +0000

    neomutt: 20250510 -> 20250905

--- pkgs/by-name/ne/neomutt/package.nix
+++ pkgs/by-name/ne/neomutt/package.nix
@@ -43,13 +43,13 @@ assert lib.warnIf (
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "neomutt";
-  version = "20250510";
+  version = "20250905";
 
   src = fetchFromGitHub {
     owner = "neomutt";
     repo = "neomutt";
     tag = finalAttrs.version;
-    hash = "sha256-62J7qyHC3hSgEgTA2zB+BQtZb+5BUXjQEOB3vGZGSNw=";
+    hash = "sha256-RLyszU2u5jV/o6LrmZFkLx/Wu94Yq3JlXNgpe4agOZI=";
   };
 
   buildInputs = [
