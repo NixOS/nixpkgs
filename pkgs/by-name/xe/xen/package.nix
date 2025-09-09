fbc146f31e226237f12b7451a4dfdab7aa908a4d
Fernando Rodrigues <alpha@sigmasquadron.net>
2025-09-09 22:45:20 +1000

    xen: patch with XSA-472
    Mutiple vulnerabilities in the Viridian interface
    
    There are multiple issues related to the handling and accessing of guest
    memory pages in the viridian code:
    
     1. A NULL pointer dereference in the updating of the reference TSC area.
        This is CVE-2025-27466.
    
     2. A NULL pointer dereference by assuming the SIM page is mapped when
        a synthetic timer message has to be delivered.  This is
        CVE-2025-58142.
    
     3. A race in the mapping of the reference TSC page, where a guest can
        get Xen to free a page while still present in the guest physical to
        machine (p2m) page tables.  This is CVE-2025-58143.
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>
    
    (cherry picked from commit 2648215258ade8d8d7ec33229395f596becae3c3)
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/by-name/xe/xen/package.nix
+++ pkgs/by-name/xe/xen/package.nix
@@ -1,5 +1,6 @@
 {
   buildXenPackage,
+  fetchpatch,
   python3Packages,
 }:
 
@@ -8,4 +9,20 @@ buildXenPackage.override { inherit python3Packages; } {
   version = "4.19.3";
   rev = "077419f04a3125c58dcf9724c954f98d1e927392";
   hash = "sha256-e9aPLgzNVxUn7WnLbBHwFIN02DAObfA24VjiqdiP+jA=";
+
+  patches = [
+    # XSA 472
+    (fetchpatch {
+      url = "https://xenbits.xen.org/xsa/xsa472-1.patch";
+      hash = "sha256-6k/X7KFno9uBG0mUtJxl7TMavaRs2Xlj9JlW9ai6p0k=";
+    })
+    (fetchpatch {
+      url = "https://xenbits.xen.org/xsa/xsa472-2.patch";
+      hash = "sha256-BisdztU9Wa5nIGmHo4IikqYPHdEhBehHaNqj1IuBe6I=";
+    })
+    (fetchpatch {
+      url = "https://xenbits.xen.org/xsa/xsa472-3.patch";
+      hash = "sha256-rikOofQeuLNMBkdQS3xzmwh7BlgMOTMSsQcAOEzNOso=";
+    })
+  ];
 }
