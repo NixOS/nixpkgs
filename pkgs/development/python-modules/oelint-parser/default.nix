cddc4aa781d8ba2259e99468b78878d74efa14bb
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-09 03:00:17 +0000

    python3Packages.oelint-parser: 8.2.4 -> 8.4.2

--- pkgs/development/python-modules/oelint-parser/default.nix
+++ pkgs/development/python-modules/oelint-parser/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "oelint-parser";
-  version = "8.2.4";
+  version = "8.4.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "priv-kweihmann";
     repo = "oelint-parser";
     tag = version;
-    hash = "sha256-MFbDK/uahH4ZNRY3f8vW0uMFC2frlYtqVOZHT3NwA30=";
+    hash = "sha256-/lfUmhMj8Gosece58f3HpiQHn+RVeA4xjzb5VG1gYVE=";
   };
 
   pythonRelaxDeps = [ "regex" ];
