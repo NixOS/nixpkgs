5f59c51a6330ab86e866132b08559e102775f8e1
Adam C. Stephens <adam@valkor.net>
2025-09-10 13:18:03 -0400

    erlang_28: 28.0.2 -> 28.0.3
    https://github.com/erlang/otp/releases/tag/OTP-28.0.3
    (cherry picked from commit 0095c1d3c38ea25dd526618f5d917af0a57a0e71)

--- pkgs/development/interpreters/erlang/28.nix
+++ pkgs/development/interpreters/erlang/28.nix
@@ -1,6 +1,6 @@
 { mkDerivation }:
 
 mkDerivation {
-  version = "28.0.2";
-  sha256 = "sha256-4+Jv7MUX4KAwasNyU7AiV9+Qd9NginYXTN0fDteTFEM=";
+  version = "28.0.3";
+  sha256 = "sha256-fNuUMJs4nPZF0iptPxrd7xi13CDmLnA348hnyPFgL5Q=";
 }
