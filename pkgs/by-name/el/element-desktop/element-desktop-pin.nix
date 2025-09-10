0cf5648a9bdf58e79635c288403639cdc499c0f4
transcaffeine <transcaffeine@finally.coffee>
2025-09-10 14:40:51 +0200

    element-desktop: 1.11.110 -> 1.11.111
    Release notes: https://github.com/element-hq/element-desktop/releases/tag/v1.11.111
    Full changelog: https://github.com/element-hq/element-desktop/compare/v1.11.110...v1.11.111

--- pkgs/by-name/el/element-desktop/element-desktop-pin.nix
+++ pkgs/by-name/el/element-desktop/element-desktop-pin.nix
@@ -1,7 +1,7 @@
 {
-  "version" = "1.11.110";
+  "version" = "1.11.111";
   "hashes" = {
-    "desktopSrcHash" = "sha256-iWdNiihpU29nek+EQjHmRi7dXadPYYoIt6bhufauKf8=";
-    "desktopYarnHash" = "sha256-l/hclDXT1JeToQPnWFDXU8JSN+oEm5hPYm4OQ7QJONk=";
+    "desktopSrcHash" = "sha256-QRnMHlq/gBoptJ/0iBdKUXbnR/oLVeA+ybF/0YFVBlo=";
+    "desktopYarnHash" = "sha256-U+MuOe0N29AFrLCi7Xa9bDW70SmhQqqtjim+x7QAiJg=";
   };
 }
