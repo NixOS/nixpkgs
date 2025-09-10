a3068ebb668fc855d16101b1f0c8ec82e1a83635
Tristan Ross <tristan.ross@midstall.com>
2025-09-09 21:43:42 -0700

    obs-studio-plugins: inherit platforms from obs-studio.meta
    Inherits platforms from obs-studio.meta so most builds on aarch64-linux.
    
    Skips the following due to not supporting aarch64-linux:
    
    - looking-glass-obs
    - waveform

--- pkgs/applications/video/obs-studio/plugins/obs-mute-filter.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-mute-filter.nix
@@ -32,9 +32,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/norihiro/obs-mute-filter";
     maintainers = with maintainers; [ flexiondotorg ];
     license = licenses.gpl2Plus;
-    platforms = [
-      "x86_64-linux"
-      "i686-linux"
-    ];
+    inherit (obs-studio.meta) platforms;
   };
 }
--- pkgs/applications/video/obs-studio/plugins/obs-scale-to-sound.nix
+++ pkgs/applications/video/obs-studio/plugins/obs-scale-to-sound.nix
@@ -30,9 +30,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/dimtpap/obs-scale-to-sound";
     maintainers = with lib.maintainers; [ flexiondotorg ];
     license = lib.licenses.gpl2Plus;
-    platforms = [
-      "x86_64-linux"
-      "i686-linux"
-    ];
+    inherit (obs-studio.meta) platforms;
   };
 }
