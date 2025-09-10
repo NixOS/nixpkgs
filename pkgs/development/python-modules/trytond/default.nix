40f1907d7ec307087156fc0afefe6093917bef35
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-10 23:13:24 +0000

    python3Packages.trytond: 7.6.5 -> 7.6.6

--- pkgs/development/python-modules/trytond/default.nix
+++ pkgs/development/python-modules/trytond/default.nix
@@ -30,14 +30,14 @@
 
 buildPythonPackage rec {
   pname = "trytond";
-  version = "7.6.5";
+  version = "7.6.6";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-1HMFHBRuVJG5oonfxQzsgonJGG5ZmOXoLDkRlryTSXM=";
+    hash = "sha256-TVgCN+DLTdoYfrCIt+Pvl6xcioV2BqJwKbo+QIaCXxY=";
   };
 
   build-system = [ setuptools ];
