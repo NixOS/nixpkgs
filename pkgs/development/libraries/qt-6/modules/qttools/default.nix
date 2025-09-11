8503c157ebfbcc66970209adcdeb13e20e90e9d1
nyanloutre <paul@nyanlout.re>
2025-09-11 08:47:08 +0000

    qt6.qttools: add desktop files for GUI tools

--- pkgs/development/libraries/qt-6/modules/qttools/default.nix
+++ pkgs/development/libraries/qt-6/modules/qttools/default.nix
@@ -7,6 +7,7 @@
   qtdeclarative,
   cups,
   llvmPackages,
+  fetchpatch,
   # clang-based c++ parser for qdoc and lupdate
   withClang ? false,
 }:
@@ -22,12 +23,20 @@ qtModule {
     qtdeclarative
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [ cups ];
-  cmakeFlags = lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+  cmakeFlags = [
+    "-DQT_INSTALL_XDG_DESKTOP_ENTRIES=ON"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
     "-DQt6LinguistTools_DIR=${pkgsBuildBuild.qt6.qttools}/lib/cmake/Qt6LinguistTools"
     "-DQt6ToolsTools_DIR=${pkgsBuildBuild.qt6.qttools}/lib/cmake/Qt6ToolsTools"
   ];
   patches = [
     ./paths.patch
+    (fetchpatch {
+      name = "add-desktop-files.patch";
+      url = "https://code.qt.io/cgit/qt/qttools.git/patch/?id=9cbd235f7e9dd5baea2f55a6fe1c29a7abd7255b";
+      hash = "sha256-Plu9Wx7pPxTa0jkNG96gBYb1T0L9VQC5xBoHsVj/NC4=";
+    })
   ];
   env.NIX_CFLAGS_COMPILE = toString [
     "-DNIX_OUTPUT_OUT=\"${placeholder "out"}\""
