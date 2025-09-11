a80e03233af56d951e4cbab4b611885f551e0826
John Garcia <jgarcia3788@yahoo.co.uk>
2025-09-11 10:01:50 +0100

    remnote: 1.20.0 -> 1.20.8

--- pkgs/by-name/re/remnote/package.nix
+++ pkgs/by-name/re/remnote/package.nix
@@ -6,10 +6,10 @@
 }:
 let
   pname = "remnote";
-  version = "1.20.0";
+  version = "1.20.8";
   src = fetchurl {
     url = "https://download2.remnote.io/remnote-desktop2/RemNote-${version}.AppImage";
-    hash = "sha256-H17jPk1wDDuygXyrcVg5WLwVPYt3yG/57ec8n1GJmHg=";
+    hash = "sha256-P2fUPYn/ZMBOGZKytrMPPKh4KLbGhQ1jR+dLG5JNfVo=";
   };
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
 in
