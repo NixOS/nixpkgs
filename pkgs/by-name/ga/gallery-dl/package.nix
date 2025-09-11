98130f7ba6196c95ec128031516506275d77ceec
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-11 23:16:54 +0000

    gallery-dl: 1.30.5 -> 1.30.6
    (cherry picked from commit ef9d0a87b9405f441eba89825b7936d9383a0dfb)

--- pkgs/by-name/ga/gallery-dl/package.nix
+++ pkgs/by-name/ga/gallery-dl/package.nix
@@ -8,7 +8,7 @@
 
 let
   pname = "gallery-dl";
-  version = "1.30.5";
+  version = "1.30.6";
 in
 python3Packages.buildPythonApplication {
   inherit pname version;
@@ -18,7 +18,7 @@ python3Packages.buildPythonApplication {
     owner = "mikf";
     repo = "gallery-dl";
     tag = "v${version}";
-    hash = "sha256-RsYg3DSiB6DWVlwAJT7iN7rNxUJqT5EAIGNEuMuIm8Y=";
+    hash = "sha256-UCioROzdfYYtTzKegEUNsF1ecKxbwW4k1cPygjpiXjo=";
   };
 
   build-system = [ python3Packages.setuptools ];
