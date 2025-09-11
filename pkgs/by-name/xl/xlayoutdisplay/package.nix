85894e03e82480ac1ed15e0f61313a113cbad6ee
Stephen Huan <stephen.huan@cgdct.moe>
2025-09-11 12:55:51 -0400

    xlayoutdisplay: 1.5.0 -> 1.5.1

--- pkgs/by-name/xl/xlayoutdisplay/package.nix
+++ pkgs/by-name/xl/xlayoutdisplay/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch2,
   pkg-config,
   xorg,
   boost,
@@ -11,23 +10,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xlayoutdisplay";
-  version = "1.5.0";
+  version = "1.5.1";
 
   src = fetchFromGitHub {
     owner = "alex-courtis";
     repo = "xlayoutdisplay";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-A37jFhVTW/3QNEf776Oi3ViRK+ebOPRTsEQqdmNhA7E=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-gJucWffchhTFdYEQqjbj1OdPTBSmGDDcKbOyIWdWQig=";
   };
-
-  patches = [
-    # https://github.com/alex-courtis/xlayoutdisplay/pull/34
-    (fetchpatch2 {
-      name = "cpp-version.patch";
-      url = "https://github.com/alex-courtis/xlayoutdisplay/commit/56983b45070edde78cc816d9cff4111315e94a7a.patch";
-      hash = "sha256-zd28Nkw8Kmm20zGT6wvdBHcHfE4p+RFotUO9zJwPQMc=";
-    })
-  ];
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = with xorg; [
