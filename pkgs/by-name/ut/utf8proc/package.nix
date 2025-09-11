2d4d0482f60283f497306d73e73de2f3777ce539
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-11 04:53:40 +0000

    utf8proc: 2.10.0 -> 2.11.0

--- pkgs/by-name/ut/utf8proc/package.nix
+++ pkgs/by-name/ut/utf8proc/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "utf8proc";
-  version = "2.10.0";
+  version = "2.11.0";
 
   src = fetchFromGitHub {
     owner = "JuliaStrings";
     repo = "utf8proc";
     rev = "v${version}";
-    hash = "sha256-wmtMo6eBK/xxxkIeJfh5Yb293po9cKK+7WjqNPoxM9g=";
+    hash = "sha256-iNITnxA1cacOBRU/XV22yzjB0XUOCYsaLLLPYLa+AoA=";
   };
 
   nativeBuildInputs = [ cmake ];
