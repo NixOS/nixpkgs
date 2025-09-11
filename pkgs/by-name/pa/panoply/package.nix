3e0c346fb7c35e48d57b5f1b1b8ee63d61292f19
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-11 02:17:26 +0000

    panoply: 5.7.0 -> 5.7.1

--- pkgs/by-name/pa/panoply/package.nix
+++ pkgs/by-name/pa/panoply/package.nix
@@ -8,11 +8,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "panoply";
-  version = "5.7.0";
+  version = "5.7.1";
 
   src = fetchurl {
     url = "https://www.giss.nasa.gov/tools/panoply/download/PanoplyJ-${version}.tgz";
-    hash = "sha256-R3hzqYytQ0k2dhmJAOIKy+zbEYQ6et/q3b0NEQkAGt4=";
+    hash = "sha256-DWH0nggPXws1ZVPPT6EH1ede9pFuE+BD4kcLUXH4o3k=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
