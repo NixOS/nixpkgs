078730139087327c3eff5306ae5dc191791ee82c
provokateurin <kate@provokateurin.de>
2025-09-12 21:55:58 +0200

    fetchNextcloudApp: move to nextcloud sub-directory

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -576,7 +576,7 @@ with pkgs;
     tests = pkgs.tests.fetchFirefoxAddon;
   };
 
-  fetchNextcloudApp = callPackage ../build-support/fetchnextcloudapp { };
+  fetchNextcloudApp = callPackage ../build-support/nextcloud/fetchnextcloudapp { };
 
   # `fetchurl' downloads a file from the network.
   fetchurl =
