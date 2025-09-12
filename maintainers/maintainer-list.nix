f0a6ff8aa691b9c943197efe896738ae5286a311
Quote <123989069+Silk-OT@users.noreply.github.com>
2025-09-12 17:56:39 -0400

    Update maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23793,9 +23793,9 @@
     keys = [ { fingerprint = "ADF4 C13D 0E36 1240 BD01  9B51 D1DE 6D7F 6936 63A5"; } ];
   };
   Silk-OT = {
-    name = "Quote"
-    email = "quotesilk@proton.me"
-    github = "Silk-OT"
+    name = "Quote";
+    email = "quotesilk@proton.me";
+    github = "Silk-OT";
     githubId = 123989069;
   };
   silky = {
