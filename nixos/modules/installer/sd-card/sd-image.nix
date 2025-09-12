acc041562785d600be113f1747e0c6a97b652d18
Jan B. <git@keksgesicht.de>
2025-09-12 14:01:46 +0200

    nixos/installer: add option to override the root filesystem image
    allows overriding the process how the filesystem image is created which the resulting sd-card image will use.

--- nixos/modules/installer/sd-card/sd-image.nix
+++ nixos/modules/installer/sd-card/sd-image.nix
@@ -142,6 +142,15 @@ in
       '';
     };
 
+    rootImage = mkOption {
+      type = types.package;
+      default = rootfsImage;
+      description = ''
+        The finished root partition image with all custom fileystem modifications.
+        This is sometimes needed then you need to override the filesystem creator itself.
+      '';
+    };
+
     firmwareSize = mkOption {
       type = types.int;
       # As of 2019-08-18 the Raspberry pi firmware + u-boot takes ~18MiB
@@ -268,11 +277,11 @@ in
             echo "file sd-image $img" >> $out/nix-support/hydra-build-products
           fi
 
-          root_fs=${rootfsImage}
+          root_fs=${config.sdImage.rootImage}
           ${lib.optionalString config.sdImage.compressImage ''
             root_fs=./root-fs.img
             echo "Decompressing rootfs image"
-            zstd -d --no-progress "${rootfsImage}" -o $root_fs
+            zstd -d --no-progress "${config.sdImage.rootImage}" -o $root_fs
           ''}
 
           # Gap in front of the first partition, in MiB
