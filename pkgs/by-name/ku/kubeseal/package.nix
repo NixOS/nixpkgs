0b869ae4d4f6ab0f4253fda7f8cc9c135ece5e70
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-12 13:43:11 +0000

    kubeseal: 0.31.0 -> 0.32.1

--- pkgs/by-name/ku/kubeseal/package.nix
+++ pkgs/by-name/ku/kubeseal/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "kubeseal";
-  version = "0.31.0";
+  version = "0.32.1";
 
   src = fetchFromGitHub {
     owner = "bitnami-labs";
     repo = "sealed-secrets";
     rev = "v${version}";
-    sha256 = "sha256-hHCHAvBLsTb0316/I5N3lUBEJul7Uh7ViZVqNVCBtog=";
+    sha256 = "sha256-JwOKyxbLy1BikRSVPONDO57YyhhaqOELZSVlMlsfUUw=";
   };
 
-  vendorHash = "sha256-KUSwNnMYn1XlKJdEEsHDeyTGi9gATVvCQoQDRHx+Z3A=";
+  vendorHash = "sha256-zoi8Z5Jmg5E1c9B4m6870hvX0C3gFDzOCdGbdlBa70M=";
 
   subPackages = [ "cmd/kubeseal" ];
 
