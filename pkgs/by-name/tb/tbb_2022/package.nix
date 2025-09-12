2c33a3a74895de28784c51b1695cb164321fef42
Emily <vcs@emily.moe>
2025-09-12 23:03:47 +0100

    tbb_2022: drop obsolete compiler flags

--- pkgs/by-name/tb/tbb_2022/package.nix
+++ pkgs/by-name/tb/tbb_2022/package.nix
@@ -43,18 +43,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   # Fix build with modern gcc
   # In member function 'void std::__atomic_base<_IntTp>::store(__int_type, std::memory_order) [with _ITp = bool]',
-  NIX_CFLAGS_COMPILE =
-    lib.optionals stdenv.cc.isGNU [
-      "-Wno-error=array-bounds"
-      "-Wno-error=stringop-overflow"
-    ]
-    ++
-      # error: variable 'val' set but not used
-      lib.optionals stdenv.cc.isClang [ "-Wno-error=unused-but-set-variable" ]
-    ++
-      # Workaround for gcc-12 ICE when using -O3
-      # https://gcc.gnu.org/PR108854
-      lib.optionals (stdenv.cc.isGNU && stdenv.hostPlatform.isx86_32) [ "-O2" ];
+  NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isGNU "-Wno-error=stringop-overflow";
 
   # Fix undefined reference errors with version script under LLVM.
   NIX_LDFLAGS = lib.optionalString (
