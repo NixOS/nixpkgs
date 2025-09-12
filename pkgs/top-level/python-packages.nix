a1a7f6ed6ed89fd45bc1276c839f734ac0cee3f8
Sylvain Fankhauser <sephi@nibelheim.ch>
2025-09-12 16:13:42 +0200

    jpegiptc: init at 1.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7529,6 +7529,8 @@ self: super: with self; {
 
   joserfc = callPackage ../development/python-modules/joserfc { };
 
+  jpegiptc = callPackage ../development/python-modules/jpegiptc { };
+
   jplephem = callPackage ../development/python-modules/jplephem { };
 
   jproperties = callPackage ../development/python-modules/jproperties { };
