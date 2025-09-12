788284bef5569f04c0d27030e98c8bf386d97dbd
paumr <paum@bdisk.eu>
2025-09-12 14:43:03 +0200

    icann-rdap: 0.0.22 -> 0.0.23

--- pkgs/by-name/ic/icann-rdap/package.nix
+++ pkgs/by-name/ic/icann-rdap/package.nix
@@ -9,24 +9,23 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "icann-rdap";
-  version = "0.0.22";
+  version = "0.0.23";
 
   src = fetchFromGitHub {
     owner = "icann";
     repo = "icann-rdap";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-J8uvFTjFY7YrohxlD7UwzrQUUoHHEb5mzZoQ64XUQHY=";
+    hash = "sha256-1e8r8JkRUokleQnDzxYLClC82R4HXlctNXxfSN3DicY=";
   };
 
-  cargoHash = "sha256-2R6GrcuksEOk8GiVkFhMljS12V2n0J1rCw9MCOtwMjA=";
+  cargoHash = "sha256-4PFLQeLMcUYEK2v0dR0EmHUcqtRqZ3UamFVOzUNAPwE=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
 
   env.OPENSSL_NO_VENDOR = true;
 
-  # https://github.com/icann/icann-rdap/issues/138
-  doCheck = !stdenv.hostPlatform.isDarwin;
+  __darwinAllowLocalNetworking = true;
 
   meta = {
     description = "Official ICANN RDAP tools, containing cli client and server";
