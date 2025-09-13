2de7ce643bb7c201da33524d51c9e9d896ec922d
Philip Taron <philip.taron@gmail.com>
2025-09-13 16:04:08 -0700

    aws-c-s3: 0.8.6 -> 0.8.7
    Diff: https://github.com/awslabs/aws-c-s3/compare/v0.8.6...v0.8.7

--- pkgs/by-name/aw/aws-c-s3/package.nix
+++ pkgs/by-name/aw/aws-c-s3/package.nix
@@ -17,13 +17,13 @@
 stdenv.mkDerivation rec {
   pname = "aws-c-s3";
   # nixpkgs-update: no auto update
-  version = "0.8.6";
+  version = "0.8.7";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-c-s3";
     rev = "v${version}";
-    hash = "sha256-g2w1igjv0N0o6+bewypJm2coHTvhYN2v8usdMN7TBI4=";
+    hash = "sha256-8yUwgiZ50BiItapeg0zIc5vr0+OFHvzIRrwWH4lQFBM=";
   };
 
   nativeBuildInputs = [
