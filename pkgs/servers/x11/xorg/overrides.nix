e9742cf527bd947cfbf6674430efdf74aee0f12c
quantenzitrone <general@dev.quantenzitrone.eu>
2025-09-13 23:53:11 +0200

    font-bitstream-{100dpi,75dpi,type1}: move font output from $out/lib to $out/share

--- pkgs/servers/x11/xorg/overrides.nix
+++ pkgs/servers/x11/xorg/overrides.nix
@@ -902,5 +902,5 @@ self: super:
 
 # deprecate some packages
 // lib.optionalAttrs config.allowAliases {
-  fontbitstreamspeedo = throw "Bitstream Speedo is an obsolete font format that hasn't been supported by Xorg since 2005";
+  fontbitstreamspeedo = throw "Bitstream Speedo is an obsolete font format that hasn't been supported by Xorg since 2005"; # added 2025-09-24
 }
