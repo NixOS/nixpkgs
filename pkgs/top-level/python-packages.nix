ded86696d3fb0d67ba244e0c2a22656d5ba58776
provokateurin <kate@provokateurin.de>
2025-09-13 14:47:38 +0200

    python3Packages.sphinxcontrib-mermaid: init at 1.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17460,6 +17460,8 @@ self: super: with self; {
 
   sphinxcontrib-log-cabinet = callPackage ../development/python-modules/sphinxcontrib-log-cabinet { };
 
+  sphinxcontrib-mermaid = callPackage ../development/python-modules/sphinxcontrib-mermaid { };
+
   sphinxcontrib-moderncmakedomain =
     callPackage ../development/python-modules/sphinxcontrib-moderncmakedomain
       { };
