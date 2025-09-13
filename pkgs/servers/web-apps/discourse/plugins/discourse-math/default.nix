a9c855e0949f0545e607b31504a5712db5ef8d70
Leona Maroni <dev@leona.is>
2025-09-13 10:47:57 +0200

    discourse: 3.4.2 -> 3.5.0
    https://meta.discourse.org/t/3-5-0-major-release/379212?tl=en
    
    This release introduces a few rust-based ruby dependencies.
    
    Two plugins were removed
    - discourse-migratepassword as it's deprecated
    - discourse-topic-voting as it's integrated into discourse core

--- pkgs/servers/web-apps/discourse/plugins/discourse-math/default.nix
+++ pkgs/servers/web-apps/discourse/plugins/discourse-math/default.nix
@@ -9,8 +9,8 @@ mkDiscoursePlugin {
   src = fetchFromGitHub {
     owner = "discourse";
     repo = "discourse-math";
-    rev = "90a56a5b463546cba5017cf25168bae759bcbf77";
-    sha256 = "sha256-q8cNRIHYEkaE6QkdsHu4tPwSw3LnCNgKjbyESBXheE0=";
+    rev = "8db3f371e543bdf21fba35b1a539036386517272";
+    sha256 = "sha256-ARyPDpDUlPlhhw4KL+eo5AUzp1F2moenXEemV72kfsI=";
   };
   meta = with lib; {
     homepage = "https://github.com/discourse/discourse-math";
--- pkgs/servers/web-apps/discourse/plugins/discourse-solved/default.nix
+++ pkgs/servers/web-apps/discourse/plugins/discourse-solved/default.nix
@@ -9,8 +9,8 @@ mkDiscoursePlugin {
   src = fetchFromGitHub {
     owner = "discourse";
     repo = "discourse-solved";
-    rev = "e82c6ae1ca38ccebb34669148f8de93a3028906e";
-    sha256 = "sha256-KwpBCk4fjkZlSzgtc0OXvQupPd+aPb9nONyyALpEZhg=";
+    rev = "8e777aa05dd3a303ccd772226e24b2e5f4d6be31";
+    sha256 = "sha256-z/fB+QQW6iMBBDHUWvr5UmWyLyo18qnXddEOiPM7aRQ=";
   };
   meta = with lib; {
     homepage = "https://github.com/discourse/discourse-solved";
