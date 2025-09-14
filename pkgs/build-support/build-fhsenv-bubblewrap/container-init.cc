4285bf563a36c6af3b0d2e7b38a6d8473449da20
Julien Moutinho <julm+nixpkgs@sourcephile.fr>
2025-09-14 02:13:25 +0200

    buildFHSEnvBubblewrap: unmute ldconfig

--- pkgs/build-support/build-fhsenv-bubblewrap/container-init.cc
+++ pkgs/build-support/build-fhsenv-bubblewrap/container-init.cc
@@ -37,7 +37,6 @@ int main(int, const char *argv[]) {
   char *ldconfig_envp[] = {NULL};
   posix_spawn_file_actions_t action;
   posix_spawn_file_actions_init(&action);
-  posix_spawn_file_actions_addopen (&action, STDERR_FILENO, "/dev/null", O_WRONLY|O_APPEND, 0);
   if ((e = posix_spawn(&pid, ldconfig_argv[0], &action, NULL,
                        (char *const *)ldconfig_argv, ldconfig_envp))) {
     fprintf(stderr, "Failed to run ldconfig: %s\n", strerror(e));
