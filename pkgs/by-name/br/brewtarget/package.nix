8c319dd31fd1e75a2272ea41d6499c789cb2d742
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-14 13:57:29 +0000

    brewtarget: 4.1.3 -> 4.2.1

--- pkgs/by-name/br/brewtarget/package.nix
+++ pkgs/by-name/br/brewtarget/package.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "brewtarget";
-  version = "4.1.3";
+  version = "4.2.1";
 
   src = fetchFromGitHub {
     owner = "Brewtarget";
     repo = "brewtarget";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VVdihVdNIAtPlugqGWDWvxMdOFGLnRmewPt6BgvbxBk=";
+    hash = "sha256-/BEVnAtuwDnJXra9EAMD3LhA/93nFJ5ObCSgSy3CJnk=";
     fetchSubmodules = true;
   };
 
