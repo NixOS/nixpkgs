58cf0a2b7bf057ed97a04846e5cef002f4086c1c
Florian Brandes <florian.brandes@posteo.de>
2025-09-15 18:30:12 +0200

    octoprint: 1.11.2 -> 1.11.3
    Signed-off-by: Florian Brandes <florian.brandes@posteo.de>

--- pkgs/by-name/oc/octoprint/package.nix
+++ pkgs/by-name/oc/octoprint/package.nix
@@ -92,14 +92,14 @@ let
       (self: super: {
         octoprint = self.buildPythonPackage rec {
           pname = "OctoPrint";
-          version = "1.11.2";
+          version = "1.11.3";
           format = "setuptools";
 
           src = fetchFromGitHub {
             owner = "OctoPrint";
             repo = "OctoPrint";
             rev = version;
-            hash = "sha256-D6lIEa7ee44DWavMLaXIo7RsKwaMneYqOBQk626pI20=";
+            hash = "sha256-AyRi9aQXLFggBzc6WH2kvRPkJu1ANX/++GdCJRNhY/A=";
           };
 
           propagatedBuildInputs =
