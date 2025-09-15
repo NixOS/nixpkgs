7b68690e1653e4a047e91eae88110459c01acd09
Thierry Delafontaine <delafthi@pm.me>
2025-09-15 21:34:48 +0200

    espansoPlugins.quotes: 0.1.0 -> 0.1.1

--- pkgs/by-name/es/espanso/plugins/quotes/default.nix
+++ pkgs/by-name/es/espanso/plugins/quotes/default.nix
@@ -5,13 +5,13 @@
 }:
 mkEspansoPlugin {
   pname = "quotes";
-  version = "0.1.0";
+  version = "0.1.1";
 
   src = fetchFromGitHub {
     owner = "espanso";
     repo = "hub";
-    rev = "d62bde95fc6f6cf90d4c23ee243922037c053def";
-    hash = "sha256-1cPtknrIi+9uTQt0pMaOBnY3C1GAGXPAA4fIgBh5Wgs=";
+    rev = "13eb7d6d7d242bbadfb43ed4284d6b46ba63ce11";
+    hash = "sha256-r0V24FeBiS8LRzbzMjLKJqitpoiOWnuynGMahmDYe3s=";
   };
 
   meta = {
