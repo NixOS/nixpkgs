be82005317d9e3a63b1a0a7a756fbb778c8618b1
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-15 05:02:30 +0000

    gradia: 1.9.0 -> 1.10.1

--- pkgs/by-name/gr/gradia/package.nix
+++ pkgs/by-name/gr/gradia/package.nix
@@ -22,14 +22,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "gradia";
-  version = "1.9.0";
+  version = "1.10.1";
   pyproject = false;
 
   src = fetchFromGitHub {
     owner = "AlexanderVanhee";
     repo = "Gradia";
     tag = "v${version}";
-    hash = "sha256-iDldzS7LLJ/+CfKBpD50LW/YrZ2xb8aqZI9Bs1AOcCM=";
+    hash = "sha256-Nc6ScPf0B2/pk9zF52810FFjAahsitPNOsDJieZwyVw=";
   };
 
   nativeBuildInputs = [
