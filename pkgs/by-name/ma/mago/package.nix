e66f3eb102d323592025727c959de9845a007e71
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-09-15 10:49:07 +0200

    mago: use `finalAttrs` pattern

--- pkgs/by-name/ma/mago/package.nix
+++ pkgs/by-name/ma/mago/package.nix
@@ -8,15 +8,15 @@
   mago,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "mago";
   version = "0.26.1";
 
   src = fetchFromGitHub {
     owner = "carthage-software";
     repo = "mago";
-    tag = version;
     hash = "sha256-9WrSHjs1EdDqTXuB0HbIzQCQWp4okkdy3jTVl4J2wUg=";
+    tag = finalAttrs.version;
   };
 
   cargoHash = "sha256-7/kbuWcy1IwAL7m87WPgyhwPidLL9K65u6ybpj0Ryl0=";
@@ -39,11 +39,11 @@ rustPlatform.buildRustPackage rec {
   };
 
   meta = {
-    changelog = "https://github.com/carthage-software/mago/releases/tag/${version}";
+    changelog = "https://github.com/carthage-software/mago/releases/tag/${finalAttrs.version}";
     description = "Toolchain for PHP that aims to provide a set of tools to help developers write better code";
     homepage = "https://github.com/carthage-software/mago";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ gaelreyrol ];
     mainProgram = "mago";
   };
-}
+})
