b06daee242e453558b7a92b5284fee2cd017348d
ghpzin <ghpzin@gmail.com>
2025-09-15 22:24:00 +0300

    unifdef: fix build with gcc15
    - add patch from unmerged upstream PR:
    https://www.github.com/fanf2/unifdef/pull/19
    
    Fixes build failure with gcc15:
    ```
    unifdef.c:205:1: error: 'constexpr' in empty declaration
    unifdef.c: In function 'eval_unary':
    unifdef.c:1089:27: error: expected identifier or '(' before '=' token
     1089 |                 constexpr = false;
          |                           ^
    unifdef.c:1106:27: error: expected identifier or '(' before '=' token
     1106 |                 constexpr = false;
          |                           ^
    unifdef.c: In function 'ifeval':
    unifdef.c:1173:19: error: expected identifier or '(' before '=' token
     1173 |         constexpr = killconsts ? false : true;
          |                   ^
    unifdef.c:1176:27: error: expected specifier-qualifier-list before '?' token
     1176 |         return (constexpr ? LT_IF : ret == LT_ERROR ? LT_IF : ret);
          |                           ^
    ```

--- pkgs/by-name/un/unifdef/package.nix
+++ pkgs/by-name/un/unifdef/package.nix
@@ -13,6 +13,12 @@ stdenv.mkDerivation rec {
     sha256 = "00647bp3m9n01ck6ilw6r24fk4mivmimamvm4hxp5p6wxh10zkj3";
   };
 
+  patches = [
+    # Fix build with gcc15
+    # https://github.com/fanf2/unifdef/pull/19
+    ./unifdef-fix-build-with-gcc15.patch
+  ];
+
   makeFlags = [
     "prefix=$(out)"
     "DESTDIR="
