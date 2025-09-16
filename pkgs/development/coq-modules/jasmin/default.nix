266277045cb2e53cc62f219889ca337a659eab48
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-09-16 21:42:08 +0200

    coqPackages.jasmin: 2025.02.0 → 2025.06.1
    (cherry picked from commit 52b2713487fa59d74c2940c52875ef89a8c2db8c)

--- pkgs/development/coq-modules/jasmin/default.nix
+++ pkgs/development/coq-modules/jasmin/default.nix
@@ -5,10 +5,11 @@
   mathcomp,
   mathcomp-algebra-tactics,
   mathcomp-word,
+  ITree,
   version ? null,
 }:
 
-mkCoqDerivation {
+(mkCoqDerivation {
   pname = "jasmin";
   owner = "jasmin-lang";
 
@@ -20,10 +21,17 @@ mkCoqDerivation {
       [
         {
           cases = [
+            (range "8.20" "9.0")
+            (range "2.2" "2.4")
+          ];
+          out = "2025.06.1";
+        }
+        {
+          cases = [
             (range "8.19" "9.0")
             (range "2.2" "2.4")
           ];
-          out = "2025.02.0";
+          out = "2025.02.1";
         }
         {
           cases = [
@@ -36,6 +44,9 @@ mkCoqDerivation {
       null;
   releaseRev = v: "v${v}";
 
+  release."2025.06.1".sha256 = "sha256-wEL1tN0HUa1Eb7FiQOBA6sAkuonrAMdkqq8gu9/CED0=";
+  release."2025.06.0".sha256 = "sha256-XfTg7ofamzMWqmRIU1/MO+S/ieNjvNEhlgIqFrchdAQ=";
+  release."2025.02.1".sha256 = "sha256-8P2GdplB12Q0e0XdL77w3nQL1/6Xl/gQNhGTB0WX/8I=";
   release."2025.02.0".sha256 = "sha256-Jlf0+VPuYWXdWyKHKHSp7h/HuCCp4VkcrgDAmh7pi5s=";
   release."2024.07.3".sha256 = "sha256-n/X8d7ILuZ07l24Ij8TxbQzAG7E8kldWFcUI65W4r+c=";
   release."2024.07.2".sha256 = "sha256-aF8SYY5jRxQ6iEr7t6mRN3BEmIDhJ53PGhuZiJGB+i8=";
@@ -59,4 +70,9 @@ mkCoqDerivation {
       vbgl
     ];
   };
-}
+}).overrideAttrs
+  (o: {
+    propagatedBuildInputs =
+      o.propagatedBuildInputs
+      ++ lib.optional (o.version == "dev" || lib.versionAtLeast o.version "2025.06") ITree;
+  })
