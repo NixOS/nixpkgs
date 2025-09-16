d71d93c5ed2e55f362b522e19c63a6a51d0b568a
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-16 16:14:07 +0000

    diesel-cli: 2.2.12 -> 2.3.0

--- pkgs/by-name/di/diesel-cli/package.nix
+++ pkgs/by-name/di/diesel-cli/package.nix
@@ -27,15 +27,15 @@ assert lib.assertMsg (lib.elem true [
 
 rustPlatform.buildRustPackage rec {
   pname = "diesel-cli";
-  version = "2.2.12";
+  version = "2.3.0";
 
   src = fetchCrate {
     inherit version;
     crateName = "diesel_cli";
-    hash = "sha256-cBufd4HwNffkK2VDPMMUT1qZfgKNa6XKpxT5QlQesyc=";
+    hash = "sha256-s/RJ83TuW42MIYgKXq5tlqJ73u0aucuXrR8bFArqT5A=";
   };
 
-  cargoHash = "sha256-CmzUe/R9iFU//u0/FxMonYWyx0EJnI/blUktYN/eNe8=";
+  cargoHash = "sha256-XmWrXLOYm7y7+0mqT0WSmT6LhHAPA9UbGODlluBKyZw=";
 
   nativeBuildInputs = [
     installShellFiles
