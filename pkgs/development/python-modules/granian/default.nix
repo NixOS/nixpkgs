914a6aae68f7281617145ea5fbd8cb4fd5666f62
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-16 14:38:40 +0000

    python3Packages.granian: 2.5.2 -> 2.5.3

--- pkgs/development/python-modules/granian/default.nix
+++ pkgs/development/python-modules/granian/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "granian";
-  version = "2.5.2";
+  version = "2.5.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "emmett-framework";
     repo = "granian";
     tag = "v${version}";
-    hash = "sha256-HeTmfOJ5DEW47UJdovqKpbIOGAoKof5wPDJ1VTsFo2o=";
+    hash = "sha256-I5rwSb//F2O3/QgWkyeUCVJRBUn6vhYNBt/16n7bC20=";
   };
 
   # Granian forces a custom allocator for all the things it runs,
@@ -39,7 +39,7 @@ buildPythonPackage rec {
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit pname version src;
-    hash = "sha256-8bvjetAgTz3mRzC+jI09djyyo9amTrIg4hnZx9ir1wU=";
+    hash = "sha256-vSw72PiIzlmjAbDENhaju3IpC2NKzFE7UqaitE3976c=";
   };
 
   nativeBuildInputs = with rustPlatform; [
