07d2354ba1bdb8c6e0fe532523c67ec7e5759d10
Jasi <normalc@posteo.net>
2025-09-17 10:00:41 -0400

    nixos/file-roller: drop
    Drops the `programs.file-roller` option as it is not needed for the
    program to function and removes all instances of it being used within
    nixpkgs.
    
    In addition, file-roller is also removed from being included by
    default in GNOME as it's not part of the GNOME core applications and
    file archival functions are handled by nautilus (GNOME Files).

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -207,7 +207,6 @@
   ./programs/extra-container.nix
   ./programs/fcast-receiver.nix
   ./programs/feedbackd.nix
-  ./programs/file-roller.nix
   ./programs/firefox.nix
   ./programs/firejail.nix
   ./programs/fish.nix
