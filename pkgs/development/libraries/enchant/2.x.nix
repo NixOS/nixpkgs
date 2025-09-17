NixOS/nixpkgs

commit fa355f2ab34d3457d6c0894cb778a50571d83dcb

libsidplayfp: drop unused `unittest-cpp` dependency
Replaced upstream with a vendored <https://github.com/neacsum/utpp>.

pkgs/development/libraries/enchant/2.x.nix
@@ -10,7 +10,6 @@
   hspell,
   nuspell,
   libvoikko,
-  unittest-cpp,
 
   withHspell ? true,
   withAspell ? true,
@@ -58,7 +57,6 @@ stdenv.mkDerivation rec {
   ];
 
   checkInputs = [
-    unittest-cpp
   ];
 
   # libtool puts these to .la files

pkgs/by-name/li/libsidplayfp/package.nix
@@ -13,7 +13,6 @@
   libgcrypt,
   perl,
   pkg-config,
-  unittest-cpp,
   xa,
 }:
 
@@ -52,8 +51,6 @@ stdenv.mkDerivation (finalAttrs: {
     libexsid
     libgcrypt
   ];
-
-  checkInputs = [ unittest-cpp ];
 
   enableParallelBuilding = true;
 

commit e1f5ee13f0fe499f223c5bbe67aaa88ad5db3b6a

enchant: restore mistakenly‐dropped `unittest-cpp` dependency
Fixes: fa355f2ab34d3457d6c0894cb778a50571d83dcb

pkgs/development/libraries/enchant/2.x.nix
@@ -10,6 +10,7 @@
   hspell,
   nuspell,
   libvoikko,
+  unittest-cpp,
 
   withHspell ? true,
   withAspell ? true,
@@ -57,6 +58,7 @@ stdenv.mkDerivation rec {
   ];
 
   checkInputs = [
+    unittest-cpp
   ];
 
   # libtool puts these to .la files