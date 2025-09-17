24b86cc45d30764a5a015e8d2598f403a266d2d7
Christoph Jabs <contact@christophjabs.info>
2025-09-17 10:38:36 +0300

    python3Packages.python-sat: 1.8.dev20 -> 1.8.dev24

--- pkgs/development/python-modules/python-sat/default.nix
+++ pkgs/development/python-modules/python-sat/default.nix
@@ -8,22 +8,14 @@
 }:
 buildPythonPackage rec {
   pname = "python-sat";
-  version = "1.8.dev20";
+  version = "1.8.dev24";
   format = "setuptools";
 
   src = fetchPypi {
     inherit version;
     pname = "python_sat";
-    hash = "sha256-8uUi6DtPVh/87EWSgTtGq7UhAs+Glw8KARPkK3ukeMg=";
+    hash = "sha256-f9NnaPcHdNNInWTvpkg91ieaYejJ29kAAOLcbnbDmM0=";
   };
-
-  # Fix hard-coded g++ reference for darwin, where stdenv uses clang
-  # FIXME: remove once https://github.com/pysathq/pysat/pull/204 is merged and
-  # has hit PyPI
-  postPatch = ''
-    substituteInPlace solvers/patches/glucose421.patch \
-      --replace-fail "+CXX      := g++" "+CXX      := c++"
-  '';
 
   preBuild = ''
     export MAKEFLAGS="-j$NIX_BUILD_CORES"
@@ -48,7 +40,7 @@ buildPythonPackage rec {
   # imported, which has `pysat.examples` correctly installed.
   # See https://github.com/NixOS/nixpkgs/issues/255262
   preCheck = ''
-    cd $out
+    rm -r pysat
   '';
 
   meta = with lib; {
