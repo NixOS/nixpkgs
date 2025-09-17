fb5933d2c1d738240237ea80ca5ae8afd1844faf
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-17 02:44:11 +0000

    python3Packages.aiohomematic: 2025.8.10 -> 2025.9.3

--- pkgs/development/python-modules/aiohomematic/default.nix
+++ pkgs/development/python-modules/aiohomematic/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "aiohomematic";
-  version = "2025.8.10";
+  version = "2025.9.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "SukramJ";
     repo = "aiohomematic";
     tag = version;
-    hash = "sha256-ii+fvjn54IT376YzwljPz2tkx2bv8jojI4rNracCgwA=";
+    hash = "sha256-q4IzFi2ygePikj3XsDmiNKIFk8pzgY6FktHLPeV5XSc=";
   };
 
   postPatch = ''
