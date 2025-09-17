b9054b96bea2ccbcd94dc2626968b2b2dda3ae6c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-17 13:36:38 +0200

    treewide: set CI=true for pnpm prune
    In a recent PNPM version `pnpm prune` started creating a confirmation
    prompt which was automatically denied, as there was no TTY.
    Adding CI environment variable will make sure there are no prompts.
    
    (cherry picked from commit b32bb54834d2cafe1d25f871fb37831f8788ee70)

--- pkgs/by-name/je/jellyseerr/package.nix
+++ pkgs/by-name/je/jellyseerr/package.nix
@@ -53,7 +53,7 @@ stdenv.mkDerivation (finalAttrs: {
     runHook preBuild
 
     pnpm build
-    pnpm prune --prod --ignore-scripts
+    CI=true pnpm prune --prod --ignore-scripts
     rm -rf .next/cache
 
     # Clean up broken symlinks left behind by `pnpm prune`
