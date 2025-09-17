d91a615d2561681d74be66e2ae68f00ef4e2503c
kyehn <kyehn@users.noreply.github.com>
2025-09-17 20:37:29 +0800

    python313Packages.supervisor: 4.2.5 -> 4.3.0
    Changelog: https://github.com/Supervisor/supervisor/blob/4.3.0/CHANGES.rst

--- pkgs/development/python-modules/supervisor/default.nix
+++ pkgs/development/python-modules/supervisor/default.nix
@@ -3,36 +3,26 @@
   stdenv,
   buildPythonPackage,
   fetchPypi,
-  fetchpatch,
   mock,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
-buildPythonPackage rec {
+let
   pname = "supervisor";
-  version = "4.2.5";
-  format = "setuptools";
 
-  disabled = pythonOlder "3.7";
+  version = "4.3.0";
+in
+buildPythonPackage {
+  inherit pname version;
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-NHYbrhojxYGSKBpRFfsH+/IsmwEzwIFmvv/HD+0+vBI=";
+    hash = "sha256-SivxSa30KZfhu0S3DEO2EydeyYUsPtrMqGqRZrJ+lF4=";
   };
 
-  patches = [
-    # adapted from 49b74cafb6e72e0e620e321711c1b81a0823be12
-    ./fix-python313-unittest.patch
-    # Fix SubprocessTests.test_getProcessStateDescription on python 3.13
-    (fetchpatch {
-      url = "https://github.com/Supervisor/supervisor/commit/27efcd59b454e4f3a81e5e1b02ab0d8d0ff2f45f.patch";
-      hash = "sha256-9KNcdRJwnZJA00dDy/mAS7RJuBei60YzVhWkeQgmJ8c=";
-    })
-  ];
-
-  propagatedBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   # wants to write to /tmp/foo which is likely already owned by another
   # nixbld user on hydra
@@ -45,11 +35,11 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "supervisor" ];
 
-  meta = with lib; {
+  meta = {
     description = "System for controlling process state under UNIX";
     homepage = "https://supervisord.org/";
     changelog = "https://github.com/Supervisor/supervisor/blob/${version}/CHANGES.rst";
-    license = licenses.free; # http://www.repoze.org/LICENSE.txt
-    maintainers = with maintainers; [ zimbatm ];
+    license = lib.licenses.free; # http://www.repoze.org/LICENSE.txt
+    maintainers = with lib.maintainers; [ zimbatm ];
   };
 }
