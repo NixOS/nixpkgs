8150a7b2e63be3a1ee84f40b012c414ed845ed44
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-09-17 11:03:24 +0200

    vcg: clean

--- pkgs/by-name/vc/vcg/package.nix
+++ pkgs/by-name/vc/vcg/package.nix
@@ -1,19 +1,22 @@
 {
   lib,
+
   stdenv,
   fetchFromGitHub,
+
+  # propagatedBuildInputs
   eigen,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "vcg";
   version = "2025.07";
 
   src = fetchFromGitHub {
     owner = "cnr-isti-vclab";
     repo = "vcglib";
-    rev = version;
-    sha256 = "sha256-OZnqFnHGXC9fS7JCLTiHNCeA//JBAZGLB5SP/rGzaA8=";
+    tag = finalAttrs.version;
+    hash = "sha256-OZnqFnHGXC9fS7JCLTiHNCeA//JBAZGLB5SP/rGzaA8=";
   };
 
   propagatedBuildInputs = [ eigen ];
@@ -24,11 +27,11 @@ stdenv.mkDerivation rec {
     find $out -name \*.h -exec sed -i 's,<eigenlib/,<eigen3/,g' {} \;
   '';
 
-  meta = with lib; {
+  meta = {
     homepage = "https://vcg.isti.cnr.it/vcglib/install.html";
     description = "C++ library for manipulation, processing and displaying with OpenGL of triangle and tetrahedral meshes";
-    license = licenses.gpl3;
-    platforms = platforms.all;
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.all;
     maintainers = [ lib.maintainers.nim65s ];
   };
-}
+})
