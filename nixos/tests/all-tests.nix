101422ad186fa7d20c5424428a87c42dd46464c3
euxane <euxane.trangirard@euxane.eu>
2025-09-19 00:21:50 +0200

    tests/otterwiki: add test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1176,6 +1176,7 @@ in
   os-prober = handleTestOn [ "x86_64-linux" ] ./os-prober.nix { };
   osquery = handleTestOn [ "x86_64-linux" ] ./osquery.nix { };
   osrm-backend = runTest ./osrm-backend.nix;
+  otterwiki = runTest ./otterwiki.nix;
   outline = runTest ./outline.nix;
   overlayfs = runTest ./overlayfs.nix;
   overseerr = runTest ./overseerr.nix;
