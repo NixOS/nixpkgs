d086eee1fc083fbf04b5dc236b5808e4a42666f0
K900 <me@0upti.me>
2025-09-18 23:14:37 +0300

    qt6.qttools: clean up

--- pkgs/development/libraries/qt-6/modules/qttools/default.nix
+++ pkgs/development/libraries/qt-6/modules/qttools/default.nix
@@ -13,26 +13,11 @@
 
 qtModule {
   pname = "qttools";
-  # FIXME: update to LLVM 21 with Qt 6.10
-  buildInputs = lib.optionals withClang [
-    llvmPackages_20.libclang
-    llvmPackages_20.llvm
-  ];
-  propagatedBuildInputs = [
-    qtbase
-    qtdeclarative
-  ]
-  ++ lib.optionals stdenv.hostPlatform.isDarwin [ cups ];
-  cmakeFlags = lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
-    "-DQt6LinguistTools_DIR=${pkgsBuildBuild.qt6.qttools}/lib/cmake/Qt6LinguistTools"
-    "-DQt6ToolsTools_DIR=${pkgsBuildBuild.qt6.qttools}/lib/cmake/Qt6ToolsTools"
-  ];
+
   patches = [
     ./paths.patch
   ];
-  env.NIX_CFLAGS_COMPILE = toString [
-    "-DNIX_OUTPUT_OUT=\"${placeholder "out"}\""
-  ];
+
   postPatch = ''
     substituteInPlace \
       src/qdoc/catch/CMakeLists.txt \
@@ -40,6 +25,32 @@ qtModule {
       src/qdoc/catch_conversions/CMakeLists.txt \
       --replace ''\'''${CMAKE_INSTALL_INCLUDEDIR}' "$out/include"
   '';
+
+  env.NIX_CFLAGS_COMPILE = toString [
+    "-DNIX_OUTPUT_OUT=\"${placeholder "out"}\""
+  ];
+
+  # FIXME: update to LLVM 21 with Qt 6.10
+  buildInputs = lib.optionals withClang [
+    llvmPackages_20.libclang
+    llvmPackages_20.llvm
+  ];
+
+  propagatedBuildInputs = [
+    qtbase
+    qtdeclarative
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [ cups ];
+
+  cmakeFlags =
+    lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+      "-DQt6LinguistTools_DIR=${pkgsBuildBuild.qt6.qttools}/lib/cmake/Qt6LinguistTools"
+      "-DQt6ToolsTools_DIR=${pkgsBuildBuild.qt6.qttools}/lib/cmake/Qt6ToolsTools"
+    ]
+    ++ lib.optionals withClang [
+      "-DFEATURE_clang=ON"
+    ];
+
   postInstall = ''
     mkdir -p "$dev"
     ln -s "$out/bin" "$dev/bin"
