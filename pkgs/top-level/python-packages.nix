806139d05daeeb55c1059642a1afb5eb50dfa23e
Jon Seager <jon@sgrs.uk>
2025-09-18 11:26:22 +0100

    python3Packages.pydantic-yaml: remove package

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12801,8 +12801,6 @@ self: super: with self; {
 
   pydantic-settings = callPackage ../development/python-modules/pydantic-settings { };
 
-  pydantic-yaml = callPackage ../development/python-modules/pydantic-yaml { };
-
   pydantic_1 = callPackage ../development/python-modules/pydantic/1.nix { };
 
   pydash = callPackage ../development/python-modules/pydash { };
