db369e6a3a2c322849d6dc39bb0782f0509d6e60
Vitor Pavan <vipavani@hotmail.com>
2025-09-18 15:08:21 +0200

    refactor: clean up formatting and indentation in freelens package definition

--- pkgs/by-name/fr/freelens/package.nix
+++ pkgs/by-name/fr/freelens/package.nix
@@ -3,7 +3,8 @@
   fetchurl,
   makeWrapper,
   lib,
-}: let
+}:
+let
   pname = "freelens";
   version = "1.5.3";
 
@@ -12,29 +13,28 @@
     sha256 = "sha256-I6jmMGCkkdZPJoLNGfWhUc5SAjNcRzPJsVckxZ6eeng=";
   };
 
-  appimageContents = appimageTools.extractType2 { inherit  pname version src; };
-    
+  appimageContents = appimageTools.extractType2 { inherit pname version src; };
+
 in
-  appimageTools.wrapType2 {
-    inherit pname version src;
+appimageTools.wrapType2 {
+  inherit pname version src;
 
-    nativeBuildInputs = [makeWrapper];
+  nativeBuildInputs = [ makeWrapper ];
 
-    extraInstallCommands = ''
-      install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-      install -m 444 -D ${appimageContents}/${pname}.png $out/share/icons/hicolor/256x256/apps/${pname}.png
-      substituteInPlace $out/share/applications/${pname}.desktop --replace-fail 'Exec=AppRun' 'Exec=${pname}'
-      wrapProgram $out/bin/${pname} --set XDG_CURRENT_DESKTOP GNOME
-    '';
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
+    install -m 444 -D ${appimageContents}/${pname}.png $out/share/icons/hicolor/256x256/apps/${pname}.png
+    substituteInPlace $out/share/applications/${pname}.desktop --replace-fail 'Exec=AppRun' 'Exec=${pname}'
+    wrapProgram $out/bin/${pname} --set XDG_CURRENT_DESKTOP GNOME
+  '';
 
-    meta = {
-      description = "Freelens is a free and open-source user interface designed for managing Kubernetes clusters.";
-      homepage = "https://freelensapp.github.io/";
-      changelog = "https://github.com/freelensapp/freelens/releases/tag/v${version}";
-      license = lib.licenses.mit;
-      mainProgram = "freelens";
-      maintainers = with lib.maintainers; [ vitorpavani ];
-      platforms = [ "x86_64-linux" ];
+  meta = {
+    description = "Freelens is a free and open-source user interface designed for managing Kubernetes clusters.";
+    homepage = "https://freelensapp.github.io/";
+    changelog = "https://github.com/freelensapp/freelens/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    mainProgram = "freelens";
+    platforms = [ "x86_64-linux" ];
 
-    };
-  }
+  };
+}
