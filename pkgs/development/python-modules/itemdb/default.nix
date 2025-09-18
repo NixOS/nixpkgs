NixOS/nixpkgs

commit 73a019ca03c4f122209b44a50960be397cbe0a60

python3Packages.itemdb: 1.2.0 -> 1.3.0
This commit was automatically generated using update-python-libraries.

pkgs/development/python-modules/itemdb/default.nix
@@ -6,7 +6,7 @@
 
 buildPythonPackage rec {
   pname = "itemdb";
-  version = "1.2.0";
+  version = "1.3.0";
   format = "setuptools";
 
   # PyPI tarball doesn't include tests directory
@@ -14,7 +14,7 @@ buildPythonPackage rec {
     owner = "almarklein";
     repo = "itemdb";
     tag = "v${version}";
-    sha256 = "sha256-egxQ1tGC6R5p1stYm4r05+b2HkuT+nBySTZPGqeAbSE=";
+    sha256 = "sha256-HXdOERq2td6CME8zWN0DRVkSlmdqTg2po7aJrOuITHE=";
   };
 
   meta = with lib; {

commit f27a33e3f0f4e2647658a934745fb9244501f8ed

python3Packages.itemdb: fix build
During the update to 1.3.0 it was missed that the author of the package adopted a pyproject.toml
instead of a setup.py; as well as introducing a dependency on flit-core

Fixes: 73a019ca03c4f122209b44a50960be397cbe0a60

pkgs/development/python-modules/itemdb/default.nix
@@ -2,12 +2,17 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  flit-core,
 }:
 
 buildPythonPackage rec {
   pname = "itemdb";
   version = "1.3.0";
-  format = "setuptools";
+  format = "pyproject";
+
+  nativeBuildInputs = [
+    flit-core
+  ];
 
   # PyPI tarball doesn't include tests directory
   src = fetchFromGitHub {