6d9b5300d34c8ce2b646172b0a04060adb638a24
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-18 12:51:50 +0000

    kdePackages.plasma-wayland-protocols: 1.18.0 -> 1.19.0

--- pkgs/kde/misc/plasma-wayland-protocols/default.nix
+++ pkgs/kde/misc/plasma-wayland-protocols/default.nix
@@ -5,11 +5,11 @@
 }:
 mkKdeDerivation rec {
   pname = "plasma-wayland-protocols";
-  version = "1.18.0";
+  version = "1.19.0";
 
   src = fetchurl {
     url = "mirror://kde/stable/plasma-wayland-protocols/plasma-wayland-protocols-${version}.tar.xz";
-    hash = "sha256-JWdHJnGtXZifiLUbrvndWTU6Xnw/LtfmuYl1XLkAQjM=";
+    hash = "sha256-RWef56Y8QU8sgXk6YlKPrmzO5YS2llcZ1/n8bdSLqEY=";
   };
 
   meta.license = with lib.licenses; [
