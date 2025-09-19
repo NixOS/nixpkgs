039040ad2a737d47afb63ea0d95c0a17decd7c85
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-19 23:15:06 +0000

    mattermostLatest: 10.12.0 -> 10.11.3

--- pkgs/by-name/ma/mattermostLatest/package.nix
+++ pkgs/by-name/ma/mattermostLatest/package.nix
@@ -11,10 +11,10 @@ mattermost.override {
     # and make sure the version regex is up to date here.
     # Ensure you also check ../mattermost/package.nix for ESR releases.
     regex = "^v(10\\.[0-9]+\\.[0-9]+)$";
-    version = "10.12.0";
-    srcHash = "sha256-oVZlXprw0NddHrtx1g2WRoGm1ATq/pqncD0mewN12nw=";
+    version = "10.11.3";
+    srcHash = "sha256-5iabZrmcMriAqLnYaoqzNZsggupFfi3tUbWhvKqYYxQ=";
     vendorHash = "sha256-Lqa463LLy41aaRbrtJFclfOj55vLjK4pWFAFLzX3TJE=";
-    npmDepsHash = "sha256-O9iX6hnwkEHK0kkHqWD6RYXqoSEW6zs+utiYHnt54JY=";
+    npmDepsHash = "sha256-p9dq31qw0EZDQIl2ysKE38JgDyLA6XvSv+VtHuRh+8A=";
     lockfileOverlay = ''
       unlock(.; "@floating-ui/react"; "channels/node_modules/@floating-ui/react")
     '';
