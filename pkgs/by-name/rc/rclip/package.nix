5131b595a629ec9c0f61801c6c770ffce101779a
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-19 03:55:04 +0000

    rclip: 2.0.7 -> 2.0.8

--- pkgs/by-name/rc/rclip/package.nix
+++ pkgs/by-name/rc/rclip/package.nix
@@ -6,14 +6,14 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "rclip";
-  version = "2.0.7";
+  version = "2.0.8";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "yurijmikhalevich";
     repo = "rclip";
     tag = "v${version}";
-    hash = "sha256-OiAOK6i088TMqD2+PzdSs7UBsycNHa+nqWima0dPYIA=";
+    hash = "sha256-ScNyy5qWDskKgqxjfRU7y8WBCdThXTjlE3x0oIa8fhU=";
   };
 
   build-system = with python3Packages; [
