27adc3e91e925d97fbc9b5822e0fdabc6d6a3555
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-09-19 10:18:55 -0700

    swiftPackages.sourcekit-lsp: drop tools-support-core patch (upstreamed)

--- pkgs/development/compilers/swift/sourcekit-lsp/default.nix
+++ pkgs/development/compilers/swift/sourcekit-lsp/default.nix
@@ -51,15 +51,6 @@ stdenv.mkDerivation {
       }
     }
 
-    swiftpmMakeMutable swift-tools-support-core
-    patch -p1 -d .build/checkouts/swift-tools-support-core -i ${
-      fetchpatch {
-        url = "https://github.com/apple/swift-tools-support-core/commit/990afca47e75cce136d2f59e464577e68a164035.patch";
-        hash = "sha256-PLzWsp+syiUBHhEFS8+WyUcSae5p0Lhk7SSRdNvfouE=";
-        includes = [ "Sources/TSCBasic/FileSystem.swift" ];
-      }
-    }
-
     # This toggles a section specific to Xcode XCTest, which doesn't work on
     # Darwin, where we also use swift-corelibs-xctest.
     substituteInPlace Sources/LSPTestSupport/PerfTestCase.swift \
