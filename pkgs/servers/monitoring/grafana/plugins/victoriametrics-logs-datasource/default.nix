c11d08d2faa9e287f6f6b7162cf87e0ec96b6cf2
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-19 07:35:53 +0000

    grafanaPlugins.victoriametrics-logs-datasource: 0.19.3 -> 0.20.0

--- pkgs/servers/monitoring/grafana/plugins/victoriametrics-logs-datasource/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/victoriametrics-logs-datasource/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "victoriametrics-logs-datasource";
-  version = "0.19.3";
-  zipHash = "sha256-UMHcH4o6/Wr7Wct977HdOiXgvXo0j9LfZxPcRCqG2+U=";
+  version = "0.20.0";
+  zipHash = "sha256-SJlknSj5Lff444cZ8Q/qX4lL/+QsYsStZUiQ3saapTI=";
   meta = {
     description = "Grafana datasource for VictoriaLogs";
     license = lib.licenses.asl20;
