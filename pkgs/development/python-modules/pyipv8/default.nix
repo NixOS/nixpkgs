9ad735324bf9a3cc835a76cdf846afb41c902df0
Michael Daniels <mdaniels5757@gmail.com>
2025-09-20 18:28:42 -0400

    python3Packages.pyipv8: 3.0.2197-unstable-2025-07-29 -> 3.1
    Diff: https://github.com/Tribler/py-ipv8/compare/db39b85f4c28880dee24d1b59d8eae8ca8b9c03d...3.1
    
    Release notes: https://github.com/Tribler/py-ipv8/releases/tag/3.1

--- pkgs/development/python-modules/pyipv8/default.nix
+++ pkgs/development/python-modules/pyipv8/default.nix
@@ -15,16 +15,16 @@
   apispec,
 }:
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "pyipv8";
-  version = "3.0.2197-unstable-2025-07-29";
+  version = "3.1";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "tribler";
     repo = "py-ipv8";
-    rev = "db39b85f4c28880dee24d1b59d8eae8ca8b9c03d";
-    hash = "sha256-VIcBPzpK8Cdaz/dRp9QK/MtK41jm8rs/pxnLS716FNM=";
+    tag = version;
+    hash = "sha256-HamjKVuBPSicoP/GldO5kg2Eay50ti03wzeNaPAl0qI=";
   };
 
   propagatedBuildInputs = [
