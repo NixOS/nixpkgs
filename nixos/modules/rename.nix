d8cf218e393bbe6ccd013f9609ec19c001a8f680
dish <pyrox@pyrox.dev>
2025-09-20 12:49:21 -0400

    amdvlk,nixos/amdvlk: drop
    AMDVLK has been deprecated by upstream and has been replaced by RADV.
    Drop it for 25.11.

--- nixos/modules/rename.nix
+++ nixos/modules/rename.nix
@@ -34,6 +34,11 @@ in
       "fontconfig"
       "penultimate"
     ] "The corresponding package has removed from nixpkgs.")
+    (mkRemovedOptionModule [
+      "hardware"
+      "amdgpu"
+      "amdvlk"
+    ] "'amdvlk' has been removed. RADV, part of Mesa, is now officially supported by AMD.")
     (mkRemovedOptionModule [ "hardware" "brightnessctl" ] ''
       The brightnessctl module was removed because newer versions of
       brightnessctl don't require the udev rules anymore (they can use the
