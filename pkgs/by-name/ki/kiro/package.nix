87d97ebaa9d2868e74541d68432a6bf34ef642e9
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-20 18:37:46 +0000

    kiro: 0.2.38 -> 0.2.59

--- pkgs/by-name/ki/kiro/package.nix
+++ pkgs/by-name/ki/kiro/package.nix
@@ -15,7 +15,7 @@ in
   inherit useVSCodeRipgrep;
   commandLineArgs = extraCommandLineArgs;
 
-  version = "0.2.38";
+  version = "0.2.59";
   pname = "kiro";
 
   # You can find the current VSCode version in the About dialog:
