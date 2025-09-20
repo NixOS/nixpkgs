01a494aa049f3acd1df6c9cfcce0e3f1c819e2c7
cdaringe <christopher.dieringer@gmail.com>
2025-09-20 16:31:07 -0700

    fix: hash

--- pkgs/by-name/sm/sm64coopdx/package.nix
+++ pkgs/by-name/sm/sm64coopdx/package.nix
@@ -43,7 +43,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "coop-deluxe";
     repo = "sm64coopdx";
     rev = "v1.3.2"; # it seems coopdx has taken on some stylistic versioning...
-    hash = "sha256:37cbb0c7658a3129d14709881e21243348fe225bfba060e8bcdb7222b4b0496a";
+    hash = "sha256-FHH3+pGowkT8asDmU9qxPNDKy4VPKlkA0X7e4gnX9KY=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
@@ -116,7 +116,7 @@ stdenv.mkDerivation (finalAttrs: {
     maintainers = [ lib.maintainers.shelvacu ];
     mainProgram = "sm64coopdx";
     homepage = "https://sm64coopdx.com/";
-    changelog = "https://github.com/coop-deluxe/sm64coopdx/releases/tag/v1.3";
+    changelog = "https://github.com/coop-deluxe/sm64coopdx/releases/tag/v1.3.2";
     sourceProvenance = with lib.sourceTypes; [
       fromSource
       # The lua engine, discord sdk, and coopnet library are vendored pre-built. See https://github.com/coop-deluxe/sm64coopdx/tree/v1.0.3/lib
