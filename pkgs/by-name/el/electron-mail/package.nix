6694aafa16e20ac9c5bd70fc2abb67b2f45cfea9
Ludovico Piero <lewdovico@gnuweeb.org>
2025-09-20 20:41:43 +0900

    electron-mail: 5.3.0 -> 5.3.3
    Changelog: https://github.com/vladimiry/ElectronMail/releases/tag/v5.3.3
    Diff: https://github.com/vladimiry/ElectronMail/compare/v5.3.0...v5.3.3
    
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pkgs/by-name/el/electron-mail/package.nix
+++ pkgs/by-name/el/electron-mail/package.nix
@@ -7,11 +7,11 @@
 
 let
   pname = "electron-mail";
-  version = "5.3.0";
+  version = "5.3.3";
 
   src = fetchurl {
     url = "https://github.com/vladimiry/ElectronMail/releases/download/v${version}/electron-mail-${version}-linux-x86_64.AppImage";
-    hash = "sha256-QGYsD8Ec6/G4X2dGZfH7LwT6o6X599kP6V34y6WxP64=";
+    hash = "sha256-i1oJ/DNGspE7ELuN7MI0e8/69SZwirqahBa7Jf5kP7s=";
   };
 
   appimageContents = appimageTools.extract { inherit pname version src; };
