c017fa968d5d83c443e172c9dbfe3b3cf8dcff51
Grimmauld <Grimmauld@grimmauld.de>
2025-09-20 13:03:27 +0200

    libxml2: 2.14.6 -> 2.15.0
    Rel notes: https://gitlab.gnome.org/GNOME/libxml2/-/releases/v2.15.0
    
    closes https://github.com/NixOS/nixpkgs/issues/444172

--- pkgs/development/libraries/libxml2/default.nix
+++ pkgs/development/libraries/libxml2/default.nix
@@ -34,6 +34,13 @@ let
         # same as upstream, fixed conflicts
         # https://gitlab.gnome.org/GNOME/libxml2/-/commit/c340e419505cf4bf1d9ed7019a87cc00ec200434
         ./CVE-2025-6170.patch
+
+        # Unmerged ABI-breaking patch required to fix the following security issues:
+        # - https://gitlab.gnome.org/GNOME/libxslt/-/issues/139
+        # - https://gitlab.gnome.org/GNOME/libxslt/-/issues/140
+        # See also https://gitlab.gnome.org/GNOME/libxml2/-/issues/906
+        # Source: https://github.com/chromium/chromium/blob/4fb4ae8ce3daa399c3d8ca67f2dfb9deffcc7007/third_party/libxml/chromium/xml-attr-extra.patch
+        ./xml-attr-extra.patch
       ];
       freezeUpdateScript = true;
       extraMeta = {
@@ -43,13 +50,13 @@ let
       };
     };
     libxml2 = callPackage ./common.nix {
-      version = "2.14.6";
+      version = "2.15.0";
       src = fetchFromGitLab {
         domain = "gitlab.gnome.org";
         owner = "GNOME";
         repo = "libxml2";
         tag = "v${packages.libxml2.version}";
-        hash = "sha256-EIcNL5B/o74hyc1N+ShrlKsPL5tHhiGgkCR1D7FcDjw=";
+        hash = "sha256-jumHSiIMDzqG2hvPUdcBP8LsszcU+loOY+vqEh/0Yqo=";
       };
       extraMeta = {
         maintainers = with lib.maintainers; [
