NixOS/nixpkgs

commit 9e4b2ce5c7c2a086eb3183f3523a89f131cddb6d

unixODBCDrivers.mariadb: 3.1.20 -> 3.2.6
Diff: https://github.com/mariadb-corporation/mariadb-connector-odbc/compare/3.1.20...3.2.6

pkgs/development/libraries/unixODBCDrivers/default.nix
@@ -42,13 +42,13 @@
 
   mariadb = stdenv.mkDerivation rec {
     pname = "mariadb-connector-odbc";
-    version = "3.1.20";
+    version = "3.2.6";
 
     src = fetchFromGitHub {
       owner = "mariadb-corporation";
       repo = "mariadb-connector-odbc";
       rev = version;
-      hash = "sha256-l+HlS7/A0shwsEXYKDhi+QCmwHaMTeKrtcvo9yYpYws=";
+      hash = "sha256-FdnA3/xDxnk2910LCMPWQTcUUSYfUsnnZ3Hqj0uey5s=";
       # this driver only seems to build correctly when built against the mariadb-connect-c subrepo
       # (see https://github.com/NixOS/nixpkgs/issues/73258)
       fetchSubmodules = true;

commit e2b11cbf5a3acb5e5dfaec95bb0c41b4ad160763

unixODBCDrivers.mariadb: fix build for musl
Fixes: 9e4b2ce5c7c2 ("unixODBCDrivers.mariadb: 3.1.20 -> 3.2.6")

pkgs/development/libraries/unixODBCDrivers/default.nix
@@ -57,6 +57,8 @@
     patches = [
       # Fix `call to undeclared function 'sleep'` with clang 16
       ./mariadb-connector-odbc-unistd.patch
+
+      ./mariadb-connector-odbc-musl.patch
     ];
 
     nativeBuildInputs = [ cmake ];

pkgs/development/libraries/unixODBCDrivers/mariadb-connector-odbc-musl.patch
@@ -1,0 +1,41 @@
+From fe6e6412ac0fb155843585647c045a1fba2ee3f2 Mon Sep 17 00:00:00 2001
+From: Alyssa Ross <hi@alyssa.is>
+Date: Sat, 20 Sep 2025 14:11:13 +0200
+Subject: [PATCH] Add missing <cstdint> includes
+
+These files use types from <cstdint> without including it.  Without
+the includes, the build fails for musl.
+---
+Link: https://github.com/mariadb-corporation/mariadb-connector-odbc/pull/65
+
+ driver/interface/Exception.h | 1 +
+ driver/template/CArray.h     | 1 +
+ 2 files changed, 2 insertions(+)
+
+diff --git a/driver/interface/Exception.h b/driver/interface/Exception.h
+index 1b2eb847..82f06273 100644
+--- a/driver/interface/Exception.h
++++ b/driver/interface/Exception.h
+@@ -21,6 +21,7 @@
+ #ifndef _EXCEPTION_H_
+ #define _EXCEPTION_H_
+ 
++#include <cstdint>
+ #include <stdexcept>
+ #include "class/SQLString.h"
+ 
+diff --git a/driver/template/CArray.h b/driver/template/CArray.h
+index 2c4be514..bd0e9912 100644
+--- a/driver/template/CArray.h
++++ b/driver/template/CArray.h
+@@ -24,6 +24,7 @@
+ #include <initializer_list>
+ #include <vector>
+ #include <stdexcept>
++#include <cstdint>
+ #include <cstring>
+ #include <string>
+ 
+-- 
+2.51.0
+