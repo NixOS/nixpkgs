fd45b6078f249370ad1f8113d5f8037d1cb69c1d
Fabian Affolter <fabian@affolter-engineering.ch>
2025-09-20 10:11:03 +0200

    pip-audit: modernize

--- pkgs/by-name/pi/pip-audit/package.nix
+++ pkgs/by-name/pi/pip-audit/package.nix
@@ -7,7 +7,7 @@
 python3.pkgs.buildPythonApplication rec {
   pname = "pip-audit";
   version = "2.9.0";
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "trailofbits";
