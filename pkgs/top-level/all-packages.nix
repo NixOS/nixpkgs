1a71e99e255ce6cdc7d5c36a38a597736d877aca
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-09-20 19:53:43 -0400

    idris2: Move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5234,8 +5234,6 @@ with pkgs;
 
   idris2Packages = recurseIntoAttrs (callPackage ../development/compilers/idris2 { });
 
-  inherit (idris2Packages) idris2;
-
   inherit (callPackage ../development/tools/database/indradb { })
     indradb-server
     indradb-client
