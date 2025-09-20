3ff4483aa309ce85fd1809abfc7f38e611bc2b92
Tristan Ross <tristan.ross@determinate.systems>
2025-09-20 10:39:28 -0700

    pythonPackages.tt-smi: init at 3.0.30

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18768,6 +18768,8 @@ self: super: with self; {
 
   tt-flash = callPackage ../development/python-modules/tt-flash { };
 
+  tt-smi = callPackage ../development/python-modules/tt-smi { };
+
   tt-tools-common = callPackage ../development/python-modules/tt-tools-common { };
 
   ttach = callPackage ../development/python-modules/ttach { };
