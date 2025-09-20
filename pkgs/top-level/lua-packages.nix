2216547a4664c0478db61f0b7f55ccd1e9e4cc8a
Alex Epelde <alex@epelde.net>
2025-09-20 17:59:02 -0400

    luaPackages.lua-resty-core: 0.1.28 -> 0.1.31

--- pkgs/top-level/lua-packages.nix
+++ pkgs/top-level/lua-packages.nix
@@ -160,13 +160,13 @@ rec {
     { fetchFromGitHub }:
     buildLuaPackage rec {
       pname = "lua-resty-core";
-      version = "0.1.28";
+      version = "0.1.31";
 
       src = fetchFromGitHub {
         owner = "openresty";
         repo = "lua-resty-core";
         rev = "v${version}";
-        sha256 = "sha256-RJ2wcHTu447wM0h1fa2qCBl4/p9XL6ZqX9pktRW64RI=";
+        sha256 = "sha256-WUiBFJ8L8NzSGoEwTAw/iHAzPqJqaOUSFyqGeEf+f94==";
       };
 
       propagatedBuildInputs = [ lua-resty-lrucache ];
