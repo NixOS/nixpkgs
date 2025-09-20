d462677dc758ec8a23d6c8e70b6d756f598fe13a
Grimmauld <Grimmauld@grimmauld.de>
2025-09-20 13:04:57 +0200

    libxslt: remove broken patch (was fixed in libxml)

--- pkgs/development/libraries/libxslt/default.nix
+++ pkgs/development/libraries/libxslt/default.nix
@@ -39,16 +39,6 @@ stdenv.mkDerivation (finalAttrs: {
     # https://gitlab.gnome.org/GNOME/libxslt/-/issues/144
     # Source: https://gitlab.gnome.org/GNOME/libxslt/-/merge_requests/77
     ./77-Use-a-dedicated-node-type-to-maintain-the-list-of-cached-rv-ts.patch
-
-    # Fix type confusion in xmlNode.psvi between stylesheet and source nodes
-    # https://gitlab.gnome.org/GNOME/libxslt/-/issues/139
-    # Fix heap-use-after-free in xmlFreeID caused by `atype` corruption
-    # https://gitlab.gnome.org/GNOME/libxslt/-/issues/140
-    #
-    # Depends on unmerged libxml2 patch that breaks ABI.
-    #
-    # Source: https://github.com/chromium/chromium/blob/4fb4ae8ce3daa399c3d8ca67f2dfb9deffcc7007/third_party/libxslt/chromium/new-unified-atype-extra.patch
-    ./new-unified-atype-extra.patch
   ];
 
   strictDeps = true;
