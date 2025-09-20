175e04b6181b820f7f06b765039f24973991770f
Lalit Yalamanchili <lalit.yalamanchili@gmail.com>
2025-09-20 16:19:47 +0530

    helium: refactor according to @acid-bongs changes

--- pkgs/by-name/he/helium/package.nix
+++ pkgs/by-name/he/helium/package.nix
@@ -3,35 +3,30 @@
   stdenvNoCC,
   fetchurl,
   undmg,
-  ...
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "helium";
   version = "0.4.6.1";
 
   src =
+    let
+      inherit (finalAttrs) version;
+    in
     {
       aarch64-darwin = fetchurl {
-        name = "helium_0.4.6.1_arm64-macos.dmg";
-        url = "https://github.com/imputnet/helium-macos/releases/download/0.4.6.1/helium_${finalAttrs.version}_arm64-macos.dmg";
+        name = "helium_${version}_arm64-macos.dmg";
+        url = "https://github.com/imputnet/helium-macos/releases/download/${version}/helium_${version}_arm64-macos.dmg";
         hash = "sha256-K9fnxc5sDAiDj008IzwLI4BBI5mrfz3FpuECF2s+5pw=";
       };
       x86_64-darwin = fetchurl {
-        name = "helium_0.4.6.1_x86_64-macos.dmg";
-        url = "https://github.com/imputnet/helium-macos/releases/download/0.4.6.1/helium_${finalAttrs.version}_x86_64-macos.dmg";
+        name = "helium_${version}_x86_64-macos.dmg";
+        url = "https://github.com/imputnet/helium-macos/releases/download/${version}/helium_${version}_x86_64-macos.dmg";
         hash = "sha256-VjhIMQBnagSQArmsF77bO2PwYZWzGOagBL134bUW6e4=";
       };
     }
     .${stdenvNoCC.system} or (throw "helium: ${stdenvNoCC.system} is unsupported.");
 
-  dontPatch = true;
-  dontConfigure = true;
-  dontBuild = true;
-  dontFixup = true;
-
   nativeBuildInputs = [ undmg ];
-
-  sourceRoot = "Helium.app";
 
   installPhase = ''
     runHook preInstall
@@ -45,13 +40,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   meta = {
     description = "Private, fast, and honest web browser based on Chromium";
     homepage = "https://helium.computer/";
-    license = lib.licenses.gpl3;
+    mainProgram = "Helium.app";
+    license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [
       lalit64
     ];
-    platforms = [
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
+    platforms = lib.platforms.darwin;
   };
 })
