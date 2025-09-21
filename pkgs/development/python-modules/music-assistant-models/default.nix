7a3a43c66f48d24d6b22f8adc1907e159de182e6
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-21 21:37:50 +0000

    python3Packages.music-assistant-models: 1.1.51 -> 1.1.58

--- pkgs/development/python-modules/music-assistant-models/default.nix
+++ pkgs/development/python-modules/music-assistant-models/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "music-assistant-models";
-  version = "1.1.51"; # Must be compatible with music-assistant-client package
+  version = "1.1.58"; # Must be compatible with music-assistant-client package
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "music-assistant";
     repo = "models";
     tag = version;
-    hash = "sha256-vx0GlW99qDpSRDihP0dYhwgksPdCNj82SATo5TpITa4=";
+    hash = "sha256-3/8doiELk8wUpkAF0TfgS0C1Hz79cziETSC4FKioG0g=";
   };
 
   postPatch = ''
