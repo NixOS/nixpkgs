244a25c4de8b6fd8c09a137bc8bb4bf9259e55c2
Fabian Affolter <fabian@affolter-engineering.ch>
2025-09-21 09:03:17 +0200

    python312Packages.mypy-boto3-license-manager-user-subscriptions: 1.40.0 -> 1.40.35

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -797,8 +797,8 @@ rec {
       "sha256-pfIQS9as02Gm4mHUc3q3gMKRHz+wT/lRKHWUc2ugt1s=";
 
   mypy-boto3-license-manager-user-subscriptions =
-    buildMypyBoto3Package "license-manager-user-subscriptions" "1.40.0"
-      "sha256-Skch80b9xDgcLwq2CGrS8xEhKZPEMJGT6qB9f0Z5XwY=";
+    buildMypyBoto3Package "license-manager-user-subscriptions" "1.40.35"
+      "sha256-SvXSGmSDMCmseJkD7QYE0SgeTO/AWKK/ROT32KP1RsE=";
 
   mypy-boto3-lightsail =
     buildMypyBoto3Package "lightsail" "1.40.1"
