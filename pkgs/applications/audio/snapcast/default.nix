ff6d8124861129cd67f13f8d7e63cf8b18c97eec
apfelkuchen06 <apfelkuchen@hrnz.li>
2025-09-21 14:30:19 +0200

    snapcast: reenable pulse support
    Since badaix/snapcast@70858352a2e2bf23022524900fdca22212bbf2b5
    snapclient is only built with pulse support when explicitly requested,
    so let's do that.

--- pkgs/applications/audio/snapcast/default.nix
+++ pkgs/applications/audio/snapcast/default.nix
@@ -58,6 +58,8 @@ stdenv.mkDerivation rec {
 
   TARGET = lib.optionalString stdenv.hostPlatform.isDarwin "MACOS";
 
+  cmakeFlags = [ (lib.cmakeBool "BUILD_WITH_PULSE" pulseaudioSupport) ];
+
   # Upstream systemd unit files are pretty awful, so we provide our own in a
   # NixOS module. It might make sense to get that upstreamed...
   postInstall = ''
