d3d7f23e6ced85a0a75f35d897918aaecc8b740a
Gaetan Lepage <gaetan@glepage.com>
2025-09-21 09:17:57 +0000

    python3Packages.coffea: 2025.7.3 -> 2025.9.0
    Diff: https://github.com/CoffeaTeam/coffea/compare/v2025.7.3...v2025.9.0
    
    Changelog: https://github.com/CoffeaTeam/coffea/releases/tag/v2025.9.0

--- pkgs/development/python-modules/coffea/default.nix
+++ pkgs/development/python-modules/coffea/default.nix
@@ -41,14 +41,14 @@
 
 buildPythonPackage rec {
   pname = "coffea";
-  version = "2025.7.3";
+  version = "2025.9.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "CoffeaTeam";
     repo = "coffea";
     tag = "v${version}";
-    hash = "sha256-lCrmWcVzu8Ls0a+r2D1DMZ/Ysq3H9bPj13XOmAS1M5I=";
+    hash = "sha256-we1BwqTEjCU/iXWKnb1QI9OE+y58N4IPJjWD/ypg4G4=";
   };
 
   build-system = [
