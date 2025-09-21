a56ff336a78ed98c39755c5c982ce8c9d42839f0
David McFarland <corngood@gmail.com>
2025-09-20 23:41:25 -0300

    cygwin-dll-link.sh: init

--- pkgs/os-specific/cygwin/newlib-cygwin/default.nix
+++ pkgs/os-specific/cygwin/newlib-cygwin/default.nix
@@ -146,6 +146,17 @@
         ++ lib.optional (stdenvNoLibc.hostPlatform != stdenvNoLibc.buildPlatform) [
           "ac_cv_prog_CC=gcc"
         ];
+
+        allowedImpureDLLs = [
+          "ADVAPI32.dll"
+          "PSAPI.DLL"
+          "NETAPI32.dll"
+          "SHELL32.dll"
+          "USER32.dll"
+          "USERENV.dll"
+          "dbghelp.dll"
+          "ntdll.dll"
+        ];
       }
   )
 )
