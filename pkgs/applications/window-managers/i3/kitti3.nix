fc661aa83b1607bdc83c07565caaf959a300807d
Michael Daniels <mdaniels5757@gmail.com>
2025-09-20 21:09:40 -0400

    kitti3: standardize version number, set updateScript
    This is the same version as before, despite the date difference.
    
    This will prevent downgrade PRs.

--- pkgs/applications/window-managers/i3/kitti3.nix
+++ pkgs/applications/window-managers/i3/kitti3.nix
@@ -1,19 +1,20 @@
 {
   buildPythonApplication,
   fetchFromGitHub,
-  poetry-core,
   i3ipc,
   lib,
+  poetry-core,
+  writeScript,
 }:
 
 buildPythonApplication rec {
   pname = "kitti3";
-  version = "unstable-2021-09-11";
-  format = "pyproject";
+  version = "0.5.1-unstable-2021-09-10";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "LandingEllipse";
-    repo = pname;
+    repo = "kitti3";
     rev = "f9f94c8b9f8b61a9d085206ada470cfe755a2a92";
     hash = "sha256-bcIzbDpIe2GKS9EcVqpjwz0IG2ixNMn06OIQpZ7PeH0=";
   };
@@ -31,6 +32,25 @@ buildPythonApplication rec {
   propagatedBuildInputs = [
     i3ipc
   ];
+
+  passthru.updateScript = writeScript "update-${pname}" ''
+    #!/usr/bin/env nix-shell
+    #!nix-shell -i bash -p git common-updater-scripts perl tomlq
+    tmpdir="$(mktemp -d)"
+    git clone --depth=1 "${src.gitRepoUrl}" "$tmpdir"
+    pushd "$tmpdir"
+    newVersionNumber=$(perl -ne 'print if s/version = "([\d.]+)"/$1/' pyproject.toml)
+    newRevision=$(git show -s --pretty='format:%H')
+    newDate=$(git show -s --pretty='format:%cs')
+    newVersion="$newVersionNumber-unstable-$newDate"
+    echo "newVersion = $newVersion" >&2
+    echo "newRevision = $newRevision" >&2
+    popd
+    rm -rf "$tmpdir"
+    update-source-version --rev="$newRevision" "${pname}" "$newVersion"
+    perl -pe 's/^(.*version = ")([\d\.]+)(.*)$/''${1}'"''${newVersion}"'";/' \
+      -i 'pkgs/applications/window-managers/i3/${pname}.nix'
+  '';
 
   meta = with lib; {
     homepage = "https://github.com/LandingEllipse/kitti3";
