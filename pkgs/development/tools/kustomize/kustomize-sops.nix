3749d1efccac49b5f4d31c1f6daefbb0908f5976
hackeryarn <artemchernyak@gmail.com>
2025-09-21 09:31:59 -0500

    kustomize-sops: add binary

--- pkgs/development/tools/kustomize/kustomize-sops.nix
+++ pkgs/development/tools/kustomize/kustomize-sops.nix
@@ -18,10 +18,12 @@ buildGoModule rec {
   vendorHash = "sha256-UyDW4WgDgEygMxrxbCATxlPk9KAuc9lO/ldSFyydZUA=";
 
   installPhase = ''
+    mkdir -p $out/bin
     mkdir -p $out/lib/viaduct.ai/v1/ksops/
     mkdir -p $out/lib/viaduct.ai/v1/ksops-exec/
-    mv $GOPATH/bin/kustomize-sops $out/lib/viaduct.ai/v1/ksops/ksops
-    ln -s $out/lib/viaduct.ai/v1/ksops/ksops $out/lib/viaduct.ai/v1/ksops-exec/ksops-exec
+    mv $GOPATH/bin/kustomize-sops $out/bin/ksops
+    ln -s $out/bin/ksops $out/lib/viaduct.ai/v1/ksops-exec/ksops-exec
+    ln -s $ous/bin/ksops $out/lib/viaduct.ai/v1/ksops/ksops
   '';
 
   # Tests are broken in a nix environment
