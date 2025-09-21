0da339397bfc8fa365c9f493c2a18547550ca0e0
Pascal Jungblut <mail@pascalj.de>
2025-09-21 10:32:30 +0200

    ceph: update fmt

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2361,11 +2361,7 @@ with pkgs;
       lua = lua5_4; # Ceph currently requires >= 5.3
 
       # To see which `fmt` version Ceph upstream recommends, check its `src/fmt` submodule.
-      #
-      # Ceph does not currently build with `fmt_11`; see https://github.com/NixOS/nixpkgs/issues/281027#issuecomment-1899128557
-      # If we want to switch for that before upstream fixes it, use this patch:
-      # https://github.com/NixOS/nixpkgs/pull/281858#issuecomment-1899648638
-      fmt = fmt_9;
+      fmt = fmt_11;
 
       # Remove once Ceph supports arrow-cpp >= 20, see:
       # * https://tracker.ceph.com/issues/71269
