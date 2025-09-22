a9fa7331a9675b46856e9ca92f8ace893879b509
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-22 06:52:02 +0000

    python3Packages.meross-iot: 0.4.9.1 -> 0.4.9.2

--- pkgs/development/python-modules/meross-iot/default.nix
+++ pkgs/development/python-modules/meross-iot/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "meross-iot";
-  version = "0.4.9.1";
+  version = "0.4.9.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "albertogeniola";
     repo = "MerossIot";
     tag = version;
-    hash = "sha256-c9nUFCjbuqJaGk5Rqo+Nmr+IYQrWsVqb36mLmYTc47s=";
+    hash = "sha256-l0mw/V965JqX/pgcs9ek9dMLzUCGnam0E5Cu8W/HfEU=";
   };
 
   build-system = [ setuptools ];
