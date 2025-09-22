ac93c99ea28f41112b9c047380c2fa6af0fb0a71
Matt Richmond <github@1z.co.za>
2025-09-22 10:16:54 +0200

    Update whatsapp-for-mac to an available version
    The version of the WhatsApp mac app that the package uses is no longer available for download.
    
    I have bumped the version and updated the hash based on output from nix-prefetch-url.

--- pkgs/by-name/wh/whatsapp-for-mac/package.nix
+++ pkgs/by-name/wh/whatsapp-for-mac/package.nix
@@ -10,13 +10,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "whatsapp-for-mac";
-  version = "2.24.23.82";
+  version = "2.25.24.76";
 
   src = fetchzip {
     extension = "zip";
     name = "WhatsApp.app";
     url = "https://web.whatsapp.com/desktop/mac_native/release/?version=${finalAttrs.version}&extension=zip&configuration=Release&branch=relbranch";
-    hash = "sha256-FnXW3l4JUdJkDQ7eASlDeUzRdZ2N5l3WvEKcuBuE0nY=";
+    hash = "sha256-0pzhr6pci9wd5376byky4v4331jzdhxnnlv45mzml3fkxvzdackz";
   };
 
   dontConfigure = true;
