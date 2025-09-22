ea6f12d80aa5c653915cd7c304e899df8a21afd7
Randy Eckenrode <randy@largeandhighquality.com>
2025-09-22 18:53:09 -0400

    bitwarden-desktop: use default SDK on Darwin (14.4)

--- pkgs/by-name/bi/bitwarden-desktop/package.nix
+++ pkgs/by-name/bi/bitwarden-desktop/package.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  apple-sdk_14,
   buildNpmPackage,
   cargo,
   copyDesktopItems,
@@ -122,10 +121,6 @@ buildNpmPackage' rec {
   ++ lib.optionals stdenv.hostPlatform.isDarwin [
     xcbuild
     darwin.autoSignDarwinBinariesHook
-  ];
-
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    apple-sdk_14
   ];
 
   preBuild = ''
