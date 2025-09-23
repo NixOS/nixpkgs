159f75fe6d829475803e27360cfe09e26a1f6e58
Fabian Affolter <fabian@affolter-engineering.ch>
2025-09-23 14:16:55 +0200

    pip-audit: override cyclonedx-python-lib

--- pkgs/by-name/pi/pip-audit/package.nix
+++ pkgs/by-name/pi/pip-audit/package.nix
@@ -4,7 +4,25 @@
   python3,
 }:
 
-python3.pkgs.buildPythonApplication rec {
+let
+  py = python3.override {
+    packageOverrides = self: super: {
+
+      # Requires cyclonedx-python-lib <10.0.0
+      cyclonedx-python-lib = super.cyclonedx-python-lib.overridePythonAttrs (oldAttrs: rec {
+        version = "9.1.0";
+        src = fetchFromGitHub {
+          owner = "CycloneDX";
+          repo = "cyclonedx-python-lib";
+          tag = "v${version}";
+          hash = "sha256-XnRyE+C29W+rKrJop15jMNAXfAOdty877fKluhmEqIc=";
+        };
+      });
+    };
+  };
+in
+
+py.pkgs.buildPythonApplication rec {
   pname = "pip-audit";
   version = "2.9.0";
   pyproject = true;
@@ -16,12 +34,10 @@ python3.pkgs.buildPythonApplication rec {
     hash = "sha256-j8ZKqE7PEwaCTUNnJunqM0A2eyuWfx8zG5i3nmZERow=";
   };
 
-  pythonRelaxDeps = [ "cyclonedx-python-lib" ];
-
-  build-system = with python3.pkgs; [ flit-core ];
+  build-system = with py.pkgs; [ flit-core ];
 
   dependencies =
-    with python3.pkgs;
+    with py.pkgs;
     [
       cachecontrol
       cyclonedx-python-lib
@@ -35,7 +51,7 @@ python3.pkgs.buildPythonApplication rec {
     ]
     ++ cachecontrol.optional-dependencies.filecache;
 
-  nativeCheckInputs = with python3.pkgs; [
+  nativeCheckInputs = with py.pkgs; [
     pretend
     pytestCheckHook
   ];
