6a17aaf6c363612d17359eef29b1eb29c80ce76b
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-23 00:30:27 +0000

    libsForQt5.plasma-wayland-protocols: 1.18.0 -> 1.19.0

--- pkgs/development/libraries/plasma-wayland-protocols/default.nix
+++ pkgs/development/libraries/plasma-wayland-protocols/default.nix
@@ -8,11 +8,11 @@
 
 mkDerivation rec {
   pname = "plasma-wayland-protocols";
-  version = "1.18.0";
+  version = "1.19.0";
 
   src = fetchurl {
     url = "mirror://kde/stable/${pname}/${pname}-${version}.tar.xz";
-    hash = "sha256-JWdHJnGtXZifiLUbrvndWTU6Xnw/LtfmuYl1XLkAQjM=";
+    hash = "sha256-RWef56Y8QU8sgXk6YlKPrmzO5YS2llcZ1/n8bdSLqEY=";
   };
 
   nativeBuildInputs = [ extra-cmake-modules ];
