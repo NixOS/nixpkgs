6451da34e9e51c818572e402688a382f33a67e7b
Michael Daniels <mdaniels5757@gmail.com>
2025-09-23 18:29:33 -0400

    maintainers: drop Sanskarzz
    Last nixpkgs activity was October 2024; there have been 2 PRs since then.
    
    Not an org member, so can't be requested for review normally.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22920,12 +22920,6 @@
     email = "hey@sandydoo.me";
     matrix = "@sandydoo:matrix.org";
   };
-  Sanskarzz = {
-    email = "sanskar.gur@gmail.com";
-    github = "Sanskarzz";
-    githubId = 92817635;
-    name = "Sanskar Gurdasani";
-  };
   santosh = {
     email = "santoshxshrestha@gmail.com";
     name = "Santosh Shrestha";
