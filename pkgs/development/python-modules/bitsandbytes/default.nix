bf194af504facd1d678bec6cae6fa9d3800182c7
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-23 06:58:14 +0000

    python3Packages.bitsandbytes: 0.47.0 -> continuous-release_main

--- pkgs/development/python-modules/bitsandbytes/default.nix
+++ pkgs/development/python-modules/bitsandbytes/default.nix
@@ -16,7 +16,7 @@
 
 let
   pname = "bitsandbytes";
-  version = "0.47.0";
+  version = "continuous-release_main";
 
   inherit (torch) cudaPackages cudaSupport;
   inherit (cudaPackages) cudaMajorMinorVersion;
@@ -62,7 +62,7 @@ buildPythonPackage {
     owner = "bitsandbytes-foundation";
     repo = "bitsandbytes";
     tag = version;
-    hash = "sha256-iUAeiNbPa3Q5jJ4lK2G0WvTKuipb0zO1mNe+wcRdnqs=";
+    hash = "sha256-Ob4pr6GLZQZV+JNQCtXEQrQG1WHHVwhG6QYK54NMrHY=";
   };
 
   # By default, which library is loaded depends on the result of `torch.cuda.is_available()`.
