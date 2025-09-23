4f81b9b642e5ebdef6514c26b65c1f5f3f414e5a
Connor Baker <ConnorBaker01@gmail.com>
2025-09-23 15:46:02 +0000

    _cuda.lib.licenses: init
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

---
+++ pkgs/development/cuda-modules/_cuda/lib/licenses.nix
@@ -0,0 +1,55 @@
+{
+  cudnn = {
+    shortName = "cuDNN EULA";
+    fullName = "cuDNN SUPPLEMENT TO SOFTWARE LICENSE AGREEMENT FOR NVIDIA SOFTWARE DEVELOPMENT KITS";
+    url = "https://docs.nvidia.com/deeplearning/cudnn/backend/latest/reference/eula.html";
+    free = false;
+    redistributable = false;
+  };
+
+  cusparse_lt = {
+    shortName = "cuSPARSELt EULA";
+    fullName = "cuSPARSELt SUPPLEMENT TO SOFTWARE LICENSE AGREEMENT FOR NVIDIA SOFTWARE DEVELOPMENT KITS";
+    url = "https://docs.nvidia.com/cuda/cusparselt/license.html";
+    free = false;
+    redistributable = false;
+  };
+
+  cutensor = {
+    shortName = "cuTENSOR EULA";
+    fullName = "cuTENSOR SUPPLEMENT TO SOFTWARE LICENSE AGREEMENT FOR NVIDIA SOFTWARE DEVELOPMENT KITS";
+    url = "https://docs.nvidia.com/cuda/cutensor/latest/license.html";
+    free = false;
+    redistributable = false;
+  };
+
+  tensorrt = {
+    shortName = "TensorRT EULA";
+    fullName = "TensorRT SUPPLEMENT TO SOFTWARE LICENSE AGREEMENT FOR NVIDIA SOFTWARE DEVELOPMENT KITS";
+    url = "https://docs.nvidia.com/deeplearning/tensorrt/latest/reference/sla.html";
+    free = false;
+    redistributable = false;
+  };
+
+  math_sdk_sla = {
+    shortName = "NVIDIA Math SDK SLA";
+    fullName = "LICENSE AGREEMENT FOR NVIDIA MATH LIBRARIES SOFTWARE DEVELOPMENT KITS";
+    url = "https://developer.download.nvidia.com/compute/mathdx/License.txt";
+    free = false;
+    redistributable = false;
+  };
+
+  # "license": "CUDA Toolkit",
+  # "license": "NVIDIA Driver",
+  # "license": "NVIDIA Proprietary",
+  # "license": "NVIDIA",
+  # "license": "NVIDIA SLA",
+  # "license": "cuDSS library",
+  # "license": "cuQuantum",
+  # "license": "libcusolvermp library",
+  # "license": "NPP PLUS library",
+  # "license": "nvCOMP library",
+  # "license": "nvJPEG 2K",
+  # "license": "NVPL",
+  # "license": "nvTIFF",
+}
