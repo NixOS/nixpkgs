29ce0536172258b788030698d1b9f06736fbf23e
Connor Baker <ConnorBaker01@gmail.com>
2025-09-23 17:09:41 +0000

    cudaPackages: build redists from manifests
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- pkgs/development/cuda-modules/packages/backendStdenv.nix
+++ pkgs/development/cuda-modules/packages/backendStdenv.nix
@@ -6,8 +6,8 @@
 # E.g. for cudaPackages_12_9 we use gcc14 with gcc's libstdc++
 # Cf. https://github.com/NixOS/nixpkgs/pull/218265 for context
 {
-  config,
   _cuda,
+  config,
   cudaMajorMinorVersion,
   lib,
   pkgs,
@@ -15,7 +15,11 @@
   stdenvAdapters,
 }:
 let
-  inherit (builtins) toJSON;
+  inherit (builtins)
+    throw
+    toJSON
+    toString
+    ;
   inherit (_cuda.db) allSortedCudaCapabilities cudaCapabilityToInfo nvccCompatibilities;
   inherit (_cuda.lib)
     _cudaCapabilityIsDefault
@@ -24,9 +28,21 @@ let
     getRedistSystem
     mkVersionedName
     ;
-  inherit (lib) addErrorContext;
-  inherit (lib.customisation) extendDerivation;
-  inherit (lib.lists) filter intersectLists subtractLists;
+  inherit (lib)
+    addErrorContext
+    assertMsg
+    extendDerivation
+    filter
+    findFirst
+    intersectLists
+    pipe
+    range
+    reverseList
+    subtractLists
+    toIntBase10
+    versionAtLeast
+    ;
+  inherit (lib.versions) major;
 
   # NOTE: By virtue of processing a sorted list (allSortedCudaCapabilities), our groups will be sorted.
 
@@ -44,6 +60,9 @@ let
 
   passthruExtra = {
     nvccHostCCMatchesStdenvCC = backendStdenv.cc == stdenv.cc;
+
+    # TODO(@connorbaker): Does it make sense to expose the `stdenv` we were called with and the `stdenv` selected
+    # prior to using `stdenvAdapters.useLibsFrom`?
 
     # The Nix system of the host platform.
     hostNixSystem = stdenv.hostPlatform.system;
@@ -146,9 +165,78 @@ let
     _evaluateAssertions assertions
   );
 
+  # TODO(@connorbaker): Seems like `stdenvAdapters.useLibsFrom` breaks clangStdenv's ability to find header files.
+  # To reproduce: use `nix shell .#cudaPackages_12_6.backendClangStdenv.cc` since CUDA 12.6 supports at most Clang
+  # 18, but the current stdenv uses Clang 19, requiring this code path.
+  # With:
+  #
+  # ```cpp
+  # #include <cmath>
+  #
+  # int main() {
+  #     double value = 0.5;
+  #     double result = std::sin(value);
+  #     return 0;
+  # }
+  # ```
+  #
+  # we get:
+  #
+  # ```console
+  # $ clang++ ./main.cpp
+  # ./main.cpp:1:10: fatal error: 'cmath' file not found
+  #     1 | #include <cmath>
+  #       |          ^~~~~~~
+  # 1 error generated.
+  # ```
+  # TODO(@connorbaker): Seems like even using unmodified `clangStdenv` causes issues -- saxpy fails to build CMake
+  # errors during CUDA compiler identification about invalid redefinitions of things like `realpath`.
   backendStdenv =
-    stdenvAdapters.useLibsFrom stdenv
-      pkgs."gcc${nvccCompatibilities.${cudaMajorMinorVersion}.gcc.maxMajorVersion}Stdenv";
+    let
+      hostCCName =
+        if stdenv.cc.isGNU then
+          "gcc"
+        else if stdenv.cc.isClang then
+          "clang"
+        else
+          throw "cudaPackages.backendStdenv: unsupported host compiler: ${stdenv.cc.name}";
+
+      versions = nvccCompatibilities.${cudaMajorMinorVersion}.${hostCCName};
+
+      stdenvIsSupportedVersion =
+        versionAtLeast (major stdenv.cc.version) versions.minMajorVersion
+        && versionAtLeast versions.maxMajorVersion (major stdenv.cc.version);
+
+      maybeGetVersionedCC =
+        if hostCCName == "gcc" then
+          version: pkgs."gcc${version}Stdenv" or null
+        else
+          version: pkgs."llvmPackages_${version}".stdenv or null;
+
+      maybeHostStdenv =
+        pipe (range (toIntBase10 versions.minMajorVersion) (toIntBase10 versions.maxMajorVersion))
+          [
+            # Convert integers to strings.
+            (map toString)
+            # Prefer the highest available version.
+            reverseList
+            # Map to the actual stdenvs or null if unavailable.
+            (map maybeGetVersionedCC)
+            # Get the first available version.
+            (findFirst (x: x != null) null)
+          ];
+    in
+    # If the current stdenv's compiler version is compatible, or we're on an unsupported host system, use stdenv
+    # directly.
+    # If we're on an unsupported host system (like darwin), there's not much else we can do, but we should not break
+    # evaluation on unsupported systems.
+    if stdenvIsSupportedVersion || passthruExtra.hostRedistSystem == "unsupported" then
+      stdenv
+    # Otherwise, try to find a compatible stdenv.
+    else
+      assert assertMsg (maybeHostStdenv != null)
+        "backendStdenv: no supported host compiler found (tried ${hostCCName} ${versions.minMajorVersion} to ${versions.maxMajorVersion})";
+      stdenvAdapters.useLibsFrom stdenv maybeHostStdenv;
 in
 # TODO: Consider testing whether we in fact use the newer libstdc++
 extendDerivation assertCondition passthruExtra backendStdenv
