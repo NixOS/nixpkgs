1b151fb275efc2af65cee5999c37baf9b796726e
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-09-23 19:18:06 +0200

    osgqt: fix for CMake v4
    ref. https://github.com/openscenegraph/osgQt/pull/65, but upstream does
    not seem reactive.
    
    part of https://github.com/NixOS/nixpkgs/issues/445447

--- pkgs/by-name/os/osgqt/package.nix
+++ pkgs/by-name/os/osgqt/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "osgQt";
-  version = "3.5.7";
+  version = "3.5.7-unstable-2021-04-05";
 
   src = fetchFromGitHub {
     owner = "openscenegraph";
     repo = "osgQt";
-    rev = finalAttrs.version;
-    hash = "sha256-iUeIqRDlcAHdKXWAi4WhEaOCxa7ZivQw0K5E7ccEKnM=";
+    rev = "8fa9e2aed141488fa0818219f29e7ee9c7e667b0";
+    hash = "sha256-i6/6jOK7goIYKxKe0dtUa4dE4nLcYzFy2k+qKMiYMzk=";
   };
 
   buildInputs = [ libsForQt5.qtbase ];
@@ -33,10 +33,11 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   postPatch = ''
+    # Fix for CMake v4
+    # ref. https://github.com/openscenegraph/osgQt/pull/65
     substituteInPlace CMakeLists.txt --replace-fail \
-      "FIND_PACKAGE(Qt5Widgets REQUIRED)" \
-      "FIND_PACKAGE(Qt5Widgets REQUIRED)
-       FIND_PACKAGE(Qt5OpenGL REQUIRED)"
+      "CMAKE_MINIMUM_REQUIRED(VERSION 3.0.0 FATAL_ERROR)" \
+      "CMAKE_MINIMUM_REQUIRED(VERSION 3.10)" \
   '';
 
   meta = {
