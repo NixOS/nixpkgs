0139fe2373a964c8aa73bd9fd5c1e28d4533e176
Robert Schütz <nix@dotlambda.de>
2025-09-24 10:55:34 -0700

    python3Packages.aiohomematic: 2025.8.10 -> 2025.9.4
    Diff: https://github.com/SukramJ/aiohomematic/compare/2025.8.10...2025.9.4
    
    Changelog: https://github.com/SukramJ/aiohomematic/blob/2025.9.4/changelog.md

--- pkgs/development/python-modules/aiohomematic/default.nix
+++ pkgs/development/python-modules/aiohomematic/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "aiohomematic";
-  version = "2025.8.10";
+  version = "2025.9.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "SukramJ";
     repo = "aiohomematic";
     tag = version;
-    hash = "sha256-ii+fvjn54IT376YzwljPz2tkx2bv8jojI4rNracCgwA=";
+    hash = "sha256-+OoWomZCrRPHtpmYnzjhvcpidP5GWQwWoZpTZ4Bdgwg=";
   };
 
   postPatch = ''
