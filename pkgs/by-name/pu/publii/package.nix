1b1dd1216e176b9b8f099cd75314174b5a143e50
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-24 12:37:01 +0000

    publii: 0.46.5 -> 0.47.0

--- pkgs/by-name/pu/publii/package.nix
+++ pkgs/by-name/pu/publii/package.nix
@@ -28,11 +28,11 @@
 
 stdenv.mkDerivation rec {
   pname = "publii";
-  version = "0.46.5";
+  version = "0.47.0";
 
   src = fetchurl {
     url = "https://getpublii.com/download/Publii-${version}.deb";
-    hash = "sha256-VymAHQNv3N7Mqe8wiUfYawi1BooczLFClxuwaW8NetA=";
+    hash = "sha256-7g5hK+DkrltS8FIkY9BlpV/Gfiv64RkV8RtfgarqhqE=";
   };
 
   dontConfigure = true;
