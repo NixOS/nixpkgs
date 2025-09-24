224b861f4f2712d8565510fe05454b2b49022d4e
qbisi <qbisicwate@gmail.com>
2025-09-25 00:29:09 +0800

    laszip_2: fix build with cmake v4

--- pkgs/by-name/la/laszip_2/package.nix
+++ pkgs/by-name/la/laszip_2/package.nix
@@ -16,6 +16,12 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-TXzse4oLjNX5R2xDR721iV+gW/rP5z3Zciv4OgxfeqA=";
   };
 
+  # fix build with cmake v4
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail 'cmake_minimum_required(VERSION 2.6.0)' 'cmake_minimum_required(VERSION 3.10)'
+  '';
+
   nativeBuildInputs = [ cmake ];
 
   meta = {
