c0095a2570ddcdd422e7a1f1faee5284ed650ba4
Nico Felbinger <nico@felbinger.eu>
2025-09-24 23:09:15 +0200

    python3Packages.findus: init at 1.11.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5189,6 +5189,8 @@ self: super: with self; {
 
   findspark = callPackage ../development/python-modules/findspark { };
 
+  findus = callPackage ../development/python-modules/findus { };
+
   finetuning-scheduler = callPackage ../development/python-modules/finetuning-scheduler { };
 
   fingerprints = callPackage ../development/python-modules/fingerprints { };
