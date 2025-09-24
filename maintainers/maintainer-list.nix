dc317ab8c56fb9fc4b8b2d06f3576d95b4cdef77
kqr <a@xkqr.org>
2025-09-24 20:08:44 +0200

    maintainers: add kqr

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13759,6 +13759,12 @@
     githubId = 7763184;
     name = "kpcyrd";
   };
+  kqr = {
+    name = "Christoffer Stjernlöf";
+    github = "kqr";
+    githubId = 356598;
+    email = "nixpkgs@xkqr.org";
+  };
   kquick = {
     email = "quick@sparq.org";
     github = "kquick";
