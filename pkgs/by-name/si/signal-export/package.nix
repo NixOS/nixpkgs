1d35ead9961253516fd7fad9550d32f212e5ce46
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-24 21:41:25 +0000

    signal-export: 3.7.1 -> 3.8.0

--- pkgs/by-name/si/signal-export/package.nix
+++ pkgs/by-name/si/signal-export/package.nix
@@ -7,13 +7,13 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "signal-export";
-  version = "3.7.1";
+  version = "3.8.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit version;
     pname = "signal_export";
-    hash = "sha256-7oSaVC1TpRwoOwpXbercFOBoyrmKx8J9QUnt/a6JHGs=";
+    hash = "sha256-bl5rl6zRn4K/CXjS/5OCfngzNeUzXsXjhd2fTFHsHbg=";
   };
 
   build-system = with python3.pkgs; [
