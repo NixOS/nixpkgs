e3c200c57f11d0f7c2bc3d3c75a78573aba216f6
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-24 00:44:46 +0000

    libwpe: 1.16.2 -> 1.16.3

--- pkgs/development/libraries/libwpe/default.nix
+++ pkgs/development/libraries/libwpe/default.nix
@@ -12,11 +12,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libwpe";
-  version = "1.16.2";
+  version = "1.16.3";
 
   src = fetchurl {
     url = "https://wpewebkit.org/releases/libwpe-${version}.tar.xz";
-    sha256 = "sha256-lgvdEcPyz1vZFWlgPtbSqkL9QADtfKyTCoBOrDZ4iNc=";
+    sha256 = "sha256-yID6jWB7Kqbq3efW1jArE5brw4No/iMy+iDhk8fuFCA=";
   };
 
   nativeBuildInputs = [
