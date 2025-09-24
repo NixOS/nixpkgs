d59000cc1afb5ec366e97e27be5c6fd5e0203584
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-09-24 18:46:15 +0200

    jrl-cmakemodules: 0-unstable-2025-05-04 -> 1.1.0

--- pkgs/by-name/jr/jrl-cmakemodules/package.nix
+++ pkgs/by-name/jr/jrl-cmakemodules/package.nix
@@ -2,27 +2,31 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  nix-update-script,
   cmake,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "jrl-cmakemodules";
-  version = "0-unstable-2025-05-04";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "jrl-umi3218";
     repo = "jrl-cmakemodules";
-    rev = "2dd858f5a71d8224f178fb3dc0bcd95256ba10e7";
-    hash = "sha256-Iq9IuhEJBmDd14FhQ3wb94AoJDUjJ1h1D3qCdQYCnUc=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-WQiAAexshQ4zgaBNo/CD91XV+PAeoPZatmehSA14aPM=";
   };
 
   nativeBuildInputs = [ cmake ];
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "CMake utility toolbox";
     homepage = "https://github.com/jrl-umi3218/jrl-cmakemodules";
+    changelog = "https://github.com/jrl-umi3218/jrl-cmakemodules/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.lgpl3Plus;
     maintainers = [ lib.maintainers.nim65s ];
     platforms = lib.platforms.all;
   };
-}
+})
