00a877b5bfbd7d32fa71552bce7fe92d2c13080e
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-09-25 13:11:43 -0300

    cosmic-files: 1.0.0-beta.1 -> 1.0.0-beta.1.1
    Diff: https://github.com/pop-os/cosmic-files/compare/epoch-1.0.0-beta.1...epoch-1.0.0-beta.1.1

--- pkgs/by-name/co/cosmic-files/package.nix
+++ pkgs/by-name/co/cosmic-files/package.nix
@@ -12,14 +12,14 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-files";
-  version = "1.0.0-beta.1";
+  version = "1.0.0-beta.1.1";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-files";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-YOvNUr63rVsTbIKENqP8/iL3ga+vh6eGMgalWS5rGVc=";
+    hash = "sha256-pSjmsWsGGhjCekMTX8iiNVbF5X33zg5YVDWtemjIDWU=";
   };
 
   cargoHash = "sha256-7RANj+aXdmBVO66QDgcNrrU4qEGK4Py4+ZctYWU1OO8=";
