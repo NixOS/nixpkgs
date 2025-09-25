bd8d15e1cd5f92a62222d2291bbc4db5f70f29c5
kyehn <kyehn@users.noreply.github.com>
2025-09-26 06:12:17 +0800

    sonic-pi: 4.5.1 -> 4.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12460,7 +12460,7 @@ with pkgs;
 
   spotify-qt = qt6Packages.callPackage ../applications/audio/spotify-qt { };
 
-  sonic-pi = libsForQt5.callPackage ../applications/audio/sonic-pi { };
+  sonic-pi = callPackage ../applications/audio/sonic-pi { };
 
   stag = callPackage ../applications/misc/stag {
     curses = ncurses;
