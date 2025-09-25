3e584fea4b59b09f507246402ce292407c858117
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-25 10:26:11 +0000

    venera: 1.5.0 -> 1.5.1

--- pkgs/by-name/ve/venera/package.nix
+++ pkgs/by-name/ve/venera/package.nix
@@ -12,13 +12,13 @@
 }:
 
 let
-  version = "1.5.0";
+  version = "1.5.1";
 
   src = fetchFromGitHub {
     owner = "venera-app";
     repo = "venera";
     tag = "v${version}";
-    hash = "sha256-LhPtoMD7IjxbTFTSzP+vtflDUixUoN9eqE1AQyWhJzg=";
+    hash = "sha256-loqDwv/MPwBiDiY62XctjHbyDTgmUW76YdEpNId7lPU=";
   };
 in
 flutter335.buildFlutterApplication {
