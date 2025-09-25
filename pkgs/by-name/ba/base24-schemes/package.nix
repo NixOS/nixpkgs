db10d6c508f0a5c22d3d7c9a536b66927aa5c8d8
fe2-Nyxar <153492661+fe2-Nyxar@users.noreply.github.com>
2025-09-25 05:21:29 +0100

    Create package.nix
    base24 schemes

---
+++ pkgs/by-name/ba/base24-schemes/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "base24-schemes";
+  version = "unstable-2025-9-25";
+
+  src = fetchFromGitHub {
+    owner = "tinted-theming";
+    repo = "schemes";
+    rev = "317a5e10c35825a6c905d912e480dfe8e71c7559";
+    hash = "sha256-d4km8W7w2zCUEmPAPUoLk1NlYrGODuVa3P7St+UrqkM=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/themes/
+    install base24/*.yaml $out/share/themes/
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "All the color schemes for use in base24 packages";
+    homepage = "https://github.com/tinted-theming/schemes";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [fe2-Nyxar];
+  };
+})
