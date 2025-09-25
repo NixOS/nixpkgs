f2f32cfac17b7128c4e824aad2b4225490748b31
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-09-25 13:19:29 -0300

    cosmic-launcher: 1.0.0-beta.1 -> 1.0.0-beta.1.1
    Diff: https://github.com/pop-os/cosmic-launcher/compare/epoch-1.0.0-beta.1...epoch-1.0.0-beta.1.1

--- pkgs/by-name/co/cosmic-launcher/package.nix
+++ pkgs/by-name/co/cosmic-launcher/package.nix
@@ -11,14 +11,14 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-launcher";
-  version = "1.0.0-beta.1";
+  version = "1.0.0-beta.1.1";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-launcher";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-/OXX+jcuXbAIH4+5RK5dNVdugxbH/qUPOYUKeeNS6vw=";
+    hash = "sha256-vAuWH9qmstPcfPvcKoM/2VjGxAfdtO9hhOPaZ5Ft4Y0=";
   };
 
   cargoHash = "sha256-57rkCufJPWm844/iMIfULfaGR9770q8VgZgnqCM57Zg=";
