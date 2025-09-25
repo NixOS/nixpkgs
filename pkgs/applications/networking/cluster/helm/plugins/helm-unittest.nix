cbded030b8ab103ac24ac09919bed2d281fdbce3
Ihar Hrachyshka <ihrachyshka@nvidia.com>
2025-09-25 11:46:06 -0400

    kubernetes-helmPlugins.helm-unittest: 0.7.2 -> 1.0.1

--- pkgs/applications/networking/cluster/helm/plugins/helm-unittest.nix
+++ pkgs/applications/networking/cluster/helm/plugins/helm-unittest.nix
@@ -2,20 +2,22 @@
   buildGoModule,
   fetchFromGitHub,
   lib,
+  yq-go,
+  nix-update-script,
 }:
 
 buildGoModule rec {
   pname = "helm-unittest";
-  version = "0.7.2";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "helm-unittest";
     repo = "helm-unittest";
     rev = "v${version}";
-    hash = "sha256-RWucFZlyVYV5pHFGP7x5I+SILAJ9k12R7l5o7WKGS/c=";
+    hash = "sha256-bdLxW6dkA+jdn6UVOGngP3U0Do1zZt3Tnb9d6yVOGG0=";
   };
 
-  vendorHash = "sha256-tTM9n/ahtAJoQt0fwf1jrSokWER+cOnpPX7NTNrhKc4=";
+  vendorHash = "sha256-kiQRttnXgcTAElPlggkk11BGilcA+hG8doMq5eAmH6Q=";
 
   # NOTE: Remove the install and upgrade hooks.
   postPatch = ''
@@ -28,6 +30,10 @@ buildGoModule rec {
     rmdir $out/bin
     install -m644 -Dt $out/helm-unittest plugin.yaml
   '';
+
+  nativeCheckInputs = [
+    yq-go
+  ];
 
   passthru = {
     updateScript = nix-update-script { };
