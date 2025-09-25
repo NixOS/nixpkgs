406e2e822670e65a845bcb5302da0737fdc099b5
ginkogruen <jasper@wolter-koeln.de>
2025-09-25 16:54:46 +0200

    cook-cli: 0.14.0 -> 0.18.0
    Update in reference to this release of cook-cli:
    https://github.com/cooklang/cookcli/releases/tag/v0.18.0
    
    In comparison with the previous release cook-cli did undergo a rewrite
    which lead to changes in the packaging derivation.
    
    - Because the Cargo.lock file contains duplicate dependencies it was
      necessary to patch the lockfile. I've decided in favor of the git
      dependency because its a direct dependency of this package and refers
      to a specific branch & features.
    
    - A test which didn't account for building without default features (for
      disabling the self-updating feature) failed.
      Because of this I disabled that specific test.

--- pkgs/by-name/co/cook-cli/package.nix
+++ pkgs/by-name/co/cook-cli/package.nix
@@ -1,52 +1,63 @@
 {
   lib,
   fetchFromGitHub,
-  buildNpmPackage,
+  fetchNpmDeps,
+  npmHooks,
   rustPlatform,
   pkg-config,
   openssl,
+  nodejs,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cook-cli";
-  version = "0.14.0";
+  version = "0.18.0";
 
   src = fetchFromGitHub {
     owner = "cooklang";
     repo = "cookcli";
     rev = "v${version}";
-    hash = "sha256-jaAgmqUuqldcBlrwqXsausXPP35RZqM1VasYyA0pPO8=";
+    hash = "sha256-lNR+MqNRRy/r50wNTmoy0HZ2BxYmH8LbEdyDOWJH6rA=";
   };
 
-  cargoHash = "sha256-JVWa5vQcskXEgOqAxr2CKQDMjYakA1HqinDbKfRp/Wo=";
+  cargoHash = "sha256-16oIOaam9pVVVjJs88+s/Nlrw78KTi1+QzS5eqJkvXE=";
+
+  cargoPatches = [
+    # This patch fixes duplicate dependencies in `Cargo.lock` which caused `cargo vendor` to fail.
+    # For this the dependency on the `cooklang 0.17.0` crate was replaced
+    # with the more specialized `cooklang 0.17.0` git dependency.
+    ./remove-duplicate-cargo-dependency.patch
+  ];
 
   nativeBuildInputs = [
     pkg-config
     openssl
+    nodejs
+    npmHooks.npmConfigHook
   ];
 
   buildInputs = [
     openssl
   ];
 
-  postPatch = ''
-    rm -rf "ui/public"
-    ln -s ${passthru.ui} "ui/public"
-  '';
-
   OPENSSL_NO_VENDOR = 1;
 
-  passthru.ui = buildNpmPackage {
-    name = "ui";
-    src = "${src}/ui";
-    npmDepsHash = "sha256-zx8G6Raop1EZAVy1YCF5ag5aL9NutRxbPfTARmjP2SY=";
-    makeCacheWritable = true;
-    npmFlags = [ "--legacy-peer-deps" ];
-    installPhase = ''
-      runHook preInstall
-      mv public/ $out
-      runHook postInstall
-    '';
+  npmDeps = fetchNpmDeps {
+    inherit src;
+    hash = "sha256-HxC9Tf+PZvvETuNqm1W3jaZx7SpYXlxZlI8FwGouK+s=";
   };
+
+  preBuild = ''
+    npm run build-css
+  '';
+
+  # Build without the self-updating feature
+  buildNoDefaultFeatures = true;
+
+  checkFlags = [
+    # Disabled `test_help_output` as the testing suite doesn't account for building without default features.
+    # The test would fail with the 'update' subcommand rightfully missing from the output of `cook --help`.
+    "--skip=test_help_output"
+  ];
 
   meta = {
     changelog = "https://github.com/cooklang/cookcli/releases/tag/v${version}";
@@ -54,7 +65,10 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://cooklang.org/";
     license = lib.licenses.mit;
     mainProgram = "cook";
-    maintainers = [ lib.maintainers.emilioziniades ];
+    maintainers = [
+      lib.maintainers.emilioziniades
+      lib.maintainers.ginkogruen
+    ];
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
   };
 }
