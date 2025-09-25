b894a50dc793fa11378a535207815cc77b52a649
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-25 15:07:07 +0000

    quarkus: 3.26.3 -> 3.28.1

--- pkgs/by-name/qu/quarkus/package.nix
+++ pkgs/by-name/qu/quarkus/package.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "quarkus-cli";
-  version = "3.26.3";
+  version = "3.28.1";
 
   src = fetchurl {
     url = "https://github.com/quarkusio/quarkus/releases/download/${finalAttrs.version}/quarkus-cli-${finalAttrs.version}.tar.gz";
-    hash = "sha256-iFJJ1l1EloP3ChDaVF1ipJA54ia/THYl+0kMvfJn5a4=";
+    hash = "sha256-cJpAm4OOYHtWctY9JVHNz9Nh/YCWv9TaA2fU5QYFOTQ=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
