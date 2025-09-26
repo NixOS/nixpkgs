61e9c8c901232b6165f2dd6cfc6056fdb8e2f2c7
Fabian Affolter <fabian@affolter-engineering.ch>
2025-09-26 12:36:50 +0200

    python313Packages.model-checker: 1.2.4 -> 1.2.10

--- pkgs/development/python-modules/model-checker/default.nix
+++ pkgs/development/python-modules/model-checker/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "model-checker";
-  version = "1.2.4";
+  version = "1.2.10";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "model_checker";
     inherit version;
-    hash = "sha256-afJDpHVQv/pUqlPBT0ucQz2AjrhmVbNlrbVpiXT2w/Y=";
+    hash = "sha256-wU79zM2hGjmv/IxfQHHSLPBJFzoAPusHgtAaFSumFr0=";
   };
 
   # z3 does not provide a dist-info, so python-runtime-deps-check will fail
