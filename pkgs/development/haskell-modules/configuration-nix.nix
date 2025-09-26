3104b066e88f8744de3a1dc677bb558e903c60ef
sternenseemann <sternenseemann@systemli.org>
2025-09-26 12:39:03 +0200

    haskellPackages.postgresql-simple-migration: refactor override

--- pkgs/development/haskell-modules/configuration-nix.nix
+++ pkgs/development/haskell-modules/configuration-nix.nix
@@ -1253,7 +1253,8 @@ builtins.intersectAttrs super {
       pkgs.postgresql
       pkgs.postgresqlTestHook
     ];
-  }) (doJailbreak super.postgresql-simple-migration);
+    jailbreak = true;
+  }) super.postgresql-simple-migration;
 
   postgresql-simple = addTestToolDepends [
     pkgs.postgresql
