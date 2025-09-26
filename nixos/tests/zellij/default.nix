43cdc4540c93fc5ad3a0b0aa487b7caae1186def
pinage404 <pinage404@gmail.com>
2025-09-26 02:59:50 +0200

    zellij: test service remotely
    with local hardcoded self-signed certificate

--- nixos/tests/zellij/default.nix
+++ nixos/tests/zellij/default.nix
@@ -5,6 +5,8 @@
 
 let
   port = 8085;
+  remote_hostname = "acme.test";
+  remote_ip = "192.168.1.100";
 in
 {
   name = "zellij";
@@ -25,6 +27,44 @@ in
       };
 
       environment.etc."zellij/config.kdl".text = ""; # without this, the server seems to not start in the test, but it seems to work in real world
+
+      security.pki.certificateFiles = [
+        ../common/acme/server/ca.cert.pem
+      ];
+      networking.extraHosts = ''
+        ${remote_ip} ${remote_hostname}
+      '';
+    };
+
+    remote = {
+      imports = [
+        ../common/user-account.nix
+      ];
+
+      services.zellij = {
+        enable = true;
+        user = "alice";
+        web = {
+          enable = true;
+          port = port;
+          openFirewall = true;
+          ip = "0.0.0.0";
+          certificate = ../common/acme/server/acme.test.cert.pem;
+          key = ../common/acme/server/acme.test.key.pem;
+        };
+      };
+
+      environment.etc."zellij/config.kdl".text = ""; # without this, the server seems to not start in the test, but it seems to work in real world
+
+      networking = {
+        useDHCP = false;
+        interfaces.eth1.ipv4.addresses = [
+          {
+            address = remote_ip;
+            prefixLength = 24;
+          }
+        ];
+      };
     };
   };
 
@@ -36,9 +76,11 @@ in
       start_all()
 
       local.succeed(f"loginctl enable-linger {user_name}")
+      remote.succeed(f"loginctl enable-linger {user_name}")
 
       with subtest("Wait all VMs to be ready"):
           local.wait_for_unit("default.target")
+          remote.wait_for_unit("default.target")
 
       with subtest("Package is installed"):
           local.succeed(f"su - {user_name} --command 'command -v zellij'")
@@ -46,5 +88,9 @@ in
       with subtest("Server works localy"):
           local.wait_for_unit("zellij-web.service", user_name)
           local.succeed("curl --silent --show-error http://localhost:${toString port}/")
+
+      with subtest("Server works remotely"):
+          remote.wait_for_unit("zellij-web.service", user_name)
+          local.succeed("curl --silent --show-error https://${remote_hostname}:${toString port}/")
     '';
 }
