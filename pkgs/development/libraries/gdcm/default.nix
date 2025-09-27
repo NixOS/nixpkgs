dc722078846ccd45beeea5543f6e3457c53b105e
Ben Darwin <bcdarwin@gmail.com>
2025-09-27 16:40:42 -0400

    gdcm: 3.2.1 -> 3.2.2
    (cherry picked from commit e9e8affbcbe8545cab57cd09fbe82088ba7ea9e8)

--- pkgs/development/libraries/gdcm/default.nix
+++ pkgs/development/libraries/gdcm/default.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = if enablePython then "python-gdcm" else "gdcm";
-  version = "3.2.1";
+  version = "3.2.2";
 
   src = fetchFromGitHub {
     owner = "malaterre";
     repo = "GDCM";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-V1UPfRlNIDZa/N9wQ6wu8uCFH6bw8Y6jomy7BB4SNEA=";
+    hash = "sha256-iUJrXcVHdCmL5atm6in4KvgPXbYmAYFxM6TGoZeY9EE=";
   };
 
   patches = [
