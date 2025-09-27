51416380fb6089afebaad22f939fbfbdf490a8c3
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-27 05:38:45 +0000

    nerdfetch: 8.4.0 -> 8.4.2

--- pkgs/by-name/ne/nerdfetch/package.nix
+++ pkgs/by-name/ne/nerdfetch/package.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "nerdfetch";
-  version = "8.4.0";
+  version = "8.4.2";
 
   src = fetchFromGitHub {
     owner = "ThatOneCalculator";
     repo = "NerdFetch";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-KMu/cMjRFEyfRxoKDGn4PfubGCrotVsKQ9wwc1wQaVM=";
+    hash = "sha256-G1BWggVPxpIKK82pKHD4Jxyis4CY156Jox2/xHRQfrI=";
   };
 
   dontUnpack = true;
