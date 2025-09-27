28f23896484429e3c719a3b6d9c88ec9aa3f2be0
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-09-27 12:55:00 +0200

    update version

--- pkgs/by-name/bu/bun/package.nix
+++ pkgs/by-name/bu/bun/package.nix
@@ -4,35 +4,32 @@ let
   buildBun = callPackage ./bun.nix { };
 in
 buildBun rec {
-  version = "1.2.21";
+  version = "1.2.22";
   sources = {
     "aarch64-darwin" = fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-darwin-aarch64.zip";
-      hash = "sha256-/YhmMLoVxIQjatXz8islXSh8Pu+NO8JvyAmFEDXATOw=";
+      hash = "sha256-64x+CcvqVyQUoKNnhI4ay/BSlKlGpZRAWgFLH7Oz/HY=";
     };
     "aarch64-linux" = fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-aarch64.zip";
-      hash = "sha256-DkyeVIdqFg6RgSrjxi02z71o8g4VjLcy9joNa3WUKHs=";
+      hash = "sha256-qXxof7XlTeTi+whpp6yaLZw691rBguK2gTjB3Y+YExs=";
     };
     "x86_64-darwin" = fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-darwin-x64-baseline.zip";
-      hash = "sha256-Qm1N3h5Rg0aNMf+G7RPAjW8p7wlcia5QtfX1IP3u2RY=";
+      hash = "sha256-58GgthtkZsbaKnJbPFGGT6BQomLSSJ8aEJ8nNkx0MnI=";
     };
     "x86_64-linux" = fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-      hash = "sha256-WU9FTVHOVxmdQyDIXL1JW+nAVO8XquvKXmyQir/aYXk=";
+      hash = "sha256-TERq8aAde0Dh4RuuvDUvmyv9Eoh+Ubl907WYec7idDo=";
     };
   };
   baseline = buildBun {
     inherit version;
     sources = {
-      "x86_64-darwin" = fetchurl {
-        url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-darwin-x64-baseline.zip";
-        hash = "sha256-Qm1N3h5Rg0aNMf+G7RPAjW8p7wlcia5QtfX1IP3u2RY=";
-      };
+      # Linux-only: darwin already uses baseline
       "x86_64-linux" = fetchurl {
         url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64-baseline.zip";
-        hash = "sha256-t79LU6V8jcPRWJIizgoq+QIQ9yDSqKeVEqPmAbeTWQo=";
+        hash = "sha256-91Po2WaAeKsPWY7iaprFrLu4IuBXRZ3VDBkbhlJNmOg=";
       };
     };
   };
