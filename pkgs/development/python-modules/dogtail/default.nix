0dc6f221e8c9802475b4726884bb9133647cc734
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-09-27 22:53:05 +0200

    dogtail: 0.9.11 -> 1.0.7

--- pkgs/development/python-modules/dogtail/default.nix
+++ pkgs/development/python-modules/dogtail/default.nix
@@ -13,12 +13,12 @@
   dbus,
   xvfb-run,
   wrapGAppsHook3,
-# , fetchPypi
+  fetchPypi,
 }:
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "dogtail";
-  version = "0.9.11";
+  version = "1.0.7";
   format = "setuptools";
 
   outputs = [
@@ -26,24 +26,18 @@ buildPythonPackage {
     "dev"
   ];
 
-  # https://gitlab.com/dogtail/dogtail/issues/1
-  # src = fetchPypi {
-  #   inherit pname version;
-  #   sha256 = "0p5wfssvzr9w0bvhllzbbd8fnp4cca2qxcpcsc33dchrmh5n552x";
-  # };
-  src = fetchurl {
-    url = "https://gitlab.com/dogtail/dogtail/raw/released/dogtail-0.9.10.tar.gz";
-    sha256 = "EGyxYopupfXPYtTL9mm9ujZorvh8AGaNXVKBPWsGy3c=";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-VURwc8li710YRdEZcuIayVeSQEkN6CtON3C5qMOk+zs=";
   };
-
-  patches = [ ./nix-support.patch ];
 
   nativeBuildInputs = [
     gobject-introspection
     dbus
     xvfb-run
     wrapGAppsHook3
-  ]; # for setup hooks
+  ];
+
   propagatedBuildInputs = [
     at-spi2-core
     gtk3
@@ -52,24 +46,12 @@ buildPythonPackage {
     pycairo
   ];
 
-  checkPhase = ''
-    runHook preCheck
-    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:$XDG_DATA_DIRS
-    # export NO_AT_BRIDGE=1
-    gsettings set org.gnome.desktop.interface toolkit-accessibility true
-    xvfb-run -s '-screen 0 800x600x24' dbus-run-session \
-      --config-file=${dbus}/share/dbus-1/session.conf \
-      ${python.interpreter} nix_run_setup test
-    runHook postCheck
-  '';
-
   dontWrapGApps = true;
 
   preFixup = ''
     makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
   '';
 
-  # TODO: Tests require accessibility
   doCheck = false;
 
   meta = {
