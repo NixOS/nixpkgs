3bebbc4cf2759c9e9d9efaf09ff9f04efb0d3dbf
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-27 01:36:22 +0000

    vscode-extensions.visualjj.visualjj: 0.16.4 -> 0.16.6

--- pkgs/applications/editors/vscode/extensions/visualjj.visualjj/default.nix
+++ pkgs/applications/editors/vscode/extensions/visualjj.visualjj/default.nix
@@ -11,26 +11,26 @@ vscode-utils.buildVscodeMarketplaceExtension {
       sources = {
         "x86_64-linux" = {
           arch = "linux-x64";
-          hash = "sha256-cuCxSODDNf2nhiUwgQT5SRugRLYOhq3igsvgP/hzcZE=";
+          hash = "sha256-HbaB0WnWNOxooZDuAFbpCZi3EDrqUzDAEPovfN670hk=";
         };
         "x86_64-darwin" = {
           arch = "darwin-x64";
-          hash = "sha256-OQqw/f2gSmFLN9CDn/auxXY6ao/XieAY7XVcI/kf1TA=";
+          hash = "sha256-K+I36W1y31WG9JNd25L4AbxsIqS6zWrrgQVUAd+BEqY=";
         };
         "aarch64-linux" = {
           arch = "linux-arm64";
-          hash = "sha256-qMWJS/4afJkSENk+BdaPPAynjEMErT2jjeFAkOSMfZ4=";
+          hash = "sha256-AMTgCdUwe0ba6tuWj+GEtkcV/x0TCjXNVlrujoQ+Pe4=";
         };
         "aarch64-darwin" = {
           arch = "darwin-arm64";
-          hash = "sha256-cj6cu/uSHflJHKub4LKaqPOMc7EC3Hlzxp7DfDpi9hU=";
+          hash = "sha256-x0zki117HMP9M3pNeN4FVIzyIcTJIe3MO0GFsK0J8BY=";
         };
       };
     in
     {
       name = "visualjj";
       publisher = "visualjj";
-      version = "0.16.4";
+      version = "0.16.6";
     }
     // sources.${stdenvNoCC.hostPlatform.system}
       or (throw "Unsupported system ${stdenvNoCC.hostPlatform.system}");
