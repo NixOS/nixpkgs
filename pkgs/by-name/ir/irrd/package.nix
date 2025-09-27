7bd2f81a1505e562c520032691b6bc08bc817f87
Fabian Affolter <fabian@affolter-engineering.ch>
2025-09-27 18:09:45 +0200

    irrd: set to pytest-asyncio_0

--- pkgs/by-name/ir/irrd/package.nix
+++ pkgs/by-name/ir/irrd/package.nix
@@ -69,7 +69,7 @@ py.pkgs.buildPythonPackage rec {
     postgresqlTestHook
   ]
   ++ (with py.pkgs; [
-    pytest-asyncio
+    pytest-asyncio_0
     pytest-freezegun
     pytestCheckHook
     smtpdfix
@@ -142,10 +142,15 @@ py.pkgs.buildPythonPackage rec {
     kill $REDIS_PID
   '';
 
-  # skip tests that require internet access
   disabledTests = [
+    # Skip tests that require internet access
     "test_020_dash_o_noop"
     "test_050_non_json_response"
+  ];
+
+  disabledTestPaths = [
+    # Doesn't work with later pytest releases
+    "irrd/server/whois/tests/test_query_response.py"
   ];
 
   meta = {
