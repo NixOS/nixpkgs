1595c0f5ad3c20c7ed26be6e3e234ce55ed463b1
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-28 11:00:18 +0000

    vscode-extensions.danielsanmedium.dscodegpt: 3.14.110 -> 3.14.118

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -1152,8 +1152,8 @@ let
         mktplcRef = {
           publisher = "DanielSanMedium";
           name = "dscodegpt";
-          version = "3.14.110";
-          hash = "sha256-8qTKmtnDFCCYwZPE2E3fDNDPTTvHVMqbNL5BybN58X8=";
+          version = "3.14.118";
+          hash = "sha256-jAFlmF7HE0pfYx5jqnUjObM+awdc62DvayV9FdEx70E=";
         };
         meta = {
           changelog = "https://marketplace.visualstudio.com/items/DanielSanMedium.dscodegpt/changelog";
