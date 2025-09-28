31e20ffc3aa265e032cdfb04ea1df5513bc8f6dc
Fernando Rodrigues <alpha@sigmasquadron.net>
2025-09-28 19:45:07 +1000

    nixos/pangolin: use new services.traefik.plugins option
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- nixos/modules/services/networking/pangolin.nix
+++ nixos/modules/services/networking/pangolin.nix
@@ -451,15 +451,11 @@ in
 
     services.traefik = {
       enable = true;
+      localPlugins = [ pkgs.fosrl-badger ];
       install.settings = {
         providers.http = {
           endpoint = "http://localhost:${toString finalSettings.server.internal_port}/api/v1/traefik-config";
           pollInterval = "5s";
-        };
-        # TODO to change this once #437073 is merged.
-        experimental.plugins.badger = {
-          moduleName = "github.com/fosrl/badger";
-          version = "v1.2.0";
         };
         certificatesResolvers.letsencrypt.acme =
           (
