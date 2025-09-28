25c300a3c2822714c824c556732ea1ed524b9867
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-28 00:35:20 +0000

    codeberg-cli: 0.5.0 -> 0.5.1

--- pkgs/by-name/co/codeberg-cli/package.nix
+++ pkgs/by-name/co/codeberg-cli/package.nix
@@ -9,17 +9,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "codeberg-cli";
-  version = "0.5.0";
+  version = "0.5.1";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "Aviac";
     repo = "codeberg-cli";
     rev = "v${version}";
-    hash = "sha256-eTr+vkHHbUnUIli7PDEb/1abr2z2m0AZK8sTrkADaqM=";
+    hash = "sha256-81435dgw7fy4igUN3TYNjRans8uRZ/6TQWBxu0benwU=";
   };
 
-  cargoHash = "sha256-/cPso8VGG7a1ODHLFMQ1Pyr6NA+7umY+Z2iCBJfIGdE=";
+  cargoHash = "sha256-1E7UuDbJKrnhMQI6trk7nHkeywCPFg1021pQhRcb4kQ=";
   nativeBuildInputs = [
     pkg-config
     installShellFiles
