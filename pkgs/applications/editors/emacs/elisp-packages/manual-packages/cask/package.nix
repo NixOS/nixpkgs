f5e9a5e247d63f93cd841f795e73d116e1b9540c
magicquark <198001825+magicquark@users.noreply.github.com>
2025-09-28 04:26:20 +0100

    emacsPackages.cask: fix import error and use non-vendored dependencies
    - Fixes issue 445465.
    
    - Cask expects a local `package-build` directory to be present in the
      output, as it loads a custom version during bootstrap. See:
      https://github.com/cask/cask/blob/master/cask-bootstrap.el#L45
    
    - The vendored version of `package-build` is then used to download from
      `melpa` and `elpa` the dependencies that `cask` requires. This is
      contrary to the philosophy of `nixpkgs` whereby the dependencies
      should be sourced from `nixpkgs` itself.
    
    - To avoid this, the bootstrap process has been patched to provide the
      required dependencies using `nixpkgs`.
    
    - Additionally, `cask-cli` has a function `upgrade-cask` that uses the
      `cask-bootstrap-dir` that its own bootstrap process creates. See
      https://github.com/cask/cask/blob/fd16ea95664b8f13c55b8608dea63942d431ca00/cask-cli.el#L131.
      As the bootstrap process has been modified to use dependencies from
      `nixpkgs`, `cask` can no longer update itself. Therefore, this
      function has been patched to make it effectively a noop.
    
    - The patch cannot be upstreamed as it is specific to the way that
      packages are integrated into `nixpkgs`.
    
    (cherry picked from commit 0749d7abed040207dc6027c83f51b1a39ab309a7)

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/cask/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/cask/package.nix
@@ -11,62 +11,90 @@
   git,
   melpaBuild,
   package-build,
+  replaceVars,
   s,
   shut-up,
 }:
+let
+  formatLoadPath = x: ''"${x}/share/emacs/site-lisp/elpa/${x.ename}-${x.melpaVersion or x.version}"'';
+  formatNativeLoadPath = x: ''"${x}/share/emacs/native-lisp"'';
+  getAllDependenciesOfPkg =
+    pkg:
+    let
+      direct = builtins.filter (x: x != null) (pkg.packageRequires or [ ]);
+      indirect = builtins.concatLists (map getAllDependenciesOfPkg direct);
+    in
+    lib.unique (direct ++ indirect);
+in
+melpaBuild (
+  finalAttrs:
+  let
+    nixpkgDependencies = getAllDependenciesOfPkg finalAttrs.finalPackage;
+    loadPaths = builtins.concatStringsSep " " (map formatLoadPath nixpkgDependencies);
+    nativeLoadPaths = builtins.concatStringsSep " " (map formatNativeLoadPath nixpkgDependencies);
+    emacsBuiltinDeps = [
+      "cl-lib"
+      "eieio"
+    ];
+    depsMod = builtins.concatStringsSep " " ((map (x: x.ename) nixpkgDependencies) ++ emacsBuiltinDeps);
+  in
+  {
+    pname = "cask";
+    version = "0.9.0";
 
-melpaBuild (finalAttrs: {
-  pname = "cask";
-  version = "0.9.0";
+    src = fetchFromGitHub {
+      name = "cask-source-${finalAttrs.version}";
+      owner = "cask";
+      repo = "cask";
+      rev = "v${finalAttrs.version}";
+      hash = "sha256-91rJFsp2SLk/JY+v6G5JmXH5bg9QnT+qhI8ccNJlI4A=";
+    };
 
-  src = fetchFromGitHub {
-    name = "cask-source-${finalAttrs.version}";
-    owner = "cask";
-    repo = "cask";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-91rJFsp2SLk/JY+v6G5JmXH5bg9QnT+qhI8ccNJlI4A=";
-  };
+    nativeBuildInputs = [ installShellFiles ];
 
-  nativeBuildInputs = [ installShellFiles ];
+    patches = [
+      # Uses LISPDIR substitution var
+      ./0000-cask-lispdir.diff
+      # Use Nix provided dependencies instead of letting Cask bootstrap itself
+      (replaceVars ./0001-cask-bootstrap.diff {
+        inherit depsMod loadPaths nativeLoadPaths;
+      })
+    ];
 
-  patches = [
-    # Uses LISPDIR substitution var
-    ./0000-cask-lispdir.diff
-  ];
+    packageRequires = [
+      ansi
+      cl-generic
+      cl-lib
+      commander
+      epl
+      f
+      git
+      package-build
+      s
+      shut-up
+    ];
 
-  packageRequires = [
-    ansi
-    cl-generic
-    cl-lib
-    commander
-    epl
-    f
-    git
-    package-build
-    s
-    shut-up
-  ];
-
-  # use melpaVersion so that it works for unstable releases too
-  postPatch = ''
-    lispdir=$out/share/emacs/site-lisp/elpa/cask-${finalAttrs.melpaVersion} \
-      substituteAllInPlace bin/cask
-  '';
-
-  postInstall = ''
-    installBin bin/cask
-  '';
-
-  meta = {
-    homepage = "https://github.com/cask/cask";
-    description = "Project management for Emacs";
-    longDescription = ''
-      Cask is a project management tool for Emacs that helps automate the
-      package development cycle; development, dependencies, testing, building,
-      packaging and more.
+    # use melpaVersion so that it works for unstable releases too
+    postPatch = ''
+      lispdir=$out/share/emacs/site-lisp/elpa/cask-${finalAttrs.melpaVersion} \
+        substituteAllInPlace bin/cask
     '';
-    license = lib.licenses.gpl3Plus;
-    mainProgram = "cask";
-    maintainers = with lib.maintainers; [ ];
-  };
-})
+
+    postInstall = ''
+      installBin bin/cask
+    '';
+
+    meta = {
+      homepage = "https://github.com/cask/cask";
+      description = "Project management for Emacs";
+      longDescription = ''
+        Cask is a project management tool for Emacs that helps automate the
+        package development cycle; development, dependencies, testing, building,
+        packaging and more.
+      '';
+      license = lib.licenses.gpl3Plus;
+      mainProgram = "cask";
+      maintainers = with lib.maintainers; [ ];
+    };
+  }
+)
