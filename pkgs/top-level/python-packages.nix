852b6129da3b7404b2d47eb4eff91ca0b562e67b
Gavin John <gavinnjohn@gmail.com>
2025-09-28 19:25:27 -0700

    python3Packages.plover: fixup

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12108,6 +12108,7 @@ self: super: with self; {
 
   plover = callPackage ../development/python-modules/plover {
     inherit (pkgs.libsForQt5) wrapQtAppsHook;
+    pyqt = self.pyqt5;
   };
 
   plover-stroke = callPackage ../development/python-modules/plover-stroke { };
