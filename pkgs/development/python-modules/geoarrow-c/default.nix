871fc7892b6ddfb5f77f9116ea1183beffd9a7c1
Augustin Trancart <augustin.trancart@gmail.com>
2025-09-29 16:08:13 +0200

    geoarrow-c: 0.1.3 -> 0.3.0

--- pkgs/development/python-modules/geoarrow-c/default.nix
+++ pkgs/development/python-modules/geoarrow-c/default.nix
@@ -12,7 +12,7 @@
 }:
 buildPythonPackage rec {
   pname = "geoarrow-c";
-  version = "0.1.3";
+  version = "0.3.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,15 +21,23 @@ buildPythonPackage rec {
     repo = "geoarrow-c";
     owner = "geoarrow";
     tag = "geoarrow-c-python-${version}";
-    hash = "sha256-kQCD3Vptl7GtRFigr4darvdtwnaHRLZWvBBpZ0xHMgM=";
+    hash = "sha256-IT3lwJ0n6hdcfq9/B4Iwr9R8V3XGQ1vU1vycvPDomuw=";
   };
 
   sourceRoot = "${src.name}/python/geoarrow-c";
+
+  preConfigure = ''
+    export CFLAGS="-I../../src/src/geoarrow"
+  '';
 
   build-system = [
     cython
     setuptools
     setuptools-scm
+  ];
+  patches = [
+    # see https://github.com/geoarrow/geoarrow-c/issues/145
+    ./cython_version.patch
   ];
 
   env.SETUPTOOLS_SCM_PRETEND_VERSION = version;
