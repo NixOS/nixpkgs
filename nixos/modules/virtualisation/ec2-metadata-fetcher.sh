7a505250f67901bb445c104a298fe2df6c31d230
Adam C. Stephens <adam@valkor.net>
2025-09-29 09:42:05 -0400

    amazon-image: improve metadata fetch script resilience

--- nixos/modules/virtualisation/ec2-metadata-fetcher.sh
+++ nixos/modules/virtualisation/ec2-metadata-fetcher.sh
@@ -1,7 +1,7 @@
 metaDir=/etc/ec2-metadata
 mkdir -p "$metaDir"
 chmod 0755 "$metaDir"
-rm -f "$metaDir/*"
+rm -f "$metaDir"/*
 
 get_imds_token() {
   # retry-delay of 1 selected to give the system a second to get going,
@@ -35,33 +35,47 @@ preflight_imds_token() {
 
 try=1
 while [ $try -le 3 ]; do
-  echo "(attempt $try/3) getting an EC2 instance metadata service v2 token..."
-  IMDS_TOKEN=$(get_imds_token) && break
+  echo "(attempt $try/3) getting an EC2 instance metadata service IMDSv2 token..."
+  IMDS_TOKEN=$(get_imds_token)
+  RC=$?
+  if [ "$RC" = 0 ] && [ "$IMDS_TOKEN" != "" ]; then
+    break
+  fi
   try=$((try + 1))
   sleep 1
 done
 
 if [ "$IMDS_TOKEN" == "" ]; then
-  echo "failed to fetch an IMDS2v token."
+  echo "failed to fetch an IMDSv2 token."
+  exit 1
 fi
+
+echo "successfully fetched IMDSv2 token"
 
 try=1
 while [ $try -le 10 ]; do
-  echo "(attempt $try/10) validating the EC2 instance metadata service v2 token..."
+  echo "(attempt $try/10) validating the EC2 instance metadata service IMDSv2 token..."
   preflight_imds_token && break
   try=$((try + 1))
   sleep 1
 done
+echo "successfully validated IMDSv2 token"
 
 echo "getting EC2 instance metadata..."
 
 get_imds() {
+  echo "fetching: $*"
   # --fail to avoid populating missing files with 404 HTML response body
   # || true to allow the script to continue even when encountering a 404
-  curl --silent --show-error --fail --header "X-aws-ec2-metadata-token: $IMDS_TOKEN" "$@" || true
+  if ! curl --silent --show-error --fail --header "X-aws-ec2-metadata-token: $IMDS_TOKEN" "$@"; then
+    echo "failed to fetch: $*"
+    true
+  fi
 }
 
 get_imds -o "$metaDir/ami-manifest-path" http://169.254.169.254/1.0/meta-data/ami-manifest-path
 (umask 077 && get_imds -o "$metaDir/user-data" http://169.254.169.254/1.0/user-data)
 get_imds -o "$metaDir/hostname" http://169.254.169.254/1.0/meta-data/hostname
 get_imds -o "$metaDir/public-keys-0-openssh-key" http://169.254.169.254/1.0/meta-data/public-keys/0/openssh-key
+
+echo "done"
