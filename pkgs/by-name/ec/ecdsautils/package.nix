64f96372d6d4cd421877040016a6ae80415264ab
Yureka <yureka@cyberchaos.dev>
2025-09-29 09:35:41 +0200

    ecdsautils: fix build with cmake 4

--- pkgs/by-name/ec/ecdsautils/package.nix
+++ pkgs/by-name/ec/ecdsautils/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   cmake,
   pkg-config,
   doxygen,
@@ -21,6 +22,13 @@ stdenv.mkDerivation {
     rev = "v${version}";
     sha256 = "sha256-vGHLAX/XOtePvdT/rljCOdlILHVO20mCt6p+MUi13dg=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/freifunk-gluon/ecdsautils/commit/19f096f9c10264f4efe4b926fe83126e85642cba.patch";
+      hash = "sha256-oJv47NckFHFONPcG3WfHwgaHRqrz2eWXzbr5SQr+kX4=";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
