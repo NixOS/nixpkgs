bf105ba1f030e249dfc8cb7562f279135dbe096a
Lu Wang <rebmit@rebmit.moe>
2025-09-29 20:40:17 +0800

    mtxclient: mark as unbroken on darwin

--- pkgs/by-name/mt/mtxclient/package.nix
+++ pkgs/by-name/mt/mtxclient/package.nix
@@ -63,8 +63,5 @@ stdenv.mkDerivation rec {
       rnhmjoj
     ];
     platforms = platforms.all;
-    # Should be fixable if a higher clang version is used, see:
-    # https://github.com/NixOS/nixpkgs/pull/85922#issuecomment-619287177
-    broken = stdenv.hostPlatform.isDarwin;
   };
 }
