7a28425cf710cd45cb7e150f72052eb56692f62c
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-29 18:43:17 +0000

    python3Packages.txaio: 25.6.1 -> 25.9.2

--- pkgs/development/python-modules/txaio/default.nix
+++ pkgs/development/python-modules/txaio/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "txaio";
-  version = "25.6.1";
+  version = "25.9.2";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-2MA9yoI1Fcm8qSDfM1BJI65U8tq/R2zFqe1cwWke1oc=";
+    hash = "sha256-5CAEoHfALrWBn/AEpJieSdsRODZwhDDVnLE9Mb0wkJk=";
   };
 
   propagatedBuildInputs = [
