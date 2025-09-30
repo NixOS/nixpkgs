59f2125ce52ac9b80217581e1dbbbe459973c77a
Peter Hansson <peter.hansson17@gmail.com>
2025-09-30 22:36:36 +0200

    maintainers: lunkentuss

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15420,6 +15420,13 @@
       }
     ];
   };
+  lunkentuss = {
+    email = "peter.hansson17@gmail.com";
+    matrix = "@lunkentuss:matrix.org";
+    github = "lunkentuss";
+    githubId = 9850798;
+    name = "Peter Hansson";
+  };
   LunNova = {
     email = "nixpkgs-maintainer@lunnova.dev";
     github = "LunNova";
