66f6beb0023761968c6ed9d10d904212514940c9
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2025-09-30 14:44:19 +0200

    containerlab: 0.69.3 -> 0.70.2
    https://github.com/srl-labs/containerlab/releases/tag/v0.70.2

--- pkgs/by-name/co/containerlab/package.nix
+++ pkgs/by-name/co/containerlab/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "containerlab";
-  version = "0.69.3";
+  version = "0.70.2";
 
   src = fetchFromGitHub {
     owner = "srl-labs";
     repo = "containerlab";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-RJNJ5LUCGaARn5NOSepTL/0Owr/ozFUYAvlynDTyqfY=";
+    hash = "sha256-QBv0SZ7XxVc0yWbOxPKdfzk9AKYlMJyeZwpAx1jbamk=";
   };
 
-  vendorHash = "sha256-28Q1R6P2rpER5RxagnsKy9W3b4FUeRRbkPPovzag//U=";
+  vendorHash = "sha256-XttJ/GXhNKVHLR33A/o3N3OYHsyKWHBhD5QOz0AlfFk=";
 
   nativeBuildInputs = [
     installShellFiles
@@ -27,9 +27,9 @@ buildGoModule (finalAttrs: {
   ldflags = [
     "-s"
     "-w"
-    "-X github.com/srl-labs/containerlab/cmd/version.Version=${finalAttrs.version}"
-    "-X github.com/srl-labs/containerlab/cmd/version.commit=${finalAttrs.src.rev}"
-    "-X github.com/srl-labs/containerlab/cmd/version.date=1970-01-01T00:00:00Z"
+    "-X github.com/srl-labs/containerlab/cmd.Version=${finalAttrs.version}"
+    "-X github.com/srl-labs/containerlab/cmd.commit=${finalAttrs.src.rev}"
+    "-X github.com/srl-labs/containerlab/cmd.date=1970-01-01T00:00:00Z"
   ];
 
   preCheck = ''
@@ -37,9 +37,10 @@ buildGoModule (finalAttrs: {
     export USER="runner"
   '';
 
-  # TestVerifyLinks wants to use docker.sock, which is not available in the Nix build environment.
+  # TestVerifyLinks wants to use docker.sock which is not available in the Nix build env
+  # KernelModulesLoaded wants to use /proc/modules which is not available in Nix build env
   checkFlags = [
-    "-skip=^TestVerifyLinks$"
+    "-skip=^TestVerifyLinks$|^TestIsKernelModuleLoaded$"
   ];
 
   postInstall = ''
