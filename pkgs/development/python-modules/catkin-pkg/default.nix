dff00dc5d5675ce846d2345faafa124f3b14d37e
Michal Sojka <michal.sojka@cvut.cz>
2025-09-30 08:15:09 +0200

    python3Packages.catkin-pkg: Address PR review comments

--- pkgs/development/python-modules/catkin-pkg/default.nix
+++ pkgs/development/python-modules/catkin-pkg/default.nix
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "ros-infrastructure";
     repo = "catkin_pkg";
-    rev = version;
+    tag = version;
     hash = "sha256-V4iurFt1WmY2jXy0A4Qa2eKMCWmR+Hs3d9pru0/zUSM=";
   };
 
@@ -28,7 +28,6 @@ buildPythonPackage rec {
     docutils
     pyparsing
     python-dateutil
-    setuptools
   ];
 
   pythonImportsCheck = [ "catkin_pkg" ];
@@ -38,7 +37,7 @@ buildPythonPackage rec {
   disabledTestPaths = [ "test/test_flake8.py" ];
 
   meta = {
-    changelog = "https://github.com/ros-infrastructure/catkin_pkg/blob/${version}/CHANGELOG.rst";
+    changelog = "https://github.com/ros-infrastructure/catkin_pkg/blob/${src.tag}/CHANGELOG.rst";
     description = "Library for retrieving information about catkin packages";
     homepage = "http://wiki.ros.org/catkin_pkg";
     license = lib.licenses.bsd3;
