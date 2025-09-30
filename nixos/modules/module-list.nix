d56a42d631d2211bae6ff3e854e15c79532d8442
Pascal Dietrich <pascal.1.dietrich@hotmail.com>
2025-09-30 15:50:28 +0200

    librepods: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -253,6 +253,7 @@
   ./programs/lazygit.nix
   ./programs/less.nix
   ./programs/liboping.nix
+  ./programs/librepods.nix
   ./programs/light.nix
   ./programs/localsend.nix
   ./programs/mdevctl.nix
