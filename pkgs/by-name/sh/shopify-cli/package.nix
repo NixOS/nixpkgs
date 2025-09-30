42e9e0fe4bbfe1b4f44e039377c74669f13d53ed
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-30 09:57:46 +0000

    shopify-cli: 3.84.2 -> 3.85.2

--- pkgs/by-name/sh/shopify-cli/package.nix
+++ pkgs/by-name/sh/shopify-cli/package.nix
@@ -5,7 +5,7 @@
   shopify-cli,
 }:
 let
-  version = "3.84.2";
+  version = "3.85.2";
 in
 buildNpmPackage {
   pname = "shopify";
@@ -13,7 +13,7 @@ buildNpmPackage {
 
   src = ./manifests;
 
-  npmDepsHash = "sha256-UW8kNTCDHJOhFuVwawXcZvmaFSDgdGWPJlK3Y/x4IMo=";
+  npmDepsHash = "sha256-a61yOMttRrzuvh/oAXBLPzxxgWNBFyGDzsISTB+Qgog=";
   dontNpmBuild = true;
 
   passthru = {
