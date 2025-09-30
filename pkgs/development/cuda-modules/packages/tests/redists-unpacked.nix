4d5226977bae14d1579eded37776680939fb7e44
Connor Baker <ConnorBaker01@gmail.com>
2025-09-30 15:28:29 -0700

    cudaPackages.tests.redists-{unpacked,installed}: include license metadata
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- pkgs/development/cuda-modules/packages/tests/redists-unpacked.nix
+++ pkgs/development/cuda-modules/packages/tests/redists-unpacked.nix
@@ -4,11 +4,24 @@
   lib,
   linkFarm,
 }:
-linkFarm "${cudaNamePrefix}-redists-unpacked" (
-  lib.concatMapAttrs (
-    name: attr:
-    lib.optionalAttrs (attr ? passthru.redistName && attr.src or null != null) {
-      ${name} = attr.src;
-    }
-  ) cudaPackages
-)
+# NOTE: Because Nixpkgs, by default, allows aliases, this derivation may contain multiple entries for a single redistributable.
+let
+  redistsForPlatform = lib.filterAttrs (
+    _: value: value ? passthru.redistName && value.src or null != null
+  ) cudaPackages;
+
+  linkedWithoutLicenses = linkFarm "${cudaNamePrefix}-redists-unpacked" (
+    lib.mapAttrs (_: drv: drv.src) redistsForPlatform
+  );
+in
+linkedWithoutLicenses.overrideAttrs (prevAttrs: {
+  passthru = prevAttrs.passthru or { } // {
+    inherit redistsForPlatform;
+  };
+
+  meta = prevAttrs.meta or { } // {
+    license = lib.unique (
+      lib.concatMap (drv: lib.toList (drv.meta.license or [ ])) (lib.attrValues redistsForPlatform)
+    );
+  };
+})
