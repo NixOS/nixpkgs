2426aa2e04dedff41b235bcbf597f75958ab49b8
R. Ryantm <ryantm-bot@ryantm.com>
2025-09-30 17:37:42 +0000

    python3Packages.oelint-data: 1.0.28 -> 1.1.0

--- pkgs/development/python-modules/oelint-data/default.nix
+++ pkgs/development/python-modules/oelint-data/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "oelint-data";
-  version = "1.0.28";
+  version = "1.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "priv-kweihmann";
     repo = "oelint-data";
     tag = version;
-    hash = "sha256-y/feC6p7Aab2ypjIW5o7IurqlvN6V9Q20xTF4fDlDj4=";
+    hash = "sha256-yqS0zYUjDg9y0nOeQ2ujQ+LGvGJpF3tuoz3GghUbrF0=";
   };
 
   build-system = [
