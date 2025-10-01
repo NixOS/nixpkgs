8c1d4bb5316ae19827b7a29dddfb645240691049
KuzushiCat <kuzushicat-nixpkgs.postwar333@passmail.net>
2025-10-01 19:30:11 +0200

    vscode-extensions.jetpack-io.devbox: init at 0.1.6

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -2540,6 +2540,8 @@ let
         };
       };
 
+      jetpack-io.devbox = callPackage ./jetpack-io.devbox { };
+
       jkillian.custom-local-formatters = buildVscodeMarketplaceExtension {
         mktplcRef = {
           publisher = "jkillian";
