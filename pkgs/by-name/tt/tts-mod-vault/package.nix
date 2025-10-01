d9364aa0a512f27e6cf15705366e9c4dc6c8a8e5
Austin Eschweiler <austin.eschweiler@gmail.com>
2025-09-30 20:23:02 -0500

    tts-mod-vault: init at 1.3.0

---
+++ pkgs/by-name/tt/tts-mod-vault/package.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  flutter329,
+  fetchFromGitHub,
+}:
+
+flutter329.buildFlutterApplication rec {
+  pname = "tts-mod-vault";
+  version = "1.3.0";
+
+  src = fetchFromGitHub {
+    owner = "markomijic";
+    repo = "TTS-Mod-Vault";
+    rev = "v${version}";
+    hash = "sha256-BTs+4QeyVJeg415uiNXww8twQwUInHfB8voWJjeVs20=";
+  };
+
+  pubspecLock = lib.importJSON ./pubspec.lock.json;
+
+  meta = {
+    description = "Download and backup assets for your Tabletop Simulator mods";
+    homepage = "https://github.com/markomijic/TTS-Mod-Vault";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ esch ];
+    mainProgram = "tts-mod-vault";
+    platforms = lib.platforms.all;
+  };
+}
