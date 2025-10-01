9ef02c9fcd5aa2988c48684eac36f6b62530a35c
Bad3r <25513724+Bad3r@users.noreply.github.com>
2025-10-01 23:39:53 +0300

    meta(electron-mail): fix description typo

--- pkgs/by-name/el/electron-mail/package.nix
+++ pkgs/by-name/el/electron-mail/package.nix
@@ -34,7 +34,7 @@ appimageTools.wrapType2 {
   passthru.updateScript = nix-update-script { };
 
   meta = {
-    description = "Unofficial Election-based ProtonMail desktop client";
+    description = "Unofficial Electron-based ProtonMail desktop client";
     mainProgram = "electron-mail";
     homepage = "https://github.com/vladimiry/ElectronMail";
     license = lib.licenses.gpl3;
