5a0c427b25aa712de7231a043d8fd56314a18821
akotro <mail@akotro.dev>
2025-10-01 18:09:03 +0100

    maintainers: add mikeodr

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16848,6 +16848,12 @@
     githubId = 123373126;
     name = "Mike Horn";
   };
+  mikeodr = {
+    email = "mike@unusedbytes.ca";
+    github = "mikeodr";
+    githubId = 61757;
+    name = "Mike O'Driscoll";
+  };
   mikesperber = {
     email = "sperber@deinprogramm.de";
     github = "mikesperber";
