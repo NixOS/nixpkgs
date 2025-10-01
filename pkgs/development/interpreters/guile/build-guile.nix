0b27fec6cca8969b4e4c90754681a7b2fcfec3dd
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-10-01 19:35:43 -0400

    guile_2_0: Move to its own directory

--- pkgs/development/interpreters/guile/build-guile.nix
+++ pkgs/development/interpreters/guile/build-guile.nix
@@ -1,13 +1,34 @@
-{ lib, stdenv, fetchurl, pkgsBuildBuild, gawk, gmp, libtool, pkg-config
-, readline, }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  pkgsBuildBuild,
+  gawk,
+  gmp,
+  libtool,
+  pkg-config,
+  readline,
+  libunistring,
+  boehmgc,
+  libffi,
+}:
 
 lib.extendMkDerivation {
   constructDrv = stdenv.mkDerivation;
 
-  excludeDrvArgNames = [ "maintainers" "srcHash" ];
+  excludeDrvArgNames = [
+    "maintainers"
+    "srcHash"
+  ];
 
-  extendDrvArgs = finalAttrs:
-    { version ? "", srcHash ? "", maintainers ? [ ], ... }@args: {
+  extendDrvArgs =
+    finalAttrs:
+    {
+      version ? "",
+      srcHash ? "",
+      ...
+    }@args:
+    {
       inherit version;
       pname = args.pname or "guile";
 
@@ -16,38 +37,96 @@ lib.extendMkDerivation {
         hash = srcHash;
       };
 
-      outputs = args.outputs or [ "out" "dev" "info" ];
+      patches =
+        args.patches or [ ]
+        ++ lib.optionals (lib.versionAtLeast finalAttrs.version "2.0.0") [
+          ./2.0/eai_system.patch
+        ];
 
-      setOutputFlags =
-        args.setOutputFlags or false; # $dev gets into the library otherwise
+      outputs =
+        args.outputs or [
+          "out"
+          "dev"
+          "info"
+        ];
+
+      setOutputFlags = args.setOutputFlags or false; # $dev gets into the library otherwise
 
       strictDeps = true;
-      depsBuildBuild = args.depsBuildBuild or [ ]
-        ++ [ pkgsBuildBuild.stdenv.cc ];
-
-      nativeBuildInputs = args.nativeBuildInputs or [ ] ++ [ pkg-config ];
-
-      buildInputs = args.buildInputs or [ ] ++ [ libtool readline ];
-
-      propagatedBuildInputs = args.propagatedBuildInputs or [ ] ++ [
-        gmp
-
-        # XXX: These ones aren't normally needed here, but `libguile*.la' has '-l'
-        # flags for them without corresponding '-L' flags. Adding them here will add
-        # the needed `-L' flags.  As for why the `.la' file lacks the `-L' flags,
-        # see below.
-        libtool
+      depsBuildBuild = args.depsBuildBuild or [ ] ++ [
+        pkgsBuildBuild.stdenv.cc
       ];
 
+      nativeBuildInputs = args.nativeBuildInputs or [ ] ++ [
+        pkg-config
+      ];
+
+      buildInputs =
+        args.buildInputs or [ ]
+        ++ [
+          libtool
+          readline
+        ]
+        ++ lib.optionals (lib.versionAtLeast finalAttrs.version "2.0.0") [
+          libunistring
+          libffi
+        ];
+
+      propagatedBuildInputs =
+        args.propagatedBuildInputs or [ ]
+        ++ [
+          gmp
+
+          # XXX: These ones aren't normally needed here, but `libguile*.la' has '-l'
+          # flags for them without corresponding '-L' flags. Adding them here will add
+          # the needed `-L' flags.  As for why the `.la' file lacks the `-L' flags,
+          # see below.
+          libtool
+        ]
+        ++ lib.optionals (lib.versionAtLeast finalAttrs.version "2.0.0") [
+          boehmgc
+          libunistring
+        ];
+
+      env =
+        args.env or { }
+        //
+          lib.optionalAttrs
+            (
+              (lib.versionAtLeast finalAttrs.version "2.0.0") && stdenv.cc.isGNU && !stdenv.hostPlatform.isStatic
+            )
+            {
+              # Explicitly link against libgcc_s, to work around the infamous
+              # "libgcc_s.so.1 must be installed for pthread_cancel to work".
+
+              # don't have "libgcc_s.so.1" on clang
+              LDFLAGS = "-lgcc_s";
+            };
+
       # GCC 4.6 raises a number of set-but-unused warnings.
-      configureFlags = args.configureFlags or [ ] ++ [
-        "--disable-error-on-warning"
-        "AWK=${lib.getExe gawk}"
-      ]
-      # Guile needs patching to preset results for the configure tests about
-      # pthreads, which work only in native builds.
-        ++ lib.optional (stdenv.hostPlatform != stdenv.buildPlatform)
-        "--with-threads=no";
+      configureFlags =
+        args.configureFlags or [ ]
+        ++ [
+          "--disable-error-on-warning"
+          "AWK=${lib.getExe gawk}"
+        ]
+        # Guile needs patching to preset results for the configure tests about
+        # pthreads, which work only in native builds.
+        ++ lib.optional (stdenv.hostPlatform != stdenv.buildPlatform) "--with-threads=no"
+        ++ lib.optionals stdenv.hostPlatform.isSunOS [
+          # Make sure the right <gmp.h> is found, and not the incompatible
+          # /usr/include/mp.h from OpenSolaris. See
+          # <https://lists.gnu.org/archive/html/hydra-users/2012-08/msg00000.html>
+          # for details.
+          "--with-libgmp-prefix=${lib.getDev gmp}"
+
+          # Same for these (?).
+          "--with-libreadline-prefix=${lib.getDev readline}"
+          "--with-libunistring-prefix=${libunistring}"
+
+          # See below.
+          "--without-threads"
+        ];
 
       # One test fails.
       # ERROR: file: "libtest-asmobs", message: "file not found"
@@ -75,7 +154,6 @@ lib.extendMkDerivation {
           foreign function call interface, and powerful string processing.
         '';
         license = lib.licenses.lgpl3Plus;
-        maintainers = maintainers;
         platforms = lib.platforms.all;
       };
     };
