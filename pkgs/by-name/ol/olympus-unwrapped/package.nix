220b54afedb8c0241bb48a010a03550358451e7b
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-01 16:04:32 +0000

    olympus-unwrapped: 25.09.09.02 -> 25.09.30.01

--- pkgs/by-name/ol/olympus-unwrapped/package.nix
+++ pkgs/by-name/ol/olympus-unwrapped/package.nix
@@ -31,9 +31,9 @@ let
 
   phome = "$out/lib/olympus";
   # The following variables are to be updated by the update script.
-  version = "25.09.09.02";
-  buildId = "5131"; # IMPORTANT: This line is matched with regex in update.sh.
-  rev = "fa3f1a9aaca5e57bfa6d2e18a0a8688c7608b747";
+  version = "25.09.30.01";
+  buildId = "5195"; # IMPORTANT: This line is matched with regex in update.sh.
+  rev = "f377dc2703a08b0871208a56a87b1796bf41b50d";
 in
 buildDotnetModule {
   pname = "olympus-unwrapped";
@@ -44,7 +44,7 @@ buildDotnetModule {
     owner = "EverestAPI";
     repo = "Olympus";
     fetchSubmodules = true; # Required. See upstream's README.
-    hash = "sha256-MJzrQDMsnbjua76twT+swZ1fMiRqokqlxg0ce1fnvLU=";
+    hash = "sha256-eE7DU4hUlawza7Q0CD3U7QQrw4T8RnT0todJz6UIZlM=";
   };
 
   nativeBuildInputs = [
