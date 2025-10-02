0b6cd268525c9eeb8e0f35bf9f1f8037b7e0580e
kyehn <kyehn@users.noreply.github.com>
2025-10-02 22:56:35 +0000

    sqlpage: 0.37.0 -> 0.37.1
    Diff: https://github.com/lovasoa/SQLpage/compare/v0.37.0...v0.37.1
    
    Changelog: https://github.com/lovasoa/SQLpage/blob/refs/tags/v0.37.1/CHANGELOG.md

--- pkgs/by-name/sq/sqlpage/package.nix
+++ pkgs/by-name/sq/sqlpage/package.nix
@@ -41,13 +41,13 @@ in
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sqlpage";
-  version = "0.37.0";
+  version = "0.37.1";
 
   src = fetchFromGitHub {
     owner = "lovasoa";
     repo = "SQLpage";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-YL6JZ+efQr66Z61oK4Ah0CA7DygaFhc/lrby/IIK0bk=";
+    hash = "sha256-YYrOBSbvr520uBBvmNmPYR3xRtQLK5Kvtirgly45zQ0=";
   };
 
   postPatch = ''
@@ -72,7 +72,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
       "$(cat ${tomselect})"
   '';
 
-  cargoHash = "sha256-p0qf0uryowkofVTUb/OcBpqfrebEFLcVeylarRdzVak=";
+  cargoHash = "sha256-Hof+l6uiPukNtWGTxCB8wN3xRStJPhGwqrrEFgcAcQs=";
 
   nativeBuildInputs = [ pkg-config ];
 
@@ -82,6 +82,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
   ];
 
   env.ZSTD_SYS_USE_PKG_CONFIG = true;
+
+  __darwinAllowLocalNetworking = true;
 
   meta = {
     description = "SQL-only webapp builder, empowering data analysts to build websites and applications quickly";
