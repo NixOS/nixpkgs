61d9ae8c00ef923aa623e594752b2ff8218bc37f
Moinak Bhattacharyya <moinakb001@gmail.com>
2025-10-02 06:45:04 -0500

    maintainers: add moinakb001

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17230,6 +17230,11 @@
     github = "mohe2015";
     githubId = 13287984;
   };
+  moinakb001 = {
+    name = "Moinak Bhattacharyya";
+    github = "moinakb001";
+    githubId = 3363259;
+  };
   momeemt = {
     name = "Mutsuha Asada";
     email = "me@momee.mt";
