ab367931b1b85a9081f864c305b336ddab28107d
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-02 01:44:18 +0000

    httptoolkit-server: 1.22.1 -> 1.23.0

--- pkgs/by-name/ht/httptoolkit-server/package.nix
+++ pkgs/by-name/ht/httptoolkit-server/package.nix
@@ -16,13 +16,13 @@ let
   nodejs = nodejs_20;
   buildNpmPackage' = buildNpmPackage.override { inherit nodejs; };
 
-  version = "1.22.1";
+  version = "1.23.0";
 
   src = fetchFromGitHub {
     owner = "httptoolkit";
     repo = "httptoolkit-server";
     tag = "v${version}";
-    hash = "sha256-6mvbR9WIjVrulDdFtW0XSfxPwKSaFK1fFIhbglS7Gq4=";
+    hash = "sha256-rcjgV71nu1Id4CQAfB9r0583qXAFk3eqDbdPWgqhtuw=";
   };
 
   overridesNodeModules = buildNpmPackage' {
@@ -102,7 +102,7 @@ buildNpmPackage' {
 
   patches = [ ./only-build-for-one-platform.patch ];
 
-  npmDepsHash = "sha256-z9LLZBt7CR3iCNsGhRboHgaaoH6MgmSrzasgwIX+iwk=";
+  npmDepsHash = "sha256-9Vy7i2L0yc8BdmoJvGr2g+OCwpdQUC/zVCUU7FcyCtk=";
 
   npmFlags = [ "--ignore-scripts" ];
 
