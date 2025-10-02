d576f7df7a19edc3d0c54765d712980ca5a29f41
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-10-01 22:27:25 -0400

    guile_3_0: Move to its own directory

--- pkgs/development/interpreters/guile/packages.nix
+++ pkgs/development/interpreters/guile/packages.nix
@@ -7,5 +7,5 @@ lib.makeScope newScope (self: {
   guile_1_8 = self.callPackage ./1.8 { };
   guile_2_0 = self.callPackage ./2.0 { };
   guile_2_2 = self.callPackage ./2.2 { };
-  guile_3_0 = self.callPackage ./3.0.nix { };
+  guile_3_0 = self.callPackage ./3.0 { };
 })
