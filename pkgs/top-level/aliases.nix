33454e20327ba1ca26f6bff96a33a470cc66c157
Michael Daniels <mdaniels5757@gmail.com>
2025-10-01 20:36:41 -0400

    sipwitch: drop
    Fails to build, has been marked broken for at least a full release cycle.
    
    Dropping per RFC 180.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2337,6 +2337,7 @@ mapAliases {
   silc_client = throw "'silc_client' has been removed because it is unmaintained"; # Added 2025-05-12
   siproxd = throw "'siproxd' has been removed as it was unmaintained and incompatible with newer libosip versions"; # Added 2025-05-18
   sisco.lv2 = throw "'sisco.lv2' has been removed as it was unmaintained and broken"; # Added 2025-08-26
+  sipwitch = throw "'sipwitch' has been removed because it has been marked as broken since at least November 2024."; # Added 2025-10-01
   sheesy-cli = throw "'sheesy-cli' has been removed due to lack of upstream maintenance"; # Added 2025-01-26
   shout = nodePackages.shout; # Added unknown; moved 2024-10-19
   sky = throw "'sky' has been removed because its upstream website disappeared"; # Added 2024-07-21
