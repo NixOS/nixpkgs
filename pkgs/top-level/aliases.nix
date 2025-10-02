4d0d5923df6d7379846488b9f93635c65af6cc6d
Michael Daniels <mdaniels5757@gmail.com>
2025-10-01 21:02:29 -0400

    waitron: drop
    Has been marked broken for at least a full release cycle.
    
    Dropping per RFC 180.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2653,6 +2653,7 @@ mapAliases {
 
   ### W ###
   w_scan = throw "'w_scan' has been removed due to lack of upstream maintenance"; # Added 2025-08-29
+  waitron = throw "'waitron' has been removed because it has been marked as broken since at least November 2024."; # Added 2025-10-01
   wakatime = wakatime-cli; # 2024-05-30
   wavm = throw "wavm has been removed, as it does not build with supported LLVM versions"; # Added 2025-08-10
   wal_e = throw "wal_e was removed as it is unmaintained upstream and depends on the removed boto package; upstream recommends using wal-g or pgbackrest"; # Added 2024-09-22
