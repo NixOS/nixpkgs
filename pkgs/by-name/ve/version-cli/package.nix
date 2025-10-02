4fa47ab880a2dbb4567bb6a89a8e96529fa451bf
AsciiMoth <ascii@moth.contact>
2025-10-02 18:05:47 +0400

    version-cli: switch from using rec

--- pkgs/by-name/ve/version-cli/package.nix
+++ pkgs/by-name/ve/version-cli/package.nix
@@ -4,33 +4,39 @@
   fetchFromGitHub,
   installShellFiles,
 }:
-buildGoModule rec {
-  pname = "version-cli";
-  version = "0.1.0";
+buildGoModule (
+  finalAttrs:
+  let
+    ver = "0.1.0";
+  in
+  {
+    pname = "version-cli";
+    version = ver;
 
-  src = fetchFromGitHub {
-    owner = "asciimoth";
-    repo = "version";
-    rev = "v${version}";
-    hash = "sha256-TOmShw4nRyTWv2F0/p9X5WHjLSUz/dYCkp6/+NrILGo=";
-  };
+    src = fetchFromGitHub {
+      owner = "asciimoth";
+      repo = "version";
+      rev = "v${ver}";
+      hash = "sha256-TOmShw4nRyTWv2F0/p9X5WHjLSUz/dYCkp6/+NrILGo=";
+    };
 
-  vendorHash = "sha256-TGrtVY1ut76K2J/dpt3OvuXaOkAAnxFTYVl1aTii+AI=";
+    vendorHash = "sha256-TGrtVY1ut76K2J/dpt3OvuXaOkAAnxFTYVl1aTii+AI=";
 
-  nativeBuildInputs = [ installShellFiles ];
+    nativeBuildInputs = [ installShellFiles ];
 
-  postInstall = ''
-    installManPage man/version.1
-  '';
+    postInstall = ''
+      installManPage man/version.1
+    '';
 
-  meta = {
-    description = "Multi-source semantic version management tool";
-    homepage = "https://github.com/asciimoth/version";
-    maintainers = with lib.maintainers; [ asciimoth ];
-    license = with lib.licenses; [
-      mit
-      asl20
-    ];
-    mainProgram = "version";
-  };
-}
+    meta = {
+      description = "Multi-source semantic version management tool";
+      homepage = "https://github.com/asciimoth/version";
+      maintainers = with lib.maintainers; [ asciimoth ];
+      license = with lib.licenses; [
+        mit
+        asl20
+      ];
+      mainProgram = "version";
+    };
+  }
+)
