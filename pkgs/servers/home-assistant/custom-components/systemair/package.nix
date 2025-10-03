0d48bd089a3305b2db8076189005d4aff0fa7c1c
Nikita Uvarov <uv.nikita@gmail.com>
2025-10-03 10:19:12 +0200

    home-assistant-custom-components.systemair: 0.2.0 -> 1.0.6
    Switch to active fork by AN3Orik since original repository is stale.

--- pkgs/servers/home-assistant/custom-components/systemair/package.nix
+++ pkgs/servers/home-assistant/custom-components/systemair/package.nix
@@ -1,23 +1,28 @@
 {
   lib,
+  pymodbus,
   buildHomeAssistantComponent,
   fetchFromGitHub,
 }:
 
 buildHomeAssistantComponent rec {
-  owner = "tesharp";
+  owner = "AN3Orik";
   domain = "systemair";
-  version = "0.2.0";
+  version = "1.0.6";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "systemair";
     tag = "v${version}";
-    hash = "sha256-lzFnKPkBOt2fkVGWCj1M/skSr8V39GgDHS+0HD4ACAw=";
+    hash = "sha256-eRRg9V/AlAGTnlvJFUPXilpwyRpE6XAHphJ0qXsTLxU=";
   };
 
+  dependencies = [
+    pymodbus
+  ];
+
   meta = with lib; {
-    changelog = "https://github.com/tesharp/systemair/releases/tag/v${version}";
+    changelog = "https://github.com/AN3Orik/systemair/releases/tag/v${version}";
     description = "Home Assistant component for Systemair SAVE Connect 2";
     homepage = "https://github.com/tesharp/systemair";
     maintainers = with maintainers; [ uvnikita ];
