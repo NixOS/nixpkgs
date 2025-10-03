df1a1e6fa2d58df22d792264b653f9376597c2f6
Connor Baker <ConnorBaker01@gmail.com>
2025-10-03 10:12:42 -0700

    cudaPackages.libcudla: only available on Xavier and Orin
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- pkgs/development/cuda-modules/packages/libcudla.nix
+++ pkgs/development/cuda-modules/packages/libcudla.nix
@@ -18,4 +18,16 @@ buildRedist {
   autoPatchelfIgnoreMissingDeps = lib.optionals backendStdenv.hasJetsonCudaCapability [
     "libnvcudla.so"
   ];
+
+  passthru.platformAssertions = [
+    {
+      message = "Only Xavier (7.2) and Orin (8.7) Jetson devices are supported";
+      assertion =
+        let
+          inherit (backendStdenv) hasJetsonCudaCapability requestedJetsonCudaCapabilities;
+        in
+        hasJetsonCudaCapability
+        -> (lib.subtractLists [ "7.2" "8.7" ] requestedJetsonCudaCapabilities == [ ]);
+    }
+  ];
 }
