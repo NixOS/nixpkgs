344edf9368ca4e1807c7180301d1f3160d370176
Roman Balashov <ubermailbox@protonmail.ch>
2025-10-03 19:13:04 +0000

    vscode-extensions.pranaygp.vscode-css-peek: init at 4.4.3

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -3764,6 +3764,8 @@ let
 
       prince781.vala = callPackage ./prince781.vala { };
 
+      pranaygp.vscode-css-peek = callPackage ./pranaygp.vscode-css-peek { };
+
       prisma.prisma = buildVscodeMarketplaceExtension {
         mktplcRef = {
           name = "prisma";
