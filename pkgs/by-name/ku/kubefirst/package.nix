2024bf1823a8455395981c2cc8f0867caed4318b
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-03 10:29:32 +0000

    kubefirst: 2.9.1 -> 2.10.1

--- pkgs/by-name/ku/kubefirst/package.nix
+++ pkgs/by-name/ku/kubefirst/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "kubefirst";
-  version = "2.9.1";
+  version = "2.10.1";
 
   src = fetchFromGitHub {
     owner = "konstructio";
     repo = "kubefirst";
     tag = "v${version}";
-    hash = "sha256-llWJTBI0MBVDG2zgersQM7F5RM5ZhpbkF67QxcSeAU0=";
+    hash = "sha256-/DmYPxnqAoCPXjkaYTj1iYliNfqyqrDrChU6fCbbCkk=";
   };
 
   vendorHash = "sha256-1u34cuPUY/5fYd073UhRUu/5/1nhPadTI06+3o+uE7w=";
