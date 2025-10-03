30bb461dd7750703c1c1336820fbb33eb3df257c
David McFarland <corngood@gmail.com>
2025-10-03 12:02:39 -0300

    cygwin: add hook to create executable links without .exe extension

--- pkgs/stdenv/generic/make-derivation.nix
+++ pkgs/stdenv/generic/make-derivation.nix
@@ -475,7 +475,10 @@ let
           nativeBuildInputs
           ++ optional separateDebugInfo' ../../build-support/setup-hooks/separate-debug-info.sh
           ++ optional isWindows ../../build-support/setup-hooks/win-dll-link.sh
-          ++ optional isCygwin ../../build-support/setup-hooks/cygwin-dll-link.sh
+          ++ optionals isCygwin [
+            ../../build-support/setup-hooks/cygwin-dll-link.sh
+            ../../os-specific/cygwin/create-exe-links.sh
+          ]
           ++ optionals doCheck nativeCheckInputs
           ++ optionals doInstallCheck nativeInstallCheckInputs;
 
