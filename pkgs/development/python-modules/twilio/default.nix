84de5e65e8ca8ddcd2a094fbce5e8cdf7e8be026
Fabian Affolter <fabian@affolter-engineering.ch>
2025-10-04 01:09:50 +0200

    python313Packages.twilio: 9.8.2 -> 9.8.3
    Diff: https://github.com/twilio/twilio-python/compare/9.8.2...9.8.3
    
    Changelog: https://github.com/twilio/twilio-python/blob/9.8.3/CHANGES.md

--- pkgs/development/python-modules/twilio/default.nix
+++ pkgs/development/python-modules/twilio/default.nix
@@ -21,7 +21,7 @@
 
 buildPythonPackage rec {
   pname = "twilio";
-  version = "9.8.2";
+  version = "9.8.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = "twilio";
     repo = "twilio-python";
     tag = version;
-    hash = "sha256-ZOkXJ1NN3qmHABfOBpR1tECCIfAwwVMGyLBbU1jYowc=";
+    hash = "sha256-bmDgzlI9W61uHbQHkaQ3VKu2Fwbr+y0N5Tec2AzjNBE=";
   };
 
   build-system = [ setuptools ];
