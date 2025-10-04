d76caee9e9c7ad2eb88d924de95e85998da96395
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-04 22:51:03 +0000

    python3Packages.python-engineio: 4.12.2 -> 4.12.3

--- pkgs/development/python-modules/python-engineio/default.nix
+++ pkgs/development/python-modules/python-engineio/default.nix
@@ -20,7 +20,7 @@
 
 buildPythonPackage rec {
   pname = "python-engineio";
-  version = "4.12.2";
+  version = "4.12.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -29,7 +29,7 @@ buildPythonPackage rec {
     owner = "miguelgrinberg";
     repo = "python-engineio";
     tag = "v${version}";
-    hash = "sha256-VgdqVzO3UToXpD9pMEDqAH2DfdBwaWUfulALAEG2DNs=";
+    hash = "sha256-VcL8Od1EM/cbbeOVyXlsXYt8Bms636XbtunrTblkGDQ=";
   };
 
   build-system = [ setuptools ];
