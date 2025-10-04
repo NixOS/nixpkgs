21e3fa9df429a4d8dd8018dab0a132627512e6f9
Thomas Gerbet <thomas@gerbet.me>
2025-10-04 15:55:30 +0200

    poco: apply patch for CVE-2025-6375, bump vendored expat to 2.7.1
    Not backported the upgrade to 1.14.2 #443831 since the
    changelog mention a breaking change.
    
    Not-cherry-picked-because: 1.14.2 changelog mention a breaking change

--- pkgs/by-name/po/poco/package.nix
+++ pkgs/by-name/po/poco/package.nix
@@ -67,6 +67,21 @@ stdenv.mkDerivation rec {
       url = "https://patch-diff.githubusercontent.com/raw/pocoproject/poco/pull/4879.patch";
       hash = "sha256-VFWuRuf0GPYFp43WKI8utl+agP+7a5biLg7m64EMnVo=";
     })
+    (fetchpatch {
+      name = "bump-vendored-expat-2.7.0.patch";
+      url = "https://github.com/pocoproject/poco/commit/1bdc41329b1629ed5c7bb55f1ba0fde452a7b8ec.patch";
+      hash = "sha256-azK6NhTh6PYaiJvXSj6w1YTTcCJVBnk0Yc9f6AatC70=";
+    })
+    (fetchpatch {
+      name = "bump-vendored-expat-2.7.1.patch";
+      url = "https://github.com/pocoproject/poco/commit/ddecf77d1cce1035ed1a99f560f92125a309f477.patch";
+      hash = "sha256-rjYWGGttQOxhkd+WwE2ZOl6t439nTvRbZaY2ATjbr+c=";
+    })
+    (fetchpatch {
+      name = "CVE-2025-6375.patch";
+      url = "https://github.com/pocoproject/poco/commit/da7fcba551a06c4f91a5e06ba983a6afc2480ae6.patch";
+      hash = "sha256-WYQ5MYHE2iBbstamWKmCQaZdK8DxeklEhCFH7mlHYiI=";
+    })
   ];
 
   postFixup = ''
