6e8e1f05ca35850501420cc639825a3447e70d69
ZHAO Jin-Xiang <xiaoxiangmoe@gmail.com>
2025-10-05 00:56:11 +0800

    bitwarden-cli: 2025.5.0 -> 2025.9.0

--- pkgs/by-name/bi/bitwarden-cli/package.nix
+++ pkgs/by-name/bi/bitwarden-cli/package.nix
@@ -2,37 +2,38 @@
   lib,
   stdenv,
   buildNpmPackage,
-  nodejs_20,
   fetchFromGitHub,
-  cctools,
-  nix-update-script,
-  nixosTests,
   perl,
   xcbuild,
+  writableTmpDirAsHomeHook,
+  versionCheckHook,
+  nixosTests,
+  nix-update-script,
 }:
 
-buildNpmPackage rec {
+buildNpmPackage (finalAttrs: {
   pname = "bitwarden-cli";
-  version = "2025.5.0";
+  version = "2025.9.0";
 
   src = fetchFromGitHub {
     owner = "bitwarden";
     repo = "clients";
-    tag = "cli-v${version}";
-    hash = "sha256-8jVKwqKhTfhur226SER4sb1i4dY+TjJRYmOY8YtO6CY=";
+    tag = "cli-v${finalAttrs.version}";
+    hash = "sha256-vxGyDYtv0O5U4pnVrQm/BOIpDtpcDUOyFFdBDehQ2to=";
   };
+
+  patches = [
+    ./patches/fix-lockfile.patch
+  ];
 
   postPatch = ''
     # remove code under unfree license
     rm -r bitwarden_license
   '';
 
-  nodejs = nodejs_20;
-
-  npmDepsHash = "sha256-0IoBPRGdtkMeTrT5cqZLHB/WrUCONtsJ6YHh0y4K5Ls=";
+  npmDepsHash = "sha256-bn39QlZXNUa/GEZhJsjLiG3PRYdQ/Y36Tvef2fXH8yQ=";
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    cctools
     perl
     xcbuild.xcrun
   ];
@@ -61,9 +62,6 @@ buildNpmPackage rec {
     rm -r node_modules/**/prebuilds
     shopt -u globstar
 
-    # FIXME one of the esbuild versions fails to download @esbuild/linux-x64
-    rm -r node_modules/esbuild node_modules/vite/node_modules/esbuild
-
     npm rebuild --verbose
   '';
 
@@ -84,6 +82,14 @@ buildNpmPackage rec {
     installShellCompletion --cmd bw --zsh <($out/bin/bw completion --shell zsh)
   '';
 
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [
+    writableTmpDirAsHomeHook
+    versionCheckHook
+  ];
+  versionCheckKeepEnvironment = [ "HOME" ];
+  versionCheckProgramArg = "--version";
+
   passthru = {
     tests = {
       vaultwarden = nixosTests.vaultwarden.sqlite;
@@ -97,9 +103,7 @@ buildNpmPackage rec {
   };
 
   meta = {
-    # https://github.com/NixOS/nixpkgs/issues/339576
-    broken = stdenv.hostPlatform.isDarwin;
-    changelog = "https://github.com/bitwarden/clients/releases/tag/${src.tag}";
+    changelog = "https://github.com/bitwarden/clients/releases/tag/${finalAttrs.src.tag}";
     description = "Secure and free password manager for all of your devices";
     homepage = "https://bitwarden.com";
     license = lib.licenses.gpl3Only;
@@ -109,4 +113,4 @@ buildNpmPackage rec {
       dotlambda
     ];
   };
-}
+})
