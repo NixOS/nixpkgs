56439c36cafeb8bf17c4a6d1167e5b9dea2d1bfd
Luna Nova <git@lunnova.dev>
2025-10-04 03:22:54 -0700

    basalt-monado: 0-unstable-2024-06-21 -> 0-unstable-2025-09-25
    bumping to new unstable rev for CMake 4 compat
    
    CMake 4 compat tracking issue: #445447

--- pkgs/by-name/ba/basalt-monado/package.nix
+++ pkgs/by-name/ba/basalt-monado/package.nix
@@ -27,14 +27,14 @@
 }:
 stdenv.mkDerivation {
   pname = "basalt-monado";
-  version = "0-unstable-2024-06-21";
+  version = "0-unstable-2025-09-25";
 
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "mateosss";
     repo = "basalt";
-    rev = "385c161f35720df3a6c606054565f9d49a1c5787";
-    hash = "sha256-+2/pc2OWDwE04xPcfHL5GGyhQ1ZTN6o7cCNAilDgd2Y=";
+    rev = "5337898271f5c2ce523258e93e80fd870130be31";
+    hash = "sha256-IoXZlXyOc5y9aSHBU3WCNhHi4L9xzHmbv6VMEvX2ZeE=";
     fetchSubmodules = true;
   };
 
