764be46fada77c6e5d3c6a1f91d66afb2c8fdbcd
Tungsten842 <886724vf@anonaddy.me>
2025-10-04 11:07:58 +0200

    nixos/fstrim: add Persistent and RandomizedDelaySec

--- nixos/modules/services/misc/fstrim.nix
+++ nixos/modules/services/misc/fstrim.nix
@@ -32,6 +32,35 @@ in
           {manpage}`systemd.time(7)`.
         '';
       };
+
+      persistent = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        example = false;
+        description = ''
+          Takes a boolean argument. If true, the time when the service
+          unit was last triggered is stored on disk. When the timer is
+          activated, the service unit is triggered immediately if it
+          would have been triggered at least once during the time when
+          the timer was inactive. Such triggering is nonetheless
+          subject to the delay imposed by RandomizedDelaySec=. This is
+          useful to catch up on missed runs of the service when the
+          system was powered down.
+        '';
+      };
+
+      randomizedDelaySec = lib.mkOption {
+        default = "0";
+        type = lib.types.singleLineStr;
+        example = "45min";
+        description = ''
+          Add a randomized delay before each fstrim operation.
+          The delay will be chosen between zero and this value.
+          This value must be a time span in the format specified by
+          {manpage}`systemd.time(7)`
+        '';
+      };
+
     };
 
   };
@@ -46,6 +75,8 @@ in
           ""
           cfg.interval
         ];
+        Persistent = cfg.persistent;
+        RandomizedDelaySec = cfg.randomizedDelaySec;
       };
       wantedBy = [ "timers.target" ];
     };
