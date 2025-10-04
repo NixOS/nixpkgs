a01f617fd54cd5ce8d0c2b7bfef8a2669520d9cb
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-04 04:27:11 +0000

    doctl: 1.142.0 -> 1.145.0

--- pkgs/by-name/do/doctl/package.nix
+++ pkgs/by-name/do/doctl/package.nix
@@ -9,7 +9,7 @@
 
 buildGoModule rec {
   pname = "doctl";
-  version = "1.142.0";
+  version = "1.145.0";
 
   vendorHash = null;
 
@@ -42,7 +42,7 @@ buildGoModule rec {
     owner = "digitalocean";
     repo = "doctl";
     tag = "v${version}";
-    hash = "sha256-wbOO9jdGfy39GeGEwLURLpxhXt17sk6gQRUbz6ysSXc=";
+    hash = "sha256-JeIpx+i1JDVfJqCKTZMTJ7rDMGO4yA/eR56C3qr08zg=";
   };
 
   meta = {
