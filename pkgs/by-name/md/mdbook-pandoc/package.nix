f7baef52b1512f92b2e6d32848269690950ab5f4
Astro <astro@spaceboyz.net>
2025-10-04 20:00:56 +0200

    mdbook-pandoc: 0.6.4 -> 0.10.5

--- pkgs/by-name/md/mdbook-pandoc/package.nix
+++ pkgs/by-name/md/mdbook-pandoc/package.nix
@@ -2,6 +2,7 @@
   lib,
   callPackage,
   fetchFromGitHub,
+  stdenv,
   makeWrapper,
   pandoc,
   rustPlatform,
@@ -11,16 +12,16 @@
 let
   self = rustPlatform.buildRustPackage {
     pname = "mdbook-pandoc";
-    version = "0.6.4";
+    version = "0.10.5";
 
     src = fetchFromGitHub {
       owner = "max-heller";
       repo = "mdbook-pandoc";
-      rev= "v${self.version}";
-      hash = "sha256-WaRcNfVfex9ujZ1fD8NhQ72phUh4WgyZtI3zuZ63Koc=";
+      rev = "v${self.version}";
+      hash = "sha256-ihKju9XXJy4JciuMLw4EcKhqSQjrBiUJDG0Rd5DbFdk=";
     };
 
-    cargoHash = "sha256-XT/YkR8kRJ30bCXmfxKQJQL7tPl+eAPr4jIhmStJ8TM=";
+    cargoHash = "sha256-SXXzGOBvfyLYhed5EMFUCzkFWoGEMM73PD3uWjkUcic=";
 
     nativeBuildInputs = [ makeWrapper ];
 
@@ -30,24 +31,31 @@ let
       texliveSmall
     ];
 
-    checkFlags = let
-      skippedTests = [
-      # require network access
-      "remote_images"
-      "cargo_book"
-      "mdbook_guide"
-      "nomicon"
-      "rust_book"
-      "rust_by_example"
-      "rust_edition_guide"
-      "rust_embedded"
-      "rust_reference"
-      "rustc_dev_guide"
-      # failed because pandoc
-      "code_block_with_very_long_line"
-      "code_block_with_very_long_line_with_special_characters"
-      ];
-    in
+    checkFlags =
+      let
+        skippedTests = [
+          # require network access
+          "remote_images"
+          "cargo_book"
+          "mdbook_guide"
+          "nomicon"
+          "rust_book"
+          "rust_by_example"
+          "rust_edition_guide"
+          "rust_embedded"
+          "rust_reference"
+          "rustc_dev_guide"
+          # failed because pandoc
+          "code_block_with_very_long_line"
+          "code_block_with_very_long_line_with_special_characters"
+          "tests::css::css"
+          "tests::definition_lists::dt_attributes"
+          "tests::html::attach_id_to_div_of_stripped_html_elements"
+          "tests::html::link_to_element_by_id"
+          "tests::images::images"
+        ]
+        ++ lib.optional stdenv.buildPlatform.isDarwin "pandoc::tests::five_item_deep_list";
+      in
       builtins.map (x: "--skip " + x) skippedTests;
 
     passthru = {
@@ -60,10 +68,13 @@ let
       changelog = "https://github.com/max-heller/mdbook-pandoc/releases/tag/${self.src.rev}";
       license = with lib.licenses; [
         asl20
-        /* or */
+        # or
         mit
       ];
-      maintainers = with lib.maintainers; [ AndersonTorres ];
+      maintainers = with lib.maintainers; [
+        AndersonTorres
+        astro
+      ];
     };
   };
 in
