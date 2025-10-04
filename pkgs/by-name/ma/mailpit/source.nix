94dd2c7894cbbd6f913a280c393a12132882b46f
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-04 18:32:10 +0000

    mailpit: 1.27.8 -> 1.27.9

--- pkgs/by-name/ma/mailpit/source.nix
+++ pkgs/by-name/ma/mailpit/source.nix
@@ -1,6 +1,6 @@
 {
-  version = "1.27.8";
-  hash = "sha256-aGrwcwzNYXW2qJsS4j8nHsKt+fCgjQBpfp97hPMVw6Y=";
-  npmDepsHash = "sha256-+oCX8XM3+ZZkwasOkiQbh4nV2c0Iv2eoctt3u8huiKE=";
-  vendorHash = "sha256-zFYNl8V1Xkqgpyn1e1IL/LHs3eVelKzhAWEWtQHNxJ8=";
+  version = "1.27.9";
+  hash = "sha256-COPNi7AoHYyY8pfTSJYKxUG2Mh08czBjiD0VzLu0V6I=";
+  npmDepsHash = "sha256-QEjfCHqE8r41fgylNRY5Gk0tYQSYuFxHrHT6/7vtLxg=";
+  vendorHash = "sha256-Nmupbw8ouxsc7/CEAWz4Cj0cyEMP4WFPZ+P5ornf1AI=";
 }
