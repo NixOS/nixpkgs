d36ea036bee6d2ae01b55e0571c17b9f155e178e
Markus Kowalewski <markus.kowalewski@gmail.com>
2025-10-05 19:35:25 +0200

    qgit: refactor, cleanup

--- pkgs/applications/version-management/qgit/default.nix
+++ pkgs/applications/version-management/qgit/default.nix
@@ -8,36 +8,36 @@
   wrapQtAppsHook,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "qgit";
   version = "2.12";
 
   src = fetchFromGitHub {
     owner = "tibirna";
     repo = "qgit";
-    rev = "qgit-${version}";
+    rev = "qgit-${finalAttrs.version}";
     hash = "sha256-q81nY9D/8riMTFP8gDRbY2PjVo+NwRu/XEN1Yn0P/pk=";
   };
-
-  buildInputs = [
-    qtbase
-    qt5compat
-  ];
 
   nativeBuildInputs = [
     cmake
     wrapQtAppsHook
   ];
 
-  meta = with lib; {
-    license = licenses.gpl2Only;
+  buildInputs = [
+    qtbase
+    qt5compat
+  ];
+
+  meta = {
+    license = lib.licenses.gpl2Only;
     homepage = "https://github.com/tibirna/qgit";
     description = "Graphical front-end to Git";
-    maintainers = with maintainers; [
+    maintainers = with lib.maintainers; [
       peterhoeg
       markuskowa
     ];
     inherit (qtbase.meta) platforms;
     mainProgram = "qgit";
   };
-}
+})
