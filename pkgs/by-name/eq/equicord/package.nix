a5cd8af34afef1e5f0dee26d438bc61352849a6a
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-05 02:32:03 +0000

    equicord: 2025-09-12 -> 2025-10-05

--- pkgs/by-name/eq/equicord/package.nix
+++ pkgs/by-name/eq/equicord/package.nix
@@ -18,19 +18,19 @@ stdenv.mkDerivation (finalAttrs: {
   # the Equicord repository. Dates as tags (and automatic releases) were the compromise
   # we came to with upstream. Please do not change the version schema (e.g., to semver)
   # unless upstream changes the tag schema from dates.
-  version = "2025-09-12";
+  version = "2025-10-05";
 
   src = fetchFromGitHub {
     owner = "Equicord";
     repo = "Equicord";
     tag = "${finalAttrs.version}";
-    hash = "sha256-915HW2cGGKtbRGghHP/Em0Wyeit6d1oSUmaZLQOH/Lc=";
+    hash = "sha256-29akM1A7wFI+uZJ2r6K+2djYoZ6zI/rcYROU++x4j8k=";
   };
 
   pnpmDeps = pnpm_10.fetchDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 1;
-    hash = "sha256-xVnryPA7+gnRvpMzuFJl4YeEPOky2+iOu76V3Rf6bow=";
+    hash = "sha256-SsvkIgIXKYBgiHOMt9+IX8pXSYvXr8ESgHJm0mzBWlU=";
   };
 
   nativeBuildInputs = [
