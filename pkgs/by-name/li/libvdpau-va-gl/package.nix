e48bcdfd3f07342f068597fe8c9dd2ff3f8cd947
albertvala <hater@hater.dev>
2025-10-06 20:43:30 +0200

    libvdpau-va-gl: update to a845e87
    Update to the latest commit. Fixes CMake incompatibility.

--- pkgs/by-name/li/libvdpau-va-gl/package.nix
+++ pkgs/by-name/li/libvdpau-va-gl/package.nix
@@ -15,15 +15,15 @@
   libGLU,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "libvdpau-va-gl";
-  version = "0.4.2";
+  version = "0.4.2-unstable-2025-5-18";
 
   src = fetchFromGitHub {
     owner = "i-rinat";
     repo = "libvdpau-va-gl";
-    rev = "v${version}";
-    sha256 = "0asndybfv8xb0fx73sjjw5kydqrahqkm6n04lh589pbf18s5qlld";
+    rev = "a845e8720d900e4bcc89e7ee16106ce63b44af0d";
+    hash = "sha256-CtpyWod+blqC3u12MaQyqFOXurCP5Rb2PYq7PoaoASA=";
   };
 
   nativeBuildInputs = [
