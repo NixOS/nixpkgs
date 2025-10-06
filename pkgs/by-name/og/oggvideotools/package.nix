ef4c045c0353cd25487e95525547816bb30374e1
Martin Joerg <martin.joerg@gmail.com>
2025-10-06 12:30:28 +0000

    oggvideotools: modernize

--- pkgs/by-name/og/oggvideotools/package.nix
+++ pkgs/by-name/og/oggvideotools/package.nix
@@ -12,13 +12,13 @@
   libvorbis,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "oggvideotools";
   version = "0.9.1";
 
   src = fetchurl {
-    url = "mirror://sourceforge/oggvideotools/oggvideotools/oggvideotools-${version}/oggvideotools-${version}.tar.bz2";
-    sha256 = "sha256-2dv3iXt86phhIgnYC5EnRzyX1u5ssNzPwrOP4+jilSM=";
+    url = "mirror://sourceforge/oggvideotools/oggvideotools/oggvideotools-${finalAttrs.version}/oggvideotools-${finalAttrs.version}.tar.bz2";
+    hash = "sha256-2dv3iXt86phhIgnYC5EnRzyX1u5ssNzPwrOP4+jilSM=";
   };
 
   patches = [
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
     (fetchpatch {
       name = "gcc-10.patch";
       url = "https://sourceforge.net/p/oggvideotools/bugs/12/attachment/fix-compile.patch";
-      sha256 = "sha256-mJttoC3jCLM3vmPhlyqh+W0ryp2RjJGIBXd6sJfLJA4=";
+      hash = "sha256-mJttoC3jCLM3vmPhlyqh+W0ryp2RjJGIBXd6sJfLJA4=";
     })
 
     # Fix pending upstream inclusion for build failure on gcc-12:
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
     (fetchpatch {
       name = "gcc-12.patch";
       url = "https://sourceforge.net/p/oggvideotools/bugs/13/attachment/fix-gcc-12.patch";
-      sha256 = "sha256-zuDXe86djWkR8SgYZHkuAJJ7Lf2VYsVRBrlEaODtMKE=";
+      hash = "sha256-zuDXe86djWkR8SgYZHkuAJJ7Lf2VYsVRBrlEaODtMKE=";
       # svn patch, rely on prefix added by fetchpatch:
       extraPrefix = "";
     })
@@ -64,14 +64,14 @@ stdenv.mkDerivation rec {
     (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "4.0")
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Toolbox for manipulating and creating Ogg video files";
-    homepage = "http://www.streamnik.de/oggvideotools.html";
-    license = licenses.gpl2Only;
+    homepage = "https://sourceforge.net/projects/oggvideotools/";
+    license = lib.licenses.gpl2Only;
     maintainers = [ ];
     # Compilation error on Darwin:
     # error: invalid argument '--std=c++0x' not allowed with 'C'
     # make[2]: *** [src/libresample/CMakeFiles/resample.dir/build.make:76: src/libresample/CMakeFiles/resample.dir/filterkit.c.o] Error 1
     broken = stdenv.hostPlatform.isDarwin;
   };
-}
+})
