d991906b914b96a758899ed4b391cfaaacabef16
Jan Tojnar <jtojnar@gmail.com>
2025-10-06 00:10:34 +0000

    gnome-shell-extensions: 49.rc → 49.0
    https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/compare/49.rc...49.0
    
    Changelog-Reviewed-By: Bobby Rong <rjl931189261@126.com>

--- pkgs/by-name/gn/gnome-shell-extensions/package.nix
+++ pkgs/by-name/gn/gnome-shell-extensions/package.nix
@@ -15,11 +15,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-shell-extensions";
-  version = "49.rc";
+  version = "49.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-shell-extensions/${lib.versions.major finalAttrs.version}/gnome-shell-extensions-${finalAttrs.version}.tar.xz";
-    hash = "sha256-5FYi5PgASfK1wS++DWvF1YrTIH7W0QVdJBp/qtYpAB0=";
+    hash = "sha256-FXDgZHhstDiXWeBIPzob29W5s48GJG600dUJKSqcamI=";
   };
 
   patches = [
