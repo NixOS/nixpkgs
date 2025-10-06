c9c614d7e2e5ede5d3abd5075745759fb6cb2aef
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-06 02:37:45 +0000

    python3Packages.pycasbin: 2.2.0 -> 2.3.0

--- pkgs/development/python-modules/pycasbin/default.nix
+++ pkgs/development/python-modules/pycasbin/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "pycasbin";
-  version = "2.2.0";
+  version = "2.3.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "casbin";
     repo = "pycasbin";
     tag = "v${version}";
-    hash = "sha256-JSaQq5BX+aXJ2iaCudzbCcMwNnf4INS/iWfMOua6fjw=";
+    hash = "sha256-geKcirTumxMjBBWcFvN9N4ktm3+MHKev1956gtVimoI=";
   };
 
   build-system = [ setuptools ];
