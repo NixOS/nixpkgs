8941d341952a39ab13c45f316309a03fc7d15247
Joonas Rautiola <joonas@rautiola.co>
2025-10-06 12:30:52 +0300

    libp11: 0.4.13 -> 0.4.16
    https://github.com/OpenSC/libp11/compare/libp11-0.4.13...libp11-0.4.16

--- pkgs/by-name/li/libp11/package.nix
+++ pkgs/by-name/li/libp11/package.nix
@@ -10,17 +10,18 @@
 
 stdenv.mkDerivation rec {
   pname = "libp11";
-  version = "0.4.13";
+  version = "0.4.16";
 
   src = fetchFromGitHub {
     owner = "OpenSC";
     repo = "libp11";
     rev = "${pname}-${version}";
-    sha256 = "sha256-teYXlPtCt6ifQDArbCJWGrYl9pdr6V7HVpU4HXTPIco=";
+    sha256 = "sha256-uuFHchf5skoNtyXTmyGHB67hcuI1N2ILasFcf7scTWo=";
   };
 
   configureFlags = [
     "--with-enginesdir=${placeholder "out"}/lib/engines"
+    "--with-modulesdir=${placeholder "out"}/lib/ossl-modules"
   ];
 
   nativeBuildInputs = [
@@ -40,5 +41,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/OpenSC/libp11";
     license = licenses.lgpl21Plus;
     platforms = platforms.all;
+    maintainers = with lib.maintainers; [ joinemm ];
   };
 }
