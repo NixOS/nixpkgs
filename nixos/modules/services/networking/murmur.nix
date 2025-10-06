9bd10e556b222e1b84094d3ad17649a041736b94
Felix Singer <felixsinger@posteo.net>
2025-10-06 14:17:43 +0200

    nixos/murmur: Enable PrivateUsers hardening in service config
    For documentation see
    https://www.freedesktop.org/software/systemd/man/latest/systemd.exec.html#PrivateUsers=
    
    Signed-off-by: Felix Singer <felixsinger@posteo.net>

--- nixos/modules/services/networking/murmur.nix
+++ nixos/modules/services/networking/murmur.nix
@@ -350,6 +350,7 @@ in
         PrivateDevices = true;
         PrivateMounts = true;
         PrivateTmp = true;
+        PrivateUsers = true;
         ProtectClock = true;
         ProtectControlGroups = "strict";
         ProtectHome = true;
