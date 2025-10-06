fb3efc45e81dfdc313968c540e2f651330f726eb
Jan Tojnar <jtojnar@gmail.com>
2025-10-06 00:53:51 +0000

    zenity: 4.1.99 → 4.2.0
    https://gitlab.gnome.org/GNOME/zenity/-/compare/4.1.99...4.2.0
    
    Changelog-Reviewed-By: Bobby Rong <rjl931189261@126.com>

--- pkgs/by-name/ze/zenity/package.nix
+++ pkgs/by-name/ze/zenity/package.nix
@@ -17,11 +17,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zenity";
-  version = "4.1.99";
+  version = "4.2.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/zenity/${lib.versions.majorMinor finalAttrs.version}/zenity-${finalAttrs.version}.tar.xz";
-    hash = "sha256-b+KqMTxx6R/6VbEdvseQTzGkAwPgo/YBaCD/be4ynuI=";
+    hash = "sha256-X5gzQMb6VfT6tal2nQdxss3xNl4sFYrBHMFv/Ykva80=";
   };
 
   nativeBuildInputs = [
