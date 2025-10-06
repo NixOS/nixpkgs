0733e9b50c63aa4813b12efa9f0c88ce2b0ee260
Robert Schütz <nix@dotlambda.de>
2025-10-06 08:09:22 -0700

    imagemagick: 7.1.2-3 -> 7.1.2-4
    Diff: https://github.com/ImageMagick/ImageMagick/compare/7.1.2-3...7.1.2-4
    
    Changelog: https://github.com/ImageMagick/Website/blob/main/ChangeLog.md
    (cherry picked from commit b5125315ff9b87f7bb052f736ce5cf7fe6ba06d3)

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -85,13 +85,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "imagemagick";
-  version = "7.1.2-3";
+  version = "7.1.2-4";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     tag = finalAttrs.version;
-    hash = "sha256-L4apUdF1VJXSVqWAyjYFG/4qDJoJ0ObmSOpd90kqXsU=";
+    hash = "sha256-FOai96rtDWFQZ8vest0pDl6m2swQwXp7hVO2sZa1U2A=";
   };
 
   outputs = [
