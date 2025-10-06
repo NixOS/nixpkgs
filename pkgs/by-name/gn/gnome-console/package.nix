d01e7ad6fcd71cd76b9b7b7bee01ac00d1edc61c
Jan Tojnar <jtojnar@gmail.com>
2025-10-06 00:06:26 +0000

    gnome-console: 49.rc → 49.0
    https://gitlab.gnome.org/GNOME/console/-/compare/49.rc...49.0
    
    Changelog-Reviewed-By: Bobby Rong <rjl931189261@126.com>

--- pkgs/by-name/gn/gnome-console/package.nix
+++ pkgs/by-name/gn/gnome-console/package.nix
@@ -21,11 +21,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-console";
-  version = "49.rc";
+  version = "49.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-console/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    hash = "sha256-+TP+13m/fP2HPZ93NfZwSdXyEhWlRGiMGuO+ULGQZsk=";
+    hash = "sha256-no6WRvRz0B9LSnvOLEetImtK6D+r8ky7tKyU9qxdXMI=";
   };
 
   nativeBuildInputs = [
