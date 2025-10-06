fadc49a5ddb9a09aaf078d9649beaba8f2e972d5
Sergei Trofimovich <slyich@gmail.com>
2025-10-06 22:40:34 +0100

    flare: backport upstream cmake-4 fixes
    Without the change the build fails on` master as:
    
        flare-game> CMake Error at /build/innoextract-1.9/cmake/VersionScript.cmake:20 (cmake_minimum_required):
        flare-game>   Compatibility with CMake < 3.5 has been removed from CMake.

--- pkgs/by-name/fl/flare/game.nix
+++ pkgs/by-name/fl/flare/game.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   cmake,
 }:
 
@@ -15,6 +16,15 @@ stdenv.mkDerivation rec {
     tag = "v${version}";
     hash = "sha256-tINIwxyQn8eeJCHwRmAMo2TYRgrgJlGaUrnrgbmM3Jo=";
   };
+
+  patches = [
+    # cmake-4 compatibility patch
+    (fetchpatch {
+      name = "cmake-4.patch";
+      url = "https://github.com/flareteam/flare-game/commit/5b61dfd69f4ecbaca6439caa9ae41b3168e4d21a.patch";
+      hash = "sha256-5Um6LWAWQyialzO3KSebmLju0VOuz1S5dzavO9EWlLE=";
+    })
+  ];
 
   nativeBuildInputs = [ cmake ];
 
