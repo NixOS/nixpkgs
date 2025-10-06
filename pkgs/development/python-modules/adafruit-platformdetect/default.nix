b7a356405480482658118ac629a32de0ff95211c
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-06 19:27:50 +0000

    python3Packages.adafruit-platformdetect: 3.83.2 -> 3.84.0

--- pkgs/development/python-modules/adafruit-platformdetect/default.nix
+++ pkgs/development/python-modules/adafruit-platformdetect/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "adafruit-platformdetect";
-  version = "3.83.2";
+  version = "3.84.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "adafruit_platformdetect";
     inherit version;
-    hash = "sha256-g5KCFpuoJBeaSV/wMfWEDpvpL2LsZfQ6j6EyPVLswBA=";
+    hash = "sha256-cdx3s1gEfmrk/pQkfqA0Sf6Ks+PpNyOdW9HUuULr7nM=";
   };
 
   build-system = [ setuptools-scm ];
