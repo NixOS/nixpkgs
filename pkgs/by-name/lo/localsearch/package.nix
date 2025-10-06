1fa2fb20a8eeef1e338e5b151ddc34a75f89af94
Jan Tojnar <jtojnar@gmail.com>
2025-10-06 00:17:15 +0000

    localsearch: 3.10.rc → 3.10.0
    https://gitlab.gnome.org/GNOME/localsearch/-/compare/3.10.rc...3.10.0
    
    Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>

--- pkgs/by-name/lo/localsearch/package.nix
+++ pkgs/by-name/lo/localsearch/package.nix
@@ -50,11 +50,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "localsearch";
-  version = "3.10.rc";
+  version = "3.10.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/localsearch/${lib.versions.majorMinor finalAttrs.version}/localsearch-${finalAttrs.version}.tar.xz";
-    hash = "sha256-EeihW7QeFzynGqL0lHnKUIwmleWmB/BA1AMvjbyNaFU=";
+    hash = "sha256-WNZupEruFknI2YH5NDc7g+GrSkoahLoDBPWvOR5+AIA=";
   };
 
   patches = [
