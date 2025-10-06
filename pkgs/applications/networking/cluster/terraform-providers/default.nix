5c96cc7a15956eea8ccd0a67f72756c480d6a78a
zowoq <59103226+zowoq@users.noreply.github.com>
2025-10-06 15:21:52 +1000

    terraform-providers: drop outdated aliases and overrides

--- pkgs/applications/networking/cluster/terraform-providers/default.nix
+++ pkgs/applications/networking/cluster/terraform-providers/default.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   buildGoModule,
-  buildGo125Module,
   fetchFromGitHub,
   fetchFromGitLab,
   callPackage,
@@ -103,15 +102,11 @@ let
       mkProviderFetcher = fetchFromGitLab;
       owner = "gitlab-org";
     };
-    # actions update always fails but can't reproduce the failure.
-    heroku = automated-providers.heroku.override { spdx = "MPL-2.0"; };
     # mkisofs needed to create ISOs holding cloud-init data and wrapped to terraform via deecb4c1aab780047d79978c636eeb879dd68630
     libvirt = automated-providers.libvirt.overrideAttrs (_: {
       propagatedBuildInputs = [ cdrtools ];
     });
     minio = automated-providers.minio.override { spdx = "AGPL-3.0-only"; };
-    # requires go >= 1.25.0
-    talos = automated-providers.talos.override { mkProviderGoModule = buildGo125Module; };
   };
 
   # Put all the providers we not longer support in this list.
@@ -121,9 +116,7 @@ let
         name: date: throw "the ${name} terraform provider has been archived by upstream on ${date}";
       removed = name: date: throw "the ${name} terraform provider removed from nixpkgs on ${date}";
     in
-    lib.optionalAttrs config.allowAliases {
-      fly = archived "fly" "2023/10";
-    };
+    lib.optionalAttrs config.allowAliases { };
 
   # excluding aliases, used by terraform-full
   actualProviders = automated-providers // special-providers;
