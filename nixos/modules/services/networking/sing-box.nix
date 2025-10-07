716ce143d9ccaa34437f7ad9c8cd774f2db24e1c
Sizhe Zhao <prc.zhao@outlook.com>
2025-10-07 23:22:09 +0800

    nixos/sing-box: wait until network is up

--- nixos/modules/services/networking/sing-box.nix
+++ nixos/modules/services/networking/sing-box.nix
@@ -88,6 +88,8 @@ in
           "${lib.getExe cfg.package} -D \${STATE_DIRECTORY} -C \${RUNTIME_DIRECTORY} run"
         ];
       };
+      # After= is specified by upstream
+      requires = [ "network-online.target" ];
       wantedBy = [ "multi-user.target" ];
     };
 
