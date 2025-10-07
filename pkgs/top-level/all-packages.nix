89bee752eb9dd5e95a2a9b1cd257cde968c7894b
Guy Chronister <guychronister@outlook.com>
2025-10-07 13:59:22 -0500

    diffoscope: 304 -> 306  - add missing dexdump, 7z, and asar packages  - add perl version of doc2txt-pl  - add lipo and otool through cctools  - upgrade diffoscope to 306  - move and test diffoscopeMinimal override to by-name  - fix skipped abootimg test

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2620,10 +2620,6 @@ with pkgs;
 
   deluge-2_x = deluge;
 
-  diffoscopeMinimal = diffoscope.override {
-    enableBloat = false;
-  };
-
   diffutils = callPackage ../tools/text/diffutils { };
 
   dotnetfx35 = callPackage ../development/libraries/dotnetfx35 { };
