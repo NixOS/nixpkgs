1123470fa8382fa3e078af2e9cd93da923ff74dc
nathanscully <nathan@adora.so>
2025-10-07 12:57:55 +1100

    maintainers: add nathanscully

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18262,6 +18262,12 @@
     githubId = 18604892;
     name = "Nathan Ruiz";
   };
+  nathanscully = {
+    email = "nath@nscully.com";
+    github = "nathanscully";
+    githubId = 3359816;
+    name = "Nathan Scully";
+  };
   nathyong = {
     github = "nathyong";
     githubId = 818502;
