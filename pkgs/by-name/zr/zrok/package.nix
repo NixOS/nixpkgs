3135d5373a35de5e73c5c04a8a4b51ad9470dfd4
kyehn <kyehn@users.noreply.github.com>
2025-10-07 04:37:27 +0000

    zrok: 1.1.7 -> 1.1.8

--- pkgs/by-name/zr/zrok/package.nix
+++ pkgs/by-name/zr/zrok/package.nix
@@ -11,7 +11,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zrok";
-  version = "1.1.7";
+  version = "1.1.8";
 
   src =
     let
@@ -28,9 +28,9 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://github.com/openziti/zrok/releases/download/v${finalAttrs.version}/zrok_${finalAttrs.version}_linux_${arch}.tar.gz";
       stripRoot = false;
       hash = selectSystem {
-        x86_64-linux = "sha256-2eLnKjIVYZCOe5KZcT2fLQbYWnK5HBNfnaO/5spBOEU=";
-        aarch64-linux = "sha256-ZGyh8w9Zcgbm9YcNDaGiSdI3D62e4G8RFX7GbEPhCtE=";
-        armv7l-linux = "sha256-H6Ae2fnS48MG5fPmPbl7Rzaw7ElB+fsdjUHfDwfn8pk=";
+        x86_64-linux = "sha256-bpnEWimCnLMBTl6lbua16TNU9/L2MVrdxpbGVPUdzmQ=";
+        aarch64-linux = "sha256-eKCYGWo20CzpvGNHg0FCnghqI7GaqXJiqtMmgOdE/To=";
+        armv7l-linux = "sha256-becVRsX1nRLeBeKnH9IYB6Xt6DVGqSKwLzGOX6D+rS8=";
       };
     };
 
