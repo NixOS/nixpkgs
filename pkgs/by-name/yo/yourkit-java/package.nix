d0c091bca3c8500651223c60d3122d237324bb63
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-07 09:35:35 +0000

    yourkit-java: 2025.3-b154 -> 2025.9-b161

--- pkgs/by-name/yo/yourkit-java/package.nix
+++ pkgs/by-name/yo/yourkit-java/package.nix
@@ -12,7 +12,7 @@ let
 
   vPath = v: lib.elemAt (lib.splitString "-" v) 0;
 
-  version = "2025.3-b154";
+  version = "2025.9-b161";
 
   arches = {
     aarch64-linux = "arm64";
@@ -22,8 +22,8 @@ let
   arch = arches.${stdenvNoCC.targetPlatform.system} or (throw "Unsupported system");
 
   hashes = {
-    arm64 = "sha256-X9YQy12rfTWOVKX2ufmS4GxLGp/I6jhZAZyRBfLuOuk=";
-    x64 = "sha256-BuEfpMEgkOcbUra6eT/sTiVhXpheMaCe55M/CuG0kHE=";
+    arm64 = "sha256-8fBe6evrCdbobx7tvWw+zqKBSLrnksib/EQmskmKiZc=";
+    x64 = "sha256-h+8BHW6Y1Km2iHyONA4DPyUWxrA9R66322lPFBz/bGw=";
   };
 
   desktopItem = makeDesktopItem {
