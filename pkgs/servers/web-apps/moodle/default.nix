84a6eb44e2d504774269d87168b0837fb6cc4fc9
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-07 01:01:03 +0000

    moodle: 5.0.2 -> 5.1

--- pkgs/servers/web-apps/moodle/default.nix
+++ pkgs/servers/web-apps/moodle/default.nix
@@ -8,7 +8,7 @@
 }:
 
 let
-  version = "5.0.2";
+  version = "5.1";
 
   versionParts = lib.take 2 (lib.splitVersion version);
   # 4.2 -> 402, 3.11 -> 311
@@ -95,7 +95,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://download.moodle.org/download.php/direct/stable${stableVersion}/${pname}-${version}.tgz";
-    hash = "sha256-p9kXrUnsFNHJ3k5EwSYO/iXNlN1AanOGln1TQSFiCUI=";
+    hash = "sha256-e5gH9DABJ6qmwnDXhexWYbbf4YHlBb0owRgYDf1nk4o=";
   };
 
   phpConfig = writeText "config.php" ''
