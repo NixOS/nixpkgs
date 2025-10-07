742f250090206c3a553011e869509606cc3aee19
K900 <me@0upti.me>
2025-10-07 20:17:13 +0300

    spirv-headers: 1.4.321.0-unstable-2025-06-24 -> 1.4.328.0
    Diff: https://github.com/KhronosGroup/SPIRV-Headers/compare/9e3836d7d6023843a72ecd3fbf3f09b1b6747a9e...vulkan-sdk-1.4.328.0

--- pkgs/by-name/sp/spirv-headers/package.nix
+++ pkgs/by-name/sp/spirv-headers/package.nix
@@ -5,18 +5,15 @@
   cmake,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "spirv-headers";
-  version = "1.4.321.0-unstable-2025-06-24";
+  version = "1.4.328.0";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "SPIRV-Headers";
-    # The release for Vulkan SDK 1.4.321.0 is missing a PR required for
-    # LLVM 21 support in SPIRV-LLVM-Translator; return to the
-    # `vulkan-sdk-*` tags on the next stable release.
-    rev = "9e3836d7d6023843a72ecd3fbf3f09b1b6747a9e";
-    hash = "sha256-N8NBAkkpOcbgap4loPJJW6E5bjG+TixCh/HN259RyjI=";
+    rev = "vulkan-sdk-${finalAttrs.version}";
+    hash = "sha256-gewCQvcVRw+qdWPWRlYUMTt/aXrZ7Lea058WyqL5c08=";
   };
 
   nativeBuildInputs = [ cmake ];
@@ -27,4 +24,4 @@ stdenv.mkDerivation {
     license = licenses.mit;
     maintainers = [ maintainers.ralith ];
   };
-}
+})
