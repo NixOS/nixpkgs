38b0fa805ea9a7d95a34ca014f928f1fa65af900
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-07 02:54:01 +0000

    windmill: 1.549.1 -> 1.555.2

--- pkgs/by-name/wi/windmill/ui_builder.nix
+++ pkgs/by-name/wi/windmill/ui_builder.nix
@@ -2,7 +2,7 @@
 { fetchzip }:
 
 fetchzip {
-  url = "https://pub-06154ed168a24e73a86ab84db6bf15d8.r2.dev/ui_builder-d44b577.tar.gz";
-  sha256 = "0wx9jq9h092c7y4x7dycvj10wna2q22062nvvzzb6646s6q2iycg";
+  url = "https://pub-06154ed168a24e73a86ab84db6bf15d8.r2.dev/ui_builder-a3f259c.tar.gz";
+  sha256 = "0p4vmzh31c6jll0xhh82nkg7619lvyzg4i10hcphjkr5d8hdr5pk";
   stripRoot = false;
 }
