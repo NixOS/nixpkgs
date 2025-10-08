01e1256074e8b8f95b7ead0b96bcdeb7ab4c9327
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-08 12:09:22 +0200

    dfc: fix for CMake v4

--- pkgs/by-name/df/dfc/package.nix
+++ pkgs/by-name/df/dfc/package.nix
@@ -15,6 +15,14 @@ stdenv.mkDerivation rec {
     sha256 = "0m1fd7l85ckb7bq4c5c3g257bkjglm8gq7x42pkmpp87fkknc94n";
   };
 
+  # Fix for CMake v4
+  # ref. https://github.com/rolinh/dfc/pull/35
+  postPatch = ''
+    substituteInPlace CMakeLists.txt --replace-fail \
+      "cmake_minimum_required(VERSION 2.8.4)" \
+      "cmake_minimum_required(VERSION 3.10)"
+  '';
+
   nativeBuildInputs = [
     cmake
     gettext
