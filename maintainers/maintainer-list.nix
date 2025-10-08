9427765fc99d72a39567d9706ef2ccdea216f986
Anders Christiansen Sørby <anders@sorby.xyz>
2025-10-08 22:10:23 +0200

    relibc: Reinit at nightly
    Based on revert of commit e6f33f4a94e0ebe6f0624af51af190beee64f2a9.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1515,6 +1515,13 @@
     githubId = 754494;
     name = "Anders Asheim Hennum";
   };
+  anderscs = {
+    email = "anders@sorby.xyz";
+    github = "anderssorby";
+    githubId = 6331075;
+    matrix = "@anders:sorby.xyz";
+    name = "Anders C. Sørby";
+  };
   andershus = {
     email = "anders.husebo@eviny.no";
     github = "andershus";
