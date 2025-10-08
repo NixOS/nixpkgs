f10e1e6cf04531b6c1783bff6566a39574282087
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-08 12:52:02 +0000

    selenium-manager: 4.29.0 -> 4.36.0

--- pkgs/by-name/se/selenium-manager/package.nix
+++ pkgs/by-name/se/selenium-manager/package.nix
@@ -6,13 +6,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "selenium-manager";
-  version = "4.29.0";
+  version = "4.36.0";
 
   src = fetchFromGitHub {
     owner = "SeleniumHQ";
     repo = "selenium";
     tag = "selenium-${version}";
-    hash = "sha256-IyMXgYl/TPTpe/Y0pFyJVKj4Mp0xbkg1LSCNHzFL3bE=";
+    hash = "sha256-ICP6saP1Ks9RA66jTkN8wK+vF28+nPxtlm91QwthaRY=";
   };
 
   sourceRoot = "${src.name}/rust";
@@ -21,7 +21,7 @@ rustPlatform.buildRustPackage rec {
     ./disable-telemetry.patch
   ];
 
-  cargoHash = "sha256-MgnmEJif4Z4CcmBFkC5BJR67DMGm1ttObtl4LhAFw4g=";
+  cargoHash = "sha256-E+4Y2n8RAsKC0Cd9Xqa5anGLpvxCH1k7LbGTlLbheo8=";
 
   # TODO: enable tests
   # The test suite depends on a number of browsers and network requests,
