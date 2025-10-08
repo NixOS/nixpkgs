0b1333979d9c23f29508b8f09d7332efb6f348cf
Fabian Affolter <fabian@affolter-engineering.ch>
2025-10-08 11:32:44 +0200

    python313Packages.pluginbase: modernize

--- pkgs/development/python-modules/pluginbase/default.nix
+++ pkgs/development/python-modules/pluginbase/default.nix
@@ -2,31 +2,31 @@
   lib,
   fetchPypi,
   buildPythonPackage,
-  pytest,
+  setuptools,
 }:
 
 buildPythonPackage rec {
-  version = "1.0.1";
-  format = "setuptools";
   pname = "pluginbase";
+  version = "1.0.1";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "ff6c33a98fce232e9c73841d787a643de574937069f0d18147028d70d7dee287";
+    hash = "sha256-/2wzqY/OIy6cc4QdeHpkPeV0k3Bp8NGBRwKNcNfe4oc=";
   };
 
-  nativeCheckInputs = [ pytest ];
+  build-system = [ setuptools ];
 
-  checkPhase = ''
-    cd tests
-    PYTHONPATH=.. pytest
-  '';
+  # https://github.com/mitsuhiko/pluginbase/issues/24
+  doCheck = false;
+
+  pythonImportsCheck = [ "pluginbase" ];
 
   meta = {
     description = "Support library for building plugins systems in Python";
     homepage = "https://github.com/mitsuhiko/pluginbase";
     changelog = "https://github.com/mitsuhiko/pluginbase/releases/tag/${version}";
     license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [ ];
+    maintainers = [ ];
   };
 }
