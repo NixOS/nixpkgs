0faab167f125e626f4a627b66271f1840256dfb1
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-08 16:45:37 +0000

    python3Packages.pulumi-aws: 7.7.0 -> 7.8.0

--- pkgs/development/python-modules/pulumi-aws/default.nix
+++ pkgs/development/python-modules/pulumi-aws/default.nix
@@ -12,7 +12,7 @@
 buildPythonPackage rec {
   pname = "pulumi-aws";
   # Version is independent of pulumi's.
-  version = "7.7.0";
+  version = "7.8.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "pulumi";
     repo = "pulumi-aws";
     tag = "v${version}";
-    hash = "sha256-GLtl9799lQv+Wus/rvUOd/FkRaja7tJddD8ffIhCCdo=";
+    hash = "sha256-aIspwjQl/aFkNAC8UWsbOTrw+eq5miTfVGc990X1sME=";
   };
 
   sourceRoot = "${src.name}/sdk/python";
