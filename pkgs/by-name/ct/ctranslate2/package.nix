51640275eb64be2906a40efd78fbbff0f4e0030a
Dave Walker <dave@daviey.com>
2025-10-09 15:20:46 +0100

    ctranslate2: fix CMake 4 compatibility with patch file
    Adds a patch file to update cmake_minimum_required from 3.7 to 3.10 in
    the main CMakeLists.txt files. Keeps the existing postPatch for vendored
    dependencies since those are git submodules that can't be easily patched
    with patch files.
    
    CMake 4 doesn't support versions older than 3.5, and versions between
    3.5-3.10 are deprecated.
    
    Upstream is working on a similar fix in https://github.com/OpenNMT/CTranslate2/pull/1907
    
    Fixes: #450258
    Signed-off-by: Dave Walker <dave@daviey.com>

--- pkgs/by-name/ct/ctranslate2/package.nix
+++ pkgs/by-name/ct/ctranslate2/package.nix
@@ -38,7 +38,14 @@ stdenv'.mkDerivation (finalAttrs: {
     hash = "sha256-EM2kunqtxo0BTIzrEomfaRsdav7sx6QEOhjDtjjSoYY=";
   };
 
-  # Fix CMake 4 compatibility
+  patches = [
+    # Update CMake minimum required version for CMake 4 compatibility
+    # https://github.com/NixOS/nixpkgs/issues/450258
+    # Inspired by: https://github.com/OpenNMT/CTranslate2/pull/1907
+    ./cmake-3.10.patch
+  ];
+
+  # Fix CMake 4 compatibility in vendored dependencies (submodules)
   postPatch = ''
     substituteInPlace third_party/cpu_features/CMakeLists.txt \
       --replace-fail \
