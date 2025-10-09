f3ba0f816cb7144f736a914e99abdabb476f81c7
OPNA2608 <opna2608@protonmail.com>
2025-10-09 15:46:15 +0200

    sofia_sip: Enable tests, speed up build

--- pkgs/by-name/so/sofia_sip/package.nix
+++ pkgs/by-name/so/sofia_sip/package.nix
@@ -3,10 +3,15 @@
   stdenv,
   fetchFromGitHub,
   fetchpatch2,
+  autoconf,
+  automake,
+  check,
   glib,
+  libtool,
   openssl,
   pkg-config,
-  autoreconfHook,
+  valgrind,
+  zlib,
 }:
 
 stdenv.mkDerivation rec {
@@ -27,16 +32,58 @@ stdenv.mkDerivation rec {
       url = "https://github.com/freeswitch/sofia-sip/commit/46b02f0655af0a9594e805f09a8ee99278f84777.diff";
       hash = "sha256-4uZVtKnXG+BPW8byjd7tu4uEZo9SYq9EzTEvMwG0Bak=";
     })
+
+    # Disable some failing tests
+    # https://github.com/freeswitch/sofia-sip/issues/234
+    # run_addrinfo: Fails due to limited networking during build
+    # torture_su_root: Aborts with: bit out of range 0 - FD_SETSIZE on fd_set
+    # run_check_nta: Times out in client_2_1_1 test, which seems to test some connection protocol fallback thing
+    # run_test_nta: "no valid IPv6 addresses available", likely due to no networking in sandbox
+    # check_nua, check_sofia, test_nua: Times out no matter how much time is given to it
+    ./2001-sofia-sip-Disable-some-tests.patch
+  ];
+
+  # This actually breaks these tests, leading to bash trying to execute itself
+  # https://github.com/freeswitch/sofia-sip/issues/234
+  postPatch = ''
+    substituteInPlace libsofia-sip-ua/nta/Makefile.am \
+      --replace-fail 'TESTS_ENVIRONMENT =' '#TESTS_ENVIRONMENT ='
+  '';
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    autoconf
+    automake
+    libtool
+    pkg-config
   ];
 
   buildInputs = [
     glib
     openssl
   ];
-  nativeBuildInputs = [
-    autoreconfHook
-    pkg-config
+
+  nativeCheckInputs = [
+    valgrind
   ];
+
+  checkInputs = [
+    check
+    zlib
+  ];
+
+  preConfigure = ''
+    ./bootstrap.sh
+  '';
+
+  configureFlags = [
+    (lib.strings.enableFeature true "expensive-checks")
+  ];
+
+  enableParallelBuilding = true;
+
+  doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
 
   meta = with lib; {
     description = "Open-source SIP User-Agent library, compliant with the IETF RFC3261 specification";
