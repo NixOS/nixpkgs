9baf121641586a3a57e86599420e806f5bde2304
Winterer, Mathis Aaron <mrmagic223325@fedora.email>
2025-10-10 02:42:00 +0200

    ibus-engines.table-chinese: 1.8.12 -> 1.8.14

--- pkgs/tools/inputmethods/ibus-engines/ibus-table-chinese/default.nix
+++ pkgs/tools/inputmethods/ibus-engines/ibus-table-chinese/default.nix
@@ -10,26 +10,16 @@
   writableTmpDirAsHomeHook,
 }:
 
-let
+stdenv.mkDerivation {
+  pname = "ibus-table-chinese";
+  version = "1.8.14";
+
   src = fetchFromGitHub {
     owner = "mike-fabian";
     repo = "ibus-table-chinese";
-    rev = "3a416012f3b898fe17225925f59d0672a8a0c0db";
-    sha256 = "sha256-KA4jRSlQ78IeP7od3VtgdR58Z/6psNkMCVwvg3vhFIM=";
+    rev = "44301450e681c23d60301747856c74b5b9d1312e";
+    sha256 = "sha256-CvODJhQTzqR58IlaL8cIP9Z1gcC8PfkfU0HWdq0Jjms=";
   };
-in
-stdenv.mkDerivation {
-  pname = "ibus-table-chinese";
-  version = "1.8.12";
-
-  inherit src;
-
-  postConfigure = ''
-    substituteInPlace cmake_install.cmake --replace-fail /var/empty $out
-    substituteInPlace CMakeLists.txt --replace-fail /var/empty $out
-  '';
-  # Fails otherwise with "no such file or directory: <table>.txt"
-  dontUseCmakeBuildDir = true;
 
   nativeBuildInputs = [
     cmake
@@ -46,7 +36,7 @@ stdenv.mkDerivation {
   meta = {
     isIbusEngine = true;
     description = "Chinese tables for IBus-Table";
-    homepage = "https://github.com/definite/ibus-table-chinese";
+    homepage = "https://github.com/mike-fabian/ibus-table-chinese";
     license = lib.licenses.gpl3;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ pneumaticat ];
