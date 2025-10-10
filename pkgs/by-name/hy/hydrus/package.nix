85de8d442057e3c132e31ad82926facd20c5be1a
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-10 17:18:34 +0000

    hydrus: 631 -> 642

--- pkgs/by-name/hy/hydrus/package.nix
+++ pkgs/by-name/hy/hydrus/package.nix
@@ -16,14 +16,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "hydrus";
-  version = "631";
+  version = "642";
   format = "other";
 
   src = fetchFromGitHub {
     owner = "hydrusnetwork";
     repo = "hydrus";
     tag = "v${version}";
-    hash = "sha256-YZnlQIiq0dUGEnQgVCTvNS+kuSpXlaAN5UvZAQ3xeZM=";
+    hash = "sha256-C4r0FDVkOrZp820xy/z2xHBtP/3zfJkmBF0/Q3bYbuQ=";
   };
 
   nativeBuildInputs = [
