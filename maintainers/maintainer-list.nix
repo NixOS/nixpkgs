d463f0eabc2798d18b2c9e3dd4bf2b705ee9cb55
Dmitry Voronin <hi@voronind.com>
2025-10-10 12:02:01 +0300

    maintainers: remove voronind gpg signature

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -27443,7 +27443,6 @@
     name = "Dmitry Voronin";
     github = "voronind-com";
     githubId = 22127600;
-    keys = [ { fingerprint = "3241 FDAD 82A7 E22D 4279  F405 913F 3267 9278 2E1C"; } ];
   };
   votava = {
     email = "votava@gmail.com";
