11539f41468d0f9265ace0768d2c7a53f0d40962
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2025-10-10 14:12:03 +0200

    superfile: 1.3.3 -> 1.5.0

--- pkgs/by-name/su/superfile/package.nix
+++ pkgs/by-name/su/superfile/package.nix
@@ -6,9 +6,10 @@
   nix-update-script,
   writableTmpDirAsHomeHook,
   exiftool,
+  zoxide,
 }:
 let
-  version = "1.3.3";
+  version = "1.5.0";
   tag = "v${version}";
 in
 buildGoModule {
@@ -19,28 +20,52 @@ buildGoModule {
     owner = "yorukot";
     repo = "superfile";
     inherit tag;
-    hash = "sha256-A1SWsBcPtGNbSReslp5L3Gg4hy3lDSccqGxFpLfVPrk=";
+    hash = "sha256-PEojifuiIjF3OUxDoMCyynOJUpFglTzh7lJUcq7g4e0=";
   };
 
-  vendorHash = "sha256-sqt0BzJW1nu6gYAhscrXlTAbwIoUY7JAOuzsenHpKEI=";
+  vendorHash = "sha256-5SSnrG3DvD1i7rNcpztHkUUap4Qp7MX04ofD7rA3xgM=";
 
   ldflags = [
     "-s"
     "-w"
   ];
 
-  nativeBuildInputs = [ exiftool ];
+  # TestLayout test does not support parallel testing
+  enableParallelBuilding = false;
+
+  nativeBuildInputs = [
+    exiftool
+    zoxide
+  ];
 
   nativeCheckInputs = [ writableTmpDirAsHomeHook ];
 
-  # Upstream notes that this could be flaky, and it consistently fails for me.
+  preCheck = ''
+    mkdir -p $HOME/.local/share/superfile
+  '';
+
   checkFlags = [
-    "-skip=^TestReturnDirElement/Sort_by_Date$"
-  ]
-  ++ lib.optionals stdenv.isDarwin [
-    # Only failing on nix darwin. I suspect this is due to the way
-    # darwin handles file permissions.
-    "-skip=^TestCompressSelectedFiles"
+    "-skip=^${
+      lib.concatStringsSep "$|^" (
+        [
+          # Upstream notes that this could be flaky, and it consistently fails for me.
+          "TestReturnDirElement/Sort_by_Date"
+
+          # Tests fail with
+          # ... `validations failed, error : file panel 1 error : invalid cursor : 0, element count : 0, layout info` ...
+          "TestLayout"
+        ]
+        ++ lib.optionals stdenv.isDarwin [
+          # Only failing on nix darwin. I suspect this is due to the way
+          # darwin handles file permissions.
+          "TestCompressSelectedFiles"
+          "TestFileDelete/Move_to_trash"
+
+          # Fails with directory cleanup
+          "TestZoxide"
+        ]
+      )
+    }$"
   ];
 
   passthru.updateScript = nix-update-script { };
