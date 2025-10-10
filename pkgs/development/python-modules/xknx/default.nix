5e8ba03c171175573daa43c90ca9d4187f54a96b
Fabian Affolter <fabian@affolter-engineering.ch>
2025-10-10 20:46:58 +0200

    python313Packages.xknx: 3.9.0 -> 3.9.1
    Diff: https://github.com/XKNX/xknx/compare/3.9.0...3.9.1
    
    Changelog: https://github.com/XKNX/xknx/releases/tag/3.9.1

--- pkgs/development/python-modules/xknx/default.nix
+++ pkgs/development/python-modules/xknx/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "xknx";
-  version = "3.9.0";
+  version = "3.9.1";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "XKNX";
     repo = "xknx";
     tag = version;
-    hash = "sha256-68Vt5jwtEND2Ej6JP10Rp+kqYc2qu9XbmgZgPOmkWWw=";
+    hash = "sha256-BcUZ2wNrWFapYNbvDXQgKQvXEEx/+z79AaPZHsdPRpo=";
   };
 
   build-system = [ setuptools ];
