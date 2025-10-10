4c2be380a346115f58c890d06019e7e0e91203ae
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-10 14:56:18 +0000

    python3Packages.clarifai-grpc: 11.8.6 -> 11.9.3

--- pkgs/development/python-modules/clarifai-grpc/default.nix
+++ pkgs/development/python-modules/clarifai-grpc/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "clarifai-grpc";
-  version = "11.8.6";
+  version = "11.9.3";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "Clarifai";
     repo = "clarifai-python-grpc";
     tag = version;
-    hash = "sha256-Fg55DTde95j2bkqILiWJaYWs4cGr0r9sH26iwikldDw=";
+    hash = "sha256-kKA+D9mIogSvx5dLqc7WSzMMZWZvSvu+RV0WtxWb2xQ=";
   };
 
   build-system = [ setuptools ];
