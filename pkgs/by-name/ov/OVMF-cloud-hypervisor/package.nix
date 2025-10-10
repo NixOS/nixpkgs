710da32605c19fd045cf0a16e6b51b14768681cf
Martin Messer <martin.messer@cyberus-technology.de>
2025-10-10 11:57:53 +0200

    OVMF-cloud-hypervisor: allow aarch64 versions for Cloud Hypervisor OVMF

--- pkgs/by-name/ov/OVMF-cloud-hypervisor/package.nix
+++ pkgs/by-name/ov/OVMF-cloud-hypervisor/package.nix
@@ -1,7 +1,16 @@
-{ lib, OVMF }:
+{
+  lib,
+  OVMF,
+  stdenv,
+}:
 
 OVMF.override {
-  projectDscPath = "OvmfPkg/CloudHv/CloudHvX64.dsc";
+  projectDscPath =
+    (lib.optionalString (stdenv.hostPlatform.isx86) "OvmfPkg/CloudHv/CloudHvX64.dsc")
+    + (lib.optionalString (stdenv.hostPlatform.isAarch64) "ArmVirtPkg/ArmVirtCloudHv.dsc");
+
   fwPrefix = "CLOUDHV";
-  metaPlatforms = builtins.filter (lib.hasPrefix "x86_64-") OVMF.meta.platforms;
+  metaPlatforms = builtins.filter (
+    platform: (lib.hasPrefix "x86_64-" platform) || (lib.hasPrefix "aarch64-" platform)
+  ) OVMF.meta.platforms;
 }
