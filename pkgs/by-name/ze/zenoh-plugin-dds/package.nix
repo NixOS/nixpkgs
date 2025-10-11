feb21f976e820f3ac61eb4cf65730c0dff683f19
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-10-10 20:55:52 -0700

    Implement suggested changes

--- pkgs/by-name/ze/zenoh-plugin-dds/package.nix
+++ pkgs/by-name/ze/zenoh-plugin-dds/package.nix
@@ -4,14 +4,14 @@
   fetchFromGitHub,
   cmake,
 }:
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zenoh-plugin-dds";
   version = "1.4.0"; # nixpkgs-update: no auto update
 
   src = fetchFromGitHub {
     owner = "eclipse-zenoh";
     repo = "zenoh-plugin-dds";
-    rev = version;
+    tag = finalAttrs.version;
     hash = "sha256-vSFgxSSbLEwpwPznvy+m66Z5grgmxZiIom/I4p0xris=";
   };
 
@@ -22,14 +22,15 @@ rustPlatform.buildRustPackage rec {
     rustPlatform.bindgenHook
   ];
 
-  # Some test time out
-  doCheck = false;
+  checkFlags = [
+    "--skip=test_name_that_times_out"
+  ];
 
   meta = {
     description = "Zenoh plugin for DDS";
     longDescription = "A zenoh plug-in that allows to transparently route DDS data. This plugin can be used by DDS applications to leverage zenoh for geographical routing or for better scaling discovery";
     homepage = "https://github.com/eclipse-zenoh/zenoh-plugin-dds";
-    changelog = "https://github.com/eclipse-zenoh/zenoh-plugin-dds/releases/tag/${src.rev}";
+    changelog = "https://github.com/eclipse-zenoh/zenoh-plugin-dds/releases/tag/${finalAttrs.src.rev}";
     license = with lib.licenses; [
       epl20
       asl20
@@ -38,4 +39,4 @@ rustPlatform.buildRustPackage rec {
     platforms = lib.platforms.linux;
     mainProgram = "zenoh-bridge-dds";
   };
-}
+})
