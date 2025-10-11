9853e01ad5ae1747b9e468a24a24ae8beb388312
Rafael Ieda <git@ieda.me>
2025-10-11 10:03:30 -0300

    gl-gsync-demo: move to by-name, modernize

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13193,10 +13193,6 @@ with pkgs;
 
   freeciv_gtk = freeciv;
 
-  gl-gsync-demo = callPackage ../games/gl-gsync-demo {
-    libXNVCtrl = linuxPackages.nvidia_x11.settings.libXNVCtrl;
-  };
-
   gscrabble = python3Packages.callPackage ../games/gscrabble { };
 
   qtads = qt5.callPackage ../games/qtads { };
