5a77a3a644b20496863222217d1b5cba96b76a00
Noa Virellia <cmiki@amono.me>
2025-10-11 08:40:06 +0800

    p7zip: add setup hook for crx files

--- pkgs/tools/archivers/p7zip/setup-hook.sh
+++ pkgs/tools/archivers/p7zip/setup-hook.sh
@@ -1,5 +1,9 @@
-unpackCmdHooks+=(_try7zip)
+unpackCmdHooks+=(_try7zip _tryCrx)
 _try7zip() {
     if ! [[ "$curSrc" =~ \.7z$ ]]; then return 1; fi
+    7z x "$curSrc"
+}
+_tryCrx(){
+    if ! [[ "$curSrc" =~ \.crx$ ]]; then return 1; fi
     7z x "$curSrc"
 }
