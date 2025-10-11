6e0626c8541561a01aa19a90b4f4273293c5c927
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-11 03:26:04 +0000

    quisk: 4.2.44 -> 4.2.45

--- pkgs/by-name/qu/quisk/package.nix
+++ pkgs/by-name/qu/quisk/package.nix
@@ -9,12 +9,12 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "quisk";
-  version = "4.2.44";
+  version = "4.2.45";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-OSGrLbCS255e/btagD7RCVpLpyIX1jveeJnzIwyegH4=";
+    hash = "sha256-uejMMjLClb+Df+gkcBqvDSJuIyv03BLQZv/ef/ctlho=";
   };
 
   buildInputs = [
