5b9fff7c72c024425ced384eaf2124d42641dedb
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-10-11 02:26:51 +0200

    lib.teams: Populate fields from synced GitHub state
    The before and after of
    
        nix-instantiate --eval -A lib.teams --strict --json | jq 'walk(if type == "array" then sort else . end)'
    
    has been ensured to be negligible, only consisting of minor team
    shortName and scope differences

---
+++ maintainers/computed-team-list.nix
@@ -0,0 +1,49 @@
+# Extends ./team-list.nix with synced GitHub information from ./github-teams.json
+{ lib }:
+let
+  githubTeams = lib.importJSON ./github-teams.json;
+  teams = import ./team-list.nix { inherit lib; };
+
+  # A fast maintainer id lookup table
+  maintainersById = lib.pipe lib.maintainers [
+    lib.attrValues
+    (lib.groupBy (attrs: toString attrs.githubId))
+    (lib.mapAttrs (_: lib.head))
+  ];
+
+  maintainerSetToList =
+    githubTeam:
+    lib.mapAttrsToList (
+      login: id:
+      maintainersById.${toString id}
+      or (throw "lib.teams: No maintainer entry for GitHub user @${login} who's part of the @NixOS/${githubTeam} team")
+    );
+
+in
+lib.mapAttrs (
+  name: attrs:
+  if attrs ? github then
+    let
+      githubTeam =
+        githubTeams.${attrs.github}
+          or (throw "lib.teams.${name}: Corresponding GitHub team ${attrs.github} not known yet, make sure to create it and wait for the regular team sync");
+    in
+    assert lib.assertMsg (!attrs ? shortName)
+      "lib.teams.${name}: Both `shortName` and `github` is set, but the former is synced from the latter";
+    assert lib.assertMsg (
+      !attrs ? scope
+    ) "lib.teams.${name}: Both `scope` and `github` is set, but the former is synced from the latter";
+    assert lib.assertMsg (
+      !attrs ? members
+    ) "lib.teams.${name}: Both `members` and `github` is set, but the former is synced from the latter";
+    attrs
+    // {
+      shortName = githubTeam.name;
+      scope = githubTeam.description;
+      members =
+        maintainerSetToList attrs.github githubTeam.maintainers
+        ++ maintainerSetToList attrs.github githubTeam.members;
+    }
+  else
+    attrs
+) teams
