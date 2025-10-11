253987b688a0521b4372659673295946b50ecdff
SchweGELBin <abramjannikmichael06@gmail.com>
2025-10-11 20:10:30 +0200

    pingus: unstable-0.7.6.0.20191104 -> 0.7.6-unstable-2025-07-21

--- pkgs/by-name/pi/pingus/package.nix
+++ pkgs/by-name/pi/pingus/package.nix
@@ -1,55 +1,83 @@
 {
   lib,
   stdenv,
-  fetchgit,
+  fetchFromGitHub,
   cmake,
+  pkg-config,
+  makeWrapper,
+  libGL,
+  libGLU,
   SDL2,
   SDL2_image,
-  boost,
+  fmt,
+  gtest,
   libpng,
-  SDL2_mixer,
-  pkg-config,
-  libGLU,
-  libGL,
-  git,
-  jsoncpp,
+  libsigcxx,
+  argpp,
+  geomcpp,
+  logmich,
+  priocpp,
+  strutcpp,
+  tinycmmc,
+  tinygettext,
+  uitest,
+  wstsound,
+  xdgcpp,
 }:
+
 stdenv.mkDerivation {
   pname = "pingus";
-  version = "unstable-0.7.6.0.20191104";
+  version = "0.7.6-unstable-2025-07-21";
+
+  src = fetchFromGitHub {
+    owner = "Pingus";
+    repo = "pingus";
+    rev = "b0ceeeeb95428c73b1b81208211535c61acfc5d0";
+    sha256 = "sha256-jQYZM7VLqbl9/+QXyswEXdGmwOq/nxRzWARvcDqNM9M=";
+  };
 
   nativeBuildInputs = [
     cmake
     pkg-config
-    git
+    makeWrapper
   ];
+
   buildInputs = [
+    libGL
+    libGLU
     SDL2
     SDL2_image
-    boost
+    fmt
+    gtest
     libpng
-    SDL2_mixer
-    libGLU
-    libGL
-    jsoncpp
+    libsigcxx
+    argpp
+    geomcpp
+    logmich
+    priocpp
+    strutcpp
+    tinycmmc
+    tinygettext
+    uitest
+    wstsound
+    xdgcpp
   ];
-  src = fetchgit {
-    url = "https://gitlab.com/pingus/pingus/";
-    rev = "709546d9b9c4d6d5f45fc9112b45ac10c7f9417d";
-    sha256 = "11mmzk0766riaw5qyd1r5i7s7vczbbzfccm92bvgrm99iy1sj022";
-    fetchSubmodules = true;
-  };
 
-  postPatch = ''
-    # Fix missing gcc-13 headers
-    sed -e '1i #include <cstdint>' -i src/util/pathname.hpp
-  '';
+  cmakeFlags = [
+    "-DWARNINGS=ON"
+    "-DWERROR=ON"
+    "-DBUILD_EXTRA=OFF"
+    "-DBUILD_TESTS=OFF"
+  ];
+
+  doCheck = true;
 
   meta = {
     description = "Puzzle game with mechanics similar to Lemmings";
+    homepage = "https://pingus.seul.org/";
     mainProgram = "pingus";
     platforms = lib.platforms.linux;
-    maintainers = [ lib.maintainers.raskin ];
+    maintainers = with lib.maintainers; [ raskin ];
     license = lib.licenses.gpl3;
   };
 }
