a4d09cbbb7e0b9f2c1fd284b3630ae1f86dcb7fb
Francesco Gazzetta <fgaz@fgaz.me>
2025-10-11 12:53:51 +0200

    tower-pixel-dungeon: 0.5.4 -> 1.0.0

--- pkgs/games/shattered-pixel-dungeon/tower-pixel-dungeon/default.nix
+++ pkgs/games/shattered-pixel-dungeon/tower-pixel-dungeon/default.nix
@@ -6,23 +6,24 @@
 
 callPackage ../generic.nix rec {
   pname = "tower-pixel-dungeon";
-  version = "0.5.4";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "FixAkaTheFix";
     repo = "Tower-Pixel-Dungeon";
     tag = "TPDv${lib.replaceStrings [ "." ] [ "" ] version}";
-    hash = "sha256-6b7EC7rye7nSevMJhRwSKJU7zuzta6KUCgzizWPFk8I=";
+    hash = "sha256-8q1Wt+FXhw58+fK9WMiAcwNrwJfBa5oRBztFmfQz0Uk=";
   };
 
   sourceRoot = src.name + "/pixel-towers-master";
 
   desktopName = "Tower Pixel Dungeon";
 
+  patches = [ ];
+
   # Sprite sources (Paint.NET files) and other files interfere with the build process.
   postPatch = ''
     rm core/src/main/assets/{levelsplashes,sprites}/*.pdn
-    rm core/src/main/assets/environment/*.lnk
   '';
 
   meta = {
