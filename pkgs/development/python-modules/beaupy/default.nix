107ef329377a46434053b60fedfcc56f650ac851
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-11 04:15:16 +0000

    python3Packages.beaupy: 3.10.1 -> 3.10.2

--- pkgs/development/python-modules/beaupy/default.nix
+++ pkgs/development/python-modules/beaupy/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "beaupy";
-  version = "3.10.1";
+  version = "3.10.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "petereon";
     repo = "beaupy";
     rev = "v${version}";
-    hash = "sha256-tN78OV0Ks1MIdqVh8yisTgK4dOaKqYlZxvIoCa44eAI=";
+    hash = "sha256-0m0qc/ei8BuU7vHK/4mY7YQvHVTfwsaPH4Kko9wCKvM=";
   };
 
   build-system = [
