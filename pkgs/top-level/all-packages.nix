29c2f28caff6d9fca6c16e062f9fd8ee33e1a337
Rafael Ieda <git@ieda.me>
2025-10-11 11:01:45 -0300

    legendary-gl: move to by-name, modernize

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13485,8 +13485,6 @@ with pkgs;
 
   leela-zero = libsForQt5.callPackage ../games/leela-zero { };
 
-  legendary-gl = python3Packages.callPackage ../games/legendary-gl { };
-
   liquidwar = callPackage ../games/liquidwar {
     guile = guile_2_0;
   };
