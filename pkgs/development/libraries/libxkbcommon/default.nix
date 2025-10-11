310fa1e82aeccec6be59440e1b20f83c0c42b536
Ben Merritt <ben@haradion.net>
2025-10-11 15:45:29 -0700

    Fix XKB configuration path
    /etc/xkb doesn't exist; /etc/X11/xkb does.

--- pkgs/development/libraries/libxkbcommon/default.nix
+++ pkgs/development/libraries/libxkbcommon/default.nix
@@ -39,7 +39,7 @@ stdenv.mkDerivation rec {
 
   mesonFlags = [
     "-Dxkb-config-root=${xkeyboard_config}/etc/X11/xkb"
-    "-Dxkb-config-extra-path=/etc/xkb" # default=$sysconfdir/xkb ($out/etc)
+    "-Dxkb-config-extra-path=/etc/X11/xkb" # default=$sysconfdir/xkb ($out/etc)
     "-Dx-locale-root=${libX11.out}/share/X11/locale"
     "-Denable-wayland=${lib.boolToString withWaylandTools}"
   ];
