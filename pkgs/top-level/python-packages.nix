984fc6f787715e9213ba0324884dea672f97a79b
Tristan Ross <tristan.ross@midstall.com>
2025-10-11 22:32:56 -0700

    tt-smi: move to by-name

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18851,8 +18851,6 @@ self: super: with self; {
 
   tt-flash = callPackage ../development/python-modules/tt-flash { };
 
-  tt-smi = callPackage ../development/python-modules/tt-smi { };
-
   tt-tools-common = callPackage ../development/python-modules/tt-tools-common { };
 
   ttach = callPackage ../development/python-modules/ttach { };
