3719cceab833a0b1d7af03acc52fefb59566f42a
SZanko <szanko_dev@proton.me>
2025-10-12 03:12:18 +0100

    maintainers: add szanko

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25099,6 +25099,12 @@
     githubId = 58266;
     name = "Sebastián Zaffarano";
   };
+  szanko = {
+    email = "szanko_dev@proton.me";
+    github = "SZanko";
+    githubId = 38001059;
+    name = "Stefan Zanko";
+  };
   szczyp = {
     email = "qb@szczyp.com";
     github = "Szczyp";
