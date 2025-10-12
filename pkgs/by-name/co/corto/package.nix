516179d46b4f7e64d88c4da4994703e3977f0a35
Sigmanificient <edhyjox@gmail.com>
2025-10-13 00:47:42 +0200

    corto: fix build with cmake4

--- pkgs/by-name/co/corto/package.nix
+++ pkgs/by-name/co/corto/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   cmake,
 }:
 
@@ -15,6 +16,14 @@ stdenv.mkDerivation {
     rev = "d880519c490c88a39d12c31a914b6a687a7019c3";
     hash = "sha256-0OUijrf+0ZNv3oYko2r8Kp9zgtg8b9RPL7DXHf15Ryc=";
   };
+
+  patches = [
+    # cmake minimum required 3.10
+    (fetchpatch {
+      url = "https://github.com/cnr-isti-vclab/corto/commit/6a49a72f6f1935f3a197727ebfdb0dc9dd02b46a.patch?full_index=1";
+      hash = "sha256-KQKGG7Edm2mbPvRguAouXW2dxfrx/L5MfKl2c7jf4Es=";
+    })
+  ];
 
   nativeBuildInputs = [ cmake ];
 
