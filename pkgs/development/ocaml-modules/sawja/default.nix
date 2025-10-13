1e1cef625b2108f3d3cab7fb6daff2fe62233243
jopejoe1 <jopejoe1@missing.ninja>
2025-10-13 19:14:09 +0200

    ocamlPackages: swap throws to meta.broken
    throws no longer work with the current ci we have in nixpkgs

--- pkgs/development/ocaml-modules/sawja/default.nix
+++ pkgs/development/ocaml-modules/sawja/default.nix
@@ -12,51 +12,47 @@ let
   pname = "sawja";
   version = "1.5.12";
 in
+stdenv.mkDerivation {
 
-lib.throwIfNot (lib.versionAtLeast ocaml.version "4.08")
-  "${pname} is not available for OCaml ${ocaml.version}"
+  pname = "ocaml${ocaml.version}-${pname}";
 
-  stdenv.mkDerivation
-  {
+  inherit version;
 
-    pname = "ocaml${ocaml.version}-${pname}";
+  src = fetchFromGitHub {
+    owner = "javalib-team";
+    repo = pname;
+    rev = version;
+    hash = "sha256-G1W8/G0TEcldnFnH/NAb9a6ZSGGP2fWTM47lI8bBHnw=";
+  };
 
-    inherit version;
+  nativeBuildInputs = [
+    which
+    ocaml
+    findlib
+  ];
 
-    src = fetchFromGitHub {
-      owner = "javalib-team";
-      repo = pname;
-      rev = version;
-      hash = "sha256-G1W8/G0TEcldnFnH/NAb9a6ZSGGP2fWTM47lI8bBHnw=";
-    };
+  strictDeps = true;
 
-    nativeBuildInputs = [
-      which
-      ocaml
-      findlib
-    ];
+  patches = [
+    ./configure.sh.patch
+    ./Makefile.config.example.patch
+  ];
 
-    strictDeps = true;
+  createFindlibDestdir = true;
 
-    patches = [
-      ./configure.sh.patch
-      ./Makefile.config.example.patch
-    ];
+  configureScript = "./configure.sh";
+  dontAddPrefix = "true";
+  dontAddStaticConfigureFlags = true;
+  configurePlatforms = [ ];
 
-    createFindlibDestdir = true;
+  propagatedBuildInputs = [ javalib ];
 
-    configureScript = "./configure.sh";
-    dontAddPrefix = "true";
-    dontAddStaticConfigureFlags = true;
-    configurePlatforms = [ ];
-
-    propagatedBuildInputs = [ javalib ];
-
-    meta = with lib; {
-      description = "Library written in OCaml, relying on Javalib to provide a high level representation of Java bytecode programs";
-      homepage = "http://sawja.inria.fr/";
-      license = licenses.gpl3Plus;
-      maintainers = [ maintainers.vbgl ];
-      inherit (ocaml.meta) platforms;
-    };
-  }
+  meta = with lib; {
+    description = "Library written in OCaml, relying on Javalib to provide a high level representation of Java bytecode programs";
+    homepage = "http://sawja.inria.fr/";
+    license = licenses.gpl3Plus;
+    maintainers = [ maintainers.vbgl ];
+    inherit (ocaml.meta) platforms;
+    broken = !(lib.versionAtLeast ocaml.version "4.08");
+  };
+}
