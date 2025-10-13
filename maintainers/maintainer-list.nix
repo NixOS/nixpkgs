9b7148792a5ffa53bca92f90f452860824cba24d
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-10-13 17:04:32 +0000

    maintainers: add bitbloxhub

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3400,6 +3400,11 @@
     githubId = 10164833;
     name = "Birk Bohne";
   };
+  bitbloxhub = {
+    name = "bitbloxhub";
+    github = "bitbloxhub";
+    githubId = 45184892;
+  };
   bizmyth = {
     email = "andrew.p.council@gmail.com";
     github = "bizmythy";
