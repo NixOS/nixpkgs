a9b7e173599920f737aa36cde62ef504deb09f64
midchildan <git@midchildan.org>
2025-10-13 09:35:28 +0900

    onscripter-en: base package on onscripter
    Also fix Darwin build.

--- pkgs/by-name/on/onscripter-en/package.nix
+++ pkgs/by-name/on/onscripter-en/package.nix
@@ -1,66 +1,88 @@
 {
   lib,
-  SDL,
-  SDL_image,
-  SDL_mixer,
-  SDL_ttf,
+  buildEnv,
   fetchFromGitHub,
-  freetype,
-  libjpeg,
-  libogg,
-  libpng,
-  libvorbis,
+  makeBinaryWrapper,
+  onscripter,
   pkg-config,
-  smpeg,
-  stdenv,
+  xcbuildHook,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
-  pname = "onscripter-en";
-  version = "20110930";
+let
+  inherit (onscripter) stdenv;
+in
+onscripter.overrideAttrs (
+  final: prev: {
+    pname = "onscripter-en";
+    version = "20110930";
 
-  # The website is not available now. Let's use a Museoa backup
-  src = fetchFromGitHub {
-    owner = "museoa";
-    repo = "onscripter-en";
-    rev = finalAttrs.version;
-    hash = "sha256-Lc5ZlH2C4ER02NmQ6icfiqpzVQdVUnOmdywGjjjSYSg=";
-  };
+    # The website is not available now. Let's use a Museoa backup
+    src = fetchFromGitHub {
+      owner = "museoa";
+      repo = "onscripter-en";
+      rev = final.version;
+      hash = "sha256-Lc5ZlH2C4ER02NmQ6icfiqpzVQdVUnOmdywGjjjSYSg=";
+    };
 
-  nativeBuildInputs = [
-    SDL
-    pkg-config
-    smpeg
-  ];
+    patches = lib.optionals stdenv.isDarwin [
+      # Build with newer compilers and SDKs
+      ./fix-darwin-build.patch
+    ];
 
-  buildInputs = [
-    SDL
-    SDL_image
-    SDL_mixer
-    SDL_ttf
-    freetype
-    libjpeg
-    libogg
-    libpng
-    libvorbis
-    smpeg
-  ];
+    sourceRoot = lib.optionalDrvAttr stdenv.isDarwin "source/macosx";
 
-  configureFlags = [ "--no-werror" ];
+    nativeBuildInputs =
+      prev.nativeBuildInputs
+      ++ [ pkg-config ]
+      ++ lib.optionals stdenv.isDarwin [
+        makeBinaryWrapper
+        xcbuildHook
+      ];
 
-  strictDeps = true;
+    preUnpack =
+      let
+        onscrlib = buildEnv {
+          name = "onscrlib";
+          paths = final.buildInputs;
+          extraOutputsToInstall = [ "dev" ];
+          extraPrefix = "/onscrlib";
+        };
+      in
+      lib.optionalString stdenv.isDarwin ''
+        ln -s ${onscrlib} onscrlib
+      '';
 
-  preBuild = ''
-    sed -i 's/.dll//g' Makefile
-  '';
+    configureFlags = [
+      "--no-werror"
+    ]
+    ++ lib.optionals (!stdenv.cc.isGNU) [
+      "--unsupported-compiler"
+    ];
 
-  meta = {
-    homepage = "https://github.com/museoa/onscripter-en";
-    description = "Japanese visual novel scripting engine";
-    license = lib.licenses.gpl2Plus;
-    mainProgram = "onscripter-en";
-    maintainers = [ ];
-    platforms = lib.platforms.unix;
-    broken = stdenv.hostPlatform.isDarwin;
-  };
-})
+    makefile = "Makefile";
+
+    preBuild = lib.optionalString (!stdenv.isDarwin) ''
+      sed -i 's/.dll//g' Makefile
+    '';
+
+    installPhase = lib.optionalDrvAttr stdenv.isDarwin ''
+      runHook preInstall
+
+      mkdir -p $out/{bin,Applications}
+      cp -R Products/Release/ONScripter.app $out/Applications
+      makeWrapper $out/Applications/ONScripter.app/Contents/MacOS/ONScripter \
+        $out/bin/${lib.escapeShellArg final.meta.mainProgram}
+
+      runHook postInstall
+    '';
+
+    meta = {
+      homepage = "https://github.com/museoa/onscripter-en";
+      description = "English-focused fork of ONScripter";
+      license = lib.licenses.gpl2Plus;
+      mainProgram = "onscripter-en";
+      maintainers = [ ];
+      platforms = lib.platforms.unix;
+    };
+  }
+)
