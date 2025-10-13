c98661c070138858ee8ce28d6fe45ada74c9d329
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-13 15:11:31 +0000

    home-assistant-custom-components.daikin_onecta: 4.2.8 -> 4.2.9

--- pkgs/servers/home-assistant/custom-components/daikin_onecta/package.nix
+++ pkgs/servers/home-assistant/custom-components/daikin_onecta/package.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent rec {
   owner = "jwillemsen";
   domain = "daikin_onecta";
-  version = "4.2.8";
+  version = "4.2.9";
 
   src = fetchFromGitHub {
     owner = "jwillemsen";
     repo = "daikin_onecta";
     tag = "v${version}";
-    hash = "sha256-ozOJQDoE/t2FhnsVCnP+WCrFr/l19+onr488JN2fSE0=";
+    hash = "sha256-bV+4nTRhtqSVBdaG3rCtIdTYhM4pqcQCtUQwsUhgcq0=";
   };
 
   meta = {
