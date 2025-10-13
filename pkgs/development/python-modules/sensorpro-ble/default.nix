4130914dfb0cb7b7e7b797d2a1ed9fdff17d1fa0
Fabian Affolter <mail@fabian-affolter.ch>
2025-10-13 09:02:48 +0200

    python313Packages.sensorpro-ble: remove disabled
    Removed dependency on pythonOlder for version check.

--- pkgs/development/python-modules/sensorpro-ble/default.nix
+++ pkgs/development/python-modules/sensorpro-ble/default.nix
@@ -7,7 +7,6 @@
   poetry-core,
   pytest-cov-stub,
   pytestCheckHook,
-  pythonOlder,
   sensor-state-data,
 }:
 
@@ -15,8 +14,6 @@ buildPythonPackage rec {
   pname = "sensorpro-ble";
   version = "1.0.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "Bluetooth-Devices";
