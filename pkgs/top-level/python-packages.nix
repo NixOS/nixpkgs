fdb77670d937a2b1e2755084b761e72b87e3b320
Bruno BELANYI <bruno@belanyi.fr>
2025-10-14 09:29:03 +0000

    python3Packages.reflink-copy: init at 0.3.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15833,6 +15833,8 @@ self: super: with self; {
 
   reflink = callPackage ../development/python-modules/reflink { };
 
+  reflink-copy = callPackage ../development/python-modules/reflink-copy { };
+
   refoss-ha = callPackage ../development/python-modules/refoss-ha { };
 
   regenmaschine = callPackage ../development/python-modules/regenmaschine { };
