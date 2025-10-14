51916404848cb1071a03a09b04f287c6359afdf8
dmkhitaryan <davtot111@icloud.com>
2025-10-14 17:57:05 +0400

    mcphost: 0.19.2 -> 0.31.1

--- pkgs/by-name/mc/mcphost/package.nix
+++ pkgs/by-name/mc/mcphost/package.nix
@@ -6,16 +6,18 @@
 
 buildGoModule (finalAttrs: {
   pname = "mcphost";
-  version = "0.19.2";
+  version = "0.31.1";
 
   src = fetchFromGitHub {
     owner = "mark3labs";
     repo = "mcphost";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-BDG4df7YG5cKjL8zsbvK4xFJMwyjSPx4jnDKwPVm+zw=";
+    hash = "sha256-WvoBQLrs9lcbaTueyIPI89Y2t37mKxlE0tOczLhw8Bg=";
   };
 
-  vendorHash = "sha256-U/XtO4OhcioaSU2iGTNmvEilp9+Yu3TVafzNEaFcWEg=";
+  vendorHash = "sha256-xZ7JbX1sAt2ZtgSMm86MQEJWwjL17O9LMsjOmkhzWt0=";
+
+  doCheck = false;
 
   meta = {
     description = "CLI host application that enables Large Language Models (LLMs) to interact with external tools through the Model Context Protocol (MCP)";
