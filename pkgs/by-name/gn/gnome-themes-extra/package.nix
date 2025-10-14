e25e6181a6b975a685758a70c84b4a25ab221066
Stefan Frijters <sfrijters@gmail.com>
2025-10-15 00:51:10 +0200

    gnome-themes-extra: drop gtk2

--- pkgs/by-name/gn/gnome-themes-extra/package.nix
+++ pkgs/by-name/gn/gnome-themes-extra/package.nix
@@ -10,7 +10,6 @@
   pkg-config,
   pango,
   atk,
-  gtk2,
   gdk-pixbuf,
   hicolor-icon-theme,
 }:
@@ -35,17 +34,22 @@ stdenv.mkDerivation rec {
     intltool
     gtk3
   ];
+
   buildInputs = [
     gtk3
     librsvg
     pango
     atk
-    gtk2
     gdk-pixbuf
   ];
+
   propagatedBuildInputs = [
     adwaita-icon-theme
     hicolor-icon-theme
+  ];
+
+  configureFlags = [
+    (lib.enableFeature false "gtk2-engine")
   ];
 
   dontDropIconThemeCache = true;
