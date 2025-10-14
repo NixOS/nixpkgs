cde5f060ef3ffa8e89f5fefb6fa320a77b518bf3
Tom Vincent <git@tlvince.com>
2025-10-14 16:27:01 +0100

    Linting

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1616,8 +1616,8 @@
   ./services/web-apps/icingaweb2/icingaweb2.nix
   ./services/web-apps/icingaweb2/module-monitoring.nix
   ./services/web-apps/ifm.nix
-  ./services/web-apps/immich-public-proxy.nix
   ./services/web-apps/immich-kiosk.nix
+  ./services/web-apps/immich-public-proxy.nix
   ./services/web-apps/immich.nix
   ./services/web-apps/invidious.nix
   ./services/web-apps/isso.nix
