9daa17df884ec37a4ce843084c55e49ca08418b1
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-14 22:03:04 +0000

    python3Packages.livekit-protocol: 1.0.6 -> 1.0.8

--- pkgs/development/python-modules/livekit-protocol/default.nix
+++ pkgs/development/python-modules/livekit-protocol/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "livekit-protocol";
-  version = "1.0.6";
+  version = "1.0.8";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "livekit";
     repo = "python-sdks";
     tag = "protocol-v${version}";
-    hash = "sha256-Sl/pAwiCS7sAY8VHJzSqm/Mj92NsO5NLuxQ/Y5GnaAw=";
+    hash = "sha256-3RdUvxGOopgakwkoWc+IMW2QUZuZLF908KtFo1f0Nqo=";
   };
 
   pypaBuildFlags = [ "livekit-protocol" ];
