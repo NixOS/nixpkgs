900b09179666a052f38f74bb26023676e3c9d1a1
Caleb Norton <n0603919@outlook.com>
2025-10-14 18:39:21 -0500

    maintainers: add gigamonster256

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -9367,6 +9367,14 @@
     github = "Gigahawk";
     githubId = 10356230;
   };
+  gigamonster256 = {
+    email = "n0603919@outlook.com";
+    github = "gigamonster256";
+    githubId = 19415285;
+    matrix = "@gigamonster256:matrix.org";
+    name = "Caleb Norton";
+    keys = [ { fingerprint = "483A 112B 3567 C4F0 DF89  74E1 D776 F570 2D7E 83AB"; } ];
+  };
   giggio = {
     email = "giggio@giggio.net";
     github = "giggio";
