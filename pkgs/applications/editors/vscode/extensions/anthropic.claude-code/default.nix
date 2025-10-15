7c5f691eab4a61a3a760b0248c8aa279a751cce2
Markus Hauck <markus1189@gmail.com>
2025-10-15 19:01:21 +0200

    claude-code: 2.0.15 -> 2.0.17
    https://github.com/anthropics/claude-code/blob/main/CHANGELOG.md

--- pkgs/applications/editors/vscode/extensions/anthropic.claude-code/default.nix
+++ pkgs/applications/editors/vscode/extensions/anthropic.claude-code/default.nix
@@ -7,8 +7,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "claude-code";
     publisher = "anthropic";
-    version = "2.0.15";
-    hash = "sha256-TKN0IR/0pzbcusO1J9n7sodqbrmzw4u4EEXkmFKb3co=";
+    version = "2.0.17";
+    hash = "sha256-8R0BvDfQDJmWD038mywNVIYtmgzyzwSEbqnCHQBpjg8=";
   };
 
   meta = {
