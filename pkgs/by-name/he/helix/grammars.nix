4c5bf2deee96daac9f9c607a425ba6277e03d917
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-10-15 16:30:33 +0200

    helix: build tree-sitter grammars from source

---
+++ pkgs/by-name/he/helix/grammars.nix
@@ -0,0 +1,71 @@
+{
+  lib,
+  stdenv,
+  pkgs,
+}:
+let
+  grammarSources = lib.mapAttrs (
+    name: source:
+    (pkgs.${source.nurl.fetcher} source.nurl.args)
+    // {
+      passthru = { inherit (source) subpath; };
+    }
+  ) (lib.importJSON ./grammars.json);
+
+  mkGrammar =
+    name: source:
+    stdenv.mkDerivation {
+      pname = "helix-tree-sitter-${name}";
+      version = lib.sources.shortRev source.rev;
+
+      src = source;
+      sourceRoot =
+        if isNull source.passthru.subpath then "source" else "source/${source.passthru.subpath}";
+
+      dontConfigure = true;
+
+      FLAGS = [
+        "-Isrc"
+        "-g"
+        "-O3"
+        "-fPIC"
+        "-fno-exceptions"
+        "-Wl,-z,relro,-z,now"
+      ];
+
+      env.NAME = name;
+
+      buildPhase = ''
+        runHook preBuild
+
+        if [[ -e src/scanner.cc ]]; then
+          $CXX -c src/scanner.cc -o scanner.o $FLAGS
+        elif [[ -e src/scanner.c ]]; then
+          $CC -c src/scanner.c -o scanner.o $FLAGS
+        fi
+
+        $CC -c src/parser.c -o parser.o $FLAGS
+        $CXX -shared -o $NAME.so *.o
+
+        runHook postBuild
+      '';
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir $out
+        mv $NAME.so $out/
+
+        runHook postInstall
+      '';
+
+      fixupPhase = ''
+        runHook preFixup
+
+        $STRIP $out/$NAME.so
+
+        runHook postFixup
+      '';
+    };
+in
+lib.mapAttrs mkGrammar grammarSources
