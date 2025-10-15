fb573dc3d00122e9b3581a07e0de0792754f8a95
Manuel Mendez <git@i.m.mmlb.dev>
2025-10-14 21:42:05 -0400

    gdlv: Bunch of clean ups
    * Add updateScript
    * Switch from rev to tag
    * Switch from rec to finalAttrs
    * Drop `with lib.maintainers`

--- pkgs/by-name/gd/gdlv/package.nix
+++ pkgs/by-name/gd/gdlv/package.nix
@@ -2,26 +2,31 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  nix-update-script,
 }:
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "gdlv";
   version = "1.15.0";
 
   src = fetchFromGitHub {
     owner = "aarzilli";
     repo = "gdlv";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-YHv/PfkQh0detM3p62oDWhEG8PWCupaBhwbxz8rHRdI=";
   };
 
   vendorHash = null;
   subPackages = ".";
 
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "GUI frontend for Delve";
     mainProgram = "gdlv";
     homepage = "https://github.com/aarzilli/gdlv";
-    maintainers = with lib.maintainers; [ mmlb ];
+    maintainers = [
+      lib.maintainers.mmlb
+    ];
     license = lib.licenses.gpl3;
   };
-}
+})
