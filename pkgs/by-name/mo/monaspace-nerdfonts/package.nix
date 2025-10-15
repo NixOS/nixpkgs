570f5910ea9a1950fa27e68212e14283321f2bab
Simon Westlin Green <simon.green@live.se>
2025-10-15 18:48:03 +0200

    monaspace-nerdfonts: init to 1.301

---
+++ pkgs/by-name/mo/monaspace-nerdfonts/package.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "monaspace-nerdfonts";
+  version = "1.301";
+
+  src = fetchzip {
+    url = "https://github.com/githubnext/monaspace/releases/download/v${finalAttrs.version}/monaspace-nerdfonts-v${finalAttrs.version}.zip";
+    stripRoot = false;
+    hash = "sha256-okhtInxOcIjS3A2w2q6f4EloMF7I8nBuD5aSreQr9bI=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm644 NerdFonts/**/*.otf -t $out/share/fonts/opentype
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Innovative superfamily of fonts for code";
+    longDescription = ''
+      Since the earliest days of the teletype machine, code has been set in
+      monospaced type — letters, on a grid. Monaspace is a new type system that
+      advances the state of the art for the display of code on screen.
+
+      Every advancement in the technology of computing has been accompanied by
+      advancements to the display and editing of code. CRTs made screen editors
+      possible. The advent of graphical user interfaces gave rise to integrated
+      development environments.
+
+      Even today, we still have limited options when we want to layer additional
+      meaning on top of code. Syntax highlighting was invented in 1982 to help
+      children to code in BASIC. But beyond colors, most editors must
+      communicate with developers through their interfaces — hovers, underlines,
+      and other graphical decorations.
+
+      Monaspace offers a more expressive palette for code and the tools we use
+      to work with it.
+    '';
+    homepage = "https://monaspace.githubnext.com/";
+    license = lib.licenses.ofl;
+    maintainers = [ ];
+    platforms = lib.platforms.all;
+  };
+})
