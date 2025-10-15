927499e9d2661b545786b766e7a037cc3462d98f
Sarah Clark <seclark@nextquestion.net>
2025-10-15 18:13:18 +0000

    python312Packages.langchain-aws: 0.2.33 -> 0.2.35

--- pkgs/development/python-modules/langchain-aws/default.nix
+++ pkgs/development/python-modules/langchain-aws/default.nix
@@ -24,14 +24,14 @@
 
 buildPythonPackage rec {
   pname = "langchain-aws";
-  version = "0.2.33";
+  version = "0.2.35";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langchain-aws";
     tag = "langchain-aws==${version}";
-    hash = "sha256-8VRZhwrR5PdLo9FamQClKbYfuHUGVxamku4osyl8Wl4=";
+    hash = "sha256-O48zsfN34t5To/TlOsxbgs2GzkzA30NdclDPuQ2fJoc=";
   };
 
   postPatch = ''
