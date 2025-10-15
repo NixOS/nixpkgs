72a533422c2257f50cf240d8ba21339b4eb09dd8
Vladimír Čunát <v@cunat.cz>
2025-10-15 11:38:20 +0200

    python3Packages.setproctitle: avoid rebuild on linux for now

--- pkgs/development/python-modules/setproctitle/default.nix
+++ pkgs/development/python-modules/setproctitle/default.nix
@@ -28,7 +28,7 @@ buildPythonPackage rec {
   ];
 
   # Setting the process title fails on macOS in the Nix builder environment (regardless of sandboxing)
-  disabledTests = lib.optionals stdenv.hostPlatform.isDarwin [ "test_setproctitle_darwin" ];
+  disabledTests = if stdenv.hostPlatform.isDarwin then [ "test_setproctitle_darwin" ] else null;
 
   pythonImportsCheck = [ "setproctitle" ];
 
