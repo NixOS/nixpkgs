4375546ab1ccbd89ff5d73e0d6255ff11b554500
Aaron Jheng <wentworth@outlook.com>
2025-10-15 11:18:07 +0800

    mysql-shell-innovation: fix build

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -535,6 +535,7 @@ with pkgs;
     protobuf = protobuf_25.override {
       abseil-cpp = abseil-cpp_202407;
     };
+    stdenv = if stdenv.hostPlatform.isDarwin then llvmPackages_19.stdenv else stdenv;
   };
 
   # this is used by most `fetch*` functions
