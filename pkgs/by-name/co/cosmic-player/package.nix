14485dcfd29f874ea955fcf8ee3f03a5a2a4ed70
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-10-15 14:05:53 -0300

    cosmic-player: 1.0.0-beta.1.1 -> 1.0.0-beta.2
    Diff: https://github.com/pop-os/cosmic-player/compare/epoch-1.0.0-beta.1.1...epoch-1.0.0-beta.2

--- pkgs/by-name/co/cosmic-player/package.nix
+++ pkgs/by-name/co/cosmic-player/package.nix
@@ -18,17 +18,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-player";
-  version = "1.0.0-beta.1.1";
+  version = "1.0.0-beta.2";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-player";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-oTTVVQkSkON5NTgO5+eUD2wVpiW5MvW3MZyeyqqc3qk=";
+    hash = "sha256-jf9KSgA94SL6JX7Nh1BQiumOYBJjjt5O5DRCjeBHovo=";
   };
 
-  cargoHash = "sha256-DodFIfthiGFSvXWfPsPjFhNY6G7z3lb6pfc5HtUXhMo=";
+  cargoHash = "sha256-fnX5BkzRAetKxHZ9XyWdmG6TSxFqGJsmg16zlpYG9Ag=";
 
   nativeBuildInputs = [
     just
