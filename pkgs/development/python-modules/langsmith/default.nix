bb8e8311875affaa3b970b731456eeb75cb6d6b7
Sarah Clark <seclark@nextquestion.net>
2025-10-15 18:15:06 +0000

    python312Packages.langsmith: 0.4.34 -> 0.4.35

--- pkgs/development/python-modules/langsmith/default.nix
+++ pkgs/development/python-modules/langsmith/default.nix
@@ -31,14 +31,14 @@
 
 buildPythonPackage rec {
   pname = "langsmith";
-  version = "0.4.34";
+  version = "0.4.35";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langsmith-sdk";
     tag = "v${version}";
-    hash = "sha256-vuet4/Y5MW2Ob4zISl9vf3PD/OlmA/4/v7awVuvhk8E=";
+    hash = "sha256-Ab+K79RDSg5nNYxeJynU5UduB17WDuMF99t+v1eYRhQ=";
   };
 
   sourceRoot = "${src.name}/python";
