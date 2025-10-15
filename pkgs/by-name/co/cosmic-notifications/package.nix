443aacabb01e630614a649154b579d8b62e577ca
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-10-15 14:02:02 -0300

    cosmic-notifications: 1.0.0-beta.1.1 -> 1.0.0-beta.2
    Diff: https://github.com/pop-os/cosmic-notifications/compare/epoch-1.0.0-beta.1.1...epoch-1.0.0-beta.2

--- pkgs/by-name/co/cosmic-notifications/package.nix
+++ pkgs/by-name/co/cosmic-notifications/package.nix
@@ -12,14 +12,14 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-notifications";
-  version = "1.0.0-beta.1.1";
+  version = "1.0.0-beta.2";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-notifications";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-wgOjaiKJ1KYdYsynQV5+KKGhdneELiLTHYqjMEWaxt0=";
+    hash = "sha256-HMs08kAS+dC8GsznmQveZczwYtlSKxS4MU3BEKLgxjY=";
   };
 
   cargoHash = "sha256-CL8xvj57yq0qzK3tyYh3YXh+fM4ZDsmL8nP1mcqTqeQ=";
