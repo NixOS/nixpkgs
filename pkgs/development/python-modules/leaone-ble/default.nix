c6c0dc40f1d914b7dce2b3d370f2ef9745599df7
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-15 02:46:06 +0000

    python3Packages.leaone-ble: 0.3.0 -> 1.0.0

--- pkgs/development/python-modules/leaone-ble/default.nix
+++ pkgs/development/python-modules/leaone-ble/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "leaone-ble";
-  version = "0.3.0";
+  version = "1.0.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "bluetooth-devices";
     repo = "leaone-ble";
     tag = "v${version}";
-    hash = "sha256-96TOjjz4EkHAnzL53BIR+PifkyrEig/0r+mIfnwc0hE=";
+    hash = "sha256-WdsAqXv3U2E5x6Io4t3NNcioV6kwREDATe4hpojzUBo=";
   };
 
   build-system = [ poetry-core ];
