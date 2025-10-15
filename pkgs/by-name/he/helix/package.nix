0b8ad91220f7f06ddba4b05fe9b6ff39d0994309
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-10-15 16:30:33 +0200

    helix: build tree-sitter grammars from source

--- pkgs/by-name/he/helix/package.nix
+++ pkgs/by-name/he/helix/package.nix
@@ -1,78 +1,141 @@
 {
-  fetchzip,
+  fetchFromGitHub,
   lib,
   rustPlatform,
   mdbook,
-  git,
+  gitMinimal,
   installShellFiles,
   versionCheckHook,
-  nix-update-script,
+  runCommand,
+  removeReferencesTo,
+  pkgs,
+  tree-sitter,
+  lockedGrammars ? lib.importJSON ./grammars.json,
+  grammarsOverlay ? (
+    final: prev: {
+      tree-sitter-sql = prev.tree-sitter-sql.override {
+        generate = false;
+      };
+      tree-sitter-qmljs = prev.tree-sitter-qmljs.overrideAttrs {
+        dontCheckForBrokenSymlinks = true;
+      };
+    }
+  ),
 }:
 
-rustPlatform.buildRustPackage (final: {
-  pname = "helix";
-  version = "25.07.1";
-  outputs = [
-    "out"
-    "doc"
-  ];
+rustPlatform.buildRustPackage (
+  finalAttrs:
+  let
+    lockedVersionsOverlay =
+      final: prev:
+      lib.mapAttrs (
+        drvName: grammar:
+        let
+          lockedGrammar = lockedGrammars.${lib.removePrefix "tree-sitter-" drvName};
+        in
+        (prev.${drvName}.override {
+          location = lockedGrammar.subpath;
+        }).overrideAttrs
+          {
+            version = lib.sources.shortRev lockedGrammar.nurl.args.rev;
+            src = (pkgs.${lockedGrammar.nurl.fetcher} lockedGrammar.nurl.args);
+          }
+      ) prev;
 
-  # This release tarball includes source code for the tree-sitter grammars,
-  # which is not ordinarily part of the repository.
-  src = fetchzip {
-    url = "https://github.com/helix-editor/helix/releases/download/${final.version}/helix-${final.version}-source.tar.xz";
-    hash = "sha256-Pj/lfcQXRWqBOTTWt6+Gk61F9F1UmeCYr+26hGdG974=";
-    stripRoot = false;
-  };
+    tree-sitter-grammars =
+      lib.filterAttrs (drvName: _: lib.hasAttr (lib.removePrefix "tree-sitter-" drvName) lockedGrammars)
+        (
+          tree-sitter.grammarsScope.overrideScope (
+            lib.composeExtensions lockedVersionsOverlay grammarsOverlay
+          )
+        );
 
-  cargoHash = "sha256-Mf0nrgMk1MlZkSyUN6mlM5lmTcrOHn3xBNzmVGtApEU=";
+    # Dynamic libraries for the grammars always use the `.so` extension, also on Darwin
+    # See here: https://github.com/helix-editor/helix/blob/master/helix-loader/src/grammar.rs
+    grammarsFarm = runCommand "helix-grammars" { } (
+      lib.concatMapAttrsStringSep "\n" (_: grammar: ''
+        install -D ${grammar}/parser $out/${grammar.language}.so
+        ${lib.getExe removeReferencesTo} -t ${grammar} $out/${grammar.language}.so
+      '') (lib.filterAttrs (_: lib.isDerivation) tree-sitter-grammars)
+    );
 
-  nativeBuildInputs = [
-    git
-    installShellFiles
-    mdbook
-  ];
+    lockedGrammarsCount = lib.length (lib.attrNames lockedGrammars);
 
-  env.HELIX_DEFAULT_RUNTIME = "${placeholder "out"}/lib/runtime";
-
-  postBuild = ''
-    mdbook build book -d ../book-html
-  '';
-
-  postInstall = ''
-    # not needed at runtime
-    rm -r runtime/grammars/sources
-
-    mkdir -p $out/lib $doc/share/doc
-    cp -r runtime $out/lib
-    installShellCompletion contrib/completion/hx.{bash,fish,zsh}
-    mkdir -p $out/share/{applications,icons/hicolor/256x256/apps}
-    cp contrib/Helix.desktop $out/share/applications
-    cp contrib/helix.png $out/share/icons/hicolor/256x256/apps
-    cp -r book-html $doc/share/doc/$name
-  '';
-
-  nativeInstallCheckInputs = [
-    versionCheckHook
-  ];
-  versionCheckProgram = "${placeholder "out"}/bin/hx";
-  versionCheckProgramArg = "--version";
-  doInstallCheck = true;
-
-  passthru = {
-    updateScript = nix-update-script { };
-  };
-
-  meta = {
-    description = "Post-modern modal text editor";
-    homepage = "https://helix-editor.com";
-    changelog = "https://github.com/helix-editor/helix/blob/${final.version}/CHANGELOG.md";
-    license = lib.licenses.mpl20;
-    mainProgram = "hx";
-    maintainers = with lib.maintainers; [
-      aciceri
-      danth
-      yusdacra
+    runtimeDir = runCommand "helix-runtime" { } ''
+      cp -r --no-preserve=mode ${finalAttrs.src}/runtime $out
+      rm -r $out/grammars
+      ln -s ${grammarsFarm} $out/grammars
+      count=$(ls -1 "$out/grammars/" | wc -l)
+      if [ "$count" -ne ${toString lockedGrammarsCount} ]; then
+        echo "Expected ${toString lockedGrammarsCount} grammars, found $count"
+        exit 1
+      fi
+    '';
+  in
+  {
+    pname = "helix";
+    version = "25.07.1";
+    outputs = [
+      "out"
+      "doc"
     ];
-  };
-})
+
+    src = fetchFromGitHub {
+      owner = "helix-editor";
+      repo = "helix";
+      tag = "${finalAttrs.version}";
+      hash = "sha256-RFSzGAcB0mMg/02ykYfTWXzQjLFu2CJ4BkS5HZ/6pBo=";
+    };
+
+    cargoHash = "sha256-Mf0nrgMk1MlZkSyUN6mlM5lmTcrOHn3xBNzmVGtApEU=";
+
+    nativeBuildInputs = [
+      gitMinimal
+      installShellFiles
+      mdbook
+    ];
+
+    env = {
+      HELIX_DEFAULT_RUNTIME = runtimeDir;
+      HELIX_DISABLE_AUTO_GRAMMAR_BUILD = "1";
+    };
+
+    postBuild = ''
+      mdbook build book -d ../book-html
+    '';
+
+    postInstall = ''
+      mkdir -p $out/lib $doc/share/doc
+      installShellCompletion contrib/completion/hx.{bash,fish,zsh}
+      mkdir -p $out/share/{applications,icons/hicolor/256x256/apps}
+      cp contrib/Helix.desktop $out/share/applications/Helix.desktop
+      cp contrib/helix.png $out/share/icons/hicolor/256x256/apps/helix.png
+    '';
+
+    nativeInstallCheckInputs = [
+      versionCheckHook
+    ];
+    versionCheckProgram = "${placeholder "out"}/bin/hx";
+    versionCheckProgramArg = "--version";
+    doInstallCheck = true;
+
+    passthru = {
+      updateScript = ./update.sh;
+      runtime = runtimeDir;
+      inherit tree-sitter-grammars;
+    };
+
+    meta = {
+      description = "Post-modern modal text editor";
+      homepage = "https://helix-editor.com";
+      changelog = "https://github.com/helix-editor/helix/blob/${finalAttrs.version}/CHANGELOG.md";
+      license = lib.licenses.mpl20;
+      mainProgram = "hx";
+      maintainers = with lib.maintainers; [
+        aciceri
+        danth
+        yusdacra
+      ];
+    };
+  }
+)
