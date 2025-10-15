f899b2c0075e1a91627fe9a2c1dc56c3d29c38ad
h7x4 <h7x4@nani.wtf>
2025-10-15 22:28:04 +0900

    nixos/tandoor-recipes: replace `preStart` with `ExecStartPre`

--- nixos/modules/services/misc/tandoor-recipes.nix
+++ nixos/modules/services/misc/tandoor-recipes.nix
@@ -119,6 +119,12 @@ in
       after = lib.optional cfg.database.createLocally "postgresql.target";
 
       serviceConfig = {
+        ExecStartPre = [
+          "${lib.getExe' pkgs.coreutils "ln"} -sf ${manage} tandoor-recipes-manage"
+
+          # Let django migrate the DB as needed
+          "${lib.getExe pkg} migrate"
+        ];
         ExecStart = ''
           ${pkg.python.pkgs.gunicorn}/bin/gunicorn recipes.wsgi
         '';
@@ -171,13 +177,6 @@ in
       };
 
       wantedBy = [ "multi-user.target" ];
-
-      preStart = ''
-        ln -sf ${manage} tandoor-recipes-manage
-
-        # Let django migrate the DB as needed
-        ${pkg}/bin/tandoor-recipes migrate
-      '';
 
       environment = env // {
         PYTHONPATH = "${pkg.python.pkgs.makePythonPath pkg.propagatedBuildInputs}:${pkg}/lib/tandoor-recipes";
