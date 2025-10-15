4abe18bd0e00868f5015a90c64e08618b16f1f20
Sarah Clark <seclark@nextquestion.net>
2025-10-15 18:15:41 +0000

    python312Packages.langgraph-checkpoint: 2.1.1 -> 2.1.2

--- pkgs/development/python-modules/langgraph-checkpoint/default.nix
+++ pkgs/development/python-modules/langgraph-checkpoint/default.nix
@@ -18,6 +18,7 @@
   pytest-asyncio,
   pytest-mock,
   pytestCheckHook,
+  redis,
 
   # passthru
   gitUpdater,
@@ -25,14 +26,14 @@
 
 buildPythonPackage rec {
   pname = "langgraph-checkpoint";
-  version = "2.1.1";
+  version = "2.1.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langgraph";
     tag = "checkpoint==${version}";
-    hash = "sha256-UY3AChShKfOrtOQzOm5vi3Yy3rlBc+TAje9L2L6My/U=";
+    hash = "sha256-91q+U1KfE5eXm6jAd1PNZzZZgpTyNCaF1YVoHYwOrRA=";
   };
 
   sourceRoot = "${src.name}/libs/checkpoint";
@@ -55,12 +56,21 @@ buildPythonPackage rec {
     pytest-asyncio
     pytest-mock
     pytestCheckHook
+    redis
   ];
 
   disabledTests = [
     # assert 1.0000000000000004 == 1.0000000000000002
     # https://github.com/langchain-ai/langgraph/issues/5845
     "test_embed_with_path"
+  ];
+
+  disabledTestPaths = [
+    # psycopg.errors.FeatureNotSupported: extension "vector" is not available
+    "tests/test_store.py::test_non_ascii[vector-cosine]"
+    "tests/test_store.py::test_non_ascii[vector-inner_product]"
+    "tests/test_store.py::test_non_ascii[halfvec-cosine]"
+    "tests/test_store.py::test_non_ascii[halfvec-inner_product]"
   ];
 
   passthru = {
