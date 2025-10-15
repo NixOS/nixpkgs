0324a38be1b05380e92483bd1a837b26b28ac938
philippschuetz <mail@philippschuetz.com>
2025-10-15 11:59:58 +0200

    maintainers: add philippschuetz

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20202,6 +20202,12 @@
     githubId = 9267430;
     name = "Philipp Mildenberger";
   };
+  philippschuetz = {
+    email = "mail@philippschuetz.com";
+    github = "philippschuetz";
+    name = "Philipp Schütz";
+    githubId = 82471105;
+  };
   philiptaron = {
     email = "philip.taron@gmail.com";
     github = "philiptaron";
