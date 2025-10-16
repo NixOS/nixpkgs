dabf0588de9bba0d08a202995bd557d6fae2fcf9
Connor <cspear1994@gmail.com>
2025-10-16 09:59:36 -0400

    Run treefmt

--- pkgs/by-name/vi/vintagestory-server/package.nix
+++ pkgs/by-name/vi/vintagestory-server/package.nix
@@ -1,17 +1,29 @@
-{ lib, stdenv, fetchurl, autoPatchelfHook, dotnet-runtime_8, makeWrapper }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  dotnet-runtime_8,
+  makeWrapper,
+}:
 
 stdenv.mkDerivation rec {
   pname = "vintagestory-server";
   version = "1.21.5";
 
   src = fetchurl {
-    url =
-      "https://cdn.vintagestory.at/gamefiles/stable/vs_server_linux-x64_${version}.tar.gz";
+    url = "https://cdn.vintagestory.at/gamefiles/stable/vs_server_linux-x64_${version}.tar.gz";
     hash = "sha256-Js9l53S2156whJlV/D3m0id+lDerEfsQaMotWJ0sygM=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
-  buildInputs = [ stdenv.cc.cc.lib dotnet-runtime_8 ];
+  nativeBuildInputs = [
+    autoPatchelfHook
+    makeWrapper
+  ];
+  buildInputs = [
+    stdenv.cc.cc.lib
+    dotnet-runtime_8
+  ];
 
   sourceRoot = ".";
 
@@ -29,8 +41,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     homepage = "https://vintagestory.at/";
-    description =
-      "Dedicated server for Vintage Story, an in-development indie sandbox game about innovation and exploration";
+    description = "Dedicated server for Vintage Story, an in-development indie sandbox game about innovation and exploration";
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     platforms = [ "x86_64-linux" ];
     license = licenses.unfree;
