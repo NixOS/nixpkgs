56dd0de5402c872d6b1218d851df99c9189dc999
Connor <cspear1994@gmail.com>
2025-10-16 08:56:06 -0400

    Rename vintagestory-server.nix to package.nix

---
+++ pkgs/by-name/vi/vintagestory-server/package.nix
@@ -0,0 +1,55 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  dotnet-runtime_8,
+  makeWrapper,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "vintagestory-server";
+  version = "1.21.5";
+
+  src = fetchurl {
+    url = "https://cdn.vintagestory.at/gamefiles/stable/vs_server_linux-x64_${version}.tar.gz";
+    hash = "sha256-Js9l53S2156whJlV/D3m0id+lDerEfsQaMotWJ0sygM=";
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    makeWrapper
+  ];
+  buildInputs = [
+    stdenv.cc.cc.lib
+    dotnet-runtime_8
+  ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp -r * $out/
+
+    makeWrapper '${lib.getExe dotnet-runtime_8}' "$out/bin/vintagestory-server" --append-flags "$out/VintagestoryServer.dll"
+
+    chmod +x $out/bin/vintagestory-server
+
+    wrapProgram $out/bin/vintagestory-server \
+      --prefix PATH : ${lib.makeBinPath [ dotnet-runtime_8 ]}
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://vintagestory.at/";
+    description = "Dedicated server for Vintage Story, an in-development indie sandbox game about innovation and exploration";
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    platforms = [ "x86_64-linux" ];
+    license = licenses.unfree;
+    mainProgram = "VintageStoryServer";
+    maintainers = with maintainers; [ cspeardev ];
+  };
+}
