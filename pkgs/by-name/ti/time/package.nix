2ab5c82da0a39ba51b04194f943c9bdbe2d31528
Randy Eckenrode <randy@largeandhighquality.com>
2025-10-16 14:28:40 -0400

    time: fix build on non-Linux
    The patch applied in https://github.com/NixOS/nixpkgs/pull/442631 uses
    `__sighandler_t`, which is glibc-specific. This alternate patch posted
    to the bug-time mailing list fixes the issue in a more portable way.

--- pkgs/by-name/ti/time/package.nix
+++ pkgs/by-name/ti/time/package.nix
@@ -18,10 +18,7 @@ stdenv.mkDerivation (finalAttrs: {
     # fixes cross-compilation to riscv64-linux
     ./time-1.9-implicit-func-decl-clang.patch
     # fix compilation with gcc15
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/time/raw/191440912c2e9a63af87802e507ca3ccb923e805/f/time-1.9-Fix-compiling-with-GCC15.patch";
-      hash = "sha256-4Qp3mV8XuCmz518GPtrW52gyaPOb+97RE6FDPKNCyJw=";
-    })
+    ./c23-fix.patch
   ];
 
   meta = {
