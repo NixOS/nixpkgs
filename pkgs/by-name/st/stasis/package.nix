5cba344dfb709db813f2d576dc0f3b9c4e588909
nartsisss <nartsiss@proton.me>
2025-10-16 12:18:08 +0300

    stasis: init at 0.4.1

---
+++ pkgs/by-name/st/stasis/package.nix
@@ -0,0 +1,73 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  installShellFiles,
+  versionCheckHook,
+  nix-update-script,
+  wayland-scanner,
+  wayland,
+  wayland-protocols,
+  dbus,
+  pkg-config,
+  libinput,
+  udev,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "stasis";
+  version = "0.4.1";
+
+  src = fetchFromGitHub {
+    owner = "saltnpepper97";
+    repo = "stasis";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Mqk4lNkp6x/X/JkvhwibiSMkzmmYyPQgwDNps4XAfkE=";
+  };
+
+  cargoHash = "sha256-u25sbnp1RnivU/jkM8uz7fUDP1EVRWiXCRcA1b0XUfA=";
+
+  nativeBuildInputs = [
+    installShellFiles
+    pkg-config
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    wayland
+    wayland-protocols
+    dbus
+    libinput
+    udev
+  ];
+
+  #There are no tests
+  doCheck = false;
+
+  postInstall = ''
+    installManPage man/stasis.5
+  '';
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Modern idle manager for Wayland";
+    longDescription = ''
+      Stasis is a smart idle manager for Wayland that understands context.
+      It automatically prevents idle when watching videos, reading documents,
+      or playing music, while allowing idle when appropriate. Features include
+      media-aware idle handling, application-specific inhibitors, Wayland idle
+      inhibitor protocol support, and flexible configuration using the RUNE
+      configuration language.
+    '';
+    homepage = "https://github.com/saltnpepper97/stasis";
+    changelog = "https://github.com/saltnpepper97/stasis/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ nartsiss ];
+    platforms = lib.platforms.linux;
+    mainProgram = "stasis";
+  };
+})
