7c2bf2960584c90f185d87855a3fdf1565e54201
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-10-16 14:39:11 +0200

    signal-desktop-bin(aarch64-linux): 7.71.0 -> 7.75.0

--- pkgs/by-name/si/signal-desktop-bin/signal-desktop-aarch64.nix
+++ pkgs/by-name/si/signal-desktop-bin/signal-desktop-aarch64.nix
@@ -1,7 +1,7 @@
 { callPackage, commandLineArgs }:
 callPackage ./generic.nix { inherit commandLineArgs; } {
   pname = "signal-desktop-bin";
-  version = "7.71.0";
+  version = "7.75.0";
 
   libdir = "usr/lib64/signal-desktop";
   bindir = "usr/bin";
@@ -10,6 +10,6 @@ callPackage ./generic.nix { inherit commandLineArgs; } {
     bsdtar -xf $downloadedFile -C "$out"
   '';
 
-  url = "https://download.copr.fedorainfracloud.org/results/useidel/signal-desktop/fedora-42-aarch64/09571107-signal-desktop/signal-desktop-7.71.0-1.fc42.aarch64.rpm";
-  hash = "sha256-sNGIkO2HAXl0ykFyZNNV75iVUQ+oRGv6NZW8tVUxfJA=";
+  url = "https://download.copr.fedorainfracloud.org/results/useidel/signal-desktop/fedora-42-aarch64/09693393-signal-desktop/signal-desktop-7.75.0-1.fc42.aarch64.rpm";
+  hash = "sha256-75M7JGPsB1Yog8fY0iDN0NmE0tJ/4k0p7fqXQfWxUw8=";
 }
