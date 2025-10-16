0fdbe8f3a647d846cdfcaefbe93276e8d093fb29
Connor <cspear1994@gmail.com>
2025-10-16 09:58:15 -0400

    Change wrapper to use meta.mainProgram and --add-flags instead of --append-flags

--- pkgs/by-name/vi/vintagestory-server/package.nix
+++ pkgs/by-name/vi/vintagestory-server/package.nix
@@ -1,29 +1,17 @@
-{
-  lib,
-  stdenv,
-  fetchurl,
-  autoPatchelfHook,
-  dotnet-runtime_8,
-  makeWrapper,
-}:
+{ lib, stdenv, fetchurl, autoPatchelfHook, dotnet-runtime_8, makeWrapper }:
 
 stdenv.mkDerivation rec {
   pname = "vintagestory-server";
   version = "1.21.5";
 
   src = fetchurl {
-    url = "https://cdn.vintagestory.at/gamefiles/stable/vs_server_linux-x64_${version}.tar.gz";
+    url =
+      "https://cdn.vintagestory.at/gamefiles/stable/vs_server_linux-x64_${version}.tar.gz";
     hash = "sha256-Js9l53S2156whJlV/D3m0id+lDerEfsQaMotWJ0sygM=";
   };
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-    makeWrapper
-  ];
-  buildInputs = [
-    stdenv.cc.cc.lib
-    dotnet-runtime_8
-  ];
+  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
+  buildInputs = [ stdenv.cc.cc.lib dotnet-runtime_8 ];
 
   sourceRoot = ".";
 
@@ -33,15 +21,16 @@ stdenv.mkDerivation rec {
     mkdir -p $out/bin
     cp -r * $out/
 
-    makeWrapper '${lib.getExe dotnet-runtime_8}' "$out/bin/vintagestory-server" \
-      --append-flags "$out/VintagestoryServer.dll"
+    makeWrapper '${lib.getExe dotnet-runtime_8}' "$out/bin/${meta.mainProgram}" \
+      --add-flags "$out/VintagestoryServer.dll"
 
     runHook postInstall
   '';
 
   meta = with lib; {
     homepage = "https://vintagestory.at/";
-    description = "Dedicated server for Vintage Story, an in-development indie sandbox game about innovation and exploration";
+    description =
+      "Dedicated server for Vintage Story, an in-development indie sandbox game about innovation and exploration";
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     platforms = [ "x86_64-linux" ];
     license = licenses.unfree;
