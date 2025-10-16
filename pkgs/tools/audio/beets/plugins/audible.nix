72f016b8e1c8575c29c93cd2fcbd12493fcb71d8
Doron Behar <doron.behar@gmail.com>
2025-10-16 12:02:39 +0300

    beetsPackages.audible: small formatting changes
    Mainly, put build-system right after src, as it is strongly coupled to
    it.

--- pkgs/tools/audio/beets/plugins/audible.nix
+++ pkgs/tools/audio/beets/plugins/audible.nix
@@ -5,6 +5,7 @@
   nix-update-script,
   python3Packages,
 }:
+
 python3Packages.buildPythonApplication rec {
   pname = "beets-audible";
   version = "1.0.2";
@@ -17,15 +18,15 @@ python3Packages.buildPythonApplication rec {
     hash = "sha256-6rf8U63SW+gwfT7ZdN/ymYKHRs0HSMDTP2ZBfULLsJs=";
   };
 
+  build-system = with python3Packages; [
+    hatchling
+  ];
+
   nativeBuildInputs = [
     beets
   ];
 
   pythonRelaxDeps = true;
-
-  build-system = with python3Packages; [
-    hatchling
-  ];
 
   dependencies = with python3Packages; [
     markdownify
