857beb6d531e914028559ee1452740058ebc2b9c
DeimElias <deimelias@gmail.com>
2025-10-16 14:34:47 -0600

    cups-zj-58:"2018-02-22" -> "2019-02-28"

--- pkgs/by-name/cu/cups-zj-58/package.nix
+++ pkgs/by-name/cu/cups-zj-58/package.nix
@@ -3,31 +3,42 @@
   stdenv,
   fetchFromGitHub,
   cups,
+  cmake,
+  pkg-config,
 }:
 
 stdenv.mkDerivation {
   pname = "cups-zj-58";
-  version = "2018-02-22";
+  version = "2019-04-28";
 
   src = fetchFromGitHub {
     owner = "klirichek";
     repo = "zj-58";
-    rev = "e4212cd702113a4bd4d7013744291c9f60bc5273";
-    sha256 = "1w2qkspm4qqg5h8n6gmakzhiww7gag64chvy9kf89xsl3wsyp6pi";
+    rev = "64743565df4379098b68a197d074c86617a8fc0a";
+    sha256 = "sha256-4l9NRfp0hiPDC6dtFsq7jLf0Gn9tktGy6oZ4GHxSfbw=";
   };
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
 
   buildInputs = [ cups ];
 
   installPhase = ''
+    install -D ppd/zj80.ppd $out/share/cups/model/zjiang/zj80.ppd
+    install -D ppd/zj58.ppd $out/share/cups/model/zjiang/zj58.ppd
     install -D rastertozj $out/lib/cups/filter/rastertozj
-    install -D ZJ-58.ppd $out/share/cups/model/zjiang/ZJ-58.ppd
   '';
 
   meta = with lib; {
     description = "CUPS filter for thermal printer Zjiang ZJ-58";
     homepage = "https://github.com/klirichek/zj-58";
     platforms = platforms.linux;
-    maintainers = with maintainers; [ makefu ];
+    maintainers = with maintainers; [
+      makefu
+      deimelias
+    ];
     license = licenses.bsd2;
   };
 }
