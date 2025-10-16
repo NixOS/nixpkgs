dd558a6f146c1d8f9aab7bb926c3f716736df58e
Rafael Ieda <git@ieda.me>
2025-10-16 19:43:28 -0300

    rare: 1.10.11 -> 1.11.3

--- pkgs/by-name/ra/rare/package.nix
+++ pkgs/by-name/ra/rare/package.nix
@@ -1,45 +1,53 @@
 {
   lib,
   fetchFromGitHub,
-  qt5,
+  qt6,
   legendary-gl,
   python3Packages,
 }:
 
 python3Packages.buildPythonApplication rec {
   pname = "rare";
-  version = "1.10.11";
+  version = "1.11.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "RareDevs";
     repo = "Rare";
     tag = version;
-    hash = "sha256-2DtI5iaK4bYdGfIEhPy52WaEqh+IJMZ6qo/348lMnLY=";
+    hash = "sha256-RYPPR1nBBG7c7Eq8A+vc6OE33JIpe9VqgjDp9YCZz9k=";
   };
 
   nativeBuildInputs = [
-    qt5.wrapQtAppsHook
+    qt6.wrapQtAppsHook
+    qt6.qtbase
   ];
 
   propagatedBuildInputs = [
     legendary-gl
+    python3Packages.pyside6
+    python3Packages.vdf
   ];
 
   build-system = with python3Packages; [
     setuptools
+    setuptools-scm
   ];
 
   dependencies = with python3Packages; [
     orjson
     pypresence
-    pyqt5
     qtawesome
     requests
-    typing-extensions
   ];
 
   dontWrapQtApps = true;
+
+  postPatch = ''
+    # PySide6-Essentials is a smaller version of PySide6, but the name mismatch breaks build
+    substituteInPlace pyproject.toml \
+      --replace-fail "PySide6-Essentials" "pyside6"
+  '';
 
   postInstall = ''
     install -Dm644 misc/rare.desktop -t $out/share/applications/
