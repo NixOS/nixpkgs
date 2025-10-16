0a9721f8cd35eddfeb3e89b7246f224fc6f81303
Boris Nagaev <bnagaev@gmail.com>
2025-10-16 16:46:59 -0300

    lightning-loop: add starius to maintainers

--- pkgs/by-name/li/lightning-loop/package.nix
+++ pkgs/by-name/li/lightning-loop/package.nix
@@ -38,6 +38,9 @@ buildGoModule rec {
     description = "Lightning Loop Client";
     homepage = "https://github.com/lightninglabs/loop";
     license = licenses.mit;
-    maintainers = with maintainers; [ proofofkeags ];
+    maintainers = with maintainers; [
+      proofofkeags
+      starius
+    ];
   };
 }
