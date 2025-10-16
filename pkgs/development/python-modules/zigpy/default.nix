da1492909cfeeb191100395ab5608cfa93318065
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-16 18:38:23 +0000

    python3Packages.zigpy: 0.83.0 -> 0.84.0

--- pkgs/development/python-modules/zigpy/default.nix
+++ pkgs/development/python-modules/zigpy/default.nix
@@ -25,14 +25,14 @@
 
 buildPythonPackage rec {
   pname = "zigpy";
-  version = "0.83.0";
+  version = "0.84.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "zigpy";
     repo = "zigpy";
     tag = version;
-    hash = "sha256-rgri4aga8qTlcURypCHY1YjClz5lpQpwxd3cAG2VoOo=";
+    hash = "sha256-NfV/1mlzvGfwJV6XUJhsUXUAqK/oJYHZFPg5/PYkvHA=";
   };
 
   postPatch = ''
