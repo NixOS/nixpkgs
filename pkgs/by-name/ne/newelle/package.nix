2c54804693823354d26b7ee8c86a70b318922bcb
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-17 05:31:04 +0000

    newelle: 1.0.1 -> 1.0.2

--- pkgs/by-name/ne/newelle/package.nix
+++ pkgs/by-name/ne/newelle/package.nix
@@ -20,7 +20,7 @@
 }:
 
 let
-  version = "1.0.1";
+  version = "1.0.2";
 in
 python3Packages.buildPythonApplication {
   pname = "newelle";
@@ -31,7 +31,7 @@ python3Packages.buildPythonApplication {
     owner = "qwersyk";
     repo = "Newelle";
     tag = version;
-    hash = "sha256-l/eNf1KLZeuDWf1A/5Y8fggOovKmjW6UFNMvNxuJXkc=";
+    hash = "sha256-xg6/1hA0tGW9iDZr7Y1DH2WxcdVDg8FdJ+8mnA4bazk=";
   };
 
   postPatch = ''
