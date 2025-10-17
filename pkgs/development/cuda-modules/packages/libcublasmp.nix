89fb3c2e80438186d550268e463c7bebb3a518e8
Connor Baker <ConnorBaker01@gmail.com>
2025-10-17 07:19:18 +0000

    cudaPackages.libcublasmp: use newly packaged libnvshmem
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- pkgs/development/cuda-modules/packages/libcublasmp.nix
+++ pkgs/development/cuda-modules/packages/libcublasmp.nix
@@ -1,8 +1,11 @@
 {
   _cuda,
   buildRedist,
+  cuda_cudart,
+  lib,
   libcublas,
-  nvshmem ? null, # TODO(@connorbaker): package this
+  libnvshmem,
+  nccl,
 }:
 buildRedist {
   redistName = "cublasmp";
@@ -18,10 +21,15 @@ buildRedist {
   # TODO: Looks like the minimum supported capability is 7.0 as of the latest:
   # https://docs.nvidia.com/cuda/cublasmp/getting_started/index.html
   buildInputs = [
+    (lib.getOutput "stubs" cuda_cudart)
     libcublas
+    libnvshmem
+    nccl
   ];
 
-  platformAssertions = _cuda.lib._mkMissingPackagesAssertions { inherit nvshmem; };
+  extraAutoPatchelfLibs = [
+    "${lib.getOutput "stubs" cuda_cudart}/lib/stubs"
+  ];
 
   meta = {
     description = "High-performance, multi-process, GPU-accelerated library for distributed basic dense linear algebra";
