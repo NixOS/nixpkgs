412db0ec6886fb158255a99081c9d1fa07baf61b
acture <acturea@gmail.com>
2025-10-17 20:15:47 +0800

    clang: set passthru for langC and langCC
    build upon the initial work by @picostove.
    
    Co-authored-by: stove <stove@rivosinc.com>
    Co-authored-by: acture <acture@gmail.com>

--- pkgs/development/compilers/llvm/common/clang/default.nix
+++ pkgs/development/compilers/llvm/common/clang/default.nix
@@ -200,6 +200,8 @@ stdenv.mkDerivation (
     passthru = {
       inherit libllvm;
       isClang = true;
+      langC = true;
+      langCC = true;
       hardeningUnsupportedFlagsByTargetPlatform =
         targetPlatform:
         [ "fortify3" ]
