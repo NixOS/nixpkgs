99a75a721071006952dfb2be7b64cbed535c771a
kyehn <kyehn@users.noreply.github.com>
2025-10-17 11:03:49 +0800

    rover: 0.24.0 -> 0.37.0

--- pkgs/by-name/ro/rover/package.nix
+++ pkgs/by-name/ro/rover/package.nix
@@ -4,20 +4,21 @@
   pkg-config,
   rustPlatform,
   openssl,
+  nix-update-script,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rover";
-  version = "0.24.0";
+  version = "0.37.0";
 
   src = fetchFromGitHub {
     owner = "apollographql";
     repo = "rover";
-    rev = "v${version}";
-    sha256 = "sha256-uyeePAHBDCzXzwIWrKcc9LHClwSI7DMBYod/o4LfK+Y=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-r/uVaj1+J8wQhc/mTCr9RaMMzEIXdJoRU5iX7/eYZMA=";
   };
 
-  cargoHash = "sha256-uR5XvkHUmZzCHZITKgScmzqjLOIvbPyrih/0B1OpsAc=";
+  cargoHash = "sha256-Z9B9DKu6t78Xd75EAKXfB+nr1Au4ylYkZojiENxSykQ=";
 
   buildInputs = [
     openssl
@@ -31,6 +32,8 @@ rustPlatform.buildRustPackage rec {
     OPENSSL_NO_VENDOR = true;
   };
 
+  __darwinAllowLocalNetworking = true;
+
   # This test checks whether the plugins specified in the plugins json file are
   # valid by making a network call to the repo that houses their binaries; but, the
   # build env can't make network calls (impurity)
@@ -38,14 +41,14 @@ rustPlatform.buildRustPackage rec {
     "-- --skip=latest_plugins_are_valid_versions"
   ];
 
-  passthru.updateScript = ./update.sh;
-
   # Some tests try to write configuration data to a location in the user's home
   # directory. Since this would be /homeless-shelter during the build, point at
   # a writeable location instead.
   preCheck = ''
     export APOLLO_CONFIG_HOME="$PWD"
   '';
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "CLI for interacting with ApolloGraphQL's developer tooling, including managing self-hosted and GraphOS graphs";
@@ -57,4 +60,4 @@ rustPlatform.buildRustPackage rec {
       lib.maintainers.aaronarinder
     ];
   };
-}
+})
