e04447612605ae07724c6cb72411638524b10aa1
Hans Christian Schmitz <git@hcsch.eu>
2025-10-17 16:33:11 +0200

    fna3d: remove non-existent mainProgram meta
    fna3d does not provide any installable binaries, except for those built
    when tracing support is enabled (and it does not by default install
    those). Tracing support and associated binaries could be added as an
    optional feature, but currently require patching the CMakeLists.txt, or
    manual installation steps.

--- pkgs/by-name/fn/fna3d/package.nix
+++ pkgs/by-name/fn/fna3d/package.nix
@@ -31,7 +31,6 @@ stdenv.mkDerivation rec {
     homepage = "https://fna-xna.github.io/";
     license = lib.licenses.mspl;
     platforms = lib.platforms.linux;
-    mainProgram = "fna3d";
     maintainers = with lib.maintainers; [ mrtnvgr ];
   };
 }
