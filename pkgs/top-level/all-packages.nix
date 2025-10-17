77ad38bf77a8760106cf2594c3fdb91bdb46d2b8
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-10-17 13:19:41 -0700

    fmt_12: init at 12.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7251,7 +7251,12 @@ with pkgs;
   fltk = fltk13;
   fltk-minimal = fltk13-minimal;
 
-  inherit (callPackages ../development/libraries/fmt { }) fmt_9 fmt_10 fmt_11;
+  inherit (callPackages ../development/libraries/fmt { })
+    fmt_9
+    fmt_10
+    fmt_11
+    fmt_12
+    ;
 
   fmt = fmt_11;
 
