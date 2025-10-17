442ded4e97ced8442f1154e6151267864e5f7cfe
Emily <vcs@emily.moe>
2025-10-17 21:48:40 +0100

    darwin.mkAppleDerivation: use `lib.extendMkDerivation`

--- pkgs/os-specific/darwin/apple-source-releases/mkAppleDerivation.nix
+++ pkgs/os-specific/darwin/apple-source-releases/mkAppleDerivation.nix
@@ -14,68 +14,61 @@ in
 let
   hasBasenamePrefix = prefix: file: lib.hasPrefix prefix (baseNameOf file);
 in
-lib.makeOverridable (
-  attrs:
-  let
-    attrs' = if lib.isFunction attrs then attrs else _: attrs;
-    attrsFixed = lib.fix attrs';
-  in
-  bootstrapStdenv.mkDerivation (
-    lib.extends (
-      self: super:
-      assert super ? releaseName;
-      let
-        inherit (super) releaseName;
-        info = versions.${releaseName};
-        files = lib.filesystem.listFilesRecursive (lib.path.append ./. releaseName);
-        mesonFiles = lib.filter (hasBasenamePrefix "meson") files;
-      in
-      # You have to have at least `meson.build.in` when using xcodeHash to trigger the Meson
-      # build support in `mkAppleDerivation`.
-      assert super ? xcodeHash -> lib.length mesonFiles > 0;
-      {
-        pname = super.pname or releaseName;
-        inherit (info) version;
+lib.extendMkDerivation {
+  constructDrv = bootstrapStdenv.mkDerivation;
+  extendDrvArgs =
+    self: super:
+    assert super ? releaseName;
+    let
+      inherit (super) releaseName;
+      info = versions.${releaseName};
+      files = lib.filesystem.listFilesRecursive (lib.path.append ./. releaseName);
+      mesonFiles = lib.filter (hasBasenamePrefix "meson") files;
+    in
+    # You have to have at least `meson.build.in` when using xcodeHash to trigger the Meson
+    # build support in `mkAppleDerivation`.
+    assert super ? xcodeHash -> lib.length mesonFiles > 0;
+    {
+      pname = super.pname or releaseName;
+      inherit (info) version;
 
-        src = super.src or fetchFromGitHub {
-          owner = "apple-oss-distributions";
-          repo = releaseName;
-          rev = info.rev or "${releaseName}-${info.version}";
-          inherit (info) hash;
-        };
+      src = super.src or fetchFromGitHub {
+        owner = "apple-oss-distributions";
+        repo = releaseName;
+        rev = info.rev or "${releaseName}-${info.version}";
+        inherit (info) hash;
+      };
 
-        strictDeps = true;
-        __structuredAttrs = true;
+      strictDeps = true;
+      __structuredAttrs = true;
 
-        meta = {
-          homepage = "https://opensource.apple.com/releases/";
-          license = lib.licenses.apple-psl20;
-          teams = [ lib.teams.darwin ];
-          platforms = lib.platforms.darwin;
-        }
-        // super.meta or { };
+      meta = {
+        homepage = "https://opensource.apple.com/releases/";
+        license = lib.licenses.apple-psl20;
+        teams = [ lib.teams.darwin ];
+        platforms = lib.platforms.darwin;
       }
-      // lib.optionalAttrs (super ? xcodeHash) {
-        postUnpack =
-          super.postUnpack or ""
-          + lib.concatMapStrings (
-            file:
-            if baseNameOf file == "meson.build.in" then
-              "substitute ${lib.escapeShellArg "${file}"} \"$sourceRoot/meson.build\" --subst-var version\n"
-            else
-              "cp ${lib.escapeShellArg "${file}"} \"$sourceRoot/\"${lib.escapeShellArg (baseNameOf file)}\n"
-          ) mesonFiles;
+      // super.meta or { };
+    }
+    // lib.optionalAttrs (super ? xcodeHash) {
+      postUnpack =
+        super.postUnpack or ""
+        + lib.concatMapStrings (
+          file:
+          if baseNameOf file == "meson.build.in" then
+            "substitute ${lib.escapeShellArg "${file}"} \"$sourceRoot/meson.build\" --subst-var version\n"
+          else
+            "cp ${lib.escapeShellArg "${file}"} \"$sourceRoot/\"${lib.escapeShellArg (baseNameOf file)}\n"
+        ) mesonFiles;
 
-        xcodeProject = super.xcodeProject or "${releaseName}.xcodeproj";
+      xcodeProject = super.xcodeProject or "${releaseName}.xcodeproj";
 
-        nativeBuildInputs = super.nativeBuildInputs or [ ] ++ [
-          meson
-          ninja
-          xcodeProjectCheckHook
-        ];
+      nativeBuildInputs = super.nativeBuildInputs or [ ] ++ [
+        meson
+        ninja
+        xcodeProjectCheckHook
+      ];
 
-        mesonBuildType = "release";
-      }
-    ) attrs'
-  )
-)
+      mesonBuildType = "release";
+    };
+}
