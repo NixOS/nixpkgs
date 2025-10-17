bcaadcc429dc0ab861f6b9a7d120e0d76b87ba05
kyehn <kyehn@users.noreply.github.com>
2025-10-17 11:18:26 +0800

    rlwrap: 0.46.2 -> 0.47.1
    Diff: https://github.com/hanslub42/rlwrap/compare/v0.46.2...v0.47.1
    
    Changelog: https://github.com/hanslub42/rlwrap/raw/refs/tags/v0.47.1/NEWS

--- pkgs/by-name/rl/rlwrap/package.nix
+++ pkgs/by-name/rl/rlwrap/package.nix
@@ -4,18 +4,19 @@
   fetchFromGitHub,
   autoreconfHook,
   perl,
+  libptytty,
   readline,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "rlwrap";
-  version = "0.46.2";
+  version = "0.47.1";
 
   src = fetchFromGitHub {
     owner = "hanslub42";
     repo = "rlwrap";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-05q24Y097GCcipXEPTbel/YIAtQl4jDyA9JFjDDM41Y=";
+    hash = "sha256-jkzvGfqrL4v8SK8Mi1SgKDYibnFfNU1xAlJJ8ZMKFfY=";
   };
 
   nativeBuildInputs = [
@@ -23,17 +24,25 @@ stdenv.mkDerivation (finalAttrs: {
     perl
   ];
 
-  buildInputs = [ readline ];
+  buildInputs = [
+    libptytty
+    readline
+  ];
 
   env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.hostPlatform.isDarwin "-Wno-error=implicit-function-declaration";
 
-  meta = with lib; {
+  preBuild = ''
+    touch src/completion.rb
+    touch src/completion.c
+  '';
+
+  meta = {
     description = "Readline wrapper for console programs";
     homepage = "https://github.com/hanslub42/rlwrap";
-    changelog = "https://github.com/hanslub42/rlwrap/raw/refs/tags/v${finalAttrs.version}/NEWS";
-    license = licenses.gpl2Plus;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ jlesquembre ];
+    changelog = "https://github.com/hanslub42/rlwrap/raw/${finalAttrs.src.rev}/NEWS";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [ jlesquembre ];
     mainProgram = "rlwrap";
   };
 })
