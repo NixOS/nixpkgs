6a8120f621e76717af656558028ff582a108d13c
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2025-10-17 15:19:11 -0400

    forgejo: 13.0.0 -> 13.0.1
    changelog: https://codeberg.org/forgejo/forgejo/src/branch/forgejo/release-notes-published/13.0.1.md
    (cherry picked from commit 28233c66015badf3a0843928f7db61a916c8ce06)

--- pkgs/by-name/fo/forgejo/package.nix
+++ pkgs/by-name/fo/forgejo/package.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
-  version = "13.0.0";
-  hash = "sha256-8NRUJpf25Bai0NtzYf2APmOt3rqpP9mPM13KMjNLl2M=";
+  version = "13.0.1";
+  hash = "sha256-P8ZIBV0pVo3cn9Yroe67Bt+/7KEdz/gBGSypmUz5V2g=";
   npmDepsHash = "sha256-7WjcMsKPtKUWJfDrJc65ZXq2tjK8+8DnqwINj+0XyiQ=";
   vendorHash = "sha256-PHItbU27d9ouykUlhr9owylMpF+3wz2vc8c0UTR1RVU=";
   lts = false;
