5040aa4e74a3354582c5efada012245b57407101
Wolfgang Walther <walther@technowledgy.de>
2025-10-17 09:04:24 +0200

    haskellPackages.digit: remove outdated override
    Setup.lhs has now been deleted upstream.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2415,8 +2415,6 @@ with haskellLib;
 
   # The shipped Setup.hs file is broken.
   csv = overrideCabal (drv: { preCompileBuildDriver = "rm Setup.hs"; }) super.csv;
-  # Build-type is simple, but ships a broken Setup.hs
-  digits = overrideCabal (drv: { preCompileBuildDriver = "rm Setup.lhs"; }) super.digits;
 
   cabal-fmt = doJailbreak (
     super.cabal-fmt.override {
