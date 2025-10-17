d9896085becde8ecf40ff5b7e5236bd5e541c45c
Rafael Ieda <git@ieda.me>
2025-10-17 02:51:15 -0300

    unciv: add maintainer iedame

--- pkgs/by-name/un/unciv/package.nix
+++ pkgs/by-name/un/unciv/package.nix
@@ -73,7 +73,7 @@ stdenv.mkDerivation rec {
     description = "Open-source Android/Desktop remake of Civ V";
     mainProgram = "unciv";
     homepage = "https://github.com/yairm210/Unciv";
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ iedame ];
     sourceProvenance = with sourceTypes; [ binaryBytecode ];
     license = licenses.mpl20;
     platforms = platforms.all;
