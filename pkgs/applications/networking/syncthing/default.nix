f619560ced8ac9731cbadcd9c9973fe4cacc60e4
Hleb Shauchenka <me@marleb.org>
2025-10-17 15:13:51 +0200

    syncthing: 2.0.8 -> 2.0.10
    Changelog: https://github.com/syncthing/syncthing/releases/tag/v2.0.10

--- pkgs/applications/networking/syncthing/default.nix
+++ pkgs/applications/networking/syncthing/default.nix
@@ -19,13 +19,13 @@ let
     }:
     buildGoModule rec {
       pname = stname;
-      version = "2.0.8";
+      version = "2.0.10";
 
       src = fetchFromGitHub {
         owner = "syncthing";
         repo = "syncthing";
         tag = "v${version}";
-        hash = "sha256-QkCLFztzaH9MvgP6HWUr5Z8yIrKlY6/t2VaZwai/H8Q=";
+        hash = "sha256-N0+i5sj/cTPDv6q428b3Y0hsPRxIl96+RIuS1AyeTbc=";
       };
 
       vendorHash = "sha256-iYTAnEy0MqJaTz/cdpteealyviwVrpwDzVigo8nnXqs=";
