504bb0011dbcea59b4c2e85e83a26ed074f9f479
Francesco Zanini <francesco@zanini.me>
2025-10-17 08:57:23 +0200

    sdrpp: fix server mode, add options, simplify
    - rework patch to apply more cleanly and cover all entrypoints in one
      place.
    - add `audio_source` parameter, useful for making "server only" builds
      (avoid dependency on rtaudio in these builds)
    - fix build on Darwin by disabling soapyplutosdr, which in turns depends
      on libiio (Linux only)

--- pkgs/by-name/sd/sdrpp/package.nix
+++ pkgs/by-name/sd/sdrpp/package.nix
@@ -10,31 +10,39 @@
   fftwFloat,
   volk,
   zstd,
+
   # Sources
   airspy_source ? true,
   airspy,
   airspyhf_source ? true,
   airspyhf,
-  bladerf_source ? true,
+  audio_source ? true,
+  bladerf_source ? stdenv.hostPlatform.isLinux,
   libbladeRF,
   file_source ? true,
   hackrf_source ? true,
   hackrf,
+  hermes_source ? true,
   limesdr_source ? true,
   limesuite,
-  perseus_source ? false, # needs libperseus-sdr, not yet available in nixpks
+  network_source ? true,
+  # needs libperseus-sdr, not yet available in nixpks
+  perseus_source ? false,
   plutosdr_source ? stdenv.hostPlatform.isLinux,
   libiio,
   libad9361,
   rfspace_source ? true,
   rtl_sdr_source ? true,
   rtl-sdr-osmocom,
-  libusb1, # osmocom better w/ rtlsdr v4
+  libusb1,
+  # osmocom better w/ rtlsdr v4
   rtl_tcp_source ? true,
   sdrplay_source ? false,
   sdrplay,
+  sdrpp_server_source ? true,
   soapy_source ? true,
   soapysdr-with-plugins,
+  spectran_http_source ? true,
   spyserver_source ? true,
   usrp_source ? false,
   uhd,
@@ -48,57 +56,54 @@
   portaudio,
 
   # Decoders
+  atv_decoder ? true,
+  dab_decoder ? false,
   falcon9_decoder ? false,
+  kg_sstv_decoder ? false,
   m17_decoder ? false,
   codec2,
   meteor_demodulator ? true,
+  pager_decoder ? true,
   radio ? true,
-  weather_sat_decoder ? false, # is missing some dsp/pll.h
+  vor_receiver ? false,
+  weather_sat_decoder ? false,
 
   # Misc
   discord_presence ? true,
   frequency_manager ? true,
+  iq_exporter ? true,
   recorder ? true,
+  rigctl_client ? true,
   rigctl_server ? true,
   scanner ? true,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "sdrpp";
 
-  # SDR++ uses a rolling release model.
-  # Choose a git hash from head and use the date from that commit as
-  # version qualifier
-  git_rev = "4658a1ade6707dee6f2ae09ba9eb71097223ea93";
-  git_hash = "sha256-UxYAcqOMPQYdUbL2636LpOGbCaxHjLiJhsH62s+0AZU=";
-  git_date = "2025-10-09";
-  version_number = "1.2.1";
-
-  version = "${version_number}-unstable-" + git_date;
+  upstreamVersion = "1.2.1";
+  version = "${finalAttrs.upstreamVersion}-unstable-2025-10-09";
 
   src = fetchFromGitHub {
     owner = "AlexandreRouma";
     repo = "SDRPlusPlus";
-    rev = git_rev;
-    hash = git_hash;
+    rev = "4658a1ade6707dee6f2ae09ba9eb71097223ea93";
+    hash = "sha256-UxYAcqOMPQYdUbL2636LpOGbCaxHjLiJhsH62s+0AZU=";
   };
 
   patches = [
-    ./runtime-prefix.patch
+    ./0001-Allow-management-of-resources-and-modules-paths.patch
     # CMake 4 dropped support of versions lower than 3.5,
     # versions lower than 3.10 are deprecated.
     ./cmake4.patch
   ];
 
   postPatch = ''
-    substituteInPlace CMakeLists.txt \
-      --replace "/usr/share" "share" \
-      --replace "set(CMAKE_INSTALL_PREFIX" "#set(CMAKE_INSTALL_PREFIX"
     substituteInPlace decoder_modules/m17_decoder/src/m17dsp.h \
-      --replace "codec2.h" "codec2/codec2.h"
-    # Since the __TIME_ and __DATE__ is canonicalized in the build,
-    # use our qualified version shown in the programs window title.
-    substituteInPlace core/src/version.h --replace-fail "${version_number}" "$version"
+      --replace-fail "codec2.h" "codec2/codec2.h"
+
+    substituteInPlace core/src/version.h \
+      --replace-fail "${finalAttrs.upstreamVersion}" "${finalAttrs.version}"
   '';
 
   nativeBuildInputs = [
@@ -133,25 +138,30 @@ stdenv.mkDerivation rec {
     uhd
     boost
   ]
-  ++ lib.optional audio_sink rtaudio
+  ++ lib.optional (audio_source || audio_sink) rtaudio
   ++ lib.optional portaudio_sink portaudio
-  ++ lib.optional m17_decoder codec2;
+  ++ lib.optional (dab_decoder || m17_decoder) codec2;
 
   cmakeFlags = [
     # Sources
     (lib.cmakeBool "OPT_BUILD_AIRSPYHF_SOURCE" airspyhf_source)
     (lib.cmakeBool "OPT_BUILD_AIRSPY_SOURCE" airspy_source)
+    (lib.cmakeBool "OPT_BUILD_AUDIO_SOURCE" audio_source)
     (lib.cmakeBool "OPT_BUILD_BLADERF_SOURCE" bladerf_source)
     (lib.cmakeBool "OPT_BUILD_FILE_SOURCE" file_source)
     (lib.cmakeBool "OPT_BUILD_HACKRF_SOURCE" hackrf_source)
+    (lib.cmakeBool "OPT_BUILD_HERMES_SOURCE" hermes_source)
     (lib.cmakeBool "OPT_BUILD_LIMESDR_SOURCE" limesdr_source)
+    (lib.cmakeBool "OPT_BUILD_NETWORK_SOURCE" network_source)
     (lib.cmakeBool "OPT_BUILD_PERSEUS_SOURCE" perseus_source)
     (lib.cmakeBool "OPT_BUILD_PLUTOSDR_SOURCE" plutosdr_source)
     (lib.cmakeBool "OPT_BUILD_RFSPACE_SOURCE" rfspace_source)
     (lib.cmakeBool "OPT_BUILD_RTL_SDR_SOURCE" rtl_sdr_source)
     (lib.cmakeBool "OPT_BUILD_RTL_TCP_SOURCE" rtl_tcp_source)
     (lib.cmakeBool "OPT_BUILD_SDRPLAY_SOURCE" sdrplay_source)
+    (lib.cmakeBool "OPT_BUILD_SDRPP_SERVER_SOURCE" sdrpp_server_source)
     (lib.cmakeBool "OPT_BUILD_SOAPY_SOURCE" soapy_source)
+    (lib.cmakeBool "OPT_BUILD_SPECTRAN_HTTP_SOURCE" spectran_http_source)
     (lib.cmakeBool "OPT_BUILD_SPYSERVER_SOURCE" spyserver_source)
     (lib.cmakeBool "OPT_BUILD_USRP_SOURCE" usrp_source)
 
@@ -161,16 +171,23 @@ stdenv.mkDerivation rec {
     (lib.cmakeBool "OPT_BUILD_NEW_PORTAUDIO_SINK" portaudio_sink)
 
     # Decoders
+    (lib.cmakeBool "OPT_BUILD_ATV_DECODER" atv_decoder)
+    (lib.cmakeBool "OPT_BUILD_DAB_DECODER" dab_decoder)
     (lib.cmakeBool "OPT_BUILD_FALCON9_DECODER" falcon9_decoder)
+    (lib.cmakeBool "OPT_BUILD_KG_SSTV_DECODER" kg_sstv_decoder)
     (lib.cmakeBool "OPT_BUILD_M17_DECODER" m17_decoder)
     (lib.cmakeBool "OPT_BUILD_METEOR_DEMODULATOR" meteor_demodulator)
+    (lib.cmakeBool "OPT_BUILD_PAGER_DECODER" pager_decoder)
     (lib.cmakeBool "OPT_BUILD_RADIO" radio)
+    (lib.cmakeBool "OPT_BUILD_VOR_RECEIVER" vor_receiver)
     (lib.cmakeBool "OPT_BUILD_WEATHER_SAT_DECODER" weather_sat_decoder)
 
     # Misc
     (lib.cmakeBool "OPT_BUILD_DISCORD_PRESENCE" discord_presence)
     (lib.cmakeBool "OPT_BUILD_FREQUENCY_MANAGER" frequency_manager)
+    (lib.cmakeBool "OPT_BUILD_IQ_EXPORTER" iq_exporter)
     (lib.cmakeBool "OPT_BUILD_RECORDER" recorder)
+    (lib.cmakeBool "OPT_BUILD_RIGCTL_CLIENT" rigctl_client)
     (lib.cmakeBool "OPT_BUILD_RIGCTL_SERVER" rigctl_server)
     (lib.cmakeBool "OPT_BUILD_SCANNER" scanner)
   ];
@@ -183,8 +200,12 @@ stdenv.mkDerivation rec {
     description = "Cross-Platform SDR Software";
     homepage = "https://github.com/AlexandreRouma/SDRPlusPlus";
     license = licenses.gpl3Only;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ sikmir ];
+    # The DAB decoder is broken upstream. See: https://github.com/AlexandreRouma/SDRPlusPlus/issues/1511
+    broken = dab_decoder;
+    maintainers = with maintainers; [
+      sikmir
+      zaninime
+    ];
     mainProgram = "sdrpp";
   };
-}
+})
