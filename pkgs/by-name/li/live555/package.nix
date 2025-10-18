4ffd9cc1425fca5f7bc5ba0051db2e5196e7ed8a
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-18 06:39:10 +0000

    live555: 2024.09.20 -> 2025.10.13

--- pkgs/by-name/li/live555/package.nix
+++ pkgs/by-name/li/live555/package.nix
@@ -19,7 +19,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "live555";
-  version = "2024.09.20";
+  version = "2025.10.13";
 
   src = fetchurl {
     urls = [
@@ -28,7 +28,7 @@ stdenv.mkDerivation (finalAttrs: {
       "https://download.videolan.org/contrib/live555/live.${finalAttrs.version}.tar.gz"
       "mirror://sourceforge/slackbuildsdirectlinks/live.${finalAttrs.version}.tar.gz"
     ];
-    hash = "sha256-TrUneCGaJJxC+GgL1ZZ/ZcONeqDH05Bp44/3lkCs9tg=";
+    hash = "sha256-ROtq5K4C72ilAo2fyLcMRbm6RcBY+Ea2i8TTLHQ1X0k=";
   };
 
   patches = [
