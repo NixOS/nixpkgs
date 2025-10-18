9423bb82df8cfd586c1a9f53886dbd87d3db87d6
oddlama <oddlama@oddlama.org>
2025-10-18 13:18:12 +0200

    treewide: Add options parameter to all genJqSecretsReplacementSnippet calls

--- nixos/modules/services/logging/filebeat.nix
+++ nixos/modules/services/logging/filebeat.nix
@@ -246,7 +246,7 @@ in
 
           umask u=rwx,g=,o=
 
-          ${utils.genJqSecretsReplacementSnippet cfg.settings "/var/lib/filebeat/filebeat.yml"}
+          ${(utils.genJqSecretsReplacementSnippet { } cfg.settings "/var/lib/filebeat/filebeat.yml").script}
         '';
         ExecStart = ''
           ${cfg.package}/bin/filebeat -e \
