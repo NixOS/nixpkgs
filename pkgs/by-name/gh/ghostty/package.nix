95da5430b57f08f05d515a5e569efc55e3b77d4e
Seth Flynn <getchoo@tuta.io>
2025-10-18 02:24:43 -0400

    ghostty: add wrapper for configuration file
    Co-authored-by: linsui <36977733+linsui@users.noreply.github.com>

--- pkgs/by-name/gh/ghostty/package.nix
+++ pkgs/by-name/gh/ghostty/package.nix
@@ -1,10 +1,13 @@
 {
   lib,
+  formats,
   ghostty-unwrapped,
+  ghostty,
   makeBinaryWrapper,
   symlinkJoin,
   versionCheckHook,
   wrapGAppsHook4,
+  settings ? { },
 }:
 
 symlinkJoin rec {
@@ -18,11 +21,18 @@ symlinkJoin rec {
     wrapGAppsHook4
   ];
 
+  makeWrapperArgs = lib.optionals (settings != { }) [
+    "--add-flags"
+    "--config-file=${passthru.configurationFile}"
+  ];
+
   versionCheckProgramArg = [ "--version" ];
 
   postBuild = ''
-    wrapGAppsHook
+    concatTo makeWrapperArgsArray makeWrapperArgs gappsWrapperArgs
+    wrapProgram $out/bin/ghostty "''${makeWrapperArgsArray[@]}"
     versionCheckHook
+    $out/bin/ghostty +validate-config
   ''
   # Link back to the unwrapped package's split outputs
   + lib.concatLines (
@@ -32,8 +42,23 @@ symlinkJoin rec {
   passthru = {
     unwrapped = ghostty-unwrapped;
 
-    inherit (passthru.unwrapped) tests;
+    configurationFileFormat = formats.keyValue {
+      listsAsDuplicateKeys = true;
+      mkKeyValue = lib.generators.mkKeyValueDefault { } " = ";
+    };
+
+    tests = lib.recursiveUpdate passthru.unwrapped.tests {
+      wrapper = ghostty.override {
+        settings = {
+          theme = "Gruvbox Dark";
+        };
+      };
+    };
+
     updateScript = ./update.nu;
+  }
+  // lib.optionalAttrs (settings != { }) {
+    configurationFile = passthru.configurationFileFormat.generate "ghostty-config" settings;
   };
 
   meta = {
