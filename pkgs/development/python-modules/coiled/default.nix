c175b7c0e314d12dc5c4cfc86fa8001c129e84dc
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-18 05:25:42 +0000

    python3Packages.coiled: 1.126.0 -> 1.127.0

--- pkgs/development/python-modules/coiled/default.nix
+++ pkgs/development/python-modules/coiled/default.nix
@@ -39,12 +39,12 @@
 
 buildPythonPackage rec {
   pname = "coiled";
-  version = "1.126.0";
+  version = "1.127.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-iS6PSxh2yV8QYsKqnxH0cWNkW/U7cxXFI48fWs/1fjU=";
+    hash = "sha256-qMwZQC2ctMtuI5lysJ5YNVuT/lzqWQiEJnJPCUmKZbw=";
   };
 
   build-system = [
