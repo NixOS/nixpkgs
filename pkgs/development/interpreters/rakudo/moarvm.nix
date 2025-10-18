41100582e744062d5f4baeee56d073737a69bafd
Sizhe Zhao <prc.zhao@outlook.com>
2025-10-19 03:09:02 +0800

    moarvm: 2025.06 -> 2025.10

--- pkgs/development/interpreters/rakudo/moarvm.nix
+++ pkgs/development/interpreters/rakudo/moarvm.nix
@@ -1,39 +1,55 @@
 {
-  lib,
-  stdenv,
   fetchFromGitHub,
+  fetchpatch2,
+  lib,
   perl,
+  pkg-config,
+  stdenv,
+  zstd,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "moarvm";
-  version = "2025.06";
+  version = "2025.10";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
-    owner = "moarvm";
-    repo = "moarvm";
-    rev = version;
-    hash = "sha256-QtJ8cLAbsFJ26wkfQCbIMVU1ArWlAXjsQ/RJbQ0wRNo=";
+    owner = "MoarVM";
+    repo = "MoarVM";
+    tag = finalAttrs.version;
     fetchSubmodules = true;
+    hash = "sha256-ZTQsjEHBAhmUdEJAKz/mt+DHcSwlY41jIMtJ2vFNNqY=";
   };
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ zstd ];
+
+  patches = [
+    (fetchpatch2 {
+      name = "fix-format-specifiers.patch";
+      url = "https://github.com/Prince213/MoarVM/commit/b937272f92c58278cca4c6188d91b0ee7a0027da.patch?full_index=1";
+      hash = "sha256-oKciRoRGXS30+5e6hvtk7fa54LS2KMPCeXrEHHq3vJQ=";
+    })
+  ];
 
   postPatch = ''
     patchShebangs .
+    substituteInPlace Configure.pl \
+      --replace-fail 'my @check_tools = qw/ar cc ld/;' 'my @check_tools = ();'
   ''
   + lib.optionalString stdenv.hostPlatform.isDarwin ''
     substituteInPlace Configure.pl \
-      --replace '`/usr/bin/arch`' '"${stdenv.hostPlatform.darwinArch}"' \
-      --replace '/usr/bin/arch' "$(type -P true)" \
-      --replace '/usr/' '/nope/'
+      --replace-fail '`/usr/bin/arch`' '"${stdenv.hostPlatform.darwinArch}"' \
+      --replace-fail '/usr/bin/arch' "$(type -P true)" \
+      --replace-fail '/usr/' '/nope/'
     substituteInPlace 3rdparty/dyncall/configure \
-      --replace '`sw_vers -productVersion`' '"11.0"'
+      --replace-fail '`sw_vers -productVersion`' '"11.0"'
   '';
 
-  buildInputs = [ perl ];
-  doCheck = false; # MoarVM does not come with its own test suite
-
-  configureScript = "${perl}/bin/perl ./Configure.pl";
+  configureScript = "${lib.getExe perl} ./Configure.pl";
+  configureFlags = [
+    "--pkgconfig=${lib.getExe pkg-config}"
+  ];
 
   meta = {
     description = "VM with adaptive optimization and JIT compilation, built for Rakudo";
@@ -47,4 +63,4 @@ stdenv.mkDerivation rec {
     mainProgram = "moar";
     platforms = lib.platforms.unix;
   };
-}
+})
