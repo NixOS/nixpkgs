98cc3659a2585c2ffaa17eeda15f19e0cdec0587
Fabian Affolter <mail@fabian-affolter.ch>
2025-10-18 12:09:58 +0200

    python313Packages.x-transformers: add changelog to meta

--- pkgs/development/python-modules/x-transformers/default.nix
+++ pkgs/development/python-modules/x-transformers/default.nix
@@ -46,9 +46,10 @@ buildPythonPackage rec {
   meta = {
     description = "Concise but fully-featured transformer";
     longDescription = ''
-      A simple but complete full-attention transformer with a set of promising experimental features from various papers
+      A simple but complete full-attention transformer with a set of promising experimental features from various papers.
     '';
     homepage = "https://github.com/lucidrains/x-transformers";
+    changelog = "https://github.com/lucidrains/x-transformers/releases/tag/${src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ByteSudoer ];
   };
