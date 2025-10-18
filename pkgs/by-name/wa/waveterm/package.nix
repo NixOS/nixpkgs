4fc619f1d05d3b65b5364b4f263266be787d98b2
Mio <mio@mio19.uk>
2025-10-18 21:00:15 +1100

    waveterm: 0.11.6 -> 0.12.0

--- pkgs/by-name/wa/waveterm/package.nix
+++ pkgs/by-name/wa/waveterm/package.nix
@@ -31,7 +31,7 @@
 let
   selectSystem = attrs: attrs.${stdenv.hostPlatform.system};
   pname = "waveterm";
-  version = "0.11.6";
+  version = "0.12.0";
 
   passthru.updateScript = ./update.sh;
 
@@ -62,8 +62,8 @@ let
       fetchurl {
         url = "https://github.com/wavetermdev/waveterm/releases/download/v${version}/waveterm-linux-${arch}-${version}.deb";
         hash = selectSystem {
-          x86_64-linux = "sha256-OM64Tyqo+phMiH6TWmp47hBhnFo0cTvzFvhX/7/JUec=";
-          aarch64-linux = "sha256-g+e2wn1n0EqFeUah5ZgvL5D/y1khRQ2BUEowQf/ZoaI=";
+          x86_64-linux = "sha256-pITwfh83l8KffGh76bIec6KdzT8AuL1vhFxU/8O6Mk4=";
+          aarch64-linux = "sha256-v/3Vi59J9TqJ2V5616Y3muOQK1CKAKdBvw+txuLG8EM=";
         };
       };
 
@@ -136,8 +136,8 @@ let
       fetchurl {
         url = "https://github.com/wavetermdev/waveterm/releases/download/v${version}/Wave-darwin-${arch}-${version}.zip";
         hash = selectSystem {
-          x86_64-darwin = "sha256-AW4AQ/U/WHIlXmgbQJ8g0yQQgpBolYT2kb/fDNWUS1g=";
-          aarch64-darwin = "sha256-Laxjj/MZzKsKhfRvOROQcPiatwNJKVqUCcGWB0chnok=";
+          x86_64-darwin = "sha256-4yuwnZQFxGXAcMoZINpKkgg4VjfX2ZOKM95deaFWuvA=";
+          aarch64-darwin = "sha256-tfCCh6gOyRCl2fe3nPhTLr/Wb6tCYnpL2h2T6d5viRA=";
         };
       };
 
