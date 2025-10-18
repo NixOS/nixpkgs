2329cce87dcc11426a6c3b358c1ace995d1976ed
Naga Praneeth <nagapraneethkandikandi@gmail.com>
2025-10-18 20:24:54 +0530

    python3Packages.random-user-agent: remove
    Package was abandoned upstream in 2018 with version 1.0.1 being the
    final release. No version 1.0.2 exists on PyPI, making unpinning
    impossible.
    
    The only reverse dependency (conkeyscan) has been patched in the
    previous commit to use fake-useragent instead.
    
    Part of #410837.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15658,8 +15658,6 @@ self: super: with self; {
 
   ramlfications = callPackage ../development/python-modules/ramlfications { };
 
-  random-user-agent = callPackage ../development/python-modules/random-user-agent { };
-
   random2 = callPackage ../development/python-modules/random2 { };
 
   randomfiletree = callPackage ../development/python-modules/randomfiletree { };
