3e0fba414d25d1427a78456ebe98af68042f5041
David McFarland <corngood@gmail.com>
2025-10-19 12:57:05 -0300

    perlPackages.XMLParser: fix cygwin build

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -38666,18 +38666,18 @@ with self;
       url = "mirror://cpan/authors/id/T/TO/TODDR/XML-Parser-2.46.tar.gz";
       hash = "sha256-0zEzJJHFHMz7TLlP/ET5zXM3jmGEmNSjffngQ2YcUV0=";
     };
-    patches = [ ../development/perl-modules/xml-parser-0001-HACK-Assumes-Expat-paths-are-good.patch ];
-    postPatch =
-      lib.optionalString (stdenv.buildPlatform != stdenv.hostPlatform) ''
-        substituteInPlace Expat/Makefile.PL --replace 'use English;' '#'
-      ''
-      + lib.optionalString stdenv.hostPlatform.isCygwin ''
-        sed -i -e "s@my \$compiler = File::Spec->catfile(\$path, \$cc\[0\]) \. \$Config{_exe};@my \$compiler = File::Spec->catfile(\$path, \$cc\[0\]) \. (\$^O eq 'cygwin' ? \"\" : \$Config{_exe});@" inc/Devel/CheckLib.pm
-      '';
+    patches = [
+      ../development/perl-modules/xml-parser-0001-HACK-Assumes-Expat-paths-are-good.patch
+    ]
+    ++ lib.optional stdenv.buildPlatform.isCygwin ../development/perl-modules/cygwin-checklib-fix.patch;
+    postPatch = lib.optionalString (stdenv.buildPlatform != stdenv.hostPlatform) ''
+      substituteInPlace Expat/Makefile.PL --replace 'use English;' '#'
+    '';
     makeMakerFlags = [
       "EXPATLIBPATH=${pkgs.expat.out}/lib"
       "EXPATINCPATH=${pkgs.expat.dev}/include"
     ];
+    buildInputs = lib.optional stdenv.hostPlatform.isCygwin pkgs.expat;
     propagatedBuildInputs = [ LWP ];
     meta = {
       description = "Perl module for parsing XML documents";
