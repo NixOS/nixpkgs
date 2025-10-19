bc1aae2c32237b9108f2ea7abc3ac529cbd6e6a6
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-19 08:23:20 +0000

    cyclonedx-python: 7.1.0 -> 7.2.0

--- pkgs/by-name/cy/cyclonedx-python/package.nix
+++ pkgs/by-name/cy/cyclonedx-python/package.nix
@@ -6,14 +6,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "cyclonedx-python";
-  version = "7.1.0";
+  version = "7.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "CycloneDX";
     repo = "cyclonedx-python";
     tag = "v${version}";
-    hash = "sha256-RHw+FYj1oYM5Yf8YcU8tOsxG+3qu0ti/AYzcGxYAp/8=";
+    hash = "sha256-4Mvt8l6kDT7GR4ufIelQd9rTt5ljdpMVWwRGZV5jIc8=";
   };
 
   build-system = with python3Packages; [ poetry-core ];
