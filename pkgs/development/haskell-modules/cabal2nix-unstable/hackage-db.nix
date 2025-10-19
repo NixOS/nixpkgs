8780e5b41f89f4a9d910c6bfc35a2dc2c01bb288
sternenseemann <sternenseemann@systemli.org>
2025-10-19 02:05:59 +0200

    haskellPackages: _ prefix attribute names that would need quoting
    In order not to break downstream expressions that (manually) introduced
    dependencies on now renamed attributes, add aliases (with proper
    warnings about the renames) for all attributes that were previously part
    of Nixpkgs, but have been renamed with this change.

--- pkgs/development/haskell-modules/cabal2nix-unstable/hackage-db.nix
+++ pkgs/development/haskell-modules/cabal2nix-unstable/hackage-db.nix
@@ -19,8 +19,8 @@ mkDerivation {
   pname = "hackage-db";
   version = "2.1.3-unstable-2025-11-20";
   src = fetchzip {
-    url = "https://github.com/NixOS/cabal2nix/archive/be62a6c198c6fd082cd5660170fa23a709d92c8e.tar.gz";
-    sha256 = "15bfhl0ypnnpgglxc7smppqi3w8w04zi97gh65kk1p6drlg29f47";
+    url = "https://github.com/NixOS/cabal2nix/archive/459859839cfe4fb352a29c1a72a1c4f0f537a1b8.tar.gz";
+    sha256 = "1443hlbz29y2dn22kf91zx7g284zp3l2vgw6jg4wgx66v2sxrqii";
   };
   postUnpack = "sourceRoot+=/hackage-db; echo source root reset to $sourceRoot";
   isLibrary = true;
