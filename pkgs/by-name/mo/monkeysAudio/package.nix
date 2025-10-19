5048a78a2f123257a1c57d385e598c6ce5cc44f8
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-19 13:23:22 +0000

    monkeysAudio: 11.61 -> 11.62

--- pkgs/by-name/mo/monkeysAudio/package.nix
+++ pkgs/by-name/mo/monkeysAudio/package.nix
@@ -6,12 +6,12 @@
 }:
 
 stdenv.mkDerivation (finalAttrs: {
-  version = "11.61";
+  version = "11.62";
   pname = "monkeys-audio";
 
   src = fetchzip {
     url = "https://monkeysaudio.com/files/MAC_${builtins.concatStringsSep "" (lib.strings.splitString "." finalAttrs.version)}_SDK.zip";
-    hash = "sha256-xjFAsnocqar53mBnbi34Q7tRr0OyJoK2S9e1t7O7r9U=";
+    hash = "sha256-qOzGOm5fzeV818Dv09A0+jieKRgvH3XbEGFmfsbtg3g=";
     stripRoot = false;
   };
 
