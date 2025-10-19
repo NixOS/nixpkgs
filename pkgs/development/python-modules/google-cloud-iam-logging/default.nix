3f5771e1e2b5e9e49c6b405e125eec87ea0e6291
Fabian Affolter <mail@fabian-affolter.ch>
2025-10-19 10:51:58 +0200

    python313Packages.google-cloud-iam-logging: remove disabled

--- pkgs/development/python-modules/google-cloud-iam-logging/default.nix
+++ pkgs/development/python-modules/google-cloud-iam-logging/default.nix
@@ -9,7 +9,6 @@
   protobuf,
   pytest-asyncio,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
@@ -17,8 +16,6 @@ buildPythonPackage rec {
   pname = "google-cloud-iam-logging";
   version = "1.5.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "google_cloud_iam_logging";
