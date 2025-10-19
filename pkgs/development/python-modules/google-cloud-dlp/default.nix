c4647fa5deaefbeb2d804b12378a8c1fb3242180
Fabian Affolter <mail@fabian-affolter.ch>
2025-10-19 11:06:28 +0200

    python313Packages.google-cloud-dlp: remove disabled
    Removed dependency on pythonOlder for version restriction.

--- pkgs/development/python-modules/google-cloud-dlp/default.nix
+++ pkgs/development/python-modules/google-cloud-dlp/default.nix
@@ -9,7 +9,6 @@
   protobuf,
   pytest-asyncio,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
@@ -17,8 +16,6 @@ buildPythonPackage rec {
   pname = "google-cloud-dlp";
   version = "3.33.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "google_cloud_dlp";
