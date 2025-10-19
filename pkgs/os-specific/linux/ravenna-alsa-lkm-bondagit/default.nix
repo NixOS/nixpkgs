f45d5f7b1b9f96c9169e709cc070e00b9c9defd0
Matthew Robbetts <mrobbetts@gmail.com>
2025-10-18 22:42:06 -0700

    ravenna-lkm: 1.14 -> 1.14+

--- pkgs/os-specific/linux/ravenna-alsa-lkm-bondagit/default.nix
+++ pkgs/os-specific/linux/ravenna-alsa-lkm-bondagit/default.nix
@@ -3,13 +3,19 @@
 stdenv.mkDerivation rec {
   name = "ravenna-alsa-lkm_${version}_${kernel.version}";
 
-  version = "1.14";
+  version = "1.14+";
 
   src = fetchFromGitHub {
     owner = "bondagit";
     repo = "ravenna-alsa-lkm";
-    rev = "7e3b67f35f7733fc7abc0bba53450ef9ca66785e";
-    sha256 = "sha256-iYZLnBsNLMwNlPk9lC0UUHoGyaPwqfN8z6nAGTMfpYA=";
+/*
+    # 1.14. Works nicely with linux-6.14
+    #rev = "7e3b67f35f7733fc7abc0bba53450ef9ca66785e";
+    #sha256 = "sha256-iYZLnBsNLMwNlPk9lC0UUHoGyaPwqfN8z6nAGTMfpYA=";
+*/
+    # Include fixes for linux-6.15
+    rev = "e1ecd6a3f998396ee3066c3ff8fa4f246a560144";
+    hash = "sha256-VF19IviTPAIL+7sHKSkPRvxpE/aPPc0XJ4XcpMz5Efw=";
   };
 
   hardeningDisable = [ "pic" ];
