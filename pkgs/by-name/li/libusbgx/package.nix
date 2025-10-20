47070909ba4654f5573782161f39e09bdbe7d842
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-10-20 12:47:58 -0700

    libusbgx: cleanup

--- pkgs/by-name/li/libusbgx/package.nix
+++ pkgs/by-name/li/libusbgx/package.nix
@@ -5,21 +5,31 @@
   pkg-config,
   libconfig,
   autoreconfHook,
+  nix-update-script,
 }:
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "libusbgx";
   version = "0.3.0";
+
   src = fetchFromGitHub {
     owner = "linux-usb-gadgets";
     repo = "libusbgx";
     tag = "libusbgx-v${finalAttrs.version}";
-    sha256 = "sha256-cG9hSv9TEs9+nfVtMrh88Gsg3P45aH+dr0d98hzqo0I=";
+    hash = "sha256-cG9hSv9TEs9+nfVtMrh88Gsg3P45aH+dr0d98hzqo0I=";
   };
+
+  strictDeps = true;
+
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
   ];
+
   buildInputs = [ libconfig ];
+
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "C library encapsulating the kernel USB gadget-configfs userspace API functionality";
     license = with lib.licenses; [
