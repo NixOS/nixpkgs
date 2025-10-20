5b980246a5267c0e49935c3a03d730b8c4c75b0b
Anders Christiansen Sørby <anders@sorby.xyz>
2025-10-20 10:37:29 +0200

    WIP

--- pkgs/development/libraries/relibc/relibc.nix
+++ pkgs/development/libraries/relibc/relibc.nix
@@ -5,34 +5,52 @@
   rustPlatform,
   src,
   rust-cbindgen,
+  # rust,
+  # cargo,
   expect,
 }:
 
 let
+  d = i: builtins.trace "${i}" i;
   target = stdenv.hostPlatform.rust.rustcTargetSpec;
   lockFile = "${src}/Cargo.lock";
+  # rustPlatform = makeRustPlatform {
+  #   rustc = rust;
+  #   cargo = rust;
+  # };
+  # rustPlatform = buildPackages.makeRustPlatform {
+  #   rustc = d rust;
+  #   cargo = rust;
+  # };
 in
 rustPlatform.buildRustPackage {
   pname = "relibc";
-  version = "latest";
+  version = "0.2.5";
 
-  LD_LIBRARY_PATH = "${buildPackages.zlib}/lib";
+  # LD_LIBRARY_PATH = "${buildPackages.zlib}/lib";
+  # RUST_SRC_PATH = d "${rustPlatform.rustLibSrc}";
+  # preCheck = ''
+  #   export RUST_SRC_PATH=${rustPlatform.rustLibSrc}
+  # '';
 
   src = src;
 
-  RUSTC_BOOTSTRAP = 1;
+  # RUSTC_BOOTSTRAP = 1;
 
-  dontInstall = true;
-  dontFixup = true;
+  # dontInstall = true;
+  # dontFixup = true;
   doCheck = false;
+  patchPhase = ''
+    patchShebangs --build renamesyms.sh stripcore.sh
+  '';
 
   buildPhase = ''
-    make CC=gcc AR=ar LD=ld NM=nm all
+    make CC=gcc AR=ar LD=ld NM=nm CARGO_COMMON_FLAGS="" all
   '';
 
   postBuild = ''
     mkdir -p $out
-    DESTDIR=$out make install
+    DESTDIR=$out make CC=gcc AR=ar LD=ld NM=nm install
   '';
 
   nativeBuildInputs = [
