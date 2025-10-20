52cd858a3e953f030ac95d6e1bbd389a1b607fb5
June Stepp <git@junestepp.me>
2025-10-20 14:39:42 -0500

    python3Packages.liblistenbrainz: init at 0.6.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8272,6 +8272,8 @@ self: super: with self; {
 
   liblarch = callPackage ../development/python-modules/liblarch { };
 
+  liblistenbrainz = callPackage ../development/python-modules/liblistenbrainz { };
+
   liblp = callPackage ../development/python-modules/liblp { };
 
   liblzfse = callPackage ../development/python-modules/liblzfse { inherit (pkgs) lzfse; };
