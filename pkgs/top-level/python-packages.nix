e4a92b858d534e234e79bbbba0f707c41e6e9c1e
Wolfgang Walther <walther@technowledgy.de>
2025-10-20 15:05:51 +0200

    plfit: python -> withPython
    Improves the override interface, because `withPython` does not conflict
    with the old `python` package.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12340,7 +12340,12 @@ self: super: with self; {
 
   plexwebsocket = callPackage ../development/python-modules/plexwebsocket { };
 
-  plfit = toPythonModule (pkgs.plfit.override { inherit (self) python; });
+  plfit = toPythonModule (
+    pkgs.plfit.override {
+      withPython = true;
+      inherit (self) python;
+    }
+  );
 
   plink = callPackage ../development/python-modules/plink { };
 
