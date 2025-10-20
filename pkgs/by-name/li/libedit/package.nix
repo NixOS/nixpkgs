850aeeec4e9e96fb669e4f648e2e9b87eae37bee
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-20 02:25:32 +0000

    libedit: 20250104-3.1 -> 20251016-3.1

--- pkgs/by-name/li/libedit/package.nix
+++ pkgs/by-name/li/libedit/package.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libedit";
-  version = "20250104-3.1";
+  version = "20251016-3.1";
 
   src = fetchurl {
     url = "https://thrysoee.dk/editline/libedit-${finalAttrs.version}.tar.gz";
-    hash = "sha256-I3knAWlFUKU3IGMM0c1hZxAbV3Ot3ctBBPc0W3OlaKw=";
+    hash = "sha256-ITYrAGU7v8HHH3GnV42ma1tSA1WdQxNNLddxnjE84EE=";
   };
 
   outputs = [
