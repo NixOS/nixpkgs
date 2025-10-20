3eb127a1811f85cbb3be6f6311ff44961f3b2a96
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-20 17:20:21 +0000

    kulala-fmt: 1.4.0 -> 2.10.2

--- pkgs/by-name/ku/kulala-fmt/package.nix
+++ pkgs/by-name/ku/kulala-fmt/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "kulala-fmt";
-  version = "1.4.0";
+  version = "2.10.2";
 
   src = fetchFromGitHub {
     owner = "mistweaverco";
     repo = "kulala-fmt";
     rev = "v${version}";
-    hash = "sha256-yq7DMrt+g5wM/tynI7Cf6MBJs/d+fP3IppndKnTJMTw=";
+    hash = "sha256-5H61ktwMRsLbfPl5Zd2ZWVROXk8srXqC7DxhNv80Bq0=";
   };
 
-  vendorHash = "sha256-GazDEm/qv0nh8vYT+Tf0n4QDGHlcYtbMIj5rlZBvpKo=";
+  vendorHash = null;
 
   env.CGO_ENABLED = 0;
 
