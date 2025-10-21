d3be4dd1dc66743b0f5f369b86c1609b3e562c4a
Fernando Rodrigues <alpha@sigmasquadron.net>
2025-10-21 11:02:19 +1100

    nixos/traefik: add documentation chapter
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- nixos/modules/services/web-servers/traefik.nix
+++ nixos/modules/services/web-servers/traefik.nix
@@ -530,8 +530,11 @@ in
     };
   };
 
-  meta.maintainers = with maintainers; [
-    jackr
-    therealgramdalf
-  ];
+  meta = {
+    maintainers = with lib.maintainers; [
+      jackr
+      therealgramdalf
+    ];
+    doc = ./traefik.md;
+  };
 }
