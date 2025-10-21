003b5ac29b2a6a54344db1108221ae8a6898f54d
Sarah Clark <seclark@nextquestion.net>
2025-10-21 15:38:03 -0700

    python3Packages.aiolifx: modernize derivation

--- pkgs/development/python-modules/aiolifx/default.nix
+++ pkgs/development/python-modules/aiolifx/default.nix
@@ -7,7 +7,6 @@
   fetchPypi,
   ifaddr,
   inquirerpy,
-  pythonOlder,
   setuptools,
 }:
 
@@ -15,8 +14,6 @@ buildPythonPackage rec {
   pname = "aiolifx";
   version = "1.2.1";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
@@ -38,12 +35,12 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "aiolifx" ];
 
-  meta = with lib; {
+  meta = {
     description = "Module for local communication with LIFX devices over a LAN";
     homepage = "https://github.com/aiolifx/aiolifx";
     changelog = "https://github.com/aiolifx/aiolifx/releases/tag/${version}";
-    license = licenses.mit;
-    maintainers = with maintainers; [ netixx ];
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ netixx ];
     mainProgram = "aiolifx";
   };
 }
