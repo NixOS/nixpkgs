30dcef68d0c61fa91a33ed01285d9d03cce75bfe
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-21 15:23:12 +0000

    coroot-node-agent: 1.26.3 -> 1.26.4

--- pkgs/by-name/co/coroot-node-agent/package.nix
+++ pkgs/by-name/co/coroot-node-agent/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "coroot-node-agent";
-  version = "1.26.3";
+  version = "1.26.4";
 
   src = fetchFromGitHub {
     owner = "coroot";
     repo = "coroot-node-agent";
     rev = "v${version}";
-    hash = "sha256-IdDH+jFVRbquM+9VkPw/Sb2dc6eajLpNsLlYkUEUNAo=";
+    hash = "sha256-bm3zMk44FNZ3vb9fP1Szbmi+0I1kOas0es8Cqu3yWeA=";
   };
 
   vendorHash = "sha256-LJq45IGXgYNx0Hky2w+O5Enwc5EvD79/cJRQ/iCythk=";
