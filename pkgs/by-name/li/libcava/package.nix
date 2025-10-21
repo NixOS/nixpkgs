884f11bb5d43bb7b06e9f217a018f9f17d0a1d9d
PerchunPak <git@perchun.it>
2025-10-21 17:31:10 +0200

    Revert "libcava: 0.10.4 -> 0.10.6"
    This reverts commit d37ee6e262a53eee2b813d1a6d2ede457d82995a.

--- pkgs/by-name/li/libcava/package.nix
+++ pkgs/by-name/li/libcava/package.nix
@@ -8,13 +8,13 @@
 cava.overrideAttrs (old: rec {
   pname = "libcava";
   # fork may not be updated when we update upstream
-  version = "0.10.6";
+  version = "0.10.4";
 
   src = fetchFromGitHub {
     owner = "LukashonakV";
     repo = "cava";
     tag = version;
-    hash = "sha256-63be1wypMiqhPA6sjMebmFE6yKpTj/bUE53sMWun554=";
+    hash = "sha256-9eTDqM+O1tA/3bEfd1apm8LbEcR9CVgELTIspSVPMKM=";
   };
 
   nativeBuildInputs = old.nativeBuildInputs ++ [
