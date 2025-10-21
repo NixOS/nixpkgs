1b2abff15cc45c5d2aff3942b184b397f5fea847
Philip Düe <42211220+nanophilip@users.noreply.github.com>
2025-10-21 14:39:38 +0800

    Update pkgs/by-name/ed/edukai/package.nix
    Co-authored-by: Yueh-Shun Li <shamrocklee@posteo.net>

--- pkgs/by-name/ed/edukai/package.nix
+++ pkgs/by-name/ed/edukai/package.nix
@@ -22,7 +22,8 @@ stdenvNoCC.mkDerivation rec {
   meta = {
     description = "The MOE Kai font, a regular-script-style Chinese character typeface";
     longDescription = ''
-        A regular-script-style Chinese character typeface published by the Ministry of Education of the Republic of China (Taiwan). Regular script is also referred to as 楷書, 楷书, kǎishū, 真書, 真书, zhēnshū, 正楷, zhèngkǎi, 楷體, 楷体, kǎitǐ, 正書, 正书, or zhèngshū in Chinese; khải thư in Vietnamese; 楷書, かいしょ, or Kaisho in Japanese.
+      A regular-script-style Chinese character typeface published by the Ministry of Education of the Republic of China (Taiwan).
+      Regular script is also referred to as 楷書, 楷书, kǎishū, 真書, 真书, zhēnshū, 正楷, zhèngkǎi, 楷體, 楷体, kǎitǐ, 正書, 正书, or zhèngshū in Chinese; khải thư in Vietnamese; 楷書, かいしょ, or Kaisho in Japanese.
     '';
     homepage = "http://language.moe.gov.tw/result.aspx?classify_sn=23&subclassify_sn=436&content_sn=47";
     license = lib.licenses.cc-by-nd-30;
