1404e235eaec2d69e6047c0e2ce274e7001ec890
Robert Schütz <nix@dotlambda.de>
2025-10-21 12:59:34 -0700

    python3Packages.aioamazondevices: 6.4.4 -> 6.4.6
    Diff: https://github.com/chemelli74/aioamazondevices/compare/v6.4.4...v6.4.6
    
    Changelog: https://github.com/chemelli74/aioamazondevices/blob/v6.4.6/CHANGELOG.md

--- pkgs/development/python-modules/aioamazondevices/default.nix
+++ pkgs/development/python-modules/aioamazondevices/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "aioamazondevices";
-  version = "6.4.4";
+  version = "6.4.6";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "chemelli74";
     repo = "aioamazondevices";
     tag = "v${version}";
-    hash = "sha256-zp6lWRnyDTHi1Pm2Ra8xCrRBGfge1wli1EEebv5T8gM=";
+    hash = "sha256-EjgZ/zmUSCaLDUmijYMCXvwpl4dWfjmTdlO5VLuzIHg=";
   };
 
   build-system = [ poetry-core ];
