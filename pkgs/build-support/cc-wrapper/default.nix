ab1e2895c4ce57f8b32fcd49a6b2562c5a9458fd
David McFarland <corngood@gmail.com>
2025-10-21 14:15:21 -0300

    cygwin-dll-link: make linkDLLs work on individual dirs/files

--- pkgs/build-support/cc-wrapper/default.nix
+++ pkgs/build-support/cc-wrapper/default.nix
@@ -602,19 +602,13 @@ stdenvNoCC.mkDerivation {
       dontUnpack = true;
       installPhase =
         if targetPlatform.isCygwin then
-          (
-            ''
-              echo addToSearchPath "HOST_PATH" "${cc_bin}/lib" >> $out
-              # Work around build failure caused by the gnulib workaround for
-              # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=114870. remove after
-              # gnulib is updated in core packages (e.g. iconv, gnupatch, gnugrep)
-              echo appendToVar configureFlags gl_cv_clean_version_stddef=yes >> $out
-            ''
-            # in a cross-toolchain, cygstdc++-6.dll is in /lib, but otherwise it's in /bin
-            + lib.optionalString (targetPlatform == hostPlatform) ''
-              echo addToSearchPath "HOST_PATH" "${cc_bin}/bin" >> $out
-            ''
-          )
+          ''
+            echo addToSearchPath "HOST_PATH" "${cc_solib}/bin" >> $out
+            # Work around build failure caused by the gnulib workaround for
+            # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=114870. remove after
+            # gnulib is updated in core packages (e.g. iconv, gnupatch, gnugrep)
+            echo appendToVar configureFlags gl_cv_clean_version_stddef=yes >> $out
+          ''
         else
           ''
             echo addToSearchPath "LINK_DLL_FOLDERS" "${cc_solib}/lib" > $out
