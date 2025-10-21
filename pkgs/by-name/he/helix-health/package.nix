073b8f189edd6bffa779c6f1b9396af79afa490a
Erdem Guner <gunererd@gmail.com>
2025-10-21 21:48:14 +0300

    helix-health: 1.0.2 -> 1.0.3, use buildGoModule
    - Update to version 1.0.3 which uses Go 1.24.5
    - Switch from buildGo125Module to standard buildGoModule
    - Use tag parameter instead of rev for better semantics
    - Remove postPatch workaround (no longer needed)

--- pkgs/by-name/he/helix-health/package.nix
+++ pkgs/by-name/he/helix-health/package.nix
@@ -1,29 +1,23 @@
 {
   lib,
-  buildGo125Module,
+  buildGoModule,
   fetchFromGitHub,
   makeWrapper,
   helix,
 }:
 
-buildGo125Module (finalAttrs: {
+buildGoModule (finalAttrs: {
   pname = "helix-health";
-  version = "1.0.2";
+  version = "1.0.3";
 
   src = fetchFromGitHub {
     owner = "gunererd";
     repo = "helix-health";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-zfdensdAOo4VtJWl3k5o/zfeu/9DddW85aq6VI1ZZRw=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-fuU8LVV30QMqpYvw1It1YBtgZ1iYmOdELOhwXk9wYio=";
   };
 
   vendorHash = "sha256-BStOOu6GxqKToSA9cEyIzJdgK2T7PPhfReuacFnh2fU=";
-
-  # Temporarily patch go.mod to work with available Go version
-  postPatch = ''
-    substituteInPlace go.mod \
-      --replace "go 1.25.2" "go 1.25.1"
-  '';
 
   nativeBuildInputs = [ makeWrapper ];
 
