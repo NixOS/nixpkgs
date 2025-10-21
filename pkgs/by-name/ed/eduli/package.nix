65855e29847f04541e65ddcd63cbb1c0420177c1
Philip Düe <42211220+nanophilip@users.noreply.github.com>
2025-10-21 14:44:53 +0800

    Update pkgs/by-name/ed/eduli/package.nix
    Co-authored-by: Yueh-Shun Li <shamrocklee@posteo.net>

--- pkgs/by-name/ed/eduli/package.nix
+++ pkgs/by-name/ed/eduli/package.nix
@@ -24,7 +24,8 @@ stdenvNoCC.mkDerivation rec {
   meta = {
     description = "The MOE Li font, a clerical-script-style Chinese character typeface";
     longDescription = ''
-        A clerical-script-style Chinese character typeface published by the Ministry of Education of the Republic of China (Taiwan). Clerical script is also referred to as 隸書, 隶书, or lìshū in Chinese; lệ thư or chữ lệ in Vietnamese; 예서, 隸書, or yeseo in Korean; 隷書体, れいしょたい, or reishotai in Japanese.
+      A clerical-script-style Chinese character typeface published by the Ministry of Education of the Republic of China (Taiwan).
+      Clerical script is also referred to as 隸書, 隶书, or lìshū in Chinese; lệ thư or chữ lệ in Vietnamese; 예서, 隸書, or yeseo in Korean; 隷書体, れいしょたい, or reishotai in Japanese.
     '';
     homepage = "http://language.moe.gov.tw/result.aspx?classify_sn=23&subclassify_sn=436&content_sn=49";
     license = lib.licenses.cc-by-nd-30;
