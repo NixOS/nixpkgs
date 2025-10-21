b6961d6534c138400363b83b98201d62dfd4e4ef
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-21 20:19:13 +0000

    teams-for-linux: 2.5.10 -> 2.6.1

--- pkgs/by-name/te/teams-for-linux/package.nix
+++ pkgs/by-name/te/teams-for-linux/package.nix
@@ -16,16 +16,16 @@
 
 buildNpmPackage rec {
   pname = "teams-for-linux";
-  version = "2.5.10";
+  version = "2.6.1";
 
   src = fetchFromGitHub {
     owner = "IsmaelMartinez";
     repo = "teams-for-linux";
     tag = "v${version}";
-    hash = "sha256-AUvsH1ul+pkWQq2GKJTjsds7IPLQoDhWBcu4LN0HRg0=";
+    hash = "sha256-fNFr1bdymPeclfICm0whAGBiUhBJHmW1m/MZWX7VOhI=";
   };
 
-  npmDepsHash = "sha256-zicJ9orYgnusTHrMX5rSrnsXA1ENjZZJKXvkqn8XLC4=";
+  npmDepsHash = "sha256-WkO/cNPq19Kh6nWwK/J3CjjfFhzEiPunEPv94eMLuaM=";
 
   nativeBuildInputs = [
     makeWrapper
