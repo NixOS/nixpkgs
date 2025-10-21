8edd06b9671979da6104d4f57fd1f5449639c223
Fabian Affolter <mail@fabian-affolter.ch>
2025-10-21 18:52:01 +0200

    python313Packages.google-cloud-vpc-access: remove disabled
    Removed the restriction for Python version 3.8.

--- pkgs/development/python-modules/google-cloud-vpc-access/default.nix
+++ pkgs/development/python-modules/google-cloud-vpc-access/default.nix
@@ -9,7 +9,6 @@
   protobuf,
   pytest-asyncio,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
@@ -17,8 +16,6 @@ buildPythonPackage rec {
   pname = "google-cloud-vpc-access";
   version = "1.14.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     pname = "google_cloud_vpc_access";
