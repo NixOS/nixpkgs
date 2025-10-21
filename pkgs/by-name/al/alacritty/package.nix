6e24250a5d315092e20614e39ee3568973613270
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-21 23:41:28 +0000

    alacritty: 0.15.1 -> 0.16.1

--- pkgs/by-name/al/alacritty/package.nix
+++ pkgs/by-name/al/alacritty/package.nix
@@ -44,7 +44,7 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "alacritty";
-  version = "0.15.1" + lib.optionalString withGraphics "-graphics";
+  version = "0.16.1" + lib.optionalString withGraphics "-graphics";
 
   src =
     # by default we want the official package
@@ -53,7 +53,7 @@ rustPlatform.buildRustPackage rec {
         owner = "alacritty";
         repo = "alacritty";
         tag = "v${version}";
-        hash = "sha256-/yERMNfCFLPb1S17Y9OacVH8UobDIIZDhM2qPzf5Vds=";
+        hash = "sha256-IOPhnJ76kZ2djJjxJEUwWPvHDeeXbJAn1ClipTH7nWs=";
       }
     # optionally we want to build the sixels feature fork
     else
@@ -66,7 +66,7 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash =
     if !withGraphics then
-      "sha256-uXwefUV1NAKqwwPIWj4Slkx0c5b+RfLR3caTb42fc4M="
+      "sha256-OBhrd4q44LCUGnjDEedhrOuoSC2UFR90IKSQfEPY/Q4="
     else
       "sha256-UtxZFqU974N+YcHoEHifBjNSyaVuMvuc1clTDgUPuoQ=";
 
