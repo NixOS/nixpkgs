28d59c1459b36b58b5e83f4b06cd0662753ff7ed
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-10-22 16:42:29 -0300

    cosmic-idle: 1.0.0-beta.2 -> 1.0.0-beta.3
    Diff: https://github.com/pop-os/cosmic-idle/compare/epoch-1.0.0-beta.2...epoch-1.0.0-beta.3

--- pkgs/by-name/co/cosmic-idle/package.nix
+++ pkgs/by-name/co/cosmic-idle/package.nix
@@ -16,7 +16,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-idle";
-  version = "1.0.0-beta.2";
+  version = "1.0.0-beta.3";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
