936d108aad26ae0a42334d99d7b393b7de32e6f9
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-22 16:01:21 +0000

    python3Packages.fastcore: 1.8.12 -> 1.8.13

--- pkgs/development/python-modules/fastcore/default.nix
+++ pkgs/development/python-modules/fastcore/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "fastcore";
-  version = "1.8.12";
+  version = "1.8.13";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "fastai";
     repo = "fastcore";
     tag = version;
-    hash = "sha256-YJONK7WMAQLCkROosGbT5C1G/JtTC7iZs2t+mx03yOo=";
+    hash = "sha256-5bgFV7ZGk+s8jjQaE13lWY+fIStin701ZrzyoJ7ziyY=";
   };
 
   build-system = [ setuptools ];
