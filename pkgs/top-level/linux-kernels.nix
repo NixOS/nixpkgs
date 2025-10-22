fc4bf6830d73a1bc643e0d225f06c7a23bec36db
Maximilian Bosch <maximilian@mbosch.me>
2025-10-22 12:45:11 +0200

    linux_6_16: remove
    EOL upstream.
    
    (cherry picked from commit 4a8ef0e0cf75aaf5e65d726a1e87de833d2fd951)

--- pkgs/top-level/linux-kernels.nix
+++ pkgs/top-level/linux-kernels.nix
@@ -211,14 +211,6 @@ in
           ];
         };
 
-        linux_6_16 = callPackage ../os-specific/linux/kernel/mainline.nix {
-          branch = "6.16";
-          kernelPatches = [
-            kernelPatches.bridge_stp_helper
-            kernelPatches.request_key_helper
-          ];
-        };
-
         linux_6_17 = callPackage ../os-specific/linux/kernel/mainline.nix {
           branch = "6.17";
           kernelPatches = [
@@ -320,6 +312,7 @@ in
         linux_6_13 = throw "linux 6.13 was removed because it has reached its end of life upstream";
         linux_6_14 = throw "linux 6.14 was removed because it has reached its end of life upstream";
         linux_6_15 = throw "linux 6.15 was removed because it has reached its end of life upstream";
+        linux_6_16 = throw "linux 6.16 was removed because it has reached its end of life upstream";
 
         linux_4_19_hardened = throw "linux 4.19 was removed because it will reach its end of life within 24.11";
         linux_6_9_hardened = throw "linux 6.9 was removed because it has reached its end of life upstream";
@@ -752,7 +745,6 @@ in
     linux_6_1 = recurseIntoAttrs (packagesFor kernels.linux_6_1);
     linux_6_6 = recurseIntoAttrs (packagesFor kernels.linux_6_6);
     linux_6_12 = recurseIntoAttrs (packagesFor kernels.linux_6_12);
-    linux_6_16 = recurseIntoAttrs (packagesFor kernels.linux_6_16);
     linux_6_17 = recurseIntoAttrs (packagesFor kernels.linux_6_17);
   }
   // lib.optionalAttrs config.allowAliases {
@@ -763,6 +755,7 @@ in
     linux_6_13 = throw "linux 6.13 was removed because it reached its end of life upstream"; # Added 2025-06-22
     linux_6_14 = throw "linux 6.14 was removed because it reached its end of life upstream"; # Added 2025-06-22
     linux_6_15 = throw "linux 6.15 was removed because it reached its end of life upstream"; # Added 2025-08-23
+    linux_6_16 = throw "linux 6.16 was removed because it reached its end of life upstream"; # Added 2025-10-22
   };
 
   rtPackages = {
