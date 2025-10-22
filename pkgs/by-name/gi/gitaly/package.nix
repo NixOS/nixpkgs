c56fff34f9e28184c066447adcd272427013e041
Leona Maroni <dev@leona.is>
2025-10-22 07:40:03 +0200

    gitlab: 18.5.0 -> 18.5.1
    https://about.gitlab.com/releases/2025/10/22/patch-release-gitlab-18-5-1-released/

--- pkgs/by-name/gi/gitaly/package.nix
+++ pkgs/by-name/gi/gitaly/package.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  version = "18.5.0";
+  version = "18.5.1";
   package_version = "v${lib.versions.major version}";
   gitaly_package = "gitlab.com/gitlab-org/gitaly/${package_version}";
 
@@ -21,7 +21,7 @@ let
       owner = "gitlab-org";
       repo = "gitaly";
       rev = "v${version}";
-      hash = "sha256-kVFO8brtXWWGU2nWTtHR1q5RrTIXy2ssra9YjtWsglU=";
+      hash = "sha256-719FC9+OBX9Li9gkIpusFoZrpMyeDwCsoWxt9pfhI1A=";
     };
 
     vendorHash = "sha256-I2YMn84wEAY+Z02bmkyP/b0eix7FW3hP/noyEKYsEaQ=";
