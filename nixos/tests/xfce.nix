b22b5a3c20c81569504934ed7c897a3ce1a0f611
Will Fancher <elvishjerricco@gmail.com>
2025-10-22 01:43:39 -0400

    nixos/tests: More temporary uaccess fixes
    See: https://github.com/NixOS/nixpkgs/pull/453603

--- nixos/tests/xfce.nix
+++ nixos/tests/xfce.nix
@@ -38,7 +38,8 @@
         machine.succeed("xauth merge ${user.home}/.Xauthority")
 
       with subtest("Check that logging in has given the user ownership of devices"):
-        machine.succeed("getfacl -p /dev/snd/timer | grep -q ${user.name}")
+        # Change back to /dev/snd/timer after systemd-258.1
+        machine.succeed("getfacl -p /dev/dri/card0 | grep -q ${user.name}")
 
       with subtest("Check if Xfce components actually start"):
         machine.wait_for_window("xfce4-panel")
