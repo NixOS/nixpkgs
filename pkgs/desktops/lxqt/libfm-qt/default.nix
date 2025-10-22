97bf55a00bdd3444daa7d1fef30e2318178c09d7
Martin Joerg <martin.joerg@gmail.com>
2025-10-22 10:00:39 +0000

    lxqt.libfm-qt: modernize

--- pkgs/desktops/lxqt/libfm-qt/default.nix
+++ pkgs/desktops/lxqt/libfm-qt/default.nix
@@ -20,23 +20,23 @@
   qtx11extras ? null,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "libfm-qt";
   inherit version;
 
   src = fetchFromGitHub {
     owner = "lxqt";
     repo = "libfm-qt";
-    rev = version;
+    tag = finalAttrs.version;
     hash =
       {
         "1.4.0" = "sha256-QxPYSA7537K+/dRTxIYyg+Q/kj75rZOdzlUsmSdQcn4=";
         "2.2.0" = "sha256-xLXHwrcMJ8PObZ2qWVZTf9FREcjUi5qtcCJgNHj391Q=";
       }
-      ."${version}";
+      ."${finalAttrs.version}";
   };
 
-  patches = lib.optionals (version == "2.2.0") [
+  patches = lib.optionals (finalAttrs.version == "2.2.0") [
     # fix build against Qt >= 6.10 (https://github.com/lxqt/libfm-qt/pull/1060)
     # TODO: drop when upgrading beyond version 2.2.0
     (fetchpatch {
@@ -63,15 +63,15 @@ stdenv.mkDerivation rec {
     lxqt-menu-data
     menu-cache
   ]
-  ++ (lib.optionals (lib.versionAtLeast "2.0.0" version) [ qtx11extras ]);
+  ++ (lib.optionals (lib.versionAtLeast "2.0.0" finalAttrs.version) [ qtx11extras ]);
 
   passthru.updateScript = gitUpdater { };
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/lxqt/libfm-qt";
     description = "Core library of PCManFM-Qt (Qt binding for libfm)";
-    license = licenses.lgpl21Plus;
-    platforms = with platforms; unix;
-    teams = [ teams.lxqt ];
+    license = lib.licenses.lgpl21Plus;
+    platforms = lib.platforms.unix;
+    teams = [ lib.teams.lxqt ];
   };
-}
+})
