ccb612fa02c5a5a4185b859ab82fa3dc61756a7c
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-22 15:40:07 +0000

    python3Packages.async-modbus: 0.2.2 -> 0.2.3

--- pkgs/development/python-modules/async-modbus/default.nix
+++ pkgs/development/python-modules/async-modbus/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "async-modbus";
-  version = "0.2.2";
+  version = "0.2.3";
   format = "pyproject";
 
   disabled = pythonOlder "3.7";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "tiagocoutinho";
     repo = "async_modbus";
     tag = "v${version}";
-    hash = "sha256-xms2OfX5bHPXswwhLhyh6HFsm1YqDwKclUirxrgL4i0=";
+    hash = "sha256-d4TTs3TtD/9eFdzXBaY+QeAMeRWTvsWeaxONeG0AXJU=";
   };
 
   patches = [
