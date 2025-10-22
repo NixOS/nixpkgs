c53d2ae60c3e366a71ad0dc31497848f5618530a
yunfachi <yunfachi@gmail.com>
2025-10-22 19:29:57 +0300

    lib/trivial: rewrite mod from integer modulus to mathematical modulo

--- lib/trivial.nix
+++ lib/trivial.nix
@@ -559,17 +559,17 @@ in
   max = x: y: if x > y then x else y;
 
   /**
-    Integer modulus
+    Mathematical modulus
 
     # Inputs
 
-    `base`
+    `dividend`
 
-    : 1\. Function argument
+    : The number to be divided
 
-    `int`
+    `divisor`
 
-    : 2\. Function argument
+    : The number by which to divide
 
     # Examples
     :::{.example}
@@ -580,11 +580,25 @@ in
     => 1
     mod 1 10
     => 1
+    mod (-1) 10
+    => 9
+    mod 10 (-3)
+    => 1
+    mod 5.5 2
+    => 1.5
+    mod (-3.7) 2.5
+    => 1.3
     ```
 
     :::
   */
-  mod = base: int: base - (int * (builtins.div base int));
+  mod =
+    dividend: divisor:
+    let
+      quotient = builtins.floor (dividend / divisor);
+      remainder = dividend - quotient * divisor;
+    in
+    if remainder < 0 then remainder + lib.abs divisor else remainder;
 
   /**
     C-style remainder
