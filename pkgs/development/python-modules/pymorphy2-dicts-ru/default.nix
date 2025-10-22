b2a38b0bfadb10e503bb07cc9a8a117a461455bb
Doron Behar <doron.behar@gmail.com>
2025-10-22 11:47:43 +0300

    python3.pkgs.pymorphy2-dicts-ru: use pyproject attribute

--- pkgs/development/python-modules/pymorphy2-dicts-ru/default.nix
+++ pkgs/development/python-modules/pymorphy2-dicts-ru/default.nix
@@ -2,17 +2,24 @@
   lib,
   fetchPypi,
   buildPythonPackage,
+
+  # build-system
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "pymorphy2-dicts-ru";
   version = "2.4.417127.4579844";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-eMrQOtymBQIavTh6Oy61FchRuG6UaCoe8jVKLHT8wZY=";
   };
+
+  build-system = [
+    setuptools
+  ];
 
   # has no tests
   doCheck = false;
