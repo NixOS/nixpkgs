0daa3ea1757c9b8f7be45a7da8c4ba28d4c82618
Acid Bong <acidbong@tilde.club>
2025-10-23 13:09:08 +0300

    birdtray: refresh the recipe to the current guidelines
    - remove `with lib`
    - use `tag` and `hash` in source
    - replace `rec` with `finalAttrs`

--- pkgs/by-name/bi/birdtray/package.nix
+++ pkgs/by-name/bi/birdtray/package.nix
@@ -8,15 +8,15 @@
   libsForQt5,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "birdtray";
   version = "1.11.4";
 
   src = fetchFromGitHub {
     owner = "gyunaev";
     repo = "birdtray";
-    rev = "v${version}";
-    sha256 = "sha256-rj8tPzZzgW0hXmq8c1LiunIX1tO/tGAaqDGJgCQda5M=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-rj8tPzZzgW0hXmq8c1LiunIX1tO/tGAaqDGJgCQda5M=";
   };
 
   nativeBuildInputs = [
@@ -40,12 +40,12 @@ stdenv.mkDerivation rec {
   # https://github.com/gyunaev/birdtray/issues/113#issuecomment-621742315
   qtWrapperArgs = [ "--set QT_QPA_PLATFORM xcb" ];
 
-  meta = with lib; {
+  meta = {
     description = "Mail system tray notification icon for Thunderbird";
     mainProgram = "birdtray";
     homepage = "https://github.com/gyunaev/birdtray";
-    license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ Flakebi ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ Flakebi ];
+    platforms = lib.platforms.linux;
   };
-}
+})
