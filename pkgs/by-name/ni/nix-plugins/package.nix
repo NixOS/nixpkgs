ae508529220343021c754aa3c47c0b7d5fd4e424
dmfrpro <dmfr2021y@gmail.com>
2025-10-24 00:52:25 +0300

    nix-plugins: add missing include
    See https://github.com/shlevy/nix-plugins/pull/23
    
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- pkgs/by-name/ni/nix-plugins/package.nix
+++ pkgs/by-name/ni/nix-plugins/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch2,
   nixVersions,
   nixComponents ? nixVersions.nixComponents_2_30,
   cmake,
@@ -11,7 +12,7 @@
 
 stdenv.mkDerivation rec {
   pname = "nix-plugins";
-  version = "16.0.0";
+  version = "16.0.0-patched";
 
   src = fetchFromGitHub {
     owner = "shlevy";
@@ -19,6 +20,15 @@ stdenv.mkDerivation rec {
     rev = version;
     hash = "sha256-yofHs1IyAkyMqrWlLkmnX+CmH+qsvlhKN1YZM4nRf1M=";
   };
+
+  patches = [
+    # https://github.com/shlevy/nix-plugins/pull/23
+    (fetchpatch2 {
+      name = "fix-build-nix-2.32.1.patch";
+      url = "https://github.com/shlevy/nix-plugins/commit/e0115e16b3835e99b7aa4ced15746159e79a1be6.patch";
+      hash = "sha256-E6QISZ8k8xKNzWqIeaOzxJN0DQvkMSQul9MJBdxwymk=";
+    })
+  ];
 
   nativeBuildInputs = [
     cmake
