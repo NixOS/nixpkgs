be0d4b539fe1f02e3f4d0cc343488babae154318
pancaek <20342389+pancaek@users.noreply.github.com>
2025-10-22 19:07:07 -0700

    pro-office-calculator: modernize, move to by-name/

--- pkgs/by-name/pr/pro-office-calculator/package.nix
+++ pkgs/by-name/pr/pro-office-calculator/package.nix
@@ -1,37 +1,39 @@
 {
-  mkDerivation,
+  stdenv,
   lib,
   fetchFromGitHub,
   tinyxml-2,
   cmake,
-  qtbase,
-  qtmultimedia,
+  libsForQt5,
 }:
-mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   version = "1.0.13";
   pname = "pro-office-calculator";
 
   src = fetchFromGitHub {
     owner = "RobJinman";
     repo = "pro_office_calc";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     sha256 = "1v75cysargmp4fk7px5zgib1p6h5ya4w39rndbzk614fcnv0iipd";
   };
 
   buildInputs = [
-    qtbase
-    qtmultimedia
+    libsForQt5.qtbase
+    libsForQt5.qtmultimedia
     tinyxml-2
   ];
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+    libsForQt5.wrapQtAppsHook
+  ];
 
-  meta = with lib; {
+  meta = {
     description = "Completely normal office calculator";
     mainProgram = "procalc";
     homepage = "https://proofficecalculator.com/";
-    maintainers = [ maintainers.pmiddend ];
-    platforms = platforms.linux;
-    license = licenses.gpl3;
+    maintainers = with lib.maintainers; [ pmiddend ];
+    platforms = lib.platforms.linux;
+    license = lib.licenses.gpl3;
   };
-}
+})
