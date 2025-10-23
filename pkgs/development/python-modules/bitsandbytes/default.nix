c06092c220a9d3d507d732931b938f41fbb10ffa
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-23 15:50:42 +0000

    python3Packages.bitsandbytes: 0.48.1 -> continuous-release_main

--- pkgs/development/python-modules/bitsandbytes/default.nix
+++ pkgs/development/python-modules/bitsandbytes/default.nix
@@ -26,7 +26,7 @@
 
 let
   pname = "bitsandbytes";
-  version = "0.48.1";
+  version = "continuous-release_main";
 
   brokenConditions = lib.attrsets.filterAttrs (_: cond: cond) {
     "CUDA and ROCm are mutually exclusive" = cudaSupport && rocmSupport;
@@ -78,7 +78,7 @@ buildPythonPackage {
     owner = "bitsandbytes-foundation";
     repo = "bitsandbytes";
     tag = version;
-    hash = "sha256-OkhWv5Mb/cnWJteCXvDEkWQvK+QK26YQex39yWIezrQ=";
+    hash = "sha256-2UE6AhMa6mjY+xCaKMZZ4l3SdbxUQh6T/WDqVWpsBxk=";
   };
 
   patches = [
