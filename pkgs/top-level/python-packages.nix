d0edcc0813d1397a23d095ed7b910ae5d9c1885e
Ben Siraphob <bensiraphob@gmail.com>
2025-10-23 14:21:22 -0700

    python3Packages.inspect-ai: init at 0.3.140

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -7135,6 +7135,8 @@ self: super: with self; {
 
   insightface = callPackage ../development/python-modules/insightface { };
 
+  inspect-ai = callPackage ../development/python-modules/inspect-ai { };
+
   installer = callPackage ../development/python-modules/installer { };
 
   insteon-frontend-home-assistant =
