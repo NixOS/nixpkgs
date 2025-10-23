6abf94e8de604b9778ae4a615b64225966d349e6
Daniel Woffinden <daw@hey.com>
2025-10-23 21:06:36 +0100

    hledger-fmt: 0.3.1 -> 0.3.3

--- pkgs/by-name/hl/hledger-fmt/package.nix
+++ pkgs/by-name/hl/hledger-fmt/package.nix
@@ -9,20 +9,20 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "hledger-fmt";
-  version = "0.3.1";
+  version = "0.3.3";
 
   src = fetchFromGitHub {
     owner = "mondeja";
     repo = "hledger-fmt";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-5dPKSUJLxQ5WXZefuwr3wLhr3MNzy+dzpJkgKFU0/SE=";
+    hash = "sha256-k84lmRhMDLGhMBsG+HBFGTrbeArbEnD8VLl8R70e8Ws=";
   };
 
   nativeBuildInputs = [
     installShellFiles
   ];
 
-  cargoHash = "sha256-yErH9AyfBZF0PByqViVhXHB/FAc0awz4bOyAHtcHe0s=";
+  cargoHash = "sha256-8pG3/huUxtNjy5R5/udOejSgPRynQycAuGxVDqqWbLM=";
 
   # Tests try to invoke the binary from "target/debug/hledger-fmt"
   # https://github.com/mondeja/hledger-fmt/blob/783abdb32eefb20195c7e9562858552935bb9c8e/src/cli/tests.rs#L5
