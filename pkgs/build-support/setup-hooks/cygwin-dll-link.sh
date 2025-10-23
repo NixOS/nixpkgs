b23c8ab816970e41fc982334896e1fc0fa0dffa5
David McFarland <corngood@gmail.com>
2025-10-23 15:02:07 -0300

    cygwin-dll-link: fix indentation

--- pkgs/build-support/setup-hooks/cygwin-dll-link.sh
+++ pkgs/build-support/setup-hooks/cygwin-dll-link.sh
@@ -1,6 +1,6 @@
 addLinkDLLPaths() {
-    addToSearchPath "LINK_DLL_FOLDERS" "$1/lib"
-    addToSearchPath "LINK_DLL_FOLDERS" "$1/bin"
+  addToSearchPath "LINK_DLL_FOLDERS" "$1/lib"
+  addToSearchPath "LINK_DLL_FOLDERS" "$1/bin"
 }
 
 addEnvHooks "$targetOffset" addLinkDLLPaths
@@ -15,33 +15,33 @@ addOutputDLLPaths() {
 postInstallHooks+=(addOutputDLLPaths)
 
 _dllDeps() {
-    "$OBJDUMP" -p "$1" \
-        | sed -n 's/.*DLL Name: \(.*\)/\1/p' \
-        | sort -u
+  "$OBJDUMP" -p "$1" \
+    | sed -n 's/.*DLL Name: \(.*\)/\1/p' \
+    | sort -u
 }
 
 _linkDeps() {
-    local target="$1" dir="$2" check="$3"
-    echo 'target:' "$target"
-    local dll
-    _dllDeps "$target" | while read dll; do
-        echo '  dll:' "$dll"
-        if [[ -e "$dir/$dll" ]]; then continue; fi
-        # Locate the DLL - it should be an *executable* file on $LINK_DLL_FOLDERS.
-        local dllPath="$(PATH="$(dirname "$target"):$LINK_DLL_FOLDERS" type -P "$dll")"
-        if [[ -z "$dllPath" ]]; then
-          if [[ -z "$check" || -n "${allowedImpureDLLsMap[$dll]}" ]]; then
-             continue
-          fi
-          echo unable to find $dll in $LINK_DLL_FOLDERS >&2
-          exit 1
-        fi
-        echo '    linking to:' "$dllPath"
-        CYGWIN+=\ winsymlinks:nativestrict ln -sr "$dllPath" "$dir"
-        # That DLL might have its own (transitive) dependencies,
-        # so add also all DLLs from its directory to be sure.
-        _linkDeps "$dllPath" "$dir" ""
-    done
+  local target="$1" dir="$2" check="$3"
+  echo 'target:' "$target"
+  local dll
+  _dllDeps "$target" | while read dll; do
+    echo '  dll:' "$dll"
+    if [[ -e "$dir/$dll" ]]; then continue; fi
+    # Locate the DLL - it should be an *executable* file on $LINK_DLL_FOLDERS.
+    local dllPath="$(PATH="$(dirname "$target"):$LINK_DLL_FOLDERS" type -P "$dll")"
+    if [[ -z "$dllPath" ]]; then
+      if [[ -z "$check" || -n "${allowedImpureDLLsMap[$dll]}" ]]; then
+        continue
+      fi
+      echo unable to find $dll in $LINK_DLL_FOLDERS >&2
+      exit 1
+    fi
+    echo '    linking to:' "$dllPath"
+    CYGWIN+=\ winsymlinks:nativestrict ln -sr "$dllPath" "$dir"
+    # That DLL might have its own (transitive) dependencies,
+    # so add also all DLLs from its directory to be sure.
+    _linkDeps "$dllPath" "$dir" ""
+  done
 }
 
 linkDLLs() {
