80e2fcd3ec34b74a67905db8b52ddd723b56ae16
Robert Hensing <roberth@users.noreply.github.com>
2025-10-23 21:22:33 +0200

    netlify-cli: add issue link
    (cherry picked from commit d9ab8762935861e148d63ce078c35265a52fbe18)

--- pkgs/by-name/ne/netlify-cli/package.nix
+++ pkgs/by-name/ne/netlify-cli/package.nix
@@ -22,6 +22,7 @@ buildNpmPackage rec {
 
   # Prevent postinstall script from running before package is built
   # See https://github.com/netlify/cli/blob/v23.9.2/scripts/postinstall.js#L70
+  # This currently breaks completions: https://github.com/NixOS/nixpkgs/issues/455005
   postPatch = ''
     touch .git
   '';
