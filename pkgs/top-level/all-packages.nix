c37ed775a74ef97b80ff14b1ec7fc184779d33a2
pancaek <20342389+pancaek@users.noreply.github.com>
2025-10-24 15:33:33 -0700

    rogue: modernize, move to by-name/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13310,10 +13310,6 @@ with pkgs;
 
   vkquake = callPackage ../by-name/qu/quakespasm/vulkan.nix { };
 
-  rogue = callPackage ../games/rogue {
-    ncurses = ncurses5;
-  };
-
   rott-shareware = callPackage ../by-name/ro/rott/package.nix {
     buildShareware = true;
   };
