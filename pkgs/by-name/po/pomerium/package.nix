c764f859060f5672abe0a8929025e39e9aace735
h7x4 <h7x4@nani.wtf>
2025-10-24 14:00:32 +0900

    pomerium{,-cli}: concatMap id -> concatLists

--- pkgs/by-name/po/pomerium/package.nix
+++ pkgs/by-name/po/pomerium/package.nix
@@ -12,8 +12,7 @@
 let
   inherit (lib)
     concatStringsSep
-    concatMap
-    id
+    concatLists
     mapAttrsToList
     ;
 in
@@ -78,7 +77,7 @@ buildGoModule rec {
         };
       };
       concatStringsSpace = list: concatStringsSep " " list;
-      mapAttrsToFlatList = fn: list: concatMap id (mapAttrsToList fn list);
+      mapAttrsToFlatList = fn: list: concatLists (mapAttrsToList fn list);
       varFlags = concatStringsSpace (
         mapAttrsToFlatList (
           package: packageVars:
