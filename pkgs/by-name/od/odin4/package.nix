d14d9c1a5d73b49ca93762ffbf336416aeee1c3d
David Wronek <david.wronek@mainlining.org>
2025-10-24 19:37:15 +0200

    odin4: init at 1.2.1-dc05e3ea
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

---
+++ pkgs/by-name/od/odin4/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchzip,
+  autoPatchelfHook,
+}:
+stdenv.mkDerivation {
+  pname = "odin4";
+  version = "1.2.1-dc05e3ea";
+
+  src = fetchzip {
+    url = "https://web.archive.org/web/20230225072710if_/https://forum.xda-developers.com/attachments/odin-zip.5629297/";
+    hash = "sha256-SoznK53UD/vblqeXBLRlkokaLJwhMZy7wqKufR0I8hI=";
+    extension = "zip";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm555 -t $out/bin odin4
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Firmware flasher for Samsung devices";
+    license = lib.licenses.unfree;
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    platforms = [ "x86_64-linux" ];
+    maintainers = with lib.maintainers; [ ungeskriptet ];
+    mainProgram = "odin4";
+  };
+}
