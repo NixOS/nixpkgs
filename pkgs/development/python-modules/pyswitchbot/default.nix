3d8f124bd8fab1c218f605b5d513e3e72eb82f92
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-24 18:42:55 +0000

    python3Packages.pyswitchbot: 0.71.0 -> 0.72.0

--- pkgs/development/python-modules/pyswitchbot/default.nix
+++ pkgs/development/python-modules/pyswitchbot/default.nix
@@ -16,7 +16,7 @@
 
 buildPythonPackage rec {
   pname = "pyswitchbot";
-  version = "0.71.0";
+  version = "0.72.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "Danielhiversen";
     repo = "pySwitchbot";
     tag = version;
-    hash = "sha256-MFWeU3KaCtEEvsNuSlLrWxZTYgER+/A6nF2yCvmGgTk=";
+    hash = "sha256-ouvsAZe3HeaSMdCdia+ttliKMxqNtps/TbHBkri/iaw=";
   };
 
   build-system = [ setuptools ];
