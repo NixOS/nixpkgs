9b6d78b4d933aed3785362dccc0bb3440b779d00
Kylie McClain <kylie@somas.is>
2025-10-23 20:21:31 -0400

    kdePackages.libquotient: add upstream patch to fix library consumers
    NeoChat currently fails to build fails without this.
    <https://github.com/quotient-im/libQuotient/pull/932>

--- pkgs/development/libraries/libquotient/default.nix
+++ pkgs/development/libraries/libquotient/default.nix
@@ -2,6 +2,7 @@
   stdenv,
   lib,
   fetchFromGitHub,
+  fetchpatch,
   cmake,
   olm,
   openssl,
@@ -25,6 +26,17 @@ stdenv.mkDerivation rec {
     tag = version;
     hash = "sha256-wdIE5LI4l3WUvpGfoJBL8sjBl2k8NfZTh9CjfJc9FIA=";
   };
+
+  patches = [
+    # Fix use of libquotient by library consumers like NeoChat;
+    # without this (upstreamed) patch, consumers will fail to
+    # build when trying to link to Qt6::CorePrivate.
+    # Remove when bumping package version.
+    (fetchpatch {
+      url = "https://github.com/quotient-im/libQuotient/commit/6d5a80ddaab5803c318240c7978a16fbdc36bb34.patch";
+      hash = "sha256-JMdcywGgZ0Gev/Nce4oPiMJQxTBJYPoq+WoT3WLWWNQ=";
+    })
+  ];
 
   nativeBuildInputs = [ cmake ];
 
