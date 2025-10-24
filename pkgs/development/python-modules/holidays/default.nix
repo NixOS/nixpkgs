1c7be4ab1f077cdf1bfddf1166d19ec36a7b2e30
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-24 05:30:28 +0000

    python3Packages.holidays: 0.82 -> 0.83

--- pkgs/development/python-modules/holidays/default.nix
+++ pkgs/development/python-modules/holidays/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "holidays";
-  version = "0.82";
+  version = "0.83";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "vacanza";
     repo = "python-holidays";
     tag = "v${version}";
-    hash = "sha256-H+qG9GqSPFODn0kWSIY/XJ1wAPI/iArCWR3yG+pFZXE=";
+    hash = "sha256-GlOydhDSg03uZUxLXDoaT/Jq3DMk+HsSxBtPQE9DQ3U=";
   };
 
   build-system = [
