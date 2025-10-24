aa2106ad9b3f6f4f1225afa251389913934f9df4
pancaek <20342389+pancaek@users.noreply.github.com>
2025-10-24 15:26:51 -0700

    chiaki-ng: modernize, move to by-name/

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13078,8 +13078,6 @@ with pkgs;
 
   cataclysm-dda-git = cataclysmDDA.git.tiles;
 
-  chiaki-ng = kdePackages.callPackage ../games/chiaki-ng { };
-
   cockatrice = libsForQt5.callPackage ../games/cockatrice {
     protobuf = protobuf_21;
   };
