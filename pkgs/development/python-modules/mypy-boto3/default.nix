4fd9d664502ca6f234ba09b44b8e0b78a3f21cfd
Fabian Affolter <mail@fabian-affolter.ch>
2025-10-24 15:02:38 +0200

    python312Packages.mypy-boto3-iot: 1.40.0 -> 1.40.57

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -633,8 +633,8 @@ in
       "sha256-mj2Pf4PNgBS0sBbTkKuszIcaBHf5kTwNzR3BYgoI5JM=";
 
   mypy-boto3-iot =
-    buildMypyBoto3Package "iot" "1.40.0"
-      "sha256-0AUK0HaqmoLVbbLDcsagUZX7KkFF9zU7obO0BmcK8+s=";
+    buildMypyBoto3Package "iot" "1.40.57"
+      "sha256-BZHAr9Qja9pNVrNd7dDmc/+bKfqmzAUJPoPJUMgJLk0=";
 
   mypy-boto3-iot-data =
     buildMypyBoto3Package "iot-data" "1.40.55"
