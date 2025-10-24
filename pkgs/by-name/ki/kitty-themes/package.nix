22a5c492c4dd28dbf8193228901a693b9da8f121
Sigmanificient <edhyjox@gmail.com>
2025-10-24 02:48:03 +0200

    kitty-themes: split into sub packages

--- pkgs/by-name/ki/kitty-themes/package.nix
+++ pkgs/by-name/ki/kitty-themes/package.nix
@@ -2,11 +2,10 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
-  unstableGitUpdater,
+  jq,
+  symlinkJoin,
 }:
-
-stdenvNoCC.mkDerivation {
-  pname = "kitty-themes";
+let
   version = "0-unstable-2024-08-14";
 
   src = fetchFromGitHub {
@@ -16,27 +15,78 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-vt5y3Ai1KMgRhFrkfhA8G9Ve6BEFrgkCF3ssGlOdekw=";
   };
 
-  dontConfigure = true;
-  dontBuild = true;
+  convertAttrName =
+    name: lib.replaceStrings [ " - " "_" " " "(" ")" ] [ "-" "-" "-" "" "" ] (lib.strings.toLower name);
 
-  installPhase = ''
-    runHook preInstall
-
-    install -Dm644 -t $out/share/kitty-themes/ themes.json
-    mv themes $out/share/kitty-themes
-
-    runHook postInstall
-  '';
-
-  passthru.updateScript = unstableGitUpdater {
-    hardcodeZeroVersion = true;
+  licenseMap = with lib.licenses; {
+    "CC-BY-SA-4.0" = cc-by-nc-sa-40;
+    "CC0 1.0" = cc0;
+    "CC0 Public Domain" = cc0;
+    "GNU GPLv3" = gpl3Only;
+    "GNU General Public License v3.0" = gpl3Plus;
+    "GPL-3" = gpl3Only;
+    "GPLv3" = gpl3Only;
+    "GPLv3+" = gpl3Plus;
+    "LGPL-3.0" = lgpl3;
+    "MIT license - https://www.mit.edu/~amini/LICENSE.md" = mit;
+    "MIT" = mit;
+    "MIT, I prefer credit (and money) to none, but I won't sue" = mit;
+    "MIT/X11" = [
+      mit
+      x11
+    ];
+    "UPL-1.0" = upl;
+    "unfree" = unfree;
   };
 
-  meta = {
-    homepage = "https://github.com/kovidgoyal/kitty-themes";
-    description = "Themes for the kitty terminal emulator";
-    license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [ sigmanificient ];
-    platforms = lib.platforms.all;
-  };
-}
+  makeKittyTheme =
+    {
+      name,
+      caskName ? convertAttrName name,
+      blurb ? "Kitty theme ${name}",
+      file,
+      license ? "unfree",
+      ...
+    }:
+
+    stdenvNoCC.mkDerivation {
+      pname = "kitty-theme-${caskName}";
+
+      inherit src version;
+
+      dontConfigure = true;
+      dontBuild = true;
+
+      nativeBuildInputs = [ jq ];
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/share/kitty-themes
+        mv ${file} $out/share/kitty-themes
+
+        jq '[.[] | select(.name == "${name}")]' themes.json \
+          > $out/share/kitty-themes/themes.json
+
+        runHook postInstall
+      '';
+
+      meta = {
+        homepage = "https://github.com/kovidgoyal/kitty-themes";
+        description = blurb;
+        license = lib.flatten [
+          lib.licenses.gpl3Only # themes.json
+          licenseMap.${license}
+        ];
+        maintainers = with lib.maintainers; [ sigmanificient ];
+        platforms = lib.platforms.all;
+      };
+    };
+
+  themes = lib.listToAttrs (
+    map (theme: {
+      name = convertAttrName theme.name;
+      value = makeKittyTheme theme;
+    }) (lib.importJSON ./themes.json)
+  );
+in themes
