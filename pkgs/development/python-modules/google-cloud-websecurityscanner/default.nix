53ab25d4b0f2e123d9087c81b6ef6ecb1624c63a
Fabian Affolter <mail@fabian-affolter.ch>
2025-10-24 20:54:30 +0200

    python313Packages.google-cloud-websecurityscanner: remove disabled
    Removed pythonOlder dependency and its usage.

--- pkgs/development/python-modules/google-cloud-websecurityscanner/default.nix
+++ pkgs/development/python-modules/google-cloud-websecurityscanner/default.nix
@@ -8,7 +8,6 @@
   protobuf,
   pytest-asyncio,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
@@ -16,8 +15,6 @@ buildPythonPackage rec {
   pname = "google-cloud-websecurityscanner";
   version = "1.18.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "google_cloud_websecurityscanner";
