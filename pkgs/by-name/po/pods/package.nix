33b3ec2cb92075dcfae4778beb1314d28f63984b
Benjamin Sparks <b.sparks@alugha.com>
2025-10-24 22:05:00 +0200

    pods: fix CVE-2025-62518
    Patch has to be vendored, as upstream has since bumped dependencies (not including tokio-tar)

--- pkgs/by-name/po/pods/package.nix
+++ pkgs/by-name/po/pods/package.nix
@@ -2,6 +2,8 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  applyPatches,
+  fetchpatch2,
   cargo,
   desktop-file-utils,
   glib,
@@ -23,16 +25,24 @@ stdenv.mkDerivation rec {
   pname = "pods";
   version = "2.2.0";
 
-  src = fetchFromGitHub {
-    owner = "marhkb";
-    repo = "pods";
-    tag = "v${version}";
-    hash = "sha256-m+0XjxY0nDAJbVX3r/Jfg+G+RU8Q51e0ZXxkdH69SiQ=";
+  src = applyPatches {
+    name = "pods-patched";
+    src = fetchFromGitHub {
+      owner = "marhkb";
+      repo = "pods";
+      tag = "v${version}";
+      hash = "sha256-m+0XjxY0nDAJbVX3r/Jfg+G+RU8Q51e0ZXxkdH69SiQ=";
+    };
+
+    # Based on upstream PR: https://github.com/marhkb/pods/pull/895
+    # which cannot be merged into 2.2.0 because dependencies were bumped since its release.
+    # Hopefully 2.2.1 will be released soon
+    patches = [ ./cve-2025-62516.patch ];
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit pname version src;
-    hash = "sha256-ssLiGYBz8fh0oci/pVaeEEIruA691tdEOUyEFssNUXU=";
+    hash = "sha256-GBWaGCNXYCiT/favrIYB30VGMMoQQk1iUh4GTNPerK8=";
   };
 
   nativeBuildInputs = [
