359be3ca984e58e9eda3a5e4dd78aa1bd9a686d5
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-25 12:40:30 +0000

    netlify-cli: 23.9.2 -> 23.9.5

--- pkgs/by-name/ne/netlify-cli/package.nix
+++ pkgs/by-name/ne/netlify-cli/package.nix
@@ -11,13 +11,13 @@
 
 buildNpmPackage rec {
   pname = "netlify-cli";
-  version = "23.9.2";
+  version = "23.9.5";
 
   src = fetchFromGitHub {
     owner = "netlify";
     repo = "cli";
     tag = "v${version}";
-    hash = "sha256-rjxm/TrKsvYCKwoHkZRZXFpFTfLd0s0D/H6p5Bull0E=";
+    hash = "sha256-gDLhgINm2XPVMy64/N4lSDh/pRmQdJtbRZ1wn0bXZ7c=";
   };
 
   # Prevent postinstall script from running before package is built
@@ -27,7 +27,7 @@ buildNpmPackage rec {
     touch .git
   '';
 
-  npmDepsHash = "sha256-itzEmCOBXxspGiwxt8t6di7/EuCo2Qkl5JVSkMfUemI=";
+  npmDepsHash = "sha256-8DawqSE7fVCEU9g6KBUNhKrSsQexfIH/E/cwjqMGM5k=";
 
   inherit nodejs;
 
