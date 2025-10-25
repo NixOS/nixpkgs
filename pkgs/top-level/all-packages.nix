20d643d500cc2aa2ab11eb7adb760f01edc63cd1
Nicola <73220426+Zerodya@users.noreply.github.com>
2025-10-25 12:33:25 +0200

    hyprfreeze: remove entry from all-packages.nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2052,10 +2052,6 @@ with pkgs;
     stdenv = gcc15Stdenv;
   };
 
-  hyprfreeze = callPackage ../by-name/hy/hyprfreeze/package.nix {
-    stdenv = gcc15Stdenv;
-  };
-
   hyprgraphics = callPackage ../by-name/hy/hyprgraphics/package.nix {
     stdenv = gcc15Stdenv;
   };
