07dbd17ee4cfab0f671c0aac791aada0436e495d
Nikolay Korotkiy <sikmir@disroot.org>
2025-10-25 18:24:55 +0400

    libhv: modernize

--- pkgs/by-name/li/libhv/package.nix
+++ pkgs/by-name/li/libhv/package.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "ithewei";
     repo = "libhv";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-YIWXdAZsWeSdtPtBaf/t9t68dFKw2nY0bvgMrzCEE5U=";
   };
 
@@ -26,19 +26,19 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   cmakeFlags = [
-    "-DENABLE_UDS=ON"
-    "-DWITH_MQTT=ON"
-    "-DWITH_CURL=ON"
-    "-DWITH_NGHTTP2=ON"
-    "-DWITH_OPENSSL=ON"
-    "-DWITH_KCP=ON"
+    (lib.cmakeBool "ENABLE_UDS" true)
+    (lib.cmakeBool "WITH_MQTT" true)
+    (lib.cmakeBool "WITH_CURL" true)
+    (lib.cmakeBool "WITH_NGHTTP2" true)
+    (lib.cmakeBool "WITH_OPENSSL" true)
+    (lib.cmakeBool "WITH_KCP" true)
   ];
 
-  meta = with lib; {
+  meta = {
     description = "C/c++ network library for developing TCP/UDP/SSL/HTTP/WebSocket/MQTT client/server";
     homepage = "https://github.com/ithewei/libhv";
-    license = licenses.bsd3;
-    maintainers = with maintainers; [ sikmir ];
-    platforms = platforms.unix;
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ sikmir ];
+    platforms = lib.platforms.unix;
   };
 })
