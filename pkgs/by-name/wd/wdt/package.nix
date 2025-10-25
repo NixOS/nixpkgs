60ea37771541df0383159b5c6f59cdc14934b02d
Rafael Ieda <git@ieda.me>
2025-10-25 08:47:39 -0300

    wdt: fix build with cmake4

--- pkgs/by-name/wd/wdt/package.nix
+++ pkgs/by-name/wd/wdt/package.nix
@@ -51,6 +51,11 @@ stdenv.mkDerivation {
     };
   };
 
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace-fail "cmake_minimum_required(VERSION 3.2)" "cmake_minimum_required(VERSION 3.10)"
+  '';
+
   meta = with lib; {
     description = "Warp speed Data Transfer";
     homepage = "https://github.com/facebook/wdt";
