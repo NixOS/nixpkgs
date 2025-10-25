c06524d067899cf0c9aecbb5f4fcb9aa18dd6135
Yueh-Shun Li <shamrocklee@posteo.net>
2025-10-25 21:10:18 +0800

    lib.customisation.callPackageWith: support optional f.argSelectors

--- lib/customisation.nix
+++ lib/customisation.nix
@@ -261,9 +261,14 @@ rec {
       f = if isFunction fn then fn else import fn;
       fargs = functionArgs f;
 
+      argSelectors = f.argSelectors or { };
+      selectedArgs = mapAttrs (_: select: select autoArgs) argSelectors;
+
+      customArgs = selectedArgs // args;
+
       # All arguments that will be passed to the function
       # This includes automatic ones and ones passed explicitly
-      allArgs = intersectAttrs fargs autoArgs // args;
+      allArgs = intersectAttrs fargs autoArgs // customArgs;
 
       # a list of argument names that the function requires, but
       # wouldn't be passed to it
@@ -278,7 +283,7 @@ rec {
       # Get a list of suggested argument names for a given missing one
       getSuggestions =
         arg:
-        pipe (autoArgs // args) [
+        pipe (autoArgs // customArgs) [
           attrNames
           # Only use ones that are at most 2 edits away. While mork would work,
           # levenshteinAtMost is only fast for 2 or less.
@@ -312,9 +317,20 @@ rec {
       # Only show the error for the first missing argument
       error = errorForArg (head (attrNames missingArgs));
 
+      result = makeOverridable f allArgs;
     in
     if missingArgs == { } then
-      makeOverridable f allArgs
+      if lib.isAttrs result then
+        result
+        // {
+          override =
+            result.override
+            // optionalAttrs (args == { }) {
+              inherit argSelectors;
+            };
+        }
+      else
+        result
     # This needs to be an abort so it can't be caught with `builtins.tryEval`,
     # which is used by nix-env and ofborg to filter out packages that don't evaluate.
     # This way we're forced to fix such errors in Nixpkgs,
