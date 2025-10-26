20c5a6a12746e4b89c3317e54f3fa1fe75d109cc
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-26 18:21:45 +0000

    python3Packages.manimpango: 0.6.0 -> 0.6.1

--- pkgs/development/python-modules/manimpango/default.nix
+++ pkgs/development/python-modules/manimpango/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "manimpango";
-  version = "0.6.0";
+  version = "0.6.1";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "ManimCommunity";
     repo = "manimpango";
     tag = "v${version}";
-    hash = "sha256-nN+XOnki8fG7URMy2Fhs2X+yNi8Y7wDo53d61xaRa3w=";
+    hash = "sha256-8eQmhVsW440/zxOwjngnPTGT7iFbdSvBV7tnI332piE=";
   };
 
   postPatch = ''
