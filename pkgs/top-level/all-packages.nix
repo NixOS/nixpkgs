d3e61ea9b3c376157661d623154d3448bcd977e4
Wolfgang Walther <walther@technowledgy.de>
2025-10-26 20:15:48 +0100

    various: drop darwin workarounds for infrec
    I can't cause infinite recursion by dropping these workarounds, so I
    conclude they're not necessary anymore.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7133,7 +7133,7 @@ with pkgs;
       null
     else
       callPackage ../development/libraries/ncurses {
-        # ncurses is included in the SDK. Avoid an infinite recursion by using a bootstrap stdenv.
+        # ncurses is included in the SDK. Avoid an infinite recursion with `pkgsStatic` by using a bootstrap stdenv.
         stdenv = if stdenv.hostPlatform.isDarwin then darwin.bootstrapStdenv else stdenv;
       };
 
@@ -7749,12 +7749,7 @@ with pkgs;
 
   xgboostWithCuda = xgboost.override { cudaSupport = true; };
 
-  zlib = callPackage ../development/libraries/zlib {
-    stdenv =
-      # zlib is a dependency of xcbuild. Avoid an infinite recursion by using a bootstrap stdenv
-      # that does not propagate xcrun.
-      if stdenv.hostPlatform.isDarwin then darwin.bootstrapStdenv else stdenv;
-  };
+  zlib = callPackage ../development/libraries/zlib { };
 
   inherit
     (rec {
