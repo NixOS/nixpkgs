45bccd7ab5f9c374d2a049965c72938e3125d5b8
Reylak <ANGULO18112008@gmail.com>
2025-10-26 19:48:09 -0300

    packaged sglauncher

---
+++ pkgs/by-name/sg/sglauncher/package.nix
@@ -0,0 +1,40 @@
+{
+  stdenv,
+  fetchFromGitea,
+  lib,
+  pkgs,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "sglauncher";
+  version = "1.1";
+
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "ItsZariep";
+    repo = "SGLauncher";
+    rev = finalAttrs.version;
+    hash = "sha256-zIhutvtNA0UJ8+nMWv2t6hMpO9wkYI2sjSNT78ggn58=";
+  };
+
+  nativeBuildInputs = [ pkgs.pkg-config ];
+  buildInputs = [ pkgs.gtk3 ];
+
+  buildPhase = ''
+    cd src
+    ${pkgs.gnumake}/bin/make sglauncher SHELL=${pkgs.bash}/bin/bash
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp sglauncher $out/bin
+  '';
+
+  meta = {
+    description = "Simple GTK Launcher";
+    homepage = "https://codeberg.org/ItsZariep/SGLauncher";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ reylak ];
+    mainProgram = "sglauncher";
+  };
+})
