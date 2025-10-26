d050b351c42492bfafdec2b65edb1a36d3a908c2
h7x4 <h7x4@nani.wtf>
2025-10-27 02:20:24 +0900

    oidentd: add meta.changelog

--- pkgs/by-name/oi/oidentd/package.nix
+++ pkgs/by-name/oi/oidentd/package.nix
@@ -23,6 +23,7 @@ stdenv.mkDerivation rec {
     description = "Configurable Ident protocol server";
     mainProgram = "oidentd";
     homepage = "https://oidentd.janikrabe.com/";
+    changelog = "https://github.com/janikrabe/oidentd/blob/${version}/NEWS";
     license = lib.licenses.gpl2Only;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ h7x4 ];
