6e1120075914a7438ee663b70e96bdaff581c989
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-10-26 16:23:05 +0200

    koboldcpp: 1.99.4 -> 1.100.1
    Changelog: https://github.com/LostRuins/koboldcpp/releases/tag/v1.100.1
    Diff: https://github.com/LostRuins/koboldcpp/compare/v1.99.4...v1.100.1

--- pkgs/by-name/ko/koboldcpp/package.nix
+++ pkgs/by-name/ko/koboldcpp/package.nix
@@ -41,13 +41,13 @@ let
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "koboldcpp";
-  version = "1.99.4";
+  version = "1.100.1";
 
   src = fetchFromGitHub {
     owner = "LostRuins";
     repo = "koboldcpp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ilBrTMtY6bhns2GcwDckGq4+RqzgzBCg0HJJ4QUx8Co=";
+    hash = "sha256-Z3yPsC7WDFHwXVInnu96ZVWAjn6XyN0aVviwGkvoe1A=";
   };
 
   enableParallelBuilding = true;
@@ -96,7 +96,7 @@ effectiveStdenv.mkDerivation (finalAttrs: {
 
     install -Dm755 koboldcpp.py "$out/bin/koboldcpp.unwrapped"
     cp *.so "$out/bin"
-    cp *.embd "$out/bin"
+    cp embd_res/*.embd "$out/bin"
 
     ${lib.optionalString metalSupport ''
       cp *.metal "$out/bin"
