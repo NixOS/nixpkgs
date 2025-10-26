555919a8efc3c297cbb11d217be0c126f28409ad
Wolfgang Walther <walther@technowledgy.de>
2025-10-26 20:15:48 +0100

    various: drop darwin workarounds for infrec
    I can't cause infinite recursion by dropping these workarounds, so I
    conclude they're not necessary anymore.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6913,12 +6913,6 @@ with pkgs;
 
   watson-ruby = callPackage ../development/tools/misc/watson-ruby { };
 
-  xcbuild = callPackage ../by-name/xc/xcbuild/package.nix {
-    stdenv =
-      # xcbuild is included in the SDK. Avoid an infinite recursion by using a bootstrap stdenv.
-      if stdenv.hostPlatform.isDarwin then darwin.bootstrapStdenv else stdenv;
-  };
-
   xcbuildHook = makeSetupHook {
     name = "xcbuild-hook";
     propagatedBuildInputs = [ xcbuild ];
@@ -7898,12 +7892,7 @@ with pkgs;
   libpeas = callPackage ../development/libraries/libpeas { };
   libpeas2 = callPackage ../development/libraries/libpeas/2.x.nix { };
 
-  libpng = callPackage ../development/libraries/libpng {
-    stdenv =
-      # libpng is a dependency of xcbuild. Avoid an infinite recursion by using a bootstrap stdenv
-      # that does not propagate xcrun.
-      if stdenv.hostPlatform.isDarwin then darwin.bootstrapStdenv else stdenv;
-  };
+  libpng = callPackage ../development/libraries/libpng { };
 
   libpng12 = callPackage ../development/libraries/libpng/12.nix { };
 
@@ -8147,7 +8136,7 @@ with pkgs;
       null
     else
       callPackage ../development/libraries/ncurses {
-        # ncurses is included in the SDK. Avoid an infinite recursion by using a bootstrap stdenv.
+        # ncurses is included in the SDK. Avoid an infinite recursion with `pkgsStatic` by using a bootstrap stdenv.
         stdenv = if stdenv.hostPlatform.isDarwin then darwin.bootstrapStdenv else stdenv;
       };
 
@@ -8776,12 +8765,7 @@ with pkgs;
 
   xgboostWithCuda = xgboost.override { cudaSupport = true; };
 
-  zlib = callPackage ../development/libraries/zlib {
-    stdenv =
-      # zlib is a dependency of xcbuild. Avoid an infinite recursion by using a bootstrap stdenv
-      # that does not propagate xcrun.
-      if stdenv.hostPlatform.isDarwin then darwin.bootstrapStdenv else stdenv;
-  };
+  zlib = callPackage ../development/libraries/zlib { };
 
   inherit
     (rec {
