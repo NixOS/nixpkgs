72d7969e7b4d945093b1008e957278ac7834de06
Gaetan Lepage <gaetan@glepage.com>
2025-10-26 12:26:36 +0000

    python3Packages.triton: 3.4.0 -> 3.5.0
    Diff: https://github.com/triton-lang/triton/compare/v3.4.0...v3.5.0

--- pkgs/by-name/tr/triton-llvm/package.nix
+++ pkgs/by-name/tr/triton-llvm/package.nix
@@ -64,7 +64,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "triton-llvm";
-  version = "21.0.0-unstable-2025-06-10"; # See https://github.com/llvm/llvm-project/blob/main/cmake/Modules/LLVMVersion.cmake
+  version = "22.0.0-unstable-2025-07-15"; # See https://github.com/llvm/llvm-project/blob/main/cmake/Modules/LLVMVersion.cmake
 
   outputs = [
     "out"
@@ -80,8 +80,8 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "llvm";
     repo = "llvm-project";
-    rev = "8957e64a20fc7f4277565c6cfe3e555c119783ce";
-    hash = "sha256-ljdwHPLGZv72RBPBg5rs7pZczsB+WJhdCeHJxoi4gJQ=";
+    rev = "7d5de3033187c8a3bb4d2e322f5462cdaf49808f";
+    hash = "sha256-ayW6sOZGvP3SBjfmpXvYQJrPOAElY0MEHPFvj2fq+bM=";
   };
 
   nativeBuildInputs = [
