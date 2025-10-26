43ffb7cf63e961d3525637a57106b47a62e2ac4e
kyehn <kyehn@users.noreply.github.com>
2025-10-26 10:42:50 +0800

    qjackctl: 0.9.91 -> 1.0.4
    Diff: https://github.com/rncbc/qjackctl/compare/v0.9.91...v1.0.4

--- pkgs/by-name/qj/qjackctl/package.nix
+++ pkgs/by-name/qj/qjackctl/package.nix
@@ -7,28 +7,25 @@
   alsa-lib,
   libjack2,
   dbus,
-  libsForQt5,
+  qt6,
   # Enable jack session support
   jackSession ? false,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
-  version = "0.9.91";
+  version = "1.0.4";
   pname = "qjackctl";
-
-  # some dependencies such as killall have to be installed additionally
 
   src = fetchFromGitHub {
     owner = "rncbc";
     repo = "qjackctl";
-    tag = "qjackctl_${lib.replaceStrings [ "." ] [ "_" ] finalAttrs.version}";
-    hash = "sha256-YfdRyylU/ktFvsh18FjpnG9MkV1HxHJBhRnHWQ7I+hY=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-eZ3PBacRdMJCHHwE0qYi4jgSb7G7uS2Q+j02EdnSYqA=";
   };
 
   buildInputs = [
-    libsForQt5.qtbase
-    libsForQt5.qtx11extras
-    libsForQt5.qttools
+    qt6.qtbase
+    qt6.qttools
     alsa-lib
     libjack2
     dbus
@@ -37,12 +34,12 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [
     cmake
     pkg-config
-    libsForQt5.wrapQtAppsHook
+    qt6.wrapQtAppsHook
   ];
 
   cmakeFlags = [
-    "-DCONFIG_JACK_VERSION=1"
-    "-DCONFIG_JACK_SESSION=${toString jackSession}"
+    (lib.cmakeFeature "CONFIG_JACK_VERSION" "1")
+    (lib.cmakeFeature "CONFIG_JACK_SESSION" (toString jackSession))
   ];
 
   meta = {
