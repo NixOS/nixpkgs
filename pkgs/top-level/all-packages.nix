ee21b31ef7a1c4cc5b792646df7bae001c749a05
qzylinra <qzylinra@users.noreply.github.com>
2025-10-27 07:54:25 +0800

    flutter327: drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4505,7 +4505,6 @@ with pkgs;
   flutter335 = flutterPackages.v3_35;
   flutter332 = flutterPackages.v3_32;
   flutter329 = flutterPackages.v3_29;
-  flutter327 = flutterPackages.v3_27;
 
   fpc = callPackage ../development/compilers/fpc { };
 
