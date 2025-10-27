f9fa267a304820e633f4261d498f7eefc235aa71
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-27 09:27:51 +0000

    riffdiff: 3.5.0 -> 3.5.1

--- pkgs/by-name/ri/riffdiff/package.nix
+++ pkgs/by-name/ri/riffdiff/package.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "riffdiff";
-  version = "3.5.0";
+  version = "3.5.1";
 
   src = fetchFromGitHub {
     owner = "walles";
     repo = "riff";
     tag = version;
-    hash = "sha256-qA20sLiGqDtIPWBNww+WXM5AG162RPTdkUPoJ0PLiYY=";
+    hash = "sha256-SRr4yFv6fulBN/HNM3uCVLXS6pcspi5X5hXvQJg1sDI=";
   };
 
-  cargoHash = "sha256-omwKOstRXIAUDgLUFqmtxu77JJzAOASzbjLEImad1cE=";
+  cargoHash = "sha256-86nBxitdA8deJHRQqLM/JWcpSX/u6C4cofJAbYj5Ijs=";
 
   passthru = {
     tests.version = testers.testVersion { package = riffdiff; };
