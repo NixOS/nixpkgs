NixOS/nixpkgs

commit adc035fad24bb95976859816f69dc3d2f239bfd6

beets: 2.4.0 -> 2.5.0

pkgs/tools/audio/beets/builtin-plugins.nix
@@ -180,4 +180,5 @@
   _typing = {
     testPaths = [ ];
   };
+  _utils = { };
 }

pkgs/tools/audio/beets/common.nix
@@ -88,6 +88,7 @@ python3Packages.buildPythonApplication {
 
   build-system = [
     python3Packages.poetry-core
+    python3Packages.poetry-dynamic-versioning
   ];
 
   dependencies =

pkgs/tools/audio/beets/default.nix
@@ -31,12 +31,12 @@ lib.makeExtensible (
 
     beets-stable = callPackage ./common.nix rec {
       inherit python3Packages extraPatches;
-      version = "2.4.0";
+      version = "2.5.0";
       src = fetchFromGitHub {
         owner = "beetbox";
         repo = "beets";
         tag = "v${version}";
-        hash = "sha256-BM4NW8iukw9+zLD2cfAETmFYNAiNPUn9cLrkVlCE+jM=";
+        hash = "sha256-YZvS9oB+v+48i1avQcs6ClnYz4aMqJQ2e6cBiZ4ULb0=";
       };
     };
 

commit f23271dffb78251b84896b80cff5218492280b89

beets: fix broken build
By default, the expression looks for the test path `test__utils.py` which doesn't exist. This broke with adc035fad24bb95976859816f69dc3d2f239bfd6

pkgs/tools/audio/beets/builtin-plugins.nix
@@ -180,5 +180,7 @@
   _typing = {
     testPaths = [ ];
   };
-  _utils = { };
+  _utils = {
+    testPaths = [ "test/plugins/utils" ];
+  };
 }