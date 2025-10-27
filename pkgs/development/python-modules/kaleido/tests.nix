d711a0890c0a28c27274c674c82f48c6851998b2
Rebecca Turner <rbt@sent.as>
2025-10-27 11:24:38 -0700

    python3Packages.kaleido: enable tests on darwin
    These seem to pass locally, deprecation warnings notwithstanding.
    
    I removed the `engine=` argument, because that printed a separate
    deprecation warning saying no engines other than `kaleido` are supported
    and the argument itself will be removed.
    
    Related:
    - #456267
    - #455432

--- pkgs/development/python-modules/kaleido/tests.nix
+++ pkgs/development/python-modules/kaleido/tests.nix
@@ -1,15 +1,19 @@
 {
   runCommand,
   python,
-  plotly,
-  pandas,
   kaleido,
 }:
 
+# NOTE: This prints warnings:
+#     /nix/store/...-tests.py:11: DeprecationWarning:
+#     Support for Kaleido versions less than 1.0.0 is deprecated and will be removed after September 2025.
+#     Please upgrade Kaleido to version 1.0.0 or greater (`pip install 'kaleido>=1.0.0'` or `pip install 'plotly[kaleido]'`).
 runCommand "${kaleido.pname}-tests" {
   nativeBuildInputs = [
-    python
-    plotly
-    pandas
+    (python.withPackages (pyPkgs: [
+      pyPkgs.plotly
+      pyPkgs.pandas
+      pyPkgs.kaleido
+    ]))
   ];
 } "python3 ${./tests.py}"
