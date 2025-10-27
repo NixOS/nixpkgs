dc6888a3394cdb34c82ed9486d35088e05b0ec14
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-27 16:41:46 +0000

    oklch-color-picker: 2.2.1 -> 2.3.0

--- pkgs/by-name/ok/oklch-color-picker/package.nix
+++ pkgs/by-name/ok/oklch-color-picker/package.nix
@@ -13,16 +13,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "oklch-color-picker";
-  version = "2.2.1";
+  version = "2.3.0";
 
   src = fetchFromGitHub {
     owner = "eero-lehtinen";
     repo = "oklch-color-picker";
     tag = "${finalAttrs.version}";
-    hash = "sha256-tPYxcZghGR1YZl1bwoDDIBmbTVGuksCpfgLYwG+k4Ws=";
+    hash = "sha256-dbsE1mYt/GhpkWIBS6MejiKKKz1B+h/3wqGKHCp0p2Q=";
   };
 
-  cargoHash = "sha256-tdIkvBYKfcbCYXhDbIwXNNbNb4X32uBwDh3mAyqt/IM=";
+  cargoHash = "sha256-R1nQAm1rw2hkhxeyzgXCTLF+gXdH1tr6DCv/L8b9D+s=";
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
 
