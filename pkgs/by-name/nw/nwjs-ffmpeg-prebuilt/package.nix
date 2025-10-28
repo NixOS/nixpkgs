4047e4fea5ebea4d93092c627533dab0ef1557f7
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-28 05:25:29 +0000

    nwjs-ffmpeg-prebuilt: 0.104.1 -> 0.105.0

--- pkgs/by-name/nw/nwjs-ffmpeg-prebuilt/package.nix
+++ pkgs/by-name/nw/nwjs-ffmpeg-prebuilt/package.nix
@@ -7,7 +7,7 @@
 
 let
   bits = if stdenv.hostPlatform.is64bit then "x64" else "ia32";
-  version = "0.104.1";
+  version = "0.105.0";
 in
 stdenv.mkDerivation {
   pname = "nwjs-ffmpeg-prebuilt";
@@ -16,8 +16,8 @@ stdenv.mkDerivation {
   src =
     let
       hashes = {
-        "x64" = "sha256-01hdumuWRAi1KdvwBDQJqUSk+NV5gYnn6QWQYj2FDUE=";
-        "ia32" = "sha256-01hdumuWRAi1KdvwBDQJqUSk+NV5gYnn6QWQYj2FDUE=";
+        "x64" = "sha256-NE0CFgGZm5QJoF7Zv+QFXdSx3lqbS9WN84B4FXFFIys=";
+        "ia32" = "sha256-NE0CFgGZm5QJoF7Zv+QFXdSx3lqbS9WN84B4FXFFIys=";
       };
     in
     fetchurl {
