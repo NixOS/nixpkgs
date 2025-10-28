d0478d83d5bc5b72c3276fea8117154cd66df991
Acid Bong <acidbong@tilde.club>
2025-10-28 18:53:39 +0200

    xterm: make `meta.changelog` point to a specific changelog section

--- pkgs/by-name/xt/xterm/package.nix
+++ pkgs/by-name/xt/xterm/package.nix
@@ -122,7 +122,7 @@ stdenv.mkDerivation (finalAttrs: {
     homepage = "https://invisible-island.net/xterm";
     license = with lib.licenses; [ mit ];
     platforms = with lib.platforms; linux ++ darwin;
-    changelog = "https://invisible-island.net/xterm/xterm.log.html";
+    changelog = "https://invisible-island.net/xterm/xterm.log.html#xterm_${finalAttrs.version}";
     mainProgram = "xterm";
   };
 })
