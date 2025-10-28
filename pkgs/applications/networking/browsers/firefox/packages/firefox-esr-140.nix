0345f0a8ba8756181abc6e81640360393e2565aa
7c6f434c <7c6f434c@mail.ru>
2025-10-28 09:05:12 +0100

    firefox-esr: mark as compatible with unsigned extensions
    This matches both the upstream documentation and experimental observations

--- pkgs/applications/networking/browsers/firefox/packages/firefox-esr-140.nix
+++ pkgs/applications/networking/browsers/firefox/packages/firefox-esr-140.nix
@@ -11,6 +11,7 @@ buildMozillaMach rec {
   pname = "firefox";
   version = "140.4.0esr";
   applicationName = "Firefox ESR";
+  requireSigning = false;
   src = fetchurl {
     url = "mirror://mozilla/firefox/releases/${version}/source/firefox-${version}.source.tar.xz";
     sha512 = "cfce0bdcf6d4599c7b96bccd9fd1390bfb3645db9276a369a30760ce6819850aaa4251869e6bd3c5d8582ea3728b920762c5f16f7ce12ce151c3e74327b8c811";
