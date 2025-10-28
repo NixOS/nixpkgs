fa6a2b673c5a94392e5a4dad0f12782822849836
kyehn <kyehn@users.noreply.github.com>
2025-10-28 08:02:18 +0800

    python312Packages.aistudio-sdk: init at 0.3.8

---
+++ pkgs/development/python-modules/aistudio-sdk/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  bce-python-sdk,
+  click,
+  prettytable,
+  psutil,
+  requests,
+  tqdm,
+}:
+
+let
+  version = "0.3.8";
+
+  format = "wheel";
+in
+buildPythonPackage {
+  pname = "aistudio-sdk";
+  inherit version format;
+
+  # No source code dist available
+  src = fetchPypi {
+    pname = "aistudio_sdk";
+    inherit version format;
+    dist = "py3";
+    python = "py3";
+    hash = "sha256-v8lq9yQ6wu4zAwFISapAKHF8zlr6Yir4z+Oh1E0ZQdY=";
+  };
+
+  dependencies = [
+    bce-python-sdk
+    click
+    prettytable
+    psutil
+    requests
+    tqdm
+  ];
+
+  pythonImportsCheck = [ "aistudio_sdk" ];
+
+  meta = {
+    description = "Python client library for the AIStudio API";
+    homepage = "https://pypi.org/project/aistudio-sdk";
+    license = lib.licenses.unfree;
+    mainProgram = "aistudio";
+    maintainers = with lib.maintainers; [ kyehn ];
+    sourceProvenance = with lib.sourceTypes; [ fromSource ];
+  };
+}
