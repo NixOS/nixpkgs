6c2997a6ee63ab00d1192ee8de59e71fb2d52f2b
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-28 23:05:37 +0000

    python3Packages.garth: 0.5.17 -> 0.5.18

--- pkgs/development/python-modules/garth/default.nix
+++ pkgs/development/python-modules/garth/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "garth";
-  version = "0.5.17";
+  version = "0.5.18";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-SO3pOMOLL9cHd+VccCVTh3XZPBBH9DzHxEgfKgSxCcs=";
+    hash = "sha256-+Df40ZYj5+Vo9IFFF+9wfGiKvhCWWy7hqhoBVZKsdx0=";
   };
 
   pythonRelaxDeps = [ "requests-oauthlib" ];
