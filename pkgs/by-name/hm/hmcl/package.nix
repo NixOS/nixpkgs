cbeb10eaf89ee1c4f7a6fc7015cc7bdaefc0b55f
Moraxyc <i@qaq.li>
2025-10-29 00:17:21 +0800

    hmcl: build from source; refactor; 3.6.18 -> 3.7.3

--- pkgs/by-name/hm/hmcl/package.nix
+++ pkgs/by-name/hm/hmcl/package.nix
@@ -1,51 +1,93 @@
 {
   lib,
   stdenv,
+
   fetchurl,
+  fetchFromGitHub,
+
+  gradle,
+  unzip,
+  replaceVars,
   makeDesktopItem,
   wrapGAppsHook3,
   copyDesktopItems,
+  stripJavaArchivesHook,
+  desktopToDarwinBundle,
   imagemagick,
+  callPackage,
+
   jdk,
   jdk17,
   jdk21,
-  hmclJdk ? jdk,
-  minecraftJdks ? [
-    jdk
-    jdk17
-    jdk21
-  ],
   xorg,
   glib,
   libGL,
-  glfw,
+  libusb1,
+  udev,
+  flite,
+  glfw3-minecraft,
   openal,
   libglvnd,
   alsa-lib,
   wayland,
   vulkan-loader,
   libpulseaudio,
+  terracotta,
   gobject-introspection,
-  callPackage,
+  zenity,
+
+  hmclJdk ? jdk,
+  minecraftJdks ? [
+    jdk
+    jdk17
+    jdk21
+  ],
+
+  extraLibs ? [ ],
+  extraPrograms ? [ ],
+  controllerSupport ? stdenv.hostPlatform.isLinux,
+  textToSpeechSupport ? stdenv.hostPlatform.isLinux,
 }:
 
+# Copied from prismlauncher
+assert lib.assertMsg (
+  controllerSupport -> stdenv.hostPlatform.isLinux
+) "controllerSupport only has an effect on Linux.";
+
+assert lib.assertMsg (
+  textToSpeechSupport -> stdenv.hostPlatform.isLinux
+) "textToSpeechSupport only has an effect on Linux.";
+
+let
+  hmclJdk' = hmclJdk.override { enableJavaFX = true; };
+  # Reduce closure size
+  minecraftJdks' = lib.lists.remove hmclJdk minecraftJdks ++ [ hmclJdk' ];
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "hmcl";
-  version = "3.6.18";
+  version = "3.7.3";
 
-  src = fetchurl {
-    # HMCL has built-in keys, such as the Microsoft OAuth secret and the CurseForge API key.
-    # See https://github.com/HMCL-dev/HMCL/blob/refs/tags/release-3.6.12/.github/workflows/gradle.yml#L26-L28
-    url = "https://github.com/HMCL-dev/HMCL/releases/download/release-${finalAttrs.version}/HMCL-${finalAttrs.version}.jar";
-    hash = "sha256-x8UcHdBYXdnTabJh2hxsknYipYNBJW2vKxJKHhryMLQ=";
+  src = fetchFromGitHub {
+    owner = "HMCL-dev";
+    repo = "HMCL";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-3YeVMaMKgyndnxnKjPaP0M0xdMpqc2L809t1v4mhmKM=";
   };
 
-  icon = fetchurl {
-    url = "https://github.com/HMCL-dev/HMCL/raw/release-${finalAttrs.version}/HMCL/src/main/resources/assets/img/icon@8x.png";
-    hash = "sha256-1OVq4ujA2ZHboB7zEk7004kYgl9YcoM4qLq154MZMGo=";
-  };
+  patches = [
+    # For deterministic
+    ./0001-nix-do-not-output-timestamp.patch
+    # Do not download binary file
+    (replaceVars ./0002-nix-use-terracotta-from-nix.patch { TERRACOTTA_BIN = lib.getExe terracotta; })
+  ];
 
-  dontUnpack = true;
+  postPatch = ''
+    substituteInPlace HMCL/build.gradle.kts \
+      --replace-fail 'dependsOn(createPropertiesFile)' "" \
+      --replace-fail '$versionRoot.SNAPSHOT' '${finalAttrs.version}'
+    # Credentials
+    unzip -j ${finalAttrs.passthru.jar} assets/hmcl.properties -d HMCL/build/
+  '';
 
   dontWrapGApps = true;
 
@@ -61,23 +103,45 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   nativeBuildInputs = [
-    gobject-introspection
-    wrapGAppsHook3
     copyDesktopItems
+    gobject-introspection
+    gradle
     imagemagick
-  ];
+    stripJavaArchivesHook
+    unzip
+    wrapGAppsHook3
+  ]
+  ++ lib.optional stdenv.hostPlatform.isDarwin desktopToDarwinBundle;
+
+  mitmCache = gradle.fetchDeps {
+    inherit (finalAttrs) pname;
+    data = ./deps.json;
+  };
+
+  __darwinAllowLocalNetworking = true;
+
+  # Use native JavaFX for platforms compatibility
+  gradleFlags = [ "-Dorg.gradle.java.home=${hmclJdk'.home}" ];
+
+  gradleUpdateScript = ''
+    runHook preBuild
+
+    gradle help --write-verification-metadata sha256
+  '';
+
+  gradleBuildTask = "shadowJar";
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/{bin,lib/hmcl}
-    cp $src $out/lib/hmcl/hmcl.jar
+    cp HMCL/build/libs/HMCL-${finalAttrs.version}.jar $out/lib/hmcl/hmcl.jar
 
     for n in 16 32 48 64 96 128 256
     do
       size=$n"x"$n
       mkdir -p $out/share/icons/hicolor/$size/apps
-      magick ${finalAttrs.icon} -resize $size $out/share/icons/hicolor/$size/apps/hmcl.png
+      magick HMCL/src/main/resources/assets/img/icon@8x.png -resize $size $out/share/icons/hicolor/$size/apps/hmcl.png
     done
 
     runHook postInstall
@@ -85,53 +149,78 @@ stdenv.mkDerivation (finalAttrs: {
 
   fixupPhase =
     let
-      libpath = lib.makeLibraryPath (
-        [
-          libGL
-          glfw
-          glib
-          openal
-          libglvnd
-          vulkan-loader
-        ]
-        ++ lib.optionals stdenv.hostPlatform.isLinux [
-          xorg.libX11
-          xorg.libXxf86vm
-          xorg.libXext
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXtst
-          libpulseaudio
-          wayland
-          alsa-lib
-        ]
-      );
+      runtimeLibs = [
+        # When use native libs
+        openal
+        glfw3-minecraft
+
+        libGL
+        glib
+        libglvnd
+        vulkan-loader
+      ]
+      ++ lib.optionals stdenv.hostPlatform.isLinux [
+        # glfw
+        xorg.libX11
+        xorg.libXxf86vm
+        xorg.libXext
+        xorg.libXcursor
+        xorg.libXrandr
+        xorg.libXtst
+
+        # oshi
+        udev
+
+        ## openal
+        alsa-lib
+        libpulseaudio
+
+        wayland
+      ]
+      ++ lib.optional textToSpeechSupport flite
+      ++ lib.optional controllerSupport libusb1
+      ++ extraLibs;
+      runtimePrograms = [
+        # Required by tiny file dialogs
+        zenity
+      ]
+      ++ minecraftJdks'
+      ++ extraPrograms;
     in
     ''
       runHook preFixup
 
-      makeBinaryWrapper ${hmclJdk}/bin/java $out/bin/hmcl \
+      makeBinaryWrapper ${hmclJdk'}/bin/java $out/bin/hmcl \
         --add-flags "-jar $out/lib/hmcl/hmcl.jar" \
-        --set LD_LIBRARY_PATH ${libpath} \
-        --prefix PATH : "${lib.makeBinPath minecraftJdks}"\
+        --set LD_LIBRARY_PATH ${lib.makeLibraryPath runtimeLibs} \
+        --prefix PATH : "${lib.makeBinPath runtimePrograms}"\
         ''${gappsWrapperArgs[@]}
 
       runHook postFixup
     '';
 
-  passthru.updateScript = lib.getExe (callPackage ./update.nix { });
+  passthru = {
+    updateScript = lib.getExe (callPackage ./update.nix { });
+    jar = fetchurl {
+      url = "https://github.com/HMCL-dev/HMCL/releases/download/v${finalAttrs.version}/HMCL-${finalAttrs.version}.jar";
+      hash = "sha256-VE/83KD1xIrkD6BGBK0rJpbKuNPOpmNSC/RHjhRsGco=";
+    };
+  };
 
   meta = {
     homepage = "https://hmcl.huangyuhui.net";
     description = "Minecraft Launcher which is multi-functional, cross-platform and popular";
     changelog = "https://docs.hmcl.net/changelog/stable.html";
     mainProgram = "hmcl";
-    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
+    sourceProvenance = with lib.sourceTypes; [
+      fromSource
+      binaryBytecode # mitm cache
+    ];
     license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [
       daru-san
       moraxyc
     ];
-    inherit (hmclJdk.meta) platforms;
+    inherit (hmclJdk'.meta) platforms;
   };
 })
--- pkgs/by-name/hm/hmcl/update.nix
+++ pkgs/by-name/hm/hmcl/update.nix
@@ -18,23 +18,21 @@ writeShellApplication {
   ];
 
   text = ''
-    # get old info
-    oldVersion=$(nix-instantiate --eval --strict -A "hmcl.version" | jq -e -r)
-
     get_latest_release() {
         curl --fail ''${GITHUB_TOKEN:+ -H "Authorization: bearer $GITHUB_TOKEN"} \
              -s "https://api.github.com/repos/HMCL-dev/HMCL/releases/latest" | jq -r ".tag_name"
     }
 
     version=$(get_latest_release)
-    version="''${version#release-}"
+    version="''${version#v-}"
 
-    if [[ "$oldVersion" == "$version" ]]; then
+    if [[ "$UPDATE_NIX_OLD_VERSION" == "$version" ]]; then
         echo "Already up to date!"
         exit 0
     fi
 
     nix-update hmcl --version="$version"
-    update-source-version hmcl --source-key=icon --ignore-same-version
+    update-source-version hmcl --source-key=jar --ignore-same-version
+    eval "$(nix-build -A hmcl.mitmCache.updateScript)"
   '';
 }
