007c38f4fc89b23bb4f2addfed1175b5278b9d89
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-10-29 16:57:14 +0100

    intel-compute-runtime: 25.35.35096.9 -> 25.40.35563.4
    Diff: https://github.com/intel/compute-runtime/compare/25.35.35096.9...25.40.35563.4
    
    Changelog: https://github.com/intel/compute-runtime/releases/tag/25.40.35563.4

--- pkgs/by-name/in/intel-compute-runtime/package.nix
+++ pkgs/by-name/in/intel-compute-runtime/package.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   pname = "intel-compute-runtime";
-  version = "25.35.35096.9";
+  version = "25.40.35563.4";
 
   src = fetchFromGitHub {
     owner = "intel";
     repo = "compute-runtime";
     tag = version;
-    hash = "sha256-GAFbpf5ZUpq+jpVECa5buauCYdpPBOBrREkgrGyhxPA=";
+    hash = "sha256-V2zmS3CFLxhyFYvGOdkix9g3E6JkeVa/pDLPC5NYivo=";
   };
 
   nativeBuildInputs = [
