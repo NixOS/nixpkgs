b0a318e26de9d8f3b04449cd038d9e95e72d749a
Niklas Korz <niklas@niklaskorz.de>
2025-10-29 17:56:52 +0100

    pixi: 0.58.0 -> 0.59.0
    https://github.com/prefix-dev/pixi/releases/tag/v0.59.0
    
    Fixes CVE-2025-62518
    
    (cherry picked from commit 5141623b342edc8eccbe2f919620538b3cabbde7)

--- pkgs/by-name/pi/pixi/package.nix
+++ pkgs/by-name/pi/pixi/package.nix
@@ -14,16 +14,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "pixi";
-  version = "0.58.0";
+  version = "0.59.0";
 
   src = fetchFromGitHub {
     owner = "prefix-dev";
     repo = "pixi";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+Bhyt01gTNWVOL0WG6pdjzbRqIfm2MUEHnbTGg3nG2k=";
+    hash = "sha256-aDwuMRceMxRCh9w89Q2ktpNWwmStg3KzggwhZUAAauk=";
   };
 
-  cargoHash = "sha256-b7/UiIkeLddo9hUipqd7zLGvFumAjFolf/jODZ0qOQw=";
+  cargoHash = "sha256-BbPX+ZKMfLtKWBnrGBRfrl6d40rBQaWJLBg0Vv5UnZE=";
 
   nativeBuildInputs = [
     pkg-config
