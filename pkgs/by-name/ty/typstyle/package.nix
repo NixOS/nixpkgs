a2b8a0eea00691627247c47c37913a3ad58f5047
Jost Alemann <alemann@posteo.com>
2025-10-29 16:46:25 +0100

    typstyle: 0.13.19 -> 0.14.0
    Changelog: https://github.com/typstyle-rs/typstyle/releases/tag/v0.14.0
    Diff: https://github.com/typstyle-rs/typstyle/compare/v0.13.19...v0.14.0

--- pkgs/by-name/ty/typstyle/package.nix
+++ pkgs/by-name/ty/typstyle/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "typstyle";
-  version = "0.13.19";
+  version = "0.14.0";
 
   src = fetchFromGitHub {
     owner = "typstyle-rs";
     repo = "typstyle";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-8gM20/dGe6VH36C2y0rcMV1aKT7aAxHhy4TigTKbCxM=";
+    hash = "sha256-L6MmXipkpTfSvSzuUmE6/hUqDoLbe3+2/bHgFbTfLpo=";
   };
 
-  cargoHash = "sha256-CIU616FI26WtRceI/vZbaJgOm7532i0uzCzfhMz6plc=";
+  cargoHash = "sha256-xGGuZw87CjS/tA74SiIyMqM6PS0OJt00axiI3jyAAr0=";
 
   # Disabling tests requiring network access
   checkFlags = [
