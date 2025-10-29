aed4ee1a79ef1321c2ba5b1efa34481f8ab0c6c6
Doron Behar <doron.behar@gmail.com>
2025-10-29 17:38:46 +0200

    nixos/mpd: apply RFC 42
    Also use `config.services.mpd.settings` directly in the documentation of
    mpdscribble.

--- nixos/tests/mpd.nix
+++ nixos/tests/mpd.nix
@@ -61,14 +61,16 @@ in
       lib.mkMerge [
         (mkServer {
           mpd = defaultMpdCfg // {
-            bind_to_address = "any";
-            extraConfig = ''
-              audio_output {
-                type "alsa"
-                name "ALSA"
-                mixer_type "null"
-              }
-            '';
+            settings = {
+              bind_to_address = "any";
+              audio_output = [
+                {
+                  type = "alsa";
+                  name = "ALSA";
+                  mixer_type = "null";
+                }
+              ];
+            };
             openFirewall = true;
           };
           musicService = musicService { inherit (defaultMpdCfg) user group dataDir; };
@@ -80,12 +82,12 @@ in
       lib.mkMerge [
         (mkServer {
           mpd = defaultMpdCfg // {
-            extraConfig = ''
-              audio_output {
-                type "pulse"
-                name "The Pulse"
+            settings.audio_output = [
+              {
+                type = "pulse";
+                name = "The Pulse";
               }
-            '';
+            ];
           };
 
           musicService = musicService { inherit (defaultMpdCfg) user group dataDir; };
