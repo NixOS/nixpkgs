482c731218671781e468fba895ea0fb4c508bd10
Doron Behar <doron.behar@gmail.com>
2025-10-30 18:06:33 +0200

    nixos/ddccontrol: enable hardware.i2c and mention its group

--- nixos/modules/services/hardware/ddccontrol.nix
+++ nixos/modules/services/hardware/ddccontrol.nix
@@ -14,7 +14,12 @@ in
 
   options = {
     services.ddccontrol = {
-      enable = lib.mkEnableOption "ddccontrol for controlling displays";
+      enable = lib.mkEnableOption ''
+        ddccontrol for controlling displays.
+
+        This [enables `hardware.i2c`](#opt-hardware.i2c.enable), so note to add
+        yourself to [`hardware.i2c.group`](#opt-hardware.i2c.group).
+      '';
     };
   };
 
@@ -22,7 +27,12 @@ in
 
   config = lib.mkIf cfg.enable {
     # Load the i2c-dev module
-    boot.kernelModules = [ "i2c_dev" ];
+    boot.kernelModules = [
+      "ddcci_backlight"
+    ];
+    hardware.i2c = {
+      enable = true;
+    };
 
     # Give users access to the "gddccontrol" tool
     environment.systemPackages = [
