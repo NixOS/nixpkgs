9044735dcb2d4c1d86325f1fa78cd8c4b105dc60
Andrew Marshall <andrew@johnandrewmarshall.com>
2025-10-30 11:26:24 -0400

    blender-hip: rename hipSupport -> rocmSupport
    Prior to e720baecf0371b0c92b6c1bddebe9f21ce2fd050 (Nov 2023), there was
    no global config.rocmSupport, and Blender’s hipSupport param predates
    that. Since the global config now exists, defer to that instead.
    
    The closure size of various ROCm packages has also been reduced recently
    in 32d957e0cd38f83fd7a40f9a3fe1dd04f5fd7f0f. At the time I wrote this,
    `pkgsRocm.blender` was 3.1 GiB (vs. 2.5 GiB for `pkgs.blender`). So
    remove the comment.
    
    Add blender to the rocm release attrs so Hydra will build
    `pkgsRocm.blender`.

--- pkgs/by-name/bl/blender/package.nix
+++ pkgs/by-name/bl/blender/package.nix
@@ -23,7 +23,6 @@
   gettext,
   glew,
   gmp,
-  hipSupport ? false,
   jackaudioSupport ? false,
   jemalloc,
   lib,
@@ -68,7 +67,8 @@
   potrace,
   pugixml,
   python3Packages, # must use instead of python3.pkgs, see https://github.com/NixOS/nixpkgs/issues/211340
-  rocmPackages, # comes with a significantly larger closure size
+  rocmPackages,
+  rocmSupport ? config.rocmSupport,
   runCommand,
   shaderc,
   spaceNavSupport ? stdenv.hostPlatform.isLinux,
@@ -137,7 +137,7 @@ stdenv'.mkDerivation (finalAttrs: {
         --replace-fail '${"$"}{LIBDIR}/brotli/lib/libbrotlidec-static.a' \
                   '${lib.getLib brotli}/lib/libbrotlidec.dylib'
     '')
-    + (lib.optionalString hipSupport ''
+    + (lib.optionalString rocmSupport ''
       substituteInPlace extern/hipew/src/hipew.c --replace-fail '"/opt/rocm/hip/lib/libamdhip64.so.${lib.versions.major rocmPackages.clr.version}"' '"${rocmPackages.clr}/lib/libamdhip64.so"'
       substituteInPlace extern/hipew/src/hipew.c --replace-fail '"opt/rocm/hip/bin"' '"${rocmPackages.clr}/bin"'
       substituteInPlace extern/hipew/src/hiprtew.cc --replace-fail '"/opt/rocm/lib/libhiprt64.so"' '"${rocmPackages.hiprt}/lib/libhiprt64.so"'
@@ -159,7 +159,7 @@ stdenv'.mkDerivation (finalAttrs: {
     (lib.cmakeBool "WITH_BUILDINFO" false)
     (lib.cmakeBool "WITH_CPU_CHECK" false)
     (lib.cmakeBool "WITH_CYCLES_CUDA_BINARIES" cudaSupport)
-    (lib.cmakeBool "WITH_CYCLES_DEVICE_HIP" hipSupport)
+    (lib.cmakeBool "WITH_CYCLES_DEVICE_HIP" rocmSupport)
     (lib.cmakeBool "WITH_CYCLES_DEVICE_ONEAPI" false)
     (lib.cmakeBool "WITH_CYCLES_DEVICE_OPTIX" cudaSupport)
     (lib.cmakeBool "WITH_CYCLES_EMBREE" embreeSupport)
@@ -186,7 +186,7 @@ stdenv'.mkDerivation (finalAttrs: {
     (lib.cmakeFeature "OPTIX_ROOT_DIR" "${optix}")
     (lib.cmakeBool "WITH_CYCLES_CUDA_BINARIES" true)
   ]
-  ++ lib.optionals hipSupport [
+  ++ lib.optionals rocmSupport [
     (lib.cmakeFeature "HIPRT_INCLUDE_DIR" "${rocmPackages.hiprt}/include")
     (lib.cmakeBool "WITH_CYCLES_DEVICE_HIPRT" true)
     (lib.cmakeBool "WITH_CYCLES_HIP_BINARIES" true)
@@ -267,7 +267,7 @@ stdenv'.mkDerivation (finalAttrs: {
     zstd
   ]
   ++ lib.optional embreeSupport embree
-  ++ lib.optional hipSupport rocmPackages.clr
+  ++ lib.optional rocmSupport rocmPackages.clr
   ++ lib.optional openImageDenoiseSupport (openimagedenoise.override { inherit cudaSupport; })
   ++ (
     if (!stdenv.hostPlatform.isDarwin) then
