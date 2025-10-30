6df0683e0ae3bdafb1501fc56bace1b18121d214
Gavin John <gavinnjohn@gmail.com>
2025-10-30 08:51:05 -0700

    some more stereokit stuff

--- pkgs/by-name/st/stereokit/package.nix
+++ pkgs/by-name/st/stereokit/package.nix
@@ -2,6 +2,8 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  cpm-cmake,
+  pkg-config,
   cmake,
   ninja,
   clang,
@@ -9,7 +11,11 @@
   libGL,
   libgbm,
   fontconfig,
-  xorg
+  xorg,
+  sk_gpu,
+  meshoptimizer,
+  openxr-loader,
+  reactphysics3d,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -20,13 +26,27 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "StereoKit";
     repo = "StereoKit";
     tag = "v${finalAttrs.version}";
+    hash = "sha256-3WKs6eyYP2lOOKeQCBuRZLXZfO2l0iss0DsiLtHTkyg=";
   };
+
+  postPatch = ''
+    install -D ${cpm-cmake}/share/cpm/CPM.cmake /build/source/build/cmake/CPM_0.38.7.cmake
+  '';
+
+  patches = [
+    ./system-packages.patch
+  ];
+
+  cmakeFlags = [
+    # (lib.cmakeFeature "CPM_DOWNLOAD_LOCATION" "${cpm-cmake}/share/cpm/CPM.cmake")
+  ];
 
   nativeBuildInputs = [
     cmake
     ninja
     clang
     lld
+    pkg-config
   ];
   buildInputs = [
     xorg.libX11
@@ -34,5 +54,9 @@ stdenv.mkDerivation (finalAttrs: {
     libGL
     libgbm
     fontconfig
+    sk_gpu
+    openxr-loader
+    meshoptimizer
+    reactphysics3d
   ];
 })
