4b0ad8042f5503bd38fbba95e5954fb9ea2add04
Matthias Riegel <public@verriegelt.net>
2025-10-30 12:03:32 +0100

    maintainers: add Kyubai

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -14216,6 +14216,12 @@
 
     keys = [ { fingerprint = "5A9A 1C9B 2369 8049 3B48  CF5B 81A1 5409 4816 2372"; } ];
   };
+  kyubai = {
+    name = "Matthias Riegel";
+    email = "nixpkgs@verriegelt.net";
+    github = "kyubai";
+    githubId = 34755366;
+  };
   l0b0 = {
     email = "victor@engmark.name";
     github = "l0b0";
