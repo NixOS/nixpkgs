939d9e7566f25427daad81172ffe7b671bffd667
Gaetan Lepage <gaetan@glepage.com>
2025-10-30 09:40:59 +0000

    python3Packages.executorch: init at 1.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5047,6 +5047,8 @@ self: super: with self; {
 
   executor = callPackage ../development/python-modules/executor { };
 
+  executorch = callPackage ../development/python-modules/executorch { };
+
   exif = callPackage ../development/python-modules/exif { };
 
   exifread = callPackage ../development/python-modules/exifread { };
