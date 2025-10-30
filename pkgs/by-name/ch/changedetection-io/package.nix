f03ae25c2930ea48fb31a31fc31d8f86bf1858c1
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-30 09:52:19 +0000

    changedetection-io: 0.49.4 -> 0.50.35

--- pkgs/by-name/ch/changedetection-io/package.nix
+++ pkgs/by-name/ch/changedetection-io/package.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "changedetection-io";
-  version = "0.49.4";
+  version = "0.50.35";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "dgtlmoon";
     repo = "changedetection.io";
     tag = version;
-    hash = "sha256-EmtJ8XXPb75W4VPj4Si9fdzVLDKVfm+8P6UZZlMpMdI=";
+    hash = "sha256-erSZ9hBrIhWTCuZP3C7lm3yxRv6z3ofYd5ceBGG8QYg=";
   };
 
   pythonRelaxDeps = true;
