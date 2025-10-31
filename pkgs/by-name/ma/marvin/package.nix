f5111f46f630c63fb06cd2ca8a732d7319bc6608
Brenton Horne <brentonhorne77@gmail.com>
2025-10-31 23:09:11 +1000

    marvin: 23.17.0->25.3.4
    Just a straightforward package update.

--- pkgs/by-name/ma/marvin/package.nix
+++ pkgs/by-name/ma/marvin/package.nix
@@ -13,12 +13,12 @@
 
 stdenv.mkDerivation rec {
   pname = "marvin";
-  version = "23.17.0";
+  version = "25.3.4";
 
   src = fetchurl {
     name = "marvin-${version}.deb";
-    url = "http://dl.chemaxon.com/marvin/${version}/marvin_linux_${lib.versions.majorMinor version}.deb";
-    hash = "sha256-zE/9EaOsNJwzE4Doasm9N8QG4t7wDOxqpV/Nhc4p7Ws=";
+    url = "http://dl.chemaxon.com/marvin/${version}/marvin_linux_${version}.deb";
+    hash = "sha256-m9L2yYknpxT+0oiIFoMn2xg7EL9GeL7ENXuA3kCri9M=";
   };
 
   nativeBuildInputs = [
