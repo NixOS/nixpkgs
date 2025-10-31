ef079ae67dc2e5620b621bb939596afeae04b04f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-10-31 17:27:41 -0400

    thunderbird: add lto{Enabled,Disabled} passthru tests

--- pkgs/applications/networking/mailreaders/thunderbird/packages.nix
+++ pkgs/applications/networking/mailreaders/thunderbird/packages.nix
@@ -35,51 +35,60 @@ let
       updateScript,
       applicationName ? "Thunderbird",
     }:
-    (buildMozillaMach rec {
-      pname = "thunderbird";
-      inherit version updateScript applicationName;
-      application = "comm/mail";
-      binaryName = "thunderbird";
-      src = fetchurl {
-        url = "mirror://mozilla/thunderbird/releases/${version}/source/thunderbird-${version}.source.tar.xz";
-        inherit sha512;
-      };
-      extraPatches = [
-        # The file to be patched is different from firefox's `no-buildconfig-ffx90.patch`.
-        (if lib.versionOlder version "140" then ./no-buildconfig.patch else ./no-buildconfig-tb140.patch)
-      ];
-      extraPassthru = {
-        icu73 = icu73';
-        icu77 = icu77';
-      };
+    let
+      self =
+        (buildMozillaMach rec {
+          pname = "thunderbird";
+          inherit version updateScript applicationName;
+          application = "comm/mail";
+          binaryName = "thunderbird";
+          src = fetchurl {
+            url = "mirror://mozilla/thunderbird/releases/${version}/source/thunderbird-${version}.source.tar.xz";
+            inherit sha512;
+          };
+          extraPatches = [
+            # The file to be patched is different from firefox's `no-buildconfig-ffx90.patch`.
+            (if lib.versionOlder version "140" then ./no-buildconfig.patch else ./no-buildconfig-tb140.patch)
+          ];
+          extraPassthru = {
+            icu73 = icu73';
+            icu77 = icu77';
+          };
 
-      meta = {
-        changelog = "https://www.thunderbird.net/en-US/thunderbird/${version}/releasenotes/";
-        description = "Full-featured e-mail client";
-        homepage = "https://thunderbird.net/";
-        mainProgram = "thunderbird";
-        maintainers = with lib.maintainers; [
-          booxter # darwin
-          lovesegfault
-          pierron
-          vcunat
-        ];
-        platforms = lib.platforms.unix;
-        broken = stdenv.buildPlatform.is32bit;
-        # since Firefox 60, build on 32-bit platforms fails with "out of memory".
-        # not in `badPlatforms` because cross-compilation on 64-bit machine might work.
-        license = lib.licenses.mpl20;
-      };
-    }).override
-      {
-        geolocationSupport = false;
-        webrtcSupport = false;
+          tests = {
+            ltoDisabled = self.override { ltoSupport = false; };
+            ltoEnabled = self.override { ltoSupport = true; };
+          };
 
-        pgoSupport = false; # console.warn: feeds: "downloadFeed: network connection unavailable"
+          meta = {
+            changelog = "https://www.thunderbird.net/en-US/thunderbird/${version}/releasenotes/";
+            description = "Full-featured e-mail client";
+            homepage = "https://thunderbird.net/";
+            mainProgram = "thunderbird";
+            maintainers = with lib.maintainers; [
+              booxter # darwin
+              lovesegfault
+              pierron
+              vcunat
+            ];
+            platforms = lib.platforms.unix;
+            broken = stdenv.buildPlatform.is32bit;
+            # since Firefox 60, build on 32-bit platforms fails with "out of memory".
+            # not in `badPlatforms` because cross-compilation on 64-bit machine might work.
+            license = lib.licenses.mpl20;
+          };
+        }).override
+          {
+            geolocationSupport = false;
+            webrtcSupport = false;
 
-        icu73 = icu73';
-        icu77 = icu77';
-      };
+            pgoSupport = false; # console.warn: feeds: "downloadFeed: network connection unavailable"
+
+            icu73 = icu73';
+            icu77 = icu77';
+          };
+    in
+    self;
 
 in
 rec {
