de7682771511e9aa470968e5987d0aad5cf19673
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-31 22:35:32 +0000

    hydralauncher: 3.6.8 -> 3.7.3

--- pkgs/by-name/hy/hydralauncher/package.nix
+++ pkgs/by-name/hy/hydralauncher/package.nix
@@ -6,10 +6,10 @@
 }:
 let
   pname = "hydralauncher";
-  version = "3.6.8";
+  version = "3.7.3";
   src = fetchurl {
     url = "https://github.com/hydralauncher/hydra/releases/download/v${version}/hydralauncher-${version}.AppImage";
-    hash = "sha256-wZszjO+4n1BaiWX9Dw+chZvL89hPmugHju5JlRfPYl4=";
+    hash = "sha256-prX/6cmWEz81nj/W28QbclyaJyNmGOug/ANXee3e0Cs=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname src version; };
