3d989ea666f09aa9db1fd513f575982e17efde45
Zexin Yuan <git@yzx9.xyz>
2025-10-31 17:41:34 +0800

    feather-tk: mark darwin as bad platform

--- pkgs/by-name/fe/feather-tk/package.nix
+++ pkgs/by-name/fe/feather-tk/package.nix
@@ -94,5 +94,9 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ liberodark ];
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    badPlatforms = [
+      # Broken on darwin with latest SDK, see https://github.com/darbyjohnston/feather-tk/issues/1
+      lib.systems.inspect.patterns.isDarwin
+    ];
   };
 })
