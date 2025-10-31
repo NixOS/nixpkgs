2767184fb5ee57f53975f72309dcec424c40cdb5
Disguised Pigeon <rubcessis.unofficial@gmail.com>
2025-10-31 11:59:27 +0100

    nixos/swap: Fix shellcheck issues
    (cherry picked from commit feee5345272bb3088c3b4281975e5de43ea26b37)

--- nixos/modules/config/swap.nix
+++ nixos/modules/config/swap.nix
@@ -297,7 +297,7 @@ in
               ${lib.optionalString (sw.size != null) ''
                 currentSize=$(( $(stat -c "%s" "$DEVICE" 2>/dev/null || echo 0) / 1024 / 1024 ))
                 if [[ ! -b "$DEVICE" && "${toString sw.size}" != "$currentSize" ]]; then
-                  if [[ $(stat -f -c %T $(dirname "$DEVICE")) == "btrfs" ]]; then
+                  if [[ $(stat -f -c %T "$(dirname "$DEVICE")") == "btrfs" ]]; then
                     # Use btrfs mkswapfile to speed up the creation of swapfile.
                     rm -f "$DEVICE"
                     btrfs filesystem mkswapfile --size "${toString sw.size}M" --uuid clear "$DEVICE"
@@ -308,7 +308,7 @@ in
 
                     echo "Creating swap file using dd and mkswap."
                     dd if=/dev/zero of="$DEVICE" bs=1M count=${toString sw.size} status=progress
-                    ${lib.optionalString (!sw.randomEncryption.enable) "mkswap ${sw.realDevice}"}
+                    ${lib.optionalString (!sw.randomEncryption.enable) "mkswap \"${sw.realDevice}\""}
                   fi
                 fi
               ''}
