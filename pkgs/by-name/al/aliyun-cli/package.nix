db84944bff2858a4d2c375ae968d5b27e415ac1c
R. Ryantm <ryantm-bot@ryantm.com>
2025-10-31 15:01:43 +0000

    aliyun-cli: 3.0.308 -> 3.1.0

--- pkgs/by-name/al/aliyun-cli/package.nix
+++ pkgs/by-name/al/aliyun-cli/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "aliyun-cli";
-  version = "3.0.308";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "aliyun";
     repo = "aliyun-cli";
     tag = "v${version}";
-    hash = "sha256-W6ooP9Ua7GtaD9ROBc0AbJdFHMUU6OYwT2JZulvn5+Q=";
+    hash = "sha256-5gwI2lhEMkKZZnYwzyegVm8EByox1XexHjW799vVeLE=";
     fetchSubmodules = true;
   };
 
