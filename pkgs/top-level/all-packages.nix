44e6d480d280fd802d1feca0906b2cb0f0831f54
Guy Chronister <guychronister@outlook.com>
2025-10-31 09:36:32 -0500

    mame: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1333,8 +1333,6 @@ with pkgs;
 
   libmirage = callPackage ../applications/emulators/cdemu/libmirage.nix { };
 
-  mame = libsForQt5.callPackage ../applications/emulators/mame { };
-
   mame-tools = lib.addMetaAttrs {
     description = mame.meta.description + " (tools only)";
   } (lib.getOutput "tools" mame);
