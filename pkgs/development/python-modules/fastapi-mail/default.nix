850e7259d57139e902c4d4514e49ed02e0c237b5
Fabian Affolter <fabian@affolter-engineering.ch>
2025-11-01 09:44:59 +0100

    python313Packages.fastapi-mail: 1.5.0 -> 1.5.7
    Changelog: https://github.com/sabuhish/fastapi-mail/releases/tag/1.5.7

--- pkgs/development/python-modules/fastapi-mail/default.nix
+++ pkgs/development/python-modules/fastapi-mail/default.nix
@@ -3,38 +3,48 @@
   aiosmtplib,
   blinker,
   buildPythonPackage,
+  cryptography,
   email-validator,
   fakeredis,
+  fastapi,
   fetchFromGitHub,
+  flake8,
   httpx,
+  isort,
   jinja2,
+  mkdocs-material,
   poetry-core,
   pydantic-settings,
   pydantic,
   pytest-asyncio,
   pytestCheckHook,
-  pythonOlder,
   redis,
+  regex,
   starlette,
+  uvicorn,
 }:
 
 buildPythonPackage rec {
   pname = "fastapi-mail";
-  version = "1.5.0";
+  version = "1.5.7";
   pyproject = true;
-
-  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "sabuhish";
     repo = "fastapi-mail";
     tag = version;
-    hash = "sha256-v8cf4GlYAdl5+iD7hJHW+FuDN/I/VygWaaZLEotDNCU=";
+    hash = "sha256-C2hjnbTNKtuonY8TZzrx1v0QOt7fgnteEOB3E2kCwhc=";
   };
 
   pythonRelaxDeps = [
     "aiosmtplib"
+    "cryptography"
+    "email-validator"
+    "fastapi"
+    "isort"
     "pydantic"
+    "regex"
+    "uvicorn"
   ];
 
   build-system = [ poetry-core ];
@@ -42,12 +52,19 @@ buildPythonPackage rec {
   dependencies = [
     aiosmtplib
     blinker
+    cryptography
     email-validator
     fakeredis
+    fastapi
+    flake8
+    isort
     jinja2
+    mkdocs-material
     pydantic
     pydantic-settings
+    regex
     starlette
+    uvicorn
   ];
 
   optional-dependencies = {
@@ -72,7 +89,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Module for sending emails and attachments";
     homepage = "https://github.com/sabuhish/fastapi-mail";
-    changelog = "https://github.com/sabuhish/fastapi-mail/releases/tag/${version}";
+    changelog = "https://github.com/sabuhish/fastapi-mail/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ fab ];
   };
