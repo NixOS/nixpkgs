10b384ac9c6f9936e4d46e29fd61e0885e258d46
Melody Renata <me@lovingmelody.io>
2025-11-01 09:44:44 -0400

    lug-helper: 4.4 -> 4.5
    * Correct package name by defining `pname` instead of `name`
    * Add missing dependency `winetricks`

--- pkgs/by-name/lu/lug-helper/package.nix
+++ pkgs/by-name/lu/lug-helper/package.nix
@@ -10,17 +10,18 @@
   unzip,
   cabextract,
   libnotify,
+  winetricks,
   fetchFromGitHub,
   nix-update-script,
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
-  name = "lug-helper";
-  version = "4.4";
+  pname = "lug-helper";
+  version = "4.5";
   src = fetchFromGitHub {
     owner = "starcitizen-lug";
     repo = "lug-helper";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-wYNYgwcves9nmUccDQPGekVt3BIB4QE/t6l9vUwXYUs=";
+    hash = "sha256-d1GqfnCgz4EJtI+6pnIXeMxx8cPu4JdTEFi1Ud7IhJ4=";
   };
 
   buildInputs = [
@@ -61,6 +62,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
           cabextract
           unzip
           libnotify
+          winetricks
         ]
       } \
       --prefix XDG_DATA_DIRS : "$out"
