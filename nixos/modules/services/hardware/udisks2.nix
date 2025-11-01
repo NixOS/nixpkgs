ac214ee0cc6442627df5f55f111fa01bf1edfb19
h7x4 <h7x4@nani.wtf>
2025-11-01 18:40:11 +0900

    nixos/udisks2: add systemd hardnening
    Hardened and sandboxed the applications as much as possible,
    with comments for some of the more non-obvious toggles.
    
    This also changes the mode of `/var/lib/udisks2` from `755` to `700`,
    since there doesn't seem to be anything interesting to other services
    there (unlike `/run/udisks2`, which is seemingly read by `udisksctl`)

--- nixos/modules/services/hardware/udisks2.nix
+++ nixos/modules/services/hardware/udisks2.nix
@@ -98,10 +98,7 @@ in
 
     services.dbus.packages = [ cfg.package ];
 
-    systemd.tmpfiles.rules = [
-      "d /var/lib/udisks2 0755 root root -"
-    ]
-    ++ lib.optional cfg.mountOnMedia "D! /media 0755 root root -";
+    systemd.tmpfiles.rules = lib.optional cfg.mountOnMedia "D! /media 0755 root root -";
 
     services.udev.packages = [ cfg.package ];
 
@@ -110,6 +107,49 @@ in
     '';
 
     systemd.packages = [ cfg.package ];
+
+    systemd.services.udisks2 = {
+      after = lib.optionals cfg.mountOnMedia [ "systemd-tmpfiles-setup.service" ];
+      requires = lib.optionals cfg.mountOnMedia [ "systemd-tmpfiles-setup.service" ];
+
+      serviceConfig = {
+        User = "root";
+        Group = "root";
+
+        StateDirectory = "udisks2";
+        StateDirectoryMode = "0700";
+        RuntimeDirectory = "udisks2";
+        RuntimeDirectoryMode = "0755";
+
+        # A lot of the omitted Private*/Protect* settings would imply
+        # this to be true, which would have the daemon only mount disks
+        # inside its own sandbox and so breaking the main functionality.
+        PrivateMounts = false;
+
+        CapabilityBoundingSet = [
+          "CAP_SYS_ADMIN" # Needed for mount(2) and umount(2)
+        ];
+        SystemCallFilter = [
+          "@system-service"
+          "~@privileged @resources"
+          "@chown @mount"
+        ];
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        ProtectClock = true;
+        ProtectHostname = true;
+        RestrictAddressFamilies = [
+          "AF_UNIX"
+          "AF_NETLINK" # Needed to talk to udev
+        ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        UMask = "022";
+      };
+    };
   };
 
 }
