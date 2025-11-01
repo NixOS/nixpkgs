bd26408d75e1f23e77019c4480172962e1c6eb44
László Kupcsik <dev@biobits.be>
2025-11-01 23:18:14 +0100

    rPackages.CNEr: patch no longer needed
    This reverts commit 8c30825271977126e43b388ac0a7de401492fb05.

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -2217,10 +2217,6 @@ let
       ];
     });
 
-    CNEr = old.CNEr.overrideAttrs (attrs: {
-      patches = [ ./patches/CNEr.patch ];
-    });
-
     covidsymptom = old.covidsymptom.overrideAttrs (attrs: {
       preConfigure = "rm R/covidsymptomdata.R";
     });
