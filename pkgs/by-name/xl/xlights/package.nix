ba98ad04f280f27006ffb2353dc429d136e5fb7b
Merikei <73759842+Merikei@users.noreply.github.com>
2025-11-01 02:04:54 +0000

    xlights: add .desktop file & icon

--- pkgs/by-name/xl/xlights/package.nix
+++ pkgs/by-name/xl/xlights/package.nix
@@ -13,6 +13,16 @@ appimageTools.wrapType2 rec {
     hash = "sha256-k5HGk5t/ujPuOU/GlxhA+yKKXy0lq8YkxcQkTUVBYXM=";
   };
 
+  appimageContents = appimageTools.extract { inherit pname version src; };
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/xlights.desktop $out/share/applications/xlights.desktop
+    install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/xlights.png \
+      $out/share/icons/hicolor/256x256/apps/xlights.png
+    substituteInPlace $out/share/applications/xlights.desktop \
+      --replace-fail 'Exec=xLights' 'Exec=xlights'
+  '';
+
   meta = {
     description = "Sequencer for lights with USB and E1.31 drivers";
     homepage = "https://xlights.org";
