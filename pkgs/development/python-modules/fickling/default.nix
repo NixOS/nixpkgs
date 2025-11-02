eabba2c232f2b5fd3154bbce0223f27496ce678f
Sarah Clark <seclark@nextquestion.net>
2025-11-02 15:57:29 -0800

    python3Packages.fickling: 0.1.3 -> 0.1.4

--- pkgs/development/python-modules/fickling/default.nix
+++ pkgs/development/python-modules/fickling/default.nix
@@ -5,22 +5,23 @@
   distutils,
   fetchFromGitHub,
   flit-core,
+  numpy,
   pytestCheckHook,
   torch,
   torchvision,
+  stdlib-list,
 }:
 
 buildPythonPackage rec {
   pname = "fickling";
-  version = "0.1.3";
+  version = "0.1.4";
   pyproject = true;
-
 
   src = fetchFromGitHub {
     owner = "trailofbits";
     repo = "fickling";
     tag = "v${version}";
-    hash = "sha256-/cV1XhJ8KMFby9nZ/qXEYxf+P6352Q2DZOLuvebyuHQ=";
+    hash = "sha256-EgVtMYPwSVBlw1bmX3qEeUKvEY7Awv6DOB5tgSLG+xQ=";
   };
 
   build-system = [
@@ -28,10 +29,16 @@ buildPythonPackage rec {
     flit-core
   ];
 
-  dependencies = [ astunparse ];
+  dependencies = [
+    astunparse
+    stdlib-list
+  ];
+
+  pythonRelaxDeps = [ "stdlib_list" ];
 
   optional-dependencies = {
     torch = [
+      numpy
       torch
       torchvision
     ];
@@ -39,12 +46,18 @@ buildPythonPackage rec {
 
   nativeCheckInputs = [ pytestCheckHook ] ++ lib.flatten (builtins.attrValues optional-dependencies);
 
+  disabledTestPaths = [
+    # https://github.com/trailofbits/fickling/issues/162
+    # AttributeError: module 'numpy.lib.format' has no attribute...
+    "test/test_polyglot.py"
+  ];
+
   pythonImportsCheck = [ "fickling" ];
 
   meta = {
     description = "Python pickling decompiler and static analyzer";
     homepage = "https://github.com/trailofbits/fickling";
-    changelog = "https://github.com/trailofbits/fickling/releases/tag/v${version}";
+    changelog = "https://github.com/trailofbits/fickling/releases/tag/${src.tag}";
     license = lib.licenses.lgpl3Plus;
     maintainers = [ ];
   };
