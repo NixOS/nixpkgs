NixOS/nixpkgs

commit c9c5db63146fb1899292491b655ae65e362ba629

valgrind: 3.25.1 -> 3.26.0
Changes: https://valgrind.org/docs/manual/dist.news.html

pkgs/by-name/va/valgrind/package.nix
@@ -11,11 +11,11 @@
 
 stdenv.mkDerivation rec {
   pname = "valgrind";
-  version = "3.25.1";
+  version = "3.26.0";
 
   src = fetchurl {
     url = "https://sourceware.org/pub/${pname}/${pname}-${version}.tar.bz2";
-    hash = "sha256-Yd640HJ7RcJo79wbO2yeZ5zZfL9e5LKNHerXyLeica8=";
+    hash = "sha256-jVTHFwKRBvFkSq2vgCq5aS5T2T3QFcvRnnQZDrpha9c=";
   };
 
   patches = [
@@ -105,7 +105,7 @@ stdenv.mkDerivation rec {
       Valgrind to build new tools.
     '';
 
-    license = lib.licenses.gpl2Plus;
+    license = lib.licenses.gpl3Plus;
 
     maintainers = [ lib.maintainers.eelco ];
     platforms =

commit 2878a4f05245408c3acfbb7f615e7319a4b49b5d

valgrind: fix build for musl
Fixes: c9c5db63146f ("valgrind: 3.25.1 -> 3.26.0")

pkgs/by-name/va/valgrind/package.nix
@@ -25,6 +25,12 @@ stdenv.mkDerivation rec {
       url = "https://bugsfiles.kde.org/attachment.cgi?id=148912";
       sha256 = "Za+7K93pgnuEUQ+jDItEzWlN0izhbynX2crSOXBBY/I=";
     })
+    # https://bugs.kde.org/show_bug.cgi?id=511548
+    (fetchpatch {
+      url = "https://bugsfiles.kde.org/attachment.cgi?id=186435";
+      hash = "sha256-Ifsg3Hr78umZR5nwy9DOY8bB59wp8EtV+fYm+h8sk9k=";
+    })
+
     # Fix build on armv7l.
     # see also https://bugs.kde.org/show_bug.cgi?id=454346
     (fetchpatch {