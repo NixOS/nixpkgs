8487791c1bd0f90a0349af428b74c7876e88f06d
Thomas Gerbet <thomas@gerbet.me>
2025-11-02 10:33:39 +0000

    virtualbox: 7.1.12 -> 7.1.14
    Fixes CVE-2025-62587, CVE-2025-62588, CVE-2025-62589,
    CVE-2025-62641, CVE-2025-62590, CVE-2025-61760, CVE-2025-61759,
    CVE-2025-62591 and CVE-2025-62592.
    https://www.oracle.com/security-alerts/cpuoct2025.html#AppendixOVIR
    
    https://www.virtualbox.org/wiki/Changelog-7.1
    
    Not-cherry-picked-because: unstable is using the 7.2.x branch.

--- pkgs/applications/virtualization/virtualbox/extpack.nix
+++ pkgs/applications/virtualization/virtualbox/extpack.nix
@@ -4,7 +4,7 @@
   virtualbox,
 }:
 let
-  virtualboxExtPackVersion = "7.1.12";
+  virtualboxExtPackVersion = "7.1.14";
 in
 fetchurl rec {
   name = "Oracle_VirtualBox_Extension_Pack-${virtualboxExtPackVersion}.vbox-extpack";
@@ -14,7 +14,7 @@ fetchurl rec {
     # Thus do not use `nix-prefetch-url` but instead plain old `sha256sum`.
     # Checksums can also be found at https://www.virtualbox.org/download/hashes/${version}/SHA256SUMS
     let
-      value = "c7ed97f4755988ecc05ec633475e299bbc1e0418cc3d143747a45c99df53abd3";
+      value = "f434446ee24167cac88bc4a2d936b6fc4e8a472626c0a90b38d451fcb88facc5";
     in
     assert (builtins.stringLength value) == 64;
     value;
