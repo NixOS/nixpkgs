cbb30140cfd5c750b614008458f7ab24cd6054b6
4a-42 <22332960+4a-42@users.noreply.github.com>
2025-11-02 01:31:21 +0000

    openapi-generator-cli: 7.16.0 -> 7.17.0
    Co-authored-by: Damien Cassou <damien@cassou.me>

--- pkgs/by-name/op/openapi-generator-cli/package.nix
+++ pkgs/by-name/op/openapi-generator-cli/package.nix
@@ -9,7 +9,7 @@
 
 let
   jre = jre_headless;
-  version = "7.16.0";
+  version = "7.17.0";
   mainProgram = "openapi-generator-cli";
   this = maven.buildMavenPackage {
     inherit version;
@@ -20,10 +20,10 @@ let
       owner = "OpenAPITools";
       repo = "openapi-generator";
       tag = "v${version}";
-      hash = "sha256-CoztWf2H2rXcx4d8Av8cBXzMqIZsrSCgx21i3+o2ufo=";
+      hash = "sha256-9jO+u08wrxW0RcXTGDh8mpPp9Eu2PwRifKNsso6s3QM=";
     };
 
-    mvnHash = "sha256-5Kzv9h3X5s/1D0Gd1XQRvNGVAyf44QcriJFvS07wdZo=";
+    mvnHash = "sha256-PiLpJponpJUXakdno+KGxzDvZG6jYRPp3t5q/G5JX+M=";
     mvnParameters = "-Duser.home=$TMPDIR";
     doCheck = false;
 
