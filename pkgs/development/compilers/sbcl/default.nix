b2ae7928f2a820bbe5b41f9f9aafaa2ce7e08606
Andrew Marshall <andrew@johnandrewmarshall.com>
2025-11-02 09:27:23 -0500

    Revert "sbcl: remove broken run-program test"
    Fixed in be092d0c7b2c334835ae8eb02f5008260efea68f.
    
    This reverts commit 79e66e6c100435b5e5a8107d7665b49d84b182b0.

--- pkgs/development/compilers/sbcl/default.nix
+++ pkgs/development/compilers/sbcl/default.nix
@@ -156,22 +156,6 @@ stdenv.mkDerivation (self: {
       "threads.pure.lisp"
     ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [
-      # This test has a gotcha on Darwin which originally showed up in
-      # 57b36ea5c83a1841b174ec6cd5e423439fe9d7a0, and later again around Oct
-      # 2025 in staging.  The test wants a clean environment (using
-      # run-program, akin to fork & execve), but darwin keeps injecting this
-      # envvar:
-      #
-      #   __CF_USER_TEXT_ENCODING=0x15F:0:0
-      #
-      # It’s not clear to maintainers where the problem lies exactly, but
-      # removing the test at least fixes the build and unblocks others.
-      #
-      # see:
-      # - https://github.com/NixOS/nixpkgs/pull/359214
-      # - https://github.com/NixOS/nixpkgs/pull/453099
-      "run-program.test.sh"
-
       # Fails in sandbox
       "sb-posix.impure.lisp"
     ];
