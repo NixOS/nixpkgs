2daf6f370fcffc4228bab3d7e084296638a657eb
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-02 13:13:59 +0000

    zeekscript: 1.3.2-unstable-2025-09-29 -> 1.3.2

--- pkgs/by-name/ze/zeekscript/package.nix
+++ pkgs/by-name/ze/zeekscript/package.nix
@@ -6,7 +6,7 @@
 
 python3.pkgs.buildPythonApplication rec {
   pname = "zeekscript";
-  version = "1.3.2-unstable-2025-09-29";
+  version = "1.3.2";
   pyproject = true;
 
   src = fetchFromGitHub {
