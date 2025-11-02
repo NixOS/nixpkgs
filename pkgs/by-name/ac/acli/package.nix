6ca2951e67e9bb65a3da2ccb3b623843640cade1
Moraxyc <i@qaq.li>
2025-11-02 13:37:40 +0800

    acli: clean up shell completion installation

--- pkgs/by-name/ac/acli/package.nix
+++ pkgs/by-name/ac/acli/package.nix
@@ -26,17 +26,17 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     runHook preInstall
 
     install -Dm755 acli $out/bin/acli
+  ''
+  + lib.optionalString (stdenvNoCC.buildPlatform.canExecute stdenvNoCC.hostPlatform) ''
+    installShellCompletion --cmd acli \
+      --bash <($out/bin/acli completion bash) \
+      --fish <($out/bin/acli completion fish) \
+      --zsh <($out/bin/acli completion zsh)
 
-    ${lib.optionalString (stdenvNoCC.buildPlatform.canExecute stdenvNoCC.hostPlatform) ''
-      installShellCompletion --cmd acli \
-        --bash <($out/bin/acli completion bash) \
-        --fish <($out/bin/acli completion fish) \
-        --zsh <($out/bin/acli completion zsh)
-
-      mkdir -p $out/share/powershell
-      $out/bin/acli completion powershell > $out/share/powershell/acli.Completion.ps1
-    ''}
-
+    mkdir -p $out/share/powershell
+    $out/bin/acli completion powershell > $out/share/powershell/acli.Completion.ps1
+  ''
+  + ''
     runHook postInstall
   '';
 
