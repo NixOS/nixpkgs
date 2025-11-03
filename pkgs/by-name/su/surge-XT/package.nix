f976571e0d0afbece056f0a8d00b2779d514bc42
mrtnvgr <martynovegorOF@yandex.ru>
2025-11-03 10:51:25 +0700

    surge-XT: add build options

--- pkgs/by-name/su/surge-XT/package.nix
+++ pkgs/by-name/su/surge-XT/package.nix
@@ -13,6 +13,11 @@
   libXext,
   libXinerama,
   libXrandr,
+
+  buildVST3 ? true,
+  buildLV2 ? true,
+  buildCLAP ? true,
+  buildStandalone ? true,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -26,6 +31,12 @@ stdenv.mkDerivation (finalAttrs: {
     fetchSubmodules = true;
     hash = "sha256-4b0H3ZioiXFc4KCeQReobwQZJBl6Ep2/8JlRIwvq/hQ=";
   };
+
+  patches = [
+    # NOTE: merged in upstream, remove on package update
+    #     (https://github.com/surge-synthesizer/surge/pull/8202)
+    ./clap-option.diff
+  ];
 
   postPatch = ''
     # see https://github.com/NixOS/nixpkgs/pull/149487#issuecomment-991747333
@@ -47,18 +58,21 @@ stdenv.mkDerivation (finalAttrs: {
     alsa-lib
     freetype
     libjack2
-    lv2
     libX11
     libXcursor
     libXext
     libXinerama
     libXrandr
-  ];
+  ]
+  ++ lib.optionals buildLV2 [ lv2 ];
 
   enableParallelBuilding = true;
 
   cmakeFlags = [
-    "-DSURGE_BUILD_LV2=TRUE"
+    (lib.cmakeBool "SURGE_SKIP_STANDALONE" (!buildStandalone))
+    (lib.cmakeBool "SURGE_SKIP_VST3" (!buildVST3))
+    (lib.cmakeBool "SURGE_SKIP_LV2" buildLV2)
+    (lib.cmakeBool "SURGE_BUILD_CLAP" buildCLAP)
   ];
 
   # JUCE dlopen's these at runtime, crashes without them
