986fa5f06808d98dd7bb3e762cf3ac30944c2e6c
László Kupcsik <dev@biobits.be>
2025-11-03 22:12:23 +0100

    rPackages.lnmixsurv: add missing dep

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -1030,6 +1030,7 @@ let
       zlib.dev
       ncurses.dev
     ];
+    lnmixsurv = [ pkgs.gsl.dev ];
     svKomodo = [ pkgs.which ];
     transmogR = [ pkgs.zlib.dev ];
     ulid = [ pkgs.zlib.dev ];
