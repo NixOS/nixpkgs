a563ddee940a08599b5401e5415479fcd5a1ce7f
nyanloutre <paul@nyanlout.re>
2025-11-03 08:29:38 +0000

    jellyfin: 10.11.1 -> 10.11.2

--- pkgs/by-name/je/jellyfin/package.nix
+++ pkgs/by-name/je/jellyfin/package.nix
@@ -13,13 +13,13 @@
 
 buildDotnetModule rec {
   pname = "jellyfin";
-  version = "10.11.1"; # ensure that jellyfin-web has matching version
+  version = "10.11.2"; # ensure that jellyfin-web has matching version
 
   src = fetchFromGitHub {
     owner = "jellyfin";
     repo = "jellyfin";
     rev = "v${version}";
-    hash = "sha256-8D8cwAqu+cfTR1SwwHTtPnMG2iaLTWSYoAig6g2jsTA=";
+    hash = "sha256-cq45OP7xNfQ09ZfrKxnmHo68Y7SkfSVArH6PlhewPaM=";
   };
 
   propagatedBuildInputs = [ sqlite ];
