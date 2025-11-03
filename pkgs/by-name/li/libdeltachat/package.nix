4fa1fa56f3b27b419726477494e8671ba8809fb9
Robert Schütz <nix@dotlambda.de>
2025-11-03 13:30:28 -0800

    libdeltachat: 2.23.0 -> 2.24.0
    Diff: https://github.com/chatmail/core/compare/v2.23.0...v2.24.0
    
    Changelog: https://github.com/chatmail/core/blob/v2.24.0/CHANGELOG.md

--- pkgs/by-name/li/libdeltachat/package.nix
+++ pkgs/by-name/li/libdeltachat/package.nix
@@ -20,13 +20,13 @@
 
 stdenv.mkDerivation rec {
   pname = "libdeltachat";
-  version = "2.23.0";
+  version = "2.24.0";
 
   src = fetchFromGitHub {
     owner = "chatmail";
     repo = "core";
     tag = "v${version}";
-    hash = "sha256-abSLSXD1bZ4Md2cs9pm7wlielLQicyl+/11TeShIsYI=";
+    hash = "sha256-OCC1qHeDSJJcbTmsyVKiwzXiIQAmofu/EhF56cRJ3kE=";
   };
 
   patches = [
@@ -36,7 +36,7 @@ stdenv.mkDerivation rec {
   cargoDeps = rustPlatform.fetchCargoVendor {
     pname = "chatmail-core";
     inherit version src;
-    hash = "sha256-dLcJV2WUwQJ1YdfqrsGp3nkMyilDhmtYA304jeSjxVQ=";
+    hash = "sha256-/PRIVdW2pnioqaYoIX/87cBbPezRaSbX5RWN6yuwpe8=";
   };
 
   nativeBuildInputs = [
