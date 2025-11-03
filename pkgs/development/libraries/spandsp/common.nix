NixOS/nixpkgs

commit 8ab72653f2cb774226a0f08577aabc7972c02e14

{spandsp,spandsp3}: Enable tests

pkgs/development/libraries/spandsp/common.nix
@@ -5,11 +5,27 @@
   libtiff,
   buildPackages,
   fetchpatch,
-  autoreconfHook,
+  autoconf,
+  automake,
+  fftw,
+  libpcap,
+  libsndfile,
+  libtool,
+  libxml2,
+  netpbm,
+  sox,
+  util-linux,
+  which,
+}:
+
+{
+  version,
+  src,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "spandsp";
+  inherit version src;
 
   patches = [
     # submitted upstream: https://github.com/freeswitch/spandsp/pull/47
@@ -19,33 +35,169 @@ stdenv.mkDerivation (finalAttrs: {
     })
   ];
 
+  postPatch = ''
+    patchShebangs autogen.sh
+
+    # pkg-config? What's that?
+    # Actually *check* the value given for --{en,dis}able-tests, not just whether the option was passed
+    substituteInPlace configure.ac \
+      --replace-fail '$xml2_include_dir /usr/include /usr/local/include /usr/include/libxml2 /usr/local/include/libxml2' '$xml2_include_dir ${lib.getDev libxml2}/include ${lib.getDev libxml2}/include/libxml2 /usr/local/include/libxml2' \
+      --replace-fail 'if test -n "$enable_tests" ; then' 'if test "$enable_tests" = "yes" ; then'
+  '';
+
   outputs = [
     "out"
     "dev"
   ];
 
-  nativeBuildInputs = [ autoreconfHook ];
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    autoconf
+    automake
+    libtool
+    util-linux
+    which
+  ];
+
   depsBuildBuild = [ buildPackages.stdenv.cc ];
+
   propagatedBuildInputs = [
     audiofile
     libtiff
   ];
 
+  nativeCheckInputs = [
+    libtiff
+    netpbm
+    sox
+  ];
+
+  checkInputs = [
+    fftw
+    libpcap
+    libsndfile
+    libxml2
+  ];
+
+  preConfigure = ''
+    ./autogen.sh
+  '';
+
   configureFlags = [
+    (lib.strings.enableFeature finalAttrs.finalPackage.doCheck "tests")
+
     # This flag is required to prevent linking error in the cross-compilation case.
     # I think it's fair to assume that realloc(NULL, size) will return a valid memory
     # block for most libc implementation, so let's just assume that and hope for the best.
     "ac_cv_func_malloc_0_nonnull=yes"
   ];
 
-  enableParallelBuilding = true;
+  # Issues with test asset generation under heavy parallelism
+  enableParallelBuilding = false;
 
   makeFlags = [
     "CC=${stdenv.cc.targetPrefix}cc"
     "CC_FOR_BUILD=${buildPackages.stdenv.cc}/bin/cc"
   ];
 
-  strictDeps = true;
+  env.NIX_CFLAGS_COMPILE = toString [
+    # Missing const conversion on some calls
+    "-Wno-error=incompatible-pointer-types"
+  ];
+
+  hardeningDisable = lib.optionals (lib.strings.versionOlder finalAttrs.version "3.0.0") [
+    "format"
+  ];
+
+  doCheck =
+    stdenv.buildPlatform.canExecute stdenv.hostPlatform
+    # Compat with i.e. Clang improved by 0446f4e0c72553f3ea9b50c5333ece6ac980e58d, too big to apply
+    && stdenv.cc.isGNU;
+
+  disabledTests = [
+    # Need proprietary recordings that aren't included
+    "ademco_contactid_tests"
+    "dtmf_rx_tests"
+    "g722_tests"
+    "g726_tests"
+    "gsm0610_tests"
+    "modem_connect_tones_tests"
+    "sig_tone_tests"
+    "t42_tests"
+    "t43_tests"
+    "v42bis_tests"
+
+    # Fails to initialise modem.
+    "data_modems_tests"
+
+    # ERROR PTY Fatal error: failed to create /dev/spandsp/0 symbolic link
+    "dummy_modems_tests" # needs extra setup, but eventually runs into the above error
+    "pseudo_terminal_tests"
+
+    # Hangs. Maybe needs setup / some interaction
+    "fax_tests"
+
+    # Needs an input file, unsure what's suitable
+    "playout_tests"
+
+    # Borked in the packaged versions due to missing audio file - copy-pasting mistake
+    # Maybe fixed after 5394b2cae6c482ccb835335b769469977e6802ae, but too big to apply
+    "super_tone_rx_tests"
+
+    # Stuck
+    "t31_pseudo_terminal_tests"
+
+    # Generated signal has more than the expected maximum samples - 441 vs 422
+    "time_scale_tests"
+
+    # Test failed - -16dBm0 of noise, signal is 9056.993164 (11440.279297)
+    "tone_detect_tests"
+
+    # Fails due to result different from reference
+    "v18_tests"
+
+    # Seemingly runs forever, with tons of output
+    "v22bis_tests"
+  ];
+
+  checkPhase = ''
+    runHook preCheck
+
+    pushd tests
+
+    for test in *_tests; do
+      testArgs=()
+      case "$test" in
+        # Skipped tests
+        ${lib.strings.concatStringsSep "|" finalAttrs.disabledTests})
+          echo "Skipping $test"
+          continue
+          ;;
+
+        # Needs list of subtests to run
+        echo_tests)
+          testArgs+=(
+            sanity 2a 2b 2ca 3a 3ba 3bb 3c 4 5 6 7 8 9 10a 10b 10c 11 12 13 14 15
+          )
+          # Fallthough for running the test
+          ;;&
+
+        *)
+          echo "Running $test"
+          if ! ./"$test" "''${testArgs[@]}" >"$test".log 2>&1; then
+            echo "$test failed! Output:"
+            cat "$test".log
+            exit 1
+          fi
+          ;;
+      esac
+    done
+
+    popd
+
+    runHook postCheck
+  '';
 
   meta = {
     description = "Portable and modular SIP User-Agent with audio and video support";

pkgs/development/libraries/spandsp/default.nix
@@ -3,10 +3,10 @@
   callPackage,
 }:
 
-(callPackage ./common.nix { }).overrideAttrs (_: rec {
+(callPackage ./common.nix { }) rec {
   version = "0.0.6";
   src = fetchurl {
     url = "https://www.soft-switch.org/downloads/spandsp/spandsp-${version}.tar.gz";
     sha256 = "0rclrkyspzk575v8fslzjpgp4y2s4x7xk3r55ycvpi4agv33l1fc";
   };
-})
+}

pkgs/development/libraries/spandsp/3.nix
@@ -4,16 +4,19 @@
   libjpeg,
 }:
 
-(callPackage ./common.nix { }).overrideAttrs (previousAttrs: {
+((callPackage ./common.nix { }) {
   version = "3.0.0";
   src = fetchFromGitHub {
     owner = "freeswitch";
-    repo = previousAttrs.pname;
+    repo = "spandsp";
     rev = "6ec23e5a7e411a22d59e5678d12c4d2942c4a4b6"; # upstream does not seem to believe in tags
     sha256 = "03w0s99y3zibi5fnvn8lk92dggfgrr0mz5255745jfbz28b2d5y7";
   };
-
-  propagatedBuildInputs = previousAttrs.propagatedBuildInputs or [ ] ++ [
-    libjpeg
-  ];
-})
+}).overrideAttrs
+  (
+    finalAttrs: previousAttrs: {
+      propagatedBuildInputs = previousAttrs.propagatedBuildInputs or [ ] ++ [
+        libjpeg
+      ];
+    }
+  )

commit 7bab532eaefabd2f4d5186a0b59d5dbace8d7796

spandsp,spandsp3: fix tests on musl
Includes a couple of test portability fixes, and also, for spandsp
0.0.6, a couple of memory safety backports that fix test failures on
musl.

Fixes: 8ab72653f2cb ("{spandsp,spandsp3}: Enable tests")

pkgs/development/libraries/spandsp/Check-for-feenableexcept-explicitly.patch
@@ -1,0 +1,132 @@
+From c0caa79a2aa6ac5b358f30719ff80ee2c6db51f7 Mon Sep 17 00:00:00 2001
+From: Alyssa Ross <hi@alyssa.is>
+Date: Mon, 3 Nov 2025 12:06:15 +0100
+Subject: [PATCH] Check for feenableexcept() explicitly
+
+musl provides <fenv.h>, but not the non-standard feenableexcept()
+function, so using feenableexcept() whenever <fenv.h> was present
+caused build failures on musl.  Instead, explicitly check for the
+non-standard function we want to use.
+---
+ configure.ac         | 2 +-
+ tests/v17_tests.c    | 6 +++---
+ tests/v27ter_tests.c | 6 +++---
+ tests/v29_tests.c    | 6 +++---
+ 4 files changed, 10 insertions(+), 10 deletions(-)
+
+diff --git a/configure.ac b/configure.ac
+index fa87245..a8864d8 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -186,6 +186,7 @@ AC_CHECK_FUNCS([strstr])
+ AC_CHECK_FUNCS([strtol])
+ AC_CHECK_FUNCS([gettimeofday])
+ AC_CHECK_FUNCS([drand48])
++AC_CHECK_FUNCS([feenableexcept])
+ 
+ AC_HEADER_SYS_WAIT
+ 
+@@ -209,7 +210,6 @@ AC_CHECK_HEADERS([sys/select.h])
+ AC_CHECK_HEADERS([sys/ioctl.h])
+ AC_CHECK_HEADERS([sys/fcntl.h])
+ AC_CHECK_HEADERS([sndfile.h])
+-AC_CHECK_HEADERS([fenv.h])
+ AC_CHECK_HEADERS([fftw3.h], , [AC_CHECK_HEADERS([fftw.h])])
+ AC_CHECK_HEADERS([pcap.h])
+ AC_CHECK_HEADERS([pthread.h])
+diff --git a/tests/v17_tests.c b/tests/v17_tests.c
+index 17c7fcb..411396c 100644
+--- a/tests/v17_tests.c
++++ b/tests/v17_tests.c
+@@ -59,7 +59,7 @@ display of modem status is maintained.
+ #include <string.h>
+ #include <sndfile.h>
+ #include <signal.h>
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+ #define __USE_GNU
+ #include <fenv.h>
+ #endif
+@@ -263,7 +263,7 @@ static void qam_report(void *user_data, const complexf_t *constel, const complex
+ }
+ /*- End of function --------------------------------------------------------*/
+ 
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+ static void sigfpe_handler(int sig_num, siginfo_t *info, void *data)
+ {
+     switch (sig_num)
+@@ -425,7 +425,7 @@ int main(int argc, char *argv[])
+     inhandle = NULL;
+     outhandle = NULL;
+ 
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+     fpe_trap_setup();
+ #endif
+ 
+diff --git a/tests/v27ter_tests.c b/tests/v27ter_tests.c
+index 14b06f8..f0559a4 100644
+--- a/tests/v27ter_tests.c
++++ b/tests/v27ter_tests.c
+@@ -58,7 +58,7 @@ display of modem status is maintained.
+ #include <string.h>
+ #include <sndfile.h>
+ #include <signal.h>
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+ #define __USE_GNU
+ #include <fenv.h>
+ #endif
+@@ -286,7 +286,7 @@ static void qam_report(void *user_data, const complexf_t *constel, const complex
+ }
+ /*- End of function --------------------------------------------------------*/
+ 
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+ static void sigfpe_handler(int sig_num, siginfo_t *info, void *data)
+ {
+     switch (sig_num)
+@@ -440,7 +440,7 @@ int main(int argc, char *argv[])
+     inhandle = NULL;
+     outhandle = NULL;
+ 
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+     fpe_trap_setup();
+ #endif
+ 
+diff --git a/tests/v29_tests.c b/tests/v29_tests.c
+index 390663f..7a43797 100644
+--- a/tests/v29_tests.c
++++ b/tests/v29_tests.c
+@@ -58,7 +58,7 @@ display of modem status is maintained.
+ #include <string.h>
+ #include <sndfile.h>
+ #include <signal.h>
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+ #define __USE_GNU
+ #include <fenv.h>
+ #endif
+@@ -257,7 +257,7 @@ static void qam_report(void *user_data, const complexf_t *constel, const complex
+ }
+ /*- End of function --------------------------------------------------------*/
+ 
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+ static void sigfpe_handler(int sig_num, siginfo_t *info, void *data)
+ {
+     switch (sig_num)
+@@ -410,7 +410,7 @@ int main(int argc, char *argv[])
+     inhandle = NULL;
+     outhandle = NULL;
+ 
+-#if defined(HAVE_FENV_H)
++#if defined(HAVE_FEENABLEEXCEPT)
+     fpe_trap_setup();
+ #endif
+ 
+-- 
+2.51.0
+

pkgs/development/libraries/spandsp/Fix-tests-pcap_parse-build-on-musl.patch
@@ -1,0 +1,28 @@
+From 5007684cd46fec39d3c8f67a222f2d34d48fd4b4 Mon Sep 17 00:00:00 2001
+From: Alyssa Ross <hi@alyssa.is>
+Date: Mon, 3 Nov 2025 12:07:48 +0100
+Subject: [PATCH] Fix tests/pcap_parse build on musl
+
+musl only defines the Linux-style struct udphdr member names if
+_GNU_SOURCE is defined, so do that, like certain other tests already
+do.
+---
+ tests/pcap_parse.c | 2 ++
+ 1 file changed, 2 insertions(+)
+
+diff --git a/tests/pcap_parse.c b/tests/pcap_parse.c
+index 4e79743..a8a69a3 100644
+--- a/tests/pcap_parse.c
++++ b/tests/pcap_parse.c
+@@ -31,6 +31,8 @@
+ #include "config.h"
+ #endif
+ 
++#define _GNU_SOURCE
++
+ #include <inttypes.h>
+ #include <stdlib.h>
+ #include <stdio.h>
+-- 
+2.51.0
+

pkgs/development/libraries/spandsp/common.nix
@@ -21,6 +21,7 @@
 {
   version,
   src,
+  patches ? [ ],
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -33,7 +34,14 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://github.com/freeswitch/spandsp/commit/1f810894804d3fa61ab3fc2f3feb0599145a3436.patch";
       hash = "sha256-Cf8aaoriAvchh5cMb75yP2gsZbZaOLha/j5mq3xlkVA=";
     })
-  ];
+
+    # https://github.com/freeswitch/spandsp/pull/110
+    ./Check-for-feenableexcept-explicitly.patch
+
+    # https://github.com/freeswitch/spandsp/pull/111
+    ./Fix-tests-pcap_parse-build-on-musl.patch
+  ]
+  ++ patches;
 
   postPatch = ''
     patchShebangs autogen.sh

pkgs/development/libraries/spandsp/default.nix
@@ -1,5 +1,6 @@
 {
   fetchurl,
+  fetchpatch,
   callPackage,
 }:
 
@@ -9,4 +10,15 @@
     url = "https://www.soft-switch.org/downloads/spandsp/spandsp-${version}.tar.gz";
     sha256 = "0rclrkyspzk575v8fslzjpgp4y2s4x7xk3r55ycvpi4agv33l1fc";
   };
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/freeswitch/spandsp/commit/f7b96b08db148763039cf3459d0e00da9636eb92.patch";
+      includes = [ "spandsp-sim/g1050.c" ];
+      hash = "sha256-TwMhWJXQG/UaWddWgice0klp1uATyHMiE6DcsCebXYQ=";
+    })
+    (fetchpatch {
+      url = "https://github.com/freeswitch/spandsp/commit/f47bcdc301fbddad44e918939eed1b361882f740.patch";
+      hash = "sha256-O+lIC3V92GVFoiHsUQOXkoTN2hJ7v5+LQP7RrAhvwlY=";
+    })
+  ];
 }