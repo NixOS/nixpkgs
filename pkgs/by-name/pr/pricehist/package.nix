77403f189b170b1b5a395db81d22b42b5a26a349
Rafael Ieda <git@ieda.me>
2025-11-03 07:23:48 -0300

    pricehist: 1.4.7 -> 1.4.14

--- pkgs/by-name/pr/pricehist/package.nix
+++ pkgs/by-name/pr/pricehist/package.nix
@@ -6,14 +6,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "pricehist";
-  version = "1.4.7";
+  version = "1.4.14";
   format = "pyproject";
 
   src = fetchFromGitLab {
     owner = "chrisberkhout";
     repo = "pricehist";
     tag = version;
-    hash = "sha256-SBRJxNnA+nOxO6h97WZZHwhxoXeNtb5+rDayn4Hw6so=";
+    hash = "sha256-BnyoSYVjs2odnOzSpvgMF860PDkz7tPNnM0s3Fep5G0=";
   };
 
   dependencies = with python3Packages; [
@@ -21,6 +21,10 @@ python3Packages.buildPythonApplication rec {
     lxml
     cssselect
     curlify
+  ];
+
+  pythonRelaxDeps = [
+    "lxml"
   ];
 
   build-system = with python3Packages; [
