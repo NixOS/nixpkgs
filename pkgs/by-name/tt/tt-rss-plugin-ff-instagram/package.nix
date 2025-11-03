29411bde1eada55efaa6bd15b8f96d6a74cfddde
gileri <e+pgp9225@linuxw.info>
2025-11-03 19:29:37 +0100

    tt-rss-plugin-ff-instagram: Set gileri as maintainer

--- pkgs/by-name/tt/tt-rss-plugin-ff-instagram/package.nix
+++ pkgs/by-name/tt/tt-rss-plugin-ff-instagram/package.nix
@@ -31,7 +31,7 @@ stdenv.mkDerivation {
     '';
     license = licenses.agpl3Plus;
     homepage = "https://github.com/wltb/ff_instagram";
-    maintainers = with maintainers; [ das_j ];
+    maintainers = with maintainers; [ gileri ];
     platforms = platforms.all;
   };
 }
