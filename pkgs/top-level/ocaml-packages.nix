9fd7bd516cc47d6bf857d4977d9b6d837a5b172b
redianthus <l@ndrs.fr>
2025-11-03 16:56:08 +0100

    ocamlPackages.ocb: init at 0.2

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1491,6 +1491,8 @@ let
 
         ocamlscript = callPackage ../development/tools/ocaml/ocamlscript { };
 
+        ocb = callPackage ../development/ocaml-modules/ocb { };
+
         ocb-stubblr = callPackage ../development/ocaml-modules/ocb-stubblr { };
 
         ocf = callPackage ../development/ocaml-modules/ocf { };
