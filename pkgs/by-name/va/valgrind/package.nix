NixOS/nixpkgs

commit 2878a4f05245408c3acfbb7f615e7319a4b49b5d

valgrind: fix build for musl
Fixes: c9c5db63146f ("valgrind: 3.25.1 -> 3.26.0")

pkgs/by-name/va/valgrind/package.nix
@@ -25,6 +25,12 @@ stdenv.mkDerivation rec {
       url = "https://bugsfiles.kde.org/attachment.cgi?id=148912";
       sha256 = "Za+7K93pgnuEUQ+jDItEzWlN0izhbynX2crSOXBBY/I=";
     })
+    # https://bugs.kde.org/show_bug.cgi?id=511548
+    (fetchpatch {
+      url = "https://bugsfiles.kde.org/attachment.cgi?id=186435";
+      hash = "sha256-Ifsg3Hr78umZR5nwy9DOY8bB59wp8EtV+fYm+h8sk9k=";
+    })
+
     # Fix build on armv7l.
     # see also https://bugs.kde.org/show_bug.cgi?id=454346
     (fetchpatch {

commit 4754a31af5118d360f7915012d63bd2b9dbed0ee

valgrind: fix build for Glibc
Add missing include.

Fixes: 2878a4f05245 ("valgrind: fix build for musl")
Closes: https://github.com/NixOS/nixpkgs/pull/457933

pkgs/by-name/va/valgrind/package.nix
@@ -27,8 +27,8 @@ stdenv.mkDerivation rec {
     })
     # https://bugs.kde.org/show_bug.cgi?id=511548
     (fetchpatch {
-      url = "https://bugsfiles.kde.org/attachment.cgi?id=186435";
-      hash = "sha256-Ifsg3Hr78umZR5nwy9DOY8bB59wp8EtV+fYm+h8sk9k=";
+      url = "https://bugsfiles.kde.org/attachment.cgi?id=186451";
+      hash = "sha256-IGmyHwwGoy00hcz3XxQSDcwcU8zHLBJ9dfqTvWDQ520=";
     })
 
     # Fix build on armv7l.