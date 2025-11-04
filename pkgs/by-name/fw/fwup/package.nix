87bdb92211f2ccbd3eb760a0099ab5f65a7cf9a3
Robert Schütz <nix@dotlambda.de>
2025-11-04 09:36:10 -0800

    fwup: add meta.changelog

--- pkgs/by-name/fw/fwup/package.nix
+++ pkgs/by-name/fw/fwup/package.nix
@@ -26,8 +26,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "fhunleth";
     repo = "fwup";
-    rev = "v${version}";
-    sha256 = "sha256-h5KUdJ+9/3Oc4/40sn3o8w6zALE7wPGJAtjhsutr55k=";
+    tag = "v${version}";
+    hash = "sha256-h5KUdJ+9/3Oc4/40sn3o8w6zALE7wPGJAtjhsutr55k=";
   };
 
   nativeBuildInputs = [
@@ -62,6 +62,7 @@ stdenv.mkDerivation rec {
   doCheck = !stdenv.hostPlatform.isDarwin;
 
   meta = with lib; {
+    changelog = "https://github.com/fwup-home/fwup/blob/${src.tag}/CHANGELOG.md";
     description = "Configurable embedded Linux firmware update creator and runner";
     homepage = "https://github.com/fhunleth/fwup";
     license = licenses.asl20;
