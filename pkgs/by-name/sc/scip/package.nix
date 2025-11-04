53f2b6f612d7303699b5ce146cbed9f48108c487
liberodark <liberodark@gmail.com>
2025-11-04 12:03:48 +0100

    scip: 0.5.1 -> 0.6.1

--- pkgs/by-name/sc/scip/package.nix
+++ pkgs/by-name/sc/scip/package.nix
@@ -2,48 +2,68 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
-  testers,
-  scip,
+  libredirect,
+  iana-etc,
+  stdenv,
+  versionCheckHook,
+  nix-update-script,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "scip";
-  version = "0.5.1";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "sourcegraph";
     repo = "scip";
-    rev = "v${version}";
-    hash = "sha256-UXa5lMFenynHRIvA4MOXkjMVd705LBWs372s3MFAc+8=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-l68xhOMgwt+ySChk7BCyklcuC6r51GgobAg3lRLvOCU=";
   };
 
-  vendorHash = "sha256-6vx3Dt0ZNR0rY5bEUF5X1hHj/gv21920bhfd+JJ9bYk=";
+  vendorHash = "sha256-8HgeG/SXkM7ptOwKSi/PUH3VySxFqqoIpXI7bZtbO4A=";
 
   ldflags = [
     "-s"
-    "-w"
     "-X=main.Reproducible=true"
   ];
 
-  # update documentation to fix broken test
-  postPatch = ''
-    substituteInPlace docs/CLI.md \
-      --replace 0.3.0 0.3.1
+  nativeCheckInputs = lib.optionals stdenv.hostPlatform.isDarwin [
+    libredirect.hook
+    iana-etc
+  ];
+
+  __darwinAllowLocalNetworking = true;
+
+  # Provide /etc/protocols for SQLite tests
+  preCheck = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    export NIX_REDIRECTS=/etc/protocols=${iana-etc}/etc/protocols:/etc/services=${iana-etc}/etc/services
   '';
 
-  passthru.tests = {
-    version = testers.testVersion {
-      package = scip;
-      version = "v${version}";
-    };
+  # Skip tests has logic error or missing test file.
+  checkFlags = [
+    "-skip=TestLessCompareConsistent|TestParseCompat|TestParseSymbol_ZeroAllocationsIfMemoryAvailable|"
+  ];
+
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    libredirect.hook
+    iana-etc
+  ];
+  versionCheckProgramArg = "--version";
+  doInstallCheck = true;
+
+  passthru = {
+    updateScript = nix-update-script { };
   };
 
-  meta = with lib; {
+  meta = {
     description = "SCIP Code Intelligence Protocol CLI";
-    mainProgram = "scip";
     homepage = "https://github.com/sourcegraph/scip";
-    changelog = "https://github.com/sourcegraph/scip/blob/${src.rev}/CHANGELOG.md";
-    license = licenses.asl20;
+    changelog = "https://github.com/sourcegraph/scip/blob/${finalAttrs.src.tag}/CHANGELOG.md";
+    license = lib.licenses.asl20;
     maintainers = [ ];
+    mainProgram = "scip";
   };
-}
+})
