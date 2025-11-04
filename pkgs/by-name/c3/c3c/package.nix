f9f94c58b3e31d1637ba7eda1e2668079277d93f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-04 21:33:51 +0000

    c3c: 0.7.6 -> 0.7.7

--- pkgs/by-name/c3/c3c/package.nix
+++ pkgs/by-name/c3/c3c/package.nix
@@ -19,13 +19,13 @@ in
 llvmPackages.stdenv.mkDerivation (finalAttrs: {
 
   pname = "c3c${optionalString debug "-debug"}";
-  version = "0.7.6";
+  version = "0.7.7";
 
   src = fetchFromGitHub {
     owner = "c3lang";
     repo = "c3c";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VZFMRu3iI3L57cQDLeS1I+1GiIWxAjUnO/cXbISB7M8=";
+    hash = "sha256-k2XMPA+MCvPJKO3fJn/YLTkcdPFj4Hi4pEYzM4EMNUM=";
   };
 
   cmakeBuildType = if debug then "Debug" else "Release";
