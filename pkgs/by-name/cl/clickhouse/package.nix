5bf4fe545840c98b8ff6e5bd3151cc443d43ac01
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-11-04 01:05:25 +0100

    clickhouse, clickhouse-lts: fix versioning

--- pkgs/by-name/cl/clickhouse/package.nix
+++ pkgs/by-name/cl/clickhouse/package.nix
@@ -1,11 +1,6 @@
 import ./generic.nix {
   version = "25.9.4.58-stable";
+  rev = "08afb4f28eefe01513442c3647a2e00b703d5922";
   hash = "sha256-HRbqVSyDuvhkv0+PSgps9AXKdLlukrLA65OLx5gZ3c0=";
   lts = false;
-  nixUpdateExtraArgs = [
-    "--version-regex"
-    "^v?(.*-stable|.*-lts)$"
-    "--override-filename"
-    "pkgs/by-name/cl/clickhouse/package.nix"
-  ];
 }
