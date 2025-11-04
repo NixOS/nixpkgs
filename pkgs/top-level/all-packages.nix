d5047d48c4d09cdfc0d0dcc1aa9ab65fddda989e
rorosen <robert.rose@mailbox.org>
2025-11-04 23:58:30 +0100

    rke2_1_30: remove
    EOL at 2025-06-28
    https://www.suse.com/lifecycle#rke2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12164,7 +12164,6 @@ with pkgs;
       callPackage ../applications/networking/instant-messengers/ripcord/darwin.nix { };
 
   inherit (callPackage ../applications/networking/cluster/rke2 { })
-    rke2_1_30
     rke2_1_31
     rke2_1_32
     rke2_stable
