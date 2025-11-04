6e036ab2d13999d2764d4297b0d84d1c22735456
phanirithvij <phanirithvij2000@gmail.com>
2025-11-04 12:21:37 +0530

    asciicam: add phanirithvij as maintainer
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/as/asciicam/package.nix
+++ pkgs/by-name/as/asciicam/package.nix
@@ -26,7 +26,7 @@ buildGoModule {
     description = "Displays your webcam on the terminal";
     homepage = "https://github.com/muesli/asciicam";
     license = licenses.mit;
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ phanirithvij ];
     mainProgram = "asciicam";
   };
 }
