31224ed929267d80d9722be7dc87ad3ac82e0d48
Defelo <mail@defelo.de>
2025-11-04 15:09:17 +0100

    cargo-hack: use finalAttrs pattern

--- pkgs/by-name/ca/cargo-hack/package.nix
+++ pkgs/by-name/ca/cargo-hack/package.nix
@@ -4,12 +4,12 @@
   fetchCrate,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cargo-hack";
   version = "0.6.39";
 
   src = fetchCrate {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-0+2bgjCgkZA8oYo5jkykB2US+LVVNo1tVk4smxYB6f4=";
   };
 
@@ -22,11 +22,11 @@ rustPlatform.buildRustPackage rec {
     description = "Cargo subcommand to provide various options useful for testing and continuous integration";
     mainProgram = "cargo-hack";
     homepage = "https://github.com/taiki-e/cargo-hack";
-    changelog = "https://github.com/taiki-e/cargo-hack/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/taiki-e/cargo-hack/blob/v${finalAttrs.version}/CHANGELOG.md";
     license = with lib.licenses; [
       asl20 # or
       mit
     ];
     maintainers = [ ];
   };
-}
+})
