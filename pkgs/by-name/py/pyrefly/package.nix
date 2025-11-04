733d193c29ccb32d4ba2f9b1e244781e1ced1225
cybardev <50134239+cybardev@users.noreply.github.com>
2025-11-04 14:07:27 -0400

    pyrefly: 0.34.0 -> 0.43.1

--- pkgs/by-name/py/pyrefly/package.nix
+++ pkgs/by-name/py/pyrefly/package.nix
@@ -8,22 +8,33 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "pyrefly";
-  version = "0.34.0";
+  version = "0.43.1";
 
   src = fetchFromGitHub {
     owner = "facebook";
     repo = "pyrefly";
     tag = finalAttrs.version;
-    hash = "sha256-HPPDsvWEFfh/GNMUPiVjQr28YBBs2DACBGM3cxo5Nx4=";
+    hash = "sha256-KVeuDK5f0VIMnhAMJvGMJ08tHOuuIBDPrTqO1YjsHXI=";
   };
 
   buildAndTestSubdir = "pyrefly";
-  cargoHash = "sha256-46kcoBG/PWwf8VdlvLNzEhfYRTmmKi/uTjwFkl7Wozg=";
+  cargoHash = "sha256-Cc3bLBP9SxMbXQmJJVIfItOzy0iUkxLMgk4fbzNP1yw=";
 
   buildInputs = [ rust-jemalloc-sys ];
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   doInstallCheck = true;
+
+  # redirect tests writing to /tmp
+  preCheck = ''
+    export TMPDIR=$(mktemp -d)
+  '';
+
+  checkFlags = [
+    # FIX: tracking on https://github.com/facebook/pyrefly/issues/1667
+    "--skip=test::lsp::lsp_interaction::configuration::test_pythonpath_change"
+    "--skip=test::lsp::lsp_interaction::configuration::test_workspace_pythonpath_ignored_when_set_in_config_file"
+  ];
 
   # requires unstable rust features
   env.RUSTC_BOOTSTRAP = 1;
