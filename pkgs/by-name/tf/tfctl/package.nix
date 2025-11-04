0c54d6b4c4f4c302eebae0ad7ed1f6ac2c6c95b9
Peter Hansson <peter.hansson17@gmail.com>
2025-11-04 22:45:01 +0100

    pr: use short commit hash in BuildSHA ld flag

--- pkgs/by-name/tf/tfctl/package.nix
+++ pkgs/by-name/tf/tfctl/package.nix
@@ -5,7 +5,12 @@
   fetchFromGitHub,
   installShellFiles,
   kubectl,
+  writeScript,
 }:
+let
+  commitHash = "a53488094851773978301511922c2ced1397cdf9"; # matches tag release
+  shortCommitHash = builtins.substring 0 8 commitHash;
+in
 buildGoModule (finalAttrs: {
   pname = "tfctl";
   version = "0.15.1";
@@ -19,7 +24,7 @@ buildGoModule (finalAttrs: {
 
   subPackages = [ "cmd/tfctl" ];
   ldflags = [
-    "-X main.BuildSHA=${finalAttrs.src.rev}"
+    "-X main.BuildSHA=${shortCommitHash}"
     "-X main.BuildVersion=${finalAttrs.version}"
   ];
 
@@ -36,6 +41,22 @@ buildGoModule (finalAttrs: {
     done
 
     installShellCompletion tfctl.{bash,zsh,fish}
+  '';
+
+  passthru.updateScript = writeScript "update-tfctl" ''
+    #!/usr/bin/env nix-shell
+    #!nix-shell -i bash -p curl jq nix-update
+
+    set -eu -o pipefail
+
+    gh_metadata="$(curl -LsS https://api.github.com/repos/flux-iac/tofu-controller/tags)"
+    version="$(echo "$gh_metadata" | jq -r '.[] | .name' | sort --version-sort | tail -1)"
+    commit_hash="$(echo "$gh_metadata" | jq -r --arg ver "$version" '.[] | select(.name == $ver).commit.sha')"
+
+    filename="$(nix-instantiate --eval -E "with import ./. {}; (builtins.unsafeGetAttrPos \"version\" tfctl).file" | tr -d '"')"
+    sed -i "s/commitHash = \"[^\"]*\"/commitHash = \"$commit_hash\"/" $filename
+
+    nix-update tfctl
   '';
 
   meta = {
