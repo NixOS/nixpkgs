b976bd5c7d1325dcd7ce027f6f240a36fcc1aa06
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-04 02:35:31 +0000

    python3Packages.django-allauth: 65.12.0 -> 65.13.0

--- pkgs/development/python-modules/django-allauth/default.nix
+++ pkgs/development/python-modules/django-allauth/default.nix
@@ -41,7 +41,7 @@
 
 buildPythonPackage rec {
   pname = "django-allauth";
-  version = "65.12.0";
+  version = "65.13.0";
   pyproject = true;
 
   src = fetchFromGitea {
@@ -49,7 +49,7 @@ buildPythonPackage rec {
     owner = "allauth";
     repo = "django-allauth";
     tag = version;
-    hash = "sha256-LM9XU8oMzg2WlYnwPmmZY+8gzZWT1br2ciZ7gCTbH7I=";
+    hash = "sha256-Rg6hUTWTFIk1VdUuVqTLLbqTDaPE40tU3HOBkL0VyGo=";
   };
 
   nativeBuildInputs = [ gettext ];
