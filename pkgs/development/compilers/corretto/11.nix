24232f2eeaec7b371bc7e155834ca0822abdef8d
Rolf Schröder <rolf.schroeder@limbus-medtec.com>
2025-11-04 22:21:55 +0100

    corretto11: 11.0.26.4.1 -> 11.0.29.7.1
    Gradle7 is phased out in nixpkgs but corretto <= 21 does not support
    Gradle8. See https://github.com/corretto/corretto-21/issues/114#issuecomment-3198171683.
    Hence, ./corretto11-gradle8.patch is added. This patch is loosely based
    on https://github.com/corretto/corretto-jdk/pull/135.

--- pkgs/development/compilers/corretto/11.nix
+++ pkgs/development/compilers/corretto/11.nix
@@ -1,6 +1,6 @@
 {
   fetchFromGitHub,
-  gradle_7,
+  gradle_8,
   jdk11,
   lib,
   stdenv,
@@ -19,20 +19,25 @@ let
       testers
       ;
     jdk = jdk11;
-    gradle = gradle_7;
+    gradle = gradle_8;
     extraConfig = [
       # jdk11 is built with --disable-warnings-as-errors (see openjdk/11.nix)
       # because of several compile errors. We need to include this parameter for
       # Corretto, too.
       "--disable-warnings-as-errors"
     ];
-    version = "11.0.26.4.1";
+    version = "11.0.29.7.1";
     src = fetchFromGitHub {
       owner = "corretto";
       repo = "corretto-11";
       rev = version;
-      hash = "sha256-buJlSvmyOVeMwaP9oDcHhG+Sabr1exf0nRUt4O7MaIY=";
+      hash = "sha256-/VlV8tAo1deOZ5Trc4VlLNtpjWx352qUGZmfVbj7HuU=";
     };
   };
 in
-corretto
+corretto.overrideAttrs (oldAttrs: {
+  patches = (oldAttrs.patches or [ ]) ++ [
+    ./corretto11-gradle8.patch
+  ];
+
+})
