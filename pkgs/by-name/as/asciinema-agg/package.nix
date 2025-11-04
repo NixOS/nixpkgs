aeda960a06dca1e7139c590ff352da13d634f2aa
phanirithvij <phanirithvij2000@gmail.com>
2025-11-05 00:12:36 +0530

    asciinema-agg: add phanirithvij as maintainer
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/as/asciinema-agg/package.nix
+++ pkgs/by-name/as/asciinema-agg/package.nix
@@ -24,7 +24,7 @@ rustPlatform.buildRustPackage rec {
     homepage = "https://github.com/asciinema/agg";
     changelog = "https://github.com/asciinema/agg/releases/tag/${src.rev}";
     license = licenses.asl20;
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ phanirithvij ];
     mainProgram = "agg";
   };
 }
