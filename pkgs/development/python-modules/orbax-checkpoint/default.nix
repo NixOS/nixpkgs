d487c96354c52164c90205f7cd1a89026014c819
Gaetan Lepage <gaetan@glepage.com>
2025-11-05 16:17:08 +0000

    python3Packages.orbax-checkpoint: 0.11.26 -> 0.11.27
    Diff: https://github.com/google/orbax/compare/v0.11.26...v0.11.27
    
    Changelog: https://github.com/google/orbax/blob/v0.11.27/checkpoint/CHANGELOG.md

--- pkgs/development/python-modules/orbax-checkpoint/default.nix
+++ pkgs/development/python-modules/orbax-checkpoint/default.nix
@@ -37,14 +37,14 @@
 
 buildPythonPackage rec {
   pname = "orbax-checkpoint";
-  version = "0.11.26";
+  version = "0.11.27";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "orbax";
     tag = "v${version}";
-    hash = "sha256-CY5Bs/o8fU57QJETYnyJVkP7Y+cahpqZftyIJNU+GvU=";
+    hash = "sha256-KXEwUHJAFxeJTs3qKUXMCYO3t5hoPTjGay9GIOQIuCY=";
   };
 
   sourceRoot = "${src.name}/checkpoint";
@@ -94,6 +94,18 @@ buildPythonPackage rec {
     # AssertionError: 2 not greater than 2.0046136379241943
     "test_async_mkdir_parallel"
     "test_async_mkdir_sequential"
+
+    # AssertionError:
+    # "Handler type string "(?:__main__|orbax.checkpoint._src.handlers.handler_type_registry_test)\.TestHandler" not found in the registry."
+    # does not match
+    # "'Handler type string "handler_type_registry_test.TestHandler" not found in the registry.'"
+    "test_get_handler_type_not_found"
+    "test_no_typestr"
+    "test_register_duplicate_handler_type"
+
+    # AssertionError: False is not true
+    "test_register_and_get"
+    "test_register_different_modules"
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [
     # Probably failing because of a filesystem impurity
@@ -107,7 +119,19 @@ buildPythonPackage rec {
     # First from multiprocess_test, Second from orbax.checkpoint._src.testing.multiprocess_test.
     # Description from first occurrence: Number of processes to use.
     # https://github.com/google/orbax/issues/1580
+    "orbax/checkpoint/_src/testing/multiprocess_test.py"
     "orbax/checkpoint/experimental/emergency/"
+
+    # ValueError: Distributed system is not available; please initialize it via `jax.distributed.initialize()` at the start of your program.
+    "orbax/checkpoint/_src/handlers/array_checkpoint_handler_test.py"
+
+    # import file mismatch:
+    # imported module 'registry_test' has this __file__ attribute:
+    #   /build/source/checkpoint/orbax/checkpoint/experimental/v1/_src/layout/registry_test.py
+    # which is not the same as the test file we want to collect:
+    #   /build/source/checkpoint/orbax/checkpoint/experimental/v1/_src/serialization/registry_test.py
+    # HINT: remove __pycache__ / .pyc files and/or use a unique basename for your test file module
+    "orbax/checkpoint/experimental/v1/_src/serialization/registry_test.py"
 
     # E   FileNotFoundError: [Errno 2] No such file or directory:
     # '/build/absl_testing/DefaultSnapshotTest/runTest/root/path/to/source/data.txt'
@@ -121,6 +145,7 @@ buildPythonPackage rec {
     "orbax/checkpoint/_src/tree/parts_of_test.py"
     "orbax/checkpoint/_src/tree/structure_utils_test.py"
     "orbax/checkpoint/_src/tree/utils_test.py"
+    "orbax/checkpoint/checkpoint_manager_test.py"
     "orbax/checkpoint/single_host_test.py"
     "orbax/checkpoint/transform_utils_test.py"
   ];
