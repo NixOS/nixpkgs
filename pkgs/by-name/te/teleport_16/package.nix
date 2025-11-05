4346d33a0fb0f895857faabf0b39fff38acd23a7
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-11-05 23:03:19 +0100

    teleport_16: 16.5.16 -> 16.5.18; teleport_17: 17.7.5 -> 17.7.8; teleport_18: 18.2.2 -> 18.3.1
    This makes one of the applied patches obsolete for all major versions.
    
    Diffs:
    https://github.com/gravitational/teleport/compare/v16.5.16...v16.5.18
    https://github.com/gravitational/teleport/compare/v17.7.5...v17.7.8
    https://github.com/gravitational/teleport/compare/v18.2.2...v18.3.1
    (cherry picked from commit af8ff991c2fa73642dee8e336ac08e44b06a7ef4)

--- pkgs/by-name/te/teleport_16/package.nix
+++ pkgs/by-name/te/teleport_16/package.nix
@@ -6,10 +6,10 @@
   extPatches ? [ ],
 }:
 buildTeleport {
-  version = "16.5.15";
-  hash = "sha256-DqNG6gl+KdjSbkE9Bwum8az8cLCSOmZwo9xpuWafHCA=";
-  vendorHash = "sha256-sZvRKLF2iZ3UpgGNUPuWMT7VTpnDa2uU0d1XjDKSmdo=";
-  pnpmHash = "sha256-8xnH9PkKz77whtq/LVYUjyG1Z1reRtW03Gv8sZ/1vww=";
+  version = "16.5.18";
+  hash = "sha256-Dikw4y62V7S62K+8EqltXM4RRYPgE2Ad/kZrSS2TEDo=";
+  vendorHash = "sha256-mcDybNt7Mr0HJW272Ulj1oWlfsH2kEp7rNyeonoIjf8=";
+  pnpmHash = "sha256-V0R/i+oENGxlmq2Q6iwnikgBFVMWXph9WMStp3HTW34=";
   cargoHash = "sha256-04zykCcVTptEPGy35MIWG+tROKFzEepLBmn04mSbt7I=";
 
   wasm-bindgen-cli = wasm-bindgen-cli_0_2_95;
--- pkgs/by-name/te/teleport_18/package.nix
+++ pkgs/by-name/te/teleport_18/package.nix
@@ -7,10 +7,10 @@
 }:
 
 buildTeleport {
-  version = "18.2.0";
-  hash = "sha256-JWgGRv9pK76u7IxwqnBcuAI93XIKfIVvme7l+a/3J7c=";
-  vendorHash = "sha256-oPi/rIuwze2ZlyHfZ2MdDHHvdIaF2IZ2aklEVNRgoLo=";
-  pnpmHash = "sha256-wW4RT1uqOTpy8wKIsAOfIlxoOamTzPqEbFQRAub+sn4=";
+  version = "18.3.1";
+  hash = "sha256-HM0pu+3O7zoClH15YC0naxxmKJC9ngamnvraTosRqG0=";
+  vendorHash = "sha256-HyS0KKW7lyn3NzedxM4+UxFV9OnxgtFDMW5jkj3ir/8=";
+  pnpmHash = "sha256-/G6WYC3YGk8ma4hAI6sZhQ6KAh+u4CxQ7891fYj5bJg=";
   cargoHash = "sha256-ia4We4IfIkqz82aFMVvXdzjDXw0w+OJSPVdutfau6PA=";
 
   wasm-bindgen-cli = wasm-bindgen-cli_0_2_99;
