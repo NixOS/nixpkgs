de89173eead5242592a4e88f1da452b1f97d6afc
Nikisys <153714639+Nikisys@users.noreply.github.com>
2025-11-05 23:45:46 +0100

    Update package.nix

--- pkgs/by-name/lu/lunar-client/package.nix
+++ pkgs/by-name/lu/lunar-client/package.nix
@@ -6,7 +6,7 @@
 }:
 
 appimageTools.wrapType2 rec {
-  pname = "lunarclient-new";
+  pname = "lunarclient";
   version = "3.5.3";
 
   src = fetchurl {
@@ -33,7 +33,7 @@ appimageTools.wrapType2 rec {
     description = "Free Minecraft client with mods, cosmetics, and performance boost";
     homepage = "https://www.lunarclient.com/";
     license = with licenses; [ unfree ];
-    mainProgram = "lunarclient-new";
+    mainProgram = "lunarclient";
     maintainers = with maintainers; [
       Technical27
       surfaceflinger
