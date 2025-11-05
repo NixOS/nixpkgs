28c3d74d4ef050697b6cc18bfb532f117b73d5a0
Jamie Quigley <jamie@quigley.xyz>
2025-11-05 14:59:11 +0000

    dr14_tmeter: 1.0.16 -> unstable-20250927

--- pkgs/by-name/dr/dr14_tmeter/package.nix
+++ pkgs/by-name/dr/dr14_tmeter/package.nix
@@ -5,22 +5,23 @@
   pkgs,
 }:
 
-python3Packages.buildPythonApplication rec {
+python3Packages.buildPythonApplication {
   pname = "dr14_tmeter";
-  version = "1.0.16";
+  version = "unstable-20250927";
   format = "setuptools";
 
   disabled = !python3Packages.isPy3k;
 
   src = fetchFromGitHub {
-    owner = "simon-r";
+    owner = "hboetes";
     repo = "dr14_t.meter";
-    rev = "v${version}";
-    sha256 = "1nfsasi7kx0myxkahbd7rz8796mcf5nsadrsjjpx2kgaaw5nkv1m";
+    rev = "f9d62f60c30d9404d4c4b644931e76049332310c";
+    sha256 = "sha256-3z9Gi32aG6Tk9UHpfT1VqmBZpFJrlKB+NZFu3CH+18U=";
   };
 
   propagatedBuildInputs = with pkgs; [
     python3Packages.numpy
+    python3Packages.mutagen
     flac
     vorbis-tools
     ffmpeg
