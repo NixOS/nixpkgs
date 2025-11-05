74cfe4614562bff690aaf4ac5516bd552a00ed70
Zexin Yuan <git@yzx9.xyz>
2025-11-05 09:50:57 +0800

    taplo: add toml-test

--- pkgs/by-name/ta/taplo/package.nix
+++ pkgs/by-name/ta/taplo/package.nix
@@ -5,10 +5,16 @@
   fetchFromGitHub,
   pkg-config,
   openssl,
-  withLsp ? true,
   installShellFiles,
   versionCheckHook,
+
+  # passthru dependencies
   nix-update-script,
+  runCommand,
+  toml-test,
+
+  # Optional feature
+  withLsp ? true,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
@@ -52,7 +58,38 @@ rustPlatform.buildRustPackage (finalAttrs: {
   versionCheckProgramArg = "--version";
   doInstallCheck = true;
 
-  passthru.updateScript = nix-update-script { };
+  passthru = {
+    updateScript = nix-update-script { };
+
+    tests = {
+      toml-test =
+        let
+          # Unfortunately, taplo does not yet pass all toml-test v1.6.0 tests.
+          # Some of the failures are reported issues, others may not be.
+          # https://github.com/tamasfe/taplo/issues/486
+          skips = [
+            "valid/comment/nonascii"
+            "valid/datetime/edge"
+            "valid/key/quoted-unicode"
+            "valid/string/quoted-unicode"
+            "invalid/control/multi-cr"
+            "invalid/control/rawmulti-cr"
+            "invalid/table/super-twice"
+          ];
+        in
+        runCommand "taplo-toml-test"
+          {
+            nativeBuildInputs = [
+              finalAttrs.finalPackage
+              toml-test
+            ];
+          }
+          ''
+            toml-test taplo ${lib.concatMapStringsSep " " (a: "-skip ${a}") skips} -- toml-test
+            touch "$out"
+          '';
+    };
+  };
 
   meta = {
     description = "TOML toolkit written in Rust";
