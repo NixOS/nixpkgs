7d368a954b07dbfd82db98a50d22c5ed1789699a
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-06 08:49:10 +0100

    python313Packages.lsassy: modernize

--- pkgs/development/python-modules/lsassy/default.nix
+++ pkgs/development/python-modules/lsassy/default.nix
@@ -6,7 +6,6 @@
   netaddr,
   poetry-core,
   pypykatz,
-  pythonOlder,
   rich,
 }:
 
@@ -14,8 +13,6 @@ buildPythonPackage rec {
   pname = "lsassy";
   version = "3.1.15";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchFromGitHub {
     owner = "Hackndo";
@@ -47,8 +44,8 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python module to extract data from Local Security Authority Subsystem Service (LSASS)";
     homepage = "https://github.com/Hackndo/lsassy";
-    changelog = "https://github.com/Hackndo/lsassy/releases/tag/v${version}";
-    license = with licenses; [ mit ];
+    changelog = "https://github.com/Hackndo/lsassy/releases/tag/${src.tag}";
+    license = licenses.mit;
     maintainers = with maintainers; [ fab ];
     mainProgram = "lsassy";
   };
