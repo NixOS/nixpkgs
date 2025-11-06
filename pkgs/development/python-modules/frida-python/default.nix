7ab6b0f14e507c008f91a758b38d0650e0012684
axolord <24368475+Axolord@users.noreply.github.com>
2025-11-06 22:46:44 +0100

    python3Packages.frida-python: 17.4.2 -> 17.5.1

--- pkgs/development/python-modules/frida-python/default.nix
+++ pkgs/development/python-modules/frida-python/default.nix
@@ -5,7 +5,7 @@
   buildPythonPackage,
 }:
 let
-  version = "17.4.2";
+  version = "17.5.1";
   format = "wheel";
   inherit (stdenvNoCC.hostPlatform) system;
 
@@ -13,19 +13,19 @@ let
   pypiMeta =
     {
       x86_64-linux = {
-        hash = "sha256-GAqiJKtG6rf6nt7eDB3KJfPRD36drt4JMIvyzbwJkdI=";
+        hash = "sha256-gUYHLjKLoUI/aXzB+aU5yvfmObcA9hctkBqLgZxHvi0=";
         platform = "manylinux1_x86_64";
       };
       aarch64-linux = {
-        hash = "sha256-S3uJTIrZpaW4QV1Hm/k2tkbhdsAfoCa/WCmco5BwyCw=";
+        hash = "sha256-zU9mmxU8WWs2dG3lHnSiY0oTDN2tUFTMolV22SHtBbc=";
         platform = "manylinux2014_aarch64";
       };
       x86_64-darwin = {
-        hash = "sha256-wn5r0eIwvqE9P+hu1PqXNq3yKoBOVqM8pf0f3lTGN+o=";
+        hash = "sha256-hgsUQHS/DCVXSMm8ViC4zd/UVvEqYH7uwedv4ag48Mc=";
         platform = "macosx_10_13_x86_64";
       };
       aarch64-darwin = {
-        hash = "sha256-4QqlhB42JXroS6izkX5TzMSKjQ5nLxwYhi3BisYPpIA=";
+        hash = "sha256-HgjAVeACzWis0WFgf/D40GAqMbqSmZ8XBilgID3Yk5A=";
         platform = "macosx_11_0_arm64";
       };
     }
