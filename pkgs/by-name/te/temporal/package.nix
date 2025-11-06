949a3fe8f3a707b386d525b01e14a146f3457842
Jonathan Davies <jpds@protonmail.com>
2025-11-06 21:40:59 +0000

    temporal: Add versionCheckHook

--- pkgs/by-name/te/temporal/package.nix
+++ pkgs/by-name/te/temporal/package.nix
@@ -5,6 +5,7 @@
   nixosTests,
   testers,
   temporal,
+  versionCheckHook,
 }:
 
 buildGoModule rec {
@@ -46,6 +47,12 @@ buildGoModule rec {
 
     runHook postInstall
   '';
+
+  nativeInstallCheckInputs = [
+    versionCheckHook
+  ];
+  versionCheckProgramArg = "--version";
+  doInstallCheck = true;
 
   passthru.tests = {
     inherit (nixosTests) temporal;
