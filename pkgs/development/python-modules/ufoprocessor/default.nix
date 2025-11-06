3570896cecfd9e34d86149df6a0bfa24599e773f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-06 14:14:57 +0000

    python3Packages.ufoprocessor: 1.13.3 -> 1.14.1

--- pkgs/development/python-modules/ufoprocessor/default.nix
+++ pkgs/development/python-modules/ufoprocessor/default.nix
@@ -14,12 +14,12 @@
 
 buildPythonPackage rec {
   pname = "ufoprocessor";
-  version = "1.13.3";
+  version = "1.14.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1187g7xs6z8i2hzfkqhfd59qsdvzydqnmwhaz71nsi1zf5bw59gw";
+    sha256 = "sha256-/TjTzDWblBcbqNP9weTe/eIgas70+X11tIUDu4rAOwE=";
   };
 
   build-system = [ setuptools-scm ];
