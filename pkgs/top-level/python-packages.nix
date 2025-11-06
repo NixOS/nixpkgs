167a3af2f4218dd1221b8f5f161c05c8ed4f87e0
gesperon <gesperon@mac.com>
2025-11-06 13:34:08 +0100

    python3Packages.aiortc: init at 1.14.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -460,6 +460,8 @@ self: super: with self; {
 
   aiorpcx = callPackage ../development/python-modules/aiorpcx { };
 
+  aiortc = callPackage ../development/python-modules/aiortc { };
+
   aiortm = callPackage ../development/python-modules/aiortm { };
 
   aiortsp = callPackage ../development/python-modules/aiortsp { };
