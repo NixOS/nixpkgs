5eadc2199c0619e3ffa5402c0693d415aab80cbb
Guanran Wang <guanran928@outlook.com>
2025-11-06 13:16:00 +0800

    telegram-desktop: 6.2.3 -> 6.2.4

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
@@ -42,14 +42,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "telegram-desktop-unwrapped";
-  version = "6.2.3";
+  version = "6.2.4";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-ojhZgzxNXUkJaXoERuOti4E2vaGLsEoQp3eSuYtvhrY=";
+    hash = "sha256-Ku2fOmAX8qtB0YO/oA9zS/c3QAPFCHVJMXHNySIkIGI=";
   };
 
   nativeBuildInputs = [
