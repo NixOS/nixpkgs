6b8180b61235bd72209e6f289012bdb9588e3818
Sarah Clark <seclark@nextquestion.net>
2025-11-06 15:04:02 -0800

    python3Packages.kerbad: 0.4.4 -> 0.5.6-unstable-2025-10-07

--- pkgs/development/python-modules/kerbad/default.nix
+++ pkgs/development/python-modules/kerbad/default.nix
@@ -18,16 +18,16 @@
   unicrypto,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "kerbad";
-  version = "0.4.4";
+  version = "0.5.6-unstable-2025-10-07";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "CravateRouge";
     repo = "kerbad";
-    tag = version;
-    hash = "sha256-pnIn7UOpnCke6voFvOwcONXDd9i/di1lE/57vkg0/0w=";
+    rev = "3c2284de4d2390e22026b550705622ed39e5c05a"; # no tag available
+    hash = "sha256-V4KaF6lsECoLVpGZTZ4p7q9drHSsrsLPI/9zEQpqm3I=";
   };
 
   build-system = [ setuptools ];
@@ -52,7 +52,6 @@ buildPythonPackage rec {
   meta = {
     description = "Kerberos manipulation library in pure Python";
     homepage = "https://github.com/CravateRouge/kerbad";
-    changelog = "https://github.com/CravateRouge/kerbad/releases/tag/${src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
