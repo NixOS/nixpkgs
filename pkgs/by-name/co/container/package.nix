a5d629f035cc1fcb14e6bfaefc309e1a56d5a9d6
Giuseppe Sorrentino <uhpnou@gmail.com>
2025-11-07 13:15:26 +0100

    container: 0.5.0 -> 0.6.0

--- pkgs/by-name/co/container/package.nix
+++ pkgs/by-name/co/container/package.nix
@@ -11,11 +11,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "container";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchurl {
     url = "https://github.com/apple/container/releases/download/${finalAttrs.version}/container-${finalAttrs.version}-installer-signed.pkg";
-    hash = "sha256-YDh6bIWUc1muXqEqy0Z6gvLZHr3MQwgsuBHGVNY1ZDg=";
+    hash = "sha256-RGL6a8EY+rOC5Er9zHfbMrYHQY4LyApOUPcFP1haOGY=";
   };
 
   nativeBuildInputs = [
