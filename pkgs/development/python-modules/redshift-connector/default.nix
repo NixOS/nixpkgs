28f11e5316770efa1df306e3b5c78eadf647938d
Sarah Clark <seclark@nextquestion.net>
2025-11-06 17:07:54 -0800

    python3Packages.redshift-connector: disable broken tests

--- pkgs/development/python-modules/redshift-connector/default.nix
+++ pkgs/development/python-modules/redshift-connector/default.nix
@@ -60,6 +60,18 @@ buildPythonPackage rec {
   # integration tests require a Redshift cluster
   enabledTestPaths = [ "test/unit" ];
 
+  disabledTests = [
+    # AttributeError: 'itertools._tee' object has no attribute 'status_code'
+    # This is due to a broken pytest_mock.
+    # TODO Remove once pytest-mock 3.15.1 lands.
+    "test_form_based_authentication_uses_user_set_login_to_rp"
+    "test_form_based_authentication_payload_is_correct"
+    "test_form_based_authentication_login_fails_should_fail"
+    "test_azure_oauth_based_authentication_payload_is_correct"
+    "test_okta_authentication_payload_is_correct"
+    "test_set_cluster_identifier_calls_describe_custom_domain_associations"
+  ];
+
   __darwinAllowLocalNetworking = true; # required for tests
 
   meta = {
