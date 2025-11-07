5d9a6478569d6c745be570a119450ce0ea546cb0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-07 10:02:21 +0100

    psysh: unbreak, update `vendorHash`
    The vendorHash here is actually what would result if I ran
    `composer remove roave/security-advisories` in composerVendor's preBuild.
    (I cannot get a vendorHash otherwise, as the security advisory stops me
    from creating it.)
    
    (cherry picked from commit b42c44d76ca30d1a15ef90ccf31ef652e9f3e24d)

--- pkgs/by-name/ps/psysh/package.nix
+++ pkgs/by-name/ps/psysh/package.nix
@@ -40,12 +40,12 @@ php.buildComposerProject2 (finalAttrs: {
 
     preBuild = ''
       composer config platform.php 7.4
-      composer require --no-update symfony/polyfill-iconv:1.31 symfony/polyfill-mbstring:1.31
-      composer require --no-update --dev roave/security-advisories:dev-latest
-      composer update --lock --no-install
+      composer require --no-cache --no-update symfony/polyfill-iconv:1.31 symfony/polyfill-mbstring:1.31
+      composer require --no-cache --no-update --dev roave/security-advisories:dev-latest
+      composer update --no-cache --lock --no-install
     '';
 
-    vendorHash = "sha256-8l5bQ+VnLOtPUspMN1f+iXo7LldPTuYqyrAeW2aVoH8=";
+    vendorHash = "sha256-jKQyQSg08uDTO2RzQCQlKzNiYtXyqiHNdjO+TwIjeUY=";
   };
 
   doInstallCheck = true;
@@ -58,7 +58,5 @@ php.buildComposerProject2 (finalAttrs: {
     mainProgram = "psysh";
     license = lib.licenses.mit;
     homepage = "https://psysh.org/";
-    # `composerVendor` doesn't build
-    broken = true;
   };
 })
