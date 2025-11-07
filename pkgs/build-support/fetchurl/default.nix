331a06ce33ea33b9d4de48d11e2113dcac216238
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-11-07 18:22:51 +0100

    prefer-remote-fetch: fix fetchurl

--- pkgs/build-support/fetchurl/default.nix
+++ pkgs/build-support/fetchurl/default.nix
@@ -8,9 +8,13 @@
   cacert ? null,
   rewriteURL,
   hashedMirrors,
+
+  # Used in `pkgs/build-support/prefer-remote-fetch/default.nix`
+  preferLocalBuild ? true,
 }:
 
 let
+  cpPreferLocalBuild = preferLocalBuild;
 
   mirrors = import ./mirrors.nix // {
     inherit hashedMirrors;
@@ -135,7 +139,7 @@ lib.extendMkDerivation {
       passthru ? { },
       # Doing the download on a remote machine just duplicates network
       # traffic, so don't do that by default
-      preferLocalBuild ? true,
+      preferLocalBuild ? cpPreferLocalBuild,
 
       # Additional packages needed as part of a fetch
       nativeBuildInputs ? [ ],
