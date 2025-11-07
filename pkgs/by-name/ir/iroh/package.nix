f1eeb47d43398a86f0f3d2a5822854987fabb39a
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-07 06:52:22 +0000

    iroh: 0.93.2 -> 0.95.0

--- pkgs/by-name/ir/iroh/package.nix
+++ pkgs/by-name/ir/iroh/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "iroh";
-  version = "0.93.2";
+  version = "0.95.0";
 
   src = fetchFromGitHub {
     owner = "n0-computer";
     repo = "iroh";
     rev = "v${version}";
-    hash = "sha256-IYuOo4dfTC7IfMkwFyjqFmOYjx87i84+ydyNxnSAfk4=";
+    hash = "sha256-NuJvvnM8xrXb5Yhr0ODoczO7xb+xCwFSQ6lxCEDEgeQ=";
   };
 
-  cargoHash = "sha256-aR78AKfXRAePnOVO/Krx1WGcQgOIz3d+GDwfAoM10UQ=";
+  cargoHash = "sha256-SSFDHVvajjRfmFXkq5hT7gktKVNPYNzk84uzm1iLJLs=";
 
   # Some tests require network access which is not available in nix build sandbox.
   doCheck = false;
