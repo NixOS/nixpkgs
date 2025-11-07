37fecf866dcb1770182e5126651002ea8fc8dd93
Tom Hunze <dev@thunze.de>
2025-11-07 17:07:08 +0100

    quorum: drop
    The quorum package has been broken since October 2024 [1], upstream's
    most recent code change was in June 2024 [2], and the project
    website [3] returns an error.
    
    [1] https://hydra.nixos.org/build/311786489
    [2] https://github.com/Consensys/quorum/commits/master/
    [3] https://www.goquorum.com/

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1308,6 +1308,7 @@ mapAliases {
   quaternion-qt5 = throw "'quaternion-qt5' has been removed as quaternion dropped Qt5 support with v0.0.97.1"; # Added 2025-05-24
   qubes-core-vchan-xen = throw "'qubes-core-vchan-xen' has been removed because it has been marked as broken since at least November 2024."; # Added 2025-10-11
   quicksynergy = throw "'quicksynergy' has been removed due to lack of maintenance upstream. Consider using 'deskflow' instead."; # Added 2025-06-18
+  quorum = throw "'quorum' has been removed as it was broken and unmaintained upstream"; # Added 2025-11-07
   qv2ray = throw "'qv2ray' has been removed as it was unmaintained"; # Added 2025-06-03
   radicale3 = throw "'radicale3' has been renamed to/replaced by 'radicale'"; # Converted to throw 2025-10-27
   railway-travel = throw "'railway-travel' has been renamed to/replaced by 'diebahn'"; # Converted to throw 2025-10-27
