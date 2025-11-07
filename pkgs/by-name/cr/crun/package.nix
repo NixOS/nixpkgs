0c3aaaa549a1fe66a426d1899b3fff78128bbf2f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-07 14:46:37 +0000

    crun: 1.24 -> 1.25

--- pkgs/by-name/cr/crun/package.nix
+++ pkgs/by-name/cr/crun/package.nix
@@ -40,13 +40,13 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "crun";
-  version = "1.24";
+  version = "1.25";
 
   src = fetchFromGitHub {
     owner = "containers";
     repo = "crun";
     rev = version;
-    hash = "sha256-Sdp6ZxUzK8T7zfrgevrLxhMh7SQfO+6mABBiFMLbgh0=";
+    hash = "sha256-g+KN3UOxAj4nVaAwGGZjzBpAlPBmqMCQQFt3sDi6q90=";
     fetchSubmodules = true;
   };
 
