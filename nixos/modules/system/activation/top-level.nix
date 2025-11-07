767cd9d97820249e5db2d3c0c7ef350132c4afc3
Tristan Ross <tristan.ross@determinate.systems>
2025-11-07 10:52:52 -0800

    nixos/top-level: mark toplevel.meta.type as operating-system

--- nixos/modules/system/activation/top-level.nix
+++ nixos/modules/system/activation/top-level.nix
@@ -75,6 +75,8 @@ let
       nixosLabel = config.system.nixos.label;
 
       inherit (config.system) extraDependencies;
+
+      meta.type = [ lib.packageTypes.operating-system ];
     }
     // config.system.systemBuilderArgs
   );
