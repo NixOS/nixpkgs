b6df40f08f2cc1c58a31d55d92f07d4726193750
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-07 21:02:42 +0000

    gnome-extensions-cli: 0.10.6 -> 0.10.7

--- pkgs/desktops/gnome/misc/gnome-extensions-cli/default.nix
+++ pkgs/desktops/gnome/misc/gnome-extensions-cli/default.nix
@@ -15,13 +15,13 @@
 
 buildPythonApplication rec {
   pname = "gnome-extensions-cli";
-  version = "0.10.6";
+  version = "0.10.7";
   format = "pyproject";
 
   src = fetchPypi {
     pname = "gnome_extensions_cli";
     inherit version;
-    hash = "sha256-REsdgsHPYBms+qbOF4ogV8D/xi5fC9ogl+HOvnsXi7o=";
+    hash = "sha256-bn3Px3C1bTcV9N/idnjWKYPJmPo4e4mEQnFhVGkWMS4=";
   };
 
   nativeBuildInputs = [
