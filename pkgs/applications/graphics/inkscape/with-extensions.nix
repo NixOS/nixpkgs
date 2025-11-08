3f11be809b87897b3a8f7e08df6298cebe30d16a
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-11-08 23:26:08 +0100

    inkscape: add man output

--- pkgs/applications/graphics/inkscape/with-extensions.nix
+++ pkgs/applications/graphics/inkscape/with-extensions.nix
@@ -17,6 +17,11 @@ in
 symlinkJoin {
   name = "inkscape-with-extensions-${lib.getVersion inkscape}";
 
+  outputs = [
+    "out"
+    "man"
+  ];
+
   paths = [ inkscape ] ++ selectedExtensions;
 
   nativeBuildInputs = [ makeWrapper ];
@@ -24,6 +29,8 @@ symlinkJoin {
   postBuild = ''
     rm -f $out/bin/inkscape
     makeWrapper "${inkscape}/bin/inkscape" "$out/bin/inkscape" --set INKSCAPE_DATADIR "$out/share"
+
+    ln -s ${inkscape.man} $man
   '';
 
   inherit (inkscape) meta;
