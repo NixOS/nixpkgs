2323d6024806ad8905df518753129084cc79fc85
Ethan Thoma <ethanthoma@gmail.com>
2025-11-08 11:51:01 -0800

    python3Packages.streamingjson: init at 0.0.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19966,6 +19966,8 @@ self: super: with self; {
 
   streaming-form-data = callPackage ../development/python-modules/streaming-form-data { };
 
+  streamingjson = callPackage ../development/python-modules/streamingjson { };
+
   streamlabswater = callPackage ../development/python-modules/streamlabswater { };
 
   streamlit = callPackage ../development/python-modules/streamlit { };
