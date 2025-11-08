9eec9c1c29d6a0f98a1de2a840bd880c163c857d
Benjamin Sparks <b.sparks@alugha.com>
2025-11-08 13:24:38 +0100

    webcord(-vencord): bump electron to v37

--- pkgs/by-name/we/webcord-vencord/package.nix
+++ pkgs/by-name/we/webcord-vencord/package.nix
@@ -1,6 +1,6 @@
 {
   # allow overriding electron
-  electron_36,
+  electron_37,
   webcord,
   replaceVars,
   lib,
@@ -8,7 +8,7 @@
 }:
 
 # nixpkgs-update: no auto update
-(webcord.override { inherit electron_36; }).overrideAttrs (old: {
+(webcord.override { inherit electron_37; }).overrideAttrs (old: {
   pname = "webcord-vencord";
 
   patches = (old.patches or [ ]) ++ [
