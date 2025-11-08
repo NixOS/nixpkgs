92fe9345e507bfe47e7ed59237784fa1c1f43bd3
Adam Dinwoodie <adam@dinwoodie.org>
2025-11-08 17:20:29 +0000

    tests.fetchtorrent: store torrent file locally
    Avoid being unnecessarily dependent on webtorrent.io being available.

--- pkgs/build-support/fetchtorrent/tests.nix
+++ pkgs/build-support/fetchtorrent/tests.nix
@@ -32,7 +32,7 @@ let
   };
 
   # Via https://webtorrent.io/free-torrents
-  http.url = "https://webtorrent.io/torrents/sintel.torrent";
+  http.url = "${./test-sintel.torrent}";
   magnet.url = "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent";
 
   # Sintel isn't a massive download, but it's not small.  There's also no real
