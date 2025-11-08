8a036f3d17d6146a1753e48c5eba431173336dc1
Sarah Clark <seclark@nextquestion.net>
2025-11-08 12:34:23 -0800

    python3Packages.curl-cffi: pin websockets at 12.0

--- pkgs/development/python-modules/curl-cffi/default.nix
+++ pkgs/development/python-modules/curl-cffi/default.nix
@@ -1,5 +1,4 @@
 {
-  stdenv,
   lib,
   buildPythonPackage,
   fetchFromGitHub,
@@ -19,10 +18,30 @@
   python-multipart,
   trustme,
   uvicorn,
-  websockets,
   writableTmpDirAsHomeHook,
 }:
+let
+  # This is only used for testing and requires 12.0 specifically
+  # due to incompatible API changes in later versions.
+  websockets = buildPythonPackage rec {
+    pname = "websockets";
+    version = "12.0";
+    pyproject = true;
 
+    src = fetchFromGitHub {
+      owner = "aaugustin";
+      repo = "websockets";
+      tag = version;
+      hash = "sha256-sOL3VI9Ib/PncZs5KN4dAIHOrBc7LfXqT15LO4M6qKg=";
+    };
+
+    build-system = [ setuptools ];
+
+    doCheck = false;
+
+    pythonImportsCheck = [ "websockets" ];
+  };
+in
 buildPythonPackage rec {
   pname = "curl-cffi";
   version = "0.14.0b2";
