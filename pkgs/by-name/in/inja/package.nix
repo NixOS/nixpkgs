990ed34aa2055a208bfbe32c3d77e5cc41ff5645
Benjamin Sparks <b.sparks@alugha.com>
2025-11-08 14:45:45 +0100

    inja: 3.4.0 -> 3.5.0
    Fixes CMake4 compat.
    Also adds missing `python3` `nativeBuildInputs` required for `scripts/update_single_include.sh` to run

--- pkgs/by-name/in/inja/package.nix
+++ pkgs/by-name/in/inja/package.nix
@@ -3,6 +3,7 @@
   stdenv,
   fetchFromGitHub,
   cmake,
+  python3,
   nlohmann_json,
   doctest,
   callPackage,
@@ -10,19 +11,23 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "inja";
-  version = "3.4.0";
+  version = "3.5.0";
 
   src = fetchFromGitHub {
     owner = "pantor";
     repo = "inja";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-B1EaR+qN32nLm3rdnlZvXQ/dlSd5XSc+5+gzBTPzUZU=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-P4XKz2FcMfP0HRMoEC2+RKE/ljZSpusUTDmF9Ao5txo=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+    python3
+  ];
   propagatedBuildInputs = [ nlohmann_json ];
 
   cmakeFlags = [
+    (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10")
     (lib.cmakeBool "INJA_BUILD_TESTS" finalAttrs.finalPackage.doCheck)
     (lib.cmakeBool "INJA_USE_EMBEDDED_JSON" false)
     (lib.cmakeBool "BUILD_BENCHMARK" false)
