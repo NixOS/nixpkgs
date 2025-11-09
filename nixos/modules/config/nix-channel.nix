02742ffb70b66bd2ab3e38cfd0ed7084df52bbea
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-11-09 14:42:09 +0100

    nixos/nix-channel: default to channels.nixos.org
    Infra is interested in deprecating the redirect over the domain apex
    eventually, since it is just a redirect to channels.nixos.org, which
    we can more reliably redistribute with a CDN.
    
    The nixos.org domain currently offers no IPv6 due to issues we've had
    with our DNS hoster. (cf. nixos/infra#873)
    
    (cherry picked from commit 5d858facb2af0b04c90ab65cd1c72bb553bdcd45)

--- nixos/modules/config/nix-channel.nix
+++ nixos/modules/config/nix-channel.nix
@@ -71,7 +71,7 @@ in
       defaultChannel = mkOption {
         internal = true;
         type = types.str;
-        default = "https://nixos.org/channels/nixos-25.05";
+        default = "https://channels.nixos.org/nixos-25.05";
         description = "Default NixOS channel to which the root user is subscribed.";
       };
     };
