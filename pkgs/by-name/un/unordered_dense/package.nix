95bffe712f336fc064031f18c771490f3d952c70
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-09 16:39:06 +0000

    unordered_dense: 4.8.0 -> 4.8.1

--- pkgs/by-name/un/unordered_dense/package.nix
+++ pkgs/by-name/un/unordered_dense/package.nix
@@ -7,13 +7,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "unordered-dense";
-  version = "4.8.0";
+  version = "4.8.1";
 
   src = fetchFromGitHub {
     owner = "martinus";
     repo = "unordered_dense";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-irjzMx0QVE6W/Tg4TV+RNw1kD16yIJPR3sBseauu6AQ=";
+    hash = "sha256-JdPlyShWnAcdgixDHRaroFg7YWdPtD4Nl1PmpcQ1SAk=";
   };
 
   nativeBuildInputs = [
