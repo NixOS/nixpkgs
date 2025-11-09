bfa23cd37ec0541e238a3916ccbd7ed3c2cad418
tea <tea@teatwig.net>
2025-11-09 13:36:31 +0100

    wayfirePlugins.wwp-switcher: remove
    It will also FTBFS with Wayfire 0.10.

--- pkgs/applications/window-managers/wayfire/plugins.nix
+++ pkgs/applications/window-managers/wayfire/plugins.nix
@@ -14,11 +14,11 @@ lib.makeScope pkgs.newScope (
     wcm = callPackage ./wcm.nix { };
     wf-shell = callPackage ./wf-shell.nix { };
     windecor = callPackage ./windecor.nix { };
-    wwp-switcher = callPackage ./wwp-switcher.nix { };
   }
 )
 // lib.optionalAttrs config.allowAliases {
   firedecor = throw "wayfirePlugins.firedecor has been removed as it is unmaintained and no longer used by mate-wayland-session."; # Added 2025-09-03
   focus-request = throw "wayfirePlugins.focus-request is now included with wayfirePlugins.wayfire-plugins-extra";
   wayfire-shadows = throw "wayfirePlugins.wayfire-shadows is now included with wayfirePlugins.wayfire-plugins-extra";
+  wwp-switcher = throw "wayfirePlugins.wwp-switcher has been removed as it is unmaintained";
 }
