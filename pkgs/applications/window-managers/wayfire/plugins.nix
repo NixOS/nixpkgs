64bfbd50173f6e8e0c5912c4c4b3f35bd96dc9d3
tea <tea@teatwig.net>
2025-11-09 13:39:21 +0100

    wayfirePlugins.windecor: remove
    It will also FTBFS with Wayfire 0.10.

--- pkgs/applications/window-managers/wayfire/plugins.nix
+++ pkgs/applications/window-managers/wayfire/plugins.nix
@@ -13,12 +13,12 @@ lib.makeScope pkgs.newScope (
     wayfire-plugins-extra = callPackage ./wayfire-plugins-extra.nix { };
     wcm = callPackage ./wcm.nix { };
     wf-shell = callPackage ./wf-shell.nix { };
-    windecor = callPackage ./windecor.nix { };
   }
 )
 // lib.optionalAttrs config.allowAliases {
   firedecor = throw "wayfirePlugins.firedecor has been removed as it is unmaintained and no longer used by mate-wayland-session."; # Added 2025-09-03
   focus-request = throw "wayfirePlugins.focus-request is now included with wayfirePlugins.wayfire-plugins-extra";
   wayfire-shadows = throw "wayfirePlugins.wayfire-shadows is now included with wayfirePlugins.wayfire-plugins-extra";
+  windecor = throw "wayfirePlugins.windecor has been removed as it is unmaintained";
   wwp-switcher = throw "wayfirePlugins.wwp-switcher has been removed as it is unmaintained";
 }
