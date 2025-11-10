5de0c74967882eb4f211b38bbbc575a46ff77632
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-10 10:20:48 +0000

    home-assistant-custom-components.sun2: 3.4.3 -> 3.4.4b1

--- pkgs/servers/home-assistant/custom-components/sun2/package.nix
+++ pkgs/servers/home-assistant/custom-components/sun2/package.nix
@@ -7,13 +7,13 @@
 buildHomeAssistantComponent rec {
   owner = "pnbruckner";
   domain = "sun2";
-  version = "3.4.3";
+  version = "3.4.4b1";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "ha-sun2";
     tag = version;
-    hash = "sha256-UATxuKTusDkKLWwJq2CupPcF/TIKAmE5VPo3054cwKc=";
+    hash = "sha256-F7uiy/nwtgFrUq5MscfO+TJh5TFWULuTD2oDdJJdxhs=";
   };
 
   meta = rec {
