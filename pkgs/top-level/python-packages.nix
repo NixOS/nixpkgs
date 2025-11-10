8df57083ec5a0658e11e7d687dca0ac5432ea39f
Jared Baur <jaredbaur@fastmail.com>
2025-11-10 11:55:48 -0800

    peakrdl-cheader: init at 1.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11746,6 +11746,8 @@ self: super: with self; {
 
   peakrdl = callPackage ../development/python-modules/peakrdl { };
 
+  peakrdl-cheader = callPackage ../development/python-modules/peakrdl-cheader { };
+
   peakrdl-ipxact = callPackage ../development/python-modules/peakrdl-ipxact { };
 
   peakrdl-regblock = callPackage ../development/python-modules/peakrdl-regblock { };
