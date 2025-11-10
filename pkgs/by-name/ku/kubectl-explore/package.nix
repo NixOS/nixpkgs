a3d19bfcb025e41bcf7e7efd0ae366c14bb22f83
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-10 07:31:29 +0000

    kubectl-explore: 0.14.0 -> 0.14.1

--- pkgs/by-name/ku/kubectl-explore/package.nix
+++ pkgs/by-name/ku/kubectl-explore/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "kubectl-explore";
-  version = "0.14.0";
+  version = "0.14.1";
 
   src = fetchFromGitHub {
     owner = "keisku";
     repo = "kubectl-explore";
     rev = "v${version}";
-    hash = "sha256-URpoIK+5MgBvCtXyZrqwU7cVubCICkAsmfS9w/8Jgks=";
+    hash = "sha256-sWVNDXbGaqVEWkHnZbHy9W4lFH8N/aULIzBJLGSXCjs=";
   };
 
   vendorHash = "sha256-TgC8IgB9E83FBP9qrgcqPesnOyOTA5u3AsXn32kaMnU=";
