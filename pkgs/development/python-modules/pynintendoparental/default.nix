9fbda3fe80c0971179931b636a3038544a51aaf3
Robert Schütz <nix@dotlambda.de>
2025-11-09 20:11:39 -0800

    python3Packages.pynintendoparental: 1.1.3 -> 2.0.0
    Diff: https://github.com/pantherale0/pynintendoparental/compare/1.1.3...2.0.0
    
    Changelog: https://github.com/pantherale0/pynintendoparental/releases/tag/2.0.0

--- pkgs/development/python-modules/pynintendoparental/default.nix
+++ pkgs/development/python-modules/pynintendoparental/default.nix
@@ -3,26 +3,28 @@
   buildPythonPackage,
   fetchFromGitHub,
   lib,
+  pynintendoauth,
   python-dotenv,
   setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "pynintendoparental";
-  version = "1.1.3";
+  version = "2.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pantherale0";
     repo = "pynintendoparental";
     tag = version;
-    hash = "sha256-mH34BcbK3qyB2sAmVyAQz6GhI+xWAdRHagZzLVI9gr8=";
+    hash = "sha256-LtIMWh4igyrZ5WQQ9ylv6NRrHAJdGq0FONw4SdH0hYI=";
   };
 
   build-system = [ setuptools ];
 
   dependencies = [
     aiohttp
+    pynintendoauth
   ];
 
   pythonImportsCheck = [ "pynintendoparental" ];
