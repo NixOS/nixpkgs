30163f424c01fe1571b9eba1545deb6da2ea282b
Jared Baur <jaredbaur@fastmail.com>
2025-11-10 11:55:24 -0800

    peakrdl-ipxact: init at 3.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11872,6 +11872,8 @@ self: super: with self; {
 
   peakrdl = callPackage ../development/python-modules/peakrdl { };
 
+  peakrdl-ipxact = callPackage ../development/python-modules/peakrdl-ipxact { };
+
   peakrdl-regblock = callPackage ../development/python-modules/peakrdl-regblock { };
 
   peakutils = callPackage ../development/python-modules/peakutils { };
