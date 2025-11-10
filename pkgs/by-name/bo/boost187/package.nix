6e89e86259ea0a4e939272b5237600cab9935b8b
Boosted-Bonobo <boostedbonobo1@outlook.com>
2025-11-10 10:49:07 +0200

    boost187: move to by-name

---
+++ pkgs/by-name/bo/boost187/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  callPackage,
+  boost-build,
+  fetchurl,
+  ...
+}@args:
+
+lib.fix (
+  self:
+  let
+    boost = import ../boost/package.nix;
+  in
+  callPackage boost (
+    args
+    // {
+      version = "1.87.0";
+      src = fetchurl {
+        urls = [
+          "mirror://sourceforge/boost/boost_${builtins.replaceStrings [ "." ] [ "_" ] self.version}.tar.bz2"
+          "https://boostorg.jfrog.io/artifactory/main/release/${self.version}/source/boost_${
+            builtins.replaceStrings [ "." ] [ "_" ] self.version
+          }.tar.bz2"
+        ];
+        hash = "sha256-r1e+JctMT0tBPtaS/jeK/7Q1LqUPvilKEe9Uj01SfYk=";
+      };
+      boost-build = boost-build.override {
+        # useBoost allows us passing in src and version from
+        # the derivation we are building to get a matching b2 version.
+        useBoost = self;
+      };
+    }
+  )
+)
