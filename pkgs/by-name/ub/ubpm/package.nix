246fbf76dd8940fe27658c6c8fc652c9a56f7f6d
magicquark <198001825+magicquark@users.noreply.github.com>
2025-11-10 06:39:51 +0000

    ubpm: 0.13.0 -> 0.13.0-unstable-2025-10-18, migrate from `Qt5` to `Qt6`
    - Unstable tracking required for upstream fixes made for `Qt
      6.10`.

--- pkgs/by-name/ub/ubpm/package.nix
+++ pkgs/by-name/ub/ubpm/package.nix
@@ -2,21 +2,22 @@
   stdenv,
   lib,
   fetchFromGitea,
-  libsForQt5,
+  qt6,
   udev,
   pkg-config,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ubpm";
-  version = "1.13.0";
+  version = "1.13.0-unstable-2025-10-18";
+  baseVersion = lib.head (lib.splitString "-" finalAttrs.version);
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "LazyT";
     repo = "ubpm";
-    rev = finalAttrs.version;
-    hash = "sha256-lS9SVWTk7Obr3g9YsqJcEL+4dxzTQ+Z98C3lFEsn3Tw=";
+    rev = "748ce8504185ae96dbdbd1cff5352d1eef2c046d";
+    hash = "sha256-WSweHj4+qgjqEsn0TNtmbVXjFJD84EWkdqK44/CsqgQ=";
     fetchSubmodules = true;
   };
 
@@ -27,26 +28,27 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   nativeBuildInputs = [
-    libsForQt5.qmake
-    libsForQt5.qttools
-    libsForQt5.wrapQtAppsHook
+    qt6.qmake
+    qt6.qttools
+    qt6.wrapQtAppsHook
     pkg-config
   ];
 
   # *.so plugins are being wrapped automatically which breaks them
   dontWrapQtApps = true;
-
+  
   buildInputs = [
-    libsForQt5.qtbase
-    libsForQt5.qtserialport
-    libsForQt5.qtconnectivity
-    libsForQt5.qtcharts
+    qt6.qtbase
+    qt6.qtserialport
+    qt6.qtconnectivity
+    qt6.qtcharts
+    qt6.qtsvg
     udev
   ];
 
   meta = {
     homepage = "https://codeberg.org/LazyT/ubpm";
-    changelog = "https://codeberg.org/LazyT/ubpm/releases/tag/${finalAttrs.version}";
+    changelog = "https://codeberg.org/LazyT/ubpm/releases/tag/${finalAttrs.baseVersion}";
     description = "Universal Blood Pressure Manager";
     mainProgram = "ubpm";
     license = lib.licenses.gpl3Only;
