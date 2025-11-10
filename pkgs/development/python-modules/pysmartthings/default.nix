f7b0783d0b5cef8ecc72e7e4afd872736685e044
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-10 15:06:16 +0000

    python3Packages.pysmartthings: 3.3.1 -> 3.3.2

--- pkgs/development/python-modules/pysmartthings/default.nix
+++ pkgs/development/python-modules/pysmartthings/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pysmartthings";
-  version = "3.3.1";
+  version = "3.3.2";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "andrewsayre";
     repo = "pysmartthings";
     tag = "v${version}";
-    hash = "sha256-BSD/7eitZZ+9iL6IEOPfv+51CuV7wJZAlqHSVbPOvTY=";
+    hash = "sha256-8p9lEf+SoU1WRJxavUwUjlIKjQxcPyBsgVLcwv8XFHs=";
   };
 
   build-system = [ poetry-core ];
