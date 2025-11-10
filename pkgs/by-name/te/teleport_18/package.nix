e8c2647da25e9890a6fa120a467eb485541e78b3
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-11-10 16:08:40 +0100

    teleport_18: 18.3.1 -> 18.3.2
    Changelog: https://github.com/gravitational/teleport/releases/tag/v18.3.2
    Diff: https://github.com/gravitational/teleport/compare/v18.3.1...v18.3.2
    (cherry picked from commit c62e99a402fcb83e76bea0e3446c966f8b21a869)

--- pkgs/by-name/te/teleport_18/package.nix
+++ pkgs/by-name/te/teleport_18/package.nix
@@ -7,9 +7,9 @@
 }:
 
 buildTeleport {
-  version = "18.3.1";
-  hash = "sha256-HM0pu+3O7zoClH15YC0naxxmKJC9ngamnvraTosRqG0=";
-  vendorHash = "sha256-HyS0KKW7lyn3NzedxM4+UxFV9OnxgtFDMW5jkj3ir/8=";
+  version = "18.3.2";
+  hash = "sha256-BeYn/uq81t9aL3xfxc7TJlNhvqJ2MDzN2Cue2XeG7g8=";
+  vendorHash = "sha256-FSG/54vVFVWiJmlUYqS+3l2EoqxM9tUH91/Nap1p8nk=";
   pnpmHash = "sha256-6sThtwACNEdV0fleaQf3iMmFxPsd0AshYeYZUatFMcg=";
   cargoHash = "sha256-ia4We4IfIkqz82aFMVvXdzjDXw0w+OJSPVdutfau6PA=";
 
