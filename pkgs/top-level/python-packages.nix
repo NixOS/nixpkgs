c5c94172d9f1906d7790d0317a9b51af482ab260
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-10 22:58:05 +0100

    python3Packages.mkdocs-literate-nav: init at 0.6.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9555,6 +9555,8 @@ self: super: with self; {
 
   mkdocs-linkcheck = callPackage ../development/python-modules/mkdocs-linkcheck { };
 
+  mkdocs-literate-nav = callPackage ../development/python-modules/mkdocs-literate-nav { };
+
   mkdocs-macros-plugin = callPackage ../development/python-modules/mkdocs-macros-plugin { };
 
   mkdocs-macros-test = callPackage ../development/python-modules/mkdocs-macros-test { };
