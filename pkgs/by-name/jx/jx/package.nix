d8ac6488e46477a8c44dbb1b5efe07bfb41ba559
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-10 10:29:52 +0000

    jx: 3.16.26 -> 3.16.28

--- pkgs/by-name/jx/jx/package.nix
+++ pkgs/by-name/jx/jx/package.nix
@@ -9,13 +9,13 @@
 
 buildGoModule rec {
   pname = "jx";
-  version = "3.16.26";
+  version = "3.16.28";
 
   src = fetchFromGitHub {
     owner = "jenkins-x";
     repo = "jx";
     rev = "v${version}";
-    sha256 = "sha256-KydLlc2FaOVdnz3ZP6obR7KbdWK0I5WXTziKAH3/UtY=";
+    sha256 = "sha256-BcT0zb4/TQPr3iwr3E72hPYbh0Oy+QZFD9zcX5ipNMA=";
   };
 
   vendorHash = "sha256-YUFpTL4BXm1iZJAQcbJSXEUKT99IulxT6qz4mrbBDN0=";
