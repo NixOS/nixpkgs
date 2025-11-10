a37f87da30f66267b645ef7357626400859cd5ed
Acid Bong <acidbong@tilde.club>
2025-11-10 22:01:50 +0200

    alacritty-theme: remove unnecessary attrs
    - `dont*` are of no use: the source doesn't have Makefile or configure
    - `sourceRoot` is also unneeded: we can just install themes from
      `/themes`

--- pkgs/by-name/al/alacritty-theme/package.nix
+++ pkgs/by-name/al/alacritty-theme/package.nix
@@ -5,7 +5,7 @@
   stdenvNoCC,
 }:
 
-stdenvNoCC.mkDerivation (finalAttrs: {
+stdenvNoCC.mkDerivation {
   pname = "alacritty-theme";
   version = "0-unstable-2026-02-13";
 
@@ -17,14 +17,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     sparseCheckout = [ "themes" ];
   };
 
-  dontConfigure = true;
-  dontBuild = true;
   preferLocalBuild = true;
 
-  sourceRoot = "${finalAttrs.src.name}/themes";
   installPhase = ''
     runHook preInstall
-    install -Dt $out/share/alacritty-theme *.toml
+    install -Dt $out/share/alacritty-theme themes/*.toml
     runHook postInstall
   '';
 
@@ -39,4 +36,4 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     maintainers = [ lib.maintainers.nicoo ];
     platforms = lib.platforms.all;
   };
-})
+}
