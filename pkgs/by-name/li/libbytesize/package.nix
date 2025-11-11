f46ecab30917a11ce8dd2678b37ee9c7a8d32c5f
Lee Bousfield <ljbousfield@gmail.com>
2025-11-11 09:18:30 -0600

    libbytesize: adopt by PlasmaPower

--- pkgs/by-name/li/libbytesize/package.nix
+++ pkgs/by-name/li/libbytesize/package.nix
@@ -72,7 +72,7 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Tiny library providing a C 'class' for working with arbitrary big sizes in bytes";
     license = lib.licenses.lgpl2Plus;
     mainProgram = "bscalc";
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.PlasmaPower ];
     platforms = lib.platforms.linux;
   };
 })
