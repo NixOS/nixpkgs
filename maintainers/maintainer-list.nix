c20ef37b2e00604bdfe933cbc290459a643700f3
André Schröder <andre.schroeder@andresco.de>
2025-11-11 02:28:02 +0100

    maintainers: add schra

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23284,6 +23284,12 @@
     githubId = 5104601;
     name = "schnusch";
   };
+  schra = {
+    email = "andre.schroeder@andresco.de";
+    github = "schra";
+    githubId = 23171985;
+    name = "André Schröder";
+  };
   schrobingus = {
     email = "brent.monning.jr@gmail.com";
     name = "Brent Monning";
