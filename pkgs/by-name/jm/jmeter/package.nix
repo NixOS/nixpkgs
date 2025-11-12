65ae5e871286e9973887c17b0ae1f9d05e180c4b
Simon Gardling <titaniumtown@proton.me>
2025-11-12 11:45:55 -0500

    jmeter: fix mirror server classpath and re-enable tests
    Because we download the binary version of jmeter, there are hard-coded library versions,
    we can override these library versions in order to fix execution, especially in the test phase.
    
    This allows us to re-enable doInstallCheck and run the installCheckPhase once again.

--- pkgs/by-name/jm/jmeter/package.nix
+++ pkgs/by-name/jm/jmeter/package.nix
@@ -31,6 +31,24 @@ stdenv.mkDerivation rec {
       "keytool -genkey" \
       "${jre}/lib/openjdk/jre/bin/keytool -genkey"
 
+    # Fix mirror-server.sh classpath: discover actual jar names at build time
+    oro_jar=$(basename $(ls $out/lib/oro-*.jar))
+    slf4j_api_jar=$(basename $(ls $out/lib/slf4j-api-*.jar))
+    jcl_over_slf4j_jar=$(basename $(ls $out/lib/jcl-over-slf4j-*.jar))
+    log4j_slf4j_impl_jar=$(basename $(ls $out/lib/log4j-slf4j-impl-*.jar))
+    log4j_api_jar=$(basename $(ls $out/lib/log4j-api-*.jar))
+    log4j_core_jar=$(basename $(ls $out/lib/log4j-core-*.jar))
+    log4j_1_2_api_jar=$(basename $(ls $out/lib/log4j-1.2-api-*.jar))
+
+    substituteInPlace $out/bin/mirror-server.sh \
+      --replace "oro-2.0.8.jar" "$oro_jar" \
+      --replace "slf4j-api-1.7.25.jar" "$slf4j_api_jar" \
+      --replace "jcl-over-slf4j-1.7.25.jar" "$jcl_over_slf4j_jar" \
+      --replace "log4j-slf4j-impl-2.11.0.jar" "$log4j_slf4j_impl_jar" \
+      --replace "log4j-api-2.11.1.jar" "$log4j_api_jar" \
+      --replace "log4j-core-2.11.1.jar" "$log4j_core_jar" \
+      --replace "log4j-1.2-api-2.11.1.jar" "$log4j_1_2_api_jar"
+
     # Prefix some scripts with jmeter to avoid clobbering the namespace
     for i in heapdump.sh mirror-server mirror-server.sh shutdown.sh stoptest.sh create-rmi-keystore.sh; do
       mv $out/bin/$i $out/bin/jmeter-$i
@@ -42,11 +60,12 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/jmeter.sh --set JAVA_HOME "${jre}"
   '';
 
-  doInstallCheck = false; # NoClassDefFoundError: org/apache/logging/log4j/Level for tests
+  doInstallCheck = true;
 
   nativeCheckInputs = [ coreutils ];
 
   installCheckPhase = ''
+    # Test basic functionality
     $out/bin/jmeter --version 2>&1 | grep -q "${version}"
     $out/bin/jmeter-heapdump.sh > /dev/null
     $out/bin/jmeter-shutdown.sh > /dev/null
