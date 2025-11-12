efb32a1da51b484cb75ef630147fa641ced04afd
whispers <dawnofmidnight@duck.com>
2025-11-12 08:46:45 -0500

    tor: 0.4.8.16 -> 0.4.8.20
    Release announcement: https://forum.torproject.org/t/stable-release-0-4-8-20/20781
    Release notes: https://gitlab.torproject.org/tpo/core/tor/-/raw/release-0.4.8/ReleaseNotes
    
    Not-cherry-picked-because: major style updates to the derivation between
    25.05 and unstable causing significant merge conflicts

--- pkgs/tools/security/tor/default.nix
+++ pkgs/tools/security/tor/default.nix
@@ -50,11 +50,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "tor";
-  version = "0.4.8.16";
+  version = "0.4.8.20";
 
   src = fetchurl {
     url = "https://dist.torproject.org/${pname}-${version}.tar.gz";
-    sha256 = "sha256-ZUDdN3oSD7jn0nUwqjt/9yoPpbT2cP4dZMmHwc/TkMs=";
+    sha256 = "sha256-G7IjKM3R7pSGR7/O1XHvp4wS/FBkGHtB1SVAhbUoL6c=";
   };
 
   outputs = [
