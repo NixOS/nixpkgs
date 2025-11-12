88bc2fa602e68851ccc8c7e80aca908906a4004f
Robert Schütz <nix@dotlambda.de>
2025-11-11 23:15:10 -0800

    python3Packages.home-assistant-chip-wheels: clean up dependencies

--- pkgs/development/python-modules/home-assistant-chip-wheels/default.nix
+++ pkgs/development/python-modules/home-assistant-chip-wheels/default.nix
@@ -1,15 +1,11 @@
 {
   aiohttp,
   alive-progress,
-  appdirs,
-  appnope,
-  black,
   build,
   clang-tools,
   click,
   colorama,
   coloredlogs,
-  coverage,
   cryptography,
   debugpy,
   diskcache,
@@ -27,7 +23,6 @@
   libnl,
   mobly,
   mypy,
-  mypy-extensions,
   mypy-protobuf,
   ninja,
   openssl,
@@ -58,18 +53,12 @@
   sphinx-argparse,
   sphinx-design,
   stdenv,
-  stringcase,
   tabulate,
-  toml,
   tornado,
-  types-protobuf,
   types-pyyaml,
   types-requests,
-  types-setuptools,
   watchdog,
   websockets,
-  wheel,
-  yapf,
   zap-chip,
 }:
 
@@ -120,11 +109,8 @@ stdenv.mkDerivation rec {
     lark
     python-path
     setuptools
-    stringcase
     build
     pip-tools
-    black
-    yapf
   ];
 
   propagatedBuildInputs = [
@@ -209,13 +195,8 @@ stdenv.mkDerivation rec {
       dependencies = [
         aiohttp
         alive-progress
-        appdirs
-        appnope
-        black
-        build
         colorama
         coloredlogs
-        coverage
         click
         cryptography
         debugpy
@@ -228,11 +209,9 @@ stdenv.mkDerivation rec {
         lark
         mobly
         mypy
-        mypy-extensions
         mypy-protobuf
         packaging
         parameterized
-        pip-tools
         pkgconfig
         prompt-toolkit
         protobuf
@@ -248,23 +227,16 @@ stdenv.mkDerivation rec {
         python-path
         pyyaml
         requests
-        setuptools
         six
         sphinx
         sphinx-argparse
         sphinx-design
-        stringcase
         tabulate
-        toml
         tornado
-        types-protobuf
         types-pyyaml
         types-requests
-        types-setuptools
         watchdog
         websockets
-        wheel
-        yapf
       ];
       filterNull = list: lib.filter (dep: dep != null) list;
       toItem = dep: {
