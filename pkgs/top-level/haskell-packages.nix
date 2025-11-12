0040e74d13b68242816064b996ab8919c7adec8a
Alex Tunstall <alex@tunstall.xyz>
2025-11-12 01:37:46 +0000

    haskell.compiler.microhs-0_14_17_0: init

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -231,6 +231,10 @@ in
       microhs-0_14_15_0 = callPackage ../development/compilers/microhs/0.14.15.0.nix {
         inherit (bb.compiler) microhs-boot;
       };
+
+      microhs-0_14_17_0 = callPackage ../development/compilers/microhs/0.14.17.0.nix {
+        inherit (bb.compiler) microhs-boot;
+      };
     }
     // pkgs.lib.optionalAttrs config.allowAliases {
       ghc810 = throw "'haskell.compiler.ghc810' has been removed."; # Added 2025-09-07
@@ -343,6 +347,13 @@ in
       microhs-0_14_15_0 = callPackage ../development/haskell-modules {
         buildHaskellPackages = bh.packages.microhs-0_14_15_0;
         ghc = bh.compiler.microhs-0_14_15_0;
+        compilerConfig = callPackage ../development/haskell-modules/configuration-microhs.nix { };
+        packageSetConfig = bootstrapPackageSet;
+      };
+
+      microhs-0_14_17_0 = callPackage ../development/haskell-modules {
+        buildHaskellPackages = bh.packages.microhs-0_14_17_0;
+        ghc = bh.compiler.microhs-0_14_17_0;
         compilerConfig = callPackage ../development/haskell-modules/configuration-microhs.nix { };
         packageSetConfig = bootstrapPackageSet;
       };
