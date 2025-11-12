db2196874a80b5eeea1ed94f11e8ad2f3c1e0edf
Jasi <normalc@posteo.net>
2025-11-11 23:56:33 -0500

    emacs-small: init at 30.2
    Adds the "-small" variant of Emacs to nixpkgs to allow users to
    install a distribution of GNU Emacs with the least amount of features
    compiled in, essentially a "least means" GNU Emacs distribution. This
    is done with an additional `.overideAttrs` call to manually override
    the `nativeBuildInputs` and `buildInputs` attributes.
    
    The motivation for this change is to introduce a variant of Emacs that
    has a greatly reduced closure size, either for CI, portability and/or
    minimalism.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10853,6 +10853,7 @@ with pkgs;
     emacs30-gtk3
     emacs30-nox
     emacs30-pgtk
+    emacs30-small
 
     emacs30-macport
     ;
@@ -10862,6 +10863,7 @@ with pkgs;
   emacs-gtk = emacs30-gtk3;
   emacs-nox = emacs30-nox;
   emacs-pgtk = emacs30-pgtk;
+  emacs-small = emacs30-small;
 
   emacsPackagesFor =
     emacs:
