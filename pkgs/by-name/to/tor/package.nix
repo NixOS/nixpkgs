df64450f7f06efdecd3b96b72fc89e6daf427980
whispers <dawnofmidnight@duck.com>
2025-11-11 19:05:34 -0500

    tor: 0.4.8.19 -> 0.4.8.20
    Release announcement: https://forum.torproject.org/t/stable-release-0-4-8-20/20781
    Release notes: https://gitlab.torproject.org/tpo/core/tor/-/raw/release-0.4.8/ReleaseNotes

--- pkgs/by-name/to/tor/package.nix
+++ pkgs/by-name/to/tor/package.nix
@@ -46,11 +46,11 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "tor";
-  version = "0.4.8.19";
+  version = "0.4.8.20";
 
   src = fetchurl {
     url = "https://dist.torproject.org/tor-${finalAttrs.version}.tar.gz";
-    hash = "sha256-PLZJodM7pqZfEJ0iRTTpOq8KbehKWxy0sFS/oGu3T1o=";
+    hash = "sha256-G7IjKM3R7pSGR7/O1XHvp4wS/FBkGHtB1SVAhbUoL6c=";
   };
 
   outputs = [
