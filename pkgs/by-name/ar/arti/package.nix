b88fa69cbae2767f934f4e151599b4d8a6add4f8
whispers <dawnofmidnight@duck.com>
2025-11-12 11:23:53 -0500

    arti: enable `full` feature flag
    The `full` feature flag includes all stable and non-conflicting arti
    feature flags. Upstream notes that the primary downsides are increased
    memory usage during build and an increased binary size, both of which
    are acceptable for the nixpkgs.
    
    Some tests enabled by this feature flag do fail due to security checks
    that are incompatible with the nixpkgs build sandbox, so those do need
    to be disabled.

--- pkgs/by-name/ar/arti/package.nix
+++ pkgs/by-name/ar/arti/package.nix
@@ -39,9 +39,25 @@ rustPlatform.buildRustPackage (finalAttrs: {
     "arti"
   ];
 
+  # `full` includes all stable and non-conflicting feature flags. the primary
+  # downsides are increased binary size and memory usage for building, but
+  # those are acceptable for nixpkgs
+  buildFeatures = [ "full" ];
+
+  # several tests under `full` require access to internal types, which are
+  # currently marked as experimental for public usage.
+  checkFeatures = [
+    "full"
+    "experimental-api"
+  ];
+
   checkFlags = [
     # problematic test that hangs the build
     "--skip=reload_cfg::test::watch_single_file"
+
+    # some of the cli tests attempt to validate that the filesystem and build
+    # is securely configured, which is somewhat broken by the nix build sandbox
+    "--skip=cli_tests"
   ];
 
   nativeInstallCheckInputs = [
