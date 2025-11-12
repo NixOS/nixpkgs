95c86e120826b15c4a69089adbc6192045866114
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-12 20:00:32 +0000

    grpc-health-probe: 0.4.41 -> 0.4.42

--- pkgs/by-name/gr/grpc-health-probe/package.nix
+++ pkgs/by-name/gr/grpc-health-probe/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "grpc-health-probe";
-  version = "0.4.41";
+  version = "0.4.42";
 
   src = fetchFromGitHub {
     owner = "grpc-ecosystem";
     repo = "grpc-health-probe";
     rev = "v${version}";
-    hash = "sha256-jAaPdDWCtUj0f6ljxgL4xigo+1mHL6qVPkBkRdgaqVI=";
+    hash = "sha256-/7Xxti2QOClWRo6EwHRb369+x/NeT6LHhDDyIJSHv00=";
   };
 
-  vendorHash = "sha256-h1iIkd2cxhqYwFJLIJgexjLwKj1IwR8oYWPpbKGZTXw=";
+  vendorHash = "sha256-9NDSkfHUa6xfLByjtuDMir2UM5flaKhD6jZDa71D+0w=";
 
   meta = with lib; {
     description = "command-line tool to perform health-checks for gRPC applications";
