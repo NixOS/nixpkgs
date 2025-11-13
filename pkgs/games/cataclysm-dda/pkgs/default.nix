909ab6755593a724760a5e59e599c1df886d673e
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-11-13 17:27:22 -0500

    cataclysm.pkgs: Use lib.makeScope for pkgSet

--- pkgs/games/cataclysm-dda/pkgs/default.nix
+++ pkgs/games/cataclysm-dda/pkgs/default.nix
@@ -1,34 +1,20 @@
 {
   lib,
-  callPackage,
+  newScope,
   build ? null,
 }:
+lib.makeScope newScope (
+  self:
+  let
+    inherit (self) callPackage;
+  in
+  {
+    mod = { };
 
-let
-  pkgs = {
-    mod = {
-    };
-
-    soundpack = {
-    };
+    soundpack = { };
 
     tileset = {
       UndeadPeople = callPackage ./tilesets/UndeadPeople { };
     };
-  };
-
-  pkgs' = lib.mapAttrs (_: mods: lib.filterAttrs isAvailable mods) pkgs;
-
-  isAvailable =
-    _: mod:
-    if (build == null) then
-      true
-    else if build.isTiles then
-      mod.forTiles or false
-    else if build.isCurses then
-      mod.forCurses or false
-    else
-      false;
-in
-
-lib.makeExtensible (_: pkgs')
+  }
+)
