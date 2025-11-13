dcb249ba2429d1e00b913d8040221914f7f237b6
liberodark <liberodark@gmail.com>
2025-11-13 11:54:29 +0100

    {zabbix.agent,agent2,server,proxy,web}72: 7.2.10 -> 7.2.14

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -4,8 +4,8 @@ generic: {
     hash = "sha256-CNLVhNE5Cxzr8OUVKA6vNhFAXW/smGdpDkA4vSWcPvw=";
   };
   v72 = generic {
-    version = "7.2.10";
-    hash = "sha256-US+TP6qtCT3LlnELWR93t7nf8A1Y1xRPI+300GA1v8g=";
+    version = "7.2.14";
+    hash = "sha256-KgXP+c5x0R+dFBtPAJSirJVWmsy1awGWMfyEdQrqCVA=";
   };
   v70 = generic {
     version = "7.0.21";
