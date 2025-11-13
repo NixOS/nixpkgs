17f95b155ab3778805d0e0dc79751912fe099c04
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-11-13 18:23:57 -0500

    cataclysm.mkCataclysm: Add withMods helper to builder

--- pkgs/games/cataclysm-dda/default.nix
+++ pkgs/games/cataclysm-dda/default.nix
@@ -10,14 +10,14 @@ lib.makeScope newScope (
   in
   {
     dark-days-ahead =
-      self.attachPkgs self.pkgs (callPackage ./dda/stable.nix { })
+      (callPackage ./dda/stable.nix { })
       // (lib.optionalAttrs config.allowAliases {
         tiles = lib.warnOnInstantiate "'cataclysm.dark-days-ahead.tiles' is now accessed with the helper 'cataclysm.dark-days-ahead.withTiles'" self.dark-days-ahead.withTiles;
         curses = lib.warnOnInstantiate "'cataclysm.dark-days-ahead.curses ' is now named 'cataclysm.dark-days-ahead'" self.dark-days-ahead;
       });
 
     dark-days-ahead-unstable =
-      self.attachPkgs self.pkgs (callPackage ./dda/git.nix { })
+      (callPackage ./dda/git.nix { })
       // (lib.optionalAttrs config.allowAliases {
         tiles = lib.warnOnInstantiate "'cataclysm.dark-days-ahead-unstable.tiles' is now accessed with the helper 'cataclysm.dark-days-ahead-unstable.withTiles'" self.dark-days-ahead-unstable.withTiles;
         curses = lib.warnOnInstantiate "'cataclysm.dark-days-ahead-unstable.curses ' is now named 'cataclysm.dark-days-ahead-unstable'" self.dark-days-ahead-unstable;
@@ -27,36 +27,6 @@ lib.makeScope newScope (
     pkgs = callPackage ./pkgs { };
 
     wrapCDDA = callPackage ./wrapper.nix { };
-
-    # Required to fix `pkgs` and `withMods` attrs after applying `overrideAttrs`.
-    #
-    # Example:
-    #     let
-    #       myBuild = cataclysmDDA.jenkins.latest.tiles.overrideAttrs (_: {
-    #         x = "hello";
-    #       });
-    #
-    #       # This refers to the derivation before overriding! So, `badExample.x` is not accessible.
-    #       badExample = myBuild.withMods (_: []);
-    #
-    #       # `myBuild` is correctly referred by `withMods` and `goodExample.x` is accessible.
-    #       goodExample = let
-    #         inherit (cataclysmDDA) attachPkgs pkgs;
-    #       in
-    #       (attachPkgs pkgs myBuild).withMods (_: []);
-    #     in
-    #     goodExample.x  # returns "hello"
-    attachPkgs =
-      pkgs: super:
-      let
-        new = super.overrideAttrs (old: {
-          passthru = old.passthru // {
-            pkgs = pkgs.override { build = new; };
-            withMods = self.wrapCDDA new;
-          };
-        });
-      in
-      new;
   }
   // lib.optionalAttrs config.allowAliases {
     buildMod = lib.warn "'cataclysm.buildMod' has been moved to 'cataclysm.pkgs.buildMod'" self.pkgs.buildMod;
