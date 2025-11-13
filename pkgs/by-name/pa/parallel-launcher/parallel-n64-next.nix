6eec4125d6375fbed1baf32972689cd43f1505a3
WheelsForReals <WheelsForReals@proton.me>
2025-11-12 20:22:57 -0600

    parallel-launcher.parallel-n64-core: 2.24.1 -> 2.27.1

--- pkgs/by-name/pa/parallel-launcher/parallel-n64-next.nix
+++ pkgs/by-name/pa/parallel-launcher/parallel-n64-next.nix
@@ -15,13 +15,13 @@ in
 # Based on the libretro parallel-n64 derivation with slight tweaks
 libretro.mkLibretroCore (finalAttrs: {
   core = "parallel-n64-next";
-  version = "2.24.1";
+  version = "2.27.1";
 
   src = fetchFromGitLab {
     owner = "parallel-launcher";
     repo = "parallel-n64";
     tag = reformatVersion finalAttrs.version;
-    hash = "sha256-BeeKX78zozxx72cmJ3HI0nH/STvkltMBZs2+mb4ukM0=";
+    hash = "sha256-u4F6CbC1NEU3OWtcqMIi/teX+SS4Jq9v5M2qc9z5bXg=";
   };
 
   extraNativeBuildInputs = [
