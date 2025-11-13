5703b62538e63a87e8535c42506ed857b795c118
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-13 11:14:28 +0000

    unciv: 4.18.12 -> 4.18.13

--- pkgs/by-name/un/unciv/package.nix
+++ pkgs/by-name/un/unciv/package.nix
@@ -12,7 +12,7 @@
   nix-update-script,
 }:
 let
-  version = "4.18.12";
+  version = "4.18.13";
 
   desktopItem = makeDesktopItem {
     name = "unciv";
@@ -42,7 +42,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/yairm210/Unciv/releases/download/${version}/Unciv.jar";
-    hash = "sha256-h0Y7a/wvmYOwr+AhVjY+P7uQQQbofpqkX5/Nd8tL3Cg=";
+    hash = "sha256-YL5wehIFuRYCQikvVmQUruQvazrGZNq6KGtuJKUcapY=";
   };
 
   dontUnpack = true;
