5e457b1a2edba400caa12938eb1fa0bbb579d8cc
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-11-13 17:22:01 -0500

    cataclysm: Move lib functions to the pkgSet

--- pkgs/games/cataclysm-dda/default.nix
+++ pkgs/games/cataclysm-dda/default.nix
@@ -17,13 +17,48 @@ lib.makeScope newScope (
       curses = self.git.tiles.override { tiles = false; };
     };
 
-    inherit (callPackage ./lib.nix { })
-      buildMod
-      buildSoundPack
-      buildTileSet
-      wrapCDDA
-      attachPkgs
-      ;
     pkgs = callPackage ./pkgs { };
+
+    buildMod = callPackage ./builder.nix {
+      type = "mod";
+    };
+    buildSoundPack = callPackage ./builder.nix {
+      type = "soundpack";
+    };
+
+    buildTileSet = callPackage ./builder.nix {
+      type = "tileset";
+    };
+    wrapCDDA = callPackage ./wrapper.nix { };
+
+    # Required to fix `pkgs` and `withMods` attrs after applying `overrideAttrs`.
+    #
+    # Example:
+    #     let
+    #       myBuild = cataclysmDDA.jenkins.latest.tiles.overrideAttrs (_: {
+    #         x = "hello";
+    #       });
+    #
+    #       # This refers to the derivation before overriding! So, `badExample.x` is not accessible.
+    #       badExample = myBuild.withMods (_: []);
+    #
+    #       # `myBuild` is correctly referred by `withMods` and `goodExample.x` is accessible.
+    #       goodExample = let
+    #         inherit (cataclysmDDA) attachPkgs pkgs;
+    #       in
+    #       (attachPkgs pkgs myBuild).withMods (_: []);
+    #     in
+    #     goodExample.x  # returns "hello"
+    attachPkgs =
+      pkgs: super:
+      let
+        new = super.overrideAttrs (old: {
+          passthru = old.passthru // {
+            pkgs = pkgs.override { build = new; };
+            withMods = self.wrapCDDA new;
+          };
+        });
+      in
+      new;
   }
 )
