e4bbb49db914b96ad6d38fca1d7fe636c9d4ed31
Jonathan Davies <jpds@protonmail.com>
2025-11-13 09:30:31 +0000

    grpc-health-probe: Use finalAttrs

--- pkgs/by-name/gr/grpc-health-probe/package.nix
+++ pkgs/by-name/gr/grpc-health-probe/package.nix
@@ -5,14 +5,14 @@
   versionCheckHook,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "grpc-health-probe";
   version = "0.4.42";
 
   src = fetchFromGitHub {
     owner = "grpc-ecosystem";
     repo = "grpc-health-probe";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-/7Xxti2QOClWRo6EwHRb369+x/NeT6LHhDDyIJSHv00=";
   };
 
@@ -22,7 +22,7 @@ buildGoModule rec {
 
   ldflags = [
     "-w"
-    "-X main.versionTag=${version}"
+    "-X main.versionTag=${finalAttrs.version}"
   ];
 
   vendorHash = "sha256-9NDSkfHUa6xfLByjtuDMir2UM5flaKhD6jZDa71D+0w=";
@@ -35,9 +35,9 @@ buildGoModule rec {
   meta = with lib; {
     description = "command-line tool to perform health-checks for gRPC applications";
     homepage = "https://github.com/grpc-ecosystem/grpc-health-probe";
-    changelog = "https://github.com/grpc-ecosystem/grpc-health-probe/releases/tag/v${version}";
+    changelog = "https://github.com/grpc-ecosystem/grpc-health-probe/releases/tag/v${finalAttrs.version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ jpds ];
     mainProgram = "grpc-health-probe";
   };
-}
+})
