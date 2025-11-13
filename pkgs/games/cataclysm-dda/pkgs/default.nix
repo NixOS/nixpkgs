7de04ced5c846599545130c9d8fddb0ec5ab9470
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-11-13 17:53:36 -0500

    cataclysm.pkgs: Use Nixpkgs naming conventions

--- pkgs/games/cataclysm-dda/pkgs/default.nix
+++ pkgs/games/cataclysm-dda/pkgs/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   newScope,
+  config,
   build ? null,
 }:
 lib.makeScope newScope (
@@ -20,12 +21,26 @@ lib.makeScope newScope (
       assetType = "tileset";
     };
 
-    mod = { };
+    mods = { };
 
-    soundpack = { };
+    soundPacks = { };
 
-    tileset = {
-      UndeadPeople = callPackage ./tilesets/UndeadPeople { };
+    tileSets = {
+      undead-people = callPackage ./tilesets/UndeadPeople { };
+    }
+    // lib.optionalAttrs config.allowAliases {
+      UndeadPeople = lib.warnOnInstantiate "'cataclysm.pkgs.tileSets.UndeadPeople' has been renamed to 'cataclysm.pkgs.tileSets.undead-people'" self.tileSets.undead-people;
     };
   }
+  // (lib.optionalAttrs config.allowAliases {
+    mod = lib.mapAttrs (
+      _: lib.warn "'cataclysm.pkgs.mod' has been renamed to 'cataclysm.pkgs.mods'"
+    ) self.mods;
+    soundpack = lib.mapAttrs (
+      _: lib.warn "'cataclysm.pkgs.soundpack' has been renamed to 'cataclysm.pkgs.soundPacks'"
+    ) self.soundPacks;
+    tileset = lib.mapAttrs (
+      _: lib.warn "'cataclysm.pkgs.tileset' has been renamed to 'cataclysm.pkgs.tileSets'"
+    ) self.tileSets;
+  })
 )
