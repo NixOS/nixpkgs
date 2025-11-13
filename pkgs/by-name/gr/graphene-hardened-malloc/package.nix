6486c6e9ccee18e0e7a902af47a40f14ef841c6f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-13 01:45:42 +0000

    graphene-hardened-malloc: 2025092700 -> 2025110800

--- pkgs/by-name/gr/graphene-hardened-malloc/package.nix
+++ pkgs/by-name/gr/graphene-hardened-malloc/package.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "graphene-hardened-malloc";
-  version = "2025092700";
+  version = "2025110800";
 
   src = fetchFromGitHub {
     owner = "GrapheneOS";
     repo = "hardened_malloc";
     rev = finalAttrs.version;
-    hash = "sha256-t7PnBwpGh53+ZqTbnm8lYaNBtUgLev9kbvFlbfSCBrU=";
+    hash = "sha256-HYXkIwy2a56lfjGL7NnOEnZSoH0LEolXbi5RVRfgpM8=";
   };
 
   nativeCheckInputs = [ python3 ];
