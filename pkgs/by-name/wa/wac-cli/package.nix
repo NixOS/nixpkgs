c5082a82bbf80fbfcc3e00f43e032d06a9672507
Fea <feathecutie@disroot.org>
2025-11-14 01:28:55 +0100

    wac-cli: 0.7.0 -> 0.8.1

--- pkgs/by-name/wa/wac-cli/package.nix
+++ pkgs/by-name/wa/wac-cli/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "wac-cli";
-  version = "0.7.0";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "bytecodealliance";
     repo = "wac";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VJq7xWTQcvXSzwCqdU53GNAk778f/Xp0IAomsD3c8pQ=";
+    hash = "sha256-LZ8d6J9akOrXbmbQ6OOl2sTHYWvObRTyV2I8Uloa64M=";
   };
 
-  cargoHash = "sha256-connilUNS+BKdVXDPCSA+QY/DY3wVt+SzxGAto8eeZE=";
+  cargoHash = "sha256-CVNFPvcdGyndV4UURa/RY0g8J90qkp3RiuyZNOXRVIE=";
 
   passthru.updateScript = nix-update-script { };
 
