7cffc26be8b0c544012ed36cc39350c268288467
Leon Schumacher <leonsch@protonmail.com>
2025-11-14 09:09:59 +0100

    neo4j_4: init at 4.4.46

---
+++ pkgs/by-name/ne/neo4j_4/package.nix
@@ -0,0 +1,65 @@
+{
+  coreutils,
+  fetchzip,
+  findutils,
+  gawk,
+  lib,
+  makeWrapper,
+  openjdk11,
+  stdenvNoCC,
+  versionCheckHook,
+  which,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "neo4j";
+  version = "4.4.46";
+
+  src = fetchzip {
+    url = "https://neo4j.com/artifact.php?name=neo4j-community-${finalAttrs.version}-unix.tar.gz";
+    hash = "sha256-qeA6lP8gqvwtJSMdJgJ0Iy2MMrtGhhUIOa4A6traSNg=";
+  };
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/{bin,share/neo4j}
+    cp -R * $out/share/neo4j
+
+    for NEO4J_SCRIPT in neo4j neo4j-admin cypher-shell; do
+      f="$out/share/neo4j/bin/$NEO4J_SCRIPT"
+      patchShebangs "$f"
+      makeWrapper "$f" "$out/bin/$NEO4J_SCRIPT" \
+        --prefix PATH : ${
+          lib.makeBinPath [
+            coreutils
+            findutils
+            gawk
+            openjdk11
+            which
+          ]
+        } \
+        --set JAVA_HOME ${openjdk11}
+    done
+
+    runHook postInstall
+  '';
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
+  doInstallCheck = true;
+
+  meta = {
+    description = "Highly scalable, robust (fully ACID) native graph database";
+    homepage = "https://neo4j.com";
+    changelog = "https://neo4j.com/release-notes/database/neo4j-${finalAttrs.version}";
+    downloadPage = "https://neo4j.com/deployment-center";
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.unix;
+    maintainers = [ lib.maintainers.eleonora ];
+    mainProgram = "neo4j";
+  };
+})
