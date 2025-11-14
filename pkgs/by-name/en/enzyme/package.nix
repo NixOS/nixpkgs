4de09b231f6941ab45c6858f8c3ee4bf275a61b7
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-14 12:06:47 +0000

    enzyme: 0.0.208 -> 0.0.215

--- pkgs/by-name/en/enzyme/package.nix
+++ pkgs/by-name/en/enzyme/package.nix
@@ -7,13 +7,13 @@
 }:
 llvmPackages.stdenv.mkDerivation rec {
   pname = "enzyme";
-  version = "0.0.208";
+  version = "0.0.215";
 
   src = fetchFromGitHub {
     owner = "EnzymeAD";
     repo = "Enzyme";
     rev = "v${version}";
-    hash = "sha256-/WNiGeKoEvKYlXIK4cU6z5gFGujQbAE13qyvnhMniXc=";
+    hash = "sha256-XK3d47Q/6+sJ2RL+on483z9PvZrdaKxIT9/GUQuLPl8=";
   };
 
   postPatch = ''
