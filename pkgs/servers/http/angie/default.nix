682c0066cbb376faceffd5469a0aa515e9ba8f77
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-14 12:51:04 +0000

    angie: 1.10.2 -> 1.10.3

--- pkgs/servers/http/angie/default.nix
+++ pkgs/servers/http/angie/default.nix
@@ -9,12 +9,12 @@
 }@args:
 
 callPackage ../nginx/generic.nix args rec {
-  version = "1.10.2";
+  version = "1.10.3";
   pname = if withQuic then "angieQuic" else "angie";
 
   src = fetchurl {
     url = "https://download.angie.software/files/angie-${version}.tar.gz";
-    hash = "sha256-pcKrk33ySoDnhq9WOJIvRuqKc9FhQYPIyQKYrocwlLg=";
+    hash = "sha256-/4IOydx6yLDoc8Ut3EizQX9QVPFXtdLfXBuO1637IlY=";
   };
 
   configureFlags =
