baf8dce2d715f99fb93baea7804d6b2661f6c67d
dai <contact@daimond113.com>
2025-11-14 11:14:50 +0100

    rename exeNamePrefix to binaryPrefix

--- nixos/modules/services/databases/mysql.nix
+++ nixos/modules/services/databases/mysql.nix
@@ -14,7 +14,7 @@ let
   hasNotify = isMariaDB || (isOracle && lib.versionAtLeast cfg.package.version "8.0");
 
   isRenamedMariaDB = isMariaDB && lib.versionAtLeast cfg.package.version "10.5";
-  exeNamePrefix = if isRenamedMariaDB then "mariadb" else "mysql";
+  binaryPrefix = if isRenamedMariaDB then "mariadb" else "mysql";
 
   mysqldOptions = "--user=${cfg.user} --datadir=${cfg.dataDir} --basedir=${cfg.package}";
 
@@ -573,7 +573,7 @@ in
         fi
 
         # The last two environment variables are used for starting Galera clusters
-        exec ${cfg.package}/bin/${exeNamePrefix}d --defaults-file=/etc/my.cnf ${mysqldOptions} $_WSREP_NEW_CLUSTER $_WSREP_START_POSITION
+        exec ${cfg.package}/bin/${binaryPrefix}d --defaults-file=/etc/my.cnf ${mysqldOptions} $_WSREP_NEW_CLUSTER $_WSREP_START_POSITION
       '';
 
       postStart =
@@ -595,10 +595,10 @@ in
             # If MariaDB is used in an Galera cluster, we have to check if the sync is done,
             # or it will fail to init the database while joining, so we get in an broken non recoverable state
             # so we wait until we have an synced state
-            if ${cfg.package}/bin/${exeNamePrefix} -u ${superUser} -N -e "SHOW VARIABLES LIKE 'wsrep_on'" 2>/dev/null | ${lib.getExe' pkgs.gnugrep "grep"} -q 'ON'; then
+            if ${cfg.package}/bin/${binaryPrefix} -u ${superUser} -N -e "SHOW VARIABLES LIKE 'wsrep_on'" 2>/dev/null | ${lib.getExe' pkgs.gnugrep "grep"} -q 'ON'; then
               echo "Galera cluster detected, waiting for node to be synced..."
               while true; do
-                STATE=$(${cfg.package}/bin/${exeNamePrefix} -u ${superUser} -N -e "SHOW STATUS LIKE 'wsrep_local_state_comment'" | ${lib.getExe' pkgs.gawk "awk"} '{print $2}')
+                STATE=$(${cfg.package}/bin/${binaryPrefix} -u ${superUser} -N -e "SHOW STATUS LIKE 'wsrep_local_state_comment'" | ${lib.getExe' pkgs.gawk "awk"} '{print $2}')
                 if [ "$STATE" = "Synced" ]; then
                   echo "Node is synced"
                   break
@@ -618,7 +618,7 @@ in
                 if isMariaDB then "unix_socket" else "auth_socket"
               };"
                 echo "GRANT ALL PRIVILEGES ON *.* TO '${cfg.user}'@'localhost' WITH GRANT OPTION;"
-              ) | ${cfg.package}/bin/${exeNamePrefix} -u ${superUser} -N
+              ) | ${cfg.package}/bin/${binaryPrefix} -u ${superUser} -N
 
               ${lib.concatMapStrings (database: ''
                 # Create initial databases
@@ -639,7 +639,7 @@ in
                             cat ${database.schema}/mysql-databases/*.sql
                         fi
                       ''}
-                    ) | ${cfg.package}/bin/${exeNamePrefix} -u ${superUser} -N
+                    ) | ${cfg.package}/bin/${binaryPrefix} -u ${superUser} -N
                 fi
               '') cfg.initialDatabases}
 
@@ -650,7 +650,7 @@ in
                   echo "CREATE USER '${cfg.replication.masterUser}'@'${cfg.replication.slaveHost}' IDENTIFIED WITH mysql_native_password;"
                   echo "SET PASSWORD FOR '${cfg.replication.masterUser}'@'${cfg.replication.slaveHost}' = PASSWORD('${cfg.replication.masterPassword}');"
                   echo "GRANT REPLICATION SLAVE ON *.* TO '${cfg.replication.masterUser}'@'${cfg.replication.slaveHost}';"
-                ) | ${cfg.package}/bin/${exeNamePrefix} -u ${superUser} -N
+                ) | ${cfg.package}/bin/${binaryPrefix} -u ${superUser} -N
               ''}
 
               ${lib.optionalString (cfg.replication.role == "slave") ''
@@ -659,14 +659,14 @@ in
                 ( echo "STOP SLAVE;"
                   echo "CHANGE MASTER TO MASTER_HOST='${cfg.replication.masterHost}', MASTER_USER='${cfg.replication.masterUser}', MASTER_PASSWORD='${cfg.replication.masterPassword}';"
                   echo "START SLAVE;"
-                ) | ${cfg.package}/bin/${exeNamePrefix} -u ${superUser} -N
+                ) | ${cfg.package}/bin/${binaryPrefix} -u ${superUser} -N
               ''}
 
               ${lib.optionalString (cfg.initialScript != null) ''
                 # Execute initial script
                 # using toString to avoid copying the file to nix store if given as path instead of string,
                 # as it might contain credentials
-                cat ${toString cfg.initialScript} | ${cfg.package}/bin/${exeNamePrefix} -u ${superUser} -N
+                cat ${toString cfg.initialScript} | ${cfg.package}/bin/${binaryPrefix} -u ${superUser} -N
               ''}
 
               rm ${cfg.dataDir}/mysql_init
@@ -677,7 +677,7 @@ in
             ${lib.concatMapStrings (database: ''
               echo "CREATE DATABASE IF NOT EXISTS \`${database}\`;"
             '') cfg.ensureDatabases}
-            ) | ${cfg.package}/bin/${exeNamePrefix} -N
+            ) | ${cfg.package}/bin/${binaryPrefix} -N
           ''}
 
           ${lib.concatMapStrings (user: ''
@@ -689,7 +689,7 @@ in
                   echo "GRANT ${permission} ON ${database} TO '${user.name}'@'localhost';"
                 '') user.ensurePermissions
               )}
-            ) | ${cfg.package}/bin/${exeNamePrefix} -N
+            ) | ${cfg.package}/bin/${binaryPrefix} -N
           '') cfg.ensureUsers}
         '';
 
