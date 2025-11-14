868af2fdcb0fd68a9508fd043aa83fe805d7c755
Safenein <safenein@cavernum.ovh>
2025-11-14 12:19:50 +0100

    zuban: 0.1.0 -> 0.3.0 and typeshed fix
    wrapping is no longer necessary, and so is removed.
    thanks to bew and mirkolenz and #16 (zubanls/zuban).
    
    Signed-off-by: Safenein <safenein@cavernum.ovh>

--- pkgs/by-name/zu/zuban/package.nix
+++ pkgs/by-name/zu/zuban/package.nix
@@ -4,22 +4,29 @@
   rustPlatform,
   versionCheckHook,
   nix-update-script,
+  python3,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zuban";
 
-  version = "0.1.0";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "zubanls";
     repo = "zuban";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-nSQf3I9O5TP1V8kwJrcBRREqS/47UlILx3IZMmt5ljQ=";
+    hash = "sha256-LHIrIO9ew5iXgem9W7QkPGic8XH6fQymLrbvQbmkB0M=";
+    fetchSubmodules = true;
   };
+
+  postInstall = ''
+    mkdir -p $out/${python3.sitePackages}/zuban
+    cp -r third_party $out/${python3.sitePackages}/zuban/
+  '';
 
   buildAndTestSubdir = "crates/zuban";
 
-  cargoHash = "sha256-Q09ZUBVa52fXIKiL6aC9VZB+4Rt/hI045CIjb/t3Xyg=";
+  cargoHash = "sha256-y3lqa+WWY8+KG59DzGdpiBLMybeqeN1fMAUMFidHX3Q=";
 
   nativeInstallCheckInputs = [
     versionCheckHook
