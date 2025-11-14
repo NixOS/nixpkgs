c399d156aefbad911330b488b6761bb5d3b89162
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-11-15 00:20:32 +0100

    python3Packages.rdflib: 7.2.1 -> 7.4.0
    https://github.com/RDFLib/rdflib/compare/7.2.1...7.4.0

--- pkgs/development/python-modules/rdflib/default.nix
+++ pkgs/development/python-modules/rdflib/default.nix
@@ -3,7 +3,6 @@
   stdenv,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch,
   pythonOlder,
 
   # builds
@@ -28,23 +27,15 @@
 
 buildPythonPackage rec {
   pname = "rdflib";
-  version = "7.2.1";
+  version = "7.4.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "RDFLib";
     repo = "rdflib";
     tag = version;
-    hash = "sha256-FisMiBTiL6emJS0d7UmlwGUzayA+CME5GGWgw/owfhc=";
+    hash = "sha256-/FEwNeNydhdW4xCKmLhh9tkXGcUAhO+D5N/b9pY6jh4=";
   };
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/RDFLib/rdflib/commit/0ab817f86b5733c9a3b4ede7ef065b8d79e53fc5.diff";
-      hash = "sha256-+yWzQ3MyH0wihgiQRMMXV/FpG8WlXaIBhpsDF4e3rbY=";
-    })
-  ];
 
   build-system = [ poetry-core ];
 
