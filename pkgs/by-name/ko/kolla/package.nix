44ea6a35b3d66eb827b1ece0a84a8051f5725e0b
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-15 14:56:44 +0000

    kolla: 20.2.0 -> 20.3.0

--- pkgs/by-name/ko/kolla/package.nix
+++ pkgs/by-name/ko/kolla/package.nix
@@ -7,14 +7,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "kolla";
-  version = "20.2.0";
+  version = "20.3.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "openstack";
     repo = "kolla";
     tag = version;
-    hash = "sha256-aqYEzBphzwB42Z0HmnCPQlV71dOi3yLKh+zL2dMTaB8=";
+    hash = "sha256-DLoQkI2cWaH+LYA97/ramOb2bAeZ8kRX386QX8WEivI=";
   };
 
   postPatch = ''
