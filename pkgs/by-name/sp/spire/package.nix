594ae4e59b759853953d8709d9130505987c9b00
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-15 18:11:36 +0000

    spire: 1.13.2 -> 1.13.3

--- pkgs/by-name/sp/spire/package.nix
+++ pkgs/by-name/sp/spire/package.nix
@@ -7,7 +7,7 @@
 
 buildGoModule (finalAttrs: {
   pname = "spire";
-  version = "1.13.2";
+  version = "1.13.3";
 
   outputs = [
     "out"
@@ -20,12 +20,12 @@ buildGoModule (finalAttrs: {
     owner = "spiffe";
     repo = "spire";
     tag = "v${finalAttrs.version}";
-    sha256 = "sha256-iZMeD5ZwWKjY9mfuXgEgh+QLotmv28T8xBgpKoQTgxw=";
+    sha256 = "sha256-Qph36DNnhZbO+bn1WnvBkWOWeCSusC0vrvZV1G32kFw=";
   };
 
   # Needed for github.co/google/go-tpm-tools/simulator  which contains non-go files that `go mod vendor` strips
   proxyVendor = true;
-  vendorHash = "sha256-nslLp/NjzsN1hSMMga67T6tMGLiqBNYQMt4Kjtwyvoc=";
+  vendorHash = "sha256-hkUA9L4lTSv7s/HtD1XOf07Hhk7ob2n/GCS+LnOSasI=";
 
   buildInputs = [ openssl ];
 
