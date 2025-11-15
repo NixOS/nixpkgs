66effdc14fe8753f3928743f5f05a30b842b076a
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-15 16:04:21 +0000

    jailer: 16.9.3 -> 16.11

--- pkgs/by-name/ja/jailer/package.nix
+++ pkgs/by-name/ja/jailer/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "jailer";
-  version = "16.9.3";
+  version = "16.11";
 
   src = fetchFromGitHub {
     owner = "Wisser";
     repo = "Jailer";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-4uXJZZ528Bc4vL8NUQDjoWGPBu/Bd9jhOMxC3e+s9hc=";
+    hash = "sha256-bSwLxO7UtKv82WoF/OcFROtbLfkdeupLtoR2/ELzO1U=";
   };
 
   nativeBuildInputs = [
