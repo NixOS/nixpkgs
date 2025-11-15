e20bc660ea2d5d66ee98fa024f98c4a20fdf0b87
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-15 11:42:39 +0100

    python313Packages.aiohwenergy: modernize

--- pkgs/development/python-modules/aiohwenergy/default.nix
+++ pkgs/development/python-modules/aiohwenergy/default.nix
@@ -3,24 +3,24 @@
   aiohttp,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "aiohwenergy";
   version = "0.8.0";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.7";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "DCSBL";
     repo = "aiohwenergy";
-    rev = version;
+    tag = version;
     hash = "sha256-WfkwIxyDzLNzhWNWST/V3iN9Bhu2oXDwGiA5UXCq5ho=";
   };
 
-  propagatedBuildInputs = [ aiohttp ];
+  build-system = [ setuptools ];
+
+  dependencies = [ aiohttp ];
 
   # Project has no tests
   doCheck = false;
@@ -30,6 +30,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library to interact with the HomeWizard Energy devices API";
     homepage = "https://github.com/DCSBL/aiohwenergy";
+    changelog = "https://github.com/DCSBL/aiohwenergy/releases/tag/${src.tag}";
     license = licenses.asl20;
     maintainers = with maintainers; [ fab ];
   };
