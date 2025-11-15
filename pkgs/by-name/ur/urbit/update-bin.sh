e4c0890dfe4355718405080b84be0c0c6e153f76
Michael Livshin <mlivshin@gmail.com>
2025-11-15 19:15:49 +0200

    urbit: 3.5 -> 4.2

--- pkgs/by-name/ur/urbit/update-bin.sh
+++ pkgs/by-name/ur/urbit/update-bin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p curl common-updater-scripts nix-prefetch
+#!nix-shell -i bash -p curl
 
 set -euo pipefail
 
@@ -10,31 +10,26 @@ if [ ! -f "$NIX_DRV" ]; then
   exit 1
 fi
 
-fetch_arch() {
-  VER="$1"; ARCH="$2"
-  URL="https://github.com/urbit/vere/releases/download/vere-v${VER}/${ARCH}.tgz";
-  nix-prefetch "{ stdenv, fetchzip }:
-stdenv.mkDerivation rec {
-  pname = \"vere\"; version = \"${VER}\";
-  src = fetchzip { url = \"$URL\"; };
-}
-"
+VERSION=$(curl https://bootstrap.urbit.org/vere/live/last)
+
+prefetch() {
+  nix-prefetch-url "https://github.com/urbit/vere/releases/download/vere-v${VERSION}/$1.tgz"
 }
 
-replace_sha() {
-  sed -i "s#$1 = \"sha256-.\{44\}\"#$1 = \"$2\"#" "$NIX_DRV"
+LINUX_AARCH64_SHA256=$(prefetch "linux-aarch64")
+LINUX_X64_SHA256=$(prefetch "linux-x86_64")
+DARWIN_AARCH64_SHA256=$(prefetch "macos-aarch64")
+DARWIN_X64_SHA256=$(prefetch "macos-x86_64")
+
+sed -i "s/version = \".*\"/version = \"$VERSION\"/" "$NIX_DRV"
+
+replace-sha256() {
+  local platform=$1
+  local sha256=$2
+  sed -i "s#${platform} = \"[^\"]*\"#${platform} = \"${sha256}\"#" "$NIX_DRV"
 }
 
-VERE_VER=$(curl https://bootstrap.urbit.org/vere/live/last)
-
-VERE_LINUX_AARCH64_SHA256=$(fetch_arch "$VERE_VER" "linux-aarch64")
-VERE_LINUX_X64_SHA256=$(fetch_arch "$VERE_VER" "linux-x86_64")
-VERE_DARWIN_AARCH64_SHA256=$(fetch_arch "$VERE_VER" "macos-aarch64")
-VERE_DARWIN_X64_SHA256=$(fetch_arch "$VERE_VER" "macos-x86_64")
-
-sed -i "s/version = \".*\"/version = \"$VERE_VER\"/" "$NIX_DRV"
-
-replace_sha "aarch64-linux" "$VERE_LINUX_AARCH64_SHA256"
-replace_sha "x86_64-linux" "$VERE_LINUX_X64_SHA256"
-replace_sha "aarch64-darwin" "$VERE_DARWIN_AARCH64_SHA256"
-replace_sha "x86_64-darwin" "$VERE_DARWIN_X64_SHA256"
+replace-sha256 "aarch64-linux" "$LINUX_AARCH64_SHA256"
+replace-sha256 "x86_64-linux" "$LINUX_X64_SHA256"
+replace-sha256 "aarch64-darwin" "$DARWIN_AARCH64_SHA256"
+replace-sha256 "x86_64-darwin" "$DARWIN_X64_SHA256"
