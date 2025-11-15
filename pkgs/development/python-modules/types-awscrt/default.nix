6110f58941a9b350fa58571749f6b3783d47b790
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-15 23:18:35 +0100

    python313Packages.types-awscrt: 0.28.2 -> 0.28.4
    Changelog: https://github.com/youtype/types-awscrt/releases/tag/0.28.4

--- pkgs/development/python-modules/types-awscrt/default.nix
+++ pkgs/development/python-modules/types-awscrt/default.nix
@@ -8,7 +8,7 @@
 
 buildPythonPackage rec {
   pname = "types-awscrt";
-  version = "0.28.2";
+  version = "0.28.4";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "types_awscrt";
     inherit version;
-    hash = "sha256-Q0m2/Hsc2cnreCcB+yE4dduJqxeBIZwOlH3XxNnc1l4=";
+    hash = "sha256-FZKdqEgC8nAZ7o5EhPscEC4fbUzyLrSGiMNKWobQLrY=";
   };
 
   build-system = [ setuptools ];
