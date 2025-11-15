84c126316aa952740daa2a852fb1e75b8fbc3c0e
WeetHet <stas.ale66@gmail.com>
2025-11-15 21:05:38 +0200

    gtk4: enable vulkan support on darwin
    Signed-off-by: WeetHet <stas.ale66@gmail.com>

--- pkgs/by-name/gt/gtk4/package.nix
+++ pkgs/by-name/gt/gtk4/package.nix
@@ -48,7 +48,7 @@
   x11Support ? stdenv.hostPlatform.isLinux,
   waylandSupport ? stdenv.hostPlatform.isLinux,
   libGL,
-  vulkanSupport ? stdenv.hostPlatform.isLinux,
+  vulkanSupport ? stdenv.hostPlatform.isLinux || stdenv.hostPlatform.isDarwin,
   shaderc,
   vulkan-loader,
   vulkan-headers,
@@ -141,6 +141,8 @@ stdenv.mkDerivation (finalAttrs: {
   ]
   ++ lib.optionals vulkanSupport [
     vulkan-headers
+  ]
+  ++ lib.optionals (vulkanSupport && stdenv.hostPlatform.isLinux) [
     libdrm
   ]
   ++ [
