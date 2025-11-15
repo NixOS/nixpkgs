85037616cdd23327995ece1b21d54352621bd221
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-15 13:02:22 +0000

    sea-orm-cli: 1.1.17 -> 1.1.19

--- pkgs/by-name/se/sea-orm-cli/package.nix
+++ pkgs/by-name/se/sea-orm-cli/package.nix
@@ -9,18 +9,18 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "sea-orm-cli";
-  version = "1.1.17";
+  version = "1.1.19";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-qf0SPFOfp180j6IHds6aQ2y0oYcwQWq7VrO8iyi7LXo=";
+    hash = "sha256-dsise5MDhR4pcD3ZWDUzTG0Q4Fg/VdKw2Q59/g6BabA=";
   };
 
   nativeBuildInputs = [ pkg-config ];
 
   buildInputs = [ openssl ];
 
-  cargoHash = "sha256-AqrS+5y3bKuqAVvbmWDO3V0OBVSkW6212WQeY1hixsk=";
+  cargoHash = "sha256-38KIJYwRvVmChGSJwaRRWbb/HPuuTp/qnvXpo3xjRpE=";
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   versionCheckProgramArg = "--version";
