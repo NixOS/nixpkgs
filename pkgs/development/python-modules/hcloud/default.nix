426d08f1c77a40eb4ea713a2fa40411e448778c5
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-15 06:47:57 +0000

    python3Packages.hcloud: 2.10.0 -> 2.11.1

--- pkgs/development/python-modules/hcloud/default.nix
+++ pkgs/development/python-modules/hcloud/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "hcloud";
-  version = "2.10.0";
+  version = "2.11.1";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-EIO8s47temWK/pgm/8Gi2xgQyoOQoi9LjEN4cTjosbo=";
+    hash = "sha256-8hL+H1PL+J7d2sDnAF7C6wIep4n4K+cJe9dM1wWynys=";
   };
 
   build-system = [ setuptools ];
