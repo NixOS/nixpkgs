321e64b1e07576b40ab442e1a77c7db474bb55a5
veyndan <veyndan@gmail.com>
2025-11-15 21:57:03 +0100

    maintainers: add veyndan

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -27409,6 +27409,13 @@
     githubId = 1486626;
     name = "Artyom Pertsovsky";
   };
+  veyndan = {
+    email = "veyndan@gmail.com";
+    github = "veyndan";
+    githubId = 6900601;
+    matrix = "@veyndan:matrix.org";
+    name = "Veyndan Stuart";
+  };
   vglfr = {
     email = "vf.velt@gmail.com";
     github = "vglfr";
