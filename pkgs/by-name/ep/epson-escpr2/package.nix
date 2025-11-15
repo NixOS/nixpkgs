1919f1b66335fb5846b4d2175f014ab49c23c2bd
Shawn8901 <shawn8901@googlemail.com>
2025-11-15 18:01:38 +0000

    epson-escpr2: 1.2.35 -> 1.2.36

--- pkgs/by-name/ep/epson-escpr2/package.nix
+++ pkgs/by-name/ep/epson-escpr2/package.nix
@@ -8,15 +8,15 @@
 
 stdenv.mkDerivation {
   pname = "epson-inkjet-printer-escpr2";
-  version = "1.2.35";
+  version = "1.2.36";
 
   src = fetchurl {
     # To find the most recent version go to
     # https://support.epson.net/linux/Printer/LSB_distribution_pages/en/escpr2.php
     # and retrieve the download link for source package for arm CPU for the tar.gz (the x86 link targets to rpm source files)
 
-    url = "https://download3.ebz.epson.net/dsc/f/03/00/17/28/09/4e8dc40219d4b52e414b608de92552af4fd46eca/epson-inkjet-printer-escpr2-1.2.35-1.tar.gz";
-    hash = "sha256-aYEuEION/C32/SnngreX/nqK/6Yzkuxf0k0HpferTYM=";
+    url = "https://download-center.epson.com/f/module/bb48b7e6-662f-4152-a86f-c1a78fc74b1f/epson-inkjet-printer-escpr2-1.2.36-1.tar.gz";
+    hash = "sha256-0R4dFhT1XhjXMOeRxCbTIT1K83bkwiAhpu/W10DSlxM=";
   };
 
   buildInputs = [ cups ];
