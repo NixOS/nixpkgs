dc957fd884c34216b5946108eba6d733ab687831
Michael Livshin <mlivshin@gmail.com>
2025-11-15 19:15:49 +0200

    urbit: 3.5 -> 4.0
    Also simplify both the derivation and the updater script as much as
    possible, for robustness.

--- pkgs/by-name/ur/urbit/package.nix
+++ pkgs/by-name/ur/urbit/package.nix
@@ -1,7 +1,7 @@
 {
   stdenv,
   lib,
-  fetchzip,
+  fetchurl,
 }:
 
 let
@@ -11,22 +11,27 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "urbit";
-  version = "3.5";
+  version = "4.0";
 
-  src = fetchzip {
+  src = fetchurl {
     url = "https://github.com/urbit/vere/releases/download/vere-v${version}/${platform}.tgz";
     sha256 =
       {
-        x86_64-linux = "sha256-eB80GuyNuVZbBsyNnek8UCtquZbNt5G4Co7IKqq7aeI=";
-        aarch64-linux = "sha256-imbzAsjjznLuxee9mWXpsG/dKEJxdEOTw+JFc4DbQ2Q=";
-        x86_64-darwin = "sha256-0c1ewdrVsfSUivrcLwVuxZdcyrOAKXF7P9W+B7o5aNU=";
-        aarch64-darwin = "sha256-j8PJ04zRz2sZdpetLyzwRasj0CkiRGY+GvzWXG90IaE=";
+        x86_64-linux = "043cpvvfg48r5vza2y18890crpk030l59sa4v3jj737cmgwzyric";
+        aarch64-linux = "1j5xsywqkms85k1p390hr2hc4zcq3bx3sxi4j0s1s6d6bqxwpikn";
+        x86_64-darwin = "0s403s66fxhihyhvriacl654a07zsi69sh5w7m23gn87mrz3hx8x";
+        aarch64-darwin = "0aczb9zxq03a69a39q6rvnhmchrj5x025ia1jzqzdw1rhb4m4yxr";
       }
       .${stdenv.hostPlatform.system} or (throw "unsupported system ${stdenv.hostPlatform.system}");
   };
 
+  unpackPhase = ''
+    mkdir src
+    tar -C src -xf $src
+  '';
+
   postInstall = ''
-    install -m755 -D vere-v${version}-${platform} $out/bin/urbit
+    install -m755 -D src/vere-v${version}-${platform} $out/bin/urbit
   '';
 
   passthru.updateScript = ./update-bin.sh;
