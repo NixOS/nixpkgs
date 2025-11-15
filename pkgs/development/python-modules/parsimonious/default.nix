7b7a59269cc8a220a45258c388dca4cd3b9b94bc
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-15 10:32:55 +0000

    python3Packages.parsimonious: 0.10.0 -> 0.11.0

--- pkgs/development/python-modules/parsimonious/default.nix
+++ pkgs/development/python-modules/parsimonious/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "parsimonious";
-  version = "0.10.0";
+  version = "0.11.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-goFgDaGA7IrjVCekq097gr/sHj0eUvgMtg6oK5USUBw=";
+    hash = "sha256-4IA3fZiVe+7AU1gNOK5U/N98Rw+3hnC6S/i1+dXK0qk=";
   };
 
   propagatedBuildInputs = [ regex ];
