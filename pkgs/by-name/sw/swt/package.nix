6967a027547ac562a3fecd052330eb800fcfa59b
Doron Behar <doron.behar@gmail.com>
2025-11-16 16:19:58 +0200

    swt: 4.34 -> 4.38M3
    Fixes #459322 .
    
    Co-Authored-By: éclairevoyant <848000+eclairevoyant@users.noreply.github.com>

--- pkgs/by-name/sw/swt/package.nix
+++ pkgs/by-name/sw/swt/package.nix
@@ -14,7 +14,7 @@ stdenv.mkDerivation (finalAttrs: {
   # NOTE: In case you wish to override, don't override version, override
   # `fullVersion`.
   version = builtins.elemAt (lib.splitString "-" finalAttrs.fullVersion) 1;
-  fullVersion = "R-4.34-202411201800";
+  fullVersion = "S-4.38M3-202511140540";
 
   hardeningDisable = [ "format" ];
 
@@ -23,17 +23,17 @@ stdenv.mkDerivation (finalAttrs: {
     # equal on all linux systems as well as all darwin systems. Even though each
     # of these zip archives themselves contains a different hash.
     x86_64-linux.platform = "gtk-linux-x86_64";
-    x86_64-linux.hash = "sha256-lKAB2aCI3dZdt3pE7uSvSfxc8vc3oMSTCx5R+71Aqdk=";
+    x86_64-linux.hash = "sha256-aEpPHn55QuK1kgUaEtDANztCJ2tTPZhK5yo72BojmQw=";
     aarch64-linux.platform = "gtk-linux-aarch64";
-    aarch64-linux.hash = "sha256-lKAB2aCI3dZdt3pE7uSvSfxc8vc3oMSTCx5R+71Aqdk=";
+    aarch64-linux.hash = "sha256-aEpPHn55QuK1kgUaEtDANztCJ2tTPZhK5yo72BojmQw=";
     ppc64le-linux.platform = "gtk-linux-ppc64le";
-    ppc64le-linux.hash = "sha256-lKAB2aCI3dZdt3pE7uSvSfxc8vc3oMSTCx5R+71Aqdk=";
+    ppc64le-linux.hash = "sha256-aEpPHn55QuK1kgUaEtDANztCJ2tTPZhK5yo72BojmQw=";
     riscv64-linux.platform = "gtk-linux-riscv64";
-    riscv64-linux.hash = "sha256-lKAB2aCI3dZdt3pE7uSvSfxc8vc3oMSTCx5R+71Aqdk=";
+    riscv64-linux.hash = "sha256-aEpPHn55QuK1kgUaEtDANztCJ2tTPZhK5yo72BojmQw=";
     x86_64-darwin.platform = "cocoa-macosx-x86_64";
-    x86_64-darwin.hash = "sha256-Uns3fMoetbZAIrL/N0eVd42/3uygXakDdxpaxf5SWDI=";
+    x86_64-darwin.hash = "sha256-B2eqUvXXRDW8wRswhtpgP1sMyBHAaS9gvrj13jlwup8=";
     aarch64-darwin.platform = "cocoa-macosx-aarch64";
-    aarch64-darwin.hash = "sha256-Uns3fMoetbZAIrL/N0eVd42/3uygXakDdxpaxf5SWDI";
+    aarch64-darwin.hash = "sha256-B2eqUvXXRDW8wRswhtpgP1sMyBHAaS9gvrj13jlwup8=";
   };
   passthru.srcMetadata =
     finalAttrs.passthru.srcMetadataByPlatform.${stdenv.hostPlatform.system} or null;
