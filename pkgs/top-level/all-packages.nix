967465fdfd0721ee649a41614dd82f4badf38255
Anders Kaseorg <andersk@mit.edu>
2025-11-16 12:18:03 -0800

    gnome49Extensions: Add to all-packages.nix
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13330,6 +13330,7 @@ with pkgs;
     gnome46Extensions
     gnome47Extensions
     gnome48Extensions
+    gnome49Extensions
     ;
 
   gnome-extensions-cli = python3Packages.callPackage ../desktops/gnome/misc/gnome-extensions-cli { };
