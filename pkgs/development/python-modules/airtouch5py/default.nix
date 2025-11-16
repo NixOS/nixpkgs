152525dbcc28e950bc1d842dbf7180feaee929d6
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-16 10:51:43 +0000

    python3Packages.airtouch5py: 0.3.0 -> 0.4.0

--- pkgs/development/python-modules/airtouch5py/default.nix
+++ pkgs/development/python-modules/airtouch5py/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "airtouch5py";
-  version = "0.3.0";
+  version = "0.4.0";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "danzel";
     repo = "airtouch5py";
     tag = version;
-    hash = "sha256-St2FjyUXyTmy0shQfUzuB+lUSaadEyjdNJJkQA4aCxQ=";
+    hash = "sha256-SJ6AVUbdEy0nvpLe39dH/Wc//fDTf0dIvrvVQDUl5eI=";
   };
 
   build-system = [ poetry-core ];
