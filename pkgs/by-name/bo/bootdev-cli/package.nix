3f4917585772e70ed5a4d366b27a280f8a1614cd
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-17 19:17:57 +0000

    bootdev-cli: 1.20.5 -> 1.20.6

--- pkgs/by-name/bo/bootdev-cli/package.nix
+++ pkgs/by-name/bo/bootdev-cli/package.nix
@@ -11,13 +11,13 @@
 
 buildGoModule rec {
   pname = "bootdev-cli";
-  version = "1.20.5";
+  version = "1.20.6";
 
   src = fetchFromGitHub {
     owner = "bootdotdev";
     repo = "bootdev";
     tag = "v${version}";
-    hash = "sha256-iVL2nRQb4A7UfhiQSBBbaxM1Yqc2pESvRfQ3xSjGq10=";
+    hash = "sha256-/53s+XYMhxo9i1ZeWuV3xiZnhcS5rBJUvM3acb8TiWM=";
   };
 
   vendorHash = "sha256-jhRoPXgfntDauInD+F7koCaJlX4XDj+jQSe/uEEYIMM=";
