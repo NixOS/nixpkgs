86cd2ef9c2cd59dfc0890e0dfc5ad775ef37917f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-17 18:28:32 +0000

    sqlcl: 25.3.0.274.1210 -> 25.3.2.317.1117

--- pkgs/by-name/sq/sqlcl/package.nix
+++ pkgs/by-name/sq/sqlcl/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sqlcl";
-  version = "25.3.0.274.1210";
+  version = "25.3.2.317.1117";
 
   src = fetchurl {
     url = "https://download.oracle.com/otn_software/java/sqldeveloper/sqlcl-${finalAttrs.version}.zip";
-    hash = "sha256-TOKTvBFoUFDqN7qjq6yYYBh0bfS66kks243q6+5+i6c=";
+    hash = "sha256-CvHssdoahA2ADP6I/iCjWuuc6sn/OHKb4AJbk3sQHX4=";
   };
 
   nativeBuildInputs = [
