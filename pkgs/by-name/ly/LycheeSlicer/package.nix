56c34550b1d7802af32ece784b1818b2678e4984
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-17 16:12:51 +0000

    LycheeSlicer: 7.4.6 -> 7.5.0

--- pkgs/by-name/ly/LycheeSlicer/package.nix
+++ pkgs/by-name/ly/LycheeSlicer/package.nix
@@ -9,11 +9,11 @@
 }:
 let
   pname = "LycheeSlicer";
-  version = "7.4.6";
+  version = "7.5.0";
 
   src = fetchurl {
     url = "https://mango-lychee.nyc3.cdn.digitaloceanspaces.com/LycheeSlicer-${version}.AppImage";
-    hash = "sha256-4Cqhr++RP8FlWUMQyfpEY7df2Ai3aqrzwX+kZ+SzGCQ=";
+    hash = "sha256-jI/V8tczgBHl3/LOEui+DbjWB5kD67+mL2HMvPz80+8=";
   };
 
   desktopItem = makeDesktopItem {
