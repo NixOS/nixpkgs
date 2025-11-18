7eea3f74b482e289d252d331861b7261691c5943
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-18 22:02:09 +0000

    pantheon.switchboard-plug-about: 8.2.1 -> 8.2.2

--- pkgs/desktops/pantheon/apps/switchboard-plugs/about/default.nix
+++ pkgs/desktops/pantheon/apps/switchboard-plugs/about/default.nix
@@ -27,13 +27,13 @@
 
 stdenv.mkDerivation rec {
   pname = "switchboard-plug-about";
-  version = "8.2.1";
+  version = "8.2.2";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = "switchboard-plug-about";
     rev = version;
-    sha256 = "sha256-H4BDLP3yzQi+ougpvBvnv1R1TImzUjSOVDGbOqw9hvg=";
+    sha256 = "sha256-SPFCBsk4tVR+5Q6uuDG/fTIn+4TXdeAobfQxkmxMiW0=";
   };
 
   nativeBuildInputs = [
