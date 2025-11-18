b4e84c8beea504079d96ae47a1420a9e25569e05
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-18 02:55:22 +0000

    application-title-bar: 0.8.6 -> 0.8.7

--- pkgs/by-name/ap/application-title-bar/package.nix
+++ pkgs/by-name/ap/application-title-bar/package.nix
@@ -8,13 +8,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "application-title-bar";
-  version = "0.8.6";
+  version = "0.8.7";
 
   src = fetchFromGitHub {
     owner = "antroids";
     repo = "application-title-bar";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-IJb2ECsFoA+YpegV0Fqk8rsNfym7KU7O79mwFdTr9gE=";
+    hash = "sha256-zcxZlQCLLtY5swqPpe0OvpJMykPEWPD4jrGslW7snP8=";
   };
 
   propagatedUserEnvPkgs = with kdePackages; [ kconfig ];
