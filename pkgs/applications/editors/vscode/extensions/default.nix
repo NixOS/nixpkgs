128135c7ee81e8ce465fc12d9b1d801506e3d4d7
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-18 14:12:20 +0000

    vscode-extensions.xdebug.php-debug: 1.37.0 -> 1.38.0

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -5318,8 +5318,8 @@ let
         mktplcRef = {
           name = "php-debug";
           publisher = "xdebug";
-          version = "1.37.0";
-          hash = "sha256-7Dz8i66tWPStk2fgFdZPY2Jz3j4IquJVyQbSnV+SVpk=";
+          version = "1.38.0";
+          hash = "sha256-QOA8At2akj9zLYxIuQJLkDL2Zg/isPIssFG/UipRBbU=";
         };
         meta = {
           description = "PHP Debug Adapter";
