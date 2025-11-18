5f84969101a7bdcb50d517207251cec698177a65
teutat3s <10206665+teutat3s@users.noreply.github.com>
2025-11-18 23:02:27 +0100

    element-web-unwrapped: 1.12.3 -> 1.12.4
    Release notes: https://github.com/element-hq/element-web/releases/tag/v1.12.4
    Changelog: https://github.com/element-hq/element-web/compare/v1.12.3...v1.12.4

--- pkgs/by-name/el/element-web-unwrapped/element-web-pin.nix
+++ pkgs/by-name/el/element-web-unwrapped/element-web-pin.nix
@@ -1,7 +1,7 @@
 {
-  "version" = "1.12.3";
+  "version" = "1.12.4";
   "hashes" = {
-    "webSrcHash" = "sha256-a/RrUzJU/pjyD36WmNIqjSTBn5cfUOGNSe/l6iGp/0A=";
-    "webYarnHash" = "sha256-TXOehZRw5UIhGTnpR0KzvEizSW9Qk2VTr+cG/XstB+k=";
+    "webSrcHash" = "sha256-5sRKVgKbU4hr1cubY/0PdLDClBcFGxdu9TITORL6beI=";
+    "webYarnHash" = "sha256-S+glH2HkGyINEGHWn1TMK2y45gk7WtxTe+u4luqVgsw=";
   };
 }
