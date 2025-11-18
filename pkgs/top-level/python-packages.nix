521e53dcdfa81d98f37a620d9cf607cb54ddb01b
László Kupcsik <dev@biobits.be>
2025-11-18 22:15:47 +0100

    python3Packages.dnaio: init at 1.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4340,6 +4340,8 @@ self: super: with self; {
 
   dnachisel = callPackage ../development/python-modules/dnachisel { };
 
+  dnaio = callPackage ../development/python-modules/dnaio { };
+
   dncil = callPackage ../development/python-modules/dncil { };
 
   dnf-plugins-core = callPackage ../development/python-modules/dnf-plugins-core { };
