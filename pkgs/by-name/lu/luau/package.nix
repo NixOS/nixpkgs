fe905dd17283591a1fcf30d9ac21e464c5ac1717
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-18 07:00:52 +0000

    luau: 0.699 -> 0.700

--- pkgs/by-name/lu/luau/package.nix
+++ pkgs/by-name/lu/luau/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "luau";
-  version = "0.699";
+  version = "0.700";
 
   src = fetchFromGitHub {
     owner = "luau-lang";
     repo = "luau";
     tag = finalAttrs.version;
-    hash = "sha256-EuhLQwj+1bxNK9E0DdfsBpsqbkZHQUrtyejn5bfErKg=";
+    hash = "sha256-tZbpSKwiXLk6Aw5VusPnPT+4pGyURqC9hmr0jiiKMg4=";
   };
 
   nativeBuildInputs = [ cmake ];
