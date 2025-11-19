065b792c6772a69025156fd67b92e8694a1394d7
Wolfgang Walther <walther@technowledgy.de>
2025-11-19 11:33:32 +0100

    maintainers: drop diegolelis
    Inactive since 2024.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -6539,12 +6539,6 @@
     githubId = 41772157;
     name = "Dict Xiong";
   };
-  diegolelis = {
-    email = "diego.o.lelis@gmail.com";
-    github = "DiegoLelis";
-    githubId = 8404455;
-    name = "Diego Lelis";
-  };
   diegs = {
     email = "dpontor@gmail.com";
     github = "diegs";
