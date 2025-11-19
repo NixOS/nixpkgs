85e32ffccb6cca7ed62da042b46305457b46f0f2
Alex Tunstall <alex@tunstall.xyz>
2025-11-19 09:08:18 +0000

    haskell.compiler.microhs-0_14_23_0: init

--- pkgs/top-level/haskell-packages.nix
+++ pkgs/top-level/haskell-packages.nix
@@ -248,7 +248,11 @@ in
         microhs-boot = bb.compiler.microhs-0_14_18_0;
       };
 
-      microhs-0_14 = compiler.microhs-0_14_21_0;
+      microhs-0_14_23_0 = callPackage ../development/compilers/microhs/0.14.23.0.nix {
+        microhs-boot = bb.compiler.microhs-0_14_18_0;
+      };
+
+      microhs-0_14 = compiler.microhs-0_14_23_0;
       microhs = compiler.microhs-0_14;
     }
     // pkgs.lib.optionalAttrs config.allowAliases {
@@ -394,7 +398,14 @@ in
         packageSetConfig = bootstrapPackageSet;
       };
 
-      microhs-0_14 = packages.microhs-0_14_21_0;
+      microhs-0_14_23_0 = callPackage ../development/haskell-modules {
+        buildHaskellPackages = bh.packages.microhs-0_14_23_0;
+        ghc = bh.compiler.microhs-0_14_23_0;
+        compilerConfig = callPackage ../development/haskell-modules/configuration-microhs.nix { };
+        packageSetConfig = bootstrapPackageSet;
+      };
+
+      microhs-0_14 = packages.microhs-0_14_23_0;
       microhs = packages.microhs-0_14;
     }
     // pkgs.lib.optionalAttrs config.allowAliases {
