66446e19de3f031b4392b93a112c0023fa3b83e4
Sarah Clark <seclark@nextquestion.net>
2025-11-19 14:53:45 -0800

    python3Packages.langchain-mongodb: switch to new home repo

--- pkgs/development/python-modules/langchain-mongodb/default.nix
+++ pkgs/development/python-modules/langchain-mongodb/default.nix
@@ -30,12 +30,12 @@ buildPythonPackage rec {
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
-    repo = "langchain";
-    tag = "langchain-mongodb==${version}";
+    repo = "langchain-mongodb";
+    tag = "libs/langchain-mongodb/v${version}";
     hash = "sha256-Jd9toXkS9dGtSIrJQ/5W+swV1z2BJOJKBtkyGzj3oSc=";
   };
 
-  sourceRoot = "${src.name}/libs/partners/mongodb";
+  sourceRoot = "${src.name}/libs/langchain-mongodb";
 
   build-system = [ poetry-core ];
 
@@ -70,14 +70,14 @@ buildPythonPackage rec {
     # python updater script sets the wrong tag
     skipBulkUpdate = true;
     updateScript = gitUpdater {
-      rev-prefix = "langchain-mongodb==";
+      rev-prefix = "libs/langchain-mongodb/v";
     };
   };
 
   meta = {
-    changelog = "https://github.com/langchain-ai/langchain/releases/tag/${src.tag}";
+    changelog = "https://github.com/langchain-ai/langchain-mongodb/releases/tag/${src.tag}";
     description = "Integration package connecting MongoDB and LangChain";
-    homepage = "https://github.com/langchain-ai/langchain/tree/master/libs/partners/mongodb";
+    homepage = "https://github.com/langchain-ai/langchain-mongodb";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [
       natsukium
