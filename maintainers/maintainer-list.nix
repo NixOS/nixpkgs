3eef7f5acedfe2a8c3620b62e93e46e494e76c80
Wolfgang Walther <walther@technowledgy.de>
2025-11-19 11:45:06 +0100

    maintainers: drop beezow
    Inactive since 2023.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3131,12 +3131,6 @@
     github = "beeb";
     githubId = 703631;
   };
-  beezow = {
-    name = "beezow";
-    email = "zbeezow@gmail.com";
-    github = "beezow";
-    githubId = 42082156;
-  };
   bellackn = {
     name = "Nico Bellack";
     email = "blcknc@pm.me";
