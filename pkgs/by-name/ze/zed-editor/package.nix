1b1fddb0eb5b8596aac4914c381db4f34c12993a
botnk <github-botnk@korz.dev>
2025-11-19 20:54:05 +0000

    zed-editor: 0.212.7 -> 0.213.3
    Changelog: https://github.com/zed-industries/zed/releases/tag/v0.213.3

--- pkgs/by-name/ze/zed-editor/package.nix
+++ pkgs/by-name/ze/zed-editor/package.nix
@@ -101,7 +101,7 @@ let
 in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zed-editor";
-  version = "0.212.7";
+  version = "0.213.3";
 
   outputs = [
     "out"
@@ -114,7 +114,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "zed-industries";
     repo = "zed";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-T7ZuXZp1/9ZvAawCry5Zu4d+UU0Vr4Lk677oAYPfPIs=";
+    hash = "sha256-H4g21r2BO0ppv/zjacUfib6pc8r4D5aE0iBQGE5XU3w=";
   };
 
   postPatch = ''
@@ -134,7 +134,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     rm -r $out/git/*/candle-book/
   '';
 
-  cargoHash = "sha256-KF4Bq9d7m0+M6p0vQLqjbS9D3kriRowjVKvxppdZq60=";
+  cargoHash = "sha256-sr77w3r1MfGXPIFbQ8FohBbErJOE7mmZRLj6agzFeuU=";
 
   nativeBuildInputs = [
     cmake
