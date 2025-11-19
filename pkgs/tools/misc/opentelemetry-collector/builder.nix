b51ef3f1cd7c59964ff5c22a874823eb96811a5d
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-19 08:01:17 +0000

    opentelemetry-collector-builder: 0.135.0 -> 0.140.0

--- pkgs/tools/misc/opentelemetry-collector/builder.nix
+++ pkgs/tools/misc/opentelemetry-collector/builder.nix
@@ -7,17 +7,17 @@ buildGoModule rec {
   pname = "ocb";
   # Also update `pkgs/tools/misc/opentelemetry-collector/releases.nix`
   # whenever that version changes.
-  version = "0.135.0";
+  version = "0.140.0";
 
   src = fetchFromGitHub {
     owner = "open-telemetry";
     repo = "opentelemetry-collector";
     rev = "cmd/builder/v${version}";
-    hash = "sha256-ntRWAYbVbtZBqewXx4+YCZspRr+wSE2iGgmH8PEfj5o=";
+    hash = "sha256-0aEPCFruqYrmlQFPEy0F/Zaazu/ErHy6cY+GFgF+6o8=";
   };
 
   sourceRoot = "${src.name}/cmd/builder";
-  vendorHash = "sha256-my52TJ2D9RXbIqSaY4PHwrYVd93dXXeg/srXHt1jcoI=";
+  vendorHash = "sha256-1k4FaMxykZ0b5em/4+1dpxQLZNinxqiNzEiGRKWRQ64=";
 
   env.CGO_ENABLED = 0;
   ldflags = [
