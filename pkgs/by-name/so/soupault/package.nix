471f5c49a856bf6b18828823318e14d0ad3f02e6
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-11-19 21:40:48 +0700

    soupault: 5.1.0 → 5.2.0

--- pkgs/by-name/so/soupault/package.nix
+++ pkgs/by-name/so/soupault/package.nix
@@ -12,16 +12,16 @@
 
 ocamlPackages.buildDunePackage rec {
   pname = "soupault";
-  version = "5.1.0";
+  version = "5.2.0";
 
-  minimalOCamlVersion = "4.13";
+  minimalOCamlVersion = "5.3";
 
   src = fetchzip {
     urls = [
       "https://github.com/PataphysicalSociety/soupault/archive/${version}.tar.gz"
       "https://codeberg.org/PataphysicalSociety/soupault/archive/${version}.tar.gz"
     ];
-    hash = "sha256-yAkJgNwF763b2DFGA+4Ve+jafFxZbFDm3QxisDD6gYo=";
+    hash = "sha256-BuQ2yQzf6PpK6HUfuHDscgKkOBttCrs0zTh37hMUR6E=";
   };
 
   nativeBuildInputs = [
