8369879f8e8849774f5f6554af792c7ba0196ebc
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-19 19:48:17 +0000

    runpodctl: 1.14.11 -> 1.14.12

--- pkgs/by-name/ru/runpodctl/package.nix
+++ pkgs/by-name/ru/runpodctl/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "runpodctl";
-  version = "1.14.11";
+  version = "1.14.12";
 
   src = fetchFromGitHub {
     owner = "runpod";
     repo = "runpodctl";
     rev = "v${version}";
-    hash = "sha256-RXI1NAdEg5QypRDXg0NCpHLDrxcRCCS22KGnZyotXrI=";
+    hash = "sha256-T0JFPoaH6UhgPMzo+491EUBmjgko1WXP1erMGKc3N5w=";
   };
 
   vendorHash = "sha256-SaaHVaN2r3DhUk7sVizhRggYZRujd+e8qbpq0xOVD88=";
