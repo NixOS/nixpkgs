5ad22c2acd971354aa918b87787b26b5346c07f6
JuliusFreudenberger <julius+git@jfreudenberger.de>
2025-11-19 22:00:52 +0100

    teleport_18: 18.3.2 -> 18.4.0
    Changelog: https://github.com/gravitational/teleport/releases/tag/v18.4.0
    Diff: https://github.com/gravitational/teleport/compare/v18.3.2...v18.4.0

--- pkgs/by-name/te/teleport_18/package.nix
+++ pkgs/by-name/te/teleport_18/package.nix
@@ -7,10 +7,10 @@
 }:
 
 buildTeleport {
-  version = "18.3.2";
-  hash = "sha256-BeYn/uq81t9aL3xfxc7TJlNhvqJ2MDzN2Cue2XeG7g8=";
-  vendorHash = "sha256-FSG/54vVFVWiJmlUYqS+3l2EoqxM9tUH91/Nap1p8nk=";
-  pnpmHash = "sha256-6sThtwACNEdV0fleaQf3iMmFxPsd0AshYeYZUatFMcg=";
+  version = "18.4.0";
+  hash = "sha256-QliroT1xJYFJvY52FCFzBY9BO3GImTy5bcWh4Z2hvHU=";
+  vendorHash = "sha256-ltPdDqUJeLYG+uHUqq3JMKlI2Bvz7yOO81NtPRMhd2A=";
+  pnpmHash = "sha256-GqWNHhltgSQkT0M7Y0vfJ6gLiVAZ03KdjrnqGOJhoNs=";
   cargoHash = "sha256-ia4We4IfIkqz82aFMVvXdzjDXw0w+OJSPVdutfau6PA=";
 
   wasm-bindgen-cli = wasm-bindgen-cli_0_2_99;
