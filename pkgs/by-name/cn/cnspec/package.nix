82a4ce6e008963cfb1692357371194106e1c8e48
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-20 14:45:56 +0100

    cnspec: 12.9.0 -> 12.10.0
    Diff: https://github.com/mondoohq/cnspec/compare/v12.9.0...v12.10.0
    
    Changelog: https://github.com/mondoohq/cnspec/releases/tag/v12.10.0

--- pkgs/by-name/cn/cnspec/package.nix
+++ pkgs/by-name/cn/cnspec/package.nix
@@ -6,18 +6,18 @@
 
 buildGoModule rec {
   pname = "cnspec";
-  version = "12.9.0";
+  version = "12.10.0";
 
   src = fetchFromGitHub {
     owner = "mondoohq";
     repo = "cnspec";
     tag = "v${version}";
-    hash = "sha256-90bM85hKI1XOxdHsVstzJANnWJEhTmmDzzUqL3qx9n4=";
+    hash = "sha256-6u5h3mF8LCEEqI/DKwEdFGjb7fufLbzPXGT1oJj5jvE=";
   };
 
   proxyVendor = true;
 
-  vendorHash = "sha256-Jf5R5Bz6VgPON49qrrV0Pyd42NfTImSFX0TATT+udOM=";
+  vendorHash = "sha256-lH91LoHHxYDfR9RuNUseQaSScFWmDrvfQNYVxlv4yyk=";
 
   subPackages = [ "apps/cnspec" ];
 
