b7ec5d7372b117660dc80133b997b8fe700e929c
Jörg Thalheim <joerg@thalheim.io>
2025-11-20 12:40:09 +0100

    nixos/facter: handle case where we do not have readOnly included

--- nixos/modules/hardware/facter/system.nix
+++ nixos/modules/hardware/facter/system.nix
@@ -8,7 +8,10 @@
   # Skip setting hostPlatform if it's read-only
   config.nixpkgs =
     lib.optionalAttrs
-      (config.hardware.facter.report.system or null != null && !options.nixpkgs.hostPlatform.readOnly)
+      (
+        config.hardware.facter.report.system or null != null
+        && !(options.nixpkgs.hostPlatform.readOnly or false)
+      )
       {
         hostPlatform = lib.mkDefault config.hardware.facter.report.system;
       };
