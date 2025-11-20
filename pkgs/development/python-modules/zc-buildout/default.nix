2769991a3622ffbc1cc03873fb519c36ae043b3f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-20 21:16:49 +0000

    python3Packages.zc-buildout: 4.1.12 -> 5.1.0

--- pkgs/development/python-modules/zc-buildout/default.nix
+++ pkgs/development/python-modules/zc-buildout/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "zc-buildout";
-  version = "4.1.12";
+  version = "5.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "buildout";
     repo = "buildout";
     tag = version;
-    hash = "sha256-4MutmUuF4WdoD/wdhYtpVvOYBJEv+2JVkskmO6sz24U=";
+    hash = "sha256-OHTw7KFFBSLcrvu848wjuxFIF83AF/wuWL8ewaO5TXE=";
   };
 
   build-system = [ setuptools ];
