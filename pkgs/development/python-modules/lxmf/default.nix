f6c7397981a8dc2080017264684b719a8c8ceff9
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-20 09:01:30 +0000

    python3Packages.lxmf: 0.9.2 -> 0.9.3

--- pkgs/development/python-modules/lxmf/default.nix
+++ pkgs/development/python-modules/lxmf/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "lxmf";
-  version = "0.9.2";
+  version = "0.9.3";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -18,7 +18,7 @@ buildPythonPackage rec {
     owner = "markqvist";
     repo = "lxmf";
     tag = version;
-    hash = "sha256-9xmg0ofp/0Cy8+et80qWNFIRyiF3vTmdzACLIO+t27U=";
+    hash = "sha256-bPRoKJGMy+JAyhKcRXKR3Jra5K1UAjRMg0lMt2lOvzA=";
   };
 
   build-system = [ setuptools ];
