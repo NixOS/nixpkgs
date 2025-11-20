a93db225b98e80d840f94f31463d61c37563515b
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-20 18:06:37 +0000

    fulcio: 1.8.1 -> 1.8.2

--- pkgs/by-name/fu/fulcio/package.nix
+++ pkgs/by-name/fu/fulcio/package.nix
@@ -15,13 +15,13 @@
 
 buildGoModule rec {
   pname = "fulcio";
-  version = "1.8.1";
+  version = "1.8.2";
 
   src = fetchFromGitHub {
     owner = "sigstore";
     repo = "fulcio";
     tag = "v${version}";
-    hash = "sha256-KUwCeG26gv0LgIxTsvOGc0iQKdYI7IK+RWu8hJYo1cY=";
+    hash = "sha256-yAaMXlcGU1JXGMr2nkUHAWkd2JAlprPbKxs1MKvU6iM=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -33,7 +33,7 @@ buildGoModule rec {
       find "$out" -name .git -print0 | xargs -0 rm -rf
     '';
   };
-  vendorHash = "sha256-4nonoxWjefbSFYErt/KT9vztPgIvkjukh34NQhFHeW4=";
+  vendorHash = "sha256-xOM92evfKrjFhPPny1kIVK5uxZkLJZ+qyJ15/4HpsN0=";
 
   nativeBuildInputs = [ installShellFiles ];
 
