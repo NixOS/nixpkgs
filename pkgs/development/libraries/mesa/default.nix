NixOS/nixpkgs

commit ae921d60d393bafb77901a08beaf93fef7409afc

mesa: 25.2.6 -> 25.3.0

pkgs/development/libraries/mesa/default.nix
@@ -13,6 +13,7 @@
   spirv-tools,
   intltool,
   jdupes,
+  libdisplay-info,
   libdrm,
   libgbm,
   libglvnd,
@@ -50,6 +51,7 @@
     "asahi" # Apple AGX
     "crocus" # Intel legacy
     "d3d12" # WSL emulated GPU (aka Dozen)
+    "ethosu" # ARM Ethos NPU
     "etnaviv" # Vivante GPU designs (mostly NXP/Marvell SoCs)
     "freedreno" # Qualcomm Adreno (all Qualcomm SoCs)
     "i915" # Intel extra legacy
@@ -61,6 +63,7 @@
     "r300" # very old AMD
     "r600" # less old AMD
     "radeonsi" # new AMD (GCN+)
+    "rocket" # Rockchip NPU
     "softpipe" # older software renderer
     "svga" # VMWare virtualized GPU
     "tegra" # Nvidia Tegra SoCs
@@ -75,7 +78,7 @@
     "broadcom" # Broadcom VC5 (Raspberry Pi 4, aka V3D)
     "freedreno" # Qualcomm Adreno (all Qualcomm SoCs)
     "gfxstream" # Android virtualized GPU
-    "imagination-experimental" # PowerVR Rogue (currently N/A)
+    "imagination" # PowerVR Rogue (currently N/A)
     "intel_hasvk" # Intel Haswell/Broadwell, "legacy" Vulkan driver (https://www.phoronix.com/news/Intel-HasVK-Drop-Dead-Code)
     "intel" # new Intel (aka ANV)
     "microsoft-experimental" # WSL virtualized GPU (aka DZN/Dozen)
@@ -96,6 +99,7 @@
     "wayland"
   ],
   vulkanLayers ? [
+    "anti-lag"
     "device-select"
     "intel-nullhw"
     "overlay"
@@ -144,8 +148,6 @@ stdenv.mkDerivation {
 
   patches = [
     ./opencl.patch
-    # https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37027
-    ./gallivm-llvm-21.patch
   ];
 
   postPatch = ''
@@ -256,6 +258,7 @@ stdenv.mkDerivation {
       elfutils
       expat
       spirv-tools
+      libdisplay-info
       libdrm
       libgbm
       libglvnd

pkgs/development/libraries/mesa/common.nix
@@ -5,14 +5,14 @@
 # nix build .#legacyPackages.x86_64-darwin.mesa .#legacyPackages.aarch64-darwin.mesa
 rec {
   pname = "mesa";
-  version = "25.2.6";
+  version = "25.3.0";
 
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "mesa";
     repo = "mesa";
     rev = "mesa-${version}";
-    hash = "sha256-erfvNR1Yor3k8gVVClyiBJsgyaOv7BMFd1NSgieTY5I=";
+    hash = "sha256-MviXDRAbCEXM9dIzD94/CM0bjlF4zCJUVE91Xst/uII=";
   };
 
   meta = {

pkgs/development/libraries/mesa/gallivm-llvm-21.patch
@@ -1,53 +1,0 @@
-From eca19331d94005485de5246cfa87a21621486cd8 Mon Sep 17 00:00:00 2001
-From: no92 <no92.mail@gmail.com>
-Date: Wed, 27 Aug 2025 16:02:31 +0200
-Subject: [PATCH] gallivm: support LLVM 21
-
-LLVM PR#146819 changed the signature of `setObjectLinkingLayerCreator`,
-dropping the Triple argument. The PR was first included in the LLVM 21
-series, and the new signature is gated behind a version check for that.
-
-`LLVMOrcThreadSafeContextGetContext` was removed in LLVM commit b18e5b6,
-and the ORC examples in the LLVM tree seem to just create a context
-instead, which we replicate here.
-
-With this commit, mesa successfully builds the llvmpipe gallium driver
-on riscv64 with LLVM 21.1.0.
-
-Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13785
-Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13852
-
-Reviewed-by: David Heidelberg <david@ixit.cz>
----
- src/gallium/auxiliary/gallivm/lp_bld_init_orc.cpp | 7 ++++++-
- 1 file changed, 6 insertions(+), 1 deletion(-)
-
-diff --git a/src/gallium/auxiliary/gallivm/lp_bld_init_orc.cpp b/src/gallium/auxiliary/gallivm/lp_bld_init_orc.cpp
-index 3d2b8cf81bc0c..0be69b02b6eef 100644
---- a/src/gallium/auxiliary/gallivm/lp_bld_init_orc.cpp
-+++ b/src/gallium/auxiliary/gallivm/lp_bld_init_orc.cpp
-@@ -340,7 +340,12 @@ LPJit::LPJit() :jit_dylib_count(0) {
-          .setJITTargetMachineBuilder(std::move(JTMB))
- #ifdef USE_JITLINK
-          .setObjectLinkingLayerCreator(
-+#if LLVM_VERSION_MAJOR >= 21
-+            /* LLVM 21 removed the Triple argument */
-+            [&](ExecutionSession &ES) {
-+#else
-             [&](ExecutionSession &ES, const llvm::Triple &TT) {
-+#endif
-                return std::make_unique<ObjectLinkingLayer>(
-                   ES, ExitOnErr(llvm::jitlink::InProcessMemoryManager::Create()));
-             })
-@@ -552,7 +557,7 @@ init_gallivm_state(struct gallivm_state *gallivm, const char *name,
-    gallivm->cache = cache;
- 
-    gallivm->_ts_context = context->ref;
--   gallivm->context = LLVMOrcThreadSafeContextGetContext(context->ref);
-+   gallivm->context = LLVMContextCreate();
- 
-    gallivm->module_name = LPJit::get_unique_name(name);
-    gallivm->module = LLVMModuleCreateWithNameInContext(gallivm->module_name,
--- 
-GitLab
-

commit cbe068324d8d11a366fb03a9697c41f392e3fd9d

mesa: fix build for musl
Fixes: ae921d60d393 ("mesa: 25.2.6 -> 25.3.0")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

pkgs/development/libraries/mesa/default.nix
@@ -150,6 +150,7 @@ stdenv.mkDerivation {
 
   patches = [
     ./opencl.patch
+    ./musl.patch
   ];
 
   postPatch = ''

pkgs/development/libraries/mesa/musl.patch
@@ -1,0 +1,39 @@
+From 571dedac8881649cd94c59488413b835cbcf0498 Mon Sep 17 00:00:00 2001
+From: Alyssa Ross <hi@alyssa.is>
+Date: Thu, 20 Nov 2025 23:16:47 +0100
+Subject: [PATCH] rocket: fix building for musl
+
+musl follows POSIX and provides ioctl as int ioctl(int, int, ...).
+
+Fixes: 5b829658f74 ("rocket: Initial commit of a driver for Rockchip's NPU")
+Signed-off-by: Alyssa Ross <hi@alyssa.is>
+Link: https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38561
+---
+ src/gallium/drivers/rocket/intercept.c | 10 ++++++++--
+ 1 file changed, 8 insertions(+), 2 deletions(-)
+
+diff --git a/src/gallium/drivers/rocket/intercept.c b/src/gallium/drivers/rocket/intercept.c
+index 6ffb8647d61f5..88e55893d9520 100644
+--- a/src/gallium/drivers/rocket/intercept.c
++++ b/src/gallium/drivers/rocket/intercept.c
+@@ -294,9 +294,15 @@ handle_action(struct rknpu_action *args)
+    }
+ }
+ 
+-typedef int (*real_ioctl_t)(int fd, unsigned long request, ...);
++#ifdef __GLIBC__
++typedef unsigned long ioctl_req;
++#else
++typedef int ioctl_req; // per POSIX
++#endif
++
++typedef int (*real_ioctl_t)(int fd, ioctl_req request, ...);
+ int
+-ioctl(int fd, unsigned long request, ...)
++ioctl(int fd, ioctl_req request, ...)
+ {
+    int ret;
+    uint32_t output_address = 0;
+-- 
+GitLab
+