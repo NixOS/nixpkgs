09cf4afac345cc4fb0fa42d9dabacfa6dc8b6223
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-21 16:50:27 +0000

    goat: 0-unstable-2024-07-31 -> 0.1
    Signed-off-by: R. Ryantm <ryantm-bot@ryantm.com>

--- pkgs/by-name/go/goat/package.nix
+++ pkgs/by-name/go/goat/package.nix
@@ -5,27 +5,28 @@
   nix-update-script,
 }:
 
-buildGoModule {
+buildGoModule (finalAttrs: {
   pname = "goat";
-  version = "0-unstable-2024-07-31"; # Upstream currently isn't doing tags/releases.
+  version = "0.1";
 
   src = fetchFromGitHub {
     owner = "blampe";
     repo = "goat";
-    rev = "177de93b192b8ffae608e5d9ec421cc99bf68402";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-/DR6RN7dCROp18P7dgm4DMppwdtYl0AOVNMEtXz8ldk=";
   };
 
   vendorHash = "sha256-24YllmSUzRcqWbJ8NLyhsJaoGG2+yE8/eXX6teJ1nV8=";
 
-  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Go ASCII Tool. Render ASCII art as SVG diagrams";
     homepage = "https://github.com/blampe/goat";
+    changelog = "https://github.com/blampe/goat/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ katexochen ];
     mainProgram = "goat";
     platforms = lib.platforms.unix;
   };
-}
+})
