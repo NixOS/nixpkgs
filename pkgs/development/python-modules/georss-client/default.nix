3f05c79a6a9268160940cbacf43f8b775453929f
Sarah Clark <seclark@nextquestion.net>
2025-11-21 10:17:42 -0800

    python3Packages.georss-client: 0.18 -> 0.19
    CHANGELOG: https://github.com/exxamalte/python-georss-client/releases/tag/v0.19
    DIFF: https://github.com/exxamalte/python-georss-client/compare/v0.18...v0.19
    (cherry picked from commit f4eea1d87cee11a20be99b6b9efd5f116d1f5332)

--- pkgs/development/python-modules/georss-client/default.nix
+++ pkgs/development/python-modules/georss-client/default.nix
@@ -7,32 +7,31 @@
   setuptools,
 
   # dependencies
-  dateparser,
   haversine,
+  python-dateutil,
   requests,
   xmltodict,
 
-  # tests
+  # testing
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "georss-client";
-  version = "0.18";
+  version = "0.19";
   pyproject = true;
-
   src = fetchFromGitHub {
     owner = "exxamalte";
     repo = "python-georss-client";
     tag = "v${version}";
-    hash = "sha256-KtndXsNvmjSGwqfKqkGAimHbapIC3I0yi4JuDh6cMzs=";
+    hash = "sha256-+CmauNb+5mDbZXQCd8ZxZCz6FSfEPAnktkMjvQueiO0=";
   };
 
   build-system = [ setuptools ];
 
   dependencies = [
-    dateparser
     haversine
+    python-dateutil
     requests
     xmltodict
   ];
@@ -42,7 +41,6 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "georss_client" ];
 
   meta = {
-    broken = lib.versionAtLeast xmltodict.version "1";
     description = "Python library for accessing GeoRSS feeds";
     homepage = "https://github.com/exxamalte/python-georss-client";
     changelog = "https://github.com/exxamalte/python-georss-client/releases/tag/${src.tag}";
