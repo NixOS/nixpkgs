63f254c2ee1efe72c017395520406b4b641772f7
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-21 20:08:48 +0000

    github-runner: 2.329.0 -> 2.330.0
    (cherry picked from commit 7b6e1b9e8fca7aa9387976c6d080241c3b0f09be)

--- pkgs/by-name/gi/github-runner/package.nix
+++ pkgs/by-name/gi/github-runner/package.nix
@@ -35,13 +35,13 @@ assert builtins.all (
 
 buildDotnetModule (finalAttrs: {
   pname = "github-runner";
-  version = "2.329.0";
+  version = "2.330.0";
 
   src = fetchFromGitHub {
     owner = "actions";
     repo = "runner";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Lf8l959y5tfvCsLwI8+tziFGm+LUJnhE2PQIz/zZneA=";
+    hash = "sha256-Ft6NCgljzMy5SrRwAdixWpPIrbq7WdW8VzXDUVsiuqo=";
     leaveDotGit = true;
     postFetch = ''
       git -C $out rev-parse --short HEAD > $out/.git-revision
