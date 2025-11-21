85f3093695985c11c7c9648718ba23841bf70b9c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-21 15:38:08 +0100

    htop-vim: replace `systemd` with `systemdLibs`

--- pkgs/by-name/ht/htop-vim/package.nix
+++ pkgs/by-name/ht/htop-vim/package.nix
@@ -10,8 +10,8 @@
   libnl,
   sensorsSupport ? stdenv.hostPlatform.isLinux,
   lm_sensors,
-  systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemd,
-  systemd,
+  systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemdLibs,
+  systemdLibs,
 }:
 
 assert systemdSupport -> stdenv.hostPlatform.isLinux;
@@ -58,7 +58,7 @@ stdenv.mkDerivation rec {
     libnl
   ]
   ++ lib.optional sensorsSupport lm_sensors
-  ++ lib.optional systemdSupport systemd;
+  ++ lib.optional systemdSupport systemdLibs;
 
   configureFlags = [
     "--enable-unicode"
@@ -77,7 +77,7 @@ stdenv.mkDerivation rec {
     in
     lib.optionalString (!stdenv.hostPlatform.isStatic) ''
       ${optionalPatch sensorsSupport "${lib.getLib lm_sensors}/lib/libsensors.so"}
-      ${optionalPatch systemdSupport "${systemd}/lib/libsystemd.so"}
+      ${optionalPatch systemdSupport "${systemdLibs}/lib/libsystemd.so"}
     '';
 
   meta = with lib; {
