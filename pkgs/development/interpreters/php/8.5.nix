3fc649ce2fd5919a5f6443dba2141ef0c91986e1
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-11-21 10:57:02 +0100

    php85: init at 8.5.0

---
+++ pkgs/development/interpreters/php/8.5.nix
@@ -0,0 +1,58 @@
+{ callPackage, ... }@_args:
+
+let
+  base = callPackage ./generic.nix (
+    _args
+    // {
+      version = "8.5.0";
+      hash = "sha256-zRbLBFs0ps7GqDAI4bM182XHqDL8xIPfgjCGZMbQIfk=";
+    }
+  );
+in
+base.withExtensions (
+  { all, ... }:
+  with all;
+  [
+    bcmath
+    calendar
+    curl
+    ctype
+    dom
+    exif
+    fileinfo
+    filter
+    ftp
+    gd
+    gettext
+    gmp
+    iconv
+    intl
+    ldap
+    mbstring
+    mysqli
+    mysqlnd
+    opcache
+    openssl
+    pcntl
+    pdo
+    pdo_mysql
+    pdo_odbc
+    pdo_pgsql
+    pdo_sqlite
+    pgsql
+    posix
+    readline
+    session
+    simplexml
+    sockets
+    soap
+    sodium
+    sysvsem
+    sqlite3
+    tokenizer
+    xmlreader
+    xmlwriter
+    zip
+    zlib
+  ]
+)
