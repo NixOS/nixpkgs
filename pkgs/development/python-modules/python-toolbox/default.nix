6082cc45372c62f4da835cae21853a5503cb87a9
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-21 13:45:57 +0100

    python313Packages.python-toolbox: modernize

--- pkgs/development/python-modules/python-toolbox/default.nix
+++ pkgs/development/python-modules/python-toolbox/default.nix
@@ -4,7 +4,6 @@
   docutils,
   fetchFromGitHub,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
@@ -12,8 +11,6 @@ buildPythonPackage rec {
   pname = "python-toolbox";
   version = "1.3.1";
   pyproject = true;
-
-  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "cool-RR";
@@ -43,7 +40,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Tools for testing PySnooper";
     homepage = "https://github.com/cool-RR/python_toolbox";
-    changelog = "https://github.com/cool-RR/python_toolbox/releases/tag/${version}";
+    changelog = "https://github.com/cool-RR/python_toolbox/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ seqizz ];
   };
