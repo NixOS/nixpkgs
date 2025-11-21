9fc475c4ea42599d530adc11c9bf1933f502fbae
Sarah Clark <seclark@nextquestion.net>
2025-11-21 15:16:44 -0800

    python3Packages.anthropic: 0.71.0 -> 0.75.0
    CHANGELOG: https://github.com/anthropics/anthropic-sdk-python/releases/tag/v0.75.0
    DIFF: https://github.com/anthropics/anthropic-sdk-python/compare/v0.71.0...v0.75.0
    (cherry picked from commit fb86236f64e373c5ec236478f14627907c3a4c3a)

--- pkgs/development/python-modules/anthropic/default.nix
+++ pkgs/development/python-modules/anthropic/default.nix
@@ -34,14 +34,14 @@
 
 buildPythonPackage rec {
   pname = "anthropic";
-  version = "0.71.0";
+  version = "0.75.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "anthropics";
     repo = "anthropic-sdk-python";
     tag = "v${version}";
-    hash = "sha256-n8hu2RFuZN0ojHhhU4qPQaT2DUvqSglFhBo2ORln/mc=";
+    hash = "sha256-wwaH9/o7qr8eXKvmKpj+/ubOdDydcXId/qiA6dXpd2I=";
   };
 
   postPatch = ''
