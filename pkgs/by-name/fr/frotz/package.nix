62e8b078ad1080066c97483da8a260757ffb03dc
Marcin Serwin <marcin@serwin.dev>
2025-11-21 18:31:02 +0100

    frotz: add override for frontend
    Signed-off-by: Marcin Serwin <marcin@serwin.dev>

--- pkgs/by-name/fr/frotz/package.nix
+++ pkgs/by-name/fr/frotz/package.nix
@@ -11,10 +11,25 @@
   ncurses,
   which,
   pkg-config,
+  SDL2,
+  SDL2_mixer,
+  zlib,
+  libjpeg,
+  libpng,
+  freetype,
+  frontend ? "ncurses",
 }:
 
+assert lib.assertOneOf "frontend" frontend [
+  "ncurses"
+  "sdl"
+  # NOTE: more options are present in the Makefile, e.g., x11, dumb, nosound, ...
+];
+let
+  progName = if frontend == "ncurses" then "frotz" else "sfrotz";
+in
 stdenv.mkDerivation (finalAttrs: {
-  pname = "frotz";
+  pname = progName;
   version = "2.55";
 
   src = fetchFromGitLab {
@@ -31,6 +46,9 @@ stdenv.mkDerivation (finalAttrs: {
       extraPrefix = "";
       hash = "sha256-ydIA1Td1ufp4y4Qfm5ijg9AY8z7cQ8BiX9hQz8gkKZY=";
     })
+
+    # https://gitlab.com/DavidGriffith/frotz/-/merge_requests/226
+    ./0001-Fix-SDL_SOUND_CFLAGS-usage.patch
   ];
 
   strictDeps = true;
@@ -45,16 +63,30 @@ stdenv.mkDerivation (finalAttrs: {
     libsamplerate
     libsndfile
     libvorbis
-    ncurses
-  ];
+  ]
+  ++ (
+    if frontend == "ncurses" then
+      [ ncurses ]
+    else
+      [
+        freetype
+        libjpeg
+        libpng
+        SDL2
+        SDL2_mixer
+        zlib
+      ]
+  );
 
-  installFlags = [ "PREFIX=$(out)" ];
+  makeFlags = [ "PREFIX=${placeholder "out"}" ];
+  buildFlags = [ frontend ];
+  installTargets = if frontend == "ncurses" then "install-frotz" else "install-${frontend}";
 
   meta = {
     homepage = "https://davidgriffith.gitlab.io/frotz/";
     changelog = "https://gitlab.com/DavidGriffith/frotz/-/raw/${finalAttrs.version}/NEWS";
-    description = "Z-machine interpreter for Infocom games and other interactive fiction";
-    mainProgram = "frotz";
+    description = "Z-machine interpreter for Infocom games and other interactive fiction (${frontend})";
+    mainProgram = progName;
     platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [
       nicknovitski
