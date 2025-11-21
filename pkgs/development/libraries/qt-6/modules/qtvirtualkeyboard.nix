a9a250b78db3b1168305db13e316281f99b9a260
Arnout Engelen <arnout@bzzt.net>
2025-11-21 14:44:54 +0100

    qt6.virtualkeyboard: cap build parallelism to 4
    Possible workaround for https://github.com/NixOS/nixpkgs/issues/436698
    
    My (admittedly nonscientific) tests suggest the nondeterminism
    doesn't show up with up to 4 cores, and the additional benefit
    of more cores probably starts to taper off at that point anyway.

--- pkgs/development/libraries/qt-6/modules/qtvirtualkeyboard.nix
+++ pkgs/development/libraries/qt-6/modules/qtvirtualkeyboard.nix
@@ -16,4 +16,8 @@ qtModule {
     hunspell
   ];
   nativeBuildInputs = [ pkg-config ];
+  preBuild = ''
+    # Max 4 cores as workaround for https://github.com/NixOS/nixpkgs/issues/463706
+    NIX_BUILD_CORES=$(echo -ne "$NIX_BUILD_CORES\n4" | sort -n | head -1)
+  '';
 }
