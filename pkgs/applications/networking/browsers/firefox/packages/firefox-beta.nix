35f2b8d4685f44b7b452c439d64359f337a1f8c2
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-22 11:22:34 +0000

    firefox-beta-unwrapped: 145.0b9 -> 146.0b6

--- pkgs/applications/networking/browsers/firefox/packages/firefox-beta.nix
+++ pkgs/applications/networking/browsers/firefox/packages/firefox-beta.nix
@@ -10,11 +10,11 @@
 buildMozillaMach rec {
   pname = "firefox-beta";
   binaryName = "firefox-beta";
-  version = "145.0b9";
+  version = "146.0b6";
   applicationName = "Firefox Beta";
   src = fetchurl {
     url = "mirror://mozilla/firefox/releases/${version}/source/firefox-${version}.source.tar.xz";
-    sha512 = "5dee0ec8bb66153d1a5905692c3e9b48e91a1d55728f9e173dfb08f9d0e781fcc8ba978a9e6a8948e56d26cb064bda7c0311260a8828752a1390fdb0e18ce769";
+    sha512 = "ff5b8d550c821c5bf87499315660cde27a255ed8cace2e9b892f28a4cc52cdaf81aa16aca56a3690568cd292f5c455ebef93e008266694c531aa9d64258af0e8";
   };
 
   meta = {
