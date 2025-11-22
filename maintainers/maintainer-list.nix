8490541ea8aab4cc1a3526ccbc337f20001aa9cf
kosumi <chankocyo@gmail.com>
2025-11-22 10:30:19 -0500

    Adopt package.nix of codex for code-rs

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -13950,6 +13950,12 @@
     githubId = 2037002;
     name = "Konstantinos";
   };
+  kosumi = {
+    email = "i@kosumi.dev";
+    github = "KaminariOS";
+    githubId = 74843779;
+    name = "kosumi";
+  };
   kotatsuyaki = {
     email = "kotatsuyaki@mail.kotatsu.dev";
     github = "kotatsuyaki";
