8ad450737a5495e21924b9801f9873f817e85114
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-22 08:36:53 +0000

    python3Packages.gwcs: 0.26.0 -> 0.26.1

--- pkgs/development/python-modules/gwcs/default.nix
+++ pkgs/development/python-modules/gwcs/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "gwcs";
-  version = "0.26.0";
+  version = "0.26.1";
   pyproject = true;
 
   disabled = pythonOlder "3.10";
@@ -26,7 +26,7 @@ buildPythonPackage rec {
     owner = "spacetelescope";
     repo = "gwcs";
     tag = version;
-    hash = "sha256-cJfNVX7rdJASQA3NmZt7d4pvYh6GAteR22jat0kccoo=";
+    hash = "sha256-S7+jDWCyQPQJAVgb/+BHC03+1aYyw2TK6SmkEz+k6u4=";
   };
 
   build-system = [
