5000435dd31e926336b1688b098e09aefd80ac2f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-22 01:42:27 +0000

    impala: 0.4.1 -> 0.5.0

--- pkgs/by-name/im/impala/package.nix
+++ pkgs/by-name/im/impala/package.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "impala";
-  version = "0.4.1";
+  version = "0.5.0";
 
   src = fetchFromGitHub {
     owner = "pythops";
     repo = "impala";
     rev = "v${version}";
-    hash = "sha256-CRnGycN2juXXNI1LhAH5HQbmXYatBZ0GxYKYgb5SBSE=";
+    hash = "sha256-sLEOMk6WlqEyvho48asd5htvxXRAtSLEvqVjT1Zqmhs=";
   };
 
-  cargoHash = "sha256-fBeSbJdFwT/ZwK2FTJQtZakKqMiAICMY2rkbNnYOGzU=";
+  cargoHash = "sha256-FfkAyNXF2c0pLpBWi78bS+N8EjgoAxbAixMwYXK9RZ8=";
 
   meta = {
     description = "TUI for managing wifi";
