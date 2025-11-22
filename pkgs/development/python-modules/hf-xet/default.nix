acb2377fbe9dc9d6bc7cc92826fa57e57d82d792
Gaetan Lepage <gaetan@glepage.com>
2025-11-22 17:28:33 +0000

    python3Packages.hf-xet: 1.1.10 -> 1.2.0
    Diff: https://github.com/huggingface/xet-core/compare/v1.1.10...v1.2.0
    
    Changelog: https://github.com/huggingface/xet-core/releases/tag/v1.2.0

--- pkgs/development/python-modules/hf-xet/default.nix
+++ pkgs/development/python-modules/hf-xet/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "hf-xet";
-  version = "1.1.10";
+  version = "1.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "huggingface";
     repo = "xet-core";
     tag = "v${version}";
-    hash = "sha256-dCyHRspViTIt8nc+KmmS6xJzL1oqRvCX4ndK2yGaf6A=";
+    hash = "sha256-nRxLVCJF3meoVa1mc3jt0hJUFDwfSFl7U/fFLDQL44M=";
   };
 
   sourceRoot = "${src.name}/hf_xet";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
       src
       sourceRoot
       ;
-    hash = "sha256-6XBKiMXTPtqbFtDVHZL23zYow/l6UYLjZOPPZdyKPwg=";
+    hash = "sha256-xMfsd7xOghktQu/do7TMmkUx4uTBHPK44XODePluUgc=";
   };
 
   nativeBuildInputs = [
