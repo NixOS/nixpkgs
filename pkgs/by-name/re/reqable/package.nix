e07ac3861d7ddae597a6b6bc1bfeb9cf4c0054d5
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-22 23:47:00 +0000

    reqable: 2.33.12 -> 3.0.27

--- pkgs/by-name/re/reqable/package.nix
+++ pkgs/by-name/re/reqable/package.nix
@@ -28,11 +28,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "reqable";
-  version = "2.33.12";
+  version = "3.0.27";
 
   src = fetchurl {
     url = "https://github.com/reqable/reqable-app/releases/download/${finalAttrs.version}/reqable-app-linux-x86_64.deb";
-    hash = "sha256-LCHeJUzTRjl/lh3PWygZV0Rd3AxJEGlTkVrI/5l+Go4=";
+    hash = "sha256-Pl5ejGUa74VNxu53t8GaLl1aiwD7PK5Uch7so2fw9Do=";
   };
 
   nativeBuildInputs = [
