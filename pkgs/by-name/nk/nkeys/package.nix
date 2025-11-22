c9fceec99b42005ff07790308433a3c469c65f5d
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-22 19:23:14 +0000

    nkeys: 0.4.11 -> 0.4.12

--- pkgs/by-name/nk/nkeys/package.nix
+++ pkgs/by-name/nk/nkeys/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "nkeys";
-  version = "0.4.11";
+  version = "0.4.12";
 
   src = fetchFromGitHub {
     owner = "nats-io";
     repo = "nkeys";
     tag = "v${version}";
-    hash = "sha256-dSkIT+KW+hT/Rk3NSkrb5ABLLiGGz2dppr9rwhjLOnM=";
+    hash = "sha256-uNvRzljA6c6ppFtZLpFmwz/c9fmDzQz44Ekys0rBZ+o=";
   };
 
-  vendorHash = "sha256-89DGLTkt9c8jJhAX3Uo8BBtLcBbnYE0q4mCqq/RGXM4=";
+  vendorHash = "sha256-yZc2qG61VsUDXr1uxF/BK7WFI6a7xOsjAxnw6G+JpeQ=";
 
   meta = {
     description = "Public-key signature system for NATS";
