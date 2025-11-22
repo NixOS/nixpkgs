500f49e60daa92215474451ad46a1533cceeb45e
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-22 16:13:32 +0100

    python313Packages.aiormq: 6.8.1 -> 6.9.2
    Diff: https://github.com/mosquito/aiormq/compare/6.8.1...6.9.2
    
    Changelog: https://github.com/mosquito/aiormq/releases/tag/6.9.2

--- pkgs/development/python-modules/aiormq/default.nix
+++ pkgs/development/python-modules/aiormq/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "aiormq";
-  version = "6.8.1";
+  version = "6.9.2";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "mosquito";
     repo = "aiormq";
     tag = version;
-    hash = "sha256-3+PoDB5Owy8BWBUisX0i1mV8rqs5K9pBFQwup8vKxlg=";
+    hash = "sha256-ApwL3okhpc3Dtq4bfWCCnoikyRx+4zPI9XtJ+qPKQdg=";
   };
 
   nativeBuildInputs = [
