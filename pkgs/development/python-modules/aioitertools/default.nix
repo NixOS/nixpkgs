788c9ebf0b749096e78ef7b524fe4102279368ba
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-22 16:12:57 +0100

    python313Packages.aioitertools: 0.12.0 -> 0.13.0
    Changelog: https://github.com/omnilib/aioitertools/blob/v0.13.0/CHANGELOG.md

--- pkgs/development/python-modules/aioitertools/default.nix
+++ pkgs/development/python-modules/aioitertools/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "aioitertools";
-  version = "0.12.0";
+  version = "0.13.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-wqkFW0+7dwX1YbnYYFPor10QzIRdIsMgCMQ0kLLY3Ws=";
+    hash = "sha256-YgvSQazAu7nsgZ8ashWGaHG0u9H3ODalX3mSAO6GlQw=";
   };
 
   build-system = [ flit-core ];
