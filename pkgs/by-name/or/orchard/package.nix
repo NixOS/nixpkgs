d2aadacff29e485d76c12d7c4e07edf1eb3b711f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-22 09:06:23 +0000

    orchard: 0.44.0 -> 0.45.0

--- pkgs/by-name/or/orchard/package.nix
+++ pkgs/by-name/or/orchard/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "orchard";
-  version = "0.44.0";
+  version = "0.45.0";
 
   src = fetchFromGitHub {
     owner = "cirruslabs";
     repo = "orchard";
     rev = version;
-    hash = "sha256-ALzXiH81i5P+sRxpEA3JI9oFmdbsnMCDfuOO74Sb+eA=";
+    hash = "sha256-4kWpMN92DWwWE53e9oZ4++MH1LI9327YFNqCBm9ZXGQ=";
     # populate values that require us to use git. By doing this in postFetch we
     # can delete .git afterwards and maintain better reproducibility of the src.
     leaveDotGit = true;
@@ -25,7 +25,7 @@ buildGoModule rec {
     '';
   };
 
-  vendorHash = "sha256-hL8gtGHdANY7s3DM2zzJhDYmbhW0hCNQHvV07x3FoSI=";
+  vendorHash = "sha256-qjOWsvG3qldBkYso0M71ZeciiUQK7I9wA56zBt+kIRk=";
 
   nativeBuildInputs = [ installShellFiles ];
 
