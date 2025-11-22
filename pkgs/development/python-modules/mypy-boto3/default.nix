3dd090527b8e9a0b3cb70753cf1d91bf7bd3d26c
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-22 13:33:33 +0100

    python312Packages.mypy-boto3-sagemaker: 1.41.1 -> 1.41.2

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1173,8 +1173,8 @@ in
       "sha256-Bi97uNPPzYVxPhs1OSYnQPq4HtQ11q9sZHxwRa4q7rM=";
 
   mypy-boto3-sagemaker =
-    buildMypyBoto3Package "sagemaker" "1.41.1"
-      "sha256-RvAe5ZhwEAoZyrNti8rFolGm03+h3vaMTyLUXsCc8ok=";
+    buildMypyBoto3Package "sagemaker" "1.41.2"
+      "sha256-bZLY2ucajAvbKEpYJVVdG7GB4ft6anSVqcsLYhUg2no=";
 
   mypy-boto3-sagemaker-a2i-runtime =
     buildMypyBoto3Package "sagemaker-a2i-runtime" "1.41.0"
