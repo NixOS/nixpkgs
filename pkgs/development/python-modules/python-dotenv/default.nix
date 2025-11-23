dd40b303f9909b3f73e7c17cb1f90fe7a3e089d1
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-11-23 23:29:13 +0100

    python3Packages.python-dotenv: 1.1.1 -> 1.2.1
    Diff: https://github.com/theskumar/python-dotenv/compare/v1.1.1...v1.2.1
    (cherry picked from commit c1c2b91f95df9eed4fd9af07554dc2e354045baf)

--- pkgs/development/python-modules/python-dotenv/default.nix
+++ pkgs/development/python-modules/python-dotenv/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "python-dotenv";
-  version = "1.1.1";
+  version = "1.2.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "theskumar";
     repo = "python-dotenv";
     tag = "v${version}";
-    hash = "sha256-GeN6/pnqhm7TTP+H9bKhJat6EwEl2EPl46mNSJWwFKk=";
+    hash = "sha256-YOwe/MHIyGdt6JqiwXwYi1cYxyPkGsBdUhjoG2Ks0y0=";
   };
 
   build-system = [ setuptools ];
