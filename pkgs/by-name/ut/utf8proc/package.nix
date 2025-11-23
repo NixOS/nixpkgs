b0710abb2e182240cf0bf74c7463d4ea1f4c3dca
Sergei Trofimovich <slyich@gmail.com>
2025-11-23 12:09:01 +0000

    utf8proc: 2.11.1 -> 2.11.2
    Changes: https://github.com/JuliaStrings/utf8proc/releases/tag/v2.11.2

--- pkgs/by-name/ut/utf8proc/package.nix
+++ pkgs/by-name/ut/utf8proc/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "utf8proc";
-  version = "2.11.1";
+  version = "2.11.2";
 
   src = fetchFromGitHub {
     owner = "JuliaStrings";
     repo = "utf8proc";
     rev = "v${version}";
-    hash = "sha256-fFeevzek6Oql+wMmkZXVzKlDh3wZ6AjGCKJFsXBaqzg=";
+    hash = "sha256-/+/IrsLQ9ykuVOaItd2ZbX60pPlP2omvS1qJz51AnWA=";
   };
 
   nativeBuildInputs = [ cmake ];
