6abd17d61bc31a87ea35cd2ac19f5c1091bede8a
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-23 08:04:14 +0000

    files-cli: 2.15.139 -> 2.15.152
    (cherry picked from commit f4768073aa1786b7ad8627ae014cdcfb7715a41a)

--- pkgs/by-name/fi/files-cli/package.nix
+++ pkgs/by-name/fi/files-cli/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule rec {
   pname = "files-cli";
-  version = "2.15.139";
+  version = "2.15.152";
 
   src = fetchFromGitHub {
     repo = "files-cli";
     owner = "files-com";
     rev = "v${version}";
-    hash = "sha256-4G0obQC2USQY9dGdLoOhfp6OvUYXRk/CsGeZHLpoMjg=";
+    hash = "sha256-0rXDVLr1V54+nXiqzhRsQJhF1JSreyQC9GcW3Sj2IcM=";
   };
 
-  vendorHash = "sha256-doL9LiFwPQ1RO6ZJzIQ4IBol2/J+Tc7e8dGDEVdpMQo=";
+  vendorHash = "sha256-BijznllCTXFgjADT/OZrgNYRM4aRXhxsIQu+jZngcFM=";
 
   ldflags = [
     "-s"
