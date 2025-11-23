1e97ad42edc1d20df04b83e7f85b14c8e0fff326
aleksana <me@aleksana.moe>
2025-11-23 10:52:16 +0800

    vsmartcard-vpcd: 0.9-unstable-2025-01-25 -> 0.10

--- pkgs/by-name/vs/vsmartcard-vpcd/package.nix
+++ pkgs/by-name/vs/vsmartcard-vpcd/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vsmartcard-vpcd";
-  version = "0.9-unstable-2025-01-25";
+  version = "0.10";
 
   src = fetchFromGitHub {
     owner = "frankmorgner";
     repo = "vsmartcard";
-    rev = "7369dae26bcb709845003ae2128b8db9df7031ae";
-    hash = "sha256-mfw/Yv12ceBVZIyAKJqBh+w4otj3rYYZbJUjKRLcsr4=";
+    tag = "virtualsmartcard-${finalAttrs.version}";
+    hash = "sha256-+BrX2aqByUvIUbN4K+sdq9bH29FD2rtTt4q+URPgx7A=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/virtualsmartcard";
