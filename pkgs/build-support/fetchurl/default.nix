45357774684b02fe054bc9693ff58bc265f7713f
Yueh-Shun Li <shamrocklee@posteo.net>
2025-11-24 05:11:05 +0800

    fetchurl: sort inherited variables

--- pkgs/build-support/fetchurl/default.nix
+++ pkgs/build-support/fetchurl/default.nix
@@ -300,11 +300,11 @@ lib.extendMkDerivation {
       curlOptsList = lib.escapeShellArgs curlOptsList;
 
       inherit
-        showURLs
-        mirrorsFile
-        postFetch
         downloadToTemp
         executable
+        mirrorsFile
+        postFetch
+        showURLs
         ;
 
       impureEnvVars = impureEnvVars ++ netrcImpureEnvVars;
