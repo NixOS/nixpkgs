0c92a9275aa58e830dec327853d3f2ec6ea07ab1
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-23 21:50:26 +0100

    python313Packages.awscrt: remove disabled

--- pkgs/development/python-modules/awscrt/default.nix
+++ pkgs/development/python-modules/awscrt/default.nix
@@ -6,15 +6,12 @@
   cmake,
   perl,
   stdenv,
-  pythonOlder,
 }:
 
 buildPythonPackage rec {
   pname = "awscrt";
   version = "0.29.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
