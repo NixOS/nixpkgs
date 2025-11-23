95c6d0162b914869daeac508565ae7c4cfde4416
Vladimír Čunát <v@cunat.cz>
2025-11-23 19:24:06 +0100

    libpng: patch CVE-2025-65018
    This one looks like the most severe from the bunch:
    https://www.openwall.com/lists/oss-security/2025/11/22/1
    
    I didn't feel like pulling several full libpng updates
    into the almost-dying NixOS 25.05, so picking this as a compromise,
    at least for now.  Certainly feel free to pick more.

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  fetchpatch,
   zlib,
   apngSupport ? true,
   testers,
@@ -26,6 +27,21 @@ stdenv.mkDerivation (finalAttrs: {
     url = "mirror://sourceforge/libpng/libpng-${finalAttrs.version}.tar.xz";
     hash = "sha256-86qLcAOZirkqTpkGwY0ZhT6Zn507ypvRZo9U+oFwfLE=";
   };
+
+  patches = [
+    # see https://github.com/pnggroup/libpng/security/advisories/GHSA-7wv6-48j4-hj3g
+    (fetchpatch {
+      name = "CVE-2025-65018.1.patch";
+      url = "https://github.com/pnggroup/libpng/commit/16b5e3823918840aae65c0a6da57c78a5a496a4d.diff";
+      hash = "sha256-pC7G/6IUCWkt9nE5RHnn8jT24KAo91y3f3+d2Us/gB0=";
+    })
+    (fetchpatch {
+      name = "CVE-2025-65018.2.patch";
+      url = "https://github.com/pnggroup/libpng/commit/218612ddd6b17944e21eda56caf8b4bf7779d1ea.diff";
+      hash = "sha256-jleVnBrsrTmRJqDo87slb7f8yETh3HN9geEwNBRZaGY=";
+    })
+  ];
+
   postPatch =
     whenPatched "gunzip < ${patch_src} | patch -Np1"
     + lib.optionalString stdenv.hostPlatform.isFreeBSD ''
