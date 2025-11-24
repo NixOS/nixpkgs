ab81d76efde40c349fd8dd1dacf5d4d9adafb79b
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-24 03:26:25 +0000

    stylance-cli: 0.7.1 -> 0.7.2

--- pkgs/by-name/st/stylance-cli/package.nix
+++ pkgs/by-name/st/stylance-cli/package.nix
@@ -5,14 +5,14 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "stylance-cli";
-  version = "0.7.1";
+  version = "0.7.2";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-Cdv+Lz+l0+8Jdk9stHACXDbUPedM/YryDMExdsqVvsU=";
+    hash = "sha256-ry/F1BbQKBkR06bwrmB/+y2XuUsREZNWhpTt4d9t9OM=";
   };
 
-  cargoHash = "sha256-cwgR5AHCeS9YkaJlyFxvEOrBXg7/tXNGXgtSEPHAwm4=";
+  cargoHash = "sha256-Xmfkk6sr4lAtD28PhUOMEax7aj4fCCJ90Aubobd2gOM=";
 
   meta = {
     description = "Library and cli tool for working with scoped CSS in rust";
