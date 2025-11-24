959384e7769cd3c70dad97662fb6438bc0298918
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2025-11-24 07:08:06 +0100

    maintainer: update caniko

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4266,7 +4266,10 @@
     github = "caniko";
     githubId = 29519599;
     name = "Can H. Tartanoglu";
-    keys = [ { fingerprint = "DF95 1EC0 9B8F 8094 C616  5589 1D63 6EDE 97DC 0280"; } ];
+    keys = [
+      { fingerprint = "DF95 1EC0 9B8F 8094 C616  5589 1D63 6EDE 97DC 0280"; }
+      { fingerprint = "818D 507F 1E62 139F 8A17  EAA6 4623 DEA0 6FDA CFE1"; }
+    ];
   };
   canndrew = {
     email = "shum@canndrew.org";
