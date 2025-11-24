c268790e7150a0d8ba2ecb9d46c414312189e078
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-24 01:37:55 +0000

    vuetorrent: 2.30.1 -> 2.31.0

--- pkgs/by-name/vu/vuetorrent/package.nix
+++ pkgs/by-name/vu/vuetorrent/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "vuetorrent";
-  version = "2.30.1";
+  version = "2.31.0";
 
   src = fetchFromGitHub {
     owner = "VueTorrent";
     repo = "VueTorrent";
     tag = "v${version}";
-    hash = "sha256-sek5kiO1T7s+PIIa0mBGj+9CfF56eRB3En9tsOcEK5Y=";
+    hash = "sha256-jWoD54cO1Tq9b2k8ySWUWQohT4qE0rW9EVJLoPi8DTA=";
   };
 
-  npmDepsHash = "sha256-vEgwEYVlyTJLOQ8j6hm1O4iTIXNPDZyrmvXyRBgEvQY=";
+  npmDepsHash = "sha256-/B/DMTH/5e9YNJ9rl+HTGkAX1KzOoBB1PD68Li2sAAw=";
 
   installPhase = ''
     runHook preInstall
