59d1f69048f0b592431632c931af0d3ef06ec126
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-24 12:57:35 +0000

    sshocker: 0.3.8 -> 0.3.9
    (cherry picked from commit 52722fc8cb15bb1d0704826660ba543ce493830f)

--- pkgs/by-name/ss/sshocker/package.nix
+++ pkgs/by-name/ss/sshocker/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule rec {
   pname = "sshocker";
-  version = "0.3.8";
+  version = "0.3.9";
 
   src = fetchFromGitHub {
     owner = "lima-vm";
     repo = "sshocker";
     tag = "v${version}";
-    hash = "sha256-uLotIvFpJL5keVyazG+g2TOKqcZwTDqSoAOaswqliNo=";
+    hash = "sha256-7s5jPMt6q7RUXxwA7rwVXhWGRrwfMc/EcEQxicwEHjs=";
   };
 
-  vendorHash = "sha256-uzKLAHxRRw0Bx7HjANOsX0tvfmbJhclT8SP346yFGwc=";
+  vendorHash = "sha256-FGaZTE8CCZ16ozsZr5MUFNkEy8+nkBYXH55n5TJG4Bg=";
 
   nativeInstallCheckInputs = [ versionCheckHook ];
 
