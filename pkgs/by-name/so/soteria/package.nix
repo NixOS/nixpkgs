6d5bd4bb2784c58a26ce09561bd31c0a97c10ffe
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-24 16:16:57 +0000

    soteria: 0.2.0 -> 0.2.2

--- pkgs/by-name/so/soteria/package.nix
+++ pkgs/by-name/so/soteria/package.nix
@@ -12,7 +12,7 @@
   polkit,
 }:
 let
-  version = "0.2.0";
+  version = "0.2.2";
 in
 rustPlatform.buildRustPackage {
   pname = "soteria";
@@ -22,10 +22,10 @@ rustPlatform.buildRustPackage {
     owner = "imvaskel";
     repo = "soteria";
     tag = "v${version}";
-    hash = "sha256-bZhxz6aycx7J3itInSsl2glbIs6OpIEkfSp3nYfPojk=";
+    hash = "sha256-OfWi2OYjcs3DYUuWi18N9EaIWgAHy3rNnmW0lcvQUvs=";
   };
 
-  cargoHash = "sha256-rxZRDx+5srBbMTVLMNH8liOqjyg90FvTaTT7g+3fq7E=";
+  cargoHash = "sha256-qIwHqDr6K7HsfaPvXgIROqFUq8fobDHR3YqLEtFcH+g=";
 
   nativeBuildInputs = [
     pkg-config
