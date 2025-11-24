39c5d77ceeb2f0e9fc29313c1562795b4e124c72
Guy Chronister <guychronister@outlook.com>
2025-11-24 14:01:25 +0000

    asmrepl: migrate to pkgs/by-name
    Moves asmrepl from pkgs/development/interpreters/asmrepl to
    pkgs/by-name/as/asmrepl following the new directory structure.
    Also renames default.nix to package.nix as per by-name conventions.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1693,8 +1693,6 @@ with pkgs;
 
   apprise = with python3Packages; toPythonApplication apprise;
 
-  asmrepl = callPackage ../development/interpreters/asmrepl { };
-
   avahi = callPackage ../development/libraries/avahi { };
 
   avahi-compat = callPackage ../development/libraries/avahi {
