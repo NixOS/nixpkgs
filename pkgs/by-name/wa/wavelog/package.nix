f2e7f8a6521672898209a443195296ef4c28eb3f
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-24 09:41:23 +0000

    wavelog: 2.1.2 -> 2.2

--- pkgs/by-name/wa/wavelog/package.nix
+++ pkgs/by-name/wa/wavelog/package.nix
@@ -8,13 +8,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "wavelog";
-  version = "2.1.2";
+  version = "2.2";
 
   src = fetchFromGitHub {
     owner = "wavelog";
     repo = "wavelog";
     tag = finalAttrs.version;
-    hash = "sha256-NXQY9ICU6YVVTnXb19pFqOx4VyTfxzk1RA03RqpEOeA=";
+    hash = "sha256-QbAM9dn86z7GniyZOdIHRN5qYeRGlBbYZ0jOUv7IoyI=";
   };
 
   installPhase = ''
