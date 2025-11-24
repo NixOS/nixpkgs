b2356314c88551e85fd7a1b5b2a97734e1fe9b5f
Valentin Chassignol <valentin.chassignol@infomaniak.com>
2025-11-24 21:04:36 +0100

    python313Packages.python-openstackclient: add octaviaclient as optional plugins

--- pkgs/development/python-modules/python-octaviaclient/default.nix
+++ pkgs/development/python-modules/python-octaviaclient/default.nix
@@ -2,30 +2,21 @@
   lib,
   buildPythonPackage,
   cliff,
-  doc8,
-  docutils,
   fetchPypi,
-  hacking,
   keystoneauth1,
   makePythonPath,
   openstackdocstheme,
   installer,
   osc-lib,
-  oslotest,
   oslo-serialization,
   oslo-utils,
   pbr,
-  pygments,
   python-neutronclient,
-  python-openstackclient,
   requests,
-  requests-mock,
   setuptools,
   sphinx,
   sphinxcontrib-apidoc,
-  stestr,
-  subunit,
-  testscenarios,
+  callPackage,
 }:
 
 buildPythonPackage rec {
@@ -39,8 +30,8 @@ buildPythonPackage rec {
     hash = "sha256-5brfxkpJQousEcXl0YerzYDjrfl0XyWV0RXPTz146Y4=";
   };
 
-  # somehow python-neutronclient cannot be found despite it being supplied
-  pythonRemoveDeps = [ "python-neutronclient" ];
+  # NOTE(vinetos): This explicit dependency is removed to avoid infinite recursion
+  pythonRemoveDeps = [ "python-openstackclient" ];
 
   build-system = [
     setuptools
@@ -57,40 +48,18 @@ buildPythonPackage rec {
     cliff
     keystoneauth1
     python-neutronclient
-    python-openstackclient
     osc-lib
     oslo-serialization
     oslo-utils
     requests
   ];
 
-  preInstall = ''
-    # TODO: I have really no idea why installer is missing...
-    export PYTHONPATH=$PYTHONPATH:${makePythonPath [ installer ]}
-  '';
+  # Checks moved to 'passthru.tests' to workaround infinite recursion
+  doCheck = false;
 
-  nativeCheckInputs = [
-    hacking
-    requests-mock
-    doc8
-    docutils
-    pygments
-    subunit
-    oslotest
-    stestr
-    testscenarios
-  ];
-
-  checkPhase = ''
-    runHook preCheck
-
-    # TODO: no idea why PYTHONPATH is broken here
-    export PYTHONPATH=$PYTHONPATH:${makePythonPath nativeCheckInputs}
-
-    stestr run
-
-    runHook postCheck
-  '';
+  passthru.tests = {
+    tests = callPackage ./tests.nix { };
+  };
 
   pythonImportsCheck = [ "octaviaclient" ];
 
