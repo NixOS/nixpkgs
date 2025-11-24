b5046855805e2d90d4ab4ae296f29cb5d142290a
redianthus <l@ndrs.fr>
2025-11-24 22:54:29 +0100

    ocamlPackages.logs: 0.9.0 -> 0.10.0

--- pkgs/development/ocaml-modules/logs/default.nix
+++ pkgs/development/ocaml-modules/logs/default.nix
@@ -3,7 +3,7 @@
   stdenv,
   fetchurl,
   ocaml,
-  version ? if lib.versionAtLeast ocaml.version "4.14" then "0.9.0" else "0.8.0",
+  version ? if lib.versionAtLeast ocaml.version "4.14" then "0.10.0" else "0.8.0",
   topkg,
   buildTopkgPackage,
   cmdlinerSupport ? true,
@@ -22,9 +22,9 @@ let
         minimalOCamlVersion = "4.03";
         sha512 = "c34c67b00d6a989a2660204ea70db8521736d6105f15d1ee0ec6287a662798fe5c4d47075c6e7c84f5d5372adb5af5c4c404f79db70d69140af5e0ebbea3b6a5";
       };
-      "0.9.0" = {
+      "0.10.0" = {
         minimalOCamlVersion = "4.14";
-        sha512 = "b75fb28e83f33461b06b5c9b60972c4a9a9a1599d637b4a0c7b1e86a87f34fe5361e817cb31f42ad7e7cbb822473b28fab9f58a02870eb189ebe88dae8e045ff";
+        sha512 = "122b7a77bd07aee1e0cb8e07e82b195a12528cf015e72fa0dd5afaae26ce04bad9b29f32a6d3bd3547fe522b8a036608785e8adb900e31580a0d555719bbb7e7";
       };
     }
     .${version};
