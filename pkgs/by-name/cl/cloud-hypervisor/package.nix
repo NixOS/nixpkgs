1859a07901b584f8e02a1d304ef01e83b6933243
Jonathan Davies <jpds@protonmail.com>
2025-11-24 16:30:10 +0000

    cloud-hypervisor: Add finalAttrs

--- pkgs/by-name/cl/cloud-hypervisor/package.nix
+++ pkgs/by-name/cl/cloud-hypervisor/package.nix
@@ -9,14 +9,14 @@
   versionCheckHook,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cloud-hypervisor";
   version = "49.0";
 
   src = fetchFromGitHub {
     owner = "cloud-hypervisor";
     repo = "cloud-hypervisor";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-bPPs/4XMcvOH4BGfQrjQdvgjGWae4UEZjzPKjalDN3w=";
   };
 
@@ -50,7 +50,7 @@ rustPlatform.buildRustPackage rec {
   meta = {
     homepage = "https://github.com/cloud-hypervisor/cloud-hypervisor";
     description = "Open source Virtual Machine Monitor (VMM) that runs on top of KVM";
-    changelog = "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/tag/v${version}";
+    changelog = "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/tag/v${finalAttrs.version}";
     license = with lib.licenses; [
       asl20
       bsd3
@@ -66,4 +66,4 @@ rustPlatform.buildRustPackage rec {
       "x86_64-linux"
     ];
   };
-}
+})
