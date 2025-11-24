3bc875379a3b75bd17ad448306b17c7d905662a5
David Wolff <nixpkgs@dav.dev>
2025-11-24 16:32:47 +0100

    stylance-cli: add version test

--- pkgs/by-name/st/stylance-cli/package.nix
+++ pkgs/by-name/st/stylance-cli/package.nix
@@ -2,10 +2,14 @@
   lib,
   rustPlatform,
   fetchCrate,
+  versionCheckHook,
 }:
-rustPlatform.buildRustPackage rec {
+let
   pname = "stylance-cli";
   version = "0.7.4";
+in
+rustPlatform.buildRustPackage {
+  inherit pname version;
 
   src = fetchCrate {
     inherit pname version;
@@ -13,6 +17,10 @@ rustPlatform.buildRustPackage rec {
   };
 
   cargoHash = "sha256-HWZQNEKTyNnmA1twN5cfo5RY2tOeCnL6zEp+M4F+Tqg=";
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  doInstallCheck = true;
+  versionCheckProgramArg = "--version";
 
   meta = {
     description = "Library and cli tool for working with scoped CSS in rust";
