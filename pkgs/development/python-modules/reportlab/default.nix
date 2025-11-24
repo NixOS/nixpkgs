a3b8d7a91529fd663f2142b7c8f620a2c29a7579
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-24 00:26:58 +0000

    python3Packages.reportlab: 4.4.3 -> 4.4.5

--- pkgs/development/python-modules/reportlab/default.nix
+++ pkgs/development/python-modules/reportlab/default.nix
@@ -17,7 +17,7 @@ let
 in
 buildPythonPackage rec {
   pname = "reportlab";
-  version = "4.4.3";
+  version = "4.4.5";
   pyproject = true;
 
   # See https://bitbucket.org/pypy/compatibility/wiki/reportlab%20toolkit
@@ -25,7 +25,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-BzsJddq2lTas0yUYWOawUk7T4IfnHx0NGJWstQrPnHs=";
+    hash = "sha256-BFfWQqp233s2sCNTSZBMWNj5xgaockVu0EQ2qvrcFRA=";
   };
 
   postPatch = ''
