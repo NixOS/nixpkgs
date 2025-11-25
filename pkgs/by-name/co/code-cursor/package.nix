2da52119364aab723a65f67486944b50b03e9859
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-25 17:45:36 +0000

    code-cursor: 2.0.64 -> 2.1.32

--- pkgs/by-name/co/code-cursor/package.nix
+++ pkgs/by-name/co/code-cursor/package.nix
@@ -16,20 +16,20 @@ let
 
   sources = {
     x86_64-linux = fetchurl {
-      url = "https://downloads.cursor.com/production/25412918da7e74b2686b25d62da1f01cfcd27683/linux/x64/Cursor-2.0.64-x86_64.AppImage";
-      hash = "sha256-zT9GhdwGDWZJQl+WpV2txbmp3/tJRtL6ds1UZQoKNzA=";
+      url = "https://downloads.cursor.com/production/ef979b1b43d85eee2a274c25fd62d5502006e425/linux/x64/Cursor-2.1.32-x86_64.AppImage";
+      hash = "sha256-CKLUa5qaT8njAyPMRz6+iX9KSYyvNoyLZFZi6wmR4g0=";
     };
     aarch64-linux = fetchurl {
-      url = "https://downloads.cursor.com/production/25412918da7e74b2686b25d62da1f01cfcd27683/linux/arm64/Cursor-2.0.64-aarch64.AppImage";
-      hash = "sha256-1pN9LfnplKyVUxlICQ2KsxcAn++dZY9hGR4XubHxLUY=";
+      url = "https://downloads.cursor.com/production/ef979b1b43d85eee2a274c25fd62d5502006e425/linux/arm64/Cursor-2.1.32-aarch64.AppImage";
+      hash = "sha256-qQV4D+C0+ncPA7i9A5Q1vVLV8XWMb2/DSwOqU0nhkmk=";
     };
     x86_64-darwin = fetchurl {
-      url = "https://downloads.cursor.com/production/25412918da7e74b2686b25d62da1f01cfcd27683/darwin/x64/Cursor-darwin-x64.dmg";
-      hash = "sha256-lY5BJeauw5VtWuaAu8C9C2inmKFvv/OnCxOicE2Zs48=";
+      url = "https://downloads.cursor.com/production/ef979b1b43d85eee2a274c25fd62d5502006e425/darwin/x64/Cursor-darwin-x64.dmg";
+      hash = "sha256-MbW7kTXY+UjMv4FIy3L6Vjm3TXySVoxHlGEA0uad4Uo=";
     };
     aarch64-darwin = fetchurl {
-      url = "https://downloads.cursor.com/production/25412918da7e74b2686b25d62da1f01cfcd27683/darwin/arm64/Cursor-darwin-arm64.dmg";
-      hash = "sha256-IIJbuRdxTG7kSspspWk8GH9KZsKPyLJahz0iqSvP1B0=";
+      url = "https://downloads.cursor.com/production/ef979b1b43d85eee2a274c25fd62d5502006e425/darwin/arm64/Cursor-darwin-arm64.dmg";
+      hash = "sha256-CUlx9iUOWP8FfRC0r7CIyYgyVmLd0+aO6doGX2/0Utc=";
     };
   };
 
@@ -39,7 +39,7 @@ in
   inherit useVSCodeRipgrep;
   commandLineArgs = finalCommandLineArgs;
 
-  version = "2.0.64";
+  version = "2.1.32";
   pname = "cursor";
 
   # You can find the current VSCode version in the About dialog:
