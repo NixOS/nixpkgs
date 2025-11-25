a4b91049f875b2da64cd59764e1bd9db0ed787b6
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-25 16:39:05 +0000

    python3Packages.dissect-regf: 3.13 -> 3.14

--- pkgs/development/python-modules/dissect-regf/default.nix
+++ pkgs/development/python-modules/dissect-regf/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "dissect-regf";
-  version = "3.13";
+  version = "3.14";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -21,7 +21,7 @@ buildPythonPackage rec {
     owner = "fox-it";
     repo = "dissect.regf";
     tag = version;
-    hash = "sha256-O2BKOzv0nFQ8rCgTCgYowQTptR1asuJBroqTNeDIIak=";
+    hash = "sha256-bzRx+GwmT+tqCIIu/cJkbnwvkiGbZrtoUFNiirZFp28=";
   };
 
   nativeBuildInputs = [
