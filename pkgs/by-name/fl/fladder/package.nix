49ec76a410dafabac7b7a62b2522f3310cba696c
Aiden Schembri <aidsch0605@outlook.com>
2025-11-25 19:04:15 +0100

    adjusted packages

--- pkgs/by-name/fl/fladder/package.nix
+++ pkgs/by-name/fl/fladder/package.nix
@@ -3,11 +3,27 @@
   stdenv,
   flutter335,
   fetchFromGitHub,
+  fetchurl,
   copyDesktopItems,
   makeDesktopItem,
   mpv-unwrapped,
   sqlite,
+  alsa-lib,
+  libepoxy,
 }:
+let
+  # MDK SDK required by fvp plugin
+  mdk-sdk = fetchurl {
+    url = "https://sourceforge.net/projects/mdk-sdk/files/nightly/mdk-sdk-linux-x64.tar.xz";
+    hash = "sha256-eFfcMNgjns89BS3LxJ0Ts1qnaQLn92hrKxkXeAsJ1Z4=";
+  };
+
+  # mimalloc required by media_kit_libs_linux
+  mimalloc = fetchurl {
+    url = "https://github.com/microsoft/mimalloc/archive/refs/tags/v2.1.2.tar.gz";
+    hash = "sha256-Kxv/b3F/lyXHC/jXnkeG2hPeiicAWeS6C90mKue+Rus=";
+  };
+in
 flutter335.buildFlutterApplication {
   pname = "fladder";
   version = "0.8.0";
@@ -28,6 +44,8 @@ flutter335.buildFlutterApplication {
   buildInputs = [
     mpv-unwrapped
     sqlite
+    alsa-lib
+    libepoxy
   ];
 
   # Git dependencies from pubspec.lock.json
@@ -81,8 +99,40 @@ flutter335.buildFlutterApplication {
         inherit version src;
         inherit (src) passthru;
 
-        # No patching needed for media_kit_libs_linux
-        dontPatch = true;
+        # Patch CMakeLists.txt to use our pre-downloaded mimalloc instead of downloading it
+        postPatch = ''
+          # Replace the download_and_verify call with a copy command using sed
+          sed -i '/download_and_verify(/,/)/{
+            /download_and_verify(/c\  execute_process(COMMAND ''${CMAKE_COMMAND} -E copy ${mimalloc} ''${MIMALLOC_ARCHIVE})
+            /MIMALLOC_URL/d
+            /MIMALLOC_MD5/d
+            /MIMALLOC_ARCHIVE/d
+            /^  )$/d
+          }' libs/linux/media_kit_libs_linux/linux/CMakeLists.txt
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir $out
+          cp -r ./* $out/
+
+          runHook postInstall
+        '';
+      };
+
+    fvp =
+      { version, src, ... }:
+      stdenv.mkDerivation {
+        pname = "fvp";
+        inherit version src;
+        inherit (src) passthru;
+
+        postPatch = ''
+          # Pre-provision the MDK SDK by extracting it directly to avoid downloading during build
+          mkdir -p linux
+          tar -xf ${mdk-sdk} -C linux
+        '';
 
         installPhase = ''
           runHook preInstall
@@ -98,8 +148,8 @@ flutter335.buildFlutterApplication {
   postInstall = ''
     # Install icon
     for size in 16 32 48 64 128 256 512; do
-      install -Dm644 assets/icon/icon-\''${size}x\''${size}.png \
-        $out/share/icons/hicolor/\''${size}x\''${size}/apps/fladder.png
+      install -Dm644 assets/icon/icon-''${size}x''${size}.png \
+        $out/share/icons/hicolor/''${size}x''${size}/apps/fladder.png
     done
     
     # Fallback to a main icon if specific sizes don't exist
