639521b138cde16e736ec57d197b33516fc9573c
Robert Rose <robert.rose@mailbox.org>
2025-11-26 22:29:25 +0100

    rke2_1_31: 1.31.11+rke2r1 -> 1.31.14+rke2r1
    https://github.com/rancher/rke2/releases/tag/v1.31.14%2Brke2r1

--- pkgs/applications/networking/cluster/rke2/1_31/versions.nix
+++ pkgs/applications/networking/cluster/rke2/1_31/versions.nix
@@ -1,12 +1,12 @@
 {
-  rke2Version = "1.31.11+rke2r1";
-  rke2Commit = "48500b495b74b976f07ebd07a643e40e6c89f355";
-  rke2TarballHash = "sha256-JPoeNB9jM4h+K5vKWi2KQfPJPTUftxwXYJY6eZN50Zs=";
-  rke2VendorHash = "sha256-CxNo9HcxuSS0LWawefF0VbtMDofQUzoCzTfBhWAUVrc=";
-  k8sImageTag = "v1.31.11-rke2r1-build20250716";
-  etcdVersion = "v3.5.21-k3s1-build20250612";
+  rke2Version = "1.31.14+rke2r1";
+  rke2Commit = "a594ea8cf80ad7b2b55f1b0286b34c8af7d53c30";
+  rke2TarballHash = "sha256-1IQdJPF546DX7eHPoA0rIfUJs3uZSTDXfn5OnJbU7FQ=";
+  rke2VendorHash = "sha256-HmJyX7QsuB5g0bajgExMi2rg9jTTTUZiuYXlrfNA5xo=";
+  k8sImageTag = "v1.31.14-rke2r1-build20251112";
+  etcdVersion = "v3.5.21-k3s1-build20251017";
   pauseVersion = "3.6";
-  ccmVersion = "v1.31.9-rc1.0.20250516171836-812206503b28-build20250612";
-  dockerizedVersion = "v1.31.11-rke2r1";
+  ccmVersion = "v1.31.14-0.20251010190929-c49b201b7cf5-build20251017";
+  dockerizedVersion = "v1.31.14-rke2r1";
   imagesVersions = with builtins; fromJSON (readFile ./images-versions.json);
 }
