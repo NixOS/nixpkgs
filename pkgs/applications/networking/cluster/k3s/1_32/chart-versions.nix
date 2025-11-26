19fdd4a20998cead97ff92f2c928582d55acd668
Robert Rose <robert.rose@mailbox.org>
2025-11-26 20:42:13 +0100

    k3s_1_32: 1.32.9+k3s1 -> 1.32.10+k3s1
    https://github.com/k3s-io/k3s/releases/tag/v1.32.10%2Bk3s1

--- pkgs/applications/networking/cluster/k3s/1_32/chart-versions.nix
+++ pkgs/applications/networking/cluster/k3s/1_32/chart-versions.nix
@@ -1,10 +1,10 @@
 {
   traefik-crd = {
-    url = "https://k3s.io/k3s-charts/assets/traefik-crd/traefik-crd-34.2.1+up34.2.0.tgz";
-    sha256 = "0bnvkp1bxfsccgdnvpq3ni3p2h8jyvj0iclbqj0rsls8vv1jnp9z";
+    url = "https://k3s.io/k3s-charts/assets/traefik-crd/traefik-crd-37.1.1+up37.1.0.tgz";
+    sha256 = "0q568ffjhxmw87fzwafxlxrzx2lgcqlqbwj87pbc2xszh9pyakyd";
   };
   traefik = {
-    url = "https://k3s.io/k3s-charts/assets/traefik/traefik-34.2.1+up34.2.0.tgz";
-    sha256 = "16ljy2ncn2kc4kr3m93w2kfw4vjk8is3cp3i2lx4b7afbmi75zyw";
+    url = "https://k3s.io/k3s-charts/assets/traefik/traefik-37.1.1+up37.1.0.tgz";
+    sha256 = "0gpcr6zfbncvp2sjjwzg732k4xfr5ba0pbc5x08lgwvibqpp4r27";
   };
 }
