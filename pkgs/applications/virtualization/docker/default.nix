0ee23d0b4a44f2fa8a39937ae9ac394f5e5b43d8
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-26 07:17:14 +0000

    docker: 28.5.1 -> 29.0.4

--- pkgs/applications/virtualization/docker/default.nix
+++ pkgs/applications/virtualization/docker/default.nix
@@ -409,12 +409,12 @@ in
 
   docker_28 =
     let
-      version = "28.5.1";
+      version = "29.0.4";
     in
     callPackage dockerGen {
       inherit version;
       cliRev = "v${version}";
-      cliHash = "sha256-iT5FLzX8Pg07V0Uo+07gy3ChP/WgLTPs/vtxnFVmCG8=";
+      cliHash = "sha256-mh2JRTHUIn+1YwfU8oHvnVytrjgMSMNf9tBYx2QbsW0=";
       mobyRev = "v${version}";
       mobyHash = "sha256-IlkEK4UeQjZsojbahzLy/rP3WqJUWXG9nthmBSEj10M=";
       runcRev = "v1.3.0";
