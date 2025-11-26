ac7a77b34fff53be5ccdc408d8ccafb8235fd224
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-26 21:35:18 +0000

    matugen: 3.0.0 -> 3.1.0

--- pkgs/by-name/ma/matugen/package.nix
+++ pkgs/by-name/ma/matugen/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "matugen";
-  version = "3.0.0";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "InioX";
     repo = "matugen";
     tag = "v${version}";
-    hash = "sha256-2VcdnUjIOEMQ87K5wv+Pbgko94PLygp1nrEYcVHk1v4=";
+    hash = "sha256-TD9XyqFdLIOLRZM7ozQ8gz4PyEQbLGLxB4MbzjLccg4=";
   };
 
-  cargoHash = "sha256-HBAsCrD/njZUYkjcaUaTS7xMwfUWLpDXkpIPWSdCvqo=";
+  cargoHash = "sha256-OdJxr01wHqPHgEGIVrLcUv5h1JaYrY1zW9NrYO114OM=";
 
   meta = {
     description = "Material you color generation tool";
