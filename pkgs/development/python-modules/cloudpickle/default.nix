2a361f3607e220e50da691d961df52f69158f555
Robert Schütz <nix@dotlambda.de>
2025-11-26 10:39:03 -0800

    python3Packages.cloudpickle: modernize

--- pkgs/development/python-modules/cloudpickle/default.nix
+++ pkgs/development/python-modules/cloudpickle/default.nix
@@ -2,13 +2,11 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
 
   # build-system
   flit-core,
 
   # tests
-  psutil,
   pytestCheckHook,
 }:
 
@@ -17,8 +15,6 @@ buildPythonPackage rec {
   version = "3.1.2";
   pyproject = true;
 
-  disabled = pythonOlder "3.6";
-
   src = fetchFromGitHub {
     owner = "cloudpipe";
     repo = "cloudpickle";
@@ -26,10 +22,9 @@ buildPythonPackage rec {
     hash = "sha256-BsCOEpNCNqq8PS+SdbzF1wq0LXEmtcHJs0pdt2qFw/w=";
   };
 
-  nativeBuildInputs = [ flit-core ];
+  build-system = [ flit-core ];
 
   nativeCheckInputs = [
-    psutil
     pytestCheckHook
   ];
 
@@ -37,14 +32,15 @@ buildPythonPackage rec {
 
   disabledTestPaths = [
     # ModuleNotFoundError: No module named 'psutil'
+    # (because _make_cwd_env() overwrites $PYTHONPATH)
     "tests/cloudpickle_test.py"
   ];
 
-  meta = with lib; {
+  meta = {
     changelog = "https://github.com/cloudpipe/cloudpickle/blob/${src.tag}/CHANGES.md";
     description = "Extended pickling support for Python objects";
     homepage = "https://github.com/cloudpipe/cloudpickle";
-    license = with licenses; [ bsd3 ];
+    license = lib.licenses.bsd3;
     maintainers = [ ];
   };
 }
