768d8a5fe134dc79b97dbea43af35912eb05f667
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-26 20:15:26 +0000

    hyprlandPlugins.hypr-darkwindow: 0.51.1 -> 0.52.1

--- pkgs/applications/window-managers/hyprwm/hyprland-plugins/hypr-darkwindow.nix
+++ pkgs/applications/window-managers/hyprwm/hyprland-plugins/hypr-darkwindow.nix
@@ -7,13 +7,13 @@
 
 mkHyprlandPlugin (finalAttrs: {
   pluginName = "hypr-darkwindow";
-  version = "0.51.1";
+  version = "0.52.1";
 
   src = fetchFromGitHub {
     owner = "micha4w";
     repo = "Hypr-DarkWindow";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-jq5j459gCVuBOpuGEvXe+9/O+HAineFxQI4sIcEPi/c=";
+    hash = "sha256-0jqliHlmW/lEDC3X8tYrqdY6d15h1hF40p32r4Nsa4w=";
   };
 
   installPhase = ''
