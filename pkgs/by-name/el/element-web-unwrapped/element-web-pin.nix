d45d12e45162c019a1b6921dc1d95cca373c4459
Akshay Mankar <itsakshaymankar@gmail.com>
2025-11-26 20:50:52 +0100

    element-web-unwrapped: Build shared-components
    - add comment for offlineCacheSharedComponents
    - add webSharedComponentsYarnHash to update.sh

--- pkgs/by-name/el/element-web-unwrapped/element-web-pin.nix
+++ pkgs/by-name/el/element-web-unwrapped/element-web-pin.nix
@@ -3,5 +3,6 @@
   "hashes" = {
     "webSrcHash" = "sha256-5sRKVgKbU4hr1cubY/0PdLDClBcFGxdu9TITORL6beI=";
     "webYarnHash" = "sha256-S+glH2HkGyINEGHWn1TMK2y45gk7WtxTe+u4luqVgsw=";
+    "webSharedComponentsYarnHash" = "sha256-azvUG/Oyq38q/llJd0AtEliFCX0+TrEtzfHsd+UpaEs=";
   };
 }
