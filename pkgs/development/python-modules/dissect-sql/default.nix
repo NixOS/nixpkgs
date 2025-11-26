457a015cdb6206fad537913837489b536d44c9a0
Fabian Affolter <mail@fabian-affolter.ch>
2025-11-26 23:11:46 +0100

    python313Packages.dissect-sql: remove disabled

--- pkgs/development/python-modules/dissect-sql/default.nix
+++ pkgs/development/python-modules/dissect-sql/default.nix
@@ -5,7 +5,6 @@
   dissect-util,
   fetchFromGitHub,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
   setuptools-scm,
 }:
@@ -14,8 +13,6 @@ buildPythonPackage rec {
   pname = "dissect-sql";
   version = "3.13";
   pyproject = true;
-
-  disabled = pythonOlder "3.10";
 
   src = fetchFromGitHub {
     owner = "fox-it";
