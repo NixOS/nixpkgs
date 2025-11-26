c4e4d9b3ea4f5d1e62dc264fc050d0cca66b4728
Leona Maroni <dev@leona.is>
2025-11-26 20:49:24 +0100

    Revert "gitlab-runner: apply patch for file variable handling"
    This reverts commit f99787269a50f51d9c4a3f6aba51925588540e30.

--- pkgs/by-name/gi/gitlab-runner/package.nix
+++ pkgs/by-name/gi/gitlab-runner/package.nix
@@ -4,7 +4,6 @@
   bash,
   buildGoModule,
   fetchFromGitLab,
-  fetchpatch,
   nix-update-script,
   versionCheckHook,
 }:
@@ -28,12 +27,6 @@ buildGoModule (finalAttrs: {
   patches = [
     ./fix-shell-path.patch
     ./remove-bash-test.patch
-    # fix regression. remove with next release.
-    (fetchpatch {
-      name = "fix-shell-executor-not-working-with-variables-that-use-file-variables.patch";
-      url = "https://gitlab.com/gitlab-org/gitlab-runner/-/commit/6318fe8e38ca9774eb0f52fa2c68555cdad3ab44.patch";
-      hash = "sha256-GTdBo+7kHHpNs6JywjOII4NBcHjFYEZ3xhdGTcGKov4=";
-    })
   ];
 
   prePatch = ''
