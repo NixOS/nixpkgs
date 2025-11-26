659240cd4b206cd21fecd9693144d488b49240b0
Aiden Schembri <aidsch0605@outlook.com>
2025-11-26 12:21:24 +0100

    removed extra comments

--- pkgs/development/compilers/dart/package-source-builders/media_kit_libs_linux_donutware/default.nix
+++ pkgs/development/compilers/dart/package-source-builders/media_kit_libs_linux_donutware/default.nix
@@ -6,7 +6,6 @@
 { version, src, ... }:
 
 let
-  # mimalloc required by media_kit_libs_linux
   mimalloc = fetchurl {
     url = "https://github.com/microsoft/mimalloc/archive/refs/tags/v2.1.2.tar.gz";
     hash = "sha256-Kxv/b3F/lyXHC/jXnkeG2hPeiicAWeS6C90mKue+Rus=";
@@ -17,9 +16,7 @@ stdenv.mkDerivation {
   inherit version src;
   inherit (src) passthru;
 
-  # Patch CMakeLists.txt to use our pre-downloaded mimalloc instead of downloading it
   postPatch = ''
-    # Replace the download_and_verify call with a copy command using sed
     sed -i '/download_and_verify(/,/)/{
       /download_and_verify(/c\  execute_process(COMMAND ''${CMAKE_COMMAND} -E copy ${mimalloc} ''${MIMALLOC_ARCHIVE})
       /MIMALLOC_URL/d
@@ -31,10 +28,8 @@ stdenv.mkDerivation {
 
   installPhase = ''
     runHook preInstall
-
     mkdir $out
     cp -r ./* $out/
-
     runHook postInstall
   '';
 }
