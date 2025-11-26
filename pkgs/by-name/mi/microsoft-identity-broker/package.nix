7227bf8c4cf0e872c939ebc44a957534d410fd45
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-11-27 01:49:44 +0200

    microsoft-identity-broker: 2.0.1 -> 2.0.3

--- pkgs/by-name/mi/microsoft-identity-broker/package.nix
+++ pkgs/by-name/mi/microsoft-identity-broker/package.nix
@@ -3,96 +3,84 @@
   lib,
   fetchurl,
   dpkg,
-  openjdk11,
-  jnr-posix,
-  makeWrapper,
-  zip,
   nixosTests,
-  bash,
-  glib,
+  libuuid,
   xorg,
-  alsa-lib,
+  curlMinimal,
+  openssl_3,
+  libsecret,
+  webkitgtk_4_1,
+  libsoup_3,
+  gtk3,
+  atk,
+  pango,
+  glib,
+  gdk-pixbuf,
+  harfbuzz,
+  p11-kit,
+  cairo,
+  zlib,
+  dbus,
 }:
+let
+  curlMinimal_openssl_3 = curlMinimal.override {
+    openssl = openssl_3;
+  };
+
+  runtimeLibPath = lib.makeLibraryPath [
+    stdenv.cc.cc
+    libuuid
+    xorg.libX11
+    curlMinimal_openssl_3
+    openssl_3
+    libsecret
+    webkitgtk_4_1
+    libsoup_3
+    gtk3
+    atk
+    glib
+    pango
+    harfbuzz
+    cairo
+    p11-kit
+    gdk-pixbuf
+    zlib
+    dbus
+  ];
+in
 stdenv.mkDerivation rec {
   pname = "microsoft-identity-broker";
-  version = "2.0.1";
+  version = "2.0.3";
 
   src = fetchurl {
     url = "https://packages.microsoft.com/ubuntu/24.04/prod/pool/main/m/microsoft-identity-broker/microsoft-identity-broker_${version}_amd64.deb";
-    hash = "sha256-JheJnsu1ZxJbcpt0367FqfHVdwWWvPem2fm0i8s7MGE=";
+    hash = "sha256-vorPf5pvNLABwntiDdfDSiubg1jbHaKK/o0fFkbZ000=";
   };
 
-  nativeBuildInputs = [
-    dpkg
-    makeWrapper
-    openjdk11
-    zip
-  ];
-
-  buildInputs = [
-    glib
-    xorg.libXtst
-    alsa-lib
-  ];
-
-  buildPhase = ''
-    runHook preBuild
-    rm opt/microsoft/identity-broker/lib/jnr-posix-3.1.4.jar
-    runHook postBuild
-  '';
+  nativeBuildInputs = [ dpkg ];
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/lib/microsoft-identity-broker
-    cp -a opt/microsoft/identity-broker/lib/* $out/lib/microsoft-identity-broker
-    cp -a usr/* $out
-    for jar in $out/lib/microsoft-identity-broker/*.jar; do
-      classpath="$classpath:$jar"
-    done
-    classpath="$classpath:${jnr-posix}/share/java/jnr-posix-${jnr-posix.version}.jar"
-    mkdir -p $out/bin
-    makeWrapper ${openjdk11}/bin/java $out/bin/microsoft-identity-broker \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
-      --add-flags "-classpath $classpath" \
-      --add-flags "-Xmx256m -Xss256k -XX:+UseParallelGC -XX:ParallelGCThreads=1" \
-      --add-flags "-verbose" \
-      --add-flags "-Djava.net.useSystemProxies=true" \
-      --add-flags "com.microsoft.identity.broker.service.IdentityBrokerService"
+    mkdir -p $out
 
-    makeWrapper ${openjdk11}/bin/java $out/bin/microsoft-identity-device-broker \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
-      --add-flags "-classpath $classpath" \
-      --add-flags "-Xmx256m -Xss256k -XX:+UseParallelGC -XX:ParallelGCThreads=1" \
-      --add-flags "-verbose" \
-      --add-flags "-Djava.net.useSystemProxies=true" \
-      --add-flags "com.microsoft.identity.broker.service.DeviceBrokerService"
+    patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) --set-rpath ${runtimeLibPath} usr/bin/microsoft-identity-broker
+    patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) --set-rpath ${runtimeLibPath} usr/bin/microsoft-identity-device-broker
+
+    cp -a usr/* $out
 
     runHook postInstall
   '';
 
   postInstall = ''
-    substituteInPlace \
-      $out/lib/systemd/user/microsoft-identity-broker.service \
-      $out/lib/systemd/system/microsoft-identity-device-broker.service \
-      $out/share/dbus-1/system-services/com.microsoft.identity.devicebroker1.service \
-      $out/share/dbus-1/services/com.microsoft.identity.broker1.service \
-      --replace \
-        ExecStartPre=sh \
-        ExecStartPre=${bash}/bin/sh \
-      --replace \
-        ExecStartPre=!sh \
-        ExecStartPre=!${bash}/bin/sh \
-      --replace \
-        /opt/microsoft/identity-broker/bin/microsoft-identity-broker \
-        $out/bin/microsoft-identity-broker \
-      --replace \
-        /opt/microsoft/identity-broker/bin/microsoft-identity-device-broker \
-        $out/bin/microsoft-identity-device-broker \
-      --replace \
-        /usr/lib/jvm/java-11-openjdk-amd64 \
-        ${openjdk11}
+    substituteInPlace $out/share/dbus-1/services/com.microsoft.identity.broker1.service \
+      --replace-fail /usr/bin/microsoft-identity-broker $out/bin/microsoft-identity-broker
+
+    substituteInPlace $out/lib/systemd/system/microsoft-identity-device-broker.service \
+      --replace-fail /usr/bin/microsoft-identity-device-broker $out/bin/microsoft-identity-device-broker
   '';
+
+  dontPatchELF = true;
 
   passthru = {
     updateScript = ./update.sh;
