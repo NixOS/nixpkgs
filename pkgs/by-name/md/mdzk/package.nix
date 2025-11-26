2ab0c48b03b311eae13fb8abcb1e73c6ea2e3533
Bryan A. S. <bryanasdev000@gmail.com>
2025-11-26 12:56:43 -0300

    maintainers: drop bryanasdev000

--- pkgs/by-name/md/mdzk/package.nix
+++ pkgs/by-name/md/mdzk/package.nix
@@ -29,7 +29,6 @@ rustPlatform.buildRustPackage rec {
     changelog = "https://github.com/mdzk-rs/mdzk/blob/main/CHANGELOG.md";
     license = licenses.mpl20;
     maintainers = with maintainers; [
-      bryanasdev000
       ratsclub
     ];
     mainProgram = "mdzk";
