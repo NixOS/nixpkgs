7f8c4b75fdcd3e66be10f5ff27f62735af70048a
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-26 15:07:24 +0000

    python3Packages.onnxslim: 0.1.70 -> 0.1.76

--- pkgs/development/python-modules/onnxslim/default.nix
+++ pkgs/development/python-modules/onnxslim/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "onnxslim";
-  version = "0.1.70";
+  version = "0.1.76";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "inisis";
     repo = "OnnxSlim";
     tag = "v${version}";
-    hash = "sha256-xShmJR0GWuGmuM0LZ0nBLDoC0m7c0iSWolUGUscVotA=";
+    hash = "sha256-mQzjxniA8HZ+dRLUzDgjv0AGwdaqUp1Rp5+2DPOBtAA=";
   };
 
   build-system = [
