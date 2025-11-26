61b15943a1245d5ea0c84e7cb0576df92e5202e1
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-26 17:21:36 +0000

    python3Packages.brother: 5.1.1 -> 6.0.0

--- pkgs/development/python-modules/brother/default.nix
+++ pkgs/development/python-modules/brother/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "brother";
-  version = "5.1.1";
+  version = "6.0.0";
   pyproject = true;
 
   disabled = pythonOlder "3.12";
@@ -24,7 +24,7 @@ buildPythonPackage rec {
     owner = "bieniu";
     repo = "brother";
     tag = version;
-    hash = "sha256-CrPtDHMz1vHprYD1IkR4J0WEY43kGDm+e7kTzanAiR0=";
+    hash = "sha256-kvR4891kaZFHKZhtgrrgtGJZUIwoYE5CsGUEQM7eKvs=";
   };
 
   build-system = [ setuptools ];
