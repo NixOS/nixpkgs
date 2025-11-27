NixOS/nixpkgs

commit 7a24e8bdb6258cb7d69470cc92ad1adf001d2f3a

vte: 0.80.3 → 0.81.90
https://gitlab.gnome.org/GNOME/vte/-/compare/0.80.3...0.81.90

Changelog-Reviewed-By: Jan Tojnar <jtojnar@gmail.com>
Co-authored-by: Bobby Rong <rjl931189261@126.com>

pkgs/by-name/vt/vte/package.nix
@@ -23,9 +23,11 @@
   pango,
   pcre2,
   cairo,
+  fmt_11,
   fribidi,
   lz4,
   icu,
+  simdutf,
   systemd,
   systemdSupport ? lib.meta.availableOn stdenv.hostPlatform systemd,
   fast-float,
@@ -36,7 +38,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "vte";
-  version = "0.80.3";
+  version = "0.81.90";
 
   outputs = [
     "out"
@@ -46,7 +48,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/vte/${lib.versions.majorMinor finalAttrs.version}/vte-${finalAttrs.version}.tar.xz";
-    hash = "sha256-Lllv0/vqu3FTFmIiTnH2osN/aEQmE21ihUYnJ2709pk=";
+    hash = "sha256-kLJhqwJZf+6UH3GmV+v50OMDqsN3tIXERNhUQ1oJEyE=";
   };
 
   patches = [
@@ -57,6 +59,12 @@ stdenv.mkDerivation (finalAttrs: {
       name = "0001-Add-W_EXITCODE-macro-for-non-glibc-systems.patch";
       url = "https://git.alpinelinux.org/aports/plain/community/vte3/fix-W_EXITCODE.patch?id=4d35c076ce77bfac7655f60c4c3e4c86933ab7dd";
       hash = "sha256-FkVyhsM0mRUzZmS2Gh172oqwcfXv6PyD6IEgjBhy2uU=";
+    })
+
+    # Fix compilation with gcc 14
+    (fetchpatch {
+      url = "https://gitlab.gnome.org/GNOME/vte/-/commit/e297ba0507d0e7863edd411dfc5264bbc490e4f6.patch";
+      hash = "sha256-uTaeb7bLUBefgF5Uxl7Qxlcp5Uj7pqW8XiR/5rsjuTI=";
     })
   ];
 
@@ -76,6 +84,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = [
     cairo
+    fmt_11
     fribidi
     gnutls
     pango # duplicated with propagatedBuildInputs to support gtkVersion == null
@@ -83,6 +92,7 @@ stdenv.mkDerivation (finalAttrs: {
     lz4
     icu
     fast-float
+    simdutf
   ]
   ++ lib.optionals systemdSupport [
     systemd

nixos/modules/config/vte.nix
@@ -9,7 +9,7 @@ let
   vteInitSnippet = ''
     # Show current working directory in VTE terminals window title.
     # Supports both bash and zsh, requires interactive shell.
-    . ${pkgs.vte.override { gtkVersion = null; }}/etc/profile.d/vte.sh
+    . ${pkgs.vte-gtk4}/etc/profile.d/vte.sh
   '';
 
 in

commit 4b7fab5e081504cf21f9d375fe335e4997297146

vte: backport fix for backspace in QEMU
Fixes: 7a24e8bdb625 ("vte: 0.80.3 → 0.81.90")

pkgs/by-name/vt/vte/package.nix
@@ -78,6 +78,12 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://github.com/GNOME/vte/commit/6b7a6a7df9df99368b7ce5ac5903bd2578167567.patch";
       hash = "sha256-s3HigfTZLtGmsZS6dfD3YE95ZdBjB4WOWDvuoatOu3o=";
     })
+
+    (fetchpatch {
+      name = "qemu-backspace.patch";
+      url = "https://gitlab.gnome.org/GNOME/vte/-/commit/79d5fea437185e52a740130d5a276b83dfdcd558.patch";
+      hash = "sha256-28Cehw5uJuGG7maLGUl1TBwfIwuXpkLKSQ2lXauLlz0=";
+    })
   ];
 
   nativeBuildInputs = [