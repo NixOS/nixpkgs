2a44e3f2b9e51e3397c1e0571bc8154efccefdb2
Alessio Caiazza <code.git@caiazza.info>
2025-11-27 12:07:04 +0100

    maintainers: add nolith

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18908,6 +18908,12 @@
     githubId = 41154684;
     name = "nokazn";
   };
+  nolith = {
+    github = "nolith";
+    githubId = 78752;
+    name = "Alessio Caiazza";
+    matrix = "@alessio:caiazza.info";
+  };
   nomaterials = {
     email = "nomaterials@gmail.com";
     github = "no-materials";
