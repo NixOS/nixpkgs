be814f780e9205a231c83c35bde789d6ce924132
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-27 10:53:59 +0000

    python3Packages.jsonargparse: 4.43.0 -> 4.44.0

--- pkgs/development/python-modules/jsonargparse/default.nix
+++ pkgs/development/python-modules/jsonargparse/default.nix
@@ -23,14 +23,14 @@
 
 buildPythonPackage rec {
   pname = "jsonargparse";
-  version = "4.43.0";
+  version = "4.44.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "omni-us";
     repo = "jsonargparse";
     tag = "v${version}";
-    hash = "sha256-hKvzdkblQ11E++ZQoZg150Kjt06wDn+gSLrH429Sg+4=";
+    hash = "sha256-VcCfoWT54/SGPYBOTLJuyX4507HMqwrZMQbUt0sN0Wg=";
   };
 
   build-system = [ setuptools ];
