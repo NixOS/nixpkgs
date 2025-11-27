425fa9413dc5a55f2e3a054d40f1783128c9807f
Yueh-Shun Li <shamrocklee@posteo.net>
2025-11-27 12:41:28 +0800

    fetchgit: take postCheckout to allow collecting revision without leaving .git

--- pkgs/build-support/fetchgit/default.nix
+++ pkgs/build-support/fetchgit/default.nix
@@ -80,6 +80,8 @@ lib.makeOverridable (
           # run operations between the checkout completing and deleting the .git
           # directory.
           preFetch ? "",
+          # Shell code executed after `git checkout` and before .git directory removal/sanitization.
+          postCheckout ? "",
           # Shell code executed after the file has been fetched
           # successfully. This can do things like check or transform the file.
           postFetch ? "",
@@ -168,6 +170,7 @@ lib.makeOverridable (
               branchName
               nonConeMode
               preFetch
+              postCheckout
               postFetch
               fetchTags
               rootDir
@@ -215,6 +218,10 @@ lib.makeOverridable (
               ];
 
             inherit preferLocalBuild meta allowedRequisites;
+
+            env = {
+              NIX_PREFETCH_GIT_CHECKOUT_HOOK = finalAttrs.postCheckout;
+            };
 
             passthru = {
               gitRepoUrl = url;
