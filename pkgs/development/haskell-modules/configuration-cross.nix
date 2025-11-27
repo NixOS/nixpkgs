65bea2df05ba6c3b873a8478ee1387870500374a
Alexandre Esteves <alexfmpe@proton.me>
2025-11-27 17:24:38 +0000

    haskellPackages: only override 'enableExternalInterpreter' when cross compiling

---
+++ pkgs/development/haskell-modules/configuration-cross.nix
@@ -0,0 +1,25 @@
+{ pkgs, haskellLib }:
+
+let
+  inherit (pkgs) lib;
+in
+
+with haskellLib;
+
+self: super: {
+  # Avoids a cycle by disabling use of the external interpreter for the packages that are dependencies of iserv-proxy.
+  # These in particular can't rely on template haskell for cross-compilation anyway as they can't rely on iserv-proxy.
+  inherit
+    (
+      let
+        noExternalInterpreter = overrideCabal {
+          enableExternalInterpreter = false;
+        };
+      in
+      lib.mapAttrs (_: noExternalInterpreter) { inherit (super) iserv-proxy libiserv network; }
+    )
+    iserv-proxy
+    libiserv
+    network
+    ;
+}
