d07b065bc918bcbcc654333147c6d0a27f825e69
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-27 14:18:54 +0000

    vscode-extensions.saoudrizwan.claude-dev: 3.38.1 -> 3.38.3

--- pkgs/applications/editors/vscode/extensions/saoudrizwan.claude-dev/default.nix
+++ pkgs/applications/editors/vscode/extensions/saoudrizwan.claude-dev/default.nix
@@ -7,8 +7,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "claude-dev";
     publisher = "saoudrizwan";
-    version = "3.38.1";
-    hash = "sha256-j3hRW7l+PEq7DJbXENO5Plbg3SePZm1lX60Y4B5RvYs=";
+    version = "3.38.3";
+    hash = "sha256-LVFZlgXrnZ0X6gt+UBghvG7t6mK5R9N7X0SYb8G3alU=";
   };
 
   meta = {
