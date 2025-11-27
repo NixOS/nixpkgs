b135620ed8dc22ab65757d8233f668ad13a7d302
Narendra S <narendra.s1232@gmail.com>
2025-11-28 04:29:57 +0530

    dell-bios-fan-control: init at 20220119
    .

---
+++ pkgs/by-name/de/dell-bios-fan-control/package.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  stdenv,
+  fetchgit,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "dell-bios-fan-control";
+  version = "20220119"; # upstream does not use versioning - use date of last commit
+
+  src = fetchgit {
+    url = "https://github.com/TomFreudenberg/dell-bios-fan-control.git";
+    rev = "27006106595bccd6c309da4d1499f93d38903f9a";
+    hash = "sha256-3ihzvwL86c9VJDfGpbWpkOwZ7qU0E5U2UuOeCwPMR1s=";
+  };
+
+  buildPhase = ''make'';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp dell-bios-fan-control $out/bin
+  '';
+
+  meta = {
+    description = "A user space utility to set control of fans by bios on some Dell Laptops";
+    homepage = "https://www.github.com/TomFreudenberg/dell-bios-fan-control";
+    license = lib.licenses.gpl2Plus;
+    maintainers = with lib.maintainers; [ totalyenglizlitrate ];
+    mainProgram = "dell-bios-fan-control";
+    platforms = lib.platforms.linux;
+  };
+})
