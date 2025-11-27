21c3366e7390b50dd73b01872e4a191356293cb2
makefu <github@syntax-fehler.de>
2025-11-27 16:27:54 +0100

    pfsshell: 1.1.1 -> 1.1.1-unstable-2025-07-13
    remove disabled hardening and build pfsfuse to mount ps2 pfs images as a file system
    
    1.1.1 release is almost 5 years old but development has continued

--- pkgs/by-name/pf/pfsshell/package.nix
+++ pkgs/by-name/pf/pfsshell/package.nix
@@ -1,30 +1,34 @@
 {
+  cmake,
   lib,
   stdenv,
   fetchFromGitHub,
+  fuse,
   meson,
   ninja,
+  pkg-config,
 }:
 
 stdenv.mkDerivation rec {
-  version = "1.1.1";
+  version = "1.1.1-unstable-2025-07-13";
   pname = "pfsshell";
 
   src = fetchFromGitHub {
-    owner = "uyjulian";
+    owner = "ps2homebrew";
     repo = "pfsshell";
-    rev = "v${version}";
-    sha256 = "0cr91al3knsbfim75rzl7rxdsglcc144x0nizn7q4jx5cad3zbn8";
+    rev = "8192de3907a05bb1844afcb1ae490179a38d4ed6";
+    fetchSubmodules = true;
+    hash = "sha256-drQNnCIqwM+Lnix5LewkD2ov8G6Mbu60xVKQKvCFbPY=";
   };
 
   nativeBuildInputs = [
+    fuse
     meson
     ninja
+    pkg-config
   ];
 
-  # Build errors since 1.1.1 when format hardening is enabled:
-  #   cc1: error: '-Wformat-security' ignored without '-Wformat' [-Werror=format-security]
-  hardeningDisable = [ "format" ];
+  mesonFlags = [ "-Denable_pfsfuse=true" ];
 
   meta = with lib; {
     inherit (src.meta) homepage;
