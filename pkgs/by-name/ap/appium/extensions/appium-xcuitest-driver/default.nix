b0fcbc58edd30a7025769bc8824e52340002bd0b
eyjhb <eyjhbb@gmail.com>
2025-11-27 08:21:53 +0100

    appium.extensions.appium-xcuitest-driver: 10.0.5 -> 10.8.3

--- pkgs/by-name/ap/appium/extensions/appium-xcuitest-driver/default.nix
+++ pkgs/by-name/ap/appium/extensions/appium-xcuitest-driver/default.nix
@@ -7,15 +7,15 @@
 buildNpmPackage rec {
   pname = "appium-xcuitest-driver";
   packageName = pname;
-  version = "10.0.5";
+  version = "10.8.3";
 
   src = fetchFromGitHub {
     owner = "appium";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-uYicjH+UyKfBvq7iYmwIcaB25Xof17COylquLssEoa8=";
+    hash = "sha256-fclwYba68frnVP3J2VdbjREsZDZKSnmWMI/CT640VsQ=";
   };
-  npmDepsHash = "sha256-z5V1jbvPI0mQ4UeGUpheoJraP5+EKr0rrWJXIrNS118=";
+  npmDepsHash = "sha256-6iGgvjj9Z0sD2Da60buvuhQJE5fRHi+KRV9sgaiewSk=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
