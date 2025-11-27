a54a00ecc2f694ac688989980321f241a620812b
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-27 12:30:53 +0000

    cargo-all-features: 1.11.0 -> 1.12.0

--- pkgs/by-name/ca/cargo-all-features/package.nix
+++ pkgs/by-name/ca/cargo-all-features/package.nix
@@ -6,11 +6,11 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cargo-all-features";
-  version = "1.11.0";
+  version = "1.12.0";
 
   src = fetchCrate {
     inherit pname version;
-    hash = "sha256-pHwQq6/KGCIYm3Q63YbUit6yUjwEFnpBJCE6lpGBcZc=";
+    hash = "sha256-pD0lyI2zSOeEDk1Lch4Qf5mo8Z8Peiy2XF5iQ62vsaI=";
   };
 
   postPatch = ''
@@ -18,7 +18,7 @@ rustPlatform.buildRustPackage rec {
       --replace-fail 'cmd.env("RUSTFLAGS", "-Cinstrument-coverage");' '''
   '';
 
-  cargoHash = "sha256-tAwU7vJLp4KLzYAEbtSpNKbZBz+hBdAiIkUD/A5CpwI=";
+  cargoHash = "sha256-EKDeBib52Os1X3sgM9CtrNkl20l1Wn/cMBIBM1/KY5A=";
 
   meta = with lib; {
     description = "Cargo subcommand to build and test all feature flag combinations";
