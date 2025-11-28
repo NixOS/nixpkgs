b2759ab0ed1882bce6039e8f2e8c992b64ee3692
Jost Alemann <alemann@posteo.com>
2025-11-28 23:40:25 +0100

    harper: 0.73.0 -> 1.0.0
    Changelog: https://github.com/Automattic/harper/releases/tag/v1.0.0
    Diff: https://github.com/Automattic/harper/compare/v0.73.0...v1.0.0

--- pkgs/by-name/ha/harper/package.nix
+++ pkgs/by-name/ha/harper/package.nix
@@ -8,18 +8,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "harper";
-  version = "0.73.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "Automattic";
     repo = "harper";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Hf086Ub0nVGET4qELDMddOErGAhK8B6ohbI5JhnU6z8=";
+    hash = "sha256-U0k6DODMW9OK+sR9kt8JzpmV32d9py9Pehv/0h8SqDg=";
   };
 
   buildAndTestSubdir = "harper-ls";
 
-  cargoHash = "sha256-K5/QPTLdT4SeZr6hvth8aDodaj2NnFsLpHDQle+FU1k=";
+  cargoHash = "sha256-TyqUKAlrcoqsDBS2Y3jzUFNNRazSBsCf6una0znc+yw=";
 
   passthru.updateScript = nix-update-script { };
 
