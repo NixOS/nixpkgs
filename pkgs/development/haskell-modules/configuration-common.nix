a1890652bf36fc5bd93efda5f80a1c58ff1b5db1
sternenseemann <sternenseemann@systemli.org>
2025-11-28 22:58:14 +0100

    git-annex: update sha256 for 10.20251114
    (cherry picked from commit 18d6699359b01bee312aa17baa326cfd21df4043)

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -600,7 +600,7 @@ with haskellLib;
         name = "git-annex-${super.git-annex.version}-src";
         url = "git://git-annex.branchable.com/";
         rev = "refs/tags/" + super.git-annex.version;
-        sha256 = "sha256-hDbwkpu7qpJZmLNe/rF4eOewLrozo/qpCjFEczZv9S4=";
+        sha256 = "sha256-+OLFMrqpf1Ooy7CQ9S+N/H5R5+aHQtbO1pYwDF4ln8A=";
         # delete android and Android directories which cause issues on
         # darwin (case insensitive directory). Since we don't need them
         # during the build process, we can delete it to prevent a hash
