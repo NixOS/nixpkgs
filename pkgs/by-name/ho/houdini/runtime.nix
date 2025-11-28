4bed60460ad00e7649319d0d4e73d321977b0dd1
pedohorse <13556996+pedohorse@users.noreply.github.com>
2025-11-28 22:52:07 +0100

    houdini: 21.0.440 -> 21.0.559

--- pkgs/by-name/ho/houdini/runtime.nix
+++ pkgs/by-name/ho/houdini/runtime.nix
@@ -1,12 +1,12 @@
 { requireFile, callPackage }:
 
 callPackage ./runtime-build.nix rec {
-  version = "21.0.440";
+  version = "21.0.559";
   eulaDate = "2021-10-13";
   src = requireFile {
     name = "houdini-${version}-linux_x86_64_gcc11.2.tar.gz";
-    hash = "sha256-qHRR+RRtUgUam6FC1TWTZjg1FSakjLoMYVaiIfO+WOY=";
+    hash = "sha256-bZmoH1NKQhhMAhIl3pTL7irUZ7HrOhS8R7GApLD5514=";
     url = "https://www.sidefx.com/download/daily-builds/?production=true";
   };
-  outputHash = "sha256-SSBiqNZRnxz6tnvusYRi2UASY1k3voiblDpkiu+qU0w=";
+  outputHash = "sha256-/7ctlMUoyJdPdBQV7rRO9pWcg9bXcnMJsB9TN/Jo8QQ=";
 }
