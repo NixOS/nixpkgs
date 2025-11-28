a32e37df2695781b89d5fac6863b46990956a263
Robert Schütz <nix@dotlambda.de>
2025-11-27 22:27:39 -0800

    Revert "libwacom: 2.16.1 -> 2.17.0"
    This reverts commit e2ce824ee6f368fdb573596224dc38c3da2a65d0 because it
    broke libwacom-surface.

--- pkgs/by-name/li/libwacom/package.nix
+++ pkgs/by-name/li/libwacom/package.nix
@@ -16,7 +16,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libwacom";
-  version = "2.17.0";
+  version = "2.16.1";
 
   outputs = [
     "out"
@@ -27,7 +27,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "linuxwacom";
     repo = "libwacom";
     rev = "libwacom-${finalAttrs.version}";
-    hash = "sha256-2JbO3B/PqsLKnaCeQcn1x9Z2pQL4kScuS39F1JlpDpc=";
+    hash = "sha256-YP6z+2HyIRmIAJIdJMbVTQA0rf3EXBZvlCdM4jrmHXM=";
   };
 
   postPatch = ''
