1748fe92284c6bec31b79da30ba4c56e7a926479
Robert Schütz <nix@dotlambda.de>
2025-11-28 12:29:08 -0800

    nfs-utils: add dotlambda to maintainers

--- pkgs/by-name/nf/nfs-utils/package.nix
+++ pkgs/by-name/nf/nfs-utils/package.nix
@@ -195,6 +195,6 @@ stdenv.mkDerivation rec {
     homepage = "https://linux-nfs.org/";
     license = licenses.gpl2Plus;
     platforms = platforms.linux;
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.dotlambda ];
   };
 }
