e78fdf3e16161fcb898926076d1ea5a9115c02c2
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-28 01:34:13 +0000

    libretro.vba-m: 0-unstable-2025-11-05 -> 0-unstable-2025-11-25

--- pkgs/applications/emulators/libretro/cores/vba-m.nix
+++ pkgs/applications/emulators/libretro/cores/vba-m.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "vbam";
-  version = "0-unstable-2025-11-05";
+  version = "0-unstable-2025-11-25";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "vbam-libretro";
-    rev = "d0787aee43d260675da203c2f85ba9fa226c0c66";
-    hash = "sha256-WhsiKsDk7jEIklrOw1YFCcWSlAmLK4vCCji3Mnsgwmw=";
+    rev = "c3db5dc221a3038ee54572047304963cbca15cd6";
+    hash = "sha256-e+elKAcQXTVH2VtW0J4FG2p6bJTWSuxrIrnMHwjaO9I=";
   };
 
   makefile = "Makefile";
