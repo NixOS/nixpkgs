4e70ca00920c4d594c3fd48d98153407ba1edf24
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-28 04:46:26 +0000

    python3Packages.pyogrio: 0.11.1 -> 0.12.0

--- pkgs/development/python-modules/pyogrio/default.nix
+++ pkgs/development/python-modules/pyogrio/default.nix
@@ -17,7 +17,7 @@
 
 buildPythonPackage rec {
   pname = "pyogrio";
-  version = "0.11.1";
+  version = "0.12.0";
   pyproject = true;
   disabled = pythonOlder "3.9";
 
@@ -25,7 +25,7 @@ buildPythonPackage rec {
     owner = "geopandas";
     repo = "pyogrio";
     tag = "v${version}";
-    hash = "sha256-F6XfkihN3k2xquYS8jJMlqtLXzaTORaduJ2Q9LhSQGM=";
+    hash = "sha256-ggyT5e2nm6XAZEtDF4lttfThd0afWKFo10RujTxDWt0=";
   };
 
   postPatch = ''
