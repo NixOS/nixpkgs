7324322b6f0ab5d0a5a3ce37c628b604fb097011
Narendra S <narendra.s1232@gmail.com>
2025-11-29 17:53:17 +0530

    dell-bios-fancontrol: Make requested changes
    - Use `fetchFromGitHub` instead of `fetchgit`
    - Modify version to adhere to convention
    - remove buildPhase as it calls make by default

--- pkgs/by-name/de/dell-bios-fan-control/package.nix
+++ pkgs/by-name/de/dell-bios-fan-control/package.nix
@@ -1,20 +1,20 @@
 {
   lib,
   stdenv,
-  fetchgit,
+  fetchFromGitHub,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "dell-bios-fan-control";
-  version = "20220119"; # upstream does not use versioning - use date of last commit
+  version = "0-unstable-2022-01-19";
 
-  src = fetchgit {
-    url = "https://github.com/TomFreudenberg/dell-bios-fan-control.git";
+  src = fetchFromGitHub {
+    owner = "TomFreudenberg";
+    repo = "dell-bios-fan-control";
     rev = "27006106595bccd6c309da4d1499f93d38903f9a";
     hash = "sha256-3ihzvwL86c9VJDfGpbWpkOwZ7qU0E5U2UuOeCwPMR1s=";
   };
 
-  buildPhase = ''make'';
   installPhase = ''
     mkdir -p $out/bin
     cp dell-bios-fan-control $out/bin
