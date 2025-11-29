6a26d0bf1919279e146f085d8c699d34e3f3035f
Adam C. Stephens <adam@valkor.net>
2025-11-29 11:37:12 -0500

    incus: 6.19.0 -> 6.19.1
    Changelog: https://github.com/lxc/incus/releases/tag/v6.19.1

--- pkgs/by-name/in/incus/package.nix
+++ pkgs/by-name/in/incus/package.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
-  hash = "sha256-0zGLMWkEeJmSrmJvjmJ+d/FjE1btMwif6ugaHUBLD0A=";
-  version = "6.19.0";
+  hash = "sha256-TdJe/vnjSc5ZT5tgZQsQacCVfW5+TKd5cjQLOp8SuZg=";
+  version = "6.19.1";
   vendorHash = "sha256-Dx/AsSvDL/cHS/nRV5invkxgBg4w8jvtZ20LK7tOW14=";
   patches = [ ];
   nixUpdateExtraArgs = [
