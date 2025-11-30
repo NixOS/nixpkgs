0f2397c22fa6e0df25e4e2f07bdd5363e9059b51
R. Ryantm <ryantm-bot@ryantm.com>
2025-11-30 19:30:23 +0000

    lubelogger: 1.5.4 -> 1.5.5

--- pkgs/by-name/lu/lubelogger/package.nix
+++ pkgs/by-name/lu/lubelogger/package.nix
@@ -7,13 +7,13 @@
 
 buildDotnetModule rec {
   pname = "lubelogger";
-  version = "1.5.4";
+  version = "1.5.5";
 
   src = fetchFromGitHub {
     owner = "hargata";
     repo = "lubelog";
     rev = "v${version}";
-    hash = "sha256-YynkoqifKgEH8yiewcVVmMT0kX+NXaansJ7Z78NfXm4=";
+    hash = "sha256-xoq1EgwA4MScuyVN4djqWPjlRXouMR3JnZviOIaDNUU=";
   };
 
   projectFile = "CarCareTracker.sln";
