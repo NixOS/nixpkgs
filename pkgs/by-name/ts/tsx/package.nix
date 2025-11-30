fb1339fa7a196fc83117512fc16cb882be0707b3
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2025-11-30 18:33:34 +0000

    tsx: add HigherOrderLogic as maintainer

--- pkgs/by-name/ts/tsx/package.nix
+++ pkgs/by-name/ts/tsx/package.nix
@@ -96,7 +96,10 @@ stdenv.mkDerivation (finalAttrs: {
     description = "TypeScript Execute (tsx): The easiest way to run TypeScript in Node.js";
     homepage = "https://tsx.is";
     license = lib.licenses.mit;
-    maintainers = [ lib.maintainers.sdedovic ];
+    maintainers = with lib.maintainers; [
+      sdedovic
+      higherorderlogic
+    ];
     mainProgram = "tsx";
   };
 })
