1b025aef88727c289b9fd02ffdaa9e12a6c56210
Julie B. <no-reply@bbjubjub.fr>
2025-11-30 22:11:10 +0100

    electrum-ltc: 4.2.2.1 -> 4.6.2-3

--- pkgs/applications/misc/electrum/ltc.nix
+++ pkgs/applications/misc/electrum/ltc.nix
@@ -6,21 +6,12 @@
   wrapQtAppsHook,
   python3,
   zbar,
-  secp256k1,
   enableQt ? true,
   qtwayland,
 }:
 
 let
-  version = "4.2.2.1";
-
-  libsecp256k1_name =
-    if stdenv.hostPlatform.isLinux then
-      "libsecp256k1.so.0"
-    else if stdenv.hostPlatform.isDarwin then
-      "libsecp256k1.0.dylib"
-    else
-      "libsecp256k1${stdenv.hostPlatform.extensions.sharedLibrary}";
+  version = "4.6.2-3";
 
   libzbar_name =
     if stdenv.hostPlatform.isLinux then
@@ -30,19 +21,6 @@ let
     else
       "libzbar${stdenv.hostPlatform.extensions.sharedLibrary}";
 
-  # Not provided in official source releases, which are what upstream signs.
-  tests = fetchFromGitHub {
-    owner = "pooler";
-    repo = "electrum-ltc";
-    rev = version;
-    sha256 = "sha256-qu72LIV07pgHqvKv+Kcw9ZmNk6IBz+4/vdJELlT5tE4=";
-
-    postFetch = ''
-      mv $out ./all
-      mv ./all/electrum_ltc/tests $out
-    '';
-  };
-
 in
 
 python3.pkgs.buildPythonApplication {
@@ -51,14 +29,9 @@ python3.pkgs.buildPythonApplication {
   format = "setuptools";
 
   src = fetchurl {
-    url = "https://electrum-ltc.org/download/Electrum-LTC-${version}.tar.gz";
-    hash = "sha256-7F28cve+HD5JDK5igfkGD/NvTCfA33g+DmQJ5mwPM9Q=";
+    url = "https://github.com/ltc-electrum/electrum-ltc/releases/download/${version}/Electrum-LTC-4.6.2.tar.gz";
+    hash = "sha256-u/iHczEAla0MiAuqzxlmbdvwrn2ygrhQdW8mRJbIzbc=";
   };
-
-  postUnpack = ''
-    # can't symlink, tests get confused
-    cp -ar ${tests} $sourceRoot/electrum_ltc/tests
-  '';
 
   nativeBuildInputs = lib.optionals enableQt [ wrapQtAppsHook ];
 
@@ -73,7 +46,13 @@ python3.pkgs.buildPythonApplication {
       certifi
       cryptography
       dnspython
+      electrum-ecc
+      electrum-aionostr
+      grpcio
+      pytest-qt
+      jaraco-path
       jsonrpclib-pelix
+      jsonpatch
       matplotlib
       pbkdf2
       protobuf
@@ -89,56 +68,31 @@ python3.pkgs.buildPythonApplication {
       distutils
     ]
     ++ lib.optionals enableQt [
-      pyqt5
+      pyqt6
       qdarkstyle
     ];
-
-  patches = [
-    # electrum-ltc attempts to pin to aiorpcX < 0.23, but nixpkgs
-    # has moved to newer versions.
-    #
-    # electrum-ltc hasn't been updated in some time, so we replicate
-    # the patch from electrum (BTC) and alter it to be usable with
-    # electrum-ltc.
-    #
-    # Similar to the BTC patch, we need to overwrite the symlink
-    # at electrum_ltc/electrum-ltc with the patched run_electrum
-    # in postPatch.
-    ./ltc-aiorpcX-version-bump.patch
-  ];
 
   postPatch = ''
     # copy the patched `/run_electrum` over `/electrum/electrum`
     # so the aiorpcx compatibility patch is used
-    cp run_electrum electrum_ltc/electrum-ltc
+    cp run_electrum electrum/electrum-ltc
 
-    # refresh stale generated code, per electrum_ltc/paymentrequest.py line 40
-    protoc --proto_path=electrum_ltc/ --python_out=electrum_ltc/ electrum_ltc/paymentrequest.proto
+    # refresh stale generated code, per electrum/paymentrequest.py line 40
+    protoc --proto_path=electrum/ --python_out=electrum/ electrum/paymentrequest.proto
   '';
 
-  preBuild = ''
-    sed -i 's,usr_share = .*,usr_share = "'$out'/share",g' setup.py
-    substituteInPlace ./electrum_ltc/ecc_fast.py \
-      --replace ${libsecp256k1_name} ${secp256k1}/lib/libsecp256k1${stdenv.hostPlatform.extensions.sharedLibrary}
-  ''
-  + (
+  preBuild =
     if enableQt then
       ''
-        substituteInPlace ./electrum_ltc/qrscanner.py \
+        substituteInPlace ./electrum/qrscanner.py \
           --replace ${libzbar_name} ${zbar.lib}/lib/libzbar${stdenv.hostPlatform.extensions.sharedLibrary}
       ''
     else
       ''
         sed -i '/qdarkstyle/d' contrib/requirements/requirements.txt
-      ''
-  );
+      '';
 
   postInstall = lib.optionalString stdenv.hostPlatform.isLinux ''
-    # Despite setting usr_share above, these files are installed under
-    # $out/nix ...
-    mv $out/${python3.sitePackages}/nix/store"/"*/share $out
-    rm -rf $out/${python3.sitePackages}/nix
-
     substituteInPlace $out/share/applications/electrum-ltc.desktop \
       --replace 'Exec=sh -c "PATH=\"\\$HOME/.local/bin:\\$PATH\"; electrum-ltc %u"' \
                 "Exec=$out/bin/electrum-ltc %u" \
@@ -158,7 +112,7 @@ python3.pkgs.buildPythonApplication {
   ];
   buildInputs = lib.optional stdenv.hostPlatform.isLinux qtwayland;
 
-  enabledTestPaths = [ "electrum_ltc/tests" ];
+  enabledTestPaths = [ "tests" ];
 
   disabledTests = [
     "test_loop" # test tries to bind 127.0.0.1 causing permission error
@@ -183,7 +137,7 @@ python3.pkgs.buildPythonApplication {
       intact. Keep it on paper, or in your head... and never worry about losing
       your litecoins to theft or hardware failure.
     '';
-    homepage = "https://electrum-ltc.org/";
+    homepage = "https://ltc-electrum.github.io/";
     license = licenses.mit;
     platforms = platforms.all;
     maintainers = with maintainers; [ bbjubjub ];
