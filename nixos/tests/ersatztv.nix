31af71f4edda735718f21528b201cecedc24a2d2
James Hollowell <jamesthollowell@gmail.com>
2025-11-30 16:39:19 -0500

    nixos/tests/ersatz: migrate to runTest
    (cherry picked from commit ca4316aadf41fa9fbe0238fff59fa78972a47a95)

--- nixos/tests/ersatztv.nix
+++ nixos/tests/ersatztv.nix
@@ -1,21 +1,19 @@
-import ./make-test-python.nix (
-  { lib, ... }:
+{ lib, ... }:
 
-  {
-    name = "ersatztv";
-    meta.maintainers = with lib.maintainers; [ allout58 ];
+{
+  name = "ersatztv";
+  meta.maintainers = with lib.maintainers; [ allout58 ];
 
-    nodes.machine =
-      { ... }:
-      {
-        services.ersatztv.enable = true;
-      };
+  nodes.machine =
+    { ... }:
+    {
+      services.ersatztv.enable = true;
+    };
 
-    # ErsatzTV doesn't really have an API to speak of currently, so just check if it responds at all
-    testScript = ''
-      machine.wait_for_unit("ersatztv.service")
-      machine.wait_for_open_port(8409)
-      machine.succeed("curl --fail http://localhost:8409/")
-    '';
-  }
-)
+  # ErsatzTV doesn't really have an API to speak of currently, so just check if it responds at all
+  testScript = ''
+    machine.wait_for_unit("ersatztv.service")
+    machine.wait_for_open_port(8409)
+    machine.succeed("curl --fail http://localhost:8409/")
+  '';
+}
