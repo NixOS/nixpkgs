b4618316979d1bb58a3efcf2ebb9df5af1d67960
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-01 05:23:25 +0000

    vscode-extensions.bmewburn.vscode-intelephense-client: 1.16.1 -> 1.16.2

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -727,8 +727,8 @@ let
         mktplcRef = {
           name = "vscode-intelephense-client";
           publisher = "bmewburn";
-          version = "1.16.1";
-          hash = "sha256-dcfFtVnk5qyCeiM0nZwscKQZDhUXIkg1TJxVUuzA69Q=";
+          version = "1.16.2";
+          hash = "sha256-v6XbytazjVkNin3wGsF/76WZSwUCjw51x1S0qTOn7Mc=";
         };
         meta = {
           description = "PHP code intelligence for Visual Studio Code";
