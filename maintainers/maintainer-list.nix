f10d53df13487df857bfd943413a76012fa6761e
Brandon Bennett <bbennett@coreweave.com>
2025-12-01 13:34:43 -0300

    maintainers: add nemith

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18554,6 +18554,12 @@
     githubId = 50854675;
     name = "Nelson Jeppesen";
   };
+  nemith = {
+    email = "nix@brbe.me";
+    github = "nemith";
+    githubId = 349360;
+    name = "Brandon Bennett";
+  };
   neosimsim = {
     email = "me@abn.sh";
     github = "neosimsim";
