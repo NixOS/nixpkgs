d50c5ddde5b07f0ec16675c8b50d6fef0519c96d
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-01 15:53:21 +0000

    oh-my-zsh: 2025-11-23 -> 2025-12-01

--- pkgs/by-name/oh/oh-my-zsh/package.nix
+++ pkgs/by-name/oh/oh-my-zsh/package.nix
@@ -19,14 +19,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "2025-11-23";
+  version = "2025-12-01";
   pname = "oh-my-zsh";
 
   src = fetchFromGitHub {
     owner = "ohmyzsh";
     repo = "ohmyzsh";
-    rev = "beadd56dd75e8a40fe0a7d4a5d63ed5bf9efcd48";
-    sha256 = "sha256-e3zTM3rj4z6RwDai9i7KKrz6imTGmZiSAqcS9Mw2LZU=";
+    rev = "e9fc134236323ce3ce376715b1e55a54ed6ac7ac";
+    sha256 = "sha256-zOB8dU1dX/sx86YIP3wsJKRY30pYYzoLLQCD334IxJo=";
   };
 
   strictDeps = true;
