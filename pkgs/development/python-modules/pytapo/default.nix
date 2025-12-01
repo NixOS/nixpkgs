7f5fd427933c7d8f671fdf7e7dc767e59fd194e4
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-01 20:48:11 +0100

    python313Packages.pytapo: modernize

--- pkgs/development/python-modules/pytapo/default.nix
+++ pkgs/development/python-modules/pytapo/default.nix
@@ -4,7 +4,6 @@
   fetchPypi,
   pycryptodome,
   python-kasa,
-  pythonOlder,
   requests,
   rtp,
   setuptools,
@@ -15,8 +14,6 @@ buildPythonPackage rec {
   pname = "pytapo";
   version = "3.3.54";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
@@ -41,7 +38,8 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Python library for communication with Tapo Cameras";
     homepage = "https://github.com/JurajNyiri/pytapo";
-    license = with licenses; [ mit ];
+    changelog = "https://github.com/JurajNyiri/pytapo/releases/tag/${version}";
+    license = licenses.mit;
     maintainers = with maintainers; [ fleaz ];
   };
 }
