980c1232963aed43a868466da9650e19070e6b86
Petr Portnov <mrjarviscraft+nix@gmail.com>
2025-12-01 18:42:35 +0300

    cargo-cranky: use finalAttrs

--- pkgs/by-name/ca/cargo-cranky/package.nix
+++ pkgs/by-name/ca/cargo-cranky/package.nix
@@ -4,28 +4,28 @@
   fetchFromGitHub,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cargo-cranky";
   version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "ericseppanen";
     repo = "cargo-cranky";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-3ARl3z+2nz05UaKf8ChN6mvPY2qMjUNxGnGJ1P0xkas=";
   };
 
   cargoHash = "sha256-m9n2YyrMpuz/X/kvHgn+g4w9/Pg+n6VnnfwjaOnyPvY=";
 
-  meta = with lib; {
+  meta = {
     description = "Easy to configure wrapper for Rust's clippy";
     mainProgram = "cargo-cranky";
     homepage = "https://github.com/ericseppanen/cargo-cranky";
-    changelog = "https://github.com/ericseppanen/cargo-cranky/releases/tag/${src.rev}";
-    license = with licenses; [
+    changelog = "https://github.com/ericseppanen/cargo-cranky/releases/tag/v${finalAttrs.version}";
+    license = with lib.licenses; [
       asl20
       mit
     ];
-    maintainers = [ maintainers.progrm_jarvis ];
+    maintainers = [ lib.maintainers.progrm_jarvis ];
   };
-}
+})
