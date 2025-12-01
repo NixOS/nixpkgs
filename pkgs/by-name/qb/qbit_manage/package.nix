e2200d8b5e9b8a6bfe6b24d342f883b13720250a
Philip Johansson <philip@phlipphlop.me>
2025-12-01 07:20:36 +0100

    qbit_manage: init at 4.6.5

---
+++ pkgs/by-name/qb/qbit_manage/package.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+  python3,
+  runCommand,
+  nix-update-script,
+  qbit_manage,
+}:
+python3Packages.buildPythonApplication rec {
+  pname = "qbit_manage";
+  version = "4.6.5";
+  src = fetchFromGitHub {
+    owner = "StuffAnThings";
+    repo = "qbit_manage";
+    hash = "sha256-JCsbf2mPRhs7Mbekl946G/y/CSNSSvQBLvlwVy/Avcg=";
+    rev = "v${version}";
+  };
+  format = "setuptools";
+  propagatedBuildInputs = [
+    (python3.withPackages (
+      python-pkgs:
+      builtins.attrValues {
+        inherit (python-pkgs)
+          argon2-cffi
+          bencode-py
+          croniter
+          fastapi
+          gitpython
+          humanize
+          pytimeparse2
+          qbittorrent-api
+          requests
+          retrying
+          ruamel-yaml
+          slowapi
+          uvicorn
+          ;
+      }
+    ))
+  ];
+
+  passthru = {
+    updateScript = nix-update-script { };
+    tests = {
+      version =
+        runCommand "qbit_manage-test-version"
+          {
+            buildInputs = [ qbit_manage ];
+          }
+          ''
+            export HOME=$TMPDIR # Needed since qbit-manage creates config files in home dir
+            outver="$(qbit-manage --version)"
+            echo "$outver" | grep -q "${version}" \
+              || (echo "Version mismatch: $outver" >&2; exit 1)
+            touch $out
+          '';
+    };
+  };
+
+  meta = {
+    description = "This tool will help manage tedious tasks in qBittorrent and automate them";
+    homepage = "https://github.com/StuffAnThings/qbit_manage";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ flyingpeakock ];
+    platforms = lib.platforms.all;
+    mainProgram = "qbit-manage";
+  };
+}
