dc77d45b85895dcbdc056b227d7d8ad384922498
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-01 02:44:59 +0000

    xv: 6.0.4 -> 6.1.0

--- pkgs/by-name/xv/xv/package.nix
+++ pkgs/by-name/xv/xv/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "xv";
-  version = "6.0.4";
+  version = "6.1.0";
 
   src = fetchFromGitHub {
     owner = "jasper-software";
     repo = "xv";
     rev = "v${version}";
-    sha256 = "sha256-5bhLMGdj7HJOsSOFjNO5s3wDA9XbPTwG+g7OSrKMMXk=";
+    sha256 = "sha256-bq9xEGQRzWZ3/Unu49q6EW9/XSCgpalyXn4l4Mg255g=";
   };
 
   nativeBuildInputs = [ cmake ];
