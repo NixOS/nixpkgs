7b4d6c56f4371afec96f013a7a6f4ac8497f9d88
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-01 20:34:48 +0100

    python313Packages.pyroute2: remove disabled
    (cherry picked from commit c5e22569e124bb7d70d7d667019edca3bfe443f0)

--- pkgs/development/python-modules/pyroute2/default.nix
+++ pkgs/development/python-modules/pyroute2/default.nix
@@ -2,7 +2,6 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
   setuptools,
 }:
 
@@ -10,8 +9,6 @@ buildPythonPackage rec {
   pname = "pyroute2";
   version = "0.9.5";
   pyproject = true;
-
-  disabled = pythonOlder "3.9";
 
   src = fetchFromGitHub {
     owner = "svinota";
