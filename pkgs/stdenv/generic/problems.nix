4a1fdd962e5c8fde75d595352ca846bcea76adb8
Silvan Mosberger <silvan.mosberger@moduscreate.com>
2025-12-02 23:03:09 +0100

    [WIP] Fast problem handler lookup switch

--- pkgs/stdenv/generic/problems.nix
+++ pkgs/stdenv/generic/problems.nix
@@ -11,14 +11,16 @@ rec {
   ];
 
   # Take the maximum (highest severity) problem handler
-  maxProblemHandler =
+  maxProblemHandler = a: b: if problemLessThan a b then b else a;
+
+  problemLessThan =
     a: b:
-    if a == "error" || b == "error" then
-      "error"
-    else if a == "warn" || b == "warn" then
-      "warn"
+    if a == "error" then
+      false
+    else if a == "warn" then
+      b == "error"
     else
-      "ignore";
+      b != "ignore";
 
   # Currently known list of known problem kinds. Keep up to date with pkgs/top-level/config.nix
   problemKinds = [
@@ -103,4 +105,81 @@ rec {
         problem // { inherit handler; };
     in
     builtins.map addHandler (lib.mapAttrsToList addDefaults problems);
+
+  /*
+      Construct a structure as follows, with the invariant that a more specific path always has a stricter handler. E.g. if byPackage.foo.fallback.fallback == "warn", then fallback.fallback.fallback must be "ignore".
+
+      The unspecific/specific handlers for a specific problem form a lattice.
+
+      specific.<package> = {
+        specific.<name> = {
+          specific.<kind> = <ignore|warn|error>;
+          fallback = <ignore|warn|error>;
+        };
+        fallback = {
+          specific.<kind> = <ignore|warn|error>;
+          fallback = <ignore|warn|error>;
+        };
+      };
+      fallback = {
+        specific.<name> = {
+          specific.<kind> = <ignore|warn|error>;
+          fallback = <ignore|warn|error>;
+        };
+        fallback = {
+          specific.<kind> = <ignore|warn|error>;
+          fallback = <ignore|warn|error>;
+        };
+      };
+
+      We can query this structure with:
+
+        switch:
+        let
+          forPackage = switch.specific.${package} or switch.fallback;
+          forName = forPackage.specific.${name} or forPackage.fallback;
+          handler = forName.specific.${name} or forName.fallback;
+        in
+        handler
+
+      which should be fairly quick
+    }
+  */
+  genHandlerSwitch =
+    { handlers, matchers }:
+    let
+      levels = [
+        "package"
+        "name"
+        "kind"
+      ];
+
+      getHandler = lib.foldl' (acc: el: maxProblemHandler acc el.handler) "ignore";
+
+      doLevel =
+        index:
+        let
+          ident = lib.elemAt levels index;
+          nextLevel = if index + 1 == lib.length levels then getHandler else doLevel (index + 1);
+        in
+        list:
+        let
+          parted = lib.partition (m: isNull m.${ident}) list;
+          fallback = nextLevel parted.right;
+        in
+        {
+          inherit fallback;
+          specific = lib.pipe parted.wrong [
+            (lib.groupBy (m: m.${ident}))
+            (lib.mapAttrs (package: handlers: nextLevel (handlers ++ parted.right)))
+            (lib.filterAttrs (name: res: res != fallback))
+          ];
+        };
+
+      matcherSwitch = doLevel 0 matchers;
+
+      # TODO: Override specific entries in matcherSwitch with the handlers
+    in
+    matcherSwitch;
+
 }
