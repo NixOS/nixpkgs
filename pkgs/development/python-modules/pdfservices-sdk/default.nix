ef02f475c445992c86b4407f4d1cf98d0bf54c9e
HHR2020 <76608828+HHR2020@users.noreply.github.com>
2025-12-02 22:38:56 +0800

    python3Packages.pdfservices-sdk: init at 4.2.0

---
+++ pkgs/development/python-modules/pdfservices-sdk/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  wheel,
+  defusedxml,
+  requests,
+  sphinx,
+  sphinx-rtd-theme,
+}:
+
+buildPythonPackage rec {
+  pname = "pdfservices-sdk";
+  version = "4.2.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "adobe";
+    repo = "pdfservices-python-sdk";
+    rev = "v${version}";
+    hash = "sha256-m2k+IS+M8UrdrpLnk2OwRolAVq73StMY1WnxzOujBIM=";
+  };
+
+  pythonRelaxDeps = true;
+
+  build-system = [
+    setuptools
+    wheel
+  ];
+
+  dependencies = [
+    defusedxml
+    requests
+    sphinx
+    sphinx-rtd-theme
+  ];
+
+  pythonImportsCheck = [
+    "adobe.pdfservices"
+  ];
+
+  meta = {
+    description = "Adobe PDFServices Python SDK";
+    homepage = "https://github.com/adobe/pdfservices-python-sdk";
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ hhr2020 ];
+    sourceProvenance = with lib.sourceTypes; [ fromSource ];
+  };
+}
