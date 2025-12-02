522a5ceae962d83cea0165fe161dd8d889c90388
confus <con-f-use@gmx.net>
2025-12-02 18:01:38 +0100

    gokey: 0.1.3 -> 0.2.0

--- pkgs/by-name/go/gokey/package.nix
+++ pkgs/by-name/go/gokey/package.nix
@@ -5,18 +5,16 @@
 }:
 buildGoModule rec {
   pname = "gokey";
-  version = "0.1.3";
-
-  patches = [ ./version.patch ];
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "cloudflare";
     repo = "gokey";
     tag = "v${version}";
-    hash = "sha256-pvtRSWq/vXlyUShb61aiDlis9AiQnrA2PWycr1Zw0og=";
+    hash = "sha256-tJ9nCHhKPrw7SRGsqAlo/tf3tBLF63+CevEXggZADlE=";
   };
 
-  vendorHash = "sha256-qlP2tI6QQMjxP59zaXgx4mX9IWSrOKWmme717wDaUEc=";
+  vendorHash = "sha256-Btac9Oi8efqRy+OH49Na3Y6RGehHEmGfvDo2/7EWPL4=";
 
   meta = with lib; {
     homepage = "https://github.com/cloudflare/gokey";
