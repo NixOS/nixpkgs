61ff096200aefc86e58374acdddd081d4a47e784
mlyxshi <mlyxdev@gmail.com>
2025-12-02 18:58:40 +0800

    maintainers: add mlyxshi

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -17425,6 +17425,11 @@
     github = "mlvzk";
     githubId = 44906333;
   };
+  mlyxshi = {
+    name = "mlyxshi";
+    github = "mlyxshi";
+    githubId = 16594754;
+  };
   mmahut = {
     email = "marek.mahut@gmail.com";
     github = "mmahut";
