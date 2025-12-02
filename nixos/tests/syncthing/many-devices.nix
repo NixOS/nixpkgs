97d5c7782dd3e8bf741d7a0dcf4fb001f3347cb4
Doron Behar <doron.behar@gmail.com>
2025-12-02 15:12:24 +0200

    nixosTests.syncthing-many-devices: fix
    Use a name & path attributes for device & folder JSON object, otherwise
    the following error is observed when running manually the script:
    
        folder "DeleteMe": folder has empty path

--- nixos/tests/syncthing/many-devices.nix
+++ nixos/tests/syncthing/many-devices.nix
@@ -141,9 +141,9 @@ let
             --retry 5 --retry-delay 1 --retry-all-errors \
             "$@"
     }
-    curl -d ${lib.escapeShellArg (builtins.toJSON { deviceID = IDsToDelete.device; })} \
+    curl -d ${lib.escapeShellArg (builtins.toJSON { deviceID = IDsToDelete.device; name = "DeleteMe"; })} \
         -X POST 127.0.0.1:8384/rest/config/devices
-    curl -d ${lib.escapeShellArg (builtins.toJSON { id = IDsToDelete.folder; })} \
+    curl -d ${lib.escapeShellArg (builtins.toJSON { id = IDsToDelete.folder; path = "/var/lib/syncthing/DeleteMe"; })} \
         -X POST 127.0.0.1:8384/rest/config/folders
   '';
 in
