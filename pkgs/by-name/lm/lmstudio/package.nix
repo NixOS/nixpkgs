84376dcdbfaa289c5bbf37eb5dd67831abffbf0a
crertel <chris@kedagital.com>
2025-12-01 21:18:22 -0600

    lmstudio: 0.3.31.7 -> 0.3.32.2
    (cherry picked from commit 9e6ba7861609ae9ee4fa424eaddd36e321ba57dc)

--- pkgs/by-name/lm/lmstudio/package.nix
+++ pkgs/by-name/lm/lmstudio/package.nix
@@ -7,10 +7,10 @@
 let
   pname = "lmstudio";
 
-  version_aarch64-darwin = "0.3.31-7";
-  hash_aarch64-darwin = "sha256-OyPHKUmZsIU0kc3JxwxdxACSN7hR6gFWjMnGp1x5diQ=";
-  version_x86_64-linux = "0.3.31-7";
-  hash_x86_64-linux = "sha256-NeVteQlzygHkwwgLkB5n7meH02WhFE68KkbGRulDiC0=";
+  version_aarch64-darwin = "0.3.32-2";
+  hash_aarch64-darwin = "sha256-7GB0Ccfa7/EuWLlp8IM/1db0ThsaKRMmLiU25rZ2sWQ=";
+  version_x86_64-linux = "0.3.32-2";
+  hash_x86_64-linux = "sha256-JH9sH1zzBJkzQnu8MZhZ81xgQGgpzRduQEiap1lmURA=";
 
   meta = {
     description = "LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models (LLMs)";
