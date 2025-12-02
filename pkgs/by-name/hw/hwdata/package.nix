5ef2969e5584f5fffed3dd8cd7582a07f543bdd1
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-02 13:58:28 +0000

    hwdata: 0.401 -> 0.402

--- pkgs/by-name/hw/hwdata/package.nix
+++ pkgs/by-name/hw/hwdata/package.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "hwdata";
-  version = "0.401";
+  version = "0.402";
 
   src = fetchFromGitHub {
     owner = "vcrhonek";
     repo = "hwdata";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-2NZwylrUfnzA0aE+xlVZ7QCpCzfW9DwGzRVHirt0TRU=";
+    hash = "sha256-akLI2MdF6BDwvSoKt1jhlMyhKQv4TWxLFZWF7ivIezA=";
   };
 
   doCheck = false; # this does build machine-specific checks (e.g. enumerates PCI bus)
