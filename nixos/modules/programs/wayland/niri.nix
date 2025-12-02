21546025de40141625ff5efe191fab8fd779cf8b
Rui Jiang <me@ruijiang.me>
2025-12-02 12:07:37 -0600

    Revert "nixos/niri: add nautilus to systemPackages"
    This reverts commit ac518ed16b15a950c61d6d12bfa8036b80901401.
    
    The original commit causes an unexpected side effect for Niri users. In
    particular, enabling the Niri module (`programs.niri.enable = true;`)
    results in Nautilus being added to `environment.systemPackages` implicitly.
    
    Reasons for revert:
    
    - This breaks the expectation that enabling Niri (or any desktop module)
      does not install unrelated GUI applications.
    - It violates NixOS module design conventions: modules should not inject
      arbitrary applications into `environment.systemPackages` by default.
    - The behavior is surprising and hard to trace: Nautilus appears without
      the user ever referencing GNOME, gvfs, portals, or Nautilus itself.
    
    Until consensus is reached on how file chooser integration should work
    for Niri and other desktops, this revert restores the expected minimal
    behavior.

--- nixos/modules/programs/wayland/niri.nix
+++ nixos/modules/programs/wayland/niri.nix
@@ -12,22 +12,12 @@ in
     enable = lib.mkEnableOption "Niri, a scrollable-tiling Wayland compositor";
 
     package = lib.mkPackageOption pkgs "niri" { };
-
-    useNautilus = lib.mkEnableOption "Nautilus as file-chooser for xdg-desktop-portal-gnome" // {
-      default = true;
-    };
   };
 
   config = lib.mkIf cfg.enable (
     lib.mkMerge [
       {
-        environment.systemPackages = [
-          cfg.package
-        ]
-        # Required for xdg-desktop-portal-gnome's FileChooser to work properly
-        ++ lib.optionals cfg.useNautilus [
-          pkgs.nautilus
-        ];
+        environment.systemPackages = [ cfg.package ];
 
         services = {
           displayManager.sessionPackages = [ cfg.package ];
@@ -43,10 +33,6 @@ in
           enable = lib.mkDefault true;
 
           configPackages = [ cfg.package ];
-
-          config.niri = lib.mkIf (!cfg.useNautilus) {
-            "org.freedesktop.impl.portal.FileChooser" = lib.mkDefault "gtk";
-          };
 
           # Recommended by upstream, required for screencast support
           # https://github.com/YaLTeR/niri/wiki/Important-Software#portals
