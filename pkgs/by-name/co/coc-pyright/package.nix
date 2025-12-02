3ed19cab461289a2c12efe5cf8676bcb69db24bf
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-02 17:50:04 +0000

    coc-pyright: 0-unstable-2025-11-01 -> 0-unstable-2025-12-01

--- pkgs/by-name/co/coc-pyright/package.nix
+++ pkgs/by-name/co/coc-pyright/package.nix
@@ -7,17 +7,17 @@
 
 buildNpmPackage {
   pname = "coc-pyright";
-  version = "0-unstable-2025-11-01";
+  version = "0-unstable-2025-12-01";
 
   src = fetchFromGitHub {
     owner = "fannheyward";
     repo = "coc-pyright";
     # No tagged releases, this commit corresponds to the latest release of the package.
-    rev = "9ac99c71ea92810b532283b690f6771601c74ff2";
-    hash = "sha256-nADl29GiApBZMKW0yQ9QVWBiduaGNXO8Cm4XxZmNfMQ=";
+    rev = "263919ddd3dce33d15626ed6c6139702839295e0";
+    hash = "sha256-tcD4Irq3IRVNFh1rCfvVg1VSbiMtc1bswAKTRmIfo8Y=";
   };
 
-  npmDepsHash = "sha256-+9bf/3b8vZZgkiC2DA15js9X5PFHzK9dltTA9XJt+GE=";
+  npmDepsHash = "sha256-LW0twhPlWZLFYSzfzoi9Rg8+EsmkllfasR51YtrcdnQ=";
 
   passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 
