0a1256c09cbd4cb56b0b179c8160925b31a9463c
Jason Yundt <jason@jasonyundt.email>
2025-12-02 17:04:46 -0500

    lib.strings.escapePythonBytes: simplify implementation
    This commit simplifies the implementation of
    lib.strings.escapePythonBytes by incorporating feedback from the
    following review comments:
    
    • <https://github.com/NixOS/nixpkgs/pull/458502#discussion_r2516097137>
    • <https://github.com/NixOS/nixpkgs/pull/458502#discussion_r2516100489>
    • <https://github.com/NixOS/nixpkgs/pull/458502#discussion_r2516108272>
    • <https://github.com/NixOS/nixpkgs/pull/458502#discussion_r2516137122>
    • <https://github.com/NixOS/nixpkgs/pull/458502#discussion_r2516149214>
    
    Co-authored-by: Matt Sturgeon <matt@sturgeon.me.uk>

--- lib/strings.nix
+++ lib/strings.nix
@@ -1544,8 +1544,33 @@ rec {
   escapePythonBytes =
     s:
     let
-      byteToHexTable = import ./byte-to-hex-table;
-      byteToEscapedHex = byte: "\\x${builtins.getAttr byte byteToHexTable}";
+      # Generate a two-digit hex string from a number (0-255).
+      toTwoDigitHexString = n: fixedWidthNumber 2 (lib.trivial.toHexString n);
+
+      # Create a string containing all bytes from 0x01 to 0xFF.
+      # We do this by reading a pre-generated binary file that contains all these bytes.
+      # This is necessary because Nix doesn't provide a way to construct arbitrary
+      # byte values programmatically - we can only work with strings that come from
+      # external sources (files, environment variables, etc.).
+      allBytes = readFile ./all-bytes-except-null.bin;
+
+      # Split the string into individual bytes.
+      byteList = stringToCharacters allBytes;
+
+      # Create attribute set mapping each byte to its hex representation.
+      # We know the file contains bytes 0x01 through 0xFF in order.
+      byteAttrs = genList (i: {
+        name = elemAt byteList i;
+        value = toTwoDigitHexString (i + 1);
+      }) 255;
+      # A lookup table mapping bytes (as single-byte strings) to their
+      # hexadecimal representation (as two-character uppercase strings).
+      #
+      # This is used to convert arbitrary byte sequences into Python bytes
+      # literals.
+      byteToHexTable = builtins.listToAttrs byteAttrs;
+
+      byteToEscapedHex = byte: "\\x" + byteToHexTable.${byte};
 
       argumentWithoutContext = unsafeDiscardStringContext s;
       returnValueWithoutContext = "b'${stringAsChars byteToEscapedHex argumentWithoutContext}'";
