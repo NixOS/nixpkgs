6abd23f7c3211bb73dc8b632d42b79d899bffe5a
Doron Behar <doron.behar@gmail.com>
2025-12-02 10:47:01 +0200

    beets: Allow to override any argument (not just ffmpeg)

--- pkgs/by-name/be/beets/package.nix
+++ pkgs/by-name/be/beets/package.nix
@@ -1,10 +1,8 @@
 {
-  ffmpeg,
   python3Packages,
-}:
+  ...
+}@args:
 
 python3Packages.toPythonApplication (
-  python3Packages.beets.override {
-    inherit ffmpeg;
-  }
+  python3Packages.beets.override (builtins.removeAttrs args [ "python3Packages" ])
 )
