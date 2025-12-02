22b0c2415654795d7495d558673cd1f0a7a488f8
qbisi <qbisicwate@gmail.com>
2025-12-02 21:15:14 +0800

    paraview: fix build against qt-6.10.1
    (cherry picked from commit ece0f4ee067dac992b7d455607e50e45b2831311)

--- pkgs/by-name/pa/paraview/package.nix
+++ pkgs/by-name/pa/paraview/package.nix
@@ -31,12 +31,18 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-XlasevXpJbPP0/q4JHCTPLq8fo/ah+FK9k+ZXWBk6wY=";
   };
 
-  # When building paraview with external vtk, we can not infer resource_dir
-  # from the path of vtk's libraries. Thus hardcoding the resource_dir.
-  # See https://gitlab.kitware.com/paraview/paraview/-/issues/23043.
   postPatch = ''
+    # When building paraview with external vtk, we can not infer resource_dir
+    # from the path of vtk's libraries. Thus hardcoding the resource_dir.
+    # See https://gitlab.kitware.com/paraview/paraview/-/issues/23043.
     substituteInPlace Remoting/Core/vtkPVFileInformation.cxx \
       --replace-fail "return resource_dir;" "return \"$out/share/paraview\";"
+
+    # fix build against qt-6.10.1
+    substituteInPlace Qt/Core/{pqFlatTreeViewEventTranslator,pqQVTKWidgetEventTranslator}.cxx \
+      ThirdParty/QtTesting/vtkqttesting/{pqAbstractItemViewEventTranslator,pqBasicWidgetEventTranslator}.cxx \
+      --replace-fail "mouseEvent->buttons()" "static_cast<int>(mouseEvent->buttons())" \
+      --replace-fail "mouseEvent->modifiers()" "static_cast<int>(mouseEvent->modifiers())"
   '';
 
   nativeBuildInputs = [
