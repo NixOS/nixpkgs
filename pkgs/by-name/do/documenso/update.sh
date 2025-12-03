8ac542e347f8a25379490fc4ae7154532ccecc11
Alexander Sieg <alex@xanderio.de>
2025-12-03 16:07:42 +0100

    documenso: 1.12.6 -> 2.2.0

--- pkgs/by-name/do/documenso/update.sh
+++ pkgs/by-name/do/documenso/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p curl git jq nodejs_24 npm-lockfile-fix
+#!nix-shell -i bash -p curl git nodejs_latest nix-update npm-lockfile-fix
+set -xeou pipefail
 
 WORKDIR=$(mktemp -d)
 trap 'rm -rf "$WORKDIR"' EXIT
@@ -13,21 +14,23 @@ if [[ "$UPDATE_NIX_OLD_VERSION" == "$NEW_VERSION" ]]; then
   echo "package is up to date: $UPDATE_NIX_OLD_VERSION"
   exit 0
 fi
-#
-# Clone source
+
 git clone "https://github.com/documenso/documenso" -b "$NEW_VERSION" "$WORKDIR/src"
+
 pushd "$WORKDIR/src" || exit
 
 echo "rm inngest-cli from root"
-npm uninstall inngest-cli
+npm uninstall --package-lock-only inngest-cli
 
 echo "adding node-addon-api dependency"
-npm install node-addon-api
+npm install --save-dev --package-lock-only node-addon-api
 
 echo "fix package-lock.json hashes"
 npm-lockfile-fix package-lock.json
 
 git diff --no-ext-diff package-lock.json > "$PACKAGE_DIR/package-lock.json.patch"
 git diff --no-ext-diff package.json > "$PACKAGE_DIR/package.json.patch"
+
+popd || exit
 
 nix-update "$UPDATE_NIX_PNAME"
