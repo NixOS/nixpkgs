7445cf86239a765b157f876d12fa65883f08642a
Pratham Patel <prathampatel@thefossguy.com>
2025-12-04 00:17:28 +0530

    xdg-desktop-portal-cosmic: 1.0.0-beta.8 -> 1.0.0-beta.9
    (cherry picked from commit 357b08f8ad2f235d851dbcfe1ebc1b80c2755cf8)

--- pkgs/by-name/xd/xdg-desktop-portal-cosmic/package.nix
+++ pkgs/by-name/xd/xdg-desktop-portal-cosmic/package.nix
@@ -16,7 +16,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "xdg-desktop-portal-cosmic";
-  version = "1.0.0-beta.8";
+  version = "1.0.0-beta.9";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
