e8553485f61483938731e6adee40a98ea80b0711
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-03 18:37:20 +0100

    python3Packages.roman-numerals: rename from roman-numerals-py
    Untagged rename required for Sphinx 9.0.

--- pkgs/development/python-modules/roman-numerals/default.nix
+++ pkgs/development/python-modules/roman-numerals/default.nix
@@ -2,13 +2,14 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   flit-core,
   pytestCheckHook,
   sphinx,
 }:
 
 buildPythonPackage rec {
-  pname = "roman-numerals-py";
+  pname = "roman-numerals";
   version = "3.1.0";
   pyproject = true;
 
@@ -18,6 +19,13 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-YLF09jYwXq48iMvmqbj/cocYJPp7RsCXzbN0DV9gpis=";
   };
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/AA-Turner/roman-numerals/commit/cc8ec3aca53c9246965500f8fc14aee636fd5307.patch";
+      hash = "sha256-zSnJ3DP0hdwhKkFzPWZGJNn1OzwLBF3W8Q6KzwX4Ap4=";
+    })
+  ];
 
   postPatch = ''
     ls -lah
@@ -37,7 +45,6 @@ buildPythonPackage rec {
     homepage = "https://github.com/AA-Turner/roman-numerals/";
     changelog = "https://github.com/AA-Turner/roman-numerals/blob/${src.tag}/CHANGES.rst";
     license = lib.licenses.cc0;
-    mainProgram = "roman-numerals-py";
     platforms = lib.platforms.all;
   };
 }
