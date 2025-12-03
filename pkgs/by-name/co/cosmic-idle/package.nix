a06c94f1e92f47c1ed877af9261d0e80eccfe574
Pratham Patel <prathampatel@thefossguy.com>
2025-12-04 00:17:23 +0530

    cosmic-idle: 1.0.0-beta.8 -> 1.0.0-beta.9
    (cherry picked from commit 49461de305b87076b66fab214ad96eda59cf6f8e)

--- pkgs/by-name/co/cosmic-idle/package.nix
+++ pkgs/by-name/co/cosmic-idle/package.nix
@@ -16,7 +16,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-idle";
-  version = "1.0.0-beta.8";
+  version = "1.0.0-beta.9";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
