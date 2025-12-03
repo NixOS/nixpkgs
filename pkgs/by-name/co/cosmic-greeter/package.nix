6e59df0b635c5ce9d4e78e3f978cc34c07174cd7
Pratham Patel <prathampatel@thefossguy.com>
2025-12-04 00:17:23 +0530

    cosmic-greeter: 1.0.0-beta.8 -> 1.0.0-beta.9

--- pkgs/by-name/co/cosmic-greeter/package.nix
+++ pkgs/by-name/co/cosmic-greeter/package.nix
@@ -19,7 +19,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-greeter";
-  version = "1.0.0-beta.8";
+  version = "1.0.0-beta.9";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
