14f8a01a3bce1f8172115678446a1a225e03644c
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-03 19:03:06 +0100

    python3Packages.sphinx-autodoc-typehints: sphinx 9.0 compat

--- pkgs/development/python-modules/sphinx-autodoc-typehints/default.nix
+++ pkgs/development/python-modules/sphinx-autodoc-typehints/default.nix
@@ -23,6 +23,12 @@ buildPythonPackage {
     hash = "sha256-X81KPreqiUJMHi4yvtymbtw4NnVpyRaagPSz6TQXH9s=";
   };
 
+  postPatch = ''
+    # https://github.com/tox-dev/sphinx-autodoc-typehints/issues/586
+    substituteInPlace src/sphinx_autodoc_typehints/__init__.py \
+      --replace-fail "sphinx.ext.autodoc.mock" "sphinx.ext.autodoc._dynamic._mock"
+  '';
+
   pythonRelaxDeps = [ "sphinx" ];
 
   build-system = [
