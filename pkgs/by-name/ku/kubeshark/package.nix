80ac3451c54d7702d75d2ec20c2aa766654c941e
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-03 21:57:44 +0000

    kubeshark: 52.10.0 -> 52.10.3

--- pkgs/by-name/ku/kubeshark/package.nix
+++ pkgs/by-name/ku/kubeshark/package.nix
@@ -11,13 +11,13 @@
 
 buildGoModule rec {
   pname = "kubeshark";
-  version = "52.10.0";
+  version = "52.10.3";
 
   src = fetchFromGitHub {
     owner = "kubeshark";
     repo = "kubeshark";
     rev = "v${version}";
-    hash = "sha256-7T23cdPIE7X5Hx03sY93z/qrqRMGUTMObWvwdH6Hj5U=";
+    hash = "sha256-n7AYUms6fn25UinLd5xFG2DfcpJU0/pR4JF3i1VY1hM=";
   };
 
   vendorHash = "sha256-4s1gxJo2w5BibZ9CJP7Jl9Z8Zzo8WpBokBnRN+zp8b4=";
