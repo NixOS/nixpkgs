744a9e9090f72fe072ac6a7368c2031f1e15d4fa
Alexander Sieg <alex@xanderio.de>
2025-12-03 16:07:42 +0100

    nixos/documenso: init basic module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1586,6 +1586,7 @@
   ./services/web-apps/dependency-track.nix
   ./services/web-apps/dex.nix
   ./services/web-apps/discourse.nix
+  ./services/web-apps/documenso.nix
   ./services/web-apps/documize.nix
   ./services/web-apps/docuseal.nix
   ./services/web-apps/dokuwiki.nix
