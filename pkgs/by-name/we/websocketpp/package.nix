fd3ddc2300142b3dd2861db2a26c4fd11011d373
aleksana <me@aleksana.moe>
2025-12-03 21:51:42 +0800

    websocketpp: add patch to fix build with boost187

--- pkgs/by-name/we/websocketpp/package.nix
+++ pkgs/by-name/we/websocketpp/package.nix
@@ -18,10 +18,14 @@ stdenv.mkDerivation rec {
   };
 
   patches = [
+    # Fix build with cmake4
     (fetchpatch {
       url = "https://github.com/zaphoyd/websocketpp/commit/deb0a334471362608958ce59a6b0bcd3e5b73c24.patch?full_index=1";
       hash = "sha256-bFCHwtRuCFz9vr4trmmBLziPSlEx6SNjsTcBv9zV8go=";
     })
+    # Fix build with boost187/newer asio
+    # https://github.com/zaphoyd/websocketpp/pull/1164
+    ./websocketpp-0.8.2-boost-1.87-compat.patch
   ];
 
   nativeBuildInputs = [ cmake ];
