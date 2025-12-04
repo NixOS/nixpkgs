fab23265d6c6678e49a0526d6111ac8a9c26fdc9
Shelvacu <git@shelvacu.com>
2025-12-03 16:07:42 -0800

    copyparty-{min,most,full-buggy}: tell nixpkgs-update not to run on these packages

--- pkgs/by-name/co/copyparty-most/package.nix
+++ pkgs/by-name/co/copyparty-most/package.nix
@@ -1,5 +1,5 @@
 { copyparty }:
-copyparty.override {
+(copyparty.override {
   withHashedPasswords = true;
   withCertgen = true;
   withThumbnails = true;
@@ -14,4 +14,16 @@ copyparty.override {
   withMagic = true;
   nameSuffix = "-most";
   longDescription = "Almost-full variant, all dependencies and features except those marked buggy";
-}
+}).overrideAttrs
+  (old: {
+    # don't try to update this package, just update `copyparty`
+    # nixpkgs-update: no auto update
+    passthru = old.passthru // {
+      updateScript = null;
+    };
+
+    meta = old.meta // {
+      # this serves two purposes: it changes the description, but also makes meta.position point to this file so that the 'no auto update' works
+      description = old.meta.description + " - most variant";
+    };
+  })
