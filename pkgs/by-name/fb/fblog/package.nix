c07afc6f205dd84073467e62b63b4f0be1e97828
Petr Portnov <mrjarviscraft+nix@gmail.com>
2025-12-04 21:18:16 +0300

    fblog: use finalAttrs
    (cherry picked from commit cd52b4e7e557df395aaceaccb2b7ce2d133c030d)

--- pkgs/by-name/fb/fblog/package.nix
+++ pkgs/by-name/fb/fblog/package.nix
@@ -2,26 +2,32 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  versionCheckHook,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "fblog";
   version = "4.16.0";
 
   src = fetchFromGitHub {
     owner = "brocode";
     repo = "fblog";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-SWwk7qNe2R1aBYGBFqltUZjeOvr4jG1P7/CPIAfHCc8=";
   };
 
   cargoHash = "sha256-du9FXuUNqQm1AMqcCFqeso5OPrPCxzTVl5e7kR0rpwc=";
 
-  meta = with lib; {
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
+
+  meta = {
     description = "Small command-line JSON log viewer";
     mainProgram = "fblog";
     homepage = "https://github.com/brocode/fblog";
-    license = licenses.wtfpl;
-    maintainers = [ maintainers.progrm_jarvis ];
+    changelog = "https://github.com/brocode/fblog/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.wtfpl;
+    maintainers = [ lib.maintainers.progrm_jarvis ];
   };
-}
+})
