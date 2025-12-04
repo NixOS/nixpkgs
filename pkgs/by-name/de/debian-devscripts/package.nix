340f7c2c32f3e25e01f25877d57fe566dac65732
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-04 11:26:13 +0000

    debian-devscripts: 2.25.27 -> 2.25.29

--- pkgs/by-name/de/debian-devscripts/package.nix
+++ pkgs/by-name/de/debian-devscripts/package.nix
@@ -30,14 +30,14 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "debian-devscripts";
-  version = "2.25.27";
+  version = "2.25.29";
 
   src = fetchFromGitLab {
     domain = "salsa.debian.org";
     owner = "debian";
     repo = "devscripts";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-pTRJAiwEvfStsOOTwh6vP6kiiYppL5XG9QcCcHNaHp8=";
+    hash = "sha256-v3xRIy5Re1oZLP8/5RpyOWzJMkrHh4Dag0x+fE9G4EY=";
   };
 
   patches = [
