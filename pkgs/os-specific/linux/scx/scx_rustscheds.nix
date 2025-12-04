c1b04b67866cc210fcacf880bd84c63eba9589d5
Gliczy <129636582+Gliczy@users.noreply.github.com>
2025-12-04 03:04:15 +0100

    scx.full: 1.0.18 -> 1.0.19

--- pkgs/os-specific/linux/scx/scx_rustscheds.nix
+++ pkgs/os-specific/linux/scx/scx_rustscheds.nix
@@ -11,19 +11,27 @@
   libseccomp,
   nix-update-script,
   nixosTests,
+  fetchpatch,
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "scx_rustscheds";
-  version = "1.0.18";
+  version = "1.0.19";
 
   src = fetchFromGitHub {
     owner = "sched-ext";
     repo = "scx";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-RkTY7gDcKbkNUKl7NJDX3Ac/I+dRG1Gj8rRHynbbxUU=";
+    hash = "sha256-bOldw2Sob5aANmVzw6VwCgJ4+VzEsohKUxOxntow7VY=";
   };
 
-  cargoHash = "sha256-tuZhqDT1xMP+Pufwz6SBt44qNzHuGzcU9QmVNIg2zS0=";
+  cargoHash = "sha256-ik05X+5jIdxtXYhN6fb1URW8TKKzgFuevi5+Wm2j15Y=";
+
+  patches = [
+    (fetchpatch {
+      url = "https://patch-diff.githubusercontent.com/raw/sched-ext/scx/pull/3127.patch";
+      hash = "sha256-HpGJR3eBZKE+VsqGivjJp1n7JIORhZUxG87AsP1WWi0=";
+    })
+  ];
 
   nativeBuildInputs = [
     pkg-config
@@ -59,6 +67,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
     "--skip=cpumask"
     "--skip=topology"
     "--skip=proc_data::tests::test_thread_operations"
+    "--skip=json::tests::test_with_resources"
+    "--skip=json::tests::test_with_dir"
   ];
 
   passthru.tests.basic = nixosTests.scx;
