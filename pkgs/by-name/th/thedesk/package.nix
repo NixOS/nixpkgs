9e82842761d4b9bb287407b1d8c26ffcbca979cc
kyehn <kyehn@users.noreply.github.com>
2025-12-04 06:14:55 +0000

    thedesk: 25.2.1 -> 25.2.2
    Diff: https://github.com/cutls/thedesk-next/compare/v25.2.1...v25.2.2

--- pkgs/by-name/th/thedesk/package.nix
+++ pkgs/by-name/th/thedesk/package.nix
@@ -17,11 +17,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "thedesk";
-  version = "25.2.1";
+  version = "25.2.2";
 
   src = fetchurl {
     url = "https://github.com/cutls/thedesk-next/releases/download/v${finalAttrs.version}/thedesk-next_${finalAttrs.version}_amd64.deb";
-    hash = "sha256-bUhMDkDKKHI9HXXheCwxmODnfDfZ4AG38XNZOzr2NR8=";
+    hash = "sha256-9Xd0YHkFHPVY6BHy0V1X7p27m2iJFVHmSickzMJeOXs=";
   };
 
   nativeBuildInputs = [
