688654718431a0c4e54aacb14c5453eea6617cdd
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-12-04 15:04:12 +0100

    msolve: 0.9.2 -> 0.9.3

--- pkgs/by-name/ms/msolve/package.nix
+++ pkgs/by-name/ms/msolve/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "msolve";
-  version = "0.9.2";
+  version = "0.9.3";
 
   src = fetchFromGitHub {
     owner = "algebraic-solving";
     repo = "msolve";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-/KV4zmato86DKDOUe3D/Ru/cFQaKOPyx6cQ8wZS4bgA=";
+    hash = "sha256-N/ePJGbF3pGU+IiyztkKujpfFRLy7FSIkpUltv6iJXY=";
   };
 
   postPatch = ''
