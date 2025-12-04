fb5a58281cc71599f3910fb4f3ce63a571a4e53a
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-12-03 17:51:12 -0800

    maintainers: add not-a-cowfr

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18924,6 +18924,12 @@
     github = "nosewings";
     githubId = 24929858;
   };
+  not-a-cowfr = {
+    name = "Andrew Gielow";
+    email = "agielow@proton.me";
+    github = "not-a-cowfr";
+    githubId = 104355555;
+  };
   not-my-segfault = {
     email = "michal@tar.black";
     matrix = "@michal:tar.black";
