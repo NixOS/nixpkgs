3273354ff323a843dccd9edaca7ce25f99b2155c
nyanloutre <paul@nyanlout.re>
2025-12-05 13:33:51 +0000

    tuliprox: disable tests in 3.2.0
    tests are broken in this version and will return an error
    no method named `is_ok` found for opaque type `impl futures::Future<Output = Result<Vec<api_user::TargetUser>, std::io::Error>>` in the current scope

--- pkgs/by-name/tu/tuliprox/package.nix
+++ pkgs/by-name/tu/tuliprox/package.nix
@@ -56,6 +56,9 @@ rustPlatform.buildRustPackage rec {
     popd
   '';
 
+  # Tests are broken in 3.2.0 version
+  doCheck = lib.versionAtLeast version "3.2.1";
+
   checkFlags = [
     "--skip=processing::parser::xmltv::tests::normalize"
     "--skip=processing::parser::xtream::tests::test_read_json_file_into_struct"
