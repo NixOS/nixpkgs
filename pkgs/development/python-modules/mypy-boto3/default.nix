05678843431d9cb1150284970f96079a5d0f4819
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-05 15:31:01 +0100

    python312Packages.mypy-boto3-redshift-serverless: 1.41.2 -> 1.42.3

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1101,8 +1101,8 @@ in
       "sha256-Mby+hQJcBXqmDY5wC1Uut4EQex1PmjT8bgB81rT5NKU=";
 
   mypy-boto3-redshift-serverless =
-    buildMypyBoto3Package "redshift-serverless" "1.41.2"
-      "sha256-hxpsKuTEZPkDx6f1ynsSCHeM46Lrdx6l+js5BUQLUys=";
+    buildMypyBoto3Package "redshift-serverless" "1.42.3"
+      "sha256-fZnYq9s72H/IrAVqWQIeKMNfyLcD7K1CIbplX3P7dzc=";
 
   mypy-boto3-rekognition =
     buildMypyBoto3Package "rekognition" "1.41.0"
