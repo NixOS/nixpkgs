f3ed2ede566905d433feca47e6e901e8cd287046
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-05 02:26:51 +0000

    mupdf: 1.26.10 -> 1.26.12

--- pkgs/by-name/mu/mupdf/package.nix
+++ pkgs/by-name/mu/mupdf/package.nix
@@ -73,12 +73,12 @@ let
   });
 in
 stdenv.mkDerivation rec {
-  version = "1.26.10";
+  version = "1.26.12";
   pname = "mupdf";
 
   src = fetchurl {
     url = "https://mupdf.com/downloads/archive/${pname}-${version}-source.tar.gz";
-    hash = "sha256-FlPzW9j72XDwVSPv3H+G5B6XKOJWSjKVKW4Dz1mlFDc=";
+    hash = "sha256-a6+RCSj0BBZ7pJvmNAGV3sNAeVckciszH1ohQ/WqDQE=";
   };
 
   patches = [
