d2534755b05dee1954eead2bfc35580949e340c4
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-05 01:47:22 +0000

    python3Packages.bellows: 0.48.0 -> 0.48.2

--- pkgs/development/python-modules/bellows/default.nix
+++ pkgs/development/python-modules/bellows/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "bellows";
-  version = "0.48.0";
+  version = "0.48.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "zigpy";
     repo = "bellows";
     tag = version;
-    hash = "sha256-KZ4VmtHQnQ+t9q/ZtzmlObpnBIxlgkT44nt11f/1FpQ=";
+    hash = "sha256-igv/F65oZKYj8hd9KCYlbz6Yf9Ny1lnW4yFs0XT4waQ=";
   };
 
   postPatch = ''
