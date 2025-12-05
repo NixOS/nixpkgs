585e5f992fe095d8991506953a2f2545f1bc3f30
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-05 15:31:14 +0100

    python312Packages.mypy-boto3-rolesanywhere: 1.41.0 -> 1.42.3

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1129,8 +1129,8 @@ in
       "sha256-jYAsZ1lMU9cl4rIvRO1UZLn4nIsuauWrNRwyB0j4HK0=";
 
   mypy-boto3-rolesanywhere =
-    buildMypyBoto3Package "rolesanywhere" "1.41.0"
-      "sha256-wcYk7NsqlkHelo0ai/ZdsRz+HVANcf+kWYFbnGpOdzE=";
+    buildMypyBoto3Package "rolesanywhere" "1.42.3"
+      "sha256-BzohFxZYhyKFmy8wIG6zdGsuPMh9Nr0GrY//d5q/Or4=";
 
   mypy-boto3-route53 =
     buildMypyBoto3Package "route53" "1.41.4"
