3793aae7bd3f8cfbe2bf40dfb3fcb8eeba197040
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-05 01:26:45 +0100

    python3Packages.pyscaffoldext-django: relax django, modernize, cleanup

--- pkgs/development/python-modules/pyscaffoldext-django/default.nix
+++ pkgs/development/python-modules/pyscaffoldext-django/default.nix
@@ -2,18 +2,14 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+
+  # build-system
   setuptools,
   setuptools-scm,
-  wheel,
+
+  # dependencies
   django,
   pyscaffold,
-  configupdater,
-  pre-commit,
-  pytest,
-  pytest-cov,
-  pytest-xdist,
-  tox,
-  virtualenv,
 }:
 
 buildPythonPackage rec {
@@ -26,29 +22,19 @@ buildPythonPackage rec {
     hash = "sha256-5yzF3VK/9VlCSrRsRJWX4arr9n34G2R6O5A51jTpLhg=";
   };
 
-  nativeBuildInputs = [
+  build-system = [
     setuptools
     setuptools-scm
-    wheel
   ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     django
     pyscaffold
   ];
 
-  optional-dependencies = {
-    testing = [
-      configupdater
-      pre-commit
-      pytest
-      pytest-cov
-      pytest-xdist
-      setuptools-scm
-      tox
-      virtualenv
-    ];
-  };
+  pythonRelaxDeps = [ "django" ];
+
+  doCheck = false; # tests require git checkout
 
   pythonImportsCheck = [ "pyscaffoldext.django" ];
 
