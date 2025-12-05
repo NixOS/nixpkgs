2714531aa159b3de180777e1ce4d1e52a0ad9841
Acid Bong <acidbong@tilde.club>
2025-12-05 19:34:09 +0200

    nixos/qt: use sessionVariables for graphic settings

--- nixos/modules/config/qt.nix
+++ nixos/modules/config/qt.nix
@@ -211,7 +211,7 @@ in
         }
       ];
 
-    environment.variables = {
+    environment.sessionVariables = {
       QT_QPA_PLATFORMTHEME = lib.mkIf (cfg.platformTheme != null) cfg.platformTheme;
       QT_STYLE_OVERRIDE = lib.mkIf (cfg.style != null) cfg.style;
     };
