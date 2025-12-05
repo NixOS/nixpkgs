fdac6a229be5f57adaa8bcbe20208298e447f0a9
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-05 10:21:19 +0000

    amp-cli: 0.0.1764374502-g55c558 -> 0.0.1764921674-g2aab2c

--- pkgs/by-name/am/amp-cli/package.nix
+++ pkgs/by-name/am/amp-cli/package.nix
@@ -9,11 +9,11 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "amp-cli";
-  version = "0.0.1764374502-g55c558";
+  version = "0.0.1764921674-g2aab2c";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@sourcegraph/amp/-/amp-${finalAttrs.version}.tgz";
-    hash = "sha256-klsg2qYdrOCfYsTnLNEqXOJtbeGRKcJxQVyv7jGr77o=";
+    hash = "sha256-N4BSXADVFAkvpVTDVJTnwGwmavydRT2Utq2SZdQiiVI=";
   };
 
   postPatch = ''
@@ -45,7 +45,7 @@ buildNpmPackage (finalAttrs: {
     chmod +x bin/amp-wrapper.js
   '';
 
-  npmDepsHash = "sha256-l5z57KOyJst0OZIcYK4qS7+KuZkmJLrpXR3nAJKorfE=";
+  npmDepsHash = "sha256-RtvUbBeHyKmMMplg+vy49ZW5SulZIFcNXIHhw6kDgpU=";
 
   propagatedBuildInputs = [
     ripgrep
