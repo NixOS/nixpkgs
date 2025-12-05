69aac07b66217d67c6fb03a55aacf37b82cc6d8b
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-05 11:13:29 +0000

    python3Packages.botocore-stubs: 1.42.1 -> 1.42.3

--- pkgs/development/python-modules/botocore-stubs/default.nix
+++ pkgs/development/python-modules/botocore-stubs/default.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "botocore-stubs";
-  version = "1.42.1";
+  version = "1.42.3";
   pyproject = true;
 
   src = fetchPypi {
     pname = "botocore_stubs";
     inherit version;
-    hash = "sha256-8j3H7P/eiZGAd8P5NaGu8eSvZyiCy5jiNiqLcdirCTk=";
+    hash = "sha256-+hiujaG1SN5+vZzgRxQc5hkBqe9JTiv4XlaMBWyc0ME=";
   };
 
   nativeBuildInputs = [ setuptools ];
