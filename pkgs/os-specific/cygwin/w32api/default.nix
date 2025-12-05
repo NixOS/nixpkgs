NixOS/nixpkgs

commit 1d6417784e142778bedc5056d3610deea51aa96e

treewide: mark failing packages as broken
On 16 separate Hydra evaluations (which I selected for their relatively
low failure numbers, and which span just over a year), these packages have done
nothing other than fail for all platforms.
For Python packages, they also fail across all built Python versions.

pkgs/os-specific/cygwin/w32api/default.nix
@@ -32,6 +32,8 @@
     };
 
     meta = {
+      # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
+      broken = true;
       description = "MinGW w32api package for Cygwin";
       inherit (windows.mingw_w64_headers.meta)
         homepage

pkgs/development/python-modules/pathy/default.nix
@@ -48,8 +48,9 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "pathy" ];
 
   meta = with lib; {
+    # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
+    broken = true;
     # https://github.com/justindujardin/pathy/issues/113
-    broken = pathlib-abc.version != "0.1.1";
     description = "Path interface for local and cloud bucket storage";
     mainProgram = "pathy";
     homepage = "https://github.com/justindujardin/pathy";

pkgs/development/python-modules/scikit-learn-extra/default.nix
@@ -65,6 +65,8 @@ buildPythonPackage rec {
   ];
 
   meta = {
+    # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
+    broken = true;
     description = "Set of tools for scikit-learn";
     homepage = "https://github.com/scikit-learn-contrib/scikit-learn-extra";
     license = lib.licenses.bsd3;

pkgs/development/tools/database/indradb/default.nix
@@ -15,6 +15,9 @@ let
   };
 
   meta = with lib; {
+    # Marked broken 2025-11-28 because both indradb-server and indradb-client
+    # have failed on Hydra for nearly a year.
+    broken = true;
     description = "Graph database written in rust";
     homepage = "https://github.com/indradb/indradb";
     license = licenses.mpl20;

pkgs/applications/blockchains/optimism/geth.nix
@@ -39,6 +39,8 @@ buildGoModule rec {
   ];
 
   meta = with lib; {
+    # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
+    broken = true;
     description = "";
     homepage = "https://github.com/ethereum-optimism/op-geth";
     license = licenses.gpl3Only;

pkgs/by-name/ma/martin/package.nix
@@ -54,6 +54,8 @@ rustPlatform.buildRustPackage rec {
   ];
 
   meta = with lib; {
+    # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
+    broken = true;
     description = "Blazing fast and lightweight PostGIS vector tiles server";
     homepage = "https://martin.maplibre.org/";
     license = with licenses; [

pkgs/by-name/ne/nearcore/package.nix
@@ -54,6 +54,8 @@ rustPlatform.buildRustPackage rec {
   requiredSystemFeatures = [ "big-parallel" ];
 
   meta = with lib; {
+    # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
+    broken = true;
     description = "Reference client for NEAR Protocol";
     homepage = "https://github.com/near/nearcore";
     license = licenses.gpl3;

pkgs/by-name/ol/olaris-server/package.nix
@@ -58,6 +58,8 @@ buildGoModule rec {
   '';
 
   meta = {
+    # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
+    broken = true;
     description = "Media manager and transcoding server";
     homepage = "https://gitlab.com/olaris/olaris-server";
     changelog = "https://gitlab.com/olaris/olaris-server/-/releases/v${version}";

commit 1ef91d7e820d4c9b0ff190924ee8479c6f905247

cygwin.w32api: unset meta.broken
This package was limit to cygwin in:
9dd0669e491ee8ce9064ab94a8301b189eabf075

It was then then marked broken in:
1d6417784e142778bedc5056d3610deea51aa96e.

pkgs/os-specific/cygwin/w32api/default.nix
@@ -32,8 +32,6 @@
     };
 
     meta = {
-      # Marked broken 2025-11-28 because it has failed on Hydra for at least one year.
-      broken = true;
       description = "MinGW w32api package for Cygwin";
       inherit (windows.mingw_w64_headers.meta)
         homepage