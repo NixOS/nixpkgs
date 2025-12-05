d5b9994c5f516c36380bcfcd950ecb01927815ad
tonybtw <tonybtw@tonybtw.com>
2025-12-04 22:17:17 -0800

    maintainers: add tonybanters

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -26575,6 +26575,12 @@
     githubId = 6740669;
     name = "Tom Smeets";
   };
+  tonybanters = {
+    email = "tony@tonybtw.com";
+    github = "tonybanters";
+    githubId = 27378358;
+    name = "tony";
+  };
   toonn = {
     email = "nixpkgs@toonn.io";
     matrix = "@toonn:matrix.org";
