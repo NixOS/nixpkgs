aa979c4a8269192285ab80a5bc35132f05080e3b
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-05 15:32:55 +0100

    python312Packages.mypy-boto3-support-app: 1.41.0 -> 1.42.3

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -1337,8 +1337,8 @@ in
       "sha256-G99OogEGnAisLOIMdCdLJHfiFoZEWL+7UmySNOKyoTQ=";
 
   mypy-boto3-support-app =
-    buildMypyBoto3Package "support-app" "1.41.0"
-      "sha256-MXdyTYfdXdf4fqj1ubfRW3MPw/TgfyhkvffjlWCU5v4=";
+    buildMypyBoto3Package "support-app" "1.42.3"
+      "sha256-1MD4eTC9fVXlQoARoHll43GMf1rKLG2pvGzPGH0Q6vw=";
 
   mypy-boto3-swf =
     buildMypyBoto3Package "swf" "1.41.0"
