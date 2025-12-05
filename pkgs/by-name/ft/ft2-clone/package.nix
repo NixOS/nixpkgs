d636a5222fbc982d73dcdf19ff882745cf58e9bf
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-05 14:19:47 +0000

    ft2-clone: 2.00 -> 2.01

--- pkgs/by-name/ft/ft2-clone/package.nix
+++ pkgs/by-name/ft/ft2-clone/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ft2-clone";
-  version = "2.00";
+  version = "2.01";
 
   src = fetchFromGitHub {
     owner = "8bitbubsy";
     repo = "ft2-clone";
     rev = "v${version}";
-    hash = "sha256-Wx4dOWGyQRHgTqKZrmRIiX74UIU/ltFVAh217RYwUus=";
+    hash = "sha256-HUCaOlkCmvq/RsKFIMTrNgvBrnnAXFXmbw3pUJK2bYw=";
   };
 
   nativeBuildInputs = [ cmake ];
