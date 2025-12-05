8c5fb334dfb7c9d96b624453ed7285f69e67a53b
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-05 00:32:55 +0000

    a2ps: 4.15.7 -> 4.15.8

--- pkgs/by-name/a2/a2ps/package.nix
+++ pkgs/by-name/a2/a2ps/package.nix
@@ -14,11 +14,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "a2ps";
-  version = "4.15.7";
+  version = "4.15.8";
 
   src = fetchurl {
     url = "mirror://gnu/a2ps/a2ps-${finalAttrs.version}.tar.gz";
-    hash = "sha256-cV84Zwr9lQtMpxwB9Gj+760mXKUtPxEpNMY8Cov7uK8=";
+    hash = "sha256-jRORWjbrv6jnsjazUMyBrccUrLIXoY6NjGB0fArTU/k=";
   };
 
   postPatch = ''
