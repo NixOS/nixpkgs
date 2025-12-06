24fcbde6bc13f2fc5d15b19d7e21060eeaed629f
ZHAO Jin-Xiang <xiaoxiangmoe@gmail.com>
2025-12-07 00:58:18 +0800

    claude-code: fix nix update script

--- pkgs/by-name/cl/claude-code/package.nix
+++ pkgs/by-name/cl/claude-code/package.nix
@@ -9,7 +9,7 @@ buildNpmPackage (finalAttrs: {
   pname = "claude-code";
   # NOTE: Use the following command to update the package
   # ```sh
-  # nix-shell maintainers/scripts/update.nix --argstr commit true --argstr package vscode-extensions.anthropic.claude-code && nix-shell maintainers/scripts/update.nix --argstr commit true --argstr package claude-code
+  # nix-shell maintainers/scripts/update.nix --argstr commit true --arg predicate '(path: pkg: builtins.elem path [["claude-code"] ["vscode-extensions" "anthropic" "claude-code"]])'
   # ```
   version = "2.0.59";
 
