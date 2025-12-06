b80965dc9b88e6d7f889d7f898200ed9bd777be4
David Wronek <david.wronek@mainlining.org>
2025-12-06 12:55:28 +0100

    sshwifty: use npmHooks instead of buildNpmPackage
    Since the 0.4.2 update, Sshwifty tries to download Go modules from the
    internet during the buildNpmPackage phase. Switch to npmHooks so that
    buildGoModule can download these modules for us instead.
    
    Link: https://nixpkgs-update-logs.nix-community.org/sshwifty/2025-12-06.log
    Signed-off-by: David Wronek <david.wronek@mainlining.org>

--- pkgs/by-name/ss/sshwifty/package.nix
+++ pkgs/by-name/ss/sshwifty/package.nix
@@ -1,14 +1,15 @@
 {
   lib,
-  buildGo125Module,
-  buildNpmPackage,
+  buildGoModule,
   fetchFromGitHub,
+  fetchNpmDeps,
+  nodejs,
+  npmHooks,
   versionCheckHook,
   nixosTests,
   nix-update-script,
-  go_1_25,
 }:
-buildGo125Module (finalAttrs: {
+buildGoModule (finalAttrs: {
   pname = "sshwifty";
   version = "0.4.1-beta-release";
 
@@ -19,27 +20,27 @@ buildGo125Module (finalAttrs: {
     hash = "sha256-Kg5aE4lkzSedo+VJgdsfO5XTKupsPU2DhZNdNhEQ/Q4=";
   };
 
-  sshwifty-ui = buildNpmPackage {
-    pname = "sshwifty-ui";
-    inherit (finalAttrs) version src;
+  nativeBuildInputs = [
+    nodejs
+    npmHooks.npmConfigHook
+  ];
 
-    npmDepsHash = "sha256-vX3CtjwjzcxxIPYG6QXsPybyBRow1YdS9pHr961P1HA=";
-
-    npmBuildScript = "generate";
-
-    postInstall = ''
-      cp -r application/controller/{static_pages,static_pages.go} \
-        $out/lib/node_modules/sshwifty-ui/application/controller
-    '';
-
-    nativeBuildInputs = [ go_1_25 ];
+  overrideModAttrs = oldAttrs: {
+    nativeBuildInputs = lib.filter (drv: drv != npmHooks.npmConfigHook) oldAttrs.nativeBuildInputs;
+    preBuild = null;
   };
 
-  postPatch = ''
-    cp -r ${finalAttrs.sshwifty-ui}/lib/node_modules/sshwifty-ui/* .
-  '';
+  npmDeps = fetchNpmDeps {
+    inherit (finalAttrs) src;
+    hash = "sha256-vX3CtjwjzcxxIPYG6QXsPybyBRow1YdS9pHr961P1HA=";
+  };
 
   vendorHash = "sha256-/SLUC0xM195QfKgX9te8UP1bbzRbKF+Npyugi19JijY=";
+
+  preBuild = ''
+    # Generate static pages
+    npm run generate
+  '';
 
   ldflags = [
     "-s"
@@ -59,8 +60,6 @@ buildGo125Module (finalAttrs: {
       extraArgs = [
         "--version=unstable"
         "--version-regex=^([0-9.]+(?!.+-prebuild).+$)"
-        "--subpackage"
-        "sshwifty-ui"
       ];
     };
   };
