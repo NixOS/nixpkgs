9e952806078f11cf871435a426907073aed70482
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-06 06:37:48 +0000

    lsh: 1.5.3 -> 1.5.5

--- pkgs/by-name/ls/lsh/package.nix
+++ pkgs/by-name/ls/lsh/package.nix
@@ -5,14 +5,14 @@
 }:
 buildGoModule rec {
   pname = "lsh";
-  version = "1.5.3";
+  version = "1.5.5";
   src = fetchFromGitHub {
     owner = "latitudesh";
     repo = "lsh";
     rev = "v${version}";
-    sha256 = "sha256-W+0yuBfDjHNMEaf+T0FSVMzRyZgkeXrFwsY47ksDD5U=";
+    sha256 = "sha256-KtsREnXjyyqzNznCfp2DZmJCWIU5P6+2ItFDNngZ1ik=";
   };
-  vendorHash = "sha256-kOGHLrnpVQe8gy827CeP+1f2fy4WpUfWDfaNq/JmXpU=";
+  vendorHash = "sha256-SvbrrunOkJhIB5AlsCY7WrtE+Na/ExEJmVWqfjHNvx4=";
   subPackages = [ "." ];
   meta = {
     changelog = "https://github.com/latitudesh/lsh/releases/tag/v${version}";
