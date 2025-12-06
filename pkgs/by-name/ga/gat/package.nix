304559988386a2db88e40fc02835559db8ac8aa7
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-06 14:02:12 +0000

    gat: 0.25.7 -> 0.25.8

--- pkgs/by-name/ga/gat/package.nix
+++ pkgs/by-name/ga/gat/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "gat";
-  version = "0.25.7";
+  version = "0.25.8";
 
   src = fetchFromGitHub {
     owner = "koki-develop";
     repo = "gat";
     tag = "v${version}";
-    hash = "sha256-omdIWyBdKNYSzaCZv1vZGPv3L7ileSVwuaX6C+ZUZXY=";
+    hash = "sha256-OT0TiYvbeybWPk2Sku38ip+qmUUuFRMu1wC9936EByY=";
   };
 
-  vendorHash = "sha256-02q83ybAy2U1kdBKV6jJuK1zGFHRsCeEJdqJgWmNQBc=";
+  vendorHash = "sha256-LOim/rW8Uz5stpIQj2Ubx4F3bTv6nNAn2ZbWqW6B4rU=";
 
   env.CGO_ENABLED = 0;
 
