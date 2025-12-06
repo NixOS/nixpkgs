e56ebf32eb93903cdd3823f936a7c49960fb6fe3
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-06 17:22:22 +0000

    gnu-shepherd: 1.0.7 -> 1.0.9

--- pkgs/by-name/gn/gnu-shepherd/package.nix
+++ pkgs/by-name/gn/gnu-shepherd/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnu-shepherd";
-  version = "1.0.7";
+  version = "1.0.9";
 
   src = fetchurl {
     url = "mirror://gnu/shepherd/shepherd-${version}.tar.gz";
-    hash = "sha256-MlqbdYHug6FRFd+/vMJHyetRD3UlSaI/OukSqOxydZc=";
+    hash = "sha256-5IjFhchBjfbo9HbcqBtykQ8zfJzTYI+0Z95SYABAANY=";
   };
 
   configureFlags = [ "--localstatedir=/" ];
