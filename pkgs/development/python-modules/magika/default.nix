7303fc6ced8b52926f8b4895f9707a26b4730d89
aleksana <me@aleksana.moe>
2025-12-07 11:21:18 +0800

    python3Packages.magika: run tests

--- pkgs/development/python-modules/magika/default.nix
+++ pkgs/development/python-modules/magika/default.nix
@@ -9,6 +9,8 @@
   python-dotenv,
   tabulate,
   tqdm,
+  pytestCheckHook,
+  dacite,
   versionCheckHook,
 }:
 
@@ -17,10 +19,19 @@ buildPythonPackage rec {
   version = "1.0.1";
   pyproject = true;
 
+  # Use pypi tarball instead of GitHub source
+  # Pypi tarball contains a pure python implementation of magika
+  # while GitHub source requires compiling magika-cli
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-MT+Mv83Jp+VcJChicyMKJzK4mCXlipPeK1dlMTk7g5g=";
   };
+
+  postPatch = ''
+    substituteInPlace tests/test_python_magika_client.py \
+      --replace-fail 'python_root_dir / "src" / "magika" / "cli" / "magika_client.py"' \
+        "Path('$out/bin/magika-python-client')"
+  '';
 
   build-system = [ hatchling ];
 
@@ -34,7 +45,22 @@ buildPythonPackage rec {
   ];
 
   nativeCheckInputs = [
+    pytestCheckHook
+    dacite
     versionCheckHook
+  ];
+
+  disabledTests = [
+    # These tests require test data which doesn't exist in pypi tarball
+    "test_features_extraction_vs_reference"
+    "test_reference_generation"
+    "test_inference_vs_reference"
+    "test_reference_generation"
+    "test_magika_module_with_one_test_file"
+    "test_magika_module_with_explicit_model_dir"
+    "test_magika_module_with_basic"
+    "test_magika_module_with_all_models"
+    "test_magika_module_with_previously_missdetected_samples"
   ];
 
   pythonImportsCheck = [ "magika" ];
