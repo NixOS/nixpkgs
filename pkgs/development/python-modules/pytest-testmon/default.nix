c5f05f163dee65588996098b35abf60183e5a7d5
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-07 16:26:06 +0100

    python313Packages.pytest-testmon: modernize

--- pkgs/development/python-modules/pytest-testmon/default.nix
+++ pkgs/development/python-modules/pytest-testmon/default.nix
@@ -4,16 +4,13 @@
   coverage,
   fetchFromGitHub,
   pytest,
-  pythonOlder,
   setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "pytest-testmon";
   version = "2.2.0";
-  format = "pyproject";
-
-  disabled = pythonOlder "3.8";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tarpas";
@@ -22,11 +19,11 @@ buildPythonPackage rec {
     hash = "sha256-BVQ7rEusbW0G1C6cUeHH7fZWndSErcBQfGNdw0/4eTg=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   buildInputs = [ pytest ];
 
-  propagatedBuildInputs = [ coverage ];
+  dependencies = [ coverage ];
 
   # The project does not include tests since version 1.3.0
   doCheck = false;
@@ -36,7 +33,7 @@ buildPythonPackage rec {
   meta = with lib; {
     description = "Pytest plug-in which automatically selects and re-executes only tests affected by recent changes";
     homepage = "https://github.com/tarpas/pytest-testmon/";
-    changelog = "https://github.com/tarpas/pytest-testmon/releases/tag/v${version}";
+    changelog = "https://github.com/tarpas/pytest-testmon/releases/tag/${src.tag}";
     license = licenses.mit;
     maintainers = with maintainers; [ dmvianna ];
   };
