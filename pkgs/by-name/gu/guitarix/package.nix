1dfc1ffeb937f96ddef5fb70c9a377caa206b3ce
Lord-Valen <lord_valen@protonmail.com>
2025-12-07 16:37:25 -0500

    guitarix: simplify longDescription

--- pkgs/by-name/gu/guitarix/package.nix
+++ pkgs/by-name/gu/guitarix/package.nix
@@ -124,26 +124,19 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Virtual guitar amplifier for Linux running with JACK";
     mainProgram = "guitarix";
     longDescription = ''
-        guitarix is a virtual guitar amplifier for Linux running with
-      JACK (Jack Audio Connection Kit). It is free as in speech and
-      free as in beer. Its free sourcecode allows to build it for
-      other UNIX-like systems also, namely for BSD and for MacOSX.
+      Guitarix takes the signal from your guitar as any real amp would do: as a
+      mono-signal from your sound card. Your tone is processed by a main amp and
+      a rack-section. Both can be routed separately and deliver a processed
+      stereo-signal via JACK. You may fill the rack with effects from more than
+      25 built-in modules spanning from a simple noise-gate to brain-slashing
+      modulation-fx like flanger, phaser or auto-wah. Your signal is processed
+      with minimum latency. On a properly set-up Linux-system you do not need to
+      wait for more than 10 milliseconds for your playing to be delivered,
+      processed by guitarix.
 
-        It takes the signal from your guitar as any real amp would do:
-      as a mono-signal from your sound card. Your tone is processed by
-      a main amp and a rack-section. Both can be routed separately and
-      deliver a processed stereo-signal via JACK. You may fill the
-      rack with effects from more than 25 built-in modules spanning
-      from a simple noise-gate to brain-slashing modulation-fx like
-      flanger, phaser or auto-wah. Your signal is processed with
-      minimum latency. On any properly set-up Linux-system you do not
-      need to wait for more than 10 milli-seconds for your playing to
-      be delivered, processed by guitarix.
-
-        guitarix offers the range of sounds you would expect from a
-      full-featured universal guitar-amp. You can get crisp
-      clean-sounds, nice overdrive, fat distortion and a diversity of
-      crazy sounds never heard before.
+      Guitarix offers the range of sounds you would expect from a full-featured
+      universal guitar-amp. You can get crisp clean-sounds, nice overdrive, fat
+      distortion and a diversity of crazy sounds never heard before.
     '';
     homepage = "https://github.com/brummer10/guitarix";
     license = lib.licenses.gpl3Plus;
