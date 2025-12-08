f84be4d7b80e8dfefe1bd5d2579c529e7733b83b
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-08 01:05:28 +0100

    python3Packages.django-rq: 3.1 -> 3.2.1
    https://github.com/rq/django-rq/releases/tag/v3.2
    https://github.com/rq/django-rq/releases/tag/v3.2.1

--- pkgs/development/python-modules/django-rq/default.nix
+++ pkgs/development/python-modules/django-rq/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "django-rq";
-  version = "3.1";
+  version = "3.2.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "rq";
     repo = "django-rq";
     tag = "v${version}";
-    hash = "sha256-TnOKgw52ykKcR0gHXcdYfv77js7I63PE1F3POdwJgvc=";
+    hash = "sha256-oHBQpGRiNJFt07KEPC+KcCF6qM5PgSTyr0t35iyYhK4=";
   };
 
   build-system = [ hatchling ];
