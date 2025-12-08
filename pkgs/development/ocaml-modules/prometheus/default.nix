24d840ac98c3b1e9c33fa2368fa0768c0b54bcf4
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-08 13:25:23 +0000

    ocamlPackages.prometheus: 1.2 -> 1.3

--- pkgs/development/ocaml-modules/prometheus/default.nix
+++ pkgs/development/ocaml-modules/prometheus/default.nix
@@ -12,11 +12,11 @@
 
 buildDunePackage rec {
   pname = "prometheus";
-  version = "1.2";
+  version = "1.3";
 
   src = fetchurl {
     url = "https://github.com/mirage/prometheus/releases/download/v${version}/prometheus-${version}.tbz";
-    sha256 = "sha256-g2Q6ApprbecdFANO7i6U/v8dCHVcSkHVg9wVMKtVW8s=";
+    sha256 = "sha256-4C0UzwaCgqtk5SGIY89rg0dxdrKm63lhdcOaQAa20L8=";
   };
 
   duneVersion = "3";
