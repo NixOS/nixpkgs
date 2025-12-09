d3e1989d57dce9a7e0700e01d713a7ac8813e766
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-09 01:01:52 +0100

    c-ares: 1.34.5 -> 1.34.6
    https://c-ares.org/changelog.html#1_34_6
    https://github.com/c-ares/c-ares/security/advisories/GHSA-jq53-42q6-pqr5
    
    Fixes: CVE-2025-62408
    (cherry picked from commit 9999bd59d0785859cf4472900448f98ae533c634)

--- pkgs/development/libraries/c-ares/default.nix
+++ pkgs/development/libraries/c-ares/default.nix
@@ -18,12 +18,12 @@
 
 stdenv.mkDerivation rec {
   pname = "c-ares";
-  version = "1.34.5";
+  version = "1.34.6";
 
   src = fetchurl {
     # Note: tag name varies in some versions, e.g. v1.30.0, c-ares-1_17_0.
     url = "https://github.com/c-ares/c-ares/releases/download/v${version}/c-ares-${version}.tar.gz";
-    hash = "sha256-fZNXkOmvCBwlxJX9E8LPzaR5KYNBjpY1jvbnMg7gY0Y=";
+    hash = "sha256-kS3XzDs+innFL9f7nA9Ozwqqc+Re/aiAJmotbia4TvU=";
   };
 
   outputs = [
