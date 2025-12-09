513e8fab2dd1c8b3b638f4468fd3ff42812d4532
Tom Hunze <dev@thunze.de>
2025-12-09 13:22:17 +0100

    python313Packages.parso: modernize

--- pkgs/development/python-modules/parso/default.nix
+++ pkgs/development/python-modules/parso/default.nix
@@ -2,13 +2,14 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "parso";
   version = "0.8.5";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "davidhalter";
@@ -17,12 +18,14 @@ buildPythonPackage rec {
     hash = "sha256-faSXCrOkybLr0bboF/8rPV/Humq8s158A3UOpdlYi0I=";
   };
 
+  build-system = [ setuptools ];
+
   nativeCheckInputs = [ pytestCheckHook ];
 
-  meta = with lib; {
+  meta = {
     description = "Python Parser";
     homepage = "https://parso.readthedocs.io/en/latest/";
     changelog = "https://github.com/davidhalter/parso/blob/master/CHANGELOG.rst";
-    license = licenses.mit;
+    license = lib.licenses.mit;
   };
 }
