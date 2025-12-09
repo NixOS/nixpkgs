c1b326795c50ad95bf72b9d76a435744f351fb73
Robert Schütz <nix@dotlambda.de>
2025-12-09 09:16:04 -0800

    python3Packages.pybreaker: init at 1.4.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12943,6 +12943,8 @@ self: super: with self; {
 
   pybravia = callPackage ../development/python-modules/pybravia { };
 
+  pybreaker = callPackage ../development/python-modules/pybreaker { };
+
   pybrowserid = callPackage ../development/python-modules/pybrowserid { };
 
   pybrowsers = callPackage ../development/python-modules/pybrowsers { };
