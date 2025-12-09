f6c93436923844de6753d0e12e44262dad9ce787
MrSmör <66489839+MrSmoer@users.noreply.github.com>
2025-12-09 21:36:19 +0100

    python3Packages.libdebug: init at 0.9.0
    Libdebug is "A Python library to debug binary executables, your own way."
    It is easlily scriptable common in the ctf-community and handy for crude binary-patching.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8407,6 +8407,8 @@ self: super: with self; {
 
   libcst = callPackage ../development/python-modules/libcst { };
 
+  libdebug = callPackage ../development/python-modules/libdebug { inherit (pkgs) zstd; };
+
   libdnf = lib.pipe pkgs.libdnf [
     toPythonModule
     (
