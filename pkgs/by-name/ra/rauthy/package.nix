de7ba7cbf0eabe58a2b89cc197d5dd9d290a77bf
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-12-09 20:49:34 +0100

    rauthy: 0.32.5 -> 0.32.6

--- pkgs/by-name/ra/rauthy/package.nix
+++ pkgs/by-name/ra/rauthy/package.nix
@@ -12,16 +12,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rauthy";
-  version = "0.32.5";
+  version = "0.32.6";
 
   src = fetchFromGitHub {
     owner = "sebadob";
     repo = "rauthy";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VrQ2GABWs+n7M01cLASSAkzWkqE1kAeFK+jCk5orl90=";
+    hash = "sha256-jww2HedX7Inr2Ek0QHyktLVIxmxMLd1lzCU72G7Yj54=";
   };
 
-  cargoHash = "sha256-GRIHC8sKHL8szT5YWb8uVS8VuIOQW9LGvXlU/rUfqcg=";
+  cargoHash = "sha256-pUPLcLyTf7ZAwlWj52MiUTZ2vvendReRroazR3maeXM=";
 
   nativeBuildInputs = [
     pkg-config
@@ -35,32 +35,32 @@ rustPlatform.buildRustPackage (finalAttrs: {
   ];
 
   preBuild = ''
-    cp -r ${finalAttrs.frontend}/lib/node_modules/frontend/dist/templates/html/ templates/html
-    cp -r ${finalAttrs.frontend}/lib/node_modules/frontend/dist/static/ static
+    cp -r ${finalAttrs.passthru.frontend}/lib/node_modules/frontend/dist/templates/html/ templates/html
+    cp -r ${finalAttrs.passthru.frontend}/lib/node_modules/frontend/dist/static/ static
   '';
 
   # tests take long, require the app and a database to be running, and some of them fail
   doCheck = false;
 
-  frontend = buildNpmPackage {
-    pname = "rauthy-frontend";
-    inherit (finalAttrs) version src;
-
-    sourceRoot = "${finalAttrs.src.name}/frontend";
-
-    patches = [
-      # otherwise permission denied error for trying to write outside of the build directory
-      ./0001-build-svelte-files-inside-the-current-directory.patch
-    ];
-
-    patchFlags = [
-      "-p2"
-    ];
-
-    npmDepsHash = "sha256-I5R+9Xb5G8w/rxsuEBJE0DfPi7TXC0o0S0ZbTWJGFNY=";
-  };
-
   passthru = {
+    frontend = buildNpmPackage {
+      pname = "rauthy-frontend";
+      inherit (finalAttrs) version src;
+
+      sourceRoot = "${finalAttrs.src.name}/frontend";
+
+      patches = [
+        # otherwise permission denied error for trying to write outside of the build directory
+        ./0001-build-svelte-files-inside-the-current-directory.patch
+      ];
+
+      patchFlags = [
+        "-p2"
+      ];
+
+      npmDepsHash = "sha256-2pGOR4Tc3MFqdPTFlSv+Yq9fFbpBdYmLADIYy3CS5bU=";
+    };
+
     updateScript = nix-update-script {
       extraArgs = [
         "--subpackage=frontend"
