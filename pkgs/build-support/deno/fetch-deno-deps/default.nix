72b91b01d4cecccb1894625989918a86b84ad76c
aMOPel <momoPFL@gmail.com>
2025-12-09 21:53:25 +0100

    fetchDenoDeps: use lib.extendMkDerivation and improve arg passing to derivations
    remove trace

--- pkgs/build-support/deno/fetch-deno-deps/default.nix
+++ pkgs/build-support/deno/fetch-deno-deps/default.nix
@@ -14,107 +14,143 @@ let
     {
       denoLock,
     }:
-    stdenvNoCC.mkDerivation {
-      name = "transformed-deno-lock";
-      src = null;
-      unpackPhase = "true";
-      buildPhase = ''
-        mkdir -p $out
-        lockfile-transformer \
-          --deno-lock-path ${denoLock} \
-          --common-lock-jsr-path $out/jsr.json \
-          --common-lock-npm-path $out/npm.json \
-          --common-lock-https-path $out/https.json;
-      '';
-      nativeBuildInputs = [
-        fetch-deno-deps-scripts
-      ];
+    lib.extendMkDerivation {
+      constructDrv = stdenvNoCC.mkDerivation;
+      extendDrvArgs =
+        _: args:
+        let
+          pname = args.pname or "";
+          version = args.version or "";
+        in
+        {
+          name = "denoDeps-transformedDenoLock-${pname}-${version}";
+          src = null;
+          unpackPhase = "true";
+          buildPhase = ''
+            mkdir -p $out
+            lockfile-transformer \
+              --deno-lock-path ${denoLock} \
+              --common-lock-jsr-path $out/jsr.json \
+              --common-lock-npm-path $out/npm.json \
+              --common-lock-https-path $out/https.json;
+          '';
+          nativeBuildInputs = [
+            fetch-deno-deps-scripts
+          ];
 
-      meta = {
-        maintainers = [ lib.maintainers.aMOPel ];
-      };
+          meta = {
+            maintainers = [ lib.maintainers.aMOPel ];
+          };
+        }
+        // args;
     };
-
-# TODO: use lib.extendMkDerivation
-# TODO: use make sure to still hash the relevant args, but not the rest
-# TODO: fix usage guide (according to
 
   singleFodFetcher =
     {
       transformedDenoLock,
       denoLock,
       hash,
-      name,
+      fodNameGenerator,
+      enableAutoFodInvalidation,
+      argNamesForFodInvalidation,
+      ...
     }:
-    let
-      args = {
-        src = null;
-        unpackPhase = "true";
-        buildPhase = ''
-          single-fod-fetcher \
-            --common-lock-jsr-path ${transformedDenoLock}/jsr.json \
-            --common-lock-npm-path ${transformedDenoLock}/npm.json \
-            --common-lock-https-path ${transformedDenoLock}/https.json \
-            --out-path-prefix $out;
-          cp ${denoLock} $out/deno.lock
-        '';
-        nativeBuildInputs = [
-          fetch-deno-deps-scripts
-        ];
-      };
-    in
-    stdenvNoCC.mkDerivation (
-      args
-      // {
-        name = "${builtins.hashString "sha1" (builtins.toJSON args)}-${name}";
-        SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    lib.extendMkDerivation {
+      constructDrv = stdenvNoCC.mkDerivation;
+      extendDrvArgs =
+        _: args:
+        let
+          defaultArgs = {
+            src = null;
+            unpackPhase = "true";
+            buildPhase = ''
+              echo hi
+              single-fod-fetcher \
+                --common-lock-jsr-path ${transformedDenoLock}/jsr.json \
+                --common-lock-npm-path ${transformedDenoLock}/npm.json \
+                --common-lock-https-path ${transformedDenoLock}/https.json \
+                --out-path-prefix $out;
+              cp ${denoLock} $out/deno.lock
+            '';
+            nativeBuildInputs = [
+              fetch-deno-deps-scripts
+            ];
+            SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
 
-        outputHashMode = "recursive";
-        outputHash = hash;
-        outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+            outputHash = hash;
+            outputHashAlgo = "sha256";
 
-        meta = {
-          maintainers = [ lib.maintainers.aMOPel ];
-        };
-      }
-    );
+            meta = {
+              maintainers = [ lib.maintainers.aMOPel ];
+            };
+          };
+
+          argsForFodInvalidation = lib.filterAttrs (
+            name: _: builtins.any (otherName: name == otherName) argNamesForFodInvalidation
+          ) mergedArgs;
+
+          mergedArgs = defaultArgs // args;
+
+          pname = args.pname or "";
+          version = args.version or "";
+          name = args.name or "denoDeps-fetched-${pname}-${version}";
+        in
+        (
+          mergedArgs
+          // {
+            name =
+              if enableAutoFodInvalidation then fodNameGenerator argsForFodInvalidation pname version else name;
+          }
+        );
+    };
 
   # this derivation only meant to be used, if the dependency dir is needed outside of buildDenoPackage
-  transformedFiles =
+  # when using buildDenoPackage, the steps of this derivation are performed inside buildDenoPackage,
+  # to avoid an unnecessary derivation in the cache
+  transformFiles =
     {
       fetched,
-      name,
       denoDir,
       vendorDir,
+      ...
     }:
-    stdenvNoCC.mkDerivation {
-      name = "denoDeps-final-${name}";
+    lib.extendMkDerivation {
+      constructDrv = stdenvNoCC.mkDerivation;
+      extendDrvArgs =
+        _: args:
+        let
+          pname = args.pname or "";
+          version = args.version or "";
+        in
+        {
+          name = "denoDeps-final-${pname}-${version}";
+          src = null;
+          unpackPhase = "true";
+          nativeBuildInputs = [
+            fetch-deno-deps-scripts
+            file-structure-transformer-vendor
+          ];
+          buildPhase = ''
+            export vendorDir="$out/${vendorDir}";
+            export DENO_DIR="$out/${denoDir}";
+            mkdir -p $DENO_DIR;
+            mkdir -p $vendorDir;
+            file-structure-transformer-npm \
+              --deno-dir-path $DENO_DIR \
+              --common-lock-npm-path "${fetched}/npm.json";
+            file-structure-transformer-vendor \
+              --deno-dir-path $DENO_DIR \
+              --vendor-dir-path $vendorDir \
+              --common-lock-jsr-path "${fetched}/jsr.json" \
+              --common-lock-https-path "${fetched}/https.json";
+          '';
 
-      src = null;
-      unpackPhase = "true";
-
-      nativeBuildInputs = [
-        fetch-deno-deps-scripts
-        file-structure-transformer-vendor
-      ];
-      buildPhase = ''
-        export vendorDir="$out/${vendorDir}";
-        export DENO_DIR="$out/${denoDir}";
-        mkdir -p $DENO_DIR;
-        mkdir -p $vendorDir;
-        file-structure-transformer-npm \
-          --deno-dir-path $DENO_DIR \
-          --common-lock-npm-path "${fetched}/npm.json";
-        file-structure-transformer-vendor \
-          --deno-dir-path $DENO_DIR \
-          --vendor-dir-path $vendorDir \
-          --common-lock-jsr-path "${fetched}/jsr.json" \
-          --common-lock-https-path "${fetched}/https.json";
-      '';
-
-      meta = {
-        maintainers = [ lib.maintainers.aMOPel ];
-      };
+          meta = {
+            maintainers = [ lib.maintainers.aMOPel ];
+          };
+        }
+        // args;
     };
 
 in
@@ -122,31 +158,63 @@ in
   fetchDenoDeps =
     {
       denoLock,
-      name ? "deno-deps",
       hash ? lib.fakeHash,
       denoDir ? ".deno",
       vendorDir ? "vendor",
+
+      # whether to enable automatic invalidation of the fetcher fod (fixed output derivation)
+      # use together with `fodNameGenerator` and `argNamesForFodInvalidation`
+      # by default all derivation args in `argNamesForFodInvalidation` are used to create a hash,
+      # which becomes part of the name of the fetcher fod, if any of those args change, the fod gets a new name and is invalidated
+      #
+      # we do this to make sure that we become aware of bugs in the fetcher immediately,
+      # e.g. after breaking changes in deno upstream force source changes to the fetcher,
+      # and not after years, when the fods slowly get removed from the nix cache
+      enableAutoFodInvalidation ? true,
+      # function to generate the name of fetcher fod, see `enableAutoFodInvalidation`
+      fodNameGenerator ?
+        argsForFodInvalidation: pname: version:
+        "${builtins.hashString "sha1" (builtins.toJSON  argsForFodInvalidation)}-${pname}-${version}",
+      # invalidate fod, if any of these derivation args change, see `enableAutoFodInvalidation`
+      argNamesForFodInvalidation ? [
+        "src"
+        "unpackPhase"
+        "buildPhase"
+        "nativeBuildInputs"
+      ],
+      # derivation args passed to step 1, the deno lock file transformer step
+      transformDenoLockArgs ? { },
+      # derivation args passed to step 2, the fetcher step
+      fetcherArgs ? { },
+      # derivation args passed to step 3, the file structure transformer
+      transformFileStructureArgs ? { },
+      # derivation args passed to all 3 steps
+      globalArgs ? { },
     }:
     let
-      transformedDenoLock = transformDenoLock { inherit denoLock; };
+
+      transformedDenoLock = transformDenoLock {
+        inherit denoLock;
+      } (globalArgs // transformDenoLockArgs);
 
       fetched = singleFodFetcher {
         inherit
           denoLock
           transformedDenoLock
           hash
-          name
+          fodNameGenerator
+          enableAutoFodInvalidation
+          argNamesForFodInvalidation
           ;
-      };
+      } (globalArgs // fetcherArgs);
 
-      denoDeps = transformedFiles {
+      denoDeps = transformFiles {
         inherit
           fetched
-          name
           denoDir
           vendorDir
           ;
-      };
+      } (globalArgs // transformFileStructureArgs);
     in
     {
       inherit
--- pkgs/test/build-deno-package/fetchDenoDeps-e2e-tests/default.nix
+++ pkgs/test/build-deno-package/fetchDenoDeps-e2e-tests/default.nix
@@ -1,22 +1,18 @@
-{ fetchDenoDeps, fetch-deno-deps-scripts }:
-let
-  # to invalidate tests when source changed
-  fetcherSrc = "${fetch-deno-deps-scripts.deno}";
-in
+{ fetchDenoDeps }:
 {
   fetchDenoDeps-e2e-tests = {
     just-jsr-linux = fetchDenoDeps {
-      name = "test-deno-fetch-${fetcherSrc}";
+      globalArgs.pname = "test-denoDeps-just-jsr-linux";
       denoLock = ./just-jsr/deno.lock;
       hash = "sha256-CV/2kYxYYA1g5/dwSKiC4h1F2+6seTItTLBYQoSt0og=";
     };
     with-https-linux = fetchDenoDeps {
-      name = "test-deno-fetch-${fetcherSrc}";
+      globalArgs.pname = "test-denoDeps-with-https-linux";
       denoLock = ./with-https/deno.lock;
       hash = "sha256-O3P2x8kkD92AdbuRIUNp+OJN54ymRhSS1Xscu8dWIGY=";
     };
     with-https-and-npm-linux = fetchDenoDeps {
-      name = "test-deno-fetch-${fetcherSrc}";
+      globalArgs.pname = "test-denoDeps-with-https-and-npm-linux";
       denoLock = ./with-https-and-npm/deno.lock;
       hash = "sha256-TrYrURHG57nGLTSrTEqOGIkQXcpP81NDrIvGVtgwNv0=";
     };
