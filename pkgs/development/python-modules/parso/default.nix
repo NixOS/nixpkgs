8fc2a9437c949a487bd89b2ff1f617021eba4cd2
Tom Hunze <dev@thunze.de>
2025-12-09 13:13:39 +0100

    python313Packages.parso: 0.8.4 -> 0.8.5
    https://github.com/davidhalter/parso/blob/v0.8.5/CHANGELOG.rst

--- pkgs/development/python-modules/parso/default.nix
+++ pkgs/development/python-modules/parso/default.nix
@@ -2,28 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  pythonAtLeast,
-  pythonOlder,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "parso";
-  version = "0.8.4";
+  version = "0.8.5";
   format = "setuptools";
-  disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-6zp7WCQPuZCZo0VXHe7MD5VA6l9N0v4UwqmdaygauS0=";
+    hash = "sha256-A01zVKmgGL3ONS9IsqikUPBenW7oXbhHZOm2vZba/lo=";
   };
 
   nativeCheckInputs = [ pytestCheckHook ];
-
-  disabledTests = lib.optionals (pythonAtLeast "3.10") [
-    # python changed exception message format in 3.10, 3.10 not yet supported
-    "test_python_exception_matches"
-  ];
 
   meta = with lib; {
     description = "Python Parser";
