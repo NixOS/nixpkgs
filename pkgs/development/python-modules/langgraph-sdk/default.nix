82a22b1ab477df9ff369e9f2f0f2a1542a5758af
Sarah Clark <seclark@nextquestion.net>
2025-12-10 17:14:19 +0000

    python312Packages.langgraph-sdk: 0.2.12 -> 0.2.15
    (cherry picked from commit f37ddcd136946da5fbaf0831e32f63394de7d0e4)

--- pkgs/development/python-modules/langgraph-sdk/default.nix
+++ pkgs/development/python-modules/langgraph-sdk/default.nix
@@ -18,14 +18,14 @@
 
 buildPythonPackage rec {
   pname = "langgraph-sdk";
-  version = "0.2.12";
+  version = "0.2.15";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langgraph";
     tag = "sdk==${version}";
-    hash = "sha256-RD+P34ksvZj8cCv4GpTGB00mZUQK9sgIzzVZoTmDowk=";
+    hash = "sha256-+T7FBmsSAkteabmhDO475qO46bRJwyAammUBwX2Bo/g=";
   };
 
   sourceRoot = "${src.name}/libs/sdk-py";
