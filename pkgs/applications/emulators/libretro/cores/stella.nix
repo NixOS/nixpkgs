c812483a27d6aa5de29fda1d0296fc54f5a64160
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-10 15:36:35 +0000

    libretro.stella: 0-unstable-2025-11-26 -> 0-unstable-2025-12-03

--- pkgs/applications/emulators/libretro/cores/stella.nix
+++ pkgs/applications/emulators/libretro/cores/stella.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "stella";
-  version = "0-unstable-2025-11-26";
+  version = "0-unstable-2025-12-03";
 
   src = fetchFromGitHub {
     owner = "stella-emu";
     repo = "stella";
-    rev = "ec7ad887c777a7924fdc786a9c5901e65d4c6cd0";
-    hash = "sha256-M0DD+xNS+kf9x49YPZHulmYeV36fTh09rsRtQT+/zFY=";
+    rev = "cc938d2194290ce25c8fbd149a864705887b1692";
+    hash = "sha256-v4l7fgXvReMsqB7fbAzkY2vIfTGnMvSbeb8dPaK/KnA=";
   };
 
   makefile = "Makefile";
