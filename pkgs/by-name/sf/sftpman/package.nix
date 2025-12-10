e078296d26bb43e9ef75b7a48ddc470aa7641fca
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-10 15:49:41 +0000

    sftpman: 2.1.0 -> 2.1.1

--- pkgs/by-name/sf/sftpman/package.nix
+++ pkgs/by-name/sf/sftpman/package.nix
@@ -7,7 +7,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sftpman";
-  version = "2.1.0";
+  version = "2.1.1";
 
   passthru.updateScript = nix-update-script { };
 
@@ -15,10 +15,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "spantaleev";
     repo = "sftpman-rs";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-6IhMBnp951mKfG054svFTezf3fpOEMJusRj45qVThmA=";
+    hash = "sha256-spI+MAjBT+FFD7X+G0ea9Me8wf+8Gn3kids+Dt6OO+w=";
   };
 
-  cargoHash = "sha256-TltizTFKrMvHNQcSoow9fuNLy6appYq9Y4LicEQrfRE=";
+  cargoHash = "sha256-fx3uC9M9q0rXPrakZ5NYLNVQzhKZgqdjjZLQ90TNvqQ=";
 
   meta = with lib; {
     homepage = "https://github.com/spantaleev/sftpman-rs";
