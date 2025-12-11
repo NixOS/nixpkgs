64c171affce88932e92910686c3187d8e6793ee2
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-12-11 01:07:22 -0300

    cosmic-launcher: update `updateScript`

--- pkgs/by-name/co/cosmic-launcher/package.nix
+++ pkgs/by-name/co/cosmic-launcher/package.nix
@@ -51,10 +51,9 @@ rustPlatform.buildRustPackage (finalAttrs: {
         cosmic-autologin-noxwayland
         ;
     };
+
     updateScript = nix-update-script {
       extraArgs = [
-        "--version"
-        "unstable"
         "--version-regex"
         "epoch-(.*)"
       ];
