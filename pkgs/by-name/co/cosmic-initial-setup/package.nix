ec8a58373cb19879e1715dbe5bf13bc61b939c96
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-12-11 01:06:57 -0300

    cosmic-initial-setup: update `updateScript`

--- pkgs/by-name/co/cosmic-initial-setup/package.nix
+++ pkgs/by-name/co/cosmic-initial-setup/package.nix
@@ -86,8 +86,6 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
     updateScript = nix-update-script {
       extraArgs = [
-        "--version"
-        "unstable"
         "--version-regex"
         "epoch-(.*)"
       ];
