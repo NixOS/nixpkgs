a368311b06f75948e6b6fbd28be8af2e24a60141
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-11 07:19:11 +0000

    vscode-extensions.james-yu.latex-workshop: 10.11.3 -> 10.12.0

--- pkgs/applications/editors/vscode/extensions/james-yu.latex-workshop/default.nix
+++ pkgs/applications/editors/vscode/extensions/james-yu.latex-workshop/default.nix
@@ -7,8 +7,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "latex-workshop";
     publisher = "James-Yu";
-    version = "10.11.3";
-    hash = "sha256-K3xp9jv8P5+WooqFC0YQrHOwTrXjMF6gihzBMkvxX0E=";
+    version = "10.12.0";
+    hash = "sha256-UrQ7Sp4hklKP+rF8Yke7qay/vSIb+B5mza2fmfcN6l8=";
   };
   meta = {
     changelog = "https://marketplace.visualstudio.com/items/James-Yu.latex-workshop/changelog";
