1c28f3a35630685c14e962a45f125ebe8f98d042
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2025-12-11 13:46:15 -0500

    super-productivity: 16.5.0 -> 16.5.5
    changelog: https://github.com/johannesjo/super-productivity/blob/v16.5.5/CHANGELOG.md
    (cherry picked from commit 1dd3dc4d2ac8dbcd3124bff3dd2ecbad68d459b8)

--- pkgs/by-name/su/super-productivity/package.nix
+++ pkgs/by-name/su/super-productivity/package.nix
@@ -16,13 +16,13 @@ let
 in
 buildNpmPackage rec {
   pname = "super-productivity";
-  version = "16.3.0";
+  version = "16.5.5";
 
   src = fetchFromGitHub {
     owner = "johannesjo";
     repo = "super-productivity";
     tag = "v${version}";
-    hash = "sha256-Im5f3FjXa8i9nbWrhbYXGNAflitjfRGs8lGEQi42+CU=";
+    hash = "sha256-0V68wkkuyOFcv4Wl2Kqk4Soa/nOB7CizelYyI0TKU+8=";
 
     postFetch = ''
       find $out -name package-lock.json -exec ${lib.getExe npm-lockfile-fix} -r {} \;
@@ -65,7 +65,7 @@ buildNpmPackage rec {
       dontInstall = true;
 
       outputHashMode = "recursive";
-      hash = "sha256-uaJ/k4xdxDdakfRfAdM//sFaGYZx4pWiMwZGmrw7X3Y=";
+      hash = "sha256-gsGzwwzt54Ww9CyHaHVAM4v1mHM2vQePw5vM8x1EGao=";
     }
   );
 
