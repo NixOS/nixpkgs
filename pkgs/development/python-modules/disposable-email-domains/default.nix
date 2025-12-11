50ea3e022496c2483170057499c785bc005d01ae
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-11 12:45:02 +0000

    python3Packages.disposable-email-domains: 0.0.151 -> 0.0.152

--- pkgs/development/python-modules/disposable-email-domains/default.nix
+++ pkgs/development/python-modules/disposable-email-domains/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "disposable-email-domains";
-  version = "0.0.151";
+  version = "0.0.152";
   pyproject = true;
 
   # No tags on GitHub
   src = fetchPypi {
     pname = "disposable_email_domains";
     inherit version;
-    hash = "sha256-jXtOepZPvp2jlOWJ1Lp2ltnrWU3qL5jHunPRcPLZc8E=";
+    hash = "sha256-5F3ZC9tZ1cpX6qTvDm74bTEJQRYWEn1+QI0d04Yry8M=";
   };
 
   build-system = [
