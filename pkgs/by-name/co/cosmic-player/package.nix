38cffaf3dead496fa11a3526bbdfc427fea5e77b
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-12-11 01:09:29 -0300

    cosmic-player: update `updateScript`

--- pkgs/by-name/co/cosmic-player/package.nix
+++ pkgs/by-name/co/cosmic-player/package.nix
@@ -77,10 +77,9 @@ rustPlatform.buildRustPackage (finalAttrs: {
         cosmic-autologin-noxwayland
         ;
     };
+
     updateScript = nix-update-script {
       extraArgs = [
-        "--version"
-        "unstable"
         "--version-regex"
         "epoch-(.*)"
       ];
