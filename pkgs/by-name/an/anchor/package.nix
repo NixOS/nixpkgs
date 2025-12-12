9f910c5496174b7f319e805262055b07d49379d7
Venkat-Sundaraneedi <venkat.subrahmanyam.34@gmail.com>
2025-12-12 09:04:20 +0530

    anchor: 0.31.1 -> 0.32.1
    Updated to version 0.32.1.
    Added new dependencies: pkg-config, openssl, systemd.
    Set OPENSSL_NO_VENDOR to use system OpenSSL instead of building from source.

--- pkgs/by-name/an/anchor/package.nix
+++ pkgs/by-name/an/anchor/package.nix
@@ -2,29 +2,33 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  pkg-config,
+  openssl,
+  systemd,
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "anchor";
-  version = "0.31.1";
-
+  version = "0.32.1";
   src = fetchFromGitHub {
     owner = "coral-xyz";
     repo = "anchor";
     tag = "v${version}";
-    hash = "sha256-pvD0v4y7DilqCrhT8iQnAj5kBxGQVqNvObJUBzFLqzA=";
+    hash = "sha256-7YOPbMuhdssROoSG6wvwKaCFRF2ZgRLG7kwHZoY+gys=";
     fetchSubmodules = true;
   };
-
-  cargoHash = "sha256-fjhLA+utQdgR75wg+/N4VwASW6+YBHglRPj14sPHmGA=";
-
+  cargoHash = "sha256-XrVvhJ1lFLBA+DwWgTV34jufrcjszpbCgXpF+TUoEvo=";
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [
+    openssl
+    systemd
+  ];
+  OPENSSL_NO_VENDOR = "1";
   checkFlags = [
     # the following test cases try to access network, skip them
     "--skip=tests::test_check_and_get_full_commit_when_full_commit"
     "--skip=tests::test_check_and_get_full_commit_when_partial_commit"
     "--skip=tests::test_get_anchor_version_from_commit"
   ];
-
   meta = {
     description = "Solana Sealevel Framework";
     homepage = "https://github.com/coral-xyz/anchor";
