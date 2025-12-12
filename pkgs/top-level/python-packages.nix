eb713d9c3d8a35c76d52f0c00f3f02fbad342885
Kerstin Humm <kerstin@erictapen.name>
2025-12-12 17:05:03 +0100

    python3Packages.python-avatars: init at 1.4.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15395,6 +15395,8 @@ self: super: with self; {
 
   python-arango = callPackage ../development/python-modules/python-arango { };
 
+  python-avatars = callPackage ../development/python-modules/python-avatars { };
+
   python-awair = callPackage ../development/python-modules/python-awair { };
 
   python-axolotl = callPackage ../development/python-modules/python-axolotl { };
