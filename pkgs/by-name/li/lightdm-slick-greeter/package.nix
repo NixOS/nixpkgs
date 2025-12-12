d7c26c8062b98c0bce01d879c7d2489ef13b8753
Bobby Rong <rjl931189261@126.com>
2025-12-12 22:48:57 +0800

    lightdm-slick-greeter: 2.2.3 -> 2.2.4
    https://github.com/linuxmint/slick-greeter/compare/2.2.3...2.2.4

--- pkgs/by-name/li/lightdm-slick-greeter/package.nix
+++ pkgs/by-name/li/lightdm-slick-greeter/package.nix
@@ -23,13 +23,13 @@
 
 stdenv.mkDerivation rec {
   pname = "lightdm-slick-greeter";
-  version = "2.2.3";
+  version = "2.2.4";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = "slick-greeter";
     rev = version;
-    hash = "sha256-htyFH1Q8RFyvkW75NMpjajNJDzv/87k/Dr8+R5beT2w=";
+    hash = "sha256-Kq5FcippvMyxxA56uH41UdOCrP2wSuw0Tyv1OFxKD5Q=";
   };
 
   nativeBuildInputs = [
