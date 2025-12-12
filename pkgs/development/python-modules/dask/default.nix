e2fe4ce2a4a2bed88f9c46fddc83e2910977876e
Gaetan Lepage <gaetan@glepage.com>
2025-12-12 21:48:50 +0000

    python3Packages.dask: 2025.11.0 -> 2025.12.0
    Diff: https://github.com/dask/dask/compare/2025.11.0...2025.12.0
    
    Changelog: https://docs.dask.org/en/latest/changelog.html

--- pkgs/development/python-modules/dask/default.nix
+++ pkgs/development/python-modules/dask/default.nix
@@ -32,6 +32,7 @@
   pytest-cov-stub,
   pytest-mock,
   pytest-rerunfailures,
+  pytest-timeout,
   pytest-xdist,
   pytestCheckHook,
   versionCheckHook,
@@ -39,14 +40,14 @@
 
 buildPythonPackage rec {
   pname = "dask";
-  version = "2025.11.0";
+  version = "2025.12.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dask";
     repo = "dask";
     tag = version;
-    hash = "sha256-cU4w4dqJQ3ew+fRyD7Lc4URNfW738kKqls6k6j65pIo=";
+    hash = "sha256-oGBOt2ULLn0Kx1rOVNWaC3l1ECotMC2yNeCHya9Tx+s=";
   };
 
   build-system = [
@@ -94,6 +95,7 @@ buildPythonPackage rec {
     pytest-cov-stub
     pytest-mock
     pytest-rerunfailures
+    pytest-timeout
     pytest-xdist
     pytestCheckHook
     versionCheckHook
