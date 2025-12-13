304c5d48731b314267edf9650b6f4b380e783ef6
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-13 14:49:23 +0000

    python3Packages.atproto: 0.0.63 -> 0.0.65

--- pkgs/development/python-modules/atproto/default.nix
+++ pkgs/development/python-modules/atproto/default.nix
@@ -27,7 +27,7 @@
 
 buildPythonPackage rec {
   pname = "atproto";
-  version = "0.0.63";
+  version = "0.0.65";
   format = "pyproject";
 
   # use GitHub, pypi does not include tests
@@ -35,7 +35,7 @@ buildPythonPackage rec {
     owner = "MarshalX";
     repo = "atproto";
     tag = "v${version}";
-    hash = "sha256-ECqBlvKa0DK96AM0yJNuw2mq0SvZohPHFgxJ2jVHZ5s=";
+    hash = "sha256-0NogKxYO+lCtNhK2ZWwRLQTV7rHU5Oz+lnE4awsoPsM=";
   };
 
   POETRY_DYNAMIC_VERSIONING_BYPASS = version;
