2172cf077c4641889020a4e9746a0254c0e3f6bb
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-13 04:13:11 +0000

    python3Packages.pudb: 2025.1.4 -> 2025.1.5

--- pkgs/development/python-modules/pudb/default.nix
+++ pkgs/development/python-modules/pudb/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "pudb";
-  version = "2025.1.4";
+  version = "2025.1.5";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-dAacEMmdINNsKQb+hV4A1QtmAgE8SIHbN0FuMnh66gY=";
+    hash = "sha256-5t7bgfw8jNzWbPYuhjN8uRNXDrssmUyatSAS0Fnghq0=";
   };
 
   build-system = [ hatchling ];
