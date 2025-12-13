02bfc659994f56dcc59a4aa3ff4503d36111baa3
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-13 10:14:09 +0100

    python313Packages.lastversion: remove disabled

--- pkgs/development/python-modules/lastversion/default.nix
+++ pkgs/development/python-modules/lastversion/default.nix
@@ -2,7 +2,6 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
   # nativeBuildInputs
   setuptools,
   # nativeCheckInputs
@@ -25,8 +24,6 @@ buildPythonPackage rec {
   pname = "lastversion";
   version = "3.5.12";
   pyproject = true;
-
-  disabled = pythonOlder "3.6";
 
   src = fetchFromGitHub {
     owner = "dvershinin";
