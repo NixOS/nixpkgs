2508214c3c4e601a52145503121bd50e1e4eddcf
Tom Hunze <dev@thunze.de>
2025-12-13 17:10:49 +0100

    gnomeExtensions.printers: patch hardcoded binary paths

--- pkgs/desktops/gnome/extensions/extensionOverrides.nix
+++ pkgs/desktops/gnome/extensions/extensionOverrides.nix
@@ -2,6 +2,7 @@
   lib,
   fetchFromGitLab,
   cpio,
+  cups,
   ddcutil,
   easyeffects,
   gjs,
@@ -168,6 +169,14 @@ lib.trivial.pipe super [
       ];
     }
   ))
+
+  (patchExtension "printers@linux-man.org" (old: {
+    patches = [
+      (replaceVars ./extensionOverridesPatches/printers_at_linux-man.org.patch {
+        inherit cups;
+      })
+    ];
+  }))
 
   (patchExtension "system-monitor@gnome-shell-extensions.gcampax.github.com" (old: {
     patches = [
