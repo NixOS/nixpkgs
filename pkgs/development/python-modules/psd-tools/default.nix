de0f0f59dcf4c45f2eb2bd38402f39a68cd7d906
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-13 07:31:48 +0000

    python3Packages.psd-tools: 1.11.0 -> 1.12.1
    (cherry picked from commit f1ff69cc556024548b97564dea9f6ac0f5594134)

--- pkgs/development/python-modules/psd-tools/default.nix
+++ pkgs/development/python-modules/psd-tools/default.nix
@@ -19,7 +19,7 @@
 
 buildPythonPackage rec {
   pname = "psd-tools";
-  version = "1.11.0";
+  version = "1.12.1";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -28,7 +28,7 @@ buildPythonPackage rec {
     owner = "psd-tools";
     repo = "psd-tools";
     tag = "v${version}";
-    hash = "sha256-L38WgBYH5TI/BcSS0i6dMC7729AKOFMj39NAgcVaZlw=";
+    hash = "sha256-FmxxLa9KasDE5hl/Hi6fRMnmUKawpm04fHJf7yXJmSI=";
   };
 
   build-system = [
