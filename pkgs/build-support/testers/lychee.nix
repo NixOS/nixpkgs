6e630090e4866c3bd6b37f8293b82aff80d0a87b
PerchunPak <git@perchun.it>
2025-12-13 15:32:46 +0100

    writeShellApplication: add `runShellCheck` argument

--- pkgs/build-support/testers/lychee.nix
+++ pkgs/build-support/testers/lychee.nix
@@ -58,8 +58,7 @@ let
         online = writeShellApplication {
           name = "run-lychee-online";
           runtimeInputs = [ finalAttrs.passthru.lychee ];
-          # Comment out to run shellcheck:
-          checkPhase = "";
+          runShellCheck = false;
           text = ''
             site=${finalAttrs.site}
             configFile=${finalAttrs.configFile}
