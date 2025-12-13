a39ff94aa2ed429d06a625eb764f8eb91dec990c
Boris Nagaev <bnagaev@gmail.com>
2025-12-13 14:45:54 -0300

    telegram-desktop: 6.3.4 -> 6.3.8
    Changelog: https://github.com/telegramdesktop/tdesktop/releases/tag/v6.3.6
    Changelog: https://github.com/telegramdesktop/tdesktop/releases/tag/v6.3.7
    Changelog: https://github.com/telegramdesktop/tdesktop/releases/tag/v6.3.8

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
@@ -42,14 +42,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "telegram-desktop-unwrapped";
-  version = "6.3.4";
+  version = "6.3.8";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-0d5PFo0tv8yayHqsd44gvodgbyRb1b5IPXtJahkWjHU=";
+    hash = "sha256-TnVZK+rvZ3gEHyhsyzyfxxx0j28Oh5lup/HtnKH1vOI=";
   };
 
   nativeBuildInputs = [
