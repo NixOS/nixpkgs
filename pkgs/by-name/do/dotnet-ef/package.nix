01c113c33fc24bb226b76fbab5cc0154e7e7841c
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-14 07:56:08 +0000

    dotnet-ef: 10.0.0 -> 10.0.1

--- pkgs/by-name/do/dotnet-ef/package.nix
+++ pkgs/by-name/do/dotnet-ef/package.nix
@@ -6,11 +6,11 @@
 
 buildDotnetGlobalTool {
   pname = "dotnet-ef";
-  version = "10.0.0";
+  version = "10.0.1";
 
   dotnet-sdk = dotnetCorePackages.sdk_10_0;
 
-  nugetHash = "sha256-GcWo4MARlrq2sWmjJ+zgaTWpMd4kfyQyTNUj4ufG8qg=";
+  nugetHash = "sha256-n3h2WOfnWbQLL4OUKibgTns8qpqcoAMx4B+f5lSr5Ts=";
 
   meta = {
     description = "Tools to help with design-time development tasks";
