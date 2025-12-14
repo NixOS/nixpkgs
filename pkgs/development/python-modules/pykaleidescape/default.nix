d6f5f05b5edd4a4a5cf530464adc0495864b1d77
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-14 16:59:49 +0000

    python3Packages.pykaleidescape: 2022.2.3 -> 1.1.1

--- pkgs/development/python-modules/pykaleidescape/default.nix
+++ pkgs/development/python-modules/pykaleidescape/default.nix
@@ -13,7 +13,7 @@
 
 buildPythonPackage rec {
   pname = "pykaleidescape";
-  version = "2022.2.3";
+  version = "1.1.1";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "SteveEasley";
     repo = "pykaleidescape";
     tag = "v${version}";
-    hash = "sha256-h5G7wV4Z+sf8Qq4GNFsp8DVDSgQgS0dLGf+DzK/egYM=";
+    hash = "sha256-HhHlgQ94aPac6zRgXBSsHxXuAj2OnGdWkJHMOqTwKcI=";
   };
 
   nativeBuildInputs = [ setuptools ];
