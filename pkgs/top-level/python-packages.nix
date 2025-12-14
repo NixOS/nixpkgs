7d6fa4f69edc5303cb7d967074789fe354b9056c
Justin Restivo <justin@restivo.me>
2025-12-14 17:47:42 -0500

    python3Packges.pyairports: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12879,8 +12879,6 @@ self: super: with self; {
 
   pyairobotrest = callPackage ../development/python-modules/pyairobotrest { };
 
-  pyairports = callPackage ../development/python-modules/pyairports { };
-
   pyairtable = callPackage ../development/python-modules/pyairtable { };
 
   pyairvisual = callPackage ../development/python-modules/pyairvisual { };
