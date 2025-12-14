e26c8447bd38500d6afbc8e3f1a9025edb543deb
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-14 14:49:53 +0000

    python3Packages.aioaudiobookshelf: 0.1.7 -> 0.1.10

--- pkgs/development/python-modules/aioaudiobookshelf/default.nix
+++ pkgs/development/python-modules/aioaudiobookshelf/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "aioaudiobookshelf";
-  version = "0.1.7";
+  version = "0.1.10";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "music-assistant";
     repo = "aioaudiobookshelf";
     tag = version;
-    hash = "sha256-sHRyrh+FwR9Vc9LVOA069iH5Wg56Ye4e9bOxdTR6PPs=";
+    hash = "sha256-fNIxLmvZzFBEnIgo1DYn14hrG4p4lnsWIDRsB0lvdpk=";
   };
 
   build-system = [
