664d606d162b314d70147034b83dda81e5a1c1c6
Justin Restivo <justin@restivo.me>
2025-12-14 18:07:56 -0500

    python3Packages.pylion: update src to use github

--- pkgs/development/python-modules/pylion/default.nix
+++ pkgs/development/python-modules/pylion/default.nix
@@ -1,7 +1,7 @@
 {
   lib,
   buildPythonPackage,
-  fetchFromBitbucket,
+  fetchFromGitHub,
   h5py,
   termcolor,
   pexpect,
@@ -15,7 +15,7 @@ buildPythonPackage {
   version = "0.5.3";
   format = "setuptools";
 
-  src = fetchFromBitbucket {
+  src = fetchFromGitHub {
     owner = "dtrypogeorgos";
     repo = "pylion";
     # Version is set in setup.cfg, but not in a git tag / bitbucket release
