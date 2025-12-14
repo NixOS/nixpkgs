3ffa29ae8aab1656d0112f49076b58ea0955fa43
Issam E. Maghni <issam.e.maghni@mailbox.org>
2025-12-14 18:11:31 -0500

    gdb: rename misnamed hostCpuOnly to targetCpuOnly
    Cross-compiled GDB with hostCpuOnly set to true can debug the target
    architecture, not the host architecture. For example, on my machine,
    pkgsCross.arm-embedded.buildPackages.gdbHostCpuOnly is compiled with
    --host=x86_64-unknown-linux-gnu --target=arm-none-eabi, and is meant to
    debug ARM, even if its name would make you think it's for x86-64.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6107,7 +6107,7 @@ with pkgs;
     enablePythonApi = false;
   };
 
-  gdbHostCpuOnly = gdb.override { hostCpuOnly = true; };
+  gdbTargetCpuOnly = gdb.override { targetCpuOnly = true; };
 
   valgrind-light = (valgrind.override { gdb = null; }).overrideAttrs (oldAttrs: {
     meta = oldAttrs.meta // {
