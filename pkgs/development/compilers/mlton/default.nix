362f9770d9c3338bb9183d5eba2bfa49d336d5bf
Victor Freire <victor@freire.dev.br>
2025-12-15 17:00:34 -0300

    mltonHEAD: 875f7912a0b135a9a7e86a04ecac9cacf0bfe5e5 -> 61baac7108fbd91413f0537b7a42d9a1023455f4

--- pkgs/development/compilers/mlton/default.nix
+++ pkgs/development/compilers/mlton/default.nix
@@ -33,7 +33,9 @@ rec {
   mltonHEAD = callPackage ./from-git-source.nix {
     mltonBootstrap = mlton20210117Binary;
     version = "HEAD";
-    rev = "875f7912a0b135a9a7e86a04ecac9cacf0bfe5e5";
-    sha256 = "sha256-/MIoVqqv8qrJPehU7VRFpXtAAo8UUzE3waEvB7WnS9A=";
+    rev = "61baac7108fbd91413f0537b7a42d9a1023455f4";
+    sha256 = "sha256-nWR7ZaXfKxeXfZ9IHipAQ39ASVtva4BeDHP3Zq8mqPo=";
+    # https://github.com/MLton/mlton/issues/631
+    doCheck = !(stdenv.hostPlatform.isAarch64 && stdenv.hostPlatform.isDarwin);
   };
 }
