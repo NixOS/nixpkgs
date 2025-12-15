3b0ee6d7cea6a4d8494cfd8c749c8549021db2be
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-12-14 20:08:31 -0500

    gpclient: use postPatch to patch files

--- pkgs/by-name/gp/gpclient/package.nix
+++ pkgs/by-name/gp/gpclient/package.nix
@@ -60,11 +60,13 @@ rustPlatform.buildRustPackage {
     gnutls
   ];
 
-  preConfigure = ''
+  postPatch = ''
     substituteInPlace crates/common/src/constants.rs \
       --replace-fail /usr/bin/gpauth ${gpauth}/bin/gpauth
     substituteInPlace crates/openconnect/src/vpn_utils.rs \
       --replace-fail /usr/sbin/vpnc-script ${vpnc-scripts}/bin/vpnc-script
+    substituteInPlace packaging/files/usr/share/applications/gpgui.desktop \
+      --replace-fail /usr/bin/gpclient gpclient
   '';
 
   postInstall = ''
@@ -75,10 +77,5 @@ rustPlatform.buildRustPackage {
   preFixup = ''
     wrapProgram "$out/bin/gpclient" \
       --prefix GIO_EXTRA_MODULES : ${glib-networking}/lib/gio/modules
-  '';
-
-  postFixup = ''
-    substituteInPlace $out/share/applications/gpgui.desktop \
-      --replace-fail /usr/bin/gpclient gpclient
   '';
 }
