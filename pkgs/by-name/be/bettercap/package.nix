9093dfbe485fd18b1beea12cc60b41a5360f20f4
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-15 15:10:32 +0000

    bettercap: 2.41.4 -> 2.41.5
    (cherry picked from commit 45643c136d7b43fc14e003e57554471ed08f1488)

--- pkgs/by-name/be/bettercap/package.nix
+++ pkgs/by-name/be/bettercap/package.nix
@@ -12,16 +12,16 @@
 
 buildGoModule rec {
   pname = "bettercap";
-  version = "2.41.4";
+  version = "2.41.5";
 
   src = fetchFromGitHub {
     owner = "bettercap";
     repo = "bettercap";
     rev = "v${version}";
-    sha256 = "sha256-y23gNqS5f/MP+wyRMxe40I+9RuZGyZEok17LIc9Z8O4=";
+    sha256 = "sha256-mw2Fe/7kSowozUpmXC5tMHZ02bF5+UHmy+lmkJ6SeLM=";
   };
 
-  vendorHash = "sha256-1kgjMPsj8z2Cl0YWe/1zY0Zuiza0X+ZAIgsMqPhCrMw=";
+  vendorHash = "sha256-ssNGy40KMJ9P33uEGyYOer92QRS2T6DQlKaf/3XMFwQ=";
 
   doCheck = false;
 
