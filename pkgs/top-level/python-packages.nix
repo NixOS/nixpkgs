37db893943ff3c21a3f2787284bd5de259cf0e89
Moraxyc <i@qaq.li>
2025-12-15 15:29:38 +0800

    python3Packages.haishoku: init at 1.1.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6670,6 +6670,8 @@ self: super: with self; {
 
   hacking = callPackage ../development/python-modules/hacking { };
 
+  haishoku = callPackage ../development/python-modules/haishoku { };
+
   hakuin = callPackage ../development/python-modules/hakuin { };
 
   halide =
