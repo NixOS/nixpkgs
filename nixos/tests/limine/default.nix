a2a3b9da80a3fb3c8934b9f8c32397f7af00380d
programmerlexi <lexi@reyer.name>
2025-12-16 18:15:17 +0100

    nixos/limine: add uki support

--- nixos/tests/limine/default.nix
+++ nixos/tests/limine/default.nix
@@ -8,4 +8,7 @@
   secureBoot = runTest ./secure-boot.nix;
   specialisations = runTest ./specialisations.nix;
   uefi = runTest ./uefi.nix;
+
+  secureBootWithUki = runTest ./secure-boot-with-uki.nix;
+  specialisationsWithUki = runTest ./specialisations-with-uki.nix;
 }
