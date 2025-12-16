05c578996061b75e38a0f831dabc935317cd8080
Kenichi Kamiya <kachick1@gmail.com>
2025-12-16 17:50:29 +0900

    scraper: modernize
    * Update GitHub repository owner
      1. Web Archive snapshot showing the old URL:
        https://web.archive.org/web/20240625100229/https://github.com/causal-agent/scraper
      2. Web Archive snapshot confirming the transfer:
        https://web.archive.org/web/20240910110851/https://github.com/causal-agent/scraper
      3. Clarification commit in v0.25.0:
        https://github.com/rust-scraper/scraper/commit/29b3d8f9f551ec2181a23a8d7dadd884516b1374
    
    * Prefer `fetchFromGitHub`
      `Cargo.lock` is available in the repository
    
    * Add `updateScript`
    
    * Prefer `finalAttrs`

--- pkgs/by-name/sc/scraper/package.nix
+++ pkgs/by-name/sc/scraper/package.nix
@@ -1,17 +1,20 @@
 {
   lib,
   rustPlatform,
-  fetchCrate,
+  fetchFromGitHub,
   installShellFiles,
+  nix-update-script,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "scraper";
   version = "0.24.0";
 
-  src = fetchCrate {
-    inherit pname version;
-    hash = "sha256-/WdzwqwxTZiWyLV/W0nsMgVJ+o3wJU6u0gOMZva+WSM=";
+  src = fetchFromGitHub {
+    owner = "rust-scraper";
+    repo = "scraper";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-T4a5eRWapDPctF8Nc2kIlgJGTckqlvt2ujARsNZH06k=";
   };
 
   cargoHash = "sha256-0k8tYJbsWRAWn7stsodC8qkzzl3O8AZ1QrQ7i/n2YzY=";
@@ -19,15 +22,19 @@ rustPlatform.buildRustPackage rec {
   nativeBuildInputs = [ installShellFiles ];
 
   postInstall = ''
-    installManPage scraper.1
+    installManPage scraper/scraper.1
   '';
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
 
   meta = {
     description = "Tool to query HTML files with CSS selectors";
     mainProgram = "scraper";
-    homepage = "https://github.com/causal-agent/scraper";
-    changelog = "https://github.com/causal-agent/scraper/releases/tag/v${version}";
+    homepage = "https://github.com/rust-scraper/scraper";
+    changelog = "https://github.com/rust-scraper/scraper/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.isc;
     maintainers = [ ];
   };
-}
+})
