c3ac061787bc4c40707e9c43991098f061767d46
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-16 13:08:30 +0000

    lxmenu-data: 0.1.6 -> 0.1.7

--- pkgs/by-name/lx/lxmenu-data/package.nix
+++ pkgs/by-name/lx/lxmenu-data/package.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "lxmenu-data";
-  version = "0.1.6";
+  version = "0.1.7";
 
   src = fetchFromGitHub {
     owner = "lxde";
     repo = "lxmenu-data";
     tag = finalAttrs.version;
-    hash = "sha256-5QdQ+7nzj7wDrfdt4GT8VW4+sHgZdE7h3cReY2pmcak=";
+    hash = "sha256-JwyJkZIJye8Mtx8yOcpxzOO1Gw0K5ZT00DGgXNqziJQ=";
   };
 
   nativeBuildInputs = [
