5ebe3b2af9b377717996e720349ec1dd494c1e3f
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2025-12-16 13:31:51 +0100

    maintainers: add pfmephisto
    Signed-off-by: Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20368,6 +20368,11 @@
     githubId = 3438604;
     name = "Petter Storvik";
   };
+  pfmephisto = {
+    github = "pfmephisto";
+    githubId = 22501790;
+    name = "Povl Filip Sonne-Frederiksen";
+  };
   phaer = {
     name = "Paul Haerle";
     email = "nix@phaer.org";
