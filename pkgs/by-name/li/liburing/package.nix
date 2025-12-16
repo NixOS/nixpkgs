359262a8f7a8f1385b43275e7e16abe9368895a9
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-16 07:30:53 +0000

    liburing: 2.12 -> 2.13

--- pkgs/by-name/li/liburing/package.nix
+++ pkgs/by-name/li/liburing/package.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "liburing";
-  version = "2.12";
+  version = "2.13";
 
   src = fetchFromGitHub {
     owner = "axboe";
     repo = "liburing";
     tag = "liburing-${version}";
-    hash = "sha256-sEMzkyjrCc49ogfUnzdgNtEXmW0Tz/PUKo99C965428=";
+    hash = "sha256-ZWM+SKeRw5iivyj0mHSxC6yw492N7CThx/pp4FJhkCo=";
   };
 
   separateDebugInfo = true;
