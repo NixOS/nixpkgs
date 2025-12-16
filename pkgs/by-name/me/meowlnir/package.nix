1c278a20ed9a31d4b03be33779cf924bb107bf34
SchweGELBin <abramjannikmichael06@gmail.com>
2025-12-16 16:04:25 +0100

    meowlnir: 25.11 -> 25.12

--- pkgs/by-name/me/meowlnir/package.nix
+++ pkgs/by-name/me/meowlnir/package.nix
@@ -9,19 +9,19 @@
 
 buildGoModule rec {
   pname = "meowlnir";
-  version = "25.11";
-  tag = "v0.2511.0";
+  version = "25.12";
+  tag = "v0.2512.0";
 
   src = fetchFromGitHub {
     owner = "maunium";
     repo = "meowlnir";
     inherit tag;
-    hash = "sha256-a73ZRhvQQPb04kos+Jw2gHgAe23UHBFU97gpsk+Xh08=";
+    hash = "sha256-nmuS04qFzY/9kjvNqgI2TpyAzaGGat9FhS4zRL+QvzY=";
   };
 
   buildInputs = [ olm ];
 
-  vendorHash = "sha256-jcA4Qw0serRixTiaJIQ4cNLLoESVLsWvfj7wjUmsbNs=";
+  vendorHash = "sha256-gZYRcxAf1eYqSRVruqDVIbCkoHWnyIkeGc0pF3eVIBQ=";
 
   doCheck = true;
   doInstallCheck = true;
