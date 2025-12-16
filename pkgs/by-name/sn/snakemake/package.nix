f186e865874a83b882cb20ce8ec61bad70ddcb63
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-16 18:17:34 +0000

    snakemake: 9.14.4 -> 9.14.5

--- pkgs/by-name/sn/snakemake/package.nix
+++ pkgs/by-name/sn/snakemake/package.nix
@@ -10,14 +10,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "snakemake";
-  version = "9.14.4";
+  version = "9.14.5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "snakemake";
     repo = "snakemake";
     tag = "v${version}";
-    hash = "sha256-r8Fz0ZOGZbkQ5x5oarxkxzGMYSuh15N4RYlZZxPwPA0=";
+    hash = "sha256-/OUlUT/5Exe24ZqBq8hKN6w5Tf9GV0JUpS6Gam4znm0=";
   };
 
   postPatch = ''
