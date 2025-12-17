01f42802553885d67ae3a93386eb546dc3164044
klea <klea@iwnp.org>
2025-12-17 20:29:03 +0000

    python3Packages.http2irc: init at 0-unstable-2025-01-05

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2981,6 +2981,8 @@ with pkgs;
 
   html-proofer = callPackage ../tools/misc/html-proofer { };
 
+  http2irc = with python3Packages; toPythonApplication http2irc;
+
   httpie = with python3Packages; toPythonApplication httpie;
 
   hue-plus = libsForQt5.callPackage ../applications/misc/hue-plus { };
