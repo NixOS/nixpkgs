038df396235db5d2c4a59aeb6304b4d1ab8e0185
Coca <coca16622@gmail.com>
2025-12-17 19:57:37 +0000

    space-station-14-launcher: 0.35.0 -> 0.36.1
    Brings in multiple updates to the package build from https://github.com/space-wizards/SS14.Launcher/blob/171b5d1892ae43a825a9311b46a466e44c26b4f5/nix/package.nix

--- pkgs/by-name/sp/space-station-14-launcher/package.nix
+++ pkgs/by-name/sp/space-station-14-launcher/package.nix
@@ -5,7 +5,6 @@
   buildDotnetModule,
   dotnetCorePackages,
   fetchFromGitHub,
-  wrapGAppsHook4,
   iconConvTools,
   copyDesktopItems,
   makeDesktopItem,
@@ -16,37 +15,29 @@
   libXcursor,
   libXext,
   libXrandr,
-  fontconfig,
-  glew,
-  glfw,
-  glibc,
   libGL,
   freetype,
-  openal,
-  fluidsynth,
-  gtk3,
-  pango,
-  atk,
-  cairo,
-  zlib,
   glib,
-  gdk-pixbuf,
   alsa-lib,
   libjack2,
   pipewire,
   libpulseaudio,
-  alsaSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
-  jackSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
-  pipewireSupport ? stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
-  pulseaudioSupport ?
-    config.pulseaudio or stdenv.hostPlatform.isLinux && !stdenv.hostPlatform.isAndroid,
+  at-spi2-atk,
+  at-spi2-core,
+  libxkbcommon,
+  wayland,
+  fontconfig,
+  alsaSupport ? stdenv.hostPlatform.isLinux,
+  jackSupport ? stdenv.hostPlatform.isLinux,
+  pipewireSupport ? stdenv.hostPlatform.isLinux,
+  pulseaudioSupport ? config.pulseaudio or stdenv.hostPlatform.isLinux,
   soundfont-fluid,
 
   # Path to set ROBUST_SOUNDFONT_OVERRIDE to, essentially the default soundfont used.
   soundfont-path ? "${soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2",
 }:
 let
-  version = "0.35.0";
+  version = "0.36.1";
   pname = "space-station-14-launcher";
 in
 buildDotnetModule rec {
@@ -60,7 +51,7 @@ buildDotnetModule rec {
     owner = "space-wizards";
     repo = "SS14.Launcher";
     tag = "v${version}";
-    hash = "sha256-8YDlX5GwL5S/gdjIWOa48sEGA/sMEYZvy2FTWSPO+Ug=";
+    hash = "sha256-6wH2CkTuwy+a3EGpKrdLDsIaQ7oZc2I1OLdmAREMazw=";
     fetchSubmodules = true;
   };
 
@@ -78,14 +69,8 @@ buildDotnetModule rec {
     inherit version;
   };
 
-  # SDK 8.0 required for Robust.LoaderApi
-  dotnet-sdk =
-    with dotnetCorePackages;
-    combinePackages [
-      sdk_9_0
-      sdk_8_0
-    ];
-  dotnet-runtime = dotnetCorePackages.runtime_9_0;
+  dotnet-sdk = dotnetCorePackages.sdk_10_0;
+  dotnet-runtime = dotnetCorePackages.runtime_10_0;
 
   dotnetFlags = [
     "-p:FullRelease=true"
@@ -94,48 +79,14 @@ buildDotnetModule rec {
   ];
 
   nativeBuildInputs = [
-    wrapGAppsHook4
     iconConvTools
     copyDesktopItems
   ];
 
-  LD_LIBRARY_PATH = lib.makeLibraryPath [
-    fontconfig
-    libX11
-    libICE
-    libSM
-    libXi
-    libXcursor
-    libXext
-    libXrandr
-
-    glfw
-    glibc
-    libGL
-    openal
-    freetype
-    fluidsynth
-  ];
-
   runtimeDeps = [
-    # Required by the game.
-    glfw
-    glibc
     libGL
-    openal
     freetype
-    fluidsynth
-
-    # Needed for file dialogs.
-    gtk3
-    pango
-    cairo
-    atk
-    zlib
     glib
-    gdk-pixbuf
-
-    # Avalonia UI dependencies.
     libX11
     libICE
     libSM
@@ -143,10 +94,11 @@ buildDotnetModule rec {
     libXcursor
     libXext
     libXrandr
-    fontconfig
-    glew
-
-    # TODO: Figure out dependencies for CEF support.
+    at-spi2-atk
+    at-spi2-core
+    libxkbcommon
+    wayland
+    fontconfig.lib
   ]
   ++ lib.optional alsaSupport alsa-lib
   ++ lib.optional jackSupport libjack2
@@ -177,12 +129,6 @@ buildDotnetModule rec {
     cp -r SS14.Loader/bin/${buildType}/*/*/* $out/lib/space-station-14-launcher/loader/
 
     icoFileToHiColorTheme SS14.Launcher/Assets/icon.ico space-station-14-launcher $out
-  '';
-
-  dontWrapGApps = true;
-
-  preFixup = ''
-    makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
   '';
 
   meta = {
