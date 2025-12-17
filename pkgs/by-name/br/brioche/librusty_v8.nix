9f9d0e6402d32edf03576267b1607f66fe00b1f1
Gaetan Lepage <gaetan@glepage.com>
2025-12-17 12:58:45 +0000

    brioche: init at 0.1.6

---
+++ pkgs/by-name/br/brioche/librusty_v8.nix
@@ -0,0 +1,13 @@
+# auto-generated file -- DO NOT EDIT!
+{ fetchLibrustyV8 }:
+
+fetchLibrustyV8 {
+  version = "140.2.0";
+  shas = {
+    # NOTE; Follows supported platforms of package (see meta.platforms attribute)!
+    x86_64-linux = "0xgkyjx4vhi001p8iv0wqjpal4nl0vj65fnhp5x8ckss6mhanymg";
+    aarch64-linux = "1gal8sylc0k9x37m4lrlncmcfaxn5bdizgi9rhyfb9da9wi41n9f";
+    x86_64-darwin = "0y117lgc1d90f3z285ljazacscs02mmk9b57h403ilgqfly8dzh7";
+    aarch64-darwin = "106n64mrmwk0jzm8zy891kjjbxrr7yggj7m986pxinf8igvab7br";
+  };
+}
