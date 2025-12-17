NixOS/nixpkgs

commit e9b977bc41dc62f67dc7a10fa53dde68c532e656

linux/common-config: update for 6.18

pkgs/os-specific/linux/kernel/common-config.nix
@@ -550,7 +550,7 @@ let
         DRM_AMD_ISP = whenAtLeast "6.11" yes;
 
         # Enable new firmware (and by extension NVK) for compatible hardware on Nouveau
-        DRM_NOUVEAU_GSP_DEFAULT = whenAtLeast "6.8" yes;
+        DRM_NOUVEAU_GSP_DEFAULT = whenBetween "6.8" "6.18" yes;
 
         # Enable Nouveau shared virtual memory (used by OpenCL)
         DEVICE_PRIVATE = whenHasDevicePrivate yes;
@@ -568,6 +568,11 @@ let
         # Intel GVT-g graphics virtualization supports 64-bit only
         DRM_I915_GVT = yes;
         DRM_I915_GVT_KVMGT = module;
+        # Enable Hyper-V guest stuff
+        HYPERV = lib.mkMerge [
+          (whenOlder "6.18" module)
+          (whenAtLeast "6.18" yes)
+        ];
         # Enable Hyper-V Synthetic DRM Driver
         DRM_HYPERV = whenAtLeast "5.14" module;
         # And disable the legacy framebuffer driver when we have the new one
@@ -683,8 +688,8 @@ let
       EXT2_FS_POSIX_ACL = yes;
       EXT2_FS_SECURITY = yes;
 
-      EXT3_FS_POSIX_ACL = yes;
-      EXT3_FS_SECURITY = yes;
+      EXT3_FS_POSIX_ACL = option yes;
+      EXT3_FS_SECURITY = option yes;
 
       EXT4_FS_POSIX_ACL = yes;
       EXT4_FS_SECURITY = yes;
@@ -1020,7 +1025,7 @@ let
       ZRAM_DEF_COMP_ZSTD = whenAtLeast "5.11" yes;
       ZSWAP = option yes;
       ZSWAP_COMPRESSOR_DEFAULT_ZSTD = whenAtLeast "5.7" (lib.mkOptionDefault yes);
-      ZPOOL = yes;
+      ZPOOL = whenOlder "6.18" yes;
       ZSMALLOC = option yes;
     };
 
@@ -1085,6 +1090,8 @@ let
         HID_BATTERY_STRENGTH = yes;
         # enabled by default in x86_64 but not arm64, so we do that here
         HIDRAW = yes;
+        # 6.18-rc1 fails to link otherwise, at least on aarch64
+        HID_HAPTIC = whenAtLeast "6.18" yes;
 
         # Enable loading HID fixups as eBPF from userspace
         HID_BPF = whenAtLeast "6.3" (whenPlatformHasEBPFJit yes);

commit 409317adb30b74fbf552294eae3760f2bbb85283

linux/common-config: fix for Linux 5.10.y on aarch64
Until 5.15, HYPERV only supports x86, so forcing module broke
configuration of 5.10 on aarch64.  Our kernel configuration script
sets every module it can to "m" anyway, so it was redundant and
breakage-prone to set any value here for pre-6.18.

I've confiremd that HYPERV is still set to "m" on 6.12 after this
change.

Fixes: e9b977bc41dc ("linux/common-config: update for 6.18")

pkgs/os-specific/linux/kernel/common-config.nix
@@ -567,10 +567,7 @@ let
           (stdenv.hostPlatform.system == "x86_64-linux" || stdenv.hostPlatform.system == "aarch64-linux")
           {
             # Enable Hyper-V guest stuff
-            HYPERV = lib.mkMerge [
-              (whenOlder "6.18" module)
-              (whenAtLeast "6.18" yes)
-            ];
+            HYPERV = whenAtLeast "6.18" yes;
             # Enable Hyper-V Synthetic DRM Driver
             DRM_HYPERV = whenAtLeast "5.14" module;
             # And disable the legacy framebuffer driver when we have the new one