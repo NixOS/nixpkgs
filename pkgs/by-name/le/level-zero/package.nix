055eb2f5dac9e3e4b6ae2892f92e1ba57741d462
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-17 21:56:20 +0100

    level-zero: 1.26.1 -> 1.26.2
    Diff: https://github.com/oneapi-src/level-zero/compare/v1.26.1...v1.26.2
    
    Changelog: https://github.com/oneapi-src/level-zero/blob/v1.26.2/CHANGELOG.md

--- pkgs/by-name/le/level-zero/package.nix
+++ pkgs/by-name/le/level-zero/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "level-zero";
-  version = "1.26.1";
+  version = "1.26.2";
 
   src = fetchFromGitHub {
     owner = "oneapi-src";
     repo = "level-zero";
     tag = "v${version}";
-    hash = "sha256-wLxMt55h+j4wzsRWXWI1NP8oi0WpsTLGGIEKADZqOfQ=";
+    hash = "sha256-I1/zhAnfgHF1jN7EhHFWshkk8lZE0rjYQxPwzgq0aF0=";
   };
 
   nativeBuildInputs = [
