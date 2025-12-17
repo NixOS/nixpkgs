NixOS/nixpkgs

commit fca84bde0f7d29bbcce78ac75eb8735d5bc9629e

linux/common-config: Enable coreboot firmware drivers

pkgs/os-specific/linux/kernel/common-config.nix
@@ -1407,6 +1407,16 @@ let
         # Enable generic kernel watch queues
         # See https://docs.kernel.org/core-api/watch_queue.html
         WATCH_QUEUE = yes;
+
+        # Enable coreboot firmware drivers.
+        # While these are called CONFIG_GOOGLE_*, they apply to coreboot systems in general.
+        GOOGLE_FIRMWARE = yes;
+        GOOGLE_CBMEM = whenAtLeast "6.2" module;
+        GOOGLE_COREBOOT_TABLE = module;
+        GOOGLE_MEMCONSOLE = module;
+        GOOGLE_MEMCONSOLE_COREBOOT = module;
+        GOOGLE_FRAMEBUFFER_COREBOOT = module;
+        GOOGLE_VPD = module;
       }
       //
         lib.optionalAttrs

commit 6062ffccd5ca2fb07472841ad27335df503bc9e3

linux/common-config: fix for Linux 6.1.y
6.1.y is old enough that we use simpledrm, but not old enough that
GOOGLE_FRAMEBUFFER_COREBOOT supports simpledrm.  Explicitly setting
GOOGLE_FRAMEBUFFER_COREBOOT to module therefore broke 6.1.y build.

Our kernel configuration system will always answer "m" where possible,
so it's not usually necessary to set options to "module" like this.
It's less fragile to rely on that default where possible.

I've checked that building the kernel configfile now works on 5.10.y,
6.1.y, 6.12.y, and 6.18.y, and checked that all these options still
get set to "m" on at least the latter two.

Fixes: fca84bde0f7d ("linux/common-config: Enable coreboot firmware drivers")

pkgs/os-specific/linux/kernel/common-config.nix
@@ -1411,12 +1411,6 @@ let
         # Enable coreboot firmware drivers.
         # While these are called CONFIG_GOOGLE_*, they apply to coreboot systems in general.
         GOOGLE_FIRMWARE = yes;
-        GOOGLE_CBMEM = whenAtLeast "6.2" module;
-        GOOGLE_COREBOOT_TABLE = module;
-        GOOGLE_MEMCONSOLE = module;
-        GOOGLE_MEMCONSOLE_COREBOOT = module;
-        GOOGLE_FRAMEBUFFER_COREBOOT = module;
-        GOOGLE_VPD = module;
       }
       //
         lib.optionalAttrs