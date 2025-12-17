865f1145dc15a2112fd66a926d6d263eed4aecb6
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-17 04:57:26 +0000

    python3Packages.ical: 12.1.1 -> 12.1.2

--- pkgs/development/python-modules/ical/default.nix
+++ pkgs/development/python-modules/ical/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "ical";
-  version = "12.1.1";
+  version = "12.1.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "allenporter";
     repo = "ical";
     tag = version;
-    hash = "sha256-7nsNI8AU966+j+fpKQbP8Ps0GbFxYHuZAczY7MwdiTw=";
+    hash = "sha256-A3rFZUjUdDkkMhh3ytsBg89o1BtCYWFB0d/usmiuyec=";
   };
 
   build-system = [ setuptools ];
