cc8d16fd2324294fa328b57404237791e7005b6c
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-17 18:23:05 +0000

    python3Packages.altcha: 0.2.0 -> 1.0.0

--- pkgs/development/python-modules/altcha/default.nix
+++ pkgs/development/python-modules/altcha/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "altcha";
-  version = "0.2.0";
+  version = "1.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "altcha-org";
     repo = "altcha-lib-py";
     tag = "v${version}";
-    hash = "sha256-QZSj24dyfdgAeHSCWcmWKr35VWjGM3kQyh9o77bHOgY=";
+    hash = "sha256-N30luHhGFkd+XvVKhVnR6degEf0Nm/K/GEaqoEEuZMU=";
   };
 
   build-system = [ setuptools ];
