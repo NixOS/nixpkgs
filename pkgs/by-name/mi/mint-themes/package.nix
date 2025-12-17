314d3bd0eab5e4d70b26b649009cf8bcdf90ce8d
Bobby Rong <rjl931189261@126.com>
2025-12-17 20:10:06 +0800

    mint-themes: 2.3.4 -> 2.3.5
    https://github.com/linuxmint/mint-themes/compare/2.3.4...2.3.5

--- pkgs/by-name/mi/mint-themes/package.nix
+++ pkgs/by-name/mi/mint-themes/package.nix
@@ -8,13 +8,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "mint-themes";
-  version = "2.3.4";
+  version = "2.3.5";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = "mint-themes";
     rev = version;
-    hash = "sha256-A4k9iy0d9pGPSYa4m3tAv2GKhFuJPrQODbv+LcP/YXU=";
+    hash = "sha256-folnnA4By1Dd1amriGiTl5mOxpcnaFjdp/UsjacE8GA=";
   };
 
   nativeBuildInputs = [
