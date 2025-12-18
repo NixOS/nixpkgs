9385196f119c0dbcb4852b35313a01cdd4e0a2b7
Doron Behar <doron.behar@gmail.com>
2025-12-18 19:08:30 +0200

    xsimd: 13.2.0 -> 14.0.0
    Diff: https://github.com/xtensor-stack/xsimd/compare/13.2.0...14.0.0
    
    Changelog: https://github.com/xtensor-stack/xsimd/blob/14.0.0/Changelog.rst#1400

--- pkgs/by-name/xs/xsimd/package.nix
+++ pkgs/by-name/xs/xsimd/package.nix
@@ -8,21 +8,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xsimd";
-  version = "13.2.0";
+  version = "14.0.0";
 
   src = fetchFromGitHub {
     owner = "xtensor-stack";
     repo = "xsimd";
     tag = finalAttrs.version;
-    hash = "sha256-L4ttJxP46uNwQAEUMoJ8rsc51Le2GeIGbT1kX7ZzcPA=";
+    hash = "sha256-ijNoHb6xC+OHJbUB4j1PRsoHMzjrnOHVoDRe/nKguDo=";
   };
 
   patches = lib.optionals stdenv.hostPlatform.isDarwin [
     # https://github.com/xtensor-stack/xsimd/issues/1030
     ./disable-test_error_gamma.patch
-
-    # https://github.com/xtensor-stack/xsimd/issues/1063
-    ./relax-asin-precision.diff
   ];
 
   # strictDeps raises the chance that xsimd will be able to be cross compiled
