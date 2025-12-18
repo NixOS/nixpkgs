e44f0f70ba3728bd6880f71daa82530a36859024
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-12-18 00:39:53 -0500

    ramalama: wrap all deps when withPodman==false, except podman
    Before the change, ramalama erroneously excluded all deps from
    wrapProgram if withPodman was false. Now only podman inclusion in the
    closure is controlled by withPodman argument; other deps are always
    included.

--- pkgs/by-name/ra/ramalama/package.nix
+++ pkgs/by-name/ra/ramalama/package.nix
@@ -48,24 +48,24 @@ python3Packages.buildPythonApplication rec {
     make docs
   '';
 
-  postInstall = lib.optionalString withPodman ''
-    wrapProgram $out/bin/ramalama \
-      --prefix PATH : ${
-        lib.makeBinPath (
-          [
-            llama-cpp-vulkan
-            podman
-          ]
-          ++ (
-            with python3Packages;
-            [
-              huggingface-hub
-            ]
-            ++ lib.optional (lib.meta.availableOn stdenv.hostPlatform mlx-lm) mlx-lm
-          )
-        )
-      }
-  '';
+  postInstall =
+    let
+      binPackages = [
+        llama-cpp-vulkan
+      ]
+      ++ (
+        with python3Packages;
+        [
+          huggingface-hub
+        ]
+        ++ lib.optional (lib.meta.availableOn stdenv.hostPlatform mlx-lm) mlx-lm
+      )
+      ++ lib.optional withPodman podman;
+    in
+    ''
+      wrapProgram $out/bin/ramalama \
+        --prefix PATH : ${lib.makeBinPath binPackages}
+    '';
 
   pythonImportsCheck = [
     "ramalama"
