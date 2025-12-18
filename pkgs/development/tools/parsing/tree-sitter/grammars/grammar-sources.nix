ce5c22f00dc490198732d1884a6b7a0d57264a51
kb <kim@simple.industries>
2025-12-19 01:16:09 +1000

    tree-sitter-grammars.tree-sitter-nickel: 0.3.0 -> 0.5.0

--- pkgs/development/tools/parsing/tree-sitter/grammars/grammar-sources.nix
+++ pkgs/development/tools/parsing/tree-sitter/grammars/grammar-sources.nix
@@ -419,9 +419,9 @@
   };
 
   nickel = {
-    version = "0.3.0";
-    url = "github:nickel-lang/tree-sitter-nickel?ref=0.3";
-    hash = "sha256-jL054OJj+1eXksNYOTTTFzZjwPqTFp06syC3TInN8rc=";
+    version = "0.5.0";
+    url = "github:nickel-lang/tree-sitter-nickel";
+    hash = "sha256-2la/9XxL2dN+rzTotgDXQFz9ktDXQ3Og9svX5Din2zo=";
   };
 
   nix = {
