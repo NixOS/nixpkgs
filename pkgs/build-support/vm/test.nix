2fec9598a863afdd6b56049d69235620fac2796d
Philip Taron <philip.taron@gmail.com>
2025-12-17 21:33:21 -0800

    vmTools: shore up the tests with each image

--- pkgs/build-support/vm/test.nix
+++ pkgs/build-support/vm/test.nix
@@ -46,6 +46,7 @@ in
     })
   );
 
+  # RPM-based distros
   testFedora42Image = makeImageTestScript diskImages.fedora42x86_64;
   testFedora43Image = makeImageTestScript diskImages.fedora43x86_64;
   testCentOSStream9Image = makeImageTestScript diskImages.centosStream9x86_64;
@@ -56,5 +57,15 @@ in
   testAlma10Image = makeImageTestScript diskImages.alma10x86_64;
   testOracle9Image = makeImageTestScript diskImages.oracle9x86_64;
   testAmazon2023Image = makeImageTestScript diskImages.amazon2023x86_64;
-  testUbuntuImage = makeImageTestScript diskImages.ubuntu2404x86_64;
+
+  # Debian-based distros
+  testDebian11i386Image = makeImageTestScript diskImages.debian11i386;
+  testDebian11x86_64Image = makeImageTestScript diskImages.debian11x86_64;
+  testDebian12i386Image = makeImageTestScript diskImages.debian12i386;
+  testDebian12x86_64Image = makeImageTestScript diskImages.debian12x86_64;
+  testDebian13i386Image = makeImageTestScript diskImages.debian13i386;
+  testDebian13x86_64Image = makeImageTestScript diskImages.debian13x86_64;
+  testUbuntu2204i386Image = makeImageTestScript diskImages.ubuntu2204i386;
+  testUbuntu2204x86_64Image = makeImageTestScript diskImages.ubuntu2204x86_64;
+  testUbuntu2404x86_64Image = makeImageTestScript diskImages.ubuntu2404x86_64;
 }
