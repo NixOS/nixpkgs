6e4ea1fdbb2adc2bf86995ce5263797914e53e62
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-18 10:07:44 +0000

    spicetify-cli: 2.42.4 -> 2.42.6

--- pkgs/by-name/sp/spicetify-cli/package.nix
+++ pkgs/by-name/sp/spicetify-cli/package.nix
@@ -7,16 +7,16 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "spicetify-cli";
-  version = "2.42.4";
+  version = "2.42.6";
 
   src = fetchFromGitHub {
     owner = "spicetify";
     repo = "cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-JOAw2GEHGr7eIIZtoAeKFqde1L7TidbyvRv1kAcUm2w=";
+    hash = "sha256-IGbe9kD04Dy70sejn84tEz+WB0oFhviZA9WZXlZlxkU=";
   };
 
-  vendorHash = "sha256-DiVu/ePiZvn9+B/r8LS0qLt8eXKAtg4IXZ1WRzzAvcE=";
+  vendorHash = "sha256-7Dk4tTyVgnNgDECYj24Uy4xavWkVVxWE8uZlkKsFobI=";
 
   postPatch = ''
     substituteInPlace src/preprocess/preprocess.go \
