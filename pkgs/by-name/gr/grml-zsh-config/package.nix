0ed562008c5b4afa4853bfd39c88e0a7bd24fdf9
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-18 11:28:26 +0000

    grml-zsh-config: 0.19.25 -> 0.19.26

--- pkgs/by-name/gr/grml-zsh-config/package.nix
+++ pkgs/by-name/gr/grml-zsh-config/package.nix
@@ -7,13 +7,13 @@
 }:
 stdenv.mkDerivation rec {
   pname = "grml-zsh-config";
-  version = "0.19.25";
+  version = "0.19.26";
 
   src = fetchFromGitHub {
     owner = "grml";
     repo = "grml-etc-core";
     rev = "v${version}";
-    sha256 = "sha256-jUdgigqK6j7Tn/Gl737iNSitUyp7uWrXfRyCBxUSD/0=";
+    sha256 = "sha256-WZ3eyX9ijsU221I4hEhWUtFmtBQ9jm1QvQZh20gcR/s=";
   };
 
   strictDeps = true;
