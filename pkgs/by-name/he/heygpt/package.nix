bb0bdd44cbbef3f3387d314a0952b932ea1500d2
Stefan Frijters <sfrijters@gmail.com>
2025-12-19 23:18:51 +0100

    heygpt: prepare for structuredAttrs by moving OPENSSL_NO_VENDOR into env

--- pkgs/by-name/he/heygpt/package.nix
+++ pkgs/by-name/he/heygpt/package.nix
@@ -19,10 +19,12 @@ rustPlatform.buildRustPackage rec {
 
   nativeBuildInputs = [ openssl ];
 
-  # Needed to get openssl-sys to use pkg-config.
-  OPENSSL_NO_VENDOR = 1;
-  OPENSSL_LIB_DIR = "${lib.getLib openssl}/lib";
-  OPENSSL_DIR = "${lib.getDev openssl}";
+  env = {
+    # Needed to get openssl-sys to use pkg-config.
+    OPENSSL_NO_VENDOR = 1;
+    OPENSSL_LIB_DIR = "${lib.getLib openssl}/lib";
+    OPENSSL_DIR = "${lib.getDev openssl}";
+  };
 
   meta = {
     description = "Simple command-line interface for ChatGPT API";
