0c4f80ba4b57dafe4f45d43334301481f0f945e4
criyle <git@goj.ac>
2025-12-19 21:04:19 +0000

    go-judge: 1.9.10 -> 1.11.0

--- pkgs/by-name/go/go-judge/package.nix
+++ pkgs/by-name/go/go-judge/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "go-judge";
-  version = "1.9.10";
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "criyle";
     repo = "go-judge";
     rev = "v${version}";
-    hash = "sha256-do+CvDEyYt9Yxrpkvyk6TWYt3TMjIcxkBQ8qf4rzT/E=";
+    hash = "sha256-9D//GOcnME4VW4cYKhJnMNdZOoeXrzz7uEOn8ZfujPY=";
   };
 
-  vendorHash = "sha256-XigQHewE0ehdMuYEktPSHcAZ3G1QxjoHVG616FZ9KZw=";
+  vendorHash = "sha256-pz/S2YIlPeGEkx+epQlDnFAUQdKET7r5c//Cu70V0Bo=";
 
   tags = [
     "nomsgpack"
