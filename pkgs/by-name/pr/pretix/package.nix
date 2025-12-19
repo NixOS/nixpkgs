00a2cb373bcc37b1086d4e5a7d8c957f4eff57d3
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-12-19 16:25:07 +0100

    pretix: 2025.10.0 -> 2025.10.1
    https://pretix.eu/about/en/blog/20251219-release-2025-10-1/
    
    Fixes: CVE-2025-14881

--- pkgs/by-name/pr/pretix/package.nix
+++ pkgs/by-name/pr/pretix/package.nix
@@ -42,13 +42,13 @@ let
   };
 
   pname = "pretix";
-  version = "2025.10.0";
+  version = "2025.10.1";
 
   src = fetchFromGitHub {
     owner = "pretix";
     repo = "pretix";
     rev = "refs/tags/v${version}";
-    hash = "sha256-q8h7wYUv5ahWvM3sT9srVHnJv5QnakkUKYhgx1X/j5k=";
+    hash = "sha256-O9HAslZ8xbmLgJi3y91M6mc1oIvJZ8nRJyFRuNorRHs=";
   };
 
   npmDeps = buildNpmPackage {
