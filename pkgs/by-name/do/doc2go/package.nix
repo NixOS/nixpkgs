2090c86a9a6cbe6b5bebe371c1e7dbd9763c065b
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-19 09:53:14 +0000

    doc2go: 0.9.1 -> 0.12.0

--- pkgs/by-name/do/doc2go/package.nix
+++ pkgs/by-name/do/doc2go/package.nix
@@ -6,15 +6,15 @@
 
 buildGoModule rec {
   pname = "doc2go";
-  version = "0.9.1";
+  version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "abhinav";
     repo = "doc2go";
     rev = "v${version}";
-    hash = "sha256-vxGzDHTtA6GgyAq1bcXL1Jrn4H6ug/ZeHl+aWezOYGo=";
+    hash = "sha256-gJQ0cs8R1fbGpFd9k2L53khnQ9M5U18DOtS2bn0lGig=";
   };
-  vendorHash = "sha256-GuBjImliR3iOthOL1/4AtH2ldf5AecYXPoexHGxm4zs=";
+  vendorHash = "sha256-lMoEbNeSQFrMG8hCMaFRvyzt6kaPVP7Kse0i7NJY10I=";
 
   ldflags = [
     "-s"
