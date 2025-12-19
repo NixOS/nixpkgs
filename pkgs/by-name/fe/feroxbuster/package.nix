abea1a7a1c478e45fa3d4bd4f9a7b6662a685bf4
Stefan Frijters <sfrijters@gmail.com>
2025-12-19 23:18:49 +0100

    feroxbuster: prepare for structuredAttrs by moving OPENSSL_NO_VENDOR into env

--- pkgs/by-name/fe/feroxbuster/package.nix
+++ pkgs/by-name/fe/feroxbuster/package.nix
@@ -22,7 +22,7 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-kWRODW1BsnifEqGZj8jK5tUK/5zK1AIRSq3JSo6YmkI=";
 
-  OPENSSL_NO_VENDOR = true;
+  env.OPENSSL_NO_VENDOR = true;
 
   nativeBuildInputs = [
     pkg-config
