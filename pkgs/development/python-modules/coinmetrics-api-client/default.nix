8d53573fe352a83e972da3240f1f802e8fd79a24
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-19 08:38:37 +0000

    python3Packages.coinmetrics-api-client: 2025.12.12.16 -> 2025.12.16.20

--- pkgs/development/python-modules/coinmetrics-api-client/default.nix
+++ pkgs/development/python-modules/coinmetrics-api-client/default.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "coinmetrics-api-client";
-  version = "2025.12.12.16";
+  version = "2025.12.16.20";
   pyproject = true;
 
   __darwinAllowLocalNetworking = true;
@@ -26,7 +26,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     inherit version;
     pname = "coinmetrics_api_client";
-    hash = "sha256-IegTRV4c/P63FUR7lPqPOVt1VtKElIgzuYW8cisNFYg=";
+    hash = "sha256-FnoaLUU+x6uOlGdFhx02z/rlpqrHHt7T8TTPmeE0I68=";
   };
 
   pythonRelaxDeps = [ "typer" ];
