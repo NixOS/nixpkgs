013b4a6d0e3d7397f2c03ef498e7b81fb4598bab
Leona Maroni <dev@leona.is>
2025-12-20 15:35:48 +0100

    hplip: fix build with gcc15

--- pkgs/by-name/hp/hplip/package.nix
+++ pkgs/by-name/hp/hplip/package.nix
@@ -144,6 +144,8 @@ python3Packages.buildPythonApplication {
     # don't on NixOS).  Add the equivalent NixOS path, /var/lib/cups/path/share.
     # See: https://github.com/NixOS/nixpkgs/issues/21796
     ./hplip-3.20.11-nixos-cups-ppd-search-path.patch
+    # https://bugs.launchpad.net/hplip/+bug/2096650
+    ./gcc-compatability.patch
 
     # Remove all ImageProcessor functionality since that is closed source
     (fetchurl {
