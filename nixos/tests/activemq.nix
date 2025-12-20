8f07183d44511750457fac8b5e5b1e9f76b33a1f
Anthony Roussel <anthony@roussel.dev>
2025-12-20 23:23:59 +0100

    nixosTests.activemq: init

---
+++ nixos/tests/activemq.nix
@@ -0,0 +1,29 @@
+{ lib, ... }:
+
+{
+  name = "activemq";
+  meta.maintainers = [ lib.maintainers.anthonyroussel ];
+
+  nodes.machine =
+    { pkgs, ... }:
+    {
+      services.activemq = {
+        enable = true;
+        package = pkgs.activemq;
+      };
+    };
+
+  testScript = ''
+    machine.wait_for_unit("activemq.service")
+
+    with subtest("Check if ports are open"):
+      machine.wait_for_open_port(8161) # http api
+      machine.wait_for_open_port(1883) # mqtt
+      machine.wait_for_open_port(5672) # amqp
+      machine.wait_for_open_port(61613) # stomp
+      machine.wait_for_open_port(61616) # openwire
+
+    with subtest("Check web console"):
+      machine.succeed("curl -sS --fail -u admin:admin http://127.0.0.1:8161/admin/")
+  '';
+}
