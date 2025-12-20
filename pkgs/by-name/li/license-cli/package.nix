642a3a09f6f0cb3faf06cb15aa5d50b1e8794823
Acid Bong <acidbong@tilde.club>
2025-12-20 16:40:04 +0000

    license-cli: 3.1.0 -> 3.2.1

--- pkgs/by-name/li/license-cli/package.nix
+++ pkgs/by-name/li/license-cli/package.nix
@@ -15,16 +15,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "license-cli";
-  version = "3.1.0";
+  version = "3.2.1";
 
   src = fetchFromSourcehut {
     owner = "~zethra";
     repo = "license";
     rev = version;
-    hash = "sha256-OGS26mE5rjxlZOaBWhYc7C8aM3Lq2xX0f31LgckjJF8=";
+    hash = "sha256-eS4KuoUJA6e+Y6WNFCJTXgjV5t3Eh7wc2KvWi/+jCeI=";
   };
 
-  cargoHash = "sha256-Jvg3XndPyQ9TYejJaO7GAI9RwLAOWB0uapA+6WIKAkI=";
+  cargoHash = "sha256-7C/KAMBXbkxsjnkIJsGBOasOGGIXV8QhVEkkP+vseos=";
 
   nativeBuildInputs = [
     installShellFiles
