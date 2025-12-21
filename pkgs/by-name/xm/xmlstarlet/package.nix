12ab1cb5bf800bd4309bb7d14bcac9dbbb21e774
Jasi <jawsi@posteo.net>
2025-12-20 21:33:20 -0500

    xmlstarlet: simplify derivation builder
    - Use finalAttrs pattern
    - Use fetchzip instead of fetchurl, update FOD
    - Remove uses of fetchpatch and vendor patches within `postPatch`
    - Simplify postInstall, change `meta.mainProgram` to "xml" instead of
    "xmlstarlet" to match documentation.

--- pkgs/by-name/xm/xmlstarlet/package.nix
+++ pkgs/by-name/xm/xmlstarlet/package.nix
@@ -1,22 +1,30 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  fetchpatch,
+  fetchzip,
   autoreconfHook,
   pkg-config,
   libxml2,
   libxslt,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xmlstarlet";
   version = "1.6.1";
 
-  src = fetchurl {
-    url = "mirror://sourceforge/xmlstar/xmlstarlet-${version}.tar.gz";
-    sha256 = "1jp737nvfcf6wyb54fla868yrr39kcbijijmjpyk4lrpyg23in0m";
+  src = fetchzip {
+    url = "mirror://sourceforge/xmlstar/xmlstarlet-${finalAttrs.version}.tar.gz";
+    hash = "sha256-lJj9ON2ycLHKUm32rOYVJNyNNnogrWBqAJp0Y+twHVo=";
   };
+
+  # libxml 2.14 and later stopped defining ATTRIBUTE_UNUSED macro in
+  # the header file so it must be redefined here to avoid a
+  # compilation error.
+  # See: https://gitlab.gnome.org/GNOME/libxml2/-/commit/208f27f9641a59863ce1f7d4992df77f7eb0ea9d
+  postPatch = ''
+    sed -i '1s/^/\#define ATTRIBUTE_UNUSED __attribute__((unused))\n/' \
+        src/xmlstar.h
+  '';
 
   nativeBuildInputs = [
     autoreconfHook
@@ -27,36 +35,19 @@ stdenv.mkDerivation rec {
     libxslt
   ];
 
-  patches = [
-    (fetchpatch {
-      name = "0001-Fix-build-with-libxml2-2.12.patch";
-      url = "https://sourceforge.net/p/xmlstar/patches/_discuss/thread/890e29655a/66ca/attachment/0001-Fix-build-with-libxml2-2.12.patch";
-      hash = "sha256-XEk7aFOdrzdec1j2ffERJQbLH0AUNJA52QwA9jf4XWA=";
-    })
-    (fetchurl {
-      name = "libxml-2.14.patch";
-      url = "https://github.com/termux/termux-packages/raw/39135f3f1190268d127b998c2c6040d9af611ba5/packages/xmlstarlet/libxml2-2.14-attribute-unused.patch";
-      hash = "sha256-zHkUQsrhPLWI3kdfCITbcixpBmDRmxSM2Viz5R+8q5E=";
-    })
-  ];
-
-  preConfigure = ''
-    export LIBXSLT_PREFIX=${libxslt.dev}
-    export LIBXML_PREFIX=${libxml2.dev}
-    export LIBXSLT_LIBS=$($PKG_CONFIG --libs libxslt libexslt)
-    export LIBXML_LIBS=$($PKG_CONFIG --libs libxml-2.0)
-  '';
-
+  # Even though only the 'xml' binary is produced, many references
+  # show 'xmlstarlet' as the binary name so it has to be matched here
+  # for convenience sake.
   postInstall = ''
-    ln -s xml $out/bin/xmlstarlet
+    ln -s $out/bin/xml $out/bin/xmlstarlet
   '';
 
   meta = {
     description = "Command line tool for manipulating and querying XML data";
     homepage = "https://xmlstar.sourceforge.net/";
     license = lib.licenses.mit;
-    mainProgram = "xmlstarlet";
+    mainProgram = "xml";
     platforms = lib.platforms.unix;
     maintainers = with lib.maintainers; [ normalcea ];
   };
-}
+})
