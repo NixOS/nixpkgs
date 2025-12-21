90a39aefb7198a8a0d622f7d7df14d4b2f500504
Martin Joerg <martin.joerg@gmail.com>
2025-12-21 10:03:28 +0000

    zxing: correctly reference scripts with absolute paths
    This is necessary for zxing to work properly when it is not in PATH, such as when using nix run,
    and ensures that the correct version is used.

--- pkgs/by-name/zx/zxing/zxing.sh
+++ pkgs/by-name/zx/zxing/zxing.sh
@@ -3,16 +3,16 @@ choice="$1";
 shift
 case "$choice" in
     encode | create | write | CommandLineEncoder)
-        zxing-cmdline-encoder "$@";
+        @out@/bin/zxing-cmdline-encoder "$@";
         ;;
     decode | read | run | CommandLineRunner)
-        zxing-cmdline-runner "$@";
+        @out@/bin/zxing-cmdline-runner "$@";
         ;;
     help | usage | --help | --usage | -h)
-        zxing read --help;
-        zxing write --help;
+        @out@/bin/zxing read --help;
+        @out@/bin/zxing write --help;
         ;;
     *)
-        zxing read "$choice" "$@"
+        @out@/bin/zxing read "$choice" "$@";
         ;;
 esac
