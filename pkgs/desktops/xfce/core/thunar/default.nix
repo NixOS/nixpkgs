NixOS/nixpkgs

commit efbd153c406e00dd3f300e2a91c686509c6896d5

xfce.{{libxfce4{util,ui,windowing},xfconf,xfce4-panel,garcon,thunar}}: Conditionalize gobject-introspection and vala
gobject-introspection doesn't support static

Although the dependencies for these don't build on static this can still
be useful in the future or for preventing additional issues being created
for xfce packages.

Tested on native by changing the conditionals to false.

pkgs/desktops/xfce/core/thunar/default.nix
@@ -1,4 +1,5 @@
-{ mkXfceDerivation
+{ stdenv
+, mkXfceDerivation
 , lib
 , docbook_xsl
 , exo
@@ -14,10 +15,12 @@
 , pcre2
 , xfce4-panel
 , xfconf
-, gobject-introspection
 , makeWrapper
 , symlinkJoin
 , thunarPlugins ? []
+, withIntrospection ? false
+, buildPackages
+, gobject-introspection
 }:
 
 let unwrapped = mkXfceDerivation {
@@ -29,8 +32,9 @@ let unwrapped = mkXfceDerivation {
 
   nativeBuildInputs = [
     docbook_xsl
-    gobject-introspection
     libxslt
+  ] ++ lib.optionals withIntrospection [
+    gobject-introspection
   ];
 
   buildInputs = [

pkgs/desktops/xfce/core/garcon/default.nix
@@ -1,4 +1,10 @@
-{ lib, mkXfceDerivation, gobject-introspection, gtk3, libxfce4ui, libxfce4util }:
+{ stdenv, lib, mkXfceDerivation, gtk3, libxfce4ui, libxfce4util,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
+  buildPackages,
+  gobject-introspection,
+}:
 
 mkXfceDerivation {
   category = "xfce";
@@ -7,7 +13,9 @@ mkXfceDerivation {
 
   sha256 = "sha256-MeZkDb2QgGMaloO6Nwlj9JmZByepd6ERqpAWqrVv1xw=";
 
-  nativeBuildInputs = [ gobject-introspection ];
+  nativeBuildInputs = lib.optionals withIntrospection [
+    gobject-introspection
+  ];
 
   buildInputs = [ gtk3 libxfce4ui libxfce4util ];
 

pkgs/desktops/xfce/core/libxfce4ui/default.nix
@@ -1,9 +1,8 @@
 {
+  stdenv,
   mkXfceDerivation,
   lib,
-  gobject-introspection,
   perl,
-  vala,
   libICE,
   libSM,
   libepoxy,
@@ -13,6 +12,12 @@
   xfconf,
   gtk3,
   libxfce4util,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
+  buildPackages,
+  gobject-introspection,
+  vala,
 }:
 
 mkXfceDerivation {
@@ -22,11 +27,14 @@ mkXfceDerivation {
 
   sha256 = "sha256-M+OapPHQ/WxlkUzHPx+ELstVyGoZanCxCL0N8hDWSN8=";
 
-  nativeBuildInputs = [
-    gobject-introspection
-    perl
-    vala
-  ];
+  nativeBuildInputs =
+    [
+      perl
+    ]
+    ++ lib.optionals withIntrospection [
+      gobject-introspection
+      vala # vala bindings require GObject introspection
+    ];
 
   buildInputs = [
     libICE

pkgs/desktops/xfce/core/libxfce4util/default.nix
@@ -1,9 +1,14 @@
 {
+  stdenv,
   mkXfceDerivation,
   lib,
-  gobject-introspection,
   vala,
   glib,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
+  buildPackages,
+  gobject-introspection,
 }:
 
 mkXfceDerivation {
@@ -13,9 +18,9 @@ mkXfceDerivation {
 
   sha256 = "sha256-0qbJSCXHsVz3XILHICFhciyz92LgMZiR7XFLAESHRGQ=";
 
-  nativeBuildInputs = [
+  nativeBuildInputs = lib.optionals withIntrospection [
     gobject-introspection
-    vala
+    vala # vala bindings require GObject introspection
   ];
 
   propagatedBuildInputs = [

pkgs/desktops/xfce/core/libxfce4windowing/default.nix
@@ -1,6 +1,6 @@
-{ lib
+{ stdenv
+, lib
 , mkXfceDerivation
-, gobject-introspection
 , wayland-scanner
 , glib
 , gtk3
@@ -11,6 +11,11 @@
 , wayland
 , wayland-protocols
 , wlr-protocols
+, withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages
+, buildPackages
+, gobject-introspection
 }:
 
 mkXfceDerivation {
@@ -21,8 +26,9 @@ mkXfceDerivation {
   sha256 = "sha256-Xw1hs854K5dZCAYoBMoqJzdSxPRFUYqEpWxg4DLSK5Q=";
 
   nativeBuildInputs = [
-    gobject-introspection
     wayland-scanner
+  ] ++ lib.optionals withIntrospection [
+    gobject-introspection
   ];
 
   buildInputs = [

pkgs/desktops/xfce/core/xfce4-panel/default.nix
@@ -1,9 +1,9 @@
-{ lib
+{ stdenv
+, lib
 , mkXfceDerivation
 , cairo
 , exo
 , garcon
-, gobject-introspection
 , gtk-layer-shell
 , gtk3
 , libdbusmenu-gtk3
@@ -12,9 +12,14 @@
 , libxfce4util
 , libxfce4windowing
 , tzdata
-, vala
 , wayland
 , xfconf
+, withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages
+, buildPackages
+, gobject-introspection
+, vala
 }:
 
 mkXfceDerivation {
@@ -24,9 +29,9 @@ mkXfceDerivation {
 
   sha256 = "sha256-tLWjU0M7tuE+qqDwaE1CtnOjDiPWno8Mf7hhxYxbvjo=";
 
-  nativeBuildInputs = [
+  nativeBuildInputs = lib.optionals withIntrospection [
     gobject-introspection
-    vala
+    vala # vala bindings require GObject introspection
   ];
 
   buildInputs = [

pkgs/desktops/xfce/core/xfconf/default.nix
@@ -1,4 +1,5 @@
 {
+  stdenv,
   lib,
   mkXfceDerivation,
   gobject-introspection,
@@ -6,6 +7,10 @@
   vala,
   libxfce4util,
   glib,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
+  buildPackages,
 }:
 
 mkXfceDerivation {
@@ -15,11 +20,14 @@ mkXfceDerivation {
 
   sha256 = "sha256-U+Sk7ubBr1ZD1GLQXlxrx0NQdhV/WpVBbnLcc94Tjcw=";
 
-  nativeBuildInputs = [
-    gobject-introspection
-    perl
-    vala
-  ];
+  nativeBuildInputs =
+    [
+      perl
+    ]
+    ++ lib.optionals withIntrospection [
+      gobject-introspection
+      vala # vala bindings require GObject introspection
+    ];
 
   buildInputs = [ libxfce4util ];
 

commit cb6bb0aa45c047a4abbde8f649f80388945f4c0e

xfce.thunar: Enable introspection conditionally
Assuming this is a fallout from efbd153c406e00dd3f300e2a91c686509c6896d5.

pkgs/desktops/xfce/core/thunar/default.nix
@@ -16,7 +16,10 @@
   pcre2,
   xfce4-panel,
   xfconf,
-  withIntrospection ? false,
+  withIntrospection ?
+    lib.meta.availableOn stdenv.hostPlatform gobject-introspection
+    && stdenv.hostPlatform.emulatorAvailable buildPackages,
+  buildPackages,
   gobject-introspection,
 }:
 