28ec6cc43d672ecd72355204f887a9678da07be5
brubsby <brubsbybrubsby@gmail.com>
2025-12-22 08:22:49 -0500

    maintainers: add brubsby

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -3942,6 +3942,12 @@
     matrix = "@brsvh:mozilla.org";
     name = "Burgess Chang";
   };
+  brubsby = {
+    email = "brubsbybrubsby@gmail.com";
+    github = "brubsby";
+    githubId = 57653502;
+    name = "Tyler Busby";
+  };
   bryango = {
     name = "Bryan Lai";
     email = "bryanlais@gmail.com";
