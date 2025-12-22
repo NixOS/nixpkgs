d24015ca373e58d07b8b7d47b0dfe306a4436f2d
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-22 22:03:12 +0000

    vscode-extensions.betterthantomorrow.calva: 2.0.540 -> 2.0.542

--- pkgs/applications/editors/vscode/extensions/betterthantomorrow.calva/default.nix
+++ pkgs/applications/editors/vscode/extensions/betterthantomorrow.calva/default.nix
@@ -11,8 +11,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "calva";
     publisher = "betterthantomorrow";
-    version = "2.0.540";
-    hash = "sha256-KzBO6BEWUUPpwNzyAAYpr6hBDUMkv3cl7EamfCqeOvk=";
+    version = "2.0.542";
+    hash = "sha256-8Tvzjr9j6kVZukz967wqbNWNJ5Ngija0UY0hs1/vECM=";
   };
 
   nativeBuildInputs = [
