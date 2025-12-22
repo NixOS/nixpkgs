0621c5b4a6bdd04de13a6bd937f7b1944db8480a
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-12-22 22:04:54 +0100

    tree-sitter-grammars: add `grammarsScope` package set
    This allows overriding grammars with overlays

--- pkgs/development/tools/parsing/tree-sitter/default.nix
+++ pkgs/development/tools/parsing/tree-sitter/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   stdenv,
+  newScope,
   fetchFromGitHub,
   fetchFromGitLab,
   fetchFromSourcehut,
@@ -67,6 +68,14 @@ let
     Use pkgs.tree-sitter-grammars.<name> to access.
   */
   builtGrammars = lib.mapAttrs (_: lib.makeOverridable buildGrammar) grammars;
+
+  /**
+    # Extensible package set for tree-sitter grammars.
+    # Provides .override and .extend for customization.
+    # Note: Use builtGrammars (not this) when iterating over grammars,
+    # as this includes package set functions alongside derivations
+  */
+  grammarsScope = lib.makeScope newScope (self: builtGrammars);
 
   # Usage:
   # pkgs.tree-sitter.withPlugins (p: [ p.tree-sitter-c p.tree-sitter-java ... ])
@@ -174,6 +183,7 @@ rustPlatform.buildRustPackage (final: {
       grammars
       buildGrammar
       builtGrammars
+      grammarsScope
       withPlugins
       allGrammars
       ;
