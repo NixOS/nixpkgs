17d915b126b945b2467bee8956b4a7a9fbd89fa0
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-22 08:08:00 +0000

    fishPlugins.exercism-cli-fish-wrapper: 0-unstable-2025-09-28 -> 0-unstable-2025-12-16

--- pkgs/shells/fish/plugins/exercism-cli-fish-wrapper.nix
+++ pkgs/shells/fish/plugins/exercism-cli-fish-wrapper.nix
@@ -6,13 +6,13 @@
 }:
 buildFishPlugin {
   pname = "exercism-cli-fish-wrapper";
-  version = "0-unstable-2025-09-28";
+  version = "0-unstable-2025-12-16";
 
   src = fetchFromGitHub {
     owner = "glennj";
     repo = "exercism-cli-fish-wrapper";
-    rev = "f6627c00324b2599239e3835af39adce04e1091c";
-    hash = "sha256-bS1NN6oWi5j9uPwhuMApDRZlVYwOwlIC9NcdBZ9noL8=";
+    rev = "60683e23a6a7600530d2e454eb238e7fd0bb349f";
+    hash = "sha256-f2tN5dRYto15n0AY9lAqqj5pZdwoxEno8JwRdwAROH8=";
   };
 
   passthru.updateScript = unstableGitUpdater { };
