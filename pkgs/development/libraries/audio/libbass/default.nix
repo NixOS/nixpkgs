d8db65d457cf7bc760c64c3e3f9c397321ebbe39
poz <poz@poz.pet>
2025-12-22 17:24:21 +0100

    libbass_fx: 2.4.12.1 -> 2.4.12.6

--- pkgs/development/libraries/audio/libbass/default.nix
+++ pkgs/development/libraries/audio/libbass/default.nix
@@ -29,14 +29,14 @@ let
 
     bass_fx = {
       h = "C/bass_fx.h";
-      version = "2.4.12.1";
+      version = "2.4.12.6";
       so = {
         i686_linux = "libs/x86/libbass_fx.so";
         x86_64-linux = "libs/x86_64/libbass_fx.so";
         armv7l-linux = "libs/armhf/libbass_fx.so";
         aarch64-linux = "libs/aarch64/libbass_fx.so";
       };
-      url = "https://web.archive.org/web/20240926184106/https://www.un4seen.com/files/z/0/bass_fx24-linux.zip";
+      url = "https://web.archive.org/web/20250627192213/https://www.un4seen.com/files/z/0/bass_fx24-linux.zip";
       hash = "sha256-Hul2ELwnaDV8TDRMDXoFisle31GATDkf3PdkR2K9QTs=";
     };
 
