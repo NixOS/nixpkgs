cf8dc1d594578dcaa4496002bab0df5fc9053e54
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-22 14:03:24 +0000

    python3Packages.netbox-bgp: 0.17.0 -> 0.18.0

--- pkgs/development/python-modules/netbox-bgp/default.nix
+++ pkgs/development/python-modules/netbox-bgp/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "netbox-bgp";
-  version = "0.17.0";
+  version = "0.18.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "netbox-community";
     repo = "netbox-bgp";
     tag = "v${version}";
-    hash = "sha256-2Ss1ImJTqhr1FTRpnk1cwpFbHPjoNuUdpXoJJ9mjzHQ=";
+    hash = "sha256-Y2+TsGBpiBfj0e3mVdE2CjKlp0wX6zaFNcnc3gRKaHM=";
   };
 
   build-system = [ setuptools ];
