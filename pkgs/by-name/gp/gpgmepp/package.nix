3e2fee2a5ef9be617972bb3a2c6bed0a52dae5c7
Marcin Serwin <marcin@serwin.dev>
2025-12-22 16:47:38 +0100

    gpgmepp: fix includedir path in the exported CMake target
    Signed-off-by: Marcin Serwin <marcin@serwin.dev>

--- pkgs/by-name/gp/gpgmepp/package.nix
+++ pkgs/by-name/gp/gpgmepp/package.nix
@@ -16,7 +16,10 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-1HlgScBnCKJvMJb3SO8JU0fho8HlcFYXAf6VLD9WU4I=";
   };
 
-  patches = [ ./0001-Fix-handling-of-absolute-install-dirs-in-.pc-install.patch ];
+  patches = [
+    ./0001-Fix-handling-of-absolute-install-dirs-in-.pc-install.patch
+    ./0001-Don-t-hardcode-include-as-includedir.patch
+  ];
 
   nativeBuildInputs = [
     cmake
