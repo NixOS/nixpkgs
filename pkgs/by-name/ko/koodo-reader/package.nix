f8e2327212ef3def28cb5dd176a378103191fe98
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-23 21:57:28 +0000

    koodo-reader: 2.2.3 -> 2.2.4

--- pkgs/by-name/ko/koodo-reader/package.nix
+++ pkgs/by-name/ko/koodo-reader/package.nix
@@ -24,13 +24,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "koodo-reader";
-  version = "2.2.3";
+  version = "2.2.4";
 
   src = fetchFromGitHub {
     owner = "troyeguo";
     repo = "koodo-reader";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-VQC1xvFw6TDw6P3UcQ4Y++UpjSgEGiRh0M+i1Z8O+N4=";
+    hash = "sha256-KUcI+0+ICMdwAF30CLM3QdS+X8UnYiHhcYkvEQ6WgS8=";
   };
 
   patches = [
@@ -39,7 +39,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   offlineCache = fetchYarnDeps {
     inherit (finalAttrs) src patches;
-    hash = "sha256-gRaHVWSTBwjVcswy6DVk5yLympudbDcKkvWDry4rsvI=";
+    hash = "sha256-cnpTgIpz9HmMb9tIo5e1bfiTnTudPUg2wxwj91Q4BHs=";
   };
 
   nativeBuildInputs = [
