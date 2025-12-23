e458ede648c7750c6ff6d7aca88cfa6e1a654bc5
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-23 05:40:25 +0000

    mongodb-tools: 100.13.0 -> 100.14.0

--- pkgs/by-name/mo/mongodb-tools/package.nix
+++ pkgs/by-name/mo/mongodb-tools/package.nix
@@ -10,13 +10,13 @@
 
 buildGoModule rec {
   pname = "mongo-tools";
-  version = "100.13.0";
+  version = "100.14.0";
 
   src = fetchFromGitHub {
     owner = "mongodb";
     repo = "mongo-tools";
     tag = version;
-    hash = "sha256-aQrwJFFdaCIkcnofdGtZ/BMX9KPqr1pHxwm+A04LhXI=";
+    hash = "sha256-QJ1pew6Lg9KAxMJ2XyVXlnRdatbgXesKKmoHCiXQb0c=";
   };
 
   vendorHash = null;
