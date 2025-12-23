45bf3ec4c64757e129888eba4510f6aced56a67c
Justin Restivo <justin@restivo.me>
2025-12-22 22:41:58 -0500

    python3Packages.threadpool: drop

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18804,8 +18804,6 @@ self: super: with self; {
 
   threadloop = callPackage ../development/python-modules/threadloop { };
 
-  threadpool = callPackage ../development/python-modules/threadpool { };
-
   threadpoolctl = callPackage ../development/python-modules/threadpoolctl { };
 
   threat9-test-bed = callPackage ../development/python-modules/threat9-test-bed { };
