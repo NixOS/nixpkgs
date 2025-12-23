NixOS/nixpkgs

commit 6b6c8b140268f092e1fbc66fe7b6d122009ac3cb

linux/common-config: explicitly select RUST_FW_LOADER_ABSTRACTIONS for now

pkgs/os-specific/linux/kernel/common-config.nix
@@ -561,6 +561,10 @@ let
         # Enable CEC over DisplayPort
         DRM_DP_CEC = whenOlder "6.10" yes;
         DRM_DISPLAY_DP_AUX_CEC = whenAtLeast "6.10" yes;
+
+        # Required for Nova
+        # FIXME: remove after https://gitlab.freedesktop.org/drm/rust/kernel/-/commit/3d3352e73a55a4ccf110f8b3419bbe2fbfd8a030 lands
+        RUST_FW_LOADER_ABSTRACTIONS = whenAtLeast "6.12" yes;
       }
       //
         lib.optionalAttrs

commit 4d9594e171e059e54fbabbef114493c2b0499c26

linux/common-config: select RUST_FW_LOADER_ABSTRACTIONS only with Rust
Rust may not be available for i686 and the counfigration would fail.

Fixes: 6b6c8b140268 ("linux/common-config: explicitly select RUST_FW_LOADER_ABSTRACTIONS for now")

pkgs/os-specific/linux/kernel/common-config.nix
@@ -564,7 +564,7 @@ let
 
         # Required for Nova
         # FIXME: remove after https://gitlab.freedesktop.org/drm/rust/kernel/-/commit/3d3352e73a55a4ccf110f8b3419bbe2fbfd8a030 lands
-        RUST_FW_LOADER_ABSTRACTIONS = whenAtLeast "6.12" yes;
+        RUST_FW_LOADER_ABSTRACTIONS = lib.mkIf withRust (whenAtLeast "6.12" yes);
       }
       //
         lib.optionalAttrs