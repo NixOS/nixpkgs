331ce73b6acb24b40bf712d39783e4b47ea569e6
Karolis Stasaitis <contact@carlossless.io>
2025-12-23 22:26:37 +0200

    yosys-synlig: mark as broken

--- pkgs/development/compilers/yosys/plugins/synlig.nix
+++ pkgs/development/compilers/yosys/plugins/synlig.nix
@@ -87,5 +87,6 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ hzeller ];
     platforms = lib.platforms.all;
+    broken = true; # expected headers not found in yosys 0.58
   };
 })
