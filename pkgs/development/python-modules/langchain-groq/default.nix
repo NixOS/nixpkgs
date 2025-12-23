c88ef27ce2b80f1208addbd40988c801ee3fcda8
Sarah Clark <seclark@nextquestion.net>
2025-12-23 14:25:47 -0800

    python3Packages.langchain-groq: 1.1.0 -> 1.1.1
    Compatible with python3Packages.groq @ 1.0.0
    
    CHANGELOG: https://github.com/langchain-ai/langchain/releases/tag/langchain-groq%3D%3D1.1.1
    DIFF: https://github.com/langchain-ai/langchain/compare/langchain-groq%3D%3D1.1.0...langchain-groq%3D%3D1.1.1

--- pkgs/development/python-modules/langchain-groq/default.nix
+++ pkgs/development/python-modules/langchain-groq/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "langchain-groq";
-  version = "1.1.0";
+  version = "1.1.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langchain";
     tag = "langchain-groq==${version}";
-    hash = "sha256-dmuDgKQW1yAz/8tjQx7LaUiuz5Sh4cAyd9nt33mCPbI=";
+    hash = "sha256-WpIP41eALPoWuYjm2ygEH7TwClKYwAG0uEd1ZbbqMTY=";
   };
 
   sourceRoot = "${src.name}/libs/partners/groq";
