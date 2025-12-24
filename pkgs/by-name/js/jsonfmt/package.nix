db24c476da790d104e09aeac18d8f962a85992ac
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-24 19:46:49 +0000

    jsonfmt: 0.5.1 -> 0.5.2

--- pkgs/by-name/js/jsonfmt/package.nix
+++ pkgs/by-name/js/jsonfmt/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule rec {
   pname = "jsonfmt";
-  version = "0.5.1";
+  version = "0.5.2";
 
   src = fetchFromGitHub {
     owner = "caarlos0";
     repo = "jsonfmt";
     rev = "v${version}";
-    hash = "sha256-4SNpW/+4S4sEwjM7b9ClqKqwqFFVbCVv5VnftGIHtjo=";
+    hash = "sha256-CMBqqTGpqoErFPKn4lxMB9XrdlhZcY6qbRZZVUVMQj0=";
   };
 
-  vendorHash = "sha256-6pCgBCwHgTRnLDNfveBEKbs7kiXSSacD0B82A2Sbl1U=";
+  vendorHash = "sha256-QcljmDsz5LsXfHaXNVBU7IIVVgkm3Vfnirchx5ZOMSg=";
 
   ldflags = [
     "-s"
