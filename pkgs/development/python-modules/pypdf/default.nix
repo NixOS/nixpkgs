5c0d87e3f1e609a0de0a7bde5ba52174eb9dec2f
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-24 16:07:06 +0000

    python3Packages.pypdf: 6.4.2 -> 6.5.0

--- pkgs/development/python-modules/pypdf/default.nix
+++ pkgs/development/python-modules/pypdf/default.nix
@@ -27,7 +27,7 @@
 
 buildPythonPackage rec {
   pname = "pypdf";
-  version = "6.4.2";
+  version = "6.5.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -38,7 +38,7 @@ buildPythonPackage rec {
     tag = version;
     # fetch sample files used in tests
     fetchSubmodules = true;
-    hash = "sha256-GkCNw7XvDPvLIiIUAgXsTLQ2OBbqhpf3xHQZpB/f2ys=";
+    hash = "sha256-GZAvd2ZIS6B/IwnJ3YWVg3vpfaXBUNAAyooiHENkFRk=";
   };
 
   outputs = [
