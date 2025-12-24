3cece8709613d460746960586268a2277afb87a6
pancaek <20342389+pancaek@users.noreply.github.com>
2025-12-24 13:11:54 -0800

    xfce4-genmon-plugin: move to top-level

--- pkgs/desktops/xfce/default.nix
+++ pkgs/desktops/xfce/default.nix
@@ -89,8 +89,6 @@ makeScopeWithSplicing' {
 
       #### PANEL PLUGINS
 
-      xfce4-genmon-plugin = callPackage ./panel-plugins/xfce4-genmon-plugin { };
-
       xfce4-i3-workspaces-plugin = callPackage ./panel-plugins/xfce4-i3-workspaces-plugin { };
 
       xfce4-netload-plugin = callPackage ./panel-plugins/xfce4-netload-plugin { };
@@ -162,5 +160,6 @@ makeScopeWithSplicing' {
   xfce4-docklike-plugin = lib.warnOnInstantiate "‘xfce.xfce4-docklike-plugin’ was moved to top-level. Please use ‘pkgs.xfce4-docklike-plugin’ directly" pkgs.xfce4-docklike-plugin; # Added on 2025-12-19
   xfce4-eyes-plugin = lib.warnOnInstantiate "‘xfce.xfce4-eyes-plugin’ was moved to top-level. Please use ‘pkgs.xfce4-eyes-plugin’ directly" pkgs.xfce4-eyes-plugin; # Added on 2025-12-19
   xfce4-fsguard-plugin = lib.warnOnInstantiate "‘xfce.xfce4-fsguard-plugin’ was moved to top-level. Please use ‘pkgs.xfce4-fsguard-plugin’ directly" pkgs.xfce4-fsguard-plugin; # Added on 2025-12-19
+  xfce4-genmon-plugin = lib.warnOnInstantiate "‘xfce.xfce4-genmon-plugin’ was moved to top-level. Please use ‘pkgs.xfce4-genmon-plugin’ directly" pkgs.xfce4-genmon-plugin; # Added on 2025-12-19
 
 }
