f72a1dbe33cd144e60d528132a625859b716ba74
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-24 16:02:27 +0000

    karing: 1.2.5.881 -> 1.2.10.1302

--- pkgs/by-name/ka/karing/package.nix
+++ pkgs/by-name/ka/karing/package.nix
@@ -24,11 +24,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "karing";
-  version = "1.2.5.881";
+  version = "1.2.10.1302";
 
   src = fetchurl {
     url = "https://github.com/KaringX/karing/releases/download/v${finalAttrs.version}/karing_${finalAttrs.version}_linux_amd64.deb";
-    hash = "sha256-gkuKdOq07C7xd8xNoXWlcqaZ7xe9VmjU8e43Hg1yN7w=";
+    hash = "sha256-10dNfIeyqEk3SfCQGVna1aPRbaHY2TFDFkHYsjTN8lI=";
   };
 
   nativeBuildInputs = [
