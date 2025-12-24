adb7c21570ece2d61675fb28fd58bdacfab88283
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-24 22:28:13 +0000

    v2rayn: 7.15.7 -> 7.16.8

--- pkgs/by-name/v2/v2rayn/package.nix
+++ pkgs/by-name/v2/v2rayn/package.nix
@@ -20,13 +20,13 @@
 
 buildDotnetModule (finalAttrs: {
   pname = "v2rayn";
-  version = "7.15.7";
+  version = "7.16.8";
 
   src = fetchFromGitHub {
     owner = "2dust";
     repo = "v2rayN";
     tag = finalAttrs.version;
-    hash = "sha256-xTD1bdL/UUGqUxDmrguO6Oapv37clDD2b3YWCe7B+Bs=";
+    hash = "sha256-O4WHyvFGtZB1eSyIXev2QvV0HmJNi42jK1J06ZFfw/8=";
     fetchSubmodules = true;
   };
 
