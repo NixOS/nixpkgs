f801a20bb7ea31923af4b976323622e081e3da9f
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-24 16:54:37 +0000

    fcitx5-mcbopomofo: 2.9.4 -> 2.9.5

--- pkgs/by-name/fc/fcitx5-mcbopomofo/package.nix
+++ pkgs/by-name/fc/fcitx5-mcbopomofo/package.nix
@@ -15,13 +15,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fcitx5-mcbopomofo";
-  version = "2.9.4";
+  version = "2.9.5";
 
   src = fetchFromGitHub {
     owner = "openvanilla";
     repo = "fcitx5-mcbopomofo";
     rev = version;
-    hash = "sha256-4nJrNAAh7N2hJW/+cKz2pmu004VXAkuw9y0PJ7ktIug=";
+    hash = "sha256-efpVvWchJywKyGu7I6pNRVKJhIv01iKAXFCJ+7kcMwc=";
   };
 
   nativeBuildInputs = [
