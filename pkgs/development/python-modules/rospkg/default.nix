dc90274e2c28c6d791b3895eaa4f0111aa43b6a8
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-24 16:17:12 +0000

    python3Packages.rospkg: 1.6.0 -> 1.6.1

--- pkgs/development/python-modules/rospkg/default.nix
+++ pkgs/development/python-modules/rospkg/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "rospkg";
-  version = "1.6.0";
+  version = "1.6.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ros-infrastructure";
     repo = "rospkg";
     tag = version;
-    hash = "sha256-6nfdY+p3P3iGuj+7Lo7ybsZ+1x104m7WzGgxr8dDDuw=";
+    hash = "sha256-YRBmL+aXQ/0rxivERja9ng+GqL8NQGmNYhKjMY7+6nc=";
   };
 
   build-system = [ setuptools ];
