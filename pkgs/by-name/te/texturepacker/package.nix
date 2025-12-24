efb06fc6a3b7d195057d742258689eec3ef1cf18
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-24 05:31:46 +0000

    texturepacker: 7.9.0 -> 7.11.0

--- pkgs/by-name/te/texturepacker/package.nix
+++ pkgs/by-name/te/texturepacker/package.nix
@@ -9,11 +9,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "texturepacker";
-  version = "7.9.0";
+  version = "7.11.0";
 
   src = fetchurl {
     url = "https://www.codeandweb.com/download/texturepacker/${finalAttrs.version}/TexturePacker-${finalAttrs.version}.deb";
-    hash = "sha256-o613XN8/ypmOEWyDjIQeF6JQsZK5gyDsgmYwd9siU1k=";
+    hash = "sha256-s0d9kcsvW3r6kkz5qIxYDtIMD2Kzqz/tCSWvwxeJFnE=";
   };
 
   nativeBuildInputs = [
