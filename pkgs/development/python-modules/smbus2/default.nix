b9259d21f6e7ef704f8656d1f311a954182c074f
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-25 10:24:29 +0100

    python313Packages.smbus2: modernize

--- pkgs/development/python-modules/smbus2/default.nix
+++ pkgs/development/python-modules/smbus2/default.nix
@@ -3,15 +3,13 @@
   buildPythonPackage,
   fetchFromGitHub,
   pytestCheckHook,
-  pythonOlder,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "smbus2";
   version = "0.6.0";
-  format = "setuptools";
-
-  disabled = pythonOlder "3.6";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "kplindegaard";
@@ -20,6 +18,8 @@ buildPythonPackage rec {
     hash = "sha256-GoXSDUmMnrJAfQ8EfCP5bdkq5g0nKLRHcvou5c6vZGU=";
   };
 
+  build-system = [ setuptools ];
+
   nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [ "smbus2" ];
@@ -27,7 +27,7 @@ buildPythonPackage rec {
   meta = {
     description = "Drop-in replacement for smbus-cffi/smbus-python";
     homepage = "https://smbus2.readthedocs.io/";
-    changelog = "https://github.com/kplindegaard/smbus2/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/kplindegaard/smbus2/blob/${src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
