dea1e0381793caa33c35ef3d9298882f5778902c
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-25 12:11:27 +0000

    rustical: 0.11.4 -> 0.11.5

--- pkgs/by-name/ru/rustical/package.nix
+++ pkgs/by-name/ru/rustical/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rustical";
-  version = "0.11.4";
+  version = "0.11.5";
 
   src = fetchFromGitHub {
     owner = "lennart-k";
     repo = "rustical";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-nTpFVtnKOB+yEhKXB9tFzNtdTm5sA3cDPt2De26ntjE=";
+    hash = "sha256-hvdYwh9nmSXS9QhyxW5mLRS4kgf164I+UxGHRlK1oH4=";
   };
 
-  cargoHash = "sha256-JpvDGbd6dQ/1vu8BEDmRj7V1Wb7R86wV1NHbHYwZ/Zc=";
+  cargoHash = "sha256-rpTQpb0a8QhFT7Qo6hYZ+nPmWFnR/vSVCoHvZFQR3Cs=";
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
