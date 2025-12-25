5a4c6c1b61fd12e51822af1f4162889dd08a8a30
aleksana <me@aleksana.moe>
2025-12-25 17:43:09 +0800

    python312Packages.pytest-subprocess: fix build by not applying patch

--- pkgs/development/python-modules/pytest-subprocess/default.nix
+++ pkgs/development/python-modules/pytest-subprocess/default.nix
@@ -3,6 +3,7 @@
   buildPythonPackage,
   pythonOlder,
   fetchFromGitHub,
+  pythonAtLeast,
   fetchpatch,
   setuptools,
   pytest,
@@ -29,9 +30,11 @@ buildPythonPackage rec {
     hash = "sha256-3vBYOk/P78NOjAbs3fT6py5QOOK3fX+AKtO4j5vxZfk=";
   };
 
-  patches = [
+  patches = lib.optionals (pythonAtLeast "3.13") [
     (fetchpatch {
       # python 3.14 compat
+      # the patch however breaks 3.12:
+      # https://github.com/aklajnert/pytest-subprocess/issues/192
       url = "https://github.com/aklajnert/pytest-subprocess/commit/be30d9a94ba45afb600717e3fcd95b8b2ff2c60e.patch";
       hash = "sha256-TYk/Zu2MF+ROEKTgZI1rzA2MlW2it++xElfGZS0Dn5s=";
     })
