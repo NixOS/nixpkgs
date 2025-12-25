2871bf402255c26bcc1eb3e65b6eb419f067b123
Sigmanificient <edhyjox@gmail.com>
2025-12-25 02:02:00 +0100

    python313Packages.hikari: 2.4.1 -> 2.5.0

--- pkgs/development/python-modules/hikari/default.nix
+++ pkgs/development/python-modules/hikari/default.nix
@@ -18,14 +18,14 @@
 }:
 buildPythonPackage rec {
   pname = "hikari";
-  version = "2.4.1";
+  version = "2.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "hikari-py";
     repo = "hikari";
     tag = version;
-    hash = "sha256-lkJICN5uXFIKUZwxZI82FSYZLWFa7Cb6tDs6wV9DsY0=";
+    hash = "sha256-dOYaGWxhLefWIhaaIPM80cpYtcB/ywibFBzWDr3hKsw=";
     # The git commit is part of the `hikari.__git_sha1__` original output;
     # leave that output the same in nixpkgs. Use the `.git` directory
     # to retrieve the commit SHA, and remove the directory afterwards,
@@ -69,9 +69,6 @@ buildPythonPackage rec {
   postPatch = ''
     substituteInPlace hikari/_about.py \
       --replace-fail "__git_sha1__: typing.Final[str] = \"HEAD\"" "__git_sha1__: typing.Final[str] = \"$(cat $src/COMMIT)\""
-    # XXX: Remove once pytest-asyncio is updated to 0.24+
-    substituteInPlace pyproject.toml \
-      --replace-fail "asyncio_default_fixture_loop_scope = \"func\"" ""
   '';
 
   meta = {
