b9c908c0c2aafb478db340af11387c04a59330d2
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-25 05:16:57 +0000

    nginx-sso: 0.27.5 -> 0.27.6
    (cherry picked from commit 7d514ccf03c3cbb5766420f5118085d19ba557b7)

--- pkgs/by-name/ng/nginx-sso/package.nix
+++ pkgs/by-name/ng/nginx-sso/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "nginx-sso";
-  version = "0.27.5";
+  version = "0.27.6";
 
   src = fetchFromGitHub {
     owner = "Luzifer";
     repo = "nginx-sso";
     rev = "v${version}";
-    hash = "sha256-yiwKnOv/4Zy9bBvQZiIp8cH1XvgV88m9t4ptqQp1LG8=";
+    hash = "sha256-nYqk1VK6R9HH67NLQDUifW3AjIW5pjD1Jmf+cYH3SQo=";
   };
 
   vendorHash = "sha256-KflzlrjOOTDZQq2yP0zQsDgULrbnoeRRxOVHxKINsYw=";
