82fdfb5692c1b663f1b5e68fb0b6b5adcbdf83b0
Jan van Brügge <supermanitu@gmail.com>
2025-12-26 02:52:08 +0100

    isabelle: remove jwiegley as maintainer
    No activity for over three years now

--- pkgs/by-name/is/isabelle/package.nix
+++ pkgs/by-name/is/isabelle/package.nix
@@ -277,7 +277,6 @@ stdenv.mkDerivation (finalAttrs: {
     ];
     license = lib.licenses.bsd3;
     maintainers = [
-      lib.maintainers.jwiegley
       lib.maintainers.jvanbruegge
     ];
     platforms = lib.platforms.unix;
