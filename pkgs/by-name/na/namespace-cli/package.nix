9eab4e2dfc8214950c1c148ad3a0e6c37c9e175a
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-26 13:05:00 +0000

    namespace-cli: 0.0.456 -> 0.0.458

--- pkgs/by-name/na/namespace-cli/package.nix
+++ pkgs/by-name/na/namespace-cli/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "namespace-cli";
-  version = "0.0.456";
+  version = "0.0.458";
 
   src = fetchFromGitHub {
     owner = "namespacelabs";
     repo = "foundation";
     rev = "v${version}";
-    hash = "sha256-V+OHqM4rqoqroH3IL+zjhxmK4G5J04Fy6Yn/GqTqtaQ=";
+    hash = "sha256-lPiEqppAjZ4ZLqcEOXwjXsOMv+G87CWrNw/rViEddZg=";
   };
 
   vendorHash = "sha256-PwSDl4dPcTPGzXAunW4J4gyu9a68qTP3MpdexUpFt1U=";
