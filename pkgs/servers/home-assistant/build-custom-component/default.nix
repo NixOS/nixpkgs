NixOS/nixpkgs

commit 9dcea72b22ffcb9dac2f17b18b6c9cb96c66ce39

buildHomeAssistantComponent: run manifestRequirementsCheckHook independently of checkPhase

pkgs/servers/home-assistant/build-custom-component/default.nix
@@ -41,13 +41,13 @@ home-assistant.python.pkgs.buildPythonPackage (
       runHook postInstall
     '';
 
-    nativeCheckInputs =
+    nativeBuildInputs =
       with home-assistant.python.pkgs;
       [
         manifestRequirementsCheckHook
         packaging
       ]
-      ++ (args.nativeCheckInputs or [ ]);
+      ++ (args.nativeBuildInputs or [ ]);
 
     passthru = {
       isHomeAssistantComponent = true;

commit 2bbd19720343c0a6ec00c388d88053c936a06f6a

buildHomeAssistantComponent: reintroduce nativeCheckInputs
This fixes a bug introduced in commit 9dcea72b22ffcb9dac2f17b18b6c9cb96c66ce39 where they moved setup hooks from `nativeCheckInputs` to `nativeBuildInputs` without modifying the `removeAttrs` logic accordingly. This meant that:

1. callee's `nativeCheckInputs` parameter would be erroneously ignored, and
2. callee's `nativeBuildInputs` parameter would erroneously clobber the setup hooks previously defined in the `buildHomeAssistantComponent` function.

Changing the `removeAttrs` logic so that it filters out callee's `nativeBuildInputs` parameter rather than `nativeCheckInputs` fixes the bug.

pkgs/servers/home-assistant/build-custom-component/default.nix
@@ -62,7 +62,7 @@ home-assistant.python.pkgs.buildPythonPackage (
   }
   // removeAttrs args [
     "meta"
-    "nativeCheckInputs"
+    "nativeBuildInputs"
     "passthru"
   ]
 )