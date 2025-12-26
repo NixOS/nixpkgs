f10e264fe8d7b7e7f526c6972157413218396786
Morgan Jones <me@numin.it>
2025-12-25 21:08:42 -0800

    klee: 3.1-unstable-2025-07-11 -> 3.2
    Still requires meta.broken = true since it has failing tests with LLVM
    18, but it's not an unstable version at least.
    
    Move to by-name while we're at it.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11181,10 +11181,6 @@ with pkgs;
 
   klayout = libsForQt5.callPackage ../applications/misc/klayout { };
 
-  klee = callPackage ../applications/science/logic/klee {
-    llvmPackages = llvmPackages_18;
-  };
-
   kotatogram-desktop =
     callPackage ../applications/networking/instant-messengers/telegram/kotatogram-desktop
       { };
