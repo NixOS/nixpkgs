73d732700ecf85ba1e8fa1f9e8fc5e069a5dcf9a
linsui <36977733+linsui@users.noreply.github.com>
2025-12-26 17:09:09 +0800

    design: 46-alpha1 -> 48-alpha1

--- pkgs/by-name/de/design/package.nix
+++ pkgs/by-name/de/design/package.nix
@@ -16,14 +16,14 @@
 
 stdenv.mkDerivation rec {
   pname = "design";
-  version = "46-alpha1";
+  version = "48-alpha1";
 
   src = fetchFromGitHub {
     owner = "dubstar-04";
     repo = "Design";
     tag = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-Q4R/Ztu4w8IRvq15xNXN/iP/6hIHe/W+me1jROGpYc8=";
+    hash = "sha256-xLARmvqJUxVjHHeak/BrpfIe18KCy9++8HRjOFjwE7I=";
   };
 
   nativeBuildInputs = [
