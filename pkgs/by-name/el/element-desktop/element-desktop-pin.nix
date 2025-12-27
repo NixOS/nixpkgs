7093dd6852d3d860cf89a1233657209b3545bc8d
teutat3s <10206665+teutat3s@users.noreply.github.com>
2025-12-27 23:04:02 +0100

    element-desktop: use electron-builder
    Co-authored-by: Akshay Mankar <itsakshaymankar@gmail.com>

--- pkgs/by-name/el/element-desktop/element-desktop-pin.nix
+++ pkgs/by-name/el/element-desktop/element-desktop-pin.nix
@@ -2,6 +2,6 @@
   "version" = "1.12.7";
   "hashes" = {
     "desktopSrcHash" = "sha256-VnZyNrylYbKhd9YyuoUSJWbTEphOsTivbbYIUeEZv2U=";
-    "desktopYarnHash" = "sha256-SAIoJRX06oE4j2Gjb/0MSiUa0UGYkQmuyuV90VRbFtk=";
+    "desktopYarnHash" = "sha256-Gnd/ouRI/OxFwsvR5arfi/FcGld3XjtW9tzuwyX8IRg=";
   };
 }
