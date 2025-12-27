260bf8a2c6657eb099c0cecd9c81e001bc1814ba
Felix Buehler <account@buehler.rocks>
2025-12-27 21:22:58 +0100

    nixosTests.freshrss.none-auth: remove

--- nixos/tests/freshrss/default.nix
+++ nixos/tests/freshrss/default.nix
@@ -3,7 +3,6 @@
 {
   extensions = runTest ./extensions.nix;
   http-auth = runTest ./http-auth.nix;
-  none-auth = runTest ./none-auth.nix;
   pgsql = runTest ./pgsql.nix;
   nginx-sqlite = runTest ./nginx-sqlite.nix;
   caddy-sqlite = runTest ./caddy-sqlite.nix;
