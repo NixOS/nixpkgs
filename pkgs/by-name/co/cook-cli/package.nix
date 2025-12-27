f0d69666360bf09d77edb7577ea1ed0eb0e6e9ef
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-27 20:35:37 +0000

    cook-cli: 0.19.0 -> 0.19.1

--- pkgs/by-name/co/cook-cli/package.nix
+++ pkgs/by-name/co/cook-cli/package.nix
@@ -10,16 +10,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "cook-cli";
-  version = "0.19.0";
+  version = "0.19.1";
 
   src = fetchFromGitHub {
     owner = "cooklang";
     repo = "cookcli";
     rev = "v${version}";
-    hash = "sha256-yNUiwMzCAj5aXuQIzfzpy2GylhB37CuSmyHsyxcmKXM=";
+    hash = "sha256-CJDWhcnY/HjaJbBJCevH9ytp7oJ734M30DxJ/vCCO/I=";
   };
 
-  cargoHash = "sha256-Kq70YgTis5e8PcIAOgAqk/fi3HmE+lbpYjaV47axGX4=";
+  cargoHash = "sha256-rI/6niK/9QgRc4cWmDLPOc70PN1mRZLXdPym1R3d/Iw=";
 
   # Build without the self-updating feature
   buildNoDefaultFeatures = true;
