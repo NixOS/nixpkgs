916c1ba1923e9a60a4fdf5782d497c514db10b9e
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-27 00:32:08 +0000

    qxmpp: 1.12.0 -> 1.13.0

--- pkgs/by-name/qx/qxmpp/package.nix
+++ pkgs/by-name/qx/qxmpp/package.nix
@@ -13,14 +13,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "qxmpp";
-  version = "1.12.0";
+  version = "1.13.0";
 
   src = fetchFromGitLab {
     domain = "invent.kde.org";
     owner = "libraries";
     repo = "qxmpp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-soOu6JyS/SEdwUngOUd0suImr70naZms9Zy2pRwBn5E=";
+    hash = "sha256-2gZiQmjxvgBp0DqP7apUnbHeVRk0hd5XoCPLgh3zAho=";
   };
 
   nativeBuildInputs = [
