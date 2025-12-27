ea3c83769a3f90416512282834d157ba76870fd3
Nicky Mouha <nmouha@users.noreply.github.com>
2025-12-26 19:27:08 -0500

    hol_light: broken on NixOS (/bin/bash: bad interpreter: No such file or directory)

--- pkgs/applications/science/logic/hol_light/default.nix
+++ pkgs/applications/science/logic/hol_light/default.nix
@@ -9,6 +9,7 @@
   zarith,
   camlp5,
   camlp-streams,
+  bash,
 }:
 
 let
@@ -51,6 +52,11 @@ stdenv.mkDerivation {
   };
 
   patches = [ ./0004-Fix-compilation-with-camlp5-7.11.patch ];
+
+  postPatch = ''
+    substituteInPlace hol_4.sh hol_4.14.sh holtest holtest_parallel \
+      --replace-fail "/bin/bash" "${bash}/bin/bash"
+  '';
 
   strictDeps = true;
 
