eda6d05b778961c98b3ea7315dc9fee9f6ba82bd
Magnus Frühling <skorpy@frankfurt.ccc.de>
2025-12-27 12:03:27 +0100

    flexoptix-app: 5.54.0-latest -> 5.57.0-latest

--- pkgs/by-name/fl/flexoptix-app/package.nix
+++ pkgs/by-name/fl/flexoptix-app/package.nix
@@ -7,12 +7,12 @@
 }:
 let
   pname = "flexoptix-app";
-  version = "5.54.0-latest";
+  version = "5.57.0-latest";
 
   src = fetchurl {
     name = "${pname}-${version}.AppImage";
     url = "https://flexbox.reconfigure.me/download/electron/linux/x64/FLEXOPTIX%20App.${version}.AppImage";
-    hash = "sha256-RTzVtAd134cKIlHWgP9Yyhu3FgdDSe1fKDGMsctO3x8=";
+    hash = "sha256-wTrvteIXiCMk4y2JnXodn5o89XJrLGHxOpHmma4SQXY=";
   };
 
   udevRules = fetchurl {
