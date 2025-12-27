f1717609d12a3c520a86dc0f3fef9f99edb00e07
Tert0 <tert0byte@gmail.com>
2025-12-27 11:16:55 +0100

    velocity: 3.4.0-unstable-2025-11-09 -> 3.4.0-unstable-2025-12-07

--- pkgs/by-name/ve/velocity/package.nix
+++ pkgs/by-name/ve/velocity/package.nix
@@ -34,13 +34,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "velocity";
-  version = "3.4.0-unstable-2025-11-09";
+  version = "3.4.0-unstable-2025-12-07";
 
   src = fetchFromGitHub {
     owner = "PaperMC";
     repo = "Velocity";
-    rev = "6cc1be7746ec49d2b600b1af1dcec99584750d38";
-    hash = "sha256-zDB9bIkclEBf+NdBkufmsmKM8Qo8uqeZ26OUdyzczi8=";
+    rev = "8f65a8142029717d3af09ecf82792f7345dc873a";
+    hash = "sha256-51n5ztuvGzWqvIfFOteV/CsvcInE7/aZmEeLCGonfLE=";
   };
 
   nativeBuildInputs = [
