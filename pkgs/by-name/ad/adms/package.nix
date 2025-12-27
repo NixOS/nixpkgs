ce3a119360d91a5c4561936dc2032291e7071f4c
Moraxyc <i@qaq.li>
2025-12-27 23:49:19 +0800

    adms: fix build with c23

--- pkgs/by-name/ad/adms/package.nix
+++ pkgs/by-name/ad/adms/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch2,
   autoreconfHook,
   flex,
   bison,
@@ -22,6 +23,15 @@ stdenv.mkDerivation rec {
     rev = "release-${version}";
     sha256 = "0i37c9k6q1iglmzp9736rrgsnx7sw8xn3djqbbjw29zsyl3pf62c";
   };
+
+  patches = [
+    # fix build with c23
+    #   admsXml.c:645:8: error: too many arguments to function 'verilogaparse'; expected 0, have 1
+    (fetchpatch2 {
+      url = "https://salsa.debian.org/science-team/adms/-/raw/01ef4a94a48736c49c67d90da506b34f6114f0b0/debian/patches/0002-fix-ftbfs-gcc-15.patch";
+      hash = "sha256-rSNBqdpuXA9ViyygRGn4KVknLCu0Q+UoOGLfoNAgccc=";
+    })
+  ];
 
   nativeBuildInputs = [ autoreconfHook ];
   buildInputs = [
