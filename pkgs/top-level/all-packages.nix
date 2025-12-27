40d53b860bd4b2a531f81ebf67c68904b0f07afd
Aliaksandr <grubian2@gmail.com>
2025-12-27 05:41:48 +0200

    alice-tools: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1011,13 +1011,13 @@ with pkgs;
     akkuPackages
     ;
 
-  alice-tools = callPackage ../tools/games/alice-tools {
+  alice-tools = callPackage ../by-name/al/alice-tools/package.nix {
     withGUI = false;
   };
 
-  alice-tools-qt5 = libsForQt5.callPackage ../tools/games/alice-tools { };
+  alice-tools-qt5 = libsForQt5.callPackage ../by-name/al/alice-tools/package.nix { };
 
-  alice-tools-qt6 = qt6Packages.callPackage ../tools/games/alice-tools { };
+  alice-tools-qt6 = qt6Packages.callPackage ../by-name/al/alice-tools/package.nix { };
 
   auditwheel = with python3Packages; toPythonApplication auditwheel;
 
