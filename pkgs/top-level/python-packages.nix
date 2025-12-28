00b68f5dc4cc5792abd99f90ebf0a551bf0ffc4b
Petingoso <petingavasco@protonmail.com>
2025-12-28 01:00:31 +0000

    python3Packages.boinor : init at 0.19.0-unstable-2025-12-27
    python3Packages.boinor : add to python-packages; format
    
    python3Packages.boinor: fix some bad practices
    
    python3Packages.boinor: add more deps and enable testing
    
    python3Packages.boinor: format

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2140,6 +2140,8 @@ self: super: with self; {
 
   boilerpy3 = callPackage ../development/python-modules/boilerpy3 { };
 
+  boinor = callPackage ../development/python-modules/boinor { };
+
   bokeh = callPackage ../development/python-modules/bokeh { };
 
   bokeh-sampledata = callPackage ../development/python-modules/bokeh-sampledata { };
