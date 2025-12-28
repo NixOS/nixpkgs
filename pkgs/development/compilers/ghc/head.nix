5710b82c565619b221d1fc45413721873544e245
sternenseemann <sternenseemann@systemli.org>
2025-12-28 12:44:21 +0100

    haskell.compiler.ghcHEAD: 9.15.20250811 -> 9.15.20251225

--- pkgs/development/compilers/ghc/head.nix
+++ pkgs/development/compilers/ghc/head.nix
@@ -1,5 +1,5 @@
 import ./common-hadrian.nix {
-  version = "9.15.20250811";
-  rev = "c8d76a2994b8620c54adc2069f4728135d6b5059";
-  sha256 = "001rf9z5a1v2xpm9qjzz2p966m5bxmqcnykq0xgb3qf40vi9rnh4";
+  version = "9.15.20251225";
+  rev = "a3afae0c85fa57479378a5df5bbde2e3a8da8a06";
+  sha256 = "sha256-inZFFKnCb8C7RTlKP2bnHOLfc1GveGi6Wy0uAaLv1Kk=";
 }
