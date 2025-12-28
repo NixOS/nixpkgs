d25160aeedbd3a59226747450c729e85ba08df91
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-28 20:05:21 +0000

    python3Packages.python-crfsuite: 0.9.11 -> 0.9.12

--- pkgs/development/python-modules/python-crfsuite/default.nix
+++ pkgs/development/python-modules/python-crfsuite/default.nix
@@ -8,13 +8,13 @@
 
 buildPythonPackage rec {
   pname = "python-crfsuite";
-  version = "0.9.11";
+  version = "0.9.12";
   format = "setuptools";
 
   src = fetchPypi {
     inherit version;
     pname = "python_crfsuite";
-    hash = "sha256-bv+WXKcFZzltgiyaNep0sPftsn2UcVJJl72r56baX1o=";
+    hash = "sha256-2zf8zDvY8MScKKdpfKecidZ7P9W/EZEihmFpJArExIA=";
   };
 
   preCheck = ''
