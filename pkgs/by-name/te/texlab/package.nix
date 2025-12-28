2ee5be1ed8fc27624cd1d6aade18dd603a5c8613
Kira Bruneau <kira.bruneau@pm.me>
2025-12-28 17:02:32 -0500

    texlab: 5.24.0 -> 5.25.0

--- pkgs/by-name/te/texlab/package.nix
+++ pkgs/by-name/te/texlab/package.nix
@@ -14,16 +14,16 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "texlab";
-  version = "5.24.0";
+  version = "5.25.0";
 
   src = fetchFromGitHub {
     owner = "latex-lsp";
     repo = "texlab";
     tag = "v${version}";
-    hash = "sha256-gwF4cBlS43u2PfOQBxD7iq2JL9tUo8TPJqR0tWNdW9k=";
+    hash = "sha256-UI88DQjLRJRPgKX3A+PoLNzgm/9uJeNERTrzh5mnAEk=";
   };
 
-  cargoHash = "sha256-9sAurVJSpLNgQvJOG7kSXHIr38MHsw3BhGAaxi9xjUE=";
+  cargoHash = "sha256-xDLKuAfLyo3V++OKb+anw9oHxehwUxqfnDCuT2GSDWw=";
 
   outputs = [ "out" ] ++ lib.optional (!isCross) "man";
 
@@ -37,7 +37,7 @@ rustPlatform.buildRustPackage rec {
   # generate the man page
   postInstall = lib.optionalString (!isCross) ''
     # TexLab builds man page separately in CI:
-    # https://github.com/latex-lsp/texlab/blob/v5.24.0/.github/workflows/publish.yml#L110-L114
+    # https://github.com/latex-lsp/texlab/blob/v5.25.0/.github/workflows/publish.yml#L110-L114
     help2man --no-info "$out/bin/texlab" > texlab.1
     installManPage texlab.1
   '';
