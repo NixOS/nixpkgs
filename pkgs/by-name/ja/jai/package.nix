6d9049d639332fd7f148f83945fb440e140bde92
Sam Estep <sam@samestep.com>
2025-12-27 22:56:11 -0500

    jai: 0.2.021 -> 0.2.024

--- pkgs/by-name/ja/jai/package.nix
+++ pkgs/by-name/ja/jai/package.nix
@@ -9,7 +9,7 @@
 let
   pname = "jai";
   minor = "2";
-  patch = "021";
+  patch = "024";
   version = "0.${minor}.${patch}";
   zipName = "jai-beta-${minor}-${patch}.zip";
   jai = stdenv.mkDerivation {
@@ -20,7 +20,7 @@ let
           nix-store --add-fixed sha256 ${zipName}
       '';
       name = zipName;
-      sha256 = "sha256-0hli9489cBtLzEcwN87sdh54hbkqtq3hCxqN9XxTX2U=";
+      sha256 = "sha256-vZonIbSxG5B/lk2YAj9lz+F3stNQn6tokhleFWnY7AI=";
     };
     nativeBuildInputs = [ unzip ];
     buildCommand = "unzip $src -d $out";
