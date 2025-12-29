eb9b80a8dd47311b49a3d6e6457bbbc792113005
Saad Nadeem <saad29nadeem@gmail.com>
2025-12-29 16:21:25 -0500

    impala: 0.6.0 -> 0.7.2

--- pkgs/by-name/im/impala/package.nix
+++ pkgs/by-name/im/impala/package.nix
@@ -5,16 +5,22 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "impala";
-  version = "0.6.0";
+  version = "0.7.2";
 
   src = fetchFromGitHub {
     owner = "pythops";
     repo = "impala";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-FU/8g2zTTHm3Sdbxt9761Z+a0zaJMdAMdHrJIwjUrYs=";
+    hash = "sha256-kDXf+zrCfsYv+5P69BiZDBqaw9SM3JPCXV7KzpIEJn0=";
   };
 
-  cargoHash = "sha256-dpTLVlDxc9eK7GwbweODoJlrBZeYwVcv1fQ2UtYbg7k=";
+  cargoHash = "sha256-Zs3x7wWbO0LL1BjEAWb1UbztJJ6K6hXxgMBynHLri8A=";
+
+  # fix for compilation of musl builds on aarch64
+  # see https://github.com/NixOS/nixpkgs/issues/145726
+  postPatch = ''
+    rm .cargo/config.toml
+  '';
 
   meta = {
     description = "TUI for managing wifi";
