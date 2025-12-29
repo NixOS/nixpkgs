061a2090b9a3bb67ce5b41895037569d24367a3b
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-29 12:59:14 +0000

    signalbackup-tools: 20251214 -> 20251229

--- pkgs/by-name/si/signalbackup-tools/package.nix
+++ pkgs/by-name/si/signalbackup-tools/package.nix
@@ -14,13 +14,13 @@
 
 stdenv.mkDerivation rec {
   pname = "signalbackup-tools";
-  version = "20251214";
+  version = "20251229";
 
   src = fetchFromGitHub {
     owner = "bepaald";
     repo = "signalbackup-tools";
     tag = version;
-    hash = "sha256-/XmrlobVbbSN94cCj/HDLLd6x2sB7A3IF/cBAvX7rSY=";
+    hash = "sha256-U94c+b3xCz9dOEXwklFDj7aZl3H2jGNqp7tso+Igf7U=";
   };
 
   nativeBuildInputs = [
