3057623b81b33d1475144e00c2783b640f6adc25
Youwes09 <yuvi.shen@outlook.com>
2025-12-29 00:11:58 -0600

    maintainers: add Youwes09

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -29040,6 +29040,11 @@
     githubId = 38934577;
     keys = [ { fingerprint = "8F5E 6C1A F909 76CA 7102 917A 8656 58ED 1FE6 1EC3"; } ];
   };
+  Youwes09 = {
+    name = "Youwes Shozikan";
+    github = "Youwes09";
+    githubId = 142514207;
+  };
   yrashk = {
     email = "yrashk@gmail.com";
     github = "yrashk";
