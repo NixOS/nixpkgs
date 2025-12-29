ebc797891830e081f846554947b802489a2f40d0
Nia <hakase@litdevs.org>
2025-12-29 14:05:47 +0100

    libretro.melondsds: init at 1.2.0

--- pkgs/applications/emulators/libretro/default.nix
+++ pkgs/applications/emulators/libretro/default.nix
@@ -106,6 +106,8 @@ lib.makeScope newScope (self: {
 
   melonds = self.callPackage ./cores/melonds.nix { };
 
+  melondsds = self.callPackage ./cores/melondsds.nix { };
+
   mesen = self.callPackage ./cores/mesen.nix { };
 
   mesen-s = self.callPackage ./cores/mesen-s.nix { };
