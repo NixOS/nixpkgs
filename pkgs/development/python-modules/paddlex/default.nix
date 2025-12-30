2d047cac05bfd4d6fe6b146c45d2deb07b8244a1
Doron Behar <doron.behar@gmail.com>
2025-12-30 13:51:00 +0200

    python3Packages.paddlex: use global python3.pkgs.gputil

--- pkgs/development/python-modules/paddlex/default.nix
+++ pkgs/development/python-modules/paddlex/default.nix
@@ -17,40 +17,13 @@
   ruamel-yaml,
   typing-extensions,
   ujson,
-  distutils,
+  gputil,
   huggingface-hub,
   modelscope,
   aistudio-sdk,
   nix-update-script,
 }:
 
-let
-  gputil = buildPythonPackage rec {
-    pname = "gputil";
-    version = "1.4.0";
-    pyproject = true;
-
-    src = fetchFromGitHub {
-      owner = "anderskm";
-      repo = "gputil";
-      tag = "v${version}";
-      hash = "sha256-iOyB653BMmDBtK1fM1ZyddjlnaypsuLMOV0sKaBt+yE=";
-    };
-
-    build-system = [ setuptools ];
-
-    dependencies = [ distutils ];
-
-    pythonImportsCheck = [ "GPUtil" ];
-
-    meta = {
-      homepage = "https://github.com/anderskm/gputil";
-      license = lib.licenses.mit;
-      description = "Getting GPU status from NVIDA GPUs using nvidia-smi";
-      changelog = "https://github.com/anderskm/gputil/releases/tag/${src.tag}";
-    };
-  };
-in
 buildPythonPackage rec {
   pname = "paddlex";
   version = "3.3.10";
