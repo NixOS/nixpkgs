1a67e1dec8d104d07d17ab2be0bb2bf7db7f198b
Doron Behar <doron.behar@gmail.com>
2025-12-30 18:45:53 +0200

    python3.pkgs.opencv-contrib-python: init at ${opencv4.version}
    Needed for the Nixpkgs equivalent of `pip install paddlex[ocr]`.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11268,6 +11268,8 @@ self: super: with self; {
 
   opencontainers = callPackage ../development/python-modules/opencontainers { };
 
+  opencv-contrib-python = callPackage ../development/python-modules/opencv-contrib-python { };
+
   opencv-python = callPackage ../development/python-modules/opencv-python { };
 
   opencv-python-headless = callPackage ../development/python-modules/opencv-python-headless { };
