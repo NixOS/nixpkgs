fac035154464d593e18818e15038250c449c3794
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-30 14:50:39 +0000

    complgen: 0.5.0 -> 0.6.0

--- pkgs/by-name/co/complgen/package.nix
+++ pkgs/by-name/co/complgen/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "complgen";
-  version = "0.5.0";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "adaszko";
     repo = "complgen";
     rev = "v${version}";
-    hash = "sha256-GgGFlrAJN9w+bsoXmVJaYUyx/ViH9m4E4EeJlmWRo6o=";
+    hash = "sha256-d5D2Vq8B3MDQEYRcoYDJ1yx1CH9PX/ipwPJD8QNBTOQ=";
   };
 
-  cargoHash = "sha256-JexvR/djdRGq3BsOWfEhFCbTe3OaP/jqQgiO+RkK1Tg=";
+  cargoHash = "sha256-8JiyVypioVM0tKl2P6QSKM/HMpJjt+tzpmSBZAucQxM=";
 
   meta = {
     description = "Generate {bash,fish,zsh} completions from a single EBNF-like grammar";
