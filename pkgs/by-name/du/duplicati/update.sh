0033813c4fdcc9c632f2314ffc271aac8c7c95bb
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-01 00:04:42 +0800

    duplicati: add missing dependency (ngclient)

--- pkgs/by-name/du/duplicati/update.sh
+++ pkgs/by-name/du/duplicati/update.sh
@@ -9,6 +9,9 @@ REPO="duplicati"
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 TARGET="$SCRIPT_DIR/package.nix"
 
+TMP=$(mktemp -d)
+trap 'rm -rf "$TMP"' EXIT
+
 TAG=$(curl ${GITHUB_TOKEN:+" -u \":$GITHUB_TOKEN\""} -s "https://api.github.com/repos/$OWNNER/$REPO/tags" |
   jq -r '.[].name' |
   grep -E '^v[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+_stable_' |
@@ -22,7 +25,35 @@ DATE=$(echo "$TAG" | cut -d_ -f3)
 HASH=$(nix-prefetch-github $OWNNER $REPO --rev "$TAG" |
   jq -r '.hash')
 
+curl -sL "https://raw.githubusercontent.com/$OWNNER/$REPO/$TAG/Duplicati/Server/webroot/ngclient/package.json" \
+  -o "$TMP/package.json"
+
+NGCLIENT_VERSION=$(
+  jq -r '.dependencies["@duplicati/ngclient"]' \
+    "$TMP/package.json" |
+    sed 's/^[^0-9]*//'
+)
+
+git clone --no-tags https://github.com/duplicati/ngclient.git "$TMP/ngclient"
+NGCLIENT_REV=$(cd "$TMP/ngclient" && git log --format="%H %s" | grep "$NGCLIENT_VERSION" | awk '{print $1}')
+
+NGCLIENT_HASH=$(
+  nix-prefetch-github $OWNNER ngclient --rev "$NGCLIENT_REV" |
+    jq -r .hash
+)
+
+echo "version=$VERSION"
+echo "channel=$CHANNEL"
+echo "date=$DATE"
+
+echo "ngclientVersion=$NGCLIENT_VERSION"
+echo "ngclientRev=$NGCLIENT_REV"
+echo "ngclientHash=$NGCLIENT_HASH"
+
 sed -i \
+  -e "/ngclientVersion = /c\  ngclientVersion = \"$NGCLIENT_VERSION\";" \
+  -e "/ngclientRev = /c\  ngclientRev = \"$NGCLIENT_REV\";" \
+  -e "/ngclientHash = /c\  ngclientHash = \"$NGCLIENT_HASH\";" \
   -e "/version = \"/c\  version = \"$VERSION\";" \
   -e "/channel = \"/c\  channel = \"$CHANNEL\";" \
   -e "/buildDate = \"/c\  buildDate = \"$DATE\";" \
