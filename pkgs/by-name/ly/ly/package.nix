a5c559e56a350026ca939460f639119520ac404c
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-31 12:35:32 +0000

    ly: 1.3.0 -> 1.3.1
    (cherry picked from commit f0cdc1d033fff25dd1eabc9ff3990564ba1d7414)

--- pkgs/by-name/ly/ly/package.nix
+++ pkgs/by-name/ly/ly/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ly";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchFromCodeberg {
     owner = "fairyglade";
     repo = "ly";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-OmbOWZZR5kvqWJkNhTkV5O2tfFj5BtJPLtINe59Y0tI=";
+    hash = "sha256-BelsR/+sfm3qdEnyf4bbadyzuUVvVPrPEhdZaNPLxiE=";
   };
 
   nativeBuildInputs = [
