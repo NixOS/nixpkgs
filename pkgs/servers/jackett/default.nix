5e60b000a96ebe26caeef2299b870131fc80331d
R. Ryantm <ryantm-bot@ryantm.com>
2025-12-31 21:17:27 +0000

    jackett: 0.24.532 -> 0.24.645

--- pkgs/servers/jackett/default.nix
+++ pkgs/servers/jackett/default.nix
@@ -11,13 +11,13 @@
 
 buildDotnetModule rec {
   pname = "jackett";
-  version = "0.24.532";
+  version = "0.24.645";
 
   src = fetchFromGitHub {
     owner = "jackett";
     repo = "jackett";
     tag = "v${version}";
-    hash = "sha256-9dLBQOAyR2oyN1lGLPN/QLZtGLASRKUXUCLCdvqOmmE=";
+    hash = "sha256-nb5WDMhba4kTGxtX4rLLZdM9iJI6dM6kPBu3hht/CYk=";
   };
 
   projectFile = "src/Jackett.Server/Jackett.Server.csproj";
