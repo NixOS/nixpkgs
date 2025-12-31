885f5af27481d84133bf1375a19d30a463d45ae4
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-31 11:06:46 +0100

    python313Packages.google-cloud-artifact-registry: remove disabled

--- pkgs/development/python-modules/google-cloud-artifact-registry/default.nix
+++ pkgs/development/python-modules/google-cloud-artifact-registry/default.nix
@@ -9,7 +9,6 @@
   protobuf,
   pytest-asyncio,
   pytestCheckHook,
-  pythonOlder,
   setuptools,
 }:
 
@@ -17,8 +16,6 @@ buildPythonPackage rec {
   pname = "google-cloud-artifact-registry";
   version = "1.18.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     pname = "google_cloud_artifact_registry";
