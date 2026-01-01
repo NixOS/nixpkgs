49171c12a15730fd32a40976daa38619de9dd844
Sigmanificient <edhyjox@gmail.com>
2026-01-01 18:09:18 +0100

    skia-aseprite: fix build

--- pkgs/by-name/sk/skia-aseprite/package.nix
+++ pkgs/by-name/sk/skia-aseprite/package.nix
@@ -42,8 +42,6 @@ clangStdenv.mkDerivation (finalAttrs: {
 
   # Using substituteInPlace because no clean upstream backport for GCC 15 exists for this version of Skia, newer versions fix this with large refactorings.
   postPatch = ''
-    substituteInPlace include/private/SkSLProgramKind.h \
-      --replace-fail "#include <cinttypes>" "#include <cinttypes>
     #include <cstdint>"
     substituteInPlace src/sksl/transform/SkSLTransform.h \
       --replace-fail "#include <vector>" "#include <vector>
