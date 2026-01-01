bc38aa55f5d7bdbbdae658b576da0890217067d0
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2026-01-01 15:53:03 +0800

    maintainers: add sempiternal-aurora

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23803,6 +23803,11 @@
     githubId = 33031;
     name = "Greg Pfeil";
   };
+  sempiternal-aurora = {
+    github = "sempiternal-aurora";
+    githubId = 78790545;
+    name = "Myria Sarvay";
+  };
   semtexerror = {
     email = "github@spampert.com";
     github = "SemtexError";
