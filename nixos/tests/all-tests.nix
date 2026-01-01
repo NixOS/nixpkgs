4640b5057278adf5ad31d48fd39ad723dedb21a2
Hythera <87016780+Hythera@users.noreply.github.com>
2026-01-01 19:34:00 +0100

    nixos/tests/waterfox: init
    Co-authored-by: Defelo <mail@defelo.de>

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -2034,6 +2034,10 @@ in
   wasabibackend = runTest ./wasabibackend.nix;
   wastebin = runTest ./wastebin.nix;
   watchdogd = runTest ./watchdogd.nix;
+  waterfox = runTest {
+    imports = [ ./firefox.nix ];
+    _module.args.firefoxPackage = pkgs.waterfox;
+  };
   watt = runTest ./watt.nix;
   webhook = runTest ./webhook.nix;
   weblate = runTest ./web-apps/weblate.nix;
