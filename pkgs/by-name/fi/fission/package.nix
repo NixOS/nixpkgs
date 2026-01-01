144a6b2aa43faeb14ade0bcc1b6b4069d9522097
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-01 11:50:01 +0000

    fission: 1.21.0 -> 1.22.0

--- pkgs/by-name/fi/fission/package.nix
+++ pkgs/by-name/fi/fission/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "fission";
-  version = "1.21.0";
+  version = "1.22.0";
 
   src = fetchFromGitHub {
     owner = "fission";
     repo = "fission";
     rev = "v${version}";
-    hash = "sha256-Tl7aKibVbNAKOa1tycKtEzdJ8rJHBMa8PTUm0i7DKA4=";
+    hash = "sha256-5+TyOBHHDhHtAJUtrcWUCRbewGODsut/w3chrmL+dis=";
   };
 
-  vendorHash = "sha256-PhB6zR/dXnOCHJiJ/EjVOD26SubaAITRm61XOvULerU=";
+  vendorHash = "sha256-y5h1lMq99gWhB9T5e8b2t9USgKc2pv+FMgl9wva8t28=";
 
   ldflags = [
     "-s"
