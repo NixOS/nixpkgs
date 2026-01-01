c65d05815fd7b42b76fcf0f3e0bde33344219943
RoGreat <roguegreat@gmail.com>
2025-12-31 21:29:37 -0600

    pascube: 1.5.1 -> 1.7.0

--- pkgs/by-name/pa/pascube/package.nix
+++ pkgs/by-name/pa/pascube/package.nix
@@ -1,85 +1,90 @@
 {
-  autoPatchelfHook,
-  copyDesktopItems,
+  clangStdenv,
   fetchFromGitHub,
   fpc,
   lazarus-qt6,
   lib,
-  libGLU,
-  makeDesktopItem,
+  libx11,
+  makeWrapper,
   nix-update-script,
   qt6Packages,
-  stdenv,
+  SDL2,
+  vulkan-loader,
+  zlib,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
+clangStdenv.mkDerivation (finalAttrs: {
   pname = "pascube";
-  version = "1.5.1";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "benjamimgois";
     repo = "pascube";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-djkrMgX3RTTXSLISYpBfdyCIh3/WWODxd473M53iFKE=";
+    tag = finalAttrs.version;
+    hash = "sha256-qKjOA5/l2trQC238WheeOzqbpltjkwksqzMtcfw7ci0=";
   };
 
   nativeBuildInputs = [
-    autoPatchelfHook
-    copyDesktopItems
     fpc
     lazarus-qt6
+    makeWrapper
     qt6Packages.wrapQtAppsHook
   ];
 
   buildInputs = [
     qt6Packages.libqtpas
     qt6Packages.qtbase
+    SDL2
   ];
 
-  runtimeDependencies = [
-    libGLU
-  ];
-
-  buildPhase = ''
-    runHook preBuild
-    HOME=$(mktemp -d) lazbuild \
-      --lazarusdir=${lazarus-qt6}/share/lazarus \
-      --widgetset=qt6 \
-      pascube.lpi
-    runHook postBuild
-  '';
+  buildPhase =
+    let
+      clangBuild =
+        if clangStdenv.hostPlatform.system == "i386-linux" then
+          "clang -c -g -gdwarf-2 -masm=intel -Wno-c++2b-extensions -Wno-return-type -Wno-deprecated -O4 -march=haswell -D linux -fverbose-asm -fno-builtin pasvulkan/src/lzma_c/LzmaDec.c -o pasvulkan/src/lzma_c/lzmadec_linux_x86_32.o"
+        else if clangStdenv.hostPlatform.system == "x86_64-linux" then
+          "clang -c -g -gdwarf-2 -masm=intel -Wno-c++2b-extensions -Wno-return-type -Wno-deprecated -O4 -march=haswell -D linux -fverbose-asm -fno-builtin pasvulkan/src/lzma_c/LzmaDec.c -o pasvulkan/src/lzma_c/lzmadec_linux_x86_64.o"
+        else if clangStdenv.hostPlatform.system == "aarch64-linux" then
+          "clang -c -g -gdwarf-2 -Wno-c++2b-extensions -Wno-return-type -Wno-deprecated -O4 -D linux -fverbose-asm -fno-builtin pasvulkan/src/lzma_c/LzmaDec.c -o pasvulkan/src/lzma_c/lzmadec_linux_aarch64.o"
+        else if clangStdenv.hostPlatform.system == "armv7-linux" then
+          "clang -c -mfloat-abi=hard -g -gdwarf-2 -Wno-c++2b-extensions -Wno-return-type -Wno-deprecated -O4 -D linux -fverbose-asm -fno-builtin pasvulkan/src/lzma_c/LzmaDec.c -o pasvulkan/src/lzma_c/lzmadec_linux_arm7.o"
+        else
+          throw "Unsupported platform: ${clangStdenv.hostPlatform.system}";
+    in
+    ''
+      runHook preBuild
+      ${clangBuild}
+      HOME=$(mktemp -d) lazbuild \
+        --lazarusdir=${lazarus-qt6}/share/lazarus \
+        --widgetset=qt6 \
+        pascube.lpi
+      runHook postBuild
+    '';
 
   installPhase = ''
     runHook preInstall
     install -Dm755 pascube $out/bin/pascube
+    mkdir -p $out/share/pascube
+    cp -a assets $out/share/pascube
+    install -Dm644 data/pascube.desktop $out/share/applications/pascube.desktop
     for sz in 128x128 256x256 512x512; do
       install -Dm644 "data/icons/''${sz}/pascube.png" \
         "$out/share/icons/hicolor/''${sz}/apps/pascube.png"
     done
-    install -Dm644 "data/skybox.png" "$out/share/pascube/skybox.png"
     runHook postInstall
   '';
 
-  desktopItems = [
-    (makeDesktopItem {
-      name = "pascube";
-      desktopName = "pasCube";
-      comment = finalAttrs.meta.description;
-      exec = finalAttrs.meta.mainProgram;
-      icon = "pascube";
-      terminal = false;
-      categories = [
-        "Graphics"
-        "Education"
-        "Qt"
-      ];
-    })
-  ];
-
-  preFixup = ''
-    qtWrapperArgs+=(
-      --set QT_QPA_PLATFORM xcb
-    )
+  fixupPhase = ''
+    runHook preFixup
+    wrapProgram $out/bin/pascube --prefix LD_LIBRARY_PATH : ${
+      lib.makeLibraryPath [
+        libx11
+        SDL2
+        vulkan-loader
+        zlib
+      ]
+    }
+    runHook postFixup
   '';
 
   passthru.updateScript = nix-update-script { };
@@ -87,7 +92,7 @@ stdenv.mkDerivation (finalAttrs: {
   meta = {
     description = "Simple OpenGL spinning cube written in Pascal";
     homepage = "https://github.com/benjamimgois/pascube";
-    changelog = "https://github.com/benjamimgois/pascube/releases/tag/v${finalAttrs.version}";
+    changelog = "https://github.com/benjamimgois/pascube/releases/tag/${finalAttrs.version}";
     license = lib.licenses.gpl2Plus;
     maintainers = with lib.maintainers; [ RoGreat ];
     mainProgram = "pascube";
