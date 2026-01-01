bbd2f04460e6eb4d85c0b858ded433cda3f16164
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2026-01-01 19:28:16 +1100

    coreutils: fix binlore synthesis when selinuxSupport is false

--- pkgs/tools/misc/coreutils/default.nix
+++ pkgs/tools/misc/coreutils/default.nix
@@ -237,8 +237,8 @@ stdenv.mkDerivation rec {
       # binlore only spots exec in runcon on some platforms (i.e., not
       # darwin; see comment on inverse case below)
       binlore.out = binlore.synthesize coreutils ''
-        execer can bin/{chroot,env,install,nice,nohup,runcon,sort,split,stdbuf,timeout}
-        execer cannot bin/{[,b2sum,base32,base64,basename,basenc,cat,chcon,chgrp,chmod,chown,cksum,comm,cp,csplit,cut,date,dd,df,dir,dircolors,dirname,du,echo,expand,expr,factor,false,fmt,fold,groups,head,hostid,id,join,kill,link,ln,logname,ls,md5sum,mkdir,mkfifo,mknod,mktemp,mv,nl,nproc,numfmt,od,paste,pathchk,pinky,pr,printenv,printf,ptx,pwd,readlink,realpath,rm,rmdir,seq,sha1sum,sha224sum,sha256sum,sha384sum,sha512sum,shred,shuf,sleep,stat,stty,sum,sync,tac,tail,tee,test,touch,tr,true,truncate,tsort,tty,uname,unexpand,uniq,unlink,uptime,users,vdir,wc,who,whoami,yes}
+        execer can bin/{chroot,env,install,nice,nohup,${optionalString selinuxSupport "runcon,"}sort,split,stdbuf,timeout}
+        execer cannot bin/{[,b2sum,base32,base64,basename,basenc,cat,${optionalString selinuxSupport "chcon,"}chgrp,chmod,chown,cksum,comm,cp,csplit,cut,date,dd,df,dir,dircolors,dirname,du,echo,expand,expr,factor,false,fmt,fold,groups,head,hostid,id,join,kill,link,ln,logname,ls,md5sum,mkdir,mkfifo,mknod,mktemp,mv,nl,nproc,numfmt,od,paste,pathchk,pinky,pr,printenv,printf,ptx,pwd,readlink,realpath,rm,rmdir,seq,sha1sum,sha224sum,sha256sum,sha384sum,sha512sum,shred,shuf,sleep,stat,stty,sum,sync,tac,tail,tee,test,touch,tr,true,truncate,tsort,tty,uname,unexpand,uniq,unlink,uptime,users,vdir,wc,who,whoami,yes}
       '';
     }
     // optionalAttrs (singleBinary == false) {
@@ -246,9 +246,11 @@ stdenv.mkDerivation rec {
       # darwin; I have a note that the behavior may need selinux?).
       # hard-set it so people working on macOS don't miss cases of
       # runcon until ofBorg fails.
-      binlore.out = binlore.synthesize coreutils ''
-        execer can bin/runcon
-      '';
+      binlore.out = binlore.synthesize coreutils (
+        optionalString selinuxSupport ''
+          execer can bin/runcon
+        ''
+      );
     };
 
   meta = {
