c63bb6eb7c299d850be63925ab794df7be6728ca
Justin ! <just1602@riseup.net>
2026-01-01 22:29:41 -0500

    treewide: fix `rec-set-dynamic-attrs`
    Lix will soon warn, and in the future, error on `rec-set-dynamic-attrs`.
    This commit fix where attrs are dynamically set in a recursive function
    currently present in nixpkgs.

--- pkgs/applications/editors/sublime/4/common.nix
+++ pkgs/applications/editors/sublime/4/common.nix
@@ -142,15 +142,13 @@ stdenv.mkDerivation rec {
 
   dontUnpack = true;
 
-  ${primaryBinary} = binaryPackage;
-
   nativeBuildInputs = [
     makeWrapper
   ];
 
   installPhase = ''
     mkdir -p "$out/bin"
-    makeWrapper "''$${primaryBinary}/${primaryBinary}" "$out/bin/${primaryBinary}"
+    makeWrapper "${binaryPackage}/${primaryBinary}" "$out/bin/${primaryBinary}"
   ''
   + builtins.concatStringsSep "" (
     map (binaryAlias: "ln -s $out/bin/${primaryBinary} $out/bin/${binaryAlias}\n") primaryBinaryAliases
@@ -159,18 +157,20 @@ stdenv.mkDerivation rec {
     mkdir -p "$out/share/applications"
 
     substitute \
-      "''$${primaryBinary}/${primaryBinary}.desktop" \
+      "${binaryPackage}/${primaryBinary}.desktop" \
       "$out/share/applications/${primaryBinary}.desktop" \
       --replace-fail "/opt/${primaryBinary}/${primaryBinary}" "${primaryBinary}"
 
-    for directory in ''$${primaryBinary}/Icon/*; do
+    for directory in ${binaryPackage}/Icon/*; do
       size=$(basename $directory)
       mkdir -p "$out/share/icons/hicolor/$size/apps"
-      ln -s ''$${primaryBinary}/Icon/$size/* $out/share/icons/hicolor/$size/apps
+      ln -s ${binaryPackage}/Icon/$size/* $out/share/icons/hicolor/$size/apps
     done
   '';
 
   passthru = {
+    unwrapped = binaryPackage;
+
     updateScript =
       let
         script = writeShellScript "${packageAttribute}-update-script" ''
@@ -191,7 +191,7 @@ stdenv.mkDerivation rec {
           fi
 
           for platform in ${lib.escapeShellArgs meta.platforms}; do
-              update-source-version "${packageAttribute}.${primaryBinary}" "$latestVersion" --ignore-same-version --file="$versionFile" --version-key=buildVersion --source-key="sources.$platform"
+              update-source-version "${packageAttribute}".unwrapped "$latestVersion" --ignore-same-version --file="$versionFile" --version-key=buildVersion --source-key="sources.$platform"
           done
         '';
       in
