0cec848adc249ef19a459d76d859d520fbdd3846
Marcel Müller <neikos@neikos.email>
2026-01-02 13:44:05 +0100

    build-support/vm: Fix missing sleep command
    The sleep command is not a bash built-in, so when a VM does not have
    coreutils in their PATH this script will break. The fix is to refer to
    it directly.
    
    Signed-off-by: Marcel Müller <neikos@neikos.email>

--- pkgs/build-support/vm/default.nix
+++ pkgs/build-support/vm/default.nix
@@ -298,8 +298,8 @@ rec {
       ${pkgs.virtiofsd}/bin/virtiofsd --xattr --socket-path virtio-xchg.sock --sandbox none --seccomp none --shared-dir xchg &
 
       # Wait until virtiofsd has created these sockets to avoid race condition.
-      until [[ -e virtio-store.sock ]]; do sleep 1; done
-      until [[ -e virtio-xchg.sock ]]; do sleep 1; done
+      until [[ -e virtio-store.sock ]]; do ${coreutils}/bin/sleep 1; done
+      until [[ -e virtio-xchg.sock ]]; do ${coreutils}/bin/sleep 1; done
 
       ${qemuCommand}
       EOF
