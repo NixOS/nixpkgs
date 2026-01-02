6e6773c862873cc2b8238f72e7b282ca45790bda
Ming-Chuan <ming@culpring.com>
2026-01-02 19:42:22 +0800

    python3Packages.streamcontroller-streamdeck: init at 0.1.5
    It's a fork of streamdeck package made by streamcontroller author,
    required by streamcontroller.
    
    pkgs/development/python-modules/streamcontroller-streamdeck/default.nix
    is copied from pkgs/development/python-modules/streamdeck/default.nix
    with minor modification.
    
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18212,6 +18212,10 @@ self: super: with self; {
     callPackage ../development/python-modules/streamcontroller-plugin-tools
       { };
 
+  streamcontroller-streamdeck =
+    callPackage ../development/python-modules/streamcontroller-streamdeck
+      { };
+
   streamdeck = callPackage ../development/python-modules/streamdeck { };
 
   streaming-form-data = callPackage ../development/python-modules/streaming-form-data { };
