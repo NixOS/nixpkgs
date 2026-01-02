9f3c18bdea1ea5e22189f14e5e4e15b02fa25ec5
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-02 13:37:25 +0100

    python313Packages.microsoft-kiota-serialization-multipart: remove disabled
    Removed pythonOlder restriction for version 3.8.

--- pkgs/development/python-modules/microsoft-kiota-serialization-multipart/default.nix
+++ pkgs/development/python-modules/microsoft-kiota-serialization-multipart/default.nix
@@ -8,7 +8,6 @@
   pytest-asyncio,
   pytest-mock,
   pytestCheckHook,
-  pythonOlder,
   gitUpdater,
 }:
 
@@ -16,8 +15,6 @@ buildPythonPackage rec {
   pname = "microsoft-kiota-serialization-multipart";
   version = "1.9.8";
   pyproject = true;
-
-  disabled = pythonOlder "3.8";
 
   src = fetchFromGitHub {
     owner = "microsoft";
