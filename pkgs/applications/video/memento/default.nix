c4fc17efa7ca403cd136db1ab345881e3300a44f
teto <886074+teto@users.noreply.github.com>
2026-01-02 23:18:31 +0100

    memento: 1.6.0 -> 1.7.0
    Added optional support for OCR. Off by default because it doubles
    closure size plus one needs to download the model imperatively.
    
    Some stats:
    1.6 weighs: /nix/store/86kmz0zc87hg1v3nwxgf0zd2839d539c-memento-1.6.0   1.3 GiB
    1.7 without Ocr : /nix/store/ygd1zr809v5apyjvmydw6nccx1ycc2mh-memento-1.7.0    1.3 GiB
    1.7 with ocr: /nix/store/19g1qk5g9kirsqgw1s0nasb676hj8qia-memento-1.7.0   3.3 GiB
    
    Amon the novelties, it now builds with system qcoro.

--- pkgs/applications/video/memento/default.nix
+++ pkgs/applications/video/memento/default.nix
@@ -9,23 +9,58 @@
   # before that => zeal
   sqlite,
   json_c,
-  mecab,
   libzip,
   mpv,
   yt-dlp,
-  # optional
   makeWrapper,
+  qcoro,
+  withOcr ? false,
+  python3,
 }:
+let
+  libmocr = stdenv.mkDerivation {
+    pname = "libmocr";
+    version = "unstable-2023-11-15"; # Placeholder version
+
+    src = fetchFromGitHub {
+      owner = "ripose-jp";
+      repo = "libmocr";
+      rev = "24ec081102208d0ff6954c3003df2fb691713bd5";
+      hash = "sha256-58W/LBJ+wIxQtutoXqq6TzpExbUBV3NfAfXTIl43ARg=";
+    };
+
+    nativeBuildInputs = [ cmake ];
+    buildInputs = [
+      qt6.qtbase
+      python3
+    ];
+    dontWrapQtApps = true;
+    meta = with lib; {
+      description = "A library for Manga OCR";
+      homepage = "https://github.com/ripose-jp/libmocr";
+      license = licenses.gpl2;
+      platforms = platforms.linux;
+    };
+  };
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "memento";
-  version = "1.6.0";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "ripose-jp";
     repo = "Memento";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-IvzvlToSyA20FWU0x+wgE3rT0dYbuY6xyaGgz1D1f6Q=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-6ipzorykt9GoGTHTTLCyDf7vXx9mT5AITKA9pyQ3GwI=";
   };
+
+  cmakeFlags = [
+    (lib.cmakeBool "SYSTEM_QCORO" true)
+    (lib.cmakeBool "SYSTEM_MOCR" true)
+  ]
+  ++ lib.optionals withOcr [
+    (lib.cmakeBool "OCR_SUPPORT" true)
+  ];
 
   nativeBuildInputs = [
     cmake
@@ -40,15 +75,26 @@ stdenv.mkDerivation (finalAttrs: {
     sqlite
     json_c
     libzip
-    mecab
-  ];
+    qcoro
+  ]
+
+  ++ lib.optionals withOcr [ libmocr ];
 
   propagatedBuildInputs = [ mpv ];
 
-  preFixup = ''
-    wrapProgram "$out/bin/memento" \
-      --prefix PATH : "${yt-dlp}/bin" \
-  '';
+  preFixup =
+    let
+      pyEnv = python3.withPackages (p: [
+        p.manga-ocr
+        p.jaconv
+      ]);
+    in
+    ''
+      wrapProgram "$out/bin/memento" \
+        --prefix PATH : "${yt-dlp}/bin" ${lib.optionalString withOcr "--suffix PYTHONPATH : \"${pyEnv}/${pyEnv.sitePackages}\""}
+    '';
+
+  passthru.libmocr = libmocr;
 
   meta = {
     description = "Mpv-based video player for studying Japanese";
