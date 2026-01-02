0ad6c9b78a363e976191dfcae373608f97438331
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-01-02 09:12:07 +0100

    ocamlPackages.cryptokit: 1.20.1 → 1.21.1
    ocamlPackages.gapi-ocaml: 0.4.7 → 0.4.8

--- pkgs/development/ocaml-modules/gapi-ocaml/default.nix
+++ pkgs/development/ocaml-modules/gapi-ocaml/default.nix
@@ -10,17 +10,20 @@
   ounit2,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "gapi-ocaml";
-  version = "0.4.7";
-
-  minimalOCamlVersion = "4.08";
+  version = if lib.versionAtLeast cryptokit.version "1.21" then "0.4.8" else "0.4.7";
 
   src = fetchFromGitHub {
     owner = "astrada";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-uQJfrgF0oafURlamHslt9hX9MP4vFeVqDhuX7T/kjiY=";
+    repo = "gapi-ocaml";
+    tag = "v${finalAttrs.version}";
+    hash =
+      {
+        "0.4.7" = "sha256-uQJfrgF0oafURlamHslt9hX9MP4vFeVqDhuX7T/kjiY=";
+        "0.4.8" = "sha256-RvHcse3ech8BwnR0Kd1oE5ycAdSBpeQ0IGAp9egFbBY=";
+      }
+      ."${finalAttrs.version}";
   };
 
   nativeBuildInputs = [ cppo ];
@@ -41,4 +44,4 @@ buildDunePackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ bennofs ];
   };
-}
+})
