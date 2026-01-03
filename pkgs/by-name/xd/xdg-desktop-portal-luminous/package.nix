1eadfd16c5bc2d72ef5e57fc3fe912c9afb9664d
hotburger <33610620+hotburger@users.noreply.github.com>
2026-01-02 18:50:17 -0800

    xdg-desktop-portal-luminous: 0.1.11 -> 0.1.13

--- pkgs/by-name/xd/xdg-desktop-portal-luminous/package.nix
+++ pkgs/by-name/xd/xdg-desktop-portal-luminous/package.nix
@@ -9,9 +9,10 @@
   cargo,
   rustPlatform,
   xdg-desktop-portal,
-  slurp,
   cairo,
   pango,
+  libgbm,
+  libGL,
   libxkbcommon,
   glib,
   pipewire,
@@ -20,18 +21,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xdg-desktop-portal-luminous";
-  version = "0.1.11";
+  version = "0.1.13";
 
   src = fetchFromGitHub {
     owner = "waycrate";
     repo = "xdg-desktop-portal-luminous";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-WxkCZwV4zuDvl0n4Qnanh/eSFTQu+8J1zMtBSTI2hM8=";
+    hash = "sha256-e5gW5oW9rSGzDlcBJ0Lg9rl4v6vULX2j2F7/+esC1lE=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-HUImAQ2lDqrLYh+YDHjXNTHZiTthocB5U5L4gBmIGQQ=";
+    hash = "sha256-+7rnlGkWG4MzxbXskNAd+awabAdC7XqcacX5AEmutlM=";
   };
 
   nativeBuildInputs = [
@@ -46,11 +47,12 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = [
     xdg-desktop-portal
-    slurp
     cairo
     pango
     glib
     pipewire
+    libgbm
+    libGL
     libxkbcommon
   ];
 
