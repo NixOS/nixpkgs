0c46b4f15d54a89ff716ebfe00b1eb0a894bf3e8
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-03 00:53:57 +0000

    python3Packages.nrgkick-api: 1.3.1 -> 1.4.0

--- pkgs/development/python-modules/nrgkick-api/default.nix
+++ pkgs/development/python-modules/nrgkick-api/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "nrgkick-api";
-  version = "1.3.1";
+  version = "1.4.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "andijakl";
     repo = "nrgkick-api";
     tag = "v${version}";
-    hash = "sha256-aVYDyLeMkyagPogP337bO88iwtt/BNS2nEzFv0l4TSA=";
+    hash = "sha256-Azk3v4cNBdbwIssb1Eu8z4kEuzQqFTYy4PBbpu9MdhE=";
   };
 
   build-system = [ setuptools ];
