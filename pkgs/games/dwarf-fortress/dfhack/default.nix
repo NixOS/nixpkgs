e340717729724e95b368eb1bd29ab5770935f7c5
DroidCoil <168860683+DroidCoil@users.noreply.github.com>
2026-01-04 04:01:17 -0800

    Update default.nix

--- pkgs/games/dwarf-fortress/dfhack/default.nix
+++ pkgs/games/dwarf-fortress/dfhack/default.nix
@@ -164,6 +164,8 @@ stdenv.mkDerivation {
       --replace-fail "cmake_minimum_required(VERSION 2.8)" "cmake_minimum_required(VERSION 3.10)"
     substituteInPlace depends/googletest/CMakeLists.txt \
       --replace-fail "cmake_minimum_required(VERSION 2.8.8)" "cmake_minimum_required(VERSION 3.10)"
+    substituteInPlace depends/googletest/googletest/CMakeLists.txt \
+      --replace-fail "cmake_minimum_required(VERSION 2.6.4)" "cmake_minimum_required(VERSION 3.10)"
     substituteInPlace depends/googletest/googlemock/CMakeLists.txt \
       --replace-fail "cmake_minimum_required(VERSION 2.6.4)" "cmake_minimum_required(VERSION 3.10)"
   '';
