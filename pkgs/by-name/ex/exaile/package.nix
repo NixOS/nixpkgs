ea91e852b9e5803e112697c6c178aa4e9c0cf631
ryneeverett <ryneeverett@gmail.com>
2026-01-04 13:09:52 -0500

    exaile: Fix translationSupport feature flag
    Setting this flag to false had no effect, which I hadn't realized
    because gettext seems to be in the build environment by default.
    Modifying the make target is necessary to actually build without
    translations.
    
    Also, remove PYTHONPATH patch which is no longer necessary for tests.

--- pkgs/by-name/ex/exaile/package.nix
+++ pkgs/by-name/ex/exaile/package.nix
@@ -99,7 +99,6 @@ stdenv.mkDerivation rec {
 
   doCheck = true;
   preCheck = ''
-    substituteInPlace Makefile --replace "PYTHONPATH=$(shell pwd)" "PYTHONPATH=$PYTHONPATH:$(shell pwd)"
     export PYTEST="py.test"
     export XDG_CACHE_HOME=$(mktemp -d)
   '';
@@ -120,4 +119,10 @@ stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ ryneeverett ];
     platforms = lib.platforms.all;
   };
+}
+// lib.optionalAttrs translationSupport {
+  preBuild = ''
+    buildFlagsArray+=(all_no_locale)
+  '';
+  installTargets = "install_no_locale";
 }
