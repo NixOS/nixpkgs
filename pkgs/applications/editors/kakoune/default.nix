fb30e60e5f864f5ad8c7a3cbbf2de3053c22b80a
An Nguyen <dephiros@gmail.com>
2026-01-05 13:17:12 +0000

    kakoune: fix emoji display on darwin
    macOS libc has outdated Unicode tables (pre-2017) causing:
    - wcwidth() to return -1 for modern emoji (should return 2)
    - iswprint() to return false for newer emoji (should return true)
    
    This results in display corruption when editing files with emoji.
    
    Bundle termux/wcwidth (Unicode 15) on Darwin only. The wcwidth.o object
    file is compiled separately and linked into the final binary, avoiding
    symbol collision by renaming wcwidth to mk_wcwidth.
    
    Upstream has declined to bundle a Unicode database:
    https://github.com/mawww/kakoune/issues/3059#issuecomment-632669848
    
    Fixes: https://github.com/mawww/kakoune/issues/3059
    Fixes: https://github.com/mawww/kakoune/issues/4257

--- pkgs/applications/editors/kakoune/default.nix
+++ pkgs/applications/editors/kakoune/default.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchurl,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -14,6 +15,17 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-AJvh6NVFpbPsyzfeii/5muE+i4TwfxhwhDVgMLYOJCM=";
   };
 
+  # On Darwin, bundle termux/wcwidth for proper Unicode 15 emoji support.
+  # macOS libc has outdated Unicode tables causing wcwidth() to return -1
+  # for modern emoji, resulting in display corruption.
+  # See: https://github.com/mawww/kakoune/issues/3059
+  patches = lib.optionals stdenv.hostPlatform.isDarwin [ ./darwin-wcwidth.patch ];
+
+  wcwidth = fetchurl {
+    url = "https://raw.githubusercontent.com/termux/wcwidth/911263c514237997c9c590517672c6ea729388dc/wcwidth.c";
+    hash = "sha256-AMo/+xE6Kju6tK8OK5RaLT44/GFH/YfnGTcK6mqybCU=";
+  };
+
   makeFlags = [
     "debug=no"
     "PREFIX=${placeholder "out"}"
@@ -21,11 +33,20 @@ stdenv.mkDerivation (finalAttrs: {
 
   postPatch = ''
     echo "v${finalAttrs.version}" >.version
+  ''
+  + lib.optionalString stdenv.hostPlatform.isDarwin ''
+    # Add termux wcwidth for Unicode 15 emoji support
+    cp ${finalAttrs.wcwidth} src/wcwidth.c
   '';
 
   enableParallelBuilding = true;
   preBuild = ''
     appendToVar makeFlags "CXX=$CXX"
+  ''
+  + lib.optionalString stdenv.hostPlatform.isDarwin ''
+    # Compile wcwidth.c with function renamed to mk_wcwidth
+    $CC -c -o src/wcwidth.o src/wcwidth.c -Dwcwidth=mk_wcwidth
+    appendToVar makeFlags "LDFLAGS=src/wcwidth.o"
   '';
 
   doInstallCheck = true;
