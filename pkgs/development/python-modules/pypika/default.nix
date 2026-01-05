3bc9bc7e922df105325b148b0e15552b20b15d63
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-05 16:55:25 +0100

    python314Packages.pypika: modernize

--- pkgs/development/python-modules/pypika/default.nix
+++ pkgs/development/python-modules/pypika/default.nix
@@ -4,30 +4,35 @@
   fetchFromGitHub,
   parameterized,
   unittestCheckHook,
+  setuptools,
 }:
-buildPythonPackage rec {
+
+buildPythonPackage (finalAttrs: {
   pname = "pypika";
   version = "0.49.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "kayak";
     repo = "pypika";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-Lawsc19sJ3U7rCOnYvDWhWqK/J+Hd3zKG6TrhDsTtVs=";
   };
 
-  pythonImportsCheck = [ "pypika" ];
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [
     parameterized
     unittestCheckHook
   ];
 
+  pythonImportsCheck = [ "pypika" ];
+
   meta = {
     description = "Python SQL query builder";
     homepage = "https://github.com/kayak/pypika";
+    changelog = "https://github.com/kayak/pypika/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
-}
+})
