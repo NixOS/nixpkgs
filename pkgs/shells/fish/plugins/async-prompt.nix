745bb065e6090c002f967022e8532b13b34bc921
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-05 14:10:43 +0000

    fishPlugins.async-prompt: 1.2.0 -> 1.3.0

--- pkgs/shells/fish/plugins/async-prompt.nix
+++ pkgs/shells/fish/plugins/async-prompt.nix
@@ -6,13 +6,13 @@
 
 buildFishPlugin rec {
   pname = "async-prompt";
-  version = "1.2.0";
+  version = "1.3.0";
 
   src = fetchFromGitHub {
     owner = "acomagu";
     repo = "fish-async-prompt";
     rev = "v${version}";
-    hash = "sha256-B7Ze0a5Zp+5JVsQUOv97mKHh5wiv3ejsDhJMrK7YOx4=";
+    hash = "sha256-HWW9191RP//48HkAHOZ7kAAAPSBKZ+BW2FfCZB36Y+g=";
   };
 
   meta = {
