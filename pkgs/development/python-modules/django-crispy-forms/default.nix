9ac15e28aa526fb0871ec6904cf715d20a6b3172
Bruno BELANYI <bruno@belanyi.fr>
2026-01-05 12:36:38 +0000

    python3Packages.django-crispy-forms: use finalAttrs

--- pkgs/development/python-modules/django-crispy-forms/default.nix
+++ pkgs/development/python-modules/django-crispy-forms/default.nix
@@ -8,7 +8,7 @@
   pytest-django,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "django-crispy-forms";
   version = "2.5";
   format = "pyproject";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "django-crispy-forms";
     repo = "django-crispy-forms";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-UZw860dOmQOAHcOPn5JO5OPe0kei4Mivy5FTh25Zo1s=";
   };
 
@@ -49,4 +49,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ambroisie ];
   };
-}
+})
