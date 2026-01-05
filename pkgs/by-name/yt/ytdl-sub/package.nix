218ba89a7bc86df023e3598fc169fec644940878
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-05 03:32:31 +0000

    ytdl-sub: 2025.12.30 -> 2025.12.31.post5

--- pkgs/by-name/yt/ytdl-sub/package.nix
+++ pkgs/by-name/yt/ytdl-sub/package.nix
@@ -9,14 +9,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "ytdl-sub";
-  version = "2025.12.30";
+  version = "2025.12.31.post5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jmbannon";
     repo = "ytdl-sub";
     tag = version;
-    hash = "sha256-MRDLtrfxGJIWz5yk4tW4MMSY9muhgZz0w8+1XWmSAks=";
+    hash = "sha256-Ksm3aXqE+rhuc9nDN4uFqePrrIonvPTSz+J6WkxlnTg=";
   };
 
   postPatch = ''
