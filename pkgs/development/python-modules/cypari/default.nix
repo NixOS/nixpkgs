38deb855b820c74183bf1307de041dbb94925bae
Alex Epelde <alex@epelde.net>
2026-01-05 17:12:40 -0600

    python3Packages.cypari: 2.5.5 -> 2.5.6-unstable-2026-01-05

--- pkgs/development/python-modules/cypari/default.nix
+++ pkgs/development/python-modules/cypari/default.nix
@@ -3,7 +3,6 @@
   fetchFromGitHub,
   python,
   buildPythonPackage,
-  fetchpatch,
 
   # build-time dependencies
   setuptools,
@@ -16,30 +15,17 @@
   pariStatic_2_15,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "cypari";
-  version = "2.5.5";
+  version = "2.5.6-unstable-2026-01-05";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "3-manifolds";
     repo = "CyPari";
-    tag = "${version}_as_released";
-    hash = "sha256-RJ9O1KsDHmMkTCIFUrcSUkA5ijTsxmoI939QCsCib0Y=";
+    rev = "d6cb914d2bdc74a51cc2a9136204ebf47b3e7369";
+    hash = "sha256-p0UuKjcRjX6WBkZKpHkmynUtk3z81XtHyxqx0myza7U=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "support-aarch64-linux.patch";
-      url = "https://github.com/3-manifolds/CyPari/commit/6197171b52ee4f44a4954ddd0e2e36769b189dee.patch";
-      hash = "sha256-j2P7DEGD2B8q9Hh4G2mQng76fQdUpeAdFYoTD7Ui/Dk=";
-    })
-    (fetchpatch {
-      name = "fix-build-with-cython-3_1.patch";
-      url = "https://github.com/3-manifolds/CyPari/compare/622e112ffcf0383e2110954ff3ac3c42c006ebe1...50bcbd2b39177f5e4c5a3551a8a14f75ab05a5d6.patch";
-      hash = "sha256-6ayvtHMS3YtzzklHaaLzl9d4zHJhm0lVZQZFS9ykFY4=";
-    })
-  ];
 
   preBuild = ''
     mkdir libcache
@@ -67,11 +53,11 @@ buildPythonPackage rec {
   meta = {
     description = "Sage's PARI extension, modified to stand alone";
     homepage = "https://github.com/3-manifolds/CyPari";
+    changelog = "https://github.com/3-manifolds/CyPari/releases/tag/2.5.6_as_released";
     license = lib.licenses.gpl2Plus;
     maintainers = with lib.maintainers; [
       noiioiu
       alejo7797
     ];
-    changelog = "https://github.com/3-manifolds/CyPari/releases/tag/${src.tag}";
   };
 }
