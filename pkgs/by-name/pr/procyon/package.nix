271c45ce847977f28eb800e71f0c0cc94b84e006
linsui <36977733+linsui@users.noreply.github.com>
2026-01-06 16:57:47 +0800

    procyon: build from source

--- pkgs/by-name/pr/procyon/package.nix
+++ pkgs/by-name/pr/procyon/package.nix
@@ -1,39 +1,73 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  runtimeShell,
-  jre_headless,
+  fetchFromGitHub,
+  makeShellWrapper,
+  jdk_headless,
+  jre_minimal,
+  gradle,
 }:
+
+let
+  jre = jre_minimal.override {
+    modules = [
+      "java.base"
+      "java.compiler"
+      "java.logging"
+    ];
+  };
+  jcommander-src = fetchFromGitHub {
+    owner = "cbeust";
+    repo = "jcommander";
+    tag = "1.78";
+    hash = "sha256-zoPymohdU8HhVmw7ACoPbgNGgzdsIDVD3bl7Fh3qf2g=";
+  };
+in
 
 stdenv.mkDerivation rec {
   pname = "procyon";
   version = "0.6.0";
 
-  src = fetchurl {
-    url = "https://github.com/mstrobel/procyon/releases/download/v${version}/procyon-decompiler-${version}.jar";
-    sha256 = "sha256-gh2pYBL8aSRPoeopjJBFXuTgIUNLx5bTuVRqskYBt3k=";
+  src = fetchFromGitHub {
+    owner = "mstrobel";
+    repo = "procyon";
+    tag = "v${version}";
+    hash = "sha256-l8+eEdJtneZY1s6rvh9h87XwL7ioU3Y9T64CH7LdjXo=";
   };
 
-  dontUnpack = true;
+  postPatch = ''
+    sed -i build.gradle \
+      -e '/subprojects {/,/^}/d' \
+      -e '/uploadArchives/d' \
+      -e "/'maven'/d" \
+      -e '/sourceCompatibility/d'
+    sed -i -e 's/compile /implementation /' -e 's/testCompile/testImplementation/' {.,*}/build.gradle
+    sed -i Procyon.Decompiler/build.gradle \
+      -e '/uploadArchives/d' \
+      -e 's/configurations.compile/configurations.runtimeClasspath/' \
+      -e "/jcommander/d"
+    cp -a ${jcommander-src}/src Procyon.Decompiler
+  '';
+
+  nativeBuildInputs = [
+    jdk_headless
+    gradle
+    makeShellWrapper
+  ];
 
   installPhase = ''
     mkdir -p $out/bin $out/share/procyon
-    cp $src $out/share/procyon/procyon-decompiler.jar
+    mv build/Procyon.Decompiler/libs/Procyon.Decompiler-${version}.jar $out/share/procyon/procyon-decompiler.jar
 
-    cat << EOF > $out/bin/procyon
-    #!${runtimeShell}
-    exec ${jre_headless}/bin/java -jar $out/share/procyon/procyon-decompiler.jar "\$@"
-    EOF
-    chmod +x $out/bin/procyon
+    makeWrapper ${jre}/bin/java $out/bin/procyon \
+      --add-flags "-jar $out/share/procyon/procyon-decompiler.jar"
   '';
 
   meta = {
     description = "Suite of Java metaprogramming tools including a Java decompiler";
-    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
     homepage = "https://github.com/mstrobel/procyon/";
     license = lib.licenses.asl20;
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ linsui ];
     mainProgram = "procyon";
   };
 }
