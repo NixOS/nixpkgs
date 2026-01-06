6124c78ba001d4ff58d535dcdd84f9dd7e10f4bf
Doron Behar <doron.behar@gmail.com>
2026-01-06 13:38:38 +0200

    xsimd: revert an upstream patch that breaks some tests

--- pkgs/by-name/xs/xsimd/package.nix
+++ pkgs/by-name/xs/xsimd/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   cmake,
   doctest,
 }:
@@ -25,6 +26,12 @@ stdenv.mkDerivation (finalAttrs: {
     # package fails to notice it doesn't apply on their platform. We prefer not
     # performing this test on linux platforms too although it should pass.
     ./disable-test_error_gamma.patch
+    # https://github.com/xtensor-stack/xsimd/issues/1232#issuecomment-3712243289
+    (fetchpatch {
+      url = "https://github.com/xtensor-stack/xsimd/commit/eb17eaaa30129a65042bedf245658014ffd94232.patch";
+      hash = "sha256-619uFD5FijtX5I7fyoaBl/8g2jbDVaSETDMUkyYIFSs=";
+      revert = true;
+    })
   ];
 
   # strictDeps raises the chance that xsimd will be able to be cross compiled
