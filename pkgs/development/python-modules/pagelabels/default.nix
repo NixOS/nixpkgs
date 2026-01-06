f74d2df7249c9b5d499433d3d590e6ba8d82cf21
Doron Behar <doron.behar@gmail.com>
2026-01-06 23:09:21 +0200

    python3.pkgs.pagelabels: use finalAttrs

--- pkgs/development/python-modules/pagelabels/default.nix
+++ pkgs/development/python-modules/pagelabels/default.nix
@@ -5,13 +5,13 @@
   pdfrw,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pagelabels";
   version = "1.2.1";
   format = "setuptools";
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     sha256 = "sha256-GAEyhECToKnIWBxnYTSOsYKZBjl50b/82mZ68i8I2ug=";
   };
 
@@ -28,4 +28,4 @@ buildPythonPackage rec {
     maintainers = with lib.maintainers; [ teto ];
     license = lib.licenses.gpl3;
   };
-}
+})
