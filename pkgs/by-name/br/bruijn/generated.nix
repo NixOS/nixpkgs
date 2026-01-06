c2ec565ee4829f1b38461aafa35c5f0be747619f
Defelo <mail@defelo.de>
2026-01-06 15:50:07 +0100

    bruijn: remove version override
    r-ryantm wasn't able to update this package automatically
    because it couldn't find the version in the derivation file
    (https://nixpkgs-update-logs.nix-community.org/bruijn/2026-01-05.log).

--- pkgs/by-name/br/bruijn/generated.nix
+++ pkgs/by-name/br/bruijn/generated.nix
@@ -22,7 +22,7 @@
 }:
 mkDerivation {
   pname = "bruijn";
-  version = "0.1.0.0";
+  version = "0-unstable-2025-11-01";
   src = fetchzip {
     url = "https://github.com/marvinborner/bruijn/archive/c37a9d27b9f654b1e0118d32269a1548800b062b.tar.gz";
     sha256 = "1a667693agsc559yy4k04gxv1hlsgkn3aw64yvg876fbn5dr7sk3";
