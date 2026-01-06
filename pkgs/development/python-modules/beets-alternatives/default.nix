c6a7a946f4a4ad036f72398dbe883eca0ba6803e
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-06 23:17:52 +0800

    python3Packages.beets-alternatives: 0.13.4 -> 0.14.0

--- pkgs/development/python-modules/beets-alternatives/default.nix
+++ pkgs/development/python-modules/beets-alternatives/default.nix
@@ -5,7 +5,7 @@
   buildPythonPackage,
 
   # build-system
-  poetry-core,
+  hatchling,
 
   # nativeBuildInputs
   beets-minimal,
@@ -22,29 +22,18 @@
 
 buildPythonPackage rec {
   pname = "beets-alternatives";
-  version = "0.13.4";
+  version = "0.14.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     repo = "beets-alternatives";
     owner = "geigerzaehler";
     tag = "v${version}";
-    hash = "sha256-jGHRoBBXqJq0r/Gbp7gkuaEFPVMGE6cqQRi84AHTXxQ=";
+    hash = "sha256-leZYXf6Oo/jAKbnJbP+rTnuRsh9P1BQXYAbthMNT60A=";
   };
 
-  patches = [
-    # Fixes build failure by ignoring DeprecationWarning during tests.
-    (fetchpatch {
-      url = "https://github.com/geigerzaehler/beets-alternatives/commit/3c15515edfe62d5d6c8f3fb729bf3dcef41c1ffa.patch";
-      hash = "sha256-gZXftDI5PXJ0c65Z1HLABJ2SlDnXU78xxIEt7IGp8RQ=";
-      excludes = [
-        "poetry.lock"
-      ];
-    })
-  ];
-
   build-system = [
-    poetry-core
+    hatchling
   ];
 
   nativeBuildInputs = [
