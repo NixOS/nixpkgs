31d9551f955e37f5748f26a980eeabf9ceaabb7b
David Wolff <nixpkgs@dav.dev>
2026-01-07 06:28:18 +0100

    python3Packages.django-decorator-include: init at 3.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4158,6 +4158,8 @@ self: super: with self; {
 
   django-debug-toolbar = callPackage ../development/python-modules/django-debug-toolbar { };
 
+  django-decorator-include = callPackage ../development/python-modules/django-decorator-include { };
+
   django-dynamic-preferences =
     callPackage ../development/python-modules/django-dynamic-preferences
       { };
