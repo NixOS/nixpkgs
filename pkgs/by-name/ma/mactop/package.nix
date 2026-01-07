8daddc4b0c3656f208061e4d3e38035e4e4b276a
metaspartan <carsenk@gmail.com>
2026-01-07 02:56:54 -0700

    mactop: 2.0.5 -> 2.0.6
    mactop: 2.0.5 -> 2.0.6

--- pkgs/by-name/ma/mactop/package.nix
+++ pkgs/by-name/ma/mactop/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "mactop";
-  version = "2.0.5";
+  version = "2.0.6";
 
   src = fetchFromGitHub {
     owner = "metaspartan";
     repo = "mactop";
     tag = "v${version}";
-    hash = "sha256-0M3nV3gjsY1vg+uqXbKUAF/8311jc8UJ2UYUFyrRiAo=";
+    hash = "sha256-J+ebxVV5aNTz0qtBkd8G4NX0EB7wWkWIIzS0h/jvQWI=";
   };
 
   vendorHash = "sha256-nlbifuj4kued8ugawAfd4V6uirkQEZ1yRQXVsF+ZEdc=";
@@ -26,14 +26,20 @@ buildGoModule rec {
   ];
 
   doInstallCheck = true;
+  doCheck = false;
   nativeInstallCheckInputs = [ versionCheckHook ];
+
+  versionCheckProgramArg = "--version";
 
   meta = {
     description = "Terminal-based monitoring tool 'top' designed to display real-time metrics for Apple Silicon chips";
     homepage = "https://github.com/metaspartan/mactop";
     changelog = "https://github.com/metaspartan/mactop/releases/tag/${src.tag}";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ natsukium ];
+    maintainers = with lib.maintainers; [
+      natsukium
+      metaspartan
+    ];
     mainProgram = "mactop";
     platforms = [ "aarch64-darwin" ];
   };
