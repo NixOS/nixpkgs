15bf75f04fecabc3e098f883d01d50745b39e0a8
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-07 11:23:09 +0000

    python3Packages.lastversion: 3.5.12 -> 3.6.7

--- pkgs/development/python-modules/lastversion/default.nix
+++ pkgs/development/python-modules/lastversion/default.nix
@@ -22,14 +22,14 @@
 
 buildPythonPackage rec {
   pname = "lastversion";
-  version = "3.5.12";
+  version = "3.6.7";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dvershinin";
     repo = "lastversion";
     tag = "v${version}";
-    hash = "sha256-0yq4rH5okkfbZRxIowClVSV9ihFMCnhRxqwUpMPFDyk=";
+    hash = "sha256-HI9HQlpzvvYl5SWZQETlPSOulx/tPfAmFJJpVxOvfPA=";
   };
 
   build-system = [ setuptools ];
