646d60cabaa584174b6c485002e2c4c113f2b2e2
Bobby Rong <rjl931189261@126.com>
2026-01-07 23:04:47 +0800

    timeshift: 25.12.1 -> 25.12.3
    https://github.com/linuxmint/timeshift/compare/25.12.1...25.12.3

--- pkgs/applications/backup/timeshift/unwrapped.nix
+++ pkgs/applications/backup/timeshift/unwrapped.nix
@@ -18,7 +18,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "timeshift";
-  version = "25.12.1";
+  version = "25.12.3";
   outputs = [
     "out"
     "man"
@@ -28,7 +28,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "linuxmint";
     repo = "timeshift";
     tag = finalAttrs.version;
-    hash = "sha256-m+eSF+cALWSaNt/0V0XTrDZiocddsLqK1YDU7yBdXjw=";
+    hash = "sha256-yrPeaGdt32bmP8lV+eYZb2H3OmVl3G5AH7eeGZ3LM3k=";
   };
 
   postPatch = ''
