dd54c0d8dd60309bdeaf28fb55d4e798558cd4ac
Jonathan Davies <jpds@protonmail.com>
2026-01-07 12:34:12 +0000

    prometheus: 3.8.1 → 3.9.0

--- pkgs/by-name/pr/prometheus/source.nix
+++ pkgs/by-name/pr/prometheus/source.nix
@@ -1,6 +1,6 @@
 {
-  version = "3.8.1";
-  hash = "sha256-mb1aMWpIELyZaqjYX1teYdtAK/+W9jzdPwzgbT7AVj4=";
-  npmDepsHash = "sha256-wmxvlQWQO2gP2apupMJssqAPr1ntWWIazduQU3AuodU=";
-  vendorHash = "sha256-35pVMl1Hu970BQE6jMzT1xaz/3FaIFWUtel0cl/uBdw=";
+  version = "3.9.0";
+  hash = "sha256-80EkJaH7NMQOCHWKz19c/8c2ncBE+qAFJ5fp1mbK+tU=";
+  npmDepsHash = "sha256-Y4874LZjj48y5eeByhhMAJp3NtX0QAehYvjzxkK2B2E=";
+  vendorHash = "sha256-CGec9hR/BpO3rXNGYm0V9AESsGIR6a0734e8t9oMOpA=";
 }
