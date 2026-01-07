f00abc949432294d390f6ba7988bc7eab5fc061b
bas <github.plated100@passmail.net>
2026-01-07 15:49:10 +0100

    nixos/nemorosa: init service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1551,6 +1551,7 @@
   ./services/torrent/flexget.nix
   ./services/torrent/flood.nix
   ./services/torrent/magnetico.nix
+  ./services/torrent/nemorosa.nix
   ./services/torrent/opentracker.nix
   ./services/torrent/peerflix.nix
   ./services/torrent/qbittorrent.nix
