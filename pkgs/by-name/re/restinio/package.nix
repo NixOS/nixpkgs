a74f683c2b5ef59e75e7a41bbc8a66e98abbddb1
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-07 23:29:27 +0000

    restinio: 0.7.8 -> 0.7.9

--- pkgs/by-name/re/restinio/package.nix
+++ pkgs/by-name/re/restinio/package.nix
@@ -21,13 +21,13 @@ assert !with_boost_asio -> asio != null;
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "restinio";
-  version = "0.7.8";
+  version = "0.7.9";
 
   src = fetchFromGitHub {
     owner = "Stiffstream";
     repo = "restinio";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-PXm9s586V1aZ7D5GwYzBc/Fljif/Iq3VChDe2NHWKSU=";
+    hash = "sha256-ZosfTdi/u8/sHDf6UYP49ZEkTtT9FkVCazff/rMNbLA=";
   };
 
   # https://www.github.com/Stiffstream/restinio/issues/230
