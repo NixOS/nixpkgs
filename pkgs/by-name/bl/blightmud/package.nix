ed03dae4a9411e96d8b1e4c8a3da9a504496ff69
Stefan Frijters <sfrijters@gmail.com>
2026-01-08 13:43:00 +0100

    blightmud: fix build with gcc15
    Otherwise, the onig_sys create breaks.

--- pkgs/by-name/bl/blightmud/package.nix
+++ pkgs/by-name/bl/blightmud/package.nix
@@ -35,6 +35,10 @@ rustPlatform.buildRustPackage (finalAttrs: {
   ++ lib.optionals (withTTS && stdenv.hostPlatform.isLinux) [ speechd-minimal ]
   ++ lib.optionals stdenv.hostPlatform.isLinux [ alsa-lib ];
 
+  env = lib.optionalAttrs (!stdenv.cc.isClang) {
+    NIX_CFLAGS_COMPILE = "-std=gnu17";
+  };
+
   checkFlags =
     let
       # Most of Blightmud's unit tests pass without trouble in the isolated
