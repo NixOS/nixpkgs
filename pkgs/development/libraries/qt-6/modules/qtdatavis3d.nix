1dc291c791b4946942855bbe53867971cd0dcaf6
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:04 -0500

    qtdatavis3d: add host Qt tools for MinGW cross
    - Add host qtdeclarative/qtquick3d for QML tooling during cross builds

--- pkgs/development/libraries/qt-6/modules/qtdatavis3d.nix
+++ pkgs/development/libraries/qt-6/modules/qtdatavis3d.nix
@@ -2,6 +2,9 @@
   qtModule,
   qtbase,
   qtdeclarative,
+  lib,
+  pkgsBuildBuild,
+  stdenv,
 }:
 
 qtModule {
@@ -9,5 +12,19 @@ qtModule {
   propagatedBuildInputs = [
     qtbase
     qtdeclarative
+  ];
+
+  # Qt6 component discovery expects Qt6<Module> packages to be available.
+  # In our split-prefix Qt packaging this needs a bit of help, especially for
+  # cross builds where QmlTools must come from the build machine.
+  #
+  # MSYS2 reference: mingw-w64-qt6-datavis3d depends on qt6-declarative.
+  cmakeFlags = [
+    "-DQt6Quick_DIR=${qtdeclarative}/lib/cmake/Qt6Quick"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    # Qt6Quick depends on the (host) Qt6QuickTools package when cross-compiling.
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
   ];
 }
