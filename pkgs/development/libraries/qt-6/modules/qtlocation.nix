1fc32ccd97cef723831f2adc61594a54e8c3ec98
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:05 -0500

    qtlocation: add host Qt tools for MinGW cross
    - Add host qtdeclarative/qtpositioning for tooling during cross builds

--- pkgs/development/libraries/qt-6/modules/qtlocation.nix
+++ pkgs/development/libraries/qt-6/modules/qtlocation.nix
@@ -3,6 +3,9 @@
   qtbase,
   qtdeclarative,
   qtpositioning,
+  lib,
+  pkgsBuildBuild,
+  stdenv,
 }:
 
 qtModule {
@@ -12,4 +15,15 @@ qtModule {
     qtdeclarative
     qtpositioning
   ];
+
+  # Qt6Qml (from qtdeclarative) requires the build-machine Qt6QmlTools package in cross builds.
+  # qtlocation disables itself when Qt::Qml isn't available, which leaves no `install` target.
+  cmakeFlags = [
+    "-DQt6Quick_DIR=${qtdeclarative}/lib/cmake/Qt6Quick"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+  ];
+
 }
