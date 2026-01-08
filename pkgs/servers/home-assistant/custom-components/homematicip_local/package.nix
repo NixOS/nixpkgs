bf79c30ffcb47b5e893d405fdf4fb51f61a5657d
Robert Schütz <nix@dotlambda.de>
2026-01-08 08:07:04 -0800

    home-assistant-custom-components.homematicip_local: 2.0.6 -> 2.1.0
    Diff: https://github.com/SukramJ/custom_homematic/compare/2.0.6...2.1.0
    
    Changelog: https://github.com/SukramJ/custom_homematic/blob/2.1.0/changelog.md

--- pkgs/servers/home-assistant/custom-components/homematicip_local/package.nix
+++ pkgs/servers/home-assistant/custom-components/homematicip_local/package.nix
@@ -13,13 +13,13 @@
 buildHomeAssistantComponent rec {
   owner = "SukramJ";
   domain = "homematicip_local";
-  version = "2.0.6";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "SukramJ";
     repo = "custom_homematic";
     tag = version;
-    hash = "sha256-BlA3PAR8tJZbYqzlt5iah7BY2LnVyfNnKm8NxjrPJ2c=";
+    hash = "sha256-zXosnxVbJ/rADzbOvm6GIJLAx+EIK3vQ50qNck5sYwg=";
   };
 
   postPatch = ''
