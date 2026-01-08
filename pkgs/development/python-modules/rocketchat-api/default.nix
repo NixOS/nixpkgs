d7cf771e68b5513fd955ecc7ffc93ecbbd5e16c1
Robert Schütz <nix@dotlambda.de>
2026-01-08 08:29:25 -0800

    python3Packages.rocketchat-api: 1.37.0 -> 2.0.0
    Diff: https://github.com/jadolg/rocketchat_API/compare/1.37.0...2.0.0
    
    Changelog: https://github.com/jadolg/rocketchat_API/releases/tag/2.0.0

--- pkgs/development/python-modules/rocketchat-api/default.nix
+++ pkgs/development/python-modules/rocketchat-api/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "rocketchat-api";
-  version = "1.37.0";
+  version = "2.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jadolg";
     repo = "rocketchat_API";
     tag = version;
-    hash = "sha256-N0IEPYN3H/KYZuTQZFTGZaDFZseGG1M6Kn5WX29afB8=";
+    hash = "sha256-CIC2pVFN+cN9HCdIHX1VFXJ5eqIgdNXBSmzt/9LH1JE=";
   };
 
   build-system = [
