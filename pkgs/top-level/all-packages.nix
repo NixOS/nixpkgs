1d300413e84d4e67a3f0530b1ecff7f69c1760f0
kurogeek <kurogeek@lmvhaus.com>
2026-01-08 16:30:35 +0700

    inventree: init
    Co-authored-by: DavHau <hsngrmpf+github@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12517,4 +12517,6 @@ with pkgs;
   gpac-unstable = callPackage ../by-name/gp/gpac/package.nix {
     releaseChannel = "unstable";
   };
+
+  inventree = callPackage ../by-name/in/inventree/package.nix { };
 }
