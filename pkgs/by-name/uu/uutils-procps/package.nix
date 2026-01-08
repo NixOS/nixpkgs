1985195d4ff06b3e0768787875745a9d277726ea
kyehn <228304369+kyehn@users.noreply.github.com>
2026-01-08 19:56:27 +0800

    uutils-procps: init at 0.0.1-unstable-2025-12-31

---
+++ pkgs/by-name/uu/uutils-procps/package.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  writableTmpDirAsHomeHook,
+  systemdLibs,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "uutils-procps";
+  version = "0.0.1-unstable-2025-12-31";
+
+  src = fetchFromGitHub {
+    owner = "uutils";
+    repo = "procps";
+    rev = "644666759ec3e8f038a0d376b594e67a8780dc29";
+    hash = "sha256-U/5thGk/1h4CkI63DllLUc3scrHbV4+djcxLiTC+3lM=";
+  };
+
+  cargoHash = "sha256-kWT5PYE68TUcYnz82zxhiMIZFAn8Rf00Oj7oLsmzsG8=";
+
+  nativeBuildInputs = [
+    pkg-config
+    writableTmpDirAsHomeHook
+  ];
+
+  buildInputs = [ systemdLibs ];
+
+  checkFlags = [
+    # can't run on sandbox
+    "--skip=test_pgrep::test_count_with_matching_pattern"
+    "--skip=test_pgrep::test_list_full_process_with_empty_cmdline"
+    "--skip=test_pgrep::test_terminal_multiple_terminals"
+    "--skip=test_pgrep::test_unknown_terminal"
+    "--skip=test_pidof"
+    "--skip=test_pmap::test_device_permission_denied"
+    "--skip=test_pmap::test_extended_permission_denied"
+    "--skip=test_pmap::test_permission_denied"
+    "--skip=test_ps::test_deselect"
+    "--skip=test_ps::test_effective_group_selection"
+    "--skip=test_ps::test_effective_user_selection"
+    "--skip=test_ps::test_real_group_selection"
+    "--skip=test_ps::test_real_user_selection"
+    "--skip=test_pkill::test_inverse"
+    "--skip=test_pgrep::test_pidfile"
+  ];
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [ "--version=branch" ];
+  };
+
+  meta = {
+    description = "Rust reimplementation of the procps project";
+    homepage = "https://github.com/uutils/procps";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ kyehn ];
+    platforms = lib.platforms.unix;
+  };
+})
