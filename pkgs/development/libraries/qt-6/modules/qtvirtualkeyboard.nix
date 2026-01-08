cbe0ed9fcae448af27aeceefa6f79a01f7c14cfa
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:06 -0500

    qtvirtualkeyboard: add host Qt tools for MinGW cross
    - Add host qtdeclarative for QML tooling during cross builds

--- pkgs/development/libraries/qt-6/modules/qtvirtualkeyboard.nix
+++ pkgs/development/libraries/qt-6/modules/qtvirtualkeyboard.nix
@@ -6,6 +6,9 @@
   hunspell,
   pkg-config,
   fetchpatch,
+  lib,
+  pkgsBuildBuild,
+  stdenv,
 }:
 
 qtModule {
@@ -24,5 +27,15 @@ qtModule {
       url = "https://github.com/qt/qtvirtualkeyboard/commit/0b1e8be8dd874e1fbacd0c30ed5be7342f6cd44d.patch";
       hash = "sha256-Uk6EJOlkCRLUg1w3ljHaxV/dXEVWyUpP/ijoyjptbNc=";
     })
+  ];
+
+  # Cross: QtVirtualKeyboard is a QtQuick module; ensure Qt::Quick is discoverable
+  # and that host-side Qml/Quick tools are available.
+  cmakeFlags = [
+    "-DQt6Quick_DIR=${qtdeclarative}/lib/cmake/Qt6Quick"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
   ];
 }
