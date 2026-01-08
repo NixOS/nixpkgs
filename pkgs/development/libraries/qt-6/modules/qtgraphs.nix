51299b34ad76f6d69d2e5a6838e1a3c7eaec3ff9
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:05 -0500

    qtgraphs: add host Qt tools for MinGW cross
    - Add host qtdeclarative/qtquick3d for QML tooling during cross builds

--- pkgs/development/libraries/qt-6/modules/qtgraphs.nix
+++ pkgs/development/libraries/qt-6/modules/qtgraphs.nix
@@ -4,6 +4,9 @@
   qtdeclarative,
   qtquick3d,
   qtquicktimeline,
+  lib,
+  pkgsBuildBuild,
+  stdenv,
 }:
 
 qtModule {
@@ -13,5 +16,15 @@ qtModule {
     qtdeclarative
     qtquick3d
     qtquicktimeline
+  ];
+
+  # QtQuick component discovery expects Qt6Quick to be findable, and Qt6Quick depends on
+  # the build-machine Qt6QuickTools package in cross builds.
+  cmakeFlags = [
+    "-DQt6Quick_DIR=${qtdeclarative}/lib/cmake/Qt6Quick"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
   ];
 }
