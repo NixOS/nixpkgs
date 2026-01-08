e9438ba7269527969c545eece862121e368b9ccf
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-08 10:05:43 +0100

    python313Packages.reconspread: init at 0.1.0-unstable-2025-07-27
    Module to extract links
    
    https://github.com/reconurge/reconspread

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16961,6 +16961,8 @@ self: super: with self; {
 
   reconplogger = callPackage ../development/python-modules/reconplogger { };
 
+  reconspread = callPackage ../development/python-modules/reconspread { };
+
   recontrack = callPackage ../development/python-modules/recontrack { };
 
   recordlinkage = callPackage ../development/python-modules/recordlinkage { };
