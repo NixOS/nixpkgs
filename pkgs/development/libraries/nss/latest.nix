845e6560eaa54ec6c2ec17481591b41783724f53
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-08 22:08:10 +0100

    nss_latest: 3.119.1 -> 3.120
    https://github.com/nss-dev/nss/blob/master/doc/rst/releases/nss_3_120.rst
    (cherry picked from commit 1b26cb6ba7e42aa8d9799cd17d8e734db78f173f)

--- pkgs/development/libraries/nss/latest.nix
+++ pkgs/development/libraries/nss/latest.nix
@@ -5,8 +5,8 @@
 #       Example: nix-shell ./maintainers/scripts/update.nix --argstr package cacert
 
 import ./generic.nix {
-  version = "3.119.1";
-  hash = "sha256-GxLTqHcVWGiFezcwdctXJ8k9wqizVJPHyLBPZzphLro=";
+  version = "3.120";
+  hash = "sha256-HZBiSwiGJs6Ff40SpY8p6kSTl1j793+A8BTjyMH4YQg=";
   filename = "latest.nix";
   versionRegex = "NSS_(\\d+)_(\\d+)(?:_(\\d+))?_RTM";
 }
