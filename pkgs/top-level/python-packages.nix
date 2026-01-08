33a6b73ae13869376df64ecaeb3afcbd03cf2be2
kurogeek <kurogeek@lmvhaus.com>
2026-01-08 13:12:38 +0700

    python3Packages.django-dbbackup: init at 4.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4148,6 +4148,8 @@ self: super: with self; {
 
   django-currentuser = callPackage ../development/python-modules/django-currentuser { };
 
+  django-dbbackup = callPackage ../development/python-modules/django-dbbackup { };
+
   django-debug-toolbar = callPackage ../development/python-modules/django-debug-toolbar { };
 
   django-dynamic-preferences =
