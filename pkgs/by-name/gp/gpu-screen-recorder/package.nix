0b4fe4b7c1d68c58ad379b381cd1db8f63e272ba
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-08 20:51:13 +0000

    gpu-screen-recorder: 5.11.2 -> 5.11.5

--- pkgs/by-name/gp/gpu-screen-recorder/package.nix
+++ pkgs/by-name/gp/gpu-screen-recorder/package.nix
@@ -28,12 +28,12 @@
 
 stdenv.mkDerivation rec {
   pname = "gpu-screen-recorder";
-  version = "5.11.2";
+  version = "5.11.5";
 
   src = fetchgit {
     url = "https://repo.dec05eba.com/${pname}";
     tag = version;
-    hash = "sha256-9hmuaqa6HQogL4OwDOID/kL4ZYaDDKJeFrEtrDS9ZYg=";
+    hash = "sha256-o9IjuKuE2/41I52vpR7EOLCDSLJty/Wq0k7UiFHHlDk=";
   };
 
   nativeBuildInputs = [
