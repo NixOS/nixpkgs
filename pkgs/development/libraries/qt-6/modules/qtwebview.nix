3e1a0b5ce6a7f445387fe5bd3c1eb3961a029609
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:06 -0500

    qtwebview: support MinGW cross
    - Avoid qtwebengine on Windows (unsupported)
    - Add host qtdeclarative for QML tooling during cross builds

--- pkgs/development/libraries/qt-6/modules/qtwebview.nix
+++ pkgs/development/libraries/qt-6/modules/qtwebview.nix
@@ -4,6 +4,7 @@
   qtModule,
   qtdeclarative,
   qtwebengine,
+  pkgsBuildBuild,
 }:
 
 qtModule {
@@ -11,5 +12,17 @@ qtModule {
   propagatedBuildInputs = [
     qtdeclarative
   ]
-  ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ qtwebengine ];
+  # qtwebengine isn't available on MinGW hostPlatform in nixpkgs. MSYS2's qt6-webview
+  # also doesn't depend on qt6-webengine. Keep WebEngine only where it is supported.
+  ++ lib.optionals (lib.meta.availableOn stdenv.hostPlatform qtwebengine) [ qtwebengine ];
+
+  # Cross: webview contains a QtQuick QML module (uses qt_internal_add_qml_module).
+  # Ensure Qt6Qml/Qt6Quick are "FOUND" by providing the build-machine tool packages.
+  cmakeFlags = [
+    "-DQt6Quick_DIR=${qtdeclarative}/lib/cmake/Qt6Quick"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+  ];
 }
