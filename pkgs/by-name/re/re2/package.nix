b56665b555d8196d0f9c7789f46494545aeae8cd
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:03 -0500

    re2: make gbenchmark test-only
    - Only depend on gbenchmark when doCheck
    - Set RE2_BUILD_TESTING accordingly
    - Helps MinGW eval where gbenchmark is unavailable

--- pkgs/by-name/re/re2/package.nix
+++ pkgs/by-name/re/re2/package.nix
@@ -36,7 +36,7 @@ stdenv.mkDerivation (finalAttrs: {
     ninja
   ];
 
-  buildInputs = [
+  checkInputs = [
     gbenchmark
     gtest
   ];
@@ -44,7 +44,7 @@ stdenv.mkDerivation (finalAttrs: {
   propagatedBuildInputs = [ abseil-cpp ] ++ lib.optionals (!stdenv.hostPlatform.isStatic) [ icu ];
 
   cmakeFlags = [
-    (lib.cmakeBool "RE2_BUILD_TESTING" true)
+    (lib.cmakeBool "RE2_BUILD_TESTING" finalAttrs.doCheck)
     (lib.cmakeFeature "CMAKE_CTEST_ARGUMENTS" "--timeout;999999")
   ]
   ++ lib.optionals (!stdenv.hostPlatform.isStatic) [
@@ -52,7 +52,7 @@ stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "BUILD_SHARED_LIBS" true)
   ];
 
-  doCheck = true;
+  doCheck = stdenv.buildPlatform.canExecute stdenv.hostPlatform;
 
   passthru.tests = {
     inherit chromium grpc mercurial;
