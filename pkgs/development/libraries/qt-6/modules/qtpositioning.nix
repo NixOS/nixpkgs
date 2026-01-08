2f757c27f9ead7418f677487bec4b4eac7b89c64
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:05 -0500

    qtpositioning: add host Qt tools for MinGW cross
    - Add host qtdeclarative for QML tooling during cross builds

--- pkgs/development/libraries/qt-6/modules/qtpositioning.nix
+++ pkgs/development/libraries/qt-6/modules/qtpositioning.nix
@@ -5,6 +5,10 @@
   qtserialport,
   pkg-config,
   openssl,
+  qtsvg,
+  lib,
+  pkgsBuildBuild,
+  stdenv,
 }:
 
 qtModule {
@@ -13,7 +17,16 @@ qtModule {
     qtbase
     qtdeclarative
     qtserialport
+    qtsvg
   ];
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ];
+
+  cmakeFlags = [
+    "-DQt6Quick_DIR=${qtdeclarative}/lib/cmake/Qt6Quick"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
+  ];
 }
