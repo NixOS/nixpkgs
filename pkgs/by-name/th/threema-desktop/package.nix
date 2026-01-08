0f7922e6b6c7797851e469246c3a71f78c8921e4
eyjhb <eyjhbb@gmail.com>
2026-01-08 10:30:19 +0100

    threema-desktop: 1.2.48 -> 1.2.49

--- pkgs/by-name/th/threema-desktop/package.nix
+++ pkgs/by-name/th/threema-desktop/package.nix
@@ -10,12 +10,12 @@
 }:
 
 let
-  version = "1.2.48";
+  version = "1.2.49";
   electronSrc = fetchFromGitHub {
     owner = "threema-ch";
     repo = "threema-web-electron";
     tag = version;
-    hash = "sha256-u1rzKFDrLxU/o7Oc2o/WBwbAncNWKJ9GAUBaNDPViZI=";
+    hash = "sha256-8Zy+LMxszqCT17FFz8sEMAtBSTXhU/F2Nz9MfEA82xw=";
   };
 
   threema-web = buildNpmPackage rec {
@@ -54,7 +54,7 @@ let
     inherit version;
     src = electronSrc;
     sourceRoot = "${src.name}/app";
-    npmDepsHash = "sha256-mafB7lC1YpIZ71R6IT3TnSzFDieK4AsAzIqpWcy9480=";
+    npmDepsHash = "sha256-Nn1WIyGRAG009GlJIedl0QUeKLalY+H3Mj8w4J1GE6I=";
     env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
     dontNpmBuild = true;
     prePatch = ''
@@ -73,7 +73,7 @@ buildNpmPackage rec {
   inherit version;
   src = electronSrc;
 
-  npmDepsHash = "sha256-A7XvzURCCM0+ISlSLpnreFIxKku4FnVdWLsF2WxQfBY=";
+  npmDepsHash = "sha256-i37rIAIhoIU9tnioyhPFR1GWBfygMREZ0HCYE7ujXuE=";
 
   env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
