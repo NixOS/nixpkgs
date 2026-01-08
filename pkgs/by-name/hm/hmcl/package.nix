1f73ec6c089ea4cb29f0374303e551aa3abde58f
Misaka13514 <Misaka13514@gmail.com>
2026-01-08 13:26:04 +0800

    hmcl: 3.8.2 -> 3.9.1

--- pkgs/by-name/hm/hmcl/package.nix
+++ pkgs/by-name/hm/hmcl/package.nix
@@ -39,13 +39,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "hmcl";
-  version = "3.8.2";
+  version = "3.9.1";
 
   src = fetchurl {
     # HMCL has built-in keys, such as the Microsoft OAuth secret and the CurseForge API key.
     # See https://github.com/HMCL-dev/HMCL/blob/refs/tags/release-3.6.12/.github/workflows/gradle.yml#L26-L28
     url = "https://github.com/HMCL-dev/HMCL/releases/download/v${finalAttrs.version}/HMCL-${finalAttrs.version}.jar";
-    hash = "sha256-0vAgyN8PC2AQbor9aVqr/K1oBLt4QGpEQ2vSL2ajQOg=";
+    hash = "sha256-UcycobEFbiSys19a3C01CqLKTM1SdAwHDNExRNVv6ug=";
   };
 
   # - HMCL prompts users to download prebuilt Terracotta binary for
