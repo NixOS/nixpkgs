b72733d3655663aa7d90597b5d7616402519b7df
kurogeek <kurogeek@lmvhaus.com>
2026-01-08 13:55:40 +0700

    python3Packages.opentelemetry-instrumentation-system-metrics: init

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11459,6 +11459,10 @@ self: super: with self; {
     callPackage ../development/python-modules/opentelemetry-instrumentation-sqlalchemy
       { };
 
+  opentelemetry-instrumentation-system-metrics =
+    callPackage ../development/python-modules/opentelemetry-instrumentation-system-metrics
+      { };
+
   opentelemetry-instrumentation-urllib3 =
     callPackage ../development/python-modules/opentelemetry-instrumentation-urllib3
       { };
