6c17f83bf852faa3adbb1f07ec5599c95e117735
Jarrad Hope <jarrad@logos.co>
2026-01-08 10:46:05 -0500

    qtquicktimeline: add host Qt tools for MinGW cross
    - Add host qtdeclarative for QML tooling during cross builds

--- pkgs/development/libraries/qt-6/modules/qtquicktimeline.nix
+++ pkgs/development/libraries/qt-6/modules/qtquicktimeline.nix
@@ -2,6 +2,9 @@
   qtModule,
   qtbase,
   qtdeclarative,
+  lib,
+  pkgsBuildBuild,
+  stdenv,
 }:
 
 qtModule {
@@ -9,5 +12,15 @@ qtModule {
   propagatedBuildInputs = [
     qtbase
     qtdeclarative
+  ];
+
+  # QtQuick component discovery expects Qt6Quick to be findable, and Qt6Quick depends on
+  # the build-machine Qt6QuickTools package in cross builds.
+  cmakeFlags = [
+    "-DQt6Quick_DIR=${qtdeclarative}/lib/cmake/Qt6Quick"
+  ]
+  ++ lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
+    "-DQt6QmlTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QmlTools"
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
   ];
 }
