63175b58d3947bded67fca6715676b53ab5861bc
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-09 00:35:06 +0000

    vcluster: 0.29.1 -> 0.31.0

--- pkgs/by-name/vc/vcluster/package.nix
+++ pkgs/by-name/vc/vcluster/package.nix
@@ -10,13 +10,13 @@
 
 buildGoModule rec {
   pname = "vcluster";
-  version = "0.29.1";
+  version = "0.31.0";
 
   src = fetchFromGitHub {
     owner = "loft-sh";
     repo = "vcluster";
     tag = "v${version}";
-    hash = "sha256-LsyJ1qiSAWOfh9DWrO1JNxf5ItH7pMn8jW/GQ9Skeq4=";
+    hash = "sha256-yGvKZ70+x+PQiTCB8MxUplymlQLm9iT+ryBHFF1a/Os=";
   };
 
   vendorHash = null;
