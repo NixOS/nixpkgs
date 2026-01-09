e5f36945c1964fbf44df71e42f6b7f5d8acf5ce5
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-09 12:36:02 +0000

    xviewer: 3.4.15 -> 3.4.16

--- pkgs/by-name/xv/xviewer/package.nix
+++ pkgs/by-name/xv/xviewer/package.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation rec {
   pname = "xviewer";
-  version = "3.4.15";
+  version = "3.4.16";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = "xviewer";
     rev = version;
-    hash = "sha256-fAB7nbkGx6aNKwRA01013hAiGJ8pEovI0WClOqT0D10=";
+    hash = "sha256-ayd91gVLuSUVlCxaPSBbx7hg4tthVTaBEnl5V9YYbQw=";
   };
 
   nativeBuildInputs = [
