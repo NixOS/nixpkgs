a2020b62b439e787dd201a4c31761ae779f8c189
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-09 20:05:46 +0000

    glooctl: 1.20.7 -> 1.20.8

--- pkgs/by-name/gl/glooctl/package.nix
+++ pkgs/by-name/gl/glooctl/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule rec {
   pname = "glooctl";
-  version = "1.20.7";
+  version = "1.20.8";
 
   src = fetchFromGitHub {
     owner = "solo-io";
     repo = "gloo";
     rev = "v${version}";
-    hash = "sha256-2GILbG7pnkaaoUQkTMTt+1GLpUPfbz3gbPV+a49ERh0=";
+    hash = "sha256-OZrVKzcFnA+mbGG0CET0bfVc77JHPCMMXIq9yzT8cWw=";
   };
 
   vendorHash = "sha256-zJmp3UWzZSI7G54DTOEOEo2ZIKjM6GZ0Cf5/BukaB4o=";
