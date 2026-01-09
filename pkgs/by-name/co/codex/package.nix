e69f0126abdeae9de185f4e0ed61d6d87ae7c635
Jhony Elmer Angulo Fabian <leohjaf@gmail.com>
2026-01-09 16:28:27 -0500

    codex: 0.79.0 -> 0.80.0
    (cherry picked from commit 2d586eeca3fbad7c5061a8a6a488bb2184517713)

--- pkgs/by-name/co/codex/package.nix
+++ pkgs/by-name/co/codex/package.nix
@@ -14,18 +14,18 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "codex";
-  version = "0.79.0";
+  version = "0.80.0";
 
   src = fetchFromGitHub {
     owner = "openai";
     repo = "codex";
     tag = "rust-v${finalAttrs.version}";
-    hash = "sha256-fp/R/F0LcFLHfSNUW4pO2EG2ICce5vzxsOBYrm2tyaA=";
+    hash = "sha256-gb0EG0vSNhC8O1JxjCP9nX74LkR8geGsNe8w6LH85xk=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/codex-rs";
 
-  cargoHash = "sha256-qCf8kJ2AuPfpykNiF2KNJqOs+Vq+BQHgMseqPtiGiz0=";
+  cargoHash = "sha256-ks7sBfYmdFUrjFGB/hXvZzHjkQT5LBh64hiC5SpEhF8=";
 
   nativeBuildInputs = [
     installShellFiles
