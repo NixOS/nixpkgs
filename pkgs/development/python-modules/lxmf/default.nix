b135f8ebf66c25cf91af107fff13373db244b850
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-09 22:32:55 +0100

    python313Packages.lxmf: migrate to finalAttrs

--- pkgs/development/python-modules/lxmf/default.nix
+++ pkgs/development/python-modules/lxmf/default.nix
@@ -6,7 +6,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "lxmf";
   version = "0.9.4";
   pyproject = true;
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "markqvist";
     repo = "lxmf";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-WeEGwdbW2hmN7sdMl8tR5pmaXGqRb6y5Zb536ty3eiY=";
   };
 
@@ -30,11 +30,11 @@ buildPythonPackage rec {
   meta = {
     description = "Lightweight Extensible Message Format for Reticulum";
     homepage = "https://github.com/markqvist/lxmf";
-    changelog = "https://github.com/markqvist/LXMF/releases/tag/${src.tag}";
+    changelog = "https://github.com/markqvist/LXMF/releases/tag/${finalAttrs.src.tag}";
     # Reticulum License
     # https://github.com/markqvist/LXMF/blob/master/LICENSE
     license = lib.licenses.unfree;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "lxmd";
   };
-}
+})
