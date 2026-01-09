bb8856dfd647a42b4a74e835e6a6bee9e7096b04
Brandon Bennett <bbennett@coreweave.com>
2026-01-09 11:56:44 -0700

    python3Packages.cloudsmith-api: 2.0.22 -> 2.0.23

--- pkgs/development/python-modules/cloudsmith-api/default.nix
+++ pkgs/development/python-modules/cloudsmith-api/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "cloudsmith-api";
-  version = "2.0.22";
+  version = "2.0.23";
   format = "wheel";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "cloudsmith_api";
     inherit format version;
-    hash = "sha256-FZcDjrK5+oHC3dVBSXf+txW6hofP6OkmkjO4NJF05YQ=";
+    hash = "sha256-CfNgISONtTrabCoNUzHbMMVKDwGj8fdmSwh1FfzH10g=";
   };
 
   propagatedBuildInputs = [
