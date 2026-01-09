c075ed4e7d61402e76236819446ef94a5ed407a7
Sinjin2300 <root@sinjin.dev>
2026-01-08 16:04:52 -0800

    Fixed formatting issues and made consistent with finalAttrs pattern

--- pkgs/by-name/bi/bincrypter/package.nix
+++ pkgs/by-name/bi/bincrypter/package.nix
@@ -1,41 +1,50 @@
 {
-  pkgs,
   lib,
   fetchFromGitHub,
   stdenv,
+  perl,
+  openssl,
+  makeWrapper,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "bincrypter";
   version = "1.3";
 
   src = fetchFromGitHub {
     owner = "hackerschoice";
     repo = "bincrypter";
-    rev = "master";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-5xymjIHgYm0d3vpdNVpagq0tnUV2HlLV2DdcCXSso+Y=";
   };
 
-  buildInputs = with pkgs; [
+  buildInputs = [
     perl
     openssl
   ];
 
-  nativeBuildInputs = with pkgs; [ makeWrapper ];
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
 
     cp bincrypter.sh $out/bin/bincrypter
 
+    runHook postInstall
+  '';
+
+  postInstall = ''
     wrapProgram $out/bin/bincrypter \
       --prefix PATH : ${
         lib.makeBinPath [
-          pkgs.perl
-          pkgs.openssl
+          perl
+          openssl
         ]
       }
   '';
+
   meta = {
     description = "Pack/Encrypt/Obfuscate ELF + SHELL scripts";
     homepage = "https://github.com/hackerschoice/bincrypter";
@@ -44,4 +53,4 @@ stdenv.mkDerivation {
     maintainers = with lib.maintainers; [ sinjin2300 ];
     mainProgram = "bincrypter";
   };
-}
+})
