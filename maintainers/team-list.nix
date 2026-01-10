9c9a40bdacb6d29e9228945596e967925f98f6d4
Wolfgang Walther <walther@technowledgy.de>
2026-01-10 20:18:18 +0100

    teams/clevercloud: drop

--- maintainers/team-list.nix
+++ maintainers/team-list.nix
@@ -141,12 +141,6 @@ with lib.maintainers;
     enableFeatureFreezePing = true;
   };
 
-  clevercloud = {
-    members = [ floriansanderscc ];
-    scope = "Maintain Clever Cloud related packages.";
-    shortName = "CleverCloud";
-  };
-
   cloudposse = {
     members = [ dudymas ];
     scope = "Maintain atmos and applications made by the Cloud Posse team.";
