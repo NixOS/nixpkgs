adb5a5debe7e490ca1c24145d1cb609ee81a4da8
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-10 02:12:48 +0000

    rime-moegirl: 20251210 -> 20260109

--- pkgs/by-name/ri/rime-moegirl/package.nix
+++ pkgs/by-name/ri/rime-moegirl/package.nix
@@ -5,10 +5,10 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "rime-moegirl";
-  version = "20251210";
+  version = "20260109";
   src = fetchurl {
     url = "https://github.com/outloudvi/mw2fcitx/releases/download/${finalAttrs.version}/moegirl.dict.yaml";
-    hash = "sha256-P4AAwApknEaV4PFn6/5rpawp0iOGdDH+muYi+R/iErk=";
+    hash = "sha256-S8zQTHMMFF0D7uW7OGAOpHXWK1KggnZZ6blvqdcGKM4=";
   };
 
   dontUnpack = true;
