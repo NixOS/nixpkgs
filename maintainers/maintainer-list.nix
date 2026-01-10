0e4c25710c2b1fffda86fb3ba17ae30a523264d7
Ludovic Vanasse <mail@ludovicvanasse.com>
2026-01-10 12:54:33 -0500

    maintainers: add lvanasse

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15695,6 +15695,11 @@
     githubId = 208297;
     name = "Luz";
   };
+  lvanasse = {
+    github = "lvanasse";
+    githubId = 12837376;
+    name = "Ludovic Vanasse";
+  };
   lwb-2021 = {
     email = "lwb-2021@qq.com";
     github = "lwb-2021";
