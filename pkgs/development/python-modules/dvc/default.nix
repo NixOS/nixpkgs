1c214e116a9d9dfe4eac135ba738d44109f04c15
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-10 11:25:42 +0000

    python3Packages.dvc: 3.64.1 -> 3.66.1

--- pkgs/development/python-modules/dvc/default.nix
+++ pkgs/development/python-modules/dvc/default.nix
@@ -65,7 +65,7 @@
 
 buildPythonPackage rec {
   pname = "dvc";
-  version = "3.64.1";
+  version = "3.66.1";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -74,7 +74,7 @@ buildPythonPackage rec {
     owner = "iterative";
     repo = "dvc";
     tag = version;
-    hash = "sha256-zbqxJ6rlxiYZWFYmdqAlu00jkMLS0aIabVqMy5NAvUk=";
+    hash = "sha256-IvO5JEM3P0ggmufrBFv7fUBwoKzNIgWMUnOo31rYJRk=";
   };
 
   pythonRelaxDeps = [
