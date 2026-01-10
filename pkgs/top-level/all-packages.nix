bc4ec8c7742773979b72d3a0f709d2868855516a
Aliaksandr <grubian2@gmail.com>
2026-01-10 12:29:56 +0200

    xorg/overrides.nix: remove unused arguments, keep sorted aliases

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9407,11 +9407,7 @@ with pkgs;
       # Use `lib.callPackageWith __splicedPackages` rather than plain `callPackage`
       # so as not to have the newly bound xorg items already in scope,  which would
       # have created a cycle.
-      overrides = lib.callPackageWith __splicedPackages ../servers/x11/xorg/overrides.nix {
-        inherit (buildPackages.darwin) bootstrap_cmds;
-        udev = if stdenv.hostPlatform.isLinux then udev else null;
-        libdrm = if stdenv.hostPlatform.isLinux then libdrm else null;
-      };
+      overrides = lib.callPackageWith __splicedPackages ../servers/x11/xorg/overrides.nix { };
 
       generatedPackages = lib.callPackageWith __splicedPackages ../servers/x11/xorg/default.nix { };
 
