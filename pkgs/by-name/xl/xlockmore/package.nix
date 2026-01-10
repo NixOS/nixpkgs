f66bce5a308fc36079f7ed9971b9bfe6dd35a5a7
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-10 12:47:16 +0000

    xlockmore: 5.86 -> 5.87

--- pkgs/by-name/xl/xlockmore/package.nix
+++ pkgs/by-name/xl/xlockmore/package.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation rec {
   pname = "xlockmore";
-  version = "5.86";
+  version = "5.87";
 
   src = fetchurl {
     url = "http://sillycycle.com/xlock/xlockmore-${version}.tar.xz";
-    sha256 = "sha256-fvsR499tYAlTj9Ld8SpGpAbvh4iT5+USmZllCU3S9Fw=";
+    sha256 = "sha256-Pzu/xN3jCMqA05MHj1FTiSHaKzx11cKmBkIOEpVga3s=";
     curlOpts = "--user-agent 'Mozilla/5.0'";
   };
 
