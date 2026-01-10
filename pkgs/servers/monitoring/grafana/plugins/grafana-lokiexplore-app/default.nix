289ee50f2c6a43f959465e11501b56d00bb63ea6
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-10 08:16:07 +0000

    grafanaPlugins.grafana-lokiexplore-app: 1.0.31 -> 1.0.33

--- pkgs/servers/monitoring/grafana/plugins/grafana-lokiexplore-app/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-lokiexplore-app/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "grafana-lokiexplore-app";
-  version = "1.0.31";
-  zipHash = "sha256-IFt2w4DYgxYw86XngS8xfSAMqMdbf3fxml/KwGB8lLY=";
+  version = "1.0.33";
+  zipHash = "sha256-i8Wws/Xm7GYCk9I/SwLFBfJywr3vqp/OauoWOGMqWM0=";
   meta = {
     description = "Browse Loki logs without the need for writing complex queries";
     license = lib.licenses.agpl3Only;
