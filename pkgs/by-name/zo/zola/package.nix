6cef2925c5d8c0d76f72e0b684d8c3732c782388
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-10 03:01:14 +0000

    zola: 0.21.0 -> 0.22.0

--- pkgs/by-name/zo/zola/package.nix
+++ pkgs/by-name/zo/zola/package.nix
@@ -12,16 +12,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "zola";
-  version = "0.21.0";
+  version = "0.22.0";
 
   src = fetchFromGitHub {
     owner = "getzola";
     repo = "zola";
     rev = "v${version}";
-    hash = "sha256-+/0MhKKDSbOEa5btAZyaS3bQPeGJuski/07I4Q9v9cg=";
+    hash = "sha256-ahinXciTzG4fBtWKLSWP9uFG3Z3M7NHg60mcziMV1qM=";
   };
 
-  cargoHash = "sha256-K2wdq61FVVG9wJF+UcRZyZ2YSEw3iavboAGkzCcTGkU=";
+  cargoHash = "sha256-37AoQOfntpdLb0EPRmlgqrElLts7IGW0fr8g2TeTTRc=";
 
   nativeBuildInputs = [
     pkg-config
