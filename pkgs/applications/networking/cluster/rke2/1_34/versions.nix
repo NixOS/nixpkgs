f3b9dc5ab294e802ccf2417953181b6419edc740
azey <me@azey.net>
2026-01-10 13:06:11 +0100

    rke2: add helmJobVersion to linker flags
    See https://github.com/rancher/rke2/commit/e469fa94299cf37aa374a0530ee706627377fda2
    
    Related: be4ee5cc75020fc3034e9f236f2227ae0129518e
    (cherry picked from commit c1715b44dce181adef8be3ef54df0c5033c61f24)

--- pkgs/applications/networking/cluster/rke2/1_34/versions.nix
+++ pkgs/applications/networking/cluster/rke2/1_34/versions.nix
@@ -8,5 +8,6 @@
   pauseVersion = "3.6";
   ccmVersion = "v1.34.2-0.20251010190833-cf0d35a732d1-build20251017";
   dockerizedVersion = "v1.34.2-rke2r1";
+  helmJobVersion = "";
   imagesVersions = with builtins; fromJSON (readFile ./images-versions.json);
 }
