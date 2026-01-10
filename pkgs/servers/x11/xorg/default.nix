3a99675817746938546ebfd106550817601ebc9d
quantenzitrone <general@dev.quantenzitrone.eu>
2026-01-10 20:30:32 +0100

    xorg: clean up

--- pkgs/servers/x11/xorg/default.nix
+++ pkgs/servers/x11/xorg/default.nix
@@ -1,9 +1,12 @@
-# THIS IS A GENERATED FILE.  DO NOT EDIT!
 {
   lib,
+  config,
+  # keep-sorted start case=no numeric=no block=yes
   appres,
+  automake,
   bdftopcf,
   bitmap,
+  ed,
   editres,
   font-adobe-100dpi,
   font-adobe-75dpi,
@@ -39,10 +42,10 @@
   font-screen-cyrillic,
   font-sony-misc,
   font-sun-misc,
-  fonttosfnt,
   font-util,
   font-winitzki-cyrillic,
   font-xfree86-type1,
+  fonttosfnt,
   gccmakedep,
   iceauth,
   ico,
@@ -121,8 +124,8 @@
   xcmsdb,
   xcompmgr,
   xconsole,
-  xcursorgen,
   xcursor-themes,
+  xcursorgen,
   xdm,
   xdpyinfo,
   xdriinfo,
@@ -186,6 +189,7 @@
   xkbprint,
   xkbutils,
   xkeyboard-config,
+  xkeyboard-config_custom,
   xkill,
   xload,
   xlsatoms,
@@ -197,9 +201,9 @@
   xmore,
   xorg-cf-files,
   xorg-docs,
-  xorgproto,
   xorg-server,
   xorg-sgml-doctools,
+  xorgproto,
   xpr,
   xprop,
   xrandr,
@@ -215,11 +219,12 @@
   xwd,
   xwininfo,
   xwud,
+# keep-sorted end
 }:
-
-self: with self; {
-
+_:
+{
   inherit
+    # keep-sorted start case=no numeric=no block=yes
     appres
     bdftopcf
     bitmap
@@ -302,7 +307,10 @@ self: with self; {
     xwd
     xwininfo
     xwud
+    # keep-sorted end
     ;
+
+  # keep-sorted start case=no numeric=no block=yes
   encodings = font-encodings;
   fontadobe100dpi = font-adobe-100dpi;
   fontadobe75dpi = font-adobe-75dpi;
@@ -355,8 +363,8 @@ self: with self; {
   libXdmcp = libxdmcp;
   libXext = libxext;
   libXfixes = libxfixes;
-  libXfont2 = libxfont_2;
   libXfont = libxfont_1;
+  libXfont2 = libxfont_2;
   libXft = libxft;
   libXi = libxi;
   libXinerama = libxinerama;
@@ -375,14 +383,15 @@ self: with self; {
   libXxf86dga = libxxf86dga;
   libXxf86misc = libxxf86misc;
   libXxf86vm = libxxf86vm;
+  mkfontdir = mkfontscale;
   twm = tab-window-manager;
   utilmacros = util-macros;
   xcbproto = xcb-proto;
+  xcbutil = libxcb-util;
   xcbutilcursor = libxcb-cursor;
   xcbutilerrors = libxcb-errors;
   xcbutilimage = libxcb-image;
   xcbutilkeysyms = libxcb-keysyms;
-  xcbutil = libxcb-util;
   xcbutilrenderutil = libxcb-render-util;
   xcbutilwm = libxcb-wm;
   xcursorthemes = xcursor-themes;
@@ -431,9 +440,26 @@ self: with self; {
   xf86videovmware = xf86-video-vmware;
   xf86videovoodoo = xf86-video-voodoo;
   xkeyboardconfig = xkeyboard-config;
+  xkeyboardconfig_custom = xkeyboard-config_custom;
   xorgcffiles = xorg-cf-files;
   xorgdocs = xorg-docs;
   xorgserver = xorg-server;
   xorgsgmldoctools = xorg-sgml-doctools;
+  # keep-sorted end
+}
 
+# deprecate some packages
+// lib.optionalAttrs config.allowAliases {
+  # keep-sorted start case=no numeric=no block=yes
+  fontbitstreamspeedo = throw "Bitstream Speedo is an obsolete font format that hasn't been supported by Xorg since 2005"; # added 2025-09-24
+  libXtrap = throw "XTrap was a proposed X11 extension that hasn't been in Xorg since X11R6 in 1994, it is deprecated and archived upstream."; # added 2025-12-13
+  xf86videoglide = throw "The Xorg Glide video driver has been archived upstream due to being obsolete"; # added 2025-12-13
+  xf86videoglint = throw ''
+    The Xorg GLINT/Permedia video driver has been broken since xorg 21.
+    see https://gitlab.freedesktop.org/xorg/driver/xf86-video-glint/-/issues/1''; # added 2025-12-13
+  xf86videonewport = throw "The Xorg Newport video driver is broken and hasn't had a release since 2012"; # added 2025-12-13
+  xf86videotga = throw "The Xorg TGA (aka DEC 21030) video driver is broken and hasn't had a release since 2012"; # added 2025-12-13
+  xf86videowsfb = throw "The Xorg BSD wsdisplay framebuffer video driver is broken and hasn't had a release since 2012"; # added 2025-12-13
+  xtrap = throw "XTrap was a proposed X11 extension that hasn't been in Xorg since X11R6 in 1994, it is deprecated and archived upstream."; # added 2025-12-13
+  # keep-sorted end
 }
