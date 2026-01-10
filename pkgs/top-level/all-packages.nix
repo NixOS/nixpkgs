20af61d0983f4ae33e9024e0789009dea2983d75
Guy Chronister <guychronister@outlook.com>
2026-01-10 14:19:06 -0600

    persistent-evdev: migrate to by-name, refactor entire script
    namespaced-openvpn: migrate to by-name, refactor derivation
    
    pdd: migrate to by-name, refactor derivation
    
    tautulli: migrate to by-name, refactor derivation

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2228,8 +2228,6 @@ with pkgs;
 
   usb-modeswitch-data = callPackage ../by-name/us/usb-modeswitch/data.nix { };
 
-  persistent-evdev = python3Packages.callPackage ../servers/persistent-evdev { };
-
   inherit (import ../development/libraries/libsbsms pkgs)
     libsbsms
     libsbsms_2_0_2
@@ -3427,8 +3425,6 @@ with pkgs;
     inherit (llvmPackages) stdenv;
   };
 
-  namespaced-openvpn = python3Packages.callPackage ../tools/networking/namespaced-openvpn { };
-
   update-dotdee = with python3Packages; toPythonApplication update-dotdee;
 
   update-nix-fetchgit = haskell.lib.compose.justStaticExecutables haskellPackages.update-nix-fetchgit;
@@ -3497,8 +3493,6 @@ with pkgs;
     polkitSupport = true;
   };
 
-  pdd = python3Packages.callPackage ../tools/misc/pdd { };
-
   pdfminer = with python3Packages; toPythonApplication pdfminer-six;
 
   pdfium-binaries-v8 = pdfium-binaries.override { withV8 = true; };
@@ -3550,8 +3544,6 @@ with pkgs;
   plexRaw = callPackage ../servers/plex/raw.nix { };
 
   tabview = with python3Packages; toPythonApplication tabview;
-
-  tautulli = python3Packages.callPackage ../servers/tautulli { };
 
   plfit = callPackage ../by-name/pl/plfit/package.nix {
     python = null;
