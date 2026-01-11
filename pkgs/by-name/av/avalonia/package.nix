NixOS/nixpkgs

commit 28520d4f4c54e1dd37edca2ad60accb158f3cbaa

avalonia: 11.3.7 -> 11.3.9

pkgs/by-name/av/avalonia/package.nix
@@ -46,14 +46,14 @@ stdenvNoCC.mkDerivation (
     }
     rec {
       pname = "Avalonia";
-      version = "11.3.7";
+      version = "11.3.9";
 
       src = fetchFromGitHub {
         owner = "AvaloniaUI";
         repo = "Avalonia";
         tag = version;
         fetchSubmodules = true;
-        hash = "sha256-ZzMb8GGdEQsn4me3AewRJsBxGJ6M/dZ+mlQOYY3xaKs=";
+        hash = "sha256-qvkQKlz9GQayAxCPITYJbCk+w4d9xJNo+P1I9J1SYho=";
       };
 
       patches = [

commit 8eefc2b1cc312d5572f98ae7f99a497f3ccb1a29

avalonia: fix build failure
Fixes: 28520d4f4c54e1dd37edca2ad60accb158f3cbaa

pkgs/by-name/av/avalonia/0003-disable-hard-links.patch
@@ -1,0 +1,14 @@
+diff --git a/Directory.Build.props b/Directory.Build.props
+index f124456..117c096 100644
+--- a/Directory.Build.props
++++ b/Directory.Build.props
+@@ -9,9 +9,5 @@
+       <AddSyntheticProjectReferencesForSolutionDependencies>false</AddSyntheticProjectReferencesForSolutionDependencies>
+       <RunApiCompat>False</RunApiCompat>
+       <LangVersion>12</LangVersion>
+-      <CreateHardLinksForCopyAdditionalFilesIfPossible>true</CreateHardLinksForCopyAdditionalFilesIfPossible>
+-      <CreateHardLinksForCopyFilesToOutputDirectoryIfPossible>true</CreateHardLinksForCopyFilesToOutputDirectoryIfPossible>
+-      <CreateHardLinksForCopyLocalIfPossible>true</CreateHardLinksForCopyLocalIfPossible>
+-      <CreateHardLinksForPublishFilesIfPossible>true</CreateHardLinksForPublishFilesIfPossible>
+   </PropertyGroup>
+ </Project>

pkgs/by-name/av/avalonia/package.nix
@@ -62,6 +62,9 @@ stdenvNoCC.mkDerivation (
         # [ERR] Compile: [...]/Microsoft.NET.Sdk.targets(148,5): error MSB4018: The "GenerateDepsFile" task failed unexpectedly. [/build/source/src/tools/DevAnalyzers/DevAnalyzers.csproj]
         # [ERR] Compile: [...]/Microsoft.NET.Sdk.targets(148,5): error MSB4018: System.IO.IOException: The process cannot access the file '/build/source/src/tools/DevAnalyzers/bin/Release/netstandard2.0/DevAnalyzers.deps.json' because it is being used by another process. [/build/source/src/tools/DevAnalyzers/DevAnalyzers.csproj]
         ./0002-disable-parallel-compile.patch
+        # Microsoft.Common.CurrentVersion.targets(5034,5): error MSB3021: Unable to copy file "[...]/JetBrains.Annotations.dll" to "bin/Debug/JetBrains.Annotations.dll". Access to the path '/build/source/nukebuild/bin/Debug/JetBrains.Annotations.dll' is denied. [/build/source/nukebuild/_build.csproj]
+        # This happens because the source packages have symlinks due to linkNuGetPackagesAndSources.
+        ./0003-disable-hard-links.patch
       ];
 
       # this needs to be match the version being patched above