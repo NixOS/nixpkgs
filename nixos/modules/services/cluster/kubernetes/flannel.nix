bfd136593ba152868082f1b4e9adcd58f6540045
Patrick Collins <Patricol@users.noreply.github.com>
2026-01-11 14:41:50 -0700

    Update flannel rbac
    add missing permission; present in flannel docs.
    confirmed required in practice; getting errors without this.

--- nixos/modules/services/cluster/kubernetes/flannel.nix
+++ nixos/modules/services/cluster/kubernetes/flannel.nix
@@ -77,6 +77,7 @@ in
             kind = "ClusterRole";
             metadata = {
               name = "flannel";
+              labels.k8s-app = "flannel";
             };
             rules = [
               {
@@ -88,6 +89,7 @@ in
                 apiGroups = [ "" ];
                 resources = [ "nodes" ];
                 verbs = [
+                  "get"
                   "list"
                   "watch"
                 ];
