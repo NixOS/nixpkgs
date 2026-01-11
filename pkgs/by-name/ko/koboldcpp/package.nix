dca9025e7ef26edd0933e5ba50c458d9e918971c
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-11 20:58:51 +0000

    koboldcpp: 1.105.3 -> 1.105.4

--- pkgs/by-name/ko/koboldcpp/package.nix
+++ pkgs/by-name/ko/koboldcpp/package.nix
@@ -39,13 +39,13 @@ let
 in
 effectiveStdenv.mkDerivation (finalAttrs: {
   pname = "koboldcpp";
-  version = "1.105.3";
+  version = "1.105.4";
 
   src = fetchFromGitHub {
     owner = "LostRuins";
     repo = "koboldcpp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-8PgliEwe9ETFmdqHZdmbApz2uxhLLV/fcC2p/N8rOkY=";
+    hash = "sha256-m3rpvp7Lqs9RUEYwtxHjfNv0ByCWEVrtGvQFtzreMI4=";
   };
 
   enableParallelBuilding = true;
