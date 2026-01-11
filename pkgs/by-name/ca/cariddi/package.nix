08ba954e7223d5bd0726150a8ae6af898942c747
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-11 19:05:18 +0100

    cariddi: migrate to finalAttrs

--- pkgs/by-name/ca/cariddi/package.nix
+++ pkgs/by-name/ca/cariddi/package.nix
@@ -4,14 +4,14 @@
   fetchFromGitHub,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "cariddi";
   version = "1.4.5";
 
   src = fetchFromGitHub {
     owner = "edoardottt";
     repo = "cariddi";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-ULeG+TmpRbHT+GsEuEsSSCzlOwRzDzUZmPZNN7KZdrA=";
   };
 
@@ -25,9 +25,9 @@ buildGoModule rec {
   meta = {
     description = "Crawler for URLs and endpoints";
     homepage = "https://github.com/edoardottt/cariddi";
-    changelog = "https://github.com/edoardottt/cariddi/releases/tag/v${version}";
-    license = with lib.licenses; [ gpl3Plus ];
+    changelog = "https://github.com/edoardottt/cariddi/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "cariddi";
   };
-}
+})
