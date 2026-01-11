93497c91d9cac65128da800445e4d353618f0b87
N4CH72HR3R <n4ch7@n3831.net>
2026-01-11 19:46:38 +0100

    maintainers: add N4CH723HR3R

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18190,6 +18190,14 @@
     githubId = 254145391;
     name = "n3tshift";
   };
+  N4CH723HR3R = {
+    email = "n4ch7@n3831.net";
+    github = "N4CH723HR3R";
+    githubId = 221765713;
+    keys = [ { fingerprint = "C5A6 76E5 DFDD 076B 9B59  ABEC D783 89DC 24B6 4620"; } ];
+    matrix = "@n4ch7:n3831.net";
+    name = "N4CH723HR3R";
+  };
   n8henrie = {
     name = "Nathan Henrie";
     email = "nate@n8henrie.com";
