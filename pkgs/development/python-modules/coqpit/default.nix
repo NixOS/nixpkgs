703b789fa6f52d6735ec72b778e826a29fd5340a
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-11 12:12:25 +0100

    python313Pacakges.coqpit-config: modernize

--- pkgs/development/python-modules/coqpit/default.nix
+++ pkgs/development/python-modules/coqpit/default.nix
@@ -8,27 +8,23 @@
   typing-extensions,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "coqpit-config";
   version = "0.2.3";
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "idiap";
     repo = "coqui-ai-coqpit";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-WXzKXFnSGgt3aHY5yqPgNKd3ukJD7wZVNGddj981cDY=";
   };
 
+  build-system = [ hatchling ];
+
+  dependencies = [ typing-extensions ];
+
   nativeCheckInputs = [ pytestCheckHook ];
-
-  nativeBuildInputs = [
-    hatchling
-  ];
-
-  propagatedBuildInputs = [
-    typing-extensions
-  ];
 
   pythonImportsCheck = [
     "coqpit"
@@ -49,4 +45,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     teams = [ lib.teams.tts ];
   };
-}
+})
