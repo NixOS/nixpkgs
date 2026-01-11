e3f2f14d12aaf24a6b62e4271dee00c68134dd31
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-11 11:02:28 +0100

    python313Packages.whoisdomain: migrate to finalAttrs

--- pkgs/development/python-modules/whoisdomain/default.nix
+++ pkgs/development/python-modules/whoisdomain/default.nix
@@ -5,7 +5,7 @@
   hatchling,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "whoisdomain";
   version = "1.20260106.1";
   pyproject = true;
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "mboot-github";
     repo = "WhoisDomain";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-OQlOqDmBhqHVFs6U3lC1EryNu4UEi8fzKERkOE3uBaw=";
   };
 
@@ -27,9 +27,9 @@ buildPythonPackage rec {
   meta = {
     description = "Module to perform whois lookups";
     homepage = "https://github.com/mboot-github/WhoisDomain";
-    changelog = "https://github.com/mboot-github/WhoisDomain/releases/tag/${src.tag}";
+    changelog = "https://github.com/mboot-github/WhoisDomain/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "whoisdomain";
   };
-}
+})
