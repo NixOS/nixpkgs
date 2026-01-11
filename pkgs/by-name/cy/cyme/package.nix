05ece90df5d54b6c83c13b5a7a821bec00151307
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-11 23:34:29 +0000

    cyme: 2.2.8 -> 2.2.10

--- pkgs/by-name/cy/cyme/package.nix
+++ pkgs/by-name/cy/cyme/package.nix
@@ -12,16 +12,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cyme";
-  version = "2.2.8";
+  version = "2.2.10";
 
   src = fetchFromGitHub {
     owner = "tuna-f1sh";
     repo = "cyme";
     rev = "v${version}";
-    hash = "sha256-PHYzaynxzKYYNTB/pJVuSZoiN3/zwixjqoxXzbxToVc=";
+    hash = "sha256-0lr4iEHzkSoGQC9IdKvIfuX7sfXc6ygbl0U5R7xMjcA=";
   };
 
-  cargoHash = "sha256-NTsiAW9SLN6vuNtbdrXqtvutnI/2DLCgMjYxG6tTtFM=";
+  cargoHash = "sha256-tT3A9adB3IhPq1lr4v43e4RPIaUFEhXSvAWRsEfHxzw=";
 
   nativeBuildInputs = [
     pkg-config
