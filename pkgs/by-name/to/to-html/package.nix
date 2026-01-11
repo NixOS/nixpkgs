eefad8f0fe45a5b94745c69a1d67ecefacc90625
Eric Rodrigues Pires <eric@eric.dev.br>
2026-01-11 13:33:08 -0300

    to-html: add runtime dependencies
    Use `wrapProgram` to add `script` and `which` to `PATH`

--- pkgs/by-name/to/to-html/package.nix
+++ pkgs/by-name/to/to-html/package.nix
@@ -2,17 +2,20 @@
   lib,
   fetchFromGitHub,
   installShellFiles,
+  makeWrapper,
   rustPlatform,
+  unixtools,
+  which,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   version = "0.1.6";
   pname = "to-html";
 
   src = fetchFromGitHub {
     owner = "Aloso";
     repo = "to-html";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-eNFt9/yK4oHOspNM8PMTewhx8APaHzmgNdrWqrUuQSU=";
   };
 
@@ -21,21 +24,32 @@ rustPlatform.buildRustPackage rec {
   # Requires external resources
   doCheck = false;
 
-  nativeBuildInputs = [ installShellFiles ];
+  nativeBuildInputs = [
+    installShellFiles
+    makeWrapper
+  ];
 
   postInstall = ''
     installShellCompletion \
       $releaseDir/build/to-html-*/out/to-html.{bash,fish} \
       --zsh $releaseDir/build/to-html-*/out/_to-html
+
+    wrapProgram $out/bin/to-html \
+      --suffix PATH : ${
+        lib.makeBinPath [
+          unixtools.script
+          which
+        ]
+      }
   '';
 
   meta = {
     description = "Terminal wrapper for rendering a terminal on a website by converting ANSI escape sequences to HTML";
     mainProgram = "to-html";
     homepage = "https://github.com/Aloso/to-html";
-    changelog = "https://github.com/Aloso/to-html/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/Aloso/to-html/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.mit;
     platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ icewind1991 ];
   };
-}
+})
