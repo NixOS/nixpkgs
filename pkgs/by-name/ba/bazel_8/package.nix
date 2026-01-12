9202560f590a51ed24fc01e6fa9d8da56efba97b
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-12 20:54:06 +0000

    bazel_8: 8.5.0 -> 8.5.1

--- pkgs/by-name/ba/bazel_8/package.nix
+++ pkgs/by-name/ba/bazel_8/package.nix
@@ -31,7 +31,7 @@
   cctools,
   # Allow to independently override the jdks used to build and run respectively
   jdk_headless,
-  version ? "8.5.0",
+  version ? "8.5.1",
 }:
 
 let
@@ -45,7 +45,7 @@ let
 
   src = fetchzip {
     url = "https://github.com/bazelbuild/bazel/releases/download/${version}/bazel-${version}-dist.zip";
-    hash = "sha256-L8gnWpQAeHMUbydrrEtZ6WGIzhunDBWCNWMA+3dAKT0=";
+    hash = "sha256-XNWekT7XptGmDyR+H8TW8J0MNttLi8hlZvMWEg0ZNa0=";
     stripRoot = false;
   };
 
