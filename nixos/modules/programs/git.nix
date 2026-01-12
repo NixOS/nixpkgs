c1848da480650ab435bfefada1c5a22de4622abb
Kisel13 <aleksei201122@gmail.com>
2026-01-12 16:29:24 +0300

    git: filter-repo options

--- nixos/modules/programs/git.nix
+++ nixos/modules/programs/git.nix
@@ -84,6 +84,11 @@ in
 
         enablePureSSHTransfer = lib.mkEnableOption "Enable pure SSH transfer in server side by adding git-lfs-transfer to environment.systemPackages";
       };
+      filter-repo = {
+        enable = lib.mkEnableOption "git-filter-repo";
+
+        package = lib.mkPackageOption pkgs "git-filter-repo" { };
+      };
     };
   };
 
@@ -112,6 +117,11 @@ in
       environment.interactiveShellInit = ''
         source ${cfg.package}/share/bash-completion/completions/git-prompt.sh
       '';
+    })
+    (lib.mkIf (cfg.enable && cfg.filter-repo.enable) {
+      environment.systemPackages = [
+        cfg.filter-repo.package
+      ];
     })
   ];
 
