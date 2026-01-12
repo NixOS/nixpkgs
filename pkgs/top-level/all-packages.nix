87e1503fc9509e0199b570d1e6695a1520457890
quantenzitrone <general@dev.quantenzitrone.eu>
2026-01-12 17:33:31 +0100

    cassandra-cpp-driver: move to pkgs/by-name
    this shouldn't create any rebuilds

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8793,8 +8793,6 @@ with pkgs;
   };
   cassandra = cassandra_4;
 
-  cassandra-cpp-driver = callPackage ../development/libraries/cassandra-cpp-driver/default.nix { };
-
   apache-jena = callPackage ../servers/nosql/apache-jena/binary.nix {
     java = jre;
   };
