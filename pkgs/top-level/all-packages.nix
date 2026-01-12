18e0dc4dc0cf764dc3e43be06b2f5dc95ed08e53
quantenzitrone <general@dev.quantenzitrone.eu>
2026-01-12 17:36:31 +0100

    cctag: move to pkgs/by-name
    this shouldn't create any rebuilds

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6642,10 +6642,6 @@ with pkgs;
 
   ormolu = lib.getBin (haskell.lib.compose.justStaticExecutables haskellPackages.ormolu);
 
-  cctag = callPackage ../development/libraries/cctag {
-    stdenv = clangStdenv;
-  };
-
   ceedling = callPackage ../development/tools/ceedling { };
 
   celt = callPackage ../development/libraries/celt { };
