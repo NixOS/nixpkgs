f67c5771ea96d2d4d153b4804ed31f22c00b172b
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-12 17:14:42 +0100

    python313Packages.python-tado: 0.18.15 -> 0.18.16
    Diff: https://github.com/wmalgadey/PyTado/compare/0.18.15...0.18.16
    
    Changelog: https://github.com/wmalgadey/PyTado/releases/tag/0.18.16

--- pkgs/development/python-modules/python-tado/default.nix
+++ pkgs/development/python-modules/python-tado/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "python-tado";
-  version = "0.18.15";
+  version = "0.18.16";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "wmalgadey";
     repo = "PyTado";
     tag = version;
-    hash = "sha256-FUnD5JVS816XQYqXGSDnypqcYuKVhEeFIFcENf8BkcU=";
+    hash = "sha256-jHPTu0/DYJXbSqiJXQzmiK6gmtJf88Y0BV1wj/X+qpc=";
   };
 
   build-system = [ setuptools ];
