6e8b5e78afd44204f4f8cf4c2abfcb872206e8c4
Sarah Clark <seclark@nextquestion.net>
2026-01-12 09:46:03 -0800

    python3Packages.fickling: apply finalAttrs fix

--- pkgs/development/python-modules/fickling/default.nix
+++ pkgs/development/python-modules/fickling/default.nix
@@ -10,7 +10,7 @@
   torchvision,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "fickling";
   version = "0.1.6";
   pyproject = true;
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "trailofbits";
     repo = "fickling";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-p2XkHKqheVHqLTQKmUApiYH7NIaHc091B/TjiCDYWtA=";
   };
 
@@ -40,7 +40,10 @@ buildPythonPackage rec {
     ];
   };
 
-  nativeCheckInputs = [ pytestCheckHook ] ++ lib.concatAttrValues optional-dependencies;
+  nativeCheckInputs = [
+    pytestCheckHook
+  ]
+  ++ lib.concatAttrValues finalAttrs.passthru.optional-dependencies;
 
   disabledTestPaths = [
     # https://github.com/trailofbits/fickling/issues/162
@@ -53,8 +56,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python pickling decompiler and static analyzer";
     homepage = "https://github.com/trailofbits/fickling";
-    changelog = "https://github.com/trailofbits/fickling/releases/tag/${src.tag}";
+    changelog = "https://github.com/trailofbits/fickling/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.lgpl3Plus;
     maintainers = with lib.maintainers; [ sarahec ];
   };
-}
+})
