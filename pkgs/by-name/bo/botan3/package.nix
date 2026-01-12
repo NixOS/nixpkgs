2bad65397369090b42938882e8c288336957f76c
nikstur <nikstur@outlook.com>
2026-01-12 16:20:35 +0100

    botan3: remove superfluous version checks
    These have become obsolete since we dropped other botan versions.

--- pkgs/by-name/bo/botan3/package.nix
+++ pkgs/by-name/bo/botan3/package.nix
@@ -92,15 +92,12 @@ stdenv.mkDerivation (finalAttrs: {
   ++ lib.optionals (stdenv.hostPlatform.isLinux && withTpm2) [
     tpm2-tss
   ]
-  ++ lib.optionals (lib.versionAtLeast finalAttrs.version "3.6.0" && !stdenv.hostPlatform.isMinGW) [
+  ++ lib.optionals (!stdenv.hostPlatform.isMinGW) [
     jitterentropy
   ]
-  ++
-    lib.optionals
-      (lib.versionAtLeast finalAttrs.version "3.7.0" && withEsdm && !stdenv.hostPlatform.isMinGW)
-      [
-        esdm
-      ]
+  ++ lib.optionals (withEsdm && !stdenv.hostPlatform.isMinGW) [
+    esdm
+  ]
   ++ lib.optionals (stdenv.hostPlatform.isMinGW) [
     windows.pthreads
   ];
@@ -132,19 +129,16 @@ stdenv.mkDerivation (finalAttrs: {
   ++ lib.optionals (stdenv.hostPlatform.isLinux && withTpm2) [
     "--with-tpm2"
   ]
-  ++ lib.optionals (lib.versionAtLeast finalAttrs.version "3.6.0" && !stdenv.hostPlatform.isMinGW) [
+  ++ lib.optionals (!stdenv.hostPlatform.isMinGW) [
     "--enable-modules=jitter_rng"
   ]
-  ++
-    lib.optionals
-      (lib.versionAtLeast finalAttrs.version "3.7.0" && withEsdm && !stdenv.hostPlatform.isMinGW)
-      [
-        "--enable-modules=esdm_rng"
-      ]
-  ++ lib.optionals (lib.versionAtLeast finalAttrs.version "3.8.0" && policy != null) [
+  ++ lib.optionals (withEsdm && !stdenv.hostPlatform.isMinGW) [
+    "--enable-modules=esdm_rng"
+  ]
+  ++ lib.optionals (policy != null) [
     "--module-policy=${policy}"
   ]
-  ++ lib.optionals (lib.versionAtLeast finalAttrs.version "3.8.0" && policy == "bsi") [
+  ++ lib.optionals (policy == "bsi") [
     "--enable-module=ffi"
     "--enable-module=shake"
   ]
