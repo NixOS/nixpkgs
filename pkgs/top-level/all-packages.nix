f5b2100856920d73e490be012615c35ed848fdc4
quantenzitrone <general@dev.quantenzitrone.eu>
2026-01-12 17:30:10 +0100

    capstone{,4}: move to pkgs/by-name
    this shouldn't create any rebuilds

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1717,9 +1717,6 @@ with pkgs;
       '';
     });
 
-  capstone = callPackage ../development/libraries/capstone { };
-  capstone_4 = callPackage ../development/libraries/capstone/4.nix { };
-
   ceres-solver = callPackage ../development/libraries/ceres-solver {
     gflags = null; # only required for examples/tests
   };
