93edc58cf15a6ddb31991489e5b6a601999f71c0
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-13 03:17:02 +0000

    terramate: 0.15.3 -> 0.15.4

--- pkgs/by-name/te/terramate/package.nix
+++ pkgs/by-name/te/terramate/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule rec {
   pname = "terramate";
-  version = "0.15.3";
+  version = "0.15.4";
 
   src = fetchFromGitHub {
     owner = "terramate-io";
     repo = "terramate";
     rev = "v${version}";
-    hash = "sha256-oiumUA8uYEbE27PYedqme4oLTzJpuRXkqGhlmvYu8g8=";
+    hash = "sha256-DSxXX3HpIFM0F/+kqTFw0gvYfFfrGzoUt7GLDM42NuI=";
   };
 
   vendorHash = "sha256-o2MNmJmYMq2mQBLslOtRyRH9euEhcJyIhohJ3CKp6kg=";
