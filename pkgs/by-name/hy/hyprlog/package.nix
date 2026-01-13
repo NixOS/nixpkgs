a0f44637bccb4657abc0fa415ab35f68c306a5cc
gusjengis <anthony.j.green@outlook.com>
2026-01-12 23:00:43 -0800

    filled in hash

--- pkgs/by-name/hy/hyprlog/package.nix
+++ pkgs/by-name/hy/hyprlog/package.nix
@@ -11,7 +11,7 @@ let
     owner = "oxalica";
     repo = "rust-overlay";
     rev = "master";
-    hash = lib.fakeHash;
+    hash = "sha256-Tg/kL8eKMpZtceDvBDQYU8zowgpr7ucFRnpP/AtfuRM=";
   };
 
   rustOverlay = import rustOverlaySrc;
