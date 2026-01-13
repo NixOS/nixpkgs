6af75ec62910be7da9965577b5c622dbdd78d34e
jopejoe1 <jopejoe1@missing.ninja>
2026-01-13 21:05:13 +0100

    linuxPackages.ena: add pname

--- pkgs/os-specific/linux/ena/default.nix
+++ pkgs/os-specific/linux/ena/default.nix
@@ -8,16 +8,16 @@
 }:
 let
   rev-prefix = "ena_linux_";
-  version = "2.16.0";
 in
-stdenv.mkDerivation {
-  inherit version;
-  name = "ena-${version}-${kernel.version}";
+stdenv.mkDerivation (finalAttrs: {
+  version = "2.16.0";
+  pname = "ena";
+  name = "${finalAttrs.pname}-${finalAttrs.version}-${kernel.version}";
 
   src = fetchFromGitHub {
     owner = "amzn";
     repo = "amzn-drivers";
-    rev = "${rev-prefix}${version}";
+    rev = "${rev-prefix}${finalAttrs.version}";
     hash = "sha256-7gPo3wPMpKPOkmZJzzpt0GdCdX/1N/Xqty1Hg+fQQlU=";
   };
 
@@ -59,4 +59,4 @@ stdenv.mkDerivation {
     ];
     platforms = lib.platforms.linux;
   };
-}
+})
