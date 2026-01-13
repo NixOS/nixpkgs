41eaf660714679cde74d883a27de1c172f93d139
Robert Schütz <nix@dotlambda.de>
2026-01-13 01:48:17 -0800

    home-assistant.python.pkgs.pytest-homeassistant-custom-component: 0.13.305 -> 0.13.306
    Diff: https://github.com/MatthewFlamm/pytest-homeassistant-custom-component/compare/0.13.305...0.13.306
    
    Changelog: https://github.com/MatthewFlamm/pytest-homeassistant-custom-component/blob/0.13.306/CHANGELOG.md

--- pkgs/servers/home-assistant/pytest-homeassistant-custom-component.nix
+++ pkgs/servers/home-assistant/pytest-homeassistant-custom-component.nix
@@ -18,7 +18,7 @@
 
 buildPythonPackage rec {
   pname = "pytest-homeassistant-custom-component";
-  version = "0.13.305";
+  version = "0.13.306";
   pyproject = true;
 
   disabled = pythonOlder "3.13";
@@ -27,7 +27,7 @@ buildPythonPackage rec {
     owner = "MatthewFlamm";
     repo = "pytest-homeassistant-custom-component";
     tag = version;
-    hash = "sha256-wxoG5xnlvViWgu0cYIG84coOGJrFVuaX5jHoHXm9s9s=";
+    hash = "sha256-RcUI15/+0nu453mcrRzMo2VvJAn3Up336LiuJygJaM0=";
   };
 
   build-system = [ setuptools ];
