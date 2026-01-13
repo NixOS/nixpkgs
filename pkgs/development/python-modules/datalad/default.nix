7817e04340b7eeb61dcbaceeb35cf8e38c5219b8
Sarah Clark <seclark@nextquestion.net>
2026-01-13 15:39:51 -0800

    python3Packages.datalad: disable broken test on python 3.14

--- pkgs/development/python-modules/datalad/default.nix
+++ pkgs/development/python-modules/datalad/default.nix
@@ -4,9 +4,9 @@
   setuptools,
   stdenv,
   fetchFromGitHub,
+  pythonAtLeast,
   installShellFiles,
   git,
-  coreutils,
   versioneer,
   # core
   platformdirs,
@@ -218,6 +218,17 @@ buildPythonPackage rec {
 
     # CommandError: 'git -c diff.ignoreSubmodules=none -c core.quotepath=false ls-files -z -m -d' failed with exitcode 128
     "test_subsuperdataset_save"
+  ]
+  ++ lib.optionals (pythonAtLeast "3.14") [
+    # For all: https://github.com/datalad/datalad/issues/7781
+    # AssertionError: `assert 1 == 0` (refcount error)
+    "test_GitRepo_flyweight"
+    "test_Dataset_flyweight"
+    "test_AnnexRepo_flyweight"
+    # TypeError: cannot pickle '_thread.lock' object
+    "test_popen_invocation"
+    # datalad.runner.exception.CommandError: '/python3.14 -i -u -q -']' timed out after 0.5 seconds
+    "test_asyncio_loop_noninterference1"
   ];
 
   nativeCheckInputs = [
