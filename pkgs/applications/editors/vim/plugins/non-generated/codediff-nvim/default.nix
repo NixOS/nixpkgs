56b3aaf29bd7d0a9680b83d5ee631c2ae8148a35
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-13 05:08:29 +0000

    vimPlugins.codediff-nvim: 2.9.1 -> 2.9.3
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- pkgs/applications/editors/vim/plugins/non-generated/codediff-nvim/default.nix
+++ pkgs/applications/editors/vim/plugins/non-generated/codediff-nvim/default.nix
@@ -8,13 +8,13 @@
 }:
 vimUtils.buildVimPlugin rec {
   pname = "codediff.nvim";
-  version = "2.9.1";
+  version = "2.9.3";
 
   src = fetchFromGitHub {
     owner = "esmuellert";
     repo = "codediff.nvim";
     tag = "v${version}";
-    hash = "sha256-xIm3/Dxn77rRtUwaKE+3xed8Yyrfnte/aroRcgqiuXM=";
+    hash = "sha256-hHBAGDWtw56gk1kWUtU9QEP2QqIATmPNeJ2lPD8dWIc=";
   };
 
   dependencies = [ vimPlugins.nui-nvim ];
