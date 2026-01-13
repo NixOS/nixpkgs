bf4f71d5ba96a27f446d13b71e62cd00e89718af
Sizhe Zhao <prc.zhao@outlook.com>
2026-01-13 16:18:33 +0800

    sing-box: 1.12.15 -> 1.12.16

--- pkgs/by-name/si/sing-box/package.nix
+++ pkgs/by-name/si/sing-box/package.nix
@@ -10,16 +10,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "sing-box";
-  version = "1.12.15";
+  version = "1.12.16";
 
   src = fetchFromGitHub {
     owner = "SagerNet";
     repo = "sing-box";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-3YUeRV2WmWXSd6oKluWOorDkENy7xLu0aGzqS3AvaxE=";
+    hash = "sha256-enmT6BnGzEpk9BPB9IZWJctMoe/FiNqYmkiq0d9/m2k=";
   };
 
-  vendorHash = "sha256-mSQHW8QwXA6TmpXexhWlgY0BWsNRPOnmGAGvPvnX8wU=";
+  vendorHash = "sha256-rs5Jxj75qU47zEH0gbYy0/QUSq3sektaITcefg2Qb78=";
 
   tags = [
     "with_quic"
