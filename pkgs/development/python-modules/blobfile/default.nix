b80eeeb7319c67fa4a7a0b066a643b407e1534f2
Sigmanificient <edhyjox@gmail.com>
2026-01-13 23:50:57 +0100

    python313Packages.blobfile: modernize

--- pkgs/development/python-modules/blobfile/default.nix
+++ pkgs/development/python-modules/blobfile/default.nix
@@ -2,25 +2,28 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   filelock,
   lxml,
   pycryptodomex,
   urllib3,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "blobfile";
   version = "3.0.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "christopher-hesse";
     repo = "blobfile";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-/v48rLvlN4lsfWKJvXRNuIO6jdsCgRcSPlJzdOfl3xk=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     pycryptodomex
     filelock
     urllib3
@@ -35,8 +38,8 @@ buildPythonPackage rec {
   meta = {
     description = "Read Google Cloud Storage, Azure Blobs, and local paths with the same interface";
     homepage = "https://github.com/christopher-hesse/blobfile";
-    changelog = "https://github.com/christopher-hesse/blobfile/blob/v${version}/CHANGES.md";
+    changelog = "https://github.com/christopher-hesse/blobfile/blob/v${finalAttrs.version}/CHANGES.md";
     license = lib.licenses.unlicense;
     maintainers = with lib.maintainers; [ happysalada ];
   };
-}
+})
