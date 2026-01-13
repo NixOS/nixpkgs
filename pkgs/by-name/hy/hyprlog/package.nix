75c702a877b3de71170aaaa6661ca3db8bc10d56
gusjengis <anthony.j.green@outlook.com>
2026-01-12 20:47:36 -0800

    filled in package, missing hash

---
+++ pkgs/by-name/hy/hyprlog/package.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "hyprlog";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "gusjengis";
+    repo = "hyprlog";
+    rev = "v${version}";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  meta = with lib; {
+    description = "Hyprland focus/activity logger";
+    homepage = "https://github.com/gusjengis/hyprlog";
+    license = licenses.mit;
+    maintainers = [ maintainers.gusjengis ];
+    mainProgram = "hyprlog";
+    platforms = platforms.linux;
+  };
+}
