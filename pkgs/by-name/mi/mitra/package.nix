69b2a97d90eb9ec6d22e2c0ed98530d98da9ccb6
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-13 15:53:48 +0000

    mitra: 4.16.0 -> 4.16.1

--- pkgs/by-name/mi/mitra/package.nix
+++ pkgs/by-name/mi/mitra/package.nix
@@ -8,17 +8,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "mitra";
-  version = "4.16.0";
+  version = "4.16.1";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "silverpill";
     repo = "mitra";
     rev = "v${version}";
-    hash = "sha256-Z3vJ2myo2fzBbH8P+JYzK9W4rlV4UaoySY/MMLhOvI4=";
+    hash = "sha256-nQhzU3LMEyqa2CciNTX5/+ViMqjmwDt3QrMZnbt/tBU=";
   };
 
-  cargoHash = "sha256-YWOGJtOu84WLKDqwhLIxYlYXetkn9YnW17U5MF/VFM8=";
+  cargoHash = "sha256-aWBJ3PDHcqm73P4oOpuSlalT5LxRgSqhuC0f0/sL+lg=";
 
   # require running database
   doCheck = false;
