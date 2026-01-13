121ff0e927127fb6b961e361dd9dda22ea149a81
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-13 09:30:15 +0100

    usbrip: modernize

--- pkgs/by-name/us/usbrip/package.nix
+++ pkgs/by-name/us/usbrip/package.nix
@@ -4,7 +4,7 @@
   python3,
 }:
 
-python3.pkgs.buildPythonApplication {
+python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "usbrip";
   version = "0-unstable-2021-07-02";
   pyproject = true;
@@ -13,8 +13,15 @@ python3.pkgs.buildPythonApplication {
     owner = "snovvcrash";
     repo = "usbrip";
     rev = "0f3701607ba13212ebefb4bbd9e68ec0e22d76ac";
-    sha256 = "1vws8ybhv7szpqvlbmv0hrkys2fhhaa5bj9dywv3q2y1xmljl0py";
+    hash = "sha256-/gIqae3BCzw29y3JVZSC0AntZ4Zg10U3vl+fDZdHmu8=";
   };
+
+  postPatch = ''
+    # Remove install helpers which we don't need
+    substituteInPlace setup.py \
+      --replace-fail "resolve('wheel')" "" \
+      --replace-fail "'install': LocalInstallCommand," ""
+  '';
 
   build-system = with python3.pkgs; [ setuptools ];
 
@@ -24,13 +31,6 @@ python3.pkgs.buildPythonApplication {
     tqdm
   ];
 
-  postPatch = ''
-    # Remove install helpers which we don't need
-    substituteInPlace setup.py \
-      --replace-fail "resolve('wheel')" "" \
-      --replace-fail "'install': LocalInstallCommand," ""
-  '';
-
   # Project has no tests
   doCheck = false;
 
@@ -38,10 +38,10 @@ python3.pkgs.buildPythonApplication {
 
   meta = {
     description = "Tool to track the history of USB events";
-    mainProgram = "usbrip";
     homepage = "https://github.com/snovvcrash/usbrip";
-    license = with lib.licenses; [ gpl3Plus ];
+    license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ fab ];
+    mainProgram = "usbrip";
     platforms = lib.platforms.linux;
   };
-}
+})
