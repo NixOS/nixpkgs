db2d7b46824586f9e51bf42021c4ca0f3e67bf23
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-13 12:49:18 +0100

    alerta: 8.5.3 -> 9.0.4
    Changelog: https://github.com/alerta/alerta/releases/tag/v9.0.4

--- pkgs/by-name/al/alerta/package.nix
+++ pkgs/by-name/al/alerta/package.nix
@@ -1,36 +1,62 @@
 {
   lib,
+  fetchFromGitHub,
   python3,
-  fetchPypi,
 }:
 
-python3.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "alerta";
-  version = "8.5.3";
-  format = "setuptools";
+  version = "9.0.4";
+  pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-ePvT2icsgv+io5aDDUr1Zhfodm4wlqh/iqXtNkFhS10=";
+  src = fetchFromGitHub {
+    owner = "alerta";
+    repo = "alerta";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-QK06Iosr4LXYA/WEQWuizLYElWfESPr7pgjkSXOmX4s=";
   };
 
-  propagatedBuildInputs = with python3.pkgs; [
-    six
-    click
-    requests
-    requests-hawk
-    pytz
-    tabulate
-  ];
+  build-system = with python3.pkgs; [ setuptools ];
 
+  dependencies =
+    with python3.pkgs;
+    [
+      bcrypt
+      blinker
+      cryptography
+      flask
+      flask-compress
+      flask-cors
+      mohawk
+      psycopg2
+      pyjwt
+      pymongo
+      pyparsing
+      python-dateutil
+      pytz
+      pyyaml
+      requests
+      requests-hawk
+      sentry-sdk
+      strenum
+      werkzeug
+    ]
+    ++ sentry-sdk.optional-dependencies.flask;
+
+  optional-dependencies = {
+    psycopg2 = with python3.pkgs; [ psycopg2 ];
+    pymongo = with python3.pkgs; [ pymongo ];
+  };
+
+  # Tests require a mongodb instance
   doCheck = false;
 
-  disabled = python3.pythonOlder "3.6";
-
   meta = {
-    homepage = "https://alerta.io";
     description = "Alerta Monitoring System command-line interface";
-    mainProgram = "alerta";
+    homepage = "https://alerta.io";
+    changelog = "https://github.com/alerta/alerta/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.asl20;
+    maintainers = [ ];
+    mainProgram = "alerta";
   };
-}
+})
