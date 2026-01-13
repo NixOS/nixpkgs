c1627b7940ca98c52fa79b06d6ed38db8cf1bfe3
whispers <whispersofthedawn@duck.com>
2026-01-13 07:49:23 -0500

    mullvad-browser: 15.0.3 -> 15.0.4
    Release announcement: github.com/mullvad/mullvad-browser/releases/tag/15.0.4
    Corresponding Firefox ESR: https://www.firefox.com/en-US/firefox/140.7.0/releasenotes/

--- pkgs/by-name/mu/mullvad-browser/package.nix
+++ pkgs/by-name/mu/mullvad-browser/package.nix
@@ -97,7 +97,7 @@ let
     ++ lib.optionals mediaSupport [ ffmpeg_7 ]
   );
 
-  version = "15.0.3";
+  version = "15.0.4";
 
   sources = {
     x86_64-linux = fetchurl {
@@ -109,7 +109,7 @@ let
         "https://tor.eff.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
         "https://tor.calyxinstitute.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
       ];
-      hash = "sha256-qrjKGaS4GYp8C4LzElbN9/+v5MYKsVRHaZDsBZe2Bcs=";
+      hash = "sha256-l41Sn69hvQXwb13T1o6ukIWOxpXuARA+J8+QjNh+bdo=";
     };
   };
 
