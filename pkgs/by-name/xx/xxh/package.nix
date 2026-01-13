fdff277f376f46b7c5e6609e93b7ccfec2d680af
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-13 09:27:20 +0100

    xxh: modernize

--- pkgs/by-name/xx/xxh/package.nix
+++ pkgs/by-name/xx/xxh/package.nix
@@ -5,7 +5,8 @@
   openssh,
   nixosTests,
 }:
-python3.pkgs.buildPythonApplication rec {
+
+python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "xxh";
   version = "0.8.14";
   pyproject = true;
@@ -13,19 +14,19 @@ python3.pkgs.buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "xxh";
     repo = "xxh";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-Y1yTn0lZemQgWsW9wlW+aNndyTXGo46PCbCl0TGYspQ=";
   };
 
-  build-system = [
-    python3.pkgs.setuptools
-  ];
+  build-system = with python3.pkgs; [ setuptools ];
 
   dependencies = [
-    python3.pkgs.pexpect
-    python3.pkgs.pyyaml
     openssh
-  ];
+  ]
+  ++ (with python3.pkgs; [
+    pexpect
+    pyyaml
+  ]);
 
   passthru.tests = {
     inherit (nixosTests) xxh;
@@ -34,7 +35,8 @@ python3.pkgs.buildPythonApplication rec {
   meta = {
     description = "Bring your favorite shell wherever you go through SSH";
     homepage = "https://github.com/xxh/xxh";
+    changelog = "https://github.com/xxh/xxh/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.bsd2;
     maintainers = with lib.maintainers; [ pasqui23 ];
   };
-}
+})
