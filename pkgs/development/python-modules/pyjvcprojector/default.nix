6b93ea713353dbe4b1db4857d04db0495cd31888
Robert Schütz <nix@dotlambda.de>
2026-01-14 04:21:44 -0800

    python3Packages.pyjvcprojector: 1.1.3 -> 2.0.0
    Diff: https://github.com/SteveEasley/pyjvcprojector/compare/v1.1.3...v2.0.0
    
    Changelog: https://github.com/SteveEasley/pyjvcprojector/releases/tag/v2.0.0

--- pkgs/development/python-modules/pyjvcprojector/default.nix
+++ pkgs/development/python-modules/pyjvcprojector/default.nix
@@ -1,5 +1,4 @@
 {
-  aiodns,
   buildPythonPackage,
   fetchFromGitHub,
   lib,
@@ -10,21 +9,17 @@
 
 buildPythonPackage rec {
   pname = "pyjvcprojector";
-  version = "1.1.3";
+  version = "2.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "SteveEasley";
     repo = "pyjvcprojector";
     tag = "v${version}";
-    hash = "sha256-j8/PHGElJf45SptvIWDt1M1PEkbtHCW3XDQezUHdOM4=";
+    hash = "sha256-c8xhpcr8HmTDTzoGn9vwsvNl2O+EamRbG9Nf6lgqw7c=";
   };
 
   build-system = [ setuptools ];
-
-  dependencies = [
-    aiodns
-  ];
 
   pythonImportsCheck = [ "jvcprojector" ];
 
