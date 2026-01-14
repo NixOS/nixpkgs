c01e9c5060b8bbb06c758c00a55a9e3f441c2485
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-14 01:18:26 +0000

    lockbook: 25.12.15 -> 26.1.13
    (cherry picked from commit d29b0fe9f1db11c186373e189c3c6b0c2a85eba0)

--- pkgs/by-name/lo/lockbook/package.nix
+++ pkgs/by-name/lo/lockbook/package.nix
@@ -12,16 +12,16 @@ let
 in
 rustPlatform.buildRustPackage rec {
   pname = "lockbook";
-  version = "25.12.15";
+  version = "26.1.13";
 
   src = fetchFromGitHub {
     owner = "lockbook";
     repo = "lockbook";
     tag = version;
-    hash = "sha256-bpT8xJST/VJvXIqrukv3OM3xD2zvX5ZqzmLG1BKzXyU=";
+    hash = "sha256-1+56xdfrRpjhb536vC8kwZ+fyGpQ38wPH7clkApIOUs=";
   };
 
-  cargoHash = "sha256-tcaLIE4Sn1+C9KIqm3f4ycf/FZtO8QQxCg5mof+TH0I=";
+  cargoHash = "sha256-JCWBG8jt8IxMaUfZaC+YnDfYSmpH/Jm1sqvEcHI8aA0=";
 
   doCheck = false; # there are no cli tests
   cargoBuildFlags = [
