6d3d331edc0afe1302c0564af826a3a20e9c9e7c
David McFarland <corngood@gmail.com>
2026-01-14 16:07:37 -0400

    space-station-14-launcher: use dotnet 8 packages from nixpkgs

--- pkgs/by-name/sp/space-station-14-launcher/package.nix
+++ pkgs/by-name/sp/space-station-14-launcher/package.nix
@@ -66,7 +66,9 @@ buildDotnetModule rec {
     inherit version;
   };
 
-  dotnet-sdk = dotnetCorePackages.sdk_10_0;
+  dotnet-sdk = dotnetCorePackages.sdk_10_0 // {
+    inherit (dotnetCorePackages.sdk_8_0) packages;
+  };
   dotnet-runtime = dotnetCorePackages.runtime_10_0;
 
   dotnetFlags = [
