995ba26f30a73eec0785f4279346f293ace2aff1
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-14 19:43:18 +0000

    mcp-grafana: 0.7.10 -> 0.8.2

--- pkgs/by-name/mc/mcp-grafana/package.nix
+++ pkgs/by-name/mc/mcp-grafana/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "mcp-grafana";
-  version = "0.7.10";
+  version = "0.8.2";
 
   src = fetchFromGitHub {
     owner = "grafana";
     repo = "mcp-grafana";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-DDkIWCJneL7l59CThzPkHzcB/lcUZrcVDZO/nWsZ2ss=";
+    hash = "sha256-oMCRZaAkvotP0oD4MjdNASDRk0FUYnH2vR22IojilN8=";
   };
 
-  vendorHash = "sha256-4dOsXrwUk+muYLIec9hBdMl/W3lk/pMvliEWeYrU5zQ=";
+  vendorHash = "sha256-SjzOmWU+mrEdLsgeT1Y2+A3skOzWHxJXxWke7aHrt+A=";
 
   ldflags = [
     "-s"
