fee9694a1cfdffd2f73fb100186ca8ac64214338
gusjengis <anthony.j.green@outlook.com>
2026-01-13 20:48:31 -0800

    hyprlog: added package.nix

---
+++ pkgs/by-name/hy/hyprlog/package.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "hyprlog";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "gusjengis";
+    repo = "hyprlog";
+    rev = "v${version}";
+    hash = "sha256-nHTUa7UpdlsO4TUrQfGo82KlgXkG3RxC9iyfiDOmOyQ=";
+  };
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+  };
+
+  meta = with lib; {
+    description = "Hyprland focus/activity logger";
+    homepage = "https://github.com/gusjengis/hyprlog";
+    license = licenses.mit;
+    maintainers = [ maintainers.gusjengis ];
+    mainProgram = "hyprlog";
+    platforms = platforms.linux;
+  };
+}
