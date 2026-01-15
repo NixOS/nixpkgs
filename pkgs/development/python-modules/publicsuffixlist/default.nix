0fdd159f670ea00841606d36155dee43945456f5
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-15 08:13:00 +0100

    python313Packages.publicsuffixlist: migrate to finalAttrs

--- pkgs/development/python-modules/publicsuffixlist/default.nix
+++ pkgs/development/python-modules/publicsuffixlist/default.nix
@@ -9,13 +9,13 @@
   publicsuffix-list,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "publicsuffixlist";
   version = "1.0.2.20260114";
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-PO+kopKEHUBqbFoHvGVQQVbssb4d2ZzXMLmLYzLx1S0=";
   };
 
@@ -38,11 +38,11 @@ buildPythonPackage rec {
   enabledTestPaths = [ "publicsuffixlist/test.py" ];
 
   meta = {
-    changelog = "https://github.com/ko-zu/psl/blob/v${version}-gha/CHANGES.md";
     description = "Public Suffix List parser implementation";
     homepage = "https://github.com/ko-zu/psl";
+    changelog = "https://github.com/ko-zu/psl/blob/v${finalAttrs.version}-gha/CHANGES.md";
     license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "publicsuffixlist-download";
   };
-}
+})
