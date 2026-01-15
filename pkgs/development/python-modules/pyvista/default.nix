8b73f8f2d735251a9d9631aa4cb91396c2e3652d
Ivan Mincik <ivan.mincik@gmail.com>
2026-01-15 10:01:02 +0100

    python3Packages.pyvista: fix python 3.14 compatibility
    The upstream patch from PR #8102 doesn't apply cleanly to version
    0.46.4.
    Created a local patch that:
      - Converts Union to | syntax (PEP 604)
      - Adds TypeAlias annotations (PEP 613)
      - Removes dynamic __doc__ assignments on type aliases

--- pkgs/development/python-modules/pyvista/default.nix
+++ pkgs/development/python-modules/pyvista/default.nix
@@ -24,6 +24,13 @@ buildPythonPackage rec {
     hash = "sha256-FFrnLiGiP6LSwaoEHx4tih6XPdKCZ/9tjvz00NQDU0Q=";
   };
 
+  patches = [
+    # Fix Python 3.14 compatibility: Convert Union to | syntax and use TypeAlias
+    # Based on https://github.com/pyvista/pyvista/pull/8102 which doesn't apply
+    # to 0.46.4
+    ./python314-compat.patch
+  ];
+
   build-system = [ setuptools ];
 
   dependencies = [
