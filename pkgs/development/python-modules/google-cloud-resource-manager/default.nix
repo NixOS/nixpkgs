f2ebc2bb78eadddc14a280a378a9635a0f39f936
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-16 00:07:50 +0100

    python313Packages.google-cloud-resource-manager: migrate to finalAttrs

--- pkgs/development/python-modules/google-cloud-resource-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-resource-manager/default.nix
@@ -12,14 +12,14 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "google-cloud-resource-manager";
   version = "1.16.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "google_cloud_resource_manager";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-zJOPh8w2wmcvBiseVBZQYp4NlUxAWk2sNc7t7nDCZ8M=";
   };
 
@@ -52,8 +52,8 @@ buildPythonPackage rec {
   meta = {
     description = "Google Cloud Resource Manager API client library";
     homepage = "https://github.com/googleapis/google-cloud-python/tree/main/packages/google-cloud-resource-manager";
-    changelog = "https://github.com/googleapis/google-cloud-python/blob/google-cloud-resource-manager-v${version}/packages/google-cloud-resource-manager/CHANGELOG.md";
+    changelog = "https://github.com/googleapis/google-cloud-python/blob/google-cloud-resource-manager-v${finalAttrs.version}/packages/google-cloud-resource-manager/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
-}
+})
