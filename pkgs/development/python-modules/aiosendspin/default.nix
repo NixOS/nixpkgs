e145b863c73abe9332b6214219375cda4f7234b5
emilylange <git@emilylange.de>
2026-01-17 00:26:35 +0100

    python3Packages.aiosendspin: 1.2.0 -> 2.1.0
    https://github.com/Sendspin/aiosendspin/releases/tag/2.0.0
    
    https://github.com/Sendspin/aiosendspin/releases/tag/2.0.1
    
    https://github.com/Sendspin/aiosendspin/releases/tag/2.1.0
    
    diff: https://github.com/Sendspin/aiosendspin/compare/1.2.0...2.1.0

--- pkgs/development/python-modules/aiosendspin/default.nix
+++ pkgs/development/python-modules/aiosendspin/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "aiosendspin";
-  version = "1.2.0";
+  version = "2.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Sendspin";
     repo = "aiosendspin";
     tag = version;
-    hash = "sha256-3vTEfXeFqouPswRKST/9U7yg9ah7J9m2KAMoxaBZNR0=";
+    hash = "sha256-9VhNtfXH2r/cGkscz51PIK2/66pPOGv0S0IpO0wFvO4=";
   };
 
   # https://github.com/Sendspin/aiosendspin/blob/1.2.0/pyproject.toml#L7
