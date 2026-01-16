9e34866058939ea4cd438bf2d6dc45deb74ff44a
Alexandre Esteves <alexfmpe@proton.me>
2026-01-16 22:11:57 +0000

    haskellPackages.iserv-proxy: workaround runtime failure due to pseudo relocation

--- pkgs/development/haskell-modules/configuration-windows.nix
+++ pkgs/development/haskell-modules/configuration-windows.nix
@@ -31,10 +31,10 @@ with haskellLib;
     temporary
     ;
 
-  # https://github.com/fpco/streaming-commons/pull/84
-  streaming-commons = appendPatch (fetchpatch {
-    name = "fix-headers-case.patch";
-    url = "https://github.com/fpco/streaming-commons/commit/6da611f63e9e862523ce6ee53262ddbc9681ae24.patch";
-    sha256 = "sha256-giEQqXZfoiAvtCFohdgOoYna2Tnu5aSYAOUH8YVldi0=";
-  }) super.streaming-commons;
+  # Workaround for
+  #   Mingw-w64 runtime failure:
+  #   32 bit pseudo relocation at 00000001400EB99E out of range, targeting 00006FFFFFEB8170, yielding the value 00006FFEBFDCC7CE.
+  # Root cause seems to be undefined references to libffi as shown by linking errors if we instead use "-Wl,--disable-auto-import"
+  # See https://github.com/rust-lang/rust/issues/132226#issuecomment-2445100058
+  iserv-proxy = appendConfigureFlag "--ghc-option=-optl=-Wl,--disable-runtime-pseudo-reloc" super.iserv-proxy;
 })
