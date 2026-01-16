3674b8fece53395492408cef91b525c33821641c
Infinidoge <infinidoge@inx.moe>
2026-01-15 19:05:37 -0500

    linssid: 2.7 -> 3.6

--- pkgs/applications/networking/linssid/default.nix
+++ pkgs/applications/networking/linssid/default.nix
@@ -15,11 +15,11 @@
 
 stdenv.mkDerivation rec {
   pname = "linssid";
-  version = "2.7";
+  version = "3.6";
 
   src = fetchurl {
     url = "mirror://sourceforge/project/linssid/LinSSID_${version}/linssid_${version}.orig.tar.gz";
-    sha256 = "13d35rlcjncd8lx3khkgn9x8is2xjd5fp6ns5xsn3w6l4xj9b4gl";
+    sha256 = "sha256-VzAe6T9wjyUSMWZov05xhQLzfyGl6Tto/GBKkDLj5Jw=";
   };
 
   nativeBuildInputs = [
@@ -34,27 +34,24 @@ stdenv.mkDerivation rec {
     qwt6_1
   ];
 
-  patches = [ ./0001-unbundled-qwt.patch ];
-
   postPatch = ''
-    sed -e "s|/usr/include/qt5.*$|& ${qwt6_1}/include|" -i linssid-app/linssid-app.pro
+    sed -e "s|/usr/include/qt5.*$|${qwt6_1}/include|" \
+        -e "s|/usr/include/qwt.*$||" \
+        -e "s|/usr/lib/libqwt.*$|-lqwt|" \
+        -i linssid-app/linssid-app.pro
     sed -e "s|/usr/include/|/nonexistent/|g" -i linssid-app/*.pro
-    sed -e 's|^LIBS .*= .*libboost_regex.a|LIBS += -lboost_regex|' \
-        -e "s|/usr|$out|g" \
-        -i linssid-app/linssid-app.pro linssid-app/linssid.desktop
+    sed -e "s|/usr|$out|g" \
+        -i linssid-app/{linssid-app.pro,linssid.desktop,com.warsev.pkexec.linssid.policy,linssid-pkexec}
     sed -e "s|\.\./\.\./\.\./\.\./usr|$out|g" -i linssid-app/*.ui
-
-    # Remove bundled qwt
-    rm -fr qwt-lib
   '';
 
   qtWrapperArgs = [
-    ''--prefix PATH : ${
+    "--prefix PATH : ${
       lib.makeBinPath [
         wirelesstools
         iw
       ]
-    }''
+    }"
   ];
 
   meta = {
