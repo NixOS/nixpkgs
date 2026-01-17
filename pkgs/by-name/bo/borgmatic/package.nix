28a66fd11cafbbb4508f6240b9ed02bae7b7fa36
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-01-17 03:06:14 +0100

    borgmatic: 2.0.13 -> 2.1.0

--- pkgs/by-name/bo/borgmatic/package.nix
+++ pkgs/by-name/bo/borgmatic/package.nix
@@ -15,12 +15,12 @@
 }:
 python3Packages.buildPythonApplication rec {
   pname = "borgmatic";
-  version = "2.0.13";
+  version = "2.1.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0Prjgh/L5nCo3CNXZccNW9Dz3d6jIfrXDZmy5fL6zrU=";
+    hash = "sha256-SV8h+/o0EwcGhi1EcQkhz/9uCgeERGQBKZVUgEaGXmQ=";
   };
 
   passthru.updateScript = nix-update-script { };
