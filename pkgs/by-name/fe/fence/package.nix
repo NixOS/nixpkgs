18cac33d026858205fc5ed96eeec4be7b2226fac
Martin Häcker <spamfaenger@gmx.de>
2026-01-17 14:32:16 +0100

    fence: init at 0.1.15

---
+++ pkgs/by-name/fe/fence/package.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  nix-update-script,
+  stdenv,
+  # linux dependencies
+  bubblewrap,
+  socat,
+  bpftrace,
+  installShellFiles,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "fence";
+  version = "0.1.15";
+
+  src = fetchFromGitHub {
+    owner = "Use-Tusk";
+    repo = "fence";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-K00PuM69vwu0VZVdSyboQ6kI6ZPmSl+haaE2w0M6+6k=";
+  };
+
+  vendorHash = "sha256-TEYfsTd8A/A5D39Kq7GV1xLAceuR75funG2u0uvYTCI=";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.version=${finalAttrs.version}"
+    "-X=main.buildTime=1970-01-01T00:00:00Z"
+    "-X=main.gitCommit=${finalAttrs.src.rev}"
+  ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
+    bubblewrap
+    socat
+    bpftrace
+  ];
+
+  # Tests want to create sandbox profiles on darwin.
+  # Nested sandboxes are unsupported by seatbelt, which means we cannot execute the tests inside the nix build sandbox.
+  doCheck = stdenv.hostPlatform.isLinux;
+
+  nativeCheckInputs = lib.optionals stdenv.hostPlatform.isLinux [
+    bubblewrap
+    socat
+    bpftrace
+  ];
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd ${finalAttrs.meta.mainProgram} \
+      --bash <($out/bin/${finalAttrs.meta.mainProgram} completion bash) \
+      --fish <($out/bin/${finalAttrs.meta.mainProgram} completion fish) \
+      --zsh <($out/bin/${finalAttrs.meta.mainProgram} completion zsh)
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Lightweight, container-free sandbox for running commands with network and filesystem restrictions";
+    homepage = "https://github.com/Use-Tusk/fence";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ dwt ];
+    mainProgram = "fence";
+  };
+})
