892175b7acb2ee213ec1c58b9c6f91b295bce1b7
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-17 17:40:43 +0000

    python3Packages.libvirt: 11.7.0 -> 12.0.0

--- pkgs/development/python-modules/libvirt/default.nix
+++ pkgs/development/python-modules/libvirt/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "libvirt";
-  version = "11.7.0";
+  version = "12.0.0";
   pyproject = true;
 
   src = fetchFromGitLab {
     owner = "libvirt";
     repo = "libvirt-python";
     tag = "v${version}";
-    hash = "sha256-1YyWGwotk1Zv0zsNDGmWXhIFYEEN5qgYrB0j2QDTZFY=";
+    hash = "sha256-j4FjAvKBsjpjv3DbpwuvvtHhFFama54YyA4+3chrMMs=";
   };
 
   postPatch = ''
