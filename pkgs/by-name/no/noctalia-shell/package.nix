0f7275a759d9dc0a5ab11f2334e5e13d8fa57d68
wrvsrx <wrvsrx@outlook.com>
2026-01-17 15:12:41 +0800

    noctalia-shell: 4.0.0 -> 4.1.1
    (cherry picked from commit 38cdf60f028848987ba6209dc743c56d51d3ae71)

--- pkgs/by-name/no/noctalia-shell/package.nix
+++ pkgs/by-name/no/noctalia-shell/package.nix
@@ -66,13 +66,13 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "noctalia-shell";
-  version = "4.0.0";
+  version = "4.1.1";
 
   src = fetchFromGitHub {
     owner = "noctalia-dev";
     repo = "noctalia-shell";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-1ByxRYrivSkD4lIQQxv88r+I/mFo+JF3ebok6375+3Q=";
+    hash = "sha256-/7yEiXC2Z/Yk/p7aNgChkAa7BgPRlV5/0z8+jZXH8e8=";
   };
 
   nativeBuildInputs = [
