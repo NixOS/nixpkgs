ebfa4fb1246982f99f8deafac3321d1191b9ff74
adeci <alex.decious@gmail.com>
2026-01-17 18:41:48 +0000

    vscode-extensions.anthropic.claude-code: 2.1.9 -> 2.1.11

--- pkgs/applications/editors/vscode/extensions/anthropic.claude-code/default.nix
+++ pkgs/applications/editors/vscode/extensions/anthropic.claude-code/default.nix
@@ -8,8 +8,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "claude-code";
     publisher = "anthropic";
-    version = "2.1.9";
-    hash = "sha256-1zrXLaTilKY11FVBGnP4ezRT5LAefGH1UKwqz+E/sFY=";
+    version = "2.1.11";
+    hash = "sha256-zqCILYNwBudsEv7+7ZV1YQoPJi30kj5w8suXqAPKORo=";
   };
 
   postInstall = ''
