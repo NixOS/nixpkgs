ddeb59cfb6156b1566badd85e735296bf4362c5c
quantenzitrone <general@dev.quantenzitrone.eu>
2026-01-17 02:39:16 +0100

    hspell: move to pkgs/by-name
    this shouldn't create any rebuilds

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7027,8 +7027,6 @@ with pkgs;
 
   highfive-mpi = highfive.override { hdf5 = hdf5-mpi; };
 
-  hspell = callPackage ../development/libraries/hspell { };
-
   hspellDicts = callPackage ../development/libraries/hspell/dicts.nix { };
 
   hunspellDicts = recurseIntoAttrs (callPackages ../by-name/hu/hunspell/dictionaries.nix { });
