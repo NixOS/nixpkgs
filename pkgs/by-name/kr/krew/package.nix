a6e25bb7b834d609cfb943fec52b3e298b443620
Vincent Demeester <vincent@sbr.pm>
2026-01-17 07:41:12 +0100

    krew: adopt modern best practices
    - Use finalAttrs pattern instead of rec
    - Replace rev with tag for GitHub releases
    - Replace sha256 with hash
    - Add ldflags for binary stripping
    - Add nix-update-script for easier updates
    - Add changelog URL to meta

--- pkgs/by-name/kr/krew/package.nix
+++ pkgs/by-name/kr/krew/package.nix
@@ -4,17 +4,18 @@
   fetchFromGitHub,
   makeWrapper,
   gitMinimal,
+  nix-update-script,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "krew";
   version = "0.4.5";
 
   src = fetchFromGitHub {
     owner = "kubernetes-sigs";
     repo = "krew";
-    rev = "v${version}";
-    sha256 = "sha256-3GoC2HEp9XJe853/JYvX9kAAcFf7XxglVEeU9oQ/5Ms=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-3GoC2HEp9XJe853/JYvX9kAAcFf7XxglVEeU9oQ/5Ms=";
   };
 
   vendorHash = "sha256-r4Dywm0+YxWWD59oaKodkldE2uq8hlt9MwOMYDaj6Gc=";
@@ -23,16 +24,27 @@ buildGoModule rec {
 
   nativeBuildInputs = [ makeWrapper ];
 
+  ldflags = [
+    "-s"
+    "-w"
+  ];
+
   postFixup = ''
     wrapProgram $out/bin/krew \
       --prefix PATH : ${lib.makeBinPath [ gitMinimal ]}
   '';
 
+  # krew doesn't support --version and requires kubectl
+  doInstallCheck = false;
+
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "Package manager for kubectl plugins";
     mainProgram = "krew";
     homepage = "https://github.com/kubernetes-sigs/krew";
+    changelog = "https://github.com/kubernetes-sigs/krew/releases/tag/v${finalAttrs.version}";
     maintainers = with lib.maintainers; [ vdemeester ];
     license = lib.licenses.asl20;
   };
-}
+})
