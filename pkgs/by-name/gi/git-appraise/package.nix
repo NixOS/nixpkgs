7af629fdd425d3a29e17b4a3c25909029f8233c0
Vincent Demeester <vincent@sbr.pm>
2026-01-17 09:09:47 +0100

    git-appraise: adopt modern best practices
    - Use finalAttrs pattern
    - Replace sha256 with hash
    - Add nix-update-script for easier updates
    - Add changelog URL to meta

--- pkgs/by-name/gi/git-appraise/package.nix
+++ pkgs/by-name/gi/git-appraise/package.nix
@@ -2,9 +2,10 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  nix-update-script,
 }:
 
-buildGoModule {
+buildGoModule (finalAttrs: {
   pname = "git-appraise";
   version = "unstable-2022-04-13";
 
@@ -12,7 +13,7 @@ buildGoModule {
     owner = "google";
     repo = "git-appraise";
     rev = "99aeb0e71544d3e1952e208c339b1aec70968cf3";
-    sha256 = "sha256-TteTI8yGP2sckoJ5xuBB5S8xzm1upXmZPlcDLvXZrpc=";
+    hash = "sha256-TteTI8yGP2sckoJ5xuBB5S8xzm1upXmZPlcDLvXZrpc=";
   };
 
   vendorHash = "sha256-Lzq4qpDAUjKFA2T685eW9NCfzEhDsn5UR1A1cIaZadE=";
@@ -22,11 +23,17 @@ buildGoModule {
     "-w"
   ];
 
+  # git-appraise doesn't support --version
+  doInstallCheck = false;
+
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "Distributed code review system for Git repos";
     homepage = "https://github.com/google/git-appraise";
+    changelog = "https://github.com/google/git-appraise/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ vdemeester ];
     mainProgram = "git-appraise";
   };
-}
+})
