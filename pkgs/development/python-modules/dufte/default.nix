70e16bb5c075fea9986ff5b4d73e69589424f01e
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-01-17 16:43:47 +0100

    python3Packages.dufte: remove superfluous pythonOlder

--- pkgs/development/python-modules/dufte/default.nix
+++ pkgs/development/python-modules/dufte/default.nix
@@ -2,8 +2,6 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  pythonOlder,
-  importlib-metadata,
   matplotlib,
   numpy,
   pytestCheckHook,
@@ -27,8 +25,7 @@ buildPythonPackage rec {
   propagatedBuildInputs = [
     matplotlib
     numpy
-  ]
-  ++ lib.optionals (pythonOlder "3.8") [ importlib-metadata ];
+  ];
 
   preCheck = ''
     export HOME=$(mktemp -d)
