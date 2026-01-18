b3f04bc9b2c8f2f61febe0c840a7de0c1140ae9b
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-18 01:25:42 +0000

    debian-devscripts: 2.26.3 -> 2.26.4

--- pkgs/by-name/de/debian-devscripts/package.nix
+++ pkgs/by-name/de/debian-devscripts/package.nix
@@ -30,14 +30,14 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "debian-devscripts";
-  version = "2.26.3";
+  version = "2.26.4";
 
   src = fetchFromGitLab {
     domain = "salsa.debian.org";
     owner = "debian";
     repo = "devscripts";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-LJZ1pt36KccnA/fimZsOzEjXvqVE2eSww1Wkq5+siJk=";
+    hash = "sha256-QGveq9PZfy/m+ah4gBvZDMkVWS50oj1f4SJsKcCKGug=";
   };
 
   patches = [
