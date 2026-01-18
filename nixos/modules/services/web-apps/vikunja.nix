696914a75728e0fa6ba816f738a8eed22f629d70
Petr Krutov <kjubybot@proton.me>
2026-01-18 20:32:47 +0100

    vikunja: add address option

--- nixos/modules/services/web-apps/vikunja.nix
+++ nixos/modules/services/web-apps/vikunja.nix
@@ -45,6 +45,11 @@ in
       type = types.str;
       description = "The Hostname under which the frontend is running.";
     };
+    address = mkOption {
+      type = types.str;
+      default = "0.0.0.0";
+      description = "The IP address to bind to.";
+    };
     port = mkOption {
       type = types.port;
       default = 3456;
@@ -105,7 +110,7 @@ in
           ;
       };
       service = {
-        interface = ":${toString cfg.port}";
+        interface = "${cfg.address}:${toString cfg.port}";
         frontendurl = "${cfg.frontendScheme}://${cfg.frontendHostname}/";
       };
       files = {
