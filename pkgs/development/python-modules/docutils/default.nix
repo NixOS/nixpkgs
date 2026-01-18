a5e6594e52d076a3b937f9d8ac502cca4879134d
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-18 01:22:00 +0100

    python3Packages.docutils: 0.21.2 -> 0.22.4
    https://sourceforge.net/projects/docutils/files/docutils/0.22.4

--- pkgs/development/python-modules/docutils/default.nix
+++ pkgs/development/python-modules/docutils/default.nix
@@ -14,12 +14,12 @@
 let
   self = buildPythonPackage rec {
     pname = "docutils";
-    version = "0.21.2";
+    version = "0.22.4";
     pyproject = true;
 
     src = fetchurl {
       url = "mirror://sourceforge/docutils/docutils-${version}.tar.gz";
-      hash = "sha256-OmsYcy7fGC2qPNEndbuzOM9WkUaPke7rEJ3v9uv6mG8=";
+      hash = "sha256-TbU7H96avsu3TZEjDTKrYm2U9rrfxXXW25GUpJ3ymWg=";
     };
 
     build-system = [ flit-core ];
