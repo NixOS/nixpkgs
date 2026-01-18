74f060fd6e3c0a03751e5c6d6923c9ccf8702bbf
Tom Herbers <mail@tomherbers.de>
2026-01-18 16:49:15 +0100

    nixos/tests/influxdb: add timeouts to ensure quick fail
    This adds some timeout values to the test to ensure the test won't need to rely on external timeout values as much.

--- nixos/tests/influxdb.nix
+++ nixos/tests/influxdb.nix
@@ -21,20 +21,20 @@
 
     start_all()
 
-    one.wait_for_unit("influxdb.service")
+    one.wait_for_unit("influxdb.service", timeout=30)
 
     # create database
     one.succeed(
-        "curl -XPOST http://localhost:8086/query --data-urlencode 'q=CREATE DATABASE test'"
+        "curl --max-time 15 -XPOST http://localhost:8086/query --data-urlencode 'q=CREATE DATABASE test'"
     )
 
     # write some points and run simple query
     out = one.succeed(
-        "curl -XPOST 'http://localhost:8086/write?db=test' --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'"
+        "curl --max-time 15 -XPOST 'http://localhost:8086/write?db=test' --data-binary 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'"
     )
 
     qv = "SELECT value FROM cpu_load_short WHERE region='us-west'"
-    cmd = f'curl -GET "http://localhost:8086/query?db=test" --data-urlencode {shlex.quote("q="+ qv)}'
+    cmd = f'curl --max-time 15 -GET "http://localhost:8086/query?db=test" --data-urlencode {shlex.quote("q="+ qv)}'
     out = one.succeed(cmd)
 
     assert "2015-06-11T20:46:02Z" in out
