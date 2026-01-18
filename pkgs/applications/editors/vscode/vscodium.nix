fc69062a1980ca52bacbe9161e01d17761de9c2a
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-18 07:20:53 +0000

    vscodium: 1.107.18627 -> 1.108.10359

--- pkgs/applications/editors/vscode/vscodium.nix
+++ pkgs/applications/editors/vscode/vscodium.nix
@@ -27,12 +27,12 @@ let
 
   hash =
     {
-      x86_64-linux = "sha256-gqBxdd6Ww1nIXovixgsuIivLXn1LoZXN5NhK4bLmSng=";
-      x86_64-darwin = "sha256-al2RcKJ3KrNywnMYbhd493kyR6I0JUbCJHy1iTk4N4s=";
-      aarch64-linux = "sha256-32WP74IJYVIKuhmsMi0EFqjTlWAO4DgC24ptm3BqSpg=";
-      aarch64-darwin = "sha256-2hYTrkCNzVy6wXT+cGWkDEAy+g/KSkWsLltMSuSSBTk=";
-      armv7l-linux = "sha256-bNjc91zjVXnQ763GHmlVTNDD5ynlJhrtGR7Xzciq7tA=";
-      loongarch64-linux = "sha256-RaxzbawVYT1V0u+RU0gaWTrn2X3uRxEO9T8IAmPJTds=";
+      x86_64-linux = "sha256-NvICNng069Z6qvL3TnjLu82wyfGyjH8wK9aNw2YR2Jk=";
+      x86_64-darwin = "sha256-Ukms3L0tIw7tDfMqFctLjIzkUdr9rvXomKwhrGswMXk=";
+      aarch64-linux = "sha256-+6G7coqgPSOA3xFMKSOpZCDzb9Ssc7wqJnnlaW54e/I=";
+      aarch64-darwin = "sha256-49Zf51yiozdY9V5sPGnECXTUpNIO3OC2c//G2FQcCy0=";
+      armv7l-linux = "sha256-6l75xTqeUkDJW6/WTtkGrAsnmlcQPiVTmiEDRXDI/QA=";
+      loongarch64-linux = "sha256-PNPVAf6jh20jpFVhDGDgT5wo5xPZDQ2xdm4LO0/Vlzk=";
     }
     .${system} or throwSystem;
 
@@ -43,7 +43,7 @@ callPackage ./generic.nix rec {
 
   # Please backport all compatible updates to the stable release.
   # This is important for the extension ecosystem.
-  version = "1.107.18627";
+  version = "1.108.10359";
   pname = "vscodium";
 
   executableName = "codium";
