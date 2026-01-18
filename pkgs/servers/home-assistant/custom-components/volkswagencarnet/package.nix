4f140ebe6634b4833b82de97ff58d274b47158a8
Robert Schütz <nix@dotlambda.de>
2026-01-18 13:34:05 -0800

    home-assistant-custom-components.volkswagencarnet: 5.4.0 -> 5.4.1
    Diff: https://github.com/robinostlund/homeassistant-volkswagencarnet/compare/v5.4.0...v5.4.1
    
    Changelog: https://github.com/robinostlund/homeassistant-volkswagencarnet/releases/tag/v5.4.1

--- pkgs/servers/home-assistant/custom-components/volkswagencarnet/package.nix
+++ pkgs/servers/home-assistant/custom-components/volkswagencarnet/package.nix
@@ -10,13 +10,13 @@
 buildHomeAssistantComponent rec {
   owner = "robinostlund";
   domain = "volkswagencarnet";
-  version = "5.4.0";
+  version = "5.4.1";
 
   src = fetchFromGitHub {
     owner = "robinostlund";
     repo = "homeassistant-volkswagencarnet";
     tag = "v${version}";
-    hash = "sha256-uIsOuc+UXhLbPm4/koANQjzPFfRVwt/rMhYw6keVgYI=";
+    hash = "sha256-nlidbT5dILw4rin4uUDQ8OSqUijpQuoePk20UIl5Uvo=";
   };
 
   postPatch = ''
