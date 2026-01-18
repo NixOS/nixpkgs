7af1ac8b7c34ab5eb4e1a2e5750c04198cf3ac88
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-18 13:20:59 +0000

    icdiff: 2.0.8-unstable-2025-11-11 -> release-2.0.9
    - Fix newline handling regression
    - Simplify terminal width detection using `shutil.get_terminal_size()`
    - Fix buffer overflow with Python 3.14
    
    Co-authored-by: Philip Taron <philip.taron@gmail.com>

--- pkgs/development/python-modules/icdiff/default.nix
+++ pkgs/development/python-modules/icdiff/default.nix
@@ -16,26 +16,18 @@ in
 
 buildPythonPackage rec {
   pname = "icdiff";
-  version = "2.0.8-unstable-2025-11-11";
+  version = "2.0.9";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jeffkaufman";
     repo = "icdiff";
-    rev = "ee4643ae3976ca023dab534eb59b911f16f762ac";
-    hash = "sha256-XV88WjZDc2NSQ5CEahr8KXLXrBACvIWOavMUPeoPGOw=";
-    leaveDotGit = true;
+    tag = "release-${version}";
+    hash = "sha256-ykQLF2b47RZSlrJXYpZ03evhpcGfVyTYwpO2UbmWqrY=";
+    deepClone = true;
   };
 
   patches = [ ./0001-Don-t-test-black-or-flake8.patch ];
-
-  postPatch = ''
-    substituteInPlace test.sh \
-      --replace-fail "check_gold 1 gold-exclude.txt" "# check_gold 1 gold-exclude.txt" \
-      --replace-fail "check_gold 1 gold-strip-cr-off.txt" "# check_gold 1 gold-strip-cr-off.txt" \
-      --replace-fail "check_gold 1 gold-strip-cr-on.txt" "# check_gold 1 gold-strip-cr-on.txt" \
-      --replace-fail "check_gold 1 gold-no-cr-indent" "# check_gold 1 gold-no-cr-indent"
-  '';
 
   build-system = [ setuptools ];
 
@@ -48,13 +40,17 @@ buildPythonPackage rec {
     writableTmpDirAsHomeHook
   ];
 
+  # Before the wheel gets created, fix up the shebangs.
+  preBuild = ''
+    patchShebangs test.sh icdiff git-icdiff
+  '';
+
   # Odd behavior in the sandbox
   doCheck = !stdenv.hostPlatform.isDarwin;
 
   checkPhase = ''
     runHook preCheck
 
-    patchShebangs test.sh
     ./test.sh ${python.interpreter}
 
     runHook postCheck
