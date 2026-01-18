131afc51744e7979bd49884acfe1909cca0fa2a9
Geoffrey “Frogeye” Preud'homme <geoffrey@frogeye.fr>
2026-01-18 22:19:57 +0100

    cfait: init at 1.0.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1837,6 +1837,8 @@ with pkgs;
 
   blacken-docs = with python3Packages; toPythonApplication blacken-docs;
 
+  cfait-gui = lib.addMetaAttrs { mainProgram = "cfait-gui"; } cfait.gui;
+
   cffconvert = python3Packages.toPythonApplication python3Packages.cffconvert;
 
   aiovban-pyaudio = python3Packages.toPythonApplication python3Packages.aiovban-pyaudio;
