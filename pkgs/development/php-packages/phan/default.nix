81c11051dcf18c7cd924ad75c6dd71b97bcd5c74
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2026-01-18 18:09:59 +0100

    phpPackages.phan: 5.5.1 -> 6.0.0

--- pkgs/development/php-packages/phan/default.nix
+++ pkgs/development/php-packages/phan/default.nix
@@ -8,17 +8,18 @@
 (php.withExtensions ({ enabled, all }: enabled ++ (with all; [ ast ]))).buildComposerProject2
   (finalAttrs: {
     pname = "phan";
-    version = "5.5.1";
+    version = "6.0.0";
 
     src = fetchFromGitHub {
       owner = "phan";
       repo = "phan";
-      rev = finalAttrs.version;
-      hash = "sha256-G17ORkHmu6nkfJ8UrGzcaybPOKPrq1Q+LXVS56aVOZ8=";
+      tag = finalAttrs.version;
+      hash = "sha256-1qRGNDptiAdcGc1x+iLrxe9TjLGaL8EM8xuTOUNB+Ww=";
     };
 
-    vendorHash = "sha256-k/ExFSr0veKYURu66HmYaNHPubAAKH5mgIPXzcpMbPc=";
+    vendorHash = "sha256-Ro5/lA72xVIkZyuRNix77Cpeyyj1GbW5J4DzjQMq0Rc=";
 
+    composerStrictValidation = false;
     doInstallCheck = true;
     nativeInstallCheckInputs = [ versionCheckHook ];
     versionCheckProgramArg = "--version";
