b680f2b10647c9d86ab6cdba50a410bffdcdda5a
Maevi <me@maevi.net>
2026-01-19 03:58:10 +0100

    rke2_1_34: 1.34.2+rke2r1 -> 1.34.3+rke2r1
    https://github.com/rancher/rke2/releases/tag/v1.34.3%2Brke2r1
    (cherry picked from commit 1ade91c7ecd125e7e0f3c68da5c6c93b99548687)

--- pkgs/applications/networking/cluster/rke2/1_34/versions.nix
+++ pkgs/applications/networking/cluster/rke2/1_34/versions.nix
@@ -1,13 +1,13 @@
 {
-  rke2Version = "1.34.2+rke2r1";
-  rke2Commit = "5e3fff8134a5269977762be64197d0cb9b26b48c";
-  rke2TarballHash = "sha256-qd8oD51x2tiIRLWWA5rWUVp/stBB2ebe8dFIB5XuZME=";
-  rke2VendorHash = "sha256-0qsCo/9kttOaFqOwfNfwxaG9z+EAdk15Dde3Gw5tK98=";
-  k8sImageTag = "v1.34.2-rke2r1-build20251112";
-  etcdVersion = "v3.6.5-k3s1-build20251017";
+  rke2Version = "1.34.3+rke2r1";
+  rke2Commit = "1b103f296ab20fac6b32951c9efe59d28a5ed79f";
+  rke2TarballHash = "sha256-94wB6Dt06/evdQcW1K8blNBHwNR3ZGCZPLJyeyMbYAM=";
+  rke2VendorHash = "sha256-hVEIhaF5gabDKWX2VCTyKQa0cZktO9w+l2JtSNQIkg8=";
+  k8sImageTag = "v1.34.3-rke2r1-build20251210";
+  etcdVersion = "v3.6.6-k3s1-build20251210";
   pauseVersion = "3.6";
-  ccmVersion = "v1.34.2-0.20251010190833-cf0d35a732d1-build20251017";
-  dockerizedVersion = "v1.34.2-rke2r1";
-  helmJobVersion = "";
+  ccmVersion = "v1.34.3-0.20251210094406-1ff6ebef7028-build20251210";
+  dockerizedVersion = "v1.34.3-rke2r1";
+  helmJobVersion = "v0.9.12-build20251215";
   imagesVersions = with builtins; fromJSON (readFile ./images-versions.json);
 }
