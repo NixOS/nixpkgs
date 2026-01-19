2a777ace4b722f2714cc06d596f2476ee628c04a
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-19 10:29:53 +0000

    go-ethereum: 1.16.5 -> 1.16.8

--- pkgs/by-name/go/go-ethereum/package.nix
+++ pkgs/by-name/go/go-ethereum/package.nix
@@ -15,17 +15,17 @@ let
 in
 buildGoModule rec {
   pname = "go-ethereum";
-  version = "1.16.5";
+  version = "1.16.8";
 
   src = fetchFromGitHub {
     owner = "ethereum";
     repo = "go-ethereum";
     rev = "v${version}";
-    hash = "sha256-f9MBHO3oh1Nh+YI1E8cPPaNRj4T12063YLqTDrdZWWA=";
+    hash = "sha256-eMvLhOSWGC31ezZeqMMt8kmFHq9NU0kh5s2IBiw46NY=";
   };
 
   proxyVendor = true;
-  vendorHash = "sha256-6tGSyx4OXMXUjhIvLJo+vyRkNzHmwiikzrLL0cQPBLo=";
+  vendorHash = "sha256-KP9oD87kn8MCvEf3ply8HbP8xIBlGAEtthGob8Yh++A=";
 
   doCheck = false;
 
