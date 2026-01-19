748379eb11d74561c575af000e28b02588ebf9d5
Jeffery Oo <oojefferywm@proton.me>
2026-01-19 03:41:18 -0500

    keycloak.plugins.keycloak-config-cli: init at 6.4.0

--- pkgs/by-name/ke/keycloak/all-plugins.nix
+++ pkgs/by-name/ke/keycloak/all-plugins.nix
@@ -4,9 +4,9 @@
   junixsocket-common,
   junixsocket-native-common,
 }:
-
 {
   keycloak-2fa-sms-authenticator = callPackage ./keycloak-2fa-sms-authenticator { };
+  keycloak-config-cli = callPackage ./keycloak-config-cli { };
   keycloak-discord = callPackage ./keycloak-discord { };
   keycloak-enforce-mfa-authenticator = callPackage ./keycloak-enforce-mfa-authenticator { };
   keycloak-magic-link = callPackage ./keycloak-magic-link { };
