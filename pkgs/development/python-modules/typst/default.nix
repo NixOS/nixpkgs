b4ffcdd747c8e9d32866369f5d33b3115e4c321a
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-19 21:59:14 +0100

    python313Packages.typst: migrate to finalAttrs

--- pkgs/development/python-modules/typst/default.nix
+++ pkgs/development/python-modules/typst/default.nix
@@ -10,7 +10,7 @@
   rustPlatform,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "typst";
   version = "0.14.7";
   pyproject = true;
@@ -18,12 +18,12 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "messense";
     repo = "typst-py";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-GhXsfsJieBMKvHji4YGfZtvGMIa3k353Erb7V8RSDkU=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
-    inherit pname version src;
+    inherit (finalAttrs) pname version src;
     hash = "sha256-A/iNQifMjpAMdoiEF3GaBe74mfsv8i/EwQL+ZmMc1YM=";
   };
 
@@ -49,8 +49,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python binding to typst";
     homepage = "https://github.com/messense/typst-py";
-    changelog = "https://github.com/messense/typst-py/releases/tag/v${src.tag}";
+    changelog = "https://github.com/messense/typst-py/releases/tag/v${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
