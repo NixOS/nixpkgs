8c27638bc2bbcd2e28aea6b7c78ee560518025ec
jopejoe1 <jopejoe1@missing.ninja>
2026-01-19 13:35:31 +0100

    minimal-bootstrap: Use `lib.recurseIntoAttrsRelease` instead of a custom override

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9570,7 +9570,12 @@ with pkgs;
   };
 
   mdadm = mdadm4;
-  minimal-bootstrap = recurseIntoAttrs (
+
+  # minimal-bootstrap packages aren't used for anything but bootstrapping our
+  # stdenv. They should not be used for any other purpose and therefore not
+  # show up in search results or repository tracking services that consume our
+  # packages.json https://github.com/NixOS/nixpkgs/issues/244966
+  minimal-bootstrap = lib.recurseIntoAttrsRelease (
     import ../os-specific/linux/minimal-bootstrap {
       inherit (stdenv) buildPlatform hostPlatform;
       inherit lib config;
--- pkgs/top-level/packages-config.nix
+++ pkgs/top-level/packages-config.nix
@@ -19,11 +19,5 @@
       # emacsPackages is an alias for emacs.pkgs
       # Re-introduce emacsPackages here so that emacs.pkgs can be searched.
       emacsPackages = emacs.pkgs;
-
-      # minimal-bootstrap packages aren't used for anything but bootstrapping our
-      # stdenv. They should not be used for any other purpose and therefore not
-      # show up in search results or repository tracking services that consume our
-      # packages.json https://github.com/NixOS/nixpkgs/issues/244966
-      minimal-bootstrap = { };
     };
 }
