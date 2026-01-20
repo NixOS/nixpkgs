d811de6ff5d705780143d2b01f6ee7446351daff
Tyce Herrman <Tyce.Herrman@pm.me>
2026-01-20 16:17:40 -0500

    unixODBC: 2.3.12 -> 2.3.14
    https://github.com/lurcher/unixODBC/releases/tag/v2.3.14
    
    - Add GitHub releases as primary source URL
          - Enable nix-update-script for automated updates
          - Keep official website/FTP as fallback sources

--- pkgs/by-name/un/unixODBC/package.nix
+++ pkgs/by-name/un/unixODBC/package.nix
@@ -2,24 +2,35 @@
   lib,
   stdenv,
   fetchurl,
+  nix-update-script,
 }:
 
 stdenv.mkDerivation rec {
   pname = "unixODBC";
-  version = "2.3.12";
+  version = "2.3.14";
 
   src = fetchurl {
     urls = [
+      # GitHub tag format changed from "2.3.12" to "v2.3.13" starting with 2.3.13
+      "https://github.com/lurcher/unixODBC/releases/download/${
+        if lib.versionAtLeast version "2.3.13" then "v${version}" else version
+      }/${pname}-${version}.tar.gz"
       "ftp://ftp.unixodbc.org/pub/unixODBC/${pname}-${version}.tar.gz"
       "https://www.unixodbc.org/${pname}-${version}.tar.gz"
     ];
-    sha256 = "sha256-8hBQFEXOIb9ge6Ue+MEl4Q4i3/3/7Dd2RkYt9fAZFew=";
+    hash = "sha256-TigU3j4B/DCwufdeg7taupGrA4TulRKGUEu3AgVSR3E=";
   };
 
   configureFlags = [
     "--disable-gui"
     "--sysconfdir=/etc"
   ];
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version-regex=^v?([0-9.]+)$"
+    ];
+  };
 
   meta = {
     description = "ODBC driver manager for Unix";
