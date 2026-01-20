5128e4915fcf784fd44e1594297b256fc2c1937f
eljamm <fedi.jamoussi@protonmail.ch>
2026-01-20 09:33:40 +0000

    linux_xanmod: 6.12.65 -> 6.12.66
    - Changelog: https://dl.xanmod.org/changelog/6.12/ChangeLog-6.12.66-xanmod1.gz
    - Diff: https://gitlab.com/xanmod/linux/-/compare/6.12.65-xanmod1..6.12.66-xanmod1?from_project_id=51590166

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -15,8 +15,8 @@ let
   variants = {
     # ./update-xanmod.sh lts
     lts = {
-      version = "6.12.65";
-      hash = "sha256-TBzCitE2JHmUa9syiOzMKOYCc1194W8b4uDOLCP/6KE=";
+      version = "6.12.66";
+      hash = "sha256-TjfSGArDcBysHtu6w9fYU1PflStZitIZDFnb6pGeLP0=";
       isLTS = true;
     };
     # ./update-xanmod.sh main
