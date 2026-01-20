5cf2004eb64ce1f783f27d10275e0d06e9c6492c
maj0e <57637846+maj0e@users.noreply.github.com>
2026-01-20 23:16:04 +0100

    zettlr: 3.4.4 -> 4.1.0

--- pkgs/by-name/ze/zettlr/package.nix
+++ pkgs/by-name/ze/zettlr/package.nix
@@ -8,11 +8,11 @@
 # Based on https://gist.github.com/msteen/96cb7df66a359b827497c5269ccbbf94 and joplin-desktop nixpkgs.
 let
   pname = "zettlr";
-  version = "3.4.4";
+  version = "4.1.0";
 
   src = fetchurl {
     url = "https://github.com/Zettlr/Zettlr/releases/download/v${version}/Zettlr-${version}-x86_64.appimage";
-    hash = "sha256-ApgmHl9WoAmWl03tqv01D0W8orja25f7KZUFLhlZloQ=";
+    hash = "sha256-/6x2HhwW0+A7/CYC+HUCsJ2u1tp4zno6XjpvBLogUKU=";
   };
   appimageContents = appimageTools.extractType2 {
     inherit pname version src;
