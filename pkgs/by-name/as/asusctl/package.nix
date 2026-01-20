ebadfdc8f15d9a0121571854fbe98f63d7249f75
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-20 01:36:52 +0000

    asusctl: 6.2.0 -> 6.3.1

--- pkgs/by-name/as/asusctl/package.nix
+++ pkgs/by-name/as/asusctl/package.nix
@@ -18,16 +18,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "asusctl";
-  version = "6.2.0";
+  version = "6.3.1";
 
   src = fetchFromGitLab {
     owner = "asus-linux";
     repo = "asusctl";
     tag = version;
-    hash = "sha256-frQbfCdK7bD6IAUa+MAOaRLhMrbdFRdHocQ0Z1tzsqE=";
+    hash = "sha256-x3WKxjYrWYaLWDi52b3uQSYnr/Qunf6JYu4ikt4ajls=";
   };
 
-  cargoHash = "sha256-Z3JFp/qH3mD3Hy/kqSONOZ+syulgr+t0ZzFRvNN+Ayg=";
+  cargoHash = "sha256-FyVbeHzwMr8UJ2OoVYVekXJFhus/ab7KwfGK4eaua6A=";
 
   postPatch = ''
     files="
