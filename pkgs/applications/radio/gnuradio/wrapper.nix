ac2dce6119e32e3209ecd4ff0af6a7d4f4ed6a9c
Doron Behar <doron.behar@gmail.com>
2026-01-20 13:17:59 +0200

    gnuradio: (wrapper) remove old conditionals

--- pkgs/applications/radio/gnuradio/wrapper.nix
+++ pkgs/applications/radio/gnuradio/wrapper.nix
@@ -81,35 +81,31 @@ let
   pname = unwrapped.pname + "-wrapped";
   inherit (unwrapped) outputs version;
   makeWrapperArgs = builtins.concatStringsSep " " (
-    [
-    ]
     # Emulating wrapGAppsHook3 & wrapQtAppsHook working together
-    ++
-      lib.optionals ((unwrapped.hasFeature "gnuradio-companion") || (unwrapped.hasFeature "gr-qtgui"))
-        [
-          "--prefix"
-          "XDG_DATA_DIRS"
-          ":"
-          "$out/share"
-          "--prefix"
-          "XDG_DATA_DIRS"
-          ":"
-          "$out/share/gsettings-schemas/${pname}"
-          "--prefix"
-          "XDG_DATA_DIRS"
-          ":"
-          "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}"
-          "--prefix"
-          "XDG_DATA_DIRS"
-          ":"
-          "${hicolor-icon-theme}/share"
-          # Needs to run `gsettings` on startup, see:
-          # https://www.mail-archive.com/debian-bugs-dist@lists.debian.org/msg1764890.html
-          "--prefix"
-          "PATH"
-          ":"
-          "${lib.getBin glib}/bin"
-        ]
+    lib.optionals ((unwrapped.hasFeature "gnuradio-companion") || (unwrapped.hasFeature "gr-qtgui")) [
+      "--prefix"
+      "XDG_DATA_DIRS"
+      ":"
+      "$out/share"
+      "--prefix"
+      "XDG_DATA_DIRS"
+      ":"
+      "$out/share/gsettings-schemas/${pname}"
+      "--prefix"
+      "XDG_DATA_DIRS"
+      ":"
+      "${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}"
+      "--prefix"
+      "XDG_DATA_DIRS"
+      ":"
+      "${hicolor-icon-theme}/share"
+      # Needs to run `gsettings` on startup, see:
+      # https://www.mail-archive.com/debian-bugs-dist@lists.debian.org/msg1764890.html
+      "--prefix"
+      "PATH"
+      ":"
+      "${lib.getBin glib}/bin"
+    ]
     ++ lib.optionals (unwrapped.hasFeature "gnuradio-companion") [
       "--set"
       "GDK_PIXBUF_MODULE_FILE"
@@ -157,45 +153,34 @@ let
       ":"
       "${lib.makeSearchPath soapysdr.passthru.searchPath extraSoapySdrPackages}"
     ]
-    ++
-      lib.optionals (unwrapped.hasFeature "gr-qtgui")
-        # 3.7 builds with qt4
-        (
-          if lib.versionAtLeast unwrapped.versionAttr.major "3.8" then
-            [
-              "--prefix"
-              "QT_PLUGIN_PATH"
-              ":"
-              "${lib.makeSearchPath unwrapped.qt.qtbase.qtPluginPrefix (
-                map lib.getBin (
-                  [
-                    unwrapped.qt.qtbase
-                  ]
-                  ++ lib.optionals stdenv.hostPlatform.isLinux [
-                    unwrapped.qt.qtwayland
-                  ]
-                )
-              )}"
-              "--prefix"
-              "QML2_IMPORT_PATH"
-              ":"
-              "${lib.makeSearchPath unwrapped.qt.qtbase.qtQmlPrefix (
-                map lib.getBin (
-                  [
-                    unwrapped.qt.qtbase
-                  ]
-                  ++ lib.optionals stdenv.hostPlatform.isLinux [
-                    unwrapped.qt.qtwayland
-                  ]
-                )
-              )}"
-            ]
-          else
-            # Add here qt4 related environment for 3.7?
-            [
-
-            ]
+    ++ lib.optionals (unwrapped.hasFeature "gr-qtgui") [
+      "--prefix"
+      "QT_PLUGIN_PATH"
+      ":"
+      "${lib.makeSearchPath unwrapped.qt.qtbase.qtPluginPrefix (
+        map lib.getBin (
+          [
+            unwrapped.qt.qtbase
+          ]
+          ++ lib.optionals stdenv.hostPlatform.isLinux [
+            unwrapped.qt.qtwayland
+          ]
         )
+      )}"
+      "--prefix"
+      "QML2_IMPORT_PATH"
+      ":"
+      "${lib.makeSearchPath unwrapped.qt.qtbase.qtQmlPrefix (
+        map lib.getBin (
+          [
+            unwrapped.qt.qtbase
+          ]
+          ++ lib.optionals stdenv.hostPlatform.isLinux [
+            unwrapped.qt.qtwayland
+          ]
+        )
+      )}"
+    ]
     ++ extraMakeWrapperArgs
   );
 
