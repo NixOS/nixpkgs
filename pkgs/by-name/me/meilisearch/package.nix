603b953bbebc213d20c9c18257473e46f5af6394
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-20 15:39:08 +0000

    meilisearch: 1.33.0 -> 1.33.1

--- pkgs/by-name/me/meilisearch/package.nix
+++ pkgs/by-name/me/meilisearch/package.nix
@@ -8,18 +8,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "meilisearch";
-  version = "1.33.0";
+  version = "1.33.1";
 
   src = fetchFromGitHub {
     owner = "meilisearch";
     repo = "meilisearch";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-OC8JPG/UAgBm+l5bFG4A+4/3cqkUMbeBkqIG+rjiucY=";
+    hash = "sha256-q1sDiIJETk22Ain8M1LfhFZ2sUSq7H5qNCs23f0qsz4=";
   };
 
   cargoBuildFlags = [ "--package=meilisearch" ];
 
-  cargoHash = "sha256-MwCbrPnWLipuSaJdtrm595e/geE/pb6Nw1vHL7l/XRU=";
+  cargoHash = "sha256-Ae/KR45LXwXd/qTeKTAoWPnsue4ekNr1SP7oJ8EN+Q8=";
 
   # Default features include mini dashboard which downloads something from the internet.
   buildNoDefaultFeatures = true;
