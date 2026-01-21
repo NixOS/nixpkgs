eb0f955bb76d329364f6a0f0346079aaa9f5dd85
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:38:31 +0100

    python3Packages.sphinx-pytest: 0.2.0 -> 0.3.0
    https://github.com/sphinx-extensions2/sphinx-pytest/releases/tag/v0.3.0
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/sphinx-pytest/default.nix
+++ pkgs/development/python-modules/sphinx-pytest/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "sphinx-pytest";
-  version = "0.2.0";
+  version = "0.3.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "chrisjsewell";
     repo = "sphinx-pytest";
     tag = "v${version}";
-    hash = "sha256-oSBBt+hSMs4mvGqibQHoYHXr2j/bpsGOnIMfwfTfWKQ=";
+    hash = "sha256-z71IrUr3e2oAPeZMjUBwMwy2SkoAA3oxtK4+iR9vLEc=";
   };
 
   nativeBuildInputs = [ flit-core ];
@@ -40,7 +40,7 @@ buildPythonPackage rec {
   ];
 
   meta = {
-    changelog = "https://github.com/sphinx-extensions2/sphinx-pytest/releases/tag/v${version}";
+    changelog = "https://github.com/sphinx-extensions2/sphinx-pytest/releases/tag/${src.tag}";
     description = "Helpful pytest fixtures for Sphinx extensions";
     homepage = "https://github.com/chrisjsewell/sphinx-pytest";
     license = lib.licenses.mit;
