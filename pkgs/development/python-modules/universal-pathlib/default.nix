a125fa7628abe94f869680d8e9ceed1ebaf8b35e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:39:13 +0100

    python3Packages.universal-pathlib: 0.2.6 -> 0.3.8
    https://github.com/fsspec/universal_pathlib/releases/tag/v0.3.8
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/universal-pathlib/default.nix
+++ pkgs/development/python-modules/universal-pathlib/default.nix
@@ -9,13 +9,13 @@
 
 buildPythonPackage rec {
   pname = "universal-pathlib";
-  version = "0.2.6";
+  version = "0.3.8";
   pyproject = true;
 
   src = fetchPypi {
     pname = "universal_pathlib";
     inherit version;
-    hash = "sha256-UIF6rqqfQWPLHnb1vfhCB/oFznKLI/13lHmzRi5UMKw=";
+    hash = "sha256-6tK2W8o99uEcO3yzb8mEY0C8PC2071cTFVAmBCKwo+g=";
   };
 
   build-system = [
