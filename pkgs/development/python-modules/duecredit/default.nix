6ac2c31a1a70419e2539811df1265496216fba5f
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:33:09 +0100

    python3Packages.duecredit: 0.10.2 -> 0.11.2
    https://github.com/duecredit/duecredit/releases/tag/0.11.2
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/duecredit/default.nix
+++ pkgs/development/python-modules/duecredit/default.nix
@@ -13,12 +13,12 @@
 
 buildPythonPackage rec {
   pname = "duecredit";
-  version = "0.10.2";
+  version = "0.11.2";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-/nOiDk+7LZcroB7fN97BsLoeZG7+XvTMrwxnJMoofUI=";
+    hash = "sha256-e1wa4Qkn+eAs9NVOLHSoqgDNKcONY33v48lI09jp8zo=";
   };
 
   nativeBuildInputs = [ setuptools ];
