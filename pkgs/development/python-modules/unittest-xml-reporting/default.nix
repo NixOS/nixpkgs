7c94dcd1c3f3ef933cb735e136a8a315c0eba40b
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:39:13 +0100

    python3Packages.unittest-xml-reporting: 3.2.0 -> 4.0.0
    https://github.com/xmlrunner/unittest-xml-reporting/releases/tag/4.0.0
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/unittest-xml-reporting/default.nix
+++ pkgs/development/python-modules/unittest-xml-reporting/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "unittest-xml-reporting";
-  version = "3.2.0";
+  version = "4.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "xmlrunner";
     repo = "unittest-xml-reporting";
     tag = version;
-    hash = "sha256-lOJ/+8CVJUXdIaZLLF5PpPkG0DzlNgo46kRZ1Xy7Ju0=";
+    hash = "sha256-9gV/DX/G12bthonBJlMDKaS6Iwt9nF5DrNG33KK7KbU=";
   };
 
   build-system = [ setuptools ];
@@ -39,7 +39,7 @@ buildPythonPackage rec {
   meta = {
     description = "Unittest-based test runner with Ant/JUnit like XML reporting";
     homepage = "https://github.com/xmlrunner/unittest-xml-reporting";
-    changelog = "https://github.com/xmlrunner/unittest-xml-reporting/releases/tag/${version}";
+    changelog = "https://github.com/xmlrunner/unittest-xml-reporting/releases/tag/${src.tag}";
     license = lib.licenses.bsd2;
     maintainers = with lib.maintainers; [ rprospero ];
   };
