2adc08aefff04facd31c3cff3611ee69c07ea407
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:34:29 +0100

    python3Packages.jupyter-repo2docker: 2025.08.0 -> 2025.12.0
    https://github.com/jupyterhub/repo2docker/blob/2025.12.0/docs/source/changelog.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/jupyter-repo2docker/default.nix
+++ pkgs/development/python-modules/jupyter-repo2docker/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage rec {
   pname = "jupyter-repo2docker";
-  version = "2025.08.0";
+  version = "2025.12.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jupyterhub";
     repo = "repo2docker";
     tag = version;
-    hash = "sha256-vqLZbqshEl3xC5hcE4OkWfZpPSlSfv70oygEYPFqyFE=";
+    hash = "sha256-9ZRew9DspRENxhqpWFTSdP8KcQQHA4vMMECLikt+nsw=";
   };
 
   nativeBuildInputs = [ setuptools ];
