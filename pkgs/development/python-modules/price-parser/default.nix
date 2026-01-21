adb8960fa71fcb6cfb7625f4c3d3b698ab1b681d
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:36:40 +0100

    python3Packages.price-parser: 0.4.0 -> 0.5.0
    https://github.com/scrapinghub/price-parser/blob/0.5.0/CHANGES.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/price-parser/default.nix
+++ pkgs/development/python-modules/price-parser/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "price-parser";
-  version = "0.4.0";
+  version = "0.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "scrapinghub";
     repo = "price-parser";
     tag = version;
-    hash = "sha256-9f/+Yw94SVvg9fl9zYR9YEMwAgKHwySG5cysPMomnA0=";
+    hash = "sha256-1aMWRyrmvilON+crWibrREIN2Rf3hCzewI+vmRppvrg=";
   };
 
   build-system = [ setuptools ];
@@ -37,7 +37,7 @@ buildPythonPackage rec {
   meta = {
     description = "Extract price amount and currency symbol from a raw text string";
     homepage = "https://github.com/scrapinghub/price-parser";
-    changelog = "https://github.com/scrapinghub/price-parser/blob/${version}/CHANGES.rst";
+    changelog = "https://github.com/scrapinghub/price-parser/blob/${src.tag}/CHANGES.rst";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ thanegill ];
   };
