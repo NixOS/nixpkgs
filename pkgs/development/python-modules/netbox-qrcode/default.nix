8d3dff1d1686655444f0a0f6f1232248aa8219d6
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:35:52 +0100

    python3Packages.netbox-qrcode: 0.0.19 -> 0.0.20
    https://github.com/netbox-community/netbox-qrcode/releases/tag/v0.0.20
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/netbox-qrcode/default.nix
+++ pkgs/development/python-modules/netbox-qrcode/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "netbox-qrcode";
-  version = "0.0.19";
+  version = "0.0.20";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "netbox-community";
     repo = "netbox-qrcode";
     tag = "v${version}";
-    hash = "sha256-hrVnNEgLL3r1BtZdBaJu6H5b/tuXUxzGjKcITTopE60=";
+    hash = "sha256-7dPMpuJ2nuj9rRmVrfthD+xrEHoUaLFqDJWC6cGGCwY=";
   };
 
   build-system = [ setuptools ];
