155c190988b774306372fbc2731b9848bb6fe8bf
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:34:23 +0100

    python3Packages.jaraco-context: 6.0.1 -> 6.1.0
    https://github.com/jaraco/jaraco.context/blob/v6.1.0/CHANGES.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/jaraco-context/default.nix
+++ pkgs/development/python-modules/jaraco-context/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "jaraco-context";
-  version = "6.0.1";
+  version = "6.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jaraco";
     repo = "jaraco.context";
     tag = "v${version}";
-    hash = "sha256-WXZX2s9Qehp0F3bSv2c5lGxhhn6HKFkABbtYKizG1/8=";
+    hash = "sha256-2UYG1xXnH1kjYNvB6EKJPRZJ1Zd0yYhTDBTdrNFN1p4=";
   };
 
   pythonNamespaces = [ "jaraco" ];
@@ -33,7 +33,7 @@ buildPythonPackage rec {
   meta = {
     description = "Python module for context management";
     homepage = "https://github.com/jaraco/jaraco.context";
-    changelog = "https://github.com/jaraco/jaraco.context/blob/v${version}/CHANGES.rst";
+    changelog = "https://github.com/jaraco/jaraco.context/blob/${src.tag}/CHANGES.rst";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
