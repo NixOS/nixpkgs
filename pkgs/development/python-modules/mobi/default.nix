15da821a791fc895f1950ec40bc8ffea9fab14ef
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:35:39 +0100

    python3Packages.mobi: 0.3.3 -> 0.4.1
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/mobi/default.nix
+++ pkgs/development/python-modules/mobi/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "mobi";
-  version = "0.3.3";
+  version = "0.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "iscc";
     repo = "mobi";
     tag = "v${version}";
-    hash = "sha256-g1L72MkJdrKQRsEdew+Qsn8LfCn8+cmj2pmY6s4nv2U=";
+    hash = "sha256-Hbw4TX/yKkuxYQ9vZZp/wasDCop8pvyQc5zWloMQbng=";
   };
 
   pythonRelaxDeps = [ "loguru" ];
