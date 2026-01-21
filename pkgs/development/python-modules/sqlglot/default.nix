10d4f508c6b028716c843f9d0dd47448f7caa282
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:38:37 +0100

    python3Packages.sqlglot: 27.29.0 -> 28.6.0
    https://github.com/tobymao/sqlglot/blob/v28.6.0/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/sqlglot/default.nix
+++ pkgs/development/python-modules/sqlglot/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "sqlglot";
-  version = "27.29.0";
+  version = "28.6.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     repo = "sqlglot";
     owner = "tobymao";
     tag = "v${version}";
-    hash = "sha256-kqtsZRtIIgQ0cTbhG5K4N+c3cq7I+Uvd+3s+b0CYLtg=";
+    hash = "sha256-1RAjGqu2D/wrec/sryPGZlPnaBGkyhYpzdaZu9KYh2Q=";
   };
 
   build-system = [
@@ -51,7 +51,7 @@ buildPythonPackage rec {
   meta = {
     description = "No dependency Python SQL parser, transpiler, and optimizer";
     homepage = "https://github.com/tobymao/sqlglot";
-    changelog = "https://github.com/tobymao/sqlglot/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/tobymao/sqlglot/blob/${src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ cpcloud ];
   };
