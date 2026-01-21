b7fcc62c06ddd794d53223a269f30fdcb9f39b94
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:37:01 +0100

    python3Packages.pygsl: 2.6.2 -> 2.6.4
    https://github.com/pygsl/pygsl/blob/v2.6.4/ChangeLog
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pygsl/default.nix
+++ pkgs/development/python-modules/pygsl/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pygsl";
-  version = "2.6.2";
+  version = "2.6.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pygsl";
     repo = "pygsl";
     tag = "v${version}";
-    hash = "sha256-1aAc2qGVlClnsw70D1QqPbSsyij0JNgfIXsLzelYx3E=";
+    hash = "sha256-dZIWOwRRrF1bux9UTIxN31/S380wPT4gpQ/gYbUO4FQ=";
   };
 
   nativeBuildInputs = [
