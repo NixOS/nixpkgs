0edc68757aac5a98e2ca562c4450d6cb4ff65a31
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:40 +0100

    python3Packages.databricks-sdk: 0.67.0 -> 0.78.0
    https://github.com/databricks/databricks-sdk-py/blob/v0.78.0/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/databricks-sdk/default.nix
+++ pkgs/development/python-modules/databricks-sdk/default.nix
@@ -21,14 +21,14 @@
 
 buildPythonPackage rec {
   pname = "databricks-sdk";
-  version = "0.67.0";
+  version = "0.78.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "databricks";
     repo = "databricks-sdk-py";
     tag = "v${version}";
-    hash = "sha256-3UGPB3KEO7M4QFYiniU4hcaOUmCMq3vW4yBIxDUhHLk=";
+    hash = "sha256-hoY4YuKIa7LmoW2MxUWKhLLsuTyO2b6qtpxr36hjP20=";
   };
 
   build-system = [
