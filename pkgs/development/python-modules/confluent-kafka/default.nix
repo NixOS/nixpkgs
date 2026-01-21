5c1dbbc0f283e5777a7806445bc979826287207d
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:31 +0100

    python3Packages.confluent-kafka: 2.11.1 -> 2.13.0
    https://github.com/confluentinc/confluent-kafka-python/blob/v2.13.0/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/confluent-kafka/default.nix
+++ pkgs/development/python-modules/confluent-kafka/default.nix
@@ -20,6 +20,7 @@
   protobuf,
   pyflakes,
   pyrsistent,
+  pytest-asyncio,
   pytestCheckHook,
   pyyaml,
   rdkafka,
@@ -31,14 +32,14 @@
 
 buildPythonPackage rec {
   pname = "confluent-kafka";
-  version = "2.11.1";
+  version = "2.13.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "confluentinc";
     repo = "confluent-kafka-python";
     tag = "v${version}";
-    hash = "sha256-WpvWv6UG7T0yJ1ZKZweHbWjh+C0PbEIYbbMAS4yyhzg=";
+    hash = "sha256-VnZf6YvvpOs9/9uJHJvcmF56Ra9hhsoqrVisDuf+C6w=";
   };
 
   buildInputs = [ rdkafka ];
@@ -87,6 +88,7 @@ buildPythonPackage rec {
     cachetools
     orjson
     pyflakes
+    pytest-asyncio
     pytestCheckHook
     requests-mock
     respx
