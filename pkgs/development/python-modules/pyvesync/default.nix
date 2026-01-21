ccc5da08108d055dd9c2f3689a1270ea3881a6df
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:37:46 +0100

    python3Packages.pyvesync: 3.3.3 -> 3.4.1
    https://github.com/webdjoe/pyvesync/releases/tag/3.4.1
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pyvesync/default.nix
+++ pkgs/development/python-modules/pyvesync/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pyvesync";
-  version = "3.4.0";
+  version = "3.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "webdjoe";
     repo = "pyvesync";
     tag = version;
-    hash = "sha256-G1Ov8xXIVkklxfLqhHiYbRgHEsjTQhG7k1V6Amtc+w4=";
+    hash = "sha256-fruuFt7Zb5ZDX8MmEXB4rypuYON3UG50mExnMpMQct4=";
   };
 
   build-system = [ setuptools ];
