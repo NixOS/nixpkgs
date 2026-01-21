e7358e7844f504a7610ee3361b13a9558254e68c
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:37:55 +0100

    python3Packages.remarshal: 1.2.0 -> 1.3.0
    https://github.com/remarshal-project/remarshal/releases/tag/v1.3.0
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/remarshal/default.nix
+++ pkgs/development/python-modules/remarshal/default.nix
@@ -22,14 +22,14 @@
 
 buildPythonPackage rec {
   pname = "remarshal";
-  version = "1.2.0"; # test with `nix-build pkgs/pkgs-lib/format`
+  version = "1.3.0"; # test with `nix-build pkgs/pkgs-lib/format`
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dbohdan";
     repo = "remarshal";
     tag = "v${version}";
-    hash = "sha256-y/odWWFJ7KDehYzUKSM/cprcCd+UaArEZFoYbtyW0Ok=";
+    hash = "sha256-/K8x6ij23pk5O1+XJdFHaGbZ47nFMbXzp+4UMO5dGp4=";
   };
 
   build-system = [ poetry-core ];
@@ -48,7 +48,7 @@ buildPythonPackage rec {
   nativeCheckInputs = [ pytestCheckHook ];
 
   meta = {
-    changelog = "https://github.com/remarshal-project/remarshal/releases/tag/v${version}";
+    changelog = "https://github.com/remarshal-project/remarshal/releases/tag/${src.tag}";
     description = "Convert between TOML, YAML and JSON";
     license = lib.licenses.mit;
     homepage = "https://github.com/dbohdan/remarshal";
