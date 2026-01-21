74945486812d70db59c7643f665c9033ec71e96d
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:21 +0100

    python3Packages.claripy: 9.2.154 -> 9.2.193
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/claripy/default.nix
+++ pkgs/development/python-modules/claripy/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "claripy";
-  version = "9.2.154";
+  version = "9.2.193";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "angr";
     repo = "claripy";
     tag = "v${version}";
-    hash = "sha256-90JX+VDWK/yKhuX6D8hbLxjIOS8vGKrN1PKR8iWjt2o=";
+    hash = "sha256-nZ7ORbhi0R79pcHpkx/lRVdfUsoutCqU+zHX8AICTUE=";
   };
 
   # z3 does not provide a dist-info, so python-runtime-deps-check will fail
