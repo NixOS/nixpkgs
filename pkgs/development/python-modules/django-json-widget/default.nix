fed9854ed96049b5d06f5856f76165a0bdde299e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:55 +0100

    python3Packages.django-json-widget: 2.0.3 -> 2.1.1
    https://github.com/jmrivas86/django-json-widget/blob/v2.1.1/CHANGELOG.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/django-json-widget/default.nix
+++ pkgs/development/python-modules/django-json-widget/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "django-json-widget";
-  version = "2.0.3";
+  version = "2.1.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jmrivas86";
     repo = "django-json-widget";
     tag = "v${version}";
-    hash = "sha256-11XDmSYJzrjcV3q3P0Kl2WkfoAtuPYzQt5D2ZzRTCCY=";
+    hash = "sha256-AABJHWoEIcyJyRHv3sp1d1l6ZByF8Q5h+xEHJe/4uC0=";
   };
 
   build-system = [ setuptools ];
@@ -39,7 +39,7 @@ buildPythonPackage rec {
   meta = {
     description = "Alternative widget that makes it easy to edit the jsonfield field of django";
     homepage = "https://github.com/jmrivas86/django-json-widget";
-    changelog = "https://github.com/jmrivas86/django-json-widget/blob/v${version}/CHANGELOG.rst";
+    changelog = "https://github.com/jmrivas86/django-json-widget/blob/${src.tag}/CHANGELOG.rst";
     # Contradictory license specifications
     # https://github.com/jmrivas86/django-json-widget/issues/93
     license = lib.licenses.mit;
