519223fdd1b7a80424822bc19d88656929f38c7e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:37:01 +0100

    python3Packages.pygobject-stubs: 2.14.0 -> 2.16.0
    https://github.com/pygobject/pygobject-stubs/blob/v2.16.0/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pygobject-stubs/default.nix
+++ pkgs/development/python-modules/pygobject-stubs/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "pygobject-stubs";
-  version = "2.14.0";
+  version = "2.16.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pygobject";
     repo = "pygobject-stubs";
     tag = "v${version}";
-    hash = "sha256-pConIc8FBq2a7yrfRHa07p2e/Axgrv4p+W0nq1WzERw=";
+    hash = "sha256-3gWz/mWAMoXAHMfCFv3ri680iuMm0WLtGyMODujq31M=";
   };
 
   build-system = [ setuptools ];
