c911f96f04551850fafd7e1fa9ee420b7b804d9d
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 23:34:07 +0100

    fixup! python3Packages.packageurl-python: 0.17.5 -> 0.17.6

--- pkgs/development/python-modules/packageurl-python/default.nix
+++ pkgs/development/python-modules/packageurl-python/default.nix
@@ -1,33 +1,39 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
+  pydantic,
   pytestCheckHook,
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "packageurl-python";
   version = "0.17.6";
   pyproject = true;
 
-  src = fetchPypi {
-    pname = "packageurl_python";
-    inherit version;
-    hash = "sha256-ElLOOhAjcspvhuuWjhb5AUxLpRHFw32Vp/Aj4spuXCU=";
+  src = fetchFromGitHub {
+    owner = "package-url";
+    repo = "packageurl-python";
+    tag = "v${finalAttrs.version}";
+    fetchSubmodules = true;
+    hash = "sha256-jH4zJN3XGPFBnto26pcvADXogpooj3dqpqkWnKXgICY=";
   };
 
   build-system = [ setuptools ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  nativeCheckInputs = [
+    pydantic
+    pytestCheckHook
+  ];
 
   pythonImportsCheck = [ "packageurl" ];
 
   meta = {
     description = "Python parser and builder for package URLs";
     homepage = "https://github.com/package-url/packageurl-python";
-    changelog = "https://github.com/package-url/packageurl-python/blob/v${version}/CHANGELOG.rst";
+    changelog = "https://github.com/package-url/packageurl-python/blob/${finalAttrs.src.tag}/CHANGELOG.rst";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ armijnhemel ];
   };
-}
+})
