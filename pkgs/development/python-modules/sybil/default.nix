5c7a3cc6333b01cdb5052ed7d6cb7b8d8364e2de
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:38:46 +0100

    python3Packages.sybil: 9.1.0 -> 9.3.0
    https://github.com/simplistix/sybil/blob/9.3.0/CHANGELOG.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/sybil/default.nix
+++ pkgs/development/python-modules/sybil/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "sybil";
-  version = "9.1.0";
+  version = "9.3.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "simplistix";
     repo = "sybil";
     tag = version;
-    hash = "sha256-ov8b8NPBbiqB/pcKgdD2D+xNSxUM1uGK8EP+20K7eGQ=";
+    hash = "sha256-rr6zVY1yJVL/s/Wg5S4pSljj9Zq+jo7CZ6TZvtPpxow=";
   };
 
   build-system = [ setuptools ];
@@ -27,7 +27,7 @@ buildPythonPackage rec {
   meta = {
     description = "Automated testing for the examples in your documentation";
     homepage = "https://github.com/cjw296/sybil";
-    changelog = "https://github.com/simplistix/sybil/blob/${version}/CHANGELOG.rst";
+    changelog = "https://github.com/simplistix/sybil/blob/${src.tag}/CHANGELOG.rst";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
