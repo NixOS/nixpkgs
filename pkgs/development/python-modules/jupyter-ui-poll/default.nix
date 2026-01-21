d4ebb2811232e40172a4ee563f7ba156a21bd0a1
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:34:30 +0100

    python3Packages.jupyter-ui-poll: 1.0.0 -> 1.1.0
    https://github.com/Kirill888/jupyter-ui-poll/releases/tag/v1.1.0
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/jupyter-ui-poll/default.nix
+++ pkgs/development/python-modules/jupyter-ui-poll/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "jupyter-ui-poll";
-  version = "1.0.0";
+  version = "1.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Kirill888";
     repo = "jupyter-ui-poll";
     tag = "v${version}";
-    hash = "sha256-mlgLd6uFDSxRBj4+Eidea2CE7FuG6NzJLWGec4KPd9k=";
+    hash = "sha256-Q+y0Xr7wuo8ZwCHEELSi0QSXa8DLtfZ8XQc48eOk4bw=";
   };
 
   build-system = [ setuptools ];
@@ -28,7 +28,7 @@ buildPythonPackage rec {
   meta = {
     description = "Block jupyter cell execution while interacting with widgets";
     homepage = "https://github.com/Kirill888/jupyter-ui-poll";
-    changelog = "https://github.com/Kirill888/jupyter-ui-poll/releases/tag/v${version}";
+    changelog = "https://github.com/Kirill888/jupyter-ui-poll/releases/tag/${src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ bcdarwin ];
   };
