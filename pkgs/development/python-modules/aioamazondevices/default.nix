c15584331151f98af73da7f3daed74f206f03b19
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:31:29 +0100

    python3Packages.aioamazondevices: 11.0.2 -> 11.1.1
    https://github.com/chemelli74/aioamazondevices/blob/v11.1.1/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/aioamazondevices/default.nix
+++ pkgs/development/python-modules/aioamazondevices/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage rec {
   pname = "aioamazondevices";
-  version = "11.0.2";
+  version = "11.1.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "chemelli74";
     repo = "aioamazondevices";
     tag = "v${version}";
-    hash = "sha256-nxKWnNLmraPtMA+WZO4FOWbcbZr/HS6dhACKcFUq7D0=";
+    hash = "sha256-D0QxCnwyXtHvcx+BaO4mzGuZxLZtgogQpHXvFckX9f8=";
   };
 
   build-system = [ poetry-core ];
