73023aa506039f56cce6f6236c7d3dcef32d6b04
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 15:28:39 +0100

    python3Packages.more-itertools: 10.7.0 -> 10.8.0
    https://more-itertools.readthedocs.io/en/stable/versions.html

--- pkgs/development/python-modules/more-itertools/default.nix
+++ pkgs/development/python-modules/more-itertools/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "more-itertools";
-  version = "10.7.0";
+  version = "10.8.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "more-itertools";
     repo = "more-itertools";
     tag = "v${version}";
-    hash = "sha256-4ZzuWVRrihhEoYRDAoYLZINR11iHs0sXF/bRm6gQoEA=";
+    hash = "sha256-ZKvucnPFCA6Q4EQn/nKC9LIevOdSYXHIJ3w3Frregic=";
   };
 
   build-system = [ flit-core ];
