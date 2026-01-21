7cda406e09695c9f3bcc11e1513781d1d8d692db
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 20:31:19 +0100

    fixup! python3Packages.pystemmer: 2.2.0.1 -> 3.0.0

--- pkgs/development/python-modules/pystemmer/default.nix
+++ pkgs/development/python-modules/pystemmer/default.nix
@@ -2,7 +2,6 @@
   lib,
   python,
   fetchFromGitHub,
-  fetchpatch2,
   buildPythonPackage,
   cython,
   setuptools,
@@ -21,15 +20,6 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-c3ucbneUo5UBfdrd5Ktl4HriVusvWBEA1brrgahEQ9A=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      # relax cython constraint
-      name = "pystemmer-relax-cython.patch";
-      url = "https://github.com/snowballstem/pystemmer/commit/d3d423dc877b4f49e0ab1776f7edaff37feb6799.patch";
-      hash = "sha256-9K6gy/cLFPfW82XYHVVPXUbQhf8XyB4NUi4YqNtyWcw=";
-    })
-  ];
 
   build-system = [
     cython
