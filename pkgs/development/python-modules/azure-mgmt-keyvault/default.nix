38f4c2700dab8e2e7dfccd99ef5cb9aa23d4fc60
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:31:58 +0100

    python3Packages.azure-mgmt-keyvault: 12.0.0 -> 13.0.0
    https://github.com/Azure/azure-sdk-for-python/blob/azure-mgmt-keyvault_13.0.0/sdk/keyvault/azure-mgmt-keyvault/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/azure-mgmt-keyvault/default.nix
+++ pkgs/development/python-modules/azure-mgmt-keyvault/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-keyvault";
-  version = "12.0.0";
+  version = "13.0.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "azure_mgmt_keyvault";
     inherit version;
-    hash = "sha256-4s8Y6KSSi10cqxJ75C6prQJG1ofKEvwnoq1mSHzhyGs=";
+    hash = "sha256-VsEpBObZrEn4hkg+UOP2Ndi/Q6SJ6zL6e0gy8yPTlsc=";
   };
 
   build-system = [ setuptools ];
