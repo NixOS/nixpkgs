38ab2b227f829e73622808be012a49bb263357ac
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:44 +0100

    python3Packages.dbt-core: 1.10.15 -> 1.11.2
    https://github.com/dbt-labs/dbt-core/blob/v1.11.2/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/dbt-core/default.nix
+++ pkgs/development/python-modules/dbt-core/default.nix
@@ -37,14 +37,14 @@
 
 buildPythonPackage rec {
   pname = "dbt-core";
-  version = "1.10.15";
+  version = "1.11.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "dbt-labs";
     repo = "dbt-core";
     tag = "v${version}";
-    hash = "sha256-n/tU34XUf/s4m/SbPth/jlfo2xKhQk1hVjP3vLA/67s=";
+    hash = "sha256-+7q332Te3R6g8HvT1Gwa7vHo8OBmT0/E/CzunBYIvZk=";
   };
 
   sourceRoot = "${src.name}/core";
