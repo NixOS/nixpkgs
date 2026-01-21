3f0643a326449351bc1f3e760c84f27deb6c1992
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:31:57 +0100

    python3Packages.azure-mgmt-cosmosdb: 9.8.0 -> 9.9.0
    https://github.com/Azure/azure-sdk-for-python/blob/azure-mgmt-cosmosdb_9.9.0/sdk/cosmos/azure-mgmt-cosmosdb/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/azure-mgmt-cosmosdb/default.nix
+++ pkgs/development/python-modules/azure-mgmt-cosmosdb/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-cosmosdb";
-  version = "9.8.0";
+  version = "9.9.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "azure_mgmt_cosmosdb";
     inherit version;
-    hash = "sha256-IU7kcWU4flePhuZhH7MptNVNwpWtedo3udksXW0g0bE=";
+    hash = "sha256-Rni/BCvcIIqiT8pxdnrCm28qJyKseHJgg3Glki87bDc=";
   };
 
   build-system = [ setuptools ];
