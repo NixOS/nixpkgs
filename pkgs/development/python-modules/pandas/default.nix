d6a3e4cff42955a8f7796410566e68f02267ace9
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 23:37:55 +0100

    fixup! python3Packages.pandas: 2.3.3 -> 3.0.0

--- pkgs/development/python-modules/pandas/default.nix
+++ pkgs/development/python-modules/pandas/default.nix
@@ -73,22 +73,6 @@ let
       hash = "sha256-pJTi0CL/ymXwERBsN4i7vjyUehklPjSSgfwpYbgoI+c=";
     };
 
-    # A NOTE regarding the Numpy version relaxing: Both Numpy versions 1.x &
-    # 2.x are supported. However upstream wants to always build with Numpy 2,
-    # and with it to still be able to run with a Numpy 1 or 2. We insist to
-    # perform this substitution even though python3.pkgs.numpy is of version 2
-    # nowadays, because our ecosystem unfortunately doesn't allow easily
-    # separating runtime and build-system dependencies. See also:
-    #
-    # https://discourse.nixos.org/t/several-comments-about-priorities-and-new-policies-in-the-python-ecosystem/51790
-    #
-    # Being able to build (& run) with Numpy 1 helps for python environments
-    # that override globally the `numpy` attribute to point to `numpy_1`.
-    postPatch = ''
-      substituteInPlace pyproject.toml \
-        --replace-fail "numpy>=2.0" numpy
-    '';
-
     build-system = [
       cython
       meson-python
@@ -105,7 +89,6 @@ let
     dependencies = [
       numpy
       python-dateutil
-      pytz
       tzdata
     ];
 
@@ -165,6 +148,7 @@ let
           ];
           spss = [ pyreadstat ];
           sql-other = [ sqlalchemy ];
+          timezone = [ pytz ];
           xml = [ lxml ];
         };
       in
