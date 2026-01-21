3bdbc290907675ab377743156b317e7ee21913e1
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 02:01:54 +0100

    python3Packages.expandvar: fix pytest9 compat

--- pkgs/development/python-modules/expandvars/default.nix
+++ pkgs/development/python-modules/expandvars/default.nix
@@ -2,12 +2,14 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  fetchpatch,
 
   # build-system
   hatchling,
 
   # tests
   pytestCheckHook,
+  pytest-cov-stub,
 }:
 
 buildPythonPackage rec {
@@ -20,11 +22,22 @@ buildPythonPackage rec {
     hash = "sha256-mK3YJot2Df7kV73hwXv3RXlf3rwit92rdf0yeGU/HgU=";
   };
 
+  patches = [
+    (fetchpatch {
+      name = "pytest9-compat.patch";
+      url = "https://github.com/sayanarijit/expandvars/commit/0ab5747185be9135b0711e72fc64dfa6a33f3fd3.patch";
+      hash = "sha256-raO5dGbcXb0adUCeHmnWp49vpIMllRW9Ow8rG4OH+Hs=";
+    })
+  ];
+
   nativeBuildInputs = [ hatchling ];
 
   pythonImportsCheck = [ "expandvars" ];
 
-  nativeCheckInputs = [ pytestCheckHook ];
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-cov-stub
+  ];
 
   meta = {
     description = "Expand system variables Unix style";
