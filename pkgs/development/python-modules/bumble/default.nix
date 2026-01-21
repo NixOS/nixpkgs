75d78ab2bb3cf67bdbd8aaddb25d1faae0b68abb
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:15 +0100

    python3Packages.bumble: 0.0.221 -> 0.0.223
    https://github.com/google/bumble/releases/tag/v0.0.223
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/bumble/default.nix
+++ pkgs/development/python-modules/bumble/default.nix
@@ -28,14 +28,14 @@
 
 buildPythonPackage rec {
   pname = "bumble";
-  version = "0.0.221";
+  version = "0.0.223";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "bumble";
     tag = "v${version}";
-    hash = "sha256-GCcvbYLHChvrsQuhFjeYnncjrzFqOlmL+LlG7t2iAkE=";
+    hash = "sha256-Iei2RBk7Tpb8yeTKdtftJfbG3DwuGjvBZuhhs+nMKcU=";
   };
 
   build-system = [
