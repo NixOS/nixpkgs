b5923f62262e1a3266d97f861b5a6a84b0601ba6
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:36:13 +0100

    python3Packages.orange3: 3.39.0 -> 3.40.0
    https://github.com/biolab/orange3/blob/3.40.0/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/orange3/default.nix
+++ pkgs/development/python-modules/orange3/default.nix
@@ -58,14 +58,14 @@
 let
   self = buildPythonPackage rec {
     pname = "orange3";
-    version = "3.39.0";
+    version = "3.40.0";
     pyproject = true;
 
     src = fetchFromGitHub {
       owner = "biolab";
       repo = "orange3";
       tag = version;
-      hash = "sha256-P2e3Wq33UXnTmGSxkoW8kYYCBfYBB9Z50v4g7n//Fbw=";
+      hash = "sha256-AOQGqOYaP3bPZtgjsVCxhA1T34Y9EIR5VeMwhZWmI88=";
     };
 
     build-system = [
