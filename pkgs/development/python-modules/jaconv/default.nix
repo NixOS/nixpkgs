68ae03b91635ed6776e7c90861ac04397baf3dd7
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:34:23 +0100

    python3Packages.jaconv: 0.3.4 -> 0.4.1
    https://github.com/ikegami-yukino/jaconv/blob/v0.4.1/CHANGES.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/jaconv/default.nix
+++ pkgs/development/python-modules/jaconv/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "jaconv";
-  version = "0.3.4";
+  version = "0.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ikegami-yukino";
     repo = "jaconv";
     tag = "v${version}";
-    hash = "sha256-9ruhOLaYNESeKOwJs3IN6ct66fSq7My9DOyA7/cH3d0=";
+    hash = "sha256-43sziwJ/SDdpLHJyGXyI5nXEofbos2W+NV7DlOpWWa8=";
   };
 
   patches = [
@@ -32,7 +32,7 @@ buildPythonPackage rec {
   meta = {
     description = "Python Japanese character interconverter for Hiragana, Katakana, Hankaku and Zenkaku";
     homepage = "https://github.com/ikegami-yukino/jaconv";
-    changelog = "https://github.com/ikegami-yukino/jaconv/blob/v${version}/CHANGES.rst";
+    changelog = "https://github.com/ikegami-yukino/jaconv/blob/${src.tag}/CHANGES.rst";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
