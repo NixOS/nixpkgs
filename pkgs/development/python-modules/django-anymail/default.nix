22979b752bc262ebba553d1363f211e3ccf5bfb4
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:51 +0100

    python3Packages.django-anymail: 13.1 -> 14.0
    https://github.com/anymail/django-anymail/blob/v14.0/CHANGELOG.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/django-anymail/default.nix
+++ pkgs/development/python-modules/django-anymail/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage rec {
   pname = "django-anymail";
-  version = "13.1";
+  version = "14.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "anymail";
     repo = "django-anymail";
     tag = "v${version}";
-    hash = "sha256-R/PPAar93yMslKnhiiMcv4DIZrIJEQGqMm5yLZ9Mn+8=";
+    hash = "sha256-S/HEbWyvfAQ/kHodN0ylrg1lU7lYWGUznSqVC+yUzSU=";
   };
 
   build-system = [ hatchling ];
