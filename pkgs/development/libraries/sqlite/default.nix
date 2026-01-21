NixOS/nixpkgs

commit a3987ff32852e6747c6e8a763a56a4ffd3a9f0e2

sqlite: Build canonical source with tcl
See
https://github.com/sqlite/sqlite/blob/051e942f5db1cbc3fce64d3a52df7428fe517de4/autoconf/README.txt#L27-L40
for motivation

Also adds support for the TCL sqlite library

pkgs/development/libraries/sqlite/default.nix
@@ -3,6 +3,7 @@
   stdenv,
   fetchurl,
   unzip,
+  tcl,
   zlib,
   readline,
   ncurses,
@@ -32,8 +33,8 @@ stdenv.mkDerivation rec {
   # nixpkgs-update: no auto update
   # NB! Make sure to update ./tools.nix src (in the same directory).
   src = fetchurl {
-    url = "https://sqlite.org/2026/sqlite-autoconf-${archiveVersion version}.tar.gz";
-    hash = "sha256-+9ifhmsUA7tmoUMGVEAInddhAPIjgxTZInSggtTyt7s=";
+    url = "https://sqlite.org/2026/sqlite-src-${archiveVersion version}.zip";
+    hash = "sha256-hREPdi1QeUFNmd1deRe8P/fgWHbmzL0T2ElqOBfyCCk=";
   };
   docsrc = fetchurl {
     url = "https://sqlite.org/2026/sqlite-doc-${archiveVersion version}.zip";
@@ -55,6 +56,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     unzip
+    tcl
   ];
   buildInputs = [
     zlib
@@ -74,10 +76,13 @@ stdenv.mkDerivation rec {
   # on a per-output basis.
   setOutputFlags = false;
 
+  env.TCLLIBDIR = "${placeholder "out"}/lib";
+
   configureFlags = [
     "--bindir=${placeholder "bin"}/bin"
     "--includedir=${placeholder "dev"}/include"
     "--libdir=${placeholder "out"}/lib"
+    "--with-tcl=${lib.getLib tcl}/lib"
   ]
   ++ lib.optional (!interactive) "--disable-readline"
   # autosetup only looks up readline.h in predefined set of directories.

commit c5516b9d0bbbf339e7f1e845b657387ea2a498fc

sqlite: fix static build
TCL support requires building libtclsqlite as a shared library.

Fixes: a3987ff32852 ("sqlite: Build canonical source with tcl")

pkgs/development/libraries/sqlite/default.nix
@@ -82,7 +82,7 @@ stdenv.mkDerivation rec {
     "--bindir=${placeholder "bin"}/bin"
     "--includedir=${placeholder "dev"}/include"
     "--libdir=${placeholder "out"}/lib"
-    "--with-tcl=${lib.getLib tcl}/lib"
+    (if stdenv.hostPlatform.isStatic then "--disable-tcl" else "--with-tcl=${lib.getLib tcl}/lib")
   ]
   ++ lib.optional (!interactive) "--disable-readline"
   # autosetup only looks up readline.h in predefined set of directories.
@@ -129,6 +129,8 @@ stdenv.mkDerivation rec {
   '';
 
   doCheck = true;
+  # When tcl is not available, only run test targets that don't need it.
+  checkTarget = lib.optionalString stdenv.hostPlatform.isStatic "fuzztest sourcetest";
 
   passthru = {
     tests = {