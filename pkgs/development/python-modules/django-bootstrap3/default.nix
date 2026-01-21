ef8dabf2695c5ce711f1b58c90b939ba093eb44b
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:52 +0100

    python3Packages.django-bootstrap3: 25.3 -> 26.1
    https://github.com/zostera/django-bootstrap3/blob/v26.1/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/django-bootstrap3/default.nix
+++ pkgs/development/python-modules/django-bootstrap3/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "django-bootstrap3";
-  version = "25.3";
+  version = "26.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "zostera";
     repo = "django-bootstrap3";
     tag = "v${version}";
-    hash = "sha256-OCr25Sc5fbL5ivrM2LpDAcTj8bPX4Q23Yj1j6jRG03U=";
+    hash = "sha256-DpdgwG+4We/r3NZ50no/SurEtL1BkB3P0nMv8KRj+GY=";
   };
 
   build-system = [ uv-build ];
@@ -42,7 +42,7 @@ buildPythonPackage rec {
   meta = {
     description = "Bootstrap 3 integration for Django";
     homepage = "https://github.com/zostera/django-bootstrap3";
-    changelog = "https://github.com/zostera/django-bootstrap3/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/zostera/django-bootstrap3/blob/${src.tag}/CHANGELOG.md";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ hexa ];
   };
