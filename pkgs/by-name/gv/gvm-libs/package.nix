339a85107b1c9db9894426fc8c48a088a2464154
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-21 10:19:37 +0100

    gvm-libs: migrate to finalAttrs

--- pkgs/by-name/gv/gvm-libs/package.nix
+++ pkgs/by-name/gv/gvm-libs/package.nix
@@ -24,14 +24,14 @@
   zlib,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "gvm-libs";
   version = "22.35.2";
 
   src = fetchFromGitHub {
     owner = "greenbone";
     repo = "gvm-libs";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-DVYU+6hUps2nwgkTWu228wVYtke4oDFUqXM73DEN6LM=";
   };
 
@@ -73,9 +73,9 @@ stdenv.mkDerivation rec {
   meta = {
     description = "Libraries module for the Greenbone Vulnerability Management Solution";
     homepage = "https://github.com/greenbone/gvm-libs";
-    changelog = "https://github.com/greenbone/gvm-libs/releases/tag/${src.tag}";
-    license = with lib.licenses; [ gpl2Plus ];
+    changelog = "https://github.com/greenbone/gvm-libs/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.gpl2Plus;
     maintainers = with lib.maintainers; [ fab ];
     platforms = lib.platforms.linux;
   };
-}
+})
