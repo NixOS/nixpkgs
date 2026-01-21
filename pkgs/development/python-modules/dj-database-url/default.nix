592b4f57bd6b330ddc6c32428a92d0e83c676772
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:32:50 +0100

    python3Packages.dj-database-url: 3.0.1 -> 3.1.0
    https://github.com/jazzband/dj-database-url/blob/v3.1.0/CHANGELOG.md
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/dj-database-url/default.nix
+++ pkgs/development/python-modules/dj-database-url/default.nix
@@ -3,22 +3,22 @@
   buildPythonPackage,
   fetchFromGitHub,
   django,
-  setuptools,
+  uv-build,
 }:
 
 buildPythonPackage rec {
   pname = "dj-database-url";
-  version = "3.0.1";
+  version = "3.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jazzband";
     repo = "dj-database-url";
     tag = "v${version}";
-    hash = "sha256-wiPTszgix4QjF82f5mmNvDKGspYl15jScK2TAsP5zP8=";
+    hash = "sha256-zQiyEtDTFZHwuEIfRZHdgszyfsRSayFqPv9MIS4Ip6s=";
   };
 
-  build-system = [ setuptools ];
+  build-system = [ uv-build ];
 
   dependencies = [ django ];
 
