7d2584c2d1de447823c3a9fff753e425d7c92395
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:36:48 +0100

    python3Packages.py-vapid: 1.9.2 -> 1.9.4
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/py-vapid/default.nix
+++ pkgs/development/python-modules/py-vapid/default.nix
@@ -2,7 +2,7 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  setuptools,
+  hatchling,
   mock,
   pytestCheckHook,
   cryptography,
@@ -10,22 +10,16 @@
 
 buildPythonPackage rec {
   pname = "py-vapid";
-  version = "1.9.2";
+  version = "1.9.4";
   pyproject = true;
 
   src = fetchPypi {
     pname = "py_vapid";
     inherit version;
-    hash = "sha256-PIlzts+DhK0MmuZNYnDMxIDguSxwLY9eoswD5rUSR/k=";
+    hash = "sha256-oAQCNWDLxU40/AY4CgWA8E/8x4joT7bRnpM57rZVGig=";
   };
 
-  patches = [
-    # Fix tests with latest cryptography
-    # Upstream PR: https://github.com/web-push-libs/vapid/pull/110
-    ./cryptography.patch
-  ];
-
-  build-system = [ setuptools ];
+  build-system = [ hatchling ];
 
   dependencies = [ cryptography ];
 
