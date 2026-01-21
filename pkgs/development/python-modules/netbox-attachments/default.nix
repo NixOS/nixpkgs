dbd9703bcf905f85f3e2188738d2e91feb5d9d6b
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:35:50 +0100

    python3Packages.netbox-attachments: 9.0.0 -> 10.0.1
    https://github.com/Kani999/netbox-attachments/releases/tag/v10.0.1
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/netbox-attachments/default.nix
+++ pkgs/development/python-modules/netbox-attachments/default.nix
@@ -10,7 +10,7 @@
 }:
 buildPythonPackage rec {
   pname = "netbox-attachments";
-  version = "9.0.0";
+  version = "10.0.1";
   pyproject = true;
 
   disabled = python.pythonVersion != netbox.python.pythonVersion;
@@ -18,8 +18,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "Kani999";
     repo = "netbox-attachments";
-    tag = version;
-    hash = "sha256-lm1+MWDT9N6Y+Uf1t3jTrFRF8jRy86JNdZSL1RQO7cw=";
+    tag = "v${version}";
+    hash = "sha256-ZsK6RKYkfzn6YSS9ern9QxLsRdcwzt8BmMdYGJTMOKM=";
   };
 
   build-system = [ setuptools ];
