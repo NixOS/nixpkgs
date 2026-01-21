0ff39b85d1fd27f190d2f66261f9edd16a2ce037
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:37:37 +0100

    python3Packages.python-didl-lite: 1.4.1 -> 1.5.0
    https://github.com/StevenLooman/python-didl-lite/blob/1.5.0/CHANGES.rst
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/python-didl-lite/default.nix
+++ pkgs/development/python-modules/python-didl-lite/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonPackage rec {
   pname = "python-didl-lite";
-  version = "1.4.1";
+  version = "1.5.0";
   format = "setuptools";
   pyroject = true;
 
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "StevenLooman";
     repo = "python-didl-lite";
     tag = version;
-    hash = "sha256-YSP24GiSngwwvpwfOoKkm/i0qCtpryjcshxZCskf5BM=";
+    hash = "sha256-pdXdGRycMB6M6qnPl+Z+ezRw6td45IqYkEpx4YtL1rQ=";
   };
 
   nativeBuildInputs = [ setuptools ];
@@ -31,7 +31,7 @@ buildPythonPackage rec {
   meta = {
     description = "DIDL-Lite (Digital Item Declaration Language) tools for Python";
     homepage = "https://github.com/StevenLooman/python-didl-lite";
-    changelog = "https://github.com/StevenLooman/python-didl-lite/blob/${version}/CHANGES.rst";
+    changelog = "https://github.com/StevenLooman/python-didl-lite/blob/${src.tag}/CHANGES.rst";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ hexa ];
   };
