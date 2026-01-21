3bb78829125c8750b24fb0d0d27a7d2a53d6d7be
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:39:07 +0100

    python3Packages.types-python-dateutil: 2.9.0.20250708 -> 2.9.0.20251115
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/types-python-dateutil/default.nix
+++ pkgs/development/python-modules/types-python-dateutil/default.nix
@@ -7,13 +7,13 @@
 
 buildPythonPackage rec {
   pname = "types-python-dateutil";
-  version = "2.9.0.20250708";
+  version = "2.9.0.20251115";
   pyproject = true;
 
   src = fetchPypi {
     pname = "types_python_dateutil";
     inherit version;
-    hash = "sha256-zNvXXastbJaWw1BXnzTP/iwoHkxfJ6WFsqJDjdHVyKs=";
+    hash = "sha256-ikfyw5IPUqmUBWuHhjCbQxQ/qlpk1Muyci1q3avfGlg=";
   };
 
   build-system = [ setuptools ];
