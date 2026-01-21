519be18d9af75ae6a51b36f6afc8aaa3513d1a7a
lelgenio <lelgenio@lelgenio.com>
2026-01-21 13:40:11 -0300

    material-maker: 1.3 -> 1.5

--- pkgs/by-name/ma/material-maker/package.nix
+++ pkgs/by-name/ma/material-maker/package.nix
@@ -2,9 +2,9 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  godot3-headless,
-  godot3-export-templates,
-  libglvnd,
+  godot_4_5,
+  vulkan-headers,
+  vulkan-loader,
   libx11,
   libxcursor,
   libxext,
@@ -18,19 +18,20 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "material-maker";
-  version = "1.3";
+  version = "1.5";
 
   src = fetchFromGitHub {
     owner = "RodZill4";
     repo = "material-maker";
     rev = finalAttrs.version;
-    hash = "sha256-vyagu7xL4ITt+xyoYyCcF8qq6L9sR6Ltdl6NwfrbZdA=";
+    hash = "sha256-BnXkEma3J7myF115zMv/VAGtwbbt1asqLUVvRCvrGG8=";
   };
 
-  nativeBuildInputs = [ godot3-headless ];
+  nativeBuildInputs = [ godot_4_5 ];
 
   buildInputs = [
-    libglvnd
+    vulkan-headers
+    vulkan-loader
 
     libxinerama
     libxcursor
@@ -47,18 +48,8 @@ stdenv.mkDerivation (finalAttrs: {
 
     export HOME=$TMPDIR
 
-    # Link the export-templates to the expected location. The --export commands
-    # expects the template-file at .../templates/{godot-version}.stable/linux_x11_64_release
-    mkdir -p $HOME/.local/share/godot
-    ln -s ${godot3-export-templates}/share/godot/templates $HOME/.local/share/godot
-
-    # Don't use the included export template, which might use a mismatched version of godot.
-    rm ./material_maker/misc/linux/godot.x11.opt.64
-    substituteInPlace ./export_presets.cfg \
-      --replace-fail '"material_maker/misc/linux/godot.x11.opt.64"' '""'
-
     mkdir -vp build
-    godot3-headless -v --export 'Linux/X11' build/material-maker
+    godot4 -v --headless --export-release 'Linux/X11' build/material-maker
 
     runHook postBuild
   '';
