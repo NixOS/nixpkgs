df93562690d7381547032372a9bb2b1551f5f56f
faukah <fau@faukah.com>
2026-01-21 20:53:21 +0100

    imagemagick: 7.1.2-12 -> 7.1.2-13
    (cherry picked from commit cfb11ceaa4d07f3b7ce3c8798bd2a8603a424c4d)

--- pkgs/applications/graphics/ImageMagick/default.nix
+++ pkgs/applications/graphics/ImageMagick/default.nix
@@ -85,13 +85,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "imagemagick";
-  version = "7.1.2-12";
+  version = "7.1.2-13";
 
   src = fetchFromGitHub {
     owner = "ImageMagick";
     repo = "ImageMagick";
     tag = finalAttrs.version;
-    hash = "sha256-0X8Zpr4frknRbWzAu1nprok2ceScTHV8d4+ktnBpQF0=";
+    hash = "sha256-meADRjoV1c48laD35TuWAwuE95L90agROuuKBd++Kn8=";
   };
 
   outputs = [
