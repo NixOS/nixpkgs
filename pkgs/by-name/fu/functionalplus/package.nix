b3b1e0099d8d917ccd8376b97d66c137ecc72587
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-21 23:22:35 +0000

    functionalplus: 0.2.25 -> 0.2.27

--- pkgs/by-name/fu/functionalplus/package.nix
+++ pkgs/by-name/fu/functionalplus/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "functionalplus";
-  version = "0.2.25";
+  version = "0.2.27";
 
   src = fetchFromGitHub {
     owner = "Dobiasd";
     repo = "FunctionalPlus";
     rev = "v${version}";
-    sha256 = "sha256-eKCOi5g8YdKgxaI/mLlqB2m1zwrU9DOSrQF+PW2DBBQ=";
+    sha256 = "sha256-LlWdzxfFkbfkb9wAmpb86Ah97pWlW3w7DdW6JPu1xdc=";
   };
 
   nativeBuildInputs = [ cmake ];
