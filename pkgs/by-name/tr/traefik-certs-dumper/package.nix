a6b13632fcf198e5e1f6a9c74b0b2d25ec19238a
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-21 02:56:50 +0000

    traefik-certs-dumper: 2.10.0 -> 2.11.0

--- pkgs/by-name/tr/traefik-certs-dumper/package.nix
+++ pkgs/by-name/tr/traefik-certs-dumper/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "traefik-certs-dumper";
-  version = "2.10.0";
+  version = "2.11.0";
 
   src = fetchFromGitHub {
     owner = "ldez";
     repo = "traefik-certs-dumper";
     rev = "v${version}";
-    sha256 = "sha256-zXbtabh5ZziELZHzvYisXETPUmhHAVo6sMuF4O3crBY=";
+    sha256 = "sha256-rjD0zt5kJ7A4TLn3jQBLGvzvHthszP9AvmcILVo5lzk=";
   };
 
-  vendorHash = "sha256-WpYxI+7qBYibojPtYlWmDrmJYlRlVwTaqCMI5Vzh1RI=";
+  vendorHash = "sha256-hGmcE8vEJI4nZOVFbDGWpnfTyupFydwGj09gMb2Mctc=";
   excludedPackages = "integrationtest";
 
   meta = {
