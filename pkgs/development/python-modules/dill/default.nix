33c9b004c5fe403696efc3655f795dfa898b4414
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 15:40:30 +0100

    python3Packages.dill: 0.4.0-unstable-2025-11-09 -> 0.4.1
    https://github.com/uqfoundation/dill/releases/tag/dill-0.4.1

--- pkgs/development/python-modules/dill/default.nix
+++ pkgs/development/python-modules/dill/default.nix
@@ -10,16 +10,16 @@
   datasets,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "dill";
-  version = "0.4.0-unstable-2025-11-09";
+  version = "0.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "uqfoundation";
     repo = "dill";
-    rev = "d948ecd748772f2812361982ec1496da0cd47b53";
-    hash = "sha256-/A84BpZnwSwsEYqLL0Xdf8OjJtg1UMu6dig3QEN+n1A=";
+    tag = finalAttrs.version;
+    hash = "sha256-Yh9WvescLgV7DmxGBTGKsb29+eRzF9qjZMg0DQQyLyY=";
   };
 
   nativeBuildInputs = [ setuptools ];
@@ -39,7 +39,7 @@ buildPythonPackage rec {
   meta = {
     description = "Serialize all of python (almost)";
     homepage = "https://github.com/uqfoundation/dill/";
-    changelog = "https://github.com/uqfoundation/dill/releases/tag/dill-${version}";
+    changelog = "https://github.com/uqfoundation/dill/releases/tag/dill-${finalAttrs.version}";
     license = lib.licenses.bsd3;
   };
-}
+})
