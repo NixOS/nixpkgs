f3aa1c23fe4ab8fca605f7827a955f3778916074
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-21 09:49:51 +0100

    python313Packages.google-cloud-workstations: migrate to finalAttrs

--- pkgs/development/python-modules/google-cloud-workstations/default.nix
+++ pkgs/development/python-modules/google-cloud-workstations/default.nix
@@ -13,14 +13,14 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "google-cloud-workstations";
   version = "0.7.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "google_cloud_workstations";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-poGhvPGpD+qOFacXsSqwaki5EZaTMQpAoXh7X0kqUsc=";
   };
 
@@ -50,8 +50,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python Client for Cloud Workstations";
     homepage = "https://github.com/googleapis/google-cloud-python/tree/main/packages/google-cloud-workstations";
-    changelog = "https://github.com/googleapis/google-cloud-python/blob/google-cloud-workstations-v${version}/packages/google-cloud-workstations/CHANGELOG.md";
+    changelog = "https://github.com/googleapis/google-cloud-python/blob/google-cloud-workstations-v${finalAttrs.version}/packages/google-cloud-workstations/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
