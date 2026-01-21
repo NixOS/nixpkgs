377c92a9acf7762806aafe7d7be1386713f7d605
Doron Behar <doron.behar@gmail.com>
2026-01-21 18:09:56 +0200

    cifs-utils: split bin, dev & man outputs

--- pkgs/by-name/ci/cifs-utils/package.nix
+++ pkgs/by-name/ci/cifs-utils/package.nix
@@ -40,12 +40,19 @@ stdenv.mkDerivation rec {
   ];
 
   configureFlags = [
-    "ROOTSBINDIR=$(out)/sbin"
+    "ROOTSBINDIR=$(bin)/sbin"
   ]
   ++ lib.optionals (stdenv.hostPlatform != stdenv.buildPlatform) [
     # AC_FUNC_MALLOC is broken on cross builds.
     "ac_cv_func_malloc_0_nonnull=yes"
     "ac_cv_func_realloc_0_nonnull=yes"
+  ];
+
+  outputs = [
+    "out"
+    "bin"
+    "man"
+    "dev"
   ];
 
   meta = {
