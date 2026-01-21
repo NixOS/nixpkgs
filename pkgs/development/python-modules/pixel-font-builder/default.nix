b772ec9e9bab718a2255d5fd2d94283b38a73a96
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:36:33 +0100

    python3Packages.pixel-font-builder: 0.0.39 -> 0.0.47
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/pixel-font-builder/default.nix
+++ pkgs/development/python-modules/pixel-font-builder/default.nix
@@ -15,7 +15,7 @@
 
 buildPythonPackage rec {
   pname = "pixel-font-builder";
-  version = "0.0.39";
+  version = "0.0.47";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
   src = fetchPypi {
     pname = "pixel_font_builder";
     inherit version;
-    hash = "sha256-osEaZDmby0Xcg3oec4m6TEXJQDfMvWeJeLOCIOwEMZA=";
+    hash = "sha256-O3HtwoZUp89mUgVMMcAd4CCPFqQpsyqlmug+QgNpgNQ=";
   };
 
   build-system = [ hatchling ];
