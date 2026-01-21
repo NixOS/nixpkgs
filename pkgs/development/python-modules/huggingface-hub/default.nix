d9a1566e4b8a807775e7d39b14809a140fb987a4
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-21 18:34:08 +0100

    python3Packages.huggingface-hub: 0.35.3 -> 1.3.2
    https://github.com/huggingface/huggingface_hub/releases/tag/v1.3.2
    
    This commit was automatically generated using update-python-libraries.

--- pkgs/development/python-modules/huggingface-hub/default.nix
+++ pkgs/development/python-modules/huggingface-hub/default.nix
@@ -10,10 +10,13 @@
   filelock,
   fsspec,
   hf-xet,
+  httpx,
   packaging,
   pyyaml,
   requests,
+  shellingham,
   tqdm,
+  typer-slim,
   typing-extensions,
 
   # optional-dependencies
@@ -43,14 +46,14 @@
 
 buildPythonPackage rec {
   pname = "huggingface-hub";
-  version = "0.35.3";
+  version = "1.3.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "huggingface";
     repo = "huggingface_hub";
     tag = "v${version}";
-    hash = "sha256-KOq3qxt3AyWQIOG0+HUbNr15u85tyTEstoUkYBFkpC4=";
+    hash = "sha256-G7DkWIjFmNyAyNAfOupncIbDbImITl5Av8r0ibTlDMs=";
   };
 
   build-system = [ setuptools ];
@@ -59,10 +62,13 @@ buildPythonPackage rec {
     filelock
     fsspec
     hf-xet
+    httpx
     packaging
     pyyaml
     requests
+    shellingham
     tqdm
+    typer-slim
     typing-extensions
   ];
 
@@ -114,7 +120,7 @@ buildPythonPackage rec {
     description = "Download and publish models and other files on the huggingface.co hub";
     mainProgram = "hf";
     homepage = "https://github.com/huggingface/huggingface_hub";
-    changelog = "https://github.com/huggingface/huggingface_hub/releases/tag/v${version}";
+    changelog = "https://github.com/huggingface/huggingface_hub/releases/tag/${src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [
       GaetanLepage
