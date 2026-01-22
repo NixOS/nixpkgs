f629652945c9a7efc44a69ae6f2e03ec7b7f97ee
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2026-01-22 06:43:00 -0500

    signal-desktop-bin: 7.85.0 -> 7.86.0

--- pkgs/by-name/si/signal-desktop-bin/signal-desktop.nix
+++ pkgs/by-name/si/signal-desktop-bin/signal-desktop.nix
@@ -1,12 +1,12 @@
 { callPackage, commandLineArgs }:
 callPackage ./generic.nix { inherit commandLineArgs; } rec {
   pname = "signal-desktop-bin";
-  version = "7.85.0";
+  version = "7.86.0";
 
   libdir = "opt/Signal";
   bindir = libdir;
   extractPkg = "dpkg-deb -x $downloadedFile $out";
 
   url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-  hash = "sha256-5DJho9aYNwgasZsSuChEvoDK7N1H+n3RdDh7FMw73SA=";
+  hash = "sha256-OOnaOnrAWAw3EJS31Wzf83ekWiJab0EvY68UTEOTMjA=";
 }
