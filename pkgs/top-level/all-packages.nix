6eaedc4d5b88e52dc9ed1b9e76fd205c14c829ae
quantenzitrone <general@dev.quantenzitrone.eu>
2026-01-22 02:39:53 +0100

    documentation-highlighter: move to pkgs/by-name, move source to separate directory

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9775,8 +9775,6 @@ with pkgs;
   docbook_xsl = docbook-xsl-nons;
   docbook_xsl_ns = docbook-xsl-ns;
 
-  documentation-highlighter = callPackage ../misc/documentation-highlighter { };
-
   moeli = eduli;
 
   font-awesome_4 = (callPackage ../data/fonts/font-awesome { }).v4;
