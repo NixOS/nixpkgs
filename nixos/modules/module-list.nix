0a50d204c89702e5c647fa08a19f70078f93cb44
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2026-01-21 23:06:03 -0300

    nixos/proton-vpn-daemon: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1332,6 +1332,7 @@
   ./services/networking/pptpd.nix
   ./services/networking/privoxy.nix
   ./services/networking/prosody.nix
+  ./services/networking/proton-vpn-daemon.nix
   ./services/networking/pyload.nix
   ./services/networking/quassel.nix
   ./services/networking/quicktun.nix
