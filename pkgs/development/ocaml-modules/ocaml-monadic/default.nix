57f7e974cacf3bb4539bf799e84bf9a9ea054176
Guy Chronister <guychronister@outlook.com>
2026-01-22 12:08:28 -0600

    ocamlPackages.ocaml-monadic: migrate to finalAttrs

--- pkgs/development/ocaml-modules/ocaml-monadic/default.nix
+++ pkgs/development/ocaml-modules/ocaml-monadic/default.nix
@@ -5,7 +5,7 @@
   ppxlib,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "ocaml-monadic";
   version = "0.5.0";
 
@@ -13,17 +13,17 @@ buildDunePackage rec {
 
   src = fetchFromGitHub {
     owner = "zepalmer";
-    repo = pname;
-    rev = version;
+    repo = "ocaml-monadic";
+    rev = finalAttrs.version;
     sha256 = "1ynv3yhdqmkhkgnz6c5kv6ryjcc934sdvw9rhh8rjg2dlzlffgbw";
   };
 
   buildInputs = [ ppxlib ];
 
   meta = {
-    inherit (src.meta) homepage;
+    inherit (finalAttrs.src.meta) homepage;
     description = "PPX extension to provide an OCaml-friendly monadic syntax";
     license = lib.licenses.bsd3;
     maintainers = [ lib.maintainers.vbgl ];
   };
-}
+})
