d770079d9e3770f3ed0a0dd276298cac9038b141
Guy Chronister <guychronister@outlook.com>
2026-01-21 19:29:08 -0600

    ocamlPackages.digestif: migrate to finalAttrs

--- pkgs/development/ocaml-modules/digestif/default.nix
+++ pkgs/development/ocaml-modules/digestif/default.nix
@@ -12,14 +12,14 @@
   fpath,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "digestif";
   version = "1.3.0";
 
   minimalOCamlVersion = "4.08";
 
   src = fetchurl {
-    url = "https://github.com/mirage/digestif/releases/download/v${version}/digestif-${version}.tbz";
+    url = "https://github.com/mirage/digestif/releases/download/v${finalAttrs.version}/digestif-${finalAttrs.version}.tbz";
     hash = "sha256-mmzcszJTnIf0cj/DvXNiayZ1p7EWH98P7TCRhs4Y9Cc=";
   };
 
@@ -44,4 +44,4 @@ buildDunePackage rec {
     license = lib.licenses.mit;
     maintainers = [ lib.maintainers.vbgl ];
   };
-}
+})
