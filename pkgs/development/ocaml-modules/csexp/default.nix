123d67c19d1431019525ea86b4ddec48ab4079ba
Guy Chronister <guychronister@outlook.com>
2026-01-21 19:28:54 -0600

    ocamlPackages.csexp: migrate to finalAttrs

--- pkgs/development/ocaml-modules/csexp/default.nix
+++ pkgs/development/ocaml-modules/csexp/default.nix
@@ -5,12 +5,12 @@
   liquidsoap,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "csexp";
   version = "1.5.2";
 
   src = fetchurl {
-    url = "https://github.com/ocaml-dune/csexp/releases/download/${version}/csexp-${version}.tbz";
+    url = "https://github.com/ocaml-dune/csexp/releases/download/${finalAttrs.version}/csexp-${finalAttrs.version}.tbz";
     hash = "sha256-GhTdBLtDeaQZkCSFUGKMd5E6nAfzw1wTcLaWDml3h/8=";
   };
 
@@ -23,8 +23,8 @@ buildDunePackage rec {
   meta = {
     description = "Minimal support for Canonical S-expressions";
     homepage = "https://github.com/ocaml-dune/csexp";
-    changelog = "https://github.com/ocaml-dune/csexp/raw/${version}/CHANGES.md";
+    changelog = "https://github.com/ocaml-dune/csexp/raw/${finalAttrs.version}/CHANGES.md";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
