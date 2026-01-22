edb5e64a0ab5d29f850131e0b51e6b051eef927c
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-22 11:51:38 +0100

    fixup! python3Packages.audioread: 3.0.1 -> 3.1.0

--- pkgs/development/python-modules/audioread/default.nix
+++ pkgs/development/python-modules/audioread/default.nix
@@ -1,28 +1,41 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
-  flit-core,
+  fetchFromGitHub,
+  poetry-core,
+  standard-aifc,
+  standard-sunau,
+  ffmpeg-headless,
+  pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "audioread";
   version = "3.1.0";
   pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-HEqy8pcnZMiWqKxhrFPiYcjSnwxszWUvhOGPCKTKsZA=";
+  src = fetchFromGitHub {
+    owner = "sampsyo";
+    repo = "audioread";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-QvgwjUGuzeHH69YAdZdImjMT+9t4YxAukbuZKk0lBro=";
   };
 
-  nativeBuildInputs = [ flit-core ];
+  build-system = [ poetry-core ];
 
-  # No tests, need to disable or py3k breaks
-  doCheck = false;
+  dependencies = [
+    standard-aifc
+    standard-sunau
+  ];
+
+  nativeCheckInputs = [
+    ffmpeg-headless
+    pytestCheckHook
+  ];
 
   meta = {
     description = "Cross-platform audio decoding";
     homepage = "https://github.com/sampsyo/audioread";
     license = lib.licenses.mit;
   };
-}
+})
