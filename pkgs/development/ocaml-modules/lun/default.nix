6eb6603ebf1c702bfd394a739dece37f662bf96c
Guy Chronister <guychronister@outlook.com>
2026-01-22 10:14:16 -0600

    ocamlPackages.lun: migrate to finalAttrs

--- pkgs/development/ocaml-modules/lun/default.nix
+++ pkgs/development/ocaml-modules/lun/default.nix
@@ -4,14 +4,14 @@
   fetchurl,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "lun";
   version = "0.0.1";
 
   minimalOCamlVersion = "4.12.0";
 
   src = fetchurl {
-    url = "https://github.com/robur-coop/lun/releases/download/v${version}/lun-${version}.tbz";
+    url = "https://github.com/robur-coop/lun/releases/download/v${finalAttrs.version}/lun-${finalAttrs.version}.tbz";
     hash = "sha256-zKi63/g7Rw/c+xhAEW+Oim8suGzeL0TtKM8my/aSp5M=";
   };
 
@@ -21,4 +21,4 @@ buildDunePackage rec {
     license = lib.licenses.isc;
     maintainers = [ ];
   };
-}
+})
