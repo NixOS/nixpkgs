b6e11be69df57fabbfc13420fada233ea29445c1
Guy Chronister <guychronister@outlook.com>
2026-01-22 10:14:30 -0600

    ocamlPackages.miou: migrate to finalAttrs

--- pkgs/development/ocaml-modules/miou/default.nix
+++ pkgs/development/ocaml-modules/miou/default.nix
@@ -4,22 +4,22 @@
   fetchurl,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "miou";
   version = "0.4.0";
 
   minimalOCamlVersion = "5.0.0";
 
   src = fetchurl {
-    url = "https://github.com/robur-coop/miou/releases/download/v${version}/miou-${version}.tbz";
+    url = "https://github.com/robur-coop/miou/releases/download/v${finalAttrs.version}/miou-${finalAttrs.version}.tbz";
     hash = "sha256-2a5SET2SPyQloTdcWU9KzPYRcXgK8e8hHbu6OP9R2s8=";
   };
 
   meta = {
     description = "Composable concurrency primitives for OCaml";
     homepage = "https://git.robur.coop/robur/miou";
-    changelog = "https://git.robur.coop/robur/miou/src/tag/v${version}/CHANGES.md";
+    changelog = "https://git.robur.coop/robur/miou/src/tag/v${finalAttrs.version}/CHANGES.md";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
