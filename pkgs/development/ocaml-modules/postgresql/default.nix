aa41e9997c6daebb5345ef1ac930d110648c2754
Guy Chronister <guychronister@outlook.com>
2026-01-22 12:08:54 -0600

    ocamlPackages.postgresql: migrate to finalAttrs

--- pkgs/development/ocaml-modules/postgresql/default.nix
+++ pkgs/development/ocaml-modules/postgresql/default.nix
@@ -7,14 +7,14 @@
   libpq,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "postgresql";
   version = "5.2.0";
 
   minimalOCamlVersion = "4.12";
 
   src = fetchurl {
-    url = "https://github.com/mmottl/postgresql-ocaml/releases/download/${version}/postgresql-${version}.tbz";
+    url = "https://github.com/mmottl/postgresql-ocaml/releases/download/${finalAttrs.version}/postgresql-${finalAttrs.version}.tbz";
     hash = "sha256-uU/K7hvQljGnUzClPRdod32tpVAGd/sGqh3NqIygJ4A=";
   };
 
@@ -25,8 +25,8 @@ buildDunePackage rec {
   meta = {
     description = "Bindings to the PostgreSQL library";
     license = lib.licenses.lgpl21Plus;
-    changelog = "https://raw.githubusercontent.com/mmottl/postgresql-ocaml/refs/tags/${version}/CHANGES.md";
+    changelog = "https://raw.githubusercontent.com/mmottl/postgresql-ocaml/refs/tags/${finalAttrs.version}/CHANGES.md";
     maintainers = with lib.maintainers; [ bcc32 ];
     homepage = "https://mmottl.github.io/postgresql-ocaml";
   };
-}
+})
