5cee6e2ab6d4a1fc1d6b81ba630f1525013b42ea
Guy Chronister <guychronister@outlook.com>
2026-01-22 12:08:15 -0600

    ocamlPackages.mldoc: migrate to finalAttrs

--- pkgs/development/ocaml-modules/mldoc/default.nix
+++ pkgs/development/ocaml-modules/mldoc/default.nix
@@ -28,7 +28,7 @@ let
   });
   uri' = uri.override { angstrom = angstrom'; };
 in
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "mldoc";
   version = "1.5.8";
 
@@ -39,7 +39,7 @@ buildDunePackage rec {
   src = fetchFromGitHub {
     owner = "logseq";
     repo = "mldoc";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-7uuNUFMSQEgakTKfpYixp43gnfpQSW++snBzgr0Ni0Y=";
   };
 
@@ -67,4 +67,4 @@ buildDunePackage rec {
     license = lib.licenses.agpl3Only;
     maintainers = [ ];
   };
-}
+})
