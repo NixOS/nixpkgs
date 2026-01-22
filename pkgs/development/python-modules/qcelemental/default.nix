ed2a4997c7d0da66fd4697bfefc0270f9596fd19
Phillip Seeber <phillip.seeber@uni-jena.de>
2026-01-22 13:38:12 +0100

    python3Packages.qcelemental: 0.29.0 -> 0.30.0

--- pkgs/development/python-modules/qcelemental/default.nix
+++ pkgs/development/python-modules/qcelemental/default.nix
@@ -2,8 +2,10 @@
   stdenv,
   buildPythonPackage,
   lib,
+  pythonAtLeast,
   fetchPypi,
   poetry-core,
+  setuptools,
   ipykernel,
   networkx,
   numpy,
@@ -16,15 +18,18 @@
 
 buildPythonPackage rec {
   pname = "qcelemental";
-  version = "0.29.0";
+  version = "0.30.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-v2NO5lLn2V6QbikZiVEyJCM7HXBcJq/qyG5FHzFrPAQ=";
+    hash = "sha256-nIW38ReKgE1FA0r55TOOsAOlvtAV3fIQexTsyqx4r4g=";
   };
 
-  build-system = [ poetry-core ];
+  build-system = [
+    poetry-core
+    setuptools
+  ];
 
   dependencies = [
     numpy
@@ -49,7 +54,7 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "qcelemental" ];
 
   meta = {
-    broken = stdenv.hostPlatform.isDarwin;
+    broken = stdenv.hostPlatform.isDarwin || pythonAtLeast "3.14"; # https://github.com/MolSSI/QCElemental/issues/375
     description = "Periodic table, physical constants and molecule parsing for quantum chemistry";
     homepage = "https://github.com/MolSSI/QCElemental";
     changelog = "https://github.com/MolSSI/QCElemental/blob/v${version}/docs/changelog.rst";
