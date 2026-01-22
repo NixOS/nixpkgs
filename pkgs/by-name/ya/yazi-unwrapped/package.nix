6818e369d970daec9e373c396036d8852ce8ff18
Austin Horstman <khaneliman12@gmail.com>
2026-01-22 10:07:09 -0600

    yazi-unwrapped: 26.1.4 -> 26.1.22
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- pkgs/by-name/ya/yazi-unwrapped/package.nix
+++ pkgs/by-name/ya/yazi-unwrapped/package.nix
@@ -8,17 +8,17 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "yazi";
-  version = "26.1.4";
+  version = "26.1.22";
 
   srcs = builtins.attrValues finalAttrs.passthru.srcs;
 
   sourceRoot = finalAttrs.passthru.srcs.code_src.name;
 
-  cargoHash = "sha256-P1ijZsY1AYosWVyxdmDX1SN6KZsrlUqJ9cNfyVWV6mI=";
+  cargoHash = "sha256-VcoIjPZqLimo44atXh7pnCtF+QaZA7uHtKEE6FgTL2k=";
 
   env.YAZI_GEN_COMPLETIONS = true;
   env.VERGEN_GIT_SHA = "Nixpkgs";
-  env.VERGEN_BUILD_DATE = "2026-1-4";
+  env.VERGEN_BUILD_DATE = "2026-01-22";
 
   nativeBuildInputs = [ installShellFiles ];
   buildInputs = [ rust-jemalloc-sys ];
@@ -36,13 +36,18 @@ rustPlatform.buildRustPackage (finalAttrs: {
     install -Dm444 assets/logo.png $out/share/pixmaps/yazi.png
   '';
 
+  postPatch = ''
+    substituteInPlace yazi-shared/Cargo.toml \
+      --replace-fail 'rust-version = "1.92.0"' 'rust-version = "1.91"'
+  '';
+
   passthru.updateScript.command = [ ./update.sh ];
   passthru.srcs = {
     code_src = fetchFromGitHub {
       owner = "sxyazi";
       repo = "yazi";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-dNDwlMM+k20c2Lnm+hqUvzu5l3Ip9GeP+nnj4WlDVdw=";
+      hash = "sha256-BZktPXn+8vyFyHapvW+9nepFsWRW/XBtdBcnLKrCNCw=";
     };
 
     man_src = fetchFromGitHub {
