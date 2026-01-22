f856593ef6a06872268815674768673300fa193d
Elias Chokeir <112160364+ltpie123@users.noreply.github.com>
2026-01-22 14:17:10 -0800

    Update pkgs/by-name/la/lazymake/package.nix
    Co-authored-by: qzylinra <225773816+qzylinra@users.noreply.github.com>

--- pkgs/by-name/la/lazymake/package.nix
+++ pkgs/by-name/la/lazymake/package.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "rshelekhov";
     repo = "lazymake";
-    rev = "v${version}";
+    tag = "v${version}";
     hash = "sha256-fEAU3ehb4ClsJiKknDcZMik0ux5zyYU+JoAfEpwNUe8=";
   };
 
