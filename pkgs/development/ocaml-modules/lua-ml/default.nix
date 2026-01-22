d7957916cc94fd21e1a4b41f090cd48f4bdf1b58
Guy Chronister <guychronister@outlook.com>
2026-01-22 09:24:10 -0600

    ocamlPackages.lua-ml: migrate to finalAttrs

--- pkgs/development/ocaml-modules/lua-ml/default.nix
+++ pkgs/development/ocaml-modules/lua-ml/default.nix
@@ -3,7 +3,7 @@
   fetchFromGitHub,
   buildDunePackage,
 }:
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "lua-ml";
   version = "0.9.4";
 
@@ -12,14 +12,14 @@ buildDunePackage rec {
   src = fetchFromGitHub {
     owner = "lindig";
     repo = "lua-ml";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-kMBTHzmlrRWNpWwG321jYcM61rE1J3YQkygSrfnZ6Wc=";
   };
 
   meta = {
     description = "Embeddable Lua 2.5 interpreter implemented in OCaml";
-    inherit (src.meta) homepage;
+    inherit ((finalAttrs.src.meta)) homepage;
     license = lib.licenses.bsd2;
     maintainers = [ lib.maintainers.vbgl ];
   };
-}
+})
