7828c54d7dd6dcd8397025a9ff6f60275e600054
Guy Chronister <guychronister@outlook.com>
2026-01-22 10:14:26 -0600

    ocamlPackages.memtrace: migrate to finalAttrs

--- pkgs/development/ocaml-modules/memtrace/default.nix
+++ pkgs/development/ocaml-modules/memtrace/default.nix
@@ -4,23 +4,23 @@
   fetchFromGitHub,
 }:
 
-buildDunePackage rec {
+buildDunePackage (finalAttrs: {
   pname = "memtrace";
   version = "0.2.3";
 
   src = fetchFromGitHub {
     owner = "janestreet";
-    repo = pname;
-    rev = "v${version}";
+    repo = "memtrace";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-dWkTrN8ZgNUz7BW7Aut8mfx8o4n8f6UZaDv/7rbbwNs=";
   };
 
   minimalOCamlVersion = "4.11";
 
   meta = {
-    homepage = "https://github.com/janestreet/${pname}";
+    homepage = "https://github.com/janestreet/memtrace";
     description = "Streaming client for OCaml's Memprof";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ niols ];
   };
-}
+})
