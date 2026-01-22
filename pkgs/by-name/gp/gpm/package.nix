1b123f9fce9d4582fc56fea556916b8a5bd18b22
Doron Behar <doron.behar@gmail.com>
2026-01-22 15:42:55 +0200

    gpm: use lib.withFeature instead of manual if else

--- pkgs/by-name/gp/gpm/package.nix
+++ pkgs/by-name/gp/gpm/package.nix
@@ -63,7 +63,7 @@ stdenv.mkDerivation {
   configureFlags = [
     "--sysconfdir=/etc"
     "--localstatedir=/var"
-    (if withNcurses then "--with-curses" else "--without-curses")
+    (lib.withFeature withNcurses "curses")
     # The code won't compile in c23 mode.
     # https://gcc.gnu.org/gcc-15/porting_to.html#c23-fn-decls-without-parameters
     "CFLAGS=-std=gnu17"
