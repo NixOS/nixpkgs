6fac07465739c3c1e52f3a20dce88a700807f857
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2026-01-21 23:01:28 -0300

    python3Packages.proton-vpn-daemon: add systemd service, dbus config and wireguard-tools

--- pkgs/development/python-modules/proton-vpn-daemon/default.nix
+++ pkgs/development/python-modules/proton-vpn-daemon/default.nix
@@ -3,6 +3,8 @@
   buildPythonPackage,
   fetchFromGitHub,
   bcc,
+  coreutils,
+  python,
   dbus-fast,
   packaging,
   proton-core,
@@ -13,6 +15,7 @@
   pytestCheckHook,
   setuptools,
   systemd-python,
+  wireguard-tools,
 }:
 
 buildPythonPackage rec {
@@ -27,6 +30,11 @@ buildPythonPackage rec {
     hash = "sha256-0CqMGyaHjMktM3N1MfrELprZQZqJD1w/mFaOJvJApZQ=";
   };
 
+  postPatch = ''
+    substituteInPlace proton/vpn/daemon/split_tunneling/apps/socket_monitor.py \
+      --replace-fail "/usr/bin/wg" "${lib.getExe wireguard-tools}"
+  '';
+
   build-system = [
     setuptools
   ];
@@ -39,6 +47,7 @@ buildPythonPackage rec {
     proton-vpn-api-core
     psutil
     systemd-python
+    wireguard-tools
   ];
 
   # Needed for `pythonImportsCheck`, `postBuild` happens between `pythonImportsCheckPhase` and `pytestCheckPhase`.
@@ -46,6 +55,22 @@ buildPythonPackage rec {
     # Needed for Permission denied: '/homeless-shelter'
     export HOME=$(mktemp -d)
     export XDG_RUNTIME_DIR=$(mktemp -d)
+  '';
+
+  postInstall = ''
+    mkdir -p "$out/lib/systemd/system"
+    mkdir -p "$out/share/dbus-1/system-services"
+    mkdir -p "$out/share/dbus-1/system.d"
+
+    install -Dm644 rpmbuild/SOURCES/systemd.service "$out/lib/systemd/system/proton.VPN.service"
+    install -Dm644 rpmbuild/SOURCES/dbus.service "$out/share/dbus-1/system-services/me.proton.vpn.split_tunneling.service"
+    install -Dm644 rpmbuild/SOURCES/dbus.conf "$out/share/dbus-1/system.d/me.proton.vpn.split_tunneling.conf"
+
+    substituteInPlace "$out/lib/systemd/system/proton.VPN.service" \
+      --replace-fail "/usr/bin/python3" "${lib.getExe python}"
+
+    substituteInPlace "$out/share/dbus-1/system-services/me.proton.vpn.split_tunneling.service" \
+      --replace-fail "/bin/false" "${lib.getExe' coreutils "false"}"
   '';
 
   nativeCheckInputs = [
