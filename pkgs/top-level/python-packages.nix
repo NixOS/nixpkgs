d46d7d661dd13b44d7e1e5d3263db5958b38c175
Gaetan Lepage <gaetan@glepage.com>
2026-01-22 23:17:45 +0000

    python3Packages.mflux: init at 0.15.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9606,6 +9606,8 @@ self: super: with self; {
 
   mficlient = callPackage ../development/python-modules/mficlient { };
 
+  mflux = callPackage ../development/python-modules/mflux { };
+
   mfusepy = callPackage ../development/python-modules/mfusepy { };
 
   mhcflurry = callPackage ../development/python-modules/mhcflurry { };
