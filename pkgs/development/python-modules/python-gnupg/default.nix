b9dab5326487e54472240b6eef850d1d0aa8bdcd
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-22 11:26:06 +0100

    fixup! python3Packages.python-gnupg: 0.5.5 -> 0.5.6

--- pkgs/development/python-modules/python-gnupg/default.nix
+++ pkgs/development/python-modules/python-gnupg/default.nix
@@ -1,31 +1,32 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   setuptools,
   gnupg,
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "python-gnupg";
   version = "0.5.6";
-
   pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-V0PpYhLTiSP8GQg4EtwSeQfkTb07zw201lfikdPCHqw=";
+  src = fetchFromGitHub {
+    owner = "vsajip";
+    repo = "python-gnupg";
+    tag = finalAttrs.version;
+    hash = "sha256-ztwITune/rO4c3wUCsw6wBN09jnpWpElgwQx7JCXsVw=";
   };
 
   postPatch = ''
     substituteInPlace gnupg.py \
-      --replace "gpgbinary='gpg'" "gpgbinary='${gnupg}/bin/gpg'"
+      --replace "gpgbinary='gpg'" "gpgbinary='${lib.getExe gnupg}'"
     substituteInPlace test_gnupg.py \
-      --replace "os.environ.get('GPGBINARY', 'gpg')" "os.environ.get('GPGBINARY', '${gnupg}/bin/gpg')"
+      --replace "os.environ.get('GPGBINARY', 'gpg')" "os.environ.get('GPGBINARY', '${lib.getExe gnupg}')"
   '';
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
@@ -39,8 +40,8 @@ buildPythonPackage rec {
   meta = {
     description = "API for the GNU Privacy Guard (GnuPG)";
     homepage = "https://github.com/vsajip/python-gnupg";
-    changelog = "https://github.com/vsajip/python-gnupg/releases/tag/${version}";
+    changelog = "https://github.com/vsajip/python-gnupg/releases/tag/${finalAttrs.version}";
     license = lib.licenses.bsd3;
     maintainers = [ ];
   };
-}
+})
