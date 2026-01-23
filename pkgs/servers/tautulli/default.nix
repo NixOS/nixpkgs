ee21fdd00f28c061661cc1df3026bee82b5741a4
Stefan Frijters <sfrijters@gmail.com>
2026-01-23 14:35:22 +0100

    tautulli: expand pythonPath bash array for structuredAttrs
    (cherry picked from commit 8196cbb3ccf516f318dc08b1273e5e300600e214)

--- pkgs/servers/tautulli/default.nix
+++ pkgs/servers/tautulli/default.nix
@@ -37,7 +37,7 @@ buildPythonApplication rec {
     # Can't just symlink to the main script, since it uses __file__ to
     # import bundled packages and manage the service
     makeWrapper $out/libexec/tautulli/Tautulli.py $out/bin/tautulli
-    wrapPythonProgramsIn "$out/libexec/tautulli" "$pythonPath"
+    wrapPythonProgramsIn "$out/libexec/tautulli" "''${pythonPath[*]}"
 
     # Creat backwards compatibility symlink to bin/plexpy
     ln -s $out/bin/tautulli $out/bin/plexpy
