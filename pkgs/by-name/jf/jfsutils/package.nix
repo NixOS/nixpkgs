3fc100857b58599345b42112de72bf6240a0606a
Doron Behar <doron.behar@gmail.com>
2026-01-23 12:29:15 +0200

    jfsutils: use install command instead of mkdir & cp

--- pkgs/by-name/jf/jfsutils/package.nix
+++ pkgs/by-name/jf/jfsutils/package.nix
@@ -46,10 +46,8 @@ stdenv.mkDerivation rec {
 
   # this required for wipefreespace
   postInstall = ''
-    mkdir -p $out/include
-    cp include/*.h $out/include
-    mkdir -p $out/lib
-    cp ./libfs/libfs.a $out/lib
+    install -Dm644 include/*.h -t $out/include
+    install -Dm644 ./libfs/libfs.a -t $out/lib
   '';
 
   meta = {
