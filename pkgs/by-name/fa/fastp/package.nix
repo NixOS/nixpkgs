b25fc08aa707090629fae9abeb98b7691c4ad906
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-23 01:13:44 +0000

    fastp: 1.0.1 -> 1.1.0

--- pkgs/by-name/fa/fastp/package.nix
+++ pkgs/by-name/fa/fastp/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "fastp";
-  version = "1.0.1";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "OpenGene";
     repo = "fastp";
     rev = "v${version}";
-    sha256 = "sha256-NR41Hklcz2wOQ39OzQYaYs5+eGjSWxCtcTGDAixZCmg=";
+    sha256 = "sha256-0hLq6r/XcYkF1J9LpiQ+qxh5MN4vDTRr5JibnIsq2J0=";
   };
 
   buildInputs = [
