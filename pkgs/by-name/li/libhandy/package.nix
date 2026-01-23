999b30b4307f5dce1be4a87997d8e84de07ac5ec
Doron Behar <doron.behar@gmail.com>
2026-01-23 03:00:33 +0200

    libhandy: rely upon multiple-outputs.sh for putting devdoc files

--- pkgs/by-name/li/libhandy/package.nix
+++ pkgs/by-name/li/libhandy/package.nix
@@ -114,11 +114,6 @@ stdenv.mkDerivation rec {
     runHook postCheck
   '';
 
-  postFixup = ''
-    # Cannot be in postInstall, otherwise _multioutDocs hook in preFixup will move right back.
-    moveToOutput "share/doc" "$devdoc"
-  '';
-
   passthru = {
     updateScript = gnome.updateScript {
       packageName = "libhandy";
