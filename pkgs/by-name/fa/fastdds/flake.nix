fe0b98f162c5299c9a1e4deabe64c948cf0939bd
Miguel Villa Floran <miguel.villafloran@gmail.com>
2026-01-23 13:28:50 -0800

    Formatting

--- pkgs/by-name/fa/fastdds/flake.nix
+++ pkgs/by-name/fa/fastdds/flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
