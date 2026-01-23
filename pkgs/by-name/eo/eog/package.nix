53b5e59af94beb417e859883a26a5034f2f3553e
Doron Behar <doron.behar@gmail.com>
2026-01-23 03:00:24 +0200

    eog: rely upon multiple-outputs.sh for putting devdoc files

--- pkgs/by-name/eo/eog/package.nix
+++ pkgs/by-name/eo/eog/package.nix
@@ -118,11 +118,6 @@ stdenv.mkDerivation rec {
     )
   '';
 
-  postFixup = ''
-    # Cannot be in postInstall, otherwise _multioutDocs hook in preFixup will move right back.
-    moveToOutput "share/doc" "$devdoc"
-  '';
-
   passthru = {
     updateScript = gnome.updateScript {
       packageName = "eog";
