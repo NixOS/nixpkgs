da9496d086c84908c122ab04d7ed9dd2bd5838c0
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-24 11:04:11 +0100

    python313Packages.opower: 0.16.4 -> 0.16.5
    Diff: https://github.com/tronikos/opower/compare/v0.16.4...v0.16.5
    
    Changelog: https://github.com/tronikos/opower/releases/tag/v0.16.5

--- pkgs/development/python-modules/opower/default.nix
+++ pkgs/development/python-modules/opower/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "opower";
-  version = "0.16.4";
+  version = "0.16.5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tronikos";
     repo = "opower";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-r1evfPKvuMXlOvpwqqOSyC80TpZWphYhDVQCi9IiI+8=";
+    hash = "sha256-MeHM6ro6SNz9w4PTCJhc3zZAIkXKujthEJaq9A5zbI8=";
   };
 
   build-system = [ setuptools ];
