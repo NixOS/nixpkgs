4dd6204f3b5546020192716b209f7f0756d1f8ca
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-24 18:46:27 +0000

    tuptime: 5.2.4 -> 5.2.5

--- pkgs/by-name/tu/tuptime/package.nix
+++ pkgs/by-name/tu/tuptime/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "tuptime";
-  version = "5.2.4";
+  version = "5.2.5";
 
   src = fetchFromGitHub {
     owner = "rfmoz";
     repo = "tuptime";
     rev = version;
-    sha256 = "sha256-pYGtgv9hPBG/URD2nOWg6qJYYLtlqLW+t5mCLpzKlEc=";
+    sha256 = "sha256-FlY8zW4AR/uczAHfF1AW/BuOtQgLcPOcl9WGiSFOAYk=";
   };
 
   nativeBuildInputs = [
