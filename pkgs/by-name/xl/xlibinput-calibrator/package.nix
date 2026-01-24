406932f6442756da38e3404c2cd748b3cfa33366
Guy Chronister <guychronister@outlook.com>
2026-01-24 11:10:25 -0600

    xlibinput-calibrator: migrate to finalAttrs

--- pkgs/by-name/xl/xlibinput-calibrator/package.nix
+++ pkgs/by-name/xl/xlibinput-calibrator/package.nix
@@ -8,14 +8,14 @@
   txt2man,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xlibinput-calibrator";
   version = "0.11";
 
   src = fetchFromGitHub {
     owner = "kreijack";
     repo = "xlibinput_calibrator";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     hash = "sha256-MvlamN8WSER0zN9Ru3Kr2MFARD9s7PYKkRtyD8s6ZPI=";
   };
 
@@ -37,8 +37,8 @@ stdenv.mkDerivation rec {
     description = "Touch calibrator for libinput";
     mainProgram = "xlibinput_calibrator";
     homepage = "https://github.com/kreijack/xlibinput_calibrator";
-    changelog = "https://github.com/kreijack/xlibinput_calibrator/blob/${src.rev}/Changelog";
+    changelog = "https://github.com/kreijack/xlibinput_calibrator/blob/${finalAttrs.src.rev}/Changelog";
     license = with lib.licenses; [ mit ];
     maintainers = with lib.maintainers; [ atemu ];
   };
-}
+})
