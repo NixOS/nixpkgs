227c9261ae1973d0d2913f975b3269ac7a2ad2b8
Eric Rodrigues Pires <eric@eric.dev.br>
2026-01-24 10:31:15 -0300

    maintainers: add matrix handle for EpicEric

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7883,6 +7883,7 @@
     email = "eric@eric.dev.br";
     github = "EpicEric";
     githubId = 3129194;
+    matrix = "@epiceric:nixos.dev";
     name = "Eric Rodrigues Pires";
   };
   epireyn = {
