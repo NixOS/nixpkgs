5374db1660a46594c05964d4f623afde227fbf0a
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-24 17:27:50 +0000

    copier: 9.11.2 -> 9.11.3

--- pkgs/development/python-modules/copier/default.nix
+++ pkgs/development/python-modules/copier/default.nix
@@ -28,7 +28,7 @@
 
 buildPythonPackage rec {
   pname = "copier";
-  version = "9.11.2";
+  version = "9.11.3";
   pyproject = true;
 
   src = fetchFromGitHub {
@@ -39,7 +39,7 @@ buildPythonPackage rec {
     postFetch = ''
       rm $out/tests/demo/doc/ma*ana.txt
     '';
-    hash = "sha256-+cbIohIfCfJRBlRmFyI9dzyZ7EcEThDNxvbxY3rlJFw=";
+    hash = "sha256-/7Bl01kuZVqVhXPnIwNL3mwt8DOPK02Qd/85J1zX4q4=";
   };
 
   POETRY_DYNAMIC_VERSIONING_BYPASS = version;
