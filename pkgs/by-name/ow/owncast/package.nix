94f0a9c2c39ba018c12931fd9b4d3eb2078f8456
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-01-24 04:53:23 +0100

    owncast: 0.2.3 -> 0.2.4
    Hardware acceleration is available in normal ffmpeg

--- pkgs/by-name/ow/owncast/package.nix
+++ pkgs/by-name/ow/owncast/package.nix
@@ -5,32 +5,32 @@
   nixosTests,
   bash,
   which,
-  ffmpeg-full,
+  ffmpeg,
   makeBinaryWrapper,
 }:
 let
-  version = "0.2.3";
+  version = "0.2.4";
 in
 buildGoModule {
   pname = "owncast";
   inherit version;
+
   src = fetchFromGitHub {
     owner = "owncast";
     repo = "owncast";
-    rev = "v${version}";
-    hash = "sha256-JCIB4G3cOSkEEO/jcsj4mUP+HeQfgn0jX4OL8NX9/C0=";
+    tag = "v${version}";
+    hash = "sha256-euqmAsGLh7enMbRKeGS7pB3L+12uAHFM2mqahst/bww=";
   };
-  vendorHash = "sha256-FuynEBoPS0p1bRgmaeCxn1RPqbYHcltZpQ9SE71xHEE=";
 
-  propagatedBuildInputs = [ ffmpeg-full ];
+  patches = [
+    ./fix-go.sum.diff
+  ];
+
+  vendorHash = "sha256-XQXv1XeedHQozB56+boi32jsXQoCtD2XIg3deDvXIfw=";
+
+  propagatedBuildInputs = [ ffmpeg ];
 
   nativeBuildInputs = [ makeBinaryWrapper ];
-
-  # lefthook is included as a tool in go.mod for a pre-commit hook, but causes the build to fail
-  preBuild = ''
-    # Remove lefthook from tools section in go.mod
-    sed -i '/tool (/,/)/{ /[[:space:]]*github.com\/evilmartians\/lefthook[[:space:]]*$/d; }' go.mod
-  '';
 
   postInstall = ''
     wrapProgram $out/bin/owncast \
@@ -38,7 +38,7 @@ buildGoModule {
         lib.makeBinPath [
           bash
           which
-          ffmpeg-full
+          ffmpeg
         ]
       }
   '';
