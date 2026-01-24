5f0ae37dd87efbb0db4a08f849414bde72cf5447
Alice ✨🌙 Luna <alice@liquidnya.dev>
2026-01-24 22:02:16 +0100

    maintainers: add liquidnya

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15096,6 +15096,14 @@
     githubId = 591860;
     name = "Lionello Lunesu";
   };
+  liquidnya = {
+    name = "Alice ✨🌙 Luna";
+    github = "liquidnya";
+    githubId = 7364785;
+    email = "alice@liquidnya.dev";
+    matrix = "@liquidnya:matrix.org";
+    keys = [ { fingerprint = "7C2D E075 FA93 D61C C9A7  876F 7966 C19E 32F9 EF06"; } ];
+  };
   litchipi = {
     email = "litchi.pi@proton.me";
     github = "litchipi";
