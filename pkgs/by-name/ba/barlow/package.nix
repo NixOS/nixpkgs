d263ce5aad9362cbe5b180dd2b00dab8012ac88a
pancaek <20342389+pancaek@users.noreply.github.com>
2026-01-24 14:21:44 -0800

    barlow: use mkFont

--- pkgs/by-name/ba/barlow/package.nix
+++ pkgs/by-name/ba/barlow/package.nix
@@ -1,31 +1,21 @@
 {
   lib,
-  stdenvNoCC,
+  mkFont,
   fetchFromGitHub,
 }:
 
-stdenvNoCC.mkDerivation rec {
+mkFont (finalAttrs: {
   pname = "barlow";
   version = "1.422";
 
   src = fetchFromGitHub {
     owner = "jpt";
     repo = "barlow";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-FG68o6qN/296RhSNDHFXYXbkhlXSZJgGhVjzlJqsksY=";
   };
 
-  installPhase = ''
-    runHook preInstall
-
-    install -Dm644 fonts/otf/*.otf -t $out/share/fonts/opentype
-    install -Dm644 fonts/ttf/*.ttf fonts/gx/*.ttf -t $out/share/fonts/truetype
-    install -Dm644 fonts/eot/*.eot -t $out/share/fonts/eot
-    install -Dm644 fonts/woff/*.woff -t $out/share/fonts/woff
-    install -Dm644 fonts/woff2/*.woff2 -t $out/share/fonts/woff2
-
-    runHook postInstall
-  '';
+  installWebfonts = true;
 
   meta = {
     description = "Grotesk variable font superfamily";
@@ -34,4 +24,4 @@ stdenvNoCC.mkDerivation rec {
     maintainers = [ ];
     platforms = lib.platforms.all;
   };
-}
+})
