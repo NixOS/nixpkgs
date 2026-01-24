5afee07a408337f67e18750060d7295bcfd9dc81
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-24 22:17:53 +0100

    Revert "python3Packages.wrapt: 1.17.2 -> 2.0.1"
    This reverts commit 6c29346299dd3d91e39f0615fcf7bd02c9620949.
    
    The opentelemetry packages aren't read for 2.x.

--- pkgs/development/python-modules/wrapt/default.nix
+++ pkgs/development/python-modules/wrapt/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "wrapt";
-  version = "2.0.1";
+  version = "1.17.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "GrahamDumpleton";
     repo = "wrapt";
     tag = version;
-    hash = "sha256-AJGxQPWB7Yj37YysfaXNrWRbuLh1PUY+siwS4q5oSDw=";
+    hash = "sha256-QduT5bncXi4LeI034h5Pqtwybru0QcQIYI7cMchLy7c=";
   };
 
   build-system = [ setuptools ];
@@ -33,9 +33,6 @@ buildPythonPackage rec {
   ];
 
   nativeCheckInputs = [ pytestCheckHook ];
-
-  # not compatible with mypy 1.19.x
-  disabledTestPaths = [ "tests/conftest.py" ];
 
   pythonImportsCheck = [ "wrapt" ];
 
