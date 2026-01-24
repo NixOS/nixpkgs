bbb37ad7afbf23281ef93243a81dde38bf5f1de2
Dan Lock <codenil@proton.me>
2026-01-24 14:26:13 +0000

    nixos/rqbit: init service module
    (cherry picked from commit 084807da0fbd386a82c4d49c0e23129557bb5a0b)

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1363,6 +1363,7 @@ in
   rosenpass = runTest ./rosenpass.nix;
   roundcube = runTest ./roundcube.nix;
   routinator = handleTest ./routinator.nix { };
+  rqbit = handleTest ./rqbit.nix { };
   rshim = handleTest ./rshim.nix { };
   rspamd = handleTest ./rspamd.nix { };
   rspamd-trainer = runTest ./rspamd-trainer.nix;
