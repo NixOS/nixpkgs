be0bf5252e95e7a5c162a996b0827fbec824694c
wrvsrx <wrvsrx@outlook.com>
2026-01-24 16:21:34 +0800

    noctalia-shell: 4.1.1 -> 4.2.5

--- pkgs/by-name/no/noctalia-shell/package.nix
+++ pkgs/by-name/no/noctalia-shell/package.nix
@@ -13,15 +13,14 @@
   cava,
   cliphist,
   ddcutil,
-  matugen,
   wlsunset,
   wl-clipboard,
   imagemagick,
   wget,
   gpu-screen-recorder,
+  python3,
 
   # calendar support
-  python3,
   evolution-data-server,
   libical,
   glib,
@@ -33,27 +32,26 @@
   cavaSupport ? true,
   cliphistSupport ? true,
   ddcutilSupport ? true,
-  matugenSupport ? true,
   wlsunsetSupport ? true,
   wl-clipboardSupport ? true,
   imagemagickSupport ? true,
-  gpuScreenRecorderSupport ? stdenvNoCC.hostPlatform.system == "x86_64-linux",
   calendarSupport ? false,
+  # gpu-screen-recorder support was moved to an optional plugin in v4.0.0
+  gpuScreenRecorderSupport ? false,
 }:
 let
   runtimeDeps = [
     wget
+    (python3.withPackages (pp: lib.optional calendarSupport pp.pygobject3))
   ]
   ++ lib.optional brightnessctlSupport brightnessctl
   ++ lib.optional cavaSupport cava
   ++ lib.optional cliphistSupport cliphist
   ++ lib.optional ddcutilSupport ddcutil
-  ++ lib.optional matugenSupport matugen
   ++ lib.optional wlsunsetSupport wlsunset
   ++ lib.optional wl-clipboardSupport wl-clipboard
   ++ lib.optional imagemagickSupport imagemagick
-  ++ lib.optional gpuScreenRecorderSupport gpu-screen-recorder
-  ++ lib.optional calendarSupport (python3.withPackages (pp: [ pp.pygobject3 ]));
+  ++ lib.optional gpuScreenRecorderSupport gpu-screen-recorder;
 
   giTypelibPath = lib.makeSearchPath "lib/girepository-1.0" [
     evolution-data-server
@@ -66,13 +64,13 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "noctalia-shell";
-  version = "4.1.1";
+  version = "4.2.5";
 
   src = fetchFromGitHub {
     owner = "noctalia-dev";
     repo = "noctalia-shell";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-/7yEiXC2Z/Yk/p7aNgChkAa7BgPRlV5/0z8+jZXH8e8=";
+    hash = "sha256-SHavMqGRv78sND/wQ53OhBBE2VBhgE3bSNRxxo5z7FE=";
   };
 
   nativeBuildInputs = [
@@ -91,11 +89,10 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     ln -s ${quickshell}/bin/qs $out/bin/noctalia-shell
 
     cp -R \
-      Assets Bin Commons CREDITS.md Helpers Modules Services Shaders Widgets shell.qml \
+      Assets Commons CREDITS.md Helpers Modules Services Shaders Scripts Widgets shell.qml \
       $out/share/noctalia-shell
 
     rm -R $out/share/noctalia-shell/Assets/Screenshots
-    rm -R $out/share/noctalia-shell/Bin/dev
 
     runHook postInstall
   '';
