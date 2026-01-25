0cf8c665a6b8267f62514e9d30cf4cd193ca20a2
Hythera <87016780+Hythera@users.noreply.github.com>
2026-01-25 12:36:53 +0100

    mago: 1.1.0 -> 1.13.3

--- pkgs/by-name/ma/mago/package.nix
+++ pkgs/by-name/ma/mago/package.nix
@@ -9,17 +9,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "mago";
-  version = "1.1.0";
+  version = "1.13.3";
 
   src = fetchFromGitHub {
     owner = "carthage-software";
     repo = "mago";
     tag = finalAttrs.version;
-    hash = "sha256-27+hUA7FNgkpzn9zIH78tuCGT/k3RC2x+Yiuoj/ez6Q=";
+    hash = "sha256-t1KowYGQgrsVroPUpUq8dZYPwVhGVImnzmbnUOlzPAY=";
     forceFetchGit = true; # Does not download all files otherwise
   };
 
-  cargoHash = "sha256-IL5/OG23/53DUNbFWkx5gul99uAzVtPDyvodJds0Tao=";
+  cargoHash = "sha256-UIz+q9u8gKXP+ewp8uXew5/cAMWOr3VGWWLjV/fip9M=";
 
   env = {
     # Get openssl-sys to use pkg-config
