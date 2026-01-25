1436a81f4fe68a4ce933155016cc07266c69aa19
Anthony Roussel <anthony@roussel.dev>
2026-01-25 14:35:48 +0100

    python3Packages.pyeclib: 1.6.4 -> 1.7.0
    Diff: https://github.com/openstack/pyeclib/compare/1.6.4...1.7.0

--- pkgs/development/python-modules/pyeclib/default.nix
+++ pkgs/development/python-modules/pyeclib/default.nix
@@ -1,53 +1,32 @@
 {
   lib,
-  stdenv,
   buildPythonPackage,
-  distutils,
   fetchFromGitHub,
   liberasurecode,
   pytestCheckHook,
   setuptools,
-  six,
 }:
 
 buildPythonPackage rec {
   pname = "pyeclib";
-  version = "1.6.4";
+  version = "1.7.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "openstack";
     repo = "pyeclib";
     tag = version;
-    hash = "sha256-oRitXlQunfqLSKMaSW3E1BnL0otA4UPj/y6bbiN0kPM=";
+    hash = "sha256-wYzZUtr80KgVTznD0ISy7qhGngm4Xmt8Mauu9lP+2T4=";
   };
 
-  postPatch = ''
-    # python's platform.platform() doesn't return "Darwin" (anymore?)
-    substituteInPlace setup.py \
-      --replace-fail '"Darwin"' '"macOS"'
-  '';
-
   build-system = [
-    distutils
     setuptools
   ];
 
-  preBuild =
-    let
-      ldLibraryPathEnvName =
-        if stdenv.hostPlatform.isDarwin then "DYLD_LIBRARY_PATH" else "LD_LIBRARY_PATH";
-    in
-    ''
-      # required for the custom _find_library function in setup.py
-      export ${ldLibraryPathEnvName}="${lib.makeLibraryPath [ liberasurecode ]}"
-    '';
-
-  dependencies = [ liberasurecode ];
+  buildInputs = [ liberasurecode ];
 
   nativeCheckInputs = [
     pytestCheckHook
-    six
   ];
 
   disabledTests = [
@@ -61,6 +40,7 @@ buildPythonPackage rec {
     description = "This library provides a simple Python interface for implementing erasure codes";
     homepage = "https://github.com/openstack/pyeclib";
     license = lib.licenses.bsd2;
+    mainProgram = "pyeclib-backend";
     teams = [ lib.teams.openstack ];
   };
 }
