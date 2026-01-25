d44d32a9642b4aad746b77cc99f70e3f4dd821a8
Robert Schütz <nix@dotlambda.de>
2026-01-24 16:49:41 -0800

    Revert "python3Packages.securetar: 2025.2.1 -> 2025.12.0"
    This reverts commit 211a4047a9e04a42bfc20e3a1dc24085156351d4.

--- pkgs/development/python-modules/securetar/default.nix
+++ pkgs/development/python-modules/securetar/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "securetar";
-  version = "2025.12.0";
+  version = "2025.2.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pvizeli";
     repo = "securetar";
     tag = version;
-    hash = "sha256-wCm4WBuzkqYWDGoyDyML1tdUyOdT2hy/qFk2DU4h/tQ=";
+    hash = "sha256-uVzyVgS8bWxS7jhwVyv7wTNF8REW+dJIhkRaS/8/FmY=";
   };
 
   build-system = [ setuptools ];
