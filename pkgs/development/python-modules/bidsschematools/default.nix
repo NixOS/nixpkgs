014b85596801eeea8bf07dd9e727902eb52b3ff9
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-01-25 19:12:18 +0100

    python3Packages.bidsschematools: 1.1.5 -> 1.1.6

--- pkgs/development/python-modules/bidsschematools/default.nix
+++ pkgs/development/python-modules/bidsschematools/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage rec {
   pname = "bidsschematools";
-  version = "1.1.5";
+  version = "1.1.6";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bids-standard";
     repo = "bids-specification";
     tag = "schema-${version}";
-    hash = "sha256-LAcrmnfksisLQb1JE82p/tm5HhHAfezCApz8CeUciZQ=";
+    hash = "sha256-HMGhEEnmr0BwkcRcysmu9SgTME4BhrwcAAnRt4qF7eI=";
   };
 
   sourceRoot = "${src.name}/tools/schemacode";
