dd4ea5992196d61ee1338786b89a6bb2e93c280a
cjshearer <cjshearer@live.com>
2026-01-25 15:56:58 -0500

    python3Packages.trame-vuetify: init at 3.2.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20967,6 +20967,8 @@ self: super: with self; {
 
   trame-server = callPackage ../development/python-modules/trame-server { };
 
+  trame-vuetify = callPackage ../development/python-modules/trame-vuetify { };
+
   trampoline = callPackage ../development/python-modules/trampoline { };
 
   transaction = callPackage ../development/python-modules/transaction { };
