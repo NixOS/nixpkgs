cf7c226dfd0fedc84f2672a64584ea68c1938c60
7c6f434c <7c6f434c@mail.ru>
2026-01-25 19:38:20 +0100

    bsh: 2.0b5 -> 2.1.1
    Switch download location, homepage refers to the new one on GitHub

--- pkgs/by-name/bs/bsh/package.nix
+++ pkgs/by-name/bs/bsh/package.nix
@@ -3,7 +3,7 @@
 fetchurl (finalAttrs: {
   name = "${finalAttrs.pname}-${finalAttrs.version}.jar";
   pname = "bsh";
-  version = "2.0b5";
-  url = "http://www.beanshell.org/bsh-${finalAttrs.version}.jar";
-  hash = "sha256-YjIZlWOAc1SzvLWs6z3BNlAvAixrDvdDmHqD9m/uWlw=";
+  version = "2.1.1";
+  url = "https://github.com/beanshell/beanshell/releases/download/${finalAttrs.version}/bsh-${finalAttrs.version}.jar";
+  hash = "sha256-cRksu+Seeiac/LoF3Fy5WcM7myba/NYmbKMoi0YfhqM=";
 })
