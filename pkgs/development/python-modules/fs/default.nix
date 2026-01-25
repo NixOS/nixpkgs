05fde8ef8143f7123e7d110ec96e258ad86cc0fc
Robert Schütz <nix@dotlambda.de>
2026-01-24 17:00:19 -0800

    python3Packages.fs: disable tests relying on the removed pyftpdlib.test

--- pkgs/development/python-modules/fs/default.nix
+++ pkgs/development/python-modules/fs/default.nix
@@ -65,6 +65,9 @@ buildPythonPackage rec {
     "tests/test_move.py"
     "tests/test_mirror.py"
     "tests/test_copy.py"
+    # pyftpdlib removed tests from installation in 2.1.0, resulting in
+    #     ModuleNotFoundError: No module named 'pyftpdlib.test'
+    "tests/test_ftpfs.py"
   ];
 
   disabledTests = [
