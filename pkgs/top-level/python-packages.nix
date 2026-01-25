2a558b0ea00ad76fa5bdc1f52f1cd394d6dfe3d0
cjshearer <cjshearer@live.com>
2026-01-25 15:47:05 -0500

    python3Packages.trame-client: init at 3.13.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20957,6 +20957,8 @@ self: super: with self; {
 
   trakit = callPackage ../development/python-modules/trakit { };
 
+  trame-client = callPackage ../development/python-modules/trame-client { };
+
   trame-common = callPackage ../development/python-modules/trame-common { };
 
   trampoline = callPackage ../development/python-modules/trampoline { };
