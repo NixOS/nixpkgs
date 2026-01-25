e04a480f1173298527382e62e9101fd674bd110e
Tom van Dijk <18gatenmaker6@gmail.com>
2026-01-25 15:32:36 +0100

    vintagestory: add update script
    Co-authored-by: Edward Wawrzynski <edward.wawrzynski@protonmail.com>

--- pkgs/by-name/vi/vintagestory/package.nix
+++ pkgs/by-name/vi/vintagestory/package.nix
@@ -120,6 +120,8 @@ stdenv.mkDerivation (finalAttrs: {
       done
     '';
 
+  passthru.updateScript = ./update.sh;
+
   meta = {
     description = "In-development indie sandbox game about innovation and exploration";
     homepage = "https://www.vintagestory.at/";
