2d2fb3351b98d1fefc325f80ed66db4980247585
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-25 01:48:45 +0000

    turso: 0.4.0 -> 0.4.4-pre.3

--- pkgs/by-name/tu/turso/package.nix
+++ pkgs/by-name/tu/turso/package.nix
@@ -7,16 +7,16 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "turso";
-  version = "0.4.0";
+  version = "0.4.4-pre.3";
 
   src = fetchFromGitHub {
     owner = "tursodatabase";
     repo = "turso";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-94ow9LUouifdE3ml2iyo72Y0nQSSLfnU5qnNHiJHqKw=";
+    hash = "sha256-VT5ErH0h3eotOlEeyoARy+KD9iiDOVhfXSy8sVT8uDk=";
   };
 
-  cargoHash = "sha256-c6PIrbRclU/l7wuhOIANBgCnRu2Y/UoUeZA/eg13KoA=";
+  cargoHash = "sha256-82PzmCikp9Hkn8px/Wml1SbEJrQUeGJ5sHYy50nLFQg=";
 
   cargoBuildFlags = [
     "-p"
