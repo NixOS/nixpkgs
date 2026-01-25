a66a742c782be798f9efa9b71c756fc6395b4cb9
cjshearer <cjshearer@live.com>
2026-01-25 15:58:47 -0500

    python3Packages.trame-vtk: init at 2.11.15

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20967,6 +20967,8 @@ self: super: with self; {
 
   trame-server = callPackage ../development/python-modules/trame-server { };
 
+  trame-vtk = callPackage ../development/python-modules/trame-vtk { };
+
   trame-vuetify = callPackage ../development/python-modules/trame-vuetify { };
 
   trampoline = callPackage ../development/python-modules/trampoline { };
