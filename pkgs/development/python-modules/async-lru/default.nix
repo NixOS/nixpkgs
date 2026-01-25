bf7741ef55f563394f63e42ad4cbef98f00da94f
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-25 01:03:25 +0100

    fixup! python3Packages.async-lru: 2.0.5 -> 2.1.0

--- pkgs/development/python-modules/async-lru/default.nix
+++ pkgs/development/python-modules/async-lru/default.nix
@@ -3,7 +3,6 @@
   buildPythonPackage,
   pythonOlder,
   fetchFromGitHub,
-  fetchpatch2,
   setuptools,
   typing-extensions,
   pytestCheckHook,
@@ -23,15 +22,6 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-ab0l3JrjMPudfMsn0Tu2UpvSt8HePEl8tYF2EybmXak=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      # https://github.com/aio-libs/async-lru/issues/635
-      name = "python314-compatibility.patch";
-      url = "https://github.com/aio-libs/async-lru/commit/4df3785d3e5210ce6277b3137c4625cd73918088.patch";
-      hash = "sha256-B9KCJPbiZTQJrnxC/7VI+jgr2PKfwOmS7naXZwKtF9c=";
-    })
-  ];
 
   build-system = [ setuptools ];
 
