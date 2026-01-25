7f35e0166f5979e44fd2f176813878807675b16a
Marcin Serwin <marcin@serwin.dev>
2026-01-25 12:23:15 +0100

    rott{,-shareware}: drop
    It fails to build with C23 because `true` and `false` are now keywords.
    It's easy to fix but there is little reason to prefer this version over
    the maintained fork 'taradino' which is already packaged.
    
    Related to: https://github.com/NixOS/nixpkgs/issues/475479
    
    Signed-off-by: Marcin Serwin <marcin@serwin.dev>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12375,10 +12375,6 @@ with pkgs;
     quake3hires
     ;
 
-  rott-shareware = callPackage ../by-name/ro/rott/package.nix {
-    buildShareware = true;
-  };
-
   inherit (callPackage ../by-name/sc/scummvm/games.nix { })
     beneath-a-steel-sky
     broken-sword-25
