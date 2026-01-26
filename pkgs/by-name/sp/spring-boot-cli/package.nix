1ebfd65a1da24cf12fc62de98d234bbc920b6f04
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-26 02:24:12 +0000

    spring-boot-cli: 4.0.1 -> 4.0.2

--- pkgs/by-name/sp/spring-boot-cli/package.nix
+++ pkgs/by-name/sp/spring-boot-cli/package.nix
@@ -12,11 +12,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "spring-boot-cli";
-  version = "4.0.1";
+  version = "4.0.2";
 
   src = fetchzip {
     url = "mirror://maven/org/springframework/boot/spring-boot-cli/${finalAttrs.version}/spring-boot-cli-${finalAttrs.version}-bin.zip";
-    hash = "sha256-cXpk6WbjAHv8DfEdYpdNPNf01UPBojmtLFSUKBkWj44=";
+    hash = "sha256-xC1K6Ab5UeJL0OkYBSZVPMj2p/r9zsxont7Nu0EkvVU=";
   };
 
   nativeBuildInputs = [
