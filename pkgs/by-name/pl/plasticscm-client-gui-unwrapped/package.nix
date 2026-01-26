af0a7fa878d8985cd1b34bca7f1250aeaedfe82e
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-26 21:01:41 +0000

    plasticscm-client-gui-unwrapped: 11.0.16.9872 -> 11.0.16.9890

--- pkgs/by-name/pl/plasticscm-client-gui-unwrapped/package.nix
+++ pkgs/by-name/pl/plasticscm-client-gui-unwrapped/package.nix
@@ -11,11 +11,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "plasticscm-client-gui-unwrapped";
-  version = "11.0.16.9872";
+  version = "11.0.16.9890";
 
   src = fetchurl {
     url = "https://www.plasticscm.com/plasticrepo/stable/debian/amd64/plasticscm-client-gui_${finalAttrs.version}_amd64.deb";
-    hash = "sha256-gLu0r7eo7XrYwquZurduj1NiKIB7EnRky5vYKW9fks4=";
+    hash = "sha256-zaknvpMzMevz/xvacd5QUacG8Kw8RPss5K23+M4M2bk=";
     nativeBuildInputs = [ dpkg ];
     downloadToTemp = true;
     recursiveHash = true;
