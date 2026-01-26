11e9dc214c612b0ba68681fc6fbe926136c9a5c0
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-26 11:42:10 +0100

    python313Packages.mcp: migrate to finalAttrs

--- pkgs/development/python-modules/mcp/default.nix
+++ pkgs/development/python-modules/mcp/default.nix
@@ -40,7 +40,7 @@
   requests,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "mcp";
   version = "1.26.0";
   pyproject = true;
@@ -48,7 +48,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "modelcontextprotocol";
     repo = "python-sdk";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-TGkAyuBcIstL2BCZYBWoi7PhnhoBvap67sLWGe0QUoU=";
   };
 
@@ -106,7 +106,7 @@ buildPythonPackage rec {
     pytestCheckHook
     requests
   ]
-  ++ lib.concatAttrValues optional-dependencies;
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
 
   disabledTests = [
     # attempts to run the package manager uv
@@ -152,7 +152,7 @@ buildPythonPackage rec {
   __darwinAllowLocalNetworking = true;
 
   meta = {
-    changelog = "https://github.com/modelcontextprotocol/python-sdk/releases/tag/${src.tag}";
+    changelog = "https://github.com/modelcontextprotocol/python-sdk/releases/tag/${finalAttrs.src.tag}";
     description = "Official Python SDK for Model Context Protocol servers and clients";
     homepage = "https://github.com/modelcontextprotocol/python-sdk";
     license = lib.licenses.mit;
@@ -161,4 +161,4 @@ buildPythonPackage rec {
       josh
     ];
   };
-}
+})
