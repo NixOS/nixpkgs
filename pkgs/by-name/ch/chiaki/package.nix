dbda37df182fc87ea26793787c0c383830a4c0d3
Guy Chronister <guychronister@outlook.com>
2026-01-26 16:36:47 +0000

    chiaki: 2.2.0 -> 2.2.0-unstable-2026-01-05

--- pkgs/by-name/ch/chiaki/package.nix
+++ pkgs/by-name/ch/chiaki/package.nix
@@ -13,15 +13,15 @@
   libsForQt5,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
+stdenv.mkDerivation {
   pname = "chiaki";
-  version = "2.2.0";
+  version = "2.2.0-unstable-2026-01-05";
 
   src = fetchgit {
     url = "https://git.sr.ht/~thestr4ng3r/chiaki";
-    tag = "v${finalAttrs.version}";
+    rev = "ab55cf4c95f7723faae08a546fbe14e0f6bddf45";
     fetchSubmodules = true;
-    hash = "sha256-mLx2ygMlIuDJt9iT4nIj/dcLGjMvvmneKd49L7C3BQk=";
+    hash = "sha256-GYhbq3QCyykMu5K1ITJ+XqNU593Gn3eBTM9coKmh/Vk=";
   };
 
   nativeBuildInputs = [
@@ -29,12 +29,6 @@ stdenv.mkDerivation (finalAttrs: {
     pkg-config
     libsForQt5.wrapQtAppsHook
   ];
-
-  postPatch = ''
-    substituteInPlace CMakeLists.txt --replace-fail \
-    'cmake_minimum_required(VERSION 3.2)' \
-    'cmake_minimum_required(VERSION 3.5)'
-  '';
 
   buildInputs = [
     ffmpeg_7 # needs avcodec_close which was removed in ffmpeg 8
@@ -65,4 +59,4 @@ stdenv.mkDerivation (finalAttrs: {
     platforms = lib.platforms.all;
     mainProgram = "chiaki";
   };
-})
+}
