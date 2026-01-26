3f6652af686c867dbc56c962d8fad0893716e755
faukah <fau@faukah.com>
2026-01-26 12:56:27 +0100

    livekit-cli: modernize

--- pkgs/by-name/li/livekit-cli/package.nix
+++ pkgs/by-name/li/livekit-cli/package.nix
@@ -2,22 +2,27 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
+  nix-update-script,
+  testers,
 }:
 
-buildGoModule rec {
+buildGoModule (finalAttrs: {
   pname = "livekit-cli";
   version = "2.13.1";
 
   src = fetchFromGitHub {
     owner = "livekit";
     repo = "livekit-cli";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-ZxMqZIkJzI/FeuCbBr9H+2bDNmPKjNEW8LFcuaO3Lfs=";
   };
 
   vendorHash = "sha256-CejPt5+dJEBEuBbliRkaHEXzZlOsddSXQaJ87CJxGyU=";
 
   subPackages = [ "cmd/lk" ];
+
+  passthru.updateScript = nix-update-script { };
+  passthru.tests.version = testers.testVersion { package = finalAttrs.finalPackage; };
 
   meta = {
     description = "Command line interface to LiveKit";
@@ -26,4 +31,4 @@ buildGoModule rec {
     maintainers = with lib.maintainers; [ mgdelacroix ];
     mainProgram = "lk";
   };
-}
+})
