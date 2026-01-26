25d300e9f8f92e935c6f85f51fb5e4249a983432
Marin <git@mar.in>
2026-01-26 13:25:03 -0700

    age-plugin-tpm: remove swtpm from nativeCheckInputs
    swtpm shouldn't be needed after age-plugin-tpm v1.0.0.

--- pkgs/by-name/ag/age-plugin-tpm/package.nix
+++ pkgs/by-name/ag/age-plugin-tpm/package.nix
@@ -4,7 +4,6 @@
   buildGoModule,
   fetchFromGitHub,
   nixosTests,
-  swtpm,
   openssl,
   age,
   versionCheckHook,
@@ -27,7 +26,6 @@ buildGoModule rec {
 
   nativeCheckInputs = [
     age
-    swtpm
   ];
   nativeInstallCheckInputs = [ versionCheckHook ];
 
