b1b7ca45e2a7f531dd7007491de4fa0daa91232e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-01-26 02:21:41 +0100

    Revert "python3Packages.cython-test-exception-raiser: 1.0.2 -> 25.11.1"
    This reverts commit 0c46062c6f65b2bdc68fa96bd7a3f7e181a75976.

--- pkgs/development/python-modules/cython-test-exception-raiser/default.nix
+++ pkgs/development/python-modules/cython-test-exception-raiser/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "cython-test-exception-raiser";
-  version = "25.11.1";
+  version = "1.0.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "twisted";
     repo = "cython-test-exception-raiser";
-    tag = version;
-    hash = "sha256-Od5LeytibiNZSDTI2KxK0SrDNvlQrKyL+5ea15Udi74=";
+    rev = "v${version}";
+    hash = "sha256-fwMq0pOrFUJnPndH/a6ghoo6mlcVSxtsWazqE9mCx3M=";
   };
 
   nativeBuildInputs = [
@@ -30,7 +30,7 @@ buildPythonPackage rec {
   meta = {
     description = "Testing only. A cython simple extension which is used as helper for twisted/twisted Failure tests";
     homepage = "https://github.com/twisted/cython-test-exception-raiser";
-    changelog = "https://github.com/twisted/cython-test-exception-raiser/blob/${src.tag}/CHANGELOG.rst";
+    changelog = "https://github.com/twisted/cython-test-exception-raiser/blob/${src.rev}/CHANGELOG.rst";
     license = with lib.licenses; [
       publicDomain
       mit
