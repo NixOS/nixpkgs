23711ccad7385b7ddca9b70f1d2743e675f5655e
Fernando Rodrigues <alpha@sigmasquadron.net>
2026-01-27 22:34:17 +1100

    xen: patch with XSA-479
                Xen Security Advisory CVE-2026-23553 / XSA-479
    
                    x86: incomplete IBPB for vCPU isolation
    
    In the context switch logic Xen attempts to skip an IBPB in the case of
    a vCPU returning to a CPU on which it was the previous vCPU to run.
    While safe for Xen's isolation between vCPUs, this prevents the guest
    kernel correctly isolating between tasks.  Consider:
    
     1) vCPU runs on CPU A, running task 1.
     2) vCPU moves to CPU B, idle gets scheduled on A.  Xen skips IBPB.
     3) On CPU B, guest kernel switches from task 1 to 2, issuing IBPB.
     4) vCPU moves back to CPU A.  Xen skips IBPB again.
    
    Now, task 2 is running on CPU A with task 1's training still in the BTB.
    
    Guest processes may leverage information leaks to obtain information
    intended to be private to other entities in a guest.
    
    Xen versions which had the XSA-254 fixes backported are vulnerable.
    Upstream, that is 4.6 and newer.
    
    Only x86 systems are vulnerable.  Arm systems are not vulerable.
    
    Systems vulnerable to SRSO (see XSA-434) with default settings use
    IBPB-on-entry to protect against SRSO.  This is a rather more aggressive
    form of flushing than only on context switch, and is believed to be
    sufficient to avoid the vulnerability.
    
    Using "spec-ctrl=ibpb-entry=hvm,ibpb-entry=pv" on the Xen command line
    will activate the SRSO mitigation on non-SRSO-vulnerable hardware, but
    it is a large overhead.
    
    This issue was discovered by David Kaplan of AMD.
    
    https://xenbits.xenproject.org/xsa/advisory-479.html
    
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- pkgs/by-name/xe/xen/package.nix
+++ pkgs/by-name/xe/xen/package.nix
@@ -192,6 +192,12 @@ stdenv.mkDerivation (finalAttrs: {
       hash = "sha256-c9i61GvHPiLwMGvd+5IKgUwyu/NPub+mtnxUPHW/HhI=";
     })
 
+    # XSA #479
+    (fetchpatch {
+      url = "https://xenbits.xenproject.org/xsa/xsa479.patch";
+      hash = "sha256-2o6RYyT4Nrg1le6BUOQ3AwedorCvxvKao2uMYWrUV1Y=";
+    })
+
     # patch `libxl` to search for `qemu-system-i386` properly. (Before 4.21)
     (fetchpatch {
       url = "https://github.com/xen-project/xen/commit/f6281291704aa356489f4bd927cc7348a920bd01.diff?full_index=1";
