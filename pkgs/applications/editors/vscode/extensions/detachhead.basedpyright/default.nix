1d86b2febef828b298d18e85155f33af60b57027
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-28 05:58:45 +0000

    vscode-extensions.detachhead.basedpyright: 1.37.1 -> 1.37.2

--- pkgs/applications/editors/vscode/extensions/detachhead.basedpyright/default.nix
+++ pkgs/applications/editors/vscode/extensions/detachhead.basedpyright/default.nix
@@ -8,8 +8,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "basedpyright";
     publisher = "detachhead";
-    version = "1.37.1";
-    hash = "sha256-xucPJrYqKM+uCxHktfx0o4c0btpluIBMyNigr7d1FJU=";
+    version = "1.37.2";
+    hash = "sha256-5ZkC84924JZe53EcXHHKeMw840O6DIFjydd6zT1QzLQ=";
   };
   meta = {
     changelog = "https://github.com/detachhead/basedpyright/releases";
