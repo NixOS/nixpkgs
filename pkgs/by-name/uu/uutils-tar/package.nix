d87843028f2604f2424ebd3661b58805b54638fe
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-28 18:58:06 +0000

    uutils-tar: 0.0.1-unstable-2026-01-08 -> 0-unstable-2026-01-28

--- pkgs/by-name/uu/uutils-tar/package.nix
+++ pkgs/by-name/uu/uutils-tar/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "uutils-tar";
-  version = "0.0.1-unstable-2026-01-08";
+  version = "0-unstable-2026-01-28";
 
   src = fetchFromGitHub {
     owner = "uutils";
     repo = "tar";
-    rev = "a73ac381d8fd78f8cb05236d26edfdae37a80ee3";
-    hash = "sha256-b8Nzp5zZuULH5YkCexVOPxioPiuauGL4+KarBAdVAd4=";
+    rev = "113912e981c979efb95b4112d218563fa0ef6329";
+    hash = "sha256-ZNK3dpdJx4L8un35LQPZgtXiPC6hcOTFIJ26YKAqSNg=";
   };
 
-  cargoHash = "sha256-UFRe+dBQhsV91tenZY4uqw9gs4ZqbYDtvBeA98dk3po=";
+  cargoHash = "sha256-cpYhIH6aYaWCcD8IA4iYuHMrcDCkLvkiwkwPc+UJw3g=";
 
   passthru.updateScript = nix-update-script {
     extraArgs = [ "--version=branch" ];
