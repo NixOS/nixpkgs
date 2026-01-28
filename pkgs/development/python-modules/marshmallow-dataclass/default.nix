594a3a82b4461a260ff818f881b479bc8a0fd33a
Fabian Affolter <mail@fabian-affolter.ch>
2026-01-28 16:35:53 +0100

    python314Packages.marshmallow-dataclass: add patch to fix test

--- pkgs/development/python-modules/marshmallow-dataclass/default.nix
+++ pkgs/development/python-modules/marshmallow-dataclass/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   marshmallow,
   pytestCheckHook,
   setuptools,
@@ -20,6 +21,15 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-0OXP78oyNe/UcI05NHskPyXAuX3dwAW4Uz4dI4b8KV0=";
   };
+
+  patches = [
+    # Fix test_set_only_work_in_hashable_types on Python 3.14, https://github.com/lovasoa/marshmallow_dataclass/pull/286
+    (fetchpatch {
+      name = "fix-test.patch";
+      url = "https://github.com/lovasoa/marshmallow_dataclass/commit/9a2ea19924a3cd5fadeb41663bfca64b9c0f75e4.patch";
+      hash = "sha256-T2UbZdCj4+HRglMp8w3kU20sUcN6WoSyKiLNr1kSius=";
+    })
+  ];
 
   build-system = [ setuptools ];
 
@@ -52,4 +62,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
