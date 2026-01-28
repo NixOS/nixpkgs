c50d347a81611fe48c6ad76503afcc98487665d0
Sarah Clark <seclark@nextquestion.net>
2026-01-28 10:08:26 -0800

    python3Packages.cytoolz: build from source, use finalAttrs

--- pkgs/development/python-modules/cytoolz/default.nix
+++ pkgs/development/python-modules/cytoolz/default.nix
@@ -1,29 +1,35 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   pytestCheckHook,
   cython,
   setuptools,
-  setuptools-git-versioning,
   toolz,
   python,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "cytoolz";
   version = "1.1.0";
   pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-E6e/JUw8DSixLiKQuCrtDwl3pMKiv4SFT83HeWop87A=";
+  src = fetchFromGitHub {
+    owner = "pytoolz";
+    repo = "cytoolz";
+    tag = finalAttrs.version;
+    hash = "sha256-beOEhm7+Nq7oA7iDcdORz03D1InHmypqsYUDUXEUPC0=";
   };
+
+  postPatch = ''
+    sed -i "/setuptools-git-versioning >=/d" pyproject.toml
+    substituteInPlace pyproject.toml \
+      --replace-fail "dynamic = [\"version\"]" "version = \"${finalAttrs.version}\""
+  '';
 
   nativeBuildInputs = [
     cython
     setuptools
-    setuptools-git-versioning
   ];
 
   propagatedBuildInputs = [ toolz ];
@@ -36,7 +42,8 @@ buildPythonPackage rec {
 
   meta = {
     homepage = "https://github.com/pytoolz/cytoolz/";
+    changelog = "https://github.com/pytoolz/cytoolz/releases/tag/${finalAttrs.src.tag}";
     description = "Cython implementation of Toolz: High performance functional utilities";
     license = lib.licenses.bsd3;
   };
-}
+})
