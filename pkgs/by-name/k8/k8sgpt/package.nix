8cc92d965b6236d6dfca85700bd57b47a700a532
Jocelyn Thode <jocelyn@thode.email>
2026-01-28 22:36:04 +0100

    k8sgpt: 0.4.8 -> 0.4.27

--- pkgs/by-name/k8/k8sgpt/package.nix
+++ pkgs/by-name/k8/k8sgpt/package.nix
@@ -1,24 +1,34 @@
 {
   lib,
+  stdenv,
   buildGoModule,
   fetchFromGitHub,
+  installShellFiles,
 }:
 
 buildGoModule rec {
   pname = "k8sgpt";
-  version = "0.4.8";
+  version = "0.4.27";
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
 
   src = fetchFromGitHub {
     owner = "k8sgpt-ai";
     repo = "k8sgpt";
     rev = "v${version}";
-    hash = "sha256-TaJBGU+nLMVOL1uiHPan8p2DfuAWTr57Lt2BtfNq6dA=";
+    hash = "sha256-nbXM3TbzEGz2A1GlnEIPDSWQ27qXhSvY6G8h5M4tLSU=";
   };
 
-  vendorHash = "sha256-960gfOCpqY2gCbHR+fYFeV9UjztWMRVQKHIg/n3ELxk=";
+  vendorHash = "sha256-tGpyJMfj9r5JAnyfhMkRRQwOEYs/8jn36LXwk7UEIRY=";
 
   # https://nixos.org/manual/nixpkgs/stable/#var-go-CGO_ENABLED
   env.CGO_ENABLED = 0;
+
+  preCheck = ''
+    export HOME=$TMPDIR
+  '';
 
   # https://nixos.org/manual/nixpkgs/stable/#ssec-skip-go-tests
   ldflags = [
@@ -28,6 +38,13 @@ buildGoModule rec {
     "-X main.commit=${src.rev}"
     "-X main.date=1970-01-01-00:00:01"
   ];
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd k8sgpt \
+      --bash <($out/bin/k8sgpt completion bash) \
+      --zsh <($out/bin/k8sgpt completion zsh) \
+      --fish <($out/bin/k8sgpt completion fish)
+  '';
 
   meta = {
     description = "Giving Kubernetes Superpowers to everyone";
