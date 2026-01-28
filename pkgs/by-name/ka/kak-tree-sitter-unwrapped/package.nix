dcc6bab2a1cef8e11c2f56aa29ae351d6fef56c6
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-28 05:30:34 +0000

    kak-tree-sitter-unwrapped: 3.0.0 -> 3.1.2

--- pkgs/by-name/ka/kak-tree-sitter-unwrapped/package.nix
+++ pkgs/by-name/ka/kak-tree-sitter-unwrapped/package.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kak-tree-sitter-unwrapped";
-  version = "3.0.0";
+  version = "3.1.2";
 
   src = fetchFromSourcehut {
     owner = "~hadronized";
     repo = "kak-tree-sitter";
     rev = "kak-tree-sitter-v${version}";
-    hash = "sha256-9QeTHmDTw1Qxnl+AnqCZgxlCCycq5qYF0aM/6fu8qwM=";
+    hash = "sha256-Pzvk4RqVe40aSKrmL/ulD6QXvDinabDCoMpW+QOB1M0=";
   };
 
-  cargoHash = "sha256-lZNM5HqICP6JfaMiBjACcUNRTTTIRhq2ou8cOLU0yOU=";
+  cargoHash = "sha256-XT9Ha8D/dBhlDu2EQ//TcWig/btfvzUBdijiElDJLOU=";
 
   passthru = {
     updateScript = nix-update-script { };
