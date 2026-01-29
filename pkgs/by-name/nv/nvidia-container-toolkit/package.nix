abc0804717a9a63798d87cd99709fea988a9dbee
talyz <kim.lindberger@gmail.com>
2026-01-29 23:41:18 +0100

    Revert "nvidia-container-toolkit: 1.18.0 -> 1.18.1"
    This reverts commit f5be3974ed1b29400a81c8181e2c197ba527b19c.
    
    Not-cherry-picked-because: Reverted for stability (#467809)

--- pkgs/by-name/nv/nvidia-container-toolkit/package.nix
+++ pkgs/by-name/nv/nvidia-container-toolkit/package.nix
@@ -13,13 +13,13 @@ let
 in
 buildGoModule (finalAttrs: {
   pname = "nvidia-container-toolkit";
-  version = "1.18.1";
+  version = "1.18.0";
 
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = "nvidia-container-toolkit";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-qrEMfynNXiUrG/QXpgrzu2BAMd5XVh5s3LpirjzX4Z8=";
+    hash = "sha256-VQcuN+LU7iljpSWrmLBHX67esEQN1HYNPj5cLxUB7dI=";
 
   };
 
