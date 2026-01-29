120a20d5e9dfd4f1c3ba60b846c232271b8847ab
Markus Hauck <markus1189@gmail.com>
2026-01-29 07:49:03 +0100

    vscode-extensions.anthropic.claude-code: 2.1.20 -> 2.1.23
    https://github.com/anthropics/claude-code/blob/main/CHANGELOG.md

--- pkgs/applications/editors/vscode/extensions/anthropic.claude-code/default.nix
+++ pkgs/applications/editors/vscode/extensions/anthropic.claude-code/default.nix
@@ -8,8 +8,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "claude-code";
     publisher = "anthropic";
-    version = "2.1.20";
-    hash = "sha256-8r2/bF5zkq4LkMKvI5YcBMaaMzgWAIezdIwTMQQBHsM=";
+    version = "2.1.23";
+    hash = "sha256-0Zhg/3xRoOIWFDBe32SuT5d2wX8wld+mSm2iaSIzeH8=";
   };
 
   postInstall = ''
