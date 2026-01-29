93ae4b5b13db4e4f60049d0b81468726fbb8b86d
highghlow <highghlow@proton.me>
2026-01-29 22:10:45 +0300

    qdb: init at 1.2
    There seems to be a lack of C brainfuck interpreters in nixpkgs, this is
    a simple, yet working one
    
    This software hasn't been updated in at least 5 years, but it doesn't
    have to be, as it's feature complete.

---
+++ pkgs/by-name/qd/qdb/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
+
+stdenv.mkDerivation {
+  pname = "qdb";
+  version = "1.2";
+
+  src = fetchurl {
+    url = "https://brainfuck.org/qdb.c";
+    hash = "sha256-5GPsZ2F7bNxoCzWGbh2Xg/H+e3IcsVpQ1UtSD3yHTMw=";
+  };
+  unpackPhase = "cp $src qdb.c";
+
+  buildPhase = ''
+    cc qdb.c -o qdb
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp qdb $out/bin/
+  '';
+
+  meta = {
+    description = "Quick and dirty brainfuck interpreter";
+    mainProgram = "qdb";
+
+    # Make any use you like of this software. I can't stop you anyway. :)
+    license = lib.licenses.publicDomain;
+  };
+}
