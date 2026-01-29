20a46ceec91b6d13d90d5ec4061d35f0c8b0c773
Harinn <prinn.dev@pm.me>
2026-01-29 08:41:01 +0700

    kalker: 2.2.1 -> 2.2.2

--- pkgs/by-name/ka/kalker/package.nix
+++ pkgs/by-name/ka/kalker/package.nix
@@ -10,26 +10,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kalker";
-  version = "2.2.1";
+  version = "2.2.2";
 
   src = fetchFromGitHub {
     owner = "PaddiM8";
     repo = "kalker";
     rev = "v${version}";
-    hash = "sha256-fFeHL+Q1Y0J3rOgbFA952rjae/OQgHTznDI0Kya1KMQ=";
+    hash = "sha256-P8Yj6uqIZ3twkIJUMZhxrisWQ27LHYIHm3x6cjNR0Ns=";
   };
 
-  cargoHash = "sha256-LEP2ebthwtpPSRmJt0BW/T/lB6EE+tylyVv+PDt8UoQ=";
+  cargoHash = "sha256-nYxpSnyXWnrj/HWH7x6yw5CSBqTvLUWdlBv1E6U/Ihg=";
 
-  cargoPatches = [
-    # Fixes build issue by just running cargo update
-    # Can be removed on next release
-    (fetchpatch {
-      name = "bump_cargo_deps.patch";
-      url = "https://github.com/PaddiM8/kalker/commit/81bf66950a9dfeca4ab5fdd12774c93e40021eb1.patch";
-      hash = "sha256-XT8jXTMIMOFw8OieoQM7IkUqw3SDi1c9eE1cD15BI9I=";
-    })
-  ];
+  cargoPatches = [ ];
 
   buildInputs = [
     gmp
