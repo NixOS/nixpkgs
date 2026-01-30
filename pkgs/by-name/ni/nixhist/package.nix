9203c74601aa398634e5e9b46da7171a633e5fb4
daskladas <xvzrsm@tutanota.de>
2026-01-30 09:52:04 +0100

    nixhist: init at 1.0.0

---
+++ pkgs/by-name/ni/nixhist/package.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  xorg,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "nixhist";
+  version = "1.0.0";
+
+  src = fetchFromGitHub {
+    owner = "daskladas";
+    repo = "nixhist";
+    rev = "v${version}";
+    hash = "sha256-Y7GisoFi96I9JeuNw4aKZpfcEZp13A8NfS5vzfsR9Mk=";
+  };
+
+  cargoHash = "sha256-/7JHIU0REuEaPWPSAM5QsW0d6dZz+cCfhlX1ytUH5Lc=";
+  doCheck = false;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    xorg.libxcb
+  ];
+
+  meta = {
+    description = "A TUI for viewing, comparing, and managing NixOS generations";
+    homepage = "https://github.com/daskladas/nixhist";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "nixhist";
+    platforms = lib.platforms.linux;
+  };
+}
