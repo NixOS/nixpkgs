2afa5a1b5b792a007bc5c402eefa0cc189c41c15
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-30 11:06:36 +0000

    netbird-management: 0.64.1 -> 0.64.3

--- pkgs/by-name/ne/netbird/package.nix
+++ pkgs/by-name/ne/netbird/package.nix
@@ -68,16 +68,16 @@ let
 in
 buildGoModule (finalAttrs: {
   pname = "netbird-${componentName}";
-  version = "0.64.1";
+  version = "0.64.3";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "netbird";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-fxQ6x/MN3FKRiGgb8OPjLnHCVjYnI4vBLwjDKAxrUQY=";
+    hash = "sha256-mNzXdrpM+oeZ7X4NRNYsoMcSfvRU6SeExstUF3fZ3bE=";
   };
 
-  vendorHash = "sha256-suQPYxZo1R2Jh4N2ulOysfhRKAnVUUMzJoj+5lmhwDk=";
+  vendorHash = "sha256-n0s2K+qbMiNfbkPbqvvXpvFvL5PkCkuJJ2CH3bEUCOk=";
 
   nativeBuildInputs = [ installShellFiles ] ++ lib.optional (componentName == "ui") pkg-config;
 
