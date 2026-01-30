9182c43b961d2de67db3f7ad9d74e12407e60b4f
Mohammed <88824957+m7medVision@users.noreply.github.com>
2026-01-30 19:31:53 +0400

    lazycommit: init at 1.4.0

---
+++ pkgs/by-name/la/lazycommit/package.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  lazycommit,
+  testers,
+  nix-update-script,
+}:
+buildGoModule rec {
+  pname = "lazycommit";
+  version = "1.4.0";
+
+  src = fetchFromGitHub {
+    owner = "m7medvision";
+    repo = "lazycommit";
+    tag = "v${version}";
+    hash = "sha256-DD3DXTev8WHNkAYDrPY2PISuA8WwKuK0GCLebpn01Rg=";
+  };
+
+  vendorHash = "sha256-4OPCUWXxsAnzxsqZPHhjvhxQQf5Knm7nGqrdjH4I4YY=";
+  subPackages = [ "." ];
+
+  ldflags = [
+    "-X main.version=${version}"
+    "-X main.buildSource=nix"
+  ];
+
+  passthru = {
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--version-regex"
+        "^v([0-9.]+)$"
+      ];
+    };
+  };
+
+  meta = {
+    description = "Simple cli for genrate git commits";
+    homepage = "https://github.com/m7medvision/lazycommit";
+    changelog = "https://github.com/m7medvision/lazycommit/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      Br1ght0ne
+      equirosa
+      khaneliman
+      starsep
+      sigmasquadron
+    ];
+    mainProgram = "lazycommit";
+  };
+}
