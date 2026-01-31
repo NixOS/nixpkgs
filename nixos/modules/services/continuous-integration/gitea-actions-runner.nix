59721fe8719e2ab4c658968b71c9e4b13585714e
Fernando Rodrigues <alpha@sigmasquadron.net>
2026-01-31 22:40:16 +1100

    nixos/gitea-actions-runner: use mkMerge for config
    Signed-off-by: Fernando Rodrigues <alpha@sigmasquadron.net>

--- nixos/modules/services/continuous-integration/gitea-actions-runner.nix
+++ nixos/modules/services/continuous-integration/gitea-actions-runner.nix
@@ -18,6 +18,7 @@ let
     mapAttrs'
     mkEnableOption
     mkIf
+    mkMerge
     mkOption
     mkPackageOption
     nameValuePair
@@ -170,114 +171,120 @@ in
     };
   };
 
-  config = mkIf (cfg.instances != { }) {
-    assertions = [
-      {
-        assertion = any tokenXorTokenFile (attrValues cfg.instances);
-        message = "Instances of gitea-actions-runner can have `token` or `tokenFile`, not both.";
-      }
-      {
-        assertion = wantsContainerRuntime -> hasDocker || hasPodman;
-        message = "Label configuration on gitea-actions-runner instance requires either docker or podman.";
-      }
-    ];
-
-    systemd.services =
+  config = mkMerge [
+    (mkIf (cfg.instances != { }) (
       let
-        mkRunnerService =
-          name: instance:
+      in
+      {
+        assertions = [
+          {
+            assertion = any tokenXorTokenFile (attrValues cfg.instances);
+            message = "Instances of gitea-actions-runner can have `token` or `tokenFile`, not both.";
+          }
+          {
+            assertion = wantsContainerRuntime -> hasDocker || hasPodman;
+            message = "Label configuration on gitea-actions-runner instance requires either docker or podman.";
+          }
+        ];
+
+        systemd.services =
           let
-            wantsContainerRuntime = hasDockerScheme instance;
-            wantsHost = hasHostScheme instance;
-            wantsDocker = wantsContainerRuntime && config.virtualisation.docker.enable;
-            wantsPodman = wantsContainerRuntime && config.virtualisation.podman.enable;
-            configFile = settingsFormat.generate "config.yaml" instance.settings;
-          in
-          nameValuePair "gitea-runner-${escapeSystemdPath name}" {
-            inherit (instance) enable;
-            description = "Gitea Actions Runner";
-            wants = [ "network-online.target" ];
-            after = [
-              "network-online.target"
-            ]
-            ++ optionals wantsDocker [
-              "docker.service"
-            ]
-            ++ optionals wantsPodman [
-              "podman.service"
-            ];
-            wantedBy = [
-              "multi-user.target"
-            ];
-            environment =
-              optionalAttrs (instance.token != null) {
-                TOKEN = "${instance.token}";
-              }
-              // optionalAttrs wantsPodman {
-                DOCKER_HOST = "unix:///run/podman/podman.sock";
-              }
-              // {
-                HOME = "/var/lib/gitea-runner/${name}";
-              };
-            path =
-              with pkgs;
-              [
-                coreutils
-              ]
-              ++ lib.optionals wantsHost instance.hostPackages;
-            serviceConfig = {
-              DynamicUser = true;
-              User = "gitea-runner";
-              StateDirectory = "gitea-runner";
-              WorkingDirectory = "-/var/lib/gitea-runner/${name}";
-
-              # gitea-runner might fail when gitea is restarted during upgrade.
-              Restart = "on-failure";
-              RestartSec = 2;
-
-              ExecStartPre = [
-                (pkgs.writeShellScript "gitea-register-runner-${name}" ''
-                  export INSTANCE_DIR="$STATE_DIRECTORY/${name}"
-                  mkdir -vp "$INSTANCE_DIR"
-                  cd "$INSTANCE_DIR"
-
-                  # force reregistration on changed labels
-                  export LABELS_FILE="$INSTANCE_DIR/.labels"
-                  export LABELS_WANTED="$(echo ${escapeShellArg (concatStringsSep "\n" instance.labels)} | sort)"
-                  export LABELS_CURRENT="$(cat $LABELS_FILE 2>/dev/null || echo 0)"
-
-                  if [ ! -e "$INSTANCE_DIR/.runner" ] || [ "$LABELS_WANTED" != "$LABELS_CURRENT" ]; then
-                    # remove existing registration file, so that changing the labels forces a re-registration
-                    rm -v "$INSTANCE_DIR/.runner" || true
-
-                    # perform the registration
-                    ${cfg.package}/bin/act_runner register --no-interactive \
-                      --instance ${escapeShellArg instance.url} \
-                      --token "$TOKEN" \
-                      --name ${escapeShellArg instance.name} \
-                      --labels ${escapeShellArg (concatStringsSep "," instance.labels)} \
-                      --config ${configFile}
-
-                    # and write back the configured labels
-                    echo "$LABELS_WANTED" > "$LABELS_FILE"
-                  fi
-
-                '')
-              ];
-              ExecStart = "${cfg.package}/bin/act_runner daemon --config ${configFile}";
-              SupplementaryGroups =
-                optionals wantsDocker [
-                  "docker"
+            mkRunnerService =
+              name: instance:
+              let
+                wantsContainerRuntime = hasDockerScheme instance;
+                wantsHost = hasHostScheme instance;
+                wantsDocker = wantsContainerRuntime && config.virtualisation.docker.enable;
+                wantsPodman = wantsContainerRuntime && config.virtualisation.podman.enable;
+                configFile = settingsFormat.generate "config.yaml" instance.settings;
+              in
+              nameValuePair "gitea-runner-${escapeSystemdPath name}" {
+                inherit (instance) enable;
+                description = "Gitea Actions Runner";
+                wants = [ "network-online.target" ];
+                after = [
+                  "network-online.target"
+                ]
+                ++ optionals wantsDocker [
+                  "docker.service"
                 ]
                 ++ optionals wantsPodman [
-                  "podman"
+                  "podman.service"
                 ];
-            }
-            // optionalAttrs (instance.tokenFile != null) {
-              EnvironmentFile = instance.tokenFile;
-            };
-          };
-      in
-      mapAttrs' mkRunnerService cfg.instances;
-  };
+                wantedBy = [
+                  "multi-user.target"
+                ];
+                environment =
+                  optionalAttrs (instance.token != null) {
+                    TOKEN = "${instance.token}";
+                  }
+                  // optionalAttrs wantsPodman {
+                    DOCKER_HOST = "unix:///run/podman/podman.sock";
+                  }
+                  // {
+                    HOME = "/var/lib/gitea-runner/${name}";
+                  };
+                path =
+                  with pkgs;
+                  [
+                    coreutils
+                  ]
+                  ++ lib.optionals wantsHost instance.hostPackages;
+                serviceConfig = {
+                  DynamicUser = true;
+                  User = "gitea-runner";
+                  StateDirectory = "gitea-runner";
+                  WorkingDirectory = "-/var/lib/gitea-runner/${name}";
+
+                  # gitea-runner might fail when gitea is restarted during upgrade.
+                  Restart = "on-failure";
+                  RestartSec = 2;
+
+                  ExecStartPre = [
+                    (pkgs.writeShellScript "gitea-register-runner-${name}" ''
+                      export INSTANCE_DIR="$STATE_DIRECTORY/${name}"
+                      mkdir -vp "$INSTANCE_DIR"
+                      cd "$INSTANCE_DIR"
+
+                      # force reregistration on changed labels
+                      export LABELS_FILE="$INSTANCE_DIR/.labels"
+                      export LABELS_WANTED="$(echo ${escapeShellArg (concatStringsSep "\n" instance.labels)} | sort)"
+                      export LABELS_CURRENT="$(cat $LABELS_FILE 2>/dev/null || echo 0)"
+
+                      if [ ! -e "$INSTANCE_DIR/.runner" ] || [ "$LABELS_WANTED" != "$LABELS_CURRENT" ]; then
+                        # remove existing registration file, so that changing the labels forces a re-registration
+                        rm -v "$INSTANCE_DIR/.runner" || true
+
+                        # perform the registration
+                        ${cfg.package}/bin/act_runner register --no-interactive \
+                          --instance ${escapeShellArg instance.url} \
+                          --token "$TOKEN" \
+                          --name ${escapeShellArg instance.name} \
+                          --labels ${escapeShellArg (concatStringsSep "," instance.labels)} \
+                          --config ${configFile}
+
+                        # and write back the configured labels
+                        echo "$LABELS_WANTED" > "$LABELS_FILE"
+                      fi
+
+                    '')
+                  ];
+                  ExecStart = "${cfg.package}/bin/act_runner daemon --config ${configFile}";
+                  SupplementaryGroups =
+                    optionals wantsDocker [
+                      "docker"
+                    ]
+                    ++ optionals wantsPodman [
+                      "podman"
+                    ];
+                }
+                // optionalAttrs (instance.tokenFile != null) {
+                  EnvironmentFile = instance.tokenFile;
+                };
+              };
+          in
+          mapAttrs' mkRunnerService cfg.instances;
+      }
+    ))
+  ];
 }
