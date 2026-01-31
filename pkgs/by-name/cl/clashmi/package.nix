28fbf3bf1ce00c6adbd9fea99dea96588f912080
R. Ryantm <ryantm-bot@ryantm.com>
2026-01-31 16:50:02 +0000

    clashmi: 1.0.16.204 -> 1.0.17.300

--- pkgs/by-name/cl/clashmi/package.nix
+++ pkgs/by-name/cl/clashmi/package.nix
@@ -24,11 +24,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "clashmi";
-  version = "1.0.16.204";
+  version = "1.0.17.300";
 
   src = fetchurl {
     url = "https://github.com/KaringX/clashmi/releases/download/v${finalAttrs.version}/clashmi_${finalAttrs.version}_linux_amd64.deb";
-    hash = "sha256-IOCrk4dl9BYf8rzhgXmRAaFP4pzsJMx6i2zTg/NpOCQ=";
+    hash = "sha256-jsWDBIFKoainN8zu9kgFmaPpZXaZZbe8MQ1i/ICpNP4=";
   };
 
   nativeBuildInputs = [
