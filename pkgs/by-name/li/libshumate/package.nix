4f2be6651f3ccb90fbf385e17da3672a02b8627f
Jan Tojnar <jtojnar@gmail.com>
2026-01-31 12:36:00 +0000

    libshumate: 1.5.2 → 1.5.3
    https://gitlab.gnome.org/GNOME/libshumate/-/compare/1.5.2...1.5.3
    (cherry picked from commit b02e19d6abde842dc7e4565a426a72b86a3ac0d2)

--- pkgs/by-name/li/libshumate/package.nix
+++ pkgs/by-name/li/libshumate/package.nix
@@ -23,7 +23,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libshumate";
-  version = "1.5.2";
+  version = "1.5.3";
 
   outputs = [
     "out"
@@ -34,7 +34,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/libshumate/${lib.versions.majorMinor finalAttrs.version}/libshumate-${finalAttrs.version}.tar.xz";
-    hash = "sha256-ppVlwetd2ezTkmi78UNeqlGlsCjtDGUqC2uTZ+BMk4Y=";
+    hash = "sha256-NfATslORhW4bXD1ECaXEpkDGzLX1KW/5gux77Tn1bUw=";
   };
 
   depsBuildBuild = [
