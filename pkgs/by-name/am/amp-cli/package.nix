94612e3698602437302e03861ac42454613d1486
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-01 15:07:59 +0000

    amp-cli: 0.0.1769346334-gc7e300 -> 0.0.1769949290-gbb0479

--- pkgs/by-name/am/amp-cli/package.nix
+++ pkgs/by-name/am/amp-cli/package.nix
@@ -9,11 +9,11 @@
 
 buildNpmPackage (finalAttrs: {
   pname = "amp-cli";
-  version = "0.0.1769346334-gc7e300";
+  version = "0.0.1769949290-gbb0479";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@sourcegraph/amp/-/amp-${finalAttrs.version}.tgz";
-    hash = "sha256-Y0ySMlYP/mvD+ydkGhqt03zYkFj6BmZpe2zLnAV5rDk=";
+    hash = "sha256-RpuTDQLv/MVL5GJw8d3C+TCGywy0fIIaRfJyzEMbTKg=";
   };
 
   postPatch = ''
@@ -45,7 +45,7 @@ buildNpmPackage (finalAttrs: {
     chmod +x bin/amp-wrapper.js
   '';
 
-  npmDepsHash = "sha256-2e0kJJqmIB3kAaSzATSaNojLvsCIq5MwTGK0y0OxfO4=";
+  npmDepsHash = "sha256-DwEXk05AKNyc4nCnah+aRByhdlXNpXQqSfTBJPDGXQ8=";
 
   propagatedBuildInputs = [
     ripgrep
