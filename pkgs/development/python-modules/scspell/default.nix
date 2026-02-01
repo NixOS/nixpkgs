7824b71b15a47115f079288aaf0fe705ee5444bc
Michael Daniels <mdaniels5757@gmail.com>
2026-01-31 20:23:06 -0500

    python3Packages.scspell: 2.3 -> 2.3.0-unstable-2025-04-06
    Fixes build with python314.

--- pkgs/development/python-modules/scspell/default.nix
+++ pkgs/development/python-modules/scspell/default.nix
@@ -2,34 +2,31 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  pyxdg,
   setuptools,
   versionCheckHook,
   writableTmpDirAsHomeHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage {
   pname = "scspell";
-  version = "2.3";
+  version = "2.3.0-unstable-2025-04-06";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "myint";
     repo = "scspell";
-    tag = "v${version}";
-    hash = "sha256-XiUdz+uHOJlqo+TWd1V/PvzkGJ2kPXzJJSe5Smfdgec=";
+    rev = "df550351f255c572c1a74852d233c83bbfbd49fb"; # Switch back to tag and remove preVersionCheck next release
+    hash = "sha256-mqU7Z6MluHTVYJ8fFbnN0OMWKjQFglD34YRnmJSE/jQ=";
   };
 
   build-system = [ setuptools ];
-
-  dependencies = [
-    pyxdg
-  ];
 
   nativeCheckInputs = [
     versionCheckHook
     writableTmpDirAsHomeHook
   ];
+
+  preVersionCheck = "export version=2.3";
 
   pythonImportsCheck = [ "scspell" ];
 
