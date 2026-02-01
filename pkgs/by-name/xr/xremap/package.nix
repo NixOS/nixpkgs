d0c7e9e8585bc8636ad42219eeea0a4b5374146f
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-01 12:45:45 +0000

    xremap: 0.14.10 -> 0.14.11

--- pkgs/by-name/xr/xremap/package.nix
+++ pkgs/by-name/xr/xremap/package.nix
@@ -43,13 +43,13 @@ assert (
 );
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "xremap${variant.suffix or ""}";
-  version = "0.14.10";
+  version = "0.14.11";
 
   src = fetchFromGitHub {
     owner = "xremap";
     repo = "xremap";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-2wylBk3+Zu1pHa41dhKwvUtxOVyHSMRDfOD9fIp8x2I=";
+    hash = "sha256-UNiyUujijIAsEvCRuMZ1MVNwoY/v5Sbo3/2r8vdA6HM=";
   };
 
   nativeBuildInputs = [ pkg-config ];
@@ -57,7 +57,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   buildNoDefaultFeatures = true;
   buildFeatures = variant.features;
 
-  cargoHash = "sha256-S9mevRDGx23mquABVH/cC1zKNxW1607mS/vaRxRqKfk=";
+  cargoHash = "sha256-roa2Akc1XuSGGkBK++qYvmTgVwzA5xNlrf0TdTUoEdM=";
 
   passthru = lib.mapAttrs (name: lib.const (xremap.override { withVariant = name; })) variants;
 
