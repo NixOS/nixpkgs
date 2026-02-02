84d9e2374c944495edced21cc89b392f60f6b9c6
Jamer Rebolledo <jamerrq@gmail.com>
2026-02-01 23:08:22 -0500

    python3Packages.stockfish: address review comments
    - Use patch file instead of inline prePatch for setup.py modification
    - Update homepage to new repository location (py-stockfish/stockfish)
    - Remove redundant passthru.tests (duplicates pythonImportsCheck)
    - Fix doCheck comment with accurate reason and issue reference
    - Remove unused test dependencies from inputs

--- pkgs/development/python-modules/stockfish/default.nix
+++ pkgs/development/python-modules/stockfish/default.nix
@@ -5,10 +5,6 @@
   pythonOlder,
   setuptools,
   wheel,
-  # For tests
-  stockfish,
-  runCommand,
-  python,
 }:
 
 buildPythonPackage rec {
@@ -23,70 +19,23 @@ buildPythonPackage rec {
     hash = "sha256-h2QSfABDSqhbf8ocBkgA5+qQe7NibM1LWD8OkRsBQHA=";
   };
 
-  # The original setup.py requires pytest-runner which has been
-  # removed from nixpkgs. We replace it with a minimal setup.py
-  # that avoids this deprecated dependency.
-  prePatch = ''
-        cat > setup.py << 'EOF'
-    from setuptools import setup, find_packages
-
-    with open("README.md", "r", encoding="utf-8") as fh:
-        long_description = fh.read()
-
-    setup(
-        name="stockfish",
-        version="3.28.0",
-        author="Ilya Zhelyabuzhsky",
-        author_email="zhelyabuzhsky@icloud.com",
-        description="Integrates the Stockfish chess engine with Python",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/zhelyabuzhsky/stockfish",
-        packages=find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.7',
-    )
-    EOF
-  '';
+  patches = [ ./setup.patch ];
 
   build-system = [
     setuptools
     wheel
   ];
 
-  # Tests require network access and additional setup
+  # Tests fail due to stockfish version incompatibility
+  # https://github.com/py-stockfish/stockfish/issues/17
   doCheck = false;
 
   pythonImportsCheck = [ "stockfish" ];
 
-  passthru.tests = {
-    # Basic import test
-    basic-import =
-      runCommand "stockfish-test-import"
-        {
-          buildInputs = [ (python.withPackages (ps: [ ps.stockfish ])) ];
-        }
-        ''
-          python3 -c "from stockfish import Stockfish; print('✓ ok')"
-          touch $out
-        '';
-  };
-
   meta = {
     description = "Integrates the Stockfish chess engine with Python";
-    homepage = "https://github.com/zhelyabuzhsky/stockfish";
+    homepage = "https://github.com/py-stockfish/stockfish";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ jamerrq ];
-    longDescription = ''
-      Python wrapper for the Stockfish chess engine. Requires the stockfish
-      chess engine binary to be available in PATH or specified explicitly.
-
-      Usage with Nix:
-      nix-shell -p 'python3.withPackages (ps: [ ps.stockfish ])' stockfish
-    '';
   };
 }
