01e2c5b319c5fa05bb13138e54520026ca62f2e6
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-02 02:32:29 +0100

    python313Packages.aiobafi6: migrate to finalAttrs

--- pkgs/development/python-modules/aiobafi6/default.nix
+++ pkgs/development/python-modules/aiobafi6/default.nix
@@ -9,7 +9,7 @@
   zeroconf,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aiobafi6";
   version = "0.10.1";
   pyproject = true;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "jfroy";
     repo = "aiobafi6";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-EXLMrZobSICAmWPREjx5D8boj/S/3AH5+lsWQlTCl1g=";
   };
 
@@ -38,9 +38,9 @@ buildPythonPackage rec {
   meta = {
     description = "Library for communication with the Big Ass Fans i6 firmware";
     homepage = "https://github.com/jfroy/aiobafi6";
-    changelog = "https://github.com/jfroy/aiobafi6/releases/tag/${src.tag}";
+    changelog = "https://github.com/jfroy/aiobafi6/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "aiobafi6";
   };
-}
+})
