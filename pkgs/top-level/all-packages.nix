a5cfd31224ece636641769ac70920188c1cf1c5b
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-02-02 03:42:13 +0100

    lichess-external-engine: init at a6ef15a

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2455,6 +2455,8 @@ with pkgs;
 
   licensee = callPackage ../tools/package-management/licensee { };
 
+  lichess-external-engine = python3Packages.callPackage ../by-name/li/lichess-external-engine/package.nix { };
+
   linux-gpib = callPackage ../applications/science/electronics/linux-gpib/user.nix { };
 
   liquidctl = with python3Packages; toPythonApplication liquidctl;
