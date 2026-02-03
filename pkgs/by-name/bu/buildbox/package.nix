90c6153796987111d5311675aa93304083eab6af
Dom Rodriguez <shymega@shymega.org.uk>
2026-02-03 23:13:27 +0000

    buildbox: Update to 1.3.52
    - Update to 1.3.49.
    - Add new `curl` buildInput as per upstream.

--- pkgs/by-name/bu/buildbox/package.nix
+++ pkgs/by-name/bu/buildbox/package.nix
@@ -18,17 +18,18 @@
   libuuid,
   tomlplusplus,
   fuse3,
+  curl,
   nix-update-script,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "buildbox";
-  version = "1.3.21";
+  version = "1.3.52";
 
   src = fetchFromGitLab {
     owner = "BuildGrid";
     repo = "buildbox/buildbox";
     tag = finalAttrs.version;
-    hash = "sha256-gZ4PnaIiMPh18Yy2120yIEaQaFpzGNnWXzS7Uw+n/+k=";
+    hash = "sha256-0uoVR9V7/nKWn5l1chTOpIHLgzUZ0ryHVtCYTn9WUAs=";
   };
 
   nativeBuildInputs = [
@@ -40,6 +41,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   buildInputs = [
     bubblewrap
+    curl
     fuse3
     gbenchmark
     glog
