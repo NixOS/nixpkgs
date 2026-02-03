6758bed1305e09dccf056b9f68fe93835cd2801c
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-03 19:34:05 +0000

    saw-tools: 1.4 -> 1.5

--- pkgs/by-name/sa/saw-tools/package.nix
+++ pkgs/by-name/sa/saw-tools/package.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "saw-tools";
-  version = "1.4";
+  version = "1.5";
 
   src = fetchurl {
     url = "https://github.com/GaloisInc/saw-script/releases/download/v${finalAttrs.version}/saw-${finalAttrs.version}-ubuntu-22.04-X64-with-solvers.tar.gz";
-    hash = "sha256-AjMGOi0Nzl0cjVltjgbqhzBiPpIZbDtS3+SqergeulE=";
+    hash = "sha256-k9Qo93d0IXBRe7P+wU20LjFjM+LdHf6Z2S0Nybmh/4E=";
   };
 
   buildInputs = [
