dd072d9a8237c33553e2799043d562ca796b9b12
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-03 14:15:16 +0000

    astal.source: 0-unstable-2025-11-26 -> 0-unstable-2026-01-30

--- pkgs/development/libraries/astal/source.nix
+++ pkgs/development/libraries/astal/source.nix
@@ -7,15 +7,15 @@ let
   originalDrv = fetchFromGitHub {
     owner = "Aylur";
     repo = "astal";
-    rev = "7d1fac8a4b2a14954843a978d2ddde86168c75ef";
-    hash = "sha256-Jh4VtPcK2Ov+RTcV9FtyQRsxiJmXFQGfqX6jjM7/mgc=";
+    rev = "eb235f8813bdea2a4a38ac228f2efc4e2a8a90af";
+    hash = "sha256-8Ve6VdUpcYbl8bS5oyDwVnnNobyPxdPmUHGgSDyOazQ=";
   };
 in
 originalDrv.overrideAttrs (
   final: prev: {
     name = "${final.pname}-${final.version}"; # fetchFromGitHub already defines name
     pname = "astal-source";
-    version = "0-unstable-2025-11-26";
+    version = "0-unstable-2026-01-30";
 
     meta = prev.meta // {
       description = "Building blocks for creating custom desktop shells (source)";
