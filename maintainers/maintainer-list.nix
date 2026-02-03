b1f21d88826b7016264bdd43e26869492f5f5d19
hetraeus <fulmare90@gmail.com>
2026-02-03 19:07:14 +0100

    maintainers: add hetraeus

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10467,6 +10467,11 @@
     githubId = 69494718;
     name = "Herschenglime";
   };
+  hetraeus = {
+    github = "hetraeus";
+    githubId = 4652857;
+    name = "Hetraeus";
+  };
   hexa = {
     email = "hexa@darmstadt.ccc.de";
     matrix = "@hexa:lossy.network";
