6c978f637268efa7f3832dbbe4ccbeb7e45008f0
John Ericson <John.Ericson@Obsidian.Systems>
2026-02-04 15:23:54 -0500

    wine: Delete `default.nix`
    It is an unncessary wrapper around `base.nix`.

--- pkgs/top-level/wine-packages.nix
+++ pkgs/top-level/wine-packages.nix
@@ -8,76 +8,113 @@
   wineBuild,
 }:
 
+let
+  sources = import ../applications/emulators/wine/sources.nix { inherit pkgs; };
+
+  baseFlags = {
+    gettextSupport = true;
+    fontconfigSupport = stdenv.hostPlatform.isLinux;
+    alsaSupport = stdenv.hostPlatform.isLinux;
+    openglSupport = true;
+    vulkanSupport = true;
+    tlsSupport = true;
+    cupsSupport = true;
+    dbusSupport = stdenv.hostPlatform.isLinux;
+    cairoSupport = stdenv.hostPlatform.isLinux;
+    cursesSupport = true;
+    saneSupport = stdenv.hostPlatform.isLinux;
+    pulseaudioSupport = config.pulseaudio or stdenv.hostPlatform.isLinux;
+    udevSupport = stdenv.hostPlatform.isLinux;
+    xineramaSupport = stdenv.hostPlatform.isLinux;
+    sdlSupport = true;
+    mingwSupport = true;
+    usbSupport = true;
+    waylandSupport = stdenv.hostPlatform.isLinux;
+    x11Support = stdenv.hostPlatform.isLinux;
+    ffmpegSupport = true;
+  };
+
+  fullFlags = baseFlags // {
+    gtkSupport = stdenv.hostPlatform.isLinux;
+    gstreamerSupport = true;
+    openclSupport = true;
+    odbcSupport = true;
+    netapiSupport = stdenv.hostPlatform.isLinux;
+    vaSupport = stdenv.hostPlatform.isLinux;
+    pcapSupport = true;
+    v4lSupport = stdenv.hostPlatform.isLinux;
+    gphoto2Support = true;
+    krb5Support = true;
+    embedInstallers = true;
+  };
+
+in
+
 lib.makeExtensible (
   self:
   let
     callPackage = newScope self;
+
+    # Map user-facing release names to sources, pname suffix, and staging flag
+    releaseInfo = {
+      stable = {
+        src = sources.stable;
+        useStaging = false;
+      };
+      unstable = {
+        src = sources.unstable;
+        useStaging = false;
+      };
+      # Many versions have a "staging" variant, but when we say "staging",
+      # the version we want to use is "unstable".
+      staging = {
+        src = sources.unstable;
+        pnameSuffix = "-staging";
+        useStaging = true;
+      };
+      # "yabridge" enables staging too --- we are not interested in
+      # yabridge without the staging patches applied.
+      yabridge = {
+        src = sources.yabridge;
+        pnameSuffix = "-yabridge";
+        useStaging = true;
+      };
+      wayland = {
+        src = sources.stable;
+        useStaging = false;
+        x11Support = false;
+      };
+    };
+
+    defaultRelease = config.wine.release or "stable";
+
+    mkWine =
+      flags: info: (callPackage ../applications/emulators/wine/packages.nix (flags // info)).${wineBuild};
+
+    mkVariants = flags: lib.mapAttrs (_: mkWine flags) (removeAttrs releaseInfo [ "yabridge" ]);
+
+    bases = mkVariants baseFlags;
+    fulls = mkVariants fullFlags;
+
   in
-  {
+  bases
+  // lib.mapAttrs' (name: pkg: lib.nameValuePair "${name}Full" pkg) fulls
+  // {
     inherit callPackage wineBuild;
 
     fonts = callPackage ../applications/emulators/wine/fonts.nix { };
-    minimal = callPackage ../applications/emulators/wine {
-      wineRelease = config.wine.release or "stable";
-      inherit wineBuild;
-    };
 
-    base = self.minimal.override {
-      gettextSupport = true;
-      fontconfigSupport = stdenv.hostPlatform.isLinux;
-      alsaSupport = stdenv.hostPlatform.isLinux;
-      openglSupport = true;
-      vulkanSupport = true;
-      tlsSupport = true;
-      cupsSupport = true;
-      dbusSupport = stdenv.hostPlatform.isLinux;
-      cairoSupport = stdenv.hostPlatform.isLinux;
-      cursesSupport = true;
-      saneSupport = stdenv.hostPlatform.isLinux;
-      pulseaudioSupport = config.pulseaudio or stdenv.hostPlatform.isLinux;
-      udevSupport = stdenv.hostPlatform.isLinux;
-      xineramaSupport = stdenv.hostPlatform.isLinux;
-      sdlSupport = true;
-      mingwSupport = true;
-      usbSupport = true;
-      waylandSupport = stdenv.hostPlatform.isLinux;
-      x11Support = stdenv.hostPlatform.isLinux;
-      ffmpegSupport = true;
-    };
-
-    full = self.base.override {
-      gtkSupport = stdenv.hostPlatform.isLinux;
-      gstreamerSupport = true;
-      openclSupport = true;
-      odbcSupport = true;
-      netapiSupport = stdenv.hostPlatform.isLinux;
-      vaSupport = stdenv.hostPlatform.isLinux;
-      pcapSupport = true;
-      v4lSupport = stdenv.hostPlatform.isLinux;
-      gphoto2Support = true;
-      krb5Support = true;
-      embedInstallers = true;
-    };
-
-    stable = self.base.override { wineRelease = "stable"; };
-    stableFull = self.full.override { wineRelease = "stable"; };
-
-    unstable = self.base.override { wineRelease = "unstable"; };
-    unstableFull = self.full.override { wineRelease = "unstable"; };
-
-    staging = self.base.override { wineRelease = "staging"; };
-    stagingFull = self.full.override { wineRelease = "staging"; };
-
-    wayland = self.base.override {
-      x11Support = false;
-    };
-    waylandFull = self.full.override {
-      x11Support = false;
-    };
+    minimal = mkWine { } releaseInfo.${defaultRelease};
+    base = bases.${defaultRelease};
+    full = fulls.${defaultRelease};
 
     yabridge =
       let
-        yabridge = self.base.override { wineRelease = "yabridge"; };
+        yabridge = (mkWine baseFlags releaseInfo.yabridge).overrideAttrs (old: {
+          env = old.env // {
+            NIX_CFLAGS_COMPILE = "-std=gnu17";
+          };
+        });
       in
       if wineBuild == "wineWow" then yabridge else lib.dontDistribute yabridge;
   }
