f47d3082d8371f207060bea28646e83eade94acf
rwxd <git@rwxd.de>
2026-02-04 10:50:22 +0100

    maintainers: add rwxd

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23171,6 +23171,12 @@
     githubId = 12877905;
     name = "Roman Volosatovs";
   };
+  rwxd = {
+    email = "git@rwxd.de";
+    github = "rwxd";
+    githubId = 40308458;
+    name = "rwxd";
+  };
   rxiao = {
     email = "ben.xiao@me.com";
     github = "benxiao";
