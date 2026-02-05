b9f627477a79ced3b28d0834483aed9b49f7486f
zowie <git@zowie.cx>
2026-02-05 22:02:52 +0100

    [backport 25.11] dolphin: fix crash when connecting to samba
    There's a bug in KDE Dolphin before v25.11.80 that causes it to crash when
    connecting to samba shares (and potentially in other circumstances,
    see [bug 451050](https://bugs.kde.org/show_bug.cgi?id=451050).
    
    As mentioned in the bug report commit [d0f8985b4](https://invent.kde.org/system/dolphin/-/commit/d0f8985b4c5c790781be6fcd06d299f087e78756) fixes it.

--- pkgs/kde/gear/dolphin/default.nix
+++ pkgs/kde/gear/dolphin/default.nix
@@ -4,6 +4,9 @@
 }:
 mkKdeDerivation {
   pname = "dolphin";
-
+  patches = [
+    # backported fix for https://bugs.kde.org/show_bug.cgi?id=451050
+    ./dolphin-samba-crash-fix.patch
+  ];
   extraBuildInputs = [ qtmultimedia ];
 }
