2f34dff79ab36714571f79b87090c7eca78e52d0
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-05 22:34:31 +0000

    easyeffects: 8.0.9 -> 8.1.2
    (cherry picked from commit b44a7fc8ff9c4724f8d66bf3cbf822dfcc44813a)

--- pkgs/by-name/ea/easyeffects/package.nix
+++ pkgs/by-name/ea/easyeffects/package.nix
@@ -59,13 +59,13 @@ in
 
 stdenv.mkDerivation rec {
   pname = "easyeffects";
-  version = "8.0.9";
+  version = "8.1.2";
 
   src = fetchFromGitHub {
     owner = "wwmm";
     repo = "easyeffects";
     tag = "v${version}";
-    hash = "sha256-cFMbeJeEIDP7uiNi+rRKErgHtjP/PbPKASo+M2qogZQ=";
+    hash = "sha256-Tz14pjI5pNJOQH0KFaf9mJkFdup1GVxlkMnzVQusx/M=";
   };
 
   patches = [ ./qmlmodule-fix.patch ];
