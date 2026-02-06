292c4fd81c1dc0027c4260f7c393e1f01ca2f91d
Sefa Eyeoglu <contact@scrumplex.net>
2026-02-06 21:48:27 +0100

    fetchCrate: make overridable
    Allows overriding input attributes.
    
    This should make overriding Rust derivations a little easier.
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- pkgs/build-support/rust/fetchcrate.nix
+++ pkgs/build-support/rust/fetchcrate.nix
@@ -3,35 +3,36 @@
   fetchzip,
   fetchurl,
 }:
-
-{
-  crateName ? args.pname,
-  pname ? null,
-  # The `dl` field of the registry's index configuration
-  # https://doc.rust-lang.org/cargo/reference/registry-index.html#index-configuration
-  registryDl ? "https://crates.io/api/v1/crates",
-  version,
-  unpack ? true,
-  ...
-}@args:
-
-assert pname == null || pname == crateName;
-
-(if unpack then fetchzip else fetchurl) (
+lib.makeOverridable (
   {
-    name = "${crateName}-${version}.tar.gz";
-    url = "${registryDl}/${crateName}/${version}/download";
+    crateName ? args.pname,
+    pname ? null,
+    # The `dl` field of the registry's index configuration
+    # https://doc.rust-lang.org/cargo/reference/registry-index.html#index-configuration
+    registryDl ? "https://crates.io/api/v1/crates",
+    version,
+    unpack ? true,
+    ...
+  }@args:
 
-    passthru = { inherit pname version; };
-  }
-  // lib.optionalAttrs unpack {
-    extension = "tar.gz";
-  }
-  // removeAttrs args [
-    "crateName"
-    "pname"
-    "registryDl"
-    "version"
-    "unpack"
-  ]
+  assert pname == null || pname == crateName;
+
+  (if unpack then fetchzip else fetchurl) (
+    {
+      name = "${crateName}-${version}.tar.gz";
+      url = "${registryDl}/${crateName}/${version}/download";
+
+      passthru = { inherit pname version; };
+    }
+    // lib.optionalAttrs unpack {
+      extension = "tar.gz";
+    }
+    // removeAttrs args [
+      "crateName"
+      "pname"
+      "registryDl"
+      "version"
+      "unpack"
+    ]
+  )
 )
