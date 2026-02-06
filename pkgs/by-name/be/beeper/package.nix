f438287f9f2088468b59d84a374041dd729f6123
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-02-05 16:38:38 -0800

    beeper: fix debug logging
    - add NODE_ENV and ELECTRON_ENABLE_LOGGING flags

--- pkgs/by-name/be/beeper/package.nix
+++ pkgs/by-name/be/beeper/package.nix
@@ -48,7 +48,9 @@ appimageTools.wrapAppImage {
     . ${makeWrapper}/nix-support/setup-hook
     wrapProgram $out/bin/beeper \
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}} --no-update" \
-      --set APPIMAGE beeper
+      --set APPIMAGE beeper \
+      --set NODE_ENV production \
+      --set ELECTRON_ENABLE_LOGGING false \
   '';
 
   passthru = {
