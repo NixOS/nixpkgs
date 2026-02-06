abbdd7df18afbc7fd1f8d5d625945aa9d01957a9
Zachary Arnaise <121795280+zacharyarnaise@users.noreply.github.com>
2026-02-06 16:00:48 +0100

    ly: 1.3.1 -> 1.3.2
    (cherry picked from commit 51f80dff2cd26941b456a97fe80631bdbcdbffa1)

--- pkgs/by-name/ly/ly/package.nix
+++ pkgs/by-name/ly/ly/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "ly";
-  version = "1.3.1";
+  version = "1.3.2";
 
   src = fetchFromCodeberg {
     owner = "fairyglade";
     repo = "ly";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-BelsR/+sfm3qdEnyf4bbadyzuUVvVPrPEhdZaNPLxiE=";
+    hash = "sha256-P0yLiRIA0bDMiYfL6Kz2/OXh+nmnbHZnsCbcYGIGnbc=";
   };
 
   nativeBuildInputs = [
