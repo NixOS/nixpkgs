c7ab2f909ff3b0bf1c4113bd1768cfbce250d102
just-written <>
2026-02-06 19:06:01 -0700

    marktext-tkaixiang: Change to source build and fix licence.

--- pkgs/by-name/ma/marktext-tkaixiang/package.nix
+++ pkgs/by-name/ma/marktext-tkaixiang/package.nix
@@ -1,8 +1,14 @@
-{
-  lib,
-  appimageTools,
-  fetchurl,
-  makeDesktopItem,
+{ lib
+, buildNpmPackage
+, fetchFromGitHub
+, makeDesktopItem
+, electron
+, makeWrapper
+, nodejs_22
+, libsecret
+, pkg-config
+, xorg
+, libxkbfile
 }:
 let
   desktopItem = makeDesktopItem {
@@ -18,27 +24,78 @@ let
     ];
   };
 in
-appimageTools.wrapType2 {
+buildNpmPackage rec {
   pname = "marktext";
   version = "0.18.6";
 
-  src = fetchurl {
-    url = "https://github.com/Tkaixiang/marktext/releases/download/v0.18.6/marktext-linux-0.18.6.AppImage";
-    sha256 = "16asgv3laqi2wz22z3bi0ijic2nf20b3bi3srrdrwq2y3hyf0h7h";
+  src = fetchFromGitHub {
+    owner = "Tkaixiang";
+    repo = "marktext";
+    rev = "v${version}";
+    hash = "sha256-JWGwW5+BtXPPr3DhXSlPNUCGUY2UJnTuRlOLvG2QpD0=";
   };
 
-  extraInstallCommands = ''
+  npmDepsHash = "sha256-AwmnvCSIA6+5QShGQmngu01MS5NXbR0b18N5ocEfdqI=";
+
+  makeCacheWritable = true;
+  npmInstallFlags = [ "--ignore-scripts" ];
+  npmRebuildFlags = [ "--ignore-scripts" ];
+  nodejs = nodejs_22;
+
+  ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+  ELECTRON_OVERRIDE_DIST_PATH = "${electron}/libexec/electron";
+
+  nativeBuildInputs = [
+    makeWrapper
+    pkg-config
+    nodejs_22.pkgs.asar
+  ];
+
+  buildInputs = [
+    libsecret
+    xorg.libX11
+    libxkbfile
+  ];
+
+# Package lock in repo doesn't work
+  postPatch = ''
+      rm package-lock.json
+      cp ${./package-lock.json} package-lock.json
+    '';
+
+  buildPhase = ''
+    npm run build:linux -- --linux --dir \
+      --config.electronDist=${electron}/libexec/electron \
+      --config.electronVersion=${electron.version}
+  '';
+
+installPhase = ''
+  mkdir -p $out/lib/marktext
+  cp -r dist/linux-unpacked/* $out/lib/marktext/
+
+# Patch the process.resourcesPath
+  asar extract $out/lib/marktext/resources/app.asar patched-asar
+  sed -i \
+    "s#process\\.resourcesPath#'$out/lib/marktext/resources'#g" \
+    patched-asar/out/main/index.js
+  asar pack patched-asar $out/lib/marktext/resources/app.asar
+
+  mkdir -p $out/bin
+  makeWrapper ${electron}/bin/electron $out/bin/marktext \
+    --add-flags "--app=$out/lib/marktext/resources/app.asar"\
+    --chdir $out/lib/marktext/resources \
+
     install -Dm444 \
         ${desktopItem}/share/applications/*.desktop \
         $out/share/applications/marktext.desktop
-  '';
+'';
 
-  meta = with lib; {
-    description = "Unofficial MarkText fork maintained by Tkaixiang (AppImage)";
+    meta = with lib; {
+    description = "Modern Markdown Editor (fork by tkaixiang)";
     homepage = "https://github.com/Tkaixiang/marktext";
-    license = licenses.unfreeRedistributable;
-    platforms = [ "x86_64-linux" ];
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    license = licenses.mit;
+    platforms = platforms.linux;
     mainProgram = "marktext";
   };
 }
+
