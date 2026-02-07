8b4b6f5977616178ead858442cf18ccc4884f412
transcaffeine <transcaffeine@finally.coffee>
2026-02-07 16:24:08 +0100

    netbox_4_3: remove end of life package

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1070,7 +1070,6 @@ in
   neo4j = runTest ./neo4j.nix;
   netbird = runTest ./netbird.nix;
   netbox-upgrade = runTest ./web-apps/netbox-upgrade.nix;
-  netbox_4_3 = handleTest ./web-apps/netbox/default.nix { netbox = pkgs.netbox_4_3; };
   netbox_4_4 = handleTest ./web-apps/netbox/default.nix { netbox = pkgs.netbox_4_4; };
   netbox_4_5 = handleTest ./web-apps/netbox/default.nix { netbox = pkgs.netbox_4_5; };
   netdata = runTest ./netdata.nix;
