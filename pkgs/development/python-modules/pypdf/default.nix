1a7ab6096722ac942ce721a291ca2d7d86a1b99f
Robert Schütz <nix@dotlambda.de>
2026-02-08 22:44:05 -0800

    python3Packages.pypdf: 6.6.2 -> 6.7.0
    Diff: https://github.com/py-pdf/pypdf/compare/6.6.2...6.7.0
    
    Changelog: https://github.com/py-pdf/pypdf/blob/6.7.0/CHANGELOG.md
    (cherry picked from commit 49ffb4166b45baf31379814238224a35d9472ba3)

--- pkgs/development/python-modules/pypdf/default.nix
+++ pkgs/development/python-modules/pypdf/default.nix
@@ -27,7 +27,7 @@
 
 buildPythonPackage rec {
   pname = "pypdf";
-  version = "6.6.2";
+  version = "6.7.0";
   pyproject = true;
 
   disabled = pythonOlder "3.8";
@@ -38,7 +38,7 @@ buildPythonPackage rec {
     tag = version;
     # fetch sample files used in tests
     fetchSubmodules = true;
-    hash = "sha256-5XgxfVtSh4Q8FksGp7dXk+RCGQi0oqg0jpdn9K/gFq8=";
+    hash = "sha256-Kd5jBsq6sE5qWdIieVWdAKFA3QiDRsTBwoFerNY9ZRU=";
   };
 
   outputs = [
