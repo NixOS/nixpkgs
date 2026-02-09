ccfdecd136bab540d5497af1271866d69f100211
Josh Godsiff <josh.godsiff@corro.co>
2026-02-10 10:19:24 +1100

    smithy-cli: Remove let ... in definitions of upstream packages

--- pkgs/by-name/sm/smithy-cli/package.nix
+++ pkgs/by-name/sm/smithy-cli/package.nix
@@ -8,38 +8,13 @@
   writeText,
 }:
 
-let
-  sources = {
-    x86_64-linux = {
-      platform = "linux-x86_64";
-      hash = "sha256-/qCxXC4uUfg10R5MeR7DrvUvAeTPJRt8z47SsAjN424=";
-    };
-    aarch64-linux = {
-      platform = "linux-aarch64";
-      hash = "sha256-8nat7tp9extvAU0VWIKQ88SGJC/lbt0G0NlIveteh1o=";
-    };
-    x86_64-darwin = {
-      platform = "darwin-x86_64";
-      hash = "sha256-K6v0o7xkRevwz47Y0PhQhFemzXvOp7Y0ggMRY9kS+iw=";
-    };
-    aarch64-darwin = {
-      platform = "darwin-aarch64";
-      hash = "sha256-XPFkVPpQjt2f6ifZxGbSTtmAltWpWdqPWlEkijvLio4=";
-    };
-  };
-
-  source =
-    sources.${stdenvNoCC.hostPlatform.system}
-      or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
-in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "smithy-cli";
   version = "1.67.0";
 
-  src = fetchzip {
-    url = "https://github.com/smithy-lang/smithy/releases/download/${finalAttrs.version}/smithy-cli-${source.platform}.zip";
-    hash = source.hash;
-  };
+  src =
+    finalAttrs.passthru.sources.${stdenvNoCC.hostPlatform.system}
+      or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
 
   dontConfigure = true;
   dontBuild = true;
@@ -62,13 +37,32 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     runHook postInstall
   '';
 
-  passthru.tests = {
-    version = testers.testVersion {
-      package = finalAttrs.finalPackage;
+  passthru = {
+    sources = {
+      "x86_64-linux" = fetchzip {
+        url = "https://github.com/smithy-lang/smithy/releases/download/${finalAttrs.version}/smithy-cli-linux-x86_64.zip";
+        hash = "sha256-/qCxXC4uUfg10R5MeR7DrvUvAeTPJRt8z47SsAjN424=";
+      };
+      "aarch64-linux" = fetchzip {
+        url = "https://github.com/smithy-lang/smithy/releases/download/${finalAttrs.version}/smithy-cli-linux-aarch64.zip";
+        hash = "sha256-8nat7tp9extvAU0VWIKQ88SGJC/lbt0G0NlIveteh1o=";
+      };
+      "x86_64-darwin" = fetchzip {
+        url = "https://github.com/smithy-lang/smithy/releases/download/${finalAttrs.version}/smithy-cli-darwin-x86_64.zip";
+        hash = "sha256-K6v0o7xkRevwz47Y0PhQhFemzXvOp7Y0ggMRY9kS+iw=";
+      };
+      "aarch64-darwin" = fetchzip {
+        url = "https://github.com/smithy-lang/smithy/releases/download/${finalAttrs.version}/smithy-cli-darwin-aarch64.zip";
+        hash = "sha256-XPFkVPpQjt2f6ifZxGbSTtmAltWpWdqPWlEkijvLio4=";
+      };
     };
-    validate =
-      let
-        sampleModel = writeText "example.smithy" ''
+
+    tests = {
+      version = testers.testVersion {
+        package = finalAttrs.finalPackage;
+      };
+      validate = runCommand "smithy-cli-validate-test" { } ''
+        ${lib.getExe finalAttrs.finalPackage} validate ${writeText "example.smithy" ''
           $version: "2.0"
 
           namespace example
@@ -92,12 +86,10 @@ stdenvNoCC.mkDerivation (finalAttrs: {
               @required
               name: String
           }
-        '';
-      in
-      runCommand "smithy-cli-validate-test" { } ''
-        ${lib.getExe finalAttrs.finalPackage} validate ${sampleModel}
+        ''}
         touch $out
       '';
+    };
   };
 
   meta = {
@@ -108,6 +100,6 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     license = lib.licenses.asl20;
     mainProgram = "smithy";
     maintainers = [ lib.maintainers.joshgodsiff ];
-    platforms = lib.attrNames sources;
+    platforms = lib.attrNames finalAttrs.passthru.sources;
   };
 })
