9010fc2d900af100fec1ddb7388455f380cad1ca
René Neumann <necoro@necoro.eu>
2026-02-09 14:58:59 +0100

    unpaper: Depend on a barebone version of ffmpeg to reduce closure size

--- pkgs/by-name/un/unpaper/package.nix
+++ pkgs/by-name/un/unpaper/package.nix
@@ -18,6 +18,81 @@
   # tests
   nixosTests,
 }:
+let
+  ffmpeg-bare =
+    (ffmpeg-headless.override {
+      # explicitly required
+      buildAvcodec = true;
+      buildAvformat = true;
+      buildAvutil = true;
+      withPixelutils = true;
+
+      # don't harm, left at default
+      # withBzlib
+      # withIconv
+      # withLzma
+      # withZlib
+      # all the build options
+
+      # rest, defaulting to withHeadlessDeps, are disabled
+      withAlsa = false;
+      withAmf = false;
+      withAom = false;
+      withAss = false;
+      withBluray = false;
+      withCudaLLVM = false;
+      withCuvid = false;
+      withDav1d = false;
+      withDrm = false;
+      withNvcodec = false;
+      withFontconfig = false;
+      withFreetype = false;
+      withFribidi = false;
+      withGmp = false;
+      withGnutls = false;
+      withHarfbuzz = false;
+      withMp3lame = false;
+      withOpenapv = false;
+      withOpencl = false;
+      withOpenjpeg = false;
+      withOpenmpt = false;
+      withOpus = false;
+      withRist = false;
+      withSoxr = false;
+      withSpeex = false;
+      withSrt = false;
+      withSsh = false;
+      withSvtav1 = false;
+      withTheora = false;
+      withV4l2 = false;
+      withVaapi = false;
+      withVidStab = false;
+      withVorbis = false;
+      withVpx = false;
+      withVulkan = false;
+      withWebp = false;
+      withX264 = false;
+      withX265 = false;
+      withXml2 = false;
+      withXvid = false;
+      withZimg = false;
+      withZvbi = false;
+      withNetwork = false;
+      buildFfmpeg = false;
+      buildFfprobe = false;
+      buildAvdevice = false;
+      buildAvfilter = false;
+      buildSwresample = false;
+      buildSwscale = false;
+      withManPages = false;
+      withHtmlDoc = false;
+      withPodDoc = false;
+      withTxtDoc = false;
+    })
+    # tests do not run anymore in this stripped down version
+    .overrideAttrs
+      { doCheck = false; };
+in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "unpaper";
@@ -42,7 +117,7 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   buildInputs = [
-    ffmpeg-headless
+    ffmpeg-bare
   ];
 
   passthru.tests = {
