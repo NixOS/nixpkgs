e01f73abc135652bbde1930eda98e9463fc6b106
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-10 00:25:01 +0000

    snd: 26.0 -> 26.1

--- pkgs/by-name/sn/snd/package.nix
+++ pkgs/by-name/sn/snd/package.nix
@@ -15,11 +15,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "snd";
-  version = "26.0";
+  version = "26.1";
 
   src = fetchurl {
     url = "mirror://sourceforge/snd/snd-${finalAttrs.version}.tar.gz";
-    hash = "sha256-8b1jyf/6Jo/0NVy+Zvwb4gJ48j5Z94JFtVMgYB03CYc=";
+    hash = "sha256-NRVrgOR5tqDJvaclKbbNQOPiJtVFuCE+H4/EnK23TmY=";
   };
 
   nativeBuildInputs = [
