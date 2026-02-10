4ac69145f21c06c95789393c54958eb6c99c6aee
Nick Cao <nickcao@nichi.co>
2026-02-09 20:24:44 -0500

    telegram-desktop: 6.4.1 -> 6.5.1
    Diff: https://github.com/telegramdesktop/tdesktop/compare/v6.4.1...v6.5.1
    
    Changelog: https://github.com/telegramdesktop/tdesktop/releases/tag/v6.5.1
    (cherry picked from commit 724f4d0f620ce021b04be4bebf56846a3c3a880c)

--- pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
+++ pkgs/applications/networking/instant-messengers/telegram/telegram-desktop/unwrapped.nix
@@ -42,14 +42,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "telegram-desktop-unwrapped";
-  version = "6.4.1";
+  version = "6.5.1";
 
   src = fetchFromGitHub {
     owner = "telegramdesktop";
     repo = "tdesktop";
     rev = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-jmUj0kTyok5CsOe/to6bTN3jH/RMF7jE+AnFJi2nW90=";
+    hash = "sha256-y2sf2wDJ6dYxRdDLKIr78z9tPBIoh2WICg4zJBmb85Q=";
   };
 
   nativeBuildInputs = [
