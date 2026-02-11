c76c164599728832e26dfbd39a1f1e8edfee90a7
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-11 05:31:03 +0000

    minder: 2.0.5 -> 2.0.6

--- pkgs/by-name/mi/minder/package.nix
+++ pkgs/by-name/mi/minder/package.nix
@@ -23,13 +23,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "minder";
-  version = "2.0.5";
+  version = "2.0.6";
 
   src = fetchFromGitHub {
     owner = "phase1geo";
     repo = "minder";
     tag = finalAttrs.version;
-    hash = "sha256-g1rz7yihbMtSvL3B9XTqtOEjjLP+DczOTCp47Cp9GHs=";
+    hash = "sha256-wPCj35zVn/WhSl7yb4mPOqt9zPnDtDhcvYBn1pCMdT8=";
   };
 
   nativeBuildInputs = [
