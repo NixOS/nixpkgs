a9ca3d05789862c76b80f653acdf7fbb11eaf623
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-02-12 18:26:26 +0100

    throne: 1.0.13 -> 1.1.0

--- pkgs/by-name/th/throne/package.nix
+++ pkgs/by-name/th/throne/package.nix
@@ -9,7 +9,7 @@
 
   protobuf,
   protoc-gen-go,
-  protorpc,
+  protoc-gen-go-grpc,
 
   cmake,
   copyDesktopItems,
@@ -19,23 +19,20 @@
 
   # override if you want to have more up-to-date rulesets
   throne-srslist ? fetchurl {
-    url = "https://raw.githubusercontent.com/throneproj/routeprofiles/0fca735ff2759422c407ac04fac819aef2fc88f9/srslist.h";
-    hash = "sha256-G2WUStxFtN0fbZm/KoD9ldUvkMWf9cDA+9fvYt8dcqo=";
+    url = "https://raw.githubusercontent.com/throneproj/routeprofiles/f84a0c9fbb927fc05c342c12f8d9b6285d3005cd/srslist.h";
+    hash = "sha256-VUU3wlSImUXn4GdEiMOfFI9nT1jkIWf1c/y3oEvWIXU=";
   },
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "throne";
-  version = "1.0.13";
+  version = "1.1.0";
 
   src = fetchFromGitHub {
     owner = "throneproj";
     repo = "Throne";
-    # the release CI job was triggered on the xhttp branch (https://github.com/throneproj/Throne/actions/runs/20588046213),
-    # but the 1.0.13 tag was wrongly created on the dev branch
-    # we'll use the revision that was used for the job as well
-    rev = "3b737ec8cf29e03e4b7d5a09b1f502bdb8ef52e2";
-    hash = "sha256-OVgmhiKL4BaFYBeUqIX3LRNa54zq5oYyNMUYwKNvo1A=";
+    tag = finalAttrs.version;
+    hash = "sha256-r6Th/BykaAyvJrO828Bf4LMq/zFXvlvSQ/MFbnvnVHM=";
   };
 
   strictDeps = true;
@@ -54,15 +51,12 @@ stdenv.mkDerivation (finalAttrs: {
 
   env.INPUT_VERSION = finalAttrs.version;
 
-  cmakeFlags = [
-    # makes sure the app uses the user's config directory to store it's non-static content
-    # it's essentially the same as always setting the -appdata flag when running the program
-    (lib.cmakeBool "NKR_PACKAGE" true)
-  ];
+  # suppress errors in 3rdparty/simple-protobuf
+  env.NIX_CFLAGS_COMPILE = "-Wno-error=maybe-uninitialized";
 
   patches = [
-    # disable suid request as it cannot be applied to throne-core in nix store
-    # and prompt users to use NixOS module instead. And use throne-core from PATH
+    # disable suid request as it cannot be applied to ThroneCore in nix store
+    # and prompt users to use NixOS module instead. And use ThroneCore from PATH
     # to make use of security wrappers
     ./nixos-disable-setuid-request.patch
   ];
@@ -71,16 +65,19 @@ stdenv.mkDerivation (finalAttrs: {
     ln -s ${throne-srslist} ./srslist.h
   '';
 
+  # we'll wrap manually
+  dontWrapQtApps = true;
+
   installPhase = ''
     runHook preInstall
 
     install -Dm755 Throne -t "$out/share/throne/"
     install -Dm644 "$src/res/public/Throne.png" -t "$out/share/icons/hicolor/512x512/apps/"
 
-    mkdir -p "$out/bin"
-    ln -s "$out/share/throne/Throne" "$out/bin/"
+    makeQtWrapper "$out/share/throne/Throne" "$out/bin/Throne" \
+      --append-flag "-appdata" # use writable config dir
 
-    ln -s ${finalAttrs.passthru.core}/bin/Core "$out/share/throne/Core"
+    ln -s ${finalAttrs.passthru.core}/bin/ThroneCore "$out/share/throne/ThroneCore"
 
     runHook postInstall
   '';
@@ -108,18 +105,18 @@ stdenv.mkDerivation (finalAttrs: {
     ];
 
     proxyVendor = true;
-    vendorHash = "sha256-cPo/2bUXEF9jomr0Pnty7ZutAaC0TFG397FSIqefrjw=";
+    vendorHash = "sha256-qVvBjYMO6lSvFSFgLlrl1pvbavuaeo14Rcr1YY6b2Ao=";
 
     nativeBuildInputs = [
       protobuf
       protoc-gen-go
-      protorpc
+      protoc-gen-go-grpc
     ];
 
     # taken from script/build_go.sh
     preBuild = ''
       pushd gen
-      protoc -I . --go_out=. --protorpc_out=. libcore.proto
+      protoc -I . --go_out=. --go-grpc_out=. libcore.proto
       popd
 
       VERSION_SINGBOX=$(go list -m -f '{{.Version}}' github.com/sagernet/sing-box)
@@ -130,6 +127,9 @@ stdenv.mkDerivation (finalAttrs: {
     ldflags = [
       "-w"
       "-s"
+      "-X"
+      "internal/godebug.defaultGODEBUG=multipathtcp=0"
+      "-checklinkname=0"
     ];
 
     tags = [
@@ -140,10 +140,12 @@ stdenv.mkDerivation (finalAttrs: {
       "with_utls"
       "with_dhcp"
       "with_tailscale"
+      "badlinkname"
+      "tfogo_checklinkname"
     ];
   };
 
-  # this tricks nix-update into also updating the vendorHash of throne-core
+  # this tricks nix-update into also updating the vendorHash of passthru.core
   passthru.goModules = finalAttrs.passthru.core.goModules;
 
   meta = {
