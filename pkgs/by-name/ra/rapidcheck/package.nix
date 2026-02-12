503020937833af8105420ee52fa1adead094e70d
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-12 05:38:36 +0000

    rapidcheck: 0-unstable-2023-12-14 -> 0-unstable-2026-02-11

--- pkgs/by-name/ra/rapidcheck/package.nix
+++ pkgs/by-name/ra/rapidcheck/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "rapidcheck";
-  version = "0-unstable-2023-12-14";
+  version = "0-unstable-2026-02-11";
 
   src = fetchFromGitHub {
     owner = "emil-e";
     repo = "rapidcheck";
-    rev = "ff6af6fc683159deb51c543b065eba14dfcf329b";
-    hash = "sha256-Ixz5RpY0n8Un/Pv4XoTfbs40+70iyMbkQUjDqoLaWOg=";
+    rev = "b96a4e626ef4c7348dcd16c500353c2f997a9f3f";
+    hash = "sha256-h7Q1Lczqsrifwl5w9TDAlDjJhbroc1W4O3icLRvZwaM=";
   };
 
   outputs = [
