4ed71b972d6836e255e16cf0ffe0eaf8062f8a58
Sigmanificient <edhyjox@gmail.com>
2026-02-12 22:21:53 +0100

    kops: modernize

--- pkgs/applications/networking/cluster/kops/default.nix
+++ pkgs/applications/networking/cluster/kops/default.nix
@@ -12,21 +12,21 @@ in
 {
   inherit mkKops;
 
-  kops_1_31 = mkKops rec {
+  kops_1_31 = mkKops (finalAttrs: {
     version = "1.31.0";
     sha256 = "sha256-q9megrNXXKJ/YqP/fjPHh8Oji4dPK5M3HLHa+ufwRAM=";
-    rev = "v${version}";
-  };
+    rev = "v${finalAttrs.version}";
+  });
 
-  kops_1_32 = mkKops rec {
+  kops_1_32 = mkKops (finalAttrs: {
     version = "1.32.1";
     sha256 = "sha256-nQKeTDajtUffPBhPrPuaJ+1XWgLDUltwDQDZHkylys4=";
-    rev = "v${version}";
-  };
+    rev = "v${finalAttrs.version}";
+  });
 
-  kops_1_33 = mkKops rec {
+  kops_1_33 = mkKops (finalAttrs: {
     version = "1.33.0";
     sha256 = "sha256-VnnKWcU83yqsKW54Q1tr99/Ln8ppMyB7GLl70rUFGDY=";
-    rev = "v${version}";
-  };
+    rev = "v${finalAttrs.version}";
+  });
 }
