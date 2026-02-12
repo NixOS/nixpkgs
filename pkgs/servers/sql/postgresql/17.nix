d1ffd08bf5fdd0c322d38a1a755947a16167fe5c
Wolfgang Walther <walther@technowledgy.de>
2026-02-12 12:20:25 +0100

    postgresql: change source back to tags
    The tags for the recent minor version releases have now been pushed.
    
    (cherry picked from commit 77867fbc7bd1ea1da755970b014faec5ceba2d89)

--- pkgs/servers/sql/postgresql/17.nix
+++ pkgs/servers/sql/postgresql/17.nix
@@ -1,7 +1,6 @@
 import ./generic.nix {
   version = "17.8";
-  # rev = "refs/tags/REL_17_8";
-  rev = "6af885119b52a2a6229959670ba3ae5e36bf9806";
+  rev = "refs/tags/REL_17_8";
   hash = "sha256-4lV1/xRmMsc5rgY3RB6WMigTXHgHjh9bmR6nzL82Rs4=";
   muslPatches = {
     dont-use-locale-a = {
