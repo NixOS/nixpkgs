9498ed315662ad3308c2f5395f11da42df3f4ab7
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-13 08:47:46 +0000

    resterm: 0.21.3 -> 0.22.0

--- pkgs/by-name/re/resterm/package.nix
+++ pkgs/by-name/re/resterm/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "resterm";
-  version = "0.21.3";
+  version = "0.22.0";
 
   src = fetchFromGitHub {
     owner = "unkn0wn-root";
     repo = "resterm";
     tag = "v${finalAttrs.version}";
-    sha256 = "sha256-ix+9TblklF0PKzuSsI/qTqOH0la9K9wE84j/wJYJIM0=";
+    sha256 = "sha256-ePWzD1y6UuNcM0nz1TaonUC83whhCg7kpO01xnpki5g=";
   };
 
-  vendorHash = "sha256-E/Y4kW5xy7YamUP5bxFmDCAK6RqiqGN7DpEPG1MaCHc=";
+  vendorHash = "sha256-w2AvGD7F5GsLkQ3epph+tQXrE/13fnDK5HfEQ1pN2Mk=";
 
   subPackages = [ "cmd/resterm" ];
 
