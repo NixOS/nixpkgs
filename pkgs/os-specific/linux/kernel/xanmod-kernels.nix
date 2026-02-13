e37bd21499ee49aa1fae1737bbe968ab13341aec
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-13 19:06:55 +0000

    linuxKernel.kernels.linux_xanmod_latest: 6.18.8 -> 6.18.10

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -21,8 +21,8 @@ let
     };
     # ./update-xanmod.sh main
     main = {
-      version = "6.18.8";
-      hash = "sha256-BagAixl3Eo9vX6F/vpQv8OCw5vm8l7JtZBqvE0m5gAs=";
+      version = "6.18.10";
+      hash = "sha256-bYKYx9ctHuWG+WS/Wtt4p2uW6vy2g5ikLqSPWeNeSn0=";
     };
   };
 
