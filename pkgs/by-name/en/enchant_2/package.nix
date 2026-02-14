1eb86eb10660943b06bb90f810bc96dc8a634c11
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-14 05:09:49 +0000

    enchant: 2.6.9 -> 2.8.15

--- pkgs/by-name/en/enchant_2/package.nix
+++ pkgs/by-name/en/enchant_2/package.nix
@@ -25,7 +25,7 @@ assert withAppleSpell -> stdenv.hostPlatform.isDarwin;
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "enchant";
-  version = "2.6.9";
+  version = "2.8.15";
 
   outputs = [
     "out"
@@ -34,7 +34,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://github.com/rrthomas/enchant/releases/download/v${finalAttrs.version}/enchant-${finalAttrs.version}.tar.gz";
-    hash = "sha256-2aWhDcmzikOzoPoix27W67fgnrU1r/YpVK/NvUDv/2s=";
+    hash = "sha256-0/2eQXC/tRELC9pXf+dko4+2BrPCXS8MOEAjRSH/ElI=";
   };
 
   strictDeps = true;
