3a38132120ff5df230e9dc1c5a6fedeb9dd28d0c
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-14 20:45:23 +0000

    expr: 1.17.7 -> 1.17.8

--- pkgs/by-name/ex/expr/package.nix
+++ pkgs/by-name/ex/expr/package.nix
@@ -6,18 +6,18 @@
 
 buildGoModule (finalAttrs: {
   pname = "expr";
-  version = "1.17.7";
+  version = "1.17.8";
 
   src = fetchFromGitHub {
     owner = "expr-lang";
     repo = "expr";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-OKKbEgJgQWL5jP+E8ZuxdVjZ89QjLy6AAPRepe4jWsk=";
+    hash = "sha256-MJM7ezZtSdDaUewNGACOKvWc+ZOPVScTuW+d6n1K5jo=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/repl";
 
-  vendorHash = "sha256-NIcHf9P2/1Me+LuWA3BEjA2mOgdSzXFQJPrhAT7uPoo=";
+  vendorHash = "sha256-GH7rn0q/YuGBx0rrfHa2EMCsynQ3Pgtz1yDsD/NIKsU=";
 
   ldflags = [
     "-s"
