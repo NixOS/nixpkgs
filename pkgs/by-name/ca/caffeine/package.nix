667e5a751b493cd90ee368409bee7458854ad8a6
ollybeck <14896256+ollybeck@users.noreply.github.com>
2026-02-15 16:14:54 +0100

    caffeine: build from source (v1.1.4)

--- pkgs/by-name/ca/caffeine/package.nix
+++ pkgs/by-name/ca/caffeine/package.nix
@@ -1,28 +1,86 @@
 {
   lib,
-  stdenvNoCC,
-  fetchurl,
-  undmg,
+  stdenv,
+  fetchFromGitHub,
+  apple-sdk_15,
 }:
 
-stdenvNoCC.mkDerivation (finalAttrs: {
+let
+  executableName = "Caffeine";
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "caffeine";
   version = "1.1.4";
 
-  src = fetchurl {
-    url = "https://github.com/IntelliScape/caffeine/releases/download/${finalAttrs.version}/Caffeine.dmg";
-    hash = "sha256-GtNMMpmgyGaHPE/rQyw+ERhjda229DxfSBrp1G0G1yM=";
+  src = fetchFromGitHub {
+    owner = "IntelliScape";
+    repo = "caffeine";
+    tag = "${finalAttrs.version}";
+    sha256 = "sha256-AmBPY5ZVWBq2ZesNvvJ/Do5XgPjb5R1ESNJm7tx0M6k=";
   };
 
-  sourceRoot = ".";
+  buildInputs = [
+    apple-sdk_15
+  ];
 
-  nativeBuildInputs = [ undmg ];
+  NIX_CFLAGS_COMPILE = [
+    "-Wno-error=deprecated-declarations"
+  ];
+
+  postPatch = ''
+    substituteInPlace Info.plist \
+      --replace-fail \''${EXECUTABLE_NAME} ${executableName} \
+      --replace-fail $\(PRODUCT_BUNDLE_IDENTIFIER\) com.intelliscapesolutions.caffeine \
+      --replace-fail \''${PRODUCT_NAME} ${executableName}
+    # Update LSMinimumSystemVersion to 11.5 (based on value in upstream's pre-built app)
+    sed -Ei '/<key>LSMinimumSystemVersion<\/key>/{n; s/(<string>)10.6(<\/string>)/\111.5\2/}' Info.plist
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    $CC \
+      -ObjC \
+      -framework Cocoa \
+      -framework WebKit \
+      -framework IOKit \
+      -framework CoreServices \
+      -framework ApplicationServices \
+      -o ${executableName} \
+      main.m \
+      AppCategory.m \
+      AppDelegate.m \
+      ISToggleSwitch.m \
+      LCMenuIconView.m
+
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/Applications
-    cp -r *.app $out/Applications
+    APPDIR=$out/Applications/${executableName}.app
+
+    install -Dm755 ${executableName} "$APPDIR/Contents/MacOS/${executableName}"
+    install -Dm644 Info.plist "$APPDIR/Contents/Info.plist"
+
+    for resource in \
+      AccessibilityPermission.gif \
+      Caffeine.icns \
+      Caffeine.sdef \
+      active.png \
+      active@2x.png \
+      highlightactive.png \
+      highlightactive@2x.png \
+      highlighted.png \
+      highlighted@2x.png \
+      inactive.png \
+      inactive@2x.png
+    do
+      install -Dm644 "$resource" "$APPDIR/Contents/Resources/$resource"
+    done
+
+    find English.lproj -type f -exec install -Dm644 "{}" "$APPDIR/Contents/Resources/{}" \;
 
     runHook postInstall
   '';
@@ -36,6 +94,5 @@ stdenvNoCC.mkDerivation (finalAttrs: {
       "x86_64-darwin"
       "aarch64-darwin"
     ];
-    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
   };
 })
