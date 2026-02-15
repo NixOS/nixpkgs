02ab50add65e39d9ca27997b0fb2ed5ee8838cba
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-15 10:33:51 +0000

    goffice: 0.10.59 -> 0.10.60

--- pkgs/by-name/go/goffice/package.nix
+++ pkgs/by-name/go/goffice/package.nix
@@ -21,7 +21,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "goffice";
-  version = "0.10.59";
+  version = "0.10.60";
 
   outputs = [
     "out"
@@ -31,7 +31,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/goffice/${lib.versions.majorMinor finalAttrs.version}/goffice-${finalAttrs.version}.tar.xz";
-    hash = "sha256-sI9xczJVlLcfu+pHajC1sxIMPa3/XAom0UDk5SSRZiI=";
+    hash = "sha256-ARVDOMv/rBcvxTM467nVJ8gh/8IZhaLM+oSSPua3YcU=";
   };
 
   nativeBuildInputs = [
