4e052beb371872fc74f4a3a088bd5b3b56c26740
repparw <ubritos@gmail.com>
2026-02-15 11:16:48 -0300

    cfait: init at 0.4.9

---
+++ pkgs/by-name/cf/cfait/package.nix
@@ -0,0 +1,92 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitea,
+  pkg-config,
+  openssl,
+  sqlite,
+  libxkbcommon,
+  wayland,
+  libx11,
+  libxcursor,
+  libxrandr,
+  libxi,
+  makeWrapper,
+  testers,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "cfait";
+  version = "0.4.9";
+
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "trougnouf";
+    repo = "cfait";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-7cbJ5HEkGhjIOnIeBEasa5TVurXcSLuOB3rOMLFd+os=";
+  };
+
+  cargoHash = "sha256-2P9ybgIIvG7oaVOkvPaWNYyIMfa9PFlJbnLaGe4IYjc=";
+
+  nativeBuildInputs = [
+    pkg-config
+    makeWrapper
+  ];
+
+  buildInputs = [
+    openssl
+    sqlite
+    libxkbcommon
+    wayland
+    libx11
+    libxcursor
+    libxrandr
+    libxi
+  ];
+
+  buildFeatures = [ "gui" ];
+
+  cargoBuildFlags = [
+    "--bin"
+    "cfait"
+    "--bin"
+    "gui"
+  ];
+
+  postInstall = ''
+    mv $out/bin/gui $out/bin/cfait-gui
+  '';
+
+  postFixup = ''
+    wrapProgram $out/bin/cfait-gui \
+      --prefix LD_LIBRARY_PATH : "${
+        lib.makeLibraryPath [
+          libxkbcommon
+          wayland
+          libx11
+          libxcursor
+          libxrandr
+          libxi
+        ]
+      }"
+  '';
+
+  doCheck = false;
+
+  passthru = {
+    updateScript = nix-update-script { };
+    tests.version = testers.testVersion {
+      package = finalAttrs.finalPackage;
+    };
+  };
+
+  meta = {
+    description = "A powerful, fast and elegant task manager (CalDAV and local, GUI, TUI, and Android clients)";
+    homepage = "https://codeberg.org/trougnouf/cfait";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ repparw ];
+    mainProgram = "cfait";
+  };
+})
