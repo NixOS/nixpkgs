d934dd25670d94bf5c961d87a180420d791dc901
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-15 13:22:05 +0100

    python314Packages.watchfiles: migrate to finalAttrs

--- pkgs/development/python-modules/watchfiles/default.nix
+++ pkgs/development/python-modules/watchfiles/default.nix
@@ -11,7 +11,7 @@
   versionCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "watchfiles";
   version = "1.1.1";
   pyproject = true;
@@ -19,12 +19,12 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "samuelcolvin";
     repo = "watchfiles";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-UlQnCYSNU9H4x31KenSfYExGun94ekrOCwajORemSco=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
-    inherit pname src version;
+    inherit (finalAttrs) pname src version;
     hash = "sha256-6sxtH7KrwAWukPjLSMAebguPmeAHbC7YHOn1QiRPigs=";
   };
 
@@ -64,9 +64,9 @@ buildPythonPackage rec {
   meta = {
     description = "File watching and code reload";
     homepage = "https://watchfiles.helpmanual.io/";
-    changelog = "https://github.com/samuelcolvin/watchfiles/releases/tag/${src.tag}";
+    changelog = "https://github.com/samuelcolvin/watchfiles/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "watchfiles";
   };
-}
+})
