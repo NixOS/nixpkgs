c6200704e22ca15145dcd1841412c683edbca71e
WoutFontaine <WoutFontaine@users.noreply.github.com>
2026-02-15 03:48:01 +0100

    fluxer-bin: init at 0.0.8

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -28789,6 +28789,11 @@
     githubId = 28888242;
     name = "WORLDofPEACE";
   };
+  WoutFontaine = {
+    name = "Wout Fontaine";
+    github = "WoutFontaine";
+    githubId = 123456;
+  };
   WoutSwinkels = {
     name = "Wout Swinkels";
     email = "nixpkgs@woutswinkels.com";
---
+++ pkgs/by-name/fl/fluxer-bin/package.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  fetchurl,
+  appimageTools,
+}:
+
+let
+  pname = "fluxer-bin";
+  version = "0.0.8";
+
+  src = fetchurl {
+    url = "https://api.fluxer.app/dl/desktop/stable/linux/x64/fluxer-stable-${version}-x86_64.AppImage";
+    hash = "sha256-GdoBK+Z/d2quEIY8INM4IQy5tzzIBBM+3CgJXQn0qAw=";
+  };
+in
+
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  meta = with lib; {
+    description = "Fluxer desktop client";
+    homepage = "https://fluxer.app";
+    license = licenses.agpl3Only;
+    platforms = platforms.linux;
+    mainProgram = "fluxer-bin";
+    maintainers = with maintainers; [ ];
+  };
+}
