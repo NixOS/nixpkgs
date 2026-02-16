cd5d5828186b0bf5729aeec2967fe23172497d80
Zax71 <67716263+zax71@users.noreply.github.com>
2026-02-16 16:18:30 +0000

    maintainers: add Zax71

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -29697,6 +29697,11 @@
     githubId = 250877;
     name = "Elmar Athmer";
   };
+  zax71 = {
+    name = "Zax71";
+    github = "zax71";
+    githubId = 67716263;
+  };
   zazedd = {
     name = "Leonardo Santos";
     email = "leomendesantos@gmail.com";
