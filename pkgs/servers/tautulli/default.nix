aa624c77bd1fab123abe66f77bf448959b79e8bf
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2026-02-16 22:00:21 +0100

    tautulli: 2.16.0 -> 2.16.1
    (cherry picked from commit 216593833dcbf5158922282ac364d632cc9cd93b)

--- pkgs/servers/tautulli/default.nix
+++ pkgs/servers/tautulli/default.nix
@@ -9,7 +9,7 @@
 
 buildPythonApplication rec {
   pname = "Tautulli";
-  version = "2.16.0";
+  version = "2.16.1";
   pyproject = false;
 
   pythonPath = [ setuptools ];
@@ -22,7 +22,7 @@ buildPythonApplication rec {
     owner = "Tautulli";
     repo = "Tautulli";
     tag = "v${version}";
-    sha256 = "sha256-nqSqWRst+gx9aZ2Ko+/tKzpQX7wuU4Bn3vLR5F87aJA=";
+    sha256 = "sha256-Zct7EhnU5LROO23Joz6OxQTtC9uGZhtceSG+aX6MI2c=";
   };
 
   installPhase = ''
