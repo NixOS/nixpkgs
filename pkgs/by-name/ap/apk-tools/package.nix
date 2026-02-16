2fc99c5cc0da16121fffc16a2a12b78badcaf1d1
Alexander Sieg <alex@xanderio.de>
2026-02-16 17:52:24 +0100

    apk-tools: switch to meson build system

--- pkgs/by-name/ap/apk-tools/package.nix
+++ pkgs/by-name/ap/apk-tools/package.nix
@@ -3,6 +3,10 @@
   stdenv,
   fetchFromGitLab,
   pkg-config,
+  meson,
+  ninja,
+  python3,
+  cmocka,
   scdoc,
   openssl,
   zlib,
@@ -11,7 +15,7 @@
   lua5_3,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "apk-tools";
   version = "3.0.3";
 
@@ -19,41 +23,35 @@ stdenv.mkDerivation rec {
     domain = "gitlab.alpinelinux.org";
     owner = "alpine";
     repo = "apk-tools";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-ydqJiLkz80TQGyf9m/l8HSXfoTAvi0av7LHETk1c0GI=";
   };
 
   nativeBuildInputs = [
+    meson
+    ninja
     pkg-config
-    scdoc
+    python3
   ]
   ++ lib.optionals luaSupport [
     lua5_3
     lua5_3.pkgs.lua-zlib
   ];
+
   buildInputs = [
     openssl
     zlib
     zstd
+    scdoc
+    cmocka
   ]
   ++ lib.optional luaSupport lua5_3;
+
   strictDeps = true;
 
-  makeFlags = [
-    "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
-    "SBINDIR=$(out)/bin"
-    "LIBDIR=$(out)/lib"
-    "LUA=${if luaSupport then "lua" else "no"}"
-    "LUA_LIBDIR=$(out)/lib/lua/${lib.versions.majorMinor lua5_3.version}"
-    "MANDIR=$(out)/share/man"
-    "DOCDIR=$(out)/share/doc/apk"
-    "INCLUDEDIR=$(out)/include"
-    "PKGCONFIGDIR=$(out)/lib/pkgconfig"
-  ];
-
-  env.NIX_CFLAGS_COMPILE = toString [
-    "-Wno-error=unused-result"
-    "-Wno-error=deprecated-declarations"
+  mesonFlags = [
+    (lib.mesonEnable "lua" luaSupport)
+    (lib.mesonOption "lua_bin" "lua")
   ];
 
   enableParallelBuilding = true;
@@ -63,7 +61,6 @@ stdenv.mkDerivation rec {
     description = "Alpine Package Keeper";
     maintainers = [ ];
     license = lib.licenses.gpl2Only;
-    platforms = lib.platforms.linux;
     mainProgram = "apk";
   };
-}
+})
