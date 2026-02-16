422a2c3288f70fc8537a45b7926434aec7f2a462
ccicnce113424 <ccicnce113424@gmail.com>
2026-02-16 13:23:34 +0800

    splayer: 3.0.0-beta.9 -> 3.0.0
    (cherry picked from commit 070fbbc9778c001f0c352d1bb8608a8cd59aacb9)

--- pkgs/by-name/sp/splayer/package.nix
+++ pkgs/by-name/sp/splayer/package.nix
@@ -11,6 +11,8 @@
   cargo,
   rustc,
   python3,
+  pkg-config,
+  openssl,
   makeWrapper,
   copyDesktopItems,
   makeDesktopItem,
@@ -19,14 +21,14 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "splayer";
-  version = "3.0.0-beta.9";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "imsyy";
     repo = "SPlayer";
     tag = "v${finalAttrs.version}";
     fetchSubmodules = false;
-    hash = "sha256-+9F4ckATxRE+/PhMi5c1GVDq5V9QMOogCD9uT6QkREM=";
+    hash = "sha256-E29TJlp7nMokJbbi/YLuYf9qWmwvo/r4qQckKrVyumI=";
   };
 
   pnpm = pnpm_10;
@@ -39,7 +41,7 @@ stdenv.mkDerivation (finalAttrs: {
       pnpm
       ;
     fetcherVersion = 2;
-    hash = "sha256-tAOtrxQasIQ1IS2jKdcX4KEM5p3zhshqw8phzsj667Q=";
+    hash = "sha256-PTfZopse+9RS7qh0miLu3duYlWDfifZS254tZKqgxKk=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
@@ -48,7 +50,7 @@ stdenv.mkDerivation (finalAttrs: {
       version
       src
       ;
-    hash = "sha256-QKk1coOuZNaqKgvbBgorvOotHmTJ+YXTHBfyhF0L37E=";
+    hash = "sha256-gd/5f3yraTQI5bu1VE6HHsGDeKJLR1oTm2H+pg1PAOA=";
   };
 
   nativeBuildInputs = [
@@ -61,6 +63,11 @@ stdenv.mkDerivation (finalAttrs: {
     python3
     makeWrapper
     copyDesktopItems
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
   ];
 
   env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
@@ -86,12 +93,12 @@ stdenv.mkDerivation (finalAttrs: {
     for f in $(find . -path '*/node_modules/better-sqlite3' -type d); do
       (cd "$f" && (
       npm run build-release --offline --nodedir="${electron.headers}"
+      rm -rf build/Release/{.deps,obj,obj.target,test_extension.node}
       find build -type f -exec \
         ${lib.getExe removeReferencesTo} \
         -t "${electron.headers}" {} \;
       ))
     done
-    rm -rf build/Release/{.deps,obj,obj.target,test_extension.node}
 
     pnpm build
 
