480c5010dc70b7a6f4560c8695cbd01794086f20
Aarav Gupta <aarav@spikonado.com>
2026-02-17 16:40:48 +0530

    python313Packages.rosdistro: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16777,6 +16777,8 @@ self: super: with self; {
 
   rosbags = callPackage ../development/python-modules/rosbags { };
 
+  rosdistro = callPackage ../development/python-modules/rosdistro { };
+
   rospkg = callPackage ../development/python-modules/rospkg { };
 
   rotary-embedding-torch = callPackage ../development/python-modules/rotary-embedding-torch { };
