6d4401e1b6a45455d8e7198540911152c7f40f2d
Guy Chronister <guychronister@outlook.com>
2026-02-17 09:57:47 -0600

    openafs: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8220,8 +8220,6 @@ with pkgs;
 
   nsdiff = perlPackages.nsdiff;
 
-  openafs = callPackage ../servers/openafs/1.8 { };
-
   openresty = callPackage ../servers/http/openresty {
     zlib-ng = zlib;
     withPerl = false;
