d8bcd4850391e9fc882d5620a48d85be76bf9d1c
Colin <colin@uninsane.org>
2026-02-17 21:48:52 +0000

    fortify-headers: 1.1 -> 3.0.1

--- pkgs/by-name/fo/fortify-headers/package.nix
+++ pkgs/by-name/fo/fortify-headers/package.nix
@@ -2,35 +2,21 @@
   lib,
   stdenv,
   fetchurl,
+  gitUpdater,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fortify-headers";
-  version = "1.1";
+  version = "3.0.1";
 
   # upstream only accessible via git - unusable during bootstrap, hence
   # extract from GitHub release.
   src = fetchurl {
     url = "https://github.com/jvoisin/fortify-headers/archive/refs/tags/${finalAttrs.version}.tar.gz";
-    hash = "sha256-3WJbFTjsIeL7fuFTTEoTaPAyz6PQ2sLBK29mQ8YG+1Y=";
+    hash = "sha256-V2rB3C25pQPYRYwen0ps6LBDfPw8UHhZ12AaO42KwOY=";
   };
 
   patches = [
-    (fetchurl {
-      # Remove when updating to >= 3.0
-      url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/4adfa7e7291a716b871969b9d1146db037009b57/main/fortify-headers/0001-add-initial-clang-support.patch";
-      hash = "sha256-QbDnMTGVmvIokow6lkA+mDHFMDP8B0lLY8w9ciUhGKU=";
-    })
-    (fetchurl {
-      # Remove when updating to >= 3.0
-      url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/4adfa7e7291a716b871969b9d1146db037009b57/main/fortify-headers/0002-avoid-__extension__-with-clang.patch";
-      hash = "sha256-LooU8I5td7fsfXjBahawvZbhR7r9HTWlT7qGbPE88zY=";
-    })
-    (fetchurl {
-      # Remove when updating to >= 3.0
-      url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/4adfa7e7291a716b871969b9d1146db037009b57/main/fortify-headers/0003-Disable-wrapping-of-ppoll.patch";
-      hash = "sha256-NW46jwkKS/0hvC7WFaetbAyQxf/YLnyEV+Y8G9payCw=";
-    })
     ./wchar-imports-skip.patch
     ./restore-macros.patch
   ];
@@ -45,6 +31,10 @@ stdenv.mkDerivation (finalAttrs: {
 
     runHook postInstall
   '';
+
+  passthru.updateScript = gitUpdater {
+    url = "git://git.2f30.org/fortify-headers";
+  };
 
   meta = {
     description = "Standalone header-based fortify-source implementation";
