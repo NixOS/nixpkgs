7ac3c9e5eb3ab580bd3f5240dfad7251a1120112
Bobby Rong <rjl931189261@126.com>
2026-02-17 10:43:29 +0800

    gweather-locations: init at 2025.1
    Based on the libgweather packaging.
    
    The git tag is 2026.1 but meson.build still says 2025.1, this should be fixed in next bump I guess.

---
+++ pkgs/by-name/gw/gweather-locations/package.nix
@@ -0,0 +1,60 @@
+{
+  stdenvNoCC,
+  lib,
+  fetchurl,
+  makeWrapper,
+  gettext,
+  meson,
+  ninja,
+  python3,
+  buildPackages,
+  gnome,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "gweather-locations";
+  version = "2025.1";
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchurl {
+    url = "mirror://gnome/sources/gweather-locations/${lib.versions.major finalAttrs.version}/gweather-locations-${finalAttrs.version}.tar.xz";
+    hash = "sha256-VgnuBM9qxMx3IfMkG9jWn/6/DxtgIQ+t2IyGmkuITYc=";
+  };
+
+  strictDeps = true;
+
+  depsBuildBuild = [
+    makeWrapper
+  ];
+
+  nativeBuildInputs = [
+    gettext
+    meson
+    ninja
+    (python3.pythonOnBuildForHost.withPackages (ps: [ ps.pygobject3 ]))
+  ];
+
+  postPatch = ''
+    patchShebangs --build build-aux/gen_locations_variant.py
+    wrapProgram $PWD/build-aux/gen_locations_variant.py \
+      --prefix GI_TYPELIB_PATH : ${lib.getLib buildPackages.glib}/lib/girepository-1.0
+  '';
+
+  passthru = {
+    updateScript = gnome.updateScript {
+      packageName = "gweather-locations";
+    };
+  };
+
+  meta = {
+    description = "GWeather locations database";
+    homepage = "https://gitlab.gnome.org/GNOME/gweather-locations";
+    platforms = lib.platforms.unix;
+    license = lib.licenses.gpl2Plus;
+    teams = [ lib.teams.gnome ];
+  };
+})
