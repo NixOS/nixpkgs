b3c32531beb5e2202eace8c2dcc4c178375ead52
David Fairbrother <DavidFair@users.noreply.github.com>
2026-02-17 19:06:32 +0000

    qemu_ga: init -> Alias to qemu:ga target only
    Currently qemu_kvm includes a full build of the QEMU emulation binary. This includes docs, tools ...etc.
    and this results in a package that has 2466 dependencies, including SDL, x265, and various different
    libs from a range of operating systems:
    
    nix derivation show -r nixpkgs#qemu_kvm.ga | jq -r '.[] | .name' | uniq | sort | wc -l
    2466
    
    This means many people building headless or slim systems are also building a lot of extra tooling
    when they select qemu_kvm.ga (usually through the module).
    
    By putting in a dedicated top level targt for qemu_ga we make it easier for:
    - Compile time and bandwidth building/pulling this derivation's deps
    - Avoid building docs and tools nobody will look at
    - Make it easy to overlay for QEMU GA only if they do want to look at this
    
    Building this target instead results in ~750 dependencies now, where the equivalent can be checked with
    
    nix eval --raw --impure --expr \
    '(builtins.getFlake "github:nixos/nixpkgs/nixpkgs-unstable").legacyPackages."${builtins.currentSystem}".qemu_kvm.override {minimal=true; guestAgentSupport=true;}' | \
    xargs nix derivation show -r | jq -r '.[] | .name' | uniq | sort | wc -l
    751

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9077,6 +9077,12 @@ with pkgs;
     else
       unixtools.procps;
 
+  qemu_ga = lowPrio (
+    qemu.override {
+      minimal = true;
+      guestAgentSupport = true;
+    }
+  );
   qemu_kvm = lowPrio (qemu.override { hostCpuOnly = true; });
   qemu_full = lowPrio (
     qemu.override {
