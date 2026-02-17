b9ff1f05690aab512e621dd317fd7695f0f0ee2b
Colin <colin@uninsane.org>
2026-02-17 19:49:16 +0000

    fortify-headers: switch source from alpine to github
    the most recognized upstream for `fortify-headers` is
    <git://git.2f30.org/fortify-headers> (see `meta.homepage`, e.g.),
    but as per the previous source comment this upstream isn't accessible
    over http.
    
    <https://github.com/jvoisin/fortify-headers> is a reasonable
    alternative: it's what [chimera linux][Chimera] sources, and
    jvoisin is a regular committer to the `git.2f30.org` repo.
    moreover, Alpine Linux is stuck at version 1.1, released in 2019.
    moving away from Alpine (much as i appreciate their work),
    allows us to take updates more timely.
    
    cherry-pick Alpine's own patches, so that the `fortify-headers`
    built before and after this patch is bit-for-bit identical.
    Alpine's patches have been upstreamed, so they can go away with
    a future package bump (see next patch).
    
    Chimera: <https://github.com/chimera-linux/cports/blob/183ea38c47a0a74f78336164d975e652f622c6e5/main/fortify-headers/template.py#L11>

--- pkgs/by-name/fo/fortify-headers/package.nix
+++ pkgs/by-name/fo/fortify-headers/package.nix
@@ -4,28 +4,44 @@
   fetchurl,
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "fortify-headers";
-  version = "1.1alpine3";
+  version = "1.1";
 
   # upstream only accessible via git - unusable during bootstrap, hence
-  # extract from the alpine package
+  # extract from GitHub release.
   src = fetchurl {
-    url = "https://dl-cdn.alpinelinux.org/alpine/v3.18/main/x86_64/fortify-headers-1.1-r3.apk";
-    name = "fortify-headers.tar.gz"; # ensure it's extracted as a .tar.gz
-    hash = "sha256-8A8JcKHIBgXpUuIP4zs3Q1yBs5jCGd5F3H2E8UN/S2g=";
+    url = "https://github.com/jvoisin/fortify-headers/archive/refs/tags/${finalAttrs.version}.tar.gz";
+    hash = "sha256-3WJbFTjsIeL7fuFTTEoTaPAyz6PQ2sLBK29mQ8YG+1Y=";
   };
 
   patches = [
+    (fetchurl {
+      # Remove when updating to >= 3.0
+      url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/4adfa7e7291a716b871969b9d1146db037009b57/main/fortify-headers/0001-add-initial-clang-support.patch";
+      hash = "sha256-QbDnMTGVmvIokow6lkA+mDHFMDP8B0lLY8w9ciUhGKU=";
+    })
+    (fetchurl {
+      # Remove when updating to >= 3.0
+      url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/4adfa7e7291a716b871969b9d1146db037009b57/main/fortify-headers/0002-avoid-__extension__-with-clang.patch";
+      hash = "sha256-LooU8I5td7fsfXjBahawvZbhR7r9HTWlT7qGbPE88zY=";
+    })
+    (fetchurl {
+      # Remove when updating to >= 3.0
+      url = "https://gitlab.alpinelinux.org/alpine/aports/-/raw/4adfa7e7291a716b871969b9d1146db037009b57/main/fortify-headers/0003-Disable-wrapping-of-ppoll.patch";
+      hash = "sha256-NW46jwkKS/0hvC7WFaetbAyQxf/YLnyEV+Y8G9payCw=";
+    })
     ./wchar-imports-skip.patch
     ./restore-macros.patch
   ];
+
+  dontBuild = true;
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out
-    cp -r include/fortify $out/include
+    cp -r include $out/include
 
     runHook postInstall
   '';
@@ -37,4 +53,4 @@ stdenv.mkDerivation {
     platforms = lib.platforms.all;
     maintainers = with lib.maintainers; [ ris ];
   };
-}
+})
