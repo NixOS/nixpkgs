0842f545ca006ac6a005b63eb76143a8144ae545
Guy Chronister <guychronister@outlook.com>
2026-02-18 09:34:40 -0600

    i3-rounded: switch to hash

--- pkgs/by-name/i3/i3-rounded/package.nix
+++ pkgs/by-name/i3/i3-rounded/package.nix
@@ -13,7 +13,7 @@ i3.overrideAttrs (oldAttrs: {
     owner = "LinoBigatti";
     repo = "i3-rounded";
     rev = "524c9f7b50f8c540b2ae3480b242c30d8775f98e";
-    sha256 = "0y7m1s1y8f9vgkp7byi33js8n4rigiykd71s936i5d4rwlzrxiwm";
+    hash = "sha256-lceeP+WZtBLNSDqcNn18MROLtBwj+nXufDs55IMO9Xg=";
   };
 
   buildInputs = oldAttrs.buildInputs ++ [ pcre ];
