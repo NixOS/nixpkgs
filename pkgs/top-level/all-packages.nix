d6c3598bfdbf2e7ef59da1a4056e7ed37d57c4f6
Guy Chronister <guychronister@outlook.com>
2026-02-18 09:54:17 -0600

    i3-cycle-focus: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9999,8 +9999,6 @@ with pkgs;
 
   sway-contrib = recurseIntoAttrs (callPackages ../applications/misc/sway-contrib { });
 
-  i3-cycle-focus = callPackage ../applications/window-managers/i3/cycle-focus.nix { };
-
   i3-easyfocus = callPackage ../applications/window-managers/i3/easyfocus.nix { };
 
   i3-layout-manager = callPackage ../applications/window-managers/i3/layout-manager.nix { };
