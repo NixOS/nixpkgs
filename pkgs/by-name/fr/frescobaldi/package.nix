d93fc0fd02db79a9e7c56bc86e4e49b2834a3b12
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-18 18:59:13 +0000

    frescobaldi: 4.0.4 -> 4.0.5

--- pkgs/by-name/fr/frescobaldi/package.nix
+++ pkgs/by-name/fr/frescobaldi/package.nix
@@ -9,14 +9,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "frescobaldi";
-  version = "4.0.4";
+  version = "4.0.5";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "wbsoft";
     repo = "frescobaldi";
     tag = "v${version}";
-    hash = "sha256-J0QC+VwNdA24vAW5Fx+cz5IFajkB8GmR4Rae0Q+2zw8=";
+    hash = "sha256-p1Is8KXT63h788x4qcC48+1cXDiohHpAspWJnxuQ8wY=";
   };
 
   dependencies = with python3Packages; [
