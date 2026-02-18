f9bcf57ec8ebc107fc85f19287c36b969f1d74f2
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-18 07:55:36 +0000

    nimble: 0.20.1 -> 0.22.0

--- pkgs/by-name/ni/nimble/package.nix
+++ pkgs/by-name/ni/nimble/package.nix
@@ -12,13 +12,13 @@
 buildNimPackage (
   final: prev: {
     pname = "nimble";
-    version = "0.20.1";
+    version = "0.22.0";
 
     src = fetchFromGitHub {
       owner = "nim-lang";
       repo = "nimble";
       rev = "v${final.version}";
-      hash = "sha256-DV/cheAoG0UviYEYqfaonhrAl4MgjDwFqbbKx7jUnKE=";
+      hash = "sha256-m0i6JOwdxvp33XAr9XJ1Daly+4DOLPTUDNXKCHJj3I8=";
       fetchSubmodules = true;
     };
 
