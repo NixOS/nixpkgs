24673210d0041c767bcddd888ab6e1a94dd4355d
Jonas Heinrich <onny@project-insanity.org>
2026-02-18 09:47:00 +0100

    nixos/greenlight: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1710,6 +1710,7 @@
   ./services/web-apps/gotosocial.nix
   ./services/web-apps/goupile.nix
   ./services/web-apps/grav.nix
+  ./services/web-apps/greenlight.nix
   ./services/web-apps/grocy.nix
   ./services/web-apps/guacamole-client.nix
   ./services/web-apps/guacamole-server.nix
