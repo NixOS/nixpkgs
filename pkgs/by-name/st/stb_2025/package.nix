0182a0b73f7674a878c2e5a0a73ca79099f152d2
peng0in <peng0in@skyoasis.de>
2026-02-19 00:13:19 +0100

    stb: init at 0-unstable-2025-10-26

---
+++ pkgs/by-name/st/stb_2025/package.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  copyPkgconfigItems,
+  makePkgconfigItem,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "stb_2025";
+  version = "0-unstable-2025-10-26";
+
+  src = fetchFromGitHub {
+    owner = "nothings";
+    repo = "stb";
+    rev = "f1c79c02822848a9bed4315b12c8c8f3761e1296";
+    hash = "sha256-BlyXJtAI7WqXCTT3ylww8zoG0hBxaojJnQDvdQOXJPE=";
+  };
+
+  nativeBuildInputs = [ copyPkgconfigItems ];
+
+  pkgconfigItems = [
+    (makePkgconfigItem rec {
+      name = "stb";
+      version = "1";
+      cflags = [ "-I${variables.includedir}/stb" ];
+      variables = rec {
+        prefix = "${placeholder "out"}";
+        includedir = "${prefix}/include";
+      };
+      inherit (finalAttrs.meta) description;
+    })
+  ];
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/include/stb
+    cp *.h $out/include/stb/
+    cp *.c $out/include/stb/
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Single-file public domain libraries for C/C++";
+    homepage = "https://github.com/nothings/stb";
+    license = with lib.licenses; [
+      mit
+      # OR
+      unlicense
+    ];
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ peng0in ];
+  };
+})
