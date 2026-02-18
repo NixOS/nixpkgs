9e19cfc8c5cc046075aa741fef458e843fe9abac
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-18 04:02:49 +0000

    tree-sitter-grammars.tree-sitter-teal: 0-unstable-2025-05-14 -> 0.1.0-unstable-2025-05-14

--- pkgs/development/tools/parsing/tree-sitter/grammars/grammar-sources.nix
+++ pkgs/development/tools/parsing/tree-sitter/grammars/grammar-sources.nix
@@ -2438,7 +2438,7 @@
   };
 
   teal = {
-    version = "0-unstable-2025-05-14";
+    version = "0.1.0-unstable-2025-05-14";
     url = "github:euclidianAce/tree-sitter-teal/05d276e737055e6f77a21335b7573c9d3c091e2f";
     hash = "sha256-JDqWr895Ob1Jn3Kf44xbkMJqyna0AiMBU5xJpA6ZP7w=";
     meta = {
