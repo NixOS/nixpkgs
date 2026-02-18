e5238dfcb88bff52f6538669b10c023ae8939f76
Guy Chronister <guychronister@outlook.com>
2026-02-18 09:33:50 -0600

    i3-rounded: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9999,8 +9999,6 @@ with pkgs;
 
   sway-contrib = recurseIntoAttrs (callPackages ../applications/misc/sway-contrib { });
 
-  i3-rounded = callPackage ../applications/window-managers/i3/rounded.nix { };
-
   i3-balance-workspace =
     python3Packages.callPackage ../applications/window-managers/i3/balance-workspace.nix
       { };
