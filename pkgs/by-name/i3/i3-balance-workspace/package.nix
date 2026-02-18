930da2ad7c6f2d9f6f6b245ca550e553d4f508c3
Guy Chronister <guychronister@outlook.com>
2026-02-18 09:41:33 -0600

    i3-balance-workspace: modernize derivation

--- pkgs/by-name/i3/i3-balance-workspace/package.nix
+++ pkgs/by-name/i3/i3-balance-workspace/package.nix
@@ -4,17 +4,29 @@
   fetchPypi,
 }:
 
-python3Packages.buildPythonPackage rec {
+python3Packages.buildPythonPackage (finalAttrs: {
   pname = "i3-balance-workspace";
   version = "1.8.6";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-zJdn/Q6r60FQgfehtQfeDkmN0Rz3ZaqgNhiWvjyQFy0=";
   };
 
-  propagatedBuildInputs = [ python3Packages.i3ipc ];
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail 'poetry.masonry.api' 'poetry.core.masonry.api' \
+      --replace-fail 'poetry>=' 'poetry-core>='
+  '';
+
+  build-system = [
+    python3Packages.poetry-core
+  ];
+
+  dependencies = [
+    python3Packages.i3ipc
+  ];
 
   doCheck = false; # project has no test
   pythonImportsCheck = [ "i3_balance_workspace" ];
@@ -26,4 +38,4 @@ python3Packages.buildPythonPackage rec {
     maintainers = with lib.maintainers; [ euxane ];
     mainProgram = "i3_balance_workspace";
   };
-}
+})
