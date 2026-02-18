54869c87a5b66ec04ec05e7025461e50d626cb0e
Bobby Rong <rjl931189261@126.com>
2026-02-18 10:57:30 +0800

    spidermonkey_140: 140.7.0 -> 140.7.1
    https://github.com/mozilla-firefox/firefox/commits/FIREFOX_140_7_1esr_RELEASE/js/src/build
    https://github.com/mozilla-firefox/firefox/commits/FIREFOX_140_7_1esr_RELEASE/build
    (cherry picked from commit 5d61458fc2d9d3a17c7ca10169d5c25335a2dd1b)

--- pkgs/development/interpreters/spidermonkey/140.nix
+++ pkgs/development/interpreters/spidermonkey/140.nix
@@ -1,4 +1,4 @@
 import ./common.nix {
-  version = "140.7.0";
-  hash = "sha512-d4Gx4gMTDBzfKgwuywWpz6gkx11Gfn+sp4tmvVVoyCEyQRKuy3dIg9n0R69/pK3jZIj/EBclWvVRDI9kGZDkcg==";
+  version = "140.7.1";
+  hash = "sha512-fYZ/o8nJSQP2WDvnWtSqjZGPmPdMmcZhWg5AyvIcVFowFJEVIUh2aT7xdYoyDr3M7wF8SENlwZXlWZjM4IhmPA==";
 }
