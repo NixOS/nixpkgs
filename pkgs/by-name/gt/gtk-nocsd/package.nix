191e2ee0d91eefbc37e456b99cc8bcebd9dd7107
Mr.Banana Egg <183596302+Mr-Banana-Egg@users.noreply.github.com>
2026-02-18 19:25:18 +0400

    Add support for GTK-NOCSD

---
+++ pkgs/by-name/gt/gtk-nocsd/package.nix
@@ -0,0 +1,53 @@
+{
+stdenv,
+gtk3,
+libadwaita,
+glib,
+gobject-introspection,
+pkg-config,
+fetchFromGitea,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "gtk-nocsd";
+  # No real version using current commit as version.
+  version = "1.5.0";
+src = fetchFromGitea {
+  domain = "codeberg.org";
+  owner = "MorsMortium";
+  repo = "GTK-NoCSD";
+  rev = "807bcbfc19e7970d0e842e99e444ce55a87b3b85";
+  hash = "sha256-5pCDNmN9i+5tsfx2n7WrgNyXA2lGYLuU0ZlsVv/yZ7E=";
+};
+
+nativeBuildInputs = [ pkg-config ];
+
+buildInputs = [
+    gtk3
+    libadwaita
+    glib
+    gobject-introspection
+];
+buildPhase = ''
+  runHook preBuild
+$CC -fPIC -shared ./Source/GTK-NoCSD.c -o libgtk-nocsd.so.0 \
+	-Wl,-soname,libgtk-nocsd.so.0 \
+	$(pkg-config --cflags libadwaita-1) \
+	$(pkg-config --cflags --libs gobject-2.0 gio-2.0)
+  runHook postBuild
+'';
+installPhase = ''
+  runHook preInstall
+  mkdir -p $out/lib
+  cp libgtk-nocsd.so.0 $out/lib/
+    runHook postInstall
+  '';
+meta = {
+    description = "An LD_PRELOAD library to disable CSD in GTK3/4, LibHandy, and LibAdwaita apps.";
+    homepage = "https://codeberg.org/MorsMortium/GTK-NoCSD.git";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ mr-banana-egg ];
+    platforms = [ "i686-linux" "x86_64-linux" ];
+  };
+}
+
