2ff989da5fcb9bb7c650b62a47babf2dd37f98a0
Kauan Decarli <kauandecarli@gmail.com>
2026-02-18 22:15:49 -0300

    thcrap-steam-proton-wrapper: update repo owner

--- pkgs/by-name/th/thcrap-steam-proton-wrapper/package.nix
+++ pkgs/by-name/th/thcrap-steam-proton-wrapper/package.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation {
   version = "0-unstable-2024-04-03";
 
   src = fetchFromGitHub {
-    owner = "tactikauan";
+    owner = "nerusuki";
     repo = "thcrap-steam-proton-wrapper";
     rev = "2b636c3f5f1ce1b9b41f731aa9397aa68d2ce66b";
     hash = "sha256-J2O8F75NMdsxSaNVr8zLf+vLEJE6CHqWQIIscuuJZ3o=";
@@ -44,7 +44,7 @@ stdenv.mkDerivation {
 
   meta = {
     description = "Wrapper script for launching the official Touhou games on Steam with patches through Proton on GNU/Linux";
-    homepage = "https://github.com/tactikauan/thcrap-steam-proton-wrapper";
+    homepage = "https://github.com/nerusuki/thcrap-steam-proton-wrapper";
     license = lib.licenses.unlicense;
     maintainers = with lib.maintainers; [ ashuramaruzxc ];
     platforms = [
