03273f6ad1b633500a605f95095eb3f9e7bace4e
Tom Hunze <dev@thunze.de>
2026-02-19 21:36:30 +0100

    vtk: fix build with boost 1.89
    Finding `Boost::system` fails because the stub compiled library of
    Boost.System, which has been a header-only library since 1.69, was
    removed in 1.89 [1].
    
    Also, the link interface of libLAS contains `Boost::serialization`
    as a dependency, so we need to add that to `boost_components`.
    
    [1] https://www.boost.org/releases/1.89.0/

--- pkgs/development/libraries/vtk/default.nix
+++ pkgs/development/libraries/vtk/default.nix
@@ -9,5 +9,15 @@ in
   vtk_9_5 = mkVtk {
     version = "9.5.2";
     sourceSha256 = "sha256-zuZLmNJw/3MC2vHvE0WN/11awey0XUdyODX399ViyYk=";
+    patches = [
+      # Finding `Boost::system` fails because the stub compiled library of
+      # Boost.System, which has been a header-only library since 1.69, was
+      # removed in 1.89.
+      # See https://www.boost.org/releases/1.89.0/ for details.
+      #
+      # Also, the link interface of libLAS contains `Boost::serialization`
+      # as a dependency, so we need to add that to `boost_components`.
+      ./boost-1.89.patch
+    ];
   };
 }
