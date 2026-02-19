cadb798509ed0404e4d7e1a608801f2fcb42299d
Manfred Macx <mfmacx@proton.me>
2026-02-19 21:57:07 +0100

    maintainers: add manfredmacx

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -16175,6 +16175,12 @@
     githubId = 115060;
     name = "Marek Maksimczyk";
   };
+  manfredmacx = {
+    email = "mfmacx@proton.me";
+    github = "manfredmacx";
+    githubId = 222261305;
+    name = "Manfred Macx";
+  };
   Mange = {
     name = "Magnus Bergmark";
     email = "me@mange.dev";
