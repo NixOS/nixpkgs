2ec6b06c8641ad837d03a9df5f3969e7ce41abed
Jonas Heinrich <onny@project-insanity.org>
2026-02-19 10:47:01 +0100

    nixos/openproject

--- nixos/modules/services/web-apps/openproject.nix
+++ nixos/modules/services/web-apps/openproject.nix
@@ -4,81 +4,79 @@
   pkgs,
   ...
 }:
+
 with lib;
+
 let
+
   pgVersion = lib.head (lib.splitString "." config.services.postgresql.package.version);
   cfg = config.services.openproject;
+
 in
 {
-  options.services.openproject = with types; {
-    enable = mkEnableOption "openproject server";
-    host = {
-      name = mkOption {
-        type = str;
-        default = config.networking.hostName;
-      };
-      rootPath = mkOption {
-        type = str;
-        default = "";
-      };
-      bind.addr = mkOption {
-        type = str;
-        default = "0.0.0.0";
-      };
-      bind.port = mkOption {
-        type = int;
-        default = 6346;
-      };
-    };
+  options.services.openproject = {
+
+    enable = mkEnableOption "OpenProject modern project management web app";
+
     package = mkPackageOption pkgs "openproject" { };
+
+    settings = lib.mkOption {
+      type = lib.types.submodule {
+        freeformType = types.attrsOf types.str;
+        options = {
+          OPENPROJECT_HOST__NAME = mkOption {
+            type = lib.types.str;
+            default = "localhost";
+            description = "Hostname to use";
+          };
+          PORT = mkOption {
+            type = lib.types.int;
+            default = 6346;
+            description = "IP address and port to bind to";
+            example = "127.0.0.1:8080";
+          };
+          DATABASE_URL = mkOption {
+            type = lib.types.str;
+            default = "postgres:///openproject?host=/run/postgresql&username=openproject&pool=20&encoding=unicode&reconnect=true";
+            description = ''
+              Database connection scheme. The default specifies the
+              connection through a local socket.
+            '';
+          };
+        };
+      };
+      default = { };
+      description = ''
+        Extra configuration options to append or override.
+        For available and default option values see
+        [upstream configuration file](https://www.openproject.org/docs/installation-and-operations/configuration/environment/).
+      '';
+    };
+
     secrets.keyBaseFile = mkOption {
-      type = str;
+      type = types.str;
+      description = "TODO";
     };
     secrets.environmentFile = mkOption {
-      type = nullOr str;
+      type = types.nullOr types.str;
       default = null;
+      description = "TODO";
     };
     secrets.extraSeedEnvironmentFile = mkOption {
-      type = nullOr str;
+      type = types.nullOr types.str;
       default = null;
-    };
-    environment = mkOption {
-      type = attrsOf str;
-      default = { };
-    };
-    useJemalloc = mkOption {
-      type = bool;
-      default = true;
-    };
-    dbUrl = mkOption {
-      type = str;
-      default = "postgres:///openproject?host=/run/postgresql&username=openproject&pool=20&encoding=unicode&reconnect=true";
-    };
-    statePath = mkOption {
-      type = str;
-      default = "/var/lib/openproject";
-    };
-    imap = {
-      enable = mkEnableOption "interact with openproject via mail";
+      description = "TODO";
     };
   };
+
   config = mkIf cfg.enable {
 
-    nixpkgs.overlays = [
-      (import ./overlay.nix { openprojectStatePath = cfg.statePath; })
-    ];
-
-    services.openproject = {
-      ## see https://www.openproject.org/docs/installation-and-operations/configuration/environment/
-      environment = {
-        OPENPROJECT_HOST__NAME = cfg.host.name;
+    services.openproject.settings = lib.mkMerge [
+      {
         OPENPROJECT_HSTS = "false";
         OPENPROJECT_RAILS_CACHE_STORE = "memcache";
-        ## FIXME run multiple memcached instances instead
-        ## FIXME or switch to redis if feasible
         OPENPROJECT_CACHE__MEMCACHE__SERVER = "unix:///run/memcached/memcached.sock";
         OPENPROJECT_CACHE__NAMESPACE = "openproject";
-        OPENPROJECT_RAILS__RELATIVE__URL__ROOT = cfg.host.rootPath;
         RAILS_ENV = "production";
         RAILS_MIN_THREADS = "4";
         RAILS_MAX_THREADS = "16";
@@ -88,58 +86,101 @@ in
         PGVERSION = pgVersion;
         CURRENT_PGVERSION = pgVersion;
         NEXT_PGVERSION = pgVersion;
-        DATABASE_URL = cfg.dbUrl;
         SECRET_KEY_BASE_FILE = cfg.secrets.keyBaseFile;
-        LD_PRELOAD = mkIf cfg.useJemalloc "${pkgs.jemalloc}/lib/libjemalloc.so";
-      };
-    };
-
-    users = {
-      groups.openproject = { };
-      users.openproject = {
-        isSystemUser = true;
-        group = "openproject";
-      };
-    };
+        LD_PRELOAD = "${pkgs.jemalloc}/lib/libjemalloc.so";
+      }
+    ];
 
     systemd.services."openproject-seeder" = {
-      serviceConfig.User = "openproject";
-      serviceConfig.Type = "oneshot";
-      serviceConfig.RemainAfterExit = true;
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        WorkingDirectory = "/var/lib/openproject";
+        StateDirectory = "openproject";
+        EnvironmentFile = remove null [
+          cfg.secrets.extraSeedEnvironmentFile
+          cfg.secrets.environmentFile
+        ];
+        ExecStart = "${lib.getExe' cfg.package "openproject-seeder"} openproject";
+
+        # hardening
+        AmbientCapabilities = "";
+        CapabilityBoundingSet = "";
+        DevicePolicy = "closed";
+        DynamicUser = true;
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProcSubset = "pid";
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        RemoveIPC = true;
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
+        ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "@system-service"
+          "~@privileged"
+        ];
+        UMask = "0077";
+      };
       bindsTo = [ "postgresql.service" ];
-      environment = cfg.environment;
-      serviceConfig.EnvironmentFile = remove null [
-        cfg.secrets.extraSeedEnvironmentFile
-        cfg.secrets.environmentFile
-      ];
-      serviceConfig.ExecStart = "${cfg.package}/bin/openproject-seeder openproject";
+      environment = cfg.settings;
     };
+
     systemd.services."openproject-web" = {
-      serviceConfig.User = "openproject";
+      serviceConfig = {
+        EnvironmentFile = remove null [ cfg.secrets.environmentFile ];
+        ExecStart = "${lib.getExe' cfg.package "openproject-web"}";
+        WorkingDirectory = "/var/lib/openproject";
+        StateDirectory = "openproject";
+      };
       bindsTo = [ "openproject-seeder.service" ];
       after = [ "openproject-seeder.service" ];
       wantedBy = [ "multi-user.target" ];
-      environment = cfg.environment;
-      serviceConfig.EnvironmentFile = remove null [ cfg.secrets.environmentFile ];
-      serviceConfig.ExecStart = "${cfg.package}/bin/openproject-web -b ${cfg.host.bind.addr} -p ${toString cfg.host.bind.port}";
+      environment = cfg.settings;
     };
+
     systemd.services."openproject-worker" = {
-      serviceConfig.User = "openproject";
+      serviceConfig = {
+        EnvironmentFile = remove null [ cfg.secrets.environmentFile ];
+        ExecStart = "${lib.getExe' cfg.package "openproject-worker"}";
+        WorkingDirectory = "/var/lib/openproject";
+        StateDirectory = "openproject";
+      };
       bindsTo = [ "openproject-seeder.service" ];
       after = [ "openproject-seeder.service" ];
       wantedBy = [ "multi-user.target" ];
-      environment = cfg.environment;
-      serviceConfig.EnvironmentFile = remove null [ cfg.secrets.environmentFile ];
-      serviceConfig.ExecStart = "${cfg.package}/bin/openproject-worker";
+      environment = cfg.settings;
     };
+
     systemd.services."openproject-cron" = {
-      serviceConfig.User = "openproject";
+      serviceConfig = {
+        EnvironmentFile = remove null [ cfg.secrets.environmentFile ];
+        ExecStart = "${lib.getExe' cfg.package "openproject-cron-step-imap"}";
+        WorkingDirectory = "/var/lib/openproject";
+        StateDirectory = "openproject";
+      };
       bindsTo = [ "openproject-seeder.service" ];
       after = [ "openproject-seeder.service" ];
-      environment = cfg.environment;
-      serviceConfig.EnvironmentFile = remove null [ cfg.secrets.environmentFile ];
-      serviceConfig.ExecStart = mkIf cfg.imap.enable "${cfg.package}/bin/openproject-cron-step-imap";
+      environment = cfg.settings;
     };
+
     systemd.timers."openproject-cron" = {
       timerConfig.OnActiveSec = "30 seconds";
       timerConfig.OnUnitInactiveSec = "5 minutes";
@@ -151,10 +192,6 @@ in
       enable = true;
       enableUnixSocket = true;
     };
-
-    systemd.tmpfiles.rules = [
-      "d ${cfg.statePath} 0750 openproject openproject"
-    ];
 
     services.postgresql = {
       enable = true;
