05157d61608d04465c783f686a8d6797c6369427
Nikita Morozov <nikitwww@gmail.com>
2026-02-19 03:13:17 +0300

    ubootOrangePi5Ultra: init at 2025.10

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -517,6 +517,19 @@ in
     ];
   };
 
+  ubootOrangePi5Ultra = buildUBoot {
+    defconfig = "orangepi-5-ultra-rk3588_defconfig";
+    extraMeta.platforms = [ "aarch64-linux" ];
+    BL31 = "${armTrustedFirmwareRK3588}/bl31.elf";
+    ROCKCHIP_TPL = rkbin.TPL_RK3588;
+    filesToInstall = [
+      "u-boot.itb"
+      "idbloader.img"
+      "u-boot-rockchip.bin"
+      "u-boot-rockchip-spi.bin"
+    ];
+  };
+
   ubootOrangePi5Max = buildUBoot {
     defconfig = "orangepi-5-max-rk3588_defconfig";
     extraMeta.platforms = [ "aarch64-linux" ];
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9190,6 +9190,7 @@ with pkgs;
     ubootOrangePi3
     ubootOrangePi3B
     ubootOrangePi5
+    ubootOrangePi5Ultra
     ubootOrangePi5Max
     ubootOrangePi5Plus
     ubootOrangePiPc
