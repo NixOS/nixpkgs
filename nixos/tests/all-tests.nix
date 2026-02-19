53a19ca4883d0144f36a0013a96e9977db0adb98
Jonas Heinrich <onny@project-insanity.org>
2026-02-19 10:07:58 +0100

    nixos/tests/openproject: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1263,6 +1263,7 @@ in
   openbao = runTest ./openbao.nix;
   opencloud = runTest ./opencloud.nix;
   openldap = runTest ./openldap.nix;
+  openproject = runTest ./openproject.nix;
   openresty-lua = runTest ./openresty-lua.nix;
   opensearch = discoverTests (import ./opensearch.nix);
   opensmtpd = handleTest ./opensmtpd.nix { };
