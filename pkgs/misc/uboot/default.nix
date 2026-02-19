73108ee531d0d4b7183968dfd25106d3ded18870
Morozov Nikita <nikitwww@gmail.com>
2026-02-19 03:38:33 +0300

    ubootOrangePi5Ultra: move env vars into env block

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -520,8 +520,10 @@ in
   ubootOrangePi5Ultra = buildUBoot {
     defconfig = "orangepi-5-ultra-rk3588_defconfig";
     extraMeta.platforms = [ "aarch64-linux" ];
-    BL31 = "${armTrustedFirmwareRK3588}/bl31.elf";
-    ROCKCHIP_TPL = rkbin.TPL_RK3588;
+    env = {
+      BL31 = "${armTrustedFirmwareRK3588}/bl31.elf";
+      ROCKCHIP_TPL = rkbin.TPL_RK3588;
+    };
     filesToInstall = [
       "u-boot.itb"
       "idbloader.img"
