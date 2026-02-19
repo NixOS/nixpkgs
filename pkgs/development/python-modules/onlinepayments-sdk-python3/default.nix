f06656c2fb6bf6368a67b91983566c489622f389
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-19 06:28:49 +0000

    python3Packages.onlinepayments-sdk-python3: 4.23.0 -> 7.1.0

--- pkgs/development/python-modules/onlinepayments-sdk-python3/default.nix
+++ pkgs/development/python-modules/onlinepayments-sdk-python3/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "onlinepayments-sdk-python3";
-  version = "4.23.0";
+  version = "7.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "wl-online-payments-direct";
     repo = "sdk-python3";
     rev = finalAttrs.version;
-    hash = "sha256-IX9kiM5ZtX4uzW+D+Bbt8535CqMtdTtv0mpDo5Swstg=";
+    hash = "sha256-0w3PdnmLpp+1MzpwetTKY7bPNb9mNJAqKjs4mqdjsRM=";
   };
 
   build-system = [ setuptools ];
