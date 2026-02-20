88c9938c1d80bb2f890d6733f55b34a5014faad0
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-20 13:11:33 +0000

    sublime-merge-dev: 2120 -> 2122

--- pkgs/applications/version-management/sublime-merge/default.nix
+++ pkgs/applications/version-management/sublime-merge/default.nix
@@ -11,9 +11,9 @@ in
   } { };
 
   sublime-merge-dev = common {
-    buildVersion = "2120";
+    buildVersion = "2122";
     dev = true;
-    aarch64sha256 = "3JKxLke1l7l+fxhIJWbXbMHK5wPgjZTEWcZd9IvrdPM=";
-    x64sha256 = "N8lhSmQnj+Ee1A2eIOkhdhQnHBK3B6vFA3vrPAbYtaI=";
+    aarch64sha256 = "SfmEaBbJCiLXm8XZu8tPD4Sx7Se2S2Zxt+fbZwi5R3w=";
+    x64sha256 = "5o4Vn/yfTU6anfPofNZBNaBCuXpPwJEQgFcPyExUxeo=";
   } { };
 }
