91833cf394a402af80db32f938307a3282605041
Boris Nagaev <bnagaev@gmail.com>
2026-02-20 13:50:06 -0500

    balanceofsatoshis: 19.4.14 -> 20.0.4

--- pkgs/by-name/ba/balanceofsatoshis/package.nix
+++ pkgs/by-name/ba/balanceofsatoshis/package.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "balanceofsatoshis";
-  version = "19.4.14";
+  version = "20.0.4";
 
   src = fetchFromGitHub {
     owner = "alexbosworth";
     repo = "balanceofsatoshis";
     tag = "v${version}";
-    hash = "sha256-lXwE7/7ZWO6GD4SY0BPh/QXNpxkCYJS00Gjna0DkOE0=";
+    hash = "sha256-2nM+B/e/FSP2rTC2Yktw4mkw7McqL32xTnEYPh/gPAA=";
   };
 
-  npmDepsHash = "sha256-WKpbYzNd0srD8yVB7Xa4v4qF9qHBiHHtKrYitnqEPTM=";
+  npmDepsHash = "sha256-B77A+HPSDo5Y60TJk7N/8taGdWAZjf72Lvv7t3ziMbI=";
 
   nativeBuildInputs = [ python3 ];
 
