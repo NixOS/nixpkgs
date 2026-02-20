3b817e7b5fb1fe11c1cacb234e198cae2fd3308c
Dan Xin <davinci42.cn@gmail.com>
2026-02-20 15:10:23 +0800

    faugus-launcher: 1.14.3 -> 1.14.4
    Diff: https://github.com/Faugus/faugus-launcher/compare/1.14.3...1.14.4
    
    Changelog: https://github.com/Faugus/faugus-launcher/releases/tag/1.14.4

--- pkgs/by-name/fa/faugus-launcher/package.nix
+++ pkgs/by-name/fa/faugus-launcher/package.nix
@@ -18,14 +18,14 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "faugus-launcher";
-  version = "1.14.3";
+  version = "1.14.4";
   pyproject = false;
 
   src = fetchFromGitHub {
     owner = "Faugus";
     repo = "faugus-launcher";
     tag = finalAttrs.version;
-    hash = "sha256-etPG5142YMWyHhn2wd/t4fPSW2oonp8qoY7aPAim/LI=";
+    hash = "sha256-rlSoBRchzQIUOklg5E6hX/R2CAmCye4bn7uVpTtyzhg=";
   };
 
   nativeBuildInputs = [
