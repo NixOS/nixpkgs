5feb8f93d139232c26f0fe34c2b765f022e7c214
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-02-20 04:37:45 +0100

    shairport-sync: remove duplicated --with-stdout configure flag

--- pkgs/by-name/sh/shairport-sync/package.nix
+++ pkgs/by-name/sh/shairport-sync/package.nix
@@ -116,7 +116,6 @@ stdenv.mkDerivation (finalAttrs: {
     "--without-configfiles"
     "--sysconfdir=/etc"
     "--with-ssl=openssl"
-    "--with-stdout"
   ]
   ++ optional enableAvahi "--with-avahi"
   ++ optional enablePulse "--with-pa"
