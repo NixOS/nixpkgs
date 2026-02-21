64952345d8742bd544500f61332fbd2ea01382d4
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-21 13:19:06 +0000

    ocamlPackages.mem_usage: 0.1.2 -> 0.1.3

--- pkgs/development/ocaml-modules/mem_usage/default.nix
+++ pkgs/development/ocaml-modules/mem_usage/default.nix
@@ -6,13 +6,13 @@
 
 buildDunePackage rec {
   pname = "mem_usage";
-  version = "0.1.2";
+  version = "0.1.3";
 
   src = fetchFromGitHub {
     owner = "savonet";
     repo = "ocaml-mem_usage";
     rev = "v${version}";
-    hash = "sha256-5tQNsqbiU9oJvKHUjeTo/ST4A0Axc95gdJISLaa9VRM=";
+    hash = "sha256-0XFltWPTOIs1h3CIit4NPkAb+dS74GubZB0shqbuvTs=";
   };
 
   minimalOCamlVersion = "4.07";
