aa18a3fc9457cea665a153391735693704b8928e
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-21 07:29:52 +0000

    vscode-extensions.continue.continue: 1.2.11 -> 1.2.16

--- pkgs/applications/editors/vscode/extensions/continue.continue/default.nix
+++ pkgs/applications/editors/vscode/extensions/continue.continue/default.nix
@@ -11,26 +11,26 @@ vscode-utils.buildVscodeMarketplaceExtension {
       sources = {
         "x86_64-linux" = {
           arch = "linux-x64";
-          hash = "sha256-YPA0+kj5ATEpPSMqdqqtG0s/hlODSmme2i2vgfUiRvI=";
+          hash = "sha256-H4vW4KGVajCJ51kdiQ4Mg0U/U93oSflhc6MLrDsQSOM=";
         };
         "x86_64-darwin" = {
           arch = "darwin-x64";
-          hash = "sha256-P/8SOKEK8y7gLPD/r88tDoMuh2AeCUd7hPA6Be7+YuU=";
+          hash = "sha256-goucET2PCGUFPCs8XS9MoisFPNS2uCZrJV/eINZ7kFk=";
         };
         "aarch64-linux" = {
           arch = "linux-arm64";
-          hash = "sha256-QqHrephuGy+gq30HhRb/zjq3yEFQ6Vf3W6x2gaobObo=";
+          hash = "sha256-Dp5p2QwFkd9gBrF7o9t8AVRgisGhhh9NHiTL9I/KGdg=";
         };
         "aarch64-darwin" = {
           arch = "darwin-arm64";
-          hash = "sha256-fhi3jgnl0Pu0WWHtOLEDlbgqNaGPJr6Ps3jxjhjvOBs=";
+          hash = "sha256-LX7PIUf/8//WJraABTw+1Awt2oj3Q8pFNt4xLQvYgvw=";
         };
       };
     in
     {
       name = "continue";
       publisher = "Continue";
-      version = "1.2.11";
+      version = "1.2.16";
     }
     // sources.${stdenv.system} or (throw "Unsupported system: ${stdenv.system}");
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
