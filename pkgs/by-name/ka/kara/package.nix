32656a8d4e835555c2fa1f578cc25bab48ce4376
ismay <263015513+ismay-tue@users.noreply.github.com>
2026-02-21 16:41:47 +0100

    kara: 0.8.0 -> 0.8.0-unstable-2026-02-20

--- pkgs/by-name/ka/kara/package.nix
+++ pkgs/by-name/ka/kara/package.nix
@@ -7,22 +7,41 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "kara";
-  version = "0.8.0";
+  version = "0.8.0-unstable-2026-02-20";
 
   src = fetchFromGitHub {
     owner = "dhruv8sh";
     repo = "kara";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-gdYwgHNnkvayd7GK9H8AZHeKL589hU6MN9DXiUkSU3A=";
+    rev = "2c9f7926d2da41324e9f56355be314a5c76fe022";
+    hash = "sha256-SqFEtz4X9ZbzA4hEgHLDIqhQE0kuB6ht5i1gyFeEHyM=";
   };
+
+  nativeBuildInputs = with pkgs; [
+    cmake
+    extra-cmake-modules
+    qt6.wrapQtAppsHook
+  ];
+
+  buildInputs = with pkgs.kdePackages; [
+    libplasma
+    plasma-activities
+    plasma-workspace
+    kwin
+    kcoreaddons
+    kconfig
+    kpackage
+    kdeclarative
+  ];
+
+  cmakeFlags = [
+    "-DCMAKE_INSTALL_PREFIX=$out/share/plasma/plasmoids/org.dhruv8sh.kara"
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DKDE_INSTALL_USE_QT_SYS_PATHS=OFF"
+  ];
 
   installPhase = ''
     runHook preInstall
-
-    mkdir -p $out/share/plasma/plasmoids/org.dhruv8sh.kara
-    cp metadata.json $out/share/plasma/plasmoids/org.dhruv8sh.kara
-    cp -r contents $out/share/plasma/plasmoids/org.dhruv8sh.kara
-
+    cmake --install .
     runHook postInstall
   '';
 
