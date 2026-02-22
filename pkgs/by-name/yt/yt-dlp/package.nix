6bca74249c9b0c14f45a41d6b4a07011b65eba1e
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-02-22 11:40:33 -0800

    yt-dlp: 2026.02.04 -> 2026.02.21

--- pkgs/by-name/yt/yt-dlp/package.nix
+++ pkgs/by-name/yt/yt-dlp/package.nix
@@ -21,14 +21,14 @@ python3Packages.buildPythonApplication rec {
   # The websites yt-dlp deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2026.02.04";
+  version = "2026.02.21";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "yt-dlp";
     repo = "yt-dlp";
     tag = version;
-    hash = "sha256-KXnz/ocHBftenDUkCiFoBRBxi6yWt0fNuRX+vKFWDQw=";
+    hash = "sha256-r9I/zLyqGPeIzsHsLxJcfnLC3jpuyKMyX1UaMoM08jk=";
   };
 
   postPatch = ''
