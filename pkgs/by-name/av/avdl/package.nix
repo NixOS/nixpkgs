f0affef4f78e15a10f5d0ee8bca3822552a9191a
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-22 14:45:58 +0000

    avdl: 0.1.5+1.12.1 -> 0.1.6+1.12.1
    (cherry picked from commit 62d759590003b8e31633fcbc6cd0e713a9b152fc)

--- pkgs/by-name/av/avdl/package.nix
+++ pkgs/by-name/av/avdl/package.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "avdl";
-  version = "0.1.5+1.12.1";
+  version = "0.1.6+1.12.1";
 
   src = fetchFromGitHub {
     owner = "jonhoo";
     repo = "avdl";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-/YGDtezPMMdogk8eGoHgqt8B0t6SNA3TVqroLOOANxs=";
+    hash = "sha256-ReTsyIZ+w7wWhREmkZT6tNsEFkpF2KVJsFAhCfL5CZQ=";
     fetchSubmodules = true;
   };
 
-  cargoHash = "sha256-QPC58tt7e8O/KJyE3c5mdLMyEt37hKG9lEDBs47prAQ=";
+  cargoHash = "sha256-WiAPUD5AAsYSxJ5tn8/6ZjMf3hEOtTXzOMIVXd6f9l0=";
 
   meta = {
     description = "Rust port of avro-tools' IDL tooling";
