7b9e4c7595832c9e2fa05212888fc2369c50eef1
Doron Behar <doron.behar@gmail.com>
2026-02-22 12:45:49 +0200

    buildLuarocksPackage: fix cross compilation wrapping
    - Make `lua` gain `luaOnBuildFor{Build,Host,Target}` and alike attributes.
    - wrapLua: correctly substitute build & host lua & luarocks_bootstrap
    - Disallow build platform references when using `buildLuarocksPackage`
      in cross building.

--- pkgs/development/interpreters/lua-5/wrap-lua.nix
+++ pkgs/development/interpreters/lua-5/wrap-lua.nix
@@ -9,5 +9,8 @@
 makeSetupHook {
   name = "wrap-lua-hook";
   propagatedBuildInputs = [ makeWrapper ];
-  substitutions.lua = lua;
+  substitutions.luaBuild = lua.luaOnBuildForHost;
+  substitutions.luaHost = lua.luaOnHostForHost;
+  substitutions.luarocksBuild = lua.luaOnBuildForHost.pkgs.luarocks_bootstrap;
+  substitutions.luarocksHost = lua.luaOnHostForHost.pkgs.luarocks_bootstrap;
 } ./wrap.sh
