97ed533d0df73b6a918ebbeccd07c88ab0c4667d
Squawkykaka <squawkykaka@gmail.com>
2026-02-22 21:59:39 +1300

    nitrolaunch: init at 0.29.0

---
+++ pkgs/by-name/ni/nitrolaunch/package.nix
@@ -0,0 +1,59 @@
+{
+  symlinkJoin,
+  buildFHSEnv,
+  nitrolaunch-gui-unwrapped,
+  nitrolaunch-cli-unwrapped,
+  extraPkgs ? pkgs: [ ],
+}:
+let
+  fhsEnv = {
+    inherit (nitrolaunch-gui-unwrapped) version;
+    targetPkgs =
+      pkgs:
+      with pkgs;
+      [
+        nitrolaunch-gui-unwrapped
+        nitrolaunch-cli-unwrapped
+        openal
+        glfw3-minecraft
+        alsa-lib
+        libjack2
+        libpulseaudio
+        pipewire
+        libGL
+        libx11
+        libxcursor
+        libxext
+        libxrandr
+        libxxf86vm
+        vulkan-loader
+      ]
+      ++ extraPkgs pkgs;
+  };
+in
+symlinkJoin {
+  name = "nitrolaunch";
+  paths = [
+    (buildFHSEnv (
+      fhsEnv
+      // {
+        pname = "Nitrolaunch";
+        runScript = "Nitrolaunch";
+      }
+    ))
+    (buildFHSEnv (
+      fhsEnv
+      // {
+        pname = "nitro";
+        runScript = "nitro";
+      }
+    ))
+  ];
+  postBuild = ''
+    mkdir -p $out/share
+    ln -s ${nitrolaunch-gui-unwrapped}/share/applications $out/share
+    ln -s ${nitrolaunch-gui-unwrapped}/share/icons $out/share
+  '';
+
+  inherit (nitrolaunch-gui-unwrapped) meta;
+}
