2342c90371ce11ffd85881165ac4a0b11efb03cd
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-22 03:27:49 +0000

    museeks: 0.23.1 -> 0.23.4

--- pkgs/by-name/mu/museeks/package.nix
+++ pkgs/by-name/mu/museeks/package.nix
@@ -16,11 +16,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "museeks";
-  version = "0.23.1";
+  version = "0.23.4";
 
   src = fetchurl {
     url = "https://github.com/martpie/museeks/releases/download/${finalAttrs.version}/Museeks_${finalAttrs.version}_amd64.deb";
-    hash = "sha256-bji49ncJriDGrYoC0VYfblcGPDU66Ep+c/z9FNEXnkI=";
+    hash = "sha256-2WkWBd4opWpCcjE+uWRbDC8RPQoVvflpxbWuuNF2Z54=";
   };
 
   nativeBuildInputs = [
