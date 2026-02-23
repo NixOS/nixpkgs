a5ef72b486a6604f3c1a5d823c1de381508c0c85
asteriau <dorahaladita@gmail.com>
2026-02-23 11:09:46 +0200

    maintainers: add asteriau

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -343,6 +343,12 @@
     githubId = 65275785;
     name = "Alexander Kenji Berthold";
   };
+  asteriau = {
+    email = "dorahaladita@gmail.com";
+    github = "asteriau";
+    githubId = 131019152;
+    name = "Laura";
+  };
   A1ca7raz = {
     email = "aya@wtm.moe";
     github = "A1ca7raz";
