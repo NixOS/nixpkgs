a685d0f88290c1aac2466fb4e84579239401b6da
Thierry Delafontaine <delafthi@pm.me>
2026-02-23 13:59:36 +0100

    proton-pass: add update script

--- pkgs/by-name/pr/proton-pass/package.nix
+++ pkgs/by-name/pr/proton-pass/package.nix
@@ -2,10 +2,47 @@
   lib,
   stdenv,
   callPackage,
+  fetchurl,
+  writeShellScript,
+  curl,
+  jq,
+  common-updater-scripts,
 }:
 let
   pname = "proton-pass";
   version = "1.34.2";
+
+  passthru = {
+    sources = {
+      "x86_64-linux" = fetchurl {
+        url = "https://proton.me/download/pass/linux/x64/proton-pass_${version}_amd64.deb";
+        hash = "sha256-i5QQ1uzQ2tSDX4I/APL60QcHh9Ovc7ciueRnz7cZUuE=";
+      };
+      "aarch64-darwin" = fetchurl {
+        url = "https://proton.me/download/pass/macos/ProtonPass_${version}.dmg";
+        hash = "sha256-oo02IYOKZEsr0+4zimSFkutTGuS63ZvMZTeUTapZrVw=";
+      };
+      "x86_64-darwin" = passthru.sources."aarch64-darwin";
+    };
+    updateScript = writeShellScript "update-proton-pass" ''
+      set -o errexit
+      export PATH="${
+        lib.makeBinPath [
+          curl
+          jq
+          common-updater-scripts
+        ]
+      }"
+      NEW_VERSION=$(curl --silent https://proton.me/download/PassDesktop/linux/x64/version.json | jq -r '[.Releases[] | select(.CategoryName == "Stable")] | first | .Version')
+      if [[ "${version}" = "$NEW_VERSION" ]]; then
+          echo "The new version is the same as the old version."
+          exit 0
+      fi
+      for platform in ${lib.escapeShellArgs meta.platforms}; do
+        update-source-version "proton-pass" "$NEW_VERSION" --ignore-same-version --source-key="sources.$platform"
+      done
+    '';
+  };
 
   meta = {
     description = "Desktop application for Proton Pass";
@@ -17,11 +54,16 @@ let
       sebtm
       shunueda
     ];
-    platforms = [ "x86_64-linux" ] ++ lib.platforms.darwin;
+    platforms = builtins.attrNames passthru.sources;
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     mainProgram = "proton-pass";
   };
 in
 callPackage (if stdenv.hostPlatform.isDarwin then ./darwin.nix else ./linux.nix) {
-  inherit pname version meta;
+  inherit
+    pname
+    version
+    passthru
+    meta
+    ;
 }
