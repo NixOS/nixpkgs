9d5f2f3f289ccaeb6613f62cfb8883d1799d8e2f
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-24 20:26:06 +0100

    python313Packages.w3lib: migrate to finalAttrs

--- pkgs/development/python-modules/w3lib/default.nix
+++ pkgs/development/python-modules/w3lib/default.nix
@@ -6,13 +6,13 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "w3lib";
   version = "2.4.0";
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-4jOtIWSbadDgR6EPMBga6Wd1JKKfb3H288dY3AyNJkg=";
   };
 
@@ -25,8 +25,8 @@ buildPythonPackage rec {
   meta = {
     description = "Library of web-related functions";
     homepage = "https://github.com/scrapy/w3lib";
-    changelog = "https://github.com/scrapy/w3lib/blob/v${version}/NEWS";
+    changelog = "https://github.com/scrapy/w3lib/blob/${finalAttrs.version}/NEWS";
     license = lib.licenses.bsd3;
     maintainers = [ ];
   };
-}
+})
