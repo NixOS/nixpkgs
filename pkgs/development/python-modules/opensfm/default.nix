78149221de81926956918fd9f47df9c468ea3c12
Aliaksandr <grubian2@gmail.com>
2026-02-24 19:55:00 +0200

    python3Packages.opensfm: unstable-2023-12-09 -> 0.5.1-unstable-2026-02-11
    - Switch build system from setuptools to scikit-build-core
    - Replace individual patches with consolidated use-system-dependencies.patch
    - Update dependencies: opencv4 -> opencv-python, fpdf -> fpdf2, add flask
    - Switch to dontUseCmakeConfigure, add __structuredAttrs
    - Add postInstall for bin scripts
    - Use finalAttrs pattern

--- pkgs/development/python-modules/opensfm/default.nix
+++ pkgs/development/python-modules/opensfm/default.nix
@@ -4,12 +4,13 @@
   buildPythonPackage,
   fetchFromGitHub,
   cmake,
-  opencv4,
+  opencv-python,
   ceres-solver,
   suitesparse,
   metis,
   eigen,
-  setuptools,
+  scikit-build-core,
+  ninja,
   pkg-config,
   pybind11,
   numpy,
@@ -33,33 +34,28 @@
   scipy,
   sphinx,
   matplotlib,
-  fpdf,
+  flask,
+  fpdf2,
+  pythonAtLeast,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "opensfm";
-  version = "unstable-2023-12-09";
+  version = "0.5.1-unstable-2026-02-11";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mapillary";
     repo = "OpenSfM";
-    rev = "7f170d0dc352340295ff480378e3ac37d0179f8e";
-    sha256 = "sha256-l/HTVenC+L+GpMNnDgnSGZ7+Qd2j8b8cuTs3SmORqrg=";
+    rev = "3a2041e28f10afe8e43ca6602f8dcf7db4679f5b";
+    hash = "sha256-5Ufzn2DUjsmE8S5Ot3Qs7n0fJuzkVkH2ayX3jxtc4E4=";
   };
 
   patches = [
-    ./0002-cmake-find-system-distributed-gtest.patch
-    ./0003-cmake-use-system-pybind11.patch
-    ./0004-pybind_utils.h-conflicts-with-nixpkgs-pybind.patch
-    ./0005-fix-numpy-2-test-failures.patch # not upstreamed due to cla, but you're free upstream it -@pbsds
-    ./fix-scripts.patch
+    ./use-system-dependencies.patch
   ];
 
   postPatch = ''
-    substituteInPlace opensfm/src/CMakeLists.txt \
-      --replace-fail "cmake_minimum_required(VERSION 3.0)" "cmake_minimum_required(VERSION 3.10)"
-
     rm opensfm/src/cmake/FindGlog.cmake
     rm opensfm/src/cmake/FindGflags.cmake
 
@@ -69,13 +65,15 @@ buildPythonPackage rec {
     echo 'feature_type: SIFT' >> data/berlin/config.yaml
     echo 'feature_type: HAHOG' >> data/lund/config.yaml
 
-    # make opensfm correctly import glog headers
-    export CXXFLAGS=-DGLOG_USE_GLOG_EXPORT
-
-    sed -i -e 's/^.*BuildDoc.*$//' setup.py
+    # add shebang to opensfm_main.py
+    sed -i '1i#!/usr/bin/env python' bin/opensfm_main.py
   '';
 
-  build-system = [ setuptools ];
+  build-system = [
+    scikit-build-core
+    pybind11
+    ninja
+  ];
 
   nativeBuildInputs = [
     cmake
@@ -99,11 +97,12 @@ buildPythonPackage rec {
     numpy
     scipy
     pyyaml
-    opencv4.cxxdev
+    opencv-python
     networkx
     pillow
     matplotlib
-    fpdf
+    flask
+    fpdf2
     exifread
     gpxpy
     pyproj
@@ -119,15 +118,27 @@ buildPythonPackage rec {
     distutils
   ];
 
-  dontUseCmakeBuildDir = true;
-  cmakeFlags = [
-    "-Bcmake_build"
-    "-Sopensfm/src"
-  ];
+  dontUseCmakeConfigure = true;
+
+  __structuredAttrs = true;
+
+  env = {
+    CMAKE_ARGS = "-DPYBIND11_FINDPYTHON=ON";
+    CXXFLAGS = "-DGLOG_USE_GLOG_EXPORT";
+  };
+
+  postInstall = ''
+    install -Dm755 bin/opensfm_main.py $out/bin/opensfm
+    install -Dm755 bin/opensfm_run_all $out/bin/opensfm_run_all
+  '';
 
   disabledTests = [
     "test_run_all" # Matplotlib issues. Broken integration is less useless than a broken build
     "test_match_candidates_from_metadata_bow" # flaky
+  ]
+  ++ lib.optionals (pythonAtLeast "3.14") [
+    # Fragile ref count assertion; fails with Python 3.14+
+    "test_shot_view_ref_count"
   ]
   ++ lib.optionals stdenv.hostPlatform.isDarwin [
     "test_reconstruction_incremental"
@@ -143,8 +154,8 @@ buildPythonPackage rec {
       lib.maintainers.SomeoneSerge
     ];
     license = lib.licenses.bsd2;
-    changelog = "https://github.com/mapillary/OpenSfM/blob/${src.rev}/CHANGELOG.md";
+    changelog = "https://github.com/mapillary/OpenSfM/blob/${finalAttrs.src.rev}/CHANGELOG.md";
     description = "Open source Structure-from-Motion pipeline from Mapillary";
     homepage = "https://opensfm.org/";
   };
-}
+})
