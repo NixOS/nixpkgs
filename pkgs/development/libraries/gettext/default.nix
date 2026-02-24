NixOS/nixpkgs

commit ffd70beac506eb216465badc0e541d05bd04cd2b

gettext: 0.25.1 -> 0.26

pkgs/development/libraries/gettext/default.nix
@@ -15,18 +15,17 @@
 
 stdenv.mkDerivation rec {
   pname = "gettext";
-  version = "0.25.1";
+  version = "0.26";
 
   src = fetchurl {
     url = "mirror://gnu/gettext/${pname}-${version}.tar.gz";
-    hash = "sha256-dG+VXULXHrac52OGnLkmgvCaQGZSjQGLbKej9ICJoIU=";
+    hash = "sha256-Oaz0sDcemxELYABVYqrOWzYx/tmxu57Mz8f1bli7HX8=";
   };
   patches = [
     ./absolute-paths.diff
     # fix reproducibile output, in particular in the grub2 build
     # https://savannah.gnu.org/bugs/index.php?59658
     ./0001-msginit-Do-not-use-POT-Creation-Date.patch
-    ./memory-safety.patch
   ];
 
   outputs = [

pkgs/development/libraries/gettext/memory-safety.patch
@@ -1,44 +1,0 @@
-From: Bruno Haible <bruno@clisp.org>
-Date: Mon, 7 Jul 2025 07:02:41 +0000 (+0200)
-Subject: xgettext: Perl: Fix bug with comment lines longer than 1024 (regr. 2024-09-26).
-X-Git-Url: https://gitweb.git.savannah.gnu.org/gitweb/?p=gettext.git;a=commitdiff_plain;h=f98de965a08d1883a46ba5411922b54cc5125f14
-
-xgettext: Perl: Fix bug with comment lines longer than 1024 (regr. 2024-09-26).
-
-Reported by Alyssa Ross <hi@alyssa.is> in
-<https://lists.gnu.org/archive/html/bug-gettext/2025-07/msg00009.html>.
-
-* gettext-tools/src/x-perl.c (phase2_getc): Move the sb_free call until after
-the savable_comment_add call.
----
-
-diff --git a/gettext-tools/src/x-perl.c b/gettext-tools/src/x-perl.c
-index d3aa50476..312fef371 100644
---- a/gettext-tools/src/x-perl.c
-+++ b/gettext-tools/src/x-perl.c
-@@ -558,7 +558,6 @@ phase2_getc (struct perl_extractor *xp)
- {
-   int lineno;
-   int c;
--  char *utf8_string;
- 
-   c = phase1_getc (xp);
-   if (c == '#')
-@@ -587,12 +586,13 @@ phase2_getc (struct perl_extractor *xp)
-           sb_xappend1 (&buffer, c);
-         }
-       /* Convert it to UTF-8.  */
--      utf8_string =
--        from_current_source_encoding (sb_xcontents_c (&buffer), lc_comment,
-+      const char *contents = sb_xcontents_c (&buffer);
-+      char *utf8_contents =
-+        from_current_source_encoding (contents, lc_comment,
-                                       logical_file_name, lineno);
--      sb_free (&buffer);
-       /* Save it until we encounter the corresponding string.  */
--      savable_comment_add (utf8_string);
-+      savable_comment_add (utf8_contents);
-+      sb_free (&buffer);
-       xp->last_comment_line = lineno;
-     }
-   return c;

commit b6e1c32c803fdb6914775a003b66f6fae35a8b1b

gettext: remove obsolete cygwin patches
Fixes: ffd70beac506 ("gettext: 0.25.1 -> 0.26")

pkgs/development/libraries/gettext/default.nix
@@ -5,7 +5,6 @@
   libiconv,
   bashNonInteractive,
   updateAutotoolsGnuConfigScriptsHook,
-  gnulib,
 }:
 
 # Note: this package is used for bootstrapping fetchurl, and thus
@@ -69,20 +68,6 @@ stdenv.mkDerivation rec {
   ''
   + lib.optionalString stdenv.hostPlatform.isMinGW ''
     sed -i "s/@GNULIB_CLOSE@/1/" */*/unistd.in.h
-  ''
-  + lib.optionalString stdenv.hostPlatform.isCygwin ''
-    for gnulib in \
-      ./libtextstyle/lib \
-      ./gettext-tools/libgettextpo \
-      ./gettext-tools/gnulib-lib \
-      ./gettext-runtime/libasprintf/gnulib-lib \
-      ./gettext-runtime/intl/gnulib-lib \
-      ./gettext-runtime/gnulib-lib
-    do
-      cd "$gnulib"
-      patch -p2 < ${gnulib.patches.memcpy-fix}
-      cd -
-    done
   '';
 
   strictDeps = true;