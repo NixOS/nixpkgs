9813c4226ac5ad807fdef4d75563485d901ecb8b
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-25 07:00:28 +0000

    appium-inspector: 2026.1.3 -> 2026.2.1

--- pkgs/by-name/ap/appium-inspector/package.nix
+++ pkgs/by-name/ap/appium-inspector/package.nix
@@ -11,7 +11,7 @@
 
 let
   electron = electron_39;
-  version = "2026.1.3";
+  version = "2026.2.1";
 in
 
 buildNpmPackage {
@@ -22,10 +22,10 @@ buildNpmPackage {
     owner = "appium";
     repo = "appium-inspector";
     tag = "v${version}";
-    hash = "sha256-9WhSQq3is2qucL1cGQIYPuLM9VkCFIWq2XVU2QioRMo=";
+    hash = "sha256-89u8MifBPh5AwaMFp+aGSzsiwp75Skca/t6OyDSzrGo=";
   };
 
-  npmDepsHash = "sha256-2DNUTvZ6yFL9U8JKw4ZFR81a6Mv9rPXtRu/v/Il/Rhw=";
+  npmDepsHash = "sha256-mwNn7TllWCtr4sif9Wc3FDtK2Icu72/iI+IllhBswHQ=";
   npmFlags = [ "--ignore-scripts" ];
 
   nativeBuildInputs = [
