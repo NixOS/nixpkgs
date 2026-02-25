f45714827be861c9a5b19f0217060afe07c98fbd
Benjamin Sparks <b.sparks@alugha.com>
2026-02-25 18:29:14 +0100

    python3Packages.whisperx: default to a headless ffmpeg
    Avoid depending on GUI dependencies that aren't used by the module anyway

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -20837,7 +20837,7 @@ self: super: with self; {
   whispers = callPackage ../development/python-modules/whispers { };
 
   whisperx = callPackage ../development/python-modules/whisperx {
-    inherit (pkgs) ffmpeg;
+    ffmpeg = pkgs.ffmpeg-headless;
     ctranslate2-cpp = pkgs.ctranslate2;
   };
 
