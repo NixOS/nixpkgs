042632f94ff046190e80eaf4666be16b9a2da5d4
jopejoe1 <jopejoe1@missing.ninja>
2026-02-26 19:02:01 +0100

    notobuild: add setup hook to build notofonts

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9213,6 +9213,8 @@ with pkgs;
 
   mplus-outline-fonts = recurseIntoAttrs (callPackage ../data/fonts/mplus-outline-fonts { });
 
+  notobuilder = with python3Packages; toPythonApplication notobuilder;
+
   noto-fonts-cjk-serif-static = callPackage ../by-name/no/noto-fonts-cjk-serif/package.nix {
     static = true;
   };
