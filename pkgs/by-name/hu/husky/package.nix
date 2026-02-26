4e064d454a42f37a677e173a3e784d8755711ef7
Florian RICHER <florian.richer@protonmail.com>
2026-02-26 15:35:07 +0100

    husky: 8.0.3 -> 9.1.7

--- pkgs/by-name/hu/husky/package.nix
+++ pkgs/by-name/hu/husky/package.nix
@@ -2,20 +2,41 @@
   lib,
   buildNpmPackage,
   fetchFromGitHub,
+
+  nix-update-script,
 }:
 
 buildNpmPackage rec {
   pname = "husky";
-  version = "8.0.3";
+  version = "9.1.7";
 
   src = fetchFromGitHub {
     owner = "typicode";
     repo = "husky";
     rev = "v${version}";
-    hash = "sha256-KoF2+vikgFyCGjfKeaqkC720UVMuvCIn9ApDPKbudsA=";
+    hash = "sha256-rjj6kG0f9dbwc3MOS3sXBp1tNOfbOgWAQzm7MbImMk8=";
   };
 
-  npmDepsHash = "sha256-u1dndTKvInobva+71yI2vPiwrW9vqzAJ2sDAqT9YJsg=";
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/node_modules/husky
+    cp {index,bin}.js $out/lib/node_modules/husky
+
+    mkdir -p $out/bin
+    ln -s $out/lib/node_modules/husky/bin.js $out/bin/husky
+
+    chmod +x $out/lib/node_modules/husky/bin.js
+
+    runHook postInstall
+  '';
+
+  # The project is dependency free
+  dontNpmBuild = true;
+  forceEmptyCache = true;
+  npmDepsHash = "sha256-68/85gX0/9wZNOTpiy3AHqTW6FU9RoaeCKf8IQPOqys=";
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Git hooks made easy";
@@ -23,6 +44,9 @@ buildNpmPackage rec {
     homepage = "https://github.com/typicode/husky";
     changelog = "https://github.com/typicode/husky/releases/tag/v${version}";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ dit7ya ];
+    maintainers = with lib.maintainers; [
+      dit7ya
+      mrdev023
+    ];
   };
 }
