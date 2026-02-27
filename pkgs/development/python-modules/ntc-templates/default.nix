fb38ff78e0fa617bed0dcd441376aa873eb707c2
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-27 02:28:23 +0000

    python3Packages.ntc-templates: 8.1.0 -> 9.0.0

--- pkgs/development/python-modules/ntc-templates/default.nix
+++ pkgs/development/python-modules/ntc-templates/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "ntc-templates";
-  version = "8.1.0";
+  version = "9.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "networktocode";
     repo = "ntc-templates";
     tag = "v${version}";
-    hash = "sha256-J1Icf9UG5IMYBH90Mfxd+p+rk57z2OXQENnoRAaepN4=";
+    hash = "sha256-yVADX0p+2CarJCgHLslgSySUCwcGMKus2T77DiD4LW4=";
   };
 
   build-system = [ poetry-core ];
