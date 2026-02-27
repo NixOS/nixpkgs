875d5fd5a2bcb3d2c28a107f5d27f59680ea5c02
Bryan Lai <bryanlais@gmail.com>
2026-02-27 09:54:12 +0800

    bashdb: improve & build for darwin
    - Pull in the normal bash in buildInputs (instead of bashMinimal which
      was implicit). This avoids various runtime errors.
    - Enable auto update with nix-update.
    - Make available for unix (including darwin).

--- pkgs/by-name/ba/bashdb/package.nix
+++ pkgs/by-name/ba/bashdb/package.nix
@@ -9,6 +9,8 @@
   perl,
 
   python3,
+  bash,
+  nix-update-script,
 }:
 
 stdenv.mkDerivation {
@@ -35,6 +37,7 @@ stdenv.mkDerivation {
   buildInputs = [
     # used at runtime by term-highlight.py
     (python3.withPackages (ps: [ ps.pygments ]))
+    bash
   ];
 
   configureFlags = [
@@ -42,6 +45,8 @@ stdenv.mkDerivation {
     # for now point to self
     "--with-dbg-main=${placeholder "out"}/share/bashdb/bashdb-main.inc"
   ];
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     homepage = "https://bashdb.sourceforge.net/";
@@ -55,6 +60,6 @@ stdenv.mkDerivation {
     maintainers = with lib.maintainers; [
       jk
     ];
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.unix;
   };
 }
