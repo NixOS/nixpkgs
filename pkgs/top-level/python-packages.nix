70b64aa3921e69a92c38bbc1c1aa573d6d6a9d10
kilianar <mail@kilianar.de>
2026-02-27 11:32:56 +0100

    python3Packages.pynormaliz: init at 2.24
    https://github.com/Normaliz/PyNormaliz/releases/tag/v2.24

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -15551,6 +15551,8 @@ self: super: with self; {
 
   pynordpool = callPackage ../development/python-modules/pynordpool { };
 
+  pynormaliz = callPackage ../development/python-modules/pynormaliz { };
+
   pynput = callPackage ../development/python-modules/pynput { };
 
   pynputfix = callPackage ../development/python-modules/pynputfix { };
