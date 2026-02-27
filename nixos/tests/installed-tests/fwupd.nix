4ccbe12a716e84b17c8df22eb7019e5aa6254c6a
Thomas Mühlbacher <tmuehlbacher@posteo.net>
2026-02-27 20:45:26 +0100

    fwupd: 2.0.19 -> 2.1.1
    Diff: https://github.com/fwupd/fwupd/compare/2.0.19...2.1.1
    
    Changelog: https://github.com/fwupd/fwupd/releases/tag/2.1.1
    
    Co-developed-by: Daniel Schaefer <git@danielschaefer.me>
    Signed-off-by: Daniel Schaefer <git@danielschaefer.me>
    Signed-off-by: Thomas Mühlbacher <tmuehlbacher@posteo.net>

--- nixos/tests/installed-tests/fwupd.nix
+++ nixos/tests/installed-tests/fwupd.nix
@@ -3,7 +3,13 @@
 makeInstalledTest {
   tested = pkgs.fwupd;
 
+  # same as fwupd upstream CI
+  testRunnerFlags = [ "--timeout=1200" ];
+
   testConfig = {
+    # for easier debugging
+    systemd.globalEnvironment.FWUPD_VERBOSE = "1";
+
     services.fwupd = {
       enable = true;
       daemonSettings.TestDevices = true;
