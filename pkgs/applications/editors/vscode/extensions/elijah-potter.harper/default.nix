92d554c88bb1e209ff8e896840c7aa7e9f83031c
Jost Alemann <alemann@posteo.com>
2026-02-27 23:00:03 +0100

    harper: 1.8.0 -> 1.9.0
    Changelog: https://github.com/Automattic/harper/releases/tag/v1.9.0
    Diff: https://github.com/Automattic/harper/compare/v1.8.0...v1.9.0

--- pkgs/applications/editors/vscode/extensions/elijah-potter.harper/default.nix
+++ pkgs/applications/editors/vscode/extensions/elijah-potter.harper/default.nix
@@ -13,7 +13,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
     name = "harper";
     publisher = "elijah-potter";
     version = harper.version;
-    hash = "sha256-3pIxfUZzUQ73AKlmcThjcuPA4kG8HBBRU0HOjn1x62g=";
+    hash = "sha256-nK97C9ZYSI6dh4w1ntDP0mbmv6ez3pyAfv/4D30I2sA=";
   };
 
   nativeBuildInputs = [
