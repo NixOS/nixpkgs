04c03c79edbb30f7f5e4b69cfb75de5170e80ea0
XYenon <i@xyenon.bid>
2026-02-27 14:32:00 +0800

    nixos/kvrocks: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -844,6 +844,7 @@ in
     inherit runTest;
     inherit (pkgs) lib;
   };
+  kvrocks = runTest ./kvrocks.nix;
   labgrid = runTest ./labgrid.nix;
   lact = runTest ./lact.nix;
   ladybird = runTest ./ladybird.nix;
