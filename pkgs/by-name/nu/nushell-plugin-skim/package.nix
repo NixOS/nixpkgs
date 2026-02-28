618392e7c01762359ecdda78f6ab2fd7244a3ce1
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-28 02:00:14 +0000

    nushellPlugins.skim: 0.23.1 -> 0.24.1

--- pkgs/by-name/nu/nushell-plugin-skim/package.nix
+++ pkgs/by-name/nu/nushell-plugin-skim/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nu_plugin_skim";
-  version = "0.23.1";
+  version = "0.24.1";
 
   src = fetchFromGitHub {
     owner = "idanarye";
     repo = "nu_plugin_skim";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-CWjds0AWYwrKk1sgBSOalEIYJd2Aymc6XK22Bd9QLuo=";
+    hash = "sha256-a8wC5RowtLiVJXkDr2SDQaaLvK9jjW5x9z/cYCgJMRI=";
   };
 
-  cargoHash = "sha256-mucbl0ow0FjNiDL1BNKT7BMVpMKvmKEz3dP6/9BBRV4=";
+  cargoHash = "sha256-/6dUbz2c0r6oV4NlmkOcu4Awky3fGkf2IkrwBpYg3ZM=";
 
   nativeBuildInputs = lib.optionals stdenv.cc.isClang [ rustPlatform.bindgenHook ];
 
