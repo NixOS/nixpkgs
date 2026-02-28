4ae593608b953f40b64ca6c1c04c69069bd971d2
Steven Allen <steven@stebalien.com>
2026-02-28 14:00:53 -0800

    nixosTests.pulseaudio.*: mark broken on aarch64-linux
    They fail with:
    
        qemu-system-aarch64: Virtio VGA not available

--- nixos/tests/pulseaudio.nix
+++ nixos/tests/pulseaudio.nix
@@ -31,6 +31,7 @@ let
         name = "pulseaudio${lib.optionalString fullVersion "Full"}${lib.optionalString systemWide "-systemWide"}";
         meta = with pkgs.lib.maintainers; {
           maintainers = [ synthetica ] ++ pkgs.pulseaudio.meta.maintainers;
+          broken = pkgs.stdenv.hostPlatform.isAarch64;
         };
 
         nodes.machine =
