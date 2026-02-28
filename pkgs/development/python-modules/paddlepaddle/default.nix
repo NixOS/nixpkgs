f08f5c0b9cb7563ffdb56116c439ca131bf38989
Robert Schütz <nix@dotlambda.de>
2026-02-27 21:16:27 -0800

    python3Packages.paddlepaddle: update dependencies

--- pkgs/development/python-modules/paddlepaddle/default.nix
+++ pkgs/development/python-modules/paddlepaddle/default.nix
@@ -19,8 +19,7 @@
   numpy,
   protobuf,
   pillow,
-  decorator,
-  astor,
+  networkx,
   opt-einsum,
   rdma-core,
   safetensors,
@@ -74,15 +73,18 @@ buildPythonPackage {
 
   buildInputs = lib.optionals cudaSupport [ rdma-core ];
 
+  pythonRelaxDeps = [
+    opt-einsum
+  ];
+
   dependencies = [
     setuptools
     httpx
     numpy
     protobuf
     pillow
-    decorator
-    astor
     opt-einsum
+    networkx
     safetensors
     typing-extensions
   ];
