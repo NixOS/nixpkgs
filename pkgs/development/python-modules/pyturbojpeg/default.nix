c7bccbac152f9692b5df8a3c379ff482bba64fc3
Robert Schütz <nix@dotlambda.de>
2026-02-28 08:01:15 -0800

    python3Packages.pyturbojpeg: 2.0.0 -> 2.2.0
    Diff: https://github.com/lilohuang/PyTurboJPEG/compare/v2.0.0...v2.2.0
    
    Changelog: https://github.com/lilohuang/PyTurboJPEG/releases/tag/v2.2.0

--- pkgs/development/python-modules/pyturbojpeg/default.nix
+++ pkgs/development/python-modules/pyturbojpeg/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pyturbojpeg";
-  version = "2.0.0";
+  version = "2.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "lilohuang";
     repo = "PyTurboJPEG";
     tag = "v${version}";
-    hash = "sha256-yITX+6mP8AihaTFjZG9A5U6pAUTdPy6UDbbjnznwuqI=";
+    hash = "sha256-Sue8S/2/dY3aF7fihHuqpHXotcXcijcqAXGCJn8Z5VE=";
   };
 
   patches = [
