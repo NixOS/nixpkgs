478b97efb3327242a276cac11f489b555db2ee35
n <54501296+2hexed@users.noreply.github.com>
2026-02-28 19:56:09 +0530

    nixos/chromium: fix top level hardcoded policies

--- nixos/modules/programs/chromium.nix
+++ nixos/modules/programs/chromium.nix
@@ -9,14 +9,8 @@ let
   cfg = config.programs.chromium;
 
   defaultProfile = lib.filterAttrs (_: v: v != null) {
-    HomepageLocation = cfg.homepageLocation;
-    DefaultSearchProviderEnabled = cfg.defaultSearchProviderEnabled;
-    DefaultSearchProviderSearchURL = cfg.defaultSearchProviderSearchURL;
-    DefaultSearchProviderSuggestURL = cfg.defaultSearchProviderSuggestURL;
     ExtensionInstallForcelist = cfg.extensions;
   };
-
-  mergedExtraOpts = cfg.policies // cfg.extraOpts;
 
   chromiumEtcConfig = {
     # Plasma integration
@@ -31,8 +25,8 @@ let
       text = builtins.toJSON defaultProfile;
     };
 
-    "chromium/policies/managed/extra.json" = lib.mkIf (mergedExtraOpts != { }) {
-      text = builtins.toJSON mergedExtraOpts;
+    "chromium/policies/managed/extra.json" = lib.mkIf (cfg.policies != { }) {
+      text = builtins.toJSON cfg.policies;
     };
 
     # First-run preferences
@@ -42,6 +36,8 @@ let
   };
 in
 {
+  disabledModules = [ "programs/chromium.nix" ];
+
   options.programs.chromium = {
     enable = lib.mkEnableOption "Enable Chromium browser";
 
@@ -66,44 +62,6 @@ in
         Chromium extension force-install list.
         Extension IDs come from Chrome Web Store URLs.
       '';
-    };
-
-    homepageLocation = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = ''
-        Sets the URL that Chromium will load when the Home button is clicked or when a new window/tab is opened (depending on user settings). This corresponds to the Chromium HomepageLocation [Managed Policy](https://chromeenterprise.google/policies/#HomepageLocation). Setting it to null allows the user to configure the value or uses Chromium's internal defaults.
-      '';
-    };
-
-    defaultSearchProviderEnabled = lib.mkOption {
-      type = lib.types.nullOr lib.types.bool;
-      default = null;
-      description = ''
-        Controls whether the default search provider is enforced. This corresponds to the Chromium DefaultSearchProviderEnabled [Managed Policy](https://chromeenterprise.google/policies/#DefaultSearchProviderEnabled). Setting to true enables the provider configured by defaultSearchProviderSearchURL, and false disables it. Set to null to use Chromium's default behavior (which is typically enabled).
-      '';
-    };
-
-    defaultSearchProviderSearchURL = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = ''
-        Sets the URL pattern to be used by the default search provider when performing a search. This corresponds to the Chromium DefaultSearchProviderSearchURL [Managed Policy](https://chromeenterprise.google/policies/#DefaultSearchProviderSearchURL). This option only takes effect if programs.chromium.defaultSearchProviderEnabled is set to true. The URL must include the placeholder {searchTerms}.
-      '';
-    };
-
-    defaultSearchProviderSuggestURL = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = ''
-        Sets the URL pattern for fetching search suggestions from the default search provider as the user types. This corresponds to the Chromium DefaultSearchProviderSuggestURL [Managed Policy](https://chromeenterprise.google/policies/#DefaultSearchProviderSuggestURL). This option only takes effect if programs.chromium.defaultSearchProviderEnabled is set to true. The URL must include the placeholder {searchTerms}.
-      '';
-    };
-
-    extraOpts = lib.mkOption {
-      type = lib.types.attrs;
-      default = { };
-      description = "Additional Chromium enterprise policy settings.";
     };
 
     initialPrefs = lib.mkOption {
