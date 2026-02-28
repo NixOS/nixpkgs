8cf75f0855f6bad4045552f7d89d3d3343ea8153
Matej Urbas <matej.urbas@gmail.com>
2026-02-28 08:48:06 +0000

    home-assistant-custom-components.oref_alert: 4.1.1 -> 5.2.0

--- pkgs/servers/home-assistant/custom-components/oref_alert/package.nix
+++ pkgs/servers/home-assistant/custom-components/oref_alert/package.nix
@@ -6,21 +6,31 @@
   shapely,
   paho-mqtt,
   pytestCheckHook,
+  pytest-cov-stub,
   pytest-homeassistant-custom-component,
   pytest-freezer,
+  pythonOlder,
 }:
 
 buildHomeAssistantComponent rec {
   owner = "amitfin";
   domain = "oref_alert";
-  version = "4.1.1";
+  version = "5.2.0";
 
   src = fetchFromGitHub {
     owner = "amitfin";
     repo = "oref_alert";
     tag = "v${version}";
-    hash = "sha256-nWp8cG0lFYUEO11lcZGkqx5QvOSfSVnqIqpHA8YAN30=";
+    hash = "sha256-Az2pZriKPXgLK1QarwycWUEn5VBufUQdYomYlpStOrY=";
   };
+
+  # This package uses the `if TYPE_CHECKING:` pattern in the test code to avoid
+  # expensive imports (see https://docs.python.org/3/library/typing.html#typing.TYPE_CHECKING for more info on this pattern).
+  # In Python 3.13, this makes tests fail with errors like "NameError: name 'AiohttpClientMocker' is not defined" error.
+  # However, Python 3.14 introduces lazy annotations (see https://peps.python.org/pep-0563/), which should fix this problem.
+  postPatch = lib.optionalString (pythonOlder "3.14") ''
+    find tests -name '*.py' -exec sh -c 'for f; do if ! grep -q "^from __future__ import annotations" "$f"; then sed -i "1i from __future__ import annotations" "$f"; fi; done' sh {} +
+  '';
 
   dependencies = [
     aiofiles
@@ -32,6 +42,7 @@ buildHomeAssistantComponent rec {
 
   nativeCheckInputs = [
     pytestCheckHook
+    pytest-cov-stub
     pytest-homeassistant-custom-component
     pytest-freezer
   ];
