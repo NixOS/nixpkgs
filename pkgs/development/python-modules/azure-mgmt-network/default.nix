c80bf56661c52a21df3d1ab8b906f5c620ccd7d0
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-01 04:54:48 +0000

    python3Packages.azure-mgmt-network: 30.1.0 -> 30.2.0

--- pkgs/development/python-modules/azure-mgmt-network/default.nix
+++ pkgs/development/python-modules/azure-mgmt-network/default.nix
@@ -11,13 +11,13 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-network";
-  version = "30.1.0";
+  version = "30.2.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "azure_mgmt_network";
     inherit version;
-    hash = "sha256-QUZKBTZJYvcRmgToqYPpCTXBoksu86Q6wQoOzJIIcJ8=";
+    hash = "sha256-mxfCWeY0SAiqqAo0u8SxPxa8ARhd2dsTfqoK4mZkhho=";
   };
 
   build-system = [ setuptools ];
