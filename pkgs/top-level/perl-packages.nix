5bee8483643b02f8dd4e362d603f4d2934868855
Stig Palmquist <git@stig.io>
2026-03-01 22:50:38 +0100

    perlPackages.NetOAuth: 0.28 -> 0.31

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -26280,20 +26280,21 @@ with self;
     };
   };
 
-  NetOAuth = buildPerlModule {
+  NetOAuth = buildPerlPackage {
     pname = "Net-OAuth";
-    version = "0.28";
+    version = "0.31";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/K/KG/KGRENNAN/Net-OAuth-0.28.tar.gz";
-      hash = "sha256-e/wxnaCsV44Ali81o1DPUREKOjEwFtH9wwciAooikEw=";
+      url = "mirror://cpan/authors/id/R/RR/RRWO/Net-OAuth-0.31.tar.gz";
+      hash = "sha256-wncPAoJp/NtlFQ8rsxP4pva13vTuIbVhn1y/pAMbfFQ=";
     };
     buildInputs = [ TestWarn ];
     propagatedBuildInputs = [
       ClassAccessor
       ClassDataInheritable
-      DigestHMAC
-      DigestSHA1
-      LWP
+      CryptURandom
+      TestWarn
+      URI
+      libwwwperl
     ];
     meta = {
       description = "Implementation of the OAuth protocol";
