e5dcc5953ffb7e12f67637a5cffa0568631646c7
Stig Palmquist <git@stig.io>
2026-03-01 22:51:29 +0100

    perlPackages.PlackMiddlewareSession: 0.33 -> 0.36

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -28877,23 +28877,26 @@ with self;
 
   PlackMiddlewareSession = buildPerlModule {
     pname = "Plack-Middleware-Session";
-    version = "0.33";
+    version = "0.36";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/M/MI/MIYAGAWA/Plack-Middleware-Session-0.33.tar.gz";
-      hash = "sha256-T/miydGK2ASbRd/ze5vdQSIeLC8eFrr7gb/tyIxRpO4=";
+      url = "mirror://cpan/authors/id/M/MI/MIYAGAWA/Plack-Middleware-Session-0.36.tar.gz";
+      hash = "sha256-kqWDFliBDSNzLm47rnpEofpafYpqbm3NdbkcapwktGY=";
     };
     propagatedBuildInputs = [
+      CookieBaker
+      CryptSysRandom
       DigestHMAC
       Plack
     ];
     buildInputs = [
+      ApacheTest
       HTTPCookies
-      LWP
       ModuleBuildTiny
       TestFatal
       TestRequires
       TestSharedFork
       TestTCP
+      libwwwperl
     ];
     meta = {
       description = "Middleware for session management";
