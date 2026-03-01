ee0b6078bc9a2bb52a68b749d0f356ba0addd589
Stig Palmquist <git@stig.io>
2026-03-01 22:53:13 +0100

    perlPackages.TypeTiny: 2.004000 -> 2.010001

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -37860,10 +37860,10 @@ with self;
 
   TypeTiny = buildPerlPackage {
     pname = "Type-Tiny";
-    version = "2.004000";
+    version = "2.010001";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/T/TO/TOBYINK/Type-Tiny-2.004000.tar.gz";
-      hash = "sha256-aX5/d17fyF9M8HeS0E/RmwnCUoX5j1k46O/E90UHoSg=";
+      url = "mirror://cpan/authors/id/T/TO/TOBYINK/Type-Tiny-2.010001.tar.gz";
+      hash = "sha256-zVg+SYMDnzUFLUkPDEQ5EkumZ9TwnE467EfeUgD5kho=";
     };
     propagatedBuildInputs = [ ExporterTiny ];
     buildInputs = [ TestMemoryCycle ];
