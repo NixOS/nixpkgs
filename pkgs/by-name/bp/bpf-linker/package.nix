3969fd50edbf7839af91b9f2132c6299b0699d56
HaeNoe <git@haenoe.party>
2026-03-01 01:32:59 +0100

    bpf-linker: 0.9.15 -> 0.10.2
    - change patch to remove sysroot override completely
    - change patch to override libllvm location via environment variable
      instead of building directory structure with `symlinkJoin`

--- pkgs/by-name/bp/bpf-linker/package.nix
+++ pkgs/by-name/bp/bpf-linker/package.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  stdenv,
   rustPlatform,
   fetchFromGitHub,
   btfdump,
@@ -8,33 +7,39 @@
   zlib,
   libxml2,
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "bpf-linker";
-  version = "0.9.15";
+  version = "0.10.2";
 
   src = fetchFromGitHub {
     owner = "aya-rs";
     repo = "bpf-linker";
     tag = "v${version}";
-    hash = "sha256-5HXYtAn6KaFXsiA3Nt0IwmFLOXBhZWYrD8cMZ8rZ1fk=";
+    hash = "sha256-jtTDjbE2F5uj9lSTO0CuOY0fXp5IZKKMJBgAStk0c48=";
   };
 
-  cargoHash = "sha256-coIcd6WjVQM/b51jwkG8It/wubXx6wuuPlzzelPFE38=";
+  cargoHash = "sha256-EPd99fRJaLT5xYsZTLb+c4a/vS91JRyOmTcgNC9x9fM=";
 
   buildNoDefaultFeatures = true;
   buildFeatures = [ "llvm-${lib.versions.major rustc.llvm.version}" ];
 
-  nativeBuildInputs = [ rustc.llvm ];
-
   buildInputs = [
     zlib
     libxml2
+    (lib.getLib rustc.llvm)
   ];
+
+  patches = [
+    ./remove-sysroot-override.patch
+    ./override-libllvm-location.patch
+  ];
+
+  env.LLVM_LIB_PATH = "${lib.getLib rustc.llvm}/lib";
 
   nativeCheckInputs = [
     btfdump
     rustc.llvmPackages.clang.cc
+    rustc.llvm
   ];
 
   meta = {
@@ -46,8 +51,5 @@ rustPlatform.buildRustPackage rec {
       mit
     ];
     maintainers = with lib.maintainers; [ nickcao ];
-    # llvm-sys crate locates llvm by calling llvm-config
-    # which is not available when cross compiling
-    broken = stdenv.buildPlatform != stdenv.hostPlatform;
   };
 }
