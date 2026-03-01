a40bd6a7ad7c5b24aee5ded20567047ca040042b
Stig Palmquist <git@stig.io>
2026-03-01 22:53:46 +0100

    perlPackages.libwwwperl: 6.72 -> 6.81

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -19015,18 +19015,20 @@ with self;
 
   libwwwperl = buildPerlPackage {
     pname = "libwww-perl";
-    version = "6.72";
+    version = "6.81";
     src = fetchurl {
-      url = "mirror://cpan/authors/id/O/OA/OALDERS/libwww-perl-6.72.tar.gz";
-      hash = "sha256-6bg1T9XiC+IHr+I93VhPzVm/gpmNwHfez2hLodrloF0=";
+      url = "mirror://cpan/authors/id/O/OA/OALDERS/libwww-perl-6.81.tar.gz";
+      hash = "sha256-qzBVLxlOi1rjrAiFEy/R1OoExMf+ZVV2W5jwGvcMFzY=";
     };
     buildInputs = [
+      HTTPCookieJar
       HTTPDaemon
       TestFatal
       TestNeeds
       TestRequiresInternet
     ];
     propagatedBuildInputs = [
+      ApacheTest
       EncodeLocale
       FileListing
       HTMLParser
