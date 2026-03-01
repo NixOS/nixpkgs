b58a3b81654ba60d3ea8dce2fba95ae555c16bae
kjubybot <kjubybot@proton.me>
2026-03-01 20:46:29 +0100

    pihole-ftl: remove patches

--- pkgs/by-name/pi/pihole-ftl/package.nix
+++ pkgs/by-name/pi/pihole-ftl/package.nix
@@ -26,11 +26,6 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-OpbBd+HS/gwcWNe/6VB3glout1sifJ8o5EnKuXfyZ/o=";
   };
 
-  patches = [
-    # https://github.com/pi-hole/FTL/pull/2610: Fix authentication redirect when webhome is /
-    ./disable-redirect-root.patch
-  ];
-
   nativeBuildInputs = [
     cmake
     xxd
@@ -46,9 +41,7 @@ stdenv.mkDerivation (finalAttrs: {
     readline
   ];
 
-  cmakeFlags = [
-    (lib.cmakeBool "STATIC" stdenv.hostPlatform.isStatic)
-  ];
+  cmakeFlags = [ (lib.cmakeBool "STATIC" stdenv.hostPlatform.isStatic) ];
 
   postPatch = ''
     substituteInPlace src/version.c.in \
@@ -63,10 +56,6 @@ stdenv.mkDerivation (finalAttrs: {
     substituteInPlace src/api/action.c \
       --replace-fail "/usr/local/bin/pihole" "pihole" \
       --replace-fail "execv" "execvp"
-
-    substituteInPlace src/database/network-table.c \
-      --replace-fail "ip neigh show" "${lib.getExe' iproute2 "ip"} neigh show" \
-      --replace-fail "ip address show" "${lib.getExe' iproute2 "ip"} address show"
   '';
 
   installPhase = ''
