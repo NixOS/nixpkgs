9dffb82ef52157230ed9b408a379f74d1a920fce
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-02 07:28:22 +0000

    python3Packages.pylxd: 2.3.7 -> 2.3.8

--- pkgs/development/python-modules/pylxd/default.nix
+++ pkgs/development/python-modules/pylxd/default.nix
@@ -17,14 +17,14 @@
 
 buildPythonPackage rec {
   pname = "pylxd";
-  version = "2.3.7";
+  version = "2.3.8";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "canonical";
     repo = "pylxd";
     tag = version;
-    hash = "sha256-UbDkau3TLwFxWZxJGNF5hgtGn6JgVq5L2CvUgnb4IC8=";
+    hash = "sha256-AWRpMXjmdfG8y57W7M7eJvVJTlstNxmf4YvR+GSBxeY=";
   };
 
   pythonRelaxDeps = [ "urllib3" ];
