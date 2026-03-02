236923c19cd04cffa2116c65bfb09f6cd081be04
Sarah Clark <seclark@nextquestion.net>
2026-03-02 14:36:32 -0800

    python3Packages.langchain-huggingface: migrate to finalAttrs

--- pkgs/development/python-modules/langchain-huggingface/default.nix
+++ pkgs/development/python-modules/langchain-huggingface/default.nix
@@ -31,7 +31,7 @@
   gitUpdater,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "langchain-huggingface";
   version = "1.2.1";
   pyproject = true;
@@ -39,11 +39,11 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "langchain-ai";
     repo = "langchain";
-    tag = "langchain-huggingface==${version}";
+    tag = "langchain-huggingface==${finalAttrs.version}";
     hash = "sha256-I6n7UNEbGqlyzT663k7+YpcaB/+rE9RlkqIToupoEyY=";
   };
 
-  sourceRoot = "${src.name}/libs/partners/huggingface";
+  sourceRoot = "${finalAttrs.src.name}/libs/partners/huggingface";
 
   build-system = [ hatchling ];
 
@@ -88,7 +88,7 @@ buildPythonPackage rec {
   };
 
   meta = {
-    changelog = "https://github.com/langchain-ai/langchain/releases/tag/${src.tag}";
+    changelog = "https://github.com/langchain-ai/langchain/releases/tag/${finalAttrs.src.tag}";
     description = "Integration package connecting Huggingface related classes and LangChain";
     homepage = "https://github.com/langchain-ai/langchain/tree/master/libs/partners/huggingface";
     license = lib.licenses.mit;
@@ -97,4 +97,4 @@ buildPythonPackage rec {
       sarahec
     ];
   };
-}
+})
