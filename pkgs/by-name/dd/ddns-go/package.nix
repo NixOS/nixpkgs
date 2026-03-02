5dbb81db45831297f378e11242bc2a9777dcf9b5
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-02 14:37:43 +0000

    ddns-go: 6.15.0 -> 6.16.0

--- pkgs/by-name/dd/ddns-go/package.nix
+++ pkgs/by-name/dd/ddns-go/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "ddns-go";
-  version = "6.15.0";
+  version = "6.16.0";
 
   src = fetchFromGitHub {
     owner = "jeessy2";
     repo = "ddns-go";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-NDIevPRIbRqh97IWk4lCqmjobepVMeG5QFKUJdw9Lyo=";
+    hash = "sha256-BpNdaEk1Cw91i8JTm3Zk6rzS9kcYP006gyLHh34i0O8=";
   };
 
-  vendorHash = "sha256-EGhZyoitQ7l0sQZbono2pKhQZJEnGrennMz453Lrbek=";
+  vendorHash = "sha256-oOhqr0D/K9FITCbz2jt+We6gyv85ipL3enbr8YDDSIg=";
 
   ldflags = [
     "-X main.version=${finalAttrs.version}"
