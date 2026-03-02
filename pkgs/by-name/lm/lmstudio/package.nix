f84179233f869a580e53e3cda40d4cf9831b50d2
Nikita Morozov <nikitwww@gmail.com>
2026-03-02 03:16:15 +0300

    lmstudio: add linux-aarch64 package

--- pkgs/by-name/lm/lmstudio/package.nix
+++ pkgs/by-name/lm/lmstudio/package.nix
@@ -7,6 +7,8 @@
 let
   pname = "lmstudio";
 
+  version_aarch64-linux = "0.4.5-2";
+  hash_aarch64-linux = "sha256-BeF9FNMde9RW2icdq07zkWQafge7ViWKvR+xupRIdjE=";
   version_aarch64-darwin = "0.4.5-2";
   hash_aarch64-darwin = "sha256-mSszzDsoXv2D9Ky3K/P2Nn/mixq3HzGMonS1I4mz5+s=";
   version_x86_64-linux = "0.4.5-2";
@@ -20,12 +22,13 @@ let
     maintainers = with lib.maintainers; [ crertel ];
     platforms = [
       "x86_64-linux"
+      "aarch64-linux"
       "aarch64-darwin"
     ];
     sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
   };
 in
-if stdenv.hostPlatform.isDarwin then
+if stdenv.hostPlatform.system == "aarch64-darwin" then
   callPackage ./darwin.nix {
     inherit pname meta;
     passthru.updateScript = ./update.sh;
@@ -34,6 +37,16 @@ if stdenv.hostPlatform.isDarwin then
       args.url
         or "https://installers.lmstudio.ai/darwin/arm64/${version_aarch64-darwin}/LM-Studio-${version_aarch64-darwin}-arm64.dmg";
     hash = args.hash or hash_aarch64-darwin;
+  }
+else if stdenv.hostPlatform.system == "aarch64-linux" then
+  callPackage ./linux.nix {
+    inherit pname meta;
+    passthru.updateScript = ./update.sh;
+    version = version_aarch64-linux;
+    url =
+      args.url
+        or "https://installers.lmstudio.ai/linux/arm64/${version_aarch64-linux}/LM-Studio-${version_aarch64-linux}-arm64.AppImage";
+    hash = args.hash or hash_aarch64-linux;
   }
 else
   callPackage ./linux.nix {
