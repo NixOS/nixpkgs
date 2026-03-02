e0d4027b5f4006c316c7238c3990e45113a9045f
PerchunPak <git@perchun.it>
2026-03-02 19:26:40 +0100

    zellijPlugins: init
    (cherry picked from commit 6c6cee98a0f8603e6645b3ebe8875fd6f46318ac)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3397,6 +3397,8 @@ with pkgs;
     nvidiaSupport = true;
   };
 
+  zellijPlugins = recurseIntoAttrs (callPackage ../by-name/ze/zellij/plugins { });
+
   zstd = callPackage ../tools/compression/zstd {
     cmake = buildPackages.cmakeMinimal;
   };
