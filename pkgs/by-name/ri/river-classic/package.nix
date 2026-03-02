7d9c7ad82e8b913a6924527396340f1f236a8c13
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-03-02 15:16:59 -0800

    river-classic: cleanup

--- pkgs/by-name/ri/river-classic/package.nix
+++ pkgs/by-name/ri/river-classic/package.nix
@@ -1,28 +1,32 @@
 {
-  lib,
-  stdenv,
   callPackage,
   fetchFromCodeberg,
+  lib,
   libGL,
-  libx11,
   libevdev,
   libinput,
+  libx11,
   libxkbcommon,
   pixman,
   pkg-config,
   scdoc,
+  stdenv,
   udev,
   versionCheckHook,
   wayland,
   wayland-protocols,
   wayland-scanner,
+  withManpages ? true,
   wlroots_0_19,
   xwayland,
-  zig_0_15,
-  withManpages ? true,
   xwaylandSupport ? true,
+  zig_0_15,
 }:
 
+let
+  wlroots = wlroots_0_19;
+  zig = zig_0_15;
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "river-classic";
   version = "0.3.14";
@@ -42,7 +46,7 @@ stdenv.mkDerivation (finalAttrs: {
     pkg-config
     wayland-scanner
     xwayland
-    zig_0_15
+    zig
   ]
   ++ lib.optional withManpages scdoc;
 
@@ -55,7 +59,7 @@ stdenv.mkDerivation (finalAttrs: {
     udev
     wayland
     wayland-protocols
-    wlroots_0_19
+    wlroots
   ]
   ++ lib.optional xwaylandSupport libx11;
 
@@ -67,8 +71,8 @@ stdenv.mkDerivation (finalAttrs: {
   ++ lib.optional xwaylandSupport "-Dxwayland";
 
   postInstall = ''
-    install contrib/river.desktop -Dt $out/share/wayland-sessions
-    install -Dm755 example/init -t $out/example/
+    install -D example/init --target-directory=$out/example
+    install -Dm644 contrib/river.desktop --target-directory=$out/share/wayland-sessions
   '';
 
   doInstallCheck = true;
@@ -81,24 +85,25 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   meta = {
-    homepage = "https://codeberg.org/river/river-classic";
     description = "Dynamic tiling wayland compositor";
     longDescription = ''
-      river-classic is a dynamic tiling Wayland compositor with flexible runtime
-      configuration.
+      river-classic is a dynamic tiling Wayland compositor with
+      flexible runtime configuration.
 
-      It is a fork of river 0.3 intended for users that are happy with how river 0.3
-      works and do not wish to deal with the majorly breaking changes planned for
-      the river 0.4.0 release.
+      It is a fork of [river](https://codeberg.org/river/river) 0.3
+      intended for users that are happy with how river 0.3 works and
+      do not wish to deal with the majorly breaking changes planned
+      for the river 0.4.0 release.
     '';
     changelog = "https://codeberg.org/river/river-classic/releases/tag/v${finalAttrs.version}";
+    homepage = "https://codeberg.org/river/river-classic";
     license = lib.licenses.gpl3Only;
+    mainProgram = "river";
     maintainers = with lib.maintainers; [
       adamcstephens
       moni
       rodrgz
     ];
-    mainProgram = "river";
     platforms = lib.platforms.linux;
   };
 })
