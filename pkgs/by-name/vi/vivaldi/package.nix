5e610ff269c5190a08250b63f6bfca586356756d
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-03 12:07:40 +0000

    vivaldi: 7.8.3925.73 -> 7.8.3925.74

--- pkgs/by-name/vi/vivaldi/package.nix
+++ pkgs/by-name/vi/vivaldi/package.nix
@@ -66,7 +66,7 @@
 
 stdenv.mkDerivation rec {
   pname = "vivaldi";
-  version = "7.8.3925.73";
+  version = "7.8.3925.74";
 
   suffix =
     {
@@ -79,8 +79,8 @@ stdenv.mkDerivation rec {
     url = "https://downloads.vivaldi.com/stable/vivaldi-stable_${version}-1_${suffix}.deb";
     hash =
       {
-        aarch64-linux = "sha256-qj41tJMWfto+NEqCqTvLAayJG5Upf/iKb5tQGRp/0y8=";
-        x86_64-linux = "sha256-A5Ab9hr402e/y7ENoWy2fWD+aw0rBd7ZxEnAXiI8Tks=";
+        aarch64-linux = "sha256-mjincXiugFWW4dnJEWC3AnBc7bk+pGmxS8w9kUJhTpM=";
+        x86_64-linux = "sha256-v7fAE8iUVYJCHnURD/XLpMz83X0RDK0IYrKmSMUtmxA=";
       }
       .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
   };
