f14edbcd015827b6f3d69a447a09c5cc30168a3f
Guy Chronister <guychronister@outlook.com>
2026-03-03 13:33:46 -0600

    dexdump: reduce closure size, extract from zip file, patch ELF binary

--- pkgs/by-name/de/dexdump/package.nix
+++ pkgs/by-name/de/dexdump/package.nix
@@ -1,22 +1,39 @@
 {
-  androidenv,
   stdenv,
+  fetchurl,
+  unzip,
+  patchelf,
+  lib,
 }:
-let
-  buildTools = builtins.head androidenv.androidPkgs.build-tools;
-in
+
 stdenv.mkDerivation (finalAttrs: {
   pname = "dexdump";
-  version = buildTools.version;
+  version = "36.1";
 
-  buildCommand = ''
+  src = fetchurl {
+    url = "https://dl.google.com/android/repository/build-tools_r${finalAttrs.version}_linux.zip";
+    hash = "sha256-p7WInkp5/POwl2vvQNQB9CQPse7YkdnZEWnaERHhHXg=";
+  };
+
+  nativeBuildInputs = [
+    unzip
+    patchelf
+  ];
+
+  installPhase = ''
     mkdir -p $out/bin
-    ln -s ${buildTools}/libexec/android-sdk/build-tools/${finalAttrs.version}/dexdump \
-    $out/bin/dexdump
+    cp dexdump $out/bin/
+
+    # Patch ELF interpreter and RPATH so the binary runs on NixOS
+    patchelf \
+      --set-interpreter ${stdenv.cc.bintools.dynamicLinker} \
+      --set-rpath ${stdenv.cc.cc.lib}/lib \
+      $out/bin/dexdump
   '';
 
   meta = {
-    inherit (buildTools.meta) license platforms;
     description = "Dexdump from Android SDK build-tools";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.unix;
   };
 })
