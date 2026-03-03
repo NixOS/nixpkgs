285f3e5a20aa5b6d090305c4684c9492debec989
Robert Schütz <nix@dotlambda.de>
2026-03-03 13:53:42 -0800

    python3Packages.aiohomematic-config: 2026.2.10 -> 2026.3.0
    Diff: https://github.com/sukramj/aiohomematic-config/compare/2026.2.10...2026.3.0
    
    Changelog: https://github.com/sukramj/aiohomematic-config/blob/2026.3.0/changelog.md

--- pkgs/development/python-modules/aiohomematic-config/default.nix
+++ pkgs/development/python-modules/aiohomematic-config/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "aiohomematic-config";
-  version = "2026.2.10";
+  version = "2026.3.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "sukramj";
     repo = "aiohomematic-config";
     tag = finalAttrs.version;
-    hash = "sha256-gb6cxo2gTQU95G76YE46/U1O0Tmj1CRq0+lMLkDQzfg=";
+    hash = "sha256-lIvyFS/m1TwAj8und8EoX2E9ngM4rUw9BkfpETuwUGc=";
   };
 
   build-system = [ setuptools ];
