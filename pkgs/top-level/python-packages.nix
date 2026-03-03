26b342e265ae932caf11cee7a3cc71e337470a0b
Burfeind, Jan-Niklas <jan-niklas.burfeind@sennheiser.com>
2026-03-03 18:21:31 +0100

    python3Packages.py-netgear-plus: init at 0.4.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13046,6 +13046,8 @@ self: super: with self; {
 
   py-multihash = callPackage ../development/python-modules/py-multihash { };
 
+  py-netgear-plus = callPackage ../development/python-modules/py-netgear-plus { };
+
   py-nextbusnext = callPackage ../development/python-modules/py-nextbusnext { };
 
   py-nightscout = callPackage ../development/python-modules/py-nightscout { };
