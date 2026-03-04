912f142de65c4edd9d5a59960fd5612c9f5199e7
Aliaksandr <grubian2@gmail.com>
2026-03-04 19:08:44 +0200

    openssl_oqs, openssl_legacy: migrate to mkPackageVariants

--- pkgs/by-name/op/openssl/variants.nix
+++ pkgs/by-name/op/openssl/variants.nix
@@ -1,8 +1,30 @@
 {
   lib,
   stdenv,
+  oqs-provider,
 }:
 
+let
+  v3_6 = {
+    version = "3.6.1";
+    hash = "sha256-sb/tzVson/Iq7ofJ1gD1FXZ+v0X3cWjLbWTyMfUYqC4=";
+    patches = [
+      ./3.0/nix-ssl-cert-file.patch
+      ./3.0/openssl-disable-kernel-detection.patch
+
+      (
+        if stdenv.hostPlatform.isDarwin then
+          ./3.5/use-etc-ssl-certs-darwin.patch
+        else
+          ./3.5/use-etc-ssl-certs.patch
+      )
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isMinGW [
+      ./3.5/fix-mingw-linking.patch
+    ]
+    ++ lib.optional stdenv.hostPlatform.isCygwin ./openssl-3.0.18-skip-dllmain-detach.patch;
+  };
+in
 {
   v1_1 = {
     version = "1.1.1w";
@@ -53,25 +75,28 @@
     ++ lib.optional stdenv.hostPlatform.isCygwin ./openssl-3.0.18-skip-dllmain-detach.patch;
   };
 
-  v3_6 = {
-    version = "3.6.1";
-    hash = "sha256-sb/tzVson/Iq7ofJ1gD1FXZ+v0X3cWjLbWTyMfUYqC4=";
-    patches =
-      [
-        ./3.0/nix-ssl-cert-file.patch
-        ./3.0/openssl-disable-kernel-detection.patch
+  inherit v3_6;
 
-        (
-          if stdenv.hostPlatform.isDarwin then
-            ./3.5/use-etc-ssl-certs-darwin.patch
-          else
-            ./3.5/use-etc-ssl-certs.patch
-        )
-      ]
-      ++ lib.optionals stdenv.hostPlatform.isMinGW [
-        ./3.5/fix-mingw-linking.patch
-      ]
-      ++
-        lib.optional stdenv.hostPlatform.isCygwin ./openssl-3.0.18-skip-dllmain-detach.patch;
+  oqs = v3_6 // {
+    overrideArgs = {
+      providers = [
+        {
+          name = "oqsprovider";
+          package = oqs-provider;
+        }
+      ];
+      autoloadProviders = true;
+      extraINIConfig = {
+        tls_system_default = {
+          Groups = "X25519MLKEM768:X25519:P-256:X448:P-521:ffdhe2048:ffdhe3072";
+        };
+      };
+    };
+  };
+
+  legacy = v3_6 // {
+    overrideArgs = {
+      conf = ./3.0/legacy.cnf;
+    };
   };
 }
