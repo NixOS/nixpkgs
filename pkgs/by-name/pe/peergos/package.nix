8c2f48810308542b8301d10d079587f251ea3527
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-04 17:31:01 +0000

    peergos: 1.18.0 -> 1.20.0

--- pkgs/by-name/pe/peergos/package.nix
+++ pkgs/by-name/pe/peergos/package.nix
@@ -41,12 +41,12 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "peergos";
-  version = "1.18.0";
+  version = "1.20.0";
   src = fetchFromGitHub {
     owner = "Peergos";
     repo = "web-ui";
     rev = "v${version}";
-    hash = "sha256-zE7BsbZV1KIPD0sn1rSlMxzNF+JcucG382Ek/N9vO8o=";
+    hash = "sha256-1gFOSe1xMR6OX9FkmZACNfpEIa84Pyvuz3t0stpZKdY=";
     fetchSubmodules = true;
   };
 
