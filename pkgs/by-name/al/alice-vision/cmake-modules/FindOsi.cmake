84324ab6969af4bdbabbd52e0b1df792708dface
whoomee <git@miglix.eu>
2026-03-05 13:04:03 +0100

    alice-vision: init at 3.3.0
    Co-Authored-By: Isidor Zeuner <nix@quidecco.pl>

---
+++ pkgs/by-name/al/alice-vision/cmake-modules/FindOsi.cmake
@@ -0,0 +1,23 @@
+find_package(PkgConfig REQUIRED)
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(PC_OSI REQUIRED IMPORTED_TARGET osi)
+
+  add_library(Coin::Osi ALIAS PkgConfig::PC_OSI)
+endif()
+
+find_path(OSI_INCLUDE_DIRS
+          NAMES OsiConfig.h
+          HINTS ${PC_OSI_INCLUDE_DIRS})
+
+if (EXISTS "${OSI_INCLUDE_DIRS}/OsiConfig.h")
+  file(STRINGS "${OSI_INCLUDE_DIRS}/OsiConfig.h" osi_version_str REGEX "^#define[\t ]+OSI_VERSION[\t ]+\".*\"")
+  string(REGEX REPLACE "^#define[\t ]+OSI_VERSION[\t ]+\"([^\"]*)\".*" "\\1" Osi_VERSION "${osi_version_str}")
+endif()
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Osi
+  REQUIRED_VARS OSI_INCLUDE_DIRS
+  VERSION_VAR Osi_VERSION
+	)
+
