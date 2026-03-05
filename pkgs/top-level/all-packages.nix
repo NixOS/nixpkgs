207a37af3d5302582d99d7b668c2257773664a40
chillcicada <2210227279@qq.com>
2026-03-05 10:41:01 +0800

    molequeue: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11404,8 +11404,6 @@ with pkgs;
 
   ### SCIENCE/CHEMISTY
 
-  molequeue = libsForQt5.callPackage ../development/libraries/science/chemistry/molequeue { };
-
   avogadro2 = callPackage ../applications/science/chemistry/avogadro2 { };
 
   libxc_7 = pkgs.libxc.override { version = "7.0.0"; };
