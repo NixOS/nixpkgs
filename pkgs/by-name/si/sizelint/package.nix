82211fa0d30233dc28a53bc5dface719401e4690
a-kenji <aks.kenji@protonmail.com>
2026-03-05 20:32:57 +0100

    sizelint: add shell completions

--- pkgs/by-name/si/sizelint/package.nix
+++ pkgs/by-name/si/sizelint/package.nix
@@ -1,7 +1,9 @@
 {
   lib,
+  stdenv,
   rustPlatform,
   fetchFromGitHub,
+  installShellFiles,
   git,
 }:
 
@@ -16,9 +18,18 @@ rustPlatform.buildRustPackage (finalAttrs: {
     hash = "sha256-1k1+7fVWhflEKyhOlb7kMn2xqeAM6Y5N9uHtOJvVn4A=";
   };
 
+  nativeBuildInputs = [ installShellFiles ];
+
   nativeCheckInputs = [ git ];
 
   cargoHash = "sha256-Z+pmlp/0LlKfc4QLosePw7TdLFYe6AnAVOJSw2DzlfI=";
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd sizelint \
+      --bash <($out/bin/sizelint completions bash) \
+      --fish <($out/bin/sizelint completions fish) \
+      --zsh <($out/bin/sizelint completions zsh)
+  '';
 
   meta = {
     description = "Lint your file tree based on file sizes";
