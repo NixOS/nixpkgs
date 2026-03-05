b942a55fdf7e89c85e2237ba1bfd76ef4427f87b
S Downward <stephen@stainless.com>
2026-03-05 17:04:17 -0500

    maintainers: add sd-st

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -24088,6 +24088,11 @@
     github = "scvalex";
     githubId = 2588;
   };
+  sd-st = {
+    name = "sd-st";
+    github = "sd-st";
+    githubId = 208631639;
+  };
   sdaqo = {
     name = "sdaqo";
     email = "sdaqo.dev@protonmail.com";
