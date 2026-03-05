3687784c2a688bd5a84c75b869ae4d5278881bb8
zzbaron <zzbarona@gmail.com>
2026-03-05 16:05:01 -0500

    godot: add godotModules input for custom C++ godot module builds, add buildGodotModule

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2116,6 +2116,7 @@ with pkgs;
   gnused = callPackage ../tools/text/gnused { };
 
   inherit (callPackage ../development/tools/godot { })
+    buildGodotModule
     godot3
     godot3-export-templates
     godot3-headless
