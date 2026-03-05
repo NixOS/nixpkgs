NixOS/nixpkgs

commit cdf2296ce4e8c576b4a715d2c267af3700d6308c

python3Packages.pystemd: 0.13.4 -> 0.15.3
https://github.com/systemd/pystemd/releases/tag/v0.15.3

This commit was automatically generated using update-python-libraries.

pkgs/development/python-modules/pystemd/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "pystemd";
-  version = "0.13.4";
+  version = "0.15.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "systemd";
     repo = "pystemd";
     tag = "v${version}";
-    hash = "sha256-Ph0buiyH2cLRXyqgA8DmpE9crb/x8OaerIoZuv8hjMI=";
+    hash = "sha256-qFBa2hIcF0hyb+QyVpFG0qOpWsVVVTGCqgfChic6JCI=";
   };
 
   buildInputs = [ systemd ];

commit 8c56edca9dad1ba18c4dff0971ad9780b9632c9b

python3Packages.pystemd: fix build
Regressed in cdf2296ce4e8c576b4a715d2c267af3700d6308c, which landed in
https://github.com/NixOS/nixpkgs/pull/482364.
Fixes #496811.

pkgs/development/python-modules/pystemd/default.nix
@@ -50,6 +50,7 @@ buildPythonPackage rec {
 
   disabledTestPaths = [
     "test_version.py" # Requires cstq which is not in nixpkgs
+    "test_pickle.py" # fails with "Could not open a bus to DBus"
   ];
 
   pythonImportsCheck = [ "pystemd" ];