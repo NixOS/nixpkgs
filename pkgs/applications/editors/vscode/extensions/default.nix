802d5aebd1e9e4326d2ac6ae5a7864197e907a24
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-05 06:32:06 +0000

    vscode-extensions.antfu.slidev: 51.4.0 -> 52.14.1

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -291,8 +291,8 @@ let
         mktplcRef = {
           publisher = "antfu";
           name = "slidev";
-          version = "51.4.0";
-          hash = "sha256-Z9YNMhRtW8A0hi3e77negSIw1avsbb6+L701pgv5RTY=";
+          version = "52.14.1";
+          hash = "sha256-g5M+r3oFVla7VImo8PwokmJum8ykhV76VEvfxd7DRws=";
         };
         meta = {
           license = lib.licenses.mit;
