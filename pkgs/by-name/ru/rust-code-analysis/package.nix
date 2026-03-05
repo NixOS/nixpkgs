977c45c56a5e1151333948db0d5c29636cfee479
Petr Portnov <mrjarviscraft+nix@gmail.com>
2026-03-05 18:29:13 +0300

    rust-code-analysis: add nix-update-script

--- pkgs/by-name/ru/rust-code-analysis/package.nix
+++ pkgs/by-name/ru/rust-code-analysis/package.nix
@@ -2,6 +2,8 @@
   lib,
   rustPlatform,
   fetchCrate,
+  versionCheckHook,
+  nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
@@ -16,9 +18,15 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   cargoHash = "sha256-HirLjKkfZfc9UmUcUF5WW7xAJuCu7ftJDH8+zTSYlxs=";
 
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru.updateScript = nix-update-script { };
+
   meta = {
     description = "Analyze and collect metrics on source code";
     homepage = "https://github.com/mozilla/rust-code-analysis";
+    changelog = "https://github.com/mozilla/rust-code-analysis/releases/tag/v${finalAttrs.version}";
     license = with lib.licenses; [
       mit # grammars
       mpl20 # code
