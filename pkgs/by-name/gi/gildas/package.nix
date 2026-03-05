4b9d15fb5b8014d93515af19e4fa2115b845b4e3
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-05 23:29:55 +0000

    gildas: 20251101_a -> 20260301_a

--- pkgs/by-name/gi/gildas/package.nix
+++ pkgs/by-name/gi/gildas/package.nix
@@ -24,8 +24,8 @@ let
 in
 
 stdenv.mkDerivation rec {
-  srcVersion = "nov25a";
-  version = "20251101_a";
+  srcVersion = "mar26a";
+  version = "20260301_a";
   pname = "gildas";
 
   src = fetchurl {
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
       "http://www.iram.fr/~gildas/dist/gildas-src-${srcVersion}.tar.xz"
       "http://www.iram.fr/~gildas/dist/archive/gildas/gildas-src-${srcVersion}.tar.xz"
     ];
-    hash = "sha256-1wUKOW0DtGuxggZXzZf2aXRf9F4EFkgU5D4SjK8EwXM=";
+    hash = "sha256-OD4Ts2vaP+6n3fmYeQ1ZBI1r72o4w3Anf4jT51dbKk0=";
   };
 
   nativeBuildInputs = [
