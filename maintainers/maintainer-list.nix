b2f802c09b460a612d1e1fc256de46bf24d0917d
Sabrina Myers <sunseed218@gmail.com>
2026-03-05 18:08:40 -0700

    maintainers: add sabrinaishere

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23650,6 +23650,13 @@
     github = "saadndm";
     githubId = 88615188;
   };
+  sabrinaishere = {
+    name = "Sabrina Myers";
+    email = "SabrinaTheBitch@protonmail.com";
+    matrix = "@sabrina_the_bitch:matrix.org";
+    github = "SabrinaIsHere";
+    githubId = 74331139;
+  };
   sagikazarmark = {
     name = "Mark Sagi-Kazar";
     email = "mark.sagikazar@gmail.com";
