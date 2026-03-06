51a40077dad07ef6e80fca1afca31586530a769a
kyehn <228304369+kyehn@users.noreply.github.com>
2026-03-06 10:06:16 +0800

    tts-mod-vault: 1.3.0 -> 2.0.0

--- pkgs/by-name/tt/tts-mod-vault/package.nix
+++ pkgs/by-name/tt/tts-mod-vault/package.nix
@@ -1,20 +1,24 @@
 {
   lib,
-  flutter329,
+  flutter341,
   makeDesktopItem,
   copyDesktopItems,
   fetchFromGitHub,
+  runCommand,
+  yq-go,
+  nix-update-script,
+  _experimental-update-script-combinators,
 }:
 
-flutter329.buildFlutterApplication rec {
+flutter341.buildFlutterApplication (finalAttrs: {
   pname = "tts-mod-vault";
-  version = "1.3.0";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "markomijic";
     repo = "TTS-Mod-Vault";
-    tag = "v${version}";
-    hash = "sha256-BTs+4QeyVJeg415uiNXww8twQwUInHfB8voWJjeVs20=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-vreamzu+jzlgzjbEro5kE5bM1k6cL6XCG6Tsv+LEiyI=";
   };
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
@@ -32,9 +36,35 @@ flutter329.buildFlutterApplication rec {
 
   nativeBuildInputs = [ copyDesktopItems ];
 
+  preBuild = ''
+    echo 'SUPABASE_URL=https://pdrmmvvtindfbpxlcdps.supabase.co' > .env
+    echo 'SUPABASE_PUBLISHABLE_KEY=sb_publishable_CF6qzs1W8zd_hcVWHSCdVw__2SRB60p' >> .env
+  '';
+
   postInstall = ''
     install -m 444 -D assets/icon/tts_mod_vault_icon.png $out/share/icons/hicolor/1024x1024/apps/tts_mod_vault.png
   '';
+
+  passthru = {
+    pubspecSource =
+      runCommand "pubspec.lock.json"
+        {
+          inherit (finalAttrs) src;
+          nativeBuildInputs = [ yq-go ];
+        }
+        ''
+          yq eval --output-format=json --prettyPrint $src/pubspec.lock > "$out"
+        '';
+    updateScript = _experimental-update-script-combinators.sequence [
+      (nix-update-script { })
+      (
+        (_experimental-update-script-combinators.copyAttrOutputToFile "tts-mod-vault.pubspecSource" ./pubspec.lock.json)
+        // {
+          supportedFeatures = [ ];
+        }
+      )
+    ];
+  };
 
   meta = {
     description = "Download and backup assets for your Tabletop Simulator mods";
@@ -44,4 +74,4 @@ flutter329.buildFlutterApplication rec {
     mainProgram = "tts_mod_vault";
     platforms = lib.platforms.linux;
   };
-}
+})
