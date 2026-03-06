072b3af762b1dd2292d38fc245e06c1c01780c90
André Lima <me@limwa.pt>
2026-03-06 22:30:06 +0000

    linuxPackages_6_19.virtualbox: fix build

--- pkgs/os-specific/linux/virtualbox/default.nix
+++ pkgs/os-specific/linux/virtualbox/default.nix
@@ -2,6 +2,7 @@
   stdenv,
   virtualbox,
   kernel,
+  fetchpatch,
 }:
 
 stdenv.mkDerivation {
@@ -12,6 +13,17 @@ stdenv.mkDerivation {
     "fortify"
     "pic"
     "stackprotector"
+  ];
+
+  patches = [
+    (fetchpatch {
+      url = "https://raw.githubusercontent.com/rpmfusion/VirtualBox-kmod/ec4795b376212b9361c2554c249886c62008c3f8/kernel-6.19.patch";
+      sha256 = "sha256-HhJ/2dnJQIXGv86Avlb42+gRzAgNx3eq81gfDwmqjeU=";
+      excludes = [
+        "vboxsf/**/*"
+        "vboxguest/**/*"
+      ];
+    })
   ];
 
   nativeBuildInputs = kernel.moduleBuildDependencies;
