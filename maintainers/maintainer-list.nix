c2fd4ee0663c26ef53974e1a07db823cf843496c
cashmere <cashmere@autistici.org>
2026-03-06 04:49:49 +0100

    sshpiper: init at 1.5.3

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -4432,6 +4432,11 @@
     githubId = 1945;
     name = "Casey Rodarmor";
   };
+  cashmeredev = {
+    github = "cashmeredev";
+    githubId = 126001665;
+    name = "cashmeredev";
+  };
   catap = {
     email = "kirill@korins.ky";
     github = "catap";
