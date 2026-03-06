8148f592d2eb853a9e45d447bf3e05a3fe15d08a
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-06 22:02:15 +0000

    traefik: 3.6.9 -> 3.6.10
    (cherry picked from commit ec23e8451b5cde3a2a73a1db5f93d81df2cdfd49)

--- pkgs/by-name/tr/traefik/package.nix
+++ pkgs/by-name/tr/traefik/package.nix
@@ -8,16 +8,16 @@
 
 buildGo125Module (finalAttrs: {
   pname = "traefik";
-  version = "3.6.9";
+  version = "3.6.10";
 
   # Archive with static assets for webui
   src = fetchzip {
     url = "https://github.com/traefik/traefik/releases/download/v${finalAttrs.version}/traefik-v${finalAttrs.version}.src.tar.gz";
-    hash = "sha256-FM4SdiEB1hY064qDZD0BaKezmvzsGd85uIE49u4fx70=";
+    hash = "sha256-WYHHpS721dlkWdOEj+jwJkQ/vsiP2PnmFI50M9I8sbs=";
     stripRoot = false;
   };
 
-  vendorHash = "sha256-rousXyjb2WcXNG6wG7Ddd8qanarrEKcdQO/8C4PS4Bs=";
+  vendorHash = "sha256-q2uy0YrE1D8V0EopKWJLbq2hFcjn3oyanwNJ27yyC+k=";
 
   proxyVendor = true;
 
