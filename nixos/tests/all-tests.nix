c9e6a02a07a592bd4d55daa215436b5d01e15cd0
Hythera <87016780+Hythera@users.noreply.github.com>
2026-03-06 16:40:06 +0100

    nixos/tests/zen-browser: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1780,6 +1780,10 @@ in
   yggdrasil = runTest ./yggdrasil.nix;
   your_spotify = runTest ./your_spotify.nix;
   zammad = runTest ./zammad.nix;
+  zen-browser = runTest {
+    imports = [ ./firefox.nix ];
+    _module.args.firefoxPackage = pkgs.zen-browser;
+  };
   zenohd = runTest ./zenohd.nix;
   zeronet-conservancy = runTest ./zeronet-conservancy.nix;
   zfs = handleTest ./zfs.nix { };
