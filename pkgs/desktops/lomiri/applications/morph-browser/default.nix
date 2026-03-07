3435e5cb13ae17978836e9cbc2aa6330a5279b73
OPNA2608 <opna2608@protonmail.com>
2026-03-07 18:15:53 +0100

    nixosTests.morph-browser: Turn into attrset of Qt5 & Qt6 variant tests

--- pkgs/desktops/lomiri/applications/morph-browser/default.nix
+++ pkgs/desktops/lomiri/applications/morph-browser/default.nix
@@ -159,10 +159,11 @@ stdenv.mkDerivation (finalAttrs: {
 
   passthru = {
     updateScript = gitUpdater { };
-    tests = lib.optionalAttrs (!withQt6) {
+    tests = {
       # Test of morph-browser itself
-      standalone = nixosTests.morph-browser;
-
+      standalone = if withQt6 then nixosTests.morph-browser.qt6 else nixosTests.morph-browser.qt5;
+    }
+    // lib.optionalAttrs (!withQt6) {
       # Interactions between the Lomiri ecosystem and this browser
       inherit (nixosTests.lomiri) desktop-basics desktop-appinteractions;
     };
