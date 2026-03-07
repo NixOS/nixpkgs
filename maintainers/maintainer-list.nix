b6f38b25ef4d35af44019593b015c8bf67a762f6
Jake Clark <jnc@jnclark.org>
2026-03-07 15:29:31 -0700

    maintainers: add jnclark

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12655,6 +12655,12 @@
     githubId = 45771313;
     name = "Sena";
   };
+  jnclark = {
+    email = "nixpkgs@jnclark.org";
+    github = "jnclark";
+    githubId = 1663189;
+    name = "J. Clark";
+  };
   jnsgruk = {
     email = "jon@sgrs.uk";
     github = "jnsgruk";
