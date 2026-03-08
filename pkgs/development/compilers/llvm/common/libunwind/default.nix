a5e869659855d24182dcfa062179d5eae274cb92
Brian McKenna <brian@brianmckenna.org>
2026-03-08 11:28:29 +1100

    llvm/libunwind: support building for WASM targets

--- pkgs/development/compilers/llvm/common/libunwind/default.nix
+++ pkgs/development/compilers/llvm/common/libunwind/default.nix
@@ -11,6 +11,7 @@
   ninja,
   python3,
   libcxx,
+  fetchpatch,
   enableShared ? !stdenv.hostPlatform.isStatic,
   doFakeLibgcc ? stdenv.hostPlatform.useLLVM && !stdenv.hostPlatform.isStatic,
   devExtraCmakeFlags ? [ ],
@@ -55,7 +56,32 @@ stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "LIBUNWIND_ENABLE_SHARED" enableShared)
     (lib.cmakeFeature "LLVM_ENABLE_RUNTIMES" "libunwind")
   ]
+  ++ lib.optionals stdenv.hostPlatform.isWasm [
+    # Required otherwise CMake fails to determine platform
+    (lib.cmakeBool "UNIX" true)
+  ]
   ++ devExtraCmakeFlags;
+
+  env = lib.optionalAttrs stdenv.hostPlatform.isWasm {
+    # New EH model is needed for Unwind-wasm.c
+    NIX_CFLAGS_COMPILE = "-fwasm-exceptions -mllvm -wasm-use-legacy-eh=false";
+  };
+
+  # https://github.com/llvm/llvm-project/pull/168449
+  prePatch = lib.optionalString stdenv.hostPlatform.isWasm ''
+    chmod -R +w ../libunwind/src
+  '';
+  patches = lib.optionals stdenv.hostPlatform.isWasm [
+    (fetchpatch {
+      url = "https://gist.githubusercontent.com/yamt/b699eb2604d2598810a2876ff2ffc8d8/raw/1bc7d7d7c5cb4b37893415521528ae1c8f77b054/a.diff";
+      hash = "sha256-Z2wbPs59rFBa0ki6DeoAULQfU/V800QcIxj99jofjLs=";
+    })
+  ];
+  patchFlags = lib.optionals stdenv.hostPlatform.isWasm [
+    "-p1"
+    "-d"
+    ".."
+  ];
 
   postInstall =
     lib.optionalString (enableShared && !stdenv.hostPlatform.isDarwin && !stdenv.hostPlatform.isWindows)
