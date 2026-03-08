a2576aa157a2eb7f830b725208e7b4646c65d07c
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-08 23:24:51 +0000

    kimai: 2.49.0 -> 2.51.0
    [peathot@hotmail.com: correct vendorHash for NixOS 25.11]
    
    (cherry picked from commit 8804596721d5ec345bb0176350be8e5e63652486)

--- pkgs/by-name/ki/kimai/package.nix
+++ pkgs/by-name/ki/kimai/package.nix
@@ -7,13 +7,13 @@
 
 php.buildComposerProject2 (finalAttrs: {
   pname = "kimai";
-  version = "2.49.0";
+  version = "2.51.0";
 
   src = fetchFromGitHub {
     owner = "kimai";
     repo = "kimai";
     tag = finalAttrs.version;
-    hash = "sha256-hP67vu/8D7dTOPklyc8g3B//LAvs5YZ0g7fZ5lePM48=";
+    hash = "sha256-3wm8y0Ryl7i3QEBP9hC2pDt8JgkkS6gXQ+Lm0fRE9+E=";
   };
 
   php = php.buildEnv {
@@ -38,7 +38,7 @@ php.buildComposerProject2 (finalAttrs: {
     '';
   };
 
-  vendorHash = "sha256-Cl9BT/AQPDVmCseGDWrKfhCiubaqHeSTRp+Z/CZVuTE=";
+  vendorHash = "sha256-OYeLIGxkNrA7xtg4mF1mDLVeFkeRHfWt0O7IT6XoGyI=";
 
   composerNoPlugins = false;
 
