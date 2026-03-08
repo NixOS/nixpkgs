38df53fa71c93bd5e7a3433556e1f606de80ecb5
Robert Schütz <nix@dotlambda.de>
2026-03-07 16:29:50 -0800

    nfs-utils: 2.8.5 -> 2.8.6
    Changelog: https://www.kernel.org/pub/linux/utils/nfs-utils/2.8.6/2.8.6-Changelog
    (cherry picked from commit b419cdf5a60eb6a816fd125f5325d3be6571dbf5)

--- pkgs/by-name/nf/nfs-utils/package.nix
+++ pkgs/by-name/nf/nfs-utils/package.nix
@@ -41,11 +41,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "nfs-utils";
-  version = "2.8.5";
+  version = "2.8.6";
 
   src = fetchurl {
     url = "mirror://kernel/linux/utils/nfs-utils/${version}/nfs-utils-${version}.tar.xz";
-    hash = "sha256-sTykqd9EGGvb4QdRSyJzLUJD94X5Xa8vuGr8HtuhW7s=";
+    hash = "sha256-K9ezToCafv8vS8X8X9luvNZqVFi0caJwy9LcFpsBFVA=";
   };
 
   # libnfsidmap is built together with nfs-utils from the same source,
