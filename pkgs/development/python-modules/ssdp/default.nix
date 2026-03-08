340afa9881340e456c4dc3184337e8959aea83ef
Fabian Affolter <mail@fabian-affolter.ch>
2026-03-08 22:43:52 +0100

    python3Packages.ssdp: migrate to finalAttrs

--- pkgs/development/python-modules/ssdp/default.nix
+++ pkgs/development/python-modules/ssdp/default.nix
@@ -10,7 +10,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "ssdp";
   version = "1.3.2";
   pyproject = true;
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "codingjoe";
     repo = "ssdp";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-1LO5+lfykaepp+MfS/2mlngobhcV1nZvU19Jb0sbVzk=";
   };
 
@@ -45,9 +45,9 @@ buildPythonPackage rec {
   meta = {
     description = "Python asyncio library for Simple Service Discovery Protocol (SSDP)";
     homepage = "https://github.com/codingjoe/ssdp";
-    changelog = "https://github.com/codingjoe/ssdp/releases/tag/${src.tag}";
+    changelog = "https://github.com/codingjoe/ssdp/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "ssdp";
   };
-}
+})
