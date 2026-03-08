NixOS/nixpkgs

commit ccc6c1ca888c6a6dc99e6fda30aad827ffd85cbc

dino: disable optional features per default as they are experimental

pkgs/applications/networking/instant-messengers/dino/default.nix
@@ -27,13 +27,13 @@
   srtp,
   libnice,
   gnutls,
-  gstreamer,
-  gst-plugins-base,
-  gst-plugins-good,
-  gst-plugins-bad,
-  gst-vaapi,
+  gst_all_1,
   webrtc-audio-processing,
 }:
+
+# Upstream is very deliberate about which features are enabled per default or are automatically enabled.
+# Everything that is disabled per default has to been seen experimental and should not be enabled without strong reasoning.
+# see https://github.com/NixOS/nixpkgs/issues/469614#issuecomment-3649662176
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "dino";
@@ -79,23 +79,12 @@ stdenv.mkDerivation (finalAttrs: {
     srtp
     libnice
     gnutls
-    gstreamer
-    gst-plugins-base
-    gst-plugins-good # contains rtpbin, required for VP9
-    gst-plugins-bad # required for H264, MSDK
-    gst-vaapi # required for VAAPI
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
     webrtc-audio-processing
   ];
 
   doCheck = true;
-
-  mesonFlags = [
-    "-Dplugin-notification-sound=enabled"
-    "-Dplugin-rtp-h264=enabled"
-    "-Dplugin-rtp-msdk=enabled"
-    "-Dplugin-rtp-vaapi=enabled"
-    "-Dplugin-rtp-vp9=enabled"
-  ];
 
   # Undefined symbols for architecture arm64: "_gpg_strerror"
   NIX_LDFLAGS = lib.optionalString stdenv.hostPlatform.isDarwin "-lgpg-error";
@@ -108,7 +97,7 @@ stdenv.mkDerivation (finalAttrs: {
   # will load
   #
   # See https://github.com/dino/dino/wiki/macOS
-  postFixup = lib.optionalString (stdenv.hostPlatform.isDarwin) ''
+  postFixup = lib.optionalString stdenv.hostPlatform.isDarwin ''
     cd "$out/lib/dino/plugins/"
     for f in *.dylib; do
       mv "$f" "$(basename "$f" .dylib).so"

pkgs/top-level/all-packages.nix
@@ -1875,15 +1875,7 @@ with pkgs;
     asciidoc = asciidoc-full;
   };
 
-  dino = callPackage ../applications/networking/instant-messengers/dino {
-    inherit (gst_all_1)
-      gstreamer
-      gst-plugins-base
-      gst-plugins-bad
-      gst-vaapi
-      ;
-    gst-plugins-good = gst_all_1.gst-plugins-good.override { gtkSupport = true; };
-  };
+  dino = callPackage ../applications/networking/instant-messengers/dino { };
 
   dnschef = python3Packages.callPackage ../tools/networking/dnschef { };
 

commit 0f7d42cf720c9dc800bfbb4ef1f4b39b8f4da3f7

dino: re-add gst-plugins-good input
This was previously removed when experimental features in Dino were
disabled, along with other dependencies required for those.  Unlike
the others, though, VP9 is a stable feature, which we inadvertently
lost when making that change.  This is therefore a partial revert of
that change.

Fixes: ccc6c1ca888c ("dino: disable optional features per default as they are experimental")
Closes: https://github.com/NixOS/nixpkgs/issues/497706

pkgs/applications/networking/instant-messengers/dino/default.nix
@@ -27,7 +27,9 @@
   srtp,
   libnice,
   gnutls,
-  gst_all_1,
+  gstreamer,
+  gst-plugins-base,
+  gst-plugins-good,
   webrtc-audio-processing,
 }:
 
@@ -79,8 +81,9 @@ stdenv.mkDerivation (finalAttrs: {
     srtp
     libnice
     gnutls
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
+    gstreamer
+    gst-plugins-base
+    gst-plugins-good # contains rtpbin, required for VP9
     webrtc-audio-processing
   ];
 

pkgs/top-level/all-packages.nix
@@ -1672,7 +1672,13 @@ with pkgs;
     plugins = lib.filter (p: p ? pluginName) (builtins.attrValues discourse.plugins);
   };
 
-  dino = callPackage ../applications/networking/instant-messengers/dino { };
+  dino = callPackage ../applications/networking/instant-messengers/dino {
+    inherit (gst_all_1)
+      gstreamer
+      gst-plugins-base
+      ;
+    gst-plugins-good = gst_all_1.gst-plugins-good.override { gtkSupport = true; };
+  };
 
   inherit (ocamlPackages) dot-merlin-reader;
 