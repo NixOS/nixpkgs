f78b7b4425cb115da17b887d4dc5c42e8e8d54a8
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-08 22:19:15 +0000

    unciv: 4.19.15 -> 4.19.17-patch1

--- pkgs/by-name/un/unciv/package.nix
+++ pkgs/by-name/un/unciv/package.nix
@@ -12,7 +12,7 @@
   nix-update-script,
 }:
 let
-  version = "4.19.15";
+  version = "4.19.17-patch1";
 
   desktopItem = makeDesktopItem {
     name = "unciv";
@@ -42,7 +42,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://github.com/yairm210/Unciv/releases/download/${version}/Unciv.jar";
-    hash = "sha256-RlKoiSxW0MPPFTDq72k6CvYTHIEZ0hR8/PhW0j8ERs8=";
+    hash = "sha256-TmaZ4ep/95lKMbAlMklED1ePNy+B6JXNjxQrWuFvbDo=";
   };
 
   dontUnpack = true;
