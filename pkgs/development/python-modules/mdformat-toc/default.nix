9dd862993434fae417aaae6e78c6c0d562f430f1
sudoforge <no-reply@sudoforge.com>
2026-03-07 18:37:14 -0800

    python3Packages.mdformat-toc: remove broken attribute
    This was set as broken due to a test failure in an mdformat plugin [0],
    which has now been fixed.
    
    [0]: https://github.com/KyleKing/mdformat-admon/issues/25

--- pkgs/development/python-modules/mdformat-toc/default.nix
+++ pkgs/development/python-modules/mdformat-toc/default.nix
@@ -35,6 +35,5 @@ buildPythonPackage rec {
       aldoborrero
       polarmutex
     ];
-    broken = true; # broken test due to changes in mdformat; compare https://github.com/KyleKing/mdformat-admon/issues/25
   };
 }
