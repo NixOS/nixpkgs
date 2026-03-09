8495bf3202f70480b4bdeab35cff8473455e69cc
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:58 -0500

    xcursorgen: hardcode finalAttrs.pname

--- pkgs/by-name/xc/xcursorgen/package.nix
+++ pkgs/by-name/xc/xcursorgen/package.nix
@@ -30,13 +30,13 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-xdriinfo" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname xdriinfo \
         --url https://xorg.freedesktop.org/releases/individual/app/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version xdriinfo "$version"
     '';
   };
 
