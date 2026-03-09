6c3810ce3704c9cd2c2deb0e0bc1ff42aadfbaf7
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:04 -0500

    font-misc-meltho: hardcode finalAttrs.pname

--- pkgs/by-name/fo/font-misc-meltho/package.nix
+++ pkgs/by-name/fo/font-misc-meltho/package.nix
@@ -19,13 +19,13 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [ mkfontscale ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-font-misc-meltho" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname font-misc-meltho \
         --url https://xorg.freedesktop.org/releases/individual/font/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version font-misc-meltho "$version"
     '';
   };
 
