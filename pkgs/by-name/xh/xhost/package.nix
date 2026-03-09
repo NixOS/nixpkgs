467f8eeb8f305131d826abffbbd66a4ae6d4543c
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:07:01 -0500

    xhost: hardcode finalAttrs.pname

--- pkgs/by-name/xh/xhost/package.nix
+++ pkgs/by-name/xh/xhost/package.nix
@@ -34,13 +34,13 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-xhost" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname xhost \
         --url https://xorg.freedesktop.org/releases/individual/app/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version xhost "$version"
     '';
   };
 
