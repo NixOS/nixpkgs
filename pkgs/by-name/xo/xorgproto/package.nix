4471028155db1dcfe29ffe4d8dc4ee19e9c05872
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:07:05 -0500

    xorgproto: hardcode finalAttrs.pname

--- pkgs/by-name/xo/xorgproto/package.nix
+++ pkgs/by-name/xo/xorgproto/package.nix
@@ -40,13 +40,13 @@ stdenv.mkDerivation (finalAttrs: {
   mesonFlags = [ "-Dlegacy=true" ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-xorgproto" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname xorgproto \
         --url https://xorg.freedesktop.org/releases/individual/proto/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version xorgproto "$version"
     '';
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
