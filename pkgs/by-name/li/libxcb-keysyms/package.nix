b7108e9ab5d608ea42be3ee16911da141dc4aa22
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:21 -0500

    libxcb-keysyms: hardcode finalAttrs.pname

--- pkgs/by-name/li/libxcb-keysyms/package.nix
+++ pkgs/by-name/li/libxcb-keysyms/package.nix
@@ -34,13 +34,13 @@ stdenv.mkDerivation (finalAttrs: {
   propagatedBuildInputs = [ libxcb ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-libxcb-keysyms" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
       version="$(list-directory-versions --pname xcb-util-keysyms \
         --url https://xorg.freedesktop.org/releases/individual/lib/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version libxcb-keysyms "$version"
     '';
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
