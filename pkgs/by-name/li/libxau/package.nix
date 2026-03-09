1d4daff4b5225f8223cdfb2d8e72672b49317b96
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:20 -0500

    libxau: hardcode finalAttrs.pname

--- pkgs/by-name/li/libxau/package.nix
+++ pkgs/by-name/li/libxau/package.nix
@@ -26,13 +26,13 @@ stdenv.mkDerivation (finalAttrs: {
   propagatedBuildInputs = [ xorgproto ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-libxau" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
       version="$(list-directory-versions --pname libXau \
         --url https://xorg.freedesktop.org/releases/individual/lib/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version libxau "$version"
     '';
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
