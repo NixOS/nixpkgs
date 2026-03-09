70d7009cbc2a3ff6ffdd4aafbea499b3c9a99933
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:31 -0500

    libxvmc: hardcode finalAttrs.pname

--- pkgs/by-name/li/libxvmc/package.nix
+++ pkgs/by-name/li/libxvmc/package.nix
@@ -43,13 +43,13 @@ stdenv.mkDerivation (finalAttrs: {
   ) "--enable-malloc0returnsnull";
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-libxvmc" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
       version="$(list-directory-versions --pname libXvMC \
         --url https://xorg.freedesktop.org/releases/individual/lib/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version libxvmc "$version"
     '';
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
