5e78539d966603d4b15c1eed7a2646e03180e44f
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-09 15:51:46 +0000

    roon-server: 2.60.1629 -> 2.62.1641

--- pkgs/by-name/ro/roon-server/package.nix
+++ pkgs/by-name/ro/roon-server/package.nix
@@ -16,7 +16,7 @@
   stdenv,
 }:
 let
-  version = "2.60.1629";
+  version = "2.62.1641";
   urlVersion = builtins.replaceStrings [ "." ] [ "0" ] version;
 in
 stdenv.mkDerivation {
@@ -25,7 +25,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_${urlVersion}.tar.bz2";
-    hash = "sha256-s+rM9jzkL+m1GW2+seI/OnBHMbfeijNJ4CoXOaWJmi8=";
+    hash = "sha256-DX05i4bgTar2d2xHyEqFXNlQ86FladX/xoEC4YLIxnI=";
   };
 
   dontConfigure = true;
