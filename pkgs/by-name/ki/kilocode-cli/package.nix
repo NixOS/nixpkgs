0b65a68cb9864c84d376dd3311f629890cec5c55
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:15 -0500

    kilocode-cli: hardcode finalAttrs.pname

--- pkgs/by-name/ki/kilocode-cli/package.nix
+++ pkgs/by-name/ki/kilocode-cli/package.nix
@@ -34,7 +34,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   };
 
   npmDeps = fetchNpmDeps {
-    name = "${finalAttrs.pname}-npm-deps";
+    name = "kilocode-cli-npm-deps";
     inherit (finalAttrs) src;
     sourceRoot = "${finalAttrs.src.name}/cli";
     postPatch = ''
