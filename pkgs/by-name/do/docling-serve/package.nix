4144295dc704975c6dbb9e48e2154a516ef1edb7
Ryan Hendrickson <ryan.hendrickson@alum.mit.edu>
2026-03-09 18:02:13 -0400

    docling-serve: use overrideAttrs

--- pkgs/by-name/do/docling-serve/package.nix
+++ pkgs/by-name/do/docling-serve/package.nix
@@ -16,9 +16,11 @@
       withCPU
       ;
   }
-))
-// {
-  passthru.tests = {
-    docling-serve = nixosTests.docling-serve;
-  };
-}
+)).overrideAttrs
+  (old: {
+    passthru = old.passthru or { } // {
+      tests = old.passthru.tests or { } // {
+        inherit (nixosTests) docling-serve;
+      };
+    };
+  })
