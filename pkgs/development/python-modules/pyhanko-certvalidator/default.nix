0193441a488f1d7f77e4ab7ffaf81bf0351abc3c
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-09 02:37:47 +0000

    python3Packages.pyhanko-certvalidator: 0.29.1 -> 0.30.1

--- pkgs/development/python-modules/pyhanko-certvalidator/default.nix
+++ pkgs/development/python-modules/pyhanko-certvalidator/default.nix
@@ -19,14 +19,14 @@
 
 buildPythonPackage rec {
   pname = "pyhanko-certvalidator";
-  version = "0.29.1";
+  version = "0.30.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "MatthiasValvekens";
     repo = "pyhanko";
     tag = "pyhanko-certvalidator/v${version}";
-    hash = "sha256-+576MAbtWFGaPu/HqhdeeRNHi84pLnDaMDa0e/J/CUs=";
+    hash = "sha256-ZBlkp6nhTKEAWxCPDq9NIoOullwWartpU3eL0QIGFpw=";
   };
 
   sourceRoot = "${src.name}/pkgs/pyhanko-certvalidator";
