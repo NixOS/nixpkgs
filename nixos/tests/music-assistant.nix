28e13bfbd746a517e9857ae074706acd8040af09
Dustin Frisch <fooker@lab.sh>
2026-03-09 09:26:27 +0100

    sendspin-cli: init at 5.3.0 python3Packages.aiosendspin-mpris: init at 2.1.1 python3Packages.aiosendspin: 2.1.0 -> 4.3.2 python3Packages.mpris-api: init at 1.3.0 python3Packages.tunit: init at 1.7.2 nixos/tests/music-assistant: Add provider check for sendspin

--- nixos/tests/music-assistant.nix
+++ nixos/tests/music-assistant.nix
@@ -10,12 +10,55 @@
   nodes.machine = {
     services.music-assistant = {
       enable = true;
+      providers = [
+        "sendspin"
+      ];
     };
   };
 
-  testScript = ''
-    machine.wait_for_unit("music-assistant.service")
-    machine.wait_until_succeeds("curl --fail http://localhost:8095")
-    machine.log(machine.succeed("systemd-analyze security music-assistant.service | grep -v ✓"))
-  '';
+  testScript = # python
+    ''
+      import json
+
+      machine.wait_for_unit("music-assistant.service")
+      machine.wait_for_open_port(8095)
+      machine.wait_until_succeeds("curl --fail http://localhost:8095/info")
+
+      machine.log(machine.succeed("systemd-analyze security music-assistant.service | grep -v ✓"))
+
+      machine.succeed("""curl --fail http://localhost:8095/setup \
+        -H 'Accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -d '{
+          "username": "hotblack_desiato",
+          "password": "disaster_area",
+          "display_name": "NixOS ♡ Music"
+        }'""")
+
+      login = json.loads(machine.succeed("""curl --fail http://localhost:8095/auth/login \
+        -X POST \
+        -H 'Accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -d '{
+          "provider_id": "builtin",
+          "credentials": {
+            "username": "hotblack_desiato",
+            "password": "disaster_area"
+          }
+        }'"""))
+
+      providers = json.loads(machine.succeed(f"""curl --fail http://localhost:8095/api \
+        -H 'Accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -H 'Authorization: Bearer {login["token"]}' \
+        -d '{{
+          "command": "providers/manifests",
+          "args": {{}}
+        }}'"""))
+
+      assert any(
+        provider["type"] == "player" and provider["domain"] == "sendspin"
+        for provider in providers
+      )
+    '';
 }
