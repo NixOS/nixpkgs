817f60d24c44b59ffaf14f2f00ad707f0a8638ed
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:02 -0500

    font-isas-misc: hardcode finalAttrs.pname

--- pkgs/by-name/fo/font-isas-misc/package.nix
+++ pkgs/by-name/fo/font-isas-misc/package.nix
@@ -23,13 +23,13 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-font-isas-misc" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname font-isas-misc \
         --url https://xorg.freedesktop.org/releases/individual/font/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version font-isas-misc "$version"
     '';
   };
 
