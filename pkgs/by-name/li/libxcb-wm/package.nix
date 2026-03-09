b8ddc52b036e1a4298bb140d1288020eb9bfd124
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:22 -0500

    libxcb-wm: hardcode finalAttrs.pname

--- pkgs/by-name/li/libxcb-wm/package.nix
+++ pkgs/by-name/li/libxcb-wm/package.nix
@@ -38,13 +38,13 @@ stdenv.mkDerivation (finalAttrs: {
   propagatedBuildInputs = [ libxcb ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-libxcb-wm" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
       version="$(list-directory-versions --pname xcb-util-wm \
         --url https://xorg.freedesktop.org/releases/individual/lib/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version libxcb-wm "$version"
     '';
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
