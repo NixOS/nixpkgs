525900ccbeb742f507391dbfe1dc230c785723fa
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:31 -0500

    libxxf86misc: hardcode finalAttrs.pname

--- pkgs/by-name/li/libxxf86misc/package.nix
+++ pkgs/by-name/li/libxxf86misc/package.nix
@@ -35,13 +35,13 @@ stdenv.mkDerivation (finalAttrs: {
   ) "--enable-malloc0returnsnull";
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-libxxf86misc" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
       version="$(list-directory-versions --pname libXxf86misc \
         --url https://xorg.freedesktop.org/releases/individual/lib/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version libxxf86misc "$version"
     '';
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
