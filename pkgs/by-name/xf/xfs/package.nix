5580949902ec6d06b0aa9c935b0e2bd6532216f0
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:07:00 -0500

    xfs: hardcode finalAttrs.pname

--- pkgs/by-name/xf/xfs/package.nix
+++ pkgs/by-name/xf/xfs/package.nix
@@ -28,13 +28,13 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-xfs" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname xfs \
         --url https://xorg.freedesktop.org/releases/individual/app/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version xfs "$version"
     '';
   };
 
