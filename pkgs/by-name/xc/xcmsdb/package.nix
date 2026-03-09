d982685e60b7f16d94671908b7fcd06fe2bf1cb2
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:57 -0500

    xcmsdb: hardcode finalAttrs.pname

--- pkgs/by-name/xc/xcmsdb/package.nix
+++ pkgs/by-name/xc/xcmsdb/package.nix
@@ -22,13 +22,13 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [ libx11 ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-xcmsdb" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname xcmsdb \
         --url https://xorg.freedesktop.org/releases/individual/app/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version xcmsdb "$version"
     '';
   };
 
