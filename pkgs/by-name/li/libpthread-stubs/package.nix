ec969600c20e0eb003f68939d9347dbb9d9ca13d
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:18 -0500

    libpthread-stubs: hardcode finalAttrs.pname

--- pkgs/by-name/li/libpthread-stubs/package.nix
+++ pkgs/by-name/li/libpthread-stubs/package.nix
@@ -16,15 +16,15 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-libpthread-stubs" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
 
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname libpthread-stubs \
         --url https://xorg.freedesktop.org/releases/individual/lib/ \
         | sort -V | tail -n1)"
 
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version libpthread-stubs "$version"
     '';
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
