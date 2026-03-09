2846a708a4226618825ef034bd1d542444d1f8d7
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:06:05 -0500

    font-screen-cyrillic: hardcode finalAttrs.pname

--- pkgs/by-name/fo/font-screen-cyrillic/package.nix
+++ pkgs/by-name/fo/font-screen-cyrillic/package.nix
@@ -23,13 +23,13 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-font-screen-cyrillic" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname font-screen-cyrillic \
         --url https://xorg.freedesktop.org/releases/individual/font/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version font-screen-cyrillic "$version"
     '';
   };
 
