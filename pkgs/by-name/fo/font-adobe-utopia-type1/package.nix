850250fa91d8e6ead234cc162689270115c580d2
Guy Chronister <guychronister@outlook.com>
2026-03-09 09:05:57 -0500

    font-adobe-utopia-type1: hardcode finalAttrs.pname

--- pkgs/by-name/fo/font-adobe-utopia-type1/package.nix
+++ pkgs/by-name/fo/font-adobe-utopia-type1/package.nix
@@ -18,13 +18,13 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [ mkfontscale ];
 
   passthru = {
-    updateScript = writeScript "update-${finalAttrs.pname}" ''
+    updateScript = writeScript "update-font-adobe-utopia-type1" ''
       #!/usr/bin/env nix-shell
       #!nix-shell -i bash -p common-updater-scripts
-      version="$(list-directory-versions --pname ${finalAttrs.pname} \
+      version="$(list-directory-versions --pname font-adobe-utopia-type1 \
         --url https://xorg.freedesktop.org/releases/individual/font/ \
         | sort -V | tail -n1)"
-      update-source-version ${finalAttrs.pname} "$version"
+      update-source-version font-adobe-utopia-type1 "$version"
     '';
   };
 
