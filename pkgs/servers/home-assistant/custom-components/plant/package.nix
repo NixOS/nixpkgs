adea95135c3356d2c0779478ab2120012ecb45c4
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-03-10 15:38:18 +0100

    home-assistant-custom-components.plant: init at 2026.2.1

---
+++ pkgs/servers/home-assistant/custom-components/plant/package.nix
@@ -0,0 +1,38 @@
+{
+  lib,
+  async-timeout,
+  buildHomeAssistantComponent,
+  fetchFromGitHub,
+  pytest-homeassistant-custom-component,
+  pytestCheckHook,
+}:
+
+buildHomeAssistantComponent rec {
+  owner = "olen";
+  domain = "plant";
+  version = "2026.2.1";
+
+  src = fetchFromGitHub {
+    inherit owner;
+    repo = "homeassistant-plant";
+    tag = "v${version}";
+    hash = "sha256-nWZFywMTw3seEAERekzMeLRPceqMl1s7VXujNz0+6zI=";
+  };
+
+  dependencies = [
+    async-timeout
+  ];
+
+  nativeBuildInputs = [
+    pytest-homeassistant-custom-component
+    pytestCheckHook
+  ];
+
+  meta = {
+    description = "Alternative Plant component of home assistant";
+    homepage = "https://github.com/Olen/homeassistant-plant";
+    changelog = "https://github.com/Olen/homeassistant-plant/releases/tag/${src.tag}";
+    maintainers = with lib.maintainers; [ SuperSandro2000 ];
+    license = lib.licenses.mit;
+  };
+}
