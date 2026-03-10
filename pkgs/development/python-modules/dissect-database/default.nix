2335b7dfd9a721bcaff65058d6c0a1fabd12e1ac
Fabian Affolter <mail@fabian-affolter.ch>
2026-03-11 00:57:40 +0100

    python3Packages.dissect-database: migrate to finalAttrs

--- pkgs/development/python-modules/dissect-database/default.nix
+++ pkgs/development/python-modules/dissect-database/default.nix
@@ -9,7 +9,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "dissect-database";
   version = "1.1";
   pyproject = true;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "fox-it";
     repo = "dissect.database";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-z3Ra8BjPGozcx5bF+FKcA/bnsO8F++UBUEQ2tBd+X5Q=";
   };
 
@@ -41,8 +41,8 @@ buildPythonPackage rec {
   meta = {
     description = "Dissect module implementing a parser for various database formats";
     homepage = "https://github.com/fox-it/dissect.database";
-    changelog = "https://github.com/fox-it/dissect.database/releases/tag/${src.tag}";
+    changelog = "https://github.com/fox-it/dissect.database/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
