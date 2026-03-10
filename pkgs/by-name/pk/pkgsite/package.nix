af538be5bc78047e41505421aa650c2d355aece8
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-10 05:29:29 +0000

    pkgsite: 0-unstable-2026-02-25 -> 0-unstable-2026-03-09

--- pkgs/by-name/pk/pkgsite/package.nix
+++ pkgs/by-name/pk/pkgsite/package.nix
@@ -7,13 +7,13 @@
 
 buildGoModule {
   pname = "pkgsite";
-  version = "0-unstable-2026-02-25";
+  version = "0-unstable-2026-03-09";
 
   src = fetchFromGitHub {
     owner = "golang";
     repo = "pkgsite";
-    rev = "d29b966ca794634ccd1758dcd59cd2c8baf23422";
-    hash = "sha256-oNCkjcBC2dCJloJUcPpFqhGDQQMSzalZKITKt8IyL4Q=";
+    rev = "59cb58a646844f6fba00bfadc39b4bea96357358";
+    hash = "sha256-CsKdUrgnjs11iVdfUwgiO9KTx1TCfqFljiFqiAYxoK4=";
   };
 
   vendorHash = "sha256-G/XTWobysyzONctabYDIfAQ/zaAA9w2Ky7Hn6cj9l/c=";
