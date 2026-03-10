66c212da539db15e528cd66482021abc295f4922
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-10 03:44:50 +0000

    confluent-cli: 4.50.0 -> 4.54.0

--- pkgs/by-name/co/confluent-cli/package.nix
+++ pkgs/by-name/co/confluent-cli/package.nix
@@ -7,7 +7,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "confluent-cli";
-  version = "4.50.0";
+  version = "4.54.0";
 
   # To get the latest version:
   # curl -L https://cnfl.io/cli | sh -s -- -l | grep -v latest | sort -V | tail -n1
@@ -26,10 +26,10 @@ stdenv.mkDerivation (finalAttrs: {
     fetchurl {
       url = "https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/${finalAttrs.version}/confluent_${finalAttrs.version}_${system}.tar.gz";
       hash = selectSystem {
-        x86_64-linux = "sha256-iBe/swUUHljqKMzJ3iVBrLc7MO0NBq9kmkdfe6+JcFw=";
-        aarch64-linux = "sha256-RSoztV0dK3Q1v7KqZYhNnu3ov1KhKpx8Cu2GtEl6vyk=";
-        x86_64-darwin = "sha256-otriw9/kzYA1rKuI+EaCcMY3sSINVMzebA5A9amtSz0=";
-        aarch64-darwin = "sha256-GRvM0J4yIRrK3vXLfRRplG26E1L7XtRNUsuA+L+dCOo=";
+        x86_64-linux = "sha256-r9d+EGYL26andVgLIqeQpWhZJPsMpAB9uVs5b1rRc1A=";
+        aarch64-linux = "sha256-9LqsuZL6Idrq/A+zjn6Q0eTsfxqMi23PtchnaWOY8RU=";
+        x86_64-darwin = "sha256-vewoG6GqlQkLlvfEXba1TeUEjKGJwoOy5GwyGdavUls=";
+        aarch64-darwin = "sha256-vZ0Yiu57UBl3Ko/AmyEuqdo0urAnaTrjn/z6lWF2eYw=";
       };
     };
 
