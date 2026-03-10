6e99e3a1bd2957a7bbf20e0599ca62f15574b28d
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-10 10:52:22 +0000

    cargo-semver-checks: 0.46.0 -> 0.47.0

--- pkgs/by-name/ca/cargo-semver-checks/package.nix
+++ pkgs/by-name/ca/cargo-semver-checks/package.nix
@@ -11,16 +11,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cargo-semver-checks";
-  version = "0.46.0";
+  version = "0.47.0";
 
   src = fetchFromGitHub {
     owner = "obi1kenobi";
     repo = "cargo-semver-checks";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-y2tkTPctit5rx6OyohPPVo117sGICg6UEDg7RWFmtMA=";
+    hash = "sha256-1D6WFsiMOl/bJr0J+mmvLlgnRSKN6rPhDSnDsdLTC9E=";
   };
 
-  cargoHash = "sha256-lP4yXCuJ89NqUBZR6zgGi5B570y+5IaabWyzd9qqa3o=";
+  cargoHash = "sha256-YbtYIHj899eJSrp5n5jODgTkL9L26EnruzECwBrBF00=";
 
   nativeBuildInputs = [
     cmake
