2d10e963bcd2347dbaa1c3d47365f519380abd80
Mica Semrick <mica@silentumbrella.com>
2026-03-09 19:36:17 -0700

    python3Packages.pdstags: init at 2026.1.29

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14084,6 +14084,8 @@ self: super: with self; {
 
   psd-tools = callPackage ../development/python-modules/psd-tools { };
 
+  psdtags = callPackage ../development/python-modules/psdtags { };
+
   psnawp = callPackage ../development/python-modules/psnawp { };
 
   psrpcore = callPackage ../development/python-modules/psrpcore { };
