68e6eee339222bc75c66440be9012d3e47cefcb6
cobbn <npcobb2003@gmail.com>
2026-03-09 20:03:56 -0500

    added platform and maintainer info

--- pkgs/by-name/uf/ufbt/package.nix
+++ pkgs/by-name/uf/ufbt/package.nix
@@ -15,13 +15,14 @@
   llvmPackages,
   heatshrink,
   protobuf,
-}: let
+}:
+let
   flipper-sdk = fetchzip {
     url = "https://update.flipperzero.one/builds/firmware/1.4.3/flipper-z-f7-sdk-1.4.3.zip";
     hash = "sha256-lU9/dPYg7J/2/KOgOxrf6THypktF0aYUefpaV47/XTk=";
     stripRoot = false;
   };
-  gcc-arm-13-2 = stdenv.mkDerivation rec {
+  gcc-arm-13-2 = stdenv.mkDerivation {
     pname = "gcc-arm-embedded-13.2";
     version = "13.2.Rel1";
     src = fetchurl {
@@ -32,11 +33,16 @@
       mkdir -p $out
       cp -r * $out/
     '';
-    nativeBuildInputs = [autoPatchelfHook];
-    buildInputs = [stdenv.cc.cc.lib ncurses5 python3 libxcrypt-legacy];
+    nativeBuildInputs = [ autoPatchelfHook ];
+    buildInputs = [
+      stdenv.cc.cc.lib
+      ncurses5
+      python3
+      libxcrypt-legacy
+    ];
   };
-  ufbt-python = python3.withPackages (ps:
-    with ps; [
+  ufbt-python = python3.withPackages (
+    ps: with ps; [
       pyserial
       oslex
       ansi
@@ -44,46 +50,61 @@
       pyelftools
       setuptools
       colorlog
-    ]);
+    ]
+  );
 in
-  python3Packages.buildPythonApplication rec {
-    pname = "ufbt";
-    version = "0.2.6";
-    format = "pyproject";
+python3Packages.buildPythonApplication rec {
+  pname = "ufbt";
+  version = "0.2.6";
+  format = "pyproject";
 
-    src = fetchPypi {
-      inherit pname version;
-      hash = "sha256-TxqFiFhZjtLiW7q2ni6mBLwAdYw7Ho7PiXophmFXNjs=";
-    };
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-TxqFiFhZjtLiW7q2ni6mBLwAdYw7Ho7PiXophmFXNjs=";
+  };
 
-    postPatch = ''
-        substituteInPlace pyproject.toml \
-          --replace "setuptools-git-versioning<2" "setuptools-git-versioning"
-        mkdir -p $out/bin
-        ln -s ${gcc-arm-13-2}/bin/arm-none-eabi-gdb $out/bin/arm-none-eabi-gdb-py3
-    '';
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace "setuptools-git-versioning<2" "setuptools-git-versioning"
+    mkdir -p $out/bin
+    ln -s ${gcc-arm-13-2}/bin/arm-none-eabi-gdb $out/bin/arm-none-eabi-gdb-py3
+  '';
 
-    nativeBuildInputs = [
-      python3Packages.setuptools-git-versioning ];
+  nativeBuildInputs = [
+    python3Packages.setuptools-git-versioning
+  ];
 
-    propagatedBuildInputs = [ ufbt-python ];
-    makeWrapperArgs = [
-      "--set FBT_NOENV 1"
-      "--set UFBT_SDK_PATH ${flipper-sdk}"
-      "--set UFBT_OPENOCD ${lib.getBin openocd}/bin/openocd"
-      "--set UFBT_DFUUTIL ${lib.getBin dfu-util}/bin/dfu-util"
-      "--set UFBT_CLANGD ${lib.getBin llvmPackages.clang-unwrapped}/bin/clangd"
-      "--set UFBT_PROTOC ${lib.getBin protobuf}/bin/protoc"
-      "--set UFBT_HEATSHRINK ${lib.getBin heatshrink}/bin/heatshrink"
-      "--set UFBT_TOOLCHAIN_BINDIR ${gcc-arm-13-2}/bin"
-      "--prefix PYTHONPATH : ${scons}/lib/python${python3.pythonVersion}/site-packages"
-      "--prefix PATH : ${lib.makeBinPath [ufbt-python gcc-arm-13-2 dfu-util openocd llvmPackages.clang-unwrapped heatshrink]}"
-      "--set UFBT_HOME .ufbt_state"
+  propagatedBuildInputs = [ ufbt-python ];
+  makeWrapperArgs = [
+    "--set FBT_NOENV 1"
+    "--set UFBT_SDK_PATH ${flipper-sdk}"
+    "--set UFBT_OPENOCD ${lib.getBin openocd}/bin/openocd"
+    "--set UFBT_DFUUTIL ${lib.getBin dfu-util}/bin/dfu-util"
+    "--set UFBT_CLANGD ${lib.getBin llvmPackages.clang-unwrapped}/bin/clangd"
+    "--set UFBT_PROTOC ${lib.getBin protobuf}/bin/protoc"
+    "--set UFBT_HEATSHRINK ${lib.getBin heatshrink}/bin/heatshrink"
+    "--set UFBT_TOOLCHAIN_BINDIR ${gcc-arm-13-2}/bin"
+    "--prefix PYTHONPATH : ${scons}/lib/python${python3.pythonVersion}/site-packages"
+    "--prefix PATH : ${
+      lib.makeBinPath [
+        ufbt-python
+        gcc-arm-13-2
+        dfu-util
+        openocd
+        llvmPackages.clang-unwrapped
+        heatshrink
+      ]
+    }"
+    "--set UFBT_HOME .ufbt_state"
+  ];
+  meta = with lib; {
+    description = "Micro Flipper Build Tool";
+    homepage = "https://github.com/flipperdevices/flipperzero-ufbt";
+    license = licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ pignated ];
+    platforms = [
+      "x86_64-linux"
     ];
-    meta = with lib; {
-      description = "Micro Flipper Build Tool";
-      homepage = "https://github.com/flipperdevices/flipperzero-ufbt";
-      license = licenses.gpl3Only;
-      mainProgram = "ufbt";
-    };
-  }
+    mainProgram = "ufbt";
+  };
+}
