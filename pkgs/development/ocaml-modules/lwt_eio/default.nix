0ee762409017b34e0e7bd2bee17360c691759db4
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-03-11 22:45:21 +0100

    ocamlPackages.lwt: 5.9.2 → 6.1.1
    ocamlPackages.conduit-lwt-unix: disable tests with lwt ≥ 6
    ocamlPackages.js_of_ocaml-lwt: log support is optional
    ocamlPackages.lwt_log: mark as broken with lwt ≥ 6
    ocamlPackages.tezt: mark as broken with lwt ≥ 6

--- pkgs/development/ocaml-modules/lwt_eio/default.nix
+++ pkgs/development/ocaml-modules/lwt_eio/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildDunePackage,
   fetchurl,
+  fetchpatch,
   eio,
   lwt,
 }:
@@ -15,6 +16,11 @@ buildDunePackage rec {
     url = "https://github.com/ocaml-multicore/${pname}/releases/download/v${version}/${pname}-${version}.tbz";
     hash = "sha256-dlJnhHh4VNO60NZJZqc1HS8wPR95WhdeBJTK37pPbCE=";
   };
+
+  patches = lib.optional (lib.versionAtLeast lwt.version "6.0.0") (fetchpatch {
+    url = "https://github.com/ocaml-multicore/lwt_eio/commit/5f8bf1e7af33590683ee45151894d7b9a20607f0.patch";
+    hash = "sha256-5A3Bh+xOXo79Rw145hYqYv42li30M0TKLW+qu/dC0KQ=";
+  });
 
   propagatedBuildInputs = [
     eio
