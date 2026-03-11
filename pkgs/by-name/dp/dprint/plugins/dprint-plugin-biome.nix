b46a5b2214f318b1b0fd2cc2258706b7afda5447
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-11 16:57:16 +0000

    dprint-plugins.dprint-plugin-biome: 0.12.1 -> 0.12.2

--- pkgs/by-name/dp/dprint/plugins/dprint-plugin-biome.nix
+++ pkgs/by-name/dp/dprint/plugins/dprint-plugin-biome.nix
@@ -1,7 +1,7 @@
 { mkDprintPlugin }:
 mkDprintPlugin {
   description = "Biome (JS/TS/JSON) wrapper plugin";
-  hash = "sha256-EZg0l50VGrEDGKEMX64d7EzvdO3+tQfCtTIJp8JUYX4=";
+  hash = "sha256-t58jDcUp60fbRJmE1B5hJnKdl2N5qU9dLC7Juo5IqtI=";
   initConfig = {
     configExcludes = [ "**/node_modules" ];
     configKey = "biome";
@@ -17,6 +17,6 @@ mkDprintPlugin {
   };
   pname = "dprint-plugin-biome";
   updateUrl = "https://plugins.dprint.dev/dprint/biome/latest.json";
-  url = "https://plugins.dprint.dev/biome-0.12.1.wasm";
-  version = "0.12.1";
+  url = "https://plugins.dprint.dev/biome-0.12.2.wasm";
+  version = "0.12.2";
 }
