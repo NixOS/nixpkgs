6cb211d99cd2918a96d7d10f550d758a014bf8de
Marin <git@mar.in>
2026-03-11 11:51:41 -0600

    maintainers: add supermarin

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25999,6 +25999,12 @@
     matrix = "@superherointj:matrix.org";
     name = "Sérgio Marcelo";
   };
+  supermarin = {
+    email = "git+nixpkgs@mar.in";
+    name = "marin";
+    github = "supermarin";
+    githubId = 627285;
+  };
   SuperSandro2000 = {
     email = "sandro.jaeckel@gmail.com";
     matrix = "@sandro:supersandro.de";
