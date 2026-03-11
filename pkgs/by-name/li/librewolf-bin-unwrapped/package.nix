7c9f825d5b10c1f16b4b32d3ba5524d7da9f8d95
Hythera <87016780+Hythera@users.noreply.github.com>
2026-03-11 09:03:55 +0100

    librewolf-bin-unwrapped: 148.0-1 -> 148.0.2-2
    (cherry picked from commit a3eb936c05cf8bcd18a6d689c8f379b5f8574b5a)

--- pkgs/by-name/li/librewolf-bin-unwrapped/package.nix
+++ pkgs/by-name/li/librewolf-bin-unwrapped/package.nix
@@ -36,7 +36,7 @@ let
 
   pname = "librewolf-bin-unwrapped";
 
-  version = "148.0-1";
+  version = "148.0.2-2";
 in
 
 stdenv.mkDerivation {
@@ -46,8 +46,8 @@ stdenv.mkDerivation {
     url = "https://codeberg.org/api/packages/librewolf/generic/librewolf/${version}/librewolf-${version}-${arch}-package.tar.xz";
     hash =
       {
-        x86_64-linux = "sha256-UV9laKmAvyafNXXcCGrEr51dlyO1DaV3iHJy981OR9M=";
-        aarch64-linux = "sha256-EZFNcshCdx3xje3/RWXJVYf3IFxeNXbZSoWjoQhoa7I=";
+        x86_64-linux = "sha256-4FigAIvksbwJ46XqH225woK+inQHUnvyxvQBP/yyagU=";
+        aarch64-linux = "sha256-wp31BZLUgH+pXkaDnlUtXMNuacWEgQbnkxMYt2vhKZY=";
       }
       .${stdenv.hostPlatform.system} or throwSystem;
   };
