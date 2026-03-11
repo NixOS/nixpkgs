b0c2a86dd4beaa994392c1eb96a104d3bfa868d2
Nick Cao <nickcao@nichi.co>
2026-03-11 18:35:18 -0400

    memtier-benchmark: update repo owner, drop outdated patchPhase

--- pkgs/by-name/me/memtier-benchmark/package.nix
+++ pkgs/by-name/me/memtier-benchmark/package.nix
@@ -15,21 +15,17 @@ stdenv.mkDerivation (finalAttrs: {
   version = "2.2.2";
 
   src = fetchFromGitHub {
-    owner = "redislabs";
+    owner = "redis";
     repo = "memtier_benchmark";
     tag = finalAttrs.version;
     hash = "sha256-/t7OY3N9VBa9o2amOFb2/MUr5Y4ep4HGUil8OtwKkng=";
   };
 
-  patchPhase = ''
-    substituteInPlace ./configure.ac \
-      --replace '1.2.8' '${finalAttrs.version}'
-  '';
-
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
   ];
+
   buildInputs = [
     libevent
     pcre
@@ -39,7 +35,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Redis and Memcached traffic generation and benchmarking tool";
-    homepage = "https://github.com/redislabs/memtier_benchmark";
+    homepage = "https://github.com/redis/memtier_benchmark";
     license = lib.licenses.gpl2Only;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ thoughtpolice ];
