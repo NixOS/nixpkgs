a4754ab4c3f06f61463f8867d37f58228c99a686
Sapphire <imsapphire0@gmail.com>
2026-03-11 00:00:49 -0500

    nixos/wivrn: remove defaultRuntime
    This option shouldn't exist for WiVRn as it manages the active runtime itself.
    It also causes apps that attempt to initialise OpenXR while WiVRn isn't running to hang.
    
    (cherry picked from commit 898febd83f521f5dfbee857869b5fbee89f408c1)

--- nixos/modules/services/video/wivrn.nix
+++ nixos/modules/services/video/wivrn.nix
@@ -80,14 +80,6 @@ in
 
       openFirewall = mkEnableOption "the default ports in the firewall for the WiVRn server";
 
-      defaultRuntime = mkEnableOption ''
-        WiVRn as the default OpenXR runtime on the system.
-        The config can be found at `/etc/xdg/openxr/1/active_runtime.json`.
-
-        Note that applications can bypass this option by setting an active
-        runtime in a writable XDG_CONFIG_DIRS location like `~/.config`
-      '';
-
       autoStart = mkEnableOption "starting the service by default";
 
       highPriority = mkEnableOption "high priority capability for asynchronous reprojection";
@@ -247,9 +239,6 @@ in
         PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES = "1";
       };
       pathsToLink = [ "/share/openxr" ];
-      etc."xdg/openxr/1/active_runtime.json" = mkIf cfg.defaultRuntime {
-        source = "${cfg.package}/share/openxr/1/openxr_wivrn.json";
-      };
     };
   };
   meta.maintainers = with maintainers; [ passivelemon ];
