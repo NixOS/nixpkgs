9a9aeab9b48d1cbd58a7011dd81384cab13087b5
FazalAAli <fazal.ali.contact@gmail.com>
2026-03-10 19:00:52 -0500

    maintainers: add FazalAAli

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -8469,6 +8469,12 @@
     name = "A Frederick Christensen";
     keys = [ { fingerprint = "5A49 F4F9 3EDC 21E9 B7CC  4E94 9EEF 4142 5355 8AC4"; } ];
   };
+  FazalAAli = {
+    email = "nix@fazali.dev";
+    github = "FazalAAli";
+    githubId = 37760883;
+    name = "Fazal Ali";
+  };
   fazzi = {
     email = "faaris.ansari@proton.me";
     github = "fxzzi";
