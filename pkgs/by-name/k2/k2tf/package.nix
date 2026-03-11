7914f932bc211341c56dfb039308522beffdd4af
chillcicada <2210227279@qq.com>
2026-03-11 11:50:13 +0800

    k2tf: 0.7.0 -> 0.8.0

--- pkgs/by-name/k2/k2tf/package.nix
+++ pkgs/by-name/k2/k2tf/package.nix
@@ -2,30 +2,23 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
-  fetchpatch,
 }:
 
 buildGoModule (finalAttrs: {
   pname = "k2tf";
-  version = "0.7.0";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "sl1pm4t";
     repo = "k2tf";
-    rev = "v${finalAttrs.version}";
-    sha256 = "sha256-zkkRzCTZCvbwBj4oIhTo5d3PvqLMJPzT3zV9jU3PEJs=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-LoYlX2kAfzI0GMUbBtvuOinDzvoHABKEaGhipe16FeA=";
   };
 
-  patches = [
-    # update dependencies
-    # https://github.com/sl1pm4t/k2tf/pull/111
-    (fetchpatch {
-      url = "https://github.com/sl1pm4t/k2tf/commit/7e7b778eeb80400cb0dadb1cdea4e617b5738147.patch";
-      hash = "sha256-ZGQUuH7u3aNLml6rvOzOxVwSTlbhZLknXbHKeY4lp00=";
-    })
-  ];
+  proxyVendor = true;
+  vendorHash = "sha256-h8ph8K/4luTUCkx5X1iakTubF651HblGDN4G1EtSKeE=";
 
-  vendorHash = "sha256-yGuoE1bgwVHk3ym382OC93me9HPlVoNgGo/3JROVC2E=";
+  subPackages = [ "." ];
 
   ldflags = [
     "-s"
