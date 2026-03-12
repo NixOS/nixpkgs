c92a446a730ce740234237702d38c6bdeb776c3b
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-12 18:06:22 +0000

    gh-dash: 4.23.0 -> 4.23.2

--- pkgs/by-name/gh/gh-dash/package.nix
+++ pkgs/by-name/gh/gh-dash/package.nix
@@ -8,13 +8,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "gh-dash";
-  version = "4.23.0";
+  version = "4.23.2";
 
   src = fetchFromGitHub {
     owner = "dlvhdr";
     repo = "gh-dash";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-MkZ3HfJ+lt05fNKCb6Xpi+x30ZtgapPcoXvGhq8nj6k=";
+    hash = "sha256-C06LPVoE23ITJpMG0x75Djgeup+eb5uYwA8wL7xxvWU=";
   };
 
   vendorHash = "sha256-4AbeoH0l7eIS7d0yyJxM7+woC7Q/FCh0BOJj3d1zyX4=";
