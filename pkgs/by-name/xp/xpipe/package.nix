8cbe09038dfd5e47e37cb8f319f4fe9e14f0e0fc
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-12 09:05:20 +0000

    xpipe: 21.5 -> 21.6

--- pkgs/by-name/xp/xpipe/package.nix
+++ pkgs/by-name/xp/xpipe/package.nix
@@ -39,7 +39,7 @@ let
 
   hash =
     {
-      x86_64-linux = "sha256-y1/8GeSdq3UI11Pcnj4CXUHVTOt9NYIE0XtByDB8tuo=";
+      x86_64-linux = "sha256-ehuEEoU+RZpYXjvdcF3Mx9oF+Q4ltPxZn7fG24jRVHQ=";
     }
     .${system} or throwSystem;
 
@@ -48,7 +48,7 @@ let
 in
 stdenvNoCC.mkDerivation rec {
   pname = "xpipe";
-  version = "21.5";
+  version = "21.6";
 
   src = fetchzip {
     url = "https://github.com/xpipe-io/xpipe/releases/download/${version}/xpipe-portable-linux-${arch}.tar.gz";
