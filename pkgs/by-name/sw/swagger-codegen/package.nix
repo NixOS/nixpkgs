cc74eb253896731a878f9e2338a8feffd0b6c0f6
Anthony ROUSSEL <anthony@roussel.dev>
2026-03-12 21:50:33 +0100

    swagger-codegen: 2.4.50 -> 2.4.51
    Diff: https://github.com/swagger-api/swagger-codegen/compare/v2.4.50...v2.4.51

--- pkgs/by-name/sw/swagger-codegen/package.nix
+++ pkgs/by-name/sw/swagger-codegen/package.nix
@@ -4,17 +4,18 @@
   fetchFromGitHub,
   jre,
   makeWrapper,
+  versionCheckHook,
 }:
 
 maven.buildMavenPackage rec {
   pname = "swagger-codegen";
-  version = "2.4.50";
+  version = "2.4.51";
 
   src = fetchFromGitHub {
     owner = "swagger-api";
     repo = "swagger-codegen";
     tag = "v${version}";
-    hash = "sha256-mlnUg+23Uv/SDgi9zT5MK8592YaLQORpFRTxcQFfcoQ=";
+    hash = "sha256-bclFD2Kn5Xn6dxEaS1M69LDUc5lvMLFNFoKZkT1JbVM=";
   };
 
   mvnHash = "sha256-QyH8AEKX94ieRS6dqFy0QJxwqtpRmnl+E2dJwNLS9kQ=";
@@ -41,10 +42,12 @@ maven.buildMavenPackage rec {
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   doInstallCheck = true;
+  versionCheckProgramArg = "version";
 
   meta = {
     description = "Allows generation of API client libraries (SDK generation), server stubs and documentation automatically given an OpenAPI Spec";
     homepage = "https://github.com/swagger-api/swagger-codegen";
+    changelog = "https://github.com/swagger-api/swagger-codegen/releases/tag/v${version}";
     sourceProvenance = with lib.sourceTypes; [
       fromSource
       binaryBytecode
