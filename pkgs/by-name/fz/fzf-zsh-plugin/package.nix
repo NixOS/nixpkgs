61494a377f304d9c939f75606051f69e1c4c117c
System administrator <root@jonquille.jonquille>
2026-03-12 16:38:12 +0100

    fzf-update-script: 1.0.0-unstable-2025-12-15 -> 1.0.0-unstable-2026-03-03

--- pkgs/by-name/fz/fzf-zsh-plugin/package.nix
+++ pkgs/by-name/fz/fzf-zsh-plugin/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation {
   pname = "fzf-zsh-plugin";
-  version = "1.0.0-unstable-2025-12-15";
+  version = "1.0.0-unstable-2026-03-03";
 
   src = fetchFromGitHub {
     owner = "unixorn";
     repo = "fzf-zsh-plugin";
-    rev = "cdd9d5cc3b41a3a390a0fb8605c40de652da6309";
-    hash = "sha256-i6qoaMWVofhD3K6/RaaNatzA2aokiNQ5ilqmahprJFU=";
+    rev = "d56d2387ce376f80e42c46654a9ee1f899a40b46";
+    hash = "sha256-twry9z9gDvRfH3AOWEV/a9HX4pnlMJDSw74Sm/MBwIk=";
   };
 
   strictDeps = true;
