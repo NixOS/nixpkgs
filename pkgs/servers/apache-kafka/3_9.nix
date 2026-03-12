d75b01eb76f4c98423ebda6c49f6b8a08570f211
Jonathan Davies <jpds@protonmail.com>
2026-03-12 22:12:34 +0000

    apacheKafka: Refactor into per-series files with generic.nix
    Split the monolithic default.nix versionMap into per-series files
    that import a shared generic.nix

---
+++ pkgs/servers/apache-kafka/3_9.nix
@@ -0,0 +1,6 @@
+import ./generic.nix {
+  seriesFile = ./3_9.nix;
+  kafkaVersion = "3.9.1";
+  scalaVersion = "2.13";
+  sha256 = "sha256-3UOZgW5niUbKt2470WhhA1VeabyPKrhobNpxqhW8MaM=";
+}
---
+++ pkgs/servers/apache-kafka/4_0.nix
@@ -0,0 +1,6 @@
+import ./generic.nix {
+  seriesFile = ./4_0.nix;
+  kafkaVersion = "4.0.1";
+  scalaVersion = "2.13";
+  sha256 = "sha256-M8xc04WE6w9yYWxMLwX7SINKVvoKnNn+AKxzIMNM9SQ=";
+}
