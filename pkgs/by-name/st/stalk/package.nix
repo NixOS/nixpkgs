3561b7cf905fc6018c0457deee98ee15eab22043
Daniel Hähnke <11663234+refactoriel@users.noreply.github.com>
2026-03-13 13:53:52 +0100

    stalk: init at 0.7.3

---
+++ pkgs/by-name/st/stalk/package.nix
@@ -0,0 +1,31 @@
+{ lib
+, fetchFromCodeberg
+, buildGoModule
+,
+}:
+buildGoModule (finalAttrs: {
+  pname = "stalk";
+  version = "0.7.3";
+
+  src = fetchFromCodeberg {
+    owner = "xrstf";
+    repo = "stalk";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-DeDmfoNYoF1JRHg4LkQrjbntOhgjdOrBFTxp2JAgSnA=";
+  };
+
+  proxyVendor = true;
+
+  vendorHash = "sha256-cWpj9MgqwsoTNTu/Wf5Nq/tDQUUhbNyCBxjZGZH+hoI=";
+
+  meta = {
+    description = "Watch your Kubernetes Resources change";
+    longDescription = ''
+      A command line tool to watch a given set of kubernetes resources and print the diffs for every change.
+    '';
+    homepage = "https://codeberg.org/xrstf/stalk";
+    license = lib.licenses.mit;
+    maintainers = [ "refactoriel" ];
+    mainProgram = "stalk";
+  };
+})
