12520b27c1d7703426adb0eb6ba56cf0307426c6
Tomas Xavier Santos <tom.xaviersantos@gmail.com>
2026-03-13 01:59:21 -0300

    maintainers: add tom-xs

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -27385,6 +27385,12 @@
     githubId = 74688871;
     name = "Tochukwu Ahanonu";
   };
+  tom-xs = {
+    email = "tom.xaviersantos@gmail.com";
+    github = "tom-xs";
+    githubId = 434264191;
+    name = "Tomas Xavier Santos";
+  };
   tom94 = {
     email = "nix@94.me";
     github = "Tom94";
