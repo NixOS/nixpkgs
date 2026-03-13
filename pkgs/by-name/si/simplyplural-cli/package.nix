5ebee4330a93fd86af9b4a7c9952f7c22c1b23c2
Ori <meow@ori.cat>
2026-03-13 11:33:23 +0100

    simplyplural-cli: init at 0.1.1

---
+++ pkgs/by-name/si/simplyplural-cli/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+}:
+
+python3Packages.buildPythonPackage {
+  pname = "simplyplural-cli";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "SiteRelEnby";
+    repo = "simplyplural-cli";
+    rev = "v0.1.1";
+    hash = "sha256-D4w4sBpAbPrquSvUHteEKGMoW7/oRR1leeevKfl5izA=";
+  };
+
+  pyproject = true;
+  build-system = [ python3Packages.setuptools ];
+
+  dependencies = [
+    python3Packages.requests
+    python3Packages.websockets
+  ];
+
+  meta = with lib; {
+    description = "A command-line interface for SimplyPlural";
+    homepage = "https://github.com/SiteRelEnby/simplyplural-cli";
+    license = licenses.lgpl21Only;
+    maintainers = with maintainers; [ oricat ];
+  };
+}
