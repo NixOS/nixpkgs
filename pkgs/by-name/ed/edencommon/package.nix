c804c11a3a0ff0b49567c0f81bc93258277390cd
Cobalt <cobalt@cobalt.rocks>
2026-03-13 11:54:29 +0100

    edencommon: 2026.01.19.00 -> 2026.03.09.00

--- pkgs/by-name/ed/edencommon/package.nix
+++ pkgs/by-name/ed/edencommon/package.nix
@@ -20,7 +20,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "edencommon";
-  version = "2026.01.19.00";
+  version = "2026.03.09.00";
 
   outputs = [
     "out"
@@ -31,7 +31,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "facebookexperimental";
     repo = "edencommon";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-gTf3NgxCYaIJ4ubkPKrPct4D6IsHnTZRAzrDHoErDNM=";
+    hash = "sha256-9LRugtsgyDAjujJEFqDMpKk6b+fybvplaQ73U6lGq4o=";
   };
 
   patches = [
