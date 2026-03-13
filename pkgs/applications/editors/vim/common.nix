488f9ade0d3ea2a9f2efb61364b18dbef478dff8
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-13 04:23:03 +0000

    vim: 9.2.0106 -> 9.2.0140

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,6 +1,6 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "9.2.0106";
+  version = "9.2.0140";
 
   outputs = [
     "out"
@@ -11,7 +11,7 @@ rec {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    hash = "sha256-byOf2Gr1vA7xQw3YHV454te1QrVxRy3sXrLdFUp2XRg=";
+    hash = "sha256-0eUOEAkR2Hcd4blxToSmYhN1+C3gNW0Up2n6n0TAryA=";
   };
 
   enableParallelBuilding = true;
