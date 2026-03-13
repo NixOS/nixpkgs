7f2ca0c0da52c4e8deeba278e409de92f66e38eb
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-13 23:29:43 +0000

    kraft: 0.12.5 -> 0.12.6

--- pkgs/by-name/kr/kraft/package.nix
+++ pkgs/by-name/kr/kraft/package.nix
@@ -15,13 +15,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "kraft";
-  version = "0.12.5";
+  version = "0.12.6";
 
   src = fetchFromGitHub {
     owner = "unikraft";
     repo = "kraftkit";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-/ReHXxvn/6dDJVxk5BOvxSZrlkDkZEfr+qM5raf2a3A=";
+    hash = "sha256-+n2qNf3uTSbLKZwdwP5mfE9po1TwSQTsTh0M49hZ/x4=";
   };
 
   nativeBuildInputs = [
@@ -39,7 +39,7 @@ buildGoModule (finalAttrs: {
     yajl
   ];
 
-  vendorHash = "sha256-1rdpyOJVeyzYT0WHJbeqO3aH15FN1/9iQ9bEsjWwn4c=";
+  vendorHash = "sha256-hML4nDeeob6Ru+VUuhSu/T4G/E+EbM7a0etCnxQNiKQ=";
 
   ldflags = [
     "-s"
