71eb33e7fcc2890b42b8e886d1502c27130a7c5c
scraptux <git@thomasjasny.de>
2026-03-14 12:26:57 +0100

    python3Packages.grpcio-tools: 1.78.0 -> 1.78.1

--- pkgs/development/python-modules/grpcio-tools/default.nix
+++ pkgs/development/python-modules/grpcio-tools/default.nix
@@ -13,13 +13,13 @@
 # nixpkgs-update: no auto update
 buildPythonPackage rec {
   pname = "grpcio-tools";
-  version = "1.78.0";
+  version = "1.78.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "grpcio_tools";
     inherit version;
-    hash = "sha256-Sw3YZWAnQxbhVdklFYJ2+FZFCBkwiLxD4g0/Xf+Vays=";
+    hash = "sha256-9Ht0awapQJVLmqhrGCSqSHTwaKfsLUtAeYDSAshqaRo=";
   };
 
   postPatch = ''
