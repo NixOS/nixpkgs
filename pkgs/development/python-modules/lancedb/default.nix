055a3002af81ac92bfb6d53242545ee8085b0645
Gaetan Lepage <gaetan@glepage.com>
2026-03-14 12:18:42 +0000

    python3Packages.lancedb: 0.27.1 -> 0.29.2
    Diff: https://github.com/lancedb/lancedb/compare/python-v0.27.1...python-v0.29.2
    
    Changelog: https://github.com/lancedb/lancedb/releases/tag/python-v0.29.2

--- pkgs/development/python-modules/lancedb/default.nix
+++ pkgs/development/python-modules/lancedb/default.nix
@@ -42,21 +42,21 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "lancedb";
-  version = "0.27.1";
+  version = "0.29.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "lancedb";
     repo = "lancedb";
     tag = "python-v${finalAttrs.version}";
-    hash = "sha256-pWrwv3VtfkfOKnkiiu26yRDrDrsNxb+0r/kcNHwzmhU=";
+    hash = "sha256-sOgRGn0romt5xJ70c+GSqNoRAp0D9F9hEq6bm7JRK/Y=";
   };
 
   buildAndTestSubdir = "python";
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-U1Od4lhaaGdYF3TISfRWY7sRmyyniZqLofBCnYAo1ew=";
+    hash = "sha256-GQA5RuxpYmrNqNrpW4GLZlXrphflErmR5PeeVcs6ZJQ=";
   };
 
   build-system = [ rustPlatform.maturinBuildHook ];
@@ -121,6 +121,17 @@ buildPythonPackage (finalAttrs: {
     # FileNotFoundError: [Errno 2] Cannot delete directory '/nix/var/nix/builds/nix-41395-654732360/.../test.lance/_indices/fts':
     # Cannot get information for path '/nix/var/nix/builds/nix-41395-654732360/.../test.lance/_indices/fts/.tmppyKXfw'
     "test_create_index_from_table"
+
+    # lance_namespace.errors.UnsupportedOperationError: Not supported: create_empty_table
+    "TestAsyncNamespaceConnection"
+    "TestNamespaceConnection"
+
+    # Failed: DID NOT RAISE <class 'Exception'>
+    "test_merge_insert"
+
+    # TypeError: FFILanceTableProvider.__datafusion_table_provider__() missing 1 required positional
+    # argument: 'session'
+    "test_sql_query"
   ]
   ++ lib.optionals ((pythonAtLeast "3.14") && stdenv.hostPlatform.isDarwin) [
     # Failed: DID NOT RAISE <class 'Exception'>
