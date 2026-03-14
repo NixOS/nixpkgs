56bd935a70e0275a4302bc07b7f20678da94cf81
santoshxshrestha <santoshxshrestha@gmail.com>
2026-03-14 20:39:23 +0545

    maintainers: update user name to the latest one

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23927,12 +23927,6 @@
     email = "hey@sandydoo.me";
     matrix = "@sandydoo:matrix.org";
   };
-  santosh = {
-    email = "santoshxshrestha@gmail.com";
-    name = "Santosh Shrestha";
-    github = "santoshxshrestha";
-    githubId = 182977126;
-  };
   sarahec = {
     email = "seclark@nextquestion.net";
     github = "sarahec";
@@ -28401,6 +28395,12 @@
     github = "viluon";
     githubId = 7235381;
     name = "Ondřej Kvapil";
+  };
+  vimlinuz = {
+    email = "vimlinuz@gmail.com";
+    name = "Santosh Shrestha";
+    github = "vimlinuz";
+    githubId = 182977126;
   };
   vincentbernat = {
     email = "vincent@bernat.ch";
