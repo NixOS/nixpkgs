b1f8553abfa1344692f5dc7090dc8d2962a6d70e
Niklas Korz <niklas@niklaskorz.de>
2026-03-15 18:01:21 +0100

    librechat: 0.8.0 -> 0.8.3
    Fixes CVE-2026-31944 and CVE-2026-31949
    
    Changelogs:
    
    - https://github.com/danny-avila/LibreChat/releases/tag/v0.8.3
    - https://github.com/danny-avila/LibreChat/releases/tag/v0.8.2
    - https://github.com/danny-avila/LibreChat/releases/tag/v0.8.1

--- pkgs/by-name/li/librechat/package.nix
+++ pkgs/by-name/li/librechat/package.nix
@@ -13,13 +13,13 @@
 
 buildNpmPackage rec {
   pname = "librechat";
-  version = "0.8.0";
+  version = "0.8.3";
 
   src = fetchFromGitHub {
     owner = "danny-avila";
     repo = "LibreChat";
     tag = "v${version}";
-    hash = "sha256-DTmb9J2nsMy6f+V6BgRtFgpTwOi9OQnvikSx4QZQ0HI=";
+    hash = "sha256-91VQg+dN6nYNWywUsoQZ1SXPT2Vyc0X6j9irKcm5/fM=";
   };
 
   patches = [
@@ -30,28 +30,14 @@ buildNpmPackage rec {
     # Also, we set the `bin` property to the server script to benefit from the
     # auto-generated wrapper.
     ./0001-npm-pack.patch
-    # LibreChat tries writing logs to the package directory, which is immutable
-    # in our case. We patch the log directory to target the current working directory
+    # LibreChat tries writing user uploads logs to the package directory, which is immutable
+    # in our case. We patch the upload directory to target the current working directory
     # instead, which in case of NixOS will be the service's data directory.
-    ./0002-logs.patch
-    # Similarly to the logs, user uploads are by default written to the package
-    # directory as well. Again, we patch this to be relative to the current working
-    # directory instead.
-    ./0003-upload-paths.patch
-    # The npm dependencies are causing issues with the build. The package @testing-library/react
-    # appears to not be included in NPM deps, even though it is present in the project
-    # This patch fixes this by placing the dependency in different files and regenerating the
-    # lock file.
-    ./0004-fix-deps-v080.patch
+    ./0002-upload-paths.patch
   ];
 
-  npmDepsHash = "sha256-97cEw6VD7FoVayrxClHuS1iUcQmDw7/aUoUV6ektvOY=";
-  npmDeps = fetchNpmDeps {
-    inherit src;
-    name = "${pname}-${version}-npm-deps-patched";
-    hash = npmDepsHash;
-    patches = [ ./0004-fix-deps-v080.patch ];
-  };
+  npmDepsHash = "sha256-gUUbEZ9tR6ozLmcxwxmaAlE7eampEhhQTmuPsDCypO8=";
+  npmDepsFetcherVersion = 2;
 
   # npm dependency install fails with nodejs_24: https://github.com/NixOS/nixpkgs/issues/474535
   nodejs = nodejs_22;
