f35639db1f965e2f5a1a67aaf695a4d1a10944ba
Philip Wilk <p.wilk@student.reading.ac.uk>
2026-03-15 14:58:13 +0000

    python3Packages.imbalanced-learn: enable tests fixed upstream

--- pkgs/development/python-modules/imbalanced-learn/default.nix
+++ pkgs/development/python-modules/imbalanced-learn/default.nix
@@ -76,12 +76,6 @@ buildPythonPackage rec {
     "imblearn/utils/tests/test_min_dependencies.py"
   ];
 
-  disabledTests = [
-    # Broken upstream test https://github.com/scikit-learn-contrib/imbalanced-learn/issues/1131
-    "test_estimators_compatibility_sklearn"
-    "test_balanced_bagging_classifier_with_function_sampler"
-  ];
-
   meta = {
     description = "Library offering a number of re-sampling techniques commonly used in datasets showing strong between-class imbalance";
     homepage = "https://github.com/scikit-learn-contrib/imbalanced-learn";
