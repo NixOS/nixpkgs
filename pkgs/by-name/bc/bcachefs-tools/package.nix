455eee68f2126528626a794e76ef507eadf2a19f
Zebreus <zebreus@zebre.us>
2026-03-15 20:46:47 +0100

    bcachefs-tools: 1.36.1 -> 1.37.0

--- pkgs/by-name/bc/bcachefs-tools/package.nix
+++ pkgs/by-name/bc/bcachefs-tools/package.nix
@@ -29,18 +29,18 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "bcachefs-tools";
-  version = "1.36.1";
+  version = "1.37.0";
 
   src = fetchFromGitHub {
     owner = "koverstreet";
     repo = "bcachefs-tools";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-15Z1lHNeXTToDpdVc/YB5ojhoiB5qdgWs47O1aKoyFM=";
+    hash = "sha256-whLigwGbMhxavTJGRyeV9GCw8OX0+9FvQk567QxywNQ=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) src;
-    hash = "sha256-YWsJUSgKNkK9W4Yuolix21bRRFSF01+sivoj7SJo7DY=";
+    hash = "sha256-wm/Zfuc+tpER7PTRjKftmtCkoKZ51ZUJKGsHQ79cJbE=";
   };
 
   postPatch = ''
