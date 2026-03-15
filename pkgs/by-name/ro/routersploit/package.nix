e2f246ef2da854c3a40801de928b93cca0d1d2e8
Sarah Clark <seclark@nextquestion.net>
2026-03-15 09:57:30 -0700

    routersploit: enable testing for python < 3.14

--- pkgs/by-name/ro/routersploit/package.nix
+++ pkgs/by-name/ro/routersploit/package.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  stdenv,
   fetchFromGitHub,
   python3,
 }:
@@ -26,12 +27,17 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
   ];
 
   # Tests are out-dated and support for newer pysnmp is not implemented yet
+  doCheck = python3.pythonOlder "3.14";
 
-  nativeCheckInputs = with python3.pkgs; [
-    pytest-xdist
-    pytestCheckHook
-    threat9-test-bed
-  ];
+  nativeCheckInputs =
+    with python3.pkgs;
+    [
+      pytest-xdist
+      pytestCheckHook
+    ]
+    ++ lib.optionals (python3.pythonOlder "3.14") [
+      threat9-test-bed
+    ];
 
   postInstall = ''
     mv $out/bin/rsf.py $out/bin/rsf
@@ -45,6 +51,26 @@ python3.pkgs.buildPythonApplication (finalAttrs: {
     "tests/test_exploit_scenarios.py"
     "tests/test_module_info.py"
   ];
+
+  disabledTestPaths =
+    lib.optionals (python3.pythonOlder "3.14") [
+      # AttributeError: module 'paramiko' has no attribute 'DSSKey'.
+      "tests/test_exploit_scenarios.py::test_exploit_empty_response[Exploit119]"
+      "tests/test_exploit_scenarios.py::test_exploit_error_response[Exploit119]"
+      "tests/test_exploit_scenarios.py::test_exploit_not_found_response[Exploit119]"
+      "tests/test_exploit_scenarios.py::test_exploit_redirect_response[Exploit119]"
+      "tests/test_exploit_scenarios.py::test_exploit_timeout_response[Exploit119]"
+      "tests/test_exploit_scenarios.py::test_exploit_trash_response[Exploit119]"
+    ]
+    ++ lib.optionals (python3.pythonOlder "3.14" && stdenv.hostPlatform.isDarwin) [
+      # AttributeError: module 'paramiko' has no attribute 'DSSKey'.
+      "tests/test_exploit_scenarios.py::test_exploit_empty_response[Exploit142]"
+      "tests/test_exploit_scenarios.py::test_exploit_error_response[Exploit142]"
+      "tests/test_exploit_scenarios.py::test_exploit_not_found_response[Exploit142]"
+      "tests/test_exploit_scenarios.py::test_exploit_redirect_response[Exploit142]"
+      "tests/test_exploit_scenarios.py::test_exploit_timeout_response[Exploit142]"
+      "tests/test_exploit_scenarios.py::test_exploit_trash_response[Exploit142]"
+    ];
 
   meta = {
     description = "Exploitation Framework for Embedded Devices";
