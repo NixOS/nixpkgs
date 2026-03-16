fd162708318e6c2aa0352afd0af1777feddcfe52
Guy Chronister <guychronister@outlook.com>
2026-03-16 16:17:29 -0500

    unigine-sanctuary: migrate to by-name, use multilib

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10710,8 +10710,6 @@ with pkgs;
     }
   );
 
-  unigine-sanctuary = pkgsi686Linux.callPackage ../applications/graphics/unigine-sanctuary { };
-
   unigine-superposition = libsForQt5.callPackage ../applications/graphics/unigine-superposition { };
 
   uuagc = haskell.lib.compose.justStaticExecutables haskellPackages.uuagc;
