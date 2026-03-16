f60636fad0e72fd1d6f4ad2b0af92cc0a92396f2
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-03-16 09:13:34 +0000

    zabbix: 6.0.44 -> 6.0.45

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -8,7 +8,7 @@ generic: {
     hash = "sha256-Q+pfyx5dsl50vcg+qJNtebgJO2FK9OiJQXSFvHTwYeI=";
   };
   v60 = generic {
-    version = "6.0.44";
-    hash = "sha256-TveW2ofMnm3PXUPwoklTPe48tI6dED/dAQKjwO2P4F4=";
+    version = "6.0.45";
+    hash = "sha256-duB2w2xDH9ZHEDh9iZ5Fr6RH0dI2lMCVvySA5dmSZcU=";
   };
 }
