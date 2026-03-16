c182d8a22747f3ba2f094ef7591b0e31b3f0fc45
Thomas Gerbet <thomas@gerbet.me>
2026-03-16 12:07:10 +0100

    libssh: 0.11.3 -> 0.11.4
    https://www.libssh.org/2026/02/10/libssh-0-12-0-and-0-11-4-security-releases/
    
    Fixes:
    * CVE-2026-0964
    * CVE-2026-0965
    * CVE-2026-0966
    * CVE-2026-0967
    * CVE-2026-0968
    * http://www.libssh.org/security/advisories/libssh-2026-sftp-extensions.txt
    
    Fixes #498202

--- pkgs/by-name/li/libssh/package.nix
+++ pkgs/by-name/li/libssh/package.nix
@@ -16,11 +16,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libssh";
-  version = "0.11.3";
+  version = "0.11.4";
 
   src = fetchurl {
     url = "https://www.libssh.org/files/${lib.versions.majorMinor finalAttrs.version}/libssh-${finalAttrs.version}.tar.xz";
-    hash = "sha256-fYoTYbsJTsP1EZZOeKWk26aJtZhuESr6vk9NDWxhJcM=";
+    hash = "sha256-ACrDIOPWbJ4QDsZXbj6EqgxIlJ79479bQKKAKZIpdwE=";
   };
 
   outputs = [
