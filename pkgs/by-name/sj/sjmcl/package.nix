473fb54b7900016ab6639bb34095391671ad40ff
FrdrCkII <FrdrCkII@outlook.com>
2026-03-16 15:12:31 +0800

    sjmcl: fix mc lib issue And add `additionalLibs` and `additionalPrograms`

--- pkgs/by-name/sj/sjmcl/package.nix
+++ pkgs/by-name/sj/sjmcl/package.nix
@@ -1,15 +1,72 @@
 {
   lib,
+  stdenv,
   symlinkJoin,
   makeWrapper,
   sjmcl-unwrapped,
-  desktop-file-utils,
   hicolor-icon-theme,
+  desktop-file-utils,
+  zenity,
+  glfw3-minecraft,
+  openal,
+  alsa-lib,
+  libjack2,
+  libpulseaudio,
+  pipewire,
+  libGL,
+  libx11,
+  libxcursor,
+  libxext,
+  libxrandr,
+  libxxf86vm,
+  udev,
+  vulkan-loader,
+  flite,
+  gamemode,
+  libusb1,
+  additionalLibs ? [ ],
+  additionalPrograms ? [ ],
+  controllerSupport ? stdenv.hostPlatform.isLinux,
+  gamemodeSupport ? stdenv.hostPlatform.isLinux,
+  textToSpeechSupport ? stdenv.hostPlatform.isLinux,
   jdks ? [ jdk ],
   jdk,
 }:
 let
   sjmcl' = sjmcl-unwrapped;
+  runtimeBin = [
+    desktop-file-utils
+    zenity
+  ]
+  ++ additionalPrograms;
+  runtimeLib = [
+    (lib.getLib stdenv.cc.cc)
+    ## native versions
+    glfw3-minecraft
+    openal
+
+    ## openal
+    alsa-lib
+    libjack2
+    libpulseaudio
+    pipewire
+
+    ## glfw
+    libGL
+    libx11
+    libxcursor
+    libxext
+    libxrandr
+    libxxf86vm
+
+    udev # oshi
+
+    vulkan-loader # VulkanMod's lwjgl
+  ]
+  ++ lib.optional textToSpeechSupport flite
+  ++ lib.optional gamemodeSupport gamemode.lib
+  ++ lib.optional controllerSupport libusb1
+  ++ additionalLibs;
 in
 symlinkJoin {
   pname = "sjmcl";
@@ -23,7 +80,6 @@ symlinkJoin {
   nativeBuildInputs = [
     makeWrapper
   ];
-
   buildInputs = [
     desktop-file-utils
     hicolor-icon-theme
@@ -31,6 +87,7 @@ symlinkJoin {
 
   postBuild = ''
     wrapProgram $out/bin/SJMCL \
-      --prefix PATH : "${lib.makeBinPath ([ desktop-file-utils ] ++ jdks)}"
+      --prefix PATH : "${lib.makeBinPath (jdks ++ runtimeBin)}" \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeLib}"
   '';
 }
