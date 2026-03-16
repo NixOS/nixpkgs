NixOS/nixpkgs

commit 84fa3564133c642b87dedebcee0672f16d53adfa

crowdsec-firewall-bouncer: directly reference cscli package

nixos/modules/services/security/crowdsec-firewall-bouncer.nix
@@ -231,7 +231,7 @@ in
           after = [ "crowdsec.service" ];
           wants = after;
           script = ''
-            cscli=/run/current-system/sw/bin/cscli
+            cscli=${lib.getExe' config.services.crowdsec.package "cscli"}
             if $cscli bouncers list --output json | ${lib.getExe pkgs.jq} -e -- ${lib.escapeShellArg "any(.[]; .name == \"${cfg.registerBouncer.bouncerName}\")"} >/dev/null; then
               # Bouncer already registered. Verify the API key is still present
               if [ ! -f ${apiKeyFile} ]; then

commit 28a3448e4e15126500f97e70980ce36cfe7c63d2

crowdsec-firewall-bouncer: fix "error while reading yaml file"
This reverts 84fa356 which introduced a regression.
Not using a wrapper script results in `cscli` trying to use a default
config in /etc/crowdsec/config.yaml and fail.

nixos/modules/services/security/crowdsec-firewall-bouncer.nix
@@ -231,7 +231,8 @@ in
           after = [ "crowdsec.service" ];
           wants = after;
           script = ''
-            cscli=${lib.getExe' config.services.crowdsec.package "cscli"}
+            # Need to use a `cscli` wrapper which sets `--config` to correct path.
+            cscli=/run/current-system/sw/bin/cscli
             if $cscli bouncers list --output json | ${lib.getExe pkgs.jq} -e -- ${lib.escapeShellArg "any(.[]; .name == \"${cfg.registerBouncer.bouncerName}\")"} >/dev/null; then
               # Bouncer already registered. Verify the API key is still present
               if [ ! -f ${apiKeyFile} ]; then