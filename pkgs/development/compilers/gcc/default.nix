56ce8eb4f4b91d1f8e1c1588f40c3604a54422aa
Luna Nova <git@lunnova.dev>
2026-03-15 19:10:15 -0700

    stdenv: add canReloc for whether relocatable code objects are supported
    necessary to decouple this from hasSharedLibraries since pkgsStatic's
    hasSharedLibraries is false but we need relocatable objects for static-pie
    
    linux-musl-gcc-15.2.0-lib/x86_64-unknown-linux-musl/lib/libstdc++.a(bad_alloc.o):
      relocation R_X86_64_32 against `.rodata._ZNKSt9bad_alloc4whatEv.str1.1' can not
      be used when making a PIE object; recompile with -fPIE

--- pkgs/development/compilers/gcc/default.nix
+++ pkgs/development/compilers/gcc/default.nix
@@ -19,7 +19,7 @@
   cargo,
   staticCompiler ? false,
   enableShared ? stdenv.targetPlatform.hasSharedLibraries,
-  enableDefaultPie ? stdenv.targetPlatform.hasSharedLibraries,
+  enableDefaultPie ? stdenv.targetPlatform.canReloc,
   enableLTO ? stdenv.hostPlatform.hasSharedLibraries,
   texinfo ? null,
   perl ? null, # optional, for texi2pod (then pod2man)
