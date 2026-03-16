171dba4b97e6742b9578e1b599893b7a4d53cf0c
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-03-16 09:15:00 +0000

    zabbix70: 7.0.23 -> 7.0.24
    (cherry picked from commit 940d40ae6e12073acd6ca9829c7bffc388e41218)

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -8,8 +8,8 @@ generic: {
     hash = "sha256-twEwY7nGJWoYX25gAe/31DuDGxg6o5Ez1u5STQVw6Fw=";
   };
   v70 = generic {
-    version = "7.0.23";
-    hash = "sha256-Q+pfyx5dsl50vcg+qJNtebgJO2FK9OiJQXSFvHTwYeI=";
+    version = "7.0.24";
+    hash = "sha256-b4rpkLmyV2fk//vLXMfEVdZ04qOS3CFHhIil0cDn1Zc=";
   };
   v60 = generic {
     version = "6.0.44";
