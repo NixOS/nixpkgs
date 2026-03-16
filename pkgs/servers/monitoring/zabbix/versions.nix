940d40ae6e12073acd6ca9829c7bffc388e41218
bstanderline <153822813+bstanderline@users.noreply.github.com>
2026-03-16 09:15:00 +0000

    zabbix70: 7.0.23 -> 7.0.24

--- pkgs/servers/monitoring/zabbix/versions.nix
+++ pkgs/servers/monitoring/zabbix/versions.nix
@@ -4,8 +4,8 @@ generic: {
     hash = "sha256-FsOZQv6yg9euoX6Ccfy0/9usBpkVYVjcs3IZZ095m9g=";
   };
   v70 = generic {
-    version = "7.0.23";
-    hash = "sha256-Q+pfyx5dsl50vcg+qJNtebgJO2FK9OiJQXSFvHTwYeI=";
+    version = "7.0.24";
+    hash = "sha256-b4rpkLmyV2fk//vLXMfEVdZ04qOS3CFHhIil0cDn1Zc=";
   };
   v60 = generic {
     version = "6.0.45";
