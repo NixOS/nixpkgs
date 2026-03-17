2cb9c37ad3a64b397d60b75813d34a70efbab0ec
Stanisław Pitucha <git@viraptor.info>
2026-03-18 09:21:49 +1100

    transmission_4-{mac,qt,qt5}: enable darwin builds, mark strictDeps

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10617,6 +10617,7 @@ with pkgs;
   transmission_4-qt5 = transmission_4.override { enableQt5 = true; };
   transmission_4-qt6 = transmission_4.override { enableQt6 = true; };
   transmission_4-qt = transmission_4-qt5;
+  transmission_4-mac = transmission_4.override { enableMac = true; };
 
   tinywl = callPackage ../applications/window-managers/tinywl {
     wlroots = wlroots_0_20;
