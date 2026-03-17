b35574756a5e3e2f3ff90755c518451da574e4c7
huanghunr <huanghunr@outlook.com>
2026-03-17 11:22:06 +0000

    maintainers: format maintainer-list.nix

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -10897,6 +10897,12 @@
     githubId = 60099368;
     name = "Rafael Kienitz";
   };
+  huanghunr = {
+    email = "huanghunr@outlook.com";
+    github = "huanghunr";
+    githubId = 160948982;
+    name = "huanghunr";
+  };
   huantian = {
     name = "David Li";
     email = "davidtianli@gmail.com";
@@ -30396,12 +30402,6 @@
     github = "zzzsyyy";
     githubId = 59917878;
     name = "Mathias Zhang";
-  };
-  huanghunr = {
-    email = "huanghunr@outlook.com";
-    github = "huanghunr";
-    githubId = 160948982;
-    name = "huanghunr";
   };
   # keep-sorted end
 }
