27c7ddbb8c9188b3cd36f2808618b24654733aa0
Miguel Villafuerte <24855057+mvillafuertem@users.noreply.github.com>
2026-03-17 15:37:17 +0100

    zulu17: 17.0.12 -> 17.0.18

--- pkgs/development/compilers/zulu/17.nix
+++ pkgs/development/compilers/zulu/17.nix
@@ -10,43 +10,43 @@ callPackage ./common.nix (
     # Note that the latest build may differ by platform
     dists = {
       x86_64-linux = {
-        zuluVersion = "17.52.17";
-        jdkVersion = "17.0.12";
+        zuluVersion = "17.64.17";
+        jdkVersion = "17.0.18";
         hash =
           if enableJavaFX then
-            "sha256-qKpkvk7/IEnlOJoy7An0AVGUXWkWeuuiQzqKKE/+Ec4="
+            "sha256-jQ2ByIKtivoc9nJ/LPMZCktyjceqXY+B6a36sXmwUbE="
           else
-            "sha256-JCRcjQzkkV2G5wsoi13psbTHjxCtuw5rqT4pEAOarRk=";
+            "sha256-gZ4/CepiiQGiGyEE7Y9SVuF66RpBRbJysushMfgyrx0=";
       };
 
       aarch64-linux = {
-        zuluVersion = "17.52.17";
-        jdkVersion = "17.0.12";
+        zuluVersion = "17.64.17";
+        jdkVersion = "17.0.18";
         hash =
           if enableJavaFX then
-            "sha256-mpTM/43oyDsOnoZM8AW1Z7EFTznnPAnYoC+T6csc8Fw="
+            "sha256-ciyywX9R72RRt6HPfdmmsgFo/0ebr2xzNhnroAEDT7E="
           else
-            "sha256-uIz2D5WjqySy5lobuvWp3kFTGsBKhXzT56QgCGyXwSY=";
+            "sha256-21fcnh+CIsL47604yMo2CxAR2Ln7m+oJVths1157Lb0=";
       };
 
       x86_64-darwin = {
-        zuluVersion = "17.52.17";
-        jdkVersion = "17.0.12";
+        zuluVersion = "17.64.17";
+        jdkVersion = "17.0.18";
         hash =
           if enableJavaFX then
-            "sha256-jb1oPmRzlYUMONovdsHowlC44X3/PFZ8KilxSsR924U="
+            "sha256-td551xWmeHcD26J0gxboKS6Y3f5iLRhcUy97pfxhHxk="
           else
-            "sha256-8VlGFUhCn3NE2A42xi69KzZqu0RoGOV1ZYj8oaqCnTc=";
+            "sha256-mHVhWicg6xAn8BaT4uCN93ChT8gYSYAfX7h7kRgdqyI=";
       };
 
       aarch64-darwin = {
-        zuluVersion = "17.52.17";
-        jdkVersion = "17.0.12";
+        zuluVersion = "17.64.17";
+        jdkVersion = "17.0.18";
         hash =
           if enableJavaFX then
-            "sha256-rEILTKTpX8DEoOGhlrhbxpcCyF2+QrjW92h0ox9ezp0="
+            "sha256-TnN+tPJ8HJSlZ0EUDq5SHo40EviO0iMX0P9eYHyRVP4="
           else
-            "sha256-RZ3hNQQFE+spTT9lFHLjmkolSkpHDg2Hvro6xi7uNww=";
+            "sha256-EiRGuK028dyAZ3Jy29rXH9XKjXbfd0wB54Oce/TSZ9Y=";
       };
     };
   }
