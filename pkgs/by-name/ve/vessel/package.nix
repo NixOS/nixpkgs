53fbca7b4562b5ed48b82812fb38688a14a67757
Guy Chronister <guychronister@outlook.com>
2026-03-16 19:23:54 -0500

    vessel: modernize derivation

--- pkgs/by-name/ve/vessel/package.nix
+++ pkgs/by-name/ve/vessel/package.nix
@@ -4,21 +4,21 @@
   pkgsi686Linux,
 }:
 
-pkgsi686Linux.stdenv.mkDerivation rec {
+pkgsi686Linux.stdenv.mkDerivation (finalAttrs: {
   pname = "vessel";
   version = "12082012";
 
   goBuyItNow = ''
     We cannot download the full version automatically, as you require a license.
     Once you bought a license, you need to add your downloaded version to the nix store.
-    You can do this by using "nix-prefetch-url file://\$PWD/vessel-${version}-bin" in the
+    You can do this by using "nix-prefetch-url file://\$PWD/vessel-${finalAttrs.version}-bin" in the
     directory where you saved it.
   '';
 
   src = requireFile {
-    message = goBuyItNow;
-    name = "vessel-${version}-bin";
-    sha256 = "1vpwcrjiln2mx43h7ib3jnccyr3chk7a5x2bw9kb4lw8ycygvg96";
+    message = finalAttrs.goBuyItNow;
+    name = "vessel-${finalAttrs.version}-bin";
+    hash = "sha256-Jr39PPOIU7Jm4kv0os6EbGTPmJVjxQMH6VVYGmVm/O4=";
   };
 
   ld_preload = ./isatty.c;
@@ -92,4 +92,4 @@ pkgsi686Linux.stdenv.mkDerivation rec {
     maintainers = with lib.maintainers; [ jcumming ];
   };
 
-}
+})
