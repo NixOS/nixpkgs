563a53da242ca02026b8385d3e8e50b77d7fdea0
Fabian Affolter <mail@fabian-affolter.ch>
2026-03-17 19:43:39 +0100

    python3Packages.aiocomelit: migrate to finalAttrs

--- pkgs/development/python-modules/aiocomelit/default.nix
+++ pkgs/development/python-modules/aiocomelit/default.nix
@@ -8,20 +8,17 @@
   poetry-core,
   pytest-cov-stub,
   pytestCheckHook,
-  pythonOlder,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aiocomelit";
   version = "2.0.1";
   pyproject = true;
 
-  disabled = pythonOlder "3.12";
-
   src = fetchFromGitHub {
     owner = "chemelli74";
     repo = "aiocomelit";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-RlSDSxJZ6xD0ifqGT4l2Jkfzo64rrrLCE/yykeJNaYs=";
   };
 
@@ -43,8 +40,8 @@ buildPythonPackage rec {
   meta = {
     description = "Library to control Comelit Simplehome";
     homepage = "https://github.com/chemelli74/aiocomelit";
-    changelog = "https://github.com/chemelli74/aiocomelit/blob/${src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/chemelli74/aiocomelit/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
