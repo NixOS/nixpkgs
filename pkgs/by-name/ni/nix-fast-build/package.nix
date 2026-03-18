40de57069ed08c65acb221d155b9c97f8ae73657
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-18 13:31:31 +0000

    nix-fast-build: 1.3.0 -> 1.4.0

--- pkgs/by-name/ni/nix-fast-build/package.nix
+++ pkgs/by-name/ni/nix-fast-build/package.nix
@@ -11,14 +11,14 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "nix-fast-build";
-  version = "1.3.0";
+  version = "1.4.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Mic92";
     repo = "nix-fast-build";
     tag = finalAttrs.version;
-    hash = "sha256-6X4BW+3C2nfkorMfe+tuoeYrdddxPtLqOJ1rZxuxPrc=";
+    hash = "sha256-sH/KWX8NO8iurnnkI7w8eWMkbnRBbvEIK9IW4LnR0qQ=";
   };
 
   build-system = [ python3Packages.setuptools ];
