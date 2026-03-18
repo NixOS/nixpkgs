263a45e72bacf2739ec62f1ab76d74406acd754f
Fabian Affolter <mail@fabian-affolter.ch>
2026-03-18 18:17:10 +0100

    python3Packages.cyclonedx-python-lib: modernize

--- pkgs/development/python-modules/cyclonedx-python-lib/default.nix
+++ pkgs/development/python-modules/cyclonedx-python-lib/default.nix
@@ -20,7 +20,7 @@
   xmldiff,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "cyclonedx-python-lib";
   version = "11.7.0";
   pyproject = true;
@@ -28,7 +28,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "CycloneDX";
     repo = "cyclonedx-python-lib";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-35JTr2he7sHOqG3Nd0UM9CZ4Q/HFv3UQsF6hxOKR/+k=";
   };
 
@@ -54,12 +54,8 @@ buildPythonPackage rec {
       jsonschema
       lxml
     ];
-    json-validation = [
-      jsonschema
-    ];
-    xml-validation = [
-      lxml
-    ];
+    json-validation = [ jsonschema ];
+    xml-validation = [ lxml ];
   };
 
   nativeCheckInputs = [
@@ -67,7 +63,7 @@ buildPythonPackage rec {
     pytestCheckHook
     xmldiff
   ]
-  ++ lib.concatAttrValues optional-dependencies;
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
 
   pythonImportsCheck = [ "cyclonedx" ];
 
@@ -93,8 +89,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python library for generating CycloneDX SBOMs";
     homepage = "https://github.com/CycloneDX/cyclonedx-python-lib";
-    changelog = "https://github.com/CycloneDX/cyclonedx-python-lib/releases/tag/${src.tag}";
-    license = with lib.licenses; [ asl20 ];
+    changelog = "https://github.com/CycloneDX/cyclonedx-python-lib/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
