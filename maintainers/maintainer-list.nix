8df805dc36aea74a4a796def0fbac6d0eb0d653e
Mootfrost <hello@mootfrost.dev>
2026-03-18 12:07:53 +0300

    maintainers: add mootfrost

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18086,6 +18086,12 @@
     githubId = 830082;
     name = "Nathan Moos";
   };
+  mootfrost = {
+    email = "hello@mootfrost.dev";
+    github = "mootfrost";
+    githubId = 75925945;
+    name = "Andrew Semeykin";
+  };
   moraxyc = {
     name = "Moraxyc Xu";
     email = "i@qaq.li";
