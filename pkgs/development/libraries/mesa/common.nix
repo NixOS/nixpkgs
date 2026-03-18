cc29bf866a5d2eddfbf83120b8b9d801e57548cb
K900 <me@0upti.me>
2026-03-18 19:41:04 +0300

    mesa: 26.0.2 -> 26.0.3
    Diff: https://gitlab.freedesktop.org/mesa/mesa/-/compare/mesa-26.0.2...mesa-26.0.3
    
    Changelog: https://docs.mesa3d.org/relnotes/26.0.3.html

--- pkgs/development/libraries/mesa/common.nix
+++ pkgs/development/libraries/mesa/common.nix
@@ -5,14 +5,14 @@
 # nix build .#legacyPackages.x86_64-darwin.mesa .#legacyPackages.aarch64-darwin.mesa
 rec {
   pname = "mesa";
-  version = "26.0.2";
+  version = "26.0.3";
 
   src = fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "mesa";
     repo = "mesa";
     rev = "mesa-${version}";
-    hash = "sha256-OaE1XM421C0rMep03wM7g4Ttwwn/8z5neLQI8LY9b2U=";
+    hash = "sha256-CTOnJKsx9hDtLhDnuJSGc/lBQ2ZcDm3/Iie7awusGho=";
   };
 
   meta = {
