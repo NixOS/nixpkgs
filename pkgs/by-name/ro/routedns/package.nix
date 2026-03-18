256805edb287d41d666604bfdbafbf08361611e1
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-18 15:25:12 +0000

    routedns: 0.1.140 -> 0.1.151

--- pkgs/by-name/ro/routedns/package.nix
+++ pkgs/by-name/ro/routedns/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "routedns";
-  version = "0.1.140";
+  version = "0.1.151";
 
   src = fetchFromGitHub {
     owner = "folbricht";
     repo = "routedns";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-byeCsm+W7H2MXAOdPFxZyALYh7C9WB/FO8Ee/y/sLV8=";
+    hash = "sha256-Uv2H3AllmFFuPb/S/aS3j7afyQYBpxSkcKRKTvY0Iyk=";
   };
 
-  vendorHash = "sha256-iUWiO8h55N+1c0EODLUxcIYEt2lT+2xyCAxa5f1ub3c=";
+  vendorHash = "sha256-a4KcKb75yWv7+1vIYCypS9nnrFJ3zogXIPzUVVA7AXs=";
 
   subPackages = [ "./cmd/routedns" ];
 
