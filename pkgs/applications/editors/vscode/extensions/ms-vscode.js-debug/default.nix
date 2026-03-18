4b7b1b8c2a0c57a99cbbf4a9298ecc9294189b3a
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-18 03:38:32 +0000

    vscode-extensions.ms-vscode.js-debug: 1.105.0 -> 1.112.0

--- pkgs/applications/editors/vscode/extensions/ms-vscode.js-debug/default.nix
+++ pkgs/applications/editors/vscode/extensions/ms-vscode.js-debug/default.nix
@@ -17,19 +17,19 @@ let
   vsix = stdenv.mkDerivation (finalAttrs: {
     name = "vscode-js-debug-${finalAttrs.version}.vsix";
     pname = "vscode-js-debug-vsix";
-    version = "1.105.0";
+    version = "1.112.0";
 
     src = fetchFromGitHub {
       owner = "microsoft";
       repo = "vscode-js-debug";
       tag = "v${finalAttrs.version}";
-      hash = "sha256-dYEINhJGrJFEq5422BEp3ups6vK0lpVW34GaYPMdfXk=";
+      hash = "sha256-pgDrGbx4E6r5lkdY49RyEe04YZYVXbjKAB+pY5w5w7U=";
     };
 
     npmDeps = fetchNpmDeps {
       name = "${finalAttrs.pname}-npm-deps";
       inherit (finalAttrs) src;
-      hash = "sha256-bBy0u6NaOAkX6vRJrRYYWUxCG6HM3h0PrzN6tZj5pVY=";
+      hash = "sha256-e+23PCPPQeHKxIT0nFEPumg2TvtNtpzil3XS5njHR9g=";
     };
     makeCacheWritable = true;
 
