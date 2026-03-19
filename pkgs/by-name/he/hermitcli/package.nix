c92e36ac7f691dc06cdb656f5eb7422c16b0b731
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-19 07:29:43 +0000

    hermitcli: 0.50.0 -> 0.50.1

--- pkgs/by-name/he/hermitcli/package.nix
+++ pkgs/by-name/he/hermitcli/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "hermit";
-  version = "0.50.0";
+  version = "0.50.1";
 
   src = fetchFromGitHub {
     rev = "v${finalAttrs.version}";
     owner = "cashapp";
     repo = "hermit";
-    hash = "sha256-2deJGcMZgZIA55Da/7W4y9ib73elQs+2Df/jf62N0EE=";
+    hash = "sha256-XxFeRcit8RcEjbLAT3SsWZrir8/AiDJIz9bzm9E5lUU=";
   };
 
   vendorHash = "sha256-2sNtok5J1kBvJZ0I1FOq1ZP54TsZbzqu/M3v1nA12m8=";
