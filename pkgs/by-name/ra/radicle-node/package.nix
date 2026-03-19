0ec5820e3a65d519e23beef7b0bf684a195c9d62
Defelo <mail@defelo.de>
2026-03-19 19:39:38 +0000

    radicle-node: 1.7.0 -> 1.7.1
    Changelog: https://app.radicle.xyz/nodes/seed.radicle.xyz/rad:z3gqcJUoA1n9HaHKufZs5FCSGazv5/tree/CHANGELOG.md

--- pkgs/by-name/ra/radicle-node/package.nix
+++ pkgs/by-name/ra/radicle-node/package.nix
@@ -15,9 +15,9 @@
   xdg-utils,
   versionCheckHook,
 
-  version ? "1.7.0",
-  srcHash ? "sha256-8/+ximoCPfNQJ/1APb0d5Z8kfWTI3SoN0RX9qrLP52o=",
-  cargoHash ? "sha256-xo8bH7f5MrUzoLat67rxiWMA24AnCngUkWWt402XAMQ=",
+  version ? "1.7.1",
+  srcHash ? "sha256-MPanUDVKol7mWVJDrGoGUkKqmcje+MsiK0WfqXQ27iI=",
+  cargoHash ? "sha256-Lru4ps9FYi03NVtRLtwZX9jhozAvBDsJ72ihdIpQcQ8=",
   updateScript ? ./update.sh,
 }:
 
