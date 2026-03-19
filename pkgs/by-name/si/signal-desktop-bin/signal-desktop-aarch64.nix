0fb6a56fb0233876337102293b7f12e1b381ac98
Gliczy <129636582+Gliczy@users.noreply.github.com>
2026-03-19 16:29:27 +0100

    signal-desktop-bin: 7.90.0 -> 8.5.0

--- pkgs/by-name/si/signal-desktop-bin/signal-desktop-aarch64.nix
+++ pkgs/by-name/si/signal-desktop-bin/signal-desktop-aarch64.nix
@@ -1,7 +1,7 @@
 { callPackage, commandLineArgs }:
 callPackage ./generic.nix { inherit commandLineArgs; } {
   pname = "signal-desktop-bin";
-  version = "7.90.0";
+  version = "8.5.0";
 
   libdir = "usr/lib64/signal-desktop";
   bindir = "usr/bin";
@@ -10,6 +10,6 @@ callPackage ./generic.nix { inherit commandLineArgs; } {
     bsdtar -xf $downloadedFile -C "$out"
   '';
 
-  url = "https://download.copr.fedorainfracloud.org/results/useidel/signal-desktop/fedora-42-aarch64/10149422-signal-desktop/signal-desktop-7.90.0-1.fc42.aarch64.rpm";
-  hash = "sha256-+6aIB/UF7oZ7I4MtDsYCOhQxiaTpIyXGZ5WLdJto7Us=";
+  url = "https://download.copr.fedorainfracloud.org/results/useidel/signal-desktop/fedora-42-aarch64/10287130-signal-desktop/signal-desktop-8.5.0-1.fc42.aarch64.rpm";
+  hash = "sha256-FPdv91o49YE5F0SCrvwyZPmoQ8Txc5GMdEk5mODSUSs=";
 }
