c855ed7eac5a8e61efeca77ad2765c434e2f041e
Hythera <87016780+Hythera@users.noreply.github.com>
2026-03-19 22:12:18 +0100

    libipt: 2.1.2 -> 2.2

--- pkgs/by-name/li/libipt/package.nix
+++ pkgs/by-name/li/libipt/package.nix
@@ -2,29 +2,20 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   cmake,
   freebsd,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libipt";
-  version = "2.1.2";
+  version = "2.2";
 
   src = fetchFromGitHub {
     owner = "intel";
     repo = "libipt";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-rO2Mf2/BfKlPh1wHe0qTuyQAyqpSB/j3Q+JWpNDyNm0=";
+    sha256 = "sha256-hSC00GbVllJStgt9iA9WT54U8NQRtgJHuyZyb5ougc8=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "libipt-fix-cmake-4.patch";
-      url = "https://github.com/intel/libipt/commit/fa7d42de25be526da532284cc8b771fdeb384f81.patch";
-      hash = "sha256-/jTyoGyKw29Nu27bAXmStpjOdTeGdQYpEX2rb29vSSQ=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake ];
   buildInputs = lib.optional stdenv.hostPlatform.isFreeBSD freebsd.libstdthreads;
