8d5bba8bd72a544229b2067b7c0d10aa87977bf0
Defelo <mail@defelo.de>
2026-03-19 19:40:17 +0000

    radicle-node-unstable: 1.7.0 -> 1.7.1
    Changelog: https://app.radicle.xyz/nodes/seed.radicle.xyz/rad:z3gqcJUoA1n9HaHKufZs5FCSGazv5/tree/CHANGELOG.md

--- pkgs/by-name/ra/radicle-node/unstable.nix
+++ pkgs/by-name/ra/radicle-node/unstable.nix
@@ -1,8 +1,8 @@
 { radicle-node }:
 
 radicle-node.override {
-  version = "1.7.0";
-  srcHash = "sha256-8/+ximoCPfNQJ/1APb0d5Z8kfWTI3SoN0RX9qrLP52o=";
-  cargoHash = "sha256-xo8bH7f5MrUzoLat67rxiWMA24AnCngUkWWt402XAMQ=";
+  version = "1.7.1";
+  srcHash = "sha256-MPanUDVKol7mWVJDrGoGUkKqmcje+MsiK0WfqXQ27iI=";
+  cargoHash = "sha256-Lru4ps9FYi03NVtRLtwZX9jhozAvBDsJ72ihdIpQcQ8=";
   updateScript = ./update-unstable.sh;
 }
