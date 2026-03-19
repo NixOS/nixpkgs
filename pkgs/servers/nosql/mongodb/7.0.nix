f4d697f1cd482e183dcb99cf6722b0c85d684855
qubitnano <146656568+qubitnano@users.noreply.github.com>
2026-03-18 20:32:10 -0400

    mongodb-7_0: 7.0.30 -> 7.0.31
    https://www.mongodb.com/docs/manual/release-notes/7.0-changelog/
    
    Fixes: CVE-2026-4147
    Fixes: CVE-2026-4148

--- pkgs/servers/nosql/mongodb/7.0.nix
+++ pkgs/servers/nosql/mongodb/7.0.nix
@@ -21,8 +21,8 @@ let
 in
 buildMongoDB {
   inherit avxSupport;
-  version = "7.0.30";
-  hash = "sha256-z0stPphy9EliDkanlDCHJ+ck3p1gUTMQ83uOW7kvlmI=";
+  version = "7.0.31";
+  hash = "sha256-Vk/XsnYut0Hfad/X6LZw6gJX1NHc4/6XT8y1KehpLMk=";
   patches = [
     # ModuleNotFoundError: No module named 'mongo_tooling_metrics':
     # NameError: name 'SConsToolingMetrics' is not defined:
