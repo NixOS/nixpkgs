2d2775fe7622b46b62a37120af0f768fe4ad20fc
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-03-20 17:43:47 +0100

    splayer: pin electron to electron_39
    (cherry picked from commit 463a015cb72906b553e34fa783b36f1d61e13e81)

--- pkgs/by-name/sp/splayer/package.nix
+++ pkgs/by-name/sp/splayer/package.nix
@@ -6,7 +6,7 @@
   pnpmConfigHook,
   pnpm_10,
   nodejs,
-  electron,
+  electron_39,
   rustPlatform,
   cargo,
   rustc,
@@ -19,6 +19,9 @@
   nix-update-script,
   removeReferencesTo,
 }:
+let
+  electron = electron_39;
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "splayer";
   version = "3.0.0";
