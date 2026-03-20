d037fb9241a9991082594eb816c13b67fee53ef2
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2026-03-21 07:42:29 +1100

    python3Packages.escapism: fix build, use setuptools

--- pkgs/development/python-modules/escapism/default.nix
+++ pkgs/development/python-modules/escapism/default.nix
@@ -2,17 +2,24 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
+  setuptools-scm,
 }:
 
 buildPythonPackage rec {
   pname = "escapism";
   version = "1.1.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-rdEw5IqFuxquo+dPsDH1AzxwVa7bOaMmX5I9X0DD+XQ=";
   };
+
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
 
   # No tests distributed
   doCheck = false;
