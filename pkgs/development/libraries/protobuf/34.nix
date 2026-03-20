7b59523f6e806ea8148f2b17366a19304a0426ef
Gaetan Lepage <gaetan@glepage.com>
2026-03-20 18:00:01 +0000

    protobuf: 34.0 -> 34.1
    Diff: https://github.com/protocolbuffers/protobuf/compare/v34.0...v34.1
    
    Changelog:
    https://github.com/protocolbuffers/protobuf/releases/tag/v34.1

--- pkgs/development/libraries/protobuf/34.nix
+++ pkgs/development/libraries/protobuf/34.nix
@@ -2,8 +2,8 @@
 
 callPackage ./generic.nix (
   {
-    version = "34.0";
-    hash = "sha256-qtSP3I7RA7F6tKe+VfGd+oNXMT5HZ/Xl7u4vkefNxX4=";
+    version = "34.1";
+    hash = "sha256-PaIVJ8NtgnrqowbKLkX+uprsQjuxDch9AUxX4YBBNh4=";
   }
   // args
 )
