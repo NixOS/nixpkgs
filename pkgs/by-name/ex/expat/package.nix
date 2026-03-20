331922862f0c2f416a65fe0509fe69dfd474addf
Vladimír Čunát <v@cunat.cz>
2026-03-20 17:51:26 +0100

    expat: 2.7.4 -> 2.7.5
    https://github.com/libexpat/libexpat/blob/R_2_7_5/expat/Changes
    Fixes CVE-2026-32776 CVE-2026-32777 CVE-2026-32778
    
    (cherry picked from commit bae4040734f48b1059b22b36487cd82b2e5bf08f)

--- pkgs/by-name/ex/expat/package.nix
+++ pkgs/by-name/ex/expat/package.nix
@@ -18,7 +18,7 @@
 # files.
 
 let
-  version = "2.7.4";
+  version = "2.7.5";
   tag = "R_${lib.replaceStrings [ "." ] [ "_" ] version}";
 in
 stdenv.mkDerivation (finalAttrs: {
@@ -29,7 +29,7 @@ stdenv.mkDerivation (finalAttrs: {
     url =
       with finalAttrs;
       "https://github.com/libexpat/libexpat/releases/download/${tag}/${pname}-${version}.tar.xz";
-    hash = "sha256-npyrtFfB4J3pHbJwbYNlZFeSY46zvh+U27IUkwEIasA=";
+    hash = "sha256-EDLf70/xf3BGSCfaooNpsg9lhNEIvDbxerFnbh7dL5E=";
   };
 
   strictDeps = true;
