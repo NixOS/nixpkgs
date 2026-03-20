a160c98b862bddcc659045e0b7e6bdc831fc139e
Harinn <prinn.dev@pm.me>
2026-03-21 00:25:58 +0700

    gotenberg: 8.27.0 -> 8.28.0

--- pkgs/by-name/go/gotenberg/package.nix
+++ pkgs/by-name/go/gotenberg/package.nix
@@ -24,7 +24,7 @@ let
 in
 buildGo126Module (finalAttrs: {
   pname = "gotenberg";
-  version = "8.27.0";
+  version = "8.28.0";
 
   outputs = [
     "out"
@@ -35,10 +35,10 @@ buildGo126Module (finalAttrs: {
     owner = "gotenberg";
     repo = "gotenberg";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-TLfIsvxKrlqNTJtdnASlGCA1XrOx7huMJ11aohVyuKI=";
+    hash = "sha256-xiST0EtoRROROwlzONHMdM8ewu6PZR77sjdAbwMbEfo=";
   };
 
-  vendorHash = "sha256-AzaN0xpQWw+Nfw22G7xgww8UsgpTIHpTPK3Bicf6gMY=";
+  vendorHash = "sha256-FV6C2SUPyny7zcCR7dzQ1XlHeDBx1q87fPuNuvNc+q0=";
 
   postPatch = ''
     find ./pkg -name '*_test.go' -exec sed -i -e 's#/tests#${finalAttrs.src}#g' {} \;
