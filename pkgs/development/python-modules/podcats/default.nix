509366436f9dc5acfb9c93a5d40256501e3cfd39
Clément <git@drawbu.dev>
2026-03-20 10:58:11 -0700

    python3Packages.podcats: modernize
    - replace `rec` with `finalAttrs`
    - sort inputs
    - change repository owner to `jkbrzt` since the repository was
      redirecting

--- pkgs/development/python-modules/podcats/default.nix
+++ pkgs/development/python-modules/podcats/default.nix
@@ -1,23 +1,23 @@
 {
-  lib,
   buildPythonPackage,
   fetchFromGitHub,
-  setuptools,
   flask,
   humanize,
+  lib,
   mutagen,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "podcats";
   version = "0.6.3";
   pyproject = true;
 
   src = fetchFromGitHub {
-    owner = "jakubroztocil";
+    owner = "jkbrzt";
     repo = "podcats";
-    tag = version;
-    sha256 = "sha256-1Jg9bR/3qMim3q5qVwUVbxeLNaXaCU6SplBUaRXeLpo=";
+    tag = finalAttrs.version;
+    hash = "sha256-1Jg9bR/3qMim3q5qVwUVbxeLNaXaCU6SplBUaRXeLpo=";
   };
 
   build-system = [ setuptools ];
@@ -28,11 +28,14 @@ buildPythonPackage rec {
     mutagen
   ];
 
+  pythonImportsCheck = [ "podcats" ];
+  doCheck = false;
+
   meta = {
-    description = "Application that generates RSS feeds for podcast episodes from local audio files";
+    description = "Generates RSS feeds for podcast episodes from local audio files";
     mainProgram = "podcats";
-    homepage = "https://github.com/jakubroztocil/podcats";
+    homepage = "https://github.com/jkbrzt/podcats";
     license = lib.licenses.bsd2;
     maintainers = with lib.maintainers; [ drawbu ];
   };
-}
+})
