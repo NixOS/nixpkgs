7b8897318ef5d2583ad75057c982e060acc04ff2
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-20 09:42:12 +0000

    python3Packages.victron-mqtt: 2026.3.3 -> 2026.3.4

--- pkgs/development/python-modules/victron-mqtt/default.nix
+++ pkgs/development/python-modules/victron-mqtt/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "victron-mqtt";
-  version = "2026.3.3";
+  version = "2026.3.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "tomer-w";
     repo = "victron_mqtt";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-KBmKHDCTBijt1GZGUuNKHLFpBB+Zfj1Vxio0qUS/Dto=";
+    hash = "sha256-yIM+oai1vzWAQkzFEZVLLF0KJvH4AxrJvw5/hsFQbS0=";
   };
 
   build-system = [
