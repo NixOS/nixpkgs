d4c44a75d0d98b9a198e462a01c26e6a8f363c75
shellhazard <shellhazard@tutanota.com>
2026-03-21 11:25:07 +0000

    fmd-server: 0.13.0 -> 0.14.0

--- pkgs/by-name/fm/fmd-server/package.nix
+++ pkgs/by-name/fm/fmd-server/package.nix
@@ -2,23 +2,69 @@
   lib,
   buildGoModule,
   fetchFromGitLab,
+  fetchPnpmDeps,
+  nodejs_22,
+  pnpm_10,
+  pnpmConfigHook,
+  stdenv,
   nix-update-script,
   versionCheckHook,
 }:
 let
-  version = "0.13.0";
+  version = "0.14.0";
+  nodejs = nodejs_22;
 in
-buildGoModule {
+buildGoModule (finalAttrs: {
   pname = "fmd-server";
   inherit version;
+
   src = fetchFromGitLab {
     owner = "fmd-foss";
     repo = "fmd-server";
     tag = "v${version}";
-    hash = "sha256-pIVsdoen45YWG/V8qW/DE/yet4o+8MqpVHgHh70Fty8=";
+    hash = "sha256-6dqLJDVQfwwNW40Wsu0Sb2kqtDLLJxQeSUcnFasLyN0=";
   };
 
-  vendorHash = "sha256-d8QP6k7vEX0jBcpIMZymgOFYyKqGhND4Xa+mANg172s=";
+  webui = stdenv.mkDerivation {
+    pname = "fmd-server-webui";
+    inherit (finalAttrs) version src;
+
+    sourceRoot = "${finalAttrs.src.name}/web";
+
+    pnpmDeps = fetchPnpmDeps {
+      inherit (finalAttrs) pname version src;
+      pnpm = pnpm_10;
+      fetcherVersion = 3;
+      sourceRoot = "${finalAttrs.src.name}/web";
+      hash = "sha256-8emvxvsP60RxV7F7dBIyexKW51u+sPl2+NO/tfH+OLc=";
+    };
+
+    nativeBuildInputs = [
+      nodejs
+      pnpmConfigHook
+      pnpm_10
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+      pnpm build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r dist/* $out
+      runHook postInstall
+    '';
+  };
+
+  vendorHash = "sha256-cFIg9mOSQbrYHW4kg4aTeTaF+gy1jNpAlg8qepb81Jc=";
+
+  preBuild = ''
+    mkdir -p web/dist/
+    cp -r ${finalAttrs.webui}/* web/dist/
+  '';
 
   nativeInstallCheckInputs = [ versionCheckHook ];
   versionCheckProgramArg = "version";
@@ -31,8 +77,11 @@ buildGoModule {
     homepage = "https://fmd-foss.org/";
     downloadPage = "https://gitlab.com/fmd-foss/fmd-server";
     license = lib.licenses.gpl3Plus;
-    maintainers = with lib.maintainers; [ j0hax ];
+    maintainers = with lib.maintainers; [
+      j0hax
+      shellhazard
+    ];
     teams = [ lib.teams.ngi ];
     mainProgram = "fmd-server";
   };
-}
+})
