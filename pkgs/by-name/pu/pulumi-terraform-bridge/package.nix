b247d72ade98dba8696ea7a62218949b68155499
nicoo <nicoo@debian.org>
2026-03-21 15:07:22 +0000

    pulumi-terraform-bridge: init at 3.135.0

---
+++ pkgs/by-name/pu/pulumi-terraform-bridge/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildGoModule,
+}:
+buildGoModule rec {
+  pname = "pulumi-terraform-bridge";
+  version = "3.135.0";
+
+  src = fetchFromGitHub {
+    owner = "pulumi";
+    repo = "pulumi-terraform-bridge";
+    tag = "v${version}";
+    hash = "sha256-MMWNIGQz0Mzs4irSV5AqFpx31Jp+liHthHrMsEb0qnE=";
+  };
+  vendorHash = "sha256-A5JU+TtkYk4Bt5Un+i/EiY1rBcmWOvfIVcYFEyjCPzY=";
+
+  excludedPackages = [
+    "tools/pulumi-hcl-lint" # build errors w/ “main module (...) does not contain package”
+  ];
+
+  checkFlags = [
+    "-skip=^Test(SchemaGeneration|RandomCreate$)"  # require Internet access
+    "-skip=^TestLogReplayProviderWithProgram$"  # segfaults?
+  ];
+
+  meta = {
+    description = "This bridge adapts any Terraform Provider built using the Terraform Plugin SDK for use with Pulumi.";
+    homepage = "https://github.com/pulumi/pulumi-terraform-bridge";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [
+      nicoo
+    ];
+  };
+}
