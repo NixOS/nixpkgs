05187de9d6bc19812d4e8ee4db8499f9b44a9bf0
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-21 05:34:02 +0000

    labwc: 0.9.3 -> 0.9.6

--- pkgs/by-name/la/labwc/package.nix
+++ pkgs/by-name/la/labwc/package.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "labwc";
-  version = "0.9.3";
+  version = "0.9.6";
 
   src = fetchFromGitHub {
     owner = "labwc";
     repo = "labwc";
     tag = finalAttrs.version;
-    hash = "sha256-C/u11vxHmWWb8/kl0tZTOgosAvbSNb3tCvp2r0Bcb+Q=";
+    hash = "sha256-3svQnSFdzKTfGLSjiNjswb4B5n0htQTltxhRmUbZD20=";
   };
 
   outputs = [
