d5089c5bf9ab340a32f77e35f70210124869a6ce
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-21 08:28:30 +0000

    frankenphp: 1.11.2 -> 1.12.1

--- pkgs/by-name/fr/frankenphp/package.nix
+++ pkgs/by-name/fr/frankenphp/package.nix
@@ -30,13 +30,13 @@ let
 in
 buildGoModule (finalAttrs: {
   pname = "frankenphp";
-  version = "1.11.2";
+  version = "1.12.1";
 
   src = fetchFromGitHub {
     owner = "php";
     repo = "frankenphp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-JzZXg/tkSZqLZn56RyLb8Q8SaeG/tHA8Sqxu99s5ks0=";
+    hash = "sha256-Bna8hp0wWlSbYR0GwGSl+ermFbmgSdgOqqYNqAr/XTc=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/caddy";
@@ -44,7 +44,7 @@ buildGoModule (finalAttrs: {
   # frankenphp requires C code that would be removed with `go mod tidy`
   # https://github.com/golang/go/issues/26366
   proxyVendor = true;
-  vendorHash = "sha256-9RHD2ZcolhgQO0UKxkqFjxXGAVijSYQxVt7s+/aXNIo=";
+  vendorHash = "sha256-aUILdXS6XNAnO3MIi+VBq3gqfSohhK3KHpMoSNKAtYs=";
 
   buildInputs = [
     phpUnwrapped
