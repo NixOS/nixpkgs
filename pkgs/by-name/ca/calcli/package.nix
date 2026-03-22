480c1ebd8308c56d6a5d2c0068520f1f596b2f9f
Siphcy <boxpinton@gmail.com>
2026-03-22 05:09:13 -0400

    calcli: init at 0.1.2

---
+++ pkgs/by-name/ca/calcli/package.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "calcli";
+  version = "0.1.2";
+
+  src = fetchFromGitHub {
+    owner = "Siphcy";
+    repo = "calcli";
+    rev = "v${version}";
+    hash = "sha256-WQHuOzSPrRU/tw1bYZj2JKLTqRyHdSJxtBB89OJ6Q6o=";
+  };
+
+  cargoHash = "sha256-XMpYNeu24cQ9dYpmNYwFqFZ54xx4WcYO2jDq0WpEiSQ=";
+
+  meta = with lib; {
+    description = "A lightweight TUI scientific calculator with Vi-style keybindings";
+    homepage = "https://github.com/Siphcy/calcli";
+    license = licenses.mit;
+    maintainers = [ Siphcy ];
+    mainProgram = "calcli";
+    platforms = platforms.all;
+  };
+}
