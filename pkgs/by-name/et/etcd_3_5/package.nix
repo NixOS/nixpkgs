e7992819cb0e0fd7727794c09ea94b3c9a99c637
Tom van Dijk <18gatenmaker6@gmail.com>
2026-03-22 14:42:27 +0100

    etcd_3_5: 3.5.27 -> 3.5.28
    (cherry picked from commit 324374f8c06e377c7790fbedd15c09e6a0ea7350)

--- pkgs/by-name/et/etcd_3_5/package.nix
+++ pkgs/by-name/et/etcd_3_5/package.nix
@@ -8,11 +8,11 @@
 }:
 
 let
-  version = "3.5.27";
-  etcdSrcHash = "sha256-ne0uKpS7KQL/Efi5sMdnD8PQl5phDivBEM1PrsSxVf4=";
-  etcdServerVendorHash = "sha256-6vu0ttbes5uN+m/tW/zuPZgyc1z+VcNpoJauykxBUUY=";
-  etcdUtlVendorHash = "sha256-Z8Rc19p5Sigg71/r9yURE6LNHqHjah3b/ZfxI0nH4qc=";
-  etcdCtlVendorHash = "sha256-uAo50jN69YKuW3NCRtc9HE8AnqKWtZQ+sJvwgw1BJj0=";
+  version = "3.5.28";
+  etcdSrcHash = "sha256-D4c+YfNQATW8/9m/5edPKtG9qQa8MxpbpDTCZY6cvA4=";
+  etcdServerVendorHash = "sha256-IiGoG/5HJBZXhWhluK1uQVOXRk2I4VDokT7EcjhEnWU=";
+  etcdUtlVendorHash = "sha256-mUzwpxAsUhXdK70BwtjWJqjzLpDnNFNXW6tHP81c72w=";
+  etcdCtlVendorHash = "sha256-Bn3mirDqs9enpNTlp65YWiQRKpc+B6wD33b0xkaqSKE=";
 
   src = fetchFromGitHub {
     owner = "etcd-io";
