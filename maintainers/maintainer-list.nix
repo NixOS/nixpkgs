874a531c3767e2de1272cc3c1480eb6648b96174
imcvampire <anhnq0@gmail.com>
2026-03-22 09:12:11 +0200

    maintainers: add imcvampire

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11393,6 +11393,11 @@
     githubId = 39416660;
     name = "Mladen Branković";
   };
+  imcvampire = {
+    github = "imcvampire";
+    githubId = 9426721;
+    name = "Quoc-Anh Nguyen";
+  };
   imgabe = {
     email = "gabrielpmonte@hotmail.com";
     github = "ImGabe";
