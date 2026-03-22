2d7acc0ce84190b44bae575d6e2f158c5d96748a
Tom Hunze <dev@thunze.de>
2026-03-22 11:39:22 +0100

    python3Packages.exiv2: use `finalAttrs`

--- pkgs/development/python-modules/exiv2/default.nix
+++ pkgs/development/python-modules/exiv2/default.nix
@@ -10,7 +10,7 @@
   toml,
   pytestCheckHook,
 }:
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "exiv2";
   version = "0.18.1";
   pyproject = true;
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "jim-easterbrook";
     repo = "python-exiv2";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-3r0qGsCkfe2sQuXiCipXzW0vF2JRg77L1IcOiLTPslM=";
   };
 
@@ -41,8 +41,8 @@ buildPythonPackage rec {
   meta = {
     description = "Low level Python interface to the Exiv2 C++ library";
     homepage = "https://github.com/jim-easterbrook/python-exiv2";
-    changelog = "https://github.com/jim-easterbrook/python-exiv2/blob/${src.tag}/CHANGELOG.txt";
+    changelog = "https://github.com/jim-easterbrook/python-exiv2/blob/${finalAttrs.src.tag}/CHANGELOG.txt";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ zebreus ];
   };
-}
+})
