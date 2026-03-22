35fc42abe1be1ecc2540b0a2d8bfb6cfd1f64412
Tom Hunze <dev@thunze.de>
2026-03-22 11:18:22 +0100

    python3Packages.filebytes: fix build with python 3.14
    Upstream PR: https://github.com/sashs/filebytes/pull/36

--- pkgs/development/python-modules/filebytes/default.nix
+++ pkgs/development/python-modules/filebytes/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  fetchpatch,
 }:
 
 buildPythonPackage rec {
@@ -13,6 +14,15 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "0h97i6h525hg401dvvaa5krxi184qpvldbdn0izmirvr9pvh4hkn";
   };
+
+  patches = [
+    # Upstream PR: https://github.com/sashs/filebytes/pull/36
+    (fetchpatch {
+      name = "python-3.14.patch";
+      url = "https://github.com/sashs/filebytes/commit/469058d50d4b7ff8da54b623a0a1aa972cd78dc6.patch";
+      hash = "sha256-VizYOqyJ3xpJIU4KKsYcz2DCurlfrWTgdsn84FVWD6w=";
+    })
+  ];
 
   meta = {
     homepage = "https://scoding.de/filebytes-introduction";
