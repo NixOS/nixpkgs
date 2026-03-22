5505e9a971f0dc9cf4e56f9fcf18b94841e1b09e
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-22 23:10:05 +0000

    stardust-xr-atmosphere: 0-unstable-2024-08-22 -> 0.51.1-unstable-2026-03-22

--- pkgs/by-name/st/stardust-xr-atmosphere/package.nix
+++ pkgs/by-name/st/stardust-xr-atmosphere/package.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage {
   pname = "stardust-xr-atmosphere";
-  version = "0-unstable-2024-08-22";
+  version = "0.51.1-unstable-2026-03-22";
 
   src = fetchFromGitHub {
     owner = "stardustxr";
     repo = "atmosphere";
-    rev = "0c8bfb91e8ca32a4895f858067334ed265517309";
-    hash = "sha256-pk1+kkPV6fx+7Xz9hKFFVw402iztcvNC31zVCc3hfTY=";
+    rev = "81927a7057f4f5aa1baab8dbb498e03c71e81eb5";
+    hash = "sha256-QsurrxkJ3bqmsHIe4vlOwvp19yA0Ak09c9bBT2BmMiQ=";
   };
 
-  cargoHash = "sha256-eQjRbavmUW2iw0OEC/DPk2FflTc4QCn0K/c4Og+sGW4=";
+  cargoHash = "sha256-y9zg/69wo0U9opN3D9laRaJa1zlsZJZgloYOm9qm7H4=";
 
   passthru = {
     tests.versionTest = testers.testVersion {
