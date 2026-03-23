0479ff6f0d3106b1ec32ebf2b71ad70633d71849
Jamie Magee <jamie.magee@gmail.com>
2026-03-22 19:08:25 -0700

    python3Packages.pyliebherrhomeapi: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14094,6 +14094,8 @@ self: super: with self; {
 
   pylibsrtp = callPackage ../development/python-modules/pylibsrtp { };
 
+  pyliebherrhomeapi = callPackage ../development/python-modules/pyliebherrhomeapi { };
+
   pylink-square = callPackage ../development/python-modules/pylink-square { };
 
   pylint = callPackage ../development/python-modules/pylint { };
