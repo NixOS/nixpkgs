a91a30e7f4f9868c6c9c2fc04386df888d4db6e9
Ulysses Zhan <UlyssesZhan@gmail.com>
2026-03-22 22:19:47 -0700

    pianobooster: 1.0.0 -> 1.0.0-unstable-2023-01-22; add ulysseszhan as maintainer

--- pkgs/by-name/pi/pianobooster/package.nix
+++ pkgs/by-name/pi/pianobooster/package.nix
@@ -1,79 +1,100 @@
 {
   lib,
   stdenv,
+  replaceVars,
   fetchFromGitHub,
   cmake,
   pkg-config,
   alsa-lib,
   ftgl,
+  freetype,
   libGLU,
   rtmidi,
   libjack2,
   fluidsynth,
   soundfont-fluid,
+  soundFonts ? [ soundfont-fluid ],
+  dejavu_fonts,
   unzip,
-  libsForQt5,
+  qt6,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "pianobooster";
-  version = "1.0.0";
+  version = "1.0.0-unstable-2023-01-22"; # use unstable version for Qt 6 support
 
   src = fetchFromGitHub {
     owner = "pianobooster";
     repo = "PianoBooster";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-1WOlAm/HXSL6QK0Kd1mnFEZxxpMseTG+6WzgMNWt+RA=";
+    rev = "6dafdcbdfc5d35d12cecb051c30632d0f5be5806";
+    hash = "sha256-ZOgTgN1CWiPHe9of8NjgoPP5xzAY1k2v9xSAY1m2Bu0=";
   };
 
   patches = [
-    # Bump cmake_minimum_required to support CMake 4
-    # https://github.com/pianobooster/PianoBooster/pull/349
-    ./bump-cmake-minimum-required-version.patch
+    # more sensible soundfont discovery
+    (replaceVars ./soundfont.patch {
+      possibleSoundFontFolders = lib.concatMapStringsSep ", " (
+        p: "\"${p}/share/soundfonts\", \"${p}/share/sounds/sf2\""
+      ) soundFonts;
+    })
   ];
 
   postPatch = ''
-    substituteInPlace src/Settings.cpp src/GuiMidiSetupDialog.cpp \
-      --replace "/usr/share/soundfonts" "${soundfont-fluid}/share/soundfonts" \
-      --replace "FluidR3_GM.sf2" "FluidR3_GM2-2.sf2"
+    substituteInPlace src/CMakeLists.txt --replace-fail "ADD_DEFINITIONS(-std=c++11)" ""
   '';
 
   nativeBuildInputs = [
     cmake
     pkg-config
-  ]
-  ++ (with libsForQt5; [
-    qttools
-    wrapQtAppsHook
-  ]);
+    qt6.qttools
+    qt6.wrapQtAppsHook
+  ];
 
   buildInputs = [
-    alsa-lib
     ftgl
+    freetype # required by ftgl; somehow qt provides freetype on linux but not on darwin
     libGLU
-    libsForQt5.qtbase
+    qt6.qtbase
+    qt6.qt5compat
     rtmidi
     libjack2
     fluidsynth
-  ];
+  ]
+  ++ lib.optional stdenv.hostPlatform.isLinux alsa-lib;
 
   cmakeFlags = [
-    "-DOpenGL_GL_PREFERENCE=GLVND"
-    "-DUSE_JACK=ON"
+    (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.5")
+    (lib.cmakeFeature "OpenGL_GL_PREFERENCE" "GLVND")
+    (lib.cmakeFeature "QT_PACKAGE_NAME" "Qt6")
+    (lib.cmakeBool "WITH_MAN" true)
+    (lib.cmakeBool "USE_BUNDLED_RTMIDI" false)
+    (lib.cmakeBool "USE_JACK" true)
+    (lib.cmakeBool "USE_SYSTEM_FONT" true)
+    (lib.cmakeFeature "MACOSX_BUNDLE_GUI_IDENTIFIER" "pianobooster")
+    (lib.cmakeFeature "MACOSX_BUNDLE_SHORT_VERSION_STRING" finalAttrs.version)
+    (lib.cmakeFeature "MACOSX_BUNDLE_BUNDLE_VERSION" finalAttrs.version)
   ];
 
   postInstall = ''
-    qtWrapperArgs+=(
-      --prefix PATH : "${lib.makeBinPath [ unzip ]}"
-    )
+    qtWrapperArgs+=(--prefix PATH : "${lib.makeBinPath [ unzip ]}")
+  ''
+  + lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
+    mkdir -p $out/share/games/pianobooster/fonts
+    # use readlink to avoid referencing dejavu-fonts but dejavu-fonts-minimal instead
+    ln -s $(readlink -f ${dejavu_fonts}/share/fonts/truetype/DejaVuSans.ttf) $out/share/games/pianobooster/fonts/DejaVuSans.ttf
+  ''
+  + lib.optionalString stdenv.hostPlatform.isDarwin ''
+    mkdir -p $out/{Applications,bin}
+    cp -r build/pianobooster.app $out/Applications
+    ln -s $out/Applications/pianobooster.app/Contents/MacOS/pianobooster $out/bin/pianobooster
   '';
 
   meta = {
     description = "MIDI file player that teaches you how to play the piano";
     mainProgram = "pianobooster";
-    homepage = "https://github.com/pianobooster/PianoBooster";
+    homepage = "https://www.pianobooster.org";
     license = lib.licenses.gpl3Plus;
-    platforms = lib.platforms.linux;
-    maintainers = [ ];
+    platforms = lib.platforms.unix;
+    maintainers = with lib.maintainers; [ ulysseszhan ];
   };
 })
