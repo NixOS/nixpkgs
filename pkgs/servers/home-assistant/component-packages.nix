b93e71b44fe5d2b35139ac7afbe9cb391c7e682b
Jamie Magee <jamie.magee@gmail.com>
2026-03-23 10:03:14 -0700

    home-assistant: update component-packages.nix

--- pkgs/servers/home-assistant/component-packages.nix
+++ pkgs/servers/home-assistant/component-packages.nix
@@ -5156,7 +5156,8 @@
       ];
     "redgtech" =
       ps: with ps; [
-      ]; # missing inputs: redgtech-api
+        redgtech-api
+      ];
     "refoss" =
       ps: with ps; [
         refoss-ha
@@ -7976,6 +7977,7 @@
     "recorder"
     "recovery_mode"
     "reddit"
+    "redgtech"
     "refoss"
     "rehlko"
     "remember_the_milk"
