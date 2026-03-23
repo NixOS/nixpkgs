9be0e0aa5229a93094680f9c0695591ca42a3a1a
phanirithvij <phanirithvij2000@gmail.com>
2026-03-24 00:49:18 +0530

    maintainers: drop khushraj
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/dp/dprint/package.nix
+++ pkgs/by-name/dp/dprint/package.nix
@@ -92,7 +92,6 @@ rustPlatform.buildRustPackage (finalAttrs: {
     homepage = "https://dprint.dev";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [
-      khushraj
       kachick
       phanirithvij
     ];
