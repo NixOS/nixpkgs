ea228cee0c333cea7f414ffb3a82215259ec2eda
Tom Hunze <dev@thunze.de>
2026-03-23 14:47:14 +0100

    python3Packages.bson: fix build with python 3.14
    Upstream PR: https://github.com/py-bson/bson/pull/140

--- pkgs/development/python-modules/bson/default.nix
+++ pkgs/development/python-modules/bson/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
   python-dateutil,
   six,
   pytestCheckHook,
@@ -18,6 +19,16 @@ buildPythonPackage rec {
     tag = version;
     hash = "sha256-mirRpo27RoOBlwxVOKnHaDIzJOErp7c2VxCOunUm/u4=";
   };
+
+  patches = [
+    # Upstream PR: https://github.com/py-bson/bson/pull/140
+    (fetchpatch {
+      name = "python-3.14.patch";
+      url = "https://github.com/py-bson/bson/commit/4e6b4c206f7204034ef74bff8ae84a95d76d1684.patch";
+      includes = [ "setup.py" ];
+      hash = "sha256-JOmW/KMqzFdXKH4TMR/PG1YU3SvLTBc3L3E9kXag3bQ=";
+    })
+  ];
 
   postPatch = ''
     find . -type f -name '*.py' -exec sed -i 's|assertEquals|assertEqual|g' {} +
