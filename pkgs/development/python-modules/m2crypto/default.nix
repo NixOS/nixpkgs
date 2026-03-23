178ffb8c1d1ea6c4020744a9709aa68b882399ed
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-23 00:24:21 +0000

    python3Packages.m2crypto: 0.47.0 -> 0.48.0
    (cherry picked from commit c58e7c09aa57787df8a3278140c3c8b0df2915ec)

--- pkgs/development/python-modules/m2crypto/default.nix
+++ pkgs/development/python-modules/m2crypto/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "m2crypto";
-  version = "0.47.0";
+  version = "0.48.0";
   pyproject = true;
 
   disabled = pythonOlder "3.7";
@@ -22,7 +22,7 @@ buildPythonPackage rec {
     owner = "m2crypto";
     repo = "m2crypto";
     tag = version;
-    hash = "sha256-yOpejD2lYbv8JrTXxXOwjAgftb9xojAYgC3iKu3bDSQ=";
+    hash = "sha256-Ya1og1x3EPbHkrY74tkdkMOJreS3x8x/1oVfwcpAEOU=";
   };
 
   # https://lists.sr.ht/~mcepl/m2crypto/%3CCAPhw1+Hg6+OJZoqt1O6aezxnTUFmfFTMzDwkD2bJ74jnmygqrg@mail.gmail.com%3E
