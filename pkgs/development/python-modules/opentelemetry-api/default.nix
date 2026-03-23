0caa73c6579aee198811eeafe65e955cc83ac268
Gaetan Lepage <gaetan@glepage.com>
2026-03-23 21:17:13 +0000

    python3Packages.opentelemetry-api: cleanup

--- pkgs/development/python-modules/opentelemetry-api/default.nix
+++ pkgs/development/python-modules/opentelemetry-api/default.nix
@@ -2,70 +2,76 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  deprecated,
+
+  # build-system
   hatchling,
+
+  # dependencies
+  deprecated,
   importlib-metadata,
   typing-extensions,
+
+  # tests
   opentelemetry-test-utils,
   pytestCheckHook,
+
+  # passthru
   writeScript,
+  opentelemetry-api,
 }:
 
-let
-  self = buildPythonPackage rec {
-    pname = "opentelemetry-api";
-    version = "1.40.0";
-    pyproject = true;
+buildPythonPackage (finalAttrs: {
+  pname = "opentelemetry-api";
+  version = "1.40.0";
+  pyproject = true;
 
-    # to avoid breakage, every package in opentelemetry-python must inherit this version, src, and meta
-    src = fetchFromGitHub {
-      owner = "open-telemetry";
-      repo = "opentelemetry-python";
-      tag = "v${version}";
-      hash = "sha256-1KVy9s+zjlB4w7E45PMCWRxPus24bgBmmM3k2R9d+Jg=";
-    };
-
-    sourceRoot = "${src.name}/opentelemetry-api";
-
-    build-system = [ hatchling ];
-
-    dependencies = [
-      deprecated
-      importlib-metadata
-      typing-extensions
-    ];
-
-    pythonRelaxDeps = [ "importlib-metadata" ];
-
-    nativeCheckInputs = [
-      opentelemetry-test-utils
-      pytestCheckHook
-    ];
-
-    pythonImportsCheck = [ "opentelemetry" ];
-
-    doCheck = false;
-
-    passthru = {
-      updateScript = writeScript "update.sh" ''
-        #!/usr/bin/env nix-shell
-        #!nix-shell -i bash -p nix-update
-
-        set -eu -o pipefail
-        nix-update --version-regex 'v(.*)' python3Packages.opentelemetry-api
-        nix-update python3Packages.opentelemetry-instrumentation
-      '';
-      # Enable tests via passthru to avoid cyclic dependency with opentelemetry-test-utils.
-      tests.${self.pname} = self.overridePythonAttrs { doCheck = true; };
-    };
-
-    meta = {
-      homepage = "https://github.com/open-telemetry/opentelemetry-python/tree/main/opentelemetry-api";
-      description = "OpenTelemetry Python API";
-      changelog = "https://github.com/open-telemetry/opentelemetry-python/releases/tag/${src.tag}";
-      license = lib.licenses.asl20;
-      maintainers = [ lib.maintainers.natsukium ];
-    };
+  # to avoid breakage, every package in opentelemetry-python must inherit this version, src, and meta
+  src = fetchFromGitHub {
+    owner = "open-telemetry";
+    repo = "opentelemetry-python";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-1KVy9s+zjlB4w7E45PMCWRxPus24bgBmmM3k2R9d+Jg=";
   };
-in
-self
+
+  sourceRoot = "${finalAttrs.src.name}/opentelemetry-api";
+
+  build-system = [ hatchling ];
+
+  dependencies = [
+    deprecated
+    importlib-metadata
+    typing-extensions
+  ];
+
+  pythonRelaxDeps = [ "importlib-metadata" ];
+
+  nativeCheckInputs = [
+    opentelemetry-test-utils
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [ "opentelemetry" ];
+
+  doCheck = false;
+
+  passthru = {
+    updateScript = writeScript "update.sh" ''
+      #!/usr/bin/env nix-shell
+      #!nix-shell -i bash -p nix-update
+
+      set -eu -o pipefail
+      nix-update --version-regex 'v(.*)' python3Packages.opentelemetry-api
+      nix-update python3Packages.opentelemetry-instrumentation
+    '';
+    # Enable tests via passthru to avoid cyclic dependency with opentelemetry-test-utils.
+    tests.pytest = opentelemetry-api.overridePythonAttrs { doCheck = true; };
+  };
+
+  meta = {
+    homepage = "https://github.com/open-telemetry/opentelemetry-python/tree/main/opentelemetry-api";
+    description = "OpenTelemetry Python API";
+    changelog = "https://github.com/open-telemetry/opentelemetry-python/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.asl20;
+    maintainers = [ lib.maintainers.natsukium ];
+  };
+})
