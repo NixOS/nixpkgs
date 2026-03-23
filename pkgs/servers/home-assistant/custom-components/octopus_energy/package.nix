863812937042723db4e26eb994f1803120742fed
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-23 21:01:14 +0000

    home-assistant-custom-components.octopus_energy: 18.1.1 -> 18.1.2

--- pkgs/servers/home-assistant/custom-components/octopus_energy/package.nix
+++ pkgs/servers/home-assistant/custom-components/octopus_energy/package.nix
@@ -11,13 +11,13 @@
 buildHomeAssistantComponent rec {
   owner = "BottlecapDave";
   domain = "octopus_energy";
-  version = "18.1.1";
+  version = "18.1.2";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "HomeAssistant-OctopusEnergy";
     tag = "v${version}";
-    hash = "sha256-wqhll0K5WKZDGWftaYLVd6uDS4w8mKU4yO4a4FLqseA=";
+    hash = "sha256-+aMMloqNsDMHt5zSfv9EA9QoGG8VCJv5Aw0ctoWcs5Q=";
   };
 
   dependencies = [ pydantic ];
