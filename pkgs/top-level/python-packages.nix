e4809bd2c267622ce4aa920a62033f1bc29fd145
Jamie Magee <jamie.magee@gmail.com>
2026-03-23 10:02:09 -0700

    python3Packages.redgtech-api: init at 0.1.38

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16590,6 +16590,8 @@ self: super: with self; {
 
   redbaron = callPackage ../development/python-modules/redbaron { };
 
+  redgtech-api = callPackage ../development/python-modules/redgtech-api { };
+
   redis = callPackage ../development/python-modules/redis { };
 
   redis-om = callPackage ../development/python-modules/redis-om { };
