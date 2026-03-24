873b65794e9ccd664b4d7eca8b13702c8d781b2b
Maciej Krüger <mkg20001@gmail.com>
2026-03-24 16:19:45 +0100

    ci: xzar install, upload to xzar

---
+++ xzar-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+nix-shell -p rustc cargo gcc pkg-config openssl --run "cargo install --git https://github.com/mkg20001/xzar xzar-client --bin xzar"
+~/.cargo/bin/xzar config add-server planai https://xzar.plan.ai "$XZAR_TOKEN"
+
+cp -L /etc/nix/nix.conf /etc/nix/nix.conf.
+rm /etc/nix/nix.conf
+mv /etc/nix/nix.conf. /etc/nix/nix.conf
+
+echo "substituters = https://cache.nixos.org/ https://xzar.plan.ai" | tee -a /etc/nix/nix.conf
+echo "trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= xzar.plan.ai:KUE66pjr6UX5HHCn9kedN1DJ2J5nSlBrKmE7tUjXewE=" | tee -a /etc/nix/nix.conf
+
+systemctl restart nix-daemon
