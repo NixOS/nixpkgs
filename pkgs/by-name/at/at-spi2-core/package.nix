085fadf23c5f6fc2a7bce1b8459d108a6f213a38
Bobby Rong <rjl931189261@126.com>
2026-03-24 12:10:40 +0000

    at-spi2-core: 2.58.3 → 2.60.0
    https://gitlab.gnome.org/GNOME/at-spi2-core/-/compare/2.58.3...2.60.0

--- pkgs/by-name/at/at-spi2-core/package.nix
+++ pkgs/by-name/at/at-spi2-core/package.nix
@@ -29,7 +29,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "at-spi2-core";
-  version = "2.58.3";
+  version = "2.60.0";
 
   outputs = [
     "out"
@@ -39,7 +39,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/at-spi2-core/${lib.versions.majorMinor finalAttrs.version}/at-spi2-core-${finalAttrs.version}.tar.xz";
-    hash = "sha256-sPq+psl0LtqMnGdfm4wdG6u6HagtoD6hEDcQIzcXwbA=";
+    hash = "sha256-gOUMGpfY/WYKP62wLKNYdt+IHCZtPWEI/FtMETYUy5k=";
   };
 
   nativeBuildInputs = [
