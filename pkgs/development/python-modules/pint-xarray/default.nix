f9fc7815525db4a84531dee260eb9efc0bc81783
Doron Behar <doron.behar@gmail.com>
2026-03-24 10:45:33 +0200

    python3Packages.pint-xarray: 0.6.0 -> 0.6.1
    Diff: https://github.com/xarray-contrib/pint-xarray/compare/v0.6.0...v0.6.1
    
    Changelog: https://github.com/xarray-contrib/pint-xarray/blob/v0.6.1/docs/whats-new.rst

--- pkgs/development/python-modules/pint-xarray/default.nix
+++ pkgs/development/python-modules/pint-xarray/default.nix
@@ -2,7 +2,6 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  fetchpatch2,
   setuptools,
   setuptools-scm,
   numpy,
@@ -14,34 +13,19 @@
 
 buildPythonPackage rec {
   pname = "pint-xarray";
-  version = "0.6.0";
+  version = "0.6.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "xarray-contrib";
     repo = "pint-xarray";
     tag = "v${version}";
-    hash = "sha256-mMAd315ijNseXbPc6UydoLcemXWlVOUAbFYLWRD6ZLA=";
+    hash = "sha256-t2I17dyl/XoO7NBvEyz7TRZkG/uQKPDHUUCG+bQXdOo=";
   };
 
   build-system = [
     setuptools
     setuptools-scm
-  ];
-
-  patches = [
-    # Fixes 2 test failures. The merge commit of PR:
-    # https://github.com/xarray-contrib/pint-xarray/pull/367
-    (fetchpatch2 {
-      url = "https://github.com/xarray-contrib/pint-xarray/commit/296ee2e60c671175507de1fe904fa7e4d0a70586.patch?full_index=1";
-      hash = "sha256-SS2zQ7fIeLzEUTd+8BW1PFBw8+qed0fLslns1jNpLtA=";
-    })
-    # Similarly to the above, fixes 3 more test failures. See:
-    # https://github.com/xarray-contrib/pint-xarray/pull/368
-    (fetchpatch2 {
-      url = "https://github.com/xarray-contrib/pint-xarray/commit/6134b61e2cb6b4c46b7b0974a6e499dff2d9e18e.patch?full_index=1";
-      hash = "sha256-s1AbaXAeL0sGkj/DWV5145FKGHW4eF+a11w1yg5QENA=";
-    })
   ];
 
   dependencies = [
