50f835d76d863185d8aa00868d14f8c7af50ab54
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-24 20:48:56 +0000

    python3Packages.python-roborock: 4.18.1 -> 5.0.0

--- pkgs/development/python-modules/python-roborock/default.nix
+++ pkgs/development/python-modules/python-roborock/default.nix
@@ -23,14 +23,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "python-roborock";
-  version = "4.18.1";
+  version = "5.0.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Python-roborock";
     repo = "python-roborock";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-5ckpRKwrLkjIAUJiXIRN2N9268pzjEjwVPVK/wSolgE=";
+    hash = "sha256-Lzr+OBbOjsLpkUsFYNJ37teegWjicUAoW9Jvw3hOvGE=";
   };
 
   pythonRelaxDeps = [ "pycryptodome" ];
