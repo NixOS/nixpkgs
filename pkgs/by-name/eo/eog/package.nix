65997407ea479f358738133d53806aaabbdc6a1b
Bobby Rong <rjl931189261@126.com>
2026-03-24 12:11:05 +0000

    eog: 49.1 → 50.0
    https://gitlab.gnome.org/GNOME/eog/-/compare/49.1...50.0

--- pkgs/by-name/eo/eog/package.nix
+++ pkgs/by-name/eo/eog/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchurl,
-  fetchpatch,
   meson,
   ninja,
   gettext,
@@ -34,7 +33,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "eog";
-  version = "49.1";
+  version = "50.0";
 
   outputs = [
     "out"
@@ -44,7 +43,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/eog/${lib.versions.major finalAttrs.version}/eog-${finalAttrs.version}.tar.xz";
-    hash = "sha256-+t0S4UupzSvmmJ37zvQAAWRbY0QM7tjgSGewdYewZtw=";
+    hash = "sha256-XAIm0Qz6E1aa3iIY8DzmL3m43Fwoji4LEj0u9Rq9ywc=";
   };
 
   patches = [
@@ -52,12 +51,6 @@ stdenv.mkDerivation (finalAttrs: {
     # We patch gobject-introspection to hardcode absolute paths but
     # our Meson patch will only pass the info when install_dir is absolute as well.
     ./fix-gir-lib-path.patch
-
-    # Switch to girepository-2.0
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/eog/raw/939eee56e5b72b02a8c0f4f867431cf6426adb9e/f/libpeas1_pygobject352.patch";
-      hash = "sha256-5QIkxxUvGKzb0cMCIcfuu7FX9XuY+RrKNunSvRDEDRc=";
-    })
   ];
 
   nativeBuildInputs = [
