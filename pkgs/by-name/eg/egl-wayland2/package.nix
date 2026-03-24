92e9c74a24c90cb624a8e9a242cee76b0495f261
ccicnce113424 <ccicnce113424@gmail.com>
2026-03-25 03:09:25 +0800

    {egl-wayland, egl-wayland2}: add update script, add maintainer, make library path absolute

--- pkgs/by-name/eg/egl-wayland2/package.nix
+++ pkgs/by-name/eg/egl-wayland2/package.nix
@@ -12,16 +12,12 @@
   libdrm,
   wayland,
   wayland-protocols,
+  nix-update-script,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "egl-wayland2";
   version = "1.0.1";
-
-  outputs = [
-    "out"
-    "dev"
-  ];
 
   src = fetchFromGitHub {
     owner = "NVIDIA";
@@ -47,22 +43,24 @@ stdenv.mkDerivation (finalAttrs: {
     libdrm
     wayland
     wayland-protocols
-  ];
-
-  propagatedBuildInputs = [
     eglexternalplatform
   ];
 
-  postInstall = ''
-    substituteInPlace $out/share/egl/egl_external_platform.d/09_nvidia_wayland2.json \
-      --replace-fail "libnvidia-egl-wayland2.so.1" "$out/lib/libnvidia-egl-wayland2.so.1"
-  '';
+  absolutizeEglExternalPlatformIcdJson = true;
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  passthru.updateScript = nix-update-script { extraArgs = [ "--use-github-releases" ]; };
 
   meta = {
     description = "Dma-buf-based Wayland external platform library";
     homepage = "https://github.com/NVIDIA/egl-wayland2/";
     license = lib.licenses.asl20;
     platforms = lib.platforms.linux;
-    maintainers = with lib.maintainers; [ vancluever ];
+    maintainers = with lib.maintainers; [
+      vancluever
+      ccicnce113424
+    ];
   };
 })
