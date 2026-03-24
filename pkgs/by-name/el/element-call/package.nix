e14eb2ecfe5d21725026c3366c4232245bf8cdca
Tiago Ferreira <me@tiferrei.com>
2026-03-24 14:32:39 +0000

    element-call: 0.11.1 -> 0.18.0

--- pkgs/by-name/el/element-call/package.nix
+++ pkgs/by-name/el/element-call/package.nix
@@ -7,23 +7,24 @@
   yarn-berry,
   yarnConfigHook,
   nodejs,
+  runCommand,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "element-call";
-  version = "0.11.1";
+  version = "0.18.0";
 
   src = fetchFromGitHub {
     owner = "element-hq";
     repo = "element-call";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-SNwNo3v2HydNXtb3H3Xh2mYBIagMkWjitlKWTfraeVU=";
+    hash = "sha256-/5RkZNf/ErSxNwW0ZfPwF52k3fZzAQAFMmbJ9xM7f74=";
   };
 
-  matrixJsSdkRevision = "19b1b901f575755d29d1fe03ca48cbf7c1cae05c";
+  matrixJsSdkRevision = "6e3efef0c5f660df47cf00874927dec1c75cc3cf";
   matrixJsSdkOfflineCache = fetchYarnDeps {
     yarnLock = "${finalAttrs.offlineCache}/checkouts/${finalAttrs.matrixJsSdkRevision}/yarn.lock";
-    hash = "sha256-pi2MW+58DCkHJDOxMWeXzF+v+5JhJFGQcUgsRsYjNvw=";
+    hash = "sha256-YvXmPWHt3qL9z8uap0/faKi5OId6zZ0ISiMT3x6ARx8=";
   };
 
   dontYarnInstallDeps = true;
@@ -34,14 +35,20 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir -p .git/{refs,objects}
     echo ${finalAttrs.matrixJsSdkRevision} > .git/HEAD
     SKIP_YARN_COREPACK_CHECK=1 offlineCache=$matrixJsSdkOfflineCache yarnConfigHook
+    SKIP_YARN_COREPACK_CHECK=1 yarn build
     popd
     offlineCache=writable
+    # The matrix-js-sdk git package checksum in yarn.lock was computed against a
+    # developer checkout with pre-compiled lib/. nix-prefetch-git stores a bare
+    # working tree so the repack at build time produces a different zip hash.
+    # The offline cache is already verified by the FOD hash, so this is safe.
+    export YARN_CHECKSUM_BEHAVIOR=ignore
   '';
 
   missingHashes = ./missing-hashes.json;
   offlineCache = yarn-berry.fetchYarnBerryDeps {
     inherit (finalAttrs) src missingHashes;
-    hash = "sha256-Z8gTt4W78w2DdkRhnaPLG2RIwfT64RFs5+UobARDG4c=";
+    hash = "sha256-Ose2PlsNHlCllynl+aIx/nToqtsqs7f43znOTLm2WEE=";
   };
 
   nativeBuildInputs = [
@@ -64,6 +71,12 @@ stdenv.mkDerivation (finalAttrs: {
     cp -r dist/* $out
 
     runHook postInstall
+  '';
+
+  passthru.tests.build = runCommand "${finalAttrs.pname}-test" { } ''
+    test -f ${finalAttrs.finalPackage}/index.html
+    test -d ${finalAttrs.finalPackage}/assets
+    touch $out
   '';
 
   meta = {
