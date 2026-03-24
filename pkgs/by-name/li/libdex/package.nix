8f86b8fc2ac0dd250992a87e9136f65c07376229
Bobby Rong <rjl931189261@126.com>
2026-03-24 12:16:55 +0000

    libdex: 1.0.0 → 1.1.0
    https://gitlab.gnome.org/GNOME/libdex/-/compare/1.0.0...1.1.0

--- pkgs/by-name/li/libdex/package.nix
+++ pkgs/by-name/li/libdex/package.nix
@@ -15,7 +15,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libdex";
-  version = "1.0.0";
+  version = "1.1.0";
 
   outputs = [
     "out"
@@ -25,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/libdex/${lib.versions.majorMinor finalAttrs.version}/libdex-${finalAttrs.version}.tar.xz";
-    hash = "sha256-e49cXbN5bhThLhBCLiNWdmuoMLkoFf7nC7yGe1sgf10=";
+    hash = "sha256-qeBMir7gHJp88RSMW3qma6N37+avbYOAHU/RqlUhmqo=";
   };
 
   nativeBuildInputs = [
