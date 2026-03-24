061ac920d52b00332f62f4a98e49871e6e92e8e9
Bobby Rong <rjl931189261@126.com>
2026-03-24 12:14:50 +0000

    gnome-shell-extensions: 49.0 → 50.0
    https://gitlab.gnome.org/GNOME/gnome-shell-extensions/-/compare/49.0...50.0

--- pkgs/by-name/gn/gnome-shell-extensions/package.nix
+++ pkgs/by-name/gn/gnome-shell-extensions/package.nix
@@ -15,11 +15,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-shell-extensions";
-  version = "49.0";
+  version = "50.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-shell-extensions/${lib.versions.major finalAttrs.version}/gnome-shell-extensions-${finalAttrs.version}.tar.xz";
-    hash = "sha256-FXDgZHhstDiXWeBIPzob29W5s48GJG600dUJKSqcamI=";
+    hash = "sha256-u4sMpPJ++1+WLK9epJ2LfKe1qefkSzUB/J3eL94nwGc=";
   };
 
   patches = [
