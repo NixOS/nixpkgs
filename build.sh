28b0ffd63cb3e8428df3e8755d50009aec5e80cf
Maciej Krüger <mkg20001@gmail.com>
2026-03-24 11:18:41 +0100

    fix: build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 # tar cf nixpkgs.tar.xz -C /nix/store "$(basename "$OUT")" --xz
 
 git worktree add clean HEAD
-mv .git /tmp -v
+mv clean/.git /tmp -v
 tar cf nixpkgs.tar.xz clean --xz
-mv /tmp/.git . -v
+mv /tmp/.git clean -v
 git worktree remove clean
 
