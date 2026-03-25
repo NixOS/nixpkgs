2f015148dc25d8fed9959091727f738fb1e063b3
phaer <phaer@phaer.org>
2026-03-25 11:01:21 +0100

    backblaze-b2: add phaer as maintainer

--- pkgs/by-name/ba/backblaze-b2/package.nix
+++ pkgs/by-name/ba/backblaze-b2/package.nix
@@ -104,6 +104,7 @@ python3Packages.buildPythonApplication (finalAttrs: {
   meta = {
     description = "Command-line tool for accessing the Backblaze B2 storage service";
     homepage = "https://github.com/Backblaze/B2_Command_Line_Tool";
+    maintainers = with lib.maintainers; [ phaer ];
     changelog = "https://github.com/Backblaze/B2_Command_Line_Tool/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     mainProgram = "backblaze-b2";
