a0a703a5744a4cad78c5397ad2deb62142985c78
winston <hey@winston.sh>
2026-03-25 22:57:53 +0800

    nixos/gdm: Drop wayland option
    daemon/WaylandEnable is gone.
    
    https://gitlab.gnome.org/GNOME/gdm/-/commit/b8fbd8ecc72b2d4974ee91e68407461fe97c5bef

--- nixos/modules/services/display-managers/gdm.nix
+++ nixos/modules/services/display-managers/gdm.nix
@@ -102,6 +102,12 @@ in
       [ "services" "xserver" "displayManager" "gdm" "autoLogin" "delay" ]
       [ "services" "displayManager" "gdm" "autoLogin" "delay" ]
     )
+    (lib.mkRemovedOptionModule [
+      "services"
+      "displayManager"
+      "gdm"
+      "wayland"
+    ] "Disabling this option is no longer supported with GNOME 50.")
   ];
 
   meta = {
@@ -124,14 +130,6 @@ in
         default = 0;
         description = ''
           Seconds of inactivity after which the autologin will be performed.
-        '';
-      };
-
-      wayland = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Allow GDM to run on Wayland instead of Xserver.
         '';
       };
 
@@ -347,7 +345,6 @@ in
     # presented and there's a little delay.
     services.displayManager.gdm.settings = {
       daemon = lib.mkMerge [
-        { WaylandEnable = cfg.wayland; }
         # nested if else didn't work
         (lib.mkIf (config.services.displayManager.autoLogin.enable && cfg.autoLogin.delay != 0) {
           TimedLoginEnable = true;
