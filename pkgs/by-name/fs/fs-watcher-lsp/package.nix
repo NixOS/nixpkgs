2eebcfc81fa92cf98a8669095a73e0499556c5a4
Lucas Andreussi <lucasandreussi@gmail.com>
2026-03-26 18:28:26 -0300

    fs-watcher-lsp: init at 0.1.0
    This patch adds fs-watcher-lsp rust crate to nixpkgs.
    This package can be used to add buffer auto reload in helix-editor.
    
    The rust crate itself doesn't have any tests, but I added the passthru.tests, in case of this crate could have tests in the future.

---
+++ pkgs/by-name/fs/fs-watcher-lsp/package.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  fetchCrate,
+  openssl,
+  pkg-config,
+  rustPlatform,
+  nix-update-script,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "fs-watcher-lsp";
+  version = "0.1.0";
+
+  src = fetchCrate {
+    version = finalAttrs.version;
+    crateName = "fs_watcher_lsp";
+    hash = "sha256-zahbi8RK8aDHcVOzIk5fCIh57+SjMGAVtUvtKhpMvF0=";
+  };
+
+  cargoHash = "sha256-w1i19IV/tjyl+W0NIjjbB0R9UpGrAUuK/yWbOZUKPUA=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+  ];
+
+  buildNoDefaultFeatures = false;
+  doCheck = true;
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Little file system watcher LSP to reload your editor";
+    changelog = "https://codeberg.org/Zentropivity/fs_watcher_lsp/releases/tag/v${finalAttrs.version}";
+    license = with lib.licenses; [
+      mit
+      asl20
+    ];
+    maintainers = [ ];
+    mainProgram = "fs_watcher_lsp";
+  };
+})
