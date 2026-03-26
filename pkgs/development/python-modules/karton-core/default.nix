632c1f1e00e3b21f4b4fad6e26fcbacb1ada14f5
Fabian Affolter <mail@fabian-affolter.ch>
2026-03-26 20:16:48 +0100

    python3Packages.karton-core: migrate to finalAttrs

--- pkgs/development/python-modules/karton-core/default.nix
+++ pkgs/development/python-modules/karton-core/default.nix
@@ -9,7 +9,7 @@
   unittestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "karton-core";
   version = "5.9.1";
   pyproject = true;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "CERT-Polska";
     repo = "karton";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-b/wOkOk6LB8uTDsXJrNQ2iru2H6mgaMhIyWSU5y2mx0=";
   };
 
@@ -41,11 +41,11 @@ buildPythonPackage rec {
   meta = {
     description = "Distributed malware processing framework";
     homepage = "https://karton-core.readthedocs.io/";
-    changelog = "https://github.com/CERT-Polska/karton/releases/tag/${src.tag}";
+    changelog = "https://github.com/CERT-Polska/karton/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [
       chivay
       fab
     ];
   };
-}
+})
