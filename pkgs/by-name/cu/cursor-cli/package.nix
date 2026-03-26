13011ac75d0ab7f717d021d5561a36a327e1fd44
Aaron Jheng <wentworth@outlook.com>
2026-03-26 09:31:06 +0800

    cursor-cli: 0-unstable-2026-03-18 -> 0-unstable-2026-03-25

--- pkgs/by-name/cu/cursor-cli/package.nix
+++ pkgs/by-name/cu/cursor-cli/package.nix
@@ -9,26 +9,26 @@ let
   inherit (stdenv) hostPlatform;
   sources = {
     x86_64-linux = fetchurl {
-      url = "https://downloads.cursor.com/lab/2026.03.18-f6873f7/linux/x64/agent-cli-package.tar.gz";
-      hash = "sha256-O0dcGKyTryFBiSCIoUtsBoWKBCdgj3Rc178nbaxFu64=";
+      url = "https://downloads.cursor.com/lab/2026.03.25-933d5a6/linux/x64/agent-cli-package.tar.gz";
+      hash = "sha256-sLNUNy95Ro953X4tha0fPj/M8DRN4Txn34m2kWa/J/U=";
     };
     aarch64-linux = fetchurl {
-      url = "https://downloads.cursor.com/lab/2026.03.18-f6873f7/linux/arm64/agent-cli-package.tar.gz";
-      hash = "sha256-JpSeyfrpX9vcAu3ymn4hBlomuRGnowVFXQ58NaCoSz8=";
+      url = "https://downloads.cursor.com/lab/2026.03.25-933d5a6/linux/arm64/agent-cli-package.tar.gz";
+      hash = "sha256-+Gk7XDf294AxHYwig2s7h4DSgy2Sf5F5Va+c5ufDbtw=";
     };
     x86_64-darwin = fetchurl {
-      url = "https://downloads.cursor.com/lab/2026.03.18-f6873f7/darwin/x64/agent-cli-package.tar.gz";
-      hash = "sha256-ubEakI8t3/BJ9eVcYueGEg7RWhvTjWbJMGNweyF9V0E=";
+      url = "https://downloads.cursor.com/lab/2026.03.25-933d5a6/darwin/x64/agent-cli-package.tar.gz";
+      hash = "sha256-JDUMpdFEnPgYJWjXXIsdqOVbkw6DbcahbcCVYjbqKxo=";
     };
     aarch64-darwin = fetchurl {
-      url = "https://downloads.cursor.com/lab/2026.03.18-f6873f7/darwin/arm64/agent-cli-package.tar.gz";
-      hash = "sha256-KHybHgQrIOBHbiJZSEbFVG0Fp1+TRPvK75JXPcSTMKk=";
+      url = "https://downloads.cursor.com/lab/2026.03.25-933d5a6/darwin/arm64/agent-cli-package.tar.gz";
+      hash = "sha256-1QTjO99QvdviRsos5pvOrfR2L1fwj6nBh0nPqcY1qnI=";
     };
   };
 in
 stdenv.mkDerivation {
   pname = "cursor-cli";
-  version = "0-unstable-2026-03-18";
+  version = "0-unstable-2026-03-25";
 
   src = sources.${hostPlatform.system};
 
