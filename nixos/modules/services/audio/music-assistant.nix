c13975b5c8bab554f2e94de346ee1c853fc9b977
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-03-27 00:54:00 +0100

    nixos/music-assistant: open port for snapcast provider

--- nixos/modules/services/audio/music-assistant.nix
+++ nixos/modules/services/audio/music-assistant.nix
@@ -82,7 +82,8 @@ in
       allowedTCPPorts =
         lib.optional cfg.enable 8097 # Music Assistant stream port
         ++ lib.optional (lib.elem "airplay" cfg.providers) 7000
-        ++ lib.optional (lib.elem "sendspin" cfg.providers) 8927;
+        ++ lib.optional (lib.elem "sendspin" cfg.providers) 8927
+        ++ lib.optional (lib.elem "snapcast" cfg.providers) 1780;
       # The information published by Apple 1 seem to not apply to libraop.
       # The closest we could find that represents the port range being used as observed by tcpdump is the ephemeral port range.
       # 1: https://support.apple.com/en-us/103229#:~:text=49152%E2%80%93-,65535,-TCP%2C%20UDP
