acfb428e3a40c435caa1712d00587846d47abf62
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-26 09:04:06 +0000

    rust-analyzer-unwrapped: 2026-03-16 -> 2026-03-23

--- pkgs/by-name/ru/rust-analyzer-unwrapped/package.nix
+++ pkgs/by-name/ru/rust-analyzer-unwrapped/package.nix
@@ -13,7 +13,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "rust-analyzer-unwrapped";
-  version = "2026-03-16";
+  version = "2026-03-23";
 
   cargoHash = "sha256-osoNyx4UEbq0J2fx7WMJBfIRV3mhsO+OSBNrvu060IM=";
 
@@ -21,7 +21,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
     owner = "rust-lang";
     repo = "rust-analyzer";
     rev = finalAttrs.version;
-    hash = "sha256-CKmkYqUi2pI1uDGDfpK0mkZbRLyjUKCpYDU3eMHtmks=";
+    hash = "sha256-aEIdkqB8gtQZtEbogdUb5iyfcZpKIlD3FkG8ANu73/I=";
   };
 
   cargoBuildFlags = [
