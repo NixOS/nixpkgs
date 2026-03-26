28ec90ec217821bb1fd46fcc7ace296595959881
Fabian Affolter <mail@fabian-affolter.ch>
2026-03-26 20:20:16 +0100

    python3Packages.pyarlo: migrate to finalattrs and modernize

--- pkgs/development/python-modules/pyarlo/default.nix
+++ pkgs/development/python-modules/pyarlo/default.nix
@@ -10,7 +10,7 @@
   sseclient-py,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalattrs: {
   pname = "pyarlo";
   version = "0.2.4";
   pyproject = true;
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "tchellomello";
     repo = "python-arlo";
-    rev = version;
+    tag = finalattrs.version;
     sha256 = "0pp7y2llk4xnf6zh57j5xas0gw5zqm42qaqssd8p4qa3g5rds8k3";
   };
 
@@ -40,7 +40,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python library to work with Netgear Arlo cameras";
     homepage = "https://github.com/tchellomello/python-arlo";
-    license = with lib.licenses; [ lgpl3Plus ];
+    changelog = "https://github.com/tchellomello/python-arlo/releases/tag/${finalattrs.src.tag}";
+    license = lib.licenses.lgpl3Plus;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
