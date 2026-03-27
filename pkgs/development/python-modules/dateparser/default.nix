e2a386d885360d1db6ce5ee4a6ed285cbf02d78d
Robert Schütz <nix@dotlambda.de>
2026-03-26 17:32:28 -0700

    python3Packages.dateparser: 1.3.0 -> 1.4.0
    Diff: https://github.com/scrapinghub/dateparser/compare/v1.3.0...v1.4.0
    
    Changelog: https://github.com/scrapinghub/dateparser/blob/v1.4.0/HISTORY.rst

--- pkgs/development/python-modules/dateparser/default.nix
+++ pkgs/development/python-modules/dateparser/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "dateparser";
-  version = "1.3.0";
+  version = "1.4.0";
 
   pyproject = true;
 
@@ -30,7 +30,7 @@ buildPythonPackage rec {
     owner = "scrapinghub";
     repo = "dateparser";
     tag = "v${version}";
-    hash = "sha256-X15zNHlF34+8Lmo6Ia3HyKOdfgsu76KbcJUxzHax0EE=";
+    hash = "sha256-CmcQf0cGcZVmZfpLSYDGdZUj83T7enNRl9FTY1Q6vtk=";
   };
 
   build-system = [ setuptools ];
