c6762d73788ced5f2ca44f4a5ed7154499a993a0
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-27 11:47:02 +0000

    zvm: 0.8.11 -> 0.8.14
    Upstream requires go 1.26 now.
    
    (cherry picked from commit 5367f990bdc5b00e676954a0638e6c0c0dcf1bf6)

--- pkgs/by-name/zv/zvm/package.nix
+++ pkgs/by-name/zv/zvm/package.nix
@@ -1,23 +1,23 @@
 {
   lib,
-  buildGoModule,
+  buildGo126Module,
   fetchFromGitHub,
   versionCheckHook,
   nix-update-script,
 }:
 
-buildGoModule rec {
+buildGo126Module rec {
   pname = "zvm";
-  version = "0.8.11";
+  version = "0.8.14";
 
   src = fetchFromGitHub {
     owner = "tristanisham";
     repo = "zvm";
     tag = "v${version}";
-    hash = "sha256-RMXF69zqqNK3tifbeDM7dxkiHMws7n+PeeHvCUK7/OU=";
+    hash = "sha256-MAE7zs60DFIicYRtMhstzsOiS2flVv+dyPJVmcyAEio=";
   };
 
-  vendorHash = "sha256-dM9FiUucSBkk8L93HfzoHQ1EyyRmAZjfedvOyRBDFBA=";
+  vendorHash = "sha256-29hFuQnLPdLkAG4x5QWqXqBIGtppi/rj3OuTfMBgTAI=";
 
   doInstallCheck = true;
   versionCheckProgramArg = "--version";
