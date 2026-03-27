02bfcdf263a02723a7a93d1f7ddbf22c24965eb6
OPNA2608 <opna2608@protonmail.com>
2026-03-27 15:07:38 +0100

    libsForQt5.pyotherside: Rename from pyotherside
    So a Qt6 build can be added.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7117,8 +7117,6 @@ with pkgs;
 
   pth = if stdenv.hostPlatform.isMusl then npth else gnupth;
 
-  pyotherside = libsForQt5.callPackage ../development/libraries/pyotherside { };
-
   qbs = libsForQt5.callPackage ../development/tools/build-managers/qbs { };
 
   qdjango = libsForQt5.callPackage ../development/libraries/qdjango { };
