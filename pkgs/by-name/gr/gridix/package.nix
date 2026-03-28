536eeb838f54079596a3c2b5598f47636b5842f3
MCB-SMART-BOY <2720838051@qq.com>
2026-03-28 08:26:28 +0800

    gridix: 3.0.0 -> 3.2.0

--- pkgs/by-name/gr/gridix/package.nix
+++ pkgs/by-name/gr/gridix/package.nix
@@ -11,13 +11,13 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "gridix";
-  version = "3.0.0";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
     owner = "MCB-SMART-BOY";
     repo = "Gridix";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-wi1XALHo8B8GZyaHDGht3cg9afxojfNbXYoDrr0Yh8k=";
+    hash = "sha256-1xh+htOC4GG9gR9Fg903ZjNeSXeGFyU1L2RPUlWo+E4=";
   };
 
   cargoHash = "sha256-x7yaHKalDX7UQk2P62F2jHMXeL3oBh/5QgCOb/tzZCw=";
