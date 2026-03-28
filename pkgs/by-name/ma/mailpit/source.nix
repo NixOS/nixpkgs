d11ae54bd85bdefe92e16489632371d221153a71
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-28 17:50:21 +0000

    mailpit: 1.29.3 -> 1.29.4
    (cherry picked from commit d57c7bbe9974cd1ea806be8b7767368fc272b3c5)

--- pkgs/by-name/ma/mailpit/source.nix
+++ pkgs/by-name/ma/mailpit/source.nix
@@ -1,6 +1,6 @@
 {
-  version = "1.29.3";
-  hash = "sha256-9j2qKXTWBxA6bKF/+ISCeB5kVrE+hljk5XiZqjE5V20=";
-  npmDepsHash = "sha256-g1qfhUve3G2ZRgEqNHYcP/l4XJVUIvvU0cr9WS9wtX4=";
-  vendorHash = "sha256-RZ8TEHRiACQoWipcDvaEWBqjWhfVK7achlBBsFOLWdc=";
+  version = "1.29.4";
+  hash = "sha256-QNTXl49WgmixaWGHhjVcMJwj/AA3agwqlPRC3/Nnfsg=";
+  npmDepsHash = "sha256-lVbNT2CEEMS2DpJhUMd/ID8yu8PuRbWrciJ/h391czs=";
+  vendorHash = "sha256-ypFnKykiDFs8JxMkOnQYwJU5FfFhRrCSrhEgsfN1TVc=";
 }
