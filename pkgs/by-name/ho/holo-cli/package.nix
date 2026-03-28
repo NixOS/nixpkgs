01f56714cdc9fb35d42488169adc0ebaaff9824f
phanirithvij <phanirithvij2000@gmail.com>
2026-03-28 21:28:03 +0530

    holo-cli: 0.5.0-unstable-2026-01-03 -> 0.5.0-unstable-2026-03-15
    Upstream pr to update the cargo lockfile
    https://github.com/holo-routing/holo-cli/pull/31
    
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/ho/holo-cli/package.nix
+++ pkgs/by-name/ho/holo-cli/package.nix
@@ -2,31 +2,40 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+
   stdenv,
   replaceVars,
+
   cmake,
   pkg-config,
   protobuf,
+
   pcre2,
+
   nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "holo-cli";
-  version = "0.5.0-unstable-2026-01-03";
+  version = "0.5.0-unstable-2026-03-15";
 
   src = fetchFromGitHub {
     owner = "holo-routing";
     repo = "holo-cli";
-    rev = "8a8f02fc56f30cca216e9a99029b736fe57b3d59";
-    hash = "sha256-pCupXT4fymydzOpdsMbimAcQZzVUNzfG3VRnrD3q7Xw=";
+    rev = "36fdc13323e384c086da8663f0d510b238fb6e4f";
+    hash = "sha256-5Nvyh9gznMsutu3wHR6gwgKkIm115hbx4R6D/Gm1Rug=";
   };
 
-  cargoHash = "sha256-7/OtT2TdLhFVZeuQOg6xQJFnGJNz/G9mna8vIeh86/k=";
-  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+  cargoPatches = [
+    # cargo lock is outdated
+    # https://github.com/holo-routing/holo-cli/pull/31
+    ./update-cargo-lock.patch
+  ];
+
+  cargoHash = "sha256-77aUfXcnVQLVEKQuUdBZ4k5/3rOoe9PvGC0AlJS0UJc=";
 
   postPatch = lib.optionalString stdenv.hostPlatform.isDarwin ''
-    pushd $cargoDepsCopy/libyang4-sys-*
+    pushd $cargoDepsCopy/*/libyang4-sys-*
     patch -p1 < ${
       replaceVars ./libyang4-sys.patch {
         PCRE2_INCLUDE_DIRS = "${lib.getInclude pcre2}/include";
@@ -44,17 +53,20 @@ rustPlatform.buildRustPackage (finalAttrs: {
     pkg-config
     protobuf
   ];
+
   buildInputs = [
     pcre2
   ];
 
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
+
   meta = {
     description = "Holo` Command Line Interface";
     homepage = "https://github.com/holo-routing/holo-cli";
-    teams = with lib.teams; [ ngi ];
-    maintainers = with lib.maintainers; [ themadbit ];
     license = lib.licenses.mit;
     mainProgram = "holo-cli";
+    maintainers = with lib.maintainers; [ themadbit ];
     platforms = lib.platforms.all;
+    teams = with lib.teams; [ ngi ];
   };
 })
