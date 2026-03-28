c112491d1175c13a67ddbd103c752d3fd1a03673
Harinn <prinn.dev@pm.me>
2026-03-28 14:36:05 +0700

    soft-serve: use finalAttrs

--- pkgs/by-name/so/soft-serve/package.nix
+++ pkgs/by-name/so/soft-serve/package.nix
@@ -8,17 +8,14 @@
   bash,
 }:
 
-let
-  version = "0.11.6";
-in
-buildGoModule {
+buildGoModule (finalAttrs: {
   pname = "soft-serve";
-  inherit version;
+  version = "0.11.6";
 
   src = fetchFromGitHub {
     owner = "charmbracelet";
     repo = "soft-serve";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-oWZfhB14Yjj3/BKKhowKDKX5v4RvIwjHDCSFfnI+f54=";
   };
 
@@ -29,7 +26,7 @@ buildGoModule {
   ldflags = [
     "-s"
     "-w"
-    "-X=main.Version=${version}"
+    "-X=main.Version=${finalAttrs.version}"
   ];
 
   nativeBuildInputs = [ makeWrapper ];
@@ -51,9 +48,9 @@ buildGoModule {
   meta = {
     description = "Tasty, self-hosted Git server for the command line";
     homepage = "https://github.com/charmbracelet/soft-serve";
-    changelog = "https://github.com/charmbracelet/soft-serve/releases/tag/v${version}";
+    changelog = "https://github.com/charmbracelet/soft-serve/releases/tag/v${finalAttrs.version}";
     mainProgram = "soft";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
