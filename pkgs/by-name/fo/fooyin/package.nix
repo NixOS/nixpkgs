c06d723775c148c0f2298c90958e17b2bf8162a0
2kybe3 <kybe@kybe.xyz>
2026-03-29 05:36:40 +0200

    fooyin: 0.9.2 -> 0.10.3
    Diff: https://github.com/fooyin/fooyin/compare/v0.9.2...v0.10.3
    Changelogs:
    > https://github.com/fooyin/fooyin/releases/tag/v0.10.0
    > https://github.com/fooyin/fooyin/releases/tag/v0.10.1
    > https://github.com/fooyin/fooyin/releases/tag/v0.10.2
    > https://github.com/fooyin/fooyin/releases/tag/v0.10.3

--- pkgs/by-name/fo/fooyin/package.nix
+++ pkgs/by-name/fo/fooyin/package.nix
@@ -2,7 +2,6 @@
   stdenv,
   lib,
   fetchFromGitHub,
-  fetchpatch,
   cmake,
   pkg-config,
   alsa-lib,
@@ -12,24 +11,27 @@
   pipewire,
   taglib,
   libebur128,
-  libvgm,
   libsndfile,
   libarchive,
   libopenmpt,
+  soundtouch,
+  soxr,
   game-music-emu,
   SDL2,
   icu,
+  # Select bundled plugins to build. Leave empty for the default set, use none for no plugins, or use a semicolon/comma-separated list of plugin names to include or -name entries to exclude
+  pluginSelection ? "",
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fooyin";
-  version = "0.9.2";
+  version = "0.10.3";
 
   src = fetchFromGitHub {
     owner = "ludouzi";
     repo = "fooyin";
     tag = "v" + finalAttrs.version;
-    hash = "sha256-sQ1zsQ/6OHGPkofiKhusCrpW2XnO+PpMvH1M2OG5Huw=";
+    hash = "sha256-mrERtHhmfEfK79pSEYFILW9WebtKVhbuXdQ52WTGa5A=";
   };
 
   buildInputs = [
@@ -47,11 +49,12 @@ stdenv.mkDerivation (finalAttrs: {
     SDL2
     # input plugins
     libebur128
-    libvgm
     libsndfile
     libarchive
     libopenmpt
     game-music-emu
+    soundtouch
+    soxr
   ];
 
   nativeBuildInputs = [
@@ -66,24 +69,10 @@ stdenv.mkDerivation (finalAttrs: {
     # we need INSTALL_FHS to be true as the various artifacts are otherwise just dumped in the root
     # of $out and the fixupPhase cleans things up anyway
     (lib.cmakeBool "INSTALL_FHS" true)
+    (lib.cmakeFeature "PLUGIN_SELECTION" pluginSelection)
   ];
 
   env.LANG = "C.UTF-8";
-
-  # Remove after next release
-  patches = [
-    (fetchpatch {
-      name = "add-missing-header.patch";
-      url = "https://github.com/fooyin/fooyin/commit/7b171c0da2b9289468696424fe51f76e1c365bb5.patch";
-      hash = "sha256-Uvggz2F6DuWYAg20qi8uHkshzCnTLrchambQ/yDyIfA=";
-    })
-  ];
-
-  # Fix compatibility with Qt 6.10.1 - should be fixed in next release
-  postPatch = ''
-    substituteInPlace src/utils/starrating.cpp \
-      --replace-fail '.arg(alignment);' '.arg(alignment.toInt());'
-  '';
 
   meta = {
     description = "Customisable music player";
