181d2c9c7aeefe4bc920c185c5df5ba95d50575c
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-29 13:23:50 +0000

    cloudlog: 2.8.9 -> 2.8.10

--- pkgs/by-name/cl/cloudlog/package.nix
+++ pkgs/by-name/cl/cloudlog/package.nix
@@ -9,13 +9,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "cloudlog";
-  version = "2.8.9";
+  version = "2.8.10";
 
   src = fetchFromGitHub {
     owner = "magicbug";
     repo = "Cloudlog";
     rev = version;
-    hash = "sha256-vq4mt0/Bfd+VR/2Inj8oJteIV9V7v0uiLmRMkpV2wI8=";
+    hash = "sha256-i/0GNVLvF0jHF83Ee9GCo80xeHoQb18OLmSZL++OTEo=";
   };
 
   postPatch = ''
