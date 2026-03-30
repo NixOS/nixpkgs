2a56eea3be9d3538c82e5cccb49b05d5bb7700fd
Wolfgang Walther <walther@technowledgy.de>
2026-03-30 12:06:57 +0200

    haskellPackages.cabal2nix-unstable: 2.21.2-unstable-2026-03-07 -> 2.21.3-unstable-2026-03-30
    (cherry picked from commit 99395f95a5b285ed9b594cc8f73a2097f4fd8643)

--- pkgs/development/haskell-modules/cabal2nix-unstable/distribution-nixpkgs.nix
+++ pkgs/development/haskell-modules/cabal2nix-unstable/distribution-nixpkgs.nix
@@ -18,10 +18,10 @@
 }:
 mkDerivation {
   pname = "distribution-nixpkgs";
-  version = "1.7.1.1-unstable-2026-03-07";
+  version = "1.7.1.1-unstable-2026-03-30";
   src = fetchzip {
-    url = "https://github.com/NixOS/cabal2nix/archive/977a1c199f7f8092ce9eef7d322b0eecebde22a2.tar.gz";
-    sha256 = "0nl5502mpwhmav7qxx051zzyx8ag9mibwhh33bf4aj07ywziza4c";
+    url = "https://github.com/NixOS/cabal2nix/archive/41239bcc0622a0975c6705a03a44dfeffeb56f23.tar.gz";
+    sha256 = "01qj6cvaif0810v83r6izcj1bbfpcqqxw4wybq04qsq92sqybpw2";
   };
   postUnpack = "sourceRoot+=/distribution-nixpkgs; echo source root reset to $sourceRoot";
   enableSeparateDataOutput = true;
@@ -49,5 +49,5 @@ mkDerivation {
   ];
   homepage = "https://github.com/NixOS/cabal2nix/tree/master/distribution-nixpkgs#readme";
   description = "Types and functions to manipulate the Nixpkgs distribution";
-  license = lib.licenses.bsd3;
+  license = lib.licensesSpdx."BSD-3-Clause";
 }
