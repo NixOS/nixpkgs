e8fe63b46b5a07cdd0d533bba71053e06bb116d8
Wolfgang Walther <walther@technowledgy.de>
2026-03-30 09:27:09 +0200

    haskellPackages.ghc-debug-common: remove jailbreak
    Has been updated upstream.

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -480,9 +480,6 @@ with haskellLib;
     sha256 = "1c7knpvxr7p8c159jkyk6w29653z5yzgjjqj11130bbb8mk9qhq7";
   }) super.c2hsc;
 
-  # 2025-02-10: Too strict bounds on bytestring < 0.12
-  ghc-debug-common = doJailbreak super.ghc-debug-common;
-
   # https://github.com/agrafix/superbuffer/issues/4
   # Too strict bounds on bytestring < 0.12
   superbuffer = doJailbreak super.superbuffer;
