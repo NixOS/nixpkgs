89b95a59887ff4ccb4a4f4e60a96f17d55153a47
Sergei Trofimovich <slyich@gmail.com>
2026-03-31 06:31:43 +0100

    libavif: 1.4.0 -> 1.4.1
    Changes: https://github.com/AOMediaCodec/libavif/releases/tag/v1.4.1

--- pkgs/by-name/li/libavif/package.nix
+++ pkgs/by-name/li/libavif/package.nix
@@ -31,7 +31,7 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libavif";
-  version = "1.4.0";
+  version = "1.4.1";
 
   outputs = [
     "out"
@@ -42,7 +42,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "AOMediaCodec";
     repo = "libavif";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Q9wE0E9HPkYjMfkVG/+n1nuLLMZqCSFf+Wo/wPyCjww=";
+    hash = "sha256-035SoxHfN121mp3LGwGykReCi1WJbl2/nZH8c/VwABU=";
   };
 
   postPatch = ''
