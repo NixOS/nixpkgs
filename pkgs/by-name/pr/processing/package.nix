f45fba0201743bb2c193f7bfc8d8d1edde3c2982
Evan Goode <mail@evangoo.de>
2026-03-31 13:27:44 -0400

    processing: Remove unused nixpkgs dependencies
    These packages, if still necessary, are now fetched from Maven.

--- pkgs/by-name/pr/processing/package.nix
+++ pkgs/by-name/pr/processing/package.nix
@@ -2,14 +2,11 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchurl,
-  unzip,
   makeWrapper,
   gradle_8,
   jdk17,
   jogl,
   rsync,
-  batik,
   stripJavaArchivesHook,
   wrapGAppsHook3,
   libGL,
@@ -19,41 +16,6 @@ let
   gradle = gradle_8.override { java = jdk17; };
   jdk = jdk17;
   buildNumber = "1310";
-  vaqua = fetchurl {
-    name = "VAqua9.jar";
-    url = "https://violetlib.org/release/vaqua/9/VAqua9.jar";
-    sha256 = "cd0b82df8e7434c902ec873364bf3e9a3e6bef8b59cbf42433130d71bf1a779c";
-  };
-
-  jna = fetchurl {
-    name = "jna-5.10.0.zip";
-    url = "https://github.com/java-native-access/jna/archive/5.10.0.zip";
-    sha256 = "B5CakOQ8225xNsk2TMV8CbK3RcsLlb+pHzjaY5JNwg0=";
-  };
-
-  flatlaf = fetchurl {
-    name = "flatlaf-2.4.jar";
-    url = "mirror://maven/com/formdev/flatlaf/2.4/flatlaf-2.4.jar";
-    sha256 = "NVMYiCd+koNCJ6X3EiRx1Aj+T5uAMSJ9juMmB5Os+zc=";
-  };
-
-  lsp4j = fetchurl {
-    name = "org.eclipse.lsp4j-0.19.0.jar";
-    url = "mirror://maven/org/eclipse/lsp4j/org.eclipse.lsp4j/0.19.0/org.eclipse.lsp4j-0.19.0.jar";
-    sha256 = "sha256-1DI5D9KW+GL4gT1qjwVZveOl5KVOEjt6uXDwsFzi8Sg=";
-  };
-
-  lsp4j-jsonrpc = fetchurl {
-    name = "org.eclipse.lsp4j.jsonrpc-0.19.0.jar";
-    url = "mirror://maven/org/eclipse/lsp4j/org.eclipse.lsp4j.jsonrpc/0.19.0/org.eclipse.lsp4j.jsonrpc-0.19.0.jar";
-    sha256 = "sha256-ozYTkvv7k0psCeX/PbSM3/Bl17qT3upX3trt65lmM9I=";
-  };
-
-  gson = fetchurl {
-    name = "gson-2.9.1.jar";
-    url = "mirror://maven/com/google/code/gson/gson/2.9.1/gson-2.9.1.jar";
-    sha256 = "sha256-N4U04znm5tULFzb7Ort28cFdG+P0wTzsbVNkEuI9pgM=";
-  };
 in
 stdenv.mkDerivation rec {
   pname = "processing";
@@ -79,7 +41,6 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     gradle
-    unzip
     makeWrapper
     stripJavaArchivesHook
     wrapGAppsHook3
@@ -88,7 +49,6 @@ stdenv.mkDerivation rec {
     jdk
     jogl
     rsync
-    batik
   ];
 
   mitmCache = gradle.fetchDeps {
@@ -106,19 +66,6 @@ stdenv.mkDerivation rec {
   gradleUpdateScript = ''
     runHook preBuild
     runHook preGradleUpdate
-
-    mkdir -p app/lib
-    ln -s ${vaqua} app/lib/VAqua9.jar
-    ln -s ${flatlaf} app/lib/flatlaf.jar
-    ln -s ${lsp4j} java/mode/org.eclipse.lsp4j.jar
-    ln -s ${lsp4j-jsonrpc} java/mode/org.eclipse.lsp4j.jsonrpc.jar
-    ln -s ${gson} java/mode/gson.jar
-    unzip -qo ${jna} -d app/lib/
-    mv app/lib/{jna-5.10.0/dist/jna.jar,}
-    mv app/lib/{jna-5.10.0/dist/jna-platform.jar,}
-
-    ln -sf ${batik}/* java/libraries/svg/library/
-    cp java/libraries/svg/library/share/java/batik-all-${batik.version}.jar java/libraries/svg/library/batik.jar
 
     gradle createDistributable
 
@@ -138,19 +85,6 @@ stdenv.mkDerivation rec {
 
   buildPhase = ''
     runHook preBuild
-
-    mkdir -p app/lib
-    ln -s ${vaqua} app/lib/VAqua9.jar
-    ln -s ${flatlaf} app/lib/flatlaf.jar
-    ln -s ${lsp4j} java/mode/org.eclipse.lsp4j.jar
-    ln -s ${lsp4j-jsonrpc} java/mode/org.eclipse.lsp4j.jsonrpc.jar
-    ln -s ${gson} java/mode/gson.jar
-    unzip -qo ${jna} -d app/lib/
-    mv app/lib/{jna-5.10.0/dist/jna.jar,}
-    mv app/lib/{jna-5.10.0/dist/jna-platform.jar,}
-
-    ln -sf ${batik}/* java/libraries/svg/library/
-    cp java/libraries/svg/library/share/java/batik-all-${batik.version}.jar java/libraries/svg/library/batik.jar
 
     gradle assemble
 
