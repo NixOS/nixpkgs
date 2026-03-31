0612f83ba234bfeef570f078f098bc8d209a0c3f
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-31 01:48:23 +0000

    python3Packages.knx-frontend: 2026.3.2.183756 -> 2026.3.28.223133

--- pkgs/development/python-modules/knx-frontend/default.nix
+++ pkgs/development/python-modules/knx-frontend/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "knx-frontend";
-  version = "2026.3.2.183756";
+  version = "2026.3.28.223133";
   pyproject = true;
 
   # TODO: source build, uses yarn.lock
   src = fetchPypi {
     pname = "knx_frontend";
     inherit version;
-    hash = "sha256-OaY14AY3iaAbs8EDCLDI1LmwjtOcz1NBBWhZEU2apjI=";
+    hash = "sha256-U/lHc4SZcUEG1yq2sMkSzM9n/n6TuNhbx4U2ZmMqAY0=";
   };
 
   build-system = [ setuptools ];
