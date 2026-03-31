5a306a774696046dfa0a4fe2ba14826f2be4cd6a
Grimmauld <Grimmauld@grimmauld.de>
2026-03-31 22:13:06 +0200

    nixos/globalprotect-vpn: drop

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1212,7 +1212,6 @@
   ./services/networking/geph.nix
   ./services/networking/ghostunnel.nix
   ./services/networking/git-daemon.nix
-  ./services/networking/globalprotect-vpn.nix
   ./services/networking/gns3-server.nix
   ./services/networking/gnunet.nix
   ./services/networking/go-autoconfig.nix
