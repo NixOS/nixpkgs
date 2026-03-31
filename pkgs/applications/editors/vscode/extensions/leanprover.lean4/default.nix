225a45acf4204faf6082a6132ca0bca5bed7078a
R. Ryantm <ryantm-bot@ryantm.com>
2026-03-31 04:11:37 +0000

    vscode-extensions.leanprover.lean4: 0.0.225 -> 0.0.226

--- pkgs/applications/editors/vscode/extensions/leanprover.lean4/default.nix
+++ pkgs/applications/editors/vscode/extensions/leanprover.lean4/default.nix
@@ -7,8 +7,8 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "lean4";
     publisher = "leanprover";
-    version = "0.0.225";
-    hash = "sha256-JVsOHO2r7YHC4QxvpjoIgT5rZhW2SS24xu3TMnoRQi8=";
+    version = "0.0.226";
+    hash = "sha256-K5zqYX1I3yHLgXzDCPGCnQzChQ4pPCHGriRKSP7ZbGE=";
   };
 
   meta = {
