b4640df6948e37ad4b41d8be245c5d497ec45265
Pui Yong Qing <puiyongqing@gmail.com>
2026-03-31 14:04:18 +0800

    matlab-language-server: 1.1.6 -> 1.3.9

--- pkgs/by-name/ma/matlab-language-server/package.nix
+++ pkgs/by-name/ma/matlab-language-server/package.nix
@@ -2,39 +2,29 @@
   lib,
   buildNpmPackage,
   fetchFromGitHub,
-  fetchpatch,
 }:
 
-buildNpmPackage {
+buildNpmPackage (finalAttrs: {
   pname = "matlab-language-server";
-  version = "1.1.6";
+  version = "1.3.9";
 
   src = fetchFromGitHub {
     owner = "mathworks";
     repo = "matlab-language-server";
-    # Upstream doesn't tag commits unfortunately, but lists versions and dates
-    # in README... See complaint at:
-    # https://github.com/mathworks/MATLAB-language-server/issues/24
-    rev = "c8c901956e3bbfbd6eab440a1b60c3fe016cf567";
-    hash = "sha256-D03gXyrvPYOMkJI2YuHfPAnWdXTz5baemykQ5j9L0rs=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-V4CW7mC3F4L7yqpB4AhpLNtOAaEGIWT8AMWCJkTHepI=";
   };
-  patches = [
-    # https://github.com/mathworks/MATLAB-language-server/pull/23
-    (fetchpatch {
-      url = "https://github.com/mathworks/MATLAB-language-server/commit/56374de620b4855529c4136539f52ab6030e2c92.patch";
-      hash = "sha256-F38ATP+eap0SnxQoib1JwIvNCFfB7g8EtXI9+iK5+HA=";
-    })
-  ];
 
-  npmDepsHash = "sha256-P3MSrwk6FVt4lK58pjwy0YOg2UZI0TG8uXjqCPudgTE=";
+  npmDepsHash = "sha256-eN6Z/UhzovwJh8EoCTuDnhsYyOxY9/fxOkPf0TqIg3k=";
 
   npmBuildScript = "package";
 
   meta = {
     description = "Language Server for MATLAB® code";
     homepage = "https://github.com/mathworks/MATLAB-language-server";
+    changelog = "https://github.com/mathworks/MATLAB-language-server/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ doronbehar ];
     mainProgram = "matlab-language-server";
   };
-}
+})
