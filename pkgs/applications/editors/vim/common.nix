5c9fdd73a056e74f9f36be77b3901dfe84d07190
Fovir <fovir@disroot.org>
2026-03-31 12:37:46 +0800

    vim: 9.2.0106 -> 9.2.0272
    fix the [CVE vulnerability](https://github.com/vim/vim/security/advisories/GHSA-2gmj-rpqf-pxvh)

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,6 +1,6 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "9.2.0140";
+  version = "9.2.0272";
 
   outputs = [
     "out"
@@ -11,7 +11,7 @@ rec {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    hash = "sha256-0eUOEAkR2Hcd4blxToSmYhN1+C3gNW0Up2n6n0TAryA=";
+    hash = "sha256-9y+dZYbwuIzgseXdg/k8fqZ5JiohgOiK00evMwF9HOM=";
   };
 
   enableParallelBuilding = true;
