NixOS/nixpkgs

commit 25eb3594716b6f1c344ca632d82aeb228084e27b

haskellPackages.cabal2nix-unstable: build against Cabal 3.16
hpack and cabal2nix support it now.

pkgs/development/haskell-modules/configuration-common.nix
@@ -143,6 +143,8 @@ with haskellLib;
 
         cabal-install-solver = super.cabal-install-solver.overrideScope cabalInstallOverlay;
 
+        cabal2nix-unstable = super.cabal2nix-unstable.overrideScope cabalInstallOverlay;
+
         # Needs cabal-install >= 3.8 /as well as/ matching Cabal
         guardian = lib.pipe (super.guardian.overrideScope cabalInstallOverlay) [
           # Tests need internet access (run stack)
@@ -156,25 +158,6 @@ with haskellLib;
     cabal-install-solver
     guardian
     ;
-
-  # cabal2nix depends on hpack which doesn't support Cabal >= 3.16
-  cabal2nix-unstable = super.cabal2nix-unstable.override (
-    prev:
-    # Manually override the relevant dependencies to reduce rebuild amount
-    let
-      cabalOverride = {
-        Cabal = self.Cabal_3_14_2_0;
-      };
-    in
-    cabalOverride
-    // lib.mapAttrs (_: drv: drv.override cabalOverride) {
-      inherit (prev)
-        distribution-nixpkgs
-        hackage-db
-        hpack
-        ;
-    }
-  );
 
   # Extensions wants a specific version of Cabal for its list of Haskell
   # language extensions.

commit c87bcf8c8557b5450a16e956849a0e113cb657d2

haskellPackages.cabal2nix-unstable: use latest Cabal
This broke in 25eb3594716b6f1c344ca632d82aeb228084e27b, but some
packages on hackage need at least 3.14 now.

pkgs/development/haskell-modules/configuration-common.nix
@@ -144,6 +144,8 @@ with haskellLib;
         cabal-install-solver = super.cabal-install-solver.overrideScope cabalInstallOverlay;
 
         cabal2nix-unstable = super.cabal2nix-unstable.overrideScope cabalInstallOverlay;
+        distribution-nixpkgs-unstable = super.distribution-nixpkgs-unstable.overrideScope cabalInstallOverlay;
+        hackage-db-unstable = super.hackage-db-unstable.overrideScope cabalInstallOverlay;
 
         # Needs cabal-install >= 3.8 /as well as/ matching Cabal
         guardian = lib.pipe (super.guardian.overrideScope cabalInstallOverlay) [
@@ -156,6 +158,9 @@ with haskellLib;
     )
     cabal-install
     cabal-install-solver
+    cabal2nix-unstable
+    distribution-nixpkgs-unstable
+    hackage-db-unstable
     guardian
     ;
 