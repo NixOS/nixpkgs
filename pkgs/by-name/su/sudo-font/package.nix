2de6a9f5abbe68b956f8ec26f61f2eae8082f5df
SkohTV <contact@skoh.dev>
2026-04-01 14:52:19 -0400

    sudo-font: adopt by pancaek

--- pkgs/by-name/su/sudo-font/package.nix
+++ pkgs/by-name/su/sudo-font/package.nix
@@ -26,7 +26,7 @@ stdenvNoCC.mkDerivation rec {
     homepage = "https://www.kutilek.de/sudo-font/";
     changelog = "https://github.com/jenskutilek/sudo-font/raw/v${version}/sudo/FONTLOG.txt";
     license = lib.licenses.ofl;
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ pancaek ];
     platforms = lib.platforms.all;
   };
 }
