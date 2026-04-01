ac765da32fa350fc8582d4c59d632cafa53f2c0b
SkohTV <contact@skoh.dev>
2026-04-01 14:53:39 -0400

    sudo-font: switch to finalAttrs

--- pkgs/by-name/su/sudo-font/package.nix
+++ pkgs/by-name/su/sudo-font/package.nix
@@ -5,12 +5,12 @@
   installFonts,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "sudo-font";
   version = "3.4";
 
   src = fetchzip {
-    url = "https://github.com/jenskutilek/sudo-font/releases/download/v${version}/sudo.zip";
+    url = "https://github.com/jenskutilek/sudo-font/releases/download/v${finalAttrs.version}/sudo.zip";
     hash = "sha256-sSLY94wY9+AYAqWDq+Xy+KctUfJVS0jeS3baF8mLO9I=";
   };
 
@@ -19,9 +19,9 @@ stdenvNoCC.mkDerivation rec {
   meta = {
     description = "Font for programmers and command line users";
     homepage = "https://www.kutilek.de/sudo-font/";
-    changelog = "https://github.com/jenskutilek/sudo-font/raw/v${version}/sudo/FONTLOG.txt";
+    changelog = "https://github.com/jenskutilek/sudo-font/raw/v${finalAttrs.version}/sudo/FONTLOG.txt";
     license = lib.licenses.ofl;
     maintainers = with lib.maintainers; [ pancaek ];
     platforms = lib.platforms.all;
   };
-}
+})
