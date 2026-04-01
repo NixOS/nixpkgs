a028988cde34931aa94ccb6a4bddd766acb0d542
Passeriform <bhardwajutkarsh.ub@gmail.com>
2026-04-02 04:44:10 +0530

    ghostline-cursor-theme: init at 1.1

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20751,6 +20751,12 @@
     githubId = 6931743;
     name = "pasqui23";
   };
+  passeriform = {
+    email = "bhardwajutkarsh.ub@gmail.com";
+    github = "Passeriform";
+    githubId = 32395996;
+    name = "Passeriform";
+  };
   passivelemon = {
     email = "jeremyseber@gmail.com";
     github = "PassiveLemon";
