7e4b58921565b8b7dbe711c9b7f2b622aa80c2f9
SkohTV <contact@skoh.dev>
2026-04-01 09:29:10 -0400

    smiley-sans: switch to finalAttrs

--- pkgs/by-name/sm/smiley-sans/package.nix
+++ pkgs/by-name/sm/smiley-sans/package.nix
@@ -6,12 +6,12 @@
   installFonts,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "smiley-sans";
   version = "2.0.1";
 
   src = fetchzip {
-    url = "https://github.com/atelier-anchor/smiley-sans/releases/download/v${version}/smiley-sans-v${version}.zip";
+    url = "https://github.com/atelier-anchor/smiley-sans/releases/download/v${finalAttrs.version}/smiley-sans-v${finalAttrs.version}.zip";
     sha256 = "sha256-p6DwX5MBPemAfV99L9ayLkEWro31ip4tf+wBQr8mkbs=";
     stripRoot = false;
   };
@@ -33,4 +33,4 @@ stdenvNoCC.mkDerivation rec {
     maintainers = with lib.maintainers; [ pancaek ];
     platforms = lib.platforms.all;
   };
-}
+})
