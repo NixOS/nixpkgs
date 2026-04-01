efff1bce51ba66e821f011c012ab69bac10b4fe3
SkohTV <contact@skoh.dev>
2026-04-01 14:56:34 -0400

    jost: switch to finalAttrs

--- pkgs/by-name/jo/jost/package.nix
+++ pkgs/by-name/jo/jost/package.nix
@@ -5,12 +5,12 @@
   installFonts,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "jost";
   version = "3.5";
 
   src = fetchzip {
-    url = "https://github.com/indestructible-type/Jost/releases/download/${version}/Jost.zip";
+    url = "https://github.com/indestructible-type/Jost/releases/download/${finalAttrs.version}/Jost.zip";
     hash = "sha256-ne81bMhmTzNZ/GGIzb7nCYh19vNLK+hJ3cP/zDxtiGM=";
   };
 
@@ -22,4 +22,4 @@ stdenvNoCC.mkDerivation rec {
     license = lib.licenses.ofl;
     maintainers = [ lib.maintainers.ar1a ];
   };
-}
+})
