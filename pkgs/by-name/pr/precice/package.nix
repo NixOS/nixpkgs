9cb526c4fa307451682d5ffac195ababc624d0ba
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-02 14:02:14 +0000

    precice: 3.3.1 -> 3.4.0

--- pkgs/by-name/pr/precice/package.nix
+++ pkgs/by-name/pr/precice/package.nix
@@ -18,13 +18,13 @@ assert petsc.mpiSupport;
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "precice";
-  version = "3.3.1";
+  version = "3.4.0";
 
   src = fetchFromGitHub {
     owner = "precice";
     repo = "precice";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-3Cg9e1n/O1kG806X3PT7IBd6QuBM38qj9BTlzYZqfSs=";
+    hash = "sha256-9aontvyKZa8fOwAIwzxmhq2Jb/GGtQhPqTQtE7LQjgI=";
   };
 
   cmakeFlags = [
