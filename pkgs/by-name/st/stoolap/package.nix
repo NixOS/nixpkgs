2b2609b833b95b9b3c6654023596495f4d0eb1b5
VZstless <i@vzstless.moe>
2026-04-02 10:52:01 +0800

    stoolap: 0.3.7 -> 0.4.0, add update script

--- pkgs/by-name/st/stoolap/package.nix
+++ pkgs/by-name/st/stoolap/package.nix
@@ -3,25 +3,28 @@
   rustPlatform,
   fetchFromGitHub,
   stdenv,
+  nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "stoolap";
-  version = "0.3.7";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "stoolap";
     repo = "stoolap";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-NfGs9TDyX+8hC2bCGJL0AWFd3C1joowT061vea5hxx0=";
+    hash = "sha256-TE16vsLzhwmqZRZrmWx8ikv2HJbB4sAXaKSPPNsMeLw=";
   };
 
-  cargoHash = "sha256-tzgxffwXd331Sz1xftXNBowqud29pKvbw+Epv01xOiQ=";
+  cargoHash = "sha256-ZWu1uu607n3wl3k7xcpS7cHbX7mifAX9gvo8KQmCB/E=";
 
   # On aarch64-darwin, dev target needs to set panic strategy to abort
   # However this must be set while the flag `-Zpanic_abort_tests` is also set,
   # which could only be done in Rust nightly toolchain.
   doCheck = !(with stdenv.hostPlatform; isDarwin && isAarch64);
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Modern Embedded SQL Database written in Rust";
