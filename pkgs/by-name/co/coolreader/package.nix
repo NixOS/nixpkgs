a054da4484c2dfad71636bfa948a4330ff70dbcb
Guy Chronister <guychronister@outlook.com>
2026-04-03 08:42:44 -0500

    coolreader: modernize derivation

--- pkgs/by-name/co/coolreader/package.nix
+++ pkgs/by-name/co/coolreader/package.nix
@@ -17,8 +17,8 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "buggins";
     repo = "coolreader";
-    rev = "cr${finalAttrs.version}";
-    sha256 = "sha256-RgVEOaNBaEuPBC75B8PdCkbqMvEzNmnEYmiI1ny/WFQ=";
+    tag = "cr${finalAttrs.version}";
+    hash = "sha256-RgVEOaNBaEuPBC75B8PdCkbqMvEzNmnEYmiI1ny/WFQ=";
   };
 
   patches = [ ./cmake_policy_version_3_5.patch ];
