1acb855c2872db63fe2d07eb7aad5780e4397ec4
Chloe A <chloe@sapphic.moe>
2026-04-04 03:08:27 +0500

    chiri: init at 0.8.0

---
+++ pkgs/by-name/ch/chiri/package.nix
@@ -0,0 +1,111 @@
+{
+  lib,
+  stdenv,
+  rustPlatform,
+  fetchFromGitHub,
+
+  # build tools
+  cargo-tauri,
+  nodejs_20,
+  pnpmConfigHook,
+  pnpm_9,
+  fetchPnpmDeps,
+  pkg-config,
+  makeBinaryWrapper,
+  wrapGAppsHook4,
+
+  # Linux dependencies
+  glib-networking,
+  libayatana-appindicator,
+  openssl,
+  webkitgtk_4_1,
+
+  # macOS dependencies
+  libiconv,
+  apple-sdk_14,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "chiri";
+  version = "0.8.0";
+
+  src = fetchFromGitHub {
+    owner = "SapphoSys";
+    repo = "chiri";
+    tag = "app-v${finalAttrs.version}";
+    hash = "sha256-VrENUwkItT+8C7JowoEfqjIX4RhThTm+4hntdm9ifVk=";
+  };
+
+  cargoHash = "sha256-2CDwuZiE4b5cBUPZs8l4pf9/FyvtSpRwNwQZ5gp85zc=";
+
+  pnpmDeps = fetchPnpmDeps {
+    inherit (finalAttrs) pname version src;
+    pnpm = pnpm_9;
+    hash = "sha256-3tUuOSW4SdcEh/dtmu2YLuTFf3mLncBo3xwPU8KA5Pw=";
+    fetcherVersion = 3;
+  };
+
+  nativeBuildInputs = [
+    cargo-tauri.hook
+    nodejs_20
+    pnpmConfigHook
+    pnpm_9
+    rustPlatform.cargoSetupHook
+    pkg-config
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    wrapGAppsHook4
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    makeBinaryWrapper
+  ];
+
+  buildInputs = [
+    openssl
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    glib-networking
+    libayatana-appindicator
+    webkitgtk_4_1
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    libiconv
+    apple-sdk_14
+  ];
+
+  cargoRoot = "src-tauri";
+  buildAndTestSubdir = "src-tauri";
+
+  postPatch =
+    lib.optionalString stdenv.hostPlatform.isLinux ''
+      substituteInPlace $cargoDepsCopy/libappindicator-sys-*/src/lib.rs \
+        --replace-fail "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1"
+    ''
+    + ''
+      substituteInPlace src-tauri/tauri.conf.json \
+        --replace-fail '"createUpdaterArtifacts": true' '"createUpdaterArtifacts": false'
+    '';
+
+  preBuild = ''
+    unset TAURI_SIGNING_PRIVATE_KEY
+    unset TAURI_SIGNING_PUBLIC_KEY
+    pnpm build
+  '';
+
+  postInstall = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    mkdir -p $out/bin
+    makeWrapper "$out/Applications/Chiri.app/Contents/MacOS/chiri" "$out/bin/chiri"
+  '';
+
+  doCheck = false;
+
+  meta = {
+    description = "Cross-platform CalDAV task management app";
+    homepage = "https://github.com/SapphoSys/chiri";
+    changelog = "https://github.com/SapphoSys/chiri/releases/tag/app-v${finalAttrs.version}";
+    license = lib.licenses.zlib;
+    maintainers = with lib.maintainers; [ SapphoSys ];
+    mainProgram = "chiri";
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+})
