93f9592c2c5bb9aca8c32d42e6657db31bddc20a
phaer <phaer@phaer.org>
2026-04-03 17:30:13 +0200

    nixos/getty: use 'or false' for boot.isContainer read
    boot.isContainer is defined in virtualisation/nixos-containers.nix.
    Guard reads so this module works without the containers module loaded.

--- nixos/modules/services/ttys/getty.nix
+++ nixos/modules/services/ttys/getty.nix
@@ -211,7 +211,7 @@ in
       ];
       serviceConfig.Restart = "always";
       restartIfChanged = false;
-      enable = mkDefault config.boot.isContainer;
+      enable = mkDefault (config.boot.isContainer or false);
     };
 
     environment.etc.issue = mkDefault {
