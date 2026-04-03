3f672122e2c4b2db121cfd32fdab8766f61c64c4
SystematicError <systematicerror@users.noreply.github.com>
2026-04-03 17:04:48 +0100

    times-newer-roman: use installFonts

--- pkgs/by-name/ti/times-newer-roman/package.nix
+++ pkgs/by-name/ti/times-newer-roman/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenvNoCC,
   fetchzip,
+  installFonts,
 }:
 
 stdenvNoCC.mkDerivation {
@@ -14,14 +15,7 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-wO4rxyJNQyhRLpswCYKXdeiXy5G+iWyxulYCHZb60QM=";
   };
 
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/share/fonts/opentype
-    cp *.otf $out/share/fonts/opentype
-
-    runHook postInstall
-  '';
+  nativeBuildInputs = [ installFonts ];
 
   meta = {
     description = "Font that looks just like Times New Roman, except each character is 5-10% wider";
