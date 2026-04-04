fc9ee662dc9e55a24e1e4fcf504cc0596ee3485f
whispers <whispersofthedawn@duck.com>
2026-04-04 12:58:05 -0400

    mullvad-browser: 15.0.8 -> 15.0.9
    Changelog: https://gitlab.torproject.org/tpo/applications/tor-browser-build/-/raw/tbb-15.0.9-build1/projects/browser/Bundle-Data/Docs-TBB/ChangeLog.txt
    Firefox ESR: https://www.firefox.com/en-US/firefox/140.9.1/releasenotes/
    Firefox diff: https://gitlab.torproject.org/tpo/applications/tor-browser/-/compare/FIREFOX_140_9_0esr_BUILD1..FIREFOX_140_9_1esr_BUILD1
    
    (cherry picked from commit 8078833fb58b6bc66af03f11696a9e7e1f1fa4d0)

--- pkgs/by-name/mu/mullvad-browser/package.nix
+++ pkgs/by-name/mu/mullvad-browser/package.nix
@@ -97,7 +97,7 @@ let
     ++ lib.optionals mediaSupport [ ffmpeg_7 ]
   );
 
-  version = "15.0.8";
+  version = "15.0.9";
 
   sources = {
     x86_64-linux = fetchurl {
@@ -109,7 +109,7 @@ let
         "https://tor.eff.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
         "https://tor.calyxinstitute.org/dist/mullvadbrowser/${version}/mullvad-browser-linux-x86_64-${version}.tar.xz"
       ];
-      hash = "sha256-EuyDwgNoVECEko66xFnmteVNnPsn2POlXRU9iS9VPlo=";
+      hash = "sha256-GkLCztflyfRFZiZg+bZjUFvK9i+jbagyzB7SgbjKlpQ=";
     };
   };
 
