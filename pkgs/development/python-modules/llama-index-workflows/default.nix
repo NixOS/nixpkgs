d0e55e676e4f5010bc903e089277d65906f153cf
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-04 13:48:45 +0000

    python3Packages.llama-index-workflows: 2.16.1 -> 2.17.1

--- pkgs/development/python-modules/llama-index-workflows/default.nix
+++ pkgs/development/python-modules/llama-index-workflows/default.nix
@@ -10,13 +10,13 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "llama-index-workflows";
-  version = "2.16.1";
+  version = "2.17.1";
   pyproject = true;
 
   src = fetchPypi {
     pname = "llama_index_workflows";
     inherit (finalAttrs) version;
-    hash = "sha256-L2viI69AcITu8GS8vsj2CuTdAR2aBGMYh86YYlj6xYo=";
+    hash = "sha256-xi+r5QnPAAPd/lsrJ/SLNEPHyahOnNyQTG+e1fjL4l0=";
   };
 
   postPatch = ''
