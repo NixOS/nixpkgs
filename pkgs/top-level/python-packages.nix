7ec39c1e781eb8735d083538556ed0c55c64c380
n3tcat <webmaster@scd31.com>
2026-04-04 12:00:30 -0400

    error-helper: init at 1.5

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5078,6 +5078,8 @@ self: super: with self; {
 
   eradicate = callPackage ../development/python-modules/eradicate { };
 
+  error-helper = callPackage ../development/python-modules/error-helper { };
+
   es-client = callPackage ../development/python-modules/es-client { };
 
   escapism = callPackage ../development/python-modules/escapism { };
