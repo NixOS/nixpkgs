47c2924280d8384962db380bade060f39b27b303
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-04 22:17:27 +0000

    ludtwig: 0.12.0 -> 0.13.0

--- pkgs/by-name/lu/ludtwig/package.nix
+++ pkgs/by-name/lu/ludtwig/package.nix
@@ -6,18 +6,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "ludtwig";
-  version = "0.12.0";
+  version = "0.13.0";
 
   src = fetchFromGitHub {
     owner = "MalteJanz";
     repo = "ludtwig";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-zRroSPI41PU1sPwvKALWkydGWYJrz5uDJ94uimsCNys=";
+    hash = "sha256-czxfze7irABr5+iGwsJ7wR2kqRwDC6wOFGGxj1SUijw=";
   };
 
   checkType = "debug";
 
-  cargoHash = "sha256-cGxstBXNwyzQRNfedTbbO8Qf9C7VlMf2asz7VgQfZaA=";
+  cargoHash = "sha256-gSTWma7zZ4K425Tx8VSyEG5IBQnsoK+B2Rmt895b580=";
 
   meta = {
     description = "Linter / Formatter for Twig template files which respects HTML and your time";
