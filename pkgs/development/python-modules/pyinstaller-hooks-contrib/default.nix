a125c1a071e9a039c3b21794807406b5fd916de1
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-04 03:21:01 +0000

    python3Packages.pyinstaller-hooks-contrib: 2026.1 -> 2026.4

--- pkgs/development/python-modules/pyinstaller-hooks-contrib/default.nix
+++ pkgs/development/python-modules/pyinstaller-hooks-contrib/default.nix
@@ -7,14 +7,14 @@
 
 buildPythonPackage rec {
   pname = "pyinstaller-hooks-contrib";
-  version = "2026.1";
+  version = "2026.4";
 
   pyproject = true;
 
   src = fetchPypi {
     pname = "pyinstaller_hooks_contrib";
     inherit version;
-    hash = "sha256-pfCJGh6B6SQGq5F9nnat/XoraEFe4uNclQp7ORC8Nhs=";
+    hash = "sha256-dmwoGssezDLiHIxmcFbX6/XaCqvV4wwhn5wqKDYg7qo=";
   };
 
   build-system = [ setuptools ];
