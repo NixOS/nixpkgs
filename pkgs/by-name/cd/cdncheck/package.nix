87bd838158efd98809a12954d3592f3e010d974e
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-05 17:20:43 +0000

    cdncheck: 1.2.28 -> 1.2.30

--- pkgs/by-name/cd/cdncheck/package.nix
+++ pkgs/by-name/cd/cdncheck/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "cdncheck";
-  version = "1.2.28";
+  version = "1.2.30";
 
   src = fetchFromGitHub {
     owner = "projectdiscovery";
     repo = "cdncheck";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-G0h/0HEPE0kOPfCJIFCXZnqeyCYs7JdnlNjusk9+Nzw=";
+    hash = "sha256-CzTwax3XuUt5ZJsYXIbXHz3czev6natd1WsTQIrokEE=";
   };
 
-  vendorHash = "sha256-bYN119IyOkO9w+CWGwnCOXqpo4QHJV6iDuToMnTo0og=";
+  vendorHash = "sha256-z/wOCtd39ENUe8WQUst4uhl8R6RwCDdcUC4OZcRJWSs=";
 
   subPackages = [ "cmd/cdncheck/" ];
 
