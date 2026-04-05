68330f2a918f17c19dd97d21d1676b57f9ac8d0c
Nikita Rudenko <rudenkornk@gmail.com>
2026-04-05 16:44:24 +0300

    vimPlugins.duck-nvim : init at 1.0.0-unstable-2024-03-07
    https://github.com/tamton-aquib/duck.nvim

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -4848,6 +4848,19 @@ final: prev: {
     meta.hydraPlatforms = [ ];
   };
 
+  duck-nvim = buildVimPlugin {
+    pname = "duck.nvim";
+    version = "1.0.0-unstable-2024-03-07";
+    src = fetchFromGitHub {
+      owner = "tamton-aquib";
+      repo = "duck.nvim";
+      rev = "d8a6b08af440e5a0e2b3b357e2f78bb1883272cd";
+      hash = "sha256-97QSkZHpHLq1XyLNhPz88i9VuWy6ux7ZFNJx/g44K2A=";
+    };
+    meta.homepage = "https://github.com/tamton-aquib/duck.nvim/";
+    meta.hydraPlatforms = [ ];
+  };
+
   earthly-vim = buildVimPlugin {
     pname = "earthly.vim";
     version = "0-unstable-2024-04-02";
