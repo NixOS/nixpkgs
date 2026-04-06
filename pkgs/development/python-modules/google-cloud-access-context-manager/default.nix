4fc1ab02d0485182c9757ff1dae10d41e2021637
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-06 14:03:58 +0200

    python3Packages.google-cloud-access-context-manager: migrate to finalAttrs

--- pkgs/development/python-modules/google-cloud-access-context-manager/default.nix
+++ pkgs/development/python-modules/google-cloud-access-context-manager/default.nix
@@ -7,14 +7,14 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "google-cloud-access-context-manager";
   version = "0.4.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "google_cloud_access_context_manager";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-IbnGOozCLKe61oTaFZW19FkA0UO4D+fByc9e/4zzqrA=";
   };
 
@@ -38,8 +38,8 @@ buildPythonPackage rec {
   meta = {
     description = "Protobufs for Google Access Context Manager";
     homepage = "https://github.com/googleapis/google-cloud-python/tree/main/packages/google-cloud-access-context-manager";
-    changelog = "https://github.com/googleapis/google-cloud-python/blob/google-cloud-access-context-manager-v${version}/packages/google-cloud-access-context-manager/CHANGELOG.md";
+    changelog = "https://github.com/googleapis/google-cloud-python/blob/google-cloud-access-context-manager-v${finalAttrs.version}/packages/google-cloud-access-context-manager/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ austinbutler ];
   };
-}
+})
