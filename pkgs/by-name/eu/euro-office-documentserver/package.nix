83bb4f7df371c36b85bc8d8d387f2699d179a9e6
Jonas Heinrich <onny@project-insanity.org>
2026-04-06 22:44:32 +0200

    Update pkgs/by-name/eu/euro-office-documentserver/package.nix
    Co-authored-by: dotlambda <github@dotlambda.de>

--- pkgs/by-name/eu/euro-office-documentserver/package.nix
+++ pkgs/by-name/eu/euro-office-documentserver/package.nix
@@ -219,7 +219,7 @@ let
     };
 
     meta = {
-      description = "Euro-Office Document Server is an online office suite comprising viewers and editors";
+      description = "Online office suite comprising viewers and editors";
       longDescription = ''
         Euro-Office Document Server is an online office suite comprising viewers and editors for texts, spreadsheets and presentations,
         fully compatible with Office Open XML formats: .docx, .xlsx, .pptx and enabling collaborative editing in real time.
