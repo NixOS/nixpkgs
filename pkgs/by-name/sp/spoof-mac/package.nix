957f4b35eae119052c348d8137ae0b562cf5cf9c
Guy Chronister <guychronister@outlook.com>
2026-04-06 18:31:08 -0500

    spoof-mac: switch to hash, add missing version string

--- pkgs/by-name/sp/spoof-mac/package.nix
+++ pkgs/by-name/sp/spoof-mac/package.nix
@@ -6,14 +6,14 @@
 
 python3Packages.buildPythonPackage {
   pname = "spoof-mac";
-  version = "unstable-2018-01-27";
+  version = "0-unstable-2018-01-27";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "feross";
     repo = "SpoofMAC";
     rev = "2cfc796150ef48009e9b765fe733e37d82c901e0";
-    sha256 = "sha256-Qiu0URjUyx8QDVQQUFGxPax0J80e2m4+bPJeqFoKxX8=";
+    hash = "sha256-Qiu0URjUyx8QDVQQUFGxPax0J80e2m4+bPJeqFoKxX8=";
   };
 
   propagatedBuildInputs = [ python3Packages.docopt ];
