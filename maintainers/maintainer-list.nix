5fd89acd0e58b17637b508947ddc9c031434f12a
philip-730 <philip.amendolia@gmail.com>
2026-04-06 01:15:18 -0400

    maintainers: add philip-730

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21102,6 +21102,12 @@
     githubId = 1640697;
     name = "Philipp Hausmann";
   };
+  philip-730 = {
+    email = "philip.amendolia@gmail.com";
+    github = "philip-730";
+    githubId = 116534928;
+    name = "Philip Amendolia";
+  };
   philipdb = {
     email = "philipdb.art110@passmail.com";
     name = "Philip de Bruin";
