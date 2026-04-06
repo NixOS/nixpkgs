c826d429040caf83160431a79274fe47fa0935ad
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-06 00:00:48 +0000

    libretro.mame2003-plus: 0-unstable-2026-03-10 -> 0-unstable-2026-04-04

--- pkgs/applications/emulators/libretro/cores/mame2003-plus.nix
+++ pkgs/applications/emulators/libretro/cores/mame2003-plus.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "mame2003-plus";
-  version = "0-unstable-2026-03-10";
+  version = "0-unstable-2026-04-04";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "mame2003-plus-libretro";
-    rev = "53453e57676ff31131441f08c0b734c9ad84e4f0";
-    hash = "sha256-IpVv/bpT7AHF8QaGFGgTWei5JKuy7H5/O70W2n88s1c=";
+    rev = "3da943f4bcf264c7af83a07b78617e98ce3822cd";
+    hash = "sha256-3hJj8BLePPdEEsQriHSWhIMhamr7pvpKswP5/RxtjcM=";
   };
 
   makefile = "Makefile";
