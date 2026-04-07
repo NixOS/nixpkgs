1a20c79082e9b8fe079b69b69cba7c238a2601da
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-04-07 05:35:18 +0200

    suitesparse: remove ? null, remove empty list

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11485,9 +11485,7 @@ with pkgs;
     lapack = lapack-ilp64;
   };
 
-  suitesparse_5_3 = callPackage ../development/libraries/science/math/suitesparse {
-    inherit (llvmPackages) openmp;
-  };
+  suitesparse_5_3 = callPackage ../development/libraries/science/math/suitesparse { };
   suitesparse = suitesparse_5_3;
 
   trilinos-mpi = trilinos.override { withMPI = true; };
