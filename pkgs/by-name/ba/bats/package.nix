7ad32b3e2999d73b6c7f3ca123c51f3b7b90d4b0
Travis A. Everett <travis.a.everett@gmail.com>
2026-04-06 20:53:21 -0500

    bats: disable packcc test on darwin
    This is failing on darwin after latest bump. Hopefully just temporary.
    Separate commit for easier reversion later.

--- pkgs/by-name/ba/bats/package.nix
+++ pkgs/by-name/ba/bats/package.nix
@@ -246,13 +246,15 @@ resholve.mkDerivation rec {
     # to see when updates would break things, include packages
     # that use nixpkgs' bats for testing (as long as they
     # aren't massive builds)
-    inherit bash-preexec locate-dominating-file packcc;
+    inherit bash-preexec locate-dominating-file;
     resholve = resholve.tests.cli;
   }
   // lib.optionalAttrs (!stdenv.hostPlatform.isDarwin) {
-    # TODO: kikit's kicad dependency is marked broken on darwin atm
-    # may be able to fold this up if that resolves.
-    inherit kikit;
+    # TODO:
+    # - kikit's kicad dependency is marked broken on darwin atm
+    #   may be able to fold this up if that resolves.
+    # - packcc's tests currently broken on darwin (apr 2026)
+    inherit kikit packcc;
   };
 
   meta = {
