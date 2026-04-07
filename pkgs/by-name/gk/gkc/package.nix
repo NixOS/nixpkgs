62bec2478086e4afde778a513b31a1b582b0579e
gkiviv <georgkivivali7@gmail.com>
2026-04-07 11:48:49 +0300

    gkc: init at 0.6.0

---
+++ pkgs/by-name/gk/gkc/package.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  bison,
+  flex,
+}:
+stdenv.mkDerivation {
+  pname = "gkc";
+  version = "0.6.0";
+
+  src = fetchFromGitHub {
+    owner = "tammet";
+    repo = "gkc";
+    tag = "v0.6.0";
+    hash = "sha256-1hVE2IrUSDrpE1en+NPU4YCncBSqqS+fTz58r+P7wAE=";
+  };
+
+  nativeBuildInputs = [
+    bison
+    flex
+  ];
+
+  env.NIX_CFLAGS_COMPILE = "-fcommon";
+
+  postPatch = ''
+    substituteInPlace makefile --replace-fail "-static" ""
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp gkc $out/bin
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "A reasoning system for large knowledge bases. ";
+    homepage = "https://github.com/tammet/gkc";
+    license = lib.licenses.agpl3Only;
+    maintainers = [ lib.maintainers.thngz ];
+    platforms = lib.platforms.unix;
+    mainProgram = "gkc";
+  };
+}
