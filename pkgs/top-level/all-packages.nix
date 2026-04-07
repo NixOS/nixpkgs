eaa9da0b5f0ae060d0556c1377826ad3f44e1151
Guy Chronister <guychronister@outlook.com>
2026-04-07 09:38:36 -0500

    kuma-cp: migrate to by-name, preserve override behavior

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9605,10 +9605,6 @@ with pkgs;
   linkerd_edge = callPackage ../applications/networking/cluster/linkerd/edge.nix { };
   linkerd_stable = linkerd;
 
-  kuma-cp = callPackage ../applications/networking/cluster/kuma {
-    components = [ "kuma-cp" ];
-    pname = "kuma-cp";
-  };
   kuma-dp = callPackage ../applications/networking/cluster/kuma {
     components = [ "kuma-dp" ];
     pname = "kuma-dp";
