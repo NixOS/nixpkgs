8f8c170c8628ca864d41eb80078147f828cc37db
Erin of Yukis <erin-dev@ninetailed.ninja>
2026-04-07 04:06:13 +0200

    nixos/firefox-syncserver: Allow configuring syncserver with PostgreSQL database

---
+++ nixos/tests/firefox-syncserver-mariadb.nix
@@ -0,0 +1,33 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  name = "firefox-syncserver";
+  nodes.machine = {
+    services.mysql = {
+      enable = true;
+      package = pkgs.mariadb;
+    };
+
+    services.firefox-syncserver = {
+      enable = true;
+      secrets = pkgs.writeText "secret" "this-is-a-test";
+      database.type = "mysql";
+      singleNode = {
+        enable = true;
+        hostname = "firefox-syncserver.local";
+        capacity = 1;
+      };
+    };
+  };
+
+  testScript = ''
+    machine.wait_for_unit("firefox-syncserver.service")
+    machine.wait_for_open_port(5000)
+
+    machine.wait_until_succeeds("curl --fail http://127.0.0.1:5000")
+
+  '';
+}
