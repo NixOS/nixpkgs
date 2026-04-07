e3b21a7341d6491b98319a3798ca99d61f07f55c
Jared Baur <jaredbaur@fastmail.com>
2026-04-06 22:08:01 -0700

    python3Packages.peakrdl-cheader: 1.0.0 -> 1.1.0
    This includes a change in license to lgpl3Only.

--- pkgs/development/python-modules/peakrdl-cheader/default.nix
+++ pkgs/development/python-modules/peakrdl-cheader/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "peakrdl-cheader";
-  version = "1.0.0";
+  version = "1.1.0";
 
   pyproject = true;
 
@@ -20,7 +20,7 @@ buildPythonPackage (finalAttrs: {
     owner = "SystemRDL";
     repo = "PeakRDL-cheader";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-1LxKGCea5ClKmrArl+CM6ZRpiTh2ThbYSe9TYYHjRlY=";
+    hash = "sha256-IPGNauPA9y1HNEbk3eEOog17++/gSJt+185i+DFb54U=";
   };
 
   build-system = [
@@ -39,7 +39,7 @@ buildPythonPackage (finalAttrs: {
   meta = {
     description = "C Header generator for a SystemRDL definition";
     homepage = "https://peakrdl-cheader.readthedocs.io/";
-    license = lib.licenses.lgpl3;
+    license = lib.licenses.lgpl3Only;
     maintainers = [ lib.maintainers.jmbaur ];
   };
 })
