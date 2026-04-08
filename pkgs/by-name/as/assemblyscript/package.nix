6d954779b0008c582b21e309675084aa96aa9be8
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-08 10:55:08 +0000

    assemblyscript: 0.28.2 -> 0.28.13

--- pkgs/by-name/as/assemblyscript/package.nix
+++ pkgs/by-name/as/assemblyscript/package.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "assemblyscript";
-  version = "0.28.2";
+  version = "0.28.13";
 
   src = fetchFromGitHub {
     owner = "AssemblyScript";
     repo = "assemblyscript";
     rev = "v${version}";
-    hash = "sha256-8nY3Rst6i+RGW+kfjn/QSbYkaZf67k/KwuyIp40fCSM=";
+    hash = "sha256-AN9W6IYmMFFiD1Au3uW+7jr2xOwVcOGd5e61cqVNnbs=";
   };
 
-  npmDepsHash = "sha256-wj/PMoqUBjXqz8/KOFAH5P5eFty3pztpQ8inGIy+ve4=";
+  npmDepsHash = "sha256-5s420YWTc4bZ5oZRBjinw4lAfMTPrnrIWx4IM3Ysuqo=";
 
   meta = {
     homepage = "https://github.com/AssemblyScript/assemblyscript";
