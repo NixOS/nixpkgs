e7d5a765dec7fd1d4b99928328b6efbfb804757c
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-08 12:03:07 +0200

    python3Packages.mypy-boto3-outposts: 1.42.3 -> 1.42.85

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -990,8 +990,8 @@ in
       "sha256-+t1Mh2gV7wu5YAFzp0jABFUC6/8P/FHMnBCHilIFXac=";
 
   mypy-boto3-outposts =
-    buildMypyBoto3Package "outposts" "1.42.3"
-      "sha256-4+T+yIICXzW6IX90M8y5eOL8jT9r0cJhxeI94SxX6vE=";
+    buildMypyBoto3Package "outposts" "1.42.85"
+      "sha256-SB07dOhPo1fFcd+9N9al/tYG1GV8NQcwz8W4d61i65c=";
 
   mypy-boto3-panorama =
     buildMypyBoto3Package "panorama" "1.42.3"
