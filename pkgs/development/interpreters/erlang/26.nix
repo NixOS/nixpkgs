1723d995c19805999e32ff9d6eefe2943a918661
Adam C. Stephens <adam@valkor.net>
2026-04-08 00:05:52 -0400

    erlang_26: 26.2.5.18 -> 26.2.5.19
    Changelog: https://github.com/erlang/otp/releases/tag/OTP-26.2.5.19

--- pkgs/development/interpreters/erlang/26.nix
+++ pkgs/development/interpreters/erlang/26.nix
@@ -1,6 +1,6 @@
 genericBuilder:
 
 genericBuilder {
-  version = "26.2.5.18";
-  hash = "sha256-mCkJeRf6PsMsVJXBIpPoHMff5JyVXLSzPzCyLiJtgJo=";
+  version = "26.2.5.19";
+  hash = "sha256-n9YQXuPmNtGin/+Sa9PBRfP9sNP6YfefCswzvEhYOlQ=";
 }
