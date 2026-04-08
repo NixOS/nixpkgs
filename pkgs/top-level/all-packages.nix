d101bd5454f6ba63556c0dbbf5f3f7e9125d59a6
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-04-08 02:36:34 +0200

    suitesparse_5_3: drop alias

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11485,8 +11485,7 @@ with pkgs;
     lapack = lapack-ilp64;
   };
 
-  suitesparse_5_3 = callPackage ../development/libraries/science/math/suitesparse { };
-  suitesparse = suitesparse_5_3;
+  suitesparse = callPackage ../development/libraries/science/math/suitesparse { };
 
   trilinos-mpi = trilinos.override { withMPI = true; };
 
