a4001c546570c536cf5d4ad4d97dc77f1f4b3203
K900 <me@0upti.me>
2026-04-08 09:14:37 +0300

    libfyaml: cherry-pick fixes for 32-bit builds

--- pkgs/by-name/li/libfyaml/package.nix
+++ pkgs/by-name/li/libfyaml/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   pkg-config,
   autoreconfHook,
   testers,
@@ -17,6 +18,18 @@ stdenv.mkDerivation (finalAttrs: {
     rev = "v${finalAttrs.version}";
     hash = "sha256-mRQQe+J5wtLt/bI/Wer9TVGdU3a1zp3zFCm4oNQON8M=";
   };
+
+  # backport 32-bit build fixes
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/pantoniou/libfyaml/commit/0982fcefc6a16d4c8cb5b06747d3fc8e630de3ae.diff";
+      hash = "sha256-aDubIn+et+1fWE7XU7a5AGZVacVFbAbC1PoSDrA6hXw=";
+    })
+    (fetchpatch {
+      url = "https://github.com/pantoniou/libfyaml/commit/9192deaac095f9881cc1e5756dede683f36b09d6.diff";
+      hash = "sha256-cNL9wQtxIRg/ShZLJP4qHYNFRrYo9kRG+/U+3FiUeaI=";
+    })
+  ];
 
   nativeBuildInputs = [
     autoreconfHook
