3a493132b09528c1d032435e795fcba4c4f81e02
Guy Chronister <guychronister@outlook.com>
2026-04-09 11:25:56 -0500

    torrentstream: migrate from rev to tag

--- pkgs/by-name/to/torrentstream/package.nix
+++ pkgs/by-name/to/torrentstream/package.nix
@@ -12,7 +12,7 @@ buildDotnetModule rec {
   src = fetchFromGitHub {
     owner = "trueromanus";
     repo = "TorrentStream";
-    rev = version;
+    tag = version;
     hash = "sha256-3lmQWx00Ulp0ZyQBEhFT+djHBi84foMlWGJEp/UOGek=";
   };
 
