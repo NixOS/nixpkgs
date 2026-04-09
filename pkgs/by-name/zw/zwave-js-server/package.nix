208ffcb40a793a1b46f7a2dbed576a30ae724533
Guy Chronister <guychronister@outlook.com>
2026-04-09 11:26:11 -0500

    zwave-js-server: migrate from rev to tag

--- pkgs/by-name/zw/zwave-js-server/package.nix
+++ pkgs/by-name/zw/zwave-js-server/package.nix
@@ -12,7 +12,7 @@ buildNpmPackage rec {
   src = fetchFromGitHub {
     owner = "zwave-js";
     repo = "zwave-js-server";
-    rev = version;
+    tag = version;
     hash = "sha256-JmPO1faJgpJ+RjocvauP0EQGken61G59CLqQAZiRSUU=";
   };
 
