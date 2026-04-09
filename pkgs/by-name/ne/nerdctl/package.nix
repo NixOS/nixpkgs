1d271e09f157c5a6dca80dc83555d2ad1ef3b6c3
Ton Sharp <45160296+66Ton99@users.noreply.github.com>
2026-04-10 01:29:47 +0300

    nerdctl: use home data root on darwin

--- pkgs/by-name/ne/nerdctl/package.nix
+++ pkgs/by-name/ne/nerdctl/package.nix
@@ -49,6 +49,88 @@ buildGoModule (finalAttrs: {
   # Many checks require a containerd socket and running nerdctl after it's built
   doCheck = false;
 
+  postPatch = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    cat > pkg/defaults/defaults_darwin.go <<'EOF'
+    /*
+       Copyright The containerd Authors.
+
+       Licensed under the Apache License, Version 2.0 (the "License");
+       you may not use this file except in compliance with the License.
+       You may obtain a copy of the License at
+
+           http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing, software
+       distributed under the License is distributed on an "AS IS" BASIS,
+       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+       See the License for the specific language governing permissions and
+       limitations under the License.
+    */
+
+    // This is a dummy file to allow usage of library functions
+    // on Darwin-based systems.
+    // All functions and variables are empty/no-ops
+
+    package defaults
+
+    import (
+      "os"
+      "path/filepath"
+
+      gocni "github.com/containerd/go-cni"
+    )
+
+    const (
+      AppArmorProfileName = ""
+      SeccompProfileName  = ""
+      Runtime             = ""
+    )
+
+    func CNIPath() string {
+      return gocni.DefaultCNIDir
+    }
+
+    func CNIRuntimeDir() (string, error) {
+      return "/var/run/cni", nil
+    }
+
+    func CNINetConfPath() string {
+      return gocni.DefaultNetDir
+    }
+
+    func DataRoot() string {
+      if home, err := os.UserHomeDir(); err == nil && home != "" {
+        return filepath.Join(home, ".local", "share", "nerdctl")
+      }
+      return "/var/lib/nerdctl"
+    }
+
+    func CgroupManager() string {
+      return ""
+    }
+
+    func CgroupnsMode() string {
+      return ""
+    }
+
+    func NerdctlTOML() string {
+      return "/etc/nerdctl/nerdctl.toml"
+    }
+
+    func HostsDirs() []string {
+      return []string{}
+    }
+
+    func HostGatewayIP() string {
+      return ""
+    }
+
+    func CDISpecDirs() []string {
+      return []string{"/etc/cdi", "/var/run/cdi"}
+    }
+    EOF
+  '';
+
   postInstall = ''
     wrapProgram $out/bin/nerdctl \
       --prefix PATH : "${lib.makeBinPath ([ buildkit ] ++ extraPackages)}"${lib.optionalString stdenv.hostPlatform.isLinux '' \
