09f423ded9a90f17d655c5aa7e6fcb2dd58db36f
Guy Chronister <guychronister@outlook.com>
2026-04-09 11:25:39 -0500

    protege: migrate from rev to tag

--- pkgs/by-name/pr/protege/package.nix
+++ pkgs/by-name/pr/protege/package.nix
@@ -16,7 +16,7 @@ maven.buildMavenPackage rec {
   src = fetchFromGitHub {
     owner = "protegeproject";
     repo = "protege";
-    rev = version;
+    tag = version;
     hash = "sha256-GplMEVEBYSTTzrGzbHlbQTXqJYka6r0QfBZFVCS7wCs=";
   };
 
