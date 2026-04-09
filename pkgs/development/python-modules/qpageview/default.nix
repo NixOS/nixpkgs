c0e00ffeae8ab6304d453be41a756870ca3b56ca
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-09 20:40:16 +0000

    python3Packages.qpageview: 1.0.3 -> 1.0.4

--- pkgs/development/python-modules/qpageview/default.nix
+++ pkgs/development/python-modules/qpageview/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "qpageview";
-  version = "1.0.3";
+  version = "1.0.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "frescobaldi";
     repo = "qpageview";
     tag = "v${version}";
-    hash = "sha256-cKidVtsqNXGuWNTTgvVOP3Wyf6M+Xctwaq7pOZb8eeo=";
+    hash = "sha256-PqoYxSYOsR/tsV3NZmxCtK1fKQuo8WUJZVBSNRjVNLw=";
   };
 
   build-system = [ hatchling ];
