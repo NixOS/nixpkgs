93ebad76bcd640742e0885ac97cdadc0831e3cec
Marco "Capypara" Köpcke <hello@capypara.de>
2026-04-09 17:46:30 +0200

    jetbrains.oss: switch to builds using bazel, bazel_8: rework buildPackage
    This is a combination of various commits to get the JetBrains IDEs built from source using Bazel 8.
    See https://github.com/theCapypara/nixpkgs/tree/jetbrains/oss-26.1
    
    Co-Authored-By: Dmitry Ivankov <boltzmannrain@gmail.com>

--- pkgs/by-name/ba/bazel_8/build-support/bazelPackage.nix
+++ pkgs/by-name/ba/bazel_8/build-support/bazelPackage.nix
@@ -1,4 +1,6 @@
 {
+  bash,
+  coreutils,
   callPackage,
   gnugrep,
   lib,
@@ -22,14 +24,15 @@
     outputHash = null;
     outputHashAlgo = "sha256";
   },
-  bazelVendorDepsFOD ? {
-    outputHash = null;
-    outputHashAlgo = "sha256";
-  },
+  postUnpack ? null,
+  bazelPreBuild ? "", # TODO: find a better interface, currently whether it is before or mkdir isn't clear
+  patches ? [ ],
   installPhase,
   buildInputs ? [ ],
   nativeBuildInputs ? [ ],
   autoPatchelfIgnoreMissingDeps ? null,
+  autoPatchelfVendorDirs ? [ ],
+  passthru ? { },
 }:
 let
   # FOD produced by `bazel fetch`
@@ -39,13 +42,19 @@ let
   #        isn't stable too. Parsing it into nix fetch* commands isn't trivial but might be possible)
   bazelRepoCache =
     if bazelRepoCacheFOD.outputHash == null then
+      # TODO: make repo cache required now that vendor FOD is removed?
       null
     else
       (callPackage ./bazelDerivation.nix { } {
+        # TODO: get rid of following steps to avoid possible bazel content-addressing issues?
+        # - shrinking RPATHs of ELF executables and libraries
+        # - patching script interpreter paths
         name = "bazelRepoCache";
         inherit (bazelRepoCacheFOD) outputHash outputHashAlgo;
         inherit
           src
+          postUnpack
+          patches
           version
           sourceRoot
           env
@@ -64,13 +73,13 @@ let
         commandArgs = [ "--repository_cache=repo_cache" ] ++ commandArgs;
         bazelPreBuild = ''
           mkdir repo_cache
-        '';
+        ''
+        + bazelPreBuild;
         installPhase = ''
           mkdir -p $out/repo_cache
           cp -r --reflink=auto repo_cache/* $out/repo_cache
         '';
       });
-  # Stage1: FOD produced by `bazel vendor`, Stage2: eventual patchelf or other tuning
   # Vendor deps contains unpacked&patches external dependencies, this may need Nix-specific
   # patching to address things like
   # - broken symlinks
@@ -78,87 +87,90 @@ let
   # - autoPatchelf for externally-fetched binaries
   #
   # Either repo cache or vendor deps should be enough to build a given package
-  bazelVendorDeps =
-    if bazelVendorDepsFOD.outputHash == null then
-      null
-    else
-      (
-        let
-          stage1 = callPackage ./bazelDerivation.nix { } {
-            name = "bazelVendorDepsStage1";
-            inherit (bazelVendorDepsFOD) outputHash outputHashAlgo;
-            inherit
-              src
-              version
-              sourceRoot
-              env
-              buildInputs
-              nativeBuildInputs
-              ;
-            inherit registry;
-            inherit
-              bazel
-              targets
-              startupArgs
-              serverJavabase
-              ;
-            dontFixup = true;
-            command = "vendor";
-            outputHashMode = "recursive";
-            commandArgs = [ "--vendor_dir=vendor_dir" ] ++ commandArgs;
-            bazelPreBuild = ''
-              mkdir vendor_dir
-            '';
-            bazelPostBuild = ''
-              # remove symlinks that point to locations under bazel_src/
-              find vendor_dir -type l -lname "$HOME/*" -exec rm '{}' \;
-              # remove symlinks to temp build directory on darwin
-              find vendor_dir -type l -lname "/private/var/tmp/*" -exec rm '{}' \;
-              # remove broken symlinks
-              find vendor_dir -xtype l -exec rm '{}' \;
-
-              # remove .marker files referencing NIX_STORE as those references aren't allowed in FOD
-              (${gnugrep}/bin/grep -rI "$NIX_STORE/" vendor_dir --files-with-matches --include="*.marker" --null || true) \
-                | xargs -0 --no-run-if-empty rm
-            '';
-            installPhase = ''
-              mkdir -p $out/vendor_dir
-              cp -r --reflink=auto vendor_dir/* $out/vendor_dir
-            '';
-
-          };
-        in
-        stdenv.mkDerivation {
-          name = "bazelVendorDeps";
-          buildInputs = lib.optional (!stdenv.hostPlatform.isDarwin) autoPatchelfHook ++ buildInputs;
-          inherit autoPatchelfIgnoreMissingDeps;
-          src = stage1;
-          installPhase = ''
-            cp -r . $out
-          '';
-        }
-      );
-
+  # TODO: make vendoring stage opt-in if no patching is needed
+  # TODO: also consider simple text patching ability via registry rewrites
+  #
+  # TODO: maybe vendorDeps should be fused with the main build? It's not a FOD anymore so why not
+  #       consider patching a patch phase during package build step? Would save disk space - repoCache
+  #       can be often compressed, vendorDeps are uncompressed and cover same content, modulo applied patches.
+  #       As opt-in it might be useful to grab patched vendor deps to debug outside nix build?
   package = callPackage ./bazelDerivation.nix { } {
     inherit
       name
       src
+      postUnpack
+      patches
       version
       sourceRoot
       env
-      buildInputs
       nativeBuildInputs
       ;
-    inherit registry bazelRepoCache bazelVendorDeps;
+    buildInputs = lib.optional (!stdenv.hostPlatform.isDarwin) autoPatchelfHook ++ buildInputs;
+    inherit autoPatchelfIgnoreMissingDeps;
+    # autoPatchelf will cross-link different jdks if run on top-level, we'll run manually
+    dontAutoPatchelf = true;
+    bazelPreBuild = ''
+      mkdir vendor_dir
+      ${bazelPreBuild}
+      ${bazel}/bin/bazel ${
+        lib.escapeShellArgs (
+          lib.optional (serverJavabase != null) "--server_javabase=${serverJavabase}"
+          ++ [ "--batch" ]
+          ++ startupArgs
+        )
+      } vendor ${
+        lib.escapeShellArgs (
+          [ "--vendor_dir=vendor_dir" ]
+          ++ lib.optional (registry != null) "--registry=file://${registry}"
+          ++ lib.optional (bazelRepoCache != null) "--repository_cache=repo_cache"
+          ++ commandArgs
+          ++ targets
+        )
+      }
+
+                    function sedVerbose() {
+                      local path=$1; shift;
+                      sed -i".bak-nix" "$path" "$@"
+                      diff -U0 "$path.bak-nix" "$path" | sed "s/^/  /" || true
+                      rm -f "$path.bak-nix"
+                    }
+                    # TODO: make opt-in & customizable
+                    ${gnugrep}/bin/grep -rlZ --include="*.bzl" --include "BUILD.bazel" --include "BUILD" /bin/bash vendor_dir \
+                      | while IFS="" read -r -d "" path; do
+                          echo "$path"
+                          sedVerbose "$path" \
+                            -e 's!/usr/bin/bash!${bash}/bin/bash!g' \
+                            -e 's!/bin/bash!${bash}/bin/bash!g'
+                      done;
+                    # TODO: make opt-in & customizable
+                    ${gnugrep}/bin/grep -rlZ --include="*.bzl" --include "BUILD.bazel" --include "BUILD" --include "java_stub_template.txt" /usr/bin/env vendor_dir \
+                      | while IFS="" read -r -d "" path; do
+                          echo "$path"
+                          sedVerbose "$path" \
+                            -e 's!/usr/bin/env bash!${bash}/bin/bash!g' \
+                            -e 's!/usr/bin/env!${coreutils}/bin/env!g'
+                      done;
+
+      # autoPatchelf may fail on some paths without permissions change
+      chmod -R u+w vendor_dir
+      ${builtins.concatStringsSep "\n" (
+        map (d: "autoPatchelf \"vendor_dir/${d}\"") autoPatchelfVendorDirs
+      )}
+    '';
+    inherit registry bazelRepoCache;
     inherit
       bazel
       targets
       startupArgs
       serverJavabase
-      commandArgs
       ;
+    commandArgs = commandArgs ++ [ "--vendor_dir=vendor_dir" ];
     inherit installPhase;
     command = "build";
+
+    passthru = passthru // {
+      inherit bazelRepoCache;
+    };
   };
 in
-package // { passthru = { inherit bazelRepoCache bazelVendorDeps; }; }
+package
