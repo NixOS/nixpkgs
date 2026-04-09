1400684a5588d52383082fcd22ca7e3c4e31c4f1
Luke Elsdon <luke.elsdon@gmail.com>
2026-04-09 21:29:16 +0100

    xtrkcad: init at 5.3.1

---
+++ pkgs/by-name/xt/xtrkcad/package.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchhg,
+  mercurial,
+  cmake,
+  libgcc,
+  pkg-config,
+  gtk2,
+  libzip,
+  libnxml,
+  inkscape,
+  doxygen,
+  pandoc,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "xtrkcad";
+  version = "5.3.1";
+
+  src = fetchhg {
+    url = "http://hg.code.sf.net/p/xtrkcad-fork/xtrkcad";
+    hash = "sha256-QtR122kC+Nqo5shw3xE/8nrHTBSgdBGLt5xBKTs5+KE=";
+  };
+
+  buildInputs = [
+    cmake
+    mercurial
+    libgcc
+    pkg-config
+    inkscape
+    gtk2
+    libzip
+    libnxml
+    inkscape
+    doxygen
+    pandoc
+  ];
+
+  meta = {
+    homepage = "https://sourceforge.net/projects/xtrkcad-fork/";
+    description = "Model Railway CAD program ";
+    mainProgram = "xtrkcad";
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.linux;
+  };
+})
