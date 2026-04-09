e0a3e13a5fb72ab24c03439779263cee21f0532c
Harinn <prinn.dev@pm.me>
2026-04-09 11:38:32 +0700

    prometheus: 3.10.0 -> 3.11.1

--- pkgs/by-name/pr/prometheus/source.nix
+++ pkgs/by-name/pr/prometheus/source.nix
@@ -1,6 +1,6 @@
 {
-  version = "3.10.0";
-  hash = "sha256-tTxHLngOsJ0STwfnBfuXN7CUaVgtpRGzEiFtXTWVDD4=";
-  npmDepsHash = "sha256-+nu7qfxlVa8OWARFgQnXj5riTQ0P1sjxbIgYUvJpcHw=";
-  vendorHash = "sha256-AsVy9RPemaKDuX8is2IXjlzRBsJFJiRfFj18SQl8Oc8=";
+  version = "3.11.1";
+  hash = "sha256-U3vvYRDzJprIQ03G7deRGc3NV0FPxTSH/RGKP1j5jxU=";
+  npmDepsHash = "sha256-4n4T2KN+QVhLso5rGk83UTdJ7zMseFpOq1anJc2dN1g=";
+  vendorHash = "sha256-wR1b5jV/2B50OeKokv8Ss+tpSXNjJBsLIZrK7gOb168=";
 }
