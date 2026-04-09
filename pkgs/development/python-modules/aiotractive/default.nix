16db4521ceb4298a9b16f37384b3a0dbf13d38f0
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-09 20:08:40 +0200

    python3Packages.aiotractive: migrate to finalAttrs

--- pkgs/development/python-modules/aiotractive/default.nix
+++ pkgs/development/python-modules/aiotractive/default.nix
@@ -8,7 +8,7 @@
   yarl,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aiotractive";
   version = "1.0.2";
   pyproject = true;
@@ -16,15 +16,13 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "zhulik";
     repo = "aiotractive";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-Tr8USF7GF9CMOcjy62e+oTu4k/1jIAOsZmWTFWEzJvk=";
   };
 
   build-system = [ setuptools ];
 
-  pythonRelaxDeps = [
-    "orjson"
-  ];
+  pythonRelaxDeps = [ "orjson" ];
 
   dependencies = [
     aiohttp
@@ -40,8 +38,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python client for the Tractive REST API";
     homepage = "https://github.com/zhulik/aiotractive";
-    changelog = "https://github.com/zhulik/aiotractive/releases/tag/${src.tag}";
+    changelog = "https://github.com/zhulik/aiotractive/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
