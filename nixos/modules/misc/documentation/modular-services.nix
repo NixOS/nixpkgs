84ec6dd35c9fa3d21ef06bac59c4e442edc103cd
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2026-04-09 10:08:10 +0000

    tlshd: add modular service and test

--- nixos/modules/misc/documentation/modular-services.nix
+++ nixos/modules/misc/documentation/modular-services.nix
@@ -20,6 +20,7 @@ let
   modularServicesModule = {
     options = {
       "<imports = [ pkgs.ghostunnel.services.default ]>" = fakeSubmodule pkgs.ghostunnel.services.default;
+      "<imports = [ pkgs.ktls-utils.services.default ]>" = fakeSubmodule pkgs.ktls-utils.services.default;
       "<imports = [ pkgs.php.services.default ]>" = fakeSubmodule pkgs.php.services.default;
       "<imports = [ pkgs.snid.services.default ]>" = fakeSubmodule pkgs.snid.services.default;
     };
