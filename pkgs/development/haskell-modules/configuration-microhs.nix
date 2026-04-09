8fbef8091f94074bcfab1db5c7613aeedf85da80
sternenseemann <sternenseemann@systemli.org>
2026-04-09 22:45:25 +0200

    haskell.packages.microhs: add doDistribute where necessary
    Packages that have an empty hydraPlatforms won't be tested on Hydra
    otherwise, even if explicitly requested.

--- pkgs/development/haskell-modules/configuration-microhs.nix
+++ pkgs/development/haskell-modules/configuration-microhs.nix
@@ -13,7 +13,7 @@ self: super: {
   MicroHs = null;
 
   # External MicroHs core libraries
-  ghc-compat = markUnbroken super.ghc-compat;
+  ghc-compat = doDistribute (markUnbroken super.ghc-compat);
 
   # Bootstrap MicroCabal
   MicroCabal = self.mkDerivation {
@@ -34,11 +34,11 @@ self: super: {
 
   # hackage-packages does not include GHC core libraries
   binary = markBroken self.binary_0_8_9_3;
-  Cabal = self.Cabal_3_16_1_0;
-  Cabal-syntax = self.Cabal-syntax_3_16_1_0;
-  containers = self.containers_0_8;
-  exceptions = self.exceptions_0_10_12;
-  filepath = self.filepath_1_5_5_0;
+  Cabal = doDistribute self.Cabal_3_16_1_0;
+  Cabal-syntax = doDistribute self.Cabal-syntax_3_16_1_0;
+  containers = doDistribute self.containers_0_8;
+  exceptions = doDistribute self.exceptions_0_10_12;
+  filepath = doDistribute self.filepath_1_5_5_0;
   ghc-bignum = null;
   ghc-boot = null;
   ghc-boot-th = null;
@@ -48,30 +48,30 @@ self: super: {
   ghc-platform = null;
   ghc-prim = null;
   ghci = null;
-  haskeline = self.haskeline_0_8_4_1;
+  haskeline = doDistribute self.haskeline_0_8_4_1;
   hpc = markBroken self.hpc_0_7_0_2;
   integer-gmp = markBroken self.integer-gmp_1_1;
   libiserv = null;
-  mtl = self.mtl_2_3_2;
-  os-string = self.os-string_2_0_10;
-  parsec = self.parsec_3_1_18_0;
+  mtl = doDistribute self.mtl_2_3_2;
+  os-string = doDistribute self.os-string_2_0_10;
+  parsec = doDistribute self.parsec_3_1_18_0;
   pretty = markBroken self.pretty_1_1_3_6;
   rts = null;
   semaphore-compat = null;
   stm = null;
   system-cxx-std-lib = null;
   template-haskell = null;
-  terminfo = self.terminfo_0_4_1_7;
-  time = self.time_1_15;
-  transformers = self.transformers_0_6_3_0;
+  terminfo = doDistribute self.terminfo_0_4_1_7;
+  time = doDistribute self.time_1_15;
+  transformers = doDistribute self.transformers_0_6_3_0;
   unix = markBroken self.unix_2_8_8_0;
   xhtml = markBroken self.xhtml_3000_4_0_0;
 
   # MicroHs replacements for widely used libraries
   array = self.array-mhs;
-  array-mhs = markUnbroken super.array-mhs;
+  array-mhs = doDistribute (markUnbroken super.array-mhs);
   random = self.random-mhs;
-  random-mhs = markUnbroken super.random-mhs;
+  random-mhs = doDistribute (markUnbroken super.random-mhs);
 
   # Depends on time when not using GHC
   splitmix = addBuildDepends [ self.time ] super.splitmix;
