039d6eb59a62a497f3863a36af27cf6acc7da02d
Harinn <prinn.dev@pm.me>
2026-04-09 12:50:27 +0700

    python3Packages.certbot: 5.4.0 -> 5.5.0

--- pkgs/development/python-modules/certbot/default.nix
+++ pkgs/development/python-modules/certbot/default.nix
@@ -24,14 +24,14 @@
 
 buildPythonPackage rec {
   pname = "certbot";
-  version = "5.4.0";
+  version = "5.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "certbot";
     repo = "certbot";
     tag = "v${version}";
-    hash = "sha256-Tu46Wybod89TiwsVccNuQcweWoeQE1wbH+pDWNC9+kE=";
+    hash = "sha256-hSRMxWQbGFFA5AA+M3wlvVckR+M9qaEIywLC5y6YH0A=";
   };
 
   sourceRoot = "${src.name}/certbot";
