34f1faecb0fb417b8f84e3fcfffa1e65ce9b539d
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-09 09:00:41 +0200

    python3Packages.mypy-boto3-drs: 1.42.3 -> 1.42.86

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -423,8 +423,8 @@ in
       "sha256-lELlqGCIpJDadB4McePclQ56IyVBo7s9B05mBI3vWdY=";
 
   mypy-boto3-drs =
-    buildMypyBoto3Package "drs" "1.42.3"
-      "sha256-31X59K6RtNoN/iIqZMkpoH1z55k69wteY/Mw2va4txI=";
+    buildMypyBoto3Package "drs" "1.42.86"
+      "sha256-Gs2IKLV8vnT9M1fT2v16WlgjjfgAUfeGh9DubBwjtBk=";
 
   mypy-boto3-ds =
     buildMypyBoto3Package "ds" "1.42.3"
