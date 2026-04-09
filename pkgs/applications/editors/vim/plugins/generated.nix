64b82af6119db9ed74eba32709425bcd6b6d2e51
Perchun Pak <git@perchun.it>
2026-04-09 14:51:04 +0200

    vimPlugins.vim-stationeers-ic10-syntax: move back to generated.nix

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -21943,6 +21943,18 @@ final: prev: {
     meta.hydraPlatforms = [ ];
   };
 
+  vim-stationeers-ic10-syntax = buildVimPlugin {
+    pname = "vim-stationeers-ic10-syntax";
+    version = "0-unstable-2025-11-02";
+    src = fetchgit {
+      url = "https://gitlab.com/LittleMorph/vim-ic10";
+      rev = "74446a16078ef4f3d2088136b32af939fb6bc2a4";
+      hash = "sha256-YCxrSB7eRQ54iZhpcsAR930Uccj+2ZyogpYGKbcSlys=";
+    };
+    meta.homepage = "https://gitlab.com/LittleMorph/vim-ic10";
+    meta.hydraPlatforms = [ ];
+  };
+
   vim-strip-trailing-whitespace = buildVimPlugin {
     pname = "vim-strip-trailing-whitespace";
     version = "1.0-unstable-2022-02-01";
