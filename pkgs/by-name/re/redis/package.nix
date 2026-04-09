2a9dd84dc1a504c123c260cc8c582dccf488ff01
Hythera <87016780+Hythera@users.noreply.github.com>
2026-04-09 22:42:41 +0200

    redis: add hythera as maintainer

--- pkgs/by-name/re/redis/package.nix
+++ pkgs/by-name/re/redis/package.nix
@@ -143,7 +143,7 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.agpl3Only;
     platforms = lib.platforms.all;
     changelog = "https://github.com/redis/redis/releases/tag/${finalAttrs.version}";
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ hythera ];
     mainProgram = "redis-cli";
   };
 })
