faa02b0a6aa7935544da9f00f4bbafba67946107
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-10 14:44:46 +0000

    xlights: 2026.04 -> 2026.05

--- pkgs/by-name/xl/xlights/package.nix
+++ pkgs/by-name/xl/xlights/package.nix
@@ -6,11 +6,11 @@
 
 appimageTools.wrapType2 rec {
   pname = "xlights";
-  version = "2026.04";
+  version = "2026.05";
 
   src = fetchurl {
     url = "https://github.com/smeighan/xLights/releases/download/${version}/xLights-${version}-x86_64.AppImage";
-    hash = "sha256-eNt1dm2TDnw+JtRP73RppKeCspxKLgS3mnYKRNQ3Srs=";
+    hash = "sha256-W1Il2dIDFUtkwPsPptdD2s/5eluGAeH7tw/ZmArN8dY=";
   };
 
   meta = {
