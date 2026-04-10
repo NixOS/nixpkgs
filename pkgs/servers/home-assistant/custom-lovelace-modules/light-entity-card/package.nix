61f6d7f8f60e8fde357f29810188b56789cbe5d7
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-04-10 02:20:52 +0200

    home-assistant-custom-lovelace-modules.light-entity-card: 6.1.3 -> 6.3.1

--- pkgs/servers/home-assistant/custom-lovelace-modules/light-entity-card/package.nix
+++ pkgs/servers/home-assistant/custom-lovelace-modules/light-entity-card/package.nix
@@ -6,18 +6,16 @@
 
 buildNpmPackage rec {
   pname = "light-entity-card";
-  version = "6.1.3";
+  version = "6.3.1";
 
   src = fetchFromGitHub {
     owner = "ljmerza";
     repo = "light-entity-card";
-    tag = version;
-    hash = "sha256-DtpNKcnxMWbKwfIwo9R2g2Vux9oAjTI0URixGC41qeA=";
+    tag = "v${version}";
+    hash = "sha256-Y25jtbKJNTVi6XUHntm2AtIzuht96/o5l+uScwEE9So=";
   };
 
-  npmDepsHash = "sha256-EZDTWtn3joikwiC5Kfn94+tXRDpBhMDHqHozfIkfbJ0=";
-
-  env.NODE_OPTIONS = "--openssl-legacy-provider";
+  npmDepsHash = "sha256-Sl2TgA73Wq1n//lqokLC7iHcE1Oqt+ZP6MT+Deidhn0=";
 
   installPhase = ''
     runHook preInstall
