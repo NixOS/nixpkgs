7aceb45f66a7d44ce080176d759f5d5dc17b4403
Guy Chronister <guychronister@outlook.com>
2026-04-10 10:01:41 -0500

    octavePackages.linear-algebra: migrate to by-name

--- pkgs/top-level/octave-packages.nix
+++ pkgs/top-level/octave-packages.nix
@@ -69,8 +69,6 @@ makeScope newScope (
         inherit (pkgs) gsl;
       };
 
-      linear-algebra = callPackage ../development/octave-modules/linear-algebra { };
-
       lssa = callPackage ../development/octave-modules/lssa { };
 
       ltfat = callPackage ../development/octave-modules/ltfat {
