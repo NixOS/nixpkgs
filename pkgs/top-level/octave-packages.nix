d231a29601568a7e969c92165cf004426ecd997f
Guy Chronister <guychronister@outlook.com>
2026-04-10 10:12:07 -0500

    octavePackages.mvn: migrate to by-name

--- pkgs/top-level/octave-packages.nix
+++ pkgs/top-level/octave-packages.nix
@@ -69,8 +69,6 @@ makeScope newScope (
         inherit (pkgs) gsl;
       };
 
-      mvn = callPackage ../development/octave-modules/mvn { };
-
       nan = callPackage ../development/octave-modules/nan { };
 
       ncarray = callPackage ../development/octave-modules/ncarray { };
