e769d423947cd1ba62de2b12bd31b6d32452306a
Fazzi <faaris.ansari@proton.me>
2026-04-10 21:12:42 +0100

    sgdboop: 1.3.1 -> 1.3.2

--- pkgs/by-name/sg/sgdboop/package.nix
+++ pkgs/by-name/sg/sgdboop/package.nix
@@ -9,22 +9,14 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "sgdboop";
-  version = "1.3.1";
+  version = "1.3.2";
 
   src = fetchFromGitHub {
     owner = "SteamGridDB";
     repo = "SGDBoop";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-FpVQQo2N/qV+cFhYZ1FVm+xlPHSVMH4L+irnQEMlUQs=";
+    hash = "sha256-/pXZMq80fb7Z+619ACnu/ZYWpouh59PIiruWY7l2cnQ=";
   };
-
-  patches = [
-    # Hide the app from app launchers, as it is not meant to be run directly
-    # Remove when https://github.com/SteamGridDB/SGDBoop/pull/112 is merged
-    ./hide_desktop_entry.patch
-    # remove unused arg to fix build
-    ./remove-unused-arg.patch
-  ];
 
   makeFlags = [
     # The flatpak install just copies things to /app - otherwise wants to do things with XDG
