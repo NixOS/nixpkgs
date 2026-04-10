4f28fbe2374f0addb81e3fa3cba11ae24d5f17d0
Edward <9159652+winterec@users.noreply.github.com>
2026-04-10 14:37:29 +0800

    edusong: 4.0 -> 4.4

--- pkgs/by-name/ed/edusong/package.nix
+++ pkgs/by-name/ed/edusong/package.nix
@@ -6,12 +6,12 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "edusong";
-  version = "4.0";
+  version = "4.4";
 
   src = fetchzip {
     name = "edusong-${finalAttrs.version}";
     url = "https://language.moe.gov.tw/001/Upload/Files/site_content/M0001/eduSong_Unicode.zip";
-    hash = "sha256-4NBnwMrYufeZbgSiD2fAhe4tuy0aAA5u9tWwjQQjEQk=";
+    hash = "sha256-+wH6I0sOfzytstDNT81LMuqknGQuYekl31e1tYaDvRg=";
   };
 
   installPhase = ''
@@ -24,8 +24,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     longDescription = ''
       A Song-style Chinese character typeface published by the Ministry of Education of the Republic of China (Taiwan). The Song style is also referred to as 宋體, 宋体, sòngtǐ, 明體, 明体, or míngtǐ, in Chinese; 명조체, 明朝體, or myeongjo in Korean; 明朝体, みんちょうたい, or minchōtai in Japanese.
     '';
-    homepage = "https://language.moe.gov.tw/result.aspx?classify_sn=23&subclassify_sn=436&content_sn=48";
+    homepage = "https://language.moe.gov.tw/001/Upload/Files/site_content/M0001/edusun.pdf";
     license = lib.licenses.cc-by-nd-30;
-    maintainers = with lib.maintainers; [ ShamrockLee ];
+    maintainers = with lib.maintainers; [
+      ShamrockLee
+      winterec
+    ];
   };
 })
