3b528599aa4671e291df20d04ff0174d57b81373
Imran Hossain <hi@imranh.org>
2026-04-10 18:36:56 -0400

    maintainers: Update email for i077

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11066,7 +11066,7 @@
     name = "Yugen";
   };
   i077 = {
-    email = "nixpkgs@imranhossa.in";
+    email = "nixpkgs@imranh.org";
     github = "i077";
     githubId = 2789926;
     name = "Imran Hossain";
