bd9809cb194247287d270021cbd07db946150c04
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-11 16:06:16 +0000

    grafanaPlugins.volkovlabs-form-panel: 6.3.1 -> 6.3.2

--- pkgs/servers/monitoring/grafana/plugins/volkovlabs-form-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/volkovlabs-form-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin {
   pname = "volkovlabs-form-panel";
-  version = "6.3.1";
-  zipHash = "sha256-HuzhTWey/6xLu6GPXGnN4/D3rs7yJ2sPGzO8PPuZmNA=";
+  version = "6.3.2";
+  zipHash = "sha256-RuwZaAqSwMvWN15jz0+r7DuZyrE52zYD8EBffFTQNEg=";
   meta = {
     description = "Plugin that allows inserting and updating application data, as well as modifying configuration directly from your Grafana dashboard";
     license = lib.licenses.asl20;
