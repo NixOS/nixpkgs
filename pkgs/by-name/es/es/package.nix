0190e23d7659184ddd96bfb6c3e6427cb53f0acf
Lein Matsumaru <applePrincess@applePrincess.io>
2026-04-11 23:40:10 +0000

    es: 0.9.2 -> 0.10.0

--- pkgs/by-name/es/es/package.nix
+++ pkgs/by-name/es/es/package.nix
@@ -1,8 +1,8 @@
 {
   lib,
   stdenv,
-  fetchpatch,
-  fetchurl,
+  fetchFromGitHub,
+  autoreconfHook,
   readline,
   bison,
 }:
@@ -10,31 +10,20 @@
 stdenv.mkDerivation (finalAttrs: {
 
   pname = "es";
-  version = "0.9.2";
+  version = "0.10.0";
 
-  src = fetchurl {
-    url = "https://github.com/wryun/es-shell/releases/download/v${finalAttrs.version}/es-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-ySZIK0IITpA+uHHuHrDO/Ana5vGt64QI3Z6TMDXE9d0=";
+  src = fetchFromGitHub {
+    owner = "wryun";
+    repo = "es-shell";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Rm0hG3B93p1lFb6IlnKYXtSxuRSIa2AT3SiAjMSb/oc=";
   };
 
-  # The distribution tarball does not have a single top-level directory.
-  preUnpack = ''
-    mkdir $name
-    cd $name
-    sourceRoot=.
-  '';
-
-  patches = [
-    (fetchpatch {
-      # https://github.com/wryun/es-shell/pull/101
-      name = "new-compiler-issues.patch";
-      url = "https://github.com/wryun/es-shell/commit/1eafb5fc4be735e59c9a091cc30adbca8f86fd96.patch";
-      hash = "sha256-0CV1seEiH6PsUnq0akPLiRMy+kIb9qnAK7Ta4I47i60=";
-    })
-  ];
-
   strictDeps = true;
-  nativeBuildInputs = [ bison ];
+  nativeBuildInputs = [
+    autoreconfHook
+    bison
+  ];
   buildInputs = [ readline ];
 
   configureFlags = [ "--with-readline" ];
