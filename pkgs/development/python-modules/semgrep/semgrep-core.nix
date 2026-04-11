ddbbe106e1e22e1bfe68903360f1b850f6ea0bfa
caverav <camilo@fvv.cl>
2026-04-11 15:32:07 -0400

    semgrep: 1.152.0 -> 1.161.0

--- pkgs/development/python-modules/semgrep/semgrep-core.nix
+++ pkgs/development/python-modules/semgrep/semgrep-core.nix
@@ -1,23 +1,25 @@
 {
   lib,
-  stdenvNoCC,
+  stdenv,
   fetchPypi,
   unzip,
+  autoPatchelfHook,
 }:
 
 let
   common = import ./common.nix { inherit lib; };
 in
-stdenvNoCC.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "semgrep-core";
   inherit (common) version;
+
   # fetch pre-built semgrep-core since the ocaml build is complex and relies on
   # the opam package manager at some point
   # pulling it out of the python wheel as r2c no longer release a built binary
   # on github releases
   src =
     let
-      inherit (stdenvNoCC.hostPlatform) system;
+      inherit (stdenv.hostPlatform) system;
       data = common.core.${system} or (throw "Unsupported system: ${system}");
     in
     fetchPypi rec {
@@ -25,11 +27,16 @@ stdenvNoCC.mkDerivation rec {
       inherit version;
       format = "wheel";
       dist = python;
-      python = "cp310.cp311.cp312.cp313.cp314.py310.py311.py312.py313.py314";
+      python = common.pythonWheelTag;
       inherit (data) platform hash;
     };
 
-  nativeBuildInputs = [ unzip ];
+  nativeBuildInputs = [
+    unzip
+  ]
+  ++ lib.optional stdenv.hostPlatform.isLinux autoPatchelfHook;
+
+  buildInputs = lib.optional stdenv.hostPlatform.isLinux stdenv.cc.cc.lib;
 
   # _tryUnzip from unzip's setup-hook doesn't recognise .whl
   # "do not know how to unpack source archive"
@@ -46,6 +53,18 @@ stdenvNoCC.mkDerivation rec {
   installPhase = ''
     runHook preInstall
     install -Dm 755 -t $out/bin semgrep-${version}.data/purelib/semgrep/bin/semgrep-core
+
+    # copy bundled libs as well
+    # keeping them in bin/libs matches the layout in the wheel
+    if [ -d semgrep-${version}.data/purelib/semgrep/bin/libs ]; then
+      mkdir -p $out/bin/libs
+      cp -rf semgrep-${version}.data/purelib/semgrep/bin/libs/* $out/bin/libs/
+
+      # help autoPatchelfHook find these libs
+      if [ -n "''${autoPatchelfHook:-}" ]; then
+        appendAutoPatchelfSearchPath $out/bin/libs
+      fi
+    fi
     runHook postInstall
   '';
 
