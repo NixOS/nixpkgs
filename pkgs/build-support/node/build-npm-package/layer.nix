b77eca6acf7ca7d33609542f68ce18412690f638
cinereal <cinereal@riseup.net>
2026-04-11 15:54:49 +0200

    layers.buildNpmPackage: default nativeBuildInputs to [ ]
    The layer unconditionally dereferenced `old.stdenvArgs.nativeBuildInputs`
    while computing the final `nativeBuildInputs` list, which crashed for
    any mkPackage consumer that didn't happen to set it on the user
    layer. Fall back to `[ ]` so the layer composes cleanly with any
    package, not only those that already have nativeBuildInputs.
    
    Matches the style of the adjacent `buildInputs` line, which already
    uses `or [ ]`.

--- pkgs/build-support/node/build-npm-package/layer.nix
+++ pkgs/build-support/node/build-npm-package/layer.nix
@@ -34,29 +34,29 @@ in
       inherit (deps) nodejs;
     };
     inherit (deps.npmHooks) npmConfigHook npmBuildHook npmInstallHook;
-  } // old.deps;
+  }
+  // old.deps;
   /**
     Arguments for fetchNpmDeps
   */
-  npmFetch =
-    {
-      name = "${name}-${version}-npm-deps";
-      hash = throw "Please specify npmFetch.hash in the package definition of ${name}";
-      forceEmptyCache = false;
-      forceGitDeps = false;
-      patchFlags = "";
-      postPatch = "";
-      prePatch = "";
-    }
-    // builtins.intersectAttrs fetchInherited this.stdenvArgs
-    // old.npmFetch;
+  npmFetch = {
+    name = "${name}-${version}-npm-deps";
+    hash = throw "Please specify npmFetch.hash in the package definition of ${name}";
+    forceEmptyCache = false;
+    forceGitDeps = false;
+    patchFlags = "";
+    postPatch = "";
+    prePatch = "";
+  }
+  // builtins.intersectAttrs fetchInherited this.stdenvArgs
+  // old.npmFetch;
   stdenvArgs = old.stdenvArgs or { } // {
     inherit (deps) nodejs;
     npmDeps = fetchNpmDeps this.npmFetch;
     npmPruneFlags = old.stdenvArgs.npmPruneFlags or this.stdenvArgs.npmInstallFlags or [ ];
     npmBuildScript = "build";
     nativeBuildInputs =
-      old.stdenvArgs.nativeBuildInputs
+      old.stdenvArgs.nativeBuildInputs or [ ]
       ++ [
         deps.nodejs
         deps.npmConfigHook
@@ -72,5 +72,16 @@ in
   };
   meta = (old.meta or { }) // {
     platforms = old.meta.platforms or deps.nodejs.meta.platforms;
+  };
+  # Mirror `extendDerivation`'s behaviour for the relevant
+  # derivation attrs that downstream tools (e.g. `nixpkgs-vet`'s
+  # NPV-165 `strictDeps` check) read directly off the package
+  # attrset. We don't merge `this.drvAttrs` wholesale because that
+  # would force `makeDerivationArgument`-output keys whose
+  # conditional structure depends on input *values*, creating a
+  # fixpoint cycle whenever `super.<key>` reads back through
+  # `old.public` in an `overrideAttrs` chain.
+  public = (old.public or { }) // {
+    strictDeps = true;
   };
 }
