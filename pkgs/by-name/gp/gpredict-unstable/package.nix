d76a83908c82f69019c97b285ae8ae1863b86ce0
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-12 23:42:45 +0000

    gpredict-unstable: 2.2.1-unstable-2025-09-20 -> 2.5.1-unstable-2026-04-09

--- pkgs/by-name/gp/gpredict-unstable/package.nix
+++ pkgs/by-name/gp/gpredict-unstable/package.nix
@@ -10,13 +10,13 @@
   goocanvas_2 = goocanvas_3;
 }).overrideAttrs
   (finalAttrs: {
-    version = "2.2.1-unstable-2025-09-20";
+    version = "2.5.1-unstable-2026-04-09";
 
     src = fetchFromGitHub {
       owner = "csete";
       repo = "gpredict";
-      rev = "34af48e6bfd9647559abb1df4907a65d064fc870";
-      hash = "sha256-G4bS7w/USIzYOa+aYv3YQCbUQolV22Z/7+71GHHSUpk=";
+      rev = "73937f7e9825747d8482884d25c65904b5ff0ef5";
+      hash = "sha256-bPHhAjFOB8RdtnOirywsN2o5LQKqSlNy7j+ftRrfxDs=";
     };
 
     patches = [ ];
