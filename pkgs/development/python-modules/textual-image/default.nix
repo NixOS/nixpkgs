0cc192e5c28b032ddc21c2572bf1d757a5787ce8
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-12 23:54:07 +0200

    python3Packages.textual-image: modernize
    - remove comments

--- pkgs/development/python-modules/textual-image/default.nix
+++ pkgs/development/python-modules/textual-image/default.nix
@@ -2,19 +2,14 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-
-  # dependencies
   rich,
-
-  # tests
   pillow,
   pytestCheckHook,
   syrupy,
-
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "textual-image";
   version = "0.11.0";
   pyproject = true;
@@ -22,11 +17,11 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "lnqs";
     repo = "textual-image";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-nWP4pxFcsjDA/SIrKXHjufiQaxHGgPpC1ZIti+TW+f0=";
   };
 
-  buildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
   dependencies = [
     pillow
@@ -45,8 +40,8 @@ buildPythonPackage rec {
   meta = {
     description = "Render images in the terminal with Textual and rich";
     homepage = "https://github.com/lnqs/textual-image/";
-    changelog = "https://github.com/lnqs/textual-image/blob/${src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/lnqs/textual-image/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.lgpl3;
     maintainers = with lib.maintainers; [ gaelj ];
   };
-}
+})
