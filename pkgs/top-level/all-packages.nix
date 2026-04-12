31fec1be1001829f3f569110bbe6b12495da92d7
Guy Chronister <guychronister@outlook.com>
2026-04-11 20:33:53 -0500

    dapl-native: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2400,11 +2400,6 @@ with pkgs;
 
   gnome-panel-with-modules = callPackage ../by-name/gn/gnome-panel/wrapper.nix { };
 
-  dapl-native = callPackage ../development/interpreters/dzaima-apl {
-    buildNativeImage = true;
-    jdk = graalvmPackages.graalvm-ce;
-  };
-
   gnucap-full = gnucap.withPlugins (p: [ p.verilog ]);
 
   gnugrep = callPackage ../tools/text/gnugrep { };
