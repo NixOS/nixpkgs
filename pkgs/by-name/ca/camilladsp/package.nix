97d58413e93d0aa38c34154db30b2673a6dbe65e
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-13 12:06:42 +0000

    camilladsp: 4.0.1 -> 4.1.3

--- pkgs/by-name/ca/camilladsp/package.nix
+++ pkgs/by-name/ca/camilladsp/package.nix
@@ -12,13 +12,13 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "camilladsp";
-  version = "4.0.1";
+  version = "4.1.3";
 
   src = fetchFromGitHub {
     owner = "HEnquist";
     repo = "camilladsp";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-PedaOmrvvE9Kxux0XwwDJfKq0fncsV/sUzZhnAp++DM=";
+    hash = "sha256-/OnD607xSPXM4AjVOZjaZQJpo7Q847Z8mq6elHmEwAU=";
   };
 
   cargoLock = {
