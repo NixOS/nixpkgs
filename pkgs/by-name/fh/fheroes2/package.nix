c00d0ba5ccbefa99a81c6996f2d324e7aec650f5
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-14 19:38:26 +0000

    fheroes2: 1.1.13 -> 1.1.15

--- pkgs/by-name/fh/fheroes2/package.nix
+++ pkgs/by-name/fh/fheroes2/package.nix
@@ -18,13 +18,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fheroes2";
-  version = "1.1.13";
+  version = "1.1.15";
 
   src = fetchFromGitHub {
     owner = "ihhub";
     repo = "fheroes2";
     rev = finalAttrs.version;
-    hash = "sha256-ct58Rkc6ORXldINQZVzMuObMl0BMk6QG88oU4tT0WcE=";
+    hash = "sha256-Rg7UF7TvpzDWZry4GAQPXbJmRXNHsn4z1i3GlbkZIYc=";
   };
 
   nativeBuildInputs = [ imagemagick ];
