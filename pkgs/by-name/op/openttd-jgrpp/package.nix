b64664dd5c6928763d95a6c09da52a57f0bc4669
Astrid Yu <astrid@astrid.tech>
2026-04-13 18:53:14 -0700

    openttd-jgrpp: 0.69.2 -> 0.72.1

--- pkgs/by-name/op/openttd-jgrpp/package.nix
+++ pkgs/by-name/op/openttd-jgrpp/package.nix
@@ -6,13 +6,13 @@
 
 openttd.overrideAttrs (oldAttrs: rec {
   pname = "openttd-jgrpp";
-  version = "0.69.2";
+  version = "0.72.1";
 
   src = fetchFromGitHub {
     owner = "JGRennison";
     repo = "OpenTTD-patches";
     rev = "jgrpp-${version}";
-    hash = "sha256-D9Oh05Isf7Atsih6tZBA8xS04aCuE8VS5Ghf0FvrU5A=";
+    hash = "sha256-gPLObFbBvvr6iH9EG1lRDDFxB/8ccwc63ZgpiMVNAYg=";
   };
   patches = [ ];
 
@@ -23,5 +23,4 @@ openttd.overrideAttrs (oldAttrs: rec {
     changelog = "https://github.com/JGRennison/OpenTTD-patches/blob/jgrpp-${version}/jgrpp-changelog.md";
     mainProgram = "openttd";
   };
-
 })
