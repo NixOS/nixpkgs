9ee15cb23d067993cf1c029feb9ae9a892b734c0
klea <klea@iwnp.org>
2026-04-14 16:30:09 +0000

    vim: 9.2.0106 -> 9.2.0340
    Fixes several security issues:
    
    CVE-2026-34982 https://github.com/vim/vim/security/advisories/GHSA-8h6p-m6gr-mpw9
    https://github.com/vim/vim/security/advisories/GHSA-2gmj-rpqf-pxvh
    https://github.com/vim/vim/security/advisories/GHSA-jc86-w7vm-8p24

--- pkgs/applications/editors/vim/common.nix
+++ pkgs/applications/editors/vim/common.nix
@@ -1,6 +1,6 @@
 { lib, fetchFromGitHub }:
 rec {
-  version = "9.2.0106";
+  version = "9.2.0340";
 
   outputs = [
     "out"
@@ -11,7 +11,7 @@ rec {
     owner = "vim";
     repo = "vim";
     rev = "v${version}";
-    hash = "sha256-byOf2Gr1vA7xQw3YHV454te1QrVxRy3sXrLdFUp2XRg=";
+    hash = "sha256-jCnOVIafx+0o1nlHv7QJQrmxs1IAxh9BBshDOFdZdCM=";
   };
 
   enableParallelBuilding = true;
