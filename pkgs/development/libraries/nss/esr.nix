9d3aa6c3c88ecc6384c75eba9c4686f6ba0f054e
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-04-14 15:11:09 +0200

    nss: 3.112.3 -> 3.112.4
    https://github.com/nss-dev/nss/blob/master/doc/rst/releases/nss_3_112_4.rst

--- pkgs/development/libraries/nss/esr.nix
+++ pkgs/development/libraries/nss/esr.nix
@@ -1,6 +1,6 @@
 import ./generic.nix {
-  version = "3.112.3";
-  hash = "sha256-1gOfP3HM1irGuJ+ln6n1toJC46+K5Z7pGm26vSryU7M=";
+  version = "3.112.4";
+  hash = "sha256-jt2AMrogSbA7cYKVAt6YZuka7/sIjgmiDlsABRp768U=";
   filename = "esr.nix";
   versionRegex = "NSS_(3)_(112)(?:_(\\d+))?_RTM";
 }
