eb99ea10a99d567233342b50b4fe71b7e5d065f3
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-14 02:02:01 +0200

    python3Packages.netutils: migrate to finalAttrs

--- pkgs/development/python-modules/netutils/default.nix
+++ pkgs/development/python-modules/netutils/default.nix
@@ -11,7 +11,7 @@
   toml,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "netutils";
   version = "1.17.2";
   pyproject = true;
@@ -19,7 +19,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "networktocode";
     repo = "netutils";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-DHftRRqbuUa74ATfh8MHxINwNkpz9lo/drwOmeo0itE=";
   };
 
@@ -59,8 +59,8 @@ buildPythonPackage rec {
   meta = {
     description = "Library that is a collection of objects for common network automation tasks";
     homepage = "https://github.com/networktocode/netutils";
-    changelog = "https://github.com/networktocode/netutils/releases/tag/${src.tag}";
+    changelog = "https://github.com/networktocode/netutils/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
