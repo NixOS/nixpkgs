4a105651850e14b1ddaf6e74340c2cdffe0afb76
wolfram444 <kamiltox2006@gmail.com>
2026-04-15 12:15:59 +0500

    maintainers: update wolfram444 github's name

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -29408,7 +29408,7 @@
   wolfram444 = {
     name = "Xabibulloh To'xtayev";
     email = "kamiltox2006@gmail.com";
-    github = "woolfram444";
+    github = "wolfram444";
     githubId = 109225370;
   };
   womeier = {
