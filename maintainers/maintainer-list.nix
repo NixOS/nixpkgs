3caa1e00eb912f56b188092372677e319c3af36e
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2026-04-15 23:20:41 +0200

    maintainers: add ilosariph

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -11358,6 +11358,12 @@
     githubId = 3948275;
     name = "Harikrishnan R";
   };
+  ilosariph = {
+    email = "simon@simon-wick.ch";
+    github = "Ilosariph";
+    githubId = 71074737;
+    name = "Simon Wick";
+  };
   ilya-epifanov = {
     email = "mail@ilya.network";
     github = "ilya-epifanov";
