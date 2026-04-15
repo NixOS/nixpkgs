004d4aebb1340697d633e76039e108f9cb01c73f
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-15 18:47:45 +0000

    dbvisualizer: 25.2.6 -> 26.1.1

--- pkgs/by-name/db/dbvisualizer/package.nix
+++ pkgs/by-name/db/dbvisualizer/package.nix
@@ -13,7 +13,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   inherit pname;
-  version = "25.2.6";
+  version = "26.1.1";
 
   src =
     let
@@ -21,7 +21,7 @@ stdenv.mkDerivation (finalAttrs: {
     in
     fetchurl {
       url = "https://www.dbvis.com/product_download/dbvis-${finalAttrs.version}/media/dbvis_linux_${underscoreVersion}.tar.gz";
-      hash = "sha256-yiL0FFkSntwLy/oOkiDQKTvTOUrtbv/9kV+1nLZtMB0=";
+      hash = "sha256-BiWFppp7+uAkLtyzA1i/CWewSZGWFlEagvqoB/3pz1Q=";
     };
 
   strictDeps = true;
