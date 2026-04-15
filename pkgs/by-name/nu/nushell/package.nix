879309c29d87c3ba673bafaeed18a5358dcee85f
Gaetan Lepage <gaetan@glepage.com>
2026-04-15 23:26:59 +0000

    nushell: 0.112.1 -> 0.112.2
    Diff: https://github.com/nushell/nushell/compare/0.112.1...0.112.2

--- pkgs/by-name/nu/nushell/package.nix
+++ pkgs/by-name/nu/nushell/package.nix
@@ -24,16 +24,16 @@ rustPlatform.buildRustPackage (finalAttrs: {
   # NOTE: when updating this to a new non-patch version, please also try to
   # update the plugins. Plugins only work if they are compiled for the same
   # major/minor version.
-  version = "0.112.1";
+  version = "0.112.2";
 
   src = fetchFromGitHub {
     owner = "nushell";
     repo = "nushell";
     tag = finalAttrs.version;
-    hash = "sha256-y0EfP95XEVvse0xaSWK5Yr748tTVYRG7Bx+Qf5JrlYY=";
+    hash = "sha256-wc7mfbwkJO5gq9mwsiTVx74+btqU6Ox8tPhnXkfmXRU=";
   };
 
-  cargoHash = "sha256-Y/yaWj2akccehFVxrGHrsjzZd02L8qE9KoSSwzAeEwg=";
+  cargoHash = "sha256-KBDgICbdYcqgMLtUXWQsMPe1fO7zT4NcavAyS2i0cDc=";
 
   nativeBuildInputs = [
     pkg-config
@@ -75,6 +75,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
         "plugins::stress_internals::test_local_socket"
 
         # Error:   × I/O error: Operation not permitted (os error 1)
+        "shell::environment::env::env_shlvl_in_exec_repl"
+        "shell::environment::env::env_shlvl_in_repl"
         "shell::environment::env::path_is_a_list_in_repl"
       ];
 
