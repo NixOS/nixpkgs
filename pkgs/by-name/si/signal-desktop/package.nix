e29239505be652028efce5a4cf1bfd8918c53ee8
Naïm Camille Favier <n@monade.li>
2026-04-15 23:15:45 +0200

    Revert "signal-desktop: deprecate commandLineArgs override"
    This reverts commit 60304c712e2f68a3331cd7af142035262effa218.

--- pkgs/by-name/si/signal-desktop/package.nix
+++ pkgs/by-name/si/signal-desktop/package.nix
@@ -23,9 +23,6 @@
 
   withAppleEmojis ? false,
 }:
-assert lib.warnIf (commandLineArgs != "")
-  "`commandLineArgs` has been deprecated and will be removed in the future. Consider creating a wrapper script or a desktop entry with your desired flags."
-  true;
 let
   nodejs = nodejs_24;
   pnpm = pnpm_10_29_2;
