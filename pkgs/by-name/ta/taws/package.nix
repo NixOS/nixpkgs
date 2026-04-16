de0dd5a41802586f415c51e63e40c2ba0ad76f53
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2026-04-16 18:45:50 -0500

    taws: init at 1.2.1

--- pkgs/by-name/ta/taws/package.nix
+++ pkgs/by-name/ta/taws/package.nix
@@ -5,7 +5,7 @@
   pkg-config,
   openssl,
   makeWrapper,
-  ssm-session-manager-plugin ? null,
+  ssm-session-manager-plugin,
   stdenv,
   testers,
   nix-update-script,
@@ -19,22 +19,22 @@ rustPlatform.buildRustPackage (finalAttrs: {
   src = fetchFromGitHub {
     owner = "huseyinbabal";
     repo = "taws";
-    rev = "cce20b064f758feb263fc266a83473f5b96ed741";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-76dC5ZLhQkItqGdWkq+U8mzimjDAAkzzpopx8ZPHCx4=";
   };
 
   cargoHash = "sha256-62Pk1RRx0eErGWNCYEyw0jFoNp97a+1kn5brgd81P5k=";
 
-  nativeBuildInputs = [ pkg-config ] ++ lib.optional (ssm-session-manager-plugin != null) makeWrapper;
+  nativeBuildInputs = [ pkg-config makeWrapper ];
 
   buildInputs = [ openssl ];
 
-  postInstall = lib.optionalString (ssm-session-manager-plugin != null) ''
+  postInstall = ''
     wrapProgram $out/bin/taws \
       --prefix PATH : ${lib.makeBinPath [ ssm-session-manager-plugin ]}
   '';
 
-  # adapting the test from k9s since it's a similar tool
+
   passthru = {
     tests.version = testers.testVersion {
       package = taws;
