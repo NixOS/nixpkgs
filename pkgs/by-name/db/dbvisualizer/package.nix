64bafe9a2330e15b7284578b40fc8f0ce1d17ef3
Boldi Koller <boldi.koller@wtss.eu>
2026-04-16 15:17:58 +0200

    dbvisualizer: 25.2.6 -> 25.3.3

--- pkgs/by-name/db/dbvisualizer/package.nix
+++ pkgs/by-name/db/dbvisualizer/package.nix
@@ -13,7 +13,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   inherit pname;
-  version = "25.2.6";
+  version = "25.3.3";
 
   src =
     let
@@ -21,7 +21,7 @@ stdenv.mkDerivation (finalAttrs: {
     in
     fetchurl {
       url = "https://www.dbvis.com/product_download/dbvis-${finalAttrs.version}/media/dbvis_linux_${underscoreVersion}.tar.gz";
-      hash = "sha256-yiL0FFkSntwLy/oOkiDQKTvTOUrtbv/9kV+1nLZtMB0=";
+      hash = "sha256-rvS2NczwmT1+/JIfpLI518I0/2AaIJEQAOwmKUK2FQs=";
     };
 
   strictDeps = true;
