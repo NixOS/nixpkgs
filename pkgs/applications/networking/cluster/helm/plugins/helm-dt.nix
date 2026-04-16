147a4f3b10a7fa2e352e33aade0138ec5cc91826
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-16 16:59:42 +0000

    kubernetes-helmPlugins.helm-dt: 0.4.12 -> 0.7.0

--- pkgs/applications/networking/cluster/helm/plugins/helm-dt.nix
+++ pkgs/applications/networking/cluster/helm/plugins/helm-dt.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "helm-dt";
-  version = "0.4.12";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "vmware-labs";
     repo = "distribution-tooling-for-helm";
     tag = "v${version}";
-    hash = "sha256-/fU56Dw7tvUnby4S78tyH/dasg4n3rb0hMlIkzvrv4M=";
+    hash = "sha256-WBrRVlYruokNKW0fSRd/YESH5Vw+hKFa76R/2ABphr8=";
   };
 
   vendorHash = "sha256-dkE3eYZnaS+kC0kDVxaFW/Ev15TY2MY3m5xgPof7Y18=";
