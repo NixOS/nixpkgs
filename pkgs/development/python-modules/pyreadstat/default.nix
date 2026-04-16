5eb8f097ed8766f8af66e0fbed19d82f032488c2
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-16 01:30:28 +0000

    python3Packages.pyreadstat: 1.3.3 -> 1.3.4

--- pkgs/development/python-modules/pyreadstat/default.nix
+++ pkgs/development/python-modules/pyreadstat/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "pyreadstat";
-  version = "1.3.3";
+  version = "1.3.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Roche";
     repo = "pyreadstat";
     tag = "v${version}";
-    hash = "sha256-CXZ9edMmup4+/2lImt6Zy19RIqGig1sUIK35snvIFEk=";
+    hash = "sha256-DC8iNv+4JpqdLq8op5gSHJEnodW+mc8utE7HCezcqpM=";
   };
 
   build-system = [
