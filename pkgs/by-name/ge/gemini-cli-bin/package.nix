52d99c2a91b93a240c232ccf2ce9b2d8fa1f94fb
Harinn <prinn.dev@pm.me>
2026-04-16 22:09:19 +0700

    gemini-cli-bin: 0.37.0 -> 0.38.1

--- pkgs/by-name/ge/gemini-cli-bin/package.nix
+++ pkgs/by-name/ge/gemini-cli-bin/package.nix
@@ -11,11 +11,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "gemini-cli-bin";
-  version = "0.37.0";
+  version = "0.38.1";
 
   src = fetchzip {
     url = "https://github.com/google-gemini/gemini-cli/releases/download/v${finalAttrs.version}/gemini-cli-bundle.zip";
-    hash = "sha256-JbR37CZGjZN0px+bGLQ67dbo/yyAb7BSx1v6IVuBCJc=";
+    hash = "sha256-+XMClLm7HGWST1MESxIVHGmgyvPPqrXGUomoiDY/eUE=";
     stripRoot = false;
   };
 
