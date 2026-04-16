61ab68447017a5859e4abe7883ac400db5d3b696
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-16 08:47:22 +0000

    python3Packages.microsoft-kiota-serialization-json: 1.9.10 -> 1.10.1

--- pkgs/development/python-modules/microsoft-kiota-serialization-json/default.nix
+++ pkgs/development/python-modules/microsoft-kiota-serialization-json/default.nix
@@ -13,14 +13,14 @@
 
 buildPythonPackage rec {
   pname = "microsoft-kiota-serialization-json";
-  version = "1.9.10";
+  version = "1.10.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "kiota-python";
     tag = "microsoft-kiota-serialization-json-v${version}";
-    hash = "sha256-J9OLxZ3vQpChhfwjXzrGF691zco/bKv51FG20VFieN0=";
+    hash = "sha256-KBCjVNZDPMh0wxWm8UVLsrfl2AYp3rKMjAT5c8F7+64=";
   };
 
   sourceRoot = "${src.name}/packages/serialization/json/";
