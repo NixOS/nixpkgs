e841c13e3d0b6d41032b904ddadf5983da2e62cd
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-17 14:22:43 +0000

    buffrs: 0.13.0 -> 0.13.2

--- pkgs/by-name/bu/buffrs/package.nix
+++ pkgs/by-name/bu/buffrs/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "buffrs";
-  version = "0.13.0";
+  version = "0.13.2";
 
   src = fetchFromGitHub {
     owner = "helsing-ai";
     repo = "buffrs";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-DTA30wVgThUu76AHWcpxAiOQZf92N6hMDPUHOUjg1vA=";
+    hash = "sha256-IKL8zBDM83TgyYHcGOBn+6nzIo6ywvlZk+G5cBC4ijE=";
   };
 
-  cargoHash = "sha256-c8Y81+IUpOWWlCsnltFWgbWW9I2ZkardzT94wGTKMVo=";
+  cargoHash = "sha256-Mku0H3fwyxtHgPuJndHA/lcd7/rGVw69+j1GdFt5JyQ=";
 
   # Disabling tests meant to work over the network, as they will fail
   # inside the builder.
