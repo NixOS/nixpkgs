2ac9e68f9d577a04307f50b68ea5fac6e0a40de8
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-17 22:16:55 +0000

    xeus: 5.2.4 -> 6.0.5

--- pkgs/by-name/xe/xeus/package.nix
+++ pkgs/by-name/xe/xeus/package.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "xeus";
-  version = "5.2.4";
+  version = "6.0.5";
 
   src = fetchFromGitHub {
     owner = "jupyter-xeus";
     repo = "xeus";
     tag = finalAttrs.version;
-    hash = "sha256-siQzTu2IYHLbZrgLTbHPt8Ek8vLA/wXB0jx7oXC6d7k=";
+    hash = "sha256-nbjq4dzrukVsZI6X3lWpr9oCZV5IUu/vkqSNKD7o3vo=";
   };
 
   nativeBuildInputs = [
