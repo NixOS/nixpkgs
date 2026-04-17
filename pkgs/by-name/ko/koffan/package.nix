3bada73477dbc1e0fc35b13df2fac177a5d05097
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-17 14:46:26 +0000

    koffan: 2.3.1 -> 2.9.0

--- pkgs/by-name/ko/koffan/package.nix
+++ pkgs/by-name/ko/koffan/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "koffan";
-  version = "2.3.1";
+  version = "2.9.0";
 
   src = fetchFromGitHub {
     owner = "PanSalut";
     repo = "Koffan";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-MCc60vQtVDfnroXMbcYu9QdFfAVYF5PJfD4xE1gKroE=";
+    hash = "sha256-GZmw4UoHoCeLhzAn0GWXR6c/61bYxtngGC2cND+XS5c=";
   };
 
-  vendorHash = "sha256-9QNqW1Cif5sNuI5rvM5JoBTdEwWWXROcmMOVP2eOc2M=";
+  vendorHash = "sha256-BYehi5LQQ0MIsKG/fN3DHaQwKVmxUFrvWGrKZeKj+ow=";
 
   passthru = {
     updateScript = nix-update-script { };
