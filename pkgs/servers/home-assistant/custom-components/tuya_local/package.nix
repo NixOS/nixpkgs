409133f08beb9d6aca71ad86200db37cd0e2fc00
Robert Schütz <nix@dotlambda.de>
2026-04-17 20:17:43 -0700

    home-assistant-custom-components.tuya_local: 2026.4.0 -> 2026.4.2
    Diff: https://github.com/make-all/tuya-local/compare/2026.4.0...2026.4.2
    
    Changelog: https://github.com/make-all/tuya-local/releases/tag/2026.4.2

--- pkgs/servers/home-assistant/custom-components/tuya_local/package.nix
+++ pkgs/servers/home-assistant/custom-components/tuya_local/package.nix
@@ -11,13 +11,13 @@
 buildHomeAssistantComponent rec {
   owner = "make-all";
   domain = "tuya_local";
-  version = "2026.4.0";
+  version = "2026.4.2";
 
   src = fetchFromGitHub {
     inherit owner;
     repo = "tuya-local";
     tag = version;
-    hash = "sha256-7Q7bkyWLIv6m8d3XaKaoiBIXcOeb/1YcL4kfl0HfUZY=";
+    hash = "sha256-yxnWiAsI3nJrugnxI6XE4s2AIK/YvUIDfLY7BINzup4=";
   };
 
   dependencies = [
