176438dc95a6629d42865e7230871b29f298e29f
Emiliano Panaro <dev@ronapa.com>
2026-04-18 23:37:52 +0200

    revpdf-editor: fix formatting

--- pkgs/by-name/re/revpdf-editor/package.nix
+++ pkgs/by-name/re/revpdf-editor/package.nix
@@ -1,24 +1,33 @@
 # /etc/nixos/pkgs/revpdf-editor/default.nix
-{ lib, appimageTools, fetchurl }:
+{
+  lib,
+  appimageTools,
+  fetchurl,
+}:
 
 appimageTools.wrapType2 rec {
-  pname   = "revpdf-editor";
+  pname = "revpdf-editor";
   version = "1.0";
 
   src = fetchurl {
-    url    = "https://github.com/Pawandeep-prog/revpdf-release/raw/refs/heads/main/linux/revpdf_editor-x86_64.AppImage";
+    url = "https://github.com/Pawandeep-prog/revpdf-release/raw/refs/heads/main/linux/revpdf_editor-x86_64.AppImage";
     sha256 = "sha256-s9po8Ml2guA3e70aTvT/A2wlCTKyB8lkxGOVfFjSh/s=";
   };
 
-  extraPkgs = pkgs: with pkgs; [
-    libepoxy gtk3 glib gdk-pixbuf libGL
-  ];
+  extraPkgs =
+    pkgs: with pkgs; [
+      libepoxy
+      gtk3
+      glib
+      gdk-pixbuf
+      libGL
+    ];
 
   meta = with lib; {
     description = "PDF editor";
-    homepage    = "https://revpdf.com";
-    license     = licenses.unfree; # o la licenza corretta
-    platforms   = [ "x86_64-linux" ];
-    maintainers = [ maintainers.e1618033]; # solo se contribuisci a nixpkgs
+    homepage = "https://revpdf.com";
+    license = licenses.unfree; # o la licenza corretta
+    platforms = [ "x86_64-linux" ];
+    maintainers = [ maintainers.e1618033 ]; # solo se contribuisci a nixpkgs
   };
 }
