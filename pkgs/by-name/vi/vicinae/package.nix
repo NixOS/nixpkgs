ad0434b7c683b3a01619ac80652b14616213262c
zspher <66728045+zspher@users.noreply.github.com>
2026-04-17 21:27:57 -0600

    vicinae: 0.20.12 -> 0.20.13

--- pkgs/by-name/vi/vicinae/package.nix
+++ pkgs/by-name/vi/vicinae/package.nix
@@ -21,13 +21,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "vicinae";
-  version = "0.20.12";
+  version = "0.20.13";
 
   src = fetchFromGitHub {
     owner = "vicinaehq";
     repo = "vicinae";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-esnDXLMXJz+Nw4VF8Dk/FX4M+eVNe3vJSq06IoJ/NwA=";
+    hash = "sha256-zRoOKeQ4ne7o7mILwb+5PKE75FhoqkG/HizWs7bKrDo=";
   };
 
   apiDeps = fetchNpmDeps {
