863065fce7b4a7c7504177d3e6c9f6e3bb88e088
Robert Schütz <nix@dotlambda.de>
2026-04-17 18:42:32 -0700

    home-assistant: 2026.4.2 -> 2026.4.3
    Diff: https://github.com/home-assistant/core/compare/2026.4.2...2026.4.3
    
    Changelog: https://github.com/home-assistant/core/releases/tag/2026.4.3

--- pkgs/servers/home-assistant/default.nix
+++ pkgs/servers/home-assistant/default.nix
@@ -253,7 +253,7 @@ let
   extraBuildInputs = extraPackages python.pkgs;
 
   # Don't forget to run update-component-packages.py after updating
-  hassVersion = "2026.4.2";
+  hassVersion = "2026.4.3";
 
 in
 python.pkgs.buildPythonApplication rec {
@@ -274,13 +274,13 @@ python.pkgs.buildPythonApplication rec {
     owner = "home-assistant";
     repo = "core";
     tag = version;
-    hash = "sha256-dMkTuFEWlWAVptNEiYbBbqUM4ymXMfBAAyRsvzkcWls=";
+    hash = "sha256-65LZU4OpvVeV70xujI3kd/8VIzZh723l9F5xe8qEvX4=";
   };
 
   # Secondary source is pypi sdist for translations
   sdist = fetchPypi {
     inherit pname version;
-    hash = "sha256-eRdwYU/D4AjGbIfNbumBUBfbsr/P0i1k0KRaC5+1jfg=";
+    hash = "sha256-P76HVL5NW8TOpic1kRUXxeMeAtsy+UxkmTvsc0J+6nY=";
   };
 
   build-system = with python.pkgs; [
