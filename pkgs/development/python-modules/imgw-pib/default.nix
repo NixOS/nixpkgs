e5ce42bf6c996ae99d92e40fd6bb464c25c846f3
Robert Schütz <nix@dotlambda.de>
2026-04-17 19:00:01 -0700

    python3Packages.imgw-pib: 2.0.4 -> 2.1.0
    Diff: https://github.com/bieniu/imgw-pib/compare/2.0.4...2.1.0
    
    Changelog: https://github.com/bieniu/imgw-pib/releases/tag/2.1.0

--- pkgs/development/python-modules/imgw-pib/default.nix
+++ pkgs/development/python-modules/imgw-pib/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "imgw-pib";
-  version = "2.0.4";
+  version = "2.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bieniu";
     repo = "imgw-pib";
     tag = finalAttrs.version;
-    hash = "sha256-87Q1aKUmDPpay4nIhFZ37fU8XgR4mekbpYZ3BPkcmUw=";
+    hash = "sha256-VwoYxV6XlDRX3zdOwtfBxZG3ntBh8l+abNByN7HIAeo=";
   };
 
   build-system = [ setuptools ];
