73a82ca4fc667e8367d90ac30ff650534d9adfef
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-19 02:50:55 +0000

    python3Packages.utitools: 0.4.0 -> 0.5.0

--- pkgs/development/python-modules/utitools/default.nix
+++ pkgs/development/python-modules/utitools/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "utitools";
-  version = "0.4.0";
+  version = "0.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "RhetTbull";
     repo = "utitools";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-oI+a+sc9+qi7aFP0dLINAQekib/9pZm10A5jhVIHWvo=";
+    hash = "sha256-mx9vcMCeDTJyWJKm0Ci9IEAPCNfx9NvPGC8cuNYnH1M=";
   };
 
   build-system = [ flit-core ];
