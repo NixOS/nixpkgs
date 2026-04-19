302485e9d73ec2bffa7c82425f78ac3279bd387c
MCSeekeri <mcseekeri@outlook.com>
2026-04-19 17:35:07 +0800

    geph: 0.2.93 -> 0.2.99

--- pkgs/by-name/ge/geph/package.nix
+++ pkgs/by-name/ge/geph/package.nix
@@ -23,16 +23,16 @@ let
 in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "geph5";
-  version = "0.2.93";
+  version = "0.2.99";
 
   src = fetchFromGitHub {
     owner = "geph-official";
     repo = "geph5";
     rev = "geph5-client-v${finalAttrs.version}";
-    hash = "sha256-ZYcGW6Ssauf5BUs75KBV+4Zub2ZCVN29cWTxeNi87cI=";
+    hash = "sha256-AWdVFpIP+LIZz6zqcx0GJxDs4ZWGR6JgpHDVAg0mHaU=";
   };
 
-  cargoHash = "sha256-0Ml8tgWghxhDJzUMMD+YGwy3fyFjKcNjbV8MDJW8rZk=";
+  cargoHash = "sha256-zFCq29vtsbwbo6JBRdX+CziKZVoxwpt6y3BYVlIqZfc=";
 
   postPatch = ''
     substituteInPlace binaries/geph5-client/src/vpn/*.sh \
