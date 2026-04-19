3e8f93faa22a2edd37a9399afabb36017967f7fe
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-19 19:54:49 +0000

    althttpd: 0-unstable-2026-01-27 -> 0-unstable-2026-04-19

--- pkgs/by-name/al/althttpd/package.nix
+++ pkgs/by-name/al/althttpd/package.nix
@@ -8,12 +8,12 @@
 
 stdenv.mkDerivation {
   pname = "althttpd";
-  version = "0-unstable-2026-01-27";
+  version = "0-unstable-2026-04-19";
 
   src = fetchfossil {
     url = "https://sqlite.org/althttpd/";
-    rev = "7758535a137da507";
-    hash = "sha256-9yszcoYpbM9/KDn7zpWgDINZF6azkQVxwlOw7BvRo+4=";
+    rev = "964440ac17a9c93f";
+    hash = "sha256-keVrYIFfF52Or9Rzq9ZH6vWLQVMOImrpPEF63B4ps7I=";
   };
 
   buildInputs = [ openssl ];
