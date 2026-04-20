ed8a5e787a9938ba34d50f7bd3829819b71170c4
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-04-21 01:20:00 +0200

    python3Packages.pydantic-ai-slim: 1.82.0 -> 1.84.1
    https://github.com/pydantic/pydantic-ai/compare/v1.82.0...v1.84.1

--- pkgs/development/python-modules/pydantic-ai-slim/default.nix
+++ pkgs/development/python-modules/pydantic-ai-slim/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pydantic-ai-slim";
-  version = "1.82.0";
+  version = "1.84.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "pydantic";
     repo = "pydantic-ai";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-M6CIoSHxxfWGU7zS4uZefRzP1vX/9Z3J1p4wmH2uiRg=";
+    hash = "sha256-LMfRGmLr51DzgJO97I/mJvyrVYD9tsF9OkCfw0ABWfw=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/pydantic_ai_slim";
