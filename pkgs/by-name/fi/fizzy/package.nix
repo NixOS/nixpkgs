dbc81de777cf2357d7f59c9827e650b3974de95a
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-20 00:11:16 -0400

    fizzy: use actual build pipeline

--- pkgs/by-name/fi/fizzy/package.nix
+++ pkgs/by-name/fi/fizzy/package.nix
@@ -1,25 +1,67 @@
 {
   lib,
-  bundlerApp,
+  stdenv,
+  fetchFromGitHub,
+  bundlerEnv,
   bundlerUpdateScript,
-  ruby,
+  ruby_3_4,
   makeWrapper,
   nodejs,
   thruster,
   sqlite,
   vips,
   openssl,
+  libyaml,
   pkg-config,
-  nixosTests,
+  perl,
 }:
 
-bundlerApp {
+let
+  addDeps = nativeBuildInputs: buildInputs: attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ nativeBuildInputs;
+    buildInputs = attrs.buildInputs or [ ] ++ buildInputs;
+  };
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "fizzy";
-  exes = [ "rails" ];
+  version = "unstable-2026-03-03";
 
-  inherit ruby;
+  src = fetchFromGitHub {
+    owner = "basecamp";
+    repo = "fizzy";
+    rev = "6e3ee266ec28053bdc8f49aa5f72f16e638dd8a4";
+    hash = "sha256-fWskgVBUgPIpeLI82UZrBmmYAKe0cCaMajQ+lZC//N0=";
+  };
 
-  gemdir = ./.;
+  rubyEnv = bundlerEnv {
+    name = "fizzy-gems-${finalAttrs.version}";
+    ruby = ruby_3_4;
+    gemdir = finalAttrs.src;
+    gemset = ./gemdir/gemset.nix;
+    gemConfig = {
+      mittens = addDeps [ perl ] [ ];
+      psych = addDeps [ pkg-config ] [ libyaml ];
+      openssl =
+        attrs:
+        (addDeps [ pkg-config ] [ ] attrs)
+        // {
+          buildFlags = (attrs.buildFlags or [ ]) ++ [
+            "--with-openssl-include=${openssl.dev}/include"
+            "--with-openssl-lib=${openssl.out}/lib"
+          ];
+        };
+      trilogy = addDeps [ pkg-config ] [ openssl ];
+      sqlite3 =
+        attrs:
+        (addDeps [ pkg-config ] [ sqlite ] attrs)
+        // {
+          buildFlags = [
+            "--with-sqlite3-include=${sqlite.dev}/include"
+            "--with-sqlite3-lib=${sqlite.out}/lib"
+          ];
+        };
+    };
+  };
 
   nativeBuildInputs = [
     makeWrapper
@@ -27,34 +69,68 @@ bundlerApp {
   ];
 
   buildInputs = [
+    finalAttrs.rubyEnv
     sqlite
     vips
     openssl
   ];
 
-  postBuild = ''
-    # Wrap the rails binary to ensure runtime deps are on PATH
-    wrapProgram $out/bin/rails \
+  env = {
+    RAILS_ENV = "production";
+    BUNDLE_WITHOUT = "development:test";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+
+    patchShebangs bin/
+
+    bundle exec bootsnap precompile --gemfile app/ lib/
+    SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r --no-preserve=mode,ownership . "$out"/
+
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    makeWrapper $out/bin/thrust $out/bin/fizzy-server \
       --prefix PATH : ${
         lib.makeBinPath [
-          thruster
           nodejs
+          thruster
         ]
       } \
-      --set RAILS_ENV production \
-      --set LD_PRELOAD ""
+      --set-default RAILS_ENV production \
+      --set BUNDLE_GEMFILE ${finalAttrs.rubyEnv.confFiles}/Gemfile \
+      --set GEM_HOME ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath} \
+      --set GEM_PATH ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath} \
+      --add-flags "$out/bin/rails server"
 
-    # Install the docker-entrypoint equivalent as a helper script
-    makeWrapper ${thruster}/bin/thruster $out/bin/fizzy-server \
-      --add-flags "$out/bin/rails server" \
-      --set RAILS_ENV production
+    for exe in rails rake thrust; do
+      wrapProgram $out/bin/$exe \
+        --prefix PATH : ${
+          lib.makeBinPath [
+            nodejs
+            thruster
+          ]
+        } \
+        --set-default RAILS_ENV production \
+        --set BUNDLE_GEMFILE ${finalAttrs.rubyEnv.confFiles}/Gemfile \
+        --set GEM_HOME ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath} \
+        --set GEM_PATH ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath}
+    done
   '';
 
   passthru = {
     updateScript = bundlerUpdateScript "fizzy";
-    tests = lib.optionalAttrs (nixosTests ? fizzy) {
-      inherit (nixosTests) fizzy;
-    };
   };
 
   meta = {
@@ -67,8 +143,9 @@ bundlerApp {
     '';
     homepage = "https://fizzy.do/";
     changelog = "https://github.com/basecamp/fizzy/releases";
-    license = lib.licenses.unfree; # O'Saasy License
+    license = lib.licenses.unfree;
     maintainers = with lib.maintainers; [ philocalyst ];
-    mainProgram = "rails";
+    mainProgram = "fizzy-server";
+    platforms = lib.platforms.unix;
   };
-}
+})
