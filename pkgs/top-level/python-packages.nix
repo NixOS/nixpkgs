7f3c12488ccf20156e9b5b7d6bbc77f671433583
Jasper Chan <jasperchan515@gmail.com>
2026-04-20 15:15:40 -0700

    python3Packages.bravado: init at 12.0.1
    Co-authored-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2291,6 +2291,8 @@ self: super: with self; {
 
   braq = callPackage ../development/python-modules/braq { };
 
+  bravado = callPackage ../development/python-modules/bravado { };
+
   bravado-core = callPackage ../development/python-modules/bravado-core { };
 
   bravia-tv = callPackage ../development/python-modules/bravia-tv { };
