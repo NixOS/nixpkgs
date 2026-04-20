cdb3de6e11403881dfa5bcd6b4f6ae01c2b945e4
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-04-20 09:05:44 -0400

    fizzy: general cleanup and patching with nix pathing

--- pkgs/by-name/fi/fizzy/package.nix
+++ pkgs/by-name/fi/fizzy/package.nix
@@ -17,135 +17,171 @@
 }:
 
 let
+  runtimeLibPath = lib.makeLibraryPath [ vips ];
+
   addDeps = nativeBuildInputs: buildInputs: attrs: {
-    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ nativeBuildInputs;
-    buildInputs = attrs.buildInputs or [ ] ++ buildInputs;
+    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ nativeBuildInputs;
+    buildInputs = (attrs.buildInputs or [ ]) ++ buildInputs;
   };
 in
-stdenv.mkDerivation (finalAttrs: {
-  pname = "fizzy";
-  version = "unstable-2026-03-03";
+stdenv.mkDerivation (
+  finalAttrs:
+  let
+    gemHome = "${finalAttrs.rubyEnv}/${ruby_3_4.gemPath}";
+    runtimeBinPath = lib.makeBinPath [
+      finalAttrs.rubyEnv.wrappedRuby
+      nodejs
+      thruster
+    ];
 
-  src = fetchFromGitHub {
-    owner = "basecamp";
-    repo = "fizzy";
-    rev = "6e3ee266ec28053bdc8f49aa5f72f16e638dd8a4";
-    hash = "sha256-fWskgVBUgPIpeLI82UZrBmmYAKe0cCaMajQ+lZC//N0=";
-  };
-
-  rubyEnv = bundlerEnv {
-    name = "fizzy-gems-${finalAttrs.version}";
-    ruby = ruby_3_4;
-    gemdir = finalAttrs.src;
-    gemset = ./gemdir/gemset.nix;
-    gemConfig = {
-      mittens = addDeps [ perl ] [ ];
-      psych = addDeps [ pkg-config ] [ libyaml ];
-      openssl =
-        attrs:
-        (addDeps [ pkg-config ] [ ] attrs)
-        // {
-          buildFlags = (attrs.buildFlags or [ ]) ++ [
-            "--with-openssl-include=${openssl.dev}/include"
-            "--with-openssl-lib=${openssl.out}/lib"
-          ];
-        };
-      trilogy = addDeps [ pkg-config ] [ openssl ];
-      sqlite3 =
-        attrs:
-        (addDeps [ pkg-config ] [ sqlite ] attrs)
-        // {
-          buildFlags = [
-            "--with-sqlite3-include=${sqlite.dev}/include"
-            "--with-sqlite3-lib=${sqlite.out}/lib"
-          ];
-        };
+    commonEnv = {
+      RAILS_ENV = "production";
+      BUNDLE_WITHOUT = "development:test";
+      BUNDLE_GEMFILE = "${finalAttrs.rubyEnv.confFiles}/Gemfile";
+      BUNDLE_FROZEN = "1";
+      BUNDLE_FORCE_RUBY_PLATFORM = "true";
+      GEM_HOME = gemHome;
+      GEM_PATH = gemHome;
+      LD_LIBRARY_PATH = runtimeLibPath;
+      DYLD_FALLBACK_LIBRARY_PATH = runtimeLibPath;
     };
-  };
 
-  nativeBuildInputs = [
-    makeWrapper
-    pkg-config
-  ];
+    commonWrapArgs = [
+      "--prefix"
+      "PATH"
+      ":"
+      runtimeBinPath
+      "--chdir"
+      (placeholder "out")
+      "--set-default"
+      "RAILS_ENV"
+      commonEnv.RAILS_ENV
+      "--set"
+      "BUNDLE_GEMFILE"
+      commonEnv.BUNDLE_GEMFILE
+      "--set"
+      "BUNDLE_FORCE_RUBY_PLATFORM"
+      commonEnv.BUNDLE_FORCE_RUBY_PLATFORM
+      "--set"
+      "GEM_HOME"
+      commonEnv.GEM_HOME
+      "--set"
+      "GEM_PATH"
+      commonEnv.GEM_PATH
+      "--set"
+      "LD_LIBRARY_PATH"
+      commonEnv.LD_LIBRARY_PATH
+      "--set"
+      "DYLD_FALLBACK_LIBRARY_PATH"
+      commonEnv.DYLD_FALLBACK_LIBRARY_PATH
+    ];
+  in
+  {
+    pname = "fizzy";
+    version = "unstable-2026-03-03";
 
-  buildInputs = [
-    finalAttrs.rubyEnv
-    sqlite
-    vips
-    openssl
-  ];
+    src = fetchFromGitHub {
+      owner = "basecamp";
+      repo = "fizzy";
+      rev = "6e3ee266ec28053bdc8f49aa5f72f16e638dd8a4";
+      hash = "sha256-fWskgVBUgPIpeLI82UZrBmmYAKe0cCaMajQ+lZC//N0=";
+    };
 
-  env = {
-    RAILS_ENV = "production";
-    BUNDLE_WITHOUT = "development:test";
-  };
+    rubyEnv = bundlerEnv {
+      name = "fizzy-gems-${finalAttrs.version}";
+      ruby = ruby_3_4;
+      gemdir = finalAttrs.src;
+      gemfile = ./gemdir/Gemfile;
+      lockfile = ./gemdir/Gemfile.lock;
+      gemset = ./gemdir/gemset.nix;
+      ignoreCollisions = true;
+      gemConfig = {
+        mittens = addDeps [ perl ] [ ];
+        psych = addDeps [ pkg-config ] [ libyaml ];
+        openssl =
+          attrs:
+          (addDeps [ pkg-config ] [ ] attrs)
+          // {
+            buildFlags = (attrs.buildFlags or [ ]) ++ [
+              "--with-openssl-include=${openssl.dev}/include"
+              "--with-openssl-lib=${openssl.out}/lib"
+            ];
+          };
+        trilogy = addDeps [ pkg-config ] [ openssl ];
+        sqlite3 =
+          attrs:
+          (addDeps [ pkg-config ] [ sqlite ] attrs)
+          // {
+            buildFlags = [
+              "--with-sqlite3-include=${sqlite.dev}/include"
+              "--with-sqlite3-lib=${sqlite.out}/lib"
+            ];
+          };
+      };
+    };
 
-  buildPhase = ''
-    runHook preBuild
+    nativeBuildInputs = [
+      makeWrapper
+      pkg-config
+    ];
 
-    patchShebangs bin/
+    buildInputs = [
+      finalAttrs.rubyEnv
+      sqlite
+      vips
+      openssl
+    ];
 
-    bundle exec bootsnap precompile --gemfile app/ lib/
-    SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
+    env = commonEnv;
 
-    runHook postBuild
-  '';
+    buildPhase = ''
+      runHook preBuild
 
-  installPhase = ''
-    runHook preInstall
+      patchShebangs bin/
 
-    mkdir -p $out
-    cp -r --no-preserve=mode,ownership . "$out"/
+      bundle exec bootsnap precompile --gemfile app/ lib/
+      SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
 
-    runHook postInstall
-  '';
-
-  postInstall = ''
-    makeWrapper $out/bin/thrust $out/bin/fizzy-server \
-      --prefix PATH : ${
-        lib.makeBinPath [
-          nodejs
-          thruster
-        ]
-      } \
-      --set-default RAILS_ENV production \
-      --set BUNDLE_GEMFILE ${finalAttrs.rubyEnv.confFiles}/Gemfile \
-      --set GEM_HOME ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath} \
-      --set GEM_PATH ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath} \
-      --add-flags "$out/bin/rails server"
-
-    for exe in rails rake thrust; do
-      wrapProgram $out/bin/$exe \
-        --prefix PATH : ${
-          lib.makeBinPath [
-            nodejs
-            thruster
-          ]
-        } \
-        --set-default RAILS_ENV production \
-        --set BUNDLE_GEMFILE ${finalAttrs.rubyEnv.confFiles}/Gemfile \
-        --set GEM_HOME ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath} \
-        --set GEM_PATH ${finalAttrs.rubyEnv}/${ruby_3_4.gemPath}
-    done
-  '';
-
-  passthru = {
-    updateScript = bundlerUpdateScript "fizzy";
-  };
-
-  meta = {
-    description = "Kanban tracking tool for issues and ideas by 37signals";
-    longDescription = ''
-      Fizzy is an open-source Kanban tracking tool for issues and ideas,
-      originally built and used by 37signals. It supports multi-tenancy,
-      SQLite and MySQL, web push notifications (VAPID), and deployment
-      via Kamal or Docker.
+      runHook postBuild
     '';
-    homepage = "https://fizzy.do/";
-    changelog = "https://github.com/basecamp/fizzy/releases";
-    license = lib.licenses.unfree;
-    maintainers = with lib.maintainers; [ philocalyst ];
-    mainProgram = "fizzy-server";
-    platforms = lib.platforms.unix;
-  };
-})
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp -r --no-preserve=ownership . "$out"/
+
+      runHook postInstall
+    '';
+
+    postInstall = ''
+      makeWrapper $out/bin/thrust $out/bin/fizzy-server \
+        ${lib.escapeShellArgs commonWrapArgs} \
+        --add-flags "$out/bin/rails server"
+
+      for exe in rails rake thrust; do
+        wrapProgram $out/bin/$exe ${lib.escapeShellArgs commonWrapArgs}
+      done
+    '';
+
+    passthru = {
+      updateScript = bundlerUpdateScript "fizzy";
+    };
+
+    meta = {
+      description = "Kanban tracking tool for issues and ideas";
+      longDescription = ''
+        Fizzy is an open-source Kanban tracking tool for issues and ideas,
+        originally built and used by 37signals. It supports multi-tenancy,
+        SQLite and MySQL, web push notifications (VAPID), and deployment
+        via Kamal or Docker.
+      '';
+      homepage = "https://fizzy.do/";
+      changelog = "https://github.com/basecamp/fizzy/releases";
+      license = lib.licenses.unfree;
+      maintainers = with lib.maintainers; [ philocalyst ];
+      mainProgram = "fizzy-server";
+      platforms = lib.platforms.unix; # Builds on Darwin but explicitly only enabled for Linux
+    };
+  }
+)
