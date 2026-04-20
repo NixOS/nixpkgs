4def2c9944a7eaa716b736da7227bc9b4d38e4d6
Guy Chronister <guychronister@outlook.com>
2026-04-20 08:28:41 -0500

    linuxPackages.vhba: add updateScript

--- pkgs/applications/emulators/cdemu/vhba.nix
+++ pkgs/applications/emulators/cdemu/vhba.nix
@@ -4,6 +4,7 @@
   fetchurl,
   kernel,
   kernelModuleMakeFlags,
+  writeScript,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -20,6 +21,20 @@ stdenv.mkDerivation (finalAttrs: {
     "INSTALL_MOD_PATH=$(out)"
   ];
   nativeBuildInputs = kernel.moduleBuildDependencies;
+
+  passthru = {
+    updateScript = writeScript "update-vhba-module" ''
+      #!/usr/bin/env nix-shell
+      #!nix-shell -i bash -p curl pcre2 common-updater-scripts
+
+      set -eu -o pipefail
+
+      # Fetch the latest version from the SourceForge RSS feed for vhba-module
+      newVersion="$(curl -s "https://sourceforge.net/projects/cdemu/rss?path=/vhba-module" | pcre2grep -o1 'vhba-module-([0-9.]+)\.tar\.xz' | head -n 1)"
+
+      update-source-version linuxPackages.vhba "$newVersion"
+    '';
+  };
 
   meta = {
     description = "Provides a Virtual (SCSI) HBA";
