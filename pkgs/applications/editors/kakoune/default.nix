3553e2402bb86f2cc6ec51f4a6d9776c32b5b02e
Christopher Fredén <christopher.freden@zenseact.com>
2026-04-20 13:59:43 +0200

    kakoune: 2025.06.03 -> 2026.04.12

--- pkgs/applications/editors/kakoune/default.nix
+++ pkgs/applications/editors/kakoune/default.nix
@@ -2,16 +2,17 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  nix-update-script,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "kakoune-unwrapped";
-  version = "2025.06.03";
+  version = "2026.04.12";
   src = fetchFromGitHub {
     repo = "kakoune";
     owner = "mawww";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-AJvh6NVFpbPsyzfeii/5muE+i4TwfxhwhDVgMLYOJCM=";
+    hash = "sha256-m8q1+TooDREbQD848ciHxeHwMajmlmhAhCqGmdbThIU=";
   };
 
   makeFlags = [
@@ -41,6 +42,10 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir autoload
     ln -s --relative "$autoload_target" autoload
   '';
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [ "--version-regex=v(.*)" ];
+  };
 
   meta = {
     homepage = "http://kakoune.org/";
