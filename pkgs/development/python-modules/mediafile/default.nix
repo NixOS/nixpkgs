3eae111e1d430af0ad7ef99571f93aa1823a1701
Doron Behar <doron.behar@gmail.com>
2026-04-20 09:38:35 +0300

    python3.pkgs.mediafile: 0.16.0 -> 0.16.2
    Diff: https://github.com/beetbox/mediafile/compare/v0.16.0...v0.16.2
    
    Changelog: https://github.com/beetbox/mediafile/releases/tag/v0.16.2

--- pkgs/development/python-modules/mediafile/default.nix
+++ pkgs/development/python-modules/mediafile/default.nix
@@ -10,14 +10,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "mediafile";
-  version = "0.16.0";
+  version = "0.16.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "beetbox";
     repo = "mediafile";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-GKEm2LKR3F9uy3FdhvpLPE9Auca8+40Zp53yaLk45XE=";
+    hash = "sha256-H7WVA5JF6bmKCLV0siGt8Jp+WE1q8z4aQrugOUW06K0=";
   };
 
   build-system = [ poetry-core ];
