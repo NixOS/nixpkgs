7605e44c644147eacec5c5372934f2f70f3c7080
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-20 21:20:11 +0000

    python3Packages.fastcore: 1.12.38 -> 1.12.39

--- pkgs/development/python-modules/fastcore/default.nix
+++ pkgs/development/python-modules/fastcore/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "fastcore";
-  version = "1.12.38";
+  version = "1.12.39";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "fastai";
     repo = "fastcore";
     tag = finalAttrs.version;
-    hash = "sha256-xqME3sz7AqGsIuTb8Ge4ByDi4sfwKo/TXEAw6/cTTmU=";
+    hash = "sha256-4pa0h+8V7E3CJtJguM/OhEK61OeK/+JKEL7fNsWIKek=";
   };
 
   build-system = [ setuptools ];
