df84d2811b3656671ba13e2679dc52e63506fe83
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-20 12:54:19 +0000

    checkip: 0.49.0 -> 0.50.0

--- pkgs/by-name/ch/checkip/package.nix
+++ pkgs/by-name/ch/checkip/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "checkip";
-  version = "0.49.0";
+  version = "0.50.0";
 
   src = fetchFromGitHub {
     owner = "jreisinger";
     repo = "checkip";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zhc32H4EUjFbU5weab+IQYARSrJXD8zqkxHLgO5jIJs=";
+    hash = "sha256-tf/PqMAOlFJrUFg5yPdARY8zTh/SjlNHp2LAX9f7QqM=";
   };
 
   vendorHash = "sha256-5sUBrzo6wJfaMMvgNflcjB2QNSIeaD2TN7qBao53NFs=";
