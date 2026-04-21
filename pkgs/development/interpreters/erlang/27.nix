232b4131925e21bfae5caf41a49b726f10f9c5a3
Adam C. Stephens <adam@valkor.net>
2026-04-21 13:23:50 -0400

    erlang_27: 27.3.4.10 -> 27.3.4.11
    Changelog: https://github.com/erlang/otp/releases/tag/OTP-27.3.4.11

--- pkgs/development/interpreters/erlang/27.nix
+++ pkgs/development/interpreters/erlang/27.nix
@@ -1,6 +1,6 @@
 genericBuilder:
 
 genericBuilder {
-  version = "27.3.4.10";
-  hash = "sha256-x1f9r1t5Ckk+AVn2P372aWLoDtgqZzNIeOuzjI6hm+g=";
+  version = "27.3.4.11";
+  hash = "sha256-yOgsaeUv6GwFX6qPVb28zet6Hli5vg/PZCKW2s2/JEA=";
 }
