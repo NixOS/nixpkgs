193e6decd7a92dcf3a880f7bb9fcde8d45e36202
0xB10C <b10c@b10c.me>
2026-04-21 16:02:24 +0200

    Revert "bitcoin: Remove zlib transistive dependency from bitcoin"
    This reverts commit 57b26dec73a9f26c301322e4ceec0618a51994aa as it broke
    the bitcoind build. See
    
    - https://github.com/NixOS/nixpkgs/pull/474827#issuecomment-4287223019
    - https://github.com/NixOS/nixpkgs/pull/511775#issuecomment-4287233678

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10898,9 +10898,6 @@ with pkgs;
 
   bitcoin = qt6Packages.callPackage ../applications/blockchains/bitcoin {
     withGui = true;
-    sqlite = sqlite.override {
-      zlib = null;
-    };
     zeromq = zeromq.override {
       enableCurve = false;
       enableDrafts = false;
