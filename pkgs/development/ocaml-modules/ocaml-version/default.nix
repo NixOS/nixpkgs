0ad98ccec10e4a6a9b318dbea500f31bef8a9cb0
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-21 01:51:37 +0000

    ocamlPackages.ocaml-version: 4.0.4 -> 4.1.0

--- pkgs/development/ocaml-modules/ocaml-version/default.nix
+++ pkgs/development/ocaml-modules/ocaml-version/default.nix
@@ -7,11 +7,11 @@
 
 buildDunePackage rec {
   pname = "ocaml-version";
-  version = "4.0.4";
+  version = "4.1.0";
 
   src = fetchurl {
     url = "https://github.com/ocurrent/ocaml-version/releases/download/v${version}/ocaml-version-${version}.tbz";
-    hash = "sha256-6sviBLiEjcCtLcnk74vGy4ZTALVd1Rd5INUzAzn+HO4=";
+    hash = "sha256-QTfVH6kNu4SkjAylM3ySyIkOYAXQFrffSFkZ2FojjgY=";
   };
 
   checkInputs = [ alcotest ];
