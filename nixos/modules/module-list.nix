0f1b2eced499125a0903f41e4a09b062132958b0
phanirithvij <phanirithvij2000@gmail.com>
2026-04-21 15:10:31 +0530

    nixos/funkwhale: init module
    Co-Authored-By: June Stepp <git@junestepp.me>
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1718,6 +1718,7 @@
   ./services/web-apps/freescout.nix
   ./services/web-apps/freshrss.nix
   ./services/web-apps/froide-govplan.nix
+  ./services/web-apps/funkwhale.nix
   ./services/web-apps/galene.nix
   ./services/web-apps/gancio.nix
   ./services/web-apps/geoserver.nix
