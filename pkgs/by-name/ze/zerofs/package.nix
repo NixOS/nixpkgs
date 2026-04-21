ae8ec0c1c153ab911ceb924bf40a593969ff4d93
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-21 03:19:52 +0000

    zerofs: 1.0.8 -> 1.0.14

--- pkgs/by-name/ze/zerofs/package.nix
+++ pkgs/by-name/ze/zerofs/package.nix
@@ -10,18 +10,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zerofs";
-  version = "1.0.8";
+  version = "1.0.14";
 
   src = fetchFromGitHub {
     owner = "Barre";
     repo = "ZeroFS";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-iMLms2UY4Ko2JMgkYEF8SlES4wYSWBiRQtKXUzi9iiQ=";
+    hash = "sha256-DsB3bnx2FFVlaqogAw8kmhSl4/kaY7OOqPAPadQJGqs=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/zerofs";
 
-  cargoHash = "sha256-9rR3Za3pnlh/t/5tBbIbhwSGvPpQ9VA4Z0vG7HNIPu8=";
+  cargoHash = "sha256-aX6TYqakp/ePtuWjvnHXWAVs0mQty4c63OJ71IFMyjc=";
 
   nativeBuildInputs = [ cmake ];
 
