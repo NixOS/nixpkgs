193c9ed2b1a2674da3442ccc0289d236086298e8
Parth Kalgaonkar <parthkalgaoooonkar@gmail.com>
2026-04-22 04:40:53 +0530

    maintainers: add parthkalgaonkar

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20823,6 +20823,12 @@
     githubId = 20658472;
     name = "Parthiv Krishna";
   };
+  parthkalgaonkar = {
+    name = "Parth Kalgaonkar";
+    email = "parthkalgaonkar@gmail.com";
+    github = "parthkalgaonkar";
+    githubId = 25819547;
+  };
   pascalj = {
     email = "nix@pascalj.de";
     github = "pascalj";
