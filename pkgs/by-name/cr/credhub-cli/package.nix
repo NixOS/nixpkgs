c4b3bcc39376fe56f61fde6796cfc6da350debed
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-22 15:36:53 +0000

    credhub-cli: 2.9.54 -> 2.9.56

--- pkgs/by-name/cr/credhub-cli/package.nix
+++ pkgs/by-name/cr/credhub-cli/package.nix
@@ -6,13 +6,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "credhub-cli";
-  version = "2.9.54";
+  version = "2.9.56";
 
   src = fetchFromGitHub {
     owner = "cloudfoundry-incubator";
     repo = "credhub-cli";
     rev = finalAttrs.version;
-    sha256 = "sha256-qp7Oj207uu2P/Jt9O5tZM0ra9fMx+DfuHPaKr5z+ef0=";
+    sha256 = "sha256-4a7nhuc9ucGBhC+jQkEtQZV3uaWEmLojhw/ISa2jGCg=";
   };
 
   # these tests require network access that we're not going to give them
