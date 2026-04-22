339c482362aa2bc4c199a1aeff5d6c541cf7305b
Jack Rosenberg <mail@jackr.eu>
2026-04-22 13:58:20 +0200

    highs: 1.12.0 -> 1.14.0

--- pkgs/by-name/hi/highs/package.nix
+++ pkgs/by-name/hi/highs/package.nix
@@ -9,12 +9,14 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "highs";
   version = "1.12.0";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "ERGO-Code";
     repo = "HiGHS";
     rev = "v${finalAttrs.version}";
     hash = "sha256-FRiYtbl1kWEkHHEIIOpefC9UdusmJKl6UmP3dKRkAXA=";
+    hash = "sha256-0KmA5B2g3AFCxMbN9gHdXxAEftZglhQKOqj1/TMxxps=";
   };
 
   strictDeps = true;
