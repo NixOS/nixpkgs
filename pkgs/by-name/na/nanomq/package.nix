fc350a585ba9c008c4168c48c040785035e8123e
Nikolay Korotkiy <sikmir@disroot.org>
2026-04-22 23:17:22 +0400

    nanomq: 0.24.11 → 0.24.14

--- pkgs/by-name/na/nanomq/package.nix
+++ pkgs/by-name/na/nanomq/package.nix
@@ -50,13 +50,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "nanomq";
-  version = "0.24.11";
+  version = "0.24.14";
 
   src = fetchFromGitHub {
     owner = "emqx";
     repo = "nanomq";
     tag = finalAttrs.version;
-    hash = "sha256-I2SLc/KbkBvqbbWuLr8ARmmg4DeE7ZbTqcM1tw8WhwQ=";
+    hash = "sha256-SRW0yla3sqkojPXl37uC1ipG1neBETL8+bPicMKquis=";
     fetchSubmodules = true;
   };
 
@@ -135,9 +135,5 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ sikmir ];
     platforms = lib.platforms.unix;
-    knownVulnerabilities = [
-      "CVE-2026-22040"
-      "CVE-2025-68699"
-    ];
   };
 })
