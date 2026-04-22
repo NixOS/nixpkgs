26e04875945079803409df348b046be141adc129
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-22 18:24:24 +0000

    zwave-js-ui: 11.16.0 -> 11.16.1

--- pkgs/by-name/zw/zwave-js-ui/package.nix
+++ pkgs/by-name/zw/zwave-js-ui/package.nix
@@ -7,15 +7,15 @@
 
 buildNpmPackage rec {
   pname = "zwave-js-ui";
-  version = "11.16.0";
+  version = "11.16.1";
 
   src = fetchFromGitHub {
     owner = "zwave-js";
     repo = "zwave-js-ui";
     tag = "v${version}";
-    hash = "sha256-6pPC500ZQmtCC3ATiWD79DTh5cagdqLoDwTHivHgfWg=";
+    hash = "sha256-GgKQN55WZxdOJxL11KxObzln+Z2KhYoueAld+nz7dl4=";
   };
-  npmDepsHash = "sha256-Qeh5sk2aLWrsmMShE26jz7nzWg6YWcPeUNhTg3u411I=";
+  npmDepsHash = "sha256-Cp3EYWiyPx5ATD2LyKPSXw4PZ+oqfKHycc9zhn89r5w=";
 
   passthru.tests.zwave-js-ui = nixosTests.zwave-js-ui;
 
