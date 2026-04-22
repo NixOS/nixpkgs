f0579ae3ebd3714b0418d8252c09cdf8b8d63d41
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-22 19:21:13 +0000

    grimblast: 0.1-unstable-2026-03-28 -> 0.1-unstable-2026-04-17

--- pkgs/by-name/gr/grimblast/package.nix
+++ pkgs/by-name/gr/grimblast/package.nix
@@ -20,13 +20,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "grimblast";
-  version = "0.1-unstable-2026-03-28";
+  version = "0.1-unstable-2026-04-17";
 
   src = fetchFromGitHub {
     owner = "hyprwm";
     repo = "contrib";
-    rev = "b3c2a094163cad4aac7af09333fddf2c9838c348";
-    hash = "sha256-xr3Ih1HrAIlO9GGrmnVrmRh50GSPMWI4QWmbEwlbYq8=";
+    rev = "1f71628d86a7701fd5ba0f8aeabe15376f4c6afc";
+    hash = "sha256-3rROoGl8xBsIOM+5m+qZS4GJnsdQPAH3NJJe1OUfJ5o=";
   };
 
   strictDeps = true;
