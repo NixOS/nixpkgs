a94a0a5b61228728370d081b59871a7877329f31
andre4ik3 <andre4ik3@fastmail.com>
2026-04-22 08:20:53 +0000

    nixos/zapret2: init module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1462,6 +1462,7 @@
   ./services/networking/xrdp.nix
   ./services/networking/yggdrasil-jumper.nix
   ./services/networking/yggdrasil.nix
+  ./services/networking/zapret2.nix
   ./services/networking/zapret.nix
   ./services/networking/zenohd.nix
   ./services/networking/zerobin.nix
