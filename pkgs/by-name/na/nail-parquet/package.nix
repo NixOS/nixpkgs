25729183f30b7b814ea4df7e7f61b787420adcf6
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-22 13:51:07 +0000

    nail-parquet: 1.6.5 -> 1.7.0

--- pkgs/by-name/na/nail-parquet/package.nix
+++ pkgs/by-name/na/nail-parquet/package.nix
@@ -11,16 +11,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nail-parquet";
-  version = "1.6.5";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "Vitruves";
     repo = "nail-parquet";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-CPiOeaESerQj+nV0hQIGv06/MFP8s7p9olpmhnWpAAg=";
+    hash = "sha256-oY+C1knZRBvZR/4mjq3wv6ZntD6+tLJFfplRBoAkds0=";
   };
 
-  cargoHash = "sha256-x4BJZcQkisw9hA/TBzSSdkxh7oUNL0OD3H/v67otYj8=";
+  cargoHash = "sha256-cPRbQBxUL+5z1Hi6asXMKb8NPxNjmc+3gIa5ZKEmqWM=";
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [
     pkg-config
