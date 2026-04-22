731afede0ec3f1e3ac7744d8d2ec29cd5eaf88f4
Thomas Butter <tbutter@gmail.com>
2026-04-22 16:30:27 +0000

    httrack: 3.49.2 -> 3.49.6

--- pkgs/by-name/ht/httrack/package.nix
+++ pkgs/by-name/ht/httrack/package.nix
@@ -1,19 +1,23 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
   zlib,
   openssl,
   libiconv,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
-  version = "3.49.2";
+  version = "3.49.6";
   pname = "httrack";
 
-  src = fetchurl {
-    url = "https://mirror.httrack.com/httrack-${finalAttrs.version}.tar.gz";
-    sha256 = "09a0gm67nml86qby1k1gh7rdxamnrnzwr6l9r5iiq94favjs0xrl";
+  src = fetchFromGitHub {
+    owner = "xroche";
+    repo = "httrack";
+    # 3.49.6 is not tagged, but corresponds to this rev.
+    rev = "748c35de7858ead963daf1393ad023d75b7820c2";
+    hash = "sha256-Iaeo6lB84I0amr2C8iZ+kQ6F8AXqyyARV6FiwpBshvA=";
+    fetchSubmodules = true;
   };
 
   buildInputs = [
@@ -22,12 +26,11 @@ stdenv.mkDerivation (finalAttrs: {
     zlib
   ];
 
-  enableParallelBuilding = true;
-
   meta = {
     description = "Easy-to-use offline browser / website mirroring utility";
-    homepage = "http://www.httrack.com";
+    homepage = "https://www.httrack.com";
     license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [ tbutter ];
     platforms = with lib.platforms; unix;
   };
 })
