2b4df41e03f7fc88797352421961afaec8b1ffc0
Erin <79354991+oati@users.noreply.github.com>
2026-04-23 13:49:38 -0500

    nibbler: init at 2.5.8

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -20171,6 +20171,11 @@
     github = "oaksoaj";
     githubId = 103952141;
   };
+  oati = {
+    github = "oati";
+    githubId = 79354991;
+    name = "Erin Kim";
+  };
   ob7 = {
     name = "ob7";
     email = "dev@ob7.us";
