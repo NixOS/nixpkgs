aff219d084a450d2f12fb2ee802c6f6c4934804a
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-23 00:25:01 +0000

    treemd: 0.5.9 -> 0.5.10

--- pkgs/by-name/tr/treemd/package.nix
+++ pkgs/by-name/tr/treemd/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "treemd";
-  version = "0.5.9";
+  version = "0.5.10";
 
   src = fetchFromGitHub {
     owner = "Epistates";
     repo = "treemd";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ImzHTdL1bKMV4mfl5tiTSQ89f3iUZZeFWmR74xMvP24=";
+    hash = "sha256-lt1dZW8na89wAcYkkoiNigGz8sh5dcuqoRmdV4M6fCk=";
   };
 
-  cargoHash = "sha256-KxIsZNDKgYWEdrDiwTHjORg44T3gc2hJSJe05H/Qavw=";
+  cargoHash = "sha256-mRHB/hJmpjMNrPeqz2ec78AIDvCQ1mbmfAkI+VoSqd0=";
 
   doInstallCheck = true;
   nativeInstallCheckInputs = [ versionCheckHook ];
