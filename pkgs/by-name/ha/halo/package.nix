e32534fb1901caea1ef2c27dbc913eb20b116adc
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-24 11:32:27 +0000

    halo: 2.23.1 -> 2.24.0

--- pkgs/by-name/ha/halo/package.nix
+++ pkgs/by-name/ha/halo/package.nix
@@ -8,10 +8,10 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "halo";
-  version = "2.23.1";
+  version = "2.24.0";
   src = fetchurl {
     url = "https://github.com/halo-dev/halo/releases/download/v${finalAttrs.version}/halo-${finalAttrs.version}.jar";
-    hash = "sha256-oU3MdmHnpf0TOwdEJb8UD2LIqEJ2BQ0puMIs1BBmA2M=";
+    hash = "sha256-71br2gG8vl3EyvC+AYzqJOtgHnhdEmcRAjXSXXXqI5s=";
   };
 
   nativeBuildInputs = [
