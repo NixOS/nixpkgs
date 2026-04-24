a4b2047641279edf0342cf74a8f3a2e7ff22a414
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-24 19:30:56 +0000

    roxterm: 3.17.2 -> 3.18.1

--- pkgs/by-name/ro/roxterm/package.nix
+++ pkgs/by-name/ro/roxterm/package.nix
@@ -31,13 +31,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "roxterm";
-  version = "3.17.2";
+  version = "3.18.1";
 
   src = fetchFromGitHub {
     owner = "realh";
     repo = "roxterm";
     rev = finalAttrs.version;
-    hash = "sha256-QMWxNgMbodkyUDG2o7nrnVZiWFpIYTdphU9yDEhzKNM=";
+    hash = "sha256-vxuY5CAeBUj7Nn7XGYlXOPImNQ0+VVm/Bln8eVk7crY=";
   };
 
   nativeBuildInputs = [
