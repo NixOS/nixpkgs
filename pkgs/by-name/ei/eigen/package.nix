0d18e2e9257a41a08684560c7631ede1c2ce1f70
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2026-04-24 21:21:53 +0200

    eigen: 3.4.1 -> 5.0.1

--- pkgs/by-name/ei/eigen/package.nix
+++ pkgs/by-name/ei/eigen/package.nix
@@ -3,7 +3,6 @@
 
   stdenv,
   fetchFromGitLab,
-  fetchpatch,
 
   # nativeBuildInputs
   cmake,
@@ -14,23 +13,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "eigen";
-  version = "3.4.1";
+  version = "5.0.1";
 
   src = fetchFromGitLab {
     owner = "libeigen";
     repo = "eigen";
     tag = finalAttrs.version;
-    hash = "sha256-NSq1tUfy2thz5gtsyASsKeYE4vMf71aSG4uXfrX86rk=";
+    hash = "sha256-8TW1MUXt2gWJmu5YbUWhdvzNBiJ/KIVwIRf2XuVZeqo=";
   };
-
-  patches = [
-    # fix bug1213 test
-    # ref https://gitlab.com/libeigen/eigen/-/merge_requests/2005 merged upstream
-    (fetchpatch {
-      url = "https://gitlab.com/libeigen/eigen/-/commit/3e1367a3b5efcdc8ce716db77a322cedeb5e01b4.patch";
-      hash = "sha256-oykUbzaZeVW1A8nBoiMtJvh68Zpu7PDFtAfAjtTQoC0=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake
@@ -44,13 +34,12 @@ stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "EIGEN_LEAVE_TEST_IN_ALL_TARGET" true) # Build tests in parallel
   ];
 
-  # too many flaky tests
-  doCheck = false;
+  doCheck = true;
 
   meta = {
     homepage = "https://eigen.tuxfamily.org";
     description = "C++ template library for linear algebra: vectors, matrices, and related algorithms";
-    license = lib.licenses.lgpl3Plus;
+    license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [
       raskin
     ];
