b2d91e208ccc54e76fe443370ccd15deb052f758
Lin Jian <me@linj.tech>
2026-04-24 21:52:45 +0800

    emacs: update patches for tree-sitter 0.26 compatibility
    The vendored patch fixing build issue is replaced by fetching from
    Gentoo.  In addition, another patch from Gentoo is added to fix the
    runtime issue.

--- pkgs/applications/editors/emacs/make-emacs.nix
+++ pkgs/applications/editors/emacs/make-emacs.nix
@@ -252,13 +252,10 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [
     makeWrapper
     pkg-config
-  ]
-  ++ lib.optionals (variant == "macport") [
     texinfo
   ]
   ++ lib.optionals srcRepo [
     autoreconfHook
-    texinfo
   ]
   ++ lib.optionals (withPgtk || withX && (withGTK3 || withXwidgets)) [ wrapGAppsHook3 ];
 
