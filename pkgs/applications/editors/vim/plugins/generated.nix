3be06844b998ed7612e1ad0d124999990eb4c521
Austin Horstman <khaneliman12@gmail.com>
2026-04-23 19:46:50 -0500

    vimPlugins.neotab-nvim: init at 0-unstable-2025-08-04
    https://github.com/kawre/neotab.nvim

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -10270,6 +10270,19 @@ final: prev: {
     meta.hydraPlatforms = [ ];
   };
 
+  neotab-nvim = buildVimPlugin {
+    pname = "neotab.nvim";
+    version = "0-unstable-2025-08-04";
+    src = fetchFromGitHub {
+      owner = "kawre";
+      repo = "neotab.nvim";
+      rev = "e99d3e28c5a2066d2d368dfe4ef3827c8d75d337";
+      hash = "sha256-pSLvpKqfi1hQrFAYmXTOUOYERtnr79M23z5so2JOGhE=";
+    };
+    meta.homepage = "https://github.com/kawre/neotab.nvim/";
+    meta.hydraPlatforms = [ ];
+  };
+
   neoterm = buildVimPlugin {
     pname = "neoterm";
     version = "0-unstable-2023-03-09";
