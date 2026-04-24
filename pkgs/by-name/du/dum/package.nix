be2af66ea819b07df5e3163436e403f7f6ad1e8b
Petr Portnov <mrjarviscraft+nix@gmail.com>
2026-04-24 21:40:59 +0300

    dum: add nix-update-script

--- pkgs/by-name/du/dum/package.nix
+++ pkgs/by-name/du/dum/package.nix
@@ -2,6 +2,8 @@
   lib,
   rustPlatform,
   fetchFromGitHub,
+  versionCheckHook,
+  nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
@@ -16,6 +18,12 @@ rustPlatform.buildRustPackage (finalAttrs: {
   };
 
   cargoHash = "sha256-CpVci0nw/6Y6uyQX6iRV9E7uXzdZ2fzYIelYxsc+tI0=";
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+  versionCheckProgramArg = "--version";
+
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "Npm scripts runner written in Rust";
