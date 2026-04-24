fcab10d1f668036af030944f7cfb15e14de8e56c
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-24 18:43:39 +0000

    kubefwd: 1.25.13 -> 1.25.14

--- pkgs/by-name/ku/kubefwd/package.nix
+++ pkgs/by-name/ku/kubefwd/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "kubefwd";
-  version = "1.25.13";
+  version = "1.25.14";
 
   src = fetchFromGitHub {
     owner = "txn2";
     repo = "kubefwd";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-fAqsBqJgDyollaJNjdXI+hv3im7v0P/+cMkd4zj10kA=";
+    hash = "sha256-fxwUolGn55gf4voGT3noz44aNMSkxZiHD6OLADJ8aGg=";
   };
 
-  vendorHash = "sha256-jMGz1pgSfr4NAOYvGRBL+A1ecWCC5Okn0vPZ1qgyxB8=";
+  vendorHash = "sha256-UL9i81ez937u2sn4ZGY89eXfTplB0LVkeuLigc0BM5Y=";
 
   subPackages = [ "cmd/kubefwd" ];
 
