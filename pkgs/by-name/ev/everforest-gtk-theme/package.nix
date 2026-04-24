c4dda2d82c9e2879259fe3e22bdeb6082594cfd1
jc29994 <15376181+jc29994@users.noreply.github.com>
2026-04-24 11:50:56 +1000

    everforest-gtk-theme: 0-unstable-2025-10-15 -> 0-unstable-2025-10-24
    The format follows nightfox and other gtk themes

--- pkgs/by-name/ev/everforest-gtk-theme/package.nix
+++ pkgs/by-name/ev/everforest-gtk-theme/package.nix
@@ -2,44 +2,121 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
+  sassc,
   gnome-themes-extra,
   gtk-engine-murrine,
+  unstableGitUpdater,
+  colorVariants ? [ ],
+  sizeVariants ? [ ],
+  themeVariants ? [ ],
+  tweakVariants ? [ ],
+  iconVariants ? [ ],
 }:
 
-stdenvNoCC.mkDerivation {
+let
   pname = "everforest-gtk-theme";
-  version = "0-unstable-2025-10-15";
-
-  src = fetchFromGitHub {
-    owner = "Fausto-Korpsvart";
-    repo = "Everforest-GTK-Theme";
-    rev = "930a5dc57f7a06e8c6538d531544e41c56dbb27a";
-    hash = "sha256-mlJE7gVElWUjJIZnAL5ztchphmaU82llol+YdKqnSxg=";
-  };
-
-  propagatedUserEnvPkgs = [
-    gtk-engine-murrine
+  colorVariantList = [
+    "dark"
+    "light"
   ];
-
-  buildInputs = [
-    gnome-themes-extra
+  sizeVariantList = [
+    "compact"
+    "standard"
   ];
+  themeVariantList = [
+    "default"
+    "green"
+    "grey"
+    "orange"
+    "pink"
+    "purple"
+    "red"
+    "teal"
+    "yellow"
+    "all"
+  ];
+  tweakVariantList = [
+    "medium"
+    "soft"
+    "black"
+    "float"
+    "outline"
+    "macos"
+  ];
+  iconVariantList = [
+    "Everforest-Dark"
+    "Everforest-Light"
+  ];
+in
+lib.checkListOfEnum "${pname}: colorVariants" colorVariantList colorVariants lib.checkListOfEnum
+  "${pname}: sizeVariants"
+  sizeVariantList
+  sizeVariants
+  lib.checkListOfEnum
+  "${pname}: themeVariants"
+  themeVariantList
+  themeVariants
+  lib.checkListOfEnum
+  "${pname}: tweakVariants"
+  tweakVariantList
+  tweakVariants
+  lib.checkListOfEnum
+  "${pname}: iconVariants"
+  iconVariantList
+  iconVariants
 
-  dontBuild = true;
+  stdenvNoCC.mkDerivation
+  {
+    inherit pname;
+    version = "0-unstable-2025-10-24";
 
-  installPhase = ''
-    runHook preInstall
-    mkdir -p "$out/share/"{themes,icons}
-    cp -a icons/* "$out/share/icons/"
-    cp -a themes/* "$out/share/themes/"
-    runHook postInstall
-  '';
+    src = fetchFromGitHub {
+      owner = "Fausto-Korpsvart";
+      repo = "Everforest-GTK-Theme";
+      rev = "9b8be4d6648ae9eaae3dd550105081f8c9054825";
+      hash = "sha256-XHO6NoXJwwZ8gBzZV/hJnVq5BvkEKYWvqLBQT00dGdE=";
+    };
 
-  meta = {
-    description = "Everforest colour palette for GTK";
-    homepage = "https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme";
-    license = lib.licenses.gpl3Only;
-    maintainers = with lib.maintainers; [ jn-sena ];
-    platforms = lib.platforms.unix;
-  };
-}
+    propagatedUserEnvPkgs = [
+      gtk-engine-murrine
+    ];
+
+    nativeBuildInputs = [ sassc ];
+    buildInputs = [
+      gnome-themes-extra
+    ];
+
+    dontBuild = true;
+
+    passthru.updateScript = unstableGitUpdater { };
+
+    postPatch = ''
+      patchShebangs themes/install.sh
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/share/themes
+      cd themes
+      ./install.sh -n Everforest \
+      ${lib.optionalString (colorVariants != [ ]) "-c " + toString colorVariants} \
+      ${lib.optionalString (sizeVariants != [ ]) "-s " + toString sizeVariants} \
+      ${lib.optionalString (themeVariants != [ ]) "-t " + toString themeVariants} \
+      ${lib.optionalString (tweakVariants != [ ]) "--tweaks " + toString tweakVariants} \
+      -d "$out/share/themes"
+      cd ../icons
+      ${lib.optionalString (iconVariants != [ ]) ''
+        mkdir -p $out/share/icons
+        cp -a ${toString (map (v: "${v}") iconVariants)} $out/share/icons/
+      ''}
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Everforest colour palette for GTK";
+      homepage = "https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme";
+      license = lib.licenses.gpl3Only;
+      maintainers = with lib.maintainers; [ jn-sena ];
+      platforms = lib.platforms.unix;
+    };
+  }
