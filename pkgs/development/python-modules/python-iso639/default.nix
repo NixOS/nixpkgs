5769517536ba8f8bb44ac12c13c7b22608ca0e96
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-25 10:05:34 +0000

    python3Packages.python-iso639: 2026.1.31 -> 2026.4.20

--- pkgs/development/python-modules/python-iso639/default.nix
+++ pkgs/development/python-modules/python-iso639/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "python-iso639";
-  version = "2026.1.31";
+  version = "2026.4.20";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jacksonllee";
     repo = "iso639";
     tag = "v${version}";
-    hash = "sha256-mavTbjf57EYUMil3DBOmTZl8WwRW7sjxddb4WuK+YlI=";
+    hash = "sha256-aXckFcWG8zcP9GELTT5eHnQzklAYG70LyX34fhVGdTo=";
   };
 
   build-system = [ setuptools ];
