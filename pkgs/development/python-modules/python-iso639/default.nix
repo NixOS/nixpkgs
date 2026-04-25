2fe9c9f8462bd6b68f4b281123a845b4247508fb
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-25 12:21:40 +0200

    python3Packages.python-iso639: migrate to finalAttrs

--- pkgs/development/python-modules/python-iso639/default.nix
+++ pkgs/development/python-modules/python-iso639/default.nix
@@ -6,7 +6,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "python-iso639";
   version = "2026.4.20";
   pyproject = true;
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "jacksonllee";
     repo = "iso639";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-aXckFcWG8zcP9GELTT5eHnQzklAYG70LyX34fhVGdTo=";
   };
 
@@ -27,10 +27,10 @@ buildPythonPackage rec {
   ];
 
   meta = {
-    changelog = "https://github.com/jacksonllee/iso639/blob/${src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/jacksonllee/iso639/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     description = "ISO 639 language codes, names, and other associated information";
     homepage = "https://github.com/jacksonllee/iso639";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ dotlambda ];
   };
-}
+})
