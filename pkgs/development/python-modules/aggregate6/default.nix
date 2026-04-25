26fe49648c7df7e6217e47d71bd8387f72bd75bb
Marcel <me@m4rc3l.de>
2026-04-25 17:06:22 +0200

    python3Packages.aggregate6: 1.0.14 -> 1.0.15
    Diff: https://github.com/job/aggregate6/compare/1.0.14...1.0.15

--- pkgs/development/python-modules/aggregate6/default.nix
+++ pkgs/development/python-modules/aggregate6/default.nix
@@ -3,33 +3,26 @@
   buildPythonPackage,
   fetchFromGitHub,
   setuptools,
-  py-radix-sr,
+  py-radix,
   versionCheckHook,
   pytestCheckHook,
 }:
 
 buildPythonPackage rec {
   pname = "aggregate6";
-  version = "1.0.14";
+  version = "1.0.15";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "job";
     repo = "aggregate6";
     rev = version;
-    hash = "sha256-sF5F4AIIQuMTuWE3zoBE1akJX9QSmAaRp1qgoHzSJMo=";
+    hash = "sha256-GXIZ2aNZUeiVkhmo2jdwIEk9jL/in2KuuKgi//TQGq0=";
   };
-
-  # py-radix-sr is a fork, with fixes
-  # NOTE: it should be worth switching to py-radix again in the future as there
-  #       is still development sadly currently without a new release.
-  postPatch = ''
-    substituteInPlace setup.py --replace-fail 'py-radix==0.10.0' 'py-radix-sr'
-  '';
 
   build-system = [ setuptools ];
 
-  dependencies = [ py-radix-sr ];
+  dependencies = [ py-radix ];
 
   nativeCheckInputs = [
     pytestCheckHook
