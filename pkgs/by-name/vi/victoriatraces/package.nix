9b04deef341432c084e5061025c74f7731f27863
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-25 17:20:41 +0000

    victoriatraces: 0.8.0 -> 0.8.1

--- pkgs/by-name/vi/victoriatraces/package.nix
+++ pkgs/by-name/vi/victoriatraces/package.nix
@@ -13,13 +13,13 @@
 
 buildGo126Module (finalAttrs: {
   pname = "VictoriaTraces";
-  version = "0.8.0";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "VictoriaMetrics";
     repo = "VictoriaTraces";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-bm1hF4LqbSxxEOMtz4jdgJHMwrahGI4VZ9nj/VscFdc=";
+    hash = "sha256-HtLgrGB04/2wYP3kmRHKDLMgzQxt9qshlYvww+ULO/E=";
   };
 
   vendorHash = null;
