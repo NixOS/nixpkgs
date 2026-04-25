b32c930a31050d147c17e7634243e2f374befc4c
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-25 12:11:48 +0200

    python3Packages.multitasking: modernize

--- pkgs/development/python-modules/multitasking/default.nix
+++ pkgs/development/python-modules/multitasking/default.nix
@@ -2,26 +2,30 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "multitasking";
   version = "0.0.13";
-  format = "setuptools";
+  pyproject = true;
 
-  # GitHub source releases aren't tagged
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-2Ja134d8nKXu3b8OWZQSRpTWy1NaummPsjNExwJRVaE=";
   };
 
+  build-system = [ setuptools ];
+
   doCheck = false; # No tests included
+
   pythonImportsCheck = [ "multitasking" ];
 
   meta = {
     description = "Non-blocking Python methods using decorators";
     homepage = "https://github.com/ranaroussi/multitasking";
+    changelog = "https://github.com/ranaroussi/multitasking/releases/tag/${finalAttrs.version}";
     license = lib.licenses.asl20;
     maintainers = [ ];
   };
-}
+})
