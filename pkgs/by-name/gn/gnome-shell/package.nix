07f8815ff56d731408af99b0a2f4416527b64267
Bobby Rong <rjl931189261@126.com>
2026-04-25 09:16:54 +0000

    gnome-shell: 50.0 → 50.1
    https://gitlab.gnome.org/GNOME/gnome-shell/-/compare/50.0...50.1

--- pkgs/by-name/gn/gnome-shell/package.nix
+++ pkgs/by-name/gn/gnome-shell/package.nix
@@ -74,7 +74,7 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-shell";
-  version = "50.0";
+  version = "50.1";
 
   outputs = [
     "out"
@@ -83,7 +83,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-shell/${lib.versions.major finalAttrs.version}/gnome-shell-${finalAttrs.version}.tar.xz";
-    hash = "sha256-hZUvvUkpz+YTuHayI34NE0ZLDCeIuKmGf7UcJgJVsOM=";
+    hash = "sha256-G0d2AXLBTz9O3Rya/zZfTeRVg1F78PgN9NOsvU5MspQ=";
   };
 
   patches = [
