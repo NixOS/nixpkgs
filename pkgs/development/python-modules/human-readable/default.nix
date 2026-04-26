1d657eb9a93b840499e6dc2e744ea5d9c507bef7
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-26 20:18:57 +0200

    python3Packages.human-readable: modernize

--- pkgs/development/python-modules/human-readable/default.nix
+++ pkgs/development/python-modules/human-readable/default.nix
@@ -6,22 +6,23 @@
   hatch-vcs,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "human-readable";
   version = "2.0.3";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "human_readable";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-P4Ef1W7oZpVyyy7J+FK1PuBwB0jlPDaVcx/9mrT8Uks=";
   };
 
-  pyproject = true;
-
-  nativeBuildInputs = [
+  build-system = [
     hatchling
     hatch-vcs
   ];
+
+  pythonImportsCheck = [ "human_readable" ];
 
   meta = {
     description = "Library to make data intended for machines, readable to humans";
@@ -29,4 +30,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ mkg20001 ];
   };
-}
+})
