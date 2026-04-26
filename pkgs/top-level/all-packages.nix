20838fca17a290836f1d17f4f825e9e1dbb2d46c
K900 <me@0upti.me>
2026-04-26 15:52:00 +0300

    tora: drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12056,8 +12056,6 @@ with pkgs;
     discord-development
     ;
 
-  tora = libsForQt5.callPackage ../development/tools/tora { };
-
   torcs-without-data = callPackage ../../pkgs/by-name/to/torcs/without-data.nix { };
 
   nitrokey-app = libsForQt5.callPackage ../tools/security/nitrokey-app { };
