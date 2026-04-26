be7b427b0209ab29db4c75ce8fef2eaf0f4edd45
Thomas Butter <tbutter@gmail.com>
2026-04-26 07:57:23 +0000

    dummyhttp: 1.1.2 -> 1.1.3

--- pkgs/by-name/du/dummyhttp/package.nix
+++ pkgs/by-name/du/dummyhttp/package.nix
@@ -1,27 +1,32 @@
 {
   lib,
+  cacert,
   fetchFromGitHub,
   rustPlatform,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "dummyhttp";
-  version = "1.1.2";
+  version = "1.1.3";
 
   src = fetchFromGitHub {
     owner = "svenstaro";
     repo = "dummyhttp";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-J8TOOLTNvm6udkPdYTrjrCX/3D35lXeFDc0H5kki+Uk=";
+    hash = "sha256-JfI6r1hSCZePzUFpmR1vBU4qHXAvfSL5snF/X2zfN4o=";
   };
 
-  cargoHash = "sha256-566hk79oXApJm5p+gEgikV08n19hH1Tk36DvgPuQKLI=";
+  nativeBuildInputs = [
+    cacert
+  ];
+
+  cargoHash = "sha256-klUifN8I0c7SnsH1V+LdUKJYimTnGV3QMRjEnUAVkfI=";
 
   meta = {
     description = "Super simple HTTP server that replies a fixed body with a fixed response code";
     homepage = "https://github.com/svenstaro/dummyhttp";
     license = with lib.licenses; [ mit ];
-    maintainers = [ ];
+    maintainers = with lib.maintainers; [ tbutter ];
     mainProgram = "dummyhttp";
   };
 })
