580ae99a0e032331e40e7b0d64b609b7318db209
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-26 06:42:01 +0000

    uutils-tar: 0-unstable-2026-04-16 -> 0-unstable-2026-04-23

--- pkgs/by-name/uu/uutils-tar/package.nix
+++ pkgs/by-name/uu/uutils-tar/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "uutils-tar";
-  version = "0-unstable-2026-04-16";
+  version = "0-unstable-2026-04-23";
 
   src = fetchFromGitHub {
     owner = "uutils";
     repo = "tar";
-    rev = "3236dd7b87413b10b9bcd650c731f0b0677671fb";
-    hash = "sha256-WGqZEAbcMykYlLdoMqdUVuc7EpA77nGqAKgzl2vDbws=";
+    rev = "f6b4dc4f2f8fba1f264889a3cb139a16e1d135b1";
+    hash = "sha256-LnNbfBoCwPpyPVNeE4s3gxHzMVJJ8konB6+KsD6dU8M=";
   };
 
-  cargoHash = "sha256-EavcBmhOhUUD0pla50tgNJw7z2jDgxTUvIZuZYa58KQ=";
+  cargoHash = "sha256-WVtJAH5JYwoiMekUSiG0uhHqlXPTFYCB0TNQOcMbGkE=";
 
   cargoBuildFlags = [ "--workspace" ];
 
