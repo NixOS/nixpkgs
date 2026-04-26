5712e8ddd2dd22bb961ed67ec80c63e7bc6fdd15
Joseph LaFreniere <git@lafreniere.xyz>
2026-04-26 14:54:28 -0500

    noto-fonts-cjk-sans: use installFonts hook

--- pkgs/by-name/no/noto-fonts-cjk-sans/package.nix
+++ pkgs/by-name/no/noto-fonts-cjk-sans/package.nix
@@ -8,14 +8,14 @@
   static ? false,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "noto-fonts-cjk-sans";
   version = "2.004";
 
   src = fetchFromGitHub {
     owner = "notofonts";
     repo = "noto-cjk";
-    tag = "Sans${version}";
+    tag = "Sans${finalAttrs.version}";
     hash = "sha256-i3ZKoSy2SVs46IViha+Sg8atH4n3ywgrunHPLtVT4Pk=";
     sparseCheckout = [
       "Sans/OTC"
@@ -55,4 +55,4 @@ stdenvNoCC.mkDerivation rec {
       emily
     ];
   };
-}
+})
