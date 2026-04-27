3cd6ad52cdbff8ff70bb467ccfb2ad47a56abc8f
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-27 23:52:33 +0000

    better-commits: 1.19.1 -> 1.21.0

--- pkgs/by-name/be/better-commits/package.nix
+++ pkgs/by-name/be/better-commits/package.nix
@@ -8,16 +8,16 @@
 
 buildNpmPackage rec {
   pname = "better-commits";
-  version = "1.19.1";
+  version = "1.21.0";
 
   src = fetchFromGitHub {
     owner = "Everduin94";
     repo = "better-commits";
     tag = "v${version}";
-    hash = "sha256-4akYngeDf7EqLxCzObjb5MUA8Cb3X3jlrvLEM71OcCA=";
+    hash = "sha256-JgAe55aMr6gP9/dEj3rDW5glA+ntftthJSCFYvtmWso=";
   };
 
-  npmDepsHash = "sha256-vtUtdgOJEQk9PzxOz7AlwOxWS6PTjAtrjAugXRXo89c=";
+  npmDepsHash = "sha256-7+WMrkAYKQgUdOBI4jSOTt6gxwfCwft4GcUKMvpTiYc=";
 
   passthru.updateScript = nix-update-script { };
 
