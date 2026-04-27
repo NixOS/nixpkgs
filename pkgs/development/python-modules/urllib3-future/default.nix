3a31921293e64084a3503f59e09ebd62dcb81646
Robert Schütz <nix@dotlambda.de>
2026-04-27 15:21:45 -0700

    python3Packages.urllib3-future: 2.19.904 -> 2.19.913
    Diff: https://github.com/jawah/urllib3.future/compare/2.19.904...2.19.913
    
    Changelog: https://github.com/jawah/urllib3.future/blob/2.19.913/CHANGES.rst

--- pkgs/development/python-modules/urllib3-future/default.nix
+++ pkgs/development/python-modules/urllib3-future/default.nix
@@ -10,6 +10,7 @@
   jh2,
   lib,
   pytest-asyncio,
+  pytest-rerunfailures,
   pytest-timeout,
   pytestCheckHook,
   python-socks,
@@ -24,14 +25,14 @@
 
 buildPythonPackage rec {
   pname = "urllib3-future";
-  version = "2.19.904";
+  version = "2.19.913";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jawah";
     repo = "urllib3.future";
     tag = version;
-    hash = "sha256-bYkT78OT665Ea3k4boORlaFiORaryqRXSfxXt0yZLSM=";
+    hash = "sha256-ReoVZb31M1N1oxSFYGd1x1P5/qBx/oFEjaxJEiMwbvM=";
   };
 
   postPatch = ''
@@ -68,6 +69,7 @@ buildPythonPackage rec {
   nativeCheckInputs = [
     aiofile
     pytest-asyncio
+    pytest-rerunfailures
     pytest-timeout
     pytestCheckHook
     tornado
@@ -78,11 +80,6 @@ buildPythonPackage rec {
   disabledTestPaths = [
     # test connects to the internet
     "test/contrib/test_resolver.py::test_url_resolver"
-  ];
-
-  disabledTests = [
-    # test hangs
-    "test_proxy_rejection"
   ];
 
   meta = {
