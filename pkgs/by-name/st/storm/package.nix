067758bf993dd6f0f236ce7ecc34ee7cdbba5205
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-27 07:09:50 +0000

    storm: 2.8.3 -> 2.8.7

--- pkgs/by-name/st/storm/package.nix
+++ pkgs/by-name/st/storm/package.nix
@@ -14,12 +14,12 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "apache-storm";
-  version = "2.8.3";
+  version = "2.8.7";
   name = "${finalAttrs.pname}-${finalAttrs.version}";
 
   src = fetchurl {
     url = "mirror://apache/storm/${finalAttrs.name}/${finalAttrs.name}.tar.gz";
-    hash = "sha256-W7a/4vJgGhiAxk0l+1jY+38Mpz8WOLodhlW6l6nQHEc=";
+    hash = "sha256-WlCwLBHopnuvgwLMPJtbCZQi/A0PB2pn9leQF0i4p0E=";
   };
 
   nativeBuildInputs = [
