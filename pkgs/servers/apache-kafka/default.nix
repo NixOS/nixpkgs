58aae1b2a230273df795f6289cd30f67b15a97a9
Erik Skytthe <ers@dbc.dk>
2026-04-27 10:47:34 +0200

    apacheKafka_4_1:  4.1.0 -> 4.1.2

--- pkgs/servers/apache-kafka/default.nix
+++ pkgs/servers/apache-kafka/default.nix
@@ -15,9 +15,9 @@
 let
   versionMap = {
     "4_1" = {
-      kafkaVersion = "4.1.0";
+      kafkaVersion = "4.1.2";
       scalaVersion = "2.13";
-      sha256 = "sha256-hbRThHDR3LmNAnMoa/q4cXBlUi5Zfs//zU24OjAhdY4=";
+      sha256 = "sha256-GHleC5qJ3g7YRDrYM2Xib2QHgTr+xP4TnBrLDDNk5tw=";
       jre = jdk17_headless;
       nixosTest = nixosTests.kafka.base.kafka_4_1;
     };
