6c0e00ba91a15ad74d038b74ffab430b508589e5
Michael Daniels <mdaniels5757@gmail.com>
2026-04-27 17:55:42 -0400

    python3Packages.tree-sitter-zeek: 0.2.12 -> 0.2.13

--- pkgs/development/python-modules/tree-sitter-zeek/default.nix
+++ pkgs/development/python-modules/tree-sitter-zeek/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "tree-sitter-zeek";
-  version = "0.2.12";
+  version = "0.2.13";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "zeek";
     repo = "tree-sitter-zeek";
     tag = "v${version}";
-    hash = "sha256-BWrzPMsUgbIvdWsafTtXApmGGr7Sdpb382iqhM8Etqk=";
+    hash = "sha256-okm65ls/38PnzHpfdNT1McouaZ1r1dBflXcGUzaM9Z0=";
   };
 
   build-system = [ setuptools ];
