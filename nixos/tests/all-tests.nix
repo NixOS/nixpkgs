113b89bc0f9fc7e02e042a333279195685c0aca8
Henri Menke <henri@henrimenke.de>
2026-04-27 16:04:36 +0200

    nixos/vnstat: add NixOS test

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1755,6 +1755,7 @@ in
   vikunja = runTest ./vikunja.nix;
   virtualbox = handleTestOn [ "x86_64-linux" ] ./virtualbox.nix { };
   vm-variant = handleTest ./vm-variant.nix { };
+  vnstat = runTest ./vnstat.nix;
   vscode-remote-ssh = handleTestOn [ "x86_64-linux" ] ./vscode-remote-ssh.nix { };
   vscodium = import ./vscodium.nix { inherit runTest; };
   vsftpd = runTest ./vsftpd.nix;
