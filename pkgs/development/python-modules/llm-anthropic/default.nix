d170df7c9542f82315da961a32db65f265f816a2
Sarah Clark <seclark@nextquestion.net>
2026-04-27 19:39:52 +0000

    python3Packages.llm-anthropic: 0.24 -> 0.25

--- pkgs/development/python-modules/llm-anthropic/default.nix
+++ pkgs/development/python-modules/llm-anthropic/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "llm-anthropic";
-  version = "0.24";
+  version = "0.25";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "simonw";
     repo = "llm-anthropic";
     tag = finalAttrs.version;
-    hash = "sha256-0nI/J7gGTUyrvluez9H8WD4kCuMFgWR5zFHRMxh9DXQ=";
+    hash = "sha256-dgt8DQkwVsAduzvwzN0xmLuJ4erj5KYU8x+SFCynDBI=";
   };
 
   build-system = [
