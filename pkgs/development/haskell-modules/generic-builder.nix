amansakhuja/nixpkgs5

haskell-modules/generic-builder.nix: Allow running Haskell tests as passthru.tests
We add an `enableSeperateTestOutput` option to the Haskell builder.
When enabled, we install the test suites into a `tests` output and
create a passthru tests.checks deriviation that will run the test suites.

We take the list of test suites from `testTargets` if it is defined, and
probe Cabal for all the test suites otherwise.

pkgs/development/haskell-modules/generic-builder.nix
@@ -162,6 +162,10 @@ in
   enableSeparateDataOutput ? false,
   enableSeparateDocOutput ? doHaddock,
   enableSeparateIntermediatesOutput ? false,
+  # When enabled test suites are not run in the main deriviation.
+  # Instead they are built, installed in the 'tests' output, and
+  # a passthru.test derivation is created for test suites.
+  enableSeparateTestOutput ? false,
   # Don't fail at configure time if there are multiple versions of the
   # same package in the (recursive) dependencies of the package being
   # built. Will delay failures, if any, to compile time.
@@ -566,7 +570,8 @@ lib.fix (
         ++ (optional enableSeparateDataOutput "data")
         ++ (optional enableSeparateDocOutput "doc")
         ++ (optional enableSeparateBinOutput "bin")
-        ++ (optional enableSeparateIntermediatesOutput "intermediates");
+        ++ (optional enableSeparateIntermediatesOutput "intermediates")
+        ++ (optional enableSeparateTestOutput "tests");
 
       setOutputFlags = false;
 
@@ -856,6 +861,25 @@ lib.fix (
           mkdir -p $doc
         ''}
         ${optionalString enableSeparateDataOutput "mkdir -p $data"}
+        ${optionalString enableSeparateTestOutput (
+          # if no testTargets are specified but this flag is enabled, then we should query Cabal for the list of all test suites and install them
+          if testTargets == [ ] then
+            ''
+              mkdir -p $test/bin
+              tests="$(${setupCommand} test ${buildFlags} -v0 --test-wrapper echo)"
+              for t in $tests; do
+                install $t $test/bin/
+              done
+            ''
+          # otherwise just install the test suites specified by testTargets
+          else
+            ''
+              mkdir -p $test/bin
+              for t in ${testTargetsString}; do
+                install dist/build/$t/$t $test/bin/
+              done
+            ''
+        )}
 
         runHook postInstall
       '';
@@ -924,6 +948,41 @@ lib.fix (
               benchmarkSystemDepends
               benchmarkToolDepends
               ;
+          };
+        tests =
+          passthru.tests or { }
+          // lib.optionalAttrs enableSeparateTestOutput {
+            checks = (
+              stdenv.mkDerivation {
+                name = "${pname}-check";
+                inherit
+                  src
+                  preCheck
+                  postCheck
+                  patches
+                  ;
+                inherit (drv) meta LANG buildInputs;
+                phases = [
+                  "unpackPhase"
+                  "patchPhase"
+                  "checkPhase"
+                  "installPhase"
+                ];
+                checkPhase = ''
+                  runHook preCheck
+                  for t in ${drv.test}/bin/*; do
+                   $t $checkFlags ${lib.concatStringsSep " " testFlags} 2> >(tee $(basename $t)-stderr) | tee $(basename $t)-stdout
+                  done
+                  runHook postCheck
+                '';
+                installPhase = ''
+                  mkdir $out
+                  cp *-stderr $out/
+                  cp *-stdout $out/
+                '';
+              }
+              // lib.optionalAttrs (drv ? LOCALE_ARCHIVE) { inherit (drv) LOCALE_ARCHIVE; }
+            );
           };
 
         # Attributes for the old definition of `shellFor`. Should be removed but

NixOS/nixpkgs

pkgs/development/haskell-modules/generic-builder.nix
@@ -210,6 +210,10 @@ in
   enableSeparateDataOutput ? false,
   enableSeparateDocOutput ? doHaddock,
   enableSeparateIntermediatesOutput ? false,
+  # When enabled test suites are not run in the main deriviation.
+  # Instead they are built, installed in the 'tests' output, and
+  # a passthru.test derivation is created for test suites.
+  enableSeparateTestOutput ? false,
   # Don't fail at configure time if there are multiple versions of the
   # same package in the (recursive) dependencies of the package being
   # built. Will delay failures, if any, to compile time.
@@ -647,7 +651,8 @@ lib.fix (
       ++ (optional enableSeparateDataOutput "data")
       ++ (optional enableSeparateDocOutput "doc")
       ++ (optional enableSeparateBinOutput "bin")
-      ++ (optional enableSeparateIntermediatesOutput "intermediates");
+      ++ (optional enableSeparateIntermediatesOutput "intermediates")
+      ++ (optional enableSeparateTestOutput "test");
 
       setOutputFlags = false;
 
@@ -857,7 +862,7 @@ lib.fix (
       # - If it is empty, it'll be unset during test suite execution.
       # - Otherwise GHC_PACKAGE_PATH will have the package db used for configuring
       #   plus GHC's core packages.
-      checkPhase = ''
+      ${if enableSeparateTestOutput then null else "checkPhase"} = ''
         runHook preCheck
         checkFlagsArray+=(
           "--show-details=streaming"
@@ -934,6 +939,26 @@ lib.fix (
           mkdir -p $doc
         ''}
         ${optionalString enableSeparateDataOutput "mkdir -p $data"}
+        ${optionalString enableSeparateTestOutput (
+          # if no testTargets are specified but this flag is enabled, then we should query Cabal for the list of all test suites and install them
+          if testTargets == [ ] || testTargets == [ "" ] then
+            ''
+              mkdir -p $test/bin
+              # TODO: fix this. This only works with some random versions of Cabal
+              tests="$(${setupCommand} test $buildFlags -v0 --test-wrapper=echo)"
+              for t in $tests; do
+                install $t "$test"/bin/
+              done
+            ''
+          # otherwise just install the test suites specified by testTargets
+          else
+            ''
+              mkdir -p $test/bin
+              for t in ${testTargetsString}; do
+                install dist/build/$t/$t $test/bin/
+              done
+            ''
+        )}
 
         runHook postInstall
       '';
@@ -1002,6 +1027,41 @@ lib.fix (
             benchmarkToolDepends
             ;
         };
+        tests =
+          passthru.tests or { }
+          // lib.optionalAttrs enableSeparateTestOutput {
+            checks = (
+              stdenv.mkDerivation {
+                name = "${pname}-check";
+                inherit
+                  src
+                  preCheck
+                  postCheck
+                  patches
+                  ;
+                inherit (drv) meta LANG buildInputs;
+                phases = [
+                  "unpackPhase"
+                  "patchPhase"
+                  "checkPhase"
+                  "installPhase"
+                ];
+                checkPhase = ''
+                  runHook preCheck
+                  for t in ${drv.test}/bin/*; do
+                   $t $checkFlags ${lib.concatStringsSep " " testFlags} 2> >(tee $(basename $t)-stderr) | tee $(basename $t)-stdout
+                  done
+                  runHook postCheck
+                '';
+                installPhase = ''
+                  mkdir $out
+                  cp *-stderr $out/
+                  cp *-stdout $out/
+                '';
+              }
+              // lib.optionalAttrs (drv ? LOCALE_ARCHIVE) { inherit (drv) LOCALE_ARCHIVE; }
+            );
+          };
 
         # Attributes for the old definition of `shellFor`. Should be removed but
         # this predates the warning at the top of `getCabalDeps`.