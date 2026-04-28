522ead47454567b95f5bfb60f9b0528300e97b34
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-28 12:54:07 +0000

    apidog: 2.8.24 -> 2.8.26

--- pkgs/by-name/ap/apidog/package.nix
+++ pkgs/by-name/ap/apidog/package.nix
@@ -7,11 +7,11 @@
 
 let
   pname = "apidog";
-  version = "2.8.24";
+  version = "2.8.26";
 
   src = fetchurl {
     url = "https://file-assets.apidog.com/download/${version}/Apidog-${version}.AppImage";
-    hash = "sha256-gGhA++wfgURhibGF2tXAmH0orX2VSUiJsAo3wAu6t1g=";
+    hash = "sha256-u+J5OKgqOKEE35IhMpL7LUB4UgaX2XZKfnUczfLhjoU=";
   };
 
   appimageContents = appimageTools.extract {
