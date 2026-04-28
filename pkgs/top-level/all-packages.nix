c923946bc55c1e7b6cfc59648c8fb1f95606ec81
Guy Chronister <guychronister@outlook.com>
2026-04-28 09:47:36 -0500

    mygpoclient: switch to python3Packages set from python3.pkgs

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6927,7 +6927,7 @@ with pkgs;
     withPm = [ ];
   };
 
-  mygpoclient = with python3.pkgs; toPythonApplication mygpoclient;
+  mygpoclient = with python3Packages; toPythonApplication mygpoclient;
 
   nanovna-saver = libsForQt5.callPackage ../applications/science/electronics/nanovna-saver { };
 
