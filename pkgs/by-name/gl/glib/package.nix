5a3c78f3014cfed39a81097bf42f3adf87051dbf
Tomoaki Kawada <kawada@kmckk.co.jp>
2026-04-28 14:30:33 +0900

    glib: disable sysprof on Windows
    libsysprof-capture does not build on Windows hosts in nixpkgs.

--- pkgs/by-name/gl/glib/package.nix
+++ pkgs/by-name/gl/glib/package.nix
@@ -160,7 +160,7 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     finalAttrs.setupHook
   ]
-  ++ lib.optionals (!stdenv.hostPlatform.isFreeBSD) [
+  ++ lib.optionals (!stdenv.hostPlatform.isFreeBSD && !stdenv.hostPlatform.isWindows) [
     libsysprof-capture
   ]
   ++ [
@@ -237,6 +237,9 @@ stdenv.mkDerivation (finalAttrs: {
   ++ lib.optionals stdenv.hostPlatform.isFreeBSD [
     "-Dxattr=false"
     "-Dsysprof=disabled" # sysprof-capture does not build on FreeBSD
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isWindows [
+    "-Dsysprof=disabled" # sysprof-capture does not build on Windows
   ];
 
   env = {
