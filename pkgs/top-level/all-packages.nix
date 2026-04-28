1536aefad33602ee5f44dd2c453e6345e92160ea
Guy Chronister <guychronister@outlook.com>
2026-04-28 09:47:29 -0500

    msoffcrypto-tool: switch to python3Packages set from python3.pkgs

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6915,7 +6915,7 @@ with pkgs;
 
   mpeg2dec = libmpeg2;
 
-  msoffcrypto-tool = with python3.pkgs; toPythonApplication msoffcrypto-tool;
+  msoffcrypto-tool = with python3Packages; toPythonApplication msoffcrypto-tool;
 
   mpich = callPackage ../development/libraries/mpich {
     automake = automake116x;
