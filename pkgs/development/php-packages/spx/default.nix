c23c1407c4d312eafdb919d69732e1b3c98634f2
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2026-04-28 08:31:11 +0200

    phpExtensions.spx: fix changelog url

--- pkgs/development/php-packages/spx/default.nix
+++ pkgs/development/php-packages/spx/default.nix
@@ -25,7 +25,7 @@ buildPecl {
   ];
 
   meta = {
-    changelog = "https://github.com/NoiseByNorthwest/php-spx/releases/tag/${version}";
+    changelog = "https://github.com/NoiseByNorthwest/php-spx/releases/tag/v${version}";
     description = "Simple & straight-to-the-point PHP profiling extension with its built-in web UI";
     homepage = "https://github.com/NoiseByNorthwest/php-spx";
     license = lib.licenses.php301;
