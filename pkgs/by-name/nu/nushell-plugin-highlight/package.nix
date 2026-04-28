9732646e467cb4c485b85f240465705701aed4d2
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-28 23:42:23 +0000

    nushell-plugin-highlight: 1.4.12+0.110.0 -> 1.4.14+0.112.2

--- pkgs/by-name/nu/nushell-plugin-highlight/package.nix
+++ pkgs/by-name/nu/nushell-plugin-highlight/package.nix
@@ -9,17 +9,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nu_plugin_highlight";
-  version = "1.4.12+0.110.0";
+  version = "1.4.14+0.112.2";
 
   src = fetchFromGitHub {
     owner = "cptpiepmatz";
     repo = "nu-plugin-highlight";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-20b+EiB95BzDVWibWQuG8ozPRV8LbxG7fHEbyTk3xTE=";
+    hash = "sha256-zNvHSUpIMpRWw+1DMG4BukfdpZ3jQ6hQ2WisXFmnHGM=";
     fetchSubmodules = true;
   };
 
-  cargoHash = "sha256-pkLcTjZYLERMhK18zPdfldHrECHXQpcg5i6rsyxw7nQ=";
+  cargoHash = "sha256-ZOu774x8EhW/QbP6dCIgz0BGjkv42rK0E8+oe/I7uhU=";
 
   nativeBuildInputs = [ pkg-config ] ++ lib.optionals stdenv.cc.isClang [ rustPlatform.bindgenHook ];
 
