8e1dc59ee89f3a370febb1228e0c3fa409ec0b58
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-28 17:23:48 +0000

    python3Packages.modelscope: 1.35.4 -> 1.36.2

--- pkgs/development/python-modules/modelscope/default.nix
+++ pkgs/development/python-modules/modelscope/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "modelscope";
-  version = "1.35.4";
+  version = "1.36.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "modelscope";
     repo = "modelscope";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-n33vf7GC4Oucw4/dWXeXeThRrkmSCk1rfS4BWzOxl7I=";
+    hash = "sha256-Cf6s5AujXL5kP7OPVbYSvfkvtAYH/XQJy1BKA8f3sGs=";
   };
 
   build-system = [ setuptools ];
