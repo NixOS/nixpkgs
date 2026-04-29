e19092f5b8c6ba3fdc5062e3dfc3bdceb66f54c1
f <socials.fl@icloud.com>
2026-04-29 21:23:56 +0200

    vncdotool: modernize build attributes

--- pkgs/development/python-modules/vncdotool/default.nix
+++ pkgs/development/python-modules/vncdotool/default.nix
@@ -24,9 +24,9 @@ buildPythonPackage rec {
     hash = "sha256-CXxuaAi/B7NiGp1dhhe7iBw0qOdPfsKg7zMMwavGCW8=";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     pillow
     cryptography
     twisted
