0b6bcc1566077804a8bc667503ce41da513e2ab8
Ricardo Pinto <hessian_loom_0u@icloud.com>
2026-04-29 17:15:56 +0100

    maintainers: drop rmgpinto

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -23361,12 +23361,6 @@
     githubId = 641278;
     name = "Robert T. McGibbon";
   };
-  rmgpinto = {
-    email = "hessian_loom_0u@icloud.com";
-    github = "rmgpinto";
-    githubId = 24584;
-    name = "Ricardo Gândara Pinto";
-  };
   rnhmjoj = {
     email = "rnhmjoj@inventati.org";
     matrix = "@rnhmjoj:eurofusion.eu";
