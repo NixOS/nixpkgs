7f18e3af6a4fb0a9c686088cee547097e8935179
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-29 19:13:43 +0000

    sccache: 0.14.0 -> 0.15.0

--- pkgs/by-name/sc/sccache/package.nix
+++ pkgs/by-name/sc/sccache/package.nix
@@ -8,17 +8,17 @@
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
-  version = "0.14.0";
+  version = "0.15.0";
   pname = "sccache";
 
   src = fetchFromGitHub {
     owner = "mozilla";
     repo = "sccache";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-vpfkWoKu1oksHmgq86QYN7fzIcptormN9ffzXbrJJLs=";
+    sha256 = "sha256-cisDCEevpF8tzdEQzgIN+OifdTDk2YXvuSLKyBQu+IE=";
   };
 
-  cargoHash = "sha256-oiB/KKxNyKu1sxlu/Ep2cgilyJPWzRkohjKL7/azVP0=";
+  cargoHash = "sha256-p08KXAnGyAbn4JzacphucV+OjZt0iKjnZEFtONkkDRs=";
 
   buildFeatures = lib.optionals distributed [
     "dist-client"
