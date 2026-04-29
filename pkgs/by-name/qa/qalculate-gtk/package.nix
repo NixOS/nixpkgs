9d92cbe427032331b77d3b4b09c008765d19adc7
Doron Behar <doron.behar@gmail.com>
2026-04-29 08:22:20 +0300

    qalculate-gtk: fix darwin build
    Fix #514605 .
    
    Co-Authored-By: Miles Wirht <regime_skylark.0y@icloud.com>

--- pkgs/by-name/qa/qalculate-gtk/package.nix
+++ pkgs/by-name/qa/qalculate-gtk/package.nix
@@ -2,10 +2,12 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   autoreconfHook,
   pkg-config,
   libqalculate,
   gtk3,
+  gtk-mac-integration-gtk3,
   curl,
   wrapGAppsHook3,
   desktopToDarwinBundle,
@@ -22,6 +24,16 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-JZfolSRLRLtv529f25lEPYOlz+y+EdRqKA0Y5d1dK3s=";
   };
 
+  patches = [
+    # Is meaningfull only for Darwin but is better applying unconditionally.
+    # Can be removed in 5.11 probably, see:
+    # https://github.com/Qalculate/qalculate-gtk/pull/705
+    (fetchpatch {
+      url = "https://github.com/Qalculate/qalculate-gtk/commit/2f1b6a32d98f122d44d158c20f79a5b9aaf9669f.patch";
+      hash = "sha256-v9T8wWvqlI9l6BUszf/575qmoFvh88Cdj/m2XqV8Q4k=";
+    })
+  ];
+
   hardeningDisable = [ "format" ];
 
   nativeBuildInputs = [
@@ -35,6 +47,9 @@ stdenv.mkDerivation (finalAttrs: {
     libqalculate
     gtk3
     curl
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    gtk-mac-integration-gtk3
   ];
 
   enableParallelBuilding = true;
