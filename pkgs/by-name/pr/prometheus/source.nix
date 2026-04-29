8d925f2e3e21745fbc8b9308148a8df2a76beb75
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-29 07:10:10 +0000

    prometheus: 3.11.2 -> 3.11.3

--- pkgs/by-name/pr/prometheus/source.nix
+++ pkgs/by-name/pr/prometheus/source.nix
@@ -1,6 +1,6 @@
 {
-  version = "3.11.2";
-  hash = "sha256-pSps9LKOOgWUA27lkgFOYliI8Skejx895EKwD88ysn8=";
-  npmDepsHash = "sha256-ZYbyWtqv7SL/SVAOuQmgb7atkrBQDumYB6Wpa39MThw=";
+  version = "3.11.3";
+  hash = "sha256-JmnVTVW6LsrdiQspKznksyhAYkmCXQcqCLK8q5nZI48=";
+  npmDepsHash = "sha256-6uE8d3+v5wobSyCl8oYlbNQZjRj/WtvVrpzo/PR2hQA=";
   vendorHash = "sha256-wR1b5jV/2B50OeKokv8Ss+tpSXNjJBsLIZrK7gOb168=";
 }
