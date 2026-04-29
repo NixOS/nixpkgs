e218e534c47635ad246ff0b051e36a0257f1ce6d
Chad Gregory <chad_gregory@moriroku-na.com>
2026-04-29 12:58:36 +0000

    php.extensions.pdo_sqlsrv, php.extensions.sqlsrv: fix formatting

--- pkgs/development/php-packages/pdo_sqlsrv/default.nix
+++ pkgs/development/php-packages/pdo_sqlsrv/default.nix
@@ -12,15 +12,15 @@ buildPecl {
   version = "5.13.0";
   sha256 = "sha256-76hZvMSNl/JSaNvevx2yXyVhDX+jaz7pEHPByZQR4kw=";
 
-  internalDeps = [php.extensions.pdo];
+  internalDeps = [ php.extensions.pdo ];
 
-  buildInputs = [unixodbc] ++ lib.optionals stdenv.hostPlatform.isDarwin [libiconv];
+  buildInputs = [ unixodbc ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ libiconv ];
 
   meta = {
     description = "Microsoft Drivers for PHP for SQL Server";
     license = lib.licenses.mit;
     homepage = "https://github.com/Microsoft/msphpsql";
-    teams = [lib.teams.php];
+    teams = [ lib.teams.php ];
     broken = lib.versionAtLeast php.version "8.5";
   };
 }
--- pkgs/development/php-packages/sqlsrv/default.nix
+++ pkgs/development/php-packages/sqlsrv/default.nix
@@ -11,12 +11,12 @@ buildPecl {
   version = "5.13.0";
   sha256 = "sha256-MdbCg1oFp7btDw3bZ1VsqRRlKlelccJokfAtitmbflw=";
 
-  buildInputs = [unixodbc] ++ lib.optionals stdenv.hostPlatform.isDarwin [libiconv];
+  buildInputs = [ unixodbc ] ++ lib.optionals stdenv.hostPlatform.isDarwin [ libiconv ];
 
   meta = {
     description = "Microsoft Drivers for PHP for SQL Server";
     license = lib.licenses.mit;
     homepage = "https://github.com/Microsoft/msphpsql";
-    teams = [lib.teams.php];
+    teams = [ lib.teams.php ];
   };
 }
