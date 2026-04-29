195ac282644122af4433e8781ab23f32a0608fc5
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-29 01:34:40 +0000

    python3Packages.ansible: 13.5.0 -> 13.6.0

--- pkgs/development/python-modules/ansible/default.nix
+++ pkgs/development/python-modules/ansible/default.nix
@@ -24,7 +24,7 @@
 
 let
   pname = "ansible";
-  version = "13.5.0";
+  version = "13.6.0";
 in
 buildPythonPackage {
   inherit pname version;
@@ -32,7 +32,7 @@ buildPythonPackage {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-abF15pTZUR/sg4sKgvFQurNm3Zy3qCkJbD06cvYEZxk=";
+    hash = "sha256-UUFVLBvTf1aDnrWxHvDZPpI5EpXJeUfVB7ja9yZbErg=";
   };
 
   # we make ansible-core depend on ansible, not the other way around,
