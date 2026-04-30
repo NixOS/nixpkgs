573459b971fd6374e2f981ad915d1ef97e013b3c
alexma233 <i@fur.im>
2026-04-30 20:42:58 +0800

    composia: init at 0.1.2

---
+++ pkgs/by-name/co/composia/package.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromForgejo,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "composia";
+  version = "0.1.2";
+
+  src = fetchFromForgejo {
+    domain = "forgejo.alexma.top";
+    owner = "alexma233";
+    repo = "composia";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-fca4lmjR2er+bfhI5yX3prL+02iFsaKfgmdTdaVeiOU=";
+  };
+
+  vendorHash = "sha256-LIts6L6jl2ZmHvOBboB9eBIf3VWraaVMca7/s7h65bU=";
+
+  subPackages = [
+    "cmd/composia"
+    "cmd/composia-controller"
+    "cmd/composia-agent"
+  ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X forgejo.alexma.top/alexma233/composia/internal/version.Value=${finalAttrs.version}"
+  ];
+
+  meta = {
+    description = "Self-hosted Docker Compose control plane and CLI";
+    homepage = "https://docs.composia.xyz";
+    changelog = "https://forgejo.alexma.top/alexma233/composia/releases/tag/v${finalAttrs.version}";
+    license = lib.licenses.agpl3Only;
+    maintainers = with lib.maintainers; [ alexma233 ];
+    mainProgram = "composia";
+    platforms = lib.platforms.linux;
+  };
+})
