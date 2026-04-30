a28c101b834daa7bd8eb8ff2315054431320faad
Adam C. Stephens <adam@valkor.net>
2026-04-29 23:30:26 -0400

    kanidm_1_8: mark unsupported

--- pkgs/top-level/release.nix
+++ pkgs/top-level/release.nix
@@ -45,6 +45,8 @@
       # so users choosing to allow don't have to rebuild them every time.
       permittedInsecurePackages = [
         "olm-3.2.16" # see PR #347899
+        "kanidm_1_8-1.8.6"
+        "kanidmWithSecretProvisioning_1_8-1.8.6"
       ];
     };
 
