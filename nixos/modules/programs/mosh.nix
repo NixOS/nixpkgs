e1da057f3698f6b13acaa94970b1560ca5901950
Adam Dinwoodie <adam@dinwoodie.org>
2026-04-30 09:14:13 +0100

    nixos/mosh: add timeout configuration options

--- nixos/modules/programs/mosh.nix
+++ nixos/modules/programs/mosh.nix
@@ -27,6 +27,38 @@ in
       '';
       default = true;
     };
+    networkTimeout = lib.mkOption {
+      description = ''
+        How long (in seconds) `mosh-server` will wait to receive an update from
+        the client before exiting.  Since `mosh` is very useful for mobile
+        clients with intermittent operation and connectivity, its authors
+        suggest setting this variable to a high value, such as 604800 (one
+        week) or 3592000 (30 days).  Otherwise, `mosh-server` will wait
+        indefinitely for a client to reappear.  This is somewhat similar to the
+        `TMOUT` variable found in many Bourne shells.  However, it is not a
+        login-session inactivity timeout; it only applies to network
+        connectivity.
+      '';
+      default = null;
+      type = lib.types.nullOr lib.types.numbers.positive;
+      example = 604800;
+    };
+    signalTimeout = lib.mkOption {
+      description = ''
+        How long (in seconds) `mosh-server` will ignore `SIGUSR1` while waiting
+        to receive an update from the client.  Otherwise, `SIGUSR1` will always
+        terminate `mosh-server`.  Users and administrators may implement
+        scripts to clean up disconnected Mosh sessions.  With this variable
+        set, a user or administrator can issue
+
+            $ pkill -SIGUSR1 mosh-server
+
+        to kill disconnected sessions without killing connected login sessions.
+      '';
+      default = null;
+      type = lib.types.nullOr lib.types.numbers.positive;
+      example = 60;
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -43,6 +75,10 @@ in
         setuid = false;
         setgid = true;
       };
+    };
+    environment.sessionVariables = {
+      MOSH_SERVER_NETWORK_TMOUT = cfg.networkTimeout;
+      MOSH_SERVER_SIGNAL_TMOUT = cfg.signalTimeout;
     };
   };
 
