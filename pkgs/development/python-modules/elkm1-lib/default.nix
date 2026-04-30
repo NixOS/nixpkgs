ed9e51292790c6e07319a1914f971e32fcac5bd3
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-30 22:00:46 +0200

    python3Packages.elkm1-lib: migrate to finalAttrs

--- pkgs/development/python-modules/elkm1-lib/default.nix
+++ pkgs/development/python-modules/elkm1-lib/default.nix
@@ -8,7 +8,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "elkm1-lib";
   version = "2.2.14";
   pyproject = true;
@@ -16,15 +16,13 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "gwww";
     repo = "elkm1";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-lejeRHteVMO7qz8qMPCG5d8V/rj550FL+WuogM/Lcbw=";
   };
 
   build-system = [ hatchling ];
 
-  dependencies = [
-    pyserial-asyncio-fast
-  ];
+  dependencies = [ pyserial-asyncio-fast ];
 
   nativeCheckInputs = [
     pytest-asyncio
@@ -34,10 +32,10 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "elkm1_lib" ];
 
   meta = {
-    description = "Python module for interacting with ElkM1 alarm/automation panel";
+    description = "Module for interacting with ElkM1 alarm/automation panel";
     homepage = "https://github.com/gwww/elkm1";
-    changelog = "https://github.com/gwww/elkm1/blob/${version}/CHANGELOG.md";
+    changelog = "https://github.com/gwww/elkm1/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
