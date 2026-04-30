0c6909bd4476b337841bb64d0a60db475f4d7022
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-30 04:14:20 +0000

    runme: 3.16.10 -> 3.16.11

--- pkgs/by-name/ru/runme/package.nix
+++ pkgs/by-name/ru/runme/package.nix
@@ -13,16 +13,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "runme";
-  version = "3.16.10";
+  version = "3.16.11";
 
   src = fetchFromGitHub {
     owner = "runmedev";
     repo = "runme";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-ExgzuX3g8JHWKGmUbaIOR7H1eSI97GRtSREtyqU0riU=";
+    hash = "sha256-c01mVr8JW+qghj7q5W3B1NsuH9hQUEq3hVHLT+9VM20=";
   };
 
-  vendorHash = "sha256-yS87r9zYQpJ7G/opqBNJ6EgqO7/R1jL+mxPVX71rQhc=";
+  vendorHash = "sha256-7F33MmGWIY1z2aw2mJ83XhcOrhT+olGwNmvdgGdxyME=";
 
   nativeBuildInputs = [
     installShellFiles
