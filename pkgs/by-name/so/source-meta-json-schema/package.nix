46855ba7e67e6e7a0593047d1694884bf50ae2d0
R. Ryantm <ryantm-bot@ryantm.com>
2026-04-30 06:25:54 +0000

    source-meta-json-schema: 14.13.4 -> 15.2.0

--- pkgs/by-name/so/source-meta-json-schema/package.nix
+++ pkgs/by-name/so/source-meta-json-schema/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "source-meta-json-schema";
-  version = "14.13.4";
+  version = "15.2.0";
 
   src = fetchFromGitHub {
     owner = "sourcemeta";
     repo = "jsonschema";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-APrlrYG6QTJqrI6+TBUd34x6Di1fCm15FqDGWrukhkQ=";
+    hash = "sha256-uJuGXACrNKz63VuiNmL3+D7ML+cpYjVdaAHsk9Mk2zc=";
   };
 
   nativeBuildInputs = [
