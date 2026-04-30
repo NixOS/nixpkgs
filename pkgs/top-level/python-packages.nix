9715cffb5c0f102bcc1b8b1eb5faed108fbd251d
kilyanni <github@nkilian.org>
2026-04-30 21:55:35 +0200

    python3Packages.bittensor-wallet: init at 4.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2114,6 +2114,8 @@ self: super: with self; {
 
   bittensor-drand = callPackage ../development/python-modules/bittensor-drand { };
 
+  bittensor-wallet = callPackage ../development/python-modules/bittensor-wallet { };
+
   bitvavo-aio = callPackage ../development/python-modules/bitvavo-aio { };
 
   bitvector-for-humans = callPackage ../development/python-modules/bitvector-for-humans { };
