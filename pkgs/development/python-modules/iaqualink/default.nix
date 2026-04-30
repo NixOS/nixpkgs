575a009d5440c2a8a4fe5a8093d3e7191144f283
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-30 09:21:11 +0200

    python3Packages.iaqualink: 0.6.0 -> 0.7.0
    Changelog: https://github.com/flz/iaqualink-py/releases/tag/v0.7.0

--- pkgs/development/python-modules/iaqualink/default.nix
+++ pkgs/development/python-modules/iaqualink/default.nix
@@ -4,25 +4,25 @@
   fetchFromGitHub,
   hatch-vcs,
   hatchling,
+  httpx-retries,
   httpx,
   pytest-cov-stub,
   pytestCheckHook,
-  pythonOlder,
+  pyyaml,
   respx,
+  typer,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "iaqualink";
-  version = "0.6.0";
+  version = "0.7.0";
   pyproject = true;
-
-  disabled = pythonOlder "3.12";
 
   src = fetchFromGitHub {
     owner = "flz";
     repo = "iaqualink-py";
-    tag = "v${version}";
-    hash = "sha256-s/ZhcbTaCvn7ei1O4+P4fKPojitl+4gsatc9PZx+W2g=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Bn4dcfTRkY+qc/c39ip+vZvlbqll7qZOl7phMgw9EjY=";
   };
 
   build-system = [
@@ -30,21 +30,34 @@ buildPythonPackage rec {
     hatchling
   ];
 
-  dependencies = [ httpx ] ++ httpx.optional-dependencies.http2;
+  dependencies = [
+    httpx
+    httpx-retries
+  ]
+  ++ httpx.optional-dependencies.http2;
+
+  optional-dependencies = {
+    cli = [
+      pyyaml
+      typer
+    ];
+  };
 
   nativeCheckInputs = [
     pytest-cov-stub
     pytestCheckHook
     respx
-  ];
+    typer
+  ]
+  ++ lib.flatten (builtins.attrValues finalAttrs.passthru.optional-dependencies);
 
   pythonImportsCheck = [ "iaqualink" ];
 
   meta = {
     description = "Python library for Jandy iAqualink";
     homepage = "https://github.com/flz/iaqualink-py";
-    changelog = "https://github.com/flz/iaqualink-py/releases/tag/${src.tag}";
+    changelog = "https://github.com/flz/iaqualink-py/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
