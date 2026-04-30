fce100473866125c3a0e0a831f9a0b6ea6df9825
Yifei Sun <ysun@hey.com>
2026-04-30 11:03:49 +0200

    ocamlPackages.carton-lwt: 0.7.0 -> 1.2.0

--- pkgs/development/ocaml-modules/carton/lwt.nix
+++ pkgs/development/ocaml-modules/carton/lwt.nix
@@ -1,64 +1,28 @@
 {
   buildDunePackage,
-  carton,
-  lwt,
-  decompress,
-  optint,
-  bigstringaf,
-  alcotest,
   alcotest-lwt,
-  cstruct,
-  fmt,
-  logs,
-  mirage-flow,
-  result,
-  rresult,
-  ke,
-  base64,
-  bos,
-  checkseum,
+  cachet-lwt,
+  carton,
   digestif,
-  fpath,
-  stdlib-shims,
-  git-binary, # pkgs.git
+  lwt,
 }:
 
 buildDunePackage {
   pname = "carton-lwt";
 
-  inherit (carton) version src postPatch;
-  duneVersion = "3";
+  inherit (carton) version src;
 
   propagatedBuildInputs = [
+    cachet-lwt
     carton
     lwt
-    decompress
-    optint
-    bigstringaf
   ];
 
-  # Tests fail with git 2.41
-  # see https://github.com/mirage/ocaml-git/issues/617
-  doCheck = false;
-  nativeCheckInputs = [
-    git-binary
-  ];
+  doCheck = true;
+
   checkInputs = [
-    alcotest
     alcotest-lwt
-    cstruct
-    fmt
-    logs
-    mirage-flow
-    result
-    rresult
-    ke
-    base64
-    bos
-    checkseum
     digestif
-    fpath
-    stdlib-shims
   ];
 
   inherit (carton) meta;
