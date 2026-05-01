b889b174b284dee80766eca84e8da2f9d0766ab0
JuliusFreudenberger <julius+git@jfreudenberger.de>
2026-05-02 00:36:03 +0200

    teleport: Use pnpm fetcherVersion 3; teleport_17: 17.7.20 -> 17.7.23; teleport_18: 18.7.2 -> 18.7.6
    Contains security fixes for both versions.
    
    Changelog: https://github.com/gravitational/teleport/releases/tag/v17.7.23
    Diff: https://github.com/gravitational/teleport/compare/v17.7.20...v17.7.23
    
    Changelog: https://github.com/gravitational/teleport/releases/v18.7.6
    Diff: http://github.com/gravitational/teleport/compare/v18.7.2...v18.7.6

--- pkgs/by-name/te/teleport_18/package.nix
+++ pkgs/by-name/te/teleport_18/package.nix
@@ -7,11 +7,11 @@
 }:
 
 buildTeleport {
-  version = "18.7.2";
-  hash = "sha256-1ANYBYNhTzSA3zhgUgKks73piHdw+4iXGFmGUWHTYpE=";
-  vendorHash = "sha256-0HxDrsWWRjuaVGtbX7xwCMsj2dYRJr3wmpf8qInlyBE=";
-  pnpmHash = "sha256-XJ2SLCMoFFeoBQ3YKIlq4APNOhSS8ipTNQufQ1nkYqs=";
-  cargoHash = "sha256-SfVoh4HnHSOz1haPPV7a/RyA6LFjLRe78Mn2fVdVyEA=";
+  version = "18.7.6";
+  hash = "sha256-p7qwsUr6n6OAl/b20SgropAubfPfwBiVTvbReb2HpO8=";
+  vendorHash = "sha256-/ZY0J0yB/8qMG6vEIta7Nf2Uv3xTZ/WPoMz+Dj5hwZA=";
+  pnpmHash = "sha256-uRsS5m0Q4fAFvJ3Qp6xcEAB8QFriLXbeGtD0o0n46RE=";
+  cargoHash = "sha256-KkFwMSBXsRmDuaPU1n6FPq2P5UQiQnb7+HEDOhhmjd0=";
 
   wasm-bindgen-cli = wasm-bindgen-cli_0_2_99;
   buildGoModule = buildGo125Module;
