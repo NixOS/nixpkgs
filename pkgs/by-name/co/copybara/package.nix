46f68be889a72842a0c2885ea92834daa12384b0
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-01 09:50:11 +0000

    copybara: 20260420 -> 20260427

--- pkgs/by-name/co/copybara/package.nix
+++ pkgs/by-name/co/copybara/package.nix
@@ -13,11 +13,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "copybara";
-  version = "20260420";
+  version = "20260427";
 
   src = fetchurl {
     url = "https://github.com/google/copybara/releases/download/v${finalAttrs.version}/copybara_deploy.jar";
-    hash = "sha256-0ddQZ5CtsV0trTi6+AhNA2KU9hQAedz47MpxvPK27cg=";
+    hash = "sha256-tkZ8YvTmBgLgZqUicM4V7GosnVKOXL7BhBam783SStA=";
   };
 
   nativeBuildInputs = [
