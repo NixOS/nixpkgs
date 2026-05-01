57b51411bd1af0dc25a8bd593561251a80ea8eed
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-01 12:22:36 +0200

    python3Packages.lxml-html-clean: migrate to finalAttrs

--- pkgs/development/python-modules/lxml-html-clean/default.nix
+++ pkgs/development/python-modules/lxml-html-clean/default.nix
@@ -7,7 +7,7 @@
   unittestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "lxml-html-clean";
   version = "0.4.4";
   pyproject = true;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "fedora-python";
     repo = "lxml_html_clean";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-mToWK2Cc2cfBmEqcL5z61WydQUb78TWZDGGLR9kNliA=";
   };
 
@@ -30,8 +30,8 @@ buildPythonPackage rec {
   meta = {
     description = "Separate project for HTML cleaning functionalities copied from lxml.html.clean";
     homepage = "https://github.com/fedora-python/lxml_html_clean/";
-    changelog = "https://github.com/fedora-python/lxml_html_clean/blob/${version}/CHANGES.rst";
+    changelog = "https://github.com/fedora-python/lxml_html_clean/blob/${finalAttrs.src.tag}/CHANGES.rst";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
