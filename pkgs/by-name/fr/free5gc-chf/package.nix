a6b8940490d3f0c9ff84a431eaba37891761dd2d
Nico Felbinger <nico@felbinger.eu>
2026-05-02 00:56:52 +0200

    free5gc-chf: init at 1.2.3

---
+++ pkgs/by-name/fr/free5gc-chf/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  versionCheckHook,
+  nix-update-script,
+}:
+buildGoModule (finalAttrs: {
+  pname = "free5gc-chf";
+  version = "1.2.3";
+
+  src = fetchFromGitHub {
+    owner = "free5gc";
+    repo = "chf";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-rKAig3y7rGDoUpMoB5k58eIf+w7S3PhTsxObzLmUrmM=";
+  };
+
+  vendorHash = "sha256-/7GH6f4+Ao183AWxJIv6cndhoVE6Dnishgl8ER0knsQ=";
+
+  ldflags = [
+    "-X github.com/free5gc/util/version.VERSION=v${finalAttrs.version}"
+  ];
+
+  __structuredAttrs = true;
+
+  doInstallCheck = true;
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Open source 5G core network based on 3GPP R15";
+    homepage = "https://free5gc.org/";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ felbinger ];
+    platforms = lib.platforms.linux;
+  };
+})
