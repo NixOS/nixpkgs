c45a677fc55a2d04b7830a08d01f5a53f355bc71
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-01 22:38:07 +0000

    deck: 1.58.0 -> 1.59.1

--- pkgs/by-name/de/deck/package.nix
+++ pkgs/by-name/de/deck/package.nix
@@ -9,13 +9,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "deck";
-  version = "1.58.0";
+  version = "1.59.1";
 
   src = fetchFromGitHub {
     owner = "Kong";
     repo = "deck";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dNqG7R8iaV/XdefGAsx1trptWK8G6NIjj7dmSMG5tUI=";
+    hash = "sha256-nBLY3X1r4ntqzosnwc2fVo/mWb7+5RHbKL1hUGmjRp4=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
@@ -28,7 +28,7 @@ buildGoModule (finalAttrs: {
   ];
 
   proxyVendor = true; # darwin/linux hash mismatch
-  vendorHash = "sha256-b/eBY5VQJJGLxVDyCJclwbOpF0pgVCu+DKE/FoOyy4Q=";
+  vendorHash = "sha256-AaZxe7VYor+XIW3gH+0YfPK7weauEh/ZHUK6KK2GhbA=";
 
   postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
     installShellCompletion --cmd deck \
