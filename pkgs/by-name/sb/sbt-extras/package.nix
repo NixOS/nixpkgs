4e53808c24cb77007c40441f73f32d5c4bb71689
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-01 21:57:03 +0000

    sbt-extras: 2025-08-25 -> 2026-05-01

--- pkgs/by-name/sb/sbt-extras/package.nix
+++ pkgs/by-name/sb/sbt-extras/package.nix
@@ -18,14 +18,14 @@
 
 stdenv.mkDerivation rec {
   pname = "sbt-extras";
-  rev = "103ab5905df0d7be505e258fc600935bbb511ec9";
-  version = "2025-08-25";
+  rev = "711b9309b840e33f7ff485713c39d71b746b3bc0";
+  version = "2026-05-01";
 
   src = fetchFromGitHub {
     owner = "paulp";
     repo = "sbt-extras";
     inherit rev;
-    sha256 = "UlxZsCi4EdcHvGwatQm1sPyamfcqJs9o8qo2HWLedQw=";
+    sha256 = "OndeOcyswz8pgSLgT5dUO9pXADlBo+1coxUtL9aU0Ks=";
   };
 
   dontBuild = true;
