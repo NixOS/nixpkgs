d062d65d6118ccd28d06dba4fa44229f33277d2f
Tom Herbers <mail@tomherbers.de>
2026-05-01 10:10:51 +0200

    kanidm_1_10: init at 1.10.0
    Changelog: https://github.com/kanidm/kanidm/releases/tag/v1.10.0

--- pkgs/servers/kanidm/1_9.nix
+++ pkgs/servers/kanidm/1_9.nix
@@ -2,4 +2,5 @@ import ./generic.nix {
   version = "1.9.3";
   hash = "sha256-YI2nShQhsCd1vn7S4VeCELfcU0HsmZrFs60kllJJFAo=";
   cargoHash = "sha256-b6kBDA/CDJHbybfvbRRZ++q9H+SVRu0BmgkwzN2i/YU=";
+  eolDate = "2026-05-31";
 }
