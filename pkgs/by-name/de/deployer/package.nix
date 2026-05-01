0f6d1329c87e47ad78e658c00922988f4ac1300f
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-01 05:31:36 +0000

    deployer: 7.5.12 -> 8.0.0

--- pkgs/by-name/de/deployer/package.nix
+++ pkgs/by-name/de/deployer/package.nix
@@ -6,7 +6,7 @@
 
 php.buildComposerProject2 (finalAttrs: {
   pname = "deployer";
-  version = "7.5.12";
+  version = "8.0.0";
 
   src = fetchFromGitHub {
     owner = "deployphp";
@@ -15,7 +15,7 @@ php.buildComposerProject2 (finalAttrs: {
     hash = "sha256-wtkixHexsJNKsLnnlHssh0IzxwWYMPKDcaf/D0zUNKk=";
   };
 
-  vendorHash = "sha256-sgWPZw5HiXd7c45I0f5qnw4l2HwgLaTJwzmXw8140kk=";
+  vendorHash = "sha256-Hw7Whjg48g9Lhhkp4gvSSZMIRLBBLCclmOANiaap9x8=";
 
   meta = {
     changelog = "https://github.com/deployphp/deployer/releases/tag/v${finalAttrs.version}";
