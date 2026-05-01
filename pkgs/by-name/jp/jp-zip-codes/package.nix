3fbcd08bd2be9a558609ecf8664841b208e558c6
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-01 14:57:15 +0000

    jp-zip-codes: 0-unstable-2026-04-01 -> 0-unstable-2026-05-01

--- pkgs/by-name/jp/jp-zip-codes/package.nix
+++ pkgs/by-name/jp/jp-zip-codes/package.nix
@@ -7,15 +7,15 @@
 
 stdenvNoCC.mkDerivation {
   pname = "jp-zip-code";
-  version = "0-unstable-2026-04-01";
+  version = "0-unstable-2026-05-01";
 
   # This package uses a mirror as the source because the
   # original provider uses the same URL for updated content.
   src = fetchFromGitHub {
     owner = "musjj";
     repo = "jp-zip-codes";
-    rev = "5404779eb05849c61c70c4489d117bcb36ea19ab";
-    hash = "sha256-pysW1hAj8acTFOytYw/ZXknUlRP+JUvh4Ia2I2CwA6A=";
+    rev = "fd586719e8b6c55763e015963eb2b7cb7383ffec";
+    hash = "sha256-88Vr1YySNT1Z+1rqxzT6Mccv/v6wL+HtW10Fmt3SXoA=";
   };
 
   installPhase = ''
