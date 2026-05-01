ee0ee0de34ffcc65ba52112142b07529e48b9c69
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-01 14:02:55 +0000

    lilypond-unstable: 2.25.34 -> 2.27.0

--- pkgs/by-name/li/lilypond-unstable/package.nix
+++ pkgs/by-name/li/lilypond-unstable/package.nix
@@ -7,11 +7,11 @@
 
 lilypond.overrideAttrs (
   finalAttrs: prevAttrs: {
-    version = "2.25.34";
+    version = "2.27.0";
 
     src = fetchzip {
       url = "https://lilypond.org/download/sources/v${lib.versions.majorMinor finalAttrs.version}/lilypond-${finalAttrs.version}.tar.gz";
-      hash = "sha256-UFuL8TZ7uLZhXgFoMGxAye04dNcvrw1gqQMOnE4P2fc=";
+      hash = "sha256-uZKpHmuYFkmj1kI+D09rPNLov83EO1QdXyUSSscBRPE=";
     };
 
     patches = [ ];
