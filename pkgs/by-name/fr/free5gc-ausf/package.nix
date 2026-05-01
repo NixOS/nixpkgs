00f787a458d1b606867cf1d6f5089e44b6616bb6
Nico Felbinger <nico@felbinger.eu>
2026-05-02 00:57:55 +0200

    free5gc-ausf: init at 1.4.3

---
+++ pkgs/by-name/fr/free5gc-ausf/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  versionCheckHook,
+  nix-update-script,
+}:
+buildGoModule (finalAttrs: {
+  pname = "free5gc-ausf";
+  version = "1.4.3";
+
+  src = fetchFromGitHub {
+    owner = "free5gc";
+    repo = "ausf";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-S96QhcerwA2c/fW7J0C4HlcQVeSHrob4VCKOSd4iUpo=";
+  };
+
+  vendorHash = "sha256-8oATdtM8pA8KKgPHFCqsEM0Lo93ZBkoTqLRPfHywCOY=";
+
+  ldflags = [
+    "-X github.com/free5gc/util/version.VERSION=v${finalAttrs.version}"
+  ];
+
+  __structuredAttrs = true;
+
+  doInstallCheck = true;
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Open source 5G core network based on 3GPP R15";
+    homepage = "https://free5gc.org/";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ felbinger ];
+    platforms = lib.platforms.linux;
+  };
+})
