666b06a721f3f43c66e9f546db129790ea068664
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-01 22:36:46 +0000

    python3Packages.cryptodatahub: 1.1.0 -> 1.0.1

--- pkgs/development/python-modules/cryptodatahub/default.nix
+++ pkgs/development/python-modules/cryptodatahub/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "cryptodatahub";
-  version = "1.1.0";
+  version = "1.0.1";
   pyproject = true;
 
   src = fetchFromGitLab {
     owner = "coroner";
     repo = "cryptodatahub";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Tz2VbWS5/sGjRsOKR7eWpWAJVNv1QMSjkepI7fVZq6w=";
+    hash = "sha256-NxLUy16u8UL6klvVuXPIIlNuiehlomPsmiS4K5QT9cE=";
   };
 
   build-system = [
