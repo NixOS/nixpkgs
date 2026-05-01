754a7877c5dd4645fd5c7b6d1a751a09f19fd750
Robert Schütz <nix@dotlambda.de>
2026-05-01 08:57:08 -0700

    python3Packages.pyezvizapi: 1.0.4.5 -> 1.0.4.7
    Diff: https://github.com/RenierM26/pyEzvizApi/compare/v1.0.4.5...v1.0.4.7
    
    Changelog: https://github.com/RenierM26/pyEzvizApi/blob/v1.0.4.7/CHANGELOG.md

--- pkgs/development/python-modules/pyezvizapi/default.nix
+++ pkgs/development/python-modules/pyezvizapi/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage rec {
   pname = "pyezvizapi";
-  version = "1.0.4.5";
+  version = "1.0.4.7";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "RenierM26";
     repo = "pyEzvizApi";
-    tag = version;
-    hash = "sha256-mzEZQmdhS/vJTFMgDSLaLqKos0ui2pFkcSToy5B8yrw=";
+    tag = "v${version}";
+    hash = "sha256-1VEEi0ZGPFypw2w78B5PPN5Wv6enPudOAHDQ6KkkfTw=";
   };
 
   build-system = [ setuptools ];
@@ -40,7 +40,7 @@ buildPythonPackage rec {
   meta = {
     description = "Python interface for for Ezviz cameras";
     homepage = "https://github.com/RenierM26/pyEzvizApi";
-    changelog = "https://github.com/RenierM26/pyEzvizApi/releases/tag/${src.tag}";
+    changelog = "https://github.com/RenierM26/pyEzvizApi/blob/${src.tag}/CHANGELOG.md";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ dotlambda ];
     mainProgram = "pyezviz";
