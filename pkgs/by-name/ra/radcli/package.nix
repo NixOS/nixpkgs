09e15c2b1b6dbf2f2501510eb7fc7b471e7caa4c
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-02 02:28:29 +0000

    radcli: 1.5.0 -> 1.5.1

--- pkgs/by-name/ra/radcli/package.nix
+++ pkgs/by-name/ra/radcli/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "radcli";
-  version = "1.5.0";
+  version = "1.5.1";
 
   src = fetchFromGitHub {
     owner = "radcli";
     repo = "radcli";
     tag = finalAttrs.version;
-    hash = "sha256-Q+rcNhYmQpEE5LYC/zvbh4RIxx2g9djuWmrSUdh00+c=";
+    hash = "sha256-bp6qwnedyutS2d97AUDCBEVyu8hAKq72zdCLvpRFuJM=";
   };
 
   postUnpack = ''
