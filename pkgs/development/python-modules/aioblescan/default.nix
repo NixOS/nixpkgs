1b10d4d7b5c3ab781acef4122e00fa3296fc0a7a
Sigmanificient <edhyjox@gmail.com>
2026-05-02 02:23:40 +0200

    python3Packages.aioblescan: use finalAttrs

--- pkgs/development/python-modules/aioblescan/default.nix
+++ pkgs/development/python-modules/aioblescan/default.nix
@@ -6,7 +6,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aioblescan";
   version = "0.2.14";
   pyproject = true;
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "frawau";
     repo = "aioblescan";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-JeA9jX566OSRiejdnlifbcNGm0J0C+xzA6zXDUyZ6jc=";
   };
 
@@ -28,8 +28,8 @@ buildPythonPackage rec {
     description = "Library to listen for BLE advertized packets";
     mainProgram = "aioblescan";
     homepage = "https://github.com/frawau/aioblescan";
-    changelog = "https://github.com/frawau/aioblescan/releases/tag/${version}";
+    changelog = "https://github.com/frawau/aioblescan/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
