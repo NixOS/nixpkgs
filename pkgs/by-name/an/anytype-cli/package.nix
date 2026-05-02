6943e4e12ba3dc10e22e49f7fac392251132fe4f
Martin Becze <martin@becze.org>
2026-05-02 21:15:20 +0200

    anytype-cli: init at 0.1.14

---
+++ pkgs/by-name/an/anytype-cli/package.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  stdenv,
+  buildGoModule,
+  fetchFromGitHub,
+  tantivy-go,
+}:
+buildGoModule (finalAttrs: {
+  pname = "anytype-cli";
+  version = "0.1.14";
+
+  src = fetchFromGitHub {
+    owner = "anyproto";
+    repo = "anytype-cli";
+    rev = "187c066c080c0ef4d535aeb7ec23370f15ed17e3";
+    hash = "sha256-xbukPdGXtITO3GtQz+7zz1o/k5rjtVqy42yINANPKUk=";
+  };
+
+  vendorHash = "sha256-PxD+2flqelGFMCrpRU6iOEasMf24YKkoPd0gUSowQwA=";
+  proxyVendor = true;
+
+  env.CGO_ENABLED = 1;
+  env.CGO_LDFLAGS = "-L${tantivy-go}/lib";
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/anyproto/anytype-cli/core.Version=v${finalAttrs.version}"
+  ];
+
+  doCheck = false;
+
+  postInstall = ''
+    ln -s $out/bin/anytype-cli $out/bin/anytype
+    ln -s $out/bin/anytype-cli $out/bin/any
+  '';
+
+  meta = with lib; {
+    description = "Command-line interface for interacting with Anytype";
+    homepage = "https://github.com/anyproto/anytype-cli";
+    license = licenses.mit;
+    mainProgram = "anytype";
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+})
