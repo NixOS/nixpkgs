d0096a2d779b5b5544b97f1d31a55f17cc31b9af
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-02 10:44:39 +0000

    sbt-with-scala-native: 1.12.4 -> 1.12.10

--- pkgs/development/tools/build-managers/sbt/default.nix
+++ pkgs/development/tools/build-managers/sbt/default.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sbt";
-  version = "1.12.4";
+  version = "1.12.10";
 
   src = fetchurl {
     url = "https://github.com/sbt/sbt/releases/download/v${finalAttrs.version}/sbt-${finalAttrs.version}.tgz";
-    hash = "sha256-OgLUzZhn/OJZgvmxaMJF/05HjddpQV21QuW+65+fuHo=";
+    hash = "sha256-yyOGijT+L0zoPB3tewq17+un3ppS8ec5sQs/+NqEQjk=";
   };
 
   postPatch = ''
