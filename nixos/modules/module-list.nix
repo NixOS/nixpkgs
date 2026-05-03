8408f6b3d31b03b9a1d7bb396139ab7bc2251eea
kilyanni <github@nkilian.org>
2026-05-03 03:29:53 +0200

    nixos/programs/kopia: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -253,6 +253,7 @@
   ./programs/kclock.nix
   ./programs/kde-pim.nix
   ./programs/kdeconnect.nix
+  ./programs/kopia.nix
   ./programs/kubeswitch.nix
   ./programs/ladybird.nix
   ./programs/lazygit.nix
