4914d3dd0dfcbae238051024478a13b20132aa67
Dom Rodriguez <shymega@shymega.org.uk>
2026-05-03 17:29:52 +0100

    buildstream-plugins: Use `finalAttrs` pattern

--- pkgs/development/python-modules/buildstream-plugins/default.nix
+++ pkgs/development/python-modules/buildstream-plugins/default.nix
@@ -8,7 +8,7 @@
   buildstream,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "buildstream-plugins";
   version = "2.7.0";
   pyproject = true;
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "apache";
     repo = "buildstream-plugins";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-vbHfceMdaedAg0fVt8pBF+S7yPYhfQlgEYvb48ym+4I=";
   };
 
@@ -42,4 +42,4 @@ buildPythonPackage rec {
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ shymega ];
   };
-}
+})
