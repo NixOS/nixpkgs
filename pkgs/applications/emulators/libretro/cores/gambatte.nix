a78425a3b47a4a44b8c684a4d2f17fe27cc33894
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-04 05:49:13 +0000

    libretro.gambatte: 0-unstable-2026-04-24 -> 0-unstable-2026-05-01

--- pkgs/applications/emulators/libretro/cores/gambatte.nix
+++ pkgs/applications/emulators/libretro/cores/gambatte.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore {
   core = "gambatte";
-  version = "0-unstable-2026-04-24";
+  version = "0-unstable-2026-05-01";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "gambatte-libretro";
-    rev = "5ba6bd1ad745e04b7069d0eb2b44091364fc997a";
-    hash = "sha256-A9OQx0eXVgrGcmpMTtTe97rHCjrV3Ksi/ooLefsysbQ=";
+    rev = "2147d9257911b484b07666994ceecc4c5a2cb318";
+    hash = "sha256-eZHAB1CNrnK9kiNDt5JUmCN5S2mYSiovNBXMM8U+kXg=";
   };
 
   meta = {
