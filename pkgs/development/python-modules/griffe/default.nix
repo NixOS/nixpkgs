739831790abd1696baf090e4caf8cebb7628bf69
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-04 09:07:11 +0200

    python3Packages.griffe: 1.15.0 -> 2.0.2
    Changelog: https://github.com/mkdocstrings/griffe/releases/tag/2.0.2

--- pkgs/development/python-modules/griffe/default.nix
+++ pkgs/development/python-modules/griffe/default.nix
@@ -5,27 +5,39 @@
   colorama,
   fetchFromGitHub,
   git,
+  griffecli,
+  griffelib,
+  hatchling,
   jsonschema,
   pdm-backend,
   pytest-gitconfig,
   pytestCheckHook,
+  uv-dynamic-versioning,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "griffe";
-  version = "1.15.0";
+  version = "2.0.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "mkdocstrings";
     repo = "griffe";
-    tag = version;
-    hash = "sha256-AMMTAqsJfj2MltTgAxfvjUTVzi+ZFmx+J9pzhMp28Z4=";
+    tag = finalAttrs.version;
+    hash = "sha256-Fxa9lrBVQ/enVLiU7hUc0d5x9ItI19EGnbxa7MX6Plc=";
   };
 
-  build-system = [ pdm-backend ];
+  build-system = [
+    hatchling
+    pdm-backend
+    uv-dynamic-versioning
+  ];
 
-  dependencies = [ colorama ];
+  dependencies = [
+    colorama
+    griffecli
+    griffelib
+  ];
 
   nativeCheckInputs = [
     git
@@ -33,10 +45,6 @@ buildPythonPackage rec {
     pytest-gitconfig
     pytestCheckHook
   ];
-
-  optional-dependencies = {
-    async = [ aiofiles ];
-  };
 
   pythonImportsCheck = [ "griffe" ];
 
@@ -48,9 +56,9 @@ buildPythonPackage rec {
   meta = {
     description = "Signatures for entire Python programs";
     homepage = "https://github.com/mkdocstrings/griffe";
-    changelog = "https://github.com/mkdocstrings/griffe/blob/${src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/mkdocstrings/griffe/blob/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.isc;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "griffe";
   };
-}
+})
