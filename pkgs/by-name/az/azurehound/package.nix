9bde61907e8e1106b2bfd2594b7eab76bf92a937
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-04 18:23:20 +0000

    azurehound: 2.12.0 -> 2.12.1

--- pkgs/by-name/az/azurehound/package.nix
+++ pkgs/by-name/az/azurehound/package.nix
@@ -8,16 +8,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "azurehound";
-  version = "2.12.0";
+  version = "2.12.1";
 
   src = fetchFromGitHub {
     owner = "SpecterOps";
     repo = "AzureHound";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-+3h9/R909/Bkxq0Y7oN0xpE6OH8+0Xvs/8X1NBQFrMg=";
+    hash = "sha256-qJ7mzG1G9ck4xM9dB9rcpojGCAbUoZ8bKZwuZV5bhjA=";
   };
 
-  vendorHash = "sha256-QCZFIDUL/RbSMrDfQ8L0A6xJPcWJorBXvHhdIA1WK4Q=";
+  vendorHash = "sha256-WF46wXaNU/Em0KpF6hkuuJ+7K1IKLGqpNS/HxpxX5WY=";
 
   nativeInstallCheckInputs = [ versionCheckHook ];
 
