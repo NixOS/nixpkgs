287c0e276cbd87c5ed662dc692385b1f1d881c97
Ben Darwin <bcdarwin@gmail.com>
2026-05-04 16:00:41 -0400

    itk: 5.4.5 -> 5.4.6

--- pkgs/development/libraries/itk/5.x.nix
+++ pkgs/development/libraries/itk/5.x.nix
@@ -1,5 +1,5 @@
 import ./generic.nix rec {
-  version = "5.4.5";
+  version = "5.4.6";
   tag = "v${version}";
-  sourceSha256 = "sha256-THGtG3gc4LnUEn8tU735dCyIKoaIxPYPzeXCvKKGRRk=";
+  sourceSha256 = "sha256-2riqX1H+h6710roYqa4iP/dJCY/P55OCs/Aw9K+7D7w=";
 }
