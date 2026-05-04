fb6bbceeddd7f2b8bb66d3da7c2543a384c20e83
Nina Fromm <nina.fromm@cyberus-technology.de>
2026-05-04 09:34:16 +0200

    outline: 1.7.0 -> 1.7.1
    Changelog: https://github.com/outline/outline/releases/tag/v1.7.1
    
    Found no breaking changes in there.
    
    (cherry picked from commit 35882f5f8bb95f79666eb368eaa05121166d937d)

--- pkgs/by-name/ou/outline/package.nix
+++ pkgs/by-name/ou/outline/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "outline";
-  version = "1.7.0";
+  version = "1.7.1";
 
   src = fetchFromGitHub {
     owner = "outline";
     repo = "outline";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-QFzLHWmU2DrmKAR5ZKAtnHXZdBfgUfGpTJ6c1zi7//k=";
+    hash = "sha256-LHU3OQglvxLVemDKoDxeI7cGyuSnIUYoPV3aj/LR9XA=";
   };
 
   missingHashes = ./missing-hashes.json;
@@ -30,7 +30,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   offlineCache = yarn-berry_4.fetchYarnBerryDeps {
     inherit (finalAttrs) src missingHashes;
-    hash = "sha256-07/cWCIAsT+zZR6BKEirGfRFw3UaImMMJPSYvmN4g7E=";
+    hash = "sha256-j1MGkIJGjXsoT9aOADRtdywRNI2k5hKeD3O20iUh0sA=";
   };
 
   buildPhase = ''
