NixOS/nixpkgs

commit be994ce8a873ccf624a26cc3377a51189ce9fc34

linux/common-config: update for 7.1
Ham radio is gone, DMABUF_MOVE_NOTIFY is unconditional,
NET_FOU is m on x86_64-linux but y on aarch64-linux,
which throws off IPV6_FOU_TUNNEL now.

pkgs/os-specific/linux/kernel/common-config.nix
@@ -185,6 +185,7 @@ let
       X86_INTEL_LPSS = yes;
       X86_INTEL_PSTATE = yes;
       X86_AMD_PSTATE = whenAtLeast "5.17" yes;
+      X86_AMD_PSTATE_DYNAMIC_EPP = whenAtLeast "7.1" yes;
       # Intel DPTF (Dynamic Platform and Thermal Framework) Support
       ACPI_DPTF = yes;
 
@@ -318,13 +319,14 @@ let
       IPV6_MROUTE = yes;
       IPV6_MROUTE_MULTIPLE_TABLES = yes;
       IPV6_PIMSM_V2 = yes;
-      IPV6_FOU_TUNNEL = module;
+      IPV6_FOU_TUNNEL = yes;
       IPV6_SEG6_LWTUNNEL = yes;
       IPV6_SEG6_HMAC = yes;
       IPV6_SEG6_BPF = yes;
       NET_CLS_ACT = yes;
       NET_CLS_BPF = module;
       NET_ACT_BPF = module;
+      NET_FOU = yes;
       NET_SCHED = yes;
       NET_SCH_BPF = whenAtLeast "6.16" (whenPlatformHasEBPFJit yes);
       L2TP_V3 = yes;
@@ -403,8 +405,8 @@ let
       MAC80211_DEBUGFS = yes;
 
       # HAM radio
-      HAMRADIO = yes;
-      AX25 = module;
+      HAMRADIO = whenOlder "7.1" yes;
+      AX25 = whenOlder "7.1" module;
     }
     // lib.optionalAttrs (stdenv.hostPlatform.system == "aarch64-linux") {
       # Not enabled by default, hides modules behind it
@@ -565,6 +567,9 @@ let
         # Enable CEC over DisplayPort
         DRM_DP_CEC = whenOlder "6.10" yes;
         DRM_DISPLAY_DP_AUX_CEC = whenAtLeast "6.10" yes;
+
+        # Enable RAS reporting via netlink
+        DRM_RAS = whenAtLeast "7.1" yes;
       }
       //
         lib.optionalAttrs
@@ -705,6 +710,7 @@ let
       EXT4_FS_SECURITY = yes;
 
       NTFS_FS = whenBetween "5.15" "6.9" no;
+      NTFS_FS_POSIX_ACL = whenAtLeast "7.1" yes;
       NTFS3_LZX_XPRESS = whenAtLeast "5.15" yes;
       NTFS3_FS_POSIX_ACL = whenAtLeast "5.15" yes;
 
@@ -1379,7 +1385,7 @@ let
         ) yes;
 
         # required for P2P DMABUF
-        DMABUF_MOVE_NOTIFY = lib.mkIf stdenv.hostPlatform.is64bit (whenAtLeast "6.6" yes);
+        DMABUF_MOVE_NOTIFY = lib.mkIf stdenv.hostPlatform.is64bit (whenBetween "6.6" "7.1" yes);
         # required for P2P transfers between accelerators
         HSA_AMD_P2P = lib.mkIf stdenv.hostPlatform.is64bit (whenAtLeast "6.6" yes);
 

commit 933822f2a2fe0808ec5642095eee269afa0512e2

linux/common-config: leave FOU options to defaults
Since IPV6_FOU_TUNNEL was set, I get an unexpected ip6tnl device at
boot.  It was set to y to accomodate Linux 7.1 divergences between
aacrh64 and x86_64, but by just not setting NET_FOU or
IPV6_FOU_TUNNEL, I get everything built as modules on both platforms,
like we presumably want.

Fixes: be994ce8a873 ("linux/common-config: update for 7.1")

pkgs/os-specific/linux/kernel/common-config.nix
@@ -319,14 +319,12 @@ let
       IPV6_MROUTE = yes;
       IPV6_MROUTE_MULTIPLE_TABLES = yes;
       IPV6_PIMSM_V2 = yes;
-      IPV6_FOU_TUNNEL = yes;
       IPV6_SEG6_LWTUNNEL = yes;
       IPV6_SEG6_HMAC = yes;
       IPV6_SEG6_BPF = yes;
       NET_CLS_ACT = yes;
       NET_CLS_BPF = module;
       NET_ACT_BPF = module;
-      NET_FOU = yes;
       NET_SCHED = yes;
       NET_SCH_BPF = whenAtLeast "6.16" (whenPlatformHasEBPFJit yes);
       L2TP_V3 = yes;