84b776646baac3cdef24cebf7dcff65417b2e8c0
Christopher Mühl <toki@toph.so>
2026-05-04 08:15:19 +0000

    maintainers: add tophcodes

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -27931,6 +27931,12 @@
     githubId = 1486805;
     name = "Toon Nolten";
   };
+  tophcodes = {
+    email = "toki@toph.so";
+    github = "tophcodes";
+    githubId = 3678770;
+    name = "Christopher Mühl";
+  };
   tornax = {
     email = "tornax@pm.me";
     github = "TornaxO7";
