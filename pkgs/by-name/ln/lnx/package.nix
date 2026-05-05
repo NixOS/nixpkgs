61ccc33e3b73a96134a585180bd12839baec910a
Harinn <prinn.dev@pm.me>
2026-05-05 18:29:21 +0700

    lnx: unstable-2022-06-25 -> 0.9.0-master

--- pkgs/by-name/ln/lnx/package.nix
+++ pkgs/by-name/ln/lnx/package.nix
@@ -1,23 +1,26 @@
 {
   lib,
+  stdenv,
   rustPlatform,
   fetchFromGitHub,
 }:
 
-# unstable was chosen because of an added Cargo.lock
-# revert to stable for the version after 0.9.0
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "lnx";
-  version = "unstable-2022-06-25";
-  
+  version = "0.9.0-master";
+
   src = fetchFromGitHub {
     owner = "lnx-search";
     repo = "lnx";
-    rev = "2cb80f344c558bfe37f21ccfb83265bf351419d9";
-    hash = "sha256-iwoZ6xRzEDArmhWYxIrbIXRTQjOizyTsXCvMdnUrs2g=";
+    tag = finalAttrs.version;
+    hash = "sha256-J2WP+/f6g1UsjpAdCvkdSpiAyDn9dyAXEbqNfWbNbHk=";
   };
 
   cargoHash = "sha256-9fro1Dx7P+P9NTsg0gtMfr0s4TEpkZA31EFAnObiNFo=";
+
+  # mimalloc uses ATOMIC_VAR_INIT which was removed in C23 (GCC 15 default)
+  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isGNU "-std=c17";
+
   meta = {
     description = "Ultra-fast, adaptable deployment of the tantivy search engine via REST";
     mainProgram = "lnx";
