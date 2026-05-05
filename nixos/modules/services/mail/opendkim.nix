5fef058b8f2669646174192c006b0a4361b4ad85
liberodark <liberodark@gmail.com>
2026-05-05 12:09:10 +0200

    nixos/opendkim: add selector in multi domain

--- nixos/modules/services/mail/opendkim.nix
+++ nixos/modules/services/mail/opendkim.nix
@@ -10,25 +10,45 @@ let
 
   defaultSock = "local:/run/opendkim/opendkim.sock";
 
+  mergedSettings =
+    if cfg.domainConfigs != { } then
+      cfg.settings
+      // {
+        KeyTable = "refile:${cfg.keyPath}/KeyTable";
+        SigningTable = "refile:${cfg.keyPath}/SigningTable";
+      }
+    else
+      cfg.settings;
+
+  configFile = pkgs.writeText "opendkim.conf" (
+    lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name} ${value}") mergedSettings)
+  );
+
   args = [
     "-f"
     "-l"
     "-p"
     cfg.socket
-    "-d"
-    cfg.domains
-    "-k"
-    "${cfg.keyPath}/${cfg.selector}.private"
-    "-s"
-    cfg.selector
   ]
-  ++ lib.optionals (cfg.configFile != null) [
-    "-x"
-    cfg.configFile
-  ];
-
-  configFile = pkgs.writeText "opendkim.conf" (
-    lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name} ${value}") cfg.settings)
+  ++ (
+    if cfg.domainConfigs != { } then
+      [
+        "-x"
+        configFile
+      ]
+    else
+      [
+        "-d"
+        cfg.domains
+        "-k"
+        "${cfg.keyPath}/${cfg.selector}.private"
+        "-s"
+        cfg.selector
+      ]
+      ++ lib.optionals (cfg.configFile != null) [
+        "-x"
+        cfg.configFile
+      ]
   );
 in
 {
@@ -99,6 +119,21 @@ in
         default = { };
         description = "Additional opendkim configuration";
       };
+
+      domainConfigs = lib.mkOption {
+        type =
+          with lib.types;
+          attrsOf (submodule {
+            options = {
+              selector = lib.mkOption {
+                type = lib.types.str;
+                description = "Selector to use for this domain";
+              };
+            };
+          });
+        default = { };
+        description = "Optional per-domain configurations for selectors";
+      };
     };
   };
 
@@ -115,7 +150,7 @@ in
     };
 
     environment = {
-      etc = lib.mkIf (cfg.settings != { }) {
+      etc = lib.mkIf (mergedSettings != { }) {
         "opendkim/opendkim.conf".source = configFile;
       };
       systemPackages = [ pkgs.opendkim ];
@@ -132,16 +167,51 @@ in
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
 
-      preStart = ''
-        cd "${cfg.keyPath}"
-        if ! test -f ${cfg.selector}.private; then
-          ${pkgs.opendkim}/bin/opendkim-genkey -s ${cfg.selector} -d all-domains-generic-key
-          echo "Generated OpenDKIM key! Please update your DNS settings:\n"
-          echo "-------------------------------------------------------------"
-          cat ${cfg.selector}.txt
-          echo "-------------------------------------------------------------"
-        fi
-      '';
+      preStart =
+        if cfg.domainConfigs != { } then
+          ''
+            cd "${cfg.keyPath}"
+            ${lib.concatStringsSep "\n" (
+              lib.mapAttrsToList (domain: conf: ''
+                mkdir -p "${cfg.keyPath}/${domain}"
+                if ! test -f ${domain}/${conf.selector}.private; then
+                  ${pkgs.opendkim}/bin/opendkim-genkey -s ${conf.selector} -d ${domain} -D "${cfg.keyPath}/${domain}"
+                  echo "Generated OpenDKIM key for ${domain}! Please update your DNS settings:\n"
+                  echo "-------------------------------------------------------------"
+                  cat ${domain}/${conf.selector}.txt
+                  echo "-------------------------------------------------------------"
+                fi
+              '') cfg.domainConfigs
+            )}
+
+            cat > ${cfg.keyPath}/KeyTable <<EOF
+            ${lib.concatStringsSep "\n" (
+              lib.mapAttrsToList (
+                domain: conf:
+                "${conf.selector}._domainkey.${domain} ${domain}:${conf.selector}:${cfg.keyPath}/${domain}/${conf.selector}.private"
+              ) cfg.domainConfigs
+            )}
+            EOF
+
+            cat > ${cfg.keyPath}/SigningTable <<EOF
+            ${lib.concatStringsSep "\n" (
+              lib.mapAttrsToList (
+                domain: conf: "*@${domain} ${conf.selector}._domainkey.${domain}"
+              ) cfg.domainConfigs
+            )}
+            EOF
+          ''
+        else
+          ''
+            cd "${cfg.keyPath}"
+            if ! test -f ${cfg.selector}.private; then
+              ${pkgs.opendkim}/bin/opendkim-genkey -s ${cfg.selector} -d all-domains-generic-key
+              echo "Generated OpenDKIM key! Please update your DNS settings:\n"
+              echo "-------------------------------------------------------------"
+              cat ${cfg.selector}.txt
+              echo "-------------------------------------------------------------"
+            fi
+          '';
 
       serviceConfig = {
         ExecStart = "${pkgs.opendkim}/bin/opendkim ${lib.escapeShellArgs args}";
