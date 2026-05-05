04fe7250eba52294b87a9a2f6a79350ef6402409
Salva <salva.hg01@gmail.com>
2026-05-05 16:39:51 -0600

    cosmic-wallpapers: 1.0.11 -> 1.0.12
    Diff: https://github.com/pop-os/cosmic-wallpapers/compare/epoch-1.0.11...epoch-1.0.12

--- pkgs/by-name/co/cosmic-wallpapers/package.nix
+++ pkgs/by-name/co/cosmic-wallpapers/package.nix
@@ -7,7 +7,7 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "cosmic-wallpapers";
-  version = "1.0.11";
+  version = "1.0.12";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
