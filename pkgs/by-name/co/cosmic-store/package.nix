54a83024e423f3d7a4fd21903cd60da8f35cf4a5
Salva <salva.hg01@gmail.com>
2026-05-05 16:38:57 -0600

    cosmic-store: 1.0.11 -> 1.0.12
    Diff: https://github.com/pop-os/cosmic-store/compare/epoch-1.0.11...epoch-1.0.12

--- pkgs/by-name/co/cosmic-store/package.nix
+++ pkgs/by-name/co/cosmic-store/package.nix
@@ -15,17 +15,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-store";
-  version = "1.0.11";
+  version = "1.0.12";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-store";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-pRBbdATuFcazEUcnIeS9uvtnhK5arzO8hC1OJUROUEA=";
+    hash = "sha256-oybxrzKofTbw8u8KMvcU0kODKZ3ttPtSfcoXRjgLq6s=";
   };
 
-  cargoHash = "sha256-eOWd9ueNFEzt1i0X7rklO2Z74eivYW1XdTzF6POfDd4=";
+  cargoHash = "sha256-DCqEXoJ1qTwKemSCGe7AwQCdOFw7Qh3PoxbB9XdLfHk=";
 
   nativeBuildInputs = [
     just
