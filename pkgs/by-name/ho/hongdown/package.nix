8b10da90026bced7d854aee18f7e11a42d3f5060
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-05 03:41:57 +0000

    hongdown: 0.3.4 -> 0.3.11

--- pkgs/by-name/ho/hongdown/package.nix
+++ pkgs/by-name/ho/hongdown/package.nix
@@ -5,15 +5,15 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "hongdown";
-  version = "0.3.4";
+  version = "0.3.11";
 
   src = fetchFromGitHub {
     owner = "dahlia";
     repo = "hongdown";
     tag = finalAttrs.version;
-    hash = "sha256-Bj0ECrYRnXSjgyblocnVjdYipuzbX2+G3KRWZvdR9Rk=";
+    hash = "sha256-0sJBMOcILWODRH0XrRrT6k7XDJJ44JB+9CwevSmTTQc=";
   };
-  cargoHash = "sha256-q84orbkrcKbO5FeI9dk0E92EtE9eQ8n/yGjXzh9MIgg=";
+  cargoHash = "sha256-L0jHVZMQ75PtY7os1Ba3xvoAyAwehFuMNGU0ksZzsf4=";
   meta = {
     description = "Markdown formatter that enforces Hong Minhee's Markdown style conventions";
     mainProgram = "hongdown";
