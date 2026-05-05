e01ca2766bb71c2776a20917ce2e0ae256261774
MoeLeak <i@leak.moe>
2026-05-05 17:48:31 +0800

    maintainers: add moeleak

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18220,6 +18220,12 @@
     githubId = 14153763;
     name = "modderme123";
   };
+  moeleak = {
+    email = "i@leak.moe";
+    github = "moeleak";
+    githubId = 52288096;
+    name = "moeleak";
+  };
   mofrim = {
     email = "mofrim@posteo.de";
     github = "mofrim";
