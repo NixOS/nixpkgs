3b189a61fff4e5a803959b41971d3326eea3e01b
Guy Chronister <guychronister@outlook.com>
2026-05-05 13:47:04 -0500

    bootstrap-studio: remove superflous usage of pname

--- pkgs/by-name/bo/bootstrap-studio/package.nix
+++ pkgs/by-name/bo/bootstrap-studio/package.nix
@@ -20,7 +20,7 @@ appimageTools.wrapType2 {
     install -m 444 -D ${appimageContents}/bstudio.desktop -t $out/share/applications
 
     substituteInPlace $out/share/applications/bstudio.desktop \
-      --replace 'Exec=AppRun' 'Exec=${pname}'
+      --replace 'Exec=AppRun' 'Exec=bootstrap-studio'
 
     install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/0x0/apps/bstudio.png \
       $out/share/icons/hicolor/512x512/apps/bstudio.png
