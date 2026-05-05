18f87e1793c2d93a03be7cbb2570e306c20cd01d
Doron Behar <doron.behar@gmail.com>
2026-05-05 22:56:15 +0300

    nixos/virtualisation: remove hard-coded virtio-gpu-pci device from aarch machines
    In 9e78baf5c75ed87360dfcab4c5eeeee2256be2dc, Aarch machines were added
    the `-device virtio-gpu-pci` argument, to mirror `-vga std` option that
    was added unconditionally to virtualised x86_64 machines. Since then,
    in a7ca287ecb3bd5589d2f815a27f1152071d92b57, the `-vga std` option was
    removed, because that is the default value of this option embedded in
    QEMU since version 2.2.
    
    Removing it from the hard-coded list of qemu options makes it possible
    for the user to choose their own `-vga` value. Similarly, for Aarch
    virtualised machines, choosing unconditionally the `-device
    virtio-gpu-pci` option instead of e.g `-device virtio-gpu-gl-pci`,
    can be bothersome too - see:
    
    https://discourse.nixos.org/t/test-an-aarch64-linux-vm-wayland-compositor-from-an-x86-64-linux-host/77416/2

--- nixos/modules/virtualisation/qemu-vm.nix
+++ nixos/modules/virtualisation/qemu-vm.nix
@@ -1380,7 +1380,6 @@ in
         "-device usb-tablet,bus=usb-bus.0"
       ])
       (mkIf pkgs.stdenv.hostPlatform.isAarch [
-        "-device virtio-gpu-pci"
         "-device usb-ehci,id=usb0"
         "-device usb-kbd"
         "-device usb-tablet"
