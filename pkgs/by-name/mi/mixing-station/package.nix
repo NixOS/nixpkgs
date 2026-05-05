e09f7e5e3ec9df067d3f7d96bb16139e565b2928
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-05 13:19:35 +0000

    mixing-station: 2.8.2 -> 2.9.1

--- pkgs/by-name/mi/mixing-station/package.nix
+++ pkgs/by-name/mi/mixing-station/package.nix
@@ -22,13 +22,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "mixing-station";
-  version = "2.8.2";
+  version = "2.9.1";
 
   src = fetchzip {
     url = "https://mixingstation.app/backend/api/web/download/archive/mixing-station-pc/update/${finalAttrs.version}";
     name = "mixing-station-${finalAttrs.version}.zip";
     extension = "zip";
-    hash = "sha256-hGxwr+Juf8ES4Mifs/B6wq5h2Vs+ymcMl8k8Y6HxcJk=";
+    hash = "sha256-tyoagT21lIT0kIL9RZT1qQ7Aa7E3WAfmdsqvqc7iEGU=";
     stripRoot = false;
   };
 
