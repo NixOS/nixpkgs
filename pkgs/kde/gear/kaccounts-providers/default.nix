d51e49725392f52f0d2c940b21678bb00f0f4542
Christoph Honal <christoph.honal@web.de>
2026-05-06 17:23:45 +0200

    kdePackages.kaccounts-providers: make google client id and scopes configurable

--- pkgs/kde/gear/kaccounts-providers/default.nix
+++ pkgs/kde/gear/kaccounts-providers/default.nix
@@ -1,9 +1,20 @@
 {
+  lib,
   mkKdeDerivation,
   intltool,
   qtdeclarative,
   qtwebengine,
+  googleClientId ? null,
+  googleClientSecret ? null,
+  withGoogleDriveScope ? false,
+  withYoutubeScope ? true,
 }:
+let
+  scopes =
+    lib.optional withYoutubeScope "'https://www.googleapis.com/auth/youtube.upload'"
+    ++ lib.optional withGoogleDriveScope "'https://www.googleapis.com/auth/drive'";
+  scopeLine = lib.concatStringsSep ",\n              " scopes;
+in
 mkKdeDerivation {
   pname = "kaccounts-providers";
 
@@ -12,4 +23,19 @@ mkKdeDerivation {
     qtdeclarative
     qtwebengine
   ];
+
+  postPatch =
+    lib.optionalString (googleClientId != null) ''
+      sed -i 's|<setting name="ClientId">[^<]*</setting>|<setting name="ClientId">${googleClientId}</setting>|' \
+        providers/google.provider.in
+    ''
+    + lib.optionalString (googleClientSecret != null) ''
+      sed -i 's|<setting name="ClientSecret">[^<]*</setting>|<setting name="ClientSecret">${googleClientSecret}</setting>|' \
+        providers/google.provider.in
+    ''
+    + ''
+      substituteInPlace providers/google.provider.in \
+        --replace-fail "'https://www.googleapis.com/auth/youtube.upload'" \
+                       ${lib.escapeShellArg scopeLine}
+    '';
 }
