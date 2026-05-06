16ecf3195933686d94d9cdf0c88f8348f20553d6
Harinn <prinn.dev@pm.me>
2026-05-06 13:53:41 +0700

    nip2: fix build with gcc 15

--- pkgs/by-name/ni/nip2/package.nix
+++ pkgs/by-name/ni/nip2/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchurl,
+  fetchpatch,
   pkg-config,
   glib,
   libxml2,
@@ -24,6 +25,19 @@ stdenv.mkDerivation (finalAttrs: {
     url = "https://github.com/libvips/nip2/releases/download/v${finalAttrs.version}/nip2-${finalAttrs.version}.tar.gz";
     sha256 = "sha256-t14m6z+5lPqpiOjgdDbKwqSWXCyrCL7zlo6BeoZtds0=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "do-not-redeclare-statfs.patch";
+      url = "https://github.com/libvips/nip2/commit/045268a78c40d7f546220504f971c728aebc00be.patch";
+      hash = "sha256-A17+/Vmjf0l1Jpl22VL11gj5m6oFB8DnvkH2EHiRTw8=";
+    })
+    (fetchpatch {
+      name = "declare-function-arguments-for-function-pointer.patch";
+      url = "https://github.com/libvips/nip2/commit/8c60c517b59f806da84d57cb1d083a213b811151.patch";
+      hash = "sha256-o5OHNSbUORGquhyCYCtGQTY74IfroByaa0UAXYsP484=";
+    })
+  ];
 
   nativeBuildInputs = [
     bison
