74e4e8ffd9d472d4a7e4b5ee8f1c5c230c3139df
Gaetan Lepage <gaetan@glepage.com>
2026-05-06 11:50:59 +0000

    python3Packages.mlflow: remove test attributes (as they are disabled)

--- pkgs/development/python-modules/mlflow/default.nix
+++ pkgs/development/python-modules/mlflow/default.nix
@@ -46,33 +46,6 @@
   sqlparse,
   starlette,
   uvicorn,
-
-  # tests
-  azure-core,
-  azure-storage-blob,
-  azure-storage-file,
-  boto3,
-  botocore,
-  catboost,
-  datasets,
-  google-cloud-storage,
-  httpx,
-  jwt,
-  keras,
-  langchain,
-  librosa,
-  moto,
-  opentelemetry-exporter-otlp,
-  optuna,
-  pyspark,
-  pytestCheckHook,
-  pytorch-lightning,
-  sentence-transformers,
-  statsmodels,
-  tensorflow,
-  torch,
-  transformers,
-  xgboost,
 }:
 
 buildPythonPackage (finalAttrs: {
@@ -142,60 +115,6 @@ buildPythonPackage (finalAttrs: {
   ];
 
   pythonImportsCheck = [ "mlflow" ];
-
-  nativeCheckInputs = [
-    azure-core
-    azure-storage-blob
-    azure-storage-file
-    boto3
-    botocore
-    catboost
-    datasets
-    google-cloud-storage
-    httpx
-    jwt
-    keras
-    langchain
-    librosa
-    moto
-    opentelemetry-exporter-otlp
-    optuna
-    pydantic
-    pyspark
-    pytestCheckHook
-    pytorch-lightning
-    sentence-transformers
-    statsmodels
-    tensorflow
-    torch
-    transformers
-    uvicorn
-    xgboost
-  ];
-
-  disabledTestPaths = [
-    # Requires unpackaged `autogen`
-    "tests/autogen/test_autogen_autolog.py"
-
-    # Requires unpackaged `diviner`
-    "tests/diviner/test_diviner_model_export.py"
-
-    # Requires unpackaged `sktime`
-    "examples/sktime/test_sktime_model_export.py"
-
-    # Requires `fastai` which would cause a circular dependency
-    "tests/fastai/test_fastai_autolog.py"
-    "tests/fastai/test_fastai_model_export.py"
-
-    # Requires `spacy` which would cause a circular dependency
-    "tests/spacy/test_spacy_model_export.py"
-
-    # Requires `tensorflow.keras` which is not included in our outdated version of `tensorflow` (2.13.0)
-    "tests/gateway/providers/test_ai21labs.py"
-    "tests/tensorflow/test_keras_model_export.py"
-    "tests/tensorflow/test_keras_pyfunc_model_works_with_all_input_types.py"
-    "tests/tensorflow/test_mlflow_callback.py"
-  ];
 
   # I (@GaetanLepage) gave up at enabling tests:
   # - They require a lot of dependencies (some unpackaged);
