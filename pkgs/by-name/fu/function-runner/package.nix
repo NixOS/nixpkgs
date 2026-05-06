56c8aaf0063d541a97f75d8d065bafe481a0c287
Thomas Butter <tbutter@gmail.com>
2026-05-06 18:53:08 +0000

    function-runner: 9.0.0 -> 9.1.2

--- pkgs/by-name/fu/function-runner/package.nix
+++ pkgs/by-name/fu/function-runner/package.nix
@@ -2,20 +2,37 @@
   lib,
   fetchFromGitHub,
   rustPlatform,
+  openssl,
+  pkg-config,
+  writableTmpDirAsHomeHook,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "function-runner";
-  version = "9.0.0";
+  version = "9.1.2";
 
   src = fetchFromGitHub {
     owner = "Shopify";
     repo = "function-runner";
     rev = "v${finalAttrs.version}";
-    sha256 = "sha256-xzajHtFs7cp7D1ZdG3jBFbjheTSgWR/Vz4fkew3iAkc=";
+    sha256 = "sha256-KvReKvmF3i4zlfM8uj3KHamjfudcrhqrKGfK8O5tMpE=";
   };
 
-  cargoHash = "sha256-fRLBKHsb+y2uyqWejRBmJm+t5CAkL9ScQl6iVCksahU=";
+  cargoHash = "sha256-gnEps/o+C8UpukO1oRF4qlhNsoAmyUmxMKGAgSykNY0=";
+
+  buildInputs = [ openssl ];
+  nativeBuildInputs = [ pkg-config ];
+
+  nativeCheckInputs = [
+    writableTmpDirAsHomeHook
+  ];
+
+  # Skip tests that require network.
+  checkFlags = [
+    "--skip=engine::tests::test_wasm_api_v1_function"
+    "--skip=tests::run_wasm_api_v2_function"
+    "--skip=tests::run_wasm_api_v1_function"
+  ];
 
   meta = {
     description = "CLI tool which allows you to run Wasm Functions intended for the Shopify Functions infrastructure";
