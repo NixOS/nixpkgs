1abd4ccdd3780b9df08ef2a8de86733b9d7aae70
Alyssa Ross <hi@alyssa.is>
2026-05-07 08:24:46 +0200

    mailmanPackages.postorius: 1.3.10 -> 1.3.13
    Even though the previous version worked fine with our current Django,
    this version adds a tight version check, so we have to backport the
    unreleased loosening of that check.

--- pkgs/servers/mail/mailman/postorius.nix
+++ pkgs/servers/mail/mailman/postorius.nix
@@ -10,12 +10,12 @@ with python3.pkgs;
 
 buildPythonPackage (finalAttrs: {
   pname = "postorius";
-  version = "1.3.10";
-  format = "setuptools";
+  version = "1.3.13";
+  format = "pyproject";
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-GmbIqO+03LgbUxJ1nTStXrYN3t2MfvzbeYRAipfTW1o=";
+    hash = "sha256-YC3vXEhSkA1J6K2VGWojNOE8MeSdnAhZMkh558UTGiI=";
   };
 
   patches = [
@@ -25,9 +25,20 @@ buildPythonPackage (finalAttrs: {
       excludes = [ "src/postorius/doc/news.rst" ];
       hash = "sha256-M8C7mO/KoVhl1YtZ5x3wqL+aBkepJ/7NoIRUmd0JpiM=";
     })
+
+    (fetchpatch {
+      name = "django-5.2.patch";
+      url = "https://gitlab.com/mailman/postorius/-/commit/0468ab0329df85b89e6b5d9f7b4d1805f47450c9.patch";
+      excludes = [
+        ".gitlab-ci.yml"
+        "src/postorius/doc/news.rst"
+      ];
+      hash = "sha256-4yk7hLF6cRfS7Kelr49LPeVfrqvNoX1jxTy8sdGrMAk=";
+    })
   ];
 
-  propagatedBuildInputs = [
+  build-system = [ pdm-backend ];
+  dependencies = [
     django-mailman3
     readme-renderer
   ]
