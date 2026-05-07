0c83db963419e1404c0094da24b5d4d86edfdf81
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2026-05-07 16:15:46 +0700

    nixtamal: 1.5.0 → 1.5.1

--- pkgs/by-name/ni/nixtamal/package.nix
+++ pkgs/by-name/ni/nixtamal/package.nix
@@ -20,7 +20,7 @@
 
 ocamlPackages.buildDunePackage (finalAttrs: {
   pname = "nixtamal";
-  version = "1.5.0";
+  version = "1.5.1";
   release_year = 2026;
 
   minimalOCamlVersion = "5.3";
@@ -29,7 +29,7 @@ ocamlPackages.buildDunePackage (finalAttrs: {
     url = "https://darcs.toastal.in.th/nixtamal/stable/";
     mirrors = [ "https://smeder.ee/~toastal/nixtamal.darcs" ];
     rev = finalAttrs.version;
-    hash = "sha256-noQU480HoxVjnHvEY/rso9jANNA8EIKbYeYpnhkkx74=";
+    hash = "sha256-KjlpE9IQxvWAA/eMQBPB1x/jto0k7CzXEBrsddQjroA=";
   };
 
   nativeBuildInputs = [
