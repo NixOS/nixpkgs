1284b579cf1302af107247412880ad96fbcd9682
Thierry Delafontaine <delafthi@pm.me>
2026-05-07 13:57:52 +0000

    opencode{,-desktop}: 1.14.33 -> 1.14.40
    - https://github.com/anomalyco/opencode/releases/tag/v1.14.40
    - https://github.com/anomalyco/opencode/releases/tag/v1.14.39
    - https://github.com/anomalyco/opencode/releases/tag/v1.14.38
    - https://github.com/anomalyco/opencode/releases/tag/v1.14.37
    - https://github.com/anomalyco/opencode/releases/tag/v1.14.35
    - https://github.com/anomalyco/opencode/releases/tag/v1.14.34

--- pkgs/by-name/op/opencode/package.nix
+++ pkgs/by-name/op/opencode/package.nix
@@ -16,13 +16,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.14.33";
+  version = "1.14.40";
 
   src = fetchFromGitHub {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-bnAV1ApOYZngG59fxFKrGN0jmBKWKnjktsbKJiEOaSo=";
+    hash = "sha256-8M7IaLDgh/vapPV38ml/cyxsu77E2SxA4eYP+PuUnEM=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -75,7 +75,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-dbpqhVcjWr+puZhV0x7pR38iMjjZdbrJydKJ/qJfDeY=";
+    outputHash = "sha256-icYqd0Oz7NCbNoVZPySm1RpatLcFIxyb1XTt6M7s0N8=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
