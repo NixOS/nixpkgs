03ade12f968c9eb9a5d044cef20c27c79d0fa6c2
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-06 22:33:10 -0400

    python3Packages.oinker: init at 0.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11427,6 +11427,8 @@ self: super: with self; {
 
   oic = callPackage ../development/python-modules/oic { };
 
+  oinker = callPackage ../development/python-modules/oinker { };
+
   okonomiyaki = callPackage ../development/python-modules/okonomiyaki { };
 
   okta = callPackage ../development/python-modules/okta { };
