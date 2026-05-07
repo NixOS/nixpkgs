7eed9ff15da6984bec8a72ddc5e28db64ff54635
Guy Chronister <guychronister@outlook.com>
2026-05-07 10:09:51 -0500

    aqbanking: add updateScript

--- pkgs/by-name/aq/aqbanking/package.nix
+++ pkgs/by-name/aq/aqbanking/package.nix
@@ -11,6 +11,7 @@
   gettext,
   xmlsec,
   zlib,
+  writeScript,
 }:
 
 stdenv.mkDerivation rec {
@@ -21,6 +22,33 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     url = "https://www.aquamaniac.de/rdm/attachments/download/${releaseId}/aqbanking-${version}.tar.gz";
     hash = "sha256-/JSivr+7T8JrmNyTyPo2qAJimM15lfeYIcSA2zVYf2s=";
+  };
+
+  passthru = {
+    updateScript = writeScript "update-aqbanking" ''
+      #!/usr/bin/env nix-shell
+      #!nix-shell -i bash -p curl gnugrep gnused coreutils common-updater-scripts
+
+      set -eu -o pipefail
+
+      url="https://www.aquamaniac.de/rdm/projects/aqbanking/files"
+
+      # Extract all download paths for the tar.gz archives
+      downloads=$(curl -s "$url" | grep -Po 'attachments/download/\d+/aqbanking-\d+\.\d+\.\d+\.tar\.gz')
+
+      # Use version sort (-V) to find the highest version number
+      latest_version=$(echo "$downloads" | grep -Po '\d+\.\d+\.\d+' | sort -V | tail -n 1)
+
+      # Grab the exact download path for that specific highest version
+      latest_download=$(echo "$downloads" | grep -F "aqbanking-$latest_version.tar.gz" | head -n 1)
+      latest_release_id=$(echo "$latest_download" | cut -d/ -f3)
+
+      # Update the releaseId statically inside this nix file
+      sed -i "s/releaseId = \"[0-9]\+\";/releaseId = \"$latest_release_id\";/" pkgs/by-name/aq/aqbanking/package.nix
+
+      # Update the version and hash using the standard Nixpkgs tool
+      update-source-version aqbanking "$latest_version"
+    '';
   };
 
   # Set the include dir explicitly, this fixes a build error when building
