268ea7f1591ca63161f3c336ee4c116ab0a26111
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-05-07 02:32:50 +0200

    Revert "python3Packages.pyaxencoapi: 1.0.7 -> 1.0.9"
    This reverts commit c37ebfe18236d7c83f88b6e67385b876a00cb590.
    
    This breaks tests.home-assistant-components.myneomitis.

--- pkgs/development/python-modules/pyaxencoapi/default.nix
+++ pkgs/development/python-modules/pyaxencoapi/default.nix
@@ -12,14 +12,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pyaxencoapi";
-  version = "1.0.9";
+  version = "1.0.7";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "AXENCO-FR";
     repo = "ha-py-axenco-api";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-rqjDI8e3ZeZHsEk0EBaKlEl3w9blinlc2MsTCAKk85o=";
+    hash = "sha256-Ml58+kstIpqQUXDt/jpZeR8ueu5U3nnH7hiUcZxveAM=";
   };
 
   build-system = [ setuptools ];
