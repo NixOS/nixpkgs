d827f6d44190f3c7197db24d503b06185256469c
Guy Chronister <guychronister@outlook.com>
2026-05-08 08:33:19 -0500

    amule-daemon: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1446,12 +1446,6 @@ with pkgs;
 
   crystfel-headless = crystfel.override { withGui = false; };
 
-  amule-daemon = amule.override {
-    monolithic = false;
-    enableDaemon = true;
-    mainProgram = "amuled";
-  };
-
   amule-gui = amule.override {
     monolithic = false;
     client = true;
