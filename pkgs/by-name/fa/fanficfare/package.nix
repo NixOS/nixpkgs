d67f2580305d65471b21af9f1529cd1a35755415
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-08 13:31:52 +0000

    fanficfare: 4.54.0 -> 4.57.0

--- pkgs/by-name/fa/fanficfare/package.nix
+++ pkgs/by-name/fa/fanficfare/package.nix
@@ -6,12 +6,12 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "fanficfare";
-  version = "4.54.0";
+  version = "4.57.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
-    hash = "sha256-Pypts27ksSx8r+nLo3wup2ltbcayJ91VyF2+JchrE2c=";
+    hash = "sha256-LdZHbUGIHhLgUUlhHkA/IkkQtdJCd8QzU77iif3vI6c=";
   };
 
   nativeBuildInputs = with python3Packages; [
