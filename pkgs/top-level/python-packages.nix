065931349534cd3da99ce4a9ca528cc66f6a223d
Sarah Clark <seclark@nextquestion.net>
2026-05-08 11:01:24 -0700

    python3Packages.pipcl: init at v4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12588,6 +12588,8 @@ self: super: with self; {
 
   pip-tools = callPackage ../development/python-modules/pip-tools { };
 
+  pipcl = callPackage ../development/python-modules/pipcl { };
+
   pipdate = callPackage ../development/python-modules/pipdate { };
 
   pipdeptree = callPackage ../development/python-modules/pipdeptree { };
