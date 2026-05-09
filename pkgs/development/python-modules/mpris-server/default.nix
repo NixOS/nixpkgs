f48e2500ceef192e261839a1d7299653150835f2
Peder Bergebakken Sundt <pbsds@hotmail.com>
2026-05-09 03:19:31 +0200

    python3Packages.mpris-server: adopt, fixup
    I assume quadradical doesn't want to re-assume maintainership simply because I reverted the package drop for my own purposes.
    
    (cherry picked from commit 261eac2ed067c4e1853a8226be29fbadf4439891)

--- pkgs/development/python-modules/mpris-server/default.nix
+++ pkgs/development/python-modules/mpris-server/default.nix
@@ -9,16 +9,23 @@
   setuptools,
   strenum,
 }:
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "mpris-server";
   version = "0.9.6";
   pyproject = true;
 
   src = fetchPypi {
     pname = "mpris_server";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-T0ZeDQiYIAhKR8aw3iv3rtwzc+R0PTQuIh6+Hi4rIHQ=";
   };
+
+  postPatch = ''
+    substituteInPlace mpris_server/__init__.py \
+      --replace-fail \
+        "__version__: Final[str] = '0.9.0'" \
+        "__version__: Final[str] = '${finalAttrs.version}'"
+  '';
 
   build-system = [ setuptools ];
 
@@ -39,6 +46,6 @@ buildPythonPackage rec {
     description = "Publish a MediaPlayer2 MPRIS device to D-Bus";
     homepage = "https://pypi.org/project/mpris-server/";
     license = lib.licenses.agpl3Only;
-    maintainers = with lib.maintainers; [ quadradical ];
+    maintainers = with lib.maintainers; [ pbsds ];
   };
-}
+})
