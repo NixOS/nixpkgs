0130c73ba748bd64dc6f416814412d069df32f0c
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-09 17:33:36 +0000

    libretro.snes9x2010: 0-unstable-2026-04-09 -> 0-unstable-2026-05-05

--- pkgs/applications/emulators/libretro/cores/snes9x2010.nix
+++ pkgs/applications/emulators/libretro/cores/snes9x2010.nix
@@ -5,13 +5,13 @@
 }:
 mkLibretroCore rec {
   core = "snes9x2010";
-  version = "0-unstable-2026-04-09";
+  version = "0-unstable-2026-05-05";
 
   src = fetchFromGitHub {
     owner = "libretro";
     repo = "snes9x2010";
-    rev = "a7a4bfaed4c6408908c76af20ad625e1645c3d11";
-    hash = "sha256-mtTgh/koM7jS7/cH7qRgTa+xJXBBJSdxHHbhOd/q4i4=";
+    rev = "d9cba8a41b3407ebb929816a7033e0407fd7b2d0";
+    hash = "sha256-OdJStJK823PayWS+bmwG+kDrdx6KeVWYiSAu61C9UFs=";
   };
 
   makeFlags = [ "GIT_VERSION=${builtins.substring 0 7 src.rev}" ];
