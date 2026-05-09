0e57863c490bee7f6346c9211374d59f5f9bef5f
Alyssa Ross <hi@alyssa.is>
2026-05-09 08:44:19 +0200

    libfyaml: fix libfyaml.pc for musl
    (and other platforms that don't require an explicit -lm)

--- pkgs/by-name/li/libfyaml/package.nix
+++ pkgs/by-name/li/libfyaml/package.nix
@@ -19,8 +19,14 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-mRQQe+J5wtLt/bI/Wer9TVGdU3a1zp3zFCm4oNQON8M=";
   };
 
-  # backport 32-bit build fixes
   patches = [
+    (fetchpatch {
+      name = "musl.patch";
+      url = "https://github.com/pantoniou/libfyaml/compare/a92caa88b0b0fe61296325a25fed99a91d77bb6c...9f2492ca27bb1fda64f2b12edc2da17406208b93.patch";
+      hash = "sha256-hIwVwyHci4SeZ9PR/Z6Sf+cS0lP7COpQ3DVua6ll9+o=";
+    })
+
+    # backport 32-bit build fixes
     (fetchpatch {
       url = "https://github.com/pantoniou/libfyaml/commit/0982fcefc6a16d4c8cb5b06747d3fc8e630de3ae.diff";
       hash = "sha256-aDubIn+et+1fWE7XU7a5AGZVacVFbAbC1PoSDrA6hXw=";
