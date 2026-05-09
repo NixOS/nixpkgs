e8308d0fcf36b7e198cbfbd21c82658225f56781
Tom Hunze <dev@thunze.de>
2026-05-09 12:18:50 +0200

    python3Packages.av: fix build
    https://github.com/PyAV-Org/PyAV/pull/2204
    https://github.com/PyAV-Org/PyAV/pull/2205
    
    Hydra: https://hydra.nixos.org/build/328526004
    
    Not bumping to 17.0.1 as that would be a breaking change and breaking
    changes are restricted at the time of writing.

--- pkgs/development/python-modules/av/default.nix
+++ pkgs/development/python-modules/av/default.nix
@@ -1,8 +1,8 @@
 {
   lib,
-  stdenv,
   buildPythonPackage,
   fetchFromGitHub,
+  fetchpatch,
 
   # build-system
   cython,
@@ -34,6 +34,21 @@ buildPythonPackage rec {
     tag = "v${version}";
     hash = "sha256-mz0VI72lqtur5HdCkPNxInk0pUWxji0boIZnfvdrxIs=";
   };
+
+  patches = [
+    # Upstream PR: https://github.com/PyAV-Org/PyAV/pull/2204
+    (fetchpatch {
+      name = "ffmpeg-8.1-compat-test_skip_samples_remux.patch";
+      url = "https://github.com/PyAV-Org/PyAV/commit/57d8f4d7215a7b7cc7d01415613fc6aa2831a8d3.patch";
+      hash = "sha256-bDp8p2Qy9tU1GD9YqxU3O0ozPxhX/Fu9p9Zy1BIlPXA=";
+    })
+    # Upstream PR: https://github.com/PyAV-Org/PyAV/pull/2205
+    (fetchpatch {
+      name = "ffmpeg-8.1-compat-test_reformat_pixel_format_align.patch";
+      url = "https://github.com/PyAV-Org/PyAV/commit/900e39b9f0f7df33c3ea192b6dc13736e0b1561c.patch";
+      hash = "sha256-5SDLg5kQl2XjKX73rEIVSTznWAbC1oHr6EXfcufwhNM=";
+    })
+  ];
 
   build-system = [
     cython
