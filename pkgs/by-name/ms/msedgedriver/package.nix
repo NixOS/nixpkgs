45ae0cc8b220cf55eb855e895cd061218aa9081e
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-10 20:52:43 +0000

    microsoft-edge: 147.0.3912.98 -> 148.0.3967.54
    (cherry picked from commit eaa2fd12c04b43b1c0e5255c1c033f4ca25b09a1)

--- pkgs/by-name/ms/msedgedriver/package.nix
+++ pkgs/by-name/ms/msedgedriver/package.nix
@@ -11,33 +11,12 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "msedgedriver";
-  # finding a version that has all 4 builds is a pain
-  # https://msedgewebdriverstorage.z22.web.core.windows.net/?form=MA13LH
-  version = "130.0.2849.1";
-
-  src =
-    let
-      inherit (stdenvNoCC.hostPlatform) system;
-      selectSystem = attrs: attrs.${system} or (throw "Unsupported system: ${system}");
-      suffix = selectSystem {
-        x86_64-linux = "linux64";
-        aarch64-linux = "arm64";
-        x86_64-darwin = "mac64";
-        aarch64-darwin = "mac64_m1";
-      };
-
-      hash = selectSystem {
-        x86_64-linux = "sha256-U6YGD2PAhVUa7f+R5pmKLazGLOBbf3bRqzlwIJewA+w=";
-        aarch64-linux = "sha256-QJ1jRw8kkWbT8US5qI8DMZI/7Q8yJWpFXrfzGdxDWKE=";
-        x86_64-darwin = "sha256-Ejcv1DtuEiLJvTsv48AwoCQlFO3xM9PkM3HvZG65AC4=";
-        aarch64-darwin = "sha256-ykn4bYREE6xmJY02WiCRGsGnyWjnmnZM8FemK4XZqhc=";
-      };
-    in
-    fetchzip {
-      url = "https://msedgedriver.azureedge.net/${finalAttrs.version}/edgedriver_${suffix}.zip";
-      inherit hash;
-      stripRoot = false;
-    };
+  version = "148.0.3967.54";
+  src = fetchzip {
+    url = "https://msedgedriver.microsoft.com/${finalAttrs.version}/edgedriver_linux64.zip";
+    hash = "sha256-woGkky1i9so+1D61irtJYjDQ0xoHUeGQsJi/eQ4VGhU=";
+    stripRoot = false;
+  };
 
   buildInputs = [
     glib
