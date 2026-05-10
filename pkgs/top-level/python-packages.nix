a80fce8305f8199ed75690d1d2963be97af53162
Jamie Magee <jamie.magee@gmail.com>
2026-05-09 22:23:11 -0700

    python3Packages.eurotronic-cometblue-ha: init at 1.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5182,6 +5182,8 @@ self: super: with self; {
 
   euporie = callPackage ../development/python-modules/euporie { };
 
+  eurotronic-cometblue-ha = callPackage ../development/python-modules/eurotronic-cometblue-ha { };
+
   eval-type-backport = callPackage ../development/python-modules/eval-type-backport { };
 
   evaluate = callPackage ../development/python-modules/evaluate { };
