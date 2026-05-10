a576b4e3a9d87d8fbc38f5877a7dda76fd4e7249
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-10 16:16:55 +0000

    tofu-ls: 0.4.1 -> 0.4.2

--- pkgs/by-name/to/tofu-ls/package.nix
+++ pkgs/by-name/to/tofu-ls/package.nix
@@ -8,13 +8,15 @@
 
 buildGoModule (finalAttrs: {
   pname = "tofu-ls";
-  version = "0.4.1";
+  version = "0.4.2";
+
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "opentofu";
     repo = "tofu-ls";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-C38Iqk+1TplSeoZcdhskdtGYUqK1ABtInXQkPDnGCsg=";
+    hash = "sha256-B7y22tmD8PLO4FRI0rskffRQAZu8Kvi8TyzmjCYsWs8=";
   };
 
   vendorHash = "sha256-Uq/4rd3OvCBhp53MEMLiWL/V6hkygwdBLSN8Wzwqoew=";
@@ -48,6 +50,7 @@ buildGoModule (finalAttrs: {
   meta = {
     description = "OpenTofu Language Server";
     homepage = "https://github.com/opentofu/tofu-ls";
+    changelog = "https://github.com/opentofu/tofu-ls/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [ GaetanLepage ];
     mainProgram = "tofu-ls";
