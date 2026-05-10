2967076bab77868bc9b4ef8558d625fbbea872fa
Yueh-Shun Li <shamrocklee@posteo.net>
2026-05-11 03:37:37 +0800

    plover: add plover.withPlugins
    Add plover.withPlugin
    taking a plugin selector similar to python.withPackages
    implemented by wrapping around the Plover-relaed executables from the
    `python.withPackage`-created Python environment
    containing Plover and selected plugins.
    
    Wrap around the Python environment to avoid installation collisions.

--- pkgs/by-name/pl/plover_4/package.nix
+++ pkgs/by-name/pl/plover_4/package.nix
@@ -1,3 +1,27 @@
-{ python3Packages }:
+{
+  lib,
+  plover,
+  python3Packages,
+}:
 
-python3Packages.toPythonApplication python3Packages.plover_4
+(python3Packages.toPythonApplication python3Packages.plover_4).overrideAttrs (
+  finalAttrs: previousAttrs: {
+    passthru = previousAttrs.passthru or { } // {
+      withPlugins = plover.withPlugins.override {
+        python3 = (
+          python3Packages.python.override (previousArgs: {
+            packageOverrides = lib.composeExtensions (previousArgs.packageOverrides or (_: _: { })) (
+              final: previous: {
+                plover = final.plover_4;
+              }
+            );
+          })
+        );
+      };
+      tests = {
+        plover-with-lapwing = finalAttrs.passthru.withPlugins (ps: with ps; [ plover-lapwing-aio ]);
+      }
+      // previousAttrs.passthru.tests or { };
+    };
+  }
+)
