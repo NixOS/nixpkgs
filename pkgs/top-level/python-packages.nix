d00eb26967dfa68e63cd0d901e6a50e9ce0ea8b6
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-05-12 01:53:38 +0200

    python3Packages.vcs-versioning: init at 1.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -21069,6 +21069,8 @@ self: super: with self; {
 
   vcrpy = callPackage ../development/python-modules/vcrpy { };
 
+  vcs-versioning = callPackage ../development/python-modules/vcs-versioning { };
+
   vcversioner = callPackage ../development/python-modules/vcversioner { };
 
   vdf = callPackage ../development/python-modules/vdf { };
