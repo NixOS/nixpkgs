dc3e5a21f8123be69be5ce8cee480abbfd0906fa
phanirithvij <phanirithvij2000@gmail.com>
2026-05-11 23:51:38 +0530

    snapcast: migrate to by-name
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3155,10 +3155,6 @@ with pkgs;
 
   # aka., pgp-tools
 
-  snapcast = callPackage ../applications/audio/snapcast {
-    pulseaudioSupport = config.pulseaudio or stdenv.hostPlatform.isLinux;
-  };
-
   specup = haskellPackages.specup.bin;
 
   spglib = callPackage ../development/libraries/spglib {
