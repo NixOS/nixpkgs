a33b4b765ac1c4d7bf256fb94af82c4f977ee703
phanirithvij <phanirithvij2000@gmail.com>
2026-05-12 00:05:32 +0530

    various: use finalAttrs
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>
    (cherry picked from commit c01c964d048c188299ab37079e7cf4704aa98f09)

--- pkgs/applications/video/qmediathekview/default.nix
+++ pkgs/applications/video/qmediathekview/default.nix
@@ -10,14 +10,14 @@
   wrapQtAppsHook,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "QMediathekView";
   version = "0.2.1";
 
   src = fetchFromGitHub {
     owner = "adamreichold";
     repo = "QMediathekView";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     sha256 = "0i9hac9alaajbra3lx23m0iiq6ww4is00lpbzg5x70agjrwj0nd6";
   };
 
@@ -42,11 +42,11 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "Alternative Qt-based front-end for the database maintained by the MediathekView project";
-    inherit (src.meta) homepage;
+    inherit (finalAttrs.src.meta) homepage;
     license = lib.licenses.gpl3Plus;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ dotlambda ];
     broken = stdenv.hostPlatform.isAarch64;
     mainProgram = "QMediathekView";
   };
-}
+})
