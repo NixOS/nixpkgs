7ac31391ce103d206712c019ea521aeaaacd0711
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-11 15:55:15 +0000

    python3Packages.py-expression-eval: 0.3.14 -> 0.3.13

--- pkgs/development/python-modules/py-expression-eval/default.nix
+++ pkgs/development/python-modules/py-expression-eval/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "py-expression-eval";
-  version = "0.3.14";
+  version = "0.3.13";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "axiacore";
     repo = "py-expression-eval";
     rev = "v${version}";
-    sha256 = "YxhZd8V6ofphcNdcbBbrT5mc37O9c6W1mfhsvFVC+KM=";
+    sha256 = "sha256-IKNTJ0FT1EHismpdeDje12RTZ6XYv9gsM08kCIzWJdg=";
   };
 
   meta = {
