e457bd8315538975e05b37d0930d94c875101157
Fede Barcelona <fede_rico_94@hotmail.com>
2026-05-12 11:20:10 +0200

    zeroclaw: 0.5.1 -> 0.7.5

--- pkgs/by-name/ze/zeroclaw/package.nix
+++ pkgs/by-name/ze/zeroclaw/package.nix
@@ -1,8 +1,10 @@
 {
   lib,
-  callPackage,
+  buildNpmPackage,
   rustPlatform,
   fetchFromGitHub,
+  cargo,
+  rustc,
   pkg-config,
   protobuf,
   sqlite,
@@ -12,27 +14,75 @@
   nix-update-script,
 }:
 
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "zeroclaw";
-  version = "0.5.1";
+let
+  version = "0.7.5";
 
   src = fetchFromGitHub {
     owner = "zeroclaw-labs";
     repo = "zeroclaw";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-5P+TjAf7i0oUxBCyBtagrmIHPXW2iZ6620PNxVYYjlQ=";
+    tag = "v${version}";
+    hash = "sha256-hVHfsBw3u0CLWAbmizLA9ZrB+3B0qBIrSUuzsyChwW0=";
   };
 
-  postPatch =
-    let
-      zeroclaw-web = callPackage ./zeroclaw-web { inherit (finalAttrs) src version; };
-    in
-    ''
-      mkdir -p web
-      ln -s ${zeroclaw-web} web/dist
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit src;
+    hash = "sha256-6MGIJsaqRp3k/ysjdu6BE2iM2sehERQR+QoSqiThSpg=";
+  };
+
+  web = buildNpmPackage {
+    pname = "zeroclaw-web";
+    inherit version src cargoDeps;
+
+    sourceRoot = "${src.name}/web";
+    cargoRoot = "..";
+
+    npmDepsHash = "sha256-DVL9kov8y1Eh3BM2Rpw+KbTDL6/AvT/epknM2X/Gf3E=";
+
+    nativeBuildInputs = [
+      cargo
+      rustc
+      rustPlatform.cargoSetupHook
+      pkg-config
+      protobuf
+    ];
+
+    buildInputs = [
+      sqlite
+    ];
+
+    postUnpack = ''
+      chmod -R u+w ${src.name}
     '';
 
-  cargoHash = "sha256-tpgeRLWyye43fuzw2MRevQx8YKEbyOnIgLzjg8EzwCg=";
+    preBuild = ''
+      pushd ..
+      mkdir -p web/dist && touch web/dist/index.html
+      cargo build --release -p xtask --bin web
+      ./target/release/web gen-api || true
+      popd
+      npx openapi-typescript ../target/openapi.json -o src/lib/api-generated.ts
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      cp -r dist $out
+      runHook postInstall
+    '';
+  };
+in
+rustPlatform.buildRustPackage {
+  pname = "zeroclaw";
+  inherit version src cargoDeps;
+
+  postPatch = ''
+    rm -rf web/dist
+    ln -s ${web} web/dist
+  '';
+
+  postInstall = ''
+    mkdir -p $out/bin/web
+    ln -s ${web} $out/bin/web/dist
+  '';
 
   nativeBuildInputs = [
     pkg-config
@@ -62,9 +112,9 @@ rustPlatform.buildRustPackage (finalAttrs: {
   meta = {
     description = "Fast, small, and fully autonomous AI assistant infrastructure — deploy anywhere, swap anything";
     homepage = "https://github.com/zeroclaw-labs/zeroclaw";
-    changelog = "https://github.com/zeroclaw-labs/zeroclaw/blob/${finalAttrs.src.tag}/CHANGELOG.md";
+    changelog = "https://github.com/zeroclaw-labs/zeroclaw/blob/v${version}/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ drupol ];
     mainProgram = "zeroclaw";
   };
-})
+}
