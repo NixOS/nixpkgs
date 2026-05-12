e682a0e50ac6a972e00b2347abfdc0d907e5e433
Vladimír Čunát <v@cunat.cz>
2026-05-12 18:27:09 +0200

    Revert "groff: only apply the latest patch on linux for now"
    This reverts commit 37a9427fd325fd4f815f4717a6280a7b076a173f.
    
    (cherry picked from commit b2e8b0c6028f84faa0cf2b959732f30591c7f18f)

--- pkgs/by-name/gr/groff/package.nix
+++ pkgs/by-name/gr/groff/package.nix
@@ -48,8 +48,7 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-dOKBl5W2r/QxrqyYPWOpyJaO6roqLrp9+LpMe0Hnz9g=";
   };
 
-  patches = lib.optionals stdenv.isLinux [
-    # TODO: apply everywhere on rebuild
+  patches = [
     # This revert a upstream refactor in continuous rendering mode, but this
     # causes a big performance regression for big manpages like
     # `man 5 configuration.nix`.
