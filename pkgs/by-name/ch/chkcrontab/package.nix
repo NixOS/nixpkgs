f67c9cd6a6e5311e0b4aef663c38376eb89bc4cd
Sigmanificient <edhyjox@gmail.com>
2026-05-13 01:09:42 +0200

    chkcrontab: migration to pyproject

--- pkgs/by-name/ch/chkcrontab/package.nix
+++ pkgs/by-name/ch/chkcrontab/package.nix
@@ -9,12 +9,20 @@ with python3.pkgs;
 buildPythonApplication (finalAttrs: {
   pname = "chkcrontab";
   version = "1.7";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
     sha256 = "0gmxavjkjkvjysgf9cf5fcpk589gb75n1mn20iki82wifi1pk1jn";
   };
+
+  postPatch = ''
+    # cannot install the manpage as it is not present in the wheel
+    substituteInPlace setup.py \
+      --replace-fail "'doc/chkcrontab.1'" ""
+  '';
+
+  build-system = [ setuptools ];
 
   meta = {
     description = "Tool to detect crontab errors";
