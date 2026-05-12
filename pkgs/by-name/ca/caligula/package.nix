f7989144f8f7be036920d3aa9636a073ece67468
Astrid Yu <astrid@astrid.tech>
2026-05-11 21:16:58 -0700

    (unstable) caligula: update from v0.4.11 -> v0.5.0-rc.2

--- pkgs/by-name/ca/caligula/package.nix
+++ pkgs/by-name/ca/caligula/package.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "caligula";
-  version = "0.4.11";
+  version = "0.5.0-rc.2";
 
   src = fetchFromGitHub {
     owner = "ifd3f";
     repo = "caligula";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-2KCP7Utb785yIn8w/Ls19UPS9ylg1PtLRki87+BD+xw=";
+    hash = "sha256-6f5g+KXvaVWr0BAxvYJ1oCxWPLa/32zOIcPvirYntKA=";
   };
 
-  cargoHash = "sha256-C86wu2Pc9O7YM1TnnfotzzOQlnJXJe2zmsX04JyJsjA=";
+  cargoHash = "sha256-vfh0PKioBG7ZP8S+VLbp5iyl282rAxo/dj0VRgFgyN0=";
 
   nativeBuildInputs = [
     rustPlatform.bindgenHook
