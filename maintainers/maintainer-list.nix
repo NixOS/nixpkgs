9a320e422647319e0161db2676b0b2d0d2de3f05
rachyandco <alexis@nymtech.net>
2026-05-13 22:43:35 +0200

    maintainers: add rachyandco
    I work for Nym Technologies SA; this maintainer entry is used on
    Nym-related packages including nym, nym-wg-go, and the forthcoming
    nym-vpn and nym-vpn-app packages.

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -22670,6 +22670,12 @@
     github = "DaRacci";
     githubId = 90304606;
   };
+  rachyandco = {
+    name = "rachyandco";
+    email = "alexis@nymtech.net";
+    github = "rachyandco";
+    githubId = 2358699;
+  };
   RadxaYuntian = {
     # This is the work account for @MakiseKurisu
     name = "ZHANG Yuntian";
