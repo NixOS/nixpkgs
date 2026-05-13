2bb92cd25e46fd0beb605ff2aec5fa06b3e62d8e
transcaffeine <transcaffeine@finally.coffee>
2026-05-13 20:23:42 +0200

    snipe-it: 8.4.1 -> 8.6.1
    Release notes:
      - https://github.com/grokability/snipe-it/releases/tag/v8.5.0
      - https://github.com/grokability/snipe-it/releases/tag/v8.6.0
      - https://github.com/grokability/snipe-it/releases/tag/v8.6.1
    Full changelog: https://github.com/grokability/snipe-it/compare/v8.4.1...v8.6.1

--- pkgs/by-name/sn/snipe-it/package.nix
+++ pkgs/by-name/sn/snipe-it/package.nix
@@ -12,16 +12,16 @@ let
 in
 php.buildComposerProject2 (finalAttrs: {
   pname = "snipe-it";
-  version = "8.4.1";
+  version = "8.6.1";
 
   src = fetchFromGitHub {
     owner = "grokability";
     repo = "snipe-it";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-SSz0bFhspS3c5z0+mpub6gIqiMwzJ/5YfzPs9NdClCo=";
+    hash = "sha256-lawidSRq7109/J05OLMswGEuonLEbAbVbWNZD+moUFQ=";
   };
 
-  vendorHash = "sha256-crHl0CU5nf1SuXWf2O4tDLiNW1T7ku5cfNXjeNE6bDw=";
+  vendorHash = "sha256-9CAUNDh7x9FiYPNGZoDedT7xB9Fve801OIm7aie502o=";
 
   postInstall = ''
     snipe_it_out="$out/share/php/snipe-it"
