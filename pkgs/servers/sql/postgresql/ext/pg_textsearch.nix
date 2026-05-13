72284d811c1996fc8e3e78a271629771e4fa3f81
Daniel Beecham daniel@beecham.se <daniel@beecham.se>
2026-05-13 15:59:40 +0200

    pg_textsearch: 1.1.0 -> 1.2.0
    https://github.com/timescale/pg_textsearch/releases/tag/v1.2.0

--- pkgs/servers/sql/postgresql/ext/pg_textsearch.nix
+++ pkgs/servers/sql/postgresql/ext/pg_textsearch.nix
@@ -8,36 +8,13 @@
 
 postgresqlBuildExtension (finalAttrs: {
   pname = "pg_textsearch";
-  version = "1.1.0";
+  version = "1.2.0";
 
   src = fetchFromGitHub {
     owner = "timescale";
     repo = "pg_textsearch";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zZctWEJVItzJBke46J4CgvOAGDLmOZbCeUThkrnaPug";
-  };
-
-  passthru.tests.extension = postgresqlTestExtension {
-    inherit (finalAttrs) finalPackage;
-    postgresqlExtraSettings = ''
-      shared_preload_libraries='pg_textsearch'
-    '';
-    sql = ''
-      CREATE EXTENSION IF NOT EXISTS pg_textsearch;
-      CREATE TABLE documents (content text);
-      INSERT INTO documents VALUES
-        ('NixOS provides declarative configuration and reproducible system builds with the Nix package manager'),
-        ('Nix was originally developed by Eelco Dolstra'),
-        ('PostgreSQL is a powerful, open source object-relational database system');
-      CREATE INDEX documents_content_bm25_idx ON documents USING bm25(content) WITH (text_config='english');
-    '';
-    asserts = [
-      {
-        query = "SELECT count(*) FROM documents ORDER BY content <@> 'nix' LIMIT 10";
-        expected = "2";
-        description = "BM25 index can be queried successfully.";
-      }
-    ];
+    hash = "sha256-aFuaz/gd72rdMdQKI12ENF+CrKPaiqxysHUYidkLsHc=";
   };
 
   meta = {
