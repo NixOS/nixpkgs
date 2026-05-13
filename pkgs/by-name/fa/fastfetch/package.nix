2b7b9e3fc524b3f1b73ccaa3a8dd4a2c56848e42
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-13 09:18:47 +0000

    fastfetch: 2.62.1 -> 2.63.0

--- pkgs/by-name/fa/fastfetch/package.nix
+++ pkgs/by-name/fa/fastfetch/package.nix
@@ -61,13 +61,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "fastfetch";
-  version = "2.62.1";
+  version = "2.63.0";
 
   src = fetchFromGitHub {
     owner = "fastfetch-cli";
     repo = "fastfetch";
     tag = finalAttrs.version;
-    hash = "sha256-lI3p0LPDg5EXQ60NIYkpv0sNeckUdZjJSsmc2XP1l0E=";
+    hash = "sha256-PdQagV8PCmDi6Jgok4ZqvaHvoclof4Pux8nRQIoyUik=";
   };
 
   outputs = [
