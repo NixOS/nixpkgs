0cfbee581ddaf8cd90d45c7d7eb7c1ff93534971
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-14 00:26:18 +0000

    opencode: 1.14.48 -> 1.14.49

--- pkgs/by-name/op/opencode/package.nix
+++ pkgs/by-name/op/opencode/package.nix
@@ -16,13 +16,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.14.48";
+  version = "1.14.49";
 
   src = fetchFromGitHub {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-gyybqabTco+5ZeWv4lCX8t/R9Jm3tYsA8wVvkrxkEYQ=";
+    hash = "sha256-rh3FEUf8OjE1jSuiMT2RcRM5LIxHcrkBI2LFKpEjHV0=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -75,7 +75,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-94uXrhyGqW016U6LPE/xIfZGoDOzyUto5DyQrYYePds=";
+    outputHash = "sha256-gEgvlufMz8nsG7xoBqs/GFMTo88Rz6yfqBWhxJM8Iz0=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
