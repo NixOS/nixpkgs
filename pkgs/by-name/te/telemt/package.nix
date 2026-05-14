004f012b56985667a58c1c00722076ad51621a10
Harinn <prinn.dev@pm.me>
2026-05-14 10:59:55 +0700

    telemt: skip flaky tests

--- pkgs/by-name/te/telemt/package.nix
+++ pkgs/by-name/te/telemt/package.nix
@@ -16,6 +16,13 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-x5/SLSlYeGx40oXj/I/5zvyTNgSBwKsA33OwVIq9LGw=";
 
+  checkFlags = [
+    # flaky: races between MiddleClientWriterCancelled and TrafficBudgetWaitCancelled observation paths
+    "--skip=proxy::middle_relay::middle_relay_atomic_quota_invariant_tests::me_writer_data_write_obeys_flow_cancellation"
+    # flaky: timing-coupling assertion fires on slower hardware
+    "--skip=proxy::masking::masking_timing_budget_coupling_security_tests::adversarial_delayed_interface_lookup_does_not_consume_outcome_floor_budget"
+  ];
+
   meta = {
     mainProgram = "telemt";
     description = "MTProxy for Telegram on Rust + Tokio";
