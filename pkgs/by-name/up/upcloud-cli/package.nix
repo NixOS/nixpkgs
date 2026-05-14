4931af19d9dd7f30cdec86a29cdf0db776a3d7f8
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-14 12:00:42 +0000

    upcloud-cli: 3.29.0 -> 3.33.0

--- pkgs/by-name/up/upcloud-cli/package.nix
+++ pkgs/by-name/up/upcloud-cli/package.nix
@@ -9,16 +9,16 @@
 
 buildGo125Module (finalAttrs: {
   pname = "upcloud-cli";
-  version = "3.29.0";
+  version = "3.33.0";
 
   src = fetchFromGitHub {
     owner = "UpCloudLtd";
     repo = "upcloud-cli";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-4TsXif8jO8NvoHFXSLbkQ3lJltUa54ESbtBwAncPXQ4=";
+    hash = "sha256-vI7GqVP0v0HkZE9gaj8a1N2TRZVDqb5OOA0ozMSLYpM=";
   };
 
-  vendorHash = "sha256-d4YYTQhAmTvf2JCN2f9XaDchXyc/6Fg5KNkY0QH9viQ=";
+  vendorHash = "sha256-iVg3nFC5YLeoVR5dlkzsAZkLHi6bDMr18GD6r7fOMis=";
 
   ldflags = [
     "-s -w -X github.com/UpCloudLtd/upcloud-cli/v3/internal/config.Version=${finalAttrs.version}"
