f4cbcfca64cae4d5d6ea5660bcd85797b5b8bb8d
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-15 12:58:06 +0000

    python3Packages.imgw-pib: 2.1.1 -> 2.2.0

--- pkgs/development/python-modules/imgw-pib/default.nix
+++ pkgs/development/python-modules/imgw-pib/default.nix
@@ -15,14 +15,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "imgw-pib";
-  version = "2.1.1";
+  version = "2.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "bieniu";
     repo = "imgw-pib";
     tag = finalAttrs.version;
-    hash = "sha256-R1WhKOboItkKamO+hyyJyTl0veDCHDc4N70UE7IuFqI=";
+    hash = "sha256-VAmRrcTTNro8J/+25YeHII7jpw2BI87FNeDIo5ATOjQ=";
   };
 
   build-system = [ setuptools ];
