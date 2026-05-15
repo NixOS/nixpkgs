da1a5fd9772f4ea8c2b79eb999de6887017cec55
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-05-15 21:00:00 +0200

    yarnBuildHook: add `yarnBuildGlobalFlags`
    So it can be used with e.g. Yarn Berry

--- pkgs/by-name/co/corepack/package.nix
+++ pkgs/by-name/co/corepack/package.nix
@@ -3,6 +3,7 @@
   stdenvNoCC,
   cacert,
   yarn-berry,
+  yarnBuildHook,
   nodejs-slim, # no need for npm
   fetchFromGitHub,
   nix-update-script,
@@ -39,6 +40,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     nodejs
     yarn-berry
     yarn-berry.yarnBerryConfigHook
+    yarnBuildHook
   ];
   buildInputs = [
     nodejs
@@ -66,13 +68,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
       --replace-fail 'require.resolve(`corepack/package.json`)' "'$out/package.json'"
   '';
 
-  buildPhase = ''
-    runHook preBuild
-
-    yarn build
-
-    runHook postBuild
-  '';
+  yarnBuildGlobalFlags = " "; # override the default `--offline` which is not supported by Yarn Berry
 
   installPhase = ''
     runHook preInstall
