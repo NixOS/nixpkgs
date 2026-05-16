35e72ac092650f9f0123cd7e685c9b363f5f6dec
Jakob Stender Guldberg <jakob1379@gmail.com>
2026-05-16 09:36:21 +0200

    netbird: add shadow to service PATH

--- nixos/modules/services/networking/netbird.nix
+++ nixos/modules/services/networking/netbird.nix
@@ -592,7 +592,7 @@ in
           after = [ "network.target" ];
           wantedBy = [ "multi-user.target" ];
 
-          path = optionals (!config.services.resolved.enable) [ pkgs.openresolv ];
+          path = [ pkgs.shadow ] ++ optionals (!config.services.resolved.enable) [ pkgs.openresolv ];
 
           serviceConfig = {
             ExecStart = "${getExe client.wrapper} service run";
