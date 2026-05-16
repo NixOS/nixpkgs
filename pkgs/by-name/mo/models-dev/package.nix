3233d7768f20fbba43c8dea4ef816fda331be6e4
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-16 14:41:18 +0000

    models-dev: 0-unstable-2026-05-07 -> 0-unstable-2026-05-16

--- pkgs/by-name/mo/models-dev/package.nix
+++ pkgs/by-name/mo/models-dev/package.nix
@@ -8,12 +8,12 @@
 }:
 let
   pname = "models-dev";
-  version = "0-unstable-2026-05-07";
+  version = "0-unstable-2026-05-16";
   src = fetchFromGitHub {
     owner = "anomalyco";
     repo = "models.dev";
-    rev = "2e5b87a9c20c198dba4e3c8eba3ccfa959499790";
-    hash = "sha256-dZLzWgur3zccc92/iaeckcCMCFYOqBz49p1b5gzMaDY=";
+    rev = "ac81822c89b385cf2ac54bdc24724da4e6961910";
+    hash = "sha256-XOXiVUkgKnHpXvLCsSG+PabPbJH83aPqFEgD3NmLiNk=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -57,7 +57,7 @@ let
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-eXgzhb/yhgw0B1WDrpaEmH5Tyya+/rye6kJVKdiV56w=";
+    outputHash = "sha256-kn5Ung5DGDYMf5MHnZ+jsqXCg+MYahfkbiixcD9kh4Y=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
