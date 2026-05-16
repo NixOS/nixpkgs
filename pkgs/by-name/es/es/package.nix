2f18d656098df5c825718448c78d91ea5aec5418
Maximilian Bosch <maximilian@mbosch.me>
2026-05-16 16:13:31 +0200

    es: 0.9.2 -> 0.10.0
    ZHF #516381
    Failing Hydra build: https://hydra.nixos.org/build/326790858#tabs-summary
    
    ChangeLog: https://github.com/wryun/es-shell/releases/tag/v0.10.0

--- pkgs/by-name/es/es/package.nix
+++ pkgs/by-name/es/es/package.nix
@@ -5,36 +5,24 @@
   fetchurl,
   readline,
   bison,
+  autoreconfHook,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
 
   pname = "es";
-  version = "0.9.2";
+  version = "0.10.0";
 
   src = fetchurl {
     url = "https://github.com/wryun/es-shell/releases/download/v${finalAttrs.version}/es-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-ySZIK0IITpA+uHHuHrDO/Ana5vGt64QI3Z6TMDXE9d0=";
+    sha256 = "sha256-VR7Al0gi7Agee5+O55N0xidmym3NscaFqY79w+bbxLk=";
   };
 
-  # The distribution tarball does not have a single top-level directory.
-  preUnpack = ''
-    mkdir $name
-    cd $name
-    sourceRoot=.
-  '';
-
-  patches = [
-    (fetchpatch {
-      # https://github.com/wryun/es-shell/pull/101
-      name = "new-compiler-issues.patch";
-      url = "https://github.com/wryun/es-shell/commit/1eafb5fc4be735e59c9a091cc30adbca8f86fd96.patch";
-      hash = "sha256-0CV1seEiH6PsUnq0akPLiRMy+kIb9qnAK7Ta4I47i60=";
-    })
-  ];
-
   strictDeps = true;
-  nativeBuildInputs = [ bison ];
+  nativeBuildInputs = [
+    bison
+    autoreconfHook
+  ];
   buildInputs = [ readline ];
 
   configureFlags = [ "--with-readline" ];
