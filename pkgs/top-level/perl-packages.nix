73c35c910010a41cdcd90f218725617cf690420d
Thomas Gerbet <thomas@gerbet.me>
2026-05-17 22:26:00 +0200

    perlPackages.ConvertPEM: init at 0.13

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -6390,6 +6390,33 @@ with self;
     };
   };
 
+  ConvertPEM = buildPerlPackage {
+    pname = "Convert-PEM";
+    version = "0.13";
+    src = fetchurl {
+      url = "mirror://cpan/authors/id/T/TI/TIMLEGGE/Convert-PEM-0.13.tar.gz";
+      hash = "sha256-eZ+jLCcAgfTmKSsN31GAlScQqKS+Ey6Qe9oxdqe9HCM=";
+    };
+    nativeCheckInputs = [
+      TestClass
+      TestException
+      ClassErrorHandler
+      pkgs.openssl
+    ];
+    propagatedBuildInputs = [
+      ConvertASN1
+      CryptDES_EDE3
+      CryptX
+    ];
+    meta = {
+      description = "Read/write encrypted ASN.1 PEM files";
+      license = with lib.licenses; [
+        artistic1
+        gpl1Plus
+      ];
+    };
+  };
+
   ConvertUU = buildPerlPackage {
     pname = "Convert-UU";
     version = "0.5201";
