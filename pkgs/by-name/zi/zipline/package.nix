dea21382eb85e313c2f667b56a5d11f25bcc5798
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-17 18:01:19 +0000

    zipline: 4.5.3 -> 4.6.0
    (cherry picked from commit 95c4d71846bc9b792d0a9f6b842156610dd7ac05)

--- pkgs/by-name/zi/zipline/package.nix
+++ pkgs/by-name/zi/zipline/package.nix
@@ -36,13 +36,13 @@ in
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "zipline";
-  version = "4.5.3";
+  version = "4.6.0";
 
   src = fetchFromGitHub {
     owner = "diced";
     repo = "zipline";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-uZnN+kqtCGydrqnOVkhlNiCNSz4g2qLPiYTXpdmv/Mc=";
+    hash = "sha256-8Uay/+GRuVhu3MHg6gunHfWmKWBWDRy7zWAQkxKqh0k=";
     leaveDotGit = true;
     postFetch = ''
       git -C $out rev-parse --short HEAD > $out/.git_head
@@ -53,8 +53,8 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     pnpm = pnpm';
-    fetcherVersion = 2;
-    hash = "sha256-eoZi4JMN9PiiRRd/z/HjqSHX9ta33cL4+d1GMGxJ33U=";
+    fetcherVersion = 3;
+    hash = "sha256-AaXlkn6Fz9EMlxIpRhI0GH73DuK00qbgr19nlmd4ZOo=";
   };
 
   buildInputs = [
