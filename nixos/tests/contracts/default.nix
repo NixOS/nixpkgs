796ad663b3232726402766b6a2eb5c8640eba58a
ibizaman <ibizaman@tiserbox.com>
2026-05-17 10:55:36 +0200

    contracts: init testing location

---
+++ nixos/tests/contracts/default.nix
@@ -0,0 +1,13 @@
+{ runTest }:
+{
+  /*
+    Tests for contract providers should be added here.
+
+    Each contract has its generic test in a separate folder <contract name>/genericTest.nix.
+
+    To prove a provider implements a contract correctly, add a file under <contract name>/<provider name>.nix
+    which implements the generic test at <contract name>/default.nix. Then, add a line here in the form
+
+       <contract name>-<provider name> = runTest ./<contract name>/<provider name>.nix
+  */
+}
