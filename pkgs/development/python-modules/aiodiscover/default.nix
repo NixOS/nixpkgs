4f8d1907c9499714fa4b7ca94dbac658e49f9ae9
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-17 23:35:06 +0200

    python3Packages.aiodiscover: modernize

--- pkgs/development/python-modules/aiodiscover/default.nix
+++ pkgs/development/python-modules/aiodiscover/default.nix
@@ -13,7 +13,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aiodiscover";
   version = "3.0.0";
   pyproject = true;
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "bdraco";
     repo = "aiodiscover";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-O5aQ2yCcy6ZtDviH8Jie3BrgS4kPhSDHBVhbXco5oqM=";
   };
 
@@ -51,8 +51,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python module to discover hosts via ARP and PTR lookup";
     homepage = "https://github.com/bdraco/aiodiscover";
-    changelog = "https://github.com/bdraco/aiodiscover/releases/tag/${src.tag}";
-    license = with lib.licenses; [ asl20 ];
+    changelog = "https://github.com/bdraco/aiodiscover/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
