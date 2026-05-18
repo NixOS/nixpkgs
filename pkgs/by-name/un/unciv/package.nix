9caaa9acdd79efc83f8c0c0300d8145aaca3c2d3
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-18 18:36:58 -0400

    unciv: building jar from source
    rather than pulling it in.
    
    also includes fixes to bundle distrubtion (plist)

--- pkgs/by-name/un/unciv/package.nix
+++ pkgs/by-name/un/unciv/package.nix
@@ -1,7 +1,9 @@
 {
   stdenv,
   lib,
-  fetchurl,
+  fetchFromGitHub,
+  gradle,
+  jdk21,
   copyDesktopItems,
   makeDesktopItem,
   makeWrapper,
@@ -12,60 +14,89 @@
   nix-update-script,
 }:
 
-  desktopItem = makeDesktopItem {
-    name = "unciv";
-    exec = "unciv";
-    comment = "An open-source Android/Desktop remake of Civ V";
-    desktopName = "Unciv";
-    icon = "unciv";
-    categories = [ "Game" ];
-  };
-
-  desktopIcon = fetchurl {
-    url = "https://github.com/yairm210/Unciv/blob/${version}/extraImages/Icons/Unciv%20icon%20v6.png?raw=true";
-    hash = "sha256-Zuz+HGfxjGviGBKTiHdIFXF8UMRLEIfM8f+LIB/xonk=";
-  };
-
-  envLibPath = lib.makeLibraryPath (
-    lib.optionals stdenv.hostPlatform.isLinux [
-      libGL
-      libpulseaudio
-      libxxf86vm
-    ]
-  );
-in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "unciv";
   version = "4.20.7";
 
-  src = fetchurl {
-    url = "https://github.com/yairm210/Unciv/releases/download/${version}/Unciv.jar";
-    hash = "sha256-RlKoiSxW0MPPFTDq72k6CvYTHIEZ0hR8/PhW0j8ERs8=";
+  src = fetchFromGitHub {
+    owner = "yairm210";
+    repo = "Unciv";
+    tag = finalAttrs.version;
+    hash = "sha256-xiOPdiNlDfG288L2TXs/fp84gnB92pWGOg79oZuJ/Lo=";
   };
 
-  dontUnpack = true;
-
   nativeBuildInputs = [
+    gradle
+    jdk21
     copyDesktopItems
     makeWrapper
   ];
 
+  env.JAVA_HOME = jdk21;
+
+  gradleBuildTask = ":desktop:dist";
+
+  mitmCache = gradle.fetchDeps {
+    inherit (finalAttrs) pname;
+    data = ./deps.json;
+  };
+
   installPhase = ''
     runHook preInstall
 
-    makeWrapper ${jre}/bin/java $out/bin/unciv \
-      --prefix LD_LIBRARY_PATH : "${envLibPath}" \
-      --prefix PATH : ${lib.makeBinPath [ jre ]} \
-      --add-flags "-jar ${src}"
+    mkdir -p $out/share/unciv $out/bin
+    cp desktop/build/libs/Unciv.jar $out/share/unciv/
 
-    install -Dm444 ${desktopIcon} $out/share/icons/hicolor/512x512/apps/unciv.png
+    install -Dm444 "extraImages/Icons/Unciv icon v6.png" "$out/share/icons/hicolor/512x512/apps/unciv.png"
+
+    ${lib.optionalString stdenv.hostPlatform.isDarwin ''
+      mkdir -p $out/Applications/Unciv.app/Contents/MacOS
+      makeWrapper ${jre}/bin/java "$out/Applications/Unciv.app/Contents/MacOS/Unciv" \
+        --prefix LD_LIBRARY_PATH : "${finalAttrs.passthru.libraries}" \
+        --add-flags "-jar $out/share/unciv/Unciv.jar" \
+        --append-flags '--data-dir=$HOME/.local/share/unciv'
+
+      ln -s "$out/Applications/Unciv.app/Contents/MacOS/Unciv" "$out/bin/unciv"
+
+      CP="$out/Applications/Unciv.app/Contents"
+      mkdir -p "$CP/Resources"
+      cp "extraImages/Icons/Unciv icon v6.png" "$CP/Resources/Unciv.png"
+
+      substitute "${./info.plist}" "$CP/Info.plist" \
+        --replace-fail "@version@" "${finalAttrs.version}"
+    ''}
+
+    ${lib.optionalString stdenv.hostPlatform.isLinux ''
+      makeWrapper ${jre}/bin/java $out/bin/unciv \
+        --prefix LD_LIBRARY_PATH : "${finalAttrs.passthru.libraries}" \
+        --add-flags "-jar $out/share/unciv/Unciv.jar" \
+        --append-flags '--data-dir=$HOME/.local/share/unciv'
+    ''}
 
     runHook postInstall
   '';
 
-  desktopItems = [ desktopItem ];
+  desktopItems = [
+    (makeDesktopItem {
+      name = "unciv";
+      exec = "unciv";
+      comment = "An open-source Android/Desktop remake of Civ V";
+      desktopName = "Unciv";
+      icon = "unciv";
+      categories = [ "Game" ];
+    })
+  ];
 
-  passthru.updateScript = nix-update-script { };
+  passthru = {
+    libraries = lib.makeLibraryPath (
+      [
+        libGL
+        libpulseaudio
+      ]
+      ++ lib.optional stdenv.hostPlatform.isLinux libxxf86vm
+    );
+    updateScript = nix-update-script { };
+  };
 
   meta = {
     description = "Open-source Android/Desktop remake of Civ V";
@@ -75,9 +106,11 @@ stdenv.mkDerivation rec {
       iedame
       philocalyst
     ];
-    maintainers = with lib.maintainers; [ iedame ];
-    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
+    sourceProvenance = with lib.sourceTypes; [
+      fromSource
+      binaryBytecode
+    ];
     license = lib.licenses.mpl20;
     platforms = lib.platforms.all;
   };
-}
+})
