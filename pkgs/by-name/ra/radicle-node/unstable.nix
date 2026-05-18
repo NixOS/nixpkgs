5459a80df36b135456c295f56cad4c547fb6cbf6
Defelo <mail@defelo.de>
2026-05-18 16:41:08 +0000

    radicle-node-unstable: 1.9.0-rc.3 -> 1.9.0
    Changelog: https://radicle.network/nodes/seed.radicle.dev/rad:z3gqcJUoA1n9HaHKufZs5FCSGazv5/tree/CHANGELOG.md

--- pkgs/by-name/ra/radicle-node/unstable.nix
+++ pkgs/by-name/ra/radicle-node/unstable.nix
@@ -1,8 +1,8 @@
 { radicle-node }:
 
 radicle-node.override {
-  version = "1.9.0-rc.3";
-  srcHash = "sha256-NUxrNv0NbqZqE0zx8L+PS32jFeb/upoGtZorEcL4O1c=";
-  cargoHash = "sha256-WUOfZ41ImJQhX5Jqj6z8v28oapG/sP4zBe8inkfDlbA=";
+  version = "1.9.0";
+  srcHash = "sha256-ECged52tJaBSW2ua3MPDEBdGKPJ50Q347evd5wdbbU0=";
+  cargoHash = "sha256-Vb86Zx851Mrn9yaNuIQHEuVr7PrD7plPZhPGFJOLKRg=";
   updateScript = ./update-unstable.sh;
 }
