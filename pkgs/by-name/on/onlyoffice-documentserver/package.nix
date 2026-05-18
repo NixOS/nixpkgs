14a0256e0b3e8df6d175446ea5535ea21ffd5cac
Raito Bezarius <masterancpp@gmail.com>
2026-05-18 15:38:02 +0200

    onlyoffice-documentserver: add document-templates
    This is required for WOPI operations, otherwise the discovery endpoint
    will throw
    
    ```
    mai 14 18:58:03 onlyoffice-wrapper[31901]: [2026-05-14T18:58:03.608]
    [ERROR] [localhost] [docId] [userId] nodeJS - wopiDiscovery error:Error:
    ENOENT: no such file or directory, scandir
    '/var/www/onlyoffice/documentserver/document-templates/new/en-US/'
    mai 14 18:58:03 onlyoffice-wrapper[31901]:     at async readdir
    (node:internal/fs/promises:956:18)
    mai 14 18:58:03 onlyoffice-wrapper[31901]:     at async
    getTemplatesFolderExts
    (/nix/store/hyjpbwg2ckaqb50ky8wcklj4ixnpx59g-onlyoffice-server-DocService/lib/node_modules/coauthoring/sources/wopiClient.js:126:24)
    ```
    
    Change-Id: Iaca58692d2c0fd0b24297fabc87ca04bf3f31de5
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- pkgs/by-name/on/onlyoffice-documentserver/package.nix
+++ pkgs/by-name/on/onlyoffice-documentserver/package.nix
@@ -27,6 +27,14 @@ let
     tag = "v9.3.1.1";
     hash = "sha256-uN1L/4I7wrg0BqAAu3zdn8LqtdfJDAHnAMbCvzQnOvI=";
   };
+  # This is required but not included in the submodules for some reason.
+  # https://github.com/ONLYOFFICE/server/blob/34adaeeb4cc1e032a5cf188924880a25546dc67c/Makefile#L81-L83
+  document-templates-src = fetchFromGitHub {
+    owner = "ONLYOFFICE";
+    repo = "document-templates";
+    tag = "v9.3.1.1";
+    hash = "sha256-+52+MK/8DARJrQRbIpN5nk3j3J9cy6Wd1FDMnCVZKRE=";
+  };
   common = buildNpmPackage (finalAttrs: {
     name = "onlyoffice-server-Common";
     src = server-src;
@@ -180,6 +188,8 @@ let
 
       mkdir -p $out/var/www/onlyoffice/documentserver/server/schema
       cp -r ${server-src}/schema/* $out/var/www/onlyoffice/documentserver/server/schema
+
+      ln -s ${document-templates-src} $out/var/www/onlyoffice/documentserver/document-templates
 
       ## required for bwrap --bind
       chmod u+w $out/var
