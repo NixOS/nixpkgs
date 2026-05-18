4dfddc5e2db3c5fc103d318ef65d9cbb49030b47
Wolfgang Walther <walther@technowledgy.de>
2026-05-18 11:26:44 +0200

    haskellPackages.hw-prim: update comment

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -2101,7 +2101,7 @@ with haskellLib;
   # https://github.com/haskell-works/hw-string-parse/issues/43
   hw-string-parse = doJailbreak super.hw-string-parse;
 
-  # 2025-09-03: allow QuickCheck 2.15
+  # 2026-05-18: allow QuickCheck 2.16
   # https://github.com/haskell-works/hw-prim/issues/150
   hw-prim = lib.pipe super.hw-prim [
     (warnAfterVersion "0.6.3.2")
