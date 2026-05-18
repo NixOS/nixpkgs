15ee3931529ea5385708b514de21b11a61b24b28
Ryan Omasta <git@ryand.ca>
2026-05-17 23:40:37 -0600

    maintainers: remove dsferruzza

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7370,12 +7370,6 @@
     github = "DSeeLP";
     githubId = 46624152;
   };
-  dsferruzza = {
-    email = "david.sferruzza@gmail.com";
-    github = "dsferruzza";
-    githubId = 1931963;
-    name = "David Sferruzza";
-  };
   dsluijk = {
     name = "Dany Sluijk";
     email = "nix@dany.dev";
