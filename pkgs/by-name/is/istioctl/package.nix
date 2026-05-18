e6f84b26194088c80babff8669cb36bd2e08073a
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-18 20:46:50 +0000

    istioctl: 1.29.2 -> 1.30.0

--- pkgs/by-name/is/istioctl/package.nix
+++ pkgs/by-name/is/istioctl/package.nix
@@ -7,15 +7,15 @@
 
 buildGoModule (finalAttrs: {
   pname = "istioctl";
-  version = "1.29.2";
+  version = "1.30.0";
 
   src = fetchFromGitHub {
     owner = "istio";
     repo = "istio";
     rev = finalAttrs.version;
-    hash = "sha256-QGohEiUMCiXiaJGmd8x0o7trr2ZOB4cVQcUhPwnzZ/M=";
+    hash = "sha256-tCh6XRnr6X6dCqUT+7au0fY2d9vV3EXPsugMSRK+064=";
   };
-  vendorHash = "sha256-/RBckWhh+fpogxVz7G6lfnhWjEvqB2b/aXAXQiTBS08=";
+  vendorHash = "sha256-HvDcknniiEfg9UCZc7S5EJfD+T4laBbSj05h/GTscoI=";
 
   nativeBuildInputs = [ installShellFiles ];
 
