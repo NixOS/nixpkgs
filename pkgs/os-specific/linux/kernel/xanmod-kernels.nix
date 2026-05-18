ba4d7e65d8be84450de0a19b3a1541248ea9be51
eljamm <fedi.jamoussi@protonmail.ch>
2026-05-18 05:34:52 +0000

    linux_xanmod: 6.18.31 -> 6.18.32
    - Changelog: https://dl.xanmod.org/changelog/6.18/ChangeLog-6.18.32-xanmod1.gz
    - Diff: https://gitlab.com/xanmod/linux/-/compare/6.18.31-xanmod1..6.18.32-xanmod1?from_project_id=51590166

--- pkgs/os-specific/linux/kernel/xanmod-kernels.nix
+++ pkgs/os-specific/linux/kernel/xanmod-kernels.nix
@@ -15,8 +15,8 @@ let
   variants = {
     # ./update-xanmod.sh lts
     lts = {
-      version = "6.18.31";
-      hash = "sha256-AvsMS5Za+k+b08Nz4UOey9Uzg0Z9hT5lkwMN5cf5ZG8=";
+      version = "6.18.32";
+      hash = "sha256-XgqytdTnL3Jjcs9riMRGgRVfyy76jxdBcC0hSt4rc58=";
       isLTS = true;
     };
     # ./update-xanmod.sh main
