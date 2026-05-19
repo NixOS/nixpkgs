58072fd90c873d7202e570f6c1c606de15a650d7
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-19 23:14:10 +0200

    python3Packages.pyorthanc: migrate to finalAttrs

--- pkgs/development/python-modules/pyorthanc/default.nix
+++ pkgs/development/python-modules/pyorthanc/default.nix
@@ -7,24 +7,21 @@
   pydicom,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pyorthanc";
   version = "1.23.0";
-
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "gacou54";
     repo = "pyorthanc";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-L1vIU6oDZ95lFt2w/TYFpHdmHmDE2XPn10XdEUIlxRQ=";
   };
 
   build-system = [ poetry-core ];
 
-  pythonRelaxDeps = [
-    "pydicom"
-  ];
+  pythonRelaxDeps = [ "pydicom" ];
 
   dependencies = [
     httpx
@@ -38,8 +35,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python library that wraps the Orthanc REST API";
     homepage = "https://github.com/gacou54/pyorthanc";
-    changelog = "https://github.com/gacou54/pyorthanc/releases/tag/${src.tag}";
+    changelog = "https://github.com/gacou54/pyorthanc/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ bcdarwin ];
   };
-}
+})
