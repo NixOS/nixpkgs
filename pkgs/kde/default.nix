1675d9c14731b3f8c10bb7772cd31a0936f65f26
SandaruKasa <sandarukasa@ya.ru>
2026-05-19 17:45:45 +0300

    kdePackages.plasma-pass: init at 1.3.1

--- pkgs/kde/default.nix
+++ pkgs/kde/default.nix
@@ -85,6 +85,7 @@ let
         oxygen-icons = self.callPackage ./misc/oxygen-icons { };
         phonon = self.callPackage ./misc/phonon { };
         phonon-vlc = self.callPackage ./misc/phonon-vlc { };
+        plasma-pass = self.callPackage ./misc/plasma-pass { };
         plasma-wayland-protocols = self.callPackage ./misc/plasma-wayland-protocols { };
         polkit-qt-1 = self.callPackage ./misc/polkit-qt-1 { };
         pulseaudio-qt = self.callPackage ./misc/pulseaudio-qt { };
