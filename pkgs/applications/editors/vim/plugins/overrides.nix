c16bac4237999cda6fe80bf67fdce4b0dc680142
Tomas Rivera <tomas.riveral@icloud.com>
2026-05-19 15:57:21 +0200

    vimPlugins.vim-erlang-compiler: override license to vim

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -5156,6 +5156,12 @@ assertNoAdditions {
     };
   });
 
+  vim-erlang-compiler = super.vim-erlang-compiler.overrideAttrs (old: {
+    meta = old.meta // {
+      license = lib.licenses.vim;
+    };
+  });
+
   vim-erlang-omnicomplete = super.vim-erlang-omnicomplete.overrideAttrs (old: {
     meta = old.meta // {
       license = lib.licenses.vim;
