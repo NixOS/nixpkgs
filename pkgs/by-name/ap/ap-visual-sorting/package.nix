9a2105315168a7bc3dd9e8abd3120fad384138f0
cedric-star <cdrc.wnsch@gmail.com>
2026-05-19 17:21:05 +0200

    ap-visual-sorting: fix strictDeps and structuredAttrs

--- pkgs/by-name/ap/ap-visual-sorting/package.nix
+++ pkgs/by-name/ap/ap-visual-sorting/package.nix
@@ -13,13 +13,13 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "cedric-star";
     repo = "ap-visual-sorting";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-SPUfhQD4i04W3Om/qhcDGSs0/0qzv5EyijU43EovyZo="; # placeholder
+    hash = "sha256-DEIN-HASH-HIER=";
   };
 
-  buildInputs = [ raylib ];
+  __structuredAttrs = true; # NPV-166
+  strictDeps = true; # NPV-164
 
-  # Falls dein Makefile kein PREFIX/DESTDIR unterstützt:
-  makeFlags = [ "PREFIX=${placeholder "out"}" ];
+  buildInputs = [ raylib ];
 
   installPhase = ''
     runHook preInstall
@@ -32,8 +32,8 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Visualize sorting algorithms written in C";
     homepage = "https://github.com/cedric-star/ap-visual-sorting";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ ]; # deinen GitHub-Namen hier, nach PR
+    maintainers = [ ];
     mainProgram = "ap-visual-sorting";
-    platforms = lib.platforms.linux; # raylib hat Einschränkungen
+    platforms = lib.platforms.linux;
   };
 })
