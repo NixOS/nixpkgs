e04def5aaf92828b6ad77e9b321d4ff46c05bd50
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-19 16:04:19 -0400

    shattered-pixel-dungeon: generic: refactor to use explicit parameters

--- pkgs/by-name/sh/shattered-pixel-dungeon/generic.nix
+++ pkgs/by-name/sh/shattered-pixel-dungeon/generic.nix
@@ -7,6 +7,10 @@
   desktopName,
   patches ? [ ./disable-beryx.patch ],
   depsPath ? null,
+  sourceRoot ? null,
+  nativeBuildInputs ? [ ],
+  passthru ? { },
+  postPatch ? "",
 
   lib,
   stdenv,
@@ -18,20 +22,53 @@
   libpulseaudio,
   makeDesktopItem,
   copyDesktopItems,
-  ...
-}@attrs:
+}:
 
-let
-  cleanAttrs = removeAttrs attrs [
-    "lib"
-    "stdenv"
-    "makeWrapper"
-    "gradle"
-    "perl"
-    "jre"
-    "libpulseaudio"
-    "makeDesktopItem"
-    "copyDesktopItems"
+stdenv.mkDerivation (finalAttrs: {
+  inherit
+    pname
+    version
+    src
+    patches
+    ;
+
+  inherit sourceRoot;
+
+  mitmCache = gradle_8.fetchDeps {
+    inherit pname;
+    data = if depsPath != null then depsPath else ./. + "/${pname}/deps.json";
+  };
+
+  __darwinAllowLocalNetworking = true;
+
+  nativeBuildInputs = [
+    gradle_8
+    perl
+    makeWrapper
+    copyDesktopItems
+  ]
+  ++ nativeBuildInputs;
+
+  gradleBuildTask = "desktop:release";
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = pname;
+      inherit desktopName;
+      comment = meta.description;
+      icon = pname;
+      exec = pname;
+      terminal = false;
+      categories = [
+        "Game"
+        "AdventureGame"
+      ];
+      keywords = [
+        "roguelike"
+        "dungeon"
+        "crawler"
+      ];
+    })
   ];
 
   postPatch = ''
@@ -40,104 +77,48 @@ let
     perl -i.bak2 -pe "s#(.*)#// \1# if /^(buildscript|task portable|task nsis|task proguard|task tgz|task\(afterEclipseImport\)|launch4j|macAppBundle|buildRpm|buildDeb|shadowJar|robovm|git-version)/ ... /^}/" build.gradle
     # Remove unbuildable Android/iOS stuff
     rm -f android/build.gradle ios/build.gradle
-    ${attrs.postPatch or ""}
+  ''
+  + postPatch;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm644 desktop/build/libs/desktop-*.jar $out/share/${pname}.jar
+    mkdir $out/bin
+    makeWrapper ${jre}/bin/java $out/bin/${pname} \
+      --prefix LD_LIBRARY_PATH : ${
+        lib.makeLibraryPath [
+          libGL
+          libpulseaudio
+        ]
+      } \
+      --add-flags "-jar $out/share/${pname}.jar"
+
+    for s in 16 32 48 64 128 256; do
+      # Some forks only have some icons and/or name them slightly differently
+      if [ -f desktop/src/main/assets/icons/icon_$s.png ]; then
+        install -Dm644 desktop/src/main/assets/icons/icon_$s.png \
+          $out/share/icons/hicolor/''${s}x$s/apps/${pname}.png
+      fi
+      if [ -f desktop/src/main/assets/icons/icon_''${s}x$s.png ]; then
+        install -Dm644 desktop/src/main/assets/icons/icon_''${s}x$s.png \
+          $out/share/icons/hicolor/''${s}x$s/apps/${pname}.png
+      fi
+    done
+
+    runHook postInstall
   '';
 
-  desktopItem = makeDesktopItem {
-    name = pname;
-    inherit desktopName;
-    comment = meta.description;
-    icon = pname;
-    exec = pname;
-    terminal = false;
-    categories = [
-      "Game"
-      "AdventureGame"
+  inherit passthru;
+
+  meta = lib.recursiveUpdate {
+    sourceProvenance = with lib.sourceTypes; [
+      fromSource
+      binaryBytecode # deps
     ];
-    keywords = [
-      "roguelike"
-      "dungeon"
-      "crawler"
-    ];
-  };
-
-  depsPath' = if depsPath != null then depsPath else ./. + "/${pname}/deps.json";
-
-  # "Deprecated Gradle features were used in this build, making it incompatible with Gradle 9.0."
-  gradle = gradle_8;
-
-in
-stdenv.mkDerivation (
-  cleanAttrs
-  // {
-    inherit
-      pname
-      version
-      src
-      patches
-      postPatch
-      ;
-
-    mitmCache = gradle.fetchDeps {
-      inherit pname;
-      data = depsPath';
-    };
-
-    __darwinAllowLocalNetworking = true;
-
-    nativeBuildInputs = [
-      gradle
-      perl
-      makeWrapper
-      copyDesktopItems
-    ]
-    ++ attrs.nativeBuildInputs or [ ];
-
-    desktopItems = [ desktopItem ];
-
-    gradleBuildTask = "desktop:release";
-
-    installPhase = ''
-      runHook preInstall
-
-      install -Dm644 desktop/build/libs/desktop-*.jar $out/share/${pname}.jar
-      mkdir $out/bin
-      makeWrapper ${jre}/bin/java $out/bin/${pname} \
-        --prefix LD_LIBRARY_PATH : ${
-          lib.makeLibraryPath [
-            libGL
-            libpulseaudio
-          ]
-        } \
-        --add-flags "-jar $out/share/${pname}.jar"
-
-      for s in 16 32 48 64 128 256; do
-        # Some forks only have some icons and/or name them slightly differently
-        if [ -f desktop/src/main/assets/icons/icon_$s.png ]; then
-          install -Dm644 desktop/src/main/assets/icons/icon_$s.png \
-            $out/share/icons/hicolor/''${s}x$s/apps/${pname}.png
-        fi
-        if [ -f desktop/src/main/assets/icons/icon_''${s}x$s.png ]; then
-          install -Dm644 desktop/src/main/assets/icons/icon_''${s}x$s.png \
-            $out/share/icons/hicolor/''${s}x$s/apps/${pname}.png
-        fi
-      done
-
-      runHook postInstall
-    '';
-
-    meta =
-
-      {
-        sourceProvenance = with lib.sourceTypes; [
-          fromSource
-          binaryBytecode # deps
-        ];
-        license = lib.licenses.gpl3Plus;
-        maintainers = with lib.maintainers; [ fgaz ];
-        platforms = lib.platforms.all;
-        mainProgram = pname;
-      }
-      // meta;
-  }
-)
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ fgaz ];
+    platforms = lib.platforms.all;
+    mainProgram = pname;
+  } meta;
+})
