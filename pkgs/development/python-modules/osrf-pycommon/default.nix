55a5425fc4f9e59a52a852b06561f2823769ff92
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-19 20:37:53 +0200

    python3Packages.osrf-pycommon: modernize

--- pkgs/development/python-modules/osrf-pycommon/default.nix
+++ pkgs/development/python-modules/osrf-pycommon/default.nix
@@ -7,7 +7,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "osrf-pycommon";
   version = "2.1.7";
   pyproject = true;
@@ -15,15 +15,11 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "osrf";
     repo = "osrf_pycommon";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-gKYeCvcJDJkW2OYP7K3eyztuPSkzE8dHoTUh4sKvxcM=";
   };
 
   build-system = [ setuptools ];
-
-  disabledTestPaths = [
-    "tests/test_code_format.py" # flake8 based tests don't work
-  ];
 
   nativeCheckInputs = [
     pytest-cov-stub
@@ -32,14 +28,16 @@ buildPythonPackage rec {
 
   pythonImportsCheck = [ "osrf_pycommon" ];
 
+  disabledTestPaths = [
+    "tests/test_code_format.py" # flake8 based tests don't work
+  ];
+
   meta = {
     description = "Commonly needed Python modules used by Python software developed at OSRF";
     homepage = "http://osrf-pycommon.readthedocs.org/";
-    changelog = "https://github.com/osrf/osrf_pycommon/blob/${src.tag}/CHANGELOG.rst";
+    changelog = "https://github.com/osrf/osrf_pycommon/blob/${finalAttrs.src.tag}/CHANGELOG.rst";
     downloadPage = "https://github.com/osrf/osrf_pycommon";
     license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [
-      guelakais
-    ];
+    maintainers = with lib.maintainers; [ guelakais ];
   };
-}
+})
