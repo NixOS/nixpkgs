d2b8c76e57c3e624605503e082a2e22b4c14a27b
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-19 16:36:05 -0400

    freedink: full darwin support
    - and on finalattrs
    - and update tooling

--- pkgs/by-name/fr/freedink/package.nix
+++ pkgs/by-name/fr/freedink/package.nix
@@ -1,13 +1,16 @@
 {
+  autoreconfHook,
   cxxtest,
-  fetchurl,
+  desktopToDarwinBundle,
   fetchgit,
   fontconfig,
+  gettext,
   glm,
+  gnulib,
   help2man,
   intltool,
   lib,
-  libzip,
+  nix-update-script,
   pkg-config,
   SDL2,
   SDL2_gfx,
@@ -15,22 +18,22 @@
   SDL2_mixer,
   SDL2_ttf,
   stdenv,
-  zip,
-  zlib,
 }:
 
 let
-
-  freedink_data = stdenv.mkDerivation (finalAttrs: {
+  data = stdenv.mkDerivation (final: {
     pname = "freedink-data";
     version = "1.08.20190120";
 
+    src = fetchgit {
+      url = "https://git.savannah.gnu.org/git/freedink/freedink-data.git";
+      tag = "v${final.version}";
+      hash = "sha256-8/yKy/oAPQkvNNQPWYCaHREHly7vJE63956ky0KVYI0=";
+    };
 
-    prePatch = "substituteInPlace Makefile --replace-fail /usr/local $out";
+    postPatch = "substituteInPlace Makefile --replace-fail /usr/local $out";
   });
-
 in
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "freedink";
   version = "109.6";
@@ -41,78 +44,47 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-D+/Reg0+K9J1WcOpe3xdnIv90Cl0GCmB/2M1XU/3eOg=";
   };
 
-  passthru = {
-      src = fetchgit {
-        url = "https://git.savannah.gnu.org/git/freedink/freedink-data.git";
-        tag = "v${final.version}";
-        hash = "sha256-8/yKy/oAPQkvNNQPWYCaHREHly7vJE63956ky0KVYI0=";
-      };
+  patches = [ ./freedink.patch ];
+
   nativeBuildInputs = [
+    autoreconfHook
     cxxtest
+    gettext
+    gnulib
     help2man
     intltool
     pkg-config
-  ];
+  ]
+  ++ lib.optional stdenv.hostPlatform.isDarwin desktopToDarwinBundle;
 
   buildInputs = [
     fontconfig
     glm
-    libzip
     SDL2
-    SDL2_mixer
-    SDL2_image
-    SDL2_ttf
     SDL2_gfx
-    zip
-    zlib
+    SDL2_image
+    SDL2_mixer
+    SDL2_ttf
   ];
 
-  preBuild = ''
-        # Fix SDL2_ttf constness error in gfx_fonts.cpp
-        substituteInPlace src/gfx_fonts.cpp \
-          --replace-fail "char *familyname = TTF_FontFaceFamilyName(font);" \
-                    "const char *familyname = TTF_FontFaceFamilyName(font);"
-        substituteInPlace src/gfx_fonts.cpp \
-          --replace-fail "char *stylename = TTF_FontFaceStyleName(font);" \
-                    "const char *stylename = TTF_FontFaceStyleName(font);"
+  env.GNULIB_TOOL_IMPL = "sh";
 
-        # Fix missing SDL hint macro error in input.cpp
-        substituteInPlace src/input.cpp \
-          --replace-fail "SDL_SetHint(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH, \"0\");" \
-                    "/* SDL_SetHint(SDL_HINT_ANDROID_SEPARATE_MOUSE_AND_TOUCH, \"0\"); */"
-
-        # Fix config.h errors in some platforms
-        substituteInPlace src/ImageLoader.cpp \
-          --replace-fail '#include "ImageLoader.h"' '#include "../config.h"
-    #include "ImageLoader.h"'
-
-        substituteInPlace src/IOGfxGLFuncs.cpp \
-          --replace-fail '#include "IOGfxGLFuncs.h"' '#include "../config.h"
-    #include "IOGfxGLFuncs.h"'
-
-        substituteInPlace src/IOGfxSurface.cpp \
-          --replace-fail '#include "IOGfxSurface.h"' '#include "../config.h"
-    #include "IOGfxSurface.h"'
-
-        substituteInPlace src/IOGfxSurfaceSW.cpp \
-          --replace-fail '#include "IOGfxSurfaceSW.h"' '#include "../config.h"
-    #include "IOGfxSurfaceSW.h"'
-
-        substituteInPlace src/IOGfxPrimitivesSW.cpp \
-          --replace-fail '#include "SDL.h"' '#include "../config.h"
-    #include "SDL.h"'
-
-        substituteInPlace src/dinkc_console_renderer.cpp \
-          --replace-fail '#include "dinkc_console_renderer.h"' '#include "../config.h"
-    #include "dinkc_console_renderer.h"'
+  postPatch = ''
+    gnulib-tool --update --more-symlinks
   '';
 
   postInstall = ''
     mkdir -p "$out/share/"
-    ln -s ${freedink_data}/share/dink "$out/share/"
+    ln -s ${data}/share/dink "$out/share/"
   '';
 
   enableParallelBuilding = true;
+
+  passthru = {
+    # Data rarely changes, so not covered by update-script
+    updateScript = nix-update-script { };
+    inherit data;
+  };
 
   meta = {
     description = "Free, portable and enhanced version of the Dink Smallwood game engine";
@@ -121,7 +93,7 @@ stdenv.mkDerivation (finalAttrs: {
       game engine, which runs the original game as well as its D-Mods,
       with close compatibility, under multiple platforms.
     '';
-    homepage = "https://gnu.org/software/freedink/"; # Formerly http://www.freedink.org
+    homepage = "https://gnu.org/software/freedink/";
     license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [
       iedame
