c3e73a1804afec5b22be212a4f1a24c353f86147
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-18 20:43:21 -0400

    aaaaxy: darwin support
    and bundles!

--- pkgs/by-name/aa/aaaaxy/package.nix
+++ pkgs/by-name/aa/aaaaxy/package.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  stdenv,
   fetchFromGitHub,
   buildGoModule,
   alsa-lib,
@@ -16,6 +17,9 @@
   zip,
   advancecomp,
   makeWrapper,
+  installShellFiles,
+  imagemagick,
+  libicns,
   nixosTests,
   strip-nondeterminism,
 }:
@@ -35,8 +39,10 @@ buildGoModule (finalAttrs: {
   vendorHash = "sha256-WfaXUYJsl3UTNNRCB6bfM9lEz4A/3vE6qW8UiJod8hg=";
 
   buildInputs = [
-    alsa-lib
     libGL
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    alsa-lib
     libx11
     libxcursor
     libxext
@@ -53,6 +59,11 @@ buildGoModule (finalAttrs: {
     advancecomp
     makeWrapper
     strip-nondeterminism
+    installShellFiles
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    imagemagick
+    libicns
   ];
 
   outputs = [
@@ -70,9 +81,11 @@ buildGoModule (finalAttrs: {
     substituteInPlace scripts/regression-test-demo.sh \
       --replace-fail 'sh scripts/run-timedemo.sh' "$testing_infra/scripts/run-timedemo.sh"
 
-    substituteInPlace Makefile --replace-fail \
-      'CPPFLAGS ?= -DNDEBUG' \
-      'CPPFLAGS ?= -DNDEBUG -D_GLFW_GLX_LIBRARY=\"${lib.getLib libGL}/lib/libGL.so\" -D_GLFW_EGL_LIBRARY=\"${lib.getLib libGL}/lib/libEGL.so\"'
+    ${lib.optionalString stdenv.hostPlatform.isLinux ''
+      substituteInPlace Makefile --replace-fail \
+        'CPPFLAGS ?= -DNDEBUG' \
+        'CPPFLAGS ?= -DNDEBUG -D_GLFW_GLX_LIBRARY=\"${lib.getLib libGL}/lib/libGL.so\" -D_GLFW_EGL_LIBRARY=\"${lib.getLib libGL}/lib/libEGL.so\"'
+    ''}
   '';
 
   overrideModAttrs = (
@@ -81,7 +94,7 @@ buildGoModule (finalAttrs: {
       # this is a fixed output derivation and when the path to libGL
       # changes, the hash would change.
       # To work around this, use environment variables.
-      postBuild = ''
+      postBuild = lib.optionalString stdenv.hostPlatform.isLinux ''
         substituteInPlace 'vendor/github.com/hajimehoshi/ebiten/v2/internal/graphicsdriver/opengl/gl/procaddr_linbsd.go' \
           --replace-fail \
           'import (' \
@@ -97,8 +110,14 @@ buildGoModule (finalAttrs: {
   );
 
   makeFlags = [
-    "BUILDTYPE=release"
+    "BUILDTYPE=${if stdenv.hostPlatform.isDarwin then "ziprelease" else "release"}"
   ];
+
+  checkPhase = ''
+    runHook preCheck
+    go test ./internal/m/...
+    runHook postCheck
+  '';
 
   buildPhase = ''
     runHook preBuild
@@ -112,11 +131,33 @@ buildGoModule (finalAttrs: {
     install -Dm644 'aaaaxy.png' -t "$out/share/icons/hicolor/128x128/apps/"
     install -Dm644 'aaaaxy.desktop' -t "$out/share/applications/"
     install -Dm644 'io.github.divverent.aaaaxy.metainfo.xml' -t "$out/share/metainfo/"
-
+  ''
+  + lib.optionalString stdenv.hostPlatform.isLinux ''
     wrapProgram $out/bin/aaaaxy \
       --set EBITENGINE_LIBGL     '${lib.getLib libGL}/lib/libGL.so' \
       --set EBITENGINE_LIBGLESv2 '${lib.getLib libGL}/lib/libGLESv2.so'
+  ''
+  + lib.optionalString stdenv.hostPlatform.isDarwin ''
+    mkdir -p "$out/Applications/aaaaxy.app/Contents/MacOS"
+    mkdir -p "$out/Applications/aaaaxy.app/Contents/Resources"
+    mv "$out/bin/aaaaxy" "$out/Applications/aaaaxy.app/Contents/MacOS/aaaaxy"
+    mv aaaaxy.dat "$out/Applications/aaaaxy.app/Contents/Resources/"
+    mv aaaaxy.png "$out/Applications/aaaaxy.app/Contents/Resources/"
 
+    mkdir -p aaaaxy.iconset
+    for res in 16 32 128 256 512; do
+      convert assets/sprites/riser_small_up_0.png \
+        -filter Point -geometry ''${res}x''${res} \
+        -define png:bit-depth=8 \
+        -define png:color-type=6 \
+        -define png:format=png32 \
+        aaaaxy.iconset/icon_''${res}x''${res}.png
+    done
+    png2icns "$out/Applications/aaaaxy.app/Contents/Resources/icon.icns" aaaaxy.iconset/*.png
+
+    sh scripts/Info.plist.sh "${finalAttrs.version}" "$out/Applications/aaaaxy.app"
+  ''
+  + ''
     install -Dm755 'scripts/run-timedemo.sh' -t "$testing_infra/scripts/"
     install -Dm755 'scripts/regression-test-demo.sh' -t "$testing_infra/scripts/"
     install -Dm644 'assets/demos/benchmark.dem' -t "$testing_infra/assets/demos/"
@@ -137,6 +178,6 @@ buildGoModule (finalAttrs: {
       Luflosi
       philocalyst
     ];
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.unix;
   };
 })
