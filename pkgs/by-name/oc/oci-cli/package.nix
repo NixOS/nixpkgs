f1045fa965b41cbc3e5f0cd8ca8a63bdec6370aa
Vladimír Čunát <v@cunat.cz>
2026-05-20 09:50:18 +0200

    oci-cli: fixup fetch of jmespath-0.10.0
    As python3.pkgs.jmespath switched its fetcher in b9472284a2be14,
    this broke due to hash change. Let's be more explicit about the source.

--- pkgs/by-name/oc/oci-cli/package.nix
+++ pkgs/by-name/oc/oci-cli/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   fetchFromGitHub,
+  fetchPypi,
   python3,
   installShellFiles,
   nix-update-script,
@@ -13,10 +14,10 @@ let
     packageOverrides = self: super: {
       jmespath = super.jmespath.overridePythonAttrs (oldAttrs: rec {
         version = "0.10.0";
-        src = oldAttrs.src.override {
+        src = fetchPypi {
+          pname = "jmespath";
           inherit version;
           sha256 = "b85d0567b8666149a93172712e68920734333c0ce7e89b78b3e987f71e5ed4f9";
-          hash = "";
         };
         doCheck = false;
       });
