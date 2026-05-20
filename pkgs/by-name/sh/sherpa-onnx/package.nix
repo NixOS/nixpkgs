a4849d2ae2b546a9d2df3fc93d3633df27fafe7b
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-20 00:47:29 +0000

    sherpa-onnx: 1.12.38 -> 1.13.2

--- pkgs/by-name/sh/sherpa-onnx/package.nix
+++ pkgs/by-name/sh/sherpa-onnx/package.nix
@@ -111,13 +111,13 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "sherpa-onnx";
-  version = "1.12.38";
+  version = "1.13.2";
 
   src = fetchFromGitHub {
     owner = "k2-fsa";
     repo = "sherpa-onnx";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-lzcquKwP68KOem50b4X0+nZ3s2IthiYB76IxfvmJo54=";
+    hash = "sha256-3SbJd9PHOjy5km6uxFqVZljn39cs/o7RLxxUivqx5VM=";
   };
 
   outputs = [ "out" ] ++ lib.optionals pythonSupport [ "python" ];
