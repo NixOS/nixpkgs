b4ba888089dc04fec194cdd18a36f1bc028869bd
Gaetan Lepage <gaetan@glepage.com>
2026-05-20 23:09:21 +0000

    python3Packages.pytorch-lightning: fix hash

--- pkgs/development/python-modules/pytorch-lightning/default.nix
+++ pkgs/development/python-modules/pytorch-lightning/default.nix
@@ -31,7 +31,7 @@ buildPythonPackage (finalAttrs: {
     owner = "Lightning-AI";
     repo = "pytorch-lightning";
     tag = finalAttrs.version;
-    hash = "sha256-zOSV2X3yZy0uh1lJ2yNl/hHBvfIDcIrATHtiRwThsQA=";
+    hash = "sha256-Qysnr76OCO9eZzhQW5EoGT2hUAYGw/qY+j6dF8XCXC4=";
   };
 
   env.PACKAGE_NAME = "pytorch";
