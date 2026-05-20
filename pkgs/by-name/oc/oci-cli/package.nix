fe3b543b8c3cc6be474747f8726c8e8d78dbf887
Vladimír Čunát <v@cunat.cz>
2026-05-20 12:21:31 +0200

    oci-cli: fix strict-eval issues, hopefully
    error: attribute 'tag' missing
    changelog = "https://github.com/jmespath/jmespath.py/blob/${finalAttrs.src.tag}/CHANGELOG.rst";
    
    It's a bit hacky.  Feel free to improve.

--- pkgs/by-name/oc/oci-cli/package.nix
+++ pkgs/by-name/oc/oci-cli/package.nix
@@ -18,7 +18,7 @@ let
           pname = "jmespath";
           inherit version;
           sha256 = "b85d0567b8666149a93172712e68920734333c0ce7e89b78b3e987f71e5ed4f9";
-        };
+        } // { tag = version; };
         doCheck = false;
       });
     };
