b66d89d4dee5860ddadcf672cd6e004ccfcf6cc0
winston <hey@winston.sh>
2026-05-20 22:22:51 +0200

    gmobile: 0.7.0 -> 0.7.1
    (cherry picked from commit 916efb3f82cc90fa627d0cb1883ec05c7f88e0e9)

--- pkgs/by-name/gm/gmobile/package.nix
+++ pkgs/by-name/gm/gmobile/package.nix
@@ -17,7 +17,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gmobile";
-  version = "0.7.0";
+  version = "0.7.1";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
@@ -25,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "Phosh";
     repo = "gmobile";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-zAF/9FQwpb6xiKRqfhWI/3lBwiDOEDu+TNkIJpEdbYY=";
+    hash = "sha256-RXkH+48WzACgNcIROlSTSO4l/ujWVHJDG+Xtk4k7Rdw=";
   };
 
   nativeBuildInputs = [
