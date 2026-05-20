eca4745b977706f61c4976f524b53500460fc4b2
Nicolas Benes <nbenes.gh@xandea.de>
2026-05-20 08:52:50 +0200

    python3Packages.pynitrokey: 0.11.4 -> 0.12.2
    https://github.com/Nitrokey/pynitrokey/releases/tag/v0.12.0
    https://github.com/Nitrokey/pynitrokey/releases/tag/v0.12.1
    https://github.com/Nitrokey/pynitrokey/releases/tag/v0.12.2
    (cherry picked from commit b1382f9711f20ad4ee9b939b2f08f4ea74b489bc)

--- pkgs/development/python-modules/pynitrokey/default.nix
+++ pkgs/development/python-modules/pynitrokey/default.nix
@@ -25,7 +25,7 @@
 
 let
   pname = "pynitrokey";
-  version = "0.11.4";
+  version = "0.12.2";
   mainProgram = "nitropy";
 in
 
@@ -35,7 +35,7 @@ buildPythonPackage {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-MSqWgYuuU7uuYasxTTLRbrrAWQAwE4qQlEZIHiYB/78=";
+    hash = "sha256-fDLkYUVBdMFbHlnFSCUUlyJNP9OzRKOJM3ExFdzDEkU=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
