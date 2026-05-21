aca073fd2c44a12c875699c3ee0cd116c10ed027
majinghe <devops008@sina.com>
2026-05-21 11:35:57 +0000

    rustfs: init at 1.0.0-beta.3

---
+++ pkgs/by-name/ru/rustfs/package.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  installShellFiles,
+  openssl,
+  protobuf,
+  stdenv,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "rustfs";
+  version = "1.0.0-beta.3";
+
+  src = fetchFromGitHub {
+    owner = "rustfs";
+    repo = "rustfs";
+    rev = version;
+    hash = "sha256-9IVXAuXfFSNM+1CMfI4OsBLYjH9fcTdXsqtpjhz2Jm0=";
+  };
+
+  cargoHash = "sha256-i6Hwr6y59CjNCm6ogO6VjVgcCtm5DrKaRMjh2h3T8zY=";
+
+  nativeBuildInputs = [
+    pkg-config
+    installShellFiles
+    protobuf
+  ];
+
+  buildInputs = [
+    openssl
+  ];
+
+  PROTOC = "${protobuf}/bin/protoc";
+  RUSTFLAGS = "--cfg tokio_unstable";
+
+  cargoBuildFlags = [
+    "--package"
+    "rustfs"
+  ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "A distributed object storage system written in Rust";
+    homepage = "https://github.com/rustfs/rustfs";
+    license = licenses.asl20;
+    maintainers = [ maintainers.majinghe ];
+    mainProgram = "rustfs";
+    platforms = platforms.unix;
+  };
+}
