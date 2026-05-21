eb9d115a7a5cff1abc853076f2d7d158d8569427
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-21 23:10:34 +0000

    sunsetr: 0.11.1 -> 0.12.2

--- pkgs/by-name/su/sunsetr/package.nix
+++ pkgs/by-name/su/sunsetr/package.nix
@@ -6,16 +6,16 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sunsetr";
-  version = "0.11.1";
+  version = "0.12.2";
 
   src = fetchFromGitHub {
     owner = "psi4j";
     repo = "sunsetr";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-fV4yb8+a2AbXkvalL9TU9JmPGX04GNE60a8OnQKNuEU=";
+    hash = "sha256-M91eW8FKJDlR8pdvXcKte3OL3uJlpapShTUNpnA/Jvo=";
   };
 
-  cargoHash = "sha256-Cxdy/4lOf7nFoHaDeLekI0o7xT1ZUjV1yyKoGUcjNGs=";
+  cargoHash = "sha256-fFk/JPB6MGmYnwARMuKF1/fVZOf+W1C+YqQvuG/ub60=";
 
   checkFlags = [
     "--skip=config::tests::test_geo_toml_exists_before_config_creation"
