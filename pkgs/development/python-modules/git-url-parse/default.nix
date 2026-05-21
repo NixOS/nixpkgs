5aef59777692848f685960b28c734ce772eab1e9
SandaruKasa <sandarukasa@ya.ru>
2026-05-22 01:38:51 +0300

    python3Packages.git-url-parse: modernize and migrate to pyproject

--- pkgs/development/python-modules/git-url-parse/default.nix
+++ pkgs/development/python-modules/git-url-parse/default.nix
@@ -4,39 +4,49 @@
   fetchFromGitHub,
   pbr,
   pytestCheckHook,
+
   pytest-cov-stub,
+  setuptools,
 }:
 
-buildPythonPackage rec {
-  pname = "git-url-parse";
-  version = "1.2.2";
-  format = "setuptools";
+buildPythonPackage (
+  finalAttrs:
+  let
+    inherit (finalAttrs) version;
+  in
+  {
+    pname = "git-url-parse";
+    version = "1.2.2";
+    pyproject = true;
 
-  src = fetchFromGitHub {
-    owner = "coala";
-    repo = "git-url-parse";
-    rev = version;
-    hash = "sha256-+0V/C3wE02ppdDGn7iqdvmgsUwTR7THUakUilvkzoYg=";
-  };
+    src = fetchFromGitHub {
+      owner = "coala";
+      repo = "git-url-parse";
+      tag = version;
+      hash = "sha256-+0V/C3wE02ppdDGn7iqdvmgsUwTR7THUakUilvkzoYg=";
+    };
 
-  # Manually set version because prb wants to get it from the git
-  # upstream repository (and we are installing from tarball instead)
-  env.PBR_VERSION = version;
+    # Manually set version because prb wants to get it from the git
+    # upstream repository (and we are installing from tarball instead)
+    env.PBR_VERSION = version;
 
-  propagatedBuildInputs = [ pbr ];
+    build-system = [ setuptools ];
 
-  pythonImportsCheck = [ "giturlparse" ];
+    dependencies = [ pbr ];
 
-  nativeCheckInputs = [
-    pytestCheckHook
-    pytest-cov-stub
-  ];
+    pythonImportsCheck = [ "giturlparse" ];
 
-  meta = {
-    description = "Simple GIT URL parser";
-    homepage = "https://github.com/coala/git-url-parse";
-    changelog = "https://github.com/coala/git-url-parse/blob/${src.rev}/CHANGELOG.rst";
-    license = lib.licenses.mit;
-    maintainers = [ ];
-  };
-}
+    nativeCheckInputs = [
+      pytestCheckHook
+      pytest-cov-stub
+    ];
+
+    meta = {
+      description = "Simple GIT URL parser";
+      homepage = "https://github.com/coala/git-url-parse";
+      changelog = "https://github.com/coala/git-url-parse/blob/${version}/CHANGELOG.rst";
+      license = lib.licenses.mit;
+      maintainers = [ ];
+    };
+  }
+)
