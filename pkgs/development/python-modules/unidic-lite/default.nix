080cb71928f9a1f9a426471b35ec6509a94ef0fe
SandaruKasa <sandarukasa@ya.ru>
2026-05-22 02:30:48 +0300

    python3Packages.unidic-lite: modernize and migrate to pyproject

--- pkgs/development/python-modules/unidic-lite/default.nix
+++ pkgs/development/python-modules/unidic-lite/default.nix
@@ -2,20 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "unidic-lite";
   version = "1.0.8";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     sha256 = "0idj4yp0sl27ylr2wzkybbh0wj7c843lp7cljw5d1m7xv5r4b7fv";
   };
 
-  # no tests
-  doCheck = false;
+  build-system = [ setuptools ];
 
   pythonImportsCheck = [ "unidic_lite" ];
 
@@ -25,4 +25,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     teams = [ lib.teams.tts ];
   };
-}
+})
