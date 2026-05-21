fa751d4e7e45a3c18be8877ae07c398cde0f390d
SandaruKasa <sandarukasa@ya.ru>
2026-05-22 02:26:20 +0300

    python3Packages.unicodecsv: modernize and migrate to pyproject

--- pkgs/development/python-modules/unicodecsv/default.nix
+++ pkgs/development/python-modules/unicodecsv/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   version = "0.14.1";
-  format = "setuptools";
   pname = "unicodecsv";
+  pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     sha256 = "1z7pdwkr6lpsa7xbyvaly7pq3akflbnz8gq62829lr28gl1hi301";
   };
+
+  build-system = [ setuptools ];
 
   # ImportError: No module named runtests
   doCheck = false;
@@ -23,4 +26,4 @@ buildPythonPackage rec {
     maintainers = with lib.maintainers; [ koral ];
     license = lib.licenses.bsd2WithViews;
   };
-}
+})
