c2ea41b3e9b07051f77e58d9ce65ec922d585814
Acid Bong <acidbong@tilde.club>
2026-05-21 18:32:28 +0300

    python3Packages.fontforge-ref-sel-util: init at 0.1.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5928,6 +5928,8 @@ self: super: with self; {
     }
   );
 
+  fontforge-ref-sel-util = callPackage ../development/python-modules/fontforge-ref-sel-util { };
+
   fontmake = callPackage ../development/python-modules/fontmake { };
 
   fontmath = callPackage ../development/python-modules/fontmath { };
