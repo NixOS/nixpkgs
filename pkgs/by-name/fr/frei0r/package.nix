fe8b62c4c05500723d359de1d00f29ad0bca0c72
Christoph Heiss <christoph@c8h4.io>
2026-05-22 12:44:09 +0200

    frei0r: 2.5.1 -> 3.1.3
    Signed-off-by: Christoph Heiss <christoph@c8h4.io>

--- pkgs/by-name/fr/frei0r/package.nix
+++ pkgs/by-name/fr/frei0r/package.nix
@@ -7,20 +7,26 @@
   cmake,
   opencv,
   pkg-config,
+  gavl,
   cudaSupport ? config.cudaSupport,
   cudaPackages,
+  testers,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "frei0r-plugins";
-  version = "2.5.1";
+  version = "3.1.3";
 
   src = fetchFromGitHub {
     owner = "dyne";
     repo = "frei0r";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-3gUWvO5izOrJt+XwcNBNiLfu+iMqo4nuPbx++TYzao0=";
+    hash = "sha256-D00sxSTnv4oqxhKYKayJPs5uWYW4HosEp2StuCVmGFY=";
   };
+
+  __structuredAttrs = true;
+  strictDeps = true;
+  enableParallelBuilding = true;
 
   nativeBuildInputs = [
     cmake
@@ -30,10 +36,18 @@ stdenv.mkDerivation (finalAttrs: {
     cairo
     opencv
   ]
+  ++ lib.optional (!stdenv.hostPlatform.isDarwin) gavl
   ++ lib.optionals cudaSupport [
     cudaPackages.cuda_cudart
     cudaPackages.cuda_nvcc
   ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "WITHOUT_GAVL" stdenv.hostPlatform.isDarwin)
+    (lib.cmakeFeature "FREI0R_VERSION" finalAttrs.version)
+  ];
+
+  doCheck = true;
 
   postInstall = lib.optionalString stdenv.hostPlatform.isDarwin ''
     for f in $out/lib/frei0r-1/*.so* ; do
@@ -41,11 +55,17 @@ stdenv.mkDerivation (finalAttrs: {
     done
   '';
 
+  passthru.tests.pkg-config = testers.hasPkgConfigModules {
+    package = finalAttrs.finalPackage;
+    versionCheck = true;
+  };
+
   meta = {
     homepage = "https://frei0r.dyne.org";
     description = "Minimalist, cross-platform, shared video plugins";
     license = lib.licenses.gpl2Plus;
-    maintainers = [ ];
+    maintainers = [ lib.maintainers.christoph-heiss ];
     platforms = lib.platforms.unix;
+    pkgConfigModules = [ "frei0r" ];
   };
 })
