d3480017f67909c0d0984cb0ab32e9abb4148770
Yuchen He <yuchenhe126@gmail.com>
2026-05-22 16:16:40 +0200

    explo: update to 1.0.1
    Co-authored-by: arunoruto <mirza.arnaut45@gmail.com>

--- pkgs/by-name/ex/explo/package.nix
+++ pkgs/by-name/ex/explo/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   buildGoModule,
+  buildNpmPackage,
   fetchFromGitHub,
   versionCheckHook,
   nix-update-script,
@@ -8,30 +9,78 @@
 
 buildGoModule (finalAttrs: {
   pname = "explo";
-  version = "0.11.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "LumePart";
     repo = "Explo";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-uBJe6bSStVIoKw9Aa/vtojlMOxo7c7RzfJTlZyRdvEk=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-EKYn2FE99H44Apxx8LTV794CRtdJR82o32oTh3qg5+w=";
   };
 
-  vendorHash = "sha256-Npi6rwopOADlbbi0RdalfRtiH2tdgXWWrIhYcYKSRZg=";
+  webui = buildNpmPackage {
+    inherit (finalAttrs)
+      pname
+      version
+      src
+      meta
+      ;
+
+    sourceRoot = "${finalAttrs.src.name}/src/web/frontend";
+
+    npmDepsHash = "sha256-MIZqj7eWKOBMX9iw7P8Xd/nRFqi+F9iLBvgTVnvt6Mo=";
+
+    buildPhase = ''
+      runHook preBuild
+
+      npx vite build --outDir dist
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp -r dist/* $out
+
+      runHook postInstall
+    '';
+  };
+
+  vendorHash = "sha256-DUs3rkDtna6vLjFwNzVdfeUeo3UTGbMf2buXAcJLJwE=";
+
+  preBuild = ''
+    mkdir -p src/web/dist
+    cp -r ${finalAttrs.webui}/* src/web/dist
+  '';
 
   postInstall = ''
     mv $out/bin/main $out/bin/explo
+    mkdir -p $out/share/explo
+    cp src/downloader/youtube_music/search_ytmusic.py $out/share/explo/
   '';
 
   nativeInstallCheckInputs = [ versionCheckHook ];
 
-  passthru.updateScript = nix-update-script { };
+  __structuredAttrs = true;
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--subpackage"
+      "webui"
+    ];
+  };
 
   meta = {
     description = "Spotify's \"Discover Weekly\" for self-hosted music systems";
     homepage = "https://github.com/LumePart/Explo/";
+    changelog = "https://github.com/${finalAttrs.src.owner}/${finalAttrs.src.repo}/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ lilacious ];
+    maintainers = with lib.maintainers; [
+      arunoruto
+      lilacious
+    ];
     mainProgram = "explo";
   };
 })
