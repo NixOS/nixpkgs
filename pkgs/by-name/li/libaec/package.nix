010411ad771c834a5b627b4b74d1581f950bf745
whispers <whispersofthedawn@duck.com>
2026-05-22 15:37:53 -0400

    libaec: switch source to github
    The old source, https://gitlab.dkrz.de/k202009/libaec, is now a redirect
    to https://gitlab.dkrz.de/dkrz-sw/libaec, and the top of this README
    links to https://github.com/Deutsches-Klimarechenzentrum/libaec as the
    source before the GitLab. Additionally, pull requests are being made on
    GitHub (https://github.com/Deutsches-Klimarechenzentrum/libaec/pulls)
    and commits seem to be mirrored to GitLab. Thus, it seems like the
    GitHub is now the primary development repository, so we fetch the source
    from there instead.
    
    (cherry picked from commit 5e77f648880ea9bd450b073c16b076f097bf27c4)

--- pkgs/by-name/li/libaec/package.nix
+++ pkgs/by-name/li/libaec/package.nix
@@ -1,7 +1,7 @@
 {
   lib,
   stdenv,
-  fetchFromGitLab,
+  fetchFromGitHub,
   cmake,
 }:
 
@@ -9,9 +9,8 @@ stdenv.mkDerivation rec {
   pname = "libaec";
   version = "1.1.6";
 
-  src = fetchFromGitLab {
-    domain = "gitlab.dkrz.de";
-    owner = "k202009";
+  src = fetchFromGitHub {
+    owner = "Deutsches-Klimarechenzentrum";
     repo = "libaec";
     rev = "v${version}";
     sha256 = "sha256-cxDP+JNwokxgzH9hO2zw+rIcz8XG7E8ujbAbWpgUEW8=";
@@ -24,7 +23,8 @@ stdenv.mkDerivation rec {
   doCheck = true;
 
   meta = {
-    homepage = "https://gitlab.dkrz.de/k202009/libaec";
+    changelog = "https://github.com/Deutsches-Klimarechenzentrum/libaec/blob/v${version}/CHANGELOG.md";
+    homepage = "https://github.com/Deutsches-Klimarechenzentrum/libaec";
     description = "Adaptive Entropy Coding library";
     license = lib.licenses.bsd2;
     maintainers = with lib.maintainers; [ tbenst ];
