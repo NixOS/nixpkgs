5360fcd3dc292ff81b24b43b086ad7d8d8ba5104
kyehn <kyehn@users.noreply.github.com>
2026-05-22 00:27:35 +0000

    wayback-machine-archiver: 1.9.1 -> 3.5.0

--- pkgs/by-name/wa/wayback-machine-archiver/package.nix
+++ pkgs/by-name/wa/wayback-machine-archiver/package.nix
@@ -6,32 +6,30 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "wayback-machine-archiver";
-  version = "1.9.1";
+  version = "3.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "agude";
     repo = "wayback-machine-archiver";
-    rev = "v${finalAttrs.version}";
-    sha256 = "0dnnqx507gpj8wsx6f2ivfmha969ydayiqsvxh23p9qcixw9257x";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-gowktQvJg09iD+6KhP/EpYXOGMXXWy4nAaxvEc2sh3o=";
   };
 
   build-system = with python3.pkgs; [
     setuptools
-    pypandoc
   ];
 
-  dependencies = with python3.pkgs; [ requests ];
+  dependencies = with python3.pkgs; [
+    requests
+    python-dotenv
+    urllib3
+  ];
 
   nativeCheckInputs = with python3.pkgs; [
     pytestCheckHook
     requests-mock
   ];
-
-  postPatch = ''
-    substituteInPlace setup.py \
-      --replace-fail \"pytest-runner\", ""
-  '';
 
   pythonImportsCheck = [ "wayback_machine_archiver" ];
 
