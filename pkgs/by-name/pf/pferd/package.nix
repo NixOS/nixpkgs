5949f728041f18149cc8008c16a9ff2d67314bea
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-22 05:17:12 +0000

    pferd: 3.9.0 -> 3.9.2

--- pkgs/by-name/pf/pferd/package.nix
+++ pkgs/by-name/pf/pferd/package.nix
@@ -5,14 +5,14 @@
 }:
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "pferd";
-  version = "3.9.0";
+  version = "3.9.2";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Garmelon";
     repo = "PFERD";
     tag = "v${finalAttrs.version}";
-    sha256 = "sha256-bJU7LytxWTb/CRODniDySXRrVyli9FI/yxQqEs/Ar2k=";
+    sha256 = "sha256-q1IyuANj47M3KR8qQXASf3WOpFgjNGD/gFZn1l6grTk=";
   };
 
   nativeBuildInputs = with python3Packages; [
