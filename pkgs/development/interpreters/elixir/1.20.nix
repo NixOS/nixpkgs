f7df550fcbe050d9ad56f523417d8bdf6eac5d11
Philip Munksgaard <philip@munksgaard.me>
2026-05-22 11:44:07 +0200

    beamPackages.elixir_1_20: 1.20.0-rc.5 -> 1.20.0-rc.6

--- pkgs/development/interpreters/elixir/1.20.nix
+++ pkgs/development/interpreters/elixir/1.20.nix
@@ -1,7 +1,7 @@
 import ./generic-builder.nix {
-  version = "1.20.0-rc.5";
-  hash = "sha256-D1lYpwD/nb9GyCSW4W9mVliqULb7Hs641OdtwPDfsME=";
-  # https://hexdocs.pm/elixir/1.20.0-rc.5/compatibility-and-deprecations.html#between-elixir-and-erlang-otp
+  version = "1.20.0-rc.6";
+  hash = "sha256-U3zBeZ4U44jXwYJva2neb3Ll1dDpxvLSIR0Tg1HP33U=";
+  # https://hexdocs.pm/elixir/1.20.0-rc.6/compatibility-and-deprecations.html#between-elixir-and-erlang-otp
   minimumOTPVersion = "27";
   maximumOTPVersion = "29";
 }
