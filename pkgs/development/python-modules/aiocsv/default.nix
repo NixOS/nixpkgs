09be9bde87024f43fc893c4f0a2f6a5a8f16fa7d
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-23 01:15:34 +0200

    python3Pakcages.aiocsv: modernize

--- pkgs/development/python-modules/aiocsv/default.nix
+++ pkgs/development/python-modules/aiocsv/default.nix
@@ -10,7 +10,7 @@
   typing-extensions,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aiocsv";
   version = "1.4.1";
   pyproject = true;
@@ -18,7 +18,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "MKuranowski";
     repo = "aiocsv";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-WENNtQKvpUuoYai6r8nTRamwCOloVA42YoAA3JGK9B8=";
   };
 
@@ -49,7 +49,8 @@ buildPythonPackage rec {
   meta = {
     description = "Library for for asynchronous CSV reading/writing";
     homepage = "https://github.com/MKuranowski/aiocsv";
-    license = with lib.licenses; [ mit ];
+    changelog = "https://github.com/MKuranowski/aiocsv/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
