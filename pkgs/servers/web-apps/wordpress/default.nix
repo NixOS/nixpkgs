cecf6b0c7c4a2c6ddc0bb0b1e06a8925a704f639
Bart Oostveen <bart@bartoostveen.nl>
2026-05-22 21:17:25 +0200

    wordpress_6_7: drop
    (cherry picked from commit 48c8a7d9513ac0306b3464df6952f2858f015504)

--- pkgs/servers/web-apps/wordpress/default.nix
+++ pkgs/servers/web-apps/wordpress/default.nix
@@ -1,10 +1,6 @@
 { callPackage }:
 builtins.mapAttrs (_: callPackage ./generic.nix) rec {
   wordpress = wordpress_7_0;
-  wordpress_6_7 = {
-    version = "6.7.5";
-    hash = "sha256-IS9B6kwjWLYLybmWG2Ym5+JAgFExMqhcEQsJJF2puXg=";
-  };
   wordpress_6_8 = {
     version = "6.8.5";
     hash = "sha256-N/WVUQxI0W3t4L+lr6KcuK8S2/Dj00WyXElMFfjIHYE=";
