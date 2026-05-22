ace2fd6664abc20b94147f87abd2f29de2db3d85
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-22 05:53:05 +0000

    python3Packages.pyrfxtrx: 0.32.0 -> 0.34.0

--- pkgs/development/python-modules/pyrfxtrx/default.nix
+++ pkgs/development/python-modules/pyrfxtrx/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pyrfxtrx";
-  version = "0.32.0";
+  version = "0.34.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Danielhiversen";
     repo = "pyRFXtrx";
     tag = version;
-    hash = "sha256-6gD6ch7DyaD9nCY/VfyJHmV4gEDPsDfVKjNaNedmVVE=";
+    hash = "sha256-JMAQhJP7U9xSoax3EvvlP5yrTWcteyoNjLKOxgi3Dvg=";
   };
 
   build-system = [ setuptools ];
