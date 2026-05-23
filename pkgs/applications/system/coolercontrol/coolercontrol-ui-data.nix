5c92ff402f558c9fc1e091ef777fcaa98ab9d379
Guy Boldon <gb@guyboldon.com>
2026-05-23 17:39:47 +0200

    coolercontrol: 4.3.0 -> 4.3.1

--- pkgs/applications/system/coolercontrol/coolercontrol-ui-data.nix
+++ pkgs/applications/system/coolercontrol/coolercontrol-ui-data.nix
@@ -12,7 +12,7 @@ buildNpmPackage {
   sourceRoot = "${src.name}/coolercontrol-ui";
 
   npmDepsFetcherVersion = 2;
-  npmDepsHash = "sha256-fWsksBQCwHHWYE82NG0Vf/f+Hk02YMCUaGMHFGhGx2U=";
+  npmDepsHash = "sha256-zolbx5ROiFzNhPGcOnJjEiY3W2IXI24wLKPj3wRSLXU=";
 
   postBuild = ''
     cp -r dist $out
