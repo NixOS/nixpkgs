b7ec96a7eaefcd9d2e61b7fffbdfc96f65cab692
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-23 19:20:21 -0400

    seafile: init module

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1543,6 +1543,7 @@ in
   scx-loader = runTest ./scx/loader.nix;
   sddm = import ./sddm.nix { inherit runTest; };
   sdl3 = runTest ./sdl3.nix;
+  seafile = runTest ./seafile.nix;
   searx = runTest ./searx.nix;
   seatd = runTest ./seatd.nix;
   seerr = runTest ./seerr.nix;
