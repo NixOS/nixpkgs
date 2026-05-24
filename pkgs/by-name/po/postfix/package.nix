813c022783494b95b836ab1bfa27ea36485d3929
Robert Schütz <nix@dotlambda.de>
2026-05-24 09:52:39 -0700

    postfix: 3.10.8 -> 3.10.10
    Changelog:
    https://www.postfix.org/announcements/postfix-3.11.2.html
    https://www.postfix.org/announcements/postfix-3.11.3.html

--- pkgs/by-name/po/postfix/package.nix
+++ pkgs/by-name/po/postfix/package.nix
@@ -74,11 +74,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "postfix";
-  version = "3.10.8";
+  version = "3.10.10";
 
   src = fetchurl {
     url = "http://ftp.porcupine.org/mirrors/postfix-release/official/postfix-${finalAttrs.version}.tar.gz";
-    hash = "sha256-MdSz64CT2CO1oVH1cXGf98BGJXG8leZEDYfKUlv7CWw=";
+    hash = "sha256-O3GX4AqY9/5GJM9RoArYBWdCVu/2JOZ4UpzPvyx3B6Q=";
   };
 
   nativeBuildInputs = [
