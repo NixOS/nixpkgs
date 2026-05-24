513b18602ee06379c4a8cfdd9106dfe95c4e4a9f
kpbaks <kristoffer.pbs@tuta.io>
2026-05-24 10:27:18 +0200

    gonzo: 0.3.0 -> 0.4.2

--- pkgs/by-name/go/gonzo/package.nix
+++ pkgs/by-name/go/gonzo/package.nix
@@ -1,33 +1,67 @@
 {
+  stdenv,
   lib,
   go,
   buildGoModule,
+  buildNpmPackage,
   fetchFromGitHub,
+  installShellFiles,
   nix-update-script,
   versionCheckHook,
 }:
-
-buildGoModule (finalAttrs: {
-  pname = "gonzo";
-  version = "0.3.0";
+let
+  version = "0.4.2";
 
   src = fetchFromGitHub {
     owner = "control-theory";
     repo = "gonzo";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-fLTZwxZcwh7y83GfuOvG0R6vm/TgKIJjBWbt39mV6H8=";
+    tag = "v${version}";
+    hash = "sha256-N38gBEe2VZiUtnffzROv9GAwXp0lMyaNj/ywtNlbmjY=";
   };
 
-  vendorHash = "sha256-uYQlZvsLUOmy7P/goNpwTGQrGFMW6LSILC6VjGbNrjI=";
+  webui = buildNpmPackage {
+    pname = "gonzo-ui";
+    inherit version src;
+    sourceRoot = "${src.name}/web";
+    npmDepsHash = "sha256-eRI5m1Bz1tcA5CYsLbe2K21EXO3ycRKcAngNm2jIvc0=";
+
+    installPhase = ''
+      mkdir -p $out/share
+      mv dist $out/share/
+    '';
+  };
+in
+
+buildGoModule (finalAttrs: {
+  pname = "gonzo";
+  inherit version src;
+
+  __structuredAttrs = true;
+
+  vendorHash = "sha256-8ATB57qiEc6ANBrt1mbqtsFQlIO9p3b4qdZX2ua7EMY=";
 
   ldflags = [
     "-s"
-    "-w"
     "-X=main.version=${finalAttrs.version}"
     "-X=main.commit=${finalAttrs.src.tag}"
     "-X=main.buildTime=1970-01-01T00:00:00Z"
     "-X=main.goVersion=${lib.getVersion go}"
   ];
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  preBuild = ''
+    # Ensure web/dist exists for go:embed
+    mkdir -p web/dist
+    cp -r ${webui}/share/dist/* web/dist
+  '';
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd gonzo \
+      --bash <($out/bin/gonzo completion bash) \
+      --fish <($out/bin/gonzo completion fish) \
+      --zsh <($out/bin/gonzo completion zsh)
+  '';
 
   passthru.updateScript = nix-update-script { };
 
