cc76ff1a44787e634d958192c421d0ab27edbdb0
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-24 04:25:28 +0000

    laravel: 5.25.3 -> 5.28.1

--- pkgs/by-name/la/laravel/package.nix
+++ pkgs/by-name/la/laravel/package.nix
@@ -7,19 +7,19 @@
 }:
 php.buildComposerProject2 (finalAttrs: {
   pname = "laravel";
-  version = "5.25.3";
+  version = "5.28.1";
 
   src = fetchFromGitHub {
     owner = "laravel";
     repo = "installer";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-WpBpKCXvfcMXb7Zxvz7fdxVMaaseH0kxr+Fh/vYVb+Y=";
+    hash = "sha256-oRPs/BhwRP9ZOFONr++JeTAZ+qszyHdjis96QhMSSoY=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   composerLock = ./composer.lock;
-  vendorHash = "sha256-zm2Xh15l/i7jDL3OasIqLUAWOLM71sQvA4LuszDP4EY=";
+  vendorHash = "sha256-sDVAudDjFaydDGCAm/uZA/9oMyGGiiykxMSngOR0CdY=";
 
   # Adding npm (nodejs) and php composer to path
   postInstall = ''
