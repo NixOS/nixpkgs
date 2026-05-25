1cb19ef4c8255bc87010a2a2cdd8a02117c84df5
yaaaarn <30006414+yaaaarn@users.noreply.github.com>
2026-05-25 14:00:13 +0200

    maintainers: add yarn

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -30263,6 +30263,12 @@
     github = "yarektyshchenko";
     githubId = 185304;
   };
+  yarn = {
+    name = "yarncat";
+    github = "yaaaarn";
+    githubId = 30006414;
+    email = "nix@yarncat.moe";
+  };
   yarny = {
     github = "Yarny0";
     githubId = 41838844;
