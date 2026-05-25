50992337cf6233dff78b1c59aed74291948a0e4d
kyehn <kyehn@users.noreply.github.com>
2026-05-25 06:25:29 +0000

    clashtui: 0.2.3 -> 0.3.0

--- pkgs/by-name/cl/clashtui/package.nix
+++ pkgs/by-name/cl/clashtui/package.nix
@@ -2,42 +2,26 @@
   lib,
   fetchFromGitHub,
   rustPlatform,
-  versionCheckHook,
   nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "clashtui";
-  version = "0.2.3";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "JohanChane";
     repo = "clashtui";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-2iQVYZrqo55EO0ZGn6ktP/3Py5v+LiVgrSYTtaxYXyQ=";
+    hash = "sha256-ZYGGlTBox7ChdBwvMWTHqys/WQ0heBXpXa6XIrurp60=";
   };
 
-  sourceRoot = "${finalAttrs.src.name}/clashtui";
-
-  cargoHash = "sha256-8oDnumyn0Ry1AIWNLO2+1HSPsxkVLRLItgEVEXqSRFI=";
-
-  cargoBuildFlags = [ "--all-features" ];
-
-  checkFlags = [
-    # need fhs
-    "--skip=utils::config::test::test_save_and_load"
-  ];
-
-  doInstallCheck = true;
-
-  nativeInstallCheckInputs = [
-    versionCheckHook
-  ];
+  cargoHash = "sha256-C71OkwEJSYoBPqDP6QJBWvOZlVxOlg3yBDAq9FRmBmo=";
 
   passthru.updateScript = nix-update-script { };
 
   meta = {
-    description = "Mihomo (Clash.Meta) TUI Client";
+    description = "Mihomo (Clash.Meta) / sing-box TUI Client";
     homepage = "https://github.com/JohanChane/clashtui";
     changelog = "https://github.com/JohanChane/clashtui/releases/tag/v${finalAttrs.version}";
     mainProgram = "clashtui";
