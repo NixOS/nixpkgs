97dc0a920b756d88ce0d3d37ed8376eb8bdd2e25
Robert Schütz <nix@dotlambda.de>
2026-05-24 21:34:13 -0700

    at-spi2-core: 2.60.1 -> 2.60.4
    (cherry picked from commit eb99a699438577d486d66e3d860c3ba67facbb1b)

--- pkgs/by-name/at/at-spi2-core/package.nix
+++ pkgs/by-name/at/at-spi2-core/package.nix
@@ -29,7 +29,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "at-spi2-core";
-  version = "2.60.1";
+  version = "2.60.4";
 
   outputs = [
     "out"
@@ -39,7 +39,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "mirror://gnome/sources/at-spi2-core/${lib.versions.majorMinor finalAttrs.version}/at-spi2-core-${finalAttrs.version}.tar.xz";
-    hash = "sha256-+ZuH48FnT1+8QXzJwdniYcDymqsFUK1jaYBQMdEvaFI=";
+    hash = "sha256-Gh9bqYBZF/QfxqpoI9z4h6KR1gekJ+LVr7a136ZQcMc=";
   };
 
   nativeBuildInputs = [
