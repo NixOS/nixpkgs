880289462edf18d93c7b52d97ed9eac2708c7fb5
Robert Schütz <nix@dotlambda.de>
2026-05-24 19:24:27 -0700

    libadwaita: 1.9.0 -> 1.9.1
    Diff: https://gitlab.gnome.org/GNOME/libadwaita/-/compare/1.9.0...1.9.1
    
    Changelog: https://gitlab.gnome.org/GNOME/libadwaita/-/blob/1.9.1/NEWS
    (cherry picked from commit d2a5f7f0cafe93df429883d62a6365ec7f4db78c)
    (cherry picked from commit 27504c8f0df600820cc88797834ab8c8a1011309)

--- pkgs/by-name/li/libadwaita/package.nix
+++ pkgs/by-name/li/libadwaita/package.nix
@@ -23,7 +23,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libadwaita";
-  version = "1.9.0";
+  version = "1.9.1";
 
   outputs = [
     "out"
@@ -37,7 +37,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "GNOME";
     repo = "libadwaita";
     tag = finalAttrs.version;
-    hash = "sha256-JAKP8CjLCKGZvHoB26ih/J3xAru4wiVf/ObG0L8r4pY=";
+    hash = "sha256-Oy3WcsymNbbmAacm5hEOrorI1wKXjSp063mh4jCJRAE=";
   };
 
   depsBuildBuild = [
