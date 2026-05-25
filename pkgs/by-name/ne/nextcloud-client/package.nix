bb714a4e626642ed80a98d20854dd1b95638b9a7
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2026-05-25 10:31:11 -0400

    nextcloud-client: 4.0.8 -> 33.0.5
    (cherry picked from commit ddc487524d370636ff72f7b23a684aee1f3c6ff8)

--- pkgs/by-name/ne/nextcloud-client/package.nix
+++ pkgs/by-name/ne/nextcloud-client/package.nix
@@ -7,12 +7,13 @@
   cmake,
   inotify-tools,
   kdePackages,
+  kdsingleapplication,
   libcloudproviders,
   libp11,
   librsvg,
   libsecret,
   openssl,
-  pcre,
+  pcre2,
   pkg-config,
   sphinx,
   sqlite,
@@ -22,7 +23,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "nextcloud-client";
-  version = "4.0.8";
+  version = "33.0.5";
 
   outputs = [
     "out"
@@ -33,7 +34,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "nextcloud-releases";
     repo = "desktop";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-EKJj2z3CkWrNL2idBHu5jHuPXQXKwjEjUAgo6OOY/08=";
+    hash = "sha256-YRaND3JdZQZGtuNfthliyqcPvpC742h4C6l3HBbYorI=";
   };
 
   patches = [
@@ -62,11 +63,12 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     inotify-tools
     kdePackages.kio
+    kdsingleapplication
     libcloudproviders
     libp11
     libsecret
     openssl
-    pcre
+    pcre2
     qt6Packages.qt5compat
     qt6Packages.qtbase
     qt6Packages.qtkeychain
