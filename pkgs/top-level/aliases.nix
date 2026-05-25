8b55b8659d9bae73909ca6bc9c5c23762ebe600a
2kybe3 <kybe@kybe.xyz>
2026-05-25 09:25:11 +0200

    paper-gtk-theme: drop
    old repo: https://github.com/snw/paper-gtk-them

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -1732,6 +1732,7 @@ mapAliases {
   pal = throw "pal has been removed, as it was broken"; # Added 2025-08-25
   pam_pgsql = pam-pgsql; # Added 2025-12-16
   pangolin = throw "pangolin has been removed due to lack of maintenance"; # Added 2025-11-17
+  paper-gtk-theme = throw "'paper-gtk-theme' is unmaintained and has been removed"; # Added 2026-05-25
   paper-plane = throw "'paper-plane' has been removed as it is unmaintained upstream and depends on vulnerable tdlib version."; # Added 2026-02-26
   paperless-ng = throw "'paperless-ng' has been renamed to/replaced by 'paperless-ngx'"; # Converted to throw 2025-10-27
   parcellite = throw "'parcellite' was remove due to lack of maintenance and relying on gtk2"; # Added 2025-10-03
