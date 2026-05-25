9f53bbe1fd85fc93dccc97fe0a71876fb5f5f0b5
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-25 22:32:09 +0200

    python3Packages.azure-mgmt-netapp: migrate to finalAttrs

--- pkgs/development/python-modules/azure-mgmt-netapp/default.nix
+++ pkgs/development/python-modules/azure-mgmt-netapp/default.nix
@@ -8,14 +8,14 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "azure-mgmt-netapp";
   version = "16.0.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "azure_mgmt_netapp";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-/tcO1+pIMcB2e+T1f2ClHxLjSzqv0PherTPMgI12/BY=";
   };
 
@@ -38,8 +38,8 @@ buildPythonPackage rec {
   meta = {
     description = "Microsoft Azure NetApp Files Management Client Library for Python";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
-    changelog = "https://github.com/Azure/azure-sdk-for-python/blob/azure-mgmt-netapp_${version}/sdk/netapp/azure-mgmt-netapp/CHANGELOG.md";
+    changelog = "https://github.com/Azure/azure-sdk-for-python/blob/azure-mgmt-netapp_${finalAttrs.version}/sdk/netapp/azure-mgmt-netapp/CHANGELOG.md";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
