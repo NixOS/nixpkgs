c005ec4201c12411e8f6daa3933dac90e717fc28
SkohTV <contact@skoh.dev>
2026-05-25 11:23:06 -0400

    python3Packages.voluptuous-stubs: modernize

--- pkgs/development/python-modules/voluptuous-stubs/default.nix
+++ pkgs/development/python-modules/voluptuous-stubs/default.nix
@@ -6,13 +6,13 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "voluptuous-stubs";
   version = "0.1.1";
   pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-cPscCIJC8g4RAjJStWSM13+DH2ks2RDI+XE8wTXPjMg=";
   };
 
@@ -30,4 +30,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ hexa ];
   };
-}
+})
