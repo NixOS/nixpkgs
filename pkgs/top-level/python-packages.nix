828b2a052bb194119a8e5f7dd0ad523c3ad0d108
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-24 22:45:56 -0400

    python3Packages.jambo: init at 0.1.7

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8363,6 +8363,8 @@ self: super: with self; {
 
   jalali-core = callPackage ../development/python-modules/jalali-core { };
 
+  jambo = callPackage ../development/python-modules/jambo { };
+
   jamo = callPackage ../development/python-modules/jamo { };
 
   janus = callPackage ../development/python-modules/janus { };
