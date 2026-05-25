74f68ee45932320adc75e44f71740d6a6b6bd646
Connor Alecks <git@econnah.uk>
2026-05-25 16:58:44 +0200

    maintainers: add eConnah

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7645,6 +7645,12 @@
     github = "eclairevoyant";
     name = "éclairevoyant";
   };
+  eConnah = {
+    email = "git@econnah.uk";
+    github = "eConnah";
+    githubId = 63052937;
+    name = "Connor Alecks";
+  };
   edanaher = {
     email = "nixos@edanaher.net";
     github = "edanaher";
