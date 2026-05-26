f833508a30b578e5df753d773da81b5e4cbee60c
Alexandre Esteves <alexfmpe@proton.me>
2026-05-26 20:54:03 +0100

    llvmPackages: llvmPackages_21 -> llvmPackages_22

--- pkgs/stdenv/darwin/default.nix
+++ pkgs/stdenv/darwin/default.nix
@@ -28,7 +28,7 @@ let
   inherit (localSystem) system;
   genericStdenv = import ../generic { defaultConfig = config; };
 
-  llvmVersion = "21"; # This needs to be updated when the default LLVM version is changed.
+  llvmVersion = "22"; # This needs to be updated when the default LLVM version is changed.
   sdkMajorVersion = lib.versions.major localSystem.darwinSdkVersion;
 
   commonImpureHostDeps = [
