32a2011f65668d7b3be4e66ae49dcaba2bd81bed
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-26 19:26:59 +0000

    phpstan: 2.1.55 -> 2.1.56

--- pkgs/by-name/ph/phpstan/package.nix
+++ pkgs/by-name/ph/phpstan/package.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "phpstan";
-  version = "2.1.55";
+  version = "2.1.56";
 
   src = fetchFromGitHub {
     owner = "phpstan";
     repo = "phpstan";
     tag = finalAttrs.version;
-    hash = "sha256-Mh0pHxn+SyQ5qQiesljBtiDycLuarLBLrKSu9gF7oaM=";
+    hash = "sha256-Ffn6rJhTKjXEjwWZ1tnSaUBsmLyLeIp5EQnGZcvxba8=";
   };
 
   nativeBuildInputs = [
