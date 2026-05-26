b622c42ead8635486e66e326fa0239eac2e8450b
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-26 05:51:28 +0000

    python3Packages.color-parser-py: 0.1.6 -> 0.1.7

--- pkgs/development/python-modules/color-parser-py/default.nix
+++ pkgs/development/python-modules/color-parser-py/default.nix
@@ -8,19 +8,19 @@
 
 buildPythonPackage rec {
   pname = "color-parser-py";
-  version = "0.1.6";
+  version = "0.1.7";
   pyproject = true;
 
   # PyPI has Cargo.lock
   src = fetchPypi {
     pname = "color_parser_py";
     inherit version;
-    hash = "sha256-m1qhVAwQNtCwz+DLSAdfKhzkohMLMjvPHxynKhlJfN8=";
+    hash = "sha256-C3Q9vaOa/SE0PtQu5Gw/sk1JMRIlhgbA5VTW+2aC5dU=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit pname version src;
-    hash = "sha256-tKXA6sd5gLCJUaqxzFcZ3lePK41Wk2TbLp0HXBacOyo=";
+    hash = "sha256-nyl0Nmf0DNLH3j2XrTTO1u3erBCbRyp/xO0w/USjDHE=";
   };
 
   nativeBuildInputs = [
