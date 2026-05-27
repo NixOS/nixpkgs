b1fde87ed6d8074bd6952d11eb4211f5214e1dd4
Salva <salva.hg01@gmail.com>
2026-05-26 21:01:32 -0600

    cosmic-panel: 1.0.13 -> 1.0.14
    Diff: https://github.com/pop-os/cosmic-panel/compare/epoch-1.0.13...epoch-1.0.14

--- pkgs/by-name/co/cosmic-panel/package.nix
+++ pkgs/by-name/co/cosmic-panel/package.nix
@@ -11,17 +11,17 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cosmic-panel";
-  version = "1.0.13";
+  version = "1.0.14";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-panel";
     tag = "epoch-${finalAttrs.version}";
-    hash = "sha256-1yry1cA/friBzyE6LH3G6rd8ZzBPt5aTGWVzATO839Y=";
+    hash = "sha256-iUjrm/ZnDV2y+B7SLFJ59KRPJkkGKLKwW9qHUsfo8aw=";
   };
 
-  cargoHash = "sha256-vU++jreNHCRjxyyoj53LB2Sa7cdVUWGAvECcqoDofF8=";
+  cargoHash = "sha256-nbfqqca0yU67GmjWDH4d7yNpEbEuDLT7K5qBbgXdlDI=";
 
   separateDebugInfo = true;
   __structuredAttrs = true;
