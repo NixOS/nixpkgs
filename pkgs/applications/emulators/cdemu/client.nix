4ebd45c2ab61a81fe9e7e46d63c7102e77987380
Guy Chronister <guychronister@outlook.com>
2026-05-27 16:39:31 -0500

    cdemu: inline common-drv-attrs and refactor
    (cherry picked from commit aa1a4a413eecb93daee0e192e73568499af9d0aa)

--- pkgs/applications/emulators/cdemu/client.nix
+++ pkgs/applications/emulators/cdemu/client.nix
@@ -1,25 +1,21 @@
 {
-  callPackage,
   python3Packages,
   cmake,
   pkg-config,
   intltool,
   wrapGAppsNoGuiHook,
   gobject-introspection,
+  fetchurl,
+  lib,
 }:
-python3Packages.buildPythonApplication {
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "cdemu-client";
+  version = "3.2.5";
 
-  inherit
-    (callPackage ./common-drv-attrs.nix {
-      version = "3.2.5";
-      pname = "cdemu-client";
-      hash = "sha256-py2F61v8vO0BCM18GCflAiD48deZjbMM6wqoCDZsOd8=";
-    })
-    pname
-    version
-    src
-    meta
-    ;
+  src = fetchurl {
+    url = "mirror://sourceforge/cdemu/cdemu-client-${finalAttrs.version}.tar.xz";
+    hash = "sha256-py2F61v8vO0BCM18GCflAiD48deZjbMM6wqoCDZsOd8=";
+  };
 
   nativeBuildInputs = [
     cmake
@@ -28,6 +24,7 @@ python3Packages.buildPythonApplication {
     wrapGAppsNoGuiHook
     gobject-introspection
   ];
+
   propagatedBuildInputs = with python3Packages; [
     dbus-python
     pygobject3
@@ -35,8 +32,27 @@ python3Packages.buildPythonApplication {
 
   pyproject = false;
   dontWrapGApps = true;
+
   preFixup = ''
     makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
   '';
 
-}
+  meta = {
+    description = "Suite of tools for emulating optical drives and discs";
+    longDescription = ''
+      CDEmu consists of:
+
+      - a kernel module implementing a virtual drive-controller
+      - libmirage which is a software library for interpreting optical disc images
+      - a daemon which emulates the functionality of an optical drive+disc
+      - textmode and GTK clients for controlling the emulator
+      - an image analyzer to view the structure of image files
+
+      Optical media emulated by CDemu can be mounted within Linux. Automounting is also allowed.
+    '';
+    homepage = "https://cdemu.sourceforge.io/";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ bendlas ];
+  };
+})
--- pkgs/applications/emulators/cdemu/gui.nix
+++ pkgs/applications/emulators/cdemu/gui.nix
@@ -1,5 +1,4 @@
 {
-  callPackage,
   cmake,
   pkg-config,
   wrapGAppsHook3,
@@ -10,20 +9,18 @@
   adwaita-icon-theme,
   gdk-pixbuf,
   libappindicator-gtk3,
+  fetchurl,
+  lib,
 }:
-python3Packages.buildPythonApplication {
 
-  inherit
-    (callPackage ./common-drv-attrs.nix {
-      version = "3.2.6";
-      pname = "gcdemu";
-      hash = "sha256-w4vzKoSotL5Cjfr4Cu4YhNSWXJqS+n/vySrwvbhR1zA=";
-    })
-    pname
-    version
-    src
-    meta
-    ;
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "gcdemu";
+  version = "3.2.6";
+
+  src = fetchurl {
+    url = "mirror://sourceforge/cdemu/gcdemu-${finalAttrs.version}.tar.xz";
+    hash = "sha256-w4vzKoSotL5Cjfr4Cu4YhNSWXJqS+n/vySrwvbhR1zA=";
+  };
 
   nativeBuildInputs = [
     cmake
@@ -32,18 +29,41 @@ python3Packages.buildPythonApplication {
     intltool
     gobject-introspection
   ];
+
   buildInputs = [
     libnotify
     adwaita-icon-theme
     gdk-pixbuf
     libappindicator-gtk3
   ];
-  propagatedBuildInputs = with python3Packages; [ pygobject3 ];
+
+  propagatedBuildInputs = with python3Packages; [
+    pygobject3
+  ];
 
   pyproject = false;
   dontWrapGApps = true;
+
   preFixup = ''
     makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
   '';
 
-}
+  meta = {
+    description = "Suite of tools for emulating optical drives and discs";
+    longDescription = ''
+      CDEmu consists of:
+
+      - a kernel module implementing a virtual drive-controller
+      - libmirage which is a software library for interpreting optical disc images
+      - a daemon which emulates the functionality of an optical drive+disc
+      - textmode and GTK clients for controlling the emulator
+      - an image analyzer to view the structure of image files
+
+      Optical media emulated by CDemu can be mounted within Linux. Automounting is also allowed.
+    '';
+    homepage = "https://cdemu.sourceforge.io/";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ bendlas ];
+  };
+})
