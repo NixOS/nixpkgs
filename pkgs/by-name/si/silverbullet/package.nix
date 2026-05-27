08623fd3842ef3bc85f0c2960e83a9a82a8dd557
CnTeng <rxsnakepi@gmail.com>
2026-05-27 20:03:32 +0800

    silverbullet: build from source
    (cherry picked from commit 2269a48d076825704991c7fd82e9a621556697d7)

--- pkgs/by-name/si/silverbullet/package.nix
+++ pkgs/by-name/si/silverbullet/package.nix
@@ -1,75 +1,74 @@
 {
-  autoPatchelfHook,
-  common-updater-scripts,
-  fetchzip,
   lib,
-  nixosTests,
-  stdenv,
-  stdenvNoCC,
-  writeShellScript,
+  fetchFromGitHub,
+  buildNpmPackage,
+  buildGoModule,
+  replaceVars,
 }:
-stdenvNoCC.mkDerivation (finalAttrs: {
+
+buildGoModule (finalAttrs: {
   pname = "silverbullet";
-  version = "2.6.1";
+  version = "2.9.0";
 
-  src =
-    finalAttrs.passthru.sources.${stdenv.hostPlatform.system}
-      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+  src = fetchFromGitHub {
+    owner = "silverbulletmd";
+    repo = "silverbullet";
+    rev = finalAttrs.version;
+    hash = "sha256-XQ0OKkiQrrmwmdGXk3dcim/2qosenF3EG2lkglQQ/iY=";
+  };
 
-  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+  vendorHash = "sha256-8zZlhVptJq8y3k2DBghJ0lPNcIcaZYkrxN67b6dNBPs=";
 
-  buildInputs = [ stdenv.cc.cc.lib ];
+  subPackages = [ "." ];
+
+  frontend = buildNpmPackage {
+    pname = "silverbullet-frontend";
+    inherit (finalAttrs) version src;
+
+    npmDepsHash = "sha256-Twcv3I3scF09onJQdYsc1zOFzMFPOEyPF7VPYa7LBko=";
+
+    patches = [
+      (replaceVars ./override-public-version.patch { inherit (finalAttrs) version; })
+    ];
+
+    postBuild = ''
+      npm run build:plug-compile
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp -r client_bundle public_version.ts $out/
+
+      runHook postInstall
+    '';
+  };
+
+  preBuild = ''
+    cp -r ${finalAttrs.frontend}/client_bundle .
+    cp ${finalAttrs.frontend}/public_version.ts .
+  '';
 
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/bin
-    cp $src/silverbullet $out/bin/
+
+    install -Dm755 "$GOPATH/bin/silverbullet" $out/bin/silverbullet
+
     runHook postInstall
   '';
 
-  passthru = {
-    sources = {
-      "x86_64-linux" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-linux-x86_64.zip";
-        hash = "sha256-m0bQ3J99WZ9CBrA7M2i7Sh/lOI5c+z/an+9bNfQZW4c=";
-        stripRoot = false;
-      };
-      "aarch64-linux" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-linux-aarch64.zip";
-        hash = "sha256-BqTKMCpifX3Y5kFWQb/9exAjjTc/KeUhYtsHSR850qE=";
-        stripRoot = false;
-      };
-      "x86_64-darwin" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-darwin-x86_64.zip";
-        hash = "sha256-sqvB9kEpMimcH/rtOc7lBMptu3Cdu6M3z85TfD9QuZ4=";
-        stripRoot = false;
-      };
-      "aarch64-darwin" = fetchzip {
-        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-darwin-aarch64.zip";
-        hash = "sha256-K/4w4jsa+RIYQA9cW2U/oycJx7PfUzcdG6WjZswRLU0=";
-        stripRoot = false;
-      };
-    };
-
-    updateScript = writeShellScript "update-silverbullet" ''
-      NEW_VERSION="$1"
-      for platform in ${lib.escapeShellArgs finalAttrs.meta.platforms}; do
-        ${lib.getExe' common-updater-scripts "update-source-version"} "silverbullet" "$NEW_VERSION" --ignore-same-version --source-key="sources.$platform"
-      done
-    '';
-
-    tests = {
-      inherit (nixosTests) silverbullet;
-    };
-  };
+  passthru.updateScript = ./update.sh;
 
   meta = {
     changelog = "https://github.com/silverbulletmd/silverbullet/blob/${finalAttrs.version}/website/CHANGELOG.md";
     description = "Open-source, self-hosted, offline-capable Personal Knowledge Management (PKM) web application";
     homepage = "https://silverbullet.md";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ aorith ];
+    maintainers = with lib.maintainers; [
+      aorith
+      CnTeng
+    ];
     mainProgram = "silverbullet";
-    platforms = builtins.attrNames finalAttrs.passthru.sources;
   };
 })
