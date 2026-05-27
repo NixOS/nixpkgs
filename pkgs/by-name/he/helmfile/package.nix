12f6043d9e52cf8b44a1974a3e79c6ff946a7e53
R. Ryantm <ryantm-bot@ryantm.com>
2026-05-27 07:55:59 +0000

    helmfile: 1.5.0 -> 1.5.2

--- pkgs/by-name/he/helmfile/package.nix
+++ pkgs/by-name/he/helmfile/package.nix
@@ -10,16 +10,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "helmfile";
-  version = "1.5.0";
+  version = "1.5.2";
 
   src = fetchFromGitHub {
     owner = "helmfile";
     repo = "helmfile";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Uqm4cnhuILGmdwyZkLNJXrBF4H/zZkg7PfdE//jLP5A=";
+    hash = "sha256-5eWD4Hr65DDWAXHXk0A9Mt90ZNcGt+6NXH5ggSHA4Fk=";
   };
 
-  vendorHash = "sha256-8JEKAiEwkzjFzoMVyPd2Wmpt/Fjh+j9ocmIn5cQBVqE=";
+  vendorHash = "sha256-bbMBesSbDMUqaJWm3Pgauz6tNw8gB0H5vQWKZJSn3sQ=";
 
   proxyVendor = true; # darwin/linux hash mismatch
 
