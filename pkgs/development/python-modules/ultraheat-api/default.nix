cf736af656b71540f6ae2e68a82dfad5ce65c615
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-27 11:35:48 +0200

    python3Packages.ultraheat-api: 0.5.7 -> 0.6.0
    Changelog: https://github.com/vpathuis/ultraheat/releases/tag/v0.6.0

--- pkgs/development/python-modules/ultraheat-api/default.nix
+++ pkgs/development/python-modules/ultraheat-api/default.nix
@@ -2,21 +2,24 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  pyserial,
+  serialx,
+  setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "ultraheat-api";
-  version = "0.5.7";
-  format = "setuptools";
+  version = "0.6.0";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "ultraheat_api";
-    inherit version;
-    hash = "sha256-rRQTjV9hyUawMaXBgUx/d6pQjM8ffjcFJE2x08Cf4Gw=";
+    inherit (finalAttrs) version;
+    hash = "sha256-YWasu54E/8xDZPvaRHph6EM0+UXeRGE2CXheYdwDpNE=";
   };
 
-  propagatedBuildInputs = [ pyserial ];
+  build-system = [ setuptools ];
+
+  dependencies = [ serialx ];
 
   # Source is not tagged, only PyPI releases
   doCheck = false;
@@ -26,7 +29,8 @@ buildPythonPackage rec {
   meta = {
     description = "Module for working with data from Landis+Gyr Ultraheat heat meter unit";
     homepage = "https://github.com/vpathuis/uh50";
+    changelog = "https://github.com/vpathuis/ultraheat/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
