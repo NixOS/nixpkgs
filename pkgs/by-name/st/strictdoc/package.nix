00f12acc8331fb003150cda4ae2c659110b60efd
puzzlewolf <nixos@nora.pink>
2026-05-27 11:26:16 +0200

    strictdoc: 0.19.0 -> 0.21.1

--- pkgs/by-name/st/strictdoc/package.nix
+++ pkgs/by-name/st/strictdoc/package.nix
@@ -6,14 +6,14 @@
 
 python3.pkgs.buildPythonApplication (finalAttrs: {
   pname = "strictdoc";
-  version = "0.19.0";
+  version = "0.21.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "strictdoc-project";
     repo = "strictdoc";
     tag = finalAttrs.version;
-    hash = "sha256-UkXVn1GVWBBhjFaRvkVk+E9mug3i2k7SQk+7JVA8KSo=";
+    hash = "sha256-kKygvUCrkftEEKLoinVkP5DARsHGguSfsPaTpYYXPpU=";
   };
 
   build-system = [
