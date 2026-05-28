b4e72c546611cc806c7b6e855e555eede13cc36c
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2026-05-28 18:28:27 -0400

    podman-desktop: add krunkit to closure on darwin
    When starting podman machine from UI, it fails because podman-desktop
    directly calls to krunkit to determine some configuration if the machine
    is of krunkit type.

--- pkgs/by-name/po/podman-desktop/package.nix
+++ pkgs/by-name/po/podman-desktop/package.nix
@@ -18,6 +18,7 @@
   jq,
   gnugrep,
   podman,
+  krunkit,
 }:
 
 let
@@ -124,7 +125,10 @@ stdenv.mkDerivation (finalAttrs: {
 
   installPhase =
     let
-      commonWrapperArgs = "--prefix PATH : ${lib.makeBinPath [ podman ]}";
+      prefixPackages = lib.makeBinPath (
+        [ podman ] ++ lib.optional (lib.meta.availableOn stdenv.hostPlatform krunkit) krunkit
+      );
+      commonWrapperArgs = "--prefix PATH : ${prefixPackages}";
     in
     (
       ''
