4f40dcef1dfa4286ff6c8462cfb17bd67c44e744
Gaetan Lepage <gaetan@glepage.com>
2026-05-28 08:16:02 +0000

    python3Packages.optuna: skip crashing tests on darwin

--- pkgs/development/python-modules/optuna/default.nix
+++ pkgs/development/python-modules/optuna/default.nix
@@ -119,9 +119,19 @@ buildPythonPackage (finalAttrs: {
   ++ lib.optionals stdenv.hostPlatform.isDarwin [
     # ValueError: Failed to start Kaleido subprocess. Error stream
     # kaleido/executable/kaleido: line 5:  5956 Illegal instruction: 4  ./bin/kaleido $@
+    "test_edf_plot_no_trials"
+    "test_edf_plot_no_trials_studies"
     "test_get_optimization_history_plot"
+    "test_get_timeline_plot"
+    "test_plot_contour"
+    "test_plot_edf_with_multiple_studies"
+    "test_plot_edf_with_target"
+    "test_plot_edf_with_target_name"
     "test_plot_intermediate_values"
+    "test_plot_parallel_coordinate"
+    "test_plot_param_importances"
     "test_plot_rank"
+    "test_plot_slice"
     "test_plot_terminator_improvement"
   ];
 
