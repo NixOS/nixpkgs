1a1ba0b606d4ecd2bbd4efcc25a64877926293be
bokicoder <1556588440@qq.com>
2026-05-29 15:34:58 +0800

    nixos/nvidia: simplify device file creation

--- nixos/modules/hardware/video/nvidia.nix
+++ nixos/modules/hardware/video/nvidia.nix
@@ -458,12 +458,8 @@ in
             "L+ /run/nvidia-docker/bin - - - - ${nvidia_x11.bin}/origBin"
           ];
           services.udev.extraRules = ''
-            # Create /dev/nvidia-uvm when the nvidia-uvm module is loaded.
-            KERNEL=="nvidia", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidiactl c 195 255'"
-            KERNEL=="nvidia", RUN+="${pkgs.runtimeShell} -c 'for i in $$(cat /proc/driver/nvidia/gpus/*/information | grep Minor | cut -d \  -f 4); do mknod -m 666 /dev/nvidia$${i} c 195 $${i}; done'"
-            KERNEL=="nvidia_modeset", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-modeset c 195 254'"
-            KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm c $$(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 0'"
-            KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm-tools c $$(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 1'"
+            KERNEL=="nvidia", RUN+="${nvidia_x11.bin}/bin/nvidia-smi -L"
+            KERNEL=="nvidia_modeset", RUN+="${pkgs.nvidia-modprobe}/bin/nvidia-modprobe -m"
           '';
           hardware.graphics =
             let
