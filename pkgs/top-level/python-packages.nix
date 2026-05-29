b8e58af8bb1be068aae157cad341f7278249a6f2
Sizhe Zhao <prc.zhao@outlook.com>
2026-05-29 19:27:22 +0800

    python3Packages.tilelang: init at 0.1.10

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19660,6 +19660,8 @@ self: super: with self; {
 
   tiledb = callPackage ../development/python-modules/tiledb { inherit (pkgs) tiledb; };
 
+  tilelang = callPackage ../development/python-modules/tilelang { };
+
   tilequant = callPackage ../development/python-modules/tilequant { };
 
   tiler = callPackage ../development/python-modules/tiler { };
