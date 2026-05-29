ccff74d3dd0c278190e0bc7fa764b823dc011e37
Sigmanificient <edhyjox@gmail.com>
2026-05-29 23:48:02 +0200

    python3Packages.aiolookin: migrate to pyproject

--- pkgs/development/python-modules/aiolookin/default.nix
+++ pkgs/development/python-modules/aiolookin/default.nix
@@ -7,12 +7,13 @@
   pytest-aiohttp,
   pytest-mock,
   pytestCheckHook,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "aiolookin";
   version = "1.0.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ANMalko";
@@ -21,7 +22,9 @@ buildPythonPackage rec {
     hash = "sha256-G3/lUgV60CMLskUo83TlvLLIfJtu5DEz+94mdVI4OrI=";
   };
 
-  propagatedBuildInputs = [ aiohttp ];
+  build-system = [ setuptools ];
+
+  dependencies = [ aiohttp ];
 
   doCheck = false; # all tests are async and no async plugin is configured
 
