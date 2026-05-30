5533e08f934816f3a7b09f907b70a31ba5a2c44d
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-05-30 16:20:19 +0200

    vapoursynth-editor: migrate to pkgs/by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6009,8 +6009,6 @@ with pkgs;
       ;
   };
 
-  vapoursynth-editor = libsForQt5.callPackage ../by-name/va/vapoursynth/editor.nix { };
-
   enchant = enchant_2;
 
   factorPackages-0_99 = callPackage ./factor-packages.nix {
