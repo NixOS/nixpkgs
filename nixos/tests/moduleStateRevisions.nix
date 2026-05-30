4a2b484ec82f65d78947962152d449a69e9aa40a
Ryan Hendrickson <ryan.hendrickson@alum.mit.edu>
2026-05-30 19:48:10 -0400

    nixos/seerr: use utils.mkStateRevisionOption

--- nixos/tests/moduleStateRevisions.nix
+++ nixos/tests/moduleStateRevisions.nix
@@ -10,6 +10,16 @@ let
       { system.stateVersion = lib.trivial.release; }
       cfg
     ]).config.system.moduleStateRevisions;
+
+  # For modules that follow the <module>.enable, <module>.stateRevision pattern:
+  testModule =
+    path:
+    evalModuleStateRevisions (lib.setAttrByPath path { enable = true; })
+    ? "${builtins.concatStringsSep "." path}.stateRevision";
 in
 assert evalModuleStateRevisions { } == { };
+assert testModule [
+  "services"
+  "seerr"
+];
 emptyFile
