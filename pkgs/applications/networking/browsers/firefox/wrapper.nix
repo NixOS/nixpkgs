NixOS/nixpkgs

commit 1da3ca73732263dc0473f0d64ccdfa810eaa1fac

firefox/wrapper: better way to disable update checks
See https://bugzilla.mozilla.org/show_bug.cgi?id=2042197

pkgs/applications/networking/browsers/firefox/wrapper.nix
@@ -166,36 +166,34 @@ let
           ) (lib.optionals usesNixExtensions nixExtensions);
 
       enterprisePolicies = {
-        policies = {
-          DisableAppUpdate = true;
-        }
-        // lib.optionalAttrs usesNixExtensions {
-          ExtensionSettings = {
-            "*" = {
-              blocked_install_message = "You can't have manual extension mixed with nix extensions";
-              installation_mode = "blocked";
-            };
-          }
-          // lib.foldr (
-            e: ret:
-            ret
-            // {
-              "${e.extid}" = {
-                installation_mode = "allowed";
+        policies =
+          lib.optionalAttrs usesNixExtensions {
+            ExtensionSettings = {
+              "*" = {
+                blocked_install_message = "You can't have manual extension mixed with nix extensions";
+                installation_mode = "blocked";
               };
             }
-          ) { } extensions;
+            // lib.foldr (
+              e: ret:
+              ret
+              // {
+                "${e.extid}" = {
+                  installation_mode = "allowed";
+                };
+              }
+            ) { } extensions;
 
-          Extensions = {
-            Install = lib.foldr (e: ret: ret ++ [ "${e.outPath}/${e.extid}.xpi" ]) [ ] extensions;
-          };
-        }
-        // lib.optionalAttrs smartcardSupport {
-          SecurityDevices = {
-            "OpenSC PKCS#11 Module" = "opensc-pkcs11.so";
-          };
-        }
-        // extraPolicies;
+            Extensions = {
+              Install = lib.foldr (e: ret: ret ++ [ "${e.outPath}/${e.extid}.xpi" ]) [ ] extensions;
+            };
+          }
+          // lib.optionalAttrs smartcardSupport {
+            SecurityDevices = {
+              "OpenSC PKCS#11 Module" = "opensc-pkcs11.so";
+            };
+          }
+          // extraPolicies;
       };
 
       mozillaCfg = ''
@@ -413,6 +411,9 @@ let
             target=''${target/#"${browser}"/"$out"}
             ln -sfT "$target" "$out/$l"
           done
+
+          # Disable update checks
+          touch $out/${libDir}/is-packaged-app
 
           cd "$out"
 

commit a2e905224c8db85037d7720854a0c3c141f3ea95

firefox/wrapper: quote is-packaged-app path
The unquoted $out/${libDir}/is-packaged-app path word-splits on
applicationNames containing spaces (e.g. "Firefox Developer Edition"),
breaking the Darwin build for firefox-devedition and similar variants.

Regression from 1da3ca73732263dc0473f0d64ccdfa810eaa1fac.

pkgs/applications/networking/browsers/firefox/wrapper.nix
@@ -413,7 +413,7 @@ let
           done
 
           # Disable update checks
-          touch $out/${libDir}/is-packaged-app
+          touch "$out/${libDir}/is-packaged-app"
 
           cd "$out"
 