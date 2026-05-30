5ca9ff267f515db8dc57ea1756841b08f82c13b6
Michael Daniels <mdaniels5757@gmail.com>
2026-05-30 12:44:01 -0400

    {ibus,ibus-with-plugins}: move to by-name
    (cherry picked from commit 6c14796461a9069142fd55be6b2e2800bf9c8c5a)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1955,8 +1955,6 @@ with pkgs;
 
   skkDictionaries = recurseIntoAttrs (callPackages ../tools/inputmethods/skk/skk-dicts { });
 
-  ibus = callPackage ../tools/inputmethods/ibus { };
-
   ibus-engines = recurseIntoAttrs {
     anthy = callPackage ../tools/inputmethods/ibus-engines/ibus-anthy { };
 
@@ -1999,8 +1997,6 @@ with pkgs;
       typing-booster = ibus-engines.typing-booster-unwrapped;
     };
   };
-
-  ibus-with-plugins = callPackage ../tools/inputmethods/ibus/wrapper.nix { };
 
   interception-tools = callPackage ../tools/inputmethods/interception-tools { };
   interception-tools-plugins = recurseIntoAttrs {
