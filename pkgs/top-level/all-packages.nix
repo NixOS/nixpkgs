40e14b821fa5c78b6f3c7adac40afa2001be5751
Michael Daniels <mdaniels5757@gmail.com>
2026-05-30 11:05:51 -0400

    {dropbox,dropbox-cli}: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9758,10 +9758,6 @@ with pkgs;
     })
   );
 
-  dropbox = callPackage ../applications/networking/dropbox { };
-
-  dropbox-cli = callPackage ../applications/networking/dropbox/cli.nix { };
-
   maestral = with python3Packages; toPythonApplication maestral;
 
   myfitnesspal = with python3Packages; toPythonApplication myfitnesspal;
