740e0a4a68ea3cf464cbfa80c03e0f7953c19d08
bokicoder <1556588440@qq.com>
2026-05-30 22:58:35 +0800

    nixos/nvidia: merge same rules for different classes

--- nixos/modules/hardware/video/nvidia.nix
+++ nixos/modules/hardware/video/nvidia.nix
@@ -843,12 +843,10 @@ in
             ''
             + ''
               # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
-              ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-              ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
+              ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", TEST=="power/control", ATTR{power/control}="auto"
 
               # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
-              ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
-              ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
+              ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", TEST=="power/control", ATTR{power/control}="on"
             ''
           );
         })
