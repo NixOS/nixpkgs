5ea149cb40c7e7ad42b83438f94b563d1721b9a6
Matthias Beyer <mail@beyermatthias.de>
2026-05-31 09:26:34 +0200

    fail2ban-ui: Add nixos module
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1533,6 +1533,7 @@
   ./services/security/endlessh-go.nix
   ./services/security/endlessh.nix
   ./services/security/esdm.nix
+  ./services/security/fail2ban-ui.nix
   ./services/security/fail2ban.nix
   ./services/security/fprintd.nix
   ./services/security/haveged.nix
