27eb6c78c3ceee52cdb3361c43abae5bc0dc20da
whispers <whispersofthedawn@duck.com>
2026-05-31 00:57:15 -0400

    squirreldisk: drop
    This package is dependent on webkitgtk 4.0 and libsoup 2.4 via Tauri v1,
    and has been marked broken since October 2025. It has not been changed
    upstream since February 2023, so this is unlikely to change.

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -2053,6 +2053,7 @@ mapAliases {
   springLobby = throw "springLobby has been removed, as it had been broken since 2023"; # Added 2025-09-16
   sqlar = throw "'sqlar' has been removed, as it is umaintained upstream, and depends on fuse2. Consider using the sqlite builtin VACUUM";
   sqlbag = throw "sqlbag has been removed because it has been marked as broken since May 2024."; # Added 2025-10-11
+  squirreldisk = throw "'squirreldisk' has been removed as it depended on webkitgtk 4.0"; # Added 2026-06-07
   src = throw "The \"src\" package has been renamed to \"simple-revision-control\". If you encounter this error and did not intend to use that package you may have a falsely constructed overlay."; # Added 2025-11-19
   ssm-agent = throw "'ssm-agent' has been renamed to/replaced by 'amazon-ssm-agent'"; # Converted to throw 2025-10-27
   stacer = throw "'stacer' has been removed because it was abandoned upstream and relied upon vulnerable software"; # Added 2025-11-08
