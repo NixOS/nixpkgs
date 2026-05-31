e74083f6e0ab86ab94e67d20f70d67409b5cf005
lukas-sgx <lukas.soigneux@epitech.eu>
2026-05-31 21:11:44 +0200

    epifaster: init at 1.0

---
+++ pkgs/by-name/ep/epifaster/package.nix
@@ -0,0 +1,39 @@
+{ lib
+, rustPlatform
+, fetchFromGitHub
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "epifaster";
+  version = "1.0";
+
+  src = fetchFromGitHub {
+    owner = "EpiSDK";
+    repo = "EpiFaster";
+    rev = "v${version}";
+    hash = "sha256-klTqsW6pzrlxoRhh/4DoVtMYEOkt62Hu+o4SKobJAV4=";
+  };
+
+  cargoHash = "sha256-LoSEGRgS/AuGk2KCEZKqDskPm083Jj59OtBShvBWxtQ=";
+
+  postPatch = ''
+    substituteInPlace src/main.rs \
+      --replace '".local/lib/epiclang/plugins"' '"${placeholder "out"}/lib/epiclang/plugins"'
+  '';
+
+  postInstall = ''
+    if ls $src/plugins/*.so 2>/dev/null; then
+      mkdir -p $out/lib/epifaster/plugins
+      cp $src/plugins/*.so $out/lib/epifaster/plugins/
+    fi
+  '';
+
+  meta = {
+    description = "Rewrite and optimization of Epiclang, ~57% faster";
+    homepage = "https://github.com/EpiSDK/EpiFaster";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ lukas-sgx ];
+    mainProgram = "epiclang";
+    platforms = lib.platforms.all;
+  };
+}
