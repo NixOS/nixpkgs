3cab26b4d7e765311f1fd79e2e02e0a80bdfe3cf
Karl Hallsby <karl@hallsby.com>
2026-05-31 13:41:34 -0500

    octavePackages.instrument-control: Bootstrap sources ourselves

--- pkgs/development/octave-modules/instrument-control/default.nix
+++ pkgs/development/octave-modules/instrument-control/default.nix
@@ -3,6 +3,8 @@
   lib,
   fetchFromGitHub,
   nix-update-script,
+  pkg-config,
+  autoreconfHook,
 }:
 
 buildOctavePackage rec {
@@ -15,6 +17,26 @@ buildOctavePackage rec {
     tag = "release-${version}";
     sha256 = "sha256-oasZryYK/KgrIMZWU6j8g24MJMr5R6LwPvuqojAXbm4=";
   };
+
+  nativeBuildInputs = [
+    pkg-config
+    autoreconfHook
+  ];
+
+  # autoreconfHook provides an autoreconfPhase that is run as a
+  # preconfigurePhase, which means it runs AFTER the source is un-tarred, and
+  # before buildOctavePackage's buildPhase re-tars it up into a format for later
+  # consumption by Octave's "pkg build" command.
+  preAutoreconf = ''
+    pushd src
+    # Upstream's bootstrap script uses wget to fetch config.guess & config.sub
+    # and has them committed to the repository. We must remove them so autoreconf
+    # actually fires for our environment.
+    rm config.*
+  '';
+  postAutoreconf = ''
+    popd
+  '';
 
   passthru.updateScript = nix-update-script {
     extraArgs = [
