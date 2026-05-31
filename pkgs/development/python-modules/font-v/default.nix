5600422f2ab56b230fb66ef2826a381cf68ee1bc
Sigmanificient <edhyjox@gmail.com>
2026-06-01 01:28:17 +0200

    font-v: migrate to pyproject

--- pkgs/development/python-modules/font-v/default.nix
+++ pkgs/development/python-modules/font-v/default.nix
@@ -6,12 +6,13 @@
   gitMinimal,
   gitpython,
   pytestCheckHook,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "font-v";
   version = "2.1.0";
-  format = "setuptools";
+  pyproject = true;
 
   # PyPI source tarballs omit tests, fetch from Github instead
   src = fetchFromGitHub {
@@ -21,7 +22,9 @@ buildPythonPackage rec {
     hash = "sha256-ceASyYcNul5aWPAPGajCQrqsQ3bN1sE+nMbCbj7f35w=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     fonttools
     gitpython
   ];
@@ -30,6 +33,7 @@ buildPythonPackage rec {
     gitMinimal
     pytestCheckHook
   ];
+
   preCheck = ''
     # Many tests assume they are running from a git checkout, although they
     # don't care which one. Create a dummy git repo to satisfy the tests:
@@ -38,6 +42,7 @@ buildPythonPackage rec {
     git config user.name Test
     git commit --allow-empty --message 'Dummy commit for tests'
   '';
+
   disabledTests = [
     # These tests assume they are actually running from a font-v git checkout,
     # so just skip them:
