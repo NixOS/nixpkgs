a674cc38b95b964efc6c65073ae949e70ee0030f
Robert Schütz <nix@dotlambda.de>
2026-05-31 12:09:12 -0700

    sshfs-fuse: remove common.nix

--- pkgs/by-name/ss/sshfs-fuse/package.nix
+++ pkgs/by-name/ss/sshfs-fuse/package.nix
@@ -1,14 +1,84 @@
 {
   lib,
-  callPackage,
-  fetchpatch,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  pkg-config,
+  ninja,
+  docutils,
+  makeWrapper,
+  fuse3,
+  macfuse-stubs,
+  glib,
+  which,
+  python3Packages,
+  openssh,
 }:
 
-let
-  mkSSHFS = args: callPackage (import ./common.nix args) { };
-in
-mkSSHFS {
+stdenv.mkDerivation (finalAttrs: {
+  pname = "sshfs-fuse";
   version = "3.7.5";
-  hash = "sha256-6SFjYYWx+p9tZQ670nbjbPtH/QvCAGCB0YwkaQbKIqA=";
-  platforms = lib.platforms.darwin ++ lib.platforms.linux;
-}
+
+  src = fetchFromGitHub {
+    owner = "libfuse";
+    repo = "sshfs";
+    tag = "sshfs-${finalAttrs.version}";
+    hash = "sha256-6SFjYYWx+p9tZQ670nbjbPtH/QvCAGCB0YwkaQbKIqA=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    pkg-config
+    ninja
+    docutils
+    makeWrapper
+  ];
+  buildInputs = [
+    fuse3
+    glib
+  ];
+  nativeCheckInputs = [
+    which
+    python3Packages.pytest
+  ];
+
+  env.NIX_CFLAGS_COMPILE = lib.optionalString (
+    stdenv.hostPlatform.system == "i686-linux"
+  ) "-D_FILE_OFFSET_BITS=64";
+
+  postInstall = ''
+    mkdir -p $out/sbin
+    ln -sf $out/bin/sshfs $out/sbin/mount.sshfs
+  ''
+  + lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
+    wrapProgram $out/bin/sshfs --prefix PATH : "${openssh}/bin"
+  '';
+
+  outputs = [
+    "out"
+    "man"
+  ];
+
+  # doCheck = true;
+  checkPhase = lib.optionalString (!stdenv.hostPlatform.isDarwin) ''
+    # The tests need fusermount:
+    mkdir bin
+    cp ${fuse3}/bin/fusermount3 bin/fusermount
+    export PATH=bin:$PATH
+    # Can't access /dev/fuse within the sandbox: "FUSE kernel module does not seem to be loaded"
+    substituteInPlace test/util.py --replace "/dev/fuse" "/dev/null"
+    # TODO: "fusermount executable not setuid, and we are not root"
+    # We should probably use a VM test instead
+    ${python3Packages.python.interpreter} -m pytest test/
+  '';
+
+  meta = {
+    description = "FUSE-based filesystem that allows remote filesystems to be mounted over SSH";
+    longDescription = macfuse-stubs.warning;
+    homepage = "https://github.com/libfuse/sshfs";
+    license = lib.licenses.gpl2Plus;
+    platforms = lib.platforms.darwin ++ lib.platforms.linux;
+    mainProgram = "sshfs";
+    maintainers = [ ];
+  };
+})
