68d772a38f6a588dd0c1f369a46ae74677eb46ac
Karl Hallsby <karl@hallsby.com>
2026-05-31 14:02:58 -0500

    octavePackages.netcdf: Bootstrap sources ourselves

--- pkgs/development/octave-modules/netcdf/default.nix
+++ pkgs/development/octave-modules/netcdf/default.nix
@@ -4,6 +4,8 @@
   fetchFromGitHub,
   netcdf,
   nix-update-script,
+  pkg-config,
+  autoreconfHook,
 }:
 
 buildOctavePackage rec {
@@ -17,9 +19,29 @@ buildOctavePackage rec {
     sha256 = "sha256-yt39bd6EBLj7mr6EYngPfPXEMusncc9tx5So1Cp1zkM=";
   };
 
+  nativeBuildInputs = [
+    pkg-config
+    autoreconfHook
+  ];
+
   propagatedBuildInputs = [
     netcdf
   ];
+
+  # autoreconfHook provides an autoreconfPhase that is run as a
+  # preconfigurePhase, which means it runs AFTER the source is un-tarred, and
+  # before buildOctavePackage's buildPhase re-tars it up into a format for later
+  # consumption by Octave's "pkg build" command.
+  preAutoreconf = ''
+    pushd src
+    # Upstream's bootstrap script uses wget to fetch config.guess & config.sub
+    # and has them committed to the repository. We must remove them so autoreconf
+    # actually fires for our environment.
+    rm config.*
+  '';
+  postAutoreconf = ''
+    popd
+  '';
 
   meta = {
     homepage = "https://gnu-octave.github.io/packages/netcdf/";
