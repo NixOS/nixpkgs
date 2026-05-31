ba2ae3b6486408829a0b96fd3866373ab25e1c66
user12xxxxxx <nauteshkanojiya@gmail.com>
2026-05-31 14:05:53 +0530

    tetro-tui: init at 3.5.2

---
+++ pkgs/by-name/te/tetro-tui/package.nix
@@ -0,0 +1,23 @@
+{ lib, rustPlatform, fetchFromGitHub }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "tetro-tui";
+  version = "3.5.2";
+
+  src = fetchFromGitHub {
+    owner = "Strophox";
+    repo = "tetro-tui";
+    rev = "v${version}";
+    hash = "sha256-9UHP3mo7sEjVPtBdLgbJpW3RlyXA8zAbY20CgBVdptg=";
+  };
+
+  cargoHash = "sha256-fBbHwCnlUuas+g1dFIRmOZM+hD32tp/++k1KNEFzphY=";
+
+  meta = with lib; {
+    description = "Tetro TUI is a terminal-based but modern tetromino-stacking game that is customizable and cross-platform.";
+    homepage = "https://github.com/Strophox/tetro-tui";
+    license = licenses.mit;
+    mainProgram = "tetro-tui";
+    platforms = platforms.linux;
+  };
+}
