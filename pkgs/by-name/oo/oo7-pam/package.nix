ce9a8220978ddf6270f56607d0de331e2106b86a
Salva <salva.hg01@gmail.com>
2026-05-31 23:33:17 -0600

    oo7-pam: init at 0.6.0

---
+++ pkgs/by-name/oo/oo7-pam/package.nix
@@ -0,0 +1,45 @@
+{
+  cargo,
+  meson,
+  ninja,
+  oo7,
+  pkg-config,
+  rustPlatform,
+  rustc,
+  stdenv,
+  systemdLibs,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "oo7-pam";
+  inherit (oo7) version src cargoDeps;
+
+  sourceRoot = "${finalAttrs.src.name}/pam";
+  cargoRoot = "../";
+
+  strictDeps = true;
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    ninja
+    rustPlatform.cargoSetupHook
+    rustc
+    cargo
+  ];
+
+  buildInputs = [
+    systemdLibs
+  ];
+
+  meta = {
+    inherit (oo7.meta)
+      homepage
+      changelog
+      license
+      maintainers
+      platforms
+      ;
+    description = "${oo7.meta.description} (PAM)";
+  };
+})
