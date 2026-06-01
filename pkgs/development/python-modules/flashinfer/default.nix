68231120c90d2d3cc1083d1cd734d1ae746f9fbe
Gaetan Lepage <gaetan@glepage.com>
2026-06-01 10:16:17 +0000

    python3Packages.flashinfer: 0.6.4 -> 0.6.12
    Diff: https://github.com/flashinfer-ai/flashinfer/compare/v0.6.4...v0.6.12

--- pkgs/development/python-modules/flashinfer/default.nix
+++ pkgs/development/python-modules/flashinfer/default.nix
@@ -6,12 +6,12 @@
 # environment variable `CUDA_HOME` to `cudatoolkit`.
 {
   lib,
-  config,
   buildPythonPackage,
   fetchFromGitHub,
 
   # build-system
   apache-tvm-ffi,
+  packaging,
   setuptools,
 
   # nativeBuildInputs
@@ -23,27 +23,32 @@
   click,
   einops,
   numpy,
+  nvidia-cutlass-dsl,
   nvidia-ml-py,
   tabulate,
   torch,
   tqdm,
+
+  cudaSupport ? torch.cudaSupport,
 }:
 
-buildPythonPackage (finalAttrs: {
+buildPythonPackage.override { inherit (torch) stdenv; } (finalAttrs: {
   pname = "flashinfer";
-  version = "0.6.4";
+  version = "0.6.12";
   pyproject = true;
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "flashinfer-ai";
     repo = "flashinfer";
     tag = "v${finalAttrs.version}";
     fetchSubmodules = true;
-    hash = "sha256-Hq3oTeEJHRvXwThI8vc06E3Ot/FnPP0sZUfze3ISa2o=";
+    hash = "sha256-UhLDUM5sNJdOYiEV2wGMdbijsi+SrIi7wgU7eDAJlW8=";
   };
 
   build-system = [
     apache-tvm-ffi
+    packaging
     setuptools
   ];
 
@@ -81,25 +86,31 @@ buildPythonPackage (finalAttrs: {
     export MAX_JOBS="$NIX_BUILD_CORES"
   '';
 
-  env.FLASHINFER_CUDA_ARCH_LIST = lib.concatStringsSep ";" torch.cudaCapabilities;
+  env = {
+    FLASHINFER_CUDA_ARCH_LIST = lib.concatStringsSep ";" torch.cudaCapabilities;
+  };
 
   pythonRemoveDeps = [
     "nvidia-cudnn-frontend"
-    "nvidia-cutlass-dsl"
   ];
   dependencies = [
     click
     einops
     numpy
+    nvidia-cutlass-dsl
     nvidia-ml-py
     tabulate
     torch
     tqdm
   ];
 
+  doCheck = "TODO";
+
   meta = {
-    broken = !torch.cudaSupport || !config.cudaSupport;
+    broken = !cudaSupport;
     homepage = "https://flashinfer.ai/";
+    downloadPage = "https://github.com/flashinfer-ai/flashinfer";
+    changelog = "https://github.com/flashinfer-ai/flashinfer/releases/tag/${finalAttrs.src.tag}";
     description = "Library and kernel generator for Large Language Models";
     longDescription = ''
       FlashInfer is a library and kernel generator for Large Language Models
