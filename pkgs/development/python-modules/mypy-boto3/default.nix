ac5443d62bc51bbd125fe48f48a6a5ec845790da
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-01 14:33:19 +0200

    python3Packages.mypy-boto3-omics: 1.43.0 -> 1.43.18

--- pkgs/development/python-modules/mypy-boto3/default.nix
+++ pkgs/development/python-modules/mypy-boto3/default.nix
@@ -962,8 +962,8 @@ in
       "sha256-BUl/wnJKR3TB1YsTCLrJdEoH9Lz8DZ6H94STOOX8gkQ=";
 
   mypy-boto3-omics =
-    buildMypyBoto3Package "omics" "1.43.0"
-      "sha256-by5t5x+vu84vjqe176tFcz1zhDUIsmUo6dH5f3OyWOw=";
+    buildMypyBoto3Package "omics" "1.43.18"
+      "sha256-aMV1kli90lXUsGmOwP1lPW9yTH+0/520hBonZC3AIjY=";
 
   mypy-boto3-opensearch =
     buildMypyBoto3Package "opensearch" "1.43.16"
