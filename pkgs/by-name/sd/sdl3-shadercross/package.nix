3f470cb574c6e3fe4e844ab895faa04d0138f61a
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-02 01:12:26 +0000

    sdl3-shadercross: 0-unstable-2026-04-24 -> 0-unstable-2026-06-01

--- pkgs/by-name/sd/sdl3-shadercross/package.nix
+++ pkgs/by-name/sd/sdl3-shadercross/package.nix
@@ -13,7 +13,7 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "sdl3-shadercross";
-  version = "0-unstable-2026-04-24";
+  version = "0-unstable-2026-06-01";
 
   outputs = [
     "out"
@@ -24,8 +24,8 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "libsdl-org";
     repo = "SDL_shadercross";
-    rev = "6b06e55c7c5d7e7a09a8a14f76e866dcfad5ab99";
-    hash = "sha256-DvgMnE0QedInYRdcZQuVOlasri79kVl0ACGvNC1cq8o=";
+    rev = "631641c39db42a2e67c9af7cf6a4e52d7250bb39";
+    hash = "sha256-Bjc2ksy4B3Vk6pgHE5nZ74FQ4ER/XpPt30OBerRkWOY=";
   };
 
   strictDeps = true;
