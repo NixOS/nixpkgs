14acb2aa6af1aa58729e4da0f4132b074093ab89
Dan Eidelman <91460846+DanEidelman@users.noreply.github.com>
2026-06-02 11:26:11 +0300

    memtier-benchmark: 2.2.2 -> 2.3.1

--- pkgs/by-name/me/memtier-benchmark/package.nix
+++ pkgs/by-name/me/memtier-benchmark/package.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "memtier-benchmark";
-  version = "2.2.2";
+  version = "2.3.1";
 
   src = fetchFromGitHub {
-    owner = "redislabs";
+    owner = "redis";
     repo = "memtier_benchmark";
     tag = finalAttrs.version;
-    hash = "sha256-/t7OY3N9VBa9o2amOFb2/MUr5Y4ep4HGUil8OtwKkng=";
+    hash = "sha256-N5oSAtlB8CSUSMQGxKHKHGakmUGJOq80LSkCdJw4H7U=";
   };
 
   patchPhase = ''
@@ -39,7 +39,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     description = "Redis and Memcached traffic generation and benchmarking tool";
-    homepage = "https://github.com/redislabs/memtier_benchmark";
+    homepage = "https://github.com/redis/memtier_benchmark";
     license = lib.licenses.gpl2Only;
     platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ thoughtpolice ];
