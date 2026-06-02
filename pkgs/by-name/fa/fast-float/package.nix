02fd117cf3c47c9da3abd485e8fae2412f56c7e1
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-02 01:19:00 +0000

    fast-float: 8.2.5 -> 8.2.6

--- pkgs/by-name/fa/fast-float/package.nix
+++ pkgs/by-name/fa/fast-float/package.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "fast-float";
-  version = "8.2.5";
+  version = "8.2.6";
 
   src = fetchFromGitHub {
     owner = "fastfloat";
     repo = "fast_float";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-ZQm8kDMYdwjKugc2vBG5mwTqXa01u6hODQc/Tai2I9A=";
+    hash = "sha256-hzoB+Mmok3oe6B494uLc5ReWpUcB89zCGPYw4gvanK0=";
   };
 
   nativeBuildInputs = [ cmake ];
