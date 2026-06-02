83aebe894e0771008fd9e9ec3b8894b8c6e33341
puttaalu <ashik@inflo.ws>
2026-06-02 21:26:46 +0200

    font-smc-keraleeyam: init at 20241013

---
+++ pkgs/by-name/fo/font-smc-keraleeyam/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "font-smc-keraleeyam";
+  version = "20241013";
+
+  src = fetchzip {
+    url = "https://smc.org.in/downloads/fonts/keraleeyam/keraleeyam.zip";
+    hash = "sha256-UEhDtBaWof/2C36IapGhtYgdeQInUn+A/UAJIF7+RuA=";
+    stripRoot = false;
+  };
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    find . -name "*.otf" -exec install -Dm644 {} -t "$out/share/fonts/opentype" \;
+    find . -name "*.ttf" -exec install -Dm644 {} -t "$out/share/fonts/truetype" \;
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Traditional-style Malayalam typeface by SMC";
+    homepage = "https://smc.org.in/fonts/keraleeyam";
+    license = lib.licenses.ofl;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ aashiks ];
+  };
+})
