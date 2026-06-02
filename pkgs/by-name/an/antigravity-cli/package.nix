08f8dcbb4c490729a5117e8abd3f94d8acd777f5
Fida Waseque Choudhury <fida.waseque@gmail.com>
2026-06-02 14:08:13 +0600

    antigravity-cli: improve update script using official manifest

--- pkgs/by-name/an/antigravity-cli/package.nix
+++ pkgs/by-name/an/antigravity-cli/package.nix
@@ -8,24 +8,24 @@
 
 let
   # Version and platform-specific data retrieved from Google's manifests
-  version = "1.0.3";
+  version = "1.0.4";
 
   sourceData = {
     "x86_64-linux" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.3-6459114696605696/linux-x64/cli_linux_x64.tar.gz";
-      hash = "sha256-UM/b3TuXROHHx0dKMU0KtENNREmY+VAYKmxRWewu/ic=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.4-6410134369468416/linux-x64/cli_linux_x64.tar.gz";
+      hash = "sha256-bIfOdECzW1aumL4eAey1udyaGtb4T2120r39WNerlWY=";
     };
     "aarch64-linux" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.3-6459114696605696/linux-arm/cli_linux_arm64.tar.gz";
-      hash = "sha256-Cp44KTj1wP85y6Z6oCOd1ylL2nTg1mDx7aJuv/Q7nBE=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.4-6410134369468416/linux-arm/cli_linux_arm64.tar.gz";
+      hash = "sha256-yOMfJ9f9wVOLs9oNsIoeFnbHfTuKs6DZ7C2djDKY3mo=";
     };
     "aarch64-darwin" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.3-6459114696605696/darwin-arm/cli_mac_arm64.tar.gz";
-      hash = "sha256-lbf6dCJ7QhDNomfpi10Dhk9VShxAxTC0zsFIFpGlbtA=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.4-6410134369468416/darwin-arm/cli_mac_arm64.tar.gz";
+      hash = "sha256-Z88fWVF5Xci3FqamkgStUT2HT+dnybAnwMLwOFpVSUE=";
     };
     "x86_64-darwin" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.3-6459114696605696/darwin-x64/cli_mac_x64.tar.gz";
-      hash = "sha256-B1zPkt4h3JN7ZWhin0iTQMCe+NhVvBGKWCLfKnBrZnw=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.4-6410134369468416/darwin-x64/cli_mac_x64.tar.gz";
+      hash = "sha256-4QzK0BItTcleh8a76D4TVdkj5H2mRxKYC0oho3M363I=";
     };
   };
 
@@ -67,14 +67,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   passthru = {
     inherit sources;
-    updateScript = [
-      ./update.sh
-      version
-    ]
-    ++ lib.concatMap (system: [
-      system
-      sourceData.${system}.url
-    ]) (lib.attrNames sourceData);
+    updateScript = ./update.py;
   };
 
   meta = {
