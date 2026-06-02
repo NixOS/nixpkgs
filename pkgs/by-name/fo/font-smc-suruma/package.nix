218e94164ed3d2f39b544be07fac2f52f0d65692
puttaalu <ashik@inflo.ws>
2026-06-02 21:27:15 +0200

    font-smc-suruma: init at 20241013

---
+++ pkgs/by-name/fo/font-smc-suruma/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "font-smc-suruma";
+  version = "20241013";
+
+  src = fetchzip {
+    url = "https://smc.org.in/downloads/fonts/suruma/suruma.zip";
+    hash = "sha256-x4ybBr5oCwD5Stu8rRGndOYRzaV6ikE+VnLJ/src+1U=";
+    stripRoot = false;
+  };
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    find . -name "*.otf" -exec install -Dm644 {} -t "$out/share/fonts/opentype" \;
+    find . -name "*.ttf" -exec install -Dm644 {} -t "$out/share/fonts/truetype" \;
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Malayalam typeface for body text by SMC";
+    homepage = "https://smc.org.in/fonts/suruma";
+    license = lib.licenses.ofl;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ aashiks ];
+  };
+})
