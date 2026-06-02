25fcfb7ab3267d6df3bda9efedc69ea39743445d
puttaalu <ashik@inflo.ws>
2026-06-02 21:26:19 +0200

    font-smc-anjalioldlipi: init at 20241013

---
+++ pkgs/by-name/fo/font-smc-anjalioldlipi/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "font-smc-anjalioldlipi";
+  version = "20241013";
+
+  src = fetchzip {
+    url = "https://smc.org.in/downloads/fonts/anjalioldlipi/anjalioldlipi.zip";
+    hash = "sha256-c3ScpdN2h39Q6GLFL97pBBGrsillcMXmhlGilOAdF1w=";
+    stripRoot = false;
+  };
+
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    find . -name "*.otf" -exec install -Dm644 {} -t "$out/share/fonts/opentype" \;
+    find . -name "*.ttf" -exec install -Dm644 {} -t "$out/share/fonts/truetype" \;
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Calligraphic-style Malayalam typeface by SMC";
+    homepage = "https://smc.org.in/fonts/anjalioldlipi";
+    license = lib.licenses.ofl;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ aashiks ];
+  };
+})
