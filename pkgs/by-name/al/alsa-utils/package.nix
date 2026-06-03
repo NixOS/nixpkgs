23d0fc9c1103daf3ab58273c12d454661435c4bd
nick-linux8 <nick.stationengineer@proton.me>
2026-06-03 01:54:08 -0400

    alsa-utils: 1.2.15.2 -> 1.2.16

--- pkgs/by-name/al/alsa-utils/package.nix
+++ pkgs/by-name/al/alsa-utils/package.nix
@@ -32,11 +32,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "alsa-utils";
-  version = "1.2.15.2";
+  version = "1.2.16";
 
   src = fetchurl {
     url = "mirror://alsa/utils/alsa-utils-${finalAttrs.version}.tar.bz2";
-    hash = "sha256-eqqvv7AZQhE+wMMeUfcFkQ6BB5IFCIyi+PE3o4aeGjo=";
+    hash = "sha256-CSOZ1eh0mh1eGI45MVdSHOxLdWk7YOu3m7znKM/yIyw=";
   };
 
   nativeBuildInputs = [
