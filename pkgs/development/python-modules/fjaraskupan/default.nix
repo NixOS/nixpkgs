649e1e3f8f547283b425fdd5889bbd871d6154e7
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-03 10:15:21 +0200

    python3Packages.fjaraskupan: modernize

--- pkgs/development/python-modules/fjaraskupan/default.nix
+++ pkgs/development/python-modules/fjaraskupan/default.nix
@@ -9,7 +9,7 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "fjaraskupan";
   version = "2.3.4";
   pyproject = true;
@@ -17,7 +17,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "elupus";
     repo = "fjaraskupan";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-0rJoUQYexB+4ehOXKa1aca401E7opDtdoBmIW/2uOOE=";
   };
 
@@ -38,8 +38,8 @@ buildPythonPackage rec {
   meta = {
     description = "Module for controlling Fjäråskupan kitchen fans";
     homepage = "https://github.com/elupus/fjaraskupan";
-    changelog = "https://github.com/elupus/fjaraskupan/releases/tag/${version}";
-    license = with lib.licenses; [ mit ];
+    changelog = "https://github.com/elupus/fjaraskupan/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
