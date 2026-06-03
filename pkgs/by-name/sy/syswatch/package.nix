943a10781b069e57776fe53ca0c589792a2ff9a2
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-03 13:01:51 +0000

    syswatch: 0.7.0 -> 0.7.1

--- pkgs/by-name/sy/syswatch/package.nix
+++ pkgs/by-name/sy/syswatch/package.nix
@@ -7,18 +7,18 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "syswatch";
-  version = "0.7.0";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "matthart1983";
     repo = "syswatch";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-93KSbhgobGqd29M+cwEr6b1NhrwKhM+nJbRWG/hnvag=";
+    hash = "sha256-9LEZ9DWp5H+ODiuE6A5Qzr/ozXQaXQ7TbDnJY8B8GD4=";
   };
 
   __structuredAttrs = true;
 
-  cargoHash = "sha256-BRmkznPCS8BTLxeHAVz4eBsJZoKdDsaPE2VWMiquMac=";
+  cargoHash = "sha256-dGobKfemSrZ+Y4haHrmLt7dqv9yOBDJ2k3XnZ9LkIlw=";
 
   nativeCheckInputs = [ versionCheckHook ];
 
