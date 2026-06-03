590672d307dd402b07a52272499bcfa50bbb8eed
Harinn <prinn.dev@pm.me>
2026-06-03 21:38:12 +0700

    python3Packages.azure-cosmosdb-nspkg: migrate to pyproject

--- pkgs/development/python-modules/azure-cosmosdb-nspkg/default.nix
+++ pkgs/development/python-modules/azure-cosmosdb-nspkg/default.nix
@@ -3,19 +3,22 @@
   buildPythonPackage,
   fetchPypi,
   azure-nspkg,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "azure-cosmosdb-nspkg";
   version = "2.0.2";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "acf691e692818d9a65c653c7a3485eb8e35c0bdc496bba652e5ea3905ba09cd8";
   };
 
-  propagatedBuildInputs = [ azure-nspkg ];
+  build-system = [ setuptools ];
+
+  dependencies = [ azure-nspkg ];
 
   # has no tests
   doCheck = false;
