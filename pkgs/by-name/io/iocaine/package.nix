4662c4513f7357f266661ec14c332fcbf975fed6
poz <poz@poz.pet>
2026-06-04 01:55:44 +0200

    iocaine: 2.5.1 -> 3.5.0

--- pkgs/by-name/io/iocaine/package.nix
+++ pkgs/by-name/io/iocaine/package.nix
@@ -1,32 +1,48 @@
 {
-  stdenv,
   lib,
-  rustPlatform,
   fetchFromGitea,
+  nftables,
+  nixosTests,
+  pkg-config,
+  rustPlatform,
+  ...
 }:
-
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "iocaine";
-  version = "2.5.1";
+  version = "3.5.0";
 
   src = fetchFromGitea {
     domain = "git.madhouse-project.org";
     owner = "iocaine";
     repo = "iocaine";
     tag = "iocaine-${finalAttrs.version}";
-    hash = "sha256-213QLpGBKSsT9r8O27PyMom5+OGPz0VtRBevxswISZA=";
+    hash = "sha256-adsQuSL4F1mfSsUtLwdgUtHVYessBM31tlBU8Rbbst4=";
   };
 
-  cargoHash = "sha256-EgPGDlJX/m+v3f/tGIO+saGHoYrtiWLZuMlXEvsgnxE=";
+  cargoHash = "sha256-rGPT0YKQ+p11V4+EOMVZrvEmF1Ylbg0hFpSao+Hqn/4=";
+
+  __structuredAttrs = true;
+
+  nativeBuildInputs = [
+    pkg-config
+    rustPlatform.bindgenHook
+  ];
+
+  buildInputs = [
+    nftables
+  ];
+
+  passthru = {
+    tests = { inherit (nixosTests) iocaine; };
+  };
 
   meta = {
     description = "Deadliest poison known to AI";
     homepage = "https://iocaine.madhouse-project.org/";
     changelog = "https://git.madhouse-project.org/iocaine/iocaine/src/tag/${finalAttrs.src.tag}/CHANGELOG.md";
     license = lib.licenses.mit;
+    platforms = lib.platforms.linux;
     maintainers = with lib.maintainers; [ sugar700 ];
     mainProgram = "iocaine";
-    # Lacking OS access to fix, and upstream doesn't support macOS.
-    broken = stdenv.hostPlatform.isDarwin;
   };
 })
