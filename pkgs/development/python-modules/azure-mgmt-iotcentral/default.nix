60e80939afff3756289b3ec567e48757f946a7aa
Harinn <prinn.dev@pm.me>
2026-06-04 21:37:37 +0700

    python3Packages.azure-mgmt-iotcentral: migrate to pyproject

--- pkgs/development/python-modules/azure-mgmt-iotcentral/default.nix
+++ pkgs/development/python-modules/azure-mgmt-iotcentral/default.nix
@@ -8,12 +8,13 @@
   azure-mgmt-core,
   azure-mgmt-nspkg,
   isPy3k,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "azure-mgmt-iotcentral";
   version = "9.0.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
@@ -21,7 +22,9 @@ buildPythonPackage rec {
     sha256 = "64df73df449a6f3717f3d0963e5869224ed3e6216c79de571493bea7c1b52cb6";
   };
 
-  propagatedBuildInputs = [
+  build-system = [ setuptools ];
+
+  dependencies = [
     azure-common
     azure-mgmt-core
     msrest
