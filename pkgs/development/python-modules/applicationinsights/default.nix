4653c84dc890f645c7db6a3554fe6c443c69fffc
Harinn <prinn.dev@pm.me>
2026-06-04 19:46:14 +0700

    python3Packages.applicationinsights: modernize

--- pkgs/development/python-modules/applicationinsights/default.nix
+++ pkgs/development/python-modules/applicationinsights/default.nix
@@ -6,19 +6,21 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   version = "0.11.10";
   pyproject = true;
   pname = "applicationinsights";
 
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "0b761f3ef0680acf4731906dfc1807faa6f2a57168ae74592db0084a6099f7b3";
+    inherit (finalAttrs) pname version;
+    hash = "sha256-C3YfPvBoCs9HMZBt/BgH+qbypXFornRZLbAISmCZ97M=";
   };
 
   build-system = [ setuptools ];
 
   dependencies = [ portalocker ];
+
+  pythonImportsCheck = [ "applicationinsights" ];
 
   meta = {
     description = "This project extends the Application Insights API surface to support Python";
@@ -26,4 +28,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
