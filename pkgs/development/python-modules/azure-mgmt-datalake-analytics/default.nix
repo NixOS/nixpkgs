6d9944055e795006f4232c95bba6da66de4b2df2
Harinn <prinn.dev@pm.me>
2026-06-04 21:26:59 +0700

    python3Packages.azure-mgmt-datalake-analytics: modernize

--- pkgs/development/python-modules/azure-mgmt-datalake-analytics/default.nix
+++ pkgs/development/python-modules/azure-mgmt-datalake-analytics/default.nix
@@ -8,15 +8,17 @@
   azure-mgmt-datalake-nspkg,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "azure-mgmt-datalake-analytics";
   version = "0.6.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     extension = "zip";
-    sha256 = "0d64c4689a67d6138eb9ffbaff2eda2bace7d30b846401673183dcb42714de8f";
+    hash = "sha256-DWTEaJpn1hOOuf+6/y7aK6zn0wuEZAFnMYPctCcU3o8=";
   };
 
   build-system = [ setuptools ];
@@ -32,10 +34,12 @@ buildPythonPackage rec {
   # has no tests
   doCheck = false;
 
+  pythonImportsCheck = [ "azure.mgmt.datalake.analytics" ];
+
   meta = {
     description = "This is the Microsoft Azure Data Lake Analytics Management Client Library";
     homepage = "https://github.com/Azure/azure-sdk-for-python";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ maxwilson ];
   };
-}
+})
