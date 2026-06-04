e082e86aaa1fc4f8474b2f8e5da4e0fc95c3d01f
Ludovico Piero <lewdovico@gnuweeb.org>
2026-06-05 05:21:33 +0900

    samrewritten: 1.4.0 -> 1.4.2
    Signed-off-by: Ludovico Piero <lewdovico@gnuweeb.org>

--- pkgs/by-name/sa/samrewritten/package.nix
+++ pkgs/by-name/sa/samrewritten/package.nix
@@ -15,16 +15,16 @@
 }:
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "samrewritten";
-  version = "1.4.0";
+  version = "1.4.2";
 
   src = fetchFromGitHub {
     owner = "PaulCombal";
     repo = "SamRewritten";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-y8ByD/dGjc+QJCbhy519fBHDXRh+m4sIC7ew17sj03g=";
+    hash = "sha256-p24V0rUnCPanci8KqxGhWA793HVcqO8VSHzabSAOR0A=";
   };
 
-  cargoHash = "sha256-anV5tiXgX9DviKe8ZlnbBrQw5vCnpgQ1xKUki8/f+20=";
+  cargoHash = "sha256-beHc4EETX6cyRFEHaiCghvaRZ6uYQOfllygdkiMg+OA=";
 
   # Tests require network access and a running Steam client. Skipping.
   doCheck = false;
