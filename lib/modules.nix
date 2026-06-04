91da5d6b41d27aaa681591fc2a8ccc8152a21bc5
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-06-03 21:50:29 -0400

    lib.modules: avoid filtering disabled modules if they're empty

--- lib/modules.nix
+++ lib/modules.nix
@@ -567,10 +567,20 @@ let
           isDisabledModule = isDisabled modulesPath disabled;
           keyFilter = filter (attrs: !isDisabledModule attrs);
         in
-        catAttrs "module" (genericClosure {
-          startSet = keyFilter modules;
-          operator = attrs: keyFilter attrs.modules;
-        });
+        catAttrs "module" (
+          genericClosure (
+            if disabled == [ ] then
+              {
+                startSet = modules;
+                operator = attrs: attrs.modules;
+              }
+            else
+              {
+                startSet = keyFilter modules;
+                operator = attrs: keyFilter attrs.modules;
+              }
+          )
+        );
 
       toGraph =
         modulesPath:
