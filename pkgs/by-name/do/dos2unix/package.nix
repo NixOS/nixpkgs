0d5e7c216670c8808a4a63da847233ba6beb55eb
SandaruKasa <sandarukasa@ya.ru>
2026-06-04 20:26:28 +0300

    dos2unix: moderniuze, split outputs, change changelog link

--- pkgs/by-name/do/dos2unix/package.nix
+++ pkgs/by-name/do/dos2unix/package.nix
@@ -15,16 +15,26 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-dfaSuEhMjCRXmi/9h98WuclCjtlUl+M5OiHRugaXrDM=";
   };
 
+  outputs = [
+    "out"
+    "doc"
+    "man"
+  ];
+
   nativeBuildInputs = [
     perl
     gettext
   ];
   makeFlags = [ "prefix=${placeholder "out"}" ];
 
+  strictDeps = true;
+  __structuredAttrs = true;
+  enableParallelBuilding = true;
+
   meta = {
     description = "Convert text files with DOS or Mac line breaks to Unix line breaks and vice versa";
     homepage = "https://waterlan.home.xs4all.nl/dos2unix.html";
-    changelog = "https://sourceforge.net/p/dos2unix/dos2unix/ci/dos2unix-${finalAttrs.version}/tree/dos2unix/NEWS.txt?format=raw";
+    changelog = "https://waterlan.home.xs4all.nl/dos2unix/ChangeLog.txt";
     license = lib.licenses.bsd2;
     maintainers = with lib.maintainers; [
       sandarukasa
