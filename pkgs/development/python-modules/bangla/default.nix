5487e1e0f3987e3f57fa6d259a4928e667b040cc
Harinn <prinn.dev@pm.me>
2026-06-06 01:14:16 +0700

    python3Packages.bangla: migrate to pyproject

--- pkgs/development/python-modules/bangla/default.nix
+++ pkgs/development/python-modules/bangla/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "bangla";
   version = "0.0.5";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-rX2/rUUf9g4otYMNX0LDPXSIDRbIE8xRl95NamHzRwQ=";
   };
+
+  build-system = [ setuptools ];
 
   pythonImportsCheck = [ "bangla" ];
 
