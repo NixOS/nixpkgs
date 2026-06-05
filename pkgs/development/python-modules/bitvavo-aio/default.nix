5ad0be813a7fa92c23ceb659dbc6aac2aa9ad7c6
Harinn <prinn.dev@pm.me>
2026-06-06 02:46:52 +0700

    python3Packages.bitvavo-aio: modernize

--- pkgs/development/python-modules/bitvavo-aio/default.nix
+++ pkgs/development/python-modules/bitvavo-aio/default.nix
@@ -6,16 +6,18 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "bitvavo-aio";
   version = "1.0.3";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "cyberjunky";
     repo = "bitvavo-aio";
-    rev = version;
-    sha256 = "1d9nbbvv7xnkixj03sfhs2da5j3i2m7p73r7j1yb7b39zas2rbig";
+    tag = finalAttrs.version;
+    hash = "sha256-L64stPpprLN8kCePc08VcciimtDQ6QFkj9P2s/daNrU=";
   };
 
   build-system = [ setuptools ];
@@ -33,4 +35,4 @@ buildPythonPackage rec {
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
