f2bc7d7124a329730b89ba2dff70aa147d737270
Harinn <prinn.dev@pm.me>
2026-06-06 02:18:36 +0700

    python3Packages.bech32: migrate to pyproject

--- pkgs/development/python-modules/bech32/default.nix
+++ pkgs/development/python-modules/bech32/default.nix
@@ -2,16 +2,19 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 buildPythonPackage rec {
   pname = "bech32";
   version = "1.2.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-fW24IUYDvXhx/PpsCCbvaLhbCr2Q+iHChanF4h0r2Jk=";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     homepage = "https://pypi.org/project/bech32/";
