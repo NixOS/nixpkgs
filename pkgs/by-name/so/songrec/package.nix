547a8605d30637ba52a30ffe6d9f052f861dd38f
Tomas Rivera <tomas.riveral@icloud.com>
2026-06-05 12:19:30 +0200

    songrec: change rev to tag
    (cherry picked from commit 79ab0268148f63a9077ce85889ba1ef994725f2e)

--- pkgs/by-name/so/songrec/package.nix
+++ pkgs/by-name/so/songrec/package.nix
@@ -23,7 +23,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   src = fetchFromGitHub {
     owner = "marin-m";
     repo = "songrec";
-    rev = finalAttrs.version;
+    tag = finalAttrs.version;
     hash = "sha256-lCwFMBQ6IimNtXYMfTnFOpwOO2qbwijOEZ+oMsQKAP0=";
   };
 
