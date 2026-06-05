da106b56f616de436e612f67b8037a89f970c9dc
Harinn <prinn.dev@pm.me>
2026-06-06 02:44:01 +0700

    python3Packages.bitvavo-aio: migrate to pyproject

--- pkgs/development/python-modules/bitvavo-aio/default.nix
+++ pkgs/development/python-modules/bitvavo-aio/default.nix
@@ -3,12 +3,13 @@
   aiohttp,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "bitvavo-aio";
   version = "1.0.3";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "cyberjunky";
@@ -17,7 +18,9 @@ buildPythonPackage rec {
     sha256 = "1d9nbbvv7xnkixj03sfhs2da5j3i2m7p73r7j1yb7b39zas2rbig";
   };
 
-  propagatedBuildInputs = [ aiohttp ];
+  build-system = [ setuptools ];
+
+  dependencies = [ aiohttp ];
 
   # Project has no tests
   doCheck = false;
