93e6226740d8d01e90759aaa06f75ab94c8873d6
Harinn <prinn.dev@pm.me>
2026-06-06 08:52:06 +0700

    python3Packages.bnnumerizer: modernize

--- pkgs/development/python-modules/bnnumerizer/default.nix
+++ pkgs/development/python-modules/bnnumerizer/default.nix
@@ -5,13 +5,15 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "bnnumerizer";
   version = "0.0.2";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-Qd9v0Le1GqTsR3a2ZDzt6+5f0R4zXX1W1KIMCFFeXw0=";
   };
 
@@ -28,4 +30,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     teams = [ lib.teams.tts ];
   };
-}
+})
