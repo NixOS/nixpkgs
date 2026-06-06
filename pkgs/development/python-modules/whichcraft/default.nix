296e40d75f02a676e09e4ed752ce2cd1042c0c2f
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-06 19:21:58 +0200

    python3Packages.whichcraft: migrate to pyproject

--- pkgs/development/python-modules/whichcraft/default.nix
+++ pkgs/development/python-modules/whichcraft/default.nix
@@ -2,18 +2,21 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   pytest,
 }:
 
 buildPythonPackage (finalAttrs: {
   pname = "whichcraft";
   version = "0.6.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
     sha256 = "11yfkzyplizdgndy34vyd5qlmr1n5mxis3a3svxmx8fnccdvknxc";
   };
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytest ];
 
