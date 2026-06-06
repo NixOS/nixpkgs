2f3bdc7f64d5093d2c2698882efa7950a9e1d72b
Harinn <prinn.dev@pm.me>
2026-06-06 09:35:35 +0700

    python3Packages.cart: modernize

--- pkgs/development/python-modules/cart/default.nix
+++ pkgs/development/python-modules/cart/default.nix
@@ -7,15 +7,17 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "cart";
   version = "1.2.3";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "CybercentreCanada";
     repo = "cart";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-oeWeay1Pr9T4oR3XSrwv9hRr/sLTel1Bt6BG6jHXxIA=";
   };
 
@@ -33,8 +35,8 @@ buildPythonPackage rec {
     description = "Python module for the CaRT Neutering format";
     mainProgram = "cart";
     homepage = "https://github.com/CybercentreCanada/cart";
-    changelog = "https://github.com/CybercentreCanada/cart/releases/tag/${src.tag}";
+    changelog = "https://github.com/CybercentreCanada/cart/releases/tag/${finalAttrs.src.tag}";
     license = with lib.licenses; [ mit ];
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
