186d0e903d48a0cdade6bd56ebb73a7f4eb74c4a
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-06 20:14:58 +0000

    python3Packages.netbox-floorplan-plugin: 0.9.1 -> 0.9.2

--- pkgs/by-name/ne/netbox_4_5/plugins/netbox-floorplan-plugin/package.nix
+++ pkgs/by-name/ne/netbox_4_5/plugins/netbox-floorplan-plugin/package.nix
@@ -10,7 +10,7 @@
 }:
 buildPythonPackage rec {
   pname = "netbox-floorplan-plugin";
-  version = "0.9.1";
+  version = "0.9.2";
   pyproject = true;
 
   disabled = python.pythonVersion != netbox.python.pythonVersion;
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "netbox-community";
     repo = "netbox-floorplan-plugin";
     tag = version;
-    hash = "sha256-0EeE5NrImbCs6xqrSTGupXOuv455EfNXgcLVix2HTPs=";
+    hash = "sha256-I7cz0IONU+RESCA8C57Yxg5ieheryZ31ed9N9OxSeLA=";
   };
 
   build-system = [ setuptools ];
