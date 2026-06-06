16624665e40e8e2703eae974198009743693c82b
Harinn <prinn.dev@pm.me>
2026-06-06 09:03:16 +0700

    python3Packages.bluepy-devices: modernize

--- pkgs/development/python-modules/bluepy-devices/default.nix
+++ pkgs/development/python-modules/bluepy-devices/default.nix
@@ -6,15 +6,17 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "bluepy-devices";
   version = "0.2.1";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
     pname = "bluepy_devices";
-    inherit version;
-    sha256 = "02zzzivxq2vifgs65m2rm8pqlsbzsbc419c032irzvfxjx539mr8";
+    inherit (finalAttrs) version;
+    hash = "sha256-KNc0Spfd7Z+jGIClQNjSf2mKL6pZ1GL0c3EL3Hf8/ws=";
   };
 
   build-system = [ setuptools ];
@@ -31,4 +33,4 @@ buildPythonPackage rec {
     license = with lib.licenses; [ mit ];
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
