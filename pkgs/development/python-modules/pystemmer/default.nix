2de0c3412b2a0fbc27f7aba8d40878224efd6ad5
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-06 23:04:09 +0200

    python3Packages.pystemmer: 3.0.0 -> 3.1.0

--- pkgs/development/python-modules/pystemmer/default.nix
+++ pkgs/development/python-modules/pystemmer/default.nix
@@ -2,6 +2,7 @@
   lib,
   python,
   fetchFromGitHub,
+  fetchpatch2,
   buildPythonPackage,
   cython,
   setuptools,
@@ -10,15 +11,29 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pystemmer";
-  version = "3.0.0";
+  version = "3.1.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "snowballstem";
     repo = "pystemmer";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-c3ucbneUo5UBfdrd5Ktl4HriVusvWBEA1brrgahEQ9A=";
+    hash = "sha256-GPPl6ioB9sB2y8G2hYfu2ksR+D9xNJjK6glMADLnr7M=";
   };
+
+  patches = [
+    # These 2 patches should be removed on the next version bump after 3.1.0
+    (fetchpatch2 {
+      name = "libstemmer-3.1-algorithms.patch";
+      url = "https://github.com/snowballstem/pystemmer/commit/301c074791708ab2c479808b410480c34183cd9a.patch?full_index=1";
+      hash = "sha256-830nep2gjlVuVaepsHIkzL/U4fejwK0jH+WujLrOfUs=";
+    })
+    (fetchpatch2 {
+      name = "dont-hard-code-algorithms-doctest.patch";
+      url = "https://github.com/snowballstem/pystemmer/commit/77ee7b34fdcd61c78146ae4c6e536a63755db0e6.patch?full_index=1";
+      hash = "sha256-eEfNFpD6T+prokVnpAOx4rABCPS5YXdbG3xKdJzb48M=";
+    })
+  ];
 
   build-system = [
     cython
