9f507825e699756437458519d12e4de2e594787f
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-06-06 14:57:29 -0700

    lexend: use installFonts, modernize
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/by-name/le/lexend/package.nix
+++ pkgs/by-name/le/lexend/package.nix
@@ -2,30 +2,24 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
+  installFonts,
 }:
 
 stdenvNoCC.mkDerivation {
   pname = "lexend";
   version = "0.pre+date=2022-09-22";
 
+  strictDeps = true;
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "googlefonts";
     repo = "lexend";
     rev = "cd26b9c2538d758138c20c3d2f10362ed613854b";
-    sha256 = "ZKogntyJ/44GBZmFwbtw5Ujw5Gnvv0tVB59ciKqR4c8=";
+    hash = "sha256-ZKogntyJ/44GBZmFwbtw5Ujw5Gnvv0tVB59ciKqR4c8=";
   };
 
-  installPhase = ''
-    runHook preInstall
-
-    cd fonts
-    for f in *; do
-      install -D -t $out/share/fonts/truetype/lexend/$f $f/ttf/*
-      install -D -t $out/share/fonts/variable/lexend/$f $f/variable/*
-    done
-
-    runHook postInstall
-  '';
+  nativeBuildInputs = [ installFonts ];
 
   meta = {
     homepage = "https://www.lexend.com";
