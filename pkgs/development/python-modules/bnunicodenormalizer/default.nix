425d74bf5b91be822896e85a4ba32866faa3ea09
Harinn <prinn.dev@pm.me>
2026-06-06 08:51:50 +0700

    python3Packages.bnunicodenormalizer: modernize

--- pkgs/development/python-modules/bnunicodenormalizer/default.nix
+++ pkgs/development/python-modules/bnunicodenormalizer/default.nix
@@ -6,13 +6,15 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "bnunicodenormalizer";
   version = "0.1.7";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-hqNInMgcc9KvtOJlvS0Ni8UvyKI3TiEMiZ4CYJQLwJE=";
   };
 
@@ -28,4 +30,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     teams = [ lib.teams.tts ];
   };
-}
+})
