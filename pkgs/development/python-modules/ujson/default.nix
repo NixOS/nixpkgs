cc491a4294daef58624a55d96c09cfc027a34c12
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-06 14:51:50 +0200

    python3Packages.ujson: migrate to pyproject

--- pkgs/development/python-modules/ujson/default.nix
+++ pkgs/development/python-modules/ujson/default.nix
@@ -9,14 +9,14 @@
 buildPythonPackage (finalAttrs: {
   pname = "ujson";
   version = "5.11.0";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit (finalAttrs) pname version;
     hash = "sha256-4gSub5CfCZumtrlCExzuNZ3dorbk6jnBLri5kf4gEOA=";
   };
 
-  nativeBuildInputs = [ setuptools-scm ];
+  build-system = [ setuptools-scm ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
