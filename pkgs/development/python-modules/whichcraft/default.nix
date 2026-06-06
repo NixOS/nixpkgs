d332e23d908bfe6477082dc2dc92dad9ede5f4f1
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-06 19:18:21 +0200

    python3Packages.whichcraft: use finalAttrs

--- pkgs/development/python-modules/whichcraft/default.nix
+++ pkgs/development/python-modules/whichcraft/default.nix
@@ -6,13 +6,13 @@
   glibcLocales,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "whichcraft";
   version = "0.6.1";
   format = "setuptools";
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     sha256 = "11yfkzyplizdgndy34vyd5qlmr1n5mxis3a3svxmx8fnccdvknxc";
   };
 
@@ -30,4 +30,4 @@ buildPythonPackage rec {
     description = "Cross-platform cross-python shutil.which functionality";
     license = lib.licenses.bsd3;
   };
-}
+})
