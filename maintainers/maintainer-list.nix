f340498074902910ac4f637a3d68e8fbbbd6a4b7
Your Name <you@example.com>
2026-06-06 09:50:10 +0200

    maintainers: add mikolajkniejski

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -18036,6 +18036,12 @@
     githubId = 3958340;
     name = "Eshin Kunishima";
   };
+  mikolajkniejski = {
+    email = "mikolajkniejski@tutanota.com";
+    github = "mikolajkniejski";
+    githubId = 64133041;
+    name = "Mikolaj Kniejski";
+  };
   mikroskeem = {
     email = "mikroskeem@mikroskeem.eu";
     github = "mikroskeem";
