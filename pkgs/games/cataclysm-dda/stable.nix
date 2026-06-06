d56af359752d82c2949003a595c981d575b4e59f
Tom Oostveen <tomoostveen.06@gmail.com>
2026-06-06 21:22:35 +0200

    cataclysm-dda: 0.H-2025-07-10-0402 -> 0.I

--- pkgs/games/cataclysm-dda/stable.nix
+++ pkgs/games/cataclysm-dda/stable.nix
@@ -17,20 +17,14 @@ let
   };
 
   self = common.overrideAttrs (common: rec {
-    version = "0.H-2025-07-10-0402";
+    version = "0.I";
 
     src = fetchFromGitHub {
       owner = "CleverRaven";
       repo = "Cataclysm-DDA";
-      # Head of 0.H-branch
-      tag = "cdda-${version}";
-      sha256 = "sha256-r4cl8cij68WmQRfg+DHQIeDBIwhgwSre6kAUYZaCPR8=n";
+      tag = "${version}";
+      hash = "sha256-nzHqN6WjhuR1IoJ50XryI3B1fUQPepzGMaDJzudUaVI=";
     };
-
-    patches = [
-      # Unconditionally look for translation files in $out/share/locale
-      ./locale-path.patch
-    ];
 
     meta = common.meta // {
       changelog = "https://github.com/CleverRaven/Cataclysm-DDA/blob/${version}/data/changelog.txt";
