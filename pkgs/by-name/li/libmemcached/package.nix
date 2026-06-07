33f6a6e239d813d65e627086d78bd9579823d708
Robert Schütz <nix@dotlambda.de>
2026-06-07 10:30:58 -0700

    libmemcached: 1.0.18 -> 1.1.4
    We switch the upstream to https://github.com/awesomized/libmemcached as
    did e.g. Alpine Linux and Arch Linux.
    
    Diff: https://github.com/awesomized/libmemcached/compare/1.0.18...1.1.4
    
    Changelog: https://github.com/awesomized/libmemcached/blob/1.1.4/ChangeLog-1.1.md

--- pkgs/by-name/li/libmemcached/package.nix
+++ pkgs/by-name/li/libmemcached/package.nix
@@ -1,41 +1,41 @@
 {
   lib,
   stdenv,
-  fetchurl,
-  fetchpatch,
+  fetchFromGitHub,
+  bison,
+  cmake,
+  flex,
   cyrus_sasl,
   libevent,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libmemcached";
-  version = "1.0.18";
+  version = "1.1.4";
 
-  src = fetchurl {
-    url = "https://launchpad.net/libmemcached/${lib.versions.majorMinor finalAttrs.version}/${finalAttrs.version}/+download/libmemcached-${finalAttrs.version}.tar.gz";
-    sha256 = "10jzi14j32lpq0if0p9vygcl2c1352hwbywzvr9qzq7x6aq0nb72";
+  src = fetchFromGitHub {
+    owner = "awesomized";
+    repo = "libmemcached";
+    tag = finalAttrs.version;
+    hash = "sha256-jEw6L2/139oo4sGprl9Xp0DTarxAK1bEF2ak2kHWSAs=";
   };
 
-  # Fix linking against libpthread (patch from Fedora)
-  # https://bugzilla.redhat.com/show_bug.cgi?id=1037707
-  # https://bugs.launchpad.net/libmemcached/+bug/1281907
-  # Fix building on macOS (patch from Homebrew)
-  # https://bugs.launchpad.net/libmemcached/+bug/1245562
-  patches =
-    lib.optional stdenv.hostPlatform.isLinux ./libmemcached-fix-linking-with-libpthread.patch
-    ++ lib.optional stdenv.hostPlatform.isDarwin (fetchpatch {
-      url = "https://raw.githubusercontent.com/Homebrew/homebrew/bfd4a0a4626b61c2511fdf573bcbbc6bbe86340e/Library/Formula/libmemcached.rb";
-      sha256 = "1gjf3vd7hiyzxjvlg2zfc3y2j0lyr6nhbws4xb5dmin3csyp8qb8";
-    })
-    ++ lib.optional stdenv.hostPlatform.isMusl ./musl-fixes.patch;
+  nativeBuildInputs = [
+    bison
+    cmake
+    flex
+  ];
+
+  cmakeFlags = [
+    "-DENABLE_SASL=ON"
+  ];
 
   buildInputs = [ libevent ];
   propagatedBuildInputs = [ cyrus_sasl ];
 
-  env.NIX_CFLAGS_COMPILE = "-fpermissive";
-
   meta = {
-    homepage = "https://libmemcached.org";
+    homepage = "https://github.com/awesomized/libmemcached";
+    changelog = "https://github.com/awesomized/libmemcached/blob/${finalAttrs.src.tag}/ChangeLog-${lib.versions.majorMinor finalAttrs.version}.md";
     description = "Open source C/C++ client library and tools for the memcached server";
     license = lib.licenses.bsd3;
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
