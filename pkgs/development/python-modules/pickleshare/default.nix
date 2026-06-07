cfb46e0e7c67bc840b67186f6bd9eb6603ffd6be
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-07 23:49:21 +0200

    python3Packages.pickleshare: migrate to pyproject

--- pkgs/development/python-modules/pickleshare/default.nix
+++ pkgs/development/python-modules/pickleshare/default.nix
@@ -2,13 +2,14 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
   path,
 }:
 
 buildPythonPackage (finalAttrs: {
-  version = "0.7.5";
-  format = "setuptools";
   pname = "pickleshare";
+  version = "0.7.5";
+  pyproject = true;
 
   __structuredAttrs = true;
 
@@ -17,7 +18,9 @@ buildPythonPackage (finalAttrs: {
     sha256 = "87683d47965c1da65cdacaf31c8441d12b8044cdec9aca500cd78fc2c683afca";
   };
 
-  propagatedBuildInputs = [ path ];
+  build-system = [ setuptools ];
+
+  dependencies = [ path ];
 
   # No proper test suite
   doCheck = false;
