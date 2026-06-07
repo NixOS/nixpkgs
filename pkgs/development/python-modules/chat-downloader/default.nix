9896e71998cfd0e7dccc29080d0ccf628efe37a2
Harinn <prinn.dev@pm.me>
2026-06-07 10:41:33 +0700

    python3Packages.chat-downloader: modernize

--- pkgs/development/python-modules/chat-downloader/default.nix
+++ pkgs/development/python-modules/chat-downloader/default.nix
@@ -11,13 +11,15 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "chat-downloader";
   version = "0.2.8";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit version pname;
+    inherit (finalAttrs) version pname;
     hash = "sha256-WBasBhefgRkOdMdz2K/agvS+cY6m3/33wiu+Jl4d1Cg=";
   };
 
@@ -42,8 +44,8 @@ buildPythonPackage rec {
     description = "Simple tool used to retrieve chat messages from livestreams, videos, clips and past broadcasts";
     mainProgram = "chat_downloader";
     homepage = "https://github.com/xenova/chat-downloader";
-    changelog = "https://github.com/xenova/chat-downloader/releases/tag/v${version}";
+    changelog = "https://github.com/xenova/chat-downloader/releases/tag/v${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
