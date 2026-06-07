446528d946321b5e2a85bc5c3c5a2787e5820c85
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-08 00:01:29 +0200

    python3Packages.pymitsubishi: migrate to finalAttrs

--- pkgs/development/python-modules/pymitsubishi/default.nix
+++ pkgs/development/python-modules/pymitsubishi/default.nix
@@ -9,7 +9,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pymitsubishi";
   version = "0.5.2";
   pyproject = true;
@@ -17,13 +17,13 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "pymitsubishi";
     repo = "pymitsubishi";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-e2BbeJQH1SSge/SoU5ylF1/wHz2BBcBRqlpvX8WFJhs=";
   };
 
   postPatch = ''
     # make sure pyproject.toml specifies the correct version
-    grep -qF 'version = "${version}"' pyproject.toml
+    grep -qF 'version = "${finalAttrs.version}"' pyproject.toml
   '';
 
   build-system = [ setuptools ];
@@ -43,8 +43,8 @@ buildPythonPackage rec {
   meta = {
     description = "Library for controlling and monitoring Mitsubishi MAC-577IF-2E air conditioners";
     homepage = "https://github.com/pymitsubishi/pymitsubishi";
-    changelog = "https://github.com/pymitsubishi/pymitsubishi/releases/tag/${src.tag}";
+    changelog = "https://github.com/pymitsubishi/pymitsubishi/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ uvnikita ];
   };
-}
+})
