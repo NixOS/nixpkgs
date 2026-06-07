02084c69754b465a77b82a798312e66d1e11fe69
Philip Johansson <philip@phlipphlop.me>
2026-06-07 11:48:30 +0200

    python3Packages.croniter: 6.0.0 -> 6.2.2

--- pkgs/development/python-modules/croniter/default.nix
+++ pkgs/development/python-modules/croniter/default.nix
@@ -1,43 +1,69 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
-  setuptools,
+  fetchFromGitHub,
+  hatchling,
+  packaging,
   pytestCheckHook,
+  python,
   python-dateutil,
   pytz,
+  trove-classifiers,
   tzlocal,
+  versionCheckHook,
+  which,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "croniter";
-  version = "6.0.0";
+  version = "6.2.2";
   pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-N8UEsxOVYRSpg+ziwrB3kLHxCU/p2BzJRzkhR0glVXc=";
+  src = fetchFromGitHub {
+    owner = "pallets-eco";
+    repo = "croniter";
+    tag = "${finalAttrs.version}";
+    hash = "sha256-RuUQ1wL/fwB8kD5yEaGHa9jkDKGQgzLLYTwJdcY7Q2s=";
   };
 
-  nativeBuildInputs = [ setuptools ];
-
-  propagatedBuildInputs = [
-    python-dateutil
-    pytz
+  build-system = [
+    hatchling
+    packaging
+    trove-classifiers
   ];
+
+  dependencies = [ python-dateutil ];
 
   nativeCheckInputs = [
     pytestCheckHook
+    pytz
     tzlocal
   ];
 
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "packaging==26.0" "packaging>=26.0" \
+      --replace-fail "trove-classifiers==2026.1.14.14" "trove-classifiers>=2026.1.14.14"
+  '';
+
   pythonImportsCheck = [ "croniter" ];
+
+  nativeInstallCheckInputs = [
+    versionCheckHook
+    which
+  ];
+
+  versionCheckProgramArg = "${placeholder "out"}/${python.sitePackages}";
+
+  preInstallCheck = ''
+    versionCheckProgram="$(which ls)"
+  '';
 
   meta = {
     description = "Library to iterate over datetime object with cron like format";
     homepage = "https://github.com/kiorky/croniter";
-    changelog = "https://github.com/kiorky/croniter/blob/${version}/CHANGELOG.rst";
+    changelog = "https://github.com/kiorky/croniter/blob/${finalAttrs.version}/CHANGELOG.rst";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
