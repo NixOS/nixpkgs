4324b96952a975ef30f4a79e86397af189b86535
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-07 23:10:28 +0200

    python3Packages.update-copyright: migrate to pyproject

--- pkgs/development/python-modules/update-copyright/default.nix
+++ pkgs/development/python-modules/update-copyright/default.nix
@@ -3,25 +3,30 @@
   buildPythonPackage,
   fetchPypi,
   isPy3k,
+  setuptools,
 }:
 
 buildPythonPackage (finalAttrs: {
   pname = "update-copyright";
   version = "0.6.2";
-  format = "setuptools";
+  pyproject = true;
 
   __structuredAttrs = true;
 
   disabled = !isPy3k;
-
-  # Has no tests
-  doCheck = false;
 
   src = fetchPypi {
     inherit (finalAttrs) version;
     pname = "update-copyright";
     sha256 = "17ybdgbdc62yqhda4kfy1vcs1yzp78d91qfhj5zbvz1afvmvdk7z";
   };
+
+  build-system = [
+    setuptools
+  ];
+
+  # Has no tests
+  doCheck = false;
 
   meta = {
     description = "Automatic copyright update tool";
