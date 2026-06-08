f3018ef83998cc9c933c0d3451e626707b569111
Adriel Velazquez <AdrielVelazquez@gmail.com>
2026-06-08 14:32:59 +0000

    antigravity-cli: 1.0.6 -> 1.0.7

--- pkgs/by-name/an/antigravity-cli/package.nix
+++ pkgs/by-name/an/antigravity-cli/package.nix
@@ -8,24 +8,24 @@
 
 let
   # Version and platform-specific data retrieved from Google's manifests
-  version = "1.0.6";
+  version = "1.0.7";
 
   sourceData = {
     "x86_64-linux" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.6-5359777384103936/linux-x64/cli_linux_x64.tar.gz";
-      hash = "sha256-rxDLuuium+yQl3SiRcFhLzC5+ZCZU/tG2LQfFZMOYx4=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.7-5858071034068992/linux-x64/cli_linux_x64.tar.gz";
+      hash = "sha256-GY2QcdWd2tUwUdQZr5SPtaEXnvWHTT5PcXdrmHuV5RE=";
     };
     "aarch64-linux" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.6-5359777384103936/linux-arm/cli_linux_arm64.tar.gz";
-      hash = "sha256-Mol5V3Lt2A89yrGdwWiOdv4y5dCZkMaT8onXG6IsQtc=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.7-5858071034068992/linux-arm/cli_linux_arm64.tar.gz";
+      hash = "sha256-sn92DI+Jsywe1Z/Lvnlsc/NssMkAU3wJjWhx8i6K7PA=";
     };
     "aarch64-darwin" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.6-5359777384103936/darwin-arm/cli_mac_arm64.tar.gz";
-      hash = "sha256-GmAxVP6KW0Zii2kSDvwwzsa88r/4ko2BVVpS8BeING4=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.7-5858071034068992/darwin-arm/cli_mac_arm64.tar.gz";
+      hash = "sha256-a5YuF96SZxZbI65Y38bvnHkTYOqrfIpkfkkmRYqgp+I=";
     };
     "x86_64-darwin" = {
-      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.6-5359777384103936/darwin-x64/cli_mac_x64.tar.gz";
-      hash = "sha256-RqjKhRqiW6Fg61eYzem+uXb5LXBYi5Cyv0hwtCaqomo=";
+      url = "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.0.7-5858071034068992/darwin-x64/cli_mac_x64.tar.gz";
+      hash = "sha256-+Qi0tJ+8z3tcbA7EMCQbns+MkjQBBvu4+OT1cF2vuv4=";
     };
   };
 
