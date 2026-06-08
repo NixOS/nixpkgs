a44d6c7cf002bd8a242ad4c1b894c02432a49698
lucasew <lucas59356@gmail.com>
2026-06-07 22:28:01 -0300

    openspec: update pnpm to 10

--- pkgs/by-name/op/openspec/package.nix
+++ pkgs/by-name/op/openspec/package.nix
@@ -3,7 +3,7 @@
   stdenvNoCC,
   fetchFromGitHub,
   nodejs,
-  pnpm_9,
+  pnpm_10,
   fetchPnpmDeps,
   pnpmConfigHook,
   makeWrapper,
@@ -24,15 +24,15 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
-    pnpm = pnpm_9;
-    fetcherVersion = 3;
-    hash = "sha256-9s2kdvd7svK4hofnD66HkDc86WTQeayfF5y7L2dmjNg=";
+    pnpm = pnpm_10;
+    fetcherVersion = 4;
+    hash = "sha256-fF0nhxvNHSJB++NNGRFp44c10IdK2EaGaOGH8qg0Lsc=";
   };
 
   nativeBuildInputs = [
     nodejs
     pnpmConfigHook
-    pnpm_9
+    pnpm_10
     makeWrapper
     installShellFiles
   ];
