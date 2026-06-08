6f9979ce6a256bb8c4ca21be8ff967d49276729a
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-08 12:39:01 +0000

    allure: 2.42.0 -> 2.42.1

--- pkgs/by-name/al/allure/package.nix
+++ pkgs/by-name/al/allure/package.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "allure";
-  version = "2.42.0";
+  version = "2.42.1";
 
   src = fetchurl {
     url = "https://github.com/allure-framework/allure2/releases/download/${finalAttrs.version}/allure-${finalAttrs.version}.tgz";
-    hash = "sha256-cpEtOo/fIH9RTvVbvu4gD2idIZ5PG7S7SaOmeQ7QTf4=";
+    hash = "sha256-+Pc79LvSz1uO7lHSdIeu9VydLyZQ2xEaUvqBCIZrqGw=";
   };
 
   dontConfigure = true;
