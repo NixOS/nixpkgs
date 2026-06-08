4cd58283471a7604545ea53be92af40e267fe9a6
SandaruKasa <sandarukasa@ya.ru>
2026-06-08 21:23:01 +0300

    vscode-extensions.gruntfuggly.todo-tree: modernize

--- pkgs/applications/editors/vscode/extensions/gruntfuggly.todo-tree/default.nix
+++ pkgs/applications/editors/vscode/extensions/gruntfuggly.todo-tree/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   vscode-utils,
+  vscode-extension-update-script,
 }:
 
 vscode-utils.buildVscodeMarketplaceExtension {
@@ -10,7 +11,17 @@ vscode-utils.buildVscodeMarketplaceExtension {
     version = "0.0.226";
     hash = "sha256-Fj9cw+VJ2jkTGUclB1TLvURhzQsaryFQs/+f2RZOLHs=";
   };
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  passthru.updateScript = vscode-extension-update-script { };
+
   meta = {
+    changelog = "https://marketplace.visualstudio.com/items/Gruntfuggly.todo-tree/changelog";
+    description = "Show TODO, FIXME, etc. comment tags in a tree view";
+    downloadPage = "https://marketplace.visualstudio.com/items?itemName=Gruntfuggly.todo-tree";
+    homepage = "https://github.com/Gruntfuggly/todo-tree";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [
       sandarukasa
