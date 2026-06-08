f2b2f00f728c304e5ca62e3c2ddf561800a01a34
ivann <ivann@0xf.re>
2026-06-08 16:03:50 +0200

    koito: init at 0.3.2
    (cherry picked from commit e973b969852edb6ff232f7e80cbf94704cd30554)

---
+++ pkgs/by-name/ko/koito/package.nix
@@ -0,0 +1,60 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  callPackage,
+  pkg-config,
+  vips,
+  makeWrapper,
+}:
+buildGoModule (finalAttrs: {
+  pname = "koito";
+  version = "0.3.2";
+  src = fetchFromGitHub {
+    owner = "gabehf";
+    repo = "koito";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-68+Z4Alzu+4v/PxU1IOboqZkF1pO+y6gswuO+HPS7dk=";
+  };
+  __structuredAttrs = true;
+
+  vendorHash = "sha256-W/+ByBlEPd4yIUD/E28q93fz6wYgvhwyBvJL8Fm1lNY=";
+
+  nativeBuildInputs = [
+    pkg-config
+    makeWrapper
+  ];
+
+  buildInputs = [ vips ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=${finalAttrs.version}"
+  ];
+
+  postInstall = ''
+    mv $out/bin/api $out/bin/.koito-wrapped
+
+    mkdir -p $out/share
+    cp -r --no-preserve=mode ${finalAttrs.passthru.client}/client $out/share/client
+
+    makeWrapper $out/bin/.koito-wrapped $out/bin/koito \
+      --run "cd $out/share"
+  '';
+
+  passthru = {
+    updateScript = ./update.sh;
+    client = callPackage ./client.nix {
+      inherit (finalAttrs) src version;
+    };
+  };
+
+  meta = {
+    description = "Modern, themeable scrobbler that you can use with any program that scrobbles to a custom ListenBrainz URL";
+    homepage = "https://koito.io";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ iv-nn ];
+    mainProgram = "koito";
+  };
+})
