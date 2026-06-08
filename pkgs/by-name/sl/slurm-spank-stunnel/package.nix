2bd77eb5a48ba03b4146ae8e38e1f18f1e9e7e1a
Markus Kowalewski <markus.kowalewski@gmail.com>
2026-06-08 16:08:41 +0200

    slurm-spank-stunnel: cleanup
    * run pre/post hooks in install and build phase
    * replace hostlist patch with upstream patch

--- pkgs/by-name/sl/slurm-spank-stunnel/package.nix
+++ pkgs/by-name/sl/slurm-spank-stunnel/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   slurm,
 }:
 
@@ -17,18 +18,30 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   patches = [
-    ./hostlist.patch
     ./backward-compat.patch
+    (fetchpatch {
+      name = "hostlist_t-signature.patch";
+      url = "https://github.com/stanford-rc/slurm-spank-stunnel/commit/84d04e4ccfe538a09c3f17a52dde616903b66db8.patch";
+      sha256 = "sha256-RaUYqeNmFPvYci2yX8Bxps1nDjX/UAG+e3JbjbcwrO0=";
+    })
   ];
 
   buildPhase = ''
+    runHook preBuild
+
     gcc -I${lib.getDev slurm}/include -shared -fPIC -o stunnel.so slurm-spank-stunnel.c
+
+    runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/lib $out/etc/slurm/plugstack.conf.d
     install -m 755 stunnel.so $out/lib
     install -m 644 plugstack.conf $out/etc/slurm/plugstack.conf.d/stunnel.conf.example
+
+    runHook postInstall
   '';
 
   meta = {
