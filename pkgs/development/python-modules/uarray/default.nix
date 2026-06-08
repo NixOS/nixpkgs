eb4f1c629f97b1ac91b2c6d00289ee0fb1e8797a
Peder Bergebakken Sundt <pbsds@hotmail.com>
2026-06-08 22:48:17 +0200

    python3Packages.uarray: 0.9.3 -> 0.9.4
    Changelog: https://github.com/Quansight-Labs/uarray/releases/tag/0.9.4

--- pkgs/development/python-modules/uarray/default.nix
+++ pkgs/development/python-modules/uarray/default.nix
@@ -2,56 +2,41 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
-  scikit-build-core,
-  setuptools,
-  setuptools-scm,
-  cmake,
-  ninja,
-  matchpy,
-  numpy,
-  typing-extensions,
+  meson-python,
+  versioningit,
+  pkg-config,
   nix-update-script,
   pytestCheckHook,
-  pytest-cov-stub,
 }:
 
 buildPythonPackage rec {
   pname = "uarray";
-  version = "0.9.3";
+  version = "0.9.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "Quansight-Labs";
     repo = "uarray";
     tag = version;
-    hash = "sha256-Nog7jvRG/EFf3n3W2DWC7UML5NyrlaaS0ECis5rtCSk=";
+    hash = "sha256-gCoSpyFPQTIi86y+4xtRb+vsRkjZ2O6KcCj8mj8tcTQ=";
   };
 
+  preBuild = ''
+    echo "__version__ = '$version'" > src/uarray/_version.py
+  '';
+
   build-system = [
-    scikit-build-core
-    setuptools
-    setuptools-scm
-    cmake
-    ninja
+    meson-python
+    versioningit
   ];
 
-  dontUseCmakeConfigure = true;
-
-  dependencies = [
-    matchpy
-    numpy
-    typing-extensions
+  nativeBuildInputs = [
+    pkg-config
   ];
 
   nativeCheckInputs = [
     pytestCheckHook
-    pytest-cov-stub
   ];
-
-  # Tests must be run from outside the source directory
-  preCheck = ''
-    cd $TMP
-  '';
 
   pytestFlags = [
     "--pyargs"
