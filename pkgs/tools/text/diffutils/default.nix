a007159ee157c986b2e6afc3f1d7e455d8f2ec7e
SandaruKasa <sandarukasa@ya.ru>
2026-06-09 22:02:55 +0300

    diffutils: `enableParallelBuilding`

--- pkgs/tools/text/diffutils/default.nix
+++ pkgs/tools/text/diffutils/default.nix
@@ -26,6 +26,8 @@ stdenv.mkDerivation (finalAttrs: {
     "info"
   ];
 
+  enableParallelBuilding = true;
+
   patches = [
     # Fixes test-float-h failure on ppc64 with C23
     # https://lists.gnu.org/archive/html/bug-gnulib/2025-07/msg00021.html
