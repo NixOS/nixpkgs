1a0aebc41bfa932eb4d6d9b3483712d0f4eb04cb
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-09 13:20:06 +0200

    python3Packages.pyxdg: enable most tests

--- pkgs/development/python-modules/pyxdg/default.nix
+++ pkgs/development/python-modules/pyxdg/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitLab,
+  pytestCheckHook,
   setuptools,
 }:
 
@@ -20,12 +21,30 @@ buildPythonPackage (finalAttrs: {
     hash = "sha256-TrFQzfkXabmfpGYwhxD1UVY1F645KycfSPPrMJFAe+0=";
   };
 
+  postPatch = ''
+    substituteInPlace test/test_basedirectory.py \
+      --replace-fail "from imp import reload" "from importlib import reload"
+  '';
+
   build-system = [
     setuptools
   ];
 
-  # Tests failed (errors=4, failures=4) on NixOS
-  doCheck = false;
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  # Disable tests doing MIME type lookups, as those are not installed in the build environment
+  disabledTests = [
+    "test_by_name"
+    "test_canonical"
+    "test_get_type"
+    "test_get_type2"
+    "test_get_type_by_contents"
+    "test_get_type_by_data"
+    "test_get_type_by_name"
+    "test_inheritance"
+  ];
 
   pythonImportsCheck = [ "xdg" ];
 
