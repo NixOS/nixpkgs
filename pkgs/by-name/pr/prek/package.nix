02c36547a4e27c0474de8f6bd32397092f62e69a
OttNorml <2350859+ottnorml@users.noreply.github.com>
2026-06-09 05:37:24 +0200

    prek: 0.3.13 -> 0.4.4
    Diff: https://github.com/j178/prek/compare/v0.3.13...v0.4.4
    
    Changelog: https://github.com/j178/prek/blob/v0.4.4/CHANGELOG.md

--- pkgs/by-name/pr/prek/package.nix
+++ pkgs/by-name/pr/prek/package.nix
@@ -13,16 +13,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "prek";
-  version = "0.3.13";
+  version = "0.4.4";
 
   src = fetchFromGitHub {
     owner = "j178";
     repo = "prek";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-OPH1H571jw0b8EsyzRcVdG54sQ7KeOclmnHCeVGteRI=";
+    hash = "sha256-PAEmRQ5Vro83fkegOWsdY59U7WAQxBPSEalzxZV6K4o=";
   };
 
-  cargoHash = "sha256-lvB09qOvVXmSdhR1TRJQ3rvYyn6cFSOGpogJZDajPuQ=";
+  cargoHash = "sha256-EmlR6Lmt5XR0uS/y3FqY5yGNeVBSdtLtEGH9jZLcP2o=";
 
   nativeBuildInputs = [
     installShellFiles
