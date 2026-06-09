e4afd940a7fd3527950999f9a62321cbe74f91a9
Nico Felbinger <nico@felbinger.eu>
2026-06-09 17:20:39 +0200

    netboxPlugins.netbox-contextmenus: migrate to finalAttrs

--- pkgs/by-name/ne/netbox_4_5/plugins/netbox-contextmenus/package.nix
+++ pkgs/by-name/ne/netbox_4_5/plugins/netbox-contextmenus/package.nix
@@ -7,7 +7,7 @@
   python,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "netbox-contextmenus";
   version = "1.4.14";
   pyproject = true;
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "PieterL75";
     repo = "netbox_contextmenus";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-YqyxZaHKXhMLDdBTAAKQsCBBSXikxBgcOvXEfa6f+0Y=";
   };
 
@@ -27,8 +27,8 @@ buildPythonPackage rec {
   meta = {
     description = "Netbox plugin to add context buttons to the links, making navigating less clicky";
     homepage = "https://github.com/PieterL75/netbox_contextmenus/";
-    changelog = "https://github.com/PieterL75/netbox_contextmenus/releases/tag/${src.tag}";
+    changelog = "https://github.com/PieterL75/netbox_contextmenus/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ felbinger ];
   };
-}
+})
