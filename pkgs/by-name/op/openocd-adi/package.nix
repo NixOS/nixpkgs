5e0c3a209311403b3d7e58d1949786d2f679a371
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-09 13:33:35 +0000

    openocd-adi: 0.12.0-1.3.1-1 -> 0.12.0-1.3.1-2

--- pkgs/by-name/op/openocd-adi/package.nix
+++ pkgs/by-name/op/openocd-adi/package.nix
@@ -7,12 +7,12 @@
 
 openocd.overrideAttrs (old: {
   pname = "openocd-adi";
-  version = "0.12.0-1.3.1-1";
+  version = "0.12.0-1.3.1-2";
   src = fetchFromGitHub {
     owner = "analogdevicesinc";
     repo = "openocd";
-    tag = "0.12.0-1.3.1-1";
-    hash = "sha256-e25mAxUmbF/hZC+aWRMt9HdwKY0FClNrZXwP3888Z9A=";
+    tag = "0.12.0-1.3.1-2";
+    hash = "sha256-MqpVZN6+kcu1bspwcCDOnydQ5tC+MtO4D35KmQFRm1o=";
     # openocd disables the vendored libraries that use submodules and replaces them with nix versions.
     # this works out as one of the submodule sources seems to be flakey.
     fetchSubmodules = false;
