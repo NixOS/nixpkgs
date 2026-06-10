bb76f2a33e158a0cb4649026cdc132dc5f580bb1
Marc Zweiler <marc.zweiler@outlook.at>
2026-06-10 20:50:27 +0200

    fip-c: init at 0.3.2

---
+++ pkgs/by-name/fi/fip-c/package.nix
@@ -0,0 +1,33 @@
+{ stdenv, fetchurl, autoPatchelfHook, lib }:
+
+stdenv.mkDerivation rec {
+  pname = "fip-c";
+  version = "0.3.2";
+
+  fipCSrc = fetchurl {
+    url = "https://github.com/flint-lang/fip/releases/download/v${version}/fip-c";
+    sha256 = "587441dcba543e44ed01c3435f8b7c7f6b5e07e438a73ceab0a331e140f67faa";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  dontUnpack = true;
+  dontPatch = true;
+  dontConfigure = true;
+  dontBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 $fipCSrc $out/bin/fip-c
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "C Interop Module utilizing the Flint Interop Protocol";
+    homepage = "https://github.com/flint-lang/fip";
+    license = licenses.mit;
+    platforms = [ "x86_64-linux" ];
+    maintainers = with maintainers; [ zweiler1 ];
+    mainProgram = "fip-c";
+  };
+}
