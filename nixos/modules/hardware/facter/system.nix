bd3b39f99f12bd6826bbeae604f091da9a67161a
Bruno Adelé <brunoadele@gmail.com>
2026-06-10 08:34:11 +0200

    nixos/facter: fix hardware/facter/system.nix linting

--- nixos/modules/hardware/facter/system.nix
+++ nixos/modules/hardware/facter/system.nix
@@ -9,9 +9,7 @@
     let
       detectedSystem = config.hardware.facter.report.system or null;
       canSetHostPlatform =
-        detectedSystem != null
-        && !config.boot.isContainer
-        && !options.nixpkgs.pkgs.isDefined;
+        detectedSystem != null && !config.boot.isContainer && !options.nixpkgs.pkgs.isDefined;
     in
     lib.mkIf canSetHostPlatform {
       nixpkgs.hostPlatform = lib.mkDefault detectedSystem;
