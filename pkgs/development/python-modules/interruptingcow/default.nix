059d6f3b00d61728af72fcf88582ebbab8cf2d90
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-10 15:08:55 +0200

    python3Packages.interruptingcow: enable __structuredAttrs

--- pkgs/development/python-modules/interruptingcow/default.nix
+++ pkgs/development/python-modules/interruptingcow/default.nix
@@ -8,6 +8,8 @@ buildPythonPackage (finalAttrs: {
   version = "0.8";
   format = "setuptools";
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
     inherit (finalAttrs) pname version;
     sha256 = "3e8cd5058b651e625702cba53e3b1fb76d7a5ec07ab69c52a167a9f784e3306c";
