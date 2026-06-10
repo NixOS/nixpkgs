06cc3b00504df267452bdfa9769a407064a9a8b5
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-10 20:47:26 +0200

    python3Packages.manuel: remove unneeded patch and dependency

--- pkgs/development/python-modules/manuel/default.nix
+++ pkgs/development/python-modules/manuel/default.nix
@@ -2,10 +2,7 @@
   lib,
   buildPythonPackage,
   fetchPypi,
-  fetchpatch,
-  python,
   setuptools,
-  six,
   zope-testing,
 }:
 
@@ -21,19 +18,7 @@ buildPythonPackage (finalAttrs: {
     hash = "sha256-XWMSDej6bZJ3gLaa4oqj6dFmmxCvPTJ4Xz+6EaW+iFo=";
   };
 
-  patches = lib.optionals (lib.versionAtLeast python.version "3.11") [
-    # https://github.com/benji-york/manuel/pull/32
-    # Applying conditionally until upstream arrives at some general solution.
-    (fetchpatch {
-      name = "TextTestResult-python311.patch";
-      url = "https://github.com/benji-york/manuel/commit/d9f12d03e39bb76e4bb3ba43ad51af6d3e9d45c0.diff";
-      hash = "sha256-k0vBtxEixoI1INiKtc7Js3Ai00iGAcCvCFI1ZIBRPvQ=";
-    })
-  ];
-
   build-system = [ setuptools ];
-
-  dependencies = [ six ];
 
   nativeCheckInputs = [ zope-testing ];
 
