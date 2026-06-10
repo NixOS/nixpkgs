332891d130f6a05d4306439f015e6310d0f373fe
Bruno Adelé <brunoadele@gmail.com>
2026-06-10 17:01:01 +0200

    nixos/facter: roolback skip hostPlatform when the option is read-only

--- nixos/modules/hardware/facter/system.nix
+++ nixos/modules/hardware/facter/system.nix
@@ -1,3 +1,20 @@
+# {
+#   config,
+#   options,
+#   lib,
+#   ...
+# }:
+# {
+#   config =
+#     let
+#       detectedSystem = config.hardware.facter.report.system or null;
+#       hasExternalPkgs = options.nixpkgs.pkgs.isDefined || (config.nixpkgs ? pkgs);
+#       canSetHostPlatform = detectedSystem != null && !config.boot.isContainer && !hasExternalPkgs;
+#     in
+#     lib.mkIf canSetHostPlatform {
+#       nixpkgs.hostPlatform = lib.mkDefault detectedSystem;
+#     };
+# }
 {
   config,
   options,
@@ -5,13 +22,12 @@
   ...
 }:
 {
-  config =
+  config.nixpkgs =
     let
       detectedSystem = config.hardware.facter.report.system or null;
-      hasExternalPkgs = options.nixpkgs.pkgs.isDefined || (config.nixpkgs ? pkgs);
-      canSetHostPlatform = detectedSystem != null && !config.boot.isContainer && !hasExternalPkgs;
+      canSetHostPlatform = detectedSystem != null && !(options.nixpkgs.hostPlatform.readOnly or false);
     in
     lib.mkIf canSetHostPlatform {
-      nixpkgs.hostPlatform = lib.mkDefault detectedSystem;
+      hostPlatform = lib.mkDefault detectedSystem;
     };
 }
