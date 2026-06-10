4d920c0d9adcea3599d0ceeee26a95bcbac446f2
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-10 15:06:37 +0200

    python3Packages.interruptingcow: use finalAttrs

--- pkgs/development/python-modules/interruptingcow/default.nix
+++ pkgs/development/python-modules/interruptingcow/default.nix
@@ -3,13 +3,13 @@
   buildPythonPackage,
   fetchPypi,
 }:
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "interruptingcow";
   version = "0.8";
   format = "setuptools";
 
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     sha256 = "3e8cd5058b651e625702cba53e3b1fb76d7a5ec07ab69c52a167a9f784e3306c";
   };
 
@@ -19,4 +19,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ benley ];
   };
-}
+})
