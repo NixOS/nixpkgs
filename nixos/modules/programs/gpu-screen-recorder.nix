2c9bfd0725ea61a20f3eece256af13cd936f390a
mowmdown <mowmdown@git.plexraid.stream>
2026-06-09 20:44:36 -0400

    module: update gpu-screen-recorder.nix to support gsr-ui

--- nixos/modules/programs/gpu-screen-recorder.nix
+++ nixos/modules/programs/gpu-screen-recorder.nix
@@ -4,10 +4,13 @@
   pkgs,
   ...
 }:
-
 let
   cfg = config.programs.gpu-screen-recorder;
+  uiCfg = config.programs.gpu-screen-recorder-ui;
   package = cfg.package.override {
+    inherit (config.security) wrapperDir;
+  };
+  uiPackage = uiCfg.package.override {
     inherit (config.security) wrapperDir;
   };
 in
@@ -15,7 +18,6 @@ in
   options = {
     programs.gpu-screen-recorder = {
       package = lib.mkPackageOption pkgs "gpu-screen-recorder" { };
-
       enable = lib.mkOption {
         type = lib.types.bool;
         default = false;
@@ -25,18 +27,41 @@ in
         '';
       };
     };
-  };
-
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ cfg.package ];
-
-    security.wrappers."gsr-kms-server" = {
-      owner = "root";
-      group = "root";
-      capabilities = "cap_sys_admin+ep";
-      source = lib.getExe' package "gsr-kms-server";
+    programs.gpu-screen-recorder-ui = {
+      package = lib.mkPackageOption pkgs "gpu-screen-recorder-ui" { };
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          Whether to install gpu-screen-recorder-ui and generate setcap
+          wrappers for global hotkeys. Enabling this also installs
+          gpu-screen-recorder and its wrappers automatically.
+        '';
+      };
     };
   };
-
-  meta.maintainers = with lib.maintainers; [ timschumi ];
+  config = lib.mkMerge [
+    (lib.mkIf (cfg.enable || uiCfg.enable) {
+      environment.systemPackages = [ cfg.package ];
+      security.wrappers."gsr-kms-server" = {
+        owner = "root";
+        group = "root";
+        capabilities = "cap_sys_admin+ep";
+        source = lib.getExe' package "gsr-kms-server";
+      };
+    })
+    (lib.mkIf uiCfg.enable {
+      environment.systemPackages = [ uiPackage ];
+      security.wrappers."gsr-global-hotkeys" = {
+        owner = "root";
+        group = "root";
+        capabilities = "cap_setuid+ep";
+        source = lib.getExe' uiPackage "gsr-global-hotkeys";
+      };
+    })
+  ];
+  meta.maintainers = with lib.maintainers; [
+    timschumi
+    mowmdown
+  ];
 }
