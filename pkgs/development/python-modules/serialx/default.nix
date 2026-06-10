23325efa0f4e0c51a53832c799218691c27bb95a
Robert Schütz <nix@dotlambda.de>
2026-06-10 07:32:06 -0700

    python3Packages.serialx: 1.8.0 -> 1.8.1
    Diff: https://github.com/puddly/serialx/compare/v1.8.0...v1.8.1
    
    Changelog: https://github.com/puddly/serialx/releases/tag/v1.8.1

--- pkgs/development/python-modules/serialx/default.nix
+++ pkgs/development/python-modules/serialx/default.nix
@@ -20,14 +20,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "serialx";
-  version = "1.8.0";
+  version = "1.8.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "puddly";
     repo = "serialx";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-JNaS7nRzqNr6y+Qj8bG4U0vwtvfNmllr9vpS4IcSjV4=";
+    hash = "sha256-89lRB96lit4XxPhACnZ3Lv01G0IcddlwyPTttrWwsLQ=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
