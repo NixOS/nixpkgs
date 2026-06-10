39e3309f6397eacfabac5b7ddb77e439756c0860
Timo Gottszky <timogottszky+git@gmail.com>
2026-06-10 19:10:33 +0200

    python3Packages.ssdeep: use finalAttrs

--- pkgs/development/python-modules/ssdeep/default.nix
+++ pkgs/development/python-modules/ssdeep/default.nix
@@ -8,7 +8,7 @@
   ssdeep,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "ssdeep";
   version = "3.4.1";
   format = "setuptools";
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "DinoTools";
     repo = "python-ssdeep";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-I5ci5BS+B3OE0xdLSahu3HCh99jjhnRHJFz830SvFpg=";
   };
 
@@ -39,8 +39,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python wrapper for the ssdeep library";
     homepage = "https://github.com/DinoTools/python-ssdeep";
-    changelog = "https://github.com/DinoTools/python-ssdeep/blob/${version}/CHANGELOG.rst";
+    changelog = "https://github.com/DinoTools/python-ssdeep/blob/${finalAttrs.version}/CHANGELOG.rst";
     license = lib.licenses.lgpl3Plus;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
