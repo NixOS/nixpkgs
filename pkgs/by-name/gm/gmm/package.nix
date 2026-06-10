71f44c701e13783bd67a2b3b33d04c7de1ede2aa
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-10 12:30:42 +0000

    gmm: 5.4.4 -> 5.5

--- pkgs/by-name/gm/gmm/package.nix
+++ pkgs/by-name/gm/gmm/package.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gmm";
-  version = "5.4.4";
+  version = "5.5";
 
   src = fetchurl {
     url = "mirror://savannah/getfem/stable/gmm-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-FesZQwEbkmZaqzsC7PPO3hz4nqFakAb4HyuizWYqoCs=";
+    sha256 = "sha256-SaXoHP+JuHEZB1YI2V2TfcAnGmKDnm1N/E1SOPXAAQI=";
   };
 
   meta = {
