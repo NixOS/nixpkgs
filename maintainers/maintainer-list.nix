1a2262dcabb8d7759a5c4aa07fdc2a15559b1f64
Sir0ne <vivian@hausofwong.com>
2026-06-10 18:57:47 +1000

    fcitx5-ilo-sitelen: init at 0-unstable-2023-02-14

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -28068,6 +28068,11 @@
     githubId = 9853194;
     name = "Philipp Bartsch";
   };
+  toadhog = {
+    name = "ToadHog";
+    github = "Sir0ne";
+    githubId = 96101308;
+  };
   toast = {
     name = "Toast";
     github = "toast003";
