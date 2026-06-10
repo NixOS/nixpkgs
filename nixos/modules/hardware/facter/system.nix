5cee79596227442dbec0b2ea60ed8e5db9c48db2
Bruno Adelé <brunoadele@gmail.com>
2026-06-10 08:52:15 +0200

    nixos/facter: skip hostPlatform when the option is read-only v3

--- nixos/modules/hardware/facter/system.nix
+++ nixos/modules/hardware/facter/system.nix
@@ -9,7 +9,10 @@
     let
       detectedSystem = config.hardware.facter.report.system or null;
       canSetHostPlatform =
-        detectedSystem != null && !config.boot.isContainer && !options.nixpkgs.pkgs.isDefined;
+        detectedSystem != null
+        && !config.boot.isContainer
+        && !options.nixpkgs.pkgs.isDefined
+        && !(options.nixpkgs.hostPlatform.readOnly or false);
     in
     lib.mkIf canSetHostPlatform {
       nixpkgs.hostPlatform = lib.mkDefault detectedSystem;
