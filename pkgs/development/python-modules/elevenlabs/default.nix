dc177496ef04e9b940ef57e003c3622c099b50e8
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-11 10:12:55 +0000

    python3Packages.elevenlabs: 2.50.0 -> 2.52.0

--- pkgs/development/python-modules/elevenlabs/default.nix
+++ pkgs/development/python-modules/elevenlabs/default.nix
@@ -14,14 +14,14 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "elevenlabs";
-  version = "2.50.0";
+  version = "2.52.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "elevenlabs";
     repo = "elevenlabs-python";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-OIkELIVPXjSqp9L7GqWwodLUrXpHa6GYKSeHF0fbhIk=";
+    hash = "sha256-Acqwa1zmDV4+xedGtabtB/utU4D5WkgbLyYCrmZiwFo=";
   };
 
   build-system = [ poetry-core ];
