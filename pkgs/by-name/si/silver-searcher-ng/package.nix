67919be717e87cfea39f4d3010a9f05c3a3de0bb
Tim Schumacher <timschumi@gmx.de>
2026-06-12 01:06:38 +0200

    silver-searcher-ng: 2.2.0 -> 3.0.0

--- pkgs/by-name/si/silver-searcher-ng/package.nix
+++ pkgs/by-name/si/silver-searcher-ng/package.nix
@@ -4,33 +4,25 @@
   fetchFromGitHub,
   autoreconfHook,
   pkg-config,
-  pcre,
+  pcre2,
   zlib,
   xz,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "silver-searcher-ng";
-  version = "2.2.0";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "silver-searcher";
     repo = "silver-searcher-ng";
     rev = finalAttrs.version;
-    sha256 = "0cyazh7a66pgcabijd27xnk1alhsccywivv6yihw378dqxb22i1p";
+    hash = "sha256-IiVFbS9XGmqcGN4NRXFC07cV6bGKDs9C2y5XxJKdvFk=";
   };
 
   patches = [ ./bash-completion.patch ];
 
-  env = {
-    # Workaround build failure on -fno-common toolchains like upstream
-    # gcc-10. Otherwise build fails as:
-    #   ld: src/zfile.o:/build/source/src/log.h:12: multiple definition of
-    #     `print_mtx'; src/ignore.o:/build/source/src/log.h:12: first defined here
-    # TODO: remove once next release has https://github.com/ggreer/the_silver_searcher/pull/1377
-    NIX_CFLAGS_COMPILE = "-fcommon";
-  }
-  // lib.optionalAttrs stdenv.hostPlatform.isLinux {
+  env = lib.optionalAttrs stdenv.hostPlatform.isLinux {
     NIX_LDFLAGS = "-lgcc_s";
   };
 
@@ -39,7 +31,7 @@ stdenv.mkDerivation (finalAttrs: {
     pkg-config
   ];
   buildInputs = [
-    pcre
+    pcre2
     zlib
     xz
   ];
