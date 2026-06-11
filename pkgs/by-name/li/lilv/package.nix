f7876e60c7afdab2f50f2686c94e2ec507861cce
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-11 06:53:15 +0000

    lilv: 0.26.4 -> 0.28.0

--- pkgs/by-name/li/lilv/package.nix
+++ pkgs/by-name/li/lilv/package.nix
@@ -19,7 +19,7 @@
 
 stdenv.mkDerivation rec {
   pname = "lilv";
-  version = "0.26.4";
+  version = "0.28.0";
 
   outputs = [
     "out"
@@ -29,7 +29,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "https://download.drobilla.net/lilv-${version}.tar.xz";
-    hash = "sha256-HItfy3hxgXPmfXblGtQj9RE6n/aEY/JWYZWuRjlgieM=";
+    hash = "sha256-jctwrbXPByM1EVprCR9BE3EL3HOrqtqj+enB5VlXsUk=";
   };
 
   nativeBuildInputs = [
