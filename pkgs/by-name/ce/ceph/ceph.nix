d8d39bd436acf52676faaf4803fafa850ba58f9b
Niklas Hambüchen <mail@nh2.me>
2026-06-11 19:33:32 +0000

    ceph.tests: Rename FileStore tests to *-deprecated-filestore
    Makes clear that there's not much point contributing new features to those.

--- pkgs/by-name/ce/ceph/ceph.nix
+++ pkgs/by-name/ce/ceph/ceph.nix
@@ -393,10 +393,10 @@ stdenv.mkDerivation {
     pythonEnv = ceph-python-env;
     tests = {
       inherit (nixosTests)
-        ceph-multi-node
-        ceph-single-node
+        ceph-multi-node-deprecated-filestore
         ceph-single-node-bluestore
         ceph-single-node-bluestore-dmcrypt
+        ceph-single-node-deprecated-filestore
         ;
     };
   };
