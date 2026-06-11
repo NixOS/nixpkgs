19d5f3137ee00a060f3e546262555ec5663d166a
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-11 16:07:27 +0200

    python3Packages.selenium-wire-roadtx: init at 0-unstable-2026-05-20
    Extends Selenium's Python bindings to give you the ability to inspect
    requests made by the browser
    
    https://github.com/dirkjanm/selenium-wire-roadtx

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17810,6 +17810,8 @@ self: super: with self; {
 
   selenium-wire = callPackage ../development/python-modules/selenium-wire { };
 
+  selenium-wire-roadtx = callPackage ../development/python-modules/selenium-wire-roadtx { };
+
   semantic-version = callPackage ../development/python-modules/semantic-version { };
 
   semaphore-bot = callPackage ../development/python-modules/semaphore-bot { };
