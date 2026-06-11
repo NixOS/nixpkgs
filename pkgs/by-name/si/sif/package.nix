465833524f2ae3b012e16a67ca4cf089479a0f69
celeste <celeste@collar.sh>
2026-06-10 19:19:33 -0700

    sif: 0-unstable-2026-06-09 -> 0-unstable-2026-06-11

--- pkgs/by-name/si/sif/package.nix
+++ pkgs/by-name/si/sif/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule {
   pname = "sif";
-  version = "0-unstable-2026-06-09";
+  version = "0-unstable-2026-06-11";
 
   src = fetchFromGitHub {
     owner = "vmfunc";
     repo = "sif";
-    rev = "83ac92a4b82a0ab92257c580c9b6a3b82ab66af9";
-    hash = "sha256-VeURSRwvuh+VJd94mG2F8wQWD6NIitxqwRQr3IJ0QzU=";
+    rev = "d62919523abfecd06e07ba6528b15e9861bd747c";
+    hash = "sha256-T/HIvcXG3OpSK7xhZpYnCWv4KsRn0bnLhyouPjgwUoE=";
   };
 
-  vendorHash = "sha256-fR63/dStMsZon22vancuLWIAvZiEYMLjMwY1kmRDNgM=";
+  vendorHash = "sha256-rOAubGbeDPl0LJovksKRfYJmUvU6hmx3Ht12M7eLiOA=";
 
   subPackages = [ "cmd/sif" ];
 
