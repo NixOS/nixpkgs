5212d05330add553dad7c77f49033e407c549efc
kyehn <kyehn@users.noreply.github.com>
2026-06-11 14:40:30 +0000

    valentina: 0.7.53 -> 1.0.1

--- pkgs/by-name/va/valentina/package.nix
+++ pkgs/by-name/va/valentina/package.nix
@@ -1,61 +1,96 @@
 {
   lib,
   stdenv,
+  libGL,
+  xercesc,
   fetchFromGitLab,
-  installShellFiles,
-  libsForQt5,
-  autoPatchelfHook,
+  qbs,
+  qt6,
 }:
 
-stdenv.mkDerivation rec {
+let
+  qtEnv = qt6.env "valentina-qt-env" (
+    with qt6;
+    [
+      qtsvg
+      qttools
+    ]
+  );
+in
+
+stdenv.mkDerivation (finalAttrs: {
   pname = "valentina";
-  version = "0.7.53";
+  version = "1.0.1";
 
   src = fetchFromGitLab {
     owner = "smart-pattern";
     repo = "valentina";
-    rev = "v${version}";
-    hash = "sha256-vIlqrK7wyFaXKfvcJ3FtkAwUt6Xb/47qxcDGy1Ty2uk=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-ob7dYqulC3xqtB3L38T6N0DsnDep53sIThBW7ZLpDeE=";
   };
 
   postPatch = ''
-    substituteInPlace src/app/translations.pri \
-      --replace '$$[QT_INSTALL_BINS]/$$LRELEASE' '${lib.getDev libsForQt5.qttools}/bin/lrelease'
+    mkdir -p qbs/modules/xerces-c
+    cat > qbs/modules/xerces-c/xerces-c.qbs << EOF
+    Module {
+        Depends { name: "cpp" }
+        cpp.includePaths: ["${xercesc}/include"]
+        cpp.libraryPaths: ["${xercesc}/lib"]
+        cpp.dynamicLibraries: ["xerces-c"]
+    }
+    EOF
   '';
 
+  enableParallelBuilding = true;
+
   nativeBuildInputs = [
-    libsForQt5.qmake
-    libsForQt5.qttools
-    libsForQt5.wrapQtAppsHook
-    installShellFiles
-    autoPatchelfHook
+    qbs
+    qt6.wrapQtAppsHook
   ];
 
   buildInputs = [
-    libsForQt5.qtsvg
-    libsForQt5.qtxmlpatterns
+    qtEnv
+    qt6.qtbase
+    libGL
+    xercesc
   ];
 
-  qmakeFlags = [
-    "-r"
-    "PREFIX=${placeholder "out"}"
-    "CONFIG+=noTests"
-    "CONFIG+=noRunPath"
-    "CONFIG+=no_ccache"
-    "CONFIG+=noDebugSymbols"
-  ];
+  strictDeps = true;
 
-  postInstall = ''
-    installManPage dist/debian/*.1
-    install -Dm644 dist/debian/valentina.sharedmimeinfo $out/share/mime/packages/valentina.xml
+  configurePhase = ''
+    runHook preConfigure
+
+    qbs setup-qt --settings-dir . ${qtEnv}/bin/qmake qtenv
+    qbs config --settings-dir . defaultProfile qtenv
+    qbs resolve --file valentina.qbs --settings-dir . config:release qbs.installPrefix:/ \
+      modules.buildconfig.enableUnitTests:false modules.buildconfig.enableCcache:false \
+      modules.buildconfig.enableRPath:false modules.buildconfig.treatWarningsAsErrors:false
+
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    qbs build --file valentina.qbs --settings-dir . config:release -j $NIX_BUILD_CORES
+
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    qbs install --file valentina.qbs --settings-dir . --install-root $out config:release
+
+    runHook postInstall
   '';
 
   meta = {
     description = "Open source sewing pattern drafting software";
     homepage = "https://smart-pattern.com.ua/";
-    changelog = "https://gitlab.com/smart-pattern/valentina/-/blob/v${version}/ChangeLog.txt";
+    changelog = "https://gitlab.com/smart-pattern/valentina/-/blob/v${finalAttrs.version}/ChangeLog.txt";
     license = lib.licenses.gpl3Plus;
     platforms = lib.platforms.linux;
     maintainers = [ ];
   };
-}
+})
