9ddd2949a16d0b9dc25af47d3bf1d40a3bd911a8
PadowYT2 <me@padow.ru>
2026-06-12 16:05:29 +0300

    maintainers: add padowyt2

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21138,6 +21138,12 @@
     githubId = 16345849;
     name = "Parthiv Seetharaman";
   };
+  padowyt2 = {
+    email = "me@padow.ru";
+    github = "PadowYT2";
+    githubId = 71085027;
+    name = "PadowYT2";
+  };
   paepcke = {
     email = "git@paepcke.de";
     github = "paepckehh";
