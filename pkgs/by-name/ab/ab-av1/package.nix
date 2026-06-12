1dc52d429994001f5c1ef1fb4228900ebaaac871
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-12 13:32:57 +0000

    ab-av1: 0.11.2 -> 0.11.3

--- pkgs/by-name/ab/ab-av1/package.nix
+++ pkgs/by-name/ab/ab-av1/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "ab-av1";
-  version = "0.11.2";
+  version = "0.11.3";
 
   src = fetchFromGitHub {
     owner = "alexheretic";
     repo = "ab-av1";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-kT0BJr4xoYigKObD6vZNRieslaJj/MWuqQmhYLWNwvs=";
+    hash = "sha256-lLZAECwF8V19Qx/FugbjLeVns7lhVlwWDTK9cdYb0xo=";
   };
 
-  cargoHash = "sha256-o64GrmHvwF2VUKw+nIRK0jesSuelxNo0TFnyKaP/YlQ=";
+  cargoHash = "sha256-AONJz1BoDi6weHT7W9DmzwoPW5khfgYjDqLNl7OM5bY=";
 
   nativeBuildInputs = [ installShellFiles ];
 
