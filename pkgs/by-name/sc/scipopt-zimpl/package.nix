e3656373447f95f46b08748497993af944b68530
pmeinhold <paulmatti@protonmail.com>
2026-06-12 12:47:30 +0200

    scipopt-zimpl: 3.7.0 -> 3.7.1

--- pkgs/by-name/sc/scipopt-zimpl/package.nix
+++ pkgs/by-name/sc/scipopt-zimpl/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "scipopt-zimpl";
-  version = "3.7.0";
+  version = "3.7.1";
 
   src = fetchFromGitHub {
     owner = "scipopt";
     repo = "zimpl";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ataepqBfdA7CgqPhbw+Xy7PC3VZTLcSrF2/xnFyx+YI=";
+    hash = "sha256-CUWLI12rH+NggQph+qWmWCTtbQubnhFKanuMlADLiHs=";
   };
 
   postPatch = ''
@@ -50,22 +50,10 @@ stdenv.mkDerivation (finalAttrs: {
     changelog = "https://zimpl.zib.de/download/CHANGELOG.txt";
     description = "Zuse Institute Mathematical Programming Language";
     longDescription = ''
-      ZIMPL is a little language to translate the mathematical model of a
-      problem into a linear or (mixed-)integer mathematical program
-      expressed in .lp or .mps file format which can be read by a LP or MIP
-      solver.
-
-      If you use Zimpl for research and publish results, the best way
-      to refer to Zimpl is to cite my Ph.d. thesis:
-
-      @PHDTHESIS{Koch2004,
-         author      = "Thorsten Koch",
-         title       = "Rapid Mathematical Programming",
-         year        = "2004",
-         school      = "Technische {Universit\"at} Berlin",
-         url         = "http://www.zib.de/Publications/abstracts/ZR-04-58/",
-         note        = "ZIB-Report 04-58",
-      }
+      Zimpl is a little language to translate the mathematical model of a
+      problem into a linear or nonlinear (mixed-)integer mathematical
+      program expressed in .lp or .mps file format which can be read and
+      (hopefully) solved by a LP or MIP solver.
     '';
     license = lib.licenses.lgpl3Plus;
     homepage = "https://zimpl.zib.de";
