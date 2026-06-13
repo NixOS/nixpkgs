e5b2fdc1d36d8cc96604a201c03b9b0900da255c
Gaetan Lepage <gaetan@glepage.com>
2026-06-13 19:15:59 +0000

    python3Packages.pytensor: 3.0.3 -> 3.0.5
    Diff: https://github.com/pymc-devs/pytensor/compare/rel-3.0.3...rel-3.0.5
    
    Changelog: https://github.com/pymc-devs/pytensor/releases/tag/rel-3.0.5

--- pkgs/development/python-modules/pytensor/default.nix
+++ pkgs/development/python-modules/pytensor/default.nix
@@ -33,7 +33,7 @@
 
 buildPythonPackage (finalAttrs: {
   pname = "pytensor";
-  version = "3.0.3";
+  version = "3.0.5";
   pyproject = true;
   __structuredAttrs = true;
 
@@ -44,7 +44,7 @@ buildPythonPackage (finalAttrs: {
     postFetch = ''
       sed -i 's/git_refnames = "[^"]*"/git_refnames = " (tag: ${finalAttrs.src.tag})"/' $out/pytensor/_version.py
     '';
-    hash = "sha256-j4fD2pS5po2XKgo4SuLO1wuUeULubqnGhhWVsbhvrOw=";
+    hash = "sha256-99AjktuPPn55ZLPKCv4qdE1lgy9p+NBuaq0vZMVhC50=";
   };
 
   build-system = [
