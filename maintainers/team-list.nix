f0fc7aee9c682906ef9f65fcab806233def3c0c7
Hythera <87016780+Hythera@users.noreply.github.com>
2026-06-13 14:01:45 +0200

    maintainers: remove aherrmann

--- maintainers/team-list.nix
+++ maintainers/team-list.nix
@@ -70,7 +70,6 @@ with lib.maintainers;
     members = [
       cbley
       groodt
-      aherrmann
       boltzmannrain
     ];
     scope = "Bazel build tool & related tools https://bazel.build/";
