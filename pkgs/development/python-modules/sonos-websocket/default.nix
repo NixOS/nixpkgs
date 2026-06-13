d88c6ea64d4bf9553dbdb6d68c41ebfe4a59524d
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-13 15:07:33 +0200

    python3Packages.sonos-websocket: 0.1.3 -> 0.2.0
    Diff: https://github.com/jjlawren/sonos-websocket/compare/0.1.3...0.2.0
    
    Changelog: https://github.com/jjlawren/sonos-websocket/releases/tag/0.2.0

--- pkgs/development/python-modules/sonos-websocket/default.nix
+++ pkgs/development/python-modules/sonos-websocket/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "sonos-websocket";
-  version = "0.1.3";
+  version = "0.2.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "jjlawren";
     repo = "sonos-websocket";
     tag = version;
-    hash = "sha256-1sgYLwIW7VWnHJGsfIQ95AGZ5j/DPMKQr5n7F+/MsuY=";
+    hash = "sha256-Yf6osDW7+PYalj3bK7+wF2RotCVlcLJD1ulqLcxrdMU=";
   };
 
   build-system = [ setuptools ];
