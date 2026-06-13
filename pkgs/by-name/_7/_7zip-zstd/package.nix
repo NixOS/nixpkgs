791e65138ad8e1cdc6daee0f8cc903649174df5f
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-13 07:25:39 +0000

    _7zip-zstd: 25.01-v1.5.7-R4 -> 26.01-v1.5.7-R1

--- pkgs/by-name/_7/_7zip-zstd/package.nix
+++ pkgs/by-name/_7/_7zip-zstd/package.nix
@@ -16,7 +16,7 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "7zip-zstd";
-  version = "25.01-v1.5.7-R4";
+  version = "26.01-v1.5.7-R1";
 
   src = fetchFromGitHub {
     owner = "mcmilk";
@@ -24,9 +24,9 @@ stdenv.mkDerivation (finalAttrs: {
     tag = "v${finalAttrs.version}";
     hash =
       if enableUnfree then
-        "sha256-qP4L5PIG7CHsmYbRock+cbCOGdgujUFG4LHenvvlqzw="
+        "sha256-P6kwg3McroMwqB5bryfoS9dIBsxcd84bVzsjUv5Bk5k="
       else
-        "sha256-R9AUWL35TPh0anyRDhnF28ZYG9FeOxntVIwnnW9e2xA=";
+        "sha256-WowqcjcA/DUwj7DJrKDTYKpCDWd/tTIZ3yiuBgqXpmU=";
     # remove the unRAR related code from the src drv
     # > the license requires that you agree to these use restrictions,
     # > or you must remove the software (source and binary) from your hard disks
