f43234842cfbcedc8a19e7de81740eedb17aef16
Doron Behar <doron.behar@gmail.com>
2026-06-14 18:14:51 +0300

    pizauth: fix systemd feature
    Although we had `systemd` included in `buildFeatures`, it didn't reach
    `$out/bin/pizauth`, because upstream's Makefile used
    `target/release/pizauth` as a Makefile target that `cargo build
    --release` (with or without `--features=systemd`) solves.
    
    (cherry picked from commit 583024221d3a8e5f0648c0e834328b2836d2aed0)

--- pkgs/by-name/pi/pizauth/package.nix
+++ pkgs/by-name/pi/pizauth/package.nix
@@ -22,6 +22,14 @@ rustPlatform.buildRustPackage (finalAttrs: {
   preConfigure = ''
     substituteInPlace lib/systemd/user/pizauth.service \
       --replace-fail /usr/bin/ ''${!outputBin}/bin/
+    # Upstream's Makefile uses target/release/pizauth as a Makefile target that
+    # the `install` target depends upon. Nixpkgs' cargoBuildHook defaults to
+    # using the explicit `--target @rustcTargetSpec@` flag, so that the
+    # executable always ends up in
+    # `target/${stdenv.hostPlatform.rust.rustcTargetSpec}/release`. To make the
+    # Makefile not run cargo build again, we use this substitution.
+    substituteInPlace Makefile \
+      --replace-fail target/release target/${stdenv.hostPlatform.rust.rustcTargetSpec}/release
   '';
 
   postInstall = ''
