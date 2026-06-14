db103d9039383dabb34aa56cea364f98216c5207
Yueh-Shun Li <shamrocklee@posteo.net>
2026-06-15 04:21:11 +0800

    fetchFromGitProvider: abstract out browsableUrl, archiveUrl and gitRepoUrl

--- pkgs/build-support/fetchgithub/default.nix
+++ pkgs/build-support/fetchgithub/default.nix
@@ -16,6 +16,10 @@ lib.makeOverridable (
         githubBase ? "github.com",
         ...
       }:
+      let
+        baseUrl = "https://${finalAttrs.domain}/${finalAttrs.owner}/${finalAttrs.repo}";
+        revWithTag = finalAttrs.rev;
+      in
       {
         inherit
           providerName
@@ -28,6 +32,25 @@ lib.makeOverridable (
             "api.github.com"
           else
             finalAttrs.domain;
+
+        archiveUrl =
+          # Use the API endpoint for private repos, as the archive URI doesn't
+          # support access with GitHub's fine-grained access tokens.
+          #
+          # Use the archive URI for non-private repos, as the API endpoint has
+          # relatively restrictive rate limits for unauthenticated users.
+          if finalAttrs.private then
+            let
+              endpoint = "/repos/${finalAttrs.owner}/${finalAttrs.repo}/tarball/${revWithTag}";
+            in
+            if finalAttrs.domain == "github.com" then
+              "https://api.github.com${endpoint}"
+            else
+              "https://${finalAttrs.domain}/api/v3${endpoint}"
+          else
+            "${baseUrl}/archive/${revWithTag}.tar.gz";
+
+        browsableUrl = baseUrl;
 
         derivationArgs = {
           inherit githubBase;
--- pkgs/build-support/fetchgitprovider/default.nix
+++ pkgs/build-support/fetchgitprovider/default.nix
@@ -67,6 +67,9 @@ lib.extendMkDerivation rec {
     "netrcMachineName"
     "varBase"
     "varPrefix"
+    "browsableUrl"
+    "archiveUrl"
+    "gitRepoUrl"
     "derivationArgs"
   ]
   ++ (lib.attrNames faUseFetchGit);
@@ -86,6 +89,9 @@ lib.extendMkDerivation rec {
     varPrefix ? null,
     varBase ? "NIX${lib.optionalString (finalAttrs.varPrefix != null) "_${finalAttrs.varPrefix}"}_${lib.toUpper finalAttrs.providerName}_PRIVATE_",
     netrcMachineName ? finalAttrs.domain,
+    browsableUrl ? "",
+    archiveUrl,
+    gitRepoUrl ? "https://${finalAttrs.domain}/${finalAttrs.owner}/${finalAttrs.repo}.git",
     passthru ? { },
     meta ? { },
     derivationArgs ? { },
@@ -115,11 +121,10 @@ lib.extendMkDerivation rec {
       else
         builtins.unsafeGetAttrPos "rev" args
     );
-    baseUrl = "https://${domain}/${owner}/${repo}";
     newMeta =
       meta
       // {
-        homepage = meta.homepage or baseUrl;
+        ${if browsableUrl != "" then "homepage" else null} = meta.homepage or browsableUrl;
         identifiers = {
           purlParts =
             if domain == "github.com" then
@@ -164,8 +169,6 @@ lib.extendMkDerivation rec {
       ]);
     };
 
-    gitRepoUrl = "${baseUrl}.git";
-
     derivationArgsCommon = {
       inherit
         domain
@@ -199,26 +202,8 @@ lib.extendMkDerivation rec {
             derivationArgs = derivationArgsCommon // derivationArgs;
           }
         else
-          let
-            revWithTag = finalAttrs.rev;
-          in
           {
-            # Use the API endpoint for private repos, as the archive URI doesn't
-            # support access with GitHub's fine-grained access tokens.
-            #
-            # Use the archive URI for non-private repos, as the API endpoint has
-            # relatively restrictive rate limits for unauthenticated users.
-            url =
-              if finalAttrs.private then
-                let
-                  endpoint = "/repos/${finalAttrs.owner}/${finalAttrs.repo}/tarball/${revWithTag}";
-                in
-                if domain == "github.com" then
-                  "https://api.github.com${endpoint}"
-                else
-                  "https://${domain}/api/v3${endpoint}"
-              else
-                "${baseUrl}/archive/${revWithTag}.tar.gz";
+            url = archiveUrl;
             extension = "tar.gz";
             derivationArgs = derivationArgsCommon // {
               inherit
