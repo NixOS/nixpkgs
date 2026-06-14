da1c954bd509f920b07d4737892392efe78afd9b
pancaek <20342389+pancaek@users.noreply.github.com>
2026-06-14 14:44:19 -0700

    samim-fonts: use installFonts

--- pkgs/by-name/sa/samim-fonts/package.nix
+++ pkgs/by-name/sa/samim-fonts/package.nix
@@ -2,26 +2,26 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
+  installFonts,
 }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "samim-fonts";
   version = "4.0.5";
+
+  outputs = [
+    "out"
+    "webfont"
+  ];
 
   src = fetchFromGitHub {
     owner = "rastikerdar";
     repo = "samim-font";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-DVBMsNOVAVwzlZ3cDus/3CSsC05bLZalQ2KeueEvwXs=";
   };
 
-  installPhase = ''
-    runHook preInstall
-
-    find . -name '*.ttf' -exec install -m444 -Dt $out/share/fonts/samim-fonts {} \;
-
-    runHook postInstall
-  '';
+  nativeBuildInputs = [ installFonts ];
 
   meta = {
     homepage = "https://github.com/rastikerdar/samim-font";
@@ -30,4 +30,4 @@ stdenvNoCC.mkDerivation rec {
     platforms = lib.platforms.all;
     maintainers = [ ];
   };
-}
+})
