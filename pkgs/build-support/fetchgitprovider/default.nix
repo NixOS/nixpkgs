6b8393f605b6ab048dac954d6d58f6c0667c6246
Yueh-Shun Li <shamrocklee@posteo.net>
2026-06-15 02:40:48 +0800

    fetchFromGitProvider: take fixed-point arguments

--- pkgs/build-support/fetchgitprovider/default.nix
+++ pkgs/build-support/fetchgitprovider/default.nix
@@ -34,13 +34,40 @@ let
 
   faUseFetchGit = lib.mapAttrs (_: _: true) useFetchGitArgsDefault;
 
-  mirrorArgs = f: lib.setFunctionArgs f (faUseFetchGit // lib.functionArgs f);
-
   # fetchzip may not be overridable when using external tools, for example nix-prefetch
   fetchzip =
     if args.fetchzip ? override then args.fetchzip.override { withUnzip = false; } else args.fetchzip;
 in
-mirrorArgs (
+lib.extendMkDerivation rec {
+  constructDrv = {
+    __functor =
+      self: fpArgsExtended:
+      let
+        blankArgs = lib.mapAttrs (n: v: null) (lib.filterAttrs (n: v: !v) (lib.functionArgs extendDrvArgs));
+        inherit (fpArgsExtended blankArgs) useFetchGit;
+        # We prefer fetchzip in cases we don't need submodules as the hash
+        # is more stable in that case.
+        fetcher = if useFetchGit then fetchgit else fetchzip;
+      in
+      fetcher (finalAttrs: lib.removeAttrs (fpArgsExtended finalAttrs) [ "useFetchGit" ]);
+    __functionArgs = lib.functionArgs fetchzip // lib.functionArgs fetchgit // faUseFetchGit;
+  };
+
+  excludeDrvArgNames = [
+    "owner"
+    "repo"
+    "tag"
+    "rev"
+    "functionName"
+    "private"
+    "domain"
+    "varPrefix"
+    "derivationArgs"
+  ]
+  ++ (lib.attrNames faUseFetchGit);
+
+  extendDrvArgs =
+  finalAttrs:
   {
     owner,
     repo,
@@ -109,24 +136,7 @@ mirrorArgs (
         # to indicate where derivation originates, similar to make-derivation.nix's mkDerivation
         position = "${position.file}:${toString position.line}";
       };
-    passthruAttrs = removeAttrs args (
-      [
-        "owner"
-        "repo"
-        "tag"
-        "rev"
-        "functionName"
-        "private"
-        "domain"
-        "varPrefix"
-        "derivationArgs"
-      ]
-      ++ (if useFetchGit then excludeUseFetchGitArgNames else lib.attrNames faUseFetchGit)
-    );
     varBase = "NIX${lib.optionalString (varPrefix != null) "_${varPrefix}"}_GITHUB_PRIVATE_";
-    # We prefer fetchzip in cases we don't need submodules as the hash
-    # is more stable in that case.
-    fetcher = if useFetchGit then fetchgit else fetchzip;
     privateAttrs = lib.optionalAttrs private {
       netrcPhase =
         # When using private repos:
@@ -155,9 +165,7 @@ mirrorArgs (
     gitRepoUrl = "${baseUrl}.git";
 
     fetcherArgs =
-      finalAttrs:
-      passthruAttrs
-      // (
+      (
         if useFetchGit then
           useFetchGitArgsWDPassing
           // {
@@ -218,6 +226,5 @@ mirrorArgs (
         meta = newMeta;
       };
   in
-
-  fetcher fetcherArgs
-)
+  fetcherArgs // { inherit useFetchGit; };
+}
