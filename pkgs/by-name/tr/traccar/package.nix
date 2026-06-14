35e79ff0c92e267c85c2850ee9744098660b3fe7
whoomee <git@miglix.eu>
2026-06-14 21:08:37 +0200

    traccar: 6.13.3 -> 6.14.4

--- pkgs/by-name/tr/traccar/package.nix
+++ pkgs/by-name/tr/traccar/package.nix
@@ -1,43 +1,103 @@
 {
-  fetchzip,
   lib,
-  pkgs,
-  stdenvNoCC,
+  stdenv,
+  fetchFromGitHub,
+  buildNpmPackage,
+  makeWrapper,
+  jre,
+  gradle,
+  nix-update-script,
 }:
-stdenvNoCC.mkDerivation rec {
-  pname = "traccar";
-  version = "6.13.3";
-  nativeBuildInputs = [ pkgs.makeWrapper ];
 
-  src = fetchzip {
-    stripRoot = false;
-    url = "https://github.com/traccar/traccar/releases/download/v${version}/traccar-other-${version}.zip";
-    hash = "sha256-QQJ3+QByXkdfGBPA38F8+sQ72NnRbet6zcD3K82JLbI=";
+stdenv.mkDerivation (finalAttrs: {
+  pname = "traccar";
+  version = "6.14.4";
+
+  src = fetchFromGitHub {
+    owner = "traccar";
+    repo = "traccar";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-9ZwwBPWBz2YfcUYETq9PZdunWiBVuZvFtsx6E+BDPmU=";
   };
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    makeWrapper
+    gradle
+  ];
+
+  # required for Darwin builds
+  __darwinAllowLocalNetworking = true;
+
+  mitmCache = gradle.fetchDeps {
+    pkg = finalAttrs.finalPackage;
+    data = ./deps.json;
+  };
+
+  doCheck = true;
+
+  traccar-web = buildNpmPackage (finalAttrs': {
+    pname = "${finalAttrs.pname}-web";
+    inherit (finalAttrs) version;
+
+    src = fetchFromGitHub {
+      owner = "traccar";
+      repo = "traccar-web";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-9rftLQIpQVZgkJaQUZ1FORKA2/cJxR5V1JGMAqC7zao=";
+    };
+
+    npmDepsHash = "sha256-KC7gPDeGl+M3T7iAUSO5BRUVXZrioeXye1gd85wC/mI=";
+
+    installPhase = ''
+      cp -r build $out
+    '';
+  });
 
   installPhase = ''
     runHook preInstall
 
-    for dir in lib schema templates web ; do
-      mkdir -p $out/$dir
-      cp -a $dir $out
-    done
+    mkdir -p $out/{bin,share/traccar}
+    cp target/tracker-server.jar $out/traccar.jar
 
-    mkdir -p $out/share/traccar
-    install -Dm644 tracker-server.jar $out
+    cp -r target/lib $out
 
-    makeWrapper ${pkgs.openjdk}/bin/java $out/bin/traccar \
-      --add-flags "-jar $out/tracker-server.jar"
+    cp -rt $out schema templates
+
+    makeWrapper ${lib.getExe jre} $out/bin/traccar \
+      --add-flags "-jar $out/traccar.jar"
+
+    mkdir -p $out/web
+    cp -r ${finalAttrs.traccar-web}/* $out/web
 
     runHook postInstall
   '';
 
+  passthru = {
+    traccar-web = finalAttrs.traccar-web;
+    updateScript = nix-update-script {
+      extraArgs = [
+        "--subpackage"
+        "traccar-web"
+      ];
+    };
+  };
+
   meta = {
     description = "Open source GPS tracking system";
     homepage = "https://www.traccar.org/";
-    sourceProvenance = with lib.sourceTypes; [ binaryBytecode ];
+    sourceProvenance = with lib.sourceTypes; [
+      fromSource
+      binaryBytecode # mitm cache
+    ];
     license = lib.licenses.asl20;
     mainProgram = "traccar";
-    maintainers = with lib.maintainers; [ frederictobiasc ];
+    maintainers = with lib.maintainers; [
+      frederictobiasc
+      tmarkus
+    ];
   };
-}
+
+})
