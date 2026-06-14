668a960aeb7b04fd9efaf01e1286e85cf74fe7b2
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-14 06:32:14 +0000

    oelint-adv: 9.8.2 -> 9.8.4

--- pkgs/by-name/oe/oelint-adv/package.nix
+++ pkgs/by-name/oe/oelint-adv/package.nix
@@ -7,14 +7,14 @@
 
 python3Packages.buildPythonApplication (finalAttrs: {
   pname = "oelint-adv";
-  version = "9.8.2";
+  version = "9.8.4";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "priv-kweihmann";
     repo = "oelint-adv";
     tag = finalAttrs.version;
-    hash = "sha256-mZleG+NAVqjeJ66SlYYvF90XwJ4H1wlsMOQRBdH1qhM=";
+    hash = "sha256-bQFjf+BAmJKsjcjZcTc34+4bcSq4hgSBKgHRcYA1ai0=";
   };
 
   postPatch = ''
