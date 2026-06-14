0dbb54ca171cdcd163cfb04f6839ce0afc7ce554
Yueh-Shun Li <shamrocklee@posteo.net>
2026-06-15 03:04:14 +0800

    fetchFromGitHub: reference to fetchGitProvider with finalAttrs

--- pkgs/build-support/fetchgithub/default.nix
+++ pkgs/build-support/fetchgithub/default.nix
@@ -2,28 +2,24 @@
   lib,
   fetchFromGitProvider,
 }:
-let
-  decorate =
-    f:
-    lib.makeOverridable (
-      lib.setFunctionArgs f (lib.functionArgs fetchFromGitProvider // lib.functionArgs f)
-    );
-in
-decorate (
-  {
-    githubBase ? "github.com",
-    ...
-  }@args:
-  fetchFromGitProvider (
-    removeAttrs args [
+lib.makeOverridable (
+  lib.extendMkDerivation {
+    constructDrv = fetchFromGitProvider;
+    excludeDrvArgNames = [
       "githubBase"
-    ]
-    // {
-      domain = githubBase;
-      functionName = "fetchFromGitHub";
-      derivationArgs = {
-        inherit githubBase;
+    ];
+    extendDrvArgs =
+      finalAttrs:
+      {
+        githubBase ? "github.com",
+        ...
+      }:
+      {
+        domain = finalAttrs.githubBase;
+        functionName = "fetchFromGitHub";
+        derivationArgs = {
+          inherit githubBase;
+        };
       };
-    }
-  )
+  }
 )
