42e7738e64b964e4d4f7bb10a2de8685e4cb0b82
Harinn <prinn.dev@pm.me>
2026-06-15 21:22:25 +0700

    python3Packages.xpybutil: migrate to pyproject

--- pkgs/development/python-modules/xpybutil/default.nix
+++ pkgs/development/python-modules/xpybutil/default.nix
@@ -2,6 +2,7 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   xcffib,
   pillow,
 }:
@@ -9,7 +10,7 @@
 buildPythonPackage rec {
   pname = "xpybutil";
   version = "0.0.6";
-  format = "setuptools";
+  pyproject = true;
 
   # Pypi only offers a wheel
   src = fetchFromGitHub {
@@ -19,8 +20,10 @@ buildPythonPackage rec {
     sha256 = "17gbqq955fcl29aayn8l0x14azc60cxgkvdxblz9q8x3l50w0xpg";
   };
 
+  build-system = [ setuptools ];
+
   # pillow is a dependency in image.py which is not listed in setup.py
-  propagatedBuildInputs = [
+  dependencies = [
     pillow
     xcffib
   ];
