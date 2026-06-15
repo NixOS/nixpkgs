a70772a6a4e3f5961c211fbfcdb2611d9e1916f0
Harinn <prinn.dev@pm.me>
2026-06-15 21:20:31 +0700

    python3Packages.xmodem: modernize

--- pkgs/development/python-modules/xmodem/default.nix
+++ pkgs/development/python-modules/xmodem/default.nix
@@ -8,16 +8,18 @@
   lrzsz,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "xmodem";
   version = "0.4.7";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "tehmaze";
     repo = "xmodem";
-    tag = version;
-    sha256 = "sha256-kwPA/lYiv6IJSKGRuH13tBofZwp19vebwQniHK7A/i8=";
+    tag = finalAttrs.version;
+    hash = "sha256-kwPA/lYiv6IJSKGRuH13tBofZwp19vebwQniHK7A/i8=";
   };
 
   build-system = [ setuptools ];
@@ -32,10 +34,12 @@ buildPythonPackage rec {
     pytest
   '';
 
+  pythonImportsCheck = [ "xmodem" ];
+
   meta = {
     description = "Pure python implementation of the XMODEM protocol";
     maintainers = with lib.maintainers; [ emantor ];
     homepage = "https://github.com/tehmaze/xmodem";
     license = lib.licenses.mit;
   };
-}
+})
