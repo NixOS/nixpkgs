3b7e0a650fe97cdd079a70a61764801548fb8678
Harinn <prinn.dev@pm.me>
2026-06-15 22:08:09 +0700

    python3Packages.yaswfp: migrate to pyproject

--- pkgs/development/python-modules/yaswfp/default.nix
+++ pkgs/development/python-modules/yaswfp/default.nix
@@ -2,13 +2,14 @@
   lib,
   buildPythonPackage,
   fetchFromGitHub,
+  setuptools,
   pytestCheckHook,
 }:
 
 buildPythonPackage {
   pname = "yaswfp";
   version = "unstable-20210331";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "facundobatista";
@@ -16,6 +17,8 @@ buildPythonPackage {
     rev = "2a2cc6ca4c0b4d52bd2e658fb5f80fdc0db4924c";
     sha256 = "1dxdz89hlycy1rnn269fwl1f0qxgxqarkc0ivs2m77f8xba2qgj9";
   };
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
