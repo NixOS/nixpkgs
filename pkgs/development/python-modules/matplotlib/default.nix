2b080788e2178808fbb42674fbef8daae9aedebe
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-06-15 15:33:57 +0200

    python3Packages.matplotlib: relax setuptools-scm constraint
    Per their upstream comment this is fine for downstream distributors, as
    it only breaks editable installs for them.

--- pkgs/development/python-modules/matplotlib/default.nix
+++ pkgs/development/python-modules/matplotlib/default.nix
@@ -95,6 +95,9 @@ buildPythonPackage (finalAttrs: {
     ''
     + ''
       patchShebangs tools
+
+      substituteInPlace pyproject.toml \
+        --replace-fail "setuptools_scm>=7,<10" "setuptools_scm"
     ''
     + lib.optionalString (stdenv.hostPlatform.isLinux && interactive) ''
       # fix paths to libraries in dlopen calls (headless detection)
