306f15149a68c96d28b4faaf13e6bc5d86f528ce
Harinn <prinn.dev@pm.me>
2026-06-15 21:33:15 +0700

    python3Packages.xstatic-bootbox: migrate to pyproject

--- pkgs/development/python-modules/xstatic-bootbox/default.nix
+++ pkgs/development/python-modules/xstatic-bootbox/default.nix
@@ -2,18 +2,21 @@
   buildPythonPackage,
   lib,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "xstatic-bootbox";
   version = "5.5.1.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "XStatic-Bootbox";
     inherit version;
     sha256 = "4b2120bb33a1d8ada8f9e0532ad99987aa03879b17b08bfdc6b8326d6eb7c205";
   };
+
+  build-system = [ setuptools ];
 
   # no tests implemented
   doCheck = false;
