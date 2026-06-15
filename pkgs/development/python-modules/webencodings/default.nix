fe8817af0d5f6a7aac5e9fba56a106f78663e81d
Harinn <prinn.dev@pm.me>
2026-06-15 23:09:20 +0700

    python3Packages.webencodings: modernize

--- pkgs/development/python-modules/webencodings/default.nix
+++ pkgs/development/python-modules/webencodings/default.nix
@@ -6,14 +6,16 @@
   pytest,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "webencodings";
   version = "0.5.1";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "b36a1c245f2d304965eb4e0a82848379241dc04b865afcc4aab16748587e1923";
+    inherit (finalAttrs) pname version;
+    hash = "sha256-s2ocJF8tMEll604KgoSDeSQdwEuGWvzEqrFnSFh+GSM=";
   };
 
   build-system = [ setuptools ];
@@ -24,9 +26,11 @@ buildPythonPackage rec {
     py.test webencodings/tests.py
   '';
 
+  pythonImportsCheck = [ "webencodings" ];
+
   meta = {
     description = "Character encoding aliases for legacy web content";
     homepage = "https://github.com/SimonSapin/python-webencodings";
     license = lib.licenses.bsd3;
   };
-}
+})
