cd27f78020062fb9e52a7ec4f351722cc910b9fa
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-15 17:12:38 +0000

    python3Packages.gocardless-pro: 3.3.0 -> 3.5.0

--- pkgs/development/python-modules/gocardless-pro/default.nix
+++ pkgs/development/python-modules/gocardless-pro/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "gocardless-pro";
-  version = "3.3.0";
+  version = "3.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "gocardless";
     repo = "gocardless-pro-python";
     tag = "v${version}";
-    hash = "sha256-XD5GUiSHTq/DLrKo6FY4moNnbFpXkVJWM13Yu6c+tZw=";
+    hash = "sha256-SNpBS8Cn6n+zXpxLLbI1sxm9xlXf3a+fAI02DfLO5ak=";
   };
 
   build-system = [ setuptools ];
