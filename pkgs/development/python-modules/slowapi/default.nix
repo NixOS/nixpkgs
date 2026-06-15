eaffa2adf20debd49ab293b93aa4464a1711c705
Fabian Affolter <mail@fabian-affolter.ch>
2026-06-15 08:22:05 +0200

    python3Packages.slowapi: migrate to finalAttrs

--- pkgs/development/python-modules/slowapi/default.nix
+++ pkgs/development/python-modules/slowapi/default.nix
@@ -13,7 +13,7 @@
   starlette,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "slowapi";
   version = "0.1.10";
   pyproject = true;
@@ -21,13 +21,13 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "laurentS";
     repo = "slowapi";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-YNL/xfs8fmkAGagMhqJX3tXoltjHznZjUrF/a2RWCDs=";
   };
 
-  nativeBuildInputs = [ poetry-core ];
+  build-system = [ poetry-core ];
 
-  propagatedBuildInputs = [
+  dependencies = [
     limits
     redis
   ];
@@ -52,8 +52,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python library for API rate limiting";
     homepage = "https://github.com/laurentS/slowapi";
-    changelog = "https://github.com/laurentS/slowapi/blob/v${version}/CHANGELOG.md";
-    license = with lib.licenses; [ mit ];
+    changelog = "https://github.com/laurentS/slowapi/blob/${finalAttrs.src.tag}/CHANGELOG.md";
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
