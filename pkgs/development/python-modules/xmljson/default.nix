8a90480cfe39bada6f87d23acd8902efdc090e4d
Harinn <prinn.dev@pm.me>
2026-06-15 21:15:49 +0700

    python3Packages.xmljson: migrate to pyproject

--- pkgs/development/python-modules/xmljson/default.nix
+++ pkgs/development/python-modules/xmljson/default.nix
@@ -2,18 +2,21 @@
   buildPythonPackage,
   fetchPypi,
   lib,
+  setuptools,
   lxml,
 }:
 
 buildPythonPackage rec {
   pname = "xmljson";
   version = "0.2.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     sha256 = "b4158e66aa1e62ee39f7f80eb2fe4f767670ba3c0d5de9804420dc53427fdec8";
   };
+
+  build-system = [ setuptools ];
 
   nativeCheckInputs = [ lxml ];
 
