2f7d4981bca14614fce68de99b9a376ddabd20e7
Harinn <prinn.dev@pm.me>
2026-06-15 22:26:48 +0700

    python3Packages.vpk: modernize

--- pkgs/development/python-modules/vpk/default.nix
+++ pkgs/development/python-modules/vpk/default.nix
@@ -6,21 +6,25 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "vpk";
   version = "1.4.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "ValvePython";
     repo = "vpk";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-SPkPb8kveAR2cN9kd2plS+TjmBYBCfa6pJ0c22l69M0=";
   };
 
   build-system = [ setuptools ];
 
   nativeCheckInputs = [ pytestCheckHook ];
+
+  pythonImportsCheck = [ "vpk" ];
 
   meta = {
     description = "Library for working with Valve Pak files";
@@ -29,4 +33,4 @@ buildPythonPackage rec {
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ joshuafern ];
   };
-}
+})
