d6e99b9efe6d42caa58c4751a1a1cb0434c7f333
Harinn <prinn.dev@pm.me>
2026-06-15 21:51:49 +0700

    python3Packages.xstatic-jquery-ui: migrate to pyproject

--- pkgs/development/python-modules/xstatic-jquery-ui/default.nix
+++ pkgs/development/python-modules/xstatic-jquery-ui/default.nix
@@ -2,13 +2,14 @@
   buildPythonPackage,
   lib,
   fetchPypi,
+  setuptools,
   xstatic-jquery,
 }:
 
 buildPythonPackage rec {
   pname = "xstatic-jquery-ui";
   version = "1.13.0.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     pname = "XStatic-jquery-ui";
@@ -16,10 +17,12 @@ buildPythonPackage rec {
     sha256 = "3697e5f0ef355b8f4a1c724221592683c2db031935cbb57b46224eef474bd294";
   };
 
+  build-system = [ setuptools ];
+
   # no tests implemented
   doCheck = false;
 
-  propagatedBuildInputs = [ xstatic-jquery ];
+  dependencies = [ xstatic-jquery ];
 
   meta = {
     homepage = "https://jqueryui.com/";
