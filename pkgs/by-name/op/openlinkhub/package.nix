05e54eea8fd756d4858195bb3b278e7e2c865a58
skelettor <skelettor@skcloud.fr>
2026-06-15 14:59:30 +0200

    openlinkhub: 0.7.5 -> 0.8.8

--- pkgs/by-name/op/openlinkhub/package.nix
+++ pkgs/by-name/op/openlinkhub/package.nix
@@ -2,6 +2,8 @@
   buildGoModule,
   lib,
   fetchFromGitHub,
+  pkg-config,
+  pipewire,
   udev,
   nix-update-script,
   versionCheckHook,
@@ -9,22 +11,32 @@
 
 buildGoModule rec {
   pname = "openlinkhub";
-  version = "0.7.5";
+  version = "0.8.8";
 
   src = fetchFromGitHub {
     owner = "jurkovic-nikola";
     repo = "OpenLinkHub";
     tag = version;
-    hash = "sha256-Jq31ZcJtl0ZmjNsMOiTTt2eZIIYn2DRVPE4Q5FTx6OM=";
+    hash = "sha256-uYuhmvdHNVs19egakwDOvIJ2IEAeZEAV6qgMYEl+Ie4=";
   };
 
   proxyVendor = true;
 
-  vendorHash = "sha256-xpIaQzl2jrWRIUe/1woODKLlwxQrdlCLkIk0qmWs7m0=";
+  vendorHash = "sha256-/itomxsbTDT7ML52bpUfDZIBZ/Rh/zx4Blg+PP7m7gE=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
 
   buildInputs = [
     udev
+    pipewire
   ];
+
+  env = {
+    CGO_ENABLED = "1";
+    CGO_CFLAGS_ALLOW = "-fno-strict-overflow";
+  };
 
   passthru.updateScript = nix-update-script { };
 
