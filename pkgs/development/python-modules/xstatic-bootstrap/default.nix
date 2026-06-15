6ef663aafea3a176a88a6a821303cbf4124594b9
Harinn <prinn.dev@pm.me>
2026-06-15 21:37:43 +0700

    python3Packages.xstatic-bootstrap: modernize

--- pkgs/development/python-modules/xstatic-bootstrap/default.nix
+++ pkgs/development/python-modules/xstatic-bootstrap/default.nix
@@ -5,14 +5,16 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "xstatic-bootstrap";
   version = "5.3.8.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
     pname = "xstatic_bootstrap";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-BPXMlbvlQ40ehR0GxMoa1/hL02oJtN5aH1S1JOhQaFk=";
   };
 
@@ -21,10 +23,12 @@ buildPythonPackage rec {
   # no tests implemented
   doCheck = false;
 
+  pythonImportsCheck = [ "xstatic.pkg.bootstrap" ];
+
   meta = {
     homepage = "https://getbootstrap.com";
     description = "Bootstrap packaged static files for python";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ makefu ];
   };
-}
+})
