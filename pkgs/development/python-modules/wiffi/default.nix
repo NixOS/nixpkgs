e4c56a27798f111ec98ec789b036a7a9852cae6b
Harinn <prinn.dev@pm.me>
2026-06-15 20:41:19 +0700

    python3Packages.wiffi: modernize

--- pkgs/development/python-modules/wiffi/default.nix
+++ pkgs/development/python-modules/wiffi/default.nix
@@ -6,15 +6,17 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "wiffi";
   version = "1.1.2";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "mampfes";
     repo = "python-wiffi";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-pnbzJxq8K947Yg54LysPPho6IRKf0cc+szTETgyzFao=";
   };
 
@@ -30,8 +32,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python module to interface with STALL WIFFI devices";
     homepage = "https://github.com/mampfes/python-wiffi";
-    changelog = "https://github.com/mampfes/python-wiffi/blob/${version}/HISTORY.md";
+    changelog = "https://github.com/mampfes/python-wiffi/blob/${finalAttrs.version}/HISTORY.md";
     license = with lib.licenses; [ mit ];
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
