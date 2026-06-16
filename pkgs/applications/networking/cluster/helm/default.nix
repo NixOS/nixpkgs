f21c0818ca34bf5728318a2a0f512bec692580c7
R. Ryantm <ryantm-bot@ryantm.com>
2026-06-16 23:12:53 +0000

    kubernetes-helm: 4.2.0 -> 4.2.1

--- pkgs/applications/networking/cluster/helm/default.nix
+++ pkgs/applications/networking/cluster/helm/default.nix
@@ -10,16 +10,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "kubernetes-helm";
-  version = "4.2.0";
+  version = "4.2.1";
 
   src = fetchFromGitHub {
     owner = "helm";
     repo = "helm";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-Wyihzf7KpnVuIdp5lmjhB7uLAGgtmI0TXYl29uaVC5Y=";
+    hash = "sha256-k2lZXdWYnewNiZdLNQrC5j9A3JkvYCwMY486QxjCpaw=";
   };
 
-  vendorHash = "sha256-QTDC0v0BPE3FoK9AAq1n2jWxOE9gB9OsoY2wnpcCDUQ=";
+  vendorHash = "sha256-XIKQF9PWgxKJUt66wQ/mPhWVfJnra0vV9ZuyclgQ21U=";
 
   subPackages = [ "cmd/helm" ];
   ldflags = [
