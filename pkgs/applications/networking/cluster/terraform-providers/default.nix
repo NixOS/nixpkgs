260f0e04edcccf1a9c133ec4e5ad47c0541d81b5
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-06-16 02:08:06 -0400

    terraform-providers: set __structuredAttrs = true;
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/applications/networking/cluster/terraform-providers/default.nix
+++ pkgs/applications/networking/cluster/terraform-providers/default.nix
@@ -45,6 +45,7 @@ let
         proxyVendor
         ;
       subPackages = [ "." ];
+      __structuredAttrs = true;
       doCheck = false;
       # https://github.com/hashicorp/terraform-provider-scaffolding/blob/a8ac8375a7082befe55b71c8cbb048493dd220c2/.goreleaser.yml
       # goreleaser (used for builds distributed via terraform registry) requires that CGO is disabled
