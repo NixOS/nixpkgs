85f89ea677bbc587959e8a4c4bcd62815c14c523
Yifei Sun <ysun@hey.com>
2026-06-17 21:34:23 +0200

    coqPackages.VplTactic: modernize mkCoqDerivation release hash format

--- pkgs/development/coq-modules/VplTactic/default.nix
+++ pkgs/development/coq-modules/VplTactic/default.nix
@@ -12,7 +12,7 @@ mkCoqDerivation {
   defaultVersion = if lib.versions.isEq "8.9" coq.version then "0.5" else null;
 
   release."0.5".rev = "487e3aff8446bed2c5116cefc7d71d98a06e85de";
-  release."0.5".sha256 = "sha256-4h0hyvj9R+GOgnGWQFDi0oENLZPiJoimyK1q327qvIY=";
+  release."0.5".hash = "sha256-4h0hyvj9R+GOgnGWQFDi0oENLZPiJoimyK1q327qvIY=";
 
   buildInputs = [ coq.ocamlPackages.vpl-core ];
   propagatedBuildInputs = [ Vpl ];
